382854b25920446382d6dcf1c9677739a3dbf2c3
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2022-01-15 17:07:05 +0300

    Show the welcome screen if opening the project on start fails: IDEA-286542
    For instance, if the user drops a file to the application icon, and then resists opening the folder in the Trust Project dialog, then the project instance is null. If there are no projects to reopen, the previous code used to stuck on the splash screen. It should show the welcome screen instead.
    
    (cherry picked from commit d05e4492886fe552353f07d3d3c5d2f36a0d2ec1)
    (cherry picked from commit 6d772ecc5ec91ad5e91cc555d2f7d9b980d7468a)
    
    GitOrigin-RevId: 789f1109d3ba7ab30e3463d4806d83f65c6b2391

--- platform/platform-impl/src/com/intellij/idea/IdeStarter.kt
+++ platform/platform-impl/src/com/intellij/idea/IdeStarter.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 @file:Suppress("ReplaceNegatedIsEmptyWithIsNotEmpty")
 package com.intellij.idea
 
@@ -159,7 +159,9 @@ open class IdeStarter : ApplicationStarter {
           }
       }
     }
-    return CompletableFuture.completedFuture(null)
+    return CompletableFuture.completedFuture(null).thenRun {
+      WelcomeFrame.showIfNoProjectOpened(lifecyclePublisher)
+    }
   }
 
   private fun showWelcomeFrame(lifecyclePublisher: AppLifecycleListener, willOpenProject: Boolean): Boolean {
