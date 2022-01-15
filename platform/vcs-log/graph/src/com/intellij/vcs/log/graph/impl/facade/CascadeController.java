08808d7a465ff7546677c977897741b4d4aff2d3
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2022-01-15 20:44:59 +0300

    [vcs-log-graph] do not implement CascadeController in base controller classes
    GitOrigin-RevId: 54e9c84f1af4aa61350583935f31c901c410e3d0

--- platform/vcs-log/graph/src/com/intellij/vcs/log/graph/impl/facade/CascadeController.java
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/graph/impl/facade/CascadeController.java
@@ -1,33 +1,18 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.log.graph.impl.facade;
 
 import com.intellij.util.Function;
 import com.intellij.vcs.log.graph.api.elements.GraphElement;
 import com.intellij.vcs.log.graph.api.permanent.PermanentGraphInfo;
 import com.intellij.vcs.log.graph.impl.print.elements.PrintElementWithGraphElement;
-import com.intellij.vcs.log.graph.utils.LinearGraphUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class CascadeController implements LinearGraphController {
-  @Nullable private final LinearGraphController myDelegateController;
+  private final @NotNull LinearGraphController myDelegateController;
   @NotNull protected final PermanentGraphInfo myPermanentGraphInfo;
 
-  protected CascadeController(@Nullable LinearGraphController delegateController, @NotNull PermanentGraphInfo permanentGraphInfo) {
+  protected CascadeController(@NotNull LinearGraphController delegateController, @NotNull PermanentGraphInfo permanentGraphInfo) {
     myDelegateController = delegateController;
     myPermanentGraphInfo = permanentGraphInfo;
   }
@@ -36,13 +21,12 @@ public abstract class CascadeController implements LinearGraphController {
   @Override
   public LinearGraphAnswer performLinearGraphAction(@NotNull LinearGraphAction action) {
     LinearGraphAnswer answer = performAction(action);
-    if (answer == null && myDelegateController != null) {
+    if (answer == null) {
       answer = myDelegateController.performLinearGraphAction(
         new VisibleGraphImpl.LinearGraphActionImpl(convertToDelegate(action.getAffectedElement()), action.getType()));
       answer = delegateGraphChanged(answer);
     }
-    if (answer != null) return answer;
-    return LinearGraphUtils.DEFAULT_GRAPH_ANSWER;
+    return answer;
   }
 
   @Nullable
@@ -74,7 +58,6 @@ public abstract class CascadeController implements LinearGraphController {
 
   @NotNull
   protected LinearGraphController getDelegateController() {
-    assert myDelegateController != null;
     return myDelegateController;
   }
 
