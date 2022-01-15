6090c7b90201fe7c445feacdf59a59a75bb6cfa4
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2022-01-15 09:38:37 +0200

    Revert "opened packages moved to resource directory for sharing between modules"
    This reverts commit 9ba0fcd178f64192999251cbe532eb4890ed1015.
    
    GitOrigin-RevId: d9f55c8829a7018527ccc3356e3607d29c1c6eab

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/OpenedPackages.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/OpenedPackages.groovy
@@ -1,19 +1,48 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.intellij.build
 
-import com.intellij.openapi.application.PathManager
 import groovy.transform.CompileStatic
-
-import java.nio.file.Paths
 
 @CompileStatic
 final class OpenedPackages implements Iterable<String> {
-  /** Opened packages need to be shared at least with [com.intellij.ide.starter.extended.runner.IdeFromCodeLauncher] */
   @SuppressWarnings('SpellCheckingInspection')
-  private static final List<String> OPENED_PACKAGES =
-    Paths.get(PathManager.getHomePathFor(OpenedPackages.class),
-              "community", "platform", "build-scripts", "resources", "OpenedPackages.txt")
-      .readLines()
+  private static final List<String> OPENED_PACKAGES = List.of(
+    '--add-opens=java.base/java.lang=ALL-UNNAMED',
+    '--add-opens=java.base/java.lang.reflect=ALL-UNNAMED',
+    '--add-opens=java.base/java.text=ALL-UNNAMED',
+    '--add-opens=java.base/java.time=ALL-UNNAMED',
+    '--add-opens=java.base/java.util=ALL-UNNAMED',
+    '--add-opens=java.base/java.util.concurrent=ALL-UNNAMED',
+    '--add-opens=java.base/java.io=ALL-UNNAMED',
+    '--add-opens=java.base/java.net=ALL-UNNAMED',
+    '--add-opens=java.base/java.nio.charset=ALL-UNNAMED',
+    '--add-opens=java.base/jdk.internal.vm=ALL-UNNAMED',
+    '--add-opens=java.base/sun.nio.ch=ALL-UNNAMED',
+    '--add-opens=java.desktop/java.awt=ALL-UNNAMED',
+    '--add-opens=java.desktop/java.awt.dnd.peer=ALL-UNNAMED',
+    '--add-opens=java.desktop/java.awt.event=ALL-UNNAMED',
+    '--add-opens=java.desktop/java.awt.image=ALL-UNNAMED',
+    '--add-opens=java.desktop/java.awt.peer=ALL-UNNAMED',
+    '--add-opens=java.desktop/javax.swing=ALL-UNNAMED',
+    '--add-opens=java.desktop/javax.swing.plaf.basic=ALL-UNNAMED',
+    '--add-opens=java.desktop/javax.swing.text.html=ALL-UNNAMED',
+    '--add-opens=java.desktop/sun.awt=ALL-UNNAMED',
+    '--add-opens=java.desktop/sun.awt.datatransfer=ALL-UNNAMED',
+    '--add-opens=java.desktop/sun.awt.image=ALL-UNNAMED',
+    '--add-opens=java.desktop/sun.awt.windows=ALL-UNNAMED',
+    '--add-opens=java.desktop/sun.awt.X11=ALL-UNNAMED',
+    '--add-opens=java.desktop/sun.font=ALL-UNNAMED',
+    '--add-opens=java.desktop/sun.java2d=ALL-UNNAMED',
+    '--add-opens=java.desktop/sun.lwawt=ALL-UNNAMED',
+    '--add-opens=java.desktop/sun.lwawt.macosx=ALL-UNNAMED',
+    '--add-opens=java.desktop/sun.swing=ALL-UNNAMED',
+    '--add-opens=java.desktop/com.apple.eawt=ALL-UNNAMED',
+    '--add-opens=java.desktop/com.apple.eawt.event=ALL-UNNAMED',
+    '--add-opens=java.desktop/com.apple.laf=ALL-UNNAMED',
+    '--add-opens=jdk.attach/sun.tools.attach=ALL-UNNAMED',
+    '--add-opens=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED',
+    '--add-opens=jdk.jdi/com.sun.tools.jdi=ALL-UNNAMED'
+  )
 
   static final OpenedPackages INSTANCE = new OpenedPackages()
 
