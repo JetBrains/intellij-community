aa4493efa10469379e0489e2d366a699f46cff34
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-01-17 13:14:21 +0100

    cleanup
    GitOrigin-RevId: 462451c0130749d01bf5ceb30e9b6c68c41814f1

--- platform/feedback/src/com/intellij/feedback/FeedbackTypeResolver.kt
+++ platform/feedback/src/com/intellij/feedback/FeedbackTypeResolver.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.feedback
 
 import com.intellij.feedback.state.DontShowAgainFeedbackService
@@ -38,7 +38,7 @@ object FeedbackTypeResolver {
     }
 
   private fun showFeedbackNotification(project: Project?) {
-    if (!Registry.`is`("platform.feedback", true) || isFeedbackNotificationDisabled) {
+    if (isFeedbackNotificationDisabled || !Registry.`is`("platform.feedback", true)) {
       return
     }
     val suitableFeedbackTypes = FeedbackTypes.values().filter { it.isSuitable() }
