91acbe2eecece752f1a819982d71a6a02f49c3da
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2022-01-17 14:41:21 +0300

    [Kotlin][MPP] Fixup HmppImportAndHighlightingTests after expect/actual marker changes
    ^KT-50120
    
    GitOrigin-RevId: d5be409bbceb117465abfe60962619fadbfaa2cd

--- plugins/kotlin/idea/tests/testData/gradle/hmppImportAndHighlighting/multiModulesHmpp/top-mpp/src/jsJvm18iOSMain/kotlin/transitiveStory/bottomActual/mppBeginning/MPOuter.kt
+++ plugins/kotlin/idea/tests/testData/gradle/hmppImportAndHighlighting/multiModulesHmpp/top-mpp/src/jsJvm18iOSMain/kotlin/transitiveStory/bottomActual/mppBeginning/MPOuter.kt
@@ -1,13 +1,13 @@
 package transitiveStory.bottomActual.mppBeginning
 
 // has a child in jsJvm18Main
-actual open class <!LINE_MARKER("descr='Has declaration in common module'"), LINE_MARKER("descr='Is subclassed by ChildOfMPOuterInShared  Click or press ... to navigate'")!>MPOuter<!> {
-    protected actual open val <!LINE_MARKER("descr='Has declaration in common module'")!>b<!>: Int = 2565
-    internal actual val <!LINE_MARKER("descr='Has declaration in common module'")!>c<!>: Int = 2456
-    actual val <!LINE_MARKER("descr='Has declaration in common module'")!>d<!>: Int = 324
+actual open class <!LINE_MARKER("descr='Has expects in multimod-hmpp.top-mpp.commonMain module'"), LINE_MARKER("descr='Is subclassed by ChildOfMPOuterInShared  Click or press ... to navigate'")!>MPOuter<!> {
+    protected actual open val <!LINE_MARKER("descr='Has expects in multimod-hmpp.top-mpp.commonMain module'")!>b<!>: Int = 2565
+    internal actual val <!LINE_MARKER("descr='Has expects in multimod-hmpp.top-mpp.commonMain module'")!>c<!>: Int = 2456
+    actual val <!LINE_MARKER("descr='Has expects in multimod-hmpp.top-mpp.commonMain module'")!>d<!>: Int = 324
 
-    protected actual class <!LINE_MARKER("descr='Has declaration in common module'")!>MPNested<!> {
-        actual val <!LINE_MARKER("descr='Has declaration in common module'")!>e<!>: Int = 245
+    protected actual class <!LINE_MARKER("descr='Has expects in multimod-hmpp.top-mpp.commonMain module'")!>MPNested<!> {
+        actual val <!LINE_MARKER("descr='Has expects in multimod-hmpp.top-mpp.commonMain module'")!>e<!>: Int = 245
     }
 
 }
