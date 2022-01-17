dc5ca00ded56ca82a0879402acf2e32e3f2112de
Razmik Seysyan <razmik.seysyan@jetbrains.com>
2022-01-17 14:56:05 +0100

    IDEA-287002 The async and error handling API for JavaScript execution in JCEF. Code review: IDEA-CR-72120.
    GitOrigin-RevId: 259f865a41898ed2532d1b905bce88f18d4c8c0a

---
+++ platform/platform-api/src/com/intellij/ui/jcef/JBCefBrowserJsCall.kt
@@ -0,0 +1,121 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ui.jcef
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.util.CheckedDisposable
+import com.intellij.openapi.util.Disposer
+import org.intellij.lang.annotations.Language
+import org.jetbrains.concurrency.AsyncPromise
+import org.jetbrains.concurrency.Promise
+
+typealias JsExpression = String
+typealias JsExpressionResult = String?
+typealias JsExpressionResultPromise = AsyncPromise<JsExpressionResult>
+
+/**
+ * Performs JavaScript expression in the JCEF browser asynchronously.
+ * @return The [Promise] that provides JS execution result or an error.
+ */
+fun JBCefBrowser.executeJavaScriptAsync(@Language("JavaScript") javaScriptExpression: JsExpression): Promise<JsExpressionResult> =
+  JBCefBrowserJsCall(javaScriptExpression, this)()
+
+/**
+ * Encapsulates the [javaScriptExpression] which is executed in the provided [browser] by the [invoke] method.
+ * Handles JavaScript errors and submits them to the execution result.
+ * @see [executeJavaScriptAsync]
+ */
+class JBCefBrowserJsCall(val javaScriptExpression: JsExpression, val browser: JBCefBrowser) {
+
+  // TODO: Ensure the related JBCefClient has a sufficient number of slots in the pool
+
+  /**
+   * Performs [javaScriptExpression] in the JCEF [browser] asynchronously.
+   * @return The [Promise] that provides JS execution result or an error.
+   * @throws IllegalStateException if the related [browser] is not initialized (displayed).
+   * @throws IllegalStateException if the related [browser] is disposed.
+   * @see [com.intellij.ui.jcef.JBCefBrowserBase.isCefBrowserCreated]
+   */
+  operator fun invoke(): Promise<JsExpressionResult> {
+    if (browser.isCefBrowserCreated.not())
+      throw IllegalStateException("Failed to execute the requested JS expression. The related JCEF browser in not initialized.")
+
+    /**
+     * The root [com.intellij.openapi.Disposable] object that indicates the lifetime of this call.
+     * Remains undisposed until the [javaScriptExpression] gets executed in the [browser] (either successfully or with an error).
+     */
+    val executionLifetime: CheckedDisposable = Disposer.newCheckedDisposable().also { Disposer.register(browser, it) }
+
+    if (executionLifetime.isDisposed)
+      throw IllegalStateException(
+        "Failed to execute the requested JS expression. The related browser is disposed.")
+
+    val resultPromise = JsExpressionResultPromise().apply {
+      onProcessed {
+        Disposer.dispose(executionLifetime)
+      }
+    }
+
+    Disposer.register(executionLifetime) {
+      resultPromise.setError("The related browser is disposed during the call.")
+    }
+
+    val resultHandlerQuery: JBCefJSQuery = createResultHandlerQuery(executionLifetime, resultPromise)
+    val errorHandlerQuery: JBCefJSQuery = createErrorHandlerQuery(executionLifetime, resultPromise)
+
+    val jsToRun = javaScriptExpression.wrapWithErrorHandling(resultQuery = resultHandlerQuery, errorQuery = errorHandlerQuery)
+
+    try {
+      browser.cefBrowser.executeJavaScript(jsToRun, "", 0)
+    }
+    catch (ex: Exception) {
+      // In case something goes wrong with the browser interop
+      resultPromise.setError(ex)
+    }
+
+    return resultPromise
+  }
+
+  private fun createResultHandlerQuery(parentDisposable: Disposable, resultPromise: JsExpressionResultPromise) =
+    createQuery(parentDisposable).apply {
+      addHandler { result ->
+        resultPromise.setResult(result)
+        null
+      }
+    }
+
+  private fun createErrorHandlerQuery(parentDisposable: Disposable, resultPromise: JsExpressionResultPromise) =
+    createQuery(parentDisposable).apply {
+      addHandler { errorMessage ->
+        resultPromise.setError(errorMessage ?: "Unknown error")
+        null
+      }
+    }
+
+  private fun createQuery(parentDisposable: Disposable) = JBCefJSQuery.create(browser).also { Disposer.register(parentDisposable, it) }
+
+  @Language("JavaScript")
+  private fun @receiver:Language("JavaScript") JsExpression.wrapWithErrorHandling(resultQuery: JBCefJSQuery, errorQuery: JBCefJSQuery) = """
+      try {
+        let result = eval("$this")
+
+        // call back the related JBCefJSQuery
+        window.${resultQuery.funcName} (     
+          {
+            request: '' + result,
+            onSuccess: function(response) {},
+            onFailure: function(error_code, error_message) {}
+          }
+        )
+      }
+      catch (e) {
+        // call back the related error handling JBCefJSQuery
+        window.${errorQuery.funcName} (     
+          {
+            request: '' + e,
+            onSuccess: function(response) {},
+            onFailure: function(error_code, error_message) {}
+          }
+        )
+      }      
+    """.trimIndent()
+}
---
+++ platform/platform-tests/testSrc/com/intellij/ui/jcef/JBCefBrowserJsCallTest.kt
@@ -0,0 +1,119 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ui.jcef
+
+import com.intellij.testFramework.ApplicationRule
+import com.intellij.ui.scale.TestScaleHelper
+import junit.framework.TestCase
+import org.intellij.lang.annotations.Language
+import org.junit.After
+import org.junit.Before
+import org.junit.ClassRule
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
+
+
+/**
+ * Tests the [JBCefBrowserJsCall] class and [executeJavaScriptAsync] method.
+ */
+class JBCefBrowserJsCallTest {
+
+  companion object {
+    @ClassRule
+    @JvmStatic
+    public fun getAppRule() = ApplicationRule()
+  }
+
+  @Before
+  fun before() {
+    TestScaleHelper.assumeStandalone()
+    TestScaleHelper.setSystemProperty("java.awt.headless", "false")
+  }
+
+  @After
+  fun after() {
+    TestScaleHelper.restoreProperties()
+  }
+
+
+  @Test
+  fun `execute unresolved expression`() {
+    doTest("""foo.bar""")
+  }
+
+  @Test
+  fun `execute throw expression`() {
+    doTest(""" throw 'error' """)
+  }
+
+  @Test
+  fun `execute math operation`() {
+    doTest("""2+2""", "4")
+  }
+
+  @Test
+  fun `execute the same call twice and simultaneously`() {
+    val browser = prepareBrowser()
+    val jsCall = JBCefBrowserJsCall("""2+2""", browser)
+    val latch = CountDownLatch(2)
+
+    var r1: String? = null
+    var r2: String? = null
+
+    JBCefTestHelper.invokeAndWaitForLatch(latch) {
+      jsCall().onProcessed { latch.countDown() }.onSuccess { r1 = it }
+      jsCall().onProcessed { latch.countDown() }.onSuccess { r2 = it }
+    }
+
+    TestCase.assertEquals("4", r1)
+    TestCase.assertEquals("4", r2)
+  }
+
+  private fun doTest(@Language("JavaScript") javaScript: String,
+                     expectedResult: String? = null,
+                     isExpectedToSucceed: Boolean = expectedResult != null) {
+
+    val latch = CountDownLatch(1)
+    val browser = prepareBrowser()
+
+    var isSucceeded: Boolean? = null
+    var actualResult: String? = null
+
+    JBCefTestHelper.invokeAndWaitForLatch(latch) {
+      browser.executeJavaScriptAsync(javaScript)
+        .onError {
+          println(it.message)
+          isSucceeded = false
+        }.onSuccess {
+          isSucceeded = true
+          actualResult = it
+        }.onProcessed {
+          latch.countDown()
+        }
+    }
+
+    TestCase.assertEquals(isExpectedToSucceed, isSucceeded)
+
+    if (isExpectedToSucceed) {
+      TestCase.assertEquals(expectedResult, actualResult)
+    }
+  }
+
+
+  private fun prepareBrowser(): JBCefBrowser {
+    val browser = JBCefApp.getInstance().createClient().also {
+      it.setProperty(JBCefClient.Properties.JS_QUERY_POOL_SIZE, 24)
+    }.let { jbCefClient ->
+      JBCefBrowser.createBuilder()
+        .setClient(jbCefClient)
+        .setUrl("about:blank")
+        .createBrowser()
+    }
+
+    JBCefTestHelper.showAndWaitForLoad(browser, "DISPATCH")
+
+    TestCase.assertNotNull(browser.component)
+    TestCase.assertTrue(browser.isCefBrowserCreated)
+
+    return browser
+  }
+}
