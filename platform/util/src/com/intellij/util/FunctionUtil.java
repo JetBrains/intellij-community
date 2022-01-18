5d15210c4bd0eca06a977c9e0664424944e112c2
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-01-18 08:18:05 +0100

    cleanup
    GitOrigin-RevId: 50e365d6de18011536dd13e1425c1360505c472a

--- platform/util/src/com/intellij/util/FunctionUtil.java
+++ platform/util/src/com/intellij/util/FunctionUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util;
 
 import org.jetbrains.annotations.Contract;
@@ -7,6 +7,7 @@ import org.jetbrains.annotations.Nullable;
 
 public final class FunctionUtil {
   private static final NullableFunction<?, ?> NULL = new NullableFunction<Object, Object>() {
+    @Override
     public Object fun(final Object o) {
       return null;
     }
