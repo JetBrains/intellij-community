53850596e2131112c71172a1745a63e0b3b33fb5
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2022-01-18 22:12:00 +0300

    Remove automatically added ApiStatus.AvailableSince
    GitOrigin-RevId: c61c567b620a1a4dc3baa90f21f88384b4cfdc9e

--- platform/util/ui/src/com/intellij/ui/ClientProperty.java
+++ platform/util/ui/src/com/intellij/ui/ClientProperty.java
@@ -1,8 +1,7 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ui;
 
 import com.intellij.openapi.util.Key;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -12,7 +11,6 @@ import javax.swing.RootPaneContainer;
 import java.awt.Component;
 import java.awt.Window;
 
-@ApiStatus.AvailableSince("2022.1")
 public final class ClientProperty {
   /**
    * Sets the value for the client property of the component.
