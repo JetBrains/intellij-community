cf1a201c6e8fd2f44bf705d69883df22f37afc33
grigoriy.myasoedov <grigoriy.myasoedov@jetbrains.com>
2022-01-18 21:08:43 +0300

    [maven] IDEA-283167 Implement Maven dependency analyzer
    MavenDependencyAnalyzerGoToAction - navigation to maven module
    
    GitOrigin-RevId: 9359179f1b9b3dd2931151bace01faffc355fe0b

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/project/actions/MavenDependencyAnalyzerGoToAction.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/project/actions/MavenDependencyAnalyzerGoToAction.kt
@@ -3,12 +3,15 @@ package org.jetbrains.idea.maven.project.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.externalSystem.dependency.analyzer.DAArtifact
+import com.intellij.openapi.externalSystem.dependency.analyzer.DAModule
 import com.intellij.openapi.externalSystem.dependency.analyzer.DependencyAnalyzerView
 import com.intellij.openapi.externalSystem.util.ExternalSystemBundle
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.idea.maven.model.MavenId
 import org.jetbrains.idea.maven.navigator.MavenNavigationUtil
+import org.jetbrains.idea.maven.project.MavenDependencyAnalyzerContributor
 import org.jetbrains.idea.maven.project.MavenProjectsManager
 import org.jetbrains.idea.maven.utils.MavenUtil
 import com.intellij.openapi.externalSystem.dependency.analyzer.DependencyAnalyzerDependency as Dependency
@@ -18,11 +21,11 @@ class MavenDependencyAnalyzerGoToAction : AnAction() {
     val dependency = e.getData(DependencyAnalyzerView.DEPENDENCY) ?: return
     val project = e.project ?: return
     val parent = dependency.parent ?: return
-    val artifactParent = parent.data as Dependency.Data.Artifact
-    val artifact = dependency.data as Dependency.Data.Artifact
+    val mavenId = getMavenId(dependency.data) ?: return
+    if (mavenId.groupId == null || mavenId.artifactId == null) return;
 
-    val artifactFile = getArtifactFile(project, artifactParent) ?: return
-    val navigatable = MavenNavigationUtil.createNavigatableForDependency(project, artifactFile, artifact.groupId, artifact.artifactId)
+    val artifactFile = getArtifactFile(project, parent.data) ?: return
+    val navigatable = MavenNavigationUtil.createNavigatableForDependency(project, artifactFile, mavenId.groupId!!, mavenId.artifactId!!)
     if (navigatable.canNavigate()) {
       navigatable.navigate(true)
     }
@@ -34,15 +37,22 @@ class MavenDependencyAnalyzerGoToAction : AnAction() {
     e.presentation.isEnabledAndVisible =
       systemId == MavenUtil.SYSTEM_ID &&
       e.project != null &&
-      dependency?.data is Dependency.Data.Artifact &&
-      dependency.parent?.data is Dependency.Data.Artifact &&
-      getArtifactFile(e.project!!, dependency.parent?.data as Dependency.Data.Artifact) != null
+      getMavenId(dependency?.data) != null &&
+      getArtifactFile(e.project!!, dependency?.parent?.data) != null
   }
 
-  private fun getArtifactFile(project: Project, artifactParent: Dependency.Data.Artifact): VirtualFile? {
-    val mavenId = MavenId(artifactParent.groupId, artifactParent.artifactId, artifactParent.version)
+  private fun getArtifactFile(project: Project, artifactParent: Dependency.Data?): VirtualFile? {
+    val mavenId = getMavenId(artifactParent) ?: return null
     val mavenProject = MavenProjectsManager.getInstance(project).findProject(mavenId)
     return mavenProject?.file ?: MavenNavigationUtil.getArtifactFile(project, mavenId)
+  }
+
+  private fun getMavenId(dependencyData: Dependency.Data?): MavenId? {
+    return when (dependencyData) {
+      is DAArtifact -> MavenId(dependencyData.groupId, dependencyData.artifactId, dependencyData.version)
+      is DAModule -> dependencyData.getUserData(MavenDependencyAnalyzerContributor.MAVEN_ARTIFACT_ID)
+      else -> null
+    }
   }
 
   init {
