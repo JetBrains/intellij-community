80972383731729645b80ad570c236651deb5c199
Anna Kozlova <anna.kozlova@jetbrains.com>
2022-01-18 11:22:47 +0100

    [java] prettify text/fix highlighting suggested for the target placement (IDEA-286811)
    (cherry picked from commit d77132fdf02f5663ef5f1e70de623aef00664225)
    
    IJ-CR-19752
    
    GitOrigin-RevId: 2949763b78c7235c5d212d091788256d9e008afc

--- java/java-tests/testSrc/com/intellij/java/refactoring/InplaceIntroduceVariableTest.java
+++ java/java-tests/testSrc/com/intellij/java/refactoring/InplaceIntroduceVariableTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.java.refactoring;
 
 import com.intellij.codeInsight.template.impl.TemplateManagerImpl;
@@ -99,6 +99,10 @@ public class InplaceIntroduceVariableTest extends AbstractJavaInplaceIntroduceTe
 
   public void testPlaceInsideLambdaBody() {
     doTestReplaceChoice("Runnable: () -> {...}", introducer -> type("expr"));
+  }
+
+  public void testPlaceInsideLambdaBody1() {
+    doTestReplaceChoice("Predicate<String>: s -> {...}", introducer -> type("first"));
   }
 
   public void testPlaceInsideLambdaBodyMultipleOccurrences1() {
