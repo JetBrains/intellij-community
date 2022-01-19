8697bd962cebbb114e11b9820ba50a1fc7490bed
Anna Kozlova <anna.kozlova@jetbrains.com>
2022-01-19 10:07:18 +0100

    [inspections] additional logging
    GitOrigin-RevId: dcfc04ee81356d9a619d057401317afb5bda9c47

--- java/java-analysis-impl/src/com/intellij/codeInspection/reference/RefClassImpl.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/reference/RefClassImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInspection.reference;
 
 import com.intellij.codeInsight.TestFrameworks;
@@ -434,7 +434,7 @@ public final class RefClassImpl extends RefJavaElementImpl implements RefClass {
   public String getExternalName() {
     return ReadAction.compute(() -> {//todo synthetic JSP
       final UClass psiClass = getUastElement();
-      LOG.assertTrue(psiClass != null);
+      LOG.assertTrue(psiClass != null, "No uast class found for psi: " + getPsiElement());
       return PsiFormatUtil.getExternalName(psiClass.getJavaPsi());
     });
   }
