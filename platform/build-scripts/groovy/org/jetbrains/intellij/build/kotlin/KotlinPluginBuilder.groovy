bfdf1237124fe6f82e5304e8f24979a13c45cfeb
Nikita Bobko <nikita.bobko@jetbrains.com>
2022-01-19 16:54:33 +0100

    KotlinPluginBuilder: fail if cannot parse build number version
    It's just more reliable
    
    (cherry picked from commit 8ebfb1593e41288027bb274f4d122b3bc23a25b5)
    
    GitOrigin-RevId: 589f154a62b68f63984e9c66cffa347d2d6fdb16

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/kotlin/KotlinPluginBuilder.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/kotlin/KotlinPluginBuilder.groovy
@@ -282,27 +282,23 @@ class KotlinPluginBuilder {
             String version = "${major}-${kotlinVersion}-${kind}${minor}"
             context.messages.info("version: $version")
             return version
-          } else {
-            // 221-1.5.10-release-IJ916
-            Matcher kotlinPluginIJBuildNumber = Pattern.compile("^(\\d+)-([.\\d]+)-(\\w+)-([A-Z]+)(\\d+)\$").matcher(buildNumber)
-
-            if (kotlinPluginIJBuildNumber.matches()) {
-              String major = kotlinPluginIJBuildNumber.group(1)
-              String kotlinVersion = kotlinPluginIJBuildNumber.group(2)
-              String type = kotlinPluginIJBuildNumber.group(3)
-              String buildKind = kotlinPluginIJBuildNumber.group(4)
-              String minor = kotlinPluginIJBuildNumber.group(5)
-
-              String version = "${major}-${kotlinVersion}-${type}-${kind}${minor}"
-              context.messages.info("Kotlin plugin IJ version: $version")
-              return version
-            }
-
-            // Build number isn't recognized as IJ build number then it means build
-            // number must be plain Kotlin plugin version which we can use directly
-            context.messages.info("buildNumber version: $buildNumber")
-            return buildNumber
           }
+          // Build number isn't recognized as IJ build number then it means build
+          // number must be plain Kotlin plugin version (build configuration in kt-branch)
+          // 221-1.5.10-release-IJ916
+          Matcher kotlinPluginIJBuildNumber = Pattern.compile("^(\\d+)-([.\\d]+)-(\\w+)-([A-Z]+)(\\d+)\$").matcher(buildNumber)
+          if (kotlinPluginIJBuildNumber.matches()) {
+            String major = kotlinPluginIJBuildNumber.group(1)
+            String kotlinVersion = kotlinPluginIJBuildNumber.group(2)
+            String type = kotlinPluginIJBuildNumber.group(3)
+            String minor = kotlinPluginIJBuildNumber.group(5)
+
+            String version = "${major}-${kotlinVersion}-${type}-${kind}${minor}"
+            context.messages.info("Kotlin plugin IJ version: $version")
+            return version
+          }
+
+          throw new IllegalStateException("Can't parse build number: $buildNumber")
         }
       })
 
