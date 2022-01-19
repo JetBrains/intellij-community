0f2f0e52cbb24d86ebc4ea0fb68c56cb203c2148
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-01-19 17:23:52 +0300

    IDEA-286601 vcs: inline diff from annotation - do not show changes by later commits
    GitOrigin-RevId: 1e65671252d1911220f5ae09569e7b01d047b457

--- platform/vcs-impl/src/com/intellij/openapi/vcs/actions/InlineDiffFromAnnotation.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/actions/InlineDiffFromAnnotation.java
@@ -3,9 +3,10 @@ package com.intellij.openapi.vcs.actions;
 
 import com.intellij.diff.comparison.ByWord;
 import com.intellij.diff.comparison.ComparisonPolicy;
+import com.intellij.diff.comparison.iterables.DiffIterable;
 import com.intellij.diff.comparison.iterables.DiffIterableUtil;
-import com.intellij.diff.comparison.iterables.FairDiffIterable;
 import com.intellij.diff.fragments.DiffFragment;
+import com.intellij.diff.tools.fragmented.LineNumberConvertor;
 import com.intellij.diff.util.DiffDrawUtil;
 import com.intellij.diff.util.Range;
 import com.intellij.diff.util.TextDiffType;
@@ -26,6 +27,7 @@ import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressIndicatorProvider;
 import com.intellij.openapi.progress.util.BackgroundTaskUtil;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.annotate.AnnotatedLineModificationDetails;
 import com.intellij.openapi.vcs.annotate.AnnotatedLineModificationDetails.InnerChange;
@@ -34,8 +36,6 @@ import com.intellij.openapi.vcs.annotate.FileAnnotation;
 import com.intellij.openapi.vcs.annotate.TextAnnotationPresentation;
 import com.intellij.util.concurrency.annotations.RequiresEdt;
 import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.containers.PeekableIterator;
-import com.intellij.util.containers.PeekableIteratorWrapper;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -192,9 +192,20 @@ final class InlineDiffFromAnnotation implements EditorMouseListener, EditorMouse
     if (contentAfter.equals(currentContent)) return changes;
 
     List<DiffFragment> delta = ByWord.compare(contentAfter, currentContent, ComparisonPolicy.DEFAULT, DumbProgressIndicator.INSTANCE);
-    FairDiffIterable diffIterable =
-      DiffIterableUtil.fair(DiffIterableUtil.createFragments(delta, contentAfter.length(), currentContent.length()));
-    return new ChangedRangesFixer().execute(changes, diffIterable);
+    DiffIterable iterable = DiffIterableUtil.createFragments(delta, contentAfter.length(), currentContent.length());
+
+    LineNumberConvertor.Builder builder = new LineNumberConvertor.Builder();
+    for (Pair<Range, Boolean> pair : DiffIterableUtil.iterateAll(iterable)) {
+      Range range = pair.first;
+      builder.put(range.start1, range.start2, range.end1 - range.start1, range.end2 - range.start2);
+    }
+    LineNumberConvertor convertor = builder.build();
+
+    return ContainerUtil.map(changes, change -> {
+      int start = convertor.convertApproximate(change.startOffset);
+      int end = convertor.convertApproximate(change.endOffset);
+      return new InnerChange(start, end, change.type);
+    });
   }
 
   @NotNull
@@ -221,108 +232,5 @@ final class InlineDiffFromAnnotation implements EditorMouseListener, EditorMouse
     int areaOffset = gutter.getAnnotationsAreaOffset();
     int areaWidth = gutter.getAnnotationsAreaWidth();
     return x >= areaOffset && x < areaOffset + areaWidth;
-  }
-
-  /**
-   * Algorithm is similar to building ranges for a merge conflict,
-   * see {@link com.intellij.diff.comparison.ComparisonMergeUtil.FairMergeBuilder}
-   * and {@link com.intellij.openapi.vcs.ex.BulkRangeChangeHandler}.
-   * <p>
-   * Given
-   * - Original line content (before commit) // is not available, replaced with {@link InnerChangeType}.
-   * - Modified line content (after commit)
-   * - Current line content (local state)
-   * <p>
-   * innerChanges - difference between Original and Modified
-   * diffIterable - difference between Modified and Current
-   * newInnerChanges - difference between Original and Current
-   * <p>
-   * We iterate both lists in 'Modified' order, collecting interleaving blocks of differences. Each block becomes a single InnerChange.
-   * [changeShift] indicates how 'Modified' offset is mapped to the 'Current' offset at the start of the current block.
-   * [dirtyChangeShift] accumulates offset difference by the current block.
-   */
-  private static final class ChangedRangesFixer {
-    private final List<InnerChange> newInnerChanges = new ArrayList<>();
-
-    private int dirtyStart = -1;
-    private int dirtyEnd = -1;
-    // whether text range in 'Original' is not empty (to differentiate 'insertion' from 'modification')
-    private boolean dirtyNonEmptyOriginal = false;
-
-    private int changeShift = 0;
-    private int dirtyChangeShift = 0;
-
-    @NotNull
-    public List<InnerChange> execute(@NotNull List<InnerChange> innerChanges,
-                                     @NotNull FairDiffIterable diffIterable) {
-      PeekableIterator<InnerChange> it1 = new PeekableIteratorWrapper<>(innerChanges.iterator());
-      PeekableIterator<Range> it2 = new PeekableIteratorWrapper<>(diffIterable.changes());
-
-      while (it1.hasNext() || it2.hasNext()) {
-        if (!it2.hasNext()) {
-          handleChange(it1.next());
-          continue;
-        }
-        if (!it1.hasNext()) {
-          handleDelta(it2.next());
-          continue;
-        }
-
-        InnerChange range1 = it1.peek();
-        Range range2 = it2.peek();
-        if (range1.endOffset <= range2.start1) {
-          handleChange(it1.next());
-        }
-        else {
-          handleDelta(it2.next());
-        }
-      }
-      flush(Integer.MAX_VALUE);
-
-      return newInnerChanges;
-    }
-
-    private void handleChange(@NotNull InnerChange innerChange) {
-      flush(innerChange.startOffset);
-      markDirtyRange(innerChange.startOffset, innerChange.endOffset);
-      dirtyNonEmptyOriginal |= innerChange.type != InnerChangeType.INSERTED;
-    }
-
-    private void handleDelta(@NotNull Range range) {
-      flush(range.start1);
-      dirtyChangeShift += (range.end2 - range.start2) - (range.end1 - range.start1);
-      markDirtyRange(range.start1, range.end1);
-      dirtyNonEmptyOriginal |= range.start1 != range.end1;
-    }
-
-    private void markDirtyRange(int start, int end) {
-      if (dirtyEnd == -1) {
-        dirtyStart = start;
-        dirtyEnd = end;
-      }
-      else {
-        dirtyEnd = Math.max(dirtyEnd, end);
-      }
-    }
-
-    private void flush(int nextOffset) {
-      if (dirtyEnd == -1) return;
-      if (dirtyEnd >= nextOffset) return;
-
-      int blockStart = dirtyStart + changeShift;
-      int blockEnd = dirtyEnd + changeShift + dirtyChangeShift;
-      InnerChangeType blockType = blockStart == blockEnd
-                                  ? InnerChangeType.DELETED
-                                  : dirtyNonEmptyOriginal ? InnerChangeType.MODIFIED
-                                                          : InnerChangeType.INSERTED;
-      newInnerChanges.add(new InnerChange(blockStart, blockEnd, blockType));
-
-      dirtyStart = -1;
-      dirtyEnd = -1;
-      dirtyNonEmptyOriginal = false;
-
-      changeShift += dirtyChangeShift;
-      dirtyChangeShift = 0;
-    }
   }
 }
