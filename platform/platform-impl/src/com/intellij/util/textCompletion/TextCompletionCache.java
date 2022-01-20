fdbcbc705081d2d69f33b39a1ed9bdf91bfd0f60
Vladimir Poliakov <vladimir.poliakov@jetbrains.com>
2022-01-20 16:11:04 +0100

    [gtw] GTW-832 add unit testsy
    GitOrigin-RevId: 857da3499e7d400894e45312cd10a602ca7cd09d

--- platform/platform-impl/src/com/intellij/util/textCompletion/TextCompletionCache.java
+++ platform/platform-impl/src/com/intellij/util/textCompletion/TextCompletionCache.java
@@ -3,13 +3,14 @@ package com.intellij.util.textCompletion;
 
 import com.intellij.codeInsight.completion.CompletionParameters;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 
 public interface TextCompletionCache<T> {
   void setItems(@NotNull Collection<T> items);
 
-  Collection<T> getItems(@NotNull String prefix, @NotNull CompletionParameters parameters);
+  Collection<T> getItems(@NotNull String prefix, @Nullable CompletionParameters parameters);
 
-  void updateCache(@NotNull String prefix, @NotNull CompletionParameters parameters);
+  void updateCache(@NotNull String prefix, @Nullable CompletionParameters parameters);
 }
