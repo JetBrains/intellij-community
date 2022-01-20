ee9f28eb427878567acf7799c3c02d0ea8887642
Andrey Vokin <andrey.vokin@jetbrains.com>
2022-01-20 23:51:54 +0100

    DS-1611 Better default path for attaching existing or creating a new directory
    GitOrigin-RevId: 61ee615d56e0d9c59d1828d34242ec779fdb994b

--- platform/platform-impl/src/com/intellij/platform/AttachProjectAction.kt
+++ platform/platform-impl/src/com/intellij/platform/AttachProjectAction.kt
@@ -15,9 +15,12 @@ import com.intellij.openapi.project.isProjectDirectoryExistsUsingIo
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.projectImport.ProjectAttachProcessor
+import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
 
@@ -45,11 +48,15 @@ open class AttachProjectAction : AnAction(ActionsBundle.message("action.AttachPr
 
   fun chooseAndAttachToProject(project: Project) {
     val descriptor = OpenProjectFileChooserDescriptor(true)
-    val virtualFile = project.getUserData(TO_SELECT_KEY)?.let {
+    var preselectedDirectory = project.getUserData(TO_SELECT_KEY)?.let {
       project.putUserData(TO_SELECT_KEY, null) // reset the value
       LocalFileSystem.getInstance().findFileByNioFile(it)
     }
-    FileChooser.chooseFiles(descriptor, project, virtualFile) {
+    if (preselectedDirectory == null && StringUtil.isNotEmpty(GeneralSettings.getInstance().defaultProjectDirectory)) {
+      preselectedDirectory = VfsUtil.findFileByIoFile(File(GeneralSettings.getInstance().defaultProjectDirectory), true)
+    }
+
+    FileChooser.chooseFiles(descriptor, project, preselectedDirectory) {
       val directory = it[0]
       if (validateDirectory(project, directory)) {
         attachProject(directory, project)
