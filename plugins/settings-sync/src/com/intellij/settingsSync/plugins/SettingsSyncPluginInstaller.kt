58c1911ef06a356d1c35349117eff45112fb56d0
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2022-01-21 20:54:06 +0300

    [settings-sync] Show notification when the IDE must be restarted to activate plugins
    GitOrigin-RevId: a68d407c16c0472abcd91e3814478e42943d4945

--- plugins/settings-sync/src/com/intellij/settingsSync/plugins/SettingsSyncPluginInstaller.kt
+++ plugins/settings-sync/src/com/intellij/settingsSync/plugins/SettingsSyncPluginInstaller.kt
@@ -1,12 +1,20 @@
 package com.intellij.settingsSync.plugins
 
 import com.intellij.ide.plugins.marketplace.MarketplaceRequests
+import com.intellij.notification.NotificationAction
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ApplicationNamesInfo
+import com.intellij.openapi.application.ex.ApplicationEx
 import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.updateSettings.impl.PluginDownloader
+import com.intellij.settingsSync.NOTIFICATION_GROUP
 import com.intellij.settingsSync.SettingsSyncBundle
+import com.intellij.util.Consumer
 
 internal class SettingsSyncPluginInstaller {
   private val pluginsIds = ArrayList<PluginId>()
@@ -43,11 +51,29 @@ internal class SettingsSyncPluginInstaller {
     }
 
     override fun onFinished() {
+      var isRestartNeeded = false
       collectedInstallers.forEach {
-        it.installDynamically(null)
-        // TODO<rv> Notify a user if the IDE needs a restart(?)
+        if (!it.installDynamically(null)) {
+          isRestartNeeded = true
+        }
       }
+      if (isRestartNeeded) notifyRestartNeeded()
     }
+
+    private fun notifyRestartNeeded() {
+      val notification = NotificationGroupManager.getInstance().getNotificationGroup(NOTIFICATION_GROUP)
+        .createNotification(SettingsSyncBundle.message("plugins.sync.restart.notification.title"),
+                            SettingsSyncBundle.message("plugins.sync.restart.notification.message"),
+                            NotificationType.INFORMATION)
+      notification.addAction(NotificationAction.create(
+        SettingsSyncBundle.message("plugins.sync.restart.notification.action", ApplicationNamesInfo.getInstance().fullProductName),
+        Consumer {
+          val app = ApplicationManager.getApplication() as ApplicationEx
+          app.restart(true)
+        }))
+      notification.notify(null)
+    }
+
   }
 
 }
