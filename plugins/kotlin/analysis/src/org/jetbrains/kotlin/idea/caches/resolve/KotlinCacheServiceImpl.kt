222a4d04248e5193642c21646c927dc1f8321f82
Nikita Nazarov <nikita.nazarov@jetbrains.com>
2022-01-21 13:35:15 +0300

    [kotlin] allow evaluator to use separate resolution facade in MPP projects
    ^KTIJ-20775 Fixed
    
    (cherry picked from commit 6df4fead455b6a7952c1726babfc088a8272d2f0)
    
    IJ-MR-19905
    
    GitOrigin-RevId: 948cd38acb0920b7055554322989bc79def82017

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/caches/resolve/KotlinCacheServiceImpl.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/caches/resolve/KotlinCacheServiceImpl.kt
@@ -107,7 +107,7 @@ class KotlinCacheServiceImpl(val project: Project) : KotlinCacheService {
         val files = getFilesForElements(elements)
         val moduleInfo = files.first().getModuleInfo()
 
-        if (!project.useCompositeAnalysis || moduleInfo.platform == platform) {
+        if (!project.useCompositeAnalysis) {
             // No need to force platform, just get facade as usual
             return getFacadeToAnalyzeFiles(files, platform)
         }
