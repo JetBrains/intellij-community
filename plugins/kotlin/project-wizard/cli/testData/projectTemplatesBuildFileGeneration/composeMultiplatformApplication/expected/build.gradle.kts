f0030ca94b238e90644d6ad9e36cf5abb4511c93
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-01-21 01:19:39 +0100

    [kotlin-testdata] ProjectTemplateBuildFileGenerationTestGenerated
    Versions were updated in 909a84ac commit, but testdata wasn't updated.
    In Compose tests we should keepKotlinVersion because compose wizard uses
    fixed kotlin version: KOTLIN_VERSION_FOR_COMPOSE
    
    (cherry picked from commit 816a8c2a075e034ee1b26e4e7d8fe08a46bcd33d)
    
    GitOrigin-RevId: 463e0d5585c4452f22b65db22511c7f8447398ba

--- plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/build.gradle.kts
+++ plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.android.tools.build:gradle:4.1.2")
     }
 }
