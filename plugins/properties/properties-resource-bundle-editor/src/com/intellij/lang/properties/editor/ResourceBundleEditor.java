6dae673952a5049f8c0bb756696ab0cd0a86013b
Nikita Eshkeev <nikita.eshkeev@jetbrains.com>
2022-01-21 11:05:15 +0300

    Revert "[properties][resource bundle editor] IDEA-276421 Slow operations are prohibited in Resource Bundle"
    This reverts commit 3b9aa0d419170cc7a2c21c87458679b4d73b3664.
    
    GitOrigin-RevId: d7aa2ae840b4a139f4a90e38fbad60efe4aafd59

--- plugins/properties/properties-resource-bundle-editor/src/com/intellij/lang/properties/editor/ResourceBundleEditor.java
+++ plugins/properties/properties-resource-bundle-editor/src/com/intellij/lang/properties/editor/ResourceBundleEditor.java
@@ -25,6 +25,7 @@ import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.application.ex.ApplicationUtil;
 import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.command.impl.UndoManagerImpl;
@@ -40,6 +41,7 @@ import com.intellij.openapi.editor.ex.FocusChangeListener;
 import com.intellij.openapi.editor.ex.util.LexerEditorHighlighter;
 import com.intellij.openapi.editor.impl.ContextMenuPopupHandler;
 import com.intellij.openapi.fileEditor.*;
+import com.intellij.openapi.progress.EmptyProgressIndicator;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
@@ -85,6 +87,7 @@ import java.awt.event.KeyEvent;
 import java.beans.PropertyChangeListener;
 import java.util.List;
 import java.util.*;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 
 public final class ResourceBundleEditor extends UserDataHolderBase implements DocumentsEditor {
@@ -679,13 +682,6 @@ public final class ResourceBundleEditor extends UserDataHolderBase implements Do
   }
 
   private Object getData(@NotNull String dataId) {
-    if (PlatformCoreDataKeys.SLOW_DATA_PROVIDERS.is(dataId)) {
-      return List.of((DataProvider)this::getSlowData);
-    }
-    return null;
-
-  }
-  private Object getSlowData(@NotNull String dataId) {
     if (SelectInContext.DATA_KEY.is(dataId)) {
       VirtualFile file = getSelectedPropertiesFile();
       return file == null ? null : new FileSelectInContext(myProject, file);
@@ -703,12 +699,10 @@ public final class ResourceBundleEditor extends UserDataHolderBase implements Do
       final PropertiesFile file = PropertiesImplUtil.getPropertiesFile(virtualFile, myProject);
       LOG.assertTrue(file != null);
 
-      final List<IProperty> properties = file.findPropertiesByKey(name);
+      final List<IProperty> properties = getPropertiesByName(file, name);
+      if (properties == null) return null;
 
-      if (properties.isEmpty()) {
-        return new Navigatable[]{ file.getContainingFile() };
-      }
-
+      if (properties.isEmpty()) return new Navigatable[]{ file.getContainingFile() };
       return properties
         .stream()
         .map(IProperty::getPsiElement)
@@ -718,6 +712,18 @@ public final class ResourceBundleEditor extends UserDataHolderBase implements Do
         .toArray(Navigatable[]::new);
     }
     return null;
+  }
+
+  @Contract(pure = true)
+  private static @Nullable List<IProperty> getPropertiesByName(@NotNull PropertiesFile file, @NotNull String name) {
+    final Callable<List<IProperty>> findProperties = () -> file.findPropertiesByKey(name);
+    try {
+      return ApplicationUtil.runWithCheckCanceled(ReadAction.nonBlocking(findProperties)::executeSynchronously,
+                                                  new EmptyProgressIndicator());
+    }
+    catch (Exception e) {
+      return null;
+    }
   }
 
   private VirtualFile getSelectedPropertiesFile() {
