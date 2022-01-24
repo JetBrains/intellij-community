a829a0685a6e650d4e11f7765f82612dab415564
Anton Bragin <Anton.Bragin@jetbrains.com>
2022-01-25 02:02:32 +0300

    DS-3080 Jupyter file type icon added to core
    Dependency on intellij.python.psi removed.
    
    IJ-CR-20036
    
    GitOrigin-RevId: 33195b4d981fe8a3113c1b12a2b00499570310bc

---
+++ jupyter/src/icons/JupyterCoreIcons.java
@@ -0,0 +1,17 @@
+package icons;
+
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class JupyterCoreIcons {
+  private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
+    return IconManager.getInstance().loadRasterizedIcon(path, JupyterCoreIcons.class.getClassLoader(), cacheKey, flags);
+  }
+  /** 16x16 */ public static final @NotNull Icon JupyterNotebook = load("icons/org.jetbrains.plugins.notebooks.jupyter/jupyterNotebook.svg", 1401972749, 0);
+}
