3ae378053854478f93fd286c4ec6f51270fb73b7
andrey.matveev <amatveev@jetbrains.com>
2022-01-24 12:45:52 +0700

    PY-26881 Fix getLocationString messages
    GitOrigin-RevId: 42a5a38b321e8611b9eb466a8cec675cb430cbd1

--- python/python-psi-impl/src/com/jetbrains/python/psi/impl/PyElementPresentation.java
+++ python/python-psi-impl/src/com/jetbrains/python/psi/impl/PyElementPresentation.java
@@ -67,18 +67,18 @@ public class PyElementPresentation implements ColoredItemPresentation {
     PyClass containingClass = myElement instanceof PyPossibleClassMember ? ((PyPossibleClassMember)myElement).getContainingClass() : null;
     if (containingClass != null) {
       if (isPyiFile) {
-        return PyPsiBundle.message("element.presentation.location.string.in.class", containingClass.getName(), packageForFile);
+        return PyPsiBundle.message("element.presentation.location.string.in.class.stub", containingClass.getName(), packageForFile);
       }
       else {
-        return PyPsiBundle.message("element.presentation.location.string.in.class.stub", containingClass.getName(), packageForFile);
+        return PyPsiBundle.message("element.presentation.location.string.in.class", containingClass.getName(), packageForFile);
       }
     }
 
     if (isPyiFile) {
-      return PyPsiBundle.message("element.presentation.location.string.module", packageForFile);
+      return PyPsiBundle.message("element.presentation.location.string.module.stub", packageForFile);
     }
     else {
-      return PyPsiBundle.message("element.presentation.location.string.module.stub", packageForFile);
+      return PyPsiBundle.message("element.presentation.location.string.module", packageForFile);
     }
   }
 
