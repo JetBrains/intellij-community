b5d489a614e6d6866d118bf5bd3a47859d830867
Nikolaj Schumacher <nikolaj.schumacher@jetbrains.com>
2022-01-24 10:36:20 +0100

    IDEA-217465 Handle keymap switches and dynamic extension changes
    GitOrigin-RevId: b9f47e2b47f12925bd7a179ed5a90b8b0d6a4e88

--- platform/lang-impl/src/com/intellij/codeInsight/intention/impl/IntentionShortcutManager.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/intention/impl/IntentionShortcutManager.kt
@@ -2,24 +2,34 @@
 package com.intellij.codeInsight.intention.impl
 
 import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.codeInsight.intention.IntentionActionBean
 import com.intellij.codeInsight.intention.IntentionManager
 import com.intellij.codeInsight.intention.IntentionShortcuts.WRAPPER_PREFIX
+import com.intellij.codeInsight.intention.impl.config.IntentionActionWrapper
+import com.intellij.codeInsight.intention.impl.config.IntentionManagerImpl
 import com.intellij.ide.ApplicationInitializedListener
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.ShortcutSet
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
+import com.intellij.openapi.extensions.ExtensionPointListener
+import com.intellij.openapi.extensions.PluginDescriptor
+import com.intellij.openapi.keymap.Keymap
 import com.intellij.openapi.keymap.KeymapManager
+import com.intellij.openapi.keymap.KeymapManagerListener
 import com.intellij.openapi.keymap.impl.ActionShortcutRestrictions
 import com.intellij.openapi.keymap.impl.ui.KeymapPanel
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.WindowManager
 
 /** Wraps intentions in [IntentionActionAsAction] to make it possible to assign shortcuts to them */
-@Service
-class IntentionShortcutManager : ApplicationInitializedListener {
+@Service(Service.Level.APP)
+class IntentionShortcutManager : Disposable {
+
+  private var registeredKeymap: Keymap? = null
 
   /** Check if intention has a shortcut assigned */
   fun hasShortcut(intention: IntentionAction): Boolean =
@@ -74,22 +84,62 @@ class IntentionShortcutManager : ApplicationInitializedListener {
   internal fun findIntention(wrappedActionId: String): IntentionAction? =
     IntentionManager.getInstance().availableIntentions.firstOrNull { it.wrappedActionId == wrappedActionId }
 
+   /** Register all intentions with shortcuts and keep them up to date */
+  private fun registerIntentionsInActiveKeymap() {
+    registerIntentionsInKeymap(KeymapManager.getInstance().activeKeymap)
 
-  override fun componentsInitialized() {
-    // Register all wrappers referenced in the keymap
-    // FIXME: Needs to be re-run when the keymap changes
-    KeymapManager.getInstance().activeKeymap.actionIds.asSequence()
-      .filter { it.startsWith(WRAPPER_PREFIX) }
-      .map { it.removePrefix(WRAPPER_PREFIX) }
-      .mapNotNull {
-        try {
-          Class.forName(it).getConstructor().newInstance() as? IntentionAction
-        }
-        catch (e: Exception) {
-          null
-        }
+    val busConnection = ApplicationManager.getApplication().messageBus.connect(this)
+    busConnection.subscribe(KeymapManagerListener.TOPIC, object : KeymapManagerListener {
+      override fun activeKeymapChanged(keymap: Keymap?) {
+        unregisterIntentionsInKeymap()
+        registerIntentionsInKeymap(keymap)
       }
-      .forEach { register(it) }
+    })
+
+    IntentionManagerImpl.EP_INTENTION_ACTIONS.addExtensionPointListener(object : ExtensionPointListener<IntentionActionBean> {
+      override fun extensionAdded(extension: IntentionActionBean, pluginDescriptor: PluginDescriptor) {
+        registerIntentionsInKeymap(registeredKeymap)
+      }
+
+      override fun extensionRemoved(extension: IntentionActionBean, pluginDescriptor: PluginDescriptor) {
+        unregisterIntentionsInExtension(extension)
+      }
+    }, this)
+  }
+
+  private fun registerIntentionsInKeymap(keymap: Keymap?) {
+    keymap?.wrappedIntentionActions()?.forEach { register(it) }
+    registeredKeymap = keymap
+  }
+
+  private fun unregisterIntentionsInKeymap() {
+    registeredKeymap?.wrappedIntentionActions()?.forEach { unregister(it) }
+    registeredKeymap = null
+  }
+
+  private fun unregisterIntentionsInExtension(extension: IntentionActionBean) {
+    registeredKeymap?.wrappedIntentionActions()?.forEach { intention ->
+      if (intention is IntentionActionWrapper && intention.implementationClassName == extension.className) {
+        unregister(intention)
+      }
+    }
+  }
+
+  /** Collect all intentions with assigned shortcuts in a keymap */
+  private fun Keymap.wrappedIntentionActions(): Sequence<IntentionAction> {
+    val intentionsById = IntentionManager.getInstance().intentionActions.associateBy { it.wrappedActionId }
+    return actionIds
+      .asSequence()
+      .filter { it.startsWith(WRAPPER_PREFIX) }
+      .mapNotNull { intentionsById[it] }
+  }
+
+  override fun dispose() {}
+
+  class InitListener : ApplicationInitializedListener {
+    override fun componentsInitialized() {
+      getInstance().registerIntentionsInActiveKeymap()
+    }
   }
 
   companion object {
