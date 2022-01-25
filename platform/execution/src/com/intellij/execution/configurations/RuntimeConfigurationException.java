a73feaa8c7c7bcd7205708dd7695a7d3092a068f
Anna Kozlova <anna.kozlova@jetbrains.com>
2022-01-25 11:01:06 +0100

    EA-441973 IAE: ValidationInfo.$$$reportNull$$$0
    GitOrigin-RevId: 008be0bdf2f73adfaaf73ed34dff074fcbc0ffb4

--- platform/execution/src/com/intellij/execution/configurations/RuntimeConfigurationException.java
+++ platform/execution/src/com/intellij/execution/configurations/RuntimeConfigurationException.java
@@ -1,22 +1,9 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.configurations;
 
 import com.intellij.execution.ExecutionBundle;
 import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.ui.ValidationInfo;
 import com.intellij.openapi.util.NlsContexts.DialogMessage;
 import com.intellij.util.ThrowableRunnable;
@@ -41,6 +28,9 @@ public class RuntimeConfigurationException extends ConfigurationException {
   public static <T extends Throwable> ValidationInfo validate(JComponent component, ThrowableRunnable<T> runnable) {
     try {
       runnable.run();
+      return new ValidationInfo("", component);
+    }
+    catch (ProcessCanceledException e) {
       return new ValidationInfo("", component);
     }
     catch (Throwable t) {
