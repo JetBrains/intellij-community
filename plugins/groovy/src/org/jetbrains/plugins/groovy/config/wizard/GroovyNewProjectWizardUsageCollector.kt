cb664de5204be58af90068b8fdb25345dd4fbacb
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-01-25 19:49:20 +0300

    [groovy] IDEA-280962: Fix FUS for Groovy NPW
    GitOrigin-RevId: 42d930cc7c9ef64185f1569fbdec10cbfa94af59

--- plugins/groovy/src/org/jetbrains/plugins/groovy/config/wizard/GroovyNewProjectWizardUsageCollector.kt
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/config/wizard/GroovyNewProjectWizardUsageCollector.kt
@@ -14,7 +14,7 @@ class GroovyNewProjectWizardUsageCollector : CounterUsagesCollector() {
   internal enum class DistributionType { MAVEN, LOCAL }
 
   companion object {
-    private val GROUP = EventLogGroup("new.project.wizard.interactions", 3)
+    private val GROUP = EventLogGroup("new.project.wizard.interactions.groovy", 1)
 
     private val SESSION_ID_FIELD = EventFields.Int("wizard_session_id")
     private val VERSION_FIELD = EventFields.StringValidatedByInlineRegexp("version", GroovyConfigUtils.GROOVY_VERSION_REGEX.pattern())
