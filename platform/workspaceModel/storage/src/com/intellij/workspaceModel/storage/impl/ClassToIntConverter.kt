d715484b500611add4efe214049cdf61b08df54c
Alex Plate <aleksei.plate@jetbrains.com>
2022-01-25 18:11:13 +0300

    [Workspace Model] IDEA-286873 Updates for ClassToIntConverter
    GitOrigin-RevId: 1145cc9144a34b72cf87762d23340544aa9a6d6a

--- platform/workspaceModel/storage/src/com/intellij/workspaceModel/storage/impl/ClassToIntConverter.kt
+++ platform/workspaceModel/storage/src/com/intellij/workspaceModel/storage/impl/ClassToIntConverter.kt
@@ -5,37 +5,32 @@ import com.intellij.workspaceModel.storage.WorkspaceEntity
 import it.unimi.dsi.fastutil.objects.Object2IntMap
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap
 import org.jetbrains.annotations.TestOnly
-import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.atomic.AtomicReference
 
 /**
  * This class fits the cases where there is a relatively small amount of classes (<50).
  * The disadvantages are:
  * - Two collections copies
- * - The generated id's may contain gaps (some ids may miss, e.g. 1, 2, 4, 5)
- * The problem with gaps affects workspace model because the classes are stored in the list according to
- *   their ids, so with gaps we'll have some memory wasting in the storage.
- * The current plan is to generate and assign ids to classes during the code generation.
+ *
+ * N.B. One of the approaches is to generate and assign ids to classes during the code generation.
  */
 internal class ClassToIntConverter {
-  private val idGenerator = AtomicInteger()
-  private val map: AtomicReference<Entry> = AtomicReference(Entry(newMap(), emptyList()))
+  private val map: AtomicReference<Entry> = AtomicReference(Entry(newMap(), emptyArray()))
 
   private class Entry(
     val classToInt: Object2IntMap<Class<*>>,
-    val intToClass: List<Class<*>?>,
+    val intToClass: Array<Class<*>?>,
   )
 
   fun getInt(clazz: Class<*>): Int {
-    var classId = -1
     while (true) {
       val entry = map.get()
       val result = entry.classToInt.getInt(clazz)
       if (result != -1) return result
-      if (classId == -1) classId = idGenerator.getAndIncrement()
+      val classId = entry.classToInt.size
       val newEntry = Entry(
         newMap(entry.classToInt).also { it.put(clazz, classId) },
-        ArrayList(entry.intToClass).also { it.extendAndPut(classId, clazz) }
+        entry.intToClass.copyExtendAndPut(classId, clazz)
       )
       if (map.compareAndSet(entry, newEntry)) return classId
     }
@@ -48,16 +43,14 @@ internal class ClassToIntConverter {
   fun fromMap(map: Map<Class<*>, Int>) {
     val entry = Entry(
       Object2IntOpenHashMap(map),
-      ArrayList<Class<*>?>().also { list -> map.forEach { (clazz, index) -> list.extendAndPut(index, clazz) } }
+      Array<Class<*>?>(map.values.maxOrNull() ?: 0) { null }.also { map.forEach { (clazz, index) -> it[index] = clazz } }
     )
     this.map.set(entry)
-    idGenerator.set((map.map { it.value }.maxOrNull() ?: -1) + 1)
   }
 
   @TestOnly
   fun clear() {
-    map.set(Entry(newMap(), emptyList()))
-    idGenerator.set(0)
+    map.set(Entry(newMap(), emptyArray()))
   }
 
   private fun newMap(oldMap: Object2IntMap<Class<*>>? = null): Object2IntOpenHashMap<Class<*>> {
@@ -66,11 +59,11 @@ internal class ClassToIntConverter {
     return newMap
   }
 
-  private fun <T> MutableList<T?>.extendAndPut(id: Int, data: T) {
-    while (this.size < id + 1) {
-      this.add(null)
-    }
-    this[id] = data
+  private inline fun <reified T> Array<T?>.copyExtendAndPut(id: Int, data: T): Array<T?> {
+    val thisSize = this.size
+    val res = Array(id + 1) { if (it < thisSize) this[it] else null }
+    res[id] = data
+    return res
   }
 
   companion object {
