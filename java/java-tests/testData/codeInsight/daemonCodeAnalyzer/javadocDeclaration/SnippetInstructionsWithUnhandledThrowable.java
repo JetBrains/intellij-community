66dc4f450956496323a94f6dd43346036c12b903
Alexandr Suhinin <Alexandr.Suhinin@jetbrains.com>
2022-01-26 12:06:31 +0200

    [javadoc inspection]: test "JavadocDeclaration" inspection (copy from `JavadocDeclarationHighlightingTest`)
    GitOrigin-RevId: 19cf2b25316068a0f5036e9f422c9e8147084ce4

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/javadocDeclaration/SnippetInstructionsWithUnhandledThrowable.java
@@ -0,0 +1,7 @@
+/** {<warning descr="'@snippet' tag is not available at this language level">@snippet</warning> :
+ * FileInputStream is = new FileInputStream("hello.world");
+ * int r = is.read();
+ * }
+ */
+class A {
+}
