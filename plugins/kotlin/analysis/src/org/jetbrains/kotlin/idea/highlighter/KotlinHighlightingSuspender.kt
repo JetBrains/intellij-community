49cad7d3e0b3667c721d33335c51e3f67e0e6f9a
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2022-01-27 16:43:21 +0100

    [kotlin] Allow close popup "Syntax highlighting has been temporarily turned off"
    #KTIJ-20768
    
    (cherry picked from commit badf944f4dbbe50864d777fd94352845e74964ed)
    
    GitOrigin-RevId: 495b4d61f53b2c12bf404ae2b438482558fdb41f

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/highlighter/KotlinHighlightingSuspender.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/highlighter/KotlinHighlightingSuspender.kt
@@ -6,6 +6,8 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.EditorNotifications
+import com.intellij.util.Alarm
+import org.jetbrains.kotlin.idea.core.KotlinPluginDisposable
 import java.util.concurrent.TimeUnit
 
 /**
@@ -19,6 +21,8 @@ class KotlinHighlightingSuspender(private val project: Project) {
 
     private val lastThrownExceptionTimestampPerFile = mutableMapOf<VirtualFile, Long>()
     private val suspendTimeoutMs = TimeUnit.SECONDS.toMillis(timeoutSeconds.toLong())
+
+    private val updateQueue = Alarm(Alarm.ThreadToUse.SWING_THREAD, KotlinPluginDisposable.getInstance(project))
 
     /**
      * @return true, when file is suspended for the 1st time (within a timeout window)
@@ -40,9 +44,23 @@ class KotlinHighlightingSuspender(private val project: Project) {
             lastThrownExceptionTimestamp
         }
 
+        scheduleUpdate(file)
         updateNotifications(file)
 
         return timestamp - lastThrownExceptionTimestamp > suspendTimeoutMs
+    }
+
+    private fun scheduleUpdate(file: VirtualFile) {
+        updateQueue.apply { addRequest(Runnable { updateNotifications(file) }, suspendTimeoutMs + 1) }
+    }
+
+    fun unsuspend(file: VirtualFile) {
+        synchronized(lastThrownExceptionTimestampPerFile) {
+            lastThrownExceptionTimestampPerFile.remove(file)
+        }
+        cleanup()
+
+        updateNotifications(file)
     }
 
     fun isSuspended(file: VirtualFile): Boolean {
@@ -60,7 +78,10 @@ class KotlinHighlightingSuspender(private val project: Project) {
         val timestamp = System.currentTimeMillis()
 
         val filesToUpdate = mutableListOf<VirtualFile>()
+        val filesToUpdateLater = mutableListOf<VirtualFile>()
         synchronized(lastThrownExceptionTimestampPerFile) {
+            if (lastThrownExceptionTimestampPerFile.isEmpty()) return
+
             val it = lastThrownExceptionTimestampPerFile.entries.iterator()
             while (it.hasNext()) {
                 val next = it.next()
@@ -69,9 +90,12 @@ class KotlinHighlightingSuspender(private val project: Project) {
                     it.remove()
                 }
             }
+            filesToUpdateLater.addAll(lastThrownExceptionTimestampPerFile.keys)
         }
 
-        filesToUpdate.forEach { updateNotifications(it) }
+        updateQueue.cancelAllRequests()
+        filesToUpdate.forEach(::updateNotifications)
+        filesToUpdateLater.forEach(::scheduleUpdate)
     }
 
     private fun updateNotifications(file: VirtualFile) {
