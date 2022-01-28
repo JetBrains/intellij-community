da946de5b01b46517b336a98d57db1e9f7911b5b
Bas Leijdekkers <basleijdekkers@gmail.com>
2022-01-28 13:10:45 +0100

    Make `New | package-info.java` dumb aware (IDEA-287596)
    GitOrigin-RevId: e2faf267138c86b4cea9c987ea50fe3815de534a

--- java/java-impl/src/com/intellij/ide/actions/CreatePackageInfoAction.java
+++ java/java-impl/src/com/intellij/ide/actions/CreatePackageInfoAction.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.actions;
 
 import com.intellij.CommonBundle;
@@ -28,6 +14,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.roots.ProjectRootManager;
@@ -47,7 +34,7 @@ import static com.intellij.ide.fileTemplates.JavaTemplateUtil.INTERNAL_PACKAGE_I
 /**
  * @author Bas Leijdekkers
  */
-public class CreatePackageInfoAction extends CreateFromTemplateActionBase {
+public class CreatePackageInfoAction extends CreateFromTemplateActionBase implements DumbAware {
   public CreatePackageInfoAction() {
     super(JavaBundle.messagePointer("action.create.new.package-info.title"), JavaBundle.messagePointer("action.create.new.package-info.description"), AllIcons.FileTypes.Java);
   }
