b1d0783ab7afe1085a33a843bcfd6b5901cdeebe
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-01-31 17:27:24 +0100

    FIR IDE: adapt to reference resolve changes related to library resolve
    GitOrigin-RevId: 1316f8f61e2099dc3eeeca8da095dc880b06bea0

--- plugins/kotlin/fir-low-level-api-ide-impl/test/org/jetbrains/kotlin/idea/fir/low/level/api/firTestUtils.kt
+++ plugins/kotlin/fir-low-level-api-ide-impl/test/org/jetbrains/kotlin/idea/fir/low/level/api/firTestUtils.kt
@@ -2,16 +2,16 @@
 
 package org.jetbrains.kotlin.idea.fir.low.level.api
 
-import org.jetbrains.kotlin.analysis.low.level.api.fir.api.FirModuleResolveState
+import org.jetbrains.kotlin.analysis.low.level.api.fir.api.LLFirModuleResolveState
 import org.jetbrains.kotlin.analysis.low.level.api.fir.createResolveStateForNoCaching
-import org.jetbrains.kotlin.analysis.low.level.api.fir.sessions.FirIdeSession
+import org.jetbrains.kotlin.analysis.low.level.api.fir.sessions.LLFirSession
 import org.jetbrains.kotlin.analysis.project.structure.getKtModule
 import org.jetbrains.kotlin.psi.KtElement
 
 internal inline fun <R> resolveWithClearCaches(
     context: KtElement,
-    noinline configureSession: FirIdeSession.() -> Unit = {},
-    action: (FirModuleResolveState) -> R,
+    noinline configureSession: LLFirSession.() -> Unit = {},
+    action: (LLFirModuleResolveState) -> R,
 ): R {
     val project = context.project
     val resolveState = createResolveStateForNoCaching(context.getKtModule(project), project, configureSession)
