d8a430394dc7b6236ad3c4dbcabf067e3b2fd63c
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-01-31 12:52:06 +0300

    diff: remove unused Registry options
    Follow-up: 0703286d8e1816970ff83d056817b961254fb45c
    
    GitOrigin-RevId: 9840088f992b7a48e036363ec73c1ba44c0e4304

--- platform/util/diff/src/com/intellij/util/diff/DiffConfig.java
+++ platform/util/diff/src/com/intellij/util/diff/DiffConfig.java
@@ -8,8 +8,9 @@ import org.jetbrains.annotations.NonNls;
 @ApiStatus.Internal
 public class DiffConfig {
   private static final @NonNls String DIFF_DELTA_THRESHOLD_SIZE_KEY = "idea.diff.delta.threshold.size";
+  private static final @NonNls String DIFF_DELTA_PATIENCE_ALG_KEY = "idea.diff.force.patience.alg";
 
-  public static final boolean USE_PATIENCE_ALG = false;
+  public static final boolean USE_PATIENCE_ALG = SystemProperties.getBooleanProperty(DIFF_DELTA_PATIENCE_ALG_KEY, false);
   public static final boolean USE_GREEDY_MERGE_MAGIC_RESOLVE = false;
   public static final int DELTA_THRESHOLD_SIZE = SystemProperties.getIntProperty(DIFF_DELTA_THRESHOLD_SIZE_KEY, 20000);
   public static final int MAX_BAD_LINES = 3; // Do not try to compare lines by-word after that many prior failures.
