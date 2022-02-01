2cc9ed777f8cb790580841a88e562efb37d2a418
Yaroslav.Chernyshev <Yaroslav.Chernyshev@jetbrains.com>
2022-02-01 15:45:24 +0300

    [minor] Deprecate KotlinSourceSetInfo#kotlinModule property
    GitOrigin-RevId: 78d1733f9e0b8596563cacd739117f4074086991

--- plugins/kotlin/gradle/gradle-idea/src/org/jetbrains/kotlin/idea/configuration/KotlinMPPDataNodes.kt
+++ plugins/kotlin/gradle/gradle-idea/src/org/jetbrains/kotlin/idea/configuration/KotlinMPPDataNodes.kt
@@ -30,8 +30,13 @@ var DataNode<out ModuleData>.kotlinImportingDiagnosticsContainer: KotlinImportin
 val DataNode<out ModuleData>.kotlinAndroidSourceSets: List<KotlinSourceSetInfo>?
         get() = ExternalSystemApiUtil.getChildren(this, KotlinAndroidSourceSetData.KEY).firstOrNull()?.data?.sourceSetInfos
 
-class KotlinSourceSetInfo @PropertyMapping("kotlinModule") constructor(val kotlinComponent: KotlinComponent) : Serializable {
+class KotlinSourceSetInfo @PropertyMapping("kotlinComponent") constructor(val kotlinComponent: KotlinComponent) : Serializable {
     @Suppress("DEPRECATION_ERROR")
+    @Deprecated(
+        level = DeprecationLevel.WARNING,
+        message = "Use KotlinSourceSetInfo#kotlinComponent instead",
+        replaceWith = ReplaceWith("kotlinComponent")
+    )
     val kotlinModule: KotlinModule
         get() = kotlinComponent
     var moduleId: String? = null
