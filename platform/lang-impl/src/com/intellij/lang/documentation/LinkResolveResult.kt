8b513f6cb858923eab5140d63a5d3da538158405
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2022-02-02 18:09:17 +0100

    IDEA-277609 [documentation] add read access assertion to `LinkResolveResult.resolvedTarget`
    GitOrigin-RevId: 4b26a9c991fff6ebb36c3ba78f2a12ad8afb79f3

--- platform/lang-impl/src/com/intellij/lang/documentation/LinkResolveResult.kt
+++ platform/lang-impl/src/com/intellij/lang/documentation/LinkResolveResult.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.lang.documentation
 
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.util.concurrency.annotations.RequiresReadLock
 import org.jetbrains.annotations.ApiStatus.Experimental
 
 @Experimental
@@ -11,8 +13,10 @@ sealed interface LinkResolveResult {
     /**
      * @return a result, which makes the browser load the documentation for the [target]
      */
+    @RequiresReadLock(generateAssertion = false)
     @JvmStatic
     fun resolvedTarget(target: DocumentationTarget): LinkResolveResult {
+      ApplicationManager.getApplication().assertReadAccessAllowed()
       return ResolvedTarget(target)
     }
   }
