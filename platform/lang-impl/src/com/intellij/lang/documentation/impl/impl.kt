32ed27910dac3bd91ba472230cd9d070c7bd55a9
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2022-02-03 12:50:39 +0100

    [documentation] add and use `resolveLink` which maps resolved target while holding RA
    GitOrigin-RevId: bcb7ef8910a0310292cad26fb4335e881862b409

--- platform/lang-impl/src/com/intellij/lang/documentation/impl/impl.kt
+++ platform/lang-impl/src/com/intellij/lang/documentation/impl/impl.kt
@@ -37,42 +37,87 @@ fun CoroutineScope.computeDocumentationAsync(targetPointer: Pointer<out Document
 @ApiStatus.Internal
 suspend fun handleLink(targetPointer: Pointer<out DocumentationTarget>, url: String): InternalLinkResult {
   return withContext(Dispatchers.Default) {
-    readAction {
-      doHandleLink(targetPointer, url)
-    }
+    tryResolveLink(targetPointer, url)
+    ?: tryContentUpdater(targetPointer, url)
+    ?: InternalLinkResult.CannotResolve
   }
 }
 
-private fun doHandleLink(targetPointer: Pointer<out DocumentationTarget>, url: String): InternalLinkResult {
-  val target = targetPointer.dereference()
-               ?: return InternalLinkResult.InvalidTarget
-  return tryResolveLink(target, url)
-         ?: tryContentUpdater(target, url)
-         ?: InternalLinkResult.CannotResolve
+private suspend fun tryResolveLink(targetPointer: Pointer<out DocumentationTarget>, url: String): InternalLinkResult? {
+  return when (val resolveResult = resolveLink(targetPointer::dereference, url)) {
+    InternalResolveLinkResult.InvalidTarget -> InternalLinkResult.InvalidTarget
+    InternalResolveLinkResult.CannotResolve -> null
+    is InternalResolveLinkResult.Value -> InternalLinkResult.Request(resolveResult.value)
+  }
 }
 
-private fun tryResolveLink(target: DocumentationTarget, url: String): InternalLinkResult? {
-  val result = resolveLink(target, url) ?: return null
+internal sealed class InternalResolveLinkResult<out X> {
+  object InvalidTarget : InternalResolveLinkResult<Nothing>()
+  object CannotResolve : InternalResolveLinkResult<Nothing>()
+  class Value<X>(val value: X) : InternalResolveLinkResult<X>()
+}
+
+internal suspend fun resolveLink(
+  targetSupplier: () -> DocumentationTarget?,
+  url: String,
+): InternalResolveLinkResult<DocumentationRequest> {
+  return resolveLink(targetSupplier, url, DocumentationTarget::documentationRequest)
+}
+
+/**
+ * @param ram read action mapper - a function which would be applied to resolved [DocumentationTarget] while holding the read action
+ */
+internal suspend fun <X> resolveLink(
+  targetSupplier: () -> DocumentationTarget?,
+  url: String,
+  ram: (DocumentationTarget) -> X,
+): InternalResolveLinkResult<X> {
+  return readAction {
+    resolveLinkInReadAction(targetSupplier, url, ram)
+  }
+}
+
+private fun <X> resolveLinkInReadAction(
+  targetSupplier: () -> DocumentationTarget?,
+  url: String,
+  m: (DocumentationTarget) -> X,
+): InternalResolveLinkResult<X> {
+  val documentationTarget = targetSupplier()
+                            ?: return InternalResolveLinkResult.InvalidTarget
   @Suppress("REDUNDANT_ELSE_IN_WHEN")
-  return when (result) {
-    is ResolvedTarget -> InternalLinkResult.Request(result.target.documentationRequest())
-    else -> error("Unexpected result: $result") // this fixes Kotlin incremental compilation
+  return when (val linkResolveResult: LinkResolveResult? = resolveLink(documentationTarget, url)) {
+    null -> InternalResolveLinkResult.CannotResolve
+    is ResolvedTarget -> InternalResolveLinkResult.Value(m(linkResolveResult.target))
+    else -> error("Unexpected result: $linkResolveResult") // this fixes Kotlin incremental compilation
   }
-}
-
-private fun tryContentUpdater(target: DocumentationTarget, url: String): InternalLinkResult? {
-  for (handler in DocumentationLinkHandler.EP_NAME.extensionList) {
-    ProgressManager.checkCanceled()
-    val updater = handler.contentUpdater(target, url) ?: continue
-    return InternalLinkResult.Updater(updater)
-  }
-  return null
 }
 
 internal fun resolveLink(target: DocumentationTarget, url: String): LinkResolveResult? {
   for (handler in DocumentationLinkHandler.EP_NAME.extensionList) {
     ProgressManager.checkCanceled()
     return handler.resolveLink(target, url) ?: continue
+  }
+  return null
+}
+
+private suspend fun tryContentUpdater(targetPointer: Pointer<out DocumentationTarget>, url: String): InternalLinkResult? {
+  return readAction {
+    contentUpdaterInReadAction(targetPointer, url)
+  }
+}
+
+private fun contentUpdaterInReadAction(targetPointer: Pointer<out DocumentationTarget>, url: String): InternalLinkResult? {
+  val target = targetPointer.dereference()
+               ?: return InternalLinkResult.InvalidTarget
+  val updater = contentUpdater(target, url)
+                ?: return null
+  return InternalLinkResult.Updater(updater)
+}
+
+private fun contentUpdater(target: DocumentationTarget, url: String): ContentUpdater? {
+  for (handler in DocumentationLinkHandler.EP_NAME.extensionList) {
+    ProgressManager.checkCanceled()
+    return handler.contentUpdater(target, url) ?: continue
   }
   return null
 }
