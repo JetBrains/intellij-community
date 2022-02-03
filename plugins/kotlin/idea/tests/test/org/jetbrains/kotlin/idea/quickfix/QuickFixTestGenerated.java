c874a9800952a35caa63dc2c5c2049ab4818536f
Dmitry Timofeev <dmitry.timofeev@jetbrains.com>
2022-02-03 13:10:40 +0300

    [kotlin] [test] Update `@OptIn`-related tests w.r.t. compiler changes
    Updated in `kt-212-master` tests to reflect recent changes in the compiler.
    
    1. Removed `-Xopt-in` command line argument, use `-opt-in` everywhere in
    the `quickfix/experimental` test suite. Removed a test that became
    duplicating after the change.
    
    Corresponding commit in the compiler: eeadd8588dfe8284c1c1ab1275b87661db5c1001
    
    2. Removed a test that become obsolete after reverting
    `OPT_IN_MARKER_ON_OVERRIDE` limitation.
    
    Corresponding commit in the compiler: c2f43dd98e4ceadd5cf5b6d81bca212e36c6a284
    
    ^KTIJ-20990 Fixed
    
    GitOrigin-RevId: a7979223f847367a97bdc1e3ffdc38167c4554bb

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
@@ -7877,11 +7877,6 @@ public abstract class QuickFixTestGenerated extends AbstractQuickFixTest {
             runTest("testData/quickfix/experimental/experimentalUnused.kt");
         }
 
-        @TestMetadata("experimentalUnused2.kt")
-        public void testExperimentalUnused2() throws Exception {
-            runTest("testData/quickfix/experimental/experimentalUnused2.kt");
-        }
-
         @TestMetadata("forbiddenTargetsExpression.kt")
         public void testForbiddenTargetsExpression() throws Exception {
             runTest("testData/quickfix/experimental/forbiddenTargetsExpression.kt");
@@ -7965,11 +7960,6 @@ public abstract class QuickFixTestGenerated extends AbstractQuickFixTest {
         @TestMetadata("override.kt")
         public void testOverride() throws Exception {
             runTest("testData/quickfix/experimental/override.kt");
-        }
-
-        @TestMetadata("overrideAnnotationRemove.kt")
-        public void testOverrideAnnotationRemove() throws Exception {
-            runTest("testData/quickfix/experimental/overrideAnnotationRemove.kt");
         }
 
         @TestMetadata("propertyInConstructor.kt")
--- plugins/kotlin/idea/tests/testData/quickfix/experimental/typeAlias.kt.after
+++ plugins/kotlin/idea/tests/testData/quickfix/experimental/typeAlias.kt.after
@@ -1,5 +1,5 @@
 // "Opt in for 'AliasMarker' on 'AliasMarkerUsage'" "true"
-// COMPILER_ARGUMENTS: -Xopt-in=kotlin.RequiresOptIn
+// COMPILER_ARGUMENTS: -opt-in=kotlin.RequiresOptIn
 // WITH_STDLIB
 // ACTION: Add '-Xopt-in=AliasMarker' to module light_idea_test_case compiler arguments
 
