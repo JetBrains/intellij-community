c8ee8e051bf16842213a81e8dce721979a4c6557
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-02-03 18:25:05 +0100

    Revert "Change dependency ktor-html-builder to ktor-server-html-builder-jvm"
    This reverts commit b618fc0e0fe606ce29e6e8931c8face6e356e262.
    
    GitOrigin-RevId: c41f7d7ab883a95374c65a9101988e38f1ed0886

--- plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/templates/KtorServerTemplate.kt
+++ plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/templates/KtorServerTemplate.kt
@@ -40,7 +40,7 @@ object KtorServerTemplate : Template() {
         withSettingsOf(module.originalModule) {
             buildList {
                 +DEPENDENCIES.KTOR_SERVER_NETTY
-                +ktorArtifactDependency("ktor-server-html-builder-jvm")
+                +ktorArtifactDependency("ktor-html-builder")
                 +ArtifactBasedLibraryDependencyIR(
                     MavenArtifact(Repositories.KOTLINX_HTML, "org.jetbrains.kotlinx", "kotlinx-html-jvm"),
                     Versions.KOTLINX.KOTLINX_HTML,
