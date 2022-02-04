d5112a3caf92e6ab857b07ae19a34ccc6b651a1a
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-02-04 18:52:19 +0700

    [kotlin] migrate LookupCancelWatcher to declarative listeners
    ^KTIJ-20997
    
    GitOrigin-RevId: 60cc0ada7e17416687593931f7dea4ff8e45cbd8

--- plugins/kotlin/completion/src/org/jetbrains/kotlin/idea/completion/LookupCancelWatcher.kt
+++ plugins/kotlin/completion/src/org/jetbrains/kotlin/idea/completion/LookupCancelWatcher.kt
@@ -1,29 +1,32 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.completion
 
+import com.intellij.codeInsight.lookup.Lookup
 import com.intellij.codeInsight.lookup.LookupManagerListener
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.event.EditorFactoryEvent
 import com.intellij.openapi.editor.event.EditorFactoryListener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import org.jetbrains.kotlin.idea.core.KotlinPluginDisposable
 
 class LookupCancelWatcher : StartupActivity {
     override fun runActivity(project: Project) {
-        val connection = project.messageBus.connect()
         EditorFactory.getInstance().addEditorFactoryListener(
             object : EditorFactoryListener {
                 override fun editorReleased(event: EditorFactoryEvent) {
                     LookupCancelService.getServiceIfCreated(project)?.disposeLastReminiscence(event.editor)
                 }
             },
-            connection
+            KotlinPluginDisposable.getInstance(project),
         )
+    }
+}
 
-        connection.subscribe(LookupManagerListener.TOPIC, LookupManagerListener { _, newLookup ->
-            if (newLookup == null) return@LookupManagerListener
-            newLookup.addLookupListener(LookupCancelService.getInstance(newLookup.project).lookupCancelListener)
-        })
+class LookupCancelWatcherListener : LookupManagerListener {
+    override fun activeLookupChanged(oldLookup: Lookup?, newLookup: Lookup?) {
+        if (newLookup == null) return
+        newLookup.addLookupListener(LookupCancelService.getInstance(newLookup.project).lookupCancelListener)
     }
 }
