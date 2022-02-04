3f00714bb0506899681def06fce632a026f8ad7e
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-02-04 20:04:15 +0700

    [kotlin] migrate JvmStartupActivity to declarative listeners
    ^KTIJ-20997
    
    GitOrigin-RevId: 230f23852386ebdb02fa2f04a103ec9875450701

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/util/fileUtils.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/util/fileUtils.kt
@@ -1,9 +1,13 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.util
 
+import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.kotlin.idea.KotlinFileType
 
 fun VirtualFile.isKotlinFileType(): Boolean =
     extension == KotlinFileType.EXTENSION || FileTypeRegistry.getInstance().isFileOfType(this, KotlinFileType.INSTANCE)
+
+fun VirtualFile.isJavaFileType(): Boolean =
+    extension == JavaFileType.DEFAULT_EXTENSION || FileTypeRegistry.getInstance().isFileOfType(this, JavaFileType.INSTANCE)
