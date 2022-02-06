9011b49540516cba2a2662c21336953f0dbd8055
Adam Malek <adam.malek@jetbrains.com>
2022-02-06 20:14:16 +0100

    ML in SE: Fix exception when getting package name (EA-441659)
    GitOrigin-RevId: 5d54610f965ef30397f1f3eb377368b4bf5d3ddb

--- plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/features/SearchEverywhereClassOrFileFeaturesProvider.kt
+++ plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/features/SearchEverywhereClassOrFileFeaturesProvider.kt
@@ -184,8 +184,10 @@ abstract class SearchEverywhereClassOrFileFeaturesProvider(vararg supportedTab: 
 
     val (openedFilePackage, foundFilePackage) = ReadAction.compute<Pair<String?, String?>, Nothing> {
       val fileIndex = ProjectRootManager.getInstance(project).fileIndex
-      val openedFileDirectory = openedFile.parent?.takeIf { it.isDirectory }  // Parents of some files may still not be directories
-      val foundFileDirectory = if (file.isDirectory) file else file.parent
+
+      // Parents of some files may still not be directories
+      val openedFileDirectory = openedFile.parent?.takeIf { it.isDirectory }
+      val foundFileDirectory = if (file.isDirectory) file else file.parent.takeIf { it.isDirectory }
 
       val openedFilePackageName = openedFileDirectory?.let { fileIndex.getPackageNameByDirectory(it) }
       val foundFilePackageName = foundFileDirectory?.let { fileIndex.getPackageNameByDirectory(it) }
