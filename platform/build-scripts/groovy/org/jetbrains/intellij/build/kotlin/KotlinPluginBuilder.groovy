63bc8a0cb53b9bf53d98a62154b74d23924bbda4
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-02-07 14:20:40 +0100

    KTIJ-20799 [kotlin] Enable K/N part of kotlin-ultimate plugin
    Js part is still disabled -- waiting for additional fixes
    
    GitOrigin-RevId: 049e790eef262f9fb6f114fdb74f30363ffb88be

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/kotlin/KotlinPluginBuilder.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/kotlin/KotlinPluginBuilder.groovy
@@ -156,13 +156,14 @@ class KotlinPluginBuilder {
       }
 
       if (isUltimate && kind == KotlinPluginKind.IJ) {
-        // Do not pack ultimate part till the KTIJ-20799 is fixed
-        //withModule("kotlin-ultimate.common-native")
-        //withModule("kotlin-ultimate.common-noncidr-native")
+        withModule("kotlin-ultimate.common-native")
+        withModule("kotlin-ultimate.common-noncidr-native")
+        withModule("kotlin-ultimate.ultimate-plugin")
+        withModule("kotlin-ultimate.ultimate-native")
+
+        // Temporary disabled the JS-ultimate part till KTIJ-20799 is fixed
         //withModule("kotlin-ultimate.javascript.debugger")
         //withModule("kotlin-ultimate.javascript.nodeJs")
-        //withModule("kotlin-ultimate.ultimate-plugin")
-        //withModule("kotlin-ultimate.ultimate-native")
       }
 
       if (kind == KotlinPluginKind.AC_KMM) {
