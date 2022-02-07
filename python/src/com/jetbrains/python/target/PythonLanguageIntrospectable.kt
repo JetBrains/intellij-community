6e7c1488ca3958a1cb7db800433e6e5f308a3967
Alexander Koshevoy <alexander.koshevoy@jetbrains.com>
2022-02-07 19:23:45 +0300

    PY-52917 Calculate HOME env value regardless of python executables location results
    GitOrigin-RevId: 31f6dd652764ebd2ec512691b6ca8935608d816a

--- python/src/com/jetbrains/python/target/PythonLanguageIntrospectable.kt
+++ python/src/com/jetbrains/python/target/PythonLanguageIntrospectable.kt
@@ -4,6 +4,7 @@ package com.jetbrains.python.target
 import com.intellij.execution.target.LanguageRuntimeType
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletionStage
+import java.util.function.BiFunction
 
 class PythonLanguageIntrospectable(val config: PythonLanguageRuntimeConfiguration) : LanguageRuntimeType.Introspector<PythonLanguageRuntimeConfiguration> {
   override fun introspect(subject: LanguageRuntimeType.Introspectable): CompletableFuture<PythonLanguageRuntimeConfiguration> {
@@ -16,7 +17,7 @@ class PythonLanguageIntrospectable(val config: PythonLanguageRuntimeConfiguratio
           if (iterator.hasNext()) config.pythonInterpreterPath = iterator.next()
         }
       }
-    val userHomeFuture = pythonExecutablePathFuture.thenCompose {
+    val userHomeFuture = pythonExecutablePathFuture.thenExecute {
       subject
         .promiseEnvironmentVariable("HOME")
         .thenComposeIf(String?::isNullOrBlank) {
@@ -41,5 +42,18 @@ class PythonLanguageIntrospectable(val config: PythonLanguageRuntimeConfiguratio
 
     private fun <T> CompletableFuture<T>.thenComposeIf(predicate: (T) -> Boolean, fn: () -> CompletionStage<T>): CompletableFuture<T> =
       thenCompose { if (predicate(it)) fn() else this }
+
+    /**
+     * Discards the return value and ignores any exception during the execution of [this] future and invokes [fn].
+     *
+     * Please note that [com.intellij.execution.target.LanguageRuntimeType.Introspectable.promiseExecuteScript] does not provide the exit
+     * code of the script in the return value. Moreover, its implementations treats non-zero exit code as an error and throws an exception
+     * in this case.
+     *
+     * However, getting non-zero exit code in some cases are perfectly normal. F.e. getting the exit code `1` in case when a Python
+     * executable cannot be found using `which python3` command is fine and this case could be handled properly.
+     */
+    private fun <T, U> CompletableFuture<T>.thenExecute(fn: () -> CompletionStage<U>): CompletableFuture<U> =
+      handle(BiFunction { _, _ -> null }).thenCompose { fn() }
   }
 }
