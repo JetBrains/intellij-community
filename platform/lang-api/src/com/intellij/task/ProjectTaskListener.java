1fe538bdd82363ccaf68ab7db784517b98d5a66b
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-02-07 19:05:06 +0700

    [platform] mark ProjectTaskListener.TOPIC as '@Topic.ProjectLevel'
    GitOrigin-RevId: a3f4fefee83626189be04489e1f6bed74a174bb4

--- platform/lang-api/src/com/intellij/task/ProjectTaskListener.java
+++ platform/lang-api/src/com/intellij/task/ProjectTaskListener.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.task;
 
 import com.intellij.util.messages.Topic;
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 public interface ProjectTaskListener {
+  @Topic.ProjectLevel
   Topic<ProjectTaskListener> TOPIC = new Topic<>("project task events", ProjectTaskListener.class);
 
   /**
