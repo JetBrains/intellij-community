7749a2a40bcce2bc8df436f770ccbd14687cc686
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2022-02-08 10:34:08 +0300

    [intellij project configuration] extract tests from platform.execution and platform.execution.impl to a separate module
    This way we'll be able to reuse code from testFramework in them. (We cannot add dependency on testFramework to platform.execution.impl module because it'll lead to circular dependencies.)
    
    GitOrigin-RevId: 678bf68acb047aeecd949da57c760977211d0f71

--- platform/execution-impl/testSources/com/intellij/execution/MessageTest.java
+++ platform/execution-impl/testSources/com/intellij/execution/MessageTest.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij;
+package com.intellij.execution;
 
-import com.intellij.execution.ExecutionBundle;
 import org.junit.Assert;
 import org.junit.Test;
 
