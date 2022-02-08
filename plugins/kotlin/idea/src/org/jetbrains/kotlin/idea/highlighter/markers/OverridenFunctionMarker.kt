59efb62bdd094b3d89d385dae8eb6a2b9880ab46
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2022-02-08 16:06:28 +0100

    [kotlin] Address review comments on clean up corresponding light method when overridden kotlin function is found
    #KTIJ-21027
    
    GitOrigin-RevId: b75b282d38628040783fff7362f5902060af76d3

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/highlighter/markers/OverridenFunctionMarker.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/highlighter/markers/OverridenFunctionMarker.kt
@@ -30,7 +30,6 @@ import org.jetbrains.kotlin.idea.util.application.runReadAction
 import org.jetbrains.kotlin.idea.util.isExpectDeclaration
 import org.jetbrains.kotlin.idea.util.projectStructure.module
 import org.jetbrains.kotlin.psi.psiUtil.hasActualModifier
-import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 import java.awt.event.MouseEvent
 import javax.swing.JComponent
 
@@ -45,20 +44,12 @@ internal fun <T> getOverriddenDeclarations(mappingToJava: MutableMap<PsiElement,
             ProgressManager.checkCanceled()
             val possiblyFakeLightMethods = overridingMember.toPossiblyFakeLightMethods()
             possiblyFakeLightMethods.find { !it.isMethodWithDeclarationInOtherClass() }?.let {
-                val declaration = mappingToJava[superMember]
-                if (declaration != null) {
-                    mappingToJava.remove(superMember)
+                mappingToJava.remove(superMember)?.let { declaration ->
                     // Light methods points to same methods
                     // and no reason to keep searching those methods
                     // those originals are found
                     if (mappingToJava.remove(it) == null) {
-                        val iterator = mappingToJava.keys.iterator()
-                        while (iterator.hasNext()) {
-                            val next = iterator.next().safeAs<KtLightMethod>() ?: continue
-                            if (mappingToJava[next] == superMember) {
-                                iterator.remove()
-                            }
-                        }
+                        mappingToJava.values.removeIf(superMember::equals)
                     }
                     overridden.add(declaration)
                 }
