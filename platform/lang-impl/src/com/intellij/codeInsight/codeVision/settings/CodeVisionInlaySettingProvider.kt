5239346887f8ee1c1a7f8e070c692aeccccce47b
Nikita.Lyubimov <lyubimov.nkt@gmail.com>
2022-02-08 03:39:14 -0800

    [Code Vision] check language in code vision settings adapter
    GitOrigin-RevId: 759f357c88d5288c08d08a2d0ae34f12d45546d2

--- platform/lang-impl/src/com/intellij/codeInsight/codeVision/settings/CodeVisionInlaySettingProvider.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/codeVision/settings/CodeVisionInlaySettingProvider.kt
@@ -8,6 +8,7 @@ import com.intellij.openapi.project.Project
 
 class CodeVisionInlaySettingProvider : InlaySettingsProvider {
   override fun createModels(project: Project, language: Language): List<InlayProviderSettingsModel> {
+    if (language != Language.ANY) return emptyList()
     val codeVisionGroupModels = CodeVisionGroupSettingProvider.EP.findGroupModels() + CodeVisionGroupSettingProvider.EP.findSingleModels(project)
     return codeVisionGroupModels.map { it.createModel(project) }
   }
