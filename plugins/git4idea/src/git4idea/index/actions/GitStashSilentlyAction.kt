c3d783cb7eea0e682f00380d0ca2fae59fe0d72e
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2022-02-10 04:55:40 +0300

    [git-stash] do not implement GitRepositoryAction in GitStashSilentlyAction
    GitOrigin-RevId: 55f3ef939716044fe02897ffcde9855e29673e91

--- plugins/git4idea/src/git4idea/index/actions/GitStashSilentlyAction.kt
+++ plugins/git4idea/src/git4idea/index/actions/GitStashSilentlyAction.kt
@@ -1,26 +1,28 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.index.actions
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.components.serviceIfCreated
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFile
-import git4idea.actions.GitRepositoryAction
+import com.intellij.openapi.project.DumbAwareAction
 import git4idea.actions.GitStash
-import git4idea.i18n.GitBundle
 import git4idea.index.GitStageTracker
+import git4idea.repo.GitRepositoryManager
 import git4idea.stash.createStashHandler
 
-class GitStashSilentlyAction : GitRepositoryAction() {
-  override fun perform(project: Project, gitRoots: MutableList<VirtualFile>, defaultRoot: VirtualFile) {
+class GitStashSilentlyAction : DumbAwareAction() {
+  override fun update(e: AnActionEvent) {
+    val project = e.project
+    if (project == null) {
+      e.presentation.isEnabledAndVisible = false
+      return
+    }
+    e.presentation.isVisible = e.isFromActionToolbar || GitRepositoryManager.getInstance(project).repositories.isNotEmpty()
+    e.presentation.isEnabled = project.serviceIfCreated<GitStageTracker>()?.state?.hasChangedRoots() == true
+  }
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project ?: return
     val roots = GitStageTracker.getInstance(project).state.changedRoots
     GitStash.runStashInBackground(project, roots) { createStashHandler(project, it) }
   }
-
-  override fun isEnabled(e: AnActionEvent): Boolean {
-    val gitStageTracker = e.project?.serviceIfCreated<GitStageTracker>() ?: return false
-    return super.isEnabled(e) && gitStageTracker.state.hasChangedRoots()
-  }
-
-  override fun getActionName() = GitBundle.message("stash.action.name")
 }
