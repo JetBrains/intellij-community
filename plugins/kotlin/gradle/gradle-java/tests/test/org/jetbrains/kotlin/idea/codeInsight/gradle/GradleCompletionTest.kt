ac0dca03eb1c3ea5e709914dd14f02e26d0bbab8
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-02-11 18:22:06 +0700

    [kotlin] completion: don't suggest expect class if actual exists
    ^KTIJ-14740
    
    GitOrigin-RevId: ebe92124f87fc165c5cdc3d69869a0f8393044fe

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/GradleCompletionTest.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/GradleCompletionTest.kt
@@ -2,37 +2,14 @@
 package org.jetbrains.kotlin.idea.codeInsight.gradle
 
 import com.intellij.openapi.actionSystem.IdeActions
-import com.intellij.testFramework.fixtures.CodeInsightTestFixture
-import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory
 import com.intellij.testFramework.runInEdtAndWait
-import com.intellij.util.ThrowableRunnable
-import org.jetbrains.kotlin.idea.test.runAll
 import org.jetbrains.plugins.gradle.tooling.annotation.PluginTargetVersions
 import org.junit.Test
-import kotlin.reflect.KMutableProperty0
 
-class GradleCompletionTest : MultiplePluginVersionGradleImportingTestCase() {
+class GradleCompletionTest : MultiplePluginVersionGradleImportingCodeInsightTestCase() {
     override fun testDataDirName() = "completion"
-    private lateinit var codeInsightTestFixture: CodeInsightTestFixture
 
-    final override fun setUpFixtures() {
-        myTestFixture = IdeaTestFixtureFactory.getFixtureFactory().createFixtureBuilder(getName()).fixture
-        codeInsightTestFixture = IdeaTestFixtureFactory.getFixtureFactory().createCodeInsightFixture(myTestFixture)
-        codeInsightTestFixture.setUp()
-    }
-
-    final override fun tearDownFixtures() = runAll(
-        ThrowableRunnable { codeInsightTestFixture.tearDown() },
-        ThrowableRunnable {
-            @Suppress("UNCHECKED_CAST")
-            (this::codeInsightTestFixture as KMutableProperty0<CodeInsightTestFixture?>).set(null)
-        },
-        ThrowableRunnable { myTestFixture = null },
-    )
-
-    @Test
-    @PluginTargetVersions(pluginVersion = "1.5.31+")
-    fun testExpectAndActualDuplication() {
+    private fun doCompletionTest(expected: List<String>) {
         val files = configureByFiles()
         importProject()
 
@@ -42,9 +19,17 @@ class GradleCompletionTest : MultiplePluginVersionGradleImportingTestCase() {
             codeInsightTestFixture.performEditorAction(IdeActions.ACTION_CODE_COMPLETION)
             val actual = codeInsightTestFixture.lookupElementStrings
             assertEquals(
-                listOf("myFunctionToCheck", "anotherMyFunctionToCheck"),
+                expected,
                 actual,
             )
         }
     }
+
+    @Test
+    @PluginTargetVersions(pluginVersion = "1.5.31+")
+    fun testExpectAndActualDuplicationFunction() = doCompletionTest(listOf("myFunctionToCheck", "anotherMyFunctionToCheck"))
+
+    @Test
+    @PluginTargetVersions(pluginVersion = "1.5.31+")
+    fun testExpectAndActualDuplicationClass() = doCompletionTest(listOf("ClassToCheck", "AnotherClassToCheck"))
 }
