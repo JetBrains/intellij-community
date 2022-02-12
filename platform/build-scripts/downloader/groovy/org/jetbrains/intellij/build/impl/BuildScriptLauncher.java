ec4dfb3e2c9099303e0ea0c6dc516a313524c067
Leonid Shalupov <leonid@shalupov.com>
2022-02-12 17:31:53 +0000

    build-scripts: move BuildScriptLauncher down the modules order
    it's required to run jps-bootstrap targets from intellij.python.tools
    in the future BuildScriptLauncher most likely will be a part of util and accessible everywhere
    
    GitOrigin-RevId: db92e30a1c1861d36d377e102d3c8b5545de170f

--- platform/build-scripts/downloader/groovy/org/jetbrains/intellij/build/impl/BuildScriptLauncher.java
+++ platform/build-scripts/downloader/groovy/org/jetbrains/intellij/build/impl/BuildScriptLauncher.java
@@ -1,13 +1,19 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.intellij.build.impl;
 
-import com.intellij.util.ExceptionUtil;
 import jetbrains.buildServer.messages.serviceMessages.Message;
 import org.jetbrains.intellij.build.dependencies.TeamCityHelper;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 
+/**
+ * jps-bootstrap launchers main classes via this wrapper to correctly log exceptions
+ * please do not add any more logic here as it won't be run if you start your target
+ * from IDE
+ */
 public class BuildScriptLauncher {
   private static final String MAIN_CLASS_PROPERTY = "build.script.launcher.main.class";
 
@@ -24,7 +30,11 @@ public class BuildScriptLauncher {
 
       System.exit(0);
     } catch (Throwable t) {
-      String message = ExceptionUtil.getThrowableText(t);
+      StringWriter sw = new StringWriter();
+      try (PrintWriter printWriter = new PrintWriter(sw)) {
+        t.printStackTrace(printWriter);
+      }
+      String message = sw.toString();
 
       if (TeamCityHelper.isIsUnderTeamCity()) {
         // Under TeamCity non-zero exit code will be displayed as a separate build error
