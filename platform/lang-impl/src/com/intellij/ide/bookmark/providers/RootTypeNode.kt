f3f8c41b9b4f0c15185dd74f96b622d2d0b82857
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2022-02-15 01:30:02 +0300

    IDEA-288530/IDEA-288531/IDEA-288541: No subfolders/subfiles for bookmarked folders outside the project
    GitOrigin-RevId: e51777f3bd48ce1ab17618d525bb3691faf26932

--- platform/lang-impl/src/com/intellij/ide/bookmark/providers/RootTypeNode.kt
+++ platform/lang-impl/src/com/intellij/ide/bookmark/providers/RootTypeNode.kt
@@ -3,10 +3,10 @@ package com.intellij.ide.bookmark.providers
 
 import com.intellij.icons.AllIcons
 import com.intellij.ide.bookmark.ui.tree.BookmarkNode
+import com.intellij.ide.bookmark.ui.tree.computeDirectoryChildren
 import com.intellij.ide.projectView.PresentationData
 import com.intellij.ide.scratch.ScratchFileService
 import com.intellij.ide.scratch.ScratchesNamedScope
-import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
@@ -20,7 +20,7 @@ internal class RootTypeNode(project: Project, bookmark: RootTypeBookmark) : Book
     ?.let { ScratchFileService.getInstance().getRootPath(it.type) }
     ?.let { LocalFileSystem.getInstance().findFileByPath(it) }
 
-  override fun getChildren() = emptyList<AbstractTreeNode<*>>()
+  override fun getChildren() = computeDirectoryChildren()
 
   override fun update(presentation: PresentationData) {
     presentation.setIcon(wrapIcon(AllIcons.Nodes.Folder))
--- platform/lang-impl/src/com/intellij/ide/bookmark/ui/tree/extensions.kt
+++ platform/lang-impl/src/com/intellij/ide/bookmark/ui/tree/extensions.kt
@@ -1,8 +1,20 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.bookmark.ui.tree
 
 import com.intellij.ide.bookmark.BookmarksManager
+import com.intellij.ide.projectView.PresentationData
+import com.intellij.ide.projectView.ProjectViewNode
+import com.intellij.ide.projectView.ViewSettings
+import com.intellij.ide.projectView.impl.CompoundIconProvider.findIcon
+import com.intellij.ide.projectView.impl.nodes.ProjectViewDirectoryHelper
+import com.intellij.ide.projectView.impl.nodes.PsiFileNode
 import com.intellij.ide.util.treeView.AbstractTreeNode
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiUtilCore.findFileSystemItem
 
 internal val Any.asAbstractTreeNode
   get() = this as? AbstractTreeNode<*>
@@ -15,3 +27,35 @@ internal val AbstractTreeNode<*>.parentRootNode: RootNode?
 
 internal val AbstractTreeNode<*>.parentFolderNode: FolderNode?
   get() = this as? FolderNode ?: parent?.parentFolderNode
+
+fun ProjectViewNode<*>.computeDirectoryChildren(): Collection<AbstractTreeNode<*>> {
+  val directory = findFileSystemItem(project, virtualFile) as? PsiDirectory ?: return emptyList()
+  if (ProjectFileIndex.getInstance(directory.project).getModuleForFile(directory.virtualFile, false) != null) {
+    return ProjectViewDirectoryHelper.getInstance(directory.project).getDirectoryChildren(directory, settings, true)
+  }
+  val children = directory.virtualFile.children ?: return emptyList()
+  return mutableListOf<AbstractTreeNode<*>>().apply {
+    children.forEach {
+      when (val item = findFileSystemItem(directory.project, it)) {
+        is PsiDirectory -> add(ExternalFolderNode(item, settings))
+        is PsiFile -> add(PsiFileNode(item.project, item, settings))
+      }
+    }
+  }
+}
+
+private class ExternalFolderNode(directory: PsiDirectory, settings: ViewSettings?)
+  : ProjectViewNode<PsiDirectory>(directory.project, directory, settings) {
+
+  override fun canRepresent(element: Any?) = virtualFile?.equals(element) ?: false
+  override fun contains(file: VirtualFile) = virtualFile?.let { VfsUtil.isAncestor(it, file, true) } ?: false
+
+  override fun getVirtualFile() = value?.virtualFile
+  override fun getChildren() = computeDirectoryChildren()
+
+  override fun update(presentation: PresentationData) {
+    val file = virtualFile ?: return
+    presentation.setIcon(findIcon(findFileSystemItem(project, file), 0))
+    presentation.presentableText = file.presentableName
+  }
+}
