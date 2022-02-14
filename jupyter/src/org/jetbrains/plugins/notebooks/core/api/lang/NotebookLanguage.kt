f428a650609525af1c412c52d498c97e4abe3e9f
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2022-02-14 23:21:01 +0700

    DS-2110 Jupyter: support 3rd-party content manager Jupytext for remote servers
    GitOrigin-RevId: 896d0ca4a64b6af9b0bd758397c04d769065cc41

--- jupyter/src/org/jetbrains/plugins/notebooks/core/api/lang/NotebookLanguage.kt
+++ jupyter/src/org/jetbrains/plugins/notebooks/core/api/lang/NotebookLanguage.kt
@@ -1,9 +1,14 @@
 package org.jetbrains.plugins.notebooks.core.api.lang
 
 import com.intellij.lang.Language
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.fileTypes.LanguageFileType
 
 /**
  * General language for notebooks.
  * To share some of the common features for the notebooks, their language should use [NotebookLanguage] as base one.
  */
 object NotebookLanguage: Language("Notebook")
+
+val FileType.isNotebookLanguage: Boolean
+  get() = (this as? LanguageFileType)?.language?.baseLanguage is NotebookLanguage
