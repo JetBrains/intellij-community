81dabf07619022175044edc21eb3878a77555b08
Roman Golyshev <roman.golyshev@jetbrains.com>
2022-02-15 18:17:09 +0300

    FIR IDE: Add test for `KotlinIdeAnnotationsResolver.annotationsOnDeclaration`
    GitOrigin-RevId: 4691396f2b7f69fe8495db4a348bd9b24c53eaf1

--- plugins/kotlin/generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ plugins/kotlin/generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -68,6 +68,7 @@ import org.jetbrains.kotlin.idea.editor.backspaceHandler.AbstractBackspaceHandle
 import org.jetbrains.kotlin.idea.editor.quickDoc.AbstractQuickDocProviderTest
 import org.jetbrains.kotlin.idea.externalAnnotations.AbstractExternalAnnotationTest
 import org.jetbrains.kotlin.idea.filters.AbstractKotlinExceptionFilterTest
+import org.jetbrains.kotlin.idea.fir.analysis.providers.AbstractKotlinIdeAnnotationsResolverTest
 import org.jetbrains.kotlin.idea.fir.analysis.providers.sessions.AbstractSessionsInvalidationTest
 import org.jetbrains.kotlin.idea.fir.analysis.providers.trackers.AbstractProjectWideOutOfBlockKotlinModificationTrackerTest
 import org.jetbrains.kotlin.idea.fir.codeInsight.handlers.AbstractHLGotoSuperActionHandlerTest
@@ -999,6 +1000,10 @@ private fun assembleWorkspace(): TWorkspace = workspace {
 
         testClass<AbstractSessionsInvalidationTest> {
             model("sessionInvalidation", pattern = DIRECTORY, isRecursive = false)
+        }
+
+        testClass<AbstractKotlinIdeAnnotationsResolverTest> {
+            model("annotationsResolver", pattern = KT_WITHOUT_DOTS)
         }
     }
 
