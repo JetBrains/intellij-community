93f2451dbaa2af984cac809c1862408809578028
Dmitry Timofeev <dmitry.timofeev@jetbrains.com>
2022-02-15 21:34:31 +0300

    [kotlin] Fixed infinite recursion in `MakeUpperBoundNonNullableFix`
    There was an infinite loop in the quick fix factory for "Make upper
    bound non-nullable" quick fix during the search for type parameters that
    would differ in nullability. The loop occurred when processing some
    types containing the star projection (e.g., `Enum<*>` and
    `Class<Enum<*>>`).
    
    This commit fixes the infinite loop by adding an
    explicit check to skip the recursive call if any involved type
    projection is a star projection.
    
    ^KTIJ-21154 Fixed
    
    GitOrigin-RevId: 3f8b886d8148fb609f9cc0669c1cf7f3754bff08

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
@@ -8839,6 +8839,11 @@ public abstract class QuickFixTestGenerated extends AbstractQuickFixTest {
             KotlinTestUtils.runTest(this::doTest, this, testDataFilePath);
         }
 
+        @TestMetadata("noStackOverflowOnStarProjection.kt")
+        public void testNoStackOverflowOnStarProjection() throws Exception {
+            runTest("testData/quickfix/makeUpperBoundNonNullable/noStackOverflowOnStarProjection.kt");
+        }
+
         @TestMetadata("typeMismatchAny1.kt")
         public void testTypeMismatchAny1() throws Exception {
             runTest("testData/quickfix/makeUpperBoundNonNullable/typeMismatchAny1.kt");
