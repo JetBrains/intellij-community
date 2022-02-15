1aa27d922add2ca5d2d0e9698abc33b2f0a6ac30
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2022-02-15 18:25:45 +0300

    [settings-sync] IDEA-288865 Add ability to choose which settings will be applied: local or from server
    GitOrigin-RevId: c2f2d2751233ae96fb96ec4dd7f14f1662757315

--- plugins/settings-sync/src/com/intellij/settingsSync/config/EnableSettingsSyncDialog.kt
+++ plugins/settings-sync/src/com/intellij/settingsSync/config/EnableSettingsSyncDialog.kt
@@ -3,7 +3,6 @@ package com.intellij.settingsSync.config
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.settingsSync.SettingsSyncBundle.message
-import com.intellij.settingsSync.SettingsSyncSettings
 import org.jetbrains.annotations.Nls
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
@@ -12,11 +11,18 @@ import javax.swing.JComponent
 
 internal class EnableSettingsSyncDialog(parent: JComponent, val settingsFound: Boolean) : DialogWrapper(parent, false) {
 
-  private lateinit var configPanel : DialogPanel
+  private lateinit var configPanel: DialogPanel
+  private var isConfirmed = false
 
   init {
     title = message("title.settings.sync")
     init()
+  }
+
+  companion object Result {
+    const val ENABLE_SYNC = 0
+    const val PUSH_LOCAL = 100
+    const val GET_FROM_SERVER = 101
   }
 
   override fun createCenterPanel(): JComponent {
@@ -35,21 +41,27 @@ internal class EnableSettingsSyncDialog(parent: JComponent, val settingsFound: B
 
   inner class EnableSyncAction : AbstractAction(message("enable.dialog.enable.sync.action")) {
     override fun actionPerformed(e: ActionEvent?) {
-      configPanel.apply()
-      SettingsSyncSettings.getInstance().syncEnabled = true
-      close(0, true)
+      applyAndClose(ENABLE_SYNC)
     }
   }
 
   inner class SyncLocalSettingsAction : AbstractAction(message("enable.dialog.sync.local.settings")) {
     override fun actionPerformed(e: ActionEvent?) {
-      TODO("Not yet implemented")
+      applyAndClose(PUSH_LOCAL)
     }
   }
 
   inner class GetSettingsFromAccountAction : AbstractAction(message("enable.dialog.get.settings.from.account")) {
     override fun actionPerformed(e: ActionEvent?) {
-      TODO("Not yet implemented")
+      applyAndClose(GET_FROM_SERVER)
     }
   }
+
+  private fun applyAndClose(exitCode: Int) {
+    configPanel.apply()
+    isConfirmed = true
+    close(exitCode, true)
+  }
+
+  fun isConfirmed() : Boolean = this.isConfirmed
 }
