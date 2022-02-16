068b6d3d4a6a22e6c8d92c101aa5ede5d9893362
Leonid Shalupov <leonid@shalupov.com>
2022-02-16 19:39:53 +0000

    jps-bootstrap: specify project to load
    this is useful when project home could not be detected from inside the project
    e.g. some projects sharing common community folder
    
    GitOrigin-RevId: bc3c7cb277f89429f7344f88cea9b8cace6c70e7

--- build/full_updater.cmd
+++ build/full_updater.cmd
@@ -4,8 +4,8 @@ GOTO :CMDSCRIPT
 ::CMDLITERAL
 
 set -eux
-exec "$(cd "$(dirname "$0")"; pwd)/../platform/jps-bootstrap/jps-bootstrap.sh" "$@" intellij.idea.community.build FullUpdaterBuildTarget
+exec "$(cd "$(dirname "$0")"; pwd)/../platform/jps-bootstrap/jps-bootstrap.sh" "$@" "$(cd "$(dirname "$0")"; pwd)/.." intellij.idea.community.build FullUpdaterBuildTarget
 :CMDSCRIPT
 
-call "%~dp0\..\platform\jps-bootstrap\jps-bootstrap.cmd" %* intellij.idea.community.build FullUpdaterBuildTarget
+call "%~dp0\..\platform\jps-bootstrap\jps-bootstrap.cmd" %* "%~dp0\.." intellij.idea.community.build FullUpdaterBuildTarget
 EXIT /B %ERRORLEVEL%
--- build/updateFromSources.cmd
+++ build/updateFromSources.cmd
@@ -4,8 +4,8 @@ GOTO :CMDSCRIPT
 ::CMDLITERAL
 
 set -eux
-exec "$(cd "$(dirname "$0")"; pwd)/../platform/jps-bootstrap/jps-bootstrap.sh" "$@" intellij.idea.community.build OpenSourceCommunityUpdateFromSourcesBuildTarget
+exec "$(cd "$(dirname "$0")"; pwd)/../platform/jps-bootstrap/jps-bootstrap.sh" "$@" "$(cd "$(dirname "$0")"; pwd)/.." intellij.idea.community.build OpenSourceCommunityUpdateFromSourcesBuildTarget
 :CMDSCRIPT
 
-call "%~dp0\..\platform\jps-bootstrap\jps-bootstrap.cmd" %* intellij.idea.community.build OpenSourceCommunityUpdateFromSourcesBuildTarget
+call "%~dp0\..\platform\jps-bootstrap\jps-bootstrap.cmd" %* "%~dp0\.." intellij.idea.community.build OpenSourceCommunityUpdateFromSourcesBuildTarget
 EXIT /B %ERRORLEVEL%
