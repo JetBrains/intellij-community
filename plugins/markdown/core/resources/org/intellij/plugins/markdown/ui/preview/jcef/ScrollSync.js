b3c3d43eef74e8ad33a4c9b9e80a828484c51b80
Ivan Posti <ivan.posti@jetbrains.com>
2022-02-16 11:45:22 +0300

    [Markdown][IDEA-256708] Update scroll element search
    GitOrigin-RevId: 583d0f72418a91838eb56fc6df6268a57a1d6474

--- plugins/markdown/core/resources/org/intellij/plugins/markdown/ui/preview/jcef/ScrollSync.js
+++ plugins/markdown/core/resources/org/intellij/plugins/markdown/ui/preview/jcef/ScrollSync.js
@@ -1,31 +1,18 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-function throttle(callback, limit) {
-  let waiting = false;
-  return (...args) => {
-    if (!waiting) {
-      callback(...args);
-      waiting = true;
-      setTimeout(() => {
-        waiting = false;
-      }, limit);
-    }
-  };
-}
-
 class ScrollController {
+  #lastOffset = 0;
+
   constructor() {
     this.positionAttributeName = document.querySelector(`meta[name="markdown-position-attribute-name"]`).content;
-    this.collectMarkdownElements = this._collectMarkdownElements();
-    const scrollHandler = throttle(() => this._scrollHandler(), 20);
-    document.addEventListener('scroll', e => {
-      scrollHandler()
-    })
+    this.collectMarkdownElements = this.#doCollectMarkdownElements();
     IncrementalDOM.notifications.afterPatchListeners.push(() => {
-      this.collectMarkdownElements = this._collectMarkdownElements();
+      this.collectMarkdownElements = this.#doCollectMarkdownElements();
     });
+    const scrollHandler = ScrollController.#throttle(() => this.#scrollHandler(), 20);
+    document.addEventListener('scroll', event => scrollHandler());
   }
 
-  _collectMarkdownElements() {
+  #doCollectMarkdownElements() {
     let elements = null;
     return () => {
       if (elements != null) {
@@ -43,12 +30,12 @@ class ScrollController {
     };
   }
 
-  _scrollHandler() {
+  #scrollHandler() {
     const value = this._getElementsAtOffset(window.scrollY);
     window.__IntelliJTools.messagePipe.post("setScroll", value.previous.from);
   }
 
-  _getNodeOffsets(node) {
+  getNodeOffsets(node) {
     if (!node || !("getAttribute" in node)) {
       return null;
     }
@@ -59,19 +46,55 @@ class ScrollController {
     return null;
   }
 
-  _findElementAtOffset(offset, node = document.body.firstChild, result = {}) {
+  getMaxOffset() {
+    const element = document.body.firstChild;
+    const offsets = this.getNodeOffsets(element);
+    if (!offsets) {
+      throw new Error("First body child is expected to be the root of the document!");
+    }
+    return offsets[1];
+  }
+
+  #findElementAtOffset(offset, node = document.body.firstChild, result = {}) {
     for (let child = node.firstChild; child !== null; child = child.nextSibling) {
-      const position = this._getNodeOffsets(child);
+      if (child.nodeType !== Node.ELEMENT_NODE) {
+        continue;
+      }
+      const position = this.getNodeOffsets(child);
       if (!position) {
         continue;
       }
       if (offset >= position[0] && offset <= position[1]) {
         result.element = child;
-        this._findElementAtOffset(offset, child, result);
+        this.#findElementAtOffset(offset, child, result);
         break;
       }
     }
     return result.element;
+  }
+
+  #actuallyFindElement(offset, forward = false) {
+    const targetElement = this.#findElementAtOffset(offset);
+    if (targetElement) {
+      return targetElement;
+    }
+    if (forward) {
+      const maxOffset = this.getMaxOffset();
+      for (let it = offset; it <= maxOffset; it += 1) {
+        const previousElement = this.#findElementAtOffset(it);
+        if (previousElement) {
+          return previousElement;
+        }
+      }
+    } else {
+      for (let it = offset - 1; it >= 0; it -= 1) {
+        const previousElement = this.#findElementAtOffset(it);
+        if (previousElement) {
+          return previousElement;
+        }
+      }
+    }
+    return null;
   }
 
   _getElementsAtOffset(offset) {
@@ -101,9 +124,15 @@ class ScrollController {
     return { previous: hiElement };
   }
 
+  #doScroll(element, smooth) {
+    this.currentScrollElement.scrollIntoView({
+      behavior: smooth ? "smooth" : "auto"
+    });
+  }
+
   scrollTo(offset, smooth = true) {
     if (this.currentScrollElement) {
-      const position = this._getNodeOffsets(this.currentScrollElement);
+      const position = this.getNodeOffsets(this.currentScrollElement);
       if (offset >= position[0] && offset <= position[1]) {
         return;
       }
@@ -112,19 +141,27 @@ class ScrollController {
     if (!body || !body.firstChild || !body.firstChild.firstChild) {
       return;
     }
-    this.currentScrollElement = this._findElementAtOffset(offset);
-    if (!this.currentScrollElement) {
-      // console.warn(`Failed to find element for offset: ${offset}`);
+    const element = this.#actuallyFindElement(offset, offset >= this.#lastOffset);
+    this.#lastOffset = offset;
+    if (!element) {
+      console.warn(`Failed to find element for offset: ${offset}`);
       return;
     }
-    if (!smooth) {
-      this.currentScrollElement.scrollIntoView();
-    }
-    else {
-      this.currentScrollElement.scrollIntoView({
-        behavior: "smooth"
-      });
-    }
+    this.currentScrollElement = element;
+    this.#doScroll(element, smooth);
+  }
+
+  static #throttle(callback, limit) {
+    let waiting = false;
+    return (...args) => {
+      if (!waiting) {
+        callback(...args);
+        waiting = true;
+        setTimeout(() => {
+          waiting = false;
+        }, limit);
+      }
+    };
   }
 }
 
