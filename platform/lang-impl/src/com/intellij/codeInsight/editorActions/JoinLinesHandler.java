ed4e4a5eabe14a67c3e6e436b8b93f02f309fcb9
Nikolaj Schumacher <nikolaj.schumacher@jetbrains.com>
2022-02-17 15:49:06 +0100

    JoinLinesHandler: Only skip conversion to block comment if next line starts with line comment
    GitOrigin-RevId: 04aa1efc4b3fac998763d90d5b6d15b62457d7bf

--- platform/lang-impl/src/com/intellij/codeInsight/editorActions/JoinLinesHandler.java
+++ platform/lang-impl/src/com/intellij/codeInsight/editorActions/JoinLinesHandler.java
@@ -39,6 +39,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static com.intellij.codeInsight.editorActions.JoinLinesHandlerDelegate.CANNOT_JOIN;
+import static com.intellij.psi.util.PsiUtilCore.getElementType;
 
 public class JoinLinesHandler extends EditorActionHandler.ForEachCaret {
   private static final Logger LOG = Logger.getInstance(JoinLinesHandler.class);
@@ -149,7 +150,7 @@ public class JoinLinesHandler extends EditorActionHandler.ForEachCaret {
             int nextStart = CharArrayUtil.shiftForward(text, myDoc.getLineStartOffset(line + 1), " \t\n");
             if (nextStart < text.length() &&
                 myDoc.getLineNumber(nextStart) <= myLine + lineCount &&
-                getCommentElement(myFile.findElementAt(nextStart)) == null) {
+                !isLineComment(getCommentElement(myFile.findElementAt(nextStart)))) {
               endComments.add(comment);
             }
           }
@@ -165,6 +166,13 @@ public class JoinLinesHandler extends EditorActionHandler.ForEachCaret {
       if (changed) {
         myManager.doPostponedOperationsAndUnblockDocument(myDoc);
       }
+    }
+
+    private static boolean isLineComment(@Nullable PsiComment element) {
+      if (element == null) return false;
+      Commenter commenter = LanguageCommenters.INSTANCE.forLanguage(element.getLanguage());
+      return commenter instanceof CodeDocumentationAwareCommenter &&
+             ((CodeDocumentationAwareCommenter)commenter).getLineCommentTokenType() == getElementType(element);
     }
 
     /**
