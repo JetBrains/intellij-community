4570a239e37e3914da16c853d9610e585547ae07
Bas Leijdekkers <basleijdekkers@gmail.com>
2022-02-18 16:00:25 +0100

    IG: improve warning message (IJPF-CR-4013)
    GitOrigin-RevId: df9d5a071b434d627df450c3400e8e98cdb0319c

--- plugins/InspectionGadgets/test/com/siyeh/igtest/style/unnecessary_interface_modifier/UnnecessaryInterfaceModifierInspection.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/style/unnecessary_interface_modifier/UnnecessaryInterfaceModifierInspection.java
@@ -24,7 +24,7 @@ interface Next {
 class X {
   static <warning descr="Modifier 'transient' is redundant for a 'static' field">transient</warning> String s;
 }
-<warning descr="Modifier 'strictfp' is redundant on Java 17 and higher">strictfp</warning> class Y {
+<warning descr="Modifier 'strictfp' is redundant on Java 17 and later">strictfp</warning> class Y {
 
-  <warning descr="Modifier 'strictfp' is redundant on Java 17 and higher">strictfp</warning> void y() {}
+  <warning descr="Modifier 'strictfp' is redundant on Java 17 and later">strictfp</warning> void y() {}
 }
