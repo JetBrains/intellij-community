b89ac1a5a3beaf7b37161a3e3dc4c83d2d22d4b2
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2022-02-18 15:56:27 +0300

    IDEA-288557 added ability to generate assets without context
    GitOrigin-RevId: 40694aa2a7c8cc2a059838fed96ace47cd5bc613

--- java/idea-ui/src/com/intellij/ide/starters/local/generator/AssetsProcessor.kt
+++ java/idea-ui/src/com/intellij/ide/starters/local/generator/AssetsProcessor.kt
@@ -1,30 +1,33 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.starters.local.generator
 
+import com.intellij.ide.starters.local.*
 import com.intellij.ide.starters.local.GeneratorContext
-import com.intellij.ide.starters.local.GeneratorEmptyDirectory
-import com.intellij.ide.starters.local.GeneratorResourceFile
-import com.intellij.ide.starters.local.GeneratorTemplateFile
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import org.jetbrains.annotations.ApiStatus
 import java.io.IOException
 
-internal class AssetsProcessor {
-  fun generateSources(context: GeneratorContext, templateProps: Map<String, Any>) {
-    val templateProperties = mutableMapOf<String, Any>()
-    templateProperties["context"] = context
-    templateProperties.putAll(templateProps)
 
+@ApiStatus.Experimental
+class AssetsProcessor {
+
+  internal fun generateSources(context: GeneratorContext, templateProperties: Map<String, Any>) {
+    generateSources(context.outputDirectory, context.assets, templateProperties + ("context" to context))
+  }
+
+  fun generateSources(outputDir: VirtualFile, assets: List<GeneratorAsset>, templateProps: Map<String, Any>) {
     val log = logger<AssetsProcessor>()
 
-    for (asset in context.assets) {
+    for (asset in assets) {
       val subPath = if (asset.targetFileName.contains("/"))
         "/" + asset.targetFileName.substringBeforeLast("/")
       else
         ""
 
-      val outputDirectory = VfsUtil.createDirectoryIfMissing(context.outputDirectory.fileSystem, context.outputDirectory.path)
-                            ?: throw IllegalStateException("Unable to create directory ${context.outputDirectory.path}")
+      val outputDirectory = VfsUtil.createDirectoryIfMissing(outputDir.fileSystem, outputDir.path)
+                            ?: throw IllegalStateException("Unable to create directory ${outputDir.path}")
 
       if (asset is GeneratorEmptyDirectory) {
         log.info("Creating empty directory ${asset.targetFileName} in ${outputDirectory.path}")
@@ -36,7 +39,7 @@ internal class AssetsProcessor {
         }
         else {
           VfsUtil.createDirectoryIfMissing(outputDirectory, subPath)
-          ?: throw IllegalStateException("Unable to create directory ${subPath} in ${context.outputDirectory.path}")
+          ?: throw IllegalStateException("Unable to create directory ${subPath} in ${outputDirectory.path}")
         }
 
         val fileName = asset.targetFileName.substringAfterLast("/")
@@ -46,7 +49,7 @@ internal class AssetsProcessor {
           is GeneratorTemplateFile -> {
             val sourceCode: String
             try {
-              sourceCode = asset.template.getText(templateProperties)
+              sourceCode = asset.template.getText(templateProps)
             }
             catch (e: Exception) {
               throw TemplateProcessingException(e)
