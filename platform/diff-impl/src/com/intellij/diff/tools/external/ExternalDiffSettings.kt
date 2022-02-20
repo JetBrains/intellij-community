eb6b2bd60e8c55f26a490c35a642e48863fb71fc
Pavel Gromov <pavel.gromov@jetbrains.com>
2022-02-21 00:07:10 +0300

    [diff] Implement migration of old external diff settings to the new UI (IDEA-289183)
    GitOrigin-RevId: 0f3e2df2de3574a53159a78974c05ad83afda75f

--- platform/diff-impl/src/com/intellij/diff/tools/external/ExternalDiffSettings.kt
+++ platform/diff-impl/src/com/intellij/diff/tools/external/ExternalDiffSettings.kt
@@ -12,8 +12,12 @@ import org.jetbrains.annotations.NonNls
 class ExternalDiffSettings : BaseState(), PersistentStateComponent<ExternalDiffSettings> {
   override fun getState(): ExternalDiffSettings = this
   override fun loadState(state: ExternalDiffSettings) {
+    migrateOldSettings(state)
     copyFrom(state)
   }
+
+  @get:OptionTag("MIGRATE_OLD_SETTINGS")
+  var isSettingsMigrated by property(false)
 
   @get:OptionTag("EXTERNAL_TOOLS_ENABLED")
   var isExternalToolsEnabled: Boolean by property(false)
@@ -57,6 +61,52 @@ class ExternalDiffSettings : BaseState(), PersistentStateComponent<ExternalDiffS
     var isMergeTrustExitCode: Boolean = false,
     var groupName: ExternalToolGroup = ExternalToolGroup.DIFF_TOOL
   )
+
+  // OLD SETTINGS AREA
+  @get:OptionTag("DIFF_ENABLED")
+  var isDiffEnabled: Boolean by property(false)
+
+  @get:OptionTag("DIFF_EXE_PATH")
+  var diffExePath: String by nonNullString()
+
+  @get:OptionTag("DIFF_PARAMETERS")
+  var diffParameters: String by nonNullString("%1 %2 %3")
+
+  @get:OptionTag("MERGE_ENABLED")
+  var isMergeEnabled: Boolean by property(false)
+
+  @get:OptionTag("MERGE_EXE_PATH")
+  var mergeExePath: String by nonNullString()
+
+  @get:OptionTag("MERGE_PARAMETERS")
+  var mergeParameters: String by nonNullString("%1 %2 %3 %4")
+
+  @get:OptionTag("MERGE_TRUST_EXIT_CODE")
+  var isMergeTrustExitCode: Boolean by property(false)
+  // ^^^ OLD SETTINGS AREA
+
+  private fun nonNullString(initialValue: String = "") = property(initialValue) { it == initialValue }
+
+  private fun migrateOldSettings(state: ExternalDiffSettings) {
+    if (!state.isSettingsMigrated) {
+      // load old settings
+      state.isExternalToolsEnabled = isDiffEnabled
+      val oldDiffTool = ExternalTool(state.diffExePath, state.diffExePath, state.diffParameters,
+                                     false, ExternalToolGroup.DIFF_TOOL)
+      val oldMergeTool = ExternalTool(state.mergeExePath, state.mergeExePath, state.mergeParameters,
+                                      state.isMergeTrustExitCode, ExternalToolGroup.MERGE_TOOL)
+      state.externalTools[ExternalToolGroup.DIFF_TOOL] = listOf(oldDiffTool)
+      state.externalTools[ExternalToolGroup.MERGE_TOOL] = listOf(oldMergeTool)
+
+      // save old settings
+      state.defaultToolConfiguration = defaultToolConfiguration.apply {
+        if (state.isDiffEnabled && diffExePath.isNotEmpty()) diffToolName = oldDiffTool.name
+        if (state.isMergeEnabled && mergeExePath.isNotEmpty()) mergeToolName = oldMergeTool.name
+      }
+
+      state.isSettingsMigrated = true
+    }
+  }
 
   companion object {
     @JvmStatic
