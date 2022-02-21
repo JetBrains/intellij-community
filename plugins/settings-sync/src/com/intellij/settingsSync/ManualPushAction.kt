606978fd6c8c6330f34623a4b8033694729a9d98
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2022-02-21 17:00:29 +0300

    settings-sync: cleanup: use EventListener instead of MessageBus for internal settingsChanged events
    With all its advantages, the MessageBus has a downside since the event delivery can fail due to an unrelated failure happened while delivering another event in the queue. Delivering settings events is critical, so a more reliable method is to be preferred. Since the whole event communication is internal for the settings-sync plugin, it is easy to use a simple EventListener.
    
    GitOrigin-RevId: 57346014d18d343cfe514123d089cca93648084c

--- plugins/settings-sync/src/com/intellij/settingsSync/ManualPushAction.kt
+++ plugins/settings-sync/src/com/intellij/settingsSync/ManualPushAction.kt
@@ -1,13 +1,12 @@
 package com.intellij.settingsSync
 
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.DumbAwareAction
 
 class ManualPushAction : DumbAwareAction() {
 
   override fun actionPerformed(e: AnActionEvent) {
-    ApplicationManager.getApplication().messageBus.syncPublisher(SETTINGS_CHANGED_TOPIC).settingChanged(SyncSettingsEvent.MustPushRequest)
+    SettingsSyncEvents.getInstance().fireSettingsChanged(SyncSettingsEvent.MustPushRequest)
   }
 
   override fun update(e: AnActionEvent) {
