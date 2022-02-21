5b63e2f8d1541daf50a2de8e44c8767a1f242431
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2022-02-21 21:59:43 +0900

    [kotlin] Update test data for 'testTopLevelMultifileRuntime()'
    Descriptor renderer now adds backticks for names that aren't valid identifiers (see KT-51248). This commit adapts tests in the IDE plugin, so they pass.
    
    GitOrigin-RevId: 91543e715e75b7ebd5c32045b6f5dc2f820bcc53

--- plugins/kotlin/idea/tests/testData/checker/duplicateJvmSignature/functionAndProperty/topLevelMultifileRuntime.kt
+++ plugins/kotlin/idea/tests/testData/checker/duplicateJvmSignature/functionAndProperty/topLevelMultifileRuntime.kt
@@ -6,8 +6,8 @@
 package test
 
 <error descr="[CONFLICTING_JVM_DECLARATIONS] Platform declaration clash: The following declarations have the same JVM signature (getX()I):
-    fun <get-x>(): Int defined in test in file topLevelMultifileRuntime.kt
+    fun `<get-x>`(): Int defined in test in file topLevelMultifileRuntime.kt
     fun getX(): Int defined in test in file topLevelMultifileRuntime.kt">val x</error> = 1
 <error descr="[CONFLICTING_JVM_DECLARATIONS] Platform declaration clash: The following declarations have the same JVM signature (getX()I):
-    fun <get-x>(): Int defined in test in file topLevelMultifileRuntime.kt
+    fun `<get-x>`(): Int defined in test in file topLevelMultifileRuntime.kt
     fun getX(): Int defined in test in file topLevelMultifileRuntime.kt">fun getX()</error> = 1
