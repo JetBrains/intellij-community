9f8367efee3366b9c59bb7b9b113c1b809d73b73
Alexander Dudinsky <Alexander.Dudinsky@jetbrains.com>
2022-02-21 17:12:23 +0300

    Change version of gradle plugin from 1.5.20-RC-238 to 1.5.20
    GitOrigin-RevId: f7f3aacc44f1ed657c83cb547ec985b95de7b988

--- plugins/kotlin/gradle/gradle-idea/test/org/jetbrains/kotlin/idea/codeInsight/gradle/MultiplePluginVersionGradleImportingTestCase.kt
+++ plugins/kotlin/gradle/gradle-idea/test/org/jetbrains/kotlin/idea/codeInsight/gradle/MultiplePluginVersionGradleImportingTestCase.kt
@@ -98,7 +98,7 @@ abstract class MultiplePluginVersionGradleImportingTestCase : KotlinGradleImport
                 arrayOf("6.7.1", "1.4.0"),
                 arrayOf("6.8.2", "1.4.32"),
                 arrayOf("7.0.2", "1.5.10"),
-                arrayOf("7.0.2", "1.5.20-RC-238"),
+                arrayOf("7.0.2", "1.5.20"),
                 arrayOf("6.8.2", "master"),
                 arrayOf("7.0.2", "master")
             )
