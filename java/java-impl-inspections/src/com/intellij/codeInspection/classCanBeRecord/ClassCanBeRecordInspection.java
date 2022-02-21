56b0ff6dee14d10b8d73a712de6f01eea1166071
Ilyas Selimov <ilyas.selimov@jetbrains.com>
2022-02-21 16:54:50 +0700

    IDEA-272274 [class to record conversion] - add annotations filter to ignore conversion
    GitOrigin-RevId: 79737b0440b33c18a7d64cf2b99f259503fd2056

--- java/java-impl-inspections/src/com/intellij/codeInspection/classCanBeRecord/ClassCanBeRecordInspection.java
+++ java/java-impl-inspections/src/com/intellij/codeInspection/classCanBeRecord/ClassCanBeRecordInspection.java
@@ -1,15 +1,21 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection.classCanBeRecord;
 
-import com.intellij.codeInsight.daemon.impl.UnusedSymbolUtil;
 import com.intellij.codeInsight.daemon.impl.analysis.HighlightingFeature;
+import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.classCanBeRecord.ConvertToRecordFix.RecordCandidate;
 import com.intellij.codeInspection.ui.InspectionOptionsPanel;
 import com.intellij.codeInspection.util.InspectionMessage;
+import com.intellij.codeInspection.util.SpecialAnnotationsUtil;
 import com.intellij.java.JavaBundle;
+import com.intellij.openapi.project.Project;
+import com.intellij.profile.codeInspection.ProjectInspectionProfileManager;
+import com.intellij.psi.PsiAnnotation;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiIdentifier;
+import com.intellij.util.ObjectUtils;
+import com.intellij.util.SmartList;
 import com.intellij.util.ui.CheckBox;
 import com.intellij.util.ui.JBUI;
 import com.siyeh.ig.BaseInspection;
@@ -18,20 +24,27 @@ import com.siyeh.ig.InspectionGadgetsFix;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.TestOnly;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClassCanBeRecordInspection extends BaseInspection {
-  @NotNull
-  public ConversionStrategy myConversionStrategy = ConversionStrategy.SHOW_AFFECTED_MEMBERS;
-  public boolean suggestAccessorsRenaming = true;
+  private static final List<String> IGNORED_ANNOTATIONS = List.of("io.micronaut.*", "jakarta.*", "javax.*", "org.springframework.*");
+
+  public @NotNull ConversionStrategy myConversionStrategy = ConversionStrategy.SHOW_AFFECTED_MEMBERS;
+  public boolean mySuggestAccessorsRenaming = true;
+  public List<String> myIgnoredAnnotations = new ArrayList<>();
 
   public ClassCanBeRecordInspection() {
+    myIgnoredAnnotations.addAll(IGNORED_ANNOTATIONS);
   }
 
+  @TestOnly
   public ClassCanBeRecordInspection(@NotNull ConversionStrategy conversionStrategy, boolean suggestAccessorsRenaming) {
     myConversionStrategy = conversionStrategy;
-    this.suggestAccessorsRenaming = suggestAccessorsRenaming;
+    this.mySuggestAccessorsRenaming = suggestAccessorsRenaming;
   }
 
   @Override
@@ -46,7 +59,8 @@ public class ClassCanBeRecordInspection extends BaseInspection {
 
   @Override
   public BaseInspectionVisitor buildVisitor() {
-    return new ClassCanBeRecordVisitor(myConversionStrategy != ConversionStrategy.DO_NOT_SUGGEST, suggestAccessorsRenaming);
+    return new ClassCanBeRecordVisitor(myConversionStrategy != ConversionStrategy.DO_NOT_SUGGEST, mySuggestAccessorsRenaming,
+                                       myIgnoredAnnotations);
   }
 
   @Override
@@ -55,15 +69,34 @@ public class ClassCanBeRecordInspection extends BaseInspection {
   }
 
   @Override
-  protected @Nullable InspectionGadgetsFix buildFix(Object... infos) {
-    return new ConvertToRecordFix(myConversionStrategy == ConversionStrategy.SHOW_AFFECTED_MEMBERS, suggestAccessorsRenaming);
+  protected InspectionGadgetsFix @NotNull [] buildFixes(Object... infos) {
+    List<InspectionGadgetsFix> fixes = new SmartList<>();
+    fixes.add(new ConvertToRecordFix(myConversionStrategy == ConversionStrategy.SHOW_AFFECTED_MEMBERS, mySuggestAccessorsRenaming,
+                                     myIgnoredAnnotations));
+    boolean isOnTheFly = (boolean)infos[0];
+    if (isOnTheFly) {
+      PsiClass psiClass = ObjectUtils.tryCast(infos[1], PsiClass.class);
+      if (psiClass != null) {
+        PsiAnnotation[] annotations = psiClass.getAnnotations();
+        // rare corner case: we don't want to make a quick-fix list too wide
+        if (annotations.length > 0 && annotations.length < 4) {
+          for (PsiAnnotation annotation : annotations) {
+            String fqn = annotation.getQualifiedName();
+            if (fqn != null) {
+              fixes.add(new AddIgnoredAnnotationFix(fqn, myIgnoredAnnotations));
+            }
+          }
+        }
+      }
+    }
+    return fixes.toArray(InspectionGadgetsFix.EMPTY_ARRAY);
   }
 
   @Override
   public @Nullable JComponent createOptionsPanel() {
     JPanel panel = new InspectionOptionsPanel();
     panel.add(new CheckBox(JavaBundle.message("class.can.be.record.suggest.renaming.accessors"), this,
-                           "suggestAccessorsRenaming"));
+                           "mySuggestAccessorsRenaming"));
 
     panel.add(new JLabel(JavaBundle.message("class.can.be.record.conversion.weakens.member")));
     ButtonGroup butGr = new ButtonGroup();
@@ -75,16 +108,24 @@ public class ClassCanBeRecordInspection extends BaseInspection {
       panel.add(radioBut);
     }
 
+    JPanel annotationsPanel = SpecialAnnotationsUtil.createSpecialAnnotationsListControl(
+      myIgnoredAnnotations, JavaBundle.message("class.can.be.record.suppress.conversion.if.annotated"), true);
+    panel.add(annotationsPanel, "growx, wrap");
+
     return panel;
   }
 
   private static class ClassCanBeRecordVisitor extends BaseInspectionVisitor {
     private final boolean myRenameIfWeakenVisibility;
     private final boolean mySuggestAccessorsRenaming;
+    private final List<String> myIgnoredAnnotations;
 
-    private ClassCanBeRecordVisitor(boolean renameIfWeakenVisibility, boolean suggestAccessorsRenaming) {
+    private ClassCanBeRecordVisitor(boolean renameIfWeakenVisibility,
+                                    boolean suggestAccessorsRenaming,
+                                    @NotNull List<String> ignoredAnnotations) {
       myRenameIfWeakenVisibility = renameIfWeakenVisibility;
       mySuggestAccessorsRenaming = suggestAccessorsRenaming;
+      myIgnoredAnnotations = ignoredAnnotations;
     }
 
     @Override
@@ -92,11 +133,10 @@ public class ClassCanBeRecordInspection extends BaseInspection {
       super.visitClass(aClass);
       PsiIdentifier classIdentifier = aClass.getNameIdentifier();
       if (classIdentifier == null) return;
-      RecordCandidate recordCandidate = ConvertToRecordFix.getClassDefinition(aClass, mySuggestAccessorsRenaming);
+      RecordCandidate recordCandidate = ConvertToRecordFix.getClassDefinition(aClass, mySuggestAccessorsRenaming, myIgnoredAnnotations);
       if (recordCandidate == null) return;
       if (!myRenameIfWeakenVisibility && !ConvertToRecordProcessor.findWeakenVisibilityUsages(recordCandidate).isEmpty()) return;
-      if (UnusedSymbolUtil.isImplicitUsage(aClass.getProject(), aClass)) return;
-      registerError(classIdentifier);
+      registerError(classIdentifier, isOnTheFly(), aClass);
     }
   }
 
@@ -115,6 +155,32 @@ public class ClassCanBeRecordInspection extends BaseInspection {
     @Nls
     String getMessage() {
       return JavaBundle.message(messageKey);
+    }
+  }
+
+  private static class AddIgnoredAnnotationFix extends InspectionGadgetsFix {
+    private final @NotNull String myPackagePrefix;
+    private final @NotNull List<String> myIgnoredPackagePrefixes;
+
+    private AddIgnoredAnnotationFix(@NotNull String packagePrefix, @NotNull List<String> ignoredPackagePrefixes) {
+      myPackagePrefix = packagePrefix;
+      myIgnoredPackagePrefixes = ignoredPackagePrefixes;
+    }
+
+    @Override
+    public @NotNull String getName() {
+      return JavaBundle.message("class.can.be.record.suppress.conversion.if.annotated.fix.name", myPackagePrefix);
+    }
+
+    @Override
+    public @NotNull String getFamilyName() {
+      return JavaBundle.message("class.can.be.record.suppress.conversion.if.annotated.fix.family.name");
+    }
+
+    @Override
+    protected void doFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+      myIgnoredPackagePrefixes.add(myPackagePrefix);
+      ProjectInspectionProfileManager.getInstance(project).fireProfileChanged();
     }
   }
 }
