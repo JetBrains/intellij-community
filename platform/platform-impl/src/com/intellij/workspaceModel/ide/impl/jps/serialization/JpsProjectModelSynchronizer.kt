d45e8a5ed47faee16280a63118cc8dac8108e01c
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2022-02-22 14:23:59 +0700

    Revert "IDEA-288703 Adjust fix to handle existent yet empty module list"
    This reverts commit 608f0f5d98ca02c438a6dc1d5a28d74f2d257bdc.
    
    GitOrigin-RevId: 6357efea5452aaab7bab3573c73f3b78fea51e81

--- platform/platform-impl/src/com/intellij/workspaceModel/ide/impl/jps/serialization/JpsProjectModelSynchronizer.kt
+++ platform/platform-impl/src/com/intellij/workspaceModel/ide/impl/jps/serialization/JpsProjectModelSynchronizer.kt
@@ -28,6 +28,7 @@ import com.intellij.openapi.vfs.newvfs.events.VFileCreateEvent
 import com.intellij.openapi.vfs.newvfs.events.VFileDeleteEvent
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
 import com.intellij.project.stateStore
+import com.intellij.util.PlatformUtils
 import com.intellij.workspaceModel.ide.*
 import com.intellij.workspaceModel.ide.impl.WorkspaceModelImpl
 import com.intellij.workspaceModel.ide.impl.WorkspaceModelInitialTestContent
@@ -232,7 +233,11 @@ class JpsProjectModelSynchronizer(private val project: Project) : Disposable {
   }
 
   // IDEA-288703
-  fun hasNoSerializedModules(): Boolean = (prepareSerializers() as JpsProjectSerializersImpl).moduleSerializers.isEmpty()
+  fun isModuleListMissing(): Boolean {
+    val currentSerializers = prepareSerializers() as JpsProjectSerializersImpl
+    if (currentSerializers.moduleSerializers.isNotEmpty()) return false
+    return currentSerializers.moduleListSerializersByUrl.keys.all { !JpsPathUtil.urlToFile(it).exists() }
+  }
 
   private fun prepareSerializers(): JpsProjectSerializers {
     val existingSerializers = this.serializers.get()
