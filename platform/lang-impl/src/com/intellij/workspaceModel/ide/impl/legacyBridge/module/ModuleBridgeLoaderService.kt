93a4e24bd36a4429550f8b7b714e8d6551f5ca74
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2022-02-22 14:23:59 +0700

    Revert "IDEA-288703 Adjust fix to handle existent yet empty module list"
    This reverts commit 608f0f5d98ca02c438a6dc1d5a28d74f2d257bdc.
    
    (cherry picked from commit 6357efea5452aaab7bab3573c73f3b78fea51e81)
    
    IJ-CR-21160
    
    GitOrigin-RevId: 68a43223941be1deb584de7865362b32fc2ab34e

--- platform/lang-impl/src/com/intellij/workspaceModel/ide/impl/legacyBridge/module/ModuleBridgeLoaderService.kt
+++ platform/lang-impl/src/com/intellij/workspaceModel/ide/impl/legacyBridge/module/ModuleBridgeLoaderService.kt
@@ -32,10 +32,7 @@ internal class ModuleBridgeLoaderService(private val project: Project) {
       val workspaceModel = WorkspaceModel.getInstance(project) as WorkspaceModelImpl
       val projectModelSynchronizer = JpsProjectModelSynchronizer.getInstance(project)
       if (projectModelSynchronizer != null) {
-        if (projectModelSynchronizer.hasNoSerializedModules()) {
-          LOG.warn("No serialized modules found. Workspace model cache will be ignored, project structure will be recreated.")
-          workspaceModel.ignoreCache()
-        }
+        if (projectModelSynchronizer.isModuleListMissing()) workspaceModel.ignoreCache()
         if (!workspaceModel.loadedFromCache) {
           LOG.info("Workspace model loaded without cache. Loading real project state into workspace model. ${Thread.currentThread()}")
           activity = StartUpMeasurer.startActivity("modules loading without cache", ActivityCategory.DEFAULT)
