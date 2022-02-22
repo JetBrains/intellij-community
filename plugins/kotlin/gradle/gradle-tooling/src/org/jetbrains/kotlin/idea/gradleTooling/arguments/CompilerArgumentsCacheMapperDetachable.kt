2c8a375a15764738d8d0076420a45e4cb47b4925
Yaroslav Chernyshev <Yaroslav.Chernyshev@jetbrains.com>
2022-02-22 11:27:31 +0300

    [import] Rollback unstable cross-project compiler arguments caching
    In some complicated environments, like WSL or remote development, distributed
    compiler arguments caching infrastructure works unstable. This commit leaves
    compiler arguments caching only within each model.
    
    (cherry picked from commit a49a4cbd11123183d9a6dc22f3da89cd84ce2d74)
    
    GitOrigin-RevId: ec4ec04d2afcf5e3b904590a01de181d2ef335dd

--- plugins/kotlin/gradle/gradle-tooling/src/org/jetbrains/kotlin/idea/gradleTooling/arguments/CompilerArgumentsCacheMapperDetachable.kt
+++ plugins/kotlin/gradle/gradle-tooling/src/org/jetbrains/kotlin/idea/gradleTooling/arguments/CompilerArgumentsCacheMapperDetachable.kt
@@ -9,47 +9,9 @@ interface CompilerArgumentsMapperDetachable : CompilerArgumentsCacheMapper {
     fun detachCacheAware(): CompilerArgumentsCacheAware
 }
 
-class CompilerArgumentsMapperDetachableFallback : AbstractCompilerArgumentsCacheMapper(), CompilerArgumentsMapperDetachable {
+class CompilerArgumentsMapperDetachableImpl : AbstractCompilerArgumentsCacheMapper(), CompilerArgumentsMapperDetachable {
     override fun detachCacheAware(): CompilerArgumentsCacheAware =
         CompilerArgumentsCacheAwareImpl(cacheOriginIdentifier, HashMap(cacheByValueMap))
 
     override val cacheOriginIdentifier: Long by lazy { RandomUtils.nextLong() }
-    override val offset: Int = 0
-}
-
-class CompilerArgumentsMapperDetachableImpl(
-    private val masterCacheMapper: AbstractCompilerArgumentsCacheMapper,
-) : AbstractCompilerArgumentsCacheMapper(), CompilerArgumentsMapperDetachable {
-
-    override val offset: Int by lazy {
-        masterCacheMapper.cacheByValueMap.keys.toIntArray().sortedArray().lastOrNull()?.plus(1) ?: 0
-    }
-
-    override fun cacheArgument(arg: String): Int {
-        return if (masterCacheMapper.checkCached(arg)) {
-            val key = masterCacheMapper.cacheArgument(arg)
-            cacheByValueMap[key] = arg
-            valueByCacheMap[arg] = key
-            key
-        } else super.cacheArgument(arg)
-    }
-
-    override val cacheOriginIdentifier: Long by lazy {
-        masterCacheMapper.cacheOriginIdentifier
-    }
-
-    override fun detachCacheAware(): CompilerArgumentsCacheAware {
-        val uniqueKeys = cacheByValueMap.keys - masterCacheMapper.distributeCacheIds().toSet()
-        uniqueKeys.forEach {
-            val key = it
-            val value = cacheByValueMap.getValue(it)
-            masterCacheMapper.cacheByValueMap[key] = value
-            masterCacheMapper.valueByCacheMap[value] = key
-        }
-
-        return CompilerArgumentsCacheAwareImpl(
-            cacheOriginIdentifier,
-            HashMap(uniqueKeys.associateWith { cacheByValueMap.getValue(it) })
-        )
-    }
 }
