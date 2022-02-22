f407b790f9bf5820c4cdd7b1ff28ffaf14420e85
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-02-22 20:11:23 +0700

    [fus] library usage collector: rework to counter collector
    ^KTIJ-21195 Fixed
    
    GitOrigin-RevId: aad60fa1518b37f379aa80b903a40cfc6e851c98

--- java/java-impl/src/com/intellij/internal/statistic/libraryUsage/LibraryUsageCollector.kt
+++ java/java-impl/src/com/intellij/internal/statistic/libraryUsage/LibraryUsageCollector.kt
@@ -1,33 +1,30 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.internal.statistic.libraryUsage
 
-import com.intellij.internal.statistic.beans.MetricEvent
-import com.intellij.internal.statistic.eventLog.FeatureUsageData
+import com.intellij.internal.statistic.eventLog.EventLogGroup
 import com.intellij.internal.statistic.eventLog.events.EventFields
-import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector
+import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
+import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.project.Project
 
-internal class LibraryUsageCollector : ProjectUsagesCollector() {
-  override fun getGroupId(): String = "libraryUsage"
-  override fun getVersion(): Int = 1
+internal class LibraryUsageCollector : CounterUsagesCollector() {
+  override fun getGroup(): EventLogGroup = GROUP
 
-  override fun getMetrics(project: Project): Set<MetricEvent> {
-    return LibraryUsageStatisticsStorageService.getInstance(project)
-      .getStatisticsAndResetState()
-      .mapNotNullTo(mutableSetOf()) { (usageInfo, count) ->
-        val libraryName = usageInfo.name ?: return@mapNotNullTo null
-        val libraryVersion = usageInfo.version ?: return@mapNotNullTo null
-        val libraryFileType = usageInfo.fileTypeName ?: return@mapNotNullTo null
+  companion object {
+    private val GROUP = EventLogGroup("libraryUsage", 2)
+    private val EVENT = GROUP.registerEvent(
+      eventId = "library_used",
+      eventField1 = EventFields.String("library_name", emptyList()), // TODO: workaround. Fix after IDEA-279202
+      eventField2 = EventFields.Version,
+      eventField3 = EventFields.FileType,
+    )
 
-        val data = FeatureUsageData().apply {
-          addData("library_name", libraryName)
-          addVersionByString(libraryVersion)
-          addData(EventFields.FileType.name, libraryFileType)
-          addCount(count)
-          addProject(project)
-        }
-
-        MetricEvent("library_used", data)
-      }
+    fun log(project: Project, libraryUsage: LibraryUsage) {
+      EVENT.log(project = project, value1 = libraryUsage.name, value2 = libraryUsage.version, value3 = libraryUsage.fileType)
+    }
   }
+}
+
+class LibraryUsage(val name: String, val version: String, val fileType: FileType) {
+  override fun toString(): String = "$name-$version for ${fileType.name}"
 }
