0044d2654495f364438dd8d4bf657b4bd878ebd1
Alexander.Kass <Alexander.Kass@jetbrains.com>
2022-02-22 15:36:53 +0300

    dbvfs: fix local history (fix vcs:// files)
    (cherry picked from commit b259a3928d21f99c68ea022981e6d2d445a57cc5)
    
    IJ-CR-21332
    
    GitOrigin-RevId: 1ca0cb3c3bf211c7b39744630ccff03af850a47a

--- platform/lvcs-impl/src/com/intellij/history/integration/IdeaGateway.java
+++ platform/lvcs-impl/src/com/intellij/history/integration/IdeaGateway.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.history.integration;
 
-import com.intellij.history.VersionedFileSystem;
 import com.intellij.history.core.LocalHistoryFacade;
 import com.intellij.history.core.Paths;
 import com.intellij.history.core.StoredContent;
