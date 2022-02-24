46d15113f861ccad9d88430936cceddedb4920ef
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2022-02-24 12:18:42 +0300

    [plugins] logging: print stacktrace to idea.log if plugin installation fails (IDEA-289597)
    GitOrigin-RevId: 9abd91036282773ec50f0e2989a33f4e45ff18e5

--- platform/platform-impl/src/com/intellij/ide/plugins/PluginInstallOperation.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/PluginInstallOperation.java
@@ -199,6 +199,7 @@ public final class PluginInstallOperation {
       }
       catch (IOException e) {
         String title = IdeBundle.message("title.plugin.error");
+        LOG.warn(e);
         NotificationGroup group = NotificationGroupManager.getInstance().getNotificationGroup("Plugin Error");
         Notifications.Bus.notify(group.createNotification(title, pluginNode.getName() + ": " + e.getMessage(), NotificationType.ERROR));
         return false;
