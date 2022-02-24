0a8935faf196c9071aecaadc5df18006b9248a5b
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-02-24 15:24:49 +0300

    vcs: cleanup
    GitOrigin-RevId: 121e3a3a1ac8a94898caadf05cba46d5df7d5bd5

--- plugins/git4idea/src/git4idea/GitBranchesUsageCollector.kt
+++ plugins/git4idea/src/git4idea/GitBranchesUsageCollector.kt
@@ -1,5 +1,4 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea
 
 import com.intellij.internal.statistic.IdeActivityDefinition
