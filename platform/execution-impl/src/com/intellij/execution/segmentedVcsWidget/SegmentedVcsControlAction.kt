3e85f3afca59f84324ab0f1e672e47de40639f27
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-02-24 15:24:49 +0300

    vcs: cleanup
    GitOrigin-RevId: b713ff9947a3e7d2e6e027a3f7ba87a5786d1ed1

--- platform/execution-impl/src/com/intellij/execution/segmentedVcsWidget/SegmentedVcsControlAction.kt
+++ platform/execution-impl/src/com/intellij/execution/segmentedVcsWidget/SegmentedVcsControlAction.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.execution.segmentedVcsWidget // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.execution.segmentedVcsWidget
 
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.impl.segmentedActionBar.SegmentedActionToolbarComponent
