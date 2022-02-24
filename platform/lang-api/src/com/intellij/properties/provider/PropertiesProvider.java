2a2df43e0dccbc12cf1fc99b04bd61bb98aeeabb
Olga Klisho <Olga.Klisho@jetbrains.com>
2022-02-24 14:41:56 +0300

    [java] (IDEA-277685) review fixes
    (cherry picked from commit 7625a27dc2984105113414930dccd84247c61aa2)
    
    GitOrigin-RevId: 0c7f577dbe5cae4c8d2c3637ad6129ad39bba61e

--- platform/lang-api/src/com/intellij/properties/provider/PropertiesProvider.java
+++ platform/lang-api/src/com/intellij/properties/provider/PropertiesProvider.java
@@ -1,13 +1,14 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.codeInspection.resources;
+package com.intellij.properties.provider;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.psi.search.GlobalSearchScope;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.openapi.module.Module;
 
-public interface TestClassPropertyProvider {
-  ExtensionPointName<TestClassPropertyProvider>
+public interface PropertiesProvider {
+  ExtensionPointName<PropertiesProvider>
     EP_NAME = ExtensionPointName.create("com.intellij.properties.files.provider");
 
-  boolean hasTestClassProperty(@NotNull Module classModule);
+  boolean hasProperty(@NotNull Module classModule, @NotNull String propertyKey, @NotNull String propertyValue, @NotNull GlobalSearchScope scope);
 }
