0313483f17187fff274dd93a338f204350052261
Olga Klisho <Olga.Klisho@jetbrains.com>
2022-02-24 14:41:56 +0300

    [java] (IDEA-277685) review fixes
    GitOrigin-RevId: 6540a3b3f7c4e52b66f6dc6a326d553122b7b622

--- plugins/properties/properties-psi-api/src/com/intellij/lang/properties/provider/JunitPropertiesProvider.java
+++ plugins/properties/properties-psi-api/src/com/intellij/lang/properties/provider/JunitPropertiesProvider.java
@@ -1,27 +1,28 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.testutils;
+package com.intellij.lang.properties.provider;
 
 import com.intellij.lang.properties.psi.Property;
 import com.intellij.lang.properties.psi.PropertyKeyIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.containers.ContainerUtil;
-import com.intellij.codeInspection.resources.TestClassPropertyProvider;
+import com.intellij.properties.provider.PropertiesProvider;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.openapi.module.Module;
 
 import java.util.Collection;
 
-public class JunitTestClassPropertyProvider implements TestClassPropertyProvider {
-  private static final String PER_CLASS_PROPERTY_KEY = "junit.jupiter.testinstance.lifecycle.default";
+public class JunitPropertiesProvider implements PropertiesProvider {
   @Override
-  public boolean hasTestClassProperty(@NotNull Module classModule) {
-    final GlobalSearchScope globalSearchScope = GlobalSearchScope.moduleRuntimeScope(classModule, true);
+  public boolean hasProperty(@NotNull Module classModule,
+                             @NotNull String propertyKey,
+                             @NotNull String propertyValue,
+                             @NotNull GlobalSearchScope scope) {
     Collection<Property> property =
-      PropertyKeyIndex.getInstance().get(PER_CLASS_PROPERTY_KEY, classModule.getProject(), globalSearchScope);
+      PropertyKeyIndex.getInstance().get(propertyKey, classModule.getProject(), scope);
     if (property == null) return false;
     Property item = ContainerUtil.getFirstItem(property);
     if (item == null) return false;
     String value = item.getValue();
-    return "PER_CLASS".equalsIgnoreCase(value);
+    return propertyValue.equalsIgnoreCase(value);
   }
 }
