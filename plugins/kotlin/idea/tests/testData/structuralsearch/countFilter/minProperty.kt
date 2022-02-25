b61bf37d05d564e03a5687257d98093e606d4a5c
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2022-02-25 14:53:52 +0100

    [kssr] Refactor count filter test
    GitOrigin-RevId: 15aee5436572dc6c3f3a3735e969c3aea8bf3361

--- plugins/kotlin/idea/tests/testData/structuralsearch/countFilter/minProperty.kt
+++
@@ -1,6 +0,0 @@
-class A {
-    <warning descr="SSR">lateinit var x: String</warning>
-    var y = 1
-
-    fun init() { x = "a" }
-}
--- plugins/kotlin/idea/tests/testData/structuralsearch/countFilter/minWhenExpression.kt
+++
@@ -1,8 +0,0 @@
-fun foo() {
-    print(<warning descr="SSR">when (1) {
-        in 0..3 -> 2
-        else -> 3
-    }</warning>)
-    print(<warning descr="SSR">when { else -> true }</warning>)
-}
-
