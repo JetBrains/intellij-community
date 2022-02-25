59cd1293c9a0701616922838db4a3faa3fa122ea
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2022-02-25 08:41:00 +0300

    kotlin: rewrite and simplify kotlin subpackage index (use a single query to get subpackages, avoid special cache for all subpackages)
    GitOrigin-RevId: 9b087c31eb621f39f4dc414b0942fbcb2ad8accc

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/stubindex/PackageIndexUtil.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/stubindex/PackageIndexUtil.kt
@@ -3,38 +3,12 @@
 package org.jetbrains.kotlin.idea.stubindex
 
 import com.intellij.openapi.project.Project
-import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.search.GlobalSearchScope
-import org.jetbrains.kotlin.idea.vfilefinder.KotlinPartialPackageNamesIndex
-import org.jetbrains.kotlin.idea.refactoring.fqName.getKotlinFqName
-import org.jetbrains.kotlin.idea.vfilefinder.hasSomethingInPackage
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.platform.TargetPlatform
-import org.jetbrains.kotlin.platform.jvm.isJvm
 import org.jetbrains.kotlin.psi.KtFile
 
 object PackageIndexUtil {
-    fun getKotlinSubPackageFqNames(
-        packageFqName: FqName,
-        scope: GlobalSearchScope,
-        project: Project,
-        targetPlatform: TargetPlatform,
-        nameFilter: (Name) -> Boolean
-    ) = sequence {
-        if (targetPlatform.isJvm()) {
-            val javaPackage = JavaPsiFacade.getInstance(project).findPackage(packageFqName.asString())
-            if (javaPackage != null) {
-                for (psiPackage in javaPackage.getSubPackages(scope)) {
-                    val fqName = psiPackage.getKotlinFqName() ?: continue
-                    if (nameFilter(fqName.shortName())) {
-                        yield(fqName)
-                    }
-                }
-            }
-        }
-    }
-
     @JvmStatic
     fun getSubPackageFqNames(
         packageFqName: FqName,
@@ -56,9 +30,4 @@ object PackageIndexUtil {
         searchScope: GlobalSearchScope,
         project: Project
     ): Boolean = SubpackagesIndexService.getInstance(project).packageExists(packageFqName, searchScope)
-
-    @JvmStatic
-    fun containsFilesWithPartialPackage(partialFqName: FqName, searchScope: GlobalSearchScope): Boolean =
-        KotlinPartialPackageNamesIndex.hasSomethingInPackage(partialFqName, searchScope)
-
 }
