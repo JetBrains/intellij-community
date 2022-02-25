9a444a379b39bd8a765e2e99af34272d2f76624f
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2022-02-25 14:40:09 +0100

    [kssr] Refactor type filter test
    GitOrigin-RevId: 9a112473d0b8de44f465e06449de0352b233d353

--- plugins/kotlin/idea/tests/testData/structuralsearch/typeFilter/typeArrayAccessArrayExpression.kt
+++
@@ -1,7 +0,0 @@
-val x = Array( 1, { 1 })
-val y = "hello"
-
-fun foo() {
-    print(<warning descr="SSR">x[0]</warning>)
-    print(y[0])
-}
--- plugins/kotlin/idea/tests/testData/structuralsearch/typeFilter/typePropertyAccessor.kt
+++
@@ -1,6 +0,0 @@
-<warning descr="SSR">val x: Int
-    get() = 1</warning>
-
-val y: String
-    get() = "1"
-
