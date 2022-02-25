3a502361701c8994578a010eb13a6dd8488c7a7f
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2022-02-25 12:53:34 +0300

    IDEA-288452: rewrite combo icon for project view
    GitOrigin-RevId: df34a23cd0472c0a0cfbb2ca494aaa384d7d7b00

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/content/ContentComboLabel.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/content/ContentComboLabel.java
@@ -1,7 +1,9 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.wm.impl.content;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.rd.GraphicsExKt;
+import com.intellij.openapi.ui.popup.ActiveIcon;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.ui.ExperimentalUI;
 import com.intellij.ui.Gray;
@@ -27,19 +29,9 @@ import java.awt.event.MouseEvent;
 final class ContentComboLabel extends ContentLabel {
   private final PopupState<JBPopup> myPopupState = PopupState.forPopup();
 
-  private final ComboIcon myComboIcon = new ComboIcon() {
-    @Override
-    public Rectangle getIconRec() {
-      Dimension size = null; // getPreferredSize(); // TODO: because of indefinite repaint
-      int width = size != null ? size.width : getWidth();
-      return new Rectangle(width - getIconWidth() - ICONS_GAP, 0, getIconWidth(), getHeight());
-    }
+  private final ActiveIcon myComboIcon = new ActiveIcon(AllIcons.General.ArrowDown);
+  private final Point myComboIconPoint = new Point();
 
-    @Override
-    public boolean isActive() {
-      return myUi.window.isActive();
-    }
-  };
   private final ComboContentLayout myLayout;
 
   ContentComboLabel(@NotNull ComboContentLayout layout) {
@@ -112,6 +104,8 @@ final class ContentComboLabel extends ContentLabel {
     Dimension size = super.getPreferredSize();
     if (!isPreferredSizeSet() && isToDrawCombo()) {
       if (hasActiveIcons()) size.width -= ICONS_GAP;
+      myComboIconPoint.x = size.width;
+      myComboIconPoint.y = getHeight() / 2 - myComboIcon.getIconHeight() / 2 + 1;
       size.width += myComboIcon.getIconWidth();
     }
 
@@ -144,7 +138,8 @@ final class ContentComboLabel extends ContentLabel {
   protected void paintChildren(Graphics g) {
     super.paintChildren(g);
     if (isToDrawCombo()) {
-      myComboIcon.paintIcon(this, g);
+      myComboIcon.setActive(myUi.window.isActive());
+      myComboIcon.paintIcon(this, g, myComboIconPoint.x, myComboIconPoint.y);
       g.setColor(Gray._255.withAlpha(100));
     }
   }
