af43835098b30a80f412887d8f40e99ad72ea24b
Konstantin Bulenkov <kb@jetbrains.com>
2022-02-27 21:58:30 +0100

    [ExpUI] Add animations when switching editor tabs
    GitOrigin-RevId: 03001adb61ef1809a892ae7d34f54d5be41c4dad

--- platform/platform-api/src/com/intellij/ui/tabs/impl/JBEditorTabsBorder.kt
+++ platform/platform-api/src/com/intellij/ui/tabs/impl/JBEditorTabsBorder.kt
@@ -1,12 +1,64 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ui.tabs.impl
 
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.ui.ExperimentalUI
 import com.intellij.ui.tabs.JBTabsBorder
 import com.intellij.ui.tabs.JBTabsPosition
+import com.intellij.ui.tabs.TabInfo
+import com.intellij.ui.tabs.TabsListener
+import com.intellij.util.animation.Easing
+import com.intellij.util.animation.JBAnimator
+import com.intellij.util.animation.animation
 import java.awt.*
 
 class JBEditorTabsBorder(tabs: JBTabsImpl) : JBTabsBorder(tabs) {
+
+  private val animator = JBAnimator()
+  private var start: Int = -1
+  private var end: Int = -1
+  private var animationId = -1L
+
+  init {
+    tabs.addListener(object : TabsListener {
+      override fun selectionChanged(oldSelection: TabInfo?, newSelection: TabInfo?) {
+        val from = bounds(oldSelection)
+        val to = bounds(newSelection)
+        if (from != null && to != null) {
+          val dur = 100
+          val del = 50
+          val s1 = from.x
+          val s2 = to.x
+          val d1 = if (s1 > s2) 0 else del
+          val e1 = from.x + from.width
+          val e2 = to.x + to.width
+          val d2 = if (e1 > e2) del else 0
+          animationId = animator.animate(
+            animation(s1, s2) {
+              start = it
+              tabs.component.repaint()
+            }.apply {
+              duration = dur - d1
+              delay = d1
+              easing = if (d1 != 0) Easing.EASE_OUT else Easing.LINEAR
+            },
+            animation(e1, e2) {
+              end = it
+              tabs.component.repaint()
+            }.apply {
+              duration = dur - d2
+              delay = d2
+              easing = if (d2 != 0) Easing.EASE_OUT else Easing.LINEAR
+            }
+          )
+        }
+      }
+
+      private fun bounds(tabInfo: TabInfo?): Rectangle? {
+        return tabs.myInfo2Label[tabInfo ?: return null]?.bounds
+      }
+    })
+  }
 
   override val effectiveBorder: Insets
     get() = Insets(thickness, 0, 0, 0)
@@ -54,7 +106,24 @@ class JBEditorTabsBorder(tabs: JBTabsImpl) : JBTabsBorder(tabs) {
       }
     }
 
-    val selectedLabel = tabs.selectedLabel ?: return
-    tabs.tabPainter.paintUnderline(tabs.position, selectedLabel.bounds, thickness, g, tabs.isActiveTabs(tabs.selectedInfo))
+    if (hasAnimation()) {
+      tabs.tabPainter.paintUnderline(tabs.position, calcRectangle() ?: return, thickness, g, tabs.isActiveTabs(tabs.selectedInfo))
+    } else {
+      val selectedLabel = tabs.selectedLabel ?: return
+      tabs.tabPainter.paintUnderline(tabs.position, selectedLabel.bounds, thickness, g, tabs.isActiveTabs(tabs.selectedInfo))
+    }
+  }
+
+
+  private fun calcRectangle(): Rectangle? {
+    val selectedLabel = tabs.selectedLabel ?: return null
+    if (animator.isRunning(animationId)) {
+      return Rectangle(start, selectedLabel.y, end - start, selectedLabel.height)
+    }
+    return selectedLabel.bounds
+  }
+
+  companion object {
+    fun hasAnimation() = ExperimentalUI.isNewUI() || Registry.`is`("ide.editor.tab.selection.animation", false)
   }
 }
