7eca2cab2e8a5db0302ccb4d9c9f226a439cdf8b
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2022-02-28 22:08:44 +0900

    [kotlin] Update test data for 'IntentionTestGenerated', OptIn release
    Opt-in is released in 1.7, see bb38eaae61caa3bc990649553b8df92d7f4e394d ("Postpone OptIn release to version 1.7") in the compiler repository.
    
    (cherry picked from commit eb316dede4fb41ade623812d6d27ff36474c3330)
    
    GitOrigin-RevId: 1303ff084f13810843cc637977a78398e3a11611

--- plugins/kotlin/idea/tests/testData/intentions/convertToIndexedFunctionCall/basic/reduceOrNull.kt
+++ plugins/kotlin/idea/tests/testData/intentions/convertToIndexedFunctionCall/basic/reduceOrNull.kt
@@ -1,6 +1,5 @@
 // INTENTION_TEXT: "Convert to 'reduceIndexedOrNull'"
 // WITH_STDLIB
-// AFTER-WARNING: This annotation should be used with the compiler argument '-opt-in=kotlin.RequiresOptIn'
 // TODO: fix warning?
 // AFTER-WARNING: Parameter 'index' is never used, could be renamed to _
 @OptIn(ExperimentalStdlibApi::class)
