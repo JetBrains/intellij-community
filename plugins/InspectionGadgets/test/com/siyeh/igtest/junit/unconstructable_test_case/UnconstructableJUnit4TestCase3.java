890e4612940a6b3ed0d8f4bfc66f15e386296d47
Bas Leijdekkers <basleijdekkers@gmail.com>
2022-02-28 17:52:19 +0100

    IG: improve "Unconstructable JUnit 'TestCase'" inspection and fix false positive (IDEA-289775)
    GitOrigin-RevId: 11e3d60632ac80a99e03988464645e4da6cd10b2

---
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/junit/unconstructable_test_case/UnconstructableJUnit4TestCase3.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+class <warning descr="Test class 'UnconstructableJUnit4TestCase' is not constructable because it is not 'public'">UnconstructableJUnit4TestCase</warning> {
+
+  public UnconstructableJUnit4TestCase() {}
+
+  @Test
+  void testMe() {}
+}
