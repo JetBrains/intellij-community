ec8fff709b3c1589843018cdd72fcbae72db8266
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2022-03-01 23:47:10 +0900

    [kotlin] Update test data for 'QuickFixMultiFileTestGenerated', exposed type in constructor
    Exposed private constructor parameter type became an error in Kotlin 1.7 (see KT-28078). This commits adapts test data to this change.
    
    GitOrigin-RevId: d65530d44801ee0340f5398c14e0b4b560639905

--- plugins/kotlin/idea/tests/testData/quickfix/createFromUsage/createFunction/call/typeArguments/javaClassMemberPartialSubstitution.after.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/createFromUsage/createFunction/call/typeArguments/javaClassMemberPartialSubstitution.after.kt
@@ -1,5 +1,6 @@
 // "Create member function 'B.foo'" "true"
 // ERROR: Unresolved reference: foo
+// ERROR: 'public' property exposes its 'public/*package*/' type B
 
 class A<T> internal constructor(val b: B<T>) {
     fun test(): Int {
