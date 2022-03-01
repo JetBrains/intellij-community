7042ec77282af11a6edd3fdc2cbdeea49668f57a
Ivan Pashchenko <ivan.pashchenko@jetbrains.com>
2022-03-01 20:11:49 +0300

    GTW-1136 cross-platform launchers pt. 2
    GitOrigin-RevId: 9ce6cfcdcd783ffff9410bf2766a7140bac02045

---
+++ native/XPlatLauncher/src/java.rs
@@ -0,0 +1,253 @@
+use std::{mem, thread};
+use std::ffi::{c_void, CString};
+use std::path::{Path, PathBuf};
+use jni::errors::Error;
+use jni::objects::{JObject, JValue};
+use log::{debug, error};
+use crate::{err_from_string, errors};
+use crate::errors::{LauncherError, Result};
+
+#[cfg(target_os = "windows")] use {
+    crate::utils::canonical_non_unc,
+    crate::utils::PathExt,
+    std::env
+};
+
+#[derive(Clone)]
+pub struct JvmLaunchParameters {
+    pub jbr_home: PathBuf,
+    pub vm_options: Vec<String>,
+    pub class_path: Vec<String>
+}
+
+pub fn run_jvm_and_event_loop(java_home: &Path, vm_options: Vec<String>, args: Vec<String>) -> Result<()> {
+    let java_home = java_home.to_path_buf();
+
+    // JNI docs says that JVM should not be created on primordial thread
+    // See Chapter 5: The Invocation API
+    thread::spawn(move || {
+        unsafe {
+            debug!("Trying to spin up VM and call IntelliJ main from non-primordial thread");
+            match intellij_main_thread(&java_home, vm_options, args) {
+                Ok(_) => {}
+                Err(e) => {
+                    error!("{e:?}")
+                }
+            }
+        }
+    });
+
+    // Using the primordial thread as at least Mac OS X needs it for GUI loop according to JBR
+    // https://github.com/JetBrains/JetBrainsRuntime/blob/363650bbf48789e4c5f68840b66372442d3e481f/src/java.base/macosx/native/libjli/java_md_macosx.c#L328
+    run_event_loop()?;
+
+    return Ok(());
+}
+
+unsafe fn intellij_main_thread(java_home: &Path, vm_options: Vec<String>, args: Vec<String>) -> Result<()> {
+    debug!("Preparing JNI env");
+    let jni_env = prepare_jni_env(&java_home, vm_options)?;
+
+    debug!("Calling main");
+    call_intellij_main(jni_env, args)
+}
+
+unsafe fn prepare_jni_env(
+    java_home: &Path,
+    vm_options: Vec<String>
+) -> Result<jni::JNIEnv<'static>> {
+    debug!("Resolving libjvm");
+    let libjvm_path = get_libjvm(&java_home)?;
+    debug!("libjvm resolved as {libjvm_path:?}");
+
+    debug!("Loading libjvm");
+    let libjvm = load_libjvm(libjvm_path)?;
+    debug!("libjvm loaded");
+
+    debug!("Getting JNI_CreateJavaVM symbol from libjvm");
+    let create_jvm_call:
+        libloading::Symbol<unsafe extern fn(*mut *mut jni_sys::JavaVM, *mut *mut c_void, *mut c_void) -> jni_sys::jint>
+        = libjvm.get(b"JNI_CreateJavaVM")?;
+    debug!("Got JNI_CreateJavaVM symbol from libjvm");
+
+    let mut java_vm: *mut jni_sys::JavaVM = std::ptr::null_mut();
+    let mut jni_env: *mut jni_sys::JNIEnv = std::ptr::null_mut();
+    let args = get_jvm_init_args(vm_options)?;
+    debug!("Constructed VM init args");
+
+    debug!("Creating VM");
+    let create_jvm_result = create_jvm_call(
+        &mut java_vm as *mut *mut jni_sys::JavaVM,
+        &mut jni_env as *mut *mut jni_sys::JNIEnv as *mut *mut c_void,
+        &args as *const jni_sys::JavaVMInitArgs as *mut c_void);
+    debug!("Create VM result={create_jvm_result}");
+
+    errors::JniError::check_result(create_jvm_result)?;
+
+    let jni_env = jni::JNIEnv::from_raw(jni_env)?;
+    debug!("Got JNI env");
+
+    Ok(jni_env)
+}
+
+pub fn call_intellij_main(jni_env: jni::JNIEnv, args: Vec<String>) -> Result<()> {
+    let main_args = jni_env.new_object_array(
+        args.len() as jni_sys::jsize,
+        "java/lang/String",
+        JObject::null()
+    )?;
+
+    for (i, arg) in args.iter().enumerate() {
+        let j_string = jni_env.new_string(arg)?;
+        jni_env.set_object_array_element(main_args, i as jni_sys::jsize, j_string)?;
+    }
+
+    let method_call_args = vec![JValue::from(main_args)];
+
+    debug!("Calling IntelliJ main");
+    match jni_env.call_static_method("com/intellij/idea/Main", "main", "([Ljava/lang/String;)V", &method_call_args) {
+        Ok(_) => {}
+        Err(e) => {
+            match e {
+                Error::JavaException => {
+                    jni_env.exception_describe()?;
+                }
+                _ => { return Err(LauncherError::JniRsError(e)) }
+            }
+        }
+    };
+
+    Ok(())
+}
+
+#[cfg(target_os = "windows")]
+unsafe fn load_libjvm(libjvm_path: PathBuf) -> Result<libloading::Library> {
+    // TODO: pass the bin
+    let jbr_bin = libjvm_path.parent_or_err()?.parent_or_err()?;
+
+
+    // using UNC as the current directory leads to crash when starting JVM
+    let non_unc_bin = canonical_non_unc(&jbr_bin)?;
+    // SetCurrentDirectoryA
+    env::set_current_dir(&non_unc_bin)?;
+    debug!("Set working dir as '{non_unc_bin:?} so that libjvm can find the dependencies");
+
+    // using UNC for libjvm leads to crash when trying to resolve jimage
+    // classloader.cpp:   guarantee(name != NULL, "jimage file name is null");
+    let load_path = canonical_non_unc(&libjvm_path)?;
+    debug!("Loading libvjm by path {load_path:?}");
+
+    match libloading::Library::new(load_path) {
+        Ok(l) => { Ok(l)}
+        Err(e) => { Err(LauncherError::LibloadingError(e))}
+    }
+}
+
+#[cfg(any(target_os = "linux", target_os = "macos"))]
+unsafe fn load_libjvm(libjvm_path: PathBuf) -> Result<libloading::Library> {
+    match libloading::Library::new(libjvm_path.as_os_str()) {
+        Ok(l) => { Ok(l)}
+        Err(e) => { Err(LauncherError::LibloadingError(e))}
+    }
+}
+
+fn get_jvm_init_args(vm_options: Vec<String>) -> Result<jni_sys::JavaVMInitArgs> {
+    let joined = vm_options.join("\n");
+    debug!("Using following vmoptions as VM init args: {joined}");
+
+    let jni_opts = get_jni_vm_opts(vm_options)?;
+    let args = jni_sys::JavaVMInitArgs {
+        version: jni_sys::JNI_VERSION_1_8,
+        nOptions: jni_opts.len() as jni_sys::jint,
+        options: jni_opts.as_ptr() as *mut jni_sys::JavaVMOption,
+        ignoreUnrecognized: true as jni_sys::jboolean
+    };
+
+    // TODO: PhantomData?
+    // vm_init_args live longer then jni_opts vec
+    mem::forget(jni_opts);
+
+    Ok(args)
+}
+
+fn get_libjvm(java_home: &Path) -> Result<PathBuf> {
+    let libjvm = get_libjvm_path(java_home);
+    if !libjvm.exists() {
+        let message = format!("libvjm not found at path {libjvm:?}");
+        return err_from_string(message);
+    }
+
+    let path = std::fs::canonicalize(libjvm)?;
+    Ok(path)
+}
+
+#[cfg(target_os = "windows")]
+fn get_libjvm_path(java_home: &Path) -> PathBuf {
+    // TODO: client/jvm.dll ?
+    java_home
+        .join("bin")
+        .join("server")
+        .join("jvm.dll")
+}
+
+#[cfg(target_os = "macos")]
+fn get_libjvm_path(java_home: &Path) -> PathBuf {
+    java_home
+        .join("lib")
+        .join("server")
+        .join("libjvm.dylib")
+}
+
+#[cfg(target_os = "linux")]
+fn get_libjvm_path(java_home: &Path) -> PathBuf {
+    java_home
+        .join("lib")
+        .join("server")
+        .join("libjvm.so")
+}
+
+fn get_jni_vm_opts(opts: Vec<String>) -> Result<Vec<jni_sys::JavaVMOption>> {
+    let mut jni_opts = Vec::with_capacity(opts.len());
+    for opt in opts {
+        let option_string = CString::new(opt.as_str())?;
+        let jvm_opt = jni_sys::JavaVMOption {
+            // TODO: possible memory leak
+            optionString: option_string.into_raw(),
+            extraInfo: std::ptr::null_mut(),
+        };
+        jni_opts.push(jvm_opt);
+    }
+
+    Ok(jni_opts)
+}
+
+#[cfg(target_os = "windows")]
+fn run_event_loop() -> Result<()> {
+    loop {
+
+    }
+}
+
+#[cfg(target_os = "macos")]
+fn run_event_loop() -> Result<()> {
+    unsafe {
+        core_foundation::runloop::CFRunLoopRun();
+    }
+
+    Ok(())
+}
+
+#[cfg(target_os = "linux")]
+fn run_event_loop() -> Result<()> {
+    debug!("Running event loop on primordial thread");
+    // TODO: proper run loop so that we can exit
+    loop {
+        sleep(Duration::from_secs(1))
+
+        // let r = intellij_main_call.load(Ordering::Relaxed);
+        // match &*r {
+        //     Ok(_) => { sleep(Duration::from_secs(1)) }
+        //     Err(e) => { return Err(LauncherError::OtherError(OtherLauncherError{message: format!("{e:?}").to_string()}));}
+        // }
+    }
+}
---
+++ native/XPlatLauncher/src/lib.rs
@@ -0,0 +1,250 @@
+extern crate core;
+
+mod errors;
+mod java;
+mod utils;
+mod remote_dev;
+mod default;
+
+use serde::{Deserialize, Serialize};
+use std::{env};
+use std::fs::{File};
+use std::path::{PathBuf};
+use log::{debug, error, LevelFilter};
+use native_dialog::{MessageDialog, MessageType};
+use simplelog::{ColorChoice, CombinedLogger, Config, TerminalMode, TermLogger, WriteLogger};
+use crate::default::DefaultLaunchConfiguration;
+use crate::errors::{err_from_string, LauncherError, Result};
+use crate::remote_dev::RemoteDevLaunchConfiguration;
+use crate::utils::{canonical_non_unc};
+
+pub fn main_lib() {
+    let main_result = main_impl();
+    unwrap_with_human_readable_error(main_result);
+    match main_impl() {
+        Ok(_) => {}
+        Err(e) => {
+
+        }
+    }
+}
+
+fn main_impl() -> Result<()> {
+    println!("Initializing logger");
+
+    // TODO: set agreed configuration (probably simple logger instead of pretty colors for terminal) or replace the lib with our own code
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Debug, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
+            WriteLogger::new(LevelFilter::Debug, Config::default(), File::create("launcher.log").unwrap()),
+        ]
+    )?;
+
+    debug!("Launching");
+
+    let configuration = &get_configuration()?;
+
+    debug!("Preparing runtime");
+    let java_home = &configuration.prepare_for_launch()?;
+
+    debug!("Resolving vm options");
+    let vm_options = get_full_vm_options(configuration)?;
+
+    debug!("Resolving args for vm launch");
+    let args = configuration.get_args();
+
+    java::run_jvm_and_event_loop(java_home, vm_options, args.to_vec())
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Serialize, Clone)]
+pub struct ProductInfo {
+    pub productCode: String,
+    pub classPathJars: Vec<String>,
+    pub productVendor: String,
+    pub dataDirectoryName: String,
+    pub vmOptionsBaseFileName: String
+}
+
+trait LaunchConfiguration {
+    fn get_args(&self) -> &[String];
+
+    fn get_intellij_vm_options(&self) -> Result<Vec<String>>;
+    fn get_properties_file(&self) -> Result<PathBuf>;
+    fn get_class_path(&self) -> Result<Vec<String>>;
+
+    fn prepare_for_launch(&self) -> Result<PathBuf>;
+}
+
+fn get_configuration() -> Result<Box<dyn LaunchConfiguration>> {
+    let cmd_args: Vec<String> = env::args().collect();
+
+    // 0 arg is binary itself
+    let is_remote_dev = cmd_args.len() > 1 && cmd_args[1] == "--remote-dev";
+
+    let (remote_dev_project_path, ij_args) = match is_remote_dev {
+        true => {
+            let remote_dev_args = RemoteDevLaunchConfiguration::parse_remote_dev_args(&cmd_args[2..])?;
+            (remote_dev_args.project_path, remote_dev_args.ij_args)
+        },
+        false => (None, cmd_args)
+    };
+
+    if is_remote_dev {
+        // required for the most basic launch (e.g. showing help)
+        // as there may be nothing on user system and we'll crash
+        RemoteDevLaunchConfiguration::setup_font_config()?;
+    }
+
+    let default = DefaultLaunchConfiguration::new(ij_args)?;
+
+    match remote_dev_project_path {
+        None => Ok(Box::new(default)),
+        Some(x) => {
+            let config = RemoteDevLaunchConfiguration::new(x, default)?;
+            Ok(Box::new(config))
+        }
+    }
+}
+
+fn unwrap_with_human_readable_error<S>(result: Result<S>) -> S {
+    match result {
+        Ok(x) => { x }
+        Err(e) => {
+            let message =
+                match e {
+                    LauncherError::HumanReadableError(e) => {
+                        e.message
+                    }
+                    _ => {
+                        format!("{e:?}")
+                    }
+                };
+
+            eprintln!("{}", message);
+            error!("{}", message);
+
+            // TODO: detect if there's no UI?
+            show_fail_to_start_message("Failed to start", format!("{message:?}").as_str());
+
+            std::process::exit(1);
+        }
+    }
+}
+
+fn get_full_vm_options(configuration: &Box<dyn LaunchConfiguration>) -> Result<Vec<String>> {
+    let mut full_vm_options: Vec<String> = Vec::new();
+
+    debug!("Resolving IDE properties file");
+    // 1. properties file
+    match configuration.get_properties_file() {
+        Ok(p) => {
+            let path_string = p.to_string_lossy();
+            let vm_option = format!("-Didea.properties.file={path_string}");
+            full_vm_options.push(vm_option);
+        }
+        Err(e) => {
+            debug!("IDE properties file is not set, skipping setting vm option")
+        }
+    };
+
+    debug!("Resolving IDE VM options from files");
+    // 2. .vmoptions
+    let intellij_vm_options = configuration.get_intellij_vm_options()?;
+    for vm_option in intellij_vm_options {
+        full_vm_options.push(vm_option);
+    }
+
+    debug!("Resolving classpath");
+    // 3. classpath
+    let class_path_separator = get_class_path_separator();
+    let class_path = configuration.get_class_path()?.join(class_path_separator);
+    let class_path_vm_option = "-Djava.class.path=".to_string() + class_path.as_str();
+
+    full_vm_options.push(class_path_vm_option);
+
+    // 4. TODO: find out if that's still needed
+    full_vm_options.push("-Djava.system.class.loader=com.intellij.util.lang.PathClassLoader".to_string());
+    full_vm_options.push("-XX:+StartAttachListener".to_string());
+
+    // TODO: shouldn't this already be in .vmoptions?
+    // answer: it's platform-specific :D
+    // linux - replaced
+    // windows - resource file
+    // macos - plist
+    // answer: add to product-info.json
+    full_vm_options.push("--add-opens=java.base/java.io=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/java.lang=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/java.lang.reflect=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/java.net=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/java.nio=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/java.nio.charset=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/java.text=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/java.time=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/java.util=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/java.util.concurrent=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/jdk.internal.vm=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/sun.nio.ch=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/sun.security.ssl=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.base/sun.security.util=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/com.apple.eawt=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/com.apple.eawt.event=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/com.apple.laf=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/com.sun.java.swing.plaf.gtk=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/java.awt=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/java.awt.dnd.peer=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/java.awt.event=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/java.awt.image=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/java.awt.peer=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/java.awt.font=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/javax.swing=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/javax.swing.plaf.basic=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/javax.swing.text.html=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/sun.awt.X11=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/sun.awt.datatransfer=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/sun.awt.image=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/sun.awt.windows=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/sun.awt=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/sun.font=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/sun.java2d=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/sun.lwawt=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/sun.lwawt.macosx=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=java.desktop/sun.swing=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=jdk.attach/sun.tools.attach=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED".to_string());
+    full_vm_options.push("--add-opens=jdk.jdi/com.sun.tools.jdi=ALL-UNNAMED".to_string());
+
+    // TODO:
+    // "-XX:ErrorFile=$HOME/java_error_in___vm_options___%p.log" \
+    // "-XX:HeapDumpPath=$HOME/java_error_in___vm_options___.hprof" \
+
+    Ok(full_vm_options)
+}
+
+#[cfg(any(target_os = "linux", target_os = "macos"))]
+fn get_class_path_separator<'a>() -> &'a str {
+    ":"
+}
+
+#[cfg(target_os = "windows")]
+fn get_class_path_separator<'a>() -> &'a str {
+    ";"
+}
+
+fn show_fail_to_start_message(title: &str, text: &str) {
+    let result = MessageDialog::new()
+        .set_title(title)
+        .set_text(text)
+        .set_type(MessageType::Error)
+        .show_alert();
+
+    match result {
+        Ok(_) => { }
+        Err(e) => {
+            // TODO: proper message
+            error!("Failed to show error message: {e:?}")
+        }
+    }
+}
