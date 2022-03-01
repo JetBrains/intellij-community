0c5a88d1c28d74d3bd4955d8331356cfa6f252d9
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2022-03-01 23:29:38 +0900

    [kotlin] Update test data for 'QuickFixMultiFileTestGenerated', exhaustive when
    'Exhaustive when' is enabled for algebraic types such as 'enum class' or 'sealed class' (see KT-47709). This commit adapts test data to this change.
    
    (cherry picked from commit 37650bd9890bbbf6f4b6ab3a4fff5299a982b7bb)
    
    GitOrigin-RevId: 1306fa1746855baf624836fd2d11d41ad5151d32

--- plugins/kotlin/idea/tests/testData/quickfix/when/addRemainingBranchesAnotherPackageAll.after.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/when/addRemainingBranchesAnotherPackageAll.after.kt
@@ -1,4 +1,5 @@
 // "Add remaining branches with * import" "true"
+// ERROR: 'when' expression must be exhaustive, add necessary 'RED', 'GREEN', 'BLUE' branches or 'else' branch instead
 
 package u
 
