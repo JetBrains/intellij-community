8327b222241714732738c782fab9b850740f748c
Lamberto Basti <lamberto.basti@jetbrains.com>
2022-03-02 15:15:38 +0100

    [PKGS] refactoring of inspections
    GitOrigin-RevId: 1a6b49bc05e98f01e9a26832df9a2cbc08370f60

--- plugins/package-search/gradle/src/com/jetbrains/packagesearch/intellij/plugin/gradle/GradlePackageUpdateInspection.kt
+++ plugins/package-search/gradle/src/com/jetbrains/packagesearch/intellij/plugin/gradle/GradlePackageUpdateInspection.kt
@@ -5,14 +5,27 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.jetbrains.packagesearch.intellij.plugin.PackageSearchBundle
 import com.jetbrains.packagesearch.intellij.plugin.extensibility.PackageUpdateInspection
+import com.jetbrains.packagesearch.intellij.plugin.extensibility.PackageVersionRangeInspection
+
+private fun getGradleVersionPsiElement(
+    dependency: UnifiedDependency,
+    file: PsiFile
+): PsiElement? {
+    val groupId = dependency.coordinates.groupId ?: return null
+    val artifactId = dependency.coordinates.artifactId ?: return null
+    return GradleModuleTransformer.findDependencyElement(file, groupId, artifactId)
+}
 
 internal class GradlePackageUpdateInspection : PackageUpdateInspection() {
 
     override fun getStaticDescription(): String = PackageSearchBundle.getMessage("packagesearch.inspection.upgrade.description.gradle")
 
-    override fun getVersionPsiElement(file: PsiFile, dependency: UnifiedDependency): PsiElement? {
-        val groupId = dependency.coordinates.groupId ?: return null
-        val artifactId = dependency.coordinates.artifactId ?: return null
-        return GradleModuleTransformer.findDependencyElement(file, groupId, artifactId)
-    }
+    override fun getVersionPsiElement(file: PsiFile, dependency: UnifiedDependency): PsiElement? = getGradleVersionPsiElement(dependency, file)
+}
+
+internal class GradlePackageVersionRangeInspection : PackageVersionRangeInspection() {
+
+    override fun getStaticDescription(): String = PackageSearchBundle.getMessage("packagesearch.inspection.range.description.gradle")
+
+    override fun getVersionPsiElement(file: PsiFile, dependency: UnifiedDependency) = getGradleVersionPsiElement(dependency, file)
 }
