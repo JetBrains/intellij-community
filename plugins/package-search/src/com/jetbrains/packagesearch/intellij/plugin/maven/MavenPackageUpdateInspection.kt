3f3ba10a3b53ab75b2d446ba249f1612c7867915
Lamberto Basti <lamberto.basti@jetbrains.com>
2022-03-02 15:15:38 +0100

    [PKGS] refactoring of inspections
    (cherry picked from commit e1883a4221a43f26204b01b56ab9f6fc2ccac72d)
    
    IJ-CR-21386
    
    (cherry picked from commit 86688cb88d98e70ec9ef75b4ba0db05a1e68b1e1)
    
    GitOrigin-RevId: e1b654030de4c7c0aaab50dcd30c8bec1f70bda2

--- plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/maven/MavenPackageUpdateInspection.kt
+++ plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/maven/MavenPackageUpdateInspection.kt
@@ -4,24 +4,42 @@ import com.intellij.buildsystem.model.unified.UnifiedDependency
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.xml.XmlTag
+import com.intellij.psi.xml.XmlText
 import com.jetbrains.packagesearch.intellij.plugin.PackageSearchBundle
 import com.jetbrains.packagesearch.intellij.plugin.extensibility.PackageUpdateInspection
+import com.jetbrains.packagesearch.intellij.plugin.extensibility.PackageVersionRangeInspection
 import org.jetbrains.idea.maven.dom.MavenDomUtil
 import org.jetbrains.idea.maven.navigator.MavenNavigationUtil
+
+private fun getMavenVersionPsiElement(
+    dependency: UnifiedDependency,
+    file: PsiFile
+): PsiElement? {
+    val groupId = dependency.coordinates.groupId ?: return null
+    val artifactId = dependency.coordinates.artifactId ?: return null
+
+    val projectModel = MavenDomUtil.getMavenDomProjectModel(file.project, file.virtualFile) ?: return null
+
+    val mavenDependency = MavenNavigationUtil.findDependency(projectModel, groupId, artifactId)
+    val element = mavenDependency?.version?.xmlElement ?: return null
+
+    return if (element is XmlTag) element.value.textElements.firstOrNull() else null
+}
 
 internal class MavenPackageUpdateInspection : PackageUpdateInspection() {
 
     override fun getStaticDescription(): String = PackageSearchBundle.getMessage("packagesearch.inspection.upgrade.description.maven")
 
-    override fun getVersionPsiElement(file: PsiFile, dependency: UnifiedDependency): PsiElement? {
-        val groupId = dependency.coordinates.groupId ?: return null
-        val artifactId = dependency.coordinates.artifactId ?: return null
+    override fun getVersionPsiElement(file: PsiFile, dependency: UnifiedDependency): PsiElement? =
+        getMavenVersionPsiElement(dependency, file)
 
-        val projectModel = MavenDomUtil.getMavenDomProjectModel(file.project, file.virtualFile) ?: return null
+}
 
-        val mavenDependency = MavenNavigationUtil.findDependency(projectModel, groupId, artifactId)
-        val element = mavenDependency?.version?.xmlElement ?: return null
+internal class MavenPackageVersionRangeInspection : PackageVersionRangeInspection() {
 
-        return (element as XmlTag).value.textElements.firstOrNull()
-    }
+    override fun getStaticDescription(): String = PackageSearchBundle.getMessage("packagesearch.inspection.range.description.maven")
+
+    override fun getVersionPsiElement(file: PsiFile, dependency: UnifiedDependency): PsiElement? =
+        getMavenVersionPsiElement(dependency, file)
+
 }
