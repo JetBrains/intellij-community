b67b78a428ba0f236661c59bf698c65efe7d945c
Leonid Shalupov <leonid@shalupov.com>
2022-03-02 20:58:19 +0100

    IDEA-289857 building intellij-community fails in installers.cmd on Windows
    %~dp0 expands to a string which ends with \ and \" is not a correct quoting.
    Add an empty directory reference to make it only \." which is perfectly ok
    
    (cherry picked from commit 02a3637a4f0955fed6dab05837158bebe45918f0)
    
    IJ-CR-21705
    
    GitOrigin-RevId: 34dce4c933304b8966b74f2b45c19c1b34d8b6b3

--- tests.cmd
+++ tests.cmd
@@ -8,5 +8,5 @@ root="$(cd "$(dirname "$0")"; pwd)"
 exec "$root/platform/jps-bootstrap/jps-bootstrap.sh" "$@" "$root" intellij.idea.community.build CommunityRunTestsBuildTarget
 :CMDSCRIPT
 
-call "%~dp0\platform\jps-bootstrap\jps-bootstrap.cmd" %* "%~dp0" intellij.idea.community.build CommunityRunTestsBuildTarget
+call "%~dp0\platform\jps-bootstrap\jps-bootstrap.cmd" %* "%~dp0." intellij.idea.community.build CommunityRunTestsBuildTarget
 EXIT /B %ERRORLEVEL%
