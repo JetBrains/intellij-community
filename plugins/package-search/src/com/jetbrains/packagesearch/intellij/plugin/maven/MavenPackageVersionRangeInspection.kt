9dd5b0ab67d88888a5c256ac0e75cf5b3ab6fdb9
Lamberto Basti <lamberto.basti@jetbrains.com>
2022-03-02 15:55:37 +0100

    [PKGS] refactoring of inspections v2
    (cherry picked from commit e152a9c54edaf39cf71734fd1a6f4d7c33970263)
    
    IJ-CR-21386
    
    (cherry picked from commit 71cc64166c91c05c8182488c78772eeb47f91cd6)
    
    GitOrigin-RevId: 115f4b0f15bdb66817fc3fea9cb2d154f6ee476e

--- plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/maven/MavenPackageVersionRangeInspection.kt
+++ plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/maven/MavenPackageVersionRangeInspection.kt
@@ -4,13 +4,13 @@ import com.intellij.buildsystem.model.unified.UnifiedDependency
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.jetbrains.packagesearch.intellij.plugin.PackageSearchBundle
-import com.jetbrains.packagesearch.intellij.plugin.extensibility.PackageUpdateInspection
+import com.jetbrains.packagesearch.intellij.plugin.extensibility.PackageVersionRangeInspection
 
-internal class MavenPackageUpdateInspection : PackageUpdateInspection() {
+internal class MavenPackageVersionRangeInspection : PackageVersionRangeInspection() {
 
-    override fun getStaticDescription(): String = PackageSearchBundle.getMessage("packagesearch.inspection.upgrade.description.maven")
+    override fun getStaticDescription(): String = PackageSearchBundle.getMessage("packagesearch.inspection.range.description.maven")
 
     override fun getVersionPsiElement(file: PsiFile, dependency: UnifiedDependency): PsiElement? =
-        getMavenVersionPsiElement(dependency, file)
+      getMavenVersionPsiElement(dependency, file)
 
 }
