4ed120077667bf06b5c7412def07c15b2025559b
Vassiliy.Kudryashov <vassiliy.kudryashov@jetbrains.com>
2022-03-03 20:51:25 +0300

    Fix NPE
    (cherry picked from commit 2a1c4e85b0cf1073bab4c467eabb8bdb0c6bfc2f)
    
    IJ-CR-21835
    
    GitOrigin-RevId: 84f8ed3b1ee87935aa6104c33c2dff63d7439e4d

--- platform/platform-impl/src/com/intellij/toolWindow/InternalDecoratorImpl.kt
+++ platform/platform-impl/src/com/intellij/toolWindow/InternalDecoratorImpl.kt
@@ -398,7 +398,7 @@ class InternalDecoratorImpl internal constructor(
     }
     else if (divider != null) {
       // docked and floating windows don't have divider
-      divider!!.parent.remove(divider)
+      divider!!.parent?.remove(divider)
       divider = null
     }
 
