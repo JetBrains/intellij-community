77f3afd2ef4eaf6aad531fa7d8e3ee574ca3f9c6
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2022-03-03 16:38:58 +0300

    IDEA-284922 added show gradle dependency analyzer for module
    (cherry picked from commit 1fd17171cdd95bf060828d04608291a29788aaec)
    
    IJ-CR-21860
    
    GitOrigin-RevId: 3cdca675cf020d091c20c70eb6d5f8630a63448f

--- plugins/gradle/src/org/jetbrains/plugins/gradle/dependency/analyzer/GradleDependencyAnalyzerAction.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/dependency/analyzer/GradleDependencyAnalyzerAction.kt
@@ -2,12 +2,59 @@
 package org.jetbrains.plugins.gradle.dependency.analyzer
 
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.externalSystem.dependency.analyzer.AbstractDependencyAnalyzerAction
-import com.intellij.openapi.externalSystem.dependency.analyzer.DependencyAnalyzerView
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.externalSystem.action.ExternalSystemActionUtil
+import com.intellij.openapi.externalSystem.dependency.analyzer.*
+import com.intellij.openapi.externalSystem.model.ExternalSystemDataKeys
+import com.intellij.openapi.externalSystem.model.project.*
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
+import com.intellij.openapi.module.ModuleManager
 import org.jetbrains.plugins.gradle.util.GradleConstants
+import com.intellij.openapi.module.Module
 
 class GradleDependencyAnalyzerAction : AbstractDependencyAnalyzerAction() {
   override fun getSystemId(e: AnActionEvent) = GradleConstants.SYSTEM_ID
 
-  override fun setSelectedState(e: AnActionEvent, view: DependencyAnalyzerView) {}
+  override fun setSelectedState(e: AnActionEvent, view: DependencyAnalyzerView) {
+    val externalProjectPath = getExternalProjectPath(e.dataContext) ?: return
+    val dependencyData = getSelectedDependencyData(e.dataContext) ?: return
+    view.setSelectedDependency(externalProjectPath, dependencyData)
+  }
+
+  private fun getExternalProjectPath(context: DataContext): String? {
+    val module = getGradleModule(context) ?: findGradleModule(context)
+    return ExternalSystemApiUtil.getExternalProjectPath(module)
+  }
+
+  private fun getGradleModule(context: DataContext): Module? {
+    val module = ExternalSystemActionUtil.getModule(context) ?: return null
+    return if (module.isGradleModule()) module else null
+  }
+
+  private fun findGradleModule(context: DataContext): Module? {
+    val project = CommonDataKeys.PROJECT.getData(context) ?: return null
+    val selectedNodes = ExternalSystemDataKeys.SELECTED_NODES.getData(context)
+    val moduleName = when (val data = selectedNodes?.firstOrNull()?.data) {
+      is ProjectData -> data.internalName
+      is ModuleData -> data.internalName
+      else -> null
+    }
+    val manager = ModuleManager.getInstance(project)
+    val module = moduleName?.let { manager.findModuleByName(it) }
+    return if (module?.isGradleModule() == true) module else null
+  }
+
+  private fun getSelectedDependencyData(context: DataContext): DependencyAnalyzerDependency.Data? {
+    val selectedNodes = ExternalSystemDataKeys.SELECTED_NODES.getData(context)
+    return when (val data = selectedNodes?.firstOrNull()?.data) {
+      is ProjectData -> DAModule(data.internalName)
+      is ModuleData -> DAModule(data.internalName)
+      else -> null
+    }
+  }
+
+  private fun Module.isGradleModule(): Boolean {
+    return ExternalSystemApiUtil.isExternalSystemAwareModule(GradleConstants.SYSTEM_ID, this)
+  }
 }
