e6b35307ab7cea436d6d1c73d4130cbdab0ae6db
Adam Malek <adam.malek@jetbrains.com>
2022-03-03 14:53:35 +0100

    ML in SE: Report new isInvalid feature when PsiElement becomes invalid
    (cherry picked from commit 6fade35d3e54f14afa516563f07a4e9a18bc3932)
    
    IJ-CR-21583
    
    GitOrigin-RevId: 3f55e75f42a26958ceda89b4b8476ac7c7fe34a4

--- plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/features/SearchEverywhereClassFeaturesProvider.kt
+++ plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/features/SearchEverywhereClassFeaturesProvider.kt
@@ -34,7 +34,7 @@ class SearchEverywhereClassFeaturesProvider : SearchEverywhereClassOrFileFeature
     }
 
     data.putIfValueNotNull(IS_DEPRECATED, isDeprecated(presentation))
-    data.putIfValueNotNull(IS_ACCESSIBLE_FROM_MODULE, isAccessibleFromModule(element, cache?.openedFile))
+    data.putAll(isAccessibleFromModule(element, cache?.openedFile))
     return data
   }
 
@@ -47,25 +47,23 @@ class SearchEverywhereClassFeaturesProvider : SearchEverywhereClassOrFileFeature
     return effectType == EffectType.STRIKEOUT
   }
 
-  private fun isAccessibleFromModule(element: PsiElement, openedFile: VirtualFile?): Boolean? {
-    if (openedFile == null) {
-      return null
-    }
+  private fun isAccessibleFromModule(element: PsiElement, openedFile: VirtualFile?): Map<String, Any> {
+    return openedFile?.let {
+      ReadAction.compute<Map<String, Any>, Nothing> {
+        if (!element.isValid) return@compute mapOf(IS_INVALID_DATA_KEY to true)
 
-    val (openedFileModule, elementModule) = ReadAction.compute<Pair<com.intellij.openapi.module.Module?,
-      com.intellij.openapi.module.Module?>, Nothing> {
-      if (!element.isValid) return@compute Pair(null, null)
+        val elementFile = element.containingFile?.virtualFile ?: return@compute emptyMap()
+        val fileIndex = ProjectRootManager.getInstance(element.project).fileIndex
 
-      val elementFile = element.containingFile?.virtualFile ?: return@compute Pair(null, null)
-      val fileIndex = ProjectRootManager.getInstance(element.project).fileIndex
+        val openedFileModule = fileIndex.getModuleForFile(it)
+        val elementModule = fileIndex.getModuleForFile(elementFile)
 
-      return@compute Pair(fileIndex.getModuleForFile(openedFile), fileIndex.getModuleForFile(elementFile))
-    }
+        if (openedFileModule == null || elementModule == null) return@compute emptyMap()
 
-    if (openedFileModule == null || elementModule == null) {
-      return null
-    }
-
-    return elementModule.name in ModuleRootManager.getInstance(openedFileModule).dependencyModuleNames
+        return@compute mapOf(
+          IS_ACCESSIBLE_FROM_MODULE to (elementModule.name in ModuleRootManager.getInstance(openedFileModule).dependencyModuleNames)
+        )
+      }
+    } ?: emptyMap()
   }
 }
