1aa11f34c5eb52ae3bdffc59e3f0dc41e7ebfbff
Victor Matchenko <Victor.Matchenko@jetbrains.com>
2022-03-03 14:22:05 +0100

    IDEA-289989 Add flag to reduce initial project state collectors delay
    (cherry picked from commit 37911df90dea1cced1bd1c53c970e7f284336145)
    
    IJ-CR-22839
    
    GitOrigin-RevId: 7be201914858a52f2b875f35336f2b5a9f4b9adb

--- platform/statistics/src/com/intellij/internal/statistic/updater/StatisticsStateCollectorsScheduler.java
+++ platform/statistics/src/com/intellij/internal/statistic/updater/StatisticsStateCollectorsScheduler.java
@@ -29,6 +29,7 @@ final class StatisticsStateCollectorsScheduler implements ApplicationInitialized
   private static final int LOG_APPLICATION_STATE_SMART_MODE_DELAY_IN_SECONDS = 60;
   public static final int LOG_PROJECTS_STATES_INITIAL_DELAY_IN_MIN = 5;
   public static final int LOG_PROJECTS_STATES_DELAY_IN_MIN = 12 * 60;
+  private static final String REDUCE_DELAY_FLAG_KEY = "fus.internal.reduce.initial.state.collectors.delay";
 
   private final Map<Project, Future<?>> persistStatisticsSessionsMap = Collections.synchronizedMap(new HashMap<>());
   private final AtomicBoolean allowExecution = new AtomicBoolean(true);
@@ -61,9 +62,11 @@ final class StatisticsStateCollectorsScheduler implements ApplicationInitialized
 
         //wait until initial indexation will be finished
         DumbService.getInstance(project).runWhenSmart(() -> {
+          boolean reduceInitialDelay = Boolean.parseBoolean(System.getProperty(REDUCE_DELAY_FLAG_KEY));
           ScheduledFuture<?> future = JobScheduler.getScheduler()
             .scheduleWithFixedDelay(() -> FUStateUsagesLogger.create().logProjectStates(project, new EmptyProgressIndicator()),
-                                    LOG_PROJECTS_STATES_INITIAL_DELAY_IN_MIN, LOG_PROJECTS_STATES_DELAY_IN_MIN, TimeUnit.MINUTES);
+                                    reduceInitialDelay ? 0 : LOG_PROJECTS_STATES_INITIAL_DELAY_IN_MIN,
+                                    LOG_PROJECTS_STATES_DELAY_IN_MIN, TimeUnit.MINUTES);
           persistStatisticsSessionsMap.put(project, future);
         });
 
