a96a20b86191889fa381a722872edd9694380924
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2022-03-04 13:05:16 +0100

    get rid of `FontSizePopup` object, put members to file level
    GitOrigin-RevId: ee67bb51b87d526ef7146efc01e07c4c0b9cc2af

--- platform/lang-impl/src/com/intellij/ui/FontSizePopup.kt
+++ platform/lang-impl/src/com/intellij/ui/FontSizePopup.kt
@@ -1,4 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:JvmName("FontSizePopup")
+@file:ApiStatus.Internal
+
 package com.intellij.ui
 
 import com.intellij.openapi.application.ApplicationBundle
@@ -18,54 +21,50 @@ import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.SwingConstants
 
-@ApiStatus.Internal
-object FontSizePopup {
-  @JvmStatic
-  fun <T> showFontSizePopup(
-    parentComponent: Component,
-    initialFont: T,
-    fontRange: List<T>,
-    onPopupClose: () -> Unit,
-    changeCallback: (T) -> Unit
-  ): FontSizePopupData {
-    val slider = JBSlider(0, fontRange.size - 1).apply {
-      orientation = SwingConstants.HORIZONTAL
-      isOpaque = true
-      minorTickSpacing = 1
-      paintTicks = true
-      paintTrack = true
-      snapToTicks = true
-    }
-    UIUtil.setSliderIsFilled(slider, true)
-    val initialIndex = fontRange.indexOf(initialFont)
-    slider.setValueWithoutEvents(initialIndex)
-    slider.addChangeListener {
-      changeCallback(fontRange[slider.value])
-    }
-
-    val panel = JPanel(FlowLayout(FlowLayout.RIGHT, 3, 0))
-    panel.isOpaque = true
-    panel.add(JLabel(ApplicationBundle.message("label.font.size")))
-    panel.add(slider)
-    panel.border = BorderFactory.createLineBorder(JBColor.border(), 1)
-
-    val popup = JBPopupFactory.getInstance().createComponentPopupBuilder(panel, slider).createPopup()
-    popup.addListener(object : JBPopupListener {
-      override fun onClosed(event: LightweightWindowEvent) {
-        onPopupClose()
-      }
-    })
-
-    val location = MouseInfo.getPointerInfo().location
-    popup.show(
-      RelativePoint(Point(
-        location.x - panel.preferredSize.width / 2,
-        location.y - panel.preferredSize.height / 2
-      )).getPointOn(parentComponent)
-    )
-
-    return FontSizePopupData(slider)
+fun <T> showFontSizePopup(
+  parentComponent: Component,
+  initialFont: T,
+  fontRange: List<T>,
+  onPopupClose: () -> Unit,
+  changeCallback: (T) -> Unit
+): FontSizePopupData {
+  val slider = JBSlider(0, fontRange.size - 1).apply {
+    orientation = SwingConstants.HORIZONTAL
+    isOpaque = true
+    minorTickSpacing = 1
+    paintTicks = true
+    paintTrack = true
+    snapToTicks = true
+  }
+  UIUtil.setSliderIsFilled(slider, true)
+  val initialIndex = fontRange.indexOf(initialFont)
+  slider.setValueWithoutEvents(initialIndex)
+  slider.addChangeListener {
+    changeCallback(fontRange[slider.value])
   }
 
-  data class FontSizePopupData(val slider: JBSlider)
+  val panel = JPanel(FlowLayout(FlowLayout.RIGHT, 3, 0))
+  panel.isOpaque = true
+  panel.add(JLabel(ApplicationBundle.message("label.font.size")))
+  panel.add(slider)
+  panel.border = BorderFactory.createLineBorder(JBColor.border(), 1)
+
+  val popup = JBPopupFactory.getInstance().createComponentPopupBuilder(panel, slider).createPopup()
+  popup.addListener(object : JBPopupListener {
+    override fun onClosed(event: LightweightWindowEvent) {
+      onPopupClose()
+    }
+  })
+
+  val location = MouseInfo.getPointerInfo().location
+  popup.show(
+    RelativePoint(Point(
+      location.x - panel.preferredSize.width / 2,
+      location.y - panel.preferredSize.height / 2
+    )).getPointOn(parentComponent)
+  )
+
+  return FontSizePopupData(slider)
 }
+
+data class FontSizePopupData(val slider: JBSlider)
