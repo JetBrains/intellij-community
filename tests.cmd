3680275cb043ddf7f551a29b418776191f4a1b1b
Leonid Shalupov <leonid@shalupov.com>
2022-03-05 12:03:01 +0100

    jps-bootstrap: describe a new way of running tests in community
    GitOrigin-RevId: 6b45414c8c43d8dd8d18b8375e8c19ddf50d7f89

--- tests.cmd
+++ tests.cmd
@@ -3,6 +3,10 @@
 GOTO :CMDSCRIPT
 ::CMDLITERAL
 
+# tests.cmd builds and runs IDEA Community tests in way suitable for calling from CI/CD like TeamCity
+# THIS SCRIPTS WORKS FOR ALL SYSTEMS Linux/Windows/macOS
+# See README.md for usage scenarios
+
 set -eux
 root="$(cd "$(dirname "$0")"; pwd)"
 exec "$root/platform/jps-bootstrap/jps-bootstrap.sh" "$@" "$root" intellij.idea.community.build CommunityRunTestsBuildTarget
