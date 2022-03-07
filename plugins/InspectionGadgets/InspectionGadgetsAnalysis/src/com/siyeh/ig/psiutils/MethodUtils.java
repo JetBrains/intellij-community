8903702f9a25b99b5da2d02db6322d82e1602029
Bas Leijdekkers <basleijdekkers@gmail.com>
2022-03-07 21:05:22 +0100

    IG: fix triviality check for return statement (IDEA-251027)
    (cherry picked from commit e943b66cbc054c1bb277151f7ba18552a3042081)
    
    IJ-CR-21983
    
    GitOrigin-RevId: 79cd59c6af089e4aee7befa8c1c5d8b4e94e2f98

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/psiutils/MethodUtils.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/psiutils/MethodUtils.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.siyeh.ig.psiutils;
 
 import com.intellij.codeInsight.AnnotationUtil;
@@ -322,8 +322,8 @@ public final class MethodUtils {
       if (statement instanceof PsiReturnStatement) {
         final PsiReturnStatement returnStatement = (PsiReturnStatement)statement;
         final PsiExpression returnValue = PsiUtil.skipParenthesizedExprDown(returnStatement.getReturnValue());
-        if (returnValue == null || returnValue instanceof PsiLiteralExpression) {
-          return true;
+        if (returnValue != null && !(returnValue instanceof PsiLiteralExpression)) {
+          return false;
         }
       }
       else if (statement instanceof PsiIfStatement) {
