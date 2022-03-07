1102820d3641d76970e23c4e137716049287644a
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-03-07 15:55:16 +0300

    [groovy] IDEA-288942: Refresh module info on every root change
    GitOrigin-RevId: ec27ee5b6911f6fa5c4552c83a34194bea725f66

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/transformations/macro/GroovyMacroModuleListener.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/transformations/macro/GroovyMacroModuleListener.kt
@@ -3,12 +3,23 @@ package org.jetbrains.plugins.groovy.transformations.macro
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.ModuleListener
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootEvent
+import com.intellij.openapi.roots.ModuleRootListener
 
-class GroovyMacroModuleListener : ModuleListener {
+class GroovyMacroModuleListener : ModuleListener, ModuleRootListener {
 
   override fun moduleAdded(project: Project, module: Module) = project.service<GroovyMacroRegistryService>().refreshModule(module)
 
   override fun moduleRemoved(project: Project, module: Module) = project.service<GroovyMacroRegistryService>().refreshModule(module)
+
+  override fun rootsChanged(event: ModuleRootEvent) {
+    val service = event.project.service<GroovyMacroRegistryService>()
+    for (module in ModuleManager.getInstance(event.project).modules) {
+      service.refreshModule(module)
+    }
+  }
+
 }
