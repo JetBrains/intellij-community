7764a7efaef92c212e12c7306b6affb64c3e1194
Bas Leijdekkers <basleijdekkers@gmail.com>
2022-03-07 21:03:27 +0100

    IG: fix typo in problem descriptor (IJPF-CR-4917)
    GitOrigin-RevId: 77db3e09a810d5ce0d94e1ba017098d6e7ed3d73

--- plugins/InspectionGadgets/test/com/siyeh/igtest/junit/unconstructable_test_case/UnconstructableTestCase2.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/junit/unconstructable_test_case/UnconstructableTestCase2.java
@@ -2,7 +2,7 @@ package com.siyeh.igtest.junit;
 
 import junit.framework.TestCase;
 
-public class <warning descr="Test class 'UnconstructableTestCase2' is not constructable because does not have a 'public' no-arg constructor">UnconstructableTestCase2</warning> extends TestCase
+public class <warning descr="Test class 'UnconstructableTestCase2' is not constructable because it does not have a 'public' no-arg constructor">UnconstructableTestCase2</warning> extends TestCase
 {
     public UnconstructableTestCase2(Object foo)
     {
