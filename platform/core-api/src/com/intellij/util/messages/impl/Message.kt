0e046203d05f91aa17451039f6d4c869bf286623
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-03-08 09:49:20 +0100

    cleanup
    GitOrigin-RevId: e65d897ca3087bd96bd5ef2bb3bd027068694ad8

--- platform/core-api/src/com/intellij/util/messages/impl/Message.kt
+++ platform/core-api/src/com/intellij/util/messages/impl/Message.kt
@@ -1,45 +1,32 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.util.messages.impl;
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.util.messages.impl
 
-import com.intellij.codeWithMe.ClientId;
-import com.intellij.util.messages.Topic;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.codeWithMe.ClientId
+import com.intellij.util.messages.Topic
+import java.lang.invoke.MethodHandle
 
-import java.lang.invoke.MethodHandle;
-import java.util.Arrays;
-
-final class Message {
-  final Topic<?> topic;
-  final String methodName;
-  final MethodHandle method;
+internal class Message(
+  @JvmField val topic: Topic<*>,
+  @JvmField val method: MethodHandle,
   // we don't bind args as part of MethodHandle creation, because object is not known yet - so, MethodHandle here is not ready to use
+  @JvmField val methodName: String,
   // it allows us to cache MethodHandle per method and partially reuse it
-  final Object[] args;
-  final @Nullable Object @NotNull [] handlers;
-  @NotNull
-  final String clientId;
+  @JvmField val args: Array<Any>,
+  @JvmField val handlers: Array<Any>
+) {
+  @JvmField
+  val clientId = ClientId.getCurrentValue()
 
   // to avoid creating Message for each handler
   // see note about pumpMessages in createPublisher (invoking job handlers can be stopped and continued as part of another pumpMessages call)
-  int currentHandlerIndex;
+  @JvmField var currentHandlerIndex = 0
 
-  Message(@NotNull Topic<?> topic, @NotNull MethodHandle method, @NotNull String methodName, Object[] args, @Nullable Object @NotNull [] handlers) {
-    this.topic = topic;
-    this.method = method;
-    this.methodName = methodName;
-    this.args = args;
-    this.handlers = handlers;
-    clientId = ClientId.getCurrentValue();
-  }
-
-  @Override
-  public String toString() {
+  override fun toString(): String {
     return "Message(" +
            "topic=" + topic +
            ", method=" + methodName +
-           ", args=" + Arrays.toString(args) +
-           ", handlers=" + Arrays.toString(handlers) +
-           ')';
+           ", args=" + args.contentToString() +
+           ", handlers=" + handlers.contentToString() +
+           ')'
   }
 }
