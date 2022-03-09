8e0b43016b0d94634416574d8f5bc28c732d8a9b
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2022-03-09 15:46:43 +0300

    IDEA-288845 added navigation from selected dependency in GTW to DA
    GitOrigin-RevId: 6336407b43187ea0bc2f9e5c507fd1ba1cd4864d

--- plugins/gradle/src/org/jetbrains/plugins/gradle/dependency/analyzer/GradleDependencyAnalyzerAction.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/dependency/analyzer/GradleDependencyAnalyzerAction.kt
@@ -2,59 +2,64 @@
 package org.jetbrains.plugins.gradle.dependency.analyzer
 
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.externalSystem.action.ExternalSystemActionUtil
 import com.intellij.openapi.externalSystem.dependency.analyzer.*
 import com.intellij.openapi.externalSystem.model.ExternalSystemDataKeys
 import com.intellij.openapi.externalSystem.model.project.*
-import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
-import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.externalSystem.model.project.dependencies.ArtifactDependencyNode
+import com.intellij.openapi.externalSystem.model.project.dependencies.DependencyScopeNode
+import com.intellij.openapi.externalSystem.model.project.dependencies.ProjectDependencyNode
+import com.intellij.openapi.externalSystem.view.ExternalSystemNode
+import com.intellij.openapi.externalSystem.view.ModuleNode
+import com.intellij.openapi.externalSystem.view.ProjectNode
+import com.intellij.openapi.util.text.StringUtil
 import org.jetbrains.plugins.gradle.util.GradleConstants
-import com.intellij.openapi.module.Module
 
 class GradleDependencyAnalyzerAction : AbstractDependencyAnalyzerAction() {
   override fun getSystemId(e: AnActionEvent) = GradleConstants.SYSTEM_ID
 
   override fun setSelectedState(e: AnActionEvent, view: DependencyAnalyzerView) {
-    val externalProjectPath = getExternalProjectPath(e.dataContext) ?: return
-    val dependencyData = getSelectedDependencyData(e.dataContext) ?: return
-    view.setSelectedDependency(externalProjectPath, dependencyData)
-  }
-
-  private fun getExternalProjectPath(context: DataContext): String? {
-    val module = getGradleModule(context) ?: findGradleModule(context)
-    return ExternalSystemApiUtil.getExternalProjectPath(module)
-  }
-
-  private fun getGradleModule(context: DataContext): Module? {
-    val module = ExternalSystemActionUtil.getModule(context) ?: return null
-    return if (module.isGradleModule()) module else null
-  }
-
-  private fun findGradleModule(context: DataContext): Module? {
-    val project = CommonDataKeys.PROJECT.getData(context) ?: return null
-    val selectedNodes = ExternalSystemDataKeys.SELECTED_NODES.getData(context)
-    val moduleName = when (val data = selectedNodes?.firstOrNull()?.data) {
-      is ProjectData -> data.internalName
-      is ModuleData -> data.internalName
-      else -> null
+    val selectedNode = getSelectedNode(e.dataContext) ?: return
+    val externalProjectPath = getExternalProjectPath(selectedNode) ?: return
+    val dependencyData = getDependencyData(selectedNode)
+    val dependencyScope = getDependencyScope(selectedNode)
+    if (dependencyData != null && dependencyScope != null) {
+      view.setSelectedDependency(externalProjectPath, dependencyData, dependencyScope)
     }
-    val manager = ModuleManager.getInstance(project)
-    val module = moduleName?.let { manager.findModuleByName(it) }
-    return if (module?.isGradleModule() == true) module else null
+    else if (dependencyData != null) {
+      view.setSelectedDependency(externalProjectPath, dependencyData)
+    }
+    else {
+      view.setSelectedExternalProject(externalProjectPath)
+    }
   }
 
-  private fun getSelectedDependencyData(context: DataContext): DependencyAnalyzerDependency.Data? {
-    val selectedNodes = ExternalSystemDataKeys.SELECTED_NODES.getData(context)
-    return when (val data = selectedNodes?.firstOrNull()?.data) {
+  private fun getSelectedNode(context: DataContext): ExternalSystemNode<*>? {
+    return ExternalSystemDataKeys.SELECTED_NODES.getData(context)?.firstOrNull()
+  }
+
+  private fun getExternalProjectPath(selectedNode: ExternalSystemNode<*>): String? {
+    if (selectedNode is ProjectNode) {
+      return selectedNode.data?.linkedExternalProjectPath
+    }
+    return selectedNode.findNode(ModuleNode::class.java)
+      ?.data?.linkedExternalProjectPath
+  }
+
+  private fun getDependencyData(selectedNode: ExternalSystemNode<*>): DependencyAnalyzerDependency.Data? {
+    return when (val data = selectedNode.data) {
       is ProjectData -> DAModule(data.internalName)
       is ModuleData -> DAModule(data.internalName)
-      else -> null
+      else -> when (val node = selectedNode.dependencyNode) {
+        is ProjectDependencyNode -> DAModule(node.projectName)
+        is ArtifactDependencyNode -> DAArtifact(node.group, node.module, node.version)
+        else -> null
+      }
     }
   }
 
-  private fun Module.isGradleModule(): Boolean {
-    return ExternalSystemApiUtil.isExternalSystemAwareModule(GradleConstants.SYSTEM_ID, this)
+  private fun getDependencyScope(selectedNode: ExternalSystemNode<*>): DependencyAnalyzerDependency.Scope? {
+    val dependencyNode = selectedNode.findDependencyNode(DependencyScopeNode::class.java) ?: return null
+    return DAScope(dependencyNode.scope, StringUtil.toTitleCase(dependencyNode.scope))
   }
 }
