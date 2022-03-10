da131b637439fc2517e99ee4cce713eda0c0f813
Gregory.Shrago <gregory.shrago@jetbrains.com>
2022-03-10 03:03:05 +0300

    decouple id and todo scanning
    `IdEntries` were collected only to be dropped when `TodoEntries` were requested.
    
    (cherry picked from commit 0a3cb3de6cc1f2c39bc34249400ff7104ec0f89e)
    
    IJ-CR-21719
    
    GitOrigin-RevId: 90be812f018608d03aea80640ff54e50c5f867ff

--- platform/indexing-impl/src/com/intellij/psi/impl/cache/impl/BaseFilterLexerUtil.java
+++ platform/indexing-impl/src/com/intellij/psi/impl/cache/impl/BaseFilterLexerUtil.java
@@ -18,58 +18,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class BaseFilterLexerUtil {
-  private static final ScanContent EMPTY = new ScanContent(Collections.emptyMap(), Collections.emptyMap());
 
-  public static @NotNull ScanContent scanContent(FileContent content, IdAndToDoScannerBasedOnFilterLexer indexer) {
-    return scanContent(content, false, indexer);
-  }
-
-  public static @NotNull ScanContent scanContent(FileContent content, boolean withTodos, IdAndToDoScannerBasedOnFilterLexer indexer) {
+  public static @NotNull Map<IdIndexEntry, Integer> calcIdEntries(@NotNull FileContent content,
+                                                                  @NotNull IdAndToDoScannerBasedOnFilterLexer indexer) {
     boolean needIdIndex = IdTableBuilding.getFileTypeIndexer(content.getFileType()) instanceof LexingIdIndexer;
-    boolean needTodo = withTodos && (TodoIndexers.needsTodoIndex(content) || content.getFile() instanceof LightVirtualFile);
-    IndexPattern[] todoPatterns = needTodo ? IndexPatternUtil.getIndexPatterns() : IndexPattern.EMPTY_ARRAY;
-    if (!needIdIndex && !needTodo || !needIdIndex && todoPatterns.length <= 0) return EMPTY;
-    return doScanContent(content, indexer, needIdIndex, needTodo, todoPatterns);
+    if (!needIdIndex) return Collections.emptyMap();
+    IdDataConsumer consumer = new IdDataConsumer();
+    OccurrenceConsumer occurrenceConsumer = new OccurrenceConsumer(consumer, false);
+    Lexer filterLexer = indexer.createLexer(occurrenceConsumer);
+    filterLexer.start(content.getContentAsText());
+    while (filterLexer.getTokenType() != null) {
+      filterLexer.advance();
+    }
+    return consumer.getResult();
   }
 
-  public static @NotNull ScanContent doScanContent(@NotNull FileContent content,
-                                                   @NotNull IdAndToDoScannerBasedOnFilterLexer indexer,
-                                                   boolean needIdIndex,
-                                                   boolean needTodo,
-                                                   IndexPattern @NotNull [] todoPatterns) {
-    final IdDataConsumer consumer = needIdIndex ? new IdDataConsumer() : null;
-    final OccurrenceConsumer todoOccurrenceConsumer = new OccurrenceConsumer(consumer, needTodo);
-    final Lexer filterLexer = indexer.createLexer(todoOccurrenceConsumer);
+  public static @NotNull Map<TodoIndexEntry, Integer> calcTodoEntries(@NotNull FileContent content,
+                                                                      @NotNull IdAndToDoScannerBasedOnFilterLexer indexer) {
+    boolean needTodo = TodoIndexers.needsTodoIndex(content) || content.getFile() instanceof LightVirtualFile;
+    IndexPattern[] todoPatterns = needTodo ? IndexPatternUtil.getIndexPatterns() : IndexPattern.EMPTY_ARRAY;
+    if (todoPatterns.length == 0) return Collections.emptyMap();
+
+    OccurrenceConsumer occurrenceConsumer = new OccurrenceConsumer(null, true);
+    Lexer filterLexer = indexer.createLexer(occurrenceConsumer);
     filterLexer.start(content.getContentAsText());
 
-    while (filterLexer.getTokenType() != null) filterLexer.advance();
-
-    Map<TodoIndexEntry,Integer> todoMap = null;
-    if (needTodo) {
-      for (IndexPattern indexPattern : todoPatterns) {
-          final int count = todoOccurrenceConsumer.getOccurrenceCount(indexPattern);
-          if (count > 0) {
-            if (todoMap == null) {
-              todoMap = new HashMap<>();
-            }
-            todoMap.put(new TodoIndexEntry(indexPattern.getPatternString(), indexPattern.isCaseSensitive()), count);
-          }
-        }
+    while (filterLexer.getTokenType() != null) {
+      filterLexer.advance();
     }
 
-    return new ScanContent(
-      consumer != null? consumer.getResult():Collections.emptyMap(),
-      todoMap != null ? todoMap: Collections.emptyMap()
-    );
-  }
-
-  public static final class ScanContent {
-    public final Map<IdIndexEntry, Integer> idMap;
-    public final Map<TodoIndexEntry,Integer> todoMap;
-
-    public ScanContent(Map<IdIndexEntry, Integer> _idMap, Map<TodoIndexEntry, Integer> _todoMap) {
-      idMap = _idMap;
-      todoMap = _todoMap;
+    Map<TodoIndexEntry, Integer> todoMap = null;
+    for (IndexPattern indexPattern : todoPatterns) {
+      int count = occurrenceConsumer.getOccurrenceCount(indexPattern);
+      if (count <= 0) continue;
+      if (todoMap == null) todoMap = new HashMap<>();
+      todoMap.put(new TodoIndexEntry(indexPattern.getPatternString(), indexPattern.isCaseSensitive()), count);
     }
+    return todoMap != null ? todoMap : Collections.emptyMap();
   }
 }
