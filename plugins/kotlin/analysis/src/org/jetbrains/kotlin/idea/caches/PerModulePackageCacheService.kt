d13850560e0112c4715b2be59d481277bf1638a5
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2022-03-10 17:21:05 +0300

    [kotlin] Use strong references for caching to have predictable results
     Added registry key for a fallback logic as there might be some implicit leaks
    
     ^KTIJ-21067 Fixed
    
    GitOrigin-RevId: 098bfb737337da7eccd320b2ec8f8feeb0a10e9d

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/caches/PerModulePackageCacheService.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/caches/PerModulePackageCacheService.kt
@@ -11,6 +11,7 @@ import com.intellij.openapi.project.rootManager
 import com.intellij.openapi.roots.ModuleRootEvent
 import com.intellij.openapi.roots.ModuleRootListener
 import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.newvfs.BulkFileListener
@@ -192,10 +193,12 @@ class ImplicitPackagePrefixCache(private val project: Project) {
 class PerModulePackageCacheService(private val project: Project) : Disposable {
 
     /*
-     * Actually an WeakMap<Module, SoftMap<ModuleSourceInfo, SoftMap<FqName, Boolean>>>
+     * Actually an WeakMap<Module, ConcurrentMap<ModuleSourceInfo, ConcurrentMap<FqName, Boolean>>>
      */
     private val cache = ContainerUtil.createConcurrentWeakMap<Module, ConcurrentMap<ModuleSourceInfo, ConcurrentMap<FqName, Boolean>>>()
     private val implicitPackagePrefixCache = ImplicitPackagePrefixCache(project)
+
+    private val useStrongMapForCaching = Registry.`is`("kotlin.cache.packages.strong.map", false)
 
     private val pendingVFileChanges: MutableSet<VFileEvent> = mutableSetOf()
     private val pendingKtFileChanges: MutableSet<KtFile> = mutableSetOf()
@@ -303,10 +306,10 @@ class PerModulePackageCacheService(private val project: Project) : Disposable {
         checkPendingChanges()
 
         val perSourceInfoCache = cache.getOrPut(module) {
-            CollectionFactory.createConcurrentSoftMap()
+            if (useStrongMapForCaching) ConcurrentHashMap() else CollectionFactory.createConcurrentSoftMap()
         }
         val cacheForCurrentModuleInfo = perSourceInfoCache.getOrPut(moduleInfo) {
-            CollectionFactory.createConcurrentSoftMap()
+            if (useStrongMapForCaching) ConcurrentHashMap() else CollectionFactory.createConcurrentSoftMap()
         }
 
         return cacheForCurrentModuleInfo.getOrPut(packageFqName) {
