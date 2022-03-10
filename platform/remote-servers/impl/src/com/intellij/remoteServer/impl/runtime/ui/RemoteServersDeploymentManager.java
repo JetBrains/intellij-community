e67d497e2677ff3ae30edca2ab9f1d4daf29a2dd
Andrii Zinchenko <andrii.zinchenko@jetbrains.com>
2022-03-10 15:02:03 +0100

    [docker] RemoteServersDeploymentManager: fix LocalDeploymentImpl selection
    GitOrigin-RevId: 32aa6d0c37a5107ea19a0359217a171d43977f58

--- platform/remote-servers/impl/src/com/intellij/remoteServer/impl/runtime/ui/RemoteServersDeploymentManager.java
+++ platform/remote-servers/impl/src/com/intellij/remoteServer/impl/runtime/ui/RemoteServersDeploymentManager.java
@@ -18,6 +18,7 @@ import com.intellij.remoteServer.impl.runtime.ui.tree.ServersTreeNodeSelector;
 import com.intellij.remoteServer.impl.runtime.ui.tree.ServersTreeStructure.RemoteServerNode;
 import com.intellij.remoteServer.runtime.*;
 import com.intellij.remoteServer.runtime.ui.RemoteServersView;
+import com.intellij.remoteServer.util.CloudApplicationRuntime;
 import com.intellij.util.Alarm;
 import com.intellij.util.containers.CollectionFactory;
 import org.jetbrains.annotations.NotNull;
@@ -269,7 +270,11 @@ public final class RemoteServersDeploymentManager {
         ServiceEventListener.EventType.SERVICE_STRUCTURE_CHANGED, serverContributor, contributor.getClass()));
 
       for (Deployment deployment : connection.getDeployments()) {
-        if (deployment.getName().equals(deploymentName)) {
+        var runtime = deployment.getRuntime();
+
+        if (deployment.getName().equals(deploymentName) ||
+            (runtime instanceof CloudApplicationRuntime &&
+             ((CloudApplicationRuntime)runtime).getApplicationName().equals(deploymentName))) {
           return contributor.createDeploymentNode(connection, serverNode, deployment);
         }
       }
