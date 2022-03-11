5bbcd7006c7f6a10aaed9b8721be43f797770a20
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2022-03-11 10:03:59 +0100

    [jvm] Refactor JUnit rule Java test
    GitOrigin-RevId: ed4cbdef35404b071a77a8d48f921131c67f21e3

--- jvm/jvm-analysis-java-tests/testSrc/com/intellij/codeInspection/tests/java/JavaJUnitRuleInspectionTest.kt
+++ jvm/jvm-analysis-java-tests/testSrc/com/intellij/codeInspection/tests/java/JavaJUnitRuleInspectionTest.kt
@@ -1,33 +1,155 @@
 package com.intellij.codeInspection.tests.java
 
 import com.intellij.codeInspection.tests.JUnitRuleInspectionTestBase
-import com.intellij.jvm.analysis.JavaJvmAnalysisTestUtil
-import com.intellij.testFramework.TestDataPath
 
-private const val inspectionPath = "/codeInspection/junitrule"
-
-@TestDataPath("\$CONTENT_ROOT/testData$inspectionPath")
 class JavaJUnitRuleInspectionTest : JUnitRuleInspectionTestBase() {
-  override fun getBasePath() = JavaJvmAnalysisTestUtil.TEST_DATA_PROJECT_RELATIVE_BASE_PATH + inspectionPath
-
-
   fun `test @Rule highlighting`() {
-    myFixture.testHighlighting("RuleTest.java")
+    myFixture.testHighlighting(ULanguage.JAVA, """
+      package test;
+
+      import org.junit.Rule;
+      import org.junit.rules.TestRule;
+
+      class RuleTest {
+        @Rule
+        private int <error descr="Fields annotated with '@org.junit.Rule' should be 'public'">x</error>;
+
+        @Rule
+        public static int <error descr="Fields annotated with '@org.junit.Rule' should be non-static">y</error>;
+      }
+    """.trimIndent())
   }
 
-  fun `test @Rule quickFixes`() {
-    val quickfixes = myFixture.getAllQuickFixes("RuleQfTest.java")
-    quickfixes.forEach { myFixture.launchAction(it) }
-    myFixture.checkResultByFile("RuleQfTest.after.java")
+  fun `test @Rule make public`() {
+    myFixture.testQuickFix(ULanguage.JAVA, """
+      package test;
+
+      import org.junit.Rule;
+      import org.junit.rules.TestRule;
+
+      class RuleQfTest {
+        @Rule
+        private int x<caret>;
+
+        @Rule
+        public int y;
+      }
+    """.trimIndent(), """
+      package test;
+      
+      import org.junit.Rule;
+      import org.junit.rules.TestRule;
+      
+      class RuleQfTest {
+        @Rule
+        public int x;
+      
+        @Rule
+        public int y;
+      }
+    """.trimIndent(), "Make 'x' public")
   }
 
   fun `test @ClassRule highlighting`() {
-    myFixture.testHighlighting("ClassRuleTest.java")
+    myFixture.testHighlighting(ULanguage.JAVA, """
+      package test;
+
+      import org.junit.rules.TestRule;
+      import org.junit.ClassRule;
+      import test.SomeTestRule;
+
+      class ClassRuleTest {
+        @ClassRule
+        static SomeTestRule <error descr="Fields annotated with '@org.junit.ClassRule' should be 'public'">x</error> = new SomeTestRule();
+
+        @ClassRule
+        public SomeTestRule <error descr="Fields annotated with '@org.junit.ClassRule' should be 'static'">y</error> = new SomeTestRule();
+
+        @ClassRule
+        private SomeTestRule <error descr="Fields annotated with '@org.junit.ClassRule' should be 'public' and 'static'">z</error> = new SomeTestRule();
+
+        @ClassRule
+        public static int <error descr="Field type should be subtype of 'org.junit.rules.TestRule'">t</error> = 0;
+      }
+    """.trimIndent())
   }
 
-  fun `test @ClassRule quickFixes`() {
-    val quickfixes = myFixture.getAllQuickFixes("ClassRuleQfTest.java")
-    quickfixes.forEach { myFixture.launchAction(it) }
-    myFixture.checkResultByFile("ClassRuleQfTest.after.java")
+  fun `test @ClassRule make public`() {
+    myFixture.testQuickFix(ULanguage.JAVA, """
+      package test;
+
+      import org.junit.rules.TestRule;
+      import org.junit.ClassRule;
+      import test.SomeTestRule;
+
+      class ClassRuleTest {
+        @ClassRule
+        static SomeTestRule x<caret> = new SomeTestRule();
+      }
+    """.trimIndent(), """
+      package test;
+
+      import org.junit.rules.TestRule;
+      import org.junit.ClassRule;
+      import test.SomeTestRule;
+
+      class ClassRuleTest {
+        @ClassRule
+        public static SomeTestRule x = new SomeTestRule();
+      }
+    """.trimIndent(), "Make 'x' public")
+  }
+
+  fun `test @ClassRule make static`() {
+    myFixture.testQuickFix(ULanguage.JAVA, """
+      package test;
+
+      import org.junit.rules.TestRule;
+      import org.junit.ClassRule;
+      import test.SomeTestRule;
+
+      class ClassRuleTest {
+        @ClassRule
+        public SomeTestRule y<caret> = new SomeTestRule();
+      }
+    """.trimIndent(), """
+      package test;
+
+      import org.junit.rules.TestRule;
+      import org.junit.ClassRule;
+      import test.SomeTestRule;
+
+      class ClassRuleTest {
+        @ClassRule
+        public static SomeTestRule y = new SomeTestRule();
+      }
+    """.trimIndent(), "Make 'y' static")
+  }
+
+
+  fun `test @ClassRule make public and static`() {
+    myFixture.testQuickFix(ULanguage.JAVA, """
+      package test;
+
+      import org.junit.rules.TestRule;
+      import org.junit.ClassRule;
+      import test.SomeTestRule;
+
+      class ClassRuleTest {
+        @ClassRule
+        private SomeTestRule z<caret> = new SomeTestRule();
+      }
+    """.trimIndent(), """
+      package test;
+
+      import org.junit.rules.TestRule;
+      import org.junit.ClassRule;
+      import test.SomeTestRule;
+
+      class ClassRuleTest {
+        @ClassRule
+        public static SomeTestRule z = new SomeTestRule();
+      }
+    """.trimIndent(), "Make 'z' public", "Make 'z' static")
   }
 }
