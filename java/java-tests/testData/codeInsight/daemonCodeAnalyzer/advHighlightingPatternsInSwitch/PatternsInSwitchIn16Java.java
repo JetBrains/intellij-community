8526e26f991e1d785f57de004ff04b5893c0f9b3
Vladimir.Petrenko <vladimir.petrenko@jetbrains.com>
2022-03-14 13:45:43 +0300

    IDEA-290334 - fix red code warnings regarding patterns in switch/instanceof for older java versions
    GitOrigin-RevId: 680b7aad6b95ecc72715118c5e5b4a3302f3e566

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlightingPatternsInSwitch/PatternsInSwitchIn16Java.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlightingPatternsInSwitch/PatternsInSwitchIn16Java.java
@@ -1,9 +1,9 @@
 class X {
   int switchTest(Object obj) {
     return switch (obj) {
-      case <error descr="Guarded and parenthesized patterns are not supported at language level '16'">(String s)</error> -> 1;
-      case <error descr="Guarded and parenthesized patterns are not supported at language level '16'">Integer i && predicate()</error> -> 2;
-      case <error descr="Patterns in switch are not supported at language level '16'">Integer i</error> -> 3;
+      case (String <error descr="Patterns in switch are not supported at language level '16'">s</error>) -> 1;
+      case Integer <error descr="Patterns in switch are not supported at language level '16'">i</error> && predicate() -> 2;
+      case Integer <error descr="Patterns in switch are not supported at language level '16'">i</error> -> 3;
       case <error descr="Patterns in switch are not supported at language level '16'">default</error> -> 4;
       case <error descr="Patterns in switch are not supported at language level '16'">null</error> -> 10;
     };
