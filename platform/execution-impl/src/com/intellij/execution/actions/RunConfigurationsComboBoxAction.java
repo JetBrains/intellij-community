bb575bae4fbf065b6cc1b4da5d2cff04a27b09c7
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2022-03-14 21:31:55 +0300

    IJP-1167 Disable the 'Run Current File' feature in 2022.1.
    (cherry picked from commit 41524938b2dbd243221b59653f267d9445cbaa54)
    
    IJ-CR-21979
    
    GitOrigin-RevId: 536b121ccc305cb6e6934075e281fb511fe1041c

--- platform/execution-impl/src/com/intellij/execution/actions/RunConfigurationsComboBoxAction.java
+++ platform/execution-impl/src/com/intellij/execution/actions/RunConfigurationsComboBoxAction.java
@@ -20,7 +20,6 @@ import com.intellij.openapi.util.registry.Registry;
 import com.intellij.ui.SizedIcon;
 import com.intellij.ui.components.panels.NonOpaquePanel;
 import com.intellij.ui.scale.JBUIScale;
-import com.intellij.util.PlatformUtils;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.EmptyIcon;
 import com.intellij.util.ui.JBUI;
@@ -50,12 +49,6 @@ public class RunConfigurationsComboBoxAction extends ComboBoxAction implements D
       // The 'Run Current File' feature doesn't look great together with the Run Widget.
       return false;
     }
-
-    if (PlatformUtils.isIdeaUltimate()) return true;
-    if (PlatformUtils.isIdeaCommunity()) return true;
-    if (PlatformUtils.isPhpStorm()) return true;
-    if (PlatformUtils.isWebStorm()) return true;
-    if (PlatformUtils.isRubyMine()) return true;
 
     return Registry.is("run.current.file.item.in.run.configurations.combobox");
   }
