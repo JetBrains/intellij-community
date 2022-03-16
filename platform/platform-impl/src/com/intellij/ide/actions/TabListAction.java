1de673f911158a7f7a092f77a150709e3c4f3713
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2022-03-16 17:00:55 +0300

    [New UI] Add chevronDownLarge icon
    GitOrigin-RevId: 2b572f8543f6f1a688e87cdbcaa31d2e6681c76b

--- platform/platform-impl/src/com/intellij/ide/actions/TabListAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/TabListAction.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.actions;
 
 import com.intellij.icons.AllIcons;
@@ -6,8 +6,11 @@ import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.ui.ExperimentalUI;
+import com.intellij.ui.IconManager;
 import com.intellij.ui.tabs.impl.MorePopupAware;
 import org.jetbrains.annotations.NotNull;
+import javax.swing.Icon;
 
 /**
  * Shows the popup of all tabs when single row editor tab layout is used and all tabs don't fit on the screen.
@@ -23,7 +26,7 @@ public class TabListAction extends DumbAwareAction {
 
   @Override
   public void update(@NotNull AnActionEvent e) {
-    e.getPresentation().setIcon(AllIcons.Actions.FindAndShowNextMatches);
+    e.getPresentation().setIcon(ExperimentalUI.isNewUI() ? LazyIcon.CHEVRON_DOWN_LARGE : AllIcons.Actions.FindAndShowNextMatches);
     if (ApplicationManager.getApplication().isHeadlessEnvironment()) {
       e.getPresentation().setEnabledAndVisible(false);
       return;
@@ -35,5 +38,10 @@ public class TabListAction extends DumbAwareAction {
   private static boolean isTabListAvailable(@NotNull AnActionEvent e) {
     MorePopupAware morePopupAware = e.getData(MorePopupAware.KEY);
     return morePopupAware != null && morePopupAware.canShowMorePopup();
+  }
+
+  private static final class LazyIcon {
+    private static final Icon CHEVRON_DOWN_LARGE =
+      IconManager.getInstance().loadRasterizedIcon("expui/general/chevronDownLarge.svg", AllIcons.class.getClassLoader(), 2, 2);
   }
 }
