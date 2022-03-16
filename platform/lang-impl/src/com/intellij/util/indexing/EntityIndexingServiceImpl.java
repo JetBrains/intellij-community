ed8bda1445bcbc261c5f76bdbfdf082d15d00e68
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2022-03-16 18:36:27 +0100

    EA-456143 - UOE: LightEditDumbService.reportUnavailable
    No indexing of changes for Light Edit projects, for they do not use indexes
    
    (cherry picked from commit c59f443d0d02420494bfe67eb824921c6ffe9153)
    
    IJ-CR-22137
    
    GitOrigin-RevId: efa417ad4c5de62f427d2aae9b184c0ae3c92923

--- platform/lang-impl/src/com/intellij/util/indexing/EntityIndexingServiceImpl.java
+++ platform/lang-impl/src/com/intellij/util/indexing/EntityIndexingServiceImpl.java
@@ -1,6 +1,7 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.indexing;
 
+import com.intellij.ide.lightEdit.LightEdit;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
@@ -34,6 +35,7 @@ class EntityIndexingServiceImpl implements EntityIndexingService {
   @Override
   public void indexChanges(@NotNull Project project, @NotNull List<? extends RootsChangeIndexingInfo> changes) {
     if (!(FileBasedIndex.getInstance() instanceof FileBasedIndexImpl)) return;
+    if (LightEdit.owns(project)) return;
     if (changes.isEmpty()) {
       runFullReindex(project, "Project roots have changed");
     }
