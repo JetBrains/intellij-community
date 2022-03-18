acb1eb594385ebde445dfbb52311e1316c5839a7
Anna Kozlova <anna.kozlova@jetbrains.com>
2022-03-18 11:42:14 +0100

    [kotlin] disable unused declaration reporting on anything but java (KTIJ-20494)
    GitOrigin-RevId: 5162c42b4d19ace347400ba927a415788a3e3d80

--- java/java-impl/src/com/intellij/codeInspection/deadCode/UnusedDeclarationPresentation.java
+++ java/java-impl/src/com/intellij/codeInspection/deadCode/UnusedDeclarationPresentation.java
@@ -14,6 +14,7 @@ import com.intellij.concurrency.ConcurrentCollectionFactory;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.util.PsiNavigationSupport;
 import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.lang.java.JavaLanguage;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
@@ -429,6 +430,7 @@ public class UnusedDeclarationPresentation extends DefaultInspectionToolPresenta
               (myFixedElements.containsKey(refElement) ||
               isExcluded(refEntity) ||
               isSuppressed(refElement))) && refElement.isValid() && getFilter().accepts(refElement)) {
+          if (refElement.getPsiElement().getLanguage() != JavaLanguage.INSTANCE) return;
           if (skipEntryPoints(refElement)) return;
           registerContentEntry(refEntity, RefJavaUtil.getInstance().getPackageName(refEntity));
         }
