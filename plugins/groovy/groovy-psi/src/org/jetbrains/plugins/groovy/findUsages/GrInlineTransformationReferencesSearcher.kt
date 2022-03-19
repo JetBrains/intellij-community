cb7acfa4e016a10d63381bbfc12af298b6cb5749
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-03-19 21:14:12 +0300

    [groovy] IDEA-288942: Don't run search for references after transformations unnecessarily
    GitOrigin-RevId: f2326b38c2a4a96210dd854c87011f5681fec2b2

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/findUsages/GrInlineTransformationReferencesSearcher.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/findUsages/GrInlineTransformationReferencesSearcher.kt
@@ -11,6 +11,7 @@ import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.refactoring.suggested.endOffset
 import com.intellij.refactoring.suggested.startOffset
 import com.intellij.util.Processor
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFileBase
 import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElement
 import org.jetbrains.plugins.groovy.lang.psi.GroovyRecursiveElementVisitor
 import org.jetbrains.plugins.groovy.transformations.inline.getHierarchicalInlineTransformationData
@@ -18,6 +19,9 @@ import org.jetbrains.plugins.groovy.transformations.inline.getHierarchicalInline
 class GrInlineTransformationReferencesSearcher : QueryExecutorBase<PsiReference, ReferencesSearch.SearchParameters>() {
   override fun processQuery(queryParameters: ReferencesSearch.SearchParameters, consumer: Processor<in PsiReference>) {
     val target = queryParameters.elementToSearch
+    if (target.containingFile !is GroovyFileBase) {
+      return
+    }
     runReadAction {
       val (call, performer) = getHierarchicalInlineTransformationData(target) ?: return@runReadAction
       call.accept(object : GroovyRecursiveElementVisitor() {
