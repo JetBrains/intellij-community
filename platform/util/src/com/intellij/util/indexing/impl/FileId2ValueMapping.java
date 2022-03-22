5470f742612a9b1636e1cf115852168d5c8f62dc
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2022-03-22 08:57:18 +0400

    revert back value container cleanup after it's read from storage KTIJ-21387
    GitOrigin-RevId: 51229cde4c52b4bc25fcfa16545d911647adc8bb

--- platform/util/src/com/intellij/util/indexing/impl/FileId2ValueMapping.java
+++ platform/util/src/com/intellij/util/indexing/impl/FileId2ValueMapping.java
@@ -1,42 +1,53 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.indexing.impl;
 
+import com.intellij.util.SmartList;
+import it.unimi.dsi.fastutil.Pair;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 final class FileId2ValueMapping<Value> {
-  private final @NotNull Int2ObjectMap<Object> id2ValueMap = new Int2ObjectOpenHashMap<>();
+  private final @NotNull Int2ObjectMap<Value> id2ValueMap = new Int2ObjectOpenHashMap<>();
   private final @NotNull ValueContainerImpl<Value> valueContainer;
 
   FileId2ValueMapping(@NotNull ValueContainerImpl<Value> valueContainer) {
     this.valueContainer = valueContainer;
 
+    List<Pair<Value, Integer>> cleanupDeletions = new SmartList<>();
+
     valueContainer.forEach((id, value) -> {
-      Object previousValue = associateFileIdToValueSkippingContainer(id, value);
+      Value previousValue = associateFileIdToValueSkippingContainer(id, value);
       if (previousValue != null) {
-        ValueContainerImpl.LOG.error("Duplicated value for id = " + id + " in " + valueContainer.getDebugMessage());
+        cleanupDeletions.add(Pair.of(previousValue, id));
+        //ValueContainerImpl.LOG.error("Duplicated value for id = " + id + " in " + valueContainer.getDebugMessage());
       }
       return true;
     });
+
+    for (Pair<Value, Integer> deletion : cleanupDeletions) {
+      valueContainer.removeValue(deletion.second(), ValueContainerImpl.unwrap(deletion.first()));
+    }
   }
-  @SuppressWarnings("unchecked")
+
   void associateFileIdToValue(int fileId, Value value) {
-    Object previousValue = associateFileIdToValueSkippingContainer(fileId, value);
+    Value previousValue = associateFileIdToValueSkippingContainer(fileId, value);
     if (previousValue != null) {
-      valueContainer.removeValue(fileId, ValueContainerImpl.unwrap((Value)previousValue));
+      valueContainer.removeValue(fileId, ValueContainerImpl.unwrap(previousValue));
     }
     valueContainer.addValue(fileId, value);
   }
-  Object associateFileIdToValueSkippingContainer(int fileId, Value value) {
+
+  Value associateFileIdToValueSkippingContainer(int fileId, Value value) {
     return id2ValueMap.put(fileId, ValueContainerImpl.wrapValue(value));
   }
 
-  @SuppressWarnings("unchecked")
   boolean removeFileId(int inputId) {
-    Object mapped = id2ValueMap.remove(inputId);
+    Value mapped = id2ValueMap.remove(inputId);
     if (mapped != null) {
-      valueContainer.removeValue(inputId, ValueContainerImpl.unwrap((Value)mapped));
+      valueContainer.removeValue(inputId, ValueContainerImpl.unwrap(mapped));
     }
     return mapped != null;
   }
