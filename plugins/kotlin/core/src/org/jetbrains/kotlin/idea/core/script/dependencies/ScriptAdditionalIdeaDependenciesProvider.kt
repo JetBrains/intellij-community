f316691c7909a5d2b14428f1622667c79425b65a
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-03-22 13:37:06 +0700

    [kotlin] ScriptAdditionalIdeaDependenciesProvider: cleanup code
    GitOrigin-RevId: 85d875c6ef50e42291a72990c63c571fd3c8c9d2

--- plugins/kotlin/core/src/org/jetbrains/kotlin/idea/core/script/dependencies/ScriptAdditionalIdeaDependenciesProvider.kt
+++ plugins/kotlin/core/src/org/jetbrains/kotlin/idea/core/script/dependencies/ScriptAdditionalIdeaDependenciesProvider.kt
@@ -1,9 +1,8 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.core.script.dependencies
 
 import com.intellij.openapi.extensions.ExtensionPointName
-import com.intellij.openapi.extensions.Extensions
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.libraries.Library
@@ -15,18 +14,14 @@ abstract class ScriptAdditionalIdeaDependenciesProvider {
 
     companion object {
         private val EP_NAME: ExtensionPointName<ScriptAdditionalIdeaDependenciesProvider> =
-            ExtensionPointName.create<ScriptAdditionalIdeaDependenciesProvider>("org.jetbrains.kotlin.scriptAdditionalIdeaDependenciesProvider")
+            ExtensionPointName.create("org.jetbrains.kotlin.scriptAdditionalIdeaDependenciesProvider")
 
-        fun getRelatedModules(file: VirtualFile, project: Project): List<Module> {
-            return Extensions.getArea(project).getExtensionPoint(EP_NAME).extensions
-                .filterIsInstance<ScriptAdditionalIdeaDependenciesProvider>()
-                .flatMap { it.getRelatedModules(file, project) }
-        }
+        fun getRelatedModules(file: VirtualFile, project: Project): List<Module> = EP_NAME.getExtensionList(project)
+            .filterIsInstance<ScriptAdditionalIdeaDependenciesProvider>()
+            .flatMap { it.getRelatedModules(file, project) }
 
-        fun getRelatedLibraries(file: VirtualFile, project: Project): List<Library> {
-            return Extensions.getArea(project).getExtensionPoint(EP_NAME).extensions
-                .filterIsInstance<ScriptAdditionalIdeaDependenciesProvider>()
-                .flatMap { it.getRelatedLibraries(file, project) }
-        }
+        fun getRelatedLibraries(file: VirtualFile, project: Project): List<Library> = EP_NAME.getExtensionList(project)
+            .filterIsInstance<ScriptAdditionalIdeaDependenciesProvider>()
+            .flatMap { it.getRelatedLibraries(file, project) }
     }
 }
