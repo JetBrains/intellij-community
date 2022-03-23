cde9d2d50933c8b7633f38964316991a957a1066
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2022-03-23 08:41:00 +0100

    [platform] `ComboBox` API optimization (pushing a pair of helpers into the base class)
    GitOrigin-RevId: f1d1aa207f1b15381406bd57efd5a92a32e1460f

--- platform/platform-api/src/com/intellij/openapi/ui/ComboBoxWithWidePopup.java
+++ platform/platform-api/src/com/intellij/openapi/ui/ComboBoxWithWidePopup.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.ui;
 
 import com.intellij.ui.SimpleColoredComponent;
@@ -20,6 +20,15 @@ public class ComboBoxWithWidePopup<E> extends JComboBox<E> {
 
   public ComboBoxWithWidePopup(E @NotNull [] items) {
     super(items);
+  }
+
+  @SuppressWarnings("unchecked")
+  public E getItem() {
+    return (E)getSelectedItem();
+  }
+
+  public void setItem(E item) {
+    setSelectedItem(item);
   }
 
   @Override
