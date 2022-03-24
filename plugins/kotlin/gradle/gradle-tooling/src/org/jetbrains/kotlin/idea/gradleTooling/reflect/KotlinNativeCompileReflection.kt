93a7a1a81c74e0f106db873d95d7a98b8857705e
Yaroslav Chernyshev <Yaroslav.Chernyshev@jetbrains.com>
2022-03-24 12:10:39 +0300

    Revert "[import] Support destinationDirectory for KotlinNativeCompile"
    This reverts commit f89af734cd14b2147f6634c41a67534f243c3034.
    
    GitOrigin-RevId: f4dfa77994f5fabd023db7848189d660d9b7fb88

--- plugins/kotlin/gradle/gradle-tooling/src/org/jetbrains/kotlin/idea/gradleTooling/reflect/KotlinNativeCompileReflection.kt
+++ plugins/kotlin/gradle/gradle-tooling/src/org/jetbrains/kotlin/idea/gradleTooling/reflect/KotlinNativeCompileReflection.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.gradleTooling.reflect
 
-import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Provider
 import java.io.File
 
@@ -14,23 +13,12 @@ interface KotlinNativeCompileReflection {
 
 private class KotlinNativeCompileReflectionImpl(private val instance: Any) : KotlinNativeCompileReflection {
     override val destinationDir: File? by lazy {
-        val instanceClazz = instance::class.java
-        when {
-            instanceClazz.getMethod("getDestinationDirectory") != null ->
-                instance.callReflectiveGetter<DirectoryProperty>("getDestinationDirectory", logger)?.asFile?.get()
-
-            instanceClazz.getMethod("getDestinationDir") != null ->
-                instance.callReflectiveGetter("getDestinationDir", logger)
-
-            instanceClazz.getMethod("getOutputFile") != null ->
-                when (val outputFile = instance.callReflectiveAnyGetter("getOutputFile", logger)) {
-                    is Provider<*> -> outputFile.orNull as? File
-                    is File -> outputFile
-                    else -> null
-                }?.parentFile
-
-            else -> null
-        }
+        instance.callReflectiveGetter("getDestinationDir", logger)
+            ?: when(val outputFile = instance.callReflective("getOutputFile", parameters(), returnType<Any>(), logger)) {
+                is Provider<*> -> outputFile.orNull as? File
+                is File -> outputFile
+                else -> null
+            }?.parentFile
     }
 
     companion object {
