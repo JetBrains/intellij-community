fa01934443198b0b3d345d1218b3ac237ec7b6e6
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2022-03-25 18:23:51 +0300

    IDEA-289479 Run Current File: icons aren't updated when select current configuration
    GitOrigin-RevId: 7e2711bc31236aa6e765986a0ab4dae10482481b

--- platform/execution-impl/src/com/intellij/execution/impl/RunnerAndConfigurationSettingsImpl.kt
+++ platform/execution-impl/src/com/intellij/execution/impl/RunnerAndConfigurationSettingsImpl.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.impl
 
 import com.intellij.configurationStore.SerializableScheme
@@ -89,6 +89,8 @@ class RunnerAndConfigurationSettingsImpl @JvmOverloads constructor(
   private var folderName: String? = null
 
   private var uniqueId: String? = null
+
+  var filePathIfRunningCurrentFile: String? = null
 
   override fun getFactory() = _configuration?.factory ?: UnknownConfigurationType.getInstance()
 
