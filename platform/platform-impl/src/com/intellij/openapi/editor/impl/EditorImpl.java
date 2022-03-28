1cafdeb3cb30798baf209adf111c212df2fcd6ab
Liudmila Kornilova <kornilova203@gmail.com>
2022-03-28 14:16:47 +0200

    DBE-13853 Turn on new behaviour for actions invoked outside of selection in all IDEs
    GitOrigin-RevId: 26d3f46dc285ba2411cbef5dd0020c900ae9bb9e

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
@@ -3273,7 +3273,7 @@ public final class EditorImpl extends UserDataHolderBase implements EditorEx, Hi
     @Override
     public boolean isCopyVisible(@NotNull DataContext dataContext) {
       Caret caret = dataContext.getData(CommonDataKeys.CARET);
-      return PlatformUtils.isDataGrip() && caret != null
+      return caret != null
              ? isCaretInsideSelection(caret)
              : getSelectionModel().hasSelection(true);
     }
@@ -3292,7 +3292,7 @@ public final class EditorImpl extends UserDataHolderBase implements EditorEx, Hi
     public boolean isCutVisible(@NotNull DataContext dataContext) {
       Caret caret = dataContext.getData(CommonDataKeys.CARET);
       return isCutEnabled(dataContext) &&
-             (PlatformUtils.isDataGrip() && caret != null
+             (caret != null
               ? isCaretInsideSelection(caret)
               : getSelectionModel().hasSelection(true));
     }
