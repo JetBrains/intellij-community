00365469bad9367931b6d240cdc6e3ed283b6f2b
Ivan Semenov <ivan.semenov@jetbrains.com>
2022-03-29 18:30:46 +0300

    [ui] revert scaling html image view to inheritance instead of delegation
    Fixes IDEA-290763
    
    GitOrigin-RevId: 6775939ed36b364bdeb8e6eb7fc68d5c8b80ca30

--- platform/util/ui/src/com/intellij/util/ui/html/HiDpiScalingImageView.kt
+++ platform/util/ui/src/com/intellij/util/ui/html/HiDpiScalingImageView.kt
@@ -10,10 +10,9 @@ import java.awt.Rectangle
 import java.awt.Shape
 import java.awt.image.BufferedImage
 import javax.swing.text.Element
-import javax.swing.text.Position
-import javax.swing.text.View
+import javax.swing.text.html.ImageView
 
-class HiDpiScalingImageView(elem: Element, private val originalView: View) : View(elem) {
+class HiDpiScalingImageView(elem: Element) : ImageView(elem) {
 
   private val scaleContext: ScaleContext?
     get() = container?.let(ScaleContext::create)
@@ -21,35 +20,26 @@ class HiDpiScalingImageView(elem: Element, private val originalView: View) : Vie
   private val sysScale: Float
     get() = scaleContext?.getScale(ScaleType.SYS_SCALE)?.toFloat() ?: 1f
 
-  override fun getMaximumSpan(axis: Int): Float = originalView.getMaximumSpan(axis) / sysScale
+  override fun getMaximumSpan(axis: Int): Float = super.getMaximumSpan(axis) / sysScale
 
-  override fun getMinimumSpan(axis: Int): Float = originalView.getMinimumSpan(axis) / sysScale
+  override fun getMinimumSpan(axis: Int): Float = super.getMinimumSpan(axis) / sysScale
 
-  override fun getPreferredSpan(axis: Int): Float = originalView.getPreferredSpan(axis) / sysScale
+  override fun getPreferredSpan(axis: Int): Float = super.getPreferredSpan(axis) / sysScale
 
   override fun paint(g: Graphics, a: Shape) {
     val scaleContext = scaleContext
     if (scaleContext == null) {
-      originalView.paint(g, a)
+      super.paint(g, a)
       return
     }
 
     val bounds = a.bounds
-    val width = originalView.getPreferredSpan(X_AXIS).toInt()
-    val height = originalView.getPreferredSpan(Y_AXIS).toInt()
+    val width = super.getPreferredSpan(X_AXIS).toInt()
+    val height = super.getPreferredSpan(Y_AXIS).toInt()
     if (width <= 0 || height <= 0) return
     val image = ImageUtil.createImage(width, height, BufferedImage.TYPE_INT_ARGB)
     val graphics = image.createGraphics()
-    originalView.paint(graphics, Rectangle(image.width, image.height))
+    super.paint(graphics, Rectangle(image.width, image.height))
     StartupUiUtil.drawImage(g, ImageUtil.ensureHiDPI(image, scaleContext), bounds.x, bounds.y, null)
   }
-
-  override fun modelToView(pos: Int, a: Shape?, b: Position.Bias?): Shape =
-    originalView.modelToView(pos, a, b)
-
-  override fun viewToModel(x: Float, y: Float, a: Shape?, biasReturn: Array<out Position.Bias>?): Int =
-    originalView.viewToModel(x, y, a, biasReturn)
-
-  override fun getToolTipText(x: Float, y: Float, allocation: Shape?): String? =
-    originalView.getToolTipText(x, y, allocation)
 }
