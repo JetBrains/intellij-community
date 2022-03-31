5f0e3bce556bac0d7a9743eeac85b0a51b67974c
Roman Ivanov <roman.ivanov@jetbrains.com>
2022-03-31 17:50:48 +0200

    [code vision] check editor expiration in hints update in settings
    GitOrigin-RevId: 9dd1245b9b510c53b19ccf71f903f1842ede3aae

--- platform/lang-impl/src/com/intellij/codeInsight/hints/settings/language/ParameterInlayProviderSettingsModel.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/hints/settings/language/ParameterInlayProviderSettingsModel.kt
@@ -83,7 +83,6 @@ class ParameterInlayProviderSettingsModel(
                                                }
                                              }, DaemonProgressIndicator())
     return Runnable {
-      if (editor.isDisposed) return@Runnable
       ParameterHintsPass(file, editor, HintInfoFilter { true }, true).doApplyInformationToEditor() // clean up hints
       pass.doApplyInformationToEditor()
     }
