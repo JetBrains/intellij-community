3e49ab3939cd9fec64f9ac927585e67f314adbfa
Roman Ivanov <roman.ivanov@jetbrains.com>
2022-04-04 15:09:18 +0200

    [code vision] shorter descriptions for kotlin inlays
    (cherry picked from commit 0e1756da7c144e9567eb8fb07cc5a53548d722ed)
    
    IJ-CR-22796
    
    GitOrigin-RevId: a96ad3db0b4b3ffdc29a5bf8e16bb782c57cefc9

--- plugins/kotlin/idea/resources/inlayProviders/kotlin.lambdas.hints/hints.lambda.return.kt
+++ plugins/kotlin/idea/resources/inlayProviders/kotlin.lambdas.hints/hints.lambda.return.kt
@@ -1,20 +1,4 @@
-fun numbers(): String {
-    val numbers = StringBuilder()
-    return with(numbers) {
-        // "with" structure starts here
-        for (letter in '0'..'9') {
-            append(letter)
-        }
-        toString() // used as the return value
-    }
-}
-
-class StringBuilder {
-    fun append(ch: Char) {
-        TODO()
-    }
-}
-
-fun <T, R> with(x: T, op: T.() -> R) : R {
-    TODO()
+val lambda = { i: Int ->
+    i + 10
+    i + 20
 }
