108e3196465713b201489ef130cd2a975d5f48fe
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2022-04-04 17:02:08 +0300

    [workspace model] Remove JSON serialization
    GitOrigin-RevId: b854d80a0e1b1221382cfd7b0e18b2e927687fd4

--- platform/workspaceModel/codegen/src/com/intellij/workspaceModel/codegen/impl/genericImplProxy.kt
+++
@@ -1,8 +0,0 @@
-package org.jetbrains.deft.obj.impl
-
-import org.jetbrains.deft.Obj
-import org.jetbrains.deft.ObjBuilder
-import org.jetbrains.deft.impl.ObjImplGeneric
-import org.jetbrains.deft.impl.ObjType
-import java.lang.reflect.Proxy
-
