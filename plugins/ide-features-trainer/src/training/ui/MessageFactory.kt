d0a1d343a46d1dd21e655a5c05e8be546c21710b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-04-04 09:57:22 +0200

    IJ-CR-22784 bring JDOM back - have to support complex XML data
    GitOrigin-RevId: 5300dc1feed6a1a27bae40a4e554ae0e0c983712

--- plugins/ide-features-trainer/src/training/ui/MessageFactory.kt
+++ plugins/ide-features-trainer/src/training/ui/MessageFactory.kt
@@ -2,9 +2,12 @@
 package training.ui
 
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.util.xml.dom.XmlElement
-import com.intellij.util.xml.dom.readXmlAsModel
+import com.intellij.openapi.util.JDOMUtil
+import com.intellij.openapi.util.text.Strings
 import org.intellij.lang.annotations.Language
+import org.jdom.Element
+import org.jdom.Text
+import org.jdom.output.XMLOutputter
 import training.dsl.LessonUtil
 import training.util.KeymapUtil
 import training.util.openLinkInBrowser
@@ -41,92 +44,88 @@ internal object MessageFactory {
       .splitToSequence("\n")
       .map { paragraph ->
         val wrappedText = "<root><text>$paragraph</text></root>"
-        val textAsElement = readXmlAsModel(wrappedText.reader()).getChild("text")
+        val textAsElement = JDOMUtil.load(wrappedText.byteInputStream()).getChild("text")
                             ?: throw IllegalStateException("Can't parse as XML:\n$paragraph")
         convert(textAsElement)
       }
       .reduce { acc, item -> acc + MessagePart("\n", MessagePart.MessageType.LINE_BREAK) + item }
   }
 
-  private fun convert(element: XmlElement?): List<MessagePart> {
+  private fun convert(element: Element?): List<MessagePart> {
     if (element == null) {
       return emptyList()
     }
 
     val list = mutableListOf<MessagePart>()
-
-    element.content?.let { text ->
-      var t = text
-      if (Pattern.matches(" *\\p{IsPunctuation}.*", t)) {
-        val indexOfFirst = text.indexOfFirst { it != ' ' }
-        t = "\u00A0".repeat(indexOfFirst) + t.substring(indexOfFirst)
+    for (content in element.content) {
+      if (content is Text) {
+        var text = content.getValue()
+        if (Pattern.matches(" *\\p{IsPunctuation}.*", text)) {
+          val indexOfFirst = text.indexOfFirst { it != ' ' }
+          text = "\u00A0".repeat(indexOfFirst) + text.substring(indexOfFirst)
+        }
+        list.add(MessagePart(text, MessagePart.MessageType.TEXT_REGULAR))
       }
-      list.add(MessagePart(t, MessagePart.MessageType.TEXT_REGULAR))
-    }
-
-    for (content in element.children) {
-      var type = MessagePart.MessageType.TEXT_REGULAR
-      val text = content.content ?: ""
-      var textAndSplitFn: (() -> Pair<String, List<IntRange>?>)? = null
-      var link: String? = null
-      var runnable: Runnable? = null
-      when (content.name) {
-        "icon" -> error("Need to return reflection-based icon processing")
-        "illustration" -> type = MessagePart.MessageType.ILLUSTRATION
-        "icon_idx" -> type = MessagePart.MessageType.ICON_IDX
-        "code" -> type = MessagePart.MessageType.CODE
-        "shortcut" -> type = MessagePart.MessageType.SHORTCUT
-        "strong" -> type = MessagePart.MessageType.TEXT_BOLD
-        "callback" -> {
-          type = MessagePart.MessageType.LINK
-          val id = content.getAttributeValue("id")
-          if (id != null) {
-            val callback = LearningUiManager.getAndClearCallback(id.toInt())
-            if (callback != null) {
-              runnable = Runnable { callback() }
-            }
-            else {
-              LOG.error("Unknown callback with id $id and text $text")
-            }
-          }
-        }
-
-        "a" -> {
-          type = MessagePart.MessageType.LINK
-          link = content.getAttributeValue("href")
-        }
-
-        "action" -> {
-          type = MessagePart.MessageType.SHORTCUT
-          link = text
-          textAndSplitFn = {
-            val shortcutByActionId = KeymapUtil.getShortcutByActionId(text)
-            if (shortcutByActionId != null) {
-              KeymapUtil.getKeyStrokeData(shortcutByActionId)
-            }
-            else {
-              KeymapUtil.getGotoActionData(text)
+      else if (content is Element) {
+        val outputter = XMLOutputter()
+        var type = MessagePart.MessageType.TEXT_REGULAR
+        val text: String = Strings.unescapeXmlEntities(outputter.outputString(content.content))
+        var textAndSplitFn: (() -> Pair<String, List<IntRange>?>)? = null
+        var link: String? = null
+        var runnable: Runnable? = null
+        when (content.name) {
+          "icon" -> error("Need to return reflection-based icon processing")
+          "illustration" -> type = MessagePart.MessageType.ILLUSTRATION
+          "icon_idx" -> type = MessagePart.MessageType.ICON_IDX
+          "code" -> type = MessagePart.MessageType.CODE
+          "shortcut" -> type = MessagePart.MessageType.SHORTCUT
+          "strong" -> type = MessagePart.MessageType.TEXT_BOLD
+          "callback" -> {
+            type = MessagePart.MessageType.LINK
+            val id = content.getAttributeValue("id")
+            if (id != null) {
+              val callback = LearningUiManager.getAndClearCallback(id.toInt())
+              if (callback != null) {
+                runnable = Runnable { callback() }
+              }
+              else {
+                LOG.error("Unknown callback with id $id and text $text")
+              }
             }
           }
-        }
-
-        "raw_shortcut" -> {
-          type = MessagePart.MessageType.SHORTCUT
-          textAndSplitFn = {
-            KeymapUtil.getKeyStrokeData(KeyStroke.getKeyStroke(text))
+          "a" -> {
+            type = MessagePart.MessageType.LINK
+            link = content.getAttributeValue("href")
+          }
+          "action" -> {
+            type = MessagePart.MessageType.SHORTCUT
+            link = text
+            textAndSplitFn = {
+              val shortcutByActionId = KeymapUtil.getShortcutByActionId(text)
+              if (shortcutByActionId != null) {
+                KeymapUtil.getKeyStrokeData(shortcutByActionId)
+              }
+              else {
+                KeymapUtil.getGotoActionData(text)
+              }
+            }
+          }
+          "raw_shortcut" -> {
+            type = MessagePart.MessageType.SHORTCUT
+            textAndSplitFn = {
+              KeymapUtil.getKeyStrokeData(KeyStroke.getKeyStroke(text))
+            }
+          }
+          "ide" -> {
+            type = MessagePart.MessageType.TEXT_REGULAR
+            textAndSplitFn = { LessonUtil.productName to null }
           }
         }
-
-        "ide" -> {
-          type = MessagePart.MessageType.TEXT_REGULAR
-          textAndSplitFn = { LessonUtil.productName to null }
-        }
+        val message = MessagePart(type, textAndSplitFn ?: { text to null })
+        message.link = link
+        message.runnable = runnable
+        list.add(message)
       }
-
-      val message = MessagePart(type, textAndSplitFn ?: { text to null })
-      message.link = link
-      message.runnable = runnable
-      list.add(message)
     }
     return list
   }
