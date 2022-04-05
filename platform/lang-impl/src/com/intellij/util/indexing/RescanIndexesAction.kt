caf5d66b74f56ee12a25fcace528e9da2797a6c9
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2022-04-05 08:04:24 +0400

    use nonblocking read actions in unindexed files updater
    GitOrigin-RevId: 219494bcf215af977e8fa97d344f4fe7afbc3b8f

--- platform/lang-impl/src/com/intellij/util/indexing/RescanIndexesAction.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/RescanIndexesAction.kt
@@ -7,12 +7,10 @@ import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.util.ProgressIndicatorUtils
 import com.intellij.openapi.project.DumbModeTask
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileWithId
 import com.intellij.psi.stubs.StubTreeBuilder
 import com.intellij.psi.stubs.StubUpdatingIndex
-import com.intellij.util.BooleanFunction
 import com.intellij.util.indexing.diagnostic.ProjectIndexingHistoryImpl
 import com.intellij.util.indexing.roots.IndexableFilesIterator
 import com.intellij.util.indexing.roots.ProjectIndexableFilesIteratorImpl
@@ -20,6 +18,7 @@ import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.Nls
 import java.util.*
 import java.util.concurrent.CompletableFuture
+import java.util.function.Predicate
 
 @ApiStatus.Internal
 class RescanIndexesAction : RecoveryAction {
@@ -49,15 +48,15 @@ class RescanIndexesAction : RecoveryAction {
           get() = "`$path` should have already indexed stub but it's not present"
       }
 
-      override fun getForceReindexingTrigger(): BooleanFunction<IndexedFile>? {
+      override fun getForceReindexingTrigger(): Predicate<IndexedFile>? {
         if (stubIndex != null) {
-          return BooleanFunction<IndexedFile> {
+          return Predicate<IndexedFile> {
             val fileId = (it.file as VirtualFileWithId).id
             if (stubIndex.getIndexingStateForFile(fileId, it) == FileIndexingState.UP_TO_DATE &&
                 stubIndex.getIndexedFileData(fileId).isEmpty() &&
                 isAbleToBuildStub(it.file)) {
               stubAndIndexingStampInconsistencies.add(StubAndIndexStampInconsistency(it.file.path))
-              return@BooleanFunction true
+              return@Predicate true
             }
             false
           }
--- platform/lang-impl/src/com/intellij/util/indexing/UnindexedFilesFinder.java
+++ platform/lang-impl/src/com/intellij/util/indexing/UnindexedFilesFinder.java
@@ -11,7 +11,6 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileWithId;
 import com.intellij.openapi.vfs.newvfs.impl.VirtualFileSystemEntry;
 import com.intellij.psi.search.FileTypeIndex;
-import com.intellij.util.BooleanFunction;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.indexing.projectFilter.FileAddStatus;
 import com.intellij.util.indexing.projectFilter.ProjectIndexableFilesFilterHolder;
@@ -24,6 +23,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 final class UnindexedFilesFinder {
@@ -33,13 +33,13 @@ final class UnindexedFilesFinder {
   private final FileBasedIndexImpl myFileBasedIndex;
   private final UpdatableIndex<FileType, Void, FileContent, ?> myFileTypeIndex;
   private final Collection<FileBasedIndexInfrastructureExtension.FileIndexingStatusProcessor> myStateProcessors;
-  private final @Nullable BooleanFunction<? super IndexedFile> myForceReindexingTrigger;
+  private final @Nullable Predicate<? super IndexedFile> myForceReindexingTrigger;
   private final @NotNull ProjectIndexableFilesFilterHolder myIndexableFilesFilterHolder;
   private final boolean myShouldProcessUpToDateFiles;
 
   UnindexedFilesFinder(@NotNull Project project,
                        @NotNull FileBasedIndexImpl fileBasedIndex,
-                       @Nullable BooleanFunction<? super IndexedFile> forceReindexingTrigger) {
+                       @Nullable Predicate<? super IndexedFile> forceReindexingTrigger) {
     myProject = project;
     myFileBasedIndex = fileBasedIndex;
     myFileTypeIndex = fileBasedIndex.getIndex(FileTypeIndex.NAME);
@@ -60,7 +60,7 @@ final class UnindexedFilesFinder {
   @Nullable("null if the file is not subject for indexing (a directory, invalid, etc.)")
   public UnindexedFileStatus getFileStatus(@NotNull VirtualFile file) {
     ProgressManager.checkCanceled(); // give a chance to suspend indexing
-    return ReadAction.compute(() -> {
+    return ReadAction.nonBlocking(() -> {
       if (myProject.isDisposed() || !file.isValid() || !(file instanceof VirtualFileWithId)) {
         return null;
       }
@@ -118,7 +118,6 @@ final class UnindexedFilesFinder {
           }
           else {
             final List<ID<?, ?>> affectedIndexCandidates = myFileBasedIndex.getAffectedIndexCandidates(indexedFile);
-            //noinspection ForLoopReplaceableByForEach
             for (int i = 0, size = affectedIndexCandidates.size(); i < size; ++i) {
               final ID<?, ?> indexId = affectedIndexCandidates.get(i);
               if (FileBasedIndexScanUtil.isManuallyManaged(indexId)) continue;
@@ -202,7 +201,7 @@ final class UnindexedFilesFinder {
           timeUpdatingContentLessIndexes.addAndGet(System.nanoTime() - nowTime);
         }
 
-        if (myForceReindexingTrigger != null && myForceReindexingTrigger.fun(indexedFile)) {
+        if (myForceReindexingTrigger != null && myForceReindexingTrigger.test(indexedFile)) {
           myFileBasedIndex.dropNontrivialIndexedStates(inputId);
           shouldIndex.set(true);
         }
@@ -217,7 +216,7 @@ final class UnindexedFilesFinder {
                                      timeProcessingUpToDateFiles.get(),
                                      timeUpdatingContentLessIndexes.get(),
                                      timeIndexingWithoutContent.get());
-    });
+    }).executeSynchronously();
   }
 
   private boolean tryIndexWithoutContentViaInfrastructureExtension(IndexedFile fileContent, int inputId, ID<?, ?> indexId) {
