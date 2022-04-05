6510c2b824937dc0fb8384a124b6d4f7b6735271
Dmitry Jemerov <yole@jetbrains.com>
2022-04-05 09:25:17 +0200

    Don't show plugin advertiser if plugin supporting pattern is enabled but file is reassigned as text (IDEA-284976)
    (cherry picked from commit 1a94579ecb42973bc6b8486e32ef378ad834d779)
    
    IJ-CR-22401
    
    (cherry picked from commit c96b38ac0e5b4e679649df89bebf6a6338fee5ce)
    
    GitOrigin-RevId: 6211001f57ebbb50777be27fc2511a7c5970e7c0

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/pluginsAdvertisement/PluginAdvertiserEditorNotificationProvider.kt
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/pluginsAdvertisement/PluginAdvertiserEditorNotificationProvider.kt
@@ -73,7 +73,7 @@ class PluginAdvertiserEditorNotificationProvider : EditorNotificationProvider,
     val suggestedIdes: List<SuggestedIde>,
   ) : Function<FileEditor, EditorNotificationPanel?> {
 
-    private var disabledPlugin: IdeaPluginDescriptor? = null
+    private var installedPlugin: IdeaPluginDescriptor? = null
     private val jbProduced = mutableSetOf<PluginId>()
 
     @VisibleForTesting
@@ -84,11 +84,8 @@ class PluginAdvertiserEditorNotificationProvider : EditorNotificationProvider,
       for (data in dataSet) {
         val pluginId = data.pluginId
 
-        val installedPlugin: IdeaPluginDescriptor? = descriptorsById[pluginId]
-        if (installedPlugin != null) {
-          if (!installedPlugin.isEnabled && disabledPlugin == null) {
-            disabledPlugin = installedPlugin
-          }
+        if (pluginId in descriptorsById) {
+          installedPlugin = descriptorsById[pluginId]
         }
         else if (!data.isBundled) {
           (if (jbPluginsIds.contains(pluginId.idString)) jbProduced else thirdParty) += pluginId
@@ -117,12 +114,20 @@ class PluginAdvertiserEditorNotificationProvider : EditorNotificationProvider,
         }
       }
 
-      if (disabledPlugin != null) {
-        panel.createActionLabel(IdeBundle.message("plugins.advertiser.action.enable.plugin", disabledPlugin!!.name)) {
-          pluginAdvertiserExtensionsState.addEnabledExtensionOrFileNameAndInvalidateCache(extensionOrFileName)
-          updateAllNotifications(project)
-          FUSEventSource.EDITOR.logEnablePlugins(listOf(disabledPlugin!!.pluginId.idString), project)
-          PluginManagerConfigurable.showPluginConfigurableAndEnable(project, setOf(disabledPlugin))
+      val installedPlugin = installedPlugin
+      if (installedPlugin != null) {
+        if (!installedPlugin.isEnabled) {
+          panel.createActionLabel(IdeBundle.message("plugins.advertiser.action.enable.plugin", installedPlugin.name)) {
+            pluginAdvertiserExtensionsState.addEnabledExtensionOrFileNameAndInvalidateCache(extensionOrFileName)
+            updateAllNotifications(project)
+            FUSEventSource.EDITOR.logEnablePlugins(listOf(installedPlugin.pluginId.idString), project)
+            PluginManagerConfigurable.showPluginConfigurableAndEnable(project, setOf(installedPlugin))
+          }
+        }
+        else {
+          // Plugin supporting the pattern is installed and enabled but the current file is reassigned to a different
+          // file type
+          return null
         }
       }
       else if (jbProduced.isNotEmpty()) {
