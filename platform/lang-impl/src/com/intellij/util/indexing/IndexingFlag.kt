9c00c2579b75892731efed4b2b6c432aa4e222c7
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2022-04-05 16:40:51 +0200

    Clean up IndexingFlag' cache whenever file id is known
    Some files are changed to their originals during indexing (see com.intellij.util.indexing.FileBasedIndexImpl.indexFileContent), they share id, but not implemented class, so resort to the minimal interface on final unlock.
    
    GitOrigin-RevId: 8ede39ad879cfb164a27e88041772597947f6cc6

--- platform/lang-impl/src/com/intellij/util/indexing/IndexingFlag.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/IndexingFlag.kt
@@ -2,6 +2,7 @@
 package com.intellij.util.indexing
 
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileWithId
 import com.intellij.openapi.vfs.newvfs.ManagingFS
 import com.intellij.openapi.vfs.newvfs.impl.VirtualFileSystemEntry
 import org.jetbrains.annotations.ApiStatus
@@ -61,7 +62,7 @@ object IndexingFlag {
 
   @JvmStatic
   fun unlockFile(file: VirtualFile) {
-    if (file is VirtualFileSystemEntry) {
+    if (file is VirtualFileWithId) {
       hashes.releaseHash(file.id)
     }
   }
