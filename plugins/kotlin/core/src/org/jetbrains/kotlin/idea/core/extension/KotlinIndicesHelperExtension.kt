374e51e7986383e514249de865d5c570d8381c8a
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-04-07 21:27:09 +0700

    [kotlin] remove deprecated method from KotlinIndicesHelperExtension
    GitOrigin-RevId: 601be0ed0665e4ae0b142af331165905647cf76e

--- plugins/kotlin/core/src/org/jetbrains/kotlin/idea/core/extension/KotlinIndicesHelperExtension.kt
+++ plugins/kotlin/core/src/org/jetbrains/kotlin/idea/core/extension/KotlinIndicesHelperExtension.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.core.extension
 
@@ -13,26 +13,11 @@ interface KotlinIndicesHelperExtension {
         "org.jetbrains.kotlin.kotlinIndicesHelperExtension", KotlinIndicesHelperExtension::class.java
     )
 
-    @JvmDefault
     fun appendExtensionCallables(
         consumer: MutableList<in CallableDescriptor>,
         moduleDescriptor: ModuleDescriptor,
         receiverTypes: Collection<KotlinType>,
         nameFilter: (String) -> Boolean,
-        lookupLocation: LookupLocation
-    ) {
-        appendExtensionCallables(consumer, moduleDescriptor, receiverTypes, nameFilter)
-    }
-
-    @Deprecated(
-        "Override the appendExtensionCallables() with the 'lookupLocation' parameter instead. " +
-                "This method can then throw an exception. " +
-                "See 'DebuggerFieldKotlinIndicesHelperExtension' as an example."
-    )
-    fun appendExtensionCallables(
-        consumer: MutableList<in CallableDescriptor>,
-        moduleDescriptor: ModuleDescriptor,
-        receiverTypes: Collection<KotlinType>,
-        nameFilter: (String) -> Boolean
+        lookupLocation: LookupLocation,
     )
 }
