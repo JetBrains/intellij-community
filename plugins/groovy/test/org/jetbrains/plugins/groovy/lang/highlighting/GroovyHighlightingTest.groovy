f86612cc8422119a2d8ce77bd45b8b5e70eb70d6
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-04-07 16:18:48 +0300

    [groovy] IDEA-291580: Prevent recursion on incorrect class hierarchy
    GitOrigin-RevId: 84e06b4870dc7e299063abf659aa593cd87d8f27

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovyHighlightingTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovyHighlightingTest.groovy
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.groovy.lang.highlighting
 
 import com.siyeh.ig.junit.AbstractTestClassNamingConvention
@@ -2191,5 +2191,11 @@ new File('').withWriter('utf-8') {
     writer -> writer.write(output)
 }
 '''
+  }
+
+  void 'test IDEA-291580'() {
+    testHighlighting """
+<error>class Foo extends Foo</error> {} 
+"""
   }
 }
