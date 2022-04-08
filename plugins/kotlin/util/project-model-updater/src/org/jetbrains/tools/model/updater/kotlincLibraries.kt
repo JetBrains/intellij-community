b4def953a62c4b7716d7da0eb6cdbf37ac053e1e
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-04-08 17:25:06 +0200

    [kotlin] Fix model updater -- remove duplicate settings
    For the following entries we had duplicate configurations:
    - kotlinc.kotlin-jps-common
    - kotlin.kotlin-jps-plugin-classpath
    
    That had happened because of the rebase on fresh 221 branch
    
    GitOrigin-RevId: 2c7b2d5e0682865e7af5b7e02e9671d9de05653a

--- plugins/kotlin/util/project-model-updater/src/org/jetbrains/tools/model/updater/kotlincLibraries.kt
+++ plugins/kotlin/util/project-model-updater/src/org/jetbrains/tools/model/updater/kotlincLibraries.kt
@@ -41,8 +41,6 @@ fun generateKotlincLibraries(kotlincArtifactsMode: KotlincArtifactsMode, version
         singleJarMvnLib("kotlinc.kotlin-jps-plugin-classpath", "$ktGroup:kotlin-jps-plugin-classpath:$version", transitive = false),
         singleJarMvnLib("kotlinc.kotlin-reflect", "$ktGroup:kotlin-reflect:$version", excludes = listOf(MavenId(ktGroup, "kotlin-stdlib"))),
         singleJarMvnLib("kotlinc.kotlin-script-runtime", "$ktGroup:kotlin-script-runtime:$version"),
-        singleJarMvnLib("kotlinc.kotlin-jps-plugin-classpath", "$ktGroup:kotlin-jps-plugin-classpath:$version"),
-        singleJarMvnLib("kotlinc.kotlin-jps-common", "$ktGroup:kotlin-jps-common-for-ide:$version"),
         run {
             val mavenIds = listOf(
                 MavenId.fromCoordinates("$ktGroup:kotlin-stdlib-jdk8:$version"),
