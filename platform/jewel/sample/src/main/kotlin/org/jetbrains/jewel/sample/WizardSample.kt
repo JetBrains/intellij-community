375bf56c31cae44a7e7d840ccd8649c01f14a629
Amaury Medeiros <amaurym@google.com>
2022-04-08 12:14:57 +0100

    Add directory selection tree to assets wizard sample
    GitOrigin-RevId: b0476a4af371aae877092e899741e63abbb9db19

--- platform/jewel/sample/src/main/kotlin/org/jetbrains/jewel/sample/WizardSample.kt
+++ platform/jewel/sample/src/main/kotlin/org/jetbrains/jewel/sample/WizardSample.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.jewel.sample
 
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,12 +10,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,6 +34,10 @@ import org.jetbrains.jewel.theme.intellij.components.Button
 import org.jetbrains.jewel.theme.intellij.components.IconButton
 import org.jetbrains.jewel.theme.intellij.components.Text
 import java.awt.event.WindowEvent
+import org.jetbrains.jewel.theme.intellij.components.Tree
+import org.jetbrains.jewel.theme.intellij.components.TreeLayout
+import org.jetbrains.jewel.theme.intellij.components.asTree
+import java.nio.file.Paths
 
 private const val WIZARD_PAGE_COUNT = 2
 
@@ -45,7 +55,7 @@ fun Wizard(onFinish: () -> Unit) {
         Box(modifier = Modifier.fillMaxSize()) {
             Column {
                 val currentPage = mutableStateOf(1) // 1-based
-                WizardHeader()
+                WizardHeader(currentPage = currentPage)
                 WizardMainContent(
                     modifier = Modifier.weight(1f),
                     currentPage = currentPage
@@ -57,7 +67,7 @@ fun Wizard(onFinish: () -> Unit) {
 }
 
 @Composable
-fun WizardHeader(modifier: Modifier = Modifier) {
+fun WizardHeader(modifier: Modifier = Modifier, currentPage: MutableState<Int>) {
     Box(modifier.background(Color(0xFF616161)).height(100.dp).fillMaxWidth()) {
         Row(modifier = modifier.fillMaxSize(), verticalAlignment = Alignment.CenterVertically) {
             Icon(
@@ -67,7 +77,11 @@ fun WizardHeader(modifier: Modifier = Modifier) {
                 tint = Color.Unspecified // FIXME: tint is being applied regardless
             )
             Text(
-                text = "Configure Image Asset",
+                text = when (currentPage.value) {
+                    1 -> "Configure Image Asset"
+                    2 ->  "Confirm Icon Path"
+                    else -> "Assets Wizard"
+                },
                 fontSize = 24.sp
             )
         }
@@ -80,7 +94,7 @@ fun WizardMainContent(modifier: Modifier = Modifier, currentPage: MutableState<I
         Box(modifier.background(Color.Green).fillMaxWidth())
     }
     else if (currentPage.value == 2) {
-        Box(modifier.background(Color.Yellow).fillMaxWidth())
+        ConfirmIconPathPage(modifier)
     }
 }
 
@@ -96,6 +110,64 @@ fun WizardFooter(modifier: Modifier = Modifier, currentPage: MutableState<Int>, 
             WizardControls(currentPage = currentPage, onFinish = onFinish)
         }
     }
+}
+
+@Composable
+fun ConfirmIconPathPage(modifier: Modifier = Modifier) {
+    Row(modifier.fillMaxSize()) {
+        DirectorySelection(modifier)
+        Box(modifier.background(Color.Magenta).fillMaxSize())
+    }
+}
+
+@Composable
+fun DirectorySelection(modifier: Modifier = Modifier) {
+    Column(modifier.fillMaxSize()) {
+        ResDirectoryLabelComboBox()
+        OutputDirectoriesLabelTree()
+    }
+}
+
+@Composable
+fun ResDirectoryLabelComboBox(modifier: Modifier = Modifier) {
+    Box(modifier.background(Color.Yellow).height(30.dp).fillMaxWidth())
+}
+
+@Composable
+fun OutputDirectoriesLabelTree(modifier: Modifier = Modifier) {
+    Row(modifier.fillMaxSize()) {
+        Text(
+            modifier = modifier.padding(5.dp),
+            text = "Output Directories:",
+            color = Color.Black
+        )
+        var tree by remember { mutableStateOf(Paths.get(System.getProperty("user.dir")).asTree(true)) }
+
+        Box {
+            val listState = rememberLazyListState()
+            TreeLayout(
+                modifier = Modifier.fillMaxWidth(),
+                tree = tree,
+                state = listState,
+                onTreeChanged = { tree = it },
+                onTreeElementDoubleClick = {},
+                rowContent = {
+                    val text: String = when (it) {
+                        is Tree.Element.Leaf -> it.data.name
+                        is Tree.Element.Node -> "[${it.data.name}]"
+                    }
+                    Text(modifier = Modifier.fillMaxWidth(), text = text, softWrap = false)
+                },
+            )
+            if (listState.layoutInfo.totalItemsCount > listState.layoutInfo.visibleItemsInfo.size) {
+                VerticalScrollbar(
+                    modifier = Modifier.align(Alignment.CenterEnd).padding(horizontal = 2.dp),
+                    adapter = rememberScrollbarAdapter(listState)
+                )
+            }
+        }
+    }
+
 }
 
 @Composable
@@ -116,7 +188,7 @@ fun HelpIcon(modifier: Modifier = Modifier) {
 @Composable
 fun WizardControls(modifier: Modifier = Modifier, currentPage: MutableState<Int>, onFinish: () -> Unit) {
     Row(modifier) {
-        Button(onClick = {}) { // TODO: close application on cancel
+        Button(onClick = onFinish) {
             Text("Cancel")
         }
         Button(onClick = { currentPage.value-- }, enabled = currentPage.value > 1) {
