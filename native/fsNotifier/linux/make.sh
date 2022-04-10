132ca8ee440c3616e2337200f3817fdee954855f
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2022-04-10 15:04:09 +0200

    [platform] IJI-844 build fsnotifier for Linux-aarch64
    GitOrigin-RevId: 0ffd25c09fc56adabc0309f62904fae605ba7b6e

--- native/fsNotifier/linux/make.sh
+++ native/fsNotifier/linux/make.sh
@@ -1,7 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
 
 VER=$(date "+%Y%m%d.%H%M")
 sed -i.bak "s/#define VERSION .*/#define VERSION \"${VER}\"/" fsnotifier.h && rm fsnotifier.h.bak
+ARCH=$(uname -m)
 
-${CC:-clang} -O2 -Wall -Wextra -Wpedantic -std=c11 -o fsnotifier main.c inotify.c util.c && \
-  chmod 755 fsnotifier
+build_fsnotifier() {
+  [ -f "$1" ] && rm "$1"
+  ${CC:-clang} -O2 -Wall -Wextra -Wpedantic -std=c11 main.c inotify.c util.c -o "$1"
+  chmod 755 "$1"
+}
+
+if [ "$ARCH" = "x86_64" ] || [ "$ARCH" = "amd64" ]; then
+  echo "*** Compiling amd64 version (fsnotifier) ..."
+  build_fsnotifier fsnotifier
+elif [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
+  echo "*** Compiling aarch64 version (fsnotifier-aarch64)..."
+  build_fsnotifier fsnotifier-aarch64
+else
+  echo "*** Compiling platform-specific version (fsnotifier-$ARCH)..."
+  build_fsnotifier "fsnotifier-$ARCH"
+fi
