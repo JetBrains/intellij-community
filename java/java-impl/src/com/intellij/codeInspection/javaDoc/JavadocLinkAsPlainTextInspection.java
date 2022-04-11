20aae18c444fd47ce469bb8e03e0c18dc0c2e0ec
Andrey.Cherkasov <andrey.cherkasov@jetbrains.com>
2022-04-11 16:14:53 +0300

    [java-inspections] JavadocLinkAsPlainText: finalize the inspection
    IDEA-261795
    IDEA-290689
    IDEA-290417
    IJ-CR-21142
    
    (cherry picked from commit 27f46f839e7062627aeee03045bf654e1e4eca4f)
    
    GitOrigin-RevId: b9b66a1a25ae43a29c8f6fe1ceb330f2b541284a

--- java/java-impl/src/com/intellij/codeInspection/javaDoc/JavadocLinkAsPlainTextInspection.java
+++ java/java-impl/src/com/intellij/codeInspection/javaDoc/JavadocLinkAsPlainTextInspection.java
@@ -5,40 +5,65 @@ import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.LocalQuickFixAndIntentionActionOnPsiElement;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.java.JavaBundle;
-import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.paths.WebReference;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.*;
-import com.intellij.psi.javadoc.PsiDocToken;
+import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry;
+import com.intellij.psi.javadoc.PsiDocComment;
+import com.intellij.psi.javadoc.PsiDocTag;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.io.URLUtil;
+import com.siyeh.ig.psiutils.CommentTracker;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JavadocLinkAsPlainTextInspection extends LocalInspectionTool {
   @Override
   public @NotNull PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
     return new JavaElementVisitor() {
       @Override
-      public void visitDocToken(PsiDocToken token) {
-        super.visitDocToken(token);
-        String text = token.getText();
-        Matcher matcher = URLUtil.URL_PATTERN.matcher(text);
-        while (matcher.find()) {
-          int start = matcher.start();
-          int end = matcher.end();
-          String prefix = text.substring(0, start);
-          String suffix = text.substring(end);
-          if ((prefix.matches(".*<a href=\".*\">$") && suffix.matches("^</a>.*")) ||
-              (prefix.endsWith("<a href=\"") && suffix.matches("\">.*</a>.*"))) {
-            continue;
+      public void visitDocComment(PsiDocComment comment) {
+        if (comment.getOwner() == null) return;
+        for (PsiReference reference : ReferenceProvidersRegistry.getReferencesFromProviders(comment)) {
+          if (!(reference instanceof WebReference)) continue;
+          String url = ((WebReference)reference).getUrl();
+          if (!url.startsWith(URLUtil.HTTP_PROTOCOL) && !url.startsWith(URLUtil.HTTPS_PROTOCOL)) return;
+          String commentText = comment.getText();
+          Pattern pattern = Pattern.compile("\n\\s*\\*");
+          Matcher matcher = pattern.matcher(commentText);
+          commentText = matcher.replaceAll(result -> " ".repeat(result.group().length()));
+          TextRange range = reference.getRangeInElement();
+          int start = range.getStartOffset();
+          int end = range.getEndOffset();
+          PsiElement element = comment.findElementAt(start);
+          if (element == null) return;
+          PsiDocTag tag = PsiTreeUtil.getParentOfType(element, PsiDocTag.class);
+          if (tag != null) {
+            String tagName = tag.getName();
+            if (tagName.equals("see")) continue; // handled by JavaDocReferenceInspection
+            if (tagName.equals("code")) continue;
           }
-          TextRange range = TextRange.create(start, end);
-          holder.registerProblem(token, range, JavaBundle.message("inspection.javadoc.link.as.plain.text.message"),
-                                 new UrlToHtmlFix(token, start, end));
+          String prefix = commentText.substring(0, start);
+          String suffix = commentText.substring(end);
+          if (isContentOfATag(prefix, suffix) || isHtmlTagAttribute(prefix, suffix)) continue;
+          holder.registerProblem(comment, range, JavaBundle.message("inspection.javadoc.link.as.plain.text.message"),
+                                 new UrlToHtmlFix(comment, start, end));
         }
+      }
+
+      private boolean isContentOfATag(String prefix, String suffix) {
+        return Pattern.compile("<\\w+\\s+\\w+\\s*=\\s*\"?.*\"?\\s*>.*$", Pattern.DOTALL).matcher(prefix).find() &&
+               Pattern.compile("^.*</\\w+>", Pattern.DOTALL).matcher(suffix).find();
+      }
+
+      private boolean isHtmlTagAttribute(String prefix, String suffix) {
+        return Pattern.compile("<\\w+\\s.*\\w+\\s*=\\s*\"?\\s*$", Pattern.DOTALL).matcher(prefix).find() &&
+               Pattern.compile("^\\s*\"?.*>", Pattern.DOTALL).matcher(suffix).find();
       }
     };
   }
@@ -59,13 +84,20 @@ public class JavadocLinkAsPlainTextInspection extends LocalInspectionTool {
                        @Nullable Editor editor,
                        @NotNull PsiElement startElement,
                        @NotNull PsiElement endElement) {
-      Document document = PsiDocumentManager.getInstance(project).getDocument(file);
-      if (document == null) return;
-      String text = startElement.getText().substring(myStartOffset, myEndOffset);
-      int offset = startElement.getTextOffset();
-      String wrappedLink = "<a href=\"" + text + "\">" + text + "</a>";
-      document.replaceString(offset + myStartOffset, offset + myEndOffset,
-                             wrappedLink);
+      String commentText = startElement.getText();
+      String prefix = commentText.substring(0, myStartOffset);
+      String url = commentText.substring(myStartOffset, myEndOffset);
+      String suffix = commentText.substring(myEndOffset);
+      String dummyText = "...";
+      String wrappedLink = "<a href=\"" + url + "\">" + dummyText + "</a>";
+      CommentTracker ct = new CommentTracker();
+      PsiElement replacement = ct.replace(startElement, prefix + wrappedLink + suffix);
+      if (editor != null) {
+        int start = replacement.getTextRange().getStartOffset() + prefix.length() + url.length() + 11;
+        int end = start + dummyText.length();
+        editor.getCaretModel().moveToOffset(start);
+        editor.getSelectionModel().setSelection(start, end);
+      }
     }
 
     @Override
--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/javadocLinkAsPlainText/before1.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/javadocLinkAsPlainText/before1.java
@@ -1,7 +1,6 @@
 // "Wrap in <a> tag" "true"
-class Main {
-  /**
-   * https://en.wikipedia.org/<caret>
-   */
-  void foo() {}
-}
+
+/**
+ * abc https://en.wikipedia.org/<caret> def
+ */
+class Main { }
