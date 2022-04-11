782506053957b6928f897864201f8cf4bfc7f86c
Andrey.Cherkasov <andrey.cherkasov@jetbrains.com>
2022-04-11 16:14:53 +0300

    Flip record components correctly in Flip Comma intention (IDEA-292057)
    GitOrigin-RevId: 5c202a283514c8220e34fd66401f8900cc4d8640

--- java/java-tests/testSrc/com/intellij/java/codeInsight/intention/FlipCommaIntentionTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/intention/FlipCommaIntentionTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package com.intellij.java.codeInsight.intention;
 
@@ -96,5 +82,10 @@ public class FlipCommaIntentionTest extends IPPTestCase {
     doTestIntentionNotAvailable("class C {" +
                                 "  int[] ones = {1/*_Flip ','*/,1,1};" +
                                 "}");
+  }
+
+  public void testRecordComponent() {
+    doTest("record A(String s,/*_Flip ','*/ int i) { }",
+           "record A(int i, String s) { }");
   }
 }
