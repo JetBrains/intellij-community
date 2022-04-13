consulo/consulo-kotlin

KTIJ-20734 Deprecated inline class: migration inspection
^KTIJ-20734 fixed

GitOrigin-RevId: cda55ee51a101b12d6aae27d2b9b347d1d62168e

idea/src/org/jetbrains/kotlin/idea/inspections/migration/InlineClassDeprecatedMigrationInspection.kt
@@ -1,0 +1,28 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+package org.jetbrains.kotlin.idea.inspections.migration
+
+import com.intellij.codeInspection.CleanupLocalInspectionTool
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.config.LanguageVersion
+import org.jetbrains.kotlin.diagnostics.DiagnosticFactoryWithPsiElement
+import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.migration.MigrationInfo
+import org.jetbrains.kotlin.idea.migration.isLanguageVersionUpdate
+import org.jetbrains.kotlin.idea.quickfix.migration.MigrationFix
+
+
+class InlineClassDeprecatedMigrationInspection :
+    AbstractDiagnosticBasedMigrationInspection<PsiElement>(PsiElement::class.java),
+    MigrationFix,
+    CleanupLocalInspectionTool {
+    override fun isApplicable(migrationInfo: MigrationInfo): Boolean {
+        return migrationInfo.isLanguageVersionUpdate(LanguageVersion.KOTLIN_1_4, LanguageVersion.KOTLIN_1_5)
+    }
+
+    override fun descriptionMessage(): String = KotlinBundle.message("inspection.deprecated.inline.class.text")
+
+    override val diagnosticFactory: DiagnosticFactoryWithPsiElement<PsiElement, *>
+        get() = Errors.INLINE_CLASS_DEPRECATED
+}

JetBrains/intellij-community

(cherry picked from commit cda55ee51a101b12d6aae27d2b9b347d1d62168e)
GitOrigin-RevId: 73509604396abbafe94f51d281a0f786ffa459ee

plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/migration/InlineClassDeprecatedMigrationInspection.kt
@@ -1,0 +1,28 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+package org.jetbrains.kotlin.idea.inspections.migration
+
+import com.intellij.codeInspection.CleanupLocalInspectionTool
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.config.LanguageVersion
+import org.jetbrains.kotlin.diagnostics.DiagnosticFactoryWithPsiElement
+import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.configuration.MigrationInfo
+import org.jetbrains.kotlin.idea.configuration.isLanguageVersionUpdate
+import org.jetbrains.kotlin.idea.quickfix.migration.MigrationFix
+
+
+class InlineClassDeprecatedMigrationInspection :
+    AbstractDiagnosticBasedMigrationInspection<PsiElement>(PsiElement::class.java),
+    MigrationFix,
+    CleanupLocalInspectionTool {
+    override fun isApplicable(migrationInfo: MigrationInfo): Boolean {
+        return migrationInfo.isLanguageVersionUpdate(LanguageVersion.KOTLIN_1_4, LanguageVersion.KOTLIN_1_5)
+    }
+
+    override fun descriptionMessage(): String = KotlinBundle.message("inspection.deprecated.inline.class.text")
+
+    override val diagnosticFactory: DiagnosticFactoryWithPsiElement<PsiElement, *>
+        get() = Errors.INLINE_CLASS_DEPRECATED
+}