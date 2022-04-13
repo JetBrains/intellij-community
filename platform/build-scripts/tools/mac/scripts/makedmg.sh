4f29a47d507d88d4d4edb3e91b828097bd178004
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2022-04-13 23:33:18 +0200

    IJI-735 installing Python library for DMG generation
    GitOrigin-RevId: b7520111f2fa508ccd7900979d4856e7eba76d66

--- platform/build-scripts/tools/mac/scripts/makedmg.sh
+++ platform/build-scripts/tools/mac/scripts/makedmg.sh
@@ -66,12 +66,15 @@ find "/Volumes/$1" -maxdepth 1
 log "Updating $VOLNAME disk image styles..."
 stat "/Volumes/$1/DSStorePlaceHolder"
 rm "/Volumes/$1/DSStorePlaceHolder"
-if python3 -c "import ds_store; import mac_alias;" >/dev/null 2>/dev/null; then
-  python3 makedmg.py "$VOLNAME" "$BG_PIC" "$1"
-  log "DMG/DS_Store is generated"
-else
-  log "DMG/DS_Store generation is skipped. If you need it please install python3 and ds_store library."
+if ! python3 --version >/dev/null 2>/dev/null; then
+  log "python3 is required for DMG/DS_Store generation"
+  exit 1
+elif ! python3 -c "import ds_store; import mac_alias;" >/dev/null 2>/dev/null; then
+  log "ds_store library is required for DMG/DS_Store generation, installing"
+  pip3 install ds_store
 fi
+python3 makedmg.py "$VOLNAME" "$BG_PIC" "$1"
+log "DMG/DS_Store is generated"
 rm -rf "/Volumes/$1/.fseventsd"
 
 sync;sync;sync
