04bf596837806936d718210c42fe5869f4526174
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2022-04-13 21:56:15 +0200

    [platform] deprecating hopelessly outdated service (IDEA-291978)
    GitOrigin-RevId: 09d686dee0d0d524325d78d86a3191f8b575f2ae

--- platform/platform-impl/src/com/intellij/openapi/fileChooser/FileSystemTreeFactory.java
+++ platform/platform-impl/src/com/intellij/openapi/fileChooser/FileSystemTreeFactory.java
@@ -1,23 +1,23 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.fileChooser;
 
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
-import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.fileChooser.ex.FileSystemTreeFactoryImpl;
 import com.intellij.openapi.project.Project;
 
-
+/** @deprecated obsolete */
+@Deprecated(forRemoval = true)
+@SuppressWarnings("unused")
 public interface FileSystemTreeFactory {
-
   FileSystemTree createFileSystemTree(Project project, FileChooserDescriptor fileChooserDescriptor);
 
   DefaultActionGroup createDefaultFileSystemActions(FileSystemTree fileSystemTree);
 
   final class SERVICE {
-    private SERVICE() {
-    }
+    private SERVICE() { }
 
     public static FileSystemTreeFactory getInstance() {
-      return ApplicationManager.getApplication().getService(FileSystemTreeFactory.class);
+      return new FileSystemTreeFactoryImpl();
     }
   }
 }
