8a5419dbd1a324b867c8bb748fe6380d11e6f3c3
Ivan Posti <ivan.posti@jetbrains.com>
2022-04-13 17:59:38 +0400

    [Markdown][IDEA-291881] Add sanity test for frontmatter header element manipulator
    GitOrigin-RevId: c48ad36176d453a6fc96cafb0eadd5567567cc96

---
+++ plugins/markdown/test/src/org/intellij/plugins/markdown/psi/MarkdownFrontMatterManipulatorSanityTest.kt
@@ -0,0 +1,52 @@
+package org.intellij.plugins.markdown.psi
+
+import com.intellij.openapi.application.runWriteActionAndWait
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.ElementManipulators
+import com.intellij.testFramework.LightPlatformCodeInsightTestCase
+import com.intellij.testFramework.RegistryKeyRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class MarkdownFrontMatterManipulatorSanityTest: LightPlatformCodeInsightTestCase() {
+  @Rule
+  @JvmField
+  val rule = RegistryKeyRule("markdown.experimental.frontmatter.support.enable", true)
+
+  @Test
+  fun `test replace`() {
+    // language=Markdown
+    val content = """
+    ---
+    title: Some title with link http://example.com
+    tags:
+      - some tag
+      - some tag with link http://example.com
+      - some other tag
+    ---
+    """.trimIndent()
+    // language=Markdown
+    val expected = """
+    ---
+    title: Some title with link https://example.com
+    tags:
+      - some tag
+      - some tag with link https://example.com
+      - some other tag
+    ---
+    """.trimIndent()
+    configureFromFileText("some.md", content)
+    val element = file.firstChild.firstChild!!
+    val manipulator = ElementManipulators.getNotNullManipulator(element)
+    runWriteActionAndWait {
+      manipulator.handleContentChange(element, TextRange(93, 100), "https://")
+      commitAllDocuments()
+      manipulator.handleContentChange(element, TextRange(32, 39), "https://")
+      commitAllDocuments()
+    }
+    checkResultByText(expected)
+  }
+}
