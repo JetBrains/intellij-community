consulo/consulo-kotlin

KTIJ-19939 Provide quickfix for deprecated confusing expressions in when branches
^KTIJ-19939 fixed

GitOrigin-RevId: ba5c306d72b06846c72517f74b8f0aeebf512bf1

idea/src/org/jetbrains/kotlin/idea/inspections/migration/AmbiguousExpressionInWhenBranchMigrationInspection.kt
@@ -1,0 +1,29 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.inspections.migration
+
+import com.intellij.codeInspection.CleanupLocalInspectionTool
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.config.LanguageVersion
+import org.jetbrains.kotlin.diagnostics.DiagnosticFactoryWithPsiElement
+import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.migration.MigrationInfo
+import org.jetbrains.kotlin.idea.migration.isLanguageVersionUpdate
+import org.jetbrains.kotlin.idea.quickfix.migration.MigrationFix
+
+
+class AmbiguousExpressionInWhenBranchMigrationInspection :
+    AbstractDiagnosticBasedMigrationInspection<PsiElement>(PsiElement::class.java),
+    MigrationFix,
+    CleanupLocalInspectionTool {
+
+    override fun isApplicable(migrationInfo: MigrationInfo): Boolean {
+        return migrationInfo.isLanguageVersionUpdate(LanguageVersion.KOTLIN_1_6, LanguageVersion.KOTLIN_1_7)
+    }
+
+    override fun descriptionMessage(): String = KotlinBundle.message("inspection.ambiguous.expression.when.branch.migration.display.name")
+
+    override val diagnosticFactory: DiagnosticFactoryWithPsiElement<PsiElement, *>
+        get() = Errors.CONFUSING_BRANCH_CONDITION.warningFactory
+}

JetBrains/intellij-community

(cherry picked from commit ba5c306d72b06846c72517f74b8f0aeebf512bf1)
GitOrigin-RevId: 23abba2545d37e0dd8bbee98dfea432ec2800003

plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/migration/AmbiguousExpressionInWhenBranchMigrationInspection.kt
@@ -1,0 +1,29 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.inspections.migration
+
+import com.intellij.codeInspection.CleanupLocalInspectionTool
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.config.LanguageVersion
+import org.jetbrains.kotlin.diagnostics.DiagnosticFactoryWithPsiElement
+import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.configuration.MigrationInfo
+import org.jetbrains.kotlin.idea.configuration.isLanguageVersionUpdate
+import org.jetbrains.kotlin.idea.quickfix.migration.MigrationFix
+
+
+class AmbiguousExpressionInWhenBranchMigrationInspection :
+    AbstractDiagnosticBasedMigrationInspection<PsiElement>(PsiElement::class.java),
+    MigrationFix,
+    CleanupLocalInspectionTool {
+
+    override fun isApplicable(migrationInfo: MigrationInfo): Boolean {
+        return migrationInfo.isLanguageVersionUpdate(LanguageVersion.KOTLIN_1_6, LanguageVersion.KOTLIN_1_7)
+    }
+
+    override fun descriptionMessage(): String = KotlinBundle.message("inspection.ambiguous.expression.when.branch.migration.display.name")
+
+    override val diagnosticFactory: DiagnosticFactoryWithPsiElement<PsiElement, *>
+        get() = Errors.CONFUSING_BRANCH_CONDITION.warningFactory
+}