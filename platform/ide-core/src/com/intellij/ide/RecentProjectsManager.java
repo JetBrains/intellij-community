a1bae372438edddace13433646a111e3e121f817
Anna Kozlova <anna.kozlova@jetbrains.com>
2022-04-14 20:11:34 +0200

    cleanup
    GitOrigin-RevId: e2423f7f01de4550ff5cb0980f628bebd5dcef15

--- platform/ide-core/src/com/intellij/ide/RecentProjectsManager.java
+++ platform/ide-core/src/com/intellij/ide/RecentProjectsManager.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide;
 
 import com.intellij.openapi.actionSystem.AnAction;
@@ -80,6 +80,6 @@ public abstract class RecentProjectsManager {
   public abstract @NotNull String suggestNewProjectLocation();
 
   public interface RecentProjectsChange {
-    public void change();
+    void change();
   }
 }
