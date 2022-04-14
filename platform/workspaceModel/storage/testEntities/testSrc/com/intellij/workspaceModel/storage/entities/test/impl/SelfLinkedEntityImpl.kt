f2602384dd9e192a8837034139451ce220c5b907
Alex Plate <aleksei.plate@jetbrains.com>
2022-04-14 15:50:20 +0300

    [Workspace Model] Add missing file
    GitOrigin-RevId: 555aa629cb25896d5a8d04368098773d38c9cb6e

---
+++ platform/workspaceModel/storage/testEntities/testSrc/com/intellij/workspaceModel/storage/entities/test/impl/SelfLinkedEntityImpl.kt
@@ -0,0 +1,217 @@
+package com.intellij.workspaceModel.storage.entities.test.api
+
+import com.intellij.workspaceModel.storage.EntitySource
+import com.intellij.workspaceModel.storage.ModifiableWorkspaceEntity
+import com.intellij.workspaceModel.storage.WorkspaceEntity
+import com.intellij.workspaceModel.storage.WorkspaceEntityStorage
+import com.intellij.workspaceModel.storage.WorkspaceEntityStorageBuilder
+import com.intellij.workspaceModel.storage.impl.ConnectionId
+import com.intellij.workspaceModel.storage.impl.ExtRefKey
+import com.intellij.workspaceModel.storage.impl.ModifiableWorkspaceEntityBase
+import com.intellij.workspaceModel.storage.impl.WorkspaceEntityBase
+import com.intellij.workspaceModel.storage.impl.WorkspaceEntityData
+import com.intellij.workspaceModel.storage.impl.extractOneToManyParent
+import com.intellij.workspaceModel.storage.impl.updateOneToManyParentOfChild
+import org.jetbrains.deft.ObjBuilder
+
+    
+
+open class SelfLinkedEntityImpl: SelfLinkedEntity, WorkspaceEntityBase() {
+    
+    companion object {
+        internal val PARENTENTITY_CONNECTION_ID: ConnectionId = ConnectionId.create(SelfLinkedEntity::class.java, SelfLinkedEntity::class.java, ConnectionId.ConnectionType.ONE_TO_MANY, true)
+    }
+        
+    override val parentEntity: SelfLinkedEntity?
+        get() = snapshot.extractOneToManyParent(PARENTENTITY_CONNECTION_ID, this)
+
+    class Builder(val result: SelfLinkedEntityData?): ModifiableWorkspaceEntityBase<SelfLinkedEntity>(), SelfLinkedEntity.Builder {
+        constructor(): this(SelfLinkedEntityData())
+                 
+        override fun build(): SelfLinkedEntity = this
+        
+        override fun applyToBuilder(builder: WorkspaceEntityStorageBuilder) {
+            if (this.diff != null) {
+                if (existsInBuilder(builder)) {
+                    this.diff = builder
+                    return
+                }
+                else {
+                    error("Entity SelfLinkedEntity is already created in a different builder")
+                }
+            }
+            
+            this.diff = builder
+            this.snapshot = builder
+            addToBuilder()
+            this.id = getEntityData().createEntityId()
+            
+            // Process entities from extension fields
+            val keysToRemove = ArrayList<ExtRefKey>()
+            for ((key, entity) in extReferences) {
+                if (!key.isChild()) {
+                    continue
+                }
+                if (entity is List<*>) {
+                    for (item in entity) {
+                        if (item is ModifiableWorkspaceEntityBase<*>) {
+                            builder.addEntity(item)
+                        }
+                    }
+                    entity as List<WorkspaceEntity>
+                    val (withBuilder_entity, woBuilder_entity) = entity.partition { it is ModifiableWorkspaceEntityBase<*> && it.diff != null }
+                    applyRef(key.getConnectionId(), withBuilder_entity)
+                    keysToRemove.add(key)
+                }
+                else {
+                    entity as WorkspaceEntity
+                    builder.addEntity(entity)
+                    applyRef(key.getConnectionId(), entity)
+                    keysToRemove.add(key)
+                }
+            }
+            for (key in keysToRemove) {
+                extReferences.remove(key)
+            }
+            
+            // Adding parents and references to the parent
+            val __parentEntity = _parentEntity
+            if (__parentEntity != null && (__parentEntity is ModifiableWorkspaceEntityBase<*>) && __parentEntity.diff == null) {
+                builder.addEntity(__parentEntity)
+            }
+            if (__parentEntity != null && (__parentEntity is ModifiableWorkspaceEntityBase<*>) && __parentEntity.diff != null) {
+                // Set field to null (in referenced entity)
+                val __mutChildren = ((__parentEntity as ModifiableWorkspaceEntityBase<*>).extReferences[ExtRefKey("SelfLinkedEntity", "parentEntity", true, PARENTENTITY_CONNECTION_ID)] as? List<Any> ?: emptyList()).toMutableList()
+                __mutChildren.remove(this)
+                __parentEntity.extReferences[ExtRefKey("SelfLinkedEntity", "parentEntity", true, PARENTENTITY_CONNECTION_ID)] = __mutChildren
+            }
+            if (__parentEntity != null) {
+                applyParentRef(PARENTENTITY_CONNECTION_ID, __parentEntity)
+                this._parentEntity = null
+            }
+            val parentKeysToRemove = ArrayList<ExtRefKey>()
+            for ((key, entity) in extReferences) {
+                if (key.isChild()) {
+                    continue
+                }
+                if (entity is List<*>) {
+                    error("Cannot have parent lists")
+                }
+                else {
+                    entity as WorkspaceEntity
+                    builder.addEntity(entity)
+                    applyParentRef(key.getConnectionId(), entity)
+                    parentKeysToRemove.add(key)
+                }
+            }
+            for (key in parentKeysToRemove) {
+                extReferences.remove(key)
+            }
+            checkInitialization() // TODO uncomment and check failed tests
+        }
+    
+        fun checkInitialization() {
+            val _diff = diff
+            if (!getEntityData().isEntitySourceInitialized()) {
+                error("Field SelfLinkedEntity#entitySource should be initialized")
+            }
+        }
+    
+        
+            var _parentEntity: SelfLinkedEntity? = null
+            override var parentEntity: SelfLinkedEntity?
+                get() {
+                    val _diff = diff
+                    return if (_diff != null) {
+                        _diff.extractOneToManyParent(PARENTENTITY_CONNECTION_ID, this) ?: _parentEntity
+                    } else {
+                        _parentEntity
+                    }
+                }
+                set(value) {
+                    checkModificationAllowed()
+                    val _diff = diff
+                    if (_diff != null && value is ModifiableWorkspaceEntityBase<*> && value.diff == null) {
+                        if (value is SelfLinkedEntityImpl.Builder) {
+                            value.extReferences[ExtRefKey("SelfLinkedEntity", "parentEntity", true, PARENTENTITY_CONNECTION_ID)] = (value.extReferences[ExtRefKey("SelfLinkedEntity", "parentEntity", true, PARENTENTITY_CONNECTION_ID)] as? List<Any> ?: emptyList()) + this
+                        }
+                        // else you're attaching a new entity to an existing entity that is not modifiable
+                        _diff.addEntity(value)
+                    }
+                    if (_diff != null && (value !is ModifiableWorkspaceEntityBase<*> || value.diff != null)) {
+                        _diff.updateOneToManyParentOfChild(PARENTENTITY_CONNECTION_ID, this, value)
+                    }
+                    else {
+                        if (value is SelfLinkedEntityImpl.Builder) {
+                            value.extReferences[ExtRefKey("SelfLinkedEntity", "parentEntity", true, PARENTENTITY_CONNECTION_ID)] = (value.extReferences[ExtRefKey("SelfLinkedEntity", "parentEntity", true, PARENTENTITY_CONNECTION_ID)] as? List<Any> ?: emptyList()) + this
+                        }
+                        // else you're attaching a new entity to an existing entity that is not modifiable
+                        
+                        this._parentEntity = value
+                    }
+                    changedProperty.add("parentEntity")
+                }
+        
+        override var entitySource: EntitySource
+            get() = getEntityData().entitySource
+            set(value) {
+                checkModificationAllowed()
+                getEntityData().entitySource = value
+                changedProperty.add("entitySource")
+                
+            }
+        
+        override fun getEntityData(): SelfLinkedEntityData = result ?: super.getEntityData() as SelfLinkedEntityData
+        override fun getEntityClass(): Class<SelfLinkedEntity> = SelfLinkedEntity::class.java
+    }
+    
+    // TODO: Fill with the data from the current entity
+    fun builder(): ObjBuilder<*> = Builder(SelfLinkedEntityData())
+}
+    
+class SelfLinkedEntityData : WorkspaceEntityData<SelfLinkedEntity>() {
+
+
+    override fun wrapAsModifiable(diff: WorkspaceEntityStorageBuilder): ModifiableWorkspaceEntity<SelfLinkedEntity> {
+        val modifiable = SelfLinkedEntityImpl.Builder(null)
+        modifiable.allowModifications {
+          modifiable.diff = diff
+          modifiable.snapshot = diff
+          modifiable.id = createEntityId()
+          modifiable.entitySource = this.entitySource
+        }
+        return modifiable
+    }
+
+    override fun createEntity(snapshot: WorkspaceEntityStorage): SelfLinkedEntity {
+        val entity = SelfLinkedEntityImpl()
+        entity.entitySource = entitySource
+        entity.snapshot = snapshot
+        entity.id = createEntityId()
+        return entity
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) return false
+        if (this::class != other::class) return false
+        
+        other as SelfLinkedEntityData
+        
+        if (this.entitySource != other.entitySource) return false
+        return true
+    }
+
+    override fun equalsIgnoringEntitySource(other: Any?): Boolean {
+        if (other == null) return false
+        if (this::class != other::class) return false
+        
+        other as SelfLinkedEntityData
+        
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = entitySource.hashCode()
+        return result
+    }
+}
