522f7eafe07ac5f2a497fb58edfd45612bbaae62
Louis Vignier <louis.vignier@jetbrains.com>
2022-04-14 16:10:34 +0200

    IDEA-291961 [java] Make 'Migrate Packages and Classes' option available in Find Action…
    #IDEA-291961 Fixed
    
    GitOrigin-RevId: e1069ac7c61ca4d4ed5095d67054ec41d3fabf32

--- java/java-impl-refactorings/src/com/intellij/refactoring/actions/MigrateAction.java
+++ java/java-impl-refactorings/src/com/intellij/refactoring/actions/MigrateAction.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.actions;
 
-import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
@@ -23,6 +22,6 @@ public class MigrateAction extends AnAction {
   public void update(@NotNull AnActionEvent event) {
     Presentation presentation = event.getPresentation();
     Project project = event.getProject();
-    presentation.setEnabledAndVisible(project != null && !ActionPlaces.isPopupPlace(event.getPlace()));
+    presentation.setEnabledAndVisible(project != null);
   }
 }
