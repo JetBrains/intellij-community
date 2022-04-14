f822e1a40d27108403fe5d762e15e6140b69a3c7
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2022-04-14 20:30:06 +0300

    IDEA-263185 Allow turning the opened project window into a tab on Mac: "Merge all project windows" button and "view tab bar" button
    GitOrigin-RevId: bf1e0a6eea1c1bfdafb6ed6d674ca743fd159c25

---
+++ platform/platform-impl/src/com/intellij/ui/mac/MergeAllWindowsAction.java
@@ -0,0 +1,54 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ui.mac;
+
+import com.intellij.jdkEx.JdkEx;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformCoreDataKeys;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.openapi.wm.IdeFrame;
+import com.intellij.openapi.wm.WindowManager;
+import com.intellij.openapi.wm.impl.ProjectFrameHelper;
+import com.intellij.ui.mac.foundation.Foundation;
+import com.intellij.ui.mac.foundation.ID;
+import com.intellij.ui.mac.foundation.MacUtil;
+import com.intellij.util.ui.UIUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.util.Objects;
+
+/**
+ * @author Alexander Lobas
+ */
+public class MergeAllWindowsAction extends DumbAwareAction {
+  @Override
+  public void update(@NotNull AnActionEvent e) {
+    Presentation presentation = e.getPresentation();
+    if (JdkEx.isTabbingModeAvailable()) {
+      presentation.setVisible(true);
+      IdeFrame[] frames = WindowManager.getInstance().getAllProjectFrames();
+      if (frames.length > 1) {
+        ID id = MacUtil.getWindowFromJavaWindow((((ProjectFrameHelper)frames[0]).getFrame()));
+        int tabs = Foundation.invoke(Foundation.invoke(id, "tabbedWindows"), "count").intValue();
+        presentation.setEnabled(frames.length != tabs);
+      }
+      else {
+        presentation.setEnabled(false);
+      }
+    }
+    else {
+      presentation.setEnabledAndVisible(false);
+    }
+  }
+
+  @Override
+  public void actionPerformed(@NotNull AnActionEvent e) {
+    Window window = Objects.requireNonNull(UIUtil.getWindow(e.getData(PlatformCoreDataKeys.CONTEXT_COMPONENT)));
+
+    Foundation.executeOnMainThread(true, false, () -> {
+      ID id = MacUtil.getWindowFromJavaWindow(window);
+      Foundation.invoke(id, "mergeAllWindows:", ID.NIL);
+    });
+  }
+}
