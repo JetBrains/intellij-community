5526375cb57ca56cc6ef37621a2778f317a43b77
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2022-04-15 13:42:32 +0200

    [inspect] Refresh filesystem cache before running inspections
    Resolves an issue where downloaded projects are not found in inspection integration tests.
    
    GitOrigin-RevId: 541093d514ebfd37b1661e2d794b40963a6cfb02

--- platform/inspect/src/com/intellij/codeInspection/InspectionApplicationBase.java
+++ platform/inspect/src/com/intellij/codeInspection/InspectionApplicationBase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInspection;
 
 import com.intellij.ProjectTopics;
@@ -215,7 +215,7 @@ public class InspectionApplicationBase implements CommandLineInspectionProgressR
   @Nullable
   protected Project openProject(@NotNull Path projectPath, @NotNull Disposable parentDisposable)
     throws InterruptedException, ExecutionException {
-    VirtualFile vfsProject = LocalFileSystem.getInstance().findFileByPath(FileUtil.toSystemIndependentName(projectPath.toString()));
+    VirtualFile vfsProject = LocalFileSystem.getInstance().refreshAndFindFileByPath(FileUtil.toSystemIndependentName(projectPath.toString()));
     if (vfsProject == null) {
       reportError(InspectionsBundle.message("inspection.application.file.cannot.be.found", projectPath));
       printHelp();
