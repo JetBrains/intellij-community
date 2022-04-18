04f57aac5305d1a83dfc61d4a1f54f6e2432ee79
Pavel Gromov <pavel.gromov@jetbrains.com>
2022-04-18 18:08:40 +0300

    [recent projects] Replace "Manage recent project" UI with the new implementation (IDEA-292229)
    GitOrigin-RevId: cb0bbdde071d1b2d768b06d909e861e9dfbe9219

--- platform/platform-impl/src/com/intellij/ide/ManageRecentProjectsAction.java
+++ platform/platform-impl/src/com/intellij/ide/ManageRecentProjectsAction.java
@@ -1,9 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide;
 
-import com.intellij.ide.actions.RecentProjectsGroup;
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.project.DumbAwareAction;
@@ -12,16 +10,14 @@ import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.ui.popup.util.PopupUtil;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.wm.impl.welcomeScreen.recentProjects.RecentProjectFilteringTree;
+import com.intellij.openapi.wm.impl.welcomeScreen.recentProjects.RecentProjectPanelComponentFactory;
 import com.intellij.openapi.wm.impl.welcomeScreen.NewRecentProjectPanel;
 import com.intellij.ui.IdeUICustomization;
 import com.intellij.ui.SearchTextField;
-import com.intellij.ui.components.JBList;
-import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import java.awt.*;
-import java.util.Arrays;
 
 /**
  * @author Konstantin Bulenkov
@@ -31,17 +27,14 @@ final class ManageRecentProjectsAction extends DumbAwareAction {
   public void actionPerformed(@NotNull AnActionEvent e) {
     Disposable disposable = Disposer.newDisposable();
     Project project = e.getRequiredData(CommonDataKeys.PROJECT);
-    NewRecentProjectPanel panel = new NewRecentProjectPanel(disposable) {
-      @Override
-      protected JBList<AnAction> createList(AnAction[] recentProjectActions, Dimension size) {
-        return super.createList(RecentProjectsGroup.removeCurrentProject(project, Arrays.asList(recentProjectActions)), size);
-      }
-    };
-    JList<?> list = UIUtil.findComponentOfType(panel, JList.class);
-    PopupUtil.applyNewUIBackground(panel);
-    SearchTextField searchTextField = UIUtil.findComponentOfType(panel, SearchTextField.class);
+
+    RecentProjectFilteringTree recentProjectFilteringTree = RecentProjectPanelComponentFactory.createComponent(disposable);
+    JComponent recentProjectTree = recentProjectFilteringTree.getComponent();
+    SearchTextField searchTextField = recentProjectFilteringTree.installSearchField();
+
+    PopupUtil.applyNewUIBackground(recentProjectTree);
     PopupUtil.applyNewUIBackground(searchTextField);
-    JBPopup popup = JBPopupFactory.getInstance().createComponentPopupBuilder(panel, list)
+    JBPopup popup = JBPopupFactory.getInstance().createComponentPopupBuilder(recentProjectTree, recentProjectTree)
       .setTitle(IdeUICustomization.getInstance().projectMessage("popup.title.recent.projects"))
       .setFocusable(true)
       .setRequestFocus(true)
