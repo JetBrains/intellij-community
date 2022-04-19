cae0e5795f399a2dde0b8993bea45dab578daa6c
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2022-04-19 17:18:54 +0200

    [kotlin] Update test data for 'IntentionTestGenerated', exhaustive when
    'Exhaustive when' is enabled for algebraic types such as 'enum class' or 'sealed class' (see KT-47709). This commit adapts test data to this change.
    
    (cherry picked from commit 2949340eceed2493fd7784de49bb08f6d2736e66)
    
    GitOrigin-RevId: 7936bcb8b98b0d28bf95bf93354fb35a24baf00a

--- plugins/kotlin/idea/tests/testData/intentions/branched/ifWhen/whenToIf/whenTrueOrFalse3.kt
+++ plugins/kotlin/idea/tests/testData/intentions/branched/ifWhen/whenToIf/whenTrueOrFalse3.kt
@@ -1,3 +1,6 @@
+// ERROR: 'when' expression must be exhaustive, add necessary 'false' branch or 'else' branch instead
+// SKIP_ERRORS_AFTER
+
 fun foo(i : Int) {
     <caret>when (0 == i) {
         true -> 1
