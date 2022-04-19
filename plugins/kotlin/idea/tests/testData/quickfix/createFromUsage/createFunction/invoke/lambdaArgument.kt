29290ef22f5420ae8527cf63282fe459659196f6
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-04-19 18:22:01 +0200

    [kotlin] Mute test QuickFixTestGenerated$CreateFromUsage$CreateFunction$Invoke.testLambdaArgument
    Issue was created: KTIJ-1262
    
    GitOrigin-RevId: c1985caee27b0ad2211468f0adc8109d470c1e94

--- plugins/kotlin/idea/tests/testData/quickfix/createFromUsage/createFunction/invoke/lambdaArgument.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/createFromUsage/createFunction/invoke/lambdaArgument.kt
@@ -1,4 +1,9 @@
-// "Create extension function 'Test.invoke'" "true"
+// "Create extension function 'Test.invoke'" "false"
+// ACTION: Add explicit type arguments
+// ACTION: Create function 't'
+// ERROR: Unresolved reference. None of the following candidates is applicable because of receiver type mismatch: <br>public operator fun <T, R> DeepRecursiveFunction<TypeVariable(T), TypeVariable(R)>.invoke(value: TypeVariable(T)): TypeVariable(R) defined in kotlin
+
+// KTIJ-1262 "Create extension function 'Test.invoke'" quickfix should suggest for variable with lambda argument
 class Test
 
 fun test() {
