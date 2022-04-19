dd6bc689ece92b114ff31bc74c28cfc386957a75
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2022-04-19 12:43:18 +0200

    [json] IDEA-291190 Avoid making JsonSchemaObject a UserDataHolder since it is a public API class
    Use class field for cached values instead
    
    (cherry picked from commit 535315474d60d2cc4eb63479fe1e8b24bca6fd06)
    
    IJ-CR-23370
    
    GitOrigin-RevId: 547bb6699ce9ecc12e7a4f4f07c5e82b5e970c2c

--- json/src/com/jetbrains/jsonSchema/impl/JsonSchemaObject.java
+++ json/src/com/jetbrains/jsonSchema/impl/JsonSchemaObject.java
@@ -37,7 +37,7 @@ import static com.jetbrains.jsonSchema.JsonPointerUtil.*;
 /**
  * @author Irina.Chernushina on 8/28/2015.
  */
-public final class JsonSchemaObject extends UserDataHolderBase {
+public final class JsonSchemaObject {
   private static final Logger LOG = Logger.getInstance(JsonSchemaObject.class);
 
   public static final String MOCK_URL = "mock:///";
@@ -131,6 +131,8 @@ public final class JsonSchemaObject extends UserDataHolderBase {
   @Nullable private Map<String, Map<String, String>> myEnumMetadata;
 
   private boolean myForceCaseInsensitive = false;
+
+  private final UserDataHolderBase myUserDataHolder = new UserDataHolderBase();
 
   public boolean isValidByExclusion() {
     return myIsValidByExclusion;
@@ -1158,7 +1160,7 @@ public final class JsonSchemaObject extends UserDataHolderBase {
 
   private ConcurrentMap<String, JsonSchemaObject> getComputedRefsStorage(@NotNull Project project) {
     return CachedValuesManager.getManager(project).getCachedValue(
-      this,
+      myUserDataHolder,
       () -> CachedValueProvider.Result.create(new ConcurrentHashMap<String, JsonSchemaObject>(), JsonDependencyModificationTracker.forProject(project))
     );
   }
