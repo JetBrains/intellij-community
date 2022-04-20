800137c5e331861a531f91b69487eb8f14b9d66a
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2022-04-20 16:27:21 +0200

    Fix wrong writing of rsync and passive mode values for deployment servers; cover with tests
    (cherry picked from commit 60255c4b997e644ecdae5fcd6c1ff3cf42033a62)
    IJ-CR-23632
    
    GitOrigin-RevId: 6131afd1280d4da2334c5009e3f35c08326c1b86

--- platform/statistics/test/com/intellij/internal/statistic/FUCounterCollectorTestCase.kt
+++ platform/statistics/test/com/intellij/internal/statistic/FUCounterCollectorTestCase.kt
@@ -1,7 +1,10 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.internal.statistic
 
+import com.intellij.internal.statistic.beans.MetricEvent
 import com.intellij.internal.statistic.eventLog.fus.FeatureUsageLogger
+import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector
+import com.intellij.openapi.project.Project
 import com.jetbrains.fus.reporting.model.lion3.LogEvent
 
 object FUCounterCollectorTestCase {
@@ -16,5 +19,11 @@ object FUCounterCollectorTestCase {
     finally {
       FeatureUsageLogger.loggerProvider = oldLogger
     }
+  }
+
+  fun collectStateCollectorEvents(collectorClass: Class<out ProjectUsagesCollector>, project: Project): Set<MetricEvent> {
+    val collector: ProjectUsagesCollector = collectorClass.getConstructor().newInstance()
+    val method = collectorClass.getMethod("getMetrics", Project::class.java)
+    return method.invoke(collector, project) as Set<MetricEvent>
   }
 }
