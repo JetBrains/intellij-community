ae5e1f04ace7f41f662e5b7c0f66ef0ff874fe40
Egor Ushakov <egor.ushakov@jetbrains.com>
2022-04-20 13:51:31 +0300

    IDEA-292557 Debugger: color key for grey text in Variables panel
    GitOrigin-RevId: 14303167ea78372ae1ae98290461d8a328427aef

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/XDebuggerUIConstants.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/XDebuggerUIConstants.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.xdebugger.impl.ui;
 
 import com.intellij.icons.AllIcons;
@@ -33,7 +33,8 @@ public final class XDebuggerUIConstants {
     get(JBColor.namedColor("Debugger.Variables.errorMessageForeground", JBColor.red));
   @NonNls public static final String EQ_TEXT = " = ";
 
-  public static final SimpleTextAttributes TYPE_ATTRIBUTES = SimpleTextAttributes.GRAY_ATTRIBUTES;
+  public static final SimpleTextAttributes TYPE_ATTRIBUTES =
+    get(JBColor.namedColor("Debugger.Variables.typeForeground", JBColor.gray));
   public static final String LAYOUT_VIEW_BREAKPOINT_CONDITION = "breakpoint";
   public static final String LAYOUT_VIEW_FINISH_CONDITION = "finish";
 
