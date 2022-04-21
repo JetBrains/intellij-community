5bae1d115fc6f82dbb689a2af4fe29c5c7f2c0f6
Andrey.Cherkasov <andrey.cherkasov@jetbrains.com>
2022-04-21 03:50:30 +0300

    [java-inspections] UrlToHtmlFix: rename and make the first constructor parameter `@NotNull`
    GitOrigin-RevId: e2a6d37761edd3b85b476ebafca1d93897459e1f

--- java/java-impl/src/com/intellij/codeInspection/javaDoc/UrlToHtmlFix.java
+++ java/java-impl/src/com/intellij/codeInspection/javaDoc/UrlToHtmlFix.java
@@ -18,13 +18,13 @@ class UrlToHtmlFix extends LocalQuickFixAndIntentionActionOnPsiElement {
   private final int myEndOffsetInDocComment;
 
   /**
-   * @param element                 target Javadoc comment to fix
+   * @param comment                 target Javadoc comment to fix
    * @param startOffsetInDocComment the start offset of the URL in Javadoc comment
    * @param endOffsetInDocComment   if you want to use "..." as the HTML link text, use the end URL offset in a JavaDoc comment;
    *                                if you want to use a substring after the URL as the HTML link text, use the end of that substring
    */
-  UrlToHtmlFix(@Nullable PsiDocComment element, int startOffsetInDocComment, int endOffsetInDocComment) {
-    super(element);
+  UrlToHtmlFix(@NotNull PsiDocComment comment, int startOffsetInDocComment, int endOffsetInDocComment) {
+    super(comment);
     myStartOffsetInDocComment = startOffsetInDocComment;
     myEndOffsetInDocComment = endOffsetInDocComment;
   }
