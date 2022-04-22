cbc824f12854afb568d310065cdeca013767fb5a
Ivan Posti <ivan.posti@jetbrains.com>
2022-04-22 17:03:18 +0400

    [Markdown] Add PsiElement.children() extension function
    GitOrigin-RevId: 3973edc6eec0fb310b773d0f1fe4ad4f64d2f5f8

--- plugins/markdown/core/src/org/intellij/plugins/markdown/util/PsiUtils.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/util/PsiUtils.kt
@@ -6,9 +6,18 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.TokenSet
 import com.intellij.psi.util.PsiUtilCore
+import com.intellij.psi.util.siblings
 
 internal fun PsiElement.hasType(type: IElementType) = PsiUtilCore.getElementType(this) == type
 internal fun PsiElement.hasType(type: TokenSet) =  PsiUtilCore.getElementType(this) in type
 
 internal fun ASTNode.hasType(type: IElementType) = PsiUtilCore.getElementType(this) == type
 internal fun ASTNode.hasType(type: TokenSet) = PsiUtilCore.getElementType(this) in type
+
+internal fun PsiElement.children(): Sequence<PsiElement> {
+  return childrenOrNull().orEmpty()
+}
+
+internal fun PsiElement.childrenOrNull(): Sequence<PsiElement>? {
+  return firstChild?.siblings(forward = true, withSelf = true)
+}
