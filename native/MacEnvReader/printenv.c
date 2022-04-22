b0a9fa9a4afbc392d926eb2f0ef6455a7833ee38
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2022-04-22 23:26:35 +0200

    [platform] uniform I/O calls in macOS `printenv`; formatting
    GitOrigin-RevId: 847f6056e2b360ce368ba0bf0bb91e682c2bb3e7

--- native/MacEnvReader/printenv.c
+++ native/MacEnvReader/printenv.c
@@ -1,16 +1,12 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors.
-// Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 #include <stdio.h>
 
-int main(int argc __attribute__((unused)),
-  const char **argv __attribute__((unused)),
-  const char **env) {
-
+int main(int argc __attribute__((unused)), const char **argv __attribute__((unused)), const char **env) {
   const char **v = env;
   while (*v != NULL) {
     fputs(*v, stdout);
-    putc('\0', stdout);
+    fputc('\0', stdout);
     v++;
   }
 
