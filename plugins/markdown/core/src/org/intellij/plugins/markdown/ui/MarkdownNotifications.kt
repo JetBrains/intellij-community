d363b20f7a8c82a4a47d521d3ee4bd33b7b352aa
Ivan Posti <ivan.posti@jetbrains.com>
2022-04-22 17:04:05 +0400

    [Markdown] Update title and message strings annotations
    GitOrigin-RevId: 5bf500f42c029bfc127056ccba5966c0acd5733b

--- plugins/markdown/core/src/org/intellij/plugins/markdown/ui/MarkdownNotifications.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/ui/MarkdownNotifications.kt
@@ -4,7 +4,7 @@ package org.intellij.plugins.markdown.ui
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.NlsSafe
+import com.intellij.openapi.util.NlsContexts
 import org.jetbrains.annotations.NonNls
 
 /**
@@ -14,19 +14,34 @@ internal object MarkdownNotifications {
   val group
     get() = requireNotNull(NotificationGroupManager.getInstance().getNotificationGroup("Markdown"))
 
-  fun showError(project: Project?, id: @NonNls String?, @NlsSafe title: String = "", @NlsSafe message: String) {
+  fun showError(
+    project: Project?,
+    id: @NonNls String?,
+    @NlsContexts.NotificationTitle title: String = "",
+    @NlsContexts.NotificationContent message: String
+  ) {
     val notification = group.createNotification(title, message, NotificationType.ERROR)
     id?.let { notification.setDisplayId(it) }
     notification.notify(project)
   }
 
-  fun showWarning(project: Project?, id: @NonNls String?, @NlsSafe title: String = "", @NlsSafe message: String) {
+  fun showWarning(
+    project: Project?,
+    id: @NonNls String?,
+    @NlsContexts.NotificationTitle title: String = "",
+    @NlsContexts.NotificationContent message: String
+  ) {
     val notification = group.createNotification(title, message, NotificationType.WARNING)
     id?.let { notification.setDisplayId(it) }
     notification.notify(project)
   }
 
-  fun showInfo(project: Project?, id: @NonNls String?, @NlsSafe title: String = "", @NlsSafe message: String) {
+  fun showInfo(
+    project: Project?,
+    id: @NonNls String?,
+    @NlsContexts.NotificationTitle title: String = "",
+    @NlsContexts.NotificationContent message: String
+  ) {
     val notification = group.createNotification(title, message, NotificationType.INFORMATION)
     id?.let { notification.setDisplayId(it) }
     notification.notify(project)
