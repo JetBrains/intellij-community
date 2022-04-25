a3eced1e77d6e8c8af1cff4bd3d0524a13ef4d53
Tagir Valeev <tagir.valeev@jetbrains.com>
2022-04-25 17:20:10 +0200

    [java-dfa] Fix imbalanced stack on incomplete guarded pattern
    Fixes EA-447737 - ESE: Stack.pop
    (at least some cases linked to this exception)
    
    GitOrigin-RevId: 5709b7fc22e9495e07f20f899cb66274b0443fc0

--- java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/java/ControlFlowAnalyzer.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/java/ControlFlowAnalyzer.java
@@ -1085,6 +1085,8 @@ public class ControlFlowAnalyzer extends JavaElementVisitor {
       PsiExpression expression = ((PsiGuardedPattern)innerPattern).getGuardingExpression();
       if (expression != null) {
         expression.accept(this);
+      } else {
+        addInstruction(new PushValueInstruction(DfTypes.BOOLEAN));
       }
       DeferredOffset condGotoOffset = new DeferredOffset();
       addInstruction(new ConditionalGotoInstruction(condGotoOffset, DfTypes.TRUE));
