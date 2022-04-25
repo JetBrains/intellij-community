0c034decbdd6b9f3dc2aedef9840fea8323e0bbb
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2022-04-25 14:02:35 +0200

    IDEA-288658: util: Move ParametersListUtil tests to ParametersListUtil
    GitOrigin-RevId: da5a546465beffac06a922dffc718980c6c5d98c

--- platform/platform-tests/testSrc/com/intellij/openapi/execution/ParametersListUtilTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/execution/ParametersListUtilTest.java
@@ -7,7 +7,9 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class ParametersListUtilTest {
 
@@ -25,5 +27,43 @@ public class ParametersListUtilTest {
 
     checkInvariants("-Xplugin:ErrorProne\t" +
                     "-XepDisableWarningsInGeneratedCode");
+  }
+
+  @Test
+  public void testQuotesInParameterList() {
+    final List<String> expected = asList(
+      "cmd",
+      "-a",
+      "-b",
+      "arg0",
+      "-c",
+      "--long-option",
+      "--long-opt2=arg1",
+      "arg2",
+      "arg3",
+      "-a",
+      "a \"r g",
+      "--foo=d e f"
+    );
+    final String doubleQuotes = "cmd -a -b arg0 -c --long-option    --long-opt2=arg1 arg2 arg3 -a \"a \\\"r g\" --foo=\"d e f\"\"\"";
+    assertEquals("Double quotes broken", expected, ParametersListUtil.parse(doubleQuotes, false, true));
+
+    final String singleQuotes = "cmd -a -b arg0 -c --long-option    --long-opt2=arg1 arg2 arg3 -a 'a \"r g' --foo='d e f'";
+    assertEquals("Single quotes broken", expected, ParametersListUtil.parse(singleQuotes, false, true));
+
+    final String mixedQuotes = "cmd -a -b arg0 -c --long-option    --long-opt2=arg1 arg2 arg3 -a \"a \\\"r g\" --foo='d e f'";
+    assertEquals("Mixed quotes broken", expected, ParametersListUtil.parse(mixedQuotes, false, true));
+  }
+
+  @Test
+  public void testEmptyParameters() {
+    assertEquals("Expected empty parameters", asList("cmd", "", "-a", "", "", "text", "--long-option", ""),
+                 ParametersListUtil.parse("cmd \"\" -a \"\" '' 'text' --long-option \"\"", false, true));
+
+    assertEquals("Should be no empty parameters", asList("cmd", "-a", "text", "--long-option"),
+                 ParametersListUtil.parse("cmd  -a   'text' --long-option ", false, true));
+
+    assertEquals("Empty trailing parameter broken", asList("cmd", "", "-a", "", "", "text", "--long-option", ""),
+                 ParametersListUtil.parse("cmd  -a   'text' --long-option ", false, true, true));
   }
 }
