ed6bee7ccfe32ffc2078e7a72bf11ffa3d735a18
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-04-26 17:23:04 +0200

    Revert "Return `intellij.platform.core` into Kotlin JPS plugin classpath"
    This reverts commit accc6c1040608da0b32f40f8e1e692c733194955.
    This is done to prevent IDEA-292483 till the 221.1 release
    
    GitOrigin-RevId: d82135581d53c0350aec21d34114ec21ba43f820

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/jps/KotlinJpsClasspathProvider.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/jps/KotlinJpsClasspathProvider.kt
@@ -3,7 +3,6 @@ package org.jetbrains.kotlin.idea.jps
 
 import com.intellij.compiler.server.BuildProcessParametersProvider
 import com.intellij.openapi.project.Project
-import com.intellij.util.PathUtil
 import org.jetbrains.kotlin.idea.compiler.configuration.KotlinJpsPluginSettings
 import org.jetbrains.kotlin.idea.compiler.configuration.KotlinPluginLayout
 
@@ -12,6 +11,6 @@ class KotlinJpsClasspathProvider(private val project: Project) : BuildProcessPar
         val jpsPluginClasspath = KotlinJpsPluginSettings.getInstance(project)?.settings?.version
             ?.let { SetupKotlinJpsPluginBeforeCompileTask.getKotlinJpsClasspathLocation(it) }
             ?: KotlinPluginLayout.instance.jpsPluginJar
-        return listOf(jpsPluginClasspath.canonicalPath, PathUtil.getJarPathForClass(com.intellij.util.PathUtil::class.java))
+        return listOf(jpsPluginClasspath.canonicalPath)
     }
 }
