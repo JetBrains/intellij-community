1d9ede9ca0e00d3bd8d17e4fc5df87ffdeae49af
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2022-04-26 23:19:44 +0300

    [vcs-log] add internal diagnostic actions for the index
    Retrieve details from git for the subset of indexed commits (either selected in the table or a number of the oldest commits in the repository, excluding root commits), then perform the following:
    1. compare commit details with the data stored in the forward index;
    2. create author, text and path filters based on the retrieved details and check that the commit is among the filtering results.
    
    IDEA-292515
    
    (cherry picked from commit 43f904ed090e94ebd1f0cfc9223f8a3a365a3f9d)
    
    GitOrigin-RevId: 83790be8ce8c07f5a49c692d9b011547b63e846a

--- platform/vcs-log/graph/src/com/intellij/vcs/log/graph/utils/IntHashSetFlags.kt
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/graph/utils/IntHashSetFlags.kt
@@ -1,9 +1,9 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.log.graph.utils
 
 import it.unimi.dsi.fastutil.ints.IntOpenHashSet
 
-internal open class IntHashSetFlags(private val size: Int) : Flags {
+open class IntHashSetFlags(private val size: Int) : Flags {
   val data = IntOpenHashSet()
 
   override fun size(): Int = size
