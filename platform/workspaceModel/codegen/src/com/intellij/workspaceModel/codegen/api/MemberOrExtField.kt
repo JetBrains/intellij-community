c1627e066473ae3f4193d2e2855bd2abb2921b1e
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2022-04-28 00:04:03 +0300

    [workspace model] Remove ExtensibleProvider interface
    GitOrigin-RevId: f3e2ec2d5683a320385e1b5b066267d2d409edea

--- platform/workspaceModel/codegen/src/com/intellij/workspaceModel/codegen/api/MemberOrExtField.kt
+++ platform/workspaceModel/codegen/src/com/intellij/workspaceModel/codegen/api/MemberOrExtField.kt
@@ -2,7 +2,6 @@ package org.jetbrains.deft.impl.fields
 
 import org.jetbrains.deft.Obj
 import org.jetbrains.deft.codegen.model.DefField
-import org.jetbrains.deft.impl.ExtensibleImpl
 import org.jetbrains.deft.impl.ObjType
 import org.jetbrains.deft.impl.ValueType
 
@@ -10,19 +9,15 @@ abstract class MemberOrExtField<P : Obj, V>(
   val owner: ObjType<P, *>,
   val name: String,
   val type: ValueType<V>
-) : ExtensibleImpl() {
+) : Obj {
   override fun toString(): String = "${owner.name}.$name"
 
   var open: Boolean = false
+  var exDef: DefField? = null
   var content: Boolean = false
   var constructorField = false
   var hasDefault: Field.Default = Field.Default.none
   var defaultValue: String? = null
-  var exDef: DefField?
-    get() = unsafeGetExtension()
-    set(value) {
-      unsafeAddExtension(value!!)
-    }
 
   val hasSetter: Boolean
     get() =
--- platform/workspaceModel/codegen/src/com/intellij/workspaceModel/codegen/api/extensible/ExtensibleProvider.kt
+++
@@ -1,7 +0,0 @@
-package org.jetbrains.deft.impl
-
-import org.jetbrains.deft.obj.api.extensible.Extensible
-
-interface ExtensibleProvider {
-  fun getExtensibleContainer(): Extensible
-}
