8989e4c6b7bb1e787330b6516abf0ce6db86dad8
Adam Malek <adam.malek@jetbrains.com>
2022-04-27 16:24:30 +0200

    ML in SE: Add Symbol features provider (IDEA-287534)
    GitOrigin-RevId: 43b9edb6731585e2e1a73b2f20d9eab1f6b436f0

---
+++ plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/features/SearchEverywhereSymbolFeaturesProvider.kt
@@ -0,0 +1,73 @@
+package com.intellij.ide.actions.searcheverywhere.ml.features
+
+import com.intellij.ide.actions.searcheverywhere.PSIPresentationBgRendererWrapper.PsiItemWithPresentation
+import com.intellij.ide.actions.searcheverywhere.SymbolSearchEverywhereContributor
+import com.intellij.ide.actions.searcheverywhere.ml.features.statistician.SearchEverywhereStatisticianService
+import com.intellij.internal.statistic.collectors.fus.LangCustomRuleValidator
+import com.intellij.internal.statistic.eventLog.events.EventField
+import com.intellij.internal.statistic.eventLog.events.EventFields
+import com.intellij.internal.statistic.eventLog.events.EventPair
+import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.components.service
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiNamedElement
+
+class SearchEverywhereSymbolFeaturesProvider : SearchEverywhereElementFeaturesProvider(SymbolSearchEverywhereContributor::class.java) {
+  companion object {
+    private val LANGUAGE_DATA_KEY = EventFields.StringValidatedByCustomRule("language", LangCustomRuleValidator::class.java)
+    private val PARENT_STAT_USE_COUNT_DATA_KEY = EventFields.Int("parentStatUseCount")
+    private val PARENT_STAT_IS_MOST_POPULAR_DATA_KEY = EventFields.Boolean("parentStatIsMostPopular")
+    private val PARENT_STAT_RECENCY_DATA_KEY = EventFields.Int("parentStatRecency")
+    private val PARENT_STAT_IS_MOST_RECENT_DATA_KEY = EventFields.Boolean("parentStatIsMostRecent")
+  }
+
+  override fun getFeaturesDeclarations(): List<EventField<*>> {
+    return listOf(LANGUAGE_DATA_KEY, PARENT_STAT_USE_COUNT_DATA_KEY, PARENT_STAT_IS_MOST_POPULAR_DATA_KEY,
+                  PARENT_STAT_RECENCY_DATA_KEY, PARENT_STAT_IS_MOST_RECENT_DATA_KEY)
+  }
+
+  override fun getElementFeatures(element: Any,
+                                  currentTime: Long,
+                                  searchQuery: String,
+                                  elementPriority: Int,
+                                  cache: Any?): List<EventPair<*>> {
+    val psiElement = getPsiElement(element) ?: return emptyList()
+    val data = arrayListOf<EventPair<*>>()
+
+    data.add(LANGUAGE_DATA_KEY.with(psiElement.language.id))
+    data.addAll(getParentStatisticianFeatures(psiElement))
+
+    getElementName(element)?.let { name ->
+      data.addAll(getNameMatchingFeatures(name, searchQuery))
+    }
+
+    return data
+  }
+
+  private fun getPsiElement(element: Any) = when (element) {
+    is PsiItemWithPresentation -> element.first
+    is PsiElement -> element
+    else -> null
+  }
+
+  private fun getParentStatisticianFeatures(element: PsiElement): List<EventPair<*>> {
+    val parent = ReadAction.compute<PsiElement?, Nothing> { element.parent } ?: return emptyList()
+    val service = service<SearchEverywhereStatisticianService>()
+
+    return service.getCombinedStats(parent)?.let { stats ->
+      arrayListOf(
+        PARENT_STAT_USE_COUNT_DATA_KEY.with(stats.useCount),
+        PARENT_STAT_IS_MOST_POPULAR_DATA_KEY.with(stats.isMostPopular),
+        PARENT_STAT_RECENCY_DATA_KEY.with(stats.recency),
+        PARENT_STAT_IS_MOST_RECENT_DATA_KEY.with(stats.isMostRecent),
+      )
+    } ?: emptyList()
+  }
+
+  private fun getElementName(element: Any): String? {
+    if (element is PsiItemWithPresentation) return element.presentation.presentableText
+    if (element !is PsiNamedElement) return null
+
+    return ReadAction.compute<String?, Nothing> { element.name }
+  }
+}
