00699255560da83c147e71bafcf9c60e452c8596
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-04-27 14:39:00 +0200

    convert JaegerJsonSpanExporter to modern language (part 2)
    GitOrigin-RevId: 15cc78131239428222b0a19ba80a5aeb2c90f86f

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/JaegerJsonSpanExporter.kt
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/JaegerJsonSpanExporter.kt
@@ -3,7 +3,6 @@ package org.jetbrains.intellij.build.impl
 
 import com.fasterxml.jackson.core.JsonFactory
 import com.fasterxml.jackson.core.JsonGenerator
-import groovy.lang.Closure
 import io.opentelemetry.api.common.AttributeKey
 import io.opentelemetry.api.common.Attributes
 import io.opentelemetry.sdk.common.CompletableResultCode
@@ -22,84 +21,14 @@ import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicReference
 
 class JaegerJsonSpanExporter : SpanExporter {
-  override fun export(spans: Collection<SpanData>): CompletableResultCode {
-    val w = writer.get() ?: return CompletableResultCode.ofSuccess()
-    for (span in spans) {
-      w.writeStartObject()
-      w.writeStringField("traceID", span.traceId)
-      w.writeStringField("spanID", span.spanId)
-      w.writeStringField("operationName", span.name)
-      w.writeStringField("processID", "p1")
-      w.writeNumberField("startTime", TimeUnit.NANOSECONDS.toMicros(span.startEpochNanos))
-      w.writeNumberField("duration", TimeUnit.NANOSECONDS.toMicros(span.endEpochNanos - span.startEpochNanos))
-      val parentContext = span.parentSpanContext
-      val hasError = span.status.statusCode == StatusData.error().statusCode
-      val attributes = span.attributes
-      if (!attributes.isEmpty || hasError) {
-        w.writeArrayFieldStart("tags")
-        if (hasError) {
-          w.writeStartObject()
-          w.writeStringField("key", "otel.status_code")
-          w.writeStringField("type", "string")
-          w.writeStringField("value", "ERROR")
-          w.writeEndObject()
-          w.writeStartObject()
-          w.writeStringField("key", "error")
-          w.writeStringField("type", "bool")
-          w.writeBooleanField("value", true)
-          w.writeEndObject()
-        }
-        writeAttributesAsJson(w, attributes)
-        w.writeEndArray()
-      }
-      val events = span.events
-      if (!events.isEmpty()) {
-        w.writeArrayFieldStart("logs")
-        for (event in events) {
-          w.writeStartObject()
-          w.writeNumberField("timestamp", TimeUnit.NANOSECONDS.toMicros(event.epochNanos))
-          w.writeArrayFieldStart("fields")
-
-          // event name as event attribute
-          w.writeStartObject()
-          w.writeStringField("key", "event")
-          w.writeStringField("type", "string")
-          w.writeStringField("value", event.name)
-          w.writeEndObject()
-          writeAttributesAsJson(w, event.attributes)
-          w.writeEndArray()
-          w.writeEndObject()
-        }
-        w.writeEndArray()
-      }
-      if (parentContext.isValid) {
-        w.writeArrayFieldStart("references")
-        w.writeStartObject()
-        w.writeStringField("refType", "CHILD_OF")
-        w.writeStringField("traceID", parentContext.traceId)
-        w.writeStringField("spanID", parentContext.spanId)
-        w.writeEndObject()
-        w.writeEndArray()
-      }
-      w.writeEndObject()
-    }
-    return CompletableResultCode.ofSuccess()
-  }
-
-  override fun flush(): CompletableResultCode {
-    writer.get()!!.flush()
-    return CompletableResultCode.ofSuccess()
-  }
-
-  override fun shutdown(): CompletableResultCode {
-    val w = writer.getAndSet(null)
-    if (w != null) {
-      finishWriter(w)
-    }
-    return CompletableResultCode.ofSuccess()
-  }
-
   companion object {
+    private val writer = AtomicReference<JsonGenerator?>()
+
+    @Volatile
+    private var file: Path? = null
+    private val shutdownHookAdded = AtomicBoolean()
+
+    @JvmStatic
     fun setOutput(file: Path) {
       var w = writer.getAndSet(null)
       if (w != null) {
@@ -139,18 +68,18 @@ class JaegerJsonSpanExporter : SpanExporter {
     }
 
     private fun writeAttributesAsJson(w: JsonGenerator, attributes: Attributes) {
-      attributes.forEach(object : Closure<Void?>(null, null) {
-        fun doCall(k: Any, v: Any) {
+      attributes.forEach { k, v ->
           if ((k as AttributeKey<*>).key == "_CES_") {
-            return
+            return@forEach
           }
+
           w.writeStartObject()
           w.writeStringField("key", k.key)
           w.writeStringField("type", k.type.name.lowercase(Locale.getDefault()))
           if (v is Iterable<*>) {
             w.writeArrayFieldStart("value")
-            for (item in v as Iterable<String?>) {
-              w.writeString(item)
+            for (item in v) {
+              w.writeString(item as String)
             }
             w.writeEndArray()
           }
@@ -159,10 +88,10 @@ class JaegerJsonSpanExporter : SpanExporter {
           }
           w.writeEndObject()
         }
-      })
     }
 
-    protected fun finish(tracerProvider: SdkTracerProvider?): Path? {
+    @JvmStatic
+    fun finish(tracerProvider: SdkTracerProvider?): Path? {
       val w = writer.getAndSet(null) ?: return null
       val f = file
       tracerProvider!!.forceFlush().join(10, TimeUnit.SECONDS)
@@ -185,11 +114,82 @@ class JaegerJsonSpanExporter : SpanExporter {
       w.writeEndObject()
       w.close()
     }
+  }
 
-    private val writer = AtomicReference<JsonGenerator?>()
+  override fun export(spans: Collection<SpanData>): CompletableResultCode {
+    val w = writer.get() ?: return CompletableResultCode.ofSuccess()
+    for (span in spans) {
+      w.writeStartObject()
+      w.writeStringField("traceID", span.traceId)
+      w.writeStringField("spanID", span.spanId)
+      w.writeStringField("operationName", span.name)
+      w.writeStringField("processID", "p1")
+      w.writeNumberField("startTime", TimeUnit.NANOSECONDS.toMicros(span.startEpochNanos))
+      w.writeNumberField("duration", TimeUnit.NANOSECONDS.toMicros(span.endEpochNanos - span.startEpochNanos))
+      val parentContext = span.parentSpanContext
+      val hasError = span.status.statusCode == StatusData.error().statusCode
 
-    @Volatile
-    private var file: Path? = null
-    private val shutdownHookAdded = AtomicBoolean()
+      val attributes = span.attributes
+      if (!attributes.isEmpty || hasError) {
+        w.writeArrayFieldStart("tags")
+        if (hasError) {
+          w.writeStartObject()
+          w.writeStringField("key", "otel.status_code")
+          w.writeStringField("type", "string")
+          w.writeStringField("value", "ERROR")
+          w.writeEndObject()
+          w.writeStartObject()
+          w.writeStringField("key", "error")
+          w.writeStringField("type", "bool")
+          w.writeBooleanField("value", true)
+          w.writeEndObject()
+        }
+        writeAttributesAsJson(w, attributes)
+        w.writeEndArray()
+      }
+
+      val events = span.events
+      if (!events.isEmpty()) {
+        w.writeArrayFieldStart("logs")
+        for (event in events) {
+          w.writeStartObject()
+          w.writeNumberField("timestamp", TimeUnit.NANOSECONDS.toMicros(event.epochNanos))
+          w.writeArrayFieldStart("fields")
+
+          // event name as event attribute
+          w.writeStartObject()
+          w.writeStringField("key", "event")
+          w.writeStringField("type", "string")
+          w.writeStringField("value", event.name)
+          w.writeEndObject()
+          writeAttributesAsJson(w, event.attributes)
+          w.writeEndArray()
+          w.writeEndObject()
+        }
+        w.writeEndArray()
+      }
+
+      if (parentContext.isValid) {
+        w.writeArrayFieldStart("references")
+        w.writeStartObject()
+        w.writeStringField("refType", "CHILD_OF")
+        w.writeStringField("traceID", parentContext.traceId)
+        w.writeStringField("spanID", parentContext.spanId)
+        w.writeEndObject()
+        w.writeEndArray()
+      }
+      w.writeEndObject()
+    }
+    return CompletableResultCode.ofSuccess()
+  }
+
+  override fun flush(): CompletableResultCode {
+    writer.get()?.flush()
+    return CompletableResultCode.ofSuccess()
+  }
+
+  override fun shutdown(): CompletableResultCode {
+    writer.getAndSet(null)?.let(::finishWriter)
+    return CompletableResultCode.ofSuccess()
   }
 }
