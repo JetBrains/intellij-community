98ada9bc508e9c1ef11a83e1b450acb4bdb74406
Austin Brooks <abrooksv@gmail.com>
2022-04-27 09:41:37 +0200

    Expose scheduling selection to sub-classes in AutoScrollFromSourceHandler
    e.g. from caret listeners or when the UI element becomes visible
    
    GitOrigin-RevId: 89718cdf15b484cc7d7f6ecc000580bacf0aeb35

--- platform/platform-api/src/com/intellij/ui/AutoScrollFromSourceHandler.java
+++ platform/platform-api/src/com/intellij/ui/AutoScrollFromSourceHandler.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package com.intellij.ui;
 
@@ -72,7 +72,7 @@ public abstract class AutoScrollFromSourceHandler {
     updateCurrentSelection();
   }
 
-  private void selectInAlarm(final FileEditor editor) {
+  protected void selectInAlarm(final FileEditor editor) {
     // Code WithMe: do not process changes from remote (client) editor switching
     if (!ClientId.isCurrentlyUnderLocalId()) return;
 
