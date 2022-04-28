958804e8f3582ade4bdf812e0859d02e7095d4ca
Gregory.Shrago <gregory.shrago@jetbrains.com>
2022-04-28 16:55:39 +0300

    do not repeat validation performed by data manager
    GitOrigin-RevId: 9c403b9483c9be2042b325c829444895a5180a59

--- platform/lang-impl/src/com/intellij/ide/impl/dataRules/PsiElementFromSelectionsRule.java
+++ platform/lang-impl/src/com/intellij/ide/impl/dataRules/PsiElementFromSelectionsRule.java
@@ -16,33 +16,23 @@
 
 package com.intellij.ide.impl.dataRules;
 
-import com.intellij.diagnostic.PluginException;
 import com.intellij.navigation.PsiElementNavigationItem;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.actionSystem.PlatformCoreDataKeys;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiAwareObject;
 import org.jetbrains.annotations.NotNull;
 
 public class PsiElementFromSelectionsRule implements GetDataRule {
-  private static final Logger LOG = Logger.getInstance(PsiElementFromSelectionsRule.class);
 
   @Override
   public Object getData(@NotNull DataProvider dataProvider) {
-    Object items = PlatformCoreDataKeys.SELECTED_ITEMS.getData(dataProvider);
-    if (items == null) return null;
+    Object[] objects = PlatformCoreDataKeys.SELECTED_ITEMS.getData(dataProvider);
+    if (objects == null) return null;
 
-    //noinspection ConstantConditions
-    if (!(items instanceof Object[])) {
-      String errorMessage = "Value of type Object[] is expected, but " + items.getClass() + " is returned by " + dataProvider.getClass();
-      PluginException.logPluginError(LOG, errorMessage, null, dataProvider.getClass());
-      return null;
-    }
     Project project = CommonDataKeys.PROJECT.getData(dataProvider);
-    Object[] objects = (Object[])items;
     PsiElement[] elements = new PsiElement[objects.length];
     for (int i = 0, len = objects.length; i < len; i++) {
       Object o = objects[i];
