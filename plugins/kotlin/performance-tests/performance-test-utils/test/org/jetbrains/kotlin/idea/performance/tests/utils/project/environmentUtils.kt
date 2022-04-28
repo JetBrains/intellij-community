61ccbae76e2e2fb1db068df35f57d13794dbf7bc
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2022-04-28 10:29:08 +0200

    [kotlin][performance tests] Init JDKs based on env variables
    GitOrigin-RevId: e28129d959c535754a342a4b6158956c5e06b203

--- plugins/kotlin/performance-tests/performance-test-utils/test/org/jetbrains/kotlin/idea/performance/tests/utils/project/environmentUtils.kt
+++ plugins/kotlin/performance-tests/performance-test-utils/test/org/jetbrains/kotlin/idea/performance/tests/utils/project/environmentUtils.kt
@@ -5,10 +5,11 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.projectRoots.JavaSdk
 import com.intellij.openapi.projectRoots.Sdk
-import com.intellij.openapi.projectRoots.impl.JavaAwareProjectJdkTableImpl
 import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
 import com.intellij.testFramework.TestApplicationManager
+import org.jetbrains.kotlin.idea.caches.project.getAllProjectSdks
 import org.jetbrains.kotlin.idea.framework.KotlinSdkType
+import org.jetbrains.kotlin.idea.performance.tests.utils.logMessage
 import org.jetbrains.kotlin.idea.test.GradleProcessOutputInterceptor
 import org.jetbrains.kotlin.idea.util.getProjectJdkTableSafe
 
@@ -20,30 +21,29 @@ fun initApp(rootDisposable: Disposable): TestApplicationManager {
 
 fun initSdk(rootDisposable: Disposable): Sdk {
     return runWriteAction {
-        val jdkTableImpl = JavaAwareProjectJdkTableImpl.getInstanceEx()
-        val homePath = if (jdkTableImpl.internalJdk.homeDirectory!!.name == "jre") {
-            jdkTableImpl.internalJdk.homeDirectory!!.parent.path
-        } else {
-            jdkTableImpl.internalJdk.homePath!!
-        }
+        val roots = mutableSetOf<String>()
+        val jdk8Home = System.getenv("JDK_18") ?: System.getenv("JAVA8_HOME")
+        jdk8Home?.let { roots += it }
 
-        val roots = mutableListOf<String>()
-        roots += homePath
-        System.getenv("JDK_18")?.let {
-            roots += it
-        }
+        val jdk11Home = System.getenv("JDK_11") ?: System.getenv("JAVA11_HOME")
+        jdk11Home?.let { roots += it }
+
+        val javaHome = System.getenv("JAVA_HOME") ?: error("env JAVA_HOME is not set")
+        roots += javaHome
         VfsRootAccess.allowRootAccess(rootDisposable, *roots.toTypedArray())
 
         val javaSdk = JavaSdk.getInstance()
-        val jdk = javaSdk.createJdk("1.8", homePath)
-        val internal = javaSdk.createJdk("IDEA jdk", homePath)
-        val gradle = javaSdk.createJdk(GRADLE_JDK_NAME, homePath)
+        val jdk8 = javaSdk.createJdk("1.8", jdk8Home ?: javaHome)
+        val jdk11 = javaSdk.createJdk("11", jdk11Home ?: javaHome )
+        val internal = javaSdk.createJdk("IDEA jdk", jdk11Home ?: jdk8Home ?: javaHome)
+        val gradle = javaSdk.createJdk(GRADLE_JDK_NAME, jdk11Home ?: jdk8Home ?: javaHome)
 
         val jdkTable = getProjectJdkTableSafe()
-        jdkTable.addJdk(jdk, rootDisposable)
-        jdkTable.addJdk(internal, rootDisposable)
-        jdkTable.addJdk(gradle, rootDisposable)
+        arrayOf(jdk8, jdk11, internal, gradle).forEach { jdkTable.addJdk(it, rootDisposable) }
         KotlinSdkType.setUpIfNeeded()
-        jdk
+
+        logMessage { "all sdks = ${getAllProjectSdks().joinToString("\n")}" }
+
+        jdk11
     }
 }
