cbd42f55d80e040865147ddd3466c3c187ff87b2
Dmitry Jemerov <yole@jetbrains.com>
2022-04-28 17:30:30 +0200

    Extract Java and XML specific parts out of CommentByBlockTest
    GitOrigin-RevId: 500c2c8ea836e4ec4131f626310f00f9b0737f2e

---
+++ java/java-tests/testData/codeInsight/commentByBlock/java/after5.java
@@ -0,0 +1,12 @@
+package temp;
+
+public class Test {
+    public String method(
+            String string1,
+            String string2) {
+<selection>        if ("something".equals(string1)) {
+            return string2;
+        }
+</selection>        return null;
+    }
+}
