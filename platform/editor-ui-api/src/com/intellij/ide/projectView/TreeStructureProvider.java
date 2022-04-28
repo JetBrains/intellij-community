e797e7bee6a859fa88635c78ec5e26977d35ab5c
Yann Cébron <yann.cebron@jetbrains.com>
2022-04-28 16:00:25 +0200

    project view customization API: javadoc cleanup
    GitOrigin-RevId: 4055136f25c3ecb3886a47aacb52a93b92f66e6a

--- platform/editor-ui-api/src/com/intellij/ide/projectView/TreeStructureProvider.java
+++ platform/editor-ui-api/src/com/intellij/ide/projectView/TreeStructureProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.projectView;
 
 import com.intellij.ide.util.treeView.AbstractTreeNode;
@@ -9,7 +9,9 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Collection;
 
 /**
- * Allows a plugin to modify the structure of a project as displayed in the project view.
+ * Allows modifying the structure of a project as displayed in the project view.
+ * <p/>
+ * Can implement {@link com.intellij.openapi.project.DumbAware} additionally.
  *
  * @see ProjectViewNodeDecorator
  */
@@ -17,7 +19,7 @@ public interface TreeStructureProvider {
   ProjectExtensionPointName<TreeStructureProvider> EP = new ProjectExtensionPointName<>("com.intellij.treeStructureProvider");
 
   /**
-   * Allows a plugin to modify the list of children displayed for the specified node in the
+   * Allows modifying the list of children displayed for the specified node in the
    * project view.
    *
    * @param parent   the parent node.
@@ -25,17 +27,19 @@ public interface TreeStructureProvider {
    *                 Elements of the collection are of type {@link ProjectViewNode}.
    * @param settings the current project view settings.
    * @return the modified collection of child nodes, or {@code children} if no modifications
-   *         are required.
+   * are required.
    */
   @NotNull
-  Collection<AbstractTreeNode<?>> modify(@NotNull AbstractTreeNode<?> parent, @NotNull Collection<AbstractTreeNode<?>> children, ViewSettings settings);
+  Collection<AbstractTreeNode<?>> modify(@NotNull AbstractTreeNode<?> parent,
+                                         @NotNull Collection<AbstractTreeNode<?>> children,
+                                         ViewSettings settings);
 
   /**
    * Returns a user data object of the specified type for the specified selection in the
    * project view.
    *
    * @param selected the list of nodes currently selected in the project view.
-   * @param dataId the identifier of the requested data object (for example, as defined in
+   * @param dataId   the identifier of the requested data object (for example, as defined in
    *                 {@link com.intellij.openapi.actionSystem.PlatformDataKeys})
    * @return the data object, or null if no data object can be returned by this provider.
    * @see com.intellij.openapi.actionSystem.DataProvider
