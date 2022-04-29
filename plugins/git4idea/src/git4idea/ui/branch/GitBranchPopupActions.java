fedd3b03fef67b1f9e19356f4574389a26d57f95
DevDengChao <2325690622@qq.com>
2022-04-29 11:40:21 +0800

    [git] Add MergeAction into TagActions group
    IDEA-171546
    
    GitOrigin-RevId: b15527157a87ab52450cca858e820bb2bebfadab

--- plugins/git4idea/src/git4idea/ui/branch/GitBranchPopupActions.java
+++ plugins/git4idea/src/git4idea/ui/branch/GitBranchPopupActions.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.ui.branch;
 
 import com.intellij.dvcs.DvcsUtil;
@@ -1173,6 +1173,7 @@ public class GitBranchPopupActions {
     @Override
     public AnAction @NotNull [] getChildren(@Nullable AnActionEvent e) {
       return new AnAction[]{
+        new MergeAction(myProject, myRepositories, myTagName, false),
         new DeleteTagAction(myProject, myRepositories, myTagName)
       };
     }
