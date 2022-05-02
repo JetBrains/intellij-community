eaca9d1aacd163d54568ce7bceb4de9e673a3826
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-05-02 16:54:15 +0200

    vcs: allow to toggle pre-commit checks in dumb mode
    This commit partially reverts aa348ed7aa54e37858f2f183553e2327e0fdf6ea
    
    GitOrigin-RevId: 16aebd412731c74f87b813eb054e923b09db4682

--- platform/vcs-impl/src/com/intellij/openapi/vcs/actions/ShowCommitOptionsAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/actions/ShowCommitOptionsAction.kt
@@ -2,12 +2,12 @@
 package com.intellij.openapi.vcs.actions
 
 import com.intellij.icons.AllIcons
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.vcs.commit.ChangesViewCommitWorkflowHandler
 import com.intellij.vcs.commit.NonModalCommitWorkflowHandler
 
-class ShowCommitOptionsAction : AnAction() {
+class ShowCommitOptionsAction : DumbAwareAction() {
   init {
     templatePresentation.icon = AllIcons.Ide.Notification.Gear
     templatePresentation.hoveredIcon = AllIcons.Ide.Notification.GearHover
