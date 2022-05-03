ddcbdf50fd66877b14e13cb3ddd58e902f50763b
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2022-05-03 20:34:41 +0200

    [jshell] JShellHandler should be created by the corresponding SnippetEditorDecorator.ConfigurationPane #IDEA-293129
    (cherry picked from commit 67fb999af5737dc8b09654b20fa822965a3207e4)
    
    IJ-CR-23927
    
    GitOrigin-RevId: 2317ae4038c8a0512105e09bedab2641c6a105b2

--- java/execution/impl/src/com/intellij/execution/jshell/LaunchJShellConsoleAction.java
+++ java/execution/impl/src/com/intellij/execution/jshell/LaunchJShellConsoleAction.java
@@ -5,13 +5,8 @@ import com.intellij.execution.console.ConsoleHistoryController;
 import com.intellij.ide.scratch.ScratchFileService;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ModalityState;
-import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
@@ -35,25 +30,6 @@ public class LaunchJShellConsoleAction extends AnAction{
       ScratchFileService.Option.create_new_always
     );
     assert contentFile != null;
-    final FileEditor[] editors = FileEditorManager.getInstance(project).openFile(contentFile, true);
-
-    ApplicationManager.getApplication().invokeLater(() -> {
-      try {
-        Sdk alternateSdk = null;
-        Module module = null;
-        for (FileEditor editor : editors) {
-          final SnippetEditorDecorator.ConfigurationPane config = SnippetEditorDecorator.ConfigurationPane.getJShellConfiguration(editor);
-          if (config != null) {
-            alternateSdk = config.getRuntimeSdk();
-            module = config.getContextModule();
-            break;
-          }
-        }
-        JShellHandler.create(project, contentFile, module, alternateSdk);
-      }
-      catch (Exception ex) {
-        JShellDiagnostic.notifyError(ex, project);
-      }
-    }, ModalityState.any(), project.getDisposed());
+    FileEditorManager.getInstance(project).openFile(contentFile, true);
   }
 }
