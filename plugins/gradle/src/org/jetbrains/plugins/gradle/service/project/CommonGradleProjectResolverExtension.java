dff59a2d1944e020dcb4e4d2c176c9b9a3b79721
Nikita Skvortsov <Nikita.Skvortsov@jetbrains.com>
2022-05-04 20:17:12 +0300

    [gradle] choose gradle output dir for existing sources.
    Gradle model can contain multiple output directories (for each language: java, scala, kotlin, etc.). For pure-kotlin projects (with scr/main/kotlin sources directory) that results in non-existing output directories when querying the CompilerModuleExtension and a false positive "classes out-of-date" notification from coverage. IDEA-235250
    
    GitOrigin-RevId: 6788e486475fa983420017d82ec3c6bf3a9a5119

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/project/CommonGradleProjectResolverExtension.java
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/project/CommonGradleProjectResolverExtension.java
@@ -3,7 +3,9 @@ package org.jetbrains.plugins.gradle.service.project;
 
 import com.intellij.build.events.MessageEvent;
 import com.intellij.build.issue.BuildIssue;
-import com.intellij.openapi.application.*;
+import com.intellij.openapi.application.ApplicationNamesInfo;
+import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.externalSystem.debugger.DebuggerBackendExtension;
 import com.intellij.openapi.externalSystem.model.ConfigurationDataImpl;
@@ -31,6 +33,7 @@ import com.intellij.openapi.util.io.FileUtilRt;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.Consumer;
 import com.intellij.util.ReflectionUtil;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.containers.FileCollectionFactory;
 import com.intellij.util.containers.MultiMap;
 import com.intellij.util.execution.ParametersListUtil;
@@ -631,7 +634,21 @@ public final class CommonGradleProjectResolverExtension extends AbstractProjectR
   @Nullable
   private static File getGradleOutputDir(@Nullable ExternalSourceDirectorySet sourceDirectorySet) {
     if (sourceDirectorySet == null) return null;
-    return sourceDirectorySet.getGradleOutputDirs().stream().findFirst().orElse(null);
+    String firstExistingLang = sourceDirectorySet.getSrcDirs().stream()
+      .filter(File::exists)
+      .findFirst()
+      .map(File::getName)
+      .orElse(null);
+
+    if (firstExistingLang == null) {
+     return ContainerUtil.getFirstItem(sourceDirectorySet.getGradleOutputDirs());
+    }
+
+    return sourceDirectorySet.getGradleOutputDirs().stream()
+      .filter(f -> f.getPath().contains(firstExistingLang))
+      .findFirst()
+      .orElse(ContainerUtil.getFirstItem(sourceDirectorySet.getGradleOutputDirs()));
+
   }
 
   private static void excludeOutDir(@NotNull DataNode<ModuleData> ideModule, File ideaOutDir) {
