e7327cb2d3448e24f48c03224c107394e02c18aa
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2022-05-04 15:47:52 +0200

    [ui] making macOS file chooser more liberal about accepting files (IDEA-292534)
    Clients asking for ZIP contents and not satisfied by ZIP roots should either adjust results (e.g. with `getFileToSelect`) or demand a platform chooser (`setForcedToUseIdeaFileChooser`).
    
    GitOrigin-RevId: a485fb965d43103e0f5ef7641058c52473b29485

--- platform/platform-impl/src/com/intellij/ui/mac/MacPathChooserDialog.java
+++ platform/platform-impl/src/com/intellij/ui/mac/MacPathChooserDialog.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ui.mac;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -66,8 +66,7 @@ public class MacPathChooserDialog implements PathChooserDialog, FileChooserDialo
     if (jbrDialog != null) {
       int hints = jbrDialog.getHints();
       if (myFileChooserDescriptor.isChooseFolders()) hints |= JBRFileDialog.SELECT_DIRECTORIES_HINT;
-      if (myFileChooserDescriptor.isChooseFiles() ||
-          (myFileChooserDescriptor.isChooseJars() && myFileChooserDescriptor.isChooseJarsAsFiles())) {
+      if (myFileChooserDescriptor.isChooseFiles() || myFileChooserDescriptor.isChooseJars() || myFileChooserDescriptor.isChooseJarContents()) {
         hints |= JBRFileDialog.SELECT_FILES_HINT;
       }
       jbrDialog.setHints(hints);
