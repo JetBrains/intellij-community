850c46e10153f4dc851f0b1bcbdd7e4cac5759c4
Konstantin Bulenkov <kb@jetbrains.com>
2022-05-05 22:35:47 +0200

    [ExpUI] move all icon mappings into ExpUIIconMapping.json
    GitOrigin-RevId: ece2da7595dd12a242a61999c97443273327d312

---
+++ platform/platform-impl/src/com/intellij/ui/ExperimentalUIImpl.kt
@@ -0,0 +1,33 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ui
+
+import com.fasterxml.jackson.jr.ob.JSON
+import java.io.IOException
+import java.util.*
+
+/**
+ * @author Konstantin Bulenkov
+ */
+class ExperimentalUIImpl : ExperimentalUI() {
+  override fun loadIconMappings(): Map<String, String> {
+    val json = JSON.builder().enable().enable(JSON.Feature.READ_ONLY).build()
+    try {
+      val fin = Objects.requireNonNull(javaClass.getResource("ExpUIIconMapping.json")).openStream()
+      return mutableMapOf<String, String>().apply { readDataFromJson(json.mapFrom(fin), "", this) }
+    }
+    catch (ignore: IOException) {
+    }
+    return emptyMap()
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  private fun readDataFromJson(json: Map<String, Any>, prefix: String, result: MutableMap<String, String>) {
+    json.forEach { (key, value) ->
+      when (value) {
+        is String -> result[value] = prefix + key
+        is Map<*, *> -> readDataFromJson(value as Map<String, Any>, "$prefix$key/", result)
+        is List<*> -> value.forEach { result[it as String] = "$prefix$key" }
+      }
+    }
+  }
+}
