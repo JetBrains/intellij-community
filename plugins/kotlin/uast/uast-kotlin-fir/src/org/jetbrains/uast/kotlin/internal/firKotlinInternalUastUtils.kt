d4dafa0c78205dab0f998385652a6c2f12f10af1
Jinseong Jeon <jsjeon@google.com>
2022-05-05 01:34:56 -0700

    FIR UAST: migrate PSI declaration provider (to Kotlin)
    GitOrigin-RevId: 3de9c086b5f077efcd79e664506f891d8af1f9a8

--- plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/internal/firKotlinInternalUastUtils.kt
+++ plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/internal/firKotlinInternalUastUtils.kt
@@ -15,6 +15,7 @@ import org.jetbrains.kotlin.analysis.api.types.KtType
 import org.jetbrains.kotlin.analysis.api.types.KtTypeMappingMode
 import org.jetbrains.kotlin.analysis.project.structure.KtSourceModule
 import org.jetbrains.kotlin.analysis.project.structure.getKtModule
+import org.jetbrains.kotlin.analysis.providers.DecompiledPsiDeclarationProvider
 import org.jetbrains.kotlin.asJava.getRepresentativeLightMethod
 import org.jetbrains.kotlin.asJava.toLightClass
 import org.jetbrains.kotlin.idea.KotlinLanguage
@@ -154,7 +155,7 @@ internal fun KtAnalysisSession.nullability(ktType: KtType?): TypeNullability? {
 internal fun KtSymbol.psiForUast(project: Project): PsiElement? {
     return when (origin) {
         KtSymbolOrigin.LIBRARY -> {
-            FirPsiDeclarationProvider.findPsi(this, project) ?: psi
+            DecompiledPsiDeclarationProvider.findPsi(this, project) ?: psi
         }
         else -> psi
     }
