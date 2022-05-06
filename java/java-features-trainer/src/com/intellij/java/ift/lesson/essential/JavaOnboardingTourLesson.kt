a9904de63e4d3375b18ca0890a36acc3a079ba38
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2022-05-06 14:50:27 +0300

    [IFT] Check sdk type and give 'getProjectJdk' method more meaningful name
    IJ-CR-24172
    
    GitOrigin-RevId: 5f63cf75d7ad7020b7039a6ee806f868640a7b7d

--- java/java-features-trainer/src/com/intellij/java/ift/lesson/essential/JavaOnboardingTourLesson.kt
+++ java/java-features-trainer/src/com/intellij/java/ift/lesson/essential/JavaOnboardingTourLesson.kt
@@ -245,7 +245,7 @@ class JavaOnboardingTourLesson : KLesson("java.onboarding", JavaLessonsBundle.me
       })
     }
 
-    val currentJdk = JavaProjectUtil.getProjectJdk(project)
+    val currentJdk = JavaProjectUtil.getEffectiveJdk(project)
 
     @Suppress("HardCodedStringLiteral")
     val currentJdkVersion: @NlsSafe String = currentJdk?.let { JavaSdk.getInstance().getVersionString(it) } ?: "none"
