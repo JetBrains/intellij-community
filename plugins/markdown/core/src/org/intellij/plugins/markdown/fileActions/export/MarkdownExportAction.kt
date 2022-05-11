ea2e6afc99df6f30734616e9968acff52b51c035
Ivan Posti <ivan.posti@jetbrains.com>
2022-05-11 18:09:45 +0300

    [Markdown] Make MarkdownExportAction work with any editor with opened md file
    GitOrigin-RevId: 4ea8dcf211b8bfac1dc0bd22a46d2326cd8a11c6

--- plugins/markdown/core/src/org/intellij/plugins/markdown/fileActions/export/MarkdownExportAction.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/fileActions/export/MarkdownExportAction.kt
@@ -3,9 +3,9 @@ package org.intellij.plugins.markdown.fileActions.export
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.vfs.VirtualFile
 import org.intellij.plugins.markdown.lang.MarkdownFileType
-import org.intellij.plugins.markdown.ui.actions.MarkdownActionUtil
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Experimental
@@ -21,7 +21,7 @@ class MarkdownExportAction : AnAction() {
   }
 
   private fun getFileToConvert(event: AnActionEvent): VirtualFile? {
-    val editor = MarkdownActionUtil.findSplitEditor(event)
+    val editor = event.getData(PlatformDataKeys.FILE_EDITOR)
     return editor?.file?.takeIf { it.fileType is MarkdownFileType }
   }
 }
