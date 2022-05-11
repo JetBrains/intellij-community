bc928952682b753f5111f70a45e124010e1c424c
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2022-05-11 14:24:13 +0300

    [editorconfig] IDEA-293287 File is loaded in wrong encoding after restarting IDE
    (cherry picked from commit 60feab22d8366e0ece3279707040287e4c14b71c)
    
    GitOrigin-RevId: b5c03252e0096f1ecd4d0006f734a5faa3a94baa

--- plugins/editorconfig/src/org/editorconfig/configmanagement/EditorConfigEncodingCache.java
+++ plugins/editorconfig/src/org/editorconfig/configmanagement/EditorConfigEncodingCache.java
@@ -138,7 +138,8 @@ public class EditorConfigEncodingCache implements PersistentStateComponent<Eleme
 
   @Nullable
   public Charset getCachedEncoding(@NotNull VirtualFile virtualFile) {
-    return ObjectUtils.doIfNotNull(getCachedCharsetData(virtualFile), CharsetData::getCharset);
+    CharsetData charsetData = getCachedCharsetData(virtualFile);
+    return charsetData != null && !charsetData.isIgnored ? charsetData.getCharset() : null;
   }
 
   @Nullable
