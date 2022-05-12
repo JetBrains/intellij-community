84fe701d6bed01d6d57ccc011e72978b4ffced48
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2022-05-12 16:29:54 +0200

    Save useless rescan for psi modification tracking
    GitOrigin-RevId: e9826f94edee42281dbb202ffb48cb41633f25e7

--- java/java-tests/testSrc/com/intellij/psi/impl/PsiModificationTrackerTest.java
+++ java/java-tests/testSrc/com/intellij/psi/impl/PsiModificationTrackerTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.impl;
 
 import com.intellij.codeInsight.JavaCodeInsightTestCase;
@@ -12,6 +12,7 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.RootsChangeRescanningInfo;
 import com.intellij.openapi.roots.ex.ProjectRootManagerEx;
 import com.intellij.openapi.util.EmptyRunnable;
 import com.intellij.openapi.util.ModificationTracker;
@@ -408,7 +409,8 @@ public class PsiModificationTrackerTest extends JavaCodeInsightTestCase {
     long js = getJavaTracker().getModificationCount();
     long ocb = tracker.getModificationCount();
 
-    WriteAction.run(() -> ProjectRootManagerEx.getInstanceEx(getProject()).makeRootsChange(EmptyRunnable.INSTANCE, false, true));
+    WriteAction.run(() -> ProjectRootManagerEx.getInstanceEx(getProject()).makeRootsChange(EmptyRunnable.INSTANCE,
+                                                                                           RootsChangeRescanningInfo.NO_RESCAN_NEEDED));
 
     assertTrue(mc != tracker.getModificationCount());
     assertTrue(js != getJavaTracker().getModificationCount());
