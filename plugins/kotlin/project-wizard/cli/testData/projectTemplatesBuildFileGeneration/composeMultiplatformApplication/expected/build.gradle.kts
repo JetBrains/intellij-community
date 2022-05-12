ac61e5997fac08ded2993741ba30f07e439059f4
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-05-12 15:14:40 +0200

    [kotlin] Update versions in Kotlin wizard
    Co-author: Margarita Bobova <margarita.bobova@jetbrains.com>
    
    #KTIJ-20919 Fixed
    
    GitOrigin-RevId: a99d5d096eb583342cfd58f2b7bd62dd1ccbc8b7

--- plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/build.gradle.kts
+++ plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("com.android.tools.build:gradle:7.0.4")
     }
 }
 
