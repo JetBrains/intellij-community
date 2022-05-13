consulo/consulo-kotlin

KTIJ-18979 Quickfix for INTEGER_OPERATOR_RESOLVE_WILL_CHANGE
^KTIJ-18979 fixed

GitOrigin-RevId: d7200bfc68da15080a449e631c414686e3e6e34e

idea/src/org/jetbrains/kotlin/idea/inspections/migration/AddConversionCallMigrationInspection.kt
@@ -1,0 +1,28 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.inspections.migration
+
+import com.intellij.codeInspection.CleanupLocalInspectionTool
+import org.jetbrains.kotlin.config.LanguageVersion
+import org.jetbrains.kotlin.diagnostics.DiagnosticFactoryWithPsiElement
+import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.migration.MigrationInfo
+import org.jetbrains.kotlin.idea.migration.isLanguageVersionUpdate
+import org.jetbrains.kotlin.idea.quickfix.migration.MigrationFix
+import org.jetbrains.kotlin.psi.KtExpression
+
+
+class AddConversionCallMigrationInspection :
+    AbstractDiagnosticBasedMigrationInspection<KtExpression>(KtExpression::class.java),
+    MigrationFix,
+    CleanupLocalInspectionTool {
+    override fun isApplicable(migrationInfo: MigrationInfo): Boolean {
+        return migrationInfo.isLanguageVersionUpdate(LanguageVersion.KOTLIN_1_6, LanguageVersion.KOTLIN_1_7)
+    }
+
+    override fun descriptionMessage(): String = KotlinBundle.message("inspection.add.conversion.call.display.name")
+
+    override val diagnosticFactory: DiagnosticFactoryWithPsiElement<KtExpression, *>
+        get() = Errors.INTEGER_OPERATOR_RESOLVE_WILL_CHANGE
+}

JetBrains/intellij-community

(cherry picked from commit d7200bfc68da15080a449e631c414686e3e6e34e)
GitOrigin-RevId: 06e3288bc4062b2d17113742956ba4fd45567dfa

plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/migration/AddConversionCallMigrationInspection.kt
@@ -1,0 +1,28 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.inspections.migration
+
+import com.intellij.codeInspection.CleanupLocalInspectionTool
+import org.jetbrains.kotlin.config.LanguageVersion
+import org.jetbrains.kotlin.diagnostics.DiagnosticFactoryWithPsiElement
+import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.configuration.MigrationInfo
+import org.jetbrains.kotlin.idea.configuration.isLanguageVersionUpdate
+import org.jetbrains.kotlin.idea.quickfix.migration.MigrationFix
+import org.jetbrains.kotlin.psi.KtExpression
+
+
+class AddConversionCallMigrationInspection :
+    AbstractDiagnosticBasedMigrationInspection<KtExpression>(KtExpression::class.java),
+    MigrationFix,
+    CleanupLocalInspectionTool {
+    override fun isApplicable(migrationInfo: MigrationInfo): Boolean {
+        return migrationInfo.isLanguageVersionUpdate(LanguageVersion.KOTLIN_1_6, LanguageVersion.KOTLIN_1_7)
+    }
+
+    override fun descriptionMessage(): String = KotlinBundle.message("inspection.add.conversion.call.display.name")
+
+    override val diagnosticFactory: DiagnosticFactoryWithPsiElement<KtExpression, *>
+        get() = Errors.INTEGER_OPERATOR_RESOLVE_WILL_CHANGE
+}