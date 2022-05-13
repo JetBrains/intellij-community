consulo/consulo-kotlin

KTIJ-20628 Quick fixes for overload resolution warnings
^KTIJ-20628 fixed

GitOrigin-RevId: 2939b2b328c2d7e51a5f41d835b9cabc4f57e6ce

idea/src/org/jetbrains/kotlin/idea/inspections/migration/CastDueToProgressionResolutionChangeMigrationInspection.kt
@@ -1,0 +1,29 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.inspections.migration
+
+import com.intellij.codeInspection.CleanupLocalInspectionTool
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.config.LanguageVersion
+import org.jetbrains.kotlin.diagnostics.DiagnosticFactoryWithPsiElement
+import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.migration.MigrationInfo
+import org.jetbrains.kotlin.idea.migration.isLanguageVersionUpdate
+import org.jetbrains.kotlin.idea.quickfix.migration.MigrationFix
+
+
+class CastDueToProgressionResolutionChangeMigrationInspection :
+    AbstractDiagnosticBasedMigrationInspection<PsiElement>(PsiElement::class.java),
+    MigrationFix,
+    CleanupLocalInspectionTool {
+
+    override fun isApplicable(migrationInfo: MigrationInfo): Boolean {
+        return migrationInfo.isLanguageVersionUpdate(LanguageVersion.KOTLIN_1_7, LanguageVersion.KOTLIN_1_7)
+    }
+
+    override fun descriptionMessage(): String = KotlinBundle.message("inspection.progression.resolution.change.migration.display.name")
+
+    override val diagnosticFactory: DiagnosticFactoryWithPsiElement<PsiElement, *>
+        get() = Errors.PROGRESSIONS_CHANGING_RESOLVE.warningFactory
+}

JetBrains/intellij-community

(cherry picked from commit 2939b2b328c2d7e51a5f41d835b9cabc4f57e6ce)
GitOrigin-RevId: d1fd47d17ca9e3a73457de67d2c8d563c6831382

plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/migration/CastDueToProgressionResolutionChangeMigrationInspection.kt
@@ -1,0 +1,29 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.inspections.migration
+
+import com.intellij.codeInspection.CleanupLocalInspectionTool
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.config.LanguageVersion
+import org.jetbrains.kotlin.diagnostics.DiagnosticFactoryWithPsiElement
+import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.configuration.MigrationInfo
+import org.jetbrains.kotlin.idea.configuration.isLanguageVersionUpdate
+import org.jetbrains.kotlin.idea.quickfix.migration.MigrationFix
+
+
+class CastDueToProgressionResolutionChangeMigrationInspection :
+    AbstractDiagnosticBasedMigrationInspection<PsiElement>(PsiElement::class.java),
+    MigrationFix,
+    CleanupLocalInspectionTool {
+
+    override fun isApplicable(migrationInfo: MigrationInfo): Boolean {
+        return migrationInfo.isLanguageVersionUpdate(LanguageVersion.KOTLIN_1_7, LanguageVersion.KOTLIN_1_7)
+    }
+
+    override fun descriptionMessage(): String = KotlinBundle.message("inspection.progression.resolution.change.migration.display.name")
+
+    override val diagnosticFactory: DiagnosticFactoryWithPsiElement<PsiElement, *>
+        get() = Errors.PROGRESSIONS_CHANGING_RESOLVE.warningFactory
+}
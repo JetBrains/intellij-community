059b3c0cf325724319e223b660d0c64333a335a6
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2022-04-11 11:21:53 +0200

    KTIJ-20628 Quick fixes for overload resolution warnings
    ^KTIJ-20628 fixed
    
    (cherry picked from commit 2939b2b328c2d7e51a5f41d835b9cabc4f57e6ce)
    
    GitOrigin-RevId: d1fd47d17ca9e3a73457de67d2c8d563c6831382

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
@@ -743,5 +743,6 @@ class QuickFixRegistrar : QuickFixContributor {
 
         WRONG_NULLABILITY_FOR_JAVA_OVERRIDE.registerFactory(ChangeMemberFunctionSignatureFix)
         CONFUSING_BRANCH_CONDITION.registerFactory(ConfusingExpressionInWhenBranchFix)
+        PROGRESSIONS_CHANGING_RESOLVE.registerFactory(OverloadResolutionChangeFix)
     }
 }
