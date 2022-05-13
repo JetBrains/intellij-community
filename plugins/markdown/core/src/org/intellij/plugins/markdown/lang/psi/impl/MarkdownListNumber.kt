e11d9302fa6bb934eaf85968eb3ef278c8654fbb
Ivan Posti <ivan.posti@jetbrains.com>
2022-05-13 14:37:24 +0300

    [Markdown] Add own base class for leaf psi elements
    GitOrigin-RevId: 4c3601c443159507aa95778e5228f3ffcd8a9e70

--- plugins/markdown/core/src/org/intellij/plugins/markdown/lang/psi/impl/MarkdownListNumber.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/lang/psi/impl/MarkdownListNumber.kt
@@ -1,12 +1,10 @@
 package org.intellij.plugins.markdown.lang.psi.impl
 
-import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.tree.IElementType
-import org.intellij.plugins.markdown.lang.psi.MarkdownPsiElement
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Experimental
-class MarkdownListNumber(type: IElementType, text: CharSequence): LeafPsiElement(type, text), MarkdownPsiElement {
+class MarkdownListNumber(type: IElementType, text: CharSequence): MarkdownLeafPsiElement(type, text) {
   val delimiter: Char
     get() = text.trim().last()
 
