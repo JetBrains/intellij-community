5ad800aabd1412817009853d861e8ea18b27cf2b
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-05-17 19:31:45 +0200

    vcs: javadoc for VcsManagedFilesHolder
    GitOrigin-RevId: f2299f8c60bc7d5f0a3e106b75e220abce87ea7a

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/VcsManagedFilesHolder.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/VcsManagedFilesHolder.java
@@ -24,16 +24,34 @@ import org.jetbrains.annotations.NotNull;
 import java.util.EventListener;
 
 /**
+ * Allows to decouple parts of {@link ChangeListManager} refresh into a separate process.
+ * <p>
+ * When specified, {@link ChangeProvider} is no longer expected to report ignored/unversioned files.
+ * Instead, these should be loaded by this handler autonomously, and updates notified using {@link VcsManagedFilesHolderListener}.
+ * Note, that holder will need to implement its own logic for tracking modifications,
+ * as {@link VcsDirtyScopeManager} serves only {@link ChangeProvider}.
+ * <p>
+ * This allows to increase general responsiveness of {@link ChangeListManager}
+ * if refresh of ignored/untracked files is a dramatically slower operation than refresh of modified files.
+ *
  * @see ChangeListManagerImpl#notifyUnchangedFileStatusChanged
  */
 public interface VcsManagedFilesHolder extends FilePathHolder {
-  ProjectExtensionPointName<Provider> VCS_UNVERSIONED_FILES_HOLDER_EP = new ProjectExtensionPointName<>("com.intellij.vcs.unversionedFilesHolder");
-  ProjectExtensionPointName<Provider> VCS_IGNORED_FILES_HOLDER_EP = new ProjectExtensionPointName<>("com.intellij.vcs.ignoredFilesHolder");
+  ProjectExtensionPointName<Provider> VCS_UNVERSIONED_FILES_HOLDER_EP
+    = new ProjectExtensionPointName<>("com.intellij.vcs.unversionedFilesHolder");
+  ProjectExtensionPointName<Provider> VCS_IGNORED_FILES_HOLDER_EP
+    = new ProjectExtensionPointName<>("com.intellij.vcs.ignoredFilesHolder");
 
   @Topic.ProjectLevel
   Topic<VcsManagedFilesHolderListener> TOPIC = Topic.create("VcsManagedFilesHolder update", VcsManagedFilesHolderListener.class);
 
-  default boolean isInUpdatingMode() {return false;}
+  /**
+   * Whether data is dirty and there's an ongoing refresh.
+   * <p>
+   * Ex: it affects argument passed to {@link ChangeListListener#unchangedFileStatusChanged(boolean)}
+   * and progress indicator in the tree.
+   */
+  default boolean isInUpdatingMode() { return false; }
 
   interface Provider {
     @NotNull
@@ -44,6 +62,9 @@ public interface VcsManagedFilesHolder extends FilePathHolder {
   }
 
   interface VcsManagedFilesHolderListener extends EventListener {
+    /**
+     * Notify that holder data was changed.
+     */
     @CalledInAny
     void updatingModeChanged();
   }
