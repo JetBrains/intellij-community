588cafcf828bf6d766690d2f6341f2e24a233bd5
Eugene Zhuravlev <jeka@jetbrains.com>
2022-05-17 20:51:04 +0200

    show ant-related progress only if there are ant before/after compile tasks configured
    GitOrigin-RevId: 45f8ad56747bf320f747342d7adf2f38d1567e7f

--- plugins/ant/src/com/intellij/lang/ant/config/impl/AntAfterCompileTask.java
+++ plugins/ant/src/com/intellij/lang/ant/config/impl/AntAfterCompileTask.java
@@ -1,26 +1,13 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.lang.ant.config.impl;
 
-import com.intellij.lang.ant.AntBundle;
-import com.intellij.lang.ant.config.AntBuildTarget;
+import com.intellij.lang.ant.config.AntConfigurationBase;
 import com.intellij.openapi.compiler.CompileContext;
-import com.intellij.openapi.compiler.CompileTask;
-import com.intellij.openapi.compiler.CompilerMessageCategory;
 import org.jetbrains.annotations.NotNull;
 
-class AntAfterCompileTask implements CompileTask {
+class AntAfterCompileTask extends AntCompileTask{
   @Override
   public boolean execute(@NotNull CompileContext context) {
-    return AntBeforeCompileTask.initializeAndRun(context, antConfiguration -> {
-      if (context.getMessageCount(CompilerMessageCategory.ERROR) > 0) {
-        final AntBuildTarget target = antConfiguration.getTargetForEvent(ExecuteAfterCompilationEvent.getInstance());
-        if (target != null) {
-          String message = AntBundle.message("message.skip.ant.target.after.compilation.errors", target.getDisplayName());
-          context.addMessage(CompilerMessageCategory.INFORMATION, message, null, -1, -1);
-        }
-        return true;
-      }
-      return antConfiguration.executeTargetAfterCompile(AntBeforeCompileTask.createDataContext(context));
-    });
+    return AntConfigurationBase.getInstance(context.getProject()).executeTargetAfterCompile(context, createDataContext(context));
   }
 }
