25451802eb228ba4d031fd990574a0fd092593ff
Pavel Gromov <pavel.gromov@jetbrains.com>
2022-05-18 21:23:50 +0300

    [recent projects] Fix "Show in Explorer" action (IDEA-294293)
    GitOrigin-RevId: 3c410d3a3f758cc94ec3bf5528f71f6e13b342c8

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/projectActions/RevealProjectDirAction.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/projectActions/RevealProjectDirAction.java
@@ -1,17 +1,14 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.wm.impl.welcomeScreen.projectActions;
 
-import com.intellij.ide.ReopenProjectAction;
 import com.intellij.ide.actions.RevealFileAction;
 import com.intellij.ide.lightEdit.LightEditCompatible;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.PlatformCoreDataKeys;
 import com.intellij.openapi.project.DumbAwareAction;
-import com.intellij.ui.components.JBList;
+import com.intellij.openapi.wm.impl.welcomeScreen.recentProjects.RecentProjectItem;
+import com.intellij.openapi.wm.impl.welcomeScreen.recentProjects.RecentProjectTreeItem;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.awt.*;
 import java.io.File;
 
 /**
@@ -24,27 +21,15 @@ public class RevealProjectDirAction extends DumbAwareAction implements LightEdit
 
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
-    ReopenProjectAction action = getReopenAction(e);
-    assert action != null;
-    String path = action.getProjectPath();
+    RecentProjectItem item = (RecentProjectItem) RecentProjectsWelcomeScreenActionBase.getSelectedItem(e);
+    assert item != null;
+    String path = item.getProjectPath();
     RevealFileAction.selectDirectory(new File(path));
   }
 
   @Override
   public void update(@NotNull AnActionEvent e) {
-    e.getPresentation().setEnabledAndVisible(getReopenAction(e) != null);
-  }
-
-  @Nullable
-  private static ReopenProjectAction getReopenAction(AnActionEvent e) {
-    Component component = e.getData(PlatformCoreDataKeys.CONTEXT_COMPONENT);
-    if (component instanceof JBList) {
-      JBList list = (JBList)component;
-      Object value = list.getSelectedValue();
-      if (value instanceof ReopenProjectAction) {
-        return (ReopenProjectAction)value;
-      }
-    }
-    return null;
+    RecentProjectTreeItem item = RecentProjectsWelcomeScreenActionBase.getSelectedItem(e);
+    e.getPresentation().setEnabledAndVisible(item instanceof RecentProjectItem);
   }
 }
