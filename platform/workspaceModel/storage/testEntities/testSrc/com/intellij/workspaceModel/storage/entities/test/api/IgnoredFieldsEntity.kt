eb660cdfd7cb8cf0e4f11451590d21deda060486
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2022-05-18 23:34:22 +0300

    [workspace model] Generate impl for entity with ignored fields
    GitOrigin-RevId: 206bdd36ba877581c3a02cfcf8d8a8d2aeaf3706

--- platform/workspaceModel/storage/testEntities/testSrc/com/intellij/workspaceModel/storage/entities/test/api/IgnoredFieldsEntity.kt
+++ platform/workspaceModel/storage/testEntities/testSrc/com/intellij/workspaceModel/storage/entities/test/api/IgnoredFieldsEntity.kt
@@ -3,14 +3,19 @@ package com.intellij.workspaceModel.storage.entities.test.api
 
 import com.intellij.workspaceModel.deft.api.annotations.Ignore
 import com.intellij.workspaceModel.storage.WorkspaceEntity
+import com.intellij.workspaceModel.storage.EntitySource
+import com.intellij.workspaceModel.storage.GeneratedCodeApiVersion
+import com.intellij.workspaceModel.storage.ModifiableWorkspaceEntity
+import com.intellij.workspaceModel.storage.MutableEntityStorage
+import org.jetbrains.deft.ObjBuilder
+import org.jetbrains.deft.Type
+
 
 
 interface IgnoredFieldsEntity: WorkspaceEntity {
   val descriptor: AnotherDataClass
   val description: String get() = "Default description"
-  val anotherVersion: Int get() {
-    return 0
-  }
+  val anotherVersion: Int get() = 0
 
   @Ignore val version: Int get() = descriptor.version
   @Ignore val source: Boolean get() = descriptor.source
@@ -24,7 +29,32 @@ interface IgnoredFieldsEntity: WorkspaceEntity {
   fun isReadOnly(): Boolean {
     return !isEditable() && descriptor.url != null
   }
+  //region generated code
+  //@formatter:off
+  @GeneratedCodeApiVersion(0)
+  interface Builder: IgnoredFieldsEntity, ModifiableWorkspaceEntity<IgnoredFieldsEntity>, ObjBuilder<IgnoredFieldsEntity> {
+      override var descriptor: AnotherDataClass
+      override var entitySource: EntitySource
+      override var description: String
+      override var anotherVersion: Int
+  }
+  
+  companion object: Type<IgnoredFieldsEntity, Builder>() {
+      operator fun invoke(descriptor: AnotherDataClass, entitySource: EntitySource, init: (Builder.() -> Unit)? = null): IgnoredFieldsEntity {
+          val builder = builder()
+          builder.descriptor = descriptor
+          builder.entitySource = entitySource
+          init?.invoke(builder)
+          return builder
+      }
+  }
+  //@formatter:on
+  //endregion
+
 }
+//region generated code
+fun MutableEntityStorage.modifyEntity(entity: IgnoredFieldsEntity, modification: IgnoredFieldsEntity.Builder.() -> Unit) = modifyEntity(IgnoredFieldsEntity.Builder::class.java, entity, modification)
+//endregion
 
 
 data class AnotherDataClass(val name: String, val version: Int, val source: Boolean, val displayName: String?, val url: String?, val revision: String?)
