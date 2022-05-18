d449f91ad84aa5b0292b87a84a1a2ccf74433804
Nikolay Kuznetsov <Nikolay.Kuznetsov@jetbrains.com>
2022-05-18 19:12:39 +0200

    [cwm-remotedev] improve javadocs around cwm/rd toolwindow extraction mode, add missing experimental annotations
    GitOrigin-RevId: 4be0c4d8efa38b84a9867b987a235f3f8d7543c3

--- platform/platform-api/src/com/intellij/ui/viewModel/extraction/ToolWindowViewModelExtractor.java
+++ platform/platform-api/src/com/intellij/ui/viewModel/extraction/ToolWindowViewModelExtractor.java
@@ -7,12 +7,31 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * This interface is used in Code With Me and unattended mode (Remote Dev) to determine how a given toolwindow is networked for a given client.<br/>
+ * If multiple extensions are registered, the first applicable one will be used for a given toolwindow/client combination.
+ * Thus, it's not recommended to implement catch-all extractors. <br/>
+ * See {@link ToolWindowExtractorMode} for extra information, available modes, and their meaning
+ *
+ * @see ToolWindowExtractorEP
+ * @see ViewModelToolWindowFactory
+ */
 @ApiStatus.Experimental
 public interface ToolWindowViewModelExtractor {
   ExtensionPointName<ToolWindowViewModelExtractor> EP_NAME = ExtensionPointName.create("com.intellij.toolWindowExtractor");
 
+  /**
+   * Decides whether this extractor is applicable to a given toolwindow for a given client
+   * @param toolWindowId the internal ID of toolwindow to potentially be shared
+   * @param clientId the client for which the toolwindow would be shared
+   * @return true, if this extractor's {@link #getMode} should be applied. false, otherwise (other extensions will be queried)
+   */
   boolean isApplicable(@NotNull String toolWindowId, @Nullable ClientId clientId);
 
+  /**
+   * The extraction mode that this extractor requests for applicable toolwindows/clients.
+   * @see ToolWindowExtractorMode
+   */
   @NotNull
   ToolWindowExtractorMode getMode();
 }
