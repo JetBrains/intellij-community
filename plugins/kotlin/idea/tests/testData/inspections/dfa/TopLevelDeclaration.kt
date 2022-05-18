d8c61b614c92bc73b53885281a095a2e7faff4db
Tagir Valeev <tagir.valeev@jetbrains.com>
2022-05-18 09:10:52 +0200

    [kotlin-dfa] Remove duplicate test (KTIJ-21724)
    GitOrigin-RevId: 6d2e5c6ce9a13b1ac515e4aa325c9ddbeaca9c7d

--- plugins/kotlin/idea/tests/testData/inspections/dfa/TopLevelDeclaration.kt
+++ plugins/kotlin/idea/tests/testData/inspections/dfa/TopLevelDeclaration.kt
@@ -1,12 +1,6 @@
 // WITH_STDLIB
 val percentage: Double = 103.0
 
-val formattedPercentage: Int = when {
-    percentage >= 10 -> 1
-    <warning descr="Condition 'percentage >= 100' is always false">percentage >= 100</warning> -> 2
-    else -> 3
-}
-
 val formattedPercentage1: Int
     get() = when {
         percentage >= 10 -> 1
