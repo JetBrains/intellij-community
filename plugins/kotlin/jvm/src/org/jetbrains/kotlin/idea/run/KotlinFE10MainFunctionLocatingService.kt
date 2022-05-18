5e0bfc24bc99c6b7d86d40144e80a4fa38dbe3b5
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2022-05-18 23:27:08 +0200

    [kotlin] Do not run resolve for any KtNamedFunction to detect is it a main function
    There are already several PSI based fast checks those not use resolve at all
    
    #KTIJ-21668
    
    GitOrigin-RevId: c4f0debfc87aacc66b6b3f88f71c0521c3cd0929

--- plugins/kotlin/jvm/src/org/jetbrains/kotlin/idea/run/KotlinFE10MainFunctionLocatingService.kt
+++ plugins/kotlin/jvm/src/org/jetbrains/kotlin/idea/run/KotlinFE10MainFunctionLocatingService.kt
@@ -2,27 +2,21 @@ package org.jetbrains.kotlin.idea.run
 
 import org.jetbrains.kotlin.idea.MainFunctionDetector
 import org.jetbrains.kotlin.idea.caches.resolve.resolveToDescriptorIfAny
-import org.jetbrains.kotlin.idea.caches.resolve.safeAnalyzeNonSourceRootCode
 import org.jetbrains.kotlin.idea.project.languageVersionSettings
 import org.jetbrains.kotlin.psi.KtDeclaration
 import org.jetbrains.kotlin.psi.KtNamedFunction
-import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 
 internal class KotlinFE10MainFunctionLocatingService : KotlinMainFunctionLocatingService {
-    override fun isMain(function: KtNamedFunction): Boolean {
-        val bindingContext =
-            function.safeAnalyzeNonSourceRootCode(BodyResolveMode.FULL).takeIf { it != BindingContext.EMPTY } ?: return false
-        val mainFunctionDetector = MainFunctionDetector(bindingContext, function.languageVersionSettings)
-        return mainFunctionDetector.isMain(function)
-    }
+    override fun isMain(function: KtNamedFunction): Boolean =
+        mainFunctionDetector(function).isMain(function)
+
+    private fun mainFunctionDetector(function: KtDeclaration) =
+        MainFunctionDetector(function.languageVersionSettings) {
+            it.resolveToDescriptorIfAny()
+        }
 
     override fun hasMain(declarations: List<KtDeclaration>): Boolean {
-        if (declarations.isEmpty()) return false
-
-        val languageVersionSettings = declarations.first().languageVersionSettings
-        val mainFunctionDetector =
-          MainFunctionDetector(languageVersionSettings) { it.resolveToDescriptorIfAny(BodyResolveMode.FULL) }
+        val mainFunctionDetector = mainFunctionDetector(declarations.firstOrNull() ?: return false)
 
         return declarations.any { it is KtNamedFunction && mainFunctionDetector.isMain(it) }
     }
