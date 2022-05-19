e2fec90863dea260dacdcb6c633f24f28168af22
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-05-19 19:38:03 +0200

    convert KotlinMavenArtifactsBuilder to modern language
    GitOrigin-RevId: dadfcd5f33a941c738193b84e88da93249fc4bf7

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/KotlinMavenArtifactsBuilder.kt
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/KotlinMavenArtifactsBuilder.kt
@@ -1,43 +1,31 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.intellij.build.impl;
+@file:Suppress("ReplaceJavaStaticMethodWithKotlinAnalog")
 
-import org.codehaus.groovy.runtime.DefaultGroovyMethods;
-import org.jetbrains.intellij.build.BuildContext;
-import org.jetbrains.jps.model.module.JpsModule;
+package org.jetbrains.intellij.build.impl
 
-import java.util.Set;
+import org.jetbrains.intellij.build.BuildContext
+import org.jetbrains.jps.model.module.JpsModule
+
+private val MODULE_GROUP_NAMES = java.util.Set.of("gradle", "uast")
 
 /**
  * Generates Maven artifacts for Kotlin IDE modules
  */
-public final class KotlinMavenArtifactsBuilder extends MavenArtifactsBuilder {
-  public KotlinMavenArtifactsBuilder(BuildContext buildContext) {
-    super(buildContext);
+class KotlinMavenArtifactsBuilder(buildContext: BuildContext?) : MavenArtifactsBuilder(buildContext) {
+  override fun shouldSkipModule(moduleName: String, moduleIsDependency: Boolean): Boolean {
+    return if (moduleIsDependency) moduleName.startsWith("intellij") else false
   }
 
-  @Override
-  protected boolean shouldSkipModule(String moduleName, boolean moduleIsDependency) {
-    if (moduleIsDependency) {
-      return moduleName.startsWith("intellij");
+  override fun generateMavenCoordinatesForModule(module: JpsModule): MavenCoordinates {
+    val moduleName = module.name
+    val names = moduleName.split("\\.".toRegex()).dropLastWhile(String::isEmpty)
+    if (names.size < 2) {
+      buildContext.messages.error("Cannot generate Maven artifacts: incorrect module name \'$moduleName\'")
     }
 
-    return false;
+    val groupId = "org.jetbrains.kotlin"
+    val firstMeaningful = if (names.size > 2 && MODULE_GROUP_NAMES.contains(names[1])) 2 else 1
+    val artifactId = names.drop(firstMeaningful).joinToString(separator = "-")
+    return MavenCoordinates(groupId, artifactId, buildContext.buildNumber)
   }
-
-  @Override
-  @SuppressWarnings("UnnecessaryQualifiedReference")
-  protected MavenCoordinates generateMavenCoordinatesForModule(JpsModule module) {
-    final String moduleName = module.getName();
-    String[] names = moduleName.split("\\.");
-    if (DefaultGroovyMethods.size(names) < 2) {
-      buildContext.getMessages().error("Cannot generate Maven artifacts: incorrect module name \'" + moduleName + "\'");
-    }
-
-    String groupId = "org.jetbrains.kotlin";
-    Integer firstMeaningful = DefaultGroovyMethods.size(names) > 2 && MODULE_GROUP_NAMES.contains(names[1]) ? 2 : 1;
-    String artifactId = DefaultGroovyMethods.join(DefaultGroovyMethods.drop(names, firstMeaningful), "-");
-    return new MavenCoordinates(groupId, artifactId, buildContext.getBuildNumber());
-  }
-
-  private static final Set<String> MODULE_GROUP_NAMES = Set.of("gradle", "uast");
 }
