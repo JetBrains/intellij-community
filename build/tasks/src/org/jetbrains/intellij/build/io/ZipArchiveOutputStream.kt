961883e3e035ac2f63e921d7a43073ab0f696280
Ilia Kirianovskii <Ilia.Kirianovskii@jetbrains.com>
2022-05-20 12:05:43 +0200

    Fix BufferOverflowException in ZipArchiveOutputStream#writeIndex by Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
    GitOrigin-RevId: 539245cae28023146c538339de2ed6bf6c8bc95a

--- build/tasks/src/org/jetbrains/intellij/build/io/ZipArchiveOutputStream.kt
+++ build/tasks/src/org/jetbrains/intellij/build/io/ZipArchiveOutputStream.kt
@@ -182,16 +182,20 @@ internal class ZipArchiveOutputStream(private val channel: WritableByteChannel,
           it.put(resourcePackages)
         }
       }
+
+      // write fingerprint count
+      buffer.putInt(entries.size)
     }
 
     // write fingerprints
     // entryCount must be not used here - index contains some dirs, but not zip (see addDirsToIndex)
-    writeData { buffer ->
-      buffer.putInt(entries.size)
-      useAsLongBuffer(buffer) { longBuffer ->
-        // bloom filter is not an option - false positive leads to error like "wrong class name" on class define
-        for (entry in entries) {
-          longBuffer.put(entry.keyHash)
+    for (chunk in entries.asSequence().chunked(16 * 1024)) {
+      writeData { buffer ->
+        useAsLongBuffer(buffer) { longBuffer ->
+          // bloom filter is not an option - false positive leads to error like "wrong class name" on class define
+          for (entry in chunk) {
+            longBuffer.put(entry.keyHash)
+          }
         }
       }
     }
