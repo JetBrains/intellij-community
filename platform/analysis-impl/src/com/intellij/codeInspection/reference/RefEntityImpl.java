cb390d528750bf7e6e96f2add699e46be93efb81
Bas Leijdekkers <basleijdekkers@gmail.com>
2022-05-20 13:43:18 +0200

    Global inspections: safe a bit of memory in RefFunctionalExpressionImpl
    no need for extra fields
    
    GitOrigin-RevId: 380cff138838ef5d1305789c102f5bfd10e14c15

--- platform/analysis-impl/src/com/intellij/codeInspection/reference/RefEntityImpl.java
+++ platform/analysis-impl/src/com/intellij/codeInspection/reference/RefEntityImpl.java
@@ -87,7 +87,7 @@ import java.util.List;
  *      <tr valign="top"><td><td><pre>constructor</pre><td>{@link com.jetbrains.php.lang.inspections.reference.elements.PhpRefMethodImpl}
  *      <tr valign="top"><td><td><pre>implicitly read</pre><td>{@link com.intellij.codeInspection.reference.RefFieldImpl}
  *
- *      <tr valign="top"><td>21<td><pre>is body empty</pre><td>{@link com.intellij.codeInspection.reference.RefMethodImpl}
+ *      <tr valign="top"><td>21<td><pre>is body empty</pre><td>{@link RefMethodImpl} & {@link RefFunctionalExpressionImpl}
  *      <tr valign="top"><td><td><pre>is final</pre><td>{@link com.jetbrains.php.lang.inspections.reference.elements.PhpRefClassImpl}
  *      <tr valign="top"><td><td><pre>with static constructor</pre><td>{@link com.jetbrains.php.lang.inspections.reference.elements.PhpRefMethodImpl}
  *      <tr valign="top"><td><td><pre>implicitly written</pre><td>{@link com.intellij.codeInspection.reference.RefFieldImpl}
