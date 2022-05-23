5d1068065b6315845fa4767605b45f047e297abd
Dmitry Krasilschikov <dmitry.krasilschikov@jetbrains.com>
2022-05-23 15:37:14 +0300

    IDEA-293493 rounded selection for new project wizard generators list
    GitOrigin-RevId: b55e690dfb587a857ecbd47cc2b3e220c726e384

--- platform/platform-api/src/com/intellij/ui/popup/list/GroupedItemsListRenderer.java
+++ platform/platform-api/src/com/intellij/ui/popup/list/GroupedItemsListRenderer.java
@@ -16,6 +16,7 @@
 package com.intellij.ui.popup.list;
 
 import com.intellij.openapi.ui.popup.ListItemDescriptor;
+import com.intellij.openapi.ui.popup.util.PopupUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.ErrorLabel;
 import com.intellij.ui.ExperimentalUI;
@@ -91,9 +92,19 @@ public class GroupedItemsListRenderer<E> extends GroupedElementsRenderer.List im
   protected JComponent layoutComponent(JComponent middleItemComponent) {
     myNextStepLabel = new JLabel();
     myNextStepLabel.setOpaque(false);
-    return JBUI.Panels.simplePanel(middleItemComponent)
-      .addToRight(myNextStepLabel)
-      .withBorder(getDefaultItemComponentBorder());
+
+    if (ExperimentalUI.isNewUI()) {
+      SelectablePanel result = SelectablePanel.wrap(middleItemComponent);
+      PopupUtil.configSelectablePanel(result);
+      result.add(myNextStepLabel, BorderLayout.EAST);
+      return result;
+    }
+    else {
+      return JBUI.Panels.simplePanel(middleItemComponent)
+        .addToRight(myNextStepLabel)
+        .withBorder(getDefaultItemComponentBorder());
+
+    }
   }
 
   protected void customizeComponent(JList<? extends E> list, E value, boolean isSelected) {
