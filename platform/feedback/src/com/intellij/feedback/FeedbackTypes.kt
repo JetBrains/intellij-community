c43b1908519510ad88bc73b182466fc146155e5d
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2022-05-24 18:19:44 +0300

    [Feedback] Move module content back to platform from IDEA
    This fix will resolve IFT-378
    Logic of this module (feedback dialogs) is required only in IDEA Community and Ultimate, so added the code to prohibit showing of project creation and Kotlin rejecters dialogs in other IDEs.
    
    (cherry picked from commit 7b2e1ecf6ce0c7519bcaa2980be03d2b2145fbfc)
    
    GitOrigin-RevId: 6ca5818cefcacd5f8bc3b27499d5c02afaa63830

--- platform/feedback/src/com/intellij/feedback/FeedbackTypes.kt
+++ platform/feedback/src/com/intellij/feedback/FeedbackTypes.kt
@@ -9,6 +9,7 @@ import com.intellij.feedback.state.projectCreation.ProjectCreationInfoService
 import com.intellij.feedback.state.projectCreation.ProjectCreationInfoState
 import com.intellij.notification.Notification
 import com.intellij.notification.NotificationAction
+import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.openapi.application.ex.ApplicationInfoEx
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
@@ -18,16 +19,23 @@ enum class FeedbackTypes {
     private val unknownProjectTypeName = "UNKNOWN"
     private val testProjectTypeName = "TEST"
     private val maxNumberNotificationShowed = 3
+    private val suitableIDEs = listOf("IntelliJ IDEA Community Edition", "IntelliJ IDEA Ultimate Edition")
     override val suitableIdeVersion: String = "2022.1"
 
     override fun isSuitable(): Boolean {
       val projectCreationInfoState = ProjectCreationInfoService.getInstance().state
 
       return isIntellijIdeaEAP() &&
+             checkIdeIsSuitable() &&
              checkIdeVersionIsSuitable() &&
              checkProjectCreationFeedbackNotSent(projectCreationInfoState) &&
              checkProjectCreated(projectCreationInfoState) &&
              checkNotificationNumberNotExceeded(projectCreationInfoState)
+    }
+
+    private fun checkIdeIsSuitable(): Boolean {
+      val ideName = ApplicationNamesInfo.getInstance().fullProductNameWithEdition
+      return ideName in suitableIDEs
     }
 
     private fun checkProjectCreationFeedbackNotSent(state: ProjectCreationInfoState): Boolean {
