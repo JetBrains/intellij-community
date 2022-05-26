cd453d50f3bb51fbab236581efc2e60876f54ee3
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2022-05-27 00:23:14 +0300

    Ensure wslproxy and other tools are executable
    GitOrigin-RevId: 5ce7b5077719ba8702ce3c5b26396a47479c81b8

--- platform/wsl-impl/src/com/intellij/execution/wsl/WslTools.kt
+++ platform/wsl-impl/src/com/intellij/execution/wsl/WslTools.kt
@@ -7,5 +7,14 @@ import java.nio.file.Path
 internal fun AbstractWslDistribution.getWslPath(path: Path): String = getWslPath(path.toString())
                                                                       ?: throw Exception("Can't access from Linux: $path")
 
-internal fun AbstractWslDistribution.getTool(toolName: String, vararg arguments: String): GeneralCommandLine =
-  createWslCommandLine(getWslPath(PathManager.findBinFileWithException(toolName)), *arguments)
+internal fun AbstractWslDistribution.getTool(toolName: String, vararg arguments: String): GeneralCommandLine {
+  val toolOnWindows = PathManager.findBinFileWithException(toolName)
+  val toolOnLinux = getWslPath(toolOnWindows)
+  try {
+    runCommand("test", "-x", toolOnLinux)
+  }
+  catch (e: ProcessExistedNotZeroException) {
+    throw Exception("File is not executable: $toolOnLinux ($toolOnWindows)", e)
+  }
+  return createWslCommandLine(toolOnLinux, *arguments)
+}
