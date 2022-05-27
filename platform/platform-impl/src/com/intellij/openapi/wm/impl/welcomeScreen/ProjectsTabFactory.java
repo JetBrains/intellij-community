b74b9e49b7923242d90b6ad566312a5646b967a5
Pavel Gromov <pavel.gromov@jetbrains.com>
2022-05-27 18:20:28 +0300

    [welcome screen] Fix shows the empty state of the recent projects panel (IDEA-294853)
    (cherry picked from commit f6794edb06b874a84a332735c5730982853a28b4)
    
    IJ-CR-26373
    
    GitOrigin-RevId: def08d607465af20c79df39eee9152b0a4fa6928

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/ProjectsTabFactory.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/ProjectsTabFactory.java
@@ -4,6 +4,7 @@ package com.intellij.openapi.wm.impl.welcomeScreen;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.IdeBundle;
 import com.intellij.ide.RecentProjectListActionProvider;
+import com.intellij.ide.RecentProjectsManager;
 import com.intellij.ide.dnd.DnDEvent;
 import com.intellij.ide.dnd.DnDNativeTarget;
 import com.intellij.ide.dnd.DnDSupport;
@@ -14,17 +15,21 @@ import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.actionSystem.ex.ActionButtonLook;
 import com.intellij.openapi.actionSystem.impl.ActionButton;
 import com.intellij.openapi.actionSystem.impl.ActionToolbarImpl;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.progress.TaskInfo;
 import com.intellij.openapi.util.Couple;
 import com.intellij.openapi.wm.WelcomeScreenTab;
 import com.intellij.openapi.wm.WelcomeTabFactory;
-import com.intellij.openapi.wm.impl.welcomeScreen.recentProjects.ProjectCollectors;
-import com.intellij.openapi.wm.impl.welcomeScreen.recentProjects.RecentProjectFilteringTree;
-import com.intellij.openapi.wm.impl.welcomeScreen.recentProjects.RecentProjectPanelComponentFactory;
+import com.intellij.openapi.wm.ex.ProgressIndicatorEx;
+import com.intellij.openapi.wm.impl.welcomeScreen.cloneableProjects.CloneableProjectsService;
+import com.intellij.openapi.wm.impl.welcomeScreen.recentProjects.*;
 import com.intellij.ui.ScrollPaneFactory;
 import com.intellij.ui.SearchTextField;
 import com.intellij.ui.border.CustomLineBorder;
+import com.intellij.ui.components.panels.Wrapper;
 import com.intellij.util.PlatformUtils;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.messages.MessageBusConnection;
 import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
 
@@ -44,102 +49,7 @@ final class ProjectsTabFactory implements WelcomeTabFactory {
 
   @Override
   public @NotNull WelcomeScreenTab createWelcomeTab(@NotNull Disposable parentDisposable) {
-    return new TabbedWelcomeScreen.DefaultWelcomeScreenTab(IdeBundle.message("welcome.screen.projects.title"),
-                                                           WelcomeScreenEventCollector.TabType.TabNavProject) {
-
-      @Override
-      protected JComponent buildComponent() {
-        JPanel mainPanel;
-        if (RecentProjectListActionProvider.getInstance().getActions(false, true).isEmpty()) {
-          mainPanel = new EmptyStateProjectsPanel(parentDisposable);
-          initDnD(mainPanel);
-        }
-        else {
-          mainPanel = JBUI.Panels.simplePanel().withBorder(JBUI.Borders.empty(13, 12)).withBackground(getProjectsBackground());
-          RecentProjectFilteringTree recentProjectTree = RecentProjectPanelComponentFactory.createComponent(
-            parentDisposable, ProjectCollectors.all
-          );
-          JComponent treeComponent = recentProjectTree.getComponent();
-          JScrollPane scrollPane = ScrollPaneFactory.createScrollPane(treeComponent, true);
-          scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-          scrollPane.setOpaque(false);
-          SearchTextField projectSearch = recentProjectTree.installSearchField();
-
-          JPanel northPanel =
-            JBUI.Panels.simplePanel().andTransparent().withBorder(new CustomLineBorder(WelcomeScreenUIManager.getSeparatorColor(), JBUI.insetsBottom(1)) {
-              @Override
-              public Insets getBorderInsets(Component c) {
-                return JBUI.insetsBottom(12);
-              }
-            });
-
-          ActionToolbar actionsToolbar = createActionsToolbar();
-          actionsToolbar.setTargetComponent(scrollPane);
-          JComponent projectActionsPanel = actionsToolbar.getComponent();
-          northPanel.add(projectSearch, BorderLayout.CENTER);
-          northPanel.add(projectActionsPanel, BorderLayout.EAST);
-          mainPanel.add(northPanel, BorderLayout.NORTH);
-          mainPanel.add(scrollPane, BorderLayout.CENTER);
-          mainPanel.add(createNotificationPanel(parentDisposable), BorderLayout.SOUTH);
-
-          initDnD(treeComponent);
-        }
-
-        return mainPanel;
-      }
-
-      private void initDnD(JComponent component) {
-        DnDNativeTarget target = createDropFileTarget();
-        DnDSupport.createBuilder(component)
-          .enableAsNativeTarget()
-          .setTargetChecker(target)
-          .setDropHandler(target)
-          .setDisposableParent(parentDisposable)
-          .install();
-      }
-
-      @NotNull
-      private ActionToolbar createActionsToolbar() {
-        Couple<DefaultActionGroup> mainAndMore =
-          splitAndWrapActions((ActionGroup)ActionManager.getInstance().getAction(IdeActions.GROUP_WELCOME_SCREEN_QUICKSTART_PROJECTS_STATE),
-                              action -> ActionGroupPanelWrapper.wrapGroups(action, parentDisposable),
-                              PRIMARY_BUTTONS_NUM);
-        DefaultActionGroup toolbarActionGroup = new DefaultActionGroup(
-          ContainerUtil.map2List(mainAndMore.getFirst().getChildren(null), action -> createButtonWrapper(action)));
-        ActionGroup moreActionGroup = mainAndMore.getSecond();
-
-        Presentation moreActionPresentation = moreActionGroup.getTemplatePresentation();
-        moreActionPresentation.setIcon(AllIcons.Actions.More);
-        moreActionPresentation.putClientProperty(HIDE_DROPDOWN_ICON, true);
-
-        toolbarActionGroup.addAction(moreActionGroup);
-        ActionToolbarImpl toolbar = new ActionToolbarImpl(ActionPlaces.WELCOME_SCREEN, toolbarActionGroup, true) {
-          @Override
-          protected @NotNull ActionButton createToolbarButton(@NotNull AnAction action,
-                                                              ActionButtonLook look,
-                                                              @NotNull String place,
-                                                              @NotNull Presentation presentation,
-                                                              @NotNull Dimension minimumSize) {
-            ActionButton toolbarButton = super.createToolbarButton(action, look, place, presentation, minimumSize);
-            toolbarButton.setFocusable(true);
-            return toolbarButton;
-          }
-        };
-        toolbar.setOpaque(false);
-        toolbar.setReservePlaceAutoPopupIcon(false);
-        return toolbar;
-      }
-
-      @NotNull
-      private ToolbarTextButtonWrapper createButtonWrapper(@NotNull AnAction action) {
-        if (action instanceof ActionGroup) {
-          List<AnAction> actions =
-            ContainerUtil.map(((ActionGroup)action).getChildren(null), a -> ActionGroupPanelWrapper.wrapGroups(a, parentDisposable));
-          return ToolbarTextButtonWrapper.wrapAsOptionButton(actions);
-        }
-        return ToolbarTextButtonWrapper.wrapAsTextButton(action);
-      }
-    };
+    return new ProjectsTab(parentDisposable);
   }
 
   @Override
@@ -147,24 +57,192 @@ final class ProjectsTabFactory implements WelcomeTabFactory {
     return !PlatformUtils.isDataSpell();
   }
 
-  private static @NotNull DnDNativeTarget createDropFileTarget() {
-    return new DnDNativeTarget() {
-      @Override
-      public boolean update(@NotNull DnDEvent event) {
-        if (!FileCopyPasteUtil.isFileListFlavorAvailable(event)) {
-          return false;
+  private static class ProjectsTab extends TabbedWelcomeScreen.DefaultWelcomeScreenTab {
+    private final @NotNull Wrapper wrapper = new Wrapper();
+    private final @NotNull Disposable parentDisposable;
+    private final @NotNull JComponent recentProjectsPanel;
+    private final @NotNull JComponent emptyStatePanel;
+    private @NotNull PanelState panelState;
+
+    private ProjectsTab(@NotNull Disposable parentDisposable) {
+      super(IdeBundle.message("welcome.screen.projects.title"), WelcomeScreenEventCollector.TabType.TabNavProject);
+      this.parentDisposable = parentDisposable;
+      this.panelState = getCurrentState();
+      this.recentProjectsPanel = createRecentProjectsPanel();
+      this.emptyStatePanel = createEmptyStatePanel();
+
+      updateState(panelState);
+
+      MessageBusConnection connect = ApplicationManager.getApplication().getMessageBus().connect(parentDisposable);
+      connect.subscribe(CloneableProjectsService.TOPIC, new CloneableProjectsService.CloneProjectListener() {
+        @Override
+        public void onCloneAdded(@NotNull ProgressIndicatorEx progressIndicator, @NotNull TaskInfo taskInfo) {
+          checkState();
         }
-        event.setDropPossible(true);
-        return false;
+
+        @Override
+        public void onCloneRemoved() {
+          checkState();
+        }
+      });
+      connect.subscribe(RecentProjectsManager.RECENT_PROJECTS_CHANGE_TOPIC, new RecentProjectsManager.RecentProjectsChange() {
+        @Override
+        public void change() {
+          checkState();
+        }
+      });
+    }
+
+    @Override
+    protected @NotNull JComponent buildComponent() {
+      return wrapper;
+    }
+
+    private void checkState() {
+      PanelState currentState = getCurrentState();
+      if (currentState == panelState) {
+        return;
       }
 
-      @Override
-      public void drop(@NotNull DnDEvent event) {
-        List<File> files = FileCopyPasteUtil.getFileListFromAttachedObject(event.getAttachedObject());
-        if (!files.isEmpty()) {
-          ProjectUtil.tryOpenFiles(null, ContainerUtil.map((List<? extends File>)files, file -> file.toPath()), "WelcomeFrame");
-        }
+      updateState(currentState);
+    }
+
+    private void updateState(@NotNull PanelState currentPanelState) {
+      if (currentPanelState == PanelState.EMPTY) {
+        wrapper.setContent(emptyStatePanel);
       }
-    };
+      else {
+        wrapper.setContent(recentProjectsPanel);
+      }
+
+      panelState = currentPanelState;
+      wrapper.repaint();
+    }
+
+    private static @NotNull PanelState getCurrentState() {
+      List<RecentProjectTreeItem> recentProjects = RecentProjectListActionProvider.getInstance().collectProjects();
+      List<CloneableProjectItem> cloneableProjects = CloneableProjectsService.getInstance().collectCloneableProjects();
+
+      return !recentProjects.isEmpty() || !cloneableProjects.isEmpty() ? PanelState.NOT_EMPTY : PanelState.EMPTY;
+    }
+
+    private @NotNull JComponent createRecentProjectsPanel() {
+      JPanel recentProjectsPanel = JBUI.Panels.simplePanel()
+        .withBorder(JBUI.Borders.empty(13, 12))
+        .withBackground(getProjectsBackground());
+      RecentProjectFilteringTree recentProjectTree = RecentProjectPanelComponentFactory.createComponent(
+        parentDisposable, ProjectCollectors.all
+      );
+      JComponent treeComponent = recentProjectTree.getComponent();
+      JScrollPane scrollPane = ScrollPaneFactory.createScrollPane(treeComponent, true);
+      scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+      scrollPane.setOpaque(false);
+      SearchTextField projectSearch = recentProjectTree.installSearchField();
+
+      JPanel northPanel = JBUI.Panels.simplePanel()
+        .andTransparent()
+        .withBorder(new CustomLineBorder(WelcomeScreenUIManager.getSeparatorColor(), JBUI.insetsBottom(1)) {
+          @Override
+          public Insets getBorderInsets(Component c) {
+            return JBUI.insetsBottom(12);
+          }
+        });
+
+      ActionToolbar actionsToolbar = createActionsToolbar();
+      actionsToolbar.setTargetComponent(scrollPane);
+      JComponent projectActionsPanel = actionsToolbar.getComponent();
+      northPanel.add(projectSearch, BorderLayout.CENTER);
+      northPanel.add(projectActionsPanel, BorderLayout.EAST);
+      recentProjectsPanel.add(northPanel, BorderLayout.NORTH);
+      recentProjectsPanel.add(scrollPane, BorderLayout.CENTER);
+      recentProjectsPanel.add(createNotificationPanel(parentDisposable), BorderLayout.SOUTH);
+
+      initDnD(treeComponent);
+
+      return recentProjectsPanel;
+    }
+
+    private @NotNull JComponent createEmptyStatePanel() {
+      EmptyStateProjectsPanel emptyStateProjectsPanel = new EmptyStateProjectsPanel(parentDisposable);
+      initDnD(emptyStateProjectsPanel);
+
+      return emptyStateProjectsPanel;
+    }
+
+    private void initDnD(@NotNull JComponent component) {
+      DnDNativeTarget target = createDropFileTarget();
+      DnDSupport.createBuilder(component)
+        .enableAsNativeTarget()
+        .setTargetChecker(target)
+        .setDropHandler(target)
+        .setDisposableParent(parentDisposable)
+        .install();
+    }
+
+    private @NotNull ActionToolbar createActionsToolbar() {
+      Couple<DefaultActionGroup> mainAndMore =
+        splitAndWrapActions((ActionGroup)ActionManager.getInstance().getAction(IdeActions.GROUP_WELCOME_SCREEN_QUICKSTART_PROJECTS_STATE),
+                            action -> ActionGroupPanelWrapper.wrapGroups(action, parentDisposable),
+                            PRIMARY_BUTTONS_NUM);
+      DefaultActionGroup toolbarActionGroup = new DefaultActionGroup(
+        ContainerUtil.map2List(mainAndMore.getFirst().getChildren(null), action -> createButtonWrapper(action)));
+      ActionGroup moreActionGroup = mainAndMore.getSecond();
+
+      Presentation moreActionPresentation = moreActionGroup.getTemplatePresentation();
+      moreActionPresentation.setIcon(AllIcons.Actions.More);
+      moreActionPresentation.putClientProperty(HIDE_DROPDOWN_ICON, true);
+
+      toolbarActionGroup.addAction(moreActionGroup);
+      ActionToolbarImpl toolbar = new ActionToolbarImpl(ActionPlaces.WELCOME_SCREEN, toolbarActionGroup, true) {
+        @Override
+        protected @NotNull ActionButton createToolbarButton(@NotNull AnAction action,
+                                                            ActionButtonLook look,
+                                                            @NotNull String place,
+                                                            @NotNull Presentation presentation,
+                                                            @NotNull Dimension minimumSize) {
+          ActionButton toolbarButton = super.createToolbarButton(action, look, place, presentation, minimumSize);
+          toolbarButton.setFocusable(true);
+          return toolbarButton;
+        }
+      };
+      toolbar.setOpaque(false);
+      toolbar.setReservePlaceAutoPopupIcon(false);
+      return toolbar;
+    }
+
+    private @NotNull ToolbarTextButtonWrapper createButtonWrapper(@NotNull AnAction action) {
+      if (action instanceof ActionGroup) {
+        List<AnAction> actions =
+          ContainerUtil.map(((ActionGroup)action).getChildren(null), a -> ActionGroupPanelWrapper.wrapGroups(a, parentDisposable));
+        return ToolbarTextButtonWrapper.wrapAsOptionButton(actions);
+      }
+      return ToolbarTextButtonWrapper.wrapAsTextButton(action);
+    }
+
+    private static @NotNull DnDNativeTarget createDropFileTarget() {
+      return new DnDNativeTarget() {
+        @Override
+        public boolean update(@NotNull DnDEvent event) {
+          if (!FileCopyPasteUtil.isFileListFlavorAvailable(event)) {
+            return false;
+          }
+          event.setDropPossible(true);
+          return false;
+        }
+
+        @Override
+        public void drop(@NotNull DnDEvent event) {
+          List<File> files = FileCopyPasteUtil.getFileListFromAttachedObject(event.getAttachedObject());
+          if (!files.isEmpty()) {
+            ProjectUtil.tryOpenFiles(null, ContainerUtil.map((List<? extends File>)files, file -> file.toPath()), "WelcomeFrame");
+          }
+        }
+      };
+    }
+
+    private enum PanelState {
+      EMPTY,
+      NOT_EMPTY
+    }
   }
 }
