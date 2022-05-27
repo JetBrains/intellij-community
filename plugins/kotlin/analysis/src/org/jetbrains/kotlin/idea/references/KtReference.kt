59fc11fdf5bc57c0129f32260456159f29388db1
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-05-27 11:04:46 +0200

    [kotlin] properly resolve KtDefaultAnnotationArgumentReference
    (cherry picked from commit 1072968ab9580efb85e9f069238f8ab3b00a1519)
    
    GitOrigin-RevId: 044e804f92452699f9ee137bf06cd6ccfbcc2ef0

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/references/KtReference.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/references/KtReference.kt
@@ -2,15 +2,26 @@
 
 package org.jetbrains.kotlin.idea.references
 
+import com.intellij.psi.PsiMember
 import org.jetbrains.kotlin.analysis.api.descriptors.references.base.KtFe10Reference
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.idea.caches.resolve.util.getJavaOrKotlinMemberDescriptor
+import org.jetbrains.kotlin.psi.KtDeclaration
 import org.jetbrains.kotlin.resolve.BindingContext
 
 fun KtReference.resolveToDescriptors(bindingContext: BindingContext): Collection<DeclarationDescriptor> {
-    if (this !is KtFe10Reference) {
-        error("Reference $this should be KtFe10Reference but was ${this::class}")
+    return when (this) {
+        is KtFe10Reference -> resolveToDescriptors(bindingContext)
+        is KtDefaultAnnotationArgumentReference -> {
+            when (val declaration = resolve()) {
+                is KtDeclaration -> listOfNotNull(bindingContext[BindingContext.DECLARATION_TO_DESCRIPTOR, declaration])
+                is PsiMember -> listOfNotNull(declaration.getJavaOrKotlinMemberDescriptor())
+                else -> emptyList()
+            }
+        }
+        else -> {
+            error("Reference $this should be KtFe10Reference but was ${this::class}")
+        }
     }
-
-    return resolveToDescriptors(bindingContext)
 }
 
