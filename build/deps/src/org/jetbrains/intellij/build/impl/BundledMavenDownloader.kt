a7a8632873a9904db3608280e999cfbc11d4f1a3
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-05-30 10:48:36 +0200

    convert BundledMavenDownloader to modern language
    GitOrigin-RevId: 674723df9eb98f0f00d51f112fe6d05e7168079e

--- build/deps/src/org/jetbrains/intellij/build/impl/BundledMavenDownloader.kt
+++ build/deps/src/org/jetbrains/intellij/build/impl/BundledMavenDownloader.kt
@@ -1,8 +1,9 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.intellij.build.impl
 
 import groovy.transform.CompileStatic
 import org.jetbrains.intellij.build.dependencies.*
+import java.net.URI
 
 import java.nio.file.Files
 import java.nio.file.Path
