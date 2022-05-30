ff9d937411120cc969e2b3071b7c4ca25cbe0010
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2022-05-30 18:50:34 +0300

    [IFT] Add java doc and api status to TipAndTrickPromotionFactory
    IJ-CR-25004
    
    GitOrigin-RevId: a4c16e0d87fc99943831bc49e566451c6a32d9f2

--- platform/platform-impl/src/com/intellij/ide/util/TipAndTrickPromotionFactory.kt
+++ platform/platform-impl/src/com/intellij/ide/util/TipAndTrickPromotionFactory.kt
@@ -3,8 +3,14 @@ package com.intellij.ide.util
 
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
+import org.jetbrains.annotations.ApiStatus
 import javax.swing.JPanel
 
+/**
+ * Adds an ability to place custom UI component to the top of Tip of the day dialog for any particular tip.
+ * @see [TipPanel.setPromotionForCurrentTip]
+ */
+@ApiStatus.Internal
 interface TipAndTrickPromotionFactory {
   fun createPromotionPanel(project: Project, tip: TipAndTrickBean): JPanel?
 
