a85e054926c7da443932a628b9666000934d2a56
Dmitry Jemerov <yole@jetbrains.com>
2022-05-30 15:19:47 +0200

    Show experimental UI configurable only in internal mode
    GitOrigin-RevId: ac316359244880fc97b445a3fa750f91e7cbb2a4

--- platform/lang-impl/src/com/intellij/ui/ExperimentalUIConfigurableProvider.kt
+++ platform/lang-impl/src/com/intellij/ui/ExperimentalUIConfigurableProvider.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ui
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.options.ConfigurableProvider
 
 /**
@@ -8,4 +9,6 @@ import com.intellij.openapi.options.ConfigurableProvider
  */
 class ExperimentalUIConfigurableProvider: ConfigurableProvider() {
   override fun createConfigurable() = ExperimentalUIConfigurable()
+
+  override fun canCreateConfigurable() = ApplicationManager.getApplication().isInternal
 }
