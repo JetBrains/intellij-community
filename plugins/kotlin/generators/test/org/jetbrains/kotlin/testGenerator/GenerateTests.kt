e747241ed5b1df8fefbd68866dbb96539326617b
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-05-31 18:34:01 +0200

    [kotlin] Do not generate tests for the `refactoring/extractFunction/inplace` directory
    Manual tests InplaceExtractFunctionTest is using testdata in this directory
    
    GitOrigin-RevId: fd945314a17116153ad2690ed84c808828da44f5

--- plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
+++ plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
@@ -875,7 +875,7 @@ private fun assembleWorkspace(): TWorkspace = workspace {
 
         testClass<AbstractExtractionTest> {
             model("refactoring/introduceVariable", pattern = KT_OR_KTS, testMethodName = "doIntroduceVariableTest")
-            model("refactoring/extractFunction", pattern = KT_OR_KTS, testMethodName = "doExtractFunctionTest")
+            model("refactoring/extractFunction", pattern = KT_OR_KTS, testMethodName = "doExtractFunctionTest", excludedDirectories = listOf("inplace"))
             model("refactoring/introduceProperty", pattern = KT_OR_KTS, testMethodName = "doIntroducePropertyTest")
             model("refactoring/introduceParameter", pattern = KT_OR_KTS, testMethodName = "doIntroduceSimpleParameterTest")
             model("refactoring/introduceLambdaParameter", pattern = KT_OR_KTS, testMethodName = "doIntroduceLambdaParameterTest")
