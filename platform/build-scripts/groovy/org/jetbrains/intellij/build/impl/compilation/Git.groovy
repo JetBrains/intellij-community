a3c00ecd065b46b647e8db89d0c55c03063f7c72
Leonid Shalupov <leonid@shalupov.com>
2022-06-01 23:48:50 +0200

    build-scripts: prepare sources for CWM-6198
    GitOrigin-RevId: f6609d13a7679abbf705df2d8be303428efc9c91

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/compilation/Git.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/compilation/Git.groovy
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.intellij.build.impl.compilation
 
+import com.intellij.openapi.util.text.StringUtil
 import groovy.transform.CompileStatic
 import groovy.transform.Immutable
 
@@ -18,15 +19,36 @@ final class Git {
   }
 
   List<String> log(int commitsCount) {
-    return execute("git log -$commitsCount --pretty=tformat:%H")
+    return execute("git", "log", "-$commitsCount", "--pretty=tformat:%H")
+  }
+
+  String formatLatestCommit(String format) {
+    List<String> lines = execute("git", "log", "--pretty=format:" + format, "-n", "1")
+    return StringUtil.join(lines, "\n")
   }
 
   List<String> status() {
-    return execute('git status --short --untracked-files=no --ignored=no')
+    return execute('git', "status", "--short", "--untracked-files=no", "--ignored=no")
+  }
+
+  List<String> listFilesUnderVersionControl() {
+    return execute("git", "ls-tree", "-r", "HEAD", "--name-only")
+  }
+
+  String currentCommitShortHash() {
+    List<String> lines = execute("git", "rev-parse", "--short", "HEAD")
+    if (lines.size() != 1) {
+      throw new IllegalStateException("Single line output is expected but got '$lines'")
+    }
+    String hash = lines[0].trim()
+    if (hash.length() != 13) {
+      throw new IllegalStateException("Short hash must be exacly 13 chars, but got '$hash'")
+    }
+    return hash
   }
 
   String lineBreaksConfig() {
-    def lines = maybeExecute("git config core.autocrlf").output.findAll { !it.isBlank() }
+    def lines = maybeExecute("git", "config", "core.autocrlf").output.findAll { !it.isBlank() }
     if (lines.isEmpty()) {
       return ""
     }
@@ -36,8 +58,8 @@ final class Git {
     return lines[0]
   }
 
-  private ExecutionResult maybeExecute(String command) {
-    def process = command.execute((List)null, dir)
+  private ExecutionResult maybeExecute(String... command) {
+    Process process = new ProcessBuilder(command).directory(dir).start()
     List<String> output = new BufferedReader(new InputStreamReader(process.inputStream, StandardCharsets.UTF_8)).withCloseable {
       it.lines().map { it.trim() }.collect(Collectors.toList())
     }
@@ -48,7 +70,7 @@ final class Git {
     return new ExecutionResult(exitCode: process.exitValue(), output: output)
   }
 
-  private List<String> execute(String command) {
+  private List<String> execute(String... command) {
     ExecutionResult result = maybeExecute(command)
     if (result.exitCode != 0) {
       throw new IllegalStateException("git process failed with $result.exitCode:\n${result.output.join('\n')}")
