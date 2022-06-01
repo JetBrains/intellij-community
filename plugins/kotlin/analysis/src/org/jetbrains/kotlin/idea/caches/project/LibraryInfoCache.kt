a45eacf3373b3c1fd0febe490bb8b02e0ed38c82
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2022-06-01 17:34:14 +0200

    [kotlin] Improve cache multithread security
    #KTIJ-21890
    
    GitOrigin-RevId: 0627f5592e3fe74d3cc3cd9f3befe50203af0bc0

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/caches/project/LibraryInfoCache.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/caches/project/LibraryInfoCache.kt
@@ -3,9 +3,12 @@ package org.jetbrains.kotlin.idea.caches.project
 
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.service
+import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.impl.libraries.LibraryEx
 import com.intellij.openapi.roots.libraries.Library
+import com.intellij.openapi.util.LowMemoryWatcher
+import com.intellij.serviceContainer.AlreadyDisposedException
 import com.intellij.workspaceModel.ide.WorkspaceModelChangeListener
 import com.intellij.workspaceModel.ide.impl.legacyBridge.library.findLibraryBridge
 import com.intellij.workspaceModel.storage.EntityChange
@@ -16,25 +19,62 @@ import org.jetbrains.kotlin.idea.framework.effectiveKind
 import org.jetbrains.kotlin.idea.framework.platform
 import org.jetbrains.kotlin.platform.DefaultIdeTargetPlatformKindProvider
 import org.jetbrains.kotlin.platform.idePlatformKind
-import java.util.concurrent.ConcurrentHashMap
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 class LibraryInfoCache(private val project: Project): Disposable {
-    private val libraryInfoCache: MutableMap<Library, List<LibraryInfo>> = ConcurrentHashMap<Library, List<LibraryInfo>>()
+    private val libraryInfoCache: MutableMap<Library, List<LibraryInfo>> = hashMapOf()
 
-    fun createLibraryInfo(library: Library): List<LibraryInfo> =
-        libraryInfoCache.getOrPut(library) {
-            val approximatePlatform = if (library is LibraryEx && !library.isDisposed) {
-                // for Native returns 'unspecifiedNativePlatform', thus "approximate"
-                library.effectiveKind(project).platform
-            } else {
-                DefaultIdeTargetPlatformKindProvider.defaultPlatform
+    init {
+        // drop entire cache when it is low free memory
+        LowMemoryWatcher.register(this::clean, this)
+    }
+
+    fun createLibraryInfo(library: Library): List<LibraryInfo> {
+        library.safeAs<LibraryEx>()?.takeIf { it.isDisposed }?.let {
+            synchronized(libraryInfoCache) {
+                libraryInfoCache.remove(library)
             }
-
-            approximatePlatform.idePlatformKind.resolution.createLibraryInfo(project, library)
+            throw AlreadyDisposedException("${library.name} is already disposed")
         }
 
+        // fast check
+        synchronized(libraryInfoCache) {
+            libraryInfoCache[library]?.let { return it }
+        }
+
+        ProgressManager.checkCanceled()
+
+        val approximatePlatform = if (library is LibraryEx && !library.isDisposed) {
+            // for Native returns 'unspecifiedNativePlatform', thus "approximate"
+            library.effectiveKind(project).platform
+        } else {
+            DefaultIdeTargetPlatformKindProvider.defaultPlatform
+        }
+
+        val libraryInfos = approximatePlatform.idePlatformKind.resolution.createLibraryInfo(project, library)
+
+        ProgressManager.checkCanceled()
+        synchronized(libraryInfoCache) {
+            libraryInfoCache.putIfAbsent(library, libraryInfos)?.let { return it }
+        }
+
+        return libraryInfos
+    }
+
     override fun dispose() {
-        libraryInfoCache.clear()
+        clean()
+    }
+
+    private fun clean() {
+        synchronized(libraryInfoCache) {
+            libraryInfoCache.clear()
+        }
+    }
+
+    private fun cleanupCache(libraries: Collection<Library>) {
+        synchronized(libraryInfoCache) {
+            libraries.forEach(libraryInfoCache::remove)
+        }
     }
 
     internal class ModelChangeListener(private val project: Project) : WorkspaceModelChangeListener {
@@ -42,19 +82,18 @@ class LibraryInfoCache(private val project: Project): Disposable {
             val storageBefore = event.storageBefore
             val changes = event.getChanges(LibraryEntity::class.java).ifEmpty { return }
 
-            val cache = getInstance(project).libraryInfoCache.ifEmpty { return }
-            for (change in changes) {
-                val oldEntity = when (change) {
-                    is EntityChange.Added -> null
-                    is EntityChange.Removed -> change.entity
-                    is EntityChange.Replaced -> change.oldEntity
-                } ?: continue
-                // cache refers to an old library entity
-                oldEntity.findLibraryBridge(storageBefore)?.let {
-                    // drop outdated library from a cache
-                    cache.remove(it)
+            val infoCache = getInstance(project)
+            val outdatedLibraries: List<Library> = changes.asSequence()
+                .mapNotNull {
+                    when (it) {
+                        is EntityChange.Added -> null
+                        is EntityChange.Removed -> it.entity
+                        is EntityChange.Replaced -> it.oldEntity
+                    }
                 }
-            }
+                .mapNotNull { it.findLibraryBridge(storageBefore) }
+                .toList()
+            infoCache.cleanupCache(outdatedLibraries)
         }
     }
 
