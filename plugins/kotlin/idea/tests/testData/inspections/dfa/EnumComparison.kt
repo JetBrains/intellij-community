c5e01a0969bd31f2134da459292243466661209c
Tagir Valeev <tagir.valeev@jetbrains.com>
2022-06-01 09:25:46 +0200

    [kotlin-dfa] Test for enum declared in Java library
    (cherry picked from commit 7c96dbfd260f908b2eace044ff8a7e26e1b441f1)
    
    IJ-CR-25045
    
    GitOrigin-RevId: fdd8a67b3f7a6f76cf226b04979f6e1587a22a82

--- plugins/kotlin/idea/tests/testData/inspections/dfa/EnumComparison.kt
+++ plugins/kotlin/idea/tests/testData/inspections/dfa/EnumComparison.kt
@@ -1,9 +1,18 @@
 // WITH_STDLIB
 import Enum.*
+import java.util.concurrent.TimeUnit
 
 fun libraryEnum(x: AnnotationTarget) {
     if (x == AnnotationTarget.TYPE) {
         if (<warning descr="Condition 'x == AnnotationTarget.CLASS' is always false">x == AnnotationTarget.CLASS</warning>) {
+
+        }
+    }
+}
+
+fun javaEnum(x: TimeUnit) {
+    if (x == TimeUnit.DAYS) {
+        if (<warning descr="Condition 'x == TimeUnit.MINUTES' is always false">x == TimeUnit.MINUTES</warning>) {
 
         }
     }
