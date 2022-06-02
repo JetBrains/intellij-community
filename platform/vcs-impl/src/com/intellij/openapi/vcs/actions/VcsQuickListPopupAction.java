cf6d52f35cbe35b2e3ecc45d2a412157586eba3b
Konstantin Bulenkov <kb@jetbrains.com>
2022-06-02 18:07:32 +0200

    don't redefine default presentation text in constructor
    GitOrigin-RevId: 33e3c1010235b9e04c19e5ad66edfc45a2600b95

--- platform/vcs-impl/src/com/intellij/openapi/vcs/actions/VcsQuickListPopupAction.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/actions/VcsQuickListPopupAction.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vcs.actions;
 
 import com.intellij.ide.actions.QuickSwitchSchemeAction;
@@ -25,7 +25,6 @@ import java.util.List;
 public class VcsQuickListPopupAction extends QuickSwitchSchemeAction implements DumbAware {
   public VcsQuickListPopupAction() {
     myActionPlace = ActionPlaces.ACTION_PLACE_VCS_QUICK_LIST_POPUP_ACTION;
-    getTemplatePresentation().setText(VcsBundle.messagePointer("vcs.quicklist.popup.title"));
   }
 
   @Override
