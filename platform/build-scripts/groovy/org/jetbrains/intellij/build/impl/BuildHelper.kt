5178d996b35cae9ea4cba170106c5f4f6cd9c98a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-06-02 18:16:43 +0200

    JavaModuleOptions cannot be in `com.intellij.util.lang` package because it uses non-JDK classes (it breaks our classloader)
    GitOrigin-RevId: b4954715023ea0e40f6e02b962582628613cf2c5

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/BuildHelper.kt
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/BuildHelper.kt
@@ -6,7 +6,7 @@ package org.jetbrains.intellij.build.impl
 import com.intellij.diagnostic.telemetry.createTask
 import com.intellij.diagnostic.telemetry.useWithScope
 import com.intellij.util.lang.CompoundRuntimeException
-import com.intellij.util.lang.JavaModuleOptions
+import com.intellij.util.JavaModuleOptions
 import com.intellij.util.system.OS
 import com.intellij.util.xml.dom.readXmlAsModel
 import io.opentelemetry.api.trace.Span
--- platform/util/src/com/intellij/util/JavaModuleOptions.java
+++ platform/util/src/com/intellij/util/JavaModuleOptions.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.util.lang;
+package com.intellij.util;
 
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.system.OS;
