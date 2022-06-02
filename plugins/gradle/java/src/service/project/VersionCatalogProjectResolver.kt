78afff630c911a5843057f1a7885f12947785399
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-06-02 12:50:17 +0300

    [gradle] IDEA-279603: Support heuristic-based navigation to setting.gradle-defined dependencies
    (cherry picked from commit 5e4572f6da1c29a91d05b34017eabad93aa15a17)
    (cherry picked from commit c51c9c0737e946cbc7cdc0e4b429c0301b1283c7)
    
    IJ-MR-27989
    
    GitOrigin-RevId: 003e6efc032e1a02e9b7e0bf9fc0e1d3be0dca58

---
+++ plugins/gradle/java/src/service/project/VersionCatalogProjectResolver.kt
@@ -0,0 +1,98 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.gradle.service.project
+
+import com.intellij.ide.impl.ProjectUtil
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.externalSystem.model.DataNode
+import com.intellij.openapi.externalSystem.model.project.ProjectData
+import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiManager
+import com.intellij.psi.util.parentsOfType
+import com.intellij.util.castSafelyTo
+import com.intellij.util.io.exists
+import org.gradle.tooling.model.idea.IdeaProject
+import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames
+import org.jetbrains.plugins.gradle.util.GradleConstants
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFileBase
+import org.jetbrains.plugins.groovy.lang.psi.GroovyRecursiveElementVisitor
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.path.GrMethodCallExpression
+import org.jetbrains.plugins.groovy.lang.psi.util.GroovyConstantExpressionEvaluator
+import java.nio.file.Path
+
+class VersionCatalogProjectResolver : AbstractProjectResolverExtension() {
+
+  override fun populateProjectExtraModels(gradleProject: IdeaProject, ideProject: DataNode<ProjectData>) {
+    val settingsFilePath = getGradleSettingsFile(ideProject)
+    val virtualFile = VfsUtil.findFile(settingsFilePath, false) ?: return super.populateProjectExtraModels(gradleProject, ideProject)
+    val projectRoot = Path.of(ideProject.data.linkedExternalProjectPath)
+    val project = ProjectUtil.findProject(projectRoot) ?: return super.populateProjectExtraModels(gradleProject, ideProject)
+    DumbService.getInstance(project).runWhenSmart {
+      val psiFile = PsiManager.getInstance(project).findFile(virtualFile).castSafelyTo<GroovyFileBase>() ?: return@runWhenSmart
+      val tracker = GradleStructureTracker(projectRoot)
+      runReadAction {
+        psiFile.accept(tracker)
+      }
+      val mapping = tracker.catalogMapping
+      val defaultLibsFile = projectRoot.resolve("gradle/libs.versions.toml")
+      if (defaultLibsFile.exists()) {
+        val libsValue = mapping["libs"]
+        if (libsValue == null) {
+          mapping["libs"] = defaultLibsFile
+        }
+      }
+      psiFile.putUserData(VERSION_CATALOGS, mapping)
+    }
+    super.populateProjectExtraModels(gradleProject, ideProject)
+  }
+
+  private fun getGradleSettingsFile(ideProject: DataNode<ProjectData>) : Path {
+    return Path.of(ideProject.data.linkedExternalProjectPath).resolve(GradleConstants.SETTINGS_FILE_NAME)
+  }
+
+  companion object {
+    val VERSION_CATALOGS = Key.create<Map<String, Path>>("gradle version catalogs")
+  }
+}
+
+/**
+ * This is a temporary and hacky implementation aimed to support navigation to version catalogs.
+ * We need a proper API from Gradle to support this functionality without these unreliable AST-based heuristics.
+ */
+private class GradleStructureTracker(val projectRoot: Path) : GroovyRecursiveElementVisitor() {
+
+  val catalogMapping: MutableMap<String, Path> = mutableMapOf()
+
+  override fun visitMethodCallExpression(methodCallExpression: GrMethodCallExpression) {
+    val method = methodCallExpression.resolveMethod() ?: return super.visitMethodCallExpression(methodCallExpression)
+    if (method.name == "from" && method.containingClass?.qualifiedName == GradleCommonClassNames.GRADLE_API_VERSION_CATALOG_BUILDER) {
+      val container = methodCallExpression.parentsOfType<GrMethodCallExpression>().find {
+        it.resolveMethod()?.returnType?.equalsToText(GradleCommonClassNames.GRADLE_API_VERSION_CATALOG_BUILDER) == true
+      }
+      val name = container?.resolveMethod()?.name
+      if (container == null || name == null) {
+        return super.visitMethodCallExpression(methodCallExpression)
+      }
+      val file = resolveDependencyNotation(methodCallExpression.expressionArguments.singleOrNull(), projectRoot)
+      if (file != null) {
+        catalogMapping[name] = file
+      }
+    }
+    super.visitMethodCallExpression(methodCallExpression)
+  }
+
+}
+
+/**
+ * @return filename of toml catalog
+ */
+private fun resolveDependencyNotation(element: PsiElement?, root: Path): Path? {
+  element ?: return null
+  if (element is GrMethodCallExpression && element.resolveMethod()?.name == "files") {
+    val argument = element.argumentList.expressionArguments.firstOrNull()
+    return GroovyConstantExpressionEvaluator.evaluate(argument)?.castSafelyTo<String>()?.let(root::resolve)?.takeIf(Path::exists)
+  }
+  return null
+}
