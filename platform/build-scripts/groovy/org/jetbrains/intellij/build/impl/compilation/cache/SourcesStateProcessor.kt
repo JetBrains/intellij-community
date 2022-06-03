019a6d393bf706b02e74a8e8d755dffe23df1ae4
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2022-06-03 13:00:04 +0200

    convert build.impl.compilation.cache.SourcesStateProcessor to modern language
    GitOrigin-RevId: 6cf492f18871247e317929dce6665cbb98eeffa6

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/compilation/cache/SourcesStateProcessor.kt
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/compilation/cache/SourcesStateProcessor.kt
@@ -4,101 +4,90 @@ package org.jetbrains.intellij.build.impl.compilation.cache
 import com.google.common.hash.Hashing
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import groovy.transform.CompileStatic
 import org.jetbrains.intellij.build.impl.compilation.BuildTargetState
 import org.jetbrains.intellij.build.impl.compilation.CompilationOutput
-
-import java.lang.reflect.Type
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Path
 
-@CompileStatic
-final class SourcesStateProcessor {
-  private static final Type SOURCES_STATE_TYPE = new TypeToken<Map<String, Map<String, BuildTargetState>>>() {}.getType()
+class SourcesStateProcessor(dataStorageRoot: Path, private val buildOutputRoot: Path) {
+  companion object {
+    private val SOURCES_STATE_TYPE = object : TypeToken<Map<String, Map<String, BuildTargetState>>>() {}.type
 
-  private static final String SOURCES_STATE_FILE_NAME = 'target_sources_state.json'
-  private static final String IDENTIFIER = '$BUILD_DIR$'
+    private const val SOURCES_STATE_FILE_NAME = "target_sources_state.json"
+    private const val IDENTIFIER = "\$BUILD_DIR\$"
 
-  private static final List<String> PRODUCTION_TYPES = ["java-production", "resources-production"]
-  private static final List<String> TEST_TYPES = ["java-test", "resources-test"]
-  private static final String PRODUCTION = "production"
-  private static final String TEST = "test"
-
-  private final Gson gson = new Gson()
-  private final Path dataStorageRoot
-  private final Path buildOutputRoot
-
-  SourcesStateProcessor(Path dataStorageRoot, Path buildOutputRoot) {
-    this.dataStorageRoot = dataStorageRoot
-    this.buildOutputRoot = buildOutputRoot
+    private val PRODUCTION_TYPES = listOf("java-production", "resources-production")
+    private val TEST_TYPES = listOf("java-test", "resources-test")
+    private const val PRODUCTION = "production"
+    private const val TEST = "test"
   }
 
-  List<CompilationOutput> getAllCompilationOutputs(Map<String, Map<String, BuildTargetState>> sourceStateFile) {
+  private val gson: Gson = Gson()
+  val sourceStateFile: Path = dataStorageRoot.resolve(SOURCES_STATE_FILE_NAME)
+
+  internal fun getAllCompilationOutputs(sourceStateFile: Map<String, Map<String, BuildTargetState>>): List<CompilationOutput> {
     return getProductionCompilationOutputs(sourceStateFile) + getTestsCompilationOutputs(sourceStateFile)
   }
 
-  Map<String, Map<String, BuildTargetState>> parseSourcesStateFile(String json = Files.readString(sourceStateFile)) {
+  internal fun parseSourcesStateFile(json: String = Files.readString(sourceStateFile)): Map<String, Map<String, BuildTargetState>> {
     return gson.fromJson(json, SOURCES_STATE_TYPE)
   }
 
-  Path getSourceStateFile() {
-    return dataStorageRoot.resolve(SOURCES_STATE_FILE_NAME)
-  }
-
-  private List<CompilationOutput> getProductionCompilationOutputs(Map<String, Map<String, BuildTargetState>> currentSourcesState) {
+  private fun getProductionCompilationOutputs(currentSourcesState: Map<String, Map<String, BuildTargetState>>): List<CompilationOutput> {
     return getCompilationOutputs(PRODUCTION, PRODUCTION_TYPES[0], PRODUCTION_TYPES[1], currentSourcesState)
   }
 
-  private List<CompilationOutput> getTestsCompilationOutputs(Map<String, Map<String, BuildTargetState>> currentSourcesState) {
+  private fun getTestsCompilationOutputs(currentSourcesState: Map<String, Map<String, BuildTargetState>>): List<CompilationOutput> {
     return getCompilationOutputs(TEST, TEST_TYPES[0], TEST_TYPES[1], currentSourcesState)
   }
 
-  private List<CompilationOutput> getCompilationOutputs(String prefix,
-                                                        String firstUploadParam,
-                                                        String secondUploadParam,
-                                                        Map<String, Map<String, BuildTargetState>> currentSourcesState) {
-    def root = buildOutputRoot.resolve("classes").toFile()
+  private fun getCompilationOutputs(prefix: String,
+                                    firstUploadParam: String,
+                                    secondUploadParam: String,
+                                    currentSourcesState: Map<String, Map<String, BuildTargetState>>): List<CompilationOutput> {
+    val root = buildOutputRoot.resolve("classes").toFile()
 
-    def firstParamMap = currentSourcesState.get(firstUploadParam)
-    def secondParamMap = currentSourcesState.get(secondUploadParam)
+    val firstParamMap = currentSourcesState[firstUploadParam]!!
+    val secondParamMap = currentSourcesState[secondUploadParam]!!
 
-    def firstParamKeys = new HashSet<>(firstParamMap.keySet())
-    def secondParamKeys = new HashSet<>(secondParamMap.keySet())
-    def intersection = firstParamKeys.intersect(secondParamKeys)
+    val firstParamKeys = HashSet<String>(firstParamMap.keys)
+    val secondParamKeys = HashSet<String>(secondParamMap.keys)
+    val intersection = firstParamKeys.intersect(secondParamKeys)
 
-    def compilationOutputs = []
+    val compilationOutputs = ArrayList<CompilationOutput>(firstParamKeys.size + secondParamKeys.size - intersection.size)
 
-    intersection.each { buildTargetName ->
-      def firstParamState = firstParamMap.get(buildTargetName)
-      def secondParamState = secondParamMap.get(buildTargetName)
-      def outputPath = firstParamState.relativePath.replace(IDENTIFIER, root.getAbsolutePath())
+    intersection.forEach { buildTargetName ->
+      val firstParamState = firstParamMap[buildTargetName]!!
+      val secondParamState = secondParamMap[buildTargetName]!!
+      val outputPath = firstParamState.relativePath.replace(IDENTIFIER, root.absolutePath)
 
-      def hash = calculateStringHash(firstParamState.hash + secondParamState.hash)
-      compilationOutputs << new CompilationOutput(buildTargetName, prefix, hash, outputPath)
+      val hash = calculateStringHash(firstParamState.hash + secondParamState.hash)
+      compilationOutputs.add(CompilationOutput(buildTargetName, prefix, hash, outputPath))
     }
 
     firstParamKeys.removeAll(intersection)
-    firstParamKeys.each { buildTargetName ->
-      def firstParamState = firstParamMap.get(buildTargetName)
-      def outputPath = firstParamState.relativePath.replace(IDENTIFIER, root.getAbsolutePath())
+    firstParamKeys.forEach { buildTargetName ->
+      val firstParamState = firstParamMap[buildTargetName]!!
+      val outputPath = firstParamState.relativePath.replace(IDENTIFIER, root.absolutePath)
 
-      compilationOutputs << new CompilationOutput(buildTargetName, firstUploadParam, firstParamState.hash, outputPath)
+      compilationOutputs.add(CompilationOutput(buildTargetName, firstUploadParam, firstParamState.hash, outputPath))
     }
 
     secondParamKeys.removeAll(intersection)
-    secondParamKeys.each { buildTargetName ->
-      def secondParamState = secondParamMap.get(buildTargetName)
-      def outputPath = secondParamState.relativePath.replace(IDENTIFIER, root.getAbsolutePath())
+    secondParamKeys.forEach { buildTargetName ->
+      val secondParamState = secondParamMap[buildTargetName]!!
+      val outputPath = secondParamState.relativePath.replace(IDENTIFIER, root.absolutePath)
 
-      compilationOutputs << new CompilationOutput(buildTargetName, secondUploadParam, secondParamState.hash, outputPath)
+      compilationOutputs.add(CompilationOutput(buildTargetName, secondUploadParam, secondParamState.hash, outputPath))
     }
 
     return compilationOutputs
   }
 
-  private static String calculateStringHash(String content) {
-    def hasher = Hashing.murmur3_128().newHasher()
-    return hasher.putString(content, StandardCharsets.UTF_8).hash().toString()
-  }
+}
+
+private fun calculateStringHash(content: String): String {
+  val hasher = Hashing.murmur3_128().newHasher()
+  return hasher.putString(content, StandardCharsets.UTF_8).hash().toString()
 }
