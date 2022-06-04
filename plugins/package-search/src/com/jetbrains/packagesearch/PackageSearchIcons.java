3e9a693bd3ce0f533d817e65a60b6668939a7878
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-06-04 08:55:17 +0200

    regenerate PackageSearchIcons
    GitOrigin-RevId: e55423876f56e73226de2ee2cb5705eca546b8a2

--- plugins/package-search/src/com/jetbrains/packagesearch/PackageSearchIcons.java
+++ plugins/package-search/src/com/jetbrains/packagesearch/PackageSearchIcons.java
@@ -13,8 +13,8 @@ public final class PackageSearchIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, PackageSearchIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon Artifact = load("icons/artifact.svg", 1950163026, 2);
-  /** 13x13 */ public static final @NotNull Icon ArtifactSmall = load("icons/artifactSmall.svg", 283407247, 2);
-  /** 16x16 */ public static final @NotNull Icon Checkmark = load("icons/checkmark.svg", 340610411, 2);
-  /** 16x16 */ public static final @NotNull Icon Package = load("icons/package.svg", 1383777902, 0);
+  /** 16x16 */ public static final @NotNull Icon Artifact = load("icons/artifact.svg", -700400148, 2);
+  /** 13x13 */ public static final @NotNull Icon ArtifactSmall = load("icons/artifactSmall.svg", 143042270, 2);
+  /** 16x16 */ public static final @NotNull Icon Checkmark = load("icons/checkmark.svg", -383357801, 2);
+  /** 16x16 */ public static final @NotNull Icon Package = load("icons/package.svg", -269315220, 0);
 }
