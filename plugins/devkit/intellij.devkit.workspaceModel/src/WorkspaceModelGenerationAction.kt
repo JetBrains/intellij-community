dca5166e031fee2b7214be571120ad807f35b888
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2022-06-08 17:08:29 +0300

    [workspace model] Allow to use action if called from the Action Search menu (fix for module with two content roots at Rider)
    GitOrigin-RevId: 93b3cbb2570088e2e32bf18a3af880a73d0cf2b7

--- plugins/devkit/intellij.devkit.workspaceModel/src/WorkspaceModelGenerationAction.kt
+++ plugins/devkit/intellij.devkit.workspaceModel/src/WorkspaceModelGenerationAction.kt
@@ -1,10 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.workspaceModel.codegen
 
-import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.LangDataKeys
+import com.intellij.openapi.actionSystem.*
 
 class WorkspaceModelGenerationAction: AnAction() {
 
@@ -18,6 +15,10 @@ class WorkspaceModelGenerationAction: AnAction() {
   }
 
   override fun update(event: AnActionEvent) {
-    event.presentation.isEnabledAndVisible = event.getData(LangDataKeys.MODULE_CONTEXT) != null
+    if (event.place == ActionPlaces.ACTION_SEARCH) {
+      event.presentation.isEnabledAndVisible = true
+    } else {
+      event.presentation.isEnabledAndVisible = event.getData(LangDataKeys.MODULE_CONTEXT) != null
+    }
   }
 }
