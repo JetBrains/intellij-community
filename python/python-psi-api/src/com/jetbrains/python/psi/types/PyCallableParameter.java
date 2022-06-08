0cdfcf231eb91a9a5679da7bdef4737240c5e947
andrey.matveev <amatveev@jetbrains.com>
2022-06-08 22:08:49 +0300

    PY-29898 Impl resolve keyword argument to dataclass attribute
    Also fix PY-55231
    Add declarationElement field to callable parameter to resolve parameters to class fields in dataclasses
    
    (cherry picked from commit ae2703b958061df6f2d74b19920a6e3be2a084f6)
    
    IJ-MR-26158
    
    GitOrigin-RevId: 93493303ca1163fea7ea30bc3709181703087938

--- python/python-psi-api/src/com/jetbrains/python/psi/types/PyCallableParameter.java
+++ python/python-psi-api/src/com/jetbrains/python/psi/types/PyCallableParameter.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.psi.types;
 
+import com.intellij.psi.PsiElement;
 import com.jetbrains.python.psi.PyExpression;
 import com.jetbrains.python.psi.PyParameter;
 import org.jetbrains.annotations.NotNull;
@@ -32,6 +33,11 @@ public interface PyCallableParameter {
    */
   @Nullable
   PyParameter getParameter();
+
+  @Nullable
+  default PsiElement getDeclarationElement() {
+    return getParameter();
+  }
 
   @Nullable
   PyExpression getDefaultValue();
