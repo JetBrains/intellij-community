da5e3d861201aeda27655ef37cd7f6e3ec634fd6
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2022-06-08 16:00:03 +0200

    [build scripts] adding tests to the updater build configuration
    GitOrigin-RevId: d728c508dcb86d30f28f3019e29a323be5dc7c12

--- build/groovy/FullUpdaterBuildTarget.kt
+++ build/groovy/FullUpdaterBuildTarget.kt
@@ -1,14 +1,26 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
-import org.jetbrains.intellij.build.BuildTasks
-import org.jetbrains.intellij.build.IdeaProjectLoaderUtil
-import org.jetbrains.intellij.build.createCommunityBuildContext
+import org.jetbrains.intellij.build.*
 
 object FullUpdaterBuildTarget {
+  private const val UPDATER_MODULE_NAME = "intellij.platform.updater"
+
   @JvmStatic
   fun main(args: Array<String>) {
-    val tasks = BuildTasks.create(createCommunityBuildContext(IdeaProjectLoaderUtil.guessCommunityHome(javaClass)))
-    tasks.compileModules(listOf("updater"))
-    tasks.buildFullUpdaterJar()
+    val context = createCommunityBuildContext(IdeaProjectLoaderUtil.guessCommunityHome(javaClass))
+
+    context.messages.block("Building artifact") {
+      val tasks = BuildTasks.create(context)
+      tasks.compileModules(listOf(UPDATER_MODULE_NAME))
+      tasks.buildFullUpdaterJar()
+    }
+
+    context.messages.block("Testing") {
+      val options = TestingOptions()
+      options.testGroups = "UPDATER_TESTS"
+      options.mainModule = UPDATER_MODULE_NAME
+      TestingTasks.create(context, options)
+        .runTests(listOf("-Dintellij.build.test.ignoreFirstAndLastTests=true"))
+    }
   }
 }
