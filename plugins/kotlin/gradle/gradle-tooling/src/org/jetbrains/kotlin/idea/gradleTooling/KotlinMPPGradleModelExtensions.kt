722690713cfaa6d59b34a29405fff92e37846176
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2022-06-09 16:07:51 +0200

    [Kotlin][Import] KotlinMPPGradleModelExtensions: Promote functions to non-experimental
    (cherry picked from commit f25447b7c9501877aa439c469b031f0dd371d22f)
    
    IJ-MR-25574
    
    GitOrigin-RevId: 0ddbf4f78a489b61bdb8a5fdc7b827ef2a032a1f

--- plugins/kotlin/gradle/gradle-tooling/src/org/jetbrains/kotlin/idea/gradleTooling/KotlinMPPGradleModelExtensions.kt
+++ plugins/kotlin/gradle/gradle-tooling/src/org/jetbrains/kotlin/idea/gradleTooling/KotlinMPPGradleModelExtensions.kt
@@ -5,14 +5,12 @@ package org.jetbrains.kotlin.idea.gradleTooling
 import org.jetbrains.kotlin.idea.projectModel.KotlinCompilation
 import org.jetbrains.kotlin.idea.projectModel.KotlinSourceSet
 
-@ExperimentalGradleToolingApi
 fun KotlinMPPGradleModel.getCompilations(sourceSet: KotlinSourceSet): Set<KotlinCompilation> {
     return targets.flatMap { target -> target.compilations }
         .filter { compilation -> compilationDependsOnSourceSet(compilation, sourceSet) }
         .toSet()
 }
 
-@ExperimentalGradleToolingApi
 fun KotlinMPPGradleModel.compilationDependsOnSourceSet(
     compilation: KotlinCompilation, sourceSet: KotlinSourceSet
 ): Boolean {
