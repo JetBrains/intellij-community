58534f2ef594cc43eb40dfe7a55373e87f37e343
Konstantin Bulenkov <kb@jetbrains.com>
2022-06-09 12:20:55 +0200

    cleanup
    GitOrigin-RevId: ee100faa8e736ed2c91f41439b5308aa46ab8f11

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/VmOptionsGenerator.kt
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/VmOptionsGenerator.kt
@@ -20,7 +20,7 @@ object VmOptionsGenerator {
     "-XX:+HeapDumpOnOutOfMemoryError",
     "-XX:-OmitStackTraceInFastThrow",
     "-XX:+IgnoreUnrecognizedVMOptions",  // allowing the JVM to start even with outdated options stuck in user configs
-    "-XX:CompileCommand=exclude,com/intellij/openapi/vfs/impl/FilePartNodeRoot,trieDescend", // temporary workaround for crashes in G1 on M1 (JBR-4509)
+    "-XX:CompileCommand=exclude,com/intellij/openapi/vfs/impl/FilePartNodeRoot,trieDescend", // temporary workaround for crashes in С2 (JBR-4509)
     "-ea",
     "-Dsun.io.useCanonCaches=false",
     "-Dsun.java2d.metal=true",
