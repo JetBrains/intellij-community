3f6f70ca4114603a90a394e4fa9817ddb339835e
Konstantin Bulenkov <kb@jetbrains.com>
2022-06-09 14:07:55 +0200

    [NewUI] disable autoscroll from source by default
    GitOrigin-RevId: 9f24e842b407102dde0bd0887d179deb7894162c

--- platform/lang-impl/src/com/intellij/ui/ExperimentalUIImpl.kt
+++ platform/lang-impl/src/com/intellij/ui/ExperimentalUIImpl.kt
@@ -2,14 +2,12 @@
 package com.intellij.ui
 
 import com.fasterxml.jackson.jr.ob.JSON
-import com.intellij.ide.projectView.impl.ProjectViewState
 import com.intellij.ide.ui.LafManager
 import com.intellij.ide.ui.RegistryBooleanOptionDescriptor
 import com.intellij.ide.ui.UISettings
 import com.intellij.ide.ui.laf.LafManagerImpl
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
-import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.util.registry.Registry
 import java.io.IOException
 import java.util.*
@@ -25,10 +23,6 @@ class ExperimentalUIImpl : ExperimentalUI() {
     setRegistryKeyIfNecessary("ide.experimental.ui", true)
     setRegistryKeyIfNecessary("debugger.new.tool.window.layout", true)
     UISettings.getInstance().openInPreviewTabIfPossible = true
-    ProjectViewState.getDefaultInstance().autoscrollFromSource = true
-    ProjectManager.getInstance().openProjects.forEach {
-      ProjectViewState.getInstance(it).autoscrollFromSource = true
-    }
     val name = if (JBColor.isBright()) "Light" else "Dark"
     val lafManager = LafManagerImpl.getInstance()
     val laf = lafManager.installedLookAndFeels.first { x: LookAndFeelInfo -> x.name == name }
@@ -49,7 +43,6 @@ class ExperimentalUIImpl : ExperimentalUI() {
   override fun onExpUIDisabled() {
     setRegistryKeyIfNecessary("ide.experimental.ui", false)
     setRegistryKeyIfNecessary("debugger.new.tool.window.layout", false)
-    //ProjectViewState.getDefaultInstance().autoscrollFromSource = false
     val mgr = LafManager.getInstance() as LafManagerImpl
     val currentLafName = mgr.currentLookAndFeel?.name
     if (currentLafName == "Dark" || currentLafName == "Light") {
