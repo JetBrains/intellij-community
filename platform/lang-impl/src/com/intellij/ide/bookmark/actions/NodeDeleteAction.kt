1a19f437c56d66f5a0fe8b119b0bfaa15a8b66c9
Louis Vignier <louis.vignier@jetbrains.com>
2022-06-10 11:10:46 +0200

    IDEA-287913 [bookmark] Warn before deleting bookmark lists
    (cherry picked from commit 2aaee0f5ebe03f647289d5493cd1422cb7ccaf77)
    
    IJ-CR-25690
    
    GitOrigin-RevId: 0453ba9e25c0c367114479085981ec8116df0937

--- platform/lang-impl/src/com/intellij/ide/bookmark/actions/NodeDeleteAction.kt
+++ platform/lang-impl/src/com/intellij/ide/bookmark/actions/NodeDeleteAction.kt
@@ -1,10 +1,16 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.bookmark.actions
 
 import com.intellij.CommonBundle.messagePointer
+import com.intellij.ide.bookmark.BookmarkBundle
+import com.intellij.ide.bookmark.BookmarkGroup
 import com.intellij.ide.bookmark.BookmarksListProviderService
+import com.intellij.ide.bookmark.ui.tree.GroupNode
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.ui.DoNotAskOption
+import com.intellij.openapi.ui.MessageDialogBuilder
+import com.intellij.ui.UIBundle
 
 internal class NodeDeleteAction : DumbAwareAction(messagePointer("button.delete")) {
 
@@ -21,7 +27,34 @@ internal class NodeDeleteAction : DumbAwareAction(messagePointer("button.delete"
     val project = event.project ?: return
     val view = event.bookmarksView ?: return
     val nodes = view.selectedNodes ?: return
-    BookmarksListProviderService.findProvider(project) { it.canDelete(nodes) }?.performDelete(nodes, view.tree)
+    val bookmarksViewState = event.bookmarksViewState
+    val shouldDelete = when {
+      bookmarksViewState == null -> true
+      nodes.any { it is GroupNode } -> {
+        if (!bookmarksViewState.askBeforeDeletingLists) true
+        else {
+          val message =
+            if (nodes.size == 1) BookmarkBundle.message("dialog.message.delete.single.bookmark.list", (nodes.first { it is GroupNode }.value as BookmarkGroup).name)
+            else BookmarkBundle.message("dialog.message.delete.multiple.bookmark.lists")
+          MessageDialogBuilder
+            .yesNo(BookmarkBundle.message("dialog.message.delete.title"), message)
+            .yesText(BookmarkBundle.message("dialog.message.delete.button"))
+            .noText(BookmarkBundle.message("dialog.message.cancel.button"))
+            .doNotAsk(object : DoNotAskOption.Adapter() {
+              override fun rememberChoice(isSelected: Boolean, exitCode: Int) {
+                bookmarksViewState.askBeforeDeletingLists = !isSelected
+              }
+            })
+            .asWarning()
+            .ask(project)
+        }
+      }
+      else -> true
+    }
+
+    if (shouldDelete) {
+        BookmarksListProviderService.findProvider(project) { it.canDelete(nodes) }?.performDelete(nodes, view.tree)
+    }
   }
 
   init {
