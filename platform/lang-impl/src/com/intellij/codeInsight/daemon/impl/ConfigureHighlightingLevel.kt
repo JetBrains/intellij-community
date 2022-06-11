b75c52e9fa5dfc692f960252892847105c99876d
Alexey Kudravtsev <cdr@intellij.com>
2022-06-11 02:47:07 +0200

    restore after revert - it didn't help
    GitOrigin-RevId: 8a94851cf2582f315d5fc1e5ab79de86a671ca6c

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/ConfigureHighlightingLevel.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/ConfigureHighlightingLevel.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.daemon.impl
 
 import com.intellij.codeInsight.daemon.DaemonBundle.message
@@ -78,6 +78,9 @@ private class LevelAction(val level: InspectionsLevel, val provider: FileViewPro
 
 
 internal class ConfigureHighlightingLevelAction : DumbAwareAction() {
+  override fun getActionUpdateThread(): ActionUpdateThread {
+    return ActionUpdateThread.BGT
+  }
 
   override fun update(event: AnActionEvent) {
     val enabled = event.getData(PSI_FILE)?.viewProvider?.languages?.isNotEmpty()
