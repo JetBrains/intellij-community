0e14761b310a9036310156d5c530bf90e0c42ca1
Andrey Uskov <andrey.uskov@jetbrains.com>
2022-06-13 00:06:56 +0300

    Updated version of Kotlin Gradle OS FUS collector
    Relates to FUS-1730
    
    GitOrigin-RevId: ff9b904a80e254c728c83cd83098f4577a6fd823

--- plugins/kotlin/gradle/gradle-idea/src/org/jetbrains/kotlin/idea/gradle/statistics/KotlinGradleFUSCollector.kt
+++ plugins/kotlin/gradle/gradle-idea/src/org/jetbrains/kotlin/idea/gradle/statistics/KotlinGradleFUSCollector.kt
@@ -15,7 +15,7 @@ class KotlinGradleFUSCollector : CounterUsagesCollector() {
 
     companion object {
 
-        private val GROUP = EventLogGroup("kotlin.gradle.performance", 4)
+        private val GROUP = EventLogGroup("kotlin.gradle.performance", 5)
 
         private fun listOfAllMetrics(): Array<Any> {
             val result = ArrayList<Any>()
