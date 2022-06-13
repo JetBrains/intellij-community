c7e6cba7e59ec547b705c91cc85d88958c9468b5
Lamberto Basti <lamberto.basti@jetbrains.com>
2022-06-13 16:11:05 +0200

    [PKGS] Added copyright header
    GitOrigin-RevId: 915ad264c7d5ac0879914e79df4d88f37ea244d8

--- plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/data/PackagesUpgradesUtils.kt
+++ plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/data/PackagesUpgradesUtils.kt
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2000-2022 JetBrains s.r.o. and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package com.jetbrains.packagesearch.intellij.plugin.data
 
 import com.intellij.openapi.module.Module
@@ -14,7 +30,7 @@ import com.jetbrains.packagesearch.intellij.plugin.ui.toolwindow.models.upgradeC
 import com.jetbrains.packagesearch.intellij.plugin.ui.toolwindow.models.versions.NormalizedPackageVersion
 import com.jetbrains.packagesearch.intellij.plugin.ui.toolwindow.models.versions.PackageVersionNormalizer
 import com.jetbrains.packagesearch.intellij.plugin.util.logError
-import com.jetbrains.packagesearch.packageversionutils.PackageVersionUtils
+import org.jetbrains.packagesearch.packageversionutils.PackageVersionUtils
 
 internal suspend fun computePackageUpgrades(
     installedPackages: List<PackageModel.Installed>,
--- plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/ui/toolwindow/panels/management/packages/PackagesListPanel.kt
+++ plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/ui/toolwindow/panels/management/packages/PackagesListPanel.kt
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2000-2022 JetBrains s.r.o. and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package com.jetbrains.packagesearch.intellij.plugin.ui.toolwindow.panels.management.packages
 
 import com.intellij.openapi.actionSystem.ActionManager
@@ -10,8 +26,6 @@ import com.intellij.ui.SearchTextField
 import com.intellij.ui.components.JBPanelWithEmptyText
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.util.ui.JBUI
-import org.jetbrains.packagesearch.api.v2.ApiPackagesResponse
-import org.jetbrains.packagesearch.api.v2.ApiStandardPackage
 import com.jetbrains.packagesearch.intellij.plugin.PackageSearchBundle
 import com.jetbrains.packagesearch.intellij.plugin.data.PackageUpgradeCandidates
 import com.jetbrains.packagesearch.intellij.plugin.fus.FUSGroupIds
@@ -84,6 +98,8 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import net.miginfocom.swing.MigLayout
+import org.jetbrains.packagesearch.api.v2.ApiPackagesResponse
+import org.jetbrains.packagesearch.api.v2.ApiStandardPackage
 import java.awt.BorderLayout
 import java.awt.Dimension
 import java.awt.event.ItemEvent
