bffb9bcefc8e3deaf2358aadff079dc6054bdb52
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2022-06-13 17:59:38 +0200

    [devkit] include intellij.platform.workspaceModel.codegen as a module to devkit plugin (IDEA-295975)
    ...and add it as a dependency to intellij.devkit.workspaceModel module.
    
    (cherry picked from commit 0dda553b1f14cd300d70920d3e2e2cf4f4c7c52e)
    
    IJ-CR-25704
    
    GitOrigin-RevId: 9ed1224328af2b33e24c8ccab2d068c2a2321649

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityRepositoryModules.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityRepositoryModules.groovy
@@ -166,6 +166,7 @@ final class CommunityRepositoryModules {
       withModule("intellij.devkit.intelliLang")
       withModule("intellij.devkit.uiDesigner")
       withModule("intellij.devkit.workspaceModel")
+      withModule("intellij.platform.workspaceModel.codegen")
       withModule("intellij.java.devkit")
       withModule("intellij.groovy.devkit")
       withModule("intellij.devkit.jps")
