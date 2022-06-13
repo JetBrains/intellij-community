02498626f0b6eaba65515c7daea6f7d3e840d7cd
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2022-06-13 17:43:24 +0200

    [devkit] use unique root package name for intellij.devkit.workspaceModel module (IDEA-295975)
    To avoid clashes with intellij.platform.workspaceModel.codegen module.
    
    GitOrigin-RevId: 480f8aa1fa6445439faf022802a36aac5ebcc7ae

--- plugins/devkit/intellij.devkit.workspaceModel/src/WorkspaceModelGenerationAction.kt
+++ plugins/devkit/intellij.devkit.workspaceModel/src/WorkspaceModelGenerationAction.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.workspaceModel.codegen
+package com.intellij.devkit.workspaceModel
 
 import com.intellij.openapi.actionSystem.*
 
