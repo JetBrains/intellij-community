23c6262b887a1045e0b82a6e442c669e25247f2c
Ivan Posti <ivan.posti@jetbrains.com>
2022-06-14 18:53:56 +0300

    [Markdown] Add inspection for unresolved header anchors
    GitOrigin-RevId: 90f221cf2990cce2c8a8ddd2faff82c85bc714c0

---
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/model/psi/headers/UnresolvedHeaderReferenceInspection.kt
@@ -0,0 +1,44 @@
+package org.intellij.plugins.markdown.model.psi.headers
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.model.psi.PsiSymbolReferenceService
+import com.intellij.openapi.components.service
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementVisitor
+import org.intellij.plugins.markdown.MarkdownBundle
+import org.intellij.plugins.markdown.lang.psi.MarkdownElementVisitor
+import org.intellij.plugins.markdown.lang.psi.impl.MarkdownLinkDestination
+import org.intellij.plugins.markdown.model.psi.MarkdownPsiSymbolReference
+import org.jetbrains.annotations.ApiStatus
+
+@ApiStatus.Internal
+class UnresolvedHeaderReferenceInspection: LocalInspectionTool() {
+  override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+    return object: MarkdownElementVisitor() {
+      override fun visitLinkDestination(linkDestination: MarkdownLinkDestination) {
+        checkReference(linkDestination, holder)
+      }
+    }
+  }
+
+  private fun checkReference(element: PsiElement, holder: ProblemsHolder) {
+    val references = findSymbolReferences(element).filter { it.resolveReference().isEmpty() }
+    for (reference in references) {
+      val text = reference.rangeInElement.substring(reference.element.text)
+      holder.registerProblem(
+        element,
+        MarkdownBundle.message("markdown.unresolved.header.reference.inspection.text", text),
+        ProblemHighlightType.LIKE_UNKNOWN_SYMBOL,
+        reference.rangeInElement
+      )
+    }
+  }
+
+  private fun findSymbolReferences(element: PsiElement): Sequence<MarkdownPsiSymbolReference> {
+    val service = service<PsiSymbolReferenceService>()
+    val references = service.getReferences(element).asSequence()
+    return references.filterIsInstance<MarkdownPsiSymbolReference>()
+  }
+}
