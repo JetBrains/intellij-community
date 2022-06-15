6bed0821859b9b96ce412f6f77f7bcf467c909d4
Gregory.Shrago <gregory.shrago@jetbrains.com>
2022-06-15 03:18:13 +0300

    use simpler shortcuts and add ability to dump threads
    GitOrigin-RevId: d8de9cc5124140d8330636e6d973ae73ddac2428

--- platform/platform-impl/src/com/intellij/openapi/progress/util/PotemkinOverlayProgress.java
+++ platform/platform-impl/src/com/intellij/openapi/progress/util/PotemkinOverlayProgress.java
@@ -1,15 +1,19 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.progress.util;
 
+import com.intellij.ide.actions.DumpThreadsAction;
 import com.intellij.ide.nls.NlsMessages;
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.actionSystem.MouseShortcut;
+import com.intellij.openapi.actionSystem.KeyboardShortcut;
+import com.intellij.openapi.actionSystem.Shortcut;
 import com.intellij.openapi.keymap.KeymapUtil;
 import com.intellij.openapi.progress.StandardProgressIndicator;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.wm.IdeFrame;
 import com.intellij.openapi.wm.IdeGlassPane;
 import com.intellij.ui.JBColor;
+import com.intellij.ui.KeyStrokeAdapter;
 import com.intellij.util.ObjectUtils;
 import com.intellij.util.ui.EDT;
 import com.intellij.util.ui.GraphicsUtil;
@@ -20,14 +24,16 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
 public final class PotemkinOverlayProgress extends AbstractProgressIndicatorBase
   implements StandardProgressIndicator, ProgressIndicatorWithDelayedPresentation, PingProgress, Disposable {
 
-  private static final MouseShortcut SHORTCUT = new MouseShortcut(
-    1, (SystemInfo.isMac ? InputEvent.META_DOWN_MASK : InputEvent.CTRL_DOWN_MASK) |
-       InputEvent.ALT_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK, 1);
+  private static final KeyboardShortcut CANCEL_SHORTCUT = new KeyboardShortcut(
+    KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), null);
+  private static final KeyboardShortcut DUMP_SHORTCUT = new KeyboardShortcut(
+    KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, (SystemInfo.isMac ? InputEvent.META_DOWN_MASK : InputEvent.CTRL_DOWN_MASK)), null);
 
   private final Component myComponent;
   private final PotemkinProgress.EventStealer myEventStealer;
@@ -83,13 +89,28 @@ public final class PotemkinOverlayProgress extends AbstractProgressIndicatorBase
   }
 
   private void dispatchInputEvent(@NotNull InputEvent event) {
-    if (!(event instanceof MouseEvent)) return;
-    boolean isCancellation = event.getID() == MouseEvent.MOUSE_PRESSED &&
-                             (event.getModifiersEx() & SHORTCUT.getModifiers()) == SHORTCUT.getModifiers() &&
-                             ((MouseEvent)event).getButton() == SHORTCUT.getButton();
-    if (isCancellation) {
+    Shortcut shortcut = createShortcut(event);
+    if (shortcut == null) return;
+    if (CANCEL_SHORTCUT.equals(shortcut)) {
+      event.consume();
       cancel();
     }
+    else if (DUMP_SHORTCUT.equals(shortcut)) {
+      event.consume();
+      IdeFrame frame = UIUtil.getParentOfType(IdeFrame.class, myComponent);
+      DumpThreadsAction.dumpThreads(frame == null ? null : frame.getProject());
+    }
+  }
+
+  private static @Nullable Shortcut createShortcut(@NotNull InputEvent event) {
+    if (event instanceof MouseEvent) {
+      return KeymapUtil.createMouseShortcut((MouseEvent)event);
+    }
+    else if (event instanceof KeyEvent) {
+      KeyStroke ks = KeyStrokeAdapter.getDefaultKeyStroke((KeyEvent)event);
+      return ks == null ? null : new KeyboardShortcut(ks, null);
+    }
+    return null;
   }
 
   private void updateUI(long now) {
@@ -106,7 +127,8 @@ public final class PotemkinOverlayProgress extends AbstractProgressIndicatorBase
     if (glassPane == null) return;
     long roundedDuration = (System.currentTimeMillis() - myCreatedAt) / 1000 * 1000;
     //noinspection HardCodedStringLiteral
-    String text = "Use " + KeymapUtil.getMouseShortcutText(SHORTCUT) + " to cancel (" +
+    String text = KeymapUtil.getShortcutText(CANCEL_SHORTCUT) + " to cancel, " +
+                  KeymapUtil.getShortcutText(DUMP_SHORTCUT) + " to dump threads (" +
                   NlsMessages.formatDurationApproximateNarrow(roundedDuration) + ")";
     Graphics graphics = rootPane.getGraphics();
     GraphicsUtil.setupAAPainting(graphics);
