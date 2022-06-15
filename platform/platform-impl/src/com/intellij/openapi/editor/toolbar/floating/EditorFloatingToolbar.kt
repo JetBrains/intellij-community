e0549b47c77ed1fe145dcf74be70205ba4ca2ebb
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2022-06-15 21:10:46 +0300

    IDEA-292208 fixed: remove component floating toolbar when plugin owner is disposed
    GitOrigin-RevId: 2aa7c6adbde3627251e35a60b690d7746ea1c8f9

--- platform/platform-impl/src/com/intellij/openapi/editor/toolbar/floating/EditorFloatingToolbar.kt
+++ platform/platform-impl/src/com/intellij/openapi/editor/toolbar/floating/EditorFloatingToolbar.kt
@@ -1,15 +1,18 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.editor.toolbar.floating
 
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.event.EditorMouseEvent
 import com.intellij.openapi.editor.event.EditorMouseMotionListener
 import com.intellij.openapi.editor.impl.EditorImpl
+import com.intellij.openapi.extensions.ExtensionPointListener
+import com.intellij.openapi.extensions.PluginDescriptor
+import com.intellij.openapi.observable.util.whenDisposed
+import com.intellij.openapi.util.Disposer
+import java.awt.Component
 import java.awt.FlowLayout
-import java.awt.Point
-import java.awt.Rectangle
 import javax.swing.BorderFactory
 import javax.swing.JPanel
-import javax.swing.JScrollPane
 
 class EditorFloatingToolbar(editor: EditorImpl) : JPanel() {
   init {
@@ -17,45 +20,60 @@ class EditorFloatingToolbar(editor: EditorImpl) : JPanel() {
     border = BorderFactory.createEmptyBorder()
     isOpaque = false
 
-    val targetComponent = editor.contentComponent
-    val container = editor.scrollPane
-    val parentDisposable = editor.disposable
-    val toolbarComponents = ArrayList<FloatingToolbarComponentImpl>()
     FloatingToolbarProvider.EP_NAME.forEachExtensionSafe { provider ->
-      val actionGroup = provider.actionGroup
-      val autoHideable = provider.autoHideable
-      val component = FloatingToolbarComponentImpl(this, targetComponent, actionGroup, autoHideable, parentDisposable)
-      provider.register(editor.dataContext, component, parentDisposable)
-      toolbarComponents.add(component)
-      add(component)
+      addFloatingToolbarComponent(provider, editor)
     }
+    FloatingToolbarProvider.EP_NAME.addExtensionPointListener(object : ExtensionPointListener<FloatingToolbarProvider> {
+      override fun extensionAdded(extension: FloatingToolbarProvider, pluginDescriptor: PluginDescriptor) {
+        addFloatingToolbarComponent(extension, editor)
+      }
+    }, editor.disposable)
 
     val executor = ExecutorWithThrottling(SCHEDULE_SHOW_DELAY)
     editor.addEditorMouseMotionListener(object : EditorMouseMotionListener {
       override fun mouseMoved(e: EditorMouseEvent) {
         executor.executeOrSkip {
-          if (isInsideActivationArea(container, e.mouseEvent.point)) {
-            for (component in toolbarComponents) {
-              if (component.autoHideable) {
-                component.scheduleShow()
-              }
-            }
-          }
+          components.asSequence()
+            .filterIsInstance<FloatingToolbarComponentImpl>()
+            .filter { it.autoHideable }
+            .forEach { it.scheduleShow() }
         }
       }
     })
   }
 
+  private fun addFloatingToolbarComponent(provider: FloatingToolbarProvider, editor: EditorImpl) {
+    val disposable = createExtensionDisposable(provider, editor.disposable)
+    val targetComponent = editor.contentComponent
+    val actionGroup = provider.actionGroup
+    val autoHideable = provider.autoHideable
+    val component = FloatingToolbarComponentImpl(this, targetComponent, actionGroup, autoHideable, disposable)
+    provider.register(editor.dataContext, component, disposable)
+    add(component, disposable)
+  }
+
+  private fun add(component: Component, parentDisposable: Disposable) {
+    add(component)
+    parentDisposable.whenDisposed {
+      remove(component)
+    }
+  }
+
+  private fun createExtensionDisposable(provider: FloatingToolbarProvider, parentDisposable: Disposable): Disposable {
+    val disposable = Disposer.newDisposable()
+    Disposer.register(parentDisposable, disposable)
+    FloatingToolbarProvider.EP_NAME.addExtensionPointListener(object : ExtensionPointListener<FloatingToolbarProvider> {
+      override fun extensionRemoved(extension: FloatingToolbarProvider, pluginDescriptor: PluginDescriptor) {
+        if (provider === extension) {
+          Disposer.dispose(disposable)
+        }
+      }
+    }, disposable)
+    return disposable
+  }
+
   companion object {
     // Should be less than [VisibilityController.RETENTION_DELAY]
     private const val SCHEDULE_SHOW_DELAY = 1000
-
-    private fun isInsideActivationArea(container: JScrollPane, p: Point): Boolean {
-      val viewport = container.viewport
-      val r = viewport.bounds
-      val viewPosition = viewport.viewPosition
-      val activationArea = Rectangle(0, 0, r.width, r.height)
-      return activationArea.contains(p.x, p.y - viewPosition.y)
-    }
   }
 }
