779a353cc787f80c3ddd8b9410239019a6777434
Ivan Semenov <ivan.semenov@jetbrains.com>
2022-06-16 20:03:40 +0300

    [collab] extract focus border
    GitOrigin-RevId: f3ac666b4272c48a693d071713fb35c2f6e6530a

--- plugins/github/src/org/jetbrains/plugins/github/ui/component/GHAccountSelectorComponentFactory.kt
+++ plugins/github/src/org/jetbrains/plugins/github/ui/component/GHAccountSelectorComponentFactory.kt
@@ -2,6 +2,7 @@
 package org.jetbrains.plugins.github.ui.component
 
 import com.intellij.collaboration.async.CompletableFutureUtil.submitIOTask
+import com.intellij.collaboration.ui.SimpleFocusBorder
 import com.intellij.collaboration.ui.codereview.avatar.CachingCircleImageIconsProvider
 import com.intellij.ide.ui.laf.darcula.DarculaUIUtil
 import com.intellij.openapi.progress.EmptyProgressIndicator
@@ -41,30 +42,10 @@ class GHAccountSelectorComponentFactory {
       isOpaque = false
       cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
       isFocusable = true
-      border = FocusBorder()
+      border = SimpleFocusBorder()
     }
     Controller(model, label)
     return label
-  }
-
-  class FocusBorder : Border {
-    override fun paintBorder(c: Component?, g: Graphics?, x: Int, y: Int, width: Int, height: Int) {
-      if (c?.hasFocus() == true && g is Graphics2D) {
-        DarculaUIUtil.paintFocusBorder(g, width, height, 0f, true)
-      }
-    }
-
-    override fun getBorderInsets(c: Component): Insets {
-      val g2d = c.graphics as? Graphics2D ?: return JBInsets.emptyInsets()
-
-      val bw = if (UIUtil.isUnderDefaultMacTheme()) JBUIScale.scale(3).toFloat() else DarculaUIUtil.BW.float
-      val f = if (UIUtil.isRetina(g2d)) 0.5f else 1.0f
-      val lw = if (UIUtil.isUnderDefaultMacTheme()) JBUIScale.scale(f) else DarculaUIUtil.LW.float
-      val insets = (bw + lw).toInt()
-      return Insets(insets, insets, insets, insets)
-    }
-
-    override fun isBorderOpaque() = false
   }
 
   class Controller(private val accountsModel: ComboBoxWithActionsModel<GithubAccount>, private val label: JLabel)
