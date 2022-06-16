e6ee5fbd975b56416b3748feac97b9e3c6cf06c2
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-06-16 16:34:56 +0300

    [gradle] Move GradleCompletionWeigher to GradleLookupWeigher
    GitOrigin-RevId: 5f9a7aabf0ae40965b1c74df8f47f8e99e4f3fae

--- plugins/gradle/java/src/service/completion/GradleLookupWeigher.kt
+++ plugins/gradle/java/src/service/completion/GradleLookupWeigher.kt
@@ -7,15 +7,31 @@ import com.intellij.codeInsight.lookup.WeighingContext
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.UserDataHolder
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiJavaFile
+import com.intellij.psi.PsiMember
 import com.intellij.util.castSafelyTo
 import org.jetbrains.plugins.groovy.lang.psi.api.GroovyResolveResult
 
-class GradleLookupWeigher : LookupElementWeigher("gradleInPlaceWeigher", true, false) {
+class GradleLookupWeigher : LookupElementWeigher("gradleWeigher", true, false) {
+
   override fun weigh(element: LookupElement, context: WeighingContext): Comparable<*> {
     val completionElement = element.`object`
-    val holder = completionElement.castSafelyTo<UserDataHolder>() ?: completionElement.castSafelyTo<GroovyResolveResult>()?.element ?: return 0
-    val data = holder.getUserData(COMPLETION_PRIORITY)
-    return data ?: 0
+    val holder =
+      completionElement.castSafelyTo<UserDataHolder>()
+      ?: completionElement.castSafelyTo<GroovyResolveResult>()?.element
+      ?: return 0
+    return holder.getUserData(COMPLETION_PRIORITY) ?: getFallbackCompletionPriority(holder)
+  }
+
+  private fun getFallbackCompletionPriority(holder: UserDataHolder): Int {
+    if (holder !is PsiMember) {
+      return 0
+    }
+    val containingFile = holder.containingFile ?: holder.castSafelyTo<PsiMember>()?.containingClass?.containingFile
+    if (containingFile is PsiJavaFile && containingFile.packageName.startsWith("org.gradle")) {
+      return 1
+    }
+    return 0
   }
 
   companion object {
