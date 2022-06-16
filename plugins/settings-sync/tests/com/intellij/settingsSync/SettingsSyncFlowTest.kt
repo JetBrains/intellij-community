84d29159ee3b166598cde3bdaef0768b854d3701
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2022-06-16 15:15:34 +0100

    settings-sync: copy current settings in all initialization cases: IDEA-296017
    GitOrigin-RevId: 1f5bddd42c62897cc884481586465f47d1ce3433

--- plugins/settings-sync/tests/com/intellij/settingsSync/SettingsSyncFlowTest.kt
+++ plugins/settings-sync/tests/com/intellij/settingsSync/SettingsSyncFlowTest.kt
@@ -1,31 +1,41 @@
 package com.intellij.settingsSync
 
+import com.intellij.util.io.write
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import kotlin.io.path.createDirectories
-import kotlin.io.path.createFile
 import kotlin.io.path.writeText
 
 @RunWith(JUnit4::class)
 internal class SettingsSyncFlowTest : SettingsSyncTestBase() {
 
-  fun initSettingsSync() {
+  private fun initSettingsSync(initMode: SettingsSyncBridge.InitMode = SettingsSyncBridge.InitMode.JustInit) {
     val ideMediator = MockSettingsSyncIdeMediator()
     val controls = SettingsSyncMain.init(application, disposable, settingsSyncStorage, configDir, remoteCommunicator, ideMediator)
     updateChecker = controls.updateChecker
     bridge = controls.bridge
-    bridge.initialize(SettingsSyncBridge.InitMode.JustInit)
+    bridge.initialize(initMode)
+  }
+  
+  @Test fun `existing settings should be copied on initialization`() {
+    val fileName = "options/laf.xml"
+    val initialContent = "LaF Initial"
+    configDir.resolve(fileName).write(initialContent)
+
+    initSettingsSync(SettingsSyncBridge.InitMode.PushToServer)
+
+    val pushedSnapshot = remoteCommunicator.pushed
+    assertNotNull("Nothing has been pushed", pushedSnapshot)
+    pushedSnapshot!!.assertSettingsSnapshot {
+      fileState(fileName, initialContent)
+    }
   }
 
   @Test fun `settings modified between IDE sessions should be logged`() {
     // emulate first session with initialization
     val fileName = "options/laf.xml"
-    val file = configDir.resolve(fileName)
-    file.parent.createDirectories()
-    file.createFile()
-    file.writeText("LaF Initial")
+    val file = configDir.resolve(fileName).write("LaF Initial")
     val log = GitSettingsLog(settingsSyncStorage, configDir, disposable, MockSettingsSyncIdeMediator.getAllFilesFromSettings(configDir))
     log.initialize()
     log.logExistingSettings()
