e42e220a9a1e47a21d3194a862d0b79c47279235
Svetlana.Zemlyanskaya <Svetlana.Zemlyanskaya@jetbrains.com>
2022-06-17 17:25:49 +0200

    ML in SE: increase data batch size
    GitOrigin-RevId: 5f3dee458d2b4bc7e3b61a7f37d7611a25d55e54

--- plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/logger/SearchEverywhereEventLoggerProvider.kt
+++ plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/logger/SearchEverywhereEventLoggerProvider.kt
@@ -8,7 +8,7 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.util.PlatformUtils
 import java.util.concurrent.TimeUnit
 
-class SearchEverywhereEventLoggerProvider : StatisticsEventLoggerProvider("MLSE", 7, TimeUnit.MINUTES.toMillis(10), 75 * 1024, sendLogsOnIdeClose = true) {
+class SearchEverywhereEventLoggerProvider : StatisticsEventLoggerProvider("MLSE", 8, TimeUnit.MINUTES.toMillis(10), 100 * 1024, sendLogsOnIdeClose = true) {
   override fun isRecordEnabled(): Boolean {
     val app = ApplicationManager.getApplication()
     return !app.isUnitTestMode && app.isEAP &&
