7494da790b173d6112e1285d6f2cc3b252e60e8d
Nikita Skvortsov <Nikita.Skvortsov@jetbrains.com>
2022-06-17 15:59:43 +0300

    [gradle][maven][es] provide Action Update Thread info for Actions in Maven and Gradle plugins.
    Also, do it for base External System actions in IJ Platform. action-update-thread
    
    GitOrigin-RevId: cc0f9ff7a4420bd9263790a19e85017404929d03

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/utils/actions/MavenAction.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/utils/actions/MavenAction.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.maven.utils.actions;
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
@@ -21,5 +22,10 @@ public abstract class MavenAction extends AnAction implements DumbAware {
 
   protected boolean isVisible(@NotNull AnActionEvent e) {
     return true;
+  }
+
+  @Override
+  public @NotNull ActionUpdateThread getActionUpdateThread() {
+    return ActionUpdateThread.BGT;
   }
 }
