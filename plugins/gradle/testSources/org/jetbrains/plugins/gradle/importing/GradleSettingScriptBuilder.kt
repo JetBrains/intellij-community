cc779bdda8b4a41836c208160092a4ce03d65cea
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-06-19 19:29:10 +0300

    [gradle] IDEA-279603: Add tests for completion of version catalogs
    GitOrigin-RevId: 26d5d826f5570c92b9e55dd74b6f0d730b3219c2

--- plugins/gradle/testSources/org/jetbrains/plugins/gradle/importing/GradleSettingScriptBuilder.kt
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/importing/GradleSettingScriptBuilder.kt
@@ -1,8 +1,7 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.importing
 
 import java.util.*
-import kotlin.collections.ArrayList
 
 class GradleSettingScriptBuilder {
 
@@ -12,6 +11,10 @@ class GradleSettingScriptBuilder {
 
   fun setProjectName(projectName: String) {
     this.projectName = projectName
+  }
+
+  fun enableFeaturePreview(featureName: String) {
+    lines.add("enableFeaturePreview('$featureName')")
   }
 
   fun include(name: String) = apply {
