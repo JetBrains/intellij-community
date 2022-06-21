cb70896afda5b3b47d7d8a4558f81704e8903e06
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2022-06-21 22:46:03 +0300

    action-update-thread: misc actions
    GitOrigin-RevId: 9ea0f584e5b20dc4994bd7fce58b5db8243e7223

--- jvm/jvm-analysis-impl/src/com/intellij/analysis/internal/DumpUastTreeActionByEach.kt
+++ jvm/jvm-analysis-impl/src/com/intellij/analysis/internal/DumpUastTreeActionByEach.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.analysis.internal
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import org.jetbrains.uast.toUElement
@@ -11,4 +12,8 @@ class DumpUastTreeActionByEach : DumpUastTreeAction() {
   override fun buildDump(file: PsiFile): String = object : IndentedPrintingVisitor({true}){
     override fun render(element: PsiElement): CharSequence? = element.toUElement()?.asLogString()
   }.also { file.accept(it) }.result
+
+  override fun getActionUpdateThread(): ActionUpdateThread {
+    return ActionUpdateThread.BGT
+  }
 }
