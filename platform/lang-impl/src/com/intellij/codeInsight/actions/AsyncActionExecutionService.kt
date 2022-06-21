fc63d329a98668eebd33477b06d9b2954a803493
Nikita.Lyubimov <nikita.lyubimov@jetbrains.com>
2022-06-21 16:33:07 +0200

    Add API to run some code on after async action execution
    (cherry picked from commit 40ffbcab98129b377e33e99e025d59998ede1074)
    
    IJ-CR-26108
    
    GitOrigin-RevId: 9c33d712f5098e186610f68843db6a58748302d8

---
+++ platform/lang-impl/src/com/intellij/codeInsight/actions/AsyncActionExecutionService.kt
@@ -0,0 +1,20 @@
+package com.intellij.codeInsight.actions
+
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+
+/**
+ *  Uses to schedule execution on after specific action, if action executes asynchronously
+ */
+open class AsyncActionExecutionService {
+
+  companion object {
+    fun getInstance(project: Project) = project.service<AsyncActionExecutionService>()
+  }
+
+
+  open fun withExecutionAfterAction(actionId: String, mainActionCallBlock: () -> Unit, onAfterAsyncActionExecute: () -> Unit) {
+    mainActionCallBlock()
+    onAfterAsyncActionExecute()
+  }
+}
