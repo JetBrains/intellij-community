c02590f01176d5e525ac579b10e731ccd8e908bf
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2022-06-21 03:11:56 +0300

    [cleanup] reuse common util method for extension disposable creation
    GitOrigin-RevId: 83452dfbf1c3bc956ad0f9ecd3cbd414d2ad4731

--- platform/platform-impl/src/com/intellij/openapi/editor/toolbar/floating/EditorFloatingToolbar.kt
+++ platform/platform-impl/src/com/intellij/openapi/editor/toolbar/floating/EditorFloatingToolbar.kt
@@ -6,7 +6,6 @@ import com.intellij.openapi.editor.impl.EditorImpl
 import com.intellij.openapi.extensions.ExtensionPointListener
 import com.intellij.openapi.extensions.PluginDescriptor
 import com.intellij.openapi.observable.util.whenDisposed
-import com.intellij.openapi.util.Disposer
 import java.awt.Component
 import java.awt.FlowLayout
 import javax.swing.BorderFactory
@@ -30,7 +29,7 @@ class EditorFloatingToolbar(editor: EditorImpl) : JPanel() {
 
   private fun addFloatingToolbarComponent(editor: EditorImpl, provider: FloatingToolbarProvider) {
     if (provider.isApplicable(editor.dataContext)) {
-      val disposable = createExtensionDisposable(provider, editor.disposable)
+      val disposable = FloatingToolbarProvider.createExtensionDisposable(provider, editor.disposable)
       val component = FloatingToolbarComponentImpl(editor, provider, disposable)
       add(component, disposable)
     }
@@ -41,18 +40,5 @@ class EditorFloatingToolbar(editor: EditorImpl) : JPanel() {
     parentDisposable.whenDisposed {
       remove(component)
     }
-  }
-
-  private fun createExtensionDisposable(provider: FloatingToolbarProvider, parentDisposable: Disposable): Disposable {
-    val disposable = Disposer.newDisposable()
-    Disposer.register(parentDisposable, disposable)
-    FloatingToolbarProvider.EP_NAME.addExtensionPointListener(object : ExtensionPointListener<FloatingToolbarProvider> {
-      override fun extensionRemoved(extension: FloatingToolbarProvider, pluginDescriptor: PluginDescriptor) {
-        if (provider === extension) {
-          Disposer.dispose(disposable)
-        }
-      }
-    }, disposable)
-    return disposable
   }
 }
