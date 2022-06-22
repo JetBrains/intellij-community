6b8c20074e2075a449270589c617cc7d909dc981
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-06-22 13:48:31 +0200

    [kotlin][light classes] drop old light classes and backend: iteration #4
    drop LightClassDataHolder from LightClassGenerationSupport
    
    ^KT-48773
    
    GitOrigin-RevId: b62aafb2f82c2d01537ad4f215e842342a4d10cc

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/project/IdeaEnvironment.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/project/IdeaEnvironment.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.project
 
@@ -6,7 +6,6 @@ import org.jetbrains.kotlin.cfg.ControlFlowInformationProviderImpl
 import org.jetbrains.kotlin.container.StorageComponentContainer
 import org.jetbrains.kotlin.container.useImpl
 import org.jetbrains.kotlin.container.useInstance
-import org.jetbrains.kotlin.idea.caches.lightClasses.LazyLightClassDataHolder
 import org.jetbrains.kotlin.idea.compiler.IdeMainFunctionDetectorFactory
 import org.jetbrains.kotlin.resolve.TargetEnvironment
 
@@ -15,7 +14,6 @@ object IdeaEnvironment : TargetEnvironment("Idea") {
         container.useImpl<ResolveElementCache>()
         container.useImpl<IdeaLocalDescriptorResolver>()
         container.useImpl<IdeaAbsentDescriptorHandler>()
-        container.useImpl<LazyLightClassDataHolder.DiagnosticsHolder>()
         container.useImpl<IdeaModuleStructureOracle>()
         container.useImpl<IdeMainFunctionDetectorFactory>()
         container.useInstance(ControlFlowInformationProviderImpl.Factory)
