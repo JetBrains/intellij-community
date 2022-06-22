343e8fb10ebd906e49a13f7d15e22651cc39280f
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-06-22 12:58:28 +0200

    [kotlin] Update versions in Kotlin wizard
    GRADLE_VERSION_FOR_COMPOSE -> GRADLE_VERSION happened because of limitation of
    our test engine. That is happening in cases where gradle version for
    Compose and usual gradle version are equal.
    
    Co-author: Margarita Bobova <margarita.bobova@jetbrains.com>
    (cherry picked from commit f102736646075ab798571565b320113e5a5d1720)
    
    GitOrigin-RevId: 746b25ffd495cbb8edc6e15481fbb2d28f31e212

--- plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/fullStackWebApplication/build.gradle.kts
+++ plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/fullStackWebApplication/build.gradle.kts
@@ -39,17 +39,17 @@ kotlin {
         }
         val jvmMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-server-netty:2.0.1")
-                implementation("io.ktor:ktor-server-html-builder-jvm:2.0.1")
+                implementation("io.ktor:ktor-server-netty:2.0.2")
+                implementation("io.ktor:ktor-server-html-builder-jvm:2.0.2")
                 implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
             }
         }
         val jvmTest by getting
         val jsMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlin-wrappers:kotlin-react:18.0.0-pre.332-kotlin-1.6.21")
-                implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:18.0.0-pre.332-kotlin-1.6.21")
-                implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.9.0-pre.332-kotlin-1.6.21")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react:18.2.0-pre.346")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:18.2.0-pre.346")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.9.3-pre.346")
             }
         }
         val jsTest by getting
