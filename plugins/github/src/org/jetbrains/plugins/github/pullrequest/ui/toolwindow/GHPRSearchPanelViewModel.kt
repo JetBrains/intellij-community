666566aba5d35450dd707baa99fa5747b634dfe9
Ivan Semenov <ivan.semenov@jetbrains.com>
2022-06-23 00:20:21 +0300

    [github] submit on enter instead of debounce
    GitOrigin-RevId: fe2a41879dd550dc25c801115b8af350c17efaee

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/toolwindow/GHPRSearchPanelViewModel.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/toolwindow/GHPRSearchPanelViewModel.kt
@@ -1,19 +1,16 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.pullrequest.ui.toolwindow
 
-import com.intellij.collaboration.ui.codereview.list.search.*
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 internal class GHPRSearchPanelViewModel(private val scope: CoroutineScope,
                                         searchState: MutableStateFlow<GHPRListSearchValue>) {
 
-  val queryState = searchState.partialState(500L, GHPRListSearchValue::searchQuery) {
+  val queryState = searchState.partialState(GHPRListSearchValue::searchQuery) {
     copy(searchQuery = it)
   }
 
@@ -39,12 +36,6 @@ internal class GHPRSearchPanelViewModel(private val scope: CoroutineScope,
 
   private fun <T> MutableStateFlow<GHPRListSearchValue>.partialState(getter: (GHPRListSearchValue) -> T,
                                                                      updater: GHPRListSearchValue.(T?) -> GHPRListSearchValue)
-    : MutableStateFlow<T?> = partialState(null, getter, updater)
-
-  @OptIn(FlowPreview::class)
-  private fun <T> MutableStateFlow<GHPRListSearchValue>.partialState(debounce: Long? = null,
-                                                                     getter: (GHPRListSearchValue) -> T,
-                                                                     updater: GHPRListSearchValue.(T?) -> GHPRListSearchValue)
     : MutableStateFlow<T?> {
 
     val filterState = MutableStateFlow<T?>(null)
@@ -54,12 +45,7 @@ internal class GHPRSearchPanelViewModel(private val scope: CoroutineScope,
       }
     }
     scope.launch {
-      if (debounce != null) {
-        filterState.debounce(debounce)
-      }
-      else {
-        filterState
-      }.collectLatest { value ->
+      filterState.collectLatest { value ->
         update { updater(it, value) }
       }
     }
