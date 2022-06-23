d09ebb41dc6090de861982bd5f8725fcb6606cb4
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-06-23 13:15:15 +0300

    [gradle] Avoid using models for storing version catalogs information
    (cherry-picked from b07fcb9e2b527c972503d69d43be08cf8854357a)
    
    (cherry picked from commit a0e9fa5e7497700cf59b847e7206c3f008964c90)
    
    IJ-MR-27989
    
    GitOrigin-RevId: 41fb82301598ae9c85c5465bf11692a3d9d6daf9

--- plugins/gradle/java/src/service/navigation/GradleVersionCatalogTomlAwareGotoDeclarationHandler.kt
+++ plugins/gradle/java/src/service/navigation/GradleVersionCatalogTomlAwareGotoDeclarationHandler.kt
@@ -3,22 +3,22 @@ package org.jetbrains.plugins.gradle.service.navigation
 
 import com.intellij.codeInsight.navigation.actions.GotoDeclarationHandler
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.externalSystem.service.project.ProjectDataManager
-import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiMethod
+import com.intellij.psi.util.PsiUtilCore
 import com.intellij.psi.util.parentOfType
 import com.intellij.util.castSafelyTo
 import com.intellij.util.containers.tail
-import org.jetbrains.plugins.gradle.model.data.VersionCatalogTomlFilesModel
+import org.jetbrains.plugins.gradle.service.project.getVersionCatalogsData
 import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames
 import org.jetbrains.plugins.gradle.service.resolve.GradleExtensionProperty
+import org.jetbrains.plugins.gradle.service.resolve.getRootGradleProjectPath
 import org.jetbrains.plugins.gradle.util.GradleConstants
 import org.jetbrains.plugins.groovy.intentions.style.inference.resolve
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFileBase
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression
 import org.jetbrains.plugins.groovy.lang.psi.util.GroovyPropertyUtils
 import org.toml.lang.psi.TomlFile
@@ -35,37 +35,35 @@ class GradleVersionCatalogTomlAwareGotoDeclarationHandler : GotoDeclarationHandl
     }
     val resolved = sourceElement.parentOfType<GrReferenceExpression>()?.resolve()
     if (resolved is GradleExtensionProperty && resolved.name == "libs") {
-      val toml = findTomlFile(sourceElement.project, resolved.name)
+      val toml = findTomlFile(sourceElement, resolved.name)
       if (toml != null) {
         return arrayOf(toml)
       }
     }
     if (resolved is PsiMethod) {
-      return resolved.resolveInToml()?.let { arrayOf(it) }
+      return resolved.resolveInToml(sourceElement)?.let { arrayOf(it) }
     }
     return null
   }
 }
 
-private fun findTomlFile(project: Project, name: String): TomlFile? {
-  val projectData = ProjectDataManager.getInstance().getExternalProjectsData(project,
-                                                                             GradleConstants.SYSTEM_ID).mapNotNull { it.externalProjectStructure }
-  for (projectDatum in projectData) {
-    val tomlRegistry = ExternalSystemApiUtil.find(projectDatum, VersionCatalogTomlFilesModel.KEY)?.data?.tomlFileRegistry ?: continue
-    val toml = tomlRegistry[name] ?: continue
-    return VfsUtil.findFile(Path.of(toml), false)?.let { PsiManager.getInstance(project).findFile(it) }?.castSafelyTo<TomlFile>() ?: continue
-  }
-  return null
+private fun findTomlFile(context: PsiElement, name: String): TomlFile? {
+  val path = context.getRootGradleProjectPath() ?: return null
+  val settingsFile = Path.of(path, GradleConstants.SETTINGS_FILE_NAME)
+  val settingsPsi = VfsUtil.findFile(settingsFile, false)?.let { PsiUtilCore.getPsiFile(context.project, it) }.castSafelyTo<GroovyFileBase>() ?: return null
+  val tomlRegistry = settingsPsi.getVersionCatalogsData()
+  val toml = tomlRegistry[name] ?: return null
+  return VfsUtil.findFile(toml, false)?.let { PsiManager.getInstance(context.project).findFile(it) }?.castSafelyTo<TomlFile>()
 }
 
-private fun PsiMethod.resolveInToml(): PsiElement? {
+private fun PsiMethod.resolveInToml(sourceElement: PsiElement): PsiElement? {
   val containingClasses = mutableListOf(containingClass ?: return null)
   while (containingClasses.last().containingClass != null) {
     containingClasses.add(containingClasses.last().containingClass!!)
   }
   containingClasses.reverse()
   val name = containingClasses.first().name?.substringAfter(LIBRARIES_FOR_PREFIX) ?: return null
-  val toml = listOf(GroovyPropertyUtils.decapitalize(name), name).firstNotNullOfOrNull { findTomlFile(project, it) }
+  val toml = listOf(GroovyPropertyUtils.decapitalize(name), name).firstNotNullOfOrNull { findTomlFile(sourceElement, it) }
              ?: return null
   val tomlVisitor = TomlVersionCatalogVisitor(containingClasses.tail(), this)
   toml.accept(tomlVisitor)
--- plugins/gradle/java/src/service/resolve/util.kt
+++ plugins/gradle/java/src/service/resolve/util.kt
@@ -1,9 +1,12 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.service.resolve
 
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.util.Key
 import com.intellij.patterns.PatternCondition
 import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.util.ProcessingContext
 import org.jetbrains.plugins.gradle.util.GradleConstants.EXTENSION
@@ -29,3 +32,15 @@ val DELEGATED_TYPE: Key<Boolean> = Key.create("gradle.delegated.type")
 internal fun PsiClass?.isResolvedInGradleScript() = this is GroovyScriptClass && this.containingFile.isGradleScript()
 
 internal fun PsiFile?.isGradleScript() = this?.originalFile?.virtualFile?.extension == EXTENSION
+
+private val PsiElement.module get() = containingFile?.originalFile?.virtualFile?.let {
+  ProjectFileIndex.getInstance(project).getModuleForFile(it)
+}
+
+internal fun PsiElement.getLinkedGradleProjectPath() : String? {
+  return ExternalSystemApiUtil.getExternalProjectPath(module)
+}
+
+internal fun PsiElement.getRootGradleProjectPath() : String? {
+  return ExternalSystemApiUtil.getExternalRootProjectPath(module)
+}
