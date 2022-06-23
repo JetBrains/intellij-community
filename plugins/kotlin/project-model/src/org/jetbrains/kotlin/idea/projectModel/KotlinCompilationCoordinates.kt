c20fd2c2ab1b36199e58b5e978f3cccf01c52425
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2022-06-23 17:32:30 +0200

    Revert "[Kotlin][Import] populateModuleDependenciesByPlatformPropagation: Include associate compilations"
    This reverts commit e14e5c7b56c5203848cfbf8f84f83981f8ba8528.
    
    (cherry picked from commit 44e9e09834e15cab16278db48611c60697a54291)
    
    IJ-CR-26268
    
    GitOrigin-RevId: 9bf6873fb0246aac3cd92750a26e5e4be6db9c55

--- plugins/kotlin/project-model/src/org/jetbrains/kotlin/idea/projectModel/KotlinCompilationCoordinates.kt
+++
@@ -1,9 +0,0 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.projectModel
-
-import java.io.Serializable
-
-interface KotlinCompilationCoordinates : Serializable {
-    val targetName: String
-    val compilationName: String
-}
