2e38feccf987de81b46270fe0dcd7dec21e70b60
Yann Cébron <yann.cebron@jetbrains.com>
2022-06-23 16:30:13 +0200

    JsonSchemaFileProvider: javadoc
    GitOrigin-RevId: e1705540a36c002bbc07210b30bb11c5f6f4057a

--- json/src/com/jetbrains/jsonSchema/extension/JsonSchemaFileProvider.java
+++ json/src/com/jetbrains/jsonSchema/extension/JsonSchemaFileProvider.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.jsonSchema.extension;
 
 
@@ -16,6 +17,9 @@ public interface JsonSchemaFileProvider {
   @Nls
   String getName();
 
+  /**
+   * @see JsonSchemaProviderFactory#getResourceFile(Class, String)
+   */
   @Nullable
   VirtualFile getSchemaFile();
 
