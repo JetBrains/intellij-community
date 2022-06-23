d92b0436c2ceb4c0ebf1cc8a50c87a9523160a18
Nikita Bobko <nikita.bobko@jetbrains.com>
2022-06-23 17:08:13 +0200

    Remove all DefaultIdeTargetPlatformKindProvider usages
    KMA-455 should be fixed
    
    DefaultIdeTargetPlatformKindProvider is useless because
    `DefaultIdeTargetPlatformKindProvider.defaultPlatform` implementation
    always returns `JvmPlatforms.defaultJvmPlatform`
    
    I will also drop DefaultIdeTargetPlatformKindProvider in the Kotlin repo
    sources a bit later once kt-master & master merged
    
    (cherry picked from commit a55a62106e40f8545d654d5519576fa4e732d0fd)
    
    GitOrigin-RevId: 816a88efaff2f475ddb851533783371ca40f7550

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/project/Platform.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/project/Platform.kt
@@ -118,7 +118,7 @@ fun Project.getLanguageVersionSettings(
     val compilerSettings = KotlinCompilerSettings.getInstance(this).settings
 
     val additionalArguments: CommonCompilerArguments = parseArguments(
-        DefaultIdeTargetPlatformKindProvider.defaultPlatform,
+        JvmPlatforms.defaultJvmPlatform,
         compilerSettings.additionalArgumentsAsList
     )
 
