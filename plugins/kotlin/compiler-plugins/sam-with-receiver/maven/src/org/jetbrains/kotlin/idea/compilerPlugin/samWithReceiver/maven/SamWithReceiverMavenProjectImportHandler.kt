f043d5a39a7604d498020558ceb2fc0960056659
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-06-23 13:35:46 +0200

    [Kotlin] Fix compilation of SamWithReceiver compiler plugin support
    (cherry picked from commit 067cd7054dfaccc0bf6ecce2a31aabeeca1fcb59)
    
    GitOrigin-RevId: 6de988d3447e09c3b9221d06e6a19b52cb087324

--- plugins/kotlin/compiler-plugins/sam-with-receiver/maven/src/org/jetbrains/kotlin/idea/compilerPlugin/samWithReceiver/maven/SamWithReceiverMavenProjectImportHandler.kt
+++ plugins/kotlin/compiler-plugins/sam-with-receiver/maven/src/org/jetbrains/kotlin/idea/compilerPlugin/samWithReceiver/maven/SamWithReceiverMavenProjectImportHandler.kt
@@ -6,14 +6,14 @@ import org.jetbrains.idea.maven.project.MavenProject
 import org.jetbrains.kotlin.idea.maven.compilerPlugin.AbstractMavenImportHandler
 import org.jetbrains.kotlin.idea.compilerPlugin.CompilerPluginSetup.PluginOption
 import org.jetbrains.kotlin.idea.artifacts.KotlinArtifacts
-import org.jetbrains.kotlin.samWithReceiver.SamWithReceiverCommandLineProcessor
+import org.jetbrains.kotlin.samWithReceiver.SamWithReceiverPluginNames
 
 class SamWithReceiverMavenProjectImportHandler : AbstractMavenImportHandler() {
     private companion object {
-        val ANNOTATION_PARAMETER_PREFIX = "sam-with-receiver:${SamWithReceiverCommandLineProcessor.ANNOTATION_OPTION.optionName}="
+        val ANNOTATION_PARAMETER_PREFIX = "sam-with-receiver:${SamWithReceiverPluginNames.ANNOTATION_OPTION_NAME}="
     }
 
-    override val compilerPluginId = SamWithReceiverCommandLineProcessor.PLUGIN_ID
+    override val compilerPluginId = SamWithReceiverPluginNames.PLUGIN_ID
     override val pluginName = "samWithReceiver"
     override val mavenPluginArtifactName = "kotlin-maven-sam-with-receiver"
     override val pluginJarFileFromIdea = KotlinArtifacts.instance.samWithReceiverCompilerPlugin
@@ -29,7 +29,7 @@ class SamWithReceiverMavenProjectImportHandler : AbstractMavenImportHandler() {
 
         val annotations = mutableListOf<String>()
 
-        for ((presetName, presetAnnotations) in SamWithReceiverCommandLineProcessor.SUPPORTED_PRESETS) {
+        for ((presetName, presetAnnotations) in SamWithReceiverPluginNames.SUPPORTED_PRESETS) {
             if (presetName in enabledCompilerPlugins) {
                 annotations.addAll(presetAnnotations)
             }
@@ -40,6 +40,6 @@ class SamWithReceiverMavenProjectImportHandler : AbstractMavenImportHandler() {
             text.substring(ANNOTATION_PARAMETER_PREFIX.length)
         })
 
-        return annotations.map { PluginOption(SamWithReceiverCommandLineProcessor.ANNOTATION_OPTION.optionName, it) }
+        return annotations.map { PluginOption(SamWithReceiverPluginNames.ANNOTATION_OPTION_NAME, it) }
     }
 }
