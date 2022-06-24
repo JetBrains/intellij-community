06b207e8b3ced923e1401b90753cf52e4085dd30
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2022-06-24 22:22:24 +0900

    [kotlin] Split test generator into FE10 and FIR parts (KTIJ-21699)
    Tests of modules reused between frontends are currently configured in the FE10 part. As number of such shared modules increases, it will possibly make sense to move them out.
    
    GitOrigin-RevId: f08cc12e280bd3b039a3ad5825b7de28a33a58b5

--- plugins/kotlin/util/test-generator/test/org/jetbrains/kotlin/testGenerator/AllTestsGeneratedTest.kt
+++
@@ -1,8 +0,0 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.testGenerator
-
-import junit.framework.TestCase
-
-class AllTestsGeneratedTest : TestCase() {
-    fun testAllTestsIsUpToDate(): Unit = generateTests(isUpToDateCheck = true)
-}
