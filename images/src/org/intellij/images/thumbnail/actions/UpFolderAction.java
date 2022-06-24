fab143271b4c7909527e398d241300bd10f164de
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2022-06-24 11:19:46 +0300

    action-update-thread: images actions
    (cherry picked from commit 0ce3f51758f6bf68463266923c722a7f4f45b903)
    
    IJ-CR-26247
    
    GitOrigin-RevId: 5dce897ce9b35d2470c27d88350ba85dead4bf55

--- images/src/org/intellij/images/thumbnail/actions/UpFolderAction.java
+++ images/src/org/intellij/images/thumbnail/actions/UpFolderAction.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.intellij.images.thumbnail.actions;
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -35,5 +36,10 @@ public final class UpFolderAction extends AnAction {
             VirtualFile root = view.getRoot();
             e.getPresentation().setEnabled(root != null && root.getParent() != null && !view.isRecursive());
         }
+    }
+
+    @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+      return ActionUpdateThread.EDT;
     }
 }
