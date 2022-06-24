d93ca1deca0346b462985a972de95aaa090e5696
Vladimir.Petrenko <vladimir.petrenko@jetbrains.com>
2022-06-24 16:21:20 +0300

    [core] editors: introduce HIDE_OTHER_EDITORS policy for FileEditorProvider-s
    This can be useful for plugin-writers.
    
    Example (based on real user request):
    There is an SVG-image file. At least two FileEditorProvider-s can open it:
    - TextEditorProvider (implements DefaultPlatformFileEditorProvider)
    - ImageFileEditorProvider (getPolicy() = HIDE_DEFAULT_EDITOR)
    ImageFileEditorProvider "suppresses" TextEditorProvider because of policy settings, and it is also the reason why ImageFileEditorProvider can't implement DefaultPlatformFileEditorProvider interface.
    Plugin-writer wants to provide his own FileEditorProvider for SVG-image files and to hide the existing ones. He can't use HIDE_DEFAULT_EDITOR policy. This is where the new policy HIDE_OTHER_EDITORS will help him.
    
    (cherry picked from commit c5fe3e611ee0052e23142c67b45216c7de5f6e6c)
    
    IJ-CR-26350
    
    GitOrigin-RevId: fdc4ac46a3c36a6b6a38f2cbfe4904cb380f6e14

--- platform/analysis-api/src/com/intellij/openapi/fileEditor/FileEditorPolicy.java
+++ platform/analysis-api/src/com/intellij/openapi/fileEditor/FileEditorPolicy.java
@@ -1,6 +1,8 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.fileEditor;
 
+import org.jetbrains.annotations.ApiStatus;
+
 public enum FileEditorPolicy {
 
   /**
@@ -18,6 +20,18 @@ public enum FileEditorPolicy {
    * Do not create default IDE editor (if any) for the file.
    */
   HIDE_DEFAULT_EDITOR,
+
+  /**
+   * Hide other editors (if any) for the file.
+   * If several instances of {@code FileEditorProvider} with such policy exist for the file,
+   * then the editors related to such instances of {@code FileEditorProvider} will be created,
+   * but the editors for other instances (if any) of {@code FileEditorProvider} will not.
+   *
+   * @see FileEditorProvider
+   * @see FileEditorProvider#getPolicy()
+   */
+  @ApiStatus.Experimental
+  HIDE_OTHER_EDITORS,
 
   /**
    * Place created editor after the default IDE editor (if any).
