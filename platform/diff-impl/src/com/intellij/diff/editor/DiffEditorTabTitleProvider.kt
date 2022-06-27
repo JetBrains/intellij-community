b7f264fee972afc19e67ab56803239ed03192308
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2022-06-27 16:31:11 +0200

    replace two last usages of `EdtExecutorService#submit` with coroutines
    GitOrigin-RevId: f140b5231edf1e6606ca93cf31e03236d67cb45d

--- platform/diff-impl/src/com/intellij/diff/editor/DiffEditorTabTitleProvider.kt
+++ platform/diff-impl/src/com/intellij/diff/editor/DiffEditorTabTitleProvider.kt
@@ -1,20 +1,23 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.diff.editor
 
 import com.intellij.diff.editor.DiffEditorTabFilesManager.Companion.isDiffOpenedInNewWindow
+import com.intellij.openapi.application.EDT
 import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.asContextElement
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.impl.EditorTabTitleProvider
-import com.intellij.openapi.progress.util.ProgressIndicatorUtils
+import com.intellij.openapi.progress.blockingContext
+import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsContexts
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.util.concurrency.EdtExecutorService
 import com.intellij.util.ui.EDT
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.jetbrains.annotations.Nls
-import java.util.function.Supplier
 
 class DiffEditorTabTitleProvider : EditorTabTitleProvider, DumbAware {
 
@@ -30,15 +33,20 @@ class DiffEditorTabTitleProvider : EditorTabTitleProvider, DumbAware {
 
   private fun getTitle(project: Project, file: VirtualFile): @Nls String? {
     if (file !is ChainDiffVirtualFile) return null
-    val supplier = Supplier<@NlsContexts.TabTitle String> {
+    val supplier = {
       FileEditorManager.getInstance(project)
         .getSelectedEditor(file)
         ?.let { it as? DiffRequestProcessorEditor }
         ?.processor?.activeRequest?.title
     }
-    if (EDT.isCurrentThreadEdt()) return supplier.get()
-    val future = EdtExecutorService.getInstance().submit(supplier::get, ModalityState.any())
-    return ProgressIndicatorUtils.awaitWithCheckCanceled(future)
+    if (EDT.isCurrentThreadEdt()) {
+      return supplier()
+    }
+    return runBlockingCancellable {
+      withContext(Dispatchers.EDT + ModalityState.any().asContextElement()) {
+        blockingContext(supplier)
+      }
+    }
   }
 
   private fun String.shorten(maxLength: Int = 30): @Nls String {
