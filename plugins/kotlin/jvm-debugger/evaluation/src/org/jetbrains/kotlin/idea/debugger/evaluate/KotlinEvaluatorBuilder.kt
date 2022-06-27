421b26e4ff030013d62df6de73abb66a4044e82c
Kristoffer Andersen <kjaa@google.com>
2022-06-27 15:47:26 +0200

    [KTIJ-21854] Order the composite binding context with the fragment at the bottom
    closes https://github.com/JetBrains/intellij-community/pull/2077
    
    (cherry picked from commit 783417170e85fcde7d6da534a851bea603aac791)
    
    IJ-MR-27414
    
    GitOrigin-RevId: 752fdc6f4f23b66a91ba819eb45fbb084f2a5869

--- plugins/kotlin/jvm-debugger/evaluation/src/org/jetbrains/kotlin/idea/debugger/evaluate/KotlinEvaluatorBuilder.kt
+++ plugins/kotlin/jvm-debugger/evaluation/src/org/jetbrains/kotlin/idea/debugger/evaluate/KotlinEvaluatorBuilder.kt
@@ -236,10 +236,38 @@ class KotlinEvaluator(val codeFragment: KtCodeFragment, private val sourcePositi
                         analysisResult.bindingContext
                     ).second
                 } else {
+                    // The IR Evaluator is sensitive to the analysis order of files in fragment compilation:
+                    // The codeFragment must be passed _last_ to analysis such that the result is stacked at
+                    // the _bottom_ of the composite analysis result.
+                    //
+                    // The situation as seen from here is as follows:
+                    //   1) `analyzeWithAllCompilerChecks` analyze each individual file passed to it separately.
+                    //   2) The individual results are "stacked" on top of each other.
+                    //   3) With distinct files, "stacking on top" is equivalent to "side by side" - there is
+                    //      no overlap in what is analyzed, so the order doesn't matter: the composite analysis
+                    //      result is just a look-up mechanism for convenience.
+                    //   4) Code Fragments perform partial analysis of the context of the fragment, e.g. a
+                    //      breakpoint in a function causes partial analysis of the surrounding function.
+                    //   5) If the surrounding function is _also_ included in the `filesToCompile`, that
+                    //      function will be analyzed more than once: in particular, fresh symbols will be
+                    //      allocated anew upon repeated analysis.
+                    //   6) Now the order of composition is significant: layering the fragment at the bottom
+                    //      ensures code that needs a consistent view of the entire function (i.e. psi2ir)
+                    //      does not mix the fresh, partial view of the function in the fragment analysis with
+                    //      the complete analysis from the separate analysis of the entire file included in the
+                    //      compilation.
+                    //
+                    fun <T> MutableList<T>.moveToLast(element: T) {
+                        removeAll(listOf(element))
+                        add(element)
+                    }
+
                     gatherProjectFilesDependedOnByFragment(
                         codeFragment,
                         analysisResult.bindingContext
-                    ).toList()
+                    ).toMutableList().apply {
+                        moveToLast(codeFragment)
+                    }
                 }
                 val analysis = resolutionFacade.analyzeWithAllCompilerChecks(filesToCompile)
                 Pair(analysis.bindingContext, filesToCompile)
