c9c2e6d43ea63120a9bb395ae9d09b9ae2f4c4e1
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-06-27 08:55:51 +0200

    action-update-thread: git
    GitOrigin-RevId: 14b49b91da29b10eb1e039c04cbcd6f5a41f6aa0

--- plugins/git4idea/src/git4idea/index/actions/GitCommitWithStagingAreaAction.kt
+++ plugins/git4idea/src/git4idea/index/actions/GitCommitWithStagingAreaAction.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.index.actions
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager
@@ -10,6 +11,10 @@ import git4idea.index.isStagingAreaAvailable
 import git4idea.index.showStagingArea
 
 class GitCommitWithStagingAreaAction : DumbAwareAction() {
+  override fun getActionUpdateThread(): ActionUpdateThread {
+    return ActionUpdateThread.EDT
+  }
+
   override fun update(e: AnActionEvent) {
     val project = e.project
     if (project == null) {
@@ -17,9 +22,9 @@ class GitCommitWithStagingAreaAction : DumbAwareAction() {
       return
     }
 
-    e.presentation.isEnabledAndVisible = ChangesViewContentManager.getToolWindowFor(project,
-                                                                                    GitStageContentProvider.STAGING_AREA_TAB_NAME) != null
-                                         && isStagingAreaAvailable(project)
+    e.presentation.isEnabledAndVisible =
+      ChangesViewContentManager.getToolWindowFor(project, GitStageContentProvider.STAGING_AREA_TAB_NAME) != null
+      && isStagingAreaAvailable(project)
   }
 
   override fun actionPerformed(e: AnActionEvent) {
