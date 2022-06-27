3d04ed373b503e13a13e6de254e37742da9560ba
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2022-06-27 16:08:53 +0200

    convert `VcsEditorTabTitleProvider` to kt
    GitOrigin-RevId: 8ead0464e7a4d1583b177364d4796e934bdc6f8d

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/VcsEditorTabTitleProvider.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/VcsEditorTabTitleProvider.kt
@@ -1,67 +1,54 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.openapi.vcs.changes;
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.vcs.changes
 
-import com.intellij.diff.editor.DiffRequestProcessorEditor;
-import com.intellij.diff.impl.DiffRequestProcessor;
-import com.intellij.diff.tools.combined.CombinedDiffModel;
-import com.intellij.diff.tools.combined.CombinedDiffModelRepository;
-import com.intellij.openapi.application.ModalityState;
-import com.intellij.openapi.fileEditor.FileEditor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.impl.EditorTabTitleProvider;
-import com.intellij.openapi.progress.util.ProgressIndicatorUtils;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.NlsContexts;
-import com.intellij.openapi.vcs.changes.actions.diff.CombinedDiffPreviewVirtualFile;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.concurrency.EdtExecutorService;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.ui.EDT;
-import kotlin.jvm.functions.Function0;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.diff.editor.DiffRequestProcessorEditor
+import com.intellij.diff.tools.combined.CombinedDiffModelRepository
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.components.service
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.impl.EditorTabTitleProvider
+import com.intellij.openapi.progress.util.ProgressIndicatorUtils
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.NlsContexts.TabTitle
+import com.intellij.openapi.vcs.changes.actions.diff.COMBINED_DIFF_PREVIEW_TAB_NAME
+import com.intellij.openapi.vcs.changes.actions.diff.CombinedDiffPreviewVirtualFile
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.concurrency.EdtExecutorService
+import com.intellij.util.containers.ContainerUtil
+import com.intellij.util.ui.EDT
 
-import java.util.concurrent.Future;
-import java.util.function.Supplier;
+class VcsEditorTabTitleProvider : EditorTabTitleProvider, DumbAware {
 
-import static com.intellij.openapi.vcs.changes.actions.diff.CombinedDiffPreviewKt.COMBINED_DIFF_PREVIEW_TAB_NAME;
-
-public class VcsEditorTabTitleProvider implements EditorTabTitleProvider, DumbAware {
-
-  @Nullable
-  @Override
-  public String getEditorTabTitle(@NotNull Project project, @NotNull VirtualFile file) {
-    return getEditorTabName(project, file);
+  override fun getEditorTabTitle(project: Project, file: VirtualFile): String? {
+    return getEditorTabName(project, file)
   }
 
-  @Nullable
-  @Override
-  public String getEditorTabTooltipText(@NotNull Project project, @NotNull VirtualFile virtualFile) {
-    return getEditorTabName(project, virtualFile);
+  override fun getEditorTabTooltipText(project: Project, virtualFile: VirtualFile): String? {
+    return getEditorTabName(project, virtualFile)
   }
 
-  @Nullable
-  @NlsContexts.TabTitle
-  private static String getEditorTabName(@NotNull Project project, @NotNull VirtualFile file) {
-    if (!(file instanceof PreviewDiffVirtualFile) && !(file instanceof CombinedDiffPreviewVirtualFile)) return null;
-    Supplier<@NlsContexts.TabTitle String> supplier = () -> {
-      FileEditor[] editors = FileEditorManager.getInstance(project).getEditors(file);
-      DiffRequestProcessorEditor editor = ContainerUtil.findInstance(editors, DiffRequestProcessorEditor.class);
-      DiffRequestProcessor processor = editor != null ? editor.getProcessor() : null;
-      if (file instanceof PreviewDiffVirtualFile) {
-        return ((PreviewDiffVirtualFile)file).getProvider().getEditorTabName(processor);
+  private fun getEditorTabName(project: Project, file: VirtualFile): @TabTitle String? {
+    if (file !is PreviewDiffVirtualFile && file !is CombinedDiffPreviewVirtualFile) {
+      return null
+    }
+    val supplier = {
+      val editors = FileEditorManager.getInstance(project).getEditors(file)
+      val editor = ContainerUtil.findInstance(editors, DiffRequestProcessorEditor::class.java)
+      val processor = editor?.processor
+      if (file is PreviewDiffVirtualFile) {
+        file.provider.getEditorTabName(processor)
       }
       else {
-        String sourceId = ((CombinedDiffPreviewVirtualFile)file).getSourceId();
-        CombinedDiffModel diffModel = project.getService(CombinedDiffModelRepository.class).findModel(sourceId);
-        Function0<@NlsContexts.TabTitle String> tabNameSupplier =
-          diffModel != null ? diffModel.getContext().getUserData(COMBINED_DIFF_PREVIEW_TAB_NAME) : null;
-        return tabNameSupplier != null ? tabNameSupplier.invoke() : null;
+        val sourceId = (file as CombinedDiffPreviewVirtualFile).sourceId
+        val diffModel = project.service<CombinedDiffModelRepository>().findModel(sourceId)
+        diffModel?.context?.getUserData(COMBINED_DIFF_PREVIEW_TAB_NAME)?.invoke()
       }
-    };
-    if (EDT.isCurrentThreadEdt()) return supplier.get();
-    Future<@NlsContexts.TabTitle String> future = EdtExecutorService.getInstance().submit(supplier::get, ModalityState.any());
-    return ProgressIndicatorUtils.awaitWithCheckCanceled(future);
+    }
+    if (EDT.isCurrentThreadEdt()) {
+      return supplier()
+    }
+    val future = EdtExecutorService.getInstance().submit(supplier, ModalityState.any())
+    return ProgressIndicatorUtils.awaitWithCheckCanceled(future)
   }
 }
