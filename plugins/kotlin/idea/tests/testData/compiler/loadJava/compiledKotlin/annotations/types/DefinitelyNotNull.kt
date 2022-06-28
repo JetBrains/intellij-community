ed2f0cb15aa05be31975e61e70f5e904f4c4cd0b
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-06-28 18:00:40 +0200

    [kotlin][light classes] drop redundant testData
    ^KT-48773
    
    (cherry picked from commit 0e8b8a083b8ffe58b5be9bd3ca439933ca1a262d)
    
    GitOrigin-RevId: 3f3d0dd67dbb71749f744296b25cb3e9aa4314bc

--- plugins/kotlin/idea/tests/testData/compiler/loadJava/compiledKotlin/annotations/types/DefinitelyNotNull.kt
+++
@@ -1,5 +0,0 @@
-// !LANGUAGE: +DefinitelyNotNullTypeParameters
-// COMPILER_ARGUMENTS: -XXLanguage:+DefinitelyNotNullTypeParameters
-package test
-
-fun <T> foo(x: T!!, y: List<T!!>, z: (T!!) -> T!!): T!! = x
