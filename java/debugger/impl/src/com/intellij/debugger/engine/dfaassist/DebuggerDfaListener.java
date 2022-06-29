a3af40138af4a523111ba6059580513014c5ef59
Tagir Valeev <tagir.valeev@jetbrains.com>
2022-06-29 17:15:14 +0200

    [debugger-dfa] JavaDoc for unreachableSegments
    GitOrigin-RevId: 540ff5687483db0d090e7bfd38468b35f9d99a32

--- java/debugger/impl/src/com/intellij/debugger/engine/dfaassist/DebuggerDfaListener.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/dfaassist/DebuggerDfaListener.java
@@ -2,6 +2,7 @@
 package com.intellij.debugger.engine.dfaassist;
 
 import com.intellij.codeInspection.dataFlow.lang.DfaListener;
+import com.intellij.codeInspection.dataFlow.lang.ir.ControlFlow;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
@@ -19,6 +20,14 @@ public interface DebuggerDfaListener extends DfaListener {
    */
   @NotNull Map<PsiElement, DfaHint> computeHints();
 
+  /**
+   * @param startAnchor an anchor returned from {@link DfaAssistProvider#getAnchor(PsiElement)}, which created this listener
+   * @param unreachableElements list of all {@link PsiElement} which were never visited during IR interpretation. This means that no
+   *                            instruction between {@link ControlFlow#startElement(PsiElement)} and
+   *                            {@link ControlFlow#finishElement(PsiElement)} was reached. Elements before startAnchor in IR are not
+   *                            considered unreachable and not added to this set.
+   * @return collection of text ranges to highlight as unreachable.
+   */
   default @NotNull Collection<TextRange> unreachableSegments(@NotNull PsiElement startAnchor, @NotNull Set<PsiElement> unreachableElements) {
     return Collections.emptyList();
   }
