148e88556708b92c64ea0683a02e0e9f8af89b4d
Dmitry Jemerov <yole@jetbrains.com>
2022-06-29 18:35:30 +0200

    Try to add diagnostics for missing line comment action (IDEA-283781)
    (cherry picked from commit 1214fd51328633477ef01b201dd2638c17a311c8)
    
    IJ-CR-26610
    
    GitOrigin-RevId: 2d1f71bb5f0044af82078e9651c450180545bcd3

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionManagerImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionManagerImpl.java
@@ -1353,6 +1353,12 @@ public class ActionManagerImpl extends ActionManagerEx implements Disposable {
         }
         return;
       }
+
+      // diagnostics for IDEA-283781
+      if (actionId.equals("CommentByLineComment")) {
+        LOG.info("Unregistering line comment action", new Throwable());
+      }
+
       AnAction actionToRemove = idToAction.remove(actionId);
       actionToId.remove(actionToRemove);
       idToIndex.removeInt(actionId);
