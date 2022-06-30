9d1cf8dea95e2cce922a9fb5e9223ed51e034c7a
Gregory.Shrago <gregory.shrago@jetbrains.com>
2022-06-30 11:44:51 +0300

    tweak common `operationName` and reuse it all places
    GitOrigin-RevId: ebaa97c5306cf04f758665f79ccb35436591e47d

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionUpdater.java
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionUpdater.java
@@ -149,7 +149,7 @@ final class ActionUpdater {
   }
 
   private <T> T callAction(@NotNull AnAction action, @NotNull Op operation, @NotNull Supplier<? extends T> call) {
-    String operationName = Utils.operationName(action, operation.name() + "@" + myPlace);
+    String operationName = Utils.operationName(action, operation.name(), myPlace);
     return callAction(operationName, action.getActionUpdateThread(), call);
   }
 
@@ -324,9 +324,8 @@ final class ActionUpdater {
     promise.onProcessed(__ -> {
       long edtWaitMillis = TimeUnit.NANOSECONDS.toMillis(myEDTWaitNanos);
       if (myLaterInvocator == null && (myEDTCallsCount > 500 || edtWaitMillis > 3000)) {
-        boolean noFqn = group.getClass() == DefaultActionGroup.class;
-        LOG.warn(edtWaitMillis + " ms total to grab EDT " + myEDTCallsCount + " times at '" + myPlace + "' to expand " +
-                 group.getClass().getSimpleName() + (noFqn ? "" : " (" + group.getClass().getName() + ")") + ". Use `ActionUpdateThread.BGT`.");
+        LOG.warn(edtWaitMillis + " ms total to grab EDT " + myEDTCallsCount + " times to expand " +
+                 Utils.operationName(group, null, myPlace) + ". Use `ActionUpdateThread.BGT`.");
       }
     });
 
@@ -632,10 +631,11 @@ final class ActionUpdater {
   private static void handleException(@NotNull Op op, @NotNull AnAction action, @Nullable AnActionEvent event, @NotNull Throwable ex) {
     if (ex instanceof ProcessCanceledException) throw (ProcessCanceledException)ex;
     String id = ActionManager.getInstance().getId(action);
+    String place = event == null ? null : event.getPlace();
     String text = event == null ? null : event.getPresentation().getText();
-    String message = op.name() + " failed for " + (action instanceof ActionGroup ? "ActionGroup" : "AnAction") +
-                     "(" + action.getClass().getName() + (id != null ? ", id=" + id : "") + ")" +
-                     (StringUtil.isNotEmpty(text) ? " with text=" + event.getPresentation().getText() : "");
+    String message = Utils.operationName(action, op.name(), place) +
+                     (id != null ? ", actionId=" + id : "") +
+                     (StringUtil.isNotEmpty(text) ? ", text='" + text + "'" : "");
     LOG.error(message, ex);
   }
 
