ed622b11de1e0566fdd590ec6d0d513e3545354a
Tagir Valeev <tagir.valeev@jetbrains.com>
2022-06-30 16:10:01 +0200

    [kotlin-dfa] Track object properties
    Fixes KTIJ-22136 Constant conditions: False positive "expression is always false when reached"
    
    GitOrigin-RevId: 9657210b686ee37b686ed90958592f7332f42a06

--- plugins/kotlin/idea/tests/testData/inspections/dfa/BoxedInt.kt
+++ plugins/kotlin/idea/tests/testData/inspections/dfa/BoxedInt.kt
@@ -1,4 +1,14 @@
 // WITH_STDLIB
+fun unboxProperty(value: Int): Boolean =
+    MyObj.test == null || MyObj.test >= value
+
+fun unboxProperty2(value: Int): Boolean =
+    MyObj.test == null || <warning descr="Condition 'MyObj.test >= value && value > 5 && MyObj.test < 5' is always false when reached">MyObj.test >= value && value > 5 && <warning descr="Condition 'MyObj.test < 5' is always false when reached">MyObj.test < 5</warning></warning>
+
+object MyObj {
+    val test: Int? = if (Math.random() > 0.5) null else 1
+}
+
 fun test(x : Int?) {
     if (x != null && x > 5) {}
     if (<warning descr="Condition 'x != null && x > 5 && x < 3' is always false">x != null && x > 5 && <warning descr="Condition 'x < 3' is always false when reached">x < 3</warning></warning>) {}
