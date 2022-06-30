5e104b68a09ebb72028ba84641177a5f0a0a44ee
Roman Ivanov <roman.ivanov@jetbrains.com>
2022-06-30 10:25:08 +0200

    [java] change wording in the error for outdated patterns
    (cherry picked from commit 5a0107985349b49236cae90c1ed82bda1aa43651)
    
    GitOrigin-RevId: 4b8691fcff83ea1173a379810ace738d16f29662

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlightingPatternsInSwitch/PatternsInSwitchIn19Java.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlightingPatternsInSwitch/PatternsInSwitchIn19Java.java
@@ -1,8 +1,8 @@
 class X {
   int switchTest(Object obj) {
     return switch (obj) {
-      case <error descr="Guarded patterns and parenthesized patterns are not available since Java 19 preview">(String s)</error> -> 1;
-      case <error descr="Guarded patterns and parenthesized patterns are not available since Java 19 preview">Integer i && predicate()</error> -> 2;
+      case <error descr="Old patterns from JEP 406 are not available since Java 19 preview">(String s)</error> -> 1;
+      case <error descr="Old patterns from JEP 406 are not available since Java 19 preview">Integer i && predicate()</error> -> 2;
       case Integer i -> 3;
       case default -> 4;
       case null -> 10;
@@ -10,10 +10,10 @@ class X {
   }
 
   int instanceofTest(Object obj) {
-    if (obj instanceof (<error descr="Guarded patterns and parenthesized patterns are not available since Java 19 preview">Integer i && predicate()</error>)) {
+    if (obj instanceof (<error descr="Old patterns from JEP 406 are not available since Java 19 preview">Integer i && predicate()</error>)) {
       return 1;
     }
-    if (obj instanceof <error descr="Guarded patterns and parenthesized patterns are not available since Java 19 preview">(String s)</error>) {
+    if (obj instanceof <error descr="Old patterns from JEP 406 are not available since Java 19 preview">(String s)</error>) {
       return 3;
     }
     return 2;
