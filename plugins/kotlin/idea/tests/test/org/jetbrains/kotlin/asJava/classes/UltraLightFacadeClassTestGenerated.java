3b782abe072f35da3087023f44d520be3fe0db2e
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-07-01 17:38:18 +0200

    [kotlin] Regenerate tests
    GitOrigin-RevId: 8e55f6a1f31e75afd5fb6af8178050a27aa940fa

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/asJava/classes/UltraLightFacadeClassTestGenerated.java
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/asJava/classes/UltraLightFacadeClassTestGenerated.java
@@ -80,6 +80,11 @@ public class UltraLightFacadeClassTestGenerated extends AbstractUltraLightFacade
         runTest(compilerTestData("compiler/testData/asJava/ultraLightFacades/properties.kt"));
     }
 
+    @TestMetadata("script.kts")
+    public void testScript() throws Exception {
+        runTest(compilerTestData("compiler/testData/asJava/ultraLightFacades/script.kts"));
+    }
+
     @TestMetadata("simpleFunctions.kt")
     public void testSimpleFunctions() throws Exception {
         runTest(compilerTestData("compiler/testData/asJava/ultraLightFacades/simpleFunctions.kt"));
