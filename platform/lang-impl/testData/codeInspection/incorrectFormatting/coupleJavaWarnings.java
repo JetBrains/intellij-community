e3304986a54a2c0ad3842ac773dcb39eeb193835
Vojtech Balik <vojtech.balik@jetbrains.com>
2022-07-01 12:14:18 +0200

    Incorrect formatting inspection: fix integration test and remove unused test-files
    GitOrigin-RevId: f54ac621d1221f76c88d3c852bc2f90b63647d01

--- platform/lang-impl/testData/codeInspection/incorrectFormatting/coupleJavaWarnings.java
+++
@@ -1,8 +0,0 @@
-class Bar {
-    public static void main(String[] args)<weak_warning descr="Incorrect whitespace">  </weak_warning>{
-        System.out.println("Hello, world");
-<weak_warning descr="Incorrect whitespace">
-
-
-    </weak_warning>}
-}
