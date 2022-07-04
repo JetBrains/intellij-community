33c912edfa89f26d3d9877e94ba6291efc2a816c
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2022-07-04 19:21:32 +0200

    IDEA-294758 Rescan only changes
    GitOrigin-RevId: fdcc81362295bf2489af72b838c3538e31c95498

--- java/execution/impl/src/com/intellij/execution/AlternativeSdkRootsProvider.kt
+++ java/execution/impl/src/com/intellij/execution/AlternativeSdkRootsProvider.kt
@@ -1,9 +1,10 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution
 
 import com.intellij.execution.configurations.ConfigurationWithAlternativeJre
 import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.RootsChangeRescanningInfo
 import com.intellij.openapi.projectRoots.ProjectJdkTable
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.projectRoots.impl.jdkDownloader.JdkUpdateCheckContributor
@@ -81,7 +82,7 @@ class AlternativeSdkRootsProvider : AdditionalLibraryRootsProvider() {
       if (update) {
         AppUIUtil.invokeOnEdt {
           WriteAction.run<RuntimeException> {
-            ProjectRootManagerEx.getInstanceEx(project).makeRootsChange(EmptyRunnable.getInstance(), false, true)
+            ProjectRootManagerEx.getInstanceEx(project).makeRootsChange(EmptyRunnable.getInstance(), RootsChangeRescanningInfo.RESCAN_DEPENDENCIES_IF_NEEDED)
           }
         }
       }
