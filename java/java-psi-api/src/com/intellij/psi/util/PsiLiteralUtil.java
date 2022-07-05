eea68829de0dfa888927bf06c002e441c2b4c46b
Bas Leijdekkers <basleijdekkers@gmail.com>
2022-07-05 15:04:45 +0200

    javadoc fix
    GitOrigin-RevId: c8a1f3690d8c525aa1de80853f42aa3363bc63d1

--- java/java-psi-api/src/com/intellij/psi/util/PsiLiteralUtil.java
+++ java/java-psi-api/src/com/intellij/psi/util/PsiLiteralUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.util;
 
 import com.intellij.openapi.util.TextRange;
@@ -198,6 +198,7 @@ public final class PsiLiteralUtil {
   /**
    * Converts given string to text block content.
    * <p>During conversion:</p>
+   * <ul>
    * <li>All escaped quotes are unescaped.</li>
    * <li>Every third quote is escaped. If escapeStartQuote / escapeEndQuote is set then start / end quote is also escaped.</li>
    * <li>All spaces before \n are converted to \040 escape sequence.
@@ -205,6 +206,7 @@ public final class PsiLiteralUtil {
    * If escapeSpacesInTheEnd is set, then all spaces before the end of the line are converted even if new line in the end is missing. </li>
    * <li> All new line escape sequences are interpreted. </li>
    * <li>Rest of the content is processed as is.</li>
+   * </ul>
    *
    * @param s                    original text
    * @param escapeStartQuote     true if first quote should be escaped (e.g. when copy-pasting into text block after two quotes)
