a604a86d34b1f894081207da38804a8cf13bc1d5
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2022-07-05 12:25:20 +0200

    [space] IDEA-297316: Unbundle Refactoring Detector plugin
    Review: IJ-CR-26794
    
    GitOrigin-RevId: 9ad607c463c804b9f51152e986b00dc7c5e6b2ed

--- build/groovy/org/jetbrains/intellij/build/BaseIdeaProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/BaseIdeaProperties.groovy
@@ -81,7 +81,6 @@ abstract class BaseIdeaProperties extends JetBrainsProductProperties {
     "intellij.ml.models.local",
     "intellij.sh",
     "intellij.vcs.changeReminder",
-    "intellij.vcs.refactoring.detector",
     "intellij.markdown",
     "intellij.webp",
     "intellij.grazie",
