ff4a0156f6382aade9241e301728eaae4b08a895
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2022-07-05 20:08:39 +0200

    [fus] fix collecting versions of JRE used for importing Maven/Gradle projects (IDEA-297440)
    Sdk::getVersionString usually includes vendor name or 'version' word, so we need to use the special method to parse it properly.
    
    GitOrigin-RevId: 48f7dcdc7349011482dea09a9ecef346804e43eb

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/statistics/ExternalSystemUsagesCollector.kt
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/statistics/ExternalSystemUsagesCollector.kt
@@ -3,7 +3,6 @@ package com.intellij.openapi.externalSystem.statistics
 
 import com.intellij.internal.statistic.StructuredIdeActivity
 import com.intellij.internal.statistic.beans.MetricEvent
-import com.intellij.internal.statistic.beans.newMetric
 import com.intellij.internal.statistic.eventLog.EventLogGroup
 import com.intellij.internal.statistic.eventLog.events.EventFields
 import com.intellij.internal.statistic.eventLog.events.EventPair
@@ -19,7 +18,7 @@ import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsSafe
-import com.intellij.openapi.util.Version
+import com.intellij.util.lang.JavaVersion
 
 class ExternalSystemUsagesCollector : ProjectUsagesCollector() {
   override fun getGroup(): EventLogGroup {
@@ -55,7 +54,7 @@ class ExternalSystemUsagesCollector : ProjectUsagesCollector() {
 
 
   companion object {
-    private val GROUP = EventLogGroup("build.tools", 2)
+    private val GROUP = EventLogGroup("build.tools", 3)
     private val EXTERNAL_SYSTEM_ID = GROUP.registerEvent("externalSystemId", EventFields.StringValidatedByEnum("value", "build_tools"))
     val JRE_TYPE_FIELD = EventFields.Enum("value", JreType::class.java) { it.description }
 
@@ -72,7 +71,7 @@ class ExternalSystemUsagesCollector : ProjectUsagesCollector() {
     fun getJreVersion(project: Project, jreName: String?): String {
       val jdk = ExternalSystemJdkUtil.getJdk(project, jreName)
       return jdk?.versionString?.let<@NlsSafe String, String?> {
-        Version.parseVersion(it)?.let { parsed -> "${parsed.major}.${parsed.minor}" }
+        JavaVersion.tryParse(it)?.let { parsed -> "${parsed.feature}.${parsed.minor}" }
       } ?: "unknown"
     }
 
