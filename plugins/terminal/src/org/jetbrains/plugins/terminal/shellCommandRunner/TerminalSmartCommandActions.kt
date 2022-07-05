ed27d73b4e008ec1c5ef35b9ecf92f70c79d9245
Liudmila Kornilova <kornilova203@gmail.com>
2022-07-05 14:36:41 +0200

    action-update-thread: mark TerminalExecutorAction as BGT
    (cherry picked from commit 920f3324bc81d546789c8536f9e2bc3b71b097be)
    
    GitOrigin-RevId: f11cd4da9b9dbfd784544580002c78f96566f930

--- plugins/terminal/src/org/jetbrains/plugins/terminal/shellCommandRunner/TerminalSmartCommandActions.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/shellCommandRunner/TerminalSmartCommandActions.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.terminal.shellCommandRunner
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
@@ -12,6 +13,8 @@ abstract class TerminalExecutorAction : AnAction() {
   override fun update(e: AnActionEvent) {
     e.presentation.isEnabled = false
   }
+
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 
   override fun actionPerformed(e: AnActionEvent) {}
 }
