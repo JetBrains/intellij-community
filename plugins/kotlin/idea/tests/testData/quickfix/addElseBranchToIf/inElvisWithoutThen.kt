09892a2c72cd796770e351c4c589c6661f3a72ea
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-07-06 11:57:21 +0200

    [kotlin] Update testdata after compiler fix KT-53072
    Fixed tests:
    - org.jetbrains.kotlin.idea.inspections.LocalInspectionTestGenerated$UseExpressionBody$ConvertToExpressionBody$Uncategorized.testElvisIfUnit
    - org.jetbrains.kotlin.idea.intentions.IntentionTestGenerated$ReplaceMapGetOrDefault.testSimple
    - org.jetbrains.kotlin.idea.quickfix.QuickFixTestGenerated$AddElseBranchToIf.testInElvisWithoutThen
    
    GitOrigin-RevId: 4a6059246685b882c306b089f9ff504852787a41

--- plugins/kotlin/idea/tests/testData/quickfix/addElseBranchToIf/inElvisWithoutThen.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/addElseBranchToIf/inElvisWithoutThen.kt
@@ -1,6 +1,7 @@
 // "Add else branch" "false"
 // ACTION: Introduce local variable
 // ACTION: Remove braces from 'while' statement
+// ERROR: 'if' must have both main and 'else' branches if used as an expression
 
 fun foo(x: String?) {
     while (true) {
