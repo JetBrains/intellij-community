1b2ed948cf6f6769433fc7a6d1aa5be4ed5f7cb0
Anna Kozlova <anna.kozlova@jetbrains.com>
2022-07-07 09:54:37 +0200

    assert Swing component access in EDT only
    fix
    
    GitOrigin-RevId: 52ba69c31e60d7df810ebbbdd59d763051bf32d5

--- platform/platform-impl/src/com/intellij/ui/SpeedSearchBase.java
+++ platform/platform-impl/src/com/intellij/ui/SpeedSearchBase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ui;
 
 import com.intellij.featureStatistics.FeatureUsageTracker;
@@ -178,6 +178,7 @@ public abstract class SpeedSearchBase<Comp extends JComponent> extends SpeedSear
 
   @Override
   public boolean isPopupActive() {
+    ApplicationManager.getApplication().assertIsDispatchThread();
     return mySearchPopup != null && mySearchPopup.isVisible() ||
            isStickySearch() && StringUtil.isNotEmpty(UIUtil.getClientProperty(myComponent, SEARCH_TEXT_KEY));
   }
