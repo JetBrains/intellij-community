b279bc0e0542697a68f04e6db839ca997dd16163
Vladimir Plyashkun <vladimir.plyashkun@jetbrains.com>
2022-07-08 16:27:31 +0300

    CPP-27198 - bundle and provide documentation for Clang-Tidy checks
    GitOrigin-RevId: 517765a923b41451df28fe924264c56e0c43d9c4

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/LocalInspectionsPass.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/LocalInspectionsPass.java
@@ -18,7 +18,6 @@ import com.intellij.lang.Language;
 import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.lang.annotation.ProblemGroup;
 import com.intellij.lang.injection.InjectedLanguageManager;
-import com.intellij.openapi.actionSystem.IdeActions;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
@@ -27,8 +26,6 @@ import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.editor.markup.UnmodifiableTextAttributes;
-import com.intellij.openapi.keymap.KeymapManager;
-import com.intellij.openapi.keymap.KeymapUtil;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
@@ -90,19 +87,6 @@ public class LocalInspectionsPass extends ProgressableTextEditorHighlightingPass
 
     // initial guess
     setProgressLimit(300 * 2);
-  }
-
-  @NotNull
-  private static String getShortcutText() {
-    String myShortcutText;
-    KeymapManager keymapManager = KeymapManager.getInstance();
-    if (keymapManager == null) {
-      myShortcutText = "";
-    }
-    else {
-      myShortcutText = "(" + KeymapUtil.getShortcutsText(keymapManager.getActiveKeymap().getShortcuts(IdeActions.ACTION_SHOW_ERROR_DESCRIPTION)) + ")";
-    }
-    return myShortcutText;
   }
 
   private @NotNull PsiFile getFile() {
@@ -323,7 +307,8 @@ public class LocalInspectionsPass extends ProgressableTextEditorHighlightingPass
     @NlsSafe String tooltip = null;
     if (descriptor.showTooltip()) {
       String rendered = presentation.getTooltip();
-      tooltip = tooltips.intern(DaemonTooltipsUtil.getWrappedTooltip(rendered, shortName, getShortcutText(), showToolDescription(toolWrapper)));
+      tooltip = tooltips.intern(DaemonTooltipsUtil.getWrappedTooltip(rendered, shortName,
+                                                                     showToolDescription(toolWrapper)));
     }
     List<IntentionAction> fixes = getQuickFixes(key, descriptor, emptyActionRegistered);
     HighlightInfo info = highlightInfoFromDescriptor(descriptor, type, plainMessage, tooltip, element, fixes, key, getColorsScheme(), severityRegistrar);
