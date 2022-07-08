d7cf8b2a15cdc4c914fda5e3be37001f761e3e8c
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2022-07-08 21:59:07 +0200

    action-update-thread: dom action fixed
    GitOrigin-RevId: aca0dc05b88ff79d87a3c2224d84cc70852b289c

--- xml/dom-openapi/src/com/intellij/util/xml/ui/DomCollectionControl.java
+++ xml/dom-openapi/src/com/intellij/util/xml/ui/DomCollectionControl.java
@@ -473,7 +473,7 @@ public class DomCollectionControl<T extends DomElement> extends DomUIControl imp
 
     @Override
     public @NotNull ActionUpdateThread getActionUpdateThread() {
-      return ActionUpdateThread.BGT;
+      return ActionUpdateThread.EDT;
     }
   }
 }
