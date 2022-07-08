8acbdcf7eadd4c9b1a75064195ed4eb3b5d56ef1
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2022-07-08 16:49:07 +0200

    [actions] action-update-thread: training actions
    (cherry picked from commit ad6cd2e22c4bffa6b4aef4ac7e61ebcd05ecdc8a)
    
    GitOrigin-RevId: 54a6093972fe563ebb28619192312b85f5786afd

--- plugins/ide-features-trainer/src/training/actions/SetCurrentLessonAsPassed.kt
+++ plugins/ide-features-trainer/src/training/actions/SetCurrentLessonAsPassed.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package training.actions
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import training.learn.lesson.LessonManager
@@ -10,6 +11,8 @@ private class SetCurrentLessonAsPassed : AnAction() {
     val currentLesson = LessonManager.instance.currentLesson ?: return
     LessonManager.instance.passLesson(currentLesson)
   }
+
+  override fun getActionUpdateThread() = ActionUpdateThread.EDT
 
   override fun update(e: AnActionEvent) {
     e.presentation.isEnabled = LessonManager.instance.currentLesson != null
