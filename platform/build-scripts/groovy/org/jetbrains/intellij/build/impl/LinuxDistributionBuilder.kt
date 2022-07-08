8ec987dfe59d809cdd6a8f7a64c0455899ce63fc
Mikhail Filippov <mikhail@filippov.me>
2022-07-09 00:35:11 +0300

    Fix NPE in release Linux SNAP build
    GitOrigin-RevId: bdb32c9162e82291735fd1b97f76afecccc00933

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/LinuxDistributionBuilder.kt
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/LinuxDistributionBuilder.kt
@@ -211,7 +211,7 @@ class LinuxDistributionBuilder(private val context: BuildContext,
         moveFile(iconPngPath, snapDir.resolve("$snapName.png"))
         val snapcraftTemplate = context.paths.communityHomeDir.communityRoot.resolve(
           "platform/build-scripts/resources/linux/snap/snapcraft-template.yaml")
-        val versionSuffix = appInfo.versionSuffix!!.replace(' ', '-')
+        val versionSuffix = appInfo.versionSuffix?.replace(' ', '-') ?: ""
         val version = "${appInfo.majorVersion}.${appInfo.minorVersion}${if (versionSuffix.isEmpty()) "" else "-${versionSuffix}"}"
         substituteTemplatePlaceholders(
           inputFile = snapcraftTemplate,
