1223d4e0e2783fb81df9b65f2b19379ef0b548e7
Maria Filipanova <maria.filipanova@jetbrains.com>
2022-07-09 20:06:48 +0300

    CWM-559: fix ShowQuickFixesAction so it works for clients
    (cherry picked from commit e1300b0c038c4d0fef8db55f686788e93cd619a1)
    
    IJ-CR-27206
    
    GitOrigin-RevId: a2c666d450c43b204aaae51e6685edc7eaf9c7a9

--- platform/lang-impl/src/com/intellij/analysis/problemsView/toolWindow/ShowQuickFixesAction.kt
+++ platform/lang-impl/src/com/intellij/analysis/problemsView/toolWindow/ShowQuickFixesAction.kt
@@ -8,13 +8,12 @@ import com.intellij.codeInsight.intention.impl.IntentionListStep
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys.PSI_FILE
 import com.intellij.openapi.actionSystem.PlatformCoreDataKeys.SELECTED_ITEM
 import com.intellij.openapi.actionSystem.impl.ActionButton
 import com.intellij.openapi.application.ApplicationManager.getApplication
 import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.editor.ClientEditorManager
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.popup.JBPopup
@@ -22,7 +21,9 @@ import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.wm.IdeFocusManager
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
 import com.intellij.ui.awt.RelativePoint
+import com.intellij.util.ui.UIUtil
 import com.intellij.util.ui.UIUtil.isAncestor
 import java.awt.event.MouseEvent
 
@@ -31,21 +32,23 @@ internal class ShowQuickFixesAction : AnAction() {
   override fun getActionUpdateThread() = ActionUpdateThread.BGT
 
   override fun update(event: AnActionEvent) {
-    val node = event.getData(SELECTED_ITEM) as? ProblemNode
+    val panel = ProblemsView.getSelectedPanel(event.project)
+    val node = panel?.let { SELECTED_ITEM.getData(it) as? ProblemNode }
     val problem = node?.problem
     with(event.presentation) {
-      isVisible = getApplication().isInternal || ProblemsView.getSelectedPanel(event.project) is HighlightingPanel
+      isVisible = getApplication().isInternal || panel is HighlightingPanel
       isEnabled = isVisible && when (problem) {
-        is HighlightingProblem -> isEnabled(event, problem)
+        is HighlightingProblem -> isEnabled(event, panel, problem)
         else -> false
       }
     }
   }
 
   override fun actionPerformed(event: AnActionEvent) {
-    val node = event.getData(SELECTED_ITEM) as? ProblemNode
+    val panel = ProblemsView.getSelectedPanel(event.project)
+    val node = panel?.let { SELECTED_ITEM.getData(it) as? ProblemNode }
     when (val problem = node?.problem) {
-      is HighlightingProblem -> actionPerformed(event, problem)
+      is HighlightingProblem -> actionPerformed(event, panel, problem)
     }
   }
 
@@ -53,13 +56,13 @@ internal class ShowQuickFixesAction : AnAction() {
   private fun getEditor(psi: PsiFile, showEditor: Boolean): Editor? {
     val file = psi.virtualFile ?: return null
     val document = PsiDocumentManager.getInstance(psi.project).getDocument(psi) ?: return null
-    val editors = EditorFactory.getInstance().editors(document, psi.project).filter { !it.isViewer }
+    val editors = ClientEditorManager.getCurrentInstance().editors(document, psi.project).filter { !it.isViewer }
     val editor = editors.findFirst().orElse(null) ?: return null
-    if (!showEditor || editor.component.isShowing) return editor
+    if (!showEditor || UIUtil.isShowing(editor.component)) return editor
     val manager = FileEditorManager.getInstance(psi.project) ?: return null
     if (manager.allEditors.none { isAncestor(it.component, editor.component) }) return null
     manager.openFile(file, false, true)
-    return if (editor.component.isShowing) editor else null
+    return if (UIUtil.isShowing(editor.component)) editor else null
   }
 
   private fun show(event: AnActionEvent, popup: JBPopup) {
@@ -78,12 +81,12 @@ internal class ShowQuickFixesAction : AnAction() {
   }
 
 
-  private fun isEnabled(event: AnActionEvent, problem: HighlightingProblem): Boolean {
-    return getCachedIntentions(event, problem, false) != null
+  private fun isEnabled(event: AnActionEvent, panel: ProblemsViewPanel, problem: HighlightingProblem): Boolean {
+    return getCachedIntentions(event, panel, problem, false) != null
   }
 
-  private fun actionPerformed(event: AnActionEvent, problem: HighlightingProblem) {
-    val intentions = getCachedIntentions(event, problem, true) ?: return
+  private fun actionPerformed(event: AnActionEvent, panel: ProblemsViewPanel, problem: HighlightingProblem) {
+    val intentions = getCachedIntentions(event, panel, problem, true) ?: return
     val editor: Editor = intentions.editor ?: return
     if (intentions.offset >= 0) editor.caretModel.moveToOffset(intentions.offset.coerceAtMost(editor.document.textLength))
     show(event, JBPopupFactory.getInstance().createListPopup(
@@ -103,10 +106,13 @@ internal class ShowQuickFixesAction : AnAction() {
     ))
   }
 
-  private fun getCachedIntentions(event: AnActionEvent, problem: HighlightingProblem, showEditor: Boolean): CachedIntentions? {
-    val psi = event.getData(PSI_FILE) ?: return null
-    val panel = ProblemsView.getSelectedPanel(event.project) ?: return null
-    if (!panel.isShowing) return null
+  private fun getCachedIntentions(event: AnActionEvent,
+                                  panel: ProblemsViewPanel,
+                                  problem: HighlightingProblem,
+                                  showEditor: Boolean): CachedIntentions? {
+    val project = event.project ?: return null
+    val psi = PsiManager.getInstance(project).findFile(panel.selectedFile ?: return null) ?: return null
+    if (!UIUtil.isShowing(panel)) return null
     val editor = panel.preview ?: getEditor(psi, showEditor) ?: return null
     val markers = problem.info?.quickFixActionMarkers ?: return null
 
