b2b58472eb3197f29f143a38dc1574a5231ae43a
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2022-07-10 16:43:13 +0200

    [tf-junit5] add `@TestDisposable` for injecting `Disposable` instance into a test
    GitOrigin-RevId: 332cc205308c0b416f48805acfeef537101ac5fe

---
+++ platform/testFramework/junit5/src/impl/extensionUtil.kt
@@ -0,0 +1,11 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.testFramework.junit5.impl
+
+import org.junit.jupiter.api.extension.ExtensionContext
+
+internal fun <T> ExtensionContext.Store.computeIfAbsent(key: String, computable: () -> T): T {
+  @Suppress("UNCHECKED_CAST")
+  return getOrComputeIfAbsent(key) {
+    computable()
+  } as T
+}
