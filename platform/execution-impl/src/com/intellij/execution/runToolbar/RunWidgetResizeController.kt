a6858e998cffff31e3fb5958652aa63d5c199c92
Elena.Korotkova <elena.korotkova@jetbrains.com>
2022-07-11 13:53:44 +0300

    Revert "[rider] fix run widget size to be per project fix #RIDER-77928 fix #RIDER-77460"
    This reverts commit 74b58b3862653c0dd94e2e7317f898c8a59413d4.
    
    GitOrigin-RevId: 170340d81b43d14e1d2da6f00fa800ffbd2ccf0c

--- platform/execution-impl/src/com/intellij/execution/runToolbar/RunWidgetResizeController.kt
+++ platform/execution-impl/src/com/intellij/execution/runToolbar/RunWidgetResizeController.kt
@@ -1,36 +1,32 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.runToolbar
 
-import com.intellij.openapi.components.service
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.wm.IdeFrame
-import java.awt.Component
+import com.intellij.execution.runToolbar.FixWidthSegmentedActionToolbarComponent.Companion.RUN_CONFIG_SCALED_WIDTH
+import com.intellij.execution.runToolbar.FixWidthSegmentedActionToolbarComponent.Companion.RUN_CONFIG_WIDTH_PROP
+import com.intellij.execution.runToolbar.FixWidthSegmentedActionToolbarComponent.Companion.RUN_CONFIG_WIDTH_UNSCALED_MIN
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.util.ui.JBUI
 import java.awt.Dimension
 import java.awt.Point
-import javax.swing.SwingUtilities
 
-class RunWidgetResizeController private constructor(val project: Project) : DraggablePane.DragListener {
+class RunWidgetResizeController private constructor() : DraggablePane.DragListener {
   companion object {
-    @JvmStatic
-    fun getInstance(project: Project) = project.service<RunWidgetResizeController>()
-
-    fun fromParentIdeFrame(component: Component): RunWidgetResizeController? {
-      val ideFrame = SwingUtilities.getWindowAncestor(component) as? IdeFrame ?: return null
-      val project = ideFrame.project ?: return null
-      return getInstance(project)
+    private val controller = RunWidgetResizeController()
+    fun getInstance(): RunWidgetResizeController {
+      return controller
     }
   }
 
   private var startWidth: Int? = null
 
   override fun dragStarted(locationOnScreen: Point) {
-    startWidth = RunToolbarSettings.getInstance(project).getRunConfigWidth()
+    startWidth = RUN_CONFIG_SCALED_WIDTH
   }
 
   override fun dragged(locationOnScreen: Point, offset: Dimension) {
     startWidth?.let {
-      val width = it - offset.width
-      RunToolbarSettings.getInstance(project).setRunConfigWidth(width)
+      RUN_CONFIG_SCALED_WIDTH = it - offset.width
+      PropertiesComponent.getInstance().setValue(RUN_CONFIG_WIDTH_PROP, RUN_CONFIG_SCALED_WIDTH, JBUI.scale(RUN_CONFIG_WIDTH_UNSCALED_MIN))
     }
   }
 
