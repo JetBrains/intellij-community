1f99ae0f7f0ba7aaf5319e7ad110ad72b5c720c3
Adam Malek <adam.malek@jetbrains.com>
2022-07-12 12:00:24 +0200

    ML in SE: Fix key provider not providing unique ID for rnc items (IDEA-295687)
    GitOrigin-RevId: e53195f2d9d1b53dbb967eef5cc3bf4acef43d48

--- plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/id/NavigationItemKeyProvider.kt
+++ plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/id/NavigationItemKeyProvider.kt
@@ -1,9 +1,14 @@
 package com.intellij.ide.actions.searcheverywhere.ml.id
 
 import com.intellij.navigation.NavigationItem
+import com.intellij.navigation.PsiElementNavigationItem
 
 private class NavigationItemKeyProvider : ElementKeyForIdProvider() {
   override fun getKey(element: Any): Any? {
-    return element as? NavigationItem
+    return when (element) {
+      is PsiElementNavigationItem -> element.targetElement
+      is NavigationItem -> element.name?.let { it + element.presentation?.locationString }
+      else -> null
+    }
   }
 }
