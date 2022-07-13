a4344f9fa5441fcc787b7795477362bd1066b906
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2022-07-13 16:39:34 +0200

    [actions] action-update-thread: ui actions
    (cherry picked from commit 7c180b106f47482b85d9a49a39788a67c92db792)
    
    GitOrigin-RevId: a3c79ae42dbbe7cd2cb0d87bdbc0ec949f611c19

--- platform/platform-api/src/com/intellij/ui/switcher/ShowQuickActionPopupAction.java
+++ platform/platform-api/src/com/intellij/ui/switcher/ShowQuickActionPopupAction.java
@@ -15,10 +15,7 @@
  */
 package com.intellij.ui.switcher;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DefaultActionGroup;
-import com.intellij.openapi.actionSystem.Separator;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.ObjectUtils;
@@ -31,6 +28,12 @@ import java.util.List;
 import static com.intellij.openapi.ui.popup.JBPopupFactory.ActionSelectionAid.ALPHA_NUMBERING;
 
 public class ShowQuickActionPopupAction extends AnAction {
+
+  @Override
+  public @NotNull ActionUpdateThread getActionUpdateThread() {
+    return ActionUpdateThread.BGT;
+  }
+
   @Override
   public void update(@NotNull AnActionEvent e) {
     QuickActionProvider quickActionProvider = e.getData(QuickActionProvider.KEY);
