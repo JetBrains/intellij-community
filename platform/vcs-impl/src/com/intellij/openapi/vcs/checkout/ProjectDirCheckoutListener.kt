e5d63ff72f942473c39afcce47a5334997b89dd4
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-07-13 13:34:24 +0200

    ProjectDirCheckoutListener - use non-blocking API to open project
    GitOrigin-RevId: 2eafc90e883d838313402e916e604fbb6514bd0a

--- platform/vcs-impl/src/com/intellij/openapi/vcs/checkout/ProjectDirCheckoutListener.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/checkout/ProjectDirCheckoutListener.kt
@@ -1,27 +1,29 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.vcs.checkout;
+package com.intellij.openapi.vcs.checkout
 
-import com.intellij.ide.impl.OpenProjectTask;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ex.ProjectManagerEx;
-import org.jetbrains.annotations.NotNull;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
+import com.intellij.ide.impl.OpenProjectTask
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ex.ProjectManagerEx
+import kotlinx.coroutines.runBlocking
+import java.nio.file.Files
+import java.nio.file.Path
 
 /**
- * Open project with {@code .idea}.
+ * Open project with `.idea`.
  */
-final class ProjectDirCheckoutListener implements CheckoutListener {
-  @Override
-  public boolean processCheckedOutDirectory(@NotNull Project project, @NotNull Path directory) {
-    Path dotIdea = directory.resolve(Project.DIRECTORY_STORE_FOLDER);
+internal class ProjectDirCheckoutListener : CheckoutListener {
+  override fun processCheckedOutDirectory(project: Project, directory: Path): Boolean {
+    check(!ApplicationManager.getApplication().isDispatchThread)
+
+    val dotIdea = directory.resolve(Project.DIRECTORY_STORE_FOLDER)
     // todo Rider project layout - several.idea.solution-name names
     if (!Files.exists(dotIdea)) {
-      return false;
+      return false
     }
-
-    ProjectManagerEx.getInstanceEx().openProject(directory, OpenProjectTask.build().withProjectToClose(project));
-    return true;
+    runBlocking {
+      ProjectManagerEx.getInstanceEx().openProjectAsync(directory, OpenProjectTask { projectToClose = project })
+    }
+    return true
   }
 }
