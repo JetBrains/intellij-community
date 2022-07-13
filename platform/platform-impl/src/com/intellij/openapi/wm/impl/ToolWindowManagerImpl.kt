a9398fbb13ffbc2ba79449b7d92b5f111a0cd1f5
Sasha Blashenkov <Sasha.Blashenkov@jetbrains.com>
2022-07-13 15:20:32 +0200

    Fix RIDER-79086 Toolwindow doesn't remember its' state, fix RIDER-79648 Environment wizard is not shown if net5 is installed (without .net 6), fix RIDER-78120 Delete useless text from initial wizard, fix RIDER-79132 "Keymaps" layout is broken in the initial wizard
    (cherry picked from commit 049b8bfe83a1ba692138251ddfe72e450d0843b9)
    
    IJ-CR-27152
    
    (cherry picked from commit e2eb6e4898ae93f3f74fbe9ba283aff594430468)
    
    (cherry picked from commit 775bea41457dfb3bc5b261f17d38aa8a913a3bf8)
    
    GitOrigin-RevId: 14c9c48ee6132ff3d9c933e8cbcf0bb939a69117

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/ToolWindowManagerImpl.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/ToolWindowManagerImpl.kt
@@ -95,7 +95,7 @@ open class ToolWindowManagerImpl @NonInjectable @TestOnly internal constructor(v
   private val state: ToolWindowManagerState
     get() = project.service()
 
-  private var layoutState
+  var layoutState
     get() = state.layout
     set(value) { state.layout = value }
 
