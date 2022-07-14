f3d106d297535c945e3e0f9ecf47d9f4624d6fbc
Liubov Afanaseva <liubov.afanaseva@jetbrains.com>
2022-07-14 12:17:42 +0200

    FUS: increase version of groups
    GitOrigin-RevId: 1b3d5d7178dc1e9bd0d4843bcd71d56a4c4f8998

--- plugins/kotlin/gradle/gradle/src/org/jetbrains/kotlin/idea/gradle/statistics/KotlinGradleFUSCollector.kt
+++ plugins/kotlin/gradle/gradle/src/org/jetbrains/kotlin/idea/gradle/statistics/KotlinGradleFUSCollector.kt
@@ -16,7 +16,7 @@ class KotlinGradleFUSCollector : CounterUsagesCollector() {
 
     companion object {
 
-        private val GROUP = EventLogGroup("kotlin.gradle.performance", 8)
+        private val GROUP = EventLogGroup("kotlin.gradle.performance", 9)
 
         private fun listOfAllMetrics(): Array<Any> {
             val result = ArrayList<Any>()
