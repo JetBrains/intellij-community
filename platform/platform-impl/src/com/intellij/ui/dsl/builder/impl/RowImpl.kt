1ac2acd1960c32a28c82fcbf05abb64770b2960b
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2022-07-15 09:47:33 +0200

    Kotlin UI DSL: opacity false is not needed for Row.textField
    GitOrigin-RevId: 6b8b627b49cd0cd4de1868d0256769648bb2f04d

--- platform/platform-impl/src/com/intellij/ui/dsl/builder/impl/RowImpl.kt
+++ platform/platform-impl/src/com/intellij/ui/dsl/builder/impl/RowImpl.kt
@@ -327,9 +327,7 @@ internal open class RowImpl(private val dialogPanelConfig: DialogPanelConfig,
   }
 
   override fun textField(): CellImpl<JBTextField> {
-    val result = cell(JBTextField()).applyToComponent {
-      isOpaque = false
-    }
+    val result = cell(JBTextField())
     result.columns(COLUMNS_SHORT)
     return result
   }
