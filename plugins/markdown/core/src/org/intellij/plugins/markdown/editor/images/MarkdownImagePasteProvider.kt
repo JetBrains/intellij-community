94bc74961acceaa3d0a37c6b6d96dd0bd27139bf
Ivan Posti <ivan.posti@jetbrains.com>
2022-07-15 16:43:47 +0200

    [Markdown] Make our image paste provider work with multiple carets
    GitOrigin-RevId: 0f08dba6dc3b134e54843f2133ec72aa3bdf597e

--- plugins/markdown/core/src/org/intellij/plugins/markdown/editor/images/MarkdownImagePasteProvider.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/editor/images/MarkdownImagePasteProvider.kt
@@ -1,33 +1,38 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.intellij.plugins.markdown.editor.images
 
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.ReadOnlyFragmentModificationException
-import com.intellij.openapi.editor.ReadOnlyModificationException
+import com.intellij.openapi.command.executeCommand
+import com.intellij.openapi.editor.*
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.vfs.VirtualFile
 import org.intellij.images.ide.EditorImagePasteProvider
 import org.intellij.plugins.markdown.lang.MarkdownFileType
 
-internal class MarkdownImagePasteProvider : EditorImagePasteProvider() {
+internal class MarkdownImagePasteProvider: EditorImagePasteProvider() {
   override val supportedFileType: FileType
     get() = MarkdownFileType.INSTANCE
 
   override fun Editor.imageFilePasted(imageFile: VirtualFile) {
     val pastedFileName = imageFile.name
+    try {
+      executeCommand {
+        caretModel.runForEachCaret(reverseOrder = true) { caret ->
+          val offset = caret.offset
+          val textToInsert = ImageUtils.createMarkdownImageText(pastedFileName, pastedFileName)
+          document.insertString(offset, textToInsert)
+          caretModel.moveToOffset(offset + textToInsert.length)
+        }
+      }
+    } catch (e: ReadOnlyModificationException) {
+      // do nothing
+    } catch (e: ReadOnlyFragmentModificationException) {
+      // do nothing
+    }
+  }
 
-    caretModel.currentCaret.offset.let { currentCaretOffset ->
-      val textToInsert = ImageUtils.createMarkdownImageText(pastedFileName, pastedFileName)
-      try {
-        document.insertString(currentCaretOffset, textToInsert)
-        caretModel.moveToOffset(currentCaretOffset + textToInsert.length)
-      }
-      catch (e: ReadOnlyModificationException) {
-        // do nothing
-      }
-      catch (e: ReadOnlyFragmentModificationException) {
-        // do nothing
-      }
+  companion object {
+    private fun CaretModel.runForEachCaret(reverseOrder: Boolean = false, block: (Caret) -> Unit) {
+      runForEachCaret(block, reverseOrder)
     }
   }
 }
