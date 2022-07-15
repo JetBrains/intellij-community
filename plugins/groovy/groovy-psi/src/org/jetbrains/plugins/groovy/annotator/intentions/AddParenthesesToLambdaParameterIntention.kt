ff814549ba5f018e1fd68a28b6cea55a75c144f9
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-07-15 14:39:24 +0300

    [groovy] Fix preview for AddParenthesesToLambdaParameterIntention
    GitOrigin-RevId: c6bd7377e23e30d0969c0a0973fe59c900e27bed

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/annotator/intentions/AddParenthesesToLambdaParameterIntention.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/annotator/intentions/AddParenthesesToLambdaParameterIntention.kt
@@ -1,17 +1,19 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.groovy.annotator.intentions
 
+import com.intellij.codeInsight.intention.FileModifier
 import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 import com.intellij.psi.SmartPsiElementPointer
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.createSmartPointer
 import org.jetbrains.plugins.groovy.GroovyBundle.message
 import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElementFactory
 import org.jetbrains.plugins.groovy.lang.psi.api.GrLambdaExpression
 
-class AddParenthesisToLambdaParameterAction(parameterList: GrLambdaExpression) : IntentionAction {
+class AddParenthesesToLambdaParameterIntention(parameterList: GrLambdaExpression) : IntentionAction {
 
   private val myLambda: SmartPsiElementPointer<GrLambdaExpression> = parameterList.createSmartPointer()
 
@@ -20,6 +22,11 @@ class AddParenthesisToLambdaParameterAction(parameterList: GrLambdaExpression) :
   override fun getFamilyName(): String = message("add.parenthesis.to.lambda.parameter.list")
 
   override fun startInWriteAction(): Boolean = true
+
+  override fun getFileModifierForPreview(target: PsiFile): FileModifier? {
+    val originalParameterList = PsiTreeUtil.findSameElementInCopy(myLambda.element, target) ?: return null
+    return AddParenthesesToLambdaParameterIntention(originalParameterList)
+  }
 
   override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean {
     val parameterList = myLambda.element?.parameterList ?: return false
