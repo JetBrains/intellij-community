b7644ed2eeaa59bddf412b282ace1e3f81efaff0
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2022-07-15 16:22:17 +0200

    [plugins] serialization fixed #IDEA-297555
    XmlSerializationException: Can't serialize instance of class com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.PluginAdvertiserExtensionsStateService$State
    
    GitOrigin-RevId: 332bf0388e31acb046d9bb28f43485e11b777a0f

--- platform/ide-core/src/com/intellij/ide/plugins/PluginFeatureService.kt
+++ platform/ide-core/src/com/intellij/ide/plugins/PluginFeatureService.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-@file:Suppress("ReplaceGetOrSet")
+@file:Suppress("ReplaceGetOrSet", "ReplacePutWithAssignment")
 
 package com.intellij.ide.plugins
 
@@ -10,11 +10,11 @@ import com.intellij.openapi.components.*
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.util.SimpleModificationTracker
 import kotlinx.serialization.Serializable
-import java.util.concurrent.locks.ReentrantReadWriteLock
+import java.util.concurrent.atomic.AtomicReference
 
 @Service(Service.Level.APP)
 @State(name = "PluginFeatureService", storages = [Storage(StoragePathMacros.CACHE_FILE)])
-class PluginFeatureService : SerializablePersistentStateComponent<PluginFeatureService.State>(State()) {
+class PluginFeatureService : PersistentStateComponentWithModificationTracker<PluginFeatureService.State> {
 
   companion object {
     @JvmStatic
@@ -22,8 +22,17 @@ class PluginFeatureService : SerializablePersistentStateComponent<PluginFeatureS
       get() = ApplicationManager.getApplication().getService(PluginFeatureService::class.java)
   }
 
-  private val readWriteLock = ReentrantReadWriteLock()
   private val tracker = SimpleModificationTracker()
+  private var state = AtomicReference(State())
+
+  override fun getState(): State = state.get()
+
+  override fun loadState(newState: State) {
+    this.state.updateAndGet { oldState ->
+      tracker.incModificationCount()
+      State(oldState.features + newState.features)
+    }
+  }
 
   @Serializable
   data class FeaturePluginList(
@@ -43,47 +52,25 @@ class PluginFeatureService : SerializablePersistentStateComponent<PluginFeatureS
     idMapping: (T) -> String,
     displayNameMapping: (T) -> String,
   ) {
-    readWriteLock.writeLock().lock()
-    try {
-      var featureList = state.features.get(featureType)
-                        ?: FeaturePluginList()
+    val featureMap = LinkedHashMap(featureMap(featureType))
 
-      // fold
-      ep.processWithPluginDescriptor { ext, descriptor ->
-        val pluginData = FeaturePluginData(
-          displayNameMapping(ext),
-          PluginData(descriptor),
-        )
+    // fold
+    ep.processWithPluginDescriptor { ext, descriptor ->
+      val pluginData = FeaturePluginData(
+        displayNameMapping(ext),
+        PluginData(descriptor),
+      )
 
-        val id = idMapping(ext)
-        featureList = FeaturePluginList(featureList.featureMap + (id to pluginData))
-      }
-
-      loadState(State(state.features + (featureType to featureList)))
+      featureMap.put(idMapping(ext), pluginData)
     }
-    finally {
-      tracker.incModificationCount()
-      readWriteLock.writeLock().unlock()
-    }
+
+    loadState(State(mapOf(featureType to FeaturePluginList(featureMap))))
   }
 
   fun getPluginForFeature(
     featureType: String,
     implementationName: String,
-  ): FeaturePluginData? {
-    return if (readWriteLock.readLock().tryLock()) {
-      try {
-        state.features
-          .get(featureType)
-          ?.featureMap
-          ?.get(implementationName)
-      }
-      finally {
-        readWriteLock.readLock().unlock()
-      }
-    }
-    else {
-      null
-    }
-  }
+  ): FeaturePluginData? = featureMap(featureType).get(implementationName)
+
+  private fun featureMap(featureType: String) = getState().features.get(featureType)?.featureMap ?: emptyMap()
 }
