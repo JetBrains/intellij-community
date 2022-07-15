2ecfbd52a83a1266d22223b9ddd7c019a941356e
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2022-07-15 14:33:08 +0200

    [testFramework-junit5] automatically register `SwingTimerWatcherExtension`
    GitOrigin-RevId: 5e72e63f581b80b05ad62f7099a92bf1d2f8e2da

--- platform/testFramework/src/com/intellij/testFramework/TestApplicationManager.kt
+++ platform/testFramework/src/com/intellij/testFramework/TestApplicationManager.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.testFramework
 
-import com.intellij.ReviseWhenPortedToJDK
 import com.intellij.application.options.CodeStyle
 import com.intellij.codeInsight.AutoPopupController
 import com.intellij.codeInsight.completion.CompletionProgressIndicator
@@ -45,19 +44,15 @@ import com.intellij.psi.templateLanguages.TemplateDataLanguageMappings
 import com.intellij.serviceContainer.ComponentManagerImpl
 import com.intellij.testFramework.common.*
 import com.intellij.ui.UiInterceptors
-import com.intellij.util.ReflectionUtil
 import com.intellij.util.concurrency.AppExecutorUtil
 import com.intellij.util.concurrency.AppScheduledExecutorService
 import com.intellij.util.ref.GCUtil
 import com.intellij.util.throwIfNotEmpty
 import com.intellij.util.ui.UIUtil
 import com.intellij.workspaceModel.ide.impl.legacyBridge.module.roots.ModuleRootComponentBridge
-import junit.framework.AssertionFailedError
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.TestOnly
-import java.util.concurrent.DelayQueue
 import java.util.concurrent.TimeUnit
-import javax.swing.Timer
 
 class TestApplicationManager private constructor() {
   companion object {
@@ -169,31 +164,6 @@ class TestApplicationManager private constructor() {
 
     private inline fun <reified T : Any, reified TI : Any> Application.serviceIfCreated(): TI? =
       this.getServiceIfCreated(T::class.java) as? TI
-
-    @ReviseWhenPortedToJDK("9")
-    private fun checkJavaSwingTimersAreDisposed() {
-      val timerQueueClass = Class.forName("javax.swing.TimerQueue")
-      val sharedInstance = timerQueueClass.getMethod("sharedInstance")
-      sharedInstance.isAccessible = true
-      val timerQueue = sharedInstance.invoke(null)
-      val delayQueue = ReflectionUtil.getField(timerQueueClass, timerQueue, DelayQueue::class.java, "queue")
-      val timer = delayQueue.peek()
-      if (timer == null) {
-        return
-      }
-
-      val delay = timer.getDelay(TimeUnit.MILLISECONDS)
-      var text = "(delayed for ${delay}ms)"
-      val getTimer = ReflectionUtil.getDeclaredMethod(timer.javaClass, "getTimer")
-      val swingTimer = getTimer!!.invoke(timer) as Timer
-      text = "Timer (listeners: ${listOf(*swingTimer.actionListeners)}) $text"
-      try {
-        throw AssertionFailedError("Not disposed javax.swing.Timer: $text; queue:$timerQueue")
-      }
-      finally {
-        swingTimer.stop()
-      }
-    }
 
     private fun dropModuleRootCaches(project: Project) {
       WriteAction.runAndWait<RuntimeException> {
