f46ece9cdf1c44a69050f68c4c85145ec436f4dc
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-07-15 11:07:45 +0200

    reduce number of rd-*.jar in distributions - merge all into one rd.jar
    GitOrigin-RevId: cb08f5ad6ad80e43d18ba096fdd17e63344d78d9

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/JarPackager.kt
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/JarPackager.kt
@@ -33,7 +33,7 @@ import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.function.IntConsumer
 
-val JAR_NAME_WITH_VERSION_PATTERN = "(.*)-\\d+(?:\\.\\d+)*\\.jar*".toPattern()
+private val JAR_NAME_WITH_VERSION_PATTERN = "(.*)-\\d+(?:\\.\\d+)*\\.jar*".toPattern()
 
 @Suppress("ReplaceJavaStaticMethodWithKotlinAnalog")
 private val libsThatUsedInJps = java.util.Set.of(
@@ -75,6 +75,7 @@ class JarPackager private constructor(private val context: BuildContext) {
     init {
       extraMergeRules.put("groovy.jar") { it.startsWith("org.codehaus.groovy:") }
       extraMergeRules.put("jsch-agent.jar") { it.startsWith("jsch-agent") }
+      extraMergeRules.put("rd.jar") { it.startsWith("rd-") }
       // see ClassPathUtil.getUtilClassPath
       extraMergeRules.put("3rd-party-rt.jar") {
         libsThatUsedInJps.contains(it) || it.startsWith("kotlinx-") || it == "kotlin-reflect"
