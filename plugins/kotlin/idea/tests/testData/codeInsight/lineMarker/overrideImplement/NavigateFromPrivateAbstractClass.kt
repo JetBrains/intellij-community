684d8caf0918f893a076bf106b324c6d8eef8331
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2022-07-17 22:51:03 +0200

    [kotlin] Improve test coverage
    #KTIJ-16712
    
    GitOrigin-RevId: bfd9a62c2d217cc58670172b343f57dabe1f5056

--- plugins/kotlin/idea/tests/testData/codeInsight/lineMarker/overrideImplement/NavigateFromPrivateAbstractClass.kt
+++ plugins/kotlin/idea/tests/testData/codeInsight/lineMarker/overrideImplement/NavigateFromPrivateAbstractClass.kt
@@ -1,9 +1,15 @@
 class NavigateFromPrivateAbstractClass {
-    private abstract class <lineMarker>Base</lineMarker> {}
+    private abstract class <lineMarker>Base</lineMarker> {
+        abstract fun <lineMarker>foo</lineMarker>()
+    }
 
-    private class Impl: Base() {}
+    private class Impl: Base() {
+        override fun <lineMarker descr="Implements function in 'Base'">foo</lineMarker>() {}
+    }
 
-    private class Impl2: Base() {}
+    private class Impl2: Base() {
+        override fun <lineMarker descr="Implements function in 'Base'">foo</lineMarker>() {}
+    }
 
 }
 
@@ -11,7 +17,27 @@ class NavigateFromPrivateAbstractClass {
 LINEMARKER: descr='Is subclassed by Impl in NavigateFromPrivateAbstractClass Impl2 in NavigateFromPrivateAbstractClass  Click or press ... to navigate'
 TARGETS:
  NavigateFromPrivateAbstractClass.kt
-    private class <1>Impl: Base() {}
+    private class <1>Impl: Base() {
+        override fun foo() {}
+    }
 
-    private class <2>Impl2: Base() {}
+    private class <2>Impl2: Base() {
+*/
+
+/*
+LINEMARKER: descr='Is implemented in NavigateFromPrivateAbstractClass.Impl NavigateFromPrivateAbstractClass.Impl2'
+TARGETS:
+NavigateFromPrivateAbstractClass.kt
+        override fun <1>foo() {}
+    }
+
+    private class Impl2: Base() {
+        override fun <2>foo() {}
+*/
+
+/*
+LINEMARKER: descr='Implements function in 'Base''
+TARGETS:
+NavigateFromPrivateAbstractClass.kt
+        abstract fun <1>foo()
 */
