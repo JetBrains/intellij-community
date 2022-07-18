90c4fcb36ab146c4a494c70a836dd476ee8d137e
Aleksandr.Slapoguzov <aleksandr.slapoguzov@jetbrains.com>
2022-07-18 15:18:44 +0000

    WI-66604, performance optimization: iterate only by unique injected files in places where injected ranges are not matter
    InjectedLanguageManager#getInjectedPsiFiles() returns pairs of injected files and corresponding ranges. In case when a file has multiple injection places the method returns the corresponding number of pairs that have the same injected file but different ranges. In places where only injected file needed executing the same code for the same injected file multiple times can lead to long freezes. For example, when #getLastCommittedDocument() is invoked multiple times for the same document in short period.
    
    - remove duplicate inspection reports from test data
    
    Merge-request: IJ-MR-26771
    Merged-by: Aleksandr Slapoguzov <Aleksandr.Slapoguzov@jetbrains.com>
    (cherry picked from commit c923314d63c2638ffcc1b9b422b6b828087be8ea)
    
    GitOrigin-RevId: e09b422ccc6e9ff60643f29b7ab4705d24e35466

--- platform/analysis-impl/src/com/intellij/codeInspection/InspectionEngine.java
+++ platform/analysis-impl/src/com/intellij/codeInspection/InspectionEngine.java
@@ -121,7 +121,7 @@ public final class InspectionEngine {
     Map<LocalInspectionToolWrapper, List<ProblemDescriptor>> map = inspectElements(toolWrappers, file, restrictRange, ignoreSuppressedElements, isOnTheFly, indicator, elements, foundDescriptorCallback);
     if (inspectInjectedPsi) {
       InjectedLanguageManager injectedLanguageManager = InjectedLanguageManager.getInstance(file.getProject());
-      List<Pair<PsiFile, PsiElement>> injectedFiles = new ArrayList<>();
+      Set<Pair<PsiFile, PsiElement>> injectedFiles = new HashSet<>();
       for (PsiElement element : elements) {
         if (element instanceof PsiLanguageInjectionHost) {
           List<Pair<PsiElement, TextRange>> files = injectedLanguageManager.getInjectedPsiFiles(element);
@@ -133,7 +133,7 @@ public final class InspectionEngine {
           }
         }
       }
-      if (!JobLauncher.getInstance().invokeConcurrentlyUnderProgress(injectedFiles, indicator, pair -> {
+      if (!JobLauncher.getInstance().invokeConcurrentlyUnderProgress(new ArrayList<>(injectedFiles), indicator, pair -> {
         PsiFile injectedFile = pair.getFirst();
         PsiElement host = pair.getSecond();
         List<PsiElement> injectedElements = new ArrayList<>();
