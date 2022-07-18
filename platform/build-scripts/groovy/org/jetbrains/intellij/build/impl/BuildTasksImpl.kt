423c5bb8607b69566c6edcf858c259b8bfa6e0fa
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2022-07-18 13:02:32 +0200

    [build scripts] restoring inclusion of macOS/AArch64 artifacts into cross-platform .zip (RIDER-79807)
    GitOrigin-RevId: bdc0424f3ac3a5175139fc6d16938d88c004f1a3

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/BuildTasksImpl.kt
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/BuildTasksImpl.kt
@@ -1025,6 +1025,7 @@ private fun buildCrossPlatformZip(distDirs: List<DistributionForOsTaskResult>, c
   val dependenciesFile = copyDependenciesFile(context)
   crossPlatformZip(
     macX64DistDir = distDirs.first { it.os == OsFamily.MACOS && it.arch == JvmArchitecture.x64 }.outDir,
+    macArm64DistDir = distDirs.first { it.os == OsFamily.MACOS && it.arch == JvmArchitecture.aarch64 }.outDir,
     linuxX64DistDir = distDirs.first { it.os == OsFamily.LINUX && it.arch == JvmArchitecture.x64 }.outDir,
     winX64DistDir = distDirs.first { it.os == OsFamily.WINDOWS && it.arch == JvmArchitecture.x64 }.outDir,
     targetFile = targetFile,
@@ -1088,6 +1089,7 @@ internal fun getLinuxFrameClass(context: BuildContext): String {
 }
 
 private fun crossPlatformZip(macX64DistDir: Path,
+                             macArm64DistDir: Path,
                              linuxX64DistDir: Path,
                              winX64DistDir: Path,
                              targetFile: Path,
@@ -1172,7 +1174,7 @@ private fun crossPlatformZip(macX64DistDir: Path,
         }
       }
 
-      val commonFilter: (String) -> Boolean = { relPath ->
+      val commonFilter: (String) -> Boolean = { relPath: String ->
         !relPath.startsWith("bin/fsnotifier") &&
         !relPath.startsWith("bin/repair") &&
         !relPath.startsWith("bin/restart") &&
@@ -1189,6 +1191,11 @@ private fun crossPlatformZip(macX64DistDir: Path,
       out.dir(macX64DistDir, "", fileFilter = { _, relPath ->
         commonFilter.invoke(relPath) &&
         filterFileIfAlreadyInZip(relPath, macX64DistDir.resolve(relPath), zipFiles)
+      }, entryCustomizer = entryCustomizer)
+
+      out.dir(macArm64DistDir, "", fileFilter = { _, relPath ->
+        commonFilter.invoke(relPath) &&
+        filterFileIfAlreadyInZip(relPath, macArm64DistDir.resolve(relPath), zipFiles)
       }, entryCustomizer = entryCustomizer)
 
       out.dir(linuxX64DistDir, "", fileFilter = { _, relPath ->
