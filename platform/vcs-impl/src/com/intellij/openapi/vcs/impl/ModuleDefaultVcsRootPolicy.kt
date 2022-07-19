5843ddbcc80ff738c2f3a285adb302efd2d3b9bc
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-07-19 13:34:17 +0200

    vcs: migrate to WorkspaceModelChangeListener
    GitOrigin-RevId: be1a6a743ff2a530bd69a0cb10481435272af4d4

--- platform/vcs-impl/src/com/intellij/openapi/vcs/impl/ModuleDefaultVcsRootPolicy.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/impl/ModuleDefaultVcsRootPolicy.kt
@@ -1,29 +1,21 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.impl
 
-import com.intellij.ProjectTopics
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
-import com.intellij.openapi.project.ModuleListener
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.AdditionalLibraryRootsListener
-import com.intellij.openapi.roots.ModuleRootEvent
-import com.intellij.openapi.roots.ModuleRootListener
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.project.isDirectoryBased
 import com.intellij.project.stateStore
-import org.jetbrains.annotations.Nls
+import com.intellij.workspaceModel.ide.WorkspaceModelTopics
 
 open class ModuleDefaultVcsRootPolicy(project: Project) : DefaultVcsRootPolicy(project) {
   init {
-    val listener = MyModulesListener()
-    val connection = myProject.messageBus.connect()
-    connection.subscribe(ProjectTopics.MODULES, listener)
-    connection.subscribe(ProjectTopics.PROJECT_ROOTS, listener)
-    connection.subscribe(AdditionalLibraryRootsListener.TOPIC, listener)
+    val busConnection = project.messageBus.connect()
+    WorkspaceModelTopics.getInstance(project).subscribeAfterModuleLoading(busConnection, MyModulesListener())
   }
 
   override fun getDefaultVcsRoots(): Collection<VirtualFile> {
@@ -53,23 +45,8 @@ open class ModuleDefaultVcsRootPolicy(project: Project) : DefaultVcsRootPolicy(p
     return result
   }
 
-  private inner class MyModulesListener : ModuleRootListener, ModuleListener, AdditionalLibraryRootsListener {
-    override fun rootsChanged(event: ModuleRootEvent) {
-      scheduleMappedRootsUpdate()
-    }
-
-    override fun moduleAdded(project: Project, module: Module) {
-      scheduleMappedRootsUpdate()
-    }
-
-    override fun moduleRemoved(project: Project, module: Module) {
-      scheduleMappedRootsUpdate()
-    }
-
-    override fun libraryRootsChanged(presentableLibraryName: @Nls String?,
-                                     oldRoots: Collection<VirtualFile>,
-                                     newRoots: Collection<VirtualFile>,
-                                     libraryNameForDebug: String) {
+  private inner class MyModulesListener : ContentRootChangeListener() {
+    override fun rootsDirectoriesChanged(removed: List<VirtualFile>, added: List<VirtualFile>) {
       scheduleMappedRootsUpdate()
     }
   }
