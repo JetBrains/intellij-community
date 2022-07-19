ac20b395d5f22b49fef7b8c5a387d38f14a13bd5
Anna Koehler <anna.marchenko@jetbrains.com>
2022-07-20 00:59:40 +0200

    Divide module starter in three modules: starter, test and metricsCollector
    GitOrigin-RevId: 592568a9101ffd053a2a5ae3ab4bbbc594a50399

--- tools/intellij.ide.starter/src/com/intellij/ide/starter/build/tool/BuildToolType.kt
+++
@@ -1,6 +0,0 @@
-package com.intellij.ide.starter.build.tool
-
-enum class BuildToolType {
-  MAVEN,
-  GRADLE
-}
--- tools/intellij.ide.starter/src/com/intellij/ide/starter/plugins/PluginInstalledState.kt
+++
@@ -1,8 +0,0 @@
-package com.intellij.ide.starter.plugins
-
-enum class PluginInstalledState {
-  BUNDLED_TO_IDE,
-  INSTALLED,
-  NOT_INSTALLED,
-  DISABLED
-}
