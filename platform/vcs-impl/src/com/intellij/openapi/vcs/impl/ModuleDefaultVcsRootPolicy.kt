da86eedf9bc9c718556d500034f90182573b1314
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-07-19 13:31:45 +0200

    vcs: convert to Kotlin
    GitOrigin-RevId: c266de24f1eaadbb1b15a164c9edcfb6258070ee

--- platform/vcs-impl/src/com/intellij/openapi/vcs/impl/ModuleDefaultVcsRootPolicy.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/impl/ModuleDefaultVcsRootPolicy.kt
@@ -1,93 +1,76 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.vcs.impl;
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.vcs.impl
 
-import com.intellij.ProjectTopics;
-import com.intellij.openapi.application.ReadAction;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
-import com.intellij.openapi.project.ModuleListener;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.AdditionalLibraryRootsListener;
-import com.intellij.openapi.roots.ModuleRootEvent;
-import com.intellij.openapi.roots.ModuleRootListener;
-import com.intellij.openapi.roots.ModuleRootManager;
-import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.project.ProjectKt;
-import com.intellij.util.messages.MessageBusConnection;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.ProjectTopics
+import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.ModuleListener
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.AdditionalLibraryRootsListener
+import com.intellij.openapi.roots.ModuleRootEvent
+import com.intellij.openapi.roots.ModuleRootListener
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.project.isDirectoryBased
+import com.intellij.project.stateStore
+import org.jetbrains.annotations.Nls
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-final class ModuleDefaultVcsRootPolicy extends DefaultVcsRootPolicy {
-  ModuleDefaultVcsRootPolicy(@NotNull Project project) {
-    super(project);
-
-    MyModulesListener listener = new MyModulesListener();
-    MessageBusConnection connection = myProject.getMessageBus().connect();
-    connection.subscribe(ProjectTopics.MODULES, listener);
-    connection.subscribe(ProjectTopics.PROJECT_ROOTS, listener);
-    connection.subscribe(AdditionalLibraryRootsListener.TOPIC, listener);
+open class ModuleDefaultVcsRootPolicy(project: Project) : DefaultVcsRootPolicy(project) {
+  init {
+    val listener = MyModulesListener()
+    val connection = myProject.messageBus.connect()
+    connection.subscribe(ProjectTopics.MODULES, listener)
+    connection.subscribe(ProjectTopics.PROJECT_ROOTS, listener)
+    connection.subscribe(AdditionalLibraryRootsListener.TOPIC, listener)
   }
 
-  @Override
-  @NotNull
-  public Collection<VirtualFile> getDefaultVcsRoots() {
-    Set<VirtualFile> result = new HashSet<>();
-
-    VirtualFile baseDir = myProject.getBaseDir();
+  override fun getDefaultVcsRoots(): Collection<VirtualFile> {
+    val result: MutableSet<VirtualFile> = HashSet()
+    val baseDir = myProject.baseDir
     if (baseDir != null) {
-      result.add(baseDir);
+      result.add(baseDir)
     }
-
-    if (ProjectKt.isDirectoryBased(myProject) && baseDir != null) {
-      VirtualFile ideaDir = LocalFileSystem.getInstance().findFileByNioFile(ProjectKt.getStateStore(myProject).getDirectoryStorePath());
+    if (myProject.isDirectoryBased && baseDir != null) {
+      val ideaDir = LocalFileSystem.getInstance().findFileByNioFile(myProject.stateStore.directoryStorePath!!)
       if (ideaDir != null) {
-        result.add(ideaDir);
+        result.add(ideaDir)
       }
     }
 
     // assertion for read access inside
-    Module[] modules = ReadAction.compute(() -> ModuleManager.getInstance(myProject).getModules());
-    for (Module module : modules) {
-      ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
-      final VirtualFile[] files = moduleRootManager.getContentRoots();
-      for (VirtualFile file : files) {
-        if (file.isDirectory()) {
-          result.add(file);
+    val modules = ReadAction.compute<Array<Module>, RuntimeException> { ModuleManager.getInstance(myProject).modules }
+    for (module in modules) {
+      val moduleRootManager = ModuleRootManager.getInstance(module)
+      val files = moduleRootManager.contentRoots
+      for (file in files) {
+        if (file.isDirectory) {
+          result.add(file)
         }
       }
     }
-    return result;
+    return result
   }
 
-  private final class MyModulesListener implements ModuleRootListener, ModuleListener, AdditionalLibraryRootsListener {
-    @Override
-    public void rootsChanged(@NotNull ModuleRootEvent event) {
-      scheduleMappedRootsUpdate();
+  private inner class MyModulesListener : ModuleRootListener, ModuleListener, AdditionalLibraryRootsListener {
+    override fun rootsChanged(event: ModuleRootEvent) {
+      scheduleMappedRootsUpdate()
     }
 
-    @Override
-    public void modulesAdded(@NotNull Project project, @NotNull List<Module> modules) {
-      scheduleMappedRootsUpdate();
+    override fun moduleAdded(project: Project, module: Module) {
+      scheduleMappedRootsUpdate()
     }
 
-    @Override
-    public void moduleRemoved(@NotNull Project project, @NotNull Module module) {
-      scheduleMappedRootsUpdate();
+    override fun moduleRemoved(project: Project, module: Module) {
+      scheduleMappedRootsUpdate()
     }
 
-    @Override
-    public void libraryRootsChanged(@Nullable @Nls String presentableLibraryName,
-                                    @NotNull Collection<? extends VirtualFile> oldRoots,
-                                    @NotNull Collection<? extends VirtualFile> newRoots,
-                                    @NotNull String libraryNameForDebug) {
-      scheduleMappedRootsUpdate();
+    override fun libraryRootsChanged(presentableLibraryName: @Nls String?,
+                                     oldRoots: Collection<VirtualFile>,
+                                     newRoots: Collection<VirtualFile>,
+                                     libraryNameForDebug: String) {
+      scheduleMappedRootsUpdate()
     }
   }
 }
