7947506a5091a25862515a0c68b33390a45d3029
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2022-07-20 19:22:14 +0900

    Fix 'KotlinPreferencesBundle' (KTIJ-22202)
    GitOrigin-RevId: 6692c878883ec9d6d52036f7e7f468d58037b084

--- plugins/kotlin/preferences/src/org/jetbrains/kotlin/idea/preferences/KotlinPreferencesBundle.kt
+++ plugins/kotlin/preferences/src/org/jetbrains/kotlin/idea/preferences/KotlinPreferencesBundle.kt
@@ -4,7 +4,6 @@ package org.jetbrains.kotlin.idea.preferences
 import org.jetbrains.annotations.Nls
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
-import org.jetbrains.kotlin.idea.update.KotlinPluginUpdaterBundle
 import org.jetbrains.kotlin.util.AbstractKotlinBundle
 
 @NonNls
@@ -13,6 +12,5 @@ private const val BUNDLE = "messages.KotlinPreferencesBundle"
 object KotlinPreferencesBundle : AbstractKotlinBundle(BUNDLE) {
     @Nls
     @JvmStatic
-    fun message(@NonNls @PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String =
-        KotlinPluginUpdaterBundle.getMessage(key, *params)
+    fun message(@NonNls @PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String = getMessage(key, *params)
 }
