2272814607722407f4c786ee58e978f4c8b5e79e
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2022-07-21 21:09:10 +0200

    Refactoring: flatten nested ifs
    GitOrigin-RevId: 0f02d6044d96430528bd11796e1203e71c20f26b

--- images/src/org/intellij/images/fileTypes/ImageDocumentationProvider.java
+++ images/src/org/intellij/images/fileTypes/ImageDocumentationProvider.java
@@ -26,40 +26,41 @@ public class ImageDocumentationProvider extends AbstractDocumentationProvider {
 
   @Override
   public @Nls String generateDoc(PsiElement element, @Nullable PsiElement originalElement) {
-    if (element instanceof PsiFileSystemItem && !((PsiFileSystemItem)element).isDirectory()) {
-      final VirtualFile file = ((PsiFileSystemItem)element).getVirtualFile();
-      if (!DumbService.isDumb(element.getProject())) {
-        ImageInfo imageInfo = ImageInfoIndex.getInfo(file, element.getProject());
-        if (imageInfo != null) {
-          int imageWidth = imageInfo.width;
-          int imageHeight = imageInfo.height;
-
-          int maxSize = Math.max(imageInfo.width, imageInfo.height);
-          if (maxSize > MAX_IMAGE_SIZE) {
-            double scaleFactor = (double)MAX_IMAGE_SIZE / (double)maxSize;
-            imageWidth *= scaleFactor;
-            imageHeight *= scaleFactor;
-          }
-          try {
-            String path = file.getPath();
-            if (SystemInfo.isWindows) {
-              path = "/" + path;
-            }
-            final String url = new URI("file", null, path, null).toString();
-            HtmlChunk.Element img = HtmlChunk.tag("img")
-              .attr("src", url)
-              .attr("width", imageWidth)
-              .attr("height", imageHeight);
-            String message = ImagesBundle.message("image.description", imageInfo.width, imageInfo.height, imageInfo.bpp);
-            return new HtmlBuilder().append(img).append(HtmlChunk.p().addText(message)).toString();
-          }
-          catch (URISyntaxException ignored) {
-            // nothing
-          }
-        }
-      }
+    if (!(element instanceof PsiFileSystemItem) || ((PsiFileSystemItem)element).isDirectory()) {
+      return null;
     }
+    final VirtualFile file = ((PsiFileSystemItem)element).getVirtualFile();
+    if (!DumbService.isDumb(element.getProject())) {
+      return null;
+    }
+    ImageInfo imageInfo = ImageInfoIndex.getInfo(file, element.getProject());
+    if (imageInfo == null) {
+      return null;
+    }
+    int imageWidth = imageInfo.width;
+    int imageHeight = imageInfo.height;
 
-    return null;
+    int maxSize = Math.max(imageInfo.width, imageInfo.height);
+    if (maxSize > MAX_IMAGE_SIZE) {
+      double scaleFactor = (double)MAX_IMAGE_SIZE / (double)maxSize;
+      imageWidth *= scaleFactor;
+      imageHeight *= scaleFactor;
+    }
+    try {
+      String path = file.getPath();
+      if (SystemInfo.isWindows) {
+        path = "/" + path;
+      }
+      final String url = new URI("file", null, path, null).toString();
+      HtmlChunk.Element img = HtmlChunk.tag("img")
+        .attr("src", url)
+        .attr("width", imageWidth)
+        .attr("height", imageHeight);
+      String message = ImagesBundle.message("image.description", imageInfo.width, imageInfo.height, imageInfo.bpp);
+      return new HtmlBuilder().append(img).append(HtmlChunk.p().addText(message)).toString();
+    }
+    catch (URISyntaxException ignored) {
+      return null;
+    }
   }
 }
