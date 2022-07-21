1a5075cd054931d486055859cd96cba2596ed6dd
Anna Kozlova <anna.kozlova@jetbrains.com>
2022-07-21 16:01:51 +0200

    [kotlin] nothing can happen on commitDocument right after psi changes
    if document was not committed, changes won't happen; otherwise it would just do nothing - let's delete!
    
    GitOrigin-RevId: 951bc15e7bafd06e9bca738ca0422385e2a8267b

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/ConvertReceiverToParameterIntention.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/ConvertReceiverToParameterIntention.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.intentions
 
@@ -13,6 +13,7 @@ import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.codeinsight.api.classic.intentions.SelfTargetingOffsetIndependentIntention
+import org.jetbrains.kotlin.idea.codeinsight.utils.ChooseStringExpression
 import org.jetbrains.kotlin.idea.core.getOrCreateValueParameterList
 import org.jetbrains.kotlin.idea.refactoring.changeSignature.*
 import org.jetbrains.kotlin.idea.refactoring.resolveToExpectedDescriptorIfPossible
@@ -24,7 +25,6 @@ import org.jetbrains.kotlin.psi.KtPsiFactory
 import org.jetbrains.kotlin.psi.KtTypeReference
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
 import org.jetbrains.kotlin.psi.typeRefHelpers.setReceiverTypeReference
-import org.jetbrains.kotlin.idea.codeinsight.utils.ChooseStringExpression
 
 class ConvertReceiverToParameterIntention : SelfTargetingOffsetIndependentIntention<KtTypeReference>(
     KtTypeReference::class.java,
@@ -80,7 +80,6 @@ class ConvertReceiverToParameterIntention : SelfTargetingOffsetIndependentIntent
                             runWriteAction {
                                 function.setReceiverTypeReference(addedParameter.typeReference)
                                 function.valueParameterList!!.removeParameter(addedParameter)
-                                PsiDocumentManager.getInstance(project).commitDocument(editor.document)
                             }
                         }
 
