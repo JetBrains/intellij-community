b648896f46d678d36d9d58ec1aec85a990cb0c1f
Roman Ivanov <roman.ivanov@jetbrains.com>
2022-07-21 15:10:44 +0200

    [java] StaticInheritanceFix: add preview to inspection (only for class)
    GitOrigin-RevId: b6b79e759e594a8970b194c6baa34f68f2a7043d

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/inheritance/StaticInheritanceFix.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/inheritance/StaticInheritanceFix.java
@@ -17,6 +17,7 @@ package com.siyeh.ig.inheritance;
 
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInsight.intention.QuickFixFactory;
+import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.java.analysis.JavaAnalysisBundle;
 import com.intellij.openapi.application.ReadAction;
@@ -27,6 +28,7 @@ import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.DebugUtil;
+import com.intellij.psi.search.LocalSearchScope;
 import com.intellij.psi.search.SearchScope;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.psi.util.InheritanceUtil;
@@ -65,6 +67,10 @@ class StaticInheritanceFix extends InspectionGadgetsFix {
 
   @Override
   public void doFix(final Project project, ProblemDescriptor descriptor) {
+    doFix(project, descriptor, false);
+  }
+
+  private void doFix(final Project project, ProblemDescriptor descriptor, boolean inPreview) {
     final PsiJavaCodeReferenceElement referenceElement = (PsiJavaCodeReferenceElement)descriptor.getPsiElement();
     final PsiClass iface = (PsiClass)referenceElement.resolve();
     assert iface != null;
@@ -74,54 +80,84 @@ class StaticInheritanceFix extends InspectionGadgetsFix {
     assert implementingClass != null;
     final PsiFile file = implementingClass.getContainingFile();
 
-    ProgressManager.getInstance().run(new Task.Modal(project,
-                                                     JavaAnalysisBundle.message("static.inheritrance.fix.replace.progress", iface.getName()), false) {
+    if (inPreview) {
+      processUsages(allFields, implementingClass, project, true, iface, file);
+    } else {
+      ProgressManager.getInstance().run(new Task.Modal(project,
+                                                       JavaAnalysisBundle.message("static.inheritrance.fix.replace.progress", iface.getName()), false) {
 
-      @Override
-      public void run(@NotNull ProgressIndicator indicator) {
-        for (final PsiField field : allFields) {
-          SearchScope scope = ReadAction.compute(() -> implementingClass.getUseScope());
-          final Query<PsiReference> search = ReferencesSearch.search(field, scope, false);
-          for (PsiReference reference : search) {
-            if (!(reference instanceof PsiReferenceExpression)) {
-              continue;
-            }
-            final PsiReferenceExpression referenceExpression = (PsiReferenceExpression)reference;
-            if (!myReplaceInWholeProject) {
-              boolean isInheritor =
-                ReadAction.compute(() -> {
-                  boolean isInheritor1 = false;
-                  PsiClass aClass = PsiTreeUtil.getParentOfType(referenceExpression, PsiClass.class);
-                  while (aClass != null) {
-                    isInheritor1 = InheritanceUtil.isInheritorOrSelf(aClass, implementingClass, true);
-                    if (isInheritor1) break;
-                    aClass = PsiTreeUtil.getParentOfType(aClass, PsiClass.class);
-                  }
-                  return isInheritor1;
-                });
-              if (!isInheritor) continue;
-            }
-            final Runnable runnable = () -> {
-              final PsiElementFactory elementFactory = JavaPsiFacade.getElementFactory(project);
-              final PsiReferenceExpression qualified = (PsiReferenceExpression)
-                elementFactory.createExpressionFromText("xxx." + referenceExpression.getText(), referenceExpression);
-              final PsiReferenceExpression newReference = (PsiReferenceExpression)referenceExpression.replace(qualified);
-              final PsiReferenceExpression qualifier = (PsiReferenceExpression)newReference.getQualifierExpression();
-              assert qualifier != null : DebugUtil.psiToString(newReference, true);
-              final PsiClass containingClass = field.getContainingClass();
-              qualifier.bindToElement(containingClass);
-            };
-            WriteCommandAction.runWriteCommandAction(project, null, null, runnable,
-                                                     ReadAction.compute(() -> referenceExpression.getContainingFile()));
-          }
+        @Override
+        public void run(@NotNull ProgressIndicator indicator) {
+          processUsages(allFields, implementingClass, project, false, iface, file);
+        }
+      });
+    }
+  }
+
+  private void processUsages(PsiField[] allFields,
+                         PsiClass implementingClass,
+                         Project project,
+                         boolean inPreview,
+                         PsiClass iface,
+                         PsiFile file) {
+    for (final PsiField field : allFields) {
+      SearchScope scope = ReadAction.compute(() -> implementingClass.getUseScope());
+      if (inPreview) {
+        scope = scope.intersectWith(new LocalSearchScope(file));
+      }
+      final Query<PsiReference> search = ReferencesSearch.search(field, scope, false);
+      for (PsiReference reference : search) {
+        if (!(reference instanceof PsiReferenceExpression)) {
+          continue;
+        }
+        final PsiReferenceExpression referenceExpression = (PsiReferenceExpression)reference;
+        if (!myReplaceInWholeProject) {
+          boolean isInheritor =
+            ReadAction.compute(() -> {
+              boolean isInheritor1 = false;
+              PsiClass aClass = PsiTreeUtil.getParentOfType(referenceExpression, PsiClass.class);
+              while (aClass != null) {
+                isInheritor1 = InheritanceUtil.isInheritorOrSelf(aClass, implementingClass, true);
+                if (isInheritor1) break;
+                aClass = PsiTreeUtil.getParentOfType(aClass, PsiClass.class);
+              }
+              return isInheritor1;
+            });
+          if (!isInheritor) continue;
         }
         final Runnable runnable = () -> {
-          PsiClassType classType = JavaPsiFacade.getInstance(project).getElementFactory().createType(iface);
-          IntentionAction fix = QuickFixFactory.getInstance().createExtendsListFix(implementingClass, classType, false);
-          fix.invoke(project, null, file);
+          final PsiElementFactory elementFactory = JavaPsiFacade.getElementFactory(project);
+          final PsiReferenceExpression qualified = (PsiReferenceExpression)
+            elementFactory.createExpressionFromText("xxx." + referenceExpression.getText(), referenceExpression);
+          final PsiReferenceExpression newReference = (PsiReferenceExpression)referenceExpression.replace(qualified);
+          final PsiReferenceExpression qualifier = (PsiReferenceExpression)newReference.getQualifierExpression();
+          assert qualifier != null : DebugUtil.psiToString(newReference, true);
+          final PsiClass containingClass = field.getContainingClass();
+          qualifier.bindToElement(containingClass);
         };
-        WriteCommandAction.runWriteCommandAction(project, null, null, runnable, file);
+        if (inPreview) {
+          runnable.run();
+        } else {
+          WriteCommandAction.runWriteCommandAction(project, null, null, runnable,
+                                                   ReadAction.compute(() -> referenceExpression.getContainingFile()));
+        }
       }
-    });
+    }
+    final Runnable runnable = () -> {
+      PsiClassType classType = JavaPsiFacade.getInstance(project).getElementFactory().createType(iface);
+      IntentionAction fix = QuickFixFactory.getInstance().createExtendsListFix(implementingClass, classType, false);
+      fix.invoke(project, null, file);
+    };
+    if (inPreview) {
+      runnable.run();
+    } else {
+      WriteCommandAction.runWriteCommandAction(project, null, null, runnable, file);
+    }
+  }
+
+  @Override
+  public @NotNull IntentionPreviewInfo generatePreview(@NotNull Project project, @NotNull ProblemDescriptor previewDescriptor) {
+    doFix(project, previewDescriptor, true);
+    return IntentionPreviewInfo.DIFF;
   }
 }
