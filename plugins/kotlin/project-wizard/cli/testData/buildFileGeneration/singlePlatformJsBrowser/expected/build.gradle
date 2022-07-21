3cdd28c9277493564c1357afd90a676d761a5c69
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2022-07-21 11:59:38 +0200

    [Kotlin/JS, Project Wizard] Fix cssSupport usages
    (cherry picked from commit 9db8f4904b573b5dc7b81cfc89507ee90d122594)
    
    IJ-MR-27668
    
    (cherry picked from commit 51228b644e4993ffaad718e1b7ab6de339abeccd)
    
    GitOrigin-RevId: ef2b14962e9e3934cd08419054630c7c7e341276

--- plugins/kotlin/project-wizard/cli/testData/buildFileGeneration/singlePlatformJsBrowser/expected/build.gradle
+++ plugins/kotlin/project-wizard/cli/testData/buildFileGeneration/singlePlatformJsBrowser/expected/build.gradle
@@ -18,7 +18,9 @@ kotlin {
         binaries.executable()
         browser {
             commonWebpackConfig {
-                cssSupport.enabled = true
+                cssSupport {
+                    enabled.set(true)
+                }
             }
         }
     }
