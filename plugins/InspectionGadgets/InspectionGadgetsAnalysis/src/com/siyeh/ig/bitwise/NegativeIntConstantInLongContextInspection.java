5a2dac4e71df4778448737930ac5b03ec89a36ea
Tagir Valeev <tagir.valeev@jetbrains.com>
2022-07-22 17:46:32 +0200

    [java-inspection] Minor fixes for NegativeIntConstantInLongContextInspection
    GitOrigin-RevId: 9ee5da890cba477dc7bce277f8eb0e51c069c0dc

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/bitwise/NegativeIntConstantInLongContextInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/bitwise/NegativeIntConstantInLongContextInspection.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2003-2021 Dave Griffith, Bas Leijdekkers
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.siyeh.ig.bitwise;
 
 import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool;
@@ -69,7 +55,8 @@ public class NegativeIntConstantInLongContextInspection extends AbstractBaseJava
 
   private static boolean isNegativeHexLiteral(@NotNull PsiLiteralExpression literal) {
     if (!PsiType.INT.equals(literal.getType())) return false;
-    if (!literal.getText().startsWith("0x")) return false;
+    String text = literal.getText();
+    if (!text.startsWith("0x") && !text.startsWith("0X")) return false;
     Integer value = tryCast(literal.getValue(), Integer.class);
     return value != null && value < 0;
   }
