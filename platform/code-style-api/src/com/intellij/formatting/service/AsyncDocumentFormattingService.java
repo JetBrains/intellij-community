d871ceeaf1d19a1b80e2324dfb1f06a2ab8bf437
Mateusz Nowakowski <mateusz.nowakowski@jetbrains.com>
2022-07-22 16:56:46 +0200

    RUBY-30245: Implement support for external formatters in RubyMine with RuboCop as external formatter
    Write service running RuboCop server in Ruby modules
    Write `RubocopFormattingService` extending `AsyncDocumentFormattingService`
    Add preferences in UI allowing user to enable and disable external formatters in Ruby.
    
    IJ-CR-27760
    
    GitOrigin-RevId: f6ee190f03c5b9629f7929bc4083ebcb68f234b3

--- platform/code-style-api/src/com/intellij/formatting/service/AsyncDocumentFormattingService.java
+++ platform/code-style-api/src/com/intellij/formatting/service/AsyncDocumentFormattingService.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.formatting.service;
 
 import com.intellij.CodeStyleBundle;
@@ -171,9 +171,9 @@ public abstract class AsyncDocumentFormattingService extends AbstractDocumentFor
       myDocument = document;
       myRanges = ranges;
       myCanChangeWhitespaceOnly = canChangeWhitespaceOnly;
-      myInitialModificationStamp = document.getModificationStamp();
       myQuickFormat = quickFormat;
       FileDocumentManager.getInstance().saveDocument(myDocument);
+      myInitialModificationStamp = document.getModificationStamp();
     }
 
     @Override
