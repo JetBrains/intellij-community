614b911e89b83b166c8783df8ad62b879053e75f
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-07-24 16:22:34 +0200

    [kotlin] move test generation description in k2 test generator to separate file
    GitOrigin-RevId: ffaee067936c1c1a8f48b9369ea70fe1ace1ad34

--- plugins/kotlin/util/test-generator-fir/test/org/jetbrains/kotlin/fir/testGenerator/FirGenerateTests.kt
+++ plugins/kotlin/util/test-generator-fir/test/org/jetbrains/kotlin/fir/testGenerator/FirGenerateTests.kt
@@ -49,6 +49,7 @@ internal fun generateTests(isUpToDateCheck: Boolean = false) {
 
 private fun assembleWorkspace(): TWorkspace = workspace {
     generateK2CodeInsightTests()
+    generateK2Fe10BindingsTests()
 
     testGroup("base/fir/analysis-api-providers") {
         testClass<AbstractProjectWideOutOfBlockKotlinModificationTrackerTest> {
@@ -197,56 +198,6 @@ private fun assembleWorkspace(): TWorkspace = workspace {
 
         testClass<AbstractHLImplementationSearcherTest> {
             model("search/implementations", pattern = KT_WITHOUT_DOTS)
-        }
-    }
-
-
-    testGroup("k2-fe10-bindings", testDataPath = "../idea/tests") {
-        testClass<AbstractFe10BindingIntentionTest> {
-            val pattern = Patterns.forRegex("^([\\w\\-_]+)\\.(kt|kts)$")
-            model("testData/intentions/conventionNameCalls", pattern = pattern)
-            model("testData/intentions/convertSecondaryConstructorToPrimary", pattern = pattern)
-            model("testData/intentions/convertToStringTemplate", pattern = pattern)
-            model("testData/intentions/convertTryFinallyToUseCall", pattern = pattern)
-            model("testData/intentions/removeRedundantCallsOfConversionMethods", pattern = pattern)
-        }
-    }
-
-    testGroup("k2-fe10-bindings", testDataPath = "../idea/tests") {
-        testClass<AbstractFe10BindingLocalInspectionTest> {
-            val pattern = Patterns.forRegex("^([\\w\\-_]+)\\.(kt|kts)$")
-            model("testData/inspectionsLocal/addOperatorModifier", pattern = pattern)
-            model("testData/inspectionsLocal/booleanLiteralArgument", pattern = pattern)
-            model("testData/inspectionsLocal/convertSealedSubClassToObject", pattern = pattern)
-            model("testData/inspectionsLocal/cascadeIf", pattern = pattern)
-            model("testData/inspectionsLocal/collections/convertCallChainIntoSequence", pattern = pattern)
-            model("testData/inspectionsLocal/convertNaNEquality", pattern = pattern)
-            model("testData/inspectionsLocal/convertPairConstructorToToFunction", pattern = pattern)
-            model("testData/inspectionsLocal/copyWithoutNamedArguments", pattern = pattern)
-            model("testData/inspectionsLocal/branched/introduceWhenSubject", pattern = pattern)
-            model("testData/inspectionsLocal/kdocMissingDocumentation", pattern = pattern)
-            model("testData/inspectionsLocal/forEachParameterNotUsed", pattern = pattern)
-            model("testData/inspectionsLocal/covariantEquals", pattern = pattern)
-            model("testData/inspectionsLocal/lateinitVarOverridesLateinitVar", pattern = pattern)
-            model("testData/inspectionsLocal/foldInitializerAndIfToElvis", pattern = pattern)
-            model("testData/inspectionsLocal/mapGetWithNotNullAssertionOperator", pattern = pattern)
-            model("testData/inspectionsLocal/memberVisibilityCanBePrivate", pattern = pattern)
-            model("testData/inspectionsLocal/redundantObjectTypeCheck", pattern = pattern)
-            model("testData/inspectionsLocal/redundantSuspend", pattern = pattern)
-            model("testData/inspectionsLocal/redundantExplicitType", pattern = pattern)
-            model("testData/inspectionsLocal/replaceArrayEqualityOpWithArraysEquals", pattern = pattern)
-            model("testData/inspectionsLocal/replaceAssociateFunction", pattern = pattern)
-            model("testData/inspectionsLocal/replaceIsEmptyWithIfEmpty", pattern = pattern)
-        }
-    }
-
-    testGroup("k2-fe10-bindings", testDataPath = "../idea/tests") {
-        testClass<AbstractFe10BindingQuickFixTest> {
-            val pattern = Patterns.forRegex("^([\\w\\-_]+)\\.kt$")
-            model("testData/quickfix/addVarianceModifier", pattern = pattern)
-            model("testData/quickfix/canBePrimaryConstructorProperty", pattern = pattern)
-            model("testData/quickfix/kdocMissingDocumentation", pattern = pattern)
-            model("testData/quickfix/memberVisibilityCanBePrivate", pattern = pattern)
         }
     }
 
