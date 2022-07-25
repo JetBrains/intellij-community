5aec2bcceca2d590020966346f8f6f3d0d7fe017
Egor Malyshev <egor.malyshev@jetbrains.com>
2022-07-25 12:25:42 +0000

    WH-4290-custom-plugin-links More post-review updates
    Post-review changes
    
    Supporting custom protocol to access help links via JetBrains redirector, step 2
    
    Supporting custom protocol to access help links via JetBrains redirector
    
    Merge-request: IJ-MR-27273
    Merged-by: Egor Malyshev <egor.malyshev@jetbrains.com>
    
    IJ-MR-27273
    
    GitOrigin-RevId: 6b2421594062802bb870c6f434bbe30ba96a5abd

---
+++ platform/platform-api/src/com/intellij/ui/HelpIdAwareLinkListener.java
@@ -0,0 +1,73 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ui;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.application.ApplicationInfo;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ApplicationNamesInfo;
+import com.intellij.openapi.components.Service;
+import com.intellij.openapi.util.text.StringUtil;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.event.HyperlinkEvent;
+
+@Service
+@ApiStatus.Experimental
+public final class HelpIdAwareLinkListener extends BrowserHyperlinkListener {
+
+  @NonNls private static final String HELP_LINK_MARKER = "helpInstance:";
+  @NonNls private static final String URL_TEMPLATE = "https://www.jetbrains.com/help/%s?%s&utm_version=%s";
+
+  @NotNull
+  public static HelpIdAwareLinkListener getInstance() {
+    return ApplicationManager.getApplication().getService(HelpIdAwareLinkListener.class);
+  }
+
+  @Override
+  protected void hyperlinkActivated(@NotNull HyperlinkEvent e) {
+
+    final String description = e.getDescription();
+
+    if (description != null && description.trim().startsWith(HELP_LINK_MARKER)) {
+
+      final String wouldBeHelpId = description.trim().substring(HELP_LINK_MARKER.length()).trim();
+
+      if (StringUtil.isNotEmpty(wouldBeHelpId)) {
+        final ApplicationNamesInfo nameInfo = ApplicationNamesInfo.getInstance();
+        final String editionName = nameInfo.getEditionName();
+
+        final String productWebPath;
+
+        final String productName = StringUtil.toLowerCase(nameInfo.getProductName());
+        switch (productName) {
+
+          case "rubymine":
+          case "ruby":
+            productWebPath = "ruby";
+            break;
+          case "intellij idea":
+          case "idea":
+            productWebPath = "idea";
+            break;
+          case "goland":
+            productWebPath = "go";
+            break;
+          case "appcode":
+            productWebPath = "objc";
+            break;
+          case "pycharm":
+            productWebPath = editionName != null && "edu".equals(StringUtil.toLowerCase(editionName)) ? "pycharm-edu" : "pycharm";
+            break;
+          default:
+            productWebPath = productName;
+        }
+
+        BrowserUtil.browse(String.format(URL_TEMPLATE, productWebPath, wouldBeHelpId, ApplicationInfo.getInstance().getShortVersion()));
+        return;
+      }
+    }
+    super.hyperlinkActivated(e);
+  }
+}
