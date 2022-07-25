22b0d1c72798306d8c7d40af2b3d8c0404f328de
Ivan Posti <ivan.posti@jetbrains.com>
2022-07-25 17:14:20 +0200

    [Markdown][IDEA-292094] Reposition copy code fence content button
    Also fixes IDEA-283074.
    
    (cherry picked from commit 6c3f4d3c09e92f8e86986ac1d12a817dbe4500db)
    
    IJ-CR-94690
    
    GitOrigin-RevId: ded1a715d8968505288ffe7e6559e613e53d82a3

--- plugins/markdown/core/src/org/intellij/plugins/markdown/ui/preview/html/DefaultCodeFenceGeneratingProvider.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/ui/preview/html/DefaultCodeFenceGeneratingProvider.kt
@@ -50,6 +50,7 @@ internal class DefaultCodeFenceGeneratingProvider(
     val indentBefore = node.getTextInNode(text).commonPrefixWith(" ".repeat(10)).length
 
     visitor.consumeHtml("<pre class=\"code-fence\" ${HtmlGenerator.getSrcPosAttribute(node)}>")
+    addCopyButton(visitor, collectFenceText(node, text).trim())
 
     var state = 0
 
@@ -81,7 +82,6 @@ internal class DefaultCodeFenceGeneratingProvider(
     }
 
     val rawContentResult = codeFenceRawContent.toString()
-    addCopyButton(visitor, rawContentResult)
     if (state == 1) {
       visitor.consumeHtml(
         pluginGeneratedHtml(language, codeFenceContent.toString(), rawContentResult, node)
@@ -97,19 +97,38 @@ internal class DefaultCodeFenceGeneratingProvider(
     visitor.consumeHtml("</code></pre>")
   }
 
+  private fun collectFenceText(node: ASTNode, allText: String): String {
+    return buildString {
+      collectFenceText(this, node, allText)
+    }
+  }
+
+  private fun collectFenceText(builder: StringBuilder, node: ASTNode, allText: String) {
+    if (node.type == MarkdownTokenTypes.CODE_FENCE_CONTENT || node.type == MarkdownTokenTypes.EOL) {
+      builder.append(codeFenceRawText(allText, node))
+    }
+    for (child in node.children) {
+      collectFenceText(builder, child, allText)
+    }
+  }
+
   private fun addCopyButton(visitor: HtmlGenerator.HtmlGeneratingVisitor, content: String) {
     val encodedContent = Base64.encode(content.toByteArray())
     // language=HTML
     val html = """
     <div class="code-fence-highlighter-copy-button" data-fence-content="$encodedContent">
-        <img class="code-fence-highlighter-copy-button-icon">
+      <img class="code-fence-highlighter-copy-button-icon">
     </div>
     """.trimIndent()
     visitor.consumeHtml(html)
   }
 
-  private fun codeFenceRawText(text: String, node: ASTNode): CharSequence =
-    if (node.type != MarkdownTokenTypes.BLOCK_QUOTE) node.getTextInNode(text) else ""
+  private fun codeFenceRawText(text: String, node: ASTNode): CharSequence {
+    return when (node.type) {
+      MarkdownTokenTypes.BLOCK_QUOTE -> ""
+      else -> node.getTextInNode(text)
+    }
+  }
 
   private fun codeFenceText(text: String, node: ASTNode): CharSequence =
     if (node.type != MarkdownTokenTypes.BLOCK_QUOTE) HtmlGenerator.leafText(text, node, false) else ""
