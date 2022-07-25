cc2a32e150f65eec63e15c08dac62dd3e06b497b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-07-25 19:43:38 +0200

    IDEA-298136 The project appears as untrusted always on re-opening
    GitOrigin-RevId: fc642ff5af905ab73aa3dc94407c3a901da5af38

--- platform/projectModel-api/src/com/intellij/openapi/components/SerializablePersistentStateComponent.kt
+++ platform/projectModel-api/src/com/intellij/openapi/components/SerializablePersistentStateComponent.kt
@@ -1,86 +1,78 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.components;
+package com.intellij.openapi.components
 
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.extensions.ExtensionNotApplicableException;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.extensions.ExtensionNotApplicableException
+import org.jetbrains.annotations.ApiStatus
+import java.lang.invoke.MethodHandles
+import java.lang.invoke.VarHandle
 
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.VarHandle;
-import java.util.function.UnaryOperator;
-
-@SuppressWarnings({"unchecked", "FieldMayBeFinal"})
 @ApiStatus.Experimental
-public abstract class SerializablePersistentStateComponent<T> implements PersistentStateComponentWithModificationTracker<T> {
+abstract class SerializablePersistentStateComponent<T : Any> protected constructor(private var state: T)
+  : PersistentStateComponentWithModificationTracker<T> {
+  companion object {
+    private val STATE_HANDLE: VarHandle
+    private val TIMESTAMP_HANDLE: VarHandle
 
-  private static final @NotNull VarHandle STATE_HANDLE;
-  private static final @NotNull VarHandle TIMESTAMP_HANDLE;
-
-  static {
-    try {
-      MethodHandles.Lookup lookup = MethodHandles.privateLookupIn(SerializablePersistentStateComponent.class,
-                                                                  MethodHandles.lookup());
-
-      STATE_HANDLE = lookup.findVarHandle(SerializablePersistentStateComponent.class,
-                                          "myState",
-                                          Object.class);
-
-      TIMESTAMP_HANDLE = lookup.findVarHandle(SerializablePersistentStateComponent.class,
-                                              "myTimestamp",
-                                              long.class);
+    init {
+      try {
+        val lookup = MethodHandles.privateLookupIn(SerializablePersistentStateComponent::class.java, MethodHandles.lookup())
+        STATE_HANDLE = lookup.findVarHandle(SerializablePersistentStateComponent::class.java, "state", Any::class.java)
+        TIMESTAMP_HANDLE = lookup.findVarHandle(SerializablePersistentStateComponent::class.java, "timestamp",
+                                                Long::class.javaPrimitiveType)
+      }
+      catch (e: IllegalAccessException) {
+        Logger.getInstance(SerializablePersistentStateComponent::class.java).error(e)
+        throw ExtensionNotApplicableException.create()
+      }
+      catch (e: NoSuchFieldException) {
+        Logger.getInstance(SerializablePersistentStateComponent::class.java).error(e)
+        throw ExtensionNotApplicableException.create()
+      }
     }
-    catch (IllegalAccessException | NoSuchFieldException e) {
-      Logger.getInstance(SerializablePersistentStateComponent.class).error(e);
-      throw ExtensionNotApplicableException.create();
+
+    @PublishedApi
+    internal fun compareAndSet(component: SerializablePersistentStateComponent<*>, prev: Any, next: Any?): Boolean {
+      if (STATE_HANDLE.weakCompareAndSet(component, prev, next)) {
+        TIMESTAMP_HANDLE.getAndAdd(component, 1L)
+        return true
+      }
+      return false
     }
   }
 
-  private @NotNull T myState;
-  private long myTimestamp = 0L;
+  private var timestamp = 0L
 
-  protected SerializablePersistentStateComponent(@NotNull T state) {
-    myState = state;
+  @Suppress("UNCHECKED_CAST")
+  override fun getState(): T = STATE_HANDLE.getVolatile(this) as T
+
+  fun setState(newState: T) {
+    STATE_HANDLE.setVolatile(this, newState)
   }
 
-  @Override
-  public final @NotNull T getState() {
-    return (T)STATE_HANDLE.getVolatile(this);
+  override fun loadState(state: T) {
+    setState(state)
   }
 
-  public final void setState(@NotNull T state) {
-    STATE_HANDLE.setVolatile(this, state);
-  }
-
-  @Override
-  public final void loadState(@NotNull T state) {
-    setState(state);
-  }
-
-  @Override
-  public final long getStateModificationCount() {
-    return (long)TIMESTAMP_HANDLE.get(this);
-  }
+  override fun getStateModificationCount(): Long = TIMESTAMP_HANDLE.getVolatile(this) as Long
 
   /**
-   * See {@link java.util.concurrent.atomic.AtomicReference#updateAndGet(UnaryOperator)}.
+   * See [java.util.concurrent.atomic.AtomicReference.updateAndGet].
    *
    * @param updateFunction a function to merge states
    */
-  protected final void updateState(@NotNull UnaryOperator<T> updateFunction) {
-    T prev = getState(), next = null;
-
-    for (boolean haveNext = false; ; ) {
+  protected fun updateState(updateFunction: (currentState: T) -> T) {
+    var prev = getState()
+    var next: T? = null
+    var haveNext = false
+    while (true) {
       if (!haveNext) {
-        next = updateFunction.apply(prev);
+        next = updateFunction(prev)
       }
-
-      if (STATE_HANDLE.weakCompareAndSet(this, prev, next)) {
-        TIMESTAMP_HANDLE.getAndAdd(this, 1L);
-        break;
+      if (compareAndSet(this, prev, next)) {
+        break
       }
-
-      haveNext = (prev == (prev = getState()));
+      haveNext = prev === getState().also { prev = it }
     }
   }
 }
