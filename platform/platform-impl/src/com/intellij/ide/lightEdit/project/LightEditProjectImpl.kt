3cd517b36c641d3148a9e15f831cef1cfdef5f5b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-07-25 17:16:09 +0200

    cleanup
    GitOrigin-RevId: 8b4a4d20c48148f9db4f88cbfc902b05a0d35ee4

--- platform/platform-impl/src/com/intellij/ide/lightEdit/project/LightEditProjectImpl.kt
+++ platform/platform-impl/src/com/intellij/ide/lightEdit/project/LightEditProjectImpl.kt
@@ -1,94 +1,83 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ide.lightEdit.project;
+package com.intellij.ide.lightEdit.project
 
-import com.intellij.ide.lightEdit.LightEditCompatible;
-import com.intellij.ide.plugins.IdeaPluginDescriptor;
-import com.intellij.ide.plugins.PluginManagerCore;
-import com.intellij.openapi.application.PathManager;
-import com.intellij.openapi.components.ServiceDescriptor;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.project.DumbService;
-import com.intellij.openapi.project.impl.ProjectImpl;
-import com.intellij.openapi.roots.FileIndexFacade;
-import com.intellij.openapi.roots.ProjectFileIndex;
-import com.intellij.openapi.roots.impl.DirectoryIndex;
-import kotlin.Unit;
-import kotlin.coroutines.EmptyCoroutineContext;
-import kotlinx.coroutines.BuildersKt;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.ide.lightEdit.LightEditCompatible
+import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.openapi.application.PathManager
+import com.intellij.openapi.components.ServiceDescriptor
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.project.impl.ProjectImpl
+import com.intellij.openapi.roots.FileIndexFacade
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.roots.impl.DirectoryIndex
+import kotlinx.coroutines.runBlocking
+import java.io.File
+import java.nio.file.Path
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+internal class LightEditProjectImpl private constructor(projectPath: Path) : ProjectImpl(projectPath, NAME), LightEditCompatible {
+  companion object {
+    private val LOG = logger<LightEditProjectImpl>()
+    private const val NAME = "LightEditProject"
 
-final class LightEditProjectImpl extends ProjectImpl implements LightEditCompatible {
-  private static final Logger LOG = Logger.getInstance(LightEditProjectImpl.class);
-  private static final String NAME = "LightEditProject";
-
-  LightEditProjectImpl() {
-    this(getProjectPath());
+    private val projectPath: Path
+      get() = Path.of(PathManager.getConfigPath() + File.separator + "light-edit")
   }
 
-  private LightEditProjectImpl(@NotNull Path projectPath) {
-    super(projectPath, NAME);
+  constructor() : this(projectPath)
 
-    registerComponents();
-    customizeRegisteredComponents();
-    getComponentStore().setPath(projectPath, false, null);
-    try {
-      BuildersKt.runBlocking(EmptyCoroutineContext.INSTANCE, (scope, continuation) -> {
-        init$intellij_platform_ide_impl(true, continuation);
-        return Unit.INSTANCE;
-      });
-    }
-    catch (InterruptedException e) {
-      throw new RuntimeException(e);
+  init {
+    registerComponents()
+    customizeRegisteredComponents()
+    componentStore.setPath(projectPath, false, null)
+    runBlocking {
+      init(preloadServices = true)
     }
   }
 
-  private void customizeRegisteredComponents() {
-    IdeaPluginDescriptor pluginDescriptor = PluginManagerCore.getPlugin(PluginManagerCore.CORE_ID);
+  private fun customizeRegisteredComponents() {
+    val pluginDescriptor = PluginManagerCore.getPlugin(PluginManagerCore.CORE_ID)
     if (pluginDescriptor == null) {
-      LOG.error("Could not find plugin by id: " + PluginManagerCore.CORE_ID);
-      return;
+      LOG.error("Could not find plugin by id: ${PluginManagerCore.CORE_ID}")
+      return
     }
-    registerService(DirectoryIndex.class, LightEditDirectoryIndex.class, pluginDescriptor, true, ServiceDescriptor.PreloadMode.FALSE);
-    registerService(ProjectFileIndex.class, LightEditProjectFileIndex.class, pluginDescriptor, true, ServiceDescriptor.PreloadMode.FALSE);
-    registerService(FileIndexFacade.class, LightEditFileIndexFacade.class, pluginDescriptor, true, ServiceDescriptor.PreloadMode.FALSE);
-    registerService(DumbService.class, LightEditDumbService.class, pluginDescriptor, true, ServiceDescriptor.PreloadMode.FALSE);
-    registerComponent(FileEditorManager.class, LightEditFileEditorManagerImpl.class, pluginDescriptor, true);
+
+    registerService(serviceInterface = DirectoryIndex::class.java,
+                    implementation = LightEditDirectoryIndex::class.java,
+                    pluginDescriptor = pluginDescriptor,
+                    override = true,
+                    preloadMode = ServiceDescriptor.PreloadMode.FALSE)
+    registerService(serviceInterface = ProjectFileIndex::class.java,
+                    implementation = LightEditProjectFileIndex::class.java,
+                    pluginDescriptor = pluginDescriptor,
+                    override = true,
+                    preloadMode = ServiceDescriptor.PreloadMode.FALSE)
+    registerService(serviceInterface = FileIndexFacade::class.java,
+                    implementation = LightEditFileIndexFacade::class.java,
+                    pluginDescriptor = pluginDescriptor,
+                    override = true,
+                    preloadMode = ServiceDescriptor.PreloadMode.FALSE)
+    registerService(serviceInterface = DumbService::class.java,
+                    implementation = LightEditDumbService::class.java,
+                    pluginDescriptor = pluginDescriptor,
+                    override = true,
+                    preloadMode = ServiceDescriptor.PreloadMode.FALSE)
+    registerComponent(key = FileEditorManager::class.java,
+                      implementation = LightEditFileEditorManagerImpl::class.java,
+                      pluginDescriptor = pluginDescriptor,
+                      override = true)
   }
 
-  @NotNull
-  private static Path getProjectPath() {
-    return Paths.get(PathManager.getConfigPath() + File.separator + "light-edit");
+  override fun setProjectName(value: String) {
+    throw IllegalStateException()
   }
 
-  @Override
-  public void setProjectName(@NotNull String name) {
-    throw new IllegalStateException();
-  }
+  override fun getName() = NAME
 
-  @Override
-  @NotNull
-  public String getName() {
-    return NAME;
-  }
+  override fun getLocationHash() = name
 
-  @Override
-  @NotNull
-  public String getLocationHash() {
-    return getName();
-  }
+  override fun isOpen() = true
 
-  @Override
-  public boolean isOpen() {
-    return true;
-  }
-
-  @Override
-  public boolean isInitialized() {
-    return true;
-  }
+  override fun isInitialized() = true
 }
