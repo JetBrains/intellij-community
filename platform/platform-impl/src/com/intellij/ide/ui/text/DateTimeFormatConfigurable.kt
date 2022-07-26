1f1fe7c05a1f2b41a2ca9a07422876aed98a0beb
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-07-26 13:12:56 +0200

    IDEA-298008 IDEA-223057 ui: warn about incorrect Date Format patterns
    GitOrigin-RevId: a5082d8faa91198269d0aae72f1a5f0bd3c8b473

--- platform/platform-impl/src/com/intellij/ide/ui/text/DateTimeFormatConfigurable.kt
+++ platform/platform-impl/src/com/intellij/ide/ui/text/DateTimeFormatConfigurable.kt
@@ -6,14 +6,17 @@ import com.intellij.ide.ui.LafManager
 import com.intellij.openapi.options.BoundSearchableConfigurable
 import com.intellij.openapi.options.Configurable.NoScroll
 import com.intellij.openapi.ui.DialogPanel
+import com.intellij.openapi.util.NlsContexts
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.ui.DocumentAdapter
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.components.JBTextField
 import com.intellij.ui.dsl.builder.*
 import com.intellij.util.text.CustomJBDateTimeFormatter
+import com.intellij.util.text.DateFormatUtil
 import com.intellij.util.text.DateTimeFormatManager
 import com.intellij.util.text.JBDateFormat
-import java.util.*
+import java.text.SimpleDateFormat
 import javax.swing.JEditorPane
 import javax.swing.event.DocumentEvent
 
@@ -47,6 +50,9 @@ class DateTimeFormatConfigurable : BoundSearchableConfigurable(
             .bindText({ settings.dateFormatPattern },
                       { settings.dateFormatPattern = it })
             .columns(16)
+            .validationOnInput { field ->
+              validateDatePattern(field.text)?.let { error(it) }
+            }
             .applyToComponent {
               document.addDocumentListener(object : DocumentAdapter() {
                 override fun textChanged(e: DocumentEvent) {
@@ -89,10 +95,29 @@ class DateTimeFormatConfigurable : BoundSearchableConfigurable(
     }
   }
 
+  private fun validateDatePattern(pattern: String): @NlsContexts.DialogMessage String? {
+    try {
+      SimpleDateFormat(pattern)
+    }
+    catch (e: IllegalArgumentException) {
+      return IdeBundle.message("date.format.error.invalid.pattern", e.message)
+    }
+
+    if (pattern.contains("'")) return null // escaped text - assume user knows what they're doing
+    if (StringUtil.containsAnyChar(pattern, "aHhKkmSs")) {
+      return IdeBundle.message("date.format.error.contains.time.pattern")
+    }
+    return null
+  }
+
   private fun updateCommentField() {
-    val formatter = CustomJBDateTimeFormatter(dateFormatField.component.text, use24HourCheckbox.component.isSelected)
-    val calendar = Calendar.getInstance()
-    calendar.set(1999, 11, 31, 23, 59, 59)
-    datePreviewField.text(formatter.formatDateTime(calendar.time))
+    val text = try {
+      val formatter = CustomJBDateTimeFormatter(dateFormatField.component.text, use24HourCheckbox.component.isSelected)
+      formatter.formatDateTime(DateFormatUtil.getSampleDateTime())
+    }
+    catch (e: IllegalArgumentException) {
+      IdeBundle.message("date.format.error.invalid.pattern", e.message)
+    }
+    datePreviewField.text(StringUtil.escapeXmlEntities(text))
   }
 }
