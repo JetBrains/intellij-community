97c394436806a73ed500aff05ff8b6c1673086c0
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2022-07-26 13:44:28 +0200

    [kotlin] start removing -NewInference directive from tests
    In kt-222-master branch directive with newInference was removed earlier,
    but test data wasn't updated
    
    (cherry picked from commit 0105eb87999020a2b973b395e847388e806daf89)
    
    GitOrigin-RevId: f7344b525922a25226f4f3a1f23acc3c7f269bc4

--- plugins/kotlin/idea/tests/testData/refactoring/extractFunction/parameters/candidateTypes/nonNullableTypes.kt
+++ plugins/kotlin/idea/tests/testData/refactoring/extractFunction/parameters/candidateTypes/nonNullableTypes.kt
@@ -1,5 +1,5 @@
 // PARAM_TYPES: X<kotlin.Any>
-// PARAM_TYPES: kotlin.String, kotlin.Comparable<kotlin.String>, kotlin.CharSequence, java.io.Serializable, kotlin.Any
+// PARAM_TYPES: kotlin.String?
 // PARAM_DESCRIPTOR: value-parameter x: X<kotlin.Any> defined in foo
 // PARAM_DESCRIPTOR: value-parameter s: kotlin.String? defined in foo
 class X<T> {
