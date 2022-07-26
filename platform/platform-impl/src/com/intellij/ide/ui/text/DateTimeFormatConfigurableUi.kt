60f1a3c22c29223a16b924a53bdcbcebd63917a5
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-07-26 13:01:01 +0200

    IDEA-298008 IDEA-223057 ui: add preview to the custom Date Format
    GitOrigin-RevId: d6bde73ef97cd700a5aa56c24b557905703d67ca

--- platform/platform-impl/src/com/intellij/ide/ui/text/DateTimeFormatConfigurableUi.kt
+++ platform/platform-impl/src/com/intellij/ide/ui/text/DateTimeFormatConfigurableUi.kt
@@ -6,17 +6,27 @@ import com.intellij.ide.ui.LafManager
 import com.intellij.openapi.options.ConfigurableUi
 import com.intellij.openapi.options.ConfigurationException
 import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.DocumentAdapter
 import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.components.JBTextField
 import com.intellij.ui.dsl.builder.*
+import com.intellij.util.text.CustomJBDateTimeFormatter
 import com.intellij.util.text.DateTimeFormatManager
 import com.intellij.util.text.JBDateFormat
+import java.util.*
 import javax.swing.JComponent
+import javax.swing.JEditorPane
+import javax.swing.event.DocumentEvent
 
 /**
  * @author Konstantin Bulenkov
  */
 class DateTimeFormatConfigurableUi(settings: DateTimeFormatManager) : ConfigurableUi<DateTimeFormatManager> {
   private val ui: DialogPanel
+
+  private lateinit var dateFormatField: Cell<JBTextField>
+  private lateinit var use24HourCheckbox: Cell<JBCheckBox>
+  private lateinit var datePreviewField: Cell<JEditorPane>
 
   init {
     ui = panel {
@@ -31,17 +41,32 @@ class DateTimeFormatConfigurableUi(settings: DateTimeFormatManager) : Configurab
 
       indent {
         row(IdeBundle.message("date.format.date.format")) {
-          textField()
+          dateFormatField = textField()
             .bindText({ settings.dateFormatPattern },
-              { settings.dateFormatPattern = it })
+                      { settings.dateFormatPattern = it })
             .columns(16)
-          browserLink(IdeBundle.message("date.format.date.patterns"), "https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html")
+            .applyToComponent {
+              document.addDocumentListener(object : DocumentAdapter() {
+                override fun textChanged(e: DocumentEvent) {
+                  updateCommentField()
+                }
+              })
+            }
+          browserLink(IdeBundle.message("date.format.date.patterns"),
+                      "https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html")
         }
 
         row {
-          checkBox(IdeBundle.message("date.format.24.hours"))
+          use24HourCheckbox = checkBox(IdeBundle.message("date.format.24.hours"))
             .bindSelected({ settings.isUse24HourTime },
-              { settings.isUse24HourTime = it })
+                          { settings.isUse24HourTime = it })
+            .applyToComponent {
+              addChangeListener { updateCommentField() }
+            }
+        }
+
+        row {
+          datePreviewField = comment("", maxLineLength = MAX_LINE_LENGTH_NO_WRAP)
         }
       }.enabledIf(overrideSystemDateFormatting.selected)
 
@@ -52,7 +77,16 @@ class DateTimeFormatConfigurableUi(settings: DateTimeFormatManager) : Configurab
             { settings.isPrettyFormattingAllowed = it })
           .comment(IdeBundle.message("date.format.relative"))
       }.topGap(TopGap.SMALL)
+
+      updateCommentField()
     }
+  }
+
+  private fun updateCommentField() {
+    val formatter = CustomJBDateTimeFormatter(dateFormatField.component.text, use24HourCheckbox.component.isSelected)
+    val calendar = Calendar.getInstance()
+    calendar.set(1999, 11, 31, 23, 59, 59)
+    datePreviewField.text(formatter.formatDateTime(calendar.time))
   }
 
   override fun reset(settings: DateTimeFormatManager) = ui.reset()
