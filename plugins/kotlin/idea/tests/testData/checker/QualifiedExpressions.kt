8bc51c69f0d110b39dbbc77c263351f799de3cfc
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2022-07-26 13:44:28 +0200

    [kotlin] start removing -NewInference directive from tests
    In kt-222-master branch directive with newInference was removed earlier,
    but test data wasn't updated
    
    (cherry picked from commit 0105eb87999020a2b973b395e847388e806daf89)
    
    GitOrigin-RevId: e22a01c9ddd912ef30bc099ccadf62e1875a41e5

--- plugins/kotlin/idea/tests/testData/checker/QualifiedExpressions.kt
+++ plugins/kotlin/idea/tests/testData/checker/QualifiedExpressions.kt
@@ -2,11 +2,11 @@
 package qualified_expressions
 
 fun test(s: IntRange?) {
-   val <warning>a</warning>: Int = <error>s?.start</error>
+   val <warning descr="[UNUSED_VARIABLE] Variable 'a' is never used">a</warning>: Int = <error descr="[TYPE_MISMATCH] Type mismatch: inferred type is Int? but Int was expected">s?.start</error>
    val b: Int? = s?.start
    val <warning>c</warning>: Int = s?.start ?: -11
-   val <warning>d</warning>: Int = s?.start ?: <error>"empty"</error>
-   val e: String = <error>s?.start</error> ?: "empty"
+   val <warning>d</warning>: Int = <error>s?.start ?: "empty"</error>
+   val e: String = <error>s?.start ?: "empty"</error>
    val <warning>f</warning>: Int = s?.endInclusive ?: b ?: 1
    val <warning>g</warning>: Boolean? = e.startsWith("s")//?.length
 }
