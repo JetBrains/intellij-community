consulo/consulo-kotlin

[kotlin] Improve `Kotlin migration` notification text ^KTIJ-22128
GitOrigin-RevId: 8c19da1955ae34e21864a71f241c792f550fea55

migration/src/org/jetbrains/kotlin/idea/configuration/notifications/migrationNotification.kt
@@ -47,11 +47,11 @@ internal fun showMigrationNotification(project: Project, migrationInfo: Migratio
         .getNotificationGroup("Kotlin Migration")
         .createNotification(
             KotlinMigrationBundle.message("configuration.migration.title.kotlin.migration"),
-            "${KotlinMigrationBundle.message("configuration.migration.text.migrations.for.kotlin.code.are.available")}<br/><br/>$detectedChangeMessage",
+            "${KotlinMigrationBundle.message("configuration.migration.text.update.your.code.to.replace.deprecated")}<br/><br/>$detectedChangeMessage",
             NotificationType.WARNING
         )
         .setSuggestionType(true)
-        .addAction(NotificationAction.createExpiring(KotlinMigrationBundle.message("configuration.migration.text.run.migrations")) { notificationAction, notification ->
+        .addAction(NotificationAction.createExpiring(KotlinMigrationBundle.message("configuration.migration.text.scan.for.deprecations")) { notificationAction, notification ->
             val notificationProject = notificationAction.project ?: return@createExpiring
             val projectContext = SimpleDataContext.getProjectContext(notificationProject)
             val migrationAction = ActionManager.getInstance().getAction(CodeMigrationAction.ACTION_ID)

JetBrains/intellij-community

(cherry picked from commit 8c19da1955ae34e21864a71f241c792f550fea55)
GitOrigin-RevId: abfb9740cce843fcadb02d83476c3e6b6224d735

plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/configuration/notifications/migrationNotification.kt
@@ -47,11 +47,11 @@ internal fun showMigrationNotification(project: Project, migrationInfo: Migratio
         .getNotificationGroup("Kotlin Migration")
         .createNotification(
             KotlinBundle.message("configuration.migration.title.kotlin.migration"),
-            "${KotlinBundle.message("configuration.migration.text.migrations.for.kotlin.code.are.available")}<br/><br/>$detectedChangeMessage",
+            "${KotlinBundle.message("configuration.migration.text.update.your.code.to.replace.deprecated")}<br/><br/>$detectedChangeMessage",
             NotificationType.WARNING
         )
         .setSuggestionType(true)
-        .addAction(NotificationAction.createExpiring(KotlinBundle.message("configuration.migration.text.run.migrations")) { notificationAction, notification ->
+        .addAction(NotificationAction.createExpiring(KotlinBundle.message("configuration.migration.text.scan.for.deprecations")) { notificationAction, notification ->
             val notificationProject = notificationAction.project ?: return@createExpiring
             val projectContext = SimpleDataContext.getProjectContext(notificationProject)
             val migrationAction = ActionManager.getInstance().getAction(CodeMigrationAction.ACTION_ID)