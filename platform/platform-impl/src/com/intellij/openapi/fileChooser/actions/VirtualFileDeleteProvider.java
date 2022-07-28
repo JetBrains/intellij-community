6d7bacfbddffad0469c5f91c9796aef121f322d0
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2022-07-28 15:41:59 +0200

    IDEA-298759 Trying to delete a file from Commit does nothing while 'Scanning files to index'
    (cherry picked from commit 314839b70aca0a0e2b1d5834942a9aceccfaa5e2)
    
    IJ-CR-27829
    
    GitOrigin-RevId: 640b1852554d297cd24fec3f06ae0077ba32c4aa

--- platform/platform-impl/src/com/intellij/openapi/fileChooser/actions/VirtualFileDeleteProvider.java
+++ platform/platform-impl/src/com/intellij/openapi/fileChooser/actions/VirtualFileDeleteProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.fileChooser.actions;
 
 import com.intellij.CommonBundle;
@@ -13,6 +13,7 @@ import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.NlsContexts;
@@ -27,7 +28,7 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
-public final class VirtualFileDeleteProvider implements DeleteProvider {
+public final class VirtualFileDeleteProvider implements DeleteProvider, DumbAware {
   private static final Logger LOG = Logger.getInstance(VirtualFileDeleteProvider.class);
 
   @Override
