2d8e1093d7404af2d3279664deb4756586f2ed59
Florian Kistner <florian.kistner@jetbrains.com>
2022-07-29 12:14:33 +0200

    [kotlin] Turn `VfsCodeBlockModificationListener` into `AsyncFileListener` to ensure OOCB is incremented in write action
    This is necessary to prevent modules from being invalidated during a read action.
    
    GitOrigin-RevId: d79e645edf7a7cd11e8309c140c92b1d41577da9

--- plugins/kotlin/base/analysis/src/org/jetbrains/kotlin/idea/caches/project/VfsCodeBlockModificationListener.kt
+++ plugins/kotlin/base/analysis/src/org/jetbrains/kotlin/idea/caches/project/VfsCodeBlockModificationListener.kt
@@ -1,43 +1,43 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.idea.caches.project
 
-import com.intellij.openapi.application.invokeLater
+import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.vfs.AsyncFileListener
+import com.intellij.openapi.vfs.AsyncFileListener.ChangeApplier
 import com.intellij.openapi.vfs.newvfs.events.VFileContentChangeEvent
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
-import com.intellij.vfs.AsyncVfsEventsListener
-import com.intellij.vfs.AsyncVfsEventsPostProcessor
+import com.intellij.util.containers.nullize
 import org.jetbrains.kotlin.idea.base.projectStructure.RootKindFilter
 import org.jetbrains.kotlin.idea.base.projectStructure.matches
 import org.jetbrains.kotlin.idea.caches.trackers.KotlinCodeBlockModificationListener
-import org.jetbrains.kotlin.idea.core.KotlinPluginDisposable
-import org.jetbrains.kotlin.idea.core.util.runInReadActionWithWriteActionPriority
-import org.jetbrains.kotlin.idea.util.application.isDispatchThread
 
-class VfsCodeBlockModificationListener: StartupActivity.Background {
-    override fun runActivity(project: Project) {
-        val disposable = KotlinPluginDisposable.getInstance(project)
-        val ocbModificationListener = KotlinCodeBlockModificationListener.getInstance(project)
-        val vfsEventsListener = AsyncVfsEventsListener { events: List<VFileEvent> ->
-            val projectRelatedVfsFileChange = events.any { event ->
-                val file = event.takeIf { it.isFromRefresh || it is VFileContentChangeEvent }?.file ?: return@any false
-                return@any runInReadActionWithWriteActionPriority { RootKindFilter.projectSources.matches(project, file) } ?: true
+class VfsCodeBlockModificationListener : AsyncFileListener {
+    override fun prepareChange(events: List<VFileEvent>): ChangeApplier? {
+        val openProjects = ProjectManager.getInstance().openProjects.nullize() ?: return null
+
+        val relevantFiles = events.mapNotNull {
+            ProgressManager.checkCanceled()
+            it.takeIf { it.isFromRefresh || it is VFileContentChangeEvent }?.file
+        }.nullize() ?: return null
+
+        val changedProjects = openProjects.filter { project ->
+            relevantFiles.any {
+                ProgressManager.checkCanceled()
+                RootKindFilter.projectSources.matches(project, it)
             }
-            if (projectRelatedVfsFileChange) {
-                if (isDispatchThread()) {
-                    ocbModificationListener.incModificationCount()
-                } else {
-                    // it is not fully correct - OCB has to be triggered ASAP as we detect VFS-change out of IJ that
-                    // can affect Kotlin resolve, we can't do that properly before Kotlin 1.6.0
-                    // see [org.jetbrains.kotlin.descriptors.InvalidModuleNotifier]
-                    invokeLater {
-                        ocbModificationListener.incModificationCount()
-                    }
-                }
+        }.nullize() ?: return null
+
+        return Applier(changedProjects)
+    }
+
+    private class Applier(private val changedProjects: List<Project>) : ChangeApplier {
+        override fun afterVfsChange() {
+            for (project in changedProjects) {
+                if (project.isDisposed) continue
+                KotlinCodeBlockModificationListener.getInstance(project).incModificationCount()
             }
         }
-
-        AsyncVfsEventsPostProcessor.getInstance().addListener(vfsEventsListener, disposable)
     }
 }
