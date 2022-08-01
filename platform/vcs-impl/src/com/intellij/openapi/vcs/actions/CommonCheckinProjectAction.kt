be53a46721c1b3d082ad88211b4c15b9b58e9c0c
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-08-01 13:07:28 +0200

    vcs: remove usages of deprecated API - CommonCheckinProjectAction
    GitOrigin-RevId: f215e9b996bf022e239c03648011b901d8f0b445

--- platform/vcs-impl/src/com/intellij/openapi/vcs/actions/CommonCheckinProjectAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/actions/CommonCheckinProjectAction.kt
@@ -3,14 +3,9 @@ package com.intellij.openapi.vcs.actions
 
 import com.intellij.openapi.actionSystem.ActionPlaces.CHANGES_VIEW_POPUP
 import com.intellij.openapi.actionSystem.ActionPlaces.CHANGES_VIEW_TOOLBAR
-import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.IdeActions
-import com.intellij.openapi.actionSystem.ex.ActionUtil
-import com.intellij.openapi.project.DumbAwareToggleAction
 import com.intellij.openapi.vcs.FilePath
 import com.intellij.openapi.vcs.ProjectLevelVcsManager
-import com.intellij.openapi.vcs.changes.ChangesViewWorkflowManager
 import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager
 import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager.Companion.LOCAL_CHANGES
 import com.intellij.vcs.commit.CommitMode
@@ -27,7 +22,8 @@ private fun AnActionEvent.isToggleCommitEnabled(): Boolean {
          commitMode.isToggleMode
 }
 
-open class CommonCheckinProjectAction : CommonCheckinProjectActionImpl() {
+@Deprecated("Use [com.intellij.openapi.vcs.actions.commit.CheckinActionUtil] instead")
+open class CommonCheckinProjectAction : AbstractCommonCheckinAction() {
   override fun update(e: AnActionEvent) {
     if (e.isToggleCommitEnabled() && e.isFromLocalChanges()) {
       e.presentation.isEnabledAndVisible = false
@@ -42,54 +38,7 @@ open class CommonCheckinProjectAction : CommonCheckinProjectActionImpl() {
 
     super.update(e)
   }
-}
 
-class ToggleChangesViewCommitUiAction : DumbAwareToggleAction() {
-  private val commitProjectAction = CommonCheckinProjectActionImpl()
-
-  init {
-    ActionUtil.copyFrom(this, IdeActions.ACTION_CHECKIN_PROJECT)
-  }
-
-  override fun getActionUpdateThread(): ActionUpdateThread {
-    return ActionUpdateThread.BGT
-  }
-
-  override fun update(e: AnActionEvent) {
-    super.update(e)
-
-    if (!(e.isToggleCommitEnabled() && e.isFromLocalChanges())) {
-      e.presentation.isEnabledAndVisible = false
-      return
-    }
-
-    if (isSelected(e)) {
-      e.presentation.isEnabledAndVisible = true
-      return
-    }
-
-    commitProjectAction.update(e)
-  }
-
-  override fun isSelected(e: AnActionEvent): Boolean {
-    val project = e.project ?: return false
-    val workflowManager = project.getServiceIfCreated(ChangesViewWorkflowManager::class.java) ?: return false
-    val workflowHandler = workflowManager.commitWorkflowHandler
-    return workflowHandler?.isActive == true
-  }
-
-  override fun setSelected(e: AnActionEvent, state: Boolean) {
-    val workflowHandler = ChangesViewWorkflowManager.getInstance(e.project!!).commitWorkflowHandler
-    if (workflowHandler != null && workflowHandler.isActive) {
-      workflowHandler.deactivate(false)
-    }
-    else {
-      commitProjectAction.actionPerformed(e)
-    }
-  }
-}
-
-open class CommonCheckinProjectActionImpl : AbstractCommonCheckinAction() {
   override fun getRoots(dataContext: VcsContext): Array<FilePath> =
     ProjectLevelVcsManager.getInstance(dataContext.project!!).allVcsRoots
       .filter { it.vcs!!.checkinEnvironment != null }
