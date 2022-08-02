4e29a8e15eb7d638b457d0eb4d32d5de9faa096a
Konstantin Bulenkov <kb@jetbrains.com>
2022-08-02 15:02:49 +0200

    IconWithOverlay - ability to precisely combine two icons
    GitOrigin-RevId: 49cbafc6ffdd15339c7cb1a0caceccc1f35f2ee4

--- platform/lang-impl/src/com/intellij/ide/scratch/ScratchFileTypeIcon.java
+++ platform/lang-impl/src/com/intellij/ide/scratch/ScratchFileTypeIcon.java
@@ -3,45 +3,31 @@ package com.intellij.ide.scratch;
 
 import com.intellij.icons.AllIcons;
 import com.intellij.ui.ExperimentalUI;
-import com.intellij.ui.LayeredIcon;
-import com.intellij.util.IconUtil;
+import com.intellij.ui.IconWithOverlay;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
 
 /**
  * @author Konstantin Bulenkov
  */
-public class ScratchFileTypeIcon extends LayeredIcon {
+public class ScratchFileTypeIcon extends IconWithOverlay {
   public ScratchFileTypeIcon(Icon fileTypeIcon) {
-    super(2);
-    setIcon(fileTypeIcon, 0);
-    setIcon(AllIcons.Actions.Scratch, 1);
+    super(fileTypeIcon, AllIcons.Actions.Scratch);
   }
 
-  @SuppressWarnings("GraphicsSetClipInspection")
   @Override
-  public void paintIcon(Component c, Graphics g, int x, int y) {
-    if (!ExperimentalUI.isNewUI()) {
-      super.paintIcon(c, g, x, y);
-      return;
+  public @Nullable Shape getOverlayShape(int x, int y) {
+    if (ExperimentalUI.isNewUI()) {
+      float scale = getScale();
+      double r = scale * (3.5 + 1.0);
+      Icon overlay = getIcon(1);
+      assert overlay != null;
+      double w = overlay.getIconWidth();
+      return new Ellipse2D.Double(x + w - 2 * r + scale, y - scale, 2 * r, 2 * r);
     }
-    float scale = getScale();
-    double r = scale * 4.5;
-    Icon badge = getIcon(1);
-    assert badge != null;
-    double w = badge.getIconWidth();
-    Shape clip = g.getClip();
-    Area newClip = new Area(clip);
-    newClip.subtract(new Area(new Ellipse2D.Double(x + w - 2 * r, y, 2 * r, 2 * r)));
-    ((Graphics2D)g).setClip(newClip);
-    super.paintIcon(c, g, x, y);
-    ((Graphics2D)g).setClip(clip);
-    if (scale != 1f) {
-      badge = IconUtil.scale(badge, null, scale);
-    }
-    badge.paintIcon(c, g, x, y);
+    return null;
   }
 }
