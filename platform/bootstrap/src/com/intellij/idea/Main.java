cc389ce25da72d9779c22d64f0a1cb5ae1689f61
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2022-08-04 15:36:36 +0200

    add note about plugin updates in `com.intellij.idea.Main#bootstrap`
    GitOrigin-RevId: 51693580f017890689069940d0bc005a7a291a29

--- platform/bootstrap/src/com/intellij/idea/Main.java
+++ platform/bootstrap/src/com/intellij/idea/Main.java
@@ -100,6 +100,12 @@ public final class Main {
     if (!isCommandLine || Boolean.getBoolean(FORCE_PLUGIN_UPDATES)) {
       boolean configImportNeeded = !isHeadless() && !Files.exists(Path.of(PathManager.getConfigPath()));
       if (!configImportNeeded) {
+        // Consider following steps:
+        // - user opens settings, and installs some plugins;
+        // - the plugins are downloaded and saved somewhere;
+        // - IDE prompts for restart;
+        // - after restart, the plugins are moved to proper directories ("installed") by the next line.
+        // TODO get rid of this: plugins should be installed before restarting the IDE
         installPluginUpdates();
       }
     }
