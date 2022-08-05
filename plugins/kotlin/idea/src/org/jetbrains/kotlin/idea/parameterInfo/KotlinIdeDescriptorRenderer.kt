67368b0cb69d2d19d6814d4e67f26a3e449e710e
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-08-05 19:48:01 +0200

    [kotlin][renderer] fix render for unresolved types
    ^KT-49643
    
    GitOrigin-RevId: 06bfd1931cafe877171cde2ef7c98a757be3d880

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/parameterInfo/KotlinIdeDescriptorRenderer.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/parameterInfo/KotlinIdeDescriptorRenderer.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.parameterInfo
 
 import com.intellij.openapi.util.text.StringUtil
@@ -24,8 +24,8 @@ import org.jetbrains.kotlin.resolve.constants.KClassValue
 import org.jetbrains.kotlin.resolve.descriptorUtil.annotationClass
 import org.jetbrains.kotlin.resolve.descriptorUtil.declaresOrInheritsDefaultValue
 import org.jetbrains.kotlin.types.*
-import org.jetbrains.kotlin.types.error.*
 import org.jetbrains.kotlin.types.TypeUtils.CANNOT_INFER_FUNCTION_PARAM_TYPE
+import org.jetbrains.kotlin.types.error.*
 import org.jetbrains.kotlin.types.error.ErrorUtils
 import org.jetbrains.kotlin.types.typeUtil.isUnresolvedType
 import org.jetbrains.kotlin.util.capitalizeDecapitalize.toLowerCaseAsciiOnly
@@ -349,15 +349,15 @@ open class KotlinIdeDescriptorRenderer(
 
         if (type.isError) {
             if (isUnresolvedType(type) && presentableUnresolvedTypes) {
-                appendHighlighted(type.debugMessage) { asError }
+                appendHighlighted(ErrorUtils.unresolvedTypeAsItIs(type)) { asError }
             } else {
                 if (type is ErrorType && !informativeErrorType) {
                     appendHighlighted(type.debugMessage) { asError }
                 } else {
                     appendHighlighted(type.constructor.toString()) { asError } // Debug name of an error type is more informative
                 }
+                appendHighlighted(renderTypeArguments(type.arguments)) { asError }
             }
-            appendHighlighted(renderTypeArguments(type.arguments)) { asError }
         } else {
             appendTypeConstructorAndArguments(type)
         }
