c21e4f7d2d844139a4800ea0934ba68ce773884a
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-08-07 11:13:30 +0200

    [kotlin] move AbstractKotlinNavigationToLibrarySourceTest to the correct package
    GitOrigin-RevId: 5afec299dccadc8959fd01ad215c4bf96aefb0fa

--- plugins/kotlin/navigation/tests/test/org/jetbrains/kotlin/idea/k2/navigation/AbstractKotlinNavigationToLibrarySourceTest.kt
+++ plugins/kotlin/navigation/tests/test/org/jetbrains/kotlin/idea/k2/navigation/AbstractKotlinNavigationToLibrarySourceTest.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.navigation
+package org.jetbrains.kotlin.idea.k2.navigation
 
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
--- plugins/kotlin/util/test-generator-fir/test/org/jetbrains/kotlin/fir/testGenerator/GenerateK2NavigationTests.kt
+++ plugins/kotlin/util/test-generator-fir/test/org/jetbrains/kotlin/fir/testGenerator/GenerateK2NavigationTests.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.fir.testGenerator
 
-import org.jetbrains.kotlin.idea.navigation.AbstractKotlinNavigationToLibrarySourceTest
+import org.jetbrains.kotlin.idea.k2.navigation.AbstractKotlinNavigationToLibrarySourceTest
 import org.jetbrains.kotlin.testGenerator.model.MutableTWorkspace
 import org.jetbrains.kotlin.testGenerator.model.model
 import org.jetbrains.kotlin.testGenerator.model.testClass
