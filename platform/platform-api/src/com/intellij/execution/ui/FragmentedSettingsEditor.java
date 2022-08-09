461bcc9f0789368b8234d0543ffa1d17d388e12f
Andrii Zinchenko <andrii.zinchenko@jetbrains.com>
2022-08-09 15:33:53 +0200

    [fragmented-ui] FragmentedSettingsEditor: fix stream has already been operated upon or closed exception
    IJ-MR-77418
    
    GitOrigin-RevId: 1ff4a88854ffa79114ce7e678ad8a7f5c7217a77

--- platform/platform-api/src/com/intellij/execution/ui/FragmentedSettingsEditor.java
+++ platform/platform-api/src/com/intellij/execution/ui/FragmentedSettingsEditor.java
@@ -60,17 +60,17 @@ public abstract class FragmentedSettingsEditor<Settings extends FragmentedSettin
   @Override
   public void applyEditorTo(@NotNull Settings settings) throws ConfigurationException {
     super.applyEditorTo(settings);
-    final var fragments = getAllFragments();
+    final var fragments = getAllFragments().collect(Collectors.toList());
 
     fragments.forEach(f -> f.validate(settings));
-    List<FragmentedSettings.Option> options = fragments.filter(fragment -> (isDefaultSettings() || fragment.isCanBeHidden()) &&
+    List<FragmentedSettings.Option> options = fragments.stream().filter(fragment -> (isDefaultSettings() || fragment.isCanBeHidden()) &&
                                                                                    fragment.isSelected() != fragment.isInitiallyVisible(settings))
       .map(fragment -> new FragmentedSettings.Option(fragment.getId(), fragment.isSelected())).collect(Collectors.toList());
     if (!isDefaultSettings()) {
       for (FragmentedSettings.Option option : settings.getSelectedOptions()) {
         if (!ContainerUtil.or(options, o -> o.getName().equals(option.getName()))) {
           SettingsEditorFragment<Settings, ?> fragment =
-            fragments.filter(f -> f.getId().equals(option.getName())).findFirst().orElse(null);
+            ContainerUtil.find(fragments, f -> f.getId().equals(option.getName()));
           if (fragment != null) {
             if (fragment.isSelected() != fragment.isInitiallyVisible(settings)) { // do not keep option in selected otherwise
               FragmentedSettings.Option updatedOption = new FragmentedSettings.Option(fragment.getId(), fragment.isSelected());
