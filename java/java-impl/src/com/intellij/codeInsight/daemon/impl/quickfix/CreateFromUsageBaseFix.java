a952ee58723353003b3a6d994fbf7045a0a1cb2c
Tagir Valeev <tagir.valeev@jetbrains.com>
2022-08-10 16:25:16 +0200

    [intention-preview] CreateClassFromUsageFix: preview; tests for preview
    GitOrigin-RevId: 2eb36ee066743ffcacf4fae2968ba9d6d9c8bcff

--- java/java-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/CreateFromUsageBaseFix.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/CreateFromUsageBaseFix.java
@@ -417,15 +417,16 @@ public abstract class CreateFromUsageBaseFix extends BaseIntentionAction {
       }
     }
     int idx = 0;
+    PsiTypeParameterList typeParameterList = Objects.requireNonNull(targetClass.getTypeParameterList());
     for (PsiType type : ref.getTypeParameters()) {
       final PsiClass psiClass = PsiUtil.resolveClassInType(type);
       if (psiClass instanceof PsiTypeParameter) {
-        targetClass.getTypeParameterList().add(factory.createTypeParameterFromText(psiClass.getName(), null));
+        typeParameterList.add(factory.createTypeParameterFromText(psiClass.getName(), null));
       } else {
         while (true) {
           final @NonNls String paramName = idx > 0 ? "T" + idx : "T";
           if (typeParamNames.add(paramName)) {
-            targetClass.getTypeParameterList().add(factory.createTypeParameterFromText(paramName, null));
+            typeParameterList.add(factory.createTypeParameterFromText(paramName, null));
             break;
           }
           idx++;
