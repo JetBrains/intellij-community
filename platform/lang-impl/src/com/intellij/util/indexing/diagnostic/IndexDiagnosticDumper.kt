9e0dd11e1864285a0bd8de4edea95a4cd4921a2a
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2022-08-11 14:53:59 +0200

    IDEA-299808 Ignore default limit of MB in indexing-diagnostics when file number limit is provided  IJ-CR-94205
    (cherry picked from commit 8b00b4562bc1435f19c757444f9c3834c9ce213d)
    
    GitOrigin-RevId: cbdc6640d9900dd1da31cc55676f00e5fb3de474

--- platform/lang-impl/src/com/intellij/util/indexing/diagnostic/IndexDiagnosticDumper.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/diagnostic/IndexDiagnosticDumper.kt
@@ -31,6 +31,8 @@ import kotlin.io.path.nameWithoutExtension
 import kotlin.math.min
 import kotlin.streams.asSequence
 
+private const val DIAGNOSTIC_LIMIT_OF_FILES_PROPERTY = "intellij.indexes.diagnostics.limit.of.files"
+
 class IndexDiagnosticDumper : Disposable {
   companion object {
     @JvmStatic
@@ -39,7 +41,8 @@ class IndexDiagnosticDumper : Disposable {
     private const val fileNamePrefix = "diagnostic-"
 
     @JvmStatic
-    val projectIndexingHistoryListenerEpName = ExtensionPointName.create<ProjectIndexingHistoryListener>("com.intellij.projectIndexingHistoryListener")
+    val projectIndexingHistoryListenerEpName =
+      ExtensionPointName.create<ProjectIndexingHistoryListener>("com.intellij.projectIndexingHistoryListener")
 
     @JvmStatic
     private val shouldDumpDiagnosticsForInterruptedUpdaters: Boolean
@@ -48,13 +51,34 @@ class IndexDiagnosticDumper : Disposable {
 
     @JvmStatic
     private val indexingDiagnosticsLimitOfFiles: Int
-      get() =
-        SystemProperties.getIntProperty("intellij.indexes.diagnostics.limit.of.files", 300)
+      get() = SystemProperties.getIntProperty(DIAGNOSTIC_LIMIT_OF_FILES_PROPERTY, 300)
+
+    private fun hasProvidedDiagnosticsLimitOfFilesValue(): Boolean {
+      val providedLimitOfFilesValue = System.getProperty(DIAGNOSTIC_LIMIT_OF_FILES_PROPERTY)
+      if (providedLimitOfFilesValue == null) return false
+      try {
+        providedLimitOfFilesValue.toInt()
+      }
+      catch (ignored: NumberFormatException) {
+        return false
+      }
+      return true
+    }
 
     @JvmStatic
     private val indexingDiagnosticsSizeLimitOfFilesInMBPerProject: Int
-      get() =
-        SystemProperties.getIntProperty("intellij.indexes.diagnostics.size.limit.of.files.MB.per.project", 10)
+      get() {
+        val providedValue = System.getProperty("intellij.indexes.diagnostics.size.limit.of.files.MB.per.project")
+        if (providedValue != null) {
+          try {
+            return providedValue.toInt()
+          }
+          catch (ignored: NumberFormatException) {
+          }
+        }
+
+        return if (hasProvidedDiagnosticsLimitOfFilesValue()) 0 else 10
+      }
 
     @JvmStatic
     val shouldDumpPathsOfIndexedFiles: Boolean
