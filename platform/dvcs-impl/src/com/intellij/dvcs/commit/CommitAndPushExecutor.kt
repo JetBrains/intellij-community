808788479b5fe0b60dd1d22ddc71e69559f46eec
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-08-11 16:59:50 +0200

    vcs: extract common CommitWorkflow state for CommitExecutors
    GitOrigin-RevId: f03597129082e2993d6fb62ea197f5122f2d9149

--- platform/dvcs-impl/src/com/intellij/dvcs/commit/CommitAndPushExecutor.kt
+++ platform/dvcs-impl/src/com/intellij/dvcs/commit/CommitAndPushExecutor.kt
@@ -4,12 +4,12 @@ package com.intellij.dvcs.commit
 import com.intellij.dvcs.ui.DvcsBundle.message
 import com.intellij.openapi.util.NlsActions
 import com.intellij.vcs.commit.CommitWorkflowHandler
-import com.intellij.vcs.commit.NonModalCommitWorkflowHandler
 
 @NlsActions.ActionText
 fun CommitWorkflowHandler.getCommitAndPushActionName(): String {
-  val isAmend = amendCommitHandler.isAmendCommitMode
-  val isSkipCommitChecks = (this as? NonModalCommitWorkflowHandler<*, *>)?.isSkipCommitChecks() == true
+  val state = getState()
+  val isAmend = state.isAmend
+  val isSkipCommitChecks = state.isSkipCommitChecks
 
   return when {
     isAmend && isSkipCommitChecks -> message("action.amend.commit.anyway.and.push.text")
