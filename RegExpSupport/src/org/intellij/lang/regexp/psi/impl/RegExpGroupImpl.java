2d28eed303474e7a7405ad49ff558e9a9c5a1c24
Filippova Maria <maria.filippova@jetbrains.com>
2022-08-11 18:19:10 +0200

    WI-60779 highlight as error if DEFINE subpattern contains more than two branches
    GitOrigin-RevId: 8af565f6438654c11285be1a24a709e40869fb40

--- RegExpSupport/src/org/intellij/lang/regexp/psi/impl/RegExpGroupImpl.java
+++ RegExpSupport/src/org/intellij/lang/regexp/psi/impl/RegExpGroupImpl.java
@@ -102,6 +102,10 @@ public class RegExpGroupImpl extends RegExpElementImpl implements RegExpGroup {
     return node != null && node.findChildByType(RegExpTT.PCRE_CONDITION) != null;
   }
 
+  public static boolean isPcreDefine(ASTNode node) {
+    return node != null && node.findChildByType(RegExpTT.PCRE_DEFINE) != null;
+  }
+
   @Override
   public String getGroupName() {
     final ASTNode nameNode = getNode().findChildByType(RegExpTT.NAME);
