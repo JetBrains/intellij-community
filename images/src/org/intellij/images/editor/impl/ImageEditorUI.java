9586c95d221e76ea7ba11d7583f204572ac9d814
Gregory.Shrago <gregory.shrago@jetbrains.com>
2022-08-13 02:11:28 +0400

    introduce slow-data-provider for ImageEditorUI
    IDEA-299869 Throwable: com.intellij.psi.impl.file.PsiBinaryFileImpl is provided on EDT on opening .jpg file in Editor
    
    GitOrigin-RevId: 9c6ec3fe53c459a5cee14ee9a2791414ae2302fd

--- images/src/org/intellij/images/editor/impl/ImageEditorUI.java
+++ images/src/org/intellij/images/editor/impl/ImageEditorUI.java
@@ -68,6 +68,7 @@ import java.awt.image.ColorModel;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.ByteArrayInputStream;
+import java.util.List;
 
 /**
  * Image editor UI
@@ -600,16 +601,6 @@ final class ImageEditorUI extends JPanel implements DataProvider, CopyProvider, 
     else if (CommonDataKeys.VIRTUAL_FILE_ARRAY.is(dataId)) {
       return editor != null ? new VirtualFile[]{editor.getFile()} : VirtualFile.EMPTY_ARRAY;
     }
-    else if (CommonDataKeys.PSI_FILE.is(dataId)) {
-      return findPsiFile();
-    }
-    else if (CommonDataKeys.PSI_ELEMENT.is(dataId)) {
-      return findPsiFile();
-    }
-    else if (LangDataKeys.PSI_ELEMENT_ARRAY.is(dataId)) {
-      PsiElement psi = findPsiFile();
-      return psi != null ? new PsiElement[]{psi} : PsiElement.EMPTY_ARRAY;
-    }
     else if (PlatformDataKeys.COPY_PROVIDER.is(dataId)) {
       return this;
     }
@@ -622,7 +613,23 @@ final class ImageEditorUI extends JPanel implements DataProvider, CopyProvider, 
     else if (ImageComponentDecorator.DATA_KEY.is(dataId)) {
       return editor != null ? editor : this;
     }
+    else if (PlatformCoreDataKeys.SLOW_DATA_PROVIDERS.is(dataId)) {
+      return List.<DataProvider>of(slowId -> getSlowData(slowId));
+    }
+    return null;
+  }
 
+  private @Nullable Object getSlowData(@NotNull String dataId) {
+    if (CommonDataKeys.PSI_FILE.is(dataId)) {
+      return findPsiFile();
+    }
+    else if (CommonDataKeys.PSI_ELEMENT.is(dataId)) {
+      return findPsiFile();
+    }
+    else if (PlatformCoreDataKeys.PSI_ELEMENT_ARRAY.is(dataId)) {
+      PsiElement psi = findPsiFile();
+      return psi != null ? new PsiElement[]{psi} : PsiElement.EMPTY_ARRAY;
+    }
     return null;
   }
 
