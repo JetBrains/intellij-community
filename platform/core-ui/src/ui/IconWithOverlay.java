0e0c4b1e7d508ce9b24a7a53e289e79d9d2415cc
Konstantin Bulenkov <kb@jetbrains.com>
2022-08-12 17:54:03 +0200

    Improve quality for IconWithOverlay
    GitOrigin-RevId: 75eea4fc3ce9bf42229cd0bc367fb79af57bea2b

--- platform/core-ui/src/ui/IconWithOverlay.java
+++ platform/core-ui/src/ui/IconWithOverlay.java
@@ -1,13 +1,16 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ui;
 
+import com.intellij.openapi.ui.GraphicsConfig;
+import com.intellij.ui.paint.PaintUtil;
 import com.intellij.util.IconUtil;
+import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Area;
+import java.awt.image.BufferedImage;
 
 /**
  * @author Konstantin Bulenkov
@@ -19,27 +22,28 @@ public abstract class IconWithOverlay extends LayeredIcon {
 
   public abstract @Nullable Shape getOverlayShape(int x, int y);
 
-  @SuppressWarnings("GraphicsSetClipInspection")
   @Override
   public void paintIcon(Component c, Graphics g, int x, int y) {
-    Shape dontPaintHere = getOverlayShape(x, y);
+    Shape dontPaintHere = getOverlayShape(0, 0);
     if (dontPaintHere == null) {
       super.paintIcon(c, g, x, y);
       return;
     }
     float scale = getScale();
+    BufferedImage img = UIUtil.createImage(((Graphics2D)g).getDeviceConfiguration(), getIconWidth(), getIconHeight(), BufferedImage.TYPE_INT_ARGB, PaintUtil.RoundingMode.CEIL);
+    Graphics2D g2 = img.createGraphics();
+    IconUtil.scale(getIcon(0), scale).paintIcon(c, g2, 0, 0);
+    GraphicsConfig config = new GraphicsConfig(g2);
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    g2.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR));
+    g2.fill(dontPaintHere);
+    config.restore();
+    UIUtil.drawImage(g,img, x, y, null);
     Icon overlay = getIcon(1);
     assert overlay != null;
-    Shape clip = g.getClip();
-    Area newClip = new Area(clip);
-    newClip.subtract(new Area(dontPaintHere));
-    g.setClip(newClip);
-    super.paintIcon(c, g, x, y);
-    g.setClip(clip);
     if (scale != 1f) {
       overlay = IconUtil.scale(overlay, null, scale);
     }
     overlay.paintIcon(c, g, x, y);
   }
-
 }
