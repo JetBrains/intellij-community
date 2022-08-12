5fc5cebb3221206d4c2870aef0b47367c714c97f
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2022-08-12 15:00:19 +0300

    [ide starter] AT-36 store qualified path to test in stacktrace. JUnit4 support
    GitOrigin-RevId: c1e2aad632b627ef1e4feec18a89f5d432868516

--- tools/ideTestingFramework/intellij.tools.ide.starter.junit4/src/com/intellij/ide/starter/junit4/JUnit4StarterRule.kt
+++ tools/ideTestingFramework/intellij.tools.ide.starter.junit4/src/com/intellij/ide/starter/junit4/JUnit4StarterRule.kt
@@ -6,8 +6,10 @@ import com.intellij.ide.starter.di.di
 import com.intellij.ide.starter.ide.IDETestContext
 import com.intellij.ide.starter.path.GlobalPaths
 import com.intellij.ide.starter.process.killOutdatedProcessesOnUnix
+import com.intellij.ide.starter.runner.CurrentTestMethod
 import com.intellij.ide.starter.runner.TestContainer
 import com.intellij.ide.starter.utils.catchAll
+import com.intellij.ide.starter.utils.logError
 import com.intellij.ide.starter.utils.logOutput
 import com.intellij.ide.starter.utils.withIndent
 import org.junit.rules.ExternalResource
@@ -30,9 +32,21 @@ open class JUnit4StarterRule(
 
   override fun apply(base: Statement, description: Description): Statement {
     testDescription = description
+
+    try {
+      val testMethod = testDescription.testClass.getMethod(testDescription.methodName)
+      di.direct.instance<CurrentTestMethod>().set(testMethod)
+    }
+    catch (_: Exception) {
+      logError("Couldn't acquire test method")
+    }
+
     return super.apply(base, description)
   }
 
+  /**
+   * Before each
+   */
   override fun before() {
     if (ciServer.isBuildRunningOnCI) {
       logOutput(buildString {
@@ -50,6 +64,9 @@ open class JUnit4StarterRule(
     catchAll { testContext.paths.close() }
   }
 
+  /**
+   * After each
+   */
   override fun after() {
     StarterListener.unsubscribe()
 
