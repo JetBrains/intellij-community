d12558c315d57f00739272e9a927e48b95450160
Evgeniy.Stepanov <Evgeniy.Stepanov@JetBrains.com>
2022-08-13 18:13:51 +0200

    Do not try to extract tar.gz as a zip archive
    GitOrigin-RevId: 9fea5a5dd54253b953350397526eb964b8469230

--- platform/jps-bootstrap/jps-bootstrap.cmd
+++ platform/jps-bootstrap/jps-bootstrap.cmd
@@ -16,7 +16,7 @@ set SCRIPT_VERSION=jps-bootstrap-cmd-v1
 set COMPANY_NAME=JetBrains
 set TARGET_DIR=%LOCALAPPDATA%\Temp\%COMPANY_NAME%\
 set JVM_TARGET_DIR=%TARGET_DIR%%JBR_VERSION%%JBR_BUILD%-%JBR_ARCH%-%SCRIPT_VERSION%\
-set JVM_TEMP_FILE=jvm-windows-x64.zip
+set JVM_TEMP_FILE=jvm-windows-x64.tar.gz
 set JVM_URL=https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk-%JBR_VERSION%-%JBR_ARCH%-%JBR_BUILD%.tar.gz
 
 set POWERSHELL=%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe
@@ -55,7 +55,8 @@ cd /d "%JVM_TARGET_DIR%"
 if errorlevel 1 goto fail
 
 echo Extracting %TARGET_DIR%%JVM_TEMP_FILE% to %JVM_TARGET_DIR%
-"%POWERSHELL%" -nologo -noprofile -command "Set-StrictMode -Version 3.0; $ErrorActionPreference = \"Stop\"; Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('..\\%JVM_TEMP_FILE%', '.');"
+REM "%POWERSHELL%" -nologo -noprofile -command "Set-StrictMode -Version 3.0; $ErrorActionPreference = \"Stop\"; Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('..\\%JVM_TEMP_FILE%', '.');"
+tar -x -f "..\%JVM_TEMP_FILE%" -C .
 if errorlevel 1 goto fail
 
 del /F "..\%JVM_TEMP_FILE%"
