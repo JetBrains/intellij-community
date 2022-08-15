6b92168e453e84f02c7adde3aabf177903d93df6
Gregory.Shrago <gregory.shrago@jetbrains.com>
2022-08-15 18:28:11 +0400

    support slow-data-providers in SimpleToolWindowPanel
    GitOrigin-RevId: a3ac3f3d51f8a6e38b74f93892d9c71fefe4e0ac

--- platform/editor-ui-api/src/com/intellij/openapi/actionSystem/CompositeDataProvider.java
+++ platform/editor-ui-api/src/com/intellij/openapi/actionSystem/CompositeDataProvider.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.actionSystem;
 
+import com.intellij.util.SmartList;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
@@ -28,12 +29,15 @@ public final class CompositeDataProvider implements DataProvider {
   }
 
   public static @NotNull DataProvider compose(@NotNull Iterable<? extends DataProvider> providers) {
-    List<DataProvider> list = new ArrayList<>();
+    List<DataProvider> list = null;
     for (DataProvider provider : providers) {
+      if (list == null) list = new SmartList<>();
       if (provider instanceof CompositeDataProvider) list.addAll(((CompositeDataProvider)provider).myProviders);
       else list.add(provider);
     }
-    return new CompositeDataProvider(new ArrayList<>(list));
+    return list == null ? dataId -> null :
+           list.size() == 1 ? list.get(0) :
+           new CompositeDataProvider(new ArrayList<>(list));
   }
 
   @Override
