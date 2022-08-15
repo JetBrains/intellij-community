8ee6a765be9b134ce84b3fcd2f99c3e153ea314b
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2022-08-15 15:41:28 +0200

    Missing commits from tools/ideTestingFramework/intellij.tools.ide.starter (fixup)
    GitOrigin-RevId: 7a30e31f6b27c6ce73640f565b5b826d4379f8a4

--- tools/ideTestingFramework/intellij.tools.ide.starter/src/com/intellij/ide/starter/community/model/IdeModel.kt
+++ tools/ideTestingFramework/intellij.tools.ide.starter/src/com/intellij/ide/starter/community/model/IdeModel.kt
@@ -1,4 +1,3 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.starter.community.model
 
 data class IdeModel(val releaseInfo: Map<Int, ReleaseInfo>)
