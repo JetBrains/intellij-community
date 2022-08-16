ca7c511d41610eea426ba0258f36e0723e1d5504
andrey.matveev <amatveev@jetbrains.com>
2022-08-16 16:56:55 +0300

    PY-36158 Add all star import sources if imported element qname not locally resolved
    (cherry picked from commit 0f65429042ebde43cbf04ec66abf6f392e71ae67)
    
    IJ-MR-98271
    
    GitOrigin-RevId: 1ac4e4ad1934fb368dffde5fc586ca86df5a7d70

--- python/testSrc/com/jetbrains/python/inspections/Py3ArgumentListInspectionTest.java
+++ python/testSrc/com/jetbrains/python/inspections/Py3ArgumentListInspectionTest.java
@@ -18,6 +18,20 @@ public class Py3ArgumentListInspectionTest extends PyInspectionTestCase {
     return ourPyLatestDescriptor;
   }
 
+  // PY-36158
+  public void testDataclassesStarImportNoUnexpectedArgumentWarning() {
+    doTestByText("from dataclasses import *\n" +
+                 "\n" +
+                 "\n" +
+                 "@dataclass(eq=True)\n" +
+                 "class Foo:\n" +
+                 "    a: float\n" +
+                 "    b: float\n" +
+                 "\n" +
+                 "\n" +
+                 "print(Foo(1, 2))\n");
+  }
+
   // PY-50404
   public void testPassingKeywordArgumentsToParamSpec() {
     doTestByText("""
