2e715d5c2b4fd44b6928619cb499f71530f488fb
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2022-08-16 16:44:04 +0400

    Unpack ion-java lib into util jar  FL-13189
    GitOrigin-RevId: 12ebd802f2e15ea92c54a126fffc764ed7a1c03a

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/PlatformModules.kt
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/PlatformModules.kt
@@ -324,6 +324,7 @@ object PlatformModules {
     }
 
     layout.projectLibrariesToUnpack.putValue(UTIL_JAR, "Trove4j")
+    layout.projectLibrariesToUnpack.putValue(UTIL_JAR, "ion")
 
     for (item in additionalProjectLevelLibraries) {
       val name = item.libraryName
