631139ff3257aae7e9aa412690475dcf181b2f4c
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-08-16 14:53:39 +0200

    vcs: convert PartialChangesUtil to Kotlin
    GitOrigin-RevId: 4478b6452545a3ce3c8c7a34827251647647e875

--- platform/vcs-impl/src/com/intellij/openapi/vcs/impl/PartialChangesUtil.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/impl/PartialChangesUtil.kt
@@ -1,208 +1,180 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.openapi.vcs.impl;
+package com.intellij.openapi.vcs.impl
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Computable;
-import com.intellij.openapi.vcs.changes.*;
-import com.intellij.openapi.vcs.changes.conflicts.ChangelistConflictTracker;
-import com.intellij.openapi.vcs.ex.ExclusionState;
-import com.intellij.openapi.vcs.ex.LineStatusTracker;
-import com.intellij.openapi.vcs.ex.PartialLocalLineStatusTracker;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.ObjectUtils;
-import com.intellij.util.PairFunction;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.containers.MultiMap;
-import com.intellij.util.ui.ThreeStateCheckBox;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Computable
+import com.intellij.openapi.vcs.changes.*
+import com.intellij.openapi.vcs.changes.conflicts.ChangelistConflictTracker
+import com.intellij.openapi.vcs.ex.ExclusionState
+import com.intellij.openapi.vcs.ex.PartialLocalLineStatusTracker
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.PairFunction
+import com.intellij.util.containers.MultiMap
+import com.intellij.util.ui.ThreeStateCheckBox
 
-import java.util.*;
+object PartialChangesUtil {
+  private val LOG = Logger.getInstance(PartialChangesUtil::class.java)
 
-import static com.intellij.openapi.diagnostic.Logger.getInstance;
-
-public final class PartialChangesUtil {
-  private static final Logger LOG = getInstance(PartialChangesUtil.class);
-
-  @Nullable
-  public static PartialLocalLineStatusTracker getPartialTracker(@NotNull Project project, @NotNull Change change) {
-    VirtualFile file = getVirtualFile(change);
-    if (file == null) return null;
-
-    return getPartialTracker(project, file);
+  @JvmStatic
+  fun getPartialTracker(project: Project, change: Change): PartialLocalLineStatusTracker? {
+    val file = getVirtualFile(change) ?: return null
+    return getPartialTracker(project, file)
   }
 
-  @Nullable
-  public static PartialLocalLineStatusTracker getPartialTracker(@NotNull Project project, @NotNull VirtualFile file) {
-    LineStatusTracker<?> tracker = LineStatusTrackerManager.getInstance(project).getLineStatusTracker(file);
-    return ObjectUtils.tryCast(tracker, PartialLocalLineStatusTracker.class);
+  @JvmStatic
+  fun getPartialTracker(project: Project, file: VirtualFile): PartialLocalLineStatusTracker? {
+    val tracker = LineStatusTrackerManager.getInstance(project).getLineStatusTracker(file)
+    return tracker as? PartialLocalLineStatusTracker
   }
 
-  @Nullable
-  public static VirtualFile getVirtualFile(@NotNull Change change) {
-    ContentRevision revision = change.getAfterRevision();
-    if (!(revision instanceof CurrentContentRevision)) return null;
-
-    return ((CurrentContentRevision)revision).getVirtualFile();
+  @JvmStatic
+  fun getVirtualFile(change: Change): VirtualFile? {
+    val revision = change.afterRevision as? CurrentContentRevision
+    return revision?.virtualFile
   }
 
-  @NotNull
-  public static List<Change> processPartialChanges(@NotNull Project project,
-                                                   @NotNull Collection<? extends Change> changes,
-                                                   boolean executeOnEDT,
-                                                   @NotNull PairFunction<? super List<ChangeListChange>, ? super PartialLocalLineStatusTracker, Boolean> partialProcessor) {
+  @JvmStatic
+  fun processPartialChanges(project: Project,
+                            changes: Collection<Change>,
+                            executeOnEDT: Boolean,
+                            partialProcessor: PairFunction<in List<ChangeListChange>, in PartialLocalLineStatusTracker, Boolean>): List<Change> {
     if (!LineStatusTrackerManager.getInstance(project).arePartialChangelistsEnabled() ||
-        !ContainerUtil.exists(changes, it -> it instanceof ChangeListChange)) {
-      return new ArrayList<>(changes);
+        changes.none { it is ChangeListChange }) {
+      return changes.toMutableList()
     }
 
-    List<Change> otherChanges = new ArrayList<>();
-
-    Runnable task = () -> {
-      MultiMap<VirtualFile, ChangeListChange> partialChangesMap = new MultiMap<>();
-      for (Change change : changes) {
-        if (change instanceof ChangeListChange) {
-          ChangeListChange changelistChange = (ChangeListChange)change;
-
-          VirtualFile virtualFile = getVirtualFile(change);
+    val otherChanges = mutableListOf<Change>()
+    val task = Runnable {
+      val partialChangesMap = MultiMap<VirtualFile, ChangeListChange>()
+      for (change in changes) {
+        if (change is ChangeListChange) {
+          val virtualFile = getVirtualFile(change)
           if (virtualFile != null) {
-            partialChangesMap.putValue(virtualFile, changelistChange);
+            partialChangesMap.putValue(virtualFile, change)
           }
           else {
-            otherChanges.add((changelistChange).getChange());
+            otherChanges.add(change.change)
           }
         }
         else {
-          otherChanges.add(change);
+          otherChanges.add(change)
         }
       }
 
-      LineStatusTrackerManagerI lstManager = LineStatusTrackerManager.getInstance(project);
-      for (Map.Entry<VirtualFile, Collection<ChangeListChange>> entry : partialChangesMap.entrySet()) {
-        VirtualFile virtualFile = entry.getKey();
-        List<ChangeListChange> partialChanges = (List<ChangeListChange>)entry.getValue();
-        Change actualChange = partialChanges.get(0).getChange();
-
-        PartialLocalLineStatusTracker tracker = ObjectUtils.tryCast(lstManager.getLineStatusTracker(virtualFile),
-                                                                    PartialLocalLineStatusTracker.class);
-        if (tracker == null) {
-          otherChanges.add(actualChange);
-          continue;
-        }
-
-        boolean success = partialProcessor.fun(partialChanges, tracker);
-        if (!success) {
-          otherChanges.add(actualChange);
+      val lstManager = LineStatusTrackerManager.getInstance(project)
+      for ((virtualFile, value) in partialChangesMap.entrySet()) {
+        @Suppress("UNCHECKED_CAST") val partialChanges = value as List<ChangeListChange>
+        val actualChange = partialChanges[0].change
+        val tracker = lstManager.getLineStatusTracker(virtualFile) as? PartialLocalLineStatusTracker
+        if (tracker == null ||
+            !partialProcessor.`fun`(partialChanges, tracker)) {
+          otherChanges.add(actualChange)
         }
       }
-    };
+    }
 
-    if (executeOnEDT && !ApplicationManager.getApplication().isDispatchThread()) {
-      ApplicationManager.getApplication().invokeAndWait(task);
+    if (executeOnEDT && !ApplicationManager.getApplication().isDispatchThread) {
+      ApplicationManager.getApplication().invokeAndWait(task)
     }
     else {
-      task.run();
+      task.run()
     }
-
-    return otherChanges;
+    return otherChanges
   }
 
-  public static void runUnderChangeList(@NotNull Project project,
-                                        @Nullable LocalChangeList targetChangeList,
-                                        @NotNull Runnable task) {
-    computeUnderChangeList(project, targetChangeList, () -> {
-      task.run();
-      return null;
-    });
+  @JvmStatic
+  fun runUnderChangeList(project: Project,
+                         targetChangeList: LocalChangeList?,
+                         task: Runnable) {
+    computeUnderChangeList(project, targetChangeList) {
+      task.run()
+      null
+    }
   }
 
-  public static <T> T computeUnderChangeList(@NotNull Project project,
-                                             @Nullable LocalChangeList targetChangeList,
-                                             @NotNull Computable<T> task) {
-    ChangeListManagerEx changeListManager = ChangeListManagerEx.getInstanceEx(project);
-    LocalChangeList oldDefaultList = changeListManager.getDefaultChangeList();
-
+  @JvmStatic
+  fun <T> computeUnderChangeList(project: Project,
+                                 targetChangeList: LocalChangeList?,
+                                 task: Computable<T>): T {
+    val changeListManager = ChangeListManagerEx.getInstanceEx(project)
+    val oldDefaultList = changeListManager.defaultChangeList
     if (targetChangeList == null ||
-        targetChangeList.equals(oldDefaultList) ||
+        targetChangeList == oldDefaultList ||
         !changeListManager.areChangeListsEnabled()) {
-      return task.compute();
+      return task.compute()
     }
 
-    switchChangeList(changeListManager, targetChangeList, oldDefaultList);
-    ChangelistConflictTracker clmConflictTracker = ChangelistConflictTracker.getInstance(project);
+    switchChangeList(changeListManager, targetChangeList, oldDefaultList)
+    val clmConflictTracker = ChangelistConflictTracker.getInstance(project)
     try {
-      clmConflictTracker.setIgnoreModifications(true);
-      return task.compute();
+      clmConflictTracker.setIgnoreModifications(true)
+      return task.compute()
     }
     finally {
-      clmConflictTracker.setIgnoreModifications(false);
-      restoreChangeList(changeListManager, targetChangeList, oldDefaultList);
+      clmConflictTracker.setIgnoreModifications(false)
+      restoreChangeList(changeListManager, targetChangeList, oldDefaultList)
     }
   }
 
-  public static <T> T computeUnderChangeListSync(@NotNull Project project,
-                                                 @Nullable LocalChangeList targetChangeList,
-                                                 @NotNull Computable<T> task) {
-    ChangeListManagerEx changeListManager = ChangeListManagerEx.getInstanceEx(project);
-    LocalChangeList oldDefaultList = changeListManager.getDefaultChangeList();
-
+  @JvmStatic
+  fun <T> computeUnderChangeListSync(project: Project,
+                                     targetChangeList: LocalChangeList?,
+                                     task: Computable<T>): T {
+    val changeListManager = ChangeListManagerEx.getInstanceEx(project)
+    val oldDefaultList = changeListManager.defaultChangeList
     if (targetChangeList == null ||
         !changeListManager.areChangeListsEnabled()) {
-      return task.compute();
+      return task.compute()
     }
 
-    switchChangeList(changeListManager, targetChangeList, oldDefaultList);
-    ChangelistConflictTracker clmConflictTracker = ChangelistConflictTracker.getInstance(project);
+    switchChangeList(changeListManager, targetChangeList, oldDefaultList)
+    val clmConflictTracker = ChangelistConflictTracker.getInstance(project)
     try {
-      clmConflictTracker.setIgnoreModifications(true);
-      return task.compute();
+      clmConflictTracker.setIgnoreModifications(true)
+      return task.compute()
     }
     finally {
-      clmConflictTracker.setIgnoreModifications(false);
-
-      if (ApplicationManager.getApplication().isReadAccessAllowed()) {
-        LOG.warn("Can't wait till changes are applied while holding read lock", new Throwable());
+      clmConflictTracker.setIgnoreModifications(false)
+      if (ApplicationManager.getApplication().isReadAccessAllowed) {
+        LOG.warn("Can't wait till changes are applied while holding read lock", Throwable())
       }
       else {
-        ChangeListManagerEx.getInstanceEx(project).waitForUpdate();
+        ChangeListManagerEx.getInstanceEx(project).waitForUpdate()
       }
-      restoreChangeList(changeListManager, targetChangeList, oldDefaultList);
+      restoreChangeList(changeListManager, targetChangeList, oldDefaultList)
     }
   }
 
-  private static void switchChangeList(@NotNull ChangeListManagerEx clm,
-                                       @NotNull LocalChangeList targetChangeList,
-                                       @NotNull LocalChangeList oldDefaultList) {
-    clm.setDefaultChangeList(targetChangeList, true);
-    LOG.debug(String.format("Active changelist changed: %s -> %s", oldDefaultList.getName(), targetChangeList.getName()));
+  private fun switchChangeList(clm: ChangeListManagerEx,
+                               targetChangeList: LocalChangeList,
+                               oldDefaultList: LocalChangeList) {
+    clm.setDefaultChangeList(targetChangeList, true)
+    LOG.debug("Active changelist changed: ${oldDefaultList.name} -> ${targetChangeList.name}")
   }
 
-  private static void restoreChangeList(@NotNull ChangeListManagerEx clm,
-                                        @NotNull LocalChangeList targetChangeList,
-                                        @NotNull LocalChangeList oldDefaultList) {
-    LocalChangeList defaultChangeList = clm.getDefaultChangeList();
-    if (Objects.equals(defaultChangeList.getId(), targetChangeList.getId())) {
-      clm.setDefaultChangeList(oldDefaultList, true);
-      LOG.debug(String.format("Active changelist restored: %s -> %s", targetChangeList.getName(), oldDefaultList.getName()));
+  private fun restoreChangeList(clm: ChangeListManagerEx,
+                                targetChangeList: LocalChangeList,
+                                oldDefaultList: LocalChangeList) {
+    val defaultChangeList = clm.defaultChangeList
+    if (defaultChangeList.id == targetChangeList.id) {
+      clm.setDefaultChangeList(oldDefaultList, true)
+      LOG.debug("Active changelist restored: ${targetChangeList.name} -> ${oldDefaultList.name}")
     }
     else {
-      LOG.warn(new Throwable(String.format("Active changelist was changed during the operation. Expected: %s -> %s, actual default: %s",
-                                           targetChangeList.getName(), oldDefaultList.getName(), defaultChangeList.getName())));
+      LOG.warn(Throwable("Active changelist was changed during the operation. " +
+                         "Expected: ${targetChangeList.name} -> ${oldDefaultList.name}, " +
+                         "actual default: ${defaultChangeList.name}"))
     }
   }
 
-  @NotNull
-  public static ThreeStateCheckBox.State convertExclusionState(@NotNull ExclusionState exclusionState) {
-    if (exclusionState == ExclusionState.ALL_INCLUDED) {
-      return ThreeStateCheckBox.State.SELECTED;
-    }
-    else if (exclusionState == ExclusionState.ALL_EXCLUDED) {
-      return ThreeStateCheckBox.State.NOT_SELECTED;
-    }
-    else {
-      return ThreeStateCheckBox.State.DONT_CARE;
+  @JvmStatic
+  fun convertExclusionState(exclusionState: ExclusionState): ThreeStateCheckBox.State {
+    return when (exclusionState) {
+      ExclusionState.ALL_INCLUDED -> ThreeStateCheckBox.State.SELECTED
+      ExclusionState.ALL_EXCLUDED -> ThreeStateCheckBox.State.NOT_SELECTED
+      else -> ThreeStateCheckBox.State.DONT_CARE
     }
   }
 }
