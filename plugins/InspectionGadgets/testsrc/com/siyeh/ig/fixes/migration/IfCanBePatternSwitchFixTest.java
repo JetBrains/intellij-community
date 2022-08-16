a4eab42f89691b4c2584fc3d9deaa430e68ca304
Andrey.Cherkasov <andrey.cherkasov@jetbrains.com>
2022-08-16 17:24:07 +0400

    [java-inspections] IfCanBeSwitch: support when clauses in pattern switches
    IDEA-300032
    
    GitOrigin-RevId: 78de1e5fc686329e5781a1c4a78e1d64741a61d9

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/fixes/migration/IfCanBePatternSwitchFixTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/fixes/migration/IfCanBePatternSwitchFixTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.siyeh.ig.fixes.migration;
 
 import com.intellij.codeInspection.CommonQuickFixBundle;
@@ -42,4 +42,7 @@ public class IfCanBePatternSwitchFixTest extends IGQuickFixesTestCase {
   public void testCastsReplacedWithPattern() { doTest(); }
   public void testMultipleCastedVariables() { doTest(); }
   public void testMutableCastedVariable() { doTest(); }
+  public void testPatternGuard() {
+    IdeaTestUtil.withLevel(myFixture.getModule(), LanguageLevel.JDK_19_PREVIEW, () -> doTest());
+  }
 }
