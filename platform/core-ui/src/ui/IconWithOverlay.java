d34019f17870fd2b9b352f2e3b26905f7200b6b8
Konstantin Bulenkov <kb@jetbrains.com>
2022-08-16 21:00:30 +0200

    clean up. introduce helper methods
    GitOrigin-RevId: 0ce9fe934dfb252f008efbac9bfbe118b1b0d34f

--- platform/core-ui/src/ui/IconWithOverlay.java
+++ platform/core-ui/src/ui/IconWithOverlay.java
@@ -22,6 +22,24 @@ public abstract class IconWithOverlay extends LayeredIcon {
 
   public abstract @Nullable Shape getOverlayShape(int x, int y);
 
+  @NotNull
+  public Icon getMainIcon() {
+    return getScaled(0);
+  }
+
+  @NotNull
+  public Icon getOverlayIcon() {
+    return getScaled(1);
+  }
+
+  @NotNull
+  private Icon getScaled(int layer) {
+    Icon icon = getIcon(layer);
+    assert icon != null;
+    float scale = getScale();
+    return scale == 1f ? icon : IconUtil.scale(icon, null, scale);
+  }
+
   @Override
   public void paintIcon(Component c, Graphics g, int x, int y) {
     Shape dontPaintHere = getOverlayShape(0, 0);
@@ -29,21 +47,15 @@ public abstract class IconWithOverlay extends LayeredIcon {
       super.paintIcon(c, g, x, y);
       return;
     }
-    float scale = getScale();
     BufferedImage img = UIUtil.createImage(((Graphics2D)g).getDeviceConfiguration(), getIconWidth(), getIconHeight(), BufferedImage.TYPE_INT_ARGB, PaintUtil.RoundingMode.CEIL);
     Graphics2D g2 = img.createGraphics();
-    IconUtil.scale(getIcon(0), scale).paintIcon(c, g2, 0, 0);
+    getMainIcon().paintIcon(c, g2, 0, 0);
     GraphicsConfig config = new GraphicsConfig(g2);
     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     g2.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR));
     g2.fill(dontPaintHere);
     config.restore();
     UIUtil.drawImage(g,img, x, y, null);
-    Icon overlay = getIcon(1);
-    assert overlay != null;
-    if (scale != 1f) {
-      overlay = IconUtil.scale(overlay, null, scale);
-    }
-    overlay.paintIcon(c, g, x, y);
+    getOverlayIcon().paintIcon(c, g, x, y);
   }
 }
