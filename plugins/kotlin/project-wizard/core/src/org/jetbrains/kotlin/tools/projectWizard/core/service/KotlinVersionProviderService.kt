da17e9fb8acc7c215b10f9e018138f5cb68cb681
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2022-08-17 12:46:50 +0200

    [Kotlin/JS, Project Wizard] Bootstrap repos in tests and in gradle plugin repos
    (cherry picked from commit b8a36d48e87f4ab827434a8727b1a5bd1df38c63)
    
    IJ-MR-77587
    
    GitOrigin-RevId: 7c53ffe12d393a257a7b8ca84a1439b7bc68d60c

--- plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/core/service/KotlinVersionProviderService.kt
+++ plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/core/service/KotlinVersionProviderService.kt
@@ -16,7 +16,7 @@ abstract class KotlinVersionProviderService : WizardService {
         version,
         getKotlinVersionKind(version),
         getKotlinVersionRepository(version),
-        getBuildSystemPluginRepository(getKotlinVersionKind(version), getDevVersionRepository()),
+        getBuildSystemPluginRepository(getKotlinVersionKind(version), listOf(getDevVersionRepository())),
     )
 
     private fun getKotlinVersionRepository(versionKind: KotlinVersionKind): Repository = when (versionKind) {
@@ -40,17 +40,17 @@ abstract class KotlinVersionProviderService : WizardService {
     companion object {
         fun getBuildSystemPluginRepository(
             versionKind: KotlinVersionKind,
-            devRepository: Repository
-        ): (BuildSystemType) -> Repository? =
+            devRepositories: List<Repository>
+        ): (BuildSystemType) -> List<Repository> =
             when (versionKind) {
                 KotlinVersionKind.STABLE, KotlinVersionKind.EAP, KotlinVersionKind.M -> { buildSystem ->
                     when (buildSystem) {
-                        BuildSystemType.GradleKotlinDsl, BuildSystemType.GradleGroovyDsl -> DefaultRepository.GRADLE_PLUGIN_PORTAL
-                        BuildSystemType.Maven -> DefaultRepository.MAVEN_CENTRAL
-                        BuildSystemType.Jps -> null
+                        BuildSystemType.GradleKotlinDsl, BuildSystemType.GradleGroovyDsl -> listOf(DefaultRepository.GRADLE_PLUGIN_PORTAL)
+                        BuildSystemType.Maven -> listOf(DefaultRepository.MAVEN_CENTRAL)
+                        BuildSystemType.Jps -> emptyList()
                     }
                 }
-                KotlinVersionKind.DEV -> { _ -> devRepository }
+                KotlinVersionKind.DEV -> { _ -> devRepositories }
             }
     }
 }
