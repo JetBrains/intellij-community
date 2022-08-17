0b3ee02f3fdd67c317e5735ffe9b5e8e00bed31c
Pavel Gromov <pavel.gromov@jetbrains.com>
2022-08-17 16:56:14 +0200

    [welcome screen] Convert RevealProjectDirAction to kotlin
    GitOrigin-RevId: 1b2091b0eaac7e747d2847a8f2b1ba39b7e8b444

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/projectActions/RevealProjectDirAction.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/projectActions/RevealProjectDirAction.kt
@@ -1,41 +1,31 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.wm.impl.welcomeScreen.projectActions;
+package com.intellij.openapi.wm.impl.welcomeScreen.projectActions
 
-import com.intellij.ide.actions.RevealFileAction;
-import com.intellij.ide.lightEdit.LightEditCompatible;
-import com.intellij.openapi.actionSystem.ActionUpdateThread;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.project.DumbAwareAction;
-import com.intellij.openapi.wm.impl.welcomeScreen.recentProjects.RecentProjectItem;
-import com.intellij.openapi.wm.impl.welcomeScreen.recentProjects.RecentProjectTreeItem;
-import org.jetbrains.annotations.NotNull;
-
-import java.io.File;
+import com.intellij.ide.actions.RevealFileAction
+import com.intellij.ide.lightEdit.LightEditCompatible
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.wm.impl.welcomeScreen.projectActions.RecentProjectsWelcomeScreenActionBase.Companion.getSelectedItem
+import com.intellij.openapi.wm.impl.welcomeScreen.recentProjects.RecentProjectItem
+import java.io.File
 
 /**
  * @author Konstantin Bulenkov
  */
-public final class RevealProjectDirAction extends DumbAwareAction implements LightEditCompatible {
-  public RevealProjectDirAction() {
-    super(RevealFileAction.getActionName());
+class RevealProjectDirAction : DumbAwareAction(RevealFileAction.getActionName()), LightEditCompatible {
+  override fun actionPerformed(e: AnActionEvent) {
+    val item = getSelectedItem(e) as RecentProjectItem
+    val path = item.projectPath
+    RevealFileAction.selectDirectory(File(path))
   }
 
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
-    RecentProjectItem item = (RecentProjectItem)RecentProjectsWelcomeScreenActionBase.Companion.getSelectedItem$intellij_platform_ide_impl(e);
-    assert item != null;
-    String path = item.getProjectPath();
-    RevealFileAction.selectDirectory(new File(path));
+  override fun getActionUpdateThread(): ActionUpdateThread {
+    return ActionUpdateThread.EDT
   }
 
-  @Override
-  public @NotNull ActionUpdateThread getActionUpdateThread() {
-    return ActionUpdateThread.EDT;
-  }
-
-  @Override
-  public void update(@NotNull AnActionEvent e) {
-    RecentProjectTreeItem item = RecentProjectsWelcomeScreenActionBase.Companion.getSelectedItem$intellij_platform_ide_impl(e);
-    e.getPresentation().setEnabledAndVisible(item instanceof RecentProjectItem);
+  override fun update(e: AnActionEvent) {
+    val item = getSelectedItem(e)
+    e.presentation.isEnabledAndVisible = item is RecentProjectItem
   }
 }
