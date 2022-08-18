843d612cf920719c3a1909d461751a23626767aa
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2022-08-18 14:01:00 +0300

    IDEA-298168 Limit the size of shortcut popup by the content
    Also, replace deprecated VerticalLayout with actual one.
    
    IJ-CR-94458
    
    GitOrigin-RevId: d23a832f2ba2b6a1fc3ffe965e2f1417a7105730

--- platform/platform-impl/src/com/intellij/ide/ui/text/ActionKeyPopup.kt
+++ platform/platform-impl/src/com/intellij/ide/ui/text/ActionKeyPopup.kt
@@ -2,7 +2,6 @@
 package com.intellij.ide.ui.text
 
 import com.intellij.ide.IdeBundle
-import com.intellij.ide.plugins.newui.VerticalLayout
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.KeyboardShortcut
 import com.intellij.openapi.keymap.KeymapManager
@@ -13,7 +12,7 @@ import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.ui.JBColor
 import com.intellij.ui.awt.RelativePoint
 import com.intellij.ui.components.ActionLink
-import com.intellij.ui.scale.JBUIScale
+import com.intellij.ui.components.panels.VerticalLayout
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
 import org.jetbrains.annotations.ApiStatus
@@ -35,7 +34,7 @@ fun showActionKeyPopup(parent: Component,
 
   lateinit var balloon: Balloon
   val jPanel = JPanel()
-  jPanel.layout = VerticalLayout(JBUIScale.scale(UIUtil.DEFAULT_VGAP), 250)
+  jPanel.layout = VerticalLayout(UIUtil.DEFAULT_VGAP)
   jPanel.isOpaque = false
   jPanel.add(JLabel(action.templatePresentation.text).also {
     it.foreground = UIUtil.getToolTipForeground()
@@ -70,7 +69,6 @@ fun showActionKeyPopup(parent: Component,
     .setHideOnAction(true)
     .setAnimationCycle(0)
     .setCalloutShift(height / 2 + 1)
-    .setCornerToPointerDistance(80)
     .setBlockClicksThroughBalloon(true)
     .setBorderColor(JBUI.CurrentTheme.Tooltip.borderColor())
     .setFillColor(UIUtil.getToolTipBackground())
