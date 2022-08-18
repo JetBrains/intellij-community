a155fef3dd793bcd48d7cf562f69d3f4ef4ff667
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-08-18 13:09:43 +0200

    vcs: convert CreatePatchConfigurationPanel to UI DSL
    GitOrigin-RevId: 9bb5766a9b28a242a4660df9de3f8caf96e38bdf

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/patch/CreatePatchConfigurationPanel.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/patch/CreatePatchConfigurationPanel.kt
@@ -1,199 +1,198 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.openapi.vcs.changes.patch;
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.vcs.changes.patch
 
-import com.intellij.ide.IdeBundle;
-import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
-import com.intellij.openapi.fileChooser.FileChooserFactory;
-import com.intellij.openapi.fileChooser.FileSaverDescriptor;
-import com.intellij.openapi.fileChooser.FileSaverDialog;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.ComboBox;
-import com.intellij.openapi.ui.TextBrowseFolderListener;
-import com.intellij.openapi.ui.TextFieldWithBrowseButton;
-import com.intellij.openapi.ui.ValidationInfo;
-import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vcs.VcsBundle;
-import com.intellij.openapi.vfs.CharsetToolkit;
-import com.intellij.openapi.vfs.VirtualFileWrapper;
-import com.intellij.openapi.vfs.encoding.EncodingProjectManager;
-import com.intellij.project.ProjectKt;
-import com.intellij.ui.JBColor;
-import com.intellij.ui.components.JBRadioButton;
-import com.intellij.util.ui.FormBuilder;
-import com.intellij.util.ui.JBUI;
-import com.intellij.util.ui.UIUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.ide.IdeBundle
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
+import com.intellij.openapi.fileChooser.FileChooserFactory
+import com.intellij.openapi.fileChooser.FileSaverDescriptor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.*
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vcs.VcsBundle.message
+import com.intellij.openapi.vfs.CharsetToolkit
+import com.intellij.openapi.vfs.encoding.EncodingProjectManager
+import com.intellij.project.stateStore
+import com.intellij.ui.JBColor
+import com.intellij.ui.dsl.builder.*
+import com.intellij.ui.dsl.gridLayout.HorizontalAlign
+import java.io.File
+import java.nio.charset.Charset
+import java.nio.file.Path
+import java.nio.file.Paths
+import javax.swing.*
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.nio.charset.Charset;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+class CreatePatchConfigurationPanel(val project: Project) {
+  private val panel: DialogPanel
 
-public final class CreatePatchConfigurationPanel {
-  private static final int TEXT_FIELD_WIDTH = 70;
+  private val fileNameField: TextFieldWithBrowseButton = TextFieldWithBrowseButton()
+  private val basePathField: TextFieldWithBrowseButton = TextFieldWithBrowseButton()
 
-  private JPanel myMainPanel;
-  private TextFieldWithBrowseButton myFileNameField;
-  private TextFieldWithBrowseButton myBasePathField;
-  private JCheckBox myReversePatchCheckbox;
-  private ComboBox<Charset> myEncoding;
-  private JLabel myWarningLabel;
-  private final Project myProject;
-  @Nullable private File myCommonParentDir;
-  private JBRadioButton myToClipboardButton;
-  private JBRadioButton myToFileButton;
+  private lateinit var toClipboardRadioButton: JRadioButton
+  private lateinit var reverseCheckBox: JCheckBox
+  private lateinit var encodingComboBox: ComboBox<Charset>
+  private lateinit var warningLabel: JLabel
 
-  public CreatePatchConfigurationPanel(@NotNull Project project) {
-    myProject = project;
-    initMainPanel();
+  private var commonParentDir: File? = null
 
-    myFileNameField.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        FileSaverDialog dialog = FileChooserFactory.getInstance()
-          .createSaveFileDialog(new FileSaverDescriptor(VcsBundle.message("patch.creation.save.to.title"), ""), myMainPanel);
-        String path = FileUtil.toSystemIndependentName(getFileName());
-        int index = path.lastIndexOf("/");
-        Path baseDir = index == -1 ? ProjectKt.getStateStore(project).getProjectBasePath() : Paths.get(path.substring(0, index));
-        String name = index == -1 ? path : path.substring(index + 1);
-        VirtualFileWrapper fileWrapper = dialog.save(baseDir, name);
-        if (fileWrapper != null) {
-          myFileNameField.setText(fileWrapper.getFile().getPath());
+  init {
+    fileNameField.addActionListener {
+      val fileName = selectFileName()
+      if (fileName != null) {
+        fileNameField.text = fileName
+      }
+    }
+
+    basePathField.addBrowseFolderListener(TextBrowseFolderListener(FileChooserDescriptorFactory.createSingleFolderDescriptor()))
+    basePathField.text = project.stateStore.projectBasePath.toString()
+
+    panel = createPanel()
+  }
+
+  private fun selectFileName(): String? {
+    @Suppress("DialogTitleCapitalization") val descriptor =
+      FileSaverDescriptor(message("patch.creation.save.to.title"), "")
+    val dialog = FileChooserFactory.getInstance().createSaveFileDialog(descriptor, fileNameField)
+    val path = FileUtil.toSystemIndependentName(getFileName())
+    val index = path.lastIndexOf("/")
+    val baseDir = if (index == -1) project.stateStore.projectBasePath else Paths.get(path.substring(0, index))
+    val name = if (index == -1) path else path.substring(index + 1)
+    val fileWrapper = dialog.save(baseDir, name) ?: return null
+    return fileWrapper.file.path
+  }
+
+  private fun createPanel(): DialogPanel {
+    return panel {
+      buttonsGroup {
+        row {
+          val toFileButton = radioButton(message("create.patch.file.path"))
+          toFileButton.component.isSelected = true
+          cell(fileNameField)
+            .columns(COLUMNS_LARGE)
+            .horizontalAlign(HorizontalAlign.FILL)
+            .enabledIf(toFileButton.selected)
+        }.layout(RowLayout.LABEL_ALIGNED)
+        row {
+          toClipboardRadioButton = radioButton(message("create.patch.to.clipboard"))
+            .component
         }
       }
-    });
 
-    myToFileButton.addChangeListener(e -> myFileNameField.setEnabled(myToFileButton.isSelected()));
-    myFileNameField.setTextFieldPreferredWidth(TEXT_FIELD_WIDTH);
-    myBasePathField.setTextFieldPreferredWidth(TEXT_FIELD_WIDTH);
-    myBasePathField.addBrowseFolderListener(new TextBrowseFolderListener(FileChooserDescriptorFactory.createSingleFolderDescriptor()));
-    myWarningLabel.setForeground(JBColor.RED);
-    selectBasePath(ProjectKt.getStateStore(project).getProjectBasePath().toString());
-    initEncodingCombo();
+      row(message("patch.creation.base.path.field")) {
+        cell(basePathField)
+          .columns(COLUMNS_LARGE)
+          .horizontalAlign(HorizontalAlign.FILL)
+      }.topGap(TopGap.SMALL)
+      row {
+        reverseCheckBox = checkBox(message("create.patch.reverse.checkbox")).component
+      }
+      row(message("create.patch.encoding")) {
+        encodingComboBox = comboBox(DefaultComboBoxModel(CharsetToolkit.getAvailableCharsets())).component
+        encodingComboBox.selectedItem = EncodingProjectManager.getInstance(project).defaultCharset
+      }
+      row {
+        warningLabel = label("")
+          .applyToComponent {
+            foreground = JBColor.RED
+            isVisible = false
+          }.component
+      }
+    }
   }
 
-  public void selectBasePath(@NotNull String baseDir) {
-    myBasePathField.setText(baseDir);
+  fun selectBasePath(baseDir: String) {
+    basePathField.text = baseDir
   }
 
-  private void initEncodingCombo() {
-    ComboBoxModel<Charset> encodingsModel = new DefaultComboBoxModel<>(CharsetToolkit.getAvailableCharsets());
-    myEncoding.setModel(encodingsModel);
-    Charset projectCharset = EncodingProjectManager.getInstance(myProject).getDefaultCharset();
-    myEncoding.setSelectedItem(projectCharset);
+  fun getEncoding(): Charset {
+    return encodingComboBox.item
   }
 
-  @NotNull
-  public Charset getEncoding() {
-    return (Charset)myEncoding.getSelectedItem();
+  fun setCommonParentPath(commonParentPath: File?) {
+    if (commonParentPath != null && !commonParentPath.isDirectory) {
+      commonParentDir = commonParentPath.parentFile
+    }
+    else {
+      commonParentDir = commonParentPath
+    }
   }
 
-  private void initMainPanel() {
-    myFileNameField = new TextFieldWithBrowseButton();
-    myBasePathField = new TextFieldWithBrowseButton();
-    myReversePatchCheckbox = new JCheckBox(VcsBundle.message("create.patch.reverse.checkbox"));
-    myEncoding = new ComboBox<>();
-    myWarningLabel = new JLabel();
-    myToFileButton = new JBRadioButton(VcsBundle.message("create.patch.file.path"), true);
+  fun getPanel(): JComponent {
+    return panel
+  }
 
-    if (UIUtil.isUnderWin10LookAndFeel()) {
-      myToFileButton.setBorder(JBUI.Borders.emptyRight(UIUtil.DEFAULT_HGAP));
+  fun getFileName(): String {
+    return FileUtil.expandUserHome(fileNameField.text.trim())
+  }
+
+  fun getBaseDirName(): String {
+    return FileUtil.expandUserHome(basePathField.getText().trim())
+  }
+
+  fun setFileName(file: Path) {
+    fileNameField.text = file.toString()
+  }
+
+  fun isReversePatch(): Boolean {
+    return reverseCheckBox.isSelected
+  }
+
+  fun setReversePatch(reverse: Boolean) {
+    reverseCheckBox.isSelected = reverse
+  }
+
+  fun setReverseEnabledAndVisible(isAvailable: Boolean) {
+    reverseCheckBox.isVisible = isAvailable
+    reverseCheckBox.isEnabled = isAvailable
+  }
+
+  fun isToClipboard(): Boolean {
+    return toClipboardRadioButton.isSelected
+  }
+
+  fun setToClipboard(toClipboard: Boolean) {
+    toClipboardRadioButton.isSelected = toClipboard
+  }
+
+  fun isOkToExecute(): Boolean {
+    return validateFields() == null
+  }
+
+  fun validateFields(): ValidationInfo? {
+    warnIfFileExists()
+
+    return validateFileName() ?: validateBaseDirPath()
+  }
+
+  private fun warnIfFileExists() {
+    val fileName = getFileName()
+    warningLabel.text = if (File(fileName).exists()) IdeBundle.message("error.file.with.name.already.exists", fileName) else ""
+    warningLabel.isVisible = warningLabel.text.isNotBlank()
+  }
+
+  private fun validateFileName(): ValidationInfo? {
+    val validateNameError = PatchNameChecker.validateName(getFileName())
+    if (validateNameError != null) {
+      return ValidationInfo(validateNameError, fileNameField)
     }
 
-    myToClipboardButton = new JBRadioButton(VcsBundle.message("create.patch.to.clipboard"));
-    ButtonGroup group = new ButtonGroup();
-    group.add(myToFileButton);
-    group.add(myToClipboardButton);
-    JPanel toFilePanel = JBUI.Panels.simplePanel().addToLeft(myToFileButton).addToCenter(myFileNameField);
-
-    myMainPanel = FormBuilder.createFormBuilder()
-      .addComponent(toFilePanel)
-      .addComponent(myToClipboardButton)
-      .addVerticalGap(5)
-      .addLabeledComponent(VcsBundle.message("patch.creation.base.path.field"), myBasePathField)
-      .addComponent(myReversePatchCheckbox)
-      .addLabeledComponent(VcsBundle.message("create.patch.encoding"), myEncoding)
-      .addComponent(myWarningLabel)
-      .getPanel();
+    return null
   }
 
-  public void setCommonParentPath(@Nullable File commonParentPath) {
-    myCommonParentDir = commonParentPath == null || commonParentPath.isDirectory() ? commonParentPath : commonParentPath.getParentFile();
-  }
-
-  private void checkExist() {
-    String fileName = getFileName();
-    myWarningLabel.setText(new File(fileName).exists() ? IdeBundle.message("error.file.with.name.already.exists", fileName) : "");
-  }
-
-  public JComponent getPanel() {
-    return myMainPanel;
-  }
-
-  public String getFileName() {
-    return FileUtil.expandUserHome(myFileNameField.getText().trim());
-  }
-
-  @NotNull
-  public String getBaseDirName() {
-    return FileUtil.expandUserHome(myBasePathField.getText().trim());
-  }
-
-  public void setFileName(@NotNull Path file) {
-    myFileNameField.setText(file.toString());
-  }
-
-  public boolean isReversePatch() {
-    return myReversePatchCheckbox.isSelected();
-  }
-
-  public void setReversePatch(boolean reverse) {
-    myReversePatchCheckbox.setSelected(reverse);
-  }
-
-  public void setReverseEnabledAndVisible(boolean isAvailable) {
-    myReversePatchCheckbox.setVisible(isAvailable);
-    myReversePatchCheckbox.setEnabled(isAvailable);
-  }
-
-  public boolean isToClipboard() {
-    return myToClipboardButton.isSelected();
-  }
-
-  public void setToClipboard(boolean toClipboard) {
-    myToClipboardButton.setSelected(toClipboard);
-  }
-
-  public boolean isOkToExecute() {
-    return validateFields() == null;
-  }
-
-  @Nullable
-  private ValidationInfo verifyBaseDirPath() {
-    String baseDirName = getBaseDirName();
-    if (StringUtil.isEmptyOrSpaces(baseDirName)) {
-      return new ValidationInfo(
-        VcsBundle.message("patch.creation.empty.base.path.error"), myBasePathField);
+  private fun validateBaseDirPath(): ValidationInfo? {
+    val baseDirName = getBaseDirName()
+    if (baseDirName.isBlank()) {
+      return ValidationInfo(message("patch.creation.empty.base.path.error"), basePathField)
     }
-    File baseFile = new File(baseDirName);
-    if (!baseFile.exists()) return new ValidationInfo(VcsBundle.message("patch.creation.base.dir.does.not.exist.error"), myBasePathField);
-    if (myCommonParentDir != null && !FileUtil.isAncestor(baseFile, myCommonParentDir, false)) {
-      return new ValidationInfo(VcsBundle.message("patch.creation.wrong.base.path.for.changes.error", myCommonParentDir.getPath()),
-                                myBasePathField);
-    }
-    return null;
-  }
 
-  @Nullable
-  public ValidationInfo validateFields() {
-    checkExist();
-    String validateNameError = PatchNameChecker.validateName(getFileName());
-    if (validateNameError != null) return new ValidationInfo(validateNameError, myFileNameField);
-    return verifyBaseDirPath();
+    val baseFile = File(baseDirName)
+    if (!baseFile.exists()) {
+      return ValidationInfo(message("patch.creation.base.dir.does.not.exist.error"), basePathField)
+    }
+
+    val commonParent = commonParentDir ?: return null
+    if (!FileUtil.isAncestor(baseFile, commonParent, false)) {
+      return ValidationInfo(message("patch.creation.wrong.base.path.for.changes.error", commonParent.path), basePathField)
+    }
+    return null
   }
 }
