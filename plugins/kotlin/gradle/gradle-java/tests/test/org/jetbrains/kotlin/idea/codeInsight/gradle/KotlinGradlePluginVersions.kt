e48c970a4c66b613cd1e778f49bd76a4907d10a8
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-08-22 18:00:15 +0200

    [kt-[213|221]-* ONLY] Cherry-pick the minimal version of the KotlinGradlePluginVersions functionality
    In `kt-[213|221]-1.7.20` branches we need this commit so CI builds won't fail with
    "unknown argument kotlinGradlePluginVersion"
    
    GitOrigin-RevId: 8bcd2fc931be0e2b09cb5808229d290e267d61f6

---
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/KotlinGradlePluginVersions.kt
@@ -0,0 +1,7 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.codeInsight.gradle
+
+// minimal version for cherry-pick to the kt-[213|221]-* branches
+object KotlinGradlePluginVersions {
+    val latest = "1.8.0-dev-446"
+}
