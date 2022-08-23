7901862c74499d3489950ff23b0a84326933f26f
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2022-08-23 18:33:16 +0300

    [ide starter] added missed onFinished test watcher implementation
    GitOrigin-RevId: ad333705ddad1050ec7726ff7285eb9fc8f31634

--- tools/ideTestingFramework/intellij.tools.ide.starter.junit5/src/com/intellij/ide/starter/junit5/JUnit5TestWatcher.kt
+++ tools/ideTestingFramework/intellij.tools.ide.starter.junit5/src/com/intellij/ide/starter/junit5/JUnit5TestWatcher.kt
@@ -8,6 +8,11 @@ import org.junit.jupiter.api.extension.TestWatcher
 class JUnit5TestWatcher(private val testContextGetter: () -> IDETestContext) : TestWatcher {
   val watcherActions = TestWatcherActions()
 
+  override fun testSuccessful(context: ExtensionContext?) {
+    watcherActions.onFinishedActions.forEach { action -> action(testContextGetter()) }
+    super.testSuccessful(context)
+  }
+
   override fun testFailed(context: ExtensionContext?, cause: Throwable?) {
     watcherActions.onFailureActions.forEach { action -> action(testContextGetter()) }
     super.testFailed(context, cause)
