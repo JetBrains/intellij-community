43a68bb14b57ec892fc11ddace99c2a59e9519a3
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-08-24 15:57:56 +0200

    vcs: unify commit flow - use same CheckinHandler for modal and non-modal workflows
    'CheckinHandler.beforeCheckin' and 'CommitCheck.runCheck' are mutually exclusive.
    
    GitOrigin-RevId: a7bb8580e4eab18a33a9e0a89d26b4ca85f7d8d7

--- platform/vcs-impl/src/com/intellij/openapi/vcs/checkin/RearrangeBeforeCheckinHandler.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/checkin/RearrangeBeforeCheckinHandler.kt
@@ -4,58 +4,24 @@ package com.intellij.openapi.vcs.checkin
 import com.intellij.codeInsight.CodeInsightBundle
 import com.intellij.codeInsight.actions.AbstractLayoutCodeProcessor
 import com.intellij.codeInsight.actions.RearrangeCodeProcessor
-import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.progress.ProgressIndicator
-import com.intellij.openapi.project.DumbService
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.vcs.CheckinProjectPanel
 import com.intellij.openapi.vcs.VcsBundle
-import com.intellij.openapi.vcs.VcsConfiguration
 import com.intellij.openapi.vcs.changes.CommitContext
 import com.intellij.openapi.vcs.changes.ui.BooleanCommitOption
 import com.intellij.openapi.vcs.checkin.CheckinHandlerUtil.getPsiFiles
-import com.intellij.openapi.vcs.checkin.RearrangeBeforeCheckinHandler.Companion.COMMAND_NAME
 import com.intellij.openapi.vcs.ui.RefreshableOnComponent
-import com.intellij.vcs.commit.isBackgroundCommitChecks
-import com.intellij.vcs.commit.isNonModalCommit
 
 open class RearrangeCheckinHandlerFactory : CheckinHandlerFactory() {
   override fun createHandler(panel: CheckinProjectPanel, commitContext: CommitContext): CheckinHandler =
-    if (isBackgroundCommitChecks() && panel.isNonModalCommit) BackgroundRearrangeCheckinHandler(panel)
-    else RearrangeBeforeCheckinHandler(panel.project, panel)
+    RearrangeBeforeCheckinHandler(panel)
 }
 
-open class RearrangeBeforeCheckinHandler(
-  private val project: Project,
-  private val panel: CheckinProjectPanel
-) : CheckinHandler(),
-    CheckinModificationHandler {
-
-  private val settings get() = VcsConfiguration.getInstance(project)
-
+private class RearrangeBeforeCheckinHandler(commitPanel: CheckinProjectPanel) : CodeProcessorCheckinHandler(commitPanel) {
   override fun getBeforeCheckinConfigurationPanel(): RefreshableOnComponent =
-    BooleanCommitOption(panel, VcsBundle.message("checkbox.checkin.options.rearrange.code"), true,
+    BooleanCommitOption(commitPanel, VcsBundle.message("checkbox.checkin.options.rearrange.code"), true,
                         settings::REARRANGE_BEFORE_PROJECT_COMMIT)
-
-  override fun beforeCheckin(): ReturnResult {
-    if (settings.REARRANGE_BEFORE_PROJECT_COMMIT && !DumbService.isDumb(project)) {
-      RearrangeCodeProcessor(project, getPsiFiles(project, panel.virtualFiles), COMMAND_NAME, null, true).run()
-      FileDocumentManager.getInstance().saveAllDocuments()
-    }
-    return ReturnResult.COMMIT
-  }
-
-  companion object {
-    @JvmField
-    @NlsSafe
-    val COMMAND_NAME: String = CodeInsightBundle.message("process.rearrange.code.before.commit")
-  }
-}
-
-private class BackgroundRearrangeCheckinHandler(commitPanel: CheckinProjectPanel) : CodeProcessorCheckinHandler(commitPanel) {
-  override fun getBeforeCheckinConfigurationPanel(): RefreshableOnComponent =
-    RearrangeBeforeCheckinHandler(project, commitPanel).beforeCheckinConfigurationPanel
 
   override fun isEnabled(): Boolean = settings.REARRANGE_BEFORE_PROJECT_COMMIT
 
@@ -67,4 +33,10 @@ private class BackgroundRearrangeCheckinHandler(commitPanel: CheckinProjectPanel
 
   override fun createCodeProcessor(): AbstractLayoutCodeProcessor =
     RearrangeCodeProcessor(project, getPsiFiles(project, commitPanel.virtualFiles), COMMAND_NAME, null, true)
+
+  companion object {
+    @JvmField
+    @NlsSafe
+    val COMMAND_NAME: String = CodeInsightBundle.message("process.rearrange.code.before.commit")
+  }
 }
