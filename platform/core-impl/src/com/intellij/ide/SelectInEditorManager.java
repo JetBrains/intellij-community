578566e59426019e6f84c0a41cfe268a32995069
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-08-24 18:48:02 +0200

    move SelectInEditorManager from core-api to core-impl
    GitOrigin-RevId: 0fbee5b4ec604684555d38eadef7724ebfb43f4a

--- platform/core-impl/src/com/intellij/ide/SelectInEditorManager.java
+++ platform/core-impl/src/com/intellij/ide/SelectInEditorManager.java
@@ -1,13 +1,10 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
-/**
- * @author MYakovlev
- */
-public abstract class SelectInEditorManager{
+public abstract class SelectInEditorManager {
   public static SelectInEditorManager getInstance(Project project) {
     return project.getService(SelectInEditorManager.class);
   }
