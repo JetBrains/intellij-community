05f2a2609522bdc620e38a3d88aebe61a6dafead
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-08-26 08:52:25 +0200

    cleanup
    GitOrigin-RevId: 49c623ee61c28c5e2d68723548ac624b675864cd

--- platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkEditorNotification.java
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkEditorNotification.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.projectRoots.impl;
 
+import com.intellij.openapi.components.Service;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.VerticalFlowLayout;
@@ -12,33 +13,31 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.util.Collections;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 
+@Service(Service.Level.PROJECT)
 public final class UnknownSdkEditorNotification {
   public static @NotNull UnknownSdkEditorNotification getInstance(@NotNull Project project) {
     return project.getService(UnknownSdkEditorNotification.class);
   }
 
-  private final AtomicReference<Set<UnknownSdkFix>> myNotifications = new AtomicReference<>(new LinkedHashSet<>());
+  private final AtomicReference<List<UnknownSdkFix>> notifications = new AtomicReference<>(Collections.emptyList());
 
   public boolean allowProjectSdkNotifications() {
-    return myNotifications.get().isEmpty();
+    return notifications.get().isEmpty();
   }
 
   public @NotNull List<UnknownSdkFix> getNotifications() {
-    return List.copyOf(myNotifications.get());
+    return notifications.get();
   }
 
   public void showNotifications(@NotNull List<? extends UnknownSdkFix> notifications) {
-    if (!Registry.is("unknown.sdk.show.editor.actions")) {
+    if (!notifications.isEmpty() && !Registry.is("unknown.sdk.show.editor.actions")) {
       notifications = Collections.emptyList();
     }
-
-    myNotifications.set(Set.copyOf(notifications));
+    this.notifications.set(List.copyOf(notifications));
   }
 }
 
