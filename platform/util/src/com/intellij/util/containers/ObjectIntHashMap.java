86c100149367591ccdea887c0f74ac50c37b060c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-08-26 09:40:47 +0200

    don't use trove
    GitOrigin-RevId: e865ee9e3d40acfa82ae8ea9031cfb14fb98b32a

--- platform/util/src/com/intellij/util/containers/ObjectIntHashMap.java
+++ platform/util/src/com/intellij/util/containers/ObjectIntHashMap.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.containers;
 
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
@@ -9,7 +9,9 @@ import java.util.Set;
 
 /**
  * return -1 instead of 0 if no such mapping exists
+ * @deprecated Use {@link Object2IntOpenHashMap}
  */
+@Deprecated
 public class ObjectIntHashMap<K> implements ObjectIntMap<K> {
   private final Object2IntMap<K> myMap;
   public ObjectIntHashMap() {
--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/highlighter/AbstractHighlightingMetaInfoTest.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/highlighter/AbstractHighlightingMetaInfoTest.kt
@@ -2,8 +2,8 @@
 package org.jetbrains.kotlin.idea.highlighter
 
 import com.intellij.lang.annotation.HighlightSeverity
-import gnu.trove.THashSet
-import gnu.trove.TObjectHashingStrategy
+import it.unimi.dsi.fastutil.Hash
+import it.unimi.dsi.fastutil.objects.ObjectOpenCustomHashSet
 import org.jetbrains.kotlin.codeMetaInfo.model.CodeMetaInfo
 import org.jetbrains.kotlin.idea.codeMetaInfo.CodeMetaInfoTestCase
 import org.jetbrains.kotlin.idea.codeMetaInfo.models.HighlightingCodeMetaInfo
@@ -50,15 +50,27 @@ abstract class AbstractHighlightingMetaInfoTest : KotlinLightCodeInsightFixtureT
 
         val ignoredSeverities = setOf(HighlightSeverity.WARNING, HighlightSeverity.WEAK_WARNING)
 
-        val seenMetaInfos = THashSet(object : TObjectHashingStrategy<HighlightingCodeMetaInfo> {
-            override fun equals(left: HighlightingCodeMetaInfo, right: HighlightingCodeMetaInfo): Boolean =
-                left.start == right.start &&
+        val seenMetaInfos = ObjectOpenCustomHashSet(object : Hash.Strategy<HighlightingCodeMetaInfo> {
+            override fun equals(left: HighlightingCodeMetaInfo?, right: HighlightingCodeMetaInfo?): Boolean {
+                if (left === right) {
+                    return true
+                }
+                if (left == null || right == null) {
+                    return false
+                }
+
+                return left.start == right.start &&
                         left.end == right.end &&
                         left.tag == right.tag &&
                         left.highlightingInfo == right.highlightingInfo
+            }
 
-            override fun computeHashCode(metaInfo: HighlightingCodeMetaInfo): Int =
-                Objects.hash(metaInfo.start, metaInfo.end, metaInfo.tag)
+            override fun hashCode(metaInfo: HighlightingCodeMetaInfo?): Int {
+                if (metaInfo == null) {
+                    return 0
+                }
+                return Objects.hash(metaInfo.start, metaInfo.end, metaInfo.tag)
+            }
         })
 
         return { metaInfo ->
