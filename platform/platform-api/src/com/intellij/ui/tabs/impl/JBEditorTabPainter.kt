fb6d19460f848dd0890646424c258b5c5374cc84
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2022-08-26 10:52:53 +0300

    IDEA-296969 Fix extra thickness of the underline in bottom and right tab placements
    Because of shift for 1px in `updateRect()` there was 4px underline instead of 3px
    
    GitOrigin-RevId: 729ab97e11f29f7e389bcaf7289bdd10ca028d8e

--- platform/platform-api/src/com/intellij/ui/tabs/impl/JBEditorTabPainter.kt
+++ platform/platform-api/src/com/intellij/ui/tabs/impl/JBEditorTabPainter.kt
@@ -4,67 +4,31 @@ package com.intellij.ui.tabs.impl
 import com.intellij.ui.ExperimentalUI
 import com.intellij.ui.tabs.JBTabsPosition
 import com.intellij.ui.tabs.impl.themes.EditorTabTheme
-import java.awt.Color
 import java.awt.Graphics2D
 import java.awt.Point
 import java.awt.Rectangle
 
 class JBEditorTabPainter : JBDefaultTabPainter(EditorTabTheme()) {
-  override fun paintTab(position: JBTabsPosition, g: Graphics2D, rect: Rectangle, borderThickness: Int, tabColor: Color?, active: Boolean, hovered: Boolean) {
-    updatedRect(position, rect, borderThickness)
-    super.paintTab(position, g, rect, borderThickness, tabColor, active, hovered)
-  }
-
-  private fun updatedRect(position: JBTabsPosition, rect: Rectangle, borderThickness: Int): Rectangle {
-    when (position) {
-      JBTabsPosition.bottom -> rect.y += borderThickness
-      JBTabsPosition.right -> rect.x += borderThickness
-      else -> { }
-    }
-    return rect
-  }
-
-  override fun paintSelectedTab(position: JBTabsPosition,
-                                g: Graphics2D,
-                                rect: Rectangle,
-                                borderThickness: Int,
-                                tabColor: Color?,
-                                active: Boolean,
-                                hovered: Boolean) {
-
-    updatedRect(position, rect, borderThickness)
-    super.paintSelectedTab(position, g, rect, borderThickness, tabColor, active, hovered)
-  }
-
-
-  fun paintLeftGap(position: JBTabsPosition, g: Graphics2D, baseRect: Rectangle, borderThickness: Int) {
-    val rect = updatedRect(position, baseRect.clone() as Rectangle, borderThickness)
-
+  fun paintLeftGap(position: JBTabsPosition, g: Graphics2D, rect: Rectangle, borderThickness: Int) {
     val maxY = rect.y + rect.height - borderThickness
 
     paintBorderLine(g, borderThickness, Point(rect.x, rect.y), Point(rect.x, maxY))
   }
 
-  fun paintRightGap(position: JBTabsPosition, g: Graphics2D, baseRect: Rectangle, borderThickness: Int) {
-    val rect = updatedRect(position, baseRect.clone() as Rectangle, borderThickness)
-
+  fun paintRightGap(position: JBTabsPosition, g: Graphics2D, rect: Rectangle, borderThickness: Int) {
     val maxX = rect.x + rect.width - borderThickness
     val maxY = rect.y + rect.height - borderThickness
 
     paintBorderLine(g, borderThickness, Point(maxX, rect.y), Point(maxX, maxY))
   }
 
-  fun paintTopGap(position: JBTabsPosition, g: Graphics2D, baseRect: Rectangle, borderThickness: Int) {
-    val rect = updatedRect(position, baseRect.clone() as Rectangle, borderThickness)
-
+  fun paintTopGap(position: JBTabsPosition, g: Graphics2D, rect: Rectangle, borderThickness: Int) {
     val maxX = rect.x + rect.width
 
     paintBorderLine(g, borderThickness, Point(rect.x, rect.y), Point(maxX, rect.y))
   }
 
-  fun paintBottomGap(position: JBTabsPosition, g: Graphics2D, baseRect: Rectangle, borderThickness: Int) {
-    val rect = updatedRect(position, baseRect.clone() as Rectangle, borderThickness)
-
+  fun paintBottomGap(position: JBTabsPosition, g: Graphics2D, rect: Rectangle, borderThickness: Int) {
     val maxX = rect.x + rect.width - borderThickness
     val maxY = rect.y + rect.height - borderThickness
 
