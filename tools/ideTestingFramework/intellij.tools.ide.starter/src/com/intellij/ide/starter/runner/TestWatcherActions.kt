07fe5fb29ad2aa2bb818f3aba2763a21ca54aae7
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2022-08-29 16:19:18 +0300

    [ide starter] fixes for WM-CR-24
    GitOrigin-RevId: daf2d004d850e926f23d28ad69454f1bfaaec5f7

--- tools/ideTestingFramework/intellij.tools.ide.starter/src/com/intellij/ide/starter/runner/TestWatcherActions.kt
+++ tools/ideTestingFramework/intellij.tools.ide.starter/src/com/intellij/ide/starter/runner/TestWatcherActions.kt
@@ -1,9 +1,6 @@
 package com.intellij.ide.starter.runner
 
 import com.intellij.ide.starter.ide.IDETestContext
-import com.intellij.ide.starter.utils.catchAll
-import com.intellij.ide.starter.utils.logOutput
-import com.intellij.util.io.exists
 import java.util.*
 
 class TestWatcherActions {
@@ -14,23 +11,6 @@ class TestWatcherActions {
   private val _onFinishedActions: MutableList<(IDETestContext) -> Unit> = Collections.synchronizedList(mutableListOf())
   val onFinishedActions: List<(IDETestContext) -> Unit>
     get() = synchronized(_onFinishedActions) { _onFinishedActions.toList() }
-
-  companion object {
-    /** Archive and add to test artifact entire ide's `system` dir */
-    fun getSystemDirAsArtifactAction(): (IDETestContext) -> Unit = { testContext ->
-      catchAll {
-        logOutput("Archive with system directory created and will be published to artifacts")
-        testContext.publishArtifact(source = testContext.paths.systemDir, artifactName = "testSystemDirSnapshot.zip")
-
-        val ideaDirPath = testContext.resolvedProjectHome.resolve(".idea")
-
-        if (ideaDirPath.exists()) {
-          logOutput("Archive with .idea dir created and will be published to artifacts")
-          testContext.publishArtifact(source = ideaDirPath, artifactName = ".idea.zip")
-        }
-      }
-    }
-  }
 
   fun addOnFailureAction(action: (IDETestContext) -> Unit): TestWatcherActions {
     synchronized(_onFailureActions) { _onFailureActions.add(action) }
