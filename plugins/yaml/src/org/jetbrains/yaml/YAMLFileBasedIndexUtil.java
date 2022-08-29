5293a238c9a2016dda3cc64dc8026d55541f66ae
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-08-29 09:17:43 +0200

    revert incorrect changes (as one commit, due to IDEA issue)
    GitOrigin-RevId: 3b63b3fe410f99e4199190b08898b64f01c4d15f

--- plugins/yaml/src/org/jetbrains/yaml/YAMLFileBasedIndexUtil.java
+++ plugins/yaml/src/org/jetbrains/yaml/YAMLFileBasedIndexUtil.java
@@ -1,29 +1,10 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.yaml;
 
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
 import com.intellij.util.indexing.DefaultFileTypeSpecificInputFilter;
 import com.intellij.util.indexing.FileBasedIndex;
 
 public final class YAMLFileBasedIndexUtil {
-
   public static final FileBasedIndex.InputFilter YAML_INPUT_FILTER =
-    new DefaultFileTypeSpecificInputFilter(YAMLFileType.YML);
-
-  /**
-   * Differs from {@link #YAML_INPUT_FILTER} in that this filter matches all files that support YAML, even if they are not pure YAML files.
-   * For example Ruby on Rails' fixture files, which are YAML files that could contain ERb tags.
-   */
-  public static final FileBasedIndex.ProjectSpecificInputFilter CONTAINING_YAML_INPUT_FILTER = indexedFile -> {
-    final Project project = indexedFile.getProject();
-    final VirtualFile vFile = indexedFile.getFile();
-    final PsiFile pFile = PsiManager.getInstance(project).findFile(vFile);
-    if (pFile == null) {
-      return false;
-    }
-    return pFile.getViewProvider().hasLanguage(YAMLLanguage.INSTANCE);
-  };
+    new DefaultFileTypeSpecificInputFilter(YAMLLanguage.INSTANCE.getAssociatedFileType());
 }
