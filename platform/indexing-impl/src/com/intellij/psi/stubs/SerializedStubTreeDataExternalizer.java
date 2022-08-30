e9ec4d345e3b2b3ac484557744e19b00d3df70fd
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2022-08-30 11:25:49 +0200

    index: initialize stub elements type only on first stub index read/write IDEA-300868
    GitOrigin-RevId: db7d9600b6d8bfa284754676f35567fa36b4b25f

--- platform/indexing-impl/src/com/intellij/psi/stubs/SerializedStubTreeDataExternalizer.java
+++ platform/indexing-impl/src/com/intellij/psi/stubs/SerializedStubTreeDataExternalizer.java
@@ -30,17 +30,17 @@ public class SerializedStubTreeDataExternalizer implements DataExternalizer<Seri
   @NotNull
   @Override
   public final SerializedStubTree read(@NotNull final DataInput in) throws IOException {
-    {
-      int serializedStubsLength = DataInputOutputUtil.readINT(in);
-      byte[] bytes = new byte[serializedStubsLength];
-      in.readFully(bytes);
-      int indexedStubByteLength;
-      byte[] indexedStubBytes;
-      indexedStubByteLength = DataInputOutputUtil.readINT(in);
-      indexedStubBytes = new byte[indexedStubByteLength];
-      in.readFully(indexedStubBytes);
-      return new SerializedStubTree(bytes, bytes.length, indexedStubBytes, indexedStubByteLength,
-                                    null, myStubIndexesExternalizer, mySerializationManager);
-    }
+    int serializedStubsLength = DataInputOutputUtil.readINT(in);
+    byte[] bytes = new byte[serializedStubsLength];
+    in.readFully(bytes);
+    int indexedStubByteLength;
+    byte[] indexedStubBytes;
+    indexedStubByteLength = DataInputOutputUtil.readINT(in);
+    indexedStubBytes = new byte[indexedStubByteLength];
+    in.readFully(indexedStubBytes);
+    return new SerializedStubTree(bytes, bytes.length, indexedStubBytes, indexedStubByteLength,
+                                  null, myStubIndexesExternalizer, mySerializationManager);
   }
+
+
 }
