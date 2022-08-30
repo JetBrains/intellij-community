aa29b0390be45cf80017ec67ae14e38c7efc09da
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-08-30 14:24:32 +0200

    [kotlin][move] ApplicableMoveHandlersTest: add more tests
    ^KTIJ-502
    IJ-CR-94896
    
    (cherry picked from commit b73580b32316863d0913b98173d06e64fba41744)
    
    GitOrigin-RevId: 12fb55bc59350360b65537b206d5612deb072de3

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/refactoring/move/ApplicableMoveHandlersTest.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/refactoring/move/ApplicableMoveHandlersTest.kt
@@ -3,7 +3,11 @@ package org.jetbrains.kotlin.idea.refactoring.move
 
 import com.intellij.psi.PsiElement
 import com.intellij.refactoring.move.MoveHandlerDelegate
+import org.jetbrains.kotlin.idea.core.getPackage
 import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtNamedFunction
 
 class ApplicableMoveHandlersTest : KotlinLightCodeInsightFixtureTestCase() {
     fun testFile() {
@@ -40,6 +44,100 @@ class ApplicableMoveHandlersTest : KotlinLightCodeInsightFixtureTestCase() {
         )
     }
 
+    fun testClass() {
+        val kotlinFile = myFixture.addFileToProject(
+            "a/kotlinFile.kt",
+            """
+                package a
+                class One
+                class Two
+            """.trimIndent(),
+        ) as KtFile
+
+        val anotherFile = myFixture.addFileToProject("b/anotherFile.kt", "package b")
+        val targetDirectory = anotherFile.containingDirectory
+        val kotlinClassAsElements: Array<PsiElement> = arrayOf(kotlinFile.declarations.first() as KtClass)
+        val declarationAndFilesHandlers = listOf(
+            "MoveKotlinDeclarationsHandler" to null,
+            "KotlinMoveFilesOrDirectoriesHandler" to "Move Files…",
+        )
+
+        // without context
+        assertApplicableHandlers(
+            sourceElements = kotlinClassAsElements,
+            targetElement = null,
+            expectedHandlers = declarationAndFilesHandlers,
+        )
+
+        // to directory
+        assertApplicableHandlers(
+            sourceElements = kotlinClassAsElements,
+            targetElement = targetDirectory,
+            expectedHandlers = declarationAndFilesHandlers,
+        )
+
+        // to file
+        assertApplicableHandlers(
+            sourceElements = kotlinClassAsElements,
+            targetElement = anotherFile,
+            expectedHandlers = listOf("MoveKotlinDeclarationsHandler" to null),
+        )
+
+        // to package
+        assertApplicableHandlers(
+            sourceElements = kotlinClassAsElements,
+            targetElement = anotherFile.containingDirectory.getPackage()!!,
+            expectedHandlers = declarationAndFilesHandlers,
+        )
+    }
+
+    fun testFunction() {
+        val kotlinFile = myFixture.addFileToProject(
+            "a/kotlinFile.kt",
+            """
+                package a
+                class One
+                class Two
+                fun check() = Unit
+            """.trimIndent(),
+        ) as KtFile
+
+        val anotherFile = myFixture.addFileToProject("b/anotherFile.kt", "package b")
+        val targetDirectory = anotherFile.containingDirectory
+        val kotlinFunctionAsElements: Array<PsiElement> = arrayOf(kotlinFile.declarations.last() as KtNamedFunction)
+        val declarationAndFilesHandlers = listOf(
+            "MoveKotlinDeclarationsHandler" to null,
+        )
+
+        // without context
+        assertApplicableHandlers(
+            sourceElements = kotlinFunctionAsElements,
+            targetElement = null,
+            expectedHandlers = declarationAndFilesHandlers,
+        )
+
+        // to directory
+        assertApplicableHandlers(
+            sourceElements = kotlinFunctionAsElements,
+            targetElement = targetDirectory,
+            expectedHandlers = declarationAndFilesHandlers,
+        )
+
+        // to file
+        assertApplicableHandlers(
+            sourceElements = kotlinFunctionAsElements,
+            targetElement = anotherFile,
+            expectedHandlers = listOf("MoveKotlinDeclarationsHandler" to null),
+        )
+
+        // to package
+        assertApplicableHandlers(
+            sourceElements = kotlinFunctionAsElements,
+            targetElement = anotherFile.containingDirectory.getPackage()!!,
+            expectedHandlers = declarationAndFilesHandlers,
+        )
+    }
+
     private fun assertApplicableHandlers(
         sourceElements: Array<PsiElement>,
         targetElement: PsiElement?,
@@ -49,7 +147,7 @@ class ApplicableMoveHandlersTest : KotlinLightCodeInsightFixtureTestCase() {
             it.canMove(sourceElements, targetElement, null)
         }
 
-        assertEquals(expectedHandlers.map{ it.first }, availableHandlers.map { it::class.simpleName })
+        assertEquals(expectedHandlers.map { it.first }, availableHandlers.map { it::class.simpleName })
         assertEquals(expectedHandlers.map { it.second }, availableHandlers.map { it.getActionName(sourceElements) })
     }
 }
