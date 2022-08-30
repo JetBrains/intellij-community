e17d8d72742f259ab24876e17745066c23a0d125
Yann Cébron <yann.cebron@jetbrains.com>
2022-08-30 09:52:38 +0200

    PluginRepositoryAuthProvider: javadoc cleanup
    GitOrigin-RevId: 20218c68a3f72b8f54cbf532d83c14184fd0eb8a

--- platform/platform-impl/src/com/intellij/ide/plugins/auth/PluginRepositoryAuthProvider.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/auth/PluginRepositoryAuthProvider.java
@@ -8,22 +8,22 @@ import java.util.Collections;
 import java.util.Map;
 
 /**
- * Provides custom authentication headers to be used in http requests to marketplace and custom IJ plugin repositories.
+ * Provides custom authentication headers to be used in HTTP requests to JetBrains Marketplace and custom plugin repositories.
  * <p>
  * Note that authentication flow and login management is not in the scope of this EP.<br>
  * It is the responsibility of the plugin author to make sure that the credentials are up-to-date and correct.<br>
- * <strong>NB!</strong>: If your authentication is stateful(headers may change over time) please notify via
+ * <strong>NB!</strong>: If your authentication is stateful (headers may change over time), please notify via
  * {@link PluginRepositoryAuthListener#notifyAuthChanged()} so that the cached responses that may have failed due to the lack
- * of authentication are invalidated
+ * of authentication are invalidated.
  */
 public interface PluginRepositoryAuthProvider {
 
   ExtensionPointName<PluginRepositoryAuthProvider> EP_NAME = ExtensionPointName.create("com.intellij.pluginRepositoryAuthProvider");
 
   /**
-   * Provide extra authentication headers(e.g. "Authentication: Bearer XXXXXX") based on the URL of the outgoing request.
+   * Provide extra authentication headers (e.g., "Authentication: Bearer XXXXXX") based on the URL of the outgoing request.
    *
-   * @return {@link Collections#emptyMap()} if url should not be handled or no credentials are available
+   * @return {@link Collections#emptyMap()} if URL should not be handled or no credentials are available
    */
   @NotNull Map<String, String> getAuthHeaders(String url);
 
