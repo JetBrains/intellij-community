238f8ef79373b03b3885fdf6e18c9d858e720744
Anna Koehler <anna.marchenko@jetbrains.com>
2022-08-31 15:44:25 +0200

    Integration UI tests: add simple integration UI tests
    GitOrigin-RevId: 595d2b24f2fce222678eb0fa3dabbdacaef199ad

--- platform/testFramework/ui/testSrc/com/intellij/uiTests/e2e/CreateNewProjectTest.kt
+++ platform/testFramework/ui/testSrc/com/intellij/uiTests/e2e/CreateNewProjectTest.kt
@@ -1,17 +1,34 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.uiTests.e2e
 
+import com.intellij.ide.starter.extended.engine.DevBuildServerParams
+import com.intellij.ide.starter.extended.engine.junit4.JUnit4MixedModeRunner
+import com.intellij.ide.starter.ide.IdeProductProvider
 import com.intellij.remoterobot.fixtures.CommonContainerFixture
 import com.intellij.remoterobot.fixtures.ContainerFixture
 import com.intellij.remoterobot.search.locators.byXpath
 import com.intellij.remoterobot.stepsProcessing.step
 import com.intellij.remoterobot.utils.waitFor
 import com.intellij.uiTests.e2e.fixtures.idea
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 import java.time.Duration
 import java.time.Duration.ofSeconds
-
+@RunWith(JUnit4MixedModeRunner::class)
 internal class CreateNewProjectTest : UITest() {
+  companion object {
+    init {
+      DevBuildServerParams.setPlatformPrefixForBuildOnDevBuildServer(IdeProductProvider.IU)
+      DevBuildServerParams.setAdditionalModulesForBuildOnDevBuildServer("intellij.platform.testFramework.ui")
+    }
+  }
+
+  @Before
+  fun runIde() {
+
+  }
 
   @Test
   fun createNewProject() {
@@ -38,5 +55,10 @@ internal class CreateNewProjectTest : UITest() {
     step("Check the message") {
       waitFor(Duration.ofMinutes(1)) { robot.find<ContainerFixture>(consoleLocator).hasText("Hello world!") }
     }
+  }
+
+  @After
+  fun killIde() {
+
   }
 }
