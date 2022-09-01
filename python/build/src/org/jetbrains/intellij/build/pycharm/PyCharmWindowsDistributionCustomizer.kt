63d9c1455ce817ec35994f233435e8cbbc28f78f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-09-01 13:54:27 +0200

    convert PyCharmWindowsDistributionCustomizer to kotlin
    GitOrigin-RevId: c04f3db895c79494c2ad9d445c49df5b8b9734c0

--- python/build/src/org/jetbrains/intellij/build/pycharm/PyCharmWindowsDistributionCustomizer.kt
+++ python/build/src/org/jetbrains/intellij/build/pycharm/PyCharmWindowsDistributionCustomizer.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.intellij.build.pycharm
 
 import org.jetbrains.intellij.build.ApplicationInfoProperties
@@ -7,8 +7,8 @@ import org.jetbrains.intellij.build.WindowsDistributionCustomizer
 
 class PyCharmWindowsDistributionCustomizer extends WindowsDistributionCustomizer {
   @Override
-  void copyAdditionalFiles(BuildContext context, String targetDirectory) {
-    super.copyAdditionalFiles(context, targetDirectory)
+  void copyAdditionalFilesBlocking(BuildContext context, String targetDirectory) {
+    super.copyAdditionalFilesBlocking(context, targetDirectory)
     PyCharmBuildUtils.copySkeletons(context, targetDirectory, "skeletons-win*.zip")
   }
 
