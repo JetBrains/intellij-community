0d3e97ba5fde90483ad8c7fc1d0a7ebb102d3893
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2022-09-02 10:55:00 +0200

    [tests] extract test methods which use compiler output from DirectoryIndexTest to a separate class
    ...located in intellij.java.tests module. Implementation of CompilerProjectExtension is located in Java plugin, so this change is needed to allow running DirectoryIndexTest in classpath of its own module.
    
    GitOrigin-RevId: 2072f4dd6d7ca88c8ec7bddb8c757cc57d5cc1f3

--- platform/platform-tests/testSrc/com/intellij/openapi/roots/impl/DirectoryIndexTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/roots/impl/DirectoryIndexTest.java
@@ -11,10 +11,7 @@ import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.module.ModuleTypeId;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.RootsChangeRescanningInfo;
 import com.intellij.openapi.roots.*;
-import com.intellij.openapi.roots.ex.ProjectRootManagerEx;
-import com.intellij.openapi.util.EmptyRunnable;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.*;
 import com.intellij.testFramework.ExtensionTestUtil;
@@ -48,8 +45,6 @@ public class DirectoryIndexTest extends DirectoryIndexTestCase {
   private VirtualFile myLibAdditionalDir, myLibAdditionalSrcDir, myLibAdditionalSrcFile, myLibAdditionalExcludedDir, myLibAdditionalClsDir, myLibAdditionalClsFile;
   private VirtualFile myCvsDir;
   private VirtualFile myExcludeDir;
-  private VirtualFile myOutputDir;
-  private VirtualFile myModule1OutputDir;
   private VirtualFile myResDir, myTestResDir;
   private SourceFolder myResDirFolder, myTestResDirFolder;
   private VirtualFile myExcludedLibSrcDir, myExcludedLibClsDir;
@@ -131,10 +126,6 @@ public class DirectoryIndexTest extends DirectoryIndexTestCase {
 
       myModule3Dir = createChildDirectory(myRootVFile, "module3");
 
-      myOutputDir = createChildDirectory(myRootVFile, "out");
-      myModule1OutputDir = createChildDirectory(myOutputDir, "module1");
-
-      getCompilerProjectExtension().setCompilerOutputUrl(myOutputDir.getUrl());
 
       // fill roots of module1
       {
@@ -197,12 +188,6 @@ public class DirectoryIndexTest extends DirectoryIndexTestCase {
     myModule2 = null;
     myModule3 = null;
     super.tearDown();
-  }
-
-  private CompilerProjectExtension getCompilerProjectExtension() {
-    final CompilerProjectExtension instance = CompilerProjectExtension.getInstance(myProject);
-    assertNotNull(instance);
-    return instance;
   }
 
   public void testDirInfos() {
@@ -443,30 +428,6 @@ public class DirectoryIndexTest extends DirectoryIndexTestCase {
     checkInfo(mySrcDir2, myModule2, false, false, "", mySrcDir2Folder, JavaSourceRootType.SOURCE, myModule2, myModule3);
   }
 
-  public void testResettingProjectOutputPath() {
-    VirtualFile output1 = createChildDirectory(myModule1Dir, "output1");
-    VirtualFile output2 = createChildDirectory(myModule1Dir, "output2");
-
-    assertInProject(output1);
-    assertInProject(output2);
-
-    getCompilerProjectExtension().setCompilerOutputUrl(output1.getUrl());
-    fireRootsChanged();
-
-    assertExcluded(output1, myModule);
-    assertInProject(output2);
-
-    getCompilerProjectExtension().setCompilerOutputUrl(output2.getUrl());
-    fireRootsChanged();
-
-    assertInProject(output1);
-    assertExcluded(output2, myModule);
-  }
-
-  private void fireRootsChanged() {
-    ApplicationManager.getApplication().runWriteAction(() -> ProjectRootManagerEx.getInstanceEx(getProject()).
-      makeRootsChange(EmptyRunnable.getInstance(), RootsChangeRescanningInfo.NO_RESCAN_NEEDED));
-  }
 
   private static OrderEntry[] toArray(Collection<OrderEntry> orderEntries) {
     return orderEntries.toArray(OrderEntry.EMPTY_ARRAY);
@@ -520,46 +481,19 @@ public class DirectoryIndexTest extends DirectoryIndexTestCase {
     checkInfo(myPack1Dir, myModule2, false, false, null, null, null);
   }
 
-  public void testSameSourceAndOutput() {
-    PsiTestUtil.setCompilerOutputPath(myModule, mySrcDir1.getUrl(), false);
-    assertExcluded(mySrcDir1, myModule);
-  }
-
   public void testExcludedDirShouldBeExcludedRightAfterItsCreation() {
     VirtualFile excluded = createChildDirectory(myModule1Dir, "excluded");
-    VirtualFile projectOutput = createChildDirectory(myModule1Dir, "projectOutput");
-    VirtualFile module2Output = createChildDirectory(myModule1Dir, "module2Output");
-    VirtualFile module2TestOutput = createChildDirectory(myModule2Dir, "module2TestOutput");
-
     assertInProject(excluded);
-    assertInProject(projectOutput);
-    assertInProject(module2Output);
-    assertInProject(module2TestOutput);
-
-    getCompilerProjectExtension().setCompilerOutputUrl(projectOutput.getUrl());
-
     PsiTestUtil.addExcludedRoot(myModule, excluded);
-    PsiTestUtil.setCompilerOutputPath(myModule2, module2Output.getUrl(), false);
-    PsiTestUtil.setCompilerOutputPath(myModule2, module2TestOutput.getUrl(), true);
-    PsiTestUtil.setExcludeCompileOutput(myModule2, true);
-
     assertExcluded(excluded, myModule);
-    assertExcluded(projectOutput, myModule);
-    assertExcluded(module2Output, myModule);
-    assertExcluded(module2TestOutput, myModule2);
 
     VfsTestUtil.deleteFile(excluded);
-    VfsTestUtil.deleteFile(projectOutput);
-    VfsTestUtil.deleteFile(module2Output);
-    VfsTestUtil.deleteFile(module2TestOutput);
-
     final List<VirtualFile> created = new ArrayList<>();
     VirtualFileListener l = new VirtualFileListener() {
       @Override
       public void fileCreated(@NotNull VirtualFileEvent e) {
         VirtualFile file = e.getFile();
-        String fileName = e.getFileName();
-        assertExcluded(file, fileName.contains("module2TestOutput") ? myModule2 : myModule);
+        assertExcluded(file, myModule);
         created.add(file);
       }
     };
@@ -567,17 +501,7 @@ public class DirectoryIndexTest extends DirectoryIndexTestCase {
 
     excluded = createChildDirectory(myModule1Dir, excluded.getName());
     assertExcluded(excluded, myModule);
-
-    projectOutput = createChildDirectory(myModule1Dir, projectOutput.getName());
-    assertExcluded(projectOutput, myModule);
-
-    module2Output = createChildDirectory(myModule1Dir, module2Output.getName());
-    assertExcluded(module2Output, myModule);
-
-    module2TestOutput = createChildDirectory(myModule2Dir, module2TestOutput.getName());
-    assertExcluded(module2TestOutput, myModule2);
-
-    assertEquals(created.toString(), 4, created.size());
+    assertEquals(created.toString(), 1, created.size());
   }
 
   public void testExcludesShouldBeRecognizedRightOnRefresh() {
@@ -665,48 +589,6 @@ public class DirectoryIndexTest extends DirectoryIndexTestCase {
 
     checkPackage("exc", false);
     checkPackage("exc", true);
-  }
-
-  public void testExcludeCompilerOutputOutsideOfContentRoot() {
-    assertTrue(myFileIndex.isExcluded(myOutputDir));
-    assertFalse(myFileIndex.isUnderIgnored(myOutputDir));
-    assertTrue(myFileIndex.isExcluded(myModule1OutputDir));
-    assertFalse(myFileIndex.isExcluded(myOutputDir.getParent()));
-    assertExcludedFromProject(myOutputDir);
-    assertExcludedFromProject(myModule1OutputDir);
-    String moduleOutputUrl = myModule1OutputDir.getUrl();
-
-    VfsTestUtil.deleteFile(myOutputDir);
-
-    PsiTestUtil.setCompilerOutputPath(myModule, moduleOutputUrl, false);
-    myOutputDir = createChildDirectory(myRootVFile, "out");
-    myModule1OutputDir = createChildDirectory(myOutputDir, "module1");
-
-    assertExcludedFromProject(myOutputDir);
-    assertExcludedFromProject(myModule1OutputDir);
-    assertTrue(myFileIndex.isExcluded(myModule1OutputDir));
-
-    PsiTestUtil.setCompilerOutputPath(myModule, moduleOutputUrl, true);
-    PsiTestUtil.setCompilerOutputPath(myModule2, moduleOutputUrl, false);
-    PsiTestUtil.setCompilerOutputPath(myModule2, moduleOutputUrl, true);
-    PsiTestUtil.setCompilerOutputPath(myModule3, moduleOutputUrl, false);
-    PsiTestUtil.setCompilerOutputPath(myModule3, moduleOutputUrl, true);
-
-    // now no module inherits project output dir, but it still should be project-excluded
-    assertExcludedFromProject(myOutputDir);
-
-    // project output inside module content shouldn't be projectExcludeRoot
-    VirtualFile projectOutputUnderContent = createChildDirectory(myModule1Dir, "projectOutputUnderContent");
-    getCompilerProjectExtension().setCompilerOutputUrl(projectOutputUnderContent.getUrl());
-    fireRootsChanged();
-
-    assertNotExcluded(myOutputDir);
-    assertExcluded(projectOutputUnderContent, myModule);
-
-    VfsTestUtil.deleteFile(projectOutputUnderContent);
-    projectOutputUnderContent = createChildDirectory(myModule1Dir, "projectOutputUnderContent");
-    assertNotExcluded(myOutputDir);
-    assertExcluded(projectOutputUnderContent, myModule);
   }
 
   public void testFileContentAndSourceRoots() {
