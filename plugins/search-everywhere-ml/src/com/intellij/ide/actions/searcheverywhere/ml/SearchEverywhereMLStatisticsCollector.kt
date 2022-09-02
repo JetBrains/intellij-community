670b4821d128ecb891c5fe98fb4ca01143d59803
fatimazahra.elakakry <fatimazahra.elakkary@jetbrains.com>
2022-09-02 13:35:00 +0200

    ML in SE: Update Actions' models
    (cherry picked from commit 41ae25de9ac849b425eedc63cebadd97985c157b)
    
    IJ-CR-95031
    
    GitOrigin-RevId: bdcc5c7a5f86c5343f85056436cb429368968893

--- plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/SearchEverywhereMLStatisticsCollector.kt
+++ plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/SearchEverywhereMLStatisticsCollector.kt
@@ -253,7 +253,7 @@ internal class SearchEverywhereMLStatisticsCollector : CounterUsagesCollector() 
   }
 
   companion object {
-    private val GROUP = EventLogGroup("mlse.log", 35, RECORDER_CODE)
+    private val GROUP = EventLogGroup("mlse.log", 36, RECORDER_CODE)
     private val EMPTY_ARRAY = IntArray(0)
     private const val REPORTED_ITEMS_LIMIT = 100
 
