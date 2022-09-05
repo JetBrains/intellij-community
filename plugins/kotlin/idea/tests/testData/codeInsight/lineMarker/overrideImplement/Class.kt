c8ca4cac72a7ae2846df8a7b5cdee653ac46afb1
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2022-09-05 12:39:28 +0200

    [kotlin] Clean up
    #KTIJ-22674
    
    GitOrigin-RevId: 7bd0d4ffe8e4e1f17a6e4af5376f45042dba2b16

--- plugins/kotlin/idea/tests/testData/codeInsight/lineMarker/overrideImplement/Class.kt
+++ plugins/kotlin/idea/tests/testData/codeInsight/lineMarker/overrideImplement/Class.kt
@@ -1,9 +1,9 @@
-open class <lineMarker>A</lineMarker> {
-  open fun <lineMarker>a</lineMarker>(){
+open class <lineMarker descr="Is subclassed by B  Click or press ... to navigate">A</lineMarker> {
+  open fun <lineMarker descr="Is overridden in B">a</lineMarker>(){
   }
 }
 
 class B:A(){
-  override fun <lineMarker>a</lineMarker>(){
+  override fun <lineMarker descr="Overrides function in 'A'">a</lineMarker>(){
   }
 }
