812756b150b6882400db04686f4b090692373db0
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2022-09-07 07:27:59 -0400

    [ide starter] IDEA-299870  IJ-CR-95179 renaming env variable to IS_STARTER_PERFORMANCE_TEST
    GitOrigin-RevId: 7bc0c09dd1e7449ce5b7aef4aec4d2568680de7c

--- tools/ideTestingFramework/intellij.tools.ide.starter/src/com/intellij/ide/starter/Const.kt
+++ tools/ideTestingFramework/intellij.tools.ide.starter/src/com/intellij/ide/starter/Const.kt
@@ -1,5 +1,5 @@
 package com.intellij.ide.starter
 
-const val IS_STARTER_PERFORMANCE_TEST_ENV = "intellij.is.starter.performance.test"
+const val IS_STARTER_PERFORMANCE_TEST_ENV = "IS_STARTER_PERFORMANCE_TEST"
 
 val isStarterPerformanceTest = System.getenv(IS_STARTER_PERFORMANCE_TEST_ENV).toBoolean()
