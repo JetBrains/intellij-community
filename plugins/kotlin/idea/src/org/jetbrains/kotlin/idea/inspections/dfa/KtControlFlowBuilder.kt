b015d1b3ec206ef266ef0bf4e2bbbf3c6d01e8eb
Tagir Valeev <tagir.valeev@jetbrains.com>
2022-09-07 16:15:06 +0200

    [kotlin-dfa] KTIJ-22546 Constant conditions: False positive "range is always empty" when iterating over an iterable enum member
    GitOrigin-RevId: f7754d24dedb014b7ee28c811805a7a6e56e456d

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/dfa/KtControlFlowBuilder.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/dfa/KtControlFlowBuilder.kt
@@ -41,7 +41,6 @@ import org.jetbrains.kotlin.contracts.description.EventOccurrencesRange
 import org.jetbrains.kotlin.descriptors.*
 import org.jetbrains.kotlin.idea.caches.resolve.resolveToCall
 import org.jetbrains.kotlin.idea.caches.resolve.safeAnalyzeNonSourceRootCode
-import org.jetbrains.kotlin.idea.util.RangeKtExpressionType.*
 import org.jetbrains.kotlin.idea.core.resolveType
 import org.jetbrains.kotlin.idea.inspections.dfa.KotlinAnchor.*
 import org.jetbrains.kotlin.idea.inspections.dfa.KotlinProblem.*
@@ -49,6 +48,7 @@ import org.jetbrains.kotlin.idea.intentions.loopToCallChain.targetLoop
 import org.jetbrains.kotlin.idea.project.builtIns
 import org.jetbrains.kotlin.idea.refactoring.move.moveMethod.type
 import org.jetbrains.kotlin.idea.references.mainReference
+import org.jetbrains.kotlin.idea.util.RangeKtExpressionType.*
 import org.jetbrains.kotlin.idea.util.getRangeBinaryExpressionType
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.*
@@ -986,7 +986,7 @@ class KtControlFlowBuilder(val factory: DfaValueFactory, val context: KtExpressi
         if (range != null) {
             val kotlinType = range.getKotlinType()
             val lengthField = findSpecialField(kotlinType)
-            if (lengthField != null) {
+            if (lengthField != null && lengthField != SpecialField.ENUM_ORDINAL) {
                 val collectionVar = flow.createTempVariable(kotlinType.toDfType())
                 addInstruction(JvmAssignmentInstruction(null, collectionVar))
                 addInstruction(PopInstruction())
