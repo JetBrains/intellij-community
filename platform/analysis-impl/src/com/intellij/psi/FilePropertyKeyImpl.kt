d416a7ea49369dc4a6b58fa1a6364b8c3b4cfbcb
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2022-09-09 18:04:04 +0300

    IJP-1342: Introduce FilePropertyKey
    GitOrigin-RevId: 47885fdbcf898e503e3e75a10ede5c60a8d4639c

---
+++ platform/analysis-impl/src/com/intellij/psi/FilePropertyKeyImpl.kt
@@ -0,0 +1,169 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.psi
+
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileWithId
+import com.intellij.openapi.vfs.newvfs.AttributeInputStream
+import com.intellij.openapi.vfs.newvfs.AttributeOutputStream
+import com.intellij.openapi.vfs.newvfs.FileAttribute
+import com.intellij.util.io.DataInputOutputUtil
+import org.jetbrains.annotations.Contract
+import org.jetbrains.annotations.VisibleForTesting
+import java.io.IOException
+import java.util.function.Function
+
+abstract class FilePropertyKeyImpl<T, RAW> protected constructor(name: String,
+                                                                 private val persistentAttribute: FileAttribute) : FilePropertyKey<T> {
+  @get:VisibleForTesting
+  val userDataKey: Key<Any?> = Key.create(name)
+
+  @Contract("null -> null")
+  override fun getPersistentValue(virtualFile: VirtualFile?): T? {
+    if (virtualFile == null) return null
+    val raw = getRaw(virtualFile)
+    return raw?.let { fromRaw(it) }
+  }
+
+  private fun getRaw(virtualFile: VirtualFile): RAW? {
+    @Suppress("UNCHECKED_CAST")
+    val memValue = userDataKey[virtualFile] as RAW?
+    if (memValue != null) {
+      return if (memValue === NULL_MARKER) null else memValue
+    }
+    val persisted = readValue(virtualFile)
+    userDataKey[virtualFile] = persisted ?: NULL_MARKER
+    return persisted
+  }
+
+  override fun setPersistentValue(virtualFile: VirtualFile?, newValue: T?): Boolean {
+    if (virtualFile == null) return false
+    val oldValue = getRaw(virtualFile)
+    val rawNewValue = newValue?.let { toRaw(it) }
+    if (keysEqual(oldValue, rawNewValue)) {
+      return false
+    }
+    else {
+      writeValue(virtualFile, rawNewValue)
+      userDataKey[virtualFile] = if (newValue == null) NULL_MARKER else rawNewValue
+      return true
+    }
+  }
+
+  protected fun keysEqual(k1: RAW?, k2: RAW?): Boolean = (k1 == k2)
+
+  protected fun readValue(virtualFile: VirtualFile): RAW? {
+    if (virtualFile !is VirtualFileWithId) {
+      LOG.debug("Only VirtualFileWithId can have persistent attributes: $virtualFile")
+      return null
+    }
+    try {
+      persistentAttribute.readFileAttribute(virtualFile).use { stream ->
+        if (stream != null && stream.available() > 0) {
+          return readValue(stream)
+        }
+      }
+    }
+    catch (e: IOException) {
+      LOG.error(e)
+    }
+    return null
+  }
+
+  protected fun writeValue(virtualFile: VirtualFile, newValue: RAW?) {
+    if (virtualFile !is VirtualFileWithId) {
+      LOG.debug("Only VirtualFileWithId can have persistent attributes: $virtualFile")
+      return
+    }
+    try {
+      persistentAttribute.writeFileAttribute(virtualFile).use { stream -> writeValue(stream, newValue) }
+    }
+    catch (e: IOException) {
+      LOG.error(e)
+    }
+  }
+
+  @Throws(IOException::class)
+  protected abstract fun readValue(stream: AttributeInputStream): RAW?
+
+  @Throws(IOException::class)
+  protected abstract fun writeValue(stream: AttributeOutputStream, newValue: RAW?)
+
+  /*
+   * Note that we can write something meaningful into persistent attribute (e.g. LanguageID="GenericSQL"), then user may remove the plugin,
+   * and stored value may become meaningless. In this case it is OK to transform non-null into null value
+   */
+  protected abstract fun fromRaw(value: RAW): T?
+  protected abstract fun toRaw(value: T): RAW
+
+  companion object {
+    @JvmStatic
+    private val NULL_MARKER = Object()
+
+    @JvmStatic
+    private val LOG = Logger.getInstance(FilePropertyKeyImpl::class.java)
+
+    @JvmStatic
+    fun createPersistentStringKey(name: String, persistentAttribute: FileAttribute): FilePropertyKey<String> {
+      return createPersistentStringKey(name, persistentAttribute, String::toString, String::toString)
+    }
+
+    @JvmStatic
+    fun <T> createPersistentStringKey(name: String,
+                                      persistentAttribute: FileAttribute,
+                                      fnToRaw: Function<T, String>,
+                                      fnFromRaw: Function<String, T?>): FilePropertyKey<T> {
+      return FilePropertyStringKey(name, persistentAttribute, fnToRaw, fnFromRaw)
+    }
+
+    @JvmStatic
+    fun <T : Enum<T>> createPersistentEnumKey(userDataName: String,
+                                              persistentDataName: String,
+                                              persistentDataVersion: Int,
+                                              clazz: Class<T>): FilePropertyKey<T> {
+      return FilePropertyEnumKey(userDataName, FileAttribute(persistentDataName, persistentDataVersion, true), clazz)
+    }
+  }
+}
+
+internal class FilePropertyStringKey<T>(name: String, persistentAttribute: FileAttribute,
+                                        private val fnToRaw: Function<T, String>,
+                                        private val fnFromRaw: Function<String, T?>) : FilePropertyKeyImpl<T, String>(name,
+                                                                                                                      persistentAttribute) {
+  override fun fromRaw(value: String): T? = fnFromRaw.apply(value)
+
+  override fun toRaw(value: T): String = fnToRaw.apply(value)
+
+  @Throws(IOException::class)
+  override fun readValue(stream: AttributeInputStream): String? = stream.readEnumeratedString()
+
+  @Throws(IOException::class)
+  override fun writeValue(stream: AttributeOutputStream, newValue: String?) {
+    StringUtil.nullize(newValue).let { stream.writeEnumeratedString(it) }
+  }
+}
+
+internal class FilePropertyEnumKey<T : Enum<T>>(name: String,
+                                                persistentAttribute: FileAttribute,
+                                                private val clazz: Class<T>) : FilePropertyKeyImpl<T, Int>(name, persistentAttribute) {
+  @Throws(IOException::class)
+  override fun readValue(stream: AttributeInputStream): Int = DataInputOutputUtil.readINT(stream)
+
+  @Throws(IOException::class)
+  override fun writeValue(stream: AttributeOutputStream, newValue: Int?) {
+    newValue?.let { DataInputOutputUtil.writeINT(stream, it) }
+  }
+
+  override fun fromRaw(value: Int): T? {
+    if (value >= 0 && value < clazz.enumConstants.size) {
+      return clazz.enumConstants[value]
+    }
+    else {
+      return null
+    }
+  }
+
+  override fun toRaw(value: T): Int = value.ordinal
+}
---
+++ platform/platform-tests/testSrc/com/intellij/psi/FilePropertyKeyImplTest.java
@@ -0,0 +1,109 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.psi;
+
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileWithId;
+import com.intellij.openapi.vfs.newvfs.FileAttribute;
+import com.intellij.testFramework.LightPlatformTestCase;
+import com.intellij.testFramework.LightVirtualFile;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class FilePropertyKeyImplTest<T> extends LightPlatformTestCase {
+  private enum TestEnum {
+    ONE, TWO
+  }
+
+  private static final FilePropertyKey<String> STRING_KEY =
+    FilePropertyKeyImpl.createPersistentStringKey("test_string_attr", new FileAttribute("string_key", 1, true));
+  private static final FilePropertyKey<TestEnum> ENUM_KEY =
+    FilePropertyKeyImpl.createPersistentEnumKey("test_enum_attr", "test_enum_attr", 1, TestEnum.class);
+
+  @Parameter(0)
+  public FilePropertyKey<T> key;
+  @Parameter(1)
+  public T sample1;
+  @Parameter(2)
+  public T sample2;
+
+  private Key<?> memKey;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    memKey = ((FilePropertyKeyImpl<T, ?>)key).getUserDataKey();
+  }
+
+  @Test
+  public void testLightVirtualFile() {
+    LightVirtualFile file = new LightVirtualFile();
+    TestCase.assertFalse("Precondition failed", file instanceof VirtualFileWithId);
+
+    TestCase.assertTrue(key.setPersistentValue(file, sample1));
+    TestCase.assertEquals(sample1, key.getPersistentValue(file));
+    TestCase.assertTrue(key.setPersistentValue(file, null));
+    TestCase.assertNull(key.getPersistentValue(file));
+  }
+
+  @Test
+  public void testReadFromFile() {
+    VirtualFile file = createFile("Foo.java", "").getVirtualFile();
+    TestCase.assertTrue("Write sample1 to file", key.setPersistentValue(file, sample1));
+    TestCase.assertEquals(sample1, key.getPersistentValue(file));
+
+    memKey.set(file, null); // clear memory data
+    TestCase.assertEquals("Should read previous value from file", sample1, key.getPersistentValue(file));
+
+    TestCase.assertTrue("Write null to file", key.setPersistentValue(file, null));
+    TestCase.assertNull(key.getPersistentValue(file));
+    memKey.set(file, null); // clear memory data
+    TestCase.assertNull("Should read null from file", key.getPersistentValue(file));
+  }
+
+  @Test
+  public void testGetFromEmpty() {
+    VirtualFile file = createFile("Foo.java", "").getVirtualFile();
+    TestCase.assertNull("No previous value in vfs, should read null", key.getPersistentValue(file));
+    TestCase.assertNull("Second read should also return null", key.getPersistentValue(file));
+  }
+
+  @Test
+  public void testSetGetTwoSamples() {
+    VirtualFile file = createFile("Foo.java", "").getVirtualFile();
+    TestCase.assertTrue("First set should change null to sample1", key.setPersistentValue(file, sample1));
+    TestCase.assertFalse("Second set should not change existing value", key.setPersistentValue(file, sample1));
+    TestCase.assertEquals(sample1, key.getPersistentValue(file));
+
+    TestCase.assertTrue("First set should change sample1 to sample2", key.setPersistentValue(file, sample2));
+    TestCase.assertFalse("Second set should not change existing value", key.setPersistentValue(file, sample2));
+    TestCase.assertEquals(sample2, key.getPersistentValue(file));
+  }
+
+  @Test
+  public void testSetGetNulls() {
+    VirtualFile file = createFile("Foo.java", "").getVirtualFile();
+    TestCase.assertNull("Initial value is null", key.getPersistentValue(file));
+    TestCase.assertTrue("Write something not-null", key.setPersistentValue(file, sample1));
+    TestCase.assertEquals(sample1, key.getPersistentValue(file));
+
+    TestCase.assertTrue("Write null, should change previous value", key.setPersistentValue(file, null));
+    TestCase.assertFalse("Second set should not change existing value", key.setPersistentValue(file, null));
+    TestCase.assertNull("We still can read null", key.getPersistentValue(file));
+  }
+
+  @Parameterized.Parameters(name = "{0}")
+  public static Collection samples() {
+    return Arrays.asList(new Object[][]{
+      {STRING_KEY, "sample1", "sample2"},
+      {ENUM_KEY, TestEnum.ONE, TestEnum.TWO}
+    });
+  }
+}
