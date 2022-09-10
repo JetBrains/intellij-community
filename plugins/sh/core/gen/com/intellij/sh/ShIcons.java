a98897a49e33d58c06dee6d6b07f98316cc9c145
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2022-09-10 14:53:31 +0400

    [intellij sh] Regenerate icons
    GitOrigin-RevId: e71ac77e930f8095f53600b2b7b4ebc1e804aad2

--- plugins/sh/core/gen/com/intellij/sh/ShIcons.java
+++ plugins/sh/core/gen/com/intellij/sh/ShIcons.java
@@ -1,4 +1,3 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.sh;
 
 import com.intellij.ui.IconManager;
@@ -10,9 +9,9 @@ import javax.swing.*;
  * NOTE THIS FILE IS AUTO-GENERATED
  * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
  */
-public final class SHIcons {
+public final class ShIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
-    return IconManager.getInstance().loadRasterizedIcon(path, SHIcons.class.getClassLoader(), cacheKey, flags);
+    return IconManager.getInstance().loadRasterizedIcon(path, ShIcons.class.getClassLoader(), cacheKey, flags);
   }
   /** 16x16 */ public static final @NotNull Icon ShFile = load("icons/shFile.svg", 26768022, 0);
 }
