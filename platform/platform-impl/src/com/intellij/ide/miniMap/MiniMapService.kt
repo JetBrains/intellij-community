a85104625a20033da69612aec1ab7c927099c25d
Nikita Pavlenko <Nikita.Pavlenko@jetbrains.com>
2022-09-12 15:38:31 +0200

    [bdt] BDIDE-3370 MiniMap (fix): MiniMap will not be shown if disabled.
    GitOrigin-RevId: 9a618cf707276455d464f875eae4b4d535579326

--- platform/platform-impl/src/com/intellij/ide/miniMap/MiniMapService.kt
+++ platform/platform-impl/src/com/intellij/ide/miniMap/MiniMapService.kt
@@ -56,6 +56,9 @@ class MiniMapService : Disposable {
   }
 
   fun editorOpened(editor: Editor) {
+    if (!settings.state.enabled) {
+      return
+    }
     getEditorImpl(editor)?.let { addMiniMap(it) }
   }
 
