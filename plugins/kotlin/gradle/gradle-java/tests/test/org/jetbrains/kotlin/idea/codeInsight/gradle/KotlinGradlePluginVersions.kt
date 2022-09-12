edf81fa00ea2ee8b9fd5fe85b182091f4ae4ec5a
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-09-12 14:12:48 +0200

    [KTIJ-22747] Update latest tested KGP version to 1.8.0-dev-2843
    GitOrigin-RevId: 862697ad36ab35abedaeff301cc4c566d816aad7

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/KotlinGradlePluginVersions.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/KotlinGradlePluginVersions.kt
@@ -8,7 +8,7 @@ object KotlinGradlePluginVersions {
     val V_1_4_32 = KotlinToolingVersion(1, 4, 32, null)
     val V_1_5_32 = KotlinToolingVersion(1, 5, 32, null)
     val V_1_6_21 = KotlinToolingVersion(1, 6, 21, null)
-    val latest = KotlinToolingVersion("1.8.0-dev-2466")
+    val latest = KotlinToolingVersion("1.8.0-dev-2843")
 
     val all = listOf(
         V_1_4_32,
