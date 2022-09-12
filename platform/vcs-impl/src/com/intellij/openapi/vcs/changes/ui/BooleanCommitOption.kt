604daf9fe80a01c861ffab3222ccabb1f818999b
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-09-12 10:51:46 +0200

    vcs: simplify Settings dialog detection
    GitOrigin-RevId: cc33200e0166ec93f4f5cd596888eb05d1556e05

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/BooleanCommitOption.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/BooleanCommitOption.kt
@@ -2,10 +2,10 @@
 package com.intellij.openapi.vcs.changes.ui
 
 import com.intellij.openapi.options.UnnamedConfigurable
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.CheckinProjectPanel
 import com.intellij.openapi.vcs.VcsBundle
 import com.intellij.openapi.vcs.checkin.CheckinHandlerUtil
-import com.intellij.openapi.vcs.configurable.CommitOptionsConfigurable
 import com.intellij.openapi.vcs.ui.RefreshableOnComponent
 import com.intellij.ui.components.JBCheckBox
 import org.jetbrains.annotations.Nls
@@ -14,7 +14,7 @@ import javax.swing.JComponent
 import kotlin.reflect.KMutableProperty0
 
 open class BooleanCommitOption(
-  private val checkinPanel: CheckinProjectPanel,
+  private val project: Project,
   @Nls text: String,
   private val disableWhenDumb: Boolean,
   private val getter: () -> Boolean,
@@ -22,12 +22,18 @@ open class BooleanCommitOption(
 ) : RefreshableOnComponent,
     UnnamedConfigurable {
 
+  constructor(project: Project, @Nls text: String, disableWhenDumb: Boolean, property: KMutableProperty0<Boolean>) :
+    this(project, text, disableWhenDumb, { property.get() }, Consumer { property.set(it) })
+
+  constructor(panel: CheckinProjectPanel, @Nls text: String, disableWhenDumb: Boolean, getter: () -> Boolean, setter: Consumer<Boolean>) :
+    this(panel.project, text, disableWhenDumb, getter, setter)
+
   constructor(panel: CheckinProjectPanel, @Nls text: String, disableWhenDumb: Boolean, property: KMutableProperty0<Boolean>) :
-    this(panel, text, disableWhenDumb, { property.get() }, Consumer { property.set(it) })
+    this(panel.project, text, disableWhenDumb, { property.get() }, Consumer { property.set(it) })
 
   protected val checkBox = JBCheckBox(text)
 
-  private val isInSettings get() = checkinPanel is CommitOptionsConfigurable.CheckinPanel
+  private var isInSettings = false
 
   override fun saveState() {
     setter.accept(checkBox.isSelected)
@@ -36,14 +42,17 @@ open class BooleanCommitOption(
   override fun restoreState() {
     checkBox.isSelected = getter()
     if (disableWhenDumb && !isInSettings) {
-      CheckinHandlerUtil.disableWhenDumb(checkinPanel.project, checkBox,
+      CheckinHandlerUtil.disableWhenDumb(project, checkBox,
                                          VcsBundle.message("changes.impossible.until.indices.are.up.to.date"))
     }
   }
 
   override fun getComponent(): JComponent = checkBox
 
-  override fun createComponent() = component
+  override fun createComponent(): JComponent {
+    isInSettings = true
+    return component
+  }
 
   override fun isModified() = checkBox.isSelected != getter()
 
