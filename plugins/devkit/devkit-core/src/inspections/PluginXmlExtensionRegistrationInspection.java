47a520efab6cc8022f18bede388b8e00051cfebe
Yann Cébron <yann.cebron@jetbrains.com>
2022-09-13 16:51:45 +0200

    IDEA-297798 DevKit: new inspection to highlight missing "language" declaration in extension point registration
    GitOrigin-RevId: 846e17770878f819afa2ad9f44bcc37790e8c5c8

---
+++ plugins/devkit/devkit-core/src/inspections/PluginXmlExtensionRegistrationInspection.java
@@ -0,0 +1,95 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.idea.devkit.inspections;
+
+import com.intellij.codeInsight.intention.IntentionActionBean;
+import com.intellij.codeInsight.intention.preview.IntentionPreviewUtils;
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.ide.util.PsiNavigationSupport;
+import com.intellij.lang.LanguageExtensionPoint;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiReferenceProviderBean;
+import com.intellij.psi.impl.source.resolve.reference.PsiReferenceContributorEP;
+import com.intellij.psi.util.InheritanceUtil;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.util.xml.DomElement;
+import com.intellij.util.xml.DomUtil;
+import com.intellij.util.xml.GenericDomValue;
+import com.intellij.util.xml.highlighting.DefineAttributeQuickFix;
+import com.intellij.util.xml.highlighting.DomElementAnnotationHolder;
+import com.intellij.util.xml.highlighting.DomHighlightingHelper;
+import com.intellij.util.xml.reflect.DomAttributeChildDescription;
+import com.intellij.util.xml.reflect.DomFixedChildDescription;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.idea.devkit.DevKitBundle;
+import org.jetbrains.idea.devkit.dom.Extension;
+import org.jetbrains.idea.devkit.dom.ExtensionPoint;
+
+public class PluginXmlExtensionRegistrationInspection extends DevKitPluginXmlInspectionBase {
+
+  @Override
+  protected void checkDomElement(DomElement element, DomElementAnnotationHolder holder, DomHighlightingHelper helper) {
+    if (!(element instanceof Extension)) {
+      return;
+    }
+
+    Extension extension = (Extension)element;
+    ExtensionPoint extensionPoint = extension.getExtensionPoint();
+    if (extensionPoint == null) {
+      return;
+    }
+
+    if (isExtensionPointWithLanguage(extensionPoint)) {
+      DomAttributeChildDescription<?> languageAttributeDescription = extension.getGenericInfo().getAttributeChildDescription("language");
+      if (languageAttributeDescription != null) {
+        if (!DomUtil.hasXml(languageAttributeDescription.getDomAttributeValue(extension))) {
+          holder.createProblem(extension,
+                               DevKitBundle.message("inspection.plugin.xml.extension.registration.should.define.language.attribute",
+                                                    extensionPoint.getEffectiveQualifiedName()),
+                               new DefineAttributeQuickFix("language"));
+        }
+        return;
+      }
+
+      // IntentionActionBean, since 223 only
+      DomFixedChildDescription languageTagDescription = extension.getGenericInfo().getFixedChildDescription("language");
+      if (languageTagDescription != null) {
+        GenericDomValue languageTag = getTag(extension, "language");
+        if (languageTag != null && !DomUtil.hasXml(languageTag)) {
+          holder.createProblem(extension,
+                               DevKitBundle.message("inspection.plugin.xml.extension.registration.should.define.language.tag",
+                                                    extensionPoint.getEffectiveQualifiedName()),
+                               new LocalQuickFix() {
+                                 @Override
+                                 public @NotNull String getFamilyName() {
+                                   return DevKitBundle.message("inspection.plugin.xml.extension.registration.should.define.language.tag.family.name");
+                                 }
+
+                                 @Override
+                                 public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+                                   Extension fixExtension = DomUtil.findDomElement(descriptor.getPsiElement(), Extension.class, false);
+                                   if (fixExtension == null) return;
+
+                                   XmlTag xmlTag = fixExtension.getXmlTag();
+                                   XmlTag languageTag = xmlTag.createChildTag("language", null, "", false);
+                                   XmlTag addedLanguageTag = xmlTag.addSubTag(languageTag, true);
+                                   if (!IntentionPreviewUtils.isPreviewElement(addedLanguageTag)) {
+                                     PsiNavigationSupport.getInstance()
+                                       .createNavigatable(project, addedLanguageTag.getContainingFile().getVirtualFile(),
+                                                          addedLanguageTag.getValue().getTextRange().getEndOffset()).navigate(true);
+                                   }
+                                 }
+                               });
+        }
+      }
+    }
+  }
+
+  private static boolean isExtensionPointWithLanguage(ExtensionPoint extensionPoint) {
+    final String extensionBeanClass = extensionPoint.getBeanClass().getStringValue();
+    return PsiReferenceContributorEP.class.getName().equals(extensionBeanClass) ||
+           PsiReferenceProviderBean.class.getName().equals(extensionBeanClass) ||
+           IntentionActionBean.class.getName().equals(extensionBeanClass) ||
+           InheritanceUtil.isInheritor(extensionPoint.getBeanClass().getValue(), LanguageExtensionPoint.class.getName());
+  }
+}
---
+++ plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/inspections/PluginXmlExtensionRegistrationInspectionTest.java
@@ -0,0 +1,54 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.idea.devkit.inspections;
+
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.codeInsight.intention.IntentionActionBean;
+import com.intellij.codeInspection.LocalInspectionEP;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.impl.source.resolve.reference.PsiReferenceContributorEP;
+import com.intellij.testFramework.TestDataPath;
+import com.intellij.testFramework.builders.JavaModuleFixtureBuilder;
+import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase;
+import com.intellij.util.IncorrectOperationException;
+import com.intellij.util.PathUtil;
+import org.jetbrains.idea.devkit.DevkitJavaTestsUtil;
+
+import java.nio.file.Paths;
+
+@TestDataPath("$CONTENT_ROOT/testData/inspections/pluginXmlExtensionRegistration")
+public class PluginXmlExtensionRegistrationInspectionTest extends JavaCodeInsightFixtureTestCase {
+
+  @Override
+  protected String getBasePath() {
+    return DevkitJavaTestsUtil.TESTDATA_PATH + "inspections/pluginXmlExtensionRegistration";
+  }
+
+  @Override
+  protected void tuneFixture(JavaModuleFixtureBuilder moduleBuilder) {
+    moduleBuilder.addLibrary("core", PathUtil.getJarPathForClass(Project.class));
+    moduleBuilder.addLibrary("core-impl", PathUtil.getJarPathForClass(PsiReferenceContributorEP.class));
+    moduleBuilder.addLibrary("analysis-api", PathUtil.getJarPathForClass(IntentionActionBean.class));
+    moduleBuilder.addLibrary("platform-rt", PathUtil.getJarPathForClass(IncorrectOperationException.class));
+    moduleBuilder.addLibrary("platform-resources", Paths.get(PathUtil.getJarPathForClass(LocalInspectionEP.class))
+      .resolveSibling("intellij.platform.resources").toString());
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    myFixture.enableInspections(PluginXmlExtensionRegistrationInspection.class);
+  }
+
+  public void testLanguageExtensions() {
+    myFixture.testHighlighting("languageExtensions.xml");
+  }
+
+  public void testLanguageAddLanguageTagFix() {
+    IntentionAction action =
+      myFixture.getAvailableIntention("Add language tag",
+                                      "languageAddLanguageTagFix.xml");
+    assertNotNull(action);
+    myFixture.checkPreviewAndLaunchAction(action);
+    myFixture.checkResultByFile("languageAddLanguageTagFix_after.xml");
+  }
+}
