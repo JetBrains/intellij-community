f71071a63464278e7b544f3632c51d3048139c1a
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2022-09-13 16:13:21 +0000

    PY-55110: Support Django project creation on targets.
    ``DjangoProjectCreatorAndConfigurator`` uses ``PythonTask`` which wasn't aware of targets and cast additional data to the legacy remote additional data -> CCE.
    
    It now provides limited support for targets API, although we better get rid of it and migrate to the native targets api.
    
    Merge-request: IJ-MR-94386
    Merged-by: Ilya Kazakevich <ilya.kazakevich@jetbrains.com>
    
    GitOrigin-RevId: 624fed3ff5e64133e6baa665a4a63bbbe1e0c825

--- python/src/com/jetbrains/python/run/PyRemoteProcessStarter.java
+++ python/src/com/jetbrains/python/run/PyRemoteProcessStarter.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.run;
 
 import com.google.common.net.HostAndPort;
@@ -9,18 +9,20 @@ import com.intellij.execution.process.ProcessTerminatedListener;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.openapi.projectRoots.SdkAdditionalData;
 import com.intellij.openapi.util.Key;
 import com.jetbrains.python.PyBundle;
 import com.jetbrains.python.remote.PyRemotePathMapper;
-import com.jetbrains.python.remote.PyRemoteSdkAdditionalDataBase;
+import com.jetbrains.python.remote.PyRemoteSdkAdditionalData;
 import com.jetbrains.python.remote.PythonRemoteInterpreterManager;
 import com.jetbrains.python.remote.UnsupportedPythonSdkTypeException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class PyRemoteProcessStarter {
+/**
+ * @deprecated use targets API instead
+ */
+@Deprecated
+public final class PyRemoteProcessStarter {
   public static final Key<Boolean> OPEN_FOR_INCOMING_CONNECTION = Key.create("OPEN_FOR_INCOMING_CONNECTION");
   public static final Key<HostAndPort> WEB_SERVER_HOST_AND_PORT = new Key<>("WEB_SERVER_HOST_AND_PORT");
   /**
@@ -33,19 +35,22 @@ public class PyRemoteProcessStarter {
    */
   public static final Key<Boolean> RUN_AS_AUXILIARY_PROCESS = Key.create("RUN_AS_AUXILIARY_PROCESS");
 
+  private PyRemoteProcessStarter() {
+
+  }
   @NotNull
-  public ProcessHandler startRemoteProcess(@NotNull Sdk sdk,
-                                           @NotNull GeneralCommandLine commandLine,
-                                           @Nullable Project project,
-                                           @Nullable PyRemotePathMapper pathMapper)
+  public static ProcessHandler startLegacyRemoteProcess(@NotNull PyRemoteSdkAdditionalData legacyAdditionalData,
+                                                        @NotNull GeneralCommandLine commandLine,
+                                                        @Nullable Project project,
+                                                        @Nullable PyRemotePathMapper pathMapper)
     throws ExecutionException {
     ProcessHandler processHandler;
 
     try {
-      processHandler = doStartRemoteProcess(sdk, commandLine, project, pathMapper);
+      processHandler = doStartLegacyRemoteProcess(legacyAdditionalData, commandLine, project, pathMapper);
     }
     catch (UnsupportedPythonSdkTypeException e) {
-      throw new ExecutionException(PyBundle.message("remote.interpreter.support.is.not.available", sdk.getName()), e);
+      throw new ExecutionException(PyBundle.message("remote.interpreter.support.is.not.available", legacyAdditionalData.getClass()), e);
     }
     catch (ExecutionException e) {
       final Application application = ApplicationManager.getApplication();
@@ -62,32 +67,27 @@ public class PyRemoteProcessStarter {
    * Starts a process using corresponding support (e.g. SSH, Vagrant, Docker,
    * etc.) for the provided {@code sdk}.
    *
-   * @param sdk         the Python SDK
-   * @param commandLine the command line to start the Python interpreter
-   * @param project     the optional project for additional path mappings
-   * @param pathMapper  the mapping between paths on the host machine and the one
-   *                    the process will be executed on
+   * @param legacyAdditionalData //TODO: DOC
+   * @param commandLine          the command line to start the Python interpreter
+   * @param project              the optional project for additional path mappings
+   * @param pathMapper           the mapping between paths on the host machine and the one
+   *                             the process will be executed on
    * @return process handler for created process
    * @throws UnsupportedPythonSdkTypeException if support cannot be found for
    *                                           the type of the provided sdk
    */
   @NotNull
-  protected ProcessHandler doStartRemoteProcess(@NotNull Sdk sdk,
-                                                @NotNull final GeneralCommandLine commandLine,
-                                                @Nullable final Project project,
-                                                @Nullable PyRemotePathMapper pathMapper)
+  private static ProcessHandler doStartLegacyRemoteProcess(@NotNull PyRemoteSdkAdditionalData legacyAdditionalData,
+                                                           @NotNull final GeneralCommandLine commandLine,
+                                                           @Nullable final Project project,
+                                                           @Nullable PyRemotePathMapper pathMapper)
     throws ExecutionException {
-    SdkAdditionalData data = sdk.getSdkAdditionalData();
-    assert data instanceof PyRemoteSdkAdditionalDataBase;
-    final PyRemoteSdkAdditionalDataBase pyRemoteSdkAdditionalDataBase = (PyRemoteSdkAdditionalDataBase)data;
-
     final PyRemotePathMapper extendedPathMapper =
-      PythonRemoteInterpreterManager.appendBasicMappings(project, pathMapper, pyRemoteSdkAdditionalDataBase);
+      PythonRemoteInterpreterManager.appendBasicMappings(project, pathMapper, legacyAdditionalData);
 
     try {
-      return PyRemoteProcessStarterManagerUtil
-        .getManager(pyRemoteSdkAdditionalDataBase)
-        .startRemoteProcess(project, commandLine, pyRemoteSdkAdditionalDataBase, extendedPathMapper);
+      return PyRemoteProcessStarterManagerUtil.getManager(legacyAdditionalData)
+        .startRemoteProcess(project, commandLine, legacyAdditionalData, extendedPathMapper);
     }
     catch (InterruptedException e) {
       throw new ExecutionException(e);
--- python/src/com/jetbrains/python/run/PythonCommandLineState.java
+++ python/src/com/jetbrains/python/run/PythonCommandLineState.java
@@ -61,13 +61,11 @@ import com.jetbrains.python.facet.LibraryContributingFacet;
 import com.jetbrains.python.facet.PythonPathContributingFacet;
 import com.jetbrains.python.library.PythonLibraryType;
 import com.jetbrains.python.remote.PyRemotePathMapper;
+import com.jetbrains.python.remote.PyRemoteSdkAdditionalData;
 import com.jetbrains.python.run.target.HelpersAwareTargetEnvironmentRequest;
 import com.jetbrains.python.run.target.PySdkTargetPaths;
 import com.jetbrains.python.run.target.PythonCommandLineTargetEnvironmentProvider;
-import com.jetbrains.python.sdk.PySdkUtil;
-import com.jetbrains.python.sdk.PythonEnvUtil;
-import com.jetbrains.python.sdk.PythonSdkAdditionalData;
-import com.jetbrains.python.sdk.PythonSdkUtil;
+import com.jetbrains.python.sdk.*;
 import com.jetbrains.python.sdk.flavors.JythonSdkFlavor;
 import com.jetbrains.python.sdk.flavors.PythonSdkFlavor;
 import org.jetbrains.annotations.NotNull;
@@ -278,7 +276,7 @@ public abstract class PythonCommandLineState extends CommandLineState {
    * <p>
    * Patches the command line parameters applying patchers from first to last, and then runs it.
    *
-   * @param patchers       any number of patchers; any patcher may be null, and the whole argument may be null.
+   * @param patchers any number of patchers; any patcher may be null, and the whole argument may be null.
    * @return handler of the started process
    */
   @NotNull
@@ -381,10 +379,15 @@ public abstract class PythonCommandLineState extends CommandLineState {
   protected final PythonProcessStarter getDefaultPythonProcessStarter() {
     return (config, commandLine) -> {
       Sdk sdk = PythonSdkUtil.findSdkByPath(myConfig.getInterpreterPath());
+      assert sdk != null : "No SDK For " + myConfig.getInterpreterPath();
       final ProcessHandler processHandler;
-      if (PythonSdkUtil.isRemote(sdk)) {
+      var additionalData = sdk.getSdkAdditionalData();
+      if (additionalData instanceof PyRemoteSdkAdditionalDataMarker) {
+        assert additionalData instanceof PyRemoteSdkAdditionalData : "additionalData is remote, but not legacy. Is it a target-based? " +
+                                                                     additionalData;
         PyRemotePathMapper pathMapper = createRemotePathMapper();
-        processHandler = createRemoteProcessStarter().startRemoteProcess(sdk, commandLine, myConfig.getProject(), pathMapper);
+        processHandler = PyRemoteProcessStarter.startLegacyRemoteProcess((PyRemoteSdkAdditionalData)additionalData, commandLine,
+                                                                         myConfig.getProject(), pathMapper);
       }
       else {
         EncodingEnvironmentUtil.setLocaleEnvironmentIfMac(commandLine);
@@ -416,10 +419,6 @@ public abstract class PythonCommandLineState extends CommandLineState {
     else {
       return PyRemotePathMapper.fromSettings(myConfig.getMappingSettings(), PyRemotePathMapper.PyPathMappingType.USER_DEFINED);
     }
-  }
-
-  protected PyRemoteProcessStarter createRemoteProcessStarter() {
-    return new PyRemoteProcessStarter();
   }
 
   /**
@@ -579,7 +578,6 @@ public abstract class PythonCommandLineState extends CommandLineState {
    * Creates a number of parameter groups in the command line:
    * GROUP_EXE_OPTIONS, GROUP_DEBUGGER, GROUP_SCRIPT.
    * These are necessary for command line patchers to work properly.
-   *
    */
   public static void createStandardGroups(GeneralCommandLine commandLine) {
     ParametersList params = commandLine.getParametersList();
