b3c0a68bd320796d5ba5c2b416a2a3075a983cc5
Kirill.Krylov <kirill.krylov@jetbrains.com>
2022-09-13 12:41:10 +0200

    [full-line] temporary remove all tests from plugin module
    GitOrigin-RevId: ee6f5c12587377c2ea2387aa1dd0eb053a340e02

--- plugins/full-line/test/org/jetbrains/completion/full/line/features/FirstTokenTests.kt
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.completion.full.line.features
-
-import org.jetbrains.completion.full.line.platform.tests.FullLineCompletionTestCase
-
-class FirstTokenTests : FullLineCompletionTestCase() {
-  override fun getBasePath() = "testData/completion/features/first-token"
-
-  fun `test InsertionAfterNew`() = doTabTest("JAVA", "new String")
-}
