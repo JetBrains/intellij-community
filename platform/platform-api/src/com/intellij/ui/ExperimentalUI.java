fe5833c7e914459f012679b010e265de19eaba83
Nikolay Kuznetsov <nikolay.kuznetsov@jetbrains.com>
2022-09-13 19:18:47 +0200

    [cwm+rd] CWM-7348 turn off new UI for JB Client and RD Host
    GitOrigin-RevId: 9dafadb09e672786dd68f32f59a1e61accef7452

--- platform/platform-api/src/com/intellij/ui/ExperimentalUI.java
+++ platform/platform-api/src/com/intellij/ui/ExperimentalUI.java
@@ -13,6 +13,7 @@ import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.registry.RegistryValue;
 import com.intellij.openapi.util.registry.RegistryValueListener;
 import com.intellij.openapi.util.text.Strings;
+import com.intellij.util.PlatformUtils;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -35,7 +36,8 @@ public abstract class ExperimentalUI {
   private static final String KEY = "ide.experimental.ui";
 
   public static boolean isNewUI() {
-    return EarlyAccessRegistryManager.INSTANCE.getBoolean(KEY);
+    // CWM-7348 thin client does not support new UI
+    return EarlyAccessRegistryManager.INSTANCE.getBoolean(KEY) && !PlatformUtils.isJetBrainsClient();
   }
 
   public static boolean isNewNavbar() {
