fb13156c269a7c5e585b3fcb77d375177a4f1f52
nataliya.valtman <nataliya.valtman@jetbrains.com>
2022-09-15 16:40:10 +0200

    KT-54492 Send gradle build errors from idea
    (cherry picked from commit bd8d190411a186cfb7345dbfe61d1b4a2e921418)
    
    Change-Id: Ibd8d190411a186cfb7345dbfe61d1b4a2e921418

--- platform/platform-impl/src/com/intellij/diagnostic/KotlinCompilerCrash.kt
+++ platform/platform-impl/src/com/intellij/diagnostic/KotlinCompilerCrash.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.diagnostic;
 
-open class KotlinCompilerCrash(message: String, cause: Throwable?, val version: String) : Exception(message, cause, false, false)  {
+open class KotlinCompilerCrash(message: String, cause: Throwable? = null, val version: String = "Does not set") : Exception(message, cause, false, false)  {
 }
