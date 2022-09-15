301397bccecdaf489668786481747f920d05dc00
Kirill.Krylov <kirill.krylov@jetbrains.com>
2022-09-15 18:31:04 +0200

    [full-line] remove python format files
    GitOrigin-RevId: e198b45e243ce7668116baf4aadd420d0804d122

--- plugins/full-line/python/comments.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from converters import CommentsConverter
-
-comments_con = CommentsConverter()
-
-if __name__ == '__main__':
-    path = sys.argv[1]
-    comments_con.convert_file(path)
