0a09ec82dba5efec03f1091818840cb39645f820
Margarita Bobova <Margarita.Bobova@jetbrains.com>
2022-09-16 11:34:06 +0200

    Update versions in wizard for 1.8.0-221 and 213
    GitOrigin-RevId: 22214d3c08317533d9d33e0c9b0074816e1b9f12

--- plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/Versions.kt
+++ plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/Versions.kt
@@ -10,10 +10,10 @@ import org.jetbrains.kotlin.tools.projectWizard.settings.version.Version
 @Suppress("ClassName", "SpellCheckingInspection")
 object Versions {
     val KOTLIN = version("1.5.0") // used as fallback version
-    val GRADLE = version("7.4.2")
-    val KTOR = version("2.0.2")
+    val GRADLE = version("7.5.1")
+    val KTOR = version("2.1.1")
     val JUNIT = version("4.13.2")
-    val JUNIT5 = version("5.8.2")
+    val JUNIT5 = version("5.9.0")
     val JETBRAINS_COMPOSE = version("1.1.1")
 
     val KOTLIN_VERSION_FOR_COMPOSE = version("1.6.10")
@@ -24,10 +24,10 @@ object Versions {
     }
 
     object ANDROID {
-        val ANDROID_MATERIAL = version("1.5.0")
-        val ANDROIDX_APPCOMPAT = version("1.4.1")
-        val ANDROIDX_CONSTRAINTLAYOUT = version("2.1.3")
-        val ANDROIDX_KTX = version("1.7.0")
+        val ANDROID_MATERIAL = version("1.6.1")
+        val ANDROIDX_APPCOMPAT = version("1.5.1")
+        val ANDROIDX_CONSTRAINTLAYOUT = version("2.1.4")
+        val ANDROIDX_KTX = version("1.9.0")
     }
 
     object KOTLINX {
@@ -38,13 +38,13 @@ object Versions {
     object JS_WRAPPERS {
         val KOTLIN_REACT = wrapperVersion("18.2.0")
         val KOTLIN_REACT_DOM = KOTLIN_REACT
-        val KOTLIN_EMOTION = wrapperVersion("11.9.3")
+        val KOTLIN_EMOTION = wrapperVersion("11.10.4")
         val KOTLIN_REACT_ROUTER_DOM = wrapperVersion("6.3.0")
         val KOTLIN_REDUX = wrapperVersion("4.1.2")
         val KOTLIN_REACT_REDUX = wrapperVersion("7.2.6")
 
         private fun wrapperVersion(version: String): Version =
-            version("$version-pre.346")
+            version("$version-pre.385)
     }
 
     object GRADLE_PLUGINS {
