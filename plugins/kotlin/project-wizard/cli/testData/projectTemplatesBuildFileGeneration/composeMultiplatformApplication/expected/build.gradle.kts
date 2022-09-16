e324457fc685a00f081527e658b55afdd3a1d59e
Margarita Bobova <margarita.bobova@jetbrains.com>
2022-09-16 11:26:36 +0200

    [kotlin] Update versions in wizard for 1.8.0-222
    GitOrigin-RevId: 7cf2515eb00c4f131d269e19dde656edeb2e6d08

--- plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/build.gradle.kts
+++ plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:7.2.2")
     }
 }
 
