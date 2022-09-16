6fbf89c541fe49f402ac114a093ebbe54856e131
Bas Leijdekkers <basleijdekkers@gmail.com>
2022-09-16 16:26:29 +0200

    guava type migration: add a couple of tests
    GitOrigin-RevId: 9b6ee6f1e29e4e6ea4d778b393e3706c24ffd588

--- java/typeMigration/test/com/intellij/codeInsight/inspections/GuavaInspectionTest.java
+++ java/typeMigration/test/com/intellij/codeInsight/inspections/GuavaInspectionTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.inspections;
 
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
@@ -59,6 +59,10 @@ public class GuavaInspectionTest extends JavaCodeInsightFixtureTestCase {
     doTest();
   }
 
+  public void testOptionalArray() {
+    doTestAllFile();
+  }
+
   public void testSimpleFluentIterable() {
     doTest();
   }
@@ -107,7 +111,7 @@ public class GuavaInspectionTest extends JavaCodeInsightFixtureTestCase {
     doTest();
   }
 
-  public void _testChainedFluentIterableWithOf() {
+  public void testChainedFluentIterableWithOf() {
     doTest();
   }
 
