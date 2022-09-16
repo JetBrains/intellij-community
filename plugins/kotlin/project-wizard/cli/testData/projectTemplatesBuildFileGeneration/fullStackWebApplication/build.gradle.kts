86dfbe4123caaf6de47634efe281cc4647d61e85
Margarita Bobova <margarita.bobova@jetbrains.com>
2022-09-16 11:26:36 +0200

    [kotlin] Update versions in wizard for 1.8.0-222
    GitOrigin-RevId: e51b73ed92f1d2611d620e8bfee46153755e45b2

--- plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/fullStackWebApplication/build.gradle.kts
+++ plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/fullStackWebApplication/build.gradle.kts
@@ -44,17 +44,17 @@ kotlin {
         }
         val jvmMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-server-netty:2.0.2")
-                implementation("io.ktor:ktor-server-html-builder-jvm:2.0.2")
+                implementation("io.ktor:ktor-server-netty:2.1.1")
+                implementation("io.ktor:ktor-server-html-builder-jvm:2.1.1")
                 implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
             }
         }
         val jvmTest by getting
         val jsMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlin-wrappers:kotlin-react:18.2.0-pre.346")
-                implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:18.2.0-pre.346")
-                implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.9.3-pre.346")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react:18.2.0-pre.385")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:18.2.0-pre.385")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.10.4-pre.385")
             }
         }
         val jsTest by getting
