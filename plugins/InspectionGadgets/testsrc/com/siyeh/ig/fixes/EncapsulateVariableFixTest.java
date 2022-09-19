445ec6856c3c23997ed38acf2c042970c5c9fdf4
Andrey.Cherkasov <andrey.cherkasov@jetbrains.com>
2022-09-19 14:32:00 +0400

    Replace string literals with text blocks
    GitOrigin-RevId: 61099ea9f7ada18c749bac99b8e8121de73994f0

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/fixes/EncapsulateVariableFixTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/fixes/EncapsulateVariableFixTest.java
@@ -20,21 +20,22 @@ public class EncapsulateVariableFixTest extends LightJavaCodeInsightFixtureTestC
                               "}");
     IntentionAction action = myFixture.findSingleIntention("Encapsulate field 'name'");
     String text = myFixture.getIntentionPreviewText(action);
-    assertEquals("class A {\n" +
-                 "    private String name;\n" +
-                 "\n" +
-                 "    public String getName() {\n" +
-                 "        return name;\n" +
-                 "    }\n" +
-                 "\n" +
-                 "    public void setName(String name) {\n" +
-                 "        this.name = name;\n" +
-                 "    }\n" +
-                 "}\n" +
-                 "class B {\n" +
-                 "    void foo(A a) {\n" +
-                 "        System.out.println(a.getName());\n" +
-                 "    }\n" +
-                 "}", text);
+    assertEquals("""
+                   class A {
+                       private String name;
+
+                       public String getName() {
+                           return name;
+                       }
+
+                       public void setName(String name) {
+                           this.name = name;
+                       }
+                   }
+                   class B {
+                       void foo(A a) {
+                           System.out.println(a.getName());
+                       }
+                   }""", text);
   }
 }
