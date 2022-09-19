a53af312605c9f5b906fe47c7cffdba81ab3682a
Anna Kozlova <anna.kozlova@jetbrains.com>
2022-09-19 14:40:37 +0200

    [kotlin] don't issue a warning if nested class is used in the import directive (KTIJ-16557)
    GitOrigin-RevId: fc44fb830a563cbbb2f9256e9f0f6ae8fc53c37f

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/refactoring/safeDelete/SafeDeleteImportDirectiveUsageInfo.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/refactoring/safeDelete/SafeDeleteImportDirectiveUsageInfo.kt
@@ -1,22 +1,13 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.refactoring.safeDelete
 
 import com.intellij.psi.PsiElement
 import com.intellij.refactoring.safeDelete.usageInfo.SafeDeleteReferenceSimpleDeleteUsageInfo
-import org.jetbrains.kotlin.asJava.unwrapped
-import org.jetbrains.kotlin.idea.references.mainReference
 import org.jetbrains.kotlin.psi.KtImportDirective
-import org.jetbrains.kotlin.psi.KtSimpleNameExpression
 import org.jetbrains.kotlin.psi.doNotAnalyze
-import org.jetbrains.kotlin.psi.psiUtil.getQualifiedElementSelector
 
 class SafeDeleteImportDirectiveUsageInfo(
     importDirective: KtImportDirective, declaration: PsiElement
-) : SafeDeleteReferenceSimpleDeleteUsageInfo(importDirective, declaration, importDirective.isSafeToDelete(declaration))
+) : SafeDeleteReferenceSimpleDeleteUsageInfo(importDirective, declaration, importDirective.containingKtFile.doNotAnalyze == null)
 
-private fun KtImportDirective.isSafeToDelete(element: PsiElement): Boolean {
-    if (this.containingKtFile.doNotAnalyze != null) return false
-    val nameExpression = importedReference?.getQualifiedElementSelector() as? KtSimpleNameExpression ?: return false
-    return element.unwrapped == nameExpression.mainReference.resolve()
-}
