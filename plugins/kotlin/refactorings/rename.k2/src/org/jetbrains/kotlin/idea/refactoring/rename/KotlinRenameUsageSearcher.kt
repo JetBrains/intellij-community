35842c4b3ef8ed3321121a4665b0e4a996eea2d4
Roman Golyshev <roman.golyshev@jetbrains.com>
2022-09-19 21:36:43 +0200

    [kotlin] KTIJ-22809 Use `ReferenceSearch` in `KotlinRenameUsageSearcher`
    This makes searching for references to rename much faster.
    Probably should be replaced by a different API in the future,
    when Kotlin symbols are be supported
    
    ^KTIJ-22809 Fixed
    
    GitOrigin-RevId: 5d9c54d669b74cc4124beb0f47e238def85a7b99

--- plugins/kotlin/refactorings/rename.k2/src/org/jetbrains/kotlin/idea/refactoring/rename/KotlinRenameUsageSearcher.kt
+++ plugins/kotlin/refactorings/rename.k2/src/org/jetbrains/kotlin/idea/refactoring/rename/KotlinRenameUsageSearcher.kt
@@ -1,20 +1,17 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.refactoring.rename
 
-import com.intellij.model.Symbol
-import com.intellij.model.psi.PsiSymbolService
-import com.intellij.model.search.LeafOccurrence
-import com.intellij.model.search.LeafOccurrenceMapper
-import com.intellij.model.search.SearchContext
-import com.intellij.model.search.SearchService
-import com.intellij.psi.util.parentOfType
+import com.intellij.psi.search.SearchScope
+import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.refactoring.rename.api.RenameUsage
 import com.intellij.refactoring.rename.api.RenameUsageSearchParameters
 import com.intellij.refactoring.rename.api.RenameUsageSearcher
 import com.intellij.util.Query
-import org.jetbrains.kotlin.idea.KotlinLanguage
-import org.jetbrains.kotlin.idea.references.mainReference
+import com.intellij.util.mappingNotNull
+import org.jetbrains.kotlin.idea.base.util.codeUsageScope
 import org.jetbrains.kotlin.psi.KtNameReferenceExpression
+import org.jetbrains.kotlin.psi.KtNamedDeclaration
+import org.jetbrains.kotlin.psi.KtParameter
 
 internal class KotlinRenameUsageSearcher : RenameUsageSearcher {
     override fun collectImmediateResults(parameters: RenameUsageSearchParameters): Collection<RenameUsage> {
@@ -25,28 +22,22 @@ internal class KotlinRenameUsageSearcher : RenameUsageSearcher {
 
     override fun collectSearchRequests(parameters: RenameUsageSearchParameters): List<Query<out RenameUsage>> {
         val renameTarget = parameters.target as? KotlinNamedDeclarationRenameUsage ?: return emptyList()
-        val symbol = PsiSymbolService.getInstance().asSymbol(renameTarget.element)
 
-        val kotlinQuery = SearchService.getInstance()
-            .searchWord(parameters.project, renameTarget.targetName)
-            .inScope(parameters.searchScope)
-            .inContexts(SearchContext.IN_CODE)
-            .inFilesWithLanguage(KotlinLanguage.INSTANCE)
-            .buildQuery(LeafOccurrenceMapper.withPointer(symbol.createPointer(), ::kotlinReferenceToRenameUsage))
+        val codeUsageScope = getCodeUsageScopeForDeclaration(renameTarget.element)
+        val searchScope = codeUsageScope.intersectWith(parameters.searchScope)
+
+        val kotlinQuery = ReferencesSearch.search(renameTarget.element, searchScope)
+            .mappingNotNull { it.element as? KtNameReferenceExpression }
+            .mapping { KotlinReferenceModifiableRenameUsage(it) }
 
         return listOf(kotlinQuery)
     }
 
-    private fun kotlinReferenceToRenameUsage(
-        expectedTarget: Symbol,
-        potentialReferenceOccurence: LeafOccurrence,
-    ): List<RenameUsage> {
-        val referenceExpression = potentialReferenceOccurence.start.parentOfType<KtNameReferenceExpression>()
-        val references = referenceExpression?.mainReference ?: return emptyList()
-        val symbolReference = PsiSymbolService.getInstance().asSymbolReference(references)
-
-        if (!symbolReference.resolvesTo(expectedTarget)) return emptyList()
-
-        return listOf(KotlinReferenceModifiableRenameUsage(referenceExpression))
+    private fun getCodeUsageScopeForDeclaration(declaration: KtNamedDeclaration): SearchScope {
+        val adjustedDeclaration = when (declaration) {
+            is KtParameter -> declaration.ownerFunction ?: declaration
+            else -> declaration
+        }
+        return adjustedDeclaration.codeUsageScope()
     }
 }
