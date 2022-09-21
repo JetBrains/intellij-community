756386931dc47d34808604d3a86929fc60266cb8
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2022-09-21 17:27:10 +0900

    [kotlin] Move 'KotlinRunLineMarkerContributor' to the 'kotlin.code-insight' module group (KTIJ-21112)
    GitOrigin-RevId: 354155f6f34aadf20e12a9ea6e06acefe2adbd79

--- plugins/kotlin/code-insight/line-markers-shared/src/org/jetbrains/kotlin/idea/codeInsight/lineMarkers/shared/KotlinRunLineMarkerContributor.kt
+++ plugins/kotlin/code-insight/line-markers-shared/src/org/jetbrains/kotlin/idea/codeInsight/lineMarkers/shared/KotlinRunLineMarkerContributor.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.base.lineMarkers.run
+package org.jetbrains.kotlin.idea.codeInsight.lineMarkers.shared
 
 import com.intellij.execution.lineMarker.ExecutorAction
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
