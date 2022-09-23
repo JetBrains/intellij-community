be5df2e7b4b8d0cd2dcff0c6ca1b8fdb92f0eb1a
Max Biloborodko <f1ashhimself@gmail.com>
2022-09-23 14:02:53 +0300

    Update _jb_pytest_runner.py
    Fix codestyle

--- python/helpers/pycharm/_jb_pytest_runner.py
+++ python/helpers/pycharm/_jb_pytest_runner.py
@@ -34,8 +34,7 @@ if __name__ == '__main__':
         args += ["--no-header", "--no-summary", "-q"]
 
     if JB_DISABLE_BUFFERING and "-s" not in args:
-      args += ["-s"]
-
+        args += ["-s"]
 
     jb_doc_args("pytest", args)
 
