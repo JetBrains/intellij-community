7b40cbe09dc8c3ba6a4c0488e07d9d2714744167
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-09-23 14:55:56 +0200

    vcs: mark 'Update Copyright' as CheckinModificationHandler
    GitOrigin-RevId: fa78c2a7e13893c53cfd0fc37db854928d271fb0

--- plugins/copyright/src/com/maddyhome/idea/copyright/actions/UpdateCopyrightCheckinHandlerFactory.java
+++ plugins/copyright/src/com/maddyhome/idea/copyright/actions/UpdateCopyrightCheckinHandlerFactory.java
@@ -10,6 +10,7 @@ import com.intellij.openapi.vcs.changes.CommitExecutor;
 import com.intellij.openapi.vcs.changes.ui.BooleanCommitOption;
 import com.intellij.openapi.vcs.checkin.CheckinHandler;
 import com.intellij.openapi.vcs.checkin.CheckinHandlerFactory;
+import com.intellij.openapi.vcs.checkin.CheckinModificationHandler;
 import com.intellij.openapi.vcs.ui.RefreshableOnComponent;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
@@ -24,43 +25,51 @@ import java.util.Collection;
 import java.util.List;
 
 
-public class UpdateCopyrightCheckinHandlerFactory extends CheckinHandlerFactory  {
+public class UpdateCopyrightCheckinHandlerFactory extends CheckinHandlerFactory {
   @Override
   @NotNull
   public CheckinHandler createHandler(@NotNull final CheckinProjectPanel panel, @NotNull CommitContext commitContext) {
-    return new CheckinHandler() {
-      @Override
-      public RefreshableOnComponent getBeforeCheckinConfigurationPanel() {
-        return new BooleanCommitOption(panel, CopyrightBundle.message("before.checkin.update.copyright"), false, () -> getSettings().UPDATE_COPYRIGHT,
-                                       value -> getSettings().UPDATE_COPYRIGHT = value);
-      }
+    return new UpdateCopyrightCheckinHandler(panel);
+  }
 
-      @Override
-      public ReturnResult beforeCheckin(@Nullable CommitExecutor executor, PairConsumer<Object, Object> additionalDataConsumer) {
-        if (getSettings().UPDATE_COPYRIGHT) {
-          new UpdateCopyrightProcessor(panel.getProject(), null, getPsiFiles()).run();
-          FileDocumentManager.getInstance().saveAllDocuments();
+  private static class UpdateCopyrightCheckinHandler extends CheckinHandler implements CheckinModificationHandler {
+    private final @NotNull CheckinProjectPanel myPanel;
+
+    UpdateCopyrightCheckinHandler(@NotNull CheckinProjectPanel panel) {
+      myPanel = panel;
+    }
+
+    @Override
+    public RefreshableOnComponent getBeforeCheckinConfigurationPanel() {
+      return new BooleanCommitOption(myPanel, CopyrightBundle.message("before.checkin.update.copyright"), false, () -> getSettings().UPDATE_COPYRIGHT,
+                                     value -> getSettings().UPDATE_COPYRIGHT = value);
+    }
+
+    @Override
+    public ReturnResult beforeCheckin(@Nullable CommitExecutor executor, PairConsumer<Object, Object> additionalDataConsumer) {
+      if (getSettings().UPDATE_COPYRIGHT) {
+        new UpdateCopyrightProcessor(myPanel.getProject(), null, getPsiFiles()).run();
+        FileDocumentManager.getInstance().saveAllDocuments();
+      }
+      return super.beforeCheckin();
+    }
+
+    @NotNull
+    private UpdateCopyrightCheckinHandlerState getSettings() {
+      return UpdateCopyrightCheckinHandlerState.getInstance(myPanel.getProject());
+    }
+
+    private PsiFile @NotNull [] getPsiFiles() {
+      final Collection<VirtualFile> files = myPanel.getVirtualFiles();
+      final List<PsiFile> psiFiles = new ArrayList<>();
+      final PsiManager manager = PsiManager.getInstance(myPanel.getProject());
+      for (final VirtualFile file : files) {
+        final PsiFile psiFile = manager.findFile(file);
+        if (psiFile != null) {
+          psiFiles.add(psiFile);
         }
-        return super.beforeCheckin();
       }
-
-      @NotNull
-      private UpdateCopyrightCheckinHandlerState getSettings() {
-        return UpdateCopyrightCheckinHandlerState.getInstance(panel.getProject());
-      }
-
-      private PsiFile @NotNull [] getPsiFiles() {
-        final Collection<VirtualFile> files = panel.getVirtualFiles();
-        final List<PsiFile> psiFiles = new ArrayList<>();
-        final PsiManager manager = PsiManager.getInstance(panel.getProject());
-        for (final VirtualFile file : files) {
-          final PsiFile psiFile = manager.findFile(file);
-          if (psiFile != null) {
-            psiFiles.add(psiFile);
-          }
-        }
-        return PsiUtilCore.toPsiFileArray(psiFiles);
-      }
-    };
+      return PsiUtilCore.toPsiFileArray(psiFiles);
+    }
   }
 }
