6401629b984ce1aa2ff7bcab4dd713b334f4db15
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-09-23 16:41:35 +0200

    vcs: convert UpdateCopyrightCheckinHandlerFactory to Kotlin
    GitOrigin-RevId: 248c681373f836bd658d4615117dbaca76711320

--- plugins/copyright/src/com/maddyhome/idea/copyright/actions/UpdateCopyrightCheckinHandlerFactory.kt
+++ plugins/copyright/src/com/maddyhome/idea/copyright/actions/UpdateCopyrightCheckinHandlerFactory.kt
@@ -1,75 +1,54 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.maddyhome.idea.copyright.actions
 
-package com.maddyhome.idea.copyright.actions;
+import com.intellij.copyright.CopyrightBundle
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.vcs.CheckinProjectPanel
+import com.intellij.openapi.vcs.changes.CommitContext
+import com.intellij.openapi.vcs.changes.CommitExecutor
+import com.intellij.openapi.vcs.changes.ui.BooleanCommitOption
+import com.intellij.openapi.vcs.checkin.CheckinHandler
+import com.intellij.openapi.vcs.checkin.CheckinHandlerFactory
+import com.intellij.openapi.vcs.checkin.CheckinModificationHandler
+import com.intellij.openapi.vcs.ui.RefreshableOnComponent
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
+import com.intellij.psi.util.PsiUtilCore
+import com.intellij.util.PairConsumer
 
-import com.intellij.copyright.CopyrightBundle;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.vcs.CheckinProjectPanel;
-import com.intellij.openapi.vcs.changes.CommitContext;
-import com.intellij.openapi.vcs.changes.CommitExecutor;
-import com.intellij.openapi.vcs.changes.ui.BooleanCommitOption;
-import com.intellij.openapi.vcs.checkin.CheckinHandler;
-import com.intellij.openapi.vcs.checkin.CheckinHandlerFactory;
-import com.intellij.openapi.vcs.checkin.CheckinModificationHandler;
-import com.intellij.openapi.vcs.ui.RefreshableOnComponent;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
-import com.intellij.psi.util.PsiUtilCore;
-import com.intellij.util.PairConsumer;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-
-public class UpdateCopyrightCheckinHandlerFactory extends CheckinHandlerFactory {
-  @Override
-  @NotNull
-  public CheckinHandler createHandler(@NotNull final CheckinProjectPanel panel, @NotNull CommitContext commitContext) {
-    return new UpdateCopyrightCheckinHandler(panel);
+class UpdateCopyrightCheckinHandlerFactory : CheckinHandlerFactory() {
+  override fun createHandler(panel: CheckinProjectPanel, commitContext: CommitContext): CheckinHandler {
+    return UpdateCopyrightCheckinHandler(panel)
   }
 
-  private static class UpdateCopyrightCheckinHandler extends CheckinHandler implements CheckinModificationHandler {
-    private final @NotNull CheckinProjectPanel myPanel;
+  private class UpdateCopyrightCheckinHandler(private val panel: CheckinProjectPanel) : CheckinHandler(), CheckinModificationHandler {
 
-    UpdateCopyrightCheckinHandler(@NotNull CheckinProjectPanel panel) {
-      myPanel = panel;
+    override fun getBeforeCheckinConfigurationPanel(): RefreshableOnComponent {
+      return BooleanCommitOption(panel, CopyrightBundle.message("before.checkin.update.copyright"), false,
+                                 { settings.UPDATE_COPYRIGHT }, { value -> settings.UPDATE_COPYRIGHT = value })
     }
 
-    @Override
-    public RefreshableOnComponent getBeforeCheckinConfigurationPanel() {
-      return new BooleanCommitOption(myPanel, CopyrightBundle.message("before.checkin.update.copyright"), false, () -> getSettings().UPDATE_COPYRIGHT,
-                                     value -> getSettings().UPDATE_COPYRIGHT = value);
-    }
-
-    @Override
-    public ReturnResult beforeCheckin(@Nullable CommitExecutor executor, PairConsumer<Object, Object> additionalDataConsumer) {
-      if (getSettings().UPDATE_COPYRIGHT) {
-        new UpdateCopyrightProcessor(myPanel.getProject(), null, getPsiFiles()).run();
-        FileDocumentManager.getInstance().saveAllDocuments();
+    override fun beforeCheckin(executor: CommitExecutor?, additionalDataConsumer: PairConsumer<Any, Any>): ReturnResult? {
+      if (settings.UPDATE_COPYRIGHT) {
+        UpdateCopyrightProcessor(panel.project, null, getPsiFiles()).run()
+        FileDocumentManager.getInstance().saveAllDocuments()
       }
-      return super.beforeCheckin();
+      return super.beforeCheckin()
     }
 
-    @NotNull
-    private UpdateCopyrightCheckinHandlerState getSettings() {
-      return UpdateCopyrightCheckinHandlerState.getInstance(myPanel.getProject());
-    }
+    private val settings: UpdateCopyrightCheckinHandlerState get() = UpdateCopyrightCheckinHandlerState.getInstance(panel.project)
 
-    private PsiFile @NotNull [] getPsiFiles() {
-      final Collection<VirtualFile> files = myPanel.getVirtualFiles();
-      final List<PsiFile> psiFiles = new ArrayList<>();
-      final PsiManager manager = PsiManager.getInstance(myPanel.getProject());
-      for (final VirtualFile file : files) {
-        final PsiFile psiFile = manager.findFile(file);
+    private fun getPsiFiles(): Array<PsiFile> {
+      val files = panel.virtualFiles
+      val psiFiles: MutableList<PsiFile> = ArrayList()
+      val manager = PsiManager.getInstance(panel.project)
+      for (file in files) {
+        val psiFile = manager.findFile(file)
         if (psiFile != null) {
-          psiFiles.add(psiFile);
+          psiFiles.add(psiFile)
         }
       }
-      return PsiUtilCore.toPsiFileArray(psiFiles);
+      return PsiUtilCore.toPsiFileArray(psiFiles)
     }
   }
 }
