4944fd4c68a5eef5aa09d79806be260d31d10531
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2022-09-23 10:16:43 +0200

    [tests] VFS refresh perf. test: cleaner restore of VM options, bumping timeouts; cleanup
    GitOrigin-RevId: c9c79aec676753ae3a1fa41f7ed2733d7bcf1268

--- tools/ideTestingFramework/intellij.tools.ide.starter/src/com/intellij/ide/starter/models/VMOptions.kt
+++ tools/ideTestingFramework/intellij.tools.ide.starter/src/com/intellij/ide/starter/models/VMOptions.kt
@@ -67,6 +67,9 @@ data class VMOptions(
     return addLine(line = "-D$key=$value", filterPrefix = "-D$key=")
   }
 
+  fun removeSystemProperty(key: String): VMOptions =
+    copy(data = data.filterNot { it.trim().startsWith("-D${key}") })
+
   fun addLine(line: String, filterPrefix: String? = null): VMOptions {
     if (data.contains(line)) return this
     val copy = if (filterPrefix == null) data else data.filterNot { it.trim().startsWith(filterPrefix) }
