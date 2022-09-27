63572f055dc9cfc9ae45fefd3eda17557795ab0f
Konstantin Bulenkov <kb@jetbrains.com>
2022-09-27 15:25:51 +0200

    [NewUI] make tool window stripes visible when enabling new UI
    GitOrigin-RevId: 5fe2e365c39dc42f6b16b93194880a86ae0c56d5

--- platform/lang-impl/src/com/intellij/ui/ExperimentalUIImpl.kt
+++ platform/lang-impl/src/com/intellij/ui/ExperimentalUIImpl.kt
@@ -28,6 +28,7 @@ class ExperimentalUIImpl : ExperimentalUI() {
     setRegistryKeyIfNecessary("ide.experimental.ui", true)
     setRegistryKeyIfNecessary("debugger.new.tool.window.layout", true)
     UISettings.getInstance().openInPreviewTabIfPossible = true
+    UISettings.getInstance().hideToolStripes = false
     val name = if (JBColor.isBright()) "Light" else "Dark"
     val lafManager = LafManager.getInstance()
     val laf = lafManager.installedLookAndFeels.firstOrNull { it.name == name }
