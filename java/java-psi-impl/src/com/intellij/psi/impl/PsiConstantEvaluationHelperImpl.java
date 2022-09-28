37430d9ba88d6d393b514a729e59899deddb995d
Anna Kozlova <anna.kozlova@jetbrains.com>
2022-09-28 08:26:35 +0200

    [kotlin] don't issue exceptions when no constant evaluator is present (KTIJ-22791)
    GitOrigin-RevId: c1d6ed19ce2cd480a1165147855fde06c6fd3bed

--- java/java-psi-impl/src/com/intellij/psi/impl/PsiConstantEvaluationHelperImpl.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/PsiConstantEvaluationHelperImpl.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.impl;
 
 import com.intellij.psi.*;
@@ -36,16 +22,20 @@ public class PsiConstantEvaluationHelperImpl extends PsiConstantEvaluationHelper
   public Object computeConstantExpression(@Nullable PsiElement expression, boolean throwExceptionOnOverflow) {
     if (expression == null) return null;
     ConstantExpressionEvaluator expressionEvaluator = LanguageConstantExpressionEvaluator.INSTANCE.forLanguage(expression.getLanguage());
-    assert expressionEvaluator != null;
 
-    return expressionEvaluator.computeConstantExpression(expression, throwExceptionOnOverflow);
+    if (expressionEvaluator != null) {
+      return expressionEvaluator.computeConstantExpression(expression, throwExceptionOnOverflow);
+    }
+    return null;
   }
 
   @Override
   public Object computeExpression(@NotNull final PsiExpression expression, final boolean throwExceptionOnOverflow, @Nullable final AuxEvaluator auxEvaluator) {
     ConstantExpressionEvaluator expressionEvaluator = LanguageConstantExpressionEvaluator.INSTANCE.forLanguage(expression.getLanguage());
-    assert expressionEvaluator != null;
-    return expressionEvaluator.computeExpression(expression, throwExceptionOnOverflow, auxEvaluator);
+    if (expressionEvaluator != null) {
+      return expressionEvaluator.computeExpression(expression, throwExceptionOnOverflow, auxEvaluator);
+    }
+    return null;
   }
 
   public static Object computeCastTo(@NotNull PsiExpression expression, @NotNull PsiType castTo, @Nullable Set<PsiVariable> visitedVars) {
