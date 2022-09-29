6a42632fbba03bccd8f489a9c652d91e775236ef
Anna Kozlova <anna.kozlova@jetbrains.com>
2022-09-29 14:21:37 +0200

    [execution] ensure that name after "configuration copy" is not treated as generated (IDEA-302749)
    GitOrigin-RevId: cf6ff31e3dd2e07915000267d9395dc950399f67

--- platform/execution-impl/src/com/intellij/execution/impl/RunConfigurable.kt
+++ platform/execution-impl/src/com/intellij/execution/impl/RunConfigurable.kt
@@ -1109,6 +1109,7 @@ open class RunConfigurable @JvmOverloads constructor(protected val project: Proj
         val typeNode = selectedConfigurationTypeNode!!
         val settings = configuration.createSnapshot(true)
         val copyName = createUniqueName(typeNode, configuration.nameText, CONFIGURATION, TEMPORARY_CONFIGURATION)
+        (settings.configuration as? LocatableConfigurationBase<*>)?.setNameChangedByUser(true)
         settings.name = copyName
         val factory = settings.factory
         @Suppress("UNCHECKED_CAST", "DEPRECATION")
