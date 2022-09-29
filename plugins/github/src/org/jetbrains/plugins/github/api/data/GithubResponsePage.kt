d1bc15e5c9067c1bd63e383d3af3e4cbb3ea2b07
Ivan Semenov <ivan.semenov@jetbrains.com>
2022-09-29 12:01:55 +0200

    fix[github]: fix response page serialization
    GitOrigin-RevId: 43eeae696510e84d464a3935dc381655868f11b7

--- plugins/github/src/org/jetbrains/plugins/github/api/data/GithubResponsePage.kt
+++ plugins/github/src/org/jetbrains/plugins/github/api/data/GithubResponsePage.kt
@@ -3,9 +3,16 @@ package org.jetbrains.plugins.github.api.data
 
 import com.intellij.collaboration.api.util.LinkHttpHeaderValue
 
-class GithubResponsePage<T> constructor(val items: List<T>, linkHeaderValue: LinkHttpHeaderValue?) {
-  val hasNext = linkHeaderValue?.nextLink != null
-  val nextLink: String? = linkHeaderValue?.nextLink
+class GithubResponsePage<T>(val items: List<T>,
+                            val firstLink: String? = null,
+                            val prevLink: String? = null,
+                            val nextLink: String? = null,
+                            val lastLink: String? = null) {
+
+  val hasNext = nextLink != null
+
+  constructor(items: List<T>, linkHeaderValue: LinkHttpHeaderValue?)
+    : this(items, linkHeaderValue?.firstLink, linkHeaderValue?.prevLink, linkHeaderValue?.nextLink, linkHeaderValue?.lastLink)
 }
 
 
