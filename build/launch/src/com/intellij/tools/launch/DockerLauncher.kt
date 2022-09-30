91a0761b5c620582cd4dbc07e5157a9a9b53e592
anastasia.katsman <anastasia.katsman@jetbrains.com>
2022-09-30 11:40:10 +0000

    [rdct-tests]: first steps to bring back docker tests
    GitOrigin-RevId: 07dedc5cde994576eb7b8a0aab4e604d348b300e

--- build/launch/src/com/intellij/tools/launch/DockerLauncher.kt
+++ build/launch/src/com/intellij/tools/launch/DockerLauncher.kt
@@ -87,7 +87,6 @@ class DockerLauncher(private val paths: PathsProvider, private val options: Dock
                            paths.configFolder,
                            paths.systemFolder,
                            paths.outputRootFolder, // classpath index making a lot of noise
-                           paths.sourcesRootFolder.resolve("remote-dev/cwm-tests/general/data"), // classpath index making a lot of noise in stderr
                            paths.communityRootFolder.resolve("build/download")) // quiche lib
 
     // docker can create these under root, so we create them ourselves
