1a39f7e80c3c285e44fc9642589c7d9200393cad
Konstantin Bulenkov <kb@jetbrains.com>
2022-10-01 17:03:20 +0200

    a utility class for accessing colors for the current editor scheme
    GitOrigin-RevId: 396fc3e24629843d1772513d18025a917b3ba19d

---
+++ platform/editor-ui-api/src/com/intellij/openapi/editor/colors/GlobalEditorScheme.java
@@ -0,0 +1,29 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.editor.colors;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
+
+/**
+ * @author Konstantin Bulenkov
+ */
+public final class GlobalEditorScheme {
+  public static @Nullable Color getColor(@NotNull ColorKey key) {
+    return getGlobalScheme().getColor(key);
+  }
+
+  @NotNull
+  private static EditorColorsScheme getGlobalScheme() {
+    return EditorColorsManager.getInstance().getGlobalScheme();
+  }
+
+  public static @NotNull Color getDefaultBackground() {
+    return getGlobalScheme().getDefaultBackground();
+  }
+
+  public static @NotNull Color getDefaultForeground() {
+    return getGlobalScheme().getDefaultForeground();
+  }
+}
