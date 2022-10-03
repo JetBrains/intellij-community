d54000c1766169cd115a92449132e7610f3d95ba
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2022-10-03 16:42:33 +0200

    [git] Improve input fields focus painting in Rebase/Merge/Pull dialog for MacOS Theme
    IDEA-301615
    
    GitOrigin-RevId: b4a9e0b13ed060cfb62d229f91afc1d7f8bcfbe3

--- platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaComboBoxUI.java
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaComboBoxUI.java
@@ -45,8 +45,10 @@ public class DarculaComboBoxUI extends BasicComboBoxUI implements Border, ErrorB
   private static final Color NON_EDITABLE_BACKGROUND = JBColor.namedColor("ComboBox.nonEditableBackground",
                                                                           JBColor.namedColor("ComboBox.darcula.nonEditableBackground", new JBColor(0xfcfcfc, 0x3c3f41)));
 
+  protected static final int DEFAULT_BORDER_COMPENSATION = 1;
+
   private float myArc = COMPONENT_ARC.getFloat();
-  private Insets myBorderCompensation = JBUI.insets(1);
+  private Insets myBorderCompensation = JBUI.insets(DEFAULT_BORDER_COMPENSATION);
   private boolean myPaintArrowButton = true;
 
   public DarculaComboBoxUI() {}
