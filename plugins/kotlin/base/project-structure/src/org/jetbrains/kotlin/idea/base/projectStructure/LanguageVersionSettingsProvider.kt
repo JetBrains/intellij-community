17a062f1b4eadc611de5e68719672ca6b703ac16
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-10-03 12:35:36 +0200

    [kotlin] cache language version settings of strong ref instead of soft ref
    Those settings are needed often.
    When the amount of ram became insufficient, they were garbage collected often and when recreated again.
    
    GitOrigin-RevId: e09830948580c938539e6845f662a2ce45bec6e3

--- plugins/kotlin/base/project-structure/src/org/jetbrains/kotlin/idea/base/projectStructure/LanguageVersionSettingsProvider.kt
+++ plugins/kotlin/base/project-structure/src/org/jetbrains/kotlin/idea/base/projectStructure/LanguageVersionSettingsProvider.kt
@@ -2,6 +2,7 @@
 @file:JvmName("LanguageVersionSettingsProviderUtils")
 package org.jetbrains.kotlin.idea.base.projectStructure
 
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.components.serviceOrNull
@@ -17,7 +18,6 @@ import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.TestOnly
-import org.jetbrains.kotlin.caches.project.cacheInvalidatingOnRootModifications
 import org.jetbrains.kotlin.cli.common.arguments.JavaTypeEnhancementStateParser
 import org.jetbrains.kotlin.cli.common.arguments.K2JVMCompilerArguments
 import org.jetbrains.kotlin.cli.common.arguments.parseCommandLineArguments
@@ -33,6 +33,7 @@ import org.jetbrains.kotlin.platform.isCommon
 import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 import org.jetbrains.kotlin.util.merge
 import java.util.*
+import org.jetbrains.kotlin.idea.compiler.configuration.KotlinCompilerSettingsListener
 
 private typealias LanguageFeatureMap = Map<LanguageFeature, LanguageFeature.State>
 private typealias AnalysisFlagMap = Map<AnalysisFlag<*>, Any>
@@ -62,19 +63,37 @@ val PsiElement.languageVersionSettings: LanguageVersionSettings
     }
 
 @Service(Service.Level.PROJECT)
-class LanguageVersionSettingsProvider(private val project: Project) {
+class LanguageVersionSettingsProvider(private val project: Project) : Disposable {
+    init {
+        project.messageBus.connect(this)
+            .subscribe(KotlinCompilerSettingsListener.TOPIC, object : KotlinCompilerSettingsListener {
+                override fun <T> settingsChanged(oldSettings: T?, newSettings: T?) {
+                    _commonSettings = null
+                    _librarySettings = null
+                }
+            })
+    }
+
     companion object {
         fun getInstance(project: Project): LanguageVersionSettingsProvider = project.service()
     }
 
+    @Volatile
+    private var _commonSettings: LanguageVersionSettings? = null
+
+    @Volatile
+    private var _librarySettings: LanguageVersionSettings? = null
+
     val commonSettings: LanguageVersionSettings
-        get() = project.cacheInvalidatingOnRootModifications {
-            computeProjectLanguageVersionSettings(useCommonFacetSettings = false)
+        get() {
+            _commonSettings?.let { return it }
+            return computeProjectLanguageVersionSettings(useCommonFacetSettings = false).also { _commonSettings = it }
         }
 
     val librarySettings: LanguageVersionSettings
-        get() = project.cacheInvalidatingOnRootModifications {
-            computeProjectLanguageVersionSettings(useCommonFacetSettings = true)
+        get() {
+            _librarySettings?.let { return it }
+            return computeProjectLanguageVersionSettings(useCommonFacetSettings = true).also { _librarySettings = it }
         }
 
     fun get(module: Module): LanguageVersionSettings {
@@ -223,4 +242,6 @@ class LanguageVersionSettingsProvider(private val project: Project) {
             set(AnalysisFlags.ideMode, true)
         }
     }
+
+    override fun dispose() {}
 }
