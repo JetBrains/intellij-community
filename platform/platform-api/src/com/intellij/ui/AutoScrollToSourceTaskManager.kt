95da2751e70df9b24e2abb84f70982d47b14db35
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2022-10-03 20:06:48 +0200

    IDEA-303044, EA-639002 [todo] async data context
    GitOrigin-RevId: 7ab19782bc800e59f47e36647a8fa7f5b9eb4e49

--- platform/platform-api/src/com/intellij/ui/AutoScrollToSourceTaskManager.kt
+++ platform/platform-api/src/com/intellij/ui/AutoScrollToSourceTaskManager.kt
@@ -1,64 +1,22 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ui
 
-import com.intellij.openapi.Disposable
-import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.EDT
-import com.intellij.openapi.application.readAction
-import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
-import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.util.await
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.util.OpenSourceUtil
-import kotlinx.coroutines.*
+import org.jetbrains.annotations.ApiStatus
 
-@Service(Service.Level.APP)
-private class AutoScrollToSourceTaskManager : Disposable {
-
-  private val scope = CoroutineScope(SupervisorJob())
-
-  init {
-    Disposer.register(ApplicationManager.getApplication(), this)
-  }
+@ApiStatus.Experimental
+interface AutoScrollToSourceTaskManager {
 
   companion object {
 
     @JvmStatic
     fun getInstance(): AutoScrollToSourceTaskManager = ApplicationManager.getApplication().service()
-
-    private fun AutoScrollToSourceHandler.canAutoScrollTo(file: VirtualFile?) =
-      file == null || isAutoScrollEnabledFor(file)
-  }
-
-  override fun dispose() {
-    scope.cancel()
   }
 
   fun scheduleScrollToSource(
     handler: AutoScrollToSourceHandler,
     dataContext: DataContext,
-  ) {
-    scope.launch(Dispatchers.EDT) {
-      PlatformDataKeys.TOOL_WINDOW.getData(dataContext)
-        ?.getReady(handler)
-        ?.await()
-
-      val navigatable = withContext(Dispatchers.IO) {
-        readAction {
-          if (handler.canAutoScrollTo(CommonDataKeys.VIRTUAL_FILE.getData(dataContext)))
-            CommonDataKeys.NAVIGATABLE_ARRAY.getData(dataContext)?.singleOrNull()
-          else
-            null
-        }
-      }
-
-      if (navigatable != null) {
-        OpenSourceUtil.navigateToSource(false, true, navigatable)
-      }
-    }
-  }
+  )
 }
