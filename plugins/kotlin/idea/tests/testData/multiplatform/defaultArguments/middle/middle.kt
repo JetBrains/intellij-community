73098967b124d0188c06db44085fd6ca3b11da3e
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-10-03 17:15:38 +0200

    [kotlin] fix highlighting testdata after NO_VALUE_FOR_PARAMETER highlighting range change
    ^KT-51740
    
    GitOrigin-RevId: d30dd5a4e67fa40633d7c284677a1a65dc17d21b

--- plugins/kotlin/idea/tests/testData/multiplatform/defaultArguments/middle/middle.kt
+++ plugins/kotlin/idea/tests/testData/multiplatform/defaultArguments/middle/middle.kt
@@ -15,21 +15,21 @@ actual fun <!LINE_MARKER("descr='Has expects in top module'")!>topLevelFunction<
 actual fun String.<!LINE_MARKER("descr='Has expects in top module'")!>topLevelExtensionFunction<!>(x: Int, y: String) = Unit
 
 fun testMiddle() {
-    A().memberFunction(<!NO_VALUE_FOR_PARAMETER!>)<!>
+    A().memberFunction<!NO_VALUE_FOR_PARAMETER!>()<!>
     A().memberFunction(42)
     A().memberFunction(42, "ok")
 
-    topLevelFunction(<!NO_VALUE_FOR_PARAMETER!>)<!>
+    topLevelFunction<!NO_VALUE_FOR_PARAMETER!>()<!>
     topLevelFunction(42)
     topLevelFunction(42, "ok")
 
-    "".topLevelExtensionFunction(<!NO_VALUE_FOR_PARAMETER!>)<!>
+    "".topLevelExtensionFunction<!NO_VALUE_FOR_PARAMETER!>()<!>
     "".topLevelExtensionFunction(42)
     "".topLevelExtensionFunction(42, "ok")
 }
 
 fun A.testMiddle() {
-    "".companionExtensionFunction(<!NO_VALUE_FOR_PARAMETER!>)<!>
+    "".companionExtensionFunction<!NO_VALUE_FOR_PARAMETER!>()<!>
     "".companionExtensionFunction(42)
     "".companionExtensionFunction(42, "ok")
 }
