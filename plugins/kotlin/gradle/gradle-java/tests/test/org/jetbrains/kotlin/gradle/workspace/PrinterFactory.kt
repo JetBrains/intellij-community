dce55ac8e4ac6e2353e0bababef3293679265527
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2022-10-04 17:19:18 +0300

    [Kotlin] KTIJ-22711 Implement workspace model printer
    (cherry picked from commit e1b8e632fc42d77261fe1261f403c2df22a7e71b)
    
    GitOrigin-RevId: c1863c5578fe920a4e0a3f8ff821dc852f7efc35

---
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/workspace/PrinterFactory.kt
@@ -0,0 +1,37 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+package org.jetbrains.kotlin.gradle.workspace
+
+class ProjectPrinterFactory {
+    private val myModulePrinterContributors = mutableListOf<ModulePrinterContributor>()
+    private val myLibraryPrinterContributors = mutableListOf<LibraryPrinterContributor>()
+    private val mySdkPrinterContributors = mutableListOf<SdkPrinterContributor>()
+
+    fun addContributor(contributor: WorkspaceModelPrinterContributor<*>) {
+        when (contributor) {
+            is ModulePrinterContributor -> myModulePrinterContributors.add(contributor)
+            is LibraryPrinterContributor -> myLibraryPrinterContributors.add(contributor)
+            is SdkPrinterContributor -> mySdkPrinterContributors.add(contributor)
+            else -> error("Unexpected contributor: ${contributor::class.qualifiedName}")
+        }
+    }
+
+    fun build(): WorkspaceModelPrinter {
+        check(myModulePrinterContributors.isNotEmpty() || myLibraryPrinterContributors.isNotEmpty() || mySdkPrinterContributors.isNotEmpty()) {
+            "Workspace model printer should have at least one contributor"
+        }
+
+        return WorkspaceModelPrinter(
+            compositeContributor(*myModulePrinterContributors.toTypedArray()),
+            compositeContributor(*myLibraryPrinterContributors.toTypedArray()),
+            compositeContributor(*mySdkPrinterContributors.toTypedArray()),
+        )
+    }
+}
+
+@Suppress("TestFunctionName")
+fun ProjectPrinter(
+    printerConfiguration: ProjectPrinterFactory.() -> Unit
+): WorkspaceModelPrinter {
+    return ProjectPrinterFactory().also(printerConfiguration).build()
+}
