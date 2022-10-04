e6ec58a3fe868113603d297cc6cce8c816115d6b
Ivan Posti <ivan.posti@jetbrains.com>
2022-10-04 15:37:55 +0200

    Do not run plugin verifier since it is broken
    GitOrigin-RevId: 6cade966b8b429119826564144a2ded9637d04aa

--- plugins/mermaid/.space.kts
+++ plugins/mermaid/.space.kts
@@ -5,11 +5,3 @@ job("Mermaid / Build") {
         }
     }
 }
-
-job("Mermaid / Run Plugin Verifier") {
-    container("openjdk:17") {
-        shellScript {
-            content = "./gradlew runPluginVerifier"
-        }
-    }
-}
