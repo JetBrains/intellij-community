5bba276dfaa578305709fb6d026bc42019f5ae16
Anže Sodja <asodja@gradle.com>
2022-10-05 18:15:11 +0200

    [kotlin] Correctly parse compiler options in IdeAssignmentContainerContributor (KTIJ-23292)
    (cherry picked from commit 2ba91d7ef24e42006256a008fa0a18772d2bc8c3)
    
    IJ-CR-96752
    
    GitOrigin-RevId: a74209578b9f9fc71a9de4ae96592aa5bca8f9e0

--- plugins/kotlin/compiler-plugins/assignment/common/src/org/jetbrains/kotlin/idea/compilerPlugin/assignment/IdeAssignmentContainerContributor.kt
+++ plugins/kotlin/compiler-plugins/assignment/common/src/org/jetbrains/kotlin/idea/compilerPlugin/assignment/IdeAssignmentContainerContributor.kt
@@ -13,6 +13,8 @@ import org.jetbrains.kotlin.container.StorageComponentContainer
 import org.jetbrains.kotlin.assignment.plugin.AssignmentPluginNames.ANNOTATION_OPTION_NAME
 import org.jetbrains.kotlin.assignment.plugin.AssignmentPluginNames.PLUGIN_ID
 import org.jetbrains.kotlin.assignment.plugin.diagnostics.AssignmentPluginDeclarationChecker
+import org.jetbrains.kotlin.cli.common.arguments.CommonCompilerArguments
+import org.jetbrains.kotlin.cli.common.arguments.parseCommandLineArguments
 import org.jetbrains.kotlin.container.useInstance
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.extensions.StorageComponentContainerContributor
@@ -21,6 +23,7 @@ import org.jetbrains.kotlin.idea.base.scripting.projectStructure.ScriptDependenc
 import org.jetbrains.kotlin.idea.base.scripting.projectStructure.ScriptModuleInfo
 import org.jetbrains.kotlin.idea.compilerPlugin.getSpecialAnnotations
 import org.jetbrains.kotlin.platform.TargetPlatform
+import org.jetbrains.kotlin.scripting.definitions.ScriptDefinition
 
 class IdeAssignmentContainerContributor(private val project: Project) : StorageComponentContainerContributor {
 
@@ -28,7 +31,7 @@ class IdeAssignmentContainerContributor(private val project: Project) : StorageC
         const val ANNOTATION_OPTION_PREFIX = "plugin:$PLUGIN_ID:$ANNOTATION_OPTION_NAME="
     }
 
-    private val cache =
+    private val moduleCache =
         CachedValuesManager.getManager(project).createCachedValue(
             {
                 CachedValueProvider.Result.create(
@@ -40,9 +43,17 @@ class IdeAssignmentContainerContributor(private val project: Project) : StorageC
             }, false
         )
 
-    private fun getAnnotationsForModule(module: Module): List<String> {
-        return cache.value.getOrPut(module) { module.getSpecialAnnotations(ANNOTATION_OPTION_PREFIX) }
-    }
+    private val scriptCache =
+        CachedValuesManager.getManager(project).createCachedValue(
+            {
+                CachedValueProvider.Result.create(
+                    ContainerUtil.createConcurrentWeakMap<String, List<String>>(),
+                    ProjectRootModificationTracker.getInstance(
+                        project
+                    )
+                )
+            }, false
+        )
 
     override fun registerModuleComponents(
         container: StorageComponentContainer,
@@ -51,11 +62,28 @@ class IdeAssignmentContainerContributor(private val project: Project) : StorageC
     ) {
         val annotations =
             when (val moduleInfo = moduleDescriptor.getCapability(ModuleInfo.Capability)) {
-                is ScriptModuleInfo -> moduleInfo.scriptDefinition.annotationsForSamWithReceivers
-                is ScriptDependenciesInfo.ForFile -> moduleInfo.scriptDefinition.annotationsForSamWithReceivers
+                is ScriptModuleInfo -> getAnnotationsForScriptDefinition(moduleInfo.scriptDefinition)
+                is ScriptDependenciesInfo.ForFile -> getAnnotationsForScriptDefinition(moduleInfo.scriptDefinition)
                 is ModuleProductionSourceInfo -> getAnnotationsForModule(moduleInfo.module)
                 else -> null
             } ?: return
         container.useInstance(AssignmentPluginDeclarationChecker(annotations))
+    }
+
+    private fun getAnnotationsForScriptDefinition(scriptDefinition: ScriptDefinition): List<String> {
+        return scriptCache.value.getOrPut(scriptDefinition.definitionId) { scriptDefinition.getSpecialAnnotations() }
+    }
+
+    private fun ScriptDefinition.getSpecialAnnotations(): List<String> {
+        val arguments = object : CommonCompilerArguments() {}
+        parseCommandLineArguments(compilerOptions.toList(), arguments)
+        return arguments.pluginOptions
+            ?.filter { it.startsWith(ANNOTATION_OPTION_PREFIX) }
+            ?.map { it.substring(ANNOTATION_OPTION_PREFIX.length) }
+            ?: emptyList()
+    }
+
+    private fun getAnnotationsForModule(module: Module): List<String> {
+        return moduleCache.value.getOrPut(module) { module.getSpecialAnnotations(ANNOTATION_OPTION_PREFIX) }
     }
 }
