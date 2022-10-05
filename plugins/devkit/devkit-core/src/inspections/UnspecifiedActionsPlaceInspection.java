497ddb21c03503d72aecd5283d6cc171e2cd30c3
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2022-10-05 16:05:46 +0200

    IDEA-189985 - UnspecifiedActionsPlaceInspection - extract methods for clarity
    GitOrigin-RevId: a73ec7bba6ca53eac85a5d6f67cbe2a69c60966e

--- plugins/devkit/devkit-core/src/inspections/UnspecifiedActionsPlaceInspection.java
+++ plugins/devkit/devkit-core/src/inspections/UnspecifiedActionsPlaceInspection.java
@@ -18,30 +18,42 @@ public class UnspecifiedActionsPlaceInspection extends DevKitInspectionBase {
     return new JavaElementVisitor() {
       @Override
       public void visitMethodCallExpression(@NotNull PsiMethodCallExpression expression) {
-        PsiMethod method = expression.resolveMethod();
-        if (method != null && "createActionToolbar".equals(method.getName())) {
-          PsiClass aClass = method.getContainingClass();
-          if (aClass != null && ActionManager.class.getName().equals(aClass.getQualifiedName())) {
-            PsiParameter[] parameters = method.getParameterList().getParameters();
-            if (parameters.length > 0 &&  parameters[0].getType() instanceof PsiClassType) {
-              PsiType type = parameters[0].getType();
-              //first check doesn't require resolve
-              if (Objects.equals(((PsiClassType)type).getClassName(), CommonClassNames.JAVA_LANG_STRING_SHORT)
-                  && CommonClassNames.JAVA_LANG_STRING.equals(type.getCanonicalText(false))) {
-                PsiExpression[] expressions = expression.getArgumentList().getExpressions();
-                if (expressions.length > 0) {
-                  String text = expressions[0].getText();
-                  if (text.equals("\"\"") || text.endsWith(".UNKNOWN")) {
-                    holder.registerProblem(expressions[0], DevKitBundle.message("inspections.unspecified.actions.place.toolbar"));
-                  }
-                }
-              }
-            }
+        if (requiresSpecifiedActionPlace(expression)) {
+          PsiExpression[] expressions = expression.getArgumentList().getExpressions();
+          if (expressions.length > 0 && actionPlaceIsUnspecified(expressions[0])) {
+            holder.registerProblem(expressions[0], DevKitBundle.message("inspections.unspecified.actions.place.toolbar"));
           }
         }
         super.visitMethodCallExpression(expression);
       }
     };
+  }
+
+  private static boolean requiresSpecifiedActionPlace(@NotNull PsiMethodCallExpression expression) {
+    PsiMethod method = expression.resolveMethod();
+    if (method != null && "createActionToolbar".equals(method.getName())) {
+      PsiClass aClass = method.getContainingClass();
+      if (aClass != null && ActionManager.class.getName().equals(aClass.getQualifiedName())) {
+        PsiParameter[] parameters = method.getParameterList().getParameters();
+        if (parameters.length > 0 && parameters[0].getType() instanceof PsiClassType) {
+          PsiType type = parameters[0].getType();
+          //first check doesn't require resolve
+          if (Objects.equals(((PsiClassType)type).getClassName(), CommonClassNames.JAVA_LANG_STRING_SHORT)
+              && CommonClassNames.JAVA_LANG_STRING.equals(type.getCanonicalText(false))) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  private static boolean actionPlaceIsUnspecified(PsiExpression placeArgument) {
+    String text = placeArgument.getText();
+    if (text.equals("\"\"") || text.endsWith(".UNKNOWN")) {
+      return true;
+    }
+    return false;
   }
 
   @NotNull
