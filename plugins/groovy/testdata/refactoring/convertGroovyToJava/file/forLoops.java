e41b7b161f6c3228853884a8c397884af8f0b90f
Bas Leijdekkers <basleijdekkers@gmail.com>
2022-10-05 19:45:07 +0200

    [groovy] Convert to Java: add explanation for commented out test data (IJ-CR-96105)
    GitOrigin-RevId: dcf0c9dcd90724dc0ae0d55d2be6e5de3cb0d5c6

--- plugins/groovy/testdata/refactoring/convertGroovyToJava/file/forLoops.java
+++ plugins/groovy/testdata/refactoring/convertGroovyToJava/file/forLoops.java
@@ -2,7 +2,10 @@ public class A {
 public void a() {
 for(int i = 0;i < 10;i++){}
 
-//for (int i = 0, j = 0; i < 10; i++, j++) {}
+
+// multiple loop variables not yet supported
+// for (int i = 0, j = 0; i < 10; i++, j++) {}
+// for (def i = 0, b = true; i < 10; i++, b = !b) {}
 }
 
 }
