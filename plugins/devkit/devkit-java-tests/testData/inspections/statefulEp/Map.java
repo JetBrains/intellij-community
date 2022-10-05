8ef6e27839129f2f2be463e331a8d0f3a558ae4a
Kirill Smelov <kirill.smelov@jetbrains.com>
2022-10-05 11:15:53 +0200

    IJ-CR-95222 Add testdata
    GitOrigin-RevId: e27404aae9ffba6b130397df05f818877082d7af

---
+++ plugins/devkit/devkit-java-tests/testData/inspections/statefulEp/Map.java
@@ -0,0 +1,10 @@
+public class Map extends com.intellij.codeInspection.LocalQuickFix {
+  java.util.Map<com.intellij.psi.PsiElement, com.intellij.psi.PsiElement> <warning descr="Potential memory leak: don't hold PsiElement, use SmartPsiElementPointer instead See also LocalQuickFixOnPsiElement">pe</warning>;
+  java.util.Map<com.intellij.psi.PsiReference, com.intellij.psi.PsiElement> <warning descr="Potential memory leak: don't hold PsiElement, use SmartPsiElementPointer instead See also LocalQuickFixOnPsiElement">r</warning>;
+  java.util.Map<java.util.Map<com.intellij.psi.PsiReference, com.intellij.psi.PsiReference>, com.intellij.psi.PsiReference> <warning descr="Do not use PsiReference as a field in quick fix">r1</warning>;
+  java.util.Map r2;
+  java.util.Map<Integer, Long> r3;
+  java.util.Collection<java.util.Map<com.intellij.psi.PsiElement, com.intellij.psi.PsiElement>> <warning descr="Potential memory leak: don't hold PsiElement, use SmartPsiElementPointer instead See also LocalQuickFixOnPsiElement">r4</warning>;
+  java.util.Map<com.intellij.psi.PsiElement, Long> <warning descr="Potential memory leak: don't hold PsiElement, use SmartPsiElementPointer instead See also LocalQuickFixOnPsiElement">r7</warning>;
+  java.util.Map<Integer, com.intellij.psi.PsiElement> <warning descr="Potential memory leak: don't hold PsiElement, use SmartPsiElementPointer instead See also LocalQuickFixOnPsiElement">r8</warning>;
+}
