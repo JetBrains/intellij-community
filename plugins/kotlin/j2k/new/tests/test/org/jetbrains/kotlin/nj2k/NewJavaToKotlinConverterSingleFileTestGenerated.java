a5cd341c3ef0f67e83258729eae6b03473b24b8a
Anastasia.Nekrasova <Anastasia.Nekrasova@jetbrains.com>
2022-10-05 19:57:18 +0300

    [kotlin] J2K: Convert Arrays.asList/List.of/Set.of to listOf/setOf
    KTIJ-12411 Fixed
    
    GitOrigin-RevId: 068178436deda4cec7f3f8bfefbe32a3a26245ee

--- plugins/kotlin/j2k/new/tests/test/org/jetbrains/kotlin/nj2k/NewJavaToKotlinConverterSingleFileTestGenerated.java
+++ plugins/kotlin/j2k/new/tests/test/org/jetbrains/kotlin/nj2k/NewJavaToKotlinConverterSingleFileTestGenerated.java
@@ -1103,6 +1103,34 @@ public abstract class NewJavaToKotlinConverterSingleFileTestGenerated extends Ab
     }
 
     @RunWith(JUnit3RunnerWithInners.class)
+    @TestMetadata("testData/newJ2k/collections")
+    public static class Collections extends AbstractNewJavaToKotlinConverterSingleFileTest {
+        private void runTest(String testDataFilePath) throws Exception {
+            KotlinTestUtils.runTest(this::doTest, this, testDataFilePath);
+        }
+
+        @TestMetadata("common.java")
+        public void testCommon() throws Exception {
+            runTest("testData/newJ2k/collections/common.java");
+        }
+
+        @TestMetadata("listRestriction.java")
+        public void testListRestriction() throws Exception {
+            runTest("testData/newJ2k/collections/listRestriction.java");
+        }
+
+        @TestMetadata("literalRestriction.java")
+        public void testLiteralRestriction() throws Exception {
+            runTest("testData/newJ2k/collections/literalRestriction.java");
+        }
+
+        @TestMetadata("setRestriction.java")
+        public void testSetRestriction() throws Exception {
+            runTest("testData/newJ2k/collections/setRestriction.java");
+        }
+    }
+
+    @RunWith(JUnit3RunnerWithInners.class)
     @TestMetadata("testData/newJ2k/comments")
     public static class Comments extends AbstractNewJavaToKotlinConverterSingleFileTest {
         private void runTest(String testDataFilePath) throws Exception {
