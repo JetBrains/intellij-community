a096835a86443c5fa4021a219e9d0388b1367165
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2022-10-06 17:59:09 +0300

    [platform] IDEA-302650 Implement Windows and Unix mouse drag selection
    On Windows, pretend the mouse is always moving horizontally.
    
    On Unix (Linux and Mac included), if the mouse is moved above or below
    the source component, we need to quickly select to the beginning or
    the end. We do it by imitating horizontal movement far away from the
    component, using coordinates very far away to the left/right.
    
    GitOrigin-RevId: 5379a7f57886d2d2064293a57f0bf35b91092ec3

--- platform/platform-impl/src/com/intellij/ide/ui/laf/MouseDragSelectionEventHandler.kt
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/MouseDragSelectionEventHandler.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.ui.laf
 
+import com.intellij.openapi.util.SystemInfo
 import java.awt.Component
 import java.awt.event.MouseEvent
 
@@ -12,24 +13,47 @@ internal class MouseDragSelectionEventHandler(private val mouseDraggedOriginal: 
     when (dragSelectionMode()) {
       DragSelectionMode.ORIGINAL -> mouseDraggedOriginal(e)
       DragSelectionMode.WINDOWS -> mouseDraggedWindows(e)
+      DragSelectionMode.UNIX -> mouseDraggedUnix(e)
     }
   }
 
   private fun dragSelectionMode(): DragSelectionMode =
     when {
-      isNativeSelectionEnabled -> DragSelectionMode.WINDOWS
-      else -> DragSelectionMode.ORIGINAL
+      !isNativeSelectionEnabled -> DragSelectionMode.ORIGINAL
+      SystemInfo.isWindows -> DragSelectionMode.WINDOWS
+      else -> DragSelectionMode.UNIX
     }
 
   private enum class DragSelectionMode {
     ORIGINAL,
-    WINDOWS
+    WINDOWS,
+    UNIX
   }
 
   private fun mouseDraggedWindows(e: MouseEvent) {
+    // Pretend the mouse always moves horizontally,
+    // as this is what Windows users generally expect.
     mouseDragged(e, e.x, e.sourceHeight?.div(2) ?: e.y)
   }
 
+  private fun mouseDraggedUnix(e: MouseEvent) {
+    val height = e.sourceHeight
+    val width = e.sourceWidth
+    if (height == null || width == null) { // A next-to-impossible scenario, fall back to original behavior.
+      mouseDraggedOriginal(e)
+      return
+    }
+    // In case of the mouse cursor moved above or below the component,
+    // imitate fast selection by moving the mouse cursor far away horizontally either left or right.
+    val normalizedY = height / 2
+    when {
+      e.y < 0 -> mouseDragged(e, -OMG_ITS_OVER_9000, normalizedY)
+      e.y > height -> mouseDragged(e, width + OMG_ITS_OVER_9000, normalizedY)
+      else -> mouseDragged(e, e.x, e.y)
+    }
+  }
+
+  private val MouseEvent.sourceWidth: Int? get() = (source as? Component)?.width
   private val MouseEvent.sourceHeight: Int? get() = (source as? Component)?.height
 
   private fun mouseDragged(e: MouseEvent, x: Int, y: Int) {
@@ -45,3 +69,5 @@ internal class MouseDragSelectionEventHandler(private val mouseDraggedOriginal: 
   }
 
 }
+
+private const val OMG_ITS_OVER_9000 = 9999
