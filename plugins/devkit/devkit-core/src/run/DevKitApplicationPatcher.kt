74b867f35f72eb8fa5304c6fd9ad246a0972389e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-10-07 12:00:12 +0200

    DevKitApplicationPatcher - support dev build
    GitOrigin-RevId: 7963895134b00522e0cc2b2620f81fe96a7599dd

--- plugins/devkit/devkit-core/src/run/DevKitApplicationPatcher.kt
+++ plugins/devkit/devkit-core/src/run/DevKitApplicationPatcher.kt
@@ -1,50 +1,91 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.idea.devkit.run;
+package org.jetbrains.idea.devkit.run
 
-import com.intellij.execution.RunConfigurationExtension;
-import com.intellij.execution.application.ApplicationConfiguration;
-import com.intellij.execution.configurations.JavaParameters;
-import com.intellij.execution.configurations.ParametersList;
-import com.intellij.execution.configurations.RunConfigurationBase;
-import com.intellij.execution.configurations.RunnerSettings;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.projectRoots.Sdk;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.idea.devkit.util.PsiUtil;
+import com.intellij.execution.RunConfigurationExtension
+import com.intellij.execution.application.ApplicationConfiguration
+import com.intellij.execution.configurations.JavaParameters
+import com.intellij.execution.configurations.RunConfigurationBase
+import com.intellij.execution.configurations.RunnerSettings
+import com.intellij.openapi.util.io.FileUtilRt
+import com.intellij.util.PlatformUtils
+import org.jetbrains.idea.devkit.util.PsiUtil
+import java.util.*
 
-public class DevKitApplicationPatcher extends RunConfigurationExtension {
-  @Override
-  public <T extends RunConfigurationBase<?>> void updateJavaParameters(@NotNull T configuration,
-                                                                       @NotNull JavaParameters javaParameters,
-                                                                       RunnerSettings runnerSettings) {
-    ParametersList vmParametersList = javaParameters.getVMParametersList();
-    Project project = configuration.getProject();
-    if (PsiUtil.isIdeaProject(project) && 
-        !vmParametersList.getList().contains("--add-modules") && 
-        "com.intellij.idea.Main".equals(((ApplicationConfiguration)configuration).getMainClassName())) {
-      Module module = ((ApplicationConfiguration)configuration).getConfigurationModule().getModule();
+internal class DevKitApplicationPatcher : RunConfigurationExtension() {
+  @Suppress("SpellCheckingInspection")
+  override fun <T : RunConfigurationBase<*>> updateJavaParameters(configuration: T,
+                                                                  javaParameters: JavaParameters,
+                                                                  runnerSettings: RunnerSettings?) {
+    val project = (configuration as ApplicationConfiguration).project
+    if (!PsiUtil.isIdeaProject(project)) {
+      return
+    }
 
-      Sdk jdk = JavaParameters.getJdkToRunModule(module, true);
-      if (jdk == null) {
-        return;
-      }
+    val vmParameters = javaParameters.vmParametersList
+    val isDev = configuration.mainClassName == "org.jetbrains.intellij.build.devServer.DevMainKt"
+    val vmParametersAsList = vmParameters.list
+    if (vmParametersAsList.contains("--add-modules") || (!isDev && configuration.mainClassName != "com.intellij.idea.Main")) {
+      return
+    }
 
-      if (!vmParametersList.hasProperty(JUnitDevKitPatcher.SYSTEM_CL_PROPERTY)) {
-        String qualifiedName = "com.intellij.util.lang.PathClassLoader";
-        if (JUnitDevKitPatcher.loaderValid(project, module, qualifiedName)) {
-          vmParametersList.addProperty (JUnitDevKitPatcher.SYSTEM_CL_PROPERTY, qualifiedName);
-        }
-      }
-
-      if (!vmParametersList.getList().contains("--add-opens")) {
-        JUnitDevKitPatcher.appendAddOpensWhenNeeded(jdk, vmParametersList);
+    val module = configuration.configurationModule.module
+    val jdk = JavaParameters.getJdkToRunModule(module, true) ?: return
+    if (!vmParameters.hasProperty(JUnitDevKitPatcher.SYSTEM_CL_PROPERTY)) {
+      val qualifiedName = "com.intellij.util.lang.PathClassLoader"
+      if (JUnitDevKitPatcher.loaderValid(project, module, qualifiedName)) {
+        vmParameters.addProperty(JUnitDevKitPatcher.SYSTEM_CL_PROPERTY, qualifiedName)
       }
     }
+
+    if (!vmParametersAsList.contains("--add-opens")) {
+      JUnitDevKitPatcher.appendAddOpensWhenNeeded(jdk, vmParameters)
+    }
+
+    if (!isDev) {
+      return
+    }
+
+    if (vmParametersAsList.none { it.startsWith("-Xmx") }) {
+      vmParameters.add("-Xmx2g")
+    }
+    if (vmParametersAsList.none { it.startsWith("-XX:ReservedCodeCacheSize") }) {
+      vmParameters.add("-XX:ReservedCodeCacheSize=240m")
+    }
+    vmParameters.addAll(
+      "-XX:+UseG1GC",
+      "-XX:SoftRefLRUPolicyMSPerMB=50",
+      "-XX:MaxJavaStackTraceDepth=10000",
+      "-ea",
+      "-XX:CICompilerCount=2",
+      "-XX:PrintIdealGraphLevel=3"
+    )
+
+    if (!vmParameters.hasProperty("idea.config.path")) {
+      var productClassifier = vmParameters.getPropertyValue("idea.platform.prefix")
+      productClassifier = when (productClassifier) {
+        null -> "idea"
+        PlatformUtils.IDEA_CE_PREFIX -> "idea-community"
+        else -> productClassifier.lowercase()
+      }
+      vmParameters.addProperty("idea.config.path",
+                               FileUtilRt.toSystemIndependentName("${configuration.workingDirectory}/out/dev-data/$productClassifier/config"))
+      vmParameters.addProperty("idea.system.path",
+                               FileUtilRt.toSystemIndependentName("${configuration.workingDirectory}/out/dev-data/$productClassifier/system"))
+    }
+
+    vmParameters.addProperty("sun.io.useCanonCaches", "false")
+    vmParameters.addProperty("sun.awt.disablegrab", "true")
+    vmParameters.addProperty("sun.java2d.metal", "true")
+    vmParameters.addProperty("idea.debug.mode", "true")
+    vmParameters.addProperty("idea.is.internal", "true")
+    vmParameters.addProperty("fus.internal.test.mode", "true")
+    vmParameters.addProperty("jbScreenMenuBar.enabled", "true")
+    vmParameters.addProperty("apple.laf.useScreenMenuBar", "true")
+    vmParameters.addProperty("jdk.attach.allowAttachSelf")
+    vmParameters.addProperty("idea.initially.ask.config", "true")
   }
 
-  @Override
-  public boolean isApplicableFor(@NotNull RunConfigurationBase<?> configuration) {
-    return configuration instanceof ApplicationConfiguration;
+  override fun isApplicableFor(configuration: RunConfigurationBase<*>): Boolean {
+    return configuration is ApplicationConfiguration
   }
 }
