f2d5bab3d7b06bd361e7179d80102b29e7212b1c
Bas Leijdekkers <basleijdekkers@gmail.com>
2022-10-07 11:34:24 +0200

    Use common quick fix bundle
    GitOrigin-RevId: 33d6956edc802a2133c7f91152bf3b04eff9ac11

--- java/java-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/InsertConstructorCallFix.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/InsertConstructorCallFix.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.daemon.impl.quickfix;
 
 import com.intellij.codeInsight.daemon.QuickFixBundle;
@@ -20,6 +6,7 @@ import com.intellij.codeInsight.daemon.impl.actions.IntentionActionWithFixAllOpt
 import com.intellij.codeInsight.intention.FileModifier;
 import com.intellij.codeInsight.intention.HighPriorityAction;
 import com.intellij.codeInsight.intention.impl.BaseIntentionAction;
+import com.intellij.codeInspection.CommonQuickFixBundle;
 import com.intellij.openapi.command.undo.UndoUtil;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
@@ -50,7 +37,7 @@ public class InsertConstructorCallFix implements IntentionActionWithFixAllOption
   @Override
   @NotNull
   public String getText() {
-    return QuickFixBundle.message("insert.super.constructor.call.text", myCall);
+    return CommonQuickFixBundle.message("fix.insert.x", myCall);
   }
 
   @Override
