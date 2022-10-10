af14f8018ddefa1b4ce58e702a982ec3953e0e81
Nikita Bobko <nikita.bobko@jetbrains.com>
2022-10-10 14:08:22 +0200

    [kotlin] Implement ReorderParametersFix
    ^KTIJ-23209 Fixed
    Review: https://jetbrains.team/p/ij/reviews/96392
    
    Note on `allowResolveInWriteAction.txt`
    
    I'm forced to add ReorderParametersFix to
    `allowResolveInWriteAction.txt` because of `runChangeSignature`. If you
    look at all other quickfixes that use `runChangeSignature` then you will
    discover that they are mentioned in `allowResolveInWriteAction.txt` too
    (e.g. `AddFunctionParametersFix` is mentioned as
    `QuickFixWithDelegateFactory` because its factory is implemented as
    `KotlinSingleIntentionActionFactoryWithDelegate`).
    
    I tried to refactor `runChangeSignature` but ended up in Kotlin light
    classes. And it was Java who called Kotlin's `getType` via light classes
    which ended up in resolve on EDT thread.
    
    (cherry-picked from master branch)
    
    GitOrigin-RevId: c46e9702939e6a22e8b1585a5125810c7690996c

---
+++ plugins/kotlin/base/util/src/org/jetbrains/kotlin/util/CollectionUtils.kt
@@ -0,0 +1,33 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.util
+
+import java.util.*
+
+inline fun <T, R> Collection<T>.mapAll(transform: (T) -> R?): List<R>? {
+    val result = ArrayList<R>(this.size)
+    for (item in this) {
+        result += transform(item) ?: return null
+    }
+    return result
+}
+
+fun <K, V> merge(vararg maps: Map<K, V>?): Map<K, V> {
+    val result = mutableMapOf<K, V>()
+    for (map in maps) {
+        if (map != null) {
+            result.putAll(map)
+        }
+    }
+    return result
+}
+
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T> Sequence<*>.takeWhileIsInstance(): Sequence<T> = takeWhile { it is T } as Sequence<T>
+
+/**
+ * Sorted by [selector] or preserves the order for elements where [selector] returns the same result
+ */
+fun <T, R : Comparable<R>> Sequence<T>.sortedConservativelyBy(selector: (T) -> R?): Sequence<T> =
+    withIndex()
+        .sortedWith(compareBy({ (_, value) -> selector(value) }, IndexedValue<T>::index))
+        .map(IndexedValue<T>::value)
--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
@@ -310,6 +310,9 @@ class QuickFixRegistrar : QuickFixContributor {
         UNCHECKED_CAST.registerFactory(ChangeToStarProjectionFix)
         CANNOT_CHECK_FOR_ERASED.registerFactory(ChangeToStarProjectionFix)
 
+        UNINITIALIZED_PARAMETER.registerFactory(ReorderParametersFix)
+        UNINITIALIZED_PARAMETER_WARNING.registerFactory(ReorderParametersFix)
+
         INACCESSIBLE_OUTER_CLASS_EXPRESSION.registerFactory(AddModifierFixFE10.createFactory(INNER_KEYWORD, KtClass::class.java))
 
         FINAL_SUPERTYPE.registerFactory(AddModifierFixFE10.MakeClassOpenFactory)
