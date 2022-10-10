adf9ef308ad6987a746d21c73af4a5b56804668d
Nikita Bobko <nikita.bobko@jetbrains.com>
2022-10-10 14:08:22 +0200

    [kotlin] Implement ReorderParametersFix
    ^KTIJ-23209 Fixed
    Review: https://jetbrains.team/p/ij/reviews/96392
    
    Note on `allowResolveInWriteAction.txt`
    
    I'm forced to add ReorderParametersFix to
    `allowResolveInWriteAction.txt` because of `runChangeSignature`. If you
    look at all other quickfixes that use `runChangeSignature` then you will
    discover that they are mentioned in `allowResolveInWriteAction.txt` too
    (e.g. `AddFunctionParametersFix` is mentioned as
    `QuickFixWithDelegateFactory` because its factory is implemented as
    `KotlinSingleIntentionActionFactoryWithDelegate`).
    
    I tried to refactor `runChangeSignature` but ended up in Kotlin light
    classes. And it was Java who called Kotlin's `getType` via light classes
    which ended up in resolve on EDT thread.
    
    (cherry-picked from master branch)
    
    GitOrigin-RevId: 6e28cc73957269592192c9d857f8f16bb0e8c722

---
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/base/psi/KotlinPsiUtils.kt
@@ -0,0 +1,216 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+@file:JvmName("KotlinPsiUtils")
+
+package org.jetbrains.kotlin.idea.base.psi
+
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.util.parentsOfType
+import com.intellij.util.asSafely
+import com.intellij.util.text.CharArrayUtil
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.name.CallableId
+import org.jetbrains.kotlin.name.ClassId
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.containingClass
+import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
+import org.jetbrains.kotlin.psi.psiUtil.hasExpectModifier
+import org.jetbrains.kotlin.psi.psiUtil.isTopLevelInFileOrScript
+
+val KtClassOrObject.classIdIfNonLocal: ClassId?
+    get() {
+        if (KtPsiUtil.isLocal(this)) return null
+        val packageName = containingKtFile.packageFqName
+        val classesNames = parentsOfType<KtDeclaration>().map { it.name }.toList().asReversed()
+        if (classesNames.any { it == null }) return null
+        return ClassId(packageName, FqName(classesNames.joinToString(separator = ".")), /*local=*/false)
+    }
+
+val KtCallableDeclaration.callableIdIfNotLocal: CallableId?
+    get() {
+        val callableName = this.nameAsName ?: return null
+        if (isTopLevelInFileOrScript(this)) {
+            return CallableId(containingKtFile.packageFqName, callableName)
+        }
+
+        val classId = containingClassOrObject?.classIdIfNonLocal ?: return null
+        return CallableId(classId, callableName)
+    }
+
+fun getElementAtOffsetIgnoreWhitespaceBefore(file: PsiFile, offset: Int): PsiElement? {
+    val element = file.findElementAt(offset)
+    if (element is PsiWhiteSpace) {
+        return file.findElementAt(element.getTextRange().endOffset)
+    }
+    return element
+}
+
+fun getElementAtOffsetIgnoreWhitespaceAfter(file: PsiFile, offset: Int): PsiElement? {
+    val element = file.findElementAt(offset - 1)
+    if (element is PsiWhiteSpace) {
+        return file.findElementAt(element.getTextRange().startOffset - 1)
+    }
+    return element
+}
+
+fun getStartLineOffset(file: PsiFile, line: Int): Int? {
+    val document = PsiDocumentManager.getInstance(file.project).getDocument(file) ?: return null
+    if (line >= document.lineCount) {
+        return null
+    }
+
+    val lineStartOffset = document.getLineStartOffset(line)
+    return CharArrayUtil.shiftForward(document.charsSequence, lineStartOffset, " \t")
+}
+
+fun getEndLineOffset(file: PsiFile, line: Int): Int? {
+    val document = PsiDocumentManager.getInstance(file.project).getDocument(file) ?: return null
+    if (line >= document.lineCount) {
+        return null
+    }
+
+    val lineStartOffset = document.getLineEndOffset(line)
+    return CharArrayUtil.shiftBackward(document.charsSequence, lineStartOffset, " \t")
+}
+
+fun getTopmostElementAtOffset(element: PsiElement, offset: Int): PsiElement {
+    var node = element
+    do {
+        val parent = node.parent
+        if (parent == null || !parent.isSuitableTopmostElementAtOffset(offset)) {
+            break
+        }
+        node = parent
+    } while (true)
+
+    return node
+}
+
+fun getTopParentWithEndOffset(element: PsiElement, stopAt: Class<*>): PsiElement {
+    var node = element
+    val endOffset = node.textOffset + node.textLength
+    do {
+        val parent = node.parent ?: break
+        if (parent.textOffset + parent.textLength != endOffset) {
+            break
+        }
+
+        node = parent
+        if (stopAt.isInstance(node)) {
+            break
+        }
+    } while (true)
+
+    return node
+}
+
+@SafeVarargs
+@Suppress("UNCHECKED_CAST")
+fun <T> getTopmostElementAtOffset(element: PsiElement, offset: Int, vararg classes: Class<out T>): T? {
+    var node = element
+    var lastElementOfType: T? = null
+    if (classes.anyIsInstance(node)) {
+        lastElementOfType = node as? T
+    }
+
+    do {
+        val parent = node.parent
+        if (parent == null || !parent.isSuitableTopmostElementAtOffset(offset)) {
+            break
+        }
+        if (classes.anyIsInstance(parent)) {
+            lastElementOfType = parent as? T
+        }
+        node = parent
+    } while (true)
+
+    return lastElementOfType
+}
+
+private fun <T> Array<out Class<out T>>.anyIsInstance(element: PsiElement): Boolean =
+    any { it.isInstance(element) }
+
+private fun PsiElement.isSuitableTopmostElementAtOffset(offset: Int): Boolean =
+    textOffset >= offset && this !is KtBlockExpression && this !is PsiFile
+
+
+fun KtExpression.safeDeparenthesize(): KtExpression = KtPsiUtil.safeDeparenthesize(this)
+
+fun KtDeclaration.isExpectDeclaration(): Boolean =
+    when {
+        hasExpectModifier() -> true
+        else -> containingClassOrObject?.isExpectDeclaration() == true
+    }
+
+fun KtPropertyAccessor.deleteBody() {
+    val leftParenthesis = leftParenthesis ?: return
+    deleteChildRange(leftParenthesis, lastChild)
+}
+
+fun KtDeclarationWithBody.singleExpressionBody(): KtExpression? =
+    when (val body = bodyExpression) {
+        is KtBlockExpression -> body.statements.singleOrNull()?.asSafely<KtReturnExpression>()?.returnedExpression
+        else -> body
+    }
+
+fun KtNamedDeclaration.isConstructorDeclaredProperty(): Boolean =
+    this is KtParameter && ownerFunction is KtPrimaryConstructor && hasValOrVar()
+
+fun KtExpression.getCallChain(): List<KtExpression> =
+    generateSequence(this) { (it as? KtDotQualifiedExpression)?.receiverExpression }
+        .map { (it as? KtDotQualifiedExpression)?.selectorExpression ?: it }
+        .toList()
+        .reversed()
+
+fun KtCallExpression.getContainingValueArgument(expression: KtExpression): KtValueArgument? {
+    fun KtElement.deparenthesizeStructurally(): KtElement? {
+        val deparenthesized = if (this is KtExpression) KtPsiUtil.deparenthesizeOnce(this) else this
+        return when {
+            deparenthesized != this -> deparenthesized
+            this is KtLambdaExpression -> this.functionLiteral
+            this is KtFunctionLiteral -> this.bodyExpression
+            else -> null
+        }
+    }
+
+    for (valueArgument in valueArguments) {
+        val argumentExpression = valueArgument.getArgumentExpression() ?: continue
+        val candidates = generateSequence<KtElement>(argumentExpression) { it.deparenthesizeStructurally() }
+        if (expression in candidates) {
+            return valueArgument
+        }
+    }
+
+    return null
+}
+
+fun KtClass.mustHaveNonEmptyPrimaryConstructor(): Boolean =
+    isData() || isInlineOrValue()
+
+fun KtClass.mustHaveOnlyPropertiesInPrimaryConstructor(): Boolean =
+    isData() || isAnnotation() || isInlineOrValue()
+
+fun KtClass.mustHaveOnlyValPropertiesInPrimaryConstructor(): Boolean =
+    isAnnotation() || isInlineOrValue()
+
+fun KtClass.isInlineOrValue(): Boolean =
+    isInline() || isValue()
+
+fun KtModifierListOwner.hasInlineModifier(): Boolean =
+    hasModifier(KtTokens.INLINE_KEYWORD)
+
+fun KtPrimaryConstructor.mustHaveValOrVar(): Boolean =
+    containingClass()?.mustHaveOnlyPropertiesInPrimaryConstructor() ?: false
+
+fun PsiElement.childrenDfsSequence(): Sequence<PsiElement> =
+    sequence {
+        suspend fun SequenceScope<PsiElement>.visit(element: PsiElement) {
+            element.children.forEach { visit(it) }
+            yield(element)
+        }
+        visit(this@childrenDfsSequence)
+    }
