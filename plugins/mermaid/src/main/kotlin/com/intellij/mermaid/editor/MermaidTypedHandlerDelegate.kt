db79d8af55970e38d119f36c31ed143d2f75ef11
Berezhko Ekaterina <berezhko.ei@gmail.com>
2022-10-11 18:02:56 +0300

    Change tilda typed handler
    GitOrigin-RevId: 0c63722d36363affde1194ebcb79c6e4b0c6d138

--- plugins/mermaid/src/main/kotlin/com/intellij/mermaid/editor/MermaidTypedHandlerDelegate.kt
+++ plugins/mermaid/src/main/kotlin/com/intellij/mermaid/editor/MermaidTypedHandlerDelegate.kt
@@ -1,7 +1,11 @@
 package com.intellij.mermaid.editor
 
-import com.intellij.mermaid.lang.psi.MermaidFile
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
+import com.intellij.mermaid.lang.lexer.MermaidTokens
+import com.intellij.mermaid.lang.psi.MermaidAttribute
+import com.intellij.mermaid.lang.psi.MermaidClassStatement
+import com.intellij.mermaid.lang.psi.MermaidFile
+import com.intellij.mermaid.lang.psi.MermaidIdentifier
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.ScrollType
@@ -9,7 +13,11 @@ import com.intellij.openapi.editor.actionSystem.TypedAction
 import com.intellij.openapi.editor.impl.TypedActionImpl
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.TokenType
+import com.intellij.psi.tree.TokenSet
+import com.intellij.psi.util.elementType
 
 class MermaidTypedHandlerDelegate : TypedHandlerDelegate() {
   override fun charTyped(c: Char, project: Project, editor: Editor, file: PsiFile): Result {
@@ -19,27 +27,42 @@ class MermaidTypedHandlerDelegate : TypedHandlerDelegate() {
     val offset = caretModel.offset
 
     val document = editor.document
-    val typedAction = TypedAction.getInstance() as TypedActionImpl
 
     if (c == '<' && offset - 2 >= 0 && document.getText(TextRange(offset - 2, offset)) == "<<") {
-      runWriteAction {
-        typedAction.defaultRawTypedHandler.beginUndoablePostProcessing()
-        document.insertString(offset, ">>")
-        caretModel.moveToOffset(offset)
-        editor.scrollingModel.scrollToCaret(ScrollType.RELATIVE)
-        editor.selectionModel.removeSelection()
-      }
+      writeText(editor, offset, ">>")
     }
-    if (c == '~') {
-      runWriteAction {
-        typedAction.defaultRawTypedHandler.beginUndoablePostProcessing()
-        document.insertString(offset, "~")
-        caretModel.moveToOffset(offset)
-        editor.scrollingModel.scrollToCaret(ScrollType.RELATIVE)
-        editor.selectionModel.removeSelection()
+    if (c == '~' && offset - 2 >= 0) {
+      val element: PsiElement = prevNonWhiteSpaceToken(file, offset - 2) ?: return Result.CONTINUE
+
+      val parent = element.parent
+      if (element.elementType == MermaidTokens.ID && parent?.parent is MermaidAttribute) {
+        writeText(editor, offset, "~")
+      }
+      if (parent is MermaidIdentifier && parent.parent is MermaidClassStatement) {
+        writeText(editor, offset, "~")
       }
     }
 
     return Result.CONTINUE
+  }
+
+  private fun prevNonWhiteSpaceToken(file: PsiFile, offset: Int): PsiElement? {
+    var element: PsiElement? = file.findElementAt(offset - 2)
+
+    while (element?.elementType in TokenSet.create(MermaidTokens.WHITE_SPACE, TokenType.WHITE_SPACE)) {
+      element = element?.prevSibling
+    }
+    return element
+  }
+
+  private fun writeText(editor: Editor, offset: Int, text: String) {
+    val typedAction = TypedAction.getInstance() as TypedActionImpl
+    runWriteAction {
+      typedAction.defaultRawTypedHandler.beginUndoablePostProcessing()
+      editor.document.insertString(offset, text)
+      editor.caretModel.moveToOffset(offset)
+      editor.scrollingModel.scrollToCaret(ScrollType.RELATIVE)
+      editor.selectionModel.removeSelection()
+    }
   }
 }
