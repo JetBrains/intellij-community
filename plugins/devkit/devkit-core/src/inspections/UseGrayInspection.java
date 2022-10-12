70e252cff2cb3e4d7661e1451e9d18b24fe5efc7
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2022-10-12 11:12:30 +0200

    IDEA-189985 - UseGrayInspection - cleanup
    GitOrigin-RevId: e459b86857908e1911c9c62c43df655580bd35ea

--- plugins/devkit/devkit-core/src/inspections/UseGrayInspection.java
+++ plugins/devkit/devkit-core/src/inspections/UseGrayInspection.java
@@ -1,79 +1,86 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.inspections;
 
-import com.intellij.codeInspection.InspectionManager;
-import com.intellij.codeInspection.ProblemDescriptor;
-import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.NullUtils;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.JavaConstantExpressionEvaluator;
-import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.ui.Gray;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.devkit.DevKitBundle;
 import org.jetbrains.idea.devkit.inspections.quickfix.ConvertToGrayQuickFix;
 
+import java.awt.*;
+
 /**
  * @author Konstantin Bulenkov
  */
 public class UseGrayInspection extends DevKitInspectionBase {
+
+  private static final String AWT_COLOR_CLASS_NAME = Color.class.getName();
+  private static final String GRAY_CLASS_NAME = Gray.class.getName();
 
   @Override
   protected PsiElementVisitor buildInternalVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
     return new JavaElementVisitor() {
       @Override
       public void visitNewExpression(@NotNull PsiNewExpression expression) {
-        final ProblemDescriptor descriptor = checkNewExpression(expression, holder.getManager(), isOnTheFly);
-        if (descriptor != null) {
-          holder.registerProblem(descriptor);
+        if (isAwtRgbColorConstructor(expression)) {
+          Integer grayValue = getGrayValue(getConstructorParams(expression));
+          if (grayValue != null && grayClassAccessible(expression)) {
+            holder.registerProblem(expression,
+                                   DevKitBundle.message("inspections.use.gray.awt.color.used.name"),
+                                   new ConvertToGrayQuickFix(grayValue));
+          }
         }
       }
     };
   }
 
+  private static boolean isAwtRgbColorConstructor(PsiNewExpression expression) {
+    PsiExpression[] constructorParams = getConstructorParams(expression);
+    if (constructorParams == null) return false;
+    PsiType type = expression.getType();
+    if (type == null) return false;
+    return constructorParams.length == 3 && AWT_COLOR_CLASS_NAME.equals(type.getCanonicalText());
+  }
+
+  @NotNull
+  private static PsiExpression @Nullable [] getConstructorParams(@NotNull PsiNewExpression expression) {
+    PsiExpressionList arguments = expression.getArgumentList();
+    if (arguments == null) return null;
+    return arguments.getExpressions();
+  }
+
   @Nullable
-  private static ProblemDescriptor checkNewExpression(PsiNewExpression expression, InspectionManager manager, boolean isOnTheFly) {
-    final Project project = manager.getProject();
-    final JavaPsiFacade facade = JavaPsiFacade.getInstance(project);
-    final PsiClass grayClass = facade.findClass(Gray.class.getName(), GlobalSearchScope.allScope(project));
-    final PsiType type = expression.getType();
-    if (type != null && grayClass != null) {
-      final PsiExpressionList arguments = expression.getArgumentList();
-      if (arguments != null) {
-        final PsiExpression[] expressions = arguments.getExpressions();
-        if (expressions.length == 3 && "java.awt.Color".equals(type.getCanonicalText())) {
-          if (!facade.getResolveHelper().isAccessible(grayClass, expression, grayClass)) return null;
-          final PsiExpression r = expressions[0];
-          final PsiExpression g = expressions[1];
-          final PsiExpression b = expressions[2];
-          if (r instanceof PsiLiteralExpression &&
-              g instanceof PsiLiteralExpression &&
-              b instanceof PsiLiteralExpression) {
-            final Object red = JavaConstantExpressionEvaluator.computeConstantExpression(r, false);
-            final Object green = JavaConstantExpressionEvaluator.computeConstantExpression(g, false);
-            final Object blue = JavaConstantExpressionEvaluator.computeConstantExpression(b, false);
-            if (NullUtils.notNull(red, green, blue)) {
-              try {
-                int rr = Integer.parseInt(red.toString());
-                int gg = Integer.parseInt(green.toString());
-                int bb = Integer.parseInt(blue.toString());
-                if (rr == gg && gg == bb && 0 <= rr && rr < 256) {
-                  return manager.createProblemDescriptor(expression, DevKitBundle.message("inspections.use.gray.awt.color.used.name"),
-                                                         new ConvertToGrayQuickFix(rr), ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
-                                                         isOnTheFly);
-                }
-              }
-              catch (Exception ignore) {
-              }
-            }
-          }
+  private static Integer getGrayValue(@NotNull PsiExpression @Nullable [] constructorParams) {
+    if (constructorParams == null) return null;
+    PsiExpression redParam = constructorParams[0];
+    Integer red = evaluateColorValue(redParam);
+    if (red == null) return null;
+    PsiExpression greenParam = constructorParams[1];
+    PsiExpression blueParam = constructorParams[2];
+    return 0 <= red && red < 256 && red.equals(evaluateColorValue(greenParam)) && red.equals(evaluateColorValue(blueParam)) ? red : null;
+  }
+
+  @Nullable
+  private static Integer evaluateColorValue(@NotNull PsiExpression expression) {
+    if (expression instanceof PsiLiteralExpression) {
+      Object evaluatedColorValue = JavaConstantExpressionEvaluator.computeConstantExpression(expression, false);
+      if (evaluatedColorValue != null) {
+        try {
+          return Integer.parseInt(evaluatedColorValue.toString());
+        }
+        catch (Exception e) {
+          // ignore
         }
       }
     }
     return null;
+  }
+
+  private static boolean grayClassAccessible(@NotNull PsiElement checkedPlace) {
+    return JavaPsiFacade.getInstance(checkedPlace.getProject()).findClass(GRAY_CLASS_NAME, checkedPlace.getResolveScope()) != null;
   }
 
   @NotNull
