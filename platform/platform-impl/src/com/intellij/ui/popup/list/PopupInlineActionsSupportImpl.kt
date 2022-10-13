5ee8ebfaaf781faf2e6db4ddb2a8421473002552
Mikhail Sokolov <mikhail.sokolov@jetbrains.com>
2022-10-13 20:04:58 +0200

    IDEA-300028 Run widget popup: incorrect icon is used for submenu
    GitOrigin-RevId: 69c6fa7bfad6105f844abf2ed1662ad2bba6e130

--- platform/platform-impl/src/com/intellij/ui/popup/list/PopupInlineActionsSupportImpl.kt
+++ platform/platform-impl/src/com/intellij/ui/popup/list/PopupInlineActionsSupportImpl.kt
@@ -5,22 +5,14 @@ import com.intellij.icons.AllIcons
 import com.intellij.ide.IdeBundle
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.ui.ExperimentalUI
-import com.intellij.ui.components.panels.Wrapper
 import com.intellij.ui.popup.ActionPopupStep
 import com.intellij.ui.popup.PopupFactoryImpl.ActionItem
 import com.intellij.ui.popup.PopupFactoryImpl.InlineActionItem
 import com.intellij.ui.popup.list.ListPopupImpl.ListWithInlineButtons
-import com.intellij.ui.scale.JBUIScale
-import com.intellij.util.ui.JBInsets
-import com.intellij.util.ui.JBUI
 import java.awt.Point
 import java.awt.event.InputEvent
-import javax.swing.Icon
 import javax.swing.JComponent
-import javax.swing.JLabel
 import javax.swing.JList
-
-private const val INLINE_BUTTON_WIDTH = 16
 
 class PopupInlineActionsSupportImpl(private val myListPopup: ListPopupImpl) : PopupInlineActionsSupport {
 
@@ -33,25 +25,16 @@ class PopupInlineActionsSupportImpl(private val myListPopup: ListPopupImpl) : Po
 
     var res = 0
     res += myStep.getInlineActions(element).size
-    if (res != 0 && myStep.hasSubstep(element)) res++
+    if (hasMoreButton(element)) res++
     return res
   }
 
   override fun calcButtonIndex(element: Any?, point: Point): Int? {
     if (element == null) return null
-    val list = myListPopup.list
-    val index = list.selectedIndex
-    val bounds = list.getCellBounds(index, index) ?: return null
-    JBInsets.removeFrom(bounds, PopupListElementRenderer.getListCellPadding())
-
     val buttonsCount: Int = calcExtraButtonsCount(element)
     if (buttonsCount <= 0) return null
 
-    val distanceToRight = bounds.x + bounds.width - point.x
-    val buttonsToRight = distanceToRight / buttonWidth()
-    if (buttonsToRight >= buttonsCount) return null
-
-    return buttonsCount - buttonsToRight - 1
+    return calcButtonIndex(myListPopup.list, buttonsCount, point)
   }
 
   override fun runInlineAction(element: Any, index: Int, event: InputEvent?) : Boolean {
@@ -68,20 +51,19 @@ class PopupInlineActionsSupportImpl(private val myListPopup: ListPopupImpl) : Po
     res.addAll(myStep.getInlineActions(element).map {
       item: InlineActionItem -> Pair(createInlineActionRunnable(item.action, event), true)
     })
-    if (!res.isEmpty() && myStep.hasSubstep(element)) res.add(Pair(createNextStepRunnable(element), false))
+    if (hasMoreButton(element)) res.add(Pair(createNextStepRunnable(element), false))
     return res
   }
 
   override fun getExtraButtons(list: JList<*>, value: Any, isSelected: Boolean): List<JComponent> {
     if (value !is ActionItem) return emptyList()
     val inlineActions = myStep.getInlineActions(value)
-    if (inlineActions.isEmpty()) return emptyList()
 
     val res: MutableList<JComponent> = java.util.ArrayList()
     val activeIndex = getActiveButtonIndex(list)
 
     for (i in 0 until inlineActions.size) res.add(createActionButton(inlineActions[i], i == activeIndex, isSelected))
-    res.add(createSubmenuButton(value, res.size == activeIndex))
+    if (hasMoreButton(value)) res.add(createSubmenuButton(value, res.size == activeIndex))
 
     return res
   }
@@ -92,7 +74,6 @@ class PopupInlineActionsSupportImpl(private val myListPopup: ListPopupImpl) : Po
     val icon = if (myStep.isFinal(value)) AllIcons.Actions.More else AllIcons.Icons.Ide.MenuArrow
     return createExtraButton(icon, active)
   }
-
 
   private fun createActionButton(action: InlineActionItem, active: Boolean, isSelected: Boolean): JComponent =
     createExtraButton(action.getIcon(isSelected), active)
@@ -107,23 +88,11 @@ class PopupInlineActionsSupportImpl(private val myListPopup: ListPopupImpl) : Po
       inlineActions.getOrNull(activeButton)?.text
   }
 
-  private fun createExtraButton(icon: Icon, active: Boolean): JComponent {
-    val label = JLabel(icon)
-    val leftRightInsets = JBUI.CurrentTheme.List.buttonLeftRightInsets()
-    label.border = JBUI.Borders.empty(0, leftRightInsets)
-    val panel = Wrapper(label)
-    val size = panel.preferredSize
-    size.width = buttonWidth(leftRightInsets)
-    panel.preferredSize = size
-    panel.minimumSize = size
-    panel.isOpaque = active
-    panel.background = JBUI.CurrentTheme.List.buttonHoverBackground()
-    return panel
-  }
-
-  private fun buttonWidth(leftRightInsets: Int = JBUI.CurrentTheme.List.buttonLeftRightInsets()) : Int = JBUIScale.scale(INLINE_BUTTON_WIDTH + leftRightInsets * 2)
-
   private fun createNextStepRunnable(element: ActionItem) = Runnable { myListPopup.showNextStepPopup(myStep.onChosen(element, false), element) }
 
   private fun createInlineActionRunnable(action: AnAction, inputEvent: InputEvent?) = Runnable { myStep.performAction(action, inputEvent) }
+
+  private fun hasMoreButton(element: ActionItem) = myStep.hasSubstep(element)
+                                                                    && !myListPopup.isShowSubmenuOnHover
+                                                                    && myStep.isFinal(element)
 }
