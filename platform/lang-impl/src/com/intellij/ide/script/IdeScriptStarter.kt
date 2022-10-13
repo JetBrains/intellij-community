c2e7e946baa2bb6a70bec0822a91bae8708e73cb
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-10-13 09:50:09 +0200

    IdeScriptStarter - use async API to open project
    GitOrigin-RevId: 7b7de54fa9b2cc9d2f82947aa05e4578a3e3bea6

--- platform/lang-impl/src/com/intellij/ide/script/IdeScriptStarter.kt
+++ platform/lang-impl/src/com/intellij/ide/script/IdeScriptStarter.kt
@@ -1,94 +1,73 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ide.script;
+package com.intellij.ide.script
 
-import com.intellij.ide.CliResult;
-import com.intellij.lang.LangBundle;
-import com.intellij.openapi.application.ApplicationNamesInfo;
-import com.intellij.openapi.application.ApplicationStarterBase;
-import com.intellij.openapi.diagnostic.DefaultLogger;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.wm.IdeFrame;
-import com.intellij.openapi.wm.ex.WindowManagerEx;
-import com.intellij.util.concurrency.AppExecutorUtil;
-import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.awt.*;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.nio.charset.Charset;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
+import com.intellij.ide.CliResult
+import com.intellij.lang.LangBundle
+import com.intellij.openapi.application.ApplicationNamesInfo
+import com.intellij.openapi.application.ApplicationStarterBase
+import com.intellij.openapi.diagnostic.DefaultLogger
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.util.Pair
+import com.intellij.openapi.wm.IdeFrame
+import com.intellij.openapi.wm.ex.WindowManagerEx
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.nio.charset.Charset
+import java.nio.file.Path
 
 /**
  * @author gregsh
  */
-final class IdeScriptStarter extends ApplicationStarterBase {
-  private static final Logger LOG = Logger.getInstance(IdeScriptStarter.class);
+internal class IdeScriptStarter : ApplicationStarterBase() {
+  override val commandName: String
+    get() = "ideScript"
 
-  @Override
-  public String getCommandName() {
-    return "ideScript";
-  }
-
-  @Override
-  public String getUsageMessage() {
-    String scriptName = ApplicationNamesInfo.getInstance().getScriptName();
-    return LangBundle.message("ide.script.starter.usage", scriptName, "ideScript");
-  }
-
-  @Override
-  protected boolean checkArguments(@NotNull List<String> args) {
-    return args.size() > 1;
-  }
-
-  @NotNull
-  @Override
-  protected CompletableFuture<CliResult> processCommand(@NotNull List<String> args,
-                                                        @Nullable String currentDirectory) throws Exception {
-    List<Path> filePaths = ContainerUtil.map(args.subList(1, args.size()), Paths::get);
-    Project project = guessProject();
-    CompletableFuture<CliResult> future = new CompletableFuture<>();
-    AppExecutorUtil.getAppExecutorService().execute(() -> {
-      List<Pair<Path, IdeScriptEngine>> result = IdeStartupScripts.prepareScriptsAndEngines(filePaths);
-      IdeStartupScripts.runAllScriptsImpl(project, result, project != null ? LOG : redirectStreamsAndGetLogger(result));
-      future.complete(CliResult.OK);
-    });
-    return future;
-  }
-
-  @Nullable
-  private static Project guessProject() {
-    Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
-    Window recentFocusedWindow = WindowManagerEx.getInstanceEx().getMostRecentFocusedWindow();
-    if (recentFocusedWindow instanceof IdeFrame) return ((IdeFrame)recentFocusedWindow).getProject();
-    return ContainerUtil.find(openProjects, o -> o.isInitialized() && !o.isDisposed());
-  }
-
-  /** @noinspection UseOfSystemOutOrSystemErr*/
-  @NotNull
-  private static Logger redirectStreamsAndGetLogger(List<Pair<Path, IdeScriptEngine>> result) {
-    for (Pair<Path, IdeScriptEngine> pair : result) {
-      pair.second.setStdOut(new OutputStreamWriter(System.out, Charset.defaultCharset()));
-      pair.second.setStdErr(new OutputStreamWriter(System.err, Charset.defaultCharset()));
-      pair.second.setStdIn(new InputStreamReader(System.in, Charset.defaultCharset()));
+  override val usageMessage: String
+    get() {
+      val scriptName = ApplicationNamesInfo.getInstance().scriptName
+      return LangBundle.message("ide.script.starter.usage", scriptName, "ideScript")
     }
-    return new DefaultLogger(null) {
-      @Override
-      public void info(String message) {
-        System.out.println("INFO: " + message);
-      }
 
-      @Override
-      public void info(String message, Throwable t) {
-        System.out.println("INFO: " + message);
-      }
-    };
+  override fun checkArguments(args: List<String>): Boolean = args.size > 1
+
+  override suspend fun executeCommand(args: List<String>, currentDirectory: String?): CliResult {
+    val filePaths = args.subList(1, args.size).map { Path.of(it) }
+    val project = guessProject()
+    val result = IdeStartupScripts.prepareScriptsAndEngines(filePaths)
+    IdeStartupScripts.runAllScriptsImpl(project, result,
+                                        if (project == null) redirectStreamsAndGetLogger(result) else logger<IdeScriptStarter>())
+    return CliResult.OK
+  }
+}
+
+private fun guessProject(): Project? {
+  val recentFocusedWindow = WindowManagerEx.getInstanceEx().mostRecentFocusedWindow
+  if (recentFocusedWindow is IdeFrame) {
+    return (recentFocusedWindow as IdeFrame).project
+  }
+  else {
+    return ProjectManager.getInstance().openProjects.firstOrNull { o -> o.isInitialized && !o.isDisposed }
+  }
+}
+
+/** @noinspection UseOfSystemOutOrSystemErr
+ */
+private fun redirectStreamsAndGetLogger(result: List<Pair<Path, IdeScriptEngine>>): Logger {
+  for (pair in result) {
+    pair.second.stdOut = OutputStreamWriter(System.out, Charset.defaultCharset())
+    pair.second.stdErr = OutputStreamWriter(System.err, Charset.defaultCharset())
+    pair.second.stdIn = InputStreamReader(System.`in`, Charset.defaultCharset())
+  }
+  return object : DefaultLogger(null) {
+    override fun info(message: String) {
+      println("INFO: $message")
+    }
+
+    override fun info(message: String, t: Throwable?) {
+      println("INFO: $message")
+    }
   }
 }
