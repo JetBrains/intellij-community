48fcc1c8fc5ae90a388da455a656ed77861d5d9d
Anna Kozlova <anna.kozlova@jetbrains.com>
2022-10-14 20:10:21 +0200

    [kotlin] implement deep overriding search
    use light classes when java is first met, use in safe delete (KTIJ-21136)
    
    GitOrigin-RevId: 331df8ea6ac48f4cc9bcb8a7e5613ce209abe08b

--- plugins/kotlin/kotlin.search/src/org/jetbrains/kotlin/idea/inheritorsSearch/kotlinSearchUtil.kt
+++ plugins/kotlin/kotlin.search/src/org/jetbrains/kotlin/idea/inheritorsSearch/kotlinSearchUtil.kt
@@ -1,23 +1,74 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.inheritorsSearch
 
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.search.searches.OverridingMethodsSearch
-import com.intellij.util.AbstractQuery
-import com.intellij.util.EmptyQuery
-import com.intellij.util.Processor
-import com.intellij.util.Query
-import org.jetbrains.kotlin.asJava.toLightMethods
+import com.intellij.util.mappingNotNull
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.symbols.KtCallableSymbol
+import org.jetbrains.kotlin.asJava.unwrapped
 import org.jetbrains.kotlin.psi.KtCallableDeclaration
+import java.util.*
 
-fun search(function: KtCallableDeclaration): Query<PsiMethod> {
-    val methods = function.toLightMethods()
-    if (methods.isEmpty()) return EmptyQuery.getEmptyQuery()
+/**
+ * Returns a set of PsiMethods/KtFunctions/KtProperties which "deep" override current [function].
+ */
+fun findAllOverridings(function: KtCallableDeclaration): Set<PsiElement> {
+    return findAllOverridings(function, false)
+}
 
-    val queries = methods.map { OverridingMethodsSearch.search(it, true) }.toList()
-    return object : AbstractQuery<PsiMethod>() {
-        override fun processResults(consumer: Processor<in PsiMethod>): Boolean {
-            return queries.all { it.forEach(consumer) }
+/**
+ * Returns a set of PsiMethods/KtFunctions/KtProperties which belong to the hierarchy of [function].
+ * 
+ * Example:
+ * ```
+ * interface A { fun f() }
+ * interface B { fun f() }
+ * interface C : A, B { override fun f() }
+ * ```
+ * Hierarchy for B.f contains both A.f and C.f
+ */
+fun findFullHierarchy(function: KtCallableDeclaration) : Set<PsiElement> {
+    return findAllOverridings(function, true)
+} 
+
+private fun findAllOverridings(function: KtCallableDeclaration, withFullHierarchy : Boolean): Set<PsiElement> {
+    val queue = ArrayDeque<PsiElement>()
+    val visited = HashSet<PsiElement>()
+
+    queue.add(function)
+    while (!queue.isEmpty()) {
+        val currentMethod = queue.poll()
+        visited += currentMethod
+        if (currentMethod is KtCallableDeclaration) {
+            DirectKotlinOverridingCallableSearch.search(currentMethod).forEach {
+                queue.offer(it)
+            }
+            if (withFullHierarchy) {
+                analyze(currentMethod) {
+                    val ktCallableSymbol = currentMethod.getSymbol() as? KtCallableSymbol ?: return@analyze
+                    ktCallableSymbol.getDirectlyOverriddenSymbols()
+                        .mapNotNull {
+                            it.psi
+                        }
+                        .filter { it !in visited }
+                        .forEach { queue.offer(it) }
+                }
+            }
+        }
+        else if (currentMethod is PsiMethod) {
+            OverridingMethodsSearch.search(currentMethod, true)
+                .mappingNotNull { it.unwrapped }
+                .filter { it !in visited }
+                .forEach { queue.offer(it) }
+            if (withFullHierarchy) {
+                currentMethod.findSuperMethods(true)
+                    .mapNotNull { it.unwrapped }
+                    .filter { it !in visited }
+                    .forEach { queue.offer(it) }
+            }
         }
     }
+    return visited
 }
