288ab575cc6dd575bc0c8ad156988e1548c8dd37
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2022-10-14 16:40:39 +0200

    IDEA-303703 do not delete all directory content on Reload library
    GitOrigin-RevId: de77333d80e9ee1da434c831fec01e7879b79d88

--- java/idea-ui/src/com/intellij/jarRepository/RepositoryLibraryWithDescriptionEditor.java
+++ java/idea-ui/src/com/intellij/jarRepository/RepositoryLibraryWithDescriptionEditor.java
@@ -119,28 +119,12 @@ public class RepositoryLibraryWithDescriptionEditor
     String toolTipText = JavaUiBundle.message("button.reload.description", directory.getPath());
     myReloadButton.setVisible(true);
     myReloadButton.setToolTipText(toolTipText);
-    myReloadButton.addActionListener(e -> reloadLibraryDirectory(project, directory, (LibraryEx)library));
+    myReloadButton.addActionListener(e -> reloadLibraryDirectory(project, (LibraryEx)library));
   }
 
-  private void reloadLibraryDirectory(Project project, VirtualFile directory, LibraryEx library) {
-    if (directory == null) return;
-    VirtualFile[] children = directory.getChildren();
-    if (children == null) return;
-
-    logger.debug("start delete " + directory);
-    try {
-      for (VirtualFile child : children) {
-        FileUtil.delete(child.toNioPath());
-      }
-    }
-    catch (IOException e) {
-      logger.error("error on delete", e);
-      String error = e.getLocalizedMessage();
-      showBalloon(error, MessageType.ERROR);
-      return;
-    }
+  private void reloadLibraryDirectory(Project project, LibraryEx library) {
     reloadDependencies(project, library);
-    showBalloon(JavaUiBundle.message("popup.reload.success.result", directory.getPath()), MessageType.INFO);
+    showBalloon(JavaUiBundle.message("popup.reload.success.result", library.getName()), MessageType.INFO);
   }
 
   private void showBalloon(@NlsSafe String text, MessageType type) {
