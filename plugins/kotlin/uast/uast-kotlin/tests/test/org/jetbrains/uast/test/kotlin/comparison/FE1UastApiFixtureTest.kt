0d5b5b7aa4cfe5cd189c68fdf1e9ebf70008a127
Jinseong Jeon <jsjeon@google.com>
2022-10-13 17:20:23 -0700

    FIR/UAST: split non-resolve API fixture tests
    GitOrigin-RevId: b13e657aa38d8d935fffe046d0e96a3746af3282

---
+++ plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/kotlin/comparison/FE1UastApiFixtureTest.kt
@@ -0,0 +1,42 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.uast.test.kotlin.org.jetbrains.uast.test.kotlin.comparison
+
+import com.intellij.testFramework.LightProjectDescriptor
+import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
+import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
+import org.jetbrains.uast.test.common.kotlin.UastApiFixtureTestBase
+import org.junit.internal.runners.JUnit38ClassRunner
+import org.junit.runner.RunWith
+
+@RunWith(JUnit38ClassRunner::class)
+class FE1UastApiFixtureTest : KotlinLightCodeInsightFixtureTestCase(), UastApiFixtureTestBase {
+    override val isFirUastPlugin: Boolean = false
+
+    override fun getProjectDescriptor(): LightProjectDescriptor =
+        KotlinWithJdkAndRuntimeLightProjectDescriptor.getInstance()
+
+    fun testAssigningArrayElementType() {
+        checkAssigningArrayElementType(myFixture)
+    }
+
+    fun testDivByZero() {
+        checkDivByZero(myFixture)
+    }
+
+    fun testDetailsOfDeprecatedHidden() {
+        checkDetailsOfDeprecatedHidden(myFixture)
+    }
+
+    fun testImplicitReceiverType() {
+        checkImplicitReceiverType(myFixture)
+    }
+
+    fun testSubstitutedReceiverType() {
+        checkSubstitutedReceiverType(myFixture)
+    }
+
+    fun testCallKindOfSamConstructor() {
+        checkCallKindOfSamConstructor(myFixture)
+    }
+
+}
