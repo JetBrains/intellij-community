7e3ef37f680434f59340dc74918e8c9c5fcb36d9
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2022-10-17 19:25:33 +0200

    [ml-ranking] Remove recommenders for Python
    GitOrigin-RevId: 1ec0a8c66e3415f341c4f895e80253fb3a74252b

--- platform/build-scripts/src/org/jetbrains/intellij/build/CommunityLibraryLicenses.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/CommunityLibraryLicenses.kt
@@ -908,6 +908,7 @@ object CommunityLibraryLicenses {
     jetbrainsLibrary("intellij.remoterobot.robot.server.core"),
     jetbrainsLibrary("jshell-frontend"),
     jetbrainsLibrary("jvm-native-trusted-roots"),
+    jetbrainsLibrary("kinference"),
     jetbrainsLibrary("kotlin-gradle-plugin-idea"),
     jetbrainsLibrary("kotlin-gradle-plugin-idea-proto"),
     jetbrainsLibrary("kotlin-script-runtime"),
