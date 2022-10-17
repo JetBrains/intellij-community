dfd8dc71e4ab50aa3b2f196ed7c4fd163c4d427c
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2022-10-17 19:12:15 +0300

    IDEA-299723 Implement tips sorting using information from linked features
    (cherry picked from commit aca2b06cf808356090611974ceb672bda9c97343)
    
    IJ-CR-96756
    
    GitOrigin-RevId: 2bd185c5b32c1623c24239ab00dc071f88897e7e

--- platform/tips-of-the-day/src/com/intellij/ide/util/TipsOrderUtil.kt
+++ platform/tips-of-the-day/src/com/intellij/ide/util/TipsOrderUtil.kt
@@ -1,8 +1,12 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.util
 
+import com.intellij.featureStatistics.FeatureUsageTracker
+import com.intellij.featureStatistics.ProductivityFeaturesRegistry
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.project.Project
 
 @Service
 internal class TipsOrderUtil {
@@ -11,12 +15,59 @@ internal class TipsOrderUtil {
    *
    * @return object that contains sorted tips and describes approach of how the tips are sorted
    */
-  fun sort(tips: List<TipAndTrickBean>): RecommendationDescription {
-    // todo: implement new sorting algorithm
-    return RecommendationDescription("shuffle", tips.shuffled(), "1")
+  fun sort(tips: List<TipAndTrickBean>, project: Project): RecommendationDescription {
+    val registry = ProductivityFeaturesRegistry.getInstance();
+    if (registry == null) {
+      thisLogger().warn("ProductivityFeaturesRegistry is not created")
+      return RecommendationDescription(SHUFFLE_ALGORITHM, tips.shuffled(), "1")
+    }
+
+    FeatureUsageTracker.getInstance()  // instantiate just to load statistics of feature usage
+    val tipsUsageManager = TipsUsageManager.getInstance()
+    val allFeatures = registry.featureIds.map { registry.getFeatureDescriptor(it) }
+    val tipInfoList = tips.map { tip ->
+      val features = allFeatures.filter { it.tipId == tip.id }
+      val lastTimeShown = tipsUsageManager.getLastTimeShown(tip.id)
+      if (features.isNotEmpty()) {
+        val isApplicable = features.any { feature ->
+          val filters = registry.getMatchingFilters(feature.id)
+          if (filters.isNotEmpty()) {
+            filters.any { filter -> filter.isApplicable(feature.id, project) }
+          }
+          else true
+        }
+        val unusedScore = features.count { it.isUnused } * 1.0 / features.size
+        TipInfo(tip, true, isApplicable, unusedScore, lastTimeShown)
+      }
+      else TipInfo(tip, false, true, 0.0, lastTimeShown)
+    }
+
+    val sortedTips = tipInfoList.sortedWith(getComparator()).map { it.tip }
+    val adjustedSortedTips = tipsUsageManager.makeLastShownTipFirst(sortedTips)
+    return RecommendationDescription(SORTING_ALGORITHM, adjustedSortedTips, SORTING_ALGORITHM_VERSION)
   }
 
+  private fun getComparator(): Comparator<TipInfo> {
+    return compareBy<TipInfo> { info -> info.featureFound && info.isApplicable && info.unusedScore > 0.1 }
+      .then(compareBy { info -> !info.featureFound })
+      .thenComparingDouble { info -> info.unusedScore }.reversed()
+      .then(compareByDescending { info -> info.isApplicable })
+      .thenComparingLong { info -> info.lastTimeShown }
+  }
+
+  private data class TipInfo(
+    val tip: TipAndTrickBean,
+    val featureFound: Boolean,
+    val isApplicable: Boolean,
+    val unusedScore: Double,
+    val lastTimeShown: Long
+  )
+
   companion object {
+    const val SHUFFLE_ALGORITHM = "shuffle"
+    const val SORTING_ALGORITHM = "usage_and_applicability"
+    private const val SORTING_ALGORITHM_VERSION = "1"
+
     @JvmStatic
     fun getInstance(): TipsOrderUtil = service()
   }
