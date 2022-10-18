a1f2fb6f03975394d909724f14c6abc9cc175f59
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2022-10-18 15:51:03 +0200

    getting rid of builder-dependent setComparator methods
    GitOrigin-RevId: 4435eb5302c0809e154d7337302d6a3ef2372b7a

--- platform/lang-impl/src/com/intellij/ide/projectView/impl/AbstractProjectViewPane.java
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/AbstractProjectViewPane.java
@@ -773,27 +773,11 @@ public abstract class AbstractProjectViewPane implements DataProvider, Disposabl
     return new GroupByTypeComparator(myProject, getId());
   }
 
-  @Deprecated(forRemoval = true)
   public void installComparator() {
-    installComparator(getTreeBuilder());
+    installComparator(createComparator());
   }
 
-  @Deprecated(forRemoval = true)
-  void installComparator(AbstractTreeBuilder treeBuilder) {
-    installComparator(treeBuilder, createComparator());
-  }
-
-  @TestOnly
-  @Deprecated(forRemoval = true)
   public void installComparator(@NotNull Comparator<? super NodeDescriptor<?>> comparator) {
-    installComparator(getTreeBuilder(), comparator);
-  }
-
-  @Deprecated(forRemoval = true)
-  protected void installComparator(AbstractTreeBuilder builder, @NotNull Comparator<? super NodeDescriptor<?>> comparator) {
-    if (builder != null) {
-      builder.setNodeDescriptorComparator(comparator);
-    }
   }
 
   public JTree getTree() {
