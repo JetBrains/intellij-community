ce813d168b6dc36699e608ad36bbaf93db98eda8
Ekaterina Berezhko <Ekaterina.Berezhko@jetbrains.com>
2022-10-18 15:50:58 +0300

    Update annotator for conflicting commit id in gitGraph
    GitOrigin-RevId: 01b5f431e812ad108abe13b42f67a97778619885

--- plugins/mermaid/src/main/kotlin/com/intellij/mermaid/lang/intention/GitGraphAnnotator.kt
+++ plugins/mermaid/src/main/kotlin/com/intellij/mermaid/lang/intention/GitGraphAnnotator.kt
@@ -7,61 +7,149 @@ import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.mermaid.MermaidBundle
 import com.intellij.mermaid.lang.psi.*
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.parentOfType
 import com.intellij.psi.util.siblings
 
 
 class GitGraphAnnotator : Annotator {
   override fun annotate(element: PsiElement, holder: AnnotationHolder) {
     if (element is MermaidMergeStatement || element is MermaidCheckoutStatement) {
-      val identifier =
-        (element as? MermaidMergeStatement)?.identifier
-          ?: (element as? MermaidCheckoutStatement)?.identifier
-          ?: return
-
-      val text = identifier.text
-      val parent = element.parent ?: return
-
-      val matchingIds = parent
-        .siblings(forward = false, withSelf = false)
-        .filterIsInstance<MermaidGitGraphStatement>()
-        .map { it.firstChild }
-        .filterIsInstance<MermaidBranchStatement>()
-        .map { it.identifier.text }
-        .filter { it == text }
-
-      if (matchingIds.toList().isNotEmpty()) {
-        return
-      }
-
-      holder.newAnnotation(HighlightSeverity.ERROR, MermaidBundle.message("annotator.unresolved.branch"))
-        .range(identifier.textRange)
-        .highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
-        .withFix(CreateBranchDeclarationIntention(identifier))
-        .create()
+      annotateUnresolvedBranch(element, holder)
     }
     if (element is MermaidCherryPickStatement) {
-      val identifier = element.commitIdAttribute.commitIdValue
+      annotateUnresolvedCommitId(element, holder)
+    }
+    if (element is MermaidMergeStatement) {
+      annotateConflictingCommitId(element, holder)
+    }
+    if (element is MermaidCommitStatement) {
+      annotateConflictingCommitId(element, holder)
+    }
+  }
 
-      val text = identifier.text
-      val parent = element.parent ?: return
+  private fun annotateUnresolvedBranch(element: PsiElement, holder: AnnotationHolder) {
+    val identifier =
+      (element as? MermaidMergeStatement)?.identifier
+        ?: (element as? MermaidCheckoutStatement)?.identifier
+        ?: return
 
-      val matchingIds = parent
-        .siblings(forward = false, withSelf = false)
-        .filterIsInstance<MermaidGitGraphStatement>()
-        .map { it.firstChild }
-        .filterIsInstance<MermaidCommitStatement>()
-        .mapNotNull { it.commitIdAttribute?.commitIdValue?.text }
-        .filter { it == text }
+    val text = identifier.text
+    val parent = element.parent ?: return
 
-      if (matchingIds.toList().isNotEmpty()) {
-        return
-      }
+    val matchingIds = parent
+      .siblings(forward = false, withSelf = false)
+      .filterIsInstance<MermaidGitGraphStatement>()
+      .map { it.firstChild }
+      .filterIsInstance<MermaidBranchStatement>()
+      .map { it.identifier.text }
+      .filter { it == text }
 
-      holder.newAnnotation(HighlightSeverity.ERROR, MermaidBundle.message("annotator.unresolved.commit.id"))
+    if (matchingIds.toList().isNotEmpty()) {
+      return
+    }
+
+    holder.newAnnotation(HighlightSeverity.ERROR, MermaidBundle.message("annotator.unresolved.branch"))
+      .range(identifier.textRange)
+      .highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
+      .withFix(CreateBranchDeclarationIntention(identifier))
+      .create()
+  }
+
+  private fun annotateUnresolvedCommitId(element: MermaidCherryPickStatement, holder: AnnotationHolder) {
+    val identifier = element.commitIdAttribute.commitIdValue
+
+    val text = identifier.text
+    val parent = element.parent ?: return
+
+    val matchingIds = parent
+      .siblings(forward = false, withSelf = false)
+      .filterIsInstance<MermaidGitGraphStatement>()
+      .map { it.firstChild }
+      .filterIsInstance<MermaidCommitStatement>()
+      .mapNotNull { it.commitIdAttribute?.commitIdValue?.text }
+      .filter { it == text }
+
+    if (matchingIds.toList().isNotEmpty()) {
+      return
+    }
+
+    holder.newAnnotation(HighlightSeverity.ERROR, MermaidBundle.message("annotator.unresolved.commit.id"))
+      .range(identifier.textRange)
+      .highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
+      .withFix(CreateCommitDeclarationIntention(identifier))
+      .create()
+  }
+
+  private fun annotateConflictingCommitId(element: MermaidMergeStatement, holder: AnnotationHolder) {
+    val identifier = element.commitIdAttribute?.commitIdValue ?: return
+
+    val text = identifier.text
+    val parent = element.parentOfType<MermaidGitGraphDocument>() ?: return
+
+    val siblings = parent
+      .children
+      .filterIsInstance<MermaidGitGraphStatement>()
+      .map { it.firstChild }
+
+    val mergeStatementIdentifiers = siblings
+      .filterIsInstance<MermaidMergeStatement>()
+      .filter { it != element }
+      .mapNotNull { it.commitIdAttribute?.commitIdValue }
+    val commitStatementIdentifiers = siblings
+      .filterIsInstance<MermaidCommitStatement>()
+      .mapNotNull { it.commitIdAttribute?.commitIdValue }
+    val matchingIds = (mergeStatementIdentifiers + commitStatementIdentifiers)
+      .map { it.text }
+      .filter { it == text }
+
+    if (matchingIds.toList().isEmpty()) {
+      return
+    }
+
+    holder.newAnnotation(HighlightSeverity.ERROR, MermaidBundle.message("annotator.conflicting.commit.id"))
+      .range(identifier.textRange)
+      .highlightType(ProblemHighlightType.ERROR)
+      .create()
+  }
+
+  private fun annotateConflictingCommitId(element: MermaidCommitStatement, holder: AnnotationHolder) {
+    val identifier = element.commitIdAttribute?.commitIdValue ?: return
+
+    val text = identifier.text
+    val parent = element.parentOfType<MermaidGitGraphDocument>() ?: return
+
+    val siblings = parent
+      .children
+      .filterIsInstance<MermaidGitGraphStatement>()
+      .map { it.firstChild }
+
+    val matchingMergeStatementIdentifiers = siblings
+      .filterIsInstance<MermaidMergeStatement>()
+      .mapNotNull { it.commitIdAttribute?.commitIdValue?.text }
+      .filter { it == text }
+
+    if (matchingMergeStatementIdentifiers.isNotEmpty()) {
+      holder.newAnnotation(HighlightSeverity.ERROR, MermaidBundle.message("annotator.conflicting.commit.id"))
         .range(identifier.textRange)
-        .highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
-        .withFix(CreateCommitDeclarationIntention(identifier))
+        .highlightType(ProblemHighlightType.ERROR)
         .create()
+
+      return
+    }
+
+    val matchingCommitStatementIdentifiers = siblings
+      .filterIsInstance<MermaidCommitStatement>()
+      .filter { it != element }
+      .mapNotNull { it.commitIdAttribute?.commitIdValue?.text }
+      .filter { it == text }
+
+    if (matchingCommitStatementIdentifiers.toList().isNotEmpty()) {
+      holder.newAnnotation(HighlightSeverity.WARNING, MermaidBundle.message("annotator.conflicting.commit.id"))
+        .range(identifier.textRange)
+        .highlightType(ProblemHighlightType.WARNING)
+        .create()
+
+      return
     }
   }
 }
