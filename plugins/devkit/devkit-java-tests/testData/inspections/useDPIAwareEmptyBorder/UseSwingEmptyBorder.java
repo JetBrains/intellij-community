8ad2854594f01532b2c20a2755253cf1d5f1b89d
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2022-10-18 13:02:40 +0200

    IDEA-189985 - UseDPIAwareEmptyBorderInspection - rewrite to UAST
    (cherry picked from commit fba675df7f2d03b534dee0f64933e3b5d7618788)
    
    IJ-CR-96703
    
    GitOrigin-RevId: 611fb9e68f39d6b53a28868ec56bae79dfb44acb

--- plugins/devkit/devkit-java-tests/testData/inspections/useDPIAwareEmptyBorder/UseSwingEmptyBorder.java
+++ plugins/devkit/devkit-java-tests/testData/inspections/useDPIAwareEmptyBorder/UseSwingEmptyBorder.java
@@ -2,7 +2,7 @@ import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import java.awt.Insets;
 
-class UseJBColorConstructor {
+class UseSwingEmptyBorder {
 
   private static final EmptyBorder SWING_EMPTY_BORDER = <warning descr="'EmptyBorder' is not DPI-aware">new EmptyBorder(1, 2, 3, 4)</warning>;
   private static final EmptyBorder SWING_EMPTY_BORDER_INSETS = new EmptyBorder(new Insets(1, 2, 3, 4)); // correct usage
