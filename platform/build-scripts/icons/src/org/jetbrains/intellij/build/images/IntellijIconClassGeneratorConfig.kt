f1e45155e28c944720202cf65fdc7bcc1f97f91a
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2022-10-19 13:30:47 +0200

    Django: Move intellij.python.django.core to plugins
    GitOrigin-RevId: 3100ed4f781edd46781f3d91bfca6cc5b1cdba22

--- platform/build-scripts/icons/src/org/jetbrains/intellij/build/images/IntellijIconClassGeneratorConfig.kt
+++ platform/build-scripts/icons/src/org/jetbrains/intellij/build/images/IntellijIconClassGeneratorConfig.kt
@@ -69,7 +69,7 @@ class IntellijIconClassGeneratorConfig : IconsClasses() {
 
       "intellij.grazie.core" -> IntellijIconClassGeneratorModuleConfig(className = "GrazieIcons", packageName = "com.intellij.grazie.icons")
       "intellij.sh.core" -> IntellijIconClassGeneratorModuleConfig(className = "ShIcons", packageName = "com.intellij.sh")
-      "intellij.python.django.core" -> IntellijIconClassGeneratorModuleConfig(
+      "intellij.django.core" -> IntellijIconClassGeneratorModuleConfig(
         className = "DjangoIcons",
         packageName = "com.jetbrains.django"
       )
