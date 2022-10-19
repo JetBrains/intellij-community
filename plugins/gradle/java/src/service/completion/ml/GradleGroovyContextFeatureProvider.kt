3edf1ef3d63f1ef83111d925733a85c129e69822
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-10-19 14:14:52 +0300

    [gradle] IDEA-303207: Use only known set of methods and classes
    IJ-CR-96626
    
    GitOrigin-RevId: 75e165d32c17d81469599599d60163d3016209a6

--- plugins/gradle/java/src/service/completion/ml/GradleGroovyContextFeatureProvider.kt
+++ plugins/gradle/java/src/service/completion/ml/GradleGroovyContextFeatureProvider.kt
@@ -11,6 +11,7 @@ import com.intellij.psi.util.parentOfType
 import com.intellij.util.asSafely
 import groovy.lang.Closure
 import org.jetbrains.plugins.gradle.config.isGradleFile
+import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.*
 import org.jetbrains.plugins.gradle.service.resolve.getLinkedGradleProjectPath
 import org.jetbrains.plugins.gradle.settings.GradleLocalSettings
 import org.jetbrains.plugins.gradle.util.GradleConstants
@@ -46,14 +47,16 @@ class GradleGroovyContextFeatureProvider : ContextFeatureProvider {
     val parentClosure = position?.parentOfType<GrFunctionalExpression>() ?: return
     val delegatesToInfo = getDelegatesToInfo(parentClosure)
     if (delegatesToInfo != null && (delegatesToInfo.strategy == Closure.DELEGATE_FIRST || delegatesToInfo.strategy == Closure.DELEGATE_ONLY)) {
-      val type = delegatesToInfo.typeToDelegate?.asSafely<PsiClassType>()?.resolve()?.name
-      if (type != null) {
-        features["groovy_delegate_$type"] = MLFeatureValue.binary(true)
+      val psiClass = delegatesToInfo.typeToDelegate?.asSafely<PsiClassType>()?.resolve()
+      if (psiClass != null) {
+        val feature = MLFeatureValue.categorical(classMapping[psiClass.qualifiedName] ?: KnownGradleClasses.OTHER)
+        features["groovy_delegate_type"] = feature
       }
     }
-    val parentMethodName = getContainingCall(parentClosure)?.resolveMethod()?.name
-    if (parentMethodName != null) {
-      features["groovy_owner_$parentMethodName"] = MLFeatureValue.binary(true)
+    val parentMethod = getContainingCall(parentClosure)?.resolveMethod()
+    if (parentMethod?.containingClass?.qualifiedName?.startsWith("org.gradle") == true) {
+      val feature = MLFeatureValue.categorical(methodMapping[parentMethod.name] ?: KnownGradleMethods.OTHER)
+      features["groovy_owner_method_name"] = feature
     }
   }
 
@@ -71,6 +74,42 @@ class GradleGroovyContextFeatureProvider : ContextFeatureProvider {
     } ?: "UNKNOWN"
 
     features["gradle_version"] = MLFeatureValue.version(gradleVersion)
+  }
+
+
+  companion object {
+    private enum class KnownGradleMethods(val methodName: String) {
+      DEPENDENCIES("dependencies"),
+      PLUGINS("plugins"),
+      REPOSITORIES("repositories"),
+      BUILDSCRIPT("buildscript"),
+      REGISTER("register"),
+      DEPENDENCY_RESOLUTION_MANAGEMENT("dependencyResolutionManagement"),
+      TASK("task"),
+      CONFIGURE_EACH("configureEach"),
+      OTHER("OTHER")
+    }
+
+    private val methodMapping: Map<String, KnownGradleMethods>
+      = KnownGradleMethods.values().associateBy(KnownGradleMethods::methodName)
+
+    private enum class KnownGradleClasses(val className: String) {
+      PROJECT(GRADLE_API_PROJECT),
+      CONFIGURATION(GRADLE_API_CONFIGURATION),
+      DEPENDENCY_HANDLER(GRADLE_API_DEPENDENCY_HANDLER),
+      REPOSITORY_HANDLER(GRADLE_API_REPOSITORY_HANDLER),
+      SOURCE_SET(GRADLE_API_SOURCE_SET),
+      DISTRIBUTION(GRADLE_API_DISTRIBUTION),
+      DISTRIBUTION_CONTAINER(GRADLE_API_DISTRIBUTION_CONTAINER),
+      TASK(GRADLE_API_TASK),
+      TASK_CONTAINER(GRADLE_API_TASK_CONTAINER),
+      DOMAIN_OBJECT_COLLECTION(GRADLE_API_DOMAIN_OBJECT_COLLECTION),
+      NAMED_DOMAIN_OBJECT_COLLECTION(GRADLE_API_NAMED_DOMAIN_OBJECT_COLLECTION),
+      OTHER("")
+    }
+
+    private val classMapping: Map<String, KnownGradleClasses>
+      = KnownGradleClasses.values().associateBy(KnownGradleClasses::className)
   }
 
 }
