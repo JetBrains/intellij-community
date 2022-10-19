96f7f7ef1a8b0c7d24ce389e1fa8153d7226f383
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2022-10-19 23:19:45 +0200

    IDEA-303227 The new menu in the new UI started taking priority over my keyboard shortcuts
    GitOrigin-RevId: 7c77134b160d420012ce77a6c441bee07e104e8e

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/customFrameDecorations/header/toolbar/MainMenuButton.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/customFrameDecorations/header/toolbar/MainMenuButton.kt
@@ -4,13 +4,13 @@ package com.intellij.openapi.wm.impl.customFrameDecorations.header.toolbar
 import com.intellij.icons.AllIcons
 import com.intellij.ide.DataManager
 import com.intellij.ide.IdeBundle
+import com.intellij.ide.ui.UISettings.Companion.getInstance
 import com.intellij.ide.ui.customization.CustomActionsSchema
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.impl.ActionButton
 import com.intellij.openapi.actionSystem.impl.PresentationFactory
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.keymap.Keymap
@@ -26,6 +26,8 @@ import com.intellij.util.messages.MessageBusConnection
 import com.intellij.util.ui.JBUI
 import org.jetbrains.annotations.ApiStatus
 import java.awt.Dimension
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
 import java.awt.event.HierarchyEvent
 import java.awt.event.KeyEvent
 import javax.swing.JComponent
@@ -38,6 +40,7 @@ internal class MainMenuButton {
   private val menuAction = ShowMenuAction()
   private var disposable: Disposable? = null
   private var shortcutsChangeConnection: MessageBusConnection? = null
+  private var registeredKeyStrokes = mutableListOf<KeyStroke>()
 
   val button: ActionButton = createMenuButton(menuAction)
   var rootPane: JComponent? = null
@@ -96,6 +99,12 @@ internal class MainMenuButton {
     disposable = null
     shortcutsChangeConnection?.let { Disposer.dispose(it) }
     shortcutsChangeConnection = null
+    rootPane?.let {
+      for (keyStroke in registeredKeyStrokes) {
+        it.unregisterKeyboardAction(keyStroke)
+      }
+    }
+    registeredKeyStrokes.clear()
   }
 
   private fun registerMenuButtonShortcut(component: JComponent) {
@@ -113,8 +122,9 @@ internal class MainMenuButton {
     for (action in mainMenu.getChildren(null)) {
       val mnemonic = action.templatePresentation.mnemonic
       if (mnemonic > 0) {
-        ShowSubMenu(action).registerCustomShortcutSet(CustomShortcutSet(KeyStroke.getKeyStroke(mnemonic, KeyEvent.ALT_DOWN_MASK)),
-                                                      component, disposable)
+        val keyStroke = KeyStroke.getKeyStroke(mnemonic, KeyEvent.ALT_DOWN_MASK)
+        component.registerKeyboardAction(ShowSubMenuAction(action), keyStroke, JComponent.WHEN_IN_FOCUSED_WINDOW)
+        registeredKeyStrokes.add(keyStroke)
       }
     }
   }
@@ -159,10 +169,12 @@ internal class MainMenuButton {
     }
   }
 
-  private inner class ShowSubMenu(private val actionToShow: AnAction) : AnAction() {
+  private inner class ShowSubMenuAction(private val actionToShow: AnAction) : ActionListener {
 
-    override fun actionPerformed(e: AnActionEvent) {
-      menuAction.showPopup(e.dataContext, actionToShow)
+    override fun actionPerformed(e: ActionEvent?) {
+      if (!getInstance().disableMnemonics) {
+        menuAction.showPopup(DataManager.getInstance().getDataContext(button), actionToShow)
+      }
     }
   }
 }
@@ -210,6 +222,6 @@ private fun AnAction.unwrap(): AnAction =
   else
     this
 
-const val MAIN_MENU_ACTION_ID = "MainMenuButton.ShowMenu"
+private const val MAIN_MENU_ACTION_ID = "MainMenuButton.ShowMenu"
 
-private val LOG = Logger.getInstance(MainMenuButton::class.java)
+private val LOG = logger<MainMenuButton>()
