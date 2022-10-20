b0bf8b5d2dc4246036105df15b2a1293503812d9
Andrey Uskov <andrey.uskov@jetbrains.com>
2022-10-20 14:56:33 +0400

    Implemented reporting of Kotlin compiler crashes in IdeErrorsDialog
    Implements IDEA-304248
    
    GitOrigin-RevId: 46759ea5ae26b4285486c3c7ee841b123a77e40a

---
+++ platform/platform-impl/src/com/intellij/diagnostic/KotlinCompilerCrash.kt
@@ -0,0 +1,5 @@
+// Copyright 2000-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.diagnostic;
+
+open class KotlinCompilerCrash(message: String, cause: Throwable?, val version: String) : Exception(message, cause, false, false)  {
+}
