a6eeed048fa2ff09a342f4fb277008d3d468f673
Andrey Sokolov <andrey.sokolov@jetbrains.com>
2022-10-20 17:55:30 +0200

    Similar usages: Heuristics for distinguish Variables from fields and class references.
    KTIJ-23321 Similar usages algorithm should process the first class reference.
    
    (cherry picked from commit e35f212e1be178fece16ae348e1870375e856100)
    
    IJ-CR-97638
    
    GitOrigin-RevId: ce6069f48b02dd655e656263c10eb18dc886ce8b

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/findUsages/similarity/KotlinSimilarityFeaturesExtractor.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/findUsages/similarity/KotlinSimilarityFeaturesExtractor.kt
@@ -2,7 +2,9 @@
 
 package org.jetbrains.kotlin.idea.findUsages.similarity
 
+import com.intellij.openapi.util.Condition
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.usages.similarity.bag.Bag
 import com.intellij.usages.similarity.features.UsageSimilarityFeaturesRecorder
 import org.jetbrains.kotlin.psi.*
@@ -10,8 +12,9 @@ import org.jetbrains.kotlin.psi.*
 class KotlinSimilarityFeaturesExtractor(element: PsiElement, context: PsiElement) : KtTreeVisitorVoid() {
     private val myUsageSimilarityFeaturesRecorder = UsageSimilarityFeaturesRecorder(context, element)
     private val myContext = context
-
+    private val myVariableNames = HashSet<String>()
     fun getFeatures(): Bag {
+        collectVariableNames()
         myContext.accept(this)
         return myUsageSimilarityFeaturesRecorder.features
     }
@@ -62,7 +65,7 @@ class KotlinSimilarityFeaturesExtractor(element: PsiElement, context: PsiElement
     override fun visitReferenceExpression(expression: KtReferenceExpression) {
         var feature = "REFERENCE: "
         val parent = expression.parent
-        if (fieldOrMethodReference(parent, expression) || parent is KtUserType) {
+        if (fieldOrMethodReference(expression) || parent is KtUserType) {
             if (expression is KtNameReferenceExpression) {
                 feature = "{REFERENCE: ${expression.getReferencedName()}}"
             }
@@ -71,7 +74,57 @@ class KotlinSimilarityFeaturesExtractor(element: PsiElement, context: PsiElement
         super.visitReferenceExpression(expression)
     }
 
-    private fun fieldOrMethodReference(parent: PsiElement?, expression: KtReferenceExpression) =
-        parent is KtQualifiedExpression && parent.firstChild != expression
+    private fun fieldOrMethodReference(expression: KtReferenceExpression) =
+        expression is KtNameReferenceExpression && !myVariableNames.contains(expression.getReferencedName())
+
+    private fun collectVariableNames() {
+        var scope = PsiTreeUtil.findFirstParent(myContext, true, Condition { e: PsiElement? -> e is KtFunction || e is KtFile })
+        while (scope != null) {
+            collectVariableNames(scope)
+            scope = PsiTreeUtil.findFirstParent(scope, true, Condition { e: PsiElement? -> e is KtFunction || e is KtFile })
+        }
+        val containingFile = myContext.containingFile
+        collectAllProperties(containingFile, myContext)
+    }
+
+    private fun collectVariableNames(
+        scope: PsiElement
+    ) {
+        if (scope is KtFunction) {
+            collectFunctionParameters(scope)
+        }
+        collectAllProperties(scope, myContext)
+    }
+
+    private fun collectFunctionParameters(scope: KtFunction) {
+        val valueParameterList = scope.valueParameterList
+        if (valueParameterList != null) {
+            for (typeParameter in valueParameterList.parameters) {
+                typeParameter.name?.let { myVariableNames.add(it) }
+            }
+        }
+    }
+
+    private fun collectAllProperties(
+        scope: PsiElement,
+        context: PsiElement
+    ) {
+        scope.accept(object : KtTreeVisitorVoid() {
+            override fun visitProperty(property: KtProperty) {
+                val startOffset = context.textRange.startOffset
+                if (property.textRange.startOffset < startOffset) {
+                    property.name?.let { myVariableNames.add(it) }
+                }
+                super.visitProperty(property)
+            }
+
+            override fun visitElement(element: PsiElement) {
+                if (element !is KtFunction || element == scope) {
+                    super.visitElement(element)
+                }
+            }
+
+        })
+    }
 
 }
