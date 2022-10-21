830ef6bc787032a85c7a785871e1d42afce2ce3f
Tagir Valeev <tagir.valeev@jetbrains.com>
2022-10-21 11:19:28 +0200

    ExpandableEditorSupport: restore caret position and selection in case if transformation function is applied
    Inject tags to store positions, then restore them from tags.
    
    GitOrigin-RevId: fba5f709ca883b5090343952bdb8e2db4e592713

--- platform/platform-impl/src/com/intellij/ui/ExpandableEditorSupport.java
+++ platform/platform-impl/src/com/intellij/ui/ExpandableEditorSupport.java
@@ -1,7 +1,9 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui;
 
+import com.intellij.openapi.editor.CaretState;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.LogicalPosition;
 import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.editor.ex.FoldingModelEx;
 import com.intellij.openapi.util.text.StringUtil;
@@ -9,13 +11,17 @@ import com.intellij.ui.components.JBScrollBar;
 import com.intellij.ui.components.fields.ExpandableSupport;
 import com.intellij.ui.components.fields.ExtendableTextComponent;
 import com.intellij.util.Function;
+import com.intellij.util.Functions;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 import java.util.List;
-import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static java.util.Arrays.asList;
 
@@ -80,7 +86,7 @@ public class ExpandableEditorSupport extends ExpandableSupport<EditorTextField> 
     popup.setPreferredSize(new Dimension(field.getWidth(), 5 * field.getHeight()));
     popup.addSettingsProvider(editor -> {
       initPopupEditor(editor, background);
-      copyCaretPosition(editor, field.getEditor());
+      copyCaretPosition(editor, field.getEditor(), onShow);
     });
     return new Content() {
       @NotNull
@@ -98,7 +104,7 @@ public class ExpandableEditorSupport extends ExpandableSupport<EditorTextField> 
       public void cancel(@NotNull Function<? super String, String> onHide) {
         field.setText(onHide.fun(popup.getText()));
         Editor editor = field.getEditor();
-        if (editor != null) copyCaretPosition(editor, popup.getEditor());
+        if (editor != null) copyCaretPosition(editor, popup.getEditor(), onHide);
         if (editor instanceof EditorEx) updateFieldFolding((EditorEx)editor);
       }
     };
@@ -114,13 +120,100 @@ public class ExpandableEditorSupport extends ExpandableSupport<EditorTextField> 
     }
   }
 
-  private static void copyCaretPosition(@NotNull Editor destination, Editor source) {
+  private static void copyCaretPosition(@NotNull Editor destination, Editor source, Function<? super String, String> mapper) {
     if (source == null) return; // unexpected
     try {
-      destination.getCaretModel().setCaretsAndSelections(
-        source.getCaretModel().getCaretsAndSelections());
+      List<CaretState> states = source.getCaretModel().getCaretsAndSelections();
+      if (!mapper.equals(Functions.identity())) {
+        states = extractStates(mapper.fun(injectStates(source.getDocument().getText(), states)));
+      }
+      destination.getCaretModel().setCaretsAndSelections(states);
     }
     catch (IllegalArgumentException ignored) {
     }
+  }
+
+  private static final Pattern TAG_PATTERN = Pattern.compile("([cse])(\\d{1,3})");
+
+  private static List<CaretState> extractStates(String text) {
+    int x = 0;
+    int y = 0;
+    List<LogicalPosition[]> list = new ArrayList<>();
+    for (int i = 0; i < text.length(); i++) {
+      char c = text.charAt(i);
+      if (c == '\0') {
+        int next = text.indexOf('\0', i + 1);
+        if (next != -1) {
+          String tag = text.substring(i + 1, next);
+          Matcher matcher = TAG_PATTERN.matcher(tag);
+          if (matcher.matches()) {
+            String what = matcher.group(1);
+            int idx = switch (what) {
+              case "c" -> 0; // caret
+              case "s" -> 1; // selection start
+              case "e" -> 2; // selection end
+              default -> throw new AssertionError("Unexpected value: " + what);
+            };
+            int offset = Integer.parseInt(matcher.group(2));
+            while (list.size() <= offset) {
+              list.add(new LogicalPosition[3]);
+            }
+            list.get(offset)[idx] = new LogicalPosition(y, x); 
+          }
+          //noinspection AssignmentToForLoopParameter
+          i = next;
+        }
+        continue;
+      }
+      if (c == '\n') {
+        y++;
+        x = 0;
+      }
+      else {
+        x++;
+      }
+    }
+    return ContainerUtil.map(list, arr -> new CaretState(arr[0], arr[1], arr[2]));
+  }
+
+  private static String injectStates(String text, List<CaretState> states) {
+    if (states.isEmpty()) return text;
+    record Point(int x, int y) {
+      static Point from(LogicalPosition pos) {
+        return new Point(pos.column, pos.line);
+      }
+    }
+    Map<Point, String> tags = new HashMap<>();
+    for (int i = 0; i < states.size(); i++) {
+      CaretState state = states.get(i);
+      LogicalPosition caretPosition = state.getCaretPosition();
+      if (caretPosition != null) {
+        tags.merge(Point.from(caretPosition), "\0c" + i + "\0", String::concat);
+      }
+      LogicalPosition selectionStart = state.getSelectionStart();
+      if (selectionStart != null) {
+        tags.merge(Point.from(selectionStart), "\0s" + i + "\0", String::concat);
+      }
+      LogicalPosition selectionEnd = state.getSelectionEnd();
+      if (selectionEnd != null) {
+        tags.merge(Point.from(selectionEnd), "\0e" + i + "\0", String::concat);
+      }
+    }
+    StringBuilder builder = new StringBuilder(text.length() + tags.values().stream().mapToInt(String::length).sum());
+    int x = 0;
+    int y = 0;
+    for (int i = 0; i < text.length(); i++) {
+      builder.append(tags.getOrDefault(new Point(x, y), ""));
+      char c = text.charAt(i);
+      if (c == '\n') {
+        y++;
+        x = 0;
+      }
+      else {
+        x++;
+      }
+      builder.append(c);
+    }
+    return builder.toString();
   }
 }
