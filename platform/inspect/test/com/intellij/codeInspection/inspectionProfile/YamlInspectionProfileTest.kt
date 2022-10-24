17ec4a09e5d56417eeee9963746207bc0aad31ba
Alexandr Suhinin <alexandr.suhinin@jetbrains.com>
2022-10-24 21:32:51 +0300

    IDEA-303609 [headless inspection] yaml profile: add tests
    GitOrigin-RevId: 16752e48f9bc9f21beb0dee2c3fdb700145bc361

---
+++ platform/inspect/test/com/intellij/codeInspection/inspectionProfile/YamlInspectionProfileTest.kt
@@ -0,0 +1,42 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.codeInspection.inspectionProfile
+
+import com.intellij.codeInspection.ex.InspectionProfileImpl
+import com.intellij.openapi.util.JDOMUtil
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.testFramework.LightPlatformTestCase
+import junit.framework.TestCase
+import org.jdom.Element
+import java.io.File
+
+class YamlInspectionProfileTest: LightPlatformTestCase() {
+
+  override fun getTestDirectoryName(): String = "testData/"
+
+  fun testBasic(){ checkEffectiveProfile() }
+
+  fun testChainedInclude(){ checkEffectiveProfile() }
+
+  fun testChainedOverride(){ checkEffectiveProfile() }
+
+  fun testGroup(){ checkEffectiveProfile() }
+
+  fun testPredefinedGroup(){ checkEffectiveProfile() }
+
+  fun testGroupExclusion(){ checkEffectiveProfile() }
+
+  private fun checkEffectiveProfile(){
+    val fileName = getTestName(true)
+    val expectedProfileText = FileUtil.loadFile(File("$testDirectoryName/${fileName}_profile.xml"))
+    InspectionProfileImpl.INIT_INSPECTIONS = true
+    val effectiveProfile = YamlInspectionProfileImpl.loadFrom(project, "$testDirectoryName/$fileName.yml").buildEffectiveProfile()
+    val externalizedText = getExternalizedText(effectiveProfile)
+    TestCase.assertEquals(expectedProfileText, externalizedText)
+  }
+
+  private fun getExternalizedText(profile: InspectionProfileImpl): String {
+    val element = Element("profile")
+    profile.writeExternal(element)
+    return JDOMUtil.write(element)
+  }
+}
