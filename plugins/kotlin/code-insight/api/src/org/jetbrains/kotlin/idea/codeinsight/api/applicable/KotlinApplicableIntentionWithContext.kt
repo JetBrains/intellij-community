42253325a2fb5dd0a779339dd3a7bbfa534bb411
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2022-10-25 19:11:51 +0200

    [kotlin] Add new KotlinApplicableIntention API
    - This API is supposed to replace the applicator-based intention API. In general, the API has been designed by taking the good parts of applicators (esp. its separation of concerns and ease of use for simple intentions). The result is a more straight-forward, class-based API that should eliminate some pain points that came with applicators (such as opaque/awkward composition, `return@` spam, excessive type arguments, and empty inputs).
    - The new API provides a separate class `KotlinApplicableIntentionWithContext` for those intentions which would previously use an input provider to compute some meaningful applicator input. Many intentions don't need this kind of context and can be based on the simpler `KotlinApplicableIntention`, removing the need for an "empty input".
    
    GitOrigin-RevId: 96c92db25bd6a2929329f143f66fb6b177ef8b1d

---
+++ plugins/kotlin/code-insight/api/src/org/jetbrains/kotlin/idea/codeinsight/api/applicable/KotlinApplicableIntentionWithContext.kt
@@ -0,0 +1,77 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.codeinsight.api.applicable
+
+import com.intellij.codeInspection.util.IntentionName
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.analysis.api.KtAllowAnalysisOnEdt
+import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.lifetime.allowAnalysisOnEdt
+import org.jetbrains.kotlin.idea.util.application.runReadAction
+import org.jetbrains.kotlin.idea.util.application.runWriteActionIfPhysical
+import org.jetbrains.kotlin.psi.KtElement
+import kotlin.reflect.KClass
+
+/**
+ * Applies a fix to the PSI with [apply] given some [Context] from [getContext] if the intention is applicable via [isApplicableByPsi] and
+ * [getContext].
+ */
+abstract class KotlinApplicableIntentionWithContext<Element : KtElement, Context>(
+    elementType: KClass<Element>,
+) : KotlinApplicableIntentionBase<Element>(elementType) {
+    /**
+     * @see com.intellij.codeInsight.intention.IntentionAction.getText
+     */
+    abstract fun getActionName(element: Element, context: Context): @IntentionName String
+
+    /**
+     * Provides some context for [apply]. If the intention is not applicable (by analyze), [getContext] should return `null`. Guaranteed to
+     * be executed from a read action.
+     *
+     * The context should not store:
+     * - Everything that came from [org.jetbrains.kotlin.analysis.api.KtAnalysisSession] like:
+     *      - [org.jetbrains.kotlin.analysis.api.symbols.KtSymbol], consider using [org.jetbrains.kotlin.analysis.api.symbols.pointers.KtSymbolPointer] instead.
+     *      - [org.jetbrains.kotlin.analysis.api.types.KtType]
+     *      - [org.jetbrains.kotlin.analysis.api.calls.KtCall]
+     * - The [org.jetbrains.kotlin.analysis.api.KtAnalysisSession] instance itself.
+     * - [PsiElement], consider using [com.intellij.psi.SmartPsiElementPointer] instead.
+     */
+    context(KtAnalysisSession)
+    abstract fun getContext(element: Element): Context?
+
+    /**
+     * Applies a fix to [element] using information from [context]. [apply] should not use the Analysis API due to performance concerns, as
+     * [apply] is usually executed on the EDT. Any information that needs to come from the Analysis API should be supplied via [getContext].
+     *
+     * [apply] is executed in a write action when [element] is physical.
+     */
+    abstract fun apply(element: Element, context: Context, project: Project, editor: Editor?)
+
+    final override fun isApplicableTo(element: Element, caretOffset: Int): Boolean {
+        if (!super.isApplicableTo(element, caretOffset)) return false
+        val context = getContextWithAnalyze(element, needsReadAction = false) ?: return false
+        val actionText = getActionName(element, context)
+        setTextGetter { actionText }
+        return true
+    }
+
+    final override fun applyTo(element: Element, project: Project, editor: Editor?) {
+        val context = getContextWithAnalyze(element, needsReadAction = true) ?: return
+        runWriteActionIfPhysical(element) {
+            apply(element, context, project, editor)
+        }
+    }
+
+    final override fun startInWriteAction(): Boolean =
+        // `applyTo` should start without a write action because it first uses `analyzeWithReadAction` to get the context. Also,
+        // `getContext` when called from `applyTo` should not have access to a write action for `element` to discourage mutating `element`.
+        false
+
+    @OptIn(KtAllowAnalysisOnEdt::class)
+    private fun getContextWithAnalyze(element: Element, needsReadAction: Boolean): Context? = allowAnalysisOnEdt {
+        fun action() = analyze(element) { getContext(element) }
+        if (needsReadAction) runReadAction { action() } else action()
+    }
+}
