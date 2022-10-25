1950dfb1c14f1913b215d24a5f179d6c7d4d4f0b
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2022-10-25 12:14:14 +0300

    [platform UI] IDEA-304431 Refactoring: get rid of a confusing method
     It was very hard to read. The logic was as follows:
     myShrinkingDisabled = isClientPropertyFalse(component, SHRINK_LONG_RENDERER, strict: false)
     The strict parameter is set to false, therefore, the SHRINK_LONG_RENDERER property
     value is compared to TRUE, and, since it's null, it's not equal to true, which means the result is
     false, but it's then negated, so it's true, which means shrinking is disabled.
    
     Now it's simply:
     myShrinkingDisabled = !ClientProperty.isTrue(component, SHRINK_LONG_RENDERER)
     If shrinking is not explicitly set to true, disable shrinking. The removal of the strict parameter
     really helps a lot here.
    
     The logic of calculating the right margin is now more complicated, but we're about to change it,
     and the sole purpose of this refactoring is to make it easier.
    
    GitOrigin-RevId: d14e8880a22eabe51091bfa3620b30d5e629903b

--- platform/platform-impl/src/com/intellij/ui/render/RenderingHelper.java
+++ platform/platform-impl/src/com/intellij/ui/render/RenderingHelper.java
@@ -2,16 +2,14 @@
 package com.intellij.ui.render;
 
 import com.intellij.openapi.util.Key;
+import com.intellij.ui.ClientProperty;
 import com.intellij.ui.ComponentUtil;
 import com.intellij.ui.ComponentWithExpandableItems;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.JComponent;
-import javax.swing.JScrollBar;
-import javax.swing.JScrollPane;
-import javax.swing.JViewport;
-import java.awt.Rectangle;
+import javax.swing.*;
+import java.awt.*;
 import java.util.Collection;
 
 import static com.intellij.openapi.util.SystemInfo.isMac;
@@ -41,10 +39,10 @@ public final class RenderingHelper {
       if (pane != null) {
         JScrollBar hsb = pane.getHorizontalScrollBar();
         if (hsb != null && hsb.isVisible()) {
-          myShrinkingDisabled = isClientPropertyFalse(component, SHRINK_LONG_RENDERER, false);
+          myShrinkingDisabled = !ClientProperty.isTrue(component, SHRINK_LONG_RENDERER);
         }
         JScrollBar vsb = pane.getVerticalScrollBar();
-        if (vsb != null && vsb.isVisible() && !vsb.isOpaque() && isClientPropertyFalse(vsb, IGNORE_SCROLLBAR_IN_INSETS, isMac)) {
+        if (vsb != null && vsb.isVisible() && !vsb.isOpaque() && (isMac ? ClientProperty.isFalse(vsb, IGNORE_SCROLLBAR_IN_INSETS) : !ClientProperty.isTrue(vsb, IGNORE_SCROLLBAR_IN_INSETS))) {
           myRightMargin = vsb.getWidth();
         }
       }
@@ -87,10 +85,5 @@ public final class RenderingHelper {
       if (item instanceof Integer) return (Integer)item;
     }
     return -1;
-  }
-
-  private static boolean isClientPropertyFalse(@NotNull JComponent component, @NotNull Object key, boolean strict) {
-    Object property = component.getClientProperty(key);
-    return strict ? Boolean.FALSE.equals(property) : !Boolean.TRUE.equals(property);
   }
 }
