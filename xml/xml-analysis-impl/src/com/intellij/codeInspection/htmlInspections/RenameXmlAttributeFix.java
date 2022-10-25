2e9152aac123c6663fb30162bf94da4f6aca05d9
Sofya.Povarova <sofya.povarova@jetbrains.com>
2022-10-25 11:26:11 +0200

    WEB-45579 Support an ability to replace class attribute by className in React
    (cherry picked from commit 4f6118d2f3b017c3178470e190585573e7d2f648)
    
    IJ-MR-97111
    
    GitOrigin-RevId: 2064974bdf345c516e6619f9b22890ece858df3d

---
+++ xml/xml-analysis-impl/src/com/intellij/codeInspection/htmlInspections/RenameXmlAttributeFix.java
@@ -0,0 +1,42 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.codeInspection.htmlInspections;
+
+import com.intellij.codeInsight.intention.HighPriorityAction;
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.xml.XmlAttribute;
+import com.intellij.xml.XmlAttributeDescriptor;
+import com.intellij.xml.analysis.XmlAnalysisBundle;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+
+public class RenameXmlAttributeFix implements LocalQuickFix, HighPriorityAction {
+  private final String name;
+
+  public RenameXmlAttributeFix(XmlAttributeDescriptor attr) {
+    name = attr.getName();
+  }
+
+  @Nls
+  @NotNull
+  @Override
+  public String getFamilyName() {
+    return XmlAnalysisBundle.message("html.quickfix.rename.attribute.family");
+  }
+
+  @Nls
+  @NotNull
+  @Override
+  public String getName() {
+    return XmlAnalysisBundle.message("html.quickfix.rename.attribute.text", name);
+  }
+
+  @Override
+  public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+    XmlAttribute attribute = PsiTreeUtil.getParentOfType(descriptor.getPsiElement(), XmlAttribute.class);
+    if (attribute == null) return;
+    attribute.setName(name);
+  }
+}
