3ebb305fcdff896f7024953d1fa280f7da31252d
Elena Artamonova <elena.artamonova@jetbrains.com>
2022-10-25 16:18:07 +0200

    AT-90 fix problem with running several tests on dev server and problem with downloading plugin
    GitOrigin-RevId: 83b22525f3506f94f836f99a0beab3284a3fff65

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/CommandsRunner.java
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/CommandsRunner.java
@@ -3,6 +3,9 @@ package com.jetbrains.performancePlugin;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.util.ActionCallback;
 import org.jetbrains.annotations.NotNull;
+import oshi.SystemInfo;
+import oshi.software.os.OSProcess;
+import oshi.software.os.OperatingSystem;
 
 import static com.jetbrains.performancePlugin.ProjectLoaded.runScript;
 
@@ -31,5 +34,12 @@ public class CommandsRunner {
   public static boolean areCommandsAfterStartFinished() {
     if (getInstance().startActionCallback == null) return false;
     return getInstance().startActionCallback.isDone();
+  }
+
+  public static int getPid() {
+    SystemInfo si = new SystemInfo();
+    OperatingSystem os = si.getOperatingSystem();
+    OSProcess myProc = os.getProcess(os.getProcessId());
+    return myProc.getProcessID();
   }
 }
