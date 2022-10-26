3b204d38ffdff96f23fa53fcb5696ed865b556b4
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2022-10-26 11:29:37 +0200

    [java] access ProjectFileIndex under read action (IDEA-304640)
    (cherry picked from commit 090cd9345eb057d38343d41c2cfce1539edecda4)
    
    IJ-CR-97542
    
    GitOrigin-RevId: 1a0ae6af09aecc84c654104bd60c07832bb9ee09

--- java/compiler/impl/src/com/intellij/compiler/impl/CompileDriver.java
+++ java/compiler/impl/src/com/intellij/compiler/impl/CompileDriver.java
@@ -559,12 +559,14 @@ public final class CompileDriver {
     compileContext.getBuildSession().setEndCompilationStamp(_status, endCompilationStamp);
     final long duration = endCompilationStamp - compileContext.getStartCompilationStamp();
     if (!myProject.isDisposed()) {
-      // refresh on output roots is required in order for the order enumerator to see all roots via VFS
-      final Module[] affectedModules = compileContext.getCompileScope().getAffectedModules();
 
       if (_status != ExitStatus.UP_TO_DATE && _status != ExitStatus.CANCELLED) {
+        // refresh on output roots is required in order for the order enumerator to see all roots via VFS
         // have to refresh in case of errors too, because run configuration may be set to ignore errors
-        Collection<String> affectedRoots = ContainerUtil.newHashSet(CompilerPaths.getOutputPaths(affectedModules));
+        Collection<String> affectedRoots = ReadAction.compute(() -> {
+          Module[] affectedModules = compileContext.getCompileScope().getAffectedModules();
+          return ContainerUtil.newHashSet(CompilerPaths.getOutputPaths(affectedModules));
+        });
         if (!affectedRoots.isEmpty()) {
           ProgressIndicator indicator = compileContext.getProgressIndicator();
           indicator.setText(JavaCompilerBundle.message("synchronizing.output.directories"));
