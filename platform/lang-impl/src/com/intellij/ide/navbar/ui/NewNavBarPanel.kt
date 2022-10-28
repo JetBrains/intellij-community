ed82be99582910f97df47f26f4849fa93cfc4797
Aleksei Pomelov <aleksei.pomelov@jetbrains.com>
2022-10-28 17:06:09 +0300

    IDEA-304748 Restore revalidate/repaint calls for proper rendering
    GitOrigin-RevId: 75df1f81bad9e682b3caffc91ccf3bf84b032df2

--- platform/lang-impl/src/com/intellij/ide/navbar/ui/NewNavBarPanel.kt
+++ platform/lang-impl/src/com/intellij/ide/navbar/ui/NewNavBarPanel.kt
@@ -136,6 +136,9 @@ internal class NewNavBarPanel(
       myItemComponents.add(itemComponent)
     }
 
+    revalidate()
+    repaint()
+
     onSizeChange?.run()
     while (!isValid) {
       yield()
