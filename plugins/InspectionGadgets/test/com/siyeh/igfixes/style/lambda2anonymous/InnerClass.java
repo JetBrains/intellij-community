10f8b83aa5d20e4c700e67ca291e29bde29f5a83
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2022-10-28 10:39:00 +0200

    [java] Add simple inner class test to lambda to anonymous class fix
    GitOrigin-RevId: 43ffe8012852038ad7d561f4e0e2ee06f77814c9

--- plugins/InspectionGadgets/test/com/siyeh/igfixes/style/lambda2anonymous/InnerClass.java
+++ plugins/InspectionGadgets/test/com/siyeh/igfixes/style/lambda2anonymous/InnerClass.java
@@ -1,9 +1,9 @@
 import java.io.Serializable;
 
-public abstract class LambdaRefInnerClass<T extends Serializable> {
+public abstract class LambdaRefInnerClass {
     public abstract class Holder { }
 
-    public static final LongKey<LambdaRefInnerClass.Holder> func = va<caret>lue -> 0L;
+    public static final LongKey<Holder> func = va<caret>lue -> 0L;
 }
 
 interface LongKey<V> extends Serializable {
