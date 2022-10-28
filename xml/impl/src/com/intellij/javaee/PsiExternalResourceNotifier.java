aa5b253418bd07bd1d06be6434942186fe260c19
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-10-28 11:44:13 +0200

    convert PsiExternalResourceNotifier to project post-startup activity
    GitOrigin-RevId: 0d543efb4af00f4cccc3ec2d807225607d223e2b

--- xml/impl/src/com/intellij/javaee/PsiExternalResourceNotifier.java
+++ xml/impl/src/com/intellij/javaee/PsiExternalResourceNotifier.java
@@ -1,14 +1,16 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.javaee;
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.psi.impl.PsiManagerEx;
 import org.jetbrains.annotations.NotNull;
 
-final class PsiExternalResourceNotifier {
-  PsiExternalResourceNotifier(@NotNull Project project) {
-    project.getMessageBus().connect().subscribe(ExternalResourceListener.TOPIC, () -> {
+final class PsiExternalResourceNotifier implements StartupActivity.DumbAware {
+  @Override
+  public void runActivity(@NotNull Project project) {
+    project.getMessageBus().simpleConnect().subscribe(ExternalResourceListener.TOPIC, () -> {
       if (!project.isDisposed()) {
         PsiManagerEx.getInstanceEx(project).beforeChange(true);
         DaemonCodeAnalyzer.getInstance(project).restart();
