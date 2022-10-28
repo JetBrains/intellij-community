e10627ce0591aa06ca6603df11bdc737c46fc5e3
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-10-28 12:20:10 +0200

    convert PsiExternalResourceNotifier to project post-startup activity
    GitOrigin-RevId: 0c944f5ce5ff1413e008fb0f2bf7ec4c09605ada

--- xml/impl/src/com/intellij/javaee/PsiExternalResourceNotifier.kt
+++ xml/impl/src/com/intellij/javaee/PsiExternalResourceNotifier.kt
@@ -1,20 +1,18 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.javaee;
+package com.intellij.javaee
 
-import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.psi.impl.PsiManagerEx;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectPostStartupActivity
+import com.intellij.psi.impl.PsiManagerEx
 
-final class PsiExternalResourceNotifier implements StartupActivity.DumbAware {
-  @Override
-  public void runActivity(@NotNull Project project) {
-    project.getMessageBus().simpleConnect().subscribe(ExternalResourceListener.TOPIC, () -> {
-      if (!project.isDisposed()) {
-        PsiManagerEx.getInstanceEx(project).beforeChange(true);
-        DaemonCodeAnalyzer.getInstance(project).restart();
+private class PsiExternalResourceNotifier : ProjectPostStartupActivity {
+  override suspend fun execute(project: Project) {
+    project.messageBus.simpleConnect().subscribe(ExternalResourceListener.TOPIC, ExternalResourceListener {
+      if (!project.isDisposed) {
+        PsiManagerEx.getInstanceEx(project).beforeChange(true)
+        DaemonCodeAnalyzer.getInstance(project).restart()
       }
-    });
+    })
   }
 }
