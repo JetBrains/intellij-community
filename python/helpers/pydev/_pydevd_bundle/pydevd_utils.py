3d695d1f3d5d22801ca3098b6f6b7b19ea8d399a
Guo Ci <zguoci@gmail.com>
2022-10-30 17:36:11 -0400

    PY-56361
    Fix for PY-56361
    https://youtrack.jetbrains.com/issue/PY-56361/pandas-Series-iteritems-in-pydevdutilspy-FutureWarning

--- python/helpers/pydev/_pydevd_bundle/pydevd_utils.py
+++ python/helpers/pydev/_pydevd_bundle/pydevd_utils.py
@@ -606,7 +606,7 @@ def should_evaluate_shape():
 def _series_to_str(s, max_items):
     res = []
     s = s[:max_items]
-    for item in s.iteritems():
+    for item in s.items():
         # item: (index, value)
         res.append(str(item))
     return ' '.join(res)
