8d4d10abc78fdc93d3f0c15fdc3578c88dacc108
Roland Illig <roland.illig@jetbrains.com>
2022-11-01 16:24:11 +0100

    [qodana] Don't print stack trace on invalid inspection name
    GitOrigin-RevId: 6c9fca5548a67c001f802e0c8c7f5532f2ebdda7

--- platform/inspect/src/com/intellij/codeInspection/InspectionApplicationException.java
+++ platform/inspect/src/com/intellij/codeInspection/InspectionApplicationException.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInspection;
 
+/** Terminates the process with the given message, without a stack trace. */
 public class InspectionApplicationException extends Exception {
   public InspectionApplicationException(String message) {
     super(message);
