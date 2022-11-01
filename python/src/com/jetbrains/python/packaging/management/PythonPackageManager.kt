e3968287ae290dc138edb59accfe55d98c1c8bdd
Aleksei Kniazev <aleksei.kniazev@jetbrains.com>
2022-11-01 12:54:56 +0100

    [python] associate python package manager with a correct project instance (PY-49324)
    - on project reopening, new package manager should be created with a new instance of project
    - replace PackageManagerHolder kotlin object with service
    
    (cherry picked from commit 62f43d861dd5b31ece6f4680a54064f0581706b7)
    
    IJ-MR-96966
    
    GitOrigin-RevId: f2266f33265f83df8276dc288c486db907a0bb97

--- python/src/com/jetbrains/python/packaging/management/PythonPackageManager.kt
+++ python/src/com/jetbrains/python/packaging/management/PythonPackageManager.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.packaging.management
 
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.util.messages.Topic
@@ -23,7 +24,7 @@ abstract class PythonPackageManager(val project: Project, val sdk: Sdk) {
 
   companion object {
     fun forSdk(project: Project, sdk: Sdk): PythonPackageManager? {
-      return PackageManagerHolder.forSdk(project, sdk)
+      return service<PackageManagerHolder>().forSdk(project, sdk)
     }
 
     @Topic.AppLevel
