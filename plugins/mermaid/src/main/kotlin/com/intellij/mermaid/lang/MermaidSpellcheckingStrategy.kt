c6166b90b7ec9249167bf87accf71c08f0f75d1a
Ekaterina Berezhko <Ekaterina.Berezhko@jetbrains.com>
2022-11-02 09:51:50 +0200

    Add MermaidSpellcheckingStrategy
    GitOrigin-RevId: 18a39b898d8bbf522d659ef5e9e867dd72edd4ee

---
+++ plugins/mermaid/src/main/kotlin/com/intellij/mermaid/lang/MermaidSpellcheckingStrategy.kt
@@ -0,0 +1,29 @@
+package com.intellij.mermaid.lang
+
+import com.intellij.mermaid.lang.lexer.MermaidTokens.ALIAS
+import com.intellij.mermaid.lang.lexer.MermaidTokens.ClassDiagram.CLASS_ID
+import com.intellij.mermaid.lang.lexer.MermaidTokens.Flowchart.LINK_TEXT
+import com.intellij.mermaid.lang.lexer.MermaidTokens.ID
+import com.intellij.mermaid.lang.lexer.MermaidTokens.LABEL
+import com.intellij.mermaid.lang.lexer.MermaidTokens.LINE_COMMENT
+import com.intellij.mermaid.lang.lexer.MermaidTokens.NOTE_CONTENT
+import com.intellij.mermaid.lang.lexer.MermaidTokens.SECTION_TITLE
+import com.intellij.mermaid.lang.lexer.MermaidTokens.STRING_VALUE
+import com.intellij.mermaid.lang.lexer.MermaidTokens.Sequence.MESSAGE
+import com.intellij.mermaid.lang.lexer.MermaidTokens.TASK_NAME
+import com.intellij.mermaid.lang.lexer.MermaidTokens.TITLE_VALUE
+import com.intellij.psi.PsiElement
+import com.intellij.psi.tree.TokenSet
+import com.intellij.psi.util.elementType
+import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy
+import com.intellij.spellchecker.tokenizer.Tokenizer
+
+class MermaidSpellcheckingStrategy : SpellcheckingStrategy() {
+  private val TOKENS_WITH_TEXT =
+    TokenSet.create(LINE_COMMENT, TITLE_VALUE, ID, CLASS_ID, ALIAS, NOTE_CONTENT, SECTION_TITLE, TASK_NAME, LINK_TEXT, MESSAGE, LABEL, STRING_VALUE)
+
+  override fun getTokenizer(element: PsiElement?): Tokenizer<*> {
+    if (element.elementType in TOKENS_WITH_TEXT) return TEXT_TOKENIZER
+    return EMPTY_TOKENIZER
+  }
+}
