a5bd898ca1e3729ff680ce1b9bdc904f562d749f
Kirill.Krylov <kirill.krylov@jetbrains.com>
2022-11-02 12:36:32 +0100

    [full-line] add simple test for py full line
    GitOrigin-RevId: bb2e65239cac529aef85276ae38bc59154048944

--- plugins/full-line/local/test/org/jetbrains/completion/full/line/local/model/FullLineTest.kt
+++ plugins/full-line/local/test/org/jetbrains/completion/full/line/local/model/FullLineTest.kt
@@ -1,22 +1,23 @@
 package org.jetbrains.completion.full.line.local.model
 
+import com.intellij.testFramework.UsefulTestCase
 import org.jetbrains.completion.full.line.local.CompletionModelFactory
 import org.jetbrains.completion.full.line.local.ModelsFiles
 import org.jetbrains.completion.full.line.local.TestExecutionContext
 import org.jetbrains.completion.full.line.local.pipeline.FullLineCompletionPipelineConfig
 import org.junit.jupiter.api.Test
 
-class FullLineTest {
+class FullLineTest : UsefulTestCase() {
   companion object {
     private val modelFiles = ModelsFiles.gpt2_py_4L_512_83_q_local
     val context = """
             def hello_
         """.trimIndent()
-    const val prefix = "_"
+    const val prefix = "hello_"
   }
 
   @Test
-  fun errorTest() {
+  fun errorTest() = assertNoException<RuntimeException>(AssertionError::class.java) {
     val completionModel = CompletionModelFactory.createFullLineCompletionModel(
       modelFiles.tokenizer,
       modelFiles.model,
@@ -31,5 +32,25 @@ class FullLineTest {
     )
 
     print(completions.map { it.text })
+  }
+
+  fun `test simple python completion`() {
+    val completionModel = CompletionModelFactory.createFullLineCompletionModel(
+      modelFiles.tokenizer,
+      modelFiles.model,
+      modelFiles.config
+    )
+    val content = """
+      if 
+    """.trimIndent()
+    val prefix = ""
+
+    val completions = completionModel.generateCompletions(
+      content, prefix,
+      FullLineCompletionPipelineConfig(maxLen = 8, filename = "main.py"),
+      TestExecutionContext.default
+    )
+
+    assertContainsElements(completions.map { it.text }, "__name__ == \"__main__\":")
   }
 }
