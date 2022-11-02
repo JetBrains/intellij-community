56048b0706d8b4bdc871cffd87b22295ec1f736e
Roman Golyshev <roman.golyshev@jetbrains.com>
2022-11-02 15:51:41 +0100

    [kotlin] KTIJ-20439 Add some kdoc to `KotlinDirectoryAsPackageRenameHandler`
    GitOrigin-RevId: 000bb17c6729589e0a85812a59eb1e47cf86909d

--- plugins/kotlin/refactorings/kotlin.refactorings.common/src/org/jetbrains/kotlin/idea/refactoring/rename/KotlinDirectoryAsPackageRenameHandler.kt
+++ plugins/kotlin/refactorings/kotlin.refactorings.common/src/org/jetbrains/kotlin/idea/refactoring/rename/KotlinDirectoryAsPackageRenameHandler.kt
@@ -7,6 +7,14 @@ import com.intellij.refactoring.rename.DirectoryAsPackageRenameHandler
 import org.jetbrains.kotlin.psi.psiUtil.isIdentifier
 import org.jetbrains.kotlin.psi.psiUtil.quoteIfNeeded
 
+/**
+ * Allows to use [DirectoryAsPackageRenameHandler]'s logic even if the name of the package is not a valid Java identifier.
+ *
+ * Without it, the platform will resort to a regular PSI element rename mechanism, which will not properly update packages.
+ *
+ * N.B. This implementation is supposed to replace the platform implementation of [DirectoryAsPackageRenameHandler],
+ * so it has to be registered after it, so it can replace it in [com.intellij.refactoring.rename.RenameHandlerRegistry].
+ */
 class KotlinDirectoryAsPackageRenameHandler : DirectoryAsPackageRenameHandler() {
     override fun isIdentifier(name: String, project: Project): Boolean = name.quoteIfNeeded().isIdentifier()
 }
