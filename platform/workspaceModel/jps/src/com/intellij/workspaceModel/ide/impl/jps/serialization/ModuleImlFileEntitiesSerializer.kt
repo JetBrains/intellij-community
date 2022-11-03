0ca9da5d61629af00f117193ab05f53201464b30
Alex Plate <aleksei.plate@jetbrains.com>
2022-11-03 14:26:54 +0200

    [Workspace Model] [IJ-CR-97661] Remove unnecessary list join
    GitOrigin-RevId: 336a8c6a09108b8660267976586a76241ee2e571

--- platform/workspaceModel/jps/src/com/intellij/workspaceModel/ide/impl/jps/serialization/ModuleImlFileEntitiesSerializer.kt
+++ platform/workspaceModel/jps/src/com/intellij/workspaceModel/ide/impl/jps/serialization/ModuleImlFileEntitiesSerializer.kt
@@ -823,7 +823,7 @@ internal open class ModuleListSerializerImpl(override val fileUrl: String,
         entitySourceFilter(it.entitySource)
         || it.contentRoots.any { cr -> entitySourceFilter(cr.entitySource) }
         || it.sourceRoots.any { sr -> entitySourceFilter(sr.entitySource) }
-        || it.contentRoots.flatMap { cr -> cr.excludedUrls }.any { ex -> entitySourceFilter(ex.entitySource) }
+        || it.contentRoots.any { cr -> cr.excludedUrls.any { ex -> entitySourceFilter(ex.entitySource) } }
       }
       .mapNotNullTo(ArrayList()) { module -> getSourceToSave(module)?.let { Pair(it, module) } }
       .sortedBy { it.second.name }
