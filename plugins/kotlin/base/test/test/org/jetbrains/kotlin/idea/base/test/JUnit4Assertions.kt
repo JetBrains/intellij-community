d2cf139f3bf1d5399370e358d205d87638f3050a
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-11-03 12:07:18 +0200

    [Kotlin] Fix compilation of JUnit4Assertions
    GitOrigin-RevId: cc498d5350580a94f3fd8d0bcab6afe82c44ab06

--- plugins/kotlin/base/test/test/org/jetbrains/kotlin/idea/base/test/JUnit4Assertions.kt
+++ plugins/kotlin/base/test/test/org/jetbrains/kotlin/idea/base/test/JUnit4Assertions.kt
@@ -37,12 +37,12 @@ object JUnit4Assertions : Assertions() {
         UsefulTestCase.assertSameElements(message?.invoke() ?: "Collections are different", actual, expected)
     }
 
-    override fun assertAll(exceptions: List<Throwable>) {
+    override fun failAll(exceptions: List<Throwable>) {
         exceptions.forEach { throw it }
     }
 
-    override fun assertAll(vararg blocks: () -> Unit) {
-        blocks.forEach { it.invoke() }
+    override fun assertAll(conditions: List<() -> Unit>) {
+        conditions.forEach { it.invoke() }
     }
 
     override fun fail(message: () -> String): Nothing {
