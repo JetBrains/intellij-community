458ea93a17fa214cc66c74487a3062183f4c6334
Alexandr Evstigneev <alexandr.evstigneev@jetbrains.com>
2022-11-03 09:19:16 +0400

    IDEA-304640 Fixed project file index access from test sources filter
    GitOrigin-RevId: de38721c46b4929d469d4ba556c57e0d50df5f56

--- platform/projectModel-impl/src/com/intellij/openapi/roots/ProjectRootTestSourcesFilter.java
+++ platform/projectModel-impl/src/com/intellij/openapi/roots/ProjectRootTestSourcesFilter.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.roots;
 
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
@@ -8,6 +9,6 @@ import org.jetbrains.annotations.NotNull;
 public class ProjectRootTestSourcesFilter extends TestSourcesFilter {
   @Override
   public boolean isTestSource(@NotNull VirtualFile file, @NotNull Project project) {
-    return ProjectFileIndex.getInstance(project).isInTestSourceContent(file);
+    return ReadAction.compute(() -> ProjectFileIndex.getInstance(project).isInTestSourceContent(file));
   }
 }
