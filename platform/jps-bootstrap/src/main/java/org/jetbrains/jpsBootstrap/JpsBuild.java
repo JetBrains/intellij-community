7083d86e58fbaa97ada605c1aa20a83dca1d9c48
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2022-11-03 16:40:41 +0100

    Revert "jps-bootstrap: private Space Maven dependencies support"
    This reverts commit 735fac1b00e41d2ccfae05fb7a6241233b6edbff.
    
    (cherry picked from commit 0250963f790798e07036381d96f91b1510de8195)
    
    IJ-CR-97769
    
    GitOrigin-RevId: 17cf9b0490e13a0cbec189cb67404000d3dd4ea7

--- platform/jps-bootstrap/src/main/java/org/jetbrains/jpsBootstrap/JpsBuild.java
+++ platform/jps-bootstrap/src/main/java/org/jetbrains/jpsBootstrap/JpsBuild.java
@@ -74,17 +74,8 @@ public class JpsBuild {
     runBuild(modules.stream().map(JpsNamedElement::getName).collect(Collectors.toSet()));
   }
 
-  /**
-   * @see com.intellij.space.java.jps.SpaceDependencyAuthenticationDataProvider
-   */
   public void resolveProjectDependencies() throws Exception {
     info("Resolving project dependencies...");
-    var spaceUsername = System.getProperty("jps.auth.spaceUsername");
-    var spacePassword = System.getProperty("jps.auth.spacePassword");
-    if (spaceUsername == null || spacePassword == null) {
-      warn("Space credentials are not provided via -Djps.auth.spaceUsername and -Djps.auth.spacePassword. " +
-        "Private Space Maven dependencies, if not available locally, will fail to be resolved.");
-    }
 
     final long buildStart = System.currentTimeMillis();
 
