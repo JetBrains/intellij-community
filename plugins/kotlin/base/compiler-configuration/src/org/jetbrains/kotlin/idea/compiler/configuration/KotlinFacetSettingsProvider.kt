01227712c98b080ea75c62b93580ec790086cda9
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2022-11-03 16:46:53 +0100

    [kotlin] Add KotlinFacetSettings cache
    Its calculation could be heavy, therefore cache it on soft reference with CacheManager results to often recalculation
    
    #KTIJ-23501
    
    GitOrigin-RevId: a7f9c75376ba388c61e9aa8fe45fa46f4bd82db0

--- plugins/kotlin/base/compiler-configuration/src/org/jetbrains/kotlin/idea/compiler/configuration/KotlinFacetSettingsProvider.kt
+++ plugins/kotlin/base/compiler-configuration/src/org/jetbrains/kotlin/idea/compiler/configuration/KotlinFacetSettingsProvider.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.config
 
+import com.intellij.openapi.components.service
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 
@@ -9,12 +10,11 @@ interface KotlinFacetSettingsProvider {
     fun getInitializedSettings(module: Module): KotlinFacetSettings
 
     companion object {
-        fun getInstance(project: Project): KotlinFacetSettingsProvider? {
+        fun getInstance(project: Project): KotlinFacetSettingsProvider? =
             if (project.isDisposed) {
-                return null
+                null
+            } else {
+                project.service()
             }
-
-            return project.getService(KotlinFacetSettingsProvider::class.java)
-        }
     }
 }
--- plugins/kotlin/base/facet/src/org/jetbrains/kotlin/idea/facet/KotlinFacetSettingsProviderImpl.kt
+++ plugins/kotlin/base/facet/src/org/jetbrains/kotlin/idea/facet/KotlinFacetSettingsProviderImpl.kt
@@ -4,23 +4,83 @@ package org.jetbrains.kotlin.idea.facet
 
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
-import com.intellij.psi.util.CachedValueProvider
-import com.intellij.psi.util.CachedValuesManager
+import com.intellij.serviceContainer.AlreadyDisposedException
+import com.intellij.workspaceModel.ide.WorkspaceModelChangeListener
+import com.intellij.workspaceModel.ide.WorkspaceModelTopics
+import com.intellij.workspaceModel.ide.impl.legacyBridge.module.findModule
+import com.intellij.workspaceModel.storage.VersionedStorageChange
+import com.intellij.workspaceModel.storage.bridgeEntities.FacetEntity
+import com.intellij.workspaceModel.storage.bridgeEntities.ModuleEntity
 import org.jetbrains.kotlin.config.KotlinFacetSettings
 import org.jetbrains.kotlin.config.KotlinFacetSettingsProvider
+import org.jetbrains.kotlin.idea.base.util.caching.*
 import org.jetbrains.kotlin.idea.compiler.configuration.KotlinCompilerSettingsTracker
+import org.jetbrains.kotlin.idea.facet.KotlinFacetModificationTracker.Companion.isKotlinFacet
 
-class KotlinFacetSettingsProviderImpl(private val project: Project) : KotlinFacetSettingsProvider {
+class KotlinFacetSettingsProviderImpl(project: Project) :
+    SynchronizedFineGrainedEntityCache<Module, KotlinFacetSettings>(project),
+    WorkspaceModelChangeListener,
+    KotlinFacetSettingsProvider {
+
+    private var actualKotlinCompilerSettingsModCount: Long = 0L
+
     override fun getSettings(module: Module) = KotlinFacet.get(module)?.configuration?.settings
 
-    override fun getInitializedSettings(module: Module): KotlinFacetSettings =
-        CachedValuesManager.getManager(project).getCachedValue(module) {
-            val kotlinFacetSettings = getSettings(module) ?: KotlinFacetSettings()
-            kotlinFacetSettings.initializeIfNeeded(module, null)
-            CachedValueProvider.Result.create(
-                kotlinFacetSettings,
-                KotlinCompilerSettingsTracker.getInstance(project),
-                KotlinFacetModificationTracker.getInstance(project),
-            )
+    override fun getInitializedSettings(module: Module): KotlinFacetSettings = get(module)
+
+    override fun get(key: Module): KotlinFacetSettings {
+        useCache { cache ->
+            val modificationCount = KotlinCompilerSettingsTracker.getInstance(project).modificationCount
+            if (actualKotlinCompilerSettingsModCount != modificationCount) {
+                cache.clear()
+                actualKotlinCompilerSettingsModCount = modificationCount
+            }
         }
+        return super.get(key)
+    }
+
+    override fun calculate(key: Module): KotlinFacetSettings {
+        val kotlinFacetSettings = getSettings(key) ?: KotlinFacetSettings()
+        kotlinFacetSettings.initializeIfNeeded(key, null)
+        return kotlinFacetSettings
+    }
+
+    override fun subscribe() {
+        project.messageBus.connect(this).subscribe(WorkspaceModelTopics.CHANGED, this)
+    }
+
+    override fun checkKeyValidity(key: Module) {
+        if (key.isDisposed) {
+            throw AlreadyDisposedException("Module '${key.name}' is already disposed")
+        }
+    }
+
+    override fun beforeChanged(event: VersionedStorageChange) {
+        val moduleChanges = event.getChanges(ModuleEntity::class.java)
+        val facetChanges = event.getChanges(FacetEntity::class.java)
+        if (moduleChanges.isEmpty() && facetChanges.isEmpty()) return
+
+        val storageBefore = event.storageBefore
+        val storageAfter = event.storageAfter
+        val outdated = mutableSetOf<Module>()
+
+        for (moduleChange in moduleChanges) {
+            val moduleEntity = moduleChange.oldEntity() ?: continue
+            // it does not matter is it explicit kotlin facet module or not
+            // there is a default kotlin facet for non-explicit kotlin facet modules
+            moduleEntity.findModule(storageBefore)?.let { outdated.add(it) }
+        }
+
+        for (facetChange in facetChanges) {
+            facetChange.oldEntity()?.takeIf { it.isKotlinFacet() }?.let { oldKotlinFacetEntity ->
+                oldKotlinFacetEntity.module.findModule(storageBefore)?.let { outdated.add(it) }
+            }
+            facetChange.newEntity()?.takeIf { it.isKotlinFacet() }?.let { newKotlinFacetEntity ->
+                newKotlinFacetEntity.module.findModule(storageAfter)?.let { outdated.add(it) }
+            }
+        }
+
+        invalidateEntries(condition = { key, _ -> key in outdated })
+    }
+
 }
