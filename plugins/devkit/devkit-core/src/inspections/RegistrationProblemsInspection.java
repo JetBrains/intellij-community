df30e8164172c08550ae0fcf2c0965af8cba1406
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2022-11-04 08:59:25 +0100

    IDEA-189985 - RegistrationProblemsInspection - Rewrite to UAST
    (cherry picked from commit 448a1751715aab23f6aa24b0d4e26fb9944eea71)
    
    IJ-CR-97828
    
    GitOrigin-RevId: 0655d577f83cb8bdc1fb01e28fcb1c51c1b3a718

--- plugins/devkit/devkit-core/src/inspections/RegistrationProblemsInspection.java
+++ plugins/devkit/devkit-core/src/inspections/RegistrationProblemsInspection.java
@@ -1,31 +1,38 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.inspections;
 
+import com.intellij.CommonBundle;
 import com.intellij.codeInsight.daemon.impl.quickfix.ImplementOrExtendFix;
+import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo;
 import com.intellij.codeInspection.InspectionManager;
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
-import com.intellij.codeInspection.ProblemHighlightType;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiIdentifier;
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiModifier;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.lang.java.JavaLanguage;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.ReadonlyStatusHandler;
+import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.PsiUtil;
-import com.intellij.util.SmartList;
+import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.devkit.DevKitBundle;
-import org.jetbrains.idea.devkit.inspections.quickfix.CreateConstructorFix;
 import org.jetbrains.idea.devkit.util.ActionType;
+import org.jetbrains.uast.UClass;
+import org.jetbrains.uast.UElement;
 
-import java.util.List;
+import java.util.Collections;
 import java.util.Set;
 
-public class RegistrationProblemsInspection extends DevKitInspectionBase {
+public class RegistrationProblemsInspection extends DevKitUastInspectionBase {
 
   @Override
-  public ProblemDescriptor @Nullable [] checkClass(@NotNull PsiClass checkedClass, @NotNull InspectionManager manager, boolean isOnTheFly) {
+  public ProblemDescriptor @Nullable [] checkClass(@NotNull UClass uClass, @NotNull InspectionManager manager, boolean isOnTheFly) {
+    PsiClass checkedClass = uClass.getJavaPsi();
     final PsiIdentifier nameIdentifier = checkedClass.getNameIdentifier();
     if (nameIdentifier != null &&
         checkedClass.getQualifiedName() != null &&
@@ -38,31 +45,38 @@ public class RegistrationProblemsInspection extends DevKitInspectionBase {
       final RegistrationCheckerUtil.RegistrationType registrationType = RegistrationCheckerUtil.RegistrationType.ALL;
       final Set<PsiClass> componentClasses = RegistrationCheckerUtil.getRegistrationTypes(checkedClass, registrationType);
       if (componentClasses != null && !componentClasses.isEmpty()) {
-        List<ProblemDescriptor> problems = new SmartList<>();
+        PsiElement sourcePsi = uClass.getSourcePsi();
+        if (sourcePsi == null) return null;
+        UElement classAnchor = uClass.getUastAnchor();
+        if (classAnchor == null) return null;
+        PsiElement classPsiAnchor = classAnchor.getSourcePsi();
+        if (classPsiAnchor == null) return null;
+
+        ProblemsHolder holder = createProblemsHolder(uClass, manager, isOnTheFly);
 
         for (PsiClass componentClass : componentClasses) {
           if (ActionType.ACTION.myClassName.equals(componentClass.getQualifiedName()) &&
               !checkedClass.isInheritor(componentClass, true)) {
-            problems.add(manager.createProblemDescriptor(nameIdentifier,
-                                                         DevKitBundle.message("inspections.registration.problems.incompatible.message",
-                                                                              componentClass.getQualifiedName()), isOnTheFly,
-                                                         ImplementOrExtendFix.createFixes(nameIdentifier, componentClass, checkedClass,
-                                                                                          isOnTheFly),
-                                                         ProblemHighlightType.GENERIC_ERROR_OR_WARNING));
+            LocalQuickFix[] fixes = sourcePsi.getLanguage().is(JavaLanguage.INSTANCE) ?
+                                    ImplementOrExtendFix.createFixes(nameIdentifier, checkedClass, componentClass, isOnTheFly) :
+                                    LocalQuickFix.EMPTY_ARRAY;
+            holder.registerProblem(classPsiAnchor,
+                                   DevKitBundle.message("inspections.registration.problems.incompatible.message",
+                                                        componentClass.getQualifiedName()),
+                                   fixes);
           }
         }
         if (ActionType.ACTION.isOfType(checkedClass) && !hasNoArgConstructor(checkedClass)) {
-          problems.add(manager.createProblemDescriptor(nameIdentifier,
-                                                       DevKitBundle.message("inspections.registration.problems.missing.noarg.ctor"),
-                                                       new CreateConstructorFix(checkedClass, isOnTheFly),
-                                                       ProblemHighlightType.GENERIC_ERROR_OR_WARNING, isOnTheFly));
+          // creating a constructor with UAST is not possible until IDEA-303510 is solved
+          LocalQuickFix[] fixes = sourcePsi.getLanguage().is(JavaLanguage.INSTANCE) ?
+                                  new LocalQuickFix[]{new CreateConstructorFix(checkedClass, isOnTheFly)} :
+                                  LocalQuickFix.EMPTY_ARRAY;
+          holder.registerProblem(classPsiAnchor, DevKitBundle.message("inspections.registration.problems.missing.noarg.ctor"), fixes);
         }
         if (checkedClass.hasModifierProperty(PsiModifier.ABSTRACT)) {
-          problems.add(manager.createProblemDescriptor(nameIdentifier,
-                                                       DevKitBundle.message("inspections.registration.problems.abstract"), isOnTheFly,
-                                                       LocalQuickFix.EMPTY_ARRAY, ProblemHighlightType.GENERIC_ERROR_OR_WARNING));
+          holder.registerProblem(classPsiAnchor, DevKitBundle.message("inspections.registration.problems.abstract"));
         }
-        return problems.toArray(ProblemDescriptor.EMPTY_ARRAY);
+        return holder.getResultsArray();
       }
     }
     return null;
@@ -84,5 +98,73 @@ public class RegistrationProblemsInspection extends DevKitInspectionBase {
   @NonNls
   public String getShortName() {
     return "ComponentRegistrationProblems";
+  }
+
+  private static class CreateConstructorFix implements LocalQuickFix {
+    protected final SmartPsiElementPointer<? extends PsiElement> myPointer;
+    protected final boolean myOnTheFly;
+
+    CreateConstructorFix(@NotNull PsiClass aClass, boolean isOnTheFly) {
+      myPointer = SmartPointerManager.createPointer((PsiElement)aClass);
+      myOnTheFly = isOnTheFly;
+    }
+
+    @Override
+    public @NotNull IntentionPreviewInfo generatePreview(@NotNull Project project, @NotNull ProblemDescriptor previewDescriptor) {
+      return IntentionPreviewInfo.EMPTY;
+    }
+
+    @Override
+    public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+      // can happen during batch-inspection if resolution has already been applied to plugin.xml or java class
+      PsiElement element = myPointer.getElement();
+      if (element == null || !element.isValid()) return;
+
+      boolean external = descriptor.getPsiElement().getContainingFile() != element.getContainingFile();
+      if (external) {
+        PsiClass clazz = PsiTreeUtil.getParentOfType(element, PsiClass.class, false);
+        ReadonlyStatusHandler readonlyStatusHandler = ReadonlyStatusHandler.getInstance(project);
+        ReadonlyStatusHandler.OperationStatus status = readonlyStatusHandler.ensureFilesWritable(
+          Collections.singletonList(element.getContainingFile().getVirtualFile()));
+
+        if (status.hasReadonlyFiles()) {
+          String className = clazz != null ? clazz.getQualifiedName() : element.getContainingFile().getName();
+          Messages.showErrorDialog(project,
+                                   DevKitBundle.message("inspections.registration.problems.quickfix.read-only", className),
+                                   CommonBundle.getErrorTitle());
+          return;
+        }
+      }
+
+      try {
+        if (!(element instanceof PsiClass)) return;
+        PsiClass clazz = (PsiClass)element;
+
+        PsiMethod ctor = JavaPsiFacade.getInstance(clazz.getProject()).getElementFactory().createConstructor();
+        PsiUtil.setModifierProperty(ctor, PsiModifier.PUBLIC, true);
+
+        PsiMethod[] constructors = clazz.getConstructors();
+        if (constructors.length > 0) {
+          ctor = (PsiMethod)clazz.addBefore(ctor, constructors[0]);
+        }
+        else {
+          // shouldn't get here - it's legal if there's no ctor present at all
+          ctor = (PsiMethod)clazz.add(ctor);
+        }
+
+        if (myOnTheFly) {
+          ctor.navigate(true);
+        }
+      }
+      catch (IncorrectOperationException e) {
+        Logger.getInstance(getClass()).error(e);
+      }
+    }
+
+    @Override
+    @NotNull
+    public String getFamilyName() {
+      return DevKitBundle.message("inspections.registration.problems.quickfix.create.constructor");
+    }
   }
 }
