d9fc1fcf103e64c7a066b7a617938dd0f6102d19
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2022-11-04 13:13:00 +0000

    [Kotlin/JS] Notification about stability of IR compiler
    Review remarks
    - KGP version can be null, so need to consider this
    - unsetValue instead of setValue
    
    Move notification to listener after import
    
    Fix place xml of notificationGroup
    
    Separate bundles for maven and Gradle
    - fix review remarks
    - add do not show again action
    
    Add test on notification of kotlin/js compiler
    
    Fix kotlin version check condition
    
    Fix notification group
    - fix display type
    - fix browser action instead of link
    
    Notification about deprecation of maven
    
    ^KTIJ-22165 fixed
    ^KTIJ-22166 fixed
    
    (cherry picked from commit 4c15fef643e92cba7c945ad2ad29d92a466acf84)
    (cherry picked from commit d58956a0eda2cb433990e243db4db883b8237298)
    
    GitOrigin-RevId: 24fd4a2fd909e6312b38aa023c3358bab01d96c2

--- plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/KotlinMPPGradleProjectResolver.kt
+++ plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/KotlinMPPGradleProjectResolver.kt
@@ -3,6 +3,8 @@
 package org.jetbrains.kotlin.idea.gradleJava.configuration
 
 import com.intellij.build.events.MessageEvent
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.notification.*
 import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.model.ProjectKeys
 import com.intellij.openapi.externalSystem.model.project.*
@@ -11,6 +13,8 @@ import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil.normalizeP
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil.toCanonicalPath
 import com.intellij.openapi.externalSystem.util.ExternalSystemConstants
 import com.intellij.openapi.externalSystem.util.Order
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.Pair
 import com.intellij.openapi.util.io.FileUtil
@@ -34,14 +38,13 @@ import org.jetbrains.kotlin.idea.gradle.configuration.GradlePropertiesFileFacade
 import org.jetbrains.kotlin.idea.gradle.configuration.utils.UnsafeTestSourceSetHeuristicApi
 import org.jetbrains.kotlin.idea.gradle.configuration.utils.predictedProductionSourceSetName
 import org.jetbrains.kotlin.idea.gradle.ui.notifyLegacyIsResolveModulePerSourceSetSettingIfNeeded
-import org.jetbrains.kotlin.idea.gradleJava.configuration.mpp.createKotlinMppPopulateModuleDependenciesContext
-import org.jetbrains.kotlin.idea.gradleJava.configuration.mpp.getCompilations
-import org.jetbrains.kotlin.idea.gradleJava.configuration.mpp.populateModuleDependenciesByCompilations
-import org.jetbrains.kotlin.idea.gradleJava.configuration.mpp.populateModuleDependenciesBySourceSetVisibilityGraph
+import org.jetbrains.kotlin.idea.gradleJava.configuration.mpp.*
 import org.jetbrains.kotlin.idea.gradleJava.configuration.utils.KotlinModuleUtils.calculateRunTasks
 import org.jetbrains.kotlin.idea.gradleJava.configuration.utils.KotlinModuleUtils.getKotlinModuleId
 import org.jetbrains.kotlin.idea.gradleJava.configuration.utils.KotlinModuleUtils.fullName
 import org.jetbrains.kotlin.idea.gradleJava.configuration.utils.KotlinModuleUtils.getGradleModuleQualifiedName
+import org.jetbrains.kotlin.idea.gradleJava.notification.KOTLIN_JS_COMPILER_SHOULD_BE_NOTIFIED
+import org.jetbrains.kotlin.idea.gradleJava.notification.IGNORE_KOTLIN_JS_COMPILER_NOTIFICATION
 import org.jetbrains.kotlin.idea.gradleTooling.*
 import org.jetbrains.kotlin.idea.gradleTooling.KotlinMPPGradleModelBuilder
 import org.jetbrains.kotlin.idea.gradleTooling.arguments.CachedExtractedArgsInfo
@@ -119,6 +122,16 @@ open class KotlinMPPGradleProjectResolver : AbstractProjectResolverExtension() {
             if (!kotlinJsInspectionPackAdvertised && mppModel.targets.any { it.platform == KotlinPlatform.JS }) {
                 kotlinJsInspectionPackAdvertised = true
                 suggestKotlinJsInspectionPackPlugin(resolverCtx.projectPath)
+            }
+            if (mppModel.targets.any { it.platform == KotlinPlatform.JS }) {
+                val projectManager = ProjectManager.getInstance()
+                val project: Project = projectManager.openProjects.firstOrNull { it.basePath == resolverCtx.projectPath } ?: return
+                mppModel.kotlinGradlePluginVersion?.let { version ->
+                    showDeprecatedKotlinJsCompilerWarning(
+                        project,
+                        version,
+                    )
+                }
             }
             if (!resolverCtx.isResolveModulePerSourceSet && !PlatformVersion.isAndroidStudio() && !PlatformUtils.isMobileIde() &&
                 !PlatformUtils.isAppCode()
@@ -585,6 +598,19 @@ open class KotlinMPPGradleProjectResolver : AbstractProjectResolverExtension() {
                 .map { it.absolutePath }
 
             ideModule.kotlinGradleProjectDataOrFail.pureKotlinSourceFolders.addAll(mppModelPureKotlinSourceFolders)
+        }
+
+        fun showDeprecatedKotlinJsCompilerWarning(
+            project: Project,
+            kotlinGradlePluginVersion: KotlinGradlePluginVersion,
+        ) {
+            kotlinGradlePluginVersion.invokeWhenAtLeast("1.7.0") {
+                if (
+                    !PropertiesComponent.getInstance(project).getBoolean(IGNORE_KOTLIN_JS_COMPILER_NOTIFICATION, false)
+                ) {
+                    PropertiesComponent.getInstance(project).setValue(KOTLIN_JS_COMPILER_SHOULD_BE_NOTIFIED, true)
+                }
+            }
         }
 
         internal data class CompilationWithDependencies(
