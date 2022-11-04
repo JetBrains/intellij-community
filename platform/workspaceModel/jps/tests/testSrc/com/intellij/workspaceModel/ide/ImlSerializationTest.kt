73ccaaa80cd4bb45788198946e2cff0ccb3420e2
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2022-11-04 18:17:37 +0400

    [workspace model] IDEA-304968 Increase version of caches
    GitOrigin-RevId: 442bcba1189da726e6dd6ced57fb9c50cfe7aaca

--- platform/workspaceModel/jps/tests/testSrc/com/intellij/workspaceModel/ide/ImlSerializationTest.kt
+++ platform/workspaceModel/jps/tests/testSrc/com/intellij/workspaceModel/ide/ImlSerializationTest.kt
@@ -46,7 +46,7 @@ class ImlSerializationTest {
     checkSerializationSize(bytes, expectedSize, 2_000)
 
     assertTrue("This assertion is a reminder. Have you updated the serializer? Update the serializer version!",
-               "v42" == EntityStorageSerializerImpl.SERIALIZER_VERSION)
+               "v43" == EntityStorageSerializerImpl.SERIALIZER_VERSION)
   }
 
   @Test
