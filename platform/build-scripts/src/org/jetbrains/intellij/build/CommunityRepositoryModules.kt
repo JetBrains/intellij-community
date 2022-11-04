800e38162372825e48cf2475d2c736b54fe6e474
Kirill.Krylov <kirill.krylov@jetbrains.com>
2022-11-05 00:11:00 +0100

    [full-line] add intellij.fullLine.local module to plugin build
    GitOrigin-RevId: b873b6eed0a8cf28075be0f78ea28b62c2071bc5

--- platform/build-scripts/src/org/jetbrains/intellij/build/CommunityRepositoryModules.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/CommunityRepositoryModules.kt
@@ -219,6 +219,7 @@ object CommunityRepositoryModules {
       spec.bundlingRestrictions.includeInEapOnly = true
       spec.withModules(listOf(
         "intellij.fullLine.core",
+        "intellij.fullLine.local",
         "intellij.fullLine.python",
         "intellij.fullLine.java",
         "intellij.fullLine.kotlin"
