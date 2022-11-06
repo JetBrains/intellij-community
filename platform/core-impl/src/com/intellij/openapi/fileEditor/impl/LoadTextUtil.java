13a81937cd3611a8503a8e2235aa91b9ca1ba473
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2022-11-06 09:20:15 +0100

    [platform] logging failed `LoadTextUtil#loadText` attempts
    GitOrigin-RevId: 7bcb8550f9d0e288f6b5e6974c757c718f7dcf72

--- platform/core-impl/src/com/intellij/openapi/fileEditor/impl/LoadTextUtil.java
+++ platform/core-impl/src/com/intellij/openapi/fileEditor/impl/LoadTextUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.fileEditor.impl;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -577,6 +577,7 @@ public final class LoadTextUtil {
       return getTextByBinaryPresentation(bytes, file);
     }
     catch (IOException e) {
+      LOG.debug(e);
       return Strings.EMPTY_CHAR_SEQUENCE;
     }
   }
