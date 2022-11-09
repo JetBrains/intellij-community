d89b587ce68be0414a2fa4e3d49c6df56245408d
Alexandr Suhinin <alexandr.suhinin@jetbrains.com>
2022-11-09 11:07:11 +0200

    IDEA-302550 [java highlighting]: fix test data
    GitOrigin-RevId: 0e5b0c830e2b041dc3887c6f71b3723486803936

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createMissingSealedClassSwitchBranches/beforeGuardedAndDominatingPatternsPresent.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createMissingSealedClassSwitchBranches/beforeGuardedAndDominatingPatternsPresent.java
@@ -1,18 +1,19 @@
 // "Create missing switch branch 'Sub1'" "true-preview"
 sealed interface I {}
 sealed interface J extends I {}
-final class Sub1 implements I, J {}
+final class Sub1 implements I {}
 final class Sub2 implements I {}
+final class Sub3 implements I, J {}
 
 class Test {
-  void test(I i) {
-    switch (i<caret>) {
-      case Sub1 sub1 && Math.random() > 0.5:
-        break;
-      case Sub2 sub2:
-        break;
-      case J j:
-        break;
+    void test(I i) {
+        switch (i<caret>) {
+            case Sub1 sub1 && Math.random() > 0.5:
+                break;
+            case Sub2 sub2:
+                break;
+            case J j:
+                break;
+        }
     }
-  }
 }
