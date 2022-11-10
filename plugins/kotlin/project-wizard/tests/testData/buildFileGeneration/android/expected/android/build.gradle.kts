a4b417d222bb4f6a5d250f61daf1eafb0455ef5d
dima.avdeev <dima.avdeev@jetbrains.com>
2022-11-10 11:19:55 +0400

    [project-wizard] KTIJ-23537 Fix kotlin wizard, Android module for IDEA 213, 221, 222
    GitOrigin-RevId: 86bef3c995ac00dbb14df90437358aeca0454aff

--- plugins/kotlin/project-wizard/tests/testData/buildFileGeneration/android/expected/android/build.gradle.kts
+++ plugins/kotlin/project-wizard/tests/testData/buildFileGeneration/android/expected/android/build.gradle.kts
@@ -13,11 +13,11 @@ dependencies {
 }
 
 android {
-    compileSdkVersion(31)
+    compileSdkVersion(32)
     defaultConfig {
         applicationId = "testGroupId.android"
         minSdkVersion(24)
-        targetSdkVersion(31)
+        targetSdkVersion(32)
         versionCode = 1
         versionName = "1.0"
     }
