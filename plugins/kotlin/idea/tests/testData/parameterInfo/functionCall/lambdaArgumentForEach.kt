9904d48f6e109ee17f09571c1d819ec3e6b3d9fa
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2022-11-10 13:56:07 +0100

    [kotlin] Add tests for ^KTIJ-23373 and ^KTIJ-21506
    GitOrigin-RevId: 52b4f4d6b10a3ae0eaf6e76a242e6e936ebad8c1

---
+++ plugins/kotlin/idea/tests/testData/parameterInfo/functionCall/lambdaArgumentForEach.kt
@@ -0,0 +1,13 @@
+// See: KTIJ-23373
+
+// WITH_STDLIB
+fun test() {
+    listOf(1, 2, 3).forEach { <caret> }
+}
+
+/*
+Text: (<highlight>Consumer<in Int!>!</highlight>), Disabled: false, Strikeout: false, Green: false
+Text: (<highlight>action: (Int) -> Unit</highlight>), Disabled: false, Strikeout: false, Green: true
+Text_K2: (<highlight>action: (Int) -> Unit</highlight>), Disabled: false, Strikeout: false, Green: true
+Text_K2: (<highlight>p0: Consumer<in Int!>!</highlight>), Disabled: false, Strikeout: false, Green: false
+*/
