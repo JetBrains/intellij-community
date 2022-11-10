cf84079ac5382dc60504849b6fa6bfcf0412cd70
Ivan Posti <ivan.posti@jetbrains.com>
2022-11-10 16:47:59 +0100

    [Markdown][IDEA-305817] Do not perform an injection if there is no actual language definition present in the IDE
    GitOrigin-RevId: e193157afce290abd1cb4321147b90783bfdf0c1

--- plugins/markdown/core/src/org/intellij/plugins/markdown/lang/parser/blocks/frontmatter/FrontMatterContentLanguage.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/lang/parser/blocks/frontmatter/FrontMatterContentLanguage.kt
@@ -1,12 +1,18 @@
 package org.intellij.plugins.markdown.lang.parser.blocks.frontmatter
 
+import com.intellij.lang.Language
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Experimental
-data class FrontMatterContentLanguage(private val name: String)
+data class FrontMatterContentLanguage(val name: String)
 
 @ApiStatus.Experimental
 object FrontMatterLanguages {
   val YAML = FrontMatterContentLanguage("YAML")
   val TOML = FrontMatterContentLanguage("TOML")
+}
+
+@ApiStatus.Experimental
+fun FrontMatterContentLanguage.findLanguage(): Language? {
+  return Language.findLanguageByID(name.lowercase())
 }
