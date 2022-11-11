179decc583d97b2cad794673a7c7d108c7deff16
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2022-11-11 17:01:13 +0700

    Revert "IDEA-305287 screen menu calls LWCToolkit.invokeAndWait in processEvent-mode"
    Because of observed crash when typing in Help -> Search
    
    GitOrigin-RevId: 268064093afebb00f99b8df6bf1c6ccec67f5f62

--- platform/platform-api/src/com/intellij/ui/mac/screenmenu/Menu.java
+++ platform/platform-api/src/com/intellij/ui/mac/screenmenu/Menu.java
@@ -330,10 +330,9 @@ public class Menu extends MenuItem {
   private static void invokeWithLWCToolkit(Runnable r, Runnable after, Component invoker) {
     try {
       Class toolkitClass = Class.forName("sun.lwawt.macosx.LWCToolkit");
-      Method invokeMethod = ReflectionUtil.getDeclaredMethod(toolkitClass, "invokeAndWait", Runnable.class, Component.class, boolean.class, int.class);
+      Method invokeMethod = ReflectionUtil.getDeclaredMethod(toolkitClass, "invokeAndWait", Runnable.class, Component.class);
       if (invokeMethod != null) {
         try {
-          invokeMethod.invoke(toolkitClass, r, invoker, true, -1);
           invokeMethod.invoke(toolkitClass, r, invoker);
         } catch (Exception e) {
           // suppress InvocationTargetException as in openjdk implementation (see com.apple.laf.ScreenMenu.java)
