1e2f5270ef3d202e2171d9180f5147e1edb1a18a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-11-11 11:44:27 +0100

    cleanup
    GitOrigin-RevId: 597a35e5b1201d3cb0c92e9b783ad5b18638a826

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/status/widget/StatusBarWidgetsActionGroup.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/status/widget/StatusBarWidgetsActionGroup.kt
@@ -1,141 +1,101 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.wm.impl.status.widget;
+package com.intellij.openapi.wm.impl.status.widget
 
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.project.DumbAwareAction;
-import com.intellij.openapi.project.DumbAwareToggleAction;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.wm.StatusBar;
-import com.intellij.openapi.wm.StatusBarWidgetFactory;
-import com.intellij.openapi.wm.impl.status.IdeStatusBarImpl;
-import com.intellij.ui.ExperimentalUI;
-import com.intellij.ui.UIBundle;
-import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.project.DumbAwareToggleAction
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.wm.StatusBarWidgetFactory
+import com.intellij.openapi.wm.impl.status.IdeStatusBarImpl
+import com.intellij.ui.ExperimentalUI
+import com.intellij.ui.UIBundle
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
+internal class StatusBarWidgetsActionGroup : DefaultActionGroup() {
+  companion object {
+    const val GROUP_ID = "ViewStatusBarWidgetsGroup"
+  }
 
-import static com.intellij.openapi.actionSystem.ActionPlaces.STATUS_BAR_PLACE;
+  override fun getChildren(e: AnActionEvent?): Array<AnAction> {
+    val project = e?.project ?: return EMPTY_ARRAY
 
-public final class StatusBarWidgetsActionGroup extends DefaultActionGroup {
-  public static final String GROUP_ID = "ViewStatusBarWidgetsGroup";
-
-  @Override
-  public AnAction @NotNull [] getChildren(@Nullable AnActionEvent e) {
-    Project project = e != null ? e.getProject() : null;
-    if (project == null) return AnAction.EMPTY_ARRAY;
-
-    ArrayList<AnAction> actions = new ArrayList<>();
-
-    AnAction[] superActions = super.getChildren(e);
-    if (superActions.length > 0) {
-      actions.addAll(Arrays.asList(superActions));
-    }
-
-    if (e.getPlace().equals(STATUS_BAR_PLACE) && ExperimentalUI.isNewUI()) {
-      AnAction navBarLocationGroup = e.getActionManager().getAction("NavbarLocationGroup");
-      if (navBarLocationGroup instanceof ActionGroup) {
-        actions.add(navBarLocationGroup);
+    val actions = mutableListOf<AnAction>()
+    actions.addAll(super.getChildren(e))
+    if (e.place == ActionPlaces.STATUS_BAR_PLACE && ExperimentalUI.isNewUI()) {
+      val navBarLocationGroup = e.actionManager.getAction("NavbarLocationGroup")
+      if (navBarLocationGroup is ActionGroup) {
+        actions.add(navBarLocationGroup)
       }
     }
-
     if (!actions.isEmpty()) {
-      actions.add(Separator.getInstance());
+      actions.add(Separator.getInstance())
     }
 
-    StatusBarWidgetsManager manager = project.getService(StatusBarWidgetsManager.class);
-    Collection<AnAction> widgetToggles = new ArrayList<>(ContainerUtil.map(manager.getWidgetFactories(), ToggleWidgetAction::new));
-    widgetToggles.add(Separator.getInstance());
-    widgetToggles.add(new HideCurrentWidgetAction());
-    actions.addAll(widgetToggles);
-
-    return actions.toArray(AnAction.EMPTY_ARRAY);
+    val manager = project.service<StatusBarWidgetsManager>()
+    manager.getWidgetFactories().mapTo(actions) { ToggleWidgetAction(it) }
+    actions.add(Separator.getInstance())
+    actions.add(HideCurrentWidgetAction())
+    return actions.toTypedArray()
   }
 
-  private static final class ToggleWidgetAction extends DumbAwareToggleAction {
-    private final StatusBarWidgetFactory myWidgetFactory;
-
-    private ToggleWidgetAction(@NotNull StatusBarWidgetFactory widgetFactory) {
-      super(widgetFactory.getDisplayName());
-      myWidgetFactory = widgetFactory;
-    }
-
-    @Override
-    public void update(@NotNull AnActionEvent e) {
-      super.update(e);
-      Project project = e.getProject();
+  private class ToggleWidgetAction(private val widgetFactory: StatusBarWidgetFactory) : DumbAwareToggleAction(widgetFactory.displayName) {
+    override fun update(e: AnActionEvent) {
+      super.update(e)
+      val project = e.project
       if (project == null) {
-        e.getPresentation().setEnabledAndVisible(false);
-        return;
+        e.presentation.isEnabledAndVisible = false
+        return
       }
-      if (ActionPlaces.isMainMenuOrActionSearch(e.getPlace())) {
-        e.getPresentation().setEnabledAndVisible(myWidgetFactory.isConfigurable() && myWidgetFactory.isAvailable(project));
-        return;
+
+      if (ActionPlaces.isMainMenuOrActionSearch(e.place)) {
+        e.presentation.isEnabledAndVisible = widgetFactory.isConfigurable && widgetFactory.isAvailable(project)
+        return
       }
-      StatusBar statusBar = e.getData(PlatformDataKeys.STATUS_BAR);
-      e.getPresentation().setEnabledAndVisible(statusBar != null && project.getService(StatusBarWidgetsManager.class)
-        .canBeEnabledOnStatusBar(myWidgetFactory, statusBar));
+
+      val statusBar = e.getData(PlatformDataKeys.STATUS_BAR)
+      e.presentation.isEnabledAndVisible = statusBar != null && project.service<StatusBarWidgetsManager>()
+        .canBeEnabledOnStatusBar(widgetFactory, statusBar)
     }
 
-    @Override
-    public @NotNull ActionUpdateThread getActionUpdateThread() {
-      return ActionUpdateThread.BGT;
-    }
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 
-    @Override
-    public boolean isSelected(@NotNull AnActionEvent e) {
-      return StatusBarWidgetSettings.getInstance().isEnabled(myWidgetFactory);
-    }
+    override fun isSelected(e: AnActionEvent): Boolean = StatusBarWidgetSettings.getInstance().isEnabled(widgetFactory)
 
-    @Override
-    public void setSelected(@NotNull AnActionEvent e, boolean state) {
-      StatusBarWidgetSettings.getInstance().setEnabled(myWidgetFactory, state);
-      for (Project project : ProjectManager.getInstance().getOpenProjects()) {
-        project.getService(StatusBarWidgetsManager.class).updateWidget(myWidgetFactory);
+    override fun setSelected(e: AnActionEvent, state: Boolean) {
+      StatusBarWidgetSettings.getInstance().setEnabled(widgetFactory, state)
+      for (project in ProjectManager.getInstance().openProjects) {
+        project.service<StatusBarWidgetsManager>().updateWidget(widgetFactory)
       }
     }
   }
+}
 
-  private static class HideCurrentWidgetAction extends DumbAwareAction {
-    @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
-      StatusBarWidgetFactory factory = getFactory(e);
-      if (factory == null) return;
-
-      StatusBarWidgetSettings.getInstance().setEnabled(factory, false);
-      for (Project project : ProjectManager.getInstance().getOpenProjects()) {
-        project.getService(StatusBarWidgetsManager.class).updateWidget(factory);
-      }
-    }
-
-    @Override
-    public void update(@NotNull AnActionEvent e) {
-      super.update(e);
-      StatusBarWidgetFactory factory = getFactory(e);
-      e.getPresentation().setEnabledAndVisible(factory != null && factory.isConfigurable());
-      if (factory != null) {
-        e.getPresentation().setText(UIBundle.message("status.bar.hide.widget.action.name", factory.getDisplayName()));
-      }
-    }
-
-    @Override
-    public @NotNull ActionUpdateThread getActionUpdateThread() {
-      return ActionUpdateThread.BGT;
-    }
-
-    private @Nullable
-    static StatusBarWidgetFactory getFactory(@NotNull AnActionEvent e) {
-      Project project = e.getProject();
-      String hoveredWidgetId = e.getData(IdeStatusBarImpl.HOVERED_WIDGET_ID);
-      StatusBar statusBar = e.getData(PlatformDataKeys.STATUS_BAR);
-      if (project != null && hoveredWidgetId != null && statusBar != null) {
-        return project.getService(StatusBarWidgetsManager.class).findWidgetFactory(hoveredWidgetId);
-      }
-      return null;
+private class HideCurrentWidgetAction : DumbAwareAction() {
+  companion object {
+    private fun getFactory(e: AnActionEvent): StatusBarWidgetFactory? {
+      val project = e.project ?: return null
+      val hoveredWidgetId = e.getData(IdeStatusBarImpl.HOVERED_WIDGET_ID)  ?: return null
+      e.getData(PlatformDataKeys.STATUS_BAR)  ?: return null
+      return project.service<StatusBarWidgetsManager>().findWidgetFactory(hoveredWidgetId)
     }
   }
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val factory = getFactory(e) ?: return
+    StatusBarWidgetSettings.getInstance().setEnabled(factory = factory, newValue = false)
+    for (project in ProjectManager.getInstance().openProjects) {
+      project.getService(StatusBarWidgetsManager::class.java).updateWidget(factory)
+    }
+  }
+
+  override fun update(e: AnActionEvent) {
+    super.update(e)
+    val factory = getFactory(e)
+    e.presentation.isEnabledAndVisible = factory != null && factory.isConfigurable
+    if (factory != null) {
+      e.presentation.text = UIBundle.message("status.bar.hide.widget.action.name", factory.displayName)
+    }
+  }
+
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 }
