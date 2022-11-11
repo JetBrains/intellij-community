0bb7249b4d90dea9a2cc4782af72772d8d174dcc
Rustam Vishniakov <rustam.vishnyakov@jetbrains.com>
2022-11-11 11:47:04 +0100

    [settings-sync] Remove extra action constructor and create an additional class [IJ-CR-98156]
    GitOrigin-RevId: 3d9582a393e19673653bf7bd950f761b0587ba29

--- plugins/settings-sync/src/com/intellij/settingsSync/config/SettingsSyncOpenSettingsAction.kt
+++ plugins/settings-sync/src/com/intellij/settingsSync/config/SettingsSyncOpenSettingsAction.kt
@@ -8,10 +8,9 @@ import com.intellij.settingsSync.SettingsSyncBundle
 
 open class SettingsSyncOpenSettingsAction(text: @NlsActions.ActionText String?) : DumbAwareAction(text) {
 
-  @Suppress("Unused") // Used in plugin.xml for default base action
-  constructor() : this("${SettingsSyncBundle.message("title.settings.sync")}...")
-
   override fun actionPerformed(e: AnActionEvent) {
     ShowSettingsUtil.getInstance().showSettingsDialog(e.project, SettingsSyncConfigurable::class.java)
   }
+
+  class Simple : SettingsSyncOpenSettingsAction("${SettingsSyncBundle.message("title.settings.sync")}...")
 }
