88b6b8211bee3995ec5115a3523674cee6edcf1e
Jinseong Jeon <jsjeon@google.com>
2022-11-11 23:55:35 -0800

    FE1.0 UAST: adapt to migration of descriptor util
    ^KTIJ-23602 Fixed
    
    GitOrigin-RevId: 3f6feeffef0b6e2ebb58e2672a4968f0ddbaca3a

--- plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/KotlinUastResolveProviderService.kt
+++ plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/KotlinUastResolveProviderService.kt
@@ -5,7 +5,6 @@ package org.jetbrains.uast.kotlin
 import com.intellij.psi.*
 import org.jetbrains.kotlin.analysis.api.types.KtTypeNullability
 import org.jetbrains.kotlin.asJava.toLightAnnotation
-import org.jetbrains.kotlin.backend.common.descriptors.explicitParameters
 import org.jetbrains.kotlin.builtins.createFunctionType
 import org.jetbrains.kotlin.codegen.state.KotlinTypeMapper
 import org.jetbrains.kotlin.config.LanguageVersionSettings
