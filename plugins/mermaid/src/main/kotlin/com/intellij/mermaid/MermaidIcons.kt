496109ccf92de1a9183b9cb9de3e386e8c897837
Ivan Posti <ivan.posti@jetbrains.com>
2022-11-14 17:00:49 +0100

    Add plugin icon
    GitOrigin-RevId: fa6449bfc5cba739c8413294fc82516b58e20fbe

---
+++ plugins/mermaid/src/main/kotlin/com/intellij/mermaid/MermaidIcons.kt
@@ -0,0 +1,8 @@
+package com.intellij.mermaid
+
+import com.intellij.openapi.util.IconLoader
+
+object MermaidIcons {
+  @JvmField
+  val Plugin = IconLoader.getIcon("/META-INF/pluginIcon.svg", javaClass)
+}
