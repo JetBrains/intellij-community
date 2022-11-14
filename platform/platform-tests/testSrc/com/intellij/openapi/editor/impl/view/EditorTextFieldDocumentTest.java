161d20461b75c736b21bbc5c249eb1c347b09f96
Andrey Sokolov <andrey.sokolov@jetbrains.com>
2022-11-14 12:53:08 +0100

    IDEA-305264 simplified test for selection in EditorTextFieldDocumentTest
    (cherry picked from commit 7c61a8c732609a0cb1df0e806ac12c2cc747493c)
    
    IJ-CR-98150
    
    GitOrigin-RevId: bf4b688ca1b2f4486c8876d49cd0230c17c2a2a8

--- platform/platform-tests/testSrc/com/intellij/openapi/editor/impl/view/EditorTextFieldDocumentTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/editor/impl/view/EditorTextFieldDocumentTest.java
@@ -1,32 +1,16 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.editor.impl.view;
 
-import com.intellij.openapi.editor.EditorFactory;
-import com.intellij.openapi.editor.VisualPosition;
-import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.editor.impl.AbstractEditorTest;
 import com.intellij.openapi.editor.impl.EditorTextFieldRendererDocument;
-import com.intellij.testFramework.EditorTestUtil;
 
 public class EditorTextFieldDocumentTest extends AbstractEditorTest {
   public void testSelection() {
     EditorTextFieldRendererDocument document = new EditorTextFieldRendererDocument();
     document.setText("""
-                         12345
-                         67890
-                         """);
-    EditorEx editor = (EditorEx)EditorFactory.getInstance().createViewer(document, getProject());
-    setEditor(editor);
-    try {
-      EditorTestUtil.setEditorVisibleSize(getEditor(), 100, 100);
-      mouse().pressAt(0, 0).dragTo(1, 3).release();
-      assertEquals(new VisualPosition(1, 3), getEditor().getCaretModel().getVisualPosition());
-      assertEquals("""
-                     12345
-                     678""",editor.getSelectionModel().getSelectedText());
-    } finally {
-      EditorFactory.getInstance().releaseEditor(editor);
-    }
-
+                       12345
+                       67890
+                       """);
+    assertEquals(5, document.getLineEndOffset(0));
   }
 }
