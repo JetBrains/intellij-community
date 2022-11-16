3514495898f80d043ec387a96a7d80b894c3cd26
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2022-11-16 15:16:01 +0100

    DS-4015 add remote file browser to DataSpell SSH remote interpreter wizard
    (cherry picked from commit f2b38062cd8d49b3b536bb851c0ed55a2c94bd89)
    
    IJ-CR-98701
    
    GitOrigin-RevId: 2bc0be5102f3571b110a83f10c770e0bb1820d8c

--- python/src/com/jetbrains/python/sdk/add/target/ProjectSync.kt
+++ python/src/com/jetbrains/python/sdk/add/target/ProjectSync.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.sdk.add.target
 
 import com.intellij.execution.target.TargetEnvironmentConfiguration
@@ -10,7 +10,10 @@ import com.intellij.ui.dsl.builder.Panel
  * The class is stateful.
  */
 interface ProjectSync {
-  fun extendDialogPanelWithOptionalFields(panel: Panel)
+  @Deprecated("Use extendDialogPanelWithOptionalFields(Panel, TargetEnvironmentConfiguration)")
+  fun extendDialogPanelWithOptionalFields(panel: Panel): Unit = throw UnsupportedOperationException()
+  fun extendDialogPanelWithOptionalFields(panel: Panel, targetEnvConf: TargetEnvironmentConfiguration): Unit =
+    extendDialogPanelWithOptionalFields(panel)
 
   fun apply(configuration: TargetEnvironmentConfiguration)
 }
