d07bf2da51fba50b78069ca852bc5e4a61835051
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2022-11-16 14:59:07 +0100

    IDEA-189985 - Remove DevKitInspectionBase
    GitOrigin-RevId: e925d01ae69fc7222035485e4e0048a8c700eccd

--- plugins/devkit/devkit-core/src/inspections/DevKitInspectionUtil.java
+++ plugins/devkit/devkit-core/src/inspections/DevKitInspectionUtil.java
@@ -1,12 +1,9 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.inspections;
 
-import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool;
-import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
-import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.idea.devkit.module.PluginModuleType;
@@ -14,30 +11,14 @@ import org.jetbrains.idea.devkit.util.PsiUtil;
 
 import java.util.function.Predicate;
 
-/**
- * @deprecated use {@link DevKitUastInspectionBase} instead.
- *
- * @author swr
- */
-@Deprecated(forRemoval = true)
-public abstract class DevKitInspectionBase extends AbstractBaseJavaLocalInspectionTool {
+public final class DevKitInspectionUtil {
 
-  @NotNull
-  @Override
-  public final PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
-    return isAllowed(holder.getFile()) ? buildInternalVisitor(holder, isOnTheFly) : PsiElementVisitor.EMPTY_VISITOR;
-  }
-
-  protected PsiElementVisitor buildInternalVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
-    return super.buildVisitor(holder, isOnTheFly);
+  static boolean isAllowedInPluginsOnly(@NotNull PsiFile file) {
+    return isAllowed(file, DevKitInspectionUtil::isPluginFile);
   }
 
   public static boolean isAllowed(@NotNull PsiFile file) {
     return isAllowed(file, __ -> true);
-  }
-
-  static boolean isAllowedInPluginsOnly(@NotNull PsiFile file) {
-    return isAllowed(file, DevKitInspectionBase::isPluginFile);
   }
 
   private static boolean isAllowed(@NotNull PsiFile file, @NotNull Predicate<? super PsiFile> predicate) {
