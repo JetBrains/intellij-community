f1b683947c57626f1b1328a0e5410063d9210d3d
Sasha Blashenkov <Sasha.Blashenkov@jetbrains.com>
2022-11-17 15:26:36 +0100

    Fix path->string problem, CWM-7614 Guest has a wrong name after user connected to Remote dev session on the same device
    (cherry picked from commit dd9527ec73a901479ae2621717ad29676f889e9c)
    
    IJ-CR-98746
    
    GitOrigin-RevId: 8d3d8af846e21a9497abe4abe610a442ef8b47c9

--- platform/remoteDev-util/src/com/intellij/remoteDev/downloader/CodeWithMeClientDownloader.kt
+++ platform/remoteDev-util/src/com/intellij/remoteDev/downloader/CodeWithMeClientDownloader.kt
@@ -652,7 +652,7 @@ object CodeWithMeClientDownloader {
 
     if (SystemInfo.isWindows) {
       val hProcess = WindowsFileUtil.windowsShellExecute(
-        executable = executable.toString(),
+        executable = executable,
         workingDirectory = extractedJetBrainsClientData.clientDir,
         parameters = parameters
       )
