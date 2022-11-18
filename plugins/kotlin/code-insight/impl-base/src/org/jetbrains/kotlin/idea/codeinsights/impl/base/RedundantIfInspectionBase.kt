439e1eaa2b263de82c6f1311c549b5a39070dd5c
Jaebaek Seo <jaebaek@google.com>
2022-11-18 00:00:17 +0000

    [kotlin] K2: Remove redundant if without else-expression
    The existing RemoveRedundantIfInspection does not handle when the
    if-expression does not have an else-expression. This commit handles the
    case. For example,
    
    ```
    // Before the inspection:
    fun foo(bar: Int): Boolean {
      if (bar % 2 == 0) return true
      return false
    }
    
    // After the inspection:
    fun foo(bar: Int): Boolean {
      return bar % 2 == 0
    }
    ```
    
    ^KTIJ-17793 fixed

--- plugins/kotlin/code-insight/impl-base/src/org/jetbrains/kotlin/idea/codeinsights/impl/base/RedundantIfInspectionBase.kt
+++ plugins/kotlin/code-insight/impl-base/src/org/jetbrains/kotlin/idea/codeinsights/impl/base/RedundantIfInspectionBase.kt
@@ -9,11 +9,14 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.PsiFile
+import com.intellij.psi.SmartPointerManager
+import com.intellij.psi.SmartPsiElementPointer
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.codeinsight.api.classic.inspections.AbstractKotlinInspection
 import org.jetbrains.kotlin.idea.codeinsight.utils.negate
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.getNextSiblingIgnoringWhitespaceAndComments
 
 /**
  * A parent class for K1 and K2 RedundantIfInspection.
@@ -26,17 +29,23 @@ import org.jetbrains.kotlin.psi.*
  */
 abstract class RedundantIfInspectionBase : AbstractKotlinInspection(), CleanupLocalInspectionTool {
 
+    private data class IfExpressionRedundancyInfo(
+        val redundancyType: RedundancyType,
+        val branchType: BranchType,
+        val returnAfterIf: KtExpression?,
+    )
+
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean, session: LocalInspectionToolSession): PsiElementVisitor {
         return ifExpressionVisitor { expression ->
             if (expression.condition == null) return@ifExpressionVisitor
-            val (redundancyType, branchType) = RedundancyType.of(expression)
+            val (redundancyType, branchType, returnAfterIf) = RedundancyType.of(expression)
             if (redundancyType == RedundancyType.NONE) return@ifExpressionVisitor
 
             holder.registerProblem(
                 expression,
                 KotlinBundle.message("redundant.if.statement"),
                 ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
-                RemoveRedundantIf(redundancyType, branchType)
+                RemoveRedundantIf(redundancyType, branchType, returnAfterIf)
             )
         }
     }
@@ -55,10 +64,12 @@ abstract class RedundantIfInspectionBase : AbstractKotlinInspection(), CleanupLo
 
         data class LabeledReturn(val label: String) : BranchType()
 
-        class Assign(val lvalue: KtExpression) : BranchType() {
-            override fun equals(other: Any?) = other is Assign && lvalue.text == other.lvalue.text
+        class Assign(left: KtExpression) : BranchType() {
+            val lvalue: SmartPsiElementPointer<KtExpression> = left.let(SmartPointerManager::createPointer)
 
-            override fun hashCode() = lvalue.text.hashCode()
+            override fun equals(other: Any?) = other is Assign && lvalue.element?.text == other.lvalue.element?.text
+
+            override fun hashCode() = lvalue.element?.text.hashCode()
         }
     }
 
@@ -68,15 +79,31 @@ abstract class RedundantIfInspectionBase : AbstractKotlinInspection(), CleanupLo
         ELSE_TRUE;
 
         companion object {
-            fun of(expression: KtIfExpression): Pair<RedundancyType, BranchType> {
-                val (thenReturn, thenType) = expression.then?.getBranchExpression() ?: return NONE to BranchType.Simple
-                val (elseReturn, elseType) = expression.`else`?.getBranchExpression() ?: return NONE to BranchType.Simple
+            private val RedundancyInfoWithNone = IfExpressionRedundancyInfo(NONE, BranchType.Simple, null)
+
+            fun of(expression: KtIfExpression): IfExpressionRedundancyInfo {
+                val (thenReturn, thenType) = expression.then?.getBranchExpression() ?: return RedundancyInfoWithNone
+                val elseOrReturnAfterIf = expression.`else` ?:
+                    // When the target if-expression does not have an else expression and the next expression is a return expression,
+                    // we can consider it as an else expression. For example,
+                    //     fun foo(bar: String?):Boolean {
+                    //       if (bar == null) return false
+                    //       return true
+                    //     }
+                    expression.returnAfterIf() ?: return RedundancyInfoWithNone
+                val (elseReturn, elseType) = elseOrReturnAfterIf.getBranchExpression() ?: return RedundancyInfoWithNone
 
                 return when {
-                    thenType != elseType -> NONE to BranchType.Simple
-                    KtPsiUtil.isTrueConstant(thenReturn) && KtPsiUtil.isFalseConstant(elseReturn) -> THEN_TRUE to thenType
-                    KtPsiUtil.isFalseConstant(thenReturn) && KtPsiUtil.isTrueConstant(elseReturn) -> ELSE_TRUE to thenType
-                    else -> NONE to BranchType.Simple
+                    thenType != elseType -> RedundancyInfoWithNone
+                    KtPsiUtil.isTrueConstant(thenReturn) && KtPsiUtil.isFalseConstant(elseReturn) -> IfExpressionRedundancyInfo(
+                        THEN_TRUE, thenType, if (expression.`else` == null) elseOrReturnAfterIf else null
+                    )
+
+                    KtPsiUtil.isFalseConstant(thenReturn) && KtPsiUtil.isTrueConstant(elseReturn) -> IfExpressionRedundancyInfo(
+                        ELSE_TRUE, thenType, if (expression.`else` == null) elseOrReturnAfterIf else null
+                    )
+
+                    else -> RedundancyInfoWithNone
                 }
             }
 
@@ -88,8 +115,11 @@ abstract class RedundantIfInspectionBase : AbstractKotlinInspection(), CleanupLo
                     }
 
                     is KtBlockExpression -> statements.singleOrNull()?.getBranchExpression()
-                    is KtBinaryExpression -> if (operationToken == KtTokens.EQ && left != null) right to BranchType.Assign(left!!)
-                    else null
+                    is KtBinaryExpression -> {
+                        val left = left
+                        if (operationToken == KtTokens.EQ && left != null) right to BranchType.Assign(left)
+                        else null
+                    }
 
                     else -> this to BranchType.Simple
                 }
@@ -99,9 +129,11 @@ abstract class RedundantIfInspectionBase : AbstractKotlinInspection(), CleanupLo
 
     private inner class RemoveRedundantIf(
         private val redundancyType: RedundancyType,
-        @SafeFieldForPreview // may refer to PsiElement of original file but we are only reading from it
         private val branchType: BranchType,
+        returnAfterIf: KtExpression?,
     ) : LocalQuickFix {
+        val returnExpressionAfterIf: SmartPsiElementPointer<KtExpression>? = returnAfterIf?.let(SmartPointerManager::createPointer)
+
         override fun getName() = KotlinBundle.message("remove.redundant.if.text")
         override fun getFamilyName() = name
 
@@ -120,11 +152,17 @@ abstract class RedundantIfInspectionBase : AbstractKotlinInspection(), CleanupLo
             val newExpressionOnlyWithCondition = when (branchType) {
                 is BranchType.Return -> factory.createExpressionByPattern("return $0", condition)
                 is BranchType.LabeledReturn -> factory.createExpressionByPattern("return${branchType.label} $0", condition)
-                is BranchType.Assign -> factory.createExpressionByPattern("$0 = $1", branchType.lvalue, condition)
+                is BranchType.Assign -> factory.createExpressionByPattern("$0 = $1", branchType.lvalue.element!!, condition)
                 else -> condition
             }
 
             runWriteAction {
+                /**
+                 * This is the case that we used the next expression of the if expression as the else expression.
+                 * See the code and comment in [RedundancyType.of].
+                 */
+                returnExpressionAfterIf?.element?.delete()
+
                 element.replace(newExpressionOnlyWithCondition)
             }
         }
@@ -138,3 +176,8 @@ abstract class RedundantIfInspectionBase : AbstractKotlinInspection(), CleanupLo
             }
     }
 }
+
+/**
+ * Returns the sibling expression after [KtIfExpression] if it is [KtReturnExpression]. Otherwise, returns null.
+ */
+private fun KtIfExpression.returnAfterIf(): KtReturnExpression? = getNextSiblingIgnoringWhitespaceAndComments() as? KtReturnExpression
