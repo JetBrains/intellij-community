c18d050762e710694dbd0be4516782cf312c466b
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2022-11-18 17:50:10 +0100

    IFT-426 Show hint in the Learn tab about switching to the old UI to enable lessons
    IJ-CR-98858
    
    GitOrigin-RevId: 774dcd24209a8158044525a54e2b1fc02ee2aa40

--- plugins/ide-features-trainer/src/training/ui/welcomeScreen/IFTInteractiveCourse.kt
+++ plugins/ide-features-trainer/src/training/ui/welcomeScreen/IFTInteractiveCourse.kt
@@ -2,9 +2,16 @@
 package training.ui.welcomeScreen
 
 import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.wm.InteractiveCourseData
 import com.intellij.openapi.wm.InteractiveCourseFactory
 import com.intellij.openapi.wm.impl.welcomeScreen.learnIde.InteractiveCoursePanel
+import com.intellij.ui.ColorUtil
+import com.intellij.ui.ExperimentalUI
+import com.intellij.ui.HyperlinkAdapter
+import com.intellij.util.ui.HTMLEditorKitBuilder
+import com.intellij.util.ui.JBUI
+import com.intellij.util.ui.UIUtil
 import training.FeaturesTrainerIcons
 import training.lang.LangManager
 import training.learn.CourseManager
@@ -14,17 +21,44 @@ import training.learn.course.IftModule
 import training.ui.views.NewContentLabel
 import training.util.iftPluginIsUsing
 import training.util.learningPanelWasOpenedInCurrentVersion
+import java.awt.Color
+import java.awt.Component
 import java.awt.event.ActionEvent
-import javax.swing.AbstractAction
-import javax.swing.Action
-import javax.swing.Icon
-import javax.swing.JComponent
+import javax.swing.*
+import javax.swing.event.HyperlinkEvent
+import javax.swing.text.html.HTMLDocument
 
 internal class IFTInteractiveCourse : InteractiveCourseFactory {
 
   override val isActive: Boolean get() = LangManager.getInstance().getLangSupport()?.useUserProjects == false
 
-  override fun getInteractiveCourseComponent(): JComponent = InteractiveCoursePanel(IFTInteractiveCourseData())
+  override fun getInteractiveCourseComponent(): JComponent = IFTInteractiveCoursePanel()
+}
+
+private class IFTInteractiveCoursePanel : InteractiveCoursePanel(IFTInteractiveCourseData(), !ExperimentalUI.isNewUI()) {
+  init {
+    if (ExperimentalUI.isNewUI()) {
+      add(JTextPane().apply {
+        contentType = "text/html"
+        addHyperlinkListener(object : HyperlinkAdapter() {
+          override fun hyperlinkActivated(e: HyperlinkEvent) {
+            Registry.get("ide.experimental.ui").setValue(false)
+          }
+        })
+        editorKit = HTMLEditorKitBuilder.simple()
+        text = LearnBundle.message("welcome.tab.toggle.new.ui.hint")
+
+        val styleSheet = (document as HTMLDocument).styleSheet
+        val textColor = "#" + ColorUtil.toHex(UIUtil.getLabelInfoForeground())
+        styleSheet.addRule("body { color: $textColor; font-size:${JBUI.Fonts.label().lessOn(3f)}pt;}")
+        isEditable = false
+        @Suppress("UseJBColor")
+        background = Color(0, true)
+        border = JBUI.Borders.empty(14, leftMargin, 0, 0)
+        alignmentX = Component.LEFT_ALIGNMENT
+      })
+    }
+  }
 }
 
 private class IFTInteractiveCourseData : InteractiveCourseData {
