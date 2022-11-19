3b6f673b832258e2606ba727aa0ac69ca31a522d
Alexandr Evstigneev <alexandr.evstigneev@jetbrains.com>
2022-11-19 13:02:36 +0400

    IDEA-306358 EA-719755 removed redundant concurrency note
    After last fix, this method looks a thread safe and may be used concurrently
    
    GitOrigin-RevId: c68681e662e73172da0681d4c8777cea79af4ee5

--- platform/platform-util-io/src/com/intellij/execution/process/AnsiEscapeDecoder.java
+++ platform/platform-util-io/src/com/intellij/execution/process/AnsiEscapeDecoder.java
@@ -29,8 +29,6 @@ public class AnsiEscapeDecoder {
    * @param textAcceptor receives text fragments with color attributes.
    *                     It can implement ColoredChunksAcceptor to receive list of pairs (text, attribute).
    * @apiNote <ul>
-   * <li>method is not in general a thread safe. You may use it safely from 2 threads - one for {@code stdout} and one for {@code stderr}
-   * of the process. But you should not invoke it concurrently for the same output channel.</li>
    * <li>method does not guarantee synchronous processing. Meaning you should not expect that {@code textAcceptor} got all colored chunks
    * after invoking this method (despite the current implementation). Processing may be deferred. The only guarantee here, is that
    * {@code text} passed to the method is going to be processed in the same order as it was passed for each channel: {@code stdout},
