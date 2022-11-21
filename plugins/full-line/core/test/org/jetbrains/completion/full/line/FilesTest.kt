4663640ef9fecc0ecc07eba99d70dd499cdff91f
igor.davidenko <igor.davidenko@jetbrains.com>
2022-11-21 17:43:16 +0100

    [full-line] fix js tests path
    GitOrigin-RevId: 13a6668259b140af315bb215b4a66f894478018c

--- plugins/full-line/core/test/org/jetbrains/completion/full/line/FilesTest.kt
+++ plugins/full-line/core/test/org/jetbrains/completion/full/line/FilesTest.kt
@@ -1,12 +1,17 @@
 package org.jetbrains.completion.full.line
 
 import com.intellij.openapi.application.PluginPathManager
+import java.nio.file.Paths
 
 object FilesTest {
   const val FORMAT_BEFORE_FOLDER = "before-formatting"
   const val FORMAT_AFTER_FOLDER = "after-formatting"
 
   fun readFile(filename: String, lang: String): String {
-    return PluginPathManager.getPluginHome("full-line").resolve("$lang/testResources/$filename").readText()
+    var testFile = PluginPathManager.getPluginHome("full-line").resolve("$lang/testResources/$filename")
+    if (!testFile.exists()) {
+      testFile = Paths.get(testFile.path.replace("community", "")).toRealPath().toFile()
+    }
+    return testFile.readText()
   }
 }
