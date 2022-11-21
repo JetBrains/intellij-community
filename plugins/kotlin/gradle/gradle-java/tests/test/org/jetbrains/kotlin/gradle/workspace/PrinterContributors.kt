956a3d76599f40c777121b391ace491937ef91d7
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2022-11-21 19:19:30 +0100

    [kt, tests] Refactor WorkspacePrinting (6/8)
    This series of commits prepares Workspace-infrastructure for further
    changes **without** changing the behaviour.
    
    After previous refactorings and, notably, cutting support
    for libraries/SDKs printing, a lot of abstractions became
    redundant and were inlined.
    
    The main idea for now is that the only IJ-entity which we
    wrap/model is OrderEntry, because we need to do some tweaks
    (like, replacing a bunch of them for K/N dist) only for them.
    In such cases wrapping is indeed useful, as you can provide
    your own implementation.
    
    Everything else is passed as plain IJ entities, which simplifies
    code a bit by removing wrappers.
    
    Main consequence:
    - Inline type of com.intellij.openapi.Module into
    ModulePrinterContributor. Removed ModulePrinterEntity and
    generic WorkspacePrinterContributor
    
    Side simplifications:
    - Removed ContributableEntity, wasn't very useful
    - Inlines processEntities in WorkspaceModelPrinter
    - Remove CompositeContributor, use just lists instead (as the
      only entry-point is through factories, which collect a list
      anyway, there's no need in additional abstraction)
    
    (cherry picked from commit 95081e82790f0033dadaea02695092299fbb8407)
    
    GitOrigin-RevId: 7e594ffc92158ce1f96c2126e7a7d6a0d5e22173

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/workspace/PrinterContributors.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/workspace/PrinterContributors.kt
@@ -3,47 +3,24 @@
 package org.jetbrains.kotlin.gradle.workspace
 
 import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.OrderEntry
 import org.jetbrains.kotlin.config.KotlinFacetSettingsProvider
 import org.jetbrains.kotlin.utils.Printer
 
-interface WorkspaceModelPrinterContributor<T : ContributableEntity> {
-    fun process(entity: T, printer: Printer)
-}
-
-interface ModulePrinterContributor : WorkspaceModelPrinterContributor<ModulePrinterEntity>
-
-class CompositeWorkspaceModelPrinterContributor<T : ContributableEntity>(
-    private val firstContributor: WorkspaceModelPrinterContributor<T>,
-    private vararg val contributors: WorkspaceModelPrinterContributor<T>,
-) : WorkspaceModelPrinterContributor<T> {
-    override fun process(entity: T, printer: Printer) {
-        firstContributor.process(entity, printer)
-        contributors.forEach { it.process(entity, printer) }
-    }
-}
-
-internal fun <T : ContributableEntity> compositeContributor(
-    vararg contributors: WorkspaceModelPrinterContributor<T>
-): WorkspaceModelPrinterContributor<T>? {
-    if (contributors.isEmpty()) return null
-
-    val first = contributors.first()
-    val rest = contributors.copyOfRange(1, contributors.size)
-
-    return CompositeWorkspaceModelPrinterContributor(first, *rest)
+interface ModulePrinterContributor {
+    fun process(module: Module, printer: Printer)
 }
 
 class NoopModulePrinterContributor : ModulePrinterContributor {
-    override fun process(entity: ModulePrinterEntity, printer: Printer) = Unit
+    override fun process(module: Module, printer: Printer) = Unit
 }
 
 class KotlinFacetSettingsPrinterContributor : ModulePrinterContributor {
-    override fun process(entity: ModulePrinterEntity, printer: Printer) = with(printer) {
-        if (entity !is ModulePrinterEntityImpl) return // synthetic entity, skip
-        val facetSettings = runReadAction { KotlinFacetSettingsProvider.getInstance(entity.module.project)
-            ?.getSettings(entity.module) }
+    override fun process(module: Module, printer: Printer) = with(printer) {
+        val facetSettings = runReadAction { KotlinFacetSettingsProvider.getInstance(module.project)
+            ?.getSettings(module) }
             ?: return
 
         indented {
@@ -64,10 +41,8 @@ class KotlinFacetSettingsPrinterContributor : ModulePrinterContributor {
 }
 
 class SanitizingOrderEntryPrinterContributor : ModulePrinterContributor {
-
-    override fun process(entity: ModulePrinterEntity, printer: Printer) = with(printer) {
-        if (entity !is ModulePrinterEntityImpl) return // synthetic entity, skip
-        val orderEntries = runReadAction { ModuleRootManager.getInstance(entity.module).orderEntries }
+    override fun process(module: Module, printer: Printer) = with(printer) {
+        val orderEntries = runReadAction { ModuleRootManager.getInstance(module).orderEntries }
             .map(OrderEntry::toPrinterEntity)
         val sanitizedEntries = replaceNativeDistributionOrderEntries(orderEntries)
 
