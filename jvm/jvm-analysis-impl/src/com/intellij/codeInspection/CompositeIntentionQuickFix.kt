a7bb93e06ed3842627cad9fad833bc9f6c848c12
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2022-11-22 19:06:08 +0100

    [jvm] Introduce CompositeIntentionQuickFix
    GitOrigin-RevId: d58f4d8909a93aed75c7e4329c8d1c68c5aac703

---
+++ jvm/jvm-analysis-impl/src/com/intellij/codeInspection/CompositeIntentionQuickFix.kt
@@ -0,0 +1,59 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.codeInspection
+
+import com.intellij.codeInsight.FileModificationService
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo
+import com.intellij.codeInsight.intention.preview.IntentionPreviewUtils
+import com.intellij.lang.jvm.JvmModifiersOwner
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.SmartPointerManager
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.util.asSafely
+import org.jetbrains.uast.toUElement
+
+/**
+ * A quickfix that can call multiple JVM intention actions and bundle them into a single quick fix.
+ */
+abstract class CompositeIntentionQuickFix: LocalQuickFix {
+  override fun startInWriteAction(): Boolean = false
+
+  protected fun generatePreviews(project: Project, previewDescriptor: ProblemDescriptor, element: PsiElement): IntentionPreviewInfo {
+    val containingFile = previewDescriptor.startElement.containingFile ?: return IntentionPreviewInfo.EMPTY
+    val editor = IntentionPreviewUtils.getPreviewEditor() ?: return IntentionPreviewInfo.EMPTY
+    val target = SmartPointerManager.getInstance(project).createSmartPsiElementPointer(element)
+    getActions(project, previewDescriptor).forEach { factory ->
+      val actions = factory(target.element?.javaPhysElement() ?: return@forEach)
+      actions.forEach { action ->
+        action.generatePreview(project, editor, containingFile)
+      }
+    }
+    return IntentionPreviewInfo.DIFF
+  }
+
+  private fun PsiElement.javaPhysElement(): JvmModifiersOwner? {
+    // workaround because langElement.originalElement doesn't always work
+    val langPhysElement = PsiTreeUtil.findSameElementInCopy(navigationElement, navigationElement.containingFile.originalFile)
+    return langPhysElement.toUElement()?.javaPsi?.asSafely<JvmModifiersOwner>()
+  }
+
+  protected fun applyFixes(project: Project, descriptor: ProblemDescriptor, element: PsiElement) {
+    val containingFile = descriptor.psiElement.containingFile ?: return
+    val target = SmartPointerManager.getInstance(project).createSmartPsiElementPointer(element)
+    if (!FileModificationService.getInstance().prepareFileForWrite(element.containingFile)) return
+    getActions(project, descriptor).forEach { factory ->
+      val actions = factory(target.element.asSafely<JvmModifiersOwner>() ?: return@forEach)
+      actions.forEach { action ->
+        if (action.startInWriteAction()) {
+          runWriteAction { action.invoke(project, null, containingFile) }
+        } else {
+          action.invoke(project, null, containingFile)
+        }
+      }
+    }
+  }
+
+  protected abstract fun getActions(project: Project, descriptor: ProblemDescriptor): List<(JvmModifiersOwner) -> List<IntentionAction>>
+}
