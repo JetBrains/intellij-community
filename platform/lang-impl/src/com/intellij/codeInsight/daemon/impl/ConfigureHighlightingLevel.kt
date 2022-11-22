8510802c671e058b3a20f042fbd2bb9aa3860b1b
Nikolay Egorov <nikolay.egorov@jetbrains.com>
2022-11-22 09:53:33 +0200

    [KTNB] Refactor NotebookInjectedCodeUtility to NotebookHighlightingUtilityObject
    GitOrigin-RevId: c029f9d3aa5ff7cc03c83c19ff39491f3e46e325

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/ConfigureHighlightingLevel.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/ConfigureHighlightingLevel.kt
@@ -4,7 +4,6 @@ package com.intellij.codeInsight.daemon.impl
 import com.intellij.codeInsight.daemon.DaemonBundle.message
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.codeInsight.daemon.impl.analysis.FileHighlightingSetting
-import com.intellij.codeInsight.daemon.impl.analysis.HighlightInfoHolder
 import com.intellij.codeInsight.daemon.impl.analysis.HighlightLevelUtil.forceRootHighlighting
 import com.intellij.codeInsight.daemon.impl.analysis.HighlightingSettingsPerFile
 import com.intellij.lang.Language
@@ -12,17 +11,13 @@ import com.intellij.lang.injection.InjectedLanguageManager
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.CommonDataKeys.PSI_FILE
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.EditorBundle
 import com.intellij.openapi.editor.markup.InspectionsLevel
-import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.ui.popup.JBPopup
 import com.intellij.openapi.ui.popup.JBPopupFactory
-import com.intellij.openapi.util.Key
-import com.intellij.openapi.util.TextRange
 import com.intellij.psi.FileViewProvider
 import com.intellij.psi.PsiFile
 
@@ -102,49 +97,8 @@ internal class ConfigureHighlightingLevelAction : DumbAwareAction() {
 }
 
 object NotebookInjectedCodeUtility {
-  @JvmField
-  val NOTEBOOK_DOCUMENT_IGNORE_ANALYSIS_RANGE = Key.create<TextRange>("notebook.document.ignored.range")
-  @JvmField
-  val ANALYZER_PASS_INJECTED_INFO_HOLDER_KEY: Key<HighlightInfoHolder> = Key.create("injected.element.pass.info.holder")
-
-  @JvmField
-  val NOTEBOOK_FILE_ANALYSIS_DONE_KEY = Key.create<Boolean>("notebook.file.analysis.done")
-
   private const val notebookInjectedFileExtension: String = "jupyter.kts"
-  private const val notebookInjectedMetaFileExtension: String = "juktm"
-  private const val notebookDocumentFileExtension: String = "ipynb"
-  private const val notebookInjectedFileSuffix = ".$notebookInjectedFileExtension"
 
   fun isSuitableKtNotebookFragment(psiFile: PsiFile?): Boolean =
-    psiFile?.name?.endsWith(notebookInjectedFileSuffix) == true
-
-  fun isLooksLikeNotebookDocument(document: Document): Boolean =
-    FileDocumentManager.getInstance().getFile(document)?.extension == notebookDocumentFileExtension
-
-  fun isLooksLikeNotebookFile(file: PsiFile): Boolean =
-    file.fileType.defaultExtension == notebookDocumentFileExtension
-
-  fun isShouldReusePreviousAnalysisForNotebook(document: Document): Boolean =
-    isLooksLikeNotebookDocument(document) && synchronized(document) { document.getUserData(NOTEBOOK_FILE_ANALYSIS_DONE_KEY) == true }
-
-  fun tryFilterOutSeenNotebookFiles(files: Collection<PsiFile>, document: Document, injectedLanguageManager: InjectedLanguageManager): List<PsiFile> {
-    if (!isLooksLikeNotebookDocument(document)) return files.toList()
-
-    val changeRange = synchronized(document) {
-      document.getUserData(NOTEBOOK_DOCUMENT_IGNORE_ANALYSIS_RANGE)
-    }
-    return if (changeRange == null) files.filter {
-      it.fileType.defaultExtension != notebookInjectedMetaFileExtension
-    }
-    else files.filter {
-      if (!isSuitableKtNotebookFragment(it)) it.fileType.defaultExtension != notebookInjectedMetaFileExtension
-      else {
-        //it.getUserData(ANALYZER_PASS_INJECTED_INFO_HOLDER_KEY) != null
-        val host = injectedLanguageManager.getInjectionHost(it)
-        val hostRange = host?.textRange
-        hostRange?.contains(changeRange) != false || changeRange.contains(hostRange)
-      }
-    }
-  }
-
+    psiFile?.name?.endsWith(notebookInjectedFileExtension) == true
 }
