9b369ffaba61534b363e1ea9d1d2e069713acc45
Ivan Posti <ivan.posti@jetbrains.com>
2022-11-22 17:29:49 +0100

    Update file type icon
    GitOrigin-RevId: 70d7fe814c3632abf82490a2e091230129c96257

--- plugins/mermaid/src/main/kotlin/com/intellij/mermaid/MermaidIcons.kt
+++ plugins/mermaid/src/main/kotlin/com/intellij/mermaid/MermaidIcons.kt
@@ -5,4 +5,7 @@ import com.intellij.openapi.util.IconLoader
 object MermaidIcons {
   @JvmField
   val Plugin = IconLoader.getIcon("/META-INF/pluginIcon.svg", javaClass)
+
+  @JvmField
+  val FileType = IconLoader.getIcon("/icons/MermaidFileType.svg", javaClass)
 }
--- plugins/mermaid/src/main/kotlin/com/intellij/mermaid/lang/MermaidFileType.kt
+++ plugins/mermaid/src/main/kotlin/com/intellij/mermaid/lang/MermaidFileType.kt
@@ -1,6 +1,6 @@
 package com.intellij.mermaid.lang
 
-import com.intellij.icons.AllIcons
+import com.intellij.mermaid.MermaidIcons
 import com.intellij.openapi.fileTypes.LanguageFileType
 import javax.swing.Icon
 
@@ -11,5 +11,5 @@ object MermaidFileType: LanguageFileType(MermaidLanguage) {
 
   override fun getDefaultExtension(): String = "mermaid"
 
-  override fun getIcon(): Icon = AllIcons.FileTypes.Diagram
+  override fun getIcon(): Icon = MermaidIcons.FileType
 }
