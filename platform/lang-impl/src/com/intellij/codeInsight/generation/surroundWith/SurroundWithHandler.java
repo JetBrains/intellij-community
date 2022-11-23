2a0f83ae4f05f695fa98b13d3083cf8621827b5a
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2022-11-23 15:09:30 +0100

    [java] IJ-CR-98530 Improve document commit in surrounder
    GitOrigin-RevId: 10c58ab61d658a2a32c0af15226c9e9396dc468a

--- platform/lang-impl/src/com/intellij/codeInsight/generation/surroundWith/SurroundWithHandler.java
+++ platform/lang-impl/src/com/intellij/codeInsight/generation/surroundWith/SurroundWithHandler.java
@@ -21,6 +21,7 @@ import com.intellij.lang.surroundWith.Surrounder;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.*;
@@ -205,7 +206,7 @@ public class SurroundWithHandler implements CodeInsightActionHandler {
   }
 
   static void doSurround(final Project project, final Editor editor, final Surrounder surrounder, final PsiElement[] elements) {
-    PsiDocumentManager.getInstance(project).commitAllDocuments();
+    WriteAction.run(() -> PsiDocumentManager.getInstance(project).commitDocument(editor.getDocument()));
     int col = editor.getCaretModel().getLogicalPosition().column;
     int line = editor.getCaretModel().getLogicalPosition().line;
     if (!editor.getCaretModel().supportsMultipleCarets()) {
