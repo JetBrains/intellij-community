c95fc229d30176b27720466741cc3be3b9be1a5d
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2022-11-23 23:08:38 +0100

    QD-4415 Wait for "final" gradle data import tasks
    GitOrigin-RevId: 6b1bff15e4004d1d0a2646752b512ef689d9466c

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/ProjectDataManagerImpl.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/ProjectDataManagerImpl.java
@@ -212,10 +212,12 @@ public final class ProjectDataManagerImpl implements ProjectDataManager {
     topic.onFinalTasksStarted(projectPath);
     try {
       ContainerUtil.reverse(tasks).forEach(Runnable::run);
-      topic.onFinalTasksFinished(projectPath);
     }
     catch (Exception e) {
       LOG.warn(e);
+    }
+    finally {
+      topic.onFinalTasksFinished(projectPath);
     }
   }
 
