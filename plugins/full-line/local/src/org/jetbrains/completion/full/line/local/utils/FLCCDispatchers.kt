d1631eabfc7c2e11431e8f0c888f47b4290f7bb7
igor.davidenko <igor.davidenko@jetbrains.com>
2022-11-23 14:15:04 +0100

    [full-line] change flcc models dispatcher to default
    GitOrigin-RevId: f02fc135174414a4a07dd126f1d3d7e93ad55481

--- plugins/full-line/local/src/org/jetbrains/completion/full/line/local/utils/FLCCDispatchers.kt
+++ plugins/full-line/local/src/org/jetbrains/completion/full/line/local/utils/FLCCDispatchers.kt
@@ -1,17 +1,7 @@
 package org.jetbrains.completion.full.line.local.utils
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder
-import kotlinx.coroutines.asCoroutineDispatcher
-import java.util.concurrent.Executors
+import kotlinx.coroutines.Dispatchers
 
 object FLCCDispatchers {
-  val models = Executors.newFixedThreadPool(
-    (Runtime.getRuntime().availableProcessors() - 1)
-      .coerceAtLeast(1),
-    ThreadFactoryBuilder()
-      .setPriority(Thread.NORM_PRIORITY)
-      .setNameFormat("flcc-models-thread-pool-%d")
-      .setDaemon(true)
-      .build()
-  ).asCoroutineDispatcher()
+  val models = Dispatchers.Default
 }
