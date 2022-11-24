55db07e640ba0d5595b60828d6605865da5e1b3c
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2022-11-24 10:33:26 +0100

    [rdct-tests]: add gateway, so it can be launched in distributed framework
    GitOrigin-RevId: 1f7efad7746df622adbcbe97d20770168c61e491

---
+++ platform/remoteDev-util/src/com/intellij/remoteDev/tests/impl/DistributedTestInplaceScheduler.kt
@@ -0,0 +1,17 @@
+package com.intellij.remoteDev.tests.impl
+
+import com.jetbrains.rd.util.reactive.IScheduler
+import org.jetbrains.annotations.ApiStatus
+
+@ApiStatus.Internal
+object DistributedTestInplaceScheduler : IScheduler {
+  override val isActive: Boolean
+    get() = true
+
+  override fun flush() {
+  }
+
+  override fun queue(action: () -> Unit) {
+    action()
+  }
+}
---
+++ platform/remoteDev-util/src/com/intellij/remoteDev/tests/modelGenerated/DistributedTestBridgeModel.Generated.kt
@@ -0,0 +1,87 @@
+@file:Suppress("EXPERIMENTAL_API_USAGE","EXPERIMENTAL_UNSIGNED_LITERALS","PackageDirectoryMismatch","UnusedImport","unused","LocalVariableName","CanBeVal","PropertyName","EnumEntryName","ClassName","ObjectPropertyName","UnnecessaryVariable","SpellCheckingInspection")
+package com.intellij.remoteDev.tests.modelGenerated
+
+import com.jetbrains.rd.framework.*
+import com.jetbrains.rd.framework.base.*
+import com.jetbrains.rd.framework.impl.*
+
+import com.jetbrains.rd.util.lifetime.*
+import com.jetbrains.rd.util.reactive.*
+import com.jetbrains.rd.util.string.*
+import com.jetbrains.rd.util.*
+import kotlin.reflect.KClass
+import kotlin.jvm.JvmStatic
+
+
+
+/**
+ * #### Generated from [DistributedTestBridgeModel.kt]
+ */
+class DistributedTestBridgeModel private constructor(
+    private val _syncCall: RdCall<Unit, Unit>
+) : RdExtBase() {
+    //companion
+    
+    companion object : ISerializersOwner {
+        
+        override fun registerSerializersCore(serializers: ISerializers)  {
+        }
+        
+        
+        @JvmStatic
+        @JvmName("internalCreateModel")
+        @Deprecated("Use create instead", ReplaceWith("create(lifetime, protocol)"))
+        internal fun createModel(lifetime: Lifetime, protocol: IProtocol): DistributedTestBridgeModel  {
+            @Suppress("DEPRECATION")
+            return create(lifetime, protocol)
+        }
+        
+        @JvmStatic
+        @Deprecated("Use protocol.distributedTestBridgeModel or revise the extension scope instead", ReplaceWith("protocol.distributedTestBridgeModel"))
+        fun create(lifetime: Lifetime, protocol: IProtocol): DistributedTestBridgeModel  {
+            TestRoot.register(protocol.serializers)
+            
+            return DistributedTestBridgeModel()
+        }
+        
+        
+        const val serializationHash = -6785668080277295037L
+        
+    }
+    override val serializersOwner: ISerializersOwner get() = DistributedTestBridgeModel
+    override val serializationHash: Long get() = DistributedTestBridgeModel.serializationHash
+    
+    //fields
+    val syncCall: RdCall<Unit, Unit> get() = _syncCall
+    //methods
+    //initializer
+    init {
+        bindableChildren.add("syncCall" to _syncCall)
+    }
+    
+    //secondary constructor
+    private constructor(
+    ) : this(
+        RdCall<Unit, Unit>(FrameworkMarshallers.Void, FrameworkMarshallers.Void)
+    )
+    
+    //equals trait
+    //hash code trait
+    //pretty print
+    override fun print(printer: PrettyPrinter)  {
+        printer.println("DistributedTestBridgeModel (")
+        printer.indent {
+            print("syncCall = "); _syncCall.print(printer); println()
+        }
+        printer.print(")")
+    }
+    //deepClone
+    override fun deepClone(): DistributedTestBridgeModel   {
+        return DistributedTestBridgeModel(
+            _syncCall.deepClonePolymorphic()
+        )
+    }
+    //contexts
+}
+val IProtocol.distributedTestBridgeModel get() = getOrCreateExtension(DistributedTestBridgeModel::class) { @Suppress("DEPRECATION") DistributedTestBridgeModel.create(lifetime, this) }
+
