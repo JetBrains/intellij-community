ea30c1c3a70919a7b6b0c479cdc79471ef154415
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-11-24 12:35:24 +0100

    [kotlin] hide the problem of classloading from module classloader on project import
    The KotlinJsCompilerNotificationImportListener lives in a Project Model 2.0 module and should be loaded with a classloader of that module.
    
    The lambda class which was passed to the ReadAction.nonBlocking.finishOnUiThread was for some reason loaded with a kotlin plugin classloader which broke the check that it should be loaded from the module classloader. After that, the exception was thrown and project import failed.
    
    The current fix just hides the problem to fix the project import. The original problem should be investigated.
    
    (cherry picked from commit 9aa06b80fb7a55c5ca6ba00478d9e9b2b985053d)
    
    GitOrigin-RevId: 8e26257f19c78ce0760bcce2f851b8471d2f1b51

--- plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/notification/KotlinJsCompilerNotificationImportListener.kt
+++ plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/notification/KotlinJsCompilerNotificationImportListener.kt
@@ -5,10 +5,12 @@ import com.intellij.ide.IdeBundle
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.notification.*
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.externalSystem.service.project.manage.ProjectDataImportListener
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Condition
 import com.intellij.util.concurrency.AppExecutorUtil
 import org.jetbrains.kotlin.idea.core.KotlinPluginDisposable
 import org.jetbrains.kotlin.idea.gradle.KotlinIdeaGradleBundle
@@ -19,18 +21,15 @@ const val KOTLIN_JS_COMPILER_SHOULD_BE_NOTIFIED = "notification.kotlin.js.compil
 
 class KotlinJsCompilerNotificationImportListener(private val project: Project) : ProjectDataImportListener {
     override fun onImportFinished(projectPath: String?) {
-        ReadAction.nonBlocking(Callable {
-            PropertiesComponent.getInstance(project).getBoolean(KOTLIN_JS_COMPILER_SHOULD_BE_NOTIFIED, false)
-        })
-            .expireWith(KotlinPluginDisposable.getInstance(project))
-            .finishOnUiThread(ModalityState.defaultModalityState()) { hasJsTarget ->
-                if (!hasJsTarget) return@finishOnUiThread
+        ApplicationManager.getApplication().invokeLater(
+            Runnable {
+                val hasJsTarget = PropertiesComponent.getInstance(project).getBoolean(KOTLIN_JS_COMPILER_SHOULD_BE_NOTIFIED, false)
+                if (!hasJsTarget) return@Runnable
                 PropertiesComponent.getInstance(project).unsetValue(KOTLIN_JS_COMPILER_SHOULD_BE_NOTIFIED)
-                showDeprecatedKotlinJsCompilerWarning(
-                    project,
-                )
-            }
-            .submit(AppExecutorUtil.getAppExecutorService())
+                showDeprecatedKotlinJsCompilerWarning(project)
+            },
+            Condition<Any?> { project.isDisposed || KotlinPluginDisposable.getInstance(project).disposed },
+        )
     }
 }
 
