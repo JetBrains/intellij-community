7e8a81a16823daa4f31cd8acf261bfc0a9786cb0
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2022-11-25 17:37:32 +0100

    IDEA-306797 Unexcluded roots from custom entitites result in exception in RescannedRootsUtil
    (cherry picked from commit 85785673508bb69fb18e9f7996f0556b8f597d51)
    
    IJ-CR-99266
    
    GitOrigin-RevId: 7ac73c986e613eaf975f45b15262cd37bc69aa13

--- platform/lang-impl/src/com/intellij/util/indexing/dependenciesCache/RescannedRootsUtil.java
+++ platform/lang-impl/src/com/intellij/util/indexing/dependenciesCache/RescannedRootsUtil.java
@@ -8,19 +8,28 @@ import com.intellij.openapi.roots.JdkOrderEntry;
 import com.intellij.openapi.roots.LibraryOrderEntry;
 import com.intellij.openapi.roots.OrderEntry;
 import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.roots.impl.CustomEntityProjectModelInfoProvider;
+import com.intellij.openapi.roots.impl.CustomEntityProjectModelInfoProvider.LibraryRoots;
 import com.intellij.openapi.roots.impl.DirectoryInfo;
 import com.intellij.openapi.roots.impl.ProjectFileIndexImpl;
 import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.indexing.roots.IndexableEntityProvider;
 import com.intellij.util.indexing.roots.IndexableEntityProvider.IndexableIteratorBuilder;
 import com.intellij.util.indexing.roots.builders.IndexableIteratorBuilders;
 import com.intellij.util.indexing.roots.builders.SyntheticLibraryIteratorBuilder;
+import com.intellij.workspaceModel.ide.WorkspaceModel;
 import com.intellij.workspaceModel.ide.impl.legacyBridge.library.LibraryEntityUtils;
 import com.intellij.workspaceModel.ide.legacyBridge.ModuleBridge;
+import com.intellij.workspaceModel.storage.EntityStorage;
+import com.intellij.workspaceModel.storage.WorkspaceEntity;
 import com.intellij.workspaceModel.storage.bridgeEntities.LibraryId;
 import com.intellij.workspaceModel.storage.url.VirtualFileUrl;
 import com.intellij.workspaceModel.storage.url.VirtualFileUrlManager;
+import kotlin.sequences.SequencesKt;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -55,6 +64,7 @@ class RescannedRootsUtil {
       VirtualFile excluded = iterator.next();
       DirectoryInfo info = fileIndex.getInfoForFileOrDirectory(excluded);
       if (!info.isInProject(excluded)) {
+        iterator.remove();
         continue;
       }
       Module module = info.getModule();
@@ -62,6 +72,7 @@ class RescannedRootsUtil {
         VirtualFileUrl url = urlManager.fromUrl(excluded.getUrl());
         result.addAll(IndexableIteratorBuilders.INSTANCE.forModuleRoots(((ModuleBridge)module).getModuleEntityId(),
                                                                         Collections.singletonList(url)));
+        iterator.remove();
         continue;
       }
 
@@ -87,14 +98,62 @@ class RescannedRootsUtil {
       SyntheticLibraryIteratorBuilder builder = createSyntheticLibraryIterator(librariesDescriptorsAfter, excluded);
       if (builder != null) {
         result.add(builder);
+        iterator.remove();
         continue;
       }
 
-      if (!found) {
-        throw new IllegalStateException("Root " + excluded + " was not found.");
+      if (found) {
+        iterator.remove();
       }
     }
+
+    if (excludedRoots.isEmpty()) {
+      return result;
+    }
+
+    EntityStorage current = WorkspaceModel.getInstance(project).getEntityStorage().getCurrent();
+    for (CustomEntityProjectModelInfoProvider<?> provider : CustomEntityProjectModelInfoProvider.EP.getExtensionList()) {
+      for (LibraryRoots<? extends WorkspaceEntity> roots : getRoots(provider, current)) {
+        Iterator<VirtualFile> rootsIterator = excludedRoots.iterator();
+        while (rootsIterator.hasNext()) {
+          VirtualFile next = rootsIterator.next();
+          if (VfsUtilCore.isUnderFiles(next, roots.sources) || VfsUtilCore.isUnderFiles(next, roots.classes)) {
+            if (!VfsUtilCore.isUnderFiles(next, roots.excluded)) {
+              Collection<? extends IndexableIteratorBuilder> builders = createCustomEntityBuilder(roots.generativeEntity, project);
+              if (!builders.isEmpty()) {
+                result.addAll(builders);
+                rootsIterator.remove();
+              }
+            }
+          }
+        }
+        if (excludedRoots.isEmpty()) {
+          break;
+        }
+      }
+    }
+
+    if (!excludedRoots.isEmpty()) {
+      throw new IllegalStateException("Roots were not found: " + StringUtil.join(excludedRoots, "\n"));
+    }
     return result;
+  }
+
+  @NotNull
+  private static <E extends WorkspaceEntity> Collection<? extends IndexableIteratorBuilder> createCustomEntityBuilder(E entity,
+                                                                                                                      Project project) {
+    for (IndexableEntityProvider<? extends WorkspaceEntity> provider : IndexableEntityProvider.EP_NAME.getExtensionList()) {
+      if (provider instanceof IndexableEntityProvider.Existing && provider.getEntityClass().equals(entity.getEntityInterface())) {
+        //noinspection unchecked
+        return ((IndexableEntityProvider.Existing<E>)provider).getExistingEntityIteratorBuilder(entity, project);
+      }
+    }
+    return Collections.emptyList();
+  }
+
+  private static <E extends WorkspaceEntity> Iterable<LibraryRoots<E>> getRoots(CustomEntityProjectModelInfoProvider<E> provider,
+                                                                                EntityStorage storage) {
+    return SequencesKt.asIterable(provider.getLibraryRoots(storage.entities(provider.getEntityClass()), storage));
   }
 
   private static SyntheticLibraryIteratorBuilder createSyntheticLibraryIterator(List<? extends SyntheticLibraryDescriptor> librariesDescriptorsAfter,
