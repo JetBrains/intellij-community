473c8243e52c5723485d2d5b7f785c23ea975893
Semyon Proshev <semyon.proshev@jetbrains.com>
2022-11-28 16:21:53 +0100

    Remove code that does nothing after f630c748
    GitOrigin-RevId: f76333cf82a27f1df07b7cc637fe99ea123980bc

--- python/testSrc/com/jetbrains/python/sdk/PySdkPathsTest.kt
+++ python/testSrc/com/jetbrains/python/sdk/PySdkPathsTest.kt
@@ -139,7 +139,6 @@ class PySdkPathsTest {
 
     mockPythonPluginDisposable()
     runWriteActionAndWait { sdk.getOrCreateAdditionalData() }
-    runWriteActionAndWait { ProjectJdkTable.getInstance().addJdk(sdk, projectModel.disposableRule.disposable) }
 
     val editableSdk = PyConfigurableInterpreterList.getInstance(projectModel.project).model.findSdk(sdk.name)
     editableSdk!!.putUserData(PythonSdkType.MOCK_PY_VERSION_KEY, pythonVersion)
@@ -189,7 +188,6 @@ class PySdkPathsTest {
     )
       .also { module.pythonSdk = it }
     sdk.putUserData(PythonSdkType.MOCK_PY_VERSION_KEY, pythonVersion)
-    runWriteActionAndWait { ProjectJdkTable.getInstance().addJdk(sdk, projectModel.disposableRule.disposable) }
 
     val editableSdk = PyConfigurableInterpreterList.getInstance(projectModel.project).model.findSdk(sdk.name)
     editableSdk!!.putUserData(PythonSdkType.MOCK_PY_VERSION_KEY, pythonVersion)
