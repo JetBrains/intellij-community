b62d3f820ad7d7c59db4b2c5df30a48f1525d272
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2022-11-29 12:46:38 +0100

    [kt, tests] Introduce KotlinBeforeAfterTestRuleWithDescription
    (cherry picked from commit 90cef26358be50addce3432a45a55f82eecfbfcc)
    
    GitOrigin-RevId: 8372049a0a24b5f3acbbe8c7e1ec22340ba0b176

---
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/newTests/testServices/KotlinBeforeAfterTestRuleWithDescription.kt
@@ -0,0 +1,32 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.gradle.newTests.testServices
+
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+/**
+ * Similar to [org.junit.rules.ExternalResource], but additionally passes test description to
+ * [before] and [after] methods.
+ */
+interface KotlinBeforeAfterTestRuleWithDescription : TestRule {
+    override fun apply(base: Statement, description: Description): Statement =
+        statement(base, description)
+
+    private fun statement(base: Statement, description: Description): Statement =
+        object : Statement() {
+            override fun evaluate() {
+                before(description)
+                try {
+                    base.evaluate()
+                }
+                finally {
+                    after(description)
+                }
+            }
+        }
+
+
+    fun before(description: Description): Unit = Unit
+    fun after(description: Description): Unit = Unit
+}
