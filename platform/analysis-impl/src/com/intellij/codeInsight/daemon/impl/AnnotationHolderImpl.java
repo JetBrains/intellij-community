1a9636b99c3c63655fc2b07cbd25689308df1662
Alexey Kudravtsev <cdr@intellij.com>
2022-11-29 16:13:15 +0100

    bring back warnings about incorrect annotator API usage (except perf tests where they may interfere), to make them actionable (https://jetbrains.team/p/ij/reviews/98874/ followup)
    GitOrigin-RevId: 234912e46adc970f842d5651b49a4172b41527fa

--- platform/analysis-impl/src/com/intellij/codeInsight/daemon/impl/AnnotationHolderImpl.java
+++ platform/analysis-impl/src/com/intellij/codeInsight/daemon/impl/AnnotationHolderImpl.java
@@ -5,6 +5,8 @@ package com.intellij.codeInsight.daemon.impl;
 import com.intellij.diagnostic.PluginException;
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.annotation.*;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ex.ApplicationManagerEx;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.NlsContexts;
@@ -19,6 +21,9 @@ import com.intellij.util.SmartList;
 import com.intellij.xml.util.XmlStringUtil;
 import org.jetbrains.annotations.*;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -185,21 +190,28 @@ public class AnnotationHolderImpl extends SmartList<Annotation> implements Annot
                                         @NotNull String methodName) {
     Annotation annotation = new Annotation(range.getStartOffset(), range.getEndOffset(), severity, message, tooltip);
     add(annotation);
-
-    if (!isClionAnnotator(callerClass)) { //todo temporary fix. CLion guys promised to fix their annotator eventually
-      String callerInfo = callerClass == null ? "" : " (the call to which was found in " + callerClass + ")";
-      PluginException pluginException = PluginException.createByClass(
-        new IncorrectOperationException("'AnnotationHolder." + methodName + "()' method" + callerInfo + " is slow, non-incremental " +
-                                        "and thus can cause unexpected behaviour (e.g. annoying blinking), " +
-                                        "is deprecated and will be removed soon. " +
-                                        "Please use `newAnnotation(...).create()` instead"), callerClass == null ? getClass() : callerClass);
+    String callerInfo = callerClass == null ? "" : " (the call to which was found in "+callerClass+")";
+    PluginException pluginException = PluginException.createByClass(
+      new IncorrectOperationException("'AnnotationHolder."+methodName + "()' method" + callerInfo + " is slow, non-incremental " +
+                                      "and thus can cause unexpected behaviour (e.g. annoying blinking), " +
+                                      "is deprecated and will be removed soon. " +
+                                      "Please use `newAnnotation(...).create()` instead"), callerClass == null ? getClass() : callerClass);
+    if ("com.jetbrains.cidr.lang.daemon.OCAnnotator".equals(callerClass == null ? null : callerClass.getName())) {
+      //todo temporary fix. CLion guys promised to fix their annotator eventually
+      //LOG.warnInProduction(pluginException);
+      if (ApplicationManager.getApplication().isInternal() || ApplicationManager.getApplication().isUnitTestMode()) {
+        Period p = Period.between(LocalDate.of(2020, Month.APRIL, 27), LocalDate.now());
+        String f = String.format("CLion developers promised to fix their annotator %d centuries %d years %d months %d days ago", p.getYears() / 100, p.getYears() % 100, p.getMonths(), p.getDays());
+        LOG.warn(f, pluginException);
+      }
+      else if (!ApplicationManagerEx.isInStressTest()) {
+        LOG.warn(pluginException);
+      }
+    }
+    else {
       LOG.warnInProduction(pluginException);
     }
     return annotation;
-  }
-
-  private static boolean isClionAnnotator(@Nullable Class<?> callerClass) {
-    return callerClass != null && "com.jetbrains.cidr.lang.daemon.OCAnnotator".equals(callerClass.getName());
   }
 
   public boolean hasAnnotations() {
