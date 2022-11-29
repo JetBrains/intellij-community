8acdd7319b03710fb6a8be984a52c3c7fa2328df
Roman Ivanov <roman.ivanov@jetbrains.com>
2022-11-29 12:04:08 +0100

    [inlays] add proper names for action and hide it when not enabled
    GitOrigin-RevId: efb9e0f8c6de5f36a2190eb64509d6288075c267

--- platform/lang-impl/src/com/intellij/codeInsight/hints/declarative/impl/DisableDeclarativeInlayAction.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/hints/declarative/impl/DisableDeclarativeInlayAction.kt
@@ -9,13 +9,20 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 
-internal class DisableDeclarativeInlayAction : AnAction() {
+internal class DisableDeclarativeInlayAction : AnAction(CodeInsightBundle.message("inlay.hints.declarative.disable.action.no.provider.text")) {
   override fun getActionUpdateThread(): ActionUpdateThread {
     return ActionUpdateThread.BGT
   }
 
   override fun update(e: AnActionEvent) {
-    val providerName = e.dataContext.getData(InlayHintsProvider.PROVIDER_NAME) ?: return
+    val providerName = e.dataContext.getData(InlayHintsProvider.PROVIDER_NAME)
+    if (providerName == null) {
+      e.presentation.isEnabledAndVisible = false
+      e.presentation.text = CodeInsightBundle.message("inlay.hints.declarative.disable.action.no.provider.text")
+      return
+    } else {
+      e.presentation.isEnabledAndVisible = true
+    }
     e.presentation.text = CodeInsightBundle.message("inlay.hints.declarative.disable.action.text", providerName)
   }
 
