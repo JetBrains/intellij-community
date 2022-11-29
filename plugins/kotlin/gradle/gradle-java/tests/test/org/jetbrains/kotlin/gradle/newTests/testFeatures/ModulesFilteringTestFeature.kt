725b6b53a8c2b6afb957241d4091856a1bfc478f
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2022-11-29 17:50:01 +0100

    [kt, tests] Support filtering of modules
    Use ModulesFilteringConfiguration to decide which fields should be printed
    
    GitOrigin-RevId: 295f7bdcfe18bad9ae57530bc31524a0465b8737

---
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/newTests/testFeatures/ModulesFilteringTestFeature.kt
@@ -0,0 +1,47 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.gradle.newTests.testFeatures
+
+import org.jetbrains.kotlin.gradle.newTests.TestConfigurationDslScope
+import org.jetbrains.kotlin.gradle.newTests.TestFeature
+import org.jetbrains.kotlin.gradle.newTests.writeAccess
+
+object FilterModulesTestFeature : TestFeature<ModulesFilteringConfiguration> {
+    override fun renderConfiguration(configuration: ModulesFilteringConfiguration): List<String> = buildList {
+        if (configuration.excludedModuleNames != null) add("hiding source modules: ${configuration.excludedModuleNames!!}")
+        if (configuration.includedModuleNames != null) add("showing only source modules: ${configuration.includedModuleNames!!}")
+
+        if (configuration.hideTestModules) add("hiding test source modules")
+        if (configuration.hideProductionModules) add("hiding production source modules")
+    }
+
+    override fun createDefaultConfiguration(): ModulesFilteringConfiguration = ModulesFilteringConfiguration()
+}
+
+class ModulesFilteringConfiguration {
+    // includedModuleNames == null -> use default (include everything), but
+    // includedModuleNames == emptySet() -> don't include anything (hide all modules)
+    //
+    // those two modes are mutually exclusive: if one isn't null, then other must be null (ensured in [FilterModulesSupport])
+    var excludedModuleNames: Set<String>? = null
+    var includedModuleNames: Set<String>? = null
+
+    var hideTestModules: Boolean = false
+    var hideProductionModules: Boolean = false
+}
+
+private val TestConfigurationDslScope.config: ModulesFilteringConfiguration
+    get() = writeAccess.getConfiguration(FilterModulesTestFeature)
+
+interface ModulesFilteringDsl {
+    fun TestConfigurationDslScope.onlyModules(vararg names: String) {
+        val config = config
+        require(config.excludedModuleNames == null) { "'onlyModules' is mutually exclusive with 'exceptModules'" }
+        config.includedModuleNames = names.toSet()
+    }
+
+    fun TestConfigurationDslScope.exceptModules(vararg names: String) {
+        val config = config
+        require(config.includedModuleNames == null) { "'onlyModules' is mutually exclusive with 'exceptModules'" }
+        config.excludedModuleNames = names.toSet()
+    }
+}
