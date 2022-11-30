420fa8d79db5d8d9244275623db35a8f8c2b13b2
Vladislav.Yaroshchuk <vladislav.yaroshchuk@jetbrains.com>
2022-11-30 13:19:59 +0400

    IDEA-269182 Create new internal actions for JAR repository library
    Hidden and disabled by default, enable via `intellij.yaml`
    
    GitOrigin-RevId: 7b0f567544e01cddd418f5e226d89f5132625796

---
+++ java/idea-ui/src/com/intellij/ide/actions/ReloadAllRepositoryLibrariesAction.kt
@@ -0,0 +1,22 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide.actions
+
+import com.intellij.jarRepository.RepositoryLibrarySettings
+import com.intellij.jarRepository.RepositoryLibraryUtils
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class ReloadAllRepositoryLibrariesAction : AnAction() {
+  override fun getActionUpdateThread() = ActionUpdateThread.BGT
+
+  override fun update(e: AnActionEvent) {
+    val project = e.project ?: return
+    e.presentation.isEnabledAndVisible = RepositoryLibrarySettings.getInstanceOrDefaults(project).isJarRepositoryBindingFeatureEnabled()
+  }
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project ?: return
+    RepositoryLibraryUtils.getInstance(project).reloadAllRepositoryLibrariesBackground()
+  }
+}
