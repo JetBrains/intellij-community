b22c623725a187d1d49e27556318b2c7a17c784e
anna.gromova <anna.gromova@jetbrains.com>
2022-11-30 18:06:11 +0100

    RIDER-84137 add rollback to previous toolbar
    GitOrigin-RevId: 24c4b6a21b20f22c3200d83700a8357f548a8612

--- platform/execution-impl/src/com/intellij/execution/runToolbar/RunToolbarRollbackToPrevious.kt
+++ platform/execution-impl/src/com/intellij/execution/runToolbar/RunToolbarRollbackToPrevious.kt
@@ -1,12 +1,17 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.runToolbar
 
+import com.intellij.application.subscribe
 import com.intellij.ide.ui.ToolbarSettings
 import com.intellij.ide.ui.UISettings.Companion.getInstance
+import com.intellij.ide.ui.UISettingsListener
 import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.util.Disposer
 
 /**
  * Used for Rider
@@ -27,15 +32,48 @@ class RunToolbarRollbackToPrevious : DumbAwareAction() {
 
       if (isFirstSessionForNewUser) {
         properties.setValue(ROLLBACK_AVAILABILITY, false)
-      } else {
+      }
+      else {
         properties.setValue(ROLLBACK_AVAILABILITY, true)
         properties.setValue(INCLUSION_NAVBAR_STATE, getInstance().showNavigationBar)
         properties.setValue(INCLUSION_TOOLBAR_STATE, getInstance().showMainToolbar)
       }
     }
+
+    private var hideListenerDisposable: Disposable? = null
+
+    /**
+     * Used for Rider
+     */
+
+    fun addHideActionHelper() {
+      val properties = PropertiesComponent.getInstance()
+
+      if(properties.getBoolean(ROLLBACK_AVAILABILITY)) {
+        if(ToolbarSettings.getInstance().isVisible) {
+          val disposable = Disposer.newCheckedDisposable()
+          Disposer.register(ApplicationManager.getApplication(), disposable)
+          hideListenerDisposable = disposable
+
+          UISettingsListener.TOPIC.subscribe(disposable, UISettingsListener {
+            checkState()
+          })
+        } else {
+          checkState()
+        }
+      }
+    }
+
+    private fun checkState() {
+      val properties = PropertiesComponent.getInstance()
+      if(!ToolbarSettings.getInstance().isVisible) {
+        properties.setValue(ROLLBACK_AVAILABILITY, false)
+        hideListenerDisposable?.dispose()
+      }
+    }
   }
 
-  override fun getActionUpdateThread() = ActionUpdateThread.EDT
+  override fun getActionUpdateThread() = ActionUpdateThread.BGT
 
   override fun update(e: AnActionEvent) {
     val properties = PropertiesComponent.getInstance()
@@ -52,6 +90,6 @@ class RunToolbarRollbackToPrevious : DumbAwareAction() {
     uiSettings.showMainToolbar = properties.getBoolean(INCLUSION_TOOLBAR_STATE)
     uiSettings.fireUISettingsChanged()
 
-    properties.setValue(ROLLBACK_AVAILABILITY, false)
+    checkState()
   }
 }
