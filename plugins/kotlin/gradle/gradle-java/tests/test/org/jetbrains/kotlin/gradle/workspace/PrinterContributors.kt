e976f9ea3e7b75f5f60497d4057562624f05b04e
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2022-11-30 13:52:26 +0100

    [kt, tests] Refactor WorkspacePrinting (8/8)
    This series of commits prepares Workspace-infrastructure for further
    changes **without** changing the behaviour.
    
    This commit extracts PrinterContributors into separate files
    
    GitOrigin-RevId: 00787f1030de2b16da84ff09fa3e6e148ff3e0bc

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/workspace/PrinterContributors.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/workspace/PrinterContributors.kt
@@ -2,11 +2,7 @@
 
 package org.jetbrains.kotlin.gradle.workspace
 
-import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.module.Module
-import com.intellij.openapi.roots.ModuleRootManager
-import com.intellij.openapi.roots.OrderEntry
-import org.jetbrains.kotlin.config.KotlinFacetSettingsProvider
 
 interface ModulePrinterContributor {
     fun PrinterContext.process(module: Module)
@@ -14,44 +10,4 @@ interface ModulePrinterContributor {
 
 class NoopModulePrinterContributor : ModulePrinterContributor {
     override fun PrinterContext.process(module: Module) = Unit
-}
-
-class KotlinFacetSettingsPrinterContributor : ModulePrinterContributor {
-    override fun PrinterContext.process(module: Module) = with(printer) {
-        val facetSettings = runReadAction { KotlinFacetSettingsProvider.getInstance(module.project)
-            ?.getSettings(module) }
-            ?: return
-
-        indented {
-            println("Settings from the Kotlin facet:")
-            indented {
-                println("External project ID: ${facetSettings.externalProjectId}")
-                println("Language level: ${facetSettings.languageLevel}")
-                println("API level: ${facetSettings.apiLevel}")
-                println("MPP version: ${facetSettings.mppVersion?.name.orEmpty()}")
-                println("dependsOn module names:")
-                indented { facetSettings.dependsOnModuleNames.sorted().forEach(printer::println) }
-                println("Additional visible module names:")
-                indented { facetSettings.additionalVisibleModuleNames.sorted().forEach(printer::println) }
-                println("Additional compiler arguments: ${facetSettings.compilerSettings?.additionalArguments.orEmpty()}")
-            }
-        }
-    }
-}
-
-class SanitizingOrderEntryPrinterContributor : ModulePrinterContributor {
-    override fun PrinterContext.process(module: Module) = with(printer) {
-        val orderEntries = runReadAction { ModuleRootManager.getInstance(module).orderEntries }
-            .map(OrderEntry::toPrinterEntity)
-        val sanitizedEntries = replaceNativeDistributionOrderEntries(orderEntries)
-
-        if (sanitizedEntries.isEmpty()) return
-
-        println("Order entries:")
-        indented {
-            for (entry in sanitizedEntries) {
-                println(entry.presentableName)
-            }
-        }
-    }
 }
---
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/workspace/SanitizingOrderEntryPrinterContributor.kt
@@ -0,0 +1,24 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.gradle.workspace
+
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.roots.OrderEntry
+
+class SanitizingOrderEntryPrinterContributor : ModulePrinterContributor {
+    override fun PrinterContext.process(module: Module) = with(printer) {
+        val orderEntries = runReadAction { ModuleRootManager.getInstance(module).orderEntries }
+            .map(OrderEntry::toPrinterEntity)
+        val sanitizedEntries = replaceNativeDistributionOrderEntries(orderEntries)
+
+        if (sanitizedEntries.isEmpty()) return
+
+        println("Order entries:")
+        indented {
+            for (entry in sanitizedEntries) {
+                println(entry.presentableName)
+            }
+        }
+    }
+}
