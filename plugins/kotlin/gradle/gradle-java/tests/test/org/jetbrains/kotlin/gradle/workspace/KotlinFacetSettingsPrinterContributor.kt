05e9c9e768883d97de0fc26b9b5d11635dd2f91a
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2022-11-30 13:52:26 +0100

    [kt, tests] Refactor WorkspacePrinting (8/8)
    This series of commits prepares Workspace-infrastructure for further
    changes **without** changing the behaviour.
    
    This commit extracts PrinterContributors into separate files
    
    (cherry picked from commit 00787f1030de2b16da84ff09fa3e6e148ff3e0bc)
    
    GitOrigin-RevId: fc34c548bac3e151f2b076e5042678c0f9687fa7

---
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/workspace/KotlinFacetSettingsPrinterContributor.kt
@@ -0,0 +1,31 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.gradle.workspace
+
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.module.Module
+import org.jetbrains.kotlin.config.KotlinFacetSettingsProvider
+import org.jetbrains.kotlin.idea.base.facet.platform.platform
+import java.io.File
+
+class KotlinFacetSettingsPrinterContributor : ModulePrinterContributor {
+    override fun PrinterContext.process(module: Module) = with(printer) {
+        val facetSettings = runReadAction { KotlinFacetSettingsProvider.getInstance(module.project)
+            ?.getSettings(module) }
+            ?: return
+
+        indented {
+            println("Settings from the Kotlin facet:")
+            indented {
+                println("External project ID: ${facetSettings.externalProjectId}")
+                println("Language level: ${facetSettings.languageLevel}")
+                println("API level: ${facetSettings.apiLevel}")
+                println("MPP version: ${facetSettings.mppVersion?.name.orEmpty()}")
+                println("dependsOn module names:")
+                indented { facetSettings.dependsOnModuleNames.sorted().forEach(printer::println) }
+                println("Additional visible module names:")
+                indented { facetSettings.additionalVisibleModuleNames.sorted().forEach(printer::println) }
+                println("Additional compiler arguments: ${facetSettings.compilerSettings?.additionalArguments.orEmpty()}")
+            }
+        }
+    }
+}
