13cd779ade1a6941e9b3662d12c0c40dc21e4f9b
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-12-01 10:25:33 +0100

    [KTIJ-23781] Support custom names for native distribution libraries
    (cherry picked from commit 712b09b811e926927a2680b9bd66f6227317685a)
    
    GitOrigin-RevId: 1e5375625f105fd0a1227f4eaa60d8c937dc4f47

--- plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/serialize/KotlinExtrasSerializationService.kt
+++ plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/serialize/KotlinExtrasSerializationService.kt
@@ -1,8 +1,13 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.gradleJava.configuration.serialize
 
+import org.jetbrains.kotlin.gradle.idea.serialize.IdeaKotlinBooleanExtrasSerializer
 import org.jetbrains.kotlin.gradle.idea.serialize.IdeaKotlinExtrasSerializationExtensionBuilder
 import org.jetbrains.kotlin.gradle.idea.serialize.IdeaKotlinExtrasSerializer
+import org.jetbrains.kotlin.gradle.idea.tcs.extras.KlibExtra
+import org.jetbrains.kotlin.gradle.idea.tcs.extras.isIdeaProjectLevelKey
+import org.jetbrains.kotlin.gradle.idea.tcs.extras.isNativeDistributionKey
+import org.jetbrains.kotlin.gradle.idea.tcs.extras.isNativeStdlibKey
 import org.jetbrains.kotlin.idea.gradle.configuration.serialize.KotlinExtrasSerializationService
 import org.jetbrains.kotlin.tooling.core.extrasKeyOf
 
@@ -14,5 +19,9 @@ class KotlinExtrasSerializationService :
     override fun IdeaKotlinExtrasSerializationExtensionBuilder.extensions() {
         /* For adding debug Strings to entities */
         register(extrasKeyOf("kotlin.debug"), IdeaKotlinExtrasSerializer.javaIoSerializable())
+        register(KlibExtra.key, IdeaKotlinExtrasSerializer.javaIoSerializable())
+        register(isIdeaProjectLevelKey, IdeaKotlinBooleanExtrasSerializer)
+        register(isNativeDistributionKey, IdeaKotlinBooleanExtrasSerializer)
+        register(isNativeStdlibKey, IdeaKotlinBooleanExtrasSerializer)
     }
 }
