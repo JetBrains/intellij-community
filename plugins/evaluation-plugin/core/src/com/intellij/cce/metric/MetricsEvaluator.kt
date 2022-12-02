f5ebd35dd0337350566b7263d6d61a70d12e30b0
Kirill.Krylov <kirill.krylov@jetbrains.com>
2022-12-02 03:15:19 +0100

    [evaluation-plugin] split evaluation metrics
    GitOrigin-RevId: e2ea6ecbf11857dd20276117d10455b1f48354ea

--- plugins/evaluation-plugin/core/src/com/intellij/cce/metric/MetricsEvaluator.kt
+++ plugins/evaluation-plugin/core/src/com/intellij/cce/metric/MetricsEvaluator.kt
@@ -7,25 +7,20 @@ class MetricsEvaluator private constructor(private val evaluationType: String) {
   companion object {
     fun withDefaultMetrics(evaluationType: String, strategy: CompletionStrategy): MetricsEvaluator {
       val evaluator = MetricsEvaluator(evaluationType)
-      evaluator.registerDefaultMetrics(strategy)
+
+      if (strategy.completionGolf) {
+        evaluator.registerCompletionGolfMetrics()
+      }
+      else {
+        evaluator.registerDefaultMetrics()
+      }
       return evaluator
     }
   }
 
   private val metrics = mutableListOf<Metric>()
 
-  fun registerDefaultMetrics(strategy: CompletionStrategy) {
-    if (strategy.completionGolf) {
-      registerMetric(CompletionGolfMovesSumMetric())
-      registerMetric(CompletionGolfMovesCountNormalised())
-      registerMetric(CompletionGolfPerfectLine())
-      registerMetric(MeanLatencyMetric())
-      registerMetric(MaxLatencyMetric())
-      registerMetric(SessionsCountMetric())
-
-      return
-    }
-
+  fun registerDefaultMetrics() {
     registerMetric(FoundAtMetric(1))
     registerMetric(FoundAtMetric(5))
     registerMetric(RecallMetric())
@@ -35,9 +30,16 @@ class MetricsEvaluator private constructor(private val evaluationType: String) {
     registerMetric(SessionsCountMetric())
   }
 
-  private fun registerMetric(metric: Metric) {
-    metrics.add(metric)
+  fun registerCompletionGolfMetrics() {
+    registerMetric(CompletionGolfMovesSumMetric())
+    registerMetric(CompletionGolfMovesCountNormalised())
+    registerMetric(CompletionGolfPerfectLine())
+    registerMetric(MeanLatencyMetric())
+    registerMetric(MaxLatencyMetric())
+    registerMetric(SessionsCountMetric())
   }
+
+  private fun registerMetric(metric: Metric) = metrics.add(metric)
 
   fun evaluate(sessions: List<Session>, comparator: SuggestionsComparator = SuggestionsComparator.DEFAULT): List<MetricInfo> {
     return metrics.map { MetricInfo(it.name, it.evaluate(sessions, comparator).toDouble(), evaluationType, it.valueType) }
