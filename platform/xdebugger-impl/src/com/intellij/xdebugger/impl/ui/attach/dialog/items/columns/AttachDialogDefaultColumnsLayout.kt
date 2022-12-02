c8f9db40c40a9c87b07734df8763259ac5ea9500
Dmitry.Neverov <dmitry.neverov@jetbrains.com>
2022-12-02 10:49:13 +0100

    Show process' user in the attach to process dialog
    (cherry picked from commit 6ed7f3d7f3a8df3b896a7085eb48415c0b49b069)
    
    IJ-CR-99582
    
    GitOrigin-RevId: 0b5e1f836e3eeb1b518b3fc28b0c661179913e78

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/attach/dialog/items/columns/AttachDialogDefaultColumnsLayout.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/attach/dialog/items/columns/AttachDialogDefaultColumnsLayout.kt
@@ -11,12 +11,15 @@ class AttachDialogDefaultColumnsLayout : AttachDialogColumnsLayout() {
   companion object {
     const val EXECUTABLE_CELL_KEY = "AttachDialogExecutable"
     const val PID_CELL_KEY = "AttachDialogProcessId"
+    const val USER_CELL_KEY = "AttachDialogProcessOwner"
     const val DEBUGGERS_CELL_KEY = "AttachDialogDebuggers"
     const val COMMAND_LINE_CELL_KEY = "AttachDialogCommandLine"
 
     private val columnInfos = listOf(
       AttachDialogColumnInfo(EXECUTABLE_CELL_KEY, ExecutableCell::class.java, XDebuggerBundle.message("xdebugger.attach.executable.column.name"), JBUI.scale(250)),
       AttachDialogColumnInfo(PID_CELL_KEY, PidCell::class.java, XDebuggerBundle.message("xdebugger.attach.pid.column.name"), JBUI.scale(70)),
+      AttachDialogColumnInfo(USER_CELL_KEY, UserCell::class.java, XDebuggerBundle.message("xdebugger.attach.user.column.name"),
+                             JBUI.scale(70)),
       AttachDialogColumnInfo(DEBUGGERS_CELL_KEY, DebuggersCell::class.java, XDebuggerBundle.message("xdebugger.attach.debuggers.column.name"), JBUI.scale(130)),
       AttachDialogColumnInfo(COMMAND_LINE_CELL_KEY, CommandLineCell::class.java, XDebuggerBundle.message("xdebugger.attach.command.line.column.name"), JBUI.scale(250)),
     )
@@ -32,8 +35,9 @@ class AttachDialogDefaultColumnsLayout : AttachDialogColumnsLayout() {
     return when (columnIndex) {
       0 -> ExecutableCell(node, filters, this, !isInsideTree)
       1 -> PidCell(node, filters, this)
-      2 -> DebuggersCell(node, filters, this)
-      3 -> CommandLineCell(node, filters, this)
+      2 -> UserCell(node, filters, this)
+      3 -> DebuggersCell(node, filters, this)
+      4 -> CommandLineCell(node, filters, this)
       else -> throw IllegalStateException("Unexpected column number: $columnIndex")
     }
   }
