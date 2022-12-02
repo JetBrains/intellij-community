d0094570782dd54cb8b27add4b40e587985d15f6
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2022-12-02 21:05:41 +0100

    KTNB: Add initial notebook dependencies to the permanent index
    ^KTNB-108 Fixed
    
    GitOrigin-RevId: 698bfd814d0c61c4e64d25d951e32f29d9107999

--- platform/platform-impl/src/com/intellij/ui/EditorNotificationsImpl.kt
+++ platform/platform-impl/src/com/intellij/ui/EditorNotificationsImpl.kt
@@ -177,7 +177,9 @@ class EditorNotificationsImpl(private val project: Project) : EditorNotification
       // delay for debounce
       delay(100)
 
-      if (!file.isValid) {
+      // Please don't remove this readAction {} here, it's needed for checking of validity of injected files,
+      // and many unpleasant exceptions appear in case if validity check is not wrapped.
+      if (!readAction { file.isValid }) {
         return@launch
       }
 
