ff4db3c6a29d094decdae99c0b9dc33d75e89c7f
Aleksei Kniazev <aleksei.kniazev@jetbrains.com>
2022-12-02 16:36:36 +0100

    [python] fixed python packages cache file uncontrollable growing (PY-57156)
    - minimizes calls to service in comparator
    - cache fields are marked as volatile
    - convert TreeSet to List before persisting with gson
    
    (cherry picked from commit 7962bcbd91c4d619f9922668551cf9fd88c292a8)
    
    IJ-MR-99615
    
    GitOrigin-RevId: 85f91d6347e30de5014042904623b50058ede266

--- python/src/com/jetbrains/python/packaging/pip/PypiPackageCache.kt
+++ python/src/com/jetbrains/python/packaging/pip/PypiPackageCache.kt
@@ -2,8 +2,7 @@
 package com.jetbrains.python.packaging.pip
 
 import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.google.gson.InstanceCreator
+import com.google.gson.JsonSyntaxException
 import com.google.gson.reflect.TypeToken
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.components.Service
@@ -11,11 +10,12 @@ import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.util.io.exists
 import com.jetbrains.python.packaging.PyPIPackageUtil
 import com.jetbrains.python.packaging.cache.PythonPackageCache
-import com.jetbrains.python.packaging.common.RANKING_AWARE_PACKAGE_NAME_COMPARATOR
+import com.jetbrains.python.packaging.common.PythonRankingAwarePackageNameComparator
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import org.jetbrains.annotations.ApiStatus
-import java.lang.reflect.Type
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Path
@@ -23,72 +23,81 @@ import java.nio.file.Paths
 import java.time.Duration
 import java.time.Instant
 import java.util.*
+import kotlin.io.path.deleteIfExists
 
 @ApiStatus.Experimental
 @Service
 class PypiPackageCache : PythonPackageCache<String> {
-  private val gson: Gson = GsonBuilder()
-    .registerTypeAdapter(object : TypeToken<TreeSet<String>>() {}.type,
-                         object : InstanceCreator<TreeSet<String>> {
-                           override fun createInstance(type: Type?): TreeSet<String> {
-                             return TreeSet<String>(RANKING_AWARE_PACKAGE_NAME_COMPARATOR)
-                           }
-                         })
-    .create()
+  private val gson: Gson = Gson()
+  private val mutex = Mutex()
 
   override val packages: List<String>
     get() = cache.toList()
 
-  private var cache: TreeSet<String> = TreeSet(RANKING_AWARE_PACKAGE_NAME_COMPARATOR)
+  @Volatile
+  private var cache: TreeSet<String> = TreeSet(PythonRankingAwarePackageNameComparator())
 
   val filePath: Path
     get() = Paths.get(PathManager.getSystemPath(), "python_packages", "packages_v2.json")
 
   suspend fun loadFromFile() {
     withContext(Dispatchers.IO) {
-      val type = object : TypeToken<TreeSet<String>>() {}.type
-      val newCache = Files.newBufferedReader(filePath, StandardCharsets.UTF_8).use { reader ->
-        val newCache: TreeSet<String> = gson.fromJson(reader, type)
-        newCache
+      val type = object : TypeToken<List<String>>() {}.type
+      val packageList = mutex.withLock {
+        Files.newBufferedReader(filePath, StandardCharsets.UTF_8)
+          .use { gson.fromJson<List<String>>(it, type) }
       }
-      withContext(Dispatchers.Main) {
-        cache = newCache
-      }
+      thisLogger().info("Package list loaded from file with ${packageList.size} entries")
+      val newCache = TreeSet(PythonRankingAwarePackageNameComparator())
+      newCache.addAll(packageList)
+      cache = newCache
     }
   }
 
-  suspend fun store() {
-    withContext(Dispatchers.IO) {
+  private suspend fun store() {
+    mutex.withLock {
+      filePath.deleteIfExists()
       Files.createDirectories(filePath.parent)
       Files.newBufferedWriter(filePath, StandardCharsets.UTF_8).use { writer ->
-        gson.toJson(cache, writer)
+        gson.toJson(cache.toList(), writer)
       }
     }
   }
 
   internal suspend fun loadCache() {
     withContext(Dispatchers.IO) {
-      thisLogger().debug("Updating PyPI packages cache")
+      val log = thisLogger()
+      log.info("Updating PyPI packages cache")
       if (filePath.exists()) {
         val fileTime = Files.getLastModifiedTime(filePath)
         if (fileTime.toInstant().plus(Duration.ofDays(1)).isAfter(Instant.now())) {
-          thisLogger().debug("Cache file is not expired, reading packages locally")
-          loadFromFile()
+          log.info("Cache file is not expired, reading packages locally")
+          try {
+            loadFromFile()
+          }
+          catch (ex: JsonSyntaxException) {
+            log.info("Corrupted cache file, will reload packages from web")
+            refresh()
+          }
           return@withContext
         }
-        thisLogger().debug("Cache expired, rebuilding it")
+        log.info("Cache expired, rebuilding it")
         refresh()
         return@withContext
       }
-      thisLogger().debug("Cache file does not exist, reading packages from PyPI")
+      log.info("Cache file does not exist, reading packages from PyPI")
       refresh()
     }
   }
 
-  suspend fun refresh() {
-    cache.clear()
+  internal suspend fun refresh() {
     withContext(Dispatchers.IO) {
-      cache.addAll(PyPIPackageUtil.parsePyPIListFromWeb(PyPIPackageUtil.PYPI_LIST_URL))
+      thisLogger().info("Loading python packages from PyPi")
+      val pypiList = PyPIPackageUtil.parsePyPIListFromWeb(PyPIPackageUtil.PYPI_LIST_URL)
+      val newCache = TreeSet(PythonRankingAwarePackageNameComparator())
+      newCache.addAll(pypiList)
+
+      cache = newCache
       store()
     }
   }
