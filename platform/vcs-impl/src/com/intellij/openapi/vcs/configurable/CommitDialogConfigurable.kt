55468b363e9d9f8dd324d7d0d6acc9612ff332e3
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-12-02 16:01:15 +0100

    vcs: migrate "Commit Options" settings to UI DSL
    GitOrigin-RevId: 051d54a13e33c5d74a2947f725fc82ff6e51193f

--- platform/vcs-impl/src/com/intellij/openapi/vcs/configurable/CommitDialogConfigurable.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/configurable/CommitDialogConfigurable.kt
@@ -1,25 +1,38 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vcs.configurable
 
-import com.intellij.openapi.options.BoundSearchableConfigurable
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.options.BoundCompositeSearchableConfigurable
+import com.intellij.openapi.options.UnnamedConfigurable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.vcs.VcsApplicationSettings
-import com.intellij.openapi.vcs.VcsBundle
-import com.intellij.openapi.vcs.VcsConfiguration
+import com.intellij.openapi.vcs.*
+import com.intellij.openapi.vcs.changes.Change
+import com.intellij.openapi.vcs.changes.CommitContext
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.dsl.builder.AlignX
 import com.intellij.ui.dsl.builder.bindSelected
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.util.containers.mapNotNullLoggingErrors
 import com.intellij.util.ui.UIUtil
+import com.intellij.vcs.commit.*
 import com.intellij.vcs.commit.AbstractCommitWorkflowHandler.Companion.getDefaultCommitActionName
-import com.intellij.vcs.commit.CommitModeManager
-import com.intellij.vcs.commit.CommitOptionsPanel
 import com.intellij.vcs.commit.message.CommitMessageInspectionsPanel
 import org.jetbrains.annotations.NonNls
+import java.io.File
 
 class CommitDialogConfigurable(private val project: Project)
-  : BoundSearchableConfigurable(VcsBundle.message("commit.dialog.configurable"), HELP_ID, ID) {
+  : BoundCompositeSearchableConfigurable<UnnamedConfigurable>(VcsBundle.message("commit.dialog.configurable"), HELP_ID, ID) {
+
+  override fun createConfigurables(): List<UnnamedConfigurable> {
+    val allVcses = ProjectLevelVcsManager.getInstance(project).allActiveVcss.toList()
+    val checkinPanel = SettingsMockCheckinPanel(project)
+    val commitContext = CommitContext()
+    val checkinHandlers = AbstractCommitWorkflow.getCommitHandlers(allVcses, checkinPanel, commitContext)
+    return checkinHandlers.mapNotNullLoggingErrors(LOG) { it.beforeCheckinSettings }
+  }
+
   override fun createPanel(): DialogPanel {
     val disposable = disposable!!
     val appSettings = VcsApplicationSettings.getInstance()
@@ -49,22 +62,42 @@ class CommitDialogConfigurable(private val project: Project)
         }.resizableRow()
       }
 
-      val actionName = UIUtil.removeMnemonic(getDefaultCommitActionName(emptyList()))
-      group(CommitOptionsPanel.commitChecksGroupTitle(project, actionName)) {
-        val panel = CommitOptionsConfigurable(project)
-        Disposer.register(disposable, panel)
-        row {
-          cell(panel)
-            .align(AlignX.FILL)
-            .onApply { panel.apply() }
-            .onReset { panel.reset() }
-            .onIsModified { panel.isModified }
-        }.resizableRow()
+      if (configurables.isNotEmpty()) {
+        val actionName = UIUtil.removeMnemonic(getDefaultCommitActionName(emptyList()))
+        group(CommitOptionsPanel.commitChecksGroupTitle(project, actionName)) {
+          for (configurable in configurables) {
+            appendDslConfigurable(configurable)
+          }
+        }
       }
     }
   }
 
+  private class SettingsMockCheckinPanel(private val project: Project) : CheckinProjectPanel {
+    override fun getComponent() = null
+    override fun getPreferredFocusedComponent() = null
+
+    override fun getCommitWorkflowHandler(): CommitWorkflowHandler = NullCommitWorkflowHandler
+    override fun getProject() = project
+
+    override fun vcsIsAffected(name: String) = false
+    override fun hasDiffs() = false
+    override fun getRoots() = emptyList<VirtualFile>()
+    override fun getVirtualFiles() = emptyList<VirtualFile>()
+    override fun getSelectedChanges() = emptyList<Change>()
+    override fun getFiles() = emptyList<File>()
+
+    override fun getCommitActionName(): String = getDefaultCommitActionName(emptyList())
+    override fun setCommitMessage(currentDescription: String) {}
+    override fun getCommitMessage() = ""
+
+    override fun saveState() {}
+    override fun restoreState() {}
+  }
+
   companion object {
+    private val LOG = logger<CommitDialogConfigurable>()
+
     const val ID: @NonNls String = "project.propVCSSupport.CommitDialog"
     private const val HELP_ID: @NonNls String = "reference.settings.VCS.CommitDialog"
   }
