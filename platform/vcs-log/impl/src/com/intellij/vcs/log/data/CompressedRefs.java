29a0cfcc5d4d5d916a06b6074eb8403735b7d41b
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2022-12-03 01:49:49 +0100

    [vcs-log] do not put NO_INDEX values into the tags map
    When VcsLogStorage returns a NO_INDEX value it indicates that an exception is happened and is being handled internally. It's not possible to retrieve a tag by this index, so it does not make sense to put it into the map.
    
    EA-548522
    
    GitOrigin-RevId: 2033b24115af6bbec5b0e55029eab35ead372bac

--- platform/vcs-log/impl/src/com/intellij/vcs/log/data/CompressedRefs.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/data/CompressedRefs.java
@@ -34,11 +34,15 @@ public class CompressedRefs {
       assert root.get() == null || root.get().equals(ref.getRoot()) : "All references are supposed to be from the single root";
       root.set(ref.getRoot());
 
+      int index = myStorage.getCommitIndex(ref.getCommitHash(), ref.getRoot());
       if (ref.getType().isBranch()) {
-        putRef(myBranches, ref, myStorage);
+        myBranches.computeIfAbsent(index, key -> new SmartList<>()).add(ref);
       }
       else {
-        putRefIndex(myTags, ref, myStorage);
+        int refIndex = myStorage.getRefIndex(ref);
+        if (refIndex != VcsLogStorageImpl.NO_INDEX) {
+          myTags.computeIfAbsent(index, key -> new IntArrayList()).add(refIndex);
+        }
       }
     });
     //noinspection SSBasedInspection
@@ -110,20 +114,5 @@ public class CompressedRefs {
     myBranches.keySet().intStream().forEach(result::add);
     myTags.keySet().intStream().forEach(result::add);
     return result;
-  }
-
-  private static void putRef(@NotNull Int2ObjectMap<List<VcsRef>> map, @NotNull VcsRef ref, @NotNull VcsLogStorage storage) {
-    int index = storage.getCommitIndex(ref.getCommitHash(), ref.getRoot());
-    List<VcsRef> list = map.get(index);
-    if (list == null) map.put(index, list = new SmartList<>());
-    list.add(ref);
-  }
-
-  private static void putRefIndex(@NotNull Int2ObjectMap<IntArrayList> map, @NotNull VcsRef ref, @NotNull VcsLogStorage storage) {
-    int index = storage.getCommitIndex(ref.getCommitHash(), ref.getRoot());
-    //noinspection SSBasedInspection
-    IntArrayList list = map.get(index);
-    if (list == null) map.put(index, list = new IntArrayList());
-    list.add(storage.getRefIndex(ref));
   }
 }
