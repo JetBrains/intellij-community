9654c6bc0ff1bdb93e1ac64f5e925c807de4fadb
James Rose <rosej@google.com>
2022-12-05 10:17:36 +0100

    Fix memory leak in WelcomeFrame when dispose method is called directly
    The current code assumes that the frame is only disposed by receiving a projectOpened event. However, it is possible to call dispose() directly. This is done by Android Studio, which uses two WelcomeFrames sequentially on a fresh install.
    
    When dispose() is called directly, the MessageBus listener is not removed. This is a memory leak. It also causes a NullPointerException when the projectOpened event occurs later: the dangling listener calls resetInstance(), which causes FlatWelcomeFrame's cleanup method to fail.
    
    closes https://github.com/JetBrains/intellij-community/pull/2259
    
    GitOrigin-RevId: f2896f4b0123ee82898aa31850a223e777d9492c

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/WelcomeFrame.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/WelcomeFrame.kt
@@ -51,6 +51,7 @@ import javax.swing.KeyStroke
 class WelcomeFrame : JFrame(), IdeFrame, AccessibleContextAccessor {
   private val myScreen: WelcomeScreen
   private val myBalloonLayout: BalloonLayout
+  private val listenerDisposable = Disposer.newDisposable()
 
   init {
     SplashManager.hideBeforeShow(this)
@@ -62,12 +63,10 @@ class WelcomeFrame : JFrame(), IdeFrame, AccessibleContextAccessor {
     contentPane = screen.welcomePanel
     title = ApplicationNamesInfo.getInstance().fullProductName
     AppUIUtil.updateWindowIcon(this)
-    val listenerDisposable = Disposer.newDisposable()
     ApplicationManager.getApplication().messageBus.connect(listenerDisposable).subscribe(ProjectManager.TOPIC,
                                                                                          object : ProjectManagerListener {
                                                                                            @Suppress("removal", "OVERRIDE_DEPRECATION")
                                                                                            override fun projectOpened(project: Project) {
-                                                                                             Disposer.dispose(listenerDisposable)
                                                                                              dispose()
                                                                                            }
                                                                                          })
@@ -213,6 +212,7 @@ class WelcomeFrame : JFrame(), IdeFrame, AccessibleContextAccessor {
     saveLocation(bounds)
     super.dispose()
     Disposer.dispose(myScreen)
+    Disposer.dispose(listenerDisposable)
     resetInstance()
   }
 
