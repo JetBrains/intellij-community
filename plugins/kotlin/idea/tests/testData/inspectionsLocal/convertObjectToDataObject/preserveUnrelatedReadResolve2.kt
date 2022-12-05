fd762be708d93bd26f8c5938f73bb4b947b6c9e2
Nikita Bobko <nikita.bobko@jetbrains.com>
2022-12-05 17:09:57 +0100

    ConvertObjectToDataObjectInspection: drop Java serialization handling
    ^KTIJ-23760 Fixed
    
    Review: https://jetbrains.team/p/ij/reviews/99733
    
    GitOrigin-RevId: da47fe21cc8608a1cde8b8b60c0b92b2b0fa5c2d

--- plugins/kotlin/idea/tests/testData/inspectionsLocal/convertObjectToDataObject/preserveUnrelatedReadResolve2.kt
+++
@@ -1,6 +0,0 @@
-// COMPILER_ARGUMENTS: -XXLanguage:+DataObjects
-import java.io.Serializable
-
-object<caret> Foo : Serializable {
-    fun readResolve(): Foo = Foo
-}
--- plugins/kotlin/idea/tests/testData/inspectionsLocal/convertObjectToDataObject/publicReadResolve.kt
+++
@@ -1,7 +0,0 @@
-// COMPILER_ARGUMENTS: -XXLanguage:+DataObjects
-// PROBLEM: none
-import java.io.Serializable
-
-object<caret> Foo : Serializable {
-    fun readResolve(): Any = Foo
-}
