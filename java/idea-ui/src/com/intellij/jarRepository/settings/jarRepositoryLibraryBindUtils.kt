fc7f165c41ff5db7b0326c38505694aa03fe09bb
Vladislav.Yaroshchuk <vladislav.yaroshchuk@jetbrains.com>
2022-12-05 11:55:48 +0400

    IDEA-269182 Simplify jarRepositoryLibraryBindUtils
    GitOrigin-RevId: 70e399e2d0b5a1e12ef3e1c4a21426c7d686d0ee

--- java/idea-ui/src/com/intellij/jarRepository/settings/jarRepositoryLibraryBindUtils.kt
+++ java/idea-ui/src/com/intellij/jarRepository/settings/jarRepositoryLibraryBindUtils.kt
@@ -8,7 +8,6 @@ import com.intellij.configurationStore.serialize
 import com.intellij.jarRepository.*
 import com.intellij.jarRepository.RepositoryLibraryType.REPOSITORY_LIBRARY_KIND
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.roots.libraries.LibraryKindRegistry
 import com.intellij.openapi.util.JDOMUtil
 import com.intellij.workspaceModel.storage.EntityStorage
 import com.intellij.workspaceModel.storage.MutableEntityStorage
@@ -16,9 +15,7 @@ import com.intellij.workspaceModel.storage.bridgeEntities.LibraryEntity
 import com.intellij.workspaceModel.storage.bridgeEntities.LibraryPropertiesEntity
 import com.intellij.workspaceModel.storage.bridgeEntities.modifyEntity
 import org.jetbrains.idea.maven.utils.library.RepositoryLibraryProperties
-import org.jetbrains.jps.model.library.JpsMavenRepositoryLibraryDescriptor.JAR_REPOSITORY_ID_NOT_SET
 import org.jetbrains.jps.model.serialization.library.JpsLibraryTableSerializer
-import java.io.StringReader
 
 private val LOG = Logger.getInstance("com.intellij.jarRepository.settings.JarRepositoryLibraryBindUtils")
 
@@ -44,11 +41,10 @@ internal fun countBindLibraries(storage: EntityStorage, remoteRepository: Remote
 internal fun updateLibrariesRepositoryId(builder: MutableEntityStorage, fromJarRepositoryIds: Set<String>, toJarRepositoryId: String?) {
   getUsagesInLibraryProperties(builder, fromJarRepositoryIds).forEach { entity ->
     val newXmlTag = try {
-      deserializeRepositoryLibraryProperties(entity.propertiesXmlTag!!)
-        .apply { jarRepositoryId = toJarRepositoryId }
-        .let { properties -> serialize(properties)!! }
-        .apply { name = JpsLibraryTableSerializer.PROPERTIES_TAG }
-        .let { JDOMUtil.writeElement(it) }
+      val libraryProperties =  deserializeRepositoryLibraryProperties(entity.propertiesXmlTag!!)
+      libraryProperties.jarRepositoryId = toJarRepositoryId
+      val element = serialize(libraryProperties)!!.setName(JpsLibraryTableSerializer.PROPERTIES_TAG)
+      JDOMUtil.writeElement(element)
     }
     catch (e: Exception) {
       LOG.warnInProduction(e)
@@ -74,14 +70,13 @@ internal fun updateLibrariesRepositoryId(builder: MutableEntityStorage,
 private fun getUsagesInLibraryProperties(storage: EntityStorage, jarRepositoryIds: Set<String>): List<LibraryPropertiesEntity> =
   storage.entities(LibraryEntity::class.java).mapNotNull { libraryEntity ->
     val propertiesEntity = libraryEntity.libraryProperties ?: return@mapNotNull null
-    if (!REPOSITORY_LIBRARY_KIND.equals(LibraryKindRegistry.getInstance().findKindById(propertiesEntity.libraryType))) {
+    if (REPOSITORY_LIBRARY_KIND.kindId != propertiesEntity.libraryType) {
       return@mapNotNull null
     }
 
     val propertiesXmlTag = propertiesEntity.propertiesXmlTag ?: return@mapNotNull null
     val properties = deserializeRepositoryLibraryProperties(propertiesXmlTag)
-    if (properties.jarRepositoryId == JAR_REPOSITORY_ID_NOT_SET ||
-        !jarRepositoryIds.contains(properties.jarRepositoryId)) {
+    if (properties.jarRepositoryId !in jarRepositoryIds) {
       return@mapNotNull null
     }
     propertiesEntity
@@ -89,4 +84,4 @@ private fun getUsagesInLibraryProperties(storage: EntityStorage, jarRepositoryId
 
 
 private fun deserializeRepositoryLibraryProperties(xmlTag: String) =
-  deserialize<RepositoryLibraryProperties>(JDOMUtil.load(StringReader(xmlTag)))
+  deserialize<RepositoryLibraryProperties>(JDOMUtil.load(xmlTag))
