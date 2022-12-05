7b0059b7693fa476343c4d27e33c84ee381a15b3
Ivan Posti <ivan.posti@jetbrains.com>
2022-12-05 17:50:07 +0100

    [Markdown][IDEA-305734] Use smart keys settings for navigating between table cells on tab/shift+tab
    GitOrigin-RevId: 0f8ce1a655ca3c68bc3ab2be4bc7d5ee89d363f4

--- plugins/markdown/core/src/org/intellij/plugins/markdown/editor/tables/handlers/MarkdownTableTabHandler.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/editor/tables/handlers/MarkdownTableTabHandler.kt
@@ -15,7 +15,7 @@ import org.intellij.plugins.markdown.editor.tables.TableUtils.firstNonWhitespace
 import org.intellij.plugins.markdown.editor.tables.TableUtils.lastNonWhitespaceOffset
 import org.intellij.plugins.markdown.lang.psi.impl.MarkdownTableCell
 import org.intellij.plugins.markdown.lang.psi.impl.MarkdownTableRow
-import org.intellij.plugins.markdown.settings.MarkdownSettings
+import org.intellij.plugins.markdown.settings.MarkdownCodeInsightSettings
 
 internal abstract class MarkdownTableTabHandler(private val baseHandler: EditorActionHandler?, private val forward: Boolean = true): EditorWriteActionHandler() {
   override fun isEnabledForCaret(editor: Editor, caret: Caret, dataContext: DataContext?): Boolean {
@@ -30,7 +30,7 @@ internal abstract class MarkdownTableTabHandler(private val baseHandler: EditorA
 
   private fun actuallyExecute(editor: Editor, caret: Caret?, dataContext: DataContext?): Boolean {
     val project = editor.project ?: return false
-    if (!TableUtils.isTableSupportEnabled() || !MarkdownSettings.getInstance(project).isEnhancedEditingEnabled) {
+    if (!isEnabled()) {
       return false
     }
     val document = editor.document
@@ -55,6 +55,10 @@ internal abstract class MarkdownTableTabHandler(private val baseHandler: EditorA
       return true
     }
     return false
+  }
+
+  private fun isEnabled(): Boolean {
+    return TableUtils.isTableSupportEnabled() && MarkdownCodeInsightSettings.getInstance().state.useTableCellNavigation
   }
 
   private fun findNextCell(currentCell: MarkdownTableCell, forward: Boolean): MarkdownTableCell? {
