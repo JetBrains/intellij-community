9a1af5345e82128e9db66034894ee5e0e086c4ed
Ivan Posti <ivan.posti@jetbrains.com>
2022-12-05 17:49:04 +0100

    [Markdown][IDEA-305734] Add Smart Keys settings page
    GitOrigin-RevId: b1c0b2bd633a0aad292c00dc324fa90f8d1f3d9e

---
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/settings/MarkdownCodeInsightSettings.kt
@@ -0,0 +1,46 @@
+package org.intellij.plugins.markdown.settings
+
+import com.intellij.openapi.components.*
+import com.intellij.util.xmlb.annotations.Property
+
+@Service
+@State(name = "MarkdownCodeInsightSettings", storages = [(Storage("markdown.xml"))])
+internal class MarkdownCodeInsightSettings: PersistentStateComponent<MarkdownCodeInsightSettings.State> {
+  private var state = State()
+
+  data class State(
+    @Property
+    val reformatTablesOnType: Boolean = true,
+    @Property
+    val insertLineBreakInsideTables: Boolean = true,
+    @Property
+    val insertNewTableRowOnShiftTab: Boolean = true,
+    @Property
+    val useTableCellNavigation: Boolean = true,
+    @Property
+    val adjustListIndentation: Boolean = true,
+    @Property
+    val indentListsOnTab: Boolean = true,
+    @Property
+    val renumberListsOnType: Boolean = true
+  )
+
+  fun update(transform: (State) -> State) {
+    loadState(transform(state))
+  }
+
+  override fun getState(): State {
+    return state
+  }
+
+  override fun loadState(state: State) {
+    this.state = state
+  }
+
+  companion object {
+    @JvmStatic
+    fun getInstance(): MarkdownCodeInsightSettings {
+      return service()
+    }
+  }
+}
---
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/settings/MarkdownSmartKeysConfigurable.kt
@@ -0,0 +1,73 @@
+package org.intellij.plugins.markdown.settings
+
+import com.intellij.application.options.CodeCompletionOptionsCustomSection
+import com.intellij.openapi.options.Configurable
+import com.intellij.openapi.options.UiDslUnnamedConfigurable
+import com.intellij.ui.dsl.builder.Panel
+import com.intellij.ui.dsl.builder.bindSelected
+import org.intellij.plugins.markdown.MarkdownBundle
+
+internal class MarkdownSmartKeysConfigurable: UiDslUnnamedConfigurable.Simple(), Configurable, CodeCompletionOptionsCustomSection {
+  private val settings
+    get() = MarkdownCodeInsightSettings.getInstance()
+
+  override fun getDisplayName(): String {
+    return MarkdownBundle.message("markdown.smart.keys.configurable.name")
+  }
+
+  override fun Panel.createContent() {
+    group(title = MarkdownBundle.message("markdown.smart.keys.configurable.tables.group.name")) {
+      row {
+        checkBox(MarkdownBundle.message("markdown.smart.keys.configurable.tables.reformat.on.type"))
+          .bindSelected(
+            getter = { settings.state.reformatTablesOnType },
+            setter = { settings.update { state -> state.copy(reformatTablesOnType = it) } }
+          )
+      }
+      row {
+        checkBox(MarkdownBundle.message("markdown.smart.keys.configurable.tables.insert.line.break"))
+          .bindSelected(
+            getter = { settings.state.insertLineBreakInsideTables },
+            setter = { settings.update { state -> state.copy(insertLineBreakInsideTables = it) } }
+          )
+      }
+      row {
+        checkBox(MarkdownBundle.message("markdown.smart.keys.configurable.tables.insert.new.row"))
+          .bindSelected(
+            getter = { settings.state.insertNewTableRowOnShiftTab },
+            setter = { settings.update { state -> state.copy(insertNewTableRowOnShiftTab = it) } }
+          )
+      }
+      row {
+        checkBox(MarkdownBundle.message("markdown.smart.keys.configurable.tables.use.cell.navigation"))
+          .bindSelected(
+            getter = { settings.state.useTableCellNavigation },
+            setter = { settings.update { state -> state.copy(useTableCellNavigation = it) } }
+          )
+      }
+    }
+    group(title = MarkdownBundle.message("markdown.smart.keys.configurable.lists.group.name")) {
+      row {
+        checkBox(MarkdownBundle.message("markdown.smart.keys.configurable.lists.adjust.indentation.on.enter.backspace"))
+          .bindSelected(
+            getter = { settings.state.adjustListIndentation },
+            setter = { settings.update { state -> state.copy(adjustListIndentation = it) } }
+          )
+      }
+      row {
+        checkBox(MarkdownBundle.message("markdown.smart.keys.configurable.lists.indent.on.tab"))
+          .bindSelected(
+            getter = { settings.state.indentListsOnTab },
+            setter = { settings.update { state -> state.copy(indentListsOnTab = it) } }
+          )
+      }
+      row {
+        checkBox(MarkdownBundle.message("markdown.smart.keys.configurable.lists.renumber.lists"))
+          .bindSelected(
+            getter = { settings.state.renumberListsOnType },
+            setter = { settings.update { state -> state.copy(renumberListsOnType = it) } }
+          )
+      }
+    }
+  }
+}
