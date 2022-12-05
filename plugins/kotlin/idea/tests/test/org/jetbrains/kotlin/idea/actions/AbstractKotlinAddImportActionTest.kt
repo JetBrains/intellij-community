0801019c28875321024370a1141f40a032c5fee5
Andy Magee <andymagee@google.com>
2022-12-05 12:53:37 -0800

    KTIJ-23683: Sort descriptors for single imports
    https://youtrack.jetbrains.com/issue/KTIJ-23683
    
    SingleImportVariant effectively displays the first descriptor in the
    list it's given (with the caveat that it prefers ClassDescriptors
    over other descriptors). This changes sorts the list before it's given
    to SingleImportVariant, using the priority that's already being
    calculated for the descriptors. Doing this ensures the highest priority
    (or rather, lowest numeric value priority) is used for display.

---
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/actions/AbstractKotlinAddImportActionTest.kt
@@ -0,0 +1,72 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.actions
+
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.idea.base.test.TestRoot
+import org.jetbrains.kotlin.idea.test.InTextDirectivesUtils
+import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
+import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
+import org.jetbrains.kotlin.test.TestMetadata
+import org.junit.internal.runners.JUnit38ClassRunner
+import org.junit.runner.RunWith
+import java.io.File
+
+abstract class AbstractKotlinAddImportActionTest : KotlinLightCodeInsightFixtureTestCase() {
+
+    protected fun doTest(unused: String) {
+        val fixture = myFixture
+
+        val dependencySuffixes = listOf(".dependency.kt", ".dependency1.kt", ".dependency2.kt")
+        for (suffix in dependencySuffixes) {
+            val depFilePath = fileName().replace(".kt", suffix)
+            if (File(testDataDirectory, depFilePath).exists()) {
+                fixture.configureByFile(depFilePath)
+            }
+        }
+
+        fixture.configureByFile(fileName())
+
+        var actualVariants: List<List<DeclarationDescriptor>>? = null
+        val executeListener = object : KotlinAddImportActionInfo.ExecuteListener {
+            override fun onExecute(variants: List<List<DeclarationDescriptor>>) {
+                assertNull(actualVariants)
+                actualVariants = variants
+            }
+        }
+        KotlinAddImportActionInfo.setExecuteListener(testRootDisposable, executeListener)
+
+        val importFix = myFixture.availableIntentions.singleOrNull { it.familyName == "Import" } ?: error("No import fix available")
+        importFix.invoke(project, editor, file)
+
+        assertNotNull(actualVariants)
+
+        val expectedVariantNames = InTextDirectivesUtils.findListWithPrefixes(fixture.file.text, "EXPECT_VARIANT_IN_ORDER")
+        val expectedAbsentVariantNames = InTextDirectivesUtils.findListWithPrefixes(fixture.file.text, "EXPECT_VARIANT_NOT_PRESENT")
+
+        if (expectedVariantNames.isNotEmpty() || expectedAbsentVariantNames.isNotEmpty()) {
+            val actualVariantNames = actualVariants!!.flatten().map { it.variantName() }
+
+            for (i in expectedVariantNames.indices) {
+                assertTrue(
+                    "'${actualVariantNames[0]}' should start with '${expectedVariantNames[0]}'",
+                    actualVariantNames[0].startsWith(expectedVariantNames[0])
+                )
+            }
+
+            for (expectedAbsentVariant in expectedAbsentVariantNames) {
+                assertTrue("$expectedAbsentVariant should not be present",
+                           actualVariantNames.none { it.startsWith(expectedAbsentVariant) })
+            }
+        }
+
+        myFixture.checkResultByFile("${fileName()}.after")
+    }
+
+    private fun DeclarationDescriptor.variantName() = when(this) {
+        is ClassDescriptor ->
+            fqNameOrNull()?.toString()?.let { "class $it" } ?: toString()
+        else ->
+            toString()
+    }
+}
