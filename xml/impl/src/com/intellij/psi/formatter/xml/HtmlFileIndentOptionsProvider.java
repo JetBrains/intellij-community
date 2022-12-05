5a9810ed654e36414f0e1e3d26c23dbed3f1ae3a
Rustam Vishniakov <rustam.vishnyakov@jetbrains.com>
2022-12-05 15:15:10 +0100

    IJPL-64: Replace usage of deprecated getIndentOptions(CodeStyleSettings,PsiFile) method
    GitOrigin-RevId: 22743d21ec22a7edf7d1166653d54c22d6dac98f

--- xml/impl/src/com/intellij/psi/formatter/xml/HtmlFileIndentOptionsProvider.java
+++ xml/impl/src/com/intellij/psi/formatter/xml/HtmlFileIndentOptionsProvider.java
@@ -1,20 +1,26 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.formatter.xml;
 
-import com.intellij.psi.PsiFile;
+import com.intellij.ide.highlighter.HtmlFileType;
+import com.intellij.lang.Language;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.codeStyle.CommonCodeStyleSettings.IndentOptions;
 import com.intellij.psi.codeStyle.FileIndentOptionsProvider;
-import com.intellij.psi.impl.source.html.HtmlFileImpl;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class HtmlFileIndentOptionsProvider extends FileIndentOptionsProvider {
   @Override
-  public @Nullable IndentOptions getIndentOptions(@NotNull CodeStyleSettings settings, @NotNull PsiFile file) {
-    if (file instanceof HtmlFileImpl) {
+  public @Nullable IndentOptions getIndentOptions(@NotNull Project project, @NotNull CodeStyleSettings settings, @NotNull VirtualFile file) {
+    FileType fileType = file.getFileType();
+    if (fileType.equals(HtmlFileType.INSTANCE)) {
       HtmlCodeStyleSettings htmlSettings = settings.getCustomSettings(HtmlCodeStyleSettings.class);
-      IndentOptions options = settings.getLanguageIndentOptions(file.getLanguage());
+      Language language = ((LanguageFileType)fileType).getLanguage();
+      IndentOptions options = settings.getLanguageIndentOptions(language);
       if (htmlSettings.HTML_UNIFORM_INDENT && options != null) {
         options = (IndentOptions)options.clone();
         options.setOverrideLanguageOptions(true);
