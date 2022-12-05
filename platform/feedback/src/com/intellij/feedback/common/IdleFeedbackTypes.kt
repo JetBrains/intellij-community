4c4b5ed4fd87bf1b66f0b0d01cd18bebec7c2b68
Dmitry Pogrebnoy <dmitry.pogrebnoy@jetbrains.com>
2022-12-05 16:33:46 +0100

    [Feedback] Disable New UI Feedback survey
    IJ-CR-99704
    
    GitOrigin-RevId: 8f62094442f877d07b0314e5bc7e1db362375387

--- platform/feedback/src/com/intellij/feedback/common/IdleFeedbackTypes.kt
+++ platform/feedback/src/com/intellij/feedback/common/IdleFeedbackTypes.kt
@@ -85,7 +85,7 @@ enum class IdleFeedbackTypes {
   },
   NEW_UI_FEEDBACK {
     override val suitableIdeVersion: String = "2022.3"
-    private val lastDayCollectFeedback = LocalDate(2022, 12, 31)
+    private val lastDayCollectFeedback = LocalDate(2022, 12, 6)
     private val maxNumberNotificationShowed = 1
     private val minNumberDaysElapsed = 5
 
