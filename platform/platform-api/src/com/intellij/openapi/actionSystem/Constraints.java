a227bbde3ce1bd0bae618854a54b6821d3ff7d27
Roland Illig <roland.illig@jetbrains.com>
2022-12-05 22:10:32 +0100

    Improve Javadoc in DefaultActionGroup and related classes
    (cherry picked from commit 3d40a3fdaef6f46dc5378959f823a5796f425785)
    
    IJ-CR-99754
    
    GitOrigin-RevId: 259cada768bf7b24be60707af594f8d13da99994

--- platform/platform-api/src/com/intellij/openapi/actionSystem/Constraints.java
+++ platform/platform-api/src/com/intellij/openapi/actionSystem/Constraints.java
@@ -1,25 +1,11 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.actionSystem;
 
 import org.jetbrains.annotations.NonNls;
 
 /**
- * Represents constraints for some action. Constraints are used to specify
- * action's position in the default group, see {@link DefaultActionGroup}.
+ * Represents a single constraint for some action.
+ * Constraints are used to specify the action's position relative to other actions.
  *
  * @see DefaultActionGroup
  */
@@ -33,30 +19,30 @@ public class Constraints implements Cloneable {
   public Anchor myAnchor;
 
   /**
-   * ID of the action to be positioned relative to. Used when anchor type
-   * is either {@link Anchor#AFTER} or {@link Anchor#BEFORE}.
-   *
+   * The ID of the action to be positioned relative to.
+   * Used when the anchor type is either {@link Anchor#AFTER} or {@link Anchor#BEFORE}.
    */
   public String myRelativeToActionId;
 
   /**
    * Creates a new Constraints instance with the specified anchor type and
-   * id of the relative action.
+   * the ID of the relative action.
    *
-   * @param anchor anchor
-   * @param relativeToActionId ID of the relative action
+   * @param anchor             anchor
+   * @param relativeToActionId ID of the relative action,
+   *                           for {@link Anchor#BEFORE} or {@link Anchor#AFTER}
    */
-  public Constraints(Anchor anchor, @NonNls String relativeToActionId){
+  public Constraints(Anchor anchor, @NonNls String relativeToActionId) {
     myAnchor = anchor;
     myRelativeToActionId = relativeToActionId;
   }
 
   @Override
-  public Object clone(){
-    try{
+  public Object clone() {
+    try {
       return super.clone();
     }
-    catch(CloneNotSupportedException exc){
+    catch (CloneNotSupportedException exc) {
       throw new RuntimeException(exc.getMessage());
     }
   }
