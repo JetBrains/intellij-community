86f636ed4fb8a58488943da06608a69e1cf4256f
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2022-12-06 22:44:19 +0100

    [kotlin] Migrate to ChooseByNameContributorEx
    #KTIJ-912
    
    GitOrigin-RevId: 1b0b0e9c7448dcdad5ccf881efe9bd59995b17ce

--- plugins/kotlin/base/indices/src/org/jetbrains/kotlin/idea/stubindex/KotlinJvmNameAnnotationIndex.kt
+++ plugins/kotlin/base/indices/src/org/jetbrains/kotlin/idea/stubindex/KotlinJvmNameAnnotationIndex.kt
@@ -4,12 +4,11 @@ package org.jetbrains.kotlin.idea.stubindex
 
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.stubs.StringStubIndexExtension
 import com.intellij.psi.stubs.StubIndex
 import com.intellij.psi.stubs.StubIndexKey
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 
-object KotlinJvmNameAnnotationIndex : StringStubIndexExtension<KtAnnotationEntry>() {
+object KotlinJvmNameAnnotationIndex : KotlinStringStubIndexExtension<KtAnnotationEntry>(KtAnnotationEntry::class.java) {
     private val KEY: StubIndexKey<String, KtAnnotationEntry> =
         StubIndexKey.createIndexKey("org.jetbrains.kotlin.idea.stubindex.KotlinJvmNameAnnotationIndex")
 
