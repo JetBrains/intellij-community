888a07d55e0113ca9894a30abb31b70223018311
Aleksey Pivovarov <AMPivovarov@gmail.com>
2022-12-06 13:21:10 +0100

    vcs: do not use VcsStartupActivity for GitCommitTemplateTracker
    Make sure Git plugin can be loaded dynamically.
    
    GitOrigin-RevId: 9644e3ac3924f40db11a0f4ae2d0fbfa88f9f943

--- platform/vcs-impl/src/com/intellij/openapi/vcs/impl/VcsStartupActivity.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/impl/VcsStartupActivity.java
@@ -2,8 +2,17 @@
 package com.intellij.openapi.vcs.impl;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Ordered pipeline for initialization of VCS-related services.
+ * Typically, should not be needed by plugins.
+ *
+ * @see ProjectLevelVcsManager#runAfterInitialization(Runnable)
+ * @see StartupActivity.Background
+ */
 public interface VcsStartupActivity {
   void runActivity(@NotNull Project project);
 
--- plugins/git4idea/src/git4idea/repo/GitCommitTemplateTracker.kt
+++ plugins/git4idea/src/git4idea/repo/GitCommitTemplateTracker.kt
@@ -8,13 +8,14 @@ import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.util.BackgroundTaskUtil
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectPostStartupActivity
 import com.intellij.openapi.util.io.FileUtil
-import com.intellij.openapi.vcs.impl.VcsInitObject
-import com.intellij.openapi.vcs.impl.VcsStartupActivity
+import com.intellij.openapi.vcs.ProjectLevelVcsManager
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.newvfs.events.*
 import com.intellij.util.ObjectUtils
 import com.intellij.util.SystemProperties
+import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
 import com.intellij.util.messages.Topic
 import com.intellij.vfs.AsyncVfsEventsListener
 import com.intellij.vfs.AsyncVfsEventsPostProcessor
@@ -22,6 +23,7 @@ import git4idea.GitUtil
 import git4idea.commands.Git
 import git4idea.config.GitConfigUtil
 import git4idea.config.GitConfigUtil.COMMIT_TEMPLATE
+import org.jetbrains.annotations.VisibleForTesting
 import java.io.File
 import java.io.IOException
 import java.util.concurrent.atomic.AtomicBoolean
@@ -83,10 +85,12 @@ internal class GitCommitTemplateTracker(private val project: Project) : GitConfi
     BackgroundTaskUtil.runUnderDisposeAwareIndicator(this) { processEvents(events) }
   }
 
-  private fun start() {
-    BackgroundTaskUtil.syncPublisher(project, GitCommitTemplateListener.TOPIC).loadingStarted()
-    GitUtil.getRepositories(project).forEach(::trackCommitTemplate)
+  @VisibleForTesting
+  @RequiresBackgroundThread
+  fun start() {
     if (started.compareAndSet(false, true)) {
+      BackgroundTaskUtil.syncPublisher(project, GitCommitTemplateListener.TOPIC).loadingStarted()
+      GitUtil.getRepositories(project).forEach(::trackCommitTemplate)
       BackgroundTaskUtil.syncPublisher(project, GitCommitTemplateListener.TOPIC).loadingFinished()
     }
   }
@@ -103,18 +107,18 @@ internal class GitCommitTemplateTracker(private val project: Project) : GitConfi
         val watchedTemplatePath = template.watchedRoot.rootPath
 
         var templateChanged = false
-          if (isEventToStopTracking(event, watchedTemplatePath)) {
-            synchronized(this) {
-              stopTrackCommitTemplate(repository)
-            }
-            templateChanged = true
+        if (isEventToStopTracking(event, watchedTemplatePath)) {
+          synchronized(this) {
+            stopTrackCommitTemplate(repository)
           }
-          else if (isEventToReloadTemplateContent(event, watchedTemplatePath)) {
-            synchronized(this) {
-              reloadCommitTemplateContent(repository)
-            }
-            templateChanged = true
+          templateChanged = true
+        }
+        else if (isEventToReloadTemplateContent(event, watchedTemplatePath)) {
+          synchronized(this) {
+            reloadCommitTemplateContent(repository)
           }
+          templateChanged = true
+        }
 
         if (templateChanged) {
           BackgroundTaskUtil.syncPublisher(project, GitCommitTemplateListener.TOPIC).notifyCommitTemplateChanged(repository)
@@ -198,6 +202,7 @@ internal class GitCommitTemplateTracker(private val project: Project) : GitConfi
     return null
   }
 
+  @RequiresBackgroundThread
   private fun trackCommitTemplate(repository: GitRepository) {
     val newTemplatePath = resolveCommitTemplatePath(repository)
     val templateChanged = synchronized(this) {
@@ -260,13 +265,12 @@ internal class GitCommitTemplateTracker(private val project: Project) : GitConfi
     }
   }
 
-  internal class GitCommitTemplateTrackerStartupActivity : VcsStartupActivity {
-
-    override fun runActivity(project: Project) {
-      project.service<GitCommitTemplateTracker>().start()
+  internal class GitCommitTemplateTrackerStartupActivity : ProjectPostStartupActivity {
+    override suspend fun execute(project: Project) {
+      ProjectLevelVcsManager.getInstance(project).runAfterInitialization {
+        project.service<GitCommitTemplateTracker>().start()
+      }
     }
-
-    override fun getOrder(): Int = VcsInitObject.AFTER_COMMON.order
   }
 
   companion object {
