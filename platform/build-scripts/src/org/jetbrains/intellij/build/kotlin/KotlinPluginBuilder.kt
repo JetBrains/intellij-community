0c17f20a0db69b3ac3e1608fa136c0f3d5ccd21d
Roman Efremov <roman.efremov@jetbrains.com>
2022-12-06 12:50:27 +0100

    KTIJ-23777 Enable Enum.values() in Java inspection in new projects created in wizard
    New module "kotlin.project-wizard.idea-k2" created to support logic in k2.
    
    (cherry picked from commit aa8d3d87a8ccd3aee2a650caf3cc66f52cfb7761)
    
    IJ-MR-99313
    
    GitOrigin-RevId: bc99980513803eaa50201b371c7afa0363781a1a

--- platform/build-scripts/src/org/jetbrains/intellij/build/kotlin/KotlinPluginBuilder.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/kotlin/KotlinPluginBuilder.kt
@@ -131,6 +131,7 @@ object KotlinPluginBuilder {
     "kotlin.project-wizard.core",
     "kotlin.project-wizard.idea",
     "kotlin.project-wizard.idea-k1",
+    "kotlin.project-wizard.idea-k2",
     "kotlin.project-wizard.maven",
     "kotlin.project-wizard.gradle",
     "kotlin.project-wizard.compose",
--- plugins/kotlin/project-wizard/idea-k2/src/org/jetbrains/kotlin/tools/projectWizard/k2/service/IdeaInspectionsWizardService.kt
+++ plugins/kotlin/project-wizard/idea-k2/src/org/jetbrains/kotlin/tools/projectWizard/k2/service/IdeaInspectionsWizardService.kt
@@ -1,15 +1,13 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.tools.projectWizard.k1.service
+package org.jetbrains.kotlin.tools.projectWizard.k2.service
 
 import com.intellij.codeHighlighting.HighlightDisplayLevel
 import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.idea.inspections.EnumValuesSoftDeprecateInJavaInspection
-import org.jetbrains.kotlin.idea.inspections.ReplaceUntilWithRangeUntilInspection
 import org.jetbrains.kotlin.tools.projectWizard.wizard.service.BaseInspectionWizardService
 
 internal class IdeaInspectionsWizardService(project: Project) : BaseInspectionWizardService(project) {
     override val inspectionsToEnable = listOf(
-        ::ReplaceUntilWithRangeUntilInspection to HighlightDisplayLevel.WEAK_WARNING,
         ::EnumValuesSoftDeprecateInJavaInspection to HighlightDisplayLevel.WARNING,
     )
 }
