fa4a95cc76f1255e4b838272d5da4876d2aedf8d
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2022-12-06 18:40:17 +0100

    IDEA-261340 AbstractTreeNodeVisitor.visit
    update tree in background
    
    GitOrigin-RevId: b8a34882abfbd52d0903647a1f7b753dbbaaba4a

--- platform/platform-impl/src/com/intellij/ui/tree/AsyncTreeModel.java
+++ platform/platform-impl/src/com/intellij/ui/tree/AsyncTreeModel.java
@@ -250,7 +250,7 @@ public final class AsyncTreeModel extends AbstractTreeModel implements Searchabl
       background.invokeLater(() -> onValidThread(() -> promiseRootEntry().onSuccess(walker::start).onError(walker::setError)));
     }
     else {
-      onValidThread(() -> walker.start(tree.root));
+      background.invoke(() -> walker.start(tree.root));
     }
     return walker.promise();
   }
