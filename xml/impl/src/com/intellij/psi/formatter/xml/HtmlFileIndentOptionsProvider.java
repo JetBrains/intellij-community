a2959ab8837147e02039560de4f2508c3d1accd2
Rustam Vishniakov <rustam.vishnyakov@jetbrains.com>
2022-12-07 13:38:26 +0100

    IJ-CR-99764 fix: use file language instead of file type
    GitOrigin-RevId: 3ee055aa95b52a06f21c2e1225c4047804786a5f

--- xml/impl/src/com/intellij/psi/formatter/xml/HtmlFileIndentOptionsProvider.java
+++ xml/impl/src/com/intellij/psi/formatter/xml/HtmlFileIndentOptionsProvider.java
@@ -1,8 +1,8 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.formatter.xml;
 
-import com.intellij.ide.highlighter.HtmlFileType;
 import com.intellij.lang.Language;
+import com.intellij.lang.html.HTMLLanguage;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.LanguageFileType;
 import com.intellij.openapi.project.Project;
@@ -17,7 +17,7 @@ public class HtmlFileIndentOptionsProvider extends FileIndentOptionsProvider {
   @Override
   public @Nullable IndentOptions getIndentOptions(@NotNull Project project, @NotNull CodeStyleSettings settings, @NotNull VirtualFile file) {
     FileType fileType = file.getFileType();
-    if (fileType.equals(HtmlFileType.INSTANCE)) {
+    if (fileType instanceof LanguageFileType && ((LanguageFileType)fileType).getLanguage().isKindOf(HTMLLanguage.INSTANCE)) {
       HtmlCodeStyleSettings htmlSettings = settings.getCustomSettings(HtmlCodeStyleSettings.class);
       Language language = ((LanguageFileType)fileType).getLanguage();
       IndentOptions options = settings.getLanguageIndentOptions(language);
