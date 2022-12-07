29e790b5ba19643fed0f3bacbad01350c46c08d5
igor.davidenko <igor.davidenko@jetbrains.com>
2022-12-07 19:02:22 +0100

    [full-line] refactor var to val
    GitOrigin-RevId: efdb4de7d7aea170f8f0dca3549a73bb0ea0bfe3

--- plugins/full-line/core/src/org/jetbrains/completion/full/line/language/supporters/FullLineLanguageSupporterBase.kt
+++ plugins/full-line/core/src/org/jetbrains/completion/full/line/language/supporters/FullLineLanguageSupporterBase.kt
@@ -199,9 +199,9 @@ abstract class FullLineLanguageSupporterBase(extraMatchers: List<LocationMatcher
 
   private fun inComment(element: PsiElement): Boolean = element is PsiComment
 
-  open var STRING_MARKERS = arrayListOf('"', '\'')
+  open val STRING_MARKERS = arrayListOf('"', '\'')
 
-  open var CLOSE_TO_OPEN = hashMapOf(
+  open val CLOSE_TO_OPEN = hashMapOf(
     // TODO: Bring back handling such angle brackets when element will be extended with psi.
     // '>' to '<',
     ')' to '(',
