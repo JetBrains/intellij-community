bac4f5be77af2d068140bc86002cd70dcdb8e02a
Ivan Semenov <ivan.semenov@jetbrains.com>
2022-12-08 16:21:49 +0100

    [collab/github/space] layout avatar in comment input correctly
    GitOrigin-RevId: 3004027c4204312d14a621d7f133e0c576dd7c61

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/comment/ui/GHCommentTextFieldFactory.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/comment/ui/GHCommentTextFieldFactory.kt
@@ -1,24 +1,17 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.pullrequest.comment.ui
 
-import com.intellij.collaboration.ui.CollaborationToolsUIUtil
 import com.intellij.collaboration.ui.codereview.comment.CommentInputActionsComponentFactory
 import com.intellij.collaboration.ui.codereview.timeline.comment.CommentInputComponentFactory
 import com.intellij.collaboration.ui.codereview.timeline.comment.CommentInputComponentFactory.CancelActionConfig
-import com.intellij.collaboration.ui.codereview.timeline.comment.CommentInputComponentFactory.getEditorTextFieldVerticalOffset
 import com.intellij.collaboration.ui.codereview.timeline.comment.CommentTextFieldFactory
 import com.intellij.collaboration.ui.codereview.timeline.comment.CommentTextFieldModel
-import com.intellij.util.ui.UIUtil
-import net.miginfocom.layout.CC
-import net.miginfocom.layout.LC
-import net.miginfocom.swing.MigLayout
 import org.jetbrains.plugins.github.api.data.GHUser
 import org.jetbrains.plugins.github.pullrequest.ui.timeline.GHPRTimelineItemUIUtil
 import org.jetbrains.plugins.github.ui.avatars.GHAvatarIconsProvider
 import javax.swing.Action
 import javax.swing.JComponent
 import javax.swing.JLabel
-import javax.swing.JPanel
 
 class GHCommentTextFieldFactory(private val model: CommentTextFieldModel) {
 
@@ -42,35 +35,16 @@ class GHCommentTextFieldFactory(private val model: CommentTextFieldModel) {
       AvatarMode.COMMENT -> GHPRReviewCommentComponent.AVATAR_SIZE
       AvatarMode.TIMELINE -> GHPRTimelineItemUIUtil.MAIN_AVATAR_SIZE
     }
-    val avatarRightGap = when (avatar.mode) {
-                           AvatarMode.COMMENT -> GHPRReviewCommentComponent.AVATAR_GAP
-                           AvatarMode.TIMELINE -> GHPRTimelineItemUIUtil.AVATAR_CONTENT_GAP
-                         } - CollaborationToolsUIUtil.getFocusBorderInset()
-    val avatarTopGap = when (avatar.mode) {
-      AvatarMode.COMMENT -> getEditorTextFieldVerticalOffset() - 2
-      AvatarMode.TIMELINE -> 0
-    }
-    val editorTopGap = when (avatar.mode) {
-      AvatarMode.COMMENT -> 0
-      AvatarMode.TIMELINE -> 1
+    val avatarGap = when (avatar.mode) {
+      AvatarMode.COMMENT -> GHPRReviewCommentComponent.AVATAR_GAP
+      AvatarMode.TIMELINE -> GHPRTimelineItemUIUtil.AVATAR_CONTENT_GAP
     }
 
-    val authorLabel = JLabel(avatar.avatarIconsProvider.getIcon(avatar.user.avatarUrl, avatarSize)).apply {
-      putClientProperty(UIUtil.HIDE_EDITOR_FROM_DATA_CONTEXT_PROPERTY, true)
-    }
-
-    return JPanel(null).apply {
-      isOpaque = false
-      layout = MigLayout(LC().gridGap("0", "0")
-                           .insets("0", "0", "0", "0")
-                           .fillX())
-
-      add(authorLabel, CC().alignY("top")
-        .gapRight("${avatarRightGap}").gapTop("${avatarTopGap}"))
-      add(component, CC().grow().push()
-        .gapTop("${editorTopGap}")
-        .minWidth("0"))
-    }
+    return CommentInputComponentFactory
+      .addIconLeft(component,
+                   avatar.avatarIconsProvider.getIcon(avatar.user.avatarUrl, avatarSize),
+                   avatarGap,
+                   avatar.user.getPresentableName())
   }
 
   data class AvatarConfig(val avatarIconsProvider: GHAvatarIconsProvider,
@@ -83,5 +57,4 @@ class GHCommentTextFieldFactory(private val model: CommentTextFieldModel) {
 
   data class ActionsConfig(val inputActions: CommentInputActionsComponentFactory.Config,
                            val cancelAction: Action? = null)
-
 }
