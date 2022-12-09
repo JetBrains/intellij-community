782411ca3066ee8e98575f8aeac80996cf782559
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2022-12-09 16:05:30 +0300

    [kt, tests] Introduce Tier One MPP Test Cases
    ^KT-55278
    
    (cherry picked from commit a12f86d7d1044898d7cb26a84666050f8e057f74)
    
    GitOrigin-RevId: e8d3ca22ef733fd45f7876afa72402ebd9cf3f39

---
+++ plugins/kotlin/idea/tests/testData/gradle/newMppTests/tier1/kmmProjectWithSdkBinary/androidApp/src/main/android/android_main_usages.kt
@@ -0,0 +1,10 @@
+package android
+
+fun use(c: CommonMainExpect) {
+    // Refinement on libs work + checking that jvmMain and commonMain symbols are visible
+    consumeJvmMainExpect(produceCommonMainExpect())
+    consumeCommonMainExpect(produceJvmMainExpect())
+
+    // Refinement on expects works
+    produceCommonMainExpect().jvmApi()
+}
---
+++ plugins/kotlin/idea/tests/testData/gradle/newMppTests/tier1/twoKmmLibrariesSource/kmmLib/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    ios()
+    jvm()
+}
