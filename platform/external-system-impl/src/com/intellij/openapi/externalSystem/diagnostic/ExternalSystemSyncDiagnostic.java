78c89720eadb287c303ebf2a8b310afbfdecfd1f
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2022-12-09 17:57:14 +0200

    [gradle] reporting project import metrics via open-telemetry
    GitOrigin-RevId: 258797a68f80bed5666746e0918059f6e8c5aac2

---
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/diagnostic/ExternalSystemSyncDiagnostic.java
@@ -0,0 +1,45 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.externalSystem.diagnostic;
+
+import com.intellij.diagnostic.telemetry.IJTracer;
+import com.intellij.diagnostic.telemetry.TraceManager;
+import com.intellij.diagnostic.telemetry.TracerLevel;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanBuilder;
+import io.opentelemetry.context.Context;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
+public class ExternalSystemSyncDiagnostic {
+  public static final IJTracer syncTracer = TraceManager.INSTANCE.getTracer("external-system-sync");
+
+  private static final ConcurrentHashMap<String, Span> spans = new ConcurrentHashMap<>();
+
+  public static final String syncSpanName = "gradle.sync.duration";
+
+  public static Context getSyncSpanContext() {
+    return Context.current().with(getOrStartSpan(syncSpanName));
+  }
+
+  public static Span getOrStartSpan(@NotNull String spanName) {
+    return getOrStartSpan(spanName, (builder) -> builder);
+  }
+
+  public static Span getOrStartSpan(@NotNull String spanName, Function<SpanBuilder, SpanBuilder> action) {
+    return spans.computeIfAbsent(spanName, (name) -> action.apply(syncTracer.spanBuilder(spanName, TracerLevel.DEFAULT)).startSpan());
+  }
+
+  public static void endSpan(@NotNull String spanName) {
+    endSpan(spanName, (span) -> span);
+  }
+
+  public static void endSpan(@NotNull String spanName, Function<Span, Span> action) {
+    if (!spans.containsKey(spanName)) {
+      throw new RuntimeException(String.format("Span with name %s isn't started yet, but called to stop", spanName));
+    }
+
+    action.apply(spans.get(spanName)).end();
+  }
+}
