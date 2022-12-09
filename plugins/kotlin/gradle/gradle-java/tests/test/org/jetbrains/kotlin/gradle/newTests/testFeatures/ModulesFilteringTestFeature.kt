a86fa191cf95c26f63828841902520d8e650da6f
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2022-12-09 16:03:21 +0300

    [kt, tests] Support regexes for filtering workspace items
    (cherry picked from commit 28fc83a6b4d1849e3b639c422754dfd8a2864aa2)
    
    GitOrigin-RevId: 9aa6fff787cbb1ba2c20b50276a942638b85b4d5

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/newTests/testFeatures/ModulesFilteringTestFeature.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/newTests/testFeatures/ModulesFilteringTestFeature.kt
@@ -1,14 +1,15 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.gradle.newTests.testFeatures
 
+import org.intellij.lang.annotations.Language
 import org.jetbrains.kotlin.gradle.newTests.TestConfigurationDslScope
 import org.jetbrains.kotlin.gradle.newTests.TestFeature
 import org.jetbrains.kotlin.gradle.newTests.writeAccess
 
 object FilterModulesTestFeature : TestFeature<ModulesFilteringConfiguration> {
     override fun renderConfiguration(configuration: ModulesFilteringConfiguration): List<String> = buildList {
-        if (configuration.excludedModuleNames != null) add("hiding source modules: ${configuration.excludedModuleNames!!}")
-        if (configuration.includedModuleNames != null) add("showing only source modules: ${configuration.includedModuleNames!!}")
+        if (configuration.excludedModuleNames != null) add("hiding source modules matching ${configuration.excludedModuleNames!!}")
+        if (configuration.includedModuleNames != null) add("showing only source modules matching ${configuration.includedModuleNames!!}")
 
         if (configuration.hideTestModules) add("hiding test source modules")
         if (configuration.hideProductionModules) add("hiding production source modules")
@@ -22,8 +23,8 @@ class ModulesFilteringConfiguration {
     // includedModuleNames == emptySet() -> don't include anything (hide all modules)
     //
     // those two modes are mutually exclusive: if one isn't null, then other must be null (ensured in [FilterModulesSupport])
-    var excludedModuleNames: Set<String>? = null
-    var includedModuleNames: Set<String>? = null
+    var excludedModuleNames: Regex? = null
+    var includedModuleNames: Regex? = null
 
     var hideTestModules: Boolean = false
     var hideProductionModules: Boolean = false
@@ -33,15 +34,15 @@ private val TestConfigurationDslScope.config: ModulesFilteringConfiguration
     get() = writeAccess.getConfiguration(FilterModulesTestFeature)
 
 interface ModulesFilteringDsl {
-    fun TestConfigurationDslScope.onlyModules(vararg names: String) {
+    fun TestConfigurationDslScope.onlyModules(@Language("Regex") regex: String) {
         val config = config
         require(config.excludedModuleNames == null) { "'onlyModules' is mutually exclusive with 'exceptModules'" }
-        config.includedModuleNames = names.toSet()
+        config.includedModuleNames = regex.toRegex()
     }
 
-    fun TestConfigurationDslScope.exceptModules(vararg names: String) {
+    fun TestConfigurationDslScope.exceptModules(@Language("Regex") regex: String) {
         val config = config
         require(config.includedModuleNames == null) { "'onlyModules' is mutually exclusive with 'exceptModules'" }
-        config.excludedModuleNames = names.toSet()
+        config.excludedModuleNames = regex.toRegex()
     }
 }
--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/newTests/testFeatures/OrderEntriesFilteringTestFeature.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/newTests/testFeatures/OrderEntriesFilteringTestFeature.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.gradle.newTests.testFeatures
 
+import org.intellij.lang.annotations.Language
 import org.jetbrains.kotlin.gradle.newTests.TestConfigurationDslScope
 import org.jetbrains.kotlin.gradle.newTests.TestFeature
 import org.jetbrains.kotlin.gradle.newTests.writeAccess
@@ -15,16 +16,22 @@ object OrderEntriesFilteringTestFeature : TestFeature<OrderEntriesFilteringConfi
             if (configuration.hideSelfDependency) add ("self")
         }
 
-        return if (hiddenStandardDependencies.isNotEmpty())
-            listOf("hiding following standard dependencies: ${hiddenStandardDependencies.joinToString()}")
-        else
-            emptyList()
+        return buildList {
+            if (hiddenStandardDependencies.isNotEmpty())
+                add("hiding following standard dependencies: ${hiddenStandardDependencies.joinToString()}")
+
+            if (configuration.excludeDependenciesRegex != null) {
+                add("hiding dependencies matching ${configuration.excludeDependenciesRegex.toString()}")
+            }
+        }
     }
 
     override fun createDefaultConfiguration(): OrderEntriesFilteringConfiguration = OrderEntriesFilteringConfiguration()
 }
 
 class OrderEntriesFilteringConfiguration {
+    var excludeDependenciesRegex: Regex? = null
+
     var hideStdlib: Boolean = false
     var hideKotlinTest: Boolean = false
     var hideKonanDist: Boolean = false
@@ -49,4 +56,8 @@ interface OrderEntriesFilteringSupport {
     var TestConfigurationDslScope.hideKotlinNativeDistribution
         get() = config.hideKonanDist
         set(value) { config.hideKonanDist = value }
+
+    fun TestConfigurationDslScope.excludeDependenciesRegex(@Language("Regex") regex: String) {
+        config.excludeDependenciesRegex = regex.toRegex()
+    }
 }
