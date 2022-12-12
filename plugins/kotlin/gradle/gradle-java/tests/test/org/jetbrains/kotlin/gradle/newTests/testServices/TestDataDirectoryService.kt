3df5089f6d475834d8cdb6ff30fb9a6f7081823f
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2022-12-12 12:26:49 +0300

    [kt, tests] Support @TestDataPath and @TestMetadata
    ^KT-55275 Fixed
    
    (cherry picked from commit 662b1fe03c08c50aafae27958870d2a85901686a)
    
    GitOrigin-RevId: 5b7839846ef321eede49210d3e9b58d3c380e0fc

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/newTests/testServices/TestDataDirectoryService.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/newTests/testServices/TestDataDirectoryService.kt
@@ -1,52 +1,114 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.gradle.newTests.testServices
 
-import org.jetbrains.kotlin.idea.test.IDEA_TEST_DATA_DIR
+import com.intellij.testFramework.TestDataPath
+import com.intellij.util.io.exists
+import com.intellij.util.text.nullize
+import org.jetbrains.kotlin.idea.base.test.KotlinRoot
+import org.jetbrains.kotlin.test.TestMetadata
+import org.jetbrains.kotlin.tooling.core.withLinearClosure
 import org.jetbrains.kotlin.util.capitalizeDecapitalize.decapitalizeAsciiOnly
 import org.junit.runner.Description
 import java.io.File
+import kotlin.io.path.Path
 
 /**
- * Rule which provides a corresponding test data directory via [testDataDirectory]
- * according to the following logic:
+ * This service provides discovery of test-related folders.
  *
- * - each test method has form like 'test<TestName>' or '<testName>'
- * - test directory for a <TestName> is expected to be found at [IDEA_TEST_DATA_DIR]/$testDataDirName/<testName>
- *   (note decapitalizing the first letter in <TestName> if necessary>
+ * General intuition:
+ * - @TestDataPath always gives the first part of the path. $PROJECT_ROOT template is supported
+ *   Tip: should be present on the common ancestors of all tests with the most
+ *   common root for all testdata (like `$PROJECT/allTests`)
+ *
+ * - @TestMetadata give subsequent parts in order from the farthest supertype to the
+ *   concrete class
+ *   Tip: allows to "inherit" test data paths. E.g. some base test class for all
+ *   Gradle-related tests can be annotated with 'gradle' (giving `$PROJECT/allTests/gradle`
+ *   together with @TestDataPath), and subsequent inheritors can add more specific paths,
+ *   e.g. 'multiplatform' for `$PROJECT/allTests/gradle/multiplatform`
+ *
+ * - Last part is either test name without 'test' or @TestMetadata on test method
+ *   Tip: use this if you want to have test method name to be different from the folder.
+ *   This is most often useful when you want to run several tests against same testdata.
+ *
+ * Such an elaborate algorithm is used mostly to comply with DevKit heuristics, which,
+ * specifically, enables 'Navigate To Testdata' actions & gutters.
  */
-class TestDataDirectoryService(
-    // relative to [IDEA_TEST_DATA_DIR]
-    private val testDataDirRootPath: String
-) : KotlinBeforeAfterTestRuleWithDescription {
-    private var testMethodName: String? = null
+class TestDataDirectoryService : KotlinBeforeAfterTestRuleWithDescription {
+    private var testDescription: Description? = null
 
     fun testDataDirectory(): File {
-        val testMethodName = requireNotNull(testMethodName) {
-            "Test method name is null, probably 'before' for this test isn't executed yet"
+        val description = requireNotNull(testDescription) {
+            "Test Description is null, probably 'before' hasn't been called for this test yet"
         }
-        val testName = if (testMethodName.startsWith("test")) 
-            testMethodName.removePrefix("test").decapitalizeAsciiOnly()
-        else
-            testMethodName
-        
-        val allTestDataRootDir = IDEA_TEST_DATA_DIR.resolve(testDataDirRootPath)
-        val thisTestData = File(allTestDataRootDir, testName)
-        
-        require(thisTestData.exists()) {
+        require(description.methodName.startsWith("test")) {
+            "Test method names are expected to start with 'test', actual name: ${description.methodName}\n" +
+                    "Please add 'test' prefix, as it helps DevKit to work (e.g. provide " +
+                    "Navigate to Test Data gutters)"
+        }
+
+        val testDataPathAnnotationValue = getTestDataPathAnnotationValueWithOwner(description)
+
+        val testMetadataOnClassesFromCurrentToSuper = description.testClass.withLinearClosure { it.superclass }
+            .mapNotNull { clazz -> clazz.getAnnotation(TestMetadata::class.java)?.value?.let { it to clazz  } }
+
+        val testFolderName = getTestFolderName(description)
+
+        val testFolderFullPath = Path(
+            testDataPathAnnotationValue.first,
+            *testMetadataOnClassesFromCurrentToSuper.asReversed().map { it.first }.toTypedArray(),
+            testFolderName
+        )
+
+        require(testFolderFullPath.exists()) {
+            val testMetadataValuesRenderedWithOwners = testMetadataOnClassesFromCurrentToSuper.joinToString(separator = "\n") { (value, clazz) ->
+                "value = $value, found on ${clazz.name}"
+            }.nullize()
             """Can't find test data directory
-                | testMethodName = $testMethodName
-                | testName = $testName
-                | IDEA_TEST_DATA_DIR = ${IDEA_TEST_DATA_DIR.canonicalPath}
-                | testDataDirRootPath = ${testDataDirRootPath}
-                | allTestDataRootDir = ${allTestDataRootDir.canonicalPath}
-                | thisTestDataDir = ${thisTestData.canonicalPath}  
+                | Full path = ${testFolderFullPath.toFile().canonicalPath}
+                |
+                | @TestDataPath.value = ${testDataPathAnnotationValue.first}
+                |     found on: ${testDataPathAnnotationValue.second.name}
+                |     
+                | @TestMetadata on classes (from current to super):
+                | ${testMetadataValuesRenderedWithOwners ?: "not found"} 
+                | 
+                | testFolderName = ${testFolderName}
+                | testMethodName = ${description.methodName}
             """.trimMargin()
         }
 
-        return thisTestData
+        return testFolderFullPath.toFile()
+    }
+
+    private fun getTestDataPathAnnotationValueWithOwner(description: Description): Pair<String, Class<*>> {
+        val result = generateSequence(description.testClass) { it.superclass }
+            .firstNotNullOfOrNull { clazz -> clazz.getAnnotation(TestDataPath::class.java)?.value?.let { it to clazz } }
+
+        requireNotNull(result) {
+            "@TestDataPath annotation is expected to be present either on the class itself, or one the one of the superclasses"
+        }
+
+        require(!result.first.contains("\$CONTENT_ROOT")) {
+            "\$CONTENT_ROOT pattern in @TestDataPath is not supported yet in MPP Test suites\n" +
+                    "Please, use \$PROJECT_ROOT instead"
+        }
+
+        return result.first.replace("\$PROJECT_ROOT", KotlinRoot.REPO.canonicalPath) to result.second
+    }
+
+    private fun getTestFolderName(description: Description): String {
+        val testMetadataOnMethod = description.getAnnotation(TestMetadata::class.java)?.value
+        return testMetadataOnMethod ?: description.methodName.removePrefix("test").decapitalizeAsciiOnly()
     }
 
     override fun before(description: Description) {
-        testMethodName = description.methodName
+        testDescription = description
+    }
+
+    override fun after(description: Description) {
+        // to be extra safe and prevent following tests accidentally using 'description'
+        // from the previous one
+        testDescription = null
     }
 }
