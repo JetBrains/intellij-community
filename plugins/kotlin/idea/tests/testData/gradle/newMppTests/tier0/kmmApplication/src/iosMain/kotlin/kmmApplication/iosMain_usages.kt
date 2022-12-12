7427cb03a343aecade659cf1e74d1112af9f6206
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2022-12-12 18:07:56 +0300

    [kt, tests] Introduce Tier Zero MPP Test cases
    ^KT-55278
    
    (cherry picked from commit 9f58993d1912a1523676516e75e7ded38847bc4c)
    
    GitOrigin-RevId: 715992d6a58366f4c264873da751bc051d7ede32

---
+++ plugins/kotlin/idea/tests/testData/gradle/newMppTests/tier0/kmmApplication/src/iosMain/kotlin/kmmApplication/iosMain_usages.kt
@@ -0,0 +1,21 @@
+package kmmApplication
+
+import platform.UIKit.UIDevice
+
+fun use() {
+    // refines = internal visibility
+    commonMainInternal()
+
+    // non-expect declaration from common are refined
+    produceCommonMainExpect()?.iosSpecificApi()
+
+    // no incompatible descriptors
+    consumeCommonMainExpect(CommonMainExpect())
+
+    // Kotlin/Native stdlib is imported and usable
+    val x: CpuArchitecture = CpuArchitecture.ARM64
+
+    // iOS libraries are commonized, imported and usable
+    // NB: Expected to be unresolved on non-Apple hosts
+    val name: String = UIDevice.currentDevice.systemName()
+}
