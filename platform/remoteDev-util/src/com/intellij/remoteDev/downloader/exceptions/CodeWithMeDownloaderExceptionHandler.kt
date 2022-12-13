691ef72821124357f9e49821faedab2757a258cb
Sasha Blashenkov <Sasha.Blashenkov@jetbrains.com>
2022-12-14 00:20:50 +0100

    Show Learn More link for banned cwm users
    GitOrigin-RevId: c78ca498d91455023b1500f22cedbf77bf53380e

--- platform/remoteDev-util/src/com/intellij/remoteDev/downloader/exceptions/CodeWithMeDownloaderExceptionHandler.kt
+++ platform/remoteDev-util/src/com/intellij/remoteDev/downloader/exceptions/CodeWithMeDownloaderExceptionHandler.kt
@@ -1,7 +1,9 @@
 package com.intellij.remoteDev.downloader.exceptions
 
+import com.intellij.ide.BrowserUtil
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.ui.messages.MessagesService
 import com.intellij.openapi.util.NlsContexts.DialogTitle
 import com.intellij.remoteDev.RemoteDevUtilBundle
 import com.intellij.util.application
@@ -17,10 +19,28 @@ object CodeWithMeDownloaderExceptionHandler {
   }
 
   private fun handle(@DialogTitle product: String, t: CodeWithMeUnavailableException) {
-    Messages.showErrorDialog(
-      t.message ?: RemoteDevUtilBundle.message("error.not.available", product),
-      RemoteDevUtilBundle.message("error.not.available", product)
-    )
+    val message = StringBuilder().apply {
+      append(t.message ?: RemoteDevUtilBundle.message("error.not.available", product))
+      if (t.reason != null) {
+        append("\n")
+        append(t.reason)
+      }
+    }.toString() // NON-NLS, the message comes from the server
+
+    val buttons = if (t.learnMoreLink != null) arrayOf(Messages.getOkButton(), RemoteDevUtilBundle.message("error.learn.more"))
+      else arrayOf(Messages.getOkButton())
+
+    val result = MessagesService
+      .getInstance()
+      .showMessageDialog(null, null,
+                         message, RemoteDevUtilBundle.message("error.not.available", product),
+                         buttons,
+                         0, 0, Messages.getErrorIcon(),
+                         null, false, null)
+
+    if (buttons.size == 2 && result == 1 && t.learnMoreLink != null) {
+      BrowserUtil.browse(t.learnMoreLink)
+    }
   }
 
   private fun handleDefault(@DialogTitle product: String, t: Throwable) {
