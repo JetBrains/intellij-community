b6498793f7d6c1839b2cc1280b5915ec550c560e
Roland Illig <roland.illig@jetbrains.com>
2022-12-14 13:28:20 +0100

    Convert package documentation for openapi.roots to Java
    Move the documentation to the platform module, as the documentation is not specific to Java.
    
    GitOrigin-RevId: 787509ad873a402c9b52c0b7210fd60c5b2a87d8

---
+++ platform/core-api/src/com/intellij/openapi/roots/package-info.java
@@ -0,0 +1,6 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+/**
+ * Provides interfaces for defining the structure and contents of projects and modules
+ * and the libraries used in them.
+ */
+package com.intellij.openapi.roots;
