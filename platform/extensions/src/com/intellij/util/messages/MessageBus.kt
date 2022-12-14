1e3929740ef5bf82075c6a599e37415b8a9b428d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-12-14 18:28:10 +0100

    Rename .java to .kt
    GitOrigin-RevId: 40a6eb7f30ec02ca0d21a68979342baa3238987b

--- platform/extensions/src/com/intellij/util/messages/MessageBus.kt
+++ platform/extensions/src/com/intellij/util/messages/MessageBus.kt
@@ -1,10 +1,11 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.messages;
 
 import com.intellij.openapi.Disposable;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import sun.jvm.hotspot.code.CompressedStream.L
 
 /**
  * Core of IntelliJ Platform messaging infrastructure. Basic functions:
