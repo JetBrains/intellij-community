f48e14572d14db4edc8c6d5c18a636ff27aeee24
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-12-15 16:44:09 +0100

    [KT-55492] Support new IdeaKotlinClasspath
    (cherry picked from commit 7931e6f2fe13d3d8977003694c0f738de807d7e1)
    
    GitOrigin-RevId: beb0988842ec2ebad847ef6e20fc334c381aff3c

--- plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/mpp/IdeaKotlinDependencyExtensions.kt
+++ plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/mpp/IdeaKotlinDependencyExtensions.kt
@@ -1,26 +1,9 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.gradleJava.configuration.mpp
 
-import com.intellij.openapi.externalSystem.model.project.LibraryPathType
 import org.jetbrains.kotlin.gradle.idea.tcs.IdeaKotlinBinaryDependency
-import org.jetbrains.kotlin.gradle.idea.tcs.IdeaKotlinDependency
-import org.jetbrains.kotlin.gradle.idea.tcs.IdeaKotlinResolvedBinaryDependency
 import org.jetbrains.kotlin.gradle.idea.tcs.IdeaKotlinSourceDependency
 
 val IdeaKotlinSourceDependency.kotlinSourceSetModuleId: KotlinSourceSetModuleId get() = KotlinSourceSetModuleId(coordinates)
 
 val IdeaKotlinBinaryDependency.kotlinLibraryName: KotlinLibraryName? get() = coordinates?.let(::KotlinLibraryName)
-
-internal val IdeaKotlinResolvedBinaryDependency.isClasspathType get() = this.binaryType == IdeaKotlinDependency.CLASSPATH_BINARY_TYPE
-
-internal val IdeaKotlinResolvedBinaryDependency.isSourcesType get() = this.binaryType == IdeaKotlinDependency.SOURCES_BINARY_TYPE
-
-internal val IdeaKotlinResolvedBinaryDependency.isDocumentationType get() = this.binaryType == IdeaKotlinDependency.DOCUMENTATION_BINARY_TYPE
-
-internal val IdeaKotlinResolvedBinaryDependency.libraryPathType: LibraryPathType?
-    get() = when {
-        isClasspathType -> LibraryPathType.BINARY
-        isSourcesType -> LibraryPathType.SOURCE
-        isDocumentationType -> LibraryPathType.DOC
-        else -> null
-    }
