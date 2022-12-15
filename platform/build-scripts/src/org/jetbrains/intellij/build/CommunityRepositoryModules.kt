7ac5c76618c8b0b9dce4d08252027abc8435d077
Vanya Pavlov <ivan.pavlov@jetbrains.com>
2022-12-15 17:46:21 +0200

    (IDEA-308538) ML in SE: Add kotlin module and features
    GitOrigin-RevId: 3c094a4529f53e082a00a55bc8281b50b106e761

--- platform/build-scripts/src/org/jetbrains/intellij/build/CommunityRepositoryModules.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/CommunityRepositoryModules.kt
@@ -271,6 +271,7 @@ object CommunityRepositoryModules {
       "intellij.searchEverywhereMl.yaml",
       "intellij.searchEverywhereMl.vcs",
       "intellij.searchEverywhereMl.java",
+      "intellij.searchEverywhereMl.kotlin",
     )),
     plugin("intellij.platform.testFramework.ui") { spec ->
       spec.withModuleLibrary("intellij.remoterobot.remote.fixtures", spec.mainModule, "")
