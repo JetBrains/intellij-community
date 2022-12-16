1c3354bab4e49e16df209a1570482944aabf8f4e
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2022-12-16 16:51:08 +0100

    [kt, tests] Introduce Misc. MPP Cases
    They consist of corner-cases/edge-checks/small features that don't
    require a lot of test cases and thus don't deserve separate test suite.
    Cases mostly migrated from HierarchicalMultiplatformProjectImportingTest
    and NewMultiplatformProjectImportingTest
    
    (cherry picked from commit 496044357c2634db94f80bf233422738ff3cf306)
    
    GitOrigin-RevId: c22ff5be5f7d5e71ea5d139be4b75f068080c54c

---
+++ plugins/kotlin/idea/tests/testData/gradle/newMppTests/features/misc/mppLibAndHmppConsumer/settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+	repositories {
+        {{kts_kotlin_plugin_repositories}}
+	}
+	plugins {
+		kotlin("multiplatform").version("{{kotlin_plugin_version}}")
+        id("com.android.library") version "{{android_gradle_plugin_version}}"
+	}
+}
+
+rootProject.name = "consumer"
