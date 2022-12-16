d0b4319a03a244154bcb2b3baa6500cbc07732b4
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2022-12-16 14:05:47 +0100

    settings-sync: add a warning that the settings are synced across IDEs: IDEA-308614
    Show a banner at the top of the Enable Settings Sync dialog. To ignore the default DialogWrapper insets we remove them and then emulate for the center and the south panes.
    
    (cherry picked from commit ceeb64cd6a9ae2a8171aafefb1c113679afe312d)
    
    IJ-CR-100337
    
    GitOrigin-RevId: d7bf71619a3cf29f95fcb53f25c60162feb564f6

--- plugins/settings-sync/src/com/intellij/settingsSync/config/SettingsSyncConfigurable.kt
+++ plugins/settings-sync/src/com/intellij/settingsSync/config/SettingsSyncConfigurable.kt
@@ -131,6 +131,12 @@ internal class SettingsSyncConfigurable : BoundConfigurable(message("title.setti
           }
           .onReset { categoriesPanel.reset() }
           .onIsModified { categoriesPanel.isModified() }
+        bottomGap(BottomGap.MEDIUM)
+      }
+      row {
+          label(message("settings.cross.ide.sync.warning.label"))
+            .apply { component.icon = AllIcons.General.Information }
+            .visibleIf(LoggedInPredicate().and(EnabledPredicate()))
       }
     }
     SettingsSyncAuthService.getInstance().addListener(object : SettingsSyncAuthService.Listener {
