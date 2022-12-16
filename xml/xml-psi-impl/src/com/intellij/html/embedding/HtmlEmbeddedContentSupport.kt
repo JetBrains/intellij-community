3f7e34a0d463b5786a3acea47d0ae03fb11a2661
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2022-12-16 18:06:39 +0100

    [html] remove @NotNull from .kt file
    GitOrigin-RevId: e4a450746e0620d84d77e9f98b4686724f1d0e81

--- xml/xml-psi-impl/src/com/intellij/html/embedding/HtmlEmbeddedContentSupport.kt
+++ xml/xml-psi-impl/src/com/intellij/html/embedding/HtmlEmbeddedContentSupport.kt
@@ -14,7 +14,6 @@ import com.intellij.openapi.extensions.ExtensionPoint
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.psi.tree.IElementType
 import org.jetbrains.annotations.ApiStatus
-import org.jetbrains.annotations.NotNull
 import org.jetbrains.annotations.TestOnly
 import java.util.stream.Stream
 
@@ -31,7 +30,7 @@ interface HtmlEmbeddedContentSupport {
     @ApiStatus.Internal
     val EP_NAME: ExtensionPointName<HtmlEmbeddedContentSupport> = ExtensionPointName("com.intellij.html.embeddedContentSupport")
 
-    fun getContentSupports(): @NotNull Stream<HtmlEmbeddedContentSupport> {
+    fun getContentSupports(): Stream<HtmlEmbeddedContentSupport> {
       return EP_NAME.extensionList.stream()
     }
 
