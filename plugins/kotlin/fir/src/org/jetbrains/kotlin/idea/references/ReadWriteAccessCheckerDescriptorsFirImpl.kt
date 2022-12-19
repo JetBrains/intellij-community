227e4c26f3bf27ff59d198147dbff4fc375350e1
Anna Kozlova <anna.kozlova@jetbrains.com>
2022-12-19 14:14:13 +0100

    [kotlin] detect read write access by operator assignment
    (part of KTIJ-21662)
    
    GitOrigin-RevId: a68edfe0a90743960d00a953dec62f2fa4a3b333

--- plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/references/ReadWriteAccessCheckerDescriptorsFirImpl.kt
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/references/ReadWriteAccessCheckerDescriptorsFirImpl.kt
@@ -1,15 +1,21 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.references
 
+import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtBinaryExpression
 import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.resolve.references.ReferenceAccess
+import org.jetbrains.kotlin.types.expressions.OperatorConventions
 
-//TODO Make valid FIR implementation
 class ReadWriteAccessCheckerDescriptorsFirImpl : ReadWriteAccessChecker {
     override fun readWriteAccessWithFullExpressionByResolve(assignment: KtBinaryExpression): Pair<ReferenceAccess, KtExpression>? {
-        // TODO
-        return null
+        val function = assignment.operationReference.mainReference.resolve() as? KtNamedFunction ?: return ReferenceAccess.READ_WRITE to assignment
+        val name = function.name ?: return ReferenceAccess.READ_WRITE to assignment
+        return if (Name.identifier(name) in OperatorConventions.ASSIGNMENT_OPERATIONS.values)
+            ReferenceAccess.READ to assignment
+        else
+        ReferenceAccess.READ_WRITE to assignment
     }
 }
