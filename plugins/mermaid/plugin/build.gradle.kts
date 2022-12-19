aa32def48fae822eff8a441cfdb4c6c5cdeb1637
Ivan Posti <ivan.posti@jetbrains.com>
2022-12-19 17:40:13 +0100

    Set correct paths for readme and changelog files
    GitOrigin-RevId: 0799c8fba7a6d4afd2e507f97d58d8430d8fa636

--- plugins/mermaid/plugin/build.gradle.kts
+++ plugins/mermaid/plugin/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.intellij.tasks.RunIdeTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -140,30 +141,36 @@ tasks {
         purgeOldFiles.set(true)
     }
 
-//    patchPluginXml {
-//        version.set(properties("pluginVersion"))
-//        sinceBuild.set(properties("pluginSinceBuild"))
-//        untilBuild.set(properties("pluginUntilBuild"))
-//
-//        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-//        pluginDescription.set(
-//            projectDir.resolve("README.md").readText().lines().run {
-//                val start = "<!-- Plugin description -->"
-//                val end = "<!-- Plugin description end -->"
-//
-//                if (!containsAll(listOf(start, end))) {
-//                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-//                }
-//                subList(indexOf(start) + 1, indexOf(end))
-//            }.joinToString("\n").run { markdownToHTML(this) }
-//        )
-//
-//        changeNotes.set(provider {
-//            changelog.run {
-//                getOrNull(properties("pluginVersion")) ?: getLatest()
-//            }.toHTML()
-//        })
-//    }
+   patchPluginXml {
+       version.set(properties("pluginVersion"))
+       sinceBuild.set(properties("pluginSinceBuild"))
+       untilBuild.set(properties("pluginUntilBuild"))
+
+       val readme = project.rootProject.projectDir.resolve("README.md")
+       // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+       pluginDescription.set(
+           readme.readText().lines().run {
+               val start = "<!-- Plugin description -->"
+               val end = "<!-- Plugin description end -->"
+
+               if (!containsAll(listOf(start, end))) {
+                   throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+               }
+               subList(indexOf(start) + 1, indexOf(end))
+           }.joinToString("\n").run { markdownToHTML(this) }
+       )
+
+       changeNotes.set(provider {
+           changelog.run {
+               getOrNull(properties("pluginVersion")) ?: getLatest()
+           }.toHTML()
+       })
+   }
+
+    changelog {
+        val changelogPath = project.rootProject.projectDir.resolve("CHANGELOG.md")
+        path.set(changelogPath.toString())
+    }
 
     runIdeForUiTests {
         systemProperty("robot-server.port", "8082")
