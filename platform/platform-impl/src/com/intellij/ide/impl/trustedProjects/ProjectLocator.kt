08e0ac6b08a18abe5633fa91428a40727eb9bf7d
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2022-12-19 20:16:58 +0100

    [cleanup] added documentation for ProjectLocator
    GitOrigin-RevId: 8b2f3a5c2b4bd9152afedd28ed3344e72dea2038

--- platform/platform-impl/src/com/intellij/ide/impl/trustedProjects/ProjectLocator.kt
+++ platform/platform-impl/src/com/intellij/ide/impl/trustedProjects/ProjectLocator.kt
@@ -5,10 +5,26 @@ import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
 import java.nio.file.Path
 
+/**
+ * Locates project roots for `Trust Project` check.
+ */
 interface ProjectLocator {
 
+  /**
+   * Locates project roots for [project].
+   * This function is used for a general case on Project.isTrusted check.
+   * Note: This function can be called hundreds of times per second.
+   */
   fun getProjectRoots(project: Project): List<Path>
 
+  /**
+   * Locates project roots for an existed project which isn't opened or linked.
+   * This function is used for cases before loading or linking a new project or
+   * module (the project or modules aren't initialised).
+   *
+   * @param projectRoot is a directory to open or link a new project or module
+   * @param project current project. It isn't null for a link module case
+   */
   fun getProjectRoots(projectRoot: Path, project: Project?): List<Path>
 
   companion object {
