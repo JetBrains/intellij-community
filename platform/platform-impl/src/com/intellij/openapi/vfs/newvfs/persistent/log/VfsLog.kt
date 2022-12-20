f55451d23d1177d5fcd9ea94ce0d432f4490d222
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2022-12-20 15:17:21 +0100

    IJPL-52 version handling
    GitOrigin-RevId: b6e7d61ecd0cfbca5eb5ad77daabc0a0004c77bf

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/log/VfsLog.kt
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/log/VfsLog.kt
@@ -1,22 +1,31 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vfs.newvfs.persistent.log
 
-import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.newvfs.persistent.intercept.ConnectionInterceptor
+import com.intellij.util.io.delete
 import kotlinx.coroutines.*
 import java.nio.file.Path
 import kotlin.io.path.div
+import kotlin.io.path.forEachDirectoryEntry
 
 class VfsLog(
   private val storagePath: Path
 ) {
+  private var version by PersistentVar.integer(storagePath / "version")
+  init {
+    version.let {
+      if (it != VERSION) {
+        if (it != null) {
+          clear()
+        }
+        version = VERSION
+      }
+    }
+  }
+
   @OptIn(DelicateCoroutinesApi::class)
   private val coroutineDispatcher = newSingleThreadContext("VFS WAL dispatcher")
   private val coroutineScope = CoroutineScope(SupervisorJob() + coroutineDispatcher)
-
-  init {
-    FileUtil.ensureExists(storagePath.toFile())
-  }
 
   val interceptors = listOf<ConnectionInterceptor>(
     ContentsLogInterceptor(storagePath / "contents"),
@@ -26,5 +35,17 @@ class VfsLog(
 
   fun dispose() {
     coroutineScope.cancel("dispose")
+  }
+
+  fun clear() {
+    storagePath.forEachDirectoryEntry { child ->
+      if (child != storagePath / "version") {
+        child.delete(true)
+      }
+    }
+  }
+
+  companion object {
+    const val VERSION = 1
   }
 }
