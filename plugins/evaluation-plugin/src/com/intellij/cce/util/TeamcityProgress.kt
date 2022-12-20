cedb26f186fb1e01e1535ba0a0065dce8d19c867
Kirill.Krylov <kirill.krylov@jetbrains.com>
2022-12-20 11:29:37 +0100

    [evaluation-plugin] fix typo in TC service message
    GitOrigin-RevId: 15c549606b43d189f9449aa49cc0e594abb68712

--- plugins/evaluation-plugin/src/com/intellij/cce/util/TeamcityProgress.kt
+++ plugins/evaluation-plugin/src/com/intellij/cce/util/TeamcityProgress.kt
@@ -4,11 +4,11 @@ class TeamcityProgress(private val title: String) : Progress {
   private val cli = CommandLineProgress(title)
 
   override fun setProgress(fileName: String, text: String, fraction: Double) {
-    println("##teamcity[progressMessage '$title: ${text.trim()}'")
+    println("##teamcity[progressMessage '$title: ${text.trim()}']")
     cli.setProgress(fileName, text, fraction)
   }
 
-  override fun start() = println("##teamcity[progressStart '$title'")
-  override fun finish() = println("##teamcity[progressFinish '$title'")
+  override fun start() = println("##teamcity[progressStart '$title']")
+  override fun finish() = println("##teamcity[progressFinish '$title']")
   override fun isCanceled() = false
 }
