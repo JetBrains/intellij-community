65a365338ad16cc23762c39c7711373ded5e16e0
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2022-12-21 10:27:11 +0100

    return null as focused component from TestWindowManager
    GitOrigin-RevId: 5d4566917cb77acce72006696e7b05233dba2822

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/TestWindowManager.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/TestWindowManager.java
@@ -96,7 +96,7 @@ public final class TestWindowManager extends WindowManagerEx {
 
   @Override
   public Component getFocusedComponent(@NotNull Window window) {
-    throw new UnsupportedOperationException();
+    return null;
   }
 
   @Override
