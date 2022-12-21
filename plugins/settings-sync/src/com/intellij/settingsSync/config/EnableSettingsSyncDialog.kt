46c8b3799366f5cd10ef6b12a0390c1724bfd6f3
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2022-12-21 14:42:07 +0100

    Revert "settings-sync: add a warning that the settings are synced across IDEs: IDEA-308614"
    We don't need the warning anymore, since we have a checkbox to enable/disable cross-ide sync, which is disabled by default.
    This reverts commit ceeb64cd
    
    GitOrigin-RevId: af57e52a85788af8ee729428e6da189be71a5809

--- plugins/settings-sync/src/com/intellij/settingsSync/config/EnableSettingsSyncDialog.kt
+++ plugins/settings-sync/src/com/intellij/settingsSync/config/EnableSettingsSyncDialog.kt
@@ -1,25 +1,13 @@
 package com.intellij.settingsSync.config
 
-import com.intellij.codeInsight.hint.HintUtil
-import com.intellij.icons.AllIcons
-import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.settingsSync.SettingsSyncBundle.message
-import com.intellij.ui.IdeBorderFactory
-import com.intellij.ui.JBColor
-import com.intellij.ui.SideBorder
-import com.intellij.ui.components.JBLabel
-import com.intellij.util.ui.JBEmptyBorder
-import com.intellij.util.ui.JBUI
-import com.intellij.util.ui.UIUtil
-import com.intellij.util.ui.UIUtil.DEFAULT_HGAP
+import org.jetbrains.annotations.Nls
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 import javax.swing.Action
 import javax.swing.JComponent
-import javax.swing.SwingConstants
-import javax.swing.border.Border
 
 internal class EnableSettingsSyncDialog
   private constructor(parent: JComponent, private val remoteSettingsFound: Boolean) : DialogWrapper(parent, false) {
@@ -45,37 +33,10 @@ internal class EnableSettingsSyncDialog
     }
   }
 
-  override fun createTitlePane(): JComponent? {
-    val warningPanel =
-      JBLabel(message("settings.cross.ide.sync.warning.label"), AllIcons.General.Information, SwingConstants.LEADING)
-        .apply {
-          background = EditorColorsManager.getInstance().globalScheme.getColor(HintUtil.PROMOTION_PANE_KEY)
-          isOpaque = true
-          border = JBUI.Borders.merge(JBUI.Borders.empty(DEFAULT_HGAP),
-                                      IdeBorderFactory.createBorder(JBColor.border(), SideBorder.BOTTOM.or(SideBorder.TOP)), true)
-        }
-    return warningPanel
-  }
-
-  override fun createContentPaneBorder(): Border? {
-    return null
-  }
-
   override fun createCenterPanel(): JComponent {
     configPanel = SettingsSyncPanelFactory.createPanel(message("enable.dialog.select.what.to.sync"))
     configPanel.reset()
-
-    val centerPanel = JBUI.Panels.simplePanel(configPanel)
-    val defaultInsets = UIUtil.getRegularPanelInsets()
-    centerPanel.border = JBEmptyBorder(defaultInsets.top, defaultInsets.left, 0, defaultInsets.right)
-    return centerPanel
-  }
-
-  override fun createSouthPanel(): JComponent {
-    val centerPanel = JBUI.Panels.simplePanel(super.createSouthPanel())
-    val defaultInsets = UIUtil.getRegularPanelInsets()
-    centerPanel.border = JBEmptyBorder(0, defaultInsets.left, defaultInsets.bottom, defaultInsets.right)
-    return centerPanel
+    return configPanel
   }
 
   override fun createActions(): Array<Action> =
