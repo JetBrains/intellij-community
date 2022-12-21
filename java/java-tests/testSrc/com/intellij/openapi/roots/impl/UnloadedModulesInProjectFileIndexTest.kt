9256f38c629bec0a6f3a835ad7e454ac73c20200
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2022-12-21 14:58:17 +0100

    [refactoring] rewrite UnloadedModulesInProjectFileIndexTest to use the common infrastructure for ProjectFileIndex tests
    This is needed to verify new incremental implementation of ProjectFileIndex (IDEA-276394).
    
    GitOrigin-RevId: 9e1e90e5c33d1138da9266536444a48737319676

--- java/java-tests/testSrc/com/intellij/openapi/roots/impl/UnloadedModulesInProjectFileIndexTest.kt
+++ java/java-tests/testSrc/com/intellij/openapi/roots/impl/UnloadedModulesInProjectFileIndexTest.kt
@@ -2,51 +2,68 @@
 package com.intellij.openapi.roots.impl
 
 import com.intellij.ide.impl.runUnderModalProgressIfIsEdt
-import com.intellij.openapi.module.ModuleManager.Companion.getInstance
 import com.intellij.openapi.roots.DependencyScope
 import com.intellij.openapi.roots.ModuleRootModificationUtil
-import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.roots.impl.ProjectFileIndexScopes.NOT_IN_PROJECT
+import com.intellij.openapi.roots.impl.ProjectFileIndexScopes.assertInModule
+import com.intellij.openapi.roots.impl.ProjectFileIndexScopes.assertScope
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.testFramework.HeavyPlatformTestCase
-import com.intellij.testFramework.UsefulTestCase
-import java.io.IOException
-import java.util.*
+import com.intellij.testFramework.UsefulTestCase.assertEmpty
+import com.intellij.testFramework.UsefulTestCase.assertSameElements
+import com.intellij.testFramework.junit5.RunInEdt
+import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.testFramework.rules.ProjectModelExtension
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
 
-class UnloadedModulesInProjectFileIndexTest : DirectoryIndexTestCase() {
-  @Throws(IOException::class)
-  fun testUnloadedModule() {
-    val unloadedModule = createModule("unloaded")
-    val root = createTempDirectory()
-    val contentRoot = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(root)
-    ModuleRootModificationUtil.addContentRoot(unloadedModule, contentRoot!!.path)
-    val file = HeavyPlatformTestCase.createChildData(contentRoot, "a.txt")
-    assertInProject(file)
+@TestApplication
+@RunInEdt
+class UnloadedModulesInProjectFileIndexTest {
+  @JvmField
+  @RegisterExtension
+  val projectModel: ProjectModelExtension = ProjectModelExtension()
+
+  private val fileIndex
+    get() = ProjectFileIndex.getInstance(projectModel.project)
+
+  private val directoryIndex
+    get() = DirectoryIndex.getInstance(projectModel.project)
+  
+  @Test
+  fun `unloaded module`() {
+    val unloadedModule = projectModel.createModule("unloaded")
+    val contentRoot = projectModel.baseProjectDir.newVirtualDirectory("content")
+    ModuleRootModificationUtil.addContentRoot(unloadedModule, contentRoot.path)
+    val file = projectModel.baseProjectDir.newVirtualFile("content/a.txt")
+    fileIndex.assertInModule(file, unloadedModule, contentRoot)
     runUnderModalProgressIfIsEdt {
-      getInstance(myProject).setUnloadedModules(Arrays.asList("unloaded"))
+      projectModel.moduleManager.setUnloadedModules(listOf("unloaded"))
     }
     assertFromUnloadedModule(file, "unloaded")
     assertFromUnloadedModule(contentRoot, "unloaded")
   }
 
-  fun testDependentUnloadedModules() {
-    val unloadedModule = createModule("unloaded")
-    val main = createModule("main")
-    val util = createModule("util")
-    val common = createModule("common")
+  @Test
+  fun `dependent unloaded modules`() {
+    val unloadedModule = projectModel.createModule("unloaded")
+    val main = projectModel.createModule("main")
+    val util = projectModel.createModule("util")
+    val common = projectModel.createModule("common")
     ModuleRootModificationUtil.addDependency(unloadedModule, main)
     ModuleRootModificationUtil.addDependency(main, util)
     ModuleRootModificationUtil.addDependency(main, common, DependencyScope.COMPILE, true)
     runUnderModalProgressIfIsEdt {
-      getInstance(myProject).setUnloadedModules(Arrays.asList("unloaded"))
+      projectModel.moduleManager.setUnloadedModules(listOf("unloaded"))
     }
-    UsefulTestCase.assertSameElements(myIndex.getDependentUnloadedModules(main), "unloaded")
-    UsefulTestCase.assertEmpty(myIndex.getDependentUnloadedModules(util))
-    UsefulTestCase.assertSameElements(myIndex.getDependentUnloadedModules(common), "unloaded")
+    assertSameElements(directoryIndex.getDependentUnloadedModules(main), "unloaded")
+    assertEmpty(directoryIndex.getDependentUnloadedModules(util))
+    assertSameElements(directoryIndex.getDependentUnloadedModules(common), "unloaded")
   }
 
   private fun assertFromUnloadedModule(file: VirtualFile, moduleName: String) {
-    assertFalse(myFileIndex.isInProject(file))
-    assertNull(myFileIndex.getModuleForFile(file))
-    assertEquals(moduleName, myIndex.getInfoForFile(file).unloadedModuleName)
+    fileIndex.assertScope(file, NOT_IN_PROJECT)
+    assertEquals(moduleName, directoryIndex.getInfoForFile(file).unloadedModuleName)
   }
 }
