6c0cb90e3ed98b12cdd13377d9945ef0e54c54f1
Ivan Semenov <ivan.semenov@jetbrains.com>
2022-12-23 13:43:41 +0100

    [collab/github] extract size restricting layout
    GitOrigin-RevId: 8e597d8e57840ad5e59a7a6c1629f64af97a569c

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/GHEditableHtmlPaneHandle.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/GHEditableHtmlPaneHandle.kt
@@ -6,29 +6,27 @@ import com.intellij.collaboration.async.CompletableFutureUtil.successOnEdt
 import com.intellij.collaboration.ui.CollaborationToolsUIUtil
 import com.intellij.collaboration.ui.codereview.comment.CommentInputActionsComponentFactory
 import com.intellij.collaboration.ui.codereview.timeline.comment.CommentInputComponentFactory
+import com.intellij.collaboration.ui.layout.SizeRestrictedSingleComponentLayout
 import com.intellij.collaboration.ui.util.swingAction
 import com.intellij.openapi.actionSystem.CommonShortcuts
 import com.intellij.openapi.keymap.KeymapUtil
 import com.intellij.openapi.project.Project
-import com.intellij.ui.scale.JBUIScale
-import com.intellij.util.ui.JBInsets
 import kotlinx.coroutines.flow.MutableStateFlow
 import org.jetbrains.plugins.github.i18n.GithubBundle
 import org.jetbrains.plugins.github.pullrequest.comment.ui.GHCommentTextFieldFactory
 import org.jetbrains.plugins.github.pullrequest.comment.ui.GHCommentTextFieldModel
 import org.jetbrains.plugins.github.pullrequest.comment.ui.submitAction
-import java.awt.*
+import java.awt.BorderLayout
 import java.util.concurrent.CompletableFuture
 import javax.swing.JComponent
 import javax.swing.JPanel
-import kotlin.math.min
 
 internal class GHEditableHtmlPaneHandle(private val project: Project,
                                         private val paneComponent: JComponent,
                                         private val getSourceText: () -> String,
                                         private val updateText: (String) -> CompletableFuture<out Any?>) {
 
-  private val paneLayout = SingleSizeRestrictedComponentLayout()
+  private val paneLayout = SizeRestrictedSingleComponentLayout()
 
   val panel = JPanel(null).apply {
     isOpaque = false
@@ -105,65 +103,5 @@ internal class GHEditableHtmlPaneHandle(private val project: Project,
       repaint()
     }
     editor = null
-  }
-
-  private class SingleSizeRestrictedComponentLayout : LayoutManager2 {
-    var maxWidth: Int? = null
-    var maxHeight: Int? = null
-
-    private var component: Component? = null
-
-    override fun addLayoutComponent(comp: Component?, constraints: Any?) {
-      component = comp
-    }
-
-    override fun addLayoutComponent(name: String?, comp: Component?) {
-      component = comp
-    }
-
-    override fun removeLayoutComponent(comp: Component?) {
-      if (comp == component) component = null
-    }
-
-    override fun minimumLayoutSize(parent: Container): Dimension =
-      Dimension(0, 0)
-
-    private fun getWidthRestriction(): Int = maxWidth?.let(JBUIScale::scale) ?: Int.MAX_VALUE
-    private fun getHeightRestriction(): Int = maxHeight?.let(JBUIScale::scale) ?: Int.MAX_VALUE
-
-    override fun preferredLayoutSize(parent: Container): Dimension {
-      val prefSize = component?.preferredSize ?: return Dimension(0, 0)
-
-      val prefWidth = min(prefSize.width, getWidthRestriction())
-      val prefHeight = min(prefSize.height, getHeightRestriction())
-
-      return Dimension(prefWidth, prefHeight).also {
-        JBInsets.removeFrom(it, parent.insets)
-      }
-    }
-
-    override fun maximumLayoutSize(target: Container): Dimension {
-      val maxSize = component?.maximumSize ?: return Dimension(0, 0)
-
-      val maxWidth = min(maxSize.width, getWidthRestriction())
-      val maxHeight = min(maxSize.height, getHeightRestriction())
-
-      return Dimension(maxWidth, maxHeight).also {
-        JBInsets.removeFrom(it, target.insets)
-      }
-    }
-
-    override fun layoutContainer(parent: Container) {
-      val bounds = Rectangle(0, 0, parent.width, parent.height)
-      JBInsets.removeFrom(bounds, parent.insets)
-
-      bounds.width = min(getWidthRestriction(), bounds.width)
-      bounds.height = min(getHeightRestriction(), bounds.height)
-      component?.bounds = bounds
-    }
-
-    override fun getLayoutAlignmentX(target: Container?): Float = 0f
-    override fun getLayoutAlignmentY(target: Container?): Float = 0f
-    override fun invalidateLayout(target: Container?) = Unit
   }
 }
