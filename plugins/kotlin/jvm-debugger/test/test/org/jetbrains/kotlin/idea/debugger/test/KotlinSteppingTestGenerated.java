7686b31fee7d2dd8e65c573339a354ef1dbca736
Nikita Nazarov <nikitanazarov@google.com>
2022-12-26 21:22:43 +0000

    [kotlin] make KotlinRequestHint continue stepping after the kotlin fake line number in case of smart stepping
    Consider the example: `1.let { it }.let { it + 1 }.let { it + 2 }`. Previously when trying to smart step into `{ it + 1 }` or `{ it + 2 }` the debugger would stop in `{ it }`, because during smart stepping the debugger would cross a fake line number that marks the end of an inline lambda, and KotlinRequestHint would stop the smart stepping process.
    
    ^KTIJ-24091 Fixed

--- plugins/kotlin/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/KotlinSteppingTestGenerated.java
+++ plugins/kotlin/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/KotlinSteppingTestGenerated.java
@@ -1355,6 +1355,11 @@ public abstract class KotlinSteppingTestGenerated extends AbstractKotlinStepping
             runTest("testData/stepping/custom/smartStepIntoInlineFun.kt");
         }
 
+        @TestMetadata("smartStepIntoInlineLambdasOnSameLine.kt")
+        public void testSmartStepIntoInlineLambdasOnSameLine() throws Exception {
+            runTest("testData/stepping/custom/smartStepIntoInlineLambdasOnSameLine.kt");
+        }
+
         @TestMetadata("smartStepIntoInlinedFunLiteral.kt")
         public void testSmartStepIntoInlinedFunLiteral() throws Exception {
             runTest("testData/stepping/custom/smartStepIntoInlinedFunLiteral.kt");
