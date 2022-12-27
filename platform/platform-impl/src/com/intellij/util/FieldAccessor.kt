ac9f30e24222b03852952096b0ab31a4641df138
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-12-27 08:44:50 +0100

    FieldAccessor - use method handle
    GitOrigin-RevId: d17c5c3e9137cd7c846b80f1bf2da2b2693dceda

--- platform/platform-impl/src/com/intellij/util/FieldAccessor.kt
+++ platform/platform-impl/src/com/intellij/util/FieldAccessor.kt
@@ -1,9 +1,10 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util
 
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.util.Ref
+import com.intellij.openapi.diagnostic.logger
 import org.jetbrains.annotations.NonNls
+import java.lang.invoke.MethodHandle
+import java.lang.invoke.MethodHandles
 import java.lang.reflect.Field
 
 /**
@@ -12,64 +13,76 @@ import java.lang.reflect.Field
  * @param <E> the type of the field's class
  * @param <T> the type of the field
 </T></E> */
-class FieldAccessor<E, T> @JvmOverloads constructor(private val aClass: Class<E>,
-                                                    private val name: @NonNls String,
-                                                    private val type: Class<T>? = null) {
-  private var fieldRef: Ref<Field?>? = null
-  val isAvailable: Boolean
-    get() {
-      if (fieldRef == null) {
-        fieldRef = Ref()
-        var result: Field? = null
-        run {
-          var aClass: Class<*>? = this.aClass
-          while (aClass != null) {
-            for (candidate in aClass.declaredFields) {
-              if (name == candidate.name && (type == null || type.isAssignableFrom(candidate.type))) {
-                candidate.isAccessible = true
-                result = candidate
-                break
-              }
-            }
-            aClass = aClass.superclass
-          }
-        }
-        if (result == null) {
-          LOG.warn("Field not found: " + aClass.name + "." + name)
-          return false
-        }
-        fieldRef!!.set(result)
-        fieldRef!!.get()!!.isAccessible = true
+class FieldAccessor<E, T>(private val aClass: Class<E>,
+                          private val name: @NonNls String,
+                          private val type: Class<T>? = null) {
+  @Deprecated("Please pass type")
+  constructor(aClass: Class<E>, name: @NonNls String) : this(aClass = aClass, name = name, type = null)
+
+  private val lookup = MethodHandles.privateLookupIn(aClass, MethodHandles.lookup())
+
+  private val getter: MethodHandle? by lazy {
+    if (type == null) {
+      field?.let {
+        lookup.unreflectGetter(field)
       }
-      return fieldRef!!.get() != null
     }
+    else {
+      try {
+        lookup.findGetter(aClass, name, type)
+      }
+      catch (e: NoSuchFieldException) {
+        warnFieldNotFound()
+        null
+      }
+    }
+  }
+
+  private val setter: MethodHandle? by lazy {
+    if (type == null) {
+      field?.let {
+        lookup.unreflectSetter(field)
+      }
+    }
+    else {
+      try {
+        lookup.findSetter(aClass, name, type)
+      }
+      catch (e: NoSuchFieldException) {
+        warnFieldNotFound()
+        null
+      }
+    }
+  }
+
+  private val field: Field? by lazy {
+    var aClass: Class<*>? = this.aClass
+    while (aClass != null) {
+      for (candidate in aClass.declaredFields) {
+        if (name == candidate.name && (type == null || type.isAssignableFrom(candidate.type))) {
+          candidate.isAccessible = true
+          return@lazy candidate
+        }
+      }
+      aClass = aClass.superclass
+    }
+    warnFieldNotFound()
+    null
+  }
+
+  val isAvailable: Boolean
+    get() = this.field != null
 
   operator fun get(`object`: E?): T? {
-    if (!isAvailable) {
-      return null
-    }
-    try {
-      return fieldRef!!.get()!!.get(`object`) as T
-    }
-    catch (e: IllegalAccessException) {
-      LOG.warn("Field not accessible: " + aClass.name + "." + name)
-    }
-    return null
+    @Suppress("UNCHECKED_CAST")
+    return (getter ?: return null).invoke(`object`) as T
   }
 
   operator fun set(`object`: E?, value: T?) {
-    if (!isAvailable) {
-      return
-    }
-    try {
-      fieldRef!!.get()!![`object`] = value
-    }
-    catch (e: IllegalAccessException) {
-      LOG.warn("Field not accessible: " + aClass.name + "." + name)
-    }
+    (setter ?: return).invoke(`object`, value)
   }
 
-  companion object {
-    private val LOG = Logger.getInstance(FieldAccessor::class.java)
+  private fun warnFieldNotFound() {
+    logger<FieldAccessor<*, *>>().warn("Field not found: ${this.aClass.name}.$name")
   }
 }
