17412a591b3812ced5ea69a2c64606406b071ef5
Leonid Shalupov <leonid@shalupov.com>
2022-12-27 19:32:40 +0100

    FileDropHandler: rewrite in kotlin
    ref IDEA-307607 editor dragndrop
    
    GitOrigin-RevId: 8c5e54a438048434e26038996c031039f69c566d

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/FileDropHandler.kt
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/FileDropHandler.kt
@@ -1,101 +1,76 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.fileEditor.impl.text;
+package com.intellij.openapi.fileEditor.impl.text
 
-import com.intellij.ide.dnd.FileCopyPasteUtil;
-import com.intellij.ide.util.PsiNavigationSupport;
-import com.intellij.openapi.editor.CustomFileDropHandler;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.EditorDropHandler;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.fileEditor.FileEditor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.TextEditor;
-import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx;
-import com.intellij.openapi.fileEditor.impl.EditorComposite;
-import com.intellij.openapi.fileEditor.impl.EditorWindow;
-import com.intellij.openapi.fileEditor.impl.FileEditorOpenOptions;
-import com.intellij.openapi.fileEditor.impl.NonProjectFileWritingAccessProvider;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.ide.dnd.FileCopyPasteUtil
+import com.intellij.ide.util.PsiNavigationSupport
+import com.intellij.openapi.editor.CustomFileDropHandler
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.EditorDropHandler
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.TextEditor
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
+import com.intellij.openapi.fileEditor.impl.EditorWindow
+import com.intellij.openapi.fileEditor.impl.FileEditorOpenOptions
+import com.intellij.openapi.fileEditor.impl.NonProjectFileWritingAccessProvider
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.util.containers.ContainerUtil
+import java.awt.datatransfer.DataFlavor
+import java.awt.datatransfer.Transferable
+import java.io.File
 
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
-import java.io.File;
-import java.util.Collections;
-import java.util.List;
-
-public class FileDropHandler implements EditorDropHandler {
-  private final Editor myEditor;
-
-  public FileDropHandler(Editor editor) {
-    myEditor = editor;
+open class FileDropHandler(private val myEditor: Editor?) : EditorDropHandler {
+  override fun canHandleDrop(transferFlavors: Array<DataFlavor>): Boolean {
+    return FileCopyPasteUtil.isFileListFlavorAvailable(transferFlavors)
   }
 
-  @Override
-  public boolean canHandleDrop(final DataFlavor[] transferFlavors) {
-    return transferFlavors != null && FileCopyPasteUtil.isFileListFlavorAvailable(transferFlavors);
-  }
-
-  @Override
-  public void handleDrop(@NotNull final Transferable t, @Nullable final Project project, EditorWindow editorWindow) {
+  override fun handleDrop(t: Transferable, project: Project?, editorWindow: EditorWindow?) {
     if (project != null) {
-      final List<File> fileList = FileCopyPasteUtil.getFileList(t);
+      val fileList = FileCopyPasteUtil.getFileList(t)
       if (fileList != null) {
-        boolean dropResult = ContainerUtil.process(CustomFileDropHandler.CUSTOM_DROP_HANDLER_EP.getExtensions(project),
-                                                   handler -> !(handler.canHandle(t, myEditor) && handler.handleDrop(t, myEditor, project)));
-        if (!dropResult) return;
-
-        openFiles(project, fileList, editorWindow);
+        val dropResult = ContainerUtil.process(CustomFileDropHandler.CUSTOM_DROP_HANDLER_EP.getExtensions(project)
+        ) { handler: CustomFileDropHandler -> !(handler.canHandle(t, myEditor) && handler.handleDrop(t, myEditor, project)) }
+        if (!dropResult) return
+        openFiles(project, fileList, editorWindow)
       }
     }
   }
 
-  private void openFiles(final Project project, final List<? extends File> fileList, EditorWindow editorWindow) {
-    if (editorWindow == null && myEditor != null) {
-      editorWindow = findEditorWindow(project);
-    }
-    final LocalFileSystem fileSystem = LocalFileSystem.getInstance();
-    for (File file : fileList) {
-      final VirtualFile vFile = fileSystem.refreshAndFindFileByIoFile(file);
-      final FileEditorManagerEx fileEditorManager = (FileEditorManagerEx) FileEditorManager.getInstance(project);
+  private fun openFiles(project: Project, fileList: List<File>, editorWindowCandidate: EditorWindow?) {
+    val editorWindow = editorWindowCandidate ?: findEditorWindow(project)
+
+    val fileSystem = LocalFileSystem.getInstance()
+    for (file in fileList) {
+      val vFile = fileSystem.refreshAndFindFileByIoFile(file)
+      val fileEditorManager = FileEditorManager.getInstance(project) as FileEditorManagerEx
       if (vFile != null) {
-        NonProjectFileWritingAccessProvider.allowWriting(Collections.singletonList(vFile));
-
+        NonProjectFileWritingAccessProvider.allowWriting(listOf(vFile))
         if (editorWindow != null) {
-          var pair = fileEditorManager.openFile(vFile, editorWindow, new FileEditorOpenOptions().withRequestFocus());
-          if (pair.getAllEditors().size() > 0) {
-            continue;
+          val pair = fileEditorManager.openFile(vFile, editorWindow, FileEditorOpenOptions().withRequestFocus())
+          if (pair.allEditors.isNotEmpty()) {
+            continue
           }
         }
-        PsiNavigationSupport.getInstance().createNavigatable(project, vFile, -1).navigate(true);
+        PsiNavigationSupport.getInstance().createNavigatable(project, vFile, -1).navigate(true)
       }
     }
   }
 
-  @Nullable
-  private EditorWindow findEditorWindow(Project project) {
-    final Document document = myEditor.getDocument();
-    final VirtualFile file = FileDocumentManager.getInstance().getFile(document);
-    if (file != null) {
-      final FileEditorManagerEx fileEditorManager = (FileEditorManagerEx) FileEditorManager.getInstance(project);
-      final EditorWindow[] windows = fileEditorManager.getWindows();
-      for (EditorWindow window : windows) {
-        final EditorComposite composite = window.getComposite(file);
-        if (composite == null) {
-          continue;
-        }
-        for (FileEditor editor : composite.getAllEditors()) {
-          if (editor instanceof TextEditor && ((TextEditor)editor).getEditor() == myEditor) {
-            return window;
-          }
+  private fun findEditorWindow(project: Project): EditorWindow? {
+    val document = myEditor?.document ?: return null
+    val file = FileDocumentManager.getInstance().getFile(document) ?: return null
+
+    val fileEditorManager = FileEditorManager.getInstance(project) as FileEditorManagerEx
+    val windows = fileEditorManager.windows
+    for (window in windows) {
+      val composite = window.getComposite(file) ?: continue
+      for (editor in composite.allEditors) {
+        if (editor is TextEditor && editor.editor === myEditor) {
+          return window
         }
       }
     }
-    return null;
+    return null
   }
 }
