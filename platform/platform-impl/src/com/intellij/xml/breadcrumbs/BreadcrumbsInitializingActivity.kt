e555dbb2eb87fa8894a298371fedb18b63bae0a3
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-12-28 14:05:03 +0100

    cleanup
    GitOrigin-RevId: 735b6edce6f1603850998cfb2d4b8f5ca8d153f0

--- platform/platform-impl/src/com/intellij/xml/breadcrumbs/BreadcrumbsInitializingActivity.kt
+++ platform/platform-impl/src/com/intellij/xml/breadcrumbs/BreadcrumbsInitializingActivity.kt
@@ -1,162 +1,149 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.xml.breadcrumbs;
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.xml.breadcrumbs
 
-import com.intellij.codeInsight.breadcrumbs.FileBreadcrumbsCollector;
-import com.intellij.ide.ui.UISettingsListener;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.ex.EditorSettingsExternalizable;
-import com.intellij.openapi.fileEditor.FileEditor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.FileEditorManagerListener;
-import com.intellij.openapi.fileEditor.TextEditor;
-import com.intellij.openapi.fileTypes.FileTypeEvent;
-import com.intellij.openapi.fileTypes.FileTypeListener;
-import com.intellij.openapi.fileTypes.FileTypeManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.VirtualFileListener;
-import com.intellij.openapi.vfs.VirtualFileManager;
-import com.intellij.openapi.vfs.VirtualFilePropertyEvent;
-import com.intellij.openapi.vfs.impl.http.HttpVirtualFile;
-import com.intellij.util.messages.MessageBusConnection;
-import com.intellij.util.ui.UIUtil;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.codeInsight.breadcrumbs.FileBreadcrumbsCollector
+import com.intellij.ide.ui.UISettingsListener
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.editor.ex.EditorSettingsExternalizable
+import com.intellij.openapi.extensions.ExtensionNotApplicableException
+import com.intellij.openapi.fileEditor.FileEditor
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.FileEditorManagerListener
+import com.intellij.openapi.fileEditor.TextEditor
+import com.intellij.openapi.fileTypes.FileTypeEvent
+import com.intellij.openapi.fileTypes.FileTypeListener
+import com.intellij.openapi.fileTypes.FileTypeManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectPostStartupActivity
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileListener
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.openapi.vfs.VirtualFilePropertyEvent
+import com.intellij.openapi.vfs.impl.http.HttpVirtualFile
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-public final class BreadcrumbsInitializingActivity implements StartupActivity.DumbAware {
-  @Override
-  public void runActivity(@NotNull Project project) {
-    if (project.isDefault() || ApplicationManager.getApplication().isHeadlessEnvironment() || project.isDisposed()) {
-      return;
+private class BreadcrumbsInitializingActivity : ProjectPostStartupActivity {
+  init {
+    if (ApplicationManager.getApplication().isHeadlessEnvironment) {
+      throw ExtensionNotApplicableException.create()
     }
+  }
 
-    MessageBusConnection connection = project.getMessageBus().connect();
-    connection.subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new MyFileEditorManagerListener());
-    connection.subscribe(FileTypeManager.TOPIC, new FileTypeListener() {
-      @Override
-      public void fileTypesChanged(@NotNull FileTypeEvent event) {
-        reinitBreadcrumbsInAllEditors(project);
+  override suspend fun execute(project: Project) {
+    val connection = project.messageBus.simpleConnect()
+    connection.subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, MyFileEditorManagerListener())
+    connection.subscribe(FileTypeManager.TOPIC, object : FileTypeListener {
+      override fun fileTypesChanged(event: FileTypeEvent) {
+        reinitBreadcrumbsInAllEditors(project)
       }
-    });
-    FileBreadcrumbsCollector.EP_NAME.getPoint(project).addChangeListener(() -> reinitBreadcrumbsInAllEditors(project), project);
-
-    VirtualFileManager.getInstance().addVirtualFileListener(new MyVirtualFileListener(project), project);
-    connection.subscribe(UISettingsListener.TOPIC, uiSettings -> reinitBreadcrumbsInAllEditors(project));
-
-    UIUtil.invokeLaterIfNeeded(() -> reinitBreadcrumbsInAllEditors(project));
-  }
-
-  private static final class MyFileEditorManagerListener implements FileEditorManagerListener {
-    @Override
-    public void fileOpened(@NotNull final FileEditorManager source, @NotNull final VirtualFile file) {
-      reinitBreadcrumbsComponent(source, file);
+    })
+    FileBreadcrumbsCollector.EP_NAME.getPoint(project).addChangeListener({ reinitBreadcrumbsInAllEditors(project) }, project)
+    VirtualFileManager.getInstance().addVirtualFileListener(MyVirtualFileListener(project), project)
+    connection.subscribe(UISettingsListener.TOPIC, UISettingsListener { reinitBreadcrumbsInAllEditors(project) })
+    withContext(Dispatchers.EDT) {
+      reinitBreadcrumbsInAllEditors(project)
     }
   }
+}
 
-  private static class MyVirtualFileListener implements VirtualFileListener {
-    private final Project myProject;
+private fun reinitBreadcrumbsInAllEditors(project: Project) {
+  if (project.isDisposed) {
+    return
+  }
 
-    MyVirtualFileListener(@NotNull Project project) {
-      myProject = project;
-    }
+  val fileEditorManager = FileEditorManager.getInstance(project)
+  for (virtualFile in fileEditorManager.openFiles) {
+    reinitBreadcrumbsComponent(fileEditorManager, virtualFile)
+  }
+}
 
-    @Override
-    public void propertyChanged(@NotNull VirtualFilePropertyEvent event) {
-      if (VirtualFile.PROP_NAME.equals(event.getPropertyName()) && !myProject.isDisposed()) {
-        FileEditorManager fileEditorManager = FileEditorManager.getInstance(myProject);
-        VirtualFile file = event.getFile();
-        if (fileEditorManager.isFileOpen(file)) {
-          reinitBreadcrumbsComponent(fileEditorManager, file);
-        }
+private class MyFileEditorManagerListener : FileEditorManagerListener {
+  override fun fileOpened(source: FileEditorManager, file: VirtualFile) {
+    reinitBreadcrumbsComponent(source, file)
+  }
+}
+
+private class MyVirtualFileListener(private val myProject: Project) : VirtualFileListener {
+  override fun propertyChanged(event: VirtualFilePropertyEvent) {
+    if (VirtualFile.PROP_NAME == event.propertyName && !myProject.isDisposed) {
+      val fileEditorManager = FileEditorManager.getInstance(myProject)
+      val file = event.file
+      if (fileEditorManager.isFileOpen(file)) {
+        reinitBreadcrumbsComponent(fileEditorManager, file)
       }
     }
   }
+}
 
-  public static void reinitBreadcrumbsInAllEditors(@NotNull Project project) {
-    if (project.isDisposed()) return;
-    FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
-    for (VirtualFile virtualFile : fileEditorManager.getOpenFiles()) {
-      reinitBreadcrumbsComponent(fileEditorManager, virtualFile);
-    }
-  }
-
-  private static void reinitBreadcrumbsComponent(@NotNull final FileEditorManager fileEditorManager, @NotNull VirtualFile file) {
-    boolean above = EditorSettingsExternalizable.getInstance().isBreadcrumbsAbove();
-    for (FileEditor fileEditor : fileEditorManager.getAllEditors(file)) {
-      if (fileEditor instanceof TextEditor) {
-        TextEditor textEditor = (TextEditor)fileEditor;
-        Editor editor = textEditor.getEditor();
-        BreadcrumbsXmlWrapper wrapper = BreadcrumbsXmlWrapper.getBreadcrumbsWrapper(editor);
-        if (isSuitable(fileEditorManager.getProject(), textEditor, file)) {
-          if (wrapper != null) {
-            if (wrapper.breadcrumbs.above != above) {
-              remove(fileEditorManager, fileEditor, wrapper);
-              wrapper.breadcrumbs.above = above;
-              add(fileEditorManager, fileEditor, wrapper);
-            }
-            wrapper.queueUpdate();
+private fun reinitBreadcrumbsComponent(fileEditorManager: FileEditorManager, file: VirtualFile) {
+  val above = EditorSettingsExternalizable.getInstance().isBreadcrumbsAbove
+  for (fileEditor in fileEditorManager.getAllEditors(file)) {
+    if (fileEditor is TextEditor) {
+      val textEditor = fileEditor
+      val editor = textEditor.editor
+      var wrapper = BreadcrumbsXmlWrapper.getBreadcrumbsWrapper(editor)
+      if (isSuitable(fileEditorManager.project, textEditor, file)) {
+        if (wrapper != null) {
+          if (wrapper.breadcrumbs.above != above) {
+            remove(fileEditorManager, fileEditor, wrapper)
+            wrapper.breadcrumbs.above = above
+            add(fileEditorManager, fileEditor, wrapper)
           }
-          else {
-            wrapper = new BreadcrumbsXmlWrapper(editor);
-            registerWrapper(fileEditorManager, fileEditor, wrapper);
-          }
-
-          fileEditorManager.getProject().getMessageBus().syncPublisher(BreadcrumbsInitListener.TOPIC)
-            .breadcrumbsInitialized(wrapper, fileEditor, fileEditorManager);
+          wrapper.queueUpdate()
         }
-        else if (wrapper != null) {
-          disposeWrapper(fileEditorManager, fileEditor, wrapper);
+        else {
+          wrapper = BreadcrumbsXmlWrapper(editor)
+          registerWrapper(fileEditorManager, fileEditor, wrapper)
         }
+        fileEditorManager.project.messageBus.syncPublisher(BreadcrumbsInitListener.TOPIC)
+          .breadcrumbsInitialized(wrapper, fileEditor, fileEditorManager)
+      }
+      else if (wrapper != null) {
+        disposeWrapper(fileEditorManager, fileEditor, wrapper)
       }
     }
   }
+}
 
-  private static boolean isSuitable(@NotNull Project project,
-                                    @NotNull TextEditor editor,
-                                    @NotNull VirtualFile file) {
-    if (file instanceof HttpVirtualFile || !editor.isValid()) {
-      return false;
-    }
-
-    for (FileBreadcrumbsCollector collector : FileBreadcrumbsCollector.EP_NAME.getExtensions(project)) {
-      if (collector.handlesFile(file) && collector.isShownForFile(editor.getEditor(), file)) {
-        return true;
-      }
-    }
-    return false;
+private fun isSuitable(project: Project, editor: TextEditor, file: VirtualFile): Boolean {
+  if (file is HttpVirtualFile || !editor.isValid) {
+    return false
   }
-
-  private static void add(@NotNull FileEditorManager manager, @NotNull FileEditor editor, @NotNull BreadcrumbsXmlWrapper wrapper) {
-    if (wrapper.breadcrumbs.above) {
-      manager.addTopComponent(editor, wrapper);
-    }
-    else {
-      manager.addBottomComponent(editor, wrapper);
+  for (collector in FileBreadcrumbsCollector.EP_NAME.getExtensions(project)) {
+    if (collector.handlesFile(file) && collector.isShownForFile(editor.editor, file)) {
+      return true
     }
   }
+  return false
+}
 
-  private static void remove(@NotNull FileEditorManager manager, @NotNull FileEditor editor, @NotNull BreadcrumbsXmlWrapper wrapper) {
-    if (wrapper.breadcrumbs.above) {
-      manager.removeTopComponent(editor, wrapper);
-    }
-    else {
-      manager.removeBottomComponent(editor, wrapper);
-    }
+private fun add(manager: FileEditorManager, editor: FileEditor, wrapper: BreadcrumbsXmlWrapper) {
+  if (wrapper.breadcrumbs.above) {
+    manager.addTopComponent(editor, wrapper)
   }
+  else {
+    manager.addBottomComponent(editor, wrapper)
+  }
+}
 
-  private static void registerWrapper(@NotNull FileEditorManager fileEditorManager,
-                                      @NotNull FileEditor fileEditor,
-                                      @NotNull BreadcrumbsXmlWrapper wrapper) {
-    add(fileEditorManager, fileEditor, wrapper);
-    Disposer.register(fileEditor, () -> disposeWrapper(fileEditorManager, fileEditor, wrapper));
+private fun remove(manager: FileEditorManager, editor: FileEditor, wrapper: BreadcrumbsXmlWrapper) {
+  if (wrapper.breadcrumbs.above) {
+    manager.removeTopComponent(editor, wrapper)
   }
+  else {
+    manager.removeBottomComponent(editor, wrapper)
+  }
+}
 
-  private static void disposeWrapper(@NotNull FileEditorManager fileEditorManager,
-                                     @NotNull FileEditor fileEditor,
-                                     @NotNull BreadcrumbsXmlWrapper wrapper) {
-    remove(fileEditorManager, fileEditor, wrapper);
-    Disposer.dispose(wrapper);
-  }
+private fun registerWrapper(fileEditorManager: FileEditorManager, fileEditor: FileEditor, wrapper: BreadcrumbsXmlWrapper) {
+  add(fileEditorManager, fileEditor, wrapper)
+  Disposer.register(fileEditor) { disposeWrapper(fileEditorManager, fileEditor, wrapper) }
+}
+
+private fun disposeWrapper(fileEditorManager: FileEditorManager, fileEditor: FileEditor, wrapper: BreadcrumbsXmlWrapper) {
+  remove(fileEditorManager, fileEditor, wrapper)
+  Disposer.dispose(wrapper)
 }
