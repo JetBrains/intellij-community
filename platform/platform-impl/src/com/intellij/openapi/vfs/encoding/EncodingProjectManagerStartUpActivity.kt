27389bc039a3a1567e704442e4be2c7f7283f51a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-12-28 08:12:28 +0100

    EncodingProjectManagerStartUpActivity - use modern and efficient API
    GitOrigin-RevId: b5d2cd998696bc8b919cf34320fba748dfbab486

--- platform/platform-impl/src/com/intellij/openapi/vfs/encoding/EncodingProjectManagerStartUpActivity.kt
+++ platform/platform-impl/src/com/intellij/openapi/vfs/encoding/EncodingProjectManagerStartUpActivity.kt
@@ -1,15 +1,19 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vfs.encoding
 
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.startup.ProjectPostStartupActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-private class EncodingProjectManagerStartUpActivity : StartupActivity.DumbAware {
-  override fun runActivity(project: Project) {
+private class EncodingProjectManagerStartUpActivity : ProjectPostStartupActivity {
+  override suspend fun execute(project: Project) {
     // do not try to init on EDT due to VFS usage in loadState
-    val service = EncodingProjectManager.getInstance(project) as EncodingProjectManagerImpl
-    ApplicationManager.getApplication().invokeLater(
-      { service.reloadAlreadyLoadedDocuments() }, project.disposed)
+    val service = project.serviceAsync<EncodingProjectManager>().await() as EncodingProjectManagerImpl
+    withContext(Dispatchers.EDT) {
+      service.reloadAlreadyLoadedDocuments()
+    }
   }
 }
