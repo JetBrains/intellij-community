db343a5e1ab6b0e5328b736021782ad2649991d0
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2022-12-29 23:14:26 +0100

    [cleanup] reuse base Intellij step in Kotlin intellij step
    GitOrigin-RevId: 284930f775cbffd4e538c7a83eccc5749541f35b

--- plugins/kotlin/project-wizard/idea/src/org/jetbrains/kotlin/tools/projectWizard/IntelliJKotlinNewProjectWizard.kt
+++ plugins/kotlin/project-wizard/idea/src/org/jetbrains/kotlin/tools/projectWizard/IntelliJKotlinNewProjectWizard.kt
@@ -1,27 +1,15 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.tools.projectWizard
 
-import com.intellij.ide.JavaUiBundle
-import com.intellij.ide.projectWizard.NewProjectWizardCollector.BuildSystem.logAddSampleCodeChanged
-import com.intellij.ide.projectWizard.NewProjectWizardCollector.BuildSystem.logSdkChanged
 import com.intellij.ide.projectWizard.NewProjectWizardConstants.BuildSystem.INTELLIJ
 import com.intellij.ide.projectWizard.generators.AssetsNewProjectWizardStep
+import com.intellij.ide.projectWizard.generators.IntelliJNewProjectWizardStep
 import com.intellij.ide.starters.local.StandardAssetsProvider
-import com.intellij.ide.wizard.AbstractNewProjectWizardStep
 import com.intellij.ide.wizard.NewProjectWizardBaseData.Companion.name
 import com.intellij.ide.wizard.NewProjectWizardBaseData.Companion.path
 import com.intellij.ide.wizard.NewProjectWizardStep
-import com.intellij.ide.wizard.NewProjectWizardStep.Companion.ADD_SAMPLE_CODE_PROPERTY_NAME
 import com.intellij.ide.wizard.chain
-import com.intellij.openapi.module.StdModuleTypes
-import com.intellij.openapi.observable.util.bindBooleanStorage
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.projectRoots.JavaSdkType
-import com.intellij.openapi.projectRoots.Sdk
-import com.intellij.openapi.projectRoots.SdkTypeId
-import com.intellij.openapi.projectRoots.impl.DependentSdkType
-import com.intellij.openapi.roots.ui.configuration.sdkComboBox
-import com.intellij.ui.UIBundle
 import com.intellij.ui.dsl.builder.*
 import org.jetbrains.kotlin.tools.projectWizard.plugins.buildSystem.BuildSystemType
 
@@ -34,37 +22,16 @@ internal class IntelliJKotlinNewProjectWizard : BuildSystemKotlinNewProjectWizar
     override fun createStep(parent: KotlinNewProjectWizard.Step) = Step(parent).chain(::AssetsStep)
 
     class Step(parent: KotlinNewProjectWizard.Step) :
-        AbstractNewProjectWizardStep(parent),
+        IntelliJNewProjectWizardStep<KotlinNewProjectWizard.Step>(parent),
         BuildSystemKotlinNewProjectWizardData by parent {
 
-        private val sdkProperty = propertyGraph.property<Sdk?>(null)
-        private val addSampleCodeProperty = propertyGraph.property(true)
-            .bindBooleanStorage(ADD_SAMPLE_CODE_PROPERTY_NAME)
-
-        private val sdk by sdkProperty
-        private val addSampleCode by addSampleCodeProperty
-
-        private fun setupJavaSdkUI(builder: Panel) {
-            builder.row(JavaUiBundle.message("label.project.wizard.new.project.jdk")) {
-                val sdkTypeFilter = { it: SdkTypeId -> it is JavaSdkType && it !is DependentSdkType }
-                sdkComboBox(context, sdkProperty, StdModuleTypes.JAVA.id, sdkTypeFilter)
-                    .columns(COLUMNS_MEDIUM)
-                    .whenItemSelectedFromUi { logSdkChanged(sdk) }
-            }.bottomGap(BottomGap.SMALL)
-        }
-
-        private fun setupSampleCodeUI(builder: Panel) {
-            builder.row {
-                checkBox(UIBundle.message("label.project.wizard.new.project.add.sample.code"))
-                    .bindSelected(addSampleCodeProperty)
-                    .whenStateChangedFromUi { logAddSampleCodeChanged(it) }
-            }
-        }
-
-        override fun setupUI(builder: Panel) {
+        override fun setupSettingsUI(builder: Panel) {
             setupJavaSdkUI(builder)
             setupSampleCodeUI(builder)
             setupKmpWizardLinkUI(builder)
+        }
+
+        override fun setupAdvancedSettingsUI(builder: Panel) {
         }
 
         override fun setupProject(project: Project) =
