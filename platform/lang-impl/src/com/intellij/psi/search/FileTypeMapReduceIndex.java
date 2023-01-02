1f71aa1b7f3608d60a84bc96148a9cf16d5dddc5
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-01-02 14:34:44 +0100

    use Path directly
    GitOrigin-RevId: 89dde08b56216df9b0ff95840f1abb6a0424d8ad

--- platform/lang-impl/src/com/intellij/psi/search/FileTypeMapReduceIndex.java
+++ platform/lang-impl/src/com/intellij/psi/search/FileTypeMapReduceIndex.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.search;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -65,7 +65,7 @@ class FileTypeMapReduceIndex extends TransientFileContentIndex<FileType, Void, V
   protected void doClear() throws StorageException, IOException {
     super.doClear();
     IOUtil.closeSafe(LOG, myFileTypeNameEnumerator);
-    IOUtil.deleteAllFilesStartingWith(getFileTypeNameEnumeratorPath().toFile());
+    IOUtil.deleteAllFilesStartingWith(getFileTypeNameEnumeratorPath());
     myFileTypeNameEnumerator = createFileTypeNameEnumerator();
   }
 
--- platform/util/src/com/intellij/util/io/PersistentMapImpl.java
+++ platform/util/src/com/intellij/util/io/PersistentMapImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.io;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -357,7 +357,7 @@ public class PersistentMapImpl<Key, Value> implements PersistentMapBase<Key, Val
     }
     catch (IOException ignored) {
     }
-    IOUtil.deleteAllFilesStartingWith(baseFile.toFile());
+    IOUtil.deleteAllFilesStartingWith(baseFile);
     try {
       if (myWal != null) {
         myWal.closeAndDelete();
@@ -400,7 +400,7 @@ public class PersistentMapImpl<Key, Value> implements PersistentMapBase<Key, Val
   @NotNull
   private static Path checkDataFiles(@NotNull Path file) {
     if (!Files.exists(file)) {
-      IOUtil.deleteAllFilesStartingWith(getDataFile(file).toFile());
+      IOUtil.deleteAllFilesStartingWith(getDataFile(file));
     }
     return file;
   }
