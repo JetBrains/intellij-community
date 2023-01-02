73c6bf165847c48c45ec36e847c9cace720bbd34
Vanya Pavlov <ivan.pavlov@jetbrains.com>
2023-01-02 16:36:34 +0200

    (IDEA-309407) ML in SE: Enable ML for Classes by default
    GitOrigin-RevId: 7581164283bb9c451fcd6ba9ca65dd01c6166ffc

--- plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/SearchEverywhereMlExperiment.kt
+++ plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/SearchEverywhereMlExperiment.kt
@@ -38,6 +38,7 @@ internal class SearchEverywhereMlExperiment {
 
     SearchEverywhereTabWithMl.CLASSES to Experiment(
       2 to ExperimentType.USE_EXPERIMENTAL_MODEL,
+      3 to ExperimentType.NO_ML
     ),
 
     SearchEverywhereTabWithMl.ALL to Experiment(
