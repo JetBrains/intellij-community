d1784742ed5ee2e0c9a45d27df18809eb9c81140
Roman Efremov <roman.efremov@jetbrains.com>
2023-01-03 22:04:58 +0100

    Remove useless ENUM_VALUES_METHOD_NAME property
    (cherry picked from commit a604ea77c26ddb0486c248a041fa63951efcc77e)
    
    IJ-MR-100842
    
    GitOrigin-RevId: 1b063936fa27f09c594d9f6e95f10b79eb8a4b8e

--- plugins/kotlin/base/code-insight/src/org/jetbrains/kotlin/idea/base/codeInsight/EnumValuesSoftDeprecate.kt
+++ plugins/kotlin/base/code-insight/src/org/jetbrains/kotlin/idea/base/codeInsight/EnumValuesSoftDeprecate.kt
@@ -8,18 +8,15 @@ import org.jetbrains.kotlin.analysis.api.symbols.KtCallableSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KtClassKind
 import org.jetbrains.kotlin.analysis.api.symbols.KtClassOrObjectSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KtSymbolOrigin
+import org.jetbrains.kotlin.builtins.StandardNames
 import org.jetbrains.kotlin.config.LanguageFeature
 import org.jetbrains.kotlin.config.LanguageVersionSettings
 import org.jetbrains.kotlin.idea.base.projectStructure.languageVersionSettings
-import org.jetbrains.kotlin.name.Name
-
-@ApiStatus.Internal
-val ENUM_VALUES_METHOD_NAME = Name.identifier("values")
 
 @ApiStatus.Internal
 fun KtAnalysisSession.isEnumValuesMethod(symbol: KtCallableSymbol): Boolean {
     return KtClassKind.ENUM_CLASS == (symbol.getContainingSymbol() as? KtClassOrObjectSymbol)?.classKind &&
-            ENUM_VALUES_METHOD_NAME == symbol.callableIdIfNonLocal?.callableName &&
+            StandardNames.ENUM_VALUES == symbol.callableIdIfNonLocal?.callableName &&
             // Don't touch user-declared methods with the name "values"
             symbol.origin == KtSymbolOrigin.SOURCE_MEMBER_GENERATED
 }
