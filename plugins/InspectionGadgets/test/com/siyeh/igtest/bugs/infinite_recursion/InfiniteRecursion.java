e6fa02de8ce6d5721ed61bea7e2c5e375208146d
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-01-04 10:36:10 +0100

    [java-inspections] IDEA-307577. Report indirect infinite recursion
    GitOrigin-RevId: 039124ac532cae554410a07b2d21b5ca1645e70b

--- plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/infinite_recursion/InfiniteRecursion.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/infinite_recursion/InfiniteRecursion.java
@@ -3,6 +3,7 @@ package com.siyeh.igtest.bugs.infinite_recursion;
 import java.util.List;
 import java.io.IOException;
 import java.io.File;
+import java.util.Objects;
 
 public class InfiniteRecursion
 {
@@ -142,4 +143,105 @@ public class InfiniteRecursion
 
 interface PsiClass {
     PsiClass getSuperClass();
+}
+
+class IndirectRecursion {
+
+  @Override
+  public int <warning descr="Method 'hashCode()' recurses infinitely, and can only end by throwing an exception">hashCode</warning>() {
+    return Objects.hashCode(this);
+  }
+
+  @Override
+  public boolean <warning descr="Method 'equals()' recurses infinitely, and can only end by throwing an exception">equals</warning>(Object obj) {
+    return Objects.equals(obj, this);
+  }
+
+  @Override
+  public String <warning descr="Method 'toString()' recurses infinitely, and can only end by throwing an exception">toString</warning>() {
+    return String.valueOf(this);
+  }
+}
+
+class IndirectRecursion2 {
+  int i = 1;
+
+  @Override
+  public int <warning descr="Method 'hashCode()' recurses infinitely, and can only end by throwing an exception">hashCode</warning>() {
+    if (i == 1) {
+      return Objects.hashCode(this);
+    }
+    return Objects.hashCode(this);
+  }
+}
+
+class IndirectRecursionNoWarning {
+
+  int i = 1;
+  @Override
+  public int hashCode() {
+    if (i == 1) {
+      return 0;
+    }
+    return Objects.hashCode(this);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (i == 1) {
+      return true;
+    }
+    return Objects.equals(obj, this);
+  }
+
+  @Override
+  public String toString() {
+    if (i == 1) {
+      return "";
+    }
+    return String.valueOf(this);
+  }
+}
+
+class Switch{
+  int i = 1;
+
+  public int t1() {
+    switch (i) {
+      case 1 ->{
+        System.out.println(1);
+        return 1;
+      }
+      default -> System.out.println(1);
+    }
+    t1();
+    return 1;
+  }
+  public int <warning descr="Method 't2()' recurses infinitely, and can only end by throwing an exception">t2</warning>() {
+    switch (i) {
+      case 1 -> System.out.println(1);
+      case 2 -> System.out.println(2);
+    }
+    t2();
+    return 1;
+  }
+  public int t3() {
+    switch (i) {
+      case 1: {
+        System.out.println(1);
+        return 1;
+      }
+      default: System.out.println(1);
+    }
+    t3();
+    return 1;
+  }
+  public int <warning descr="Method 't4()' recurses infinitely, and can only end by throwing an exception">t4</warning>() {
+    switch (i) {
+      case 1: System.out.println(1);
+      case 2: System.out.println(2);
+    }
+    t4();
+    return 1;
+  }
 }
