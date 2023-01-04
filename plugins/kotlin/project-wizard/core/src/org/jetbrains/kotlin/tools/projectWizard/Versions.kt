68f70d2bf61dbf6da7d43032bb0cd4c2faa3e8b7
Margarita Bobova <margarita.bobova@jetbrains.com>
2023-01-04 15:45:55 +0100

    Kotlin 1.8.20: update versions for wizards
    GitOrigin-RevId: 2623ef12ad108487f20aff7a8482b6d583887639

--- plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/Versions.kt
+++ plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/Versions.kt
@@ -10,36 +10,36 @@ import org.jetbrains.kotlin.tools.projectWizard.settings.version.Version
 @Suppress("ClassName", "SpellCheckingInspection")
 object Versions {
     val KOTLIN = version("1.5.0") // used as fallback version
-    val GRADLE = version("7.4.2")
-    val KTOR = version("2.0.2")
+    val GRADLE = version("7.5.1")
+    val KTOR = version("2.2.1")
     val JUNIT = version("4.13.2")
-    val JUNIT5 = version("5.8.2")
+    val JUNIT5 = version("5.9.1")
 
     object ANDROID {
-        val ANDROID_MATERIAL = version("1.5.0")
-        val ANDROIDX_APPCOMPAT = version("1.4.1")
-        val ANDROIDX_CONSTRAINTLAYOUT = version("2.1.3")
+        val ANDROID_MATERIAL = version("1.7.0")
+        val ANDROIDX_APPCOMPAT = version("1.5.1")
+        val ANDROIDX_CONSTRAINTLAYOUT = version("2.1.4")
     }
 
     object KOTLINX {
-        val KOTLINX_HTML = version("0.7.2")
+        val KOTLINX_HTML = version("0.8.1")
         val KOTLINX_NODEJS: Version = version("0.0.7")
     }
 
     object JS_WRAPPERS {
         val KOTLIN_REACT = wrapperVersion("18.2.0")
         val KOTLIN_REACT_DOM = KOTLIN_REACT
-        val KOTLIN_EMOTION = wrapperVersion("11.9.3")
+        val KOTLIN_EMOTION = wrapperVersion("11.10.5")
         val KOTLIN_REACT_ROUTER_DOM = wrapperVersion("6.3.0")
         val KOTLIN_REDUX = wrapperVersion("4.1.2")
         val KOTLIN_REACT_REDUX = wrapperVersion("7.2.6")
 
         private fun wrapperVersion(version: String): Version =
-            version("$version-pre.346")
+            version("$version-pre.467")
     }
 
     object GRADLE_PLUGINS {
-        val ANDROID = version("7.0.4")
+        val ANDROID = version("7.3.1")
     }
 
     object MAVEN_PLUGINS {
