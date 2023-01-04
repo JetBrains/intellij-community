e6683a69dcfc278abd04ba84efc8324f90fefe90
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2023-01-04 18:05:52 +0100

    Revert "Fix threading issues in TestApplicationLeakTrackerExtension"
    This reverts commit 8a6376692cb2e6c1666679ae442b4042ddd13449.
    
    GitOrigin-RevId: 3a14998fc55ab4a12f6e8a274d776d702d458d47

--- platform/testFramework/junit5/src/impl/TestApplicationLeakTrackerExtension.kt
+++ platform/testFramework/junit5/src/impl/TestApplicationLeakTrackerExtension.kt
@@ -1,13 +1,10 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.testFramework.junit5.impl
 
-import com.intellij.openapi.application.EDT
 import com.intellij.openapi.roots.impl.libraries.LibraryTableTracker
 import com.intellij.openapi.vfs.impl.VirtualFilePointerTracker
 import com.intellij.testFramework.SdkLeakTracker
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import com.intellij.testFramework.common.runAll
 import org.jetbrains.annotations.TestOnly
 import org.junit.jupiter.api.extension.AfterEachCallback
 import org.junit.jupiter.api.extension.BeforeEachCallback
@@ -36,12 +33,11 @@ internal class TestApplicationLeakTrackerExtension : BeforeEachCallback, AfterEa
     val virtualFilePointerTracker = VirtualFilePointerTracker()
 
     fun checkNothingLeaked() {
-      @Suppress("RAW_RUN_BLOCKING")  // Control flow exceptions are not supposed to be called in AfterEachCallback, AfterAllCallback, etc.
-      runBlocking {
-        launch(Dispatchers.EDT) { sdkLeakTracker.checkForJdkTableLeaks() }
-        launch { libraryLeakTracker.assertDisposed() }
-        launch { virtualFilePointerTracker.assertPointersAreDisposed() }
-      }
+      runAll(
+        { sdkLeakTracker.checkForJdkTableLeaks() },
+        { libraryLeakTracker.assertDisposed() },
+        { virtualFilePointerTracker.assertPointersAreDisposed() },
+      )
     }
   }
 }
