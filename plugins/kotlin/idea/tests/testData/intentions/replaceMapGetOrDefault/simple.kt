076fc356599440ff6b281cd8234049e829c062a8
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2023-01-06 12:25:49 +0100

    [kotlin] Bundle Kotlin 1.8.0 JPS plug-in as a default one
     Note that testdata changes are caused by the expected breaking changes:
     - KT-45508 Changes about overrides of abstract super class members
     - KT-44705 Deprecation of using non-exhaustive if's and when's in RHS of elvis
     - KT-55585 Handle nullability from RX Java 3 (io.reactivex.rxjava3.annotations) properly
    
     ^KTIJ-24179
    
    (cherry picked from commit a1d836490b319f2fb1fcd5cec19b0f09e3fc0588)
    
    IJ-CR-101042
    
    GitOrigin-RevId: ee0c8158fd9745b7ae77161a3c6572110d62aae2

--- plugins/kotlin/idea/tests/testData/intentions/replaceMapGetOrDefault/simple.kt
+++ plugins/kotlin/idea/tests/testData/intentions/replaceMapGetOrDefault/simple.kt
@@ -1,5 +1,4 @@
 // RUNTIME_WITH_FULL_JDK
-// AFTER-WARNING: The expression is unused
 fun test(map: Map<Int, String>) {
     map.getOrDefault<caret>(1, "bar")
 }
--- plugins/kotlin/idea/tests/testData/quickfix/specifySuperExplicitly/abstractSuperCallHashCode.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/specifySuperExplicitly/abstractSuperCallHashCode.kt
@@ -1,5 +1,6 @@
 // "Generate 'hashCode()'" "true"
 // WITH_STDLIB
+// ERROR: Abstract member cannot be accessed directly
 
 interface I
 
