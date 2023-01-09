361404a513888bf4ab83eafd52f57eb7ac2527d4
Artem Vasilev <artem.vasilev@jetbrains.com>
2023-01-09 15:15:07 +0100

    [kotlin] change applicability range for UsePropertyAccessSyntaxIntention in case of callable references
    ^KTIJ-24186
    
    (cherry picked from commit e5d89c4be9e04284645761d59f617d8b6d1c80d9)
    (cherry picked from commit 84421e4e6db9dfd454926c3cfdc6cefa8a7700b8)
    
    GitOrigin-RevId: c8edfc6c06f69828adc0b8f370766737ae1720de

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/UsePropertyAccessSyntaxIntention.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/UsePropertyAccessSyntaxIntention.kt
@@ -68,7 +68,7 @@ import javax.swing.JComponent
 
 @Suppress("DEPRECATION")
 class UsePropertyAccessSyntaxInspection : IntentionBasedInspection<KtExpression>(UsePropertyAccessSyntaxIntention::class),
-    CleanupLocalInspectionTool {
+                                          CleanupLocalInspectionTool {
 
     val fqNameList = NotPropertiesService.DEFAULT.map(::FqNameUnsafe).toMutableList()
 
@@ -352,8 +352,8 @@ private inline fun <T> KtExpression.callOrReferenceOrNull(
     call: (KtCallExpression) -> T,
     reference: (KtCallableReferenceExpression) -> T
 ): T? =
-    when {
-        this is KtCallExpression -> call(this)
-        this is KtSimpleNameExpression && parent is KtCallableReferenceExpression -> reference(parent as KtCallableReferenceExpression)
+    when (this) {
+        is KtCallExpression -> call(this)
+        is KtCallableReferenceExpression -> reference(this)
         else -> null
     }
