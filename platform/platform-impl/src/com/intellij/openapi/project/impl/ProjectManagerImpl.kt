a1f11f7fee77362095d6718ae5e150f002fe3f8b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-01-10 06:14:43 +0100

    IDEA-304329 Welcome Screen doesn't appear after rejecting opening untrusted project
    GitOrigin-RevId: 77b3acf89cfccd026de3b5e0f98fe3760bf9db49

--- platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectManagerImpl.kt
+++ platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectManagerImpl.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 @file:Suppress("ReplaceGetOrSet", "ReplacePutWithAssignment")
 
 package com.intellij.openapi.project.impl
@@ -72,7 +72,10 @@ import org.jetbrains.annotations.TestOnly
 import org.jetbrains.annotations.VisibleForTesting
 import java.io.File
 import java.io.IOException
-import java.nio.file.*
+import java.nio.file.Files
+import java.nio.file.InvalidPathException
+import java.nio.file.LinkOption
+import java.nio.file.Path
 import java.util.concurrent.CancellationException
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.coroutines.coroutineContext
@@ -1226,8 +1229,11 @@ private suspend fun initProject(file: Path,
   }
   catch (initThrowable: Throwable) {
     try {
-      withContext(Dispatchers.EDT + NonCancellable) {
-        ApplicationManager.getApplication().runWriteAction { Disposer.dispose(project) }
+      withContext(NonCancellable) {
+        project.coroutineScope.coroutineContext.job.cancelAndJoin()
+        writeAction {
+          Disposer.dispose(project)
+        }
       }
     }
     catch (disposeThrowable: Throwable) {
