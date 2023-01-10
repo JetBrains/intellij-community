d652827163f5a784e4f8ddf63d1f857bbf2223d3
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-01-10 12:15:44 +0100

    [java-inspections] IDEA-265253 supported equals methods of test frameworks. fix PR
    GitOrigin-RevId: a3cad0886c6d9d3f7b7bd1b12f07e61998afb61e

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/bugs/EqualsWithItselfInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/bugs/EqualsWithItselfInspection.java
@@ -69,11 +69,10 @@ public class EqualsWithItselfInspection extends BaseInspection {
   private static final CallMatcher ASSERTJ_ASSERT_THAT =
     CallMatcher.staticCall("org.assertj.core.api.Assertions", "assertThat").parameterCount(1);
 
-  private static final CallMatcher TEST_ASSERTIONS = CallMatcher.anyOf(ASSERT_ARGUMENT_COMPARISON, ASSERT_ARGUMENTS_THE_SAME,
-                                                                       ASSERTJ_COMPARISON, ASSERTJ_THE_SAME, ASSERTJ_ASSERT_THAT);
+  private static final CallMatcher TEST_ASSERTIONS = CallMatcher.anyOf(ASSERT_ARGUMENT_COMPARISON, ASSERTJ_COMPARISON);
 
   @SuppressWarnings("PublicField")
-  public boolean ignoreNonFinalClassesInTest = false;
+  public boolean ignoreNonFinalClassesInTest = true;
 
   @Override
   public @NotNull OptPane getOptionsPane() {
@@ -179,19 +178,7 @@ public class EqualsWithItselfInspection extends BaseInspection {
   }
 
   private static boolean isTheSame(@Nullable PsiExpression left, @Nullable PsiExpression right) {
-    left = PsiUtil.skipParenthesizedExprDown(left);
-    right = PsiUtil.skipParenthesizedExprDown(right);
-
-    if (!(left instanceof PsiReferenceExpression leftReference && right instanceof PsiReferenceExpression rightReference)) {
-      return false;
-    }
-    PsiElement resolvedFromLeft = leftReference.resolve();
-    PsiElement resolvedFromRight = rightReference.resolve();
-    boolean equalReferences = resolvedFromLeft != null && resolvedFromLeft == resolvedFromRight;
-    if (!equalReferences) return false;
-    PsiExpression leftQualifier = ExpressionUtils.getEffectiveQualifier(leftReference);
-    PsiExpression rightQualifier = ExpressionUtils.getEffectiveQualifier(rightReference);
-    return leftQualifier == rightQualifier || isTheSame(leftQualifier, rightQualifier);
+    return isItself(left, right) && !ExpressionUtils.isNewObject(left);
   }
 
   private static boolean isItself(@Nullable PsiExpression left, @Nullable PsiExpression right) {
