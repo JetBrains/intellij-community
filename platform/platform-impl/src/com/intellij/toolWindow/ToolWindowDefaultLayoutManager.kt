6dbfc0572f9b64879f844ad4edfcfd7b045767f0
Sasha Blashenkov <Sasha.Blashenkov@jetbrains.com>
2023-01-10 18:26:17 +0100

    Fix RIDER-87535 Tool window state is not preserved when updating from 223 to 231
    (cherry picked from commit 0870559e74c09718c1a0b619070b42a28edeaeec)
    
    GitOrigin-RevId: f14c3229de38873590defb0f29a79435610cc806

--- platform/platform-impl/src/com/intellij/toolWindow/ToolWindowDefaultLayoutManager.kt
+++ platform/platform-impl/src/com/intellij/toolWindow/ToolWindowDefaultLayoutManager.kt
@@ -87,6 +87,15 @@ class ToolWindowDefaultLayoutManager(private val isNewUi: Boolean)
     }
   }
 
+  /**
+   * Rider uses default layout for per-app toolwindows feature, so we need to migrate default layout
+   */
+  @Serializable
+  data class ToolWindowLayoutStorageManagerStateV1(
+    val v1: List<ToolWindowDescriptor> = emptyList(),
+    val v2: List<ToolWindowDescriptor> = emptyList()
+  )
+
   @Serializable
   data class ToolWindowLayoutStorageManagerState(
     val activeLayoutName: String = DEFAULT_LAYOUT_NAME,
