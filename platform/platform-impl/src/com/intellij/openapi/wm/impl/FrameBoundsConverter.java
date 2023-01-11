50628cd6b837bf366179150f35bfc57c06072920
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-01-11 11:09:48 +0100

    [ui] correct scaling of project frame bounds on reopening (IDEA-310234)
    GitOrigin-RevId: 63593ac94b114e1e3e231840001e4784b9ad61e9

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/FrameBoundsConverter.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/FrameBoundsConverter.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.wm.impl;
 
 import com.intellij.openapi.util.SystemInfoRt;
@@ -51,7 +51,7 @@ public final class FrameBoundsConverter {
         if (b.height > screen.height) {
           b.height = screen.height;
         }
-        return new Pair<>(bounds, gd);
+        return new Pair<>(b, gd);
       }
     }
 
