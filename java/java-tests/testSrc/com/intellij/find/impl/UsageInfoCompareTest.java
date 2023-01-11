ff671c8fc7860e8642fce25afe826c3319d720b5
Alexey Kudravtsev <cdr@intellij.com>
2023-01-11 17:26:06 +0100

    reduce visibility
    GitOrigin-RevId: ce7d6da1110e3c583fb416c9a67ecee30920e905

--- java/java-tests/testSrc/com/intellij/find/impl/UsageInfoCompareTest.java
+++ java/java-tests/testSrc/com/intellij/find/impl/UsageInfoCompareTest.java
@@ -1,10 +1,8 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.usages;
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.find.impl;
 
 import com.intellij.find.FindManager;
 import com.intellij.find.FindModel;
-import com.intellij.find.impl.FindResultImpl;
-import com.intellij.find.impl.FindResultUsageInfo;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import com.intellij.testFramework.HeavyPlatformTestCase;
