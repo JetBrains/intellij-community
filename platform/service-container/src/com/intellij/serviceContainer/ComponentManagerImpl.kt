9390fe003e4753919bd4ec3b796af1b0f19289da
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-01-11 13:07:36 +0100

    IJPL-83 inject coroutine scope into service constructors
    GitOrigin-RevId: 7e5386d88022693b1f0ee72bf78600effe4385c1

--- platform/service-container/src/com/intellij/serviceContainer/ComponentManagerImpl.kt
+++ platform/service-container/src/com/intellij/serviceContainer/ComponentManagerImpl.kt
@@ -31,11 +31,14 @@ import com.intellij.util.messages.*
 import com.intellij.util.messages.impl.MessageBusEx
 import com.intellij.util.messages.impl.MessageBusImpl
 import com.intellij.util.runSuppressing
+import kotlinx.collections.immutable.PersistentMap
+import kotlinx.collections.immutable.persistentHashMapOf
 import kotlinx.coroutines.*
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.TestOnly
 import org.picocontainer.ComponentAdapter
 import org.picocontainer.PicoContainer
+import java.lang.invoke.MethodHandle
 import java.lang.invoke.MethodHandles
 import java.lang.invoke.MethodType
 import java.lang.reflect.Constructor
@@ -51,6 +54,16 @@ internal val LOG = logger<ComponentManagerImpl>()
 private val constructorParameterResolver = ConstructorParameterResolver()
 private val methodLookup = MethodHandles.lookup()
 private val emptyConstructorMethodType = MethodType.methodType(Void.TYPE)
+private val coroutineScopeMethodType = emptyConstructorMethodType.appendParameterTypes(CoroutineScope::class.java)
+
+private fun MethodHandles.Lookup.findConstructorOrNull(clazz: Class<*>, type: MethodType): MethodHandle? {
+  return try {
+    findConstructor(clazz, type)
+  }
+  catch (e: NoSuchMethodException) {
+    return null
+  }
+}
 
 @ApiStatus.Internal
 abstract class ComponentManagerImpl(
@@ -125,8 +138,10 @@ abstract class ComponentManagerImpl(
   @Volatile
   private var isServicePreloadingCancelled = false
 
+  private val debugString: String get() = "${javaClass.name}@${System.identityHashCode(this)}"
+
   @Suppress("LeakingThis")
-  internal val serviceParentDisposable = Disposer.newDisposable("services of ${javaClass.name}@${System.identityHashCode(this)}")
+  internal val serviceParentDisposable = Disposer.newDisposable("services of $debugString")
 
   protected open val isLightServiceSupported = parent?.parent == null
   protected open val isMessageBusSupported = parent?.parent == null
@@ -959,34 +974,36 @@ abstract class ComponentManagerImpl(
     checkCanceledIfNotInClassInit()
 
     try {
+      val lookup = MethodHandles.privateLookupIn(aClass, methodLookup)
       if (parent == null) {
+        val instance = lookup.findConstructorOrNull(aClass, emptyConstructorMethodType)?.invoke()
+                       ?: lookup.findConstructor(aClass, coroutineScopeMethodType).invoke(intersectionCoroutineScope(aClass))
         @Suppress("UNCHECKED_CAST")
-        return MethodHandles.privateLookupIn(aClass, methodLookup).findConstructor(aClass, emptyConstructorMethodType).invoke() as T
+        return instance as T
       }
       else {
         val constructors: Array<Constructor<*>> = aClass.declaredConstructors
-        var constructor = if (constructors.size > 1) {
-          // see ConfigurableEP - prefer constructor that accepts our instance
-          constructors.firstOrNull { it.parameterCount == 1 && it.parameterTypes[0].isAssignableFrom(javaClass) }
-        }
-        else {
-          null
-        }
-
-        if (constructor == null) {
-          constructors.sortBy { it.parameterCount }
-          constructor = constructors.first()
-        }
-
-        constructor.isAccessible = true
+        val instance =
+          constructors.firstOrNull {
+            // see ConfigurableEP - prefer constructor that accepts our instance
+            it.parameterCount == 1
+            && it.parameterTypes[0].isAssignableFrom(javaClass)
+          }?.run {
+            isAccessible = true
+            newInstance(getActualContainerInstance())
+          }
+          ?: constructors.firstOrNull {
+            it.parameterCount == 2
+            && it.parameterTypes[1] == CoroutineScope::class.java
+            && it.parameterTypes[0].isAssignableFrom(javaClass)
+          }?.run {
+            isAccessible = true
+            newInstance(getActualContainerInstance(), intersectionCoroutineScope(aClass))
+          }
+          ?: lookup.findConstructorOrNull(aClass, emptyConstructorMethodType)?.invoke()
+          ?: lookup.findConstructor(aClass, coroutineScopeMethodType).invoke(intersectionCoroutineScope(aClass))
         @Suppress("UNCHECKED_CAST")
-        if (constructor.parameterCount == 1) {
-          return constructor.newInstance(getActualContainerInstance()) as T
-        }
-        else {
-          @Suppress("UNCHECKED_CAST")
-          return MethodHandles.privateLookupIn(aClass, methodLookup).unreflectConstructor(constructor).invoke() as T
-        }
+        return instance as T
       }
     }
     catch (e: Throwable) {
@@ -1430,6 +1447,61 @@ abstract class ComponentManagerImpl(
       else -> throw IllegalArgumentException("Unknown OS '$os'")
     }
   }
+
+  /**
+   * Key: plugin coroutine scope.
+   * Value: intersection of this container scope and plugin coroutine scope.
+   */
+  private val pluginScopes: AtomicReference<PersistentMap<CoroutineScope, CoroutineScope>> = AtomicReference(persistentHashMapOf())
+
+  private fun intersectionCoroutineScope(pluginClass: Class<*>): CoroutineScope {
+    val pluginClassloader = pluginClass.classLoader
+    if (pluginClassloader is PluginAwareClassLoader) {
+      return intersectionCoroutineScope(pluginClassloader.pluginCoroutineScope)
+    }
+    else { // non-unloadable
+      return getCoroutineScope()
+    }
+  }
+
+  private fun intersectionCoroutineScope(pluginScope: CoroutineScope): CoroutineScope {
+    var scopes = pluginScopes.get()
+    scopes.get(pluginScope)?.let {
+      return it
+    }
+
+    val containerScope = getCoroutineScope()
+    val intersectionName = "$debugString x ${pluginScope.coroutineContext[CoroutineName]?.name}"
+    val intersectionScope = CoroutineScope(SupervisorJob() + CoroutineName(intersectionName)).also {
+      it.attachTo(containerScope)
+      it.attachTo(pluginScope)
+    }
+    while (true) {
+      val newScopes = scopes.put(pluginScope, intersectionScope)
+      val witness = pluginScopes.compareAndExchange(scopes, newScopes)
+      if (witness === scopes) {
+        intersectionScope.coroutineContext.job.invokeOnCompletion {
+          removePluginScope(pluginScope)
+        }
+        // published successfully
+        return intersectionScope
+      }
+      witness.get(pluginScope)?.let {
+        // another thread published the scope for given plugin
+        // => use the value from another thread, and cancel the unpublished scope
+        intersectionScope.cancel()
+        return it
+      }
+      // try to publish again
+      scopes = witness
+    }
+  }
+
+  private fun removePluginScope(pluginScope: CoroutineScope) {
+    pluginScopes.updateAndGet { scopes: PersistentMap<CoroutineScope, CoroutineScope> ->
+      scopes.remove(pluginScope)
+    }
+  }
 }
 
 /**
@@ -1555,4 +1627,41 @@ private inline fun executeRegisterTask(mainPluginDescriptor: IdeaPluginDescripto
                                        crossinline task: (IdeaPluginDescriptorImpl) -> Unit) {
   task(mainPluginDescriptor)
   executeRegisterTaskForOldContent(mainPluginDescriptor, task)
+}
+
+/**
+ * Makes [this] scope job behave as if it was a child of [secondaryParent] job
+ * as per the coroutine framework parent-child [Job] relation:
+ * - child prevents completion of the parent;
+ * - child failure is propagated to the parent (note, parent might not cancel itself if it's a supervisor);
+ * - parent cancellation is propagated to the child.
+ *
+ * The [real parent][ChildHandle.parent] of [this] scope job is not changed.
+ * It does not matter whether the job of [this] scope has a real parent.
+ *
+ * Example usage:
+ * ```
+ * val containerScope: CoroutineScope = ...
+ * val pluginScope: CoroutineScope = ...
+ * CoroutineScope(SupervisorJob()).also {
+ *   it.attachTo(containerScope)
+ *   it.attachTo(pluginScope)
+ * }
+ * ```
+ */
+private fun CoroutineScope.attachTo(secondaryParent: CoroutineScope) {
+  attachChildInternal(
+    parent = secondaryParent.coroutineContext.job,
+    child = this.coroutineContext.job,
+  )
+}
+
+@OptIn(InternalCoroutinesApi::class)
+private fun attachChildInternal(parent: Job, child: Job) {
+  @Suppress("DEPRECATION_ERROR")
+  val childJob = child as ChildJob
+  val childHandle = parent.attachChild(childJob)
+  child.invokeOnCompletion(onCancelling = true) {
+    childHandle.dispose()
+  }
 }
