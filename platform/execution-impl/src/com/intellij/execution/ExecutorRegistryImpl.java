4668b289114eaf0793ea8d8637b63db31f6de134
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2023-01-12 20:07:00 +0100

    IDEA-309282 Select run  configuration in run widget when it starts by any way
    GitOrigin-RevId: 5a296979875edea31426326335d79c98c944b670

--- platform/execution-impl/src/com/intellij/execution/ExecutorRegistryImpl.java
+++ platform/execution-impl/src/com/intellij/execution/ExecutorRegistryImpl.java
@@ -829,6 +829,7 @@ public final class ExecutorRegistryImpl extends ExecutorRegistry {
                            @NotNull Executor executor) {
       if (settings != null) {
         RunConfigurationStartHistory.getInstance(project).register(settings);
+        RunManager.getInstance(project).setSelectedConfiguration(settings);
       }
       runSubProcess(project, configuration, settings, dataContext, executor, RunToolbarProcessData.prepareBaseSettingCustomization(settings, null));
     }
