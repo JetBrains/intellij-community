e3d8859723675592f005543a685a1f2ecbc13c51
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-01-13 22:21:56 +0100

    [platform] getting rid of `NSAlert` invocation from a background thread (IDEA-282172) [1/2]
    GitOrigin-RevId: 23b6da2120e848c59aed5eb0526c526d8624b703

--- native/MacLauncher/Launcher.m
+++ native/MacLauncher/Launcher.m
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 #import "Launcher.h"
 #import "VMOptionsReader.h"
@@ -388,12 +388,7 @@ NSArray *parseVMOptions() {
         return vmOptions;
     }
     else {
-        NSAlert *alert = [[NSAlert alloc] init];
-        [alert addButtonWithTitle:@"OK"];
-        [alert setMessageText:@"Cannot find VM options file"];
-        [alert setAlertStyle:NSAlertStyleWarning];
-        [alert runModal];
-        [alert release];
+        NSLog(@"parseVMOptions: cannot find VM options file");
         return nil;
     }
 }
