0171e703cba4863c14b6bebb9223499b213533a2
Egor Malyshev <egor.malyshev@jetbrains.com>
2023-01-16 10:12:57 +0100

    Adjusting built-in help plugin to the new standards of search
    (cherry picked from commit 4808078928cfb8a37d8e5a9e97079b613c2b9151)
    
    IJ-MR-101348
    
    GitOrigin-RevId: 774a601cf8fa952af9331b7c93e026d699e1d107

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/BuiltInHelpPlugin.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/BuiltInHelpPlugin.kt
@@ -13,6 +13,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 private const val MODULE_NAME = "intellij.platform.builtInHelp"
+private val LUCENE_LIBRARIES = setOf("lucene-queryparser", "lucene-highlighter", "lucene-memory")
 
 internal fun buildHelpPlugin(pluginVersion: String, context: BuildContext): PluginLayout? {
   val productName = context.applicationInfo.productName
@@ -46,6 +47,7 @@ internal fun buildHelpPlugin(pluginVersion: String, context: BuildContext): Plug
                                 content = pluginXml(buildContext, pluginVersion),
                                 overwrite = true)
     }
+    LUCENE_LIBRARIES.forEach { spec.withProjectLibrary(it) }
   }
 }
 
