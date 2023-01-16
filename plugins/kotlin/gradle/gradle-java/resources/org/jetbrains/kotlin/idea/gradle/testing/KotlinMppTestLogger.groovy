8fc74f2c3e7e53506c5bc1cea8bbba9fae59ba7d
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2023-01-16 15:32:50 +0000

    [Kotlin, Gradle] Make KotlinMppTestLogger compatible with Gradle configuration cache
    [Kotlin, Gradle] Move test logger to kotlin gradle module
    
    ^KTIJ-23696 fixed
    
    GitOrigin-RevId: fa4aec8e25755ac09d63c3378232b3a715a95acb

--- plugins/kotlin/gradle/gradle-java/resources/org/jetbrains/kotlin/idea/gradle/testing/KotlinMppTestLogger.groovy
+++ plugins/kotlin/gradle/gradle-java/resources/org/jetbrains/kotlin/idea/gradle/testing/KotlinMppTestLogger.groovy
@@ -1,15 +1,42 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 import groovy.xml.MarkupBuilder
+import org.gradle.api.tasks.testing.TestDescriptor
+import org.gradle.api.tasks.testing.TestListener
+import org.gradle.api.tasks.testing.TestOutputEvent
+import org.gradle.api.tasks.testing.TestOutputListener
+import org.gradle.api.tasks.testing.TestResult
 import org.gradle.api.internal.tasks.testing.TestDescriptorInternal
 
 class KotlinMppTestLogger {
     static def configureTestEventLogging(def task) {
-        task.beforeSuite { descriptor -> logTestEvent("beforeSuite", descriptor, null, null) }
-        task.afterSuite { descriptor, result -> logTestEvent("afterSuite", descriptor, null, result) }
+        task.addTestListener(new TestListener() {
+            @Override
+            void beforeSuite(TestDescriptor descriptor) {
+                logTestEvent("beforeSuite", (TestDescriptorInternal)descriptor, null, null)
+            }
 
-        task.beforeTest { descriptor -> logTestEvent("beforeTest", descriptor, null, null) }
-        task.onOutput { descriptor, event -> logTestEvent("onOutput", descriptor, event, null) }
-        task.afterTest { descriptor, result -> logTestEvent("afterTest", descriptor, null, result) }
+            @Override
+            void afterSuite(TestDescriptor descriptor, TestResult result) {
+                logTestEvent("afterSuite", (TestDescriptorInternal)descriptor, null, result)
+            }
+
+            @Override
+            void beforeTest(TestDescriptor descriptor) {
+                logTestEvent("beforeTest", (TestDescriptorInternal)descriptor, null, null)
+            }
+
+            @Override
+            void afterTest(TestDescriptor descriptor, TestResult result) {
+                logTestEvent("afterTest", (TestDescriptorInternal)descriptor, null, result)
+            }
+        })
+
+        task.addTestOutputListener(new TestOutputListener() {
+            @Override
+            void onOutput(TestDescriptor descriptor, TestOutputEvent event) {
+                logTestEvent("onOutput", (TestDescriptorInternal)descriptor, event, null)
+            }
+        })
     }
 
     static def logTestEvent(testEventType, TestDescriptorInternal testDescriptor, testEvent, testResult) {
@@ -18,7 +45,11 @@ class KotlinMppTestLogger {
         xml.event(type: testEventType) {
             test(id: testDescriptor.id, parentId: testDescriptor.parent?.id ?: '') {
                 if (testDescriptor) {
-                    descriptor(name: testDescriptor.name ?: '', displayName: getName(testDescriptor) ?: '', className: testDescriptor.className ?: '')
+                    descriptor(
+                            name: testDescriptor.name ?: '',
+                            displayName: getName(testDescriptor) ?: '',
+                            className: testDescriptor.className ?: ''
+                    )
                 }
                 if (testEvent) {
                     def message = escapeCdata(testEvent.message)
@@ -53,8 +84,8 @@ class KotlinMppTestLogger {
     }
 
     static def wrap(String s) {
-        if(!s) return s;
-        s.replaceAll("\r\n|\n\r|\n|\r","<ijLogEol/>\n")
+        if (!s) return s;
+        s.replaceAll("\r\n|\n\r|\n|\r", "<ijLogEol/>\n")
     }
 
     static def writeLog(s) {
@@ -62,7 +93,7 @@ class KotlinMppTestLogger {
     }
 
     static def logTestReportLocation(def report) {
-        if(!report) return
+        if (!report) return
         def writer = new StringWriter()
         def xml = new MarkupBuilder(writer)
         xml.event(type: 'reportLocation', testReport: report)
@@ -80,7 +111,7 @@ class KotlinMppTestLogger {
     }
 
     static def getStackTrace(Throwable t) {
-        if(!t) return ''
+        if (!t) return ''
         StringWriter sw = new StringWriter()
         t.printStackTrace(new PrintWriter(sw))
         sw.toString()
@@ -89,7 +120,8 @@ class KotlinMppTestLogger {
     static def getName(TestDescriptorInternal descriptor) {
         try {
             return descriptor.getDisplayName() // available starting from ver. 4.10.3
-        } catch (Throwable ignore) {
+        }
+        catch (Throwable ignore) {
             return descriptor.getName()
         }
     }
