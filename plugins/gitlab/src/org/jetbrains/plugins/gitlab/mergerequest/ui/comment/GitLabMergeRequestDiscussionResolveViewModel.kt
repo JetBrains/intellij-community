d4a6040c2fe7e552fad26688e047914bc1066a68
Ivan Semenov <ivan.semenov@jetbrains.com>
2023-01-16 19:14:55 +0100

    [gitlab] extract single task launcher
    GitOrigin-RevId: 30caee55418d4fc30856aa3b57741c90d1893c8c

--- plugins/gitlab/src/org/jetbrains/plugins/gitlab/mergerequest/ui/comment/GitLabMergeRequestDiscussionResolveViewModel.kt
+++ plugins/gitlab/src/org/jetbrains/plugins/gitlab/mergerequest/ui/comment/GitLabMergeRequestDiscussionResolveViewModel.kt
@@ -4,13 +4,9 @@ package org.jetbrains.plugins.gitlab.mergerequest.ui.comment
 import com.intellij.util.childScope
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 import org.jetbrains.plugins.gitlab.mergerequest.data.GitLabDiscussion
-import java.util.*
+import org.jetbrains.plugins.gitlab.util.SingleCoroutineLauncher
 
 interface GitLabMergeRequestDiscussionResolveViewModel {
   val resolved: Flow<Boolean>
@@ -21,27 +17,20 @@ interface GitLabMergeRequestDiscussionResolveViewModel {
 
 class GitLabMergeRequestDiscussionResolveViewModelImpl(parentCs: CoroutineScope, private val discussion: GitLabDiscussion)
   : GitLabMergeRequestDiscussionResolveViewModel {
-  private val cs = parentCs.childScope()
 
   override val resolved: Flow<Boolean> = discussion.resolved
 
-  private val currentTaskKey = MutableStateFlow<UUID?>(null)
-  override val busy: Flow<Boolean> = currentTaskKey.map { it !== null }
+  private val taskLauncher = SingleCoroutineLauncher(parentCs.childScope())
+  override val busy: Flow<Boolean> = taskLauncher.busy
 
   override fun changeResolvedState() {
-    cs.launch {
-      val key = UUID.randomUUID()
-      // other task in progress
-      if (!currentTaskKey.compareAndSet(null, key)) return@launch
+    taskLauncher.launch {
       try {
         discussion.changeResolvedState()
       }
       catch (e: Exception) {
         if (e is CancellationException) throw e
         //TODO: handle???
-      }
-      finally {
-        currentTaskKey.compareAndSet(key, null)
       }
     }
   }
