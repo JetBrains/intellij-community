6fc182ecf46000527bb3477ee7b2b6df3cb2518d
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2023-01-16 14:34:44 +0100

    IDEA-310633 Settings: NPE on opening Passwords
    GitOrigin-RevId: 5771142ed31dfecb4672ff035c4bcec363b60aa0

--- platform/platform-api/src/com/intellij/ui/dsl/builder/utils.kt
+++ platform/platform-api/src/com/intellij/ui/dsl/builder/utils.kt
@@ -110,7 +110,10 @@ fun UINumericRange.asRange(): IntRange = min..max
 fun <T> listCellRenderer(renderer: SimpleListCellRenderer<T>.(T) -> Unit): SimpleListCellRenderer<T> {
   return object : SimpleListCellRenderer<T>() {
     override fun customize(list: JList<out T>, value: T, index: Int, selected: Boolean, hasFocus: Boolean) {
-      renderer(value)
+      // BasicComboBoxUI.getBaseline can try to get renderer for null value even when comboBox doesn't allow nullable elements
+      if (index != -1 || value != null) {
+        renderer(value)
+      }
     }
   }
 }
