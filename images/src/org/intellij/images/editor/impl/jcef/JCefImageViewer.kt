470eb2007e1ddeedbd2fc9816217489bd3c5fe85
Vladimir Kharitonov <Vladimir.Kharitonov@jetbrains.com>
2023-01-17 17:10:33 +0100

    [jcef] IDEA-230850 SVG viewer: fix zooming and refactor resource handlers
    GitOrigin-RevId: 7a37c71565ccf7efb16157035215d9b531334a22

--- images/src/org/intellij/images/editor/impl/jcef/JCefImageViewer.kt
+++ images/src/org/intellij/images/editor/impl/jcef/JCefImageViewer.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.intellij.images.editor.impl.jcef
 
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.event.DocumentEvent
@@ -9,29 +8,24 @@ import com.intellij.openapi.editor.event.DocumentListener
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorState
+import com.intellij.openapi.fileEditor.FileEditorStateLevel
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.UserDataHolderBase
 import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.openapi.util.registry.RegistryManager
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.ui.components.Magnificator
-import com.intellij.ui.components.ZoomableViewport
 import com.intellij.ui.jcef.*
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.cef.browser.CefBrowser
 import org.cef.browser.CefFrame
-import org.cef.callback.CefCallback
 import org.cef.handler.*
-import org.cef.misc.BoolRef
-import org.cef.misc.IntRef
-import org.cef.misc.StringRef
-import org.cef.network.CefRequest
-import org.cef.network.CefResponse
 import org.intellij.images.ImagesBundle
 import org.intellij.images.editor.ImageZoomModel
+import org.intellij.images.editor.impl.ImageFileEditorState
+import org.intellij.images.options.OptionsManager
 import org.intellij.images.thumbnail.actionSystem.ThumbnailViewActions
 import org.intellij.images.ui.ImageComponentDecorator
 import org.jetbrains.annotations.Nls
@@ -40,13 +34,13 @@ import java.beans.PropertyChangeListener
 import java.io.ByteArrayInputStream
 import java.io.IOException
 import java.io.InputStream
-import java.net.MalformedURLException
-import java.net.URL
 import java.nio.charset.StandardCharsets
+import java.util.concurrent.atomic.AtomicReference
 import javax.swing.JComponent
+import javax.swing.SwingUtilities
 
 class JCefImageViewer(private val myFile: VirtualFile,
-                      mimeType: String) : UserDataHolderBase(), FileEditor, DocumentListener, ZoomableViewport, ImageComponentDecorator {
+                      mimeType: String) : UserDataHolderBase(), FileEditor, DocumentListener, ImageComponentDecorator {
 
   companion object {
     private const val NAME = "SvgViewer"
@@ -56,38 +50,70 @@ class JCefImageViewer(private val myFile: VirtualFile,
     private const val IMAGE_PATH = "/image"
     private const val VIEWER_URL = "$PROTOCOL://$HOST_NAME$VIEWER_PATH"
     private const val IMAGE_URL = "$PROTOCOL://$HOST_NAME$IMAGE_PATH"
-    private val REJECTING_HANDLER: CefResourceHandler = object : CefResourceHandlerAdapter() {
-      override fun processRequest(request: CefRequest, callback: CefCallback): Boolean {
-        callback.cancel()
-        return false
-      }
-    }
   }
 
   private val myDocument: Document = FileDocumentManager.getInstance().getDocument(myFile)!!
   private val myCefClient: JBCefClient = JBCefApp.getInstance().createClient()
   private val myBrowser: JBCefBrowser = JBCefBrowserBuilder().setClient(myCefClient).build()
   private val myUIComponent: JCefImageViewerUI
+  private val myMagnificationFinishedJSQuery: JBCefJSQuery
+
+  private val myInitializer: AtomicReference<() -> Unit> = AtomicReference()
   private var myState = ViewerState()
 
   override fun getComponent(): JComponent = myUIComponent
   override fun getPreferredFocusedComponent(): JComponent? = null
   override fun getName(): @Nls(capitalization = Nls.Capitalization.Title) String = NAME
 
-  override fun setState(state: FileEditorState) {}
-
   override fun isModified(): Boolean = false
   override fun isValid(): Boolean = true
 
   override fun addPropertyChangeListener(listener: PropertyChangeListener) {}
   override fun removePropertyChangeListener(listener: PropertyChangeListener) {}
+  override fun getFile(): VirtualFile = myFile
+  override fun getState(level: FileEditorStateLevel): FileEditorState =
+    ImageFileEditorState(myState.chessboardEnabled, myState.gridEnabled, myState.zoom, !myState.realSize)
+
+  // TODO: Simplify initialization
+  override fun setState(state: FileEditorState) {
+    if (!SwingUtilities.isEventDispatchThread()) {
+      SwingUtilities.invokeLater { setState(state) }
+      return
+    }
+    val initializer = myInitializer.get()
+
+    if (initializer != null && myInitializer.compareAndSet(initializer) { initializer(); setState(state) }) {
+      return
+    }
+
+    if (state is ImageFileEditorState) {
+      isTransparencyChessboardVisible = state.isBackgroundVisible
+      isGridVisible = state.isGridVisible
+      execute("setZoom(${state.zoomFactor});")
+    }
+  }
+
   override fun dispose() {}
   override fun documentChanged(event: DocumentEvent) = execute("reload()")
 
-  override fun getMagnificator(): Magnificator? = myViewportDelegate.magnificator
-  override fun magnificationStarted(at: Point) = myViewportDelegate.magnificationStarted(at)
-  override fun magnificationFinished(magnification: Double) = myViewportDelegate.magnificationFinished(magnification)
-  override fun magnify(magnification: Double) = myViewportDelegate.magnify(magnification)
+  fun magnify(scale: Double, at: Point, callback: (() -> Unit)?) {
+    if (callback != null) {
+      myMagnificationFinishedJSQuery.addHandler {
+        callback.invoke()
+        myMagnificationFinishedJSQuery.clearHandlers()
+        null
+      }
+    }
+
+    execute("setZoom(${myState.zoom * scale}, {'x': ${at.x}, 'y': ${at.y}});\n" +
+            "window.requestAnimationFrame(() => {\n" +
+            "  window.requestIdleCallback(() => {\n" +
+            "    setTimeout(() => {\n" +
+            "      ${myMagnificationFinishedJSQuery.inject("")}\n" +
+            "    }, 150)\n" +
+            "  })\n" +
+            "})\n")
+  }
 
   override fun setTransparencyChessboardVisible(visible: Boolean) {
     if (myState.status != ViewerState.Status.OK) return
@@ -103,8 +129,10 @@ class JCefImageViewer(private val myFile: VirtualFile,
   override fun isEnabledForActionPlace(place: String): Boolean = ThumbnailViewActions.ACTION_PLACE != place
   override fun getZoomModel(): ImageZoomModel = ZOOM_MODEL
   override fun isGridVisible(): Boolean = myState.status == ViewerState.Status.OK && myState.gridEnabled
+  fun getClientWidth() = myState.viewportSize.width
+  fun getClientHeight() = myState.viewportSize.height
 
-  private fun execute(script: String) = myBrowser.cefBrowser.executeJavaScript(script, myBrowser.cefBrowser.url, 0)
+  private fun execute(/*language=javascript*/ script: String) = myBrowser.cefBrowser.executeJavaScript(script, myBrowser.cefBrowser.url, 0)
 
   private val ZOOM_MODEL: ImageZoomModel = object : ImageZoomModel {
     override fun getZoomFactor(): Double = myState.zoom
@@ -119,76 +147,42 @@ class JCefImageViewer(private val myFile: VirtualFile,
     override fun isZoomLevelChanged(): Boolean = myState.status != ViewerState.Status.OK || !myState.fittedToViewport
   }
 
-  private val myViewportDelegate: ZoomableViewport = object : ZoomableViewport {
-    private var currentZoom = 1.0
-    private var x = 0.5
-    private var y = 0.5
-    override fun getMagnificator(): Magnificator = Magnificator { _: Double, _: Point? -> null }
-
-    override fun magnificationStarted(at: Point) {
-      currentZoom = myState.zoom
-      x = (at.x - myState.canvasBBox.x) / myState.canvasBBox.width
-      y = (at.y - myState.canvasBBox.y) / myState.canvasBBox.height
-    }
-
-    override fun magnificationFinished(magnification: Double) {}
-    override fun magnify(magnification: Double) {
-      val scale = if (magnification < 0) 1f / (1 - magnification) else 1 + magnification
-      execute("setZoom(${currentZoom * scale}, {'x': ${x}, 'y': ${y}});")
-    }
-  }
-
   private val jsonParser = Json { ignoreUnknownKeys = true }
 
   init {
     myDocument.addDocumentListener(this)
-    myCefClient.addRequestHandler(object : CefRequestHandlerAdapter() {
-      override fun getResourceRequestHandler(browser: CefBrowser,
-                                             frame: CefFrame,
-                                             request: CefRequest,
-                                             isNavigation: Boolean,
-                                             isDownload: Boolean,
-                                             requestInitiator: String,
-                                             disableDefaultHandling: BoolRef): CefResourceRequestHandler {
-        return object : CefResourceRequestHandlerAdapter() {
-          override fun getResourceHandler(browser: CefBrowser, frame: CefFrame, request: CefRequest): CefResourceHandler {
-            val url: URL = try {
-              URL(request.url)
-            }
-            catch (e: MalformedURLException) {
-              Logger.getInstance(JCefImageViewer::class.java).warn("Failed to parse URL", e)
-              return REJECTING_HANDLER
-            }
-            if (url.host != HOST_NAME || url.protocol != PROTOCOL) {
-              return REJECTING_HANDLER
-            }
-            val handler = when (url.path) {
-                            VIEWER_PATH -> javaClass.getResourceAsStream("resources/image_viewer.html")?.let {
-                              MyResourceHandler(it, "text/html")
-                            }
-                            IMAGE_PATH -> {
-                              var stream: InputStream? = null
-                              try {
-                                stream = if (FileUtilRt.isTooLarge(myFile.length)) myFile.inputStream
-                                else ByteArrayInputStream(myDocument.text.toByteArray(
-                                  StandardCharsets.UTF_8))
-                              }
-                              catch (e: IOException) {
-                                Logger.getInstance(
-                                  JCefImageViewer::class.java).warn("Failed to read the file", e)
-                              }
-                              stream?.let { MyResourceHandler(it, mimeType) }
-                            }
-                            else -> null
-                          } ?: return REJECTING_HANDLER
-            Disposer.register(this@JCefImageViewer, handler)
-            return handler
-          }
-        }
+    val resourceRequestHandler = CefLocalRequestHandler(PROTOCOL, HOST_NAME)
+
+    resourceRequestHandler.addResource(VIEWER_PATH) {
+      javaClass.getResourceAsStream("resources/image_viewer.html")?.let {
+        CefStreamResourceHandler(it, "text/html", this@JCefImageViewer)
       }
-    }, myBrowser.cefBrowser)
-    @Suppress("DEPRECATION") val query = JBCefJSQuery.create(myBrowser)
+    }
+
+    resourceRequestHandler.addResource(IMAGE_PATH) {
+      var stream: InputStream? = null
+      try {
+        stream = if (FileUtilRt.isTooLarge(myFile.length)) myFile.inputStream
+        else ByteArrayInputStream(myDocument.text.toByteArray(
+          StandardCharsets.UTF_8))
+      }
+      catch (e: IOException) {
+        Logger.getInstance(
+          JCefImageViewer::class.java).warn("Failed to read the file", e)
+      }
+      stream?.let { CefStreamResourceHandler(it, mimeType, this@JCefImageViewer) }
+    }
+
+    myCefClient.addRequestHandler(resourceRequestHandler, myBrowser.cefBrowser)
+
+    @Suppress("DEPRECATION")
+    myMagnificationFinishedJSQuery = JBCefJSQuery.create(myBrowser)
+
     myUIComponent = JCefImageViewerUI(myBrowser.component, this)
+    Disposer.register(this, myUIComponent);
+
+    @Suppress("DEPRECATION")
+    val query = JBCefJSQuery.create(myBrowser)
     query.addHandler { s: String ->
       myState = jsonParser.decodeFromString(s)
       myUIComponent.setInfo(
@@ -196,12 +190,20 @@ class JCefImageViewer(private val myFile: VirtualFile,
                              myState.imageSize.width, myState.imageSize.height, StringUtil.formatFileSize(myFile.length)))
       null
     }
+
+    myInitializer.set {
+      execute("send_info = function(info_text) {${query.inject("info_text")};}")
+      execute("setImageUrl('$IMAGE_URL');")
+      isGridVisible = OptionsManager.getInstance().options.editorOptions.gridOptions.isShowDefault
+      isTransparencyChessboardVisible = OptionsManager.getInstance().options.editorOptions.transparencyChessboardOptions.isShowDefault
+    }
+
     myCefClient.addLoadHandler(object : CefLoadHandlerAdapter() {
       override fun onLoadEnd(browser: CefBrowser, frame: CefFrame, httpStatusCode: Int) {
-        execute("setImageUrl('$IMAGE_URL');")
-        execute("send_info = function(info_text) {${query.inject("info_text")};}")
+        SwingUtilities.invokeLater { myInitializer.getAndSet(null).invoke() }
       }
     }, myBrowser.cefBrowser)
+
     if (RegistryManager.getInstance().`is`("ide.browser.jcef.svg-viewer.debug")) {
       myBrowser.loadURL("$VIEWER_URL?debug")
     }
@@ -210,53 +212,12 @@ class JCefImageViewer(private val myFile: VirtualFile,
     }
   }
 
-  private class MyResourceHandler constructor(val myStream: InputStream, val myMimeType: String) : CefResourceHandler, Disposable {
-    override fun processRequest(request: CefRequest, callback: CefCallback): Boolean {
-      callback.Continue()
-      return true
-    }
-
-    override fun getResponseHeaders(response: CefResponse, responseLength: IntRef, redirectUrl: StringRef) {
-      response.mimeType = myMimeType
-      response.status = 200
-    }
-
-    override fun readResponse(dataOut: ByteArray, bytesToRead: Int, bytesRead: IntRef, callback: CefCallback): Boolean {
-      try {
-        bytesRead.set(myStream.read(dataOut, 0, bytesToRead))
-        if (bytesRead.get() != -1) {
-          return true
-        }
-      }
-      catch (e: IOException) {
-        Logger.getInstance(JCefImageViewer::class.java).warn("Failed to read from the stream", e)
-        callback.cancel()
-      }
-      bytesRead.set(0)
-      Disposer.dispose(this)
-      return false
-    }
-
-    override fun cancel() {
-      Disposer.dispose(this)
-    }
-
-    override fun dispose() {
-      try {
-        myStream.close()
-      }
-      catch (e: IOException) {
-        Logger.getInstance(JCefImageViewer::class.java).warn("Failed to close the stream", e)
-      }
-    }
-  }
-
   @Serializable
   private data class ViewerState(
     val status: Status = Status.OK,
     val zoom: Double = 0.0,
+    val viewportSize: Size = Size(0, 0),
     val imageSize: Size = Size(0, 0),
-    val canvasBBox: Rectangle = Rectangle(0.0, 0.0, 0.0, 0.0),
     val zoomInPossible: Boolean = false,
     val zoomOutPossible: Boolean = false,
     val fittedToViewport: Boolean = false,
@@ -266,9 +227,6 @@ class JCefImageViewer(private val myFile: VirtualFile,
   ) {
     @Serializable
     enum class Status { OK, FAILED }
-
-    @Serializable
-    data class Rectangle(val x: Double, val y: Double, val width: Double, val height: Double)
 
     @Serializable
     data class Size(val width: Int, val height: Int)
