b78f798ec682c348e5782c291a5a0961eee27334
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-01-17 17:40:35 +0100

    [kotlin-inspections] Remove empty options from list automatically
    GitOrigin-RevId: 8dec92cb92987409c0962638118bdfb828474143

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/UsePropertyAccessSyntaxIntention.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/UsePropertyAccessSyntaxIntention.kt
@@ -94,6 +94,7 @@ class UsePropertyAccessSyntaxInspection : IntentionBasedInspection<KtExpression>
         return super.getOptionController()
             .onValueSet("fqNameStrings") { newList ->
                 assert(newList === fqNameStrings)
+                fqNameStrings.removeIf { it.isBlank() }
                 fqNameList.clear()
                 fqNameStrings.mapTo(fqNameList, ::FqNameUnsafe)
             }
