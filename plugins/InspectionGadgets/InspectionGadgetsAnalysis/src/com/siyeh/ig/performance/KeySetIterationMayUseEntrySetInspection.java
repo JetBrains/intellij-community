fc26fec72487e65c5aab534075f3c182f1a21038
Roman Ivanov <roman.ivanov@jetbrains.com>
2023-01-17 14:30:08 +0100

    [java] migrate to PsiForeachPatternStatement
    GitOrigin-RevId: 398b97a019ab0778247931b7b66d446562ac278d

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/performance/KeySetIterationMayUseEntrySetInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/performance/KeySetIterationMayUseEntrySetInspection.java
@@ -315,9 +315,7 @@ public class KeySetIterationMayUseEntrySetInspection extends BaseInspection {
       final PsiExpression iteratedValue = PsiUtil.skipParenthesizedExprDown(statement.getIteratedValue());
       final PsiExpression iteratedExpression = getIteratedExpression(iteratedValue);
       if (iteratedExpression == null) return;
-      PsiForeachDeclarationElement declaration = statement.getIterationDeclaration();
-      if (!(declaration instanceof PsiParameter)) return;
-      final PsiParameter parameter = (PsiParameter)declaration;
+      PsiParameter parameter = statement.getIterationParameter();
       PsiElement context = statement.getBody();
       if (context == null) return;
       PsiReferenceExpression mapExpression = getMapReferenceFromKeySetCall(iteratedExpression);
