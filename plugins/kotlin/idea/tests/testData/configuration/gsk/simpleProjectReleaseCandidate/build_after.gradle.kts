3c78f56bc920efbeeb3f39e2459e3fe5291de8e9
Nikolay Pachkov <nikolay.pachkov@jetbrains.com>
2023-01-17 18:36:39 +0100

    [IDE.Configuration] KTIJ-23771 use kotlin toolchain in java gradle project
    GitOrigin-RevId: c6c27426216c8a6e826bff78d23414fb4afe9802

--- plugins/kotlin/idea/tests/testData/configuration/gsk/simpleProjectReleaseCandidate/build_after.gradle.kts
+++ plugins/kotlin/idea/tests/testData/configuration/gsk/simpleProjectReleaseCandidate/build_after.gradle.kts
@@ -1,4 +1,5 @@
 import org.gradle.api.JavaVersion.VERSION_1_7
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
@@ -19,6 +20,11 @@ dependencies {
 }
 
 // VERSION: 1.5.20-RC-release-26
-kotlin {
-    jvmToolchain(8)
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
