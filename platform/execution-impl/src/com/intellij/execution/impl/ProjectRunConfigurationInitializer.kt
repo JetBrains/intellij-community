53f0f4543d3cc286baa400e3d4b1768c7583d39a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-01-18 16:22:56 +0100

    ProjectRunConfigurationInitializer - use preloaded service instead of ProjectServiceContainerInitializedListener
    GitOrigin-RevId: 79f75c08858ae20d4c841bf71418f9d6d8603ef6

--- platform/execution-impl/src/com/intellij/execution/impl/ProjectRunConfigurationInitializer.kt
+++ platform/execution-impl/src/com/intellij/execution/impl/ProjectRunConfigurationInitializer.kt
@@ -1,18 +1,18 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.impl
 
 import com.intellij.diagnostic.runActivity
 import com.intellij.execution.RunManager
 import com.intellij.execution.RunManager.Companion.IS_RUN_MANAGER_INITIALIZED
 import com.intellij.openapi.application.readActionBlocking
-import com.intellij.openapi.components.ComponentManagerEx
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.impl.ProjectServiceContainerInitializedListener
 import kotlinx.coroutines.launch
 
-private class ProjectRunConfigurationInitializer : ProjectServiceContainerInitializedListener {
-  override suspend fun execute(project: Project) {
+private class ProjectRunConfigurationInitializer(project: Project) {
+  init {
+    @Suppress("DEPRECATION")
     project.coroutineScope.launch {
       if (IS_RUN_MANAGER_INITIALIZED.get(project) == true) {
         return@launch
@@ -20,12 +20,13 @@ private class ProjectRunConfigurationInitializer : ProjectServiceContainerInitia
 
       // wait for module manager - may be required for module level run configurations
       // it allows us to avoid thread blocking
-      // (RunManager itself cannot yet do the same, as platform doesn't provide non-blocking load state)
-      (project as ComponentManagerEx).getServiceAsync(ModuleManager::class.java).join()
+      // (RunManager itself cannot yet do the same, as the platform doesn't provide non-blocking load state)
+      project.serviceAsync<ModuleManager>().join()
 
       runActivity("RunManager initialization") {
-        // we must not fire beginUpdate here, because message bus will fire queued parent message bus messages (and, so, SOE may occur because all other projectOpened will be processed before us)
-        // simply, you should not listen changes until project opened
+        // we must not fire beginUpdate here, because message bus will fire queued parent message bus messages
+        // (and, so, SOE may occur because all other projectOpened will be processed before us)
+        // simply, you should not listen changes until a project opened
         readActionBlocking {
           project.getService(RunManager::class.java)
         }
