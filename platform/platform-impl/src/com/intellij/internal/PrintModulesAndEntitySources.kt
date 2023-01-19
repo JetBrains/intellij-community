e4d31e9c41a29044c538724b9c8224a912838ce0
Alex Plate <aleksei.plate@jetbrains.com>
2023-01-19 12:20:42 +0200

    [Workspace Model] Add action to print modules
    GitOrigin-RevId: 3acc46a517f504a2b47c4a3d8412c0dea27a718b

---
+++ platform/platform-impl/src/com/intellij/internal/PrintModulesAndEntitySources.kt
@@ -0,0 +1,36 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.internal
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.workspaceModel.ide.workspaceModel
+import com.intellij.workspaceModel.storage.bridgeEntities.ModuleEntity
+
+class PrintModulesAndEntitySources : DumbAwareAction("Print Modules and Entity Sources to Log") {
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project
+    if (project == null) {
+      log.info("Project is null, can't print modules")
+      return
+    }
+
+    val snap = project.workspaceModel.currentSnapshot
+    snap.entities(ModuleEntity::class.java).forEach { module ->
+      log.info("Module: ${module.name}, source: ${module.entitySource}")
+      module.contentRoots.forEach { contentRootEntity ->
+        log.info(" - ContentRoot: ${contentRootEntity.url}, source: ${contentRootEntity.entitySource}")
+        contentRootEntity.sourceRoots.forEach { sourceRootEntity ->
+          log.info("   - SourceRoot: ${sourceRootEntity.url}, source: ${sourceRootEntity.entitySource}")
+        }
+        contentRootEntity.excludedUrls.forEach { exclude ->
+          log.info("   - ExcludeRoot: ${exclude.url}, source: ${exclude.entitySource}")
+        }
+      }
+    }
+  }
+
+  companion object {
+    val log = logger<PrintModulesAndEntitySources>()
+  }
+}
