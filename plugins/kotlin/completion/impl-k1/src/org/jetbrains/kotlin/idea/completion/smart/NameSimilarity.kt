26ccf7316c3001205735667ca547b5236af057bf
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-01-19 19:55:37 +0100

    cleanup
    GitOrigin-RevId: 39617afab337eac838ff459ca4cabb6512636391

--- plugins/kotlin/completion/impl-k1/src/org/jetbrains/kotlin/idea/completion/smart/NameSimilarity.kt
+++ plugins/kotlin/completion/impl-k1/src/org/jetbrains/kotlin/idea/completion/smart/NameSimilarity.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.completion.smart
 
@@ -10,7 +10,7 @@ import com.intellij.psi.codeStyle.NameUtil
 import org.jetbrains.kotlin.idea.core.ExpectedInfo
 import kotlin.math.min
 
-val NAME_SIMILARITY_KEY = Key<Int>("NAME_SIMILARITY_KEY")
+val NAME_SIMILARITY_KEY: Key<Int> = Key<Int>("NAME_SIMILARITY_KEY")
 
 object NameSimilarityWeigher : LookupElementWeigher("kotlin.nameSimilarity") {
     override fun weigh(element: LookupElement, context: WeighingContext) = -(element.getUserData(NAME_SIMILARITY_KEY) ?: 0)
