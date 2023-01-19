ad6ded6f676115ab9848956cb91430ba94cf2660
Alex Plate <aleksei.plate@jetbrains.com>
2023-01-19 17:43:15 +0200

    [Workspace Model] Add docs
    GitOrigin-RevId: 4bcb1f7987f2f74812447fccc16f0b74aae5755e

--- platform/projectModel-api/src/com/intellij/workspaceModel/ide/orphanage.kt
+++ platform/projectModel-api/src/com/intellij/workspaceModel/ide/orphanage.kt
@@ -12,6 +12,28 @@ import com.intellij.workspaceModel.storage.impl.VersionedEntityStorageImpl
 import com.intellij.workspaceModel.storage.url.VirtualFileUrl
 import kotlin.system.measureTimeMillis
 
+/**
+ * Orphanage - storage that contains content roots/source roots/excludes that currently don't have an associated parent in the storage.
+ * These components will be added to the storage in the corresponding parents will appear there.
+ * Also, if the parents are already exist in storage, they'll be immediately moved to the original storage.
+ *
+ * All entities that have the role of temporal parent (it's [ModuleEntity] usually) should have [OrphanageWorkerEntitySource] entity source.
+ *
+ * This storage is actively used to store the custom content roots of the modules in case the module is not yet added to the storage.
+ *
+ * This storage is designed only for described functionality (so you can save only content roots, source roots, and excludes).
+ *   However, it may be extended to a general-usage store if needed.
+ *
+ * ## iml saving format
+ * - Entities from orphan storage are not saved in iml files. So, if the original module won't appear in the storage, custom roots will be lost
+ *     pros: If the original module was removed without IJ, we won't have an obsolete file
+ *     cons: iml file may disappear temporally if the import is not yet finished.
+ * - Custom content roots and other elements are saved under AdditionalModuleElements component.
+ *   - Orphan storage DOES NOT save entities to this component. However, it's loaded from this component on start.
+ * - If we create a custom *source root*, the created content root has [OrphanageWorkerEntitySource] entity source in orphan storage and
+ *     have `dumb="true"` tag in iml file.
+ */
+
 object OrphanageWorkerEntitySource : EntitySource
 
 class Orphanage(private val project: Project) {
