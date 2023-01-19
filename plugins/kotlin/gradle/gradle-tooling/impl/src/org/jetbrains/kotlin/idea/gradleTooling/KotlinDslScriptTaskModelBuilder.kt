ff55244163c0e41543d5c1c5433049ac17128f93
nataliya.valtman <nataliya.valtman@jetbrains.com>
2023-01-19 13:01:07 +0100

    KTIJ-24232 Rollback BUILD_PREPARE_KOTLIN_BUILD_SCRIPT_MODEL metric usage
    GitOrigin-RevId: 5ff84ad9c1aea935afec7ec7b2cf28b2aed1235f

--- plugins/kotlin/gradle/gradle-tooling/impl/src/org/jetbrains/kotlin/idea/gradleTooling/KotlinDslScriptTaskModelBuilder.kt
+++ plugins/kotlin/gradle/gradle-tooling/impl/src/org/jetbrains/kotlin/idea/gradleTooling/KotlinDslScriptTaskModelBuilder.kt
@@ -10,9 +10,6 @@ import org.jetbrains.kotlin.idea.gradleTooling.reflect.KotlinExtensionReflection
 import org.jetbrains.plugins.gradle.tooling.AbstractModelBuilderService
 import org.jetbrains.plugins.gradle.tooling.ErrorMessageBuilder
 import org.jetbrains.plugins.gradle.tooling.ModelBuilderContext
-import java.lang.management.ManagementFactory
-import javax.management.MBeanServer
-import javax.management.ObjectName
 
 class KotlinDslScriptTaskModelBuilder : AbstractModelBuilderService() {
 
@@ -23,47 +20,13 @@ class KotlinDslScriptTaskModelBuilder : AbstractModelBuilderService() {
         return KotlinDslScriptAdditionalTask::class.java.name == modelName
     }
 
-    //since kotlin 1.8.20
-    private fun reportFUSMetricByJMXWithWeight(method: String, type: String, metricName: String, value: Any, subprojectName: String?, weight: Long?) {
-        val name = "org.jetbrains.kotlin.gradle.plugin.statistics:type=StatsService"
-        val beanName = ObjectName(name)
-        val mbs: MBeanServer = ManagementFactory.getPlatformMBeanServer()
-         mbs.invoke(
-                beanName,
-                method,
-                arrayOf(metricName, value, subprojectName, weight),
-                arrayOf("java.lang.String", type, "java.lang.String", "java.lang.Long")
-            )
-    }
-
-    private fun reportFUSMetricByJMX(method: String, type: String, metricName: String, value: Any, subprojectName: String?) {
-        val name = "org.jetbrains.kotlin.gradle.plugin.statistics:type=StatsService"
-        val beanName = ObjectName(name)
-        val mbs: MBeanServer = ManagementFactory.getPlatformMBeanServer()
-        mbs.invoke(
-            beanName,
-            method,
-            arrayOf(metricName, value, subprojectName),
-            arrayOf("java.lang.String", type, "java.lang.String")
-        )
-    }
-
     override fun buildAll(modelName: String, project: Project, context: ModelBuilderContext): Any? {
-
         if (kotlinDslScriptsModelImportSupported(project.gradle.gradleVersion)) {
             val startParameter = project.gradle.startParameter
+
             val tasks = HashSet(startParameter.taskNames)
             tasks.add(PREPARATION_TASK_NAME)
             startParameter.setTaskNames(tasks)
-
-            val kotlinExtension = project.extensions.findByName("kotlin") ?: return null
-            val kotlinExtensionReflection = KotlinExtensionReflection(project, kotlinExtension).parseKotlinGradlePluginVersion()
-
-            if (kotlinExtensionReflection.supportsFusMetricsWeight()) {
-                reportFUSMetricByJMXWithWeight("reportBoolean", "boolean", "BUILD_PREPARE_KOTLIN_BUILD_SCRIPT_MODEL", true, null, null)
-            } else {
-                reportFUSMetricByJMX("reportBoolean", "boolean", "BUILD_PREPARE_KOTLIN_BUILD_SCRIPT_MODEL", true, null)
-            }
         }
         return null
     }
