6f7fb3a971bb614506425b5578382bc5c656d00f
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-01-20 11:42:17 +0100

    Cleanup (uniform assertions; formatting)
    GitOrigin-RevId: 7a8d4b863c744c13bce8156b55a2b7581dddc28e

--- platform/platform-tests/testSrc/com/intellij/idea/LockSupportTest.java
+++ platform/platform-tests/testSrc/com/intellij/idea/LockSupportTest.java
@@ -4,7 +4,6 @@ package com.intellij.idea;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.testFramework.rules.TempDirectory;
 import kotlinx.coroutines.GlobalScope;
-import org.jetbrains.annotations.NotNull;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -13,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeTrue;
 
@@ -29,8 +27,8 @@ public class LockSupportTest {
     var lock1 = new SocketLock(StartupUtil.canonicalPath(path1 + "/c"), StartupUtil.canonicalPath(path1 + "/s"));
     var lock2 = new SocketLock(StartupUtil.canonicalPath(path2 + "/c"), StartupUtil.canonicalPath(path2 + "/s"));
     try {
-      assertThat(tryActivate(lock1)).isEqualTo(SocketLock.ActivationStatus.NO_INSTANCE);
-      assertThat(tryActivate(lock2)).isEqualTo(SocketLock.ActivationStatus.ACTIVATED);
+      assertEquals(SocketLock.ActivationStatus.NO_INSTANCE, tryActivate(lock1));
+      assertEquals(SocketLock.ActivationStatus.ACTIVATED, tryActivate(lock2));
     }
     finally {
       lock1.dispose();
@@ -56,36 +54,21 @@ public class LockSupportTest {
       assertEquals(SocketLock.ActivationStatus.NO_INSTANCE, createLockAndTryActivate(toClose, tempDir.getRootPath(), "c1", "s1"));
       assertEquals(SocketLock.ActivationStatus.NO_INSTANCE, createLockAndTryActivate(toClose, tempDir.getRootPath(), "c2", "s2"));
       assertEquals(SocketLock.ActivationStatus.NO_INSTANCE, createLockAndTryActivate(toClose, tempDir.getRootPath(), "c3", "s3"));
-
-      assertThat(createLockAndTryActivate(toClose, tempDir.getRootPath(), "c1", "s1")).isEqualTo(SocketLock.ActivationStatus.ACTIVATED);
-      assertThat(createLockAndTryActivate(toClose, tempDir.getRootPath(), "c2", "s2")).isEqualTo(SocketLock.ActivationStatus.ACTIVATED);
-      assertThat(createLockAndTryActivate(toClose, tempDir.getRootPath(), "c3", "s3")).isEqualTo(SocketLock.ActivationStatus.ACTIVATED);
+      assertEquals(SocketLock.ActivationStatus.ACTIVATED, createLockAndTryActivate(toClose, tempDir.getRootPath(), "c1", "s1"));
+      assertEquals(SocketLock.ActivationStatus.ACTIVATED, createLockAndTryActivate(toClose, tempDir.getRootPath(), "c2", "s2"));
+      assertEquals(SocketLock.ActivationStatus.ACTIVATED, createLockAndTryActivate(toClose, tempDir.getRootPath(), "c3", "s3"));
     }
     finally {
       toClose.forEach(SocketLock::dispose);
     }
   }
 
-  private static SocketLock.ActivationStatus createLockAndTryActivate(List<SocketLock> toClose, @NotNull Path dir, String cfg, String sys) {
-    var lock = new SocketLock(dir.resolve(cfg), dir.resolve(sys));
-    toClose.add(lock);
-    return tryActivate(lock);
-  }
-
-  private static SocketLock.ActivationStatus tryActivate(@NotNull SocketLock lock) {
-    var statusAndResult = lock.lockAndTryActivate(List.of(), GlobalScope.INSTANCE);
-    lock.getServer();
-    return statusAndResult.getFirst();
-  }
-
   @Test(timeout = 30000)
   public void dispose() {
     var lock1 = new SocketLock(tempDir.getRootPath().resolve("c"), tempDir.getRootPath().resolve("s"));
     var lock2 = new SocketLock(tempDir.getRootPath().resolve("c"), tempDir.getRootPath().resolve("s"));
-
     assertEquals(SocketLock.ActivationStatus.NO_INSTANCE, tryActivate(lock1));
     assertEquals(SocketLock.ActivationStatus.ACTIVATED, tryActivate(lock2));
-
     lock1.dispose();
     assertEquals(SocketLock.ActivationStatus.NO_INSTANCE, tryActivate(lock2));
     lock2.dispose();
@@ -95,5 +78,17 @@ public class LockSupportTest {
   public void pathCollision() {
     var path = tempDir.getRootPath().resolve("d");
     new SocketLock(path, path);
+  }
+
+  private static SocketLock.ActivationStatus createLockAndTryActivate(List<SocketLock> toClose, Path dir, String cfg, String sys) {
+    var lock = new SocketLock(dir.resolve(cfg), dir.resolve(sys));
+    toClose.add(lock);
+    return tryActivate(lock);
+  }
+
+  private static SocketLock.ActivationStatus tryActivate(SocketLock lock) {
+    var statusAndResult = lock.lockAndTryActivate(List.of(), GlobalScope.INSTANCE);
+    lock.getServer();
+    return statusAndResult.getFirst();
   }
 }
