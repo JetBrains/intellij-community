575da8209ec62987f87d6502b68f81e2e873f964
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-01-23 15:42:09 +0100

    cleanup
    GitOrigin-RevId: f499ebc0c00bcf40dd9f9384bb996604e449fa6d

--- platform/ide-core/src/com/intellij/projectImport/ProjectAttachProcessor.kt
+++ platform/ide-core/src/com/intellij/projectImport/ProjectAttachProcessor.kt
@@ -1,16 +1,19 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.projectImport;
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.projectImport
 
-import com.intellij.openapi.extensions.ExtensionPointName;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.project.Project;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import java.nio.file.Path
 
-import java.nio.file.Path;
+open class ProjectAttachProcessor {
+  companion object {
+    @JvmField
+    val EP_NAME: ExtensionPointName<ProjectAttachProcessor> = ExtensionPointName("com.intellij.projectAttachProcessor")
 
-public class ProjectAttachProcessor {
-  public static final ExtensionPointName<ProjectAttachProcessor> EP_NAME = new ExtensionPointName<>("com.intellij.projectAttachProcessor");
+    @JvmStatic
+    fun canAttachToProject(): Boolean = EP_NAME.hasAnyExtensions()
+  }
 
   /**
    * Called to attach the directory projectDir as a module to the specified project.
@@ -18,15 +21,11 @@ public class ProjectAttachProcessor {
    * @param project    the project to attach the directory to.
    * @param projectDir the directory to attach.
    * @param callback   the callback to call on successful attachment
-   * @return true if the attach succeeded, false if the project should be opened in a new window.
+   * @return true, if the `attach` succeeded, false if the project should be opened in a new window.
    */
-  public boolean attachToProject(@NotNull Project project, @NotNull Path projectDir, @Nullable ProjectOpenedCallback callback) {
-    return false;
+  open fun attachToProject(project: Project, projectDir: Path, callback: ProjectOpenedCallback?): Boolean {
+    return false
   }
 
-  public void beforeDetach(@NotNull Module module) {}
-
-  public static boolean canAttachToProject() {
-    return EP_NAME.hasAnyExtensions();
-  }
+  open fun beforeDetach(module: Module) {}
 }
