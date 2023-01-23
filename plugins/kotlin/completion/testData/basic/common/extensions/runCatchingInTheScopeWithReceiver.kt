a08af32eb8107c3a2dd30574a8f21245ba018201
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2023-01-23 18:42:14 +0100

    [kotlin] Prioritise functions with receiver over regular ones in completion
     Before this change, functions without receivers win which results in inserting FQ name for the item to avoid using the receiver unintentionally. This isn't convenient and, moreover, it's not fully correct as functions with receiver win in this situation (see https://kotlinlang.org/spec/overload-resolution.html?sentence=building-the-overload-candidate-set,call-without-an-explicit-receiver,4,1), so even if we have two imported function "foo" and one of them is T.foo and the other one is just foo, then the compiler will pick T.foo
    
     ^KTIJ-11561
    
    GitOrigin-RevId: db8db23bfb72d1432008d80fb6462e20908cfc6c

---
+++ plugins/kotlin/completion/testData/basic/common/extensions/runCatchingInTheScopeWithReceiver.kt
@@ -0,0 +1,7 @@
+class Foo {
+    val x = runCatchin<caret>
+}
+
+// WITH_ORDER
+// EXIST: { "lookupString":"runCatching", "tailText":" {...} (block: Foo.() -> R) for T in kotlin" }
+// EXIST: { "lookupString":"runCatching", "tailText":" {...} (block: () -> R) (kotlin)" }
