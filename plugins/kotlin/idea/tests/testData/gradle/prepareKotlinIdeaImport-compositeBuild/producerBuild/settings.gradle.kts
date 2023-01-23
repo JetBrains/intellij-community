3d645ac7bc43fd0f26d34c7a302f0d0612b04589
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-01-23 15:30:07 +0100

    [KTIJ-24368] PrepareKotlinIdeaImportTaskModelBuilder: Support composite builds
    ^KTIJ-24368 Verification Pending
    
    GitOrigin-RevId: 80007aab31f05a18b8b606b5266b5434c6ed5e1e

---
+++ plugins/kotlin/idea/tests/testData/gradle/prepareKotlinIdeaImport-compositeBuild/producerBuild/settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        { { kts_kotlin_plugin_repositories } }
+    }
+
+    plugins {
+        kotlin("jvm") version "{{kotlin_plugin_version}}"
+    }
+}
+
+include(":producerA")
