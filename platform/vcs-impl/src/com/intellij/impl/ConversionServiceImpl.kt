a28b414c60d9569e2669d52bbce8ad1e3a30d8f7
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-01-23 09:12:39 +0100

    cleanup
    GitOrigin-RevId: 0474e080cdf881113aa42e80b9ba4a84155cf2ab

--- platform/vcs-impl/src/com/intellij/impl/ConversionServiceImpl.kt
+++ platform/vcs-impl/src/com/intellij/impl/ConversionServiceImpl.kt
@@ -1,246 +1,232 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.impl;
+package com.intellij.impl
 
-import com.intellij.conversion.*;
-import com.intellij.conversion.impl.ConversionContextImpl;
-import com.intellij.conversion.impl.ConversionRunner;
-import com.intellij.conversion.impl.ProjectConversionUtil;
-import com.intellij.conversion.impl.ui.ConvertProjectDialog;
-import com.intellij.ide.IdeBundle;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ex.ApplicationManagerEx;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.extensions.impl.ExtensionPointImpl;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.util.Ref;
-import com.intellij.openapi.vcs.VcsBundle;
-import com.intellij.project.ProjectStoreOwner;
-import it.unimi.dsi.fastutil.objects.Object2LongMap;
-import it.unimi.dsi.fastutil.objects.Object2LongMaps;
-import it.unimi.dsi.fastutil.objects.ObjectIterator;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.conversion.*
+import com.intellij.conversion.impl.ConversionContextImpl
+import com.intellij.conversion.impl.ConversionRunner
+import com.intellij.conversion.impl.ProjectConversionUtil
+import com.intellij.conversion.impl.ui.ConvertProjectDialog
+import com.intellij.ide.IdeBundle
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ex.ApplicationManagerEx
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.extensions.impl.ExtensionPointImpl
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.vcs.VcsBundle
+import com.intellij.project.ProjectStoreOwner
+import it.unimi.dsi.fastutil.objects.Object2LongMaps
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.function.Supplier
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.*;
-import java.util.function.Supplier;
-
-public final class ConversionServiceImpl extends ConversionService {
-  private static final Logger LOG = Logger.getInstance(ConversionServiceImpl.class);
-
-  @NotNull
-  @Override
-  public ConversionResult convertSilently(@NotNull Path projectPath, @NotNull ConversionListener listener) {
+class ConversionServiceImpl : ConversionService() {
+  override fun convertSilently(projectPath: Path, listener: ConversionListener): ConversionResult {
     try {
-      ConversionContextImpl context = new ConversionContextImpl(projectPath);
-      List<ConversionRunner> runners = isConversionNeeded(context);
+      val context = ConversionContextImpl(projectPath)
+      val runners = isConversionNeeded(context)
       if (runners.isEmpty()) {
-        return ConversionResultImpl.CONVERSION_NOT_NEEDED;
+        return ConversionResultImpl.CONVERSION_NOT_NEEDED
       }
 
-      listener.conversionNeeded();
-
-      Set<Path> affectedFiles = new HashSet<>();
-      for (ConversionRunner runner : runners) {
-        runner.collectAffectedFiles(affectedFiles);
+      listener.conversionNeeded()
+      val affectedFiles = HashSet<Path>()
+      for (runner in runners) {
+        runner.collectAffectedFiles(affectedFiles)
       }
-
-      List<Path> readOnlyFiles = ConversionRunner.getReadOnlyFiles(affectedFiles);
+      val readOnlyFiles = ConversionRunner.getReadOnlyFiles(affectedFiles)
       if (!readOnlyFiles.isEmpty()) {
-        listener.cannotWriteToFiles(readOnlyFiles);
-        return ConversionResultImpl.ERROR_OCCURRED;
+        listener.cannotWriteToFiles(readOnlyFiles)
+        return ConversionResultImpl.ERROR_OCCURRED
       }
-      Path backupDir = ProjectConversionUtil.backupFiles(affectedFiles, context.getProjectBaseDir());
-      for (ConversionRunner runner : runners) {
-        if (runner.isConversionNeeded()) {
-          runner.preProcess();
-          runner.process();
-          runner.postProcess();
+
+      val backupDir = ProjectConversionUtil.backupFiles(affectedFiles, context.projectBaseDir)
+      for (runner in runners) {
+        if (runner.isConversionNeeded) {
+          runner.preProcess()
+          runner.process()
+          runner.postProcess()
         }
       }
-      context.saveFiles(affectedFiles);
-      listener.successfullyConverted(backupDir);
-      context.saveConversionResult();
-      return new ConversionResultImpl(runners);
+      context.saveFiles(affectedFiles)
+      listener.successfullyConverted(backupDir)
+      context.saveConversionResult()
+      return ConversionResultImpl(runners)
     }
-    catch (CannotConvertException | IOException e) {
-      listener.error(e.getMessage());
+    catch (e: CannotConvertException) {
+      listener.error(e.message!!)
     }
-    return ConversionResultImpl.ERROR_OCCURRED;
+    catch (e: IOException) {
+      listener.error(e.message!!)
+    }
+    return ConversionResultImpl.ERROR_OCCURRED
   }
 
-  @Override
-  public @NotNull ConversionResult convert(@NotNull Path projectPath) throws CannotConvertException {
-    if (ApplicationManager.getApplication().isHeadlessEnvironment() || !ConverterProvider.EP_NAME.hasAnyExtensions() || !Files.exists(projectPath)) {
-      return ConversionResultImpl.CONVERSION_NOT_NEEDED;
+  @Throws(CannotConvertException::class)
+  override fun convert(projectPath: Path): ConversionResult {
+    if (ApplicationManager.getApplication().isHeadlessEnvironment ||
+        !ConverterProvider.EP_NAME.hasAnyExtensions() ||
+        !Files.exists(projectPath)) {
+      return ConversionResultImpl.CONVERSION_NOT_NEEDED
     }
 
-    ConversionContextImpl context = new ConversionContextImpl(projectPath);
-    List<ConversionRunner> converters = isConversionNeeded(context);
+    val context = ConversionContextImpl(projectPath)
+    val converters = isConversionNeeded(context)
     if (converters.isEmpty()) {
-      return ConversionResultImpl.CONVERSION_NOT_NEEDED;
+      return ConversionResultImpl.CONVERSION_NOT_NEEDED
     }
 
-    Ref<ConversionResult> ref = new Ref<>();
+    var result: ConversionResultImpl
     if (ApplicationManagerEx.isInIntegrationTest()) {
-      ref.set(new ConversionResultImpl(converters));
+      result = ConversionResultImpl(converters)
     }
     else {
-      ref.set(ConversionResultImpl.CONVERSION_CANCELED);
-      ApplicationManager.getApplication().invokeAndWait(() -> {
-        ConvertProjectDialog dialog = new ConvertProjectDialog(context, converters);
-        dialog.show();
-        if (dialog.isConverted()) {
-          ref.set(new ConversionResultImpl(converters));
+      result = ConversionResultImpl.CONVERSION_CANCELED
+      ApplicationManager.getApplication().invokeAndWait {
+        val dialog = ConvertProjectDialog(context, converters)
+        dialog.show()
+        if (dialog.isConverted) {
+          result = ConversionResultImpl(converters)
         }
-      });
-    }
-
-    if (!ref.isNull()) {
-      try {
-        context.saveConversionResult();
-      }
-      catch (IOException e) {
-        LOG.error(e);
       }
     }
-    return ref.get();
-  }
-
-  private static @NotNull List<ConversionRunner> isConversionNeeded(@NotNull ConversionContextImpl context) {
     try {
-      Object2LongMap<String> oldMap = context.getProjectFileTimestamps();
-      boolean changed = false;
-      if (oldMap.isEmpty()) {
-        LOG.debug("conversion will be performed because no information about project files");
-      }
-      else {
-        Object2LongMap<String> newMap = context.getAllProjectFiles();
-        LOG.debug("Checking project files");
-        for (ObjectIterator<Object2LongMap.Entry<String>> iterator = Object2LongMaps.fastIterator(newMap); iterator.hasNext(); ) {
-          Object2LongMap.Entry<String> entry = iterator.next();
-          String path = entry.getKey();
-          long oldValue = oldMap.getLong(path);
-          long newValue = entry.getLongValue();
-          if (newValue != oldValue) {
-            LOG.info("conversion will be performed because at least " + path + " is changed (oldLastModified=" + oldValue + ", newLastModified=" + newValue + ")");
-            changed = true;
-            break;
-          }
-        }
-      }
-
-      Set<String> performedConversionIds;
-      if (changed) {
-        performedConversionIds = Collections.emptySet();
-      }
-      else {
-        performedConversionIds = context.getAppliedConverters();
-        if (LOG.isDebugEnabled()) {
-          LOG.debug("Project files are up to date. Applied converters: " + performedConversionIds);
-        }
-      }
-
-      List<ConversionRunner> runners = new ArrayList<>();
-      ExtensionPointImpl<ConverterProvider> point = (ExtensionPointImpl<ConverterProvider>)ConverterProvider.EP_NAME.getPoint();
-      point.processIdentifiableImplementations((supplier, id) -> {
-        String providerId = getProviderId(supplier, id);
-        if (!performedConversionIds.contains(providerId)) {
-          ConverterProvider provider = supplier.get();
-          if (provider != null) {
-            ConversionRunner runner = new ConversionRunner(providerId, provider, context);
-            if (runner.isConversionNeeded()) {
-              runners.add(runner);
-            }
-          }
-        }
-      });
-      return runners;
+      context.saveConversionResult()
     }
-    catch (CannotConvertException e) {
-      LOG.info("Cannot check whether conversion of project files is needed or not, conversion won't be performed", e);
-      return Collections.emptyList();
+    catch (e: IOException) {
+      LOG.error(e)
     }
+    return result
   }
 
-  private static @NotNull String getProviderId(@NotNull Supplier<? extends ConverterProvider> supplier, @Nullable String id) {
-    return id == null ? supplier.get().getDeprecatedId() : id;
-  }
-
-  @Override
-  public void saveConversionResult(@NotNull Path projectPath) {
+  override fun saveConversionResult(projectPath: Path) {
     try {
-      new ConversionContextImpl(projectPath).saveConversionResult();
+      ConversionContextImpl(projectPath).saveConversionResult()
     }
-    catch (IOException e) {
-      LOG.error(e);
+    catch (e: IOException) {
+      LOG.error(e)
     }
-    catch (CannotConvertException e) {
-      LOG.info(e);
+    catch (e: CannotConvertException) {
+      LOG.info(e)
     }
   }
 
-  @Override
-  public @NotNull ConversionResult convertModule(@NotNull Project project, @NotNull Path moduleFile) {
-    ConversionContextImpl context = new ConversionContextImpl(((ProjectStoreOwner)project).getComponentStore().getProjectFilePath());
-
-    List<ConversionRunner> runners = new ArrayList<>();
+  override fun convertModule(project: Project, moduleFile: Path): ConversionResult {
+    val context = ConversionContextImpl((project as ProjectStoreOwner).componentStore.projectFilePath)
+    val runners = ArrayList<ConversionRunner>()
     try {
-      ExtensionPointImpl<ConverterProvider> point = (ExtensionPointImpl<ConverterProvider>)ConverterProvider.EP_NAME.getPoint();
-      point.processIdentifiableImplementations((supplier, id) -> {
-        ConverterProvider provider = supplier.get();
+      val point = ConverterProvider.EP_NAME.point as ExtensionPointImpl<ConverterProvider>
+      point.processIdentifiableImplementations { supplier, id ->
+        val provider = supplier.get()
         if (provider != null) {
-          ConversionRunner runner = new ConversionRunner(getProviderId(supplier, id), provider, context);
+          val runner = ConversionRunner(getProviderId(supplier, id), provider, context)
           if (runner.isModuleConversionNeeded(moduleFile)) {
-            runners.add(runner);
+            runners.add(runner)
           }
         }
-      });
+      }
     }
-    catch (CannotConvertException e) {
-      LOG.info(e);
+    catch (e: CannotConvertException) {
+      LOG.info(e)
     }
-
     if (runners.isEmpty()) {
-      return ConversionResultImpl.CONVERSION_NOT_NEEDED;
+      return ConversionResultImpl.CONVERSION_NOT_NEEDED
     }
-
-    int answer = Messages.showYesNoDialog(project, IdeBundle.message("message.module.file.has.an.older.format.do.you.want.to.convert.it"),
-                                       IdeBundle.message("dialog.title.convert.module"), Messages.getQuestionIcon());
+    val answer = Messages.showYesNoDialog(project,
+                                          IdeBundle.message("message.module.file.has.an.older.format.do.you.want.to.convert.it"),
+                                          IdeBundle.message("dialog.title.convert.module"), Messages.getQuestionIcon())
     if (answer != Messages.YES) {
-      return ConversionResultImpl.CONVERSION_CANCELED;
+      return ConversionResultImpl.CONVERSION_CANCELED
     }
-
     if (!Files.isWritable(moduleFile)) {
       Messages.showErrorDialog(project, IdeBundle.message("error.message.cannot.modify.file.0", moduleFile.toAbsolutePath().toString()),
-                               IdeBundle.message("dialog.title.convert.module"));
-      return ConversionResultImpl.ERROR_OCCURRED;
+                               IdeBundle.message("dialog.title.convert.module"))
+      return ConversionResultImpl.ERROR_OCCURRED
     }
 
     try {
-      File backupFile = ProjectConversionUtil.backupFile(moduleFile);
-      for (ConversionRunner runner : runners) {
-        runner.convertModule(moduleFile);
+      val backupFile = ProjectConversionUtil.backupFile(moduleFile)
+      for (runner in runners) {
+        runner.convertModule(moduleFile)
       }
-
-      context.saveFiles(Collections.singletonList(moduleFile));
-      Messages.showInfoMessage(project, IdeBundle.message("message.your.module.was.successfully.converted.br.old.version.was.saved.to.0", backupFile.getAbsolutePath()),
-                               IdeBundle.message("dialog.title.convert.module"));
-      return new ConversionResultImpl(runners);
+      context.saveFiles(listOf(moduleFile))
+      Messages.showInfoMessage(project, IdeBundle.message("message.your.module.was.successfully.converted.br.old.version.was.saved.to.0",
+                                                          backupFile.absolutePath),
+                               IdeBundle.message("dialog.title.convert.module"))
+      return ConversionResultImpl(runners)
     }
-    catch (CannotConvertException e) {
-      LOG.info(e);
-      Messages.showErrorDialog(IdeBundle.message("error.cannot.load.project", e.getMessage()),
-                               VcsBundle.message("dialog.title.cannot.convert.module"));
-      return ConversionResultImpl.ERROR_OCCURRED;
+    catch (e: CannotConvertException) {
+      LOG.info(e)
+      Messages.showErrorDialog(IdeBundle.message("error.cannot.load.project", e.message),
+                               VcsBundle.message("dialog.title.cannot.convert.module"))
+      return ConversionResultImpl.ERROR_OCCURRED
     }
-    catch (IOException e) {
-      LOG.info(e);
-      return ConversionResultImpl.ERROR_OCCURRED;
+    catch (e: IOException) {
+      LOG.info(e)
+      return ConversionResultImpl.ERROR_OCCURRED
     }
   }
+}
+
+private val LOG = logger<ConversionServiceImpl>()
+
+private fun isConversionNeeded(context: ConversionContextImpl): List<ConversionRunner> {
+  try {
+    val oldMap = context.projectFileTimestamps
+    var changed = false
+    if (oldMap.isEmpty()) {
+      LOG.debug("conversion will be performed because no information about project files")
+    }
+    else {
+      val newMap = context.allProjectFiles
+      LOG.debug("Checking project files")
+      val iterator = Object2LongMaps.fastIterator(newMap)
+      while (iterator.hasNext()) {
+        val entry = iterator.next()
+        val path = entry.key
+        val oldValue = oldMap.getLong(path)
+        val newValue = entry.longValue
+        if (newValue != oldValue) {
+          LOG.info("conversion will be performed because at least $path is changed (oldLastModified=$oldValue, newLastModified=$newValue)")
+          changed = true
+          break
+        }
+      }
+    }
+    val performedConversionIds: Set<String>
+    if (changed) {
+      performedConversionIds = emptySet()
+    }
+    else {
+      performedConversionIds = context.appliedConverters
+      if (LOG.isDebugEnabled) {
+        LOG.debug("Project files are up to date. Applied converters: $performedConversionIds")
+      }
+    }
+
+    val runners = ArrayList<ConversionRunner>()
+    val point = ConverterProvider.EP_NAME.point as ExtensionPointImpl<ConverterProvider>
+    point.processIdentifiableImplementations { supplier: Supplier<out ConverterProvider?>, id: String? ->
+      val providerId = getProviderId(supplier, id)
+      if (!performedConversionIds.contains(providerId)) {
+        val provider = supplier.get()
+        if (provider != null) {
+          val runner = ConversionRunner(providerId, provider, context)
+          if (runner.isConversionNeeded) {
+            runners.add(runner)
+          }
+        }
+      }
+    }
+    return runners
+  }
+  catch (e: CannotConvertException) {
+    LOG.info("Cannot check whether conversion of project files is needed or not, conversion won't be performed", e)
+    return emptyList()
+  }
+}
+
+private fun getProviderId(supplier: Supplier<out ConverterProvider?>, id: String?): String {
+  return id ?: supplier.get()!!.deprecatedId
 }
