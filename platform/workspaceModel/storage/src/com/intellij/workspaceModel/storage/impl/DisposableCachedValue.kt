f191a64bf9642abe97dd4ea2682a8c9ebe1126e5
Alex Plate <aleksei.plate@jetbrains.com>
2023-01-24 20:15:38 +0200

    [IDEA-282382] Add additional diagnostic
    GitOrigin-RevId: fd1d913f3063932d99bb947efab2bf0900efafac

--- platform/workspaceModel/storage/src/com/intellij/workspaceModel/storage/impl/DisposableCachedValue.kt
+++ platform/workspaceModel/storage/src/com/intellij/workspaceModel/storage/impl/DisposableCachedValue.kt
@@ -1,14 +1,19 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.workspaceModel.storage.impl
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.diagnostic.debug
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.util.Disposer
 import com.intellij.workspaceModel.storage.CachedValue
 import com.intellij.workspaceModel.storage.VersionedEntityStorage
 import com.intellij.workspaceModel.storage.MutableEntityStorage
 
-class DisposableCachedValue<R : Disposable>(private val entityStorage: () -> VersionedEntityStorage,
-                                            private val cachedValue: CachedValue<R>) : Disposable {
+class DisposableCachedValue<R : Disposable>(
+  private val entityStorage: () -> VersionedEntityStorage,
+  private val cachedValue: CachedValue<R>,
+  private val cacheName: String = "-",
+) : Disposable {
 
   private var latestValue: R? = null
   private var latestStorageModificationCount: Long? = null
@@ -32,6 +37,7 @@ class DisposableCachedValue<R : Disposable>(private val entityStorage: () -> Ver
 
       val oldValue = latestValue
       if (oldValue !== currentValue && oldValue != null) {
+        log.debug { "Dispose old value. Cache name: `$cacheName`. Store type: ${storage.javaClass}" }
         Disposer.dispose(oldValue)
       }
       latestValue = currentValue
@@ -52,5 +58,9 @@ class DisposableCachedValue<R : Disposable>(private val entityStorage: () -> Ver
       latestStorageModificationCount = null
       latestValue = null
     }
+  }
+
+  companion object {
+    private val log = logger<DisposableCachedValue<*>>()
   }
 }
