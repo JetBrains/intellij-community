8b07a6d58fdd7c7ccd19495ae5f579c3e8cfd5b8
Alexandr Suhinin <alexandr.suhinin@jetbrains.com>
2023-01-24 09:31:13 +0200

    extract method with object: use pojo objects for older java
    GitOrigin-RevId: 9b9f667f4edc0ec7280a1a5e4f0cbe80f02cdb10

--- java/java-impl-refactorings/src/com/intellij/refactoring/extractMethod/newImpl/ObjectBuilder.kt
+++ java/java-impl-refactorings/src/com/intellij/refactoring/extractMethod/newImpl/ObjectBuilder.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.refactoring.extractMethod.newImpl
 
+import com.intellij.codeInsight.daemon.impl.analysis.HighlightingFeature
 import com.intellij.codeInsight.hint.EditorCodePreview
 import com.intellij.codeInsight.hint.HintManager
 import com.intellij.java.refactoring.JavaRefactoringBundle
@@ -65,7 +66,11 @@ interface ObjectBuilder {
       require(variables.isNotEmpty())
       require(scope.isNotEmpty())
 
-      val objectBuilder: ObjectBuilder = RecordObjectBuilder.create(variables, scope)
+      val objectBuilder = if (HighlightingFeature.RECORDS.isAvailable(variables.first())) {
+        RecordObjectBuilder.create(variables, scope)
+      } else {
+        ClassObjectBuilder.create(variables, scope)
+      }
       val file = scope.first().containingFile
       val extractRange = createGreedyRangeMarker(file.viewProvider.document, scope.first().textRange.union(scope.last().textRange))
       val editorState = EditorState(editor)
@@ -135,9 +140,11 @@ interface ObjectBuilder {
       val file = introducedClass.containingFile
       val classReference = (declaration.initializer as? PsiNewExpression)?.classReference?.element ?: throw IllegalStateException()
       val variableName = declaration.nameIdentifier ?: throw IllegalStateException()
+      val constructorIdentifiers = introducedClass.constructors.mapNotNull { method -> method.nameIdentifier }
+      val typeIdentifiersToUpdate = listOfNotNull(declaration.typeElement, introducedClass.nameIdentifier) + constructorIdentifiers
       val typeNameField = TemplateField(
         classReference.textRange,
-        listOfNotNull(declaration.typeElement, introducedClass.nameIdentifier).map(PsiElement::getTextRange),
+        typeIdentifiersToUpdate.map(PsiElement::getTextRange),
         validator = { variableRange -> InplaceExtractUtils.checkClassReference(editor, file, variableRange) }
       )
       val variableNameField = TemplateField(
@@ -174,7 +181,7 @@ class RecordObjectBuilder(private val record: PsiClass, private val references: 
       return record
     }
 
-    private fun findAffectedReferences(variables: List<PsiVariable>, startingElement: PsiElement?): List<PsiReferenceExpression> {
+    fun findAffectedReferences(variables: List<PsiVariable>, startingElement: PsiElement?): List<PsiReferenceExpression> {
       val startingPoint = startingElement?.textRange?.startOffset ?: return emptyList()
       return  variables.flatMap { findAffectedReferences(it, startingPoint) }
     }
@@ -210,6 +217,62 @@ class RecordObjectBuilder(private val record: PsiClass, private val references: 
 
   override fun createReferenceReplacement(reference: PsiReferenceExpression): PsiExpression {
     return PsiElementFactory.getInstance(reference.project).createExpressionFromText("result.${reference.text}()", reference.context)
+  }
+
+  override fun findVariableReferenceInReplacement(replacement: PsiExpression): PsiReferenceExpression? {
+    val place = replacement.textRange.startOffset
+    return PsiTreeUtil.findElementOfClassAtOffset(replacement.containingFile, place, PsiReferenceExpression::class.java, false)
+  }
+
+  override fun getAffectedReferences(): List<PsiReferenceExpression> = references
+}
+
+class ClassObjectBuilder(private val pojoClass: PsiClass, private val references: List<PsiReferenceExpression>): ObjectBuilder{
+  companion object {
+    fun create(variables: List<PsiVariable>, scope: List<PsiElement>): ClassObjectBuilder {
+      val pojoClass = createPojoClass(variables)
+      val affectedReferences = RecordObjectBuilder.findAffectedReferences(variables, scope.last().nextSibling)
+      return ClassObjectBuilder(pojoClass, affectedReferences)
+    }
+
+    private fun createPojoClass(variables: List<PsiVariable>): PsiClass {
+      val project = variables.first().project
+      val factory = PsiElementFactory.getInstance(project)
+      val pojoClass = factory.createClass("Result")
+      variables.forEach { variable ->
+        val field = factory.createField(variable.name!!, variable.type)
+        field.modifierList?.setModifierProperty(PsiModifier.PUBLIC, true)
+        field.modifierList?.setModifierProperty(PsiModifier.FINAL, true)
+        pojoClass.add(field)
+      }
+      val constructor = factory.createConstructor()
+      val body = factory.createCodeBlock()
+      variables.forEach { variable ->
+        val parameter = factory.createParameter(variable.name!!, variable.type)
+        val assignment = factory.createStatementFromText("this.${variable.name} = ${variable.name};", constructor)
+        constructor.parameterList.add(parameter)
+        body.add(assignment)
+      }
+      constructor.body?.replace(body)
+      pojoClass.add(constructor)
+      pojoClass.modifierList?.setModifierProperty(PsiModifier.STATIC, true)
+      return pojoClass
+    }
+  }
+
+  override fun createClass(): PsiClass = pojoClass
+
+  override fun createDeclaration(): PsiDeclarationStatement {
+    val fields = pojoClass.allFields
+    val parameters = fields.joinToString(separator = ",") { field -> field.name }
+    val initializer = "new ${pojoClass.name}($parameters)"
+    val factory = PsiElementFactory.getInstance(pojoClass.project)
+    val expression = factory.createExpressionFromText(initializer, pojoClass)
+    return factory.createVariableDeclarationStatement("result", TypeUtils.getType(pojoClass), expression)
+  }
+
+  override fun createReferenceReplacement(reference: PsiReferenceExpression): PsiExpression {
+    return PsiElementFactory.getInstance(reference.project).createExpressionFromText("result.${reference.text}", reference.context)
   }
 
   override fun findVariableReferenceInReplacement(replacement: PsiExpression): PsiReferenceExpression? {
