2535e120ca447284010d1525019dd0c6a2f2e827
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2023-01-24 12:06:14 +0300

    [warm-up] Use more appropriate way to perform an activity on project closing
    GitOrigin-RevId: d4253d3302e530cce3e3f00d8750e1f56b6507c0

--- platform/warmup/performanceTesting/src/com/intellij/warmup/CheckWarmupBuildStatusCommand.kt
+++ platform/warmup/performanceTesting/src/com/intellij/warmup/CheckWarmupBuildStatusCommand.kt
@@ -1,10 +1,11 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.warmup
 
-import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectCloseListener
 import com.intellij.openapi.ui.playback.PlaybackContext
 import com.intellij.openapi.ui.playback.commands.AbstractCommand
-import com.intellij.openapi.util.Disposer
 import com.jetbrains.performancePlugin.utils.ActionCallbackProfilerStopper
 import org.jetbrains.concurrency.Promise
 import org.jetbrains.concurrency.toPromise
@@ -13,11 +14,16 @@ internal class CheckWarmupBuildStatusCommand(text: String, line: Int) : Abstract
 
   override fun _execute(context: PlaybackContext): Promise<Any?> {
     val callback = ActionCallbackProfilerStopper()
-    Disposer.register(context.project, Disposable {
-      when (val buildStatus = WarmupBuildStatus.currentStatus()) {
-        is WarmupBuildStatus.NotInvoked -> callback.reject("Warm-up build was not invoked")
-        is WarmupBuildStatus.Failure -> callback.reject("Warm-up build failed: ${buildStatus.message}")
-        else -> callback.setDone()
+
+    val messageBus = ApplicationManager.getApplication().messageBus
+    val connection = messageBus.simpleConnect()
+    connection.subscribe(ProjectCloseListener.TOPIC, object : ProjectCloseListener {
+      override fun projectClosing(project: Project) {
+        when (val buildStatus = WarmupBuildStatus.currentStatus()) {
+          is WarmupBuildStatus.NotInvoked -> callback.reject("Warm-up build was not invoked")
+          is WarmupBuildStatus.Failure -> callback.reject("Warm-up build failed: ${buildStatus.message}")
+          else -> callback.setDone()
+        }
       }
     })
 
