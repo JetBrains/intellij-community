06488542df48a3a90587ea8f2c38218b40a4975f
Adam Malek <adam.malek@jetbrains.com>
2023-01-24 15:24:28 +0000

    ML in SE: Always log when under test (IDEA-295364)
    GitOrigin-RevId: 75778054430f596e40cae21f19700a73ca9e4e37

--- plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/SearchEverywhereMlExperiment.kt
+++ plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/SearchEverywhereMlExperiment.kt
@@ -7,6 +7,7 @@ import com.intellij.ide.actions.searcheverywhere.SymbolSearchEverywhereContribut
 import com.intellij.internal.statistic.eventLog.EventLogConfiguration
 import com.intellij.internal.statistic.utils.StatisticsUploadAssistant
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ex.ApplicationManagerEx
 import com.intellij.openapi.util.registry.Registry
 import java.util.*
 
@@ -96,5 +97,7 @@ internal class FeaturesLoggingRandomisation {
 
   private val seed: Double = Random().nextDouble()
 
-  fun shouldLogFeatures(tabId: String): Boolean = seed < (thresholdsByTab[tabId] ?: 1.0)
+  private fun isInTestMode(): Boolean = ApplicationManagerEx.isInIntegrationTest() || ApplicationManagerEx.getApplication().isUnitTestMode
+
+  fun shouldLogFeatures(tabId: String): Boolean = isInTestMode() || (seed < (thresholdsByTab[tabId] ?: 1.0))
 }
