21ebb507a1d642163535685e69492609f98960e2
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-01-27 11:02:08 +0100

    [platform] simpler restarter code on Windows
    GitOrigin-RevId: df4dc6895959f293fe61e377bdbe00aaeef6e94a

--- platform/platform-impl/src/com/intellij/util/Restarter.java
+++ platform/platform-impl/src/com/intellij/util/Restarter.java
@@ -1,9 +1,8 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util;
 
 import com.intellij.execution.configurations.PathEnvironmentVariableUtil;
 import com.intellij.execution.process.OSProcessUtil;
-import com.intellij.jna.JnaLoader;
 import com.intellij.openapi.application.ApplicationNamesInfo;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.diagnostic.Logger;
@@ -11,12 +10,6 @@ import com.intellij.openapi.updateSettings.impl.UpdateInstaller;
 import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.openapi.util.NullableLazyValue;
 import com.intellij.openapi.util.SystemInfo;
-import com.sun.jna.Native;
-import com.sun.jna.Pointer;
-import com.sun.jna.WString;
-import com.sun.jna.platform.win32.WinDef;
-import com.sun.jna.ptr.IntByReference;
-import com.sun.jna.win32.StdCallLibrary;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -29,7 +22,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -46,11 +38,12 @@ public final class Restarter {
   }
 
   private static final NullableLazyValue<Path> ourStarter = lazyNullable(() -> {
-    if (SystemInfo.isWindows && JnaLoader.isLoaded()) {
-      Kernel32 kernel32 = Native.load("kernel32", Kernel32.class);
-      char[] buffer = new char[32767];  // using 32,767 as buffer size to avoid limiting ourselves to MAX_PATH (260)
-      int result = kernel32.GetModuleFileNameW(null, buffer, new WinDef.DWORD(buffer.length)).intValue();
-      if (result != 0) return Path.of(Native.toString(buffer));
+    if (SystemInfo.isWindows) {
+      var name = ApplicationNamesInfo.getInstance().getScriptName() + (Boolean.getBoolean("ide.native.launcher") ? "64.exe" : ".bat");
+      var starter = Path.of(PathManager.getBinPath(), name);
+      if (Files.exists(starter)) {
+        return starter;
+      }
     }
     else if (SystemInfo.isMac) {
       File appDir = new File(PathManager.getHomePath()).getParentFile();
@@ -85,11 +78,8 @@ public final class Restarter {
       }
     }
     else if (SystemInfo.isWindows) {
-      if (!JnaLoader.isLoaded()) {
-        problem = "JNA not loaded";
-      }
-      else if (ourStarter.getValue() == null) {
-        problem = "GetModuleFileName() failed";
+      if (ourStarter.getValue() == null) {
+        problem = "cannot find starter executable in " + PathManager.getBinPath();
       }
       else {
         problem = checkRestarter("restarter.exe");
@@ -168,69 +158,32 @@ public final class Restarter {
   }
 
   private static void restartOnWindows(boolean elevate, String... beforeRestart) throws IOException {
-    Kernel32 kernel32 = Native.load("kernel32", Kernel32.class);
-    Shell32 shell32 = Native.load("shell32", Shell32.class);
+    var starter = ourStarter.getValue();
+    if (starter == null) throw new IOException("Starter executable not found in " + PathManager.getBinPath());
 
-    int pid = OSProcessUtil.getCurrentProcessId();
-    IntByReference argc = new IntByReference();
-    Pointer argvPtr = shell32.CommandLineToArgvW(kernel32.GetCommandLineW(), argc);
-    String[] argv = getRestartArgv(argvPtr.getWideStringArray(0, argc.getValue()));
-    kernel32.LocalFree(argvPtr);
-
-    // See https://blogs.msdn.microsoft.com/oldnewthing/20060515-07/?p=31203
-    // argv[0] as the program name is only a convention, i.e. there is no guarantee the name is the full path to the executable
-    Path starter = ourStarter.getValue();
-    if (starter == null) {
-      throw new IOException("GetModuleFileName() failed");
-    }
-    argv[0] = starter.toString();
-
-    List<String> args = new ArrayList<>();
-    args.add(String.valueOf(pid));
+    var args = new ArrayList<String>();
+    args.add(String.valueOf(ProcessHandle.current().pid()));
 
     if (beforeRestart.length > 0) {
       args.add(String.valueOf(beforeRestart.length));
       Collections.addAll(args, beforeRestart);
     }
 
-    Path launcher;
-    if (elevate && (launcher = PathManager.findBinFile("launcher.exe")) != null) {
-      args.add(String.valueOf(argv.length + 1));
+    if (elevate) {
+      var launcher = PathManager.findBinFile("launcher.exe");
+      if (launcher == null) throw new IOException("Cannot find 'launcher.exe'; please reinstall the IDE");
+      args.add("2");
       args.add(launcher.toString());
     }
     else {
-      args.add(String.valueOf(argv.length));
+      args.add("1");
     }
-    Collections.addAll(args, argv);
+    args.add(starter.toString());
 
-    Path restarter = PathManager.findBinFile("restarter.exe");
-    if (restarter == null) {
-      throw new IOException("Can't find restarter.exe; please reinstall the IDE");
-    }
+    var restarter = PathManager.findBinFile("restarter.exe");
+    if (restarter == null) throw new IOException("Cannot find 'restarter.exe'; please reinstall the IDE");
+
     runRestarter(restarter.toFile(), args);
-
-    // Since the process ID is passed through the command line, we want to make sure we don't exit before the "restarter"
-    // process has a chance to open the handle to our process, and that it doesn't wait for the termination of an unrelated
-    // process that happened to have the same process ID.
-    TimeoutUtil.sleep(500);
-  }
-
-  private static String[] getRestartArgv(String[] argv) {
-    String mainClass = System.getProperty("idea.main.class.name", "com.intellij.idea.Main");
-
-    int countArgs = argv.length;
-    for (int i = argv.length-1; i >=0; i--) {
-      if (argv[i].equals(mainClass) || argv[i].endsWith(".exe")) {
-        countArgs = i + 1;
-        if (argv[i].endsWith(".exe") && argv[i].indexOf(File.separatorChar) < 0) {
-          //absolute path
-          argv[i] = new File(PathManager.getBinPath(), argv[i]).getPath();
-        }
-        break;
-      }
-    }
-
-    return Arrays.copyOf(argv, countArgs);
   }
 
   private static void restartOnMac(String... beforeRestart) throws IOException {
@@ -307,16 +260,5 @@ public final class Restarter {
     catch (Exception e) {
       Logger.getInstance(Restarter.class).warn("Couldn't set DESKTOP_STARTUP_ID", e);
     }
-  }
-
-  @SuppressWarnings({"SameParameterValue", "UnusedReturnValue"})
-  private interface Kernel32 extends StdCallLibrary {
-    WString GetCommandLineW();
-    Pointer LocalFree(Pointer pointer);
-    WinDef.DWORD GetModuleFileNameW(WinDef.HMODULE hModule, char[] lpFilename, WinDef.DWORD nSize);
-  }
-
-  private interface Shell32 extends StdCallLibrary {
-    Pointer CommandLineToArgvW(WString command_line, IntByReference argc);
   }
 }
