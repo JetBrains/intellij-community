8a1e46fa8901455b9e51c205cad8e82f122d907b
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-01-29 21:30:50 +0100

    IDEA-307609 ConfigFileItem moved to intellij.java module
    GitOrigin-RevId: cbbd90224b5c224f08ceef3225c382e58136632e

--- java/openapi/src/com/intellij/util/descriptors/ConfigFileItemSerializer.kt
+++ java/openapi/src/com/intellij/util/descriptors/ConfigFileItemSerializer.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.util.descriptors.impl.workspaceModel
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.util.descriptors
 
 import org.jdom.Element
 import org.jetbrains.annotations.NonNls
