0bb109c6fbd5e20f131e05f2c27ec6bdc6668350
Andrey Vokin <andrey.vokin@jetbrains.com>
2023-01-30 16:24:25 +0100

    DS-4423 Cell's progress bar doesn't work in Gateway notebooks
    Move SteadyUIPanel to visualization, so that to make it work in BackendBlockInlayModelCreator
    
    GitOrigin-RevId: 4245a783b296f6822a9aa5be85f814fdc246322f

--- notebooks/notebook-ui/src/org/jetbrains/plugins/notebooks/ui/NotebookEditorUiUtil.kt
+++ notebooks/notebook-ui/src/org/jetbrains/plugins/notebooks/ui/NotebookEditorUiUtil.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.notebooks.visualization.ui
+package org.jetbrains.plugins.notebooks.ui
 
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.Inlay
@@ -10,8 +10,29 @@ import java.awt.event.ComponentEvent
 import java.awt.event.HierarchyEvent
 import java.lang.ref.WeakReference
 import javax.swing.JComponent
+import javax.swing.JPanel
 import javax.swing.JScrollPane
 import javax.swing.SwingUtilities
+import javax.swing.plaf.PanelUI
+
+/**
+ * A workaround base class for panels with custom UI. Every look and feel change in the IDE (like theme, font family and size, etc.)
+ * leads to call of the `updateUI()`. All default Swing implementations resets the UI to the default one.
+ */
+public open class SteadyUIPanel(private val steadyUi: PanelUI) : JPanel() {
+  init {
+    // Required for correct UI initialization. It leaks in the super class anyway.
+    @Suppress("LeakingThis") setUI(steadyUi)
+  }
+
+  override fun updateUI() {
+    // Update the UI. Don't call super.updateUI() to prevent resetting to the default UI.
+    // There's another way to set specific UI for specific components: by defining java.swing.plaf.ComponentUI#createUI and overriding
+    // java.swing.JComponent#getUIClassID. This approach can't be used in our code since it requires UI classes to have empty constructors,
+    // while in reality some additional data should be provided to UI instances in advance.
+    setUI(steadyUi)
+  }
+}
 
 fun EditorEx.addComponentInlay(
   component: JComponent,
@@ -30,7 +51,6 @@ fun EditorEx.addComponentInlay(
       isRelatedToPrecedingText,
       showAbove,
       showWhenFolded,
-      true,
       priority,
       offset,
     )
