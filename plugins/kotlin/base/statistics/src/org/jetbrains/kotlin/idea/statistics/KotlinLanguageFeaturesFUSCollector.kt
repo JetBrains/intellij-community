990b45eeda222958b549ed4ee4bdd5cf34ac822c
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-01-30 16:53:54 +0100

    Implement feature usage statistics to measure `rangeUntil` operator adoption
    ^KTIJ-22568 Fixed
    
    IJ-MR-102502
    
    GitOrigin-RevId: b011374721f3e7ec1242e5eb32936281a41f45cc

--- plugins/kotlin/base/statistics/src/org/jetbrains/kotlin/idea/statistics/KotlinLanguageFeaturesFUSCollector.kt
+++ plugins/kotlin/base/statistics/src/org/jetbrains/kotlin/idea/statistics/KotlinLanguageFeaturesFUSCollector.kt
@@ -5,53 +5,47 @@ import com.intellij.internal.statistic.eventLog.EventLogGroup
 import com.intellij.internal.statistic.eventLog.events.EventFields
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
 import com.intellij.openapi.vfs.VirtualFile
-import org.jetbrains.annotations.ApiStatus
-
-@ApiStatus.Internal
-enum class AllowedListOfLanguageFeatures {OLD_UNTIL, NEW_RANGE_UNTIL}
 
 class KotlinLanguageFeaturesFUSCollector : CounterUsagesCollector() {
 
     override fun getGroup(): EventLogGroup = GROUP
 
-    companion object {
-        private val GROUP = EventLogGroup("kotlin.language.features", 1)
+    object RangeUntil {
+        private const val RANGE_UNTIL_FEATURE = "range_until_feature"
+        private val rangeUntilEventField = EventFields.Enum<RangeUntilLanguageFeature>(RANGE_UNTIL_FEATURE)
+        private enum class RangeUntilLanguageFeature {
+            OLD_UNTIL_SEEN,
+            NEW_RANGE_UNTIL_SEEN,
+            UNTIL_TO_RANGE_UNTIL_QUICK_FIX_IS_APPLIED
+        }
 
-        private val feature_seen = EventFields.Enum<AllowedListOfLanguageFeatures>("feature_seen")
-        private val feature_used = EventFields.Enum<AllowedListOfLanguageFeatures>("feature_used")
-        private val kotlinLanguageVersionField = EventFields.StringValidatedByRegexp("kotlin_language_version", "version_lang_api")
-
-        private val feature_seen_event = GROUP.registerVarargEvent(
-            "feature_seen",
-            feature_seen,
+        private val rangeUntilFeatureEvent = GROUP.registerVarargEvent(
+            RANGE_UNTIL_FEATURE,
+            rangeUntilEventField,
             EventFields.AnonymizedPath,
             kotlinLanguageVersionField
         )
 
-        private val feature_used_event = GROUP.registerVarargEvent(
-            "feature_used",
-            feature_used,
-            EventFields.AnonymizedPath,
-            kotlinLanguageVersionField
-        )
+        fun logOldUntilOccurence(file: VirtualFile, kotlinLanguageVersion: String): Unit =
+            log(RangeUntilLanguageFeature.OLD_UNTIL_SEEN, file, kotlinLanguageVersion)
 
-        fun logOldUntilOccurence(
+        fun logNewRangeUntilOccurence(file: VirtualFile, kotlinLanguageVersion: String): Unit =
+            log(RangeUntilLanguageFeature.NEW_RANGE_UNTIL_SEEN, file, kotlinLanguageVersion)
+
+        fun logUntilToRangeUntilQuickFixIsApplied(file: VirtualFile, kotlinLanguageVersion: String): Unit =
+            log(RangeUntilLanguageFeature.UNTIL_TO_RANGE_UNTIL_QUICK_FIX_IS_APPLIED, file, kotlinLanguageVersion)
+
+        private fun log(
+            event: RangeUntilLanguageFeature,
             file: VirtualFile,
-            kotlinLanguageVersion: String
-        ) = feature_seen_event.log(
-            feature_seen.with(AllowedListOfLanguageFeatures.OLD_UNTIL),
+            kotlinLanguageVersion: String,
+        ): Unit = rangeUntilFeatureEvent.log(
+            rangeUntilEventField.with(event),
             EventFields.AnonymizedPath.with(file.path),
-            this.kotlinLanguageVersionField.with(kotlinLanguageVersion)
+            kotlinLanguageVersionField.with(kotlinLanguageVersion)
         )
-
-        fun logNewRangeUntilOccurence(
-            file: VirtualFile,
-            kotlinLanguageVersion: String
-        ) = feature_seen_event.log(
-            feature_seen.with(AllowedListOfLanguageFeatures.NEW_RANGE_UNTIL),
-            EventFields.AnonymizedPath.with(file.path),
-            this.kotlinLanguageVersionField.with(kotlinLanguageVersion)
-        )
-
     }
 }
+
+private val GROUP = EventLogGroup("kotlin.language.features", 1)
+private val kotlinLanguageVersionField = EventFields.StringValidatedByRegexp("kotlin_language_version", "version_lang_api")
