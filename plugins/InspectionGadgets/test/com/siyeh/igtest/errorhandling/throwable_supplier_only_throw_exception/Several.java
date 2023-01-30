7af3a71b61da95c8fd11dbff92c7f012916faea0
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-01-30 17:34:06 +0100

    [java-inspections] IDEA-309236: ThrowableSupplierOnlyThrowException in orElseThrow. Review
    GitOrigin-RevId: e94f1649a5dc0a9d39f936fe784a3d9f50e193d3

--- plugins/InspectionGadgets/test/com/siyeh/igtest/errorhandling/throwable_supplier_only_throw_exception/Several.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/errorhandling/throwable_supplier_only_throw_exception/Several.java
@@ -3,7 +3,7 @@ import java.util.Optional;
 public class Several {
   public static void main(String[] args) {
     Optional<String> optional = Optional.ofNullable("1");
-
+    optional.orElseThrow(() -> new RuntimeException());
     optional.<warning descr="Throwable supplier doesn't return any exception">orElseThrow<caret></warning>(() -> {
       if (args.length == 1) {
         throw new RuntimeException();
