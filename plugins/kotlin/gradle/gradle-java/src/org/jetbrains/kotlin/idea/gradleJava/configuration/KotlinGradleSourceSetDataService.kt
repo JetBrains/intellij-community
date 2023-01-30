467c85520f2b99c0d746cec3d987a224c52317a0
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2023-01-30 23:17:42 +0900

    [kotlin] Make 'KotlinGradleFacadeImpl' a class (KTIJ-24476)
    Services are instantiated by the platform, breaking the objects' singleton contract.
    
    GitOrigin-RevId: e5eb972221ff91e97cb1872c0ebb67c652665cb9

--- plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/KotlinGradleSourceSetDataService.kt
+++ plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/KotlinGradleSourceSetDataService.kt
@@ -31,6 +31,7 @@ import org.jetbrains.kotlin.config.KotlinFacetSettings
 import org.jetbrains.kotlin.config.TargetPlatformKind
 import org.jetbrains.kotlin.extensions.ProjectExtensionDescriptor
 import org.jetbrains.kotlin.idea.base.codeInsight.tooling.tooling
+import org.jetbrains.kotlin.idea.base.externalSystem.KotlinGradleFacade
 import org.jetbrains.kotlin.idea.base.externalSystem.findAll
 import org.jetbrains.kotlin.idea.base.platforms.KotlinCommonLibraryKind
 import org.jetbrains.kotlin.idea.base.platforms.KotlinJavaScriptLibraryKind
@@ -280,7 +281,7 @@ fun configureFacetByGradleModule(
 
     val compilerVersion = kotlinGradleSourceSetDataNode?.data?.kotlinPluginVersion?.let(IdeKotlinVersion::opt)
     // required for GradleFacetImportTest.{testCommonImportByPlatformPlugin, testKotlinAndroidPluginDetection}
-        ?: KotlinGradleFacadeImpl.findKotlinPluginVersion(moduleNode)
+        ?: KotlinGradleFacade.instance?.findKotlinPluginVersion(moduleNode)
 
     if (compilerVersion == null) {
         logger.error("[Kotlin Facet]: cannot create facet for module '${ideModule.name}' due to unknown compiler version. " +
