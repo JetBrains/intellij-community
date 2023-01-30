8c574d1661914b3b71d6a9791f93201955b36a98
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2023-01-30 15:52:41 +0100

    [kt, tests] Couple of minor fixes
    - look for env variables stricter on CI and don't fallback to default
    
    - fix few typos
    
    - remove 'compositeBuild/jvmAndNative' committed by mistake
    
    - rename *.Values enums -> *.Value
    
    GitOrigin-RevId: 1571d9b02e27150cbbd5c9b0d6884400108cf52f

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/newTests/KotlinTestProperties.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/newTests/KotlinTestProperties.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.gradle.newTests
 
+import com.intellij.testFramework.UsefulTestCase
+
 interface KotlinTestsResolvableProperty {
     val id: String
     val valuesByAcronyms: Map<String, String>
@@ -8,7 +10,13 @@ interface KotlinTestsResolvableProperty {
 }
 
 fun KotlinTestsResolvableProperty.resolveFromEnvironment(): String {
-    val acronymOrValue = System.getenv()[id.uppercase()] ?: return defaultValue
+    val acronymOrValue = System.getenv()[id.uppercase()] ?:
+        if (!UsefulTestCase.IS_UNDER_TEAMCITY)
+            return defaultValue
+        else
+            error("Error: can't find environment variable ${id.uppercase()} required for CI runs")
+
+
     if (acronymOrValue in valuesByAcronyms.keys) return valuesByAcronyms[acronymOrValue]!!
     if (acronymOrValue in valuesByAcronyms.values) return acronymOrValue
 
