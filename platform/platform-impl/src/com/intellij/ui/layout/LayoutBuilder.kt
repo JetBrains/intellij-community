ac891c6b6ca3d1278aa82478f17141cd65892550
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2023-01-31 14:43:30 +0100

    [Kotlin UI DSL] Restored some API because of incompatibility
    -  Product:PhpStorm 231.4840.331 Plugin: Projector 1.11.0
    Caused by: java.lang.NoSuchMethodError: 'com.intellij.ui.layout.CellBuilder com.intellij.ui.layout.RowBuilderWithButtonGroupProperty.radioButton$default(com.intellij.ui.layout.RowBuilderWithButtonGroupProperty, com.intellij.ui.layout.Row, java.lang.String, java.lang.Object, java.lang.String, int, java.lang.Object)'
    
    GitOrigin-RevId: 85d89bef9e9e36ba3fe0af4630bc281d879da70e

--- platform/platform-impl/src/com/intellij/ui/layout/LayoutBuilder.kt
+++ platform/platform-impl/src/com/intellij/ui/layout/LayoutBuilder.kt
@@ -6,8 +6,12 @@ import com.intellij.openapi.actionSystem.PlatformCoreDataKeys
 import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
+import com.intellij.openapi.util.NlsContexts
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.components.JBRadioButton
 import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.Nls
+import javax.swing.AbstractButton
 import javax.swing.ButtonGroup
 
 @ApiStatus.ScheduledForRemoval
@@ -38,9 +42,31 @@ class CellBuilderWithButtonGroupProperty<T : Any>
 @Deprecated("Use Kotlin UI DSL Version 2")
 class RowBuilderWithButtonGroupProperty<T : Any>
 @PublishedApi internal constructor(private val builder: RowBuilder, private val prop: PropertyBinding<T>) : RowBuilder by builder {
+
+  @Deprecated("Use Kotlin UI DSL Version 2", level = DeprecationLevel.HIDDEN)
+  fun Row.radioButton(@NlsContexts.RadioButton text: String, value: T, @Nls comment: String? = null): CellBuilder<JBRadioButton> {
+    val component = JBRadioButton(text, prop.get() == value)
+    attachSubRowsEnabled(component)
+    return component(comment = comment).bindValueToProperty(prop, value)
+  }
+
+  @Deprecated("Use Kotlin UI DSL Version 2", level = DeprecationLevel.HIDDEN)
+  fun CellBuilder<JBRadioButton>.bindValue(value: T): CellBuilder<JBRadioButton> = bindValueToProperty(prop, value)
+}
+
+@Deprecated("Use Kotlin UI DSL Version 2")
+private fun <T> CellBuilder<JBRadioButton>.bindValueToProperty(prop: PropertyBinding<T>, value: T): CellBuilder<JBRadioButton> = apply {
+  onApply { if (component.isSelected) prop.set(value) }
+  onReset { component.isSelected = prop.get() == value }
+  onIsModified { component.isSelected != (prop.get() == value) }
 }
 
 fun FileChooserDescriptor.chooseFile(event: AnActionEvent, fileChosen: (chosenFile: VirtualFile) -> Unit) {
   FileChooser.chooseFile(this, event.getData(PlatformDataKeys.PROJECT), event.getData(PlatformCoreDataKeys.CONTEXT_COMPONENT), null,
                          fileChosen)
+}
+
+private fun Row.attachSubRowsEnabled(component: AbstractButton) {
+  subRowsEnabled = component.selected()
+  component.selected.addListener { subRowsEnabled = it }
 }
