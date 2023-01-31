1bdc070fa59c52d969017ca68d4c5be21915ccef
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2023-01-31 14:10:47 +0100

    [workspace model] add javadoc to VirtualFileUrl
    GitOrigin-RevId: 81e2d0b7ebb9db016c33b1107062234904748f33

--- platform/workspaceModel/storage/src/com/intellij/workspaceModel/storage/url/VirtualFileUrl.java
+++ platform/workspaceModel/storage/src/com/intellij/workspaceModel/storage/url/VirtualFileUrl.java
@@ -1,11 +1,21 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.workspaceModel.storage.url;
 
-import org.jetbrains.annotations.ApiStatus;
-
 import java.util.List;
 
+/**
+ * Represents a reference to a file or directory. This interface must be used instead of {@link String} for properties of workspace model
+ * entities. It stores a URL of the file inside in a compact form.
+ * <br>
+ * Use {@link VirtualFileUrlManager#fromUrl} or {@link com.intellij.workspaceModel.ide.VirtualFileUrls#toVirtualFileUrl toVirtualFileUrl}
+ * extension function to get an instance of this interface. 
+ * Use {@link com.intellij.workspaceModel.ide.VirtualFileUrls#getVirtualFile virtualFile} extension property to locate a 
+ * {@link com.intellij.openapi.vfs.VirtualFile VirtualFile} instance by an instance of this interface.
+ */
 public interface VirtualFileUrl {
+  /**
+   * Returns URL in the Virtual File System format.
+   */
   String getUrl();
 
   default String getUrlWithoutTrailingSlash() {
