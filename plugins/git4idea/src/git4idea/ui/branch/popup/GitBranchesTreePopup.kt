be6d739ecab944f6e44c590c0a6578ad79ff07da
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2023-01-31 13:25:41 +0100

    git: do not skip branches tree update while pasting string with space characters (IDEA-311908)
    This relates to paste action only, since typing of space characters rejected by the corresponding dispatch method in the branches tree popup.
    Also, trim special space characters like nbsp.
    
    GitOrigin-RevId: 884a4f9e7d03cf14b9ad1d4fe56e783e5568ab98

--- plugins/git4idea/src/git4idea/ui/branch/popup/GitBranchesTreePopup.kt
+++ plugins/git4idea/src/git4idea/ui/branch/popup/GitBranchesTreePopup.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.ui.branch.popup
 
 import com.intellij.dvcs.branch.DvcsBranchManager
@@ -640,9 +640,8 @@ class GitBranchesTreePopup(project: Project, step: GitBranchesTreePopupStep, par
 
   override fun onSpeedSearchPatternChanged() {
     val currentPrefix = speedSearch.enteredPrefix
-    if (currentPrefix?.endsWith(" ") == true) {
+    if (currentPrefix?.lastOrNull()?.isWhitespace() == true) {
       speedSearch.updatePattern(currentPrefix.trimEnd())
-      return
     }
 
     with(uiScope(this)) {
