4790d6c1777d911309e0700369b370c3c84797c3
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2023-01-31 21:04:46 +0100

    Remove useless iteration
    GitOrigin-RevId: fa6f2aca12fc280ac5aac858a141503735cfe5f0

--- platform/lang-impl/src/com/intellij/util/indexing/dependenciesCache/RescannedRootsUtil.java
+++ platform/lang-impl/src/com/intellij/util/indexing/dependenciesCache/RescannedRootsUtil.java
@@ -22,9 +22,6 @@ import com.intellij.util.indexing.roots.IndexableEntityProvider.IndexableIterato
 import com.intellij.util.indexing.roots.IndexingRootsCollectionUtil;
 import com.intellij.util.indexing.roots.builders.IndexableIteratorBuilders;
 import com.intellij.util.indexing.roots.builders.SyntheticLibraryIteratorBuilder;
-import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileIndexContributor;
-import com.intellij.workspaceModel.core.fileIndex.impl.PlatformInternalWorkspaceFileIndexContributor;
-import com.intellij.workspaceModel.core.fileIndex.impl.WorkspaceFileIndexImpl;
 import com.intellij.workspaceModel.ide.WorkspaceModel;
 import com.intellij.workspaceModel.ide.impl.legacyBridge.library.LibraryEntityUtils;
 import com.intellij.workspaceModel.ide.legacyBridge.ModuleBridge;
@@ -147,23 +144,15 @@ class RescannedRootsUtil {
       new IndexingRootsCollectionUtil.IndexingRootsCollectionSettings();
     settings.collectModuleAwareContent = false;
 
-    for (WorkspaceFileIndexContributor<?> contributor : WorkspaceFileIndexImpl.Companion.getEP_NAME().getExtensionList()) {
-      if (contributor instanceof PlatformInternalWorkspaceFileIndexContributor) {
-        continue;
-      }
-      IndexingRootsCollectionUtil.IndexingRootsDescriptions roots =
-        IndexingRootsCollectionUtil.collectRootsFromWorkspaceFileIndexContributors(project, current, settings);
-      Iterator<VirtualFile> rootsIterator = excludedRoots.iterator();
-      while (rootsIterator.hasNext()) {
-        VirtualFile next = rootsIterator.next();
-        Collection<? extends IndexableIteratorBuilder> builders = IndexingRootsCollectionUtil.createBuilderForFile(roots, next);
-        if (!builders.isEmpty()) {
-          result.addAll(builders);
-          rootsIterator.remove();
-        }
-      }
-      if (excludedRoots.isEmpty()) {
-        break;
+    IndexingRootsCollectionUtil.IndexingRootsDescriptions roots =
+      IndexingRootsCollectionUtil.collectRootsFromWorkspaceFileIndexContributors(project, current, settings);
+    Iterator<VirtualFile> rootsIterator = excludedRoots.iterator();
+    while (rootsIterator.hasNext()) {
+      VirtualFile next = rootsIterator.next();
+      Collection<? extends IndexableIteratorBuilder> builders = IndexingRootsCollectionUtil.createBuilderForFile(roots, next);
+      if (!builders.isEmpty()) {
+        result.addAll(builders);
+        rootsIterator.remove();
       }
     }
 
