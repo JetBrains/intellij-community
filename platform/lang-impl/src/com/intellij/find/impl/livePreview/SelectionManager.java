9c1d5992cf0f15eb2d0f80f51734bc33b501654f
Bas Leijdekkers <basleijdekkers@gmail.com>
2023-02-01 15:04:38 +0100

    [search] don't remove selection if not found and searching in selection (IDEA-296030)
    GitOrigin-RevId: 1a1241f0790f5c5c9158b62bc6bd193cc78e6a04

--- platform/lang-impl/src/com/intellij/find/impl/livePreview/SelectionManager.java
+++ platform/lang-impl/src/com/intellij/find/impl/livePreview/SelectionManager.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.find.impl.livePreview;
 
 import com.intellij.find.EditorSearchSession;
+import com.intellij.find.FindModel;
 import com.intellij.find.FindResult;
 import com.intellij.find.FindUtil;
 import com.intellij.openapi.editor.*;
@@ -40,12 +27,15 @@ public class SelectionManager {
     if (removeAllPreviousSelections) {
       editor.getCaretModel().removeSecondaryCarets();
     }
+    FindModel findModel = mySearchResults.getFindModel();
     final FindResult cursor = mySearchResults.getCursor();
     if (cursor == null) {
-      if (removePreviousSelection && !myHadSelectionInitially) editor.getSelectionModel().removeSelection();
+      if (removePreviousSelection && !myHadSelectionInitially && findModel.isGlobal()) {
+        editor.getSelectionModel().removeSelection();
+      }
       return;
     }
-    if (mySearchResults.getFindModel().isGlobal()) {
+    if (findModel.isGlobal()) {
       if (removePreviousSelection || removeAllPreviousSelections) {
         FoldingModel foldingModel = editor.getFoldingModel();
         final FoldRegion[] allRegions = editor.getFoldingModel().getAllFoldRegions();
