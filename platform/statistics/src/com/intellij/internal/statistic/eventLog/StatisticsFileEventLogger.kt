60357b5ea9d66708e17abe223056d4a187b47825
Victor Matchenko <Victor.Matchenko@jetbrains.com>
2023-02-01 16:29:42 +0100

    IDEA-312169 StatisticsFileEventLogger logExecutor name is dynamic
    GitOrigin-RevId: ddc0452f983eb6f586a1b71aa8d5a542d99d5aa8

--- platform/statistics/src/com/intellij/internal/statistic/eventLog/StatisticsFileEventLogger.kt
+++ platform/statistics/src/com/intellij/internal/statistic/eventLog/StatisticsFileEventLogger.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.internal.statistic.eventLog
 
 import com.intellij.internal.statistic.eventLog.validator.IntellijSensitiveDataValidator
@@ -23,7 +23,7 @@ open class StatisticsFileEventLogger(private val recorderId: String,
                                      private val mergeStrategy: StatisticsEventMergeStrategy = FilteredEventMergeStrategy(emptySet()),
                                      private val ideMode: String? = null
 ) : StatisticsEventLogger, Disposable {
-  protected val logExecutor = AppExecutorUtil.createBoundedApplicationPoolExecutor("StatisticsFileEventLogger: $sessionId", 1)
+  protected val logExecutor = AppExecutorUtil.createBoundedApplicationPoolExecutor("StatisticsFileEventLogger", 1)
 
   private var lastEvent: FusEvent? = null
   private var lastEventTime: Long = 0
