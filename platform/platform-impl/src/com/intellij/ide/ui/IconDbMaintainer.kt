5baf2bc96ac605c927682fdda71fa395df9e5ec1
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-02-02 14:35:04 +0100

    IconDbMaintainer - use injected coroutine scope
    GitOrigin-RevId: 187ece0fa13ff4528c34baa903197ef38aa80331

--- platform/platform-impl/src/com/intellij/ide/ui/IconDbMaintainer.kt
+++ platform/platform-impl/src/com/intellij/ide/ui/IconDbMaintainer.kt
@@ -1,31 +1,25 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.ui
 
-import com.intellij.openapi.Disposable
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.util.SVGLoader
-import com.intellij.util.childScope
 import kotlinx.coroutines.*
 import kotlin.time.Duration.Companion.minutes
 
 // icons maybe loaded before app loaded, so, SvgCacheMapper cannot be as a service
-private class IconDbMaintainer : Disposable {
-  @Suppress("DEPRECATION")
-  private val coroutineScope: CoroutineScope = ApplicationManager.getApplication().coroutineScope.childScope()
-
+private class IconDbMaintainer(coroutineScope: CoroutineScope) {
   init {
     coroutineScope.launch(CoroutineName("auto-save icon cache")) {
-      delay(5.minutes)
-      SVGLoader.persistentCache?.save()
-    }
-  }
-
-  override fun dispose() {
-    try {
-      SVGLoader.persistentCache?.close()
-    }
-    finally {
-      coroutineScope.cancel()
+      try {
+        while (true) {
+          delay(5.minutes)
+          SVGLoader.persistentCache?.save()
+        }
+      }
+      finally {
+        withContext(NonCancellable) {
+          SVGLoader.persistentCache?.close()
+        }
+      }
     }
   }
 }
