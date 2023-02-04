c380a887b25efb269e1bba38e336fa4df4edc0a7
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2023-02-04 15:21:28 +0100

    [vcs-log-graph] convert Selection to a sealed class
    GitOrigin-RevId: fb426e84ad5f6010242399bf116b34aaa6eb0b11

--- platform/vcs-log/graph/src/com/intellij/vcs/log/graph/impl/facade/PrintElementPresentationManagerImpl.kt
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/graph/impl/facade/PrintElementPresentationManagerImpl.kt
@@ -11,24 +11,23 @@ import com.intellij.vcs.log.graph.api.permanent.PermanentGraphInfo
 import com.intellij.vcs.log.graph.api.printer.PrintElementPresentationManager
 import com.intellij.vcs.log.graph.impl.print.ColorGetterByLayoutIndex
 import com.intellij.vcs.log.graph.impl.print.elements.PrintElementWithGraphElement
-import java.util.*
 
 internal class PrintElementPresentationManagerImpl<CommitId>(permanentGraphInfo: PermanentGraphInfo<CommitId>,
                                                              private val linearGraph: LinearGraph,
                                                              colorManager: GraphColorManager<CommitId>) : PrintElementPresentationManager {
   private val colorGetter = ColorGetterByLayoutIndex(linearGraph, permanentGraphInfo, colorManager)
-  private var selection: Selection = Selection(linearGraph, emptySet())
+  private var selection: Selection = Selection.FromNodeIds(linearGraph, emptySet())
 
   override fun isSelected(printElement: PrintElementWithGraphElement): Boolean {
     return selection.isSelected(printElement)
   }
 
   fun setSelectedElement(printElement: PrintElementWithGraphElement): Boolean {
-    return setSelection(Selection(linearGraph, printElement))
+    return setSelection(Selection.FromPrintElement(printElement))
   }
 
   fun setSelectedElements(selectedNodeId: Set<Int>): Boolean {
-    return setSelection(Selection(linearGraph, selectedNodeId))
+    return setSelection(Selection.FromNodeIds(linearGraph, selectedNodeId))
   }
 
   private fun setSelection(newSelection: Selection): Boolean {
@@ -41,41 +40,42 @@ internal class PrintElementPresentationManagerImpl<CommitId>(permanentGraphInfo:
     return colorGetter.getColorId(element)
   }
 
-  class Selection private constructor(private val linearGraph: LinearGraph,
-                                      private val selectedPrintElement: PrintElementWithGraphElement?,
-                                      private val selectedNodeIds: Set<Int>) {
-    constructor(linearGraph: LinearGraph, selectedNodeId: Set<Int>) : this(linearGraph, null, selectedNodeId)
-    constructor(linearGraph: LinearGraph, printElement: PrintElementWithGraphElement) : this(linearGraph, printElement, emptySet<Int>())
+  sealed class Selection {
 
-    fun isSelected(printElement: PrintElementWithGraphElement): Boolean {
-      if (printElement == selectedPrintElement) return true
+    abstract fun isSelected(printElement: PrintElementWithGraphElement): Boolean
 
-      val graphElement = printElement.graphElement
-      if (graphElement is GraphNode) {
-        val nodeId = linearGraph.getNodeId(graphElement.nodeIndex)
-        return selectedNodeIds.contains(nodeId)
+    data class FromPrintElement(private val selectedPrintElement: PrintElementWithGraphElement) : Selection() {
+      override fun isSelected(printElement: PrintElementWithGraphElement): Boolean {
+        return printElement == selectedPrintElement
       }
-      if (graphElement is GraphEdge) {
-        var selected = graphElement.targetId == null || selectedNodeIds.contains(graphElement.targetId)
-        selected = selected and
-          (graphElement.upNodeIndex == null || selectedNodeIds.contains(linearGraph.getNodeId(graphElement.upNodeIndex!!)))
-        selected = selected and
-          (graphElement.downNodeIndex == null || selectedNodeIds.contains(linearGraph.getNodeId(graphElement.downNodeIndex!!)))
-        return selected
-      }
-      return false
     }
 
-    override fun equals(other: Any?): Boolean {
-      if (this === other) return true
-      if (other == null || javaClass != other.javaClass) return false
-      val selection = other as Selection
-      return selectedPrintElement == selection.selectedPrintElement &&
-             Comparing.haveEqualElements(selectedNodeIds, selection.selectedNodeIds)
-    }
+    class FromNodeIds(private val linearGraph: LinearGraph, private val selectedNodeIds: Set<Int>) : Selection() {
+      override fun isSelected(printElement: PrintElementWithGraphElement): Boolean {
+        val graphElement = printElement.graphElement
+        if (graphElement is GraphNode) {
+          val nodeId = linearGraph.getNodeId(graphElement.nodeIndex)
+          return selectedNodeIds.contains(nodeId)
+        }
+        if (graphElement is GraphEdge) {
+          var selected = graphElement.targetId == null || selectedNodeIds.contains(graphElement.targetId)
+          selected = selected and
+            (graphElement.upNodeIndex == null || selectedNodeIds.contains(linearGraph.getNodeId(graphElement.upNodeIndex!!)))
+          selected = selected and
+            (graphElement.downNodeIndex == null || selectedNodeIds.contains(linearGraph.getNodeId(graphElement.downNodeIndex!!)))
+          return selected
+        }
+        return false
+      }
 
-    override fun hashCode(): Int {
-      return 31 * Objects.hashCode(selectedPrintElement) + Comparing.unorderedHashcode(selectedNodeIds)
+      override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is FromNodeIds) return false
+
+        return Comparing.haveEqualElements(selectedNodeIds, other.selectedNodeIds)
+      }
+
+      override fun hashCode() = Comparing.unorderedHashcode(selectedNodeIds)
     }
   }
 }
