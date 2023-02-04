0c7e007203f0088eacba50ccae7192ad6100147b
Alexandr Evstigneev <alexandr.evstigneev@jetbrains.com>
2023-02-04 12:10:53 +0400

    IDEA-312416 do not duplicate empty quick fixes arrays
    According to the API we could just return null, but even our code base has implementations relying on this method return non-null values, despite the annotation says otherwise.
    
    GitOrigin-RevId: e88c1b045504d209910cbc4db7b6c7c92ac4d476

--- platform/analysis-impl/src/com/intellij/codeInspection/CommonProblemDescriptorImpl.java
+++ platform/analysis-impl/src/com/intellij/codeInspection/CommonProblemDescriptorImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInspection;
 
 import com.intellij.codeInspection.util.InspectionMessage;
@@ -15,20 +15,23 @@ public class CommonProblemDescriptorImpl implements CommonProblemDescriptor {
   private final @InspectionMessage String myDescriptionTemplate;
 
   CommonProblemDescriptorImpl(@NotNull @InspectionMessage String descriptionTemplate, QuickFix<?> @Nullable [] fixes) {
+    QuickFix<?>[] filteredFixes = fixes;
     if (fixes != null && fixes.length > 0) {
-      myFixes = ArrayUtil.contains(null, fixes) ? ContainerUtil.mapNotNull(fixes, FunctionUtil.id(), ArrayUtil.newArray(ArrayUtil.getComponentType(fixes), 0))
-                                                : fixes;
+      if (ArrayUtil.contains(null, fixes)) {
+        filteredFixes = ContainerUtil.mapNotNull(fixes, FunctionUtil.id(), ArrayUtil.newArray(ArrayUtil.getComponentType(fixes), 0));
+      }
       if (!(this instanceof ProblemDescriptor)) {
         for (QuickFix<?> fix : fixes) {
           if (fix instanceof LocalQuickFix) {
-            LOG.error("Local quick fix expect ProblemDescriptor, but here only CommonProblemDescriptor available: " + this.getClass().getName() + "; descr: " + descriptionTemplate);
+            LOG.error("Local quick fix expect ProblemDescriptor, but here only CommonProblemDescriptor available: " +
+                      this.getClass().getName() +
+                      "; descr: " +
+                      descriptionTemplate);
           }
         }
       }
     }
-    else {
-      myFixes = fixes;
-    }
+    myFixes = filteredFixes != null && filteredFixes.length == 0 ? null : filteredFixes;
     myDescriptionTemplate = descriptionTemplate;
   }
 
@@ -40,7 +43,7 @@ public class CommonProblemDescriptorImpl implements CommonProblemDescriptor {
 
   @Override
   public QuickFix<?> @Nullable [] getFixes() {
-    return myFixes;
+    return myFixes == null ? QuickFix.EMPTY_ARRAY : myFixes;
   }
 
   @Override
--- platform/analysis-impl/src/com/intellij/codeInspection/ProblemDescriptorBase.java
+++ platform/analysis-impl/src/com/intellij/codeInspection/ProblemDescriptorBase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInspection;
 
 import com.intellij.codeInspection.util.InspectionMessage;
@@ -18,6 +18,7 @@ import com.intellij.psi.util.PsiUtilCore;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class ProblemDescriptorBase extends CommonProblemDescriptorImpl implements ProblemDescriptor {
@@ -286,6 +287,7 @@ public class ProblemDescriptorBase extends CommonProblemDescriptorImpl implement
 
   @Override
   public LocalQuickFix @Nullable [] getFixes() {
-    return (LocalQuickFix[])super.getFixes();
+    var fixes = Objects.requireNonNull(super.getFixes());
+    return fixes.length == 0 ? LocalQuickFix.EMPTY_ARRAY : (LocalQuickFix[])fixes;
   }
 }
