58a7339be3595aefd7e7b90fe9bcf00e1d24e2d8
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2023-02-06 17:57:39 +0100

    [js debugger] fix connecting to node (WEB-59376)
    GitOrigin-RevId: aece61eb6e9b6b2a678e59f6171108bda17628a6

--- platform/script-debugger/debugger-ui/src/RemoteVmConnection.kt
+++ platform/script-debugger/debugger-ui/src/RemoteVmConnection.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.debugger.connection
 
-import com.intellij.execution.ExecutionException
 import com.intellij.execution.process.ProcessHandler
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.ui.popup.JBPopupFactory
@@ -15,14 +14,11 @@ import com.intellij.util.io.socketConnection.ConnectionStatus
 import com.intellij.xdebugger.XDebuggerBundle
 import io.netty.bootstrap.Bootstrap
 import org.jetbrains.concurrency.*
-import org.jetbrains.debugger.ScriptDebuggerBundle
 import org.jetbrains.debugger.Vm
 import org.jetbrains.io.NettyUtil
 import org.jetbrains.rpc.LOG
 import java.net.ConnectException
-import java.net.InetAddress
 import java.net.InetSocketAddress
-import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicReference
 import java.util.function.Consumer
 import javax.swing.JList
