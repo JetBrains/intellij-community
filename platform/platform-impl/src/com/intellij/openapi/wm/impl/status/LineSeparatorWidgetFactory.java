4c9174632a7a8dad46be86aa33566fe6f22f2c13
Aleksei Pomelov <aleksei.pomelov@jetbrains.com>
2023-02-06 14:10:40 +0200

    IDEA-310426: Open externally used classes (16353-for-mainframe)
    GitOrigin-RevId: 39711debc82c87cd3ae68f9b353d96028092d9ff

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/status/LineSeparatorWidgetFactory.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/status/LineSeparatorWidgetFactory.java
@@ -7,9 +7,12 @@ import com.intellij.openapi.wm.StatusBarWidget;
 import com.intellij.openapi.wm.impl.status.widget.StatusBarEditorBasedWidgetFactory;
 import com.intellij.ui.UIBundle;
 import kotlinx.coroutines.CoroutineScope;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-final class LineSeparatorWidgetFactory extends StatusBarEditorBasedWidgetFactory {
+
+@ApiStatus.NonExtendable
+public class LineSeparatorWidgetFactory extends StatusBarEditorBasedWidgetFactory {
   @Override
   public @NotNull String getId() {
     return StatusBar.StandardWidgets.LINE_SEPARATOR_PANEL;
