78fde5b797f3a929651f5936cbe43e74b9699586
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-02-06 11:55:45 +0100

    RunToolbarInitializeService must be not executed in headless mode
    GitOrigin-RevId: df52395669e712e36b909750f4283a9ec71a3ce8

--- platform/execution-impl/src/com/intellij/execution/runToolbar/RunToolbarInitializeService.kt
+++ platform/execution-impl/src/com/intellij/execution/runToolbar/RunToolbarInitializeService.kt
@@ -1,7 +1,9 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.runToolbar
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.EDT
+import com.intellij.openapi.extensions.ExtensionNotApplicableException
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectPostStartupActivity
 import kotlinx.coroutines.Dispatchers
@@ -10,6 +12,12 @@ import kotlinx.coroutines.withContext
 import kotlin.time.Duration.Companion.seconds
 
 private class RunToolbarInitializeService : ProjectPostStartupActivity {
+  init {
+    if (ApplicationManager.getApplication().isHeadlessEnvironment) {
+      throw ExtensionNotApplicableException.create()
+    }
+  }
+
   override suspend fun execute(project: Project) {
     delay(5.seconds)
     val toolbarSlotManager = RunToolbarSlotManager.getInstance(project)
