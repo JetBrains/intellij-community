8c54342f13a5bee0c355526fc173e28bb36b10a2
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2023-02-06 14:26:21 +0200

    [UI] IDEA-256861 Set default ide.tree.autoscrollToVCenter value to false
    It was agreed in an internal discussion to try to test it in the upcoming EAP.
    
    GitOrigin-RevId: a8ae9e3b39f1150771032ffe08917afa300909f9

--- platform/platform-api/src/com/intellij/util/ui/tree/TreeUtil.java
+++ platform/platform-api/src/com/intellij/util/ui/tree/TreeUtil.java
@@ -1609,7 +1609,7 @@ public final class TreeUtil {
     if (paths.length == 0) return;
     tree.setSelectionPaths(paths);
     for (TreePath path : paths) {
-      if (scrollToVisible(tree, path, Registry.is("ide.tree.autoscrollToVCenter", true))) {
+      if (scrollToVisible(tree, path, Registry.is("ide.tree.autoscrollToVCenter", false))) {
         break;
       }
     }
