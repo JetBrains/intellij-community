ecfa7dd1b4bc1b5ffba120f4de7c2521fb9c9568
Eugene Zhuravlev <eugene.zhuravlev@jetbrains.com>
2023-02-06 16:04:41 +0100

    review followup for external access token providers
    - javadoc added
    - compare ServiceID case-insensitively
    
    (cherry picked from commit 9a761b5afd96e3324bca1e6c6b84b049fbd939f9)
    
    GitOrigin-RevId: 85c3d43081e8be5587cf11a84a5d2f92eb9f6baf

--- platform/platform-impl/src/com/intellij/ide/impl/AuthorizationProvider.java
+++ platform/platform-impl/src/com/intellij/ide/impl/AuthorizationProvider.java
@@ -10,6 +10,15 @@ import org.jetbrains.annotations.Nullable;
 public interface AuthorizationProvider {
   ExtensionPointName<AuthorizationProvider> EP_NAME = new ExtensionPointName<>("com.intellij.authorizationProvider");
 
+  /**
+   * This method is called from a background thread in order to obtain an authorization access token for the given resource URL.
+   * The access token has to be valid for at least 10 seconds. The implementation should not throw an exception, it is supposed to return a valid authentication token value or null if there was any error or
+   * there is no access token obtained for the given resource.
+   * The implementation is not expected to initiate an interactive authorization process and react on `Thread#interrupt`
+   *
+   * @param url the URL denoting the resource for which the access token is required
+   * @return a valid and not expired access token or null otherwise (the access token does not exist for the given resource, or there was an error obtaining the token)
+   */
   @Nullable
   String getAccessToken(@NotNull String url);
 }
