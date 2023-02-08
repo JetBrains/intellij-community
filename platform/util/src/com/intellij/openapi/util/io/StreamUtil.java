4f47a9965619c1fb10d1648a17831b21b218b391
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-02-08 22:16:00 +0100

    [project] replacing `StreamUtil#readBytes` usages with `InputStream#readAllBytes`
    GitOrigin-RevId: 13f2bea15c2dd3750995ea69e7591e2b08cfb03b

--- platform/util/src/com/intellij/openapi/util/io/StreamUtil.java
+++ platform/util/src/com/intellij/openapi/util/io/StreamUtil.java
@@ -1,6 +1,7 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.util.io;
 
+import com.intellij.ReviseWhenPortedToJDK;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.util.io.UnsyncByteArrayOutputStream;
 import org.jetbrains.annotations.NotNull;
@@ -37,6 +38,7 @@ public final class StreamUtil {
     return total;
   }
 
+  @ReviseWhenPortedToJDK(value = "9", description = "InputStream#readAllBytes")
   public static byte @NotNull [] readBytes(@NotNull InputStream inputStream) throws IOException {
     UnsyncByteArrayOutputStream outputStream = new UnsyncByteArrayOutputStream();
     copy(inputStream, outputStream);
