b6f50d1d6adbdd75086e9906dd41d9b5f25da72d
Ivan Semenov <ivan.semenov@jetbrains.com>
2023-02-09 17:39:27 +0100

    [collab/space] refactor to pass coroutine scope
    GitOrigin-RevId: 9ec331aa4ed2a419fc296a36d864c01e5933a279

--- platform/collaboration-tools/src/com/intellij/collaboration/ui/codereview/timeline/TimelineDiffComponentFactory.kt
+++ platform/collaboration-tools/src/com/intellij/collaboration/ui/codereview/timeline/TimelineDiffComponentFactory.kt
@@ -4,6 +4,7 @@ package com.intellij.collaboration.ui.codereview.timeline
 import com.intellij.collaboration.ui.CollaborationToolsUIUtil
 import com.intellij.collaboration.ui.codereview.comment.RoundedPanel
 import com.intellij.collaboration.ui.util.ActivatableCoroutineScopeProvider
+import com.intellij.collaboration.ui.util.bindChild
 import com.intellij.collaboration.ui.util.bindVisibility
 import com.intellij.diff.util.DiffDrawUtil
 import com.intellij.icons.AllIcons
@@ -18,6 +19,7 @@ import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.editor.impl.LineNumberConverterAdapter
 import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.vcs.changes.patch.AppliedTextPatch
 import com.intellij.openapi.vcs.changes.patch.tool.PatchChangeBuilder
 import com.intellij.ui.IdeBorderFactory
@@ -29,10 +31,13 @@ import com.intellij.util.ui.EmptyIcon
 import com.intellij.util.ui.InlineIconButton
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import net.miginfocom.layout.CC
 import net.miginfocom.layout.LC
 import net.miginfocom.swing.MigLayout
@@ -151,6 +156,51 @@ object TimelineDiffComponentFactory {
     }
   }
 
+  fun createDiffWithHeader(cs: CoroutineScope,
+                           collapseVm: CollapsibleTimelineItemViewModel,
+                           filePath: @NlsSafe String,
+                           onFileNameClick: () -> Unit,
+                           diffComponentFactory: (CoroutineScope) -> JComponent): JComponent {
+    val expandCollapseButton = InlineIconButton(EmptyIcon.ICON_16).apply {
+      cs.launch(start = CoroutineStart.UNDISPATCHED) {
+        collapseVm.collapsed.collect { collapsed ->
+          icon = if (collapsed) {
+            AllIcons.General.ExpandComponent
+          }
+          else {
+            AllIcons.General.CollapseComponent
+          }
+          hoveredIcon = if (collapsed) {
+            AllIcons.General.ExpandComponentHover
+          }
+          else {
+            AllIcons.General.CollapseComponentHover
+          }
+
+          actionListener = ActionListener {
+            collapseVm.setCollapsed(!collapsed)
+          }
+        }
+      }
+      bindVisibility(cs, collapseVm.collapsible)
+    }
+
+
+
+    return RoundedPanel(ListLayout.vertical(0), 8).apply {
+      isOpaque = false
+      add(createFileNameComponent(filePath, expandCollapseButton, onFileNameClick))
+
+      bindChild(cs, collapseVm.collapsed) { cs, collapsed ->
+        if (collapsed) return@bindChild null
+        diffComponentFactory(cs).apply {
+          border = IdeBorderFactory.createBorder(SideBorder.TOP)
+        }
+      }
+    }
+  }
+
+  @Deprecated("deprecated in favor of a reactive solution")
   fun wrapWithHeader(diffComponent: JComponent,
                      filePath: @NonNls String,
                      collapsibleState: StateFlow<Boolean>,
