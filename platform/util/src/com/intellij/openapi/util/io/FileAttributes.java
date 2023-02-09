ba4e72d45d6819d63e8928ea536a4b82b4b38e56
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-02-09 14:43:53 +0100

    Cleanup (needlessly exposed `FileAttributes` constants; obsolete annotation)
    GitOrigin-RevId: f57df0ca1ea8e91580ffe370d7659a22241151ce

--- platform/util/src/com/intellij/openapi/util/io/FileAttributes.java
+++ platform/util/src/com/intellij/openapi/util/io/FileAttributes.java
@@ -2,13 +2,10 @@
 package com.intellij.openapi.util.io;
 
 import com.intellij.openapi.util.SystemInfo;
-import org.intellij.lang.annotations.MagicConstant;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -31,18 +28,13 @@ public final class FileAttributes {
     UNKNOWN
   }
 
-  public static final byte SYM_LINK = 0b001;
-  public static final byte HIDDEN = 0b010;
-  public static final byte READ_ONLY = 0b100;
-
-  @MagicConstant(flags = {SYM_LINK, HIDDEN, READ_ONLY})
-  @Target(ElementType.TYPE_USE)
-  public @interface Flags { }
-
-  public static final FileAttributes BROKEN_SYMLINK = new FileAttributes(SYM_LINK, 0, 0);
-
+  private static final byte SYM_LINK = 0b001;
+  private static final byte HIDDEN = 0b010;
+  private static final byte READ_ONLY = 0b100;
   private static final int TYPE_SHIFT = 3;
   private static final int CASE_SENSITIVITY_SHIFT = 5;
+
+  public static final FileAttributes BROKEN_SYMLINK = new FileAttributes(SYM_LINK, 0, 0);
 
   /**
    * <p>Bits 0-2: modifiers ({@link #SYM_LINK}, {@link #HIDDEN}, {@link #READ_ONLY})</p>
@@ -50,7 +42,7 @@ public final class FileAttributes {
    * <p>Bits 5-7: {@link CaseSensitivity CaseSensitivity} (00={@link CaseSensitivity#UNKNOWN UNKNOWN},
    *   01={@link CaseSensitivity#SENSITIVE SENSITIVE}, 10={@link CaseSensitivity#INSENSITIVE INSENSITIVE})</p>
    */
-  private final @Flags byte flags;
+  private final byte flags;
 
   /**
    * In bytes, 0 for special files.<br/>
@@ -81,7 +73,7 @@ public final class FileAttributes {
     this(flags(isDirectory, isSpecial, isSymlink, isHidden, isWritable, caseSensitivity), length, lastModified);
   }
 
-  private FileAttributes(@Flags byte flags, long length, long lastModified) {
+  private FileAttributes(byte flags, long length, long lastModified) {
     if (flags != -1 && (flags & 0b10000000) != 0) {
       throw new IllegalArgumentException("Invalid flags: " + Integer.toBinaryString(flags));
     }
@@ -93,18 +85,18 @@ public final class FileAttributes {
     this.lastModified = lastModified;
   }
 
-  private static @Flags byte flags(boolean isDirectory, boolean isSpecial, boolean isSymlink, boolean isHidden, boolean isWritable, CaseSensitivity sensitivity) {
-    @Flags byte flags = 0;
+  private static byte flags(boolean isDirectory, boolean isSpecial, boolean isSymlink, boolean isHidden, boolean isWritable, CaseSensitivity sensitivity) {
+    byte flags = 0;
     if (isSymlink) flags |= SYM_LINK;
     if (isHidden) flags |= HIDDEN;
     if (!isWritable) flags |= READ_ONLY;
-    @Flags int type_flags = (isSpecial ? 0b11 : isDirectory ? 0b10 : 0b01) << TYPE_SHIFT;
+    int type_flags = (isSpecial ? 0b11 : isDirectory ? 0b10 : 0b01) << TYPE_SHIFT;
     flags |= type_flags;
     flags = packSensitivityIntoFlags(isDirectory ? sensitivity : CaseSensitivity.UNKNOWN, flags);
     return flags;
   }
 
-  private static @Flags byte packSensitivityIntoFlags(CaseSensitivity sensitivity, byte flags) {
+  private static byte packSensitivityIntoFlags(CaseSensitivity sensitivity, byte flags) {
     int sensitivity_flags = sensitivity == CaseSensitivity.UNKNOWN ? 0 : sensitivity == CaseSensitivity.SENSITIVE ? 1 : 2;
     flags |= sensitivity_flags << CASE_SENSITIVITY_SHIFT;
     return flags;
