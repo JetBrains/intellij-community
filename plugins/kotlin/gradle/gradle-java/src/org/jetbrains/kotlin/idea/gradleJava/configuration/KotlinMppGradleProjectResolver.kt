610d7108c25a6066b0542781f8957381763bb7e4
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-02-09 10:12:43 +0100

    [KTIJ-24552] Rename KotlinMPPGradleProjectResolver to KotlinMppGradleProjectResolver
    GitOrigin-RevId: a3471534bbf7a6fa4fb6c8d583c0352df24ffb12

--- plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/KotlinMppGradleProjectResolver.kt
+++ plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/KotlinMppGradleProjectResolver.kt
@@ -26,8 +26,12 @@ import org.jetbrains.plugins.gradle.service.project.ProjectResolverContext
 import java.lang.reflect.Proxy
 import java.util.*
 
+@Suppress("unused") // Can be removed once AS rebased on 23.1
+@Deprecated("Use KotlinMppGradleProjectResolver instead", replaceWith = ReplaceWith("KotlinMppGradleProjectResolver"))
+typealias KotlinMPPGradleProjectResolver = KotlinMppGradleProjectResolver
+
 @Order(ExternalSystemConstants.UNORDERED + 1)
-open class KotlinMPPGradleProjectResolver : AbstractProjectResolverExtension() {
+open class KotlinMppGradleProjectResolver : AbstractProjectResolverExtension() {
 
     interface Context {
         val mppModel: KotlinMPPGradleModel
@@ -61,7 +65,7 @@ open class KotlinMPPGradleProjectResolver : AbstractProjectResolverExtension() {
     override fun createModule(gradleModule: IdeaModule, projectDataNode: DataNode<ProjectData>): DataNode<ModuleData>? {
         val moduleDataNode = super.createModule(gradleModule, projectDataNode) ?: return null
         val model = resolverCtx.getMppModel(gradleModule) ?: return moduleDataNode
-        val context = KotlinMPPGradleProjectResolver.Context(model, resolverCtx, gradleModule, projectDataNode, moduleDataNode)
+        val context = KotlinMppGradleProjectResolver.Context(model, resolverCtx, gradleModule, projectDataNode, moduleDataNode)
         moduleDataNode.kotlinMppGradleProjectResolverContext = context
         populateMppModuleDataNode(context)
         return moduleDataNode
@@ -152,9 +156,9 @@ open class KotlinMPPGradleProjectResolver : AbstractProjectResolverExtension() {
 fun ProjectResolverContext.getMppModel(gradleModule: IdeaModule): KotlinMPPGradleModel? =
     this.getExtraProject(gradleModule, KotlinMPPGradleModel::class.java)?.let { mppModel ->
         if (mppModel is Proxy) {
-            KotlinMPPGradleProjectResolver.proxyObjectCloningCache[mppModel] as? KotlinMPPGradleModelImpl
-                ?: KotlinMPPGradleModelImpl(mppModel, KotlinMPPGradleProjectResolver.proxyObjectCloningCache).also {
-                    KotlinMPPGradleProjectResolver.proxyObjectCloningCache[mppModel] = it
+            KotlinMppGradleProjectResolver.proxyObjectCloningCache[mppModel] as? KotlinMPPGradleModelImpl
+                ?: KotlinMPPGradleModelImpl(mppModel, KotlinMppGradleProjectResolver.proxyObjectCloningCache).also {
+                    KotlinMppGradleProjectResolver.proxyObjectCloningCache[mppModel] = it
                 }
         } else mppModel
     }
