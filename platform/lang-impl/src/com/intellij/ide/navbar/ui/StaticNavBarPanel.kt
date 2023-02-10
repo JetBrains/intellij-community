1e39e29d7c6776bd608a1280a2b7e339b20a390c
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-02-10 21:22:59 +0100

    extract `StaticNavBarPanel.showPanel`, use supervisor, remove panel on cancellation of its own coroutine
    GitOrigin-RevId: 5439687ada6b60d5d88877ca54a784aee1c862ab

--- platform/lang-impl/src/com/intellij/ide/navbar/ui/StaticNavBarPanel.kt
+++ platform/lang-impl/src/com/intellij/ide/navbar/ui/StaticNavBarPanel.kt
@@ -1,36 +1,42 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.navbar.ui
 
+import com.intellij.ide.navbar.vm.NavBarVm
 import com.intellij.ide.navbar.vm.StaticNavBarVm
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.project.Project
 import com.intellij.util.ui.EDT
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 import java.awt.BorderLayout
 import javax.swing.JPanel
 
 internal class StaticNavBarPanel(
   cs: CoroutineScope,
   staticVm: StaticNavBarVm,
-  project: Project,
+  private val project: Project,
 ) : JPanel(BorderLayout()) {
 
   init {
     EDT.assertIsEdt()
     cs.launch(Dispatchers.EDT) {
       staticVm.vm.collectLatest { vm ->
-        if (vm == null) {
-          removeAll()
+        if (vm != null) {
+          showPanel(vm)
         }
-        else {
-          coroutineScope {
-            add(NewNavBarPanel(this@coroutineScope, vm, project, isFloating = false))
-          }
-        }
+      }
+    }
+  }
+
+  private suspend fun showPanel(vm: NavBarVm): Nothing {
+    supervisorScope {
+      val panel = NewNavBarPanel(this@supervisorScope, vm, project, isFloating = false)
+      add(panel)
+      try {
+        awaitCancellation()
+      }
+      finally {
+        remove(panel)
       }
     }
   }
