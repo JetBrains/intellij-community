c83c11e0f99c767eebf7ff79384126e78bbec698
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-02-10 14:27:11 +0100

    Fix mpp test after fix in kotlin repo
    The test is broken by https://jetbrains.team/p/kt/reviews/8731
    
    Now expect and actual in the same module issues a warning
    
    GitOrigin-RevId: 464491d05d6caa3386fbc88c9f27e93607745757

--- plugins/kotlin/idea/tests/testData/multiplatform/hierarcicalActualization/js/js.kt
+++ plugins/kotlin/idea/tests/testData/multiplatform/hierarcicalActualization/js/js.kt
@@ -6,5 +6,5 @@ actual class <!LINE_MARKER("descr='Has expects in jvmAndJs module'")!>ExpectInMi
 
 expect class <!NO_ACTUAL_FOR_EXPECT!>ExpectInJsWithoutActual<!>
 
-expect class <!LINE_MARKER("descr='Has actuals in js module'")!>ExpectInJsActualInJs<!>
-actual class <!LINE_MARKER("descr='Has expects in js module'")!>ExpectInJsActualInJs<!>
+expect <!EXPECT_AND_ACTUAL_IN_THE_SAME_MODULE!>class <!LINE_MARKER("descr='Has actuals in js module'")!>ExpectInJsActualInJs<!><!>
+actual <!EXPECT_AND_ACTUAL_IN_THE_SAME_MODULE!>class <!LINE_MARKER("descr='Has expects in js module'")!>ExpectInJsActualInJs<!><!>
