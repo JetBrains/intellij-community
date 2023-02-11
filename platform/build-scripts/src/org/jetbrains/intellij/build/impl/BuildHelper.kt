1ca22d510995dcbdd2747db6655f9aca4818884a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-02-11 08:17:53 +0100

    convert PythonProfessionalPluginModules to kotlin (part 2)
    GitOrigin-RevId: c1dcc7fcf3b72bd849c0aa17a5d025703c0074ba

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/BuildHelper.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/BuildHelper.kt
@@ -1,5 +1,7 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-@file:Suppress("ReplacePutWithAssignment", "ReplaceNegatedIsEmptyWithIsNotEmpty", "LiftReturnOrAssignment")
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:Suppress("ReplacePutWithAssignment", "ReplaceNegatedIsEmptyWithIsNotEmpty", "LiftReturnOrAssignment",
+               "BlockingMethodInNonBlockingContext", "RAW_RUN_BLOCKING")
+
 package org.jetbrains.intellij.build.impl
 
 import com.intellij.diagnostic.telemetry.useWithScope
