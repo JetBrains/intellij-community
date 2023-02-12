f0be15b1323d1c8f4bec7fe0d542916511ff72e4
Bas Leijdekkers <basleijdekkers@gmail.com>
2023-02-12 16:18:22 +0100

    Java: simplify and improve test
    GitOrigin-RevId: ec2f70969e3d91a5dc2d0f9ee5bb89dd4d39b48d

--- java/java-tests/testSrc/com/intellij/codeStyle/CodeStyleManagerTest.java
+++ java/java-tests/testSrc/com/intellij/codeStyle/CodeStyleManagerTest.java
@@ -1,86 +1,73 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeStyle;
 
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiExpression;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.JavaCodeStyleManager;
+import com.intellij.psi.codeStyle.SuggestedNameInfo;
 import com.intellij.psi.codeStyle.VariableKind;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.PsiUtilCore;
-import com.intellij.testFramework.assertions.Assertions;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 import org.intellij.lang.annotations.Language;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Collection;
+import org.junit.Assert;
 
 public class CodeStyleManagerTest extends LightJavaCodeInsightFixtureTestCase {
 
   public void testSuggestSemanticNameEnumConstantVariable() {
-    testSuggestedFirstName("""
-                             public class Test {
-                                 public static void main(String[] args) {
-                                     check("Some", (Month.J<caret>ANUARY), 17);
-                                 }
-                                 
-                                 private enum Month {
-                                    JANUARY
-                                 }
-                             }
-                             """, "january", VariableKind.LOCAL_VARIABLE);
+    @Language("JAVA") String source = """
+      public class Test {
+          public static void main(String[] args) {
+              check("Some", (Month.J<caret>ANUARY), 17);
+          }
+          
+          private enum Month {
+             JANUARY
+          }
+      }
+      """;
+    checkSuggestedNames(source, VariableKind.LOCAL_VARIABLE, "january", "month");
+    checkSuggestedNames(source, VariableKind.PARAMETER, "month", "january");
   }
 
   public void testSuggestSemanticNameStringConstant() {
-    testSuggestedFirstName("""
-                             public class Test {
-                                 public static void main(String[] args) {
-                                     check("Som<caret>e", (Month.JANUARY), 17);
-                                 }
-                                 
-                                 private enum Month {
-                                    JANUARY
-                                 }
-                             }
-                             """, "Some", VariableKind.LOCAL_VARIABLE);
+    checkSuggestedNames("""
+                          public class Test {
+                              public static void main(String[] args) {
+                                  check("Som<caret>e", (Month.JANUARY), 17);
+                              }
+                              
+                              private enum Month {
+                                 JANUARY
+                              }
+                          }
+                          """, VariableKind.LOCAL_VARIABLE, "some", "s");
   }
 
   public void testSuggestSemanticNameVariableNameParameter() {
-    testSuggestedFirstName("""
-                             public class Test {
-                                 public static void main(String[] args) {
-                                     Month month2 = Month.JANUARY;
-                                     check("Some", m<caret>onth2, 17);
-                                 }
-                                 
-                                 private enum Month {
-                                    JANUARY
-                                 }
-                             }
-                             """, "month2", VariableKind.PARAMETER);
+    checkSuggestedNames("""
+                          public class Test {
+                              public static void main(String[] args) {
+                                  Month month2 = Month.JANUARY;
+                                  check("Some", m<caret>onth2, 17);
+                              }
+                              
+                              private enum Month {
+                                 JANUARY
+                              }
+                          }
+                          """, VariableKind.PARAMETER, "month2", "month");
   }
 
-  public void testSuggestSemanticNameEnumConstantParameter() {
-    testSuggestedFirstName("""
-                             public class Test {
-                                 public static void main(String[] args) {
-                                     check("Some",(Month.J<caret>ANUARY), 17);
-                                 }
-                                 
-                                 private enum Month {
-                                    JANUARY
-                                 }
-                             }
-                             """, "Month", VariableKind.PARAMETER);
-  }
-
-  private void testSuggestedFirstName(@NotNull @Language("JAVA") String text, @NotNull String expected, @NotNull VariableKind parameter) {
-    PsiFile file = myFixture.configureByText("Test.java", text);
-    int offset = myFixture.getCaretOffset();
-    PsiElement element = PsiUtilCore.getElementAtOffset(file, offset);
+  private void checkSuggestedNames(@NotNull @Language("JAVA") String code, @NotNull VariableKind kind, String @NotNull ... expected) {
+    PsiFile file = myFixture.configureByText("Test.java", code);
+    PsiElement element = PsiUtilCore.getElementAtOffset(file, myFixture.getCaretOffset());
     PsiExpression expression = PsiTreeUtil.getParentOfType(element, PsiExpression.class, false);
-    final JavaCodeStyleManager javaCodeStyleManager = JavaCodeStyleManager.getInstance(getProject());
-    Collection<String> names = javaCodeStyleManager.suggestSemanticNames(expression, parameter);
-    Assertions.assertThat(names.iterator().next()).isEqualTo(expected);
+    SuggestedNameInfo nameInfo = JavaCodeStyleManager.getInstance(getProject())
+      .suggestVariableName(kind, null, expression, expression.getType());
+    Assert.assertArrayEquals('"' + StringUtil.join(nameInfo.names, "\", \"") + '"', expected, nameInfo.names);
   }
 }
