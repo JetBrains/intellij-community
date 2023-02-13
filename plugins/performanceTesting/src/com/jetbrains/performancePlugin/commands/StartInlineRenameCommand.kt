538cca0029aef2d06754c79562bcc968bad8f3a6
roman.ivanitskii <roman.ivanitskii@jetbrains.com>
2023-02-14 00:04:47 +0200

    [performance-tests] disable automatic dialog for rename in startInlineRename command
    GitOrigin-RevId: d7edba8b2cccbd19d356916c8a9e9b7e05224fa3

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/StartInlineRenameCommand.kt
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/StartInlineRenameCommand.kt
@@ -9,6 +9,7 @@ import com.intellij.openapi.ui.playback.commands.AbstractCommand
 import com.intellij.openapi.wm.IdeFocusManager
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.refactoring.InplaceRefactoringContinuation
+import com.intellij.refactoring.RefactoringSettings
 import com.intellij.refactoring.actions.RenameElementAction
 import com.intellij.refactoring.rename.Renamer
 import com.intellij.refactoring.rename.RenamerFactory
@@ -26,6 +27,8 @@ class StartInlineRenameCommand(text: String, line: Int) : AbstractCommand(text, 
   }
 
   override fun _execute(context: PlaybackContext): Promise<Any?> {
+    //disable popup
+    RefactoringSettings.getInstance().RENAME_SHOW_AUTOMATIC_RENAMING_DIALOG = false
     val actionCallback = ActionCallbackProfilerStopper()
     val project = context.project
     ApplicationManager.getApplication().invokeAndWait(Context.current().wrap(Runnable {
