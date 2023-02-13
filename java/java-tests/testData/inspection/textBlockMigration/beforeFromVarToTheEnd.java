50c04d921fe2fe509aa7b7b3df1f7b85d11fe703
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2023-02-13 18:39:13 +0400

    Revert "[java-inspections] TextBlockMigration: Suggest converting part of a string concatenation to the text block"
    This reverts commit 8cbb8bcd7b0492e5657cfe69613cb27ff6e49414.
    
    GitOrigin-RevId: 661bf51388bfc0c07b7f01dd182c76a82f70ae9b

--- java/java-tests/testData/inspection/textBlockMigration/beforeFromVarToTheEnd.java
+++
@@ -1,9 +0,0 @@
-// "Replace with text block" "true-preview"
-class Main {
-  String foo(String s) {
-    return s + s "(QWERTYUIOP<caret>\n" +
-           "ASDFGHJLKZ\n" +
-           "ZVNHGLMLSK\n" +
-           ")";
-  }
-}
--- java/java-tests/testData/inspection/textBlockMigration/beforeFromVarToVarWithComment.java
+++
@@ -1,9 +0,0 @@
-// "Replace with text block" "INFORMATION-preview"
-class Main {
-  String foo(String s) {
-    return s + "(QWERTYUIOP<caret>\n" +
-           "ASDFGHJLKZ\n" /*blah blah blah*/ +
-           "ZVNHGLMLSK\n" +
-           ")" + s + s;
-  }
-}
