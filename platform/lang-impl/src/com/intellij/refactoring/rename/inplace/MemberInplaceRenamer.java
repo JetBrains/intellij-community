a8d297e83e9b2f28700aaa99eadfdf8f7f4e1459
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-02-14 16:03:22 +0100

    IDEA-305674 replace `allowSlowOperations` with `ACTION_PERFORM` section in `MemberInplaceRenamer.performRefactoringRename`
    GitOrigin-RevId: 5a3ff84ea57612facc61ad6a2a4c412b3aa6b468

--- platform/lang-impl/src/com/intellij/refactoring/rename/inplace/MemberInplaceRenamer.java
+++ platform/lang-impl/src/com/intellij/refactoring/rename/inplace/MemberInplaceRenamer.java
@@ -224,20 +224,23 @@ public class MemberInplaceRenamer extends VariableInplaceRenamer {
             return;
           }
 
-          Runnable performRunnable = () -> SlowOperations.allowSlowOperations(() -> {
-            if (DumbService.isDumb(myProject)) {
-              DumbService.getInstance(myProject).showDumbModeNotification(RefactoringBundle.message("refactoring.not.available.indexing"));
-              return;
-            }
+          Runnable performRunnable = () -> {
+            try (var ignored = SlowOperations.startSection(SlowOperations.ACTION_PERFORM)) {
+              if (DumbService.isDumb(myProject)) {
+                DumbService.getInstance(myProject)
+                  .showDumbModeNotification(RefactoringBundle.message("refactoring.not.available.indexing"));
+                return;
+              }
 
-            final String commandName = RefactoringBundle.message("renaming.0.1.to.2",
-                                                                 UsageViewUtil.getType(variable),
-                                                                 DescriptiveNameUtil.getDescriptiveName(variable), newName);
-            CommandProcessor.getInstance().executeCommand(myProject, () -> {
-              performRenameInner(substituted, newName);
-              PsiDocumentManager.getInstance(myProject).commitAllDocuments();
-            }, commandName, null);
-          });
+              final String commandName = RefactoringBundle.message("renaming.0.1.to.2",
+                                                                   UsageViewUtil.getType(variable),
+                                                                   DescriptiveNameUtil.getDescriptiveName(variable), newName);
+              CommandProcessor.getInstance().executeCommand(myProject, () -> {
+                performRenameInner(substituted, newName);
+                PsiDocumentManager.getInstance(myProject).commitAllDocuments();
+              }, commandName, null);
+            }
+          };
 
           if (ApplicationManager.getApplication().isUnitTestMode()) {
             performRunnable.run();
