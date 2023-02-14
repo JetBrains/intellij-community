f9bd15b59cce006c3ad8fcd4338ad203ed2540d9
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2023-02-14 18:06:16 +0100

    [kt, tests] Fix kmmProjectWithSdk
    - Rename it to better reflect the structure of the projects
    - fix incorrect source folder placement due to which sources
      were not actually highlighted
    - fix syntactical bug in sources
    
    (cherry picked from commit 7da61d334a56649d7ec9a0fd6e3bec20916c0750)
    
    GitOrigin-RevId: 5f2361296e96e2126d359561bcd354176ac2d6ea

--- plugins/kotlin/idea/tests/testData/gradle/newMppTests/tier1/androidAppConsumesKmmLibBinary/androidApp/src/main/kotlin/android/android_main_usages.kt
+++ plugins/kotlin/idea/tests/testData/gradle/newMppTests/tier1/androidAppConsumesKmmLibBinary/androidApp/src/main/kotlin/android/android_main_usages.kt
@@ -1,8 +1,11 @@
 package android
 
+import test.*
+
 fun use(c: CommonMainExpect) {
     // Refinement on libs work + checking that jvmMain and commonMain symbols are visible
     consumeJvmMainExpect(produceCommonMainExpect())
+
     consumeCommonMainExpect(produceJvmMainExpect())
 
     // Refinement on expects works
