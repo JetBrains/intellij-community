5c3e5ac67df4f346a9ed094f52282e8bb79db145
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2023-02-14 17:25:33 +0100

    IJ-CR-102807 IDEA-309683 IDEA-313253 Rewrite LibrarySynchronizationQueue to coroutines
    Reduce blocking operations such as synchronized, runReadAction etc.
    
    GitOrigin-RevId: bbee68fc3f929319ef82df433a0a5ec6136a7766

--- java/idea-ui/src/com/intellij/jarRepository/LibrarySynchronizationQueue.kt
+++ java/idea-ui/src/com/intellij/jarRepository/LibrarySynchronizationQueue.kt
@@ -2,73 +2,105 @@
 package com.intellij.jarRepository
 
 import com.intellij.jarRepository.RepositoryLibrarySynchronizer.removeDuplicatedUrlsFromRepositoryLibraries
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.impl.libraries.LibraryEx
 import com.intellij.openapi.roots.impl.libraries.LibraryTableImplUtil
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableSharedFlow
 import org.jetbrains.idea.maven.utils.library.RepositoryLibraryProperties
 import org.jetbrains.idea.maven.utils.library.RepositoryUtils
+import kotlin.coroutines.coroutineContext
 
 @Service(Service.Level.PROJECT)
 internal class LibrarySynchronizationQueue(private val project: Project, private val scope: CoroutineScope) {
   companion object {
     @JvmStatic
     fun getInstance(project: Project): LibrarySynchronizationQueue = project.service()
+    private val log = logger<LibrarySynchronizationQueue>()
   }
 
-  private val queue = LinkedHashSet<LibraryEx>()
+  private val synchronizationRequests = MutableSharedFlow<Request>()
+  private val toSynchronize = mutableSetOf<LibraryEx>()
+
+  init {
+    scope.launch(Dispatchers.IO) {
+      synchronizationRequests.collect {
+        try {
+          when (it) {
+            is Request.QueueSynchronization -> {
+              toSynchronize.add(it.library)
+            }
+            is Request.RevokeSynchronization -> {
+              toSynchronize.remove(it.library)
+            }
+            is Request.Flush -> {
+              toSynchronize.addAll(it.newLibraries)
+              synchronizeLibraries(toSynchronize)
+              toSynchronize.clear()
+            }
+          }
+        }
+        catch (e: CancellationException) {
+          throw e
+        }
+        catch (e: Exception) {
+          // continue collecting on exceptions
+          log.warn(e)
+        }
+      }
+    }
+  }
 
   fun requestAllLibrariesSynchronization() {
     scope.launch(Dispatchers.IO) {
       removeDuplicatedUrlsFromRepositoryLibraries(project)
       val libraries = RepositoryLibrarySynchronizer.collectLibrariesToSync(project)
-      coroutineScope {
-        for (library in libraries) {
-          requestSynchronization(library as LibraryEx)
-        }
-      }
-      flush()
+      synchronizationRequests.emit(Request.Flush(libraries.map { it as LibraryEx }))
     }
   }
 
   fun requestSynchronization(library: LibraryEx) {
-    scope.launch {
-      if (library.needToReload()) {
-        synchronized(queue) {
-          queue.add(library)
-        }
+    scope.launch(Dispatchers.IO) {
+      if (readAction { library.needToReload() }) {
+        synchronizationRequests.emit(Request.QueueSynchronization(library))
       }
     }
   }
 
   fun revokeSynchronization(library: LibraryEx) {
-    synchronized(queue) {
-      queue.remove(library)
+    scope.launch(Dispatchers.IO) {
+      synchronizationRequests.emit(Request.RevokeSynchronization(library))
     }
   }
 
   fun flush() {
     scope.launch(Dispatchers.IO) {
-      while (isActive) {
-        if (project.isDisposed) break
+      synchronizationRequests.emit(Request.Flush(emptyList()))
+    }
+  }
 
-        val library = nextLibrary()
-        if (library == null) break
+  private suspend fun synchronizeLibraries(libraries: Collection<LibraryEx>) {
+    for (library in libraries) {
+      if (!coroutineContext.isActive) {
+        return
+      }
+      val shouldReload = readAction { library.needToReload() }
 
-        if (library.needToReload()) {
-          RepositoryUtils.reloadDependencies(project, library)
-        }
+      if (shouldReload) {
+        RepositoryUtils.reloadDependencies(project, library)
       }
     }
   }
 
-  private fun nextLibrary(): LibraryEx? = synchronized(queue) {
-    val library = queue.firstOrNull() ?: return null
-    queue.remove(library)
-    return library
+  private sealed interface Request {
+    class QueueSynchronization(val library: LibraryEx) : Request
+    class RevokeSynchronization(val library: LibraryEx) : Request
+    class Flush(val newLibraries: Collection<LibraryEx>) : Request
   }
 }
 
