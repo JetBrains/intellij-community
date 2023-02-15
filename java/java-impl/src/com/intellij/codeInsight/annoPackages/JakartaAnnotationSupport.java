2c3bbc97cc79777392d649aeb84385a78d840cb6
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-02-15 11:51:25 +0100

    [java-analysis] Support Jakarta nullability annotations out of the box
    #fixes IDEA-311538
    
    GitOrigin-RevId: 3c8ec1c188baa39eff257d7e270b1bb3a16b5e15

---
+++ java/java-impl/src/com/intellij/codeInsight/annoPackages/JakartaAnnotationSupport.java
@@ -0,0 +1,21 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.codeInsight.annoPackages;
+
+import com.intellij.codeInsight.Nullability;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
+
+// package: jakarta.annotation:jakarta.annotation-api
+class JakartaAnnotationSupport implements AnnotationPackageSupport {
+  @NotNull
+  @Override
+  public List<String> getNullabilityAnnotations(@NotNull Nullability nullability) {
+    return switch (nullability) {
+      case NOT_NULL -> List.of("jakarta.annotation.Nonnull");
+      case NULLABLE -> List.of("jakarta.annotation.Nullable");
+      default -> Collections.emptyList();
+    };
+  }
+}
