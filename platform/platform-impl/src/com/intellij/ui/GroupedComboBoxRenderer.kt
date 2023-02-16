1d26bb6234e2d3b96edc6fa724615798069a7748
Louis Vignier <louis.vignier@jetbrains.com>
2023-02-16 12:03:40 +0100

    [ui] Use disabled colors in GroupedComboBoxRenderer when the combo is disabled
    #IDEA-313428 Fixed
    
    GitOrigin-RevId: 1155951e553ff6b7e76e23c0aea40e9422f67d79

--- platform/platform-impl/src/com/intellij/ui/GroupedComboBoxRenderer.kt
+++ platform/platform-impl/src/com/intellij/ui/GroupedComboBoxRenderer.kt
@@ -75,8 +75,13 @@ abstract class GroupedComboBoxRenderer<T>(val combo: ComboBox<T>) : GroupedEleme
     }
   }
 
-  override fun getBackground(): Color = UIUtil.getListBackground(false, false)
-  override fun getForeground(): Color = UIUtil.getListForeground(false, false)
+  // TODO: remove when old UI is not supported
+  @Suppress("UNNECESSARY_SAFE_CALL")
+  private val enabled: Boolean
+    get() = combo?.isEnabled == true
+
+  override fun getBackground(): Color = if (enabled) UIUtil.getListBackground(false, false) else UIUtil.getComboBoxDisabledBackground()
+  override fun getForeground(): Color = if (enabled) UIUtil.getListForeground(false, false) else UIUtil.getComboBoxDisabledForeground()
   override fun getSelectionBackground(): Color = UIUtil.getListSelectionBackground(true)
   override fun getSelectionForeground(): Color = UIUtil.getListSelectionForeground(true)
 
