d4e605b27587b19b1a336caffd457023fe1e1c01
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2023-02-16 12:00:52 +0100

    [kt, tests] Migrate a couple of tests to new tests infra
    - KTIJ22345SyntheticJavaProperties
    - PrepareKotlinIdeaImport-tests
    - ImportKotlinAndroidSourceSetInfo
    - ImportKotlinAndroidSourceSetInfoModelBuilderTest to new infra
    
    (cherry picked from commit 8eba1846c6a657c6967e1ae7a50215783d2854ca)
    
    GitOrigin-RevId: d11f2c3cdcf0f41cdf20f9dd96bfd65f53db3403

---
+++ plugins/kotlin/idea/tests/testData/gradle/newMppTests/regress/ktij22345SyntheticJavaProperties/src/commonMain/kotlin/common.kt
@@ -0,0 +1,8 @@
+//region Test configuration
+// - hidden: line markers
+//endregion
+class KotlinFile1(userPath: String): MyFile1(userPath)
+class KotlinFile2(userPath: String): MyFile2(userPath)
+
+expect open class MyFile1(userPath: String)
+expect open class MyFile2(userPath: String)
