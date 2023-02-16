7a204e75520f2a7bb6841e89c754db8239dd8131
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2023-02-16 15:04:42 +0100

    IDEA-287171 Implement a switch to new UI
    - Meet the new UI toolwindow
    
    (cherry picked from commit 47764c92cebb2d8a1a5b08a850d0bf0956881e68)
    (cherry picked from commit 379625c90c17b9594f4721ad74ceb1f96a713690)
    
    IJ-CR-103971
    
    GitOrigin-RevId: 1dad203603ff68c3b3b4c3338fcab02928e52969

--- platform/platform-impl/src/com/intellij/ide/ui/experimental/meetNewUi/MeetNewUiToolWindow.kt
+++ platform/platform-impl/src/com/intellij/ide/ui/experimental/meetNewUi/MeetNewUiToolWindow.kt
@@ -6,6 +6,8 @@ import com.intellij.ide.actions.QuickChangeLookAndFeel
 import com.intellij.ide.ui.LafManager
 import com.intellij.ide.ui.LafManager.LafReference
 import com.intellij.ide.ui.LafManagerListener
+import com.intellij.ide.ui.UISettings
+import com.intellij.ide.ui.UISettingsListener
 import com.intellij.ide.ui.experimental.ExperimentalUiCollector
 import com.intellij.ide.ui.laf.darcula.ui.DarculaButtonUI.DEFAULT_STYLE_KEY
 import com.intellij.openapi.actionSystem.DataProvider
@@ -25,6 +27,7 @@ import com.intellij.ui.dsl.gridLayout.JBVerticalGaps
 import com.intellij.util.IconUtil
 import com.intellij.util.ui.JBFont
 import com.intellij.util.ui.JBUI
+import org.jetbrains.annotations.Nls
 import javax.swing.Icon
 import javax.swing.JLabel
 
@@ -37,9 +40,13 @@ internal class MeetNewUiToolWindow(private val project: Project, private val too
     private val LIGHT = IconLoader.getIcon("expui/meetNewUi/light.svg", MeetNewUiToolWindow::class.java.classLoader)
     private val DARK = IconLoader.getIcon("expui/meetNewUi/dark.svg", MeetNewUiToolWindow::class.java.classLoader)
     private val SYSTEM = IconLoader.getIcon("expui/meetNewUi/system.svg", MeetNewUiToolWindow::class.java.classLoader)
+    private val CLEAN = IconLoader.getIcon("expui/meetNewUi/clean.svg", MeetNewUiToolWindow::class.java.classLoader)
+    private val COMPACT = IconLoader.getIcon("expui/meetNewUi/compact.svg", MeetNewUiToolWindow::class.java.classLoader)
   }
 
   private val themeButtons = mutableMapOf<LafReference?, MeetNewUiButton>()
+  private lateinit var cleanDensity: Density
+  private lateinit var compactDensity: Density
 
   private val panel = panel {
     customizeSpacingConfiguration(EmptySpacingConfiguration()) {
@@ -68,18 +75,17 @@ internal class MeetNewUiToolWindow(private val project: Project, private val too
           }
           theme(IdeBundle.message("meetnewui.toolwindow.system"), SYSTEM, null)
 
+          cell() // Deny right component to shrink
+
           // todo add other themes?
         }.customize(JBVerticalGaps(bottom = 20))
         row {
           label(IdeBundle.message("meetnewui.toolwindow.density"))
         }.customize(JBVerticalGaps(bottom = 8))
         row {
-          button(IdeBundle.message("meetnewui.toolwindow.clean")) {
-            // todo
-          }.customize(JBGaps(right = 8))
-          button(IdeBundle.message("meetnewui.toolwindow.compact")) {
-            // todo
-          }
+          cleanDensity = density(CLEAN, IdeBundle.message("meetnewui.toolwindow.clean"), JBGaps(right = 8), false)
+
+          compactDensity = density(COMPACT, IdeBundle.message("meetnewui.toolwindow.compact"), Gaps.EMPTY, true)
 
           cell() // Deny right component to shrink
         }.customize(JBVerticalGaps(bottom = 20))
@@ -114,10 +120,13 @@ internal class MeetNewUiToolWindow(private val project: Project, private val too
   init {
     setContent(panel)
     updateThemeSelection()
+    updateDensitySelection()
 
-    ApplicationManager.getApplication().messageBus
+    val connection = ApplicationManager.getApplication().messageBus
       .connect(toolWindow.disposable)
-      .subscribe(LafManagerListener.TOPIC, LafManagerListener { lookAndFeelChanged() })
+
+    connection.subscribe(LafManagerListener.TOPIC, LafManagerListener { updateThemeSelection() })
+    connection.subscribe(UISettingsListener.TOPIC, UISettingsListener { updateDensitySelection() })
   }
 
   /**
@@ -140,11 +149,13 @@ internal class MeetNewUiToolWindow(private val project: Project, private val too
     val lafManager = LafManager.getInstance()
     if (lafReference == null) {
       lafManager.autodetect = true
+      ExperimentalUiCollector.logMeetNewUiTheme("System")
     }
     else {
       if (lafManager.autodetect) {
         lafManager.autodetect = false
       }
+      ExperimentalUiCollector.logMeetNewUiTheme(lafReference.toString())
       QuickChangeLookAndFeel.switchLafAndUpdateUI(lafManager, lafManager.findLaf(lafReference), true)
     }
   }
@@ -152,7 +163,9 @@ internal class MeetNewUiToolWindow(private val project: Project, private val too
   private fun updateThemeSelection() {
     val lafManager = LafManager.getInstance()
     for ((lafReference, button) in themeButtons) {
-      button.selected = if (lafManager.autodetect) lafReference == null else lafManager.lookAndFeelReference == lafReference
+      val selected = if (lafManager.autodetect) lafReference == null else lafManager.lookAndFeelReference == lafReference
+      button.selected = selected
+      button.font = if (selected) JBFont.regular().asBold() else JBFont.regular()
     }
   }
 
@@ -161,7 +174,61 @@ internal class MeetNewUiToolWindow(private val project: Project, private val too
     return lafManager.lafComboBoxModel.items.find { it.toString() == name }
   }
 
-  private fun lookAndFeelChanged() {
-    updateThemeSelection()
+  private fun Row.density(icon: Icon, @Nls name: String, gaps: Gaps, compactMode: Boolean): Density {
+    val button = MeetNewUiButton(icon = icon).apply {
+      putClientProperty(DslComponentProperty.VISUAL_PADDINGS, Gaps.EMPTY)
+      selectionArc = JBUI.scale(8)
+      addClickListener {
+        setDensity(compactMode)
+      }
+    }
+
+    lateinit var label: JLabel
+    this.panel {
+      row {
+        cell(button)
+          .customize(JBGaps(bottom = 8))
+      }
+
+      row {
+        label = label(name).component
+      }
+    }.customize(gaps)
+
+    return Density(button, label)
+  }
+
+  private fun updateDensitySelection() {
+    val compactMode = UISettings.getInstance().compactMode
+    cleanDensity.setSelection(!compactMode)
+    compactDensity.setSelection(compactMode)
+  }
+
+  private fun setDensity(compactMode: Boolean) {
+    val uiSettings = UISettings.getInstance()
+
+    if (uiSettings.compactMode != compactMode) {
+      val densityAction = if (compactMode) ExperimentalUiCollector.MeetNewUiAction.DENSITY_COMPACT else ExperimentalUiCollector.MeetNewUiAction.DENSITY_CLEAN
+      ExperimentalUiCollector.logMeetNewUiAction(densityAction)
+
+      uiSettings.compactMode = compactMode
+      LafManager.getInstance().applyDensity()
+    }
+  }
+}
+
+private data class Density(val button: MeetNewUiButton, val label: JLabel) {
+
+  fun setSelection(selected: Boolean) {
+    button.selected = selected
+
+    if (selected) {
+      label.font = JBFont.medium().asBold()
+      label.foreground = JBUI.CurrentTheme.Label.foreground()
+    }
+    else {
+      label.font = JBFont.medium()
+      label.foreground = JBUI.CurrentTheme.ContextHelp.FOREGROUND
+    }
   }
 }
