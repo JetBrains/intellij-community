0ae1aa82a2186e1d8f36a07c3969a1a1fc89583a
Aleksey Pivovarov <AMPivovarov@gmail.com>
2023-02-17 18:47:43 +0100

    RDCT-98 ui: always fireUISettingsChanged on LaF changes
    Do not skip event if a theme sets an explicit editor color scheme.
    
    (cherry picked from commit 249934f9fda977535c5a4444d76555d79d1a94da)
    IJ-CR-103223
    
    GitOrigin-RevId: 9b8198b440f4bc095ea3a554e4aae3a56355cdbc

--- platform/platform-impl/src/com/intellij/ide/ui/laf/LafManagerImpl.kt
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/LafManagerImpl.kt
@@ -536,9 +536,15 @@ class LafManagerImpl : LafManager(), PersistentStateComponent<Element>, Disposab
     if (!isFirstSetup && installEditorScheme) {
       if (processChangeSynchronously) {
         updateEditorSchemeIfNecessary(oldLaf, true)
+        shadowInstance.fireUISettingsChanged()
+        ActionToolbarImpl.updateAllToolbarsImmediately()
       }
       else {
-        ApplicationManager.getApplication().invokeLater { updateEditorSchemeIfNecessary(oldLaf, false) }
+        ApplicationManager.getApplication().invokeLater {
+          updateEditorSchemeIfNecessary(oldLaf, false)
+          shadowInstance.fireUISettingsChanged()
+          ActionToolbarImpl.updateAllToolbarsImmediately()
+        }
       }
     }
     isFirstSetup = false
@@ -748,8 +754,6 @@ class LafManagerImpl : LafManager(), PersistentStateComponent<Element>, Disposab
         (editorColorManager as EditorColorsManagerImpl).setGlobalScheme(scheme, processChangeSynchronously)
       }
     }
-    shadowInstance.fireUISettingsChanged()
-    ActionToolbarImpl.updateAllToolbarsImmediately()
   }
 
   /**
