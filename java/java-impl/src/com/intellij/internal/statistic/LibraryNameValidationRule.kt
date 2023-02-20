de3d4ef76af240003e8c3899629302ba584dc940
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-02-20 15:32:51 +0100

    [statistics] Fix validation of library usages
    (cherry picked from commit 10ff526a54d75a9e0039ffb08eed2c7b87233923)
    
    IJ-CR-103248
    
    GitOrigin-RevId: d8ad2337c7540172632224ceb1a53bfedc83f754

--- java/java-impl/src/com/intellij/internal/statistic/LibraryNameValidationRule.kt
+++ java/java-impl/src/com/intellij/internal/statistic/LibraryNameValidationRule.kt
@@ -4,6 +4,8 @@ package com.intellij.internal.statistic
 import com.intellij.internal.statistic.eventLog.validator.ValidationResultType
 import com.intellij.internal.statistic.eventLog.validator.rules.EventContext
 import com.intellij.internal.statistic.eventLog.validator.rules.impl.CustomValidationRule
+import com.intellij.internal.statistic.libraryJar.LibraryJarStatisticsService
+import com.intellij.internal.statistic.libraryUsage.LibraryUsageDescriptors
 
 /**
  * All valid allowed library names are bundled with IDE.
@@ -11,9 +13,12 @@ import com.intellij.internal.statistic.eventLog.validator.rules.impl.CustomValid
  * See 'library-jar-statistics.xml' and 'library-usage-statistics.xml' files.
  */
 internal class LibraryNameValidationRule : CustomValidationRule() {
+  private val allowedNames: Set<String> = (LibraryUsageDescriptors.libraryNames +
+                                           LibraryJarStatisticsService.getInstance().libraryNames).toHashSet()
+
   override fun getRuleId(): String = "used_library_name"
 
   override fun doValidate(data: String, context: EventContext): ValidationResultType {
-    return acceptWhenReportedByJetBrainsPlugin(context)
+    return if (allowedNames.contains(data)) ValidationResultType.ACCEPTED else ValidationResultType.REJECTED
   }
 }
