71ad5513482c57211aa8bcdb7d00a408a3b1e47b
Pavel Gromov <pavel.gromov@jetbrains.com>
2023-02-20 16:22:17 +0100

    [collab/github] Cleanup: use method from collab
    (cherry picked from commit da78537a4e27386c36c8ff592b55e1a55273ce66)
    
    IJ-CR-103999
    
    GitOrigin-RevId: 3e457d583f4b74623b2d4bf811e0436685123679

--- plugins/github/src/org/jetbrains/plugins/github/ui/cloneDialog/GHRepositoryListCellRenderer.kt
+++ plugins/github/src/org/jetbrains/plugins/github/ui/cloneDialog/GHRepositoryListCellRenderer.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.ui.cloneDialog
 
+import com.intellij.collaboration.ui.util.getName
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.ui.CellRendererPanel
 import com.intellij.ui.ColoredListCellRenderer
@@ -11,7 +12,6 @@ import com.intellij.util.ui.UIUtil
 import org.jetbrains.annotations.Nls
 import org.jetbrains.plugins.github.authentication.accounts.GithubAccount
 import org.jetbrains.plugins.github.authentication.accounts.isGHAccount
-import org.jetbrains.plugins.github.ui.util.getName
 import java.awt.BorderLayout
 import java.awt.Component
 import javax.swing.Action
