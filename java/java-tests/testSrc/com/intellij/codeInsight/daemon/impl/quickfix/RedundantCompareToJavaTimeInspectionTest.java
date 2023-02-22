37489ae66cc1703b32374c482ce6b27122f9d1cd
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-02-22 10:03:44 +0100

    [java-inspections] move RedundantCompareToJavaTimeInspectionTest
    GitOrigin-RevId: 99872d2cd72367bfa1244dc2446affb5771c19ec

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/impl/quickfix/RedundantCompareToJavaTimeInspectionTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/impl/quickfix/RedundantCompareToJavaTimeInspectionTest.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.java.codeInsight.daemon.quickFix;
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.codeInsight.daemon.impl.quickfix;
 
 import com.intellij.JavaTestUtil;
 import com.intellij.codeInspection.LocalInspectionTool;
