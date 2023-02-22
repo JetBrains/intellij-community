e7c1b2d7d606da7946eda46d58de9f53a552ffcd
Aleksei.Cherepanov <aleksei.cherepanov@jetbrains.com>
2023-02-22 16:39:42 +0100

    Fix flaky GradleRelativeConfigCalculatingTest
    The main idea of this test is to create two identical Gradle projects (even the root folder should be the same), but existing in different neighboring folders. This parent folder should not be part of the project, so we will have identical projects from the root, but they will have different absolute paths. And relativiser must write the same config.dat so that we can re-use it using the portable caches.
    
    GitOrigin-RevId: 5f97c481c3521466b5c94943ddc77d3d35994465

--- plugins/gradle/java/testSources/compiler/GradleRelativeConfigCalculatingTest.kt
+++ plugins/gradle/java/testSources/compiler/GradleRelativeConfigCalculatingTest.kt
@@ -5,11 +5,10 @@ import com.intellij.openapi.application.writeAction
 import com.intellij.testFramework.useProjectAsync
 import com.intellij.testFramework.utils.vfs.createFile
 import com.intellij.testFramework.withProjectAsync
-import com.intellij.util.io.readBytes
+import com.intellij.util.io.readText
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import java.nio.file.Path
 
 
 class GradleRelativeConfigCalculatingTest : GradleRelativeConfigCalculatingTestCase() {
@@ -17,48 +16,42 @@ class GradleRelativeConfigCalculatingTest : GradleRelativeConfigCalculatingTestC
   @Test
   fun testGradleRelativeConfigEquality() {
     runBlocking {
-      val projectInfo1 = projectInfo("project1") {
-        withSettingsFile { setProjectName("project1") }
+      val projectInfo1 = projectInfo("project1/project") {
+        withSettingsFile { setProjectName("project") }
         withBuildFile { withJavaPlugin() }
       }
-      val projectInfo2 = projectInfo("project2") {
-        withSettingsFile { setProjectName("project2") }
+      val projectInfo2 = projectInfo("project2/project") {
+        withSettingsFile { setProjectName("project") }
         withBuildFile { withJavaPlugin() }
       }
       initProject(projectInfo1)
       initProject(projectInfo2)
 
       writeAction {
-        testRoot.createFile("project1/src/main/resources/dir/file-main.properties")
-        testRoot.createFile("project1/src/test/resources/dir/file-test.properties")
-        testRoot.createFile("project2/src/main/resources/dir/file-main.properties")
-        testRoot.createFile("project2/src/test/resources/dir/file-test.properties")
+        testRoot.createFile("project1/project/src/main/resources/dir/file-main.properties")
+        testRoot.createFile("project1/project/src/test/resources/dir/file-test.properties")
+        testRoot.createFile("project2/project/src/main/resources/dir/file-main.properties")
+        testRoot.createFile("project2/project/src/test/resources/dir/file-test.properties")
       }
 
-      val configFiles1 = openProject("project1")
+      val configFiles1 = openProject("project1/project")
         .withProjectAsync { project ->
           compileProject(project)
 
-          assertFileExists("project1/out/production/resources/dir/file-main.properties")
-          assertFileExists("project1/out/test/resources/dir/file-test.properties")
+          assertFileExists("project1/project/out/production/resources/dir/file-main.properties")
+          assertFileExists("project1/project/out/test/resources/dir/file-test.properties")
         }.useProjectAsync { project ->
           project.getGradleJpsResourceConfigs()
-
-          // Todo(Aleksei Cherepanov): remove it
-          emptyList<Path>()
         }
 
-      val configFiles2 = openProject("project2")
+      val configFiles2 = openProject("project2/project")
         .withProjectAsync { project ->
           compileProject(project)
 
-          assertFileExists("project2/out/production/resources/dir/file-main.properties")
-          assertFileExists("project2/out/test/resources/dir/file-test.properties")
+          assertFileExists("project2/project/out/production/resources/dir/file-main.properties")
+          assertFileExists("project2/project/out/test/resources/dir/file-test.properties")
         }.useProjectAsync { project ->
           project.getGradleJpsResourceConfigs()
-
-          // Todo(Aleksei Cherepanov): remove it
-          emptyList<Path>()
         }
 
       Assertions.assertEquals(configFiles1.size, configFiles2.size) {
@@ -69,11 +62,13 @@ class GradleRelativeConfigCalculatingTest : GradleRelativeConfigCalculatingTestC
         """.trimMargin()
       }
       for ((configFile1, configFile2) in configFiles1.zip(configFiles2)) {
-        Assertions.assertEquals(configFile1.readBytes(), configFile2.readBytes()) {
+        val configFile1String = configFile1.readText()
+        val configFile2String = configFile2.readText()
+        Assertions.assertEquals(configFile1String, configFile2String) {
           """
             |Config contents aren't equal
-            | Config1: $configFile1
-            | Config2: $configFile2
+            | Config1: $configFile1String
+            | Config2: $configFile2String
           """.trimMargin()
         }
       }
