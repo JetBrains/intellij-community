69c9be25821431d4b7668bafa03fea7b33277aca
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2023-02-22 12:16:40 +0100

    KTIJ-24591: enable experimental MPP features in Kotlin wizard tests to enable K/JS specific wizards
    GitOrigin-RevId: a6f0ce4559d8d17ba3db96c090963967c960ddf1

--- plugins/kotlin/project-wizard/tests/test/org/jetbrains/kotlin/tools/projectWizard/cli/AbstractProjectTemplateBuildFileGenerationTest.kt
+++ plugins/kotlin/project-wizard/tests/test/org/jetbrains/kotlin/tools/projectWizard/cli/AbstractProjectTemplateBuildFileGenerationTest.kt
@@ -2,11 +2,20 @@
 
 package org.jetbrains.kotlin.tools.projectWizard.cli
 
+import com.intellij.openapi.options.advanced.AdvancedSettings
+import com.intellij.openapi.options.advanced.AdvancedSettingsImpl
 import org.jetbrains.kotlin.tools.projectWizard.wizard.Wizard
 import org.jetbrains.kotlin.tools.projectWizard.wizard.YamlWizard
 import java.nio.file.Path
 
 abstract class AbstractProjectTemplateBuildFileGenerationTest : AbstractBuildFileGenerationTest() {
+
+    override fun setUp() {
+        super.setUp()
+        //enable experimental MPP features to enable K/JS specific wizards
+        (AdvancedSettings.getInstance() as AdvancedSettingsImpl).setSetting("kotlin.mpp.experimental", true, testRootDisposable)
+    }
+
     override fun createWizard(directory: Path, buildSystem: BuildSystem, projectDirectory: Path): Wizard =
         ProjectTemplateBasedTestWizard.createByDirectory(directory, buildSystem, projectDirectory, CLI_WIZARD_TEST_SERVICES_MANAGER)
 }
