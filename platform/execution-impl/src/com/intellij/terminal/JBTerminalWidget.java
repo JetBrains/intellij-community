92b2e976b69ce953e15a7988cc7bf6b858d0fc0b
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2023-02-23 00:16:01 +0100

    terminal: remove excessive `setTtyConnector`
    It's already done in `JBTerminalWidget.TerminalWidgetBridge.connectToTty`
    
    GitOrigin-RevId: 91f72ad564c60f9df726646f53e060a78e4f9eb4

--- platform/execution-impl/src/com/intellij/terminal/JBTerminalWidget.java
+++ platform/execution-impl/src/com/intellij/terminal/JBTerminalWidget.java
@@ -187,7 +187,6 @@ public class JBTerminalWidget extends JediTermWidget implements Disposable, Data
   @Override
   public void setTtyConnector(@NotNull TtyConnector ttyConnector) {
     super.setTtyConnector(ttyConnector);
-    myBridge.getTtyConnectorAccessor().setTtyConnector(ttyConnector);
     myTerminalTitle.change(terminalTitleState -> {
       if (terminalTitleState.getDefaultTitle() == null) {
         terminalTitleState.setDefaultTitle(getDefaultSessionName(ttyConnector));
