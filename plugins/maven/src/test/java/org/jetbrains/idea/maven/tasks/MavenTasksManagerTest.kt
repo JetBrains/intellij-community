dc3b164f8d89a5862079079008fd8fde2f9f506b
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-02-22 12:35:16 +0100

    IDEA-309027 convert java to kt
    GitOrigin-RevId: 20532a1140ea1aa6761bb14b7d04a0108097de41

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/tasks/MavenTasksManagerTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/tasks/MavenTasksManagerTest.kt
@@ -1,38 +1,33 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.idea.maven.tasks;
+package org.jetbrains.idea.maven.tasks
 
-import com.intellij.execution.ExecutionListener;
-import com.intellij.execution.ExecutionManager;
-import com.intellij.execution.runners.ExecutionEnvironment;
-import com.intellij.maven.testFramework.MavenCompilingTestCase;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.idea.maven.execution.MavenRunConfiguration;
-import org.junit.Test;
+import com.intellij.execution.ExecutionListener
+import com.intellij.execution.ExecutionManager
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.maven.testFramework.MavenCompilingTestCase
+import org.jetbrains.idea.maven.execution.MavenRunConfiguration
+import org.junit.Test
+import java.util.List
+import java.util.concurrent.atomic.AtomicBoolean
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-public class MavenTasksManagerTest extends MavenCompilingTestCase {
+class MavenTasksManagerTest : MavenCompilingTestCase() {
   @Test
-  public void testRunExecuteBeforeBuildTasks() {
+  fun testRunExecuteBeforeBuildTasks() {
     importProject("""
                     <groupId>test</groupId>
                     <artifactId>project</artifactId>
                     <version>1</version>
-                    """);
-
-    var mavenGoalExecuted = new AtomicBoolean(false);
-    subscribeToMavenGoalExecution("clean", mavenGoalExecuted);
-
-    addCompileTask(myProjectPom.getPath(), "clean");
-
-    compileModules("project");
-
-    assertTrue(mavenGoalExecuted.get());
+                    
+                    """.trimIndent())
+    val mavenGoalExecuted = AtomicBoolean(false)
+    subscribeToMavenGoalExecution("clean", mavenGoalExecuted)
+    addCompileTask(myProjectPom.path, "clean")
+    compileModules("project")
+    assertTrue(mavenGoalExecuted.get())
   }
 
   @Test
-  public void testRunExecuteBeforeBuildTasksInTheSameModule() {
+  fun testRunExecuteBeforeBuildTasksInTheSameModule() {
     createProjectPom("""
                   <groupId>group</groupId>
                   <artifactId>parent</artifactId>
@@ -42,8 +37,9 @@ public class MavenTasksManagerTest extends MavenCompilingTestCase {
                     <module>m1</module>
                     <module>m2</module>
                   </modules>
-                  """);
-    var m1File = createModulePom("m1", """
+                  
+                  """.trimIndent())
+    val m1File = createModulePom("m1", """
                   <artifactId>m1</artifactId>
                   <version>1</version>
                   <parent>
@@ -51,8 +47,9 @@ public class MavenTasksManagerTest extends MavenCompilingTestCase {
                     <artifactId>parent</artifactId>
                     <version>1</version>
                   </parent>
-                  """);
-    var m2File = createModulePom("m2", """
+                  
+                  """.trimIndent())
+    val m2File = createModulePom("m2", """
                   <artifactId>m2</artifactId>
                   <version>1</version>
                   <parent>
@@ -60,21 +57,18 @@ public class MavenTasksManagerTest extends MavenCompilingTestCase {
                     <artifactId>parent</artifactId>
                     <version>1</version>
                   </parent>
-                  """);
-    importProject();
-
-    var mavenGoalExecuted = new AtomicBoolean(false);
-    subscribeToMavenGoalExecution("generate-sources", mavenGoalExecuted);
-
-    addCompileTask(m1File.getPath(), "generate-sources");
-
-    compileModules("m1");
-
-    assertTrue(mavenGoalExecuted.get());
+                  
+                  """.trimIndent())
+    importProject()
+    val mavenGoalExecuted = AtomicBoolean(false)
+    subscribeToMavenGoalExecution("generate-sources", mavenGoalExecuted)
+    addCompileTask(m1File.path, "generate-sources")
+    compileModules("m1")
+    assertTrue(mavenGoalExecuted.get())
   }
 
   @Test
-  public void testDontRunExecuteBeforeBuildTasksInAnotherModule() {
+  fun testDontRunExecuteBeforeBuildTasksInAnotherModule() {
     createProjectPom("""
                   <groupId>group</groupId>
                   <artifactId>parent</artifactId>
@@ -84,8 +78,9 @@ public class MavenTasksManagerTest extends MavenCompilingTestCase {
                     <module>m1</module>
                     <module>m2</module>
                   </modules>
-                  """);
-    var m1File = createModulePom("m1", """
+                  
+                  """.trimIndent())
+    val m1File = createModulePom("m1", """
                   <artifactId>m1</artifactId>
                   <version>1</version>
                   <parent>
@@ -93,8 +88,9 @@ public class MavenTasksManagerTest extends MavenCompilingTestCase {
                     <artifactId>parent</artifactId>
                     <version>1</version>
                   </parent>
-                  """);
-    var m2File = createModulePom("m2", """
+                  
+                  """.trimIndent())
+    val m2File = createModulePom("m2", """
                   <artifactId>m2</artifactId>
                   <version>1</version>
                   <parent>
@@ -102,37 +98,33 @@ public class MavenTasksManagerTest extends MavenCompilingTestCase {
                     <artifactId>parent</artifactId>
                     <version>1</version>
                   </parent>
-                  """);
-    importProject();
-
-    var mavenGoalExecuted = new AtomicBoolean(false);
-    subscribeToMavenGoalExecution("generate-sources", mavenGoalExecuted);
-
-    addCompileTask(m1File.getPath(), "generate-sources");
-
-    compileModules("m2");
-
-    assertFalse(mavenGoalExecuted.get());
+                  
+                  """.trimIndent())
+    importProject()
+    val mavenGoalExecuted = AtomicBoolean(false)
+    subscribeToMavenGoalExecution("generate-sources", mavenGoalExecuted)
+    addCompileTask(m1File.path, "generate-sources")
+    compileModules("m2")
+    assertFalse(mavenGoalExecuted.get())
   }
 
-  private void addCompileTask(String pomPath, String goal) {
-    var mavenTasksManager = MavenTasksManager.getInstance(myProject);
-    var task = new MavenCompilerTask(pomPath, goal);
-    mavenTasksManager.addCompileTasks(List.of(task), MavenTasksManager.Phase.BEFORE_COMPILE);
+  private fun addCompileTask(pomPath: String, goal: String) {
+    val mavenTasksManager = MavenTasksManager.getInstance(myProject)
+    val task = MavenCompilerTask(pomPath, goal)
+    mavenTasksManager.addCompileTasks(List.of(task), MavenTasksManager.Phase.BEFORE_COMPILE)
   }
 
-  private void subscribeToMavenGoalExecution(String goal, AtomicBoolean executionFlag) {
-    var connection = myProject.getMessageBus().connect();
-    connection.subscribe(ExecutionManager.EXECUTION_TOPIC, new ExecutionListener() {
-      @Override
-      public void processStartScheduled(@NotNull final String executorIdLocal, @NotNull final ExecutionEnvironment environmentLocal) {
-        if (environmentLocal.getRunProfile() instanceof MavenRunConfiguration) {
-          var mavenGoal = ((MavenRunConfiguration)environmentLocal.getRunProfile()).getRunnerParameters().getGoals().get(0);
-          if (mavenGoal.equals(goal)) {
-            executionFlag.set(true);
+  private fun subscribeToMavenGoalExecution(goal: String, executionFlag: AtomicBoolean) {
+    val connection = myProject.messageBus.connect()
+    connection.subscribe(ExecutionManager.EXECUTION_TOPIC, object : ExecutionListener {
+      override fun processStartScheduled(executorIdLocal: String, environmentLocal: ExecutionEnvironment) {
+        if (environmentLocal.runProfile is MavenRunConfiguration) {
+          val mavenGoal = (environmentLocal.runProfile as MavenRunConfiguration).runnerParameters.goals[0]
+          if (mavenGoal == goal) {
+            executionFlag.set(true)
           }
         }
       }
-    });
+    })
   }
 }
