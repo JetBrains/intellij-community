7821a583cbddbe9973e0a8f95942f0869b476d1a
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2023-02-22 12:12:42 +0100

    IJ-CR-102807 IDEA-309683 incorrect all libraries synchronization request
    GitOrigin-RevId: d13ca7fe9a0982e0abdccf9c7f9e94531ae5b51d

--- java/idea-ui/src/com/intellij/jarRepository/LibrarySynchronizationQueue.kt
+++ java/idea-ui/src/com/intellij/jarRepository/LibrarySynchronizationQueue.kt
@@ -66,7 +66,7 @@ internal class LibrarySynchronizationQueue(private val project: Project, private
   }
 
   fun requestAllLibrariesSynchronization() {
-    synchronizationRequests.trySend(Request.Flush)
+    synchronizationRequests.trySend(Request.AllLibrariesSynchronization)
   }
 
   fun requestSynchronization(library: LibraryEx) {
