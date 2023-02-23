5d7a5e0ac7d32cff16d0222b9237eca55fe54920
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-02-23 15:15:21 +0100

    [java-inspections] IDEA-311156 inspection for migration to foreach with a record pattern. Fix code review
    GitOrigin-RevId: 63fda207fe39162b307eff0126abb9d586881d18

--- java/java-tests/testData/inspection/forEachWithRecordPatternCanBeUsed/afterUnusedCounts.java
+++ java/java-tests/testData/inspection/forEachWithRecordPatternCanBeUsed/afterUnusedCounts.java
@@ -8,6 +8,7 @@ public class UnusedCounts {
 
   public static void test(List<Record3> record3List) {
       for (Record3(var i1, var i2, var i3) : record3List) {
+          int t1 = i1;
           System.out.println(i1);
           System.out.println(i2);
           System.out.println(i3);
