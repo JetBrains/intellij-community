31dd8c5be9a295efee16e520e41968e97158a12b
Pavel Gromov <pavel.gromov@jetbrains.com>
2023-02-23 14:05:24 +0100

    [collab] Update commit popup UI
    GitOrigin-RevId: 38fc64fe39cc2eb30499d11ef0233f5ee2b4c2b3

--- platform/collaboration-tools/src/com/intellij/collaboration/ui/codereview/details/CommitRenderer.kt
+++ platform/collaboration-tools/src/com/intellij/collaboration/ui/codereview/details/CommitRenderer.kt
@@ -5,32 +5,32 @@ import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.ui.IdeBorderFactory
 import com.intellij.ui.SideBorder
-import com.intellij.ui.SimpleColoredComponent
 import com.intellij.ui.scale.JBUIScale
 import com.intellij.util.text.DateFormatUtil
-import com.intellij.util.ui.EmptyIcon
-import com.intellij.util.ui.JBUI
-import com.intellij.util.ui.ListUiUtil
-import com.intellij.util.ui.UIUtil
+import com.intellij.util.ui.*
 import com.intellij.util.ui.components.BorderLayoutPanel
 import java.awt.Component
 import java.util.*
 import javax.swing.JLabel
 import javax.swing.JList
 import javax.swing.ListCellRenderer
+import javax.swing.SwingConstants
 
 class CommitRenderer<T>(private val presenter: (T?) -> CommitPresenter) : ListCellRenderer<T> {
   private val iconLabel = JLabel().apply {
-    border = JBUI.Borders.empty(0, ICON_LEFT_RIGHT_OFFSET)
+    horizontalAlignment = SwingConstants.CENTER
+    verticalAlignment = SwingConstants.TOP
+    border = JBUI.Borders.empty(12, BORDER_OFFSET, 0, ICON_TEXT_OFFSET)
   }
-  private val allCommitsMessage = SimpleColoredComponent().apply {
-    border = JBUI.Borders.empty(TOP_BOTTOM_OFFSET, 0)
+  private val allCommitsMessage = JLabel().apply {
+    border = JBUI.Borders.empty(BORDER_OFFSET, 0)
   }
-  private val commitMessage = SimpleColoredComponent().apply {
-    border = JBUI.Borders.emptyTop(TOP_BOTTOM_OFFSET)
+  private val commitMessage = JLabel().apply {
+    border = JBUI.Borders.empty(BORDER_OFFSET, 0, 0, BORDER_OFFSET)
   }
-  private val authorAndDate = SimpleColoredComponent().apply {
-    border = JBUI.Borders.emptyBottom(TOP_BOTTOM_OFFSET)
+  private val authorAndDate = JLabel().apply {
+    border = JBUI.Borders.empty(0, 0, BORDER_OFFSET, BORDER_OFFSET)
+    foreground = NamedColorUtil.getInactiveTextColor()
   }
   private val textPanel = BorderLayoutPanel()
   private val commitPanel = BorderLayoutPanel()
@@ -42,20 +42,17 @@ class CommitRenderer<T>(private val presenter: (T?) -> CommitPresenter) : ListCe
                                             cellHasFocus: Boolean): Component {
     cleanupComponents()
 
-    commitMessage.foreground = ListUiUtil.WithTallRow.foreground(isSelected, list.hasFocus())
-    authorAndDate.foreground = ListUiUtil.WithTallRow.secondaryForeground(isSelected, list.hasFocus())
-
     val presentation = presenter(value)
     iconLabel.icon = if (presentation.isSelected) AllIcons.Actions.Checked_selected else emptyIcon
 
     when (presentation) {
       is CommitPresenter.SingleCommit -> {
-        commitMessage.append(presentation.title)
-        authorAndDate.append("${presentation.author} ${DateFormatUtil.formatPrettyDateTime(presentation.date)}")
+        commitMessage.text = presentation.title
+        authorAndDate.text = "${presentation.author}, ${DateFormatUtil.formatPrettyDateTime(presentation.date)}"
         textPanel.addToCenter(commitMessage).addToBottom(authorAndDate)
       }
       is CommitPresenter.AllCommits -> {
-        allCommitsMessage.append(presentation.title)
+        allCommitsMessage.text = presentation.title
         textPanel.addToCenter(allCommitsMessage)
       }
     }
@@ -69,15 +66,11 @@ class CommitRenderer<T>(private val presenter: (T?) -> CommitPresenter) : ListCe
   private fun cleanupComponents() {
     textPanel.removeAll()
     commitPanel.removeAll()
-
-    allCommitsMessage.clear()
-    commitMessage.clear()
-    authorAndDate.clear()
   }
 
   companion object {
-    private const val TOP_BOTTOM_OFFSET = 4
-    private const val ICON_LEFT_RIGHT_OFFSET = 8
+    private const val BORDER_OFFSET = 8
+    private const val ICON_TEXT_OFFSET = 6
     private const val EMPTY_ICON_SIZE = 12
 
     private val emptyIcon: EmptyIcon = JBUIScale.scaleIcon(EmptyIcon.create(EMPTY_ICON_SIZE))
