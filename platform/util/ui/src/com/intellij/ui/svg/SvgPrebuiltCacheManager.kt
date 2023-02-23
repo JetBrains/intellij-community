f3491a53102a1d467acef0a1022d8555a7aab97a
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-02-23 12:35:34 +0100

    Cleanup (idiomatic Kotlin; formatting)
    GitOrigin-RevId: cfbd0e1813e0fab9d69f0d6c23dae69a0ebad21c

--- platform/util/ui/src/com/intellij/ui/svg/SvgPrebuiltCacheManager.kt
+++ platform/util/ui/src/com/intellij/ui/svg/SvgPrebuiltCacheManager.kt
@@ -1,5 +1,4 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-@file:Suppress("ReplaceGetOrSet", "ReplacePutWithAssignment", "JAVA_MODULE_DOES_NOT_EXPORT_PACKAGE")
 package com.intellij.ui.svg
 
 import com.intellij.diagnostic.StartUpMeasurer
@@ -16,29 +15,22 @@ internal class SvgPrebuiltCacheManager(dbDir: Path) {
 
   fun loadFromCache(key: Int, mapper: SvgCacheMapper): Image? {
     val start = StartUpMeasurer.getCurrentTimeIfEnabled()
+
     val list = if (mapper.isDark) darkStores else lightStores
-    val store = if (mapper.scale == 1f) list.s1.getOrCreate()
-    else if (mapper.scale == 2f) list.s2.getOrCreate()
-    else if (mapper.scale == 1.25f && SystemInfoRt.isWindows) list.s1_25.getOrCreate()
-    else if (mapper.scale == 1.5f && SystemInfoRt.isWindows) list.s1_5.getOrCreate()
-    else return null  // unsupported scale
+    val store = when {
+      mapper.scale == 1f -> list.s1.getOrCreate()
+      mapper.scale == 1.25f && SystemInfoRt.isWindows -> list.s1_25.getOrCreate()
+      mapper.scale == 1.5f && SystemInfoRt.isWindows -> list.s1_5.getOrCreate()
+      mapper.scale == 2f -> list.s2.getOrCreate()
+      else -> return null  // unsupported scale
+    }
 
     val data = store.getUnboundedValue(key) ?: return null
-    val actualWidth: Int
-    val actualHeight: Int
     val format = data.get().toInt() and 0xff
-    @Suppress("DuplicatedCode")
-    if (format < 254) {
-      actualWidth = format
-      actualHeight = format
-    }
-    else if (format == 255) {
-      actualWidth = DataUtils.readVarInt(data)
-      actualHeight = actualWidth
-    }
-    else {
-      actualWidth = DataUtils.readVarInt(data)
-      actualHeight = DataUtils.readVarInt(data)
+    val (actualWidth, actualHeight) = when {
+      format < 254 -> format to format
+      format == 255 -> { val size = DataUtils.readVarInt(data); size to size }
+      else -> DataUtils.readVarInt(data) to DataUtils.readVarInt(data)
     }
 
     val image = readImage(buffer = data, w = actualWidth, h = actualHeight)
@@ -59,16 +51,15 @@ private class StoreContainer(private val dbDir: Path, private val scale: Float, 
   @Volatile
   private var store: Ikv.SizeUnawareIkv? = null
 
-  fun getOrCreate() = store ?: getSynchronized()
+  fun getOrCreate(): Ikv.SizeUnawareIkv = store ?: getSynchronized()
 
   @Synchronized
   private fun getSynchronized(): Ikv.SizeUnawareIkv {
     var store = store
     if (store == null) {
-      store = Ikv.loadSizeUnawareIkv(dbDir.resolve("icon-v4-$scale$classifier.db"))
+      store = Ikv.loadSizeUnawareIkv(dbDir.resolve("icon-v4-${scale}${classifier}.db"))
       this.store = store
     }
     return store
   }
 }
-
