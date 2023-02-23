34758d871a930bc49690d698fc19be75c3599851
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2023-02-23 20:03:47 +0400

    [workspace model] Fix generator for source roots with multiple packages
    GitOrigin-RevId: 5b00a421a92fb5fee01b3da49a8561571f49da67

--- plugins/devkit/intellij.devkit.workspaceModel/tests/testSrc/com/intellij/devkit/workspaceModel/AllIntellijEntitiesGenerationTest.kt
+++ plugins/devkit/intellij.devkit.workspaceModel/tests/testSrc/com/intellij/devkit/workspaceModel/AllIntellijEntitiesGenerationTest.kt
@@ -77,11 +77,11 @@ class AllIntellijEntitiesGenerationTest : CodeGenerationTestBase() {
     executeEntitiesGeneration(::generate)
   }
 
-  private fun executeEntitiesGeneration(generationFunction: (MutableEntityStorage, ModuleEntity, SourceRootEntity, String, Boolean) -> Boolean) {
+  private fun executeEntitiesGeneration(generationFunction: (MutableEntityStorage, ModuleEntity, SourceRootEntity, Set<String>, Boolean) -> Boolean) {
     val regex = Regex("interface [a-zA-Z0-9]+\\s*:\\s*WorkspaceEntity[a-zA-Z0-9]*")
     val (storage, jpsProjectSerializer) = runBlocking { loadProjectIntellijProject() }
 
-    val modulesToCheck = mutableSetOf<Triple<ModuleEntity, SourceRootEntity, String>>()
+    val modulesToCheck = mutableMapOf<Pair<ModuleEntity, SourceRootEntity>, MutableSet<String>>()
     storage.entities(SourceRootEntity::class.java).forEach { sourceRoot ->
       val moduleEntity = sourceRoot.contentRoot.module
 
@@ -90,7 +90,7 @@ class AllIntellijEntitiesGenerationTest : CodeGenerationTestBase() {
           if (regex.containsMatchIn(it.readText())) {
             val relativePath = Path.of(sourceRoot.url.presentableUrl).relativize(Path.of(it.parent)).systemIndependentPath
             if (moduleEntity.name to relativePath !in skippedModulePaths) {
-              modulesToCheck.add(Triple(moduleEntity, sourceRoot, relativePath))
+              modulesToCheck.getOrDefault(moduleEntity to sourceRoot, mutableSetOf()).add(relativePath)
             }
           }
         }
@@ -98,7 +98,7 @@ class AllIntellijEntitiesGenerationTest : CodeGenerationTestBase() {
     }
 
     var storageChanged = false
-    modulesToCheck.forEach {(moduleEntity, sourceRoot, pathToPackage) ->
+    modulesToCheck.forEach { (moduleEntity, sourceRoot), pathToPackages ->
       when (moduleEntity.name) {
         "intellij.platform.workspaceModel.storage" -> {
           runWriteActionAndWait {
@@ -106,7 +106,7 @@ class AllIntellijEntitiesGenerationTest : CodeGenerationTestBase() {
             removeWorkspaceStorageLibrary(modifiableModel)
             modifiableModel.commit()
           }
-          storageChanged = storageChanged || generationFunction(storage, moduleEntity, sourceRoot, pathToPackage, false)
+          storageChanged = storageChanged || generationFunction(storage, moduleEntity, sourceRoot, pathToPackages, false)
           runWriteActionAndWait {
             val modifiableModel = ModuleRootManager.getInstance(module).modifiableModel
             addWorkspaceStorageLibrary(modifiableModel)
@@ -114,10 +114,10 @@ class AllIntellijEntitiesGenerationTest : CodeGenerationTestBase() {
           }
         }
         in modulesWithUnknownFields -> {
-          storageChanged = storageChanged || generationFunction(storage, moduleEntity, sourceRoot, pathToPackage, true)
+          storageChanged = storageChanged || generationFunction(storage, moduleEntity, sourceRoot, pathToPackages, true)
         }
         else -> {
-          storageChanged = storageChanged || generationFunction(storage, moduleEntity, sourceRoot, pathToPackage, false)
+          storageChanged = storageChanged || generationFunction(storage, moduleEntity, sourceRoot, pathToPackages, false)
         }
       }
     }
@@ -127,49 +127,54 @@ class AllIntellijEntitiesGenerationTest : CodeGenerationTestBase() {
     PlatformTestUtil.dispatchAllInvocationEventsInIdeEventQueue()
   }
 
-  private fun generate(storage: MutableEntityStorage, moduleEntity: ModuleEntity, sourceRoot: SourceRootEntity, pathToPackage: String,
+  private fun generate(storage: MutableEntityStorage, moduleEntity: ModuleEntity, sourceRoot: SourceRootEntity, pathToPackages: Set<String>,
                        keepUnknownFields: Boolean = false): Boolean {
-    val packagePath = pathToPackage.replace(".", "/")
     val relativize = Path.of(IdeaTestExecutionPolicy.getHomePathWithPolicy()).relativize(Path.of(sourceRoot.url.presentableUrl))
     myFixture.copyDirectoryToProject(relativize.systemIndependentPath, "")
     LOG.info("Generating entities for module: ${moduleEntity.name}")
-    val (srcRoot, genRoot) = generateCode(packagePath, keepUnknownFields)
-    return runWriteActionAndWait {
-      var storageChanged = false
-      val genSourceRoot = sourceRoot.contentRoot.sourceRoots.flatMap { it.javaSourceRoots }.firstOrNull { it.generated }?.sourceRoot ?: run {
-        val genFolderVirtualFile = VfsUtil.createDirectories("${sourceRoot.contentRoot.url.presentableUrl}/${WorkspaceModelGenerator.GENERATED_FOLDER_NAME}")
-        val javaSourceRoot = sourceRoot.javaSourceRoots.first()
-        val result = storage.addEntity(
-          SourceRootEntity(genFolderVirtualFile.toVirtualFileUrl(virtualFileManager), sourceRoot.rootType, sourceRoot.entitySource) {
-            contentRoot = sourceRoot.contentRoot
-            javaSourceRoots = listOf(JavaSourceRootPropertiesEntity(true, javaSourceRoot.packagePrefix, javaSourceRoot.entitySource))
-          })
-        storageChanged = true
-        result
-      }
 
-      val apiRootPath = Path.of(sourceRoot.url.presentableUrl, pathToPackage)
-      val implRootPath = Path.of(genSourceRoot.url.presentableUrl, pathToPackage)
-      val virtualFileManager = VirtualFileManager.getInstance()
-      val apiDir = virtualFileManager.refreshAndFindFileByNioPath(apiRootPath)!!
-      val implDir = VirtualFileManager.getInstance().refreshAndFindFileByNioPath(implRootPath) ?: run {
-        VfsUtil.createDirectories(implRootPath.pathString)
+    return pathToPackages.map { pathToPackage->
+      val packagePath = pathToPackage.replace(".", "/")
+      val (srcRoot, genRoot) = generateCode(packagePath, keepUnknownFields)
+      runWriteActionAndWait {
+        var storageChanged = false
+        val genSourceRoot = sourceRoot.contentRoot.sourceRoots.flatMap { it.javaSourceRoots }.firstOrNull { it.generated }?.sourceRoot ?: run {
+          val genFolderVirtualFile = VfsUtil.createDirectories("${sourceRoot.contentRoot.url.presentableUrl}/${WorkspaceModelGenerator.GENERATED_FOLDER_NAME}")
+          val javaSourceRoot = sourceRoot.javaSourceRoots.first()
+          val result = storage.addEntity(
+            SourceRootEntity(genFolderVirtualFile.toVirtualFileUrl(virtualFileManager), sourceRoot.rootType, sourceRoot.entitySource) {
+              contentRoot = sourceRoot.contentRoot
+              javaSourceRoots = listOf(JavaSourceRootPropertiesEntity(true, javaSourceRoot.packagePrefix, javaSourceRoot.entitySource))
+            })
+          storageChanged = true
+          result
+        }
+
+        val apiRootPath = Path.of(sourceRoot.url.presentableUrl, pathToPackage)
+        val implRootPath = Path.of(genSourceRoot.url.presentableUrl, pathToPackage)
+        val virtualFileManager = VirtualFileManager.getInstance()
+        val apiDir = virtualFileManager.refreshAndFindFileByNioPath(apiRootPath)!!
+        val implDir = VirtualFileManager.getInstance().refreshAndFindFileByNioPath(implRootPath) ?: run {
+          VfsUtil.createDirectories(implRootPath.pathString)
+        }
+        VfsUtil.copyDirectory(this, srcRoot, apiDir, VirtualFileFilter { it != genRoot })
+        VfsUtil.copyDirectory(this, genRoot, implDir, null)
+        storageChanged
       }
-      VfsUtil.copyDirectory(this, srcRoot, apiDir, VirtualFileFilter { it != genRoot })
-      VfsUtil.copyDirectory(this, genRoot, implDir, null)
-      storageChanged
-    }
+    }.any { it }
   }
 
   private fun generateAndCompare(storage: MutableEntityStorage, moduleEntity: ModuleEntity, sourceRoot: SourceRootEntity,
-                                 pathToPackage: String, keepUnknownFields: Boolean = false): Boolean {
+                                 pathToPackages: Set<String>, keepUnknownFields: Boolean = false): Boolean {
     val genSourceRoots = sourceRoot.contentRoot.sourceRoots.flatMap { it.javaSourceRoots }.filter { it.generated }
     val relativize = Path.of(IdeaTestExecutionPolicy.getHomePathWithPolicy()).relativize(Path.of(sourceRoot.url.presentableUrl))
     myFixture.copyDirectoryToProject(relativize.systemIndependentPath, "")
-    val apiRootPath = Path.of(sourceRoot.url.presentableUrl, pathToPackage)
-    val implRootPath = Path.of(genSourceRoots.first().sourceRoot.url.presentableUrl, pathToPackage)
     LOG.info("Generating entities for module: ${moduleEntity.name}")
-    generateAndCompare(apiRootPath, implRootPath, keepUnknownFields, pathToPackage)
+    pathToPackages.forEach { pathToPackage ->
+      val apiRootPath = Path.of(sourceRoot.url.presentableUrl, pathToPackage)
+      val implRootPath = Path.of(genSourceRoots.first().sourceRoot.url.presentableUrl, pathToPackage)
+      generateAndCompare(apiRootPath, implRootPath, keepUnknownFields, pathToPackage)
+    }
     (tempDirFixture as LightTempDirTestFixtureImpl).deleteAll()
     return false
   }
