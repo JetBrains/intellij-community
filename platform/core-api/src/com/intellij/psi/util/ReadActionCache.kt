6d52de21a6f6519ada3d2831ca4e37a3bc5a5fb2
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2023-02-24 12:55:04 +0100

    IJ-CR-100779: some kdocs for `ReadActionCache` and `ReadActionCachedValue`
    GitOrigin-RevId: 020d02f00d636d5f6e7d0f0ed0185925a492f5a9

--- platform/core-api/src/com/intellij/psi/util/ReadActionCache.kt
+++ platform/core-api/src/com/intellij/psi/util/ReadActionCache.kt
@@ -5,6 +5,20 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.util.ProcessingContext
 import org.jetbrains.annotations.ApiStatus
 
+/**
+ * An application service, which provides a [processingContext] bound to the current read-action.
+ * It allows storing some data, which is actual for the current read-action only.
+ * The [processingContext] is shared only in the current thread, thus no thread-contention is expected,
+ * and no synchronisation is needed.
+ * After the current read-action ends, the [processingContext] is removed and all the data inside it becomes
+ * eligible for garbage collection. It makes a good way for a [short-living caching][ReadActionCachedValue].
+ *
+ * The [processingContext] is available only inside read-actions, which guarantee that "world" will not change
+ * between accessing the same [processingContext].
+ * It also available in write-action when called inside the [allowInWriteAction] blocks.
+ *
+ * @see ReadActionCachedValue
+ */
 @ApiStatus.Experimental
 interface ReadActionCache {
 
@@ -12,8 +26,6 @@ interface ReadActionCache {
    * @return the [ProcessingContext] associated with the current read action,
    * or `null` if called outside the read-action.
    * Inside write action returns `null` unless called inside [allowInWriteAction].
-   * 
-   * @see [ReadActionCachedValue]
    */
   val processingContext: ProcessingContext?
 
