7af18ed073fa654e36834ecc51c912eca20d1e29
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-02-24 11:02:28 +0100

    [code-inspections] CustomComponentExtension: experimental
    (cherry picked from commit 9458b14009930ffd579a19398466b58de0d1a00b)
    
    IJ-CR-103570
    
    GitOrigin-RevId: 00f6f7bb73d3c4cf7e1c472ca019c969e2b678e8

--- platform/lang-api/src/com/intellij/codeInspection/ui/CustomComponentExtensionWithSwingRenderer.java
+++ platform/lang-api/src/com/intellij/codeInspection/ui/CustomComponentExtensionWithSwingRenderer.java
@@ -1,14 +1,16 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInspection.ui;
 
 import com.intellij.codeInspection.options.CustomComponentExtension;
 import com.intellij.codeInspection.options.OptCustom;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
 
+@ApiStatus.Experimental
 public abstract class CustomComponentExtensionWithSwingRenderer<T> extends CustomComponentExtension<T> {
   protected CustomComponentExtensionWithSwingRenderer(@NotNull String id) {
     super(id);
