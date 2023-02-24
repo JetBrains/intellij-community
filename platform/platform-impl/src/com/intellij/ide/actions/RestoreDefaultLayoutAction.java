13cc8a20808f777b8ff12cf041e004bbc011d860
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2023-02-24 17:02:05 +0200

    [UI] IDEA-93140 Rename Save Current Layout, remove Save Changes
    Remove Save Changes from the layout submenu to avoid accidentally
    overwriting one.
    
    While we're at it, correct some descriptions.
    
    (cherry picked from commit 947b18c4f32757383a3639eab596cf78d1000038)
    (cherry picked from commit 99e285d85febe0646445753ed0955adb2a23684e)
    
    IJ-CR-104180
    
    GitOrigin-RevId: 51793619f027bfb4f842f8336a36f72d295b20f4

--- platform/platform-impl/src/com/intellij/ide/actions/RestoreDefaultLayoutAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/RestoreDefaultLayoutAction.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.actions;
 
+import com.intellij.idea.ActionsBundle;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.toolWindow.ToolWindowDefaultLayoutManager;
 import org.jetbrains.annotations.NotNull;
@@ -14,6 +15,12 @@ public final class RestoreDefaultLayoutAction extends RestoreNamedLayoutAction {
   @Override
   public boolean isSelected(@NotNull AnActionEvent e) {
     return false; // Restore Current Layout in the main Layouts menu looks weird with a check mark
+  }
+
+  @Override
+  public void update(@NotNull AnActionEvent e) {
+    super.update(e);
+    e.getPresentation().setDescription(ActionsBundle.message("action.RestoreDefaultLayout.description", getLayoutNameSupplier().invoke()));
   }
 
 }
