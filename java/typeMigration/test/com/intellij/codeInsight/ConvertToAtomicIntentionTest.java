886c37df3394956ded07b9065713d4e5c88f8d32
Bas Leijdekkers <basleijdekkers@gmail.com>
2023-02-24 18:31:27 +0100

    Java: fix converting multi-dimensional arrays to atomic
    GitOrigin-RevId: 43a811d589e04404cfe717e2b79e254be369ea23

--- java/typeMigration/test/com/intellij/codeInsight/ConvertToAtomicIntentionTest.java
+++ java/typeMigration/test/com/intellij/codeInsight/ConvertToAtomicIntentionTest.java
@@ -1,12 +1,14 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight;
 
 import com.intellij.codeInsight.daemon.quickFix.LightQuickFixParameterizedTestCase;
 import com.intellij.refactoring.typeMigration.TypeMigrationProcessor;
+import com.intellij.refactoring.typeMigration.intentions.ConvertFieldToAtomicIntention;
 import com.intellij.testFramework.PlatformTestUtil;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * @see ConvertFieldToAtomicIntention
  * @author anna
  */
 public class ConvertToAtomicIntentionTest extends LightQuickFixParameterizedTestCase {
