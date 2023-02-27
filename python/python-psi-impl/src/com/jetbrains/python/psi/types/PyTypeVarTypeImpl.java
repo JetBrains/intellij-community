fb5a3a200aef088940057acc5f3a470c06716d92
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2023-02-27 09:42:24 +0200

    PY-59241 Extract PyTypeVarType and PyTypeParameter interfaces, deprecate PyGenericType
    Existing usages haven't been updated yet not to hinder the upcoming integration
    of TypeVarTuple (PY-53105) and LiteralString (PY-58857) support.
    
    GitOrigin-RevId: 16aafd07edfdc98dee0240dce7cd3383b5eb0b00

---
+++ python/python-psi-impl/src/com/jetbrains/python/psi/types/PyTypeVarTypeImpl.java
@@ -0,0 +1,14 @@
+package com.jetbrains.python.psi.types;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public final class PyTypeVarTypeImpl extends PyGenericType {
+  public PyTypeVarTypeImpl(@NotNull String name, @Nullable PyType bound) {
+    super(name, bound);
+  }
+
+  public PyTypeVarTypeImpl(@NotNull String name, @Nullable PyType bound, boolean isDefinition) {
+    super(name, bound, isDefinition);
+  }
+}
