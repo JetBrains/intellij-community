d47015c21a579976099725085cdd3a11ecddfda9
Aleksey Pivovarov <AMPivovarov@gmail.com>
2023-02-27 11:33:31 +0100

    actions update thread - vcs
    GitOrigin-RevId: 8cc7f113dc4f6cb0ae398ab4cb75bdb146e031d9

--- platform/vcs-impl/src/com/intellij/vcs/console/ShowVcsConsoleTabAction.kt
+++ platform/vcs-impl/src/com/intellij/vcs/console/ShowVcsConsoleTabAction.kt
@@ -1,12 +1,17 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.console
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager
 import com.intellij.openapi.wm.ToolWindowManager
 
 class ShowVcsConsoleTabAction : DumbAwareAction() {
+  override fun getActionUpdateThread(): ActionUpdateThread {
+    return ActionUpdateThread.EDT
+  }
+
   override fun update(e: AnActionEvent) {
     val project = e.project
     if (project == null) {
