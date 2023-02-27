5cd52a418bba19de9c243e2d47497546d53133c2
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2023-02-27 18:07:48 +0100

    IDEA-313470 Add hovers for all icons at the bottom of welcome screen
    - Added hover for settings icon
    - Fixed aligning 'Settings' icon
    
    (cherry picked from commit 5f485fd8252748ac3bb04092106ef14bd6cb717a)
    
    IJ-CR-103851
    
    GitOrigin-RevId: c061e11a0b4e0466f5ca522b239dcf72863dfd3d

--- platform/platform-impl/src/com/intellij/ide/actions/ShowSettingsAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/ShowSettingsAction.java
@@ -4,21 +4,21 @@ package com.intellij.ide.actions;
 import com.intellij.CommonBundle;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.lightEdit.LightEditCompatible;
-import com.intellij.openapi.actionSystem.ActionPlaces;
-import com.intellij.openapi.actionSystem.ActionUpdateThread;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.actionSystem.ex.CustomComponentAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.roots.ui.configuration.actions.IconWithTextAction;
 import com.intellij.openapi.util.SystemInfo;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
-public class ShowSettingsAction extends AnAction implements DumbAware, LightEditCompatible {
+public class ShowSettingsAction extends AnAction implements DumbAware, LightEditCompatible, CustomComponentAction {
+
   private static final Logger LOG = Logger.getInstance(ShowSettingsAction.class);
 
   public ShowSettingsAction() {
@@ -31,8 +31,22 @@ public class ShowSettingsAction extends AnAction implements DumbAware, LightEdit
     if (e.getPlace().equals(ActionPlaces.WELCOME_SCREEN)) {
       e.getPresentation().setText(CommonBundle.settingsTitle());
     }
+    else if (e.getPlace().equals(ActionPlaces.WELCOME_SCREEN_QUICK_PANEL)) {
+      e.getPresentation().setIcon(null);
+      e.getPresentation().setText(CommonBundle.settingsTitle());
+    }
     else if (SystemInfo.isMacOSVentura) {
       e.getPresentation().setText(CommonBundle.settingsAction());
+    }
+  }
+
+  @Override
+  public @NotNull JComponent createCustomComponent(@NotNull Presentation presentation, @NotNull String place) {
+    if (place.equals(ActionPlaces.WELCOME_SCREEN_QUICK_PANEL)) {
+      return IconWithTextAction.createCustomComponentImpl(this, presentation, place);
+    }
+    else {
+      return CustomComponentAction.super.createCustomComponent(presentation, place);
     }
   }
 
