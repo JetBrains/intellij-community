30d8e5272cff5b51840a4f391439c201ba574c28
Pavel Gromov <pavel.gromov@jetbrains.com>
2023-02-28 16:56:12 +0100

    [fus] Fix Kotlin object registered as an extension (IDEA-312977)
    (cherry picked from commit fecc872b2821895cb754e61942f8014d2a0fb3e3)
    
    IJ-CR-104017
    
    GitOrigin-RevId: 3df184947d1cc4f7e5f69bd5ee389220229c98b5

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/cloneableProjects/VcsCloneCollector.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/cloneableProjects/VcsCloneCollector.kt
@@ -8,22 +8,26 @@ import com.intellij.internal.statistic.eventLog.events.EventPair
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
 import com.intellij.openapi.wm.impl.welcomeScreen.cloneableProjects.CloneableProjectsService.CloneStatus
 
-internal object VcsCloneCollector : CounterUsagesCollector() {
-  private val GROUP = EventLogGroup("vcs.clone", 1)
-
-  private val CLONE_STATUS_EVENT_FIELD = EventFields.Enum("status", CloneStatus::class.java)
-  private val CLONE_ACTIVITY = GROUP.registerIdeActivity(activityName = "cloning",
-                                                         finishEventAdditionalFields = arrayOf(CLONE_STATUS_EVENT_FIELD))
-
+internal class VcsCloneCollector : CounterUsagesCollector() {
   override fun getGroup(): EventLogGroup {
     return GROUP
   }
 
-  fun cloneStarted(): StructuredIdeActivity {
-    return CLONE_ACTIVITY.started(null)
-  }
+  companion object {
+    private val GROUP = EventLogGroup("vcs.clone", 1)
 
-  fun cloneFinished(activity: StructuredIdeActivity, cloneStatus: CloneStatus) {
-    activity.finished { listOf(EventPair(CLONE_STATUS_EVENT_FIELD, cloneStatus)) }
+    private val CLONE_STATUS_EVENT_FIELD = EventFields.Enum("status", CloneStatus::class.java)
+    private val CLONE_ACTIVITY = GROUP.registerIdeActivity(
+      activityName = "cloning",
+      finishEventAdditionalFields = arrayOf(CLONE_STATUS_EVENT_FIELD)
+    )
+
+    fun cloneStarted(): StructuredIdeActivity {
+      return CLONE_ACTIVITY.started(null)
+    }
+
+    fun cloneFinished(activity: StructuredIdeActivity, cloneStatus: CloneStatus) {
+      activity.finished { listOf(EventPair(CLONE_STATUS_EVENT_FIELD, cloneStatus)) }
+    }
   }
 }
