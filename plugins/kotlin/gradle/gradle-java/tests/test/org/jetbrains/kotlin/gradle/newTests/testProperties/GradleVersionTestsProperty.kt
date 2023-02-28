52d5c749bd8df7759f8b545447b59cca9c4f591a
Pavel Kargashinsky <pavel.kargashinsky@jetbrains.com>
2023-02-28 15:22:57 +0100

    [Multiplatform][Tests] Bump Gradle versions enum
    (cherry picked from commit fdb659eb3d98e5963b64ee0eb201a66dd951c7f3)
    
    GitOrigin-RevId: 3f7fea794f4b6116e39690094c2d819efbe05f69

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/newTests/testProperties/GradleVersionTestsProperty.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/newTests/testProperties/GradleVersionTestsProperty.kt
@@ -9,8 +9,8 @@ object GradleVersionTestsProperty : KotlinTestsResolvableProperty {
     enum class Value(val acronym: String, val version: String) {
         ForMinAgp("REQUIRED_FOR_MIN_AGP", "7.4"),
         ForStableAgp("REQUIRED_FOR_STABLE_AGP", "7.5.1"),
-        ForBetaAgp("REQUIRED_FOR_BETA_AGP", "8.0-rc-1"),
-        ForAlphaAgp("REQUIRED_FOR_ALPHA_AGP", "8.0-rc-1")
+        ForBetaAgp("REQUIRED_FOR_BETA_AGP", "8.0-rc-2"),
+        ForAlphaAgp("REQUIRED_FOR_ALPHA_AGP", "8.0-rc-2")
     }
 
     override val valuesByAcronyms: Map<String, String> = Value.values().map { it.acronym to it.version }.toMap()
