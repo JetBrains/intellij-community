cbee23de8151cc27ea27367bc6a787bbfc8b888d
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2023-03-01 14:25:21 +0100

    CPP-32396: xdebugger: Move XAlternativeSourceHandler to outer subpackage
    GitOrigin-RevId: 61f6d0b7e79a7d462cc90eb0bd12fd60e1bff22e

--- platform/xdebugger-api/src/com/intellij/xdebugger/XAlternativeSourceHandler.java
+++ platform/xdebugger-api/src/com/intellij/xdebugger/XAlternativeSourceHandler.java
@@ -1,8 +1,7 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.xdebugger.frame;
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.xdebugger;
 
-import com.intellij.xdebugger.XDebugProcess;
-import com.intellij.xdebugger.XSourcePosition;
+import com.intellij.xdebugger.frame.XStackFrame;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
