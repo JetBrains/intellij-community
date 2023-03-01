f6df1501b35aac926e7fada23a0ac677b2ab48fd
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-03-01 15:11:33 +0100

    [util] IDEA-314221 Changelist Chinese speed search issue
    (cherry picked from commit c4003b3e0eec31704ff16c0b5a6f618fe126ea45)
    
    IJ-CR-103916
    
    GitOrigin-RevId: 8d1f7007501fce95f141f2639c9286d493d2ddb6

--- platform/platform-tests/testSrc/com/intellij/psi/NameUtilTest.java
+++ platform/platform-tests/testSrc/com/intellij/psi/NameUtilTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi;
 
 import com.intellij.psi.codeStyle.NameUtil;
@@ -51,6 +37,11 @@ public class NameUtilTest {
   @Test
   public void testSplitIntoWords6() {
     assertSplitEquals(new String[]{"gg", "J"}, "ggJ");
+  }
+
+  @Test
+  public void testSplitIntoWordsCN() {
+    assertSplitEquals(new String[]{"测", "试", "打", "补", "丁"}, "测试打补丁");
   }
 
   private static void assertSplitEquals(String[] expected, String name) {
