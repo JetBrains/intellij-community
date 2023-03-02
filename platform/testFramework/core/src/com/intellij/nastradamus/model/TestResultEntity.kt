0715dbdc88c4445e415b32da38d62364997e87d9
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2023-03-02 11:13:25 +0200

    [aggregator] IDEA-313833 sending bucket id and buckets count in test results
    GitOrigin-RevId: 3cf8290bb4c92b8d7361ac2a2b97abd7e176cd87

--- platform/testFramework/core/src/com/intellij/nastradamus/model/TestResultEntity.kt
+++ platform/testFramework/core/src/com/intellij/nastradamus/model/TestResultEntity.kt
@@ -35,4 +35,11 @@ data class TestResultEntity(
 
   @JsonProperty("is_muted")
   val isMuted: Boolean,
+
+  @JsonProperty("bucket_id")
+  val bucketId: Int,
+
+  // total buckets number for build type
+  @JsonProperty("buckets_number")
+  val bucketsNumber: Int
 )
