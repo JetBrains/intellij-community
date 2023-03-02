493d721061c8f3ed522b9d064de88b47554edf8a
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2023-03-02 14:10:29 +0100

    MarkerType refactored to use PsiTargetNavigator
    GitOrigin-RevId: 3cdf79aaa5bc0a1c65852f323e926a1ca63ac29f

--- platform/lang-impl/src/com/intellij/codeInsight/navigation/PsiTargetNavigator.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/navigation/PsiTargetNavigator.kt
@@ -2,6 +2,7 @@
 package com.intellij.codeInsight.navigation
 
 import com.intellij.codeInsight.CodeInsightBundle
+import com.intellij.find.FindUtil
 import com.intellij.ide.util.EditSourceUtil
 import com.intellij.navigation.TargetPresentation
 import com.intellij.openapi.actionSystem.ex.ActionUtil
@@ -10,14 +11,17 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.popup.JBPopup
 import com.intellij.openapi.util.Computable
 import com.intellij.openapi.util.NlsContexts.PopupTitle
+import com.intellij.openapi.util.NlsContexts.TabTitle
 import com.intellij.psi.PsiElement
 import com.intellij.psi.SmartPointerManager
+import com.intellij.psi.SmartPsiElementPointer
 import com.intellij.psi.search.PsiElementProcessor
+import com.intellij.ui.awt.RelativePoint
 import com.intellij.ui.list.buildTargetPopupWithMultiSelect
-import java.util.function.BiConsumer
+import com.intellij.util.containers.map2Array
+import java.awt.event.MouseEvent
+import java.util.function.*
 import java.util.function.Function
-import java.util.function.Predicate
-import java.util.function.Supplier
 
 class PsiTargetNavigator<T: PsiElement>(val supplier: Supplier<List<T>>) {
 
@@ -27,11 +31,13 @@ class PsiTargetNavigator<T: PsiElement>(val supplier: Supplier<List<T>>) {
   private var presentationProvider: Function<T, TargetPresentation> = Function { targetPresentation(it) }
   private var elementsConsumer: BiConsumer<List<T>, PsiTargetNavigator<T>>? = null
   private var title: @PopupTitle String? = null
+  private var tabTitle: @TabTitle String? = null
 
   fun selection(selection: PsiElement?): PsiTargetNavigator<T> = apply { this.selection = selection }
   fun presentationProvider(provider: Function<T, TargetPresentation>): PsiTargetNavigator<T> = apply { this.presentationProvider = provider }
   fun elementsConsumer(consumer: BiConsumer<List<T>, PsiTargetNavigator<T>>): PsiTargetNavigator<T> = apply { elementsConsumer = consumer }
   fun title(title: @PopupTitle String?): PsiTargetNavigator<T> = apply { this.title = title }
+  fun tabTitle(title: @TabTitle String?): PsiTargetNavigator<T> = apply { this.tabTitle = title }
 
   fun createPopup(project: Project, @PopupTitle title: String?): JBPopup {
     return createPopup(project, title) { element -> EditSourceUtil.navigateToPsiElement(element) }
@@ -39,7 +45,7 @@ class PsiTargetNavigator<T: PsiElement>(val supplier: Supplier<List<T>>) {
 
   fun createPopup(project: Project, @PopupTitle title: String?, processor: PsiElementProcessor<T>): JBPopup {
     val (items, selected) = computeItems(project)
-    return buildPopup(items, title, selected, getPredicate(processor))
+    return buildPopup(items, title, project, selected, getPredicate(processor))
   }
 
   private fun getPredicate(processor: PsiElementProcessor<T>) = Predicate<ItemWithPresentation> {
@@ -47,7 +53,19 @@ class PsiTargetNavigator<T: PsiElement>(val supplier: Supplier<List<T>>) {
   }
 
   fun navigate(editor: Editor, @PopupTitle title: String?, processor: PsiElementProcessor<T>) {
-    val (items, selected) = computeItems(editor.project!!)
+    navigate(editor.project!!, title, processor, Consumer { it.showInBestPositionFor(editor) })
+  }
+
+  fun navigate(e: MouseEvent, @PopupTitle title: String?, project: Project) {
+    navigate(project, title, { element -> EditSourceUtil.navigateToPsiElement(element) }, Consumer { it.show(RelativePoint(e)) })
+  }
+
+  private fun navigate(project: Project,
+                       @PopupTitle title: String?,
+                       processor: PsiElementProcessor<T>,
+                       popupConsumer: Consumer<JBPopup>
+  ) {
+    val (items, selected) = computeItems(project)
     val predicate = getPredicate(processor)
     if (items.isEmpty()) {
       return
@@ -56,7 +74,8 @@ class PsiTargetNavigator<T: PsiElement>(val supplier: Supplier<List<T>>) {
       predicate.test(items.first())
     }
     else {
-      buildPopup(items, title, selected, predicate).showInBestPositionFor(editor)
+      val popup = buildPopup(items, title, project, selected, predicate)
+      popupConsumer.accept(popup)
     }
   }
 
@@ -88,12 +107,16 @@ class PsiTargetNavigator<T: PsiElement>(val supplier: Supplier<List<T>>) {
 
   private fun buildPopup(targets: List<ItemWithPresentation>,
                          @PopupTitle title: String?,
+                         project: Project,
                          selected: ItemWithPresentation?,
                          predicate: Predicate<ItemWithPresentation>): JBPopup {
     val builder = buildTargetPopupWithMultiSelect(targets, Function { it.presentation }, predicate)
     val s = title ?: this.title
     s?.let {
       builder.setTitle(s)
+    }
+    if (tabTitle != null) {
+      builder.setCouldPin { FindUtil.showInUsageView(null, tabTitle!!, project, targets.map2Array { item ->  item.item as SmartPsiElementPointer<*>}) != null }
     }
     selected.let { builder.setSelectedValue(selected, true) }
     return builder.createPopup()
