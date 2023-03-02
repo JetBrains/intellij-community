ca16ff7fc0ffa90f17067029275d1aafd2871278
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2023-03-02 14:04:34 +0200

    [KTIJ-24691] Advance test KGP version to 1.9.0-dev-2695
    (cherry picked from commit f2077e1b310c71d05940432484d59de9d75c3374)
    
    GitOrigin-RevId: 93bef2348814527080b4cd7d9f69add3a9139345

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/KotlinGradlePluginVersions.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/KotlinGradlePluginVersions.kt
@@ -7,7 +7,7 @@ import org.jetbrains.kotlin.tooling.core.isStable
 object KotlinGradlePluginVersions {
     val V_1_7_21 = KotlinToolingVersion(1, 7, 21, null)
     val V_1_8_0 = KotlinToolingVersion(1, 8, 0, null)
-    val latest = KotlinToolingVersion("1.9.0-dev-764")
+    val latest = KotlinToolingVersion("1.9.0-dev-2695")
 
     val all = listOf(
         V_1_7_21,
