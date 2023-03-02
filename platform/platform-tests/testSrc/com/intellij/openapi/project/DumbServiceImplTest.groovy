62d6743b6eab96080a202f4b92b03a8ce496bba3
Andrei.Kuznetsov <andrei.kuznetsov@jetbrains.com>
2023-03-02 13:14:56 +0100

    IDEA-314488: make sure that runnables scheduled via `DumbService.runWhenSmart` are executed on EDT *without* write lock
    (cherry picked from commit f4051cfe398690658b089d65edd2e35e08094f44)
    
    IJ-CR-104131
    
    GitOrigin-RevId: 36afa0da2c60794deb667034d971fc4355b24729

--- platform/platform-tests/testSrc/com/intellij/openapi/project/DumbServiceImplTest.groovy
+++ platform/platform-tests/testSrc/com/intellij/openapi/project/DumbServiceImplTest.groovy
@@ -25,15 +25,13 @@ import com.intellij.util.indexing.FileBasedIndexImpl
 import com.intellij.util.indexing.contentQueue.IndexUpdateRunner
 import com.intellij.util.indexing.diagnostic.ProjectIndexingHistoryImpl
 import com.intellij.util.indexing.diagnostic.ScanningType
+import com.intellij.util.messages.Topic
 import org.jetbrains.annotations.NotNull
 import org.junit.*
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.Future
-import java.util.concurrent.Phaser
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.*
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -111,6 +109,7 @@ class DumbServiceImplTest {
       }
     }
 
+    // TODO: queued tasks never executed, because new DumbServiceImpl creates a service in DUMB mode
     dumbService.queueTask(task1)
     dumbService.queueTask(task2)
     Disposer.dispose(dumbService)
@@ -253,6 +252,76 @@ class DumbServiceImplTest {
     long avg = ArrayUtil.averageAmongMedians(delays, 3)
     assert avg == 0: "Seems there's is a significant delay between becoming smart and waitForSmartMode() return. Delays in ms:\n" +
                      Arrays.toString(delays) + "\n"
+  }
+
+
+  @Topic.ProjectLevel
+  private static final Topic<DumbService.DumbModeListener> TEST_DUMB_MODE = new Topic<>("test dumb mode",
+                                                                                        DumbService.DumbModeListener.class,
+                                                                                        Topic.BroadcastDirection.NONE)
+
+  @Topic.ProjectLevel
+  private static final Topic<Runnable> TEST_TOPIC = new Topic<>("test topic", Runnable.class, Topic.BroadcastDirection.NONE)
+
+  @Test
+  void "test write lock in DumbModeListener does not propagate to runWhenSmart subscribers"() {
+    // The same is applicable to ScanningListener and ProjectActivity listeners in SmartModeScheduler.
+    // But test only considers DumbModeListener (two other tests look exactly the same).
+    // Proper solution (coming soon) - use async listeners in SmartModeScheduler (e.g. MutableStateFlow)
+
+    // First listener.
+    // Starts write action and publishes unrelated event. Publishing will propagate write action to subsequent DumbModeListener's
+    project.messageBus.connect(testDisposable).subscribe(TEST_DUMB_MODE, new DumbService.DumbModeListener() {
+      @Override
+      void exitDumbMode() {
+        // The whole point of the test is that this write action should not propagate to DumbService#runWhenSmart
+        ApplicationManager.application.runWriteAction {
+          project.messageBus.syncPublisher(TEST_TOPIC).run()
+        }
+      }
+    })
+
+    // subscriber for event published from first listener under write action (to enforce write action propagation)
+    project.messageBus.connect(testDisposable).subscribe(TEST_TOPIC) {}
+
+    // Second listener. Verifies that write action propagates, otherwise we didn't need this test at all
+    boolean writeActionPropagatesToSubscribers = false
+    project.messageBus.connect(testDisposable).subscribe(TEST_DUMB_MODE, new DumbService.DumbModeListener() {
+      @Override
+      void exitDumbMode() {
+        writeActionPropagatesToSubscribers = ApplicationManager.application.writeAccessAllowed
+      }
+    })
+
+    // Third listener. Ends dumb mode and invokes DumbService#runWhenSmart runnables. runWhenSmart runnables should not enjoy write action
+    project.messageBus.connect(testDisposable).subscribe(TEST_DUMB_MODE, new SmartModeScheduler.SmartModeSchedulerDumbModeListener(project))
+
+    // now start dumb mode
+    assert project.getService(SmartModeScheduler.class).currentMode == 0
+    EdtTestUtil.runInEdtAndWait {
+      project.messageBus.syncPublisher(TEST_DUMB_MODE).enteredDumbMode()
+    }
+    assert project.getService(SmartModeScheduler.class).currentMode != 0
+
+    // this is our runWhenSmart subscriber. We want it to not see write action started by the first listener
+    CompletableFuture<Boolean> writeIntentLockInRunWhenSmart = new CompletableFuture<>()
+    dumbService.runWhenSmart {
+      writeIntentLockInRunWhenSmart.complete(ApplicationManager.application.writeAccessAllowed)
+    }
+
+    // Now end dumb mode and check how different listeners behave
+    EdtTestUtil.runInEdtAndWait {
+      project.messageBus.syncPublisher(TEST_DUMB_MODE).exitDumbMode()
+    }
+    dumbService.waitForSmartMode()
+
+    assert writeActionPropagatesToSubscribers: "Assumption about platform behavior failed. " +
+                                               "It is not that we want this behavior, this behavior is hard wired into the platform. " +
+                                               "If we don't observe write lock started by the first listener in the second listener, " +
+                                               "then the whole test does not make any sense."
+
+    assert writeIntentLockInRunWhenSmart.get(5, TimeUnit.SECONDS) == false:
+      "DumbService should not propagate write action to runWhenSmart subscribers"
   }
 
   @Test
