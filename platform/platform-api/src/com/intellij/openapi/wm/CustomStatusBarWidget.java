469b93e7eb870ddb46a05e3dc7a5bcddb080c47c
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2023-03-03 16:48:37 +0200

    [platform] IDEA-313422 Document threading rules for CustomStatusBarWidget
    It's yet another of the cases when a component is not really a Swing component,
    but more like an API entity that wraps a Swing component. The said entity
    can be instantiated on a BGT, a Swing component can not, as it can lead
    to CME when Swing decides to modify some internal structures in a constructor,
    specifically UI Defaults.
    
    GitOrigin-RevId: d53371d32adedc9938382b2ccf818eb102ea36d8

--- platform/platform-api/src/com/intellij/openapi/wm/CustomStatusBarWidget.java
+++ platform/platform-api/src/com/intellij/openapi/wm/CustomStatusBarWidget.java
@@ -3,6 +3,16 @@ package com.intellij.openapi.wm;
 
 import javax.swing.*;
 
+/**
+ * A status bar widget with a custom component.
+ * <p>
+ *   <em>Implementation note:</em> status bar widgets can be instantiated on a BGT,
+ *   so implementations should take care not to instantiate any Swing components in constructors
+ *   and/or field/property initializers. All initialization should happen in the {@link #getComponent()}
+ *   method. The simplest way to achieve this is to use Kotlin's lazy initialization for the component
+ *   property.
+ * </p>
+ */
 public interface CustomStatusBarWidget extends StatusBarWidget {
   JComponent getComponent();
 }
