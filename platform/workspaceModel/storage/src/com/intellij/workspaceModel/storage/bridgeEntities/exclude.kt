908729cffe7b5daed8dac9701dd946b2ac26c167
Semyon Kolton <semyon.kolton@jetbrains.com>
2023-03-03 17:56:32 +0100

    CPP-32474 Compare only `url` for ExcludeUrlEntity when checking for changes in `ContentRootEntity`
    (cherry picked from commit c6ae85f5d10b78c74f1f5b3ab591fec180ea4f1a)
    
    GitOrigin-RevId: 3b9be8606a6b3b30c5d89c94b8fc719bab35d57e

--- platform/workspaceModel/storage/src/com/intellij/workspaceModel/storage/bridgeEntities/exclude.kt
+++ platform/workspaceModel/storage/src/com/intellij/workspaceModel/storage/bridgeEntities/exclude.kt
@@ -14,6 +14,7 @@ import org.jetbrains.deft.ObjBuilder
 import org.jetbrains.deft.Type
 
 interface ExcludeUrlEntity : WorkspaceEntity {
+  @EqualsBy
   val url: VirtualFileUrl
 
   //region generated code
