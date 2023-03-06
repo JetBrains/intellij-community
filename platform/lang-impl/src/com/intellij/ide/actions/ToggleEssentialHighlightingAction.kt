f75d4512438a8163360a36ffd7bf070911d7958f
Filipp Vakhitov <filipp.vakhitov@jetbrains.com>
2023-03-06 11:56:33 +0200

    IDEA-312155 Global "essential" highlighting mode settings.
    (cherry picked from commit d7cab125b881efc59f5a1a2d6f7c5deb8120f72a)
    
    Change-Id: Id7cab125b881efc59f5a1a2d6f7c5deb8120f72a

--- platform/lang-impl/src/com/intellij/ide/actions/ToggleEssentialHighlightingAction.kt
+++ platform/lang-impl/src/com/intellij/ide/actions/ToggleEssentialHighlightingAction.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.actions
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ToggleAction
 import com.intellij.openapi.util.registry.RegistryManager
@@ -12,5 +13,9 @@ class ToggleEssentialHighlightingAction : ToggleAction() {
 
   override fun setSelected(e: AnActionEvent, state: Boolean) {
     RegistryManager.getInstance().get("ide.highlighting.mode.essential").setValue(state)
+  }
+
+  override fun getActionUpdateThread(): ActionUpdateThread {
+    return ActionUpdateThread.BGT
   }
 }
