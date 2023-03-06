f5effaaeb326ea05b181459309812a746f42f7a4
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-03-06 13:59:02 +0100

    [java] IDEA-210466 Suggest to move Java file to the source root
    GitOrigin-RevId: f225ebbb2bcb6084b36ca504aae4e5463c074fc5

--- java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/MoveFileFix.java
+++ java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/MoveFileFix.java
@@ -1,24 +1,16 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.daemon.impl.quickfix;
 
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo;
 import com.intellij.codeInspection.util.IntentionName;
+import com.intellij.java.analysis.JavaAnalysisBundle;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
@@ -63,13 +55,25 @@ public class MoveFileFix implements IntentionAction {
 
   @Override
   public void invoke(@NotNull Project project, Editor editor, PsiFile file) throws IncorrectOperationException {
+    int offset = -1;
+    if (editor.getVirtualFile().equals(myFile)) {
+      offset = editor.getCaretModel().getOffset();
+    }
     if (myFile.isValid() && myTarget.isValid()) {
       try {
         myFile.move(this, myTarget);
       }
       catch (IOException e) {
-        throw new IncorrectOperationException("Cannot move '" + myFile.getPath() + "' into '" + myTarget.getPath() + "'", (Throwable)e);
+        new Notification("FileSystemIssue", 
+                         JavaAnalysisBundle.message("notification.content.cannot.move.file", myFile.getPath(), myTarget.getPath(), e.getMessage()),
+                         NotificationType.ERROR)
+          .notify(project);
       }
+    }
+    FileEditorManager manager = FileEditorManager.getInstance(project);
+    manager.closeFile(myFile);
+    if (offset >= 0) {
+      manager.openTextEditor(new OpenFileDescriptor(project, myFile, offset), true);
     }
   }
 
