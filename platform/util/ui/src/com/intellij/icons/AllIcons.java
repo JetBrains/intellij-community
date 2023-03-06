ca0f88787d1d81c8ac0507156ecad8608e6da3f5
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2023-03-06 13:27:06 +0100

    IDEA-314575 Update Enable New UI option in Preferences menu
    (cherry picked from commit 5304e6ace1127794aac3e6f34802f0087d3f90d4)
    
    IJ-CR-104161
    
    GitOrigin-RevId: f7feaf17c3b0efa0a2c39f6347b16e2a41641cd9

--- platform/util/ui/src/com/intellij/icons/AllIcons.java
+++ platform/util/ui/src/com/intellij/icons/AllIcons.java
@@ -54,7 +54,8 @@ public class AllIcons {
     /** 16x16 */ public static final @NotNull Icon Edit = load("actions/edit.svg", -1253654087, 2);
     /** 16x16 */ public static final @NotNull Icon EditScheme = load("actions/editScheme.svg", 356615607, 2);
     /** 16x16 */ public static final @NotNull Icon EditSource = load("actions/editSource.svg", -1793495184, 2);
-    /** 16x16 */ public static final @NotNull Icon EnableNewUi = load("actions/enableNewUi.svg", 1145610884, 2);
+    /** 16x16 */ public static final @NotNull Icon EnableNewUi = load("actions/enableNewUi.svg", 1145610884, 0);
+    /** 16x16 */ public static final @NotNull Icon EnableNewUiSelected = load("actions/enableNewUiSelected.svg", -1417821710, 0);
     /** 16x16 */ public static final @NotNull Icon Execute = load("actions/execute.svg", -1785415525, 10);
     /** 16x16 */ public static final @NotNull Icon Exit = load("actions/exit.svg", -837971561, 2);
     /** 16x16 */ public static final @NotNull Icon Expandall = load("actions/expandall.svg", 1240410990, 2);
