69a6659447c6ecee941c139e065383fe73b6f331
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2023-03-07 18:24:24 +0100

    IJPL-52 update LogStats to account incomplete descriptors
    GitOrigin-RevId: 12af3dfa1457d7d1da071d42e855bf48e7683f4c

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/log/diagnostic/LogStats.kt
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/log/diagnostic/LogStats.kt
@@ -3,6 +3,7 @@
 
 package com.intellij.openapi.vfs.newvfs.persistent.log.diagnostic
 
+import com.intellij.openapi.vfs.newvfs.persistent.log.DescriptorStorage
 import com.intellij.openapi.vfs.newvfs.persistent.log.VfsLog
 import com.intellij.openapi.vfs.newvfs.persistent.log.VfsOperation
 import com.intellij.openapi.vfs.newvfs.persistent.log.VfsOperationTag
@@ -25,6 +26,7 @@ private data class Stats(
   val exceptionResultCount: AtomicInteger = AtomicInteger(0),
   val payloadSizeHist: IntHistogram = IntHistogram(listOf(0, 1, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 1024*16, 1024*128, 1024*1024)),
   val tagsCount: ConcurrentHashMap<VfsOperationTag, Int> = ConcurrentHashMap(VfsOperationTag.values().size),
+  val incompleteTagsCount: ConcurrentHashMap<VfsOperationTag, Int> = ConcurrentHashMap(VfsOperationTag.values().size),
   var elapsedTime: Duration = 0.seconds
 ) {
   val totalSize get() = descriptorStorageSize + payloadStorageSize
@@ -43,32 +45,43 @@ private fun calcStats(log: VfsLog): Stats {
         stats.payloadStorageSize = payloadStorage.size()
 
         descriptorStorage.readAll {
-          val descCount = stats.descriptorCount.incrementAndGet()
-          if (!it.result.hasValue) stats.exceptionResultCount.incrementAndGet()
-          stats.tagsCount.compute(it.tag, ::incStat)
           when (it) {
-            is VfsOperation.AttributesOperation.WriteAttribute -> {
-              val attributeId = stringEnumerator.valueOf(it.attributeIdEnumerated)
-              if (attributeId == null) stats.nullEnumeratedString.incrementAndGet()
-              val data = payloadStorage.readAt(it.attrDataPayloadRef)
-              if (data == null) {
-                stats.nullPayloads.incrementAndGet()
-              }
-              else {
-                stats.payloadSizeHist.add(data.size)
+            is DescriptorStorage.DescriptorReadResult.Incomplete -> {
+              stats.descriptorCount.incrementAndGet()
+              stats.incompleteTagsCount.compute(it.tag, ::incStat)
+            }
+            is DescriptorStorage.DescriptorReadResult.Valid -> {
+              stats.descriptorCount.incrementAndGet()
+              if (!it.operation.result.hasValue) stats.exceptionResultCount.incrementAndGet()
+              stats.tagsCount.compute(it.operation.tag, ::incStat)
+              when (it.operation) {
+                is VfsOperation.AttributesOperation.WriteAttribute -> {
+                  val attributeId = stringEnumerator.valueOf(it.operation.attributeIdEnumerated)
+                  if (attributeId == null) stats.nullEnumeratedString.incrementAndGet()
+                  val data = payloadStorage.readAt(it.operation.attrDataPayloadRef)
+                  if (data == null) {
+                    stats.nullPayloads.incrementAndGet()
+                  }
+                  else {
+                    stats.payloadSizeHist.add(data.size)
+                  }
+                }
+                is VfsOperation.ContentsOperation.WriteBytes -> {
+                  val data = payloadStorage.readAt(it.operation.dataPayloadRef)
+                  if (data == null) {
+                    stats.nullPayloads.incrementAndGet()
+                  } else {
+                    stats.payloadSizeHist.add(data.size)
+                  }
+                }
+                else -> {
+                  // TODO other content-writing ops, at the moment they don't seem to happen at fresh-start
+                  // no-op
+                }
               }
             }
-            is VfsOperation.ContentsOperation.WriteBytes -> {
-              val data = payloadStorage.readAt(it.dataPayloadRef)
-              if (data == null) {
-                stats.nullPayloads.incrementAndGet()
-              } else {
-                stats.payloadSizeHist.add(data.size)
-              }
-            }
-            else -> {
-              // TODO other content-writing ops, at the moment they don't seem to happen at fresh-start
-              // no-op
+            is DescriptorStorage.DescriptorReadResult.Invalid -> {
+              throw it.cause
             }
           }
           true
@@ -82,7 +95,7 @@ private fun calcStats(log: VfsLog): Stats {
 private fun Double.format(fmt: String) = String.format(fmt, this)
 
 
-private fun benchmark(log: VfsLog, runs: Int = 50) {
+private fun benchmark(log: VfsLog, runs: Int = 25) {
   val statsArr = mutableListOf<Stats>()
 
   repeat(runs) {
@@ -114,6 +127,6 @@ fun main(args: Array<String>) {
   assert(args.size == 1) { "Usage: <LogStats> <path to vfslog folder>" }
 
   val log = VfsLog(Path.of(args[0]), true)
-
-  benchmark(log)
+  single(log)
+  //benchmark(log)
 }
