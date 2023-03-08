2e7109f6fe37cf3ba74778b72bfbe9ddeb8ca4b4
Artem Mukhin <Artem.Mukhin@jetbrains.com>
2023-03-08 13:34:36 +0100

    PY-44191 PY-48163 Provide `Attach to process` binary for Linux AArch64 and universal binary for macOS
    Now we use the following binaries for `Attach to process`:
    
    Windows:
    - `attach_amd64.dll`
    - `attach_x86.dll`
    (no changes in this commit, no ARM64 support)
    
    Linux:
    - `attach_linux_amd64.so`
    - `attach_linux_x86.so`
    - `attach_linux_aarch64.so`
    (the last one was added)
    
    macOS:
    - `attach.dylib`
    (universal binary containing both arm64 and x86_64 code)
    
    Note, `attach_x86.dylib` was removed and was not merged into the universal binary because Apple dropped support for 32-bit apps in macOS Catalina (10.15). However, there is still obsolete code for macOS i386 in the `pydev` module that should be dropped in the future
    
    GitOrigin-RevId: 51cdf976eaf5580218cf6026849e4da51c9370e6

--- python/helpers/pydev/pydevd_attach_to_process/linux_and_mac/compile_mac_arm64.sh
+++
@@ -1,4 +0,0 @@
-g++ -fPIC -D_REENTRANT -std=c++11 -arch arm64 -c -o attach_arm64.o attach.cpp
-g++ -dynamiclib -nostartfiles -arch arm64 -o attach_arm64.dylib attach_arm64.o -lc
-rm attach_arm64.o
-mv attach_arm64.dylib ../attach_arm64.dylib
