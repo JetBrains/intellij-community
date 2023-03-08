96d4f8c832966a41e77c6434d7bf890750f5e381
Alex Plate <aleksei.plate@jetbrains.com>
2023-03-08 09:12:27 +0200

    Add docs regarding adding of source folders
    GitOrigin-RevId: fb01be551a7cd6527aa945186c4c4b64664f3f4b

--- platform/projectModel-api/src/com/intellij/openapi/roots/ContentEntry.java
+++ platform/projectModel-api/src/com/intellij/openapi/roots/ContentEntry.java
@@ -106,6 +106,13 @@ public interface ContentEntry extends Synthetic {
 
   /**
    * Adds a source or test source root under the content root. This method may be called only on an instance obtained from {@link ModifiableRootModel}.
+   * <p>
+   * This method will mark source folder as "user-defined". That means if the content root is imported from some external system,
+   * the new SourceRootEntity will use the internal entity source. Such source folder will be stored in .idea folder and won't
+   * be affected by external system reimport.
+   * If you want you source folder to behave exactly as content root, you can add source folder
+   * using {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, boolean)} or
+   * {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, JpsElement, ProjectModelExternalSource)} methods.
    *
    * @param file         the file or directory to add as a source root.
    * @param isTestSource true if the file or directory is added as a test source root.
@@ -117,6 +124,13 @@ public interface ContentEntry extends Synthetic {
   /**
    * Adds a source or test source root with the specified package prefix under the content root. This method may be called only on an
    * instance obtained from {@link ModifiableRootModel}.
+   * <p>
+   * This method will mark source folder as "user-defined". That means if the content root is imported from some external system,
+   * the new SourceRootEntity will use the internal entity source. Such source folder will be stored in .idea folder and won't
+   * be affected by external system reimport.
+   * If you want you source folder to behave exactly as content root, you can add source folder
+   * using {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, boolean)} or
+   * {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, JpsElement, ProjectModelExternalSource)} methods.
    *
    * @param file          the file or directory to add as a source root.
    * @param isTestSource  true if the file or directory is added as a test source root.
@@ -130,6 +144,13 @@ public interface ContentEntry extends Synthetic {
   /**
    * Adds a source root of the given type with the given properties. This method may be called only on an instance obtained from
    * {@link ModifiableRootModel}.
+   * <p>
+   * This method will mark source folder as "user-defined". That means if the content root is imported from some external system,
+   * the new SourceRootEntity will use the internal entity source. Such source folder will be stored in .idea folder and won't
+   * be affected by external system reimport.
+   * If you want you source folder to behave exactly as content root, you can add source folder
+   * using {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, boolean)} or
+   * {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, JpsElement, ProjectModelExternalSource)} methods.
    */
   @NotNull <P extends JpsElement> SourceFolder addSourceFolder(@NotNull VirtualFile file,
                                                                @NotNull JpsModuleSourceRootType<P> type,
@@ -138,12 +159,26 @@ public interface ContentEntry extends Synthetic {
   /**
    * Adds a source root of the given type with the default properties. This method may be called only on an instance obtained from
    * {@link ModifiableRootModel}.
+   * <p>
+   * This method will mark source folder as "user-defined". That means if the content root is imported from some external system,
+   * the new SourceRootEntity will use the internal entity source. Such source folder will be stored in .idea folder and won't
+   * be affected by external system reimport.
+   * If you want you source folder to behave exactly as content root, you can add source folder
+   * using {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, boolean)} or
+   * {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, JpsElement, ProjectModelExternalSource)} methods.
    */
   @NotNull <P extends JpsElement>
   SourceFolder addSourceFolder(@NotNull VirtualFile file, @NotNull JpsModuleSourceRootType<P> type);
 
   /**
    * Adds a source or test source root under the content root. This method may be called only on an instance obtained from {@link ModifiableRootModel}.
+   * <p>
+   * This method will mark source folder as "user-defined". That means if the content root is imported from some external system,
+   * the new SourceRootEntity will use the internal entity source. Such source folder will be stored in .idea folder and won't
+   * be affected by external system reimport.
+   * If you want you source folder to behave exactly as content root, you can add source folder
+   * using {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, boolean)} or
+   * {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, JpsElement, ProjectModelExternalSource)} methods.
    *
    * @param url          the file or directory url to add as a source root.
    * @param isTestSource true if the file or directory is added as a test source root.
@@ -155,16 +190,32 @@ public interface ContentEntry extends Synthetic {
   /**
    * Adds a source root of the given type with the default properties. This method may be called only on an instance obtained from
    * {@link ModifiableRootModel}.
+   * <p>
+   * This method will mark source folder as "user-defined". That means if the content root is imported from some external system,
+   * the new SourceRootEntity will use the internal entity source. Such source folder will be stored in .idea folder and won't
+   * be affected by external system reimport.
+   * If you want you source folder to behave exactly as content root, you can add source folder
+   * using {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, boolean)} or
+   * {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, JpsElement, ProjectModelExternalSource)} methods.
    */
   @NotNull <P extends JpsElement> SourceFolder addSourceFolder(@NotNull String url, @NotNull JpsModuleSourceRootType<P> type);
   /**
    * Adds a source root of the given type with the default properties. This method may be called only on an instance obtained from
-   * Also method defines an external source
    * {@link ModifiableRootModel}.
+   * <p>
+   * This method accepts externalSource, what means that if the content root is "externally imported" (e.g. using maven or gradle),
+   * the source root will get the {@link com.intellij.workspaceModel.storage.EntitySource} of the content entity and will
+   * also be marked as "externally imported".
    */
   @NotNull <P extends JpsElement> SourceFolder addSourceFolder(@NotNull String url,
                                                                @NotNull JpsModuleSourceRootType<P> type,
                                                                @NotNull ProjectModelExternalSource externalSource);
+
+  /**
+   * This method accepts useSourceOfContentRoot, what means that if the content root is "externally imported" (e.g. using maven or gradle),
+   * the source root will get the {@link com.intellij.workspaceModel.storage.EntitySource} of the content entity and will
+   * also be marked as "externally imported".
+   */
   @NotNull <P extends JpsElement> SourceFolder addSourceFolder(@NotNull String url,
                                                                @NotNull JpsModuleSourceRootType<P> type,
                                                                boolean useSourceOfContentRoot);
@@ -172,6 +223,13 @@ public interface ContentEntry extends Synthetic {
   /**
    * Adds a source root of the given type with given properties. This method may be called only on an instance obtained from
    * {@link ModifiableRootModel}.
+   * <p>
+   * This method will mark source folder as "user-defined". That means if the content root is imported from some external system,
+   * the new SourceRootEntity will use the internal entity source. Such source folder will be stored in .idea folder and won't
+   * be affected by external system reimport.
+   * If you want you source folder to behave exactly as content root, you can add source folder
+   * using {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, boolean)} or
+   * {@link ContentEntry#addSourceFolder(String, JpsModuleSourceRootType, JpsElement, ProjectModelExternalSource)} methods.
    */
   @NotNull <P extends JpsElement> SourceFolder addSourceFolder(@NotNull String url,
                                                                @NotNull JpsModuleSourceRootType<P> type,
@@ -179,8 +237,11 @@ public interface ContentEntry extends Synthetic {
 
   /**
    * Adds a source root of the given type with given properties. This method may be called only on an instance obtained from
-   * Also method defines an external source
    * {@link ModifiableRootModel}.
+   * <p>
+   * This method accepts externalSource, what means that if the content root is "externally imported" (e.g. using maven or gradle),
+   * the source root will get the {@link com.intellij.workspaceModel.storage.EntitySource} of the content entity and will
+   * also be marked as "externally imported".
    */
   @NotNull <P extends JpsElement> SourceFolder addSourceFolder(@NotNull String url,
                                                                @NotNull JpsModuleSourceRootType<P> type,
