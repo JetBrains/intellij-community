6f9762d76d8b605e5ac085fa2ad7ab8873df2584
Adam Malek <adam.malek@jetbrains.com>
2023-03-08 10:29:18 +0100

    ML in SE: Add extension functions for working with list of event pairs in tests
    GitOrigin-RevId: fa382ba3e2141410ad71c2fd0f1a375f6e69e238

--- plugins/search-everywhere-ml/test/com/intellij/ide/actions/searcheverywhere/ml/features/SearchEverywhereStateFeaturesProviderTest.kt
+++ plugins/search-everywhere-ml/test/com/intellij/ide/actions/searcheverywhere/ml/features/SearchEverywhereStateFeaturesProviderTest.kt
@@ -4,8 +4,6 @@ import com.intellij.ide.actions.searcheverywhere.FileSearchEverywhereContributor
 import com.intellij.ide.actions.searcheverywhere.SearchEverywhereManagerImpl
 import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereStateFeaturesProvider.Companion.IS_DUMB_MODE
 import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereStateFeaturesProvider.Companion.QUERY_CONTAINS_PATH_DATA_KEY
-import com.intellij.internal.statistic.eventLog.events.EventField
-import com.intellij.internal.statistic.eventLog.events.EventPair
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 
 class SearchEverywhereStateFeaturesProviderTest : BasePlatformTestCase() {
@@ -13,55 +11,49 @@ class SearchEverywhereStateFeaturesProviderTest : BasePlatformTestCase() {
 
   fun `test contains path feature exists in all tab`() {
     val features = featuresProvider.getSearchStateFeatures(SearchEverywhereManagerImpl.ALL_CONTRIBUTORS_GROUP_ID, "foo", false, null, false)
-    assertNotNull(findFeature(QUERY_CONTAINS_PATH_DATA_KEY, features))
+    assertNotNull(features.findFeature(QUERY_CONTAINS_PATH_DATA_KEY))
   }
 
   fun `test contains path feature exists in files tab`() {
     val features = featuresProvider.getSearchStateFeatures(FileSearchEverywhereContributor::class.java.simpleName, "foo", false, null, false)
-    assertNotNull(findFeature(QUERY_CONTAINS_PATH_DATA_KEY, features))
+    assertNotNull(features.findFeature(QUERY_CONTAINS_PATH_DATA_KEY))
   }
 
   fun `test contains path feature is false when no path specified`() {
     val features = featuresProvider.getSearchStateFeatures(FileSearchEverywhereContributor::class.java.simpleName, "foo", false, null, false)
-    val containsPath = findBooleanFeature(QUERY_CONTAINS_PATH_DATA_KEY, features)
+    val containsPath = features.getFeatureValue(QUERY_CONTAINS_PATH_DATA_KEY)
 
     assertFalse(containsPath)
   }
 
   fun `test contains path feature is false when nothing before slash`() {
     val features = featuresProvider.getSearchStateFeatures(FileSearchEverywhereContributor::class.java.simpleName, "/foo", false, null, false)
-    val containsPath = findBooleanFeature(QUERY_CONTAINS_PATH_DATA_KEY, features)
+    val containsPath = features.getFeatureValue(QUERY_CONTAINS_PATH_DATA_KEY)
 
     assertFalse(containsPath)
   }
 
   fun `test contains path feature is true with one slash`() {
     val features = featuresProvider.getSearchStateFeatures(FileSearchEverywhereContributor::class.java.simpleName, "bar/foo", false, null, false)
-    val containsPath = findBooleanFeature(QUERY_CONTAINS_PATH_DATA_KEY, features)
+    val containsPath = features.getFeatureValue(QUERY_CONTAINS_PATH_DATA_KEY)
 
     assertTrue(containsPath)
   }
 
   fun `test contains path feature is true with multiple slashes`() {
     val features = featuresProvider.getSearchStateFeatures(FileSearchEverywhereContributor::class.java.simpleName, "/x/bar/foo", false, null, false)
-    val containsPath = findBooleanFeature(QUERY_CONTAINS_PATH_DATA_KEY, features)
+    val containsPath = features.getFeatureValue(QUERY_CONTAINS_PATH_DATA_KEY)
 
     assertTrue(containsPath)
   }
 
   fun `test contains isDumbMode feature is true when isDumb is true`() {
     val featuresDumbFalse = featuresProvider.getSearchStateFeatures(FileSearchEverywhereContributor::class.java.simpleName, "/x/bar/foo", true, null, false)
-    assertTrue(findBooleanFeature(IS_DUMB_MODE, featuresDumbFalse))
+    assertTrue(featuresDumbFalse.getFeatureValue(IS_DUMB_MODE))
   }
 
   fun `test contains isDumbMode feature is null when isDumb null`() {
     val featuresDumbNull = featuresProvider.getSearchStateFeatures(FileSearchEverywhereContributor::class.java.simpleName, "/x/bar/foo", null, null, false)
-    assertNull(findFeature(IS_DUMB_MODE, featuresDumbNull))
+    assertNull(featuresDumbNull.findFeature(IS_DUMB_MODE))
   }
-
-  private fun findFeature(field: EventField<*>, features: List<EventPair<*>>): EventPair<*>? =
-    features.find { field.name == it.field.name }
-
-  private fun findBooleanFeature(field: EventField<*>, features: List<EventPair<*>>): Boolean=
-    findFeature(field, features)!!.data as Boolean
 }
