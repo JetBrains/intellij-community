557db9e948de437fba03d4e14cea5e7ded4e018e
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2023-03-09 10:35:07 +0100

    [kotlin] Clean up input filter
    #KTIJ-24889
    
    GitOrigin-RevId: 59093ef0a431f6dc0cbea12dde4155c12825fb7c

--- plugins/kotlin/base/indices/src/org/jetbrains/kotlin/idea/vfilefinder/KotlinShortClassNameFileIndex.kt
+++ plugins/kotlin/base/indices/src/org/jetbrains/kotlin/idea/vfilefinder/KotlinShortClassNameFileIndex.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.vfilefinder
 
-import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.util.indexing.DataIndexer
 import com.intellij.util.indexing.DefaultFileTypeSpecificInputFilter
 import com.intellij.util.indexing.FileBasedIndexExtension
@@ -15,54 +14,48 @@ import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtTreeVisitorVoid
 
 class KotlinShortClassNameFileIndex : FileBasedIndexExtension<String, Collection<String>>() {
-  companion object {
-    val NAME: ID<String, Collection<String>> = ID.create(KotlinShortClassNameFileIndex::class.java.canonicalName)
-  }
-
-  override fun getName(): ID<String, Collection<String>> = NAME
-
-  override fun dependsOnFileContent(): Boolean = true
-
-  override fun getKeyDescriptor(): EnumeratorStringDescriptor = EnumeratorStringDescriptor.INSTANCE
-
-  override fun getValueExternalizer() = StringSetExternalizer
-
-  override fun getInputFilter(): DefaultFileTypeSpecificInputFilter =
-    DefaultFileTypeSpecificInputFilter(
-      KotlinFileType.INSTANCE,
-      JavaFileType.INSTANCE
-    )
-
-  override fun getVersion() = 1
-
-  override fun traceKeyHashToVirtualFileMapping(): Boolean = true
-
-  override fun getIndexer() = DataIndexer<String, Collection<String>, FileContent> { fileContent ->
-    if (fileContent.fileName.contains("ConstantValueKind")) {
-      Unit
+    companion object {
+        val NAME: ID<String, Collection<String>> = ID.create(KotlinShortClassNameFileIndex::class.java.canonicalName)
     }
-    val ktFile = fileContent.psiFile as? KtFile ?: return@DataIndexer emptyMap()
-    val map = hashMapOf<String, Collection<String>>()
-    ktFile.acceptChildren(object : KtTreeVisitorVoid() {
-      override fun visitEnumEntry(enumEntry: KtEnumEntry) {
-        add(enumEntry.name, enumEntry.fqName?.asString())
-        super.visitEnumEntry(enumEntry)
-      }
 
-      override fun visitClassOrObject(classOrObject: KtClassOrObject) {
-        add(classOrObject.name, classOrObject.fqName?.asString())
-        super.visitClassOrObject(classOrObject)
-      }
+    override fun getName(): ID<String, Collection<String>> = NAME
 
-      private fun add(name: String?, fqName: String?) {
-        if (name != null && fqName != null) {
-          val fqNames = map.computeIfAbsent(name) {
-            hashSetOf()
-          } as HashSet
-          fqNames += fqName
-        }
-      }
-    })
-    map
-  }
+    override fun dependsOnFileContent(): Boolean = true
+
+    override fun getKeyDescriptor(): EnumeratorStringDescriptor = EnumeratorStringDescriptor.INSTANCE
+
+    override fun getValueExternalizer() = StringSetExternalizer
+
+    override fun getInputFilter(): DefaultFileTypeSpecificInputFilter =
+        DefaultFileTypeSpecificInputFilter(KotlinFileType.INSTANCE)
+
+    override fun getVersion() = 1
+
+    override fun traceKeyHashToVirtualFileMapping(): Boolean = true
+
+    override fun getIndexer() = DataIndexer<String, Collection<String>, FileContent> { fileContent ->
+        val ktFile = fileContent.psiFile as? KtFile ?: return@DataIndexer emptyMap()
+        val map = hashMapOf<String, Collection<String>>()
+        ktFile.acceptChildren(object : KtTreeVisitorVoid() {
+            override fun visitEnumEntry(enumEntry: KtEnumEntry) {
+                add(enumEntry.name, enumEntry.fqName?.asString())
+                super.visitEnumEntry(enumEntry)
+            }
+
+            override fun visitClassOrObject(classOrObject: KtClassOrObject) {
+                add(classOrObject.name, classOrObject.fqName?.asString())
+                super.visitClassOrObject(classOrObject)
+            }
+
+            private fun add(name: String?, fqName: String?) {
+                if (name != null && fqName != null) {
+                    val fqNames = map.computeIfAbsent(name) {
+                        hashSetOf()
+                    } as HashSet
+                    fqNames += fqName
+                }
+            }
+        })
+        map
+    }
 }
