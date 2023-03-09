cca4d45357c97b9905f30ffa2483f95cf58a7b89
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-03-09 15:33:34 +0100

    convert SvgIconSizeTest to kotlin
    GitOrigin-RevId: a6ce2db45f55053e4519cb31a10e7622fc2a6628

--- platform/platform-tests/testSrc/com/intellij/ui/scale/SvgIconSizeTest.kt
+++ platform/platform-tests/testSrc/com/intellij/ui/scale/SvgIconSizeTest.kt
@@ -1,110 +1,102 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.ui.scale;
+package com.intellij.ui.scale
 
-import com.intellij.testFramework.PlatformTestUtil;
-import com.intellij.ui.RestoreScaleRule;
-import com.intellij.ui.svg.SvgKt;
-import com.intellij.util.SVGLoader;
-import com.intellij.util.ui.ImageUtil;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.junit.rules.ExternalResource;
-
-import java.awt.*;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Path;
-
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertNotNull;
+import com.intellij.testFramework.PlatformTestUtil
+import com.intellij.ui.RestoreScaleRule
+import com.intellij.ui.scale.JBUIScale.setSystemScaleFactor
+import com.intellij.ui.scale.JBUIScale.setUserScaleFactor
+import com.intellij.ui.scale.JBUIScale.sysScale
+import com.intellij.ui.scale.TestScaleHelper.loadImage
+import com.intellij.ui.scale.TestScaleHelper.overrideJreHiDPIEnabled
+import com.intellij.ui.svg.getSvgDocumentSize
+import com.intellij.util.SVGLoader
+import com.intellij.util.SVGLoader.load
+import com.intellij.util.ui.ImageUtil
+import junit.framework.TestCase
+import org.junit.ClassRule
+import org.junit.Test
+import org.junit.rules.ExternalResource
+import java.io.IOException
+import java.nio.file.Path
 
 /**
- * Tests that {@link SVGLoader} correctly interprets SVG document size.
- *
- * @author tav
+ * Tests that [SVGLoader] correctly interprets SVG document size.
  */
-public class SvgIconSizeTest {
-  @ClassRule
-  public static final ExternalResource manageState = new RestoreScaleRule();
-
+class SvgIconSizeTest {
   @Test
-  public void test() throws IOException {
-    JBUIScale.setUserScaleFactor((float)1);
-    TestScaleHelper.overrideJreHiDPIEnabled(true);
-
-    test(ScaleContext.create(ScaleType.SYS_SCALE.of(1)));
-    test(ScaleContext.create(ScaleType.SYS_SCALE.of(2)));
-
-    float currentSysScale = JBUIScale.sysScale();
-    if (currentSysScale != 2) {
-      JBUIScale.setSystemScaleFactor(2);
+  @Throws(IOException::class)
+  fun test() {
+    setUserScaleFactor(1f)
+    overrideJreHiDPIEnabled(true)
+    test(ScaleContext.create(ScaleType.SYS_SCALE.of(1.0)))
+    test(ScaleContext.create(ScaleType.SYS_SCALE.of(2.0)))
+    val currentSysScale = sysScale()
+    if (currentSysScale != 2f) {
+      setSystemScaleFactor(2f)
       /*
        * Test with the system scale equal to the current system scale.
-       */
-      test(ScaleContext.create(ScaleType.SYS_SCALE.of(2)));
-      JBUIScale.setSystemScaleFactor(currentSysScale);
+       */test(ScaleContext.create(ScaleType.SYS_SCALE.of(2.0)))
+      setSystemScaleFactor(currentSysScale)
     }
 
     /*
      * Test overridden size.
      */
-    URL url = getSvgIconPath("20x10").toUri().toURL();
-    ScaleContext ctx = ScaleContext.create(ScaleType.SYS_SCALE.of(2));
-    double pixScale = ctx.getScale(DerivedScaleType.PIX_SCALE);
-    Image image = SVGLoader.load(url, url.openStream(), ctx, 25, 15);
-    assertNotNull(image);
-    image = ImageUtil.toBufferedImage(image);
-    assertEquals("wrong image width", pixScale * 25, (double)image.getWidth(null));
-    assertEquals("wrong image height", pixScale * 15, (double)image.getHeight(null));
+    var url = getSvgIconPath("20x10").toUri().toURL()
+    val ctx = ScaleContext.create(ScaleType.SYS_SCALE.of(2.0))
+    val pixScale = ctx.getScale(DerivedScaleType.PIX_SCALE)
+    var image = load(url, url.openStream(), ctx, 25.0, 15.0)
+    TestCase.assertNotNull(image)
+    image = ImageUtil.toBufferedImage(image)
+    TestCase.assertEquals("wrong image width", pixScale * 25, image.getWidth(null).toDouble())
+    TestCase.assertEquals("wrong image height", pixScale * 15, image.getHeight(null).toDouble())
 
     /*
      * Test SVGLoader.getDocumentSize for SVG starting with <svg.
-     */
-    url = getSvgIconPath("20x10").toUri().toURL();
-    Rectangle2D.Float size = SvgKt.getSvgDocumentSize(url.openStream().readAllBytes());
-    assertEquals("wrong svg doc width", 20d, size.getWidth());
-    assertEquals("wrong svg doc height", 10d, size.getHeight());
+     */url = getSvgIconPath("20x10").toUri().toURL()
+    var size = getSvgDocumentSize(url.openStream().readAllBytes())
+    TestCase.assertEquals("wrong svg doc width", 20.0, size.getWidth())
+    TestCase.assertEquals("wrong svg doc height", 10.0, size.getHeight())
 
     /*
      * Test SVGLoader.getDocumentSize for SVG starting with <?xml.
-     */
-    url = getSvgIconPath("xml_20x10").toUri().toURL();
-    size = SvgKt.getSvgDocumentSize(url.openStream().readAllBytes());
-    assertEquals("wrong svg doc width", 20d, size.getWidth());
-    assertEquals("wrong svg doc height", 10d, size.getHeight());
+     */url = getSvgIconPath("xml_20x10").toUri().toURL()
+    size = getSvgDocumentSize(url.openStream().readAllBytes())
+    TestCase.assertEquals("wrong svg doc width", 20.0, size.getWidth())
+    TestCase.assertEquals("wrong svg doc height", 10.0, size.getHeight())
   }
 
-  private static void test(ScaleContext ctx) {
-    int scale = (int)ctx.getScale(ScaleType.SYS_SCALE);
+  companion object {
+    @ClassRule
+    val manageState: ExternalResource = RestoreScaleRule()
+    private fun test(ctx: ScaleContext) {
+      val scale = ctx.getScale(ScaleType.SYS_SCALE).toInt()
 
-    /*
+      /*
      * Test default unit ("px").
      */
-    BufferedImage image = TestScaleHelper.loadImage(getSvgIconPath("20x10"), ctx);
-    assertNotNull(image);
-    assertEquals("wrong image width", 20 * scale, image.getWidth());
-    assertEquals("wrong image height", 10 * scale, image.getHeight());
+      var image = loadImage(getSvgIconPath("20x10"), ctx)
+      TestCase.assertNotNull(image)
+      TestCase.assertEquals("wrong image width", 20 * scale, image.width)
+      TestCase.assertEquals("wrong image height", 10 * scale, image.height)
 
-    /*
+      /*
      * Test "px" unit.
-     */
-    image = TestScaleHelper.loadImage(getSvgIconPath("20px10px"), ctx);
-    assertNotNull(image);
-    assertEquals("wrong image width", 20 * scale, image.getWidth());
-    assertEquals("wrong image height", 10 * scale, image.getHeight());
+     */image = loadImage(getSvgIconPath("20px10px"), ctx)
+      TestCase.assertNotNull(image)
+      TestCase.assertEquals("wrong image width", 20 * scale, image.width)
+      TestCase.assertEquals("wrong image height", 10 * scale, image.height)
 
-    /*
+      /*
      * Test default size.
-     */
-    image = TestScaleHelper.loadImage(getSvgIconPath("default"), ctx);
-    assertNotNull(image);
-    assertEquals("wrong image width", SVGLoader.ICON_DEFAULT_SIZE * scale, image.getWidth());
-    assertEquals("wrong image height", SVGLoader.ICON_DEFAULT_SIZE * scale, image.getHeight());
-  }
+     */image = loadImage(getSvgIconPath("default"), ctx)
+      TestCase.assertNotNull(image)
+      TestCase.assertEquals("wrong image width", SVGLoader.ICON_DEFAULT_SIZE * scale, image.width)
+      TestCase.assertEquals("wrong image height", SVGLoader.ICON_DEFAULT_SIZE * scale, image.height)
+    }
 
-  private static Path getSvgIconPath(String size) {
-    return Path.of(PlatformTestUtil.getPlatformTestDataPath() + "ui/myIcon_" + size + ".svg");
+    private fun getSvgIconPath(size: String): Path {
+      return Path.of(PlatformTestUtil.getPlatformTestDataPath() + "ui/myIcon_" + size + ".svg")
+    }
   }
 }
