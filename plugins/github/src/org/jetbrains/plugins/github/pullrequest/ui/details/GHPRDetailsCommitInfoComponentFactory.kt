50029217d8f7f97f276a610c4a0c45a45b9e8bc2
Pavel Gromov <pavel.gromov@jetbrains.com>
2023-03-09 19:46:40 +0100

    [github] Use inactive text color for author and date in a commit info panel
    (cherry picked from commit 13b67e13665a23d79488eef0fda8ae2ec5d7c023)
    
    IJ-CR-105184
    
    GitOrigin-RevId: b4c829a491233d096073ef33ea40e0de122af8d6

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/details/GHPRDetailsCommitInfoComponentFactory.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/details/GHPRDetailsCommitInfoComponentFactory.kt
@@ -4,7 +4,11 @@ package org.jetbrains.plugins.github.pullrequest.ui.details
 import com.intellij.collaboration.ui.VerticalListPanel
 import com.intellij.collaboration.ui.util.bindText
 import com.intellij.collaboration.ui.util.bindVisibility
+import com.intellij.openapi.util.text.HtmlBuilder
+import com.intellij.openapi.util.text.HtmlChunk
+import com.intellij.ui.ColorUtil
 import com.intellij.util.text.DateFormatUtil
+import com.intellij.util.ui.NamedColorUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.map
 import org.jetbrains.plugins.github.pullrequest.ui.details.model.GHPRCommitsViewModel
@@ -25,7 +29,10 @@ internal object GHPRDetailsCommitInfoComponentFactory {
       bindText(scope, commitsVm.selectedCommit.map { commit ->
         commit ?: return@map ""
         val author = commit.author?.user ?: commitsVm.ghostUser
-        "${author.getPresentableName()} ${DateFormatUtil.formatPrettyDateTime(commit.committedDate)}"
+        HtmlBuilder()
+          .append(HtmlChunk.text("${author.getPresentableName()} ${DateFormatUtil.formatPrettyDateTime(commit.committedDate)}"))
+          .wrapWith(HtmlChunk.font(ColorUtil.toHex(NamedColorUtil.getInactiveTextColor())))
+          .toString()
       })
     }
 
