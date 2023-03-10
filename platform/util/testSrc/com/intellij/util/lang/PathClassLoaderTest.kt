71106d06f37cc8c7e1c0d0dc79dd9164383b2391
Matthew Gharrity <gharrma@google.com>
2023-03-10 17:17:47 +0100

    IDEA-314175 PathClassLoader disallows relative jar paths on the Java classpath
    (cherry picked from commit c4b1c6006714e6c9c9a1182de2ceb45fcef808fe)
    
    Change-Id: Ic4b1c6006714e6c9c9a1182de2ceb45fcef808fe

--- platform/util/testSrc/com/intellij/util/lang/PathClassLoaderTest.kt
+++ platform/util/testSrc/com/intellij/util/lang/PathClassLoaderTest.kt
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.net.URL
 import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.invariantSeparatorsPathString
 
 class PathClassLoaderTest {
   @Test
@@ -23,5 +25,18 @@ class PathClassLoaderTest {
     val resource = classPath.findResource("help/help.html")
     assertThat(resource).isNotNull()
     assertThat(URL(resource!!.url, "screenshot.png").content).isEqualTo(expectedData)
+  }
+
+  @Test
+  fun `relative jar path`(@TempDir dir: Path) {
+    // Regression test for IDEA-314175.
+    val jarAbsolutePath = dir.resolve("lib.jar")
+    Compressor.Zip(jarAbsolutePath.toFile()).use { compressor ->
+      compressor.addFile("resource.txt", "contents".encodeToByteArray())
+    }
+    val jarRelativePath = Paths.get("").toAbsolutePath().relativize(jarAbsolutePath)
+    val classPath = ClassPath(listOf(jarRelativePath), UrlClassLoader.build(), PathClassLoader.RESOURCE_FILE_FACTORY, true)
+    val resource = checkNotNull(classPath.findResource("resource.txt"))
+    assertThat(resource.url.toString()).isEqualTo("jar:file:${jarAbsolutePath.invariantSeparatorsPathString}!/resource.txt")
   }
 }
