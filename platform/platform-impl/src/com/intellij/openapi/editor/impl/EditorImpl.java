53b6cf1d72029679d627c78faa24d07edae07f57
Egor Ushakov <egor.ushakov@jetbrains.com>
2023-03-10 20:08:38 +0100

    IDEA-314363 New UI: Removing breakpoint by dragging to the editor doesn't work, selects text instead
    (cherry picked from commit a2d0ac9cbc44939fc0f2728d51ff1f0611583708)
    
    IJ-CR-104566
    
    GitOrigin-RevId: 4219c5079409be010e54f80e15f05d0be786bff7

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
@@ -2595,7 +2595,7 @@ public final class EditorImpl extends UserDataHolderBase implements EditorEx, Hi
           }
           else {
             if (caretShift != 0) {
-              if (myMousePressedEvent != null) {
+              if (myMousePressedEvent != null && myGutterComponent.getGutterRenderer(e.getPoint()) == null) {
                 if (mySettings.isDndEnabled()) {
                   if (!myDragStarted) {
                     if (ApplicationManager.getApplication().isUnitTestMode()) {
