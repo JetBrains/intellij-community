b2bfe360ffc52030696c7556f0437c3baede322d
Anna Kozlova <anna.kozlova@jetbrains.com>
2023-03-13 10:58:24 +0100

    [kotlin] copy part of package part provider to declaration provider
    required to get rid of package part provider in IDE completely
    
    GitOrigin-RevId: 2c1a8787b8f96642e480c4be1ace72d1ee9d9f26

--- plugins/kotlin/base/analysis-api-providers/src/org/jetbrains/kotlin/idea/base/analysisApiProviders/IdeKotlinDeclarationProviderFactory.kt
+++ plugins/kotlin/base/analysis-api-providers/src/org/jetbrains/kotlin/idea/base/analysisApiProviders/IdeKotlinDeclarationProviderFactory.kt
@@ -9,11 +9,13 @@ import com.intellij.psi.PsiManager
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.StubIndex
 import com.intellij.psi.stubs.StubIndexKey
+import com.intellij.util.indexing.FileBasedIndex
 import org.jetbrains.kotlin.analysis.providers.KotlinDeclarationProvider
 import org.jetbrains.kotlin.analysis.providers.KotlinDeclarationProviderFactory
 import org.jetbrains.kotlin.idea.base.indices.names.KotlinTopLevelCallableByPackageShortNameIndex
 import org.jetbrains.kotlin.idea.base.indices.names.KotlinTopLevelClassLikeDeclarationByPackageShortNameIndex
 import org.jetbrains.kotlin.idea.stubindex.*
+import org.jetbrains.kotlin.idea.vfilefinder.KotlinModuleMappingIndex
 import org.jetbrains.kotlin.name.CallableId
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
@@ -83,6 +85,13 @@ private class IdeKotlinDeclarationProvider(
 
     override fun findFilesForScript(scriptFqName: FqName): Collection<KtScript> {
         return KotlinScriptFqnIndex[scriptFqName.asString(), project, scope]
+    }
+
+    /**
+     * [org.jetbrains.kotlin.idea.caches.resolve.IDEPackagePartProvider.computePackageSetWithNonClassDeclarations]
+     */
+    override fun computePackageSetWithTopLevelCallableDeclarations(): Set<String> = buildSet {
+        FileBasedIndex.getInstance().processAllKeys(KotlinModuleMappingIndex.NAME, { name -> add(name); true }, scope, null)
     }
 
     override fun findFilesForFacade(facadeFqName: FqName): Collection<KtFile> {
