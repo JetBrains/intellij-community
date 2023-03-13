d3a1297bf1a2df114d4d015e1bd45639bff40bc2
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2023-03-13 08:06:47 +0400

    [devKit] LightServiceMustBeFinalInspectionTest: use properties instead of not hard-coded string literals
    IJ-CR-102194
    
    GitOrigin-RevId: 23b6a2581fd4dbfa61a405706525c022ad9d82a8

--- plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/inspections/LightServiceMustBeFinalInspectionTest.kt
+++ plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/inspections/LightServiceMustBeFinalInspectionTest.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.inspections
 
+import com.intellij.codeInsight.daemon.QuickFixBundle
 import com.intellij.testFramework.TestDataPath
 import org.jetbrains.idea.devkit.DevkitJavaTestsUtil
 import org.jetbrains.idea.devkit.inspections.quickfix.LightServiceMustBeFinalInspectionTestBase
@@ -8,11 +9,12 @@ import org.jetbrains.idea.devkit.inspections.quickfix.LightServiceMustBeFinalIns
 @TestDataPath("\$CONTENT_ROOT/testData/inspections/lightServiceMustBeFinal")
 class LightServiceMustBeFinalInspectionTest : LightServiceMustBeFinalInspectionTestBase() {
 
+  private val MAKE_FINAL_FIX_NAME = QuickFixBundle.message("add.modifier.fix", "MyService", "final")
   override fun getBasePath() = DevkitJavaTestsUtil.TESTDATA_PATH + "inspections/lightServiceMustBeFinal/"
 
   override fun getFileExtension() = "java"
 
   fun testMakeFinal() {
-    doTest("Make 'MyService' final")
+    doTest(MAKE_FINAL_FIX_NAME)
   }
 }
