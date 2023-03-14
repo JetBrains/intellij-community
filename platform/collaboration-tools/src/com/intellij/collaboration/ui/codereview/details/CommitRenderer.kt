871c51b2b415ce8391e00f6fdbea35c7e810606e
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2023-03-14 17:55:16 +0100

    [collab] CommitRenderer: adjust gaps and simplify layout
    (cherry picked from commit 439b90e3d47480a0292a3881f7db770b24cba026)
    
    IJ-MR-104863
    
    GitOrigin-RevId: a3070885ec5c852ba732a0e55c9b06e6d65815e0

--- platform/collaboration-tools/src/com/intellij/collaboration/ui/codereview/details/CommitRenderer.kt
+++ platform/collaboration-tools/src/com/intellij/collaboration/ui/codereview/details/CommitRenderer.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.collaboration.ui.codereview.details
 
+import com.intellij.collaboration.ui.CollaborationToolsUIUtil
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.ui.popup.util.RoundedCellRenderer
 import com.intellij.openapi.util.NlsSafe
@@ -14,26 +15,28 @@ import java.util.*
 import javax.swing.JLabel
 import javax.swing.JList
 import javax.swing.ListCellRenderer
-import javax.swing.SwingConstants
 
 class CommitRenderer<T> private constructor(private val presenter: (T?) -> SelectableWrapper<CommitPresenter>) : ListCellRenderer<T> {
-  private val iconLabel = JLabel().apply {
-    horizontalAlignment = SwingConstants.CENTER
-    verticalAlignment = SwingConstants.TOP
-    border = JBUI.Borders.empty(12, BORDER_OFFSET, 0, ICON_TEXT_OFFSET)
+  private val allCommitsMessage: JLabel = JLabel().apply {
+    border = JBUI.Borders.empty(ALL_COMMITS_TOP_BOTTOM, LEFT_RIGHT_GAP)
+    iconTextGap = JBUIScale.scale(ICON_TEXT_OFFSET)
   }
-  private val allCommitsMessage = JLabel().apply {
-    border = JBUI.Borders.empty(BORDER_OFFSET, 0)
+
+  private val commitMessage: JLabel = JLabel().apply {
+    iconTextGap = JBUIScale.scale(ICON_TEXT_OFFSET)
   }
-  private val commitMessage = JLabel().apply {
-    border = JBUI.Borders.empty(BORDER_OFFSET, 0, 0, BORDER_OFFSET)
-  }
-  private val authorAndDate = JLabel().apply {
-    border = JBUI.Borders.empty(0, 0, BORDER_OFFSET, BORDER_OFFSET)
+
+  private val authorAndDate: JLabel = JLabel().apply {
+    border = JBUI.Borders.emptyTop(MESSAGE_INFO_VERTICAL_GAP)
+    iconTextGap = JBUIScale.scale(ICON_TEXT_OFFSET)
     foreground = NamedColorUtil.getInactiveTextColor()
   }
-  private val textPanel = BorderLayoutPanel()
-  private val commitPanel = BorderLayoutPanel()
+
+  private val textPanel: BorderLayoutPanel = BorderLayoutPanel().apply {
+    border = JBUI.Borders.empty(COMMIT_TOP_BOTTOM, LEFT_RIGHT_GAP)
+  }
+
+  private val commitPanel: BorderLayoutPanel = BorderLayoutPanel()
 
   override fun getListCellRendererComponent(list: JList<out T>,
                                             value: T?,
@@ -44,21 +47,25 @@ class CommitRenderer<T> private constructor(private val presenter: (T?) -> Selec
 
     val presentation = presenter(value)
     val commit = presentation.value
-    iconLabel.icon = if (presentation.isSelected) AllIcons.Actions.Checked_selected else emptyIcon
+
+    commitMessage.icon = if (presentation.isSelected) AllIcons.Actions.Checked_selected else emptyIcon
+    allCommitsMessage.icon = if (presentation.isSelected) AllIcons.Actions.Checked_selected else emptyIcon
+    authorAndDate.icon = emptyIcon
 
     when (commit) {
       is CommitPresenter.SingleCommit -> {
         commitMessage.text = commit.title
         authorAndDate.text = "${commit.author}, ${DateFormatUtil.formatPrettyDateTime(commit.committedDate)}"
         textPanel.addToCenter(commitMessage).addToBottom(authorAndDate)
+        commitPanel.addToCenter(textPanel)
       }
       is CommitPresenter.AllCommits -> {
         allCommitsMessage.text = commit.title
-        textPanel.addToCenter(allCommitsMessage)
+        commitPanel.addToCenter(allCommitsMessage)
       }
     }
 
-    return commitPanel.addToLeft(iconLabel).addToCenter(textPanel).apply {
+    return commitPanel.apply {
       UIUtil.setBackgroundRecursively(this, ListUiUtil.WithTallRow.background(list, cellSelected, list.hasFocus()))
     }
   }
@@ -69,11 +76,15 @@ class CommitRenderer<T> private constructor(private val presenter: (T?) -> Selec
   }
 
   companion object {
-    private const val BORDER_OFFSET = 8
-    private const val ICON_TEXT_OFFSET = 6
-    private const val EMPTY_ICON_SIZE = 12
+    private const val ALL_COMMITS_TOP_BOTTOM = 4
+    private const val COMMIT_TOP_BOTTOM = 8
+    private val LEFT_RIGHT_GAP: Int
+      get() = CollaborationToolsUIUtil.getSize(oldUI = 8, newUI = 0) // in case of the newUI gap handled by SelectablePanel
 
-    private val emptyIcon: EmptyIcon = JBUIScale.scaleIcon(EmptyIcon.create(EMPTY_ICON_SIZE))
+    private const val ICON_TEXT_OFFSET = 6
+    private const val MESSAGE_INFO_VERTICAL_GAP = 4
+
+    private val emptyIcon: EmptyIcon = JBUIScale.scaleIcon(EmptyIcon.create(AllIcons.Actions.Checked_selected))
 
     @JvmStatic
     fun <T> createCommitRenderer(presenter: (T?) -> SelectableWrapper<CommitPresenter>): ListCellRenderer<T> {
