13a9cbc8144b535e0a7751060286698f97d9970b
Alexey Kudravtsev <cdr@intellij.com>
2023-03-14 11:59:53 +0100

    mark some functional interfaces obsolete to avoid confusion with existing standard JDK ones
    GitOrigin-RevId: 7c52b039a28405b0840f1a811a4278bd3a9d4831

--- platform/util/src/com/intellij/util/Producer.java
+++ platform/util/src/com/intellij/util/Producer.java
@@ -1,10 +1,13 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util;
 
+import org.jetbrains.annotations.ApiStatus;
+
 /**
  * Deprecated. Use {@link java.util.function.Supplier} instead
  */
 @FunctionalInterface
+@ApiStatus.Obsolete
 public interface Producer<T> {
 
   T produce();
