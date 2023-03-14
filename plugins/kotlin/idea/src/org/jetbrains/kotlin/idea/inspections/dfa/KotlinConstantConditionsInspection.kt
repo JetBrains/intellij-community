f35a4b37934e000a013beded9bcfbfb0ed9ea21f
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-03-14 13:33:19 +0100

    [kotlin-dfa] EA-796352 - T: KotlinConstantConditionsInspection.access$processDataflowAnalysis
    GitOrigin-RevId: 7ec4a417aa798924574966f0604d1b091b6350c8

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/dfa/KotlinConstantConditionsInspection.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/dfa/KotlinConstantConditionsInspection.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.inspections.dfa
 
 import com.intellij.codeInsight.PsiEquivalenceUtil
@@ -217,6 +217,7 @@ class KotlinConstantConditionsInspection : AbstractKotlinInspection() {
                                     .filterIsInstance<KtWhenEntry>()
                                     .filterNot { entry -> entry.isElse }
                                     .flatMap { entry -> entry.conditions.asSequence() }
+                                    .filter { cond -> cond.text.isNotEmpty() }
                                     .forEach { cond -> holder.registerProblem(cond, message) }
                             }
                         }
