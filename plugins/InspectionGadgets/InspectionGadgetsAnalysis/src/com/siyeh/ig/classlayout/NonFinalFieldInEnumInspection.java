ffc7e4630a582a9157eb685ec9c44dc221b495b0
Bas Leijdekkers <basleijdekkers@gmail.com>
2023-03-15 13:30:29 +0100

    Java: "Non-final field in 'enum'" tweaks
    GitOrigin-RevId: 721f2edf47f5c3d671a1cdf89812833dcf59ca89

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/classlayout/NonFinalFieldInEnumInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/classlayout/NonFinalFieldInEnumInspection.java
@@ -1,22 +1,7 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.siyeh.ig.classlayout;
 
 import com.intellij.codeInspection.options.OptPane;
-import com.intellij.codeInspection.ui.SingleCheckboxOptionsPanel;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiModifier;
@@ -29,9 +14,8 @@ import com.siyeh.ig.psiutils.FinalUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
-
-import static com.intellij.codeInspection.options.OptPane.*;
+import static com.intellij.codeInspection.options.OptPane.checkbox;
+import static com.intellij.codeInspection.options.OptPane.pane;
 
 /**
  * @author Bas Leijdekkers
@@ -70,11 +54,11 @@ public class NonFinalFieldInEnumInspection extends BaseInspection {
     @Override
     public void visitField(@NotNull PsiField field) {
       super.visitField(field);
-      final PsiClass containingClass = field.getContainingClass();
-      if (containingClass == null || !containingClass.isEnum()) {
+      if (field.hasModifierProperty(PsiModifier.FINAL)) {
         return;
       }
-      if (field.hasModifierProperty(PsiModifier.FINAL)) {
+      final PsiClass containingClass = field.getContainingClass();
+      if (containingClass == null || !containingClass.isEnum()) {
         return;
       }
       if (onlyWarnWhenQuickFix && !FinalUtils.canBeFinal(field)) {
