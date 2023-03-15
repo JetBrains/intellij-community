a4222cddf0ce98ec98736742363f731ae79e1e1e
Toshiaki Kameyama <kmymtsak@gmail.com>
2023-03-15 10:58:20 +0900

    [kotlin] Fix false positive "Unused symbol" with serialVersionUID property of objects
    ^KTIJ-24888 Fixed

---
+++ plugins/kotlin/idea/tests/testData/inspections/unusedSymbol/property/serialVersionUidInObjectDeclaration.kt
@@ -0,0 +1,16 @@
+// FULL_JDK
+
+import java.io.Serializable
+
+object Foo : Serializable {
+    private const val serialVersionUID: Long = 0L
+}
+
+object Bar : Serializable {
+    @JvmStatic
+    private val serialVersionUID: Long = 0L
+}
+
+fun main() {
+    Foo
+    Bar
