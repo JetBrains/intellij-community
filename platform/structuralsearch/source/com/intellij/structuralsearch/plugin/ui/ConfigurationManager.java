1e1c893f1cbd3a34b829a9306f636ab6c029a0cc
Bas Leijdekkers <basleijdekkers@gmail.com>
2023-03-16 11:18:41 +0100

    SSR: remove unused but dangerous method
    GitOrigin-RevId: f23e9828d5b700c5973063b9592df043659d6e1d

--- platform/structuralsearch/source/com/intellij/structuralsearch/plugin/ui/ConfigurationManager.java
+++ platform/structuralsearch/source/com/intellij/structuralsearch/plugin/ui/ConfigurationManager.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.structuralsearch.plugin.ui;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -315,10 +315,6 @@ public class ConfigurationManager implements PersistentStateComponent<Element> {
 
     public static ConfigurationManagerState getInstance() {
       return ApplicationManager.getApplication().getService(ConfigurationManagerState.class);
-    }
-
-    public static ConfigurationManagerState getInstance(Project project) {
-      return project.getService(ConfigurationManagerState.class);
     }
 
     public void add(Configuration configuration) {
