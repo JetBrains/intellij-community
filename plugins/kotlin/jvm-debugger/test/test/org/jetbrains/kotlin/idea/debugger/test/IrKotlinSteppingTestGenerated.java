15dfe0dee49333de4d791ec32eafd7b9b27be164
Nikita Nazarov <nikitanazarov@google.com>
2023-03-16 15:00:18 +0100

    [kotlin] fix smart stepping into lambdas inlined multiple times
    The smart stepping machinery is designed in a way that it needs to put a breakpoint in a place of interest, and then it is trying to step until the breakpoint is hit. However, when trying to smart step into inline lambda that is inside an inline function that is inlined several times, it may happen that multiple locations in a class file are sufficient candidates for the smart stepping breakpoint. In that case the breakpoint will be placed only on the first sufficient location, others will be ignored. That's why when trying to step into inline lambda in its first inlined call site, smart stepping works, but it never succeeds after that. This commit adds a workaround for this issue, it makes smart stepping machinery stop if we think that we are already inside a lambda of interest PSI wise.
    
    ^KTIJ-25635 Fixed
    
    GitOrigin-RevId: b85594c337a47976d825c1731037c264c3f2171d

--- plugins/kotlin/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/IrKotlinSteppingTestGenerated.java
+++ plugins/kotlin/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/IrKotlinSteppingTestGenerated.java
@@ -1450,6 +1450,11 @@ public abstract class IrKotlinSteppingTestGenerated extends AbstractIrKotlinStep
             runTest("testData/stepping/custom/smartStepIntoMethodReference.kt");
         }
 
+        @TestMetadata("smartStepIntoMultiplyInlinedLambdas.kt")
+        public void testSmartStepIntoMultiplyInlinedLambdas() throws Exception {
+            runTest("testData/stepping/custom/smartStepIntoMultiplyInlinedLambdas.kt");
+        }
+
         @TestMetadata("smartStepIntoNullSafeFunctionCalls.kt")
         public void testSmartStepIntoNullSafeFunctionCalls() throws Exception {
             runTest("testData/stepping/custom/smartStepIntoNullSafeFunctionCalls.kt");
--- plugins/kotlin/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/KotlinSteppingTestGenerated.java
+++ plugins/kotlin/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/KotlinSteppingTestGenerated.java
@@ -1451,6 +1451,11 @@ public abstract class KotlinSteppingTestGenerated extends AbstractKotlinStepping
             runTest("testData/stepping/custom/smartStepIntoMethodReference.kt");
         }
 
+        @TestMetadata("smartStepIntoMultiplyInlinedLambdas.kt")
+        public void testSmartStepIntoMultiplyInlinedLambdas() throws Exception {
+            runTest("testData/stepping/custom/smartStepIntoMultiplyInlinedLambdas.kt");
+        }
+
         @TestMetadata("smartStepIntoNullSafeFunctionCalls.kt")
         public void testSmartStepIntoNullSafeFunctionCalls() throws Exception {
             runTest("testData/stepping/custom/smartStepIntoNullSafeFunctionCalls.kt");
