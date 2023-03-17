956b4b61e95e98264635b6b96a38220557c2d0e5
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2023-03-17 15:00:42 +0100

    [kt, tests] Move new MPP tests to IJ community
    ^KT-56810 Fixed
    
    (cherry picked from commit cfdfa58159316b4b56c5ff33f6f35d90e5b73ffd)
    
    GitOrigin-RevId: b1d042066def0c62e08a4c4cbfd4fbf5cf0461c7

---
+++ plugins/kotlin/gradle/multiplatform-tests/test/org/jetbrains/kotlin/gradle/idea/importing/newTests/KotlinMppMiscCasesImportingTests.kt
@@ -0,0 +1,182 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.gradle.idea.importing.newTests
+
+import com.intellij.lang.annotation.HighlightSeverity
+import org.jetbrains.kotlin.config.KotlinFacetSettings
+import org.jetbrains.kotlin.gradle.newTests.AbstractKotlinMppGradleImportingTest
+import org.jetbrains.kotlin.gradle.newTests.TestConfigurationDslScope
+import org.jetbrains.kotlin.gradle.newTests.testFeatures.checkers.contentRoots.ContentRootsChecker
+import org.jetbrains.kotlin.gradle.newTests.testFeatures.checkers.facets.KotlinFacetSettingsChecker
+import org.jetbrains.kotlin.gradle.newTests.testFeatures.checkers.highlighting.HighlightingChecker
+import org.jetbrains.kotlin.gradle.newTests.testFeatures.checkers.orderEntries.OrderEntriesChecker
+import org.jetbrains.kotlin.test.TestMetadata
+import org.junit.Test
+
+@TestMetadata("newMppTests/features/misc")
+class KotlinMppMiscCasesImportingTests : AbstractKotlinMppGradleImportingTest()  {
+    override fun TestConfigurationDslScope.defaultTestConfiguration() {
+        hideStdlib = true
+        hideKotlinTest = true
+        hideKotlinNativeDistribution = true
+
+        onlyFacetFields(KotlinFacetSettings::targetPlatform)
+
+        hideResourceRoots = true
+    }
+
+    @Test
+    fun testDiamond() {
+        doTest {
+            onlyCheckers(OrderEntriesChecker, KotlinFacetSettingsChecker, ContentRootsChecker)
+        }
+    }
+
+    @Test
+    fun testOrphanSourceSet() {
+        doTest {
+            onlyCheckers(KotlinFacetSettingsChecker)
+        }
+    }
+
+    @Test
+    fun testDependencyOnStdlibFromPlatformSourceSets() {
+        doTest {
+            hideStdlib = false
+            onlyCheckers(OrderEntriesChecker)
+        }
+    }
+
+    @Test
+    fun testDependencyOnKotlinTestFromPlatformSourceSets() {
+        doTest {
+            hideKotlinTest = false
+            onlyCheckers(OrderEntriesChecker)
+        }
+    }
+
+    @Test
+    fun testMppLibAndHmppConsumer() {
+        doTest {
+            onlyCheckers(OrderEntriesChecker)
+
+            publish("lib")
+            excludeDependencies(""".*consumer.*""")
+            excludeModules(""".*lib.*""")
+        }
+    }
+
+    @Test
+    fun testHmppLibAndMppConsumer() {
+        doTest {
+            onlyCheckers(OrderEntriesChecker)
+
+            publish("lib")
+            excludeDependencies(""".*consumer.*""")
+            excludeModules(""".*lib.*""")
+        }
+    }
+
+    @Test
+    fun testUnresolvedDependency() {
+        doTest {
+            onlyCheckers(OrderEntriesChecker)
+        }
+    }
+
+    @Test
+    fun testBinaryDependenciesOrderIsStable() {
+        doTest {
+            onlyCheckers(OrderEntriesChecker)
+
+            publish("lib1")
+            publish("lib2")
+            excludeDependencies(".*consumer.*")
+            excludeModules(".*lib.*")
+            sortDependencies = false
+        }
+    }
+
+    @Test
+    fun testSourceDependenciesOrderIsStable() {
+        doTest {
+            onlyCheckers(OrderEntriesChecker)
+
+            excludeDependencies(".*consumer.*")
+            excludeModules(".*lib.*")
+            sortDependencies = false
+        }
+    }
+
+    @Test
+    fun testMismatchedAttributesDependencyBinary() {
+        // NB: Variant-mismatch error is printed verbatim in stderr
+        doTest {
+            onlyCheckers(OrderEntriesChecker)
+
+            publish("producer")
+            excludeDependencies(".*consumer.*")
+            excludeModules(".*producer.*")
+
+            hideHighlightsBelow = HighlightSeverity.ERROR
+        }
+    }
+
+    @Test
+    fun testMismatchedAttributesDependencySource() {
+        // NB: Variant-mismatch error is printed verbatim in stderr
+        doTest {
+            onlyCheckers(OrderEntriesChecker)
+
+            excludeDependencies(".*consumer.*")
+            excludeModules(".*producer.*")
+
+            hideHighlightsBelow = HighlightSeverity.ERROR
+        }
+    }
+
+    @Test
+    fun testKmmAppWithIntermediateExternalDependencies() {
+        doTest {
+            onlyCheckers(OrderEntriesChecker)
+
+            // not concerned with inter-project dependencies
+            excludeDependencies(".*rootProject.*")
+
+            hideLineMarkers = true // too much noise
+        }
+    }
+
+    @Test
+    fun testTransitiveKmmLibraryThroughJava() {
+        doTest {
+            onlyCheckers(OrderEntriesChecker)
+
+            onlyDependencies(from = ".*m3-kt.*", to = ".*m1-kt-mpp.*")
+            hideLineMarkers = true
+        }
+    }
+
+    @Test
+    fun testNativeUnsupportedPlatform() {
+        doTest {
+            onlyCheckers(OrderEntriesChecker)
+
+            allowFilesNotUnderContentRoot = true
+            hideStdlib = false
+            hideKotlinNativeDistribution = false
+            excludeModules(".*(jvmMain|jvmTest).*") // jvm-target is just to make commonMain not common-shared
+        }
+    }
+
+    // This test checks only stable and released part of associate
+    // configurations, i.e. the support for custom associated compilations
+    // in Kotlin/JVM + support for default associated compilations in MPP
+    @Test
+    fun testAssociateCompilationIntegrationTest() {
+        doTest {
+            onlyCheckers(HighlightingChecker, KotlinFacetSettingsChecker)
+            onlyFacetFields(KotlinFacetSettings::additionalVisibleModuleNames)
+            hideLineMarkers = true
+        }
+    }
+}
