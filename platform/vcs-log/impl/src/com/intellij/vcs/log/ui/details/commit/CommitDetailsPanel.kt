6f3b700c1d99eaa193b157a7ec85a678f1568845
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2023-03-17 11:06:54 +0100

    IDEA-315582 Rid off unnecessary border updates in CommitDetailsPanel
    GitOrigin-RevId: 96c2ad14230eb98ee568621011aa3b96ffe412e3

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/details/commit/CommitDetailsPanel.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/details/commit/CommitDetailsPanel.kt
@@ -71,20 +71,8 @@ class CommitDetailsPanel @JvmOverloads constructor(navigate: (CommitId) -> Unit 
       layout = MigLayout(LC().gridGap("0", "0").insets("0").fill().flowY())
       isOpaque = false
 
-      val metadataPanel = object: BorderLayoutPanel() {
-        init {
-          updateBorder()
-        }
-
-        override fun updateUI() {
-          super.updateUI()
-          updateBorder()
-        }
-
-        private fun updateBorder() {
-          border = JBUI.Borders.empty(INTERNAL_BORDER, SIDE_BORDER, INTERNAL_BORDER, 0)
-        }
-      }.apply {
+      val metadataPanel = BorderLayoutPanel().apply {
+        border = JBUI.Borders.empty(INTERNAL_BORDER, SIDE_BORDER, INTERNAL_BORDER, 0)
         isOpaque = false
         addToLeft(rootPanel)
         addToCenter(hashAndAuthorPanel)
@@ -200,14 +188,6 @@ private class CommitMessagePanel(private val navigate: (CommitId) -> Unit) : Htm
   }
 
   init {
-    updateBorder()
-  }
-
-  override fun updateUI() {
-    super.updateUI()
-    updateBorder()
-  }
-  private fun updateBorder() {
     border = JBUI.Borders.empty(CommitDetailsPanel.EXTERNAL_BORDER, CommitDetailsPanel.SIDE_BORDER, CommitDetailsPanel.INTERNAL_BORDER, 0)
   }
 
@@ -231,17 +211,8 @@ private class ContainingBranchesPanel : HtmlPanel() {
   private var expanded = false
 
   init {
-    updateBorder()
-    isVisible = false
-  }
-
-  override fun updateUI() {
-    super.updateUI()
-    updateBorder()
-  }
-
-  private fun updateBorder() {
     border = JBUI.Borders.empty(0, CommitDetailsPanel.SIDE_BORDER, CommitDetailsPanel.EXTERNAL_BORDER, 0)
+    isVisible = false
   }
 
   override fun setBounds(x: Int, y: Int, w: Int, h: Int) {
