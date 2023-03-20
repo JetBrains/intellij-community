12406bf1590416345f8608974292681558bb8948
Alexey Kudravtsev <cdr@intellij.com>
2023-03-20 17:31:28 +0100

    elaborate javadoc
    GitOrigin-RevId: e93c0f9245c201683c46ca5ac40c7d713e40ccc0

--- platform/core-impl/src/com/intellij/openapi/editor/impl/SweepProcessor.java
+++ platform/core-impl/src/com/intellij/openapi/editor/impl/SweepProcessor.java
@@ -29,9 +29,14 @@ public interface SweepProcessor<T> {
   boolean process(int offset, @NotNull T interval, boolean atStart, @NotNull Collection<? extends T> overlappingIntervals);
 
   /**
-   * Process all intervals from generator in their "start offset - then end offset" order.
-   * For each interval call sweepProcessor and pass this interval with its current endpoint (start or end) and current overlapping intervals which this endpoint stabs.
-   * E.g. for (0,4), (2,5) intervals call sweepProcessor with (0, empty), (2, 0-4), (4, 2-5), (5, empty)
+   * Process all intervals from the {@code generator} in their "start offset; then end offset" order.
+   * For each interval call {@code sweepProcessor} and pass this interval, its current endpoint (start or end), and current overlapping intervals which this endpoint stabs.
+   * E.g. for (0,4), (2,5) intervals, this method will call {@code sweepProcessor} with:<pre>
+   *   (offset=0, atStart=true, overlapping=empty),
+   *   (offset=2, atStart=true, overlapping=(0-4)),
+   *   (offset=4, atStart=false, overlapping=(2-5),
+   *   (offset=5, atStart=false, overlapping=empty)
+   * </pre>
    */
   static <T extends Segment> boolean sweep(@NotNull Generator<? extends T> generator, @NotNull final SweepProcessor<T> sweepProcessor) {
     Queue<T> ends = new PriorityQueue<>(5, Comparator.comparingInt(Segment::getEndOffset));
