ce888296dceef6df0f6e1c2df3ef6531c30a1418
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-03-20 20:53:40 +0100

    IDEA-315100 CustomMaven36ArtifactDescriptorReader (turned on)
    GitOrigin-RevId: 78df3284c82f2789bf6990dbe0f1bebe98ce1d07

--- plugins/maven/maven36-server-impl/src/org/jetbrains/idea/maven/server/Maven36ServerEmbedderImpl.java
+++ plugins/maven/maven36-server-impl/src/org/jetbrains/idea/maven/server/Maven36ServerEmbedderImpl.java
@@ -8,12 +8,14 @@ import org.eclipse.aether.impl.ArtifactResolver;
 import org.eclipse.aether.internal.impl.DefaultRepositorySystem;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.maven.model.MavenWorkspaceMap;
+import org.jetbrains.idea.maven.server.embedder.CustomMaven36ArtifactDescriptorReader;
 import org.jetbrains.idea.maven.server.embedder.CustomMaven36ArtifactResolver;
 
 import java.rmi.RemoteException;
 
 public class Maven36ServerEmbedderImpl extends Maven3XServerEmbedder {
   private CustomMaven36ArtifactResolver enhancedArtifactResolver;
+  private CustomMaven36ArtifactDescriptorReader enhancedArtifactDescriptorReader;
 
   public Maven36ServerEmbedderImpl(MavenEmbedderSettings settings) throws RemoteException {
     super(settings);
@@ -24,9 +26,14 @@ public class Maven36ServerEmbedderImpl extends Maven3XServerEmbedder {
       ArtifactResolver defaultArtifactResolver = getComponent(ArtifactResolver.class);
       enhancedArtifactResolver = new CustomMaven36ArtifactResolver(defaultArtifactResolver);
 
+      ArtifactDescriptorReader defaultArtifactDescriptorReader = getComponent(ArtifactDescriptorReader.class);
+      enhancedArtifactDescriptorReader = new CustomMaven36ArtifactDescriptorReader(defaultArtifactDescriptorReader);
+
       RepositorySystem repositorySystem = getComponent(RepositorySystem.class);
       if (repositorySystem instanceof DefaultRepositorySystem) {
-        ((DefaultRepositorySystem)repositorySystem).setArtifactResolver(enhancedArtifactResolver);
+        DefaultRepositorySystem defaultRepositorySystem = (DefaultRepositorySystem)repositorySystem;
+        defaultRepositorySystem.setArtifactResolver(enhancedArtifactResolver);
+        defaultRepositorySystem.setArtifactDescriptorReader(enhancedArtifactDescriptorReader);
       }
 
       ArtifactDescriptorReader artifactDescriptorReader = getComponent(ArtifactDescriptorReader.class);
@@ -39,6 +46,9 @@ public class Maven36ServerEmbedderImpl extends Maven3XServerEmbedder {
   private synchronized void resetArtifactResolver() {
     if (null != enhancedArtifactResolver) {
       enhancedArtifactResolver.reset();
+    }
+    if (null != enhancedArtifactDescriptorReader) {
+      enhancedArtifactDescriptorReader.reset();
     }
   }
 
