71c0c3853748abc289ca438694ff511eac68ec67
Maria Filipanova <maria.filipanova@jetbrains.com>
2023-03-21 12:33:36 +0100

    GTW-4219: project Dependencies toolwindow in remdev
    GitOrigin-RevId: ba3e16d1b16894599d6e6357fe793cf075e151ca

--- platform/dependencies-toolwindow/src/com/intellij/dependencytoolwindow/DependencyToolWindowViewModelExtractor.kt
+++ platform/dependencies-toolwindow/src/com/intellij/dependencytoolwindow/DependencyToolWindowViewModelExtractor.kt
@@ -7,7 +7,7 @@ import com.intellij.ui.viewModel.extraction.ToolWindowViewModelExtractor
 
 class DependencyToolWindowViewModelExtractor : ToolWindowViewModelExtractor {
   override fun isApplicable(toolWindowId: String, clientId: ClientId?) =
-    toolWindowId == DEPENDENCIES_TOOL_WINDOW_ID
+    toolWindowId == DEPENDENCIES_TOOL_WINDOW_ID && clientId != ClientId.ownerId
 
   override fun getMode() = ToolWindowExtractorMode.UNSUPPORTED
 }
