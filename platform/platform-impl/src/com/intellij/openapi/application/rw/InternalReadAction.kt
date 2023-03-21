2066fd5077c090c59280c8b59b44e52d06b8d7b3
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-03-21 17:36:41 +0100

    clean up
    GitOrigin-RevId: d3eacfd96a87eb026a06458986281c9941f5d920

--- platform/platform-impl/src/com/intellij/openapi/application/rw/InternalReadAction.kt
+++ platform/platform-impl/src/com/intellij/openapi/application/rw/InternalReadAction.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.application.rw
 
 import com.intellij.openapi.application.ApplicationManager
@@ -23,7 +23,7 @@ internal class InternalReadAction<T>(
 
   suspend fun runReadAction(): T {
     return if (undispatched) {
-      ApplicationManager.getApplication().assertIsNonDispatchThread();
+      ApplicationManager.getApplication().assertIsNonDispatchThread()
       if (application.isReadAccessAllowed) {
         val unsatisfiedConstraint = findUnsatisfiedConstraint()
         check(unsatisfiedConstraint == null) {
