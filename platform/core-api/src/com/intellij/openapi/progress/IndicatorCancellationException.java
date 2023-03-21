2af9a600291220b92df9f2c3203c601f15771607
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-03-21 18:49:07 +0100

    IJPL-10 move cancellation classes to `intellij.platform.core`
    GitOrigin-RevId: 2745ed74f16d5574c5db0ed7d2550fa40b98eb9e

--- platform/core-api/src/com/intellij/openapi/progress/IndicatorCancellationException.java
+++ platform/core-api/src/com/intellij/openapi/progress/IndicatorCancellationException.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.progress;
 
 import org.jetbrains.annotations.ApiStatus.Internal;
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.concurrent.CancellationException;
 
 @Internal
-public final class IndicatorCancellationException extends CancellationException {
+final class IndicatorCancellationException extends CancellationException {
 
   IndicatorCancellationException(@NotNull ProcessCanceledException pce) {
     initCause(pce);
