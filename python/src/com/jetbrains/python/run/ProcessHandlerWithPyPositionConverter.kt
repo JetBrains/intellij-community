d9de3adb0b459efcfecc383fe841a284434c7fc3
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2023-03-21 10:20:52 +0100

    Use more general type for pathMapper field of ProcessHandlerWithPyPositionConverter class
    These changes are possible because `ProcessControlWithMappings.getFileMappings()` can be considered obsolete. The only notable usage of this method is located in Python coverage functionality related to an obsolete pre-targets implementation.
    
    GitOrigin-RevId: a010d06f3c302d84d9e41b76261aa3e09a95f8ff

--- python/src/com/jetbrains/python/run/ProcessHandlerWithPyPositionConverter.kt
+++ python/src/com/jetbrains/python/run/ProcessHandlerWithPyPositionConverter.kt
@@ -5,18 +5,22 @@ import com.intellij.execution.process.KillableColoredProcessHandler
 import com.intellij.remote.ProcessControlWithMappings
 import com.intellij.util.PathMapper
 import com.intellij.util.PathMappingSettings
-import com.jetbrains.python.debugger.*
+import com.jetbrains.python.debugger.PositionConverterProvider
+import com.jetbrains.python.debugger.PyDebugProcess
+import com.jetbrains.python.debugger.PyPositionConverter
+import com.jetbrains.python.debugger.createTargetedPositionConverter
+import com.jetbrains.python.remote.PyRemotePathMapper
 import java.nio.charset.Charset
 
 internal class ProcessHandlerWithPyPositionConverter(process: Process,
                                                      commandLine: String,
                                                      charset: Charset,
-                                                     private val pathMapper: PyTargetPathMapper)
+                                                     private val pathMapper: PyRemotePathMapper)
   : KillableColoredProcessHandler(process, commandLine, charset), PositionConverterProvider, ProcessControlWithMappings {
   override fun createPositionConverter(debugProcess: PyDebugProcess): PyPositionConverter =
     createTargetedPositionConverter(debugProcess, pathMapper)
 
   override fun getMappingSettings(): PathMapper = pathMapper
 
-  override fun getFileMappings(): MutableList<PathMappingSettings.PathMapping> = pathMapper.getFileMappings()
+  override fun getFileMappings(): List<PathMappingSettings.PathMapping> = emptyList()
 }
