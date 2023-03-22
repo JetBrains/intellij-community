129383842975e2225e8a0e4e03851e21744b47b7
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-03-22 14:09:57 +0100

    [java] JDK_20 is the default language level
    (cherry picked from commit 969cb6be151e0026b82ab637824c1d7a170ad947)
    
    IJ-CR-105213
    
    GitOrigin-RevId: 01430e3779d656a560692cdce1438f7f93d9613a

--- java/java-psi-api/src/com/intellij/pom/java/LanguageLevel.java
+++ java/java-psi-api/src/com/intellij/pom/java/LanguageLevel.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.pom.java;
 
 import com.intellij.core.JavaPsiBundle;
@@ -48,7 +48,7 @@ public enum LanguageLevel {
   /**
    * Should point to the last released JDK.
    */
-  public static final LanguageLevel HIGHEST = JDK_19;
+  public static final LanguageLevel HIGHEST = JDK_20;
 
   private final Supplier<@Nls String> myPresentableText;
   private final JavaVersion myVersion;
