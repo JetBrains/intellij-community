ce1306627046f14e5019cc377921cad1d3a972b5
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-03-22 11:57:47 +0100

    convert JreHiDpiUtil to kotlin
    GitOrigin-RevId: 22af8821b9c3a768de8cee61dcc74184c812a05c

--- platform/util/ui/src/com/intellij/ui/JreHiDpiUtil.kt
+++ platform/util/ui/src/com/intellij/ui/JreHiDpiUtil.kt
@@ -1,95 +1,88 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.ui;
+package com.intellij.ui
 
-import com.intellij.openapi.util.SystemInfo;
-import com.intellij.openapi.util.SystemInfoRt;
-import com.intellij.ui.scale.JBUIScale;
-import com.intellij.ui.scale.ScaleType;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.TestOnly;
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.util.SystemInfoRt
+import com.intellij.ui.scale.JBUIScale.isHiDPI
+import com.intellij.ui.scale.JBUIScale.sysScale
+import com.intellij.ui.scale.isHiDPI
+import org.jetbrains.annotations.TestOnly
+import java.awt.Graphics2D
+import java.awt.GraphicsConfiguration
+import java.awt.GraphicsEnvironment
+import java.util.concurrent.atomic.AtomicReference
 
-import java.awt.*;
-import java.lang.reflect.Method;
-import java.util.concurrent.atomic.AtomicReference;
-
-public final class JreHiDpiUtil {
-  private static final AtomicReference<Boolean> jreHiDPI = new AtomicReference<>();
+object JreHiDpiUtil {
+  private val jreHiDPI = AtomicReference<Boolean>()
 
   /**
    * Returns whether the JRE-managed HiDPI mode is enabled and the graphics configuration represents a HiDPI device.
-   * (analogue of {@link com.intellij.util.ui.UIUtil#isRetina(Graphics2D)} on macOS)
+   * (analogue of [com.intellij.util.ui.UIUtil.isRetina] on macOS)
    */
-  public static boolean isJreHiDPI(@Nullable GraphicsConfiguration gc) {
-    return isJreHiDPI(JBUIScale.sysScale(gc));
+  @JvmStatic
+  fun isJreHiDPI(gc: GraphicsConfiguration?): Boolean {
+    return isJreHiDPI(sysScale(gc))
   }
 
   /**
    * Returns whether the JRE-managed HiDPI mode is enabled and the specified scaling level corresponds to a HiDPI device.
-   * (analogue of {@link com.intellij.util.ui.UIUtil#isRetina(Graphics2D)} on macOS)
+   * (analogue of [com.intellij.util.ui.UIUtil.isRetina] on macOS)
    */
-  public static boolean isJreHiDPI(float sysScale) {
-    return isJreHiDPIEnabled() && JBUIScale.isHiDPI(sysScale);
+  @JvmStatic
+  fun isJreHiDPI(sysScale: Float): Boolean {
+    return isJreHiDPIEnabled() && isHiDPI(sysScale)
   }
 
   /**
    * Returns whether the JRE-managed HiDPI mode is enabled and the graphics represents a HiDPI device.
-   * (analogue of {@link com.intellij.util.ui.UIUtil#isRetina(Graphics2D)} on macOS)
+   * (Analogue of [com.intellij.util.ui.UIUtil.isRetina] on macOS)
    */
-  public static boolean isJreHiDPI(@Nullable Graphics2D g) {
-    return isJreHiDPIEnabled() && JBUIScale.isHiDPI(JBUIScale.sysScale(g));
+  @JvmStatic
+  fun isJreHiDPI(g: Graphics2D?): Boolean {
+    return isJreHiDPIEnabled() && isHiDPI(sysScale(g).toDouble())
   }
 
-  /**
-   * Returns whether the JRE-managed HiDPI mode is enabled.
-   * (True for macOS JDK >= 7.10 versions)
-   *
-   * @see ScaleType
-   */
-  public static boolean isJreHiDPIEnabled() {
+  @JvmStatic
+  fun isJreHiDPIEnabled(): Boolean {
     if (SystemInfoRt.isMac) {
-      return true;
+      return true
     }
-
-    Boolean value = jreHiDPI.get();
+    var value = jreHiDPI.get()
     if (value != null) {
-      return value;
+      return value
     }
-
-    synchronized (jreHiDPI) {
-      value = jreHiDPI.get();
+    synchronized(jreHiDPI) {
+      value = jreHiDPI.get()
       if (value != null) {
-        return value;
+        return value
       }
-
-      value = false;
-      if (Boolean.parseBoolean(System.getProperty("hidpi", "true")) && SystemInfo.isJetBrainsJvm) {
+      value = false
+      if (java.lang.Boolean.parseBoolean(System.getProperty("hidpi", "true")) && SystemInfo.isJetBrainsJvm) {
         try {
-          GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-          Class<?> sunGraphicsEnvironmentClass = Class.forName("sun.java2d.SunGraphicsEnvironment");
+          val ge = GraphicsEnvironment.getLocalGraphicsEnvironment()
+          val sunGraphicsEnvironmentClass = Class.forName("sun.java2d.SunGraphicsEnvironment")
           if (sunGraphicsEnvironmentClass.isInstance(ge)) {
             try {
-              Method method = sunGraphicsEnvironmentClass.getDeclaredMethod("isUIScaleEnabled");
-              method.setAccessible(true);
-              value = (Boolean)method.invoke(ge);
+              val method = sunGraphicsEnvironmentClass.getDeclaredMethod("isUIScaleEnabled")
+              method.isAccessible = true
+              value = method.invoke(ge) as Boolean
             }
-            catch (NoSuchMethodException e) {
-              value = false;
+            catch (e: NoSuchMethodException) {
+              value = false
             }
           }
         }
-        catch (Throwable ignore) {
+        catch (ignore: Throwable) {
         }
       }
-      jreHiDPI.set(value);
+      jreHiDPI.set(value)
     }
-    return value;
+    return value
   }
 
   @TestOnly
-  @NotNull
-  public static AtomicReference<Boolean> test_jreHiDPI() {
-    isJreHiDPIEnabled(); // force init
-    return jreHiDPI;
+  fun test_jreHiDPI(): AtomicReference<Boolean> {
+    isJreHiDPIEnabled() // force init
+    return jreHiDPI
   }
 }
