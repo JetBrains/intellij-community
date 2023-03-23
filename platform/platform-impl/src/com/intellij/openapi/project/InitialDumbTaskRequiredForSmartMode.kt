f7bc1a5f3bb6af25fc53d0995b506f264bd3327d
Andrei.Kuznetsov <andrei.kuznetsov@jetbrains.com>
2023-03-23 16:06:58 +0100

    Make sure that OnDiskSharedIndexChunkLocator executes before scanning
    Note that now all the `StartupActivity.RequiredForSmartMode` are executed in dumb mode under dumb progress indicator (not as part of InitProjectActivity as it was before COMMIT 0c153f8317ddac4aed1a1a71ed01f8dcb7bd5278)
    
    GitOrigin-RevId: 8b02f80d28b31b92ac83b58a83482bbcc23850c9

--- platform/platform-impl/src/com/intellij/openapi/project/InitialDumbTaskRequiredForSmartMode.kt
+++ platform/platform-impl/src/com/intellij/openapi/project/InitialDumbTaskRequiredForSmartMode.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.project
 
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.DumbServiceImpl.REQUIRED_FOR_SMART_MODE_STARTUP_ACTIVITY
@@ -11,7 +12,16 @@ class InitialDumbTaskRequiredForSmartMode(private val project: Project) : DumbMo
     val activities = REQUIRED_FOR_SMART_MODE_STARTUP_ACTIVITY.extensionList
     for (activity in activities) {
       ProgressManager.checkCanceled()
-      activity.runActivity(project)
+      logger<InitialDumbTaskRequiredForSmartMode>().assertTrue(indicator == ProgressManager.getGlobalProgressIndicator(),
+                                                               "There might be visual inconsistencies: " +
+                                                               "launched activities can only use thread's global indicator.")
+      indicator.pushState()
+      try {
+        activity.runActivity(project)
+      }
+      finally {
+        indicator.popState()
+      }
     }
   }
 }
