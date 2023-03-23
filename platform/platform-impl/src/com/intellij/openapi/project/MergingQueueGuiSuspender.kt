c2169dff7d62d38df207a0a53097c69dbafbbbab
Andrei.Kuznetsov <andrei.kuznetsov@jetbrains.com>
2023-03-23 18:54:37 +0100

    IDEA-315703: MergingQueueGuiSuspender: convert java > kotlin
    GitOrigin-RevId: f09ac170bc3c11ead97d4ad35040915c1c9c4a44

--- platform/platform-impl/src/com/intellij/openapi/project/MergingQueueGuiSuspender.kt
+++ platform/platform-impl/src/com/intellij/openapi/project/MergingQueueGuiSuspender.kt
@@ -1,90 +1,80 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.openapi.project;
+package com.intellij.openapi.project
 
-import com.intellij.ide.IdeBundle;
-import com.intellij.openapi.application.AccessToken;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.progress.impl.ProgressSuspender;
-import com.intellij.openapi.util.NlsContexts;
-import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.ide.IdeBundle
+import com.intellij.openapi.application.AccessToken
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.progress.impl.ProgressSuspender
+import com.intellij.openapi.util.NlsContexts
+import com.intellij.util.containers.ContainerUtil
+import org.jetbrains.annotations.VisibleForTesting
+import java.util.function.Supplier
 
-import java.util.List;
-import java.util.function.Supplier;
-
+@VisibleForTesting
 class MergingQueueGuiSuspender {
-  private static final Logger LOG = Logger.getInstance(MergingQueueGuiSuspender.class);
-  private volatile ProgressSuspender myCurrentSuspender;
-  private final List<String> myRequestedSuspensions = ContainerUtil.createEmptyCOWList();
-
-
-  void suspendAndRun(@NlsContexts.ProgressText @NotNull String activityName, @NotNull Runnable activity) {
-    try (AccessToken ignore = heavyActivityStarted(activityName)) {
-      activity.run();
-    }
+  @Volatile
+  private var myCurrentSuspender: ProgressSuspender? = null
+  private val myRequestedSuspensions: MutableList<@NlsContexts.ProgressText String> = ContainerUtil.createEmptyCOWList()
+  fun suspendAndRun(activityName: @NlsContexts.ProgressText String, activity: Runnable) {
+    heavyActivityStarted(activityName).use { activity.run() }
   }
 
-  @NotNull AccessToken heavyActivityStarted(@NlsContexts.ProgressText @NotNull String activityName) {
-    String reason = IdeBundle.message("dumb.service.indexing.paused.due.to", activityName);
-    synchronized (myRequestedSuspensions) {
-      myRequestedSuspensions.add(reason);
-    }
-
-    suspendCurrentTask(reason);
-    return new AccessToken() {
-      @Override
-      public void finish() {
-        synchronized (myRequestedSuspensions) {
-          myRequestedSuspensions.remove(reason);
-        }
-        resumeAutoSuspendedTask(reason);
+  fun heavyActivityStarted(activityName: @NlsContexts.ProgressText String): AccessToken {
+    val reason = IdeBundle.message("dumb.service.indexing.paused.due.to", activityName)
+    synchronized(myRequestedSuspensions) { myRequestedSuspensions.add(reason) }
+    suspendCurrentTask(reason)
+    return object : AccessToken() {
+      override fun finish() {
+        synchronized(myRequestedSuspensions) { myRequestedSuspensions.remove(reason) }
+        resumeAutoSuspendedTask(reason)
       }
-    };
-  }
-
-  void resumeProgressIfPossible() {
-    ProgressSuspender suspender = myCurrentSuspender;
-    if (suspender != null && suspender.isSuspended()) {
-      suspender.resumeProcess();
     }
   }
 
-  <T> T setCurrentSuspenderAndSuspendIfRequested(@Nullable ProgressSuspender suspender, @NotNull Supplier<T> runnable) {
-    LOG.assertTrue(myCurrentSuspender == null, "Already suspended in another thread, or recursive invocation.");
-    try {
-      myCurrentSuspender = suspender;
-      suspendIfRequested(suspender);
-      return runnable.get();
+  fun resumeProgressIfPossible() {
+    val suspender = myCurrentSuspender
+    if (suspender != null && suspender.isSuspended) {
+      suspender.resumeProcess()
+    }
+  }
+
+  fun <T> setCurrentSuspenderAndSuspendIfRequested(suspender: ProgressSuspender, runnable: Supplier<T>): T {
+    LOG.assertTrue(myCurrentSuspender == null, "Already suspended in another thread, or recursive invocation.")
+    return try {
+      myCurrentSuspender = suspender
+      suspendIfRequested(suspender)
+      runnable.get()
     }
     finally {
-      LOG.assertTrue(myCurrentSuspender == suspender, "Suspender has changed unexpectedly");
-      myCurrentSuspender = null;
+      LOG.assertTrue(myCurrentSuspender == suspender, "Suspender has changed unexpectedly")
+      myCurrentSuspender = null
     }
   }
 
-  private void resumeAutoSuspendedTask(@NlsContexts.ProgressText @NotNull String reason) {
-    ProgressSuspender currentSuspender = myCurrentSuspender;
-    if (currentSuspender != null && currentSuspender.isSuspended() && reason.equals(currentSuspender.getSuspendedText())) {
-      currentSuspender.resumeProcess();
-      suspendIfRequested(currentSuspender); // take the following reason from the queue (if any)
+  private fun resumeAutoSuspendedTask(reason: @NlsContexts.ProgressText String) {
+    val currentSuspender = myCurrentSuspender
+    if (currentSuspender != null && currentSuspender.isSuspended && reason == currentSuspender.suspendedText) {
+      currentSuspender.resumeProcess()
+      suspendIfRequested(currentSuspender) // take the following reason from the queue (if any)
     }
   }
 
-  private void suspendIfRequested(ProgressSuspender suspender) {
-    String suspendedReason;
-    synchronized (myRequestedSuspensions) {
-      suspendedReason = ContainerUtil.getLastItem(myRequestedSuspensions);
-    }
+  private fun suspendIfRequested(suspender: ProgressSuspender) {
+    var suspendedReason: String?
+    synchronized(myRequestedSuspensions) { suspendedReason = myRequestedSuspensions.lastOrNull() }
     if (suspendedReason != null) {
-      suspender.suspendProcess(suspendedReason);
+      suspender.suspendProcess(suspendedReason)
     }
   }
 
-  private void suspendCurrentTask(@NlsContexts.ProgressText String reason) {
-    ProgressSuspender currentSuspender = myCurrentSuspender;
-    if (currentSuspender != null && !currentSuspender.isSuspended()) {
-      currentSuspender.suspendProcess(reason);
+  private fun suspendCurrentTask(reason: @NlsContexts.ProgressText String) {
+    val currentSuspender = myCurrentSuspender
+    if (currentSuspender != null && !currentSuspender.isSuspended) {
+      currentSuspender.suspendProcess(reason)
     }
+  }
+
+  companion object {
+    private val LOG = Logger.getInstance(MergingQueueGuiSuspender::class.java)
   }
 }
