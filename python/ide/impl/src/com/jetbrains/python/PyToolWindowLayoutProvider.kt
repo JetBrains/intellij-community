8f6b1f712a6ef53358102601473e51aa727aeedb
Elizaveta Shashkova <elizaveta.shashkova@jetbrains.com>
2023-03-23 13:29:19 +0100

    PY-54547 Enable layout for new UI in PyCharm IDE
    (cherry picked from commit 5171adaf1b49290183909f516bed2a79969ead99)
    
    IJ-MR-105631
    
    GitOrigin-RevId: 09eaee61f2620569ecf68fc273200787849ff662

--- python/ide/impl/src/com/jetbrains/python/PyToolWindowLayoutProvider.kt
+++ python/ide/impl/src/com/jetbrains/python/PyToolWindowLayoutProvider.kt
@@ -1,10 +1,11 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.jetbrains.python.ui
+package com.jetbrains.python
 
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.toolWindow.DefaultToolWindowLayoutBuilder
 import com.intellij.toolWindow.DefaultToolWindowLayoutExtension
 
-open class PyToolWindowLayoutProvider : DefaultToolWindowLayoutExtension {
+class PyToolWindowLayoutProvider : DefaultToolWindowLayoutExtension {
   override fun buildV1Layout(builder: DefaultToolWindowLayoutBuilder) {
   }
 
@@ -12,6 +13,6 @@ open class PyToolWindowLayoutProvider : DefaultToolWindowLayoutExtension {
     builder.right.addOrUpdate("SciView") { weight = 0.1f }
     builder.bottom.addOrUpdate("Python Packages") { weight = 0.1f }
     builder.bottom.addOrUpdate("Python Console") { weight = 0.1f }
+    thisLogger().info("Python default layout applied")
   }
-
 }
