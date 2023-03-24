07cdec4e06a040996e31875532e850134cf85a72
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2023-03-24 17:31:07 +0200

    PY-53599 Enable PyTensorFlowTest env test on TC
    GitOrigin-RevId: 45b60ff82ee8f1f9ddad9bd9b7246e65914cf392

--- python/testSrc/com/jetbrains/env/python/PyTensorFlowTest.kt
+++ python/testSrc/com/jetbrains/env/python/PyTensorFlowTest.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.jetbrains.env.tensorFlow
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.jetbrains.env.python
 
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.testFramework.UsefulTestCase
