60bb9c600c0513630cf72bfa5925061f9917b1dc
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2023-03-24 13:20:54 +0100

    IDEA-162940 ProblemDescriptor shouldn't require text files
    PsiBinaryFile with null range allowed
    
    GitOrigin-RevId: 2da16184f36a453c2e798f30d8e98fe6cd4ad2ae

--- platform/analysis-impl/src/com/intellij/codeInspection/ProblemDescriptorBase.java
+++ platform/analysis-impl/src/com/intellij/codeInspection/ProblemDescriptorBase.java
@@ -56,9 +56,9 @@ public class ProblemDescriptorBase extends CommonProblemDescriptorImpl implement
     if (startElement != endElement) assertPhysical(endElement);
 
     TextRange startElementRange = getAnnotationRange(startElement);
-    LOG.assertTrue(startElement instanceof ExternallyAnnotated || startElementRange != null, startElement);
+    LOG.assertTrue(startElement instanceof ExternallyAnnotated || startElement instanceof PsiBinaryFile || startElementRange != null, startElement);
     TextRange endElementRange = startElement == endElement ? startElementRange : getAnnotationRange(endElement);
-    LOG.assertTrue(endElement instanceof ExternallyAnnotated || endElementRange != null, endElement);
+    LOG.assertTrue(endElement instanceof ExternallyAnnotated || endElement instanceof PsiBinaryFile || endElementRange != null, endElement);
     if (startElementRange != null
         && endElementRange != null
         && startElementRange.getStartOffset() >= endElementRange.getEndOffset()) {
