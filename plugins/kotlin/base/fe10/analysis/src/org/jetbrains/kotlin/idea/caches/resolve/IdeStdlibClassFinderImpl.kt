c00d6f239559213121d07321fba1b01019710b47
Roman Efremov <roman.efremov@jetbrains.com>
2023-03-28 10:33:13 +0200

    [kotlin] Fix for exception on resolving enum classes
    Fix for exception "Resolver for 'project source roots
    and libraries <...> does not know how to resolve".
    
    According to exception analyzer, "infos" is empty in
    `DiagnoseUnknownModuleInfoReporter.errorInSdkResolver`.
    Which means no modules found in
    `ProjectResolutionFacade.resolverForElement`.
    We try to use `resolverForModuleInfo` instead, which
    doesn't need modules. For this we can use
    `getIdeService` instead of `tryGetFrontendService` in
    `ResolutionFacade`.
    
    ^KTIJ-25083 Fixed
    
    GitOrigin-RevId: 5a581880b487bd0398246e9eeb92bd9d95e9c5eb

--- plugins/kotlin/base/fe10/analysis/src/org/jetbrains/kotlin/idea/caches/resolve/IdeStdlibClassFinderImpl.kt
+++ plugins/kotlin/base/fe10/analysis/src/org/jetbrains/kotlin/idea/caches/resolve/IdeStdlibClassFinderImpl.kt
@@ -6,9 +6,12 @@ import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.search.ProjectScope
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
-import org.jetbrains.kotlin.idea.caches.resolve.util.getJavaClassDescriptor
+import org.jetbrains.kotlin.idea.caches.resolve.util.javaResolutionFacade
+import org.jetbrains.kotlin.idea.resolve.ideService
+import org.jetbrains.kotlin.load.java.structure.impl.JavaClassImpl
 import org.jetbrains.kotlin.name.StandardClassIds
 import org.jetbrains.kotlin.resolve.StdlibClassFinder
+import org.jetbrains.kotlin.resolve.jvm.JavaDescriptorResolver
 
 internal class IdeStdlibClassFinderImpl(
   private val project: Project,
@@ -17,6 +20,6 @@ internal class IdeStdlibClassFinderImpl(
         val javaPsiFacade = JavaPsiFacade.getInstance(project)
         val libScope = ProjectScope.getLibrariesScope(javaPsiFacade.project)
         val psiClass = javaPsiFacade.findClass(StandardClassIds.EnumEntries.asFqNameString(), libScope)
-        return psiClass?.getJavaClassDescriptor()
+        return psiClass?.javaResolutionFacade()?.ideService<JavaDescriptorResolver>()?.resolveClass(JavaClassImpl(psiClass))
     }
 }
