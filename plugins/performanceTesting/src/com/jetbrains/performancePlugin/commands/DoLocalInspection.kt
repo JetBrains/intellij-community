f445c67f8b72ee89a2a46cd02c5624902a65b77a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-03-28 14:18:40 +0200

    convert DoLocalInspection to kotlin
    GitOrigin-RevId: f2b965da255de159032b8bc82784470f92a75a2a

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/DoLocalInspection.kt
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/DoLocalInspection.kt
@@ -1,128 +1,103 @@
-package com.jetbrains.performancePlugin.commands;
+package com.jetbrains.performancePlugin.commands
 
-import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
-import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl;
-import com.intellij.codeInsight.daemon.impl.HighlightInfo;
-import com.intellij.lang.annotation.HighlightSeverity;
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.project.DumbService;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.TypingTarget;
-import com.intellij.openapi.ui.playback.PlaybackContext;
-import com.intellij.openapi.ui.playback.commands.AbstractCommand;
-import com.intellij.openapi.util.ActionCallback;
-import com.intellij.openapi.util.Ref;
-import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.PsiFile;
-import com.intellij.util.messages.MessageBusConnection;
-import com.jetbrains.performancePlugin.PerformanceTestSpan;
-import com.jetbrains.performancePlugin.utils.ActionCallbackProfilerStopper;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.SpanBuilder;
-import io.opentelemetry.context.Scope;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.concurrency.Promise;
-import org.jetbrains.concurrency.Promises;
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer.DaemonListener
+import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.DumbService.Companion.getInstance
+import com.intellij.openapi.ui.playback.PlaybackContext
+import com.intellij.openapi.ui.playback.commands.AbstractCommand
+import com.intellij.openapi.ui.playback.commands.AlphaNumericTypeCommand
+import com.intellij.openapi.util.ActionCallback
+import com.intellij.openapi.util.Ref
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiManager
+import com.jetbrains.performancePlugin.PerformanceTestSpan
+import com.jetbrains.performancePlugin.utils.ActionCallbackProfilerStopper
+import io.opentelemetry.api.trace.Span
+import io.opentelemetry.context.Scope
+import org.jetbrains.concurrency.Promise
+import org.jetbrains.concurrency.toPromise
 
-import java.util.List;
-
-import static com.intellij.openapi.ui.playback.commands.AlphaNumericTypeCommand.findTarget;
-import static com.intellij.psi.PsiManager.getInstance;
-
-@SuppressWarnings("TestOnlyProblems")
-public final class DoLocalInspection extends AbstractCommand implements Disposable {
-  public static final String PREFIX = CMD_PREFIX + "doLocalInspection";
-  public static final String SPAN_NAME = "localInspections";
-
-  public DoLocalInspection(@NotNull String text, int line) {
-    super(text, line);
-  }
-
-  @NotNull
-  @Override
-  protected Promise<Object> _execute(@NotNull PlaybackContext context) {
-    ActionCallback actionCallback = new ActionCallbackProfilerStopper();
-    Project project = context.getProject();
-    DumbService.getInstance(project).waitForSmartMode();
-
-    ApplicationManager.getApplication().invokeAndWait(() -> {
-      TypingTarget target = findTarget(context);
+class DoLocalInspection(text: String, line: Int) : AbstractCommand(text, line), Disposable {
+  override fun _execute(context: PlaybackContext): Promise<Any> {
+    val actionCallback: ActionCallback = ActionCallbackProfilerStopper()
+    val project = context.project
+    getInstance(project).waitForSmartMode()
+    ApplicationManager.getApplication().invokeAndWait {
+      val target = AlphaNumericTypeCommand.findTarget(context)
       if (target == null) {
-        actionCallback.reject("There is no focus in editor");
+        actionCallback.reject("There is no focus in editor")
       }
-    });
-    if (actionCallback.isRejected()) {
-      return Promises.toPromise(actionCallback);
     }
-    MessageBusConnection busConnection = project.getMessageBus().connect();
-    SpanBuilder span = PerformanceTestSpan.TRACER.spanBuilder(SPAN_NAME).setParent(PerformanceTestSpan.getContext());
-    Ref<Span> spanRef = new Ref<>();
-    Ref<Scope> scopeRef = new Ref<>();
-    busConnection.subscribe(DaemonCodeAnalyzer.DAEMON_EVENT_TOPIC, new DaemonCodeAnalyzer.DaemonListener() {
-      @Override
-      public void daemonFinished() {
-        DaemonCodeAnalyzerImpl myDaemonCodeAnalyzer = (DaemonCodeAnalyzerImpl)DaemonCodeAnalyzer.getInstance(project);
-        final Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
-        assert editor != null;
-        PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument());
-        assert psiFile != null;
+    if (actionCallback.isRejected) {
+      return actionCallback.toPromise()
+    }
+    val busConnection = project.messageBus.connect()
+    val span = PerformanceTestSpan.TRACER.spanBuilder(SPAN_NAME).setParent(PerformanceTestSpan.getContext())
+    val spanRef = Ref<Span>()
+    val scopeRef = Ref<Scope>()
+    busConnection.subscribe<DaemonListener>(DaemonCodeAnalyzer.DAEMON_EVENT_TOPIC, object : DaemonListener {
+      override fun daemonFinished() {
+        val myDaemonCodeAnalyzer = DaemonCodeAnalyzer.getInstance(project) as DaemonCodeAnalyzerImpl
+        val editor = FileEditorManager.getInstance(project).selectedTextEditor!!
+        val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.document)!!
         if (myDaemonCodeAnalyzer.isErrorAnalyzingFinished(psiFile)) {
-          List<HighlightInfo> errorsOnHighlighting =
-            DaemonCodeAnalyzerImpl.getHighlights(editor.getDocument(), HighlightSeverity.ERROR, project);
-          List<HighlightInfo> warningsOnHighlighting =
-            DaemonCodeAnalyzerImpl.getHighlights(editor.getDocument(), HighlightSeverity.WARNING, project);
-          List<HighlightInfo> weakWarningsOnHighlighting =
-            DaemonCodeAnalyzerImpl.getHighlights(editor.getDocument(), HighlightSeverity.WEAK_WARNING, project);
-
-          StringBuilder finishMessage = new StringBuilder("Local inspections have been finished with: ");
-
-          spanRef.get().setAttribute("Errors", errorsOnHighlighting.size());
+          val errorsOnHighlighting = DaemonCodeAnalyzerImpl.getHighlights(
+            editor.document, HighlightSeverity.ERROR, project)
+          val warningsOnHighlighting = DaemonCodeAnalyzerImpl.getHighlights(
+            editor.document, HighlightSeverity.WARNING, project)
+          val weakWarningsOnHighlighting = DaemonCodeAnalyzerImpl.getHighlights(
+            editor.document, HighlightSeverity.WEAK_WARNING, project)
+          val finishMessage = StringBuilder("Local inspections have been finished with: ")
+          spanRef.get().setAttribute("Errors", errorsOnHighlighting.size.toLong())
           if (!errorsOnHighlighting.isEmpty()) {
-            finishMessage.append("\n").append(new String("Errors: " + errorsOnHighlighting.size()));
+            finishMessage.append("\n").append(java.lang.String("Errors: " + errorsOnHighlighting.size) as String)
           }
-          for (HighlightInfo error : errorsOnHighlighting) {
-            finishMessage.append("\n").append(new String(error.getText() + ": " + error.getDescription()));
+          for (error in errorsOnHighlighting) {
+            finishMessage.append("\n").append(java.lang.String(error.text + ": " + error.description) as String)
           }
-
-          spanRef.get().setAttribute("Warnings", warningsOnHighlighting.size());
+          spanRef.get().setAttribute("Warnings", warningsOnHighlighting.size.toLong())
           if (!warningsOnHighlighting.isEmpty()) {
-            finishMessage.append("\n").append(new String("Warnings: " + warningsOnHighlighting.size()));
+            finishMessage.append("\n").append(java.lang.String("Warnings: " + warningsOnHighlighting.size) as String)
           }
-          for (HighlightInfo warning : warningsOnHighlighting) {
-            finishMessage.append("\n").append(new String(warning.getText() + ": " + warning.getDescription()));
+          for (warning in warningsOnHighlighting) {
+            finishMessage.append("\n").append(java.lang.String(warning.text + ": " + warning.description) as String)
           }
-
-          spanRef.get().setAttribute("Weak Warnings", warningsOnHighlighting.size());
+          spanRef.get().setAttribute("Weak Warnings", warningsOnHighlighting.size.toLong())
           if (!weakWarningsOnHighlighting.isEmpty()) {
-            finishMessage.append("\n").append(new String("Weak Warnings: " + weakWarningsOnHighlighting.size()));
+            finishMessage.append("\n").append(
+              java.lang.String("Weak Warnings: " + weakWarningsOnHighlighting.size) as String)
           }
-          for (HighlightInfo weakWarning : weakWarningsOnHighlighting) {
-            finishMessage.append("\n").append(new String(weakWarning.getText() + ": " + weakWarning.getDescription()));
+          for (weakWarning in weakWarningsOnHighlighting) {
+            finishMessage.append("\n").append(
+              java.lang.String(weakWarning.text + ": " + weakWarning.description) as String)
           }
-          spanRef.get().end();
-          scopeRef.get().close();
-          busConnection.disconnect();
-          context.message(finishMessage.toString(), getLine());
-          actionCallback.setDone();
+          spanRef.get().end()
+          scopeRef.get().close()
+          busConnection.disconnect()
+          context.message(finishMessage.toString(), line)
+          actionCallback.setDone()
         }
       }
-    });
-
-    DumbService.getInstance(project).smartInvokeLater(() -> {
-      getInstance(project).dropPsiCaches();
-      context.message("Local inspections have been started", getLine());
-      spanRef.set(span.startSpan());
-      scopeRef.set(spanRef.get().makeCurrent());
-      DaemonCodeAnalyzer.getInstance(project).restart();
-    });
-
-    return Promises.toPromise(actionCallback);
+    })
+    getInstance(project).smartInvokeLater {
+      PsiManager.getInstance(project).dropPsiCaches()
+      context.message("Local inspections have been started", line)
+      spanRef.set(span.startSpan())
+      scopeRef.set(spanRef.get().makeCurrent())
+      DaemonCodeAnalyzer.getInstance(project).restart()
+    }
+    return actionCallback.toPromise()
   }
 
-  @Override
-  public void dispose() {
+  override fun dispose() {}
+
+  companion object {
+    const val PREFIX = CMD_PREFIX + "doLocalInspection"
+    const val SPAN_NAME = "localInspections"
   }
 }
