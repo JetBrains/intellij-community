1b890589421234ef02b027f82d57b766adb299f2
Alexander Zolotov <zolotov@jetbrains.com>
2023-03-28 12:40:07 +0200

    [textmate] always set up-to-date extensions mapping
    GitOrigin-RevId: 9749829b035c5813cca4dfd5e29d11f0d93633fc

--- plugins/textmate/src/org/jetbrains/plugins/textmate/TextMateServiceImpl.java
+++ plugins/textmate/src/org/jetbrains/plugins/textmate/TextMateServiceImpl.java
@@ -109,15 +109,13 @@ public final class TextMateServiceImpl extends TextMateService {
         }
       }
 
-      if (!oldExtensionsMapping.equals(newExtensionsMapping)) {
-        if (fireEvents) {
-          fireFileTypesChangedEvent("old mappings = " + oldExtensionsMapping + ", new mappings" + newExtensionsMapping, () -> {
-            myExtensionsMapping = newExtensionsMapping;
-          });
-        }
-        else {
+      if (fireEvents && !oldExtensionsMapping.equals(newExtensionsMapping)) {
+        fireFileTypesChangedEvent("old mappings = " + oldExtensionsMapping + ", new mappings" + newExtensionsMapping, () -> {
           myExtensionsMapping = newExtensionsMapping;
-        }
+        });
+      }
+      else {
+        myExtensionsMapping = newExtensionsMapping;
       }
       mySyntaxTable.compact();
     }
