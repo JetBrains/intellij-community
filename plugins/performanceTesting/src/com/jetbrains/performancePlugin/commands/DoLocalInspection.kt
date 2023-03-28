1bbbef972f38886bacf1fa4c8a86ee569f666b5f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-03-28 14:30:28 +0200

    DoLocalInspection - use coroutine instead of promise
    GitOrigin-RevId: a218fabade4b02922f4ce584c8ca66b62dd4de2b

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/DoLocalInspection.kt
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/DoLocalInspection.kt
@@ -4,100 +4,109 @@ import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer.DaemonListener
 import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl
 import com.intellij.lang.annotation.HighlightSeverity
-import com.intellij.openapi.Disposable
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.EDT
 import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.project.DumbService.Companion.getInstance
+import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.project.waitForSmartMode
+import com.intellij.openapi.ui.TypingTarget
 import com.intellij.openapi.ui.playback.PlaybackContext
-import com.intellij.openapi.ui.playback.commands.AbstractCommand
-import com.intellij.openapi.ui.playback.commands.AlphaNumericTypeCommand
-import com.intellij.openapi.util.ActionCallback
-import com.intellij.openapi.util.Ref
+import com.intellij.openapi.ui.playback.commands.PlaybackCommandCoroutineAdapter
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiManager
 import com.jetbrains.performancePlugin.PerformanceTestSpan
-import com.jetbrains.performancePlugin.utils.ActionCallbackProfilerStopper
 import io.opentelemetry.api.trace.Span
 import io.opentelemetry.context.Scope
-import org.jetbrains.concurrency.Promise
-import org.jetbrains.concurrency.toPromise
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
+import java.awt.KeyboardFocusManager
+import kotlin.coroutines.resume
 
-class DoLocalInspection(text: String, line: Int) : AbstractCommand(text, line), Disposable {
-  override fun _execute(context: PlaybackContext): Promise<Any> {
-    val actionCallback: ActionCallback = ActionCallbackProfilerStopper()
-    val project = context.project
-    getInstance(project).waitForSmartMode()
-    ApplicationManager.getApplication().invokeAndWait {
-      val target = AlphaNumericTypeCommand.findTarget(context)
-      if (target == null) {
-        actionCallback.reject("There is no focus in editor")
-      }
-    }
-    if (actionCallback.isRejected) {
-      return actionCallback.toPromise()
-    }
-    val busConnection = project.messageBus.connect()
-    val span = PerformanceTestSpan.TRACER.spanBuilder(SPAN_NAME).setParent(PerformanceTestSpan.getContext())
-    val spanRef = Ref<Span>()
-    val scopeRef = Ref<Scope>()
-    busConnection.subscribe<DaemonListener>(DaemonCodeAnalyzer.DAEMON_EVENT_TOPIC, object : DaemonListener {
-      override fun daemonFinished() {
-        val myDaemonCodeAnalyzer = DaemonCodeAnalyzer.getInstance(project) as DaemonCodeAnalyzerImpl
-        val editor = FileEditorManager.getInstance(project).selectedTextEditor!!
-        val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.document)!!
-        if (myDaemonCodeAnalyzer.isErrorAnalyzingFinished(psiFile)) {
-          val errorsOnHighlighting = DaemonCodeAnalyzerImpl.getHighlights(
-            editor.document, HighlightSeverity.ERROR, project)
-          val warningsOnHighlighting = DaemonCodeAnalyzerImpl.getHighlights(
-            editor.document, HighlightSeverity.WARNING, project)
-          val weakWarningsOnHighlighting = DaemonCodeAnalyzerImpl.getHighlights(
-            editor.document, HighlightSeverity.WEAK_WARNING, project)
-          val finishMessage = StringBuilder("Local inspections have been finished with: ")
-          spanRef.get().setAttribute("Errors", errorsOnHighlighting.size.toLong())
-          if (!errorsOnHighlighting.isEmpty()) {
-            finishMessage.append("\n").append(java.lang.String("Errors: " + errorsOnHighlighting.size) as String)
-          }
-          for (error in errorsOnHighlighting) {
-            finishMessage.append("\n").append(java.lang.String(error.text + ": " + error.description) as String)
-          }
-          spanRef.get().setAttribute("Warnings", warningsOnHighlighting.size.toLong())
-          if (!warningsOnHighlighting.isEmpty()) {
-            finishMessage.append("\n").append(java.lang.String("Warnings: " + warningsOnHighlighting.size) as String)
-          }
-          for (warning in warningsOnHighlighting) {
-            finishMessage.append("\n").append(java.lang.String(warning.text + ": " + warning.description) as String)
-          }
-          spanRef.get().setAttribute("Weak Warnings", warningsOnHighlighting.size.toLong())
-          if (!weakWarningsOnHighlighting.isEmpty()) {
-            finishMessage.append("\n").append(
-              java.lang.String("Weak Warnings: " + weakWarningsOnHighlighting.size) as String)
-          }
-          for (weakWarning in weakWarningsOnHighlighting) {
-            finishMessage.append("\n").append(
-              java.lang.String(weakWarning.text + ": " + weakWarning.description) as String)
-          }
-          spanRef.get().end()
-          scopeRef.get().close()
-          busConnection.disconnect()
-          context.message(finishMessage.toString(), line)
-          actionCallback.setDone()
-        }
-      }
-    })
-    getInstance(project).smartInvokeLater {
-      PsiManager.getInstance(project).dropPsiCaches()
-      context.message("Local inspections have been started", line)
-      spanRef.set(span.startSpan())
-      scopeRef.set(spanRef.get().makeCurrent())
-      DaemonCodeAnalyzer.getInstance(project).restart()
-    }
-    return actionCallback.toPromise()
+class DoLocalInspection(text: String, line: Int) : PlaybackCommandCoroutineAdapter(text, line) {
+  companion object {
+    const val PREFIX: String = CMD_PREFIX + "doLocalInspection"
+    const val SPAN_NAME: String = "localInspections"
   }
 
-  override fun dispose() {}
+  @Suppress("TestOnlyProblems")
+  override suspend fun doExecute(context: PlaybackContext) {
+    val project = context.project
+    project.waitForSmartMode()
+    checkFocusInEditor(context)
 
-  companion object {
-    const val PREFIX = CMD_PREFIX + "doLocalInspection"
-    const val SPAN_NAME = "localInspections"
+    val busConnection = project.messageBus.simpleConnect()
+    val span = PerformanceTestSpan.TRACER.spanBuilder(SPAN_NAME).setParent(PerformanceTestSpan.getContext())
+    var spanRef: Span? = null
+    var scopeRef: Scope? = null
+    suspendCancellableCoroutine { continuation ->
+      busConnection.subscribe(DaemonCodeAnalyzer.DAEMON_EVENT_TOPIC, object : DaemonListener {
+        override fun daemonFinished() {
+          val daemonCodeAnalyzer = DaemonCodeAnalyzer.getInstance(project) as DaemonCodeAnalyzerImpl
+          val document = FileEditorManager.getInstance(project).selectedTextEditor!!.document
+          val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document)!!
+          if (!daemonCodeAnalyzer.isErrorAnalyzingFinished(psiFile)) {
+            return
+          }
+
+          val errorsOnHighlighting = DaemonCodeAnalyzerImpl.getHighlights(document, HighlightSeverity.ERROR, project)
+          val warningsOnHighlighting = DaemonCodeAnalyzerImpl.getHighlights(document, HighlightSeverity.WARNING, project)
+          val weakWarningsOnHighlighting = DaemonCodeAnalyzerImpl.getHighlights(document, HighlightSeverity.WEAK_WARNING, project)
+          val finishMessage = StringBuilder("Local inspections have been finished with: ")
+          spanRef!!.setAttribute("Errors", errorsOnHighlighting.size.toLong())
+          if (!errorsOnHighlighting.isEmpty()) {
+            finishMessage.append("\n").append("Errors: ${errorsOnHighlighting.size}")
+          }
+          for (error in errorsOnHighlighting) {
+            finishMessage.append("\n").append("${error.text}: ${error.description}")
+          }
+          spanRef!!.setAttribute("Warnings", warningsOnHighlighting.size.toLong())
+          if (!warningsOnHighlighting.isEmpty()) {
+            finishMessage.append("\n").append("Warnings: ${warningsOnHighlighting.size}")
+          }
+          for (warning in warningsOnHighlighting) {
+            finishMessage.append("\n").append("${warning.text}: ${warning.description}")
+          }
+          spanRef!!.setAttribute("Weak Warnings", warningsOnHighlighting.size.toLong())
+          if (!weakWarningsOnHighlighting.isEmpty()) {
+            finishMessage.append("\n").append("Weak Warnings: ${weakWarningsOnHighlighting.size}")
+          }
+          for (weakWarning in weakWarningsOnHighlighting) {
+            finishMessage.append("\n").append("${weakWarning.text}: ${weakWarning.description}")
+          }
+          spanRef!!.end()
+          scopeRef!!.close()
+          busConnection.disconnect()
+          context.message(finishMessage.toString(), line)
+          continuation.resume(Unit)
+        }
+      })
+
+      DumbService.getInstance(project).smartInvokeLater {
+        PsiManager.getInstance(project).dropPsiCaches()
+        context.message("Local inspections have been started", line)
+        spanRef = span.startSpan()
+        scopeRef = spanRef!!.makeCurrent()
+        DaemonCodeAnalyzer.getInstance(project).restart()
+      }
+    }
+  }
+
+  private suspend fun checkFocusInEditor(context: PlaybackContext) {
+    if (!context.isUseTypingTargets) {
+      return
+    }
+
+    withContext(Dispatchers.EDT) {
+      val focusOwner = KeyboardFocusManager.getCurrentKeyboardFocusManager().focusOwner
+      var each = focusOwner
+      while (each != null) {
+        if (each is TypingTarget) {
+          return@withContext
+        }
+        each = each.parent
+      }
+
+      throw IllegalStateException("There is no focus in editor (focusOwner=$focusOwner)")
+    }
   }
 }
