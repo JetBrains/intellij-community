861b0b36f6ead3e3407151aa3c766230dc910b41
Yann Cébron <yann.cebron@jetbrains.com>
2023-03-30 16:46:48 +0200

    [devkit] UnregisteredNamedColorInspectionTest: light, cleanup
    GitOrigin-RevId: 989dbfeed5f07dda40af4e9b60fb35906db57a50

--- plugins/devkit/intellij.devkit.themes/test/UnregisteredNamedColorInspectionTestBase.kt
+++ plugins/devkit/intellij.devkit.themes/test/UnregisteredNamedColorInspectionTestBase.kt
@@ -1,30 +1,16 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.themes
 
-import com.intellij.openapi.application.PathManager
-import com.intellij.testFramework.builders.JavaModuleFixtureBuilder
-import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase
-import com.intellij.ui.components.JBList
-import com.intellij.util.PathUtil
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 
-abstract class UnregisteredNamedColorInspectionTestBase : JavaCodeInsightFixtureTestCase() {
+abstract class UnregisteredNamedColorInspectionTestBase : LightJavaCodeInsightFixtureTestCase() {
 
-  companion object {
-    const val themeMetadata = "/themes/metadata/IntelliJPlatform.themeMetadata.json"
-    const val knownNamedColor = "CompletionPopup.background"
-  }
-
-  override fun tuneFixture(moduleBuilder: JavaModuleFixtureBuilder<*>?) {
-    val resourceRoot = PathManager.getResourceRoot(javaClass, themeMetadata)
-    moduleBuilder!!.addLibrary("platform-resources", resourceRoot)
-    moduleBuilder.addLibrary("platform-ide", PathUtil.getJarPathForClass(JBList::class.java))
-  }
+  protected val knownNamedColor = "CompletionPopup.background"
 
   override fun setUp() {
     super.setUp()
     myFixture.enableInspections(UnregisteredNamedColorInspection())
 
-    //language=JAVA
     myFixture.addClass("""
       package com.intellij.ui;
 
