96b1c53d49d439266c40891c0bb8e6454d053ceb
Aleksandr Izmailov <aleksandr.izmaylov@jetbrains.com>
2023-03-30 16:30:43 +0200

    [devkit] IDEA-314358: inspection for propagation of `RequiresBlockingContext` for Kotlin and Java
    GitOrigin-RevId: 40bcfc7d89cdf377c23f7dabf2b03c1056b98794

---
+++ plugins/devkit/devkit-core/src/inspections/CallingJavaMethodShouldBerRequiresBlockingContextInspection.kt
@@ -0,0 +1,74 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.idea.devkit.inspections
+
+import com.intellij.codeInsight.AnnotationUtil
+import com.intellij.codeInsight.intention.AddAnnotationFix
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.*
+import org.jetbrains.idea.devkit.DevKitBundle
+import org.jetbrains.idea.devkit.util.QuickFixWithReferenceToElement
+import org.jetbrains.idea.devkit.util.REQUIRES_BLOCKING_CONTEXT_ANNOTATION
+import org.jetbrains.idea.devkit.util.isInspectionForBlockingContextAvailable
+
+class CallingJavaMethodShouldBerRequiresBlockingContextInspection : LocalInspectionTool() {
+  override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+    return if (isInspectionForBlockingContextAvailable(holder.file)) {
+      MethodVisitor(holder)
+    } else {
+      PsiElementVisitor.EMPTY_VISITOR
+    }
+  }
+
+  private class MethodVisitor(private val holder: ProblemsHolder) : JavaElementVisitor() {
+    override fun visitMethod(method: PsiMethod) {
+      if (!AnnotationUtil.isAnnotated(method, REQUIRES_BLOCKING_CONTEXT_ANNOTATION, AnnotationUtil.CHECK_HIERARCHY)) {
+        method.body?.accept(MethodBodyVisitor(holder, method))
+      }
+      super.visitMethod(method)
+    }
+  }
+
+  private class MethodBodyVisitor(
+    private val holder: ProblemsHolder,
+    private val currentMethod: PsiMethod
+  ) : JavaRecursiveElementWalkingVisitor() {
+    override fun visitCallExpression(callExpression: PsiCallExpression) {
+      val method = callExpression.resolveMethod()
+      if (method == null || !AnnotationUtil.isAnnotated(method, REQUIRES_BLOCKING_CONTEXT_ANNOTATION, 0)) {
+        return super.visitCallExpression(callExpression)
+      }
+
+      val elementToHighlight = when (callExpression) {
+        is PsiMethodCallExpression -> callExpression.methodExpression.referenceNameElement
+        is PsiNewExpression -> callExpression.classOrAnonymousClassReference?.referenceNameElement
+        else -> null
+      } ?: callExpression
+
+      holder.registerProblem(
+        elementToHighlight,
+        DevKitBundle.message("inspections.calling.method.should.be.rbc.annotated.java.message"),
+        ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
+        AnnotateFix(callExpression, currentMethod)
+      )
+    }
+
+    override fun visitLambdaExpression(expression: PsiLambdaExpression): Unit = Unit
+
+    override fun visitClass(aClass: PsiClass): Unit = Unit
+
+    override fun visitMethod(method: PsiMethod): Unit = Unit
+  }
+
+  private class AnnotateFix(element: PsiElement, method: PsiMethod) : QuickFixWithReferenceToElement<PsiMethod>(element, method) {
+    override fun getFamilyName(): String = DevKitBundle.message("inspections.calling.method.should.be.rbc.annotated.java.annotate.fix")
+    override fun getText(): String = familyName
+
+    override fun invoke(project: Project, file: PsiFile, editor: Editor?, startElement: PsiElement, endElement: PsiElement) {
+      AddAnnotationFix(REQUIRES_BLOCKING_CONTEXT_ANNOTATION, referencedElement.element!!).applyFix()
+    }
+  }
+}
---
+++ plugins/devkit/intellij.kotlin.devkit/src/inspections/CallingKtFunctionShouldBeRequiresBlockingContextInspection.kt
@@ -0,0 +1,103 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.idea.devkit.kotlin.inspections
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiFile
+import org.jetbrains.idea.devkit.kotlin.DevKitKotlinBundle
+import org.jetbrains.idea.devkit.util.QuickFixWithReferenceToElement
+import org.jetbrains.idea.devkit.util.isInspectionForBlockingContextAvailable
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.annotations.hasAnnotation
+import org.jetbrains.kotlin.analysis.api.calls.singleFunctionCallOrNull
+import org.jetbrains.kotlin.analysis.api.calls.symbol
+import org.jetbrains.kotlin.analysis.api.symbols.KtFunctionSymbol
+import org.jetbrains.kotlin.analysis.api.symbols.markers.KtNamedSymbol
+import org.jetbrains.kotlin.idea.util.addAnnotation
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtTreeVisitorVoid
+
+class CallingKtFunctionShouldBeRequiresBlockingContextInspection : LocalInspectionTool() {
+  override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+    return if (isInspectionForBlockingContextAvailable(holder.file)) {
+      FunctionVisitor(holder)
+    }
+    else {
+      PsiElementVisitor.EMPTY_VISITOR
+    }
+  }
+
+  private class FunctionVisitor(
+    private val holder: ProblemsHolder,
+  ) : KtTreeVisitorVoid() {
+    override fun visitElement(element: PsiElement) = Unit
+
+    override fun visitNamedFunction(function: KtNamedFunction) {
+      if (!function.hasModifier(KtTokens.SUSPEND_KEYWORD) && !isAnnotated(function)) {
+        function.bodyExpression?.accept(FunctionBodyVisitor(holder, function))
+        return
+      }
+      super.visitNamedFunction(function)
+    }
+  }
+
+  private class FunctionBodyVisitor(
+    private val holder: ProblemsHolder,
+    private val currentFunction: KtNamedFunction,
+  ) : BlockingContextFunctionBodyVisitor() {
+    override fun visitCallExpression(expression: KtCallExpression) {
+      analyze(expression) {
+        val functionCall = expression.resolveCall().singleFunctionCallOrNull()
+        val calledSymbol = functionCall?.partiallyAppliedSymbol?.symbol
+
+        if (calledSymbol !is KtNamedSymbol) return
+        val hasAnnotation = calledSymbol.hasAnnotation(requiresBlockingContextAnnotationId)
+
+        if (!hasAnnotation) {
+          if (calledSymbol is KtFunctionSymbol && calledSymbol.isInline) {
+            checkInlineLambdaArguments(functionCall)
+          }
+
+          return super.visitCallExpression(expression)
+        }
+      }
+
+      holder.registerProblem(
+        extractElementToHighlight(expression),
+        DevKitKotlinBundle.message("inspections.calling.method.should.be.rbc.annotated.kt.message"),
+        ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
+        AnnotateFix(expression, currentFunction)
+      )
+    }
+  }
+}
+
+private class AnnotateFix(
+  element: PsiElement, callingMethod: KtNamedFunction
+) : QuickFixWithReferenceToElement<KtNamedFunction>(element, callingMethod) {
+  override fun getFamilyName(): String = DevKitKotlinBundle.message("inspections.calling.method.should.be.rbc.annotated.kt.annotate.fix")
+
+  override fun getText(): String = familyName
+
+  override fun invoke(project: Project, file: PsiFile, editor: Editor?, startElement: PsiElement, endElement: PsiElement) {
+    referencedElement.element!!.addAnnotation(requiresBlockingContextAnnotation)
+  }
+}
+
+private fun isAnnotated(function: KtNamedFunction): Boolean {
+  analyze(function) {
+    val functionSymbol = function.getFunctionLikeSymbol()
+    if (functionSymbol.hasAnnotation(requiresBlockingContextAnnotationId)) {
+      return true
+    }
+
+    return functionSymbol.getAllOverriddenSymbols().any { it.hasAnnotation(requiresBlockingContextAnnotationId) }
+  }
+}
