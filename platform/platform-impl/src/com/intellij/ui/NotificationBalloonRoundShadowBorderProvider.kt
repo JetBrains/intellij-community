f31ddd318b4586867f404f3b8c3baf1b2232e928
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-03-31 12:35:17 +0200

    convert NotificationBalloonRoundShadowBorderProvider to kotlin
    GitOrigin-RevId: 308c704c8ee652b3ebc67202ea2bb546f5d072b3

--- platform/platform-impl/src/com/intellij/ui/NotificationBalloonRoundShadowBorderProvider.kt
+++ platform/platform-impl/src/com/intellij/ui/NotificationBalloonRoundShadowBorderProvider.kt
@@ -1,31 +1,41 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.ui;
+package com.intellij.ui
 
-import com.intellij.icons.AllIcons.Ide.RoundShadow;
-import com.intellij.util.ui.JBValue;
-import org.jetbrains.annotations.NotNull;
-
-import java.awt.*;
-import java.awt.geom.RoundRectangle2D;
+import com.intellij.icons.AllIcons.Ide.RoundShadow
+import com.intellij.util.ui.JBValue
+import com.intellij.util.ui.JBValue.UIInteger
+import java.awt.Color
+import java.awt.Graphics2D
+import java.awt.Rectangle
+import java.awt.geom.RoundRectangle2D
 
 /**
  * @author Alexander Lobas
  */
-public final class NotificationBalloonRoundShadowBorderProvider extends NotificationBalloonShadowBorderProvider {
-  public static final JBValue CORNER_RADIUS = new JBValue.UIInteger("Notification.arc", 12);
-
-  public NotificationBalloonRoundShadowBorderProvider(@NotNull Color fillColor, @NotNull Color borderColor) {
-    super(fillColor, borderColor,
-          RoundShadow.Top, RoundShadow.Left, RoundShadow.Bottom, RoundShadow.Right,
-          RoundShadow.TopLeft, RoundShadow.TopRight, RoundShadow.BottomLeft, RoundShadow.BottomRight);
+class NotificationBalloonRoundShadowBorderProvider(fillColor: Color, borderColor: Color) : NotificationBalloonShadowBorderProvider(
+  fillColor = fillColor,
+  borderColor = borderColor,
+  topIcon = RoundShadow.Top,
+  leftIcon = RoundShadow.Left,
+  bottomIcon = RoundShadow.Bottom,
+  rightIcon = RoundShadow.Right,
+  topLeftIcon = RoundShadow.TopLeft,
+  topRightIcon = RoundShadow.TopRight,
+  bottomLeftIcon = RoundShadow.BottomLeft,
+  bottomRightIcon = RoundShadow.BottomRight,
+) {
+  companion object {
+    @JvmField
+    val CORNER_RADIUS: JBValue = UIInteger("Notification.arc", 12)
   }
 
-  @Override
-  public void paintBorder(@NotNull Rectangle bounds, @NotNull Graphics2D g) {
-    int cornerRadius = CORNER_RADIUS.get();
-    g.setColor(fillColor);
-    g.fill(new RoundRectangle2D.Double(bounds.x, bounds.y, bounds.width, bounds.height, cornerRadius, cornerRadius));
-    g.setColor(borderColor);
-    g.draw(new RoundRectangle2D.Double(bounds.x + 0.5, bounds.y + 0.5, bounds.width - 1, bounds.height - 1, cornerRadius, cornerRadius));
+  override fun paintBorder(bounds: Rectangle, g: Graphics2D) {
+    val cornerRadius = CORNER_RADIUS.get()
+    g.color = fillColor
+    g.fill(RoundRectangle2D.Double(bounds.x.toDouble(), bounds.y.toDouble(), bounds.width.toDouble(), bounds.height.toDouble(),
+                                   cornerRadius.toDouble(), cornerRadius.toDouble()))
+    g.color = borderColor
+    g.draw(RoundRectangle2D.Double(bounds.x + 0.5, bounds.y + 0.5, (bounds.width - 1).toDouble(), (bounds.height - 1).toDouble(),
+                                   cornerRadius.toDouble(), cornerRadius.toDouble()))
   }
 }
