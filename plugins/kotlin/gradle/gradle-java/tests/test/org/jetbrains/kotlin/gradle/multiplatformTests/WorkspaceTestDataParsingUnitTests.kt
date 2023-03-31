05191bdba6d62e2f8ec8ece2e3bab45eff0d249a
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2023-03-31 19:40:55 +0200

    [kt, tests] Support applying comments from test data to the actual report
    (cherry picked from commit e243063616868b3f458a7be6a0d8fc20494ade0b)
    
    GitOrigin-RevId: c50cfc83a857a5c0788c22354f0ebdf4aae6b92a

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/multiplatformTests/WorkspaceTestDataParsingUnitTests.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/multiplatformTests/WorkspaceTestDataParsingUnitTests.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.gradle.newTests
+package org.jetbrains.kotlin.gradle.multiplatformTests
 
 import org.jetbrains.kotlin.gradle.multiplatformTests.workspace.WorkspaceModelTestReportParser
 import org.jetbrains.kotlin.gradle.multiplatformTests.workspace.render
