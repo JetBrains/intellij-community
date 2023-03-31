0b14491c86800c72a9efd71b21419c87c6b3a08c
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-03-31 14:57:36 +0200

    [KTIJ-24976][Minor] Optimise imports on createKotlinMPPGradleModel
    GitOrigin-RevId: 82e9cc601b28964bc4fd19a696fe618dcbb2df0a

--- plugins/kotlin/gradle/gradle-tooling/tests/test/org/jetbrains/kotlin/gradle/createKotlinMPPGradleModel.kt
+++ plugins/kotlin/gradle/gradle-tooling/tests/test/org/jetbrains/kotlin/gradle/createKotlinMPPGradleModel.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.gradle
 
-import org.gradle.internal.impldep.org.apache.commons.lang.math.RandomUtils
 import org.jetbrains.kotlin.idea.gradleTooling.*
 import org.jetbrains.kotlin.idea.gradleTooling.IdeaKotlinExtras
 import org.jetbrains.kotlin.idea.projectModel.*
