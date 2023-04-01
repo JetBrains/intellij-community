8ca4746b773b603954c0d241caa5291a6b8f393c
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2023-04-01 23:39:45 +0200

    [kotlin] Persist jvm library kinds to avoid extra library scanning
    (cherry picked from commit b8238081c7d05ae06fe55d46357eb847d8156469)
    
    Change-Id: Ib8238081c7d05ae06fe55d46357eb847d8156469
    Issue: 288142539

--- plugins/kotlin/base/platforms/src/org/jetbrains/kotlin/idea/base/platforms/LibraryEffectiveKindProvider.kt
+++ plugins/kotlin/base/platforms/src/org/jetbrains/kotlin/idea/base/platforms/LibraryEffectiveKindProvider.kt
@@ -1,13 +1,20 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.base.platforms
 
+import com.intellij.ide.caches.CachesInvalidator
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
 import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.project.getProjectCachePath
 import com.intellij.openapi.roots.OrderRootType
 import com.intellij.openapi.roots.impl.libraries.LibraryEx
 import com.intellij.openapi.roots.impl.libraries.UnknownLibraryKind
 import com.intellij.openapi.roots.libraries.Library
+import com.intellij.openapi.roots.libraries.LibraryKind
+import com.intellij.openapi.roots.libraries.LibraryKindRegistry
 import com.intellij.openapi.roots.libraries.PersistentLibraryKind
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VfsUtil
@@ -16,13 +23,18 @@ import com.intellij.openapi.vfs.VirtualFileVisitor
 import com.intellij.util.indexing.roots.IndexableFileScanner
 import com.intellij.util.indexing.roots.kind.IndexableSetOrigin
 import com.intellij.util.indexing.roots.kind.LibraryOrigin
+import com.intellij.util.io.EnumeratorStringDescriptor
+import com.intellij.util.io.IOUtil
+import com.intellij.util.io.PersistentHashMap
+import com.intellij.util.io.StorageLockContext
 import org.jetbrains.kotlin.analysis.decompiler.psi.KotlinBuiltInFileType
 import org.jetbrains.kotlin.platform.idePlatformKind
 import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 import org.jetbrains.kotlin.serialization.deserialization.MetadataPackageFragment
+import java.io.IOException
 
 @Service(Service.Level.PROJECT)
-class LibraryEffectiveKindProvider {
+class LibraryEffectiveKindProvider(private val project: Project): Disposable {
     private companion object {
         val LIBRARY_KIND_KEY = Key.create<PersistentLibraryKind<*>?>("LibraryEffectiveKind")
         val NEEDS_TO_BE_CLARIFIED_KIND: UnknownLibraryKind = UnknownLibraryKind.getOrCreate("Needs to be clarified")
@@ -32,8 +44,60 @@ class LibraryEffectiveKindProvider {
         COMMON, JS, UNKNOWN
     }
 
+    @Volatile
+    private var cleanCache: Boolean = false
+    private val rootKindMap: Lazy<PersistentHashMap<String, String>> = lazy {
+        val path = project.getProjectCachePath("kotlin-library-kind")
+        try {
+            IOUtil.openCleanOrResetBroken(
+                {
+                    PersistentHashMap(
+                        path,
+                        EnumeratorStringDescriptor.INSTANCE,
+                        EnumeratorStringDescriptor.INSTANCE,
+                        256,
+                        0,
+                        StorageLockContext()
+                    )
+                }, path
+            )
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    private fun findKind(classRoot: VirtualFile): PersistentLibraryKind<*>? =
+        if (cleanCache) {
+            null
+        } else {
+            val persistentHashMap = rootKindMap.value
+            val value = synchronized(persistentHashMap) {
+                persistentHashMap[classRoot.url]
+            }
+            value?.let { kindId ->
+                when(kindId) {
+                    // as KotlinJvmEffectiveLibraryKind is a fake library kind
+                    KotlinJvmEffectiveLibraryKind.kindId -> KotlinJvmEffectiveLibraryKind
+                    else -> {
+                        val findKindById = LibraryKindRegistry.getInstance().findKindById(kindId)
+                        findKindById as? PersistentLibraryKind<*>
+                    }
+                }
+            }
+        }
+
+    private fun storeKind(classRoot: VirtualFile, detectedKind: LibraryKind?) {
+        if (!cleanCache && detectedKind != null && detectedKind != NEEDS_TO_BE_CLARIFIED_KIND) {
+            val persistentHashMap = rootKindMap.value
+            synchronized(persistentHashMap) {
+                persistentHashMap.put(classRoot.url, detectedKind.kindId)
+            }
+            persistentHashMap.force()
+        }
+    }
+
     fun getEffectiveKind(library: Library): PersistentLibraryKind<*>? {
-        library as LibraryEx
+        require(library is LibraryEx)
 
         if (library.isDisposed) {
             return null
@@ -48,23 +112,35 @@ class LibraryEffectiveKindProvider {
                 var platformKind = classRoot.getUserData(LIBRARY_KIND_KEY)
 
                 if (platformKind == null) {
-                    LibraryKindScanner.runScannerOutsideScanningSession(classRoots)
-                    platformKind = classRoot.getUserData(LIBRARY_KIND_KEY)
+                    findKind(classRoot)?.let {
+                        platformKind = it
+                        classRoot.putUserData(LIBRARY_KIND_KEY, it)
+                    }
+
+                    if (platformKind == null) {
+                        LibraryKindScanner.runScannerOutsideScanningSession(classRoots)
+                        platformKind = classRoot.getUserData(LIBRARY_KIND_KEY)
+                        storeKind(classRoot, platformKind)
+                    }
                 }
 
                 if (platformKind == NEEDS_TO_BE_CLARIFIED_KIND) {
                     val matchingPlatformKind = IdePlatformKindProjectStructure.getLibraryPlatformKind(classRoot)
                         ?: JvmPlatforms.defaultJvmPlatform.idePlatformKind
 
-                    val detectedKind = IdePlatformKindProjectStructure.getLibraryKind(matchingPlatformKind)
+                    val detectedKind: PersistentLibraryKind<*>? = IdePlatformKindProjectStructure.getLibraryKind(matchingPlatformKind)
                     classRoot.putUserData(LIBRARY_KIND_KEY, detectedKind)
-                    return detectedKind
+                    storeKind(classRoot, detectedKind)
+                    platformKind = detectedKind
                 }
 
-                return platformKind
+                platformKind?.nonJvmOrNull()
             }
         }
     }
+
+    // Kotlin JVM kind is expected to be cached to prevent reevaluation, but is not expected to be exposed to not break non-Kotlin kinds
+    private fun PersistentLibraryKind<*>.nonJvmOrNull() = this.takeIf { it !is KotlinJvmEffectiveLibraryKind }
 
     // it's assumed that this scanner runs everytime when library has been changed and library kind will be updated
     class LibraryKindScanner : IndexableFileScanner {
@@ -138,4 +214,26 @@ class LibraryEffectiveKindProvider {
             }
         }
     }
+
+    internal fun cleanCache() {
+        cleanCache = true
+        rootKindMap.value.closeAndClean()
+    }
+
+    override fun dispose() {
+        if (!cleanCache && rootKindMap.isInitialized()) {
+            rootKindMap.value.force()
+        }
+    }
+}
+
+class LibraryEffectiveKindProviderInvalidator: CachesInvalidator() {
+    override fun invalidateCaches() {
+        ProjectManager.getInstance().openProjects.forEach { project ->
+            if (!project.isDefault) {
+                project.service<LibraryEffectiveKindProvider>().cleanCache()
+            }
+        }
+    }
+
 }
