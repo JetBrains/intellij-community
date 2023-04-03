cdc051adec2e8794fc28a69a96faef7c75ecfed4
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2023-04-03 12:33:19 +0200

    [mpp, tests] Drop old MPP test suites (2/2)
    This commit removes the testdata for test cases removed in the previous
    commit, as well as cleans up some obsolete testdata folders that were
    unused (preivously it was hard to notice them)
    
    (cherry picked from commit fbffb2830134dc42edbc4de5eb6f8934846d1209)
    
    GitOrigin-RevId: ef725160a7636edb93033c21b18ff756c07c7794

--- plugins/kotlin/idea/tests/testData/gradle/androidImportAndCheckHighlighting/androidAppDependingOnMppAndroidLibraryWithOnlyAndroidTarget/mylibrary/src/androidMain/kotlin/AndroidMainApi.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mylibrary
-
-import androidx.fragment.app.Fragment
-
-interface <lineMarker descr="Is implemented by     MainActivity  Click or press ... to navigate">AndroidMainApi</lineMarker> {
-    fun <lineMarker descr="Is implemented in     com.example.app.MainActivity">fragment</lineMarker>(fragment: Fragment)
-}
--- plugins/kotlin/idea/tests/testData/gradle/androidImportAndCheckHighlighting/androidAppDependingOnMppAndroidLibraryWithOnlyAndroidTarget/mylibrary/src/commonMain/kotlin/CommonMainApi.kt
+++
@@ -1,4 +0,0 @@
-package com.example.mylibrary
-
-interface <lineMarker descr="Is implemented by     MainActivity  Click or press ⌥⌘Bto navigate">CommonMainApi</lineMarker>
-
