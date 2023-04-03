e3eb0022b8a7de1bc62754cdc4a52d42d57167b1
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2023-04-03 16:06:41 +0300

    [UI] IDEA-316107 Compact Mode: Find/Replace popup
    (cherry picked from commit c56976d1193f514a29e25a863a99abb8df8fe8cd)
    
    IJ-CR-105784
    
    GitOrigin-RevId: e279011cdec0b85de42280e6fe8e05fe2a3feb91

--- platform/platform-impl/src/com/intellij/ide/ui/laf/LafManagerImpl.kt
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/LafManagerImpl.kt
@@ -696,6 +696,10 @@ class LafManagerImpl : LafManager(), PersistentStateComponent<Element>, Disposab
       defaults.put(JBUI.CurrentTheme.BigPopup.advertiserBorderInsetsKey(), cmInsets(4, 20, 5, 20))
       defaults.put(JBUI.CurrentTheme.CompletionPopup.Advertiser.borderInsetsKey(), cmInsets(2, 12, 2, 8))
       defaults.put(JBUI.CurrentTheme.CompletionPopup.selectionInnerInsetsKey(), cmInsets(0, 2, 0, 2))
+      defaults.put(JBUI.CurrentTheme.FindPopup.scopesPanelInsetsKey(), cmInsets(1, 20))
+      defaults.put(JBUI.CurrentTheme.FindPopup.bottomPanelInsetsKey(), cmInsets(1, 18))
+      defaults.put(JBUI.CurrentTheme.ComplexPopup.headerInsetsKey(), cmInsets(10, 20, 8, 15))
+      defaults.put(JBUI.CurrentTheme.ComplexPopup.textFieldInputInsetsKey(), cmInsets(4, 2))
       // status bar
       defaults.put(JBUI.CurrentTheme.StatusBar.Widget.insetsKey(), cmInsets(4, 8, 3, 8))
       defaults.put(JBUI.CurrentTheme.StatusBar.Breadcrumbs.navBarInsetsKey(), cmInsets(1, 0, 1, 4))
