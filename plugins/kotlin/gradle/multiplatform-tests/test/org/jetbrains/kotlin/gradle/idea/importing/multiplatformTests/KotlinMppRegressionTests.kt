3170a13be74a7d2d3f63a06b84bbb5661c0d3d42
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2023-04-03 18:35:49 +0200

    [mpp, tests] Minor: remove 'new' word from the new tests infrastructure
    Old one is dropped, so new is not new anymore
    
    (cherry picked from commit a5d6cf553a6cd82416af44f03ee944a9714d0dfb)
    
    GitOrigin-RevId: ba4ef9fcef7053d0112ecd530e88f9cbab54cc00

---
+++ plugins/kotlin/gradle/multiplatform-tests/test/org/jetbrains/kotlin/gradle/idea/importing/multiplatformTests/KotlinMppRegressionTests.kt
@@ -0,0 +1,59 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.gradle.idea.importing.multiplatformTests
+
+import org.jetbrains.kotlin.gradle.multiplatformTests.AbstractKotlinMppGradleImportingTest
+import org.jetbrains.kotlin.gradle.multiplatformTests.TestConfigurationDslScope
+import org.jetbrains.kotlin.gradle.multiplatformTests.testFeatures.checkers.highlighting.HighlightingChecker
+import org.jetbrains.kotlin.gradle.multiplatformTests.testFeatures.checkers.orderEntries.OrderEntriesChecker
+import org.jetbrains.kotlin.test.TestMetadata
+import org.junit.Test
+
+@TestMetadata("multiplatform/regress")
+class KotlinMppRegressionTests : AbstractKotlinMppGradleImportingTest() {
+    override fun TestConfigurationDslScope.defaultTestConfiguration() {
+        hideStdlib = true
+        hideKotlinTest = true
+        hideKotlinNativeDistribution = true
+    }
+
+    @Test
+    fun testKT42381BadDependencyOnArtifactInsteadOfSources() {
+        doTest {
+            onlyModules(".*p1\\.jvm.*")
+            onlyCheckers(OrderEntriesChecker)
+        }
+    }
+
+    @Test
+    fun testKT42392BadDependencyOnForeignCommonTest() {
+        doTest {
+            onlyModules(""".*p1\.main.*""")
+            onlyCheckers(OrderEntriesChecker)
+        }
+    }
+
+    @Test
+    fun testKT46417NativePlatformTestSourceSets() {
+        doTest {
+            onlyModules(""".*p2\.(iosTest|iosX64Test|iosArm64Test|linuxX64Test|linuxArm64Test).*""")
+            onlyCheckers(OrderEntriesChecker)
+        }
+    }
+
+    @Test
+    fun testKtij20056lowerTransitiveStdlibInPlatformSourceSet() {
+        doTest {
+            onlyModules(""".*p.jvmMain.*""")
+            hideStdlib = false
+            onlyCheckers(OrderEntriesChecker)
+        }
+    }
+
+    @Test
+    fun testKtij22345SyntheticJavaProperties() {
+        doTest {
+            onlyCheckers(HighlightingChecker)
+            hideLineMarkers = true
+        }
+    }
+}
