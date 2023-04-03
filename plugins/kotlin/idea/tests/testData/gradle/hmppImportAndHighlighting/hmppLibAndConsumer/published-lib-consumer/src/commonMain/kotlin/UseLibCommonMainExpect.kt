965d7f5261eb111e656fa310a17cdfd11cf20247
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2023-04-03 12:33:19 +0200

    [mpp, tests] Drop old MPP test suites (2/2)
    This commit removes the testdata for test cases removed in the previous
    commit, as well as cleans up some obsolete testdata folders that were
    unused (preivously it was hard to notice them)
    
    (cherry picked from commit fbffb2830134dc42edbc4de5eb6f8934846d1209)
    
    GitOrigin-RevId: 276d6fa76fc43b488ad4bbd7aaf11ffe6e062755

--- plugins/kotlin/idea/tests/testData/gradle/hmppImportAndHighlighting/hmppLibAndConsumer/published-lib-consumer/src/commonMain/kotlin/UseLibCommonMainExpect.kt
+++
@@ -1,8 +0,0 @@
-package com.h0tk3y.hmpp.klib.demo.app
-
-import com.h0tk3y.hmpp.klib.demo.*
-
-fun useExpect(e: <!HIGHLIGHTING("severity='ERROR'; descr='[UNRESOLVED_REFERENCE] Unresolved reference: LibCommonMainExpect'")!>LibCommonMainExpect<!>) {
-    println(<!HIGHLIGHTING("severity='ERROR'; descr='[DEBUG] Resolved to error element'")!>e<!>.<!HIGHLIGHTING("severity='ERROR'; descr='[DEBUG] Reference is not resolved to anything, but is not marked unresolved'")!>libCommonMainExpectFun<!>())
-//    println(e.additionalFunInActual()) // won't resolve
-}
--- plugins/kotlin/idea/tests/testData/gradle/importAndCheckHighlighting/hmppStdlibUsageInAllBackends/p1/src/commonMain/kotlin/KTIJ2447CommonMain.kt
+++
@@ -1,5 +0,0 @@
-object KTIJ2447CommonMain {
-    val a = hashMapOf<String, String>()
-    val b = arrayListOf("").associateBy { }
-    val c = buildString { append(342) }
-}
