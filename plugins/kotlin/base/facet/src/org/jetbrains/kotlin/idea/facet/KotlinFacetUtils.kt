fbb09cc94370080421626d824b91d7ba8b6f1c5d
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-04-03 15:08:31 +0200

    [KTIJ-24976] Ignore 'main' and 'metaInfo' arguments as additionalArguments
    GitOrigin-RevId: 5eb83689233c2d0bc4e36ecd77e9d3b484c1a848

--- plugins/kotlin/base/facet/src/org/jetbrains/kotlin/idea/facet/KotlinFacetUtils.kt
+++ plugins/kotlin/base/facet/src/org/jetbrains/kotlin/idea/facet/KotlinFacetUtils.kt
@@ -184,7 +184,10 @@ fun applyCompilerArgumentsToFacet(
             K2NativeCompilerArguments::produce.name,
             K2NativeCompilerArguments::target.name,
             K2NativeCompilerArguments::shortModuleName.name,
-            K2NativeCompilerArguments::noendorsedlibs.name
+            K2NativeCompilerArguments::noendorsedlibs.name,
+
+            K2JSCompilerArguments::main.name,
+            K2JSCompilerArguments::metaInfo.name,
         )
 
         fun exposeAsAdditionalArgument(property: KProperty1<CommonCompilerArguments, Any?>) =
