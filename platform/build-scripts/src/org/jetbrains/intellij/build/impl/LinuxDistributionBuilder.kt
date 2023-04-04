07f7aa2624e8ff1c2b3ee8a83681b74de299008d
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2023-04-04 17:35:11 +0200

    CPP-31989 IJI-971 updating executable permissions for .snap
    GitOrigin-RevId: b028734c423d51424b50e6ae9eb5d3b911188b52

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/LinuxDistributionBuilder.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/LinuxDistributionBuilder.kt
@@ -5,10 +5,9 @@ import com.intellij.diagnostic.telemetry.useWithScope
 import com.intellij.diagnostic.telemetry.useWithScope2
 import com.intellij.openapi.util.SystemInfoRt
 import com.intellij.openapi.util.io.NioFiles
-import io.opentelemetry.api.common.AttributeKey
-import io.opentelemetry.api.common.Attributes
 import io.opentelemetry.api.trace.Span
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.jetbrains.intellij.build.*
 import org.jetbrains.intellij.build.TraceManager.spanBuilder
@@ -20,7 +19,6 @@ import java.nio.file.Files
 import java.nio.file.NoSuchFileException
 import java.nio.file.Path
 import java.nio.file.StandardCopyOption
-import java.nio.file.attribute.PosixFilePermissions
 import kotlin.io.path.name
 import kotlin.io.path.nameWithoutExtension
 import kotlin.time.Duration.Companion.minutes
@@ -231,21 +229,12 @@ class LinuxDistributionBuilder(override val context: BuildContext,
           |# </${snapcraftConfig.name}>
         """.trimMargin())
 
-        FileSet(unixSnapDistPath)
-          .include("bin/*.sh")
-          .include("bin/*.py")
-          .include("bin/fsnotifier*")
-          .enumerate().forEach(::makeFileExecutable)
-
-        FileSet(runtimeDir)
-          .include("jbr/bin/*")
-          .enumerate().forEach(::makeFileExecutable)
-
-        if (!customizer.extraExecutables.isEmpty()) {
-          for (distPath in listOf(unixSnapDistPath, context.paths.distAllDir)) {
-            val fs = FileSet(distPath)
-            customizer.extraExecutables.forEach(fs::include)
-            fs.enumerateNoAssertUnusedPatterns().forEach(::makeFileExecutable)
+        withContext(Dispatchers.IO) {
+          val executableFilesMatchers = generateExecutableFilesMatchers(includeRuntime = true, arch).keys
+          for (distPath in listOf(unixSnapDistPath, context.paths.distAllDir, runtimeDir)) {
+            launch {
+              updateExecutablePermissions(distPath, executableFilesMatchers)
+            }
           }
         }
         validateProductJson(jsonText = generateProductJson(unixSnapDistPath, arch = arch, "jbr/bin/java", context),
@@ -369,12 +358,6 @@ private fun generateVersionMarker(unixDistPath: Path, context: BuildContext) {
 private fun artifactName(buildContext: BuildContext, suffix: String?): String {
   val baseName = buildContext.productProperties.getBaseArtifactName(buildContext.applicationInfo, buildContext.buildNumber)
   return "$baseName$suffix.tar.gz"
-}
-
-private fun makeFileExecutable(file: Path) {
-  Span.current().addEvent("set file permission to 0755", Attributes.of(AttributeKey.stringKey("file"), file.toString()))
-  @Suppress("SpellCheckingInspection")
-  Files.setPosixFilePermissions(file, PosixFilePermissions.fromString("rwxr-xr-x"))
 }
 
 private fun copyScript(sourceFile: Path,
