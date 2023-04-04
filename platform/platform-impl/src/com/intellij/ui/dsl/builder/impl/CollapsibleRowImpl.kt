92153b2ba368c198d8a012aa0a73673dbab31032
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2023-04-04 09:22:36 +0200

    IDEA-315584 Simplify gaps calculation
    GitOrigin-RevId: b62af7a3aee92e708d567675f66e5699560cf030

--- platform/platform-impl/src/com/intellij/ui/dsl/builder/impl/CollapsibleRowImpl.kt
+++ platform/platform-impl/src/com/intellij/ui/dsl/builder/impl/CollapsibleRowImpl.kt
@@ -8,8 +8,7 @@ import com.intellij.openapi.util.NlsContexts
 import com.intellij.openapi.util.text.TextWithMnemonic
 import com.intellij.ui.Expandable
 import com.intellij.ui.dsl.builder.*
-import com.intellij.ui.dsl.gridLayout.UnscaledGaps
-import com.intellij.ui.dsl.gridLayout.unscale
+import com.intellij.ui.dsl.gridLayout.toUnscaledGaps
 import org.jetbrains.annotations.ApiStatus
 import java.awt.Font
 import java.awt.event.ActionEvent
@@ -52,9 +51,7 @@ internal class CollapsibleRowImpl(dialogPanelConfig: DialogPanelConfig,
   init {
     collapsibleTitledSeparator.setLabelFocusable(true)
     (collapsibleTitledSeparator.label.border as? EmptyBorder)?.borderInsets?.let {
-      val unscaled = it.unscale()
-      collapsibleTitledSeparator.putClientProperty(DslComponentProperty.VISUAL_PADDINGS,
-                                                   UnscaledGaps(top = unscaled.top, left = unscaled.left, bottom = unscaled.bottom))
+      collapsibleTitledSeparator.putClientProperty(DslComponentProperty.VISUAL_PADDINGS, it.toUnscaledGaps().copy(right = 0))
     }
 
     collapsibleTitledSeparator.label.putClientProperty(Expandable::class.java, object : Expandable {
