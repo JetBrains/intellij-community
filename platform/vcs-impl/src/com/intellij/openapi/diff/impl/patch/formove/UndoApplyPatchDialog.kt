19ea91a827b8657b153d3c7060f99883b46764be
Aleksey Pivovarov <AMPivovarov@gmail.com>
2023-04-05 16:16:45 +0200

    vcs: convert UndoApplyPatchDialog to UI DSL
    GitOrigin-RevId: 55572655d2284b6b9a894ab7861e0037c4d401f3

--- platform/vcs-impl/src/com/intellij/openapi/diff/impl/patch/formove/UndoApplyPatchDialog.kt
+++ platform/vcs-impl/src/com/intellij/openapi/diff/impl/patch/formove/UndoApplyPatchDialog.kt
@@ -7,45 +7,43 @@ import com.intellij.openapi.vcs.FilePath
 import com.intellij.openapi.vcs.VcsBundle
 import com.intellij.openapi.vcs.changes.ui.ChangesTree
 import com.intellij.openapi.vcs.changes.ui.ChangesTreeImpl
-import com.intellij.ui.ScrollPaneFactory
-import com.intellij.ui.components.JBLabel
+import com.intellij.ui.dsl.builder.Align
+import com.intellij.ui.dsl.builder.panel
 import com.intellij.util.ui.UIUtil
-import com.intellij.xml.util.XmlStringUtil
-import java.awt.BorderLayout
 import javax.swing.JComponent
-import javax.swing.JLabel
-import javax.swing.JPanel
 
-internal class UndoApplyPatchDialog(private val myProject: Project,
-                                    filePaths: List<FilePath>,
-                                    shouldInformAboutBinaries: Boolean) : DialogWrapper(myProject, true) {
-  private val myFailedFilePaths: List<FilePath>
-  private val myShouldInformAboutBinaries: Boolean
+internal class UndoApplyPatchDialog(
+  private val project: Project,
+  private val failedFilePaths: List<FilePath>,
+  private val shouldInformAboutBinaries: Boolean
+) : DialogWrapper(project, true) {
 
   init {
     title = VcsBundle.message("patch.apply.partly.failed.title")
     setOKButtonText(VcsBundle.message("patch.apply.rollback.action"))
-    myFailedFilePaths = filePaths
-    myShouldInformAboutBinaries = shouldInformAboutBinaries
     init()
   }
 
-  override fun createCenterPanel(): JComponent? {
-    val panel = JPanel(BorderLayout())
-    val numFiles = myFailedFilePaths.size
-    val labelsPanel = JPanel(BorderLayout())
-    val infoLabel: JLabel = JBLabel(XmlStringUtil.wrapInHtml(VcsBundle.message("patch.apply.rollback.prompt", numFiles)))
-    labelsPanel.add(infoLabel, BorderLayout.NORTH)
-    if (myShouldInformAboutBinaries) {
-      val warningLabel = JLabel(VcsBundle.message("patch.apply.rollback.will.not.affect.binaries.info"))
-      warningLabel.icon = UIUtil.getBalloonWarningIcon()
-      labelsPanel.add(warningLabel, BorderLayout.CENTER)
+  override fun createCenterPanel(): JComponent {
+    val numFiles = failedFilePaths.size
+    return panel {
+      row {
+        label(VcsBundle.message("patch.apply.rollback.prompt", numFiles))
+      }
+      if (shouldInformAboutBinaries) {
+        row {
+          label(VcsBundle.message("patch.apply.rollback.will.not.affect.binaries.info", numFiles)).applyToComponent {
+            icon = UIUtil.getBalloonWarningIcon()
+          }
+        }
+      }
+      if (numFiles > 0) {
+        val browser: ChangesTree = ChangesTreeImpl.FilePaths(project, false, false, failedFilePaths)
+        row {
+          scrollCell(browser)
+            .align(Align.FILL)
+        }.resizableRow()
+      }
     }
-    panel.add(labelsPanel, BorderLayout.NORTH)
-    if (numFiles > 0) {
-      val browser: ChangesTree = ChangesTreeImpl.FilePaths(myProject, false, false, myFailedFilePaths)
-      panel.add(ScrollPaneFactory.createScrollPane(browser), BorderLayout.CENTER)
-    }
-    return panel
   }
 }
