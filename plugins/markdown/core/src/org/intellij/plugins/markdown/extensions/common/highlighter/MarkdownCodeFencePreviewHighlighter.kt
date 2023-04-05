a90df3cf61768cbfb1142610f824b5ab89ed7d51
Ivan Posti <ivan.posti@jetbrains.com>
2023-04-05 17:48:43 +0200

    [Markdown] Rewrite html generation for code fences
    Should fix IDEA-315549.
    
    GitOrigin-RevId: da7723b516b22d2a2b20873153bd2bdbe1dd01e7

--- plugins/markdown/core/src/org/intellij/plugins/markdown/extensions/common/highlighter/MarkdownCodeFencePreviewHighlighter.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/extensions/common/highlighter/MarkdownCodeFencePreviewHighlighter.kt
@@ -1,23 +1,20 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.intellij.plugins.markdown.extensions.common.highlighter
 
-import com.intellij.lang.Language
-import com.intellij.markdown.utils.lang.HtmlSyntaxHighlighter
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.ui.ColorUtil
-import org.intellij.markdown.MarkdownTokenTypes
 import org.intellij.markdown.ast.ASTNode
-import org.intellij.markdown.html.HtmlGenerator
 import org.intellij.plugins.markdown.extensions.CodeFenceGeneratingProvider
 import org.intellij.plugins.markdown.extensions.jcef.commandRunner.CommandRunnerExtension
 import org.intellij.plugins.markdown.injection.aliases.CodeFenceLanguageGuesser
 import org.intellij.plugins.markdown.ui.preview.html.DefaultCodeFenceGeneratingProvider
+import org.jetbrains.annotations.ApiStatus
 
-internal class MarkdownCodeFencePreviewHighlighter : CodeFenceGeneratingProvider {
+@ApiStatus.Internal
+class MarkdownCodeFencePreviewHighlighter: CodeFenceGeneratingProvider {
   private val cacheManager
     get() = HtmlCacheManager.getInstance()
 
-  private val currentFile: ThreadLocal<VirtualFile?> = ThreadLocal()
+  private val currentFile = ThreadLocal<VirtualFile?>()
 
   override fun isApplicable(language: String): Boolean {
     return CodeFenceLanguageGuesser.guessLanguageForInjection(language) != null
@@ -31,8 +28,8 @@ internal class MarkdownCodeFencePreviewHighlighter : CodeFenceGeneratingProvider
   }
 
   override fun generateHtml(language: String, raw: String, node: ASTNode): String {
-    val lang = CodeFenceLanguageGuesser.guessLanguageForInjection(language)
-    if (lang == null) {
+    val injectedLanguage = CodeFenceLanguageGuesser.guessLanguageForInjection(language)
+    if (injectedLanguage == null) {
       return DefaultCodeFenceGeneratingProvider.escape(raw)
     }
     val cacheKey = cacheManager.obtainCacheKey(raw, language)
@@ -40,65 +37,21 @@ internal class MarkdownCodeFencePreviewHighlighter : CodeFenceGeneratingProvider
     if (cached != null) {
       return cached
     }
-    val text = render(lang, raw, node)
+    val highlightedRanges = collectHighlightedChunks(injectedLanguage, raw)
+    val text = buildHighlightedFenceContent(
+      raw,
+      highlightedRanges,
+      node,
+      useAbsoluteOffsets = true,
+      additionalLineProcessor = ::processCodeLine
+    )
     cacheManager.cacheHtml(cacheKey, text)
     return text
   }
 
-  private fun render(lang: Language, text: String, node: ASTNode): String {
-    val highlightTokens = mutableMapOf<IntRange, String>()
-    HtmlSyntaxHighlighter.parseContent(null, lang, text) { content, intRange, color ->
-      if (color != null) {
-        highlightTokens[intRange] = "<span style=\"color:${ColorUtil.toHtmlColor(color)}\">${
-          DefaultCodeFenceGeneratingProvider.escape(content)
-        }</span>"
-      }
-    }
-
-    // Mannually walk over each line and recalculate line offsets
-    val baseOffset = DefaultCodeFenceGeneratingProvider.calcCodeFenceContentBaseOffset(node)
-    val lines = ArrayList<String>()
-    var left = 0
-    for (line in text.lines()) {
-      val targets = highlightTokens.entries.filter { (range, _) ->
-        range.first >= left && range.last <= left + line.length
-      }.map { (range, replacement) -> (range.shift(-left)) to replacement }
-      val right = left + line.length + 1
-      lines.add(buildString {
-        append("<span ${HtmlGenerator.SRC_ATTRIBUTE_NAME}='${left + baseOffset}..${right + baseOffset}'>")
-        if (lines.isNotEmpty()) {
-          // skip first line processing since there is always run marker for whole block
-          append(processCodeLine(line))
-        }
-        appendWithReplacements(line, targets, this)
-        append("</span>")
-      })
-      left = right
-    }
-    return lines.joinToString(
-      separator = "\n",
-      prefix = "<span ${HtmlGenerator.SRC_ATTRIBUTE_NAME}='${node.startOffset}..${baseOffset}'/>",
-      postfix = node.children.find { it.type == MarkdownTokenTypes.CODE_FENCE_END }?.let {
-        "<span ${HtmlGenerator.SRC_ATTRIBUTE_NAME}='${it.startOffset}..${it.endOffset}'/>"
-      } ?: ""
-    )
+  private fun processCodeLine(line: String): String {
+    val file = currentFile.get() ?: return ""
+    val runner = CommandRunnerExtension.getRunnerByFile(file)
+    return runner?.processCodeLine(line, true).orEmpty()
   }
-
-  private fun IntRange.shift(value: Int): IntRange = (first + value)..(last + value)
-
-  private fun appendWithReplacements(line: String, targets: List<Pair<IntRange, String>>, builder: StringBuilder) {
-    var actualLine = line
-    var left = 0
-    for ((range, replacement) in targets.sortedBy { it.first.first }) {
-      builder.append(DefaultCodeFenceGeneratingProvider.escape(actualLine.substring(0, range.first - left)))
-      builder.append(replacement)
-      actualLine = actualLine.substring(range.last - left)
-      left = range.last
-    }
-    builder.append(DefaultCodeFenceGeneratingProvider.escape(actualLine))
-  }
-
-  private fun processCodeLine(rawCodeLine: String): String = currentFile.get()?.let { file ->
-    CommandRunnerExtension.getRunnerByFile(file)?.processCodeLine(rawCodeLine, true)
-  } ?: ""
 }
