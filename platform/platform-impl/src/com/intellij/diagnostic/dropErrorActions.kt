3377e20623058e8c261014d42d31c4710d272f38
Leonid Shalupov <leonid@shalupov.com>
2023-04-06 15:46:51 +0200

    freeze simulation action
    GitOrigin-RevId: 96c268356d0e45ad14a5852f07052f4d530772c4

--- platform/platform-impl/src/com/intellij/diagnostic/dropErrorActions.kt
+++ platform/platform-impl/src/com/intellij/diagnostic/dropErrorActions.kt
@@ -8,6 +8,9 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Attachment
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.ui.InputValidator
+import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.util.TimeoutUtil
 import java.awt.event.ActionEvent.SHIFT_MASK
 import java.util.*
@@ -79,5 +82,29 @@ internal class DropAnOutOfMemoryErrorAction : DumbAwareAction("Drop an OutOfMemo
     else {
       throw OutOfMemoryError("foo Metaspace foo")
     }
+  }
+}
+
+@Suppress("HardCodedStringLiteral")
+internal class SimulateFreeze : DumbAwareAction("Simulate a Freeze") {
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val durationString = Messages.showInputDialog(
+      e.project,
+      "Enter freeze duration in ms",
+      "Freeze Simulator",
+      null,
+      "",
+      object : InputValidator {
+        override fun checkInput(inputString: String?): Boolean = StringUtil.parseInt(inputString, -1) > 0
+        override fun canClose(inputString: String?): Boolean = StringUtil.parseInt(inputString, -1) > 0
+      }) ?: return
+    simulatedFreeze(durationString.toLong())
+  }
+
+  // Keep it a function to detect it in EA
+  private fun simulatedFreeze(ms: Long) {
+    Thread.sleep(ms.toLong())
   }
 }
