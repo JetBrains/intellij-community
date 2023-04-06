945c4636e60deaaa6836fc841ca41a791972bdec
Anna Kozlova <anna.kozlova@jetbrains.com>
2023-04-06 09:47:25 +0200

    [ui] ensure browse listeners are disposed as soon as the corresponding text field is removed
    IDEA-316267
    
    GitOrigin-RevId: ccf319564ce0b35238a281649d83d9f48c75d1d7

--- platform/platform-api/src/com/intellij/ui/TextFieldWithHistoryWithBrowseButton.java
+++ platform/platform-api/src/com/intellij/ui/TextFieldWithHistoryWithBrowseButton.java
@@ -1,11 +1,13 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ui;
 
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.fileChooser.FileChooserFactory;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.ComponentWithBrowseButton;
 import com.intellij.openapi.ui.TextComponentAccessor;
+import com.intellij.openapi.util.Disposer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -13,6 +15,7 @@ import static com.intellij.openapi.util.NlsContexts.DialogTitle;
 import static com.intellij.openapi.util.NlsContexts.Label;
 
 public class TextFieldWithHistoryWithBrowseButton extends ComponentWithBrowseButton<TextFieldWithHistory> {
+  private final Disposable myDisposable = Disposer.newDisposable();
   public TextFieldWithHistoryWithBrowseButton() {
     super(new TextFieldWithHistory(), null);
   }
@@ -24,7 +27,7 @@ public class TextFieldWithHistoryWithBrowseButton extends ComponentWithBrowseBut
                                       FileChooserDescriptor fileChooserDescriptor,
                                       TextComponentAccessor<? super TextFieldWithHistory> accessor) {
     super.addBrowseFolderListener(title, description, project, fileChooserDescriptor, accessor);
-    FileChooserFactory.getInstance().installFileCompletion(getChildComponent().getTextEditor(), fileChooserDescriptor, false, project);
+    FileChooserFactory.getInstance().installFileCompletion(getChildComponent().getTextEditor(), fileChooserDescriptor, false, myDisposable);
   }
 
   @Override
@@ -35,7 +38,13 @@ public class TextFieldWithHistoryWithBrowseButton extends ComponentWithBrowseBut
                                       TextComponentAccessor<? super TextFieldWithHistory> accessor,
                                       boolean autoRemoveOnHide) {
     addBrowseFolderListener(title, description, project, fileChooserDescriptor, accessor);
-    FileChooserFactory.getInstance().installFileCompletion(getChildComponent().getTextEditor(), fileChooserDescriptor, false, project);
+    FileChooserFactory.getInstance().installFileCompletion(getChildComponent().getTextEditor(), fileChooserDescriptor, false, myDisposable);
+  }
+
+  @Override
+  public void removeNotify() {
+    super.removeNotify();
+    Disposer.dispose(myDisposable);
   }
 
   public String getText() {
