cc8661c04b0d02b2b1ce93e026e600c6fc4503c5
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2023-04-06 11:26:49 +0200

    [jvm] Migrate UnstableApiInspection tests to JvmInspectionTests API
    GitOrigin-RevId: c5016bc1991720d45400ef89eb4eae955cb4fa86

--- jvm/jvm-analysis-kotlin-tests/testData/codeInspection/unstableApiUsage/scheduledForRemoval/pkg/AnnotatedEnum.java
+++
@@ -1,9 +0,0 @@
-package pkg;
-
-import org.jetbrains.annotations.ApiStatus;
-
-@ApiStatus.ScheduledForRemoval(inVersion = "123.456")
-public enum AnnotatedEnum {
-  NON_ANNOTATED_VALUE_IN_ANNOTATED_ENUM,
-  @ApiStatus.ScheduledForRemoval(inVersion = "123.456") ANNOTATED_VALUE_IN_ANNOTATED_ENUM
-}
