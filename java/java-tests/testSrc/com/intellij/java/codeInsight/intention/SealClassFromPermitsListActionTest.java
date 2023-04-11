da5b6df3bd21f4ac2898edcd4fe82d59eb7be492
Bas Leijdekkers <basleijdekkers@gmail.com>
2023-04-11 17:11:20 +0200

    Java: don't offer to make abstract class extending sealed interface 'final' and add test (IJ-CR-102367)
    GitOrigin-RevId: 327dd647f2bab8ad2ceac1726e8cc8526faa277a

--- java/java-tests/testSrc/com/intellij/java/codeInsight/intention/SealClassFromPermitsListActionTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/intention/SealClassFromPermitsListActionTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.java.codeInsight.intention;
 
 import com.intellij.codeInsight.intention.impl.SealClassFromPermitsListAction;
@@ -20,6 +20,12 @@ public class SealClassFromPermitsListActionTest extends LightJavaCodeInsightFixt
     myFixture.addClass("package foo; class Child extends Parent {}");
     myFixture.configureByText("Parent.java", "package foo; sealed class Parent permits <caret>Child {}");
     doTest(false);
+  }
+
+  public void testSubInterface() {
+    myFixture.addClass("package foo; interface Child extends Parent {}");
+    myFixture.configureByText("Parent.java", "package foo; sealed interface Parent permits <caret>Child {}");
+    assertTrue(myFixture.filterAvailableIntentions("Make 'Child' final").isEmpty());
   }
 
   private void doTest(boolean isAvailable) {
