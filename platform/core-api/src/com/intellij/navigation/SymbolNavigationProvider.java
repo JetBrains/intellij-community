3135aa4093773261b9bb5cad244f8d1587f0078b
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-04-11 18:09:58 +0200

    clean up
    GitOrigin-RevId: bac9cbc5f906766f20b444b0c6efcf0eacb4c09d

--- platform/core-api/src/com/intellij/navigation/SymbolNavigationProvider.java
+++ platform/core-api/src/com/intellij/navigation/SymbolNavigationProvider.java
@@ -1,9 +1,9 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.navigation;
 
 import com.intellij.model.Symbol;
 import com.intellij.openapi.project.Project;
-import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.ApiStatus.Experimental;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
@@ -14,7 +14,7 @@ import java.util.Collection;
  *
  * @see NavigatableSymbol
  */
-@ApiStatus.Experimental
+@Experimental
 public interface SymbolNavigationProvider {
 
   @NotNull
