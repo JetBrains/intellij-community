136cca3a198126ce7872dac47b27c796576ae044
Eugene Zhuravlev <eugene.zhuravlev@jetbrains.com>
2023-04-11 19:40:41 +0200

    treat file/jar copying problems as a build error and not as corrupted caches (IDEA-317650)
    GitOrigin-RevId: 29a489ab7c56042ee03000d696dee32e72fdcc0a

--- jps/jps-builders/src/org/jetbrains/jps/incremental/artifacts/instructions/FileBasedArtifactRootDescriptor.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/artifacts/instructions/FileBasedArtifactRootDescriptor.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.jps.incremental.artifacts.instructions;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -14,6 +14,8 @@ import org.jetbrains.jps.incremental.artifacts.ArtifactBuildTarget;
 import org.jetbrains.jps.incremental.artifacts.ArtifactOutputToSourceMapping;
 import org.jetbrains.jps.incremental.artifacts.IncArtifactBuilder;
 import org.jetbrains.jps.incremental.artifacts.impl.JpsArtifactPathUtil;
+import org.jetbrains.jps.incremental.messages.BuildMessage;
+import org.jetbrains.jps.incremental.messages.CompilerMessage;
 import org.jetbrains.jps.incremental.relativizer.PathRelativizerService;
 
 import java.io.File;
@@ -80,7 +82,15 @@ public class FileBasedArtifactRootDescriptor extends ArtifactRootDescriptor {
       if (logger.isEnabled()) {
         logger.logCompiledFiles(Collections.singletonList(file), IncArtifactBuilder.BUILDER_ID, "Copying file:");
       }
-      myCopyingHandler.copyFile(file, targetFile, context);
+
+      try {
+        myCopyingHandler.copyFile(file, targetFile, context);
+      }
+      catch (IOException e) {
+        context.processMessage(new CompilerMessage(IncArtifactBuilder.getBuilderName(), BuildMessage.Kind.ERROR, CompilerMessage.getTextFromThrowable(e)));
+        return;
+      }
+
       outputConsumer.registerOutputFile(targetFile, Collections.singletonList(filePath));
     }
     else if (LOG.isDebugEnabled()) {
