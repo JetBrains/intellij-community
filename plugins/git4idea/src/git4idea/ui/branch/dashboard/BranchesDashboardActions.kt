7d04ed98a965a0f75d6b686fffa8cf1e8be97989
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2023-04-12 16:41:15 +0200

    [git-branches-dashboard] update "Show My Branches" action on background thread to avoid freezes
    Since 3a1da68d955a2e0cddaa547ad08bc90f5e1ed8d0, VcsLogPersistentIndex.IndexingRequest#markCommits processes all commits under a single lock, instead of taking one lock per commit. This may cause other methods, including VcsLogPersistentIndex#isIndexed(int), to hang waiting for the lock, which makes them undesirable to be called from the EDT.
    
    EA-780060, IDEA-316324
    
    GitOrigin-RevId: c52eae67d892195bfa74eacde41d52ac403756dd

--- plugins/git4idea/src/git4idea/ui/branch/dashboard/BranchesDashboardActions.kt
+++ plugins/git4idea/src/git4idea/ui/branch/dashboard/BranchesDashboardActions.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.ui.branch.dashboard
 
 import com.intellij.dvcs.DvcsUtil.disableActionIfAnyRepositoryIsFresh
@@ -373,7 +373,7 @@ internal object BranchesDashboardActions {
     }
 
     override fun getActionUpdateThread(): ActionUpdateThread {
-      return ActionUpdateThread.EDT
+      return ActionUpdateThread.BGT
     }
 
     override fun update(e: AnActionEvent) {
