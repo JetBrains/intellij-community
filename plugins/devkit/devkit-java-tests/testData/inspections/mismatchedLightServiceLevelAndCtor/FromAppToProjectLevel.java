7930871894e6c2477316cff0b299b9c18214936a
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2023-04-12 14:06:35 +0400

    [devkit] MismatchedLightServiceLevelAndCtorInspection: fix inspection name and editor messages
    IJ-CR-102194
    
    GitOrigin-RevId: 8c01a35a4c885a56c1d632fe6b91e4246428c2d4

--- plugins/devkit/devkit-java-tests/testData/inspections/mismatchedLightServiceLevelAndCtor/FromAppToProjectLevel.java
+++ plugins/devkit/devkit-java-tests/testData/inspections/mismatchedLightServiceLevelAndCtor/FromAppToProjectLevel.java
@@ -1,11 +1,11 @@
 import com.intellij.openapi.components.Service;
 import com.intellij.openapi.project.Project;
 
-@<warning descr="If constructor takes Project, Service.Level.PROJECT is required">Service<caret></warning>(Service.Level.APP)
+@<warning descr="Light service with a constructor that takes a single parameter of type 'com.intellij.openapi.project.Project' must specify '@Service(Service.Level.PROJECT)'">Service<caret></warning>(Service.Level.APP)
 final class MyService {
   private final Project myProject;
 
-  public <warning descr="Application level service requires no-arg constructor or constructor taking Coroutine">MyService</warning>(Project project) {
+  public <warning descr="Application-level service requires a no-arg or single parameter constructor with 'kotlinx.coroutines.CoroutineScope' type">MyService</warning>(Project project) {
     myProject = project;
   }
 }
--- plugins/devkit/devkit-java-tests/testData/inspections/mismatchedLightServiceLevelAndCtor/FromEmptyArrayToProjectLevel.java
+++ plugins/devkit/devkit-java-tests/testData/inspections/mismatchedLightServiceLevelAndCtor/FromEmptyArrayToProjectLevel.java
@@ -1,7 +1,7 @@
 import com.intellij.openapi.components.Service;
 import com.intellij.openapi.project.Project;
 
-@<warning descr="If constructor takes Project, Service.Level.PROJECT is required">Service<caret></warning>({})
+@<warning descr="Light service with a constructor that takes a single parameter of type 'com.intellij.openapi.project.Project' must specify '@Service(Service.Level.PROJECT)'">Service<caret></warning>({})
 final class MyService {
   private final Project myProject;
 
