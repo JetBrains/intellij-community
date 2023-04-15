fccd86451c31e9e7651a923052cb0360a022eb4b
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2023-04-15 20:32:13 +0200

    [Kotlin] Enable KGP-based dependency import by default
    This reverts commit 429f7bb87fac843a8b55abd91029180c4bc28de5.
    We disabled the KGP-based import in the `231` branch,
    because it's release is too close to the 1.8.20 release
    (where the new import is supported).
    
    But in `kt-231-master` it should be enabled by default
    
    GitOrigin-RevId: ba75c2865dc5dfcd8eaf418f67797ef700fe8c4b

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/MultiplePluginVersionGradleImportingTestCase.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/MultiplePluginVersionGradleImportingTestCase.kt
@@ -8,6 +8,8 @@ package org.jetbrains.kotlin.idea.codeInsight.gradle
 
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
 import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.gradle.ProjectInfo
 import org.jetbrains.kotlin.gradle.newTests.TestConfiguration
@@ -20,6 +22,7 @@ import org.jetbrains.kotlin.tooling.core.KotlinToolingVersion
 import org.jetbrains.plugins.gradle.tooling.util.VersionMatcher
 import org.junit.Rule
 import org.junit.runners.Parameterized
+import java.io.File
 
 @Suppress("ACCIDENTAL_OVERRIDE")
 abstract class MultiplePluginVersionGradleImportingTestCase : KotlinGradleImportingTestCase(), TestWithKotlinPluginAndGradleVersions {
@@ -221,6 +224,26 @@ fun MultiplePluginVersionGradleImportingTestCase.kotlinPluginVersionMatches(vers
 fun MultiplePluginVersionGradleImportingTestCase.isStdlibJdk78AddedByDefault() =
     kotlinPluginVersion >= KotlinToolingVersion("1.5.0-M1")
 
+fun MultiplePluginVersionGradleImportingTestCase.isKgpDependencyResolutionEnabled(): Boolean =
+    kotlinPluginVersion >= KotlinToolingVersion("1.8.20-beta-0")
+
 fun MultiplePluginVersionGradleImportingTestCase.gradleVersionMatches(version: String): Boolean {
     return VersionMatcher(GradleVersion.version(gradleVersion)).isVersionMatch(version, true)
+}
+
+// for representation differences between KGP-based and non-KGP-based import
+fun MultiplePluginVersionGradleImportingTestCase.nativeDistLibraryDependency(
+    libraryName: String,
+    libraryPlatform: String?
+): Regex {
+    val namePart = "Kotlin/Native(:| ${kotlinPluginVersion} -) (platform\\.)?$libraryName"
+    val platformPart = " \\| $libraryPlatform".takeIf { libraryPlatform != null } ?: ".*"
+
+    return Regex("$namePart$platformPart")
+}
+
+fun setKgpImportInGradlePropertiesFile(projectRoot: VirtualFile, enableKgpDependencyResolution: Boolean) {
+    VfsUtil.virtualToIoFile(projectRoot).resolve("gradle.properties").apply {
+        appendText("\nkotlin.mpp.import.enableKgpDependencyResolution=$enableKgpDependencyResolution")
+    }
 }
