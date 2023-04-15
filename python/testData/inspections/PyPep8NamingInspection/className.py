47009b0dc78a93e867a5ffcfc13c162ddc459404
Daniel Schmidt <rendner@users.noreply.github.com>
2023-04-15 23:03:48 +0200

    PY-39352 - use the more accurate term "CapWords" instead of "CamelCase"

--- python/testData/inspections/PyPep8NamingInspection/className.py
+++ python/testData/inspections/PyPep8NamingInspection/className.py
@@ -1,4 +1,4 @@
 
-class <weak_warning descr="Class names should use CamelCase convention">A_B</weak_warning>:
+class <weak_warning descr="Class names should use CapWords convention">A_B</weak_warning>:
     def foo(self):
         pass
