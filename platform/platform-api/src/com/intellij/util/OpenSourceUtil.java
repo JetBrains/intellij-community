cddb080260009a18c5c9b1e5a42e0277296fd58c
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-04-17 16:00:01 +0200

    deprecate `OpenSourceUtil.openSourcesFrom(DataProvider, boolean)`
    GitOrigin-RevId: eb1e326e1d66f776dc02a364e2d2adb12fdda0da

--- platform/platform-api/src/com/intellij/util/OpenSourceUtil.java
+++ platform/platform-api/src/com/intellij/util/OpenSourceUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util;
 
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -20,8 +20,12 @@ public final class OpenSourceUtil {
     navigate(requestFocus, false, CommonDataKeys.NAVIGATABLE_ARRAY.getData(context));
   }
 
+  /**
+   * @deprecated use {@link #openSourcesFrom(DataContext, boolean)} instead
+   */
+  @Deprecated
   public static void openSourcesFrom(@NotNull DataProvider context, boolean requestFocus) {
-    navigate(requestFocus, CommonDataKeys.NAVIGATABLE_ARRAY.getData(context));
+    openSourcesFrom((DataContext)context::getData, requestFocus);
   }
 
   /**
