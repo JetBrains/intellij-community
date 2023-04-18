6e82beafc1ae20bae8c8330d111e3f31466a4c98
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-04-18 19:48:14 +0200

    [tests] refactoring cross-platform launcher tests
    - simpler configuration (parameterized tests turned out to be unnecessary)
    - more reliable deletion of test directories
    - ordered execution of runtime selection tests
    
    GitOrigin-RevId: 7a07ae4783d2371a9b3757c6efa946740adc4222

--- native/XPlatLauncher/tests/smoke_tests.rs
+++ native/XPlatLauncher/tests/smoke_tests.rs
@@ -3,26 +3,25 @@ pub mod utils;
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
     use std::fs;
-    use std::path::Path;
-    use rstest::*;
+    use std::fs::File;
+    use std::io::Write;
+    use std::path::PathBuf;
+    use std::sync::Mutex;
     use crate::utils::*;
-    use utils::is_executable;
 
-    #[rstest]
-    #[case::main_bin(& LayoutSpec {launcher_location: LauncherLocation::MainBin, java_type: JavaType::JBR})]
-    #[case::plugins_bin(& LayoutSpec {launcher_location: LauncherLocation::PluginBin, java_type: JavaType::JBR})]
-    fn correct_launcher_startup_test(#[case] layout_spec: &LayoutSpec) {
-        let result = run_launcher(layout_spec);
-        assert!(result.exit_status.success(), "The exit status of the launcher is not successful: {:?}", result);
+    /// Because of the shared config directory, runtime selection tests cannot run concurrently.
+    static RT_LOCK: Mutex<usize> = Mutex::new(0);
+
+    #[test]
+    fn correct_launcher_startup_test() {
+        run_launcher(&LauncherRunSpec::standard().assert_status());
     }
 
-    #[rstest]
-    #[case::main_bin(& LayoutSpec {launcher_location: LauncherLocation::MainBin, java_type: JavaType::JBR})]
-    #[case::plugins_bin(& LayoutSpec {launcher_location: LauncherLocation::PluginBin, java_type: JavaType::JBR})]
-    fn classpath_test(#[case] layout_spec: &LayoutSpec) {
-        let test = prepare_test_env(layout_spec);
-        let dump = run_launcher_and_get_dump_default(&test);
+    #[test]
+    fn classpath_test() {
+        let dump = run_launcher(&LauncherRunSpec::standard().with_dump().assert_status()).dump();
         let classpath = &dump.systemProperties["java.class.path"];
 
         assert!(
@@ -31,12 +30,9 @@ mod tests {
         );
     }
 
-    #[rstest]
-    #[case::main_bin(& LayoutSpec {launcher_location: LauncherLocation::MainBin, java_type: JavaType::JBR})]
-    #[case::plugins_bin(& LayoutSpec {launcher_location: LauncherLocation::PluginBin, java_type: JavaType::JBR})]
-    fn additional_jvm_arguments_in_product_info_test(#[case] layout_spec: &LayoutSpec) {
-        let test = prepare_test_env(layout_spec);
-        let dump = run_launcher_and_get_dump_default(&test);
+    #[test]
+    fn additional_jvm_arguments_in_product_info_test() {
+        let dump = run_launcher(&LauncherRunSpec::standard().with_dump().assert_status()).dump();
         let idea_vendor_name_vm_option = dump.vmOptions.iter().find(|&vm| vm.starts_with("-Didea.vendor.name=JetBrains"));
 
         assert!(
@@ -45,257 +41,103 @@ mod tests {
         );
     }
 
-    #[rstest]
-    #[case::main_bin(& LayoutSpec {launcher_location: LauncherLocation::MainBin, java_type: JavaType::JBR})]
-    #[case::plugins_bin(& LayoutSpec {launcher_location: LauncherLocation::PluginBin, java_type: JavaType::JBR})]
-    fn arguments_test(#[case] layout_spec: &LayoutSpec) {
-        let test = prepare_test_env(layout_spec);
-
+    #[test]
+    fn arguments_test() {
+        let test = prepare_test_env(LauncherLocation::Standard);
         let args = &["arguments-test-123"];
-
-        let dump = run_launcher_and_get_dump_with_args(&test, args);
+        let dump = run_launcher_ext(&test, &LauncherRunSpec::standard().with_dump().with_args(args).assert_status()).dump();
 
         assert_eq!(&dump.cmdArguments[0], "dump-launch-parameters");
         assert_eq!(&dump.cmdArguments[1], "--output");
-        assert_eq!(&dump.cmdArguments[2], &test.test_root_dir.path().join(TEST_OUTPUT_FILE_NAME).to_string_lossy());
         assert_eq!(&dump.cmdArguments[3], args[0]);
     }
 
-    // todo: order tests
+    #[test]
+    fn selecting_product_jdk_env_runtime_test() {
+        let test = prepare_test_env(LauncherLocation::Standard);
+        let expected_rt = test.create_jbr_link("_prod_jdk_jbr");
+        let env = HashMap::from([("IU_JDK", expected_rt.to_str().unwrap())]);
 
-    // # shellcheck disable=SC2154
-    // if [ -n "$IDEA_JDK" ] && [ -x "$IDEA_JDK/bin/java" ]; then
-    //   JRE="$IDEA_JDK"
-    // fi
-    #[cfg(target_os = "freebsd")]
-    #[rstest]
-    #[case::main_bin(& LayoutSpec {launcher_location: LauncherLocation::MainBin, java_type: JavaType::EnvVar})]
-    #[case::plugins_bin(& LayoutSpec {launcher_location: LauncherLocation::PluginBin, java_type: JavaType::EnvVar})]
-    fn jre_is_idea_jdk_test(#[case] layout_spec: &LayoutSpec) {
-        let test = prepare_test_env(layout_spec);
-
-        let dump = run_launcher_and_get_dump_with_java_env(&test, "IU_JDK");
-
-        assert!(&dump.environmentVariables.contains_key("IU_JDK"), "IU_JDK is not set");
-        assert!(
-            !&dump.environmentVariables["IU_JDK"].is_empty(),
-            "IU_JDK is not found in dumped env vars"
-        );
-        assert!(
-            get_bin_java_path(&Path::new(&dump.environmentVariables["IU_JDK"]))
-                .exists(),
-            "Java executable from JDK does not exists"
-        );
-        assert!(
-            get_bin_java_path(&Path::new(&dump.environmentVariables["IU_JDK"]))
-                .is_executable(),
-            "Java executable from JDK is not executable"
-        );
-        assert!(
-            &dump.systemProperties["java.home"].starts_with(
-                &dump.environmentVariables["IU_JDK"]),
-            "Resolved java is not equal to env var"
-        );
+        let _lock = RT_LOCK.lock().expect("Failed to acquire the runtime lock");
+        let result = run_launcher_ext(&test, LauncherRunSpec::standard().assert_status().with_env(&env));
+        do_test_runtime_selection(result, expected_rt);
     }
 
-    // if [ -z "$JRE" ] && [ -s "${CONFIG_HOME}/JetBrains/IntelliJIdea2022.3/idea.jdk" ]; then
-    //   USER_JRE=$(cat "${CONFIG_HOME}/JetBrains/IntelliJIdea2022.3/idea.jdk")
-    //   if [ -x "$USER_JRE/bin/java" ]; then
-    //     JRE="$USER_JRE"
-    //   fi
-    // fi
-    #[rstest]
-    #[case::main_bin(& LayoutSpec {launcher_location: LauncherLocation::MainBin, java_type: JavaType::UserJRE})]
-    #[case::plugins_bin(& LayoutSpec {launcher_location: LauncherLocation::PluginBin, java_type: JavaType::UserJRE})]
-    #[cfg(target_os = "macos")]
-    fn jre_is_user_jre_test(#[case] layout_spec: &LayoutSpec) {
-        let test = prepare_test_env(layout_spec);
-        let dump = run_launcher_and_get_dump_default(&test);
+    #[test]
+    fn selecting_user_config_runtime_test() {
+        let mut test = prepare_test_env(LauncherLocation::Standard);
+        let expected_rt = test.create_jbr_link("_user_jbr");
 
-        let idea_jdk = get_custom_user_file_with_java_path().unwrap().join("idea.jdk");
-        let idea_jdk_content = fs::read_to_string(&idea_jdk).unwrap();
-        let jbr_home = get_jbr_home(&Path::new(&idea_jdk_content).to_path_buf()).unwrap();
-        let resolved_jdk_path = Path::new(&idea_jdk_content);
-        let resolved_jdk = get_bin_java_path(resolved_jdk_path);
-        let metadata = idea_jdk.metadata().unwrap();
+        let _lock = RT_LOCK.lock().expect("Failed to acquire the runtime lock");
 
-        assert!(idea_jdk.exists(), "Config file is not exists");
-        assert_ne!(0, metadata.len(), "Config file is empty");
-        assert!(resolved_jdk_path.exists(), "JDK from idea.jdk is not exists");
-        assert!(resolved_jdk.exists(), "JDK from idea.jdk is not exists");
-        assert!(is_executable(&resolved_jdk).expect("Failed to check java executable"), "Java executable from JDK is not executable");
-        assert_eq!(
-            &dump.systemProperties["java.home"],
-            jbr_home.to_str().expect("Can't display jbr_home"),
-            "Resolved java is not from .config"
-        );
+        let custom_config_dir = get_custom_config_dir();
+        test.delete_later(&custom_config_dir);
+
+        let jdk_config_file = custom_config_dir.join("idea.jdk");
+        fs::create_dir_all(custom_config_dir).unwrap();
+        File::create(jdk_config_file).unwrap()
+            .write_all(expected_rt.to_str().unwrap().as_bytes()).unwrap();
+
+        let result = run_launcher_ext(&test, LauncherRunSpec::standard().assert_status());
+        do_test_runtime_selection(result, expected_rt);
     }
 
-    #[rstest]
-    #[case::main_bin(& LayoutSpec {launcher_location: LauncherLocation::MainBin, java_type: JavaType::UserJRE})]
-    #[case::plugins_bin(& LayoutSpec {launcher_location: LauncherLocation::PluginBin, java_type: JavaType::UserJRE})]
-    #[cfg(any(target_os = "windows", target_os = "linux"))]
-    fn jre_is_user_jre_test(#[case] layout_spec: &LayoutSpec) {
-        let test = prepare_test_env(layout_spec);
-        let dump = run_launcher_and_get_dump_default(&test);
+    #[test]
+    fn selecting_bundled_runtime_test() {
+        let test = prepare_test_env(LauncherLocation::Standard);
+        let expected_rt = test.dist_root.join("jbr");
 
-        let idea_jdk = get_custom_user_file_with_java_path().unwrap().join("idea.jdk");
-        let idea_jdk_content = fs::read_to_string(&idea_jdk).unwrap();
-        let resolved_jdk_path = Path::new(&idea_jdk_content);
-        let java_executable = get_bin_java_path(resolved_jdk_path);
-        let metadata = idea_jdk.metadata().unwrap();
-
-        assert!(idea_jdk.exists(), "Config file is not exists");
-        assert_ne!(0, metadata.len(), "Config file is empty");
-        assert!(resolved_jdk_path.exists(), "JDK from idea.jdk is not exists");
-        assert!(java_executable.exists(), "Java executable idea.jdk is not exists");
-        assert!(is_executable(&java_executable).expect("Failed to check java executable"), "Java executable from JDK is not executable");
-        assert_eq!(
-            &dump.systemProperties["java.home"],
-            &idea_jdk_content.to_string(),
-            "Resolved java is not from .config"
-        );
+        let _lock = RT_LOCK.lock().expect("Failed to acquire the runtime lock");
+        let result = run_launcher_ext(&test, LauncherRunSpec::standard().assert_status());
+        do_test_runtime_selection(result, expected_rt);
     }
 
-    // if [ -z "$JRE" ] && [ "$OS_TYPE" = "Linux" ] && [ -f "$IDE_HOME/jbr/release" ]; then
-    //   JBR_ARCH="OS_ARCH=\"$OS_ARCH\""
-    //   if grep -q -e "$JBR_ARCH" "$IDE_HOME/jbr/release" ; then
-    //     JRE="$IDE_HOME/jbr"
-    //   fi
-    // fi
-    #[rstest]
-    #[case::main_bin(& LayoutSpec {launcher_location: LauncherLocation::MainBin, java_type: JavaType::JBR})]
-    #[case::plugins_bin(& LayoutSpec {launcher_location: LauncherLocation::PluginBin, java_type: JavaType::JBR})]
-    fn jre_is_jbr_test(#[case] layout_spec: &LayoutSpec) {
-        let test = prepare_test_env(layout_spec);
-        let dump = run_launcher_and_get_dump_default(&test);
+    #[test]
+    fn selecting_jdk_home_env_runtime_test() {
+        let test = prepare_test_env(LauncherLocation::Standard);
+        fs::remove_file(test.dist_root.join("jbr")).unwrap();  // the bundled one has a priority
+        let expected_rt = test.create_jbr_link("_jdk_home_jbr");
+        let env = HashMap::from([("JDK_HOME", expected_rt.to_str().unwrap())]);
 
-        let test_root_dir = test.test_root_dir.path();
-
-        let jbr_dir = match std::env::consts::OS {
-            "linux" => test_root_dir.join("jbr"),
-            "macos" => test_root_dir.join("Contents").join("jbr"),
-            "windows" => test_root_dir.join("jbr"),
-            unsupported_os => panic!("Unsupported OS: {unsupported_os}")
-        };
-
-        let jbr_home = get_jbr_home(&jbr_dir).expect("Can't get jbr home");
-
-        let java_executable = get_bin_java_path(&jbr_dir);
-
-        assert!(jbr_dir.exists(), "JBR dir is not exists");
-        assert!(jbr_dir.is_dir(), "Resolved JBR dir is not a directory");
-        assert!(java_executable.exists(), "Resolved java executable is not exists");
-        assert!(is_executable(&java_executable).expect("Failed to check java executable"), "Resolved java executable is not executable");
-        // todo: turn on after wrapper with https://github.com/mfilippov/gradle-jvm-wrapper/pull/31
-        // assert_eq!(
-        //     &dump.systemProperties["java.vendor"],
-        //     "JetBrains s.r.o.",
-        //     "Java vendor is not JetBrains. Resolved java is not JBR");
-        assert_eq!(
-            &dump.systemProperties["java.home"],
-            jbr_home.to_str().expect("Can't display jbr_home"),
-            "Resolved java is not JBR"
-        );
-        // assert!(
-        //     &dump.systemProperties["java.home"].contains("jbr"),
-        //     "java.home property does not contains 'jbr' in path. Resolved java is not JBR"
-        // );
+        let _lock = RT_LOCK.lock().expect("Failed to acquire the runtime lock");
+        let result = run_launcher_ext(&test, LauncherRunSpec::standard().assert_status().with_env(&env));
+        do_test_runtime_selection(result, expected_rt);
     }
 
-    #[rstest]
-    #[case::main_bin(& LayoutSpec {launcher_location: LauncherLocation::MainBin, java_type: JavaType::EnvVar})]
-    #[case::plugins_bin(& LayoutSpec {launcher_location: LauncherLocation::PluginBin, java_type: JavaType::EnvVar})]
-    fn jre_is_jdk_home_test(#[case] layout_spec: &LayoutSpec) {
-        let test = prepare_test_env(layout_spec);
-        let dump = run_launcher_and_get_dump_with_java_env(&test, "JDK_HOME");
+    #[test]
+    fn selecting_java_home_env_runtime_test() {
+        let test = prepare_test_env(LauncherLocation::Standard);
+        fs::remove_file(test.dist_root.join("jbr")).unwrap();  // the bundled one has a priority
+        let expected_rt = test.create_jbr_link("_java_home_jbr");
+        let env = HashMap::from([("JAVA_HOME", expected_rt.to_str().unwrap())]);
 
-        assert!(
-            std::env::var("IU_JDK").is_err(),
-            "IU_JDK var is present. It has a higher priority than tested var"
-        );
-
-        assert!(
-            &dump.environmentVariables.contains_key("JDK_HOME"),
-            "JDK_HOME not found in dump"
-        );
-        assert!(
-            !&dump.environmentVariables["JDK_HOME"].is_empty(),
-            "JDK_HOME is empty"
-        );
-        assert!(
-            get_bin_java_path(&Path::new(&dump.environmentVariables["JDK_HOME"]))
-                .exists(),
-            "Java executable from JDK does not exists"
-        );
-        let java_executable = get_bin_java_path(&Path::new(&dump.environmentVariables["JDK_HOME"]));
-        assert!(
-            is_executable(&java_executable).expect("Failed to check java executable"),
-            "Java executable from JDK is not executable"
-        );
-        assert!(
-            &dump.systemProperties["java.home"].starts_with(
-                &dump.environmentVariables["JDK_HOME"]),
-            "Resolved java is not equal to env var"
-        );
+        let _lock = RT_LOCK.lock().expect("Failed to acquire the runtime lock");
+        let result = run_launcher_ext(&test, LauncherRunSpec::standard().assert_status().with_env(&env));
+        do_test_runtime_selection(result, expected_rt);
     }
 
-    #[cfg(target_os = "freebsd")]
-    #[rstest]
-    #[case::main_bin(& LayoutSpec {launcher_location: LauncherLocation::MainBin, java_type: JavaType::EnvVar})]
-    #[case::plugins_bin(& LayoutSpec {launcher_location: LauncherLocation::PluginBin, java_type: JavaType::EnvVar})]
-    fn jre_is_java_home_test(#[case] layout_spec: &LayoutSpec) {
-        let test = prepare_test_env(layout_spec);
-        let dump = run_launcher_and_get_dump_with_java_env(&test, "JAVA_HOME");
+    fn do_test_runtime_selection(result: LauncherRunResult, expected_rt: PathBuf) {
+        let rt_line = result.stdout.lines().into_iter()
+            .find(|line| line.contains("Resolved runtime: "))
+            .expect(format!("The 'resolved runtime' line is not in the output: {}", result.stdout).as_str());
+        let resolved_rt = rt_line.split_once("Resolved runtime: ").unwrap().1;
+        let actual_rt = &resolved_rt[1..resolved_rt.len() - 1];
 
-        assert!(
-            std::env::var("IU_JDK").is_err(),
-            "IU_JDK var is present. It has a higher priority than tested var"
-        );
+        let adjusted_rt = if cfg!(target_os = "macos") { expected_rt.join("Contents/Home") } else { expected_rt };
 
-        assert!(
-            std::env::var("JDK_HOME").is_err(),
-            "JDK_HOME var is present. It has a higher priority than tested var"
-        );
-
-        assert!(
-            &dump.environmentVariables.contains_key("JAVA_HOME"),
-            "JAVA_HOME not found in dump"
-        );
-        assert!(
-            !&dump.environmentVariables["JAVA_HOME"].is_empty(),
-            "JAVA_HOME is empty"
-        );
-        assert!(
-            get_bin_java_path(&Path::new(&dump.environmentVariables["JAVA_HOME"]))
-                .exists(),
-            "Java executable from JDK does not exists"
-        );
-        assert!(
-            get_bin_java_path(&Path::new(&dump.environmentVariables["JAVA_HOME"]))
-                .is_executable(),
-            "Java executable from JDK is not executable"
-        );
-        assert!(
-            &dump.systemProperties["java.home"].starts_with(
-                &dump.environmentVariables["JAVA_HOME"]),
-            "Resolved java is not equal to env var"
-        );
+        assert_eq!(adjusted_rt.to_str().unwrap(), actual_rt, "Wrong runtime; run result: {:?}", result);
     }
 
-    #[rstest]
-    #[case::main_bin(& LayoutSpec {launcher_location: LauncherLocation::MainBin, java_type: JavaType::JBR})]
-    #[cfg(any(target_os = "macos", target_os = "linux"))]
-    fn async_profiler_loading(#[case] layout_spec: &LayoutSpec) {
-        let result = run_launcher_with_args(layout_spec, &["async-profiler"]);
-        assert!(result.exit_status.success(), "The exit status of the launcher is not successful: {:?}", result);
+    #[test]
+    #[cfg(any(target_family = "unix"))]
+    fn async_profiler_loading() {
+        let result = run_launcher(LauncherRunSpec::standard().assert_status().with_args(&["async-profiler"]));
         assert!(result.stdout.contains("version="), "Profiler version is missing from the output: {:?}", result);
     }
 
-    #[rstest]
-    #[case::main_bin(& LayoutSpec {launcher_location: LauncherLocation::MainBin, java_type: JavaType::JBR})]
-    fn exit_code_passing(#[case] layout_spec: &LayoutSpec) {
-        let result = run_launcher_with_args(layout_spec, &["exit-code", "42"]);
+    #[test]
+    fn exit_code_passing() {
+        let result = run_launcher(LauncherRunSpec::standard().with_args(&["exit-code", "42"]));
         assert_eq!(result.exit_status.code(), Some(42), "The exit code of the launcher is unexpected: {:?}", result);
     }
 }
