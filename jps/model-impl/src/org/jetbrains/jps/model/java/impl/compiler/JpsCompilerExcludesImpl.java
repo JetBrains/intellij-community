f228ae08eb5e902d96cd40e7a340dd8bb35d8458
Eugene Zhuravlev <eugene.zhuravlev@jetbrains.com>
2023-04-18 19:19:36 +0200

    automatically exclude from compilation sources under "snippet-files" directory (relates to IDEA-314513)
    GitOrigin-RevId: c69e9f40daf6b31fffb94aa423bfed3ffa1f566f

--- jps/model-impl/src/org/jetbrains/jps/model/java/impl/compiler/JpsCompilerExcludesImpl.java
+++ jps/model-impl/src/org/jetbrains/jps/model/java/impl/compiler/JpsCompilerExcludesImpl.java
@@ -1,21 +1,9 @@
-/*
- * Copyright 2000-2012 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.jps.model.java.impl.compiler;
 
+import com.intellij.openapi.util.SystemInfoRt;
 import com.intellij.openapi.util.io.FileUtilRt;
+import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.jps.model.java.compiler.JpsCompilerExcludes;
 import org.jetbrains.jps.util.JpsPathUtil;
 
@@ -24,6 +12,7 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class JpsCompilerExcludesImpl implements JpsCompilerExcludes {
+  private static final String SNIPPET_DIR_PATTERN = "/snippet-files/";
   private final Set<File> myFiles = new LinkedHashSet<>();
   private final Set<File> myDirectories = new LinkedHashSet<>();
   private final Set<File> myRecursivelyExcludedDirectories = new LinkedHashSet<>();
@@ -65,6 +54,12 @@ public class JpsCompilerExcludesImpl implements JpsCompilerExcludes {
         parent = FileUtilRt.getParentFile(parent);
       }
     }
+
+    String path = FileUtilRt.toSystemIndependentName(file.getPath());
+    if (SystemInfoRt.isFileSystemCaseSensitive? StringUtil.contains(path, SNIPPET_DIR_PATTERN) : StringUtil.containsIgnoreCase(path, SNIPPET_DIR_PATTERN)) {
+      return true;
+    }
+
     return false;
   }
 
