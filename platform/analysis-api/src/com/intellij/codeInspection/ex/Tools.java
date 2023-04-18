7f585bd1c513b05f6a135b11d28b78d990181f47
Roland Illig <roland.illig@jetbrains.com>
2023-04-18 12:00:33 +0200

    Add Javadoc for Tools and ToolsImpl
    GitOrigin-RevId: d01cbf1c0ac428be9602b6b93be2aec6b355772b

--- platform/analysis-api/src/com/intellij/codeInspection/ex/Tools.java
+++ platform/analysis-api/src/com/intellij/codeInspection/ex/Tools.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInspection.ex;
 
 import com.intellij.psi.PsiElement;
@@ -7,6 +7,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
+/** A single inspection, together with the scopes in which it is active. */
 public interface Tools {
   @NotNull
   InspectionToolWrapper<?, ?> getInspectionTool(@Nullable PsiElement element);
