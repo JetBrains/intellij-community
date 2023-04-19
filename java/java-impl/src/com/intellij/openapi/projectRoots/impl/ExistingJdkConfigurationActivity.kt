82cdefb40d1836b2465bcc5394987e5732eebecb
Louis Vignier <louis.vignier@jetbrains.com>
2023-04-19 23:39:16 +0200

    [jdk] Run ExistingJdkConfigurationActivity on project load
    #IDEA-309066
    #IDEA-316432
    
    GitOrigin-RevId: 2b80ff4b1690249cbc32eb661893a780911939fe

---
+++ java/java-impl/src/com/intellij/openapi/projectRoots/impl/ExistingJdkConfigurationActivity.kt
@@ -0,0 +1,64 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.projectRoots.impl
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.PreloadingActivity
+import com.intellij.openapi.application.invokeLater
+import com.intellij.openapi.application.writeAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.projectRoots.JavaSdk
+import com.intellij.openapi.projectRoots.JdkFinder
+import com.intellij.openapi.projectRoots.ProjectJdkTable
+import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.openapi.util.Computable
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.registry.Registry
+import java.util.function.Consumer
+
+/**
+ * This [PreloadingActivity] makes it possible to index an installed JDK without any
+ * configuration.
+ * The registry key `jdk.configure.existing` must be enabled.
+ *
+ *  - Automatically registers JDKs on the computer in the [ProjectJdkTable]
+ *  - Uses the first JDK found as project SDK if none was configured
+ */
+class ExistingJdkConfigurationActivity : ProjectActivity {
+  override suspend fun execute(project: Project) {
+    if (!Registry.`is`("jdk.configure.existing", false)) return
+
+    val javaSdk = JavaSdk.getInstance()
+    val registeredJdks = ProjectJdkTable.getInstance().getSdksOfType(javaSdk)
+    val jdkPathsToAdd = ArrayList<String>()
+
+    // Collect JDKs to register
+    JdkFinder.getInstance().suggestHomePaths().forEach { homePath: String ->
+      if (javaSdk.isValidSdkHome(homePath) && registeredJdks.none {
+          FileUtil.toCanonicalPath(it.homePath) == FileUtil.toCanonicalPath(homePath)
+        }) {
+        jdkPathsToAdd.add(homePath)
+      }
+    }
+
+    val rootManager = ProjectRootManager.getInstance(project)
+    val addedJdks = registeredJdks.toMutableList()
+    writeAction {
+      // Register collected JDKs
+      for (path in jdkPathsToAdd) {
+        addedJdks.add(SdkConfigurationUtil.createAndAddSDK(path, javaSdk))
+      }
+
+      // Set project SDK
+      if (rootManager.projectSdk == null) {
+        JavaHomeFinderMac.JAVA_HOME_FIND_UTIL
+        addedJdks
+          .filterNotNull()
+          .maxByOrNull { JavaSdk.getInstance().getVersion(it)?.ordinal ?: 0 }
+          ?.let { rootManager.projectSdk = it }
+      }
+    }
+  }
+}
