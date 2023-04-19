8c076de2008486167ba1b267c7dee6f97f956994
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2023-04-19 15:32:00 +0200

    Revert "[UI] IDEA-315658 Decrease separator insets in Git Branches popup"
    This reverts commit 9892567e
    
    GitOrigin-RevId: cd4bcf42d8194345b91503af1ece2f6f0f241f36

--- plugins/git4idea/src/git4idea/ui/branch/popup/GitBranchesTreePopup.kt
+++ plugins/git4idea/src/git4idea/ui/branch/popup/GitBranchesTreePopup.kt
@@ -408,7 +408,7 @@ class GitBranchesTreePopup(project: Project, step: GitBranchesTreePopupStep, par
     accessibleContext.accessibleName = GitBundle.message("git.branches.popup.tree.accessible.name")
 
     ClientProperty.put(this, DefaultTreeUI.LARGE_MODEL_ALLOWED, true)
-    rowHeight = if (ExperimentalUI.isNewUI()) 0 else treeRowHeight // in the new UI we need variable height for separators
+    rowHeight = treeRowHeight
     isLargeModel = true
     expandsSelectedPaths = true
     SmartExpander.installOn(this)
@@ -739,10 +739,8 @@ class GitBranchesTreePopup(project: Project, step: GitBranchesTreePopupStep, par
     internal fun createTreeSeparator(text: @NlsContexts.Separator String? = null) =
       SeparatorWithText().apply {
         caption = text
-        if (!ExperimentalUI.isNewUI()) { // the new UI uses variable height, no need to add extra space here, it's too large
-          border = JBUI.Borders.emptyTop(
-            if (text == null) treeRowHeight / 2 else JBUIScale.scale(SeparatorWithText.DEFAULT_H_GAP))
-        }
+        border = JBUI.Borders.emptyTop(
+          if (text == null) treeRowHeight / 2 else JBUIScale.scale(SeparatorWithText.DEFAULT_H_GAP))
       }
 
     private fun uiScope(parent: Disposable) =
