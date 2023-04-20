d326d9200cf38b332226b69ae2fc276c1271e626
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2023-04-20 11:34:40 +0200

    [kotlin] Share logic between MoveToPropertyToConstructor intention and CanBePrimaryConstructorProperty inspection
    ^KTIJ-22988
    
    GitOrigin-RevId: df51035652fcdde9c29bf9c60cc728594276ffa1

--- plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/CanBePrimaryConstructorPropertyInspection.kt
+++ plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/CanBePrimaryConstructorPropertyInspection.kt
@@ -1,65 +1,42 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.k2.codeinsight.inspections
 
-import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.psi.PsiElementVisitor
-import com.intellij.psi.search.LocalSearchScope
-import com.intellij.psi.search.searches.ReferencesSearch
-import org.jetbrains.kotlin.analysis.api.analyze
-import org.jetbrains.kotlin.analysis.api.symbols.KtConstructorSymbol
-import org.jetbrains.kotlin.analysis.api.symbols.KtNamedClassOrObjectSymbol
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
 import org.jetbrains.kotlin.analysis.api.symbols.KtValueParameterSymbol
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.inspections.AbstractKotlinInspection
-import org.jetbrains.kotlin.idea.k2.codeinsight.intentions.MovePropertyToConstructorIntention
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.AbstractKotlinApplicableInspectionWithContext
+import org.jetbrains.kotlin.idea.codeinsight.api.applicators.KotlinApplicabilityRange
+import org.jetbrains.kotlin.idea.codeinsights.impl.base.applicators.ApplicabilityRanges
+import org.jetbrains.kotlin.idea.codeinsights.impl.base.intentions.MovePropertyToConstructorInfo
+import org.jetbrains.kotlin.idea.codeinsights.impl.base.intentions.MovePropertyToConstructorUtils.isMovableToConstructorByPsi
+import org.jetbrains.kotlin.idea.codeinsights.impl.base.intentions.MovePropertyToConstructorUtils.moveToConstructor
 import org.jetbrains.kotlin.idea.references.mainReference
-import org.jetbrains.kotlin.lexer.KtTokens
-import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.psi.psiUtil.containingClass
-import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstanceOrNull
+import org.jetbrains.kotlin.psi.KtProperty
 
-class CanBePrimaryConstructorPropertyInspection : AbstractKotlinInspection() {
-    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
-        return propertyVisitor(fun(property) {
-            if (property.isLocal) return
-            if (property.getter != null || property.setter != null || property.delegate != null) return
-            val initializer: KtReferenceExpression = property.initializer as? KtReferenceExpression ?: return
-            analyze(initializer) {
-                val constructorParamSymbol = initializer.mainReference.resolveToSymbol() as? KtValueParameterSymbol ?: return
-                val containingConstructorSymbol = constructorParamSymbol.getContainingSymbol() as? KtConstructorSymbol ?: return
-                val containingClassSymbol = containingConstructorSymbol.getContainingSymbol() as? KtNamedClassOrObjectSymbol ?: return
-                if (containingClassSymbol.isData) return
+internal class CanBePrimaryConstructorPropertyInspection
+    : AbstractKotlinApplicableInspectionWithContext<KtProperty, MovePropertyToConstructorInfo>(KtProperty::class) {
 
-                val propertyTypeReference = property.typeReference
-                val propertyType = propertyTypeReference?.getKtType()
-                if (propertyType != null && propertyType != constructorParamSymbol.returnType) return
+    override fun getProblemDescription(element: KtProperty, context: MovePropertyToConstructorInfo): String = KotlinBundle.message(
+        "property.is.explicitly.assigned.to.parameter.0.can", element.name!!
+    )
 
-                if (property.nameAsName != constructorParamSymbol.name) return
+    override fun getActionFamilyName() = KotlinBundle.message("inspection.can.be.primary.constructor.property.display.name")
 
-                val constructorParamPsi = constructorParamSymbol.psi as? KtParameter ?: return
-                val containingClassPsi = property.containingClass() ?: return
-                if (containingClassPsi !== constructorParamPsi.containingClass()) return
-                if (containingClassPsi.isInterface()) return
-                if (property.hasModifier(KtTokens.OPEN_KEYWORD)
-                    && containingClassPsi.isOpen()
-                    && constructorParamPsi.isUsedInClassInitializer(containingClassPsi)
-                ) return
+    override fun getApplicabilityRange(): KotlinApplicabilityRange<KtProperty> = ApplicabilityRanges.DECLARATION_NAME
 
-                holder.registerProblem(
-                    property.nameIdentifier ?: return,
-                    KotlinBundle.message("property.is.explicitly.assigned.to.parameter.0.can", constructorParamSymbol.name),
-                    MovePropertyToConstructorIntention()
-                )
-            }
-        })
+    override fun isApplicableByPsi(element: KtProperty): Boolean = element.isMovableToConstructorByPsi()
+
+    context(KtAnalysisSession)
+    override fun prepareContext(element: KtProperty): MovePropertyToConstructorInfo? {
+        val initializer = element.initializer ?: return null
+        val paramSymbol = initializer.mainReference?.resolveToSymbol() as? KtValueParameterSymbol ?: return null
+        if (element.nameAsName != paramSymbol.name) return null
+        return MovePropertyToConstructorInfo.create(element)
     }
 
-    private fun KtClass.isOpen(): Boolean {
-        return hasModifier(KtTokens.OPEN_KEYWORD) || hasModifier(KtTokens.ABSTRACT_KEYWORD) || hasModifier(KtTokens.SEALED_KEYWORD)
-    }
-
-    private fun KtParameter.isUsedInClassInitializer(containingClass: KtClass): Boolean {
-        val classInitializer = containingClass.body?.declarations?.firstIsInstanceOrNull<KtClassInitializer>() ?: return false
-        return ReferencesSearch.search(this, LocalSearchScope(classInitializer)).any()
+    override fun apply(element: KtProperty, context: MovePropertyToConstructorInfo, project: Project, editor: Editor?) {
+        element.moveToConstructor(context)
     }
 }
