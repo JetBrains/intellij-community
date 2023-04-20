2966d50227b8a8aaa330a41e5b1e49d3d256404f
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2023-04-20 18:54:49 +0300

    [project import perf tests] AT-92 publishing open telemetry collected meter data to IJ Perf dashboard
    GitOrigin-RevId: b63e3dd193fb15dc2dd26de059c15e73131f3e0d

--- platform/diagnostic/telemetry/src/MetricsImporterUtils.kt
+++ platform/diagnostic/telemetry/src/MetricsImporterUtils.kt
@@ -3,7 +3,6 @@ package com.intellij.diagnostic.telemetry
 
 import io.opentelemetry.api.common.Attributes
 import io.opentelemetry.sdk.metrics.data.LongPointData
-import io.opentelemetry.sdk.metrics.data.PointData
 import io.opentelemetry.sdk.metrics.internal.data.ImmutableLongPointData
 import org.jetbrains.annotations.ApiStatus
 import java.nio.file.Path
@@ -28,21 +27,20 @@ object MetricsImporterUtils {
    * TODO: to differentiate between different types of meters we need to store MetricDataType in csv as well
    */
   @JvmStatic
-  fun fromCsvFile(metricsCsvPath: Path): HashMap<String, MutableList<PointData>> {
-    val meters = HashMap<String, MutableList<PointData>>()
+  fun fromCsvFile(metricsCsvPath: Path): HashMap<String, MutableList<LongPointData>> {
+    val meters = HashMap<String, MutableList<LongPointData>>()
 
     metricsCsvPath.bufferedReader().useLines { lines ->
       lines.drop(MetricsExporterUtils.csvHeadersLines().size).forEach {
         // See: [MetricsExporterUtils.concatToCsvLine]
         // # NAME, PERIOD_START_NANOS, PERIOD_END_NANOS, VALUE
-        val parts = it.split(",")
-        val metricName = parts[0]
-        val startEpochNanos = parts[1].toLong()
-        val endEpochNanos = parts[2].toLong()
-        val value = parts[3]
+        val (metricName, startEpochNanos, endEpochNanos, value) = it.split(",")
 
         if (value.isLongNumber()) {
-          val data: LongPointData = ImmutableLongPointData.create(startEpochNanos, endEpochNanos, Attributes.empty(), value.toLong())
+          val data: LongPointData = ImmutableLongPointData.create(startEpochNanos.toLong(),
+                                                                  endEpochNanos.toLong(),
+                                                                  Attributes.empty(),
+                                                                  value.toLong())
           val metrics = meters.computeIfAbsent(metricName) { mutableListOf() }
           metrics.add(data)
         }
