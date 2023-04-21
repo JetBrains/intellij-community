b4944b26a5586a78ef5bba0a1f71f2570aadca8b
Ivan Semenov <ivan.semenov@jetbrains.com>
2023-04-21 13:20:34 +0200

    [collab] force HTML image re-acquisition when HTML pane is shown after being hidden
    IDEA-318619 Fixed
    
    (cherry picked from commit 0499f48da1f1df5191e14f69010f4abb11eef7f3)
    
    IJ-MR-106689
    
    GitOrigin-RevId: dcddddf0570e845d60814d86dccb3c912880c4d0

--- platform/collaboration-tools/src/com/intellij/collaboration/ui/html/ResizingHtmlImageView.kt
+++ platform/collaboration-tools/src/com/intellij/collaboration/ui/html/ResizingHtmlImageView.kt
@@ -45,7 +45,7 @@ class ResizingHtmlImageView(element: Element) : View(element) {
       field = value
     }
   private val loader: ImageLoader
-    get() = _loader ?: run {
+    get() = _loader?.takeIf { it.isActual } ?: run {
       createImageLoader().also {
         _loader = it
       }
@@ -186,7 +186,7 @@ private class ImageLoader(
   private val baseUrl: URL?,
   private val src: String?,
   private val asyncLoader: AsyncHtmlImageLoader?,
-  private val isActual: () -> Boolean,
+  private val isActualTest: () -> Boolean,
   private val onStateChange: (old: State, new: State) -> Unit
 ) : ImageObserver {
 
@@ -196,6 +196,8 @@ private class ImageLoader(
   var state: State by observable(State.NotLoaded) { _, old, new ->
     onStateChange(old, new)
   }
+    private set
+  var isActual: Boolean = true
     private set
 
   private fun requestImage(): Job {
@@ -256,7 +258,14 @@ private class ImageLoader(
 
   override fun imageUpdate(img: Image, flags: Int, x: Int, y: Int, width: Int, height: Int): Boolean =
     invokeAndWaitIfNeeded {
-      if (!loadingJob.isCancelled && isActual()) doUpdate(img, flags, width, height) else false
+      isActual = isActualTest()
+      val updateResult = doUpdate(img, flags, width, height)
+      if (!loadingJob.isCancelled && isActual) {
+        updateResult
+      }
+      else {
+        false
+      }
     }
 
   private fun doUpdate(img: Image, flags: Int, width: Int, height: Int): Boolean {
