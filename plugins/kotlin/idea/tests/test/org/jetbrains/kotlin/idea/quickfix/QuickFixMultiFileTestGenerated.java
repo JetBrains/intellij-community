ed7025ec5f1f8aa0e40ae7cb35c0fa9bd9bf0c91
Alexander Udalov <alexander.udalov@jetbrains.com>
2023-04-22 00:42:17 +0200

    JvmDefault: remove quickfix to add `@JvmDefault`
    JvmDefault is going to be deprecated with ERROR according to KT-54746.
    
    GitOrigin-RevId: d90923550c76d397c494777bb2f8fb99f08b6068

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/quickfix/QuickFixMultiFileTestGenerated.java
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/quickfix/QuickFixMultiFileTestGenerated.java
@@ -84,19 +84,6 @@ public abstract class QuickFixMultiFileTestGenerated extends AbstractQuickFixMul
     }
 
     @RunWith(JUnit3RunnerWithInners.class)
-    @TestMetadata("testData/quickfix/addJvmDefault")
-    public static class AddJvmDefault extends AbstractQuickFixMultiFileTest {
-        private void runTest(String testDataFilePath) throws Exception {
-            KotlinTestUtils.runTest(this::doTestWithExtraFile, this, testDataFilePath);
-        }
-
-        @TestMetadata("javaDefaultOverride.before.Main.kt")
-        public void testJavaDefaultOverride() throws Exception {
-            runTest("testData/quickfix/addJvmDefault/javaDefaultOverride.before.Main.kt");
-        }
-    }
-
-    @RunWith(JUnit3RunnerWithInners.class)
     @TestMetadata("testData/quickfix/addSpreadOperatorForArrayAsVarargAfterSam")
     public static class AddSpreadOperatorForArrayAsVarargAfterSam extends AbstractQuickFixMultiFileTest {
         private void runTest(String testDataFilePath) throws Exception {
