fc4636873afd0501db55f3bad79f18627989a0c8
Adam Malek <adam.malek@jetbrains.com>
2023-04-21 13:05:58 +0200

    ML in SE: Temp change lang model computation to non-coroutine version (LAB-84)
    As we are experiencing issues with coroutines
    and thread leaks, we have decided that for now
    we'd like to merge the branch with old-school
    way of performing background computations.
    
    We are going to change to coroutines soon.
    
    GitOrigin-RevId: 58bc05276aa1b989025f08886710e58f9e22c55e

--- plugins/search-everywhere-ml/typos/src/com/intellij/searchEverywhereMl/typos/ActionsLanguageModel.kt
+++ plugins/search-everywhere-ml/typos/src/com/intellij/searchEverywhereMl/typos/ActionsLanguageModel.kt
@@ -5,27 +5,21 @@ import com.intellij.ide.actions.ShowSettingsUtilImpl
 import com.intellij.openapi.actionSystem.ActionGroup
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.impl.ActionManagerImpl
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.components.serviceIfCreated
 import com.intellij.openapi.options.SearchableConfigurable
-import com.intellij.openapi.progress.withBackgroundProgress
-import com.intellij.openapi.progress.withRawProgressReporter
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.wm.IdeFocusManager
 import com.intellij.spellchecker.dictionary.Dictionary
 import com.intellij.spellchecker.dictionary.RuntimeDictionaryProvider
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 @Service(Service.Level.APP)
-internal class ActionsLanguageModel(coroutineScope: CoroutineScope,
-                                    private val actionsDictionary: ActionsDictionary) : Dictionary by actionsDictionary,
-                                                                                        FrequencyMetadata by actionsDictionary {
-
-  constructor(coroutineScope: CoroutineScope) : this(coroutineScope, ActionsDictionaryImpl())
+internal class ActionsLanguageModel(private val actionsDictionary: ActionsDictionary = ActionsDictionaryImpl()) : Dictionary by actionsDictionary,
+                                                                                                                  FrequencyMetadata by actionsDictionary {
 
   companion object {
     /**
@@ -37,26 +31,17 @@ internal class ActionsLanguageModel(coroutineScope: CoroutineScope,
     }
   }
 
-  private val languageModelComputation = coroutineScope.launch {
-    val project = IdeFocusManager.getGlobalInstance().lastFocusedFrame?.project ?: ProjectManager.getInstance().defaultProject
+  private val languageModelComputationTask = LanguageModelComputationTask()  // TODO: Change to a coroutine
 
-    withBackgroundProgress(project, TyposBundle.getMessage("progress.title.computing.language.model"), true) {
-      withRawProgressReporter {
-        (getWordsFromActions() + getWordsFromSettings())
-          .flatMap { it.split(" ", "-", ".") }
-          .map { it.filter { c -> c.isLetterOrDigit() } }
-          .filterNot { it.isEmpty() || it.isSingleCharacter() }
-          .map { it.lowercase() }
-          .forEach(actionsDictionary::addWord)
-      }
-    }
+  init {
+    ApplicationManager.getApplication().executeOnPooledThread(languageModelComputationTask)
   }
 
   /**
    * Returns true if all words from actions and options have been processed and added to the dictionary
    */
   val isComputed: Boolean
-    get() = languageModelComputation.isCompleted
+    get() = languageModelComputationTask.isFinished
 
   private fun getWordsFromActions() = (ActionManager.getInstance() as ActionManagerImpl)
     .let { actionManager ->
@@ -103,6 +88,21 @@ internal class ActionsLanguageModel(coroutineScope: CoroutineScope,
       getInstance()
     }
   }
+
+  private inner class LanguageModelComputationTask : Runnable {
+    var isFinished: Boolean = false
+
+    override fun run() {
+      (getWordsFromActions() + getWordsFromSettings())
+        .flatMap { it.split(" ", "-", ".") }
+        .map { it.filter { c -> c.isLetterOrDigit() } }
+        .filterNot { it.isEmpty() || it.isSingleCharacter() }
+        .map { it.lowercase() }
+        .forEach(actionsDictionary::addWord)
+
+      isFinished = true
+    }
+  }
 }
 
 private class RuntimeActionsDictionaryProvider : RuntimeDictionaryProvider {
@@ -110,5 +110,8 @@ private class RuntimeActionsDictionaryProvider : RuntimeDictionaryProvider {
     return serviceIfCreated<ActionsLanguageModel>()?.let { arrayOf(it) } ?: arrayOf()
   }
 }
+
+private fun getProject(): Project = IdeFocusManager.getGlobalInstance().lastFocusedFrame?.project
+                                    ?: ProjectManager.getInstance().defaultProject
 
 private fun String.isSingleCharacter(): Boolean = this.length == 1
