55d172468efdaed712a9049538b61af2faefff62
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-04-21 15:07:13 +0200

    cleanup
    GitOrigin-RevId: 1032878d6bd5217eb1b6c79000709ef1d4f756a3

--- platform/tips-of-the-day/src/com/intellij/ide/actions/ShowTipsAction.kt
+++ platform/tips-of-the-day/src/com/intellij/ide/actions/ShowTipsAction.kt
@@ -1,33 +1,28 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.ide.actions;
+package com.intellij.ide.actions
 
-import com.intellij.ide.IdeBundle;
-import com.intellij.ide.TipsOfTheDayUsagesCollector;
-import com.intellij.ide.util.TipAndTrickManager;
-import com.intellij.openapi.actionSystem.ActionUpdateThread;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.Task;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.ide.IdeBundle
+import com.intellij.ide.TipsOfTheDayUsagesCollector
+import com.intellij.ide.util.TipAndTrickManager.Companion.getInstance
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.Task
+import com.intellij.openapi.project.DumbAware
 
-final class ShowTipsAction extends AnAction implements DumbAware {
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
-    Project project = e.getProject();
-    new Task.Backgroundable(project, IdeBundle.message("tip.of.the.day.progress.title"), false) {
-      @Override
-      public void run(@NotNull ProgressIndicator indicator) {
-        TipsOfTheDayUsagesCollector.triggerDialogShown(TipsOfTheDayUsagesCollector.DialogType.manually);
-        TipAndTrickManager.Companion.getInstance().showTipDialog(project);
+private class ShowTipsAction : AnAction(), DumbAware {
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project
+    object : Task.Backgroundable(project, IdeBundle.message("tip.of.the.day.progress.title"), false) {
+      override fun run(indicator: ProgressIndicator) {
+        TipsOfTheDayUsagesCollector.triggerDialogShown(TipsOfTheDayUsagesCollector.DialogType.manually)
+        getInstance().showTipDialog(project)
       }
-    }.queue();
+    }.queue()
   }
 
-  @Override
-  public @NotNull ActionUpdateThread getActionUpdateThread() {
-    return ActionUpdateThread.BGT;
+  override fun getActionUpdateThread(): ActionUpdateThread {
+    return ActionUpdateThread.BGT
   }
 }
