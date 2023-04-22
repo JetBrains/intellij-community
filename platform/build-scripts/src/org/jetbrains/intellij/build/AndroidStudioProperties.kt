26adb46eb05a2ac04dd8c5a4f76200c08c7aee82
Jacob Logsdon <jlogs@google.com>
2023-04-22 03:38:20 +0000

    Increase -Xmx arg from 1280m to 2048m
    Bug: 274154546
    Change-Id: I349363202c45df9400c2bfe3d210493ec6661741

--- platform/build-scripts/src/org/jetbrains/intellij/build/AndroidStudioProperties.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/AndroidStudioProperties.kt
@@ -87,7 +87,7 @@ class AndroidStudioProperties(home: Path) : BaseIdeaProperties() {
 
     allLibraryLicenses += AndroidStudioLibraryLicenses.LICENSES_LIST
     includeIntoSourcesArchiveFilter = BiPredicate { _, _ -> true }
-    customJvmMemoryOptions = customJvmMemoryOptions.putAll(arrayOf("-Xms" to "256m", "-Xmx" to "1280m"))
+    customJvmMemoryOptions = customJvmMemoryOptions.putAll(arrayOf("-Xms" to "256m", "-Xmx" to "2048m"))
     additionalIdeJvmArguments = mutableListOf("-XX:FlightRecorderOptions=stackdepth=256")
 
     productLayout.productImplementationModules =
