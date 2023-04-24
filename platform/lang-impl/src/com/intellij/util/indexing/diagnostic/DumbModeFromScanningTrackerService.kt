a9089b0a42759ca8536ccaf0b8faf2141f8bb258
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2023-04-24 19:12:17 +0200

    More info for DumbModeFromScanningTrackerService in integration tests. Part of IDEA-316502
    GitOrigin-RevId: 714ea3d41dc540ab967ba856abdb03664b14600e

--- platform/lang-impl/src/com/intellij/util/indexing/diagnostic/DumbModeFromScanningTrackerService.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/diagnostic/DumbModeFromScanningTrackerService.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.indexing.diagnostic
 
+import com.intellij.openapi.application.ex.ApplicationManagerEx
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -27,14 +28,22 @@ class DumbModeFromScanningTrackerService(private val project: Project, cs: Corou
   }
 
   fun setScanningDumbModeStartCallback(callback: Runnable) {
-    val oldValue = dumbModeStartCallback.compareAndExchange(null, callback)
+    if (ApplicationManagerEx.isInIntegrationTest()) {
+      thisLogger().info("setScanningDumbModeStartCallback: $callback")
+    }
+
+    val oldValue = dumbModeStartCallback.getAndSet(callback)
     if (oldValue != null) {
       thisLogger().error("Callback already provided: $oldValue")
     }
   }
 
   fun cleanScanningDumbModeStartCallback(callback: Runnable) {
-    val oldValue = dumbModeStartCallback.compareAndExchange(callback, null)
+    if (ApplicationManagerEx.isInIntegrationTest()) {
+      thisLogger().info("cleanScanningDumbModeStartCallback: $callback")
+    }
+
+    val oldValue = dumbModeStartCallback.getAndSet(null)
     if (oldValue != callback) {
       thisLogger().error("Callback already removed: $oldValue")
     }
