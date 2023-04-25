587e6bca201d458c814e15d7eccce25579df0868
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-04-25 18:19:22 +0200

    [intention-preview] IDEA-318882 adjust position
    GitOrigin-RevId: fcdb9a1d5ae9bef504eb6fda99f5d0f25d99726c

--- platform/lang-impl/src/com/intellij/codeInsight/intention/impl/preview/IntentionPreviewPopupUpdateProcessor.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/intention/impl/preview/IntentionPreviewPopupUpdateProcessor.kt
@@ -23,6 +23,7 @@ import com.intellij.openapi.ui.popup.JBPopup
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.psi.PsiFile
+import com.intellij.ui.ExperimentalUI
 import com.intellij.ui.ScreenUtil
 import com.intellij.ui.popup.PopupPositionManager.Position.LEFT
 import com.intellij.ui.popup.PopupPositionManager.Position.RIGHT
@@ -32,6 +33,7 @@ import com.intellij.util.concurrency.AppExecutorUtil
 import com.intellij.util.ui.UIUtil
 import org.jetbrains.annotations.TestOnly
 import java.awt.Dimension
+import java.awt.Point
 import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
 import javax.swing.JWindow
@@ -62,10 +64,14 @@ class IntentionPreviewPopupUpdateProcessor(private val project: Project,
 
       component.multiPanel.select(LOADING_PREVIEW, true)
 
-      popup = JBPopupFactory.getInstance().createComponentPopupBuilder(component, null)
+      val popupBuilder = JBPopupFactory.getInstance().createComponentPopupBuilder(component, null)
         .setCancelCallback { cancel() }
         .setCancelKeyEnabled(false)
-        .setShowBorder(true)
+        .setShowBorder(false)
+      if (ExperimentalUI.isNewUI()) {
+        popupBuilder.setShowBorder(true)
+      }
+      popup = popupBuilder
         .addUserData(IntentionPreviewPopupKey())
         .createPopup()
 
@@ -74,15 +80,11 @@ class IntentionPreviewPopupUpdateProcessor(private val project: Project,
           var size = popup.size
           val key = component.multiPanel.key
           if (key != NO_PREVIEW) {
-            size = Dimension(size.width.coerceAtLeast(MIN_WIDTH), size.height)
+           size = Dimension(size.width.coerceAtLeast(MIN_WIDTH), size.height)
           }
-          if (popup.content.preferredSize != size) {
-            popup.content.preferredSize = size
-          }
-          if (popup.size != size) {
-            adjustPosition(originalPopup)
-            popup.size = size
-          }
+          popup.content.preferredSize = size
+          popup.size = size
+          adjustPosition(originalPopup, size)
         }
       })
       adjustPosition(originalPopup)
@@ -106,8 +108,26 @@ class IntentionPreviewPopupUpdateProcessor(private val project: Project,
       .submit(AppExecutorUtil.getAppExecutorService())
   }
 
-  private fun adjustPosition(originalPopup: JBPopup?) {
+  private fun adjustPosition(originalPopup: JBPopup?, size: Dimension? = null) {
     if (originalPopup != null && originalPopup.content.isShowing) {
+      if (size != null) {
+        val positionAdjuster = PositionAdjuster(originalPopup.content)
+        val adjustBounds = positionAdjuster.adjustBounds(Dimension(size.width, size.height), arrayOf(LEFT, RIGHT))
+        if (adjustBounds.size.width >= MIN_WIDTH) {
+          PositionAdjuster(originalPopup.content).adjust(popup, RIGHT, LEFT)
+          return
+        }
+        val adjustBoundsRight = positionAdjuster.adjustBounds(size, arrayOf(RIGHT))
+        if (adjustBounds == adjustBoundsRight) {
+          val newX = adjustBounds.x + (adjustBounds.size.width - MIN_WIDTH)
+          popup.setLocation(Point(newX, adjustBoundsRight.y))
+        }
+        else {
+          PositionAdjuster(originalPopup.content).adjust(popup, RIGHT, LEFT)
+          return
+        }
+        return
+      }
       PositionAdjuster(originalPopup.content).adjust(popup, RIGHT, LEFT)
     }
   }
@@ -195,7 +215,7 @@ class IntentionPreviewPopupUpdateProcessor(private val project: Project,
         override fun recalculationEnds() {
           val height = (it as EditorImpl).offsetToXY(it.document.textLength).y + it.lineHeight + (it.lineHeight / 3)
           val dimension = Dimension(it.component.preferredSize.width, min(height, MAX_HEIGHT))
-          if (dimension != it.component.preferredSize) {
+          if (dimension != it.component.preferredSize || ExperimentalUI.isNewUI()) {
             it.component.preferredSize = dimension
             it.component.parent.invalidate()
             popup.pack(true, true)
