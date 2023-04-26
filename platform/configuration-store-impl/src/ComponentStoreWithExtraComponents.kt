48d1f36150bddd4a65275e9882f16e0cf047a4d2
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-04-26 17:28:41 +0200

    simplify handling of deprecated SettingsSavingComponent
    GitOrigin-RevId: fc2e82445a6500e25cb89075da5bdb4d697e0013

--- platform/configuration-store-impl/src/ComponentStoreWithExtraComponents.kt
+++ platform/configuration-store-impl/src/ComponentStoreWithExtraComponents.kt
@@ -1,28 +1,21 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.configurationStore
 
-import com.intellij.openapi.application.EDT
 import com.intellij.openapi.components.RoamingType
 import com.intellij.openapi.components.ServiceDescriptor
 import com.intellij.openapi.diagnostic.getOrLogException
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.openapi.progress.blockingContext
 import com.intellij.serviceContainer.ComponentManagerImpl
 import com.intellij.util.concurrency.SynchronizedClearableLazy
-import com.intellij.util.containers.ContainerUtil
 import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 
 // A way to remove obsolete component data.
 internal val OBSOLETE_STORAGE_EP = ExtensionPointName<ObsoleteStorageBean>("com.intellij.obsoleteStorage")
 
 abstract class ComponentStoreWithExtraComponents : ComponentStoreImpl() {
-  @Suppress("DEPRECATION")
-  private val settingsSavingComponents = ContainerUtil.createLockFreeCopyOnWriteList<com.intellij.openapi.components.SettingsSavingComponent>()
-
   protected abstract val serviceContainer: ComponentManagerImpl
 
   private val asyncSettingsSavingComponents = SynchronizedClearableLazy {
@@ -31,15 +24,21 @@ abstract class ComponentStoreWithExtraComponents : ComponentStoreImpl() {
       if (instance is SettingsSavingComponent) {
         result.add(instance)
       }
+      else if (instance is @Suppress("DEPRECATION") com.intellij.openapi.components.SettingsSavingComponent) {
+        result.add(object : SettingsSavingComponent {
+          override suspend fun save() {
+            blockingContext {
+              instance.save()
+            }
+          }
+        })
+      }
     }
     result
   }
 
   final override fun initComponent(component: Any, serviceDescriptor: ServiceDescriptor?, pluginId: PluginId?) {
-    if (component is @Suppress("DEPRECATION") com.intellij.openapi.components.SettingsSavingComponent) {
-      settingsSavingComponents.add(component)
-    }
-    else if (component is SettingsSavingComponent) {
+    if (component is SettingsSavingComponent) {
       asyncSettingsSavingComponents.drop()
     }
 
@@ -56,27 +55,14 @@ abstract class ComponentStoreWithExtraComponents : ComponentStoreImpl() {
   internal suspend fun saveSettingsSavingComponentsAndCommitComponents(result: SaveResult,
                                                                        forceSavingAllSettings: Boolean,
                                                                        saveSessionProducerManager: SaveSessionProducerManager) {
-    coroutineScope {
-      // expects EDT
-      launch(Dispatchers.EDT) {
-        for (settingsSavingComponent in settingsSavingComponents) {
-          runAndCollectException(result) {
-            settingsSavingComponent.save()
-          }
-        }
-      }
-
-      launch {
-        for (settingsSavingComponent in asyncSettingsSavingComponents.value) {
-          runAndCollectException(result) {
-            settingsSavingComponent.save()
-          }
-        }
+    for (settingsSavingComponent in asyncSettingsSavingComponents.value) {
+      runAndCollectException(result) {
+        settingsSavingComponent.save()
       }
     }
 
-    // SchemeManager (asyncSettingsSavingComponent) must be saved before saving components (component state uses scheme manager in an ipr project, so, we must save it before)
-    // so, call it sequentially, not inside coroutineScope
+    // SchemeManager (asyncSettingsSavingComponent) must be saved before saving components
+    // (component state uses scheme manager in an ipr project, so, we must save it before) so, call it sequentially
     commitComponentsOnEdt(result, forceSavingAllSettings, saveSessionProducerManager)
   }
 
