434ebd9f53d66256e02a48fb3addce8b1b417307
Roman Efremov <roman.efremov@jetbrains.com>
2023-04-26 17:09:31 +0200

    Fix exception caused by EnumEntries resolve
    For .kts support extra kotlin-stdlib.jar is added. As a result,
    IdeStdlibClassFinderImpl can find two EnumEntries classes.
    Class from .kts stdlib will cause exception in `ideService()` call,
    because it doesn't belong to any module.
    
    ^KTIJ-25083 Fixed
    
    GitOrigin-RevId: 04499d5a81029951b83d5e1790c7176e68392341

--- plugins/kotlin/base/fe10/analysis/src/org/jetbrains/kotlin/idea/caches/resolve/IdeStdlibClassFinderImpl.kt
+++ plugins/kotlin/base/fe10/analysis/src/org/jetbrains/kotlin/idea/caches/resolve/IdeStdlibClassFinderImpl.kt
@@ -6,6 +6,8 @@ import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.search.ProjectScope
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
+import org.jetbrains.kotlin.descriptors.findClassAcrossModuleDependencies
+import org.jetbrains.kotlin.idea.base.projectStructure.moduleInfoOrNull
 import org.jetbrains.kotlin.idea.caches.resolve.util.javaResolutionFacade
 import org.jetbrains.kotlin.idea.resolve.ideService
 import org.jetbrains.kotlin.load.java.structure.impl.JavaClassImpl
@@ -17,9 +19,15 @@ internal class IdeStdlibClassFinderImpl(
   private val project: Project,
 ) : StdlibClassFinder {
     override fun findEnumEntriesClass(moduleDescriptor: ModuleDescriptor): ClassDescriptor? {
+        moduleDescriptor.findClassAcrossModuleDependencies(StandardClassIds.EnumEntries)?.let { return it }
+
         val javaPsiFacade = JavaPsiFacade.getInstance(project)
         val libScope = ProjectScope.getLibrariesScope(javaPsiFacade.project)
-        val psiClass = javaPsiFacade.findClass(StandardClassIds.EnumEntries.asFqNameString(), libScope) ?: return null
-        return psiClass.javaResolutionFacade()?.ideService<JavaDescriptorResolver>()?.resolveClass(JavaClassImpl(psiClass))
+        val psiClasses = javaPsiFacade.findClasses(StandardClassIds.EnumEntries.asFqNameString(), libScope)
+        return psiClasses.asSequence()
+            // Filter out class not belonging to module which comes from embedded kotlin-stdlib.jar added for .kts support
+            .filter { it.moduleInfoOrNull != null }
+            .map { it.javaResolutionFacade()?.ideService<JavaDescriptorResolver>()?.resolveClass(JavaClassImpl(it)) }
+            .firstOrNull()
     }
 }
