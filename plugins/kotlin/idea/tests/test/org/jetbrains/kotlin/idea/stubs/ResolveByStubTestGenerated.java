4b078d7f66ea2d099bed496000e2b83de14fa23c
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2023-04-26 13:50:21 +0200

    Regenerated tests
    GitOrigin-RevId: 89577811a75868d60f1a28f2632f87956130fbb7

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/stubs/ResolveByStubTestGenerated.java
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/stubs/ResolveByStubTestGenerated.java
@@ -2350,31 +2350,31 @@ public abstract class ResolveByStubTestGenerated extends AbstractResolveByStubTe
     }
 
     @RunWith(JUnit3RunnerWithInners.class)
-    @TestMetadata("../../../../out/kotlinc-testdata-2/compiler/testData/loadJava/compiledKotlin/nested")
-    public static class Nested extends AbstractResolveByStubTest {
+    @TestMetadata("../../../../out/kotlinc-testdata-2/compiler/testData/loadJava/compiledKotlin/nestedClasses")
+    public static class NestedClasses extends AbstractResolveByStubTest {
         private void runTest(String testDataFilePath) throws Exception {
             KotlinTestUtils.runTest(this::doTest, this, testDataFilePath);
         }
 
         @Override
         protected void setUp() {
-            compilerTestData("compiler/testData/loadJava/compiledKotlin/nested");
+            compilerTestData("compiler/testData/loadJava/compiledKotlin/nestedClasses");
             super.setUp();
         }
 
         @TestMetadata("deepInnerGeneric.kt")
         public void testDeepInnerGeneric() throws Exception {
-            runTest(compilerTestData("compiler/testData/loadJava/compiledKotlin/nested/deepInnerGeneric.kt"));
+            runTest(compilerTestData("compiler/testData/loadJava/compiledKotlin/nestedClasses/deepInnerGeneric.kt"));
         }
 
         @TestMetadata("innerClassReferencesOuterTP.kt")
         public void testInnerClassReferencesOuterTP() throws Exception {
-            runTest(compilerTestData("compiler/testData/loadJava/compiledKotlin/nested/innerClassReferencesOuterTP.kt"));
+            runTest(compilerTestData("compiler/testData/loadJava/compiledKotlin/nestedClasses/innerClassReferencesOuterTP.kt"));
         }
 
         @TestMetadata("membersReferenceOuterTP.kt")
         public void testMembersReferenceOuterTP() throws Exception {
-            runTest(compilerTestData("compiler/testData/loadJava/compiledKotlin/nested/membersReferenceOuterTP.kt"));
+            runTest(compilerTestData("compiler/testData/loadJava/compiledKotlin/nestedClasses/membersReferenceOuterTP.kt"));
         }
     }
 
