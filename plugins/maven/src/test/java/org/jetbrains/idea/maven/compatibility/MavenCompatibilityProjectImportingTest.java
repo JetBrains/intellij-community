603d39b43ac340fd648344aee15a373e2bee42b5
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-04-26 16:01:02 +0200

    [maven] 4.0.0-alpha-5 is currently the latest version
    GitOrigin-RevId: 85ffaa85392224387c4a15d4aa8ca4919ac8a354

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/compatibility/MavenCompatibilityProjectImportingTest.java
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/compatibility/MavenCompatibilityProjectImportingTest.java
@@ -34,7 +34,7 @@ public class MavenCompatibilityProjectImportingTest extends MavenImportingTestCa
   @Parameterized.Parameters(name = "with Maven-{0}")
   public static List<String[]> getMavenVersions() {
     return Arrays.asList(
-     // new String[]{"4.0.0-alpha-4"},
+     // new String[]{"4.0.0-alpha-5"},
       new String[]{"3.9.1"},
       new String[]{"3.9.0"},
       new String[]{"3.8.6"},
