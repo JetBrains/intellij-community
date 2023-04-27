53d6b3436e4ddf4425dd3bcdd3320388a6de0cd8
Vladislav.Yaroshchuk <vladislav.yaroshchuk@jetbrains.com>
2023-04-27 17:12:14 +0400

    [java] IDEA-269182 ResolveAllRepositoryLibrariesAction: make invisible if a project is not opened
    Fix after IJ-CR-106163 review
    
    GitOrigin-RevId: 010a267053a1144f86ffb27cd2758dcdfbb3e558

--- java/idea-ui/src/com/intellij/ide/actions/ResolveAllRepositoryLibrariesAction.kt
+++ java/idea-ui/src/com/intellij/ide/actions/ResolveAllRepositoryLibrariesAction.kt
@@ -9,6 +9,10 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 class ResolveAllRepositoryLibrariesAction : AnAction() {
   override fun getActionUpdateThread() = ActionUpdateThread.BGT
 
+  override fun update(e: AnActionEvent) {
+    e.presentation.isEnabledAndVisible = e.project != null
+  }
+
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.project ?: return
     RepositoryLibraryUtils.getInstance(project).resolveAllBackground()
