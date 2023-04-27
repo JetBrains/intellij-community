3a5a108f96838a66ae072cd4ca05230b5a950f43
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2023-04-27 14:19:31 +0200

    [core] IDEA-318658: workaround for inability to delete mapped file on Win
    + //noinspection for System.gc() call
    
    GitOrigin-RevId: 327516c9cdf4d23bbdb54d2941e65ec1a7ac4e9c

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/PersistentFSRecordsLockFreeOverMMappedFile.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/PersistentFSRecordsLockFreeOverMMappedFile.java
@@ -625,6 +625,8 @@ public class PersistentFSRecordsLockFreeOverMMappedFile implements PersistentFSR
       //On Win there are a lot of issues with removing file that was mmapped.
       // Let's give mapped buffers at least a chance to be collected & unmapped -- not a guarantee from that kind of
       // issues, but a small step in the right direction
+      
+      //noinspection CallToSystemGC
       System.gc();
     }
     FileUtil.delete(storage.storagePath);
