9a6a541e693c86ff46111aa67e929e676688564a
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2023-04-27 18:30:46 +0200

    git-branches-dashboard: avoid empty text in remote actions (EA-729915)
    Follow-up: 17b374b5bc65f6c875ff749723f1cafd4c52a51a
    
    GitOrigin-RevId: b428a6b6b3268d8d90d168fc26132b7da25341f2

--- plugins/git4idea/src/git4idea/ui/branch/dashboard/BranchesDashboardActions.kt
+++ plugins/git4idea/src/git4idea/ui/branch/dashboard/BranchesDashboardActions.kt
@@ -535,7 +535,7 @@ internal object BranchesDashboardActions {
     }
   }
 
-  class RemoveRemoteAction : RemoteActionBase() {
+  class RemoveRemoteAction : RemoteActionBase(messagePointer("action.Git.Log.Remove.Remote.text", 0)) {
 
     override fun update(e: AnActionEvent, project: Project, selectedRemotes: Map<GitRepository, Set<GitRemote>>) {
       e.presentation.text = message("action.Git.Log.Remove.Remote.text", selectedRemotes.size)
@@ -562,7 +562,7 @@ internal object BranchesDashboardActions {
     }
   }
 
-  abstract class RemoteActionBase(text: () -> @Nls(capitalization = Nls.Capitalization.Title) String = { "" },
+  abstract class RemoteActionBase(text: () -> @Nls(capitalization = Nls.Capitalization.Title) String,
                                   private val description: () -> @Nls(capitalization = Nls.Capitalization.Sentence) String = { "" },
                                   icon: Icon? = null) :
     DumbAwareAction(text, description, icon) {
