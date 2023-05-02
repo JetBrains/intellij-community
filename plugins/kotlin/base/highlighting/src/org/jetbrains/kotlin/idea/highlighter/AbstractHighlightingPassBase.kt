24e8cdeb85ba79e86088339227d3919f2c1ac5b4
Alexey Kudravtsev <cdr@intellij.com>
2023-05-02 15:27:52 +0200

    remove unused methods to be able to refactor
    GitOrigin-RevId: 868eebe164e58a7553bb179ddfd954a45c7dedea

--- plugins/kotlin/base/highlighting/src/org/jetbrains/kotlin/idea/highlighter/AbstractHighlightingPassBase.kt
+++ plugins/kotlin/base/highlighting/src/org/jetbrains/kotlin/idea/highlighter/AbstractHighlightingPassBase.kt
@@ -6,7 +6,6 @@ import com.intellij.codeHighlighting.TextEditorHighlightingPass
 import com.intellij.codeInsight.daemon.impl.AnnotationHolderImpl
 import com.intellij.codeInsight.daemon.impl.HighlightInfo
 import com.intellij.codeInsight.daemon.impl.UpdateHighlightersUtil
-import com.intellij.lang.annotation.Annotation
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.AnnotationSession
 import com.intellij.openapi.application.ApplicationManager
@@ -14,7 +13,6 @@ import com.intellij.openapi.editor.Document
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.DumbAware
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiRecursiveElementVisitor
 import org.jetbrains.annotations.TestOnly
 import org.jetbrains.kotlin.psi.KtFile
 
@@ -25,47 +23,24 @@ abstract class AbstractHighlightingPassBase(
     protected val file: KtFile,
     document: Document
 ) : TextEditorHighlightingPass(file.project, document), DumbAware {
-    private val highlightInfos: MutableList<HighlightInfo> = mutableListOf()
-    protected var annotationCallback: ((Annotation) -> Unit)? = null
     private var annotationHolder: AnnotationHolderImpl? = null
 
-    fun annotationCallback(callback: (Annotation) -> Unit) {
-        annotationCallback = callback
-    }
-
-    fun resetAnnotationCallback() {
-        annotationCallback = null
-    }
-
     override fun doCollectInformation(progress: ProgressIndicator) {
-        highlightInfos.clear()
-
         // TODO: YES, IT USES `@ApiStatus.Internal` AnnotationHolderImpl intentionally:
         //  there is no other way to highlight:
         //  - HighlightInfo could not be highlighted immediately as myHighlightInfoProcessor.infoIsAvailable is not accessible
         //  (HighlightingSessionImpl impl is closed) and/or UpdateHighlightersUtil.addHighlighterToEditorIncrementally is closed as well.
         //  therefore direct usage of AnnotationHolderImpl is the smallest evil
 
-        val annotationHolder = object : AnnotationHolderImpl(AnnotationSession(file), false) {
-            override fun add(element: Annotation?): Boolean {
-                element?.let { annotationCallback?.invoke(it) }
-                return super.add(element)
-            }
-        }
+        val annotationHolder = AnnotationHolderImpl(AnnotationSession(file), false)
         annotationHolder.runAnnotatorWithContext(file) { element, holder ->
             runAnnotatorWithContext(element, holder)
         }
         this.annotationHolder = annotationHolder
     }
 
-    protected open fun runAnnotatorWithContext(
-        element: PsiElement,
-        holder: AnnotationHolder
-    ) {
-        element.accept(object : PsiRecursiveElementVisitor() {})
+    protected open fun runAnnotatorWithContext(element: PsiElement, holder: AnnotationHolder) {
     }
-
-    override fun getInfos(): MutableList<HighlightInfo> = highlightInfos
 
     companion object {
         @Volatile
@@ -94,7 +69,6 @@ abstract class AbstractHighlightingPassBase(
         }
         try {
             val infos = annotationHolder?.map { HighlightInfo.fromAnnotation(it) } ?: return
-            highlightInfos.addAll(infos)
             UpdateHighlightersUtil.setHighlightersToEditor(myProject, myDocument, 0, file.textLength, infos, colorsScheme, id)
         } finally {
             annotationHolder = null
