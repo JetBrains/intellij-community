c8bd74fb3b4b367553a657fc87d232483a18d8fd
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-05-02 16:19:55 +0200

    rethrow CancellationException
    (cherry picked from commit 9ebeaac993da0f3921c4df0857b4cd54eba1dc48)
    
    IJ-CR-106939
    
    GitOrigin-RevId: b7205fbe25233d46e7299dda35bef5488da9df37

--- platform/configuration-store-impl/src/schemeManager/SchemeManagerImpl.kt
+++ platform/configuration-store-impl/src/schemeManager/SchemeManagerImpl.kt
@@ -35,6 +35,7 @@ import java.nio.file.FileSystemException
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.*
+import java.util.concurrent.CancellationException
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.function.Function
 import java.util.function.Predicate
@@ -336,6 +337,9 @@ class SchemeManagerImpl<T: Scheme, MUTABLE_SCHEME : T>(
     for (scheme in changedSchemes) {
       try {
         saveScheme(scheme, nameGenerator, filesToDelete)
+      }
+      catch (e: CancellationException) {
+        throw e
       }
       catch (e: Throwable) {
         errors.add(RuntimeException("Cannot save scheme $fileSpec/$scheme", e))
