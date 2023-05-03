123973f4f956927c35e2566cdf9c1dc4e50b6fd0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-05-03 13:41:18 +0200

    cleanup
    GitOrigin-RevId: 0c73cb9513f2e2497927d459a41fb81ff4d1b68c

--- platform/credential-store/src/credentialStore.kt
+++ platform/credential-store/src/credentialStore.kt
@@ -11,7 +11,7 @@ import java.security.SecureRandom
 internal val LOG: Logger
   get() = logger<PasswordSafeSettings>()
 
-fun joinData(user: String?, password: OneTimeString?): ByteArray? {
+internal fun joinData(user: String?, password: OneTimeString?): ByteArray? {
   if (user == null && password == null) {
     return null
   }
@@ -30,7 +30,7 @@ fun joinData(user: String?, password: OneTimeString?): ByteArray? {
   return buffer.toByteArray()
 }
 
-fun splitData(data: String?): Credentials? {
+internal fun splitData(data: String?): Credentials? {
   if (data.isNullOrEmpty()) {
     return null
   }
@@ -72,10 +72,6 @@ private fun parseString(data: String, @Suppress("SameParameterValue") delimiter:
   return result
 }
 
-// check isEmpty before
-@JvmOverloads
-fun Credentials.serialize(storePassword: Boolean = true): ByteArray = joinData(userName, if (storePassword) password else null)!!
-
 fun createSecureRandom(): SecureRandom {
   // do not use SecureRandom.getInstanceStrong()
   // https://tersesystems.com/blog/2015/12/17/the-right-way-to-use-securerandom/
@@ -89,6 +85,3 @@ internal fun SecureRandom.generateBytes(size: Int): ByteArray {
   nextBytes(result)
   return result
 }
-
-val ACCESS_TO_KEY_CHAIN_DENIED: Credentials = Credentials.ACCESS_TO_KEY_CHAIN_DENIED
-val CANNOT_UNLOCK_KEYCHAIN: Credentials = Credentials.CANNOT_UNLOCK_KEYCHAIN
