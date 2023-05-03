bcb8de9a1b4ad6eee6744f385723cfef626ea8da
Sviatoslav Vlasov <sviatoslav.vlasov@jetbrains.com>
2023-05-03 16:30:21 +0200

    [lux] Make FileChooserDialog per-client
    GitOrigin-RevId: f2a6d9f1f2213f95fb74caf0db7be7cd62d7ab0c

---
+++ platform/platform-impl/src/com/intellij/openapi/fileChooser/ClientFileChooserFactory.kt
@@ -0,0 +1,71 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.fileChooser
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.PathMacros
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.mac.MacPathChooserDialog
+import com.intellij.ui.win.WinPathChooserDialog
+import java.awt.Component
+import javax.swing.JTextField
+
+interface ClientFileChooserFactory {
+  companion object {
+    @JvmStatic
+    val instance: ClientFileChooserFactory = service()
+
+    @JvmStatic
+    fun useNewChooser(descriptor: FileChooserDescriptor): Boolean {
+      return Registry.`is`("ide.ui.new.file.chooser") &&
+             descriptor.roots.all({ f: VirtualFile -> f.isInLocalFileSystem })
+    }
+
+    @JvmStatic
+    fun useNativeWinChooser(descriptor: FileChooserDescriptor): Boolean {
+      return SystemInfo.isWindows &&
+             !descriptor.isForcedToUseIdeaFileChooser &&
+             Registry.`is`("ide.win.file.chooser.native", false)
+    }
+
+    @JvmStatic
+    fun useNativeMacChooser(descriptor: FileChooserDescriptor): Boolean {
+      return SystemInfo.isMac &&
+             SystemInfo.isJetBrainsJvm &&
+             !descriptor.isForcedToUseIdeaFileChooser &&
+             Registry.`is`("ide.mac.file.chooser.native", true)
+    }
+
+    @JvmStatic
+    fun createNativePathChooserIfEnabled(descriptor: FileChooserDescriptor, project: Project?, parent: Component?): PathChooserDialog? {
+      return if (useNativeMacChooser(descriptor)) {
+        MacPathChooserDialog(descriptor, parent, project)
+      }
+      else if (useNativeWinChooser(descriptor)) {
+        WinPathChooserDialog(descriptor, parent, project)
+      }
+      else null
+    }
+
+    @JvmStatic
+    fun getMacroMap(): Map<String, String?> {
+      val macros = PathMacros.getInstance()
+      val allNames = macros.allMacroNames
+      val map: MutableMap<String, String?> = HashMap(allNames.size)
+      for (eachMacroName in allNames) {
+        map["$$eachMacroName$"] = macros.getValue(eachMacroName)
+      }
+      return map
+    }
+  }
+
+  fun createFileChooser(descriptor: FileChooserDescriptor, project: Project?, parent: Component?): FileChooserDialog
+  fun createPathChooser(descriptor: FileChooserDescriptor, project: Project?, parent: Component?): PathChooserDialog
+  fun createSaveFileDialog(descriptor: FileSaverDescriptor, project: Project?): FileSaverDialog
+  fun createSaveFileDialog(descriptor: FileSaverDescriptor, parent: Component): FileSaverDialog
+  fun createFileTextField(descriptor: FileChooserDescriptor, showHidden: Boolean, parent: Disposable?): FileTextField
+  fun installFileCompletion(field: JTextField, descriptor: FileChooserDescriptor, showHidden: Boolean, parent: Disposable?)
+}
