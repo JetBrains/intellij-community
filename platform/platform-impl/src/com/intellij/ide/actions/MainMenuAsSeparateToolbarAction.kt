46884760743f5b1f634f1448834b86090a637d88
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2023-05-03 22:13:20 +0200

    IDEA-319249 Support Horizontal main menu on Linux
    - Added restart message for `Menu as Separate Toolbar` menu item
    
    (cherry picked from commit 972c955bcb7c671a097c538175655bf0dd90aa0a)
    
    IJ-CR-107045
    
    GitOrigin-RevId: 8f19617f2dc8bd9ab9742d58890eb01b349da572

--- platform/platform-impl/src/com/intellij/ide/actions/MainMenuAsSeparateToolbarAction.kt
+++ platform/platform-impl/src/com/intellij/ide/actions/MainMenuAsSeparateToolbarAction.kt
@@ -1,12 +1,18 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.actions
 
+import com.intellij.CommonBundle
+import com.intellij.ide.IdeBundle
 import com.intellij.ide.ui.UISettings
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ToggleAction
+import com.intellij.openapi.application.ApplicationNamesInfo
+import com.intellij.openapi.application.ex.ApplicationManagerEx
 import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.util.SystemInfoRt
 import com.intellij.ui.ExperimentalUI
 
 class MainMenuAsSeparateToolbarAction : ToggleAction(), DumbAware {
@@ -16,9 +22,36 @@ class MainMenuAsSeparateToolbarAction : ToggleAction(), DumbAware {
   }
 
   override fun setSelected(e: AnActionEvent, state: Boolean) {
+    var restartNeeded = false
+
+    if (SystemInfoRt.isXWindow) {
+      val result = Messages.showYesNoCancelDialog(
+        IdeBundle.message("dialog.message.must.be.restarted.for.changes.to.take.effect",
+                          ApplicationNamesInfo.getInstance().fullProductName),
+        IdeBundle.message("dialog.title.restart.required"),
+        IdeBundle.message("ide.restart.action"),
+        IdeBundle.message("ide.notnow.action"),
+        CommonBundle.getCancelButtonText(),
+        Messages.getQuestionIcon())
+
+      when (result) {
+        Messages.YES -> {
+          restartNeeded = true
+        }
+        Messages.NO -> {}
+        else -> return
+      }
+    }
+
     val uiSettings = UISettings.getInstance()
     uiSettings.separateMainMenu = state
-    uiSettings.fireUISettingsChanged()
+
+    if (restartNeeded) {
+      ApplicationManagerEx.getApplicationEx().restart(true)
+    }
+    else {
+      uiSettings.fireUISettingsChanged()
+    }
   }
 
   override fun update(e: AnActionEvent) {
