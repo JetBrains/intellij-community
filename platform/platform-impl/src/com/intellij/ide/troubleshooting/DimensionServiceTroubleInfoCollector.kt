7d528890feaaced0e19f6120be8a59987e14d901
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2023-05-04 17:35:16 +0300

    IDEA-319469 Add app frame bounds to dimension.txt
    GitOrigin-RevId: 24d0da228c12f41bac680032ab9169085e03ae7c

--- platform/platform-impl/src/com/intellij/ide/troubleshooting/DimensionServiceTroubleInfoCollector.kt
+++ platform/platform-impl/src/com/intellij/ide/troubleshooting/DimensionServiceTroubleInfoCollector.kt
@@ -6,6 +6,8 @@ import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.DimensionService
 import com.intellij.openapi.util.WindowStateService
+import com.intellij.openapi.wm.ex.WindowManagerEx
+import com.intellij.openapi.wm.impl.WindowManagerImpl
 import com.intellij.troubleshooting.GeneralTroubleInfoCollector
 import org.jdom.Element
 
@@ -13,6 +15,7 @@ internal fun collectDimensionServiceDiagnosticsData(project: Project): String {
   return CompositeGeneralTroubleInfoCollector.collectInfo(
     project,
     ProjectFrameTroubleInfoCollector(),
+    AppFrameTroubleInfoCollector(),
     WindowStateProjectServiceTroubleInfoCollector(),
     WindowStateApplicationServiceTroubleInfoCollector(),
     DimensionServiceTroubleInfoCollector()
@@ -20,11 +23,19 @@ internal fun collectDimensionServiceDiagnosticsData(project: Project): String {
 }
 
 private class ProjectFrameTroubleInfoCollector : GeneralTroubleInfoCollector {
-  override fun getTitle() = "Project Frames"
+  override fun getTitle() = "IDE Frame (per project)"
 
   override fun collectInfo(project: Project): String =
     RecentProjectsManagerBase.getInstanceEx().state.additionalInfo.entries
       .joinToString("\n") { entry -> "${entry.key}: ${entry.value.frame?.toString()}" }
+
+}
+
+private class AppFrameTroubleInfoCollector : GeneralTroubleInfoCollector {
+  override fun getTitle() = "IDE Frame (per app)"
+
+  override fun collectInfo(project: Project): String =
+    (WindowManagerEx.getInstanceEx() as? WindowManagerImpl).serviceToTroubleshootingString()
 
 }
 
