f13bc1a29521992413f3bbe1c39a4e4d9c02de7f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-05-04 13:13:09 +0200

    use serviceAsync
    GitOrigin-RevId: ad17f62dfc4639775ac6df1206d3d08fcd6c00ed

--- platform/execution-impl/src/com/intellij/execution/impl/ProjectRunConfigurationInitializer.kt
+++ platform/execution-impl/src/com/intellij/execution/impl/ProjectRunConfigurationInitializer.kt
@@ -4,8 +4,7 @@ package com.intellij.execution.impl
 import com.intellij.diagnostic.runActivity
 import com.intellij.execution.RunManager
 import com.intellij.execution.RunManager.Companion.IS_RUN_MANAGER_INITIALIZED
-import com.intellij.openapi.application.readActionBlocking
-import com.intellij.openapi.components.ComponentManagerEx
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.impl.ProjectServiceContainerInitializedListener
@@ -13,6 +12,7 @@ import kotlinx.coroutines.launch
 
 private class ProjectRunConfigurationInitializer : ProjectServiceContainerInitializedListener {
   override suspend fun execute(project: Project) {
+    @Suppress("DEPRECATION")
     project.coroutineScope.launch {
       if (IS_RUN_MANAGER_INITIALIZED.get(project) == true) {
         return@launch
@@ -20,13 +20,14 @@ private class ProjectRunConfigurationInitializer : ProjectServiceContainerInitia
 
       // wait for module manager - may be required for module level run configurations
       // it allows us to avoid thread blocking
-      // (RunManager itself cannot yet do the same, as platform doesn't provide non-blocking load state)
-      (project as ComponentManagerEx).getServiceAsync(ModuleManager::class.java).join()
+      // (RunManager itself cannot yet do the same, as the platform doesn't provide non-blocking load state)
+      project.serviceAsync<ModuleManager>().join()
 
       runActivity("RunManager initialization") {
-        // we must not fire beginUpdate here, because message bus will fire queued parent message bus messages (and, so, SOE may occur because all other projectOpened will be processed before us)
-        // simply, you should not listen changes until project opened
-        project.getService(RunManager::class.java)
+        // we must not fire beginUpdate here, because message bus will fire queued parent message bus messages
+        // (and, so, SOE may occur because all other projectOpened will be processed before us)
+        // simply, you should not listen changes until the project opened
+        project.serviceAsync<RunManager>().join()
         IS_RUN_MANAGER_INITIALIZED.set(project, true)
       }
     }
