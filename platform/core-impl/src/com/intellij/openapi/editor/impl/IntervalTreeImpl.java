3ae794ea1cc81d9d10db4c9067af85b4f11a4832
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-05-04 11:51:02 +0200

    [core] IntervalTreeImpl: remove redundant condition
    GitOrigin-RevId: 64daf9affec978adfb901e3afa82d7092f4249d1

--- platform/core-impl/src/com/intellij/openapi/editor/impl/IntervalTreeImpl.java
+++ platform/core-impl/src/com/intellij/openapi/editor/impl/IntervalTreeImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.editor.impl;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -885,9 +885,7 @@ abstract class IntervalTreeImpl<T> extends RedBlackTree<T> implements IntervalTr
       if (((RangeMarkerImpl)t).isValid()) {
         long id = ((RangeMarkerImpl)t).getId();
         boolean added = ids.add(id);
-        if (!added) {
-          assert added : t +"\nids:"+ids+"; id="+id+"\n; root.intervals="+root.intervals;
-        }
+        assert added : t +"\nids:"+ids+"; id="+id+"\n; root.intervals="+root.intervals;
       }
     }
     if (assertInvalid && liveInterval != null) {
