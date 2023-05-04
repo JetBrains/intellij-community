aa6156e1cc2057275bf412f5e1c00da50ecec213
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-05-04 22:11:00 +0200

    Cleanup (`fs::read_to_string` instead of `BufReader` in cross-platform launcher)
    GitOrigin-RevId: a4218dd934e8652f7f58f5d6bb6c1a531d1ef569

--- native/XPlatLauncher/src/docker.rs
+++ native/XPlatLauncher/src/docker.rs
@@ -15,8 +15,7 @@ use {
 use {
     anyhow::Context,
     log::{debug, info},
-    std::fs::File,
-    std::io::{BufReader, Read},
+    std::fs,
     std::path::PathBuf,
 };
 
@@ -56,10 +55,7 @@ pub fn is_control_group_matches_docker(cgroup_parent_path: Option<PathBuf>) -> R
     }
 
     // Read cgroup file content
-    let mut cgroup_content = String::new();
-    let cgroup_file = File::open(&cgroup_path)
-        .context(format!("Unable to open file '{}'", cgroup_path.display()))?;
-    let _n = BufReader::new(cgroup_file).read_to_string(&mut cgroup_content)
+    let cgroup_content = fs::read_to_string(&cgroup_path)
         .context(format!("Unable to read file '{}'", cgroup_path.display()))?;
 
     // Check file contains any groups with 'docker' or 'lxc' suffix that define a container environment.
