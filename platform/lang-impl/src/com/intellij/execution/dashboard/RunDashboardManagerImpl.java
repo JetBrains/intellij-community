6f8128b2e7519a1894049530e65c86e992475bdf
Vyacheslav Karpukhin <Vyacheslav.Karpukhin@jetbrains.com>
2023-05-05 23:17:23 +0200

    Run Dashboard: Fix unsafe cast to ExecutionManagerImpl
    GitOrigin-RevId: af4d95e307a82177bf9962e575b6381c0a44aae9

--- platform/lang-impl/src/com/intellij/execution/dashboard/RunDashboardManagerImpl.java
+++ platform/lang-impl/src/com/intellij/execution/dashboard/RunDashboardManagerImpl.java
@@ -409,9 +409,12 @@ public final class RunDashboardManagerImpl implements RunDashboardManager, Persi
   }
 
   private List<RunContentDescriptor> getConfigurationDescriptors(@NotNull RunConfiguration configuration) {
-    return ExecutionManagerImpl.getInstance(myProject).getDescriptors(s -> configuration.equals(s.getConfiguration()) ||
-                                                                           configuration.equals(
-                                                                             getBaseConfiguration(s.getConfiguration())));
+    ExecutionManager instance = ExecutionManager.getInstance(myProject);
+    if (!(instance instanceof ExecutionManagerImpl)) {
+      return Collections.emptyList();
+    }
+    return ((ExecutionManagerImpl)instance).getDescriptors(s -> configuration.equals(s.getConfiguration()) ||
+                                                                configuration.equals(getBaseConfiguration(s.getConfiguration())));
   }
 
   @Override
