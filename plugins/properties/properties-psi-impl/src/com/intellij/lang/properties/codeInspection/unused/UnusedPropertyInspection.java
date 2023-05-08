05b1a42456d00ea3da6fff14780cb0c1971ad63b
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-05-08 17:20:23 +0200

    [properties] UnusedPropertyInspection: typo in option controller fixed
    GitOrigin-RevId: a729c36dc9a64ff1d87169babb7fd3b7540e149e

--- plugins/properties/properties-psi-impl/src/com/intellij/lang/properties/codeInspection/unused/UnusedPropertyInspection.java
+++ plugins/properties/properties-psi-impl/src/com/intellij/lang/properties/codeInspection/unused/UnusedPropertyInspection.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.lang.properties.codeInspection.unused;
 
 import com.intellij.codeInspection.LocalInspectionToolSession;
@@ -63,7 +63,7 @@ public final class UnusedPropertyInspection extends PropertiesInspectionBase {
 
   @Override
   public @NotNull OptionController getOptionController() {
-    return super.getOptionController().onValueSet("fineNameMask", value -> {
+    return super.getOptionController().onValueSet("fileNameMask", value -> {
       if ("".equals(value)) fileNameMask = ".*";
     });
   }
