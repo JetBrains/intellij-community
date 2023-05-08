5ee8250c7e9088cbac4c1efeffe60bdf9f03f1c9
Alexander.Kass <Alexander.Kass@jetbrains.com>
2023-05-08 10:03:15 +0300

    stripe: cache actions
    GitOrigin-RevId: 297cbb57ea735a7d9c36496191452db0d2847ad3

--- platform/platform-impl/src/com/intellij/toolWindow/StripeActionGroup.kt
+++ platform/platform-impl/src/com/intellij/toolWindow/StripeActionGroup.kt
@@ -1,32 +1,36 @@
 package com.intellij.toolWindow
 
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.*
-import com.intellij.openapi.actionSystem.ex.CustomComponentAction
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.wm.ToolWindowAnchor
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.openapi.wm.impl.SquareStripeButton
 import com.intellij.openapi.wm.impl.ToolWindowImpl
-import com.intellij.ui.ToggleActionButton
-import javax.swing.JComponent
+import com.intellij.util.containers.ConcurrentFactoryMap
+import com.intellij.util.containers.ContainerUtil
 
 class StripeActionGroup: ActionGroup(), DumbAware {
+  private val myFactory: Map<ToolWindowImpl, AnAction> = ConcurrentFactoryMap.create(::createAction) {
+    ContainerUtil.createConcurrentWeakKeyWeakValueMap()
+  }
+
   override fun getChildren(e: AnActionEvent?): Array<AnAction> {
     val twm = e?.project?.let { ToolWindowManager.getInstance(it) } ?: return emptyArray()
-    val res = ArrayList<AnAction>()
-
     val toolWindows = twm.toolWindowIds.mapNotNullTo(ArrayList()) { twm.getToolWindow(it) as? ToolWindowImpl }
-    toolWindows.sortBy { tw -> tw.windowInfo.run { when (anchor) {
-      ToolWindowAnchor.LEFT -> 0 + order
-      ToolWindowAnchor.TOP -> 100 + order
-      ToolWindowAnchor.BOTTOM -> 200 + order
-      ToolWindowAnchor.RIGHT -> 300 + order
-      else -> -1
-    } } }
-    for (tw in toolWindows) {
-      res.add(SquareStripeButton(tw).action)
-    }
-    return res.toArray(AnAction.EMPTY_ARRAY)
+    toolWindows.sortBy(::getOrder)
+    return toolWindows.mapNotNull(myFactory::get).toTypedArray()
   }
+
+  private fun getOrder(tw: ToolWindowImpl): Int =
+    tw.windowInfo.run {
+      when (anchor) {
+        ToolWindowAnchor.LEFT -> 0 + order
+        ToolWindowAnchor.TOP -> 100 + order
+        ToolWindowAnchor.BOTTOM -> 200 + order
+        ToolWindowAnchor.RIGHT -> 300 + order
+        else -> -1
+      }
+    }
+
+  private fun createAction(tw: ToolWindowImpl) = SquareStripeButton(tw).action
 }
