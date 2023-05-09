30854e04f7844068e0d2f1ff488f41289a04b853
Artem Mukhin <Artem.Mukhin@jetbrains.com>
2023-05-09 22:19:14 +0200

    PY-60585 Add shebangs to `Attach to process` build scripts
    GitOrigin-RevId: 3e5f396d030a9474b403dd5f024b47529c5400e3

--- python/helpers/pydev/pydevd_attach_to_process/linux_and_mac/compile_mac.sh
+++ python/helpers/pydev/pydevd_attach_to_process/linux_and_mac/compile_mac.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -euxo pipefail
+
 clang++ -fPIC -D_REENTRANT -std=c++11 -arch arm64 -c -o attach_arm64.o attach.cpp
 clang++ -dynamiclib -nostartfiles -arch arm64 -o attach_arm64.dylib attach_arm64.o -lc
 rm attach_arm64.o
