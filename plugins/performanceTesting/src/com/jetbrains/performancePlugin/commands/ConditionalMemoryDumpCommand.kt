72774a2744c5c65891ce0dd41461a805d5b77de6
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-05-09 10:15:57 +0200

    Make sure that LowMemoryWatcher is not GCed
    GitOrigin-RevId: a84525cb555ecf45850cbaeb67d0eefe692598e9

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/ConditionalMemoryDumpCommand.kt
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/ConditionalMemoryDumpCommand.kt
@@ -1,11 +1,13 @@
 package com.jetbrains.performancePlugin.commands
 
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.ui.playback.PlaybackContext
+import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.LowMemoryWatcher
 import com.intellij.util.MemoryDumpHelper
 
-class ConditionalMemoryDumpCommand(text: String, line: Int) : PerformanceCommandCoroutineAdapter(text, line) {
+class ConditionalMemoryDumpCommand(text: String, line: Int) : PerformanceCommandCoroutineAdapter(text, line), Disposable {
   companion object {
     const val NAME = "conditionalMemoryDumpCommand"
     const val PREFIX = CMD_PREFIX + NAME
@@ -15,20 +17,21 @@ class ConditionalMemoryDumpCommand(text: String, line: Int) : PerformanceCommand
   override suspend fun doExecute(context: PlaybackContext) {
     val targetMessageCount = extractCommandArgument(PREFIX).toInt()
     var currentMessageCount = 0
-    var memoryDumpCollected = false
-    LowMemoryWatcher.register(
-      {
+    LowMemoryWatcher.register({
         currentMessageCount++
-        if (currentMessageCount == targetMessageCount && !memoryDumpCollected) {
-          memoryDumpCollected = true
+        if (currentMessageCount == targetMessageCount) {
           val memoryDumpPath = MemoryDumpCommand.getMemoryDumpPath()
           LOG.info("Dumping memory snapshot to: $memoryDumpPath")
           MemoryDumpHelper.captureMemoryDumpZipped(memoryDumpPath)
+          Disposer.dispose(this)
         }
-      }, LowMemoryWatcher.LowMemoryWatcherType.ONLY_AFTER_GC)
+      }, LowMemoryWatcher.LowMemoryWatcherType.ONLY_AFTER_GC, this)
   }
 
   override fun getName(): String {
     return NAME
+  }
+
+  override fun dispose() {
   }
 }
