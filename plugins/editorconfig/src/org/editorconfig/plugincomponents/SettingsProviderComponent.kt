a25974bd0052cb96c972dca1b1d47fd5e7fb2b61
Vojtech Balik <vojtech.balik@jetbrains.com>
2023-05-09 16:41:12 +0200

    [editorconfig] IDEA-318111 Allow empty values in .editorconfig properties
    GitOrigin-RevId: 3fe0b626686065440852f51265991cd220e183bd

--- plugins/editorconfig/src/org/editorconfig/plugincomponents/SettingsProviderComponent.kt
+++ plugins/editorconfig/src/org/editorconfig/plugincomponents/SettingsProviderComponent.kt
@@ -15,12 +15,12 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
 import com.intellij.util.SlowOperations
-import org.ec4j.core.EditorConfigConstants
-import org.ec4j.core.EditorConfigLoader
-import org.ec4j.core.ResourceProperties
-import org.ec4j.core.ResourcePropertiesService
+import org.ec4j.core.*
+import org.ec4j.core.model.Version
+import org.ec4j.core.parser.EditorConfigModelHandler
 import org.ec4j.core.parser.ParseException
 import org.editorconfig.EditorConfigRegistry
+import org.editorconfig.core.ec4jwrappers.EditorConfigErrorHandler
 import org.editorconfig.core.ec4jwrappers.EditorConfigPermanentCache
 import org.editorconfig.core.ec4jwrappers.VirtualFileResource
 import java.io.IOException
@@ -43,7 +43,7 @@ class SettingsProviderComponent(private val project: Project) : SimpleModificati
     .configFileName(EditorConfigConstants.EDITORCONFIG)
     .rootDirectories(getRootDirs().map { VirtualFileResource(it) })
     .cache(resourceCache)
-    .loader(EditorConfigLoader.default_())
+    .loader(EditorConfigLoader.of(Version.CURRENT, PropertyTypeRegistry.default_(), EditorConfigErrorHandler()))
     // TODO custom handling of unset values?
     .keepUnset(true)
     .build()
