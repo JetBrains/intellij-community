cc86fae1075e02d05c1edd714bcb584f63c3bc1e
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-05-09 22:29:36 +0200

    [maven] IDEA-313767 if maven server is debugged, do not kill it after watchdog timeout
    GitOrigin-RevId: 7f68765acc507cf517ebf51cabc14123a6f133f8

--- plugins/maven-server-api/src/main/java/org/jetbrains/idea/maven/server/RemoteServerUtil.java
+++ plugins/maven-server-api/src/main/java/org/jetbrains/idea/maven/server/RemoteServerUtil.java
@@ -1,8 +1,14 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.maven.server;
 
+import java.util.Arrays;
+
 public abstract class RemoteServerUtil {
   public static boolean isWSL(){
     return Boolean.parseBoolean(System.getProperty("idea.maven.wsl"));
+  }
+
+  public static boolean isDebug(String[] args){
+    return Arrays.stream(args).anyMatch(arg -> "runWithDebugger".equals(arg));
   }
 }
