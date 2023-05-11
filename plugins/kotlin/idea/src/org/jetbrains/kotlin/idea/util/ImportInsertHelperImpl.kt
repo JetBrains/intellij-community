a6d67668527b0fc8e4862c789bdbdd7e55425a56
Kurt Dresner <kdresner@google.com>
2023-05-11 13:06:08 -0700

    [kotlin] Add ability to filter classes out of automatic imports
    Currently, some actions like refactoring cause imports to happen
    as a side-effect, notably for nested classes. For certain classes,
    it is almost never correct to import them. An example here is in
    Android Studio where R.string, R.values, R.drawable, etc. are
    technically nested classes (despite the lowercase letter).
    
    This change creates a new ClassImportFilter interface and associated
    Extension Point, which plugins can use to veto the import of particular
    classes.
    
    #KTIJ-25503
    
    closes https://github.com/JetBrains/intellij-community/pull/2431
    
    GitOrigin-RevId: 09f422c47f8caed473edc87707ec6666e305b9c5

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/util/ImportInsertHelperImpl.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/util/ImportInsertHelperImpl.kt
@@ -52,15 +52,26 @@ class ImportInsertHelperImpl(private val project: Project) : ImportInsertHelper(
     }
 
     override fun mayImportOnShortenReferences(descriptor: DeclarationDescriptor, contextFile: KtFile): Boolean {
-        return when (descriptor.getImportableDescriptor()) {
+        return when (val importableDescriptor = descriptor.getImportableDescriptor()) {
             is PackageViewDescriptor -> false // now package cannot be imported
 
-            is ClassDescriptor -> {
-                descriptor.getImportableDescriptor().containingDeclaration is PackageFragmentDescriptor || getCodeStyleSettings(contextFile).IMPORT_NESTED_CLASSES
-            }
+            is ClassDescriptor -> allowClassImport(importableDescriptor, contextFile)
 
             else -> descriptor.getImportableDescriptor().containingDeclaration is PackageFragmentDescriptor // do not import members (e.g. java static members)
         }
+    }
+
+    private fun allowClassImport(classDescriptor: ClassDescriptor, contextFile: KtFile): Boolean {
+        val nested = classDescriptor.containingDeclaration !is PackageFragmentDescriptor
+        // If nested classes are blanket-prohibited from being imported, don't import any.
+        if (nested && !getCodeStyleSettings(contextFile).IMPORT_NESTED_CLASSES) return false
+        val classInfo = ClassImportFilter.ClassInfo(
+            classDescriptor.fqNameSafe,
+            classDescriptor.kind,
+            classDescriptor.modality,
+            classDescriptor.visibility.delegate,
+            nested)
+        return ClassImportFilter.allowClassImport(classInfo, contextFile)
     }
 
     override fun importDescriptor(
