efcb075901c47e2969e82c2cb85432b0d92c53f9
Andrii Rublov <andrii.rublov@jetbrains.com>
2023-05-11 23:16:44 +0200

    Fix warning from default IJ inspections profile
    GitOrigin-RevId: b86d2c1e17e2d4b6c0e028cb933a28852c40ae3c

--- platform/execution-impl/src/com/intellij/execution/actions/StopAction.java
+++ platform/execution-impl/src/com/intellij/execution/actions/StopAction.java
@@ -22,6 +22,7 @@ import com.intellij.reference.SoftReference;
 import com.intellij.ui.popup.list.GroupedItemsListRenderer;
 import com.intellij.util.IconUtil;
 import com.intellij.util.SmartList;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
@@ -273,7 +274,7 @@ public class StopAction extends DumbAwareAction {
   @ApiStatus.Internal
   public static @NotNull List<RunContentDescriptor> getActiveStoppableDescriptors(@Nullable Project project) {
     List<RunContentDescriptor> runningProcesses = project != null ?
-                                                  getStoppableDescriptors(project).stream().map(kotlin.Pair::getFirst).toList() :
+                                                  ContainerUtil.map(getStoppableDescriptors(project), kotlin.Pair::getFirst) :
                                                   Collections.emptyList();
     if (runningProcesses.isEmpty()) {
       return Collections.emptyList();
