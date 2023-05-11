e737264f4c437f6ea6957a501782112e6bd89377
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-05-11 20:21:30 +0200

    convert LanguageBundleListener to kotlin
    GitOrigin-RevId: f35ca2470ebe2f7a8672d063c3d55d88a96197d4

--- platform/platform-impl/src/com/intellij/ide/LanguageBundleListener.kt
+++ platform/platform-impl/src/com/intellij/ide/LanguageBundleListener.kt
@@ -1,26 +1,20 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.ide;
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide
 
-import com.intellij.DynamicBundle;
-import com.intellij.UtilBundle;
-import com.intellij.core.CoreBundle;
-import com.intellij.openapi.extensions.PluginDescriptor;
-import com.intellij.ui.UtilUiBundle;
+import com.intellij.DynamicBundle
+import com.intellij.DynamicBundle.LanguageBundleEP
+import com.intellij.UtilBundle
+import com.intellij.core.CoreBundle
+import com.intellij.ui.UtilUiBundle
 
-final class LanguageBundleListener implements ApplicationInitializedListener {
-  @Override
-  public void componentsInitialized() {
-    DynamicBundle.LanguageBundleEP langBundle = DynamicBundle.LanguageBundleEP.EP_NAME.findExtension(DynamicBundle.LanguageBundleEP.class);
-    PluginDescriptor pluginDescriptor = langBundle == null ? null : langBundle.pluginDescriptor;
-    if (pluginDescriptor == null) {
-      return;
-    }
-
-    ClassLoader pluginClassLoader = pluginDescriptor.getPluginClassLoader();
-    UtilBundle.loadBundleFromPlugin(pluginClassLoader);
-    UtilUiBundle.loadBundleFromPlugin(pluginClassLoader);
-
-    DynamicBundle.loadLocale(langBundle);
-    CoreBundle.clearCache();
+private class LanguageBundleListener : ApplicationInitializedListener {
+  override fun componentsInitialized() {
+    val langBundle = LanguageBundleEP.EP_NAME.findExtension(LanguageBundleEP::class.java)
+    val pluginDescriptor = langBundle?.pluginDescriptor ?: return
+    val pluginClassLoader = pluginDescriptor.pluginClassLoader
+    UtilBundle.loadBundleFromPlugin(pluginClassLoader)
+    UtilUiBundle.loadBundleFromPlugin(pluginClassLoader)
+    DynamicBundle.loadLocale(langBundle)
+    CoreBundle.clearCache()
   }
 }
