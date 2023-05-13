59319107deaf36e588267dd6bc02fe6b69761693
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-05-13 23:20:04 +0200

    [maven] IDEA-313767 MavenEmbedderWrapperEx runBlocking -> runBlockingCancellable
    GitOrigin-RevId: 1444c4455979145a9b6500508031c7c05cd405ff

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/server/MavenEmbedderWrapperEx.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/server/MavenEmbedderWrapperEx.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.maven.server
 
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.Project
 import kotlinx.coroutines.*
 import org.jetbrains.idea.maven.project.MavenConsole
@@ -8,14 +10,32 @@ import org.jetbrains.idea.maven.server.MavenArtifactEvent.ArtifactEventType
 import org.jetbrains.idea.maven.utils.MavenProcessCanceledException
 import org.jetbrains.idea.maven.utils.MavenProgressIndicator
 import java.util.*
+import java.util.concurrent.atomic.AtomicReference
 
 abstract class MavenEmbedderWrapperEx(project: Project) : MavenEmbedderWrapper(project) {
   @Throws(MavenProcessCanceledException::class)
   override fun <R> runLongRunningTask(task: LongRunningEmbedderTask<R>,
                                       indicator: MavenProgressIndicator?,
                                       console: MavenConsole?): R {
-    return runBlocking {
-      return@runBlocking runLongRunningTaskAsync<R>(indicator, console, task)
+    val progress = indicator?.indicator
+    if (null == progress) {
+      val longRunningTaskId = UUID.randomUUID().toString()
+      val embedder = getOrCreateWrappee()
+      return task.run(embedder, longRunningTaskId)
+    }
+    else {
+      val result = AtomicReference<R>();
+      val process: () -> Unit = { result.set(doRunLongRunningTask(task, indicator, console)) }
+      ProgressManager.getInstance().executeProcessUnderProgress(process, progress)
+      return result.get();
+    }
+  }
+
+  private fun <R> doRunLongRunningTask(task: LongRunningEmbedderTask<R>,
+                                       indicator: MavenProgressIndicator?,
+                                       console: MavenConsole?): R {
+    return runBlockingCancellable {
+      return@runBlockingCancellable runLongRunningTaskAsync(indicator, console, task)
     }
   }
 
