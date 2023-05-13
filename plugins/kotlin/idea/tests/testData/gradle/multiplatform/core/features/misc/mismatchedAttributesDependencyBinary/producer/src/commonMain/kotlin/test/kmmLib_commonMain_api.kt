b79ae9fd60af735dc9feb3dc8718cd28a7c5f547
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2023-05-13 02:36:26 +0200

    [kt, tests] Update diagnostic message for kt-223-* branches
    GitOrigin-RevId: 9f2fd4af531c309b0fb41cfb183dca75eff4e56c

--- plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/features/misc/mismatchedAttributesDependencyBinary/producer/src/commonMain/kotlin/test/kmmLib_commonMain_api.kt
+++ plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/features/misc/mismatchedAttributesDependencyBinary/producer/src/commonMain/kotlin/test/kmmLib_commonMain_api.kt
@@ -1,7 +1,7 @@
 package test
 
-expect class <!LINE_MARKER("descr='Has actuals in [project.producer.jsMain, project.producer.jvmMain] modules'")!>CommonMainExpect<!> {
-    fun <!LINE_MARKER("descr='Has actuals in [project.producer.jsMain, project.producer.jvmMain] modules'")!>commonApi<!>()
+expect class <!LINE_MARKER("descr='Has actuals in [project.producer.jsMain, project.producer.jvmMain] module'")!>CommonMainExpect<!> {
+    fun <!LINE_MARKER("descr='Has actuals in [project.producer.jsMain, project.producer.jvmMain] module'")!>commonApi<!>()
 }
 
 fun produceCommonMainExpect(): CommonMainExpect = null!!
