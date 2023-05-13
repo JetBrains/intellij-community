25816da5d7fbfea95c2ba510fb22452dc27f5bb0
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2023-05-13 04:01:04 +0200

    [kt, tests] Update testdata for KotlinMppExperimentalTierCasesImportingTest#testJvmAndAndroidBinary
    GitOrigin-RevId: 2896d0e59dde64c7bb5d9ae21bd757a1c8a3ba9f

--- plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/experimentalTier/jvmAndAndroidBinary/consumer/src/jvmMain/kotlin/test/consumer_jvmMain_usages.kt
+++ plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/experimentalTier/jvmAndAndroidBinary/consumer/src/jvmMain/kotlin/test/consumer_jvmMain_usages.kt
@@ -12,7 +12,7 @@ fun useJvmMain() {
     // JDK-specific parts are visible
     val label: java.awt.Label? = null
     // Android SDK is not visible
-    val bundle: <!HIGHLIGHTING("severity='ERROR'; descr='[UNRESOLVED_REFERENCE] Unresolved reference: android'")!>android<!>.<!HIGHLIGHTING("severity='ERROR'; descr='[DEBUG] Reference is not resolved to anything, but is not marked unresolved'")!>os<!>.<!HIGHLIGHTING("severity='ERROR'; descr='[DEBUG] Reference is not resolved to anything, but is not marked unresolved'")!>Bundle<!>? = null
+    val bundle: android.<!HIGHLIGHTING("severity='ERROR'; descr='[UNRESOLVED_REFERENCE] Unresolved reference: os'")!>os<!>.<!HIGHLIGHTING("severity='ERROR'; descr='[DEBUG] Reference is not resolved to anything, but is not marked unresolved'")!>Bundle<!>? = null
 
     // commonMain is visible
     commonMainApi()
