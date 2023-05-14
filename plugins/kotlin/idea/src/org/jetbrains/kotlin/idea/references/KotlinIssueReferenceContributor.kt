559d6df9c940fae8f615822fc68244ba0cbfc486
Leonid Shalupov <leonid@shalupov.com>
2023-05-14 17:49:00 +0200

    [kotlin] Reuse common code for finding issue tracker IDs in psi elements
    KTIJ-25549 No highlighting/navigation for issue tracker links from string literals
    IJ-CR-107473
    
    GitOrigin-RevId: 266fe8d1297c5cb8c2fd09b7067890b51bd9b608

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/references/KotlinIssueReferenceContributor.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/references/KotlinIssueReferenceContributor.kt
@@ -1,43 +1,18 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.references
 
-import com.intellij.openapi.paths.GlobalPathReferenceProvider
-import com.intellij.openapi.paths.PathReferenceManager
-import com.intellij.openapi.util.text.StringUtil
-import com.intellij.openapi.vcs.IssueNavigationConfiguration
 import com.intellij.patterns.PlatformPatterns
-import com.intellij.psi.*
-import com.intellij.util.ProcessingContext
-import com.intellij.util.SmartList
+import com.intellij.psi.PsiReferenceContributor
+import com.intellij.psi.PsiReferenceRegistrar
+import com.intellij.psi.impl.source.resolve.reference.CommentsReferenceContributor
 import org.jetbrains.kotlin.psi.KtStringTemplateExpression
 
 class KotlinIssueReferenceContributor : PsiReferenceContributor() {
-    object KotlinIssueReferenceProvider : PsiReferenceProvider() {
-        override fun getReferencesByElement(element: PsiElement, context: ProcessingContext): Array<PsiReference> {
-            if (element !is KtStringTemplateExpression) return PsiReference.EMPTY_ARRAY
-
-            val navigationConfiguration = IssueNavigationConfiguration.getInstance(element.project) ?: return PsiReference.EMPTY_ARRAY
-
-            val refs = SmartList<PsiReference>()
-
-            val issueText = StringUtil.newBombedCharSequence(element.text, 500)
-            for (link in navigationConfiguration.findIssueLinks(issueText)) {
-                val provider = PathReferenceManager.getInstance().globalWebPathReferenceProvider as GlobalPathReferenceProvider
-                provider.createUrlReference(element, link.targetUrl, link.range, refs)
-            }
-
-            return refs.toArray(PsiReference.EMPTY_ARRAY)
-        }
-
-        // web references do not point to any real PsiElement
-        override fun acceptsTarget(target: PsiElement): Boolean = false
-    }
-
     override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
         registrar.registerReferenceProvider(
             PlatformPatterns.psiElement(KtStringTemplateExpression::class.java),
-            KotlinIssueReferenceProvider,
-            PsiReferenceRegistrar.LOWER_PRIORITY
+            CommentsReferenceContributor.COMMENTS_REFERENCE_PROVIDER_TYPE.provider,
+            PsiReferenceRegistrar.LOWER_PRIORITY,
         )
     }
 }
