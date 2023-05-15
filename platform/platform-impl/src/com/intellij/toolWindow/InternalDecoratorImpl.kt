ec3c6d6f8e71e3f11912dbd32e82f14a198c965d
Konstantin Bulenkov <kb@jetbrains.com>
2023-05-15 17:10:25 +0200

    IDEA-320398 Provide a way to stop hiding toolwindow header toolbar in new UI
    GitOrigin-RevId: e77dd105c1d9140567a14fdeb6bdbd92b14936e2

--- platform/platform-impl/src/com/intellij/toolWindow/InternalDecoratorImpl.kt
+++ platform/platform-impl/src/com/intellij/toolWindow/InternalDecoratorImpl.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.toolWindow
 
 import com.intellij.accessibility.AccessibilityUtils
@@ -519,7 +519,8 @@ class InternalDecoratorImpl internal constructor(
   fun updateActiveAndHoverState() {
     val isHoverAlphaAnimationEnabled =
       toolWindow.toolWindowManager.isNewUi &&
-      !AdvancedSettings.getBoolean("ide.always.show.tool.window.header.icons")
+      !AdvancedSettings.getBoolean("ide.always.show.tool.window.header.icons") &&
+      toolWindow.component.getClientProperty(ToolWindowContentUi.DONT_HIDE_TOOLBAR_IN_HEADER) != true
     val narrow = this.toolWindow.decorator?.width?.let { it < JBUI.scale(120) } ?: false
     val isVisible = narrow || !isHoverAlphaAnimationEnabled || isWindowHovered || header.isPopupShowing || toolWindow.isActive
 
