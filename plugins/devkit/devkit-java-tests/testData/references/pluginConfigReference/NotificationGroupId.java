9843d2a01afbd6bd7009f214a1c1e32c08c0ab45
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2023-05-15 16:46:12 +0200

    [platform] API cleanup: remove unused deprecated NotificationBuilder class (IDEA-312594)
    GitOrigin-RevId: 6bdb5bb80ab101f6b6aadc0b858b66ee46454188

--- plugins/devkit/devkit-java-tests/testData/references/pluginConfigReference/NotificationGroupId.java
+++ plugins/devkit/devkit-java-tests/testData/references/pluginConfigReference/NotificationGroupId.java
@@ -1,16 +1,13 @@
 import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationBuilder;
 import com.intellij.notification.NotificationGroupManager;
 import com.intellij.notification.NotificationType;
 
 public class NotificationGroupId {
   public static void main(String[] args) {
     new Notification("my.balloon", "Content", NotificationType.INFORMATION);
-    new NotificationBuilder("my.balloon", "Content", NotificationType.WARNING);
     NotificationGroupManager.getInstance().getNotificationGroup("my.balloon");
 
     new Notification("<error descr="Cannot resolve notification group id 'INVALID_VALUE'">INVALID_VALUE</error>", "Content", NotificationType.INFORMATION);
-    new NotificationBuilder("<error descr="Cannot resolve notification group id 'INVALID_VALUE'">INVALID_VALUE</error>", "Content", NotificationType.WARNING);
     NotificationGroupManager.getInstance().getNotificationGroup("<error descr="Cannot resolve notification group id 'INVALID_VALUE'">INVALID_VALUE</error>");
   }
 }
