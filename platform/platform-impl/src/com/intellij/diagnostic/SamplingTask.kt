942ffd5282c2a9ba624f43d70557060d729fc3dc
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-05-15 13:51:09 +0200

    convert SamplingTask to modern language
    GitOrigin-RevId: 539278b2b41f6115f22f5f3f3020ffda594ba422

--- platform/platform-impl/src/com/intellij/diagnostic/SamplingTask.kt
+++ platform/platform-impl/src/com/intellij/diagnostic/SamplingTask.kt
@@ -1,89 +1,65 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.diagnostic;
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.diagnostic
 
-import com.sun.management.OperatingSystemMXBean;
-import kotlinx.coroutines.Job;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.diagnostic.PerformanceWatcher.Companion.getInstance
+import com.sun.management.OperatingSystemMXBean
+import kotlinx.coroutines.Job
+import java.lang.management.ManagementFactory
+import java.lang.management.ThreadInfo
+import java.util.concurrent.TimeUnit
 
-import java.lang.management.GarbageCollectorMXBean;
-import java.lang.management.ManagementFactory;
-import java.lang.management.ThreadInfo;
-import java.lang.management.ThreadMXBean;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+internal open class SamplingTask(@JvmField internal val dumpInterval: Int, maxDurationMs: Int) {
+  private val maxDumps: Int
+  private val myThreadInfos = ArrayList<Array<ThreadInfo>>()
+  private val job: Job
+  private val startTime: Long
+  private var currentTime: Long
+  private val gcStartTime: Long
+  private var gcCurrentTime: Long
+  val processCpuLoad: Double
 
-class SamplingTask {
-  private final static ThreadMXBean THREAD_MX_BEAN = ManagementFactory.getThreadMXBean();
-  private final static List<GarbageCollectorMXBean> GC_MX_BEANS = ManagementFactory.getGarbageCollectorMXBeans();
+  val threadInfos: List<Array<ThreadInfo>>
+    get() = myThreadInfos
+  val totalTime: Long
+    get() = TimeUnit.NANOSECONDS.toMillis(currentTime - startTime)
+  val gcTime: Long
+    get() = gcCurrentTime - gcStartTime
 
-  private final int myDumpInterval;
-  private final int myMaxDumps;
-
-  private final List<ThreadInfo[]> myThreadInfos = new ArrayList<>();
-
-  private final Job job;
-
-  private final long myStartTime;
-  private long myCurrentTime;
-  private final long myGcStartTime;
-  private long myGcCurrentTime;
-  private final double myProcessCpuLoad;
-
-  SamplingTask(int intervalMs, int maxDurationMs) {
-    myDumpInterval = intervalMs;
-    myMaxDumps = maxDurationMs / intervalMs;
-    myCurrentTime = myStartTime = System.nanoTime();
-    myGcCurrentTime = myGcStartTime = currentGcTime();
-    myProcessCpuLoad = ((OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean()).getProcessCpuLoad();
-    job = PerformanceWatcher.getInstance().scheduleWithFixedDelay(this::dumpThreads, myDumpInterval);
+  init {
+    maxDumps = maxDurationMs / dumpInterval
+    startTime = System.nanoTime()
+    currentTime = startTime
+    gcStartTime = currentGcTime()
+    gcCurrentTime = gcStartTime
+    processCpuLoad = (ManagementFactory.getOperatingSystemMXBean() as OperatingSystemMXBean).processCpuLoad
+    job = getInstance().scheduleWithFixedDelay({ dumpThreads() }, dumpInterval.toLong())
   }
 
-  private void dumpThreads() {
-    myCurrentTime = System.nanoTime();
-    myGcCurrentTime = currentGcTime();
-    ThreadInfo[] infos = ThreadDumper.getThreadInfos(THREAD_MX_BEAN, false);
-    if (!job.isCancelled()) {
-      myThreadInfos.add(infos);
-      if (myThreadInfos.size() >= myMaxDumps) {
-        stop();
+  private fun dumpThreads() {
+    currentTime = System.nanoTime()
+    gcCurrentTime = currentGcTime()
+    val infos = ThreadDumper.getThreadInfos(THREAD_MX_BEAN, false)
+    if (!job.isCancelled) {
+      myThreadInfos.add(infos)
+      if (myThreadInfos.size >= maxDumps) {
+        stop()
       }
-      dumpedThreads(ThreadDumper.getThreadDumpInfo(infos, true));
+      dumpedThreads(ThreadDumper.getThreadDumpInfo(infos, true))
     }
   }
 
-  protected void dumpedThreads(@NotNull ThreadDump threadDump) {
+  protected open fun dumpedThreads(threadDump: ThreadDump) {}
+
+  fun isValid(dumpingDuration: Long): Boolean {
+    return myThreadInfos.size >= 10L.coerceAtLeast(maxDumps.toLong().coerceAtMost(dumpingDuration / dumpInterval / 2))
   }
 
-  private static long currentGcTime() {
-    return GC_MX_BEANS.stream().mapToLong(GarbageCollectorMXBean::getCollectionTime).sum();
-  }
-
-  public final int getDumpInterval() {
-    return myDumpInterval;
-  }
-
-  public final List<ThreadInfo[]> getThreadInfos() {
-    return myThreadInfos;
-  }
-
-  public final long getTotalTime() {
-    return TimeUnit.NANOSECONDS.toMillis(myCurrentTime - myStartTime);
-  }
-
-  public final long getGcTime() {
-    return myGcCurrentTime - myGcStartTime;
-  }
-
-  public final double getProcessCpuLoad() {
-    return myProcessCpuLoad;
-  }
-
-  public final boolean isValid(long dumpingDuration) {
-    return myThreadInfos.size() >= Math.max(10, Math.min(myMaxDumps, dumpingDuration / myDumpInterval / 2));
-  }
-
-  public void stop() {
-    job.cancel(null);
+  open fun stop() {
+    job.cancel(null)
   }
 }
+
+private val THREAD_MX_BEAN = ManagementFactory.getThreadMXBean()
+private val GC_MX_BEANS = ManagementFactory.getGarbageCollectorMXBeans()
+
+private fun currentGcTime(): Long = GC_MX_BEANS.sumOf { it.collectionTime }
