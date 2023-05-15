9692adb19beedc9a0f04f4495b768f5d761f130d
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2023-05-15 09:30:47 +0200

    [kotlin] Use well-known file extensions instead of fileType call
    Acquire FileType is quite expensive operation, for most common file names (like java/kt/class) it's possible to use just extension.
    
    #KTIJ-25470
    
    GitOrigin-RevId: 7ba03fe538af6f3443a48c2015b77020d39ada11

--- plugins/kotlin/base/analysis/src/org/jetbrains/kotlin/idea/base/analysis/RootKindMatcherImpl.kt
+++ plugins/kotlin/base/analysis/src/org/jetbrains/kotlin/idea/base/analysis/RootKindMatcherImpl.kt
@@ -2,6 +2,8 @@
 package org.jetbrains.kotlin.idea.base.analysis
 
 import com.intellij.ide.highlighter.ArchiveFileType
+import com.intellij.ide.highlighter.JavaClassFileType
+import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.ide.scratch.ScratchUtil
 import com.intellij.injected.editor.VirtualFileWindow
 import com.intellij.openapi.fileTypes.FileTypeManager
@@ -9,12 +11,17 @@ import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.vfs.VirtualFile
+import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.base.projectStructure.KotlinBuiltInBinary
 import org.jetbrains.kotlin.idea.base.projectStructure.RootKindFilter
 import org.jetbrains.kotlin.idea.base.projectStructure.RootKindMatcher
 import org.jetbrains.kotlin.idea.base.projectStructure.isKotlinBinary
 import org.jetbrains.kotlin.idea.core.script.ScriptConfigurationManager
 import org.jetbrains.kotlin.idea.util.isKotlinFileType
 import org.jetbrains.kotlin.scripting.definitions.findScriptDefinition
+import org.jetbrains.kotlin.serialization.deserialization.MetadataPackageFragment.Companion.METADATA_FILE_EXTENSION
+import org.jetbrains.kotlin.serialization.deserialization.builtins.BuiltInSerializerProtocol
+import org.jetbrains.kotlin.serialization.deserialization.builtins.BuiltInSerializerProtocol.BUILTINS_FILE_EXTENSION
 import kotlin.script.experimental.api.ScriptAcceptedLocation
 import kotlin.script.experimental.api.ScriptCompilationConfiguration
 import kotlin.script.experimental.api.acceptedLocations
@@ -76,10 +83,32 @@ internal class RootKindMatcherImpl(private val project: Project) : RootKindMatch
             return false
         }
 
-        val fileType = FileTypeManager.getInstance().getFileTypeByFileName(virtualFile.nameSequence)
+        val canContainClassFiles: Boolean
+        val isBinary: Boolean
+
         // NOTE: the following is a workaround for cases when class files are under library source roots and source files are under class roots
-        val canContainClassFiles = fileType == ArchiveFileType.INSTANCE || virtualFile.isDirectory
-        val isBinary = fileType.isKotlinBinary
+        if (virtualFile.isDirectory) {
+            canContainClassFiles = true
+            isBinary = false
+        } else {
+            val extension = virtualFile.extension
+            when (extension) {
+                JavaFileType.INSTANCE.defaultExtension, KotlinFileType.EXTENSION -> {
+                    canContainClassFiles = false
+                    isBinary = false
+                }
+                JavaClassFileType.INSTANCE.defaultExtension, BUILTINS_FILE_EXTENSION, METADATA_FILE_EXTENSION -> {
+                    canContainClassFiles = false
+                    isBinary = true
+                }
+                else -> {
+                    val fileType = FileTypeManager.getInstance().getFileTypeByFileName(virtualFile.nameSequence)
+                    // NOTE: the following is a workaround for cases when class files are under library source roots and source files are under class roots
+                    canContainClassFiles = fileType == ArchiveFileType.INSTANCE || virtualFile.isDirectory
+                    isBinary = fileType.isKotlinBinary
+                }
+            }
+        }
 
         if (correctedFilter.includeLibraryClassFiles && (isBinary || canContainClassFiles)) {
             if (fileIndex.isInLibraryClasses(virtualFile)) {
--- plugins/kotlin/base/util/src/org/jetbrains/kotlin/idea/util/FileUtils.kt
+++ plugins/kotlin/base/util/src/org/jetbrains/kotlin/idea/util/FileUtils.kt
@@ -2,6 +2,7 @@
 @file:JvmName("FileUtils")
 package org.jetbrains.kotlin.idea.util
 
+import com.intellij.ide.highlighter.JavaClassFileType
 import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.vfs.VfsUtilCore
@@ -9,8 +10,14 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileVisitor
 import org.jetbrains.kotlin.idea.KotlinFileType
 
-fun VirtualFile.isKotlinFileType(): Boolean =
-    extension == KotlinFileType.EXTENSION || FileTypeRegistry.getInstance().isFileOfType(this, KotlinFileType.INSTANCE)
+fun VirtualFile.isKotlinFileType(): Boolean {
+    val extension = extension
+    if (extension == KotlinFileType.EXTENSION) return true
+    if (extension == JavaFileType.INSTANCE.defaultExtension ||
+            extension == JavaClassFileType.INSTANCE.defaultExtension) return false
+
+    return FileTypeRegistry.getInstance().isFileOfType(this, KotlinFileType.INSTANCE)
+}
 
 fun VirtualFile.isJavaFileType(): Boolean =
     extension == JavaFileType.DEFAULT_EXTENSION || FileTypeRegistry.getInstance().isFileOfType(this, JavaFileType.INSTANCE)
