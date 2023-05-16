0149d22d47ddcce30de8ec5c1c8def506d52603d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-05-16 20:11:33 +0200

    cleanup
    GitOrigin-RevId: 39e782882006e1b4893a780de8b36e09dc4f3b3a

--- java/java-impl/src/com/intellij/codeInsight/daemon/problems/pass/ProjectProblemFileSelectionListener.kt
+++ java/java-impl/src/com/intellij/codeInsight/daemon/problems/pass/ProjectProblemFileSelectionListener.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.daemon.problems.pass
 
-import com.intellij.codeInsight.daemon.problems.FileStateCache.Companion.getInstance
+import com.intellij.codeInsight.daemon.problems.FileStateCache
 import com.intellij.codeInsight.daemon.problems.FileStateUpdater.Companion.removeState
 import com.intellij.codeInsight.daemon.problems.FileStateUpdater.Companion.setPreviousState
 import com.intellij.codeInsight.daemon.problems.pass.ProjectProblemCodeVisionProvider.Companion.hintsEnabled
@@ -161,10 +161,9 @@ private class ProjectProblemFileSelectionListenerStartupActivity : ProjectActivi
       override fun beforePropertyChange(event: PsiTreeChangeEvent) {
         updateFileState(event.file, project)
       }
-    }, getInstance(project))
+    }, FileStateCache.getInstance(project))
   }
 }
-
 
 private fun onHintsDisabled(project: Project) {
   val editorManager = FileEditorManager.getInstance(project)
