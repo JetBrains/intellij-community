7d06364ff73df0b5125d7a95fc62fdaaefbf7c1c
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2023-05-17 12:25:38 +0300

    IDEA-320506 Fix details in Project View for Packages View
    This turned out to be more complicated than it seemed.
    The problem is that it uses PackageElementNode which
    is a part of Java PSI implementation that isn't even available
    in lang-impl. And it has no useful inherited methods either.
    
    So we employ a possibly ugly trick here: instead of checking
    that the parent node is a directory, we check that it can't be
    a file instead. After all, all this code is about avoiding inplace
    comments for members, and members are usually a part of
    something backed by a virtual file which is a real file and not
    a directory.
    
    GitOrigin-RevId: 1e5fa26d2cebe343d7b279713285e45467b2b69f

--- platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewInplaceCommentProducerImpl.kt
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewInplaceCommentProducerImpl.kt
@@ -3,25 +3,53 @@ package com.intellij.ide.projectView.impl
 
 import com.intellij.ide.projectView.ProjectViewNode
 import com.intellij.ide.projectView.impl.nodes.getVirtualFileForNodeOrItsPSI
-import com.intellij.ide.projectView.impl.nodes.isDirectory
+import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.ide.util.treeView.InplaceCommentAppender
 import com.intellij.openapi.fileEditor.impl.IdeDocumentHistoryImpl
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.isFile
+import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFileSystemItem
 import com.intellij.ui.SimpleTextAttributes
+import com.intellij.ui.tree.project.ProjectFileNode
 import com.intellij.util.text.JBDateFormat
 
 internal fun appendInplaceComments(node: ProjectViewNode<*>, appender: InplaceCommentAppender) {
-  val parentNode = node.parent
-  val content = node.value
-  if (content is PsiFileSystemItem || content !is PsiElement || parentNode != null && parentNode.isDirectory()) {
+  if (node.shouldTryToShowInplaceComments()) {
     appendInplaceComments(appender, node.project, node.getVirtualFileForNodeOrItsPSI())
   }
 }
+
+private fun ProjectViewNode<*>.shouldTryToShowInplaceComments(): Boolean {
+  // Most of this stuff is about avoiding inplace comments for members (UX-159).
+  val content = value
+  if (content.cantBePsiMember()) {
+    return true
+  }
+  val parentNode = parent ?: return false // It's the root? We don't even show it, so don't bother with further checks.
+  return parentNode.cantBeFile() // All members belong to files.
+}
+
+private fun Any?.cantBePsiMember(): Boolean =
+  this == null ||
+  this is PsiFileSystemItem || // The node itself is either a file or directory, not a member.
+  this !is PsiElement // All members inherit from PsiElement, so it's something else (e.g. a plain text file).
+
+private fun <T> AbstractTreeNode<T>.cantBeFile(): Boolean =
+  isDirectory() || // Either we know for sure it's a directory,
+  !isFile() // or we know for sure it's not a file (could be neither, e.g. a package in Packages View).
+
+private fun <T> AbstractTreeNode<T>.isDirectory(): Boolean {
+  val value = this.value
+  // The ProjectFileNode check is for Scope-based views that don't use PsiDirectory.
+  return value is PsiDirectory || (value is ProjectFileNode && value.virtualFile.isDirectory)
+}
+
+private fun <T> AbstractTreeNode<T>.isFile(): Boolean = getVirtualFileForNodeOrItsPSI()?.isFile == true
 
 // To be used in Rider once it migrates from legacy logic, don't change the signature and/or visibility.
 fun appendInplaceComments(appender: InplaceCommentAppender, project: Project?, file: VirtualFile?) {
