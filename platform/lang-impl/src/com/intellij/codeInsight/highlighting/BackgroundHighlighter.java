e86e6faa0f76ee83b6382328dc805202133332bf
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-05-17 18:26:32 +0200

    fix compilation
    GitOrigin-RevId: 5401a388a99a6148eaca19028964f4038da7e1d9

--- platform/lang-impl/src/com/intellij/codeInsight/highlighting/BackgroundHighlighter.java
+++ platform/lang-impl/src/com/intellij/codeInsight/highlighting/BackgroundHighlighter.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package com.intellij.codeInsight.highlighting;
 
@@ -31,6 +31,7 @@ import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.ProperTextRange;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiUtilBase;
 import com.intellij.util.Alarm;
