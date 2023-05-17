9a63850218f12e0021698d7e876ba31fc817a113
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-05-17 10:06:19 +0200

    [java-inspections] OptionalOfNullableMisuse: change message
    GitOrigin-RevId: 168f791e21376d5a134683b7f3fc0661fdcaf091

--- java/java-tests/testData/inspection/dataFlow/optionalOfNullable/OptionalOfNullableMisuse.java
+++ java/java-tests/testData/inspection/dataFlow/optionalOfNullable/OptionalOfNullableMisuse.java
@@ -16,25 +16,25 @@ class Test {
   }
 
   Optional<String> getName3() {
-    return Optional.ofNullable(<warning descr="Passing 'null' argument to 'Optional'">null</warning>);
+    return Optional.ofNullable(<warning descr="'Optional.ofNullable()' with null argument should be replaced with 'Optional.empty()'">null</warning>);
   }
 
   long field;
 
   Optional<Long> getName4() {
-    return Optional.ofNullable(<warning descr="Passing a non-null argument to 'Optional'">field</warning>);
+    return Optional.ofNullable(<warning descr="'Optional.ofNullable()' with non-null argument should be replaced with 'Optional.of()'">field</warning>);
   }
 
   Optional<String> getName5() {
     String s = "";
-    return Optional.ofNullable(<warning descr="Passing a non-null argument to 'Optional'">s</warning>);
+    return Optional.ofNullable(<warning descr="'Optional.ofNullable()' with non-null argument should be replaced with 'Optional.of()'">s</warning>);
   }
 
   Optional<String> testMethodRef(List<String> list) {
-    return list.stream().filter(Objects::isNull).map(Optional::<warning descr="Passing 'null' argument to 'Optional'">ofNullable</warning>).findFirst().orElse(Optional.empty());
+    return list.stream().filter(Objects::isNull).map(Optional::<warning descr="'Optional.ofNullable()' with null argument should be replaced with 'Optional.empty()'">ofNullable</warning>).findFirst().orElse(Optional.empty());
   }
 
   Optional<String> testMethodRef2(List<String> list) {
-    return list.stream().filter(Objects::nonNull).map(Optional::<warning descr="Passing a non-null argument to 'Optional'">ofNullable</warning>).findFirst().orElse(Optional.empty());
+    return list.stream().filter(Objects::nonNull).map(Optional::<warning descr="'Optional.ofNullable()' with non-null argument should be replaced with 'Optional.of()'">ofNullable</warning>).findFirst().orElse(Optional.empty());
   }
 }
