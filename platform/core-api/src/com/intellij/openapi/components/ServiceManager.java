337bcc327a5d34720e134e312768de414c5572b0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-05-17 09:00:30 +0200

    don't use deprecated ServiceManager
    GitOrigin-RevId: 3b9982a03a8c10febba42f3bf135218920c2e608

--- platform/core-api/src/com/intellij/openapi/components/ServiceManager.java
+++ platform/core-api/src/com/intellij/openapi/components/ServiceManager.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.components;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -8,6 +8,10 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * @deprecated Don't use.
+ */
+@Deprecated
 public final class ServiceManager {
   private ServiceManager() { }
 
