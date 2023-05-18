4c4e02636c8d2b732fccf6c8ca8ca0407f0c83fb
Ivan Semenov <ivan.semenov@jetbrains.com>
2023-05-18 14:03:39 +0200

    [gitlab] override global diff actions to show MR diff
    IDEA-319416 Fixed
    
    GitOrigin-RevId: 9717f6bb1bbefa16681b5146946d06b768b20be0

--- plugins/gitlab/src/org/jetbrains/plugins/gitlab/mergerequest/ui/details/GitLabMergeRequestDetailsComponentFactory.kt
+++ plugins/gitlab/src/org/jetbrains/plugins/gitlab/mergerequest/ui/details/GitLabMergeRequestDetailsComponentFactory.kt
@@ -25,6 +25,7 @@ import net.miginfocom.swing.MigLayout
 import org.jetbrains.plugins.gitlab.api.dto.GitLabCommitDTO
 import org.jetbrains.plugins.gitlab.api.dto.GitLabUserDTO
 import org.jetbrains.plugins.gitlab.mergerequest.action.GitLabMergeRequestsActionKeys
+import org.jetbrains.plugins.gitlab.mergerequest.ui.details.model.GitLabMergeRequestChangesViewModel
 import org.jetbrains.plugins.gitlab.mergerequest.ui.details.model.GitLabMergeRequestDetailsLoadingViewModel
 import org.jetbrains.plugins.gitlab.mergerequest.ui.details.model.GitLabMergeRequestDetailsViewModel
 import org.jetbrains.plugins.gitlab.util.GitLabBundle
@@ -54,6 +55,7 @@ internal object GitLabMergeRequestDetailsComponentFactory {
               DataManager.registerDataProvider(this) { dataId ->
                 when {
                   GitLabMergeRequestsActionKeys.MERGE_REQUEST.`is`(dataId) -> detailsVm.detailsInfoVm.mergeRequest
+                  GitLabMergeRequestChangesViewModel.DATA_KEY.`is`(dataId) -> detailsVm.changesVm
                   else -> null
                 }
               }
