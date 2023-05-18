dac7c7e5d48239c21c97c1744cc6378dabc24ea9
Toshiaki Kameyama <kmymtsak@gmail.com>
2023-05-18 15:43:48 +0900

    [kotlin] "Convert concatenation to template" (K2): remove redundant `toString` calls
     ^KTIJ-25478 Fixed
    
    closes https://github.com/JetBrains/intellij-community/pull/2440
    
    GitOrigin-RevId: fe90796f92087065d9e860d038ee2ab1740d0085

--- plugins/kotlin/code-insight/intentions-k2/tests/test/org/jetbrains/kotlin/idea/k2/intentions/tests/K2IntentionTestGenerated.java
+++ plugins/kotlin/code-insight/intentions-k2/tests/test/org/jetbrains/kotlin/idea/k2/intentions/tests/K2IntentionTestGenerated.java
@@ -1902,6 +1902,11 @@ public abstract class K2IntentionTestGenerated extends AbstractK2IntentionTest {
             runTest("../../../idea/tests/testData/intentions/convertToStringTemplate/insertBracesForSimpleNamedExpression.kt");
         }
 
+        @TestMetadata("intToString.kt")
+        public void testIntToString() throws Exception {
+            runTest("../../../idea/tests/testData/intentions/convertToStringTemplate/intToString.kt");
+        }
+
         @TestMetadata("interpolate2StringConstants.kt")
         public void testInterpolate2StringConstants() throws Exception {
             runTest("../../../idea/tests/testData/intentions/convertToStringTemplate/interpolate2StringConstants.kt");
