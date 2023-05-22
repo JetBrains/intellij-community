consulo/consulo-kotlin

KTIJ-16167: Group kotlin diagnostics by the same psi element
GitOrigin-RevId: cfda9095861e04bdf4d528cc97c169dde4c90acf

base/fe10/highlighting/src/org/jetbrains/kotlin/idea/highlighter/AbstractKotlinHighlightVisitor.kt
@@ -9,7 +9,6 @@ import com.intellij.codeInsight.daemon.impl.analysis.HighlightingLevelManager
 import com.intellij.openapi.diagnostic.ControlFlowException
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.util.Key
-import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.util.CommonProcessors
@@ -120,22 +119,18 @@ abstract class AbstractKotlinHighlightVisitor : HighlightVisitor {
         //cleanUpCalculatingAnnotations(highlightInfoByTextRange)
         if (!shouldHighlightErrors) return
 
-        for (diagnostic in bindingContext.diagnostics) {
-            val psiElement = diagnostic.psiElement
-            if (psiElement !in elements) continue
-            // has been processed earlier e.g. on-fly or for some reasons it could be duplicated diagnostics for the same factory
-            //  see [PsiCheckerTestGenerated$Checker.testRedeclaration]
-            if (diagnostic in highlightInfoByDiagnostic) continue
-
-            // annotate diagnostics those were not possible to report (and therefore render) on-the-fly
-            annotateDiagnostic(
-                psiElement,
-                holder,
-                listOf(diagnostic),
-                highlightInfoByDiagnostic,
-                calculatingInProgress = false
-            )
-        }
+        bindingContext.diagnostics
+            .filter { diagnostic ->
+                diagnostic.psiElement in elements
+                        // has been processed earlier e.g. on-fly or for some reasons it could be duplicated diagnostics for the same factory
+                        //  see [PsiCheckerTestGenerated$Checker.testRedeclaration]
+                        && diagnostic !in highlightInfoByDiagnostic
+            }
+            .groupBy { it.psiElement }
+            .forEach { (psiElement, diagnostics) ->
+                // annotate diagnostics those were not possible to report (and therefore render) on-the-fly
+                annotateDiagnostic(psiElement, holder, diagnostics, highlightInfoByDiagnostic, calculatingInProgress = false)
+            }
 
         // apply quick fixes for all diagnostics grouping by element
         highlightInfoByDiagnostic.keys

JetBrains/intellij-community

(cherry picked from commit cfda9095861e04bdf4d528cc97c169dde4c90acf)
GitOrigin-RevId: 007f2bf20c13a674032b91015a409da17455f21e

plugins/kotlin/base/fe10/highlighting/src/org/jetbrains/kotlin/idea/highlighter/AbstractKotlinHighlightVisitor.kt
@@ -92,7 +92,7 @@ abstract class AbstractKotlinHighlightVisitor : HighlightVisitor {
                             it !in diagnosticHighlighted &&
                             !RenderingContext.parameters(it).any(::checkIfDescriptor)
                         ) {
-                            annotateDiagnostic(element, holder, it, diagnosticHighlighted)
+                            annotateDiagnostic(element, holder, listOf(it), diagnosticHighlighted)
                         }
                     }
                 )
@@ -117,26 +117,27 @@ abstract class AbstractKotlinHighlightVisitor : HighlightVisitor {
         //cleanUpCalculatingAnnotations(highlightInfoByTextRange)
         if (!shouldHighlightErrors) return
 
-        for (diagnostic in bindingContext.diagnostics) {
-            val psiElement = diagnostic.psiElement
-            if (psiElement !in elements) continue
-            // has been processed earlier e.g. on-fly or for some reasons it could be duplicated diagnostics for the same factory
-            //  see [PsiCheckerTestGenerated$Checker.testRedeclaration]
-            if (diagnostic in diagnosticHighlighted) continue
-
-            // annotate diagnostics those were not possible to report (and therefore render) on-the-fly
-            annotateDiagnostic(psiElement, holder, diagnostic, diagnosticHighlighted)
-        }
+        bindingContext.diagnostics
+            .filter { diagnostic ->
+                diagnostic.psiElement in elements
+                        // has been processed earlier e.g. on-fly or for some reasons it could be duplicated diagnostics for the same factory
+                        //  see [PsiCheckerTestGenerated$Checker.testRedeclaration]
+                        && diagnostic !in diagnosticHighlighted
+            }
+            .groupBy { it.psiElement }
+            .forEach { (psiElement, diagnostics) ->
+                // annotate diagnostics those were not possible to report (and therefore render) on-the-fly
+                annotateDiagnostic(psiElement, holder, diagnostics, diagnosticHighlighted)
+            }
     }
 
     private fun annotateDiagnostic(
         element: PsiElement,
         holder: HighlightInfoHolder,
-        diagnostic: Diagnostic,
+        diagnostics: List<Diagnostic>,
         diagnosticHighlighted: MutableSet<Diagnostic>
     ) {
         if (element.getUserData(DO_NOT_HIGHLIGHT_KEY) != null) return
-        val diagnostics = listOf(diagnostic)
         assertBelongsToTheSameElement(element, diagnostics)
         if (element is KtNameReferenceExpression) {
             val unresolved = diagnostics.any { it.factory == Errors.UNRESOLVED_REFERENCE }