7454e4fa941e9231b41b89e9f99524ec768aafa7
Alexandr Suhinin <alexandr.suhinin@jetbrains.com>
2023-05-24 23:43:34 +0300

    [floating toolbar] IDEA-314026: add extension point to enable toolbar for specific languages
    GitOrigin-RevId: 42bda06db09378423a2c161f28d8b7fd77c6569a

--- platform/lang-impl/src/com/intellij/ui/codeFloatingToolbar/FloatingCodeToolbarEditorCustomizer.kt
+++ platform/lang-impl/src/com/intellij/ui/codeFloatingToolbar/FloatingCodeToolbarEditorCustomizer.kt
@@ -4,10 +4,15 @@ package com.intellij.ui.codeFloatingToolbar
 import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.fileEditor.impl.text.TextEditorCustomizer
 import com.intellij.openapi.util.Disposer
+import com.intellij.psi.PsiDocumentManager
 
 internal class FloatingCodeToolbarEditorCustomizer : TextEditorCustomizer {
   override fun customize(textEditor: TextEditor) {
-    val toolbar = CodeFloatingToolbar(textEditor.editor)
+    val editor = textEditor.editor
+    val project = editor.project ?: return
+    val file = PsiDocumentManager.getInstance(project).getPsiFile(editor.document) ?: return
+    if (!FloatingToolbarFilter.isEnabledForLanguage(file.language)) return
+    val toolbar = CodeFloatingToolbar(editor)
     Disposer.register(textEditor, toolbar)
   }
 
