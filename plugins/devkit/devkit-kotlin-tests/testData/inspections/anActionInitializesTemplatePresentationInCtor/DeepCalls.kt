26a8c89ff585029c104972813a38bbfd248d6e0c
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2023-05-25 12:59:01 +0300

    [devkit] AnActionInitializesTemplatePresentationInCtorInspection: fix the error message
    IJ-CR-107717
    
    GitOrigin-RevId: c3654356638641125cd0c4a7770b674b51da94fd

--- plugins/devkit/devkit-kotlin-tests/testData/inspections/anActionInitializesTemplatePresentationInCtor/DeepCalls.kt
+++ plugins/devkit/devkit-kotlin-tests/testData/inspections/anActionInitializesTemplatePresentationInCtor/DeepCalls.kt
@@ -1,12 +1,8 @@
 import com.intellij.openapi.actionSystem.AnAction
 
 class A : B {
-  <warning descr="Action registered in plugin.xml should not initialize the presentation (text, description, and/or icon) inits ctor. Instead, use no-argument constructors of 'AnAction' and other base classes and followthe convention for setting the text, description, and icon:
-Set the 'id' attribute for the action in plugin.xml.If an icon is needed, optionally set the icon attribute for the action in plugin.xml.Follow this structure when setting the keys for text and description in the property file:
-In the property file, specify the text key as 'action.<action-id>.text=Translated Action Text.'In the property file, specify the description key as 'action.<action-id>.description=Translated Action Description.'">constructor</warning>() :this(42)
-  <warning descr="Action registered in plugin.xml should not initialize the presentation (text, description, and/or icon) inits ctor. Instead, use no-argument constructors of 'AnAction' and other base classes and followthe convention for setting the text, description, and icon:
-Set the 'id' attribute for the action in plugin.xml.If an icon is needed, optionally set the icon attribute for the action in plugin.xml.Follow this structure when setting the keys for text and description in the property file:
-In the property file, specify the text key as 'action.<action-id>.text=Translated Action Text.'In the property file, specify the description key as 'action.<action-id>.description=Translated Action Description.'">constructor</warning>(<warning descr="[UNUSED_PARAMETER] Parameter 'i' is never used">i</warning>: Int) : super()
+  <warning descr="Action presentation initialized in the constructor">constructor</warning>() :this(42)
+  <warning descr="Action presentation initialized in the constructor">constructor</warning>(<warning descr="[UNUSED_PARAMETER] Parameter 'i' is never used">i</warning>: Int) : super()
 }
 
 open class B : AnAction {
--- plugins/devkit/devkit-kotlin-tests/testData/inspections/anActionInitializesTemplatePresentationInCtor/DefaultCtor.kt
+++ plugins/devkit/devkit-kotlin-tests/testData/inspections/anActionInitializesTemplatePresentationInCtor/DefaultCtor.kt
@@ -1,8 +1,6 @@
 import com.intellij.openapi.actionSystem.AnAction;
 
-class <warning descr="Action registered in plugin.xml should not initialize the presentation (text, description, and/or icon) inits ctor. Instead, use no-argument constructors of 'AnAction' and other base classes and followthe convention for setting the text, description, and icon:
-Set the 'id' attribute for the action in plugin.xml.If an icon is needed, optionally set the icon attribute for the action in plugin.xml.Follow this structure when setting the keys for text and description in the property file:
-In the property file, specify the text key as 'action.<action-id>.text=Translated Action Text.'In the property file, specify the description key as 'action.<action-id>.description=Translated Action Description.'">A</warning> : B()
+class <warning descr="Action presentation initialized in the constructor">A</warning> : B()
 
 abstract class B : AnAction {
   constructor() : super("blah blah blah")
