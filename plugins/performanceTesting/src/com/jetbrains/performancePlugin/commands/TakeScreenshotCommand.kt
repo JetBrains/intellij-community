48f3a76fe6bf2558db0d6afe1e39f74985486982
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-05-30 08:52:44 +0200

    Refactor TakeScreenshotCommand from Java to Kotlin
    Removed the Java implementation of TakeScreenshotCommand and replaced it with a Kotlin implementation. This change was made to modernize the codebase and take advantage of Kotlin's concise and expressive syntax.
    
    In addition to the language conversion, minor improvements were made to the code, such as using the logger function, simplifying conditionals, and using better naming conventions. The new Kotlin implementation is functionally equivalent to the old Java version and has been tested to ensure proper behavior.
    
    GitOrigin-RevId: f95be41006495d84c139d0c8785861b1527c3f0e

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/TakeScreenshotCommand.kt
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/TakeScreenshotCommand.kt
@@ -1,123 +1,123 @@
-package com.jetbrains.performancePlugin.commands;
+package com.jetbrains.performancePlugin.commands
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.PathManager;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.ui.playback.PlaybackContext;
-import com.intellij.openapi.ui.playback.commands.AbstractCommand;
-import com.intellij.openapi.wm.IdeFrame;
-import com.intellij.openapi.wm.WindowManager;
-import com.intellij.util.ui.ImageUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.concurrency.AsyncPromise;
-import org.jetbrains.concurrency.Promise;
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.PathManager
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.ui.playback.PlaybackContext
+import com.intellij.openapi.ui.playback.commands.AbstractCommand
+import com.intellij.openapi.wm.WindowManager
+import com.intellij.util.ui.ImageUtil
+import org.jetbrains.concurrency.AsyncPromise
+import org.jetbrains.concurrency.Promise
+import java.awt.AWTException
+import java.awt.Robot
+import java.awt.Toolkit
+import java.awt.image.BufferedImage
+import java.io.File
+import java.io.IOException
+import java.nio.file.Paths
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+import javax.imageio.ImageIO
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+private val LOG: Logger
+  get() = logger<TakeScreenshotCommand>()
 
 /**
  * Command takes screenshot.
  * Takes JPG screenshot of current screen to specified file
  * (or if empty parameter is specified, the file will be stored under the LOG dir).
- * <p>
+ *
+ *
  * Syntax: %takeScreenshot <fullPathToFile>
  * Example: %takeScreenshot ./myScreenshot.jpg
- */
-public class TakeScreenshotCommand extends AbstractCommand {
-  public static final String PREFIX = CMD_PREFIX + "takeScreenshot";
-  private static final Logger LOG = Logger.getInstance(TakeScreenshotCommand.class);
-
-  public TakeScreenshotCommand(@NotNull String text, int line) {
-    super(text, line);
+</fullPathToFile> */
+class TakeScreenshotCommand(text: String, line: Int) : AbstractCommand(text, line) {
+  companion object {
+    const val PREFIX = CMD_PREFIX + "takeScreenshot"
   }
 
-  @Override
-  public @NotNull Promise<Object> _execute(final @NotNull PlaybackContext context) {
-    final AsyncPromise<Object> result = new AsyncPromise<>();
-    String arguments = extractCommandArgument(PREFIX);
-    final String fullPathToFile = (!arguments.isEmpty()) ? arguments : Paths.get(PathManager.getLogPath()).resolve("screenshot_before_exit.png").toString();
-    ApplicationManager.getApplication().executeOnPooledThread(() -> {
-      takeScreenshotOfFrame(fullPathToFile);
-    });
-
-    result.setResult(null);
-
-    return result;
+  public override fun _execute(context: PlaybackContext): Promise<Any> {
+    val result = AsyncPromise<Any>()
+    val arguments = extractCommandArgument(PREFIX)
+    val fullPathToFile = if (!arguments.isEmpty()) arguments
+    else Paths.get(PathManager.getLogPath()).resolve("screenshot_before_exit.png").toString()
+    ApplicationManager.getApplication().executeOnPooledThread { takeScreenshotOfFrame(fullPathToFile) }
+    result.setResult(null)
+    return result
   }
+}
 
-  public static void takeScreenshotWithAwtRobot(String fullPathToFile) {
-    Rectangle rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-    Robot robot = null;
+private fun takeScreenshotWithAwtRobot(fullPathToFile: String) {
+  val rectangle = Rectangle(Toolkit.getDefaultToolkit().screenSize)
+  var robot: Robot? = null
+  try {
+    robot = Robot()
+  }
+  catch (e: AWTException) {
+    LOG.info("Exceptions occurs at attempt to create Robot for taking screenshot")
+    LOG.info(e)
+  }
+  assert(robot != null)
+  val img = robot!!.createScreenCapture(rectangle)
+  try {
+    val screenshotFile = File(fullPathToFile)
+    ImageIO.write(img, "jpg", screenshotFile)
+    if (screenshotFile.exists()) {
+      LOG.info("Screenshot saved:$fullPathToFile")
+    }
+  }
+  catch (e: IOException) {
+    LOG.info("Exceptions occurs at attempt to write screenshot to file")
+    LOG.info(e)
+  }
+}
+
+private fun takeScreenshotOfFrame(fileName: String) {
+  val projects = ProjectManager.getInstance().openProjects
+  for (project in projects) {
+    val result = CompletableFuture<Boolean>()
+    ApplicationManager.getApplication().invokeLater {
+      val frame = WindowManager.getInstance().getIdeFrame(project)
+      if (frame != null) {
+        val component = frame.component
+        val img = ImageUtil.createImage(component.width, component.height, BufferedImage.TYPE_INT_ARGB)
+        component.printAll(img.createGraphics())
+        val prefix = if (projects.size == 1) "" else project.name + "_"
+        ApplicationManager.getApplication().executeOnPooledThread {
+          try {
+            result.complete(ImageIO.write(img, "png", File(prefix + fileName)))
+          }
+          catch (e: IOException) {
+            LOG.info(e)
+          }
+        }
+      }
+      else {
+        LOG.info("Frame was empty when takeScreenshot was called")
+      }
+    }
     try {
-      robot = new Robot();
-    }
-    catch (AWTException e) {
-      LOG.info("Exceptions occurs at attempt to create Robot for taking screenshot");
-      LOG.info(e);
-    }
-
-    assert robot != null;
-    BufferedImage img = robot.createScreenCapture(rectangle);
-    try {
-      File screenshotFile = new File(fullPathToFile);
-      ImageIO.write(img, "jpg", screenshotFile);
-      if (screenshotFile.exists()) {
-        LOG.info("Screenshot saved:" + fullPathToFile);
+      val fileCreated = result[30, TimeUnit.SECONDS]
+      if (fileCreated) {
+        LOG.info("Screenshot is saved at: $fileName")
+      }
+      else {
+        LOG.info("No writers are found for screenshot")
       }
     }
-    catch (IOException e) {
-      LOG.info("Exceptions occurs at attempt to write screenshot to file");
-      LOG.info(e);
+    catch (e: InterruptedException) {
+      LOG.info(e)
     }
-  }
-
-  public static void takeScreenshotOfFrame(String fileName) {
-    Project[] projects = ProjectManager.getInstance().getOpenProjects();
-    for (Project project : projects) {
-      CompletableFuture<Boolean> result = new CompletableFuture<>();
-      ApplicationManager.getApplication().invokeLater(() -> {
-        IdeFrame frame = WindowManager.getInstance().getIdeFrame(project);
-        if (frame != null) {
-          JComponent component = frame.getComponent();
-          BufferedImage img = ImageUtil.createImage(component.getWidth(), component.getHeight(), BufferedImage.TYPE_INT_ARGB);
-          component.printAll(img.createGraphics());
-          String prefix = projects.length == 1 ? "" : project.getName() + "_";
-          ApplicationManager.getApplication().executeOnPooledThread(() -> {
-            try {
-              result.complete(ImageIO.write(img, "png", new File(prefix + fileName)));
-            }
-            catch (IOException e) {
-              LOG.info(e);
-            }
-          });
-        }
-        else {
-          LOG.info("Frame was empty when takeScreenshot was called");
-        }
-      });
-      try {
-        Boolean fileCreated = result.get(30, TimeUnit.SECONDS);
-        if (fileCreated) {
-          LOG.info("Screenshot is saved at: " + fileName);
-        }
-        else {
-          LOG.info("No writers are found for screenshot");
-        }
-      }
-      catch (InterruptedException | ExecutionException | TimeoutException e) {
-        LOG.info(e);
-      }
+    catch (e: ExecutionException) {
+      LOG.info(e)
+    }
+    catch (e: TimeoutException) {
+      LOG.info(e)
     }
   }
 }
