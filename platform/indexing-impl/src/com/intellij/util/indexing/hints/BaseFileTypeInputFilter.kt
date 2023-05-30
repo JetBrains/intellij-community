94e1e6d6ea8008e56cbd2540c5fdda59d13b083d
Andrei.Kuznetsov <andrei.kuznetsov@jetbrains.com>
2023-05-30 16:21:46 +0200

    IDEA-320457: simplify API: configure BaseFileTypeInputFilter with FileTypeStrategy (instead of method overriding)
    GitOrigin-RevId: 2a03faef2dd23bda448fbf93e989aa6501e89dfb

--- platform/indexing-impl/src/com/intellij/util/indexing/hints/BaseFileTypeInputFilter.kt
+++ platform/indexing-impl/src/com/intellij/util/indexing/hints/BaseFileTypeInputFilter.kt
@@ -6,6 +6,7 @@ import com.intellij.util.ThreeState
 import com.intellij.util.indexing.FileBasedIndex
 import com.intellij.util.indexing.IndexedFile
 import com.intellij.util.indexing.SubstitutedFileType
+import com.intellij.util.indexing.hints.BaseFileTypeInputFilter.FileTypeStrategy
 import org.jetbrains.annotations.ApiStatus
 
 /**
@@ -13,12 +14,29 @@ import org.jetbrains.annotations.ApiStatus
  *
  * Contains default implementation of `acceptInput(file: VirtualFile)` which delegate to hints.
  *
- * If filetype is a [SubstitutedFileType] invokes [acceptFileType] with substituted filetype ([SubstitutedFileType.getFileType]).
+ * If filetype is a [SubstitutedFileType] there are two options how to invoke [acceptFileType]: with filetype before substitution as
+ * an argument ([SubstitutedFileType.getOriginalFileType]), or filetype after substitution ([SubstitutedFileType.getFileType]).
+ * Default behavior is to use filetype after substitution. This can be changed via [FileTypeStrategy]
+ *
+ * @see com.intellij.psi.LanguageSubstitutor
  */
 @ApiStatus.Experimental
-abstract class BaseFileTypeInputFilter : FileBasedIndex.ProjectSpecificInputFilter, FileTypeIndexingHint {
-  override fun hintAcceptFileType(fileType: FileType): ThreeState {
-    return acceptFileType((fileType as? SubstitutedFileType)?.fileType ?: fileType)
+abstract class BaseFileTypeInputFilter(private val fileTypeStrategy: FileTypeStrategy) : FileBasedIndex.ProjectSpecificInputFilter,
+                                                                                         FileTypeIndexingHint {
+  enum class FileTypeStrategy { BEFORE_SUBSTITUTION, AFTER_SUBSTITUTION }
+
+  constructor() : this(FileTypeStrategy.AFTER_SUBSTITUTION)
+
+  final override fun hintAcceptFileType(fileType: FileType): ThreeState {
+    val fileTypeToUse: FileType =
+      if (fileType is SubstitutedFileType) {
+        if (fileTypeStrategy == FileTypeStrategy.BEFORE_SUBSTITUTION) fileType.originalFileType else fileType.fileType
+      }
+      else {
+        fileType
+      }
+
+    return acceptFileType(fileTypeToUse)
   }
 
   final override fun acceptInput(file: IndexedFile): Boolean {
