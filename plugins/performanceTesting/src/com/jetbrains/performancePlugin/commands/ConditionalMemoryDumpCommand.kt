fb413d40d64d297af01da46fd916322a87f0a1e2
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-05-30 10:56:27 +0200

    Use Application as disposable
    we're registering listener that might be triggered during the whole test so proper Disposable registration is not feasible
    
    GitOrigin-RevId: a83bfa6b93bb86c03fcb8c45cc646370cd0307a0

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/ConditionalMemoryDumpCommand.kt
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/ConditionalMemoryDumpCommand.kt
@@ -1,13 +1,12 @@
 package com.jetbrains.performancePlugin.commands
 
-import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.ui.playback.PlaybackContext
-import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.LowMemoryWatcher
 import com.intellij.util.MemoryDumpHelper
 
-class ConditionalMemoryDumpCommand(text: String, line: Int) : PerformanceCommandCoroutineAdapter(text, line), Disposable {
+class ConditionalMemoryDumpCommand(text: String, line: Int) : PerformanceCommandCoroutineAdapter(text, line) {
   companion object {
     const val NAME = "conditionalMemoryDumpCommand"
     const val PREFIX = CMD_PREFIX + NAME
@@ -17,21 +16,20 @@ class ConditionalMemoryDumpCommand(text: String, line: Int) : PerformanceCommand
   override suspend fun doExecute(context: PlaybackContext) {
     val targetMessageCount = extractCommandArgument(PREFIX).toInt()
     var currentMessageCount = 0
+
+    @Suppress("IncorrectParentDisposable")
     LowMemoryWatcher.register({
         currentMessageCount++
         if (currentMessageCount == targetMessageCount) {
           val memoryDumpPath = MemoryDumpCommand.getMemoryDumpPath()
           LOG.info("Dumping memory snapshot to: $memoryDumpPath")
           MemoryDumpHelper.captureMemoryDump(memoryDumpPath)
-          Disposer.dispose(this)
         }
-      }, LowMemoryWatcher.LowMemoryWatcherType.ONLY_AFTER_GC, this)
+      //we can't register a proper disposable since we wait till all commands are finished
+      }, LowMemoryWatcher.LowMemoryWatcherType.ONLY_AFTER_GC, ApplicationManager.getApplication())
   }
 
   override fun getName(): String {
     return NAME
-  }
-
-  override fun dispose() {
   }
 }
