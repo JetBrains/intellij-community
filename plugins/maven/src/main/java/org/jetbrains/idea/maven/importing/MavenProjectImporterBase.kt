dc0680699dc12aaf26c20f351c2420bbea918d54
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-05-30 12:37:56 +0200

    [maven] IDEA-313767 refactoring: MavenProjectImporterBase converted to kotlin
    GitOrigin-RevId: 53e18fcc5da2c08d4b865130cb11c82f688824e4

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/MavenProjectImporterBase.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/MavenProjectImporterBase.kt
@@ -1,173 +1,150 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.idea.maven.importing;
+package org.jetbrains.idea.maven.importing
 
-import com.intellij.compiler.impl.javaCompiler.javac.JavacConfiguration;
-import com.intellij.internal.statistic.StructuredIdeActivity;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider;
-import com.intellij.openapi.externalSystem.service.project.IdeUIModifiableModelsProvider;
-import com.intellij.openapi.externalSystem.service.project.ProjectDataManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ex.ProjectRootManagerEx;
-import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.idea.maven.model.MavenArtifact;
-import org.jetbrains.idea.maven.project.*;
-import org.jetbrains.idea.maven.statistics.MavenImportCollector;
-import org.jetbrains.idea.maven.utils.MavenProgressIndicator;
-import org.jetbrains.idea.maven.utils.MavenUtil;
-import org.jetbrains.jps.model.java.compiler.JpsJavaCompilerOptions;
-
-import java.io.File;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
+import com.intellij.compiler.impl.javaCompiler.javac.JavacConfiguration
+import com.intellij.internal.statistic.StructuredIdeActivity
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
+import com.intellij.openapi.externalSystem.service.project.IdeUIModifiableModelsProvider
+import com.intellij.openapi.externalSystem.service.project.ProjectDataManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ex.ProjectRootManagerEx
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.util.containers.ContainerUtil
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.idea.maven.importing.MavenLegacyModuleImporter.ExtensionImporter
+import org.jetbrains.idea.maven.importing.MavenLegacyModuleImporter.ExtensionImporter.CountAndTime
+import org.jetbrains.idea.maven.project.*
+import org.jetbrains.idea.maven.statistics.MavenImportCollector
+import org.jetbrains.idea.maven.utils.MavenProgressIndicator
+import org.jetbrains.idea.maven.utils.MavenUtil
+import java.io.File
+import java.util.concurrent.TimeUnit
+import java.util.function.Consumer
 
 @ApiStatus.Internal
-public abstract class MavenProjectImporterBase implements MavenProjectImporter {
-  protected final Project myProject;
+abstract class MavenProjectImporterBase(@JvmField protected val myProject: Project,
+                                        @JvmField protected val myProjectsTree: MavenProjectsTree,
+                                        @JvmField protected val myImportingSettings: MavenImportingSettings,
+                                        @JvmField protected val myIdeModifiableModelsProvider: IdeModifiableModelsProvider) : MavenProjectImporter {
+  @JvmField
+  protected val myModelsProvider: IdeModifiableModelsProvider
 
-  protected final MavenProjectsTree myProjectsTree;
-  protected final MavenImportingSettings myImportingSettings;
-
-  protected final IdeModifiableModelsProvider myIdeModifiableModelsProvider;
-  protected final IdeModifiableModelsProvider myModelsProvider;
-
-  public MavenProjectImporterBase(Project project,
-                                  MavenProjectsTree projectsTree,
-                                  MavenImportingSettings importingSettings,
-                                  @NotNull IdeModifiableModelsProvider modelsProvider) {
-    myProject = project;
-
-    myProjectsTree = projectsTree;
-    myImportingSettings = importingSettings;
-
-    myIdeModifiableModelsProvider = modelsProvider;
-    myModelsProvider = myIdeModifiableModelsProvider;
+  init {
+    myModelsProvider = myIdeModifiableModelsProvider
   }
 
-  protected Set<MavenProject> selectProjectsToImport(Collection<MavenProject> originalProjects) {
-    Set<MavenProject> result = new HashSet<>();
-    for (MavenProject each : originalProjects) {
-      if (!shouldCreateModuleFor(each)) continue;
-      result.add(each);
+  protected fun selectProjectsToImport(originalProjects: Collection<MavenProject>): Set<MavenProject> {
+    val result: MutableSet<MavenProject> = HashSet()
+    for (each in originalProjects) {
+      if (!shouldCreateModuleFor(each)) continue
+      result.add(each)
     }
-    return result;
+    return result
   }
 
-  protected boolean shouldCreateModuleFor(MavenProject project) {
-    if (myProjectsTree.isIgnored(project)) return false;
-    return !project.isAggregator() || myImportingSettings.isCreateModulesForAggregators();
+  protected fun shouldCreateModuleFor(project: MavenProject): Boolean {
+    return if (myProjectsTree.isIgnored(project)) false else !project.isAggregator || myImportingSettings.isCreateModulesForAggregators
   }
 
-  public static void importExtensions(Project project,
-                                      IdeModifiableModelsProvider modifiableModelsProvider,
-                                      List<MavenLegacyModuleImporter.ExtensionImporter> extensionImporters,
-                                      List<MavenProjectsProcessorTask> postTasks,
-                                      StructuredIdeActivity activity) {
-    extensionImporters = ContainerUtil.filter(extensionImporters, it -> !it.isModuleDisposed());
-
-    if (extensionImporters.isEmpty()) return;
-
-    long beforeBridgesCreation = System.nanoTime();
-    IdeModifiableModelsProvider provider;
-    if (modifiableModelsProvider instanceof IdeUIModifiableModelsProvider) {
-      provider = modifiableModelsProvider; // commit does nothing for this provider, so it should be reused
+  protected class RefreshingFilesTask(private val myFiles: Set<File>) : MavenProjectsProcessorTask {
+    override fun perform(project: Project,
+                         embeddersManager: MavenEmbeddersManager,
+                         console: MavenConsole,
+                         indicator: MavenProgressIndicator) {
+      indicator.setText(MavenProjectBundle.message("progress.text.refreshing.files"))
+      doRefreshFiles(myFiles)
     }
-    else {
-      provider = ProjectDataManager.getInstance().createModifiableModelsProvider(project);
-    }
-    long bridgesCreationNano = System.nanoTime() - beforeBridgesCreation;
+  }
 
-    try {
-      long beforeInitInit = System.nanoTime();
-      extensionImporters.forEach(importer -> importer.init(provider));
-      bridgesCreationNano += System.nanoTime() - beforeInitInit;
-
-      Map<Class<? extends MavenImporter>, MavenLegacyModuleImporter.ExtensionImporter.CountAndTime> counters = new HashMap<>();
-
-      extensionImporters.forEach(importer -> importer.preConfig(counters));
-      extensionImporters.forEach(importer -> importer.config(postTasks, counters));
-      extensionImporters.forEach(importer -> importer.postConfig(counters));
-
-      for (Map.Entry<Class<? extends MavenImporter>, MavenLegacyModuleImporter.ExtensionImporter.CountAndTime> each : counters.entrySet()) {
-        MavenImportCollector.IMPORTER_RUN.log(project,
-                                              MavenImportCollector.ACTIVITY_ID.with(activity),
-                                              MavenImportCollector.IMPORTER_CLASS.with(each.getKey()),
-                                              MavenImportCollector.NUMBER_OF_MODULES.with(each.getValue().count),
-                                              MavenImportCollector.TOTAL_DURATION_MS.with(
-                                                TimeUnit.NANOSECONDS.toMillis(each.getValue().timeNano)));
+  companion object {
+    @JvmStatic
+    fun importExtensions(project: Project?,
+                         modifiableModelsProvider: IdeModifiableModelsProvider?,
+                         extensionImporters: List<ExtensionImporter>,
+                         postTasks: List<MavenProjectsProcessorTask?>?,
+                         activity: StructuredIdeActivity?) {
+      var extensionImporters = extensionImporters
+      extensionImporters = ContainerUtil.filter(extensionImporters) { it: ExtensionImporter -> !it.isModuleDisposed }
+      if (extensionImporters.isEmpty()) return
+      val beforeBridgesCreation = System.nanoTime()
+      val provider: IdeModifiableModelsProvider
+      provider = // commit does nothing for this provider, so it should be reused
+        modifiableModelsProvider as? IdeUIModifiableModelsProvider ?: ProjectDataManager.getInstance().createModifiableModelsProvider(
+          project!!)
+      var bridgesCreationNano = System.nanoTime() - beforeBridgesCreation
+      try {
+        val beforeInitInit = System.nanoTime()
+        extensionImporters.forEach(
+          Consumer { importer: ExtensionImporter -> importer.init(provider) })
+        bridgesCreationNano += System.nanoTime() - beforeInitInit
+        val counters: Map<Class<out MavenImporter?>, CountAndTime> = HashMap()
+        extensionImporters.forEach(
+          Consumer { importer: ExtensionImporter -> importer.preConfig(counters) })
+        extensionImporters.forEach(
+          Consumer { importer: ExtensionImporter -> importer.config(postTasks, counters) })
+        extensionImporters.forEach(
+          Consumer { importer: ExtensionImporter -> importer.postConfig(counters) })
+        for ((key, value) in counters) {
+          MavenImportCollector.IMPORTER_RUN.log(project,
+                                                MavenImportCollector.ACTIVITY_ID.with(activity!!),
+                                                MavenImportCollector.IMPORTER_CLASS.with(key),
+                                                MavenImportCollector.NUMBER_OF_MODULES.with(value.count),
+                                                MavenImportCollector.TOTAL_DURATION_MS.with(
+                                                  TimeUnit.NANOSECONDS.toMillis(value.timeNano)))
+        }
       }
-    }
-    finally {
-      long beforeCommit = System.nanoTime();
-      MavenUtil.invokeAndWaitWriteAction(project, () -> {
-        ProjectRootManagerEx.getInstanceEx(project).mergeRootsChangesDuring(() -> {
-          provider.commit();
-        });
-      });
-      long afterCommit = System.nanoTime();
-
-      MavenImportCollector.LEGACY_IMPORTERS_STATS.log(project,
-                                                      MavenImportCollector.ACTIVITY_ID.with(activity),
-                                                      MavenImportCollector.DURATION_OF_LEGACY_BRIDGES_CREATION_MS.with(
-                                                TimeUnit.NANOSECONDS.toMillis(bridgesCreationNano)),
-                                                      MavenImportCollector.DURATION_OF_LEGACY_BRIDGES_COMMIT_MS.with(
-                                                TimeUnit.NANOSECONDS.toMillis(afterCommit - beforeCommit)));
-    }
-  }
-
-  public static void scheduleRefreshResolvedArtifacts(List<MavenProjectsProcessorTask> postTasks,
-                                                      Iterable<MavenProject> projectsToRefresh) {
-    // We have to refresh all the resolved artifacts manually in order to
-    // update all the VirtualFilePointers. It is not enough to call
-    // VirtualFileManager.refresh() since the newly created files will be only
-    // picked by FS when FileWatcher finishes its work. And in the case of import
-    // it doesn't finish in time.
-    // I couldn't manage to write a test for this since behaviour of VirtualFileManager
-    // and FileWatcher differs from real-life execution.
-
-    HashSet<File> files = new HashSet<>();
-    for (MavenProject project : projectsToRefresh) {
-      for (MavenArtifact dependency : project.getDependencies()) {
-        files.add(dependency.getFile());
+      finally {
+        val beforeCommit = System.nanoTime()
+        MavenUtil.invokeAndWaitWriteAction(project!!) {
+          ProjectRootManagerEx.getInstanceEx(project).mergeRootsChangesDuring { provider.commit() }
+        }
+        val afterCommit = System.nanoTime()
+        MavenImportCollector.LEGACY_IMPORTERS_STATS.log(project,
+                                                        MavenImportCollector.ACTIVITY_ID.with(activity!!),
+                                                        MavenImportCollector.DURATION_OF_LEGACY_BRIDGES_CREATION_MS.with(
+                                                          TimeUnit.NANOSECONDS.toMillis(bridgesCreationNano)),
+                                                        MavenImportCollector.DURATION_OF_LEGACY_BRIDGES_COMMIT_MS.with(
+                                                          TimeUnit.NANOSECONDS.toMillis(afterCommit - beforeCommit)))
       }
     }
 
-    if (MavenUtil.isMavenUnitTestModeEnabled()) {
-      doRefreshFiles(files);
-    }
-    else {
-      postTasks.add(new RefreshingFilesTask(files));
-    }
-  }
-
-  protected static void removeOutdatedCompilerConfigSettings(Project project) {
-    ApplicationManager.getApplication().assertWriteAccessAllowed();
-
-    final JpsJavaCompilerOptions javacOptions = JavacConfiguration.getOptions(project, JavacConfiguration.class);
-    String options = javacOptions.ADDITIONAL_OPTIONS_STRING;
-    options = options.replaceFirst("(-target (\\S+))", ""); // Old IDEAs saved
-    javacOptions.ADDITIONAL_OPTIONS_STRING = options;
-  }
-
-
-  protected static void doRefreshFiles(Set<File> files) {
-    LocalFileSystem.getInstance().refreshIoFiles(files);
-  }
-
-  protected static class RefreshingFilesTask implements MavenProjectsProcessorTask {
-    private final Set<File> myFiles;
-
-    protected RefreshingFilesTask(Set<File> files) {
-      myFiles = files;
+    @JvmStatic
+    fun scheduleRefreshResolvedArtifacts(postTasks: MutableList<MavenProjectsProcessorTask>,
+                                         projectsToRefresh: Iterable<MavenProject>) {
+      // We have to refresh all the resolved artifacts manually in order to
+      // update all the VirtualFilePointers. It is not enough to call
+      // VirtualFileManager.refresh() since the newly created files will be only
+      // picked by FS when FileWatcher finishes its work. And in the case of import
+      // it doesn't finish in time.
+      // I couldn't manage to write a test for this since behaviour of VirtualFileManager
+      // and FileWatcher differs from real-life execution.
+      val files = HashSet<File>()
+      for (project in projectsToRefresh) {
+        for (dependency in project.dependencies) {
+          files.add(dependency.file)
+        }
+      }
+      if (MavenUtil.isMavenUnitTestModeEnabled()) {
+        doRefreshFiles(files)
+      }
+      else {
+        postTasks.add(RefreshingFilesTask(files))
+      }
     }
 
-    @Override
-    public void perform(Project project, MavenEmbeddersManager embeddersManager, MavenConsole console, MavenProgressIndicator indicator) {
-      indicator.setText(MavenProjectBundle.message("progress.text.refreshing.files"));
-      doRefreshFiles(myFiles);
+    @ApiStatus.Internal
+    @JvmStatic
+    fun removeOutdatedCompilerConfigSettings(project: Project) {
+      ApplicationManager.getApplication().assertWriteAccessAllowed()
+      val javacOptions = JavacConfiguration.getOptions(project, JavacConfiguration::class.java)
+      var options = javacOptions.ADDITIONAL_OPTIONS_STRING
+      options = options.replaceFirst("(-target (\\S+))".toRegex(), "") // Old IDEAs saved
+      javacOptions.ADDITIONAL_OPTIONS_STRING = options
+    }
+
+    protected fun doRefreshFiles(files: Set<File>?) {
+      LocalFileSystem.getInstance().refreshIoFiles(files!!)
     }
   }
 }
