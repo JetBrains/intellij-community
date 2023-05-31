a80ff00bffb6f0c391da2c226851581325f316bd
Leonid Shalupov <leonid@shalupov.com>
2023-05-31 18:39:47 +0200

    Revert "passwords: getting a password from password safe is a slow operation"
    This reverts commit 2014a82606bb988d459da637854c03c3b0c14835.
    
    GitOrigin-RevId: 1a82cf6981d016f42ad674b3c87c6b3884de19f1

--- platform/credential-store/src/PasswordSafeImpl.kt
+++ platform/credential-store/src/PasswordSafeImpl.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 @file:Suppress("PackageDirectoryMismatch")
 package com.intellij.ide.passwordSafe.impl
 
@@ -15,7 +15,6 @@ import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.util.NlsContexts
 import com.intellij.serviceContainer.NonInjectable
-import com.intellij.util.SlowOperations
 import com.intellij.util.concurrency.SynchronizedClearableLazy
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -90,10 +89,6 @@ abstract class BasePasswordSafe(private val coroutineScope: CoroutineScope) : Pa
     get() = settings.providerType == ProviderType.MEMORY_ONLY
 
   override fun get(attributes: CredentialAttributes): Credentials? {
-    // Doing PasswordSafe operations on EDT leads to freezes
-    // see e.g. https://youtrack.jetbrains.com/issue/IDEA-218531/Freezes-in-BasePasswordSafe.get
-    SlowOperations.assertSlowOperationsAreAllowed()
-
     val value = currentProvider.get(attributes)
     if ((value == null || value.password.isNullOrEmpty()) && memoryHelperProvider.isInitialized()) {
       // if password was set as `memoryOnly`
@@ -105,10 +100,6 @@ abstract class BasePasswordSafe(private val coroutineScope: CoroutineScope) : Pa
   }
 
   override fun set(attributes: CredentialAttributes, credentials: Credentials?) {
-    // Doing PasswordSafe operations on EDT leads to freezes
-    // see e.g. https://youtrack.jetbrains.com/issue/IDEA-218531/Freezes-in-BasePasswordSafe.get
-    SlowOperations.assertSlowOperationsAreAllowed()
-
     currentProvider.set(attributes, credentials)
     if (attributes.isPasswordMemoryOnly && !credentials?.password.isNullOrEmpty()) {
       // we must store because otherwise on get will be no password
