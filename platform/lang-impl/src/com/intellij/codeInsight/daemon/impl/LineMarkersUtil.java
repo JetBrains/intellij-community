cd969780894a5d27536e3cef4cbf02e16d6feacc
Alexey Kudravtsev <cdr@intellij.com>
2023-06-02 19:03:12 +0200

    when PCE is thrown in the middle of setLineMarkersToEditor(), release involved highlighters to avoid double-creating them in the next pass to fix IDEA-319760 Duplicate icons on gutter
    GitOrigin-RevId: 784abf78bf3ece597593984aba831cbc670bab23

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/LineMarkersUtil.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/LineMarkersUtil.java
@@ -55,36 +55,41 @@ final class LineMarkersUtil {
     MarkupModelEx markupModel = (MarkupModelEx)DocumentMarkupModel.forDocument(document, project, true);
     HighlightersRecycler toReuse = new HighlightersRecycler();
     synchronized (LOCK) {
-      processLineMarkers(project, document, bounds, group, info -> {
-        RangeHighlighterEx highlighter = (RangeHighlighterEx)info.highlighter;
-        if (highlighter != null) {
-          toReuse.recycleHighlighter(highlighter);
-        }
-        return true;
-      });
+      try {
+        processLineMarkers(project, document, bounds, group, info -> {
+          RangeHighlighterEx highlighter = (RangeHighlighterEx)info.highlighter;
+          if (highlighter != null) {
+            toReuse.recycleHighlighter(highlighter);
+          }
+          return true;
+        });
 
-      if (LOG.isDebugEnabled()) {
-        List<LineMarkerInfo<?>> oldMarkers = DaemonCodeAnalyzerImpl.getLineMarkers(document, project);
-        LOG.debug("LineMarkersUtil.setLineMarkersToEditor(markers: " + newMarkers + ", group: " + group +
-                  "); oldMarkers: " + oldMarkers + "; reused: " + toReuse.forAllInGarbageBin().size());
+        if (LOG.isDebugEnabled()) {
+          List<LineMarkerInfo<?>> oldMarkers = DaemonCodeAnalyzerImpl.getLineMarkers(document, project);
+          LOG.debug("LineMarkersUtil.setLineMarkersToEditor(markers: " + newMarkers + ", group: " + group +
+                    "); oldMarkers: " + oldMarkers + "; reused: " + toReuse.forAllInGarbageBin().size());
+        }
+
+        for (LineMarkerInfo<?> info : newMarkers) {
+          PsiElement element = info.getElement();
+          if (element == null) {
+            continue;
+          }
+
+          TextRange textRange = element.getTextRange();
+          if (textRange == null) continue;
+          TextRange elementRange = InjectedLanguageManager.getInstance(project).injectedToHost(element, textRange);
+          if (bounds.contains(elementRange)) {
+            createOrReuseLineMarker(info, markupModel, toReuse);
+          }
+        }
+
+        for (RangeHighlighter highlighter : toReuse.forAllInGarbageBin()) {
+          highlighter.dispose();
+        }
       }
-
-      for (LineMarkerInfo<?> info : newMarkers) {
-        PsiElement element = info.getElement();
-        if (element == null) {
-          continue;
-        }
-
-        TextRange textRange = element.getTextRange();
-        if (textRange == null) continue;
-        TextRange elementRange = InjectedLanguageManager.getInstance(project).injectedToHost(element, textRange);
-        if (bounds.contains(elementRange)) {
-          createOrReuseLineMarker(info, markupModel, toReuse);
-        }
-      }
-
-      for (RangeHighlighter highlighter : toReuse.forAllInGarbageBin()) {
-        highlighter.dispose();
+      finally {
+        toReuse.releaseHighlighters();
       }
     }
   }
