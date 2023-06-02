91012acf1cacac70ee538c6c967af0aca939a94c
Alexey Kudravtsev <cdr@intellij.com>
2023-06-02 19:52:14 +0200

    change preview text to be an actual compilable java code
    GitOrigin-RevId: db888569e6c49635822ac9f5b7f550571a06f4b7

--- java/java-impl/resources/inlayProviders/java.method.chains/preview.java
+++ java/java-impl/resources/inlayProviders/java.method.chains/preview.java
@@ -1,8 +1,9 @@
-abstract class Foo<T> {
-  void main() {
-    Stream.of(1, 2, 3).filter(x -> x % 2 == 0) <# Foo<Integer> #>
-      .map(x -> x * 2)<# Foo<Integer> #>
-      .map(x -> "item: " + x)<# Foo<Object> #>
+import java.util.stream.*;
+class Foo {
+  {
+    Stream.of(1, 2, 3).filter(x -> x % 2 == 0)<# Stream<Integer> #>
+      .map(x -> x * 2)<# Stream<Integer> #>
+      .map(x -> "item: " + x)<# Stream<String> #>
       .forEach(System.out::println);
   }
 }
