09dfe569aa3164f96f03778e9cdea682bd43dabe
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-06-03 13:17:50 +0200

    [android] IDEA-230089 Make Smali Support optional dependency of Android plugin and unbundle it
    GitOrigin-RevId: 2128d6d82be24f25e356baee86b2d3bd338d3449

--- platform/build-scripts/src/org/jetbrains/intellij/build/BaseIdeaProperties.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/BaseIdeaProperties.kt
@@ -85,7 +85,6 @@ val IDEA_BUNDLED_PLUGINS: PersistentList<String> = DEFAULT_BUNDLED_PLUGINS + per
   "intellij.eclipse",
   "intellij.platform.langInjection",
   "intellij.java.debugger.streams",
-  "intellij.android.smali",
   "intellij.completionMlRanking",
   "intellij.completionMlRankingModels",
   "intellij.statsCollector",
