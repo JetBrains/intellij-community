ab7ca74474a808d054785666492aefaef631db7c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-06-03 09:52:46 +0200

    Improve TextMate bundle registration and refactor code
    Update TextMate bundle registration to run in parallel using kotlinx.coroutines to improve performance. Also refactor variable and method names for better readability and consistency.
    
    The changes in this commit:
    - Replace some of the imports in the bundlesLoader.kt to better suit the updated implementation.
    - Modify the registerBundlesInParallel function to run the registration tasks in parallel using kotlinx.coroutines.
    - Change the TextMateServiceImpl.java variable and method names for better readability and consistency.
    - Update TextMateService.java and TextMateEditorUtils.java to use the refactored names.
    - Update TextMateCustomLiveTemplate.java to use the refactored snippet registry name.
    
    GitOrigin-RevId: 849b65da8ba899ca959dc196bf974f1e941c18ff

--- plugins/textmate/src/org/jetbrains/plugins/textmate/bundlesLoader.kt
+++ plugins/textmate/src/org/jetbrains/plugins/textmate/bundlesLoader.kt
@@ -3,30 +3,30 @@
 package org.jetbrains.plugins.textmate
 
 import com.intellij.openapi.application.EDT
-import com.intellij.openapi.progress.runBlockingMaybeCancellable
 import com.intellij.openapi.progress.util.ProgressIndicatorUtils
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.future.asCompletableFuture
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.nio.file.Path
 import java.util.function.Consumer
 
 @JvmOverloads
 internal fun registerBundlesInParallel(scope: CoroutineScope,
                                        bundlePaths: List<Path>,
-                                       registrar: java.util.function.Function<Path, Boolean>,
+                                       registrar: (Path) -> Boolean,
                                        registrationFailed: Consumer<Path>? = null) {
-  val initializationJob = scope.launch(Dispatchers.IO) {
-    coroutineScope {
-      bundlePaths.map { bundlePath ->
-        launch {
-          if (!registrar.apply(bundlePath)) {
-            if (registrationFailed != null) {
-              withContext(Dispatchers.EDT) {
-                registrationFailed.accept(bundlePath)
-              }
-            }
-            else {
-              TextMateService.LOG.error("Cannot load builtin textmate bundle", bundlePath.toString())
+  val initializationJob = scope.launch {
+    bundlePaths.map { bundlePath ->
+      launch(Dispatchers.IO) {
+        if (!registrar(bundlePath)) {
+          if (registrationFailed == null) {
+            TextMateService.LOG.error("Cannot load builtin textmate bundle", bundlePath.toString())
+          }
+          else {
+            withContext(Dispatchers.EDT) {
+              registrationFailed.accept(bundlePath)
             }
           }
         }
