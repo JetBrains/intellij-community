aaf592c7209626b24d24bec05bd071d6f3c6f4cc
Adam Malek <adam.malek@jetbrains.com>
2023-06-05 13:27:54 +0200

    ML in SE: Fix suggestion always null (IDEA-321742)
    GitOrigin-RevId: 0805678e63584ad89f8f0d5148e73325e9e69285

--- plugins/search-everywhere-ml/typos/src/com/intellij/searchEverywhereMl/typos/log/SearchEverywhereTyposLogger.kt
+++ plugins/search-everywhere-ml/typos/src/com/intellij/searchEverywhereMl/typos/log/SearchEverywhereTyposLogger.kt
@@ -12,7 +12,7 @@ import com.intellij.searchEverywhereMl.common.log.MLSE_RECORDER_ID
 
 class SearchEverywhereTyposLogger : CounterUsagesCollector(), SearchEverywhereItemSelectedListener {
   companion object {
-    private val GROUP = EventLogGroup("typos.log", 1, MLSE_RECORDER_ID)
+    private val GROUP = EventLogGroup("typos.log", 2, MLSE_RECORDER_ID)
 
     private val TAB_ID = EventFields.String("tabId", SE_TABS)
 
@@ -34,6 +34,7 @@ class SearchEverywhereTyposLogger : CounterUsagesCollector(), SearchEverywhereIt
                               closePopup: Boolean) {
     val suggestion = elementsProvider.invoke()
       .firstOrNull()
+      ?.element
       ?.takeIfIsInstance<SearchEverywhereSpellCheckResult.Correction>()
 
     val eventData = computeEventData(suggestion, selectedItems)
