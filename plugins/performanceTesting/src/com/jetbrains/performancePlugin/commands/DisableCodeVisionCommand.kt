5b1100aaea040ffce341c6f12a3e474f69f8556e
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-06-05 17:46:07 +0200

    [performance] IDEA-321376 Measure code vision impact on startup
    GitOrigin-RevId: 8e3031d1b9238b99cb75258582ac0461f5309c4b

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/DisableCodeVisionCommand.kt
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/DisableCodeVisionCommand.kt
@@ -1,10 +1,9 @@
 package com.jetbrains.performancePlugin.commands
 
-import com.intellij.codeInsight.codeVision.settings.CodeVisionSettings
 import com.intellij.openapi.ui.playback.PlaybackContext
 import com.intellij.openapi.ui.playback.commands.AbstractCommand
 import com.intellij.openapi.util.ActionCallback
-import com.jetbrains.performancePlugin.utils.ActionCallbackProfilerStopper
+import com.intellij.openapi.util.registry.Registry
 import org.jetbrains.concurrency.Promise
 import org.jetbrains.concurrency.toPromise
 
@@ -14,7 +13,8 @@ class DisableCodeVisionCommand(text: String, line: Int) : AbstractCommand(text, 
   }
 
   override fun _execute(context: PlaybackContext): Promise<Any?> {
-    CodeVisionSettings.instance().codeVisionEnabled = false
+    Registry.get("editor.codeVision.new").setValue(false)
+
     return ActionCallback.DONE.toPromise()
   }
 }
