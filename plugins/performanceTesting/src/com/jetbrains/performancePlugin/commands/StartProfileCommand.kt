a6b47d5b05b52ea7186b8d81904f0104ce827825
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-06-06 12:51:15 +0200

    convert StartProfileCommand to kotlin
    GitOrigin-RevId: f29a676075d592af57d08530ba890f60aa9951b8

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/StartProfileCommand.kt
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/StartProfileCommand.kt
@@ -1,58 +1,50 @@
-package com.jetbrains.performancePlugin.commands;
+package com.jetbrains.performancePlugin.commands
 
-import com.intellij.openapi.ui.playback.PlaybackContext;
-import com.intellij.openapi.ui.playback.commands.AbstractCommand;
-import com.intellij.openapi.util.ActionCallback;
-import com.intellij.openapi.util.text.StringUtil;
-import com.jetbrains.performancePlugin.PerformanceTestingBundle;
-import com.jetbrains.performancePlugin.profilers.Profiler;
-import com.jetbrains.performancePlugin.utils.ActionCallbackProfilerStopper;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.concurrency.Promise;
-import org.jetbrains.concurrency.Promises;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.intellij.openapi.ui.playback.PlaybackContext
+import com.intellij.openapi.ui.playback.commands.AbstractCommand
+import com.intellij.openapi.util.ActionCallback
+import com.intellij.openapi.util.text.StringUtil
+import com.jetbrains.performancePlugin.PerformanceTestingBundle
+import com.jetbrains.performancePlugin.commands.Waiter.checkCondition
+import com.jetbrains.performancePlugin.profilers.Profiler
+import com.jetbrains.performancePlugin.utils.ActionCallbackProfilerStopper
+import org.jetbrains.concurrency.Promise
+import org.jetbrains.concurrency.toPromise
+import java.util.*
 
 /**
  * Command starts async or yourkit profiler based on system property integrationTests.profiler.
  * Command must be followed by activity name and optionally by parameters.
- * <p>
+ *
+ *
  * Syntax: %startProfile &lt;activityName&gt; [parameters]
  * Example: %startProfile magento_inspection event=alloc
  */
-public final class StartProfileCommand extends AbstractCommand {
-  public static final String PREFIX = CMD_PREFIX + "startProfile";
-
-  public StartProfileCommand(@NotNull String text, int line) {
-    super(text, line);
-  }
-
-  @NotNull
-  @Override
-  protected Promise<Object> _execute(@NotNull PlaybackContext context) {
-    final ActionCallback myActionCallback = new ActionCallbackProfilerStopper();
-
-    final String[] executedCommand = extractCommandArgument(PREFIX).split("\\s+", 2);
-    final String myActivityName = executedCommand[0]; //activityName should go right after %startProfile
-
+class StartProfileCommand(text: String, line: Int) : AbstractCommand(text, line) {
+  override fun _execute(context: PlaybackContext): Promise<Any> {
+    val myActionCallback: ActionCallback = ActionCallbackProfilerStopper()
+    val executedCommand = extractCommandArgument(PREFIX).split("\\s+".toRegex(), limit = 2).toTypedArray()
+    val myActivityName = executedCommand[0] //activityName should go right after %startProfile
     if (StringUtil.isEmpty(myActivityName)) {
-      myActionCallback.reject(PerformanceTestingBundle.message("command.start.error"));
+      myActionCallback.reject(PerformanceTestingBundle.message("command.start.error"))
     }
     else {
       try {
-        List<String> parameters = executedCommand.length > 1 ? Arrays.asList(executedCommand[1].trim().split(","))
-                                                             : new ArrayList<>();
-        Profiler.getCurrentProfilerHandler().startProfiling(myActivityName, parameters);
-        Waiter.checkCondition(() -> Profiler.isAnyProfilingStarted()).await();
-        myActionCallback.setDone();
+        val parameters = if (executedCommand.size > 1) Arrays.asList(
+          *executedCommand[1].trim { it <= ' ' }.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray())
+        else ArrayList()
+        Profiler.getCurrentProfilerHandler().startProfiling(myActivityName, parameters)
+        checkCondition { Profiler.isAnyProfilingStarted() }.await()
+        myActionCallback.setDone()
       }
-      catch (Throwable e) {
-        myActionCallback.reject(e.getMessage());
+      catch (e: Throwable) {
+        myActionCallback.reject(e.message)
       }
     }
+    return myActionCallback.toPromise()
+  }
 
-    return Promises.toPromise(myActionCallback);
+  companion object {
+    const val PREFIX = CMD_PREFIX + "startProfile"
   }
 }
