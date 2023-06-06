8277b367d1a767c04b497e0ea4f5cf9dfc1bf45d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-06-06 18:46:06 +0200

    OpenProjectView - use coroutines
    GitOrigin-RevId: 965cd1900865d43d13c8c07019322b19b2e5bf14

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/OpenProjectView.kt
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/OpenProjectView.kt
@@ -1,42 +1,36 @@
 package com.jetbrains.performancePlugin.commands
 
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.components.serviceAsync
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.ui.playback.PlaybackContext
-import com.intellij.openapi.ui.playback.commands.AbstractCommand
-import com.intellij.openapi.util.ActionCallback
+import com.intellij.openapi.ui.playback.commands.PlaybackCommandCoroutineAdapter
 import com.intellij.openapi.wm.ToolWindowId
-import com.intellij.openapi.wm.ToolWindowManager.Companion.getInstance
-import com.jetbrains.performancePlugin.utils.ActionCallbackProfilerStopper
-import org.jetbrains.concurrency.Promise
-import org.jetbrains.concurrency.toPromise
+import com.intellij.openapi.wm.ToolWindowManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
+import kotlin.time.Duration.Companion.seconds
 
-class OpenProjectView(text: String, line: Int) : AbstractCommand(text, line) {
-  override fun _execute(context: PlaybackContext): Promise<Any> {
-    val actionCallback: ActionCallback = ActionCallbackProfilerStopper()
-    ApplicationManager.getApplication().invokeLater {
-      val windowManager = getInstance(context.project)
-      val window = windowManager.getToolWindow(ToolWindowId.PROJECT_VIEW)
-      if (window != null) {
-        if (!window.isActive &&
-            (windowManager.isEditorComponentActive || ToolWindowId.PROJECT_VIEW != windowManager.activeToolWindowId)) {
+internal class OpenProjectView(text: String, line: Int) : PlaybackCommandCoroutineAdapter(text, line) {
+  companion object {
+    const val PREFIX: String = CMD_PREFIX + "openProjectView"
+    private val LOG = logger<OpenProjectView>()
+  }
+
+  override suspend fun doExecute(context: PlaybackContext) {
+    val windowManager = context.project.serviceAsync<ToolWindowManager>()
+    withTimeout(60.seconds) {
+      withContext(Dispatchers.EDT) {
+        val window = windowManager.getToolWindow(ToolWindowId.PROJECT_VIEW) ?: throw IllegalStateException("Window is not found")
+        if (!window.isActive && (windowManager.isEditorComponentActive || ToolWindowId.PROJECT_VIEW != windowManager.activeToolWindowId)) {
           window.activate(null)
           LOG.warn("Project View is opened")
         }
         else {
           LOG.warn("Project View has been opened already")
         }
-        actionCallback.setDone()
-      }
-      else {
-        actionCallback.reject("Window is not found")
       }
     }
-    return actionCallback.toPromise()
-  }
-
-  companion object {
-    const val PREFIX = CMD_PREFIX + "openProjectView"
-    private val LOG = Logger.getInstance(OpenProjectView::class.java)
   }
 }
