3b165eb1c4739053f1b737801602605a0cc63709
Eugene Lazurin <eugene.lazurin@jetbrains.com>
2023-06-06 13:55:53 +0200

    [rdct] launcher: add missed UI property in eap registry file
    GitOrigin-RevId: 818758d05d1f2993199324323110254fc47cc6ab

--- native/XPlatLauncher/src/remote_dev.rs
+++ native/XPlatLauncher/src/remote_dev.rs
@@ -321,6 +321,8 @@ impl RemoteDevLaunchConfiguration {
                 match remote_dev_new_ui_enabled.as_str() {
                     "1" | "true" => {
                         info!("Force enable new UI");
+                        self.init_eap_registry_file_if_needed()?.write_all("\nide.experimental.ui\ntrue\n".as_bytes())
+                            .with_context(|| "Failed to write in 'early-access-registry.txt'")?;
                         remote_dev_properties.push(("ide.experimental.ui", "true"));
                     },
                     _ => {
@@ -446,6 +448,21 @@ impl RemoteDevLaunchConfiguration {
             .context("Failed to create a trust file")?;
 
         Ok(trust_file_path)
+    }
+
+    fn init_eap_registry_file_if_needed(&self) -> Result<File> {
+        let ij_host_config_dir = &self.config_dir;
+        let eap_registry_file_path = &ij_host_config_dir.join("early-access-registry.txt");
+
+        if eap_registry_file_path.exists() {
+            debug!("{eap_registry_file_path:?} exists");
+            return Ok(File::open(&eap_registry_file_path)?);
+        }
+
+        let eap_registry_file = File::create(eap_registry_file_path).with_context(|| format!("Failed to create {eap_registry_file_path:?}"))?;
+        debug!("File '{eap_registry_file:?}' has been created");
+
+        Ok(eap_registry_file)
     }
 
     #[cfg(not(target_os = "linux"))]
