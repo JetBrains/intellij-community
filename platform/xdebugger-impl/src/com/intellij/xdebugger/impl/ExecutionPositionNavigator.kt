ca42f31c5982f8b64f2abb70888cb5f607f5a1b5
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2023-06-06 20:33:01 +0200

    RIDER-94415: xdebugger: Load execution position document on BGT
    Fixes:
     - RIDER-94415 Slow operations are prohibited on EDT
    
    GitOrigin-RevId: d41a0465c8cd89eab3bd414f797cbe2d8da4cf6c

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ExecutionPositionNavigator.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ExecutionPositionNavigator.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.xdebugger.impl
 
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.ScrollType
 import com.intellij.openapi.fileEditor.FileEditorManager
@@ -8,11 +9,18 @@ import com.intellij.openapi.fileEditor.OpenFileDescriptor
 import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.project.Project
 import com.intellij.util.asSafely
+import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
+import com.intellij.util.concurrency.annotations.RequiresEdt
+import com.intellij.util.concurrency.annotations.RequiresReadLock
 import com.intellij.xdebugger.XSourcePosition
 import com.intellij.xdebugger.impl.settings.XDebuggerSettingManagerImpl
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 
 
 enum class ExecutionPositionNavigationMode {
@@ -28,6 +36,7 @@ internal class ExecutionPositionNavigator(
 ) {
   private var openedEditor: Editor? = null
   private var openFileDescriptor: OpenFileDescriptor? = null
+  private val descriptorMutex = Mutex()
 
   init {
     coroutineScope.launch {
@@ -40,30 +49,36 @@ internal class ExecutionPositionNavigator(
     }
   }
 
-  fun navigateTo(navigationMode: ExecutionPositionNavigationMode, isActiveSourceKind: Boolean = true) {
+  @RequiresEdt
+  suspend fun navigateTo(navigationMode: ExecutionPositionNavigationMode, isActiveSourceKind: Boolean = true) {
     val effectiveNavigationMode = if (isActiveSourceKind) navigationMode else ExecutionPositionNavigationMode.SCROLL
     val descriptor = getDescriptor()
     navigateTo(descriptor, effectiveNavigationMode)
   }
 
-  private fun invalidate() {
-    synchronized(this) {
+  private suspend fun invalidate() {
+    descriptorMutex.withLock {
       openFileDescriptor?.dispose()
     }
   }
 
-  private fun getDescriptor(): OpenFileDescriptor {
-    synchronized(this) {
+  private suspend fun getDescriptor(): OpenFileDescriptor {
+    descriptorMutex.withLock {
       var descriptor = openFileDescriptor
       if (descriptor == null || descriptor.rangeMarker?.isValid == false) {
         openFileDescriptor?.dispose()
-        descriptor = createOpenFileDescriptor()
+        descriptor = withContext(Dispatchers.Default) {
+          readAction {
+            createOpenFileDescriptor()
+          }
+        }
         openFileDescriptor = descriptor
       }
       return descriptor
     }
   }
 
+  @RequiresEdt
   private fun navigateTo(openFileDescriptor: OpenFileDescriptor, navigationMode: ExecutionPositionNavigationMode) {
     when (navigationMode) {
       ExecutionPositionNavigationMode.OPEN -> {
@@ -79,6 +94,8 @@ internal class ExecutionPositionNavigator(
     }
   }
 
+  @RequiresReadLock
+  @RequiresBackgroundThread
   private fun createOpenFileDescriptor(): OpenFileDescriptor {
     return createPositionNavigatable().apply {
       isUseCurrentWindow = false //see IDEA-125645 and IDEA-63459
@@ -87,6 +104,8 @@ internal class ExecutionPositionNavigator(
     }
   }
 
+  @RequiresReadLock
+  @RequiresBackgroundThread
   private fun createPositionNavigatable(): OpenFileDescriptor {
     val navigatable = sourcePosition.createNavigatable(project)
     return if (navigatable is OpenFileDescriptor) {
