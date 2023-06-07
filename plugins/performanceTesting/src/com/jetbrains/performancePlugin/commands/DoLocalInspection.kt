5864d857427354c888a5a9066632714e89c3b970
roman.ivanitskii <roman.ivanitskii@jetbrains.com>
2023-06-07 16:54:23 +0300

    [performance-tests] ignore daemonFinished events in DoLocalInspection when open telemetry span is not inited
    daemonFinished can be invoked not for current expected file, so that is why we should ignore such cases
    
    GitOrigin-RevId: 39c807e21cf496f4715355ebef5992f294214e28

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/DoLocalInspection.kt
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/DoLocalInspection.kt
@@ -53,6 +53,9 @@ class DoLocalInspection(text: String, line: Int) : PlaybackCommandCoroutineAdapt
     suspendCancellableCoroutine { continuation ->
       busConnection.subscribe(DaemonCodeAnalyzer.DAEMON_EVENT_TOPIC, object : DaemonListener {
         override fun daemonFinished() {
+          if (spanRef == null) {
+            return
+          }
           val daemonCodeAnalyzer = DaemonCodeAnalyzer.getInstance(project) as DaemonCodeAnalyzerImpl
           val document = FileEditorManager.getInstance(project).selectedTextEditor!!.document
           val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document)!!
