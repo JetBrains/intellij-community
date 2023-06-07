fff06fc4ab2faaccf81b1772cb5a55094c73255a
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-06-07 13:59:21 +0200

    Add commands to install/uninstall JBR
    GitOrigin-RevId: 4e20bb8d65b1bc97098e46dacfa1ae26aa30a656

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/BaseCommandProvider.java
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/BaseCommandProvider.java
@@ -86,7 +86,8 @@ public final class BaseCommandProvider implements CommandProvider {
       Map.entry(WaitForFinishedCodeAnalysis.PREFIX, WaitForFinishedCodeAnalysis::new),
       Map.entry(ConditionalMemoryDumpCommand.PREFIX, ConditionalMemoryDumpCommand::new),
       Map.entry(AcceptDecompileNotice.PREFIX, AcceptDecompileNotice::new),
-      Map.entry(DisableCodeVisionCommand.PREFIX, DisableCodeVisionCommand::new)
+      Map.entry(DisableCodeVisionCommand.PREFIX, DisableCodeVisionCommand::new),
+      Map.entry(InstallCustomJBR.PREFIX, InstallCustomJBR::new)
     );
   }
 }
