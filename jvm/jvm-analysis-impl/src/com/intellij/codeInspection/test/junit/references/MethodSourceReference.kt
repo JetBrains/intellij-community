a912fb27baff19d234e1026b91b5cf8beda91ab8
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2023-06-08 11:30:40 +0200

    [junit] `MethodSourceReference` to Kotlin
    GitOrigin-RevId: 961837691cba0f6fe9ba9424b770be830798bc1f

--- jvm/jvm-analysis-impl/src/com/intellij/codeInspection/test/junit/references/MethodSourceReference.kt
+++ jvm/jvm-analysis-impl/src/com/intellij/codeInspection/test/junit/references/MethodSourceReference.kt
@@ -1,26 +1,17 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.codeInspection.test.junit.references;
+package com.intellij.codeInspection.test.junit.references
 
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiLanguageInjectionHost;
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiModifier;
-import com.siyeh.ig.psiutils.TestUtils;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.uast.UClass;
-import org.jetbrains.uast.UMethod;
+import com.intellij.psi.PsiLanguageInjectionHost
+import com.intellij.psi.PsiMethod
+import com.intellij.psi.PsiModifier
+import com.siyeh.ig.psiutils.TestUtils
+import org.jetbrains.uast.UClass
+import org.jetbrains.uast.UMethod
 
-public final class MethodSourceReference extends BaseJunitAnnotationReference {
-  public MethodSourceReference(PsiLanguageInjectionHost element) {
-    super(element);
-  }
-
-  @Override
-  protected boolean hasNoStaticProblem(@NotNull PsiMethod method, @NotNull UClass literalClazz, @Nullable UMethod literalMethod) {
-    boolean isStatic = method.hasModifierProperty(PsiModifier.STATIC);
-    PsiClass psiClass = method.getContainingClass();
-    if (psiClass == null) return false;
-    return method.getParameterList().isEmpty() && (TestUtils.testInstancePerClass(psiClass) != isStatic);
+class MethodSourceReference(element: PsiLanguageInjectionHost) : BaseJunitAnnotationReference(element) {
+  override fun hasNoStaticProblem(method: PsiMethod, literalClazz: UClass, literalMethod: UMethod?): Boolean {
+    val isStatic = method.hasModifierProperty(PsiModifier.STATIC)
+    val psiClass = method.containingClass ?: return false
+    return method.parameterList.isEmpty && TestUtils.testInstancePerClass(psiClass) != isStatic
   }
 }
