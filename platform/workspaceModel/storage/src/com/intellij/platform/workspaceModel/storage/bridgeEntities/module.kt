bd93e089f2d81739051edea79c5258dd04bd96f1
Alex Plate <aleksei.plate@jetbrains.com>
2023-06-08 11:47:10 +0300

    [Workspace Model] Move java-related classes into the same file
    GitOrigin-RevId: 888660b861b5b8bca76fc8b53155aec250c6bc67

--- platform/workspaceModel/storage/src/com/intellij/platform/workspaceModel/storage/bridgeEntities/module.kt
+++ platform/workspaceModel/storage/src/com/intellij/platform/workspaceModel/storage/bridgeEntities/module.kt
@@ -150,52 +150,6 @@ fun MutableEntityStorage.modifyEntity(entity: ModuleGroupPathEntity, modificatio
   ModuleGroupPathEntity.Builder::class.java, entity, modification)
 //endregion
 
-interface JavaModuleSettingsEntity: WorkspaceEntity {
-    val module: ModuleEntity
-
-    val inheritedCompilerOutput: Boolean
-    val excludeOutput: Boolean
-    val compilerOutput: VirtualFileUrl?
-    val compilerOutputForTests: VirtualFileUrl?
-    val languageLevelId: @NonNls String?
-
-  //region generated code
-  @GeneratedCodeApiVersion(1)
-  interface Builder : JavaModuleSettingsEntity, WorkspaceEntity.Builder<JavaModuleSettingsEntity>, ObjBuilder<JavaModuleSettingsEntity> {
-    override var entitySource: EntitySource
-    override var module: ModuleEntity
-    override var inheritedCompilerOutput: Boolean
-    override var excludeOutput: Boolean
-    override var compilerOutput: VirtualFileUrl?
-    override var compilerOutputForTests: VirtualFileUrl?
-    override var languageLevelId: String?
-  }
-
-  companion object : Type<JavaModuleSettingsEntity, Builder>() {
-    @JvmOverloads
-    @JvmStatic
-    @JvmName("create")
-    operator fun invoke(inheritedCompilerOutput: Boolean,
-                        excludeOutput: Boolean,
-                        entitySource: EntitySource,
-                        init: (Builder.() -> Unit)? = null): JavaModuleSettingsEntity {
-      val builder = builder()
-      builder.inheritedCompilerOutput = inheritedCompilerOutput
-      builder.excludeOutput = excludeOutput
-      builder.entitySource = entitySource
-      init?.invoke(builder)
-      return builder
-    }
-  }
-  //endregion
-
-}
-
-//region generated code
-fun MutableEntityStorage.modifyEntity(entity: JavaModuleSettingsEntity,
-                                      modification: JavaModuleSettingsEntity.Builder.() -> Unit) = modifyEntity(
-  JavaModuleSettingsEntity.Builder::class.java, entity, modification)
-//endregion
 
 interface ExternalSystemModuleOptionsEntity: WorkspaceEntity {
     val module: ModuleEntity
