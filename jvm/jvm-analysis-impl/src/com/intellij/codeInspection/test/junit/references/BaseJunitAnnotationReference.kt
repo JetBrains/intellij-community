630e36240e20e9e54571c4f5e2d7be649ec90b11
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2023-06-08 12:37:09 +0200

    [junit] `BaseJunitAnnotationReference` to Kotlin
    GitOrigin-RevId: 254c3478cb7cea6486579483fdeefdfbba45e5ba

--- jvm/jvm-analysis-impl/src/com/intellij/codeInspection/test/junit/references/BaseJunitAnnotationReference.kt
+++ jvm/jvm-analysis-impl/src/com/intellij/codeInspection/test/junit/references/BaseJunitAnnotationReference.kt
@@ -1,178 +1,128 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.codeInspection.test.junit.references;
+package com.intellij.codeInspection.test.junit.references
 
-import com.intellij.codeInsight.lookup.AutoCompletionPolicy;
-import com.intellij.codeInsight.lookup.LookupElementBuilder;
-import com.intellij.codeInspection.reference.PsiMemberReference;
-import com.intellij.lang.jvm.JvmMethod;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.psi.*;
-import com.intellij.psi.search.searches.ClassInheritorsSearch;
-import com.intellij.psi.util.ClassUtil;
-import com.intellij.psi.util.InheritanceUtil;
-import com.intellij.psi.util.PsiUtil;
-import com.intellij.util.IncorrectOperationException;
-import com.intellij.util.SmartList;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.uast.*;
+import com.intellij.codeInsight.lookup.AutoCompletionPolicy
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.codeInspection.reference.PsiMemberReference
+import com.intellij.lang.jvm.JvmMethod
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.*
+import com.intellij.psi.search.searches.ClassInheritorsSearch
+import com.intellij.psi.util.ClassUtil
+import com.intellij.psi.util.InheritanceUtil
+import com.intellij.psi.util.PsiUtil
+import com.intellij.util.SmartList
+import org.jetbrains.uast.*
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static com.intellij.psi.CommonClassNames.JAVA_LANG_OBJECT;
-
-public abstract class BaseJunitAnnotationReference extends PsiReferenceBase<PsiLanguageInjectionHost> implements PsiMemberReference {
-  public BaseJunitAnnotationReference(PsiLanguageInjectionHost element) {
-    super(element, false);
-  }
-
-  @Override
-  public PsiElement bindToElement(@NotNull PsiElement element) throws IncorrectOperationException {
-    if (element instanceof PsiMethod) {
-      return handleElementRename(((PsiMethod)element).getName());
+abstract class BaseJunitAnnotationReference(
+  element: PsiLanguageInjectionHost
+) : PsiReferenceBase<PsiLanguageInjectionHost>(element, false), PsiMemberReference {
+  override fun bindToElement(element: PsiElement): PsiElement {
+    if (element is PsiMethod) {
+      return handleElementRename(element.name)
     }
-    return super.bindToElement(element);
+    return super.bindToElement(element)
   }
 
-  @Override
-  public PsiElement handleElementRename(@NotNull String newElementName) throws IncorrectOperationException {
-    String methodName = getValue();
-    String className = StringUtil.getPackageName(methodName, '#');
-    boolean selfClassReference = className.isEmpty() ||
-                                 ClassUtil
-                                   .findPsiClass(getElement().getManager(), className, null, false, getElement().getResolveScope()) == null;
-    return super.handleElementRename(selfClassReference ? newElementName : className + '#' + newElementName);
+  override fun handleElementRename(newElementName: String): PsiElement {
+    val methodName = value
+    val className = StringUtil.getPackageName(methodName, '#')
+    val selfClassReference = className.isEmpty() || ClassUtil.findPsiClass(
+      element.manager, className, null, false, element.resolveScope
+    ) == null
+    return super.handleElementRename(if (selfClassReference) newElementName else "$className#$newElementName")
   }
 
-  @Override
-  public boolean isReferenceTo(@NotNull PsiElement element) {
-    UExpression myLiteral = UastContextKt.toUElement(getElement(), UExpression.class);
-    if (myLiteral == null) return false;
-    UMethod uMethod = UastContextKt.toUElement(element, UMethod.class);
-    if (uMethod == null) return false;
-    PsiMethod method = uMethod.getJavaPsi();
-    String methodName = (String)myLiteral.evaluate();
-    if (methodName == null) return false;
-    String shortName = StringUtil.getShortName(methodName, '#');
-    if (!shortName.equals(method.getName())) return false;
-    PsiClass methodClass = method.getContainingClass();
-    if (methodClass == null) return false;
+  override fun isReferenceTo(element: PsiElement): Boolean {
+    val myLiteral = getElement().toUElement(UExpression::class.java) ?: return false
+    val uMethod = element.toUElement(UMethod::class.java) ?: return false
+    val method = uMethod.javaPsi
+    val methodName = myLiteral.evaluate() as String? ?: return false
+    val shortName = StringUtil.getShortName(methodName, '#')
+    if (shortName != method.name) return false
+    val methodClass = method.containingClass ?: return false
     if (method.hasModifierProperty(PsiModifier.STATIC)) {
-      String className = StringUtil.getPackageName(methodName, '#');
+      val className = StringUtil.getPackageName(methodName, '#')
       if (!className.isEmpty()) {
-        return className.equals(ClassUtil.getJVMClassName(methodClass));
+        return className == ClassUtil.getJVMClassName(methodClass)
       }
     }
-    PsiClass psiClazz = null;
-    UClass literalClazz = UastUtils.getParentOfType(myLiteral, UClass.class);
-    if (literalClazz != null) {
-      psiClazz = literalClazz.getPsi();
-    }
-    if (psiClazz == null) return false;
-    if (InheritanceUtil.isInheritorOrSelf(psiClazz, methodClass, true) ||
-        InheritanceUtil.isInheritorOrSelf(methodClass, psiClazz, true)) {
-      return true;
-    }
-    return false;
+    val literalClazz = myLiteral.getParentOfType(UClass::class.java) ?: return false
+    val psiClazz = literalClazz.javaPsi
+    return InheritanceUtil.isInheritorOrSelf(psiClazz, methodClass, true) ||
+           InheritanceUtil.isInheritorOrSelf(methodClass, psiClazz, true)
   }
 
-  @Override
-  @Nullable
-  public PsiElement resolve() {
-    UExpression myLiteral = UastContextKt.toUElement(getElement(), UExpression.class);
-    if (myLiteral == null) return null;
-    PsiClass psiClazz = null;
-    UClass literalClazz = UastUtils.getParentOfType(myLiteral, UClass.class);
-    if (literalClazz != null) {
-      psiClazz = literalClazz.getPsi();
-    }
-    UMethod literalMethod = UastUtils.getParentOfType(myLiteral, UMethod.class);
-    if (psiClazz == null) return null;
-    String methodName = (String)myLiteral.evaluate();
-    if (methodName == null) return null;
-    String className = StringUtil.getPackageName(methodName, '#');
+  override fun resolve(): PsiElement? {
+    val myLiteral = element.toUElement(UExpression::class.java) ?: return null
+    val literalClazz = myLiteral.getParentOfType(UClass::class.java) ?: return null
+    val literalMethod = myLiteral.getParentOfType(UMethod::class.java)
+    var methodName = myLiteral.evaluate() as String? ?: return null
+    val className = StringUtil.getPackageName(methodName, '#')
+    var psiClazz = literalClazz.javaPsi
     if (!className.isEmpty()) {
-      PsiClass aClass = ClassUtil.findPsiClass(psiClazz.getManager(), className, null, false, psiClazz.getResolveScope());
+      val aClass = ClassUtil.findPsiClass(psiClazz.manager, className, null, false, psiClazz.resolveScope)
       if (aClass != null) {
-        psiClazz = aClass;
-        methodName = StringUtil.getShortName(methodName, '#');
+        psiClazz = aClass
+        methodName = StringUtil.getShortName(methodName, '#')
       }
     }
-    final String finalMethodName = methodName;
-    PsiMethod[] clazzMethods = psiClazz.findMethodsByName(methodName, true);
-    SmartList<JvmMethod> methodsInner = new SmartList<>();
-    if (clazzMethods.length == 0) {
-      PsiClass[] classes = psiClazz.getInnerClasses();
-      for (PsiClass cl : classes) {
-        JvmMethod[] name = cl.findMethodsByName(finalMethodName);
-        if (name.length > 0) {
-          methodsInner.addAll(Arrays.asList(name));
+    val finalMethodName = methodName
+    var clazzMethods = psiClazz.findMethodsByName(methodName, true)
+    if (clazzMethods.isEmpty()) {
+      val classes = psiClazz.innerClasses
+      val methodsInner = SmartList<JvmMethod>()
+      for (cl in classes) {
+        val name = cl.findMethodsByName(finalMethodName)
+        if (name.isNotEmpty()) {
+          methodsInner.addAll(listOf(*name))
         }
       }
-      if (methodsInner.size() > 0) {
-        clazzMethods = methodsInner.stream()
-          .filter(jm -> jm instanceof PsiMethod)
-          .map(jm -> (PsiMethod)jm)
-          .toArray(size -> new PsiMethod[size]);
+      if (methodsInner.size > 0) {
+        clazzMethods = methodsInner.mapNotNull { method -> method as? PsiMethod? }.toTypedArray()
       }
     }
-    if (clazzMethods.length == 0 && (psiClazz.isInterface() || PsiUtil.isAbstractClass(psiClazz))) {
-      PsiElementResolveResult neededMethod = ClassInheritorsSearch.search(psiClazz, psiClazz.getResolveScope(), false)
-        .mapping(aClazz -> {
-          final PsiMethod[] methods = aClazz.findMethodsByName(finalMethodName, false);
-          return filteredMethod(methods, literalClazz, literalMethod);
-        })
-        .filtering(method -> {
-          return method != null;
-        })
-        .mapping(method -> {
-          return new PsiElementResolveResult(method);
-        })
-        .findFirst();
-      if (neededMethod == null) return null;
-      return neededMethod.getElement();
+    if (clazzMethods.isEmpty() && (psiClazz.isInterface || PsiUtil.isAbstractClass(psiClazz))) {
+      return ClassInheritorsSearch.search(psiClazz, psiClazz.resolveScope, false)
+               .mapNotNull { aClazz ->
+                 val methods = aClazz.findMethodsByName(finalMethodName, false)
+                 filteredMethod(methods, literalClazz, literalMethod)
+               }
+               .map { method -> PsiElementResolveResult(method) }
+               .firstOrNull()?.element ?: return null
     }
-    return filteredMethod(clazzMethods, literalClazz, literalMethod);
+    return filteredMethod(clazzMethods, literalClazz, literalMethod)
   }
 
-  @Nullable
-  private PsiMethod filteredMethod(PsiMethod[] clazzMethods, UClass uClass, UMethod uMethod) {
-    return Arrays.stream(clazzMethods)
-      .filter(method -> hasNoStaticProblem(method, uClass, uMethod))
-      .findFirst()
-      .orElse(clazzMethods.length == 0 ? null : clazzMethods[0]);
+  private fun filteredMethod(clazzMethods: Array<PsiMethod>, uClass: UClass, uMethod: UMethod?): PsiMethod? {
+    return clazzMethods.firstOrNull { method ->
+      hasNoStaticProblem(method, uClass, uMethod)
+    } ?: if (clazzMethods.isEmpty()) null else clazzMethods.first()
   }
 
-  @Override
-  public Object @NotNull [] getVariants() {
-    UExpression myLiteral = UastContextKt.toUElement(getElement(), UExpression.class);
-    final List<Object> list = new ArrayList<>();
-    if (myLiteral == null) return list.toArray();
-    final UClass topLevelClass = UastUtils.getParentOfType(myLiteral, UClass.class);
-    if (topLevelClass != null) {
-      final UMethod current = UastUtils.getParentOfType(myLiteral, UMethod.class);
-      PsiClass psiTopLevelClass = topLevelClass.getJavaPsi();
-      final PsiMethod[] methods = psiTopLevelClass.getAllMethods();
-      for (PsiMethod method : methods) {
-        PsiClass aClass = method.getContainingClass();
-        if (aClass == null) continue;
-        if (JAVA_LANG_OBJECT.equals(aClass.getQualifiedName())) continue;
-        if (current != null && method.getName().equals(current.getName())) continue;
-        if (current != null && !hasNoStaticProblem(method, topLevelClass, current)) continue;
-        final LookupElementBuilder builder = LookupElementBuilder.create(method);
-        list.add(builder.withAutoCompletionPolicy(AutoCompletionPolicy.SETTINGS_DEPENDENT));
-      }
+  override fun getVariants(): Array<Any> {
+    val myLiteral = element.toUElement(UExpression::class.java) ?: return emptyArray()
+    val topLevelClass = myLiteral.getParentOfType(UClass::class.java) ?: return emptyArray()
+    val current = myLiteral.getParentOfType(UMethod::class.java)
+    val psiTopLevelClass = topLevelClass.javaPsi
+    val methods = psiTopLevelClass.allMethods
+    val list = mutableListOf<Any>()
+    for (method in methods) {
+      val aClass = method.containingClass ?: continue
+      if (CommonClassNames.JAVA_LANG_OBJECT == aClass.qualifiedName) continue
+      if (current != null && method.name == current.name) continue
+      if (current != null && !hasNoStaticProblem(method, topLevelClass, current)) continue
+      val builder = LookupElementBuilder.create(method)
+      list.add(builder.withAutoCompletionPolicy(AutoCompletionPolicy.SETTINGS_DEPENDENT))
     }
-    return list.toArray();
+    return list.toTypedArray()
   }
 
   /**
    * @param method method referenced from within JUnit annotation
-   * @param uClass the class where the annotation is located
-   * @param uMethod the JUnit annotated method, is null in case the annotation is class-level
+   * @param literalClazz the class where the annotation is located
+   * @param literalMethod the JUnit annotated method, is null in case the annotation is class-level
    * @return true in case static check is successful
    */
-  abstract protected boolean hasNoStaticProblem(@NotNull PsiMethod method, @NotNull UClass uClass, @Nullable UMethod uMethod);
+  protected abstract fun hasNoStaticProblem(method: PsiMethod, literalClazz: UClass, literalMethod: UMethod?): Boolean
 }
