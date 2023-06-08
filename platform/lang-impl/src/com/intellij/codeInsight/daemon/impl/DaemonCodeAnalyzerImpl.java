f731e7b30d4efdc9b342506121ff469f6345ebd0
Andrei Efanov <andrei.efanov@jetbrains.com>
2023-06-08 11:52:38 +0200

    Revert "do not call test-only methods from production to fix IDEA-321374 'ExternalToolPass.getInfos' accesses '@TestOnly' method in production"
    This reverts commit 39c093bdacfc079fe33ef53f16bc4f115affa825.
    
    (cherry picked from commit eedc43896c2e525618a37beba9f528d34287ce4d)
    
    IJ-MR-108308
    
    GitOrigin-RevId: 8fa7544e346f3441c0e8c32039f8659ee9a5e9ef

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/DaemonCodeAnalyzerImpl.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/DaemonCodeAnalyzerImpl.java
@@ -443,8 +443,6 @@ public final class DaemonCodeAnalyzerImpl extends DaemonCodeAnalyzerEx implement
           if (savedException != null) throw savedException;
           return progress.isRunning();
         });
-        // it will wait for the async spawned processes
-        ((HighlightingSessionImpl)session).myExternalPassFuture.get(1, TimeUnit.MINUTES);
         if (progress.isRunning() && !progress.isCanceled()) {
           throw new RuntimeException("Highlighting still running after " +
              (System.currentTimeMillis() - start) / 1000 + " seconds. Still submitted passes: " +
--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/ExternalAnnotatorManager.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/ExternalAnnotatorManager.java
@@ -7,10 +7,15 @@ import com.intellij.openapi.components.Service;
 import com.intellij.util.ui.update.MergingUpdateQueue;
 import com.intellij.util.ui.update.Update;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.TestOnly;
 
-@Service
-final class ExternalAnnotatorManager implements Disposable {
-  static ExternalAnnotatorManager getInstance() {
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Service(Service.Level.APP)
+public final class ExternalAnnotatorManager implements Disposable {
+  public static ExternalAnnotatorManager getInstance() {
     return ApplicationManager.getApplication().getService(ExternalAnnotatorManager.class);
   }
 
@@ -22,7 +27,12 @@ final class ExternalAnnotatorManager implements Disposable {
   public void dispose() {
   }
 
-  void queue(@NotNull Update update) {
+  public void queue(@NotNull Update update) {
     myExternalActivitiesQueue.queue(update);
+  }
+
+  @TestOnly
+  public void waitForAllExecuted(long timeout, @NotNull TimeUnit unit) throws ExecutionException, InterruptedException, TimeoutException {
+    myExternalActivitiesQueue.waitForAllExecuted(timeout, unit);
   }
 }
