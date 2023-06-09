5c1360b2d49338993a0aecd32be1c07059c71a5b
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2023-06-09 12:50:42 +0200

    [gradle] limit conventions test with v8.2 add explicit tests for JavaExtension IDEA-320266
    GitOrigin-RevId: 43645e4b9580e7b8247935985593f3cdb3f700af

--- plugins/gradle/java/testSources/dsl/GradleJavaExtensionsTest.kt
+++ plugins/gradle/java/testSources/dsl/GradleJavaExtensionsTest.kt
@@ -3,35 +3,35 @@ package org.jetbrains.plugins.gradle.dsl
 
 import com.intellij.psi.PsiMethod
 import org.gradle.util.GradleVersion
-import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.GRADLE_API_JAVA_PLUGIN_CONVENTION
+import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.GRADLE_API_JAVA_PLUGIN_EXTENSION
 import org.jetbrains.plugins.gradle.testFramework.GradleCodeInsightTestCase
 import org.jetbrains.plugins.gradle.testFramework.annotations.AllGradleVersionsSource
 import org.jetbrains.plugins.gradle.tooling.annotation.TargetVersions
 import org.junit.jupiter.params.ParameterizedTest
 
-class GradleConventionsTest : GradleCodeInsightTestCase() {
+class GradleJavaExtensionsTest : GradleCodeInsightTestCase() {
 
   @ParameterizedTest
   @AllGradleVersionsSource(DECORATORS, """
-    "<caret>docsDir",
-    "project.<caret>docsDir"
+    java."<caret>docsDir",
+    "project.java.<caret>docsDir"
   """)
-  @TargetVersions("<8.2")
+  @TargetVersions("7.1+")
   fun `test property read`(gradleVersion: GradleVersion, decorator: String, expression: String) {
     testJavaProject(gradleVersion) {
       testBuildscript(decorator, expression) {
-        methodTest(resolveTest(PsiMethod::class.java), "getDocsDir", GRADLE_API_JAVA_PLUGIN_CONVENTION)
+        methodTest(resolveTest(PsiMethod::class.java), "getDocsDir", GRADLE_API_JAVA_PLUGIN_EXTENSION)
       }
     }
   }
 
   @ParameterizedTest
   @AllGradleVersionsSource(DECORATORS)
-  @TargetVersions("<8.2")
+  @TargetVersions("7.1+")
   fun `test property write`(gradleVersion: GradleVersion, decorator: String) {
     testJavaProject(gradleVersion) {
-      testBuildscript(decorator, "<caret>sourceCompatibility = 42") {
-        methodTest(resolveTest(PsiMethod::class.java), "setSourceCompatibility", GRADLE_API_JAVA_PLUGIN_CONVENTION)
+      testBuildscript(decorator, "java.<caret>sourceCompatibility = 42") {
+        methodTest(resolveTest(PsiMethod::class.java), "setSourceCompatibility", GRADLE_API_JAVA_PLUGIN_EXTENSION)
       }
     }
   }
@@ -39,11 +39,11 @@ class GradleConventionsTest : GradleCodeInsightTestCase() {
   // this test is wrong and exists only to preserve current behaviour and to fail when behaviour changes
   @ParameterizedTest
   @AllGradleVersionsSource(DECORATORS)
-  @TargetVersions("<8.2")
+  @TargetVersions("7.1+")
   fun `test setter method`(gradleVersion: GradleVersion, decorator: String) {
     testJavaProject(gradleVersion) {
-      testBuildscript(decorator, "<caret>targetCompatibility('1.8')") {
-        setterMethodTest("targetCompatibility", "setTargetCompatibility", GRADLE_API_JAVA_PLUGIN_CONVENTION)
+      testBuildscript(decorator, "java.<caret>targetCompatibility('1.8')") {
+        setterMethodTest("targetCompatibility", "setTargetCompatibility", GRADLE_API_JAVA_PLUGIN_EXTENSION)
         //// the correct test is below:
         //val call = elementUnderCaret(GrMethodCall::class.java)
         //val result = call.advancedResolve()
