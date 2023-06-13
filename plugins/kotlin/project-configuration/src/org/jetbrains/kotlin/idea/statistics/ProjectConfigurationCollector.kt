585c1da084726f7fba58d93b46a43a267c347b4a
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2023-06-13 10:14:22 +0000

    [kotlin] Added collector to track J2K conversion
    The new collector tracks the steps of adding Kotlin to Java projects to allow insights into this process.
    
    ^KTIJ-24940 fixed
    
    Merge-request: IJ-MR-105072
    Merged-by: Frederik Haselmeier <Frederik.Haselmeier@jetbrains.com>
    
    GitOrigin-RevId: b90ed1c60b7c362475a7171ee9b33e99c5c09068

--- plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/statistics/ProjectConfigurationCollector.kt
+++ plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/statistics/ProjectConfigurationCollector.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.statistics
 
@@ -18,6 +18,7 @@ import org.jetbrains.kotlin.idea.configuration.getPlatform
 import org.jetbrains.kotlin.idea.facet.KotlinFacet
 import org.jetbrains.kotlin.idea.facet.KotlinFacetType
 import org.jetbrains.kotlin.konan.target.KonanTarget
+
 
 class ProjectConfigurationCollector : ProjectUsagesCollector() {
     override fun getGroup() = GROUP
