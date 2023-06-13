8186b15e527939590c1c2f597e0feff20866d612
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-06-13 15:20:40 +0200

    deprecate `com.intellij.util.Futures`
    GitOrigin-RevId: 934405c871889503ac94387463f456617e99be89

--- platform/util-ex/src/com/intellij/util/Futures.kt
+++ platform/util-ex/src/com/intellij/util/Futures.kt
@@ -1,5 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package com.intellij.util
 
@@ -27,7 +26,9 @@ import java.util.function.Function
  *
  * @see "com.intellij.collaboration.async.CompletableFutureUtil"
  */
+@ApiStatus.Internal
 @ApiStatus.Experimental
+@Deprecated("Use coroutines instead.")
 object Futures {
 
   /* ------------------------------------------------------------------------------------------- */
