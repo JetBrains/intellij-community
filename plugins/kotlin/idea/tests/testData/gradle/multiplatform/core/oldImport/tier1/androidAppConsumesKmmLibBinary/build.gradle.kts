aa0681dc6b5decdfe850efa8758ed57a5fc452bf
Pavel Kargashinsky <pavel.kargashinsky@jetbrains.com>
2023-06-13 17:04:31 +0200

    [MPP][Tests] Copy Tier1 default TestData to oldImport
    (cherry picked from commit a52d3eecbb8ee496b0ae770a1dcd33ab50f03aac)
    
    GitOrigin-RevId: 1f988e4d721a21c640f4a44dc5bc591088c32853

---
+++ plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/oldImport/tier1/androidAppConsumesKmmLibBinary/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    kotlin("multiplatform") apply false
+    id("com.android.library")
+}
+
+{{default_android_block}}
+
+allprojects {
+    repositories {
+        { { kts_kotlin_plugin_repositories } }
+        maven("$rootDir/repo")
+    }
+}
---
+++ plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/oldImport/tier1/pureJavaConsumesKmmLibBinary/javaOnly/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("java")
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin.mpp.tests:kmmLib:1.0")
+}
