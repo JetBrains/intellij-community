b53d964f725f098a7046842181e6b550b7d7d465
Alex Plate <aleksei.plate@jetbrains.com>
2023-06-14 16:43:52 +0300

    [Workspace Model] [IDEA-320676] Fix incorrect package
    GitOrigin-RevId: c475ccf7353912277b9566b583c1bd03d967845f

--- platform/workspace/jps/gen/com/intellij/java/workspace/entities/JavaModuleSettingsEntityImpl.kt
+++ platform/workspace/jps/gen/com/intellij/java/workspace/entities/JavaModuleSettingsEntityImpl.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.workspace.storage.bridgeEntities
+package com.intellij.java.workspace.entities
 
 import com.intellij.java.workspace.entities.JavaModuleSettingsEntity
 import com.intellij.platform.workspace.jps.entities.ModuleEntity
