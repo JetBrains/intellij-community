13fd7f6caf89450cbbfa40b869792ae26e1114ee
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2023-06-14 15:54:45 +0200

    [core] IDEA-321555: reduce VFS self-checks on startup
    - since we have regular VFSHealthCheck, we could check less on startup
    - check only up to 32 fileId on startup
    - add test
    
    GitOrigin-RevId: a67ef21c114b66ecd1a8ac14fcef5fa845bc9ff8

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/PersistentFSConnector.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/PersistentFSConnector.java
@@ -4,6 +4,7 @@ package com.intellij.openapi.vfs.newvfs.persistent;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.IntRef;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.newvfs.persistent.dev.OffsetBasedNonStrictStringsEnumerator;
 import com.intellij.openapi.vfs.newvfs.persistent.dev.blobstorage.LargeSizeStreamlinedBlobStorage;
@@ -45,13 +46,6 @@ import static java.nio.charset.StandardCharsets.UTF_8;
  */
 final class PersistentFSConnector {
   private static final Logger LOG = Logger.getInstance(PersistentFSConnector.class);
-
-  /**
-   * Run self-checks on a limited number of files during VFS init.
-   * Self-check all the files could significantly slow down VFS initialization, but even a small number of checks
-   * could catch VFS corruption, if any.
-   */
-  private static final int FILES_TO_SELF_CHECK_ON_INIT = SystemProperties.getIntProperty("vfs.files-to-self-check-on-init", 1024);
 
   private static final int MAX_INITIALIZATION_ATTEMPTS = 10;
 
@@ -399,17 +393,17 @@ final class PersistentFSConnector {
       LOG.info("VFS uses regular attributes storage");
       return new AttributesStorageOld(
         /*bulk attribute support: */false,
-        FSRecordsImpl.INLINE_ATTRIBUTES,
-        new Storage(attributesFile, PersistentFSConnection.REASONABLY_SMALL) {
-          @Override
-          protected AbstractRecordsTable createRecordsTable(@NotNull StorageLockContext context,
-                                                            @NotNull Path recordsFile)
-            throws IOException {
-            return FSRecordsImpl.INLINE_ATTRIBUTES && FSRecordsImpl.USE_SMALL_ATTR_TABLE
-                   ? new CompactRecordsTable(recordsFile, context, false)
-                   : super.createRecordsTable(context, recordsFile);
-          }
-        });
+                                    FSRecordsImpl.INLINE_ATTRIBUTES,
+                                    new Storage(attributesFile, PersistentFSConnection.REASONABLY_SMALL) {
+                                      @Override
+                                      protected AbstractRecordsTable createRecordsTable(@NotNull StorageLockContext context,
+                                                                                        @NotNull Path recordsFile)
+                                        throws IOException {
+                                        return FSRecordsImpl.INLINE_ATTRIBUTES && FSRecordsImpl.USE_SMALL_ATTR_TABLE
+                                               ? new CompactRecordsTable(recordsFile, context, false)
+                                               : super.createRecordsTable(context, recordsFile);
+                                      }
+                                    });
     }
   }
 
@@ -454,6 +448,16 @@ final class PersistentFSConnector {
     throws IOException {
     final long startedAtNs = System.nanoTime();
     try {
+      //We wantRun self-checks on a limited number of files during VFS init.Self-check all the files could significantly slow
+      // down VFS initialization, but even a small number of checks could catch VFS corruption, if any.
+
+      //VFS errors early on startup cause terrifying UX error messages -- it is much better to catch VFS
+      // corruption early on and rebuild VFS from 0. But we also don't want to always check each VFS file
+      // on startup, since it delays regular startup (i.e. without corruptions) quite a lot.
+      // A tradeoff between those two goals: check only fileId=1, 2, 4, 8... log(maxID) total checks,
+      // i.e. always < 32 checks, given fileId is int.
+      IntRef nextPowFileIdToCheck = new IntRef(0);
+
       records.processAllRecords((fileId, nameId, flags, parentId, attributeRecordId, contentId, corrupted) -> {
         if (hasDeletedFlag(flags)) {
           freeFileIds.add(fileId);
@@ -464,7 +468,8 @@ final class PersistentFSConnector {
           invertedNameIndexToFill.updateDataInner(fileId, nameId);
         }
 
-        if (fileId <= FILES_TO_SELF_CHECK_ON_INIT) {
+        if (fileId >> nextPowFileIdToCheck.get() == 1) {
+          nextPowFileIdToCheck.inc();
           //Try to resolve few nameId/contentId against apt enumerators -- which is quite likely fails
           //if enumerators' files are corrupted anyhow, hence serves as a self-check heuristic:
 
@@ -485,7 +490,7 @@ final class PersistentFSConnector {
             catch (IOException e) {
               throw new UncheckedIOException("file[#" + fileId + "].nameId(=" + nameId + ") failed resolution in namesEnumerator", e);
             }
-          }                                           
+          }
 
           if (contentHashesEnumerator != null
               && contentId != DataEnumeratorEx.NULL_ID) {
