e47980dbd9af1dd3d3be46e751ab15aa3d28ee7a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-06-15 13:22:27 +0200

    refactor the LanguageModelComputationTask to use coroutines for better efficiency. Also, optimise the dictionary by using Object2IntOpenHashMap library to reduce memory usage
    GitOrigin-RevId: aa15f684ca219d09bf7464260113ef9428f6ebad

--- plugins/search-everywhere-ml/typos/src/com/intellij/searchEverywhereMl/typos/SearchEverywhereTypoFixing.kt
+++ plugins/search-everywhere-ml/typos/src/com/intellij/searchEverywhereMl/typos/SearchEverywhereTypoFixing.kt
@@ -5,7 +5,8 @@ import com.intellij.searchEverywhereMl.common.SearchEverywhereMlExperiment
 import com.intellij.searchEverywhereMl.common.SearchEverywhereMlExperiment.ExperimentType.ENABLE_TYPOS
 import com.intellij.searchEverywhereMl.common.SearchEverywhereTabWithMlRanking
 
-
 internal val isTypoFixingEnabled: Boolean
-  get() = AdvancedSettings.getBoolean("searcheverywhere.ml.typos.enable")
-          && SearchEverywhereMlExperiment().getExperimentForTab(SearchEverywhereTabWithMlRanking.ACTION) == ENABLE_TYPOS
+  get() {
+    return (AdvancedSettings.getBoolean("searcheverywhere.ml.typos.enable")
+            && SearchEverywhereMlExperiment().getExperimentForTab(SearchEverywhereTabWithMlRanking.ACTION) == ENABLE_TYPOS)
+  }
--- plugins/search-everywhere-ml/typos/src/com/intellij/searchEverywhereMl/typos/models/ActionsLanguageModel.kt
+++ plugins/search-everywhere-ml/typos/src/com/intellij/searchEverywhereMl/typos/models/ActionsLanguageModel.kt
@@ -5,70 +5,86 @@ import com.intellij.ide.actions.ShowSettingsUtilImpl
 import com.intellij.openapi.actionSystem.ActionGroup
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.impl.ActionManagerImpl
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.components.serviceIfCreated
 import com.intellij.openapi.options.SearchableConfigurable
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.startup.ProjectActivity
-import com.intellij.openapi.wm.IdeFocusManager
+import com.intellij.openapi.util.NlsActions
+import com.intellij.openapi.util.NlsContexts
 import com.intellij.searchEverywhereMl.typos.SearchEverywhereStringToken
 import com.intellij.searchEverywhereMl.typos.isTypoFixingEnabled
 import com.intellij.searchEverywhereMl.typos.splitText
+import com.intellij.serviceContainer.NonInjectable
 import com.intellij.spellchecker.dictionary.Dictionary
 import com.intellij.spellchecker.dictionary.RuntimeDictionaryProvider
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 @Service(Service.Level.APP)
-internal class ActionsLanguageModel(private val actionDictionary: ActionDictionary = ActionDictionaryImpl()) : Dictionary by actionDictionary,
-                                                                                                               FrequencyMetadata by actionDictionary {
+internal class ActionsLanguageModel @NonInjectable constructor(private val actionDictionary: ActionDictionary,
+                                                               coroutineScope: CoroutineScope) :
+  Dictionary by actionDictionary, FrequencyMetadata by actionDictionary {
+
+  @Suppress("unused")
+  constructor(coroutineScope: CoroutineScope) : this(actionDictionary = ActionDictionaryImpl(), coroutineScope)
 
   companion object {
     /**
      * Returns null if the application is not in an internal mode
      */
     fun getInstance(): ActionsLanguageModel? {
-      if (!isTypoFixingEnabled) return null
+      if (!isTypoFixingEnabled) {
+        return null
+      }
       return service<ActionsLanguageModel>()
     }
   }
 
-  private val languageModelComputationTask = LanguageModelComputationTask()  // TODO: Change to a coroutine
+  private val languageModelComputationJob: Job
 
   init {
-    ApplicationManager.getApplication().executeOnPooledThread(languageModelComputationTask)
+    languageModelComputationJob = coroutineScope.launch {
+      asyncInit()
+    }
   }
 
   /**
    * Returns true if all words from actions and options have been processed and added to the dictionary
    */
   val isComputed: Boolean
-    get() = languageModelComputationTask.isFinished
+    get() = languageModelComputationJob.isCompleted
 
-  private fun getWordsFromActions() = (ActionManager.getInstance() as ActionManagerImpl)
-    .let { actionManager ->
-      actionManager.actionIds
-        .asSequence()
-        .mapNotNull { actionManager.getActionOrStub(it) }
-    }.filterNot { it is ActionGroup && !it.isSearchable }
-    .mapNotNull { it.templateText }
+  private fun getWordsFromActions(): Sequence<@NlsActions.ActionText String> {
+    return (ActionManager.getInstance() as ActionManagerImpl)
+      .let { actionManager ->
+        actionManager.actionIds
+          .asSequence()
+          .mapNotNull { actionManager.getActionOrStub(it) }
+      }.filterNot { it is ActionGroup && !it.isSearchable }
+      .mapNotNull { it.templateText }
+  }
 
 
-  private fun getWordsFromSettings() = ShowSettingsUtilImpl.getConfigurables(null, true, false)
-    .asSequence()
-    .filterIsInstance<SearchableConfigurable>()
-    .map { it.displayName }
+  private fun getWordsFromSettings(): Sequence<@NlsContexts.ConfigurableName String> {
+    return ShowSettingsUtilImpl.getConfigurables(null, true, false)
+      .asSequence()
+      .filterIsInstance<SearchableConfigurable>()
+      .map { it.displayName }
+  }
 
   internal interface ActionDictionary : Dictionary, FrequencyMetadata {
     fun addWord(word: String)
   }
 
   private class ActionDictionaryImpl : ActionDictionary {
-    private val words: MutableMap<String, Int> = HashMap(1500)
+    private val words = Object2IntOpenHashMap<String>(1500)
 
     override fun addWord(word: String) {
-      words[word] = (words[word] ?: 0) + 1
+      words.addTo(word, 1)
     }
 
     override fun getName(): String = "Actions Dictionary"
@@ -82,43 +98,34 @@ internal class ActionsLanguageModel(private val actionDictionary: ActionDictiona
     override val maxFrequency: Int
       get() = words.maxOf { it.value }
 
-    override fun getFrequency(word: String): Int? = words[word]
+    override fun getFrequency(word: String): Int? = if (words.containsKey(word)) words.getInt(word) else null
   }
 
-  @Suppress("unused")  // Registered in the plugin's XML file
-  private class ModelComputationStarter : ProjectActivity {
-    override suspend fun execute(project: Project) {
-      getInstance()
-    }
-  }
-
-  private inner class LanguageModelComputationTask : Runnable {
-    var isFinished: Boolean = false
-
-    override fun run() {
-      (getWordsFromActions() + getWordsFromSettings())
-        .flatMap {
-          splitText(it)
-            .filter { token -> token is SearchEverywhereStringToken.Word }
-            .map { token -> token.value }
-        }
-        .map { it.filter { c -> c.isLetterOrDigit() } }
-        .filterNot { it.isEmpty() || it.isSingleCharacter() }
-        .map { it.lowercase() }
-        .forEach(actionDictionary::addWord)
-
-      isFinished = true
-    }
+  private fun asyncInit() {
+    (getWordsFromActions() + getWordsFromSettings())
+      .flatMap {
+        splitText(it)
+          .filter { token -> token is SearchEverywhereStringToken.Word }
+          .map { token -> token.value }
+      }
+      .map { it.filter { c -> c.isLetterOrDigit() } }
+      .filterNot { it.isEmpty() || it.isSingleCharacter() }
+      .map { it.lowercase() }
+      .forEach(actionDictionary::addWord)
   }
 }
+
+private class ModelComputationStarter : ProjectActivity {
+  override suspend fun execute(project: Project) {
+    ActionsLanguageModel.getInstance()
+  }
+}
+
 
 private class RuntimeActionsDictionaryProvider : RuntimeDictionaryProvider {
   override fun getDictionaries(): Array<Dictionary> {
     return serviceIfCreated<ActionsLanguageModel>()?.let { arrayOf(it) } ?: arrayOf()
   }
 }
-
-private fun getProject(): Project = IdeFocusManager.getGlobalInstance().lastFocusedFrame?.project
-                                    ?: ProjectManager.getInstance().defaultProject
 
 private fun String.isSingleCharacter(): Boolean = this.length == 1
