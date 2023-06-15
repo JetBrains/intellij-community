7b1cdaa86a8c0f62d49246f31d5797fdafaf334f
Filipp Vakhitov <filipp.vakhitov@jetbrains.com>
2023-06-15 17:58:41 +0300

    IDEA-210640 Remember color scheme used with particular LaF
    GitOrigin-RevId: 9d6dd64d3cdee7e88585cb90be3b571c82f0f950

--- platform/platform-api/src/com/intellij/ide/ui/LafManager.java
+++ platform/platform-api/src/com/intellij/ide/ui/LafManager.java
@@ -57,6 +57,7 @@ public abstract class LafManager {
 
   public abstract void setPreferredLightLaf(@NotNull UIManager.LookAndFeelInfo value);
 
+  @ApiStatus.Internal
   public abstract @Nullable EditorColorsScheme getPreviousSchemeForLaf(@NotNull UIManager.LookAndFeelInfo lookAndFeelInfo);
 
   @ApiStatus.Internal
