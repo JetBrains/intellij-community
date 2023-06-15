404d1299917f70dd0b30528a60af8b4a14e7e9ec
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2023-06-15 19:10:15 +0200

    IDEA-322594 added: fast Java compatibility build failure and quick fix
    GitOrigin-RevId: ce6e27b13e22b3674097c76f1f6eb1a48d9d0a90

---
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/issue/UnsupportedGradleJvmIssueChecker.kt
@@ -0,0 +1,95 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.gradle.issue
+
+import com.intellij.build.issue.BuildIssue
+import com.intellij.openapi.externalSystem.service.execution.ExternalSystemJdkUtil
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.NlsContexts
+import com.intellij.pom.Navigatable
+import com.intellij.util.PlatformUtils
+import com.intellij.util.lang.JavaVersion
+import org.gradle.tooling.model.build.BuildEnvironment
+import org.gradle.util.GradleVersion
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.plugins.gradle.issue.quickfix.GradleSettingsQuickFix
+import org.jetbrains.plugins.gradle.jvmcompat.GradleJvmSupportMatrix
+import org.jetbrains.plugins.gradle.util.GradleBundle
+import org.jetbrains.plugins.gradle.util.getOldestSupportedJavaVersion
+import org.jetbrains.plugins.gradle.util.suggestOldestCompatibleJavaVersion
+
+class UnsupportedGradleJvmIssueChecker : GradleIssueChecker {
+
+  override fun check(issueData: GradleIssueData): BuildIssue? {
+    val buildEnvironment = issueData.buildEnvironment ?: return null
+    val gradleVersion = GradleVersion.version(buildEnvironment.gradle.gradleVersion)
+    if (!Util.isSupportedGradleJvm(buildEnvironment)) {
+      val title = GradleBundle.message("gradle.build.issue.gradle.jvm.unsupported.title")
+      val description = DescriptionBuilder()
+      val oldestSupportedJavaVersion = getOldestSupportedJavaVersion()
+      description.addDescription(
+        GradleBundle.message("gradle.build.issue.gradle.jvm.unsupported.description", oldestSupportedJavaVersion.feature)
+      )
+      val gradleSettingsFix = GradleSettingsQuickFix(
+        issueData.projectPath, true,
+        GradleSettingsQuickFix.GradleJvmChangeDetector,
+        GradleBundle.message("gradle.settings.text.jvm.path")
+      )
+      val isAndroidStudio = "AndroidStudio" == PlatformUtils.getPlatformPrefix()
+      val oldestCompatibleJavaVersion = suggestOldestCompatibleJavaVersion(gradleVersion)
+      if (!isAndroidStudio && oldestCompatibleJavaVersion != null) {
+        description.addQuickFixPrompt(
+          GradleBundle.message("gradle.build.quick.fix.gradle.jvm", oldestCompatibleJavaVersion, gradleSettingsFix.id)
+        )
+      }
+      return object : BuildIssue {
+        override val title = title
+        override val description = description.toString()
+        override val quickFixes = listOf(gradleSettingsFix)
+        override fun getNavigatable(project: Project): Navigatable? = null
+      }
+    }
+    return null
+  }
+
+  object Util {
+    /**
+     * Checks that Java which used for Gradle execution is supported by IDE
+     */
+    @JvmStatic
+    @ApiStatus.Internal
+    fun isSupportedGradleJvm(buildEnvironment: BuildEnvironment): Boolean {
+      val javaHome = buildEnvironment.java.javaHome
+      val javaSdkType = ExternalSystemJdkUtil.getJavaSdkType()
+      val javaVersionString = javaSdkType.getVersionString(javaHome.path) ?: return false
+      val javaVersion = JavaVersion.tryParse(javaVersionString) ?: return false
+      return GradleJvmSupportMatrix.getInstance().isSupportedByIdea(javaVersion)
+    }
+  }
+
+  private class DescriptionBuilder {
+
+    private val descriptions = ArrayList<String>()
+    private val quickFixPrompts = ArrayList<String>()
+
+    fun addDescription(description: @NlsContexts.DetailedDescription String) {
+      descriptions.add(description)
+    }
+
+    fun addQuickFixPrompt(prompt: @NlsContexts.DetailedDescription String) {
+      quickFixPrompts.add(prompt)
+    }
+
+    override fun toString(): String {
+      return buildString {
+        append(descriptions.joinToString("\n"))
+          .append("\n")
+        if (quickFixPrompts.isNotEmpty()) {
+          append(GradleBundle.message("gradle.build.quick.fix.title"))
+            .append("\n")
+          append(quickFixPrompts.joinToString("\n") { "- $it" })
+            .append("\n")
+        }
+      }
+    }
+  }
+}
