090459357a3e2efd9c8a11b2cb8ea478855c6100
dima.avdeev <dima.avdeev@jetbrains.com>
2023-06-16 20:06:14 +0000

    [Kotlin Wizards] KT-59347 Compose Multiplatform, remove redundant platforms
    (cherry picked from commit 92806d78797ff93b8d9bfb81c8807841e8565b8f)
    
    GitOrigin-RevId: 4d7f526678c433f4136e12e60db574a37e5ee2ac

--- plugins/kotlin/project-wizard/compose/testData/etalons/desktop/src/main/kotlin/Main.kt
+++ plugins/kotlin/project-wizard/compose/testData/etalons/desktop/src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
