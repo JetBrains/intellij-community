d99fa5bb58273e49600ff264931be300a814edac
aleksandrina-streltsova <alexandrina.streltsova@jetbrains.com>
2023-06-16 11:59:47 +0300

    [kotlin k2] Completion: fix insertion of a suggestion when special key is pressed
    ^KTIJ-25896 Fixed
    
    GitOrigin-RevId: d8ea8815193b88728376e3a45c6fc5bf3dc018cc

--- plugins/kotlin/completion/impl-shared/src/org/jetbrains/kotlin/idea/completion/implCommon/handlers/WithExpressionPrefixInsertHandler.kt
+++ plugins/kotlin/completion/impl-shared/src/org/jetbrains/kotlin/idea/completion/implCommon/handlers/WithExpressionPrefixInsertHandler.kt
@@ -1,6 +1,6 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
-package org.jetbrains.kotlin.idea.completion.handlers
+package org.jetbrains.kotlin.idea.completion.implCommon.handlers
 
 import com.intellij.codeInsight.completion.InsertHandler
 import com.intellij.codeInsight.completion.InsertionContext
