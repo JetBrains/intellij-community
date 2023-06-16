8a0f51291bed6b2df4d7e94cf21b188b33908496
Ivan Semenov <ivan.semenov@jetbrains.com>
2023-06-16 13:55:45 +0200

    [gitlab] show discussions count in MR changes tree
    IDEA-320514 Fixed
    
    (cherry picked from commit b618446d4b69a6787acca5286408228ae4754215)
    
    GitOrigin-RevId: accd902010c19c294de987e23388a36baa007254

--- plugins/gitlab/src/org/jetbrains/plugins/gitlab/mergerequest/ui/details/model/GitLabMergeRequestChangesViewModel.kt
+++ plugins/gitlab/src/org/jetbrains/plugins/gitlab/mergerequest/ui/details/model/GitLabMergeRequestChangesViewModel.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gitlab.mergerequest.ui.details.model
 
+import com.intellij.collaboration.async.launchNow
 import com.intellij.collaboration.async.modelFlow
 import com.intellij.collaboration.ui.codereview.details.model.CodeReviewChangesViewModel
 import com.intellij.collaboration.ui.codereview.details.model.CodeReviewChangesViewModelBase
@@ -9,19 +10,25 @@ import com.intellij.openapi.actionSystem.DataKey
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.vcs.changes.Change
 import com.intellij.util.childScope
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.CoroutineScope
+import com.intellij.util.containers.CollectionFactory
+import git4idea.changes.GitBranchComparisonResult
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
 import org.jetbrains.plugins.gitlab.api.dto.GitLabCommitDTO
-import org.jetbrains.plugins.gitlab.mergerequest.data.GitLabMergeRequestChanges
+import org.jetbrains.plugins.gitlab.mergerequest.data.GitLabMergeRequest
+import org.jetbrains.plugins.gitlab.mergerequest.data.GitLabNotePosition
+import org.jetbrains.plugins.gitlab.mergerequest.data.firstNote
+import org.jetbrains.plugins.gitlab.mergerequest.data.mapToLocation
 import org.jetbrains.plugins.gitlab.mergerequest.diff.isEqual
+import java.util.concurrent.ConcurrentHashMap
 
 internal interface GitLabMergeRequestChangesViewModel : CodeReviewChangesViewModel<GitLabCommitDTO> {
   val changesResult: Flow<Result<Collection<Change>>>
 
   val changesSelection: StateFlow<ListSelection<Change>>
   val changeSelectionRequests: Flow<Change>
+
+  val mappedDiscussionsCounts: Flow<Map<Change, Int>>
 
   fun selectChange(change: Change)
 
@@ -36,16 +43,16 @@ internal interface GitLabMergeRequestChangesViewModel : CodeReviewChangesViewMod
 
 internal class GitLabMergeRequestChangesViewModelImpl(
   parentCs: CoroutineScope,
-  changes: Flow<GitLabMergeRequestChanges>
+  mergeRequest: GitLabMergeRequest
 ) : GitLabMergeRequestChangesViewModel,
     CodeReviewChangesViewModelBase<GitLabCommitDTO>() {
   private val cs = parentCs.childScope()
 
   override val reviewCommits: StateFlow<List<GitLabCommitDTO>> =
-    changes.map { it.commits }
+    mergeRequest.changes.map { it.commits }
       .stateIn(cs, SharingStarted.Lazily, listOf())
 
-  private val parsedChanges = changes.map { runCatching { it.getParsedChanges() } }
+  private val parsedChanges = mergeRequest.changes.map { runCatching { it.getParsedChanges() } }
     .modelFlow(cs, thisLogger())
   override val changesResult: Flow<Result<Collection<Change>>> =
     combine(parsedChanges, selectedCommit) { changesResult, commit ->
@@ -59,6 +66,25 @@ internal class GitLabMergeRequestChangesViewModelImpl(
 
   private val _changeSelectionRequests = MutableSharedFlow<Change>()
   override val changeSelectionRequests: Flow<Change> = _changeSelectionRequests.asSharedFlow()
+
+
+  private val unresolvedDiscussionsPositions: Flow<Collection<GitLabNotePosition>> = createUnresolvedDiscussionsPositionsFlow(mergeRequest)
+
+  override val mappedDiscussionsCounts: Flow<Map<Change, Int>> =
+    combine(parsedChanges, selectedCommit) { changesResult, commit ->
+      val diffResult = changesResult.getOrNull() ?: return@combine emptyMap()
+      (diffResult.changesByCommits[commit?.sha] ?: diffResult.changes).associateWith {
+        diffResult.patchesByChange[it]!!
+      }
+    }.combine(unresolvedDiscussionsPositions) { changes, positions ->
+      val result: MutableMap<Change, Int> =
+        CollectionFactory.createCustomHashingStrategyMap(GitBranchComparisonResult.REVISION_COMPARISON_HASHING_STRATEGY)
+      changes.mapValuesTo(result) { (_, patch) ->
+        //TODO: cache?
+        positions.count { it.mapToLocation(patch) != null }
+      }
+      result
+    }.modelFlow(cs, thisLogger())
 
   override fun selectChange(change: Change) {
     cs.launch {
@@ -104,6 +130,40 @@ internal class GitLabMergeRequestChangesViewModelImpl(
       if (oldList.size != newList.size) return false
 
       return oldList.isEqual(newList)
+    }
+  }
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+private fun createUnresolvedDiscussionsPositionsFlow(mergeRequest: GitLabMergeRequest) = channelFlow {
+  withContext(CoroutineName("GitLab Merge Request discussions positions collector")) {
+    val discussionsCache = ConcurrentHashMap<String, GitLabNotePosition>()
+    val draftNotesCache = ConcurrentHashMap<String, GitLabNotePosition>()
+    mergeRequest.discussions.collectLatest { discussions ->
+      discussionsCache.clear()
+      discussions.forEach { disc ->
+        launchNow {
+          val positionFlow = disc.firstNote.filterNotNull().flatMapLatest { it.position }
+          combine(disc.resolved, positionFlow) { resolved, position ->
+            if (resolved) null else position
+          }.collectLatest {
+            if (it != null) discussionsCache[disc.id] = it else discussionsCache.remove(disc.id)
+            send(discussionsCache.values.toList() + draftNotesCache.values.toList())
+          }
+        }
+      }
+    }
+
+    mergeRequest.draftNotes.collectLatest { notes ->
+      draftNotesCache.clear()
+      notes.forEach { note ->
+        launchNow {
+          note.position.collectLatest {
+            if (it != null) draftNotesCache[note.id] = it else draftNotesCache.remove(note.id)
+            send(discussionsCache.values.toList() + draftNotesCache.values.toList())
+          }
+        }
+      }
     }
   }
 }
