f5cb83d42dd075cea4877d8810f029a103362faf
Nikita.Lyubimov <nikita.lyubimov@jetbrains.com>
2023-06-17 12:03:46 +0200

    [IDEA] get action title from template as targetGroupActionId can be registered not in IDEA itself
    (cherry picked from commit 77dbdb24eeeaf9920561fa00d807e2f61b109a7a)
    
    GitOrigin-RevId: bddeaa6c3776c1687cb510c5cfd01cf09429f887

--- platform/lang-impl/src/com/intellij/ui/codeFloatingToolbar/CustomizeCodeFloatingToolbarAction.kt
+++ platform/lang-impl/src/com/intellij/ui/codeFloatingToolbar/CustomizeCodeFloatingToolbarAction.kt
@@ -3,13 +3,14 @@ package com.intellij.ui.codeFloatingToolbar
 
 import com.intellij.ide.ui.customization.CustomizationUtil
 import com.intellij.idea.ActionsBundle
+import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
 class CustomizeCodeFloatingToolbarAction(private val targetGroupActionId: String): AnAction(ActionsBundle.actionText("Floating.CodeToolbar.Customize")) {
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.project ?: return
-    val groupName = ActionsBundle.groupText(targetGroupActionId)
+    val groupName = ActionManager.getInstance().getAction(targetGroupActionId).templateText
     val dialogWrapper = CustomizationUtil.createCustomizeGroupDialog(project, targetGroupActionId, groupName, null)
     dialogWrapper.show()
   }
