151d34db883b76cb1ce386be2df1a5d11228f41e
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2023-06-17 09:43:29 +0200

    propagate project directly to FileEncodingProvider extension
    GitOrigin-RevId: 216a54683ef2591e534e00a1d9a3e5b3cc4eb50b

--- platform/platform-impl/src/com/intellij/openapi/vfs/encoding/EncodingProjectManagerImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/encoding/EncodingProjectManagerImpl.java
@@ -51,7 +51,7 @@ import java.util.stream.Collectors;
 @State(name = "Encoding", storages = @Storage("encodings.xml"))
 public final class EncodingProjectManagerImpl extends EncodingProjectManager implements PersistentStateComponent<Element>, Disposable {
   private static final @NonNls String PROJECT_URL = "PROJECT";
-  private final Project myProject;
+  private final @NotNull Project myProject;
   private final EncodingManagerImpl myIdeEncodingManager;
   private boolean myNative2AsciiForPropertiesFiles;
   private Charset myDefaultCharsetForPropertiesFiles;
@@ -176,7 +176,7 @@ public final class EncodingProjectManagerImpl extends EncodingProjectManager imp
   public @Nullable Charset getEncoding(@Nullable VirtualFile virtualFile, boolean useParentDefaults) {
     if (virtualFile != null) {
       for (FileEncodingProvider encodingProvider : FileEncodingProvider.EP_NAME.getIterable()) {
-        Charset encoding = encodingProvider.getEncoding(virtualFile);
+        Charset encoding = encodingProvider.getEncoding(virtualFile, myProject);
         if (encoding != null) return encoding;
       }
     }
