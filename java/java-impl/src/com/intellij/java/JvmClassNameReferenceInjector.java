37821d0da00222c5659bff64b574d3ad745c0eb4
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-06-17 14:39:22 +0200

    [java] IDEA-117997 Add "Class reference" to "inject language/reference" intention
    GitOrigin-RevId: f7459451e28d143a915925adeac74d16105cd07c

--- java/java-impl/src/com/intellij/java/JvmClassNameReferenceInjector.java
+++ java/java-impl/src/com/intellij/java/JvmClassNameReferenceInjector.java
@@ -2,18 +2,22 @@
 package com.intellij.java;
 
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.JavaClassReferenceProvider;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.JavaClassReferenceSet;
 import com.intellij.psi.injection.ReferenceInjector;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.uast.UastContextKt;
 import org.jetbrains.uast.expressions.UInjectionHost;
 
 import javax.swing.*;
+
+import static com.intellij.psi.search.GlobalSearchScope.allScope;
 
 final class JvmClassNameReferenceInjector extends ReferenceInjector {
   @Override
@@ -28,6 +32,7 @@ final class JvmClassNameReferenceInjector extends ReferenceInjector {
 
   @Override
   public @NotNull String getDisplayName() {
+    //noinspection DialogTitleCapitalization
     return JavaBundle.message("label.jvm.class.name");
   }
 
@@ -41,7 +46,16 @@ final class JvmClassNameReferenceInjector extends ReferenceInjector {
     String className = host.evaluateToString();
     if (className == null) className = "";
 
-    return new JavaClassReferenceSet(className, element, range.getStartOffset(), false, new JavaClassReferenceProvider())
+    return new JavaClassReferenceSet(className, element, range.getStartOffset(), false, getReferenceProvider())
       .getReferences();
+  }
+
+  private static @NotNull JavaClassReferenceProvider getReferenceProvider() {
+    return new JavaClassReferenceProvider() {
+      @Override
+      public @NotNull GlobalSearchScope getScope(@NotNull Project project) {
+        return allScope(project); // relax search to all known classes because we do not know the usage context here
+      }
+    };
   }
 }
