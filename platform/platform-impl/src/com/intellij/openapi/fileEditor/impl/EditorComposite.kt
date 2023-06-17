c3c266b6d371b5ec8256a8f80865422c5494ea3a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-06-17 19:06:51 +0200

    introduce getEditorList to return list instead of array
    (cherry picked from commit e249d5fd06089907d11a59a62eeb0692ad7c1575)
    
    GitOrigin-RevId: cf2e42c171be5c5be7a5f01e7f96fbb991708f97

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorComposite.kt
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorComposite.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 @file:Suppress("ReplaceGetOrSet", "ReplacePutWithAssignment")
 
 package com.intellij.openapi.fileEditor.impl
@@ -279,7 +279,7 @@ open class EditorComposite internal constructor(
   val editors: Array<FileEditor>
     get() = allEditors.toTypedArray()
 
-  override val allEditors: List<FileEditor>
+  final override val allEditors: List<FileEditor>
     get() = editorsWithProviders.map { it.fileEditor }
 
   val allEditorsWithProviders: List<FileEditorWithProvider>
