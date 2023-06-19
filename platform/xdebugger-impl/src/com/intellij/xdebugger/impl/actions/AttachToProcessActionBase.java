fbd4c456f6edf7bb82b0823f1d1b61f0ba44aa82
Max Medvedev <maxim.medvedev@jetbrains.com>
2023-06-19 16:26:12 +0200

    IDEA-322908 "Attach to process" action: attach automatically if there is only one process available
    It's a proof of concept.
    Works if `xdebugger.silently.attach.to.process.if.only.one` registry option is on
    
    (cherry picked from commit dc3a32c326a6ba9b5d604f81ef85b19c57805f8d)
    
    GitOrigin-RevId: 5fe61ec8ccb9bbf129f63497961a92f14d06779f

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/AttachToProcessActionBase.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/AttachToProcessActionBase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.xdebugger.impl.actions;
 
 import com.intellij.execution.ExecutionException;
@@ -101,6 +101,14 @@ public abstract class AttachToProcessActionBase extends AnAction implements Dumb
 
         List<AttachItem> allItems = Collections.unmodifiableList(getTopLevelItems(indicator, project));
 
+        if (Registry.is("xdebugger.silently.attach.to.process.if.only.one")) {
+          AttachToProcessItem singleItem = getSingleItemIfItsAttachToProcess(allItems);
+          if (singleItem != null) {
+            singleItem.startDebugSession(project);
+            return;
+          }
+        }
+
         ApplicationManager.getApplication().invokeLater(() -> {
           AttachListStep step = new AttachListStep(allItems, XDebuggerBundle.message("xdebugger.attach.popup.title.default"), project);
 
@@ -137,6 +145,18 @@ public abstract class AttachToProcessActionBase extends AnAction implements Dumb
         }, project.getDisposed());
       }
     }.queue();
+  }
+
+  private static @Nullable AttachToProcessItem getSingleItemIfItsAttachToProcess(List<AttachItem> items) {
+    String recentGroupName = XDebuggerBundle.message("xdebugger.attach.toLocal.popup.recent");
+
+    List<AttachItem> nonRecentItems = items.stream().filter(i -> !i.getGroup().getGroupName().equals(recentGroupName)).limit(2).toList();
+    if (nonRecentItems.size() != 1) return null;
+
+    AttachItem single = nonRecentItems.get(0);
+    if (!(single instanceof AttachToProcessItem)) return null;
+
+    return (AttachToProcessItem)single;
   }
 
   protected List<XAttachHostProvider<XAttachHost>> getAvailableHosts() {
