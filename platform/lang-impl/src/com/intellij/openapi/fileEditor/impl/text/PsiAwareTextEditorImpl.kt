9b9b6f81c0995e91a980aa44a6aead4ea982905a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-06-19 08:40:05 +0200

    remove unneeded daemon restart
    (cherry picked from commit 47759199c61981cfaf1bdc7cfc60e695e09bf883)
    
    GitOrigin-RevId: f6a6ea02c2279d9135734454d6dac76b70cf2ce6

--- platform/lang-impl/src/com/intellij/openapi/fileEditor/impl/text/PsiAwareTextEditorImpl.kt
+++ platform/lang-impl/src/com/intellij/openapi/fileEditor/impl/text/PsiAwareTextEditorImpl.kt
@@ -4,7 +4,6 @@ package com.intellij.openapi.fileEditor.impl.text
 import com.intellij.codeHighlighting.BackgroundEditorHighlighter
 import com.intellij.codeInsight.codeVision.CodeVisionEntry
 import com.intellij.codeInsight.codeVision.CodeVisionInitializer
-import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.codeInsight.daemon.impl.TextEditorBackgroundHighlighter
 import com.intellij.codeInsight.documentation.render.DocRenderManager
 import com.intellij.codeInsight.documentation.render.DocRenderPassFactory
@@ -112,9 +111,6 @@ open class PsiAwareTextEditorImpl : TextEditorImpl {
       }
       state.placeholders?.takeIf { it.isNotEmpty() }?.let { placeholders ->
         applyPlaceholders(editor, placeholders)
-      }
-      if (psiFile != null && psiFile.isValid) {
-        DaemonCodeAnalyzer.getInstance(project).restart(psiFile)
       }
     }
   }
