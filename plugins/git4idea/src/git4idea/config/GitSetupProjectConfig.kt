7c5b1b29ef9e8244cc415a045815f6c69f5746f6
Aleksey Pivovarov <AMPivovarov@gmail.com>
2023-06-19 16:56:10 +0200

    git: do not set 'core.fsmonitor' option from intellij.yaml for older git versions
    GitOrigin-RevId: 2155d0fe8e73ec017d2cbd6f2bbc770ec61f16f8

--- plugins/git4idea/src/git4idea/config/GitSetupProjectConfig.kt
+++ plugins/git4idea/src/git4idea/config/GitSetupProjectConfig.kt
@@ -8,9 +8,10 @@ import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.vcs.ProjectLevelVcsManager
 import git4idea.repo.GitRepository
 import git4idea.repo.GitRepositoryManager
+import kotlin.reflect.KProperty1
 
 internal class GitSetupProjectConfig : ProjectActivity {
-  override suspend fun execute(project: Project) : Unit = blockingContext {
+  override suspend fun execute(project: Project): Unit = blockingContext {
     ProjectLevelVcsManager.getInstance(project).runAfterInitialization {
       setupConfigIfNeeded(project)
     }
@@ -19,26 +20,42 @@ internal class GitSetupProjectConfig : ProjectActivity {
   private fun setupConfigIfNeeded(project: Project) {
     val settings = GitVcsSettings.getInstance(project).state
     for (configVar in ConfigVariables.values()) {
-      val settingsValue = configVar.settingsGetter(settings)
-      if (settingsValue != null) {
-        val rootsToUpdate = mutableListOf<GitRepository>()
-        for (repo in GitRepositoryManager.getInstance(project).repositories) {
-          val value = GitConfigUtil.getValue(project, repo.root, configVar.gitName)
-          if (value == null) rootsToUpdate.add(repo)
-        }
-
-        for (repo in rootsToUpdate) {
-          GitConfigUtil.setValue(project, repo.root, configVar.gitName, settingsValue, "--local")
-        }
-      }
+      setupConfigIfNeeded(project, configVar, settings)
     }
   }
 
-  private enum class ConfigVariables(val gitName: String, val settingsGetter: (GitVcsOptions) -> String?) {
-    GC_AUTO("gc.auto", { it.gcAuto }),
-    CORE_FS_MONITOR("core.fsmonitor", { it.coreFsMonitor }),
-    CORE_UNTRACKED_CACHE("core.untrackedcache", { it.coreUntrackedCache }),
-    CORE_LONGPATHS("core.longpaths", { if (SystemInfo.isWindows) it.coreLongpaths else null }),
-    FEATURE_MANY_FILES("feature.manyFiles", { it.featureManyFiles }),
+  private fun setupConfigIfNeeded(project: Project,
+                                  configVar: ConfigVariables,
+                                  settings: GitVcsOptions) {
+    val settingsValue = configVar.settingsGetter(settings) ?: return
+
+    if (!configVar.condition(project, settingsValue)) {
+      return
+    }
+
+    val rootsToUpdate = mutableListOf<GitRepository>()
+    for (repo in GitRepositoryManager.getInstance(project).repositories) {
+      val value = GitConfigUtil.getValue(project, repo.root, configVar.gitName)
+      if (value == null) rootsToUpdate.add(repo)
+    }
+
+    for (repo in rootsToUpdate) {
+      GitConfigUtil.setValue(project, repo.root, configVar.gitName, settingsValue, "--local")
+    }
+  }
+
+  private enum class ConfigVariables(val gitName: String,
+                                     val settingsGetter: KProperty1<GitVcsOptions, String?>,
+                                     val condition: (project: Project, value: String) -> Boolean = { _, _ -> true }) {
+    GC_AUTO("gc.auto", GitVcsOptions::gcAuto),
+    CORE_FS_MONITOR("core.fsmonitor", GitVcsOptions::coreFsMonitor, { project, value ->
+      when {
+        GitConfigUtil.getBooleanValue(value) != null -> GitVersionSpecialty.SUPPORTS_BOOLEAN_FSMONITOR_OPTION.existsIn(project)
+        else -> true
+      }
+    }),
+    CORE_UNTRACKED_CACHE("core.untrackedcache", GitVcsOptions::coreUntrackedCache),
+    CORE_LONGPATHS("core.longpaths", GitVcsOptions::coreLongpaths, { _, _ -> SystemInfo.isWindows }),
+    FEATURE_MANY_FILES("feature.manyFiles", GitVcsOptions::featureManyFiles),
   }
 }
