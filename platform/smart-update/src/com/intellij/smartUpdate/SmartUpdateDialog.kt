16348fe1820ff13b4e1c3fe453a2492cfc24a34d
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2023-06-19 16:19:59 +0200

    smart update: scheduling
    (cherry picked from commit a0d813e6d5ecbd3c15fb2780f7468138cfe3052e)
    
    GitOrigin-RevId: ca2c0c8faad59323674dcf32f4f519a027e691d1

--- platform/smart-update/src/com/intellij/smartUpdate/SmartUpdateDialog.kt
+++ platform/smart-update/src/com/intellij/smartUpdate/SmartUpdateDialog.kt
@@ -4,8 +4,18 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.dsl.builder.Cell
 import com.intellij.ui.dsl.builder.bindSelected
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.builder.selected
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.ZoneId
+import java.util.*
+import javax.swing.JSpinner
+import javax.swing.SpinnerDateModel
 
 class SmartUpdateDialog(private val project: Project) : DialogWrapper(project) {
   init {
@@ -26,6 +36,21 @@ class SmartUpdateDialog(private val project: Project) : DialogWrapper(project) {
           indent { row { cell(it).enabled(enabled) } }
         }
       }
+      lateinit var scheduled: Cell<JBCheckBox>
+      row { scheduled = checkBox(SmartUpdateBundle.message("checkbox.schedule.update")).bindSelected({ options.scheduled }, { options.scheduled = it}) }
+      indent {
+        val spinner = JSpinner(SpinnerDateModel().apply { value = getScheduled(options.scheduledTime) })
+        spinner.editor = JSpinner.DateEditor(spinner, "hh:mm")
+        row {
+          label(SmartUpdateBundle.message("label.every.day.at"))
+          cell(spinner)
+        }.enabledIf(scheduled.selected)
+      }
     }
+  }
+
+  private fun getScheduled(time: LocalTime): Date {
+    val instant: Instant = time.atDate(LocalDate.now()).atZone(ZoneId.systemDefault()).toInstant()
+    return Date.from(instant)
   }
 }
