75ed3c0cc004cc8c8ae6fad52e8a62cf59755e80
Alex Plate <aleksei.plate@jetbrains.com>
2023-06-19 12:42:24 +0300

    [Workspace Model] [IDEA-320676] Add @Internal annotation on BridgeInitializer
    (cherry picked from commit d7618a4108e28d17810dc8f12b65634b3c7546aa)
    
    GitOrigin-RevId: 718a66aca18615fda4e2c1eaa3089f03f38a354c

--- platform/backend/workspace/src/BridgeInitializer.kt
+++ platform/backend/workspace/src/BridgeInitializer.kt
@@ -5,7 +5,9 @@ import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
 import com.intellij.platform.workspace.storage.EntityChange
 import com.intellij.platform.workspace.storage.MutableEntityStorage
+import org.jetbrains.annotations.ApiStatus
 
+@ApiStatus.Internal
 interface BridgeInitializer {
   fun isEnabled(): Boolean
   fun initializeBridges(project: Project, changes: Map<Class<*>, List<EntityChange<*>>>, builder: MutableEntityStorage)
