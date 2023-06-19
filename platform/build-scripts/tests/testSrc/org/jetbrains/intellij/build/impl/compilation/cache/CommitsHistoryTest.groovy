e6af46e28be1d1c1e56dd4a459086e61e0376fae
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2023-06-20 01:56:38 +0200

    build scripts: commit history test converted to Kotlin
    GitOrigin-RevId: 3279ec98d14341cfa7a6b87dde6a71fea4f0cae9

--- platform/build-scripts/tests/testSrc/org/jetbrains/intellij/build/impl/compilation/cache/CommitsHistoryTest.groovy
+++ platform/build-scripts/tests/testSrc/org/jetbrains/intellij/build/impl/compilation/cache/CommitsHistoryTest.groovy
@@ -5,31 +5,31 @@ import org.junit.Test
 
 class CommitsHistoryTest {
   @Test
-  void 'commit-history json union test'() {
-    def union = new CommitsHistory('''{
+  fun `commit-history json union test`() {
+    val union = CommitsHistory("""{
       "remote1" : [ "commit1.1", "commit1.2" ],
       "remote2" : [ "commit2.1", "commit2.2" ]
-    }''') + new CommitsHistory('''{
+    }""") + CommitsHistory("""{
       "remote1" : [ "commit1.3", "commit1.3" ],
       "remote3" : [ "commit3.1" ]
-    }''')
-    assert (union.commitsForRemote('remote1') - ['commit1.1', 'commit1.2', 'commit1.3']).isEmpty()
-    assert (union.commitsForRemote('remote2') - ['commit2.1', 'commit2.2']).isEmpty()
-    assert (union.commitsForRemote('remote3') - ['commit3.1']).isEmpty()
+    }""")
+    assert((union.commitsForRemote("remote1") - listOf("commit1.1", "commit1.2", "commit1.3")).isEmpty())
+    assert((union.commitsForRemote("remote2") - listOf("commit2.1", "commit2.2")).isEmpty())
+    assert((union.commitsForRemote("remote3") - listOf("commit3.1")).isEmpty())
   }
 
   @Test
-  void 'commit-history json subtraction test'() {
-    def subtraction = new CommitsHistory('''{
+  fun `commit-history json subtraction test`() {
+    val subtraction = CommitsHistory("""{
       "remote1" : [ "commit1.1", "commit1.2" ],
       "remote2" : [ "commit2.1", "commit2.2" ]
-    }''') - new CommitsHistory('''{
+    }""") - CommitsHistory("""{
       "remote1" : [ "commit1.2", "commit1.3" ],
       "remote2" : [ "commit2.1", "commit2.3" ],
       "remote3" : [ "commit3.1" ]
-    }''')
-    assert (subtraction.commitsForRemote('remote1') - ['commit1.1']).isEmpty()
-    assert (subtraction.commitsForRemote('remote2') - ['commit2.2']).isEmpty()
-    assert subtraction.commitsForRemote('remote3').isEmpty()
+    }""")
+    assert((subtraction.commitsForRemote("remote1") - "commit1.1").isEmpty())
+    assert((subtraction.commitsForRemote("remote2") - "commit2.2").isEmpty())
+    assert(subtraction.commitsForRemote("remote3").isEmpty())
   }
 }
