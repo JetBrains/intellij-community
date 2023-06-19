8afef54ba4e3026b224330d729395310f9230f29
Andrei.Kuznetsov <andrei.kuznetsov@jetbrains.com>
2023-06-19 19:07:32 +0200

    IDEA-322925: `FileContentImpl.getContentAsText()` returns wrong text for XML files with BOM (e.g. UTF-16LE)
    (cherry picked from commit 59fca0fcb00d855b60dc40202546c7c2b54ec2d9)
    
    GitOrigin-RevId: 3f8e024261944732d42898ef6fe56f1c8a82afb5

--- platform/platform-tests/testSrc/com/intellij/util/indexing/FileContentImplTest.kt
+++ platform/platform-tests/testSrc/com/intellij/util/indexing/FileContentImplTest.kt
@@ -2,6 +2,7 @@
 package com.intellij.util.indexing
 
 import com.intellij.openapi.fileEditor.impl.LoadTextUtil
+import com.intellij.openapi.fileTypes.ex.FileTypeManagerEx
 import com.intellij.openapi.util.NotNullComputable
 import com.intellij.openapi.vfs.CharsetToolkit
 import com.intellij.openapi.vfs.VirtualFile
@@ -9,11 +10,21 @@ import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.testFramework.HeavyPlatformTestCase
 import com.intellij.util.ArrayUtil
 import com.intellij.util.io.write
+import junit.framework.TestCase
 import org.junit.Assert.assertArrayEquals
 import java.nio.charset.StandardCharsets
 import java.util.concurrent.atomic.AtomicBoolean
 
 class FileContentImplTest : HeavyPlatformTestCase() {
+
+  private val CHARSETS_WITH_BOM = listOf(
+    StandardCharsets.UTF_8,
+    StandardCharsets.UTF_16LE,
+    StandardCharsets.UTF_16BE,
+    CharsetToolkit.UTF_32LE_CHARSET,
+    CharsetToolkit.UTF_32BE_CHARSET
+  )
+
   fun `test text file`() {
     val text = "aaa"
     val content = createFileContent(text.toByteArray())
@@ -75,13 +86,7 @@ class FileContentImplTest : HeavyPlatformTestCase() {
 
   fun `test bom is truncated from getContent()`() {
     val text = "哇你居然翻译了这篇中文文本"
-    for (charset in listOf(
-      StandardCharsets.UTF_8,
-      StandardCharsets.UTF_16LE,
-      StandardCharsets.UTF_16BE,
-      CharsetToolkit.UTF_32LE_CHARSET,
-      CharsetToolkit.UTF_32BE_CHARSET
-    )) {
+    for (charset in CHARSETS_WITH_BOM) {
       val bom = CharsetToolkit.getPossibleBom(charset)!!
       val bytes = ArrayUtil.mergeArrays(bom, text.toByteArray (charset))
       val fileContent = createFileContent(bytes)
@@ -91,22 +96,50 @@ class FileContentImplTest : HeavyPlatformTestCase() {
     }
   }
 
+  fun `test contentAsText() returns correct content for files with BOM`() {
+    val text = "<xml/>"
+    for (charset in CHARSETS_WITH_BOM) {
+      for (ext in listOf("xml", "txt")) {
+        // in txt files content {60,0,120,0,...}=("<x..." in UTF16) is recognized as "guessed" binary content
+        // in xml files content {60,0,120,0,...}=("<x..." in UTF16) is recognized as "hardCoded" UTF8 content (see XmlLikeFileType.getCharset which defaults to UTF8)
+        val bom = CharsetToolkit.getPossibleBom(charset)!!
+        val bytes = ArrayUtil.mergeArrays(bom, text.toByteArray(charset))
+        val fileContent = createFileContent(bytes, ext)
+        val contentAsText = fileContent.contentAsText
+        val detectedFileType = FileTypeManagerEx.getInstance().getFileTypeByFile(fileContent.file)
+        TestCase.assertFalse(detectedFileType.isBinary)
+        TestCase.assertEquals("Wrong text: charset=$charset, file=$ext", text, contentAsText.toString())
+      }
+    }
+
+  }
+
   private fun createFileContent(bytes: ByteArray, binary: Boolean = false): FileContent {
-    val virtualFile = createInputFile(bytes, binary)
+    return createFileContent(bytes, suggestExtension(binary))
+  }
+
+  private fun createFileContent(bytes: ByteArray, fileExtension: String): FileContent {
+    val virtualFile = createInputFile(bytes, fileExtension)
     return FileContentImpl.createByFile(virtualFile, myProject)
   }
 
   private fun createInputFile(content: ByteArray, binary: Boolean = false): VirtualFile {
+    return createInputFile(content, suggestExtension(binary)).also {
+      assertEquals(binary, it.fileType.isBinary)
+    }
+  }
+
+  private fun suggestExtension(binary: Boolean) = if (binary) "jpg" else "txt"
+
+  private fun createInputFile(content: ByteArray, fileExtension: String): VirtualFile {
     val home = createTempDir("hashing-test").toPath()
 
-    val path = home.resolve(if (binary) "file.jpg" else "test-file.txt")
+    val path = home.resolve("test-file.$fileExtension")
 
     //we create the file via low-level code to make sure IntelliJ Utils would not alter the newlines in it
     path.write(content)
 
-    val virtualFile = VirtualFileManager.getInstance().refreshAndFindFileByNioPath(path) ?: error("Failed to find temp file")
-    assertEquals(binary, virtualFile.fileType.isBinary)
-    return virtualFile
+    return VirtualFileManager.getInstance().refreshAndFindFileByNioPath(path) ?: error("Failed to find temp file")
   }
 
 }
