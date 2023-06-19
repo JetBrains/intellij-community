7c08960864e9ef76519dda5129fe7254a7910287
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2023-06-19 15:55:59 +0200

    [core] IDEA-316275: restricted ID name to safe [A-Za-z0-9_-.] pattern
    + log a warning on invalid name (exception would break existing code -- probably, for th next release?)
    + added apt @Pattern annotation
    
    GitOrigin-RevId: b0ee881062e312a5f530e433ce53b5ef0813063b

--- platform/util/src/com/intellij/util/indexing/IndexId.java
+++ platform/util/src/com/intellij/util/indexing/IndexId.java
@@ -1,17 +1,34 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.indexing;
 
+import com.intellij.openapi.diagnostic.Logger;
+import org.intellij.lang.annotations.Pattern;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class IndexId<K, V> {
+  private static final Logger LOG = Logger.getInstance(IndexId.class);
+
   private static final Map<String, IndexId<?, ?>> ourInstances = new HashMap<>();
+
+  public static final String VALID_ID_REGEXP = "[A-Za-z0-9_.\\-]+";
+  private static final java.util.regex.Pattern VALID_ID_REGEXP_PATTERN = java.util.regex.Pattern.compile(VALID_ID_REGEXP);
 
   private final @NotNull String myName;
 
-  protected IndexId(@NotNull String name) {myName = name;}
+
+  protected IndexId(@NotNull @Pattern(VALID_ID_REGEXP) String name) {
+    if (!VALID_ID_REGEXP_PATTERN.matcher(name).matches()) {
+      LOG.warn("IndexId name[" + name + "] should match [" + VALID_ID_REGEXP + "]. " +
+               "Names with unsafe characters could cause issues on some platforms. " +
+               "This warning likely will be escalated to an error in the following releases.");
+      //TODO RC: move to exception in v23.3?
+      //throw new IllegalArgumentException("name[" + name + "] must match [" + VALID_ID_REGEXP + "]");
+    }
+    myName = name;
+  }
 
   public final @NotNull String getName() {
     return myName;
