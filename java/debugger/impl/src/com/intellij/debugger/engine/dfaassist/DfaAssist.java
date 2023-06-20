b0729f78c426faf62df982f312df36ea342bec90
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-06-20 13:52:37 +0200

    [dfa-assist] Register DfaAssist in Disposer
    Fixes IDEA-322899 Memory leak detected: 'com.intellij.debugger.engine.dfaassist.DfaAssist'
    
    (cherry picked from commit 598de05c66c37c56bc22a74f7864c9b404889a46)
    
    IJ-CR-108994
    
    GitOrigin-RevId: e6e85274980af8c2f2db806d2e4ac3a422ab2d8d

--- java/debugger/impl/src/com/intellij/debugger/engine/dfaassist/DfaAssist.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/dfaassist/DfaAssist.java
@@ -13,6 +13,7 @@ import com.intellij.debugger.impl.DebuggerStateManager;
 import com.intellij.debugger.jdi.StackFrameProxyEx;
 import com.intellij.debugger.jdi.StackFrameProxyImpl;
 import com.intellij.debugger.settings.ViewsGeneralSettings;
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.project.Project;
@@ -183,12 +184,14 @@ public final class DfaAssist extends DfaAssistBase implements DebuggerContextLis
    * @param session     X debugger session
    */
   public static void installDfaAssist(@NotNull DebuggerSession javaSession,
-                                      @NotNull XDebugSession session) {
+                                      @NotNull XDebugSession session,
+                                      @NotNull Disposable parent) {
     DebuggerStateManager manager = javaSession.getContextManager();
     DebuggerContextImpl context = manager.getContext();
     Project project = context.getProject();
     if (project != null) {
       DfaAssist assist = new DfaAssist(project, manager);
+      Disposer.register(parent, assist);
       manager.addListener(assist);
       session.addSessionListener(new XDebugSessionListener() {
         @Override
