2114b5158cf439d961a33bc860abfd273517734f
Anna Kozlova <anna.kozlova@jetbrains.com>
2023-06-20 13:16:43 +0200

    [kotlin] k2 completion: avoid requesting fir for fake elements from completion (KTIJ-25929)
    notUnderContentRootModuleInfo recognizes exactly one file. If it is a copy, then a consequence resolve won't work because it will take `originalDeclaration` first and then moduleKind won't match.
    
    GitOrigin-RevId: 9bf0ea13430f283f283239fbec38113ab975659c

--- plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/completion/FirCompletionOutsideSourceRootTest.kt
+++ plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/completion/FirCompletionOutsideSourceRootTest.kt
@@ -1,17 +1,25 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.fir.completion
 
+import com.intellij.testFramework.common.runAll
 import com.intellij.testFramework.LightProjectDescriptor
 import java.nio.charset.Charset
+import org.jetbrains.kotlin.idea.fir.invalidateCaches
 import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
 import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
 
 class FirCompletionOutsideSourceRootTest : KotlinLightCodeInsightFixtureTestCase() {
-  fun testFunctionName() {
-      val tempFile = createTempFile("kt", null, "fun <caret>", Charset.defaultCharset())
-      myFixture.configureFromExistingVirtualFile(tempFile)
-      myFixture.completeBasic()
-  }
+    fun testFunctionName() {
+        val tempFile = createTempFile("kt", null, "fun <caret>", Charset.defaultCharset())
+        myFixture.configureFromExistingVirtualFile(tempFile)
+        myFixture.completeBasic()
+    }
+
+    fun testOverrideHandler() {
+        val tempFile = createTempFile("kt", null, "interface A {fu<caret>}", Charset.defaultCharset())
+        myFixture.configureFromExistingVirtualFile(tempFile)
+        myFixture.completeBasic()
+    }
 
     override fun isFirPlugin(): Boolean {
         return true
@@ -19,5 +27,12 @@ class FirCompletionOutsideSourceRootTest : KotlinLightCodeInsightFixtureTestCase
 
     override fun getProjectDescriptor(): LightProjectDescriptor {
         return KotlinWithJdkAndRuntimeLightProjectDescriptor.getInstance()
+    }
+
+    override fun tearDown() {
+        runAll(
+            { project.invalidateCaches() },
+            { super.tearDown() }
+        )
     }
 }
