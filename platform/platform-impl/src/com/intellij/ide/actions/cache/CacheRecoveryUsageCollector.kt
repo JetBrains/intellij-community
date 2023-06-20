2abd85cc0ba3a381dc15e18112f2c65da05a3542
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2023-06-20 14:42:44 +0200

    IJPL-52 bump "cache.recovery.actions" event group version
    GitOrigin-RevId: ab9ed2234e78164d0ebc452c50b168c6fec4bc63

--- platform/platform-impl/src/com/intellij/ide/actions/cache/CacheRecoveryUsageCollector.kt
+++ platform/platform-impl/src/com/intellij/ide/actions/cache/CacheRecoveryUsageCollector.kt
@@ -8,7 +8,7 @@ import com.intellij.openapi.project.Project
 
 internal class CacheRecoveryUsageCollector : CounterUsagesCollector() {
   companion object {
-    private val GROUP = EventLogGroup("cache.recovery.actions", 2)
+    private val GROUP = EventLogGroup("cache.recovery.actions", 3)
 
     private val ACTION_ID_FIELD =
       EventFields.String("action-id",
