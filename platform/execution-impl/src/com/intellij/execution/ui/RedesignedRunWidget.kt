38e4ace7b032096babfdfdf4c0cbc0dc12b43c00
Mikhail Sokolov <mikhail.sokolov@jetbrains.com>
2023-06-21 15:29:17 +0200

    IDEA-320087 Main toolbar: update hover area sizes
    GitOrigin-RevId: 2b7dcdcdafbde456634d0d63044f024c7c8d1f2b

--- platform/execution-impl/src/com/intellij/execution/ui/RedesignedRunWidget.kt
+++ platform/execution-impl/src/com/intellij/execution/ui/RedesignedRunWidget.kt
@@ -266,7 +266,7 @@ private class RunWidgetButtonLook(private val isCurrentConfigurationRunning: () 
   }
 
   override fun paintLookBorder(g: Graphics, rect: Rectangle, color: Color) {}
-  override fun getButtonArc(): JBValue = JBValue.Float(10f)
+  override fun getButtonArc(): JBValue = JBValue.Float(6f)
 }
 
 internal const val MINIMAL_POPUP_WIDTH = 270
