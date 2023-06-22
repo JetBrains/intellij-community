745f8114b9efd317d7ea037351f245eeeef3e6f7
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-06-22 02:19:23 +0200

    IDEA-323220 do not load icons as a part of a base laf init
    GitOrigin-RevId: b09435793813d79512c4825ccf3f7ccde447df1f

--- platform/platform-impl/src/com/intellij/ide/ui/laf/IdeaLaf.java
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/IdeaLaf.java
@@ -8,14 +8,15 @@ import com.intellij.ui.plaf.beg.*;
 import com.intellij.ui.scale.JBUIScale;
 import com.intellij.util.ui.StartupUiUtil;
 import kotlin.Pair;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.plaf.ColorUIResource;
 import javax.swing.plaf.metal.MetalLookAndFeel;
 import java.awt.*;
 import java.util.Map;
+import java.util.function.Supplier;
 
 /**
 * @author Konstantin Bulenkov
@@ -56,102 +57,104 @@ public final class IdeaLaf extends MetalLookAndFeel {
     defaults.put("ScrollBar.background", col);
     defaults.put("ScrollBar.track", col);
 
-    defaults.put("TextField.border", BegBorders.getTextFieldBorder());
-    defaults.put("PasswordField.border", BegBorders.getTextFieldBorder());
-    Border popupMenuBorder = new BegPopupMenuBorder();
-    defaults.put("PopupMenu.border", popupMenuBorder);
-    defaults.put("ScrollPane.border", BegBorders.getScrollPaneBorder());
+    defaults.put("TextField.border", createLazyValue(() -> BegBorders.getTextFieldBorder()));
+    defaults.put("PasswordField.border", createLazyValue(() -> BegBorders.getTextFieldBorder()));
+    defaults.put("PopupMenu.border", createLazyValue(() -> new BegPopupMenuBorder()));
+    defaults.put("ScrollPane.border", createLazyValue(() -> BegBorders.getScrollPaneBorder()));
 
-    defaults.put("ToggleButtonUI", BegToggleButtonUI.class.getName());
-    defaults.put("RadioButtonUI", BegRadioButtonUI.class.getName());
-    defaults.put("TabbedPaneUI", BegTabbedPaneUI.class.getName());
-    defaults.put("TableUI", BegTableUI.class.getName());
-    defaults.put("TreeUI", BegTreeUI.class.getName());
+    defaults.put("ToggleButtonUI", createLazyValue(() -> BegToggleButtonUI.class.getName()));
+    defaults.put("RadioButtonUI", createLazyValue(() -> BegRadioButtonUI.class.getName()));
+    defaults.put("TabbedPaneUI", createLazyValue(() -> BegTabbedPaneUI.class.getName()));
+    defaults.put("TableUI", createLazyValue(() -> BegTableUI.class.getName()));
+    defaults.put("TreeUI", createLazyValue(() -> BegTreeUI.class.getName()));
 
     defaults.put("TabbedPane.tabInsets", new Insets(0, 4, 0, 4));
     defaults.put("ToolTip.background", TOOLTIP_BACKGROUND_COLOR);
     defaults.put("ToolTip.border", new ColoredSideBorder(Color.gray, Color.gray, Color.black, Color.black, 1));
     defaults.put("Tree.ancestorInputMap", null);
-    defaults.put("FileView.directoryIcon", AllIcons.Nodes.Folder);
-    defaults.put("FileChooser.upFolderIcon", AllIcons.Nodes.UpFolder);
-    defaults.put("FileChooser.newFolderIcon", AllIcons.Nodes.Folder);
-    defaults.put("FileChooser.homeFolderIcon", AllIcons.Nodes.HomeFolder);
-    defaults.put("OptionPane.errorIcon", AllIcons.General.ErrorDialog);
-    defaults.put("OptionPane.informationIcon", AllIcons.General.InformationDialog);
-    defaults.put("OptionPane.warningIcon", AllIcons.General.WarningDialog);
-    defaults.put("OptionPane.questionIcon", AllIcons.General.QuestionDialog);
-    defaults.put("Table.ancestorInputMap", new UIDefaults.LazyInputMap(new Object[] {
-                       "ctrl C", "copy",
-                       "ctrl V", "paste",
-                       "ctrl X", "cut",
-                         "COPY", "copy",
-                        "PASTE", "paste",
-                          "CUT", "cut",
-               "control INSERT", "copy",
-                 "shift INSERT", "paste",
-                 "shift DELETE", "cut",
-                        "RIGHT", TableActions.Right.ID,
-                     "KP_RIGHT", TableActions.Right.ID,
-                         "LEFT", TableActions.Left.ID,
-                      "KP_LEFT", TableActions.Left.ID,
-                         "DOWN", TableActions.Down.ID,
-                      "KP_DOWN", TableActions.Down.ID,
-                           "UP", TableActions.Up.ID,
-                        "KP_UP", TableActions.Up.ID,
-                  "shift RIGHT", TableActions.ShiftRight.ID,
-               "shift KP_RIGHT", TableActions.ShiftRight.ID,
-                   "shift LEFT", TableActions.ShiftLeft.ID,
-                "shift KP_LEFT", TableActions.ShiftLeft.ID,
-                   "shift DOWN", TableActions.ShiftDown.ID,
-                "shift KP_DOWN", TableActions.ShiftDown.ID,
-                     "shift UP", TableActions.ShiftUp.ID,
-                  "shift KP_UP", TableActions.ShiftUp.ID,
-                      "PAGE_UP", TableActions.PageUp.ID,
-                    "PAGE_DOWN", TableActions.PageDown.ID,
-                         "HOME", "selectFirstColumn",
-                          "END", "selectLastColumn",
-                "shift PAGE_UP", TableActions.ShiftPageUp.ID,
-              "shift PAGE_DOWN", TableActions.ShiftPageDown.ID,
-                   "shift HOME", "selectFirstColumnExtendSelection",
-                    "shift END", "selectLastColumnExtendSelection",
-                 "ctrl PAGE_UP", "scrollLeftChangeSelection",
-               "ctrl PAGE_DOWN", "scrollRightChangeSelection",
-                    "ctrl HOME", TableActions.CtrlHome.ID,
-                     "ctrl END", TableActions.CtrlEnd.ID,
-           "ctrl shift PAGE_UP", "scrollRightExtendSelection",
-         "ctrl shift PAGE_DOWN", "scrollLeftExtendSelection",
-              "ctrl shift HOME", TableActions.CtrlShiftHome.ID,
-               "ctrl shift END", TableActions.CtrlShiftEnd.ID,
-                          "TAB", "selectNextColumnCell",
-                    "shift TAB", "selectPreviousColumnCell",
-                        //"ENTER", "selectNextRowCell",
-                  "shift ENTER", "selectPreviousRowCell",
-                       "ctrl A", "selectAll",
-                       //"ESCAPE", "cancel",
-                           "F2", "startEditing"
-         }));
+    defaults.put("FileView.directoryIcon", createLazyValue(() -> AllIcons.Nodes.Folder));
+    defaults.put("FileChooser.upFolderIcon", createLazyValue(() -> AllIcons.Nodes.UpFolder));
+    defaults.put("FileChooser.newFolderIcon", createLazyValue(() -> AllIcons.Nodes.Folder));
+    defaults.put("FileChooser.homeFolderIcon", createLazyValue(() -> AllIcons.Nodes.HomeFolder));
+    defaults.put("OptionPane.errorIcon", createLazyValue(() -> AllIcons.General.ErrorDialog));
+    defaults.put("OptionPane.informationIcon", createLazyValue(() -> AllIcons.General.InformationDialog));
+    defaults.put("OptionPane.warningIcon", createLazyValue(() -> AllIcons.General.WarningDialog));
+    defaults.put("OptionPane.questionIcon", createLazyValue(() -> AllIcons.General.QuestionDialog));
+    defaults.put("Table.ancestorInputMap", new UIDefaults.LazyValue() {
+      @Override
+      public Object createValue(UIDefaults table) {
+        return LookAndFeel.makeInputMap(new Object[]{
+          "ctrl C", "copy",
+          "ctrl V", "paste",
+          "ctrl X", "cut",
+          "COPY", "copy",
+          "PASTE", "paste",
+          "CUT", "cut",
+          "control INSERT", "copy",
+          "shift INSERT", "paste",
+          "shift DELETE", "cut",
+          "RIGHT", TableActions.Right.ID,
+          "KP_RIGHT", TableActions.Right.ID,
+          "LEFT", TableActions.Left.ID,
+          "KP_LEFT", TableActions.Left.ID,
+          "DOWN", TableActions.Down.ID,
+          "KP_DOWN", TableActions.Down.ID,
+          "UP", TableActions.Up.ID,
+          "KP_UP", TableActions.Up.ID,
+          "shift RIGHT", TableActions.ShiftRight.ID,
+          "shift KP_RIGHT", TableActions.ShiftRight.ID,
+          "shift LEFT", TableActions.ShiftLeft.ID,
+          "shift KP_LEFT", TableActions.ShiftLeft.ID,
+          "shift DOWN", TableActions.ShiftDown.ID,
+          "shift KP_DOWN", TableActions.ShiftDown.ID,
+          "shift UP", TableActions.ShiftUp.ID,
+          "shift KP_UP", TableActions.ShiftUp.ID,
+          "PAGE_UP", TableActions.PageUp.ID,
+          "PAGE_DOWN", TableActions.PageDown.ID,
+          "HOME", "selectFirstColumn",
+          "END", "selectLastColumn",
+          "shift PAGE_UP", TableActions.ShiftPageUp.ID,
+          "shift PAGE_DOWN", TableActions.ShiftPageDown.ID,
+          "shift HOME", "selectFirstColumnExtendSelection",
+          "shift END", "selectLastColumnExtendSelection",
+          "ctrl PAGE_UP", "scrollLeftChangeSelection",
+          "ctrl PAGE_DOWN", "scrollRightChangeSelection",
+          "ctrl HOME", TableActions.CtrlHome.ID,
+          "ctrl END", TableActions.CtrlEnd.ID,
+          "ctrl shift PAGE_UP", "scrollRightExtendSelection",
+          "ctrl shift PAGE_DOWN", "scrollLeftExtendSelection",
+          "ctrl shift HOME", TableActions.CtrlShiftHome.ID,
+          "ctrl shift END", TableActions.CtrlShiftEnd.ID,
+          "TAB", "selectNextColumnCell",
+          "shift TAB", "selectPreviousColumnCell",
+          //"ENTER", "selectNextRowCell",
+          "shift ENTER", "selectPreviousRowCell",
+          "ctrl A", "selectAll",
+          //"ESCAPE", "cancel",
+          "F2", "startEditing"
+        });
+      }
+    });
   }
 
   static void fillFallbackDefaults(UIDefaults defaults) {
     // These icons are only needed to prevent Swing from trying to fetch defaults with AWT ImageFetcher threads (IDEA-322089),
     // but might as well just put something sensibly-looking there, just in case they show up due to some bug:
-    UIDefaults.LazyValue folderIcon = new UIDefaults.LazyValue() {
-      @Override
-      public Object createValue(UIDefaults table) {
-        return AllIcons.Nodes.Folder;
-      }
-    };
+    UIDefaults.LazyValue folderIcon = createLazyValue(() -> AllIcons.Nodes.Folder);
     defaults.put("Tree.openIcon", folderIcon);
     defaults.put("Tree.closedIcon", folderIcon);
-    defaults.put("Tree.leafIcon", new UIDefaults.LazyValue() {
-      @Override
-      public Object createValue(UIDefaults table) {
-        return AllIcons.FileTypes.Any_type;
-      }
-    });
+    defaults.put("Tree.leafIcon", createLazyValue(() -> AllIcons.FileTypes.Any_type));
     // These two are actually set by our themes, so we don't want to override them here:
     //defaults.put("Tree.expandedIcon", AllIcons.Toolbar.Expand);
     //defaults.put("Tree.collapsedIcon", AllIcons.Actions.ArrowExpand);
   }
 
+  private static @NotNull UIDefaults.LazyValue createLazyValue(@NotNull Supplier<Object> supplier) {
+    return new UIDefaults.LazyValue() {
+      @Override
+      public Object createValue(UIDefaults table) {
+        return supplier.get();
+      }
+    };
+  }
 }
