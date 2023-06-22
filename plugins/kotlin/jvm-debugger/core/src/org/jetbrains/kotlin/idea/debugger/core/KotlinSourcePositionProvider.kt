629a90d222932678cb7fdcc8b1f9c96d4eb05111
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2023-06-22 13:47:22 +0900

    [kotlin] Migrate 'KotlinSourcePositionProvider' to Analysis API
    GitOrigin-RevId: 5ba050dbd0073e14271c44768768c7c7ef00d6cf

--- plugins/kotlin/jvm-debugger/core/src/org/jetbrains/kotlin/idea/debugger/core/KotlinSourcePositionProvider.kt
+++ plugins/kotlin/jvm-debugger/core/src/org/jetbrains/kotlin/idea/debugger/core/KotlinSourcePositionProvider.kt
@@ -1,6 +1,5 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
-package org.jetbrains.kotlin.idea.debugger
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.debugger.core
 
 import com.intellij.debugger.SourcePosition
 import com.intellij.debugger.engine.SourcePositionProvider
@@ -19,21 +18,17 @@ import com.sun.jdi.AbsentInformationException
 import com.sun.jdi.ClassNotPreparedException
 import com.sun.jdi.ClassType
 import com.sun.jdi.ReferenceType
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.symbols.KtVariableLikeSymbol
 import org.jetbrains.kotlin.codegen.AsmUtil
-import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.idea.debugger.base.util.safeAllInterfaces
 import org.jetbrains.kotlin.idea.debugger.base.util.safeAllLineLocations
-import org.jetbrains.kotlin.idea.debugger.core.GetterDescriptor
-import org.jetbrains.kotlin.idea.debugger.core.getContextElement
+import org.jetbrains.kotlin.idea.references.mainReference
 import org.jetbrains.kotlin.load.java.possibleGetMethodNames
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
-import org.jetbrains.kotlin.resolve.BindingContextUtils
 import org.jetbrains.kotlin.resolve.jvm.JvmClassName
-import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
-import org.jetbrains.kotlin.resolve.source.KotlinSourceElement
-import org.jetbrains.kotlin.resolve.source.getPsi
 
 class KotlinSourcePositionProvider : SourcePositionProvider() {
     override fun computeSourcePosition(
@@ -44,7 +39,7 @@ class KotlinSourcePositionProvider : SourcePositionProvider() {
     ): SourcePosition? {
         if (context.frameProxy == null || DumbService.isDumb(project)) return null
 
-        return when(descriptor) {
+        return when (descriptor) {
             is FieldDescriptor -> computeSourcePosition(descriptor, context, nearest)
             is GetterDescriptor -> computeSourcePosition(descriptor, context, nearest)
             is LocalVariableDescriptor -> computeSourcePosition(descriptor, context, nearest)
@@ -63,30 +58,25 @@ class KotlinSourcePositionProvider : SourcePositionProvider() {
         val contextElement = getContextElement(place) ?: return null
 
         val codeFragment = KtPsiFactory(context.project).createExpressionCodeFragment(descriptor.name, contextElement)
-        val expression = codeFragment.getContentElement()
-        if (expression is KtSimpleNameExpression) {
-            val bindingContext = expression.analyze(BodyResolveMode.PARTIAL)
-            val declarationDescriptor = BindingContextUtils.extractVariableDescriptorFromReference(bindingContext, expression)
-            val sourceElement = declarationDescriptor?.source
-            if (sourceElement is KotlinSourceElement) {
-                val element = sourceElement.getPsi() ?: return null
-                if (nearest) {
-                    return DebuggerContextUtil.findNearest(context, element, element.containingFile)
+        val localReferenceExpression = codeFragment.getContentElement()
+        if (localReferenceExpression is KtSimpleNameExpression) {
+            analyze(localReferenceExpression) {
+                for (symbol in localReferenceExpression.mainReference.resolveToSymbols()) {
+                    if (symbol !is KtVariableLikeSymbol) {
+                        continue
+                    }
+
+                    val element = symbol.psi ?: continue
+
+                    return when {
+                        nearest -> DebuggerContextUtil.findNearest(context, element, element.containingFile)
+                        else -> SourcePosition.createFromOffset(element.containingFile, element.textOffset)
+                    }
                 }
-                return SourcePosition.createFromOffset(element.containingFile, element.textOffset)
             }
         }
 
         return null
-    }
-
-    private fun computeSourcePositionForDeclaration(
-        name: String,
-        declaringType: ReferenceType,
-        context: DebuggerContextImpl,
-        nearest: Boolean
-    ) = computeSourcePositionForDeclaration(declaringType, context, nearest) {
-        it.name == name
     }
 
     private fun computeSourcePositionForPropertyDeclaration(
@@ -132,30 +122,32 @@ class KotlinSourcePositionProvider : SourcePositionProvider() {
             return null
         }
 
-        return computeSourcePositionForDeclaration(fieldName, descriptor.field.declaringType(), context, nearest)
+        return computeSourcePositionForDeclaration(descriptor.field.declaringType(), context, nearest) {
+            it.name == fieldName
+        }
     }
 
     private fun computeSourcePosition(
         descriptor: GetterDescriptor,
         context: DebuggerContextImpl,
         nearest: Boolean
-    ): SourcePosition?  {
+    ): SourcePosition? {
         val name = descriptor.name
         val type = descriptor.getter.declaringType()
         computeSourcePositionForPropertyDeclaration(name, type, context, nearest)?.let { return it }
 
         if (type is ClassType) {
-            val interfaces = type.safeAllInterfaces().distinct()
-            for (intf in interfaces) {
-                computeSourcePositionForPropertyDeclaration(name, intf, context, nearest)?.let { return it }
+            val superInterfaceTypes = type.safeAllInterfaces().distinct()
+            for (interfaceType in superInterfaceTypes) {
+                computeSourcePositionForPropertyDeclaration(name, interfaceType, context, nearest)?.let { return it }
             }
         }
+
         return null
     }
 
     private fun findClassByType(project: Project, type: ReferenceType, context: DebuggerContextImpl): PsiElement? {
-        val session = context.debuggerSession
-        val scope = session?.searchScope ?: GlobalSearchScope.allScope(project)
+        val scope = context.debuggerSession?.searchScope ?: GlobalSearchScope.allScope(project)
         val className = JvmClassName.byInternalName(type.name()).fqNameForClassNameWithoutDollars.asString()
 
         val myClass = JavaPsiFacade.getInstance(project).findClass(className, scope)
@@ -168,22 +160,23 @@ class KotlinSourcePositionProvider : SourcePositionProvider() {
                 return element.getStrictParentOfType<KtClassOrObject>()
             }
         }
+
         return null
     }
 
     private fun getLastSourcePosition(type: ReferenceType, context: DebuggerContextImpl): SourcePosition? {
-        val debugProcess = context.debugProcess
-        if (debugProcess != null) {
-            try {
-                val locations = type.safeAllLineLocations()
-                if (locations.isNotEmpty()) {
-                    val lastLocation = locations[locations.size - 1]
-                    return debugProcess.positionManager.getSourcePosition(lastLocation)
-                }
-            } catch (ignored: AbsentInformationException) {
-            } catch (ignored: ClassNotPreparedException) {
+        val debugProcess = context.debugProcess ?: return null
+
+        try {
+            val locations = type.safeAllLineLocations()
+            if (locations.isNotEmpty()) {
+                val lastLocation = locations[locations.size - 1]
+                return debugProcess.positionManager.getSourcePosition(lastLocation)
             }
+        } catch (ignored: AbsentInformationException) {
+        } catch (ignored: ClassNotPreparedException) {
         }
+
         return null
     }
 }
