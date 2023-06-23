02509be6c85162f558b6ac7d10b0b588f2b7054a
Roman Golyshev <roman.golyshev@jetbrains.com>
2023-06-23 12:04:21 +0200

    KT-58161 [kotlin] Remove obsolete cast from `OperatorToFunctionIntention`
    Now there is only a single `resolveCall` overload, so there is no reason
    for the cast
    
    GitOrigin-RevId: 80166de513bc807419504bc73534260acd2b03fc

--- plugins/kotlin/code-insight/intentions-shared/src/org/jetbrains/kotlin/idea/codeInsight/intentions/shared/OperatorToFunctionIntention.kt
+++ plugins/kotlin/code-insight/intentions-shared/src/org/jetbrains/kotlin/idea/codeInsight/intentions/shared/OperatorToFunctionIntention.kt
@@ -68,7 +68,7 @@ class OperatorToFunctionIntention : AbstractKotlinApplicableIntention<KtExpressi
 
     context(KtAnalysisSession)
     private fun isImplicitInvokeFunctionCall(element: KtCallExpression): Boolean {
-        val functionCall = (element as KtElement).resolveCall()?.singleFunctionCallOrNull()
+        val functionCall = element.resolveCall()?.singleFunctionCallOrNull()
         return functionCall is KtSimpleFunctionCall && functionCall.isImplicitInvoke
     }
 
