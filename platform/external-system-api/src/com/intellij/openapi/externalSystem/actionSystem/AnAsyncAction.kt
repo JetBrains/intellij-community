5e9f9eb5470c1ff90c16163c9d1727af5214c933
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-06-23 12:35:24 +0200

    [maven] AnAsyncAction: use app/project coroutine scope
    GitOrigin-RevId: d8aeeeeb94b9f70c0598d4682befc71a92be2c2d

--- platform/external-system-api/src/com/intellij/openapi/externalSystem/actionSystem/AnAsyncAction.kt
+++ platform/external-system-api/src/com/intellij/openapi/externalSystem/actionSystem/AnAsyncAction.kt
@@ -3,15 +3,24 @@ package com.intellij.openapi.externalSystem.actionSystem
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.Service
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Experimental
 abstract class AnAsyncAction : AnAction() {
+  @Service
+  private class AnAsyncActionAppService(val coroutineScope: CoroutineScope)
+
+  @Service(Service.Level.PROJECT)
+  private class AnAsyncActionProjectService(val coroutineScope: CoroutineScope)
+
   override fun actionPerformed(e: AnActionEvent) {
-    val cs = CoroutineScope(SupervisorJob())
+    val project = e.project
+    val cs = project?.getService(AnAsyncActionProjectService::class.java)?.coroutineScope
+             ?: ApplicationManager.getApplication().getService(AnAsyncActionAppService::class.java).coroutineScope
     cs.launch { actionPerformedAsync(e) }
   }
 
