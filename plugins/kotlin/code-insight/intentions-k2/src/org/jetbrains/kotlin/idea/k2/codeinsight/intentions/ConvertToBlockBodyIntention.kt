f0f70b3fadf66603a632afcfbe68827e0a732e49
Toshiaki Kameyama <kmymtsak@gmail.com>
2023-06-23 11:05:16 +0900

    [kotlin] Introduce ConvertToBlockBodyUtils
    GitOrigin-RevId: af26715f6fbd9ef22f2d6ed4163a5f0d40e72f72

--- plugins/kotlin/code-insight/intentions-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/intentions/ConvertToBlockBodyIntention.kt
+++ plugins/kotlin/code-insight/intentions-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/intentions/ConvertToBlockBodyIntention.kt
@@ -4,158 +4,38 @@ package org.jetbrains.kotlin.idea.k2.codeinsight.intentions
 
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiWhiteSpace
 import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
-import org.jetbrains.kotlin.analysis.api.types.KtClassErrorType
 import org.jetbrains.kotlin.idea.base.analysis.api.utils.shortenReferences
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.AbstractKotlinApplicableIntentionWithContext
 import org.jetbrains.kotlin.idea.codeinsight.api.applicators.KotlinApplicabilityRange
-import org.jetbrains.kotlin.idea.codeinsight.utils.adjustLineIndent
+import org.jetbrains.kotlin.idea.codeinsight.utils.ConvertToBlockBodyContext
+import org.jetbrains.kotlin.idea.codeinsight.utils.ConvertToBlockBodyUtils
 import org.jetbrains.kotlin.idea.codeinsights.impl.base.applicators.ApplicabilityRanges
-import org.jetbrains.kotlin.name.ClassId
-import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.psi.psiUtil.endOffset
-import org.jetbrains.kotlin.psi.psiUtil.siblings
-import org.jetbrains.kotlin.psi.psiUtil.startOffset
-import org.jetbrains.kotlin.types.Variance
+import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.psi.KtDeclarationWithBody
 
 internal class ConvertToBlockBodyIntention :
-    AbstractKotlinApplicableIntentionWithContext<KtDeclarationWithBody, ConvertToBlockBodyIntention.Context>(KtDeclarationWithBody::class) {
-
-    class Context(
-        val returnTypeIsUnit: Boolean,
-        val returnTypeIsNothing: Boolean,
-        val returnTypeString: String,
-        val returnTypeClassId: ClassId?,
-        val bodyTypeIsUnit: Boolean,
-        val bodyTypeIsNothing: Boolean,
-        val reformat: Boolean,
-    )
+    AbstractKotlinApplicableIntentionWithContext<KtDeclarationWithBody, ConvertToBlockBodyContext>(KtDeclarationWithBody::class) {
 
     override fun getFamilyName(): String = KotlinBundle.message("convert.to.block.body")
-    override fun getActionName(element: KtDeclarationWithBody, context: Context): String = familyName
+
+    override fun getActionName(element: KtDeclarationWithBody, context: ConvertToBlockBodyContext): String = familyName
 
     override fun getApplicabilityRange(): KotlinApplicabilityRange<KtDeclarationWithBody> = ApplicabilityRanges.SELF
 
-    override fun isApplicableByPsi(element: KtDeclarationWithBody): Boolean =
-        (element is KtNamedFunction || element is KtPropertyAccessor) && !element.hasBlockBody() && element.hasBody()
+    override fun isApplicableByPsi(element: KtDeclarationWithBody) = ConvertToBlockBodyUtils.isConvertibleByPsi(element)
 
     context(KtAnalysisSession)
-    override fun prepareContext(element: KtDeclarationWithBody): Context? {
-        if (element is KtNamedFunction) {
-            val returnType = element.getReturnKtType()
-            if (!element.hasDeclaredReturnType() && returnType is KtClassErrorType) return null
-        }
-        return createContextForDeclaration(element, reformat = true)
+    override fun prepareContext(element: KtDeclarationWithBody): ConvertToBlockBodyContext? {
+        return ConvertToBlockBodyUtils.createContext(element, ::shortenReferences, reformat = true)
     }
 
-    override fun apply(element: KtDeclarationWithBody, context: Context, project: Project, editor: Editor?) {
-        val body = element.bodyExpression ?: return
-
-        val prevComments = body.comments(next = false)
-        val nextComments = body.comments(next = true)
-
-        val newBody = when (element) {
-            is KtNamedFunction -> {
-                if (!element.hasDeclaredReturnType() && !context.returnTypeIsUnit) {
-                    element.setType(context.returnTypeString, context.returnTypeClassId)
-                }
-                generateBody(
-                    body,
-                    prevComments,
-                    nextComments,
-                    context,
-                    returnsValue = !context.returnTypeIsUnit && !context.returnTypeIsNothing
-                )
-            }
-
-            is KtPropertyAccessor -> {
-                val parent = element.parent
-                if (parent is KtProperty && parent.typeReference == null) {
-                    parent.setType(context.returnTypeString, context.returnTypeClassId)
-                }
-
-                generateBody(body, prevComments, nextComments, context, element.isGetter)
-            }
-
-            else -> throw RuntimeException("Unknown declaration type: $element")
-        }
-
-        element.equalsToken?.delete()
-        prevComments.filterIsInstance<PsiComment>().forEach { it.delete() }
-        nextComments.forEach { it.delete() }
-        val replaced = body.replace(newBody)
-        if (context.reformat) element.containingKtFile.adjustLineIndent(replaced.startOffset, replaced.endOffset)
+    override fun apply(element: KtDeclarationWithBody, context: ConvertToBlockBodyContext, project: Project, editor: Editor?) {
+        ConvertToBlockBodyUtils.convert(element, context)
     }
 
     override fun skipProcessingFurtherElementsAfter(element: PsiElement) =
         element is KtDeclaration || super.skipProcessingFurtherElementsAfter(element)
 }
-
-private fun KtAnalysisSession.createContextForDeclaration(
-    declaration: KtDeclarationWithBody,
-    reformat: Boolean
-): ConvertToBlockBodyIntention.Context? {
-    val body = declaration.bodyExpression ?: return null
-    val returnType = declaration.getReturnKtType().approximateToSuperPublicDenotableOrSelf(approximateLocalTypes = true)
-    val bodyType = body.getKtType() ?: return null
-    return ConvertToBlockBodyIntention.Context(
-        returnType.isUnit,
-        returnType.isNothing,
-        returnType.render(position = Variance.OUT_VARIANCE),
-        returnType.expandedClassSymbol?.classIdIfNonLocal,
-        bodyType.isUnit,
-        bodyType.isNothing,
-        reformat,
-    )
-}
-
-private fun generateBody(
-    body: KtExpression,
-    prevComments: List<PsiElement>,
-    nextComments: List<PsiElement>,
-    context: ConvertToBlockBodyIntention.Context,
-    returnsValue: Boolean,
-): KtExpression {
-    val factory = KtPsiFactory(body.project)
-    if (context.bodyTypeIsUnit && body is KtNameReferenceExpression) return factory.createEmptyBody()
-    val needReturn = returnsValue && (!context.bodyTypeIsUnit && !context.bodyTypeIsNothing)
-    val newBody = if (needReturn) {
-        val annotatedExpr = body as? KtAnnotatedExpression
-        val returnedExpr = annotatedExpr?.baseExpression ?: body
-        val block = factory.createSingleStatementBlock(factory.createExpressionByPattern("return $0", returnedExpr))
-        val statement = block.firstStatement
-        annotatedExpr?.annotationEntries?.forEach {
-            block.addBefore(it, statement)
-            block.addBefore(factory.createNewLine(), statement)
-        }
-        block
-    } else {
-        factory.createSingleStatementBlock(body)
-    }
-    prevComments
-        .dropWhile { it is PsiWhiteSpace }
-        .forEach { newBody.addAfter(it, newBody.lBrace) }
-    nextComments
-        .reversed()
-        .forEach { newBody.addAfter(it, newBody.firstStatement) }
-    return newBody
-}
-
-private fun KtCallableDeclaration.setType(typeString: String, classId: ClassId?) {
-    val addedTypeReference = setTypeReference(KtPsiFactory(project).createType(typeString))
-    if (classId != null && addedTypeReference != null) {
-        shortenReferences(addedTypeReference)
-    }
-}
-
-private fun KtExpression.comments(next: Boolean): List<PsiElement> = siblings(forward = next, withItself = false)
-    .takeWhile { it is PsiWhiteSpace || it is PsiComment }
-    .takeIf { it.hasComment() }
-    .orEmpty()
-    .toList()
-
-private fun Sequence<PsiElement>.hasComment(): Boolean = any { it is PsiComment }
--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/ConvertToBlockBodyIntention.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/ConvertToBlockBodyIntention.kt
@@ -3,23 +3,19 @@
 package org.jetbrains.kotlin.idea.intentions
 
 import com.intellij.openapi.editor.Editor
-import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiWhiteSpace
 import org.jetbrains.kotlin.descriptors.CallableDescriptor
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.idea.caches.resolve.resolveToDescriptorIfAny
 import org.jetbrains.kotlin.idea.codeinsight.api.classic.intentions.SelfTargetingIntention
-import org.jetbrains.kotlin.idea.codeinsight.utils.adjustLineIndent
-import org.jetbrains.kotlin.idea.core.setType
+import org.jetbrains.kotlin.idea.codeinsight.utils.ConvertToBlockBodyContext
+import org.jetbrains.kotlin.idea.codeinsight.utils.ConvertToBlockBodyUtils
+import org.jetbrains.kotlin.idea.core.ShortenReferences
 import org.jetbrains.kotlin.idea.intentions.ConvertToBlockBodyIntention.Holder.convert
-import org.jetbrains.kotlin.idea.intentions.ConvertToBlockBodyIntention.Holder.returnType
-import org.jetbrains.kotlin.idea.util.resultingWhens
+import org.jetbrains.kotlin.idea.intentions.ConvertToBlockBodyIntention.Holder.createContext
+import org.jetbrains.kotlin.idea.util.IdeDescriptorRenderers
 import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.psi.psiUtil.endOffset
-import org.jetbrains.kotlin.psi.psiUtil.siblings
-import org.jetbrains.kotlin.psi.psiUtil.startOffset
 import org.jetbrains.kotlin.types.KotlinType
 import org.jetbrains.kotlin.types.isError
 import org.jetbrains.kotlin.types.isNullabilityFlexible
@@ -31,21 +27,7 @@ class ConvertToBlockBodyIntention : SelfTargetingIntention<KtDeclarationWithBody
     KtDeclarationWithBody::class.java,
     KotlinBundle.lazyMessage("convert.to.block.body")
 ) {
-    override fun isApplicableTo(element: KtDeclarationWithBody, caretOffset: Int): Boolean {
-        if (element is KtFunctionLiteral || element.hasBlockBody() || !element.hasBody()) return false
-
-        when (element) {
-            is KtNamedFunction -> {
-                val returnType = element.returnType() ?: return false
-                if (!element.hasDeclaredReturnType() && returnType.isError) return false// do not convert when type is implicit and unknown
-                return true
-            }
-
-            is KtPropertyAccessor -> return true
-
-            else -> error("Unknown declaration type: $element")
-        }
-    }
+    override fun isApplicableTo(element: KtDeclarationWithBody, caretOffset: Int) = createContext(element) != null
 
     override fun skipProcessingFurtherElementsAfter(element: PsiElement) =
         element is KtDeclaration || super.skipProcessingFurtherElementsAfter(element)
@@ -56,85 +38,47 @@ class ConvertToBlockBodyIntention : SelfTargetingIntention<KtDeclarationWithBody
 
     object Holder {
         fun convert(declaration: KtDeclarationWithBody, withReformat: Boolean = false): KtDeclarationWithBody {
-            val body = declaration.bodyExpression!!
-            val prevComments = body.comments(next = false)
-            val nextComments = body.comments(next = true)
-
-            fun generateBody(returnsValue: Boolean): KtExpression {
-                val bodyType = body.analyze().getType(body)
-                val psiFactory = KtPsiFactory(declaration.project)
-                if (bodyType != null && bodyType.isUnit() && body is KtNameReferenceExpression) return psiFactory.createEmptyBody()
-                val unitWhenAsResult = (bodyType == null || bodyType.isUnit()) && body.resultingWhens().isNotEmpty()
-                val needReturn = returnsValue && (bodyType == null || (!bodyType.isUnit() && !bodyType.isNothing()))
-                val newBody = if (needReturn || unitWhenAsResult) {
-                    val annotatedExpr = body as? KtAnnotatedExpression
-                    val returnedExpr = annotatedExpr?.baseExpression ?: body
-                    val block = psiFactory.createSingleStatementBlock(psiFactory.createExpressionByPattern("return $0", returnedExpr))
-                    val statement = block.firstStatement
-                    annotatedExpr?.annotationEntries?.forEach {
-                        block.addBefore(it, statement)
-                        block.addBefore(psiFactory.createNewLine(), statement)
-                    }
-                    block
-                } else {
-                    psiFactory.createSingleStatementBlock(body)
-                }
-                prevComments
-                    .dropWhile { it is PsiWhiteSpace }
-                    .forEach { newBody.addAfter(it, newBody.lBrace) }
-                nextComments
-                    .reversed()
-                    .forEach { newBody.addAfter(it, newBody.firstStatement) }
-                return newBody
-            }
-
-            val newBody = when (declaration) {
-                is KtNamedFunction -> {
-                    val returnType = declaration.returnType()!!
-                    if (!declaration.hasDeclaredReturnType() && !returnType.isUnit()) {
-                        declaration.setType(returnType)
-                    }
-                    generateBody(!returnType.isUnit() && !returnType.isNothing())
-                }
-
-                is KtPropertyAccessor -> {
-                    val parent = declaration.parent
-                    if (parent is KtProperty && parent.typeReference == null) {
-                        val descriptor = parent.resolveToDescriptorIfAny()
-                        (descriptor as? CallableDescriptor)?.returnType?.let { parent.setType(it) }
-                    }
-
-                    generateBody(declaration.isGetter)
-                }
-
-                else -> throw RuntimeException("Unknown declaration type: $declaration")
-            }
-
-            declaration.equalsToken!!.delete()
-            prevComments.filterIsInstance<PsiComment>().forEach { it.delete() }
-            nextComments.forEach { it.delete() }
-            val replaced = body.replace(newBody)
-            if (withReformat) {
-                declaration.containingKtFile.adjustLineIndent(replaced.startOffset, replaced.endOffset)
-            }
+            val context = createContext(declaration, withReformat) ?: return declaration
+            ConvertToBlockBodyUtils.convert(declaration, context)
             return declaration
         }
 
-        internal fun KtNamedFunction.returnType(): KotlinType? {
-            val descriptor = resolveToDescriptorIfAny()
-            val returnType = descriptor?.returnType ?: return null
-            if (returnType.isNullabilityFlexible()
-                && descriptor.overriddenDescriptors.firstOrNull()?.returnType?.isMarkedNullable == false
-            ) return returnType.makeNotNullable()
-            return returnType
+        fun createContext(declaration: KtDeclarationWithBody, reformat: Boolean = false): ConvertToBlockBodyContext? {
+            if (!ConvertToBlockBodyUtils.isConvertibleByPsi(declaration)) return null
+
+            val body = declaration.bodyExpression ?: return null
+
+            val returnType = declaration.returnType() ?: return null
+            if (returnType.isError && declaration is KtNamedFunction && !declaration.hasDeclaredReturnType()) return null
+
+            val bodyType = body.analyze().getType(body)
+
+            return ConvertToBlockBodyContext(
+                returnTypeIsUnit = returnType.isUnit(),
+                returnTypeIsNothing = returnType.isNothing(),
+                returnTypeString = IdeDescriptorRenderers.SOURCE_CODE.renderType(returnType),
+                bodyTypeIsUnit = bodyType?.isUnit() == true,
+                bodyTypeIsNothing = bodyType?.isNothing() == true,
+                reformat = reformat,
+                shortenReferences = { ShortenReferences.DEFAULT.process(it) }
+            )
         }
 
-        private fun KtExpression.comments(next: Boolean): List<PsiElement> = siblings(forward = next, withItself = false)
-            .takeWhile { it is PsiWhiteSpace || it is PsiComment }
-            .takeIf { it.hasComment() }
-            .orEmpty()
-            .toList()
-
-        private fun Sequence<PsiElement>.hasComment(): Boolean = any { it is PsiComment }
+        private fun KtDeclarationWithBody.returnType(): KotlinType? {
+            return when (this) {
+                is KtNamedFunction -> {
+                    val descriptor = resolveToDescriptorIfAny()
+                    val returnType = descriptor?.returnType ?: return null
+                    if (returnType.isNullabilityFlexible()
+                        && descriptor.overriddenDescriptors.firstOrNull()?.returnType?.isMarkedNullable == false
+                    ) returnType.makeNotNullable() else returnType
+                }
+                is KtPropertyAccessor -> {
+                    val parent = this.parent as? KtProperty
+                    (parent?.resolveToDescriptorIfAny() as? CallableDescriptor)?.returnType
+                }
+                else -> null
+            }
+        }
     }
 }
