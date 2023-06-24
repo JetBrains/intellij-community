0f9b87f0702ef2384e80813fe27d887ab8b38775
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-06-25 01:57:26 +0200

    rename to "fileOpened event executing"
    (cherry picked from commit 3041e9e28f7a56ade6f77648ae7891d723a48fe9)
    
    IJ-CR-109343
    
    GitOrigin-RevId: 55c5fc5d49197c30e5472e0fe3608238c8948a86

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/FileEditorManagerImpl.kt
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/FileEditorManagerImpl.kt
@@ -2107,7 +2107,7 @@ open class FileEditorManagerImpl(
 
       val publisher = project.messageBus.syncAndPreloadPublisher(FileEditorManagerListener.FILE_EDITOR_MANAGER)
       // must be executed with a current modality — that's why coroutineScope.launch should be not used
-      withContext(Dispatchers.EDT + CoroutineName("fileOpened handler execution")) {
+      subtask("fileOpened event executing", Dispatchers.EDT) {
         if (isFileOpen(file)) {
           runCatching {
             publisher.fileOpened(this@FileEditorManagerImpl, file)
