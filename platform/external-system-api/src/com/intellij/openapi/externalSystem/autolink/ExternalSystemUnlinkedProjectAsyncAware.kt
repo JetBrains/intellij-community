139c47a42033859a09934b839c0876b392ef0246
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-06-26 13:45:04 +0200

    [maven] non-blocking linkAndLoadProject
    fixes the error when called from UnlinkedProjectStartupActivity
    
    GitOrigin-RevId: 4551f3d1c9576058818d463fce35b04144c21e57

--- platform/external-system-api/src/com/intellij/openapi/externalSystem/autolink/ExternalSystemUnlinkedProjectAsyncAware.kt
+++ platform/external-system-api/src/com/intellij/openapi/externalSystem/autolink/ExternalSystemUnlinkedProjectAsyncAware.kt
@@ -1,17 +1,29 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.externalSystem.autolink
 
-import com.intellij.openapi.progress.runBlockingCancellable
+import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Experimental
 interface ExternalSystemUnlinkedProjectAsyncAware : ExternalSystemUnlinkedProjectAware {
+  @Service(Service.Level.PROJECT)
+  private class ExternalSystemUnlinkedProjectAsyncAwareService(val coroutineScope: CoroutineScope)
+
   suspend fun linkAndLoadProjectAsync(project: Project, externalProjectPath: String)
 
   // prefer linkAndLoadProjectAsync
   override fun linkAndLoadProject(project: Project, externalProjectPath: String) {
-    runBlockingCancellable { linkAndLoadProjectAsync(project, externalProjectPath) }
+    val cs = project.getService(ExternalSystemUnlinkedProjectAsyncAwareService::class.java).coroutineScope
+    cs.launch {
+      withContext(Dispatchers.Default) {
+        linkAndLoadProjectAsync(project, externalProjectPath)
+      }
+    }
   }
 
 }
