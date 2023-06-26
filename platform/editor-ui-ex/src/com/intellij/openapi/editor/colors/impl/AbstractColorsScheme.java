55bc09daae070eb04ec914fd1c4b6b6857657a48
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-06-26 18:09:30 +0200

    *EditorFontSizeGlobal actions must save changes to scheme
    (cherry picked from commit 2584deb04ce0f2f181beca5282052138f77ae37c)
    
    IJ-CR-109444
    
    GitOrigin-RevId: 02023b51f4c88f3247adb30af367c632172c75c8

--- platform/editor-ui-ex/src/com/intellij/openapi/editor/colors/impl/AbstractColorsScheme.java
+++ platform/editor-ui-ex/src/com/intellij/openapi/editor/colors/impl/AbstractColorsScheme.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.editor.colors.impl;
 
 import com.intellij.BundleBase;
@@ -223,6 +223,7 @@ public abstract class AbstractColorsScheme extends EditorFontCacheImpl implement
     fontSize = EditorFontsConstants.checkAndFixEditorFontSize(fontSize);
     ensureEditableFontPreferences().register(myFontPreferences.getFontFamily(), fontSize);
     initFonts();
+    setSaveNeeded(true);
   }
 
   @Override
