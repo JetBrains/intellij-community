46506ab96790b2865cbad9e482e618eda8d4536e
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2023-06-26 23:40:48 +0200

    [file-history] remove deprecated VcsAppendableHistorySessionPartner#finished method
    GitOrigin-RevId: 746d525dc0d1c63b3648b9fcddd5493f6e705236

--- platform/vcs-impl/src/com/intellij/openapi/vcs/history/VcsHistorySessionConsumer.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/history/VcsHistorySessionConsumer.java
@@ -1,7 +1,6 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vcs.history;
 
 public interface VcsHistorySessionConsumer extends VcsAppendableHistorySessionPartner {
-  @Override
   void finished();
 }
