aaf15f7bd9b0e84d226e3e31b603300b06bfb1ba
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-06-26 18:19:10 +0200

    ResetFontSizeAppInitListener - use appStarted event to make sure that we don't init LafManager on start
    (cherry picked from commit 499594374902d05d8e952b6719c74e3b2c1ca86b)
    
    IJ-CR-109444
    
    GitOrigin-RevId: d0f64e13b7007a265c19447ba43a75dadb364fc6

--- platform/platform-impl/src/com/intellij/openapi/editor/actions/ResetFontSizeAppInitListener.kt
+++ platform/platform-impl/src/com/intellij/openapi/editor/actions/ResetFontSizeAppInitListener.kt
@@ -1,26 +1,25 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.editor.actions
 
-import com.intellij.ide.ApplicationInitializedListener
+import com.intellij.ide.AppLifecycleListener
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.editor.colors.EditorColorsListener
 import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.editor.colors.EditorColorsScheme
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-private class ResetFontSizeAppInitListener : ApplicationInitializedListener {
-  override suspend fun execute(asyncScope: CoroutineScope) {
+private class ResetFontSizeAppInitListener : AppLifecycleListener {
+  override fun appStarted() {
     val app = ApplicationManager.getApplication()
     if (app.isHeadlessEnvironment || app.isUnitTestMode) {
       return
     }
 
-    asyncScope.launch {
-      resetFontSizeAppInitListener(force = true,
-                                   propertyManager = app.serviceAsync<PropertiesComponent>(),
+    @Suppress("DEPRECATION")
+    app.coroutineScope.launch {
+      resetFontSizeAppInitListener(propertyManager = app.serviceAsync<PropertiesComponent>(),
                                    editorColorManager = app.serviceAsync<EditorColorsManager>())
     }
   }
@@ -28,15 +27,13 @@ private class ResetFontSizeAppInitListener : ApplicationInitializedListener {
 
 private class ResetFontSizeEditorActionHandler : EditorColorsListener {
   override fun globalSchemeChange(scheme: EditorColorsScheme?) {
-    resetFontSizeAppInitListener(force = false,
-                                 propertyManager = PropertiesComponent.getInstance(),
-                                 editorColorManager = EditorColorsManager.getInstance())
+    resetFontSizeAppInitListener(propertyManager = PropertiesComponent.getInstance(), editorColorManager = EditorColorsManager.getInstance())
   }
 }
 
-private fun resetFontSizeAppInitListener(force: Boolean, propertyManager: PropertiesComponent, editorColorManager: EditorColorsManager) {
+private fun resetFontSizeAppInitListener(propertyManager: PropertiesComponent, editorColorManager: EditorColorsManager) {
   val globalScheme = editorColorManager.getGlobalScheme()
-  if (force || propertyManager.getValue(ResetFontSizeAction.PREVIOUS_COLOR_SCHEME, "") != globalScheme.getName()) {
+  if (propertyManager.getValue(ResetFontSizeAction.PREVIOUS_COLOR_SCHEME, "") != globalScheme.getName()) {
     propertyManager.setValue(ResetFontSizeAction.PREVIOUS_COLOR_SCHEME, globalScheme.getName())
     propertyManager.setValue(ResetFontSizeAction.UNSCALED_FONT_SIZE_TO_RESET_CONSOLE, globalScheme.consoleFontSize2D, -1f)
     propertyManager.setValue(ResetFontSizeAction.UNSCALED_FONT_SIZE_TO_RESET_EDITOR, globalScheme.editorFontSize2D, -1f)
