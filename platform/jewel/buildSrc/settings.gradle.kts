539df1e3d64c5c493146e187d09777aba705c2a8
Lamberto Basti <basti.lamberto@gmail.com>
2023-06-26 17:22:00 +0200

    Add typesafe project accessors and publish workflow
    Enable typesafe project accessors in settings.gradle.kts and add GitHub Actions workflow for publishing artifacts to Space.
    
    - Enabled feature preview for typesafe project accessors in buildSrc settings.gradle.kts
    - Added GitHub Actions workflow to publish artifacts in Space on release or push to main branch
    
    GitOrigin-RevId: af33fc0028cd05cf06548a27ad31232493c85c6d

---
+++ platform/jewel/buildSrc/settings.gradle.kts
@@ -0,0 +1,18 @@
+@file:Suppress("UnstableApiUsage")
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "buildSrc"
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
