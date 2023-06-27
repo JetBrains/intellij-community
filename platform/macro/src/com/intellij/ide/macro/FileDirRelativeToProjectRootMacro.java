ca0f1fd27a904b5d2738a081776dc5d92799064a
Evgeniy.Stepanov <Evgeniy.Stepanov@JetBrains.com>
2023-06-27 23:21:48 +0200

    ++
    (cherry picked from commit 8bdf2ad6b3a35b10b739f1368d7e3ae6aca8dcc2)
    
    IJ-CR-109592
    
    GitOrigin-RevId: 2ae591e8fb03a53d893b5ed46e8cfae39de0bfee

--- platform/macro/src/com/intellij/ide/macro/FileDirRelativeToProjectRootMacro.java
+++ platform/macro/src/com/intellij/ide/macro/FileDirRelativeToProjectRootMacro.java
@@ -21,6 +21,7 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.project.BaseProjectDirectories;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
@@ -55,10 +56,16 @@ public class FileDirRelativeToProjectRootMacro extends Macro {
       }
     }
 
-    final VirtualFile baseDirectory = BaseProjectDirectories.getInstance(project).getBaseDirectoryFor(file);
-    if (baseDirectory == null) {
-      return null;
+    VirtualFile contentRoot = ProjectRootManager.getInstance(project).getFileIndex().getContentRootForFile(file);
+    if (contentRoot != null && contentRoot.isDirectory()) {
+      return FileUtil.getRelativePath(getIOFile(contentRoot), getIOFile(file));
     }
-    return FileUtil.getRelativePath(getIOFile(baseDirectory), getIOFile(file));
+
+    final VirtualFile baseDirectory = BaseProjectDirectories.getInstance(project).getBaseDirectoryFor(file);
+    if (baseDirectory != null) {
+      return FileUtil.getRelativePath(getIOFile(baseDirectory), getIOFile(file));
+    }
+
+    return null;
   }
 }
