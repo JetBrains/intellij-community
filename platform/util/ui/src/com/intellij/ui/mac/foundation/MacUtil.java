2a263a6ab64613ff384f9e39be0795e2763ae899
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-06-27 20:17:33 +0200

    IDEA-323593 IDE UI gets unresponsive after opening another project in the current window until resize the window
    GitOrigin-RevId: e67a16183f99e5bf3f80993b5fd2b04e0ed20a96

--- platform/util/ui/src/com/intellij/ui/mac/foundation/MacUtil.java
+++ platform/util/ui/src/com/intellij/ui/mac/foundation/MacUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ui.mac.foundation;
 
 import com.intellij.openapi.Disposable;
@@ -127,6 +127,10 @@ public final class MacUtil {
     }
     catch (Throwable e) {
       LOG.debug(e);
+    }
+    finally {
+      // https://youtrack.jetbrains.com/issue/IDEA-323593
+      window.revalidate();
     }
   }
 
