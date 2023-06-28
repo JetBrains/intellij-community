bcca76fd3585cf3ed4d15c3a3f310034880e5f7b
Julia.Reshetnikova <Julia.Reshetnikova@TeamCity>
2023-06-28 16:39:14 +0000

    TeamCity change in 'Grazie / Mermaid' project: Versioned settings configuration updated
    GitOrigin-RevId: a7c7acde9a438e32236f8b5cf9c4e1488107e586

---
+++ plugins/mermaid/.teamcity/settings.kts
@@ -0,0 +1,28 @@
+import jetbrains.buildServer.configs.kotlin.*
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2023.05"
+
+project {
+}
