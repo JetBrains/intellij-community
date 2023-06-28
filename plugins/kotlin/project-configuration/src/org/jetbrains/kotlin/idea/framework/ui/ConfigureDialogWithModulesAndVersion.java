e27c94275454531757295eff3cc46aaf0cd1db6d
Victoria.Petrakovich <victoria.petrakovich@jetbrains.com>
2023-06-28 16:07:03 +0000

    [kotlin] Remove trailing line breaks from JVM target warning
    Merge-request: IJ-MR-109621
    Merged-by: Victoria Petrakovich <Victoria.Petrakovich@jetbrains.com>
    
    GitOrigin-RevId: 14f998566e8ad8359182db540a54a4b4995e4c78

--- plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/framework/ui/ConfigureDialogWithModulesAndVersion.java
+++ plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/framework/ui/ConfigureDialogWithModulesAndVersion.java
@@ -63,7 +63,6 @@ public class ConfigureDialogWithModulesAndVersion extends DialogWrapper {
     private JPanel infoPanel;
     private JTextPane listOfKotlinVersionsAndModules;
     private JTextPane deprecatedJvmTargetsUsedWarning;
-    private JTextPane textPaneForLink;
 
     private final AsyncProcessIcon processIcon = new AsyncProcessIcon("loader");
 
@@ -185,16 +184,14 @@ public class ConfigureDialogWithModulesAndVersion extends DialogWrapper {
                                                                                          jvmTargetVersion, modulesEnumeration.toString()));
                              }
                     );
+            message.append(KotlinProjectConfigurationBundle.message("configurator.kotlin.jvm.target.bump.manually.learn.more"));
+            Messages.installHyperlinkSupport(deprecatedJvmTargetsUsedWarning);
             // It's not hardcoded, we take strings from resources
             //noinspection HardCodedStringLiteral
             deprecatedJvmTargetsUsedWarning.setText(message.toString());
 
-            Messages.installHyperlinkSupport(textPaneForLink);
-            textPaneForLink.setText(KotlinProjectConfigurationBundle.message("configurator.kotlin.jvm.target.bump.manually.learn.more"));
-            textPaneForLink.setVisible(true);
             deprecatedJvmTargetsUsedWarning.setVisible(true);
         } else {
-            textPaneForLink.setVisible(false);
             deprecatedJvmTargetsUsedWarning.setVisible(false);
         }
     }
