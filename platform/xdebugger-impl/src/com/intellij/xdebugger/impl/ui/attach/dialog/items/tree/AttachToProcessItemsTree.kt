5b92ae63cff5a6f77eaaab56519b73b6229fc75e
Arseny.Chernyaev <arseny.chernyaev@jetbrains.com>
2023-06-28 20:17:23 +0200

    IDEA-323715: notify all AttachTreeModel listeners on filtration being applied
    GitOrigin-RevId: 5f8dd82196f2ffed0b69066193569f4c54c0ce6a

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/attach/dialog/items/tree/AttachToProcessItemsTree.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/attach/dialog/items/tree/AttachToProcessItemsTree.kt
@@ -121,7 +121,6 @@ internal class AttachToProcessItemsTree(
     refilterSaveSelection(filters) {
       getFilteringModel().refilter()
       TreeUtil.expandAll(this.tree)
-      updateUI()
     }
   }
 
@@ -154,6 +153,9 @@ internal class AttachTreeModel(private val rootNode: AttachTreeNodeWrapper, priv
   override fun setTree(tree: JTree?) {
   }
 
+  internal fun treeStructureChanged() {
+    super.treeStructureChanged(null, null, null)
+  }
 }
 
 internal suspend fun buildTree(
