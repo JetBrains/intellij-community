feb9c65cef22a97dc5238afec4857c009d3093bc
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-06-28 16:05:59 +0200

    [tests] using less clumsy assertions in `WslPathInfoTest` and making it more symlink-resistant
    GitOrigin-RevId: 1e3640fb94315d84fa6206056fb17b22238480e8

--- platform/platform-tests/testSrc/com/intellij/execution/wsl/WslPathInfoTest.kt
+++ platform/platform-tests/testSrc/com/intellij/execution/wsl/WslPathInfoTest.kt
@@ -1,15 +1,12 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.wsl
 
 import com.intellij.execution.target.TargetConfigurationWithLocalFsAccess
 import com.intellij.execution.target.readableFs.PathInfo.*
 import com.intellij.execution.wsl.target.WslTargetEnvironmentConfiguration
 import com.intellij.testFramework.fixtures.TestFixtureRule
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.anyOf
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNull
 import org.junit.ClassRule
 import org.junit.Test
 import org.junit.rules.RuleChain
@@ -31,11 +28,11 @@ class WslPathInfoTest {
   @Test
   fun testPathInfo() {
     val target = WslTargetEnvironmentConfiguration(wslRule.wsl)
-    assertNull("Path doesn't exist", target.getPathInfo("/path/doesn/exist"))
-    assertThat("Directory", target.getPathInfo("/bin"), anyOf(`is`(Unknown), `is`(Directory(false))))
-    assertThat("Directory", target.getPathInfo("/usr/bin"), anyOf(`is`(Unknown), `is`(Directory(false))))
-    assertThat("Executable file", target.getPathInfo("/bin/sh"), anyOf(`is`(Unknown), `is`(RegularFile(true))))
-    assertThat("Not executable file", target.getPathInfo("/etc/resolv.conf"), anyOf(`is`(Unknown), `is`(RegularFile(false))))
+    assertThat(target.getPathInfo("/path/doesn/exist")).isNull()
+    assertThat(target.getPathInfo("/bin")).isIn(Unknown, Directory(false))
+    assertThat(target.getPathInfo("/usr/bin")).isIn(Unknown, Directory(false))
+    assertThat(target.getPathInfo("/bin/ls") ?: target.getPathInfo("/usr/bin/ls")).isIn(Unknown, RegularFile(true))
+    assertThat(target.getPathInfo("/etc/resolv.conf")).isIn(Unknown, RegularFile(false))
     val emptyDir = wslTempDirRule.linuxPath
     assertEquals("Empty dir", Directory(true), target.getPathInfo(emptyDir))
   }
