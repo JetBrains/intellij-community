79183971a86f7d0d133b602ad7b74e197c1d72ea
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2023-06-28 17:58:17 +0300

    IDEA-323344 Define onboarding steps structure and implement onboarding executor
    GitOrigin-RevId: 396304560abc685f931d93bf4820f85bfbf1c616

---
+++ platform/new-ui-onboarding/src/com/intellij/ide/newUiOnboarding/NewUiOnboardingService.kt
@@ -0,0 +1,38 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide.newUiOnboarding
+
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@Service(Service.Level.PROJECT)
+internal class NewUiOnboardingService(private val project: Project, private val cs: CoroutineScope) {
+  fun startOnboarding() {
+    val steps = getSteps()
+    val executor = NewUiOnboardingExecutor(project, steps, cs, project)
+    cs.launch(Dispatchers.EDT) { executor.start() }
+  }
+
+  private fun getSteps(): List<NewUiOnboardingStep> {
+    // todo: add an ability to provide custom steps order (for other IDEs that want to customize the onboarding)
+    val stepIds = getDefaultStepsOrder()
+    val stepExtensions = NewUiOnboardingStep.EP_NAME.extensions
+    return stepIds.mapNotNull { id ->
+      val step = stepExtensions.find { it.key == id }?.instance
+      step?.takeIf { it.isAvailable() }
+    }
+  }
+
+  private fun getDefaultStepsOrder(): List<String> {
+    return emptyList()
+  }
+
+  companion object {
+    @JvmStatic
+    fun getInstance(project: Project): NewUiOnboardingService = project.service()
+  }
+}
---
+++ platform/new-ui-onboarding/src/com/intellij/ide/newUiOnboarding/NewUiOnboardingStep.kt
@@ -0,0 +1,18 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide.newUiOnboarding
+
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.project.Project
+import com.intellij.util.KeyedLazyInstanceEP
+import org.jetbrains.annotations.ApiStatus
+
+@ApiStatus.Internal
+interface NewUiOnboardingStep {
+  suspend fun performStep(project: Project): NewUiOnboardingStepData?
+
+  fun isAvailable(): Boolean = true
+
+  companion object {
+    val EP_NAME: ExtensionPointName<KeyedLazyInstanceEP<NewUiOnboardingStep>> = ExtensionPointName.create("com.intellij.ide.newUiOnboarding.step")
+  }
+}
