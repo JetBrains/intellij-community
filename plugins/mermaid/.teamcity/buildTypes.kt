4b17873adebc799a06471d35ad65e9904f2f3999
Ivan Posti <ivan.posti@jetbrains.com>
2023-06-28 20:31:50 +0200

    Fix formatting
    GitOrigin-RevId: 56cf59925ce4611f5def0ec2038cd5380335d025

--- plugins/mermaid/.teamcity/buildTypes.kt
+++ plugins/mermaid/.teamcity/buildTypes.kt
@@ -9,26 +9,24 @@ object Tests: MermaidBuild(
   script = "./gradlew test --info"
 )
 
-open class MermaidBuild(buildTypeName: String, dockerImageName: String, script: String) : BuildType({
-    name = buildTypeName
+open class MermaidBuild(buildTypeName: String, dockerImageName: String, script: String): BuildType({
+  name = buildTypeName
 
-    vcs {
-        root(Mermaid)
+  vcs {
+    root(Mermaid)
+  }
+  steps {
+    script {
+      scriptContent = script.trimIndent()
+      dockerImage = dockerImageName
+      dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
     }
-
-    steps {
-        script {
-            scriptContent = script.trimIndent()
-            dockerImage = dockerImageName
-            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-        }
+  }
+  features {
+    dockerSupport {
+      loginToRegistry = on {
+        dockerRegistryId = "PROJECT_EXT_3495"
+      }
     }
-
-    features {
-        dockerSupport {
-            loginToRegistry = on {
-                dockerRegistryId = "PROJECT_EXT_3495"
-            }
-        }
-    }
+  }
 })
