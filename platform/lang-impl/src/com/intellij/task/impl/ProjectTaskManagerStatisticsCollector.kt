999aa982f33963ecccd54e87ac544adc0b5e27c5
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2023-06-29 17:25:20 +0200

    IDEA-323982 add build originator to FUS statistics
    (cherry picked from commit 9031c45010971a2e8cabc0d24a911e6fe6aa4ad8)
    
    GitOrigin-RevId: 3ebef2b1ef77cc146d90b90c5d0e075de474a2ab

--- platform/lang-impl/src/com/intellij/task/impl/ProjectTaskManagerStatisticsCollector.kt
+++ platform/lang-impl/src/com/intellij/task/impl/ProjectTaskManagerStatisticsCollector.kt
@@ -1,13 +1,10 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.task.impl
 
 import com.intellij.internal.statistic.IdeActivityDefinition
 import com.intellij.internal.statistic.collectors.fus.ClassNameRuleValidator
 import com.intellij.internal.statistic.eventLog.EventLogGroup
-import com.intellij.internal.statistic.eventLog.events.BooleanEventField
-import com.intellij.internal.statistic.eventLog.events.EventFields
-import com.intellij.internal.statistic.eventLog.events.IntEventField
-import com.intellij.internal.statistic.eventLog.events.StringListEventField
+import com.intellij.internal.statistic.eventLog.events.*
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
 
 class ProjectTaskManagerStatisticsCollector : CounterUsagesCollector() {
@@ -24,11 +21,14 @@ class ProjectTaskManagerStatisticsCollector : CounterUsagesCollector() {
     val INCREMENTAL: BooleanEventField = EventFields.Boolean("incremental")
 
     @JvmField
+    val BUILD_ORIGINATOR: StringEventField = EventFields.StringValidatedByCustomRule("build_originator", ClassNameRuleValidator::class.java)
+
+    @JvmField
     val HAS_ERRORS: BooleanEventField = EventFields.Boolean("has_errors")
 
     @JvmField
     val BUILD_ACTIVITY: IdeActivityDefinition = GROUP.registerIdeActivity(null,
-                                                                          startEventAdditionalFields = arrayOf(TASK_RUNNER, EventFields.PluginInfo, MODULES, INCREMENTAL),
+                                                                          startEventAdditionalFields = arrayOf(TASK_RUNNER, EventFields.PluginInfo, MODULES, INCREMENTAL, BUILD_ORIGINATOR),
                                                                           finishEventAdditionalFields = arrayOf(HAS_ERRORS))
   }
 
--- platform/smart-update/src/com/intellij/smartUpdate/BuildProjectStep.kt
+++ platform/smart-update/src/com/intellij/smartUpdate/BuildProjectStep.kt
@@ -3,6 +3,7 @@ package com.intellij.smartUpdate
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
 import com.intellij.task.ProjectTaskManager
+import com.intellij.task.impl.ProjectTaskManagerImpl
 
 const val BUILD_PROJECT = "build.project"
 
@@ -11,6 +12,7 @@ class BuildProjectStep: SmartUpdateStep {
   override val stepName = SmartUpdateBundle.message("checkbox.build.project")
 
   override fun performUpdateStep(project: Project, e: AnActionEvent?, onSuccess: () -> Unit) {
+    ProjectTaskManagerImpl.putBuildOriginator(project, this.javaClass)
     ProjectTaskManager.getInstance(project).buildAllModules().onSuccess { onSuccess.invoke() }
   }
 }
