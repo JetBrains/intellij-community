98daf22e7c4b02b30caffabdc62c747b9cf179e1
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2023-06-29 04:42:49 +0400

    [kotlin] Move introduce utils to common refactoring module
    ^KTIJ-21135
    
    GitOrigin-RevId: 3d7eeffa6efccc3beb30da068516abda031d746b

--- plugins/kotlin/refactorings/kotlin.refactorings.common/src/org/jetbrains/kotlin/idea/refactoring/introduce/introduceUtils.kt
+++ plugins/kotlin/refactorings/kotlin.refactorings.common/src/org/jetbrains/kotlin/idea/refactoring/introduce/introduceUtils.kt
@@ -1,7 +1,5 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
-@file:JvmName("IntroduceUtils.kt")
-
 package org.jetbrains.kotlin.idea.refactoring.introduce
 
 import com.intellij.openapi.util.Key
@@ -16,31 +14,31 @@ import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
 import org.jetbrains.kotlin.psi.psiUtil.findDescendantOfType
 
 fun KtExpression.removeTemplateEntryBracesIfPossible(): KtExpression {
-  val parent = parent as? KtBlockStringTemplateEntry ?: return this
-  return parent.dropCurlyBracketsIfPossible().expression!!
+    val parent = parent as? KtBlockStringTemplateEntry ?: return this
+    return parent.dropCurlyBracketsIfPossible().expression!!
 }
 
 fun KtExpression.mustBeParenthesizedInInitializerPosition(): Boolean {
-  if (this !is KtBinaryExpression) return false
+    if (this !is KtBinaryExpression) return false
 
-  if (left?.mustBeParenthesizedInInitializerPosition() == true) return true
-  return PsiChildRange(left, operationReference).any { (it is PsiWhiteSpace) && it.textContains('\n') }
+    if (left?.mustBeParenthesizedInInitializerPosition() == true) return true
+    return PsiChildRange(left, operationReference).any { (it is PsiWhiteSpace) && it.textContains('\n') }
 }
 
 fun PsiElement.findExpressionByCopyableDataAndClearIt(key: Key<Boolean>): KtExpression? {
-  val result = findDescendantOfType<KtExpression> { it.getCopyableUserData(key) != null } ?: return null
-  result.putCopyableUserData(key, null)
-  return result
+    val result = findDescendantOfType<KtExpression> { it.getCopyableUserData(key) != null } ?: return null
+    result.putCopyableUserData(key, null)
+    return result
 }
 
 fun PsiElement.findElementByCopyableDataAndClearIt(key: Key<Boolean>): PsiElement? {
-  val result = findDescendantOfType<PsiElement> { it.getCopyableUserData(key) != null } ?: return null
-  result.putCopyableUserData(key, null)
-  return result
+    val result = findDescendantOfType<PsiElement> { it.getCopyableUserData(key) != null } ?: return null
+    result.putCopyableUserData(key, null)
+    return result
 }
 
 fun PsiElement.findExpressionsByCopyableDataAndClearIt(key: Key<Boolean>): List<KtExpression> {
-  val results = collectDescendantsOfType<KtExpression> { it.getCopyableUserData(key) != null }
-  results.forEach { it.putCopyableUserData(key, null) }
-  return results
+    val results = collectDescendantsOfType<KtExpression> { it.getCopyableUserData(key) != null }
+    results.forEach { it.putCopyableUserData(key, null) }
+    return results
 }
