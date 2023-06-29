0006ccb9850b5a50b74a8182c15dda5e2de63888
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-06-29 10:10:27 +0200

    Cleanup (formatting)
    GitOrigin-RevId: d1217270f41258179440bea517941523db4d0587

--- platform/platform-tests/testSrc/com/intellij/execution/wsl/WslPathBrowserTest.kt
+++ platform/platform-tests/testSrc/com/intellij/execution/wsl/WslPathBrowserTest.kt
@@ -4,7 +4,7 @@ package com.intellij.execution.wsl
 import com.intellij.execution.wsl.ui.createFileChooserDescriptor
 import com.intellij.execution.wsl.ui.getBestWindowsPathFromLinuxPath
 import com.intellij.testFramework.fixtures.TestFixtureRule
-import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.ClassRule
 import org.junit.Test
 import org.junit.rules.RuleChain
@@ -24,26 +24,22 @@ class WslPathBrowserTest {
 
   @Test
   fun correctPath() {
-    Assert.assertEquals("${WSLUtil.getUncPrefix()}${wslRule.wsl.msId}\\bin",
-                        getBestWindowsPathFromLinuxPath(wslRule.wsl, "/bin")!!.presentableUrl)
+    assertEquals("${WSLUtil.getUncPrefix()}${wslRule.wsl.msId}\\bin", getBestWindowsPathFromLinuxPath(wslRule.wsl, "/bin")!!.presentableUrl)
   }
 
   @Test
   fun bestPath() {
-    Assert.assertEquals("${WSLUtil.getUncPrefix()}${wslRule.wsl.msId}\\bin",
-                        getBestWindowsPathFromLinuxPath(wslRule.wsl, "/bin/foo/bur/buz")!!.presentableUrl)
+    assertEquals("${WSLUtil.getUncPrefix()}${wslRule.wsl.msId}\\bin", getBestWindowsPathFromLinuxPath(wslRule.wsl, "/bin/foo/bur/buz")!!.presentableUrl)
   }
 
   @Test
   fun wrongPath() {
-    Assert.assertEquals("${WSLUtil.getUncPrefix()}${wslRule.wsl.msId}\\",
-                        getBestWindowsPathFromLinuxPath(wslRule.wsl, "/something_that_simply_doesnt_exist")!!.presentableUrl)
+    assertEquals("${WSLUtil.getUncPrefix()}${wslRule.wsl.msId}\\", getBestWindowsPathFromLinuxPath(wslRule.wsl, "/something_that_simply_doesnt_exist")!!.presentableUrl)
   }
 
   @Test
   fun unparsablePath() {
-    val winPath = getBestWindowsPathFromLinuxPath(wslRule.wsl, "c:\\windows")
-    Assert.assertNull(winPath)
+    assertNull(getBestWindowsPathFromLinuxPath(wslRule.wsl, "c:\\windows"))
   }
 
   @Test
@@ -51,10 +47,10 @@ class WslPathBrowserTest {
     val rootDrives = listWindowsLocalDriveRoots()
     wslRule.wsl.executeOnWsl(20_000, "ls").exitCode // To reanimate wsl in case of failure
     val roots = createFileChooserDescriptor(wslRule.wsl, true).roots.map { it.toNioPath() }
-    Assert.assertEquals("Wrong number of roots: ${roots}, while root drives are: ${rootDrives}", rootDrives.count() + 1, roots.size)
+    assertEquals("Wrong number of roots: ${roots}, while root drives are: ${rootDrives}", rootDrives.count() + 1, roots.size)
     for (root in roots) {
       if (root != wslRule.wsl.getUNCRootPath() && root !in rootDrives) {
-        Assert.fail("Unexpected root $root")
+        fail("Unexpected root $root")
       }
     }
   }
@@ -62,6 +58,6 @@ class WslPathBrowserTest {
   @Test
   fun fileDescriptorNoWindows() {
     val roots = createFileChooserDescriptor(wslRule.wsl, false).roots.map { it.toNioPath() }
-    Assert.assertEquals(roots.toList(), listOf(wslRule.wsl.getUNCRootPath()))
+    assertEquals(roots.toList(), listOf(wslRule.wsl.getUNCRootPath()))
   }
 }
