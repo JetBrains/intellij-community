6c4d0cd5e1c58eed6e10a8a98bb150d22f6b74fc
Alexey Belkov <alexey.belkov@jetbrains.com>
2023-06-29 16:28:38 +0400

    [kotlin] J2K: Don't destructure regular variables
    It will lose the original variable name and may hurt code readability.
    
    ^KTIJ-21653 Fixed
    
    GitOrigin-RevId: 4325583db173588b6c25fcb489fec1e00789967c

--- plugins/kotlin/j2k/post-processing/src/org/jetbrains/kotlin/idea/j2k/post/processing/J2kPostProcessor.kt
+++ plugins/kotlin/j2k/post-processing/src/org/jetbrains/kotlin/idea/j2k/post/processing/J2kPostProcessor.kt
@@ -222,7 +222,10 @@ private val inspectionLikePostProcessingGroup =
         inspectionBasedProcessing(ReplaceGetOrSetInspection()),
         intentionBasedProcessing(ObjectLiteralToLambdaIntention(), writeActionNeeded = true),
         intentionBasedProcessing(RemoveUnnecessaryParenthesesIntention()),
-        intentionBasedProcessing(DestructureIntention(), writeActionNeeded = false),
+        intentionBasedProcessing(DestructureIntention(), writeActionNeeded = false) {
+            // Don't destructure regular variables, it will lose the original variable name and may hurt code readability
+            it.parent is KtForExpression
+        },
         inspectionBasedProcessing(SimplifyAssertNotNullInspection()),
         LiftReturnInspectionBasedProcessing(),
         LiftAssignmentInspectionBasedProcessing(),
