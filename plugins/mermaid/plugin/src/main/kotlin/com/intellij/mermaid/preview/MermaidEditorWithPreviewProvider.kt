cdc7554bb1bae21530d1b02944d48e69c15fbc4e
Ivan Posti <ivan.posti@jetbrains.com>
2023-06-29 16:45:15 +0200

    Don't try to create editor with preview if JCEF is not available
    GitOrigin-RevId: eb6f29f762e875f5b4b338e94672311bfc958be0

--- plugins/mermaid/plugin/src/main/kotlin/com/intellij/mermaid/preview/MermaidEditorWithPreviewProvider.kt
+++ plugins/mermaid/plugin/src/main/kotlin/com/intellij/mermaid/preview/MermaidEditorWithPreviewProvider.kt
@@ -1,14 +1,15 @@
 package com.intellij.mermaid.preview
 
-import com.intellij.mermaid.lang.MermaidFileType
+import com.intellij.mermaid.lang.isMermaidFile
 import com.intellij.openapi.fileEditor.*
 import com.intellij.openapi.fileEditor.impl.text.PsiAwareTextEditorProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.jcef.JBCefApp
 
 internal class MermaidEditorWithPreviewProvider: WeighedFileEditorProvider(), AsyncFileEditorProvider {
   override fun accept(project: Project, file: VirtualFile): Boolean {
-    return file.fileType == MermaidFileType
+    return file.isMermaidFile() && JBCefApp.isSupported()
   }
 
   override fun createEditor(project: Project, file: VirtualFile): FileEditor {
