d161fb24ad8bb777d665ea934505792616cb951d
Mikhail Sokolov <mikhail.sokolov@jetbrains.com>
2023-06-29 17:32:14 +0200

    IDEA-320087 Main toolbar: update hover area sizes
    GitOrigin-RevId: b47d5ab665c5b46b20255620d742566eb7374899

--- platform/util/ui/src/com/intellij/util/ui/JBUI.java
+++ platform/util/ui/src/com/intellij/util/ui/JBUI.java
@@ -1174,7 +1174,7 @@ public final class JBUI {
       public static final class Dropdown {
 
         @NotNull public static Insets borderInsets() {
-          return insets("MainToolbar.Dropdown.borderInsets", isNewUI() ? insets(5, 12, 5, 6) : insets(3, 5));
+          return insets("MainToolbar.Dropdown.borderInsets", isNewUI() ? insets(5, 10, 5, 6) : insets(3, 5));
         }
       }
 
