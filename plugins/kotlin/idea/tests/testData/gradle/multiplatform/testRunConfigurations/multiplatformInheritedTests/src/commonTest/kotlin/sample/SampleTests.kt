b6848601898fb574f27d1346ed4402e51418c13d
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2023-06-29 22:17:00 +0200

    [kotlin] Fix test method detection is a common code
    #KTIJ-26074 Fixed
    
    GitOrigin-RevId: 10a332d49fe6a7a0fa6eb991a4360bed466ba3b8

--- plugins/kotlin/idea/tests/testData/gradle/multiplatform/testRunConfigurations/multiplatformInheritedTests/src/commonTest/kotlin/sample/SampleTests.kt
+++ plugins/kotlin/idea/tests/testData/gradle/multiplatform/testRunConfigurations/multiplatformInheritedTests/src/commonTest/kotlin/sample/SampleTests.kt
@@ -3,6 +3,13 @@ package sample
 import kotlin.test.Test
 
 abstract class <lineMarker descr="Run Test" settings="null"><lineMarker descr="Is subclassed by ChildCommon1 (sample) ChildCommon2 (sample) Press ⌥⌘B to navigate">BaseTestCommon</lineMarker></lineMarker> {
+    private fun privateFunIsNotATest() {}
+
+    fun notATest() {}
+
+    @Test
+    private fun alsoNotATest() {}
+
     @Test
     fun <lineMarker descr="Run Test" settings="null">myTest</lineMarker>() {}
 }
--- plugins/kotlin/idea/tests/testData/gradle/multiplatform/testRunConfigurations/multiplatformInheritedTests/src/jvmTest/kotlin/sample/SampleTestsJVM.kt
+++ plugins/kotlin/idea/tests/testData/gradle/multiplatform/testRunConfigurations/multiplatformInheritedTests/src/jvmTest/kotlin/sample/SampleTestsJVM.kt
@@ -3,6 +3,13 @@ package sample
 import kotlin.test.Test
 
 abstract class <lineMarker descr="Run Test" settings="null"><lineMarker descr="Is subclassed by ChildTestJvm1 (sample) ChildTestJvm2 (sample) Press ⌥⌘B to navigate">BaseTestJvm</lineMarker></lineMarker> {
+    private fun privateFunIsNotATest() {}
+
+    fun notATest() {}
+
+    @Test
+    private fun alsoNotATest() {}
+
     @Test
     fun <lineMarker descr="Run Test" settings="null">myTest</lineMarker>() {}
 }
