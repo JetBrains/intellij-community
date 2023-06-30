a3228dd2102bcdf6b51ae3e2f585661523e1ae50
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2023-06-30 18:15:38 +0300

    [terminal] Zsh: make function names consistent with other shell integrations
    All visible functions should start with '__jetbrains_intellij'
    
    GitOrigin-RevId: c012ec2789d31276b5732f3266bf3110f7939e13

--- plugins/terminal/resources/zsh/hooks.zsh
+++ plugins/terminal/resources/zsh/hooks.zsh
@@ -1,7 +1,7 @@
 # Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 builtin autoload -Uz add-zsh-hook
 
-__intellij_encode() {
+__jetbrains_intellij_encode() {
   local out=''
   # Use LC_CTYPE=C to process text byte-by-byte and
   # LC_COLLATE=C to compare byte-for-byte. Ensure that
@@ -15,21 +15,21 @@ __intellij_encode() {
   builtin print -r "$out"
 }
 
-__intellij_encode_large() {
+__jetbrains_intellij_encode_large() {
   builtin local value="$1"
   if builtin whence od > /dev/null && builtin whence sed > /dev/null && builtin whence tr > /dev/null; then
     builtin echo -n "$value" | od -v -A n -t x1 | sed 's/ *//g' | tr -d '\n'
   else
-    __intellij_encode "$value"
+    __jetbrains_intellij_encode "$value"
   fi
 }
 
-__intellij_prompt_shown() {
+__jetbrains_intellij_prompt_shown() {
   builtin printf '\e]1341;prompt_shown\a'
 }
 
-__intellij_configure_prompt() {
-  PS1="%{$(__intellij_prompt_shown)%}"
+__jetbrains_intellij_configure_prompt() {
+  PS1="%{$(__jetbrains_intellij_prompt_shown)%}"
   # do not show right prompt
   builtin unset RPS1
   builtin unset RPROMPT
@@ -37,21 +37,24 @@ __intellij_configure_prompt() {
   builtin unsetopt ALWAYS_LAST_PROMPT
 }
 
-__intellij_command_preexec() {
+__jetbrains_intellij_command_preexec() {
   builtin local entered_command="$1"
   builtin local current_directory="$PWD"
-  builtin printf '\e]1341;command_started;command=%s;current_directory=%s\a' "$(__intellij_encode "${entered_command}")" "$(__intellij_encode "${current_directory}")"
+  builtin printf '\e]1341;command_started;command=%s;current_directory=%s\a' \
+    "$(__jetbrains_intellij_encode "${entered_command}")" \
+    "$(__jetbrains_intellij_encode "${current_directory}")"
 }
 
-__intellij_command_precmd() {
+__jetbrains_intellij_command_precmd() {
   builtin local LAST_EXIT_CODE="$?"
   builtin local current_directory="$PWD"
-  builtin printf '\e]1341;command_finished;exit_code=%s;current_directory=%s\a' "$LAST_EXIT_CODE" "$(__intellij_encode "${current_directory}")"
-  __intellij_configure_prompt
+  builtin printf '\e]1341;command_finished;exit_code=%s;current_directory=%s\a' \
+    "$LAST_EXIT_CODE" "$(__jetbrains_intellij_encode "${current_directory}")"
+  __jetbrains_intellij_configure_prompt
 }
 
-add-zsh-hook preexec __intellij_command_preexec
-add-zsh-hook precmd __intellij_command_precmd
+add-zsh-hook preexec __jetbrains_intellij_command_preexec
+add-zsh-hook precmd __jetbrains_intellij_command_precmd
 
 # Do not show "zsh: do you wish to see all <N> possibilities (<M> lines)?" question
 # when there are big number of completion items
@@ -60,9 +63,9 @@ LISTMAX=1000000
 # This script is sourced from inside a `precmd` hook, i.e. right before the first prompt.
 builtin printf '\e]1341;initialized\a'
 
-__intellij_configure_prompt
+__jetbrains_intellij_configure_prompt
 
 # `HISTFILE` is already initialized at this point.
 # Get all commands from history from the first command
 builtin local hist="$(builtin history 1)"
-builtin printf '\e]1341;command_history;history_string=%s\a' "$(__intellij_encode_large "${hist}")"
+builtin printf '\e]1341;command_history;history_string=%s\a' "$(__jetbrains_intellij_encode_large "${hist}")"
