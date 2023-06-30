725a375705d83cc480b5d1e4076c3d9e2e5004a7
Ivan Posti <ivan.posti@jetbrains.com>
2023-06-30 15:04:02 +0200

    Use fixed browser size in tests
    GitOrigin-RevId: ecb17e8ef29290c7ff3b7f99892c606fc48e0f3a

--- plugins/mermaid/plugin/src/main/kotlin/com/intellij/mermaid/preview/MermaidDiagramPreviewComponent.kt
+++ plugins/mermaid/plugin/src/main/kotlin/com/intellij/mermaid/preview/MermaidDiagramPreviewComponent.kt
@@ -3,13 +3,19 @@ package com.intellij.mermaid.preview
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.ui.jcef.JBCefApp
 import com.intellij.ui.jcef.JBCefBrowser
 import com.intellij.ui.jcef.JBCefBrowserBase
 import com.intellij.ui.jcef.JBCefClient
 import com.intellij.ui.jcef.JBCefJSQuery
+import com.intellij.ui.jcef.JBCefOSRHandlerFactory
+import com.intellij.util.Function
+import com.intellij.util.application
 import com.intellij.util.ui.components.BorderLayoutPanel
 import org.intellij.plugins.markdown.ui.preview.accessor.MarkdownLinkOpener
+import java.awt.Rectangle
+import javax.swing.JComponent
 
 class MermaidDiagramPreviewComponent(private val project: Project): BorderLayoutPanel(), Disposable {
   internal val browser = createBrowser()
@@ -52,6 +58,14 @@ class MermaidDiagramPreviewComponent(private val project: Project): BorderLayout
       setProperty(JBCefClient.Properties.JS_QUERY_POOL_SIZE, 20)
     }
     val builder = JBCefBrowser.createBuilder().apply {
+      // Use fixed resolution in tests
+      if (application.isUnitTestMode) {
+        setOSRHandlerFactory(object: JBCefOSRHandlerFactory {
+          override fun createScreenBoundsProvider(): Function<in JComponent, out Rectangle> {
+            return Function { Rectangle(1920, 1080) }
+          }
+        })
+      }
       setOffScreenRendering(true)
       setCreateImmediately(true)
       setClient(client)
