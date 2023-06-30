5e6ca4db122badd735f29dd99c84fb6d6d6d1ed9
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-06-30 22:08:15 +0200

    Revert "[platform] minor optimization in Windows launcher: trying to activate another instance before anything else"
    This reverts commit 7b58e567
    
    IDEA-324092
    
    (cherry-picked from commits ceae4363ffd48eab0f1a1e42a87fd744e4dc09d1, 4e27124719db4c1c574626e59150ca9fa972a020)
    
    GitOrigin-RevId: 247b1e8ba5cbc857c57373477f738799cf2565c9

--- native/WinLauncher/WinLauncher.cpp
+++ native/WinLauncher/WinLauncher.cpp
@@ -903,9 +903,6 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 
   if (!IsSupportedVersion()) return 1;
 
-  int exitCode = CheckSingleInstance();
-  if (exitCode != -1) return exitCode;
-
   std::string homeDir = GetHomeDir();
   if (!LocateJVM(homeDir)) return 1;
 
@@ -922,6 +919,10 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
   // ensures path variables are defined
   SetPathVariable(L"APPDATA", FOLDERID_RoamingAppData);
   SetPathVariable(L"LOCALAPPDATA", FOLDERID_LocalAppData);
+
+  //it's OK to return 0 here, because the control is transferred to the first instance
+  int exitCode = CheckSingleInstance();
+  if (exitCode != -1) return exitCode;
 
   // Read current directory and pass it to JVM through environment variable. The real current directory will be changed
   // in LoadJVMLibrary.
