855197a45f7b1dac2f90224fc85d7bbe5c0adf0e
Jaebaek Seo <jaebaek@google.com>
2023-06-30 13:47:46 -0700

    Add K2 KotlinCallHighlighterExtension
    This commit adds KotlinCallHighlighterExtension for K2 call highlighter.
    If we register an implementation of KotlinCallHighlighterExtension
    interface in XML, FunctionCallHighlighter will use it instead of the
    default call highlighter (the default call highlighter function is
    FunctionCallHighlighter::getTextAttributesForCall(call: KtCall)
    function).
    
    GitOrigin-RevId: ba69aff2c819887bd0d3b5cac761def9301d28ab

--- plugins/kotlin/util/test-generator-fir/test/org/jetbrains/kotlin/fir/testGenerator/GenerateK2HighlighterTests.kt
+++ plugins/kotlin/util/test-generator-fir/test/org/jetbrains/kotlin/fir/testGenerator/GenerateK2HighlighterTests.kt
@@ -3,6 +3,7 @@ package org.jetbrains.kotlin.fir.testGenerator
 
 import org.jetbrains.kotlin.idea.k2.highlighting.AbstractK2BundledCompilerPluginsHighlightingMetaInfoTest
 import org.jetbrains.kotlin.idea.k2.highlighting.AbstractK2HighlightingMetaInfoTest
+import org.jetbrains.kotlin.idea.k2.highlighting.AbstractK2HighlightingMetaInfoWithExtensionTest
 import org.jetbrains.kotlin.idea.k2.highlighting.AbstractOutsiderHighlightingTest
 import org.jetbrains.kotlin.testGenerator.model.*
 
@@ -14,6 +15,10 @@ internal fun MutableTWorkspace.generateK2HighlighterTests() {
 
         testClass<AbstractK2BundledCompilerPluginsHighlightingMetaInfoTest> {
             model("highlighterMetaInfoWithBundledCompilerPlugins")
+        }
+
+        testClass<AbstractK2HighlightingMetaInfoWithExtensionTest> {
+            model("highlighterMetaInfoWithExtension")
         }
     }
 
