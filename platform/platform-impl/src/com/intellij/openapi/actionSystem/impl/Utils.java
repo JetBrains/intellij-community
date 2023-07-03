d807163fd2fd24ea3fea1b58dca4bc54a58e0dd3
Artem Bukhonov <artem.bukhonov@jetbrains.com>
2023-07-03 21:52:02 +0200

    [rdct] Use conditional fast track expand for thin client in expandActionGroupAsync
    (cherry picked from commit a94a385b93e28e85a0cb1f4a52221cb481bf9133)
    
    IJ-MR-110039
    
    GitOrigin-RevId: d9c867431385268fa9045ab8b511bb06813e7464

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/Utils.java
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/Utils.java
@@ -166,9 +166,11 @@ public final class Utils {
                                                                           boolean fastTrack) {
     LOG.assertTrue(isAsyncDataContext(context), "Async data context required in '" + place + "': " + dumpDataContextClass(context));
     ActionUpdater updater = new ActionUpdater(presentationFactory, context, place, ActionPlaces.isPopupPlace(place), isToolbarAction);
-    List<AnAction> actions = !fastTrack ? null : expandActionGroupFastTrack(place, updater, group, group instanceof CompactActionGroup, null);
-    if (actions != null) {
-      return Promises.resolvedCancellablePromise(actions);
+    if (ActionGroupExpander.getInstance().allowsFastUpdate(CommonDataKeys.PROJECT.getData(context), group, place)) {
+      List<AnAction> actions = !fastTrack ? null : expandActionGroupFastTrack(place, updater, group, group instanceof CompactActionGroup, null);
+      if (actions != null) {
+        return Promises.resolvedCancellablePromise(actions);
+      }
     }
     return updater.expandActionGroupAsync(group, group instanceof CompactActionGroup);
   }
@@ -231,7 +233,7 @@ public final class Utils {
       if (isContextMenu) {
         ActionUpdater.cancelAllUpdates("context menu requested");
       }
-      if (expander.allowsFastUpdate(project, place) && !Registry.is("actionSystem.update.actions.suppress.dataRules.on.edt")) {
+      if (expander.allowsFastUpdate(project, group, place) && !Registry.is("actionSystem.update.actions.suppress.dataRules.on.edt")) {
         Set<String> missedKeys = new HashSet<>();
         list = expandActionGroupFastTrack(place, updater, group, group instanceof CompactActionGroup, missedKeys::add);
         if (list != null && missedKeys.isEmpty()) {
