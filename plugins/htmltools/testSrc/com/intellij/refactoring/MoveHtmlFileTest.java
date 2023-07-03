00adcdf1205365546b9b117ccb790047225f5b64
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-07-03 14:39:09 +0200

    [model-branch] Remove ModelBranch tests
    IDEA-309887 Drop ModelBranch-es from IntelliJ repo
    
    GitOrigin-RevId: 362b2433ce92b95788d626c444b23e4efbf3e6c7

--- plugins/htmltools/testSrc/com/intellij/refactoring/MoveHtmlFileTest.java
+++ plugins/htmltools/testSrc/com/intellij/refactoring/MoveHtmlFileTest.java
@@ -1,7 +1,6 @@
 package com.intellij.refactoring;
 
 import com.intellij.openapi.application.PathManager;
-import com.intellij.openapi.util.registry.Registry;
 import org.jetbrains.annotations.NotNull;
 
 public class MoveHtmlFileTest extends MoveFileTestCase {
@@ -11,12 +10,4 @@ public class MoveHtmlFileTest extends MoveFileTestCase {
   }
 
   public void testHtml() { doTest("toDir", "B.html"); }
-
-  public static class BranchTest extends MoveHtmlFileTest {
-    @Override
-    protected void setUp() throws Exception {
-      super.setUp();
-      Registry.get("run.refactorings.in.model.branch").setValue(true, getTestRootDisposable());
-    }
-  }
 }
