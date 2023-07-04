014b89199ba24351b83987f1fd21a892881451e9
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-07-04 20:29:27 +0200

    [java-highlighting] IDEA-324307 Allow to use qualified name for enum in switch. Check duplicates
    GitOrigin-RevId: 7bfce5172e1e6f398f01063449eaaeeb2d77f1c9

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/switchExpressions/QualifiedEnumInSwitch.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/switchExpressions/QualifiedEnumInSwitch.java
@@ -2,6 +2,16 @@ public class QualifiedEnumInSwitch {
   sealed interface IT{}
   enum T implements IT {A, B, C,}
 
+
+  void f(T e1) {
+    switch (e1) {
+      case <error descr="Duplicate label 'A'">T.A</error>, T.B:
+        break;
+      case <error descr="Duplicate label 'A'">A</error>:
+      default: break;
+    }
+  }
+
   public int test2(T t2) {
     switch (t2) {
       case T.A -> System.out.println(1);
