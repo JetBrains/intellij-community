c9a85f8431e9a70c0c7ea1856c5331e3bd95362e
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-07-04 18:27:03 +0200

    [statistics] IDEA-324321 Add ServerSide and Vulnerabilities tabs to usage statistics of Problems View
    (cherry picked from commit 374b8ace99c8e2b7f9c1ef66b05b0915b145e0b1)
    IJ-CR-110152
    
    GitOrigin-RevId: cfc2a7cccc63b753241a440676a46eddae823ed8

--- platform/lang-impl/src/com/intellij/analysis/problemsView/toolWindow/ProblemsViewTab.kt
+++ platform/lang-impl/src/com/intellij/analysis/problemsView/toolWindow/ProblemsViewTab.kt
@@ -1,5 +1,6 @@
 package com.intellij.analysis.problemsView.toolWindow
 
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsContexts
 import com.intellij.ui.content.Content
 import org.jetbrains.annotations.ApiStatus
@@ -23,5 +24,16 @@ interface ProblemsViewTab {
 
   @ApiStatus.Internal
   fun customizeTabContent(content: Content) {
+  }
+}
+
+@ApiStatus.Internal
+object ProblemsViewTabUsages {
+  fun logTabShown(project: Project, tabName: String, problemsCount: Int) {
+    ProblemsViewStatsCollector.logTabShown(project, tabName, 0)
+  }
+
+  fun logTabHidden(project: Project, tabName: String, problemsCount: Int, durationNano: Long) {
+    ProblemsViewStatsCollector.logTabHidden(project, tabName, 0, durationNano)
   }
 }
