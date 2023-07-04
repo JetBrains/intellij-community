b4d104dfda4cea0b566340abf62673e8994fcead
Alexey Belkov <alexey.belkov@jetbrains.com>
2023-07-04 10:38:25 +0400

    [kotlin] shortenReferencesInRange: proofread KDoc
    GitOrigin-RevId: e8ea98f4261c8f59bd85f1053b437f6fdf8d92cd

--- plugins/kotlin/base/analysis-api/analysis-api-utils/src/org/jetbrains/kotlin/idea/base/analysis/api/utils/shortenUtils.kt
+++ plugins/kotlin/base/analysis-api/analysis-api-utils/src/org/jetbrains/kotlin/idea/base/analysis/api/utils/shortenUtils.kt
@@ -2,17 +2,16 @@
 
 package org.jetbrains.kotlin.idea.base.analysis.api.utils
 
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.util.TextRange
+import org.jetbrains.kotlin.analysis.api.KtAllowAnalysisOnEdt
 import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.components.ShortenCommand
 import org.jetbrains.kotlin.analysis.api.components.ShortenOption
 import org.jetbrains.kotlin.analysis.api.components.ShortenOption.Companion.defaultCallableShortenOption
 import org.jetbrains.kotlin.analysis.api.components.ShortenOption.Companion.defaultClassShortenOption
+import org.jetbrains.kotlin.analysis.api.lifetime.allowAnalysisOnEdt
 import org.jetbrains.kotlin.analysis.api.symbols.KtCallableSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KtClassLikeSymbol
-import org.jetbrains.kotlin.analysis.api.KtAllowAnalysisOnEdt
-import org.jetbrains.kotlin.analysis.api.components.ShortenCommand
-import org.jetbrains.kotlin.analysis.api.lifetime.allowAnalysisOnEdt
 import org.jetbrains.kotlin.idea.base.psi.imports.addImport
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
 import org.jetbrains.kotlin.psi.KtElement
@@ -34,11 +33,14 @@ fun shortenReferences(
 )
 
 /**
- * Shorten references in the given [file] and [range]. The function must be invoked on EDT thread because it modifies the underlying
- * PSI. This method analyze Kotlin code and hence could block the EDT thread for longer period of time. Hence, this method should be
- * called only to shorten references in *newly generated code* by IDE actions. In other cases, please consider using
+ * Shorten references in the given [file] and [range].
+ *
+ * This function must be invoked on the EDT thread because it modifies the underlying PSI. It analyzes Kotlin code
+ * and hence could block the EDT thread for a long period of time. Therefore, this function should be called only to shorten references
+ * in *newly generated code* by IDE actions. In other cases, please consider using
  * [org.jetbrains.kotlin.analysis.api.components.KtReferenceShortenerMixIn] in a background thread to perform the analysis and then
- * modify PSI on the EDt thread by invoking [org.jetbrains.kotlin.analysis.api.components.ShortenCommand.invokeShortening]. */
+ * modify PSI on the EDT thread by invoking [org.jetbrains.kotlin.analysis.api.components.ShortenCommand.invokeShortening].
+ */
 @OptIn(KtAllowAnalysisOnEdt::class)
 fun shortenReferencesInRange(
     file: KtFile,
@@ -46,14 +48,13 @@ fun shortenReferencesInRange(
     classShortenOption: (KtClassLikeSymbol) -> ShortenOption = defaultClassShortenOption,
     callableShortenOption: (KtCallableSymbol) -> ShortenOption = defaultCallableShortenOption
 ) {
-     val shortenCommand = allowAnalysisOnEdt {
+    val shortenCommand = allowAnalysisOnEdt {
         analyze(file) {
             collectPossibleReferenceShortenings(file, range, classShortenOption, callableShortenOption)
         }
     }
     shortenCommand.invokeShortening()
 }
-
 
 /**
  * Shortens the references specified in [ShortenCommand] and inserts needed imports
@@ -86,6 +87,7 @@ fun ShortenCommand.invokeShortening(): List<KtElement> {
     //        }
     return shorteningResults
 }
+
 private fun KtDotQualifiedExpression.deleteQualifier(): KtExpression? {
     val selectorExpression = selectorExpression ?: return null
     return this.replace(selectorExpression) as KtExpression
