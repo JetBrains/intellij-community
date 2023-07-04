0773be922f456ce583a889c2434c88e3a7ca153d
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-07-04 14:29:23 +0200

    [ui] using the same ID in VFS refresh widget (IDEA-323620)
    GitOrigin-RevId: 2727f23c1aa40be5eb1e4070433406e75cc96b9a

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/status/VfsRefreshIndicatorWidgetFactory.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/status/VfsRefreshIndicatorWidgetFactory.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.wm.impl.status;
 
 import com.intellij.ide.ui.UISettings;
@@ -22,7 +22,7 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 
 public final class VfsRefreshIndicatorWidgetFactory implements StatusBarWidgetFactory {
-  private static final String ID = "VFS_REFRESH";
+  private static final String ID = "VfsRefresh";
 
   private volatile boolean myAvailable;
 
