5e1827ec2c1509e57ad8cb8c1e1c7df598c9cb2c
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2023-07-04 16:53:37 +0200

    [kotlin] Simplify `AbstractK2BindToElementTest`
    GitOrigin-RevId: 149f621efeb3c2c7392a89a7db709c7997d8fa49

--- plugins/kotlin/refactorings/kotlin.refactorings.tests.k2/test/org/jetbrains/kotlin/idea/k2/refactoring/safeDelete/AbstractK2BindToElementTest.kt
+++ plugins/kotlin/refactorings/kotlin.refactorings.tests.k2/test/org/jetbrains/kotlin/idea/k2/refactoring/safeDelete/AbstractK2BindToElementTest.kt
@@ -3,7 +3,6 @@ package org.jetbrains.kotlin.idea.k2.refactoring.safeDelete
 
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.util.parentOfType
@@ -16,7 +15,6 @@ import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
 import org.jetbrains.kotlin.idea.test.ProjectDescriptorWithStdlibSources
 import org.jetbrains.kotlin.idea.util.application.executeWriteCommand
 import org.jetbrains.kotlin.psi.KtSimpleNameExpression
-import java.nio.file.Path
 
 abstract class AbstractK2BindToElementTest : KotlinLightCodeInsightFixtureTestCase() {
     override fun isFirPlugin() = true
@@ -25,8 +23,8 @@ abstract class AbstractK2BindToElementTest : KotlinLightCodeInsightFixtureTestCa
 
     @OptIn(KtAllowAnalysisOnEdt::class)
     protected fun doTest(path: String) {
+        myFixture.copyDirectoryToProject("", "")
         val file = myFixture.configureByFile(path)
-        addAllFilesToProject(Path.of(path).parent)
         val elem = file.findElementAt(myFixture.caretOffset) ?: error("Couldn't find element at caret")
         val nameReference = elem.parentOfType<KtSimpleNameExpression>(withSelf = true)
             ?: error("Element at caret isn't of type 'KtSimpleNameExpression'")
@@ -37,13 +35,6 @@ abstract class AbstractK2BindToElementTest : KotlinLightCodeInsightFixtureTestCa
             }
         }
         myFixture.checkResultByFile("${myFixture.file.name}.after")
-    }
-
-    private fun addAllFilesToProject(rootDir: Path) {
-        rootDir.toFile().listFiles()?.forEach {
-            if (it.isDirectory) myFixture.copyDirectoryToProject(it.name, it.name)
-        }
-        PsiDocumentManager.getInstance(myFixture.project).commitAllDocuments()
     }
 
     private fun findElementToBind(): PsiElement? {
