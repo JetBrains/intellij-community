ecbd45768a3d7651b69d74c80fb24d4790d60d5a
Konstantin Bulenkov <kb@jetbrains.com>
2023-07-05 15:52:25 +0200

    Make project icons 16x16 in Project Widget popup
    GitOrigin-RevId: 5af5b4934ed7aac578d1b6681a6acd8008e78121

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/headertoolbar/ProjectToolbarWidgetAction.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/headertoolbar/ProjectToolbarWidgetAction.kt
@@ -169,7 +169,7 @@ private class ProjectWidgetRenderer : ListCellRenderer<PopupFactoryImpl.ActionIt
     val content = panel {
       customizeSpacingConfiguration(EmptySpacingConfiguration()) {
         row {
-          icon(RecentProjectsManagerBase.getInstanceEx().getProjectIcon(projectPath, true))
+          icon(RecentProjectsManagerBase.getInstanceEx().getProjectIcon(projectPath, true, 16))
             .align(AlignY.TOP)
             .customize(UnscaledGaps(right = 8))
 
