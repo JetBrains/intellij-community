764d94b894dc6ac17381abd0e2ecb62621bcc971
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-07-05 11:39:38 +0200

    Refactor ToolWindowFactory from Java to Kotlin
    GitOrigin-RevId: 836afa6c46716b0f64d4de8037113b7cc529bdda

--- platform/platform-api/src/com/intellij/openapi/wm/ToolWindowFactory.kt
+++ platform/platform-api/src/com/intellij/openapi/wm/ToolWindowFactory.kt
@@ -1,61 +1,49 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.wm;
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.wm
 
-import com.intellij.openapi.project.Project;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
+import com.intellij.openapi.project.Project
+import org.jetbrains.annotations.ApiStatus
+import javax.swing.Icon
 
 /**
- * Performs lazy initialization of a tool window registered in {@code plugin.xml}.
- * Please implement {@link com.intellij.openapi.project.DumbAware} marker interface to indicate that the tool window content should be
+ * Performs lazy initialization of a tool window registered in `plugin.xml`.
+ * Please implement [com.intellij.openapi.project.DumbAware] marker interface to indicate that the tool window content should be
  * available during the indexing process.
- * <p/>
- * To localize tool window stripe title, add key {@code toolwindow.stripe.yourToolWindowId.replace(" ", "_")} to plugin's resource bundle.
- * <p/>
- * See <a href="https://www.jetbrains.org/intellij/sdk/docs/user_interface_components/tool_windows.html">Tool Windows</a> in SDK Docs.
+ *
+ * To localizing tool window stripe title, add key `toolwindow.stripe.yourToolWindowId.replace(" ", "_")` to plugin's resource bundle.
+ *
+ * See [Tool Windows](https://www.jetbrains.org/intellij/sdk/docs/user_interface_components/tool_windows.html) in SDK Docs.
  */
-public interface ToolWindowFactory {
-  default boolean isApplicable(@NotNull Project project) {
-    return true;
-  }
+interface ToolWindowFactory {
+  fun isApplicable(project: Project): Boolean = true
 
-  void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow);
+  fun createToolWindowContent(project: Project, toolWindow: ToolWindow)
 
   /**
    * Perform additional initialization routine here.
    */
-  default void init(@NotNull ToolWindow toolWindow) {}
+  fun init(toolWindow: ToolWindow) {}
 
   /**
    * Check if tool window (and its stripe button) should be visible after startup.
    *
-   * @see ToolWindow#isAvailable()
+   * @see ToolWindow.isAvailable
    */
-  default boolean shouldBeAvailable(@NotNull Project project) {
-    return true;
-  }
+  fun shouldBeAvailable(project: Project): Boolean = true
 
-  /**
-   * @deprecated Use {@link ToolWindowEP#isDoNotActivateOnStart}
-   */
-  @Deprecated(forRemoval = true)
-  default boolean isDoNotActivateOnStart() {
-    return false;
-  }
+  @Suppress("DeprecatedCallableAddReplaceWith")
+  @get:Deprecated("Use {@link ToolWindowEP#isDoNotActivateOnStart}")
+  val isDoNotActivateOnStart: Boolean
+    get() = false
 
-  /**
-   * Return custom anchor or null to use anchor defined in Tool Window Registration or customized by user.
-   */
-  @ApiStatus.Internal
-  default @Nullable ToolWindowAnchor getAnchor() {
-    return null;
-  }
+  @get:ApiStatus.Internal
+  val anchor: ToolWindowAnchor?
+    /**
+     * Return custom anchor or null to use anchor defined in Tool Window Registration or customized by user.
+     */
+    get() = null
 
-  @ApiStatus.Internal
-  default @Nullable Icon getIcon() {
-    return null;
-  }
+  @get:ApiStatus.Internal
+  val icon: Icon?
+    get() = null
 }
