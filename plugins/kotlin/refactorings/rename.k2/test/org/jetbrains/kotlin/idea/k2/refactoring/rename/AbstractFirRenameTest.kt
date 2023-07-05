55e0704313c8871bcf14b8c31c8070fc63c44f0a
Anna Kozlova <anna.kozlova@jetbrains.com>
2023-07-05 12:01:09 +0200

    [kotlin] k2 rename: fix tests
    GitOrigin-RevId: 83277bb7f6d8e85559b0f2ccb8bcddc16a18b145

--- plugins/kotlin/refactorings/rename.k2/test/org/jetbrains/kotlin/idea/k2/refactoring/rename/AbstractFirRenameTest.kt
+++ plugins/kotlin/refactorings/rename.k2/test/org/jetbrains/kotlin/idea/k2/refactoring/rename/AbstractFirRenameTest.kt
@@ -7,12 +7,15 @@ import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.lifetime.allowAnalysisOnEdt
 import org.jetbrains.kotlin.analysis.api.symbols.KtFunctionSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KtPropertySymbol
+import org.jetbrains.kotlin.analysis.api.symbols.markers.KtPossiblyNamedSymbol
+import org.jetbrains.kotlin.analysis.api.symbols.markers.KtSymbolWithMembers
 import org.jetbrains.kotlin.idea.fir.invalidateCaches
 import org.jetbrains.kotlin.idea.refactoring.rename.AbstractRenameTest
 import org.jetbrains.kotlin.idea.refactoring.rename.loadTestConfiguration
 import org.jetbrains.kotlin.idea.test.KotlinLightProjectDescriptor
 import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
 import org.jetbrains.kotlin.idea.test.runAll
+import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.psi.KtFile
 
 abstract class AbstractFirRenameTest : AbstractRenameTest() {
@@ -53,13 +56,18 @@ abstract class AbstractFirRenameTest : AbstractRenameTest() {
     @OptIn(KtAllowAnalysisOnEdt::class)
     override fun findPsiDeclarationToRename(contextFile: KtFile, target: KotlinTarget): PsiElement = allowAnalysisOnEdt {
         analyze(contextFile) {
+            fun getContainingMemberSymbol(classId: ClassId): KtSymbolWithMembers {
+                getClassOrObjectSymbolByClassId(classId)?.let { return it }
+                val parentSymbol = getClassOrObjectSymbolByClassId(classId.parentClassId!!)!!
+                return parentSymbol.getDeclaredMemberScope().getAllSymbols().first { (it as? KtPossiblyNamedSymbol)?.name == classId.shortClassName } as KtSymbolWithMembers
+            }
+
             when (target) {
-                is KotlinTarget.Classifier -> getClassOrObjectSymbolByClassId(target.classId)?.psi!!
+                is KotlinTarget.Classifier -> getContainingMemberSymbol(target.classId).psi!!
                 is KotlinTarget.Callable -> {
                     val callableId = target.callableId
-
                     val scope = callableId.classId
-                        ?.let { classId -> getClassOrObjectSymbolByClassId(classId)!!.getMemberScope() }
+                        ?.let { classId -> getContainingMemberSymbol(classId).getMemberScope() }
                         ?: getPackageSymbolIfPackageExists(callableId.packageName)!!.getPackageScope()
 
                     val callablesOfProperType = scope.getCallableSymbols(callableId.callableName)
