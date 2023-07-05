865faea6efea1ab9d24ce7fbc3921ee2f0b3f112
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2023-07-05 14:38:56 +0200

    IDEA-317165 Update Zoom IDE popup location to keep selected option under the cursor
    GitOrigin-RevId: b946c4795d85ef32157ddf73c10f250bd7304736

--- platform/platform-impl/src/com/intellij/ide/actions/QuickChangeIdeScaleAction.kt
+++ platform/platform-impl/src/com/intellij/ide/actions/QuickChangeIdeScaleAction.kt
@@ -16,12 +16,17 @@ import com.intellij.openapi.ui.popup.JBPopupListener
 import com.intellij.openapi.ui.popup.LightweightWindowEvent
 import com.intellij.openapi.ui.popup.ListPopup
 import com.intellij.openapi.util.Condition
+import com.intellij.ui.hover.HoverListener
+import com.intellij.ui.scale.JBUIScale
 import com.intellij.util.Alarm
+import java.awt.Component
 import javax.swing.JList
 import javax.swing.event.ListSelectionEvent
+import kotlin.math.roundToInt
 
 class QuickChangeIdeScaleAction : QuickSwitchSchemeAction() {
   private val switchAlarm = Alarm()
+  private var popupSession: PopupSession? = null
 
   override fun fillActions(project: Project?, group: DefaultActionGroup, dataContext: DataContext) {
     val initialScale = UISettingsUtils.getInstance().currentIdeScale
@@ -57,6 +62,7 @@ class QuickChangeIdeScaleAction : QuickSwitchSchemeAction() {
             applyUserScale(anAction.scale, true)
             if (!popup.isDisposed) {
               popup.pack(true, true)
+              popupSession?.updateLocation()
             }
           }, SELECTION_THROTTLING_MS)
         }
@@ -66,12 +72,30 @@ class QuickChangeIdeScaleAction : QuickSwitchSchemeAction() {
     popup.addListener(object : JBPopupListener {
       override fun onClosed(event: LightweightWindowEvent) {
         switchAlarm.cancelAllRequests()
+        popupSession = null
         if (!event.isOk) {
           applyUserScale(initialScale, false)
           logSwitcherClosed(false)
         }
       }
     })
+
+    val hoverListener = object: HoverListener() {
+      override fun mouseEntered(component: Component, x: Int, y: Int) {
+        popupSession?.updateMouseCoordinates(x, y)
+      }
+
+      override fun mouseMoved(component: Component, x: Int, y: Int) {
+        popupSession?.updateMouseCoordinates(x, y)
+      }
+
+      override fun mouseExited(component: Component) {
+        popupSession?.mouseIsInside = false
+      }
+    }
+
+    hoverListener.addTo(popup.content)
+    popupSession = PopupSession(popup)
 
     super.showPopup(e, popup)
   }
@@ -98,6 +122,46 @@ class QuickChangeIdeScaleAction : QuickSwitchSchemeAction() {
     }
 
     private const val SELECTION_THROTTLING_MS = 500
+  }
+
+  /**
+   * Helper for recalculating popup location to avoid popup jumping after selecting zoom level by a mouse cursor
+   *
+   * @param popup The list popup associated with this session.
+   */
+  private class PopupSession(val popup: ListPopup) {
+    var lastMouseX = -1
+    var lastMouseY = -1
+    var lastScale = 1f
+    var mouseIsInside = false
+
+    fun updateMouseCoordinates(x: Int, y: Int) {
+      mouseIsInside = true
+      lastMouseX = x
+      lastMouseY = y
+      lastScale = JBUIScale.scale(1f)
+    }
+
+    fun updateLocation() {
+      val oldX = lastMouseX
+      val oldY = lastMouseY
+      val oldScale = lastScale
+
+      if (popup.isDisposed || !mouseIsInside || lastMouseX < 0 || lastMouseY < 0) return
+
+      val newScale = JBUIScale.scale(1f)
+
+      val newX = (oldX * newScale / oldScale).roundToInt()
+      val newY = (oldY * newScale / oldScale).roundToInt()
+
+      val dX = newX - oldX
+      val dY = newY - oldY
+
+      val location = popup.locationOnScreen
+      location.x -= dX
+      location.y -= dY
+      popup.setLocation(location)
+    }
   }
 }
 
