952accdfe8121f5175ee5f3d4941d4490f611f0f
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2023-07-05 18:53:05 +0200

    [kotlin] Add simple update usages implementation
    ^KTIJ-23273
    
    GitOrigin-RevId: 6d90b8d35643211d5099cfea7191a50f4c89ac0b

--- plugins/kotlin/refactorings/kotlin.refactorings.move.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/move/K2MoveMembersRefactoringProcessor.kt
+++ plugins/kotlin/refactorings/kotlin.refactorings.move.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/move/K2MoveMembersRefactoringProcessor.kt
@@ -2,11 +2,19 @@
 package org.jetbrains.kotlin.idea.k2.refactoring.move
 
 import com.intellij.ide.IdeDeprecatedMessagesBundle
+import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.refactoring.BaseRefactoringProcessor
 import com.intellij.refactoring.move.MoveMultipleElementsViewDescriptor
+import com.intellij.refactoring.util.MoveRenameUsageInfo
 import com.intellij.usageView.UsageInfo
 import com.intellij.usageView.UsageViewDescriptor
+import org.jetbrains.kotlin.analysis.api.KtAllowAnalysisOnEdt
+import org.jetbrains.kotlin.analysis.api.lifetime.allowAnalysisOnEdt
+import org.jetbrains.kotlin.idea.base.psi.deleteSingle
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
+import org.jetbrains.kotlin.psi.KtImportDirective
+import org.jetbrains.kotlin.psi.KtNamedDeclaration
+import org.jetbrains.kotlin.psi.psiUtil.getNonStrictParentOfType
 
 class K2MoveMembersRefactoringProcessor(
     val descriptor: K2MoveDescriptor.Members
@@ -19,17 +27,33 @@ class K2MoveMembersRefactoringProcessor(
     }
 
     override fun findUsages(): Array<UsageInfo> {
-        return emptyArray()
+        return descriptor.source.elements.flatMap { member ->
+            val references = ReferencesSearch.search(member).findAll().filter { reference ->
+                reference.element.getNonStrictParentOfType<KtImportDirective>() == null
+            }
+            references.map { reference -> MoveRenameUsageInfo(reference, member) }
+        }.toTypedArray()
     }
 
+    @OptIn(KtAllowAnalysisOnEdt::class)
     override fun performRefactoring(usages: Array<out UsageInfo>) {
         val targetFile = descriptor.target.file
-        for (declaration in descriptor.source.elements) {
-            targetFile.add(declaration)
-            declaration.delete()
+
+        val usageMap = usages.groupBy {
+            (it as MoveRenameUsageInfo).referencedElement as KtNamedDeclaration
         }
 
         val sourceFiles = descriptor.source.elements.map { it.containingKtFile }.distinct()
+
+        for (declaration in descriptor.source.elements) {
+            val newElement = targetFile.add(declaration) as KtNamedDeclaration
+            usageMap[declaration]?.forEach { usage ->
+                val renameUsage = usage as MoveRenameUsageInfo
+                allowAnalysisOnEdt { renameUsage.reference?.bindToElement(newElement) }
+            }
+            declaration.deleteSingle() // can't use delete here because calling it on single element top level classes wil remove the file
+        }
+
         if (descriptor.deleteEmptySourceFiles) {
             for (sourceFile in sourceFiles) {
                 if (sourceFile.declarations.isEmpty()) sourceFile.delete()
