a93097dd296b66e5e348f9e6db0798fb335ecec5
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-07-05 12:07:30 +0200

    [java-highlighting] IDEA-323955 Record patterns support available only for Java 20 preview. Fix compiler errors and tests
    (cherry picked from commit e21dde6aa890c64b4463362db36abe8656bc700d)
    
    GitOrigin-RevId: 21faf1ae00f523e3f2c3d2c4c56135e2b412b8ed

--- java/java-tests/testData/inspection/duplicateBranchesInEnhancedSwitch/NullDuplicatesDefault.java
+++ java/java-tests/testData/inspection/duplicateBranchesInEnhancedSwitch/NullDuplicatesDefault.java
@@ -1,7 +1,8 @@
 class C {
     void foo(String s) {
         switch (s) {
-            case "blah blah blah", null -> bar("A");
+            case "blah blah blah" -> <weak_warning descr="Branch in 'switch' is a duplicate of the default branch">bar("A");</weak_warning>
+            case null -> <weak_warning descr="Branch in 'switch' is a duplicate of the default branch">bar("A");</weak_warning>
             default -> bar("A");
         }
     }
--- java/java-tests/testSrc/com/intellij/java/codeInspection/PatternVariableCanBeUsedInspectionTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInspection/PatternVariableCanBeUsedInspectionTest.java
@@ -4,6 +4,7 @@ package com.intellij.java.codeInspection;
 import com.intellij.codeInsight.daemon.quickFix.LightQuickFixParameterizedTestCase;
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.PatternVariableCanBeUsedInspection;
+import com.intellij.pom.java.LanguageLevel;
 import com.intellij.testFramework.LightProjectDescriptor;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 import org.jetbrains.annotations.NotNull;
@@ -13,7 +14,12 @@ public class PatternVariableCanBeUsedInspectionTest extends LightQuickFixParamet
   protected LocalInspectionTool @NotNull [] configureLocalInspectionTools() {
     return new LocalInspectionTool[]{new PatternVariableCanBeUsedInspection()};
   }
-  
+
+  @Override
+  protected LanguageLevel getLanguageLevel() {
+    return LanguageLevel.JDK_21;
+  }
+
   @Override
   protected String getBasePath() {
     return "/inspection/patternVariableCanBeUsed";
