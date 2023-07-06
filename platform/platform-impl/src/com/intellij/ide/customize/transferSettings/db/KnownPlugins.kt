54a9553db4e14580016d260fe480cf220253c9bb
Dmitry Yudin <dmitry.yudin@jetbrains.com>
2023-07-06 22:58:26 +0000

    Update plugin mappings and known plugins list
    This commit updates the mapping of plugins and the list of known plugins, including Maven, Gradle, Debugger, WSL, TOML, Vue.js, AI Assistant, and language packs for Chinese, Korean, and Japanese.
    
    Merge-request: IJ-MR-110097
    Merged-by: Dmitry Yudin <Dmitry.Yudin@jetbrains.com>
    
    GitOrigin-RevId: b77d0fa1c4628e84a8df8c0eea2c0ab84c313b02

--- platform/platform-impl/src/com/intellij/ide/customize/transferSettings/db/KnownPlugins.kt
+++ platform/platform-impl/src/com/intellij/ide/customize/transferSettings/db/KnownPlugins.kt
@@ -13,6 +13,8 @@ object KnownPlugins {
   val WebSupport: BuiltInFeature = BuiltInFeature("Web support", "HTML, CSS, JS")
   val Docker: BuiltInFeature = BuiltInFeature("Docker")
 
+  val Java: BuiltInFeature = BuiltInFeature("Java")
+  val Kotlin: BuiltInFeature = BuiltInFeature("Kotlin")
   val CSharp: BuiltInFeature = BuiltInFeature("C#")
   val NuGet: BuiltInFeature = BuiltInFeature("NuGet")
   val TestExplorer: BuiltInFeature = BuiltInFeature("TestExplorer")
@@ -24,6 +26,19 @@ object KnownPlugins {
   val DotNetDecompiler: BuiltInFeature = BuiltInFeature(".NET Decompiler")
   val DatabaseSupport: BuiltInFeature = BuiltInFeature("Database Support")
   val TSLint: BuiltInFeature = BuiltInFeature("TSLint")
+  val Maven: BuiltInFeature = BuiltInFeature("Maven")
+  val Gradle: BuiltInFeature = BuiltInFeature("Gradle")
+  val Debugger: BuiltInFeature = BuiltInFeature("Debugger")
+  val WindowsSubsystemLinux: BuiltInFeature = BuiltInFeature("WSL")
+  val Toml: BuiltInFeature = BuiltInFeature("TOML")
+  val Vue: BuiltInFeature = BuiltInFeature("Vue.js")
+  val AiAssistant: BuiltInFeature = BuiltInFeature("AI Assistant")
+
+  // Language packs
+
+  val ChineseLanguage: PluginFeature = PluginFeature("com.intellij.zh", "Chinese (Simplified) Language Pack / 中文语言包")
+  val KoreanLanguage: PluginFeature = PluginFeature("com.intellij.ko", "Korean Language Pack / 한국어 언어 팩")
+  val JapaneseLanguage: PluginFeature = PluginFeature("com.intellij.ja", "Japanese Language Pack / 日本語言語パック")
 
   // Plugins
 
@@ -31,7 +46,12 @@ object KnownPlugins {
   val Ideolog: PluginFeature = PluginFeature("com.intellij.ideolog", "Ideolog (logging)")
   val IdeaVim: PluginFeature = PluginFeature("IdeaVIM", "IdeaVIM")
   val TeamCity: PluginFeature = PluginFeature("Jetbrains TeamCity Plugin", "TeamCity")
-  val NodeJSSupport: PluginFeature = PluginFeature("NodeJS", "NodeJS support")
+  val Scala: PluginFeature = PluginFeature("org.intellij.scala", "Scala")
+  val Dart: PluginFeature = PluginFeature("Dart", "Dart")
+  val Flutter: PluginFeature = PluginFeature("io.flutter", "Flutter")
+  val Lombok: PluginFeature = PluginFeature("Lombook Plugin", "Lombok")
+  val Prettier: PluginFeature = PluginFeature("intellij.prettierJS", "Prettier")
+  val Kubernetes: PluginFeature = PluginFeature("com.intellij.kubernetes", "Kubernetes")
 
   val Monokai: PluginFeature = PluginFeature("monokai-pro", "Monokai")
   val Solarized: PluginFeature = PluginFeature("com.tylerthrailkill.intellij.solarized", "Solarized")
--- platform/platform-impl/src/com/intellij/ide/customize/transferSettings/providers/vscode/mappings/PluginsMappings.kt
+++ platform/platform-impl/src/com/intellij/ide/customize/transferSettings/providers/vscode/mappings/PluginsMappings.kt
@@ -10,7 +10,6 @@ object PluginsMappings {
     "xinyayang0506.log-analysis" to KnownPlugins.Ideolog,
     "vscodevim.vim" to KnownPlugins.IdeaVim,
     "msveden.teamcity-checker" to KnownPlugins.TeamCity,
-    "ms-vscode.node-debug2" to KnownPlugins.NodeJSSupport,
 
     // Features
     "donjayamanne.githistory" to KnownPlugins.Git,
@@ -19,10 +18,29 @@ object PluginsMappings {
     "mhutchie.git-graph" to KnownPlugins.Git,
     "ritwickdey.liveserver" to KnownPlugins.WebSupport,
     "ms-azuretools.vscode-docker" to KnownPlugins.Docker,
+    "vscjava.vscode-java-pack" to KnownPlugins.Java,
+    "redhat.java" to KnownPlugins.Java,
+    "vscjava.vscode-maven" to KnownPlugins.Maven,
+    "vscjava.vscode-gradle" to KnownPlugins.Gradle,
+    "vscjava.vscode-java-debug" to KnownPlugins.Debugger,
+    "donjayamanne.javadebugger" to KnownPlugins.Debugger,
+    "mathiasfrohlich.Kotlin" to KnownPlugins.Kotlin,
+    "fwcd.kotlin" to KnownPlugins.Kotlin,
+    "ms-vscode-remote.remote-wsl" to KnownPlugins.WindowsSubsystemLinux,
+    "bungcip.better-toml" to KnownPlugins.Toml,
+    "Vue.volar" to KnownPlugins.Vue,
+    //"GitHub.copilot-chat" to KnownPlugins.AiAssistant,
+
+    // Language packs
+    "ms-ceintl.vscode-language-pack-zh-hans" to KnownPlugins.ChineseLanguage,
+    "ms-ceintl.vscode-language-pack-ja" to KnownPlugins.KoreanLanguage,
+    "ms-ceintl.vscode-language-pack-ko" to KnownPlugins.JapaneseLanguage,
 
     // New mappings
     "ms-dotnettools.csharp" to KnownPlugins.CSharp,
     "jchannon.csharpextensions" to KnownPlugins.CSharp,
+    "ms-dotnettools.csdevkit" to KnownPlugins.CSharp,
+    "kreativ-software.csharpextensions" to KnownPlugins.CSharp,
     "jmrog.vscode-nuget-package-manager" to KnownPlugins.NuGet,
     "formulahendry.dotnet-test-explorer" to KnownPlugins.TestExplorer,
     "formulahendry.dotnet" to KnownPlugins.RunConfigurations,
@@ -39,7 +57,13 @@ object PluginsMappings {
     "editorconfig.editorconfig" to KnownPlugins.editorconfig,
     "ms-vscode.vscode-typescript-tslint-plugin" to KnownPlugins.TSLint,
     "github.vscode-pull-request-github" to KnownPlugins.Git,
-    "mtxr.sqltools" to KnownPlugins.DatabaseSupport
+    "mtxr.sqltools" to KnownPlugins.DatabaseSupport,
+    "scala-lang.scala" to KnownPlugins.Scala,
+    "Dart-Code.dart-code" to KnownPlugins.Dart,
+    "Dart-Code.flutter" to KnownPlugins.Flutter,
+    "vscjava.vscode-lombok" to KnownPlugins.Lombok,
+    "esbenp.prettier-vscode" to KnownPlugins.Prettier,
+    "ms-kubernetes-tools.vscode-kubernetes-tools" to KnownPlugins.Kubernetes,
   )
 
   fun pluginIdMap(pluginId: String): FeatureInfo? = theMap[pluginId]
