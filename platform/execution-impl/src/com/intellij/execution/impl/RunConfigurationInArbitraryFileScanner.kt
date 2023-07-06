ed00a296d5fdf53f92757d7172875f71149d7a4d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-07-06 23:39:19 +0200

    avoid getting run manager without a reason
    GitOrigin-RevId: e9319ab56cd67ba125ec827b70b09189fe950c49

--- platform/execution-impl/src/com/intellij/execution/impl/RunConfigurationInArbitraryFileScanner.kt
+++ platform/execution-impl/src/com/intellij/execution/impl/RunConfigurationInArbitraryFileScanner.kt
@@ -4,7 +4,6 @@ package com.intellij.execution.impl
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.util.PathUtil
 import com.intellij.util.indexing.roots.IndexableFileScanner
 import com.intellij.util.indexing.roots.kind.ContentOrigin
 import com.intellij.util.indexing.roots.kind.ProjectFileOrDirOrigin
@@ -13,31 +12,30 @@ import com.intellij.util.indexing.roots.kind.ProjectFileOrDirOrigin
  * This class doesn't push any file properties, it is used for scanning the project for `*.run.xml` files - files with run configurations.
  * This is to handle run configurations stored in arbitrary files within project content (not in .idea/runConfigurations or project.ipr file).
  */
-internal class RunConfigurationInArbitraryFileScanner : IndexableFileScanner {
-  companion object {
-    fun isFileWithRunConfigs(file: VirtualFile): Boolean {
-      if (!file.isInLocalFileSystem || !StringUtil.endsWith(file.nameSequence, ".run.xml")) return false
-      var parent = file.parent
-      while (parent != null) {
-        if (StringUtil.equals(parent.nameSequence, ".idea")) return false
-        parent = parent.parent
-      }
-      return true
-    }
-
-    fun isFileWithRunConfigs(path: String) = !path.contains("/.idea/") && PathUtil.getFileName(path).endsWith(".run.xml")
-  }
-
+private class RunConfigurationInArbitraryFileScanner : IndexableFileScanner {
   override fun startSession(project: Project): IndexableFileScanner.ScanSession {
-    val runManagerImpl = RunManagerImpl.getInstanceImpl(project)
+    val runManager by lazy(LazyThreadSafetyMode.NONE) { RunManagerImpl.getInstanceImpl(project) }
     return IndexableFileScanner.ScanSession {
-      if (it is ContentOrigin || it is ProjectFileOrDirOrigin)
+      if (it is ContentOrigin || it is ProjectFileOrDirOrigin) {
         IndexableFileScanner.IndexableFileVisitor { fileOrDir ->
           if (isFileWithRunConfigs(fileOrDir)) {
-            runManagerImpl.updateRunConfigsFromArbitraryFiles(emptyList(), listOf(fileOrDir.path))
+            runManager.updateRunConfigsFromArbitraryFiles(emptyList(), listOf(fileOrDir.path))
           }
         }
-      else null
+      }
+      else {
+        null
+      }
     }
   }
+}
+
+private fun isFileWithRunConfigs(file: VirtualFile): Boolean {
+  if (!file.isInLocalFileSystem || !file.nameSequence.endsWith(".run.xml")) return false
+  var parent = file.parent
+  while (parent != null) {
+    if (StringUtil.equals(parent.nameSequence, ".idea")) return false
+    parent = parent.parent
+  }
+  return true
 }
