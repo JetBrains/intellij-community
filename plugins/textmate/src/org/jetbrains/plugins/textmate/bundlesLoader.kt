66382621b608475ba2f0a96524cfffdf8e259940
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2023-07-07 12:34:14 +0200

    Prevent flaky shared indexes build failures due to thread pool starvation
    Look at the snippet below:
    ```
    val initializationJob = scope.launch {
      bundlePaths.map { bundlePath ->
        launch(Dispatchers.IO) {
          print("hello world")
        }
      }
    }
    ```
    
    The pitfall is that initially it starts a coroutine in the default thread pool, which has a hard limit of concurrently launched tasks. Only when there is a free thread, the coroutine launches and schedules new coroutines in another, unlimited thread pool from Dispatchers.IO.
    
    This becomes more dangerous when this asynchronous approach is combined with blocking approach. Here, `ProgressIndicatorUtils.awaitWithCheckCanceled(initializationJob.asCompletableFuture())` can be executed in a thread from the very same limited thread pool, increasing the probability of thread starvation.
    
    GitOrigin-RevId: b9e7f765547ca8199ab32b9df35b7b6b60712166

--- plugins/textmate/src/org/jetbrains/plugins/textmate/bundlesLoader.kt
+++ plugins/textmate/src/org/jetbrains/plugins/textmate/bundlesLoader.kt
@@ -4,22 +4,23 @@ package org.jetbrains.plugins.textmate
 
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.progress.util.ProgressIndicatorUtils
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import com.intellij.openapi.util.IntellijInternalApi
+import com.intellij.util.io.blockingDispatcher
+import kotlinx.coroutines.*
 import kotlinx.coroutines.future.asCompletableFuture
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.nio.file.Path
 import java.util.function.Consumer
 
+@OptIn(DelicateCoroutinesApi::class)
+@IntellijInternalApi
 @JvmOverloads
 internal fun registerBundlesInParallel(scope: CoroutineScope,
                                        bundlePaths: List<Path>,
                                        registrar: (Path) -> Boolean,
                                        registrationFailed: Consumer<Path>? = null) {
-  val initializationJob = scope.launch {
+  val initializationJob = scope.launch(blockingDispatcher) {
     bundlePaths.map { bundlePath ->
-      launch(Dispatchers.IO) {
+      launch {
         if (!registrar(bundlePath)) {
           if (registrationFailed == null) {
             TextMateService.LOG.error("Cannot load builtin textmate bundle", bundlePath.toString())
