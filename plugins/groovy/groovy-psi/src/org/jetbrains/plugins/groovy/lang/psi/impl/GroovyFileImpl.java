c75724fbb847e8156da8d073475ffd82b5435770
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2023-07-07 17:05:44 +0200

    [groovy] Flush caches less aggressively
    GitOrigin-RevId: d30068c19c539d20b033f5371fc5f4d7ad3463af

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/GroovyFileImpl.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/GroovyFileImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.groovy.lang.psi.impl;
 
 import com.intellij.lang.ASTNode;
@@ -300,7 +300,9 @@ public class GroovyFileImpl extends GroovyFileBaseImpl implements GroovyFile, Ps
   @Override
   public void clearCaches() {
     super.clearCaches();
-    myScriptClass = null;
+    if (!isScript()) {
+      myScriptClass = null;
+    }
     mySyntheticArgsParameter = null;
   }
 
