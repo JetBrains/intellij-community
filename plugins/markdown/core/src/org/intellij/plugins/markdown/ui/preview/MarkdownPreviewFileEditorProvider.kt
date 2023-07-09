217a826583170a2c8a33f51031c58239f2d0f4c6
Ivan Posti <ivan.posti@jetbrains.com>
2023-07-09 21:15:28 +0200

    [Markdown][RDCT-220] Convert preview editor provider to Kotlin
    GitOrigin-RevId: aff41c4364a8bada1ef17f6c3831f945bac7d0a6

---
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/ui/preview/MarkdownPreviewFileEditorProvider.kt
@@ -0,0 +1,40 @@
+package org.intellij.plugins.markdown.ui.preview
+
+import com.intellij.ide.scratch.ScratchUtil
+import com.intellij.lang.LanguageUtil
+import com.intellij.openapi.fileEditor.FileEditor
+import com.intellij.openapi.fileEditor.FileEditorPolicy
+import com.intellij.openapi.fileEditor.WeighedFileEditorProvider
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import org.intellij.plugins.markdown.lang.MarkdownFileType
+import org.intellij.plugins.markdown.lang.MarkdownLanguageUtils.isMarkdownLanguage
+
+internal class MarkdownPreviewFileEditorProvider: WeighedFileEditorProvider() {
+  override fun accept(project: Project, file: VirtualFile): Boolean {
+    if (!MarkdownHtmlPanelProvider.hasAvailableProviders()) {
+      return false
+    }
+    return file.fileType == MarkdownFileType.INSTANCE || isMarkdownScratch(project, file)
+  }
+
+  private fun isMarkdownScratch(project: Project, file: VirtualFile): Boolean {
+    if (!ScratchUtil.isScratch(file)) {
+      return false
+    }
+    val language = LanguageUtil.getLanguageForPsi(project, file, MarkdownFileType.INSTANCE)
+    return language?.isMarkdownLanguage() == true
+  }
+
+  override fun createEditor(project: Project, file: VirtualFile): FileEditor {
+    return MarkdownPreviewFileEditor(project, file)
+  }
+
+  override fun getEditorTypeId(): String {
+    return "markdown-preview-editor"
+  }
+
+  override fun getPolicy(): FileEditorPolicy {
+    return FileEditorPolicy.PLACE_AFTER_DEFAULT_EDITOR
+  }
+}
