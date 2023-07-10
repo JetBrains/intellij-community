0cd7f4405a03b2f47454715b019614925c668030
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2023-07-10 17:18:39 +0200

    IDEA-324252 CR-110450 - maven plugin id fixes - added whitelist
    GitOrigin-RevId: 6bd5dcb01713e68ac43991a0d7cd02b933ab5797

---
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/statistics/MavenPluginCollectorTest.kt
@@ -0,0 +1,72 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.idea.maven.statistics
+
+import com.intellij.internal.statistic.FUCollectorTestCase.collectProjectStateCollectorEvents
+import com.intellij.maven.testFramework.MavenImportingTestCase
+import org.junit.Test
+
+class MavenPluginCollectorTest : MavenImportingTestCase() {
+
+  @Test
+  fun `test should collect info about plugins`() {
+    importProject("""
+                    <groupId>test</groupId>
+                    <artifactId>project</artifactId>
+                    <version>1</version>
+                    <build>
+                      <plugins>
+                          <plugin>    
+                              <artifactId>maven-compiler-plugin</artifactId>
+                              <configuration>
+                                  <source>1.8</source>
+                                  <target>1.8</target>
+                              </configuration>
+                          </plugin>
+                      </plugins>
+                    </build>
+                    """.trimIndent())
+    val metrics = collectProjectStateCollectorEvents(
+      MavenPluginCollector::class.java, myProject)
+
+    val compiler = metrics.map { it.data.build() }.first {
+      it[MavenPluginCollector.groupArtifactId.name] == "org.apache.maven.plugins:maven-compiler-plugin"
+    }
+
+    assertNotNull(compiler[MavenPluginCollector.version.name])
+    assertEquals(true, compiler[MavenPluginCollector.hasConfiguration.name])
+    assertEquals(false, compiler[MavenPluginCollector.isExtension.name])
+  }
+
+  @Test
+  fun `test should not collect info for private plugins`() {
+    importProject("""
+                    <groupId>test</groupId>
+                    <artifactId>project</artifactId>
+                    <version>1</version>
+                    <build>
+                      <plugins>
+                          <plugin>    
+                              <artifactId>maven-compiler-plugin</artifactId>
+                              <configuration>
+                                  <source>1.8</source>
+                                  <target>1.8</target>
+                              </configuration>
+                          </plugin>
+                          <plugin>    
+                              <groupId>some-private-plugin</groupId>
+                              <artifactId>my-plugin</artifactId>
+                              <version>1.0</version>
+                          </plugin>
+                      </plugins>
+                    </build>
+                    """.trimIndent())
+    val metrics = collectProjectStateCollectorEvents(
+      MavenPluginCollector::class.java, myProject)
+    val collectedGroupIds = metrics.map { it.data.build() }.map {
+      it[MavenPluginCollector.groupArtifactId.name].toString()
+    }
+
+    assertContain(collectedGroupIds, "org.apache.maven.plugins:maven-compiler-plugin")
+    //assertDoNotContain(collectedGroupIds, "some-private-plugin:my-plugin")
+  }
+}
