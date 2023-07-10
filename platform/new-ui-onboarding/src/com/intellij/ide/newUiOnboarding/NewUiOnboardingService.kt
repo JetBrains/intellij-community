3c10560f5be8c1eee5b1999a21bd7743b37cdc47
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2023-07-10 15:11:24 +0300

    IDEA-323344 Add main menu step
    (cherry picked from commit b395fd657e1605007f04d006a99bae88e3f0a7df)
    
    GitOrigin-RevId: 9ea1ed1b33376a7952fdf5dc078dea84777b7f05

--- platform/new-ui-onboarding/src/com/intellij/ide/newUiOnboarding/NewUiOnboardingService.kt
+++ platform/new-ui-onboarding/src/com/intellij/ide/newUiOnboarding/NewUiOnboardingService.kt
@@ -28,7 +28,8 @@ internal class NewUiOnboardingService(private val project: Project, private val 
   }
 
   private fun getDefaultStepsOrder(): List<String> {
-    return listOf("projectWidget",
+    return listOf("mainMenu",
+                  "projectWidget",
                   "gitWidget",
                   "runWidget",
                   "codeWithMe",
