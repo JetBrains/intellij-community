0d71eebb42d2b863e4bad9f1865107db3eb8463c
Ivan Semenov <ivan.semenov@jetbrains.com>
2023-07-10 18:06:39 +0200

    [github] remove unused keys
    GitOrigin-RevId: 8a2a5046cc152098b906b50c7d8cf42323d175c9

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/toolwindow/GHPRToolWindowRepositoryContentController.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/toolwindow/GHPRToolWindowRepositoryContentController.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.pullrequest.ui.toolwindow
 
-import com.intellij.openapi.util.Key
 import org.jetbrains.plugins.github.api.GHRepositoryCoordinates
 import org.jetbrains.plugins.github.pullrequest.data.GHPRIdentifier
 
@@ -23,8 +22,4 @@ interface GHPRToolWindowRepositoryContentController {
   fun openPullRequestTimeline(id: GHPRIdentifier, requestFocus: Boolean)
 
   fun openPullRequestDiff(id: GHPRIdentifier, requestFocus: Boolean)
-
-  companion object {
-    val KEY = Key.create<GHPRToolWindowRepositoryContentController>("Github.PullRequests.ToolWindow.Repository.Content.Controller")
-  }
 }
