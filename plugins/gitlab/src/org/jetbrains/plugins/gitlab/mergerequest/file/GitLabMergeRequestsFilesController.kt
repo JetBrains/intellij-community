f3c0da89fa57c1e7ac46765624df26e60ce1d838
Ivan Semenov <ivan.semenov@jetbrains.com>
2023-07-10 15:18:33 +0200

    [gitlab] fix exception on timeline file close
    IDEA-322699 Fixed
    
    GitOrigin-RevId: bd80d38175214a9cf6761fa860770194d2616d3a

--- plugins/gitlab/src/org/jetbrains/plugins/gitlab/mergerequest/file/GitLabMergeRequestsFilesController.kt
+++ plugins/gitlab/src/org/jetbrains/plugins/gitlab/mergerequest/file/GitLabMergeRequestsFilesController.kt
@@ -1,8 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gitlab.mergerequest.file
 
-import com.intellij.openapi.application.EDT
-import com.intellij.openapi.application.writeAction
+import com.intellij.openapi.application.*
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.changes.VcsEditorTabFilesManager
@@ -42,14 +41,17 @@ class GitLabMergeRequestsFilesControllerImpl(
   }
 
   override suspend fun closeAllFiles() {
-    withContext(Dispatchers.EDT) {
+    withContext(Dispatchers.EDT + ModalityState.nonModal().asContextElement()) {
       if (project.isDisposed) return@withContext
       val fileManager = FileEditorManager.getInstance(project)
       writeAction {
-        // cache?
-        fileManager.openFiles.forEach { file ->
-          if (file is GitLabVirtualFile && connection.id == file.connectionId) {
-            fileManager.closeFile(file)
+        // otherwise the exception is thrown when removing an editor tab
+        (TransactionGuard.getInstance() as TransactionGuardImpl).performUserActivity {
+          // cache?
+          fileManager.openFiles.forEach { file ->
+            if (file is GitLabVirtualFile && connection.id == file.connectionId) {
+              fileManager.closeFile(file)
+            }
           }
         }
       }
