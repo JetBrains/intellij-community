ba215468a95a106e53ac6fa3f7068b5b4ca249bf
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2023-07-11 12:38:46 +0200

    make field private again IJ-CR-110250
    (cherry picked from commit 678960c22ea4b8458996a9c2cfbf091b41f95036)
    
    IJ-CR-110250
    
    GitOrigin-RevId: 6514851b406447a39f45514aa2eeaf23325e8a26

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/FSRecordsImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/FSRecordsImpl.java
@@ -97,7 +97,7 @@ public final class FSRecordsImpl {
   };
 
 
-  public final @NotNull PersistentFSConnection connection;
+  private final @NotNull PersistentFSConnection connection;
   private final @NotNull PersistentFSContentAccessor contentAccessor;
   private final @NotNull PersistentFSAttributeAccessor attributeAccessor;
   private final @NotNull PersistentFSTreeAccessor treeAccessor;
