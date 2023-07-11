efb4fb48c28577ca5f2c388b71ff1ea1816d4bc8
Konstantin Bulenkov <kb@jetbrains.com>
2023-07-11 15:55:07 +0200

    Whitelist 'Light with Light Header' color scheme
    GitOrigin-RevId: 5c85156444d1af9355a6c537d706d4eb898346cf

--- platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/ui/EditorColorSchemesUsagesCollector.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/ui/EditorColorSchemesUsagesCollector.java
@@ -21,7 +21,7 @@ import java.util.Set;
 
 public class EditorColorSchemesUsagesCollector extends ApplicationUsagesCollector {
 
-  private final static int CURR_VERSION = 6;
+  private final static int CURR_VERSION = 7;
 
   public static final String SCHEME_NAME_OTHER = "Other";
   public final static String[] KNOWN_NAMES = {
@@ -48,6 +48,7 @@ public class EditorColorSchemesUsagesCollector extends ApplicationUsagesCollecto
     "Rider",
     "IntelliJ Light",
     "Light",
+    "Light with Light Header",
     "Dark",
     SCHEME_NAME_OTHER
   };
