422e580a34b3144a7ed9da56fef5fd21a32c7e7e
Artem Daugel-Dauge <artem.daugel-dauge@jetbrains.com>
2023-07-12 21:26:40 +0200

    [Kotlin] Enable expect/actual line markers and navigation in library sources
    ^KTIJ-13660
    
    GitOrigin-RevId: 87cd7e3cde17693b950fe315ab238397ededcc0c

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/multiplatformTests/testFeatures/checkers/highlighting/HighlightingChecker.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/multiplatformTests/testFeatures/checkers/highlighting/HighlightingChecker.kt
@@ -1,6 +1,10 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.gradle.multiplatformTests.testFeatures.checkers.highlighting
 
+import com.intellij.openapi.application.runWriteActionAndWait
+import com.intellij.openapi.roots.OrderRootType
+import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar
+import com.intellij.openapi.vfs.VfsUtil
 import org.jetbrains.kotlin.gradle.multiplatformTests.AbstractTestChecker
 import org.jetbrains.kotlin.gradle.multiplatformTests.KotlinMppTestsContext
 import org.jetbrains.kotlin.idea.codeInsight.gradle.HighlightingCheck
@@ -33,14 +37,23 @@ object HighlightingChecker : AbstractTestChecker<HighlightingCheckConfiguration>
         val highlightingConfig = testConfiguration.getConfiguration(HighlightingChecker)
         if (highlightingConfig.skipCodeHighlighting) return
         val renderedConfig = renderConfiguration()
-        HighlightingCheck(
+        val highlightingCheck = HighlightingCheck(
             project = testProject,
             projectPath = testProjectRoot.path,
             testDataDirectory = testDataDirectory,
             testLineMarkers = !highlightingConfig.hideLineMarkers,
             severityLevel = highlightingConfig.hideHighlightsBelow,
             postprocessActualTestData = { actualTestData -> renderedConfig + "\n" + actualTestData }
-        ).invokeOnAllModules()
+        )
+
+        if (!highlightingConfig.checkLibrarySources) {
+            highlightingCheck.invokeOnAllModules()
+        } else {
+            val librarySources = parseLibrarySourcesConfig()
+            addSourcesJarToWorkspace(librarySources)
+            highlightingCheck.invokeOnLibraries(librarySources)
+        }
+
     }
 
     private fun KotlinMppTestsContext.renderConfiguration(): String {
@@ -61,7 +74,46 @@ object HighlightingChecker : AbstractTestChecker<HighlightingCheckConfiguration>
             ""
     }
 
+    private fun KotlinMppTestsContext.parseLibrarySourcesConfig(): Map<String, LibrarySourceRoot> {
+        fun error(): Nothing = error(
+            "Error while parsing library-sources.txt. Expected line format:\n\n" +
+                    "<library name> <path to sources jar> <path to test data sources>"
+        )
+
+        val configFile = testDataDirectory.resolve("library-sources.txt")
+
+        if (!configFile.exists()) error()
+
+        return configFile.readLines()
+            .filter { it.isNotBlank() }
+            .map {
+                val split = it.split(' ')
+
+                LibrarySourceRoot(
+                    libraryName = split.getOrNull(0) ?: error(),
+                    sourcesJar = split.getOrNull(1) ?: error(),
+                    testDataPath = split.getOrNull(2) ?: error(),
+                )
+            }.associateBy { it.libraryName }
+    }
+
+    // workaround for IDEA-227215
+    private fun KotlinMppTestsContext.addSourcesJarToWorkspace(librarySources: Map<String, LibrarySourceRoot>) {
+        LibraryTablesRegistrar.getInstance().getLibraryTable(testProject).libraries.forEach { library ->
+            val sourceRoot = librarySources[library.name] ?: return@forEach
+
+            runWriteActionAndWait {
+                library.modifiableModel.apply {
+                    addRoot(VfsUtil.getUrlForLibraryRoot(testProjectRoot.resolve(sourceRoot.sourcesJar)), OrderRootType.SOURCES)
+                    commit()
+                }
+            }
+        }
+    }
+
     private const val HIGHLIGHTING_CONFIGURATION_HEADER = "//region Test configuration"
     private const val HIGHLIGHTING_CONFIGURATION_LINE_PREFIX = "// - "
     private const val HIGHLIGHTING_CONFIGURATION_FOOTER = "//endregion"
 }
+
+class LibrarySourceRoot(val libraryName: String, val sourcesJar: String, val testDataPath: String)
--- plugins/kotlin/highlighting/highlighting-k1/src/org/jetbrains/kotlin/idea/highlighter/markers/HasExpectActualMarkerUtil.kt
+++ plugins/kotlin/highlighting/highlighting-k1/src/org/jetbrains/kotlin/idea/highlighter/markers/HasExpectActualMarkerUtil.kt
@@ -1,12 +1,15 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.highlighter.markers
 
+import com.intellij.openapi.roots.libraries.Library
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.kotlin.analyzer.moduleInfo
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.descriptors.MemberDescriptor
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.idea.base.facet.isHMPPEnabled
+import org.jetbrains.kotlin.idea.base.projectStructure.moduleInfo.LibraryInfo
+import org.jetbrains.kotlin.idea.base.projectStructure.moduleInfo.LibrarySourceInfo
 import org.jetbrains.kotlin.idea.base.projectStructure.moduleInfo.ModuleSourceInfo
 import org.jetbrains.kotlin.resolve.descriptorUtil.module
 
@@ -18,8 +21,15 @@ fun getModulesStringForExpectActualMarkerTooltip(
     val isExpectDescriptors = descriptors.all { it is MemberDescriptor && it.isExpect }
 
     fun ModuleDescriptor.nameForTooltip(): String {
-        /* For hmpp modules, prefer the module name, if present */
-        moduleInfo?.let { it as? ModuleSourceInfo }?.takeIf { it.module.isHMPPEnabled }?.module?.name?.let { return it }
+        when (val moduleInfo = moduleInfo) {
+            /* For hmpp modules, prefer the module name, if present */
+            is ModuleSourceInfo -> moduleInfo.takeIf { it.module.isHMPPEnabled }?.module?.name?.let { return it }
+
+            /* For libraries we're trying to show artifact variant name */
+            is LibrarySourceInfo -> moduleInfo.library.extractVariantName()?.let { return it }
+            is LibraryInfo -> moduleInfo.library.extractVariantName()?.let { return it }
+        }
+
         stableName?.asStringStripSpecialMarkers()?.let { return it }
 
         /* Handle non-hmpp modes where no stableName might be present: */
@@ -37,5 +47,24 @@ fun getModulesStringForExpectActualMarkerTooltip(
     return when (descriptors.size) {
         1 -> descriptors.single().module.nameForTooltip()
         else -> descriptors.joinToString(", ", "[", "]") { it.module.nameForTooltip() }
+    }
+}
+
+/*
+    Supported formats:
+
+    <groupId>:<artifactId>:<variant>:<version>
+    <groupId>:<artifactId>-<variant>:<version>
+ */
+private fun Library.extractVariantName(): String? {
+    val split = name.orEmpty().split(":")
+    if (split.size != 3 && split.size != 4) {
+        return null
+    }
+
+    return when (split.size) {
+        3 -> split[1].substringAfterLast('-')
+        4 -> split[2]
+        else -> null
     }
 }
