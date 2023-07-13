4f951e7be94a503f1a51105191d52a70608d11d1
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-07-13 12:32:25 +0200

    [java-highlighting] IDEA-324327 IJ-CR-110223 Constant associated with switch block must be used with primitives or String
    GitOrigin-RevId: 56154226d27f3c254f27cc8db27ca95d2f967ac1

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/switchExpressions/ConstantAssignment.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/switchExpressions/ConstantAssignment.java
@@ -1,14 +1,14 @@
 public class ConstantAssignment {
   void dd(Double i) {
     switch (i) {
-      case <error descr="Incompatible types. Type of constant label: 'double', switch selector type: 'java.lang.Double'">1.0</error> -> System.out.println(1);
+      case <error descr="Pattern expected for switch selector type 'java.lang.Double'">1.0</error> -> System.out.println(1);
       default -> throw new IllegalStateException("Unexpected value: " + i);
     }
   }
 
   void dd2(Long i) {
     switch (i) {
-      case <error descr="Incompatible types. Type of constant label: 'long', switch selector type: 'java.lang.Long'">1L</error>:
+      case <error descr="Pattern expected for switch selector type 'java.lang.Long'">1L</error>:
         System.out.println(1);
       default:
         throw new IllegalStateException("Unexpected value: " + i);
