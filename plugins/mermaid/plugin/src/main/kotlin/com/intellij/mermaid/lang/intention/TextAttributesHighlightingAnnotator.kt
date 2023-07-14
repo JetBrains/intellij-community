cd9883b1430a01fd1c2855dbe5cb24a8160fee87
Ekaterina Berezhko <Ekaterina.Berezhko@jetbrains.com>
2023-07-14 09:45:01 +0300

    [MERMAID-94] Add highlights for data types
    GitOrigin-RevId: b370582b1f643862cf60fe7f78dab9bf533ac9ca

--- plugins/mermaid/plugin/src/main/kotlin/com/intellij/mermaid/lang/intention/TextAttributesHighlightingAnnotator.kt
+++ plugins/mermaid/plugin/src/main/kotlin/com/intellij/mermaid/lang/intention/TextAttributesHighlightingAnnotator.kt
@@ -4,16 +4,38 @@ import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.mermaid.lang.highlighting.MermaidTextAttributes
+import com.intellij.mermaid.lang.lexer.MermaidTokens
 import com.intellij.mermaid.lang.psi.MermaidSpecialState
+import com.intellij.mermaid.lang.psi.hasType
 import com.intellij.psi.PsiElement
 
 
 class TextAttributesHighlightingAnnotator : Annotator {
+  private val types = setOf(
+    "int", "integer",
+    "float",
+    "double",
+    "bool", "boolean",
+    "string",
+    "char", "character",
+    "list",
+    "array",
+    "BigDecimal"
+  )
+
   override fun annotate(element: PsiElement, holder: AnnotationHolder) {
     if (element is MermaidSpecialState) {
       holder.newSilentAnnotation(HighlightSeverity.TEXT_ATTRIBUTES)
         .textAttributes(MermaidTextAttributes.identifier)
         .create()
+    } else if (element.hasType(MermaidTokens.ATTRIBUTE_WORD) && element.isTypeAttribute()) {
+      holder.newSilentAnnotation(HighlightSeverity.TEXT_ATTRIBUTES)
+        .textAttributes(MermaidTextAttributes.constant)
+        .create()
     }
+  }
+
+  private fun PsiElement.isTypeAttribute(): Boolean {
+    return types.any { text.equals(it, ignoreCase = true) || text.equals("$it[]", ignoreCase = true) }
   }
 }
