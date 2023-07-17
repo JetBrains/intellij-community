a79228c281d8a42993779c249c12659d704dca67
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2023-07-17 15:06:00 +0200

    PY-61669: After review 111111: remove `setWorkDir`
    GitOrigin-RevId: f65548dead397eebb68caf1f67aefd800d50a209

--- python/src/com/jetbrains/python/run/PythonExecution.kt
+++ python/src/com/jetbrains/python/run/PythonExecution.kt
@@ -4,12 +4,11 @@ package com.jetbrains.python.run
 import com.intellij.execution.target.FullPathOnTarget
 import com.intellij.execution.target.value.TargetEnvironmentFunction
 import com.intellij.execution.target.value.constant
-import com.intellij.execution.target.value.getTargetEnvironmentValueForLocalPath
 import com.intellij.openapi.vfs.encoding.EncodingManager
 import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.ApiStatus.ScheduledForRemoval
 import java.io.File
 import java.nio.charset.Charset
-import java.nio.file.Path
 
 /**
  * Represents the Python script or module to be executed and its parameters.
@@ -29,10 +28,6 @@ sealed class PythonExecution {
   var charset: Charset = EncodingManager.getInstance().defaultConsoleEncoding
 
   var inputFile: File? = null
-
-  fun setWorkDir(localPath: Path) {
-    this.workingDir = getTargetEnvironmentValueForLocalPath(localPath)
-  }
 
   fun addParameter(value: String) {
     addParameter(constant(value))
