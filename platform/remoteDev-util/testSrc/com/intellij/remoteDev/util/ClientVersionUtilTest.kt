aa3ebab95b2cc7c4776a9809be7719613e516e02
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2023-07-17 12:35:48 +0200

    [rdct tests] refactoring: convert tests in intellij.remoteDev.util module to JUnit5
    ...and simplify ClientVersionUtilTest using parameterized tests.
    
    GitOrigin-RevId: da96ef4a92674cde5bd3c840a6b406a078c69584

--- platform/remoteDev-util/testSrc/com/intellij/remoteDev/util/ClientVersionUtilTest.kt
+++ platform/remoteDev-util/testSrc/com/intellij/remoteDev/util/ClientVersionUtilTest.kt
@@ -1,41 +1,21 @@
 package com.intellij.remoteDev.util
 
-import org.junit.Assert
-import org.junit.Test
+import com.intellij.remoteDev.util.ClientVersionUtil.isJBCSeparateConfigSupported
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class ClientVersionUtilTest {
-  @Test
-  fun `233_173 is supported`() {
-    Assert.assertTrue(ClientVersionUtil.isJBCSeparateConfigSupported("233.173"))
+  @ValueSource(strings = ["233.173", "233.SNAPSHOT", "241.1", "241.SNAPSHOT"])
+  @ParameterizedTest
+  fun `supported version`(version: String) {
+    assertTrue(isJBCSeparateConfigSupported(version))
   }
 
-  @Test
-  fun `233_172 is not supported`() {
-    Assert.assertFalse(ClientVersionUtil.isJBCSeparateConfigSupported("233.172"))
-  }
-
-  @Test
-  fun `232_9999 is not supported`() {
-    Assert.assertFalse(ClientVersionUtil.isJBCSeparateConfigSupported("232.9999"))
-  }
-
-  @Test
-  fun `241_1 is supported`() {
-    Assert.assertTrue(ClientVersionUtil.isJBCSeparateConfigSupported("241.1"))
-  }
-
-  @Test
-  fun `241_SNAPSHOT is supported`() {
-    Assert.assertTrue(ClientVersionUtil.isJBCSeparateConfigSupported("241.SNAPSHOT"))
-  }
-
-  @Test
-  fun `233_SNAPSHOT is supported`() {
-    Assert.assertTrue(ClientVersionUtil.isJBCSeparateConfigSupported("233.SNAPSHOT"))
-  }
-
-  @Test
-  fun `232_SNAPSHOT is not supported`() {
-    Assert.assertFalse(ClientVersionUtil.isJBCSeparateConfigSupported("232.SNAPSHOT"))
+  @ValueSource(strings = ["233.172", "232.SNAPSHOT", "232.9999"])
+  @ParameterizedTest
+  fun `not supported versions`(version: String) {
+    assertFalse(isJBCSeparateConfigSupported(version))
   }
 }
