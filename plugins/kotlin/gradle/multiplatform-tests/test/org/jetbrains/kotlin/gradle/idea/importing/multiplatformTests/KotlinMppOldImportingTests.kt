ab0efce4d0d8fc4dcad8294bfb3ea14decd5af99
Pavel Kargashinsky <pavel.kargashinsky@jetbrains.com>
2023-07-17 15:57:45 +0200

    [MPP][Tests] Add AndroidManifest.xml in test-data to avoid sdk leaking
    GitOrigin-RevId: 5bbc22dd560bf7ac1e3414cb493119f3bb13e232

--- plugins/kotlin/gradle/multiplatform-tests/test/org/jetbrains/kotlin/gradle/idea/importing/multiplatformTests/KotlinMppOldImportingTests.kt
+++ plugins/kotlin/gradle/multiplatform-tests/test/org/jetbrains/kotlin/gradle/idea/importing/multiplatformTests/KotlinMppOldImportingTests.kt
@@ -1,3 +1,4 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.gradle.idea.importing.multiplatformTests
 
 import com.intellij.lang.annotation.HighlightSeverity
