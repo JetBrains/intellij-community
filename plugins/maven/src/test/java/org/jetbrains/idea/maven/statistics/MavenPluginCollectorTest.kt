e133d90ad7125811196e75c098b8f916b428f832
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2023-07-18 11:14:01 +0200

    IDEA-324252 maven plugin id fixes for versions
    GitOrigin-RevId: 9c9328168c1cdbd1d452b8bfe0981295dcc79a3f

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/statistics/MavenPluginCollectorTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/statistics/MavenPluginCollectorTest.kt
@@ -2,6 +2,8 @@
 package org.jetbrains.idea.maven.statistics
 
 import com.intellij.internal.statistic.FUCollectorTestCase.collectProjectStateCollectorEvents
+import com.intellij.internal.statistic.eventLog.validator.ValidationResultType
+import com.intellij.internal.statistic.eventLog.validator.rules.EventContext
 import com.intellij.maven.testFramework.MavenImportingTestCase
 import org.junit.Test
 
@@ -68,5 +70,66 @@ class MavenPluginCollectorTest : MavenImportingTestCase() {
 
     assertContain(collectedGroupIds, "org.apache.maven.plugins:maven-compiler-plugin")
     //assertDoNotContain(collectedGroupIds, "some-private-plugin:my-plugin")
+  }
+
+  @Test
+  fun `test check whitelist plugin Rule`() {
+    val rule = MavenPluginCoordinatesWhitelistValidationRule()
+    assertEquals(ValidationResultType.ACCEPTED,
+                 rule.validate("org.apache.maven.plugins:maven-eclipse-plugin", EventContext.create("", emptyMap())))
+    assertEquals(ValidationResultType.REJECTED,
+                 rule.validate("org.apache.maven.plugins:my-custom-plugin", EventContext.create("", emptyMap())))
+
+  }
+
+  @Test
+  fun `test check regexp version`() {
+    val rule = MavenPluginVersionValidationRule()
+
+    val acceptedVersions = listOf(
+      "0.0.4",
+      "1.2.3",
+      "10.20.30",
+      "1",
+      "1.2",
+      "1.1.2-prerelease+meta",
+      "1.1.2+meta",
+      "1.1.2+meta-valid",
+      "1.0.0-alpha",
+      "1.0.0-beta",
+      "1.0.0-alpha.beta",
+      "1.0.0-alpha.beta.1",
+      "1.0.0-alpha.1",
+      "1.0.0-alpha0.valid",
+      "1.0.0-alpha.0valid",
+      "1.0.0-alpha-a.b-c-somethinglong+build.1-aef.1-its-okay",
+      "1.0.0-rc.1+build.1",
+      "2.0.0-rc.1+build.123",
+      "1.2.3-beta",
+      "10.2.3-DEV-SNAPSHOT",
+      "1.2.3-SNAPSHOT-123",
+      "1.0.0",
+      "2.0.0",
+      "1.1.7",
+      "2.0.0+build.1848",
+      "2.0.1-alpha.1227",
+      "1.0.0-alpha+beta",
+      "1.2.3----RC-SNAPSHOT.12.9.1--.12+788",
+      "1.2.3----R-S.12.9.1--.12+meta",
+      "1.2.3----RC-SNAPSHOT.12.9.1--.12",
+      "1.0.0+0.build.1-rc.10000aaa-kk-0.1",
+      "99999999999999999999999.999999999999999999.99999999999999999",
+      "1.0.0-0A.is.legal");
+
+    acceptedVersions.forEach {
+      assertEquals(ValidationResultType.ACCEPTED,
+                   rule.validate(it, EventContext.create("", emptyMap())))
+
+    }
+    assertEquals(ValidationResultType.REJECTED,
+                 rule.validate("1.A", EventContext.create("", emptyMap())))
+    assertEquals(ValidationResultType.REJECTED,
+                 rule.validate("Some.String.", EventContext.create("", emptyMap())))
+
   }
 }
