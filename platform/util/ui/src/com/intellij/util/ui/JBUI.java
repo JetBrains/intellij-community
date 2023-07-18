87399f366652846d3fcbd29f3a853def9ac1eecd
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2023-07-18 17:32:37 +0300

    IDEA-323344 Paint different background for animations in different UI themes
    Also edit animation border colors.
    
    GitOrigin-RevId: 08175fb94be56be449bd4af6d9c91a8f0b165c4f

--- platform/util/ui/src/com/intellij/util/ui/JBUI.java
+++ platform/util/ui/src/com/intellij/util/ui/JBUI.java
@@ -1777,6 +1777,10 @@ public final class JBUI {
         return JBColor.namedColor("GotItTooltip.imageBorderColor", background(useContrastColors));
       }
 
+      public static @NotNull Color animationBackground(boolean useContrastColors) {
+        return JBColor.namedColor("GotItTooltip.animationBackground", background(useContrastColors));
+      }
+
       public static @NotNull Color buttonBackgroundContrast() {
         return JBColor.namedColor("Tooltip.Learning.spanBackground", 0x0D5CBD, 0x0250B0);
       }
