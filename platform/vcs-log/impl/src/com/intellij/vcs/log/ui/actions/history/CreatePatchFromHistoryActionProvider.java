c1ccf72af5660cb48acc65695b65b140da5a06db
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2023-07-19 02:51:30 +0200

    [vcs-log] simplify VcsLogUtil#collectChanges arguments
    GitOrigin-RevId: 1e1e93bd6678c247ad0a17569b0301f6a42dc29f

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/history/CreatePatchFromHistoryActionProvider.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/history/CreatePatchFromHistoryActionProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.log.ui.actions.history;
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
@@ -74,7 +74,7 @@ public class CreatePatchFromHistoryActionProvider implements AnActionExtensionPr
     String commitMessage = e.getRequiredData(VcsDataKeys.PRESET_COMMIT_MESSAGE);
 
     selection.requestFullDetails(detailsList -> {
-      List<Change> changes = VcsLogUtil.collectChanges(detailsList, VcsFullCommitDetails::getChanges);
+      List<Change> changes = VcsLogUtil.collectChanges(detailsList);
       CreatePatchFromChangesAction.createPatch(project, commitMessage, changes, mySilentClipboard);
     });
   }
