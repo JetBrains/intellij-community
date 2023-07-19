6cbf49d649ef567a2cc84ff6bc0daf8b2950e060
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-07-19 18:53:01 +0200

    IDEA-325864 look for `ReadCancellationException` in the cause of `PceCancellationException`
    GitOrigin-RevId: 96c4a80fe6e5b5522c065a6b5f248a092b549660

--- platform/platform-impl/src/com/intellij/openapi/application/rw/cancellableReadAction.kt
+++ platform/platform-impl/src/com/intellij/openapi/application/rw/cancellableReadAction.kt
@@ -57,6 +57,13 @@ internal fun <X> cancellableReadActionInternal(ctx: CoroutineContext, action: ()
     }
     throw currentJobCe
   }
+  catch (ce: PceCancellationException) {
+    val pce = ce.cause
+    if (pce.cause is ReadCancellationException) {
+      throw ReadCancellationException(pce)
+    }
+    throw ce
+  }
   catch (ce: CancellationException) {
     readJob.cancel(ce)
     throw ce
