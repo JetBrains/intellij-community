f61c8612f94a1e5ecd7eaed67f2cab13994b98d6
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-07-20 12:22:57 +0200

    [java-highlighting] IDEA-325916 Get rid of deconstruction patterns with pattern variables.
    Clean tests
    
    GitOrigin-RevId: cb3ca829cabc9ab6e96f3ccb77d213896606c93e

--- java/java-tests/testData/codeInsight/gotoDeclaration/FromArrowToDestructuringPattern.java
+++ java/java-tests/testData/codeInsight/gotoDeclaration/FromArrowToDestructuringPattern.java
@@ -2,7 +2,7 @@ record R(int x, int y){}
 class Test{
   void foo(Object o) {
     switch(o){
-      case R(int w, int c) s  -> System.out.println(w<caret>);
+      case R(int w, int c)  -> System.out.println(w<caret>);
       default -> System.out.println()
     }
   }
--- java/java-tests/testData/codeInsight/gotoDeclaration/FromStatementToDestructuringPattern.java
+++ java/java-tests/testData/codeInsight/gotoDeclaration/FromStatementToDestructuringPattern.java
@@ -2,7 +2,7 @@ record R(int x, int y){}
 class Test{
   void foo(Object o) {
     switch(o){
-      case R(int w, int c) s:
+      case R(int w, int c):
         System.out.println(c<caret>);
         break;
       default -> System.out.println()
