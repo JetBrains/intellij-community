51b97bf46dff21508920dad6f0201c419277a419
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2023-07-20 18:58:15 +0300

    IDEA-323344 Show New UI Onboarding dialog at the startup after the first New UI switch
    For now only under `ide.experimental.ui.onboarding` registry key.
    
    (cherry picked from commit afb05fe6457ef6ab73a539eab398171edd3f22e8)
    
    GitOrigin-RevId: 9ee8e0f224abd6183c9f49102fd96e992a345311

---
+++ platform/new-ui-onboarding/src/com/intellij/platform/ide/newUiOnboarding/NewUiOnboardingStartupActivity.kt
@@ -0,0 +1,23 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.ide.newUiOnboarding
+
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.ui.ExperimentalUI
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class NewUiOnboardingStartupActivity : ProjectActivity {
+  override suspend fun execute(project: Project) {
+    if (Registry.`is`("ide.experimental.ui.onboarding")
+        && PropertiesComponent.getInstance().getBoolean(ExperimentalUI.NEW_UI_FIRST_SWITCH)) {
+      PropertiesComponent.getInstance().unsetValue(ExperimentalUI.NEW_UI_FIRST_SWITCH)
+      withContext(Dispatchers.EDT) {
+        NewUiOnboardingService.getInstance(project).showOnboardingDialog()
+      }
+    }
+  }
+}
