dbb4b11cbeba5410634d1286d8b7f8419a3c3e5b
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-07-20 12:14:09 +0200

    [java-completion] IDEA-324653 Completion for objects with enums. Fix tests
    GitOrigin-RevId: ff1ca41e8ef385d6abae9c64eaf9177e549a73e6

--- java/java-tests/testData/codeInsight/completion/normal/QualifierEnumConstantInSwitchWithObject_after.java
+++ java/java-tests/testData/codeInsight/completion/normal/QualifierEnumConstantInSwitchWithObject_after.java
@@ -1,14 +1,11 @@
 class Main {
 
   public enum En {
-    HHHH, HE, LI, BE, B, C, N, O, F, NE
+    HHHH
   }
   public void foo(Object o) {
     switch (o) {
-      case String s -> System.out.println(s);
-      case Integer i -> System.out.println(i);
-        case En.HHHH<caret> -> System.out.println("En -> H");
-      case null, default -> System.out.println("null, default");
+        case En.HHHH -> <caret>
     }
   }}
 
