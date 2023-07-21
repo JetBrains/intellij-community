c0bba0f7f7ccd569088388a3ac2fbdaf0feaf1b6
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-07-21 09:39:53 +0200

    Cleanup (needless nullability; invalid markup)
    GitOrigin-RevId: acd632cccd7dcf1d428c52453ef24da41e7b66c9

--- platform/build-scripts/src/org/jetbrains/intellij/build/MacOsCodesignIdentity.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/MacOsCodesignIdentity.kt
@@ -1,8 +1,8 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.intellij.build
 
 /**
  * Full name of a keychain identity (Applications > Utilities > Keychain Access).
- * More info in SIGNING IDENTITIES (https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man1/codesign.1.html)
+ * More info in [SIGNING IDENTITIES](https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man1/codesign.1.html).
  */
-data class MacOsCodesignIdentity(val value: String?)
+data class MacOsCodesignIdentity(val value: String)
