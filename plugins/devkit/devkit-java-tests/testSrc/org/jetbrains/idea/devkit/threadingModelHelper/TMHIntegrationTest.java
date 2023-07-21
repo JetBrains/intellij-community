d1eff6c1bd1e116bd2f7a98364a2f9b79898ff96
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-07-21 19:11:20 +0200

    move threading model helper tests to proper packages
    GitOrigin-RevId: f746f0e30b41d17226e6c519eb045714c48973b8

--- plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/threadingModelHelper/TMHIntegrationTest.java
+++ plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/threadingModelHelper/TMHIntegrationTest.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.application.impl;
+package org.jetbrains.idea.devkit.threadingModelHelper;
 
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
--- plugins/devkit/jps-plugin/testSrc/org/jetbrains/jps/devkit/threadingModelHelper/TMHInstrumenterTest.java
+++ plugins/devkit/jps-plugin/testSrc/org/jetbrains/jps/devkit/threadingModelHelper/TMHInstrumenterTest.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.application.impl;
+package org.jetbrains.jps.devkit.threadingModelHelper;
 
 import com.intellij.openapi.application.ex.PathManagerEx;
 import com.intellij.openapi.util.io.FileUtil;
@@ -10,7 +10,6 @@ import com.intellij.util.ExceptionUtil;
 import com.intellij.util.ThrowableRunnable;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.jps.devkit.threadingModelHelper.TMHTestUtil;
 
 import java.io.File;
 import java.io.IOException;
