7660bebc5e5d56b8cad57b58df29f8a7c8faf9d2
Alisa Afonina <alisa.afonina@jetbrains.com>
2023-07-21 15:24:13 +0200

    fix IDEA-326009 [Popups height is too big] because of call of abstract method in constructor used field inside this method was not properly initialized
    GitOrigin-RevId: 0a76803c6c94d7e742be20ec32353dddbddab867

--- platform/platform-impl/src/com/intellij/ui/popup/list/ListPopupImpl.java
+++ platform/platform-impl/src/com/intellij/ui/popup/list/ListPopupImpl.java
@@ -50,6 +50,7 @@ import static java.awt.event.InputEvent.META_MASK;
 
 public class ListPopupImpl extends WizardPopup implements ListPopup, NextStepHandler {
   static final int NEXT_STEP_AREA_WIDTH = 20;
+  private static final int DEFAULT_MAX_ROW_COUNT = 30;
 
   private static final Logger LOG = Logger.getInstance(ListPopupImpl.class);
   protected final PopupInlineActionsSupport myPopupInlineActionsSupport = PopupInlineActionsSupport.Companion.create(this);
@@ -63,7 +64,6 @@ public class ListPopupImpl extends WizardPopup implements ListPopup, NextStepHan
   private ListPopupModel myListModel;
 
   private int myIndexForShowingChild = -1;
-  private int myMaxRowCount = 30;
   private boolean myAutoHandleBeforeShow;
   private boolean myShowSubmenuOnHover;
 
@@ -90,8 +90,7 @@ public class ListPopupImpl extends WizardPopup implements ListPopup, NextStepHan
 
   public void setMaxRowCount(int maxRowCount) {
     if (maxRowCount <= 0) return;
-    myMaxRowCount = maxRowCount;
-    myList.setVisibleRowCount(myMaxRowCount);
+    myList.setVisibleRowCount(maxRowCount);
   }
 
   public void showUnderneathOfLabel(@NotNull JLabel label) {
@@ -256,7 +255,7 @@ public class ListPopupImpl extends WizardPopup implements ListPopup, NextStepHan
     ListPopupStep<Object> step = getListStep();
     myListModel = new ListPopupModel(this, getSpeedSearch(), step);
     myList = new MyList();
-    myList.setVisibleRowCount(myMaxRowCount);
+    myList.setVisibleRowCount(DEFAULT_MAX_ROW_COUNT);
     if (myStep.getTitle() != null) {
       myList.getAccessibleContext().setAccessibleName(myStep.getTitle());
     }
