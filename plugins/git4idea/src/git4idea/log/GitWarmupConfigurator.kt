6aa9e8c4a883d506660e9a10e1c8e78360690339
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2023-07-24 17:16:35 +0200

    [warmup] Use cancellation propagation to await git log indexing
    GitOrigin-RevId: 08ffb57b438e2897167a9631c7f859833e84641c

---
+++ plugins/git4idea/src/git4idea/log/GitWarmupConfigurator.kt
@@ -0,0 +1,53 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package git4idea.log
+
+import com.intellij.ide.CommandLineProgressReporterElement
+import com.intellij.ide.warmup.WarmupConfigurator
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.components.service
+import com.intellij.openapi.components.serviceAsync
+import com.intellij.openapi.progress.blockingContextScope
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.openapi.vcs.ProjectLevelVcsManager
+import com.intellij.vcs.log.impl.VcsLogManager
+import com.intellij.vcs.log.impl.VcsLogProjectTabsProperties
+import com.intellij.vcs.log.impl.VcsLogSharedSettings
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.coroutineContext
+
+/**
+ * Awaits git log indexing process
+ */
+class GitWarmupConfigurator : WarmupConfigurator {
+
+  override val configuratorPresentableName: String = "git"
+
+  override suspend fun runWarmup(project: Project): Boolean {
+    val logger = coroutineContext[CommandLineProgressReporterElement.Key]?.reporter
+    if (!Registry.`is`("vcs.log.index.git") || !VcsLogSharedSettings.isIndexSwitchedOn(project)) {
+      logger?.reportMessage(1, "Indexing of git log is disabled")
+      return false
+    }
+
+    val projectLevelVcsManager = project.serviceAsync<ProjectLevelVcsManager>()
+    val logProviders = VcsLogManager.findLogProviders(projectLevelVcsManager.allVcsRoots.toList(), project)
+    if (logProviders.isEmpty()) {
+      logger?.reportMessage(1, "No git roots to index")
+      return false
+    }
+    val manager = blockingContextScope {
+      VcsLogManager(project, project.service<VcsLogProjectTabsProperties>(), logProviders, true) { a, b -> }
+    }
+    withContext(Dispatchers.EDT) {
+      blockingContextScope {
+        manager.dispose(null)
+      }
+    }
+
+    logger?.reportMessage(1, "Git log indexing has finished")
+    return false
+  }
+}
