56e330331eda8f44d8260480e225ff9d1fa950be
Alexey Kryuchkov <alexey.kryuchkov@jetbrains.com>
2023-07-24 16:28:41 +0300

    Temporary revert "Store VCS persist storage metrics" to watch up for metrics behavior
    This reverts commit 35102847
    
    GitOrigin-RevId: 6e767bc803b7b71a07a4dcebf58874a7c91ab4da

--- platform/vcs-log/impl/src/com/intellij/vcs/log/data/index/VcsLogPersistentIndex.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/data/index/VcsLogPersistentIndex.java
@@ -53,7 +53,6 @@ import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
 import static com.intellij.openapi.vcs.VcsScopeKt.VcsScope;
-import static com.intellij.platform.diagnostic.telemetry.helpers.TraceUtil.runWithSpanThrows;
 import static com.intellij.vcs.log.util.PersistentUtil.calcLogId;
 
 public final class VcsLogPersistentIndex implements VcsLogModifiableIndex, Disposable {
@@ -144,8 +143,7 @@ public final class VcsLogPersistentIndex implements VcsLogModifiableIndex, Dispo
         if (oldFull == null) return full;
         return oldFull || full;
       });
-    }
-    else {
+    } else {
       doScheduleIndex(full);
     }
   }
@@ -191,11 +189,7 @@ public final class VcsLogPersistentIndex implements VcsLogModifiableIndex, Dispo
 
   private void storeDetail(@NotNull VcsLogIndexer.CompressedDetails detail, @NotNull VcsLogWriter mutator) {
     try {
-      runWithSpanThrows(TelemetryManager.getInstance().getTracer(VcsScope), "store detail",
-                        span -> {
-                          span.setAttribute("vcsLogWriter", mutator.getClass().getName());
-                          mutator.putCommit(myStorage.getCommitIndex(detail.getId(), detail.getRoot()), detail);
-                        });
+      mutator.putCommit(myStorage.getCommitIndex(detail.getId(), detail.getRoot()), detail);
     }
     catch (IOException | UncheckedIOException e) {
       myErrorHandler.handleError(VcsLogErrorHandler.Source.Index, e);
