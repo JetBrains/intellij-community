f8d6187b9b8e3844c50b597f950cdbba084d78e0
Roman Ivanov <roman.ivanov@jetbrains.com>
2023-07-24 16:32:10 +0200

    more specific documentation for PsiClass
    GitOrigin-RevId: 6bd5a2965a1d028d22a83e2c88cadaaff57ab74f

--- java/java-psi-api/src/com/intellij/psi/PsiUnnamedClass.java
+++ java/java-psi-api/src/com/intellij/psi/PsiUnnamedClass.java
@@ -1,8 +1,36 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi;
 
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.Nullable;
+
 /**
  * Represents unnamed class from <a href="https://openjdk.org/jeps/445">JEP 445</a>
  */
+@ApiStatus.Experimental
 public interface PsiUnnamedClass extends PsiClass {
+  @Contract("-> null")
+  @Override
+  @Nullable String getQualifiedName();
+
+  @Contract("-> null")
+  @Override
+  @Nullable PsiIdentifier getNameIdentifier();
+
+  @Contract("-> null")
+  @Override
+  @Nullable String getName();
+
+  @Contract("-> null")
+  @Override
+  @Nullable PsiElement getLBrace();
+
+  @Contract("-> null")
+  @Override
+  @Nullable PsiElement getRBrace();
+
+  @Contract("-> null")
+  @Override
+  @Nullable PsiClass getContainingClass();
 }
