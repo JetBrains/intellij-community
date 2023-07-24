da73617396012f0cd985dcb689ad15a5a46ea40c
Ivan Semenov <ivan.semenov@jetbrains.com>
2023-07-24 11:13:44 +0200

    [collab/gitlab] show more info for json errors
    (cherry picked from commit f51a3306557a048012b63deac7009a4625f920e5)
    
    GitOrigin-RevId: 1c417e2e796dbaa10db6904f39392258bd8b0e45

--- platform/collaboration-tools/src/com/intellij/collaboration/api/json/HttpJsonDeserializationException.kt
+++ platform/collaboration-tools/src/com/intellij/collaboration/api/json/HttpJsonDeserializationException.kt
@@ -1,9 +1,8 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.collaboration.api.json
 
-class HttpJsonDeserializationException : RuntimeException {
-  constructor(requestName: String) : super(getMessage(requestName))
-  constructor(requestName: String, cause: Throwable) : super(getMessage(requestName), cause)
-}
+class HttpJsonDeserializationException(requestName: String, cause: Throwable)
+  : RuntimeException(getMessage(requestName, cause), cause)
 
-private fun getMessage(requestName: String) = "Deserialization of $requestName response to JSON failed"
+private fun getMessage(requestName: String, cause: Throwable) =
+  "Deserialization of $requestName response to JSON failed - ${cause.localizedMessage}"
