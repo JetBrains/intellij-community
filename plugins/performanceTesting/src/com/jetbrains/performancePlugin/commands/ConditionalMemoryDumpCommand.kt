f9fab525fd6c5f794e45e56cabf1bd1c24c22eb1
roman.ivanitskii <roman.ivanitskii@jetbrains.com>
2023-07-24 13:53:26 +0300

    [performance-tests] add an error message for conditional memory dump command
    GitOrigin-RevId: 99fe2cb547b416473b5cc7381cc3420f41f5b76d

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/ConditionalMemoryDumpCommand.kt
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/ConditionalMemoryDumpCommand.kt
@@ -6,17 +6,21 @@ import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.ui.playback.PlaybackContext
 import com.intellij.openapi.util.LowMemoryWatcher
 import com.intellij.util.MemoryDumpHelper
+import java.lang.IllegalStateException
 
 class ConditionalMemoryDumpCommand(text: String, line: Int) : PerformanceCommandCoroutineAdapter(text, line) {
   companion object {
     const val NAME = "conditionalMemoryDumpCommand"
     const val PREFIX = CMD_PREFIX + NAME
+    const val WITH_ERROR_TEXT = "WITH_ERROR_MESSAGE"
     private val LOG = logger<ConditionalMemoryDumpCommand>()
   }
 
   override suspend fun doExecute(context: PlaybackContext) {
-    val targetMessageCount = extractCommandArgument(PREFIX).toInt()
+    val params = extractCommandArgument(PREFIX).split("\\s+".toRegex())
+    val targetMessageCount = params[0].toInt()
     var currentMessageCount = 0
+    val withErrorMessages = params.contains(WITH_ERROR_TEXT)
 
     @Suppress("IncorrectParentDisposable")
     LowMemoryWatcher.register({
@@ -25,6 +29,9 @@ class ConditionalMemoryDumpCommand(text: String, line: Int) : PerformanceCommand
           val memoryDumpPath = MemoryDumpCommand.getMemoryDumpPath()
           LOG.info("Dumping memory snapshot to: $memoryDumpPath")
           MemoryDumpHelper.captureMemoryDump(memoryDumpPath)
+          if (withErrorMessages) {
+            throw IllegalStateException("Got low memory signal, memory dump attached")
+          }
         }
       //we can't register a proper disposable since we wait till all commands are finished
       }, LowMemoryWatcher.LowMemoryWatcherType.ONLY_AFTER_GC, ApplicationManager.getApplication())
