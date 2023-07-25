effbdc5baff4e58c08753815bdb60b336198c257
Maria Filipanova <maria.filipanova@jetbrains.com>
2023-07-25 17:19:42 +0200

    RDCT-401: transfer completion inlays
    GitOrigin-RevId: 05e10cc3583396ac9e451bae927539ff7726236a

--- platform/platform-impl/src/com/intellij/codeInsight/inline/completion/listeners/InlineCompletionListeners.kt
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/listeners/InlineCompletionListeners.kt
@@ -4,16 +4,14 @@ package com.intellij.codeInsight.inline.completion.listeners
 import com.intellij.codeInsight.completion.CompletionUtil
 import com.intellij.codeInsight.inline.completion.InlineCompletionContext.Companion.resetInlineCompletionContext
 import com.intellij.codeInsight.inline.completion.InlineCompletionHandler
+import com.intellij.codeWithMe.ClientId
+import com.intellij.codeWithMe.isCurrent
+import com.intellij.openapi.editor.ClientEditorManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.event.DocumentListener
-import com.intellij.openapi.editor.event.EditorMouseEvent
-import com.intellij.openapi.editor.event.EditorMouseListener
 import com.intellij.openapi.editor.ex.FocusChangeListener
 import com.intellij.openapi.editor.impl.EditorImpl
-import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.fileEditor.TextEditor
-import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl
 import org.jetbrains.annotations.ApiStatus
 import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
@@ -21,7 +19,7 @@ import java.awt.event.KeyEvent
 @ApiStatus.Experimental
 class InlineCompletionDocumentListener(private val editor: EditorImpl) : DocumentListener {
   override fun documentChanged(event: DocumentEvent) {
-    if (!isEnabled(event) || event.document.isInBulkUpdate) {
+    if (!isEnabled(event) || event.document.isInBulkUpdate || !(ClientEditorManager.getClientId(editor) ?: ClientId.localId).isCurrent()) {
       return
     }
 
