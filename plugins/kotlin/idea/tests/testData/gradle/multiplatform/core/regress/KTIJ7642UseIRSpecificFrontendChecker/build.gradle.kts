220c1342d1fa807b0e6d08dbf1add3f592c276fe
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-07-26 16:29:09 +0200

    [KTIJ-7642] Multiplatform: Shared Source Sets: set JvmAnalysisFlags.useIR to true by default
    There is at least one FE checker (SuspendInFunInterfaceChecker) which
    is reading the 'useIR' analysis flag. Since non-IR mode is phased out
    heavily already, it seems fair to just configure shared source sets to
    include this flag by default.
    
    ^KTIJ-7642 Verification Pending
    
    (cherry picked from commit 66fe6191e1a3485c1f4e39ce5147ccccdbc338e4)
    
    GitOrigin-RevId: ca6d7d45a9eae74f3dcbbc7525dd54c44aeae39e

---
+++ plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/regress/KTIJ7642UseIRSpecificFrontendChecker/build.gradle.kts
@@ -0,0 +1,19 @@
+buildscript {
+    repositories {
+        {{kts_kotlin_plugin_repositories}}
+    }
+}
+
+repositories {
+    {{kts_kotlin_plugin_repositories}}
+}
+
+plugins {
+    kotlin("multiplatform").version("{{kgp_version}}")
+}
+
+kotlin {
+    jvm()
+    linuxX64()
+    linuxArm64()
+}
