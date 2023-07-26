20f970b92841ba8889a32f034514938fe9fed9a0
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2023-07-26 15:19:03 +0200

    IDEA-322041 Initialize project color in workspace in all cases
    GitOrigin-RevId: c9d92a1754c12006665315ef2fab8c7824074693

--- platform/platform-impl/src/com/intellij/ide/ProjectWindowCustomizerService.kt
+++ platform/platform-impl/src/com/intellij/ide/ProjectWindowCustomizerService.kt
@@ -46,26 +46,6 @@ private class ProjectWindowCustomizerIconCache(private val project: Project) {
     busConnection.subscribe(LafManagerListener.TOPIC, LafManagerListener {
       cachedIcon.drop()
     })
-
-    // Perform initial setup of storages for the project
-    ProjectWindowCustomizerService.projectPath(project)?.let { path ->
-      val workspaceStorage = WorkspaceProjectColorStorage(project)
-      val recentProjectsStorage = RecentProjectColorStorage(path)
-
-      if (workspaceStorage.isEmpty) {
-        // Clean caches
-        ProjectWindowCustomizerService.getInstance().clearToolbarColorsAndInMemoryCache(project)
-
-        if (recentProjectsStorage.isEmpty) {
-          // If recent projects storage is empty, generate the associated index and save it into workspace
-          ProjectWindowCustomizerService.getInstance().getCurrentProjectColorIndex(project)
-        }
-        else {
-          // Otherwise, get colors from recent projects storage
-          workspaceStorage.getDataFrom(recentProjectsStorage)
-        }
-      }
-    }
   }
 
   private fun getIconRaw(): Icon {
@@ -257,6 +237,26 @@ class ProjectWindowCustomizerService : Disposable {
   }
 
   @Internal
+  internal fun setupWorkspaceStorage(project: Project) {
+    clearToolbarColorsAndInMemoryCache(project)
+
+    val workspaceStorage = WorkspaceProjectColorStorage(project)
+    if (!workspaceStorage.isEmpty) return
+
+    // Perform initial setup of storages for the project
+    val path = projectPath(project) ?: return
+    val recentProjectsStorage = RecentProjectColorStorage(path)
+
+    if (recentProjectsStorage.isEmpty) {
+      // If recent projects storage is empty, generate the associated index and save it into workspace
+      getOrGenerateAssociatedColorIndex(workspaceStorage)
+    }
+    else {
+      workspaceStorage.getDataFrom(recentProjectsStorage)
+    }
+  }
+
+  @Internal
   fun clearToolbarColorsAndInMemoryCache(project: Project) {
     clearToolbarColorsAndInMemoryCache(storageFor(project))
   }
@@ -406,7 +406,9 @@ private class ProjectWindowCustomizerListener : ProjectActivity, UISettingsListe
   }
 
   override suspend fun execute(project: Project) {
-    serviceAsync<ProjectWindowCustomizerService>().enableIfNeeded()
+    val service = serviceAsync<ProjectWindowCustomizerService>()
+    service.enableIfNeeded()
+    service.setupWorkspaceStorage(project)
   }
 
   override fun uiSettingsChanged(uiSettings: UISettings) {
