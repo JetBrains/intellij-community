b8e0b6d877df37c8583389a33a8cf4b642cf2081
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-07-26 23:58:37 +0200

    VFSErrorsToFUSReporter should not prevent app closing
    GitOrigin-RevId: 7ff3059d55c6dd303e8bcc1c4e9c15ca375555f0

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/FSRecords.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/FSRecords.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vfs.newvfs.persistent;
 
 import com.intellij.openapi.application.PathManager;
@@ -119,6 +119,10 @@ public final class FSRecords {
     return implOrFail();
   }
 
+  static @Nullable FSRecordsImpl getInstanceIfCreatedAndNotDisposed() {
+    FSRecordsImpl _impl = impl;
+    return _impl == null || _impl.isDisposed() ? null : _impl;
+  }
 
   //========== FS records-as-a-whole properties: ==============================
 
