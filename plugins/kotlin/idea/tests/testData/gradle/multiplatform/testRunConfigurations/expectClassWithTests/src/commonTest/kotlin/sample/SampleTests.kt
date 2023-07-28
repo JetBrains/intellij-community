a90db2d73a821016af52014e90f0230ee628e556
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2023-07-28 13:32:37 +0200

    [Kotlin] Update testdata to make tests pass
    Because of the previous commits (KT-59304) the HMPP is enabled by default if the corresponding property
    is not specified. And for very old projects it is not specified (here in test 1.3.61 is used).
    
    That slightly changed the test behaviour. But the fact that we are still testing something against 1.3.61
    is something that we should solve.
    
    #KTIJ-26437 Open
    
    (cherry picked from commit f3c4fb290d728308b34aada7d95d1335e7d8c7c4)
    
    GitOrigin-RevId: 1d1c6748c51d7785606ddbb99704a4618dc55f89

--- plugins/kotlin/idea/tests/testData/gradle/multiplatform/testRunConfigurations/expectClassWithTests/src/commonTest/kotlin/sample/SampleTests.kt
+++ plugins/kotlin/idea/tests/testData/gradle/multiplatform/testRunConfigurations/expectClassWithTests/src/commonTest/kotlin/sample/SampleTests.kt
@@ -3,7 +3,7 @@ package sample
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
-expect class <lineMarker descr="Run Test" settings=":cleanJsBrowserTest :jsBrowserTest --tests \"sample.SampleTests\" :cleanJsNodeTest :jsNodeTest --tests \"sample.SampleTests\" :cleanJvmTest :jvmTest --tests \"sample.SampleTests\" --continue"><lineMarker descr="Has actuals in [{JS}, {JVM}] modules">SampleTests</lineMarker></lineMarker> {
+expect class <lineMarker descr="Run Test" settings=":cleanJsBrowserTest :jsBrowserTest --tests \"sample.SampleTests\" :cleanJsNodeTest :jsNodeTest --tests \"sample.SampleTests\" :cleanJvmTest :jvmTest --tests \"sample.SampleTests\" --continue"><lineMarker descr="Has actuals in [mppLibrary.jsTest, mppLibrary.jvmTest] modules">SampleTests</lineMarker></lineMarker> {
     @Test
-    fun <lineMarker descr="Run Test" settings=":cleanJsBrowserTest :jsBrowserTest --tests \"sample.SampleTests.testMe\" :cleanJsNodeTest :jsNodeTest --tests \"sample.SampleTests.testMe\" :cleanJvmTest :jvmTest --tests \"sample.SampleTests.testMe\" --continue"><lineMarker descr="Has actuals in [{JS}, {JVM}] modules">testMe</lineMarker></lineMarker>()
+    fun <lineMarker descr="Run Test" settings=":cleanJsBrowserTest :jsBrowserTest --tests \"sample.SampleTests.testMe\" :cleanJsNodeTest :jsNodeTest --tests \"sample.SampleTests.testMe\" :cleanJvmTest :jvmTest --tests \"sample.SampleTests.testMe\" --continue"><lineMarker descr="Has actuals in [mppLibrary.jsTest, mppLibrary.jvmTest] modules">testMe</lineMarker></lineMarker>()
 }
