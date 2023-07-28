d33bc258463a86024cf5cec800f7c473b6c42bf4
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2023-07-28 13:32:37 +0200

    [Kotlin] Update testdata to make tests pass
    Because of the previous commits (KT-59304) the HMPP is enabled by default if the corresponding property
    is not specified. And for very old projects it is not specified (here in test 1.3.61 is used).
    
    That slightly changed the test behaviour. But the fact that we are still testing something against 1.3.61
    is something that we should solve.
    
    #KTIJ-26437 Open
    
    (cherry picked from commit f3c4fb290d728308b34aada7d95d1335e7d8c7c4)
    
    GitOrigin-RevId: e90fd8e6542cfe785d12f72de8e8d6ed24c054a8

--- plugins/kotlin/idea/tests/testData/gradle/multiplatform/testRunConfigurations/expectClassWithTests/src/jsTest/kotlin/sample/SampleTestsJS.kt
+++ plugins/kotlin/idea/tests/testData/gradle/multiplatform/testRunConfigurations/expectClassWithTests/src/jsTest/kotlin/sample/SampleTestsJS.kt
@@ -4,8 +4,8 @@ import kotlin.test.Test
 import kotlin.test.assertTrue
 
 // JS
-actual class <lineMarker descr="Run Test" settings=":cleanJsBrowserTest :jsBrowserTest --tests \"sample.SampleTests\" :cleanJsNodeTest :jsNodeTest --tests \"sample.SampleTests\" --continue"><lineMarker descr="Has expects in ?common? module">SampleTests</lineMarker></lineMarker> {
+actual class <lineMarker descr="Run Test" settings=":cleanJsBrowserTest :jsBrowserTest --tests \"sample.SampleTests\" :cleanJsNodeTest :jsNodeTest --tests \"sample.SampleTests\" --continue"><lineMarker descr="Has expects in mppLibrary.commonTest module">SampleTests</lineMarker></lineMarker> {
     @Test
-    actual fun <lineMarker descr="Run Test" settings=":cleanJsBrowserTest :jsBrowserTest --tests \"sample.SampleTests.testMe\" :cleanJsNodeTest :jsNodeTest --tests \"sample.SampleTests.testMe\" --continue"><lineMarker descr="Has expects in ?common? module">testMe</lineMarker></lineMarker>() {
+    actual fun <lineMarker descr="Run Test" settings=":cleanJsBrowserTest :jsBrowserTest --tests \"sample.SampleTests.testMe\" :cleanJsNodeTest :jsNodeTest --tests \"sample.SampleTests.testMe\" --continue"><lineMarker descr="Has expects in mppLibrary.commonTest module">testMe</lineMarker></lineMarker>() {
     }
 }
