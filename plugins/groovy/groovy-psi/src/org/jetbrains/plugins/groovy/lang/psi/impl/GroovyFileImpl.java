89aaad7cd79d51563496072ad3e77711e12054b7
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2023-07-28 13:14:10 +0200

    IDEA-325427: Revert "[groovy] Flush caches less aggressively"
    This reverts commit d30068c19c539d20b033f5371fc5f4d7ad3463af.
    
    GitOrigin-RevId: 5788712368756cb6e851ee906360cc593349e59b

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/GroovyFileImpl.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/GroovyFileImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.psi.impl;
 
 import com.intellij.lang.ASTNode;
@@ -300,9 +300,7 @@ public class GroovyFileImpl extends GroovyFileBaseImpl implements GroovyFile, Ps
   @Override
   public void clearCaches() {
     super.clearCaches();
-    if (!isScript()) {
-      myScriptClass = null;
-    }
+    myScriptClass = null;
     mySyntheticArgsParameter = null;
   }
 
