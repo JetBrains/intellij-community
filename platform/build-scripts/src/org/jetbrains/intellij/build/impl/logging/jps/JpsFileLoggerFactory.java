7f71c1900f2ff75bee4a90d641d3afc9e048e024
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2023-07-28 19:50:21 +0200

    build scripts: Jps file logger converted to Kotlin
    GitOrigin-RevId: 926b9f7eaf5cac926861f6159a7699f31f511e6c

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/logging/jps/JpsFileLoggerFactory.java
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/logging/jps/JpsFileLoggerFactory.java
@@ -1,50 +1,39 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.intellij.build.impl.logging.jps;
 
-import com.intellij.openapi.diagnostic.IdeaLogRecordFormatter;
-import com.intellij.openapi.diagnostic.JulLogger;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.diagnostic.Logger.Factory;
-import com.intellij.openapi.diagnostic.RollingFileHandler;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.diagnostic.IdeaLogRecordFormatter
+import com.intellij.openapi.diagnostic.JulLogger
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.Logger.Factory
+import com.intellij.openapi.diagnostic.RollingFileHandler
+import org.jetbrains.annotations.ApiStatus
 
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.logging.Level;
+import java.nio.file.Path
+import java.util.logging.Level
 
 @ApiStatus.Internal
-public class JpsFileLoggerFactory implements Factory {
-  private final RollingFileHandler myAppender;
-  private final List<String> myCategoriesWithDebugLevel;
+class JpsFileLoggerFactory(logFile: Path, categoriesWithDebugLevel: String) : Factory {
+  private val appender: RollingFileHandler
+  private val categoriesWithDebugLevel: List<String>
 
-  public JpsFileLoggerFactory(Path logFile, String categoriesWithDebugLevel) {
-    myCategoriesWithDebugLevel = categoriesWithDebugLevel.isEmpty() ? Collections.emptyList() : Arrays.asList(categoriesWithDebugLevel.split(","));
-    myAppender = new RollingFileHandler(logFile, 20_000_000L, 10, true);
-    myAppender.setFormatter(new IdeaLogRecordFormatter());
+  init {
+    this.categoriesWithDebugLevel = if (categoriesWithDebugLevel.isEmpty()) emptyList() else categoriesWithDebugLevel.split(",")
+    this.appender = RollingFileHandler(logFile, 20_000_000L, 10, true)
+    this.appender.setFormatter(IdeaLogRecordFormatter())
   }
 
-  @NotNull
-  @Override
-  public Logger getLoggerInstance(@NotNull String category) {
-    var logger = java.util.logging.Logger.getLogger(category);
-    var level = isDebugLevel(category) ? Level.FINE : Level.INFO;
-    JulLogger.clearHandlers(logger);
-    myAppender.setLevel(level);
-    logger.addHandler(myAppender);
-    logger.setUseParentHandlers(false);
-    logger.setLevel(level);
-    return new JulLogger(logger);
+  override fun getLoggerInstance(category: String): Logger {
+    val logger = java.util.logging.Logger.getLogger(category)
+    val level = if (isDebugLevel(category)) Level.FINE else Level.INFO
+    JulLogger.clearHandlers(logger)
+    appender.setLevel(level)
+    logger.addHandler(appender)
+    logger.setUseParentHandlers(false)
+    logger.setLevel(level)
+    return JulLogger(logger)
   }
 
-  private boolean isDebugLevel(String category) {
-    for (String debug : myCategoriesWithDebugLevel) {
-      if (category.startsWith(debug)) {
-        return true;
-      }
-    }
-    return false;
+  private fun isDebugLevel(category: String): Boolean {
+    return categoriesWithDebugLevel.any(category::startsWith)
   }
 }
