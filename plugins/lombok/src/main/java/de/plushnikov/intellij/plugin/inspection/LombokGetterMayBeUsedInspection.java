61892c0ee0a2749ac2acf762eed314ef8d38e1bd
Michail Plushnikov <michail.plushnikov@jetbrains.com>
2023-07-30 19:51:07 +0200

    [lombok] IDEA-326444 Fix handling of static fields already containing lombok annotation
    GitOrigin-RevId: f347fea37b87f2f21229bf75a997c7cdce861f75

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/inspection/LombokGetterMayBeUsedInspection.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/inspection/LombokGetterMayBeUsedInspection.java
@@ -58,15 +58,14 @@ public class LombokGetterMayBeUsedInspection extends LombokJavaInspectionBase im
       for (PsiField field : psiClass.getFields()) {
         PsiAnnotation annotation = field.getAnnotation(LombokClassNames.GETTER);
         if (annotation != null) {
-          if (!annotation.getAttributes().isEmpty()) {
+          if (!annotation.getAttributes().isEmpty() || field.hasModifierProperty(PsiModifier.STATIC)) {
             isGetterAtClassLevel = false;
           }
           else {
             annotatedFields.add(field);
           }
-          break;
         }
-        if (!field.hasModifierProperty(PsiModifier.STATIC)) {
+        else if (!field.hasModifierProperty(PsiModifier.STATIC)) {
           boolean found = false;
           for (Pair<PsiField, PsiMethod> instanceCandidate : instanceCandidates) {
             if (field.equals(instanceCandidate.getFirst())) {
@@ -74,7 +73,11 @@ public class LombokGetterMayBeUsedInspection extends LombokJavaInspectionBase im
               break;
             }
           }
-          isGetterAtClassLevel &= found;
+          isGetterAtClassLevel = found;
+        }
+
+        if (!isGetterAtClassLevel) {
+          break;
         }
       }
       List<Pair<PsiField, PsiMethod>> allCandidates = new ArrayList<>(staticCandidates);
