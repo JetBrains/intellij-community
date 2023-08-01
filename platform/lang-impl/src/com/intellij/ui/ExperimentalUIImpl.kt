8a8b0c08f6f9d85350cf52efb6dccc23c2722a68
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2023-08-01 12:22:53 +0300

    IDEA-326748 Do not show tool window stripes forcefully on enabling New UI in DFM
    Use the proper method that reflects the Main toolbar state.
    
    GitOrigin-RevId: 836c8fbea523cbe0a1eb015010fea53d0f8f1e8c

--- platform/lang-impl/src/com/intellij/ui/ExperimentalUIImpl.kt
+++ platform/lang-impl/src/com/intellij/ui/ExperimentalUIImpl.kt
@@ -7,7 +7,7 @@ import com.intellij.feedback.new_ui.state.NewUIInfoService
 import com.intellij.icons.AllIcons
 import com.intellij.ide.AppLifecycleListener
 import com.intellij.ide.IdeBundle
-import com.intellij.ide.actions.DistractionFreeModeController
+import com.intellij.ide.actions.ToggleDistractionFreeModeAction
 import com.intellij.ide.ui.IconMapLoader
 import com.intellij.ide.ui.LafManager
 import com.intellij.ide.ui.UISettings
@@ -98,7 +98,8 @@ private class ExperimentalUIImpl : ExperimentalUI() {
 
     if (isEnabled) {
       NewUIInfoService.getInstance().updateEnableNewUIDate()
-      if (!DistractionFreeModeController.isDistractionFreeModeEnabled()) {
+      // Do not force enabling tool window stripes in DFM
+      if (!ToggleDistractionFreeModeAction.shouldMinimizeCustomHeader()) {
         UISettings.getInstance().hideToolStripes = false
       }
     }
