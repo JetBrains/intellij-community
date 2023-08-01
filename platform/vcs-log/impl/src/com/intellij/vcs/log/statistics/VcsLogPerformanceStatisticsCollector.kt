878ab75da02532190bb1269c0c694df947aac50a
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2023-08-01 11:20:01 +0200

    [vcs-log] past tense for FUS event ids in performance statistic collector
    GitOrigin-RevId: e2ff7c385ab51207721a04f5bea9307c63ca3157

--- platform/vcs-log/impl/src/com/intellij/vcs/log/statistics/VcsLogPerformanceStatisticsCollector.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/statistics/VcsLogPerformanceStatisticsCollector.kt
@@ -8,13 +8,12 @@ import com.intellij.internal.statistic.eventLog.events.EventFields
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
 
 object VcsLogPerformanceStatisticsCollector : CounterUsagesCollector() {
-  private val GROUP = EventLogGroup("vcs.log.performance", 1)
-
-  val FILE_HISTORY_COMPUTING = GROUP.registerEvent("file.history.computing",
+  private val GROUP = EventLogGroup("vcs.log.performance", 2)
+  val FILE_HISTORY_COMPUTING = GROUP.registerEvent("file.history.computed",
                                                    EventFields.String("vcs", listOf("Git", "hg4idea", "Perforce")),
-                                                   EventFields.Boolean("withIndex"),
+                                                   EventFields.Boolean("with_index"),
                                                    EventFields.DurationMs)
-  val FILE_HISTORY_COLLECTING_RENAMES = GROUP.registerEvent("file.history.collecting.renames", EventFields.DurationMs)
+  val FILE_HISTORY_COLLECTING_RENAMES = GROUP.registerEvent("file.history.collected.renames", EventFields.DurationMs)
 
   override fun getGroup() = GROUP
 }
