bbc98da03d8cf7c0a937828b9a075e506dc70058
Ivan Posti <ivan.posti@jetbrains.com>
2023-08-02 12:11:21 +0200

    Replace coroutine scope stub with service constructor injection
    GitOrigin-RevId: 62d3e772f53001148470786afec5b880d291826e

--- plugins/mermaid/plugin/src/main/kotlin/com/intellij/mermaid/MermaidPlugin.kt
+++ plugins/mermaid/plugin/src/main/kotlin/com/intellij/mermaid/MermaidPlugin.kt
@@ -4,13 +4,9 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.Service
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.cancel
-import kotlin.coroutines.EmptyCoroutineContext
 
-// TODO: Replace with service constructor injection after 231
 @Service
-internal class MermaidPlugin: Disposable {
-  val coroutineScope: CoroutineScope = CoroutineScope(EmptyCoroutineContext)
-
+internal class MermaidPlugin(val coroutineScope: CoroutineScope): Disposable {
   override fun dispose() {
     coroutineScope.cancel()
   }
