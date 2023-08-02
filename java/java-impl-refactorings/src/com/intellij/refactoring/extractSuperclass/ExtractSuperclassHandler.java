c18e77260aa7d7a0d44abbf48dc4e7242370e6f0
Roman Ivanov <roman.ivanov@jetbrains.com>
2023-08-02 16:54:15 +0200

    [java, refactoring] disable extract superclass for unnamed class
    GitOrigin-RevId: 376d8944f5b8fa29a97e17b51d02f46c452c2483

--- java/java-impl-refactorings/src/com/intellij/refactoring/extractSuperclass/ExtractSuperclassHandler.java
+++ java/java-impl-refactorings/src/com/intellij/refactoring/extractSuperclass/ExtractSuperclassHandler.java
@@ -70,6 +70,12 @@ public class ExtractSuperclassHandler implements ElementsHandler, ExtractSupercl
       return;
     }
 
+    if (mySubclass instanceof PsiUnnamedClass) {
+      String message = RefactoringBundle.message("error.superclass.cannot.be.extracted.from.unnamed.class");
+      CommonRefactoringUtil.showErrorHint(project, null, message, getRefactoringName(), HelpID.EXTRACT_SUPERCLASS);
+      return;
+    }
+
     if (!CommonRefactoringUtil.checkReadOnlyStatus(project, mySubclass)) return;
 
     Editor editor = dataContext != null ? CommonDataKeys.EDITOR.getData(dataContext) : null;
