347a2fe4dfb618ff339fb3fa63c049ac9a864463
Leonid Shalupov <leonid@shalupov.com>
2023-08-02 22:51:43 +0200

    devkit: convert tests from groovy to java
    GitOrigin-RevId: 4ccc258805caefdb8ffd483f139b53de9bd41a99

--- plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/codeInsight/PluginXmlAutoPopupTest.java
+++ plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/codeInsight/PluginXmlAutoPopupTest.java
@@ -13,37 +13,37 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jetbrains.idea.devkit.codeInsight
+package org.jetbrains.idea.devkit.codeInsight;
 
-import com.intellij.codeInsight.completion.JavaCompletionAutoPopupTestCase
-import groovy.transform.CompileStatic
+import com.intellij.codeInsight.completion.JavaCompletionAutoPopupTestCase;
 
-@CompileStatic
-class PluginXmlAutoPopupTest extends JavaCompletionAutoPopupTestCase {
+import java.util.List;
 
-  void "test autopopup for class references"() {
-    myFixture.addClass("public class FooFooFooFooFoo { }")
-    myFixture.configureByText 'plugin.xml', '''
+public class PluginXmlAutoPopupTest extends JavaCompletionAutoPopupTestCase {
+
+  public void test_autopopup_for_class_references() {
+    myFixture.addClass("public class FooFooFooFooFoo { }");
+    myFixture.configureByText("plugin.xml", """
 <idea-plugin>
   <extensionPoints>
     <extensionPoint name="FooFooFooFooFox" interface="FFFF<caret>"/>
   </extensionPoints>
 </idea-plugin>
-'''
-    myTester.typeWithPauses('o')
-    assertNotNull(myFixture.getLookup())
-    assertSameElements(myFixture.lookupElementStrings, ['FooFooFooFooFoo'])
+""");
+    myTester.typeWithPauses("o");
+    assertNotNull(myFixture.getLookup());
+    assertSameElements(myFixture.getLookupElementStrings(), List.of("FooFooFooFooFoo"));
   }
 
-  void "test no autopopup when only word completion is available"() {
-    myFixture.configureByText 'plugin.xml', '''
+  public void test_no_autopopup_when_only_word_completion_is_available() {
+    myFixture.configureByText("plugin.xml", """
 <idea-plugin>
   <extensionPoints>
     <extensionPoint name="<caret>"/>
   </extensionPoints>
 </idea-plugin>
-'''
-    myTester.typeWithPauses('e')
-    assertNull(myFixture.getLookup())
+""");
+    myTester.typeWithPauses("e");
+    assertNull(myFixture.getLookup());
   }
 }
