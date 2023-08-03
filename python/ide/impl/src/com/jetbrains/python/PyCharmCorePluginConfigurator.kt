db982fd04d75049f818ca2b90f26f8f941817d8f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-08-03 16:07:51 +0300

    PyCharmCorePluginConfigurator - use modern non-blocking API
    GitOrigin-RevId: 5b7027c57967e1bc58900d8a392eb06599260676

--- python/ide/impl/src/com/jetbrains/python/PyCharmCorePluginConfigurator.kt
+++ python/ide/impl/src/com/jetbrains/python/PyCharmCorePluginConfigurator.kt
@@ -3,16 +3,24 @@ package com.jetbrains.python
 
 import com.intellij.application.options.CodeStyle
 import com.intellij.codeInsight.CodeInsightSettings
+import com.intellij.diagnostic.LoadingState
 import com.intellij.ide.ApplicationInitializedListener
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.Experiments
+import com.intellij.openapi.application.writeAction
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.editor.ex.EditorSettingsExternalizable
+import com.intellij.openapi.extensions.ExtensionNotApplicableException
 import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.util.PlatformUtils
 import com.jetbrains.python.codeInsight.PyCodeInsightSettings
 import com.jetbrains.python.console.PyConsoleOptions
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.milliseconds
 
 /**
  * Initialize PyCharm.
@@ -20,48 +28,64 @@ import com.jetbrains.python.console.PyConsoleOptions
  * This class is called **only in PyCharm**.
  * It does not work in plugin
  */
-internal class PyCharmCorePluginConfigurator : ApplicationInitializedListener {
-  override fun componentsInitialized() {
+private class PyCharmCorePluginConfigurator : ApplicationInitializedListener {
+  init {
     if (ApplicationManager.getApplication().isHeadlessEnvironment()) {
-      return
+      throw ExtensionNotApplicableException.create()
     }
-    val propertiesComponent = PropertiesComponent.getInstance()
-    if (!propertiesComponent.getBoolean("PyCharm.InitialConfiguration")) {
-      propertiesComponent.setValue("PyCharm.InitialConfiguration", "true")
+  }
+
+  override suspend fun execute(asyncScope: CoroutineScope) {
+    val propertyManager = serviceAsync<PropertiesComponent>()
+    if (!propertyManager.getBoolean("PyCharm.InitialConfiguration")) {
+      propertyManager.setValue("PyCharm.InitialConfiguration", "true")
       EditorSettingsExternalizable.getInstance().setVirtualSpace(false)
     }
-    if (!propertiesComponent.getBoolean("PyCharm.InitialConfiguration.V2")) {
-      propertiesComponent.setValue("PyCharm.InitialConfiguration.V2", true)
-      val settings = CodeStyle.getDefaultSettings()
-      settings.getCommonSettings(PythonLanguage.getInstance()).ALIGN_MULTILINE_PARAMETERS_IN_CALLS = true
+
+    if (!propertyManager.getBoolean("PyCharm.InitialConfiguration.V2")) {
+      propertyManager.setValue("PyCharm.InitialConfiguration.V2", true)
+      CodeStyle.getDefaultSettings().getCommonSettings(PythonLanguage.getInstance()).ALIGN_MULTILINE_PARAMETERS_IN_CALLS = true
     }
-    if (!propertiesComponent.getBoolean("PyCharm.InitialConfiguration.V3")) {
-      propertiesComponent.setValue("PyCharm.InitialConfiguration.V3", "true")
-      val ignoredFilesList = FileTypeManager.getInstance().getIgnoredFilesList()
-      ApplicationManager.getApplication().invokeLater(Runnable {
-        ApplicationManager.getApplication().runWriteAction(
-          Runnable { FileTypeManager.getInstance().setIgnoredFilesList("$ignoredFilesList;*\$py.class") })
-      })
+
+    if (!propertyManager.getBoolean("PyCharm.InitialConfiguration.V3")) {
+      propertyManager.setValue("PyCharm.InitialConfiguration.V3", "true")
+      asyncScope.launch {
+        while (!LoadingState.COMPONENTS_LOADED.isOccurred) {
+          delay(10.milliseconds)
+        }
+
+        val fileTypeManager = FileTypeManager.getInstance()
+        val ignoredFilesList = fileTypeManager.getIgnoredFilesList()
+        writeAction {
+          fileTypeManager.setIgnoredFilesList("$ignoredFilesList;*\$py.class")
+        }
+      }
     }
-    if (!propertiesComponent.getBoolean("PyCharm.InitialConfiguration.V4")) {
-      propertiesComponent.setValue("PyCharm.InitialConfiguration.V4", true)
+
+    if (!propertyManager.getBoolean("PyCharm.InitialConfiguration.V4")) {
+      propertyManager.setValue("PyCharm.InitialConfiguration.V4", true)
       PyCodeInsightSettings.getInstance().SHOW_IMPORT_POPUP = false
     }
-    if (!propertiesComponent.getBoolean("PyCharm.InitialConfiguration.V5")) {
-      propertiesComponent.setValue("PyCharm.InitialConfiguration.V5", true)
+
+    if (!propertyManager.getBoolean("PyCharm.InitialConfiguration.V5")) {
+      propertyManager.setValue("PyCharm.InitialConfiguration.V5", true)
       CodeInsightSettings.getInstance().REFORMAT_ON_PASTE = CodeInsightSettings.NO_REFORMAT
     }
-    if (!propertiesComponent.getBoolean("PyCharm.InitialConfiguration.V6")) {
-      propertiesComponent.setValue("PyCharm.InitialConfiguration.V6", true)
+
+    if (!propertyManager.getBoolean("PyCharm.InitialConfiguration.V6")) {
+      propertyManager.setValue("PyCharm.InitialConfiguration.V6", true)
       CodeInsightSettings.getInstance().INDENT_TO_CARET_ON_PASTE = true
     }
-    if (!propertiesComponent.getBoolean("PyCharm.InitialConfiguration.V7")) {
-      propertiesComponent.setValue("PyCharm.InitialConfiguration.V7", true)
+
+    if (!propertyManager.getBoolean("PyCharm.InitialConfiguration.V7")) {
+      propertyManager.setValue("PyCharm.InitialConfiguration.V7", true)
     }
-    if (!propertiesComponent.getBoolean("PyCharm.InitialConfiguration.V8")) {
-      propertiesComponent.setValue("PyCharm.InitialConfiguration.V8", true)
+
+    if (!propertyManager.getBoolean("PyCharm.InitialConfiguration.V8")) {
+      propertyManager.setValue("PyCharm.InitialConfiguration.V8", true)
       PyConsoleOptions.getInstance(ProjectManager.getInstance().getDefaultProject()).setCommandQueueEnabled(PlatformUtils.isDataSpell())
     }
-    Experiments.getInstance().setFeatureEnabled("terminal.shell.command.handling", false)
+
+    serviceAsync<Experiments>().setFeatureEnabled("terminal.shell.command.handling", false)
   }
 }
