2d212bc0becddf4aed564c62b450df7f1fea81c7
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2023-08-04 16:09:57 +0200

    [kotlin] Log projectId for LanguageFeatureDeprecationCollector
    ^KT-60956 fixed
    
    GitOrigin-RevId: 0b000b1760c44d6a5d53ca3a60e80a50bc163808

--- plugins/kotlin/base/statistics/src/org/jetbrains/kotlin/idea/statistics/KotlinLanguageFeaturesFUSCollector.kt
+++ plugins/kotlin/base/statistics/src/org/jetbrains/kotlin/idea/statistics/KotlinLanguageFeaturesFUSCollector.kt
@@ -154,6 +154,7 @@ class LanguageFeatureDeprecationCollector<T : InspectionData>(
         if (!service.recordInspection(file, inspectionType, data)) return
 
         inspectionUpdatedEvent.log(
+            file.project,
             EventFields.AnonymizedPath.with(file.virtualFilePath),
             kotlinLanguageVersionField.with(languageVersion.versionString),
             KotlinLanguageFeaturesFUSCollector.inspectionTypeField.with(inspectionType),
@@ -164,6 +165,7 @@ class LanguageFeatureDeprecationCollector<T : InspectionData>(
     fun logQuickFixApplied(file: PsiFile?) {
         if (file !is KtFile || file.virtualFile == null) return
         inspectionAppliedEvent.log(
+            file.project,
             EventFields.AnonymizedPath.with(file.virtualFilePath),
             KotlinLanguageFeaturesFUSCollector.inspectionTypeField.with(inspectionType)
         )
