d76a92a87ae5a9f22e79e5bc1c4ed8ad182aaf6a
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2023-08-05 15:07:18 +0200

    IDEA-324496 Add Links to New UI pages in Settings
    - phpstorm, ruby, pycharm
    
    (cherry picked from commit 2a67b75d8512779e6cd9a4b9333b08d0e364d462)
    (cherry picked from commit f8add50f664e2e30d4cd7f85059b47ba8604e9b4)
    
    IJ-CR-113197
    
    GitOrigin-RevId: 79c2730d8b1af95500bbf2bc609d2ec429bbdb12

---
+++ python/src/com/jetbrains/python/configuration/PyExperimentalUIConfigurable.kt
@@ -0,0 +1,11 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.jetbrains.python.configuration
+
+import com.intellij.ui.ExperimentalUIConfigurable
+
+class PyExperimentalUIConfigurable : ExperimentalUIConfigurable() {
+
+  override fun getExploreNewUiUrl(): String {
+    return EXPLORE_NEW_UI_URL_TEMPLATE.format("pycharm")
+  }
+}
