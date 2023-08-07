d19d98a89e89036ee79c53eef50f9f84c38a6824
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-08-07 09:54:17 +0300

    IDEA-327665 Refactor StatusBarWidgetProviderToFactoryAdapter to Kotlin
    GitOrigin-RevId: c7a9883952bdf0e410305348826c822653bd61fc

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/status/widget/StatusBarWidgetProviderToFactoryAdapter.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/status/widget/StatusBarWidgetProviderToFactoryAdapter.kt
@@ -1,99 +1,67 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.wm.impl.status.widget;
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.wm.impl.status.widget
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.wm.*;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.wm.IdeFrame
+import com.intellij.openapi.wm.StatusBarWidget
+import com.intellij.openapi.wm.StatusBarWidgetFactory
+import java.util.*
 
-import java.util.Objects;
+internal class StatusBarWidgetProviderToFactoryAdapter(
+  private val project: Project,
+  private val frame: IdeFrame,
+  @Suppress("DEPRECATION", "removal") @JvmField val provider: com.intellij.openapi.wm.StatusBarWidgetProvider,
+) : StatusBarWidgetFactory {
+  private var widgetWasCreated = false
+  private var myWidget: StatusBarWidget? = null
 
-@ApiStatus.Internal
-public final class StatusBarWidgetProviderToFactoryAdapter implements StatusBarWidgetFactory {
-  private final Project myProject;
-  private final IdeFrame myFrame;
-  @SuppressWarnings("removal") final StatusBarWidgetProvider provider;
-
-  private boolean widgetWasCreated;
-  private @Nullable StatusBarWidget myWidget;
-
-  public StatusBarWidgetProviderToFactoryAdapter(@NotNull Project project,
-                                                 @NotNull IdeFrame frame,
-                                                 @SuppressWarnings("removal") @NotNull StatusBarWidgetProvider provider) {
-    myProject = project;
-    myFrame = frame;
-    this.provider = provider;
+  override fun getId(): String {
+    return widget?.ID() ?: provider.javaClass.getName()
   }
 
-  @Override
-  public @NotNull String getId() {
-    StatusBarWidget widget = getWidget();
-    return widget != null ? widget.ID() : provider.getClass().getName();
-  }
-
-  @Override
-  public @NotNull String getDisplayName() {
-    StatusBarWidget widget = getWidget();
-    if (widget != null) {
-      StatusBarWidget.WidgetPresentation presentation = widget.getPresentation();
-      String result = presentation != null ? StringUtil.notNullize(presentation.getTooltipText()) : "";
-      if (!result.isEmpty()) {
-        return result;
-      }
-      if (ApplicationManager.getApplication().isInternal()) {
-        //noinspection HardCodedStringLiteral
-        return widget.ID();
-      }
+  @Suppress("HardCodedStringLiteral", "DialogTitleCapitalization")
+  override fun getDisplayName(): String {
+    val widget = widget ?: return ""
+    val result = widget.getPresentation()?.getTooltipText()
+    if (!result.isNullOrEmpty()) {
+      return result
     }
-    return "";
+    return if (ApplicationManager.getApplication().isInternal()) widget.ID() else ""
   }
 
-  @Override
-  public boolean isAvailable(@NotNull Project project) {
-    //noinspection removal
-    return provider.isCompatibleWith(myFrame) && getWidget() != null;
+  override fun isAvailable(project: Project): Boolean {
+    @Suppress("removal")
+    return provider.isCompatibleWith(frame) && widget != null
   }
 
-  @Override
-  public boolean isConfigurable() {
-    return !getDisplayName().isEmpty();
-  }
+  override fun isConfigurable(): Boolean = !getDisplayName().isEmpty()
 
-  @Override
-  public @NotNull StatusBarWidget createWidget(@NotNull Project project) {
-    return Objects.requireNonNull(getWidget());
-  }
+  override fun createWidget(project: Project) = widget!!
 
-  private @Nullable StatusBarWidget getWidget() {
-    if (!widgetWasCreated) {
-      //noinspection removal
-      myWidget = provider.getWidget(myProject);
-      widgetWasCreated = true;
+  @Suppress("removal")
+  private val widget: StatusBarWidget?
+    get() {
+      if (!widgetWasCreated) {
+        myWidget = provider.getWidget(project)
+        widgetWasCreated = true
+      }
+      return myWidget
     }
-    return myWidget;
+
+  override fun disposeWidget(widget: StatusBarWidget) {
+    myWidget = null
+    widgetWasCreated = false
+    Disposer.dispose(widget)
   }
 
-  @Override
-  public void disposeWidget(@NotNull StatusBarWidget widget) {
-    myWidget = null;
-    widgetWasCreated = false;
-    Disposer.dispose(widget);
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other == null || javaClass != other.javaClass) return false
+    val adapter = other as StatusBarWidgetProviderToFactoryAdapter
+    return provider == adapter.provider && project == adapter.project
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    StatusBarWidgetProviderToFactoryAdapter adapter = (StatusBarWidgetProviderToFactoryAdapter)o;
-    return provider.equals(adapter.provider) && myProject.equals(adapter.myProject);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(provider, myProject);
-  }
+  override fun hashCode(): Int = Objects.hash(provider, project)
 }
