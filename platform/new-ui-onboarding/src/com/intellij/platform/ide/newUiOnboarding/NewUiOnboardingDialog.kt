f2321aab641fef0e3242a0d851496a2bc5b8fb55
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2023-08-07 13:16:41 +0300

    IDEA-327694 Allow moving the New UI Onboarding proposal dialog
    (cherry picked from commit 15988ec60e2dd99d68a70efa9129c4bfd3b6d497)
    (cherry picked from commit aff182fce8e1c2d52a5157cd7b06853bce7fa963)
    
    IJ-CR-112857
    
    GitOrigin-RevId: 83a4ebdd9436406aa8c62ea568699272c186baf0

--- platform/new-ui-onboarding/src/com/intellij/platform/ide/newUiOnboarding/NewUiOnboardingDialog.kt
+++ platform/new-ui-onboarding/src/com/intellij/platform/ide/newUiOnboarding/NewUiOnboardingDialog.kt
@@ -7,10 +7,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.util.SystemInfoRt
-import com.intellij.ui.ClientProperty
-import com.intellij.ui.JBColor
-import com.intellij.ui.PopupBorder
-import com.intellij.ui.WindowRoundedCornersManager
+import com.intellij.ui.*
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.gridLayout.UnscaledGaps
 import com.intellij.ui.dsl.gridLayout.UnscaledGapsY
@@ -47,10 +44,10 @@ class NewUiOnboardingDialog(project: Project)
     val contentGaps = UnscaledGaps(28, 32, 22, 32)
 
     val panel = panel {
-      panel {
-        row {
-          icon(banner).customize(UnscaledGaps.EMPTY)
-        }
+      row {
+        icon(banner)
+          .customize(UnscaledGaps.EMPTY)
+          .applyToComponent { WindowMoveListener(this).installTo(this) }
       }
       panel {
         row {
