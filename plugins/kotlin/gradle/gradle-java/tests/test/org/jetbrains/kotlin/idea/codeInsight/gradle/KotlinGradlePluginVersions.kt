b05fbddb662c38c4d1e8851120d4ae6d04972dee
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-08-07 19:00:21 +0200

    [KTIJ-26562] Update latest Kotlin Gradle Plugin version to 1.9.20-dev-9107
    ^KTIJ-26562 Verification Pending
    
    GitOrigin-RevId: 0437cde9d18eba366b7fba7e5993b3f9630a8efa

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/KotlinGradlePluginVersions.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/KotlinGradlePluginVersions.kt
@@ -7,7 +7,7 @@ import org.jetbrains.kotlin.tooling.core.isStable
 object KotlinGradlePluginVersions {
     val V_1_7_21 = KotlinToolingVersion(1, 7, 21, null)
     val V_1_8_0 = KotlinToolingVersion(1, 8, 0, null)
-    val latest = KotlinToolingVersion("1.9.20-dev-6845")
+    val latest = KotlinToolingVersion("1.9.20-dev-9107")
 
     val all = listOf(
         V_1_7_21,
