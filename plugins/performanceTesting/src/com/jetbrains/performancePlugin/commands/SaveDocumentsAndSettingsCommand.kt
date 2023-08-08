ac21b1cda993ad20caedad8393d7554c4f89044c
Alexey Kryuchkov <alexey.kryuchkov@jetbrains.com>
2023-08-08 17:26:24 +0300

    Move SaveDocumentsAndSettingsCommand to common command module due to this functionality needed in Vcs tests too
    GitOrigin-RevId: 5d59914307ad5b49210aa1f79a8c4cc220dbfad5

---
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/SaveDocumentsAndSettingsCommand.kt
@@ -0,0 +1,33 @@
+package com.jetbrains.performancePlugin.commands
+
+import com.intellij.configurationStore.saveProjectsAndApp
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.playback.PlaybackContext
+import com.intellij.openapi.ui.playback.commands.PlaybackCommandCoroutineAdapter
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * Saving project settings and unsaved documents
+ */
+class SaveDocumentsAndSettingsCommand(text: String, line: Int) : PlaybackCommandCoroutineAdapter(text, line) {
+  companion object {
+    const val PREFIX = CMD_PREFIX + "saveDocumentsAndSettings"
+
+    suspend fun save(project: Project) {
+      withContext(CoroutineName("save docs and project settings")) {
+        withContext(Dispatchers.EDT) {
+          FileDocumentManager.getInstance().saveAllDocuments()
+        }
+        saveProjectsAndApp(forceSavingAllSettings = true, onlyProject = project)
+      }
+    }
+  }
+
+  override suspend fun doExecute(context: PlaybackContext) {
+    save(context.project)
+  }
+}
