667d166045a9e997eae6881d6fb2182e5f2d501f
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2023-08-08 16:32:29 +0200

    [core] IJPL-193: enable new FilePageCache (brand name 'lock-free')
    + `-Dvfs.lock-free-impl.enable=false` to disable
    
    GitOrigin-RevId: b9941013315a39d51a1f7f26639fdc7362849db6

--- platform/util/src/com/intellij/util/io/PageCacheUtils.java
+++ platform/util/src/com/intellij/util/io/PageCacheUtils.java
@@ -28,7 +28,7 @@ public final class PageCacheUtils {
    * So far they co-exist with the legacy implementations
    * TODO rename to LOCK_FREE_PAGE_CACHE_ENABLED ('cos it is not only about VFS)
    */
-  public static final boolean LOCK_FREE_VFS_ENABLED = getBooleanProperty("vfs.lock-free-impl.enable", false);
+  public static final boolean LOCK_FREE_VFS_ENABLED = getBooleanProperty("vfs.lock-free-impl.enable", true);
 
   /**
    * How much direct memory new (code name 'lock-free') FilePageCache impl allowed to utilize:
