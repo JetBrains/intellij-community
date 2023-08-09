b8b3a2fe19fa772dd461feb63e6d62418dc8523f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-08-09 09:05:41 +0300

    IDEA-327903 cleanup
    GitOrigin-RevId: 22c1a866d85458c508faadf1f87641c9fa602ae2

--- platform/platform-impl/src/com/intellij/xml/breadcrumbs/BreadcrumbsXmlWrapper.java
+++ platform/platform-impl/src/com/intellij/xml/breadcrumbs/BreadcrumbsXmlWrapper.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.xml.breadcrumbs;
 
 import com.intellij.codeInsight.breadcrumbs.FileBreadcrumbsCollector;
@@ -13,7 +13,6 @@ import com.intellij.ui.ExperimentalUI;
 import com.intellij.ui.components.breadcrumbs.Crumb;
 import com.intellij.ui.paint.LinePainter2D;
 import com.intellij.ui.paint.RectanglePainter2D;
-import com.intellij.util.ObjectUtils;
 import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -21,10 +20,10 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.border.Border;
 import java.awt.*;
 
-public class BreadcrumbsXmlWrapper extends BreadcrumbsPanel implements Border {
+public final class BreadcrumbsXmlWrapper extends BreadcrumbsPanel implements Border {
   private final VirtualFile myFile;
 
-  public BreadcrumbsXmlWrapper(@NotNull final Editor editor) {
+  public BreadcrumbsXmlWrapper(final @NotNull Editor editor) {
     super(editor);
     myFile = FileDocumentManager.getInstance().getFile(myEditor.getDocument());
     if (ExperimentalUI.isNewUI()) {
@@ -33,9 +32,8 @@ public class BreadcrumbsXmlWrapper extends BreadcrumbsPanel implements Border {
     setBorder(this);
   }
 
-  @Nullable
   @Override
-  protected Iterable<? extends Crumb> computeCrumbs(int offset) {
+  protected @Nullable Iterable<? extends Crumb> computeCrumbs(int offset) {
     FileBreadcrumbsCollector breadcrumbsCollector = findCollectorFor(myProject, myFile, this);
     if (breadcrumbsCollector == null) return null;
 
@@ -73,8 +71,8 @@ public class BreadcrumbsXmlWrapper extends BreadcrumbsPanel implements Border {
     return true;
   }
 
-  @Nullable
-  public static BreadcrumbsXmlWrapper getBreadcrumbsWrapper(@NotNull Editor editor) {
-    return ObjectUtils.tryCast(BreadcrumbsPanel.getBreadcrumbsComponent(editor), BreadcrumbsXmlWrapper.class);
+  public static @Nullable BreadcrumbsXmlWrapper getBreadcrumbWrapper(@NotNull Editor editor) {
+    Object obj = BreadcrumbsPanel.getBreadcrumbsComponent(editor);
+    return obj instanceof BreadcrumbsXmlWrapper ? (BreadcrumbsXmlWrapper)obj : null;
   }
 }
