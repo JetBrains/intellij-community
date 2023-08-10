1ff3f9273d1adc131a5612aa9322e3d34139ddb1
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-08-10 12:48:28 +0300

    IDEA-328015 convert PsiIconUtil to kotlin
    GitOrigin-RevId: aa729968d0cd84dc7ccd78fb846de5f005e0e81f

--- platform/core-api/src/com/intellij/util/PsiIconUtil.kt
+++ platform/core-api/src/com/intellij/util/PsiIconUtil.kt
@@ -1,28 +1,25 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.util;
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.util
 
-import com.intellij.ide.IconProvider;
-import com.intellij.openapi.project.DumbService;
-import com.intellij.openapi.util.Iconable;
-import com.intellij.psi.PsiElement;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.ide.IconProvider
+import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.project.DumbService.Companion.isDumbAware
+import com.intellij.openapi.util.Iconable.IconFlags
+import com.intellij.psi.PsiElement
+import javax.swing.Icon
 
-import javax.swing.*;
-
-public final class PsiIconUtil {
-  public static @Nullable Icon getProvidersIcon(@NotNull PsiElement element, @Iconable.IconFlags int flags) {
-    boolean isDumb = DumbService.getInstance(element.getProject()).isDumb();
-    for (IconProvider provider : IconProvider.EXTENSION_POINT_NAME.getIterable()) {
-      if (isDumb && !DumbService.isDumbAware(provider)) {
-        continue;
+object PsiIconUtil {
+  @JvmStatic
+  fun getProvidersIcon(element: PsiElement, @IconFlags flags: Int): Icon? {
+    val isDumb = DumbService.getInstance(element.getProject()).isDumb
+    for (provider in IconProvider.EXTENSION_POINT_NAME.getIterable()) {
+      if (isDumb && !isDumbAware(provider)) {
+        continue
       }
-
-      Icon icon = provider.getIcon(element, flags);
-      if (icon != null) {
-        return icon;
+      provider!!.getIcon(element, flags)?.let {
+        return it
       }
     }
-    return null;
+    return null
   }
 }
