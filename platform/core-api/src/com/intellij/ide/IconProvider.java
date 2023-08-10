4d7a36b724d59e8adbced371d1cfc6ca1fdd5647
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-08-10 11:49:54 +0300

    cleanup
    GitOrigin-RevId: 704a98c71ca8be53a5a26a5a9b6e5f30325f27db

--- platform/core-api/src/com/intellij/ide/IconProvider.java
+++ platform/core-api/src/com/intellij/ide/IconProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
@@ -23,6 +23,5 @@ public abstract class IconProvider {
    * @return {@code null} if this provider cannot provide icon for given element.
    * @see Iconable
    */
-  @Nullable
-  public abstract Icon getIcon(@NotNull PsiElement element, @Iconable.IconFlags int flags);
+  public abstract @Nullable Icon getIcon(@NotNull PsiElement element, @Iconable.IconFlags int flags);
 }
