47846261f7c9b41a88d89296a73059bca34f3e64
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-08-10 17:02:57 +0300

    mark StoreReloadManager as internal
    GitOrigin-RevId: 6e1a9eed3383eb7ea6ddb963d572b7a319d9ada7

--- platform/platform-impl/src/com/intellij/configurationStore/StoreReloadManager.kt
+++ platform/platform-impl/src/com/intellij/configurationStore/StoreReloadManager.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.configurationStore
 
 import com.intellij.openapi.components.StateStorage
@@ -7,7 +7,9 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.ApiStatus.Internal
 
+@Internal
 interface StoreReloadManager {
   companion object {
     fun getInstance(project: Project): StoreReloadManager = project.service<StoreReloadManager>()
