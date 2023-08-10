9a87105c7ab8280edc51a99c171948bbc4db1b89
Alexey Kryuchkov <alexey.kryuchkov@jetbrains.com>
2023-08-10 12:25:31 +0300

    AT-476 ShowFileHistory tests for monorepo project + refactoring tests into appropriate packages
    GitOrigin-RevId: 878144f928ab1072bcb9bcb33d8d5df8df4649d2

--- plugins/git4idea/src/git4idea/performanceTesting/ShowFileHistoryCommand.kt
+++ plugins/git4idea/src/git4idea/performanceTesting/ShowFileHistoryCommand.kt
@@ -25,13 +25,6 @@ class ShowFileHistoryCommand(text: String, line: Int) : AbstractCommand(text, li
   override fun _execute(context: PlaybackContext): Promise<Any?> {
     val actionCallback: ActionCallback = ActionCallbackProfilerStopper()
     val focusedComponent = IdeFocusManager.findInstance().focusOwner
-    if(focusedComponent==null) {
-      LOG.info("Focused element is null")
-    }
-    else {
-      LOG.info("Focused element ${focusedComponent.javaClass.name}")
-    }
-
     val dataContext = DataManager.getInstance().getDataContext(focusedComponent)
     val selectedFiles = VcsContextUtil.selectedFilePaths(dataContext)
     val historyProvider: VcsLogFileHistoryProvider = context.project.getService(VcsLogFileHistoryProvider::class.java)
@@ -54,13 +47,12 @@ class ShowFileHistoryCommand(text: String, line: Int) : AbstractCommand(text, li
           scope.close()
           actionCallback.setDone()
         }
-        else {
-          if (firstPackSpan.isRecording) {
+        else if (firstPackSpan.isRecording) {
             firstPackSpan.end()
           }
-        }
       }
     }
+
     return actionCallback.toPromise()
   }
 
@@ -68,7 +60,6 @@ class ShowFileHistoryCommand(text: String, line: Int) : AbstractCommand(text, li
     const val MAIN_SPAN_NAME = "showFileHistory"
     const val FIRST_PACK_SPAN_NAME = "showFirstPack"
     const val PREFIX = "${CMD_PREFIX}showFileHistory"
-    private val LOG = Logger.getInstance(ShowFileHistoryCommand::class.java)
   }
 
 }
