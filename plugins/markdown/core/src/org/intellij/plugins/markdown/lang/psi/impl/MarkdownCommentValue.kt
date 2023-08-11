b012191eb570b85f1894cbfb482761c6a3444fb2
Ivan Posti <ivan.posti@jetbrains.com>
2023-08-11 18:04:01 +0200

    [Markdown][IDEA-327860] Support issue pattern references in link reference comments
    GitOrigin-RevId: 30cd4a58548a3efafeec291b867e3b8ba00bf66c

--- plugins/markdown/core/src/org/intellij/plugins/markdown/lang/psi/impl/MarkdownCommentValue.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/lang/psi/impl/MarkdownCommentValue.kt
@@ -1,7 +1,8 @@
 package org.intellij.plugins.markdown.lang.psi.impl
 
+import com.intellij.model.psi.UrlReferenceHost
 import org.intellij.plugins.markdown.lang.MarkdownElementTypes
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Experimental
-class MarkdownCommentValue(text: CharSequence): MarkdownLeafPsiElement(MarkdownElementTypes.COMMENT_VALUE, text)
+class MarkdownCommentValue(text: CharSequence): MarkdownLeafPsiElement(MarkdownElementTypes.COMMENT_VALUE, text), UrlReferenceHost
