055f22f8ef832675652122bba75b04afce077e13
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-08-11 13:07:22 +0200

    [KTIJ-26562] KotlinGradlePluginVersionTestsProperty: Add 'snapshot' option
    ^KTIJ-26562 Verification Pending
    
    (cherry picked from commit 8df689a308988ef286a87180f861fb91a69fc993)
    
    GitOrigin-RevId: 4d4d0e7177920c3a15b8ef2d30dab81be27ed7fb

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/multiplatformTests/testProperties/KotlinGradlePluginVersionTestsProperty.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/gradle/multiplatformTests/testProperties/KotlinGradlePluginVersionTestsProperty.kt
@@ -9,11 +9,13 @@ import org.jetbrains.kotlin.idea.codeInsight.gradle.KotlinGradlePluginVersions
 object KotlinGradlePluginVersionTestsProperty : KotlinTestsResolvableProperty {
     override val id: String = "KGP_VERSION"
 
+    @Suppress("unused") // passed using environment variables
     enum class Value(val acronym: String, val version: String) {
         MinSupported("MIN", "1.7.21"),
         PreviousMajorRelease("PREV_RELEASE", "1.8.22"),
         LatestStable("STABLE", "1.9.0"),
-        Latest("LATEST", KotlinGradlePluginVersions.latest.toString())
+        Latest("LATEST", KotlinGradlePluginVersions.latest.toString()),
+        SNAPSHOT("SNAPSHOT", KotlinGradlePluginVersions.latest.run { "$major.$minor.255-SNAPSHOT" })
     }
 
     override val valuesByAcronyms: Map<String, String> = Value.values().associate { it.acronym to it.version }
