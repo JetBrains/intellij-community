e4a0602516a1fc038dff0efb99a078027ab54867
Alexander Koshevoy <alexander.koshevoy@jetbrains.com>
2023-08-14 16:00:58 +0200

    Optimize imports in LocalTargetEnvironment
    GitOrigin-RevId: 8b039496d00d93ca5153d9e3a03d401a536e374b

--- platform/execution/src/com/intellij/execution/target/local/LocalTargetEnvironment.java
+++ platform/execution/src/com/intellij/execution/target/local/LocalTargetEnvironment.java
@@ -10,7 +10,6 @@ import com.intellij.execution.target.*;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.util.io.FileUtil;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
