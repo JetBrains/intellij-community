9eaea96a1eaf952fa333a77059eebcc580a8e21e
Roman Golyshev <roman.golyshev@jetbrains.com>
2023-08-16 18:10:17 +0200

    KTIJ-26685 [kotlin] Move `K2OptimizeImportsFacility` to kotlin.code-insight.k2 module
    GitOrigin-RevId: f961736ea250e13cc244cc5e1eb7a1e1cfb86dc4

--- plugins/kotlin/code-insight/kotlin.code-insight.k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/K2OptimizeImportsFacility.kt
+++ plugins/kotlin/code-insight/kotlin.code-insight.k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/K2OptimizeImportsFacility.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.base.fir.codeInsight
+package org.jetbrains.kotlin.idea.k2.codeinsight
 
 import com.intellij.psi.PsiDocumentManager
 import org.jetbrains.kotlin.analysis.api.analyze
