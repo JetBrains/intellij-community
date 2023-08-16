0dacbdb6c8660c9caa21026e5217c580f9ab81cc
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2023-08-16 16:21:32 +0200

    wait for context outside of read action and wrap it with async context to be accessible on bgt later IDEA-326733
    (cherry picked from commit 24101aea472707e2f1a630e137834b3b62b781ab)
    
    IJ-CR-113417
    
    GitOrigin-RevId: d07d9fcfd911c03781ed2c6aae73ecac7099bd6b

--- platform/execution-impl/src/com/intellij/execution/impl/RunnerAndConfigurationSettingsImpl.kt
+++ platform/execution-impl/src/com/intellij/execution/impl/RunnerAndConfigurationSettingsImpl.kt
@@ -15,6 +15,7 @@ import com.intellij.execution.configurations.*
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.runners.ProgramRunner
 import com.intellij.ide.DataManager
+import com.intellij.ide.ui.IdeUiService
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ReadAction
@@ -343,16 +344,16 @@ class RunnerAndConfigurationSettingsImpl @JvmOverloads constructor(
     val configuration = configuration
     var warning: RuntimeConfigurationException? = null
 
-    ReadAction.nonBlocking {
-      try {
-        val dataContext = if (ApplicationManager.getApplication().isHeadlessEnvironment) {
-          val dataContextFromFocusAsync = DataManager.getInstance().dataContextFromFocusAsync
-          ProgressIndicatorUtils.awaitWithCheckCanceled(dataContextFromFocusAsync.asCompletableFuture())
-        }
-        else {
-          DataContext.EMPTY_CONTEXT
-        }
+    var dataContext = DataContext.EMPTY_CONTEXT
+    if (!ApplicationManager.getApplication().isHeadlessEnvironment) {
+      val dataContextFromFocusAsync = DataManager.getInstance().dataContextFromFocusAsync.onSuccess {
+        dataContext = IdeUiService.getInstance().createAsyncDataContext(it)
+      }
+      ProgressIndicatorUtils.awaitWithCheckCanceled(dataContextFromFocusAsync.asCompletableFuture())
+    }
 
+     ReadAction.nonBlocking {
+      try {
         ExecutionManagerImpl.withEnvironmentDataContext(dataContext).use {
           configuration.checkConfiguration()
         }
