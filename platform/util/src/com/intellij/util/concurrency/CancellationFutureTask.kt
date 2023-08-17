ad9fb0700ed12ce05228d402d7a86f0039d6d3fa
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-08-17 16:02:01 +0200

    convert `CancellationFutureTask` to Kotlin
    GitOrigin-RevId: cafa49e06618db3bd79efb41a60be2923c36f804

--- platform/util/src/com/intellij/util/concurrency/CancellationFutureTask.kt
+++ platform/util/src/com/intellij/util/concurrency/CancellationFutureTask.kt
@@ -1,28 +1,21 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.util.concurrency;
+package com.intellij.util.concurrency
 
-import kotlinx.coroutines.Job;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.concurrent.Callable;
-import java.util.concurrent.FutureTask;
+import kotlinx.coroutines.Job
+import java.util.concurrent.Callable
+import java.util.concurrent.FutureTask
 
 /**
  * A FutureTask, which cancels the given job when it's cancelled.
  */
-final class CancellationFutureTask<V> extends FutureTask<V> {
+internal class CancellationFutureTask<V>(
+  private val job: Job,
+  callable: Callable<V>,
+) : FutureTask<V>(callable) {
 
-  private final @NotNull Job myJob;
-
-  CancellationFutureTask(@NotNull Job job, @NotNull Callable<V> callable) {
-    super(callable);
-    myJob = job;
-  }
-
-  @Override
-  public boolean cancel(boolean mayInterruptIfRunning) {
-    boolean result = super.cancel(mayInterruptIfRunning);
-    myJob.cancel(null);
-    return result;
+  override fun cancel(mayInterruptIfRunning: Boolean): Boolean {
+    val result = super.cancel(mayInterruptIfRunning)
+    job.cancel(null)
+    return result
   }
 }
