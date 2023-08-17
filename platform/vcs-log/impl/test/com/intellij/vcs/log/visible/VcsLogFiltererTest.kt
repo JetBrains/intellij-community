e2c8f6b85282dbf0dff9ce8b2465207daeb6fbc5
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2023-08-17 18:17:53 +0200

    [vcs-log] rename VisiblePackBuilderTest test class to VcsLogFiltererTest
    GitOrigin-RevId: 973e2cc157eda1c365b5495d894379ec44e02965

--- platform/vcs-log/impl/test/com/intellij/vcs/log/visible/VcsLogFiltererTest.kt
+++ platform/vcs-log/impl/test/com/intellij/vcs/log/visible/VcsLogFiltererTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.log.visible
 
 import com.intellij.mock.MockVirtualFile
@@ -25,7 +25,7 @@ import kotlin.random.nextInt
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
-class VisiblePackBuilderTest {
+class VcsLogFiltererTest {
   @Rule
   @JvmField
   val tempDir = TemporaryDirectory()
