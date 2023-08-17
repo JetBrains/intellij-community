6c6c367371169958635b758d5fe543b679790266
aleksandrina-streltsova <alexandrina.streltsova@jetbrains.com>
2023-08-17 14:54:22 +0300

    [kotlin] k2: update quick fix test data
    KTIJ-389
    
    GitOrigin-RevId: c4e0d25da0c48ed14471547222ee9e014b64ec13

--- plugins/kotlin/idea/tests/testData/quickfix/autoImports/importKType.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/autoImports/importKType.kt
@@ -4,3 +4,4 @@
 fun foo(x: <caret>KType) {}
 
 // FUS_QUICKFIX_NAME: org.jetbrains.kotlin.idea.quickfix.ImportFix
+// FUS_K2_QUICKFIX_NAME: org.jetbrains.kotlin.idea.quickfix.fixes.ImportQuickFix
--- plugins/kotlin/idea/tests/testData/quickfix/autoImports/notExcludedFromImportWhenInternalUse.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/autoImports/notExcludedFromImportWhenInternalUse.kt
@@ -19,3 +19,4 @@ fun some() {
     FunctionReference<caret>::class
 }
 // FUS_QUICKFIX_NAME: org.jetbrains.kotlin.idea.quickfix.ImportFix
+// FUS_K2_QUICKFIX_NAME: org.jetbrains.kotlin.idea.quickfix.fixes.ImportQuickFix
