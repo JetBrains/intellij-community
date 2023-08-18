6de300e693102dfb0a16f4b678a42a87c93a6f1f
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2023-08-18 11:52:40 +0200

    [platform] refactoring: move LegacyExternalProductResourceUrls to proper package
    ...to be consistent with the package naming convention.
    
    GitOrigin-RevId: cc1fcd6228788c010932a17004e833e61e786113

--- platform/platform-impl/src/com/intellij/platform/ide/impl/customization/LegacyExternalProductResourceUrls.kt
+++ platform/platform-impl/src/com/intellij/platform/ide/impl/customization/LegacyExternalProductResourceUrls.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.ide.customization.impl
+package com.intellij.platform.ide.impl.customization
 
 import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.application.ex.ApplicationInfoEx
