55cbe26e68da655b515bfe0927221a35fc608372
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2023-08-18 11:06:11 +0200

    [kotlin] Navigate to test
    #KTIJ-23440
    
    GitOrigin-RevId: 387bb6898ed28918d999b715294467b37683241e

--- plugins/kotlin/code-insight/impl-base/src/org/jetbrains/kotlin/idea/codeinsights/impl/base/testIntegration/KotlinCreateTestDialog.kt
+++ plugins/kotlin/code-insight/impl-base/src/org/jetbrains/kotlin/idea/codeinsights/impl/base/testIntegration/KotlinCreateTestDialog.kt
@@ -1,6 +1,6 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
-package org.jetbrains.kotlin.idea.testIntegration
+package org.jetbrains.kotlin.idea.codeinsights.impl.base.testIntegration
 
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
@@ -18,5 +18,5 @@ class KotlinCreateTestDialog(
 ) : CreateTestDialog(project, title, targetClass, targetPackage, targetModule) {
     var explicitClassName: String? = null
 
-    override fun getClassName() = explicitClassName ?: super.getClassName()
+    override fun getClassName(): String = explicitClassName ?: super.getClassName()
 }
--- plugins/kotlin/code-insight/impl-base/src/org/jetbrains/kotlin/idea/codeinsights/impl/base/testIntegration/testIntegrationUtils.kt
+++ plugins/kotlin/code-insight/impl-base/src/org/jetbrains/kotlin/idea/codeinsights/impl/base/testIntegration/testIntegrationUtils.kt
@@ -1,6 +1,6 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package org.jetbrains.kotlin.idea.testIntegration
+package org.jetbrains.kotlin.idea.codeinsights.impl.base.testIntegration
 
 import com.intellij.codeInsight.TestFrameworks
 import com.intellij.testIntegration.TestFramework
