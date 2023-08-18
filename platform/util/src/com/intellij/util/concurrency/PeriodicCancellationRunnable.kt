48a3a130e70c2519dc6f26ce58acc8cbe33c7618
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-08-18 16:44:23 +0200

    convert `PeriodicCancellationRunnable` to Kotlin
    GitOrigin-RevId: c96b8af8198c860a8b0f638da2fc2d4e246b8b51

--- platform/util/src/com/intellij/util/concurrency/PeriodicCancellationRunnable.kt
+++ platform/util/src/com/intellij/util/concurrency/PeriodicCancellationRunnable.kt
@@ -1,37 +1,25 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.util.concurrency;
+package com.intellij.util.concurrency
 
-import kotlinx.coroutines.CompletableJob;
-import org.jetbrains.annotations.NotNull;
+import kotlinx.coroutines.CompletableJob
+import java.util.concurrent.CancellationException
 
-import java.util.concurrent.CancellationException;
+internal class PeriodicCancellationRunnable(
+  private val job: CompletableJob,
+  private val runnable: Runnable,
+) : Runnable {
 
-import static com.intellij.util.concurrency.Propagation.runAsCoroutine;
-
-final class PeriodicCancellationRunnable implements Runnable {
-
-  private final @NotNull CompletableJob myJob;
-  private final @NotNull Runnable myRunnable;
-
-  PeriodicCancellationRunnable(@NotNull CompletableJob job, @NotNull Runnable runnable) {
-    myJob = job;
-    myRunnable = runnable;
-  }
-
-  @Override
-  public void run() {
+  override fun run() {
     // don't complete the job, it can be either failed, or cancelled
     try {
-      runAsCoroutine(myJob, false, () -> {
-        myRunnable.run();
-        return null;
-      });
-    } catch (CancellationException e) {
+      runAsCoroutine(job, false, runnable::run)
+    }
+    catch (e: CancellationException) {
       // According to the specification of the FutureTask, the runnable should not throw in case of cancellation.
       // Instead, Java relies on interruptions
       // This does not go along with the coroutines framework rules, but we have to play Java rules here
-      if (!myJob.isCancelled()) {
-        throw e;
+      if (!job.isCancelled) {
+        throw e
       }
     }
   }
