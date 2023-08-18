2c7b4e57d534e43d30c13622f1d9c5f6cbd266b2
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-08-18 14:33:19 +0200

    [java-inspections] IDEA-325304 Replace if with switch produces not compilable result
    - fix tests
    
    GitOrigin-RevId: 7518126437e42aa31fb3a858975c0c35a79a2818

--- plugins/InspectionGadgets/test/com/siyeh/igfixes/migration/if_can_be_switch/NullCast.after.java
+++ plugins/InspectionGadgets/test/com/siyeh/igfixes/migration/if_can_be_switch/NullCast.after.java
@@ -2,9 +2,13 @@ import org.jetbrains.annotations.Nullable;
 
 class Test {
   int foo(@Nullable Integer x) {
-      <caret>return switch (x) {
-          case null -> 123;
-          default -> throw new IllegalStateException("Unexpected value: " + x);
-      };
+      <caret>switch (x) {
+          case null -> {
+              return 123;
+          }
+          default -> {
+          }
+      }
+    throw new IllegalStateException("Unexpected value: " + x);
   }
 }
