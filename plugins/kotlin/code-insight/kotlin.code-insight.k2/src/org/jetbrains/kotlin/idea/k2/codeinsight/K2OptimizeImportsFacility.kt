3e2f51ebc3f974ee79853da9e071c9868e413059
Roman Golyshev <roman.golyshev@jetbrains.com>
2023-08-18 17:41:54 +0200

    KTIJ-26423 [kotlin] Copy logic for computing unused imports for K2 plugin from kotlin repository
    This logic belongs to the plugin, given the fact that the plugin
    knows more about how exactly the imports should be arranged
    
    GitOrigin-RevId: 853392202618e761f92a7d138daf2c8ee8385748

--- plugins/kotlin/code-insight/kotlin.code-insight.k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/K2OptimizeImportsFacility.kt
+++ plugins/kotlin/code-insight/kotlin.code-insight.k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/K2OptimizeImportsFacility.kt
@@ -3,10 +3,13 @@ package org.jetbrains.kotlin.idea.k2.codeinsight
 
 import org.jetbrains.kotlin.analysis.api.KtAllowAnalysisFromWriteAction
 import org.jetbrains.kotlin.analysis.api.KtAllowAnalysisOnEdt
+import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
 import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.components.KtImportOptimizerResult
 import org.jetbrains.kotlin.analysis.api.lifetime.allowAnalysisFromWriteAction
 import org.jetbrains.kotlin.analysis.api.lifetime.allowAnalysisOnEdt
 import org.jetbrains.kotlin.idea.base.codeInsight.KotlinOptimizeImportsFacility
+import org.jetbrains.kotlin.name.parentOrNull
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtImportDirective
 import org.jetbrains.kotlin.resolve.ImportPath
@@ -17,18 +20,68 @@ internal class K2OptimizeImportsFacility : KotlinOptimizeImportsFacility {
     override fun analyzeImports(file: KtFile): KotlinOptimizeImportsFacility.ImportData? {
         // Import optimizer might be called from reformat action in EDT, see KTIJ-25031
         @OptIn(KtAllowAnalysisOnEdt::class)
-        val unusedImports = allowAnalysisOnEdt {
+        val importAnalysis = allowAnalysisOnEdt {
             // Import optimizer might invoke be from write action in refactorings
             @OptIn(KtAllowAnalysisFromWriteAction::class)
             allowAnalysisFromWriteAction {
                 analyze(file) {
-                    analyseImports(file).unusedImports
+                    analyseImports(file)
                 }
             }
         }
 
-
+        val unusedImports = computeUnusedImports(file, importAnalysis)
         return K2ImportData(unusedImports.toList())
+    }
+
+    private fun computeUnusedImports(file: KtFile, result: KtImportOptimizerResult): Set<KtImportDirective> {
+        val existingImports = file.importDirectives
+        if (existingImports.isEmpty()) return emptySet()
+
+        val explicitlyImportedFqNames = existingImports
+            .asSequence()
+            .mapNotNull { it.importPath }
+            .filter { !it.isAllUnder && !it.hasAlias() }
+            .map { it.fqName }
+            .toSet()
+
+        val (usedImports, unresolvedNames) = result.usedDeclarations to result.unresolvedNames
+
+        val referencesEntities = usedImports
+            .filterNot { (fqName, referencedByNames) ->
+                val fromCurrentPackage = fqName.parentOrNull() == file.packageFqName
+                val noAliasedImports = referencedByNames.singleOrNull() == fqName.shortName()
+
+                fromCurrentPackage && noAliasedImports
+            }
+
+        val requiredStarImports = referencesEntities.keys
+            .asSequence()
+            .filterNot { it in explicitlyImportedFqNames }
+            .mapNotNull { it.parentOrNull() }
+            .filterNot { it.isRoot }
+            .toSet()
+
+        val unusedImports = mutableSetOf<KtImportDirective>()
+        val alreadySeenImports = mutableSetOf<ImportPath>()
+
+        for (import in existingImports) {
+            val importPath = import.importPath ?: continue
+
+            val isUsed = when {
+                importPath.importedName in unresolvedNames -> true
+                !alreadySeenImports.add(importPath) -> false
+                importPath.isAllUnder -> unresolvedNames.isNotEmpty() || importPath.fqName in requiredStarImports
+                importPath.fqName in referencesEntities -> importPath.importedName in referencesEntities.getValue(importPath.fqName)
+                else -> false
+            }
+
+            if (!isUsed) {
+                unusedImports += import
+            }
+        }
+
+        return unusedImports
     }
 
     override fun prepareOptimizedImports(
