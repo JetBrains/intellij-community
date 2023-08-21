e50d8990c9346d97e4964d826afb1c665d7c2104
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2023-08-21 23:51:33 +0200

    [git-index] allow to switch to staging area and back from the commit toolwindow toolbar
    GitOrigin-RevId: 5046b3a814bd561d258d094acebe8a8c7577974d

---
+++ plugins/git4idea/src/git4idea/index/actions/GitEnableStagingAreaAction.kt
@@ -0,0 +1,56 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package git4idea.index.actions
+
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.Presentation
+import com.intellij.openapi.actionSystem.RightAlignedToolbarAction
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.vcs.changes.ChangeListManager
+import git4idea.config.GitVcsApplicationSettings
+import git4idea.i18n.GitBundle
+import git4idea.index.canEnableStagingArea
+import git4idea.index.enableStagingArea
+
+class GitEnableStagingAreaAction : DumbAwareAction(), RightAlignedToolbarAction {
+
+  init {
+    updatePresentation(templatePresentation, false)
+  }
+
+  override fun update(e: AnActionEvent) {
+    val project = e.project
+    if (project == null || !canEnableStagingArea()) {
+      e.presentation.isEnabledAndVisible = false
+      return
+    }
+
+    val isStagingAreaEnabled = GitVcsApplicationSettings.getInstance().isStagingAreaEnabled
+    if (!isStagingAreaEnabled && ChangeListManager.getInstance(project).changeListsNumber > 1) {
+      e.presentation.isEnabledAndVisible = false
+      return
+    }
+
+    updatePresentation(e.presentation, isStagingAreaEnabled)
+  }
+
+  override fun actionPerformed(e: AnActionEvent) {
+    enableStagingArea(!GitVcsApplicationSettings.getInstance().isStagingAreaEnabled)
+  }
+
+  private fun updatePresentation(presentation: Presentation, isStagingAreaEnabled: Boolean) {
+    if (isStagingAreaEnabled) {
+      presentation.text = GitBundle.message("action.Git.Stage.Enable.Enabled.text")
+      presentation.description = GitBundle.message("action.Git.Stage.Enable.Enabled.description")
+      presentation.icon = AllIcons.Vcs.Changelist
+    }
+    else {
+      presentation.text = GitBundle.message("action.Git.Stage.Enable.Disabled.text")
+      presentation.description = GitBundle.message("action.Git.Stage.Enable.Disabled.description")
+      presentation.icon = AllIcons.Ide.Gift
+    }
+  }
+
+  override fun getActionUpdateThread() = ActionUpdateThread.EDT
+}
