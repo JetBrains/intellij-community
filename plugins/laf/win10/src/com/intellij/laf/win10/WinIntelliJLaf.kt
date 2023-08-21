ecb8037f5574d16c479fb6abce4815203b36e8f1
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-08-21 17:44:41 +0300

    fix compilation
    GitOrigin-RevId: 3f6352397b8df13ea1fc95d87cda689d6a3d3778

--- plugins/laf/win10/src/com/intellij/laf/win10/WinIntelliJLaf.kt
+++ plugins/laf/win10/src/com/intellij/laf/win10/WinIntelliJLaf.kt
@@ -1,9 +1,13 @@
+@file:Suppress("ReplacePutWithAssignment")
+
 package com.intellij.laf.win10
 
 import com.intellij.ide.ui.laf.IntelliJLaf
 import com.intellij.ide.ui.laf.MenuArrowIcon
 import com.intellij.util.ui.StartupUiUtil
 import javax.swing.UIDefaults
+
+private const val MENU_TRIANGLE_ICON_NAME = "menuTriangle"
 
 internal class WinIntelliJLaf : IntelliJLaf() {
   init {
@@ -12,25 +16,19 @@ internal class WinIntelliJLaf : IntelliJLaf() {
 
   override fun getName() = WinLafProvider.LAF_NAME
 
-  override fun getPrefix(): String {
-    return "com/intellij/laf/win10/win10intellijlaf"
-  }
-
-  override fun getSystemPrefix(): String? {
-    return null
-  }
+  override val prefix: String
+    get() = "com/intellij/laf/win10/win10intellijlaf"
+  override val systemPrefix: String?
+    get() = null
 
   override fun loadDefaults(defaults: UIDefaults) {
     super.loadDefaults(defaults)
-    defaults["Menu.arrowIcon"] = Win10MenuArrowIcon()
-  }
 
-  private class Win10MenuArrowIcon :
-    MenuArrowIcon({ WinIconLookup.getIcon(name = MENU_TRIANGLE_ICON_NAME) },
-                  { WinIconLookup.getIcon(name = MENU_TRIANGLE_ICON_NAME, selected = true) },
-                  { WinIconLookup.getIcon(name = MENU_TRIANGLE_ICON_NAME, enabled = false) })
-
-  companion object {
-    const val MENU_TRIANGLE_ICON_NAME = "menuTriangle"
+    defaults.put("Menu.arrowIcon", Win10MenuArrowIcon())
   }
 }
+
+private class Win10MenuArrowIcon :
+  MenuArrowIcon(icon = { WinIconLookup.getIcon(name = MENU_TRIANGLE_ICON_NAME) },
+                selectedIcon = { WinIconLookup.getIcon(name = MENU_TRIANGLE_ICON_NAME, selected = true) },
+                disabledIcon = { WinIconLookup.getIcon(name = MENU_TRIANGLE_ICON_NAME, enabled = false) })
