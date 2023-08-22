c3f0e3229232bea496557efcbb7950c04e49740f
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2023-08-22 20:04:33 +0200

    IDEA-327883 persistent string enumerator test (kinda WIP)
    GitOrigin-RevId: 932d0ec4d9974d845c70511923b052abee94d619

---
+++ platform/platform-tests/durability/testSrc/com/intellij/openapi/vfs/newvfs/persistent/enumerators/StringPersistentEnumeratorApp.kt
@@ -0,0 +1,40 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.vfs.newvfs.persistent.enumerators
+
+import com.intellij.openapi.vfs.newvfs.persistent.App
+import com.intellij.openapi.vfs.newvfs.persistent.AppAgent
+import com.intellij.openapi.vfs.newvfs.persistent.dev.StringPersistentEnumerator
+import java.nio.file.Path
+import kotlin.io.path.absolute
+
+class StringPersistentEnumeratorApp: App {
+  private class SPE : StringEnum {
+    val instance = StringPersistentEnumerator(Path.of("pse.data").absolute())
+
+    override fun enumerate(s: String): Int {
+      return instance.enumerate(s)
+        //.also { instance.force() }
+    }
+
+    override fun valueOf(idx: Int): String? {
+      try {
+        return instance.valueOf(idx)
+      } catch (_: IllegalArgumentException) {
+        return null
+      }
+    }
+
+    override fun flush() {
+      return instance.force()
+    }
+
+    override fun close() {
+      return instance.close()
+    }
+  }
+
+  override fun run(appAgent: AppAgent) {
+    val backend = SPE()
+    StringEnumApp(backend).run(appAgent)
+  }
+}
