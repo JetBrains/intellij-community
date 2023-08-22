137bcdc03b7dae6e3939b2bc96dc5caa544d67b2
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2023-08-22 19:07:13 +0400

    [wsm] Change CanarySdk from MockSdk to the real one
    GitOrigin-RevId: d6dbdbf2ede66be171dad944da874e03007018fd

--- platform/lang-impl/testSources/com/intellij/openapi/roots/ui/configuration/SdkComboBoxTestCase.kt
+++ platform/lang-impl/testSources/com/intellij/openapi/roots/ui/configuration/SdkComboBoxTestCase.kt
@@ -2,6 +2,7 @@
 package com.intellij.openapi.roots.ui.configuration
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.invokeAndWaitIfNeeded
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.project.Project
@@ -79,26 +80,9 @@ abstract class SdkComboBoxTestCase : SdkTestCase() {
    *  [SdkConfigurationUtil.selectSdkHome(SdkType, Component, Consumer<in String>)],
    *  [SdkComboBoxTestCase.TestProjectSdksModel]
    */
-  object CanarySdk : TestSdk("canary", "canary-home", "canary-version", TestSdkType) {
-    fun <R> replaceByTestSdk(action: () -> R): R {
-      return invokeAndWaitIfNeeded {
-        runWriteAction {
-          val projectSdkTable = ProjectJdkTable.getInstance()
-          projectSdkTable.addJdk(CanarySdk)
-          try {
-            action()
-          }
-          finally {
-            projectSdkTable.removeJdk(CanarySdk)
-          }
-        }
-      }
-    }
-  }
-
   class TestProjectSdksModel : ProjectSdksModel(), Disposable {
     override fun addSdk(type: SdkType, home: String, callback: com.intellij.util.Consumer<in Sdk>?) {
-      if (home == CanarySdk.homePath) {
+      if (home == "canary-home") {
         val sdk = TestSdkGenerator.createNextSdk()
         setupSdk(sdk, callback)
       }
@@ -172,10 +156,25 @@ abstract class SdkComboBoxTestCase : SdkTestCase() {
     }
 
     fun SdkComboBox.touchAddAction(): Sdk {
-      CanarySdk.replaceByTestSdk {
-        selectedItem = itemSequence
-          .filterIsInstance<SdkListItem.ActionItem>()
-          .first { it.role == SdkListItem.ActionRole.ADD }
+      val canarySdk: Sdk = ProjectJdkTable.getInstance().createSdk("canary", TestSdkType)
+      val sdkModificator = canarySdk.sdkModificator
+      sdkModificator.homePath = "canary-home"
+      sdkModificator.versionString = "canary-version"
+      ApplicationManager.getApplication().runWriteAction { sdkModificator.commitChanges() }
+
+      invokeAndWaitIfNeeded {
+        runWriteAction {
+          val projectSdkTable = ProjectJdkTable.getInstance()
+          projectSdkTable.addJdk(canarySdk)
+          try {
+            selectedItem = itemSequence
+              .filterIsInstance<SdkListItem.ActionItem>()
+              .first { it.role == SdkListItem.ActionRole.ADD }
+          }
+          finally {
+            projectSdkTable.removeJdk(canarySdk)
+          }
+        }
       }
       return TestSdkGenerator.getCurrentSdk()
     }
