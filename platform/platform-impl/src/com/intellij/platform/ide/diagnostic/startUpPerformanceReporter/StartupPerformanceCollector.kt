fdd4c7d0934280b19fa8218975d86f62c4475551
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-08-23 17:17:41 +0300

    Add comment to clarify "totalDuration" in StartupPerformanceCollector
    In the StartupPerformanceCollector in platform-impl, a comment was added to provide clarity on the meaning of "totalDuration". The comment specifies that the term refers to the "Activities.PROJECT_DUMB_POST_START_UP_ACTIVITIES" metric.
    
    GitOrigin-RevId: c83d147215e7dbda162289147b5f20748fde56db

--- platform/platform-impl/src/com/intellij/platform/ide/diagnostic/startUpPerformanceReporter/StartupPerformanceCollector.kt
+++ platform/platform-impl/src/com/intellij/platform/ide/diagnostic/startUpPerformanceReporter/StartupPerformanceCollector.kt
@@ -37,6 +37,7 @@ internal fun logStartupPerformanceEvent(project: Project?, eventId: String, dura
     "totalDuration" -> {
       val openedProjects = ProjectManager.getInstance().openProjects.size
       val projectToReport: Project? = if (openedProjects == 1) project else null
+      // total duration here it is Activities.PROJECT_DUMB_POST_START_UP_ACTIVITIES
       TOTAL_DURATION.log(projectToReport, DURATION.with(duration), PROJECTS_COUNT.with(openedProjects))
     }
     else -> logger<StartupPerformanceCollector>().error("Trying to log unknown startup performance metric ('$eventId'). To record it, register a corresponding event and increment group version.")
