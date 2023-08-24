4bd88f09905d56c5cc8e1f8775344f3413a39ff0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-08-24 07:59:14 +0300

    remove dead code
    GitOrigin-RevId: d08d26eb176313ae867307041f87fae7d8874bb6

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/refresh.kt
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/refresh.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vfs.newvfs
 
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.progress.blockingContext
 import com.intellij.openapi.util.registry.Registry
@@ -21,8 +20,6 @@ internal class RefreshWorkerHelper(private val coroutineScope: CoroutineScope) {
   @OptIn(ExperimentalCoroutinesApi::class)
   fun <T> createRefreshWorkerExecutor(): Function<Supplier<T>, CompletableFuture<T>> {
     val dispatcher = Dispatchers.IO.limitedParallelism(parallelism) + CoroutineName("VFS Refresh")
-    @Suppress("DEPRECATION")
-    ApplicationManager.getApplication().coroutineScope
     return Function {
       coroutineScope.async(dispatcher) {
         blockingContext {
