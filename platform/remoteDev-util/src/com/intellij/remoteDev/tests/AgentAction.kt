10840dd6119b52cadb9b489486d11f59f21205ed
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2023-08-24 19:11:28 +0200

    [rdct-tests]: use own WaitUtils, use kotlin.time
    GitOrigin-RevId: 596e2719f32bc1c627ea9e477b7247e6c1cc88d0

--- platform/remoteDev-util/src/com/intellij/remoteDev/tests/AgentAction.kt
+++ platform/remoteDev-util/src/com/intellij/remoteDev/tests/AgentAction.kt
@@ -2,6 +2,7 @@ package com.intellij.remoteDev.tests
 
 import com.jetbrains.rd.framework.impl.RdTask
 import org.jetbrains.annotations.ApiStatus
+import kotlin.time.Duration
 
 /**
  * Represents a single test action in a distributed test
