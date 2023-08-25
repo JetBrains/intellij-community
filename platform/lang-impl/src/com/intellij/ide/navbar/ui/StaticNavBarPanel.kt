219e424de4e900bc966adc5ecad1b5700f39f15c
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-08-25 14:28:00 +0200

    IDEA-320784 extract `trackCurrentWindow` from `StaticNavBarPanel`
    GitOrigin-RevId: c4cb2461cd0ec05c558e8accf0f49601aa1991d9

--- platform/lang-impl/src/com/intellij/ide/navbar/ui/StaticNavBarPanel.kt
+++ platform/lang-impl/src/com/intellij/ide/navbar/ui/StaticNavBarPanel.kt
@@ -7,11 +7,8 @@ import com.intellij.ide.navbar.vm.NavBarVm
 import com.intellij.model.Pointer
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.project.Project
-import com.intellij.ui.ComponentUtil
 import com.intellij.ui.components.JBPanel
 import com.intellij.util.attachAsChildTo
-import com.intellij.util.ui.update.Activatable
-import com.intellij.util.ui.update.UiNotifyConnector
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import java.awt.BorderLayout
@@ -22,23 +19,13 @@ internal class StaticNavBarPanel(
   private val cs: CoroutineScope,
   private val updateRequests: Flow<Any>,
   private val requestNavigation: (Pointer<out NavBarItem>) -> Unit,
-) : JBPanel<StaticNavBarPanel>(BorderLayout()), Activatable {
+) : JBPanel<StaticNavBarPanel>(BorderLayout()) {
 
-  private val _window: MutableStateFlow<Window?> = MutableStateFlow(null)
+  private val _window = trackCurrentWindow(this)
   private val _vm: MutableStateFlow<NavBarVm?> = MutableStateFlow(null)
   val model: NavBarVm? get() = _vm.value
 
-  override fun hideNotify() {
-    _window.value = null
-  }
-
-  override fun showNotify() {
-    _window.value = ComponentUtil.getWindow(this)
-  }
-
   init {
-    UiNotifyConnector.installOn(this, this, false)
-
     // This coroutine will GC-ed together with the `StaticNavBarPanel` instance as long as it isn't referenced by anything else,
     // `attachAsChildTo` makes sure the coroutine refs are cleaned.
     @OptIn(DelicateCoroutinesApi::class)
