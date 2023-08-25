f3cdebaf53d806b9c36363cd43746ac66128122b
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2023-08-25 11:45:03 +0200

    [gradle-runner] IDEA-330527 extract init script parameters class
    - KTIJ-26692
    
    GitOrigin-RevId: 5e05cd7b774234a39352981bcf20cd6c80b7143a

---
+++ plugins/gradle/java/src/execution/build/GradleInitScriptParameters.kt
@@ -0,0 +1,18 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.gradle.execution.build
+
+import com.intellij.execution.JavaRunConfigurationBase
+import com.intellij.openapi.module.Module
+
+interface GradleInitScriptParameters {
+  val configuration: JavaRunConfigurationBase
+  val module: Module
+  val workingDirectory: String?
+  val params: String
+  val gradleTaskPath: String
+  val runAppTaskName: String
+  val mainClass: String
+  val javaExePath: String
+  val sourceSetName: String
+  val javaModuleName: String?
+}
