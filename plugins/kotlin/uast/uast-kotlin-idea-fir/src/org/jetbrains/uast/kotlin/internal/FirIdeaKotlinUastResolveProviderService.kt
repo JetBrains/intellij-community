566e36b6eef104dece344bfe03a705974f9e8e49
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-08-26 13:50:49 +0200

    [kotlin][k2][uast] drop redundant isJvmElement check
    This check is because in most cases as a platform was chosen JVM
    due to `KtElement#platform` implementation.
    Anyway, this check is redundant now, because SLC will care about the platform.
    
    ^KT-60318
    
    GitOrigin-RevId: 28119b02b073fcff5f222df7846149164cbd68db

--- plugins/kotlin/uast/uast-kotlin-idea-fir/src/org/jetbrains/uast/kotlin/internal/FirIdeaKotlinUastResolveProviderService.kt
+++ plugins/kotlin/uast/uast-kotlin-idea-fir/src/org/jetbrains/uast/kotlin/internal/FirIdeaKotlinUastResolveProviderService.kt
@@ -6,27 +6,11 @@ import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.idea.base.util.isOutsiderFile
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.uast.kotlin.FirKotlinUastResolveProviderService
-import org.jetbrains.uast.kotlin.internal.util.ReadActionSingleValueCache
 import org.jetbrains.uast.kotlin.unwrapFakeFileForLightClass
 
 class FirIdeaKotlinUastResolveProviderService : FirKotlinUastResolveProviderService {
-    /**
-     * A [ReadActionSingleValueCache] is appropriate for UAST conversions because elements from the same file are checked together. The
-     * read-action cache allows us to avoid cache invalidation after module or target platform changes, and also to use the [KtFile] as a
-     * key without worrying about smart pointers.
-     */
-    private val supportedFileCache = ReadActionSingleValueCache(::isSupportedFile)
-
     override fun isSupportedElement(psiElement: PsiElement): Boolean {
         val file = psiElement.containingFile?.let(::unwrapFakeFileForLightClass) as? KtFile ?: return false
-        return supportedFileCache.getCachedOrEvaluate(file)
-    }
-
-    private fun isSupportedFile(file: KtFile): Boolean {
-        if (!file.isJvmElement) {
-            return false
-        }
-
         val virtualFile = file.virtualFile
         return virtualFile == null || !isOutsiderFile(virtualFile)
     }
