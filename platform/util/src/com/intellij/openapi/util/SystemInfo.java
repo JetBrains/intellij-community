8b51a25f75e5d93991f41d95487eaab4b399a6ea
Konstantin Bulenkov <kb@jetbrains.com>
2023-08-28 22:39:06 +0200

    Add macOS Sonoma
    GitOrigin-RevId: 3bd2898b1e453037ff03d6cda01159d9ec22c7b5

--- platform/util/src/com/intellij/openapi/util/SystemInfo.java
+++ platform/util/src/com/intellij/openapi/util/SystemInfo.java
@@ -95,6 +95,7 @@ public final class SystemInfo {
   public static final boolean isMacOSBigSur = isMac && isOsVersionAtLeast("10.16");
   public static final boolean isMacOSMonterey = isMac && isOsVersionAtLeast("12.0");
   public static final boolean isMacOSVentura = isMac && isOsVersionAtLeast("13.0");
+  public static final boolean isMacOSSonoma = isMac && isOsVersionAtLeast("14.0");
 
   /**
    * Build number is the only more or less stable approach to get comparable win version.
