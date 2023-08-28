249febac61acfa4ea16766e92b53d8f9553a05bf
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2023-08-28 14:30:27 +0000

    IJPL-176 move module intellij.platform.ijent to community and make it available for WSL
    As for now, IJent is available only when running PyCharm Pro from sources. Other targets are coming later.
    
    Also, this commit seems to resolve IDEA-330810.
    
    
    Merge-request: IJ-MR-113871
    Merged-by: Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
    
    GitOrigin-RevId: ccf417002f0ec32c8f9cc9b3808e831593c166c8

---
+++ platform/ijent/src/com/intellij/platform/ijent/IjentSessionProvider.kt
@@ -0,0 +1,143 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.ijent
+
+import com.intellij.openapi.components.serviceAsync
+import com.intellij.openapi.diagnostic.debug
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.diagnostic.trace
+import com.intellij.util.attachAsChildTo
+import com.intellij.util.namedChildScope
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.SendChannel
+import org.jetbrains.annotations.ApiStatus.OverrideOnly
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicLong
+
+/**
+ * Given that there is some IJent process launched, this extension gets handles to stdin+stdout of the process and returns
+ * an [IjentApi] instance for calling procedures on IJent side.
+ */
+interface IjentSessionProvider {
+  @get:OverrideOnly
+  val epCoroutineScope: CoroutineScope
+
+  /**
+   * When calling the method, there's no need to wire [communicationCoroutineScope] to [epCoroutineScope],
+   * since it is already performed by factory methods.
+   */
+  @OverrideOnly
+  suspend fun connect(
+    id: Long,
+    communicationCoroutineScope: CoroutineScope,
+    inputStream: InputStream,
+    outputStream: OutputStream,
+  ): IjentApi
+
+  companion object {
+    private val LOG = logger<IjentSessionProvider>()
+
+    private val counter = AtomicLong()
+
+    /**
+     * The session exits when one of the following happens:
+     * * The job corresponding to [communicationCoroutineScope] is finished.
+     * * [epCoroutineScope] is finished.
+     * * [inputStream] is closed.
+     */
+    suspend fun connect(communicationCoroutineScope: CoroutineScope, process: Process): IjentApi {
+      val provider = serviceAsync<IjentSessionProvider>()
+      val id = counter.getAndIncrement()
+      val label = "IJent #$id"
+      val epCoroutineScope = provider.epCoroutineScope
+      val childScope = communicationCoroutineScope
+        .namedChildScope(label, supervisor = false)
+        .apply { attachAsChildTo(epCoroutineScope) }
+      childScope.launch(Dispatchers.IO + childScope.coroutineNameAppended("$label > watchdog")) {
+        while (true) {
+          if (process.waitFor(10, TimeUnit.MILLISECONDS)) {
+            val exitValue = process.exitValue()
+            LOG.debug { "$label exit code $exitValue" }
+            check(exitValue == 0) { "Process has exited with code $exitValue" }
+            cancel()
+            break
+          }
+          delay(100)
+        }
+      }
+      childScope.launch(Dispatchers.IO + childScope.coroutineNameAppended("$label > finalizer")) {
+        try {
+          awaitCancellation()
+        }
+        catch (err: Exception) {
+          LOG.debug(err) { "$label is going to be terminated due to receiving an error" }
+          throw err
+        }
+        finally {
+          if (process.isAlive) {
+            GlobalScope.launch(Dispatchers.IO + coroutineNameAppended("actual destruction")) {
+              try {
+                if (process.waitFor(5, TimeUnit.SECONDS)) {
+                  LOG.debug { "$label exit code ${process.exitValue()}" }
+                }
+              }
+              finally {
+                if (process.isAlive) {
+                  LOG.debug { "The process $label is still alive, it will be killed" }
+                  process.destroy()
+                }
+              }
+            }
+            GlobalScope.launch(Dispatchers.IO) {
+              LOG.debug { "Closing stdin of $label" }
+              process.outputStream.close()
+            }
+          }
+        }
+      }
+
+      val processScopeNamePrefix = childScope.coroutineContext[CoroutineName]?.let { "$it >" } ?: ""
+
+      epCoroutineScope.launch(Dispatchers.IO) {
+        withContext(coroutineNameAppended("$processScopeNamePrefix $label > logger")) {
+          process.errorReader().use { errorReader ->
+            for (line in errorReader.lineSequence()) {
+              // TODO It works incorrectly with multiline log messages.
+              when (line.splitToSequence(' ').drop(1).take(1).firstOrNull()) {
+                "TRACE" -> LOG.trace { "$label log: $line" }
+                "DEBUG" -> LOG.debug { "$label log: $line" }
+                "INFO" -> LOG.info("$label log: $line")
+                "WARN" -> LOG.warn("$label log: $line")
+                "ERROR" -> LOG.error("$label log: $line")
+                else -> LOG.trace { "$label log: $line" }
+              }
+              yield()
+            }
+          }
+        }
+      }
+      return provider.connect(id, childScope, process.inputStream, process.outputStream)
+    }
+  }
+}
+
+interface IjentApi {
+  suspend fun executeProcess(exe: String, vararg args: String, env: Map<String, String> = emptyMap()): ExecuteProcessResult
+
+  sealed interface ExecuteProcessResult {
+    class Success(val process: IjentChildProcess) : ExecuteProcessResult
+    data class Failure(val errno: Int, val message: String) : ExecuteProcessResult
+  }
+}
+
+interface IjentChildProcess {
+  val pid: Int
+  val stdin: SendChannel<ByteArray>
+  val stdout: ReceiveChannel<ByteArray>
+  val stderr: ReceiveChannel<ByteArray>
+  val exitCode: Deferred<Int>
+
+  suspend fun sendSignal(signal: Int)  // TODO Use a separate class for signals.
+}
