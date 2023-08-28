3072396ff6502d1c41a98f7eeeda4d8347ba9347
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-08-28 18:28:31 +0200

    [java-inspections] IDEA-328239 Casting with conjunction
    - Check if SAM is implemented in conjunction
    - Created lambda Methods signature with generic from types
    
    GitOrigin-RevId: 97e21dbacccd9382e48387295a911b127882e625

--- java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/LambdaHighlightingUtil.java
+++ java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/LambdaHighlightingUtil.java
@@ -75,17 +75,23 @@ public final class LambdaHighlightingUtil {
     if (functionalInterfaceType instanceof PsiIntersectionType) {
       Set<MethodSignature> signatures = new HashSet<>();
       for (PsiType type : ((PsiIntersectionType)functionalInterfaceType).getConjuncts()) {
-        if (checkInterfaceFunctional(type) == null) {
-          MethodSignature signature = LambdaUtil.getFunction(PsiUtil.resolveClassInType(type));
+        String error = checkInterfaceFunctional(type);
+        if (error == null) {
+          MethodSignature signature = LambdaUtil.getLambdaSignatureWithCommonSubstitutor(type);
           LOG.assertTrue(signature != null, type.getCanonicalText());
           signatures.add(signature);
+        }
+        else {
+          if (!error.equals(JavaErrorBundle.message("target.method.is.generic"))) {
+            return error;
+          }
         }
       }
 
       if (signatures.size() > 1) {
         return JavaErrorBundle.message("multiple.non.overriding.abstract.methods.found.in.0", functionalInterfaceType.getPresentableText());
       }
-      return null;
+      return signatures.isEmpty() ? JavaErrorBundle.message("not.a.functional.interface",functionalInterfaceType.getPresentableText()) : null;
     }
     PsiClassType.ClassResolveResult resolveResult = PsiUtil.resolveGenericsClassInType(functionalInterfaceType);
     PsiClass aClass = resolveResult.getElement();
