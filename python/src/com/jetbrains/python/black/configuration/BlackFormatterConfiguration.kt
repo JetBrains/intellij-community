ba8f50a6cd1574e7b19fc137e863243086398a1f
Daniil Kalinin <Daniil.Kalinin@jetbrains.com>
2023-08-28 11:17:14 +0200

    PY-61921 Black configuration: Don't put sdkUUID in shared configuration file
    Store selected sdk as sdk name as it is done for project sdks
    
    (cherry picked from commit 9cf81c03d37de31ca17db5eb93a33d6603c98940)
    (cherry picked from commit 09b20de4d95fdee88a72a1e1602eecb4f35c3711)
    
    IJ-CR-114685
    
    GitOrigin-RevId: 7dc3f6d2c927ca120170c4db38f4e38b90a44dac

--- python/src/com/jetbrains/python/black/configuration/BlackFormatterConfiguration.kt
+++ python/src/com/jetbrains/python/black/configuration/BlackFormatterConfiguration.kt
@@ -7,9 +7,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.modules
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.util.xmlb.XmlSerializerUtil
-import com.jetbrains.python.sdk.PythonSdkAdditionalData
 import com.jetbrains.python.sdk.pythonSdk
-import java.util.*
 
 const val BLACK_ID: String = "Black"
 
@@ -19,7 +17,7 @@ data class BlackFormatterConfiguration(var enabledOnReformat: Boolean,
                                        var executionMode: ExecutionMode,
                                        var pathToExecutable: String?,
                                        var cmdArguments: String,
-                                       var sdkUUID: String?)
+                                       var sdkName: String?)
   : PersistentStateComponent<BlackFormatterConfiguration> {
 
   @Suppress("unused") // Empty constructor required for state components
@@ -35,16 +33,12 @@ data class BlackFormatterConfiguration(var enabledOnReformat: Boolean,
     PACKAGE,
   }
 
-  fun getSdk(project: Project): Sdk? = sdkUUID?.let { uuidString ->
-    val uuid = runCatching {
-      UUID.fromString(uuidString)
-    }.getOrElse {
-      return@let null
-    }
+  fun getSdk(project: Project): Sdk? {
+    if (sdkName == null) return null
 
-    project.modules
+    return project.modules
       .mapNotNull { it.pythonSdk }
-      .firstOrNull { sdk -> (sdk.sdkAdditionalData as PythonSdkAdditionalData).uuid == uuid }
+      .firstOrNull { sdk -> sdk.name == sdkName }
   }
 
   companion object {
@@ -67,7 +61,16 @@ data class BlackFormatterConfiguration(var enabledOnReformat: Boolean,
                                                                                "auto-detection."),
     )
 
-    fun getBlackConfiguration(project: Project): BlackFormatterConfiguration = project.getService(BlackFormatterConfiguration::class.java)
+    fun getBlackConfiguration(project: Project): BlackFormatterConfiguration {
+      val config = project.getService(BlackFormatterConfiguration::class.java)
+
+      // Try to use first interpreter as a fallback and default value.
+      if (config.sdkName == null) {
+        config.sdkName = project.modules.firstNotNullOfOrNull { it.pythonSdk }?.name
+      }
+
+      return config
+    }
   }
 
   class CliOptionFlag(val flag: String, val option: BlackFormatterOption) {
