55218fc9e455dad7e11a5acf30217baa55d00df6
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-08-29 14:01:19 +0200

    [kotlin][k2] move out-of-block modification processing to LL FIR
    This is required to separate LL FIR implementation details from IDE usage
    
    ^KT-60610 Fixed
    
    GitOrigin-RevId: 01dd818b252468bdca1585b5e1e1bf78ef87ff71

--- plugins/kotlin/base/fir/analysis-api-providers/src/org/jetbrains/kotlin/idea/base/fir/analysisApiProviders/FirIdeOutOfBlockPsiTreeChangePreprocessor.kt
+++ plugins/kotlin/base/fir/analysis-api-providers/src/org/jetbrains/kotlin/idea/base/fir/analysisApiProviders/FirIdeOutOfBlockPsiTreeChangePreprocessor.kt
@@ -2,7 +2,6 @@
 
 package org.jetbrains.kotlin.idea.base.fir.analysisApiProviders
 
-import com.intellij.lang.java.JavaLanguage
 import com.intellij.openapi.project.Project
 import com.intellij.psi.*
 import com.intellij.psi.impl.PsiModificationTrackerImpl
@@ -10,14 +9,9 @@ import com.intellij.psi.impl.PsiTreeChangeEventImpl
 import com.intellij.psi.impl.PsiTreeChangeEventImpl.PsiEventType
 import com.intellij.psi.impl.PsiTreeChangePreprocessor
 import org.jetbrains.kotlin.analysis.low.level.api.fir.LLFirInternals
-import org.jetbrains.kotlin.analysis.low.level.api.fir.file.structure.getNonLocalReanalyzableContainingDeclaration
-import org.jetbrains.kotlin.analysis.low.level.api.fir.file.structure.invalidateAfterInBlockModification
-import org.jetbrains.kotlin.analysis.project.structure.ProjectStructureProvider
-import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.kotlin.analysis.low.level.api.fir.file.structure.LLFirDeclarationModificationService
+import org.jetbrains.kotlin.analysis.low.level.api.fir.file.structure.LLFirDeclarationModificationService.ModificationType
 import org.jetbrains.kotlin.idea.util.publishGlobalSourceOutOfBlockModification
-import org.jetbrains.kotlin.idea.util.publishModuleOutOfBlockModification
-import org.jetbrains.kotlin.psi.KtAnnotated
-import org.jetbrains.kotlin.psi.KtCodeFragment
 
 internal class FirIdeOutOfBlockPsiTreeChangePreprocessor(private val project: Project) : PsiTreeChangePreprocessor {
     override fun treeChanged(event: PsiTreeChangeEventImpl) {
@@ -48,55 +42,15 @@ internal class FirIdeOutOfBlockPsiTreeChangePreprocessor(private val project: Pr
             return
         }
 
-        val changeType = calculateChangeType(event.code, rootElement, child)
-        when (changeType) {
-            ChangeType.Invisible -> {}
-            ChangeType.OutOfBlock -> outOfBlockInvalidation(rootElement)
-            is ChangeType.InBlock -> {
-                // trigger in-block a FIR tree invalidation
-                @OptIn(LLFirInternals::class)
-                val isOutOfBlock = !invalidateAfterInBlockModification(changeType.blockOwner)
-
-                // In some cases, we can understand that it is not in-block modification only during in-block modification.
-                // Probably it is not reachable, but this statement requires some additional investigation
-                if (isOutOfBlock) {
-                    outOfBlockInvalidation(rootElement)
-                }
-            }
-        }
-    }
-
-    private fun outOfBlockInvalidation(element: PsiElement) {
-        val module = ProjectStructureProvider.getModule(project, element, contextualModule = null)
-        module.publishModuleOutOfBlockModification()
-    }
-}
-
-private fun calculateChangeType(
-  code: PsiEventType,
-  rootElement: PsiElement,
-  child: PsiElement?,
-): ChangeType = when (rootElement.language) {
-    KotlinLanguage.INSTANCE -> kotlinChangeType(code, child ?: rootElement)
-
-    // TODO improve for Java KTIJ-21684
-    JavaLanguage.INSTANCE -> ChangeType.OutOfBlock
-
-    // Any other language may cause OOBM in Kotlin too
-    else -> ChangeType.OutOfBlock
-}
-
-private fun kotlinChangeType(code: PsiEventType, psi: PsiElement): ChangeType = when {
-    // If PSI is not valid, well something bad happened, OOBM won't hurt
-    !psi.isValid -> ChangeType.OutOfBlock
-    psi is PsiWhiteSpace || psi is PsiComment -> ChangeType.Invisible
-    else -> {
-        val inBlockModificationOwner = nonLocalDeclarationForLocalChange(psi)
-        if (inBlockModificationOwner != null && (psi.parent != inBlockModificationOwner || code != PsiEventType.CHILD_ADDED)) {
-            ChangeType.InBlock(inBlockModificationOwner)
-        } else {
-            ChangeType.OutOfBlock
-        }
+        @OptIn(LLFirInternals::class)
+        LLFirDeclarationModificationService.getInstance(project).elementModified(
+            element = child ?: rootElement,
+            modificationType = if (event.code == PsiEventType.CHILD_ADDED) {
+                ModificationType.NewElement
+            } else {
+                ModificationType.Unknown
+            },
+        )
     }
 }
 
@@ -106,7 +60,7 @@ private fun kotlinChangeType(code: PsiEventType, psi: PsiElement): ChangeType = 
  * We don't want to publish any global out-of-block modification on roots changes, because relevant roots changes already cause module
  * state modification events. Such a module state modification event includes the exact module that was affected by the roots change,
  * instead of a less specific global out-of-block modification event. This allows a consumer such as session invalidation to invalidate
- * sessions more granularly. Additionally, many roots changes don't require any event to be published because a corresponding [KtModule]
+ * sessions more granularly. Additionally, many roots changes don't require any event to be published because a corresponding [KtModule][org.jetbrains.kotlin.analysis.project.structure.KtModule]
  * does not exist for the changed module (e.g. when no content roots have been added yet), so roots changes [PsiTreeChangeEvent]s are
  * overzealous, while the module state modification service can handle such cases gracefully.
  */
@@ -114,15 +68,4 @@ private fun PsiTreeChangeEventImpl.isGlobalChange() = when (code) {
     PsiEventType.PROPERTY_CHANGED -> propertyName === PsiTreeChangeEvent.PROP_UNLOADED_PSI
     PsiEventType.CHILD_MOVED -> oldParent is PsiDirectory || newParent is PsiDirectory
     else -> parent is PsiDirectory
-}
-
-internal fun nonLocalDeclarationForLocalChange(psi: PsiElement): KtAnnotated? {
-    @OptIn(LLFirInternals::class)
-    return psi.getNonLocalReanalyzableContainingDeclaration() ?: psi.containingFile as? KtCodeFragment
-}
-
-private sealed class ChangeType {
-    object OutOfBlock : ChangeType()
-    object Invisible : ChangeType()
-    class InBlock(val blockOwner: KtAnnotated) : ChangeType()
 }
--- plugins/kotlin/base/fir/analysis-api-providers/src/org/jetbrains/kotlin/idea/base/fir/analysisApiProviders/KotlinChangeLocalityDetector.kt
+++ plugins/kotlin/base/fir/analysis-api-providers/src/org/jetbrains/kotlin/idea/base/fir/analysisApiProviders/KotlinChangeLocalityDetector.kt
@@ -3,10 +3,14 @@ package org.jetbrains.kotlin.idea.base.fir.analysisApiProviders
 
 import com.intellij.codeInsight.daemon.ChangeLocalityDetector
 import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.analysis.low.level.api.fir.LLFirInternals
+import org.jetbrains.kotlin.analysis.low.level.api.fir.file.structure.LLFirDeclarationModificationService
 
 internal class KotlinChangeLocalityDetector : ChangeLocalityDetector {
-  override fun getChangeHighlightingDirtyScopeFor(changedElement: PsiElement): PsiElement? {
-    // we shouldn't process comments here because the default detector will do that for us
-    return nonLocalDeclarationForLocalChange(changedElement)
-  }
+    override fun getChangeHighlightingDirtyScopeFor(changedElement: PsiElement): PsiElement? {
+        // we shouldn't process comments here because the default detector will do that for us
+
+        @OptIn(LLFirInternals::class)
+        return LLFirDeclarationModificationService.getInstance(changedElement.project).elementToRehighlight(changedElement)
+    }
 }
