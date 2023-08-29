4c67115a72fedf918003d20b34fa6375e9f689bc
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2023-08-29 12:28:38 +0300

    IDEA-329708 Show static banner when animation is loading in New UI Onboarding dialog
    So there will be no blinking when video will finally loaded.
    
    GitOrigin-RevId: 93f49ce0c86bc41c44f17683dc506a2813d66c7f

--- platform/new-ui-onboarding/src/com/intellij/platform/ide/newUiOnboarding/NewUiOnboardingDialog.kt
+++ platform/new-ui-onboarding/src/com/intellij/platform/ide/newUiOnboarding/NewUiOnboardingDialog.kt
@@ -1,9 +1,13 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.ide.newUiOnboarding
 
+import com.intellij.ide.plugins.MultiPanel
 import com.intellij.ide.ui.laf.darcula.ui.DarculaButtonUI
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.util.text.HtmlChunk
 import com.intellij.ui.*
 import com.intellij.ui.dsl.builder.panel
@@ -15,14 +19,21 @@ import com.intellij.util.ui.JBDimension
 import com.intellij.util.ui.JBFont
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
+import org.cef.browser.CefBrowser
+import org.cef.handler.CefLoadHandlerAdapter
+import java.awt.Color
 import java.awt.Font
 import java.util.*
 import javax.swing.JComponent
+import javax.swing.JLabel
 import javax.swing.JRootPane
 import javax.swing.border.Border
 
 class NewUiOnboardingDialog(project: Project)
   : DialogWrapper(project, null, false, IdeModalityType.PROJECT, false) {
+  private val backgroundColor: Color
+    get() = JBColor.namedColor("NewUiOnboarding.Dialog.background", UIUtil.getPanelBackground())
+
   init {
     init()
     setUndecorated(true)
@@ -37,11 +48,7 @@ class NewUiOnboardingDialog(project: Project)
 
     val panel = panel {
       row {
-        val videoBase64 = readVideoAsBase64()
-        val browser = JBCefBrowser.createBuilder().setMouseWheelEventEnable(false).build()
-        val pageHtml = createVideoHtmlPage(videoBase64)
-        browser.loadHTML(pageHtml)
-        cell(browser.component)
+        cell(createAnimationPanel())
           .customize(UnscaledGaps.EMPTY)
           .applyToComponent {
             WindowMoveListener(this).installTo(components?.firstOrNull() ?: this)
@@ -82,15 +89,48 @@ class NewUiOnboardingDialog(project: Project)
       }
     }
 
-    panel.background = JBColor.namedColor("NewUiOnboarding.Dialog.background", UIUtil.getPanelBackground())
+    panel.background = backgroundColor
     return panel
   }
 
   override fun createContentPaneBorder(): Border? = null
 
+  private fun createAnimationPanel(): JComponent {
+    val browser = JBCefBrowser.createBuilder().setMouseWheelEventEnable(false).build()
+    UIUtil.setNotOpaqueRecursively(browser.component)
+
+    val banner = IconLoader.findIcon(BANNER_PATH, NewUiOnboardingDialog::class.java.classLoader)
+    val bannerLabel = JLabel(banner)
+    val multiPanel = object : MultiPanel() {
+      override fun create(key: Int): JComponent = when (key) {
+        BANNER_KEY -> bannerLabel
+        BROWSER_KEY -> browser.component
+        else -> throw IllegalArgumentException("Unknown key: ${key}")
+      }
+    }
+
+    browser.jbCefClient.addLoadHandler(object : CefLoadHandlerAdapter() {
+      override fun onLoadingStateChange(b: CefBrowser?, isLoading: Boolean, canGoBack: Boolean, canGoForward: Boolean) {
+        invokeLater(ModalityState.any()) {
+          if (isLoading) {
+            multiPanel.select(BANNER_KEY, true)
+          }
+          else multiPanel.select(BROWSER_KEY, true)
+        }
+      }
+    }, browser.cefBrowser)
+
+    val videoBase64 = readVideoAsBase64()
+    val pageHtml = createVideoHtmlPage(videoBase64)
+    browser.loadHTML(pageHtml)
+
+    multiPanel.select(BROWSER_KEY, true)
+    return multiPanel
+  }
+
   private fun createVideoHtmlPage(videoBase64: String): String {
     val componentId = "video"
-    val head = HtmlChunk.head().child(LottieUtils.getSingleContentCssStyles(Gray._32, componentId))
+    val head = HtmlChunk.head().child(LottieUtils.getSingleContentCssStyles(backgroundColor, componentId))
 
     val videoTag = HtmlChunk.tag("video")
       .attr("id", componentId)
@@ -117,5 +157,8 @@ class NewUiOnboardingDialog(project: Project)
 
   companion object {
     private const val VIDEO_PATH: String = "newUiOnboarding/DialogVideo.webm"
+    private const val BANNER_PATH: String = "newUiOnboarding/banner.png"
+    private const val BANNER_KEY: Int = 0
+    private const val BROWSER_KEY: Int = 1
   }
 }
