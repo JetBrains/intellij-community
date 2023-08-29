097bf4a23f4921b4ccfbe63f9e2115d95bf20c43
Bas Leijdekkers <basleijdekkers@gmail.com>
2023-08-29 17:40:18 +0200

    Java: add more Javadoc (IJ-CR-110406)
    (IJ-CR-110406, cherry picked from commit a587e9cd7170ab4df8c049b16a3677d738a5cf93)
    
    GitOrigin-RevId: 2c0294c05dde0a882885a541ca7a355e37899009

--- java/java-psi-api/src/com/intellij/psi/PsiFragment.java
+++ java/java-psi-api/src/com/intellij/psi/PsiFragment.java
@@ -2,14 +2,17 @@
 package com.intellij.psi;
 
 import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.Nullable;
 
 /**
+ * Represents a fragment of a template of a Java 21 Preview template expression.
+ *
  * @author Bas Leijdekkers
  */
 public interface PsiFragment extends PsiLiteralValue {
 
   /**
-   * Returns the type of the token.
+   * Returns the type of the fragment token.
    * One of the fields STRING_TEMPLATE_BEGIN, STRING_TEMPLATE_MID, STRING_TEMPLATE_END,
    * TEXT_BLOCK_TEMPLATE_BEGIN, TEXT_BLOCK_TEMPLATE_MID or TEXT_BLOCK_TEMPLATE_END of JavaTokenType.
    *
@@ -17,5 +20,16 @@ public interface PsiFragment extends PsiLiteralValue {
    */
   IElementType getTokenType();
 
+  /**
+   * @return true, if this fragment is a text block fragment, false, if this is string fragment.
+   */
   boolean isTextBlock();
+
+  /**
+   * Returns the string value of this fragment.
+   *
+   * @return the value of the expression, or null if the fragment is invalid (e.g. unclosed).
+   */
+  @Override
+  @Nullable String getValue();
 }
