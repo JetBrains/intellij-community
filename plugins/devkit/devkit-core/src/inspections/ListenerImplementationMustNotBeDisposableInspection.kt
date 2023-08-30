1b09369074550e29f9f1a61786774ec61fe77c1c
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2023-08-30 15:26:23 +0200

    IDEA-323204: Use checkClass() in ListenerImplementationMustNotBeDisposableInspection
    GitOrigin-RevId: 78338ccc5e488d60af93f8396e97ab8986723aff

--- plugins/devkit/devkit-core/src/inspections/ListenerImplementationMustNotBeDisposableInspection.kt
+++ plugins/devkit/devkit-core/src/inspections/ListenerImplementationMustNotBeDisposableInspection.kt
@@ -1,54 +1,49 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.inspections
 
-import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.codeInspection.InspectionManager
+import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.registerUProblem
 import com.intellij.lang.jvm.JvmClassKind
 import com.intellij.openapi.Disposable
-import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiClass
 import com.intellij.psi.util.InheritanceUtil
 import com.intellij.psi.util.PsiUtil
-import com.intellij.uast.UastHintedVisitorAdapter
 import org.jetbrains.idea.devkit.DevKitBundle
 import org.jetbrains.idea.devkit.dom.index.IdeaPluginRegistrationIndex
 import org.jetbrains.idea.devkit.util.PluginRelatedLocatorsUtils
 import org.jetbrains.uast.UClass
-import org.jetbrains.uast.visitor.AbstractUastNonRecursiveVisitor
 
-internal class ListenerImplementationMustNotBeDisposableInspection : DevKitUastInspectionBase() {
+internal class ListenerImplementationMustNotBeDisposableInspection : DevKitUastInspectionBase(UClass::class.java) {
 
-  override fun buildInternalVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
-    return UastHintedVisitorAdapter.create(
-      holder.file.language,
-      object : AbstractUastNonRecursiveVisitor() {
+  override fun checkClass(aClass: UClass, manager: InspectionManager, isOnTheFly: Boolean): Array<ProblemDescriptor>? {
+    val psiClass = aClass.javaPsi
 
-        override fun visitClass(node: UClass): Boolean {
-          val psiClass = node.javaPsi
+    if (psiClass.classKind != JvmClassKind.CLASS ||
+        PsiUtil.isAbstractClass(psiClass) ||
+        PsiUtil.isLocalOrAnonymousClass(psiClass) ||
+        PsiUtil.isInnerClass(psiClass) ||
+        psiClass.qualifiedName == null) return ProblemDescriptor.EMPTY_ARRAY
 
-          if (psiClass.classKind != JvmClassKind.CLASS ||
-              PsiUtil.isAbstractClass(psiClass) ||
-              PsiUtil.isLocalOrAnonymousClass(psiClass) ||
-              PsiUtil.isInnerClass(psiClass) ||
-              psiClass.qualifiedName == null) return true
+    if (!InheritanceUtil.isInheritor(psiClass, Disposable::class.java.canonicalName)) return ProblemDescriptor.EMPTY_ARRAY
 
-          if (!InheritanceUtil.isInheritor(psiClass, Disposable::class.java.canonicalName)) return true
+    if (!isRegisteredListener(psiClass)) return ProblemDescriptor.EMPTY_ARRAY
 
-          val isRegisteredListener = !IdeaPluginRegistrationIndex.processListener(
-            holder.project,
-            psiClass,
-            PluginRelatedLocatorsUtils.getCandidatesScope(holder.project),
-          ) { false }
-
-          if (!isRegisteredListener) return true
-
-          holder.registerUProblem(
-            node,
-            DevKitBundle.message("inspections.listener.implementation.must.not.implement.disposable")
-          )
-          return true
-        }
-      },
-      arrayOf(UClass::class.java)
+    val holder = createProblemsHolder(aClass, manager, isOnTheFly)
+    holder.registerUProblem(
+      aClass,
+      DevKitBundle.message("inspections.listener.implementation.must.not.implement.disposable")
     )
+    return holder.resultsArray
   }
+
+  private fun isRegisteredListener(psiClass: PsiClass): Boolean {
+    val project = psiClass.project
+    return !IdeaPluginRegistrationIndex.processListener(
+      project,
+      psiClass,
+      PluginRelatedLocatorsUtils.getCandidatesScope(project),
+    ) { false }
+  }
+
 }
