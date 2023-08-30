8cf55c35b15deb8778303ec457d805c85a6cb653
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-08-30 21:19:14 +0200

    [maven] plugin resolution logging: info level
    GitOrigin-RevId: 4dbf06b0d17f0339e3fa760bc426565c9bcc5b11

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/project/MavenPluginResolver.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/project/MavenPluginResolver.kt
@@ -41,10 +41,10 @@ class MavenPluginResolver(private val myTree: MavenProjectsTree) {
     try {
       val mavenPluginIdsToResolve = collectMavenPluginIdsToResolve(mavenProjects)
       val mavenPluginIds = mavenPluginIdsToResolve.map { it.first }
-      MavenLog.LOG.warn("maven plugin resolution started: $mavenPluginIds")
+      MavenLog.LOG.info("maven plugin resolution started: $mavenPluginIds")
       val resolutionResults = embedder.resolvePlugins(mavenPluginIdsToResolve, process, syncConsole, console)
       val unresolvedPlugins = resolutionResults.filter { !it.isResolved }.map { it.mavenPluginId }
-      MavenLog.LOG.warn("maven plugin resolution finished, unresolved: $unresolvedPlugins")
+      MavenLog.LOG.info("maven plugin resolution finished, unresolved: $unresolvedPlugins")
       val artifacts = resolutionResults.flatMap { it.artifacts }
       for (artifact in artifacts) {
         val pluginJar = artifact.file.toPath()
