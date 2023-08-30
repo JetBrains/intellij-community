9330900cb528f203b032d668998b1f7cb1b0fc7e
Chris Lemaire <chris.lemaire@jetbrains.com>
2023-08-30 21:24:07 +0200

    [gitlab] Change method for checking GitLab servers to checking projects
    The previously used approach checked for a specific header.
    This new approach checks for a specific page to load without
    problems.
    
    GitOrigin-RevId: ccaaa54be28c1ccc26004a07df302106fd5d4a8b

--- plugins/gitlab/src/org/jetbrains/plugins/gitlab/api/request/GitLabServersApi.kt
+++ plugins/gitlab/src/org/jetbrains/plugins/gitlab/api/request/GitLabServersApi.kt
@@ -1,22 +1,42 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gitlab.api.request
 
+import com.intellij.collaboration.api.HttpStatusErrorException
+import com.intellij.collaboration.api.httpclient.HttpClientUtil.inflateAndReadWithErrorHandlingAndLogging
 import com.intellij.collaboration.api.json.loadJsonValue
 import com.intellij.collaboration.util.resolveRelative
 import org.jetbrains.plugins.gitlab.api.GitLabApi
-import org.jetbrains.plugins.gitlab.api.GitLabServerPath
 import org.jetbrains.plugins.gitlab.api.SinceGitLab
+import com.intellij.openapi.diagnostic.logger
 import org.jetbrains.plugins.gitlab.api.dto.GitLabServerMetadataDTO
 import org.jetbrains.plugins.gitlab.api.dto.GitLabServerVersionDTO
 import java.net.http.HttpResponse
 
-@SinceGitLab("?", note = "It is undocumented since when headers with 'gitlab' were sent back")
+private val LOG = logger<GitLabApi>()
+
+/**
+ * Checks whether the given server looks like and acts like a GitLab server.
+ *
+ * Note that this depends on the /projects API endpoint being unauthenticated.
+ * It has been this since at least 9.0 and it is still. If GitLab changes the
+ * visibility of projects, this function will need to be replaced.
+ *
+ * Maybe we can combine behavioural checks in the future if this turns out to
+ * be insufficient to recognize GitLab server.
+ */
+@SinceGitLab("9.0", note = "Older, but no need to figure out exactly")
 suspend fun GitLabApi.Rest.checkIsGitLabServer(): Boolean {
-  val uri = server.restApiUri.resolveRelative("metadata")
+  val uri = server.restApiUri.resolveRelative("projects?page=1&per_page=1")
   val request = request(uri).GET().build()
-  val response = sendAndAwaitCancellable(request, HttpResponse.BodyHandlers.discarding())
-  return response.headers().map().keys.any {
-    it.contains("gitlab", true)
+  val bodyHandler = inflateAndReadWithErrorHandlingAndLogging(LOG, request) { reader, _ ->
+    reader.readText()
+  }
+  return try {
+    sendAndAwaitCancellable(request, bodyHandler)
+    true
+  }
+  catch (e: HttpStatusErrorException) {
+    false
   }
 }
 
