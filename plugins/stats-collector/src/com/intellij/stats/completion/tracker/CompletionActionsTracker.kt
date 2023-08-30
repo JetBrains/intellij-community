2f6c57677d65747a98c44920a3ec9d82feb783f6
Alexey Kalina <alexey.kalina@jetbrains.com>
2023-08-30 11:02:28 +0200

    [stats-collector] reformat code
    GitOrigin-RevId: b9bf34c9b9037ef21673c3c15fafd2cab994c9fc

--- plugins/stats-collector/src/com/intellij/stats/completion/tracker/CompletionActionsTracker.kt
+++ plugins/stats-collector/src/com/intellij/stats/completion/tracker/CompletionActionsTracker.kt
@@ -15,147 +15,147 @@ class CompletionActionsTracker(private val lookup: LookupImpl,
                                private val lookupStorage: LookupStorage,
                                private val logger: CompletionLogger,
                                private val experimentInfo: ExperimentInfo)
-    : CompletionActionsListener {
+  : CompletionActionsListener {
 
-    private var completionStarted = false
-    private var selectedByDotTyping = false
-    private var prefixLength = 0
+  private var completionStarted = false
+  private var selectedByDotTyping = false
+  private var prefixLength = 0
 
-    private val deferredLog = DeferredLog()
+  private val deferredLog = DeferredLog()
 
-    private fun isCompletionActive(): Boolean {
-        return completionStarted && !lookup.isLookupDisposed
-                || ApplicationManager.getApplication().isUnitTestMode
+  private fun isCompletionActive(): Boolean {
+    return completionStarted && !lookup.isLookupDisposed
+           || ApplicationManager.getApplication().isUnitTestMode
+  }
+
+  override fun lookupCanceled(event: LookupEvent) {
+    if (!completionStarted) return
+
+    val timestamp = System.currentTimeMillis()
+    deferredLog.log()
+    val currentItem = lookup.currentItem
+    val performance = lookupStorage.performanceTracker.measurements()
+
+    if (isSelectedByTyping(currentItem) || selectedByDotTyping) {
+      logger.itemSelectedByTyping(lookup, performance, timestamp)
+    }
+    else {
+      logger.completionCancelled(event.isCanceledExplicitly, performance, timestamp)
+    }
+  }
+
+  override fun currentItemChanged(event: LookupEvent) {
+    if (completionStarted) {
+      return
     }
 
-    override fun lookupCanceled(event: LookupEvent) {
-        if (!completionStarted) return
-
-        val timestamp = System.currentTimeMillis()
-        deferredLog.log()
-        val currentItem = lookup.currentItem
-        val performance = lookupStorage.performanceTracker.measurements()
-
-        if (isSelectedByTyping(currentItem) || selectedByDotTyping) {
-            logger.itemSelectedByTyping(lookup, performance, timestamp)
-        }
-        else {
-            logger.completionCancelled(event.isCanceledExplicitly, performance, timestamp)
-        }
+    val timestamp = System.currentTimeMillis()
+    completionStarted = true
+    prefixLength = lookup.prefix().length
+    deferredLog.defer {
+      logger.completionStarted(lookup, prefixLength, experimentInfo.inExperiment, experimentInfo.version, timestamp)
     }
+  }
 
-    override fun currentItemChanged(event: LookupEvent) {
-        if (completionStarted) {
-            return
-        }
+  override fun itemSelected(event: LookupEvent) {
+    if (!completionStarted) return
 
-        val timestamp = System.currentTimeMillis()
-        completionStarted = true
-        prefixLength = lookup.prefix().length
-        deferredLog.defer {
-            logger.completionStarted(lookup, prefixLength, experimentInfo.inExperiment, experimentInfo.version, timestamp)
-        }
+    val timestamp = System.currentTimeMillis()
+    deferredLog.log()
+    val performance = lookupStorage.performanceTracker.measurements()
+    if (isSelectedByTyping(lookup.currentItem)) {
+      logger.itemSelectedByTyping(lookup, performance, timestamp)
     }
-
-    override fun itemSelected(event: LookupEvent) {
-        if (!completionStarted) return
-
-        val timestamp = System.currentTimeMillis()
-        deferredLog.log()
-        val performance = lookupStorage.performanceTracker.measurements()
-        if (isSelectedByTyping(lookup.currentItem)) {
-            logger.itemSelectedByTyping(lookup, performance, timestamp)
-        }
-        else {
-            logger.itemSelectedCompletionFinished(lookup, event.completionChar, performance, timestamp)
-        }
+    else {
+      logger.itemSelectedCompletionFinished(lookup, event.completionChar, performance, timestamp)
     }
+  }
 
-    override fun beforeDownPressed() {
-        deferredLog.log()
+  override fun beforeDownPressed() {
+    deferredLog.log()
+  }
+
+  override fun downPressed() {
+    if (!isCompletionActive()) return
+
+    val timestamp = System.currentTimeMillis()
+    deferredLog.log()
+    deferredLog.defer {
+      logger.downPressed(lookup, timestamp)
     }
+  }
 
-    override fun downPressed() {
-        if (!isCompletionActive()) return
+  override fun beforeUpPressed() {
+    deferredLog.log()
+  }
 
-        val timestamp = System.currentTimeMillis()
-        deferredLog.log()
-        deferredLog.defer {
-            logger.downPressed(lookup, timestamp)
-        }
+  override fun upPressed() {
+    if (!isCompletionActive()) return
+
+    val timestamp = System.currentTimeMillis()
+    deferredLog.log()
+    deferredLog.defer {
+      logger.upPressed(lookup, timestamp)
     }
+  }
 
-    override fun beforeUpPressed() {
-        deferredLog.log()
+  override fun beforeBackspacePressed() {
+    if (!isCompletionActive()) return
+    deferredLog.log()
+  }
+
+  override fun afterBackspacePressed() {
+    if (!isCompletionActive()) return
+
+    val timestamp = System.currentTimeMillis()
+    prefixLength--
+    deferredLog.log()
+    deferredLog.defer {
+      logger.afterBackspacePressed(lookup, prefixLength, timestamp)
     }
+  }
 
-    override fun upPressed() {
-        if (!isCompletionActive()) return
+  override fun beforeCharTyped(c: Char) {
+    if (!isCompletionActive()) return
 
-        val timestamp = System.currentTimeMillis()
-        deferredLog.log()
-        deferredLog.defer {
-            logger.upPressed(lookup, timestamp)
-        }
+    val timestamp = System.currentTimeMillis()
+    deferredLog.log()
+
+    if (c == '.') {
+      val item = lookup.currentItem
+      if (item == null) {
+        logger.customMessage("Before typed $c lookup.currentItem is null; lookup size: ${lookup.items.size}", timestamp)
+        return
+      }
+      val text = lookup.itemPattern(item)
+      if (item.lookupString == text) {
+        selectedByDotTyping = true
+      }
     }
+  }
 
-    override fun beforeBackspacePressed() {
-        if (!isCompletionActive()) return
-        deferredLog.log()
+
+  override fun afterAppend(c: Char) {
+    if (!isCompletionActive() || !c.isJavaIdentifierPart()) return
+
+    val timestamp = System.currentTimeMillis()
+    prefixLength++
+    deferredLog.log()
+    deferredLog.defer {
+      logger.afterCharTyped(c, lookup, prefixLength, timestamp)
     }
+  }
 
-    override fun afterBackspacePressed() {
-        if (!isCompletionActive()) return
-
-        val timestamp = System.currentTimeMillis()
-        prefixLength--
-        deferredLog.log()
-        deferredLog.defer {
-            logger.afterBackspacePressed(lookup, prefixLength, timestamp)
-        }
+  private fun isSelectedByTyping(item: LookupElement?): Boolean {
+    if (item != null) {
+      val pattern = lookup.itemPattern(item)
+      val lookupString = item.lookupString
+      val cancellationChar = lookup.getUserData(LookupTypedHandler.CANCELLATION_CHAR)
+      if (cancellationChar != null && lookupString.endsWith(cancellationChar)) {
+        return lookupString.dropLast(1) == pattern
+      }
+      return lookupString == pattern
     }
-
-    override fun beforeCharTyped(c: Char) {
-        if (!isCompletionActive()) return
-
-        val timestamp = System.currentTimeMillis()
-        deferredLog.log()
-
-        if (c == '.') {
-            val item = lookup.currentItem
-            if (item == null) {
-                logger.customMessage("Before typed $c lookup.currentItem is null; lookup size: ${lookup.items.size}", timestamp)
-                return
-            }
-            val text = lookup.itemPattern(item)
-            if (item.lookupString == text) {
-                selectedByDotTyping = true
-            }
-        }
-    }
-
-
-    override fun afterAppend(c: Char) {
-        if (!isCompletionActive() || !c.isJavaIdentifierPart()) return
-
-        val timestamp = System.currentTimeMillis()
-        prefixLength++
-        deferredLog.log()
-        deferredLog.defer {
-            logger.afterCharTyped(c, lookup, prefixLength, timestamp)
-        }
-    }
-
-    private fun isSelectedByTyping(item: LookupElement?): Boolean {
-        if (item != null) {
-            val pattern = lookup.itemPattern(item)
-            val lookupString = item.lookupString
-            val cancellationChar = lookup.getUserData(LookupTypedHandler.CANCELLATION_CHAR)
-            if (cancellationChar != null && lookupString.endsWith(cancellationChar)) {
-              return lookupString.dropLast(1) == pattern
-            }
-            return lookupString == pattern
-        }
-        return false
-    }
+    return false
+  }
 }
