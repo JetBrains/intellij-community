b917bf081688d5dec5667f4a49274951685ce3e1
Daniil Kalinin <Daniil.Kalinin@jetbrains.com>
2023-08-31 16:37:54 +0200

    PY-61877, PY-61878 PEP 695 Type Parameter Syntax: Control flow for type parameters and type aliases
    (cherry picked from commit 2db381cbb97891296bae09e48c17b46eefa57a04)
    (cherry picked from commit 573eb92fb4fc33a0d6a3bc3099d53526852755d3)
    
    IJ-CR-115808
    
    GitOrigin-RevId: b5b058998fd351436e440dbecd86241e6ca3ba96

--- python/python-psi-impl/src/com/jetbrains/python/codeInsight/controlflow/PyControlFlowBuilder.java
+++ python/python-psi-impl/src/com/jetbrains/python/codeInsight/controlflow/PyControlFlowBuilder.java
@@ -58,10 +58,12 @@ public class PyControlFlowBuilder extends PyRecursiveElementVisitor {
     return this.myBuilder;
   }
 
+
   @Override
   public void visitPyFunction(final @NotNull PyFunction node) {
     // Create node and stop here
     myBuilder.startNode(node);
+
     visitParameterListExpressions(node.getParameterList());
     visitDecorators(node.getDecoratorList());
     final PyAnnotation annotation = node.getAnnotation();
@@ -106,6 +108,7 @@ public class PyControlFlowBuilder extends PyRecursiveElementVisitor {
   public void visitPyClass(final @NotNull PyClass node) {
     // Create node and stop here
     myBuilder.startNode(node);
+
     for (PsiElement element : node.getSuperClassExpressions()) {
       element.accept(this);
     }
@@ -990,6 +993,20 @@ public class PyControlFlowBuilder extends PyRecursiveElementVisitor {
 
     final PyTargetExpression target = node.getTarget();
     if (target != null) target.accept(this);
+  }
+
+  @Override
+  public void visitPyTypeAliasStatement(@NotNull PyTypeAliasStatement node) {
+    myBuilder.startNode(node);
+
+    PyExpression typeExpression = node.getTypeExpression();
+    if (typeExpression != null) {
+      typeExpression.accept(this);
+    }
+
+    final ReadWriteInstruction instruction = ReadWriteInstruction.write(myBuilder, node, node.getName());
+    myBuilder.addNode(instruction);
+    myBuilder.checkPending(instruction);
   }
 
   private static boolean isCallOfNoReturnFunction(@NotNull PyExpression callee) {
