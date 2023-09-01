fe5e24a5ba51fa4b6d9b28a9051604fcb26119e3
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2023-09-01 18:51:00 +0200

    Mute diagnostic from the compiler introduced in KT-22841
    The fact that this diagnostic is reported even if
    the project using KGP <= 1.9.20 is not all right.
    Reported to relevant developers, but it isn't relevant to our tests anyway
    
    # KT-22841
    
    GitOrigin-RevId: fa87ce470bc166fdb91b91c9b19047e2ea8b76d3

--- plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/regress/ktij22345SyntheticJavaProperties/src/jvmMain/kotlin/jvm.kt
+++ plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/regress/ktij22345SyntheticJavaProperties/src/jvmMain/kotlin/jvm.kt
@@ -1,6 +1,7 @@
 //region Test configuration
 // - hidden: line markers
 //endregion
+@file:Suppress("ACTUAL_CLASSIFIER_MUST_HAVE_THE_SAME_SUPERTYPES_AS_NON_FINAL_EXPECT_CLASSIFIER", "ACTUAL_CLASSIFIER_MUST_HAVE_THE_SAME_MEMBERS_AS_NON_FINAL_EXPECT_CLASSIFIER")
 actual open class MyFile1 actual constructor(userPath: String): MyJavaFile(userPath)
 actual typealias MyFile2 = java.io.File
 
