1e1f74928cf8620e9fff86bd11e01cd2365a63f0
Jinseong Jeon <jsjeon@google.com>
2023-09-02 12:32:07 -0700

    FIR/UAST: split (fake) Uast*Parameter/Base into their own files
    GitOrigin-RevId: f0730e2cc224f3595384c5afef9df4b6deaf1ceb

--- plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/psi/UastKotlinPsiParameter.kt
+++ plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/psi/UastKotlinPsiParameter.kt
@@ -1,19 +1,16 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.uast.kotlin.psi
 
 import com.intellij.lang.Language
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiParameter
 import com.intellij.psi.PsiType
-import com.intellij.psi.impl.light.LightParameter
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.kotlin.analysis.api.types.KtTypeNullability
 import org.jetbrains.kotlin.asJava.elements.KtLightAnnotationForSourceEntry
 import org.jetbrains.kotlin.idea.KotlinLanguage
-import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtParameter
 import org.jetbrains.kotlin.utils.SmartList
@@ -25,7 +22,7 @@ import org.jetbrains.uast.kotlin.BaseKotlinUastResolveProviderService
 import org.jetbrains.uast.kotlin.lz
 
 @ApiStatus.Internal
-class UastKotlinPsiParameter(
+class UastKotlinPsiParameter internal constructor(
     private val baseResolveProviderService: BaseKotlinUastResolveProviderService,
     name: String,
     type: PsiType,
@@ -90,28 +87,4 @@ class UastKotlinPsiParameter(
 
     val ktParameter: KtParameter get() = ktOrigin
 
-}
-
-@ApiStatus.Internal
-open class UastKotlinPsiParameterBase<T : KtElement>(
-    name: String,
-    type: PsiType,
-    private val parent: PsiElement,
-    val ktOrigin: T,
-    language: Language = ktOrigin.language,
-    isVarArgs: Boolean = false,
-    val ktDefaultValue: KtExpression? = null,
-) : LightParameter(name, type, parent, language, isVarArgs) {
-
-    override fun getParent(): PsiElement = parent
-
-    override fun getContainingFile(): PsiFile? = ktOrigin.containingFile
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || other::class.java != this::class.java) return false
-        return ktOrigin == (other as? UastKotlinPsiParameterBase<*>)?.ktOrigin
-    }
-
-    override fun hashCode(): Int = ktOrigin.hashCode()
 }
--- plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/psi/UastDescriptorLightParameter.kt
+++ plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/psi/UastDescriptorLightParameter.kt
@@ -1,24 +1,10 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.uast.kotlin.psi
 
 import com.intellij.lang.Language
-import com.intellij.psi.*
-import com.intellij.psi.impl.light.LightParameter
-import org.jetbrains.kotlin.descriptors.ParameterDescriptor
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiType
 import org.jetbrains.kotlin.descriptors.ValueParameterDescriptor
-import org.jetbrains.kotlin.descriptors.VariableDescriptor
-import org.jetbrains.kotlin.idea.KotlinLanguage
-import org.jetbrains.kotlin.psi.KtElement
-import org.jetbrains.kotlin.psi.KtExpression
-import org.jetbrains.kotlin.psi.KtParameter
-import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.calls.components.isVararg
-import org.jetbrains.uast.UDeclaration
-import org.jetbrains.uast.UElement
-import org.jetbrains.uast.UastErrorType
-import org.jetbrains.uast.getParentOfType
-import org.jetbrains.uast.kotlin.analyze
-import org.jetbrains.uast.kotlin.toPsiType
 
 internal class UastDescriptorLightParameter(
     name: String,
@@ -27,24 +13,3 @@ internal class UastDescriptorLightParameter(
     ktParameter: ValueParameterDescriptor,
     language: Language = parent.language,
 ) : UastDescriptorLightParameterBase<ValueParameterDescriptor>(name, type, parent, ktParameter, language)
-
-internal open class UastDescriptorLightParameterBase<T : ParameterDescriptor>(
-  name: String,
-  type: PsiType,
-  private val parent: PsiElement,
-  private val ktOrigin: T,
-  language: Language = parent.language,
-) : LightParameter(name, type, parent, language, ktOrigin.isVararg) {
-
-    override fun getParent(): PsiElement = parent
-
-    override fun getContainingFile(): PsiFile? = parent.containingFile
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || other::class.java != this::class.java) return false
-        return ktOrigin == (other as? UastDescriptorLightParameterBase<*>)?.ktOrigin
-    }
-
-    override fun hashCode(): Int = ktOrigin.hashCode()
-}
