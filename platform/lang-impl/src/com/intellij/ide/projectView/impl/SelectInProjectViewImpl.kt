401686827e7b8d313d3e57054bbad146183a0909
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2023-09-03 11:43:48 +0300

    IDEA-331018 Implement select in project view logging
    Log every step of select in logic starting
    from SelectInProjectViewImpl.selectInCurrentTarget,
    which is the main entry point into the whole
    "scroll from source" business.
    
    Implementation note: everything is logged using
    the same logger to avoid complicated setup on the user
    side, as the logic is spread out through a lot of classes.
    
    The only thing that's not covered by this logging
    is visitors, those are used in a lot of places and
    logging a lot of stuff there would spam a lot of unrelated
    messages.
    
    GitOrigin-RevId: 0daf1d3f504f5df99b41771e719a21edfba21cef

--- platform/lang-impl/src/com/intellij/ide/projectView/impl/SelectInProjectViewImpl.kt
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/SelectInProjectViewImpl.kt
@@ -34,6 +34,9 @@ internal class SelectInProjectViewImpl(
 
   fun selectInCurrentTarget(fileEditor: FileEditor?, invokedManually: Boolean) {
     coroutineScope.launch(CoroutineName("ProjectView.selectInCurrentTarget(invokedManually=$invokedManually,fileEditor=$fileEditor")) {
+      if (LOG.isDebugEnabled) {
+        LOG.debug("selectInCurrentTarget: fileEditor=$fileEditor, invokedManually=$invokedManually")
+      }
       val editorsToCheck = if (fileEditor == null) allEditors() else listOf(fileEditor)
       selectInCurrentTarget(invokedManually, editorsToCheck)
     }
@@ -46,12 +49,22 @@ internal class SelectInProjectViewImpl(
         AdvancedSettings.getBoolean("project.view.do.not.autoscroll.to.libraries") &&
         readAction { fileEditor.file?.let { file ->ProjectFileIndex.getInstance(project).isInLibrary(file) } == true }
       ) {
+        if (LOG.isDebugEnabled) {
+          LOG.debug("Skipping $fileEditor because the file is in a library and autoscroll to libraries is off")
+        }
         continue
       }
       val psiFilePointer = getPsiFilePointer(fileEditor)
       if (psiFilePointer != null) {
+        if (LOG.isDebugEnabled) {
+          LOG.debug("Trying to select using $fileEditor with psiFilePointer=$psiFilePointer")
+        }
         withContext(Dispatchers.EDT) {
-          createSelectInContext(psiFilePointer, fileEditor).selectInCurrentTarget(requestFocus = invokedManually)
+          val selectInContext = createSelectInContext(psiFilePointer, fileEditor)
+          if (LOG.isDebugEnabled) {
+            LOG.debug("Created select-in context and delegating to it: $selectInContext")
+          }
+          selectInContext.selectInCurrentTarget(requestFocus = invokedManually)
         }
         break
       }
@@ -107,20 +120,48 @@ internal class SelectInProjectViewImpl(
     result: ActionCallback?,
   ) {
     coroutineScope.launch(CoroutineName("ProjectView.ensureSelected(pane=$paneId,virtualFile=$virtualFile,focus=$requestFocus))")) {
+      if (LOG.isDebugEnabled) {
+        LOG.debug("ensureSelected: " +
+                  "paneId=$paneId, " +
+                  "virtualFile=$virtualFile, " +
+                  "elementSupplier=$elementSupplier, " +
+                  "requestFocus=$requestFocus, " +
+                  "allowSubIdChange=$allowSubIdChange, " +
+                  "result=$result"
+        )
+      }
       val projectView = project.serviceOrNull<ProjectView>() as ProjectViewImpl?
       if (projectView == null) {
+        LOG.debug("Not selecting anything because there is no project view")
         result?.setRejected()
         return@launch
       }
       val pane = if (requestFocus) null else projectView.getProjectViewPaneById(paneId)
       val target = if (pane == null) null else projectView.getProjectViewSelectInTarget(pane)
       if (!allowSubIdChange) {
+        if (LOG.isDebugEnabled) {
+          LOG.debug("SubId change not allowed, checking: " +
+                    "pane=$pane, " +
+                    "target=$target, " +
+                    "virtualFile=$virtualFile, " +
+                    "and isSubIdSelectable"
+          )
+        }
         val isSelectableInCurrentSubId =
-          pane != null &&
-          target != null &&
-          virtualFile != null &&
-          readAction {
-            target.isSubIdSelectable(pane.subId, FileSelectInContext(project, virtualFile, null))
+          if (pane == null || target == null || virtualFile == null) {
+            if (LOG.isDebugEnabled) {
+              LOG.debug("File $virtualFile is NOT selectable because there's not enough non-null parameters to go on, not selecting anything")
+            }
+            false
+          }
+          else {
+            val isSubIdSelectable = readAction {
+              target.isSubIdSelectable(pane.subId, FileSelectInContext(project, virtualFile, null))
+            }
+            if (LOG.isDebugEnabled && !isSubIdSelectable) {
+              LOG.debug("File $virtualFile is NOT selectable in $pane with target $target and changing subId is not allowed, not selecting anything")
+            }
+            isSubIdSelectable
           }
         if (!isSelectableInCurrentSubId) {
           return@launch
@@ -129,22 +170,39 @@ internal class SelectInProjectViewImpl(
       val visibleAndSelectedUserObject = withContext(Dispatchers.EDT) {
         pane?.visibleAndSelectedUserObject
       }
+      if (LOG.isDebugEnabled && !requestFocus) { // if requestFocus, pane is always null
+        LOG.debug("Currently visible and selected node is $visibleAndSelectedUserObject")
+      }
+
       data class SelectionContext(
         val isAlreadyVisibleAndSelected: Boolean,
         val virtualFile: VirtualFile?,
       )
       val context = readAction {
-        val elementToSelect = elementSupplier.get() ?: virtualFile ?: return@readAction null
+        val suppliedElement = elementSupplier.get()
+        val elementToSelect = suppliedElement ?: virtualFile ?: return@readAction null
+        if (LOG.isDebugEnabled) {
+          LOG.debug("Element to select is $elementToSelect (from ${if (suppliedElement == null) "virtual file" else "supplier"})")
+        }
+        val isAlreadyVisibleAndSelected = visibleAndSelectedUserObject != null && visibleAndSelectedUserObject.canRepresent(elementToSelect)
+        if (LOG.isDebugEnabled && isAlreadyVisibleAndSelected) {
+          LOG.debug("This element is already visible and selected")
+        }
         SelectionContext(
-visibleAndSelectedUserObject != null && visibleAndSelectedUserObject.canRepresent(elementToSelect),
-virtualFile ?: (elementToSelect as? PsiElement)?.virtualFile
+          isAlreadyVisibleAndSelected,
+          virtualFile ?: (elementToSelect as? PsiElement)?.virtualFile
         )
       }
+      if (LOG.isDebugEnabled) {
+        LOG.debug("The selection context is $context")
+      }
       if (context == null || context.isAlreadyVisibleAndSelected || context.virtualFile == null) {
+        LOG.debug("Nothing to do")
         result?.setDone()
         return@launch
       }
       withContext(Dispatchers.EDT) {
+        LOG.debug("Delegating back to the project view")
         projectView.select(elementSupplier, context.virtualFile, requestFocus, result)
       }
     }
@@ -163,8 +221,16 @@ private open class SimpleSelectInContext(
   }
 
   open suspend fun selectInCurrentTarget(requestFocus: Boolean) {
-    val currentTarget = (project.serviceOrNull<ProjectView>() as ProjectViewImpl?)?.currentSelectInTarget ?: return
+    val currentTarget = (project.serviceOrNull<ProjectView>() as ProjectViewImpl?)?.currentSelectInTarget
+    if (LOG.isDebugEnabled) {
+      LOG.debug("The current target is $currentTarget")
+    }
+    if (currentTarget == null) return
     currentTarget.selectIn(this, requestFocus)
+  }
+
+  override fun toString(): String {
+    return "SimpleSelectInContext(project=$project) ${super.toString()}"
   }
 
 }
@@ -181,11 +247,21 @@ private class EditorSelectInContext(
     withContext(Dispatchers.EDT) {
       while (true) {
         if (editor.isDisposed) {
+          LOG.debug("Not selecting anything because the editor is disposed")
           break
         }
         val offset = editor.caretModel.offset
-        constrainedReadAction(ReadConstraint.withDocumentsCommitted(project)) {
+        if (LOG.isDebugEnabled) {
+          LOG.debug("Looking for the element at offset $offset")
+        }
+        val element = constrainedReadAction(ReadConstraint.withDocumentsCommitted(project)) {
           psiFile?.findElementAt(offset)
+        }
+        // No, the fact that the element is only used for logging isn't a bug:
+        // the whole point of the read action above is to ensure the document is committed
+        // and parsed before selecting the current element, otherwise it may be outdated.
+        if (LOG.isDebugEnabled) {
+          LOG.debug("The element is $element")
         }
         if (editor.caretModel.offset == offset && PsiDocumentManager.getInstance(project).isCommitted(editor.document)) {
           super.selectInCurrentTarget(requestFocus)
@@ -206,6 +282,10 @@ private class EditorSelectInContext(
     }
     return file
   }
+
+  override fun toString(): String {
+    return "EditorSelectInContext(editor=$editor) ${super.toString()}"
+  }
 }
 
 private fun <T : FileEditor> T.nullIfInvalid(): T? = if (isValid) this else null
@@ -213,4 +293,4 @@ private fun <T : Editor> T.nullIfDisposed(): T? = if (isDisposed) null else this
 private fun <T : VirtualFile> T.nullIfInvalid(): T? = if (isValid) this else null
 private val PsiElement.virtualFile: VirtualFile? get() = PsiUtilCore.getVirtualFile(this)
 
-private val LOG = logger<SelectInProjectViewImpl>()
+internal val LOG = logger<SelectInProjectViewImpl>()
