16db548efc4a904f70e00f94dc2e5c4ca4e82c8f
Nikita Barkov <nikita.barkov@jetbrains.com>
2023-09-04 20:40:36 +0200

    [perform_tests]AT-526 Finished test
    GitOrigin-RevId: 4bd463a21fe3bfbfa6442fce057dd7ef35b003bd

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/performancePlugin/ExecuteMavenGoalCommand.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/performancePlugin/ExecuteMavenGoalCommand.kt
@@ -1,7 +1,10 @@
 package org.jetbrains.idea.maven.performancePlugin
 
+import com.intellij.execution.ExecutionListener
+import com.intellij.execution.ExecutionManager
 import com.intellij.execution.process.ProcessAdapter
 import com.intellij.execution.process.ProcessEvent
+import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ProgramRunner
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
@@ -20,19 +23,31 @@ import org.jetbrains.idea.maven.project.MavenProjectsManager
  * Example: %executeMavenGoal mainModule package
  */
 class ExecuteMavenGoalCommand(text: String, line: Int) : AbstractCommand(text, line) {
+  companion object {
+    const val PREFIX = "%executeMavenGoal"
+  }
+
   override fun _execute(context: PlaybackContext): Promise<Any?> {
     val promise = AsyncPromise<Any?>()
     val split = extractCommandArgument(PREFIX).split(" ")
     val moduleName = split[0]
     val goal = split[1]
     val project = context.getProject()
-    perform(project, moduleName, goal)
-    {
-      it.processHandler?.addProcessListener(object : ProcessAdapter() {
-        override fun processTerminated(event: ProcessEvent) {
-          promise.setResult(null)
+    project.messageBus.connect().subscribe(ExecutionManager.EXECUTION_TOPIC, object : ExecutionListener {
+      override fun processNotStarted(executorId: String, env: ExecutionEnvironment, cause: Throwable?) {
+        if (cause != null) {
+          promise.setError(cause)
         }
-      })
+      }
+    })
+
+    perform(project, moduleName, goal) { descriptor ->
+      descriptor?.processHandler?.addProcessListener(
+        object : ProcessAdapter() {
+          override fun processTerminated(event: ProcessEvent) {
+            promise.setResult(null)
+          }
+        })
     }
     return promise
   }
@@ -55,9 +70,5 @@ class ExecuteMavenGoalCommand(text: String, line: Int) : AbstractCommand(text, l
                                          explicitProfiles.disabledProfiles)
       MavenRunConfigurationType.runConfiguration(project, params, callback)
     }
-  }
-
-  companion object {
-    const val PREFIX = "%executeMavenGoal"
   }
 }
