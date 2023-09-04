e46b342076365bf58e8674dcbab7457892b65618
Jacob Logsdon <jlogs@google.com>
2023-09-04 08:14:01 +0200

    IDEA-330383 fix before listener in RegistryValue
    Close https://github.com/JetBrains/intellij-community/pull/2563
    
    GitOrigin-RevId: fb6870432593141083c59714504a7d9567f0d4f7
    
    (cherry picked from commit 3a08ca3f08a6bed769f554b6edb27918cd1def62)
    
    Change-Id: I3a08ca3f08a6bed769f554b6edb27918cd1def62

--- platform/util/testSrc/com/intellij/openapi/util/registry/RegistryTest.java
+++ platform/util/testSrc/com/intellij/openapi/util/registry/RegistryTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.util.registry;
 
 import com.intellij.idea.TestFor;
@@ -20,7 +6,6 @@ import com.intellij.openapi.util.Pair;
 import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -180,6 +165,34 @@ public class RegistryTest {
     }}), null);
     assertEquals(firstKeyChangedVal, Registry.get(firstKey).asString());
     assertEquals(secondKeyInitValue, Registry.get(secondKey).asString());
+  }
+
+  @Test
+  public void dontPersistDefaultValue(){
+    int originalValue = Registry.intValue(INT_KEY_REQUIRE_RESTART);
+    Registry.get(INT_KEY_REQUIRE_RESTART).setValue("2222");
+    assertTrue(JDOMUtil.writeElement(Registry.getInstance().getState()).contains(
+      "<entry key=\"%s\" value=\"2222\"".formatted(INT_KEY_REQUIRE_RESTART))
+    );
+    Registry.get(INT_KEY_REQUIRE_RESTART).setValue(String.valueOf(originalValue));
+    assertFalse(JDOMUtil.writeElement(Registry.getInstance().getState()).contains(
+      INT_KEY_REQUIRE_RESTART)
+    );
+  }
+
+  @Test
+  public void beforeListenerDoesNotChangeValueWhenSetting() {
+    String registryValue = "testBoolean";
+    RegistryValue regValue = new RegistryValue(Registry.getInstance(), registryValue, null);
+    regValue.setValue(false);
+    Registry.setValueChangeListener(new RegistryValueListener() {
+      @Override
+      public void beforeValueChanged(@NotNull RegistryValue value) {
+        regValue.asBoolean();
+      }
+    });
+    regValue.setValue(true);
+    assertTrue(regValue.asBoolean());
   }
 
 
