9c8ff435723a12fe652832888fde55f332c9ece6
Leonid Shalupov <leonid@shalupov.com>
2023-09-04 15:41:02 +0200

    xml: convert tests from groovy to java
    GitOrigin-RevId: faee0c847619c89f8ec21aa32d0173ca6151af94

--- xml/tests/src/com/intellij/codeInsight/XmlXIncludeTest.java
+++ xml/tests/src/com/intellij/codeInsight/XmlXIncludeTest.java
@@ -1,99 +1,104 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.codeInsight
+package com.intellij.codeInsight;
 
-import com.intellij.codeInsight.daemon.impl.analysis.XmlPathReferenceInspection
-import com.intellij.openapi.application.WriteAction
-import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.util.text.StringUtil
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiDocumentManager
-import com.intellij.psi.xml.XmlFile
-import com.intellij.psi.xml.XmlTag
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
+import com.intellij.codeInsight.daemon.impl.analysis.XmlPathReferenceInspection;
+import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.xml.XmlFile;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import junit.framework.TestCase;
 
-class XmlXIncludeTest extends LightJavaCodeInsightFixtureTestCase {
-  private static final String BASE_PATH = "/psi/xi"
+import java.util.Arrays;
+import java.util.List;
 
-  void testBasicXi() throws Exception {
-    configure()
-    assertSubtagNames("before", "include1", "after")
+public class XmlXIncludeTest extends LightJavaCodeInsightFixtureTestCase {
+  public void testBasicXi() {
+    configure();
+    assertSubtagNames("before", "include1", "after");
   }
 
-  void testXpointerXi1() throws Exception {
-    configure()
-    assertSubtagNames("before", "foo", "bar", "after")
+  public void testXpointerXi1() {
+    configure();
+    assertSubtagNames("before", "foo", "bar", "after");
   }
 
-  void testXmlBase() throws Exception {
-    myFixture.copyDirectoryToProject("xmlBase", "xmlBase")
-    myFixture.enableInspections(XmlPathReferenceInspection.class)
-    myFixture.testHighlighting("xmlBase/XmlBase.xml")
+  public void testXmlBase() {
+    myFixture.copyDirectoryToProject("xmlBase", "xmlBase");
+    myFixture.enableInspections(XmlPathReferenceInspection.class);
+    myFixture.testHighlighting("xmlBase/XmlBase.xml");
   }
 
   @Override
   protected String getBasePath() {
-    return "/xml/tests/testData/" + BASE_PATH
+    return "/xml/tests/testData/" + BASE_PATH;
   }
 
   private void assertSubtagNames(String... expectedNames) {
-    final XmlTag tag = ((XmlFile)myFixture.file).document.rootTag
-    final List<String> names = tag.getSubTags().collect { it.name }
+    final XmlTag tag = ((XmlFile)myFixture.getFile()).getDocument().getRootTag();
+    final List<String> names = Arrays.stream(tag.getSubTags()).map(t -> t.getName()).toList();
 
-    assertEquals(
-      "subtags are different",
-      StringUtil.join(expectedNames, "\n"), StringUtil.join(names, "\n")
-    )
+    TestCase.assertEquals("subtags are different", StringUtil.join(expectedNames, "\n"), StringUtil.join(names, "\n"));
   }
 
-  private void configure() throws Exception {
-    myFixture.configureByFiles(getName() + ".xml", "/include1.xml")
+  private void configure() {
+    myFixture.configureByFiles(getName() + ".xml", "/include1.xml");
   }
 
-  void testModifyingIncludedFile() {
-    myFixture.configureByText 'a.xml', '''
-<root xmlns:xi="http://www.w3.org/2001/XInclude">
-  <before/>
-  <xi:include href="include1.xml" xpointer="xpointer(/include1/*)"/>
-  <after/>
-</root>
-'''
-    def inc1 = myFixture.copyFileToProject("include1.xml")
-    assertSubtagNames("before", "foo", "bar", "after")
+  public void testModifyingIncludedFile() {
+    myFixture.configureByText("a.xml", """
 
-    changeText inc1, '<include1><a/></include1>'
+      <root xmlns:xi="http://www.w3.org/2001/XInclude">
+        <before/>
+        <xi:include href="include1.xml" xpointer="xpointer(/include1/*)"/>
+        <after/>
+      </root>
+      """);
+    VirtualFile inc1 = myFixture.copyFileToProject("include1.xml");
+    assertSubtagNames("before", "foo", "bar", "after");
 
-    assertSubtagNames("before", "a", "after")
+    changeText(inc1, "<include1><a/></include1>");
+
+    assertSubtagNames("before", "a", "after");
   }
 
-  void testModifyingTransitivelyIncludedFile() {
-    myFixture.configureByText 'a.xml', '''
-<root xmlns:xi="http://www.w3.org/2001/XInclude">
-  <before/>
-  <xi:include href="include1.xml" xpointer="xpointer(/include1/*)"/>
-  <after/>
-</root>
-'''
+  public void testModifyingTransitivelyIncludedFile() {
+    myFixture.configureByText("a.xml", """
+
+      <root xmlns:xi="http://www.w3.org/2001/XInclude">
+        <before/>
+        <xi:include href="include1.xml" xpointer="xpointer(/include1/*)"/>
+        <after/>
+      </root>
+      """);
     myFixture.addFileToProject("include1.xml", """
-<include1 xmlns:xi="http://www.w3.org/2001/XInclude">
-  <foo attr1="val1"> <fooChild/> </foo>
-  <bar/>
-  <xi:include href="include2.xml""/>
-</include1>
-""")
-    def inc2 = myFixture.addFileToProject("include2.xml", '<a/>')
 
-    assertSubtagNames("before", "foo", "bar", "a", "after")
+      <include1 xmlns:xi="http://www.w3.org/2001/XInclude">
+        <foo attr1="val1"> <fooChild/> </foo>
+        <bar/>
+        <xi:include href="include2.xml""/>
+      </include1>
+      """);
+    PsiFile inc2 = myFixture.addFileToProject("include2.xml", "<a/>");
 
-    changeText inc2.virtualFile, '<b/>'
+    assertSubtagNames("before", "foo", "bar", "a", "after");
 
-    assertSubtagNames("before", "foo", "bar", "b", "after")
+    changeText(inc2.getVirtualFile(), "<b/>");
 
+    assertSubtagNames("before", "foo", "bar", "b", "after");
   }
 
-  private changeText(VirtualFile inc1, String s) {
-    WriteAction.run {
-      FileDocumentManager.instance.getDocument(inc1).setText s
-      PsiDocumentManager.getInstance(project).commitAllDocuments()
-    }
+  private void changeText(final VirtualFile inc1, final String s) {
+    WriteAction.run(
+      () -> {
+        FileDocumentManager.getInstance().getDocument(inc1).setText(s);
+        PsiDocumentManager.getInstance(getProject()).commitAllDocuments();
+      }
+    );
   }
+
+  private static final String BASE_PATH = "/psi/xi";
 }
--- xml/tests/src/com/intellij/xml/arrangement/XmlRearrangerTest.java
+++ xml/tests/src/com/intellij/xml/arrangement/XmlRearrangerTest.java
@@ -1,240 +1,252 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.intellij.xml.arrangement
+package com.intellij.xml.arrangement;
 
-import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.lang.xml.XMLLanguage
-import com.intellij.psi.codeStyle.arrangement.AbstractRearrangerTest
-import com.intellij.psi.codeStyle.arrangement.match.StdArrangementEntryMatcher
-import com.intellij.psi.codeStyle.arrangement.match.StdArrangementMatchRule
-import com.intellij.psi.codeStyle.arrangement.model.ArrangementAtomMatchCondition
-import com.intellij.psi.codeStyle.arrangement.std.StdArrangementTokens
-import org.jetbrains.annotations.NotNull
+import com.intellij.ide.highlighter.XmlFileType;
+import com.intellij.lang.xml.XMLLanguage;
+import com.intellij.psi.codeStyle.arrangement.AbstractRearrangerTest;
+import com.intellij.psi.codeStyle.arrangement.match.StdArrangementEntryMatcher;
+import com.intellij.psi.codeStyle.arrangement.match.StdArrangementMatchRule;
+import com.intellij.psi.codeStyle.arrangement.model.ArrangementAtomMatchCondition;
+import com.intellij.psi.codeStyle.arrangement.std.StdArrangementTokens;
+import org.jetbrains.annotations.NotNull;
 
-import static com.intellij.psi.codeStyle.arrangement.std.StdArrangementTokens.Order.BY_NAME
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 
-class XmlRearrangerTest extends AbstractRearrangerTest {
-  XmlRearrangerTest() {
-    fileType = XmlFileType.INSTANCE
-    language = XMLLanguage.INSTANCE
+import static com.intellij.psi.codeStyle.arrangement.std.StdArrangementTokens.Order.BY_NAME;
+
+public class XmlRearrangerTest extends AbstractRearrangerTest {
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    fileType = XmlFileType.INSTANCE;
+    language = XMLLanguage.INSTANCE;
   }
 
-  void testAttributeSorting1() {
-    doTest(
-      initial: '''<root xmlns:ns="http://ns.com" attr2="value2" attr1="value1" attr3="value3"/>''',
-      expected: '''<root attr1="value1" attr2="value2" attr3="value3" xmlns:ns="http://ns.com"/>''',
-      rules: [ruleWithOrder(BY_NAME, nameRule(".*"))]
-    )
+  public void testAttributeSorting1() {
+    LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(3);
+    map.put("initial", """
+      <root xmlns:ns="http://ns.com" attr2="value2" attr1="value1" attr3="value3"/>""");
+    map.put("expected", """
+      <root attr1="value1" attr2="value2" attr3="value3" xmlns:ns="http://ns.com"/>""");
+    map.put("rules", new ArrayList<>(
+      Arrays.asList(ruleWithOrder(BY_NAME, nameRule(".*")))));
+    doTest(map);
   }
 
-  void testAttributeSorting2() {
-    doTest(
-      initial: '''<root attr3="value3" attr2="value2" attr1="value1" xmlns:ns="http://ns.com"/>''',
-      expected: '''<root attr1="value1" attr2="value2" attr3="value3" xmlns:ns="http://ns.com"/>''',
-      rules: [ruleWithOrder(BY_NAME, nameRule(".*"))]
-    )
+  public void testAttributeSorting2() {
+    LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(3);
+    map.put("initial", """
+      <root attr3="value3" attr2="value2" attr1="value1" xmlns:ns="http://ns.com"/>""");
+    map.put("expected", """
+      <root attr1="value1" attr2="value2" attr3="value3" xmlns:ns="http://ns.com"/>""");
+    map.put("rules", new ArrayList<>(
+      Arrays.asList(ruleWithOrder(BY_NAME, nameRule(".*")))));
+    doTest(map);
   }
 
-  void testAttributeSorting3() {
-    doTest(
-      initial: '''\
-<root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" attr2="value2" attr1="value1">
-  <tag1 attr2="value2" attr1="value1">
-      <tag2 attr1="value1" attr2="value2"/>
-      <tag3 attr2="value2" attr1="value1" xmlns:ns3="http://ns.com"/>
-  </tag1>
-</root>
-''',
-      expected: '''\
-<root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" attr1="value1" attr2="value2">
-  <tag1 attr1="value1" attr2="value2">
-      <tag2 attr1="value1" attr2="value2"/>
-      <tag3 xmlns:ns3="http://ns.com" attr1="value1" attr2="value2"/>
-  </tag1>
-</root>
-''',
-      rules: [ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, nameRule(".*"))]
-    )
+  public void testAttributeSorting3() {
+    LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(3);
+    map.put("initial", """
+      <root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" attr2="value2" attr1="value1">
+        <tag1 attr2="value2" attr1="value1">
+            <tag2 attr1="value1" attr2="value2"/>
+            <tag3 attr2="value2" attr1="value1" xmlns:ns3="http://ns.com"/>
+        </tag1>
+      </root>
+      """);
+    map.put("expected", """
+      <root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" attr1="value1" attr2="value2">
+        <tag1 attr1="value1" attr2="value2">
+            <tag2 attr1="value1" attr2="value2"/>
+            <tag3 xmlns:ns3="http://ns.com" attr1="value1" attr2="value2"/>
+        </tag1>
+      </root>
+      """);
+    map.put("rules", new ArrayList<>(
+      Arrays.asList(ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, nameRule(".*")))));
+    doTest(map);
   }
 
-  void testAttributeSorting4() throws Exception {
-    doTest(
-      initial: '''<root attr3="value3" attr2="value2" attr1="value1" xmlns:ns="http://ns.com"/>''',
-      expected: '''<root xmlns:ns="http://ns.com" attr1="value1" attr2="value2" attr3="value3"/>''',
-      rules: [ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, nameRule(".*"))]
-    )
+  public void testAttributeSorting4() {
+    LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(3);
+    map.put("initial", """
+      <root attr3="value3" attr2="value2" attr1="value1" xmlns:ns="http://ns.com"/>""");
+    map.put("expected", """
+      <root xmlns:ns="http://ns.com" attr1="value1" attr2="value2" attr3="value3"/>""");
+    map.put("rules", new ArrayList<>(
+      Arrays.asList(ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, nameRule(".*")))));
+    doTest(map);
   }
 
-  void testAttributeSortingByNamespace1() throws Exception {
-    doTest(
-      initial: '''\
-<root attr1="value1" ns1:attr2="value2" xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1">
-  <tag1 ns2:attr2="value2" attr2="value2" attr1="value1" ns2:attr1="value1" ns1:attr2="value2" ns1:attr1="value1">
-  </tag1>
-</root>
-''',
-      expected: '''\
-<root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" attr1="value1" ns1:attr1="value1" ns1:attr2="value2">
-  <tag1 attr1="value1" attr2="value2" ns1:attr1="value1" ns1:attr2="value2" ns2:attr1="value1" ns2:attr2="value2">
-  </tag1>
-</root>
-''',
-      rules: [ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, namespaceRule(".*"))]
-    )
+  public void testAttributeSortingByNamespace1() {
+    LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(3);
+    map.put("initial", """
+      <root attr1="value1" ns1:attr2="value2" xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1">
+        <tag1 ns2:attr2="value2" attr2="value2" attr1="value1" ns2:attr1="value1" ns1:attr2="value2" ns1:attr1="value1">
+        </tag1>
+      </root>
+      """);
+    map.put("expected", """
+      <root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" attr1="value1" ns1:attr1="value1" ns1:attr2="value2">
+        <tag1 attr1="value1" attr2="value2" ns1:attr1="value1" ns1:attr2="value2" ns2:attr1="value1" ns2:attr2="value2">
+        </tag1>
+      </root>
+      """);
+    map.put("rules", new ArrayList<>(
+      Arrays.asList(ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, namespaceRule(".*")))));
+    doTest(map);
   }
 
-  void testAttributeSortingByNamespace2() throws Exception {
-    doTest(
-      initial: '''\
-<root attr1="value1" ns1:attr2="value2" xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1">
-  <tag1 ns2:attr2="value2" attr2="value2" attr1="value1" ns2:attr1="value1" ns1:attr2="value2" ns1:attr1="value1">
-  </tag1>
-</root>
-''',
-      expected: '''\
-<root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1" ns1:attr2="value2" attr1="value1">
-  <tag1 ns1:attr1="value1" ns1:attr2="value2" ns2:attr1="value1" ns2:attr2="value2" attr2="value2" attr1="value1">
-  </tag1>
-</root>
-''',
-      rules: [ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, namespaceRule("http://ns.*"))]
-    )
+  public void testAttributeSortingByNamespace2() {
+    LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(3);
+    map.put("initial", """
+      <root attr1="value1" ns1:attr2="value2" xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1">
+        <tag1 ns2:attr2="value2" attr2="value2" attr1="value1" ns2:attr1="value1" ns1:attr2="value2" ns1:attr1="value1">
+        </tag1>
+      </root>
+      """);
+    map.put("expected", """
+      <root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1" ns1:attr2="value2" attr1="value1">
+        <tag1 ns1:attr1="value1" ns1:attr2="value2" ns2:attr1="value1" ns2:attr2="value2" attr2="value2" attr1="value1">
+        </tag1>
+      </root>
+      """);
+    map.put("rules", new ArrayList<>(
+      Arrays.asList(ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, namespaceRule("http://ns.*")))));
+    doTest(map);
   }
 
-  void testAttributeSortingByNamespace3() throws Exception {
-    doTest(
-      initial: '''\
-<root attr1="value1" ns1:attr2="value2" xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1">
-  <tag1 ns2:attr2="value2" attr2="value2" attr1="value1" ns2:attr1="value1" ns1:attr2="value2" ns1:attr1="value1">
-  </tag1>
-</root>
-''',
-      expected: '''\
-<root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1" ns1:attr2="value2" attr1="value1">
-  <tag1 ns1:attr1="value1" ns1:attr2="value2" ns2:attr1="value1" ns2:attr2="value2" attr1="value1" attr2="value2">
-  </tag1>
-</root>
-''',
-      rules: [ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, namespaceRule("http://ns.*")),
-        ruleWithOrder(BY_NAME, nameRule(".*"))]
-    )
+  public void testAttributeSortingByNamespace3() {
+    LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(3);
+    map.put("initial", """
+      <root attr1="value1" ns1:attr2="value2" xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1">
+        <tag1 ns2:attr2="value2" attr2="value2" attr1="value1" ns2:attr1="value1" ns1:attr2="value2" ns1:attr1="value1">
+        </tag1>
+      </root>
+      """);
+    map.put("expected", """
+      <root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1" ns1:attr2="value2" attr1="value1">
+        <tag1 ns1:attr1="value1" ns1:attr2="value2" ns2:attr1="value1" ns2:attr2="value2" attr1="value1" attr2="value2">
+        </tag1>
+      </root>
+      """);
+    map.put("rules", new ArrayList<>(
+      Arrays.asList(ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, namespaceRule("http://ns.*")),
+                    ruleWithOrder(BY_NAME, nameRule(".*")))));
+    doTest(map);
   }
 
-  void testAttributeSortingByNamespace4() throws Exception {
-    doTest(
-      initial: '''\
-<root attr1="value1" ns1:attr2="value2" xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1">
-  <tag1 ns2:attr2="value2" attr2="value2" attr1="value1" ns2:attr1="value1" ns1:attr2="value2" ns1:attr1="value1">
-  </tag1>
-</root>
-''',
-      expected: '''\
-<root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" attr1="value1" ns1:attr1="value1" ns1:attr2="value2">
-  <tag1 ns2:attr1="value1" ns2:attr2="value2" attr1="value1" attr2="value2" ns1:attr1="value1" ns1:attr2="value2">
-  </tag1>
-</root>
-''',
-      rules: [ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, namespaceRule("http://ns2.com")),
-        ruleWithOrder(BY_NAME, nameRule(".*"))]
-    )
+  public void testAttributeSortingByNamespace4() {
+    LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(3);
+    map.put("initial", """
+      <root attr1="value1" ns1:attr2="value2" xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1">
+        <tag1 ns2:attr2="value2" attr2="value2" attr1="value1" ns2:attr1="value1" ns1:attr2="value2" ns1:attr1="value1">
+        </tag1>
+      </root>
+      """);
+    map.put("expected", """
+      <root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" attr1="value1" ns1:attr1="value1" ns1:attr2="value2">
+        <tag1 ns2:attr1="value1" ns2:attr2="value2" attr1="value1" attr2="value2" ns1:attr1="value1" ns1:attr2="value2">
+        </tag1>
+      </root>
+      """);
+    map.put("rules", new ArrayList<>(
+      Arrays.asList(ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, namespaceRule("http://ns2.com")),
+                    ruleWithOrder(BY_NAME, nameRule(".*")))));
+    doTest(map);
   }
 
-  void testAttributeSortingByNamespace5() throws Exception {
-    doTest(
-      initial: '''\
-<root attr1="value1" ns1:attr2="value2" xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1">
-  <tag1 ns2:attr2="value2" attr2="value2" attr1="value1" ns2:attr1="value1" ns1:attr2="value2" ns1:attr1="value1">
-  </tag1>
-</root>
-''',
-      expected: '''\
-<root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1" ns1:attr2="value2" attr1="value1">
-  <tag1 ns2:attr1="value1" ns2:attr2="value2" ns1:attr1="value1" ns1:attr2="value2" attr2="value2" attr1="value1">
-  </tag1>
-</root>
-''',
-      rules: [ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, namespaceRule("http://ns2.com")),
-        ruleWithOrder(BY_NAME, namespaceRule("http://ns1.com"))]
-    )
+  public void testAttributeSortingByNamespace5() {
+    LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(3);
+    map.put("initial", """
+      <root attr1="value1" ns1:attr2="value2" xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1">
+        <tag1 ns2:attr2="value2" attr2="value2" attr1="value1" ns2:attr1="value1" ns1:attr2="value2" ns1:attr1="value1">
+        </tag1>
+      </root>
+      """);
+    map.put("expected", """
+      <root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1" ns1:attr2="value2" attr1="value1">
+        <tag1 ns2:attr1="value1" ns2:attr2="value2" ns1:attr1="value1" ns1:attr2="value2" attr2="value2" attr1="value1">
+        </tag1>
+      </root>
+      """);
+    map.put("rules", new ArrayList<>(
+      Arrays.asList(ruleWithOrder(BY_NAME, nameRule("xmlns:.*")),
+                    ruleWithOrder(BY_NAME, namespaceRule("http://ns2.com")),
+                    ruleWithOrder(BY_NAME, namespaceRule("http://ns1.com")))));
+    doTest(map);
   }
 
-  void testAttributeSortingByNamespace6() throws Exception {
-    doTest(
-      initial: '''\
-<root attr1="value1" ns1:attr2="value2" xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1">
-  <tag1 ns2:attr2="value2" attr2="value2" attr1="value1" ns2:attr1="value1" ns1:attr2="value2" ns1:attr1="value1">
-  </tag1>
-</root>
-''',
-      expected: '''\
-<root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr2="value2" attr1="value1" ns1:attr1="value1">
-  <tag1 ns1:attr2="value2" ns2:attr2="value2" attr1="value1" attr2="value2" ns1:attr1="value1" ns2:attr1="value1">
-  </tag1>
-</root>
-''',
-      rules: [ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, compositeRule(".*:attr2", "http://ns.*")),
-        ruleWithOrder(BY_NAME, nameRule(".*"))]
-    )
+  public void testAttributeSortingByNamespace6() {
+    LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(3);
+    map.put("initial", """
+      <root attr1="value1" ns1:attr2="value2" xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr1="value1">
+        <tag1 ns2:attr2="value2" attr2="value2" attr1="value1" ns2:attr1="value1" ns1:attr2="value2" ns1:attr1="value1">
+        </tag1>
+      </root>
+      """);
+    map.put("expected", """
+      <root xmlns:ns1="http://ns1.com" xmlns:ns2="http://ns2.com" ns1:attr2="value2" attr1="value1" ns1:attr1="value1">
+        <tag1 ns1:attr2="value2" ns2:attr2="value2" attr1="value1" attr2="value2" ns1:attr1="value1" ns2:attr1="value1">
+        </tag1>
+      </root>
+      """);
+    map.put("rules", new ArrayList<>(
+      Arrays.asList(ruleWithOrder(BY_NAME, nameRule("xmlns:.*")), ruleWithOrder(BY_NAME, compositeRule(".*:attr2", "http://ns.*")),
+                    ruleWithOrder(BY_NAME, nameRule(".*")))));
+    doTest(map);
   }
 
-  void testTagSorting() throws Exception {
-    doTest(
-      initial: '''\
-<root>
-  <meta></meta>
-  <title></title>
-</root>
-''',
-      expected: '''\
-<root>
-  <title></title>
-  <meta></meta>
-</root>
-''',
-      rules: [ruleWithOrder(BY_NAME, nameRule("title"))]
-    )
+  public void testTagSorting() {
+    LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(3);
+    map.put("initial", """
+      <root>
+        <meta></meta>
+        <title></title>
+      </root>
+      """);
+    map.put("expected", """
+      <root>
+        <title></title>
+        <meta></meta>
+      </root>
+      """);
+    map.put("rules", new ArrayList<>(
+      Arrays.asList(ruleWithOrder(BY_NAME, nameRule("title")))));
+    doTest(map);
   }
 
-  void testTagAndAttrSorting() throws Exception {
-    doTest(
-      initial: '''\
-<root beta="">
-  <meta></meta>
-  <title></title>
-</root>
-''',
-      expected: '''\
-<root beta="">
-  <title></title>
-  <meta></meta>
-</root>
-''',
-      rules: [ruleWithOrder(BY_NAME, nameRule("title"))]
-    )
+  public void testTagAndAttrSorting() {
+    LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(3);
+    map.put("initial", """
+      <root beta="">
+        <meta></meta>
+        <title></title>
+      </root>
+      """);
+    map.put("expected", """
+      <root beta="">
+        <title></title>
+        <meta></meta>
+      </root>
+      """);
+    map.put("rules", new ArrayList<>(
+      Arrays.asList(ruleWithOrder(BY_NAME, nameRule("title")))));
+    doTest(map);
   }
 
   @NotNull
   private static StdArrangementMatchRule namespaceRule(@NotNull String filter) {
-    return new StdArrangementMatchRule(new StdArrangementEntryMatcher(
-      new ArrangementAtomMatchCondition(StdArrangementTokens.Regexp.XML_NAMESPACE, filter)))
+    return new StdArrangementMatchRule(
+      new StdArrangementEntryMatcher(new ArrangementAtomMatchCondition(StdArrangementTokens.Regexp.XML_NAMESPACE, filter)));
   }
 
   @NotNull
   protected static StdArrangementMatchRule compositeRule(@NotNull String nameFilter, @NotNull String namespaceFilter) {
-    return rule(new ArrangementAtomMatchCondition(StdArrangementTokens.Regexp.NAME, nameFilter),
-                new ArrangementAtomMatchCondition(StdArrangementTokens.Regexp.XML_NAMESPACE, namespaceFilter))
+    return AbstractRearrangerTest.rule(new ArrangementAtomMatchCondition(StdArrangementTokens.Regexp.NAME, nameFilter),
+                                       new ArrangementAtomMatchCondition(StdArrangementTokens.Regexp.XML_NAMESPACE, namespaceFilter));
   }
 }
