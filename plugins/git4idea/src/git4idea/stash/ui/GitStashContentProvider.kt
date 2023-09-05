eeae0038fb3c0d59dd2f74e3f90e59fa54e0d10e
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2023-09-05 15:58:36 +0200

    [git-stash] add const modifier
    GitOrigin-RevId: bf8e829f999b44e42a5ce99c418e3bd460722cce

--- plugins/git4idea/src/git4idea/stash/ui/GitStashContentProvider.kt
+++ plugins/git4idea/src/git4idea/stash/ui/GitStashContentProvider.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.stash.ui
 
 import com.intellij.icons.AllIcons
@@ -92,7 +92,7 @@ internal class GitStashContentProvider(private val project: Project) : ChangesVi
 
   companion object {
     @NonNls
-    val TAB_NAME = "Stash"
+    const val TAB_NAME = "Stash"
   }
 }
 
