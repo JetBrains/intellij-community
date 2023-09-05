3b12f0dc2fd7d59ac9b395e968712aa0c320e2fd
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2023-09-05 15:40:00 +0200

    [CoroutineDebugger] KTIJ-26064 Improve Gradle kotlin coroutines debug setup
    After the change, this setup is compatible with Gradle configuration cache. Also, this setup doesn't cause configuration cache invalidation anymore on switch between run and debug configurations, as it's now always being applied.
    The minimal expected version was changed because the kotlinx-coroutines-core artifact contains javaagent since 1.5.1
    
    GitOrigin-RevId: 78139374eb39d001e09117215a6ea76334cebda2

--- plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/KotlinGradleCoroutineDebugProjectResolver.kt
+++ plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/KotlinGradleCoroutineDebugProjectResolver.kt
@@ -3,6 +3,7 @@
 package org.jetbrains.kotlin.idea.gradleJava.configuration
 
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.externalSystem.rt.execution.ForkedDebuggerHelper
 import com.intellij.util.Consumer
 import org.jetbrains.kotlin.idea.extensions.KotlinJvmDebuggerFacade
 import org.jetbrains.plugins.gradle.service.project.AbstractProjectResolverExtension
@@ -13,7 +14,7 @@ class KotlinGradleCoroutineDebugProjectResolver : AbstractProjectResolverExtensi
     override fun enhanceTaskProcessing(taskNames: MutableList<String>, initScriptConsumer: Consumer<String>, parameters: Map<String, String>) {
         try {
             val allowCoroutineAgent = KotlinJvmDebuggerFacade.instance?.isCoroutineAgentAllowedInDebug ?: false
-            if (allowCoroutineAgent && parameters.containsKey(DEBUG_DISPATCH_PORT_KEY)) {
+            if (allowCoroutineAgent) {
                 setupCoroutineAgentForJvmForkedTestTasks(initScriptConsumer)
             }
         } catch (e: Exception) {
@@ -27,21 +28,33 @@ class KotlinGradleCoroutineDebugProjectResolver : AbstractProjectResolverExtensi
             """
             gradle.taskGraph.whenReady { TaskExecutionGraph taskGraph ->
                 taskGraph.allTasks.each { Task task ->
-                    if (task instanceof Test || task instanceof JavaExec) {
-                        task.doFirst { Task forkedTask ->
-                            def kotlinxCoroutinesCoreJar = forkedTask.classpath.find { it.name.startsWith("kotlinx-coroutines-core") }
-                            if (kotlinxCoroutinesCoreJar) {
-                                def results = (kotlinxCoroutinesCoreJar.getName() =~ /kotlinx-coroutines-core(\-jvm)?-(\d[\w\.\-]+)\.jar${'$'}/).findAll()
-                                if (results) {
-                                    def version = results.first()[2]
-                                    def referenceVersion = org.gradle.util.VersionNumber.parse('1.3.7-255')
-                                    if (org.gradle.util.VersionNumber.parse(version) > referenceVersion) {
-                                        forkedTask.jvmArgs ("-javaagent:${'$'}{kotlinxCoroutinesCoreJar?.absolutePath}", "-ea")
-                                    }
-                                }
+                    if (!(task instanceof Test || task instanceof JavaExec)) return
+                    FileCollection taskClasspath = task.classpath
+                    task.jvmArgumentProviders.add(new CommandLineArgumentProvider() {
+                        private static def VERSION_PATTERN = java.util.regex.Pattern.compile(/(\d+)\.(\d+)(\.(\d+))?.*/)
+                    
+                        @Override
+                        Iterable<String> asArguments() {
+                            List<String> emptyList = Collections.emptyList()
+                            if (System.getProperty("${ForkedDebuggerHelper.DISPATCH_PORT_SYS_PROP}") == null) return emptyList
+                            def kotlinxCoroutinesCoreJar = taskClasspath.find { it.name.startsWith("kotlinx-coroutines-core") && !it.name.contains("sources") }
+                            if (kotlinxCoroutinesCoreJar == null) return emptyList
+                            def results = (kotlinxCoroutinesCoreJar.getName() =~ /kotlinx-coroutines-core(\-jvm)?-(\d[\w\.\-]+)\.jar${'$'}/).findAll()
+                            if (results.isEmpty()) return emptyList
+                            String version = results.first()[2]
+                            def matcher = VERSION_PATTERN.matcher(version)
+                            try {
+                                if (!matcher.matches()) return emptyList
+                                int major = Integer.parseInt(matcher.group(1)) 
+                                int minor = Integer.parseInt(matcher.group(2))
+                                int patch = Integer.parseInt(matcher.group(4) ?: "0")
+                                if (major < 1 || (major == 1 && (minor < 5 || (minor == 5 && patch < 1)))) return emptyList
+                            } catch (NumberFormatException ignored) {
+                                return emptyList
                             }
+                            return ["-javaagent:${'$'}{kotlinxCoroutinesCoreJar?.absolutePath}", "-ea"]
                         }
-                    }
+                    })
                 }
             }
             """.trimIndent()
