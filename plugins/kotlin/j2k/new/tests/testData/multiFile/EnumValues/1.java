c40b5eb323264c8317484639c0a905a200e50182
Alexey Belkov <alexey.belkov@jetbrains.com>
2023-09-05 17:00:33 +0400

    [kotlin] J2K: fix TODO in testdata after Enum.entries became stable
    ^KTIJ-22299
    
    GitOrigin-RevId: dc38905548911435f03670e24018061e8409055d

--- plugins/kotlin/j2k/new/tests/testData/multiFile/EnumValues/1.java
+++ plugins/kotlin/j2k/new/tests/testData/multiFile/EnumValues/1.java
@@ -1,8 +1,4 @@
-// COMPILER_ARGUMENTS: -XXLanguage:+EnumEntries
-
 class EnumTest {
-    //TODO: Remove after Enum.entries is marked as non-experimental in Kotlin 1.9
-    @ExperimentalStdlibApi
     void testToReplaceEnumValues() {
         MyEnum x1 = MyEnum.values()[1];
         MyEnumKt x2 = MyEnumKt.values()[1];
