2aef1c7d159ea5fea211ed56bd99faea45cd3ad7
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2023-09-06 16:56:11 +0200

    [cwm build tests] diagnostics: report all problems with RuntimeModuleRepository at once
    GitOrigin-RevId: 72ac1655dcfe3e6b15bd33aa3b3a357eb7099069

--- platform/build-scripts/testFramework/src/com/intellij/platform/buildScripts/testFramework/RuntimeModuleRepositoryChecker.kt
+++ platform/build-scripts/testFramework/src/com/intellij/platform/buildScripts/testFramework/RuntimeModuleRepositoryChecker.kt
@@ -3,27 +3,22 @@ package com.intellij.platform.buildScripts.testFramework
 
 import com.intellij.devkit.runtimeModuleRepository.jps.build.RuntimeModuleRepositoryBuildConstants
 import com.intellij.platform.runtime.repository.serialization.RuntimeModuleRepositorySerialization
-import org.junit.jupiter.api.Assertions
+import org.assertj.core.api.SoftAssertions
 import java.nio.file.Path
 
 /**
  * Checks that runtime module descriptors in the product distribution are valid.
  */
 class RuntimeModuleRepositoryChecker(private val distAllDir: Path) {
-  fun check() {
+  fun check(softly: SoftAssertions) {
     val jarFile = distAllDir.resolve(RuntimeModuleRepositoryBuildConstants.JAR_REPOSITORY_FILE_NAME)
     val descriptors = RuntimeModuleRepositorySerialization.loadFromJar(jarFile)
-    val errors = ArrayList<String>()
     descriptors.values.forEach { descriptor -> 
       descriptor.dependencies.forEach { dependency ->
-        if (dependency !in descriptors) {
-          errors.add("Unknown dependency '$dependency' in module '${descriptor.id}'")
-        }
+        softly.assertThat(dependency in descriptors)
+          .describedAs("Unknown dependency '$dependency' in module '${descriptor.id}'")
+          .isTrue
       }
-    }
-    if (errors.isNotEmpty()) {
-      return Assertions.fail("Runtime module repository has problems:\n" +
-                             errors.joinToString("\n"))
     }
   }
 }
