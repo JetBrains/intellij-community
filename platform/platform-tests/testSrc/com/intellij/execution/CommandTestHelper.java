4c990df2842dc2c913c07878bacc8512ced0563a
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-09-06 18:25:39 +0200

    [tests] command line tests diagnostic & cleanup
    GitOrigin-RevId: 7c82faf4adf04f4a9f78c3df46dc853bd1d9ad05

--- platform/platform-tests/testSrc/com/intellij/execution/CommandTestHelper.java
+++ platform/platform-tests/testSrc/com/intellij/execution/CommandTestHelper.java
@@ -1,20 +1,18 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 
 public final class CommandTestHelper {
   public static final String ARG = "-arg";
   public static final String ENV = "-env";
   public static final String OUT = "-out";
-  public static final String ENC = "UTF-8";
 
   public static void main(String[] args) throws IOException {
-    String mode = null;
-    String out = null;
+    String mode = null, out = null;
     if (args.length >= 3) {
       if (ARG.equals(args[0])) mode = ARG;
       if (ENV.equals(args[0])) mode = ENV;
@@ -25,10 +23,15 @@ public final class CommandTestHelper {
       System.exit(1);
     }
 
-    try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(out), ENC)) {
+    try (var writer = Files.newBufferedWriter(Path.of(out))) {
       if (mode.equals(ENV)) {
-        for (Map.Entry<String, String> entry : System.getenv().entrySet()) {
+        for (var entry : System.getenv().entrySet()) {
           writer.write(format(entry));
+          writer.write('\n');
+        }
+        writer.write("=== debug ===\n");
+        for (var entry : System.getenv().entrySet()) {
+          writer.write(entry.getKey() + "=" + entry.getValue());
           writer.write('\n');
         }
       }
--- platform/platform-tests/testSrc/com/intellij/execution/configurations/PtyCommandLineTest.java
+++ platform/platform-tests/testSrc/com/intellij/execution/configurations/PtyCommandLineTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.configurations;
 
 import com.intellij.execution.GeneralCommandLineTest;
@@ -11,9 +11,8 @@ import org.junit.Test;
 import java.util.Arrays;
 
 public class PtyCommandLineTest extends GeneralCommandLineTest {
-  @NotNull
   @Override
-  protected String filterExpectedOutput(@NotNull String output) {
+  protected @NotNull String filterExpectedOutput(@NotNull String output) {
     if (SystemInfo.isWindows) {
       output = StringUtil.trimTrailing(expandTabs(output, 8));
     }
@@ -25,7 +24,6 @@ public class PtyCommandLineTest extends GeneralCommandLineTest {
     return new PtyCommandLine(Arrays.asList(command)).withWindowsAnsiColorDisabled();
   }
 
-  @NotNull
   private static String expandTabs(String s, @SuppressWarnings("SameParameterValue") int tabSize) {
     StringBuilder sb = new StringBuilder();
 
