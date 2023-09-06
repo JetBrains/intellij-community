162b7f7a5feee26380d015b707d3f5a166a507ec
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2023-09-06 11:00:29 +0200

    [platform] API: mark ProjectTopics as deprecated (IDEA-331726)
    GitOrigin-RevId: bee9004b501e9f0cbe13e98d2eaab556dc56bbdc

--- platform/projectModel-api/src/com/intellij/ProjectTopics.java
+++ platform/projectModel-api/src/com/intellij/ProjectTopics.java
@@ -5,6 +5,10 @@ import com.intellij.openapi.project.ModuleListener;
 import com.intellij.openapi.roots.ModuleRootListener;
 import com.intellij.util.messages.Topic;
 
+/**
+ * @deprecated don't add new constants to this class and use replacements for existing ones.
+ */
+@Deprecated
 public final class ProjectTopics {
   /**
    * @deprecated use {@link ModuleRootListener#TOPIC} instead
