49444b60492d1693323b43471c289e6dc3f011f1
Aleksey Pivovarov <AMPivovarov@gmail.com>
2023-09-07 17:15:18 +0200

    vcs: introduce smart dirty scope set
    GitOrigin-RevId: 1c88a4f76aeaa5e67c1cace5075e9cd8471d9c50

--- platform/vcs-impl/src/com/intellij/openapi/vcs/util/paths/FilePathMapping.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/util/paths/FilePathMapping.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vcs.util.paths;
 
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.UriUtil;
 import com.intellij.util.containers.CollectionFactory;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
@@ -29,7 +28,7 @@ public final class FilePathMapping<T> {
   public void add(@NotNull String filePath, @NotNull T value) {
     String path = UriUtil.trimTrailingSlashes(filePath);
     myPathMap.put(path, value);
-    myPathHashSet.add(pathHashCode(myCaseSensitive, path));
+    myPathHashSet.add(FilePathHashUtil.pathHashCode(myCaseSensitive, path));
   }
 
   public void remove(@NotNull String filePath) {
@@ -70,7 +69,7 @@ public final class FilePathMapping<T> {
       int nextIndex = path.indexOf('/', index + 1);
       if (nextIndex == -1) nextIndex = path.length();
 
-      prefixHash = pathHashCode(myCaseSensitive, path, index, nextIndex, prefixHash);
+      prefixHash = FilePathHashUtil.pathHashCode(myCaseSensitive, path, index, nextIndex, prefixHash);
 
       if (myPathHashSet.contains(prefixHash)) {
         matches.add(nextIndex);
@@ -86,18 +85,5 @@ public final class FilePathMapping<T> {
     }
 
     return null;
-  }
-
-  private static int pathHashCode(boolean caseSensitive, @NotNull String path) {
-    return pathHashCode(caseSensitive, path, 0, path.length(), 0);
-  }
-
-  private static int pathHashCode(boolean caseSensitive, @NotNull String path, int offset1, int offset2, int prefixHash) {
-    if (caseSensitive) {
-      return StringUtil.stringHashCode(path, offset1, offset2, prefixHash);
-    }
-    else {
-      return StringUtil.stringHashCodeInsensitive(path, offset1, offset2, prefixHash);
-    }
   }
 }
