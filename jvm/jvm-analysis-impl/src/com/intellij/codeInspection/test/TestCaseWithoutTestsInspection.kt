d88b380b327b0513c7cfc4d37edb5ea6e4d69618
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2023-09-08 16:36:31 +0200

    [jvm] Ignore test classes annotated with RunWith
    #IDEA-331957 Fixed
    
    GitOrigin-RevId: 9fdb59baede8452cf38dcc2f16f89a7c1ef72055

--- jvm/jvm-analysis-impl/src/com/intellij/codeInspection/test/TestCaseWithoutTestsInspection.kt
+++ jvm/jvm-analysis-impl/src/com/intellij/codeInspection/test/TestCaseWithoutTestsInspection.kt
@@ -10,7 +10,9 @@ import com.intellij.lang.jvm.JvmElementVisitor
 import com.intellij.lang.jvm.JvmModifier
 import com.intellij.lang.jvm.inspection.JvmLocalInspection
 import com.intellij.openapi.project.Project
-import com.intellij.psi.*
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiModifier
+import com.intellij.psi.PsiTypeParameter
 import com.intellij.psi.search.searches.ClassInheritorsSearch
 import com.intellij.psi.util.PsiUtil
 import com.intellij.testIntegration.JavaTestFramework
@@ -35,7 +37,7 @@ private class TestCaseWithoutTestsVisitor(
   private val sink: JvmLocalInspection.HighlightSink,
   private val ignoreSupers: Boolean
 ) : DefaultJvmElementVisitor<Boolean> {
-  override fun visitClass(clazz: JvmClass): Boolean? {
+  override fun visitClass(clazz: JvmClass): Boolean {
     if (clazz !is PsiClass) return true
     if (clazz is PsiTypeParameter) return true
     if (clazz.isEnum || clazz.isAnnotationType || clazz.isInterface || PsiUtil.isLocalOrAnonymousClass(clazz)) return true
@@ -55,6 +57,7 @@ private class TestCaseWithoutTestsVisitor(
     if (ignoreSupers) {
       var superClass = clazz.getSuperClass()
       while (superClass != null && clazz.getManager().isInProject(superClass)) {
+        if (superClass.hasAnnotation(JUnitCommonClassNames.ORG_JUNIT_RUNNER_RUN_WITH)) return true
         if (hasTestMethods(superClass, applicableFrameworks, applicableToNestedClasses, false)) return true
         superClass = superClass.getSuperClass()
       }
@@ -76,9 +79,7 @@ private class TestCaseWithoutTestsVisitor(
     val methods = aClass.getMethods()
     for (framework in selfFrameworks) {
       if (checkSuite && framework is JavaTestFramework && framework.isSuiteClass(aClass)) return true
-      if (methods.any { method ->
-        framework.isTestMethod(method)
-      }) return true
+      if (methods.any { method -> framework.isTestMethod(method, false) }) return true
     }
 
     if (!nestedTestFrameworks.isEmpty()) {
