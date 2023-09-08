ac91fb0ca53e1f6f101623a0035aec3856130d19
Mikhail Sokolov <mikhail.sokolov@jetbrains.com>
2023-09-08 23:48:42 +0200

    IDEA-326930 Incorrect window controls colour on hovering in High Contrast theme
    (cherry picked from commit 20c41cc052b93020cde9ee98a746857e0bb8910d)
    
    IJ-CR-114654
    
    GitOrigin-RevId: c4a5dfe10e25c82f8f5d5939b6bf77eeae445ea7

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/AbstractGotoSEContributor.java
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/AbstractGotoSEContributor.java
@@ -27,6 +27,7 @@ import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.pom.Navigatable;
