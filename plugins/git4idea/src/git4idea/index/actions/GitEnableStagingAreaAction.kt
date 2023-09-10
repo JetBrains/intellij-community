52901bf8d340f99871ac42134d29e64b35b55f6c
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2023-09-10 20:43:03 +0200

    [git-index] split GitEnableStagingAreaAction into two separate actions
    GitOrigin-RevId: 7ab92df05a71922b6e32f9c5f61c0d7d89ba9ce9

--- plugins/git4idea/src/git4idea/index/actions/GitEnableStagingAreaAction.kt
+++ plugins/git4idea/src/git4idea/index/actions/GitEnableStagingAreaAction.kt
@@ -1,25 +1,18 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.index.actions
 
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.actionSystem.RightAlignedToolbarAction
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.vcs.ProjectLevelVcsManager
 import com.intellij.openapi.vcs.changes.ChangeListManager
 import git4idea.GitVcs
 import git4idea.config.GitVcsApplicationSettings
-import git4idea.i18n.GitBundle
 import git4idea.index.canEnableStagingArea
 import git4idea.index.enableStagingArea
 
-class GitEnableStagingAreaAction : DumbAwareAction(), RightAlignedToolbarAction {
-
-  init {
-    updatePresentation(templatePresentation, false)
-  }
+abstract class GitToggleStagingAreaAction(private val enable: Boolean) : DumbAwareAction(), RightAlignedToolbarAction {
 
   override fun update(e: AnActionEvent) {
     val project = e.project
@@ -29,31 +22,24 @@ class GitEnableStagingAreaAction : DumbAwareAction(), RightAlignedToolbarAction 
       return
     }
 
-    val isStagingAreaEnabled = GitVcsApplicationSettings.getInstance().isStagingAreaEnabled
-    if (!isStagingAreaEnabled && ChangeListManager.getInstance(project).changeListsNumber > 1) {
-      e.presentation.isEnabledAndVisible = false
-      return
-    }
-
-    updatePresentation(e.presentation, isStagingAreaEnabled)
+    e.presentation.isEnabledAndVisible = GitVcsApplicationSettings.getInstance().isStagingAreaEnabled != enable
   }
 
   override fun actionPerformed(e: AnActionEvent) {
     enableStagingArea(!GitVcsApplicationSettings.getInstance().isStagingAreaEnabled)
   }
 
-  private fun updatePresentation(presentation: Presentation, isStagingAreaEnabled: Boolean) {
-    if (isStagingAreaEnabled) {
-      presentation.text = GitBundle.message("action.Git.Stage.Enable.Enabled.text")
-      presentation.description = GitBundle.message("action.Git.Stage.Enable.Enabled.description")
-      presentation.icon = AllIcons.Vcs.Changelist
-    }
-    else {
-      presentation.text = GitBundle.message("action.Git.Stage.Enable.Disabled.text")
-      presentation.description = GitBundle.message("action.Git.Stage.Enable.Disabled.description")
-      presentation.icon = AllIcons.Ide.Gift
-    }
-  }
-
   override fun getActionUpdateThread() = ActionUpdateThread.EDT
 }
+
+class GitEnableStagingAreaAction : GitToggleStagingAreaAction(true) {
+  override fun update(e: AnActionEvent) {
+    super.update(e)
+    val project = e.project
+    if (e.presentation.isEnabled && project != null && ChangeListManager.getInstance(project).changeListsNumber > 1) {
+      e.presentation.isEnabledAndVisible = false
+    }
+  }
+}
+
+class GitDisableStagingAreaAction : GitToggleStagingAreaAction(false)
