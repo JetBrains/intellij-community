b48e16fafc711ae844f298c15471ef7b57cd167e
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2023-09-11 13:16:40 +0200

    maven - separate repository http test logic into fixture
    GitOrigin-RevId: 0e2076798261eba8ab1940cf6139687661f1a7ff

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenRepositoriesDownloadingTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenRepositoriesDownloadingTest.kt
@@ -1,16 +1,11 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.maven.importing
 
-import com.intellij.ReviseWhenPortedToJDK
 import com.intellij.maven.testFramework.MavenMultiVersionImportingTestCase
-import com.intellij.openapi.util.io.StreamUtil
+import com.intellij.maven.testFramework.utils.MavenHttpRepositoryServerFixture
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.testFramework.RunAll.Companion.runAll
 import com.intellij.util.ThrowableRunnable
-import com.intellij.util.concurrency.AppExecutorUtil
-import com.sun.net.httpserver.Authenticator
-import com.sun.net.httpserver.BasicAuthenticator
-import com.sun.net.httpserver.HttpServer
 import junit.framework.TestCase
 import org.intellij.lang.annotations.Language
 import org.jetbrains.idea.maven.MavenCustomRepositoryHelper
@@ -20,32 +15,24 @@ import org.jetbrains.idea.maven.server.MisconfiguredPlexusDummyEmbedder
 import org.junit.Assume.assumeTrue
 import org.junit.Test
 import java.io.File
-import java.io.FileInputStream
-import java.net.InetSocketAddress
-import java.nio.charset.StandardCharsets
 
 class MavenRepositoriesDownloadingTest : MavenMultiVersionImportingTestCase() {
 
-  @ReviseWhenPortedToJDK("18") //replace with SimpleFileServers
-
-  private lateinit var myServer: HttpServer
+  private val httpServerFixture = MavenHttpRepositoryServerFixture()
   private lateinit var myUrl: String
 
   @Throws(Exception::class)
   public override fun setUp() {
     super.setUp()
-    myServer = HttpServer.create()
-    myServer.setExecutor(AppExecutorUtil.getAppExecutorService())
-    myServer.bind(InetSocketAddress(LOCALHOST, 0), 5)
-    myServer.start()
-    myUrl = "http://" + LOCALHOST + ":" + myServer.getAddress().port
+    httpServerFixture.setUp()
+    myUrl = httpServerFixture.url()
   }
 
 
   public override fun tearDown() {
     runAll(
       ThrowableRunnable<Throwable> {
-        myServer?.stop(0)
+        httpServerFixture.tearDown()
       },
       ThrowableRunnable<Throwable> { super.tearDown() }
     )
@@ -57,7 +44,7 @@ class MavenRepositoriesDownloadingTest : MavenMultiVersionImportingTestCase() {
     val helper = MavenCustomRepositoryHelper(myDir, "local1", "remote")
     val remoteRepoPath = helper.getTestDataPath("remote")
     val localRepoPath = helper.getTestDataPath("local1")
-    setupRemoteRepositoryServer(remoteRepoPath)
+    httpServerFixture.startRepositoryFor(remoteRepoPath)
     repositoryPath = localRepoPath
     val settingsXml = createProjectSubFile(
       "settings.xml",
@@ -79,11 +66,7 @@ class MavenRepositoriesDownloadingTest : MavenMultiVersionImportingTestCase() {
     val helper = MavenCustomRepositoryHelper(myDir, "local1", "remote")
     val remoteRepoPath = helper.getTestDataPath("remote")
     val localRepoPath = helper.getTestDataPath("local1")
-    setupRemoteRepositoryServer(remoteRepoPath, object : BasicAuthenticator("/") {
-      override fun checkCredentials(username: String?, password: String?): Boolean {
-        return username == USERNAME && password == PASSWORD
-      }
-    })
+    httpServerFixture.startRepositoryFor(File(remoteRepoPath), USERNAME, PASSWORD)
     repositoryPath = localRepoPath
     val settingsXml = createProjectSubFile(
       "settings.xml",
@@ -115,11 +98,7 @@ class MavenRepositoriesDownloadingTest : MavenMultiVersionImportingTestCase() {
     val helper = MavenCustomRepositoryHelper(myDir, "local1", "remote")
     val remoteRepoPath = helper.getTestDataPath("remote")
     val localRepoPath = helper.getTestDataPath("local1")
-    setupRemoteRepositoryServer(remoteRepoPath, object : BasicAuthenticator("/") {
-      override fun checkCredentials(username: String?, password: String?): Boolean {
-        return username == USERNAME && password == PASSWORD
-      }
-    })
+    httpServerFixture.startRepositoryFor(File(remoteRepoPath), USERNAME, PASSWORD)
     repositoryPath = localRepoPath
     val settingsXml = createProjectSubFile(
       "settings.xml",
@@ -153,11 +132,7 @@ class MavenRepositoriesDownloadingTest : MavenMultiVersionImportingTestCase() {
     val helper = MavenCustomRepositoryHelper(myDir, "local1", "remote")
     val remoteRepoPath = helper.getTestDataPath("remote")
     val localRepoPath = helper.getTestDataPath("local1")
-    setupRemoteRepositoryServer(remoteRepoPath, object : BasicAuthenticator("/") {
-      override fun checkCredentials(username: String?, password: String?): Boolean {
-        return username == USERNAME && password == PASSWORD
-      }
-    })
+    httpServerFixture.startRepositoryFor(File(remoteRepoPath), USERNAME, PASSWORD)
     repositoryPath = localRepoPath
     @Language(value = "XML") val settingsXmlText = """
        <settings>
@@ -200,9 +175,9 @@ class MavenRepositoriesDownloadingTest : MavenMultiVersionImportingTestCase() {
     createProjectPom("""<groupId>test</groupId>
                        <artifactId>project</artifactId>
                        <version>1</version>
-                       """);
+                       """)
 
-    val extensionsXml = createProjectSubFile(".mvn/extensions.xml", """
+    createProjectSubFile(".mvn/extensions.xml", """
     <extensions xmlns="http://maven.apache.org/EXTENSIONS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
         xsi:schemaLocation="http://maven.apache.org/EXTENSIONS/1.0.0 http://maven.apache.org/xsd/core-extensions-1.0.0.xsd">
       <extension>
@@ -211,7 +186,7 @@ class MavenRepositoriesDownloadingTest : MavenMultiVersionImportingTestCase() {
         <version>1.0</version>
       </extension>
     </extensions>
-    """.trimIndent());
+    """.trimIndent())
 
     Registry.get("maven.server.debug").setValue(false)
     removeFromLocalRepository("org/mytest/myartifact/")
@@ -224,29 +199,6 @@ class MavenRepositoriesDownloadingTest : MavenMultiVersionImportingTestCase() {
 
   }
 
-  private fun setupRemoteRepositoryServer(repoPath: String, authenticator: Authenticator? = null) {
-    val repo = File(repoPath)
-    val httpContext = myServer.createContext("/") { exchange ->
-      val path = exchange.requestURI.path
-      val file = File(repo, path)
-      if (file.isDirectory) {
-        exchange.responseHeaders.add("Content-Type", "text/html")
-        exchange.sendResponseHeaders(200, 0)
-        val list = java.lang.String.join(",\n", *file.list())
-        exchange.responseBody.write(list.toByteArray(StandardCharsets.UTF_8))
-      }
-      else if (file.isFile) {
-        exchange.responseHeaders.add("Content-Type", "application/octet-stream")
-        exchange.sendResponseHeaders(200, 0)
-        StreamUtil.copy(FileInputStream(file), exchange.responseBody)
-      }
-      else {
-        exchange.sendResponseHeaders(404, -1)
-      }
-      exchange.close()
-    }
-    authenticator?.let { httpContext.authenticator = it }
-  }
 
   fun pom() = """
                        <groupId>test</groupId>
@@ -269,7 +221,6 @@ class MavenRepositoriesDownloadingTest : MavenMultiVersionImportingTestCase() {
                        """.trimIndent()
 
   companion object {
-    private const val LOCALHOST = "127.0.0.1"
     private const val USERNAME = "myUsername"
     private const val PASSWORD = "myPassword"
   }
