19d203b8d8427e5ce13bf91a91003f28a17f89ae
fscarponi <fscarponi@gmail.com>
2023-09-12 14:07:16 +0200

    Modifier.appenIf inlined
    GitOrigin-RevId: e275508265ffd2985ccd6c2e61658f720cc6760d

--- platform/jewel/compose-utils/src/main/kotlin/org/jetbrains/jewel/util/ModifierExtensions.kt
+++ platform/jewel/compose-utils/src/main/kotlin/org/jetbrains/jewel/util/ModifierExtensions.kt
@@ -2,5 +2,5 @@ package org.jetbrains.jewel.util
 
 import androidx.compose.ui.Modifier
 
-fun Modifier.appendIf(condition: Boolean, transformer: Modifier.() -> Modifier): Modifier =
+inline fun Modifier.appendIf(condition: Boolean, transformer: Modifier.() -> Modifier): Modifier =
     if (!condition) this else transformer()
