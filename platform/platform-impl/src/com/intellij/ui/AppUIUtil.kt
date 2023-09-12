ea705c7dedcd9bdeec73ca3ac6d26391e3e22a0d
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-09-12 15:44:59 +0200

    [ui] manually fitting the small app icon into a context (IDEA-330012)
    GitOrigin-RevId: 59d34613d0627b7873e195327db7a8e9f7402f35

--- platform/platform-impl/src/com/intellij/ui/AppUIUtil.kt
+++ platform/platform-impl/src/com/intellij/ui/AppUIUtil.kt
@@ -140,20 +140,17 @@ private fun findAppIconSvgData(path: String, pixScale: Float): ByteArray? {
   return null
 }
 
-
-fun loadSmallApplicationIcon(scaleContext: ScaleContext,
-                             size: Int = 16,
-                             isReleaseIcon: Boolean = !ApplicationInfoImpl.getShadowInstance().isEAP): Icon {
+fun loadSmallApplicationIcon(scaleContext: ScaleContext, size: Int, requestReleaseIcon: Boolean): Icon {
   val appInfo = ApplicationInfoImpl.getShadowInstance()
-  val smallIconUrl = if (isReleaseIcon && appInfo.isEAP && appInfo is ApplicationInfoImpl) {
+  val upscale = size * scaleContext.getScale(DerivedScaleType.PIX_SCALE) >= 20
+  val svgUrl = if (appInfo is ApplicationInfoImpl) {
     // This is the way to load the release icon in EAP. Needed for some actions.
-    appInfo.getSmallApplicationSvgIconUrl(false)
+    if (upscale) appInfo.getApplicationSvgIconUrl(!requestReleaseIcon) else appInfo.getSmallApplicationSvgIconUrl(!requestReleaseIcon)
   }
   else {
-    appInfo.smallApplicationSvgIconUrl
+    if (upscale) appInfo.applicationSvgIconUrl else appInfo.smallApplicationSvgIconUrl
   }
-  val image = loadAppIconImage(svgPath = smallIconUrl, scaleContext = scaleContext, size = size) ?: error("Can't load '${smallIconUrl}'")
-  return JBImageIcon(image)
+  return JBImageIcon(loadAppIconImage(svgUrl, scaleContext, size) ?: error("Can't load '${svgUrl}'"))
 }
 
 fun findAppIcon(): String? {
@@ -184,11 +181,8 @@ object AppUIUtil {
   // todo[tav] JBR supports loading icon resource (id=2000) from the exe launcher, remove when OpenJDK supports it as well
   @JvmOverloads
   @JvmStatic
-  fun loadSmallApplicationIcon(scaleContext: ScaleContext, size: Int = 16): Icon {
-    return loadSmallApplicationIcon(scaleContext = scaleContext,
-                                    size = size,
-                                    isReleaseIcon = !ApplicationInfoImpl.getShadowInstance().isEAP)
-  }
+  fun loadSmallApplicationIcon(scaleContext: ScaleContext, size: Int = 16): Icon =
+    loadSmallApplicationIcon(scaleContext, size, requestReleaseIcon = !ApplicationInfoImpl.getShadowInstance().isEAP)
 
   @JvmStatic
   fun loadApplicationIcon(ctx: ScaleContext, size: Int): Icon? {
