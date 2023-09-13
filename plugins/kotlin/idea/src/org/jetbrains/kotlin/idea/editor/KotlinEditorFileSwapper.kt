3a600d0d929d333fc56fc3089e3061e432dbe1a1
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2023-09-13 18:26:09 +0200

    [kotlin] [IDEA-331264] use KotlinDeclarationNavigationPolicy instead of raw SourceNavigationHelper
    GitOrigin-RevId: 2e70ec94dc47701248637858eb19e0393c6a514d

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/editor/KotlinEditorFileSwapper.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/editor/KotlinEditorFileSwapper.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.editor
 
+import com.intellij.openapi.components.serviceOrNull
 import com.intellij.openapi.fileEditor.impl.EditorComposite
 import com.intellij.openapi.fileEditor.impl.EditorFileSwapper
 import com.intellij.openapi.project.Project
@@ -8,7 +9,7 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.kotlin.analysis.decompiler.psi.file.KtDecompiledFile
-import org.jetbrains.kotlin.idea.decompiler.navigation.SourceNavigationHelper
+import org.jetbrains.kotlin.psi.KotlinDeclarationNavigationPolicy
 import org.jetbrains.kotlin.psi.KtDeclaration
 
 class KotlinEditorFileSwapper : EditorFileSwapper {
@@ -32,7 +33,7 @@ class KotlinEditorFileSwapper : EditorFileSwapper {
 
     private fun getSourcesLocation(psiFile: KtDecompiledFile): VirtualFile? = psiFile.declarations
         .firstOrNull()?.let {
-            val element = SourceNavigationHelper.getNavigationElement(it)
+            val element = serviceOrNull<KotlinDeclarationNavigationPolicy>()?.getNavigationElement(it) ?: return@let null
             // declaration from the decompiled file should not be equal to a source declaration
             if (it != element) {
                 val sourceFile = element.containingFile
@@ -44,13 +45,10 @@ class KotlinEditorFileSwapper : EditorFileSwapper {
         }
 
     private fun getCursorPosition(originalOffset: Int, decompiledFile: KtDecompiledFile): Int {
-        val cursor = decompiledFile.navigationElement.findElementAt(originalOffset)
-        if (cursor != null) {
-            val declaration = PsiTreeUtil.getParentOfType(cursor, KtDeclaration::class.java, false)
-            if (declaration != null) {
-                return SourceNavigationHelper.getNavigationElement(declaration).textOffset
-            }
+        val cursor = decompiledFile.navigationElement.findElementAt(originalOffset) ?: return 0
+        val declarationInSources = PsiTreeUtil.getParentOfType(cursor, KtDeclaration::class.java, false)?.let {
+            serviceOrNull<KotlinDeclarationNavigationPolicy>()?.getNavigationElement(it)
         }
-        return 0
+        return declarationInSources?.textOffset ?: 0
     }
 }
