198b2402e6b7c6dc95dacf20869bcc1a9c4335c4
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-09-14 18:56:33 +0200

    [java-refactoring] IDEA-324451 SlowOp for EncapsulateFieldsHandler
    GitOrigin-RevId: 9c035597260514a9513fda4e4b6b27ba4b53726d

--- java/java-impl-refactorings/src/com/intellij/refactoring/encapsulateFields/EncapsulateFieldsHandler.java
+++ java/java-impl-refactorings/src/com/intellij/refactoring/encapsulateFields/EncapsulateFieldsHandler.java
@@ -6,6 +6,8 @@ import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo;
 import com.intellij.java.refactoring.JavaRefactoringBundle;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.ScrollType;
@@ -17,12 +19,16 @@ import com.intellij.refactoring.PreviewableRefactoringActionHandler;
 import com.intellij.refactoring.RefactoringBundle;
 import com.intellij.refactoring.util.CommonRefactoringUtil;
 import com.intellij.refactoring.util.DocCommentPolicy;
+import com.intellij.util.concurrency.AppExecutorUtil;
 import com.intellij.util.containers.ContainerUtil;
 import com.siyeh.ig.psiutils.VariableAccessUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class EncapsulateFieldsHandler implements PreviewableRefactoringActionHandler {
   private static final Logger LOG = Logger.getInstance(EncapsulateFieldsHandler.class);
@@ -49,66 +55,106 @@ public class EncapsulateFieldsHandler implements PreviewableRefactoringActionHan
    */
   @Override
   public void invoke(@NotNull final Project project, final PsiElement @NotNull [] elements, DataContext dataContext) {
-    PsiClass aClass = null;
-    final HashSet<PsiField> preselectedFields = new HashSet<>();
-    if (elements.length == 1) {
-      if (elements[0] instanceof PsiClass) {
-        aClass = (PsiClass) elements[0];
-      } else if (elements[0] instanceof PsiField field) {
-        aClass = field.getContainingClass();
-        preselectedFields.add(field);
-      } else {
-        return;
-      }
-    } else {
-      for (PsiElement element : elements) {
-        if (!(element instanceof PsiField field)) {
-          return;
-        }
-        if (aClass == null) {
-          aClass = field.getContainingClass();
-          preselectedFields.add(field);
-        }
-        else {
-          if (aClass.equals(field.getContainingClass())) {
+    ReadAction.nonBlocking(() -> {
+        PsiClass aClass = null;
+        Runnable callback = null;
+        final HashSet<PsiField> preselectedFields = new HashSet<>();
+        if (elements.length == 1) {
+          if (elements[0] instanceof PsiClass) {
+            aClass = (PsiClass)elements[0];
+          }
+          else if (elements[0] instanceof PsiField field) {
+            aClass = field.getContainingClass();
             preselectedFields.add(field);
           }
           else {
-            String message = RefactoringBundle.getCannotRefactorMessage(
-              JavaRefactoringBundle.message("fields.to.be.refactored.should.belong.to.the.same.class"));
-            Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
-            CommonRefactoringUtil.showErrorHint(project, editor, message, getRefactoringName(), HelpID.ENCAPSULATE_FIELDS);
-            return;
+            return null;
           }
         }
-      }
-    }
+        else {
+          for (PsiElement element : elements) {
+            if (!(element instanceof PsiField field)) {
+              return null;
+            }
+            if (aClass == null) {
+              aClass = field.getContainingClass();
+              preselectedFields.add(field);
+            }
+            else {
+              if (aClass.equals(field.getContainingClass())) {
+                preselectedFields.add(field);
+              }
+              else {
+                callback = () ->
+                {
+                  String message = RefactoringBundle.getCannotRefactorMessage(
+                    JavaRefactoringBundle.message("fields.to.be.refactored.should.belong.to.the.same.class"));
+                  Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
+                  CommonRefactoringUtil.showErrorHint(project, editor, message, getRefactoringName(), HelpID.ENCAPSULATE_FIELDS);
+                };
+                return callback;
+              }
+            }
+          }
+        }
 
-    LOG.assertTrue(aClass != null);
-    final List<PsiField> fields = ContainerUtil.filter(aClass.getFields(), field -> !(field instanceof PsiEnumConstant));
-    if (fields.isEmpty()) {
-      CommonRefactoringUtil.showErrorHint(project, CommonDataKeys.EDITOR.getData(dataContext),
-                                          JavaRefactoringBundle.message("encapsulate.fields.nothing.todo.warning.message"),
-                                          getRefactoringName(), HelpID.ENCAPSULATE_FIELDS);
-      return;
-    }
+        LOG.assertTrue(aClass != null);
+        final List<PsiField> fields = ContainerUtil.filter(aClass.getFields(), field -> !(field instanceof PsiEnumConstant));
+        if (fields.isEmpty()) {
+          callback = () -> {
+            CommonRefactoringUtil.showErrorHint(project, CommonDataKeys.EDITOR.getData(dataContext),
+                                                JavaRefactoringBundle.message("encapsulate.fields.nothing.todo.warning.message"),
+                                                getRefactoringName(), HelpID.ENCAPSULATE_FIELDS);
+          };
+          return callback;
+        }
 
-    if (aClass.isInterface()) {
-      String message = RefactoringBundle.getCannotRefactorMessage(
-        JavaRefactoringBundle.message("encapsulate.fields.refactoring.cannot.be.applied.to.interface"));
-      Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
-      CommonRefactoringUtil.showErrorHint(project, editor, message, getRefactoringName(), HelpID.ENCAPSULATE_FIELDS);
-      return;
-    }
-
-    if (!CommonRefactoringUtil.checkReadOnlyStatus(project, aClass)) return;
-
-    EncapsulateFieldsDialog dialog = createDialog(project, aClass, preselectedFields);
-    dialog.show();
+        if (aClass.isInterface()) {
+          callback = () -> {
+            String message = RefactoringBundle.getCannotRefactorMessage(
+              JavaRefactoringBundle.message("encapsulate.fields.refactoring.cannot.be.applied.to.interface"));
+            Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
+            CommonRefactoringUtil.showErrorHint(project, editor, message, getRefactoringName(), HelpID.ENCAPSULATE_FIELDS);
+          };
+          return callback;
+        }
+        EncapsulateFieldsDialog.EncapsulateFieldsContainer container = prepare( aClass, preselectedFields);
+        PsiClass finalAClass = aClass;
+        callback = () -> {
+          EncapsulateFieldsDialog dialog = getDialog(project, container);
+          if (!CommonRefactoringUtil.checkReadOnlyStatus(project, finalAClass)) return;
+          dialog.show();
+        };
+        return callback;
+      })
+      .finishOnUiThread(ModalityState.defaultModalityState(), callback -> {
+        if (callback != null) {
+          callback.run();
+        }
+      })
+      .expireWhen(() -> Arrays.stream(elements)
+        .map(t -> t instanceof PsiField field ? field.getContainingClass() : t)
+        .anyMatch(t -> t != null && !t.isValid()))
+      .submit(AppExecutorUtil.getAppExecutorService());
   }
 
-  protected static EncapsulateFieldsDialog createDialog(Project project, PsiClass aClass, HashSet<PsiField> preselectedFields) {
-    return new EncapsulateFieldsDialog(project, aClass, preselectedFields, new JavaEncapsulateFieldHelper());
+  @NotNull
+  private static EncapsulateFieldsDialog.EncapsulateFieldsContainer prepare(@NotNull  PsiClass aClass,
+                                                                            @NotNull Set<PsiField> preselectedFields) {
+    return EncapsulateFieldsDialog.EncapsulateFieldsContainer.create(aClass, preselectedFields, new JavaEncapsulateFieldHelper());
+  }
+
+  protected static EncapsulateFieldsDialog createDialog(@Nullable Project project,
+                                                        @NotNull PsiClass aClass,
+                                                        @NotNull Set<PsiField> preselectedFields) {
+    EncapsulateFieldsDialog.EncapsulateFieldsContainer container = prepare(aClass, preselectedFields);
+    return getDialog(project, container);
+  }
+
+  @NotNull
+  private static EncapsulateFieldsDialog getDialog(@Nullable  Project project,
+                                                   @NotNull EncapsulateFieldsDialog.EncapsulateFieldsContainer container) {
+    return new EncapsulateFieldsDialog(project, container);
   }
 
   @Override
