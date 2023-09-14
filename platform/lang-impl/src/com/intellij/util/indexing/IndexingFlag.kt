df659e137bdab2b18a33d4b64f40bcf0502c396c
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2023-09-14 15:01:13 +0200

    Equivalent refactoring to unify check
    GitOrigin-RevId: 43d97d2a7962a70e2c604fd84730cba97782c674

--- platform/lang-impl/src/com/intellij/util/indexing/IndexingFlag.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/IndexingFlag.kt
@@ -1,12 +1,12 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.indexing
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.VirtualFileWithId
 import com.intellij.openapi.vfs.newvfs.impl.VfsData
 import com.intellij.openapi.vfs.newvfs.impl.VirtualFileSystemEntry
 import com.intellij.util.application
+import com.intellij.util.asSafely
 import com.intellij.util.indexing.dependencies.FileIndexingStampService
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.TestOnly
@@ -26,61 +26,59 @@ object IndexingFlag {
     application.service<FileIndexingStampService>().invalidateAllStamps()
   }
 
+  private fun VirtualFile.asApplicable(): VirtualFileSystemEntry? {
+    return asSafely<VirtualFileSystemEntry>()
+  }
+
   @JvmStatic
   fun cleanProcessedFlagRecursively(file: VirtualFile) {
-    if (file !is VirtualFileSystemEntry) return
-    cleanProcessingFlag(file)
-    if (file.isDirectory()) {
-      for (child in file.cachedChildren) {
-        cleanProcessedFlagRecursively(child)
+    file.asApplicable()?.also { entry ->
+      cleanProcessingFlag(entry)
+      if (entry.isDirectory()) {
+        for (child in entry.cachedChildren) {
+          cleanProcessedFlagRecursively(child)
+        }
       }
     }
   }
 
   @JvmStatic
   fun cleanProcessingFlag(file: VirtualFile) {
-    if (file is VirtualFileSystemEntry) {
-      hashes.releaseHash(file.id)
-      file.indexedStamp = FileIndexingStampService.NULL_STAMP.toInt()
+    file.asApplicable()?.also { entry ->
+      hashes.releaseHash(entry.id)
+      entry.indexedStamp = FileIndexingStampService.NULL_STAMP.toInt()
     }
   }
 
   @JvmStatic
   fun setFileIndexed(file: VirtualFile, stamp: FileIndexingStampService.FileIndexingStamp) {
-    if (file is VirtualFileSystemEntry) {
-      file.indexedStamp = stamp.toInt()
-    }
+    file.asApplicable()?.also { entry -> entry.indexedStamp = stamp.toInt() }
   }
 
   @JvmStatic
   fun isFileIndexed(file: VirtualFile, stamp: FileIndexingStampService.FileIndexingStamp): Boolean {
     if (VfsData.isIsIndexedFlagDisabled()) {
-      return false;
+      return false
     }
-    return file is VirtualFileSystemEntry && file.indexedStamp == stamp.toInt()
+    return file.asApplicable()?.let { entry -> entry.indexedStamp == stamp.toInt() } ?: false
   }
 
   @JvmStatic
   fun getOrCreateHash(file: VirtualFile): Long {
-    if (file is VirtualFileSystemEntry) {
-      return hashes.getHash(file.id)
-    }
-    return nonExistentHash
+    return file.asApplicable()?.let { entry -> hashes.getHash(entry.id) } ?: nonExistentHash
   }
 
   @JvmStatic
   fun unlockFile(file: VirtualFile) {
-    if (file is VirtualFileWithId) {
-      hashes.releaseHash(file.id)
-    }
+    file.asApplicable()?.also { entry -> hashes.releaseHash(entry.id) }
   }
 
   @JvmStatic
   fun setIndexedIfFileWithSameLock(file: VirtualFile, lockObject: Long, stamp: FileIndexingStampService.FileIndexingStamp) {
-    if (file is VirtualFileSystemEntry) {
-      val hash = hashes.releaseHash(file.id)
-      if (file.indexedStamp != stamp.toInt()) {
-        file.indexedStamp = (if (hash == lockObject) stamp else FileIndexingStampService.NULL_STAMP).toInt()
+    file.asApplicable()?.also { entry ->
+      val hash = hashes.releaseHash(entry.id)
+      if (entry.indexedStamp != stamp.toInt()) {
+        entry.indexedStamp = (if (hash == lockObject) stamp else FileIndexingStampService.NULL_STAMP).toInt()
       }
     }
   }
