329889fe1b5f6d99abf53e8d02736aba09623700
Pavel Karateev <pavel.karateev@jetbrains.com>
2023-09-14 15:41:00 +0200

    [python] Fix PY-62800 `teamcity-messages` breaking `pytest` integration
    Passing `name=None` to old API was redundant,
    for the new one - mistaken.
    
    (cherry picked from commit 4a1f8a20d1f792623623a86d42c70c423e32ec91)
    
    IJ-CR-115023
    
    GitOrigin-RevId: f6f1330069cb211ec9578c7534023f9f0812a67c

--- python/helpers/pycharm/_jb_pytest_runner.py
+++ python/helpers/pycharm/_jb_pytest_runner.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     # to prevent "plugin already registered" problem we check it first
     plugins_to_load = []
     if not get_plugin_manager().hasplugin("pytest-teamcity"):
-        if "pytest-teamcity" not in map(lambda e: e.name, iter_entry_points(group='pytest11', name=None)):
+        if "pytest-teamcity" not in map(lambda e: e.name, iter_entry_points(group='pytest11')):
             plugins_to_load.append(pytest_plugin)
 
     args = sys.argv[1:]
