5f68cf6f000a6d68d1592bedc82885ac5e2a6cf1
Pavel Kargashinsky <pavel.kargashinsky@jetbrains.com>
2023-09-15 11:57:09 +0200

    [MPP][Tests] kt-232-1.9.20: add expect/actual is Beta warning in test-data
    GitOrigin-RevId: 40aed4c064569c17bcbe70d38f2dc557bfb2bfa1

--- plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/regress/ktij22345SyntheticJavaProperties/src/commonMain/kotlin/common.kt
+++ plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/regress/ktij22345SyntheticJavaProperties/src/commonMain/kotlin/common.kt
@@ -4,5 +4,5 @@
 class KotlinFile1(userPath: String): MyFile1(userPath)
 class KotlinFile2(userPath: String): MyFile2(userPath)
 
-expect open class MyFile1(userPath: String)
-expect open class MyFile2(userPath: String)
+<!HIGHLIGHTING("severity='WARNING'; descr='[EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING] 'expect'/'actual' classes (including interfaces, objects, annotations, enums, and 'actual' typealiases) are in Beta. You can use -Xexpect-actual-classes flag to suppress this warning. Also see: https://youtrack.jetbrains.com/issue/KT-61573'")!>expect<!> open class MyFile1(userPath: String)
+<!HIGHLIGHTING("severity='WARNING'; descr='[EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING] 'expect'/'actual' classes (including interfaces, objects, annotations, enums, and 'actual' typealiases) are in Beta. You can use -Xexpect-actual-classes flag to suppress this warning. Also see: https://youtrack.jetbrains.com/issue/KT-61573'")!>expect<!> open class MyFile2(userPath: String)
