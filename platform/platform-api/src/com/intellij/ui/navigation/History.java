e35d608ee1794e4136e9e92d69478fbd0d395c19
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2023-09-18 16:02:41 +0200

    IDEA-319606 1026 ms to call on EDT BackAction#update@MainToolbar (com.intellij.ide.actions.BackAction).
    making history thread-safe
    
    GitOrigin-RevId: 28836d7bdbdd06eada6dbf245c7442209ba33870

--- platform/platform-api/src/com/intellij/ui/navigation/History.java
+++ platform/platform-api/src/com/intellij/ui/navigation/History.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.DataKey;
 import com.intellij.openapi.util.ActionCallback;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -33,12 +34,12 @@ public final class History {
     pushPlace(place);
   }
 
-  public void pushPlace(@NotNull Place place) {
+  public synchronized void pushPlace(@NotNull Place place) {
     while (myCurrentPos >= 0 && myCurrentPos < myHistory.size() - 1) {
       myHistory.remove(myHistory.size() - 1);
     }
 
-    if (myHistory.size() > 0) {
+    if (!myHistory.isEmpty()) {
       final Place prev = myHistory.get(myHistory.size() - 1);
       if (prev.equals(place)) return;
 
@@ -50,7 +51,7 @@ public final class History {
     addPlace(place);
   }
 
-  private void addPlace(Place place) {
+  private synchronized void addPlace(Place place) {
     myHistory.add(place);
     myCurrentPos = myHistory.size() - 1;
   }
@@ -63,30 +64,24 @@ public final class History {
     pushPlace(checkPlace.cloneForElement(name, value));
   }
 
-  public Place getPlaceForElement(String name, String value) {
-    final Place checkPlace = getCheckPlace(name);
-    if (checkPlace == null) return new Place();
-
-    return checkPlace.cloneForElement(name, value);
-  }
-
   public void navigateTo(Place place) {
     myRoot.navigateTo(place, false);
   }
 
   public void back() {
-    int next = findValid(myCurrentPos, -1);
+    int next = findValid(-1);
     assert next != -1;
     goThere(next);
   }
 
-  private int findValid(int start, int increment) {
-    for (int idx = start + increment; idx >= 0 && idx < myHistory.size(); idx += increment) {
-      if (myRoot.isValid(myHistory.get(idx))) {
-        return idx;
+  private int findValid(int increment) {
+    List<Place> places = new ArrayList<>();
+    synchronized (this) {
+      for (int idx = myCurrentPos + increment; idx >= 0 && idx < myHistory.size(); idx += increment) {
+        places.add(myHistory.get(idx));
       }
     }
-    return -1;
+    return ContainerUtil.indexOf(places, place -> myRoot.isValid(place));
   }
 
   private void goThere(final int nextPos) {
@@ -96,7 +91,12 @@ public final class History {
     fireStarted(from, next);
     try {
       final ActionCallback callback = myRoot.navigateTo(next, false);
-      callback.doWhenDone(() -> myCurrentPos = nextPos).doWhenProcessed(() -> {
+      assert callback != null;
+      callback.doWhenDone(() -> {
+        synchronized (this) {
+          myCurrentPos = nextPos;
+        }
+      }).doWhenProcessed(() -> {
         myNavigatedNow = false;
         fireFinished(from, next);
       });
@@ -112,20 +112,20 @@ public final class History {
   }
 
   public boolean canGoBack() {
-    return findValid(myCurrentPos, -1) != -1;
+    return findValid(-1) != -1;
   }
 
   public void forward() {
-    int next = findValid(myCurrentPos, 1);
+    int next = findValid(1);
     assert next != -1;
     goThere(next);
   }
 
   public boolean canGoForward() {
-    return findValid(myCurrentPos, 1) != -1;
+    return findValid(1) != -1;
   }
 
-  public void clear() {
+  public synchronized void clear() {
     myHistory.clear();
     myCurrentPos = -1;
   }
@@ -136,7 +136,7 @@ public final class History {
     return result;
   }
 
-  private Place getCurrent() {
+  private synchronized Place getCurrent() {
     if (myCurrentPos >= 0 && myCurrentPos < myHistory.size()) {
       return myHistory.get(myCurrentPos);
     } else {
