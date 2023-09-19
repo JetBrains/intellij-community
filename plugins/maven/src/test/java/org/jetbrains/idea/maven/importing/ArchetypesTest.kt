86fa26e6b1db03f84423872d70e32852f2517a06
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-09-19 22:06:51 +0200

    [maven] convert ArchetypesTest to kotlin
    GitOrigin-RevId: aa9732e2616dc328bdf00a6261a637acb43419c0

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/ArchetypesTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/ArchetypesTest.kt
@@ -1,98 +1,96 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.maven.importing;
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.idea.maven.importing
 
-import com.intellij.execution.configurations.JavaParameters;
-import com.intellij.execution.configurations.ParametersList;
-import com.intellij.maven.testFramework.MavenTestCase;
-import org.jetbrains.idea.maven.execution.*;
-import org.junit.Assert;
+import com.intellij.maven.testFramework.MavenTestCase
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.idea.maven.execution.*
+import org.junit.Assert
+import java.io.File
+import java.lang.Boolean
+import java.util.*
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import kotlin.String
 
-import java.io.File;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
-public class ArchetypesTest extends MavenTestCase {
+class ArchetypesTest : MavenTestCase() {
+  override fun runInDispatchThread() = false
 
   /**
    * very time consumed test (uses the network and -U maven flag)
    */
-  public void ignoreTestGenerating() throws Exception {
-    File dir = new File(myDir.getPath(), "generated");
-    dir.mkdirs();
+  fun ignoreTestGenerating() = runBlocking {
+    val dir = File(myDir.path, "generated")
+    dir.mkdirs()
 
-    MavenRunnerParameters params = new MavenRunnerParameters(
-      false, dir.getPath(), (String)null,
-      Arrays.asList("org.apache.maven.plugins:maven-archetype-plugin:RELEASE:generate"),
-      Collections.emptyList()
-    );
+    val params = MavenRunnerParameters(
+      false, dir.path, null as String?,
+      mutableListOf("org.apache.maven.plugins:maven-archetype-plugin:RELEASE:generate"),
+      emptyList()
+    )
 
-    MavenRunnerSettings settings = new MavenRunnerSettings();
-    Map<String, String> props = new HashMap<>();
-    props.put("archetypeGroupId", "org.apache.maven.archetypes");
-    props.put("archetypeArtifactId", "maven-archetype-quickstart");
-    props.put("archetypeVersion", "1.0");
-    props.put("interactiveMode", "false");
-    props.put("groupId", "foo");
-    props.put("artifactId", "bar");
+    val settings = MavenRunnerSettings()
+    val props: MutableMap<String, String> = HashMap()
+    props["archetypeGroupId"] = "org.apache.maven.archetypes"
+    props["archetypeArtifactId"] = "maven-archetype-quickstart"
+    props["archetypeVersion"] = "1.0"
+    props["interactiveMode"] = "false"
+    props["groupId"] = "foo"
+    props["artifactId"] = "bar"
 
-    settings.setMavenProperties(props);
-    settings.setJreName(MavenRunnerSettings.USE_INTERNAL_JAVA);
-    CountDownLatch latch = new CountDownLatch(1);
-    MavenRunner.getInstance(myProject).run(params, settings, () -> latch.countDown());
+    settings.mavenProperties = props
+    settings.setJreName(MavenRunnerSettings.USE_INTERNAL_JAVA)
+    val latch = CountDownLatch(1)
+    MavenRunner.getInstance(myProject).run(params, settings) { latch.countDown() }
 
-    boolean tryAcquire = latch.await(20, TimeUnit.SECONDS);
-    assertTrue("Maven execution failed", tryAcquire);
-    assertTrue(new File(dir, "bar/pom.xml").exists());
+    val tryAcquire = latch.await(20, TimeUnit.SECONDS)
+    assertTrue("Maven execution failed", tryAcquire)
+    assertTrue(File(dir, "bar/pom.xml").exists())
   }
 
-  public void testVmParametersGenerating() throws Exception {
-    String mavenArchetypeGeneratorPlugin = "org.apache.maven.plugins:maven-archetype-plugin:RELEASE:generate";
-    MavenRunnerParameters params = new MavenRunnerParameters(
-      false, "generated", (String)null,
+  fun testVmParametersGenerating() = runBlocking {
+    val mavenArchetypeGeneratorPlugin = "org.apache.maven.plugins:maven-archetype-plugin:RELEASE:generate"
+    val params = MavenRunnerParameters(
+      false, "generated", null as String?,
       Arrays.asList(mavenArchetypeGeneratorPlugin),
-      Collections.emptyList()
-    );
+      emptyList()
+    )
 
-    String archetypeGroupId = "archetypeGroupId";
-    String archetypeArtifactId = "archetypeArtifactId";
-    String archetypeVersion = "archetypeVersion";
-    String interactiveMode = "interactiveMode";
-    String groupId = "groupId";
-    String artifactId = "artifactId";
+    val archetypeGroupId = "archetypeGroupId"
+    val archetypeArtifactId = "archetypeArtifactId"
+    val archetypeVersion = "archetypeVersion"
+    val interactiveMode = "interactiveMode"
+    val groupId = "groupId"
+    val artifactId = "artifactId"
 
-    String archetypeGroupIdValue = "org.apache.maven.archetypes";
-    String archetypeArtifactIdValue = "maven-archetype-quickstart";
-    String archetypeVersionValue = "1.0";
-    String groupIdValue = "foo";
-    String artifactIdValue = "bar";
+    val archetypeGroupIdValue = "org.apache.maven.archetypes"
+    val archetypeArtifactIdValue = "maven-archetype-quickstart"
+    val archetypeVersionValue = "1.0"
+    val groupIdValue = "foo"
+    val artifactIdValue = "bar"
 
-    MavenRunnerSettings settings = new MavenRunnerSettings();
-    Map<String, String> props = new HashMap<>();
-    props.put(archetypeGroupId, archetypeGroupIdValue);
-    props.put(archetypeArtifactId, archetypeArtifactIdValue);
-    props.put(archetypeVersion, archetypeVersionValue);
-    props.put(interactiveMode, Boolean.FALSE.toString());
-    props.put(groupId, groupIdValue);
-    props.put(artifactId, artifactIdValue);
+    val settings = MavenRunnerSettings()
+    val props: MutableMap<String, String> = HashMap()
+    props[archetypeGroupId] = archetypeGroupIdValue
+    props[archetypeArtifactId] = archetypeArtifactIdValue
+    props[archetypeVersion] = archetypeVersionValue
+    props[interactiveMode] = Boolean.FALSE.toString()
+    props[groupId] = groupIdValue
+    props[artifactId] = artifactIdValue
 
-    settings.setMavenProperties(props);
-    settings.setJreName(MavenRunnerSettings.USE_INTERNAL_JAVA);
-    MavenRunConfiguration configuration = (MavenRunConfiguration)MavenRunConfigurationType
+    settings.mavenProperties = props
+    settings.setJreName(MavenRunnerSettings.USE_INTERNAL_JAVA)
+    val configuration = MavenRunConfigurationType
       .createRunnerAndConfigurationSettings(null, settings, params, myProject)
-      .getConfiguration();
+      .getConfiguration() as MavenRunConfiguration
 
-    JavaParameters parameters = configuration.createJavaParameters(myProject);
-    ParametersList parametersList = parameters.getProgramParametersList();
-    Assert.assertEquals(archetypeGroupIdValue, parametersList.getPropertyValue(archetypeGroupId));
-    Assert.assertEquals(archetypeArtifactIdValue, parametersList.getPropertyValue(archetypeArtifactId));
-    Assert.assertEquals(archetypeVersionValue, parametersList.getPropertyValue(archetypeVersion));
-    Assert.assertEquals(Boolean.FALSE.toString(), parametersList.getPropertyValue(interactiveMode));
-    Assert.assertEquals(groupIdValue, parametersList.getPropertyValue(groupId));
-    Assert.assertEquals(artifactIdValue, parametersList.getPropertyValue(artifactId));
-    Assert.assertTrue(parametersList.hasParameter(mavenArchetypeGeneratorPlugin));
+    val parameters = configuration.createJavaParameters(myProject)
+    val parametersList = parameters.programParametersList
+    Assert.assertEquals(archetypeGroupIdValue, parametersList.getPropertyValue(archetypeGroupId))
+    Assert.assertEquals(archetypeArtifactIdValue, parametersList.getPropertyValue(archetypeArtifactId))
+    Assert.assertEquals(archetypeVersionValue, parametersList.getPropertyValue(archetypeVersion))
+    Assert.assertEquals(Boolean.FALSE.toString(), parametersList.getPropertyValue(interactiveMode))
+    Assert.assertEquals(groupIdValue, parametersList.getPropertyValue(groupId))
+    Assert.assertEquals(artifactIdValue, parametersList.getPropertyValue(artifactId))
+    Assert.assertTrue(parametersList.hasParameter(mavenArchetypeGeneratorPlugin))
   }
 }
