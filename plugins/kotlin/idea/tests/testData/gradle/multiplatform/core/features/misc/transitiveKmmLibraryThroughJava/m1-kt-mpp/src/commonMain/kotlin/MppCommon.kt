3a105be76bfa6e26b4b585ee199044d85be23b21
Pavel Kargashinsky <pavel.kargashinsky@jetbrains.com>
2023-09-19 11:16:19 +0200

    [MPP][Tests] Add expect/actual-warning in testTransitiveKmmLibrary test-data
    GitOrigin-RevId: 291f358ea0bc7c43e1c809715cf78e381eda5386

--- plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/features/misc/transitiveKmmLibraryThroughJava/m1-kt-mpp/src/commonMain/kotlin/MppCommon.kt
+++ plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/features/misc/transitiveKmmLibraryThroughJava/m1-kt-mpp/src/commonMain/kotlin/MppCommon.kt
@@ -3,4 +3,4 @@
 //endregion
 object MppCommon
 
-expect class MppExpectActual
+<!HIGHLIGHTING("severity='WARNING'; descr='[EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING] 'expect'/'actual' classes (including interfaces, objects, annotations, enums, and 'actual' typealiases) are in Beta. You can use -Xexpect-actual-classes flag to suppress this warning. Also see: https://youtrack.jetbrains.com/issue/KT-61573'")!>expect<!> class MppExpectActual
