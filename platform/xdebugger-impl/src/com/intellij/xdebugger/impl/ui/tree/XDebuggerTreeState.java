8b6e968d0c2694eaa4fc4a03c25f4c4339e88d39
Egor Ushakov <egor.ushakov@jetbrains.com>
2023-09-19 18:44:10 +0200

    IDEA-332792 Collections in expression view are collapsed sometimes
    GitOrigin-RevId: 4e13244cff2ef748a1c8ed6f3929461f51ea839c

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/tree/XDebuggerTreeState.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/tree/XDebuggerTreeState.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.xdebugger.impl.ui.tree;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -34,6 +34,7 @@ public final class XDebuggerTreeState {
     XDebuggerTreeRestorer restorer = null;
     if (myRootInfo != null) {
       restorer = new XDebuggerTreeRestorer(tree, myLastVisibleNodeRect);
+      tree.setCurrentRestorer(restorer);
       restorer.restore(tree.getRoot(), myRootInfo);
     }
     return restorer;
