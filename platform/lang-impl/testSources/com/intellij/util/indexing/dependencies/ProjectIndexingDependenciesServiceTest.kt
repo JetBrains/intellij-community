864ba5a1bbbaca7985e6178801395920306e31de
Andrei.Kuznetsov <andrei.kuznetsov@jetbrains.com>
2023-09-20 11:05:58 +0200

    IJPL-229: extract duplicate test code to dedicated `TestFactories` class
    GitOrigin-RevId: 7d6023022b4b721f3f8c7f16cad21ad531609b2d

--- platform/lang-impl/testSources/com/intellij/util/indexing/dependencies/ProjectIndexingDependenciesServiceTest.kt
+++ platform/lang-impl/testSources/com/intellij/util/indexing/dependencies/ProjectIndexingDependenciesServiceTest.kt
@@ -20,13 +20,13 @@ class ProjectIndexingDependenciesServiceTest {
   val temp = TempDirectory()
 
   private val testDisposable = Disposer.newDisposable()
-  private lateinit var appService: AppIndexingDependenciesService
   private lateinit var tmpDir: File
+  private lateinit var factory: TestFactories
 
   @Before
   fun setup() {
     tmpDir = temp.newDirectory("fileIndexingStamp")
-    appService = newAppIndexingDependenciesService(nonExistingFile("app"))
+    factory = TestFactories(tmpDir, testDisposable, useApplication = false)
   }
 
   @After
@@ -36,20 +36,20 @@ class ProjectIndexingDependenciesServiceTest {
 
   @Test
   fun `test no previous file`() {
-    val file = nonExistingFile("nonExistingFile")
-    newProjectIndexingDependenciesService(file)
+    val file = factory.nonExistingFile("nonExistingFile")
+    factory.newProjectIndexingDependenciesService(file)
     assertTrue(file.exists())
   }
 
   @Test
   fun `test corrupted file version`() {
-    val file = nonExistingFile()
+    val file = factory.nonExistingFile()
     FileOutputStream(file).use {
       it.write(ByteArray(3)) // 3 bytes is too few to read file version
     }
 
     try {
-      newProjectIndexingDependenciesService(file)
+      factory.newProjectIndexingDependenciesService(file)
       fail("Should throw exception, because 3 bytes is too few to read file version")
     }
     catch (ae: AssertionError) {
@@ -61,13 +61,13 @@ class ProjectIndexingDependenciesServiceTest {
 
   @Test
   fun `test corrupted indexingRequestId`() {
-    val file = nonExistingFile()
+    val file = factory.nonExistingFile()
     FileOutputStream(file).use {
       it.write(ByteArray(7)) // 4 bytes for file version + 3 bytes
     }
 
     try {
-      newProjectIndexingDependenciesService(file)
+      factory.newProjectIndexingDependenciesService(file)
       fail("Should throw exception, because 3 bytes is too few to read indexing stamp")
     }
     catch (ae: AssertionError) {
@@ -79,8 +79,8 @@ class ProjectIndexingDependenciesServiceTest {
 
   @Test
   fun `test invalidateAllStamps in project`() {
-    val file = nonExistingFile()
-    val inst = newProjectIndexingDependenciesService(file)
+    val file = factory.nonExistingFile()
+    val inst = factory.newProjectIndexingDependenciesService(file)
     val oldStamp = inst.getLatestIndexingRequestToken()
     inst.invalidateAllStamps()
     val newStamp = inst.getLatestIndexingRequestToken()
@@ -90,11 +90,11 @@ class ProjectIndexingDependenciesServiceTest {
 
   @Test
   fun `test invalidateAllStamps in project invalidates only that project`() {
-    val file1 = nonExistingFile("storage1")
-    val file2 = nonExistingFile("storage2")
+    val file1 = factory.nonExistingFile("storage1")
+    val file2 = factory.nonExistingFile("storage2")
 
-    val inst1 = newProjectIndexingDependenciesService(file1)
-    val inst2 = newProjectIndexingDependenciesService(file2)
+    val inst1 = factory.newProjectIndexingDependenciesService(file1)
+    val inst2 = factory.newProjectIndexingDependenciesService(file2)
 
     val oldStamp1 = inst1.getLatestIndexingRequestToken()
     val oldStamp2 = inst2.getLatestIndexingRequestToken()
@@ -110,10 +110,10 @@ class ProjectIndexingDependenciesServiceTest {
 
   @Test
   fun `test invalidateAllStamps in app`() {
-    val file = nonExistingFile()
-    val inst = newProjectIndexingDependenciesService(file)
+    val file = factory.nonExistingFile()
+    val inst = factory.newProjectIndexingDependenciesService(file)
     val oldStamp = inst.getLatestIndexingRequestToken()
-    appService.invalidateAllStamps()
+    factory.sharedAppService.invalidateAllStamps()
     val newStamp = inst.getLatestIndexingRequestToken()
 
     assertNotEquals(oldStamp, newStamp)
@@ -121,40 +121,15 @@ class ProjectIndexingDependenciesServiceTest {
 
   @Test
   fun `test service reload keeps state`() {
-    val file = nonExistingFile()
-    val inst1 = newProjectIndexingDependenciesService(file)
+    val file = factory.nonExistingFile()
+    val inst1 = factory.newProjectIndexingDependenciesService(file)
 
     inst1.invalidateAllStamps() // make some non-default sate
     val oldStamp = inst1.getLatestIndexingRequestToken()
 
-    val inst2 = newProjectIndexingDependenciesService(file)
+    val inst2 = factory.newProjectIndexingDependenciesService(file)
     val newStamp = inst2.getLatestIndexingRequestToken()
 
     assertEquals(oldStamp, newStamp)
-  }
-
-  private fun nonExistingFile(s: String = "storage"): File {
-    val file = tmpDir.resolve(s)
-    assertFalse(file.exists())
-    return file
-  }
-
-  private fun newAppIndexingDependenciesService(file: File): AppIndexingDependenciesService {
-    return AppIndexingDependenciesService(file.toPath()).also {
-      Disposer.register(testDisposable, it)
-      assertTrue(file.exists())
-    }
-  }
-
-  private fun newProjectIndexingDependenciesService(file: File): ProjectIndexingDependenciesService {
-    return newProjectIndexingDependenciesService(file, appService)
-  }
-
-  private fun newProjectIndexingDependenciesService(file: File,
-                                                    appService: AppIndexingDependenciesService): ProjectIndexingDependenciesService {
-    return ProjectIndexingDependenciesService(file.toPath(), appService).also {
-      Disposer.register(testDisposable, it)
-      assertTrue(file.exists())
-    }
   }
 }
