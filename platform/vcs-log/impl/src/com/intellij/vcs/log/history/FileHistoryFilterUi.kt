50fe8efc6aa3a6d8bbe300a5199c690b7575a3eb
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2023-09-21 00:34:04 +0200

    [file-history] convert FileHistoryFilterUi to kotlin
    GitOrigin-RevId: 54405f92c95aea3803d5f50b6b7be109e3f6be31

--- platform/vcs-log/impl/src/com/intellij/vcs/log/history/FileHistoryFilterUi.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/history/FileHistoryFilterUi.kt
@@ -1,30 +1,16 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.vcs.log.history;
+package com.intellij.vcs.log.history
 
-import com.intellij.openapi.vcs.FilePath;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.vcs.log.Hash;
-import com.intellij.vcs.log.VcsLogFilterCollection;
-import com.intellij.vcs.log.VcsLogFilterUi;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.vcs.FilePath
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.vcs.log.Hash
+import com.intellij.vcs.log.VcsLogFilterCollection
+import com.intellij.vcs.log.VcsLogFilterUi
+import com.intellij.vcs.log.history.FileHistoryFilterer.Companion.createFilters
 
-public class FileHistoryFilterUi implements VcsLogFilterUi {
-  private final @NotNull FilePath myPath;
-  private final @Nullable Hash myHash;
-  private final @NotNull VirtualFile myRoot;
-  private final @NotNull FileHistoryUiProperties myProperties;
-
-  public FileHistoryFilterUi(@NotNull FilePath path, @Nullable Hash hash, @NotNull VirtualFile root,
-                             @NotNull FileHistoryUiProperties properties) {
-    myPath = path;
-    myHash = hash;
-    myRoot = root;
-    myProperties = properties;
-  }
-
-  @Override
-  public @NotNull VcsLogFilterCollection getFilters() {
-    return FileHistoryFilterer.createFilters(myPath, myHash, myRoot, myProperties.get(FileHistoryUiProperties.SHOW_ALL_BRANCHES));
+class FileHistoryFilterUi(private val path: FilePath, private val hash: Hash?, private val root: VirtualFile,
+                          private val properties: FileHistoryUiProperties) : VcsLogFilterUi {
+  override fun getFilters(): VcsLogFilterCollection {
+    return createFilters(path, hash, root, properties.get(FileHistoryUiProperties.SHOW_ALL_BRANCHES))
   }
 }
