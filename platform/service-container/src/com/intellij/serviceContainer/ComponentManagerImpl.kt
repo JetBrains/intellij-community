4bbc7858aec930e17445822f7b72721f2f72e82d
Ilya.Usov <Ilya.Usov@jetbrains.com>
2023-09-20 17:49:24 +0200

    Move FrontendActionsRegistrationHost to app level
    GitOrigin-RevId: 393e38dc24ec49a5a81d4096a807aca7d374a89d

--- platform/service-container/src/com/intellij/serviceContainer/ComponentManagerImpl.kt
+++ platform/service-container/src/com/intellij/serviceContainer/ComponentManagerImpl.kt
@@ -1593,7 +1593,6 @@ private val servicePreloadingAllowListForNonCorePlugin = java.util.Set.of(
   "com.jetbrains.rdserver.editors.BackendEditorService",
   "com.jetbrains.rdserver.toolWindow.BackendServerToolWindowManager",
   "com.jetbrains.rdserver.toolbar.CWMHostClosedToolbarNotification",
-  "com.jetbrains.rdclient.actions.cwm.FrontendActionsRegistrationHost",
   "com.jetbrains.rdclient.client.FrontendProjectSessionsManager",
   "com.jetbrains.rider.projectView.workspace.impl.RiderWorkspaceModel",
 )
