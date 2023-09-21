60946bc98a0ea5c1913d92d4b754ba0e17f7a42f
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-09-21 12:47:54 +0200

    [kotlin] update bootstrap version to 2.0
    GitOrigin-RevId: 9c4ea18cdad530e2429d134b5cdc8eae482bddd4

--- plugins/kotlin/util/project-model-updater/src/org/jetbrains/tools/model/updater/kotlincLibraries.kt
+++ plugins/kotlin/util/project-model-updater/src/org/jetbrains/tools/model/updater/kotlincLibraries.kt
@@ -5,7 +5,7 @@ import org.jetbrains.tools.model.updater.GeneratorPreferences.ArtifactMode
 import org.jetbrains.tools.model.updater.impl.*
 
 private const val ktGroup = "org.jetbrains.kotlin"
-private const val BOOTSTRAP_VERSION = "1.9.255"
+private const val BOOTSTRAP_VERSION = "2.0.255"
 
 // see .idea/jarRepositories.xml
 private val KOTLIN_IDE_DEPS_REPOSITORY = JpsRemoteRepository(
