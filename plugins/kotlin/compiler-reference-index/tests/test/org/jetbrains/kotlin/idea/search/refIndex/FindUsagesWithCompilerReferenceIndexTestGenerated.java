fdb948798fbcbf345b6d7074f82244b96a4f4c20
Jaebaek Seo <jaebaek@google.com>
2023-09-21 16:43:33 -0700

    Delete find-usage test for circular class inheritance
    We added a find-usage test to check a corner case for circular class
    inheritance, which fails because "Loop in supertype" breaks `isInheritor`
    check for both K1 and K2. This commit deletes the problematic test case.
    
    ^KTIJ-26993

--- plugins/kotlin/compiler-reference-index/tests/test/org/jetbrains/kotlin/idea/search/refIndex/FindUsagesWithCompilerReferenceIndexTestGenerated.java
+++ plugins/kotlin/compiler-reference-index/tests/test/org/jetbrains/kotlin/idea/search/refIndex/FindUsagesWithCompilerReferenceIndexTestGenerated.java
@@ -3735,19 +3735,6 @@ public abstract class FindUsagesWithCompilerReferenceIndexTestGenerated extends 
 
             @RunWith(JUnit3RunnerWithInners.class)
             @TestMetadata("../../idea/tests/testData/findUsages/java/findJavaClassUsages")
-            public static class TestJKClassDerivedClassesCircular extends AbstractFindUsagesWithCompilerReferenceIndexTest {
-                private void runTest(String testDataFilePath) throws Exception {
-                    KotlinTestUtils.runTest(this::doTest, this, testDataFilePath);
-                }
-
-                @TestMetadata("JKClassDerivedClassesCircular.0.java")
-                public void testJKClassDerivedClassesCircular() throws Exception {
-                    runTest("../../idea/tests/testData/findUsages/java/findJavaClassUsages/JKClassDerivedClassesCircular.0.java");
-                }
-            }
-
-            @RunWith(JUnit3RunnerWithInners.class)
-            @TestMetadata("../../idea/tests/testData/findUsages/java/findJavaClassUsages")
             public static class TestJKClassDerivedInnerClasses extends AbstractFindUsagesWithCompilerReferenceIndexTest {
                 private void runTest(String testDataFilePath) throws Exception {
                     KotlinTestUtils.runTest(this::doTest, this, testDataFilePath);
