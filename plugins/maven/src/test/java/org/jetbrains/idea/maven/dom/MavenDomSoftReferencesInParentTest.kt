2ad7006933c200adacbcd6eccfa036edae90a640
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-09-23 00:02:28 +0200

    [maven] add VFS event logging to troubleshoot testHighlightSourceDirectory()
    GitOrigin-RevId: 705a3a41af2605a68e8284fb298c940776590ddb

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/dom/MavenDomSoftReferencesInParentTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/dom/MavenDomSoftReferencesInParentTest.kt
@@ -16,7 +16,12 @@
 package org.jetbrains.idea.maven.dom
 
 import com.intellij.maven.testFramework.MavenDomTestCase
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.openapi.vfs.newvfs.BulkFileListener
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent
 import kotlinx.coroutines.runBlocking
+import org.jetbrains.idea.maven.utils.MavenLog
 import org.junit.Test
 
 class MavenDomSoftReferencesInParentTest : MavenDomTestCase() {
@@ -39,6 +44,17 @@ class MavenDomSoftReferencesInParentTest : MavenDomTestCase() {
 
   @Test
   fun testHighlightSourceDirectory() = runBlocking {
+    ApplicationManager.getApplication().messageBus.connect(testRootDisposable)
+      .subscribe(VirtualFileManager.VFS_CHANGES, object : BulkFileListener {
+        override fun before(events: MutableList<out VFileEvent>) {
+          MavenLog.LOG.warn("before $events")
+        }
+
+        override fun after(events: MutableList<out VFileEvent>) {
+          MavenLog.LOG.warn("after $events")
+        }
+      })
+
     importProjectAsync("""
                     <groupId>test</groupId>
                     <artifactId>project</artifactId>
