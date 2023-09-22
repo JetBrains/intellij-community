b89851cf097e71040be0213c8d09c4508a793ed6
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2023-09-22 17:52:37 +0400

    [kotlin k2] DestructuringWrongNameInspection: move a method
    ^KTIJ-21135
    IJ-CR-113137
    
    GitOrigin-RevId: 842c119a6c24f92c993efac5b2afa4c2b0b1a144

--- plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/declarations/DestructuringWrongNameInspection.kt
+++ plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/declarations/DestructuringWrongNameInspection.kt
@@ -5,15 +5,9 @@ import com.intellij.codeInsight.daemon.impl.quickfix.RenameElementFix
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
-import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
-import org.jetbrains.kotlin.analysis.api.analyze
-import org.jetbrains.kotlin.analysis.api.symbols.KtNamedClassOrObjectSymbol
-import org.jetbrains.kotlin.analysis.api.types.KtFlexibleType
-import org.jetbrains.kotlin.analysis.api.types.KtNonErrorClassType
-import org.jetbrains.kotlin.analysis.api.types.KtTypeNullability
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
+import org.jetbrains.kotlin.idea.codeinsight.utils.getParameterNames
 import org.jetbrains.kotlin.psi.KtDestructuringDeclaration
-import org.jetbrains.kotlin.psi.KtParameter
 import org.jetbrains.kotlin.psi.destructuringDeclarationVisitor
 
 internal class DestructuringWrongNameInspection : LocalInspectionTool() {
@@ -38,38 +32,5 @@ internal class DestructuringWrongNameInspection : LocalInspectionTool() {
                     holder.registerProblem(variable, message)
                 }
             }
-    }
-
-    private fun getParameterNames(declaration: KtDestructuringDeclaration): List<String>? {
-        return analyze(declaration) {
-            val type = getClassType(declaration) ?: return null
-            if (type.nullability != KtTypeNullability.NON_NULLABLE) return null
-            val classSymbol = type.expandedClassSymbol
-
-            if (classSymbol is KtNamedClassOrObjectSymbol && classSymbol.isData) {
-                val constructorSymbol = classSymbol.getDeclaredMemberScope()
-                    .getConstructors()
-                    .find { it.isPrimary }
-                    ?: return null
-
-                constructorSymbol.valueParameters.map { it.name.asString() }
-            } else null
-        }
-    }
-
-    context(KtAnalysisSession)
-    private fun getClassType(declaration: KtDestructuringDeclaration): KtNonErrorClassType? {
-        val initializer = declaration.initializer
-        val parentAsParameter = declaration.parent as? KtParameter
-        val type = when {
-            initializer != null -> initializer.getKtType()
-            parentAsParameter != null -> parentAsParameter.getParameterSymbol().returnType
-            else -> null
-        }
-        return when (type) {
-            is KtNonErrorClassType -> type
-            is KtFlexibleType -> type.lowerBound as? KtNonErrorClassType
-            else -> null
-        }
     }
 }
