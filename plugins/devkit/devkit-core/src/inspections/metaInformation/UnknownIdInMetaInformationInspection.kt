1b1e3f6df0c51562aa45a20e8a363a11f1c6bf72
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2023-09-25 21:58:38 +0200

    IJ-CR-114480 QD-6980 Addressing review. Rewrite checking if id is known.
    GitOrigin-RevId: 6485588f554c30a5fe692a1a2e489bf5d67ddba7

--- plugins/devkit/devkit-core/src/inspections/metaInformation/UnknownIdInMetaInformationInspection.kt
+++ plugins/devkit/devkit-core/src/inspections/metaInformation/UnknownIdInMetaInformationInspection.kt
@@ -12,25 +12,21 @@ import com.intellij.psi.PsiElementVisitor
 import com.intellij.util.xml.DomElement
 import com.intellij.util.xml.DomManager
 import org.jetbrains.idea.devkit.DevKitBundle
-import org.jetbrains.idea.devkit.dom.Extension
-import org.jetbrains.idea.devkit.dom.index.ExtensionPointIndex
 import org.jetbrains.idea.devkit.inspections.DevKitInspectionUtil
 import org.jetbrains.idea.devkit.util.DevKitDomUtil
-import org.jetbrains.idea.devkit.util.PluginRelatedLocatorsUtils
-import org.jetbrains.idea.devkit.util.locateExtensionsByExtensionPoint
+import org.jetbrains.idea.devkit.util.processExtensionDeclarations
+import java.util.concurrent.atomic.AtomicBoolean
 
 
 class UnknownIdInMetaInformationInspection : LocalInspectionTool() {
   override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
     if (!isAllowed(holder)) return PsiElementVisitor.EMPTY_VISITOR
-    val knownIds = collectKnownIds(holder).toSet()
     return object : JsonElementVisitor() {
       override fun visitObject(o: JsonObject) {
         val idProperty = o.findProperty("id") ?: return
         val valueElement = idProperty.value as? JsonStringLiteral
         val id = valueElement?.value ?: return
-
-        if (id !in knownIds) {
+        if (!isKnownId(id, holder)) {
           holder.registerProblem(
             valueElement,
             DevKitBundle.message("inspections.meta.information.unknown.inspection.id", id),
@@ -44,33 +40,32 @@ class UnknownIdInMetaInformationInspection : LocalInspectionTool() {
   fun isAllowed(holder: ProblemsHolder): Boolean {
     return DevKitInspectionUtil.isAllowed(holder.file) && isMetaInformationFile(holder.file)
   }
-
-  private fun collectKnownIds(holder: ProblemsHolder): List<String> {
-    return collectKnownIds(holder, "com.intellij.localInspection") +
-           collectKnownIds(holder, "com.intellij.globalInspection")
+  private fun isKnownId(id: String, holder: ProblemsHolder): Boolean {
+    val className = getShortName(id) + "Inspection"
+    return isKnownId(id, id, holder) || isKnownId(id, className, holder)
   }
-
-  private fun collectKnownIds(holder: ProblemsHolder, epName: String): List<String> {
+  private fun isKnownId(id: String, textToSearch: String, holder: ProblemsHolder): Boolean {
     val project = holder.project
-    val extensionPoint = ExtensionPointIndex.findExtensionPoint(
-      project,
-      PluginRelatedLocatorsUtils.getCandidatesScope(project),
-      epName
-    ) ?: return emptyList()
-
     val manager = DomManager.getDomManager(project)
+    val found = AtomicBoolean(false)
 
-    return locateExtensionsByExtensionPoint(extensionPoint).mapNotNull { candidate ->
-      val element = candidate.pointer.getElement() ?: return@mapNotNull null
-      val domElement = manager.getDomElement(element) ?: return@mapNotNull null
-      if (domElement is Extension) {
-        getShortName(domElement)
+    processExtensionDeclarations(textToSearch, project, false) { extension, tag ->
+      val extensionName = extension.extensionPoint?.effectiveQualifiedName
+      if (extensionName != "com.intellij.localInspection" && extensionName != "com.intellij.globalInspection") {
+        return@processExtensionDeclarations true
       }
-      else null
+      val domElement = manager.getDomElement(tag) ?: return@processExtensionDeclarations true
+      if (id == getShortNameByElement(domElement)) {
+        found.set(true)
+        return@processExtensionDeclarations false
+      }
+      return@processExtensionDeclarations true
     }
+
+    return found.get()
   }
 
-  private fun getShortName(domElement: DomElement): String? {
+  private fun getShortNameByElement(domElement: DomElement): String? {
     val shortNameAttr = DevKitDomUtil.getAttribute(domElement, "shortName")
     if (shortNameAttr != null && shortNameAttr.value != null) return shortNameAttr.rawText
     val implementationClass = DevKitDomUtil.getAttribute(domElement, "implementationClass")?.rawText
