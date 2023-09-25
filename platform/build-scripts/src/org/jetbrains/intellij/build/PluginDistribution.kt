37e8bb5351e39621b0832ba137c482701cd6a9e7
Kirill.Krylov <kirill.krylov@jetbrains.com>
2023-09-25 10:08:37 +0200

    [build-scripts] ML-1276 add kdoc for PluginDistribution values
    GitOrigin-RevId: 9ae0f298e9e270c11f4e33c93f96e50e0e8f0d59

--- platform/build-scripts/src/org/jetbrains/intellij/build/PluginDistribution.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/PluginDistribution.kt
@@ -4,8 +4,16 @@
 package org.jetbrains.intellij.build
 
 enum class PluginDistribution {
+  /**
+   * Use [PluginDistribution.ALL] if the plugin should be included all distribution for EAP, Nightly, Release and all other distributions.
+   */
   ALL,
+  /**
+   * Use [PluginDistribution.NOT_FOR_RELEASE] if the plugin should be included in distribution for EAP and Nightly builds only (non Release).
+   */
   NOT_FOR_RELEASE,
+  /**
+   * Use [PluginDistribution.NOT_FOR_PUBLIC_BUILDS] if the plugin should be included in distribution for nightly builds only (non EAP, non Release).
+   */
   NOT_FOR_PUBLIC_BUILDS,
-  ;
 }
