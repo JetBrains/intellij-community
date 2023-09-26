6648814f5c2c6a6ce4affa20b4108dd718aab1e5
Olga Klisho <olga.klisho@jetbrains.com>
2023-09-26 14:50:15 +0200

    (IDEA-333128) fixing trace scenarios from properties
    GitOrigin-RevId: 58253a15820fbf61ee604da54fdffe48862bf858

--- platform/platform-impl/src/com/intellij/diagnostic/logs/LogLevelConfigurationManager.kt
+++ platform/platform-impl/src/com/intellij/diagnostic/logs/LogLevelConfigurationManager.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.diagnostic.logs
 
+import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.components.*
 import com.intellij.openapi.diagnostic.Logger
 import kotlinx.serialization.Serializable
@@ -20,6 +21,13 @@ class LogLevelConfigurationManager : SerializablePersistentStateComponent<LogLev
 
   companion object {
     private val LOG = Logger.getInstance(LogLevelConfigurationManager::class.java)
+
+    private const val LOG_DEBUG_CATEGORIES = "log.debug.categories"
+    private const val LOG_TRACE_CATEGORIES = "log.trace.categories"
+    private const val LOG_ALL_CATEGORIES = "log.all.categories"
+    private const val LOG_DEBUG_CATEGORIES_SYSTEM_PROPERTY = "idea.$LOG_DEBUG_CATEGORIES"
+    private const val LOG_TRACE_CATEGORIES_SYSTEM_PROPERTY = "idea.$LOG_TRACE_CATEGORIES"
+    private const val LOG_ALL_CATEGORIES_SYSTEM_PROPERTY = "idea.$LOG_ALL_CATEGORIES"
 
     @JvmStatic
     fun getInstance(): LogLevelConfigurationManager = service()
@@ -50,12 +58,12 @@ class LogLevelConfigurationManager : SerializablePersistentStateComponent<LogLev
         DebugLogLevel.ALL -> Level.ALL
       }
 
-      val trimmed = logCategory.category.trimStart('#')
-
+      val trimmed = logCategory.category.trim('#').split(':')[0]
       // IDEA-297747 Convention for categories naming is not clear, so set logging for both with '#' and without '#'
       addLogger(logCategory.category, loggerLevel, level)
       addLogger("#$trimmed", loggerLevel, level)
       addLogger(trimmed, loggerLevel, level)
+      logCategory.category = trimmed
     }
     return filteredCategories
   }
@@ -108,7 +116,37 @@ class LogLevelConfigurationManager : SerializablePersistentStateComponent<LogLev
 
   override fun loadState(state: State) {
     super.loadState(state)
-    applyCategories(state.categories)
+    applyCategories(state.categories.toList())
+
+    val categories = mutableListOf<LogCategory>()
+    categories.addAll(getSavedCategories())
+
+    // add categories from system properties (e.g., for tests on CI server)
+    categories.addAll(fromString(System.getProperty(LOG_DEBUG_CATEGORIES_SYSTEM_PROPERTY), DebugLogLevel.DEBUG))
+    categories.addAll(fromString(System.getProperty(LOG_TRACE_CATEGORIES_SYSTEM_PROPERTY), DebugLogLevel.TRACE))
+    categories.addAll(fromString(System.getProperty(LOG_ALL_CATEGORIES_SYSTEM_PROPERTY), DebugLogLevel.ALL))
+    addCategories(categories)
+  }
+
+
+  private fun getSavedCategories(): List<LogCategory> {
+    val properties = PropertiesComponent.getInstance()
+    return fromString(properties.getValue(LOG_DEBUG_CATEGORIES), DebugLogLevel.DEBUG) +
+           fromString(properties.getValue(LOG_TRACE_CATEGORIES), DebugLogLevel.TRACE) +
+           fromString(properties.getValue(LOG_ALL_CATEGORIES), DebugLogLevel.ALL)
+  }
+
+  private fun fromString(text: String?, level: DebugLogLevel): List<LogCategory> {
+    if (text == null) {
+      return emptyList()
+    }
+
+    val byNewlines = text.lines()
+    val byCommas = text.split(',')
+    if (byCommas.size > 1 && byNewlines.size > 1) {
+      error("Do not mix commas and newlines as category separators: $text")
+    }
+    val categories = if (byCommas.size > byNewlines.size) byCommas else byNewlines
+    return categories.mapNotNull { if (it.isBlank()) null else LogCategory(it, level) }
   }
 }
-
