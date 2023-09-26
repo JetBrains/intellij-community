45955de5665da1c6c664e281cf0d6d063c5cf21a
Bas Leijdekkers <basleijdekkers@gmail.com>
2023-09-26 15:27:27 +0200

    Java: convert test to check highlighting as quick fix
    GitOrigin-RevId: 75e88ded9aeedbe503a5e9e63a5dcf7ed6ea97c9

--- java/java-tests/testSrc/com/siyeh/ig/fixes/style/UnnecessarilyQualifiedInnerClassAccessFixTest.java
+++ java/java-tests/testSrc/com/siyeh/ig/fixes/style/UnnecessarilyQualifiedInnerClassAccessFixTest.java
@@ -1,81 +1,60 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.siyeh.ig.fixes.style;
 
-import com.intellij.openapi.application.PathManager;
-import com.intellij.pom.java.LanguageLevel;
-import com.intellij.testFramework.builders.JavaModuleFixtureBuilder;
 import com.siyeh.ig.BaseInspection;
-import com.siyeh.ig.IGQuickFixesTestCase;
+import com.siyeh.ig.LightJavaInspectionTestCase;
 import com.siyeh.ig.style.UnnecessarilyQualifiedInnerClassAccessInspection;
 
-public class UnnecessarilyQualifiedInnerClassAccessFixTest extends IGQuickFixesTestCase {
+public class UnnecessarilyQualifiedInnerClassAccessFixTest extends LightJavaInspectionTestCase {
 
   public void testRemoveQualifier() {
-    doTest("Remove qualifier",
-           """
+    doTest("""
              class X {
-               /**/X/*1*/./*2*/Y foo;
+               /*'Y' is unnecessarily qualified with 'X'*//*_*/X/**//* 1*/./* 2*/Y foo;
               \s
                class Y{}
-             }""",
-
-           """
-             class X {
-               /*2*//*1*/ Y foo;
-              \s
-               class Y{}
-             }"""
-    );
+             }""");
+    checkQuickFix("Remove qualifier", """
+      class X {
+        /* 2*//* 1*/ Y foo;
+       \s
+        class Y{}
+      }""");
   }
 
   public void testRemoveQualifierWithImport() {
-    doTest("Remove qualifier",
-           """
+    doTest("""
              package p;
              import java.util.List;
-             abstract class X implements List</**/X.Y> {
+             abstract class X implements List</*'Y' is unnecessarily qualified with 'X'*//*_*/X/**/.Y> {
                class Y{}
-             }""",
+             }""");
+    checkQuickFix("Remove qualifier", """
+      package p;
+      import p.X.Y;
 
-           """
-             package p;
-             import p.X.Y;
-
-             import java.util.List;
-             abstract class X implements List<Y> {
-               class Y{}
-             }"""
+      import java.util.List;
+      abstract class X implements List<Y> {
+        class Y{}
+      }"""
     );
   }
 
   public void testUnnecessarilyQualifiedInnerClassAccess() {
-    doTest("Fix all 'Unnecessarily qualified inner class access' problems in file");
+    doTest();
+    checkQuickFixAll();
   }
 
   public void testNoImports() {
     final UnnecessarilyQualifiedInnerClassAccessInspection inspection = new UnnecessarilyQualifiedInnerClassAccessInspection();
     inspection.ignoreReferencesNeedingImport = true;
     myFixture.enableInspections(inspection);
-    doTest("Fix all 'Unnecessarily qualified inner class access' problems in file");
-  }
-
-  @Override
-  protected String getTestDataPath() {
-    return PathManager.getCommunityHomePath() + "/java/java-tests/testData/ig/com/siyeh/igtest/";
-  }
-
-  @Override
-  protected String getRelativePath() {
-    return "style/unnecessarily_qualified_inner_class_access";
+    doTest();
+    checkQuickFixAll();
   }
 
   @Override
   protected BaseInspection getInspection() {
     return new UnnecessarilyQualifiedInnerClassAccessInspection();
-  }
-
-  @Override
-  protected void tuneFixture(JavaModuleFixtureBuilder builder) {
-    builder.setLanguageLevel(LanguageLevel.HIGHEST);
   }
 }
