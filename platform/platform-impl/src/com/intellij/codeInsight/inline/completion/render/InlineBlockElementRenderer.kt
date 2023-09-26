4b1522513af2460688e445b857e52405bfaced58
Oleg Taratukhin <oleg.taratukhin@jetbrains.com>
2023-09-26 22:09:38 +0200

    [inline] render with precise multi-line block height
    GitOrigin-RevId: 9b121e376a679ce3425a4136c7b4eef4198f3f0e

--- platform/platform-impl/src/com/intellij/codeInsight/inline/completion/render/InlineBlockElementRenderer.kt
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/render/InlineBlockElementRenderer.kt
@@ -7,35 +7,24 @@ import com.intellij.openapi.editor.EditorCustomElementRenderer
 import com.intellij.openapi.editor.Inlay
 import com.intellij.openapi.editor.markup.TextAttributes
 import org.jetbrains.annotations.ApiStatus
-import java.awt.FontMetrics
 import java.awt.Graphics
 import java.awt.Rectangle
 
 @ApiStatus.Experimental
 class InlineBlockElementRenderer(private val editor: Editor, val lines: List<String>) : EditorCustomElementRenderer, InlineCompletionElementRenderer {
 
-  private val width = editor.contentComponent.getFontMetrics(InlineFontUtils.font(editor)).stringWidth(lines.maxBy { it.length })
+  private val width = editor
+    .contentComponent
+    .getFontMetrics(InlineFontUtils.font(editor))
+    .stringWidth(lines.maxBy { it.length })
 
-  override fun calcWidthInPixels(inlay: Inlay<*>): Int {
-    return width
-  }
+  override fun calcWidthInPixels(inlay: Inlay<*>) = width
 
-  override fun calcHeightInPixels(inlay: Inlay<*>): Int {
-    val lineSpacing = editor.getColorsScheme().getLineSpacing()
-    val fontHeight = editor.contentComponent.getFontMetrics(InlineFontUtils.font(editor)).height
-    return (fontHeight * lineSpacing * lines.size).toInt()
-  }
+  override fun calcHeightInPixels(inlay: Inlay<*>) = editor.lineHeight * lines.size
 
   override fun paint(inlay: Inlay<*>, g: Graphics, targetRegion: Rectangle, textAttributes: TextAttributes) {
     g.color = InlineFontUtils.color(editor)
     g.font = InlineFontUtils.font(editor)
-    val lineSpacing = editor.getColorsScheme().getLineSpacing()
-    val fontMetrics: FontMetrics = g.fontMetrics
-    val lineHeight: Int = fontMetrics.height
-    var y: Int = targetRegion.y + editor.ascent
-    lines.forEach { line ->
-      g.drawString(line, 0, y)
-      y += (lineHeight * lineSpacing).toInt()
-    }
+    lines.forEachIndexed { i, it -> g.drawString(it, 0, targetRegion.y + editor.ascent + i * editor.lineHeight) }
   }
 }
