4930afe1d2b12c1d0fcfeddb65569dbc0f1e5b6b
Alexander Koshevoy <alexander.koshevoy@jetbrains.com>
2023-09-26 12:59:04 +0200

    Optimize imports in UnixPythonSdkFlavor.java
    GitOrigin-RevId: a83bf3d165fea253c79644486a41d566318cb325

--- python/src/com/jetbrains/python/sdk/flavors/UnixPythonSdkFlavor.java
+++ python/src/com/jetbrains/python/sdk/flavors/UnixPythonSdkFlavor.java
@@ -18,7 +18,6 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 
 public final class UnixPythonSdkFlavor extends CPythonSdkFlavor<PyFlavorData.Empty> {
