4ba2279967018f597b958ea85d7d00928bc05418
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-09-26 13:20:11 +0200

    [java-inspections] RedundantEmbeddedExpressionInspection: extract isEmbeddedLiteralRedundant and inlineEmbeddedExpression to reuse
    GitOrigin-RevId: b32e290c62f237edca4c300f10de5a4b2ccf66bc

--- java/java-impl/src/com/siyeh/ig/redundancy/RedundantEmbeddedExpressionInspection.java
+++ java/java-impl/src/com/siyeh/ig/redundancy/RedundantEmbeddedExpressionInspection.java
@@ -5,7 +5,6 @@ import com.intellij.codeInsight.daemon.impl.analysis.HighlightingFeature;
 import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool;
 import com.intellij.codeInspection.CleanupLocalInspectionTool;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.codeInspection.actions.CleanupInspectionIntention;
 import com.intellij.modcommand.ActionContext;
 import com.intellij.modcommand.ModPsiUpdater;
 import com.intellij.modcommand.PsiUpdateModCommandAction;
@@ -20,6 +19,9 @@ import com.intellij.util.ObjectUtils;
 import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.psiutils.CommentTracker;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
 
 public final class RedundantEmbeddedExpressionInspection extends AbstractBaseJavaLocalInspectionTool implements CleanupLocalInspectionTool {
   @Override
@@ -32,12 +34,9 @@ public final class RedundantEmbeddedExpressionInspection extends AbstractBaseJav
         if (template == null) return;
         if (!JavaPsiStringTemplateUtil.isStrTemplate(expression.getProcessor())) return;
         for (PsiExpression embeddedExpression : template.getEmbeddedExpressions()) {
-          if (embeddedExpression instanceof PsiLiteralExpression literal && !literal.isTextBlock()) {
-            Object value = literal.getValue();
-            if (value instanceof String || String.valueOf(literal.getValue()).equals(literal.getText())) {
-              holder.problem(embeddedExpression, InspectionGadgetsBundle.message("inspection.redundant.embedded.expression.message.literal"))
-                .fix(new RemoveEmbeddedExpressionFix(literal)).register();
-            }
+          if (isEmbeddedLiteralRedundant(embeddedExpression)) {
+            holder.problem(embeddedExpression, InspectionGadgetsBundle.message("inspection.redundant.embedded.expression.message.literal"))
+              .fix(new RemoveEmbeddedExpressionFix(embeddedExpression)).register();
           }
           else if (embeddedExpression instanceof PsiEmptyExpressionImpl) {
             String text = template.getText();
@@ -56,6 +55,76 @@ public final class RedundantEmbeddedExpressionInspection extends AbstractBaseJav
     };
   }
 
+  /**
+   * Determines if an embedded literal expression is redundant and can be inlined.
+   *
+   * @param embeddedExpression the embedded expression to check
+   * @return true if the embedded literal expression is redundant, false otherwise
+   */
+  public static boolean isEmbeddedLiteralRedundant(@Nullable PsiExpression embeddedExpression) {
+    if (embeddedExpression instanceof PsiLiteralExpression literal && !literal.isTextBlock()) {
+      Object value = literal.getValue();
+      return value instanceof String || String.valueOf(literal.getValue()).equals(literal.getText());
+    }
+    return false;
+  }
+
+  /**
+   * Inlines the given embedded expression within a template expression.
+   *
+   * @param expression the embedded expression to be inlined
+   * @return updated template expression; null if inlining fails (e.g., there's no parent template) 
+   */
+  public static @Nullable PsiTemplateExpression inlineEmbeddedExpression(@NotNull PsiExpression expression) {
+    PsiTemplate template = (PsiTemplate)expression.getParent();
+    if (template == null) return null;
+    PsiTemplateExpression templateExpression = ObjectUtils.tryCast(template.getParent(), PsiTemplateExpression.class);
+    if (templateExpression == null) return null;
+    PsiElement[] children = template.getChildren();
+    int index = ArrayUtil.indexOf(children, expression);
+    if (index == -1) return null;
+    StringBuilder newTemplate = new StringBuilder();
+    for (PsiElement child : templateExpression.getChildren()) {
+      if (child == template) break;
+      newTemplate.append(child.getText());
+    }
+    boolean suffixProcessed = false;
+    boolean prefixProcessed = false;
+    CommentTracker ct = new CommentTracker();
+    PsiElement prev = PsiTreeUtil.skipWhitespacesAndCommentsBackward(expression);
+    PsiElement next = PsiTreeUtil.skipWhitespacesAndCommentsForward(expression);
+    for (int i = 0; i < children.length; i++) {
+      PsiElement child = children[i];
+      String childText = child.getText();
+      if (i < index && prefixProcessed) continue;
+      if (prev == child) {
+        childText = StringUtil.trimEnd(childText, "\\{");
+        prefixProcessed = true;
+      } else if (next == child) {
+        childText = StringUtil.trimStart(childText, "}");
+        suffixProcessed = true;
+      }
+      if (i > index && !suffixProcessed) continue;
+      if (child == expression) {
+        if (expression instanceof PsiEmptyExpressionImpl) {
+          childText = "null";
+        } else if (expression instanceof PsiLiteralExpression literal) {
+          Object value = literal.getValue();
+          if (value instanceof String && !literal.isTextBlock()) {
+            childText = StringUtil.trimEnd(StringUtil.trimStart(childText, "\""), "\"");
+          } else {
+            childText = StringUtil.escapeStringCharacters(String.valueOf(value));
+          }
+        }
+      }
+      ct.markUnchanged(child);
+      newTemplate.append(childText);
+    }
+    PsiTemplateExpression newTemplateExpression = (PsiTemplateExpression)JavaPsiFacade.getElementFactory(expression.getProject())
+      .createExpressionFromText(newTemplate.toString(), expression);
+    return (PsiTemplateExpression)ct.replaceAndRestoreComments(templateExpression, newTemplateExpression);
+  }
+
   private static class RemoveEmbeddedExpressionFix extends PsiUpdateModCommandAction<PsiExpression> {
     private RemoveEmbeddedExpressionFix(PsiExpression embeddedExpression) {
       super(embeddedExpression);
@@ -63,55 +132,9 @@ public final class RedundantEmbeddedExpressionInspection extends AbstractBaseJav
 
     @Override
     protected void invoke(@NotNull ActionContext context, @NotNull PsiExpression expression, @NotNull ModPsiUpdater updater) {
-      PsiTemplate template = ObjectUtils.tryCast(expression.getParent(), PsiTemplate.class);
-      if (template == null) return;
-      PsiTemplateExpression templateExpression = ObjectUtils.tryCast(template.getParent(), PsiTemplateExpression.class);
-      if (templateExpression == null) return;
-      PsiElement[] children = template.getChildren();
-      int index = ArrayUtil.indexOf(children, expression);
-      if (index == -1) return;
-      StringBuilder newTemplate = new StringBuilder();
-      for (PsiElement child : templateExpression.getChildren()) {
-        if (child == template) break;
-        newTemplate.append(child.getText());
-      }
-      boolean suffixProcessed = false;
-      boolean prefixProcessed = false;
-      CommentTracker ct = new CommentTracker();
-      PsiElement prev = PsiTreeUtil.skipWhitespacesAndCommentsBackward(expression);
-      PsiElement next = PsiTreeUtil.skipWhitespacesAndCommentsForward(expression);
-      for (int i = 0; i < children.length; i++) {
-        PsiElement child = children[i];
-        String childText = child.getText();
-        if (i < index && prefixProcessed) continue;
-        if (prev == child) {
-          childText = StringUtil.trimEnd(childText, "\\{");
-          prefixProcessed = true;
-        } else if (next == child) {
-          childText = StringUtil.trimStart(childText, "}");
-          suffixProcessed = true;
-        }
-        if (i > index && !suffixProcessed) continue;
-        if (child == expression) {
-          if (expression instanceof PsiEmptyExpressionImpl) {
-            childText = "null";
-          } else if (expression instanceof PsiLiteralExpression literal) {
-            Object value = literal.getValue();
-            if (value instanceof String && !literal.isTextBlock()) {
-              childText = StringUtil.trimEnd(StringUtil.trimStart(childText, "\""), "\"");
-            } else {
-              childText = StringUtil.escapeStringCharacters(String.valueOf(value)); 
-            }
-          }
-        }
-        ct.markUnchanged(child);
-        newTemplate.append(childText);
-      }
-      PsiTemplateExpression newTemplateExpression = (PsiTemplateExpression)JavaPsiFacade.getElementFactory(context.project())
-        .createExpressionFromText(newTemplate.toString(), expression);
-      ct.replaceAndRestoreComments(templateExpression, newTemplateExpression);
+      inlineEmbeddedExpression(expression);
     }
-
+    
     @Override
     public @NotNull String getFamilyName() {
       return InspectionGadgetsBundle.message("inspection.redundant.embedded.expression.fix.family.name");
