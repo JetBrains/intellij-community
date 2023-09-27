5fb7c59e77e91df85552663aa74ebb5342103b5e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-09-27 10:48:25 +0200

    IJPL-259 add escapeHtml/escapeJavaScript/contains/containsAny/containsIgnoreCase/countMatches
    GitOrigin-RevId: 9b2beff570e409499f0fa82966e64fb1e2cd86b5

--- platform/util/commons-lang-v2-shim/src/org/apache/commons/lang/StringUtils.java
+++ platform/util/commons-lang-v2-shim/src/org/apache/commons/lang/StringUtils.java
@@ -6,6 +6,38 @@ package org.apache.commons.lang;
  * please consider migrating to either the commons-lang3 or commons-text libraries and bundling them with your plugin.
  * Or consider using the corresponding API from IJ Platform.
  */
+@SuppressWarnings("unused")
 @Deprecated(forRemoval = true)
 public class StringUtils extends org.apache.commons.lang3.StringUtils {
+  public static boolean contains(String seq, String searchSeq) {
+    return org.apache.commons.lang3.StringUtils.contains(seq, searchSeq);
+  }
+
+  public static boolean contains(String seq, char c) {
+    return org.apache.commons.lang3.StringUtils.contains(seq, c);
+  }
+
+  public static boolean containsAny(String seq, String s2) {
+    return org.apache.commons.lang3.StringUtils.containsAny(seq, s2);
+  }
+
+  public static boolean containsAny(String seq, char[] chars) {
+    return org.apache.commons.lang3.StringUtils.containsAny(seq, chars);
+  }
+
+  public static boolean containsIgnoreCase(String str, String searchStr) {
+    return org.apache.commons.lang3.StringUtils.containsIgnoreCase(str, searchStr);
+  }
+
+  public static boolean containsNone(String str, String searchStr) {
+    return org.apache.commons.lang3.StringUtils.containsNone(str, searchStr);
+  }
+
+  public static boolean containsNone(String str, char[] chars) {
+    return org.apache.commons.lang3.StringUtils.containsNone(str, chars);
+  }
+
+  public static int countMatches(String str, String s2) {
+    return org.apache.commons.lang3.StringUtils.countMatches(str, s2);
+  }
 }
