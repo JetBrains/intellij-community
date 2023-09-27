eb85741b253a49f9c6de9eaa7d735ba2a39fc8ef
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-09-27 11:36:08 +0200

    [java-inspections] CreateConstructorParameterFromFieldFix: rewrite to ModCommand, using trackDeclaration()
    Also fixes IDEA-329769 'Add constructor parameter' hint and action discrepancy
    
    GitOrigin-RevId: f897347600bfdfd2ebf7a4de0209e5887323f091

--- java/java-impl/src/com/intellij/codeInsight/intention/impl/AssignFieldFromParameterAction.java
+++ java/java-impl/src/com/intellij/codeInsight/intention/impl/AssignFieldFromParameterAction.java
@@ -1,22 +1,23 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.intention.impl;
 
+import com.intellij.codeHighlighting.HighlightDisplayLevel;
 import com.intellij.codeInsight.daemon.impl.analysis.JavaHighlightUtil;
+import com.intellij.codeInspection.ex.ToolsImpl;
 import com.intellij.java.JavaBundle;
 import com.intellij.lang.java.JavaLanguage;
 import com.intellij.modcommand.*;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
+import com.intellij.profile.codeInspection.InspectionProfileManager;
 import com.intellij.psi.*;
-import com.intellij.psi.codeStyle.CodeStyleManager;
-import com.intellij.psi.codeStyle.JavaCodeStyleManager;
-import com.intellij.psi.codeStyle.SuggestedNameInfo;
-import com.intellij.psi.codeStyle.VariableKind;
+import com.intellij.psi.codeStyle.*;
 import com.intellij.psi.controlFlow.*;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.IncorrectOperationException;
 import com.siyeh.ig.psiutils.VariableAccessUtils;
+import com.siyeh.ig.style.UnqualifiedFieldAccessInspection;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -102,6 +103,7 @@ public class AssignFieldFromParameterAction extends PsiUpdateModCommandAction<Ps
     return field;
   }
 
+  @SuppressWarnings("unused") // used from third-party plugins
   public static PsiElement addFieldAssignmentStatement(@NotNull Project project,
                                                        @NotNull PsiField field,
                                                        @NotNull PsiParameter parameter,
@@ -109,10 +111,19 @@ public class AssignFieldFromParameterAction extends PsiUpdateModCommandAction<Ps
     return addFieldAssignmentStatement(project, field, parameter, ModPsiNavigator.fromEditor(editor));
   }
 
-  private static PsiElement addFieldAssignmentStatement(@NotNull Project project,
-                                                        @NotNull PsiField field,
-                                                        @NotNull PsiParameter parameter,
-                                                        @Nullable ModPsiNavigator updater) throws IncorrectOperationException {
+  /**
+   * Adds a field assignment statement to the method body
+   *
+   * @param project   current project
+   * @param field     field to assign
+   * @param parameter parameter to assign from
+   * @param updater   updater to use to set the caret at the added statement
+   * @return inserted statement; null if insertion fails (e.g., method does not belong to a class)
+   */
+  public static @Nullable PsiStatement addFieldAssignmentStatement(@NotNull Project project,
+                                                                   @NotNull PsiField field,
+                                                                   @NotNull PsiParameter parameter,
+                                                                   @Nullable ModPsiNavigator updater) {
     PsiMethod method = (PsiMethod)parameter.getDeclarationScope();
     PsiCodeBlock methodBody = method.getBody();
     if (methodBody == null) return null;
@@ -123,8 +134,14 @@ public class AssignFieldFromParameterAction extends PsiUpdateModCommandAction<Ps
     PsiClass targetClass = method.getContainingClass();
     if (targetClass == null) return null;
 
+    ToolsImpl tool = InspectionProfileManager.getInstance(project).getCurrentProfile()
+      .getToolsOrNull(UnqualifiedFieldAccessInspection.SHORT_NAME, project);
+    boolean codeStyleRequiresThis = tool != null && tool.isEnabled(field) && tool.getLevel(field) != HighlightDisplayLevel.DO_NOT_SHOW;
+
     String stmtText = fieldName + " = " + parameterName + ";";
-    if (Comparing.strEqual(fieldName, parameterName) || JavaPsiFacade.getInstance(project).getResolveHelper().resolveReferencedVariable(fieldName, methodBody) != field) {
+    if (Comparing.strEqual(fieldName, parameterName) ||
+        JavaPsiFacade.getInstance(project).getResolveHelper().resolveReferencedVariable(fieldName, methodBody) != field ||
+        (codeStyleRequiresThis && !isMethodStatic)) {
       @NonNls String prefix = isMethodStatic ? targetClass.getName() == null ? "" : targetClass.getName() + "." : "this.";
       stmtText = prefix + stmtText;
     }
@@ -132,12 +149,12 @@ public class AssignFieldFromParameterAction extends PsiUpdateModCommandAction<Ps
     PsiStatement assignmentStmt = (PsiStatement)CodeStyleManager.getInstance(project).reformat(factory.createStatementFromText(stmtText, methodBody));
     PsiStatement[] statements = methodBody.getStatements();
     int i = FieldFromParameterUtils.findFieldAssignmentAnchor(statements, null, null, targetClass, parameter);
-    PsiElement inserted;
+    PsiStatement inserted;
     if (i == statements.length) {
-      inserted = methodBody.add(assignmentStmt);
+      inserted = (PsiStatement)methodBody.add(assignmentStmt);
     }
     else {
-      inserted = methodBody.addAfter(assignmentStmt, i > 0 ? statements[i - 1] : null);
+      inserted = (PsiStatement)methodBody.addAfter(assignmentStmt, i > 0 ? statements[i - 1] : null);
     }
     if (updater != null) {
       updater.moveTo(inserted.getTextRange().getEndOffset());
--- java/java-impl/src/com/intellij/codeInsight/intention/impl/config/QuickFixFactoryImpl.java
+++ java/java-impl/src/com/intellij/codeInsight/intention/impl/config/QuickFixFactoryImpl.java
@@ -606,7 +606,7 @@ public final class QuickFixFactoryImpl extends QuickFixFactory {
   @NotNull
   @Override
   public IntentionAction createCreateConstructorParameterFromFieldFix(@NotNull PsiField field) {
-    return new CreateConstructorParameterFromFieldFix(field);
+    return new CreateConstructorParameterFromFieldFix(field).asIntention();
   }
 
   @NotNull
