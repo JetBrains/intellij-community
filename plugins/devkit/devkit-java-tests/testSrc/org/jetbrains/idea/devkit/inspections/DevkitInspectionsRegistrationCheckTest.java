a7f206e6faeb436c06d11ca764a6f15c28be913a
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2023-09-27 15:54:26 +0200

    IDEA-311444: Add UsePlatformProcessAwaitExit to the WIP inspection list
    GitOrigin-RevId: fda555bcebe5379c977a24cfd749b8a9f2ebc295

--- plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/inspections/DevkitInspectionsRegistrationCheckTest.java
+++ plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/inspections/DevkitInspectionsRegistrationCheckTest.java
@@ -28,7 +28,8 @@ public class DevkitInspectionsRegistrationCheckTest extends BasePlatformTestCase
             "CallingMethodShouldBeRequiresBlockingContext",
             "IncorrectProcessCanceledExceptionHandling",
             "StaticInitializationInExtensions",
-            "ListenerImplementationMustNotBeDisposable");
+            "ListenerImplementationMustNotBeDisposable",
+            "UsePlatformProcessAwaitExit");
 
   /**
    * Validates all DevKit inspections that are disabled by default match the expected known set.
@@ -37,7 +38,7 @@ public class DevkitInspectionsRegistrationCheckTest extends BasePlatformTestCase
     List<LocalInspectionEP> devkitInspections = ContainerUtil.filter(LocalInspectionEP.LOCAL_INSPECTION.getExtensionList(), ep -> {
       return "DevKit".equals(ep.getPluginDescriptor().getPluginId().getIdString());
     });
-    assertEquals("Mismatch in total inspections, check classpath in test run configuration (intellij.devkit.plugin)", 66,
+    assertEquals("Mismatch in total inspections, check classpath in test run configuration (intellij.devkit.plugin)", 67,
                  devkitInspections.size());
 
     List<LocalInspectionEP> disabledInspections = ContainerUtil.filter(devkitInspections, ep -> !ep.enabledByDefault);
