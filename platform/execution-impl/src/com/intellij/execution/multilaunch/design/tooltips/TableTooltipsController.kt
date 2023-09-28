b4895ee8021a4297305c38a3dde595c688df77b0
Andrii Rublov <andrii.rublov@jetbrains.com>
2023-09-28 22:34:23 +0200

    Multi-Launch: Fix custom table popups look & feel and behavior (RIDER-99529)
    GitOrigin-RevId: 2ae8ebc0b3f06ff92c4cb93255397aa0bc92991a

--- platform/execution-impl/src/com/intellij/execution/multilaunch/design/tooltips/TableTooltipsController.kt
+++ platform/execution-impl/src/com/intellij/execution/multilaunch/design/tooltips/TableTooltipsController.kt
@@ -1,15 +1,15 @@
 package com.intellij.execution.multilaunch.design.tooltips
 
+import com.intellij.execution.multilaunch.design.Debouncer
+import com.intellij.execution.multilaunch.design.extensions.isOver
 import com.intellij.openapi.ui.popup.Balloon
 import com.intellij.openapi.ui.popup.JBPopupFactory
-import com.intellij.ui.JBColor
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.ui.awt.RelativePoint
 import com.intellij.ui.hover.HoverListener
 import com.intellij.util.ui.Html
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
-import com.intellij.execution.multilaunch.design.Debouncer
-import com.intellij.execution.multilaunch.design.extensions.isOver
 import java.awt.Component
 import java.awt.Point
 import java.util.concurrent.TimeUnit
@@ -50,18 +50,29 @@ class TableTooltipsController(private val schedulerKey: Any = TableTooltipsContr
       }
       else -> {
         tooltipScheduler.call(schedulerKey) {
+          val shadowEnabled = Registry.`is`("ide.balloon.shadowEnabled")
+          val shadowSize = Registry.intValue("ide.balloon.shadow.size")
           val newTooltip = JBPopupFactory.getInstance()
-            .createHtmlTextBalloonBuilder(Html(provider.tooltipText).setKeepFont(true), null, UIUtil.getLabelTextForeground(),
-                                          UIUtil.getPanelBackground(), null)
-            .setShadow(false)
+            .createHtmlTextBalloonBuilder(
+              Html(provider.tooltipText).setKeepFont(true),
+              null,
+              UIUtil.getToolTipForeground(),
+              UIUtil.getToolTipBackground(),
+              null)
+            .setShadow(shadowEnabled)
+            .setAnimationCycle(0)
+            .setBorderColor(JBUI.CurrentTheme.Tooltip.borderColor())
             .setShowCallout(false)
-            .setAnimationCycle(100)
-            .setBorderColor(JBColor.border())
             .createBalloon()
           val tooltipSize = newTooltip.preferredSize
+          val shadowOffset = when (shadowEnabled) {
+            true -> Point(JBUI.scale(shadowSize), JBUI.scale(shadowSize))
+            false -> Point(0, 0)
+          }
+          val balloonOffset = Point(JBUI.scale(10), JBUI.scale(10))
           val tooltipLocation = RelativePoint(table, Point(
-            tableMouse.x + tooltipSize.width / 2 + JBUI.scale(10),
-            tableMouse.y + tooltipSize.height / 2 + JBUI.scale(10)
+            tableMouse.x + tooltipSize.width / 2 - shadowOffset.x + balloonOffset.x,
+            tableMouse.y + tooltipSize.height / 2 - shadowOffset.y + balloonOffset.y
           ))
           newTooltip.show(tooltipLocation, Balloon.Position.below)
           tooltip = newTooltip
