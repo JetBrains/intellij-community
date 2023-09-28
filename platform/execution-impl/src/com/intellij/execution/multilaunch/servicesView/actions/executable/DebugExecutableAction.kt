0e8bd189edae80f184a2883671648ffc044131fa
Andrii Rublov <andrii.rublov@jetbrains.com>
2023-09-28 23:17:16 +0200

    Multi-Launch: Drop commented unused actions
    GitOrigin-RevId: 6ace30647861c015f05822f9e7a981351db34c20

--- platform/execution-impl/src/com/intellij/execution/multilaunch/servicesView/actions/executable/DebugExecutableAction.kt
+++
@@ -1,7 +0,0 @@
-package com.intellij.execution.multilaunch.servicesView.actions.executable
-
-//class DebugExecutableAction : ExecuteExecutableAction("Run", AllIcons.Actions.StartDebugger) {
-//  override fun execute(executable: MultirunExecutable) {
-//    TODO("Not yet implemented")
-//  }
-//}
--- platform/execution-impl/src/com/intellij/execution/multilaunch/servicesView/actions/executable/RunExecutableAction.kt
+++
@@ -1,4 +0,0 @@
-package com.intellij.execution.multilaunch.servicesView.actions.executable
-
-//class RunExecutableAction : ExecuteExecutableAction("Run", AllIcons.Actions.Execute) {
-//}
