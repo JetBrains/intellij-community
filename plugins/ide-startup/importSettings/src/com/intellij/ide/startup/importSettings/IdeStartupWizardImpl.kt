3175b80610205a60ec0cf4f6742291ab60d2e55a
Ivan Migalev <ivan.migalev@jetbrains.com>
2023-09-28 20:01:50 +0200

    (IDEA-333614) Settings Transfer: show the new dialog on startup
    GitOrigin-RevId: 7cb791f4b3f51c238578e787b4a0a0316e1864d7

--- plugins/ide-startup/importSettings/src/com/intellij/ide/startup/importSettings/IdeStartupWizardImpl.kt
+++ plugins/ide-startup/importSettings/src/com/intellij/ide/startup/importSettings/IdeStartupWizardImpl.kt
@@ -1,10 +1,9 @@
 package com.intellij.ide.startup.importSettings
 
+import com.intellij.ide.startup.importSettings.chooser.ImportSettingsFromDialog
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.extensions.ExtensionNotApplicableException
 import com.intellij.platform.ide.bootstrap.IdeStartupWizard
-import com.intellij.ui.components.dialog
-import com.intellij.ui.dsl.builder.panel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -15,13 +14,12 @@ private class IdeStartupWizardImpl : IdeStartupWizard {
     }
   }
 
-  @Suppress("HardCodedStringLiteral") // temporary
   override suspend fun run() {
     withContext(Dispatchers.EDT) {
-      val panel = panel {
-        row("Import test panel") {  }
+      val dialog = ImportSettingsFromDialog().apply {
+        isResizable = false
+        pack()
       }
-      val dialog = dialog("Import Test", panel)
       dialog.show()
     }
   }
