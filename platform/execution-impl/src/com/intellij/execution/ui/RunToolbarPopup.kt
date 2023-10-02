0209c06eee0d18d348e08053eb32b6d957c47d31
Vyacheslav Moklev <Vyacheslav.Moklev@jetbrains.com>
2023-10-02 11:04:15 +0300

    [RDCT] RDCT-564: Support HideableAction conversion in popups
    Since HideableAction API provides arbitrary function for showing/hiding action in popups, in this change only basic condition is transferred (show in speed search / show without speed search). This is needed to hide folders in Run Widget Popup when speed search is not active.
    
    GitOrigin-RevId: 768f2cd513df7a1e1b7254393be6df5babe10d1f

--- platform/execution-impl/src/com/intellij/execution/ui/RunToolbarPopup.kt
+++ platform/execution-impl/src/com/intellij/execution/ui/RunToolbarPopup.kt
@@ -14,6 +14,7 @@ import com.intellij.icons.AllIcons
 import com.intellij.ide.ActivityTracker
 import com.intellij.ide.dnd.*
 import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.actionSystem.ex.HideableAction
 import com.intellij.openapi.actionSystem.ex.InlineActionsHolder
 import com.intellij.openapi.actionSystem.impl.PresentationFactory
 import com.intellij.openapi.actionSystem.remoting.ActionRemotePermissionRequirements
@@ -246,10 +247,6 @@ internal class RunConfigurationsActionGroupPopup(actionGroup: ActionGroup, dataC
       return DnDDragStartBean(DraggedIndex(index))
     }
   }
-}
-
-private interface HideableAction {
-  val shouldBeShown: (holdingFilter: Boolean) -> Boolean
 }
 
 private class HideableDefaultActionGroup(@NlsSafe name: String, override val shouldBeShown: (holdingFilter: Boolean) -> Boolean)
