92fd0b02017c26e3f981e7b2d65c26ddfc923f06
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2023-10-03 20:45:58 +0200

    Extract `getOrCreateUserData` to reuse it all over the code
    GitOrigin-RevId: 5b618fa60b8c9ef7d56b9f22d060f617d28d9fa7

--- platform/rd-platform-community/src/com/intellij/openapi/rd/util/UserDataHolder.kt
+++ platform/rd-platform-community/src/com/intellij/openapi/rd/util/UserDataHolder.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.rd.createLifetime
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.UserDataHolder
+import com.intellij.openapi.util.getOrCreateUserData
 import com.jetbrains.rd.util.assert
 import com.jetbrains.rd.util.lifetime.Lifetime
 import kotlin.properties.ReadWriteProperty
@@ -11,15 +12,6 @@ import kotlin.reflect.KProperty
 
 val <T> T.lifetime: Lifetime where T : UserDataHolder, T : Disposable by userData(Key("com.jetbrains.rd.platform.util.lifetime")) {
   it.createLifetime()
-}
-
-inline fun <T> UserDataHolder.getOrCreateUserData(key: Key<T>, producer: () -> T): T {
-  val existing = getUserData(key)
-  if (existing != null) return existing
-
-  val value = producer()
-  putUserData(key, value)
-  return value
 }
 
 fun <T> UserDataHolder.putUserData(lt: Lifetime, key: Key<T>, value: T) {
