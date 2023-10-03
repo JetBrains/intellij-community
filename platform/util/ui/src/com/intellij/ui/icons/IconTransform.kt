173acd68bff2acf0ca65caa4119fb5f73d450b80
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-10-03 14:18:59 +0200

    IDEA-327116 convert IconTransform to modern language
    GitOrigin-RevId: 297dc23b4ea1e1e86063c12ca7e542320dd04afb

--- platform/util/ui/src/com/intellij/ui/icons/IconTransform.kt
+++ platform/util/ui/src/com/intellij/ui/icons/IconTransform.kt
@@ -1,127 +1,115 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ui.icons;
+package com.intellij.ui.icons
 
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.progress.ProcessCanceledException;
-import com.intellij.openapi.util.IconPathPatcher;
-import com.intellij.util.ArrayUtil;
-import kotlin.Pair;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.debug
+import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.openapi.util.IconPathPatcher
+import com.intellij.util.ArrayUtil
+import org.jetbrains.annotations.ApiStatus
+import java.awt.image.ImageFilter
 
-import java.awt.image.ImageFilter;
+private val LOG = Logger.getInstance(IconTransform::class.java)
 
 /**
  * Immutable representation of a global transformation applied to all icons
  */
 @ApiStatus.Internal
-public final class IconTransform {
-  private static final Logger LOG = Logger.getInstance(IconTransform.class);
+class IconTransform
+/**
+ * Creates a new instance of IconTransform with the specified parameters.
+ *
+ * @param isDark             true if the icon should be transformed for dark mode, false otherwise
+ * @param patchers         an array of IconPathPatcher objects used to modify the icon path before transforming it
+ * @param postPatchers     an array of IconPathPatcher objects used to modify the icon path before transforming it.
+ * During icon path patching patchers are iterated first, and if neither of them worked out, postPatchers are iterated.
+ * @param filter           the ImageFilter to apply to the transformed icon, or null if no filter should be applied
+ */ private constructor(val isDark: Boolean,
+                        private val patchers: Array<IconPathPatcher>,
+                        private val postPatchers: Array<IconPathPatcher>,
+                        val filter: ImageFilter?) {
+  constructor(dark: Boolean, patchers: Array<IconPathPatcher>, filter: ImageFilter?)
+    : this(isDark = dark, patchers = patchers, postPatchers = emptyArray(), filter = filter)
 
-  private final boolean dark;
-  private final IconPathPatcher @NotNull [] patchers;
-  private final IconPathPatcher @NotNull [] postPatchers;
-  private final @Nullable ImageFilter filter;
-
-  public IconTransform(boolean dark, IconPathPatcher @NotNull [] patchers, @Nullable ImageFilter filter) {
-    this(dark, patchers, new IconPathPatcher[0], filter);
-  }
-
-  /**
-   * Creates a new instance of IconTransform with the specified parameters.
-   *
-   * @param dark             true if the icon should be transformed for dark mode, false otherwise
-   * @param patchers         an array of IconPathPatcher objects used to modify the icon path before transforming it
-   * @param postPatchers     an array of IconPathPatcher objects used to modify the icon path before transforming it.
-   *                         During icon path patching patchers are iterated first, and if neither of them worked out, postPatchers are iterated.
-   * @param filter           the ImageFilter to apply to the transformed icon, or null if no filter should be applied
-   */
-  private IconTransform(boolean dark,
-                       IconPathPatcher @NotNull [] patchers,
-                       IconPathPatcher @NotNull [] postPatchers,
-                       @Nullable ImageFilter filter) {
-    this.dark = dark;
-    this.patchers = patchers;
-    this.postPatchers = postPatchers;
-    this.filter = filter;
-  }
-
-  public boolean isDark() {
-    return dark;
-  }
-
-  public @Nullable ImageFilter getFilter() {
-    return filter;
-  }
-
-  public @NotNull IconTransform withPathPatcher(@NotNull IconPathPatcher patcher) {
-    return new IconTransform(dark, ArrayUtil.append(patchers, patcher), postPatchers, filter);
+  fun withPathPatcher(patcher: IconPathPatcher): IconTransform {
+    return IconTransform(isDark = isDark, patchers = patchers + patcher, postPatchers = postPatchers, filter = filter)
   }
 
   @ApiStatus.Internal
-  public @NotNull IconTransform withPostPathPatcher(@NotNull IconPathPatcher patcher) {
-    return new IconTransform(dark, patchers, ArrayUtil.append(postPatchers, patcher), filter);
+  fun withPostPathPatcher(patcher: IconPathPatcher): IconTransform {
+    return IconTransform(isDark, patchers, ArrayUtil.append(postPatchers, patcher), filter)
   }
 
-  public @NotNull IconTransform withoutPathPatcher(@NotNull IconPathPatcher patcher) {
-    IconPathPatcher[] newPatchers = ArrayUtil.remove(patchers, patcher);
-    IconPathPatcher[] newLastPatchers = postPatchers;
-    if (newPatchers == patchers) newLastPatchers = ArrayUtil.remove(postPatchers, patcher);
+  fun withoutPathPatcher(patcher: IconPathPatcher): IconTransform {
+    val newPatchers = ArrayUtil.remove(patchers, patcher)
+    var newLastPatchers = postPatchers
+    if (newPatchers === patchers) {
+      newLastPatchers = ArrayUtil.remove(postPatchers, patcher)
+    }
 
-    return newPatchers == patchers && newLastPatchers == postPatchers ? this : new IconTransform(dark, newPatchers, postPatchers, filter);
+    if (newPatchers === patchers && newLastPatchers === postPatchers) {
+      return this
+    }
+    else {
+      return IconTransform(isDark = isDark, patchers = newPatchers, postPatchers = postPatchers, filter = filter)
+    }
   }
 
-  public @NotNull IconTransform withFilter(ImageFilter filter) {
-    return filter == this.filter ? this : new IconTransform(dark, patchers, postPatchers, filter);
+  fun withFilter(filter: ImageFilter): IconTransform {
+    if (filter === this.filter) {
+      return this
+    }
+    else {
+      return IconTransform(isDark = isDark, patchers = patchers, postPatchers = postPatchers, filter = filter)
+    }
   }
 
-  public @NotNull IconTransform withDark(boolean dark) {
-    return dark == this.dark ? this : new IconTransform(dark, patchers, postPatchers, filter);
+  fun withDark(dark: Boolean): IconTransform {
+    if (dark == this.isDark) {
+      return this
+    }
+    else {
+      return IconTransform(isDark = dark, patchers = patchers, postPatchers = postPatchers, filter = filter)
+    }
   }
 
-  public @Nullable Pair<String, ClassLoader> patchPath(@NotNull String path, @Nullable ClassLoader classLoader) {
-    String pathWithLeadingSlash = path.charAt(0) == '/' ? path : ('/' + path);
-    int length = patchers.length + postPatchers.length;
-    for (int i = 0; i < length; i++) {
-      IconPathPatcher patcher;
-      if (i < patchers.length) patcher = patchers[i];
-      else patcher = postPatchers[i - patchers.length];
+  fun patchPath(path: String, classLoader: ClassLoader?): Pair<String, ClassLoader?>? {
+    val pathWithLeadingSlash = if (path[0] == '/') path else "/$path"
+    val length = patchers.size + postPatchers.size
+    for (i in 0 until length) {
+      val patcher = if (i < patchers.size) patchers[i] else postPatchers[i - patchers.size]
 
-      String newPath;
+      var newPath: String?
       try {
-        newPath = patcher.patchPath(pathWithLeadingSlash, classLoader);
+        newPath = patcher.patchPath(pathWithLeadingSlash, classLoader)
       }
-      catch (ProcessCanceledException e) {
-        throw e;
+      catch (e: ProcessCanceledException) {
+        throw e
       }
-      catch (Exception e) {
-        LOG.error(patcher + " cannot patch icon path", e);
-        continue;
+      catch (e: Exception) {
+        LOG.error("$patcher cannot patch icon path", e)
+        continue
       }
 
       if (newPath == null) {
-        continue;
+        continue
       }
 
-      if (LOG.isDebugEnabled()) {
-        LOG.debug("replace '" + path + "' with '" + newPath + "'");
+      LOG.debug {
+        "replace '$path' with '$newPath'"
       }
 
-      ClassLoader contextClassLoader = patcher.getContextClassLoader(pathWithLeadingSlash, classLoader);
+      var contextClassLoader = patcher.getContextClassLoader(pathWithLeadingSlash, classLoader)
       if (contextClassLoader == null) {
-        //noinspection deprecation
-        Class<?> contextClass = patcher.getContextClass(pathWithLeadingSlash);
-        if (contextClass != null) {
-          contextClassLoader = contextClass.getClassLoader();
-        }
+        @Suppress("DEPRECATION")
+        contextClassLoader = patcher.getContextClass(pathWithLeadingSlash).getClassLoader()
       }
-      return new Pair<>(newPath, contextClassLoader);
+      return Pair(newPath, contextClassLoader)
     }
-    return null;
+    return null
   }
 
-  public @NotNull IconTransform copy() {
-    return new IconTransform(dark, patchers, postPatchers, filter);
+  fun copy(): IconTransform {
+    return IconTransform(isDark = isDark, patchers = patchers, postPatchers = postPatchers, filter = filter)
   }
 }
