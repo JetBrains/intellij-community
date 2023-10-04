67bbc0bf9a7b8cd36935b1363b050d7a59bf9d48
Roman Ivanov <roman.ivanov@jetbrains.com>
2023-10-04 22:40:43 +0200

    [fus] CodeVisionFusEditorListener: check project disposal in read action, use coroutines
    GitOrigin-RevId: 5a74809ce0430f432cf8afeb3d01f1595dc264d6

--- platform/lang-impl/src/com/intellij/codeInsight/hints/codeVision/CodeVisionFusEditorListener.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/hints/codeVision/CodeVisionFusEditorListener.kt
@@ -6,43 +6,65 @@ import com.intellij.internal.statistic.eventLog.events.EventFields
 import com.intellij.internal.statistic.eventLog.events.EventPair
 import com.intellij.internal.statistic.utils.StatisticsUtil
 import com.intellij.lang.Language
+import com.intellij.openapi.application.readAction
+import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.event.EditorFactoryEvent
 import com.intellij.openapi.editor.event.EditorFactoryListener
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiFile
 import com.intellij.util.concurrency.AppExecutorUtil
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Runnable
+import kotlinx.coroutines.launch
 import java.util.concurrent.ConcurrentHashMap
 
-class CodeVisionFusEditorListener : EditorFactoryListener {
+class CodeVisionFusEditorListener(private val scope: CoroutineScope) : EditorFactoryListener {
   override fun editorReleased(event: EditorFactoryEvent) {
     val editor = event.editor
     val project = editor.project ?: return
     val histogramBuilder = editor.getUserData(CodeVisionFusCollector.PROVIDER_STORAGE_KEY)
+    val document = editor.document
+    val textLength = StatisticsUtil.roundToPowerOfTwo(document.textLength)
     if (histogramBuilder != null) {
       val fusData: CodeVisionFusData = histogramBuilder.flush()
-      val document = editor.document
-      val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document)
+      scope.launch {
+        reportToFus(project, document, textLength, fusData)
+      }
+    }
+  }
 
-      val vcsAnnotationHistogram: FusHistogram? = psiFile?.getUserData(CodeVisionFusCollector.VCS_ANNOTATION_HISTOGRAM_KEY)?.build()
-      // we remove it because editor may be closed, but the file remains. In order not to report its data several times, we need to remove
-      psiFile?.putUserData(CodeVisionFusCollector.VCS_ANNOTATION_HISTOGRAM_KEY, null)
-      val textLength = StatisticsUtil.roundToPowerOfTwo(document.textLength)
+  private suspend fun reportToFus(project: Project,
+                                  document: Document,
+                                  textLength: Int,
+                                  fusData: CodeVisionFusData) {
+    class Result(val psiFile: PsiFile, val vcsAnnotationHistogram: FusHistogram)
 
-      AppExecutorUtil.getAppExecutorService().submit(Runnable {
-        if (vcsAnnotationHistogram != null) {
-          CodeVisionFusCollector.VCS_ANNOTATION_CALCULATION_DURATION_HISTOGRAM.log(project, vcsAnnotationHistogram.buckets.toList(), psiFile.language, textLength)
-        }
+    val vcsAnnotationHistResult = readAction {
+      if (project.isDisposed) return@readAction null
+      val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document) ?: return@readAction null
+      val vcsAnnotationHistogram = psiFile.getUserData(CodeVisionFusCollector.VCS_ANNOTATION_HISTOGRAM_KEY)?.build()
+                                   ?: return@readAction null
+      Result(psiFile, vcsAnnotationHistogram)
+    }
+    // we remove it because editor may be closed, but the file remains. In order not to report its data several times, we need to remove e
+    if (vcsAnnotationHistResult != null) {
+      val file = vcsAnnotationHistResult.psiFile
+      file.putUserData(CodeVisionFusCollector.VCS_ANNOTATION_HISTOGRAM_KEY, null)
+      val vcsAnnotationHistogram = vcsAnnotationHistResult.vcsAnnotationHistogram
+      CodeVisionFusCollector.VCS_ANNOTATION_CALCULATION_DURATION_HISTOGRAM.log(project, vcsAnnotationHistogram.buckets.toList(),
+                                                                               file.language,
+                                                                               textLength)
+    }
 
-        val language = fusData.language
-        for ((providerClass, histogram) in fusData.providerClassToHistogram) {
-          CodeVisionFusCollector.CODE_VISION_DURATION_HISTOGRAM.log(project, listOf(
-            EventPair(HISTOGRAM_FIELD, histogram.toList()),
-            EventPair(EventFields.Language, language),
-            EventPair(EventFields.Size, textLength),
-            EventPair(CodeVisionFusCollector.PROVIDER_CLASS_FIELD, providerClass)
-          ))
-        }
-      })
+    val language = fusData.language
+    for ((providerClass, histogram) in fusData.providerClassToHistogram) {
+      CodeVisionFusCollector.CODE_VISION_DURATION_HISTOGRAM.log(project, listOf(
+        EventPair(HISTOGRAM_FIELD, histogram.toList()),
+        EventPair(EventFields.Language, language),
+        EventPair(EventFields.Size, textLength),
+        EventPair(CodeVisionFusCollector.PROVIDER_CLASS_FIELD, providerClass)
+      ))
     }
   }
 }
@@ -51,8 +73,8 @@ internal class CodeVisionFusProviderStorage(val language: Language) {
   private val providerClassToHistogram: ConcurrentHashMap<Class<*>, FusHistogramBuilder> = ConcurrentHashMap()
 
   fun logProviderCollectionDuration(providerClass: Class<*>, durationMs: Long) {
-    val fusHistogramBuilder = providerClassToHistogram.computeIfAbsent(providerClass,
-                                                                       { FusHistogramBuilder(CodeVisionFusCollector.HISTOGRAM_BUCKETS) })
+    val fusHistogramBuilder = providerClassToHistogram.computeIfAbsent(providerClass
+    ) { FusHistogramBuilder(CodeVisionFusCollector.HISTOGRAM_BUCKETS) }
     synchronized(fusHistogramBuilder) {
       fusHistogramBuilder.addValue(durationMs)
     }
