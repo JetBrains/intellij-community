0ded06b513fff059ca726932c1e2cef1e4ad9abe
vladimir.petrenko <vladimir.petrenko@jetbrains.com>
2023-10-04 17:12:14 +0200

    RDCT-555 sync java level for project level to thin client
    (cherry picked from commit d1e5399dbb6840517035b28584ebeeab9309d09b)
    
    IJ-MR-118792
    
    GitOrigin-RevId: 85e2181ebe532a6e2ce3a188aa1d8a1f360bfcaa

---
+++ java/java-frontback-psi-api/src/com/intellij/lang/Constants.java
@@ -0,0 +1,7 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.lang;
+
+public final class Constants {
+
+  public static final String PROJECT_JAVA_LEVEL_KEY = "projectJavaLevel";
+}
