f84ff9a09132b57ff571c88a190467c09e0cc7f8
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2023-10-06 02:04:11 +0200

    [vcs-log] move VcsLogTelemetryExporter to the statistics package
    GitOrigin-RevId: 736776066dde3e84e65c179e490ba82e6ab2a3f3

--- platform/vcs-log/impl/src/com/intellij/vcs/log/statistics/VcsLogTelemetryExporter.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/statistics/VcsLogTelemetryExporter.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.vcs.log
+package com.intellij.vcs.log.statistics
 
 import com.intellij.openapi.vcs.VcsScope
 import com.intellij.openapi.vcs.telemetry.VcsTelemetrySpan.LogHistory
