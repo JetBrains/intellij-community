279d40809d5eb7ef498362d28b02f5d7069deac1
Evgeny Abramov <evgeny.abramov@jetbrains.com>
2023-10-07 14:13:33 +0300

    (IDEA-334267) ML in SE: save compiled regular expression in static properties
    GitOrigin-RevId: 566e6cc637fdfc174dd4b63b7d4bb93dc0a8d785

--- platform/ml-embeddings/src/com/intellij/platform/ml/embeddings/utils/IndexingUtils.kt
+++ platform/ml-embeddings/src/com/intellij/platform/ml/embeddings/utils/IndexingUtils.kt
@@ -10,30 +10,37 @@ import kotlin.math.sqrt
 
 private const val SEPARATOR = "~"
 
-fun splitIdentifierIntoTokens(initialScope: String): List<String> {
-  var scope = initialScope
-  for (reg in arrayOf("(?<=[A-Za-z])(?=[A-Z][a-z])", "[^\\w\\s]", "[_\\-]")) {
-    scope = scope.replace(reg.toRegex(), SEPARATOR)
+private object SplittingRegExps {
+  val wordsEndingLocations = arrayOf(
+    "(?<=[A-Za-z])(?=[A-Z][a-z])", "[^\\w\\s]", "[_\\-]").map { it.toRegex() }
+
+  val boundDigitsLocation = "(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)".toRegex()
+}
+
+fun splitIdentifierIntoTokens(identifier: String): List<String> {
+  var transformedIdentifier = identifier
+  for (regex in SplittingRegExps.wordsEndingLocations) {
+    transformedIdentifier = transformedIdentifier.replace(regex, SEPARATOR)
   }
 
-  val initialScopeArray = scope.split(SEPARATOR)
-
   return buildList {
-    for (elem in initialScopeArray) {
-      if (elem.isEmpty()) continue
-      var prev = Character.isUpperCase(elem[elem.length - 1])
-      var part = elem
+    for (token in transformedIdentifier.split(SEPARATOR)) {
+      if (token.isEmpty()) continue
+      var isNextCharUpperCase = Character.isUpperCase(token.last())
+      var transformedToken = token
 
-      for (i in elem.length - 2 downTo 0) {
-        val nextElem = Character.isUpperCase(part[i])
-        if (prev xor nextElem) {
-          part = part.substring(0, i + if (prev) 1 else 0) + SEPARATOR + part.substring(i + if (prev) 1 else 0)
-          prev = nextElem
+      for (index in token.length - 2 downTo 0) {
+        val isCurCharUpperCase = Character.isUpperCase(transformedToken[index])
+        val isCaseChanging = isNextCharUpperCase xor isCurCharUpperCase
+        if (isCaseChanging) {
+          val splitPosition = index + if (isNextCharUpperCase) 1 else 0
+          transformedToken = transformedToken.substring(0, splitPosition) + SEPARATOR + transformedToken.substring(splitPosition)
+          isNextCharUpperCase = isCurCharUpperCase
         }
       }
 
-      part.split(SEPARATOR)
-        .flatMap { it.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)".toRegex()) }
+      transformedToken.split(SEPARATOR)
+        .flatMap { it.split(SplittingRegExps.boundDigitsLocation) }
         .filterNot(String::isEmpty)
         .map { it.lowercase() }
         .forEach(this::add)
