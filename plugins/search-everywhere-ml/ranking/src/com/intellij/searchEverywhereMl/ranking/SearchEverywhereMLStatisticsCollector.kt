ee2ae72ba6b0b137b0f2e9f9ac69484741eff11f
Samuel Soukup <samuel.soukup@jetbrains.com>
2023-10-09 19:50:09 +0200

    ML in SE: New experimental model for Files 233.1.1.
    This model should address IDEA-315980 after IDEA-334403 is solved, but we need data for A/B testing for this.
    
    GitOrigin-RevId: a6111ee0f55e6f1fca3276ccc974153e27fd6ce7

--- plugins/search-everywhere-ml/ranking/src/com/intellij/searchEverywhereMl/ranking/SearchEverywhereMLStatisticsCollector.kt
+++ plugins/search-everywhere-ml/ranking/src/com/intellij/searchEverywhereMl/ranking/SearchEverywhereMLStatisticsCollector.kt
@@ -274,7 +274,7 @@ class SearchEverywhereMLStatisticsCollector : CounterUsagesCollector() {
   }
 
   companion object {
-    private val GROUP = EventLogGroup("mlse.log", 78, MLSE_RECORDER_ID)
+    private val GROUP = EventLogGroup("mlse.log", 79, MLSE_RECORDER_ID)
     private const val REPORTED_ITEMS_LIMIT = 50
 
     private val IS_INTERNAL = EventFields.Boolean("isInternal")
