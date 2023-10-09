2011192025dd48d2af67c3da857ebd2a4d8daa02
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-10-09 13:58:47 +0200

    [util] Make isKanaBreak faster
    Should help EA-900407 - F: NameUtilCore.isKanaBreak
    
    GitOrigin-RevId: e263fcd8c5a88f2cd0ffdf2c87cec674d3654f44

--- platform/util/base/src/com/intellij/util/text/NameUtilCore.java
+++ platform/util/base/src/com/intellij/util/text/NameUtilCore.java
@@ -9,6 +9,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class NameUtilCore {
+
+  private static final int KANA_START = 0x3040;
+  private static final int KANA_END = 0x3358;
+  private static final int KANA2_START = 0xFF66;
+  private static final int KANA2_END = 0xFF9D;
+
   /**
    * Splits an identifier into words, separated with underscores or upper-case characters
    * (camel-case).
@@ -99,8 +105,14 @@ public final class NameUtilCore {
     return i == 0 || !Character.isLetterOrDigit(text.charAt(i - 1)) || isHardCodedWordStart(text, i) ||
            isKanaBreak(cur, prev);
   }
+  
+  private static boolean maybeKana(int codePoint) {
+    return codePoint >= KANA_START && codePoint <= KANA_END ||
+           codePoint >= KANA2_START && codePoint <= KANA2_END;
+  }
 
   private static boolean isKanaBreak(int cur, int prev) {
+    if (!maybeKana(cur) && !maybeKana(prev)) return false;
     Character.UnicodeScript curScript = Character.UnicodeScript.of(cur);
     Character.UnicodeScript prevScript = Character.UnicodeScript.of(prev);
     if (prevScript == curScript) return false;
