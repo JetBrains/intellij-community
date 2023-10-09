efac4b0af628dd504a6b62e4f72ee63bd63eb412
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2023-10-09 19:27:22 +0200

    [vcs] IJPL-72638 convert GitDisposable to kotlin
    GitOrigin-RevId: 19fd489cce0c36d52e16e710ad1d013a74936ebf

--- plugins/git4idea/src/git4idea/GitDisposable.kt
+++ plugins/git4idea/src/git4idea/GitDisposable.kt
@@ -1,17 +1,18 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package git4idea;
+package git4idea
 
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.components.Service;
-import com.intellij.openapi.project.Project;
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
 
 @Service(Service.Level.PROJECT)
-public final class GitDisposable implements Disposable {
-  @Override
-  public void dispose() {
+class GitDisposable : Disposable {
+  override fun dispose() {
   }
 
-  public static GitDisposable getInstance(Project project) {
-    return project.getService(GitDisposable.class);
+  companion object {
+    @JvmStatic
+    fun getInstance(project: Project): GitDisposable = project.service()
   }
 }
