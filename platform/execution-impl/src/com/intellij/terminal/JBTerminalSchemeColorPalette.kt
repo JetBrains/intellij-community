08e448108831d3cafc9af6847c01777f94b9de47
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2023-10-09 16:52:38 +0300

    [terminal] IDEA-315836 Translate JBTerminalSchemeColorPalette to Kotlin
    GitOrigin-RevId: 3b213a778d0046c6d6074edcc7debf63a2f19f31

--- platform/execution-impl/src/com/intellij/terminal/JBTerminalSchemeColorPalette.kt
+++ platform/execution-impl/src/com/intellij/terminal/JBTerminalSchemeColorPalette.kt
@@ -1,56 +1,43 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.terminal;
+package com.intellij.terminal
 
-import com.intellij.execution.process.ColoredOutputTypeRegistryImpl;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.editor.colors.EditorColorsScheme;
-import com.intellij.openapi.editor.markup.TextAttributes;
-import com.jediterm.core.Color;
-import com.jediterm.terminal.emulator.ColorPalette;
-import com.jediterm.terminal.ui.AwtTransformers;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.execution.process.ColoredOutputTypeRegistryImpl
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.editor.colors.EditorColorsScheme
+import com.jediterm.core.Color
+import com.jediterm.terminal.emulator.ColorPalette
+import com.jediterm.terminal.ui.AwtTransformers
 
-public final class JBTerminalSchemeColorPalette extends ColorPalette {
-
-  private static final Logger LOG = Logger.getInstance(JBTerminalSchemeColorPalette.class);
-
-  private final EditorColorsScheme myColorsScheme;
-
-  JBTerminalSchemeColorPalette(@NotNull EditorColorsScheme scheme) {
-    myColorsScheme = scheme;
+internal class JBTerminalSchemeColorPalette(private val colorsScheme: EditorColorsScheme) : ColorPalette() {
+  override fun getForegroundByColorIndex(colorIndex: Int): Color {
+    val attributes = colorsScheme.getAttributes(ColoredOutputTypeRegistryImpl.getAnsiColorKey(colorIndex))
+    return when {
+      attributes.foregroundColor != null -> AwtTransformers.fromAwtColor(attributes.foregroundColor)!!
+      attributes.backgroundColor != null -> AwtTransformers.fromAwtColor(attributes.backgroundColor)!!
+      else -> {
+        if (LOG.isDebugEnabled()) {
+          LOG.debug("Default foreground color will be used for ANSI color index #$colorIndex")
+        }
+        AwtTransformers.fromAwtColor(colorsScheme.defaultForeground)!!
+      }
+    }
   }
 
-  @Override
-  protected @NotNull Color getForegroundByColorIndex(int colorIndex) {
-    TextAttributes attributes = myColorsScheme.getAttributes(ColoredOutputTypeRegistryImpl.getAnsiColorKey(colorIndex));
-    java.awt.Color foregroundColor = attributes.getForegroundColor();
-    if (foregroundColor != null) {
-      return AwtTransformers.fromAwtColor(foregroundColor);
+  override fun getBackgroundByColorIndex(colorIndex: Int): Color {
+    val attributes = colorsScheme.getAttributes(ColoredOutputTypeRegistryImpl.getAnsiColorKey(colorIndex))
+    return when {
+      attributes.backgroundColor != null -> AwtTransformers.fromAwtColor(attributes.backgroundColor)!!
+      attributes.foregroundColor != null -> AwtTransformers.fromAwtColor(attributes.foregroundColor)!!
+      else -> {
+        if (LOG.isDebugEnabled()) {
+          LOG.debug("Default background color will be used for ANSI color index #$colorIndex")
+        }
+        AwtTransformers.fromAwtColor(colorsScheme.defaultBackground)!!
+      }
     }
-    java.awt.Color backgroundColor = attributes.getBackgroundColor();
-    if (backgroundColor != null) {
-      return AwtTransformers.fromAwtColor(backgroundColor);
-    }
-    if (LOG.isDebugEnabled()) {
-      LOG.debug("Default foreground color will be used for ANSI color index #" + colorIndex);
-    }
-    return AwtTransformers.fromAwtColor(myColorsScheme.getDefaultForeground());
   }
 
-  @Override
-  protected @NotNull Color getBackgroundByColorIndex(int colorIndex) {
-    TextAttributes attributes = myColorsScheme.getAttributes(ColoredOutputTypeRegistryImpl.getAnsiColorKey(colorIndex));
-    java.awt.Color backgroundColor = attributes.getBackgroundColor();
-    if (backgroundColor != null) {
-      return AwtTransformers.fromAwtColor(backgroundColor);
-    }
-    java.awt.Color foregroundColor = attributes.getForegroundColor();
-    if (foregroundColor != null) {
-      return AwtTransformers.fromAwtColor(foregroundColor);
-    }
-    if (LOG.isDebugEnabled()) {
-      LOG.debug("Default background color will be used for ANSI color index #" + colorIndex);
-    }
-    return AwtTransformers.fromAwtColor(myColorsScheme.getDefaultBackground());
+  companion object {
+    private val LOG = Logger.getInstance(JBTerminalSchemeColorPalette::class.java)
   }
 }
