0c94a7526bd983a87a33b25c3c82758378999abc
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2023-10-10 14:10:18 +0300

    IDEA-333209 Move FilenameToolbarWidgetAction update to the service
    This way we can reuse this code for direct updates,
    bypassing the action system to update faster.
    
    GitOrigin-RevId: d72f150dd636c37191417a3934fb22eeca74447d

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/headertoolbar/FilenameToolbarWidgetAction.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/headertoolbar/FilenameToolbarWidgetAction.kt
@@ -2,13 +2,12 @@
 package com.intellij.openapi.wm.impl.headertoolbar
 
 import com.intellij.ide.impl.ProjectUtil
-import com.intellij.ide.ui.UISettings
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.actionSystem.ex.CustomComponentAction
 import com.intellij.openapi.components.service
-import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.impl.EditorHistoryManager.Companion.getInstance
 import com.intellij.openapi.project.DumbAwareAction
@@ -16,20 +15,13 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.ui.popup.PopupStep
 import com.intellij.openapi.ui.popup.util.BaseListPopupStep
-import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.util.Iconable
-import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.NlsSafe
-import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vcs.FileStatusManager
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.newvfs.VfsPresentationUtil
 import com.intellij.ui.ClickListener
-import com.intellij.ui.ColorUtil
-import com.intellij.ui.JBColor
 import com.intellij.ui.components.JBLabel
 import com.intellij.util.IconUtil
-import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
 import java.awt.Color
 import java.awt.Cursor
@@ -42,56 +34,14 @@ import javax.swing.JComponent
  */
 class FilenameToolbarWidgetAction: DumbAwareAction(), CustomComponentAction {
 
-  companion object {
-    private val FILE_COLOR: Key<Color> = Key.create("FILENAME_WIDGET_FILE_COLOR")
-    private val FILE_FULL_PATH: Key<String?> = Key.create("FILENAME_WIDGET_FILE_PATH")
-    private const val isIDEA331002Fixed = false //todo[mikhail.sokolov]
-  }
-
   override fun actionPerformed(e: AnActionEvent) {
   }
 
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 
   override fun update(e: AnActionEvent) {
-    e.presentation.isEnabledAndVisible = false
-    val uiSettings = UISettings.getInstance()
-    if (uiSettings.editorTabPlacement != UISettings.TABS_NONE && !(uiSettings.fullPathsInWindowHeader && isIDEA331002Fixed)) return
-    val project = e.project ?: return
-    val file = FileEditorManager.getInstance(project).selectedFiles.firstOrNull() ?: return
-    updatePresentationFromFile(project, file, e.presentation)
-    project.service<FilenameToolbarWidgetUpdateService>() // Just make sure it's initialized, it'll keep doing its thing then.
+    e.project?.service<FilenameToolbarWidgetUpdateService>()?.updatePresentation(e.presentation)
   }
-
-  private fun updatePresentationFromFile(project: Project, file: VirtualFile, presentation: Presentation) {
-    val status = FileStatusManager.getInstance(project).getStatus(file)
-    var fg:Color?
-
-    var icon = IconUtil.getIcon(file, Iconable.ICON_FLAG_READ_STATUS, project)
-    if (JBColor.isBright() && isDarkToolbar()) {
-      icon = IconLoader.getDarkIcon(icon, true)
-      fg = EditorColorsManager.getInstance().getScheme("Dark").getColor(status.colorKey)
-    }
-    else {
-      fg = status.color
-    }
-
-    if (fg == null) {
-      @Suppress("UnregisteredNamedColor")
-      fg = JBColor.namedColor("MainToolbar.Dropdown.foreground", JBColor.foreground())
-    }
-
-    @Suppress("HardCodedStringLiteral")
-    val filename = VfsPresentationUtil.getUniquePresentableNameForUI(project, file)
-    presentation.isEnabledAndVisible = true
-    presentation.putClientProperty(FILE_COLOR, fg)
-    presentation.putClientProperty(FILE_FULL_PATH, if (UISettings.getInstance().fullPathsInWindowHeader) file.path else null)
-    presentation.description = StringUtil.shortenTextWithEllipsis(filename, 60, 30)
-    presentation.icon = icon
-  }
-
-  @Suppress("UseJBColor")
-  private fun isDarkToolbar() = ColorUtil.isDark(JBColor.namedColor("MainToolbar.background", Color.WHITE))
 
   override fun createCustomComponent(presentation: Presentation, place: String): JBLabel = JBLabel().apply {
     isOpaque = false
@@ -148,19 +98,6 @@ class FilenameToolbarWidgetAction: DumbAwareAction(), CustomComponentAction {
   }
 
   override fun updateCustomComponent(component: JComponent, presentation: Presentation) {
-    (component as JBLabel).apply {
-      @Suppress("HardCodedStringLiteral")
-      val path = presentation.getClientProperty(FILE_FULL_PATH)
-      isOpaque = false
-      iconTextGap = JBUI.scale(4)
-      icon = presentation.icon
-      foreground = presentation.getClientProperty(FILE_COLOR)
-      text = presentation.description
-      if (path != null && isIDEA331002Fixed) {
-        val htmlColor = ColorUtil.toHtmlColor(JBColor.namedColor("Component.infoForeground", foreground))
-        @Suppress("HardCodedStringLiteral")
-        text = "<html><body>$text <font color='$htmlColor'>[$path]</font></body></html>"
-      }
-    }
+    ProjectUtil.getProjectForComponent(component)?.service<FilenameToolbarWidgetUpdateService>()?.updateComponent(component, presentation)
   }
 }
