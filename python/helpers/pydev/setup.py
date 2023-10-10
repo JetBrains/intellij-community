95bf7c3942b6692fa4614d44e922e269bec43631
Andrey Lisin <andrey.lisin@jetbrains.com>
2023-10-10 11:07:05 +0200

    Update Python debugger classifiers
    GitOrigin-RevId: 451611e0ab03c9385936b8ac5e0d7a3af874ce6c

--- python/helpers/pydev/setup.py
+++ python/helpers/pydev/setup.py
@@ -157,6 +157,9 @@ args = dict(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Software Development :: Debuggers',
     ],
     entry_points={
