9f4533fdaeb978293ae624d4222b011a71a9a302
Nikolai Rykunov <Nikolay.Rykunov@jetbrains.com>
2023-10-11 16:37:49 +0200

    Handle that colorPalette in Dark theme contains non String values (#170)
    GitOrigin-RevId: 9843548a5040825fe837302cb7874f668ebec4b4

--- platform/jewel/ide-laf-bridge/ide-laf-bridge-233/src/main/kotlin/org/jetbrains/jewel/bridge/BridgePaletteMapperFactory.kt
+++ platform/jewel/ide-laf-bridge/ide-laf-bridge-233/src/main/kotlin/org/jetbrains/jewel/bridge/BridgePaletteMapperFactory.kt
@@ -17,7 +17,15 @@ object BridgePaletteMapperFactory : PaletteMapperFactory() {
 
         val bean = uiTheme.describe()
 
-        val iconColorPalette = bean.colorPalette
+        // TODO: in New UI + Dark theme values can be non String, but IJColorUIResource
+        //  since these are deserialized values simply casted to Map<String, String>.
+        //  Let's handle it while it is not fixed in platform
+        val iconColorPalette = (bean.colorPalette as Map<String, Any?>).mapValues {
+            when (val value = it.value) {
+                is String -> value
+                else -> null
+            }
+        }
         val keyPalette = UITheme.getColorPalette()
         val themeColors = bean.colors
 
