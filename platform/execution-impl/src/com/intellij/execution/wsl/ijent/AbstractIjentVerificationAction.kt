facb5ce247ef2a9a842b9ed92c0e70b098352bea
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2023-10-11 12:53:49 +0200

    IJent: move internal smoke testing actions into ijent.impl
    GitOrigin-RevId: 5dc76403e65a9a651ccd7add507e6adff2f7f838

--- platform/execution-impl/src/com/intellij/execution/wsl/ijent/AbstractIjentVerificationAction.kt
+++ platform/execution-impl/src/com/intellij/execution/wsl/ijent/AbstractIjentVerificationAction.kt
@@ -1,9 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 @file:Suppress("DialogTitleCapitalization", "HardCodedStringLiteral")
-
 package com.intellij.execution.wsl.ijent
 
-import com.intellij.execution.wsl.WslDistributionManager
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
@@ -18,108 +16,21 @@ import com.intellij.openapi.progress.TaskCancellation
 import com.intellij.openapi.progress.withModalProgress
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.ui.Messages
-import com.intellij.openapi.util.SystemInfo
-import com.intellij.platform.ijent.*
+import com.intellij.platform.ijent.IjentApi
+import com.intellij.platform.ijent.IjentMissingBinary
 import com.intellij.platform.ijent.fs.nio.asNioFileSystem
 import com.intellij.util.childScope
-import com.intellij.util.system.CpuArch
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.consumeEach
+import org.jetbrains.annotations.ApiStatus.Internal
 import java.io.ByteArrayOutputStream
-import java.nio.file.Path
-import java.time.LocalDateTime
-import java.time.ZoneOffset
-import kotlin.io.path.absolutePathString
 import kotlin.io.path.isDirectory
 
-class IjentWslVerificationAction : AbstractIjentVerificationAction() {
-  override fun update(e: AnActionEvent) {
-    super.update(e)
-    e.presentation.run {
-      text = "Test IJent + WSL"
-      isEnabled = isEnabled && WslDistributionManager.getInstance().installedDistributions.isNotEmpty()
-    }
-  }
-
-  override suspend fun launchIjent(childScope: CoroutineScope): Pair<IjentApi, String> {
-    val wslDistribution = WslDistributionManager.getInstance().installedDistributions.first()
-    val ijent = deployAndLaunchIjent(
-      ijentCoroutineScope = childScope,
-      project = null,
-      wslDistribution = wslDistribution,
-    )
-    return ijent to "IJent on $wslDistribution: uname -a"
-  }
-}
-
-class IjentDockerVerificationAction : AbstractIjentVerificationAction() {
-  private val dockerImage = "ubuntu:22.04"
-
-  override fun update(e: AnActionEvent) {
-    super.update(e)
-    e.presentation.text = "Test IJent + Docker $dockerImage"
-  }
-
-  override suspend fun launchIjent(childScope: CoroutineScope): Pair<IjentApi, String> {
-    val containerName = "ijent-test-${LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)}"
-
-    childScope.launch {
-      try {
-        awaitCancellation()
-      }
-      finally {
-        @OptIn(DelicateCoroutinesApi::class)
-        GlobalScope.launch(Dispatchers.IO) {
-          ProcessBuilder().command("docker", "rm", "-vf", containerName).start()
-        }
-      }
-    }
-
-    val targetPlatform = when (val arch = CpuArch.CURRENT) {
-      CpuArch.ARM64 -> IjentExecFileProvider.SupportedPlatform.AARCH64__LINUX
-
-      CpuArch.X86_64 -> IjentExecFileProvider.SupportedPlatform.X86_64__LINUX
-
-      null, CpuArch.X86, CpuArch.ARM32, CpuArch.OTHER, CpuArch.UNKNOWN -> error("Unsupported CPU arch: $arch")
-    }
-
-    val ijentLinuxBinary = IjentExecFileProvider.getIjentBinary(targetPlatform)
-
-    val process = withContext(Dispatchers.IO) {
-      ProcessBuilder()
-        .command(
-          "docker",
-          "run",
-          "--interactive",
-          "--rm",
-          "--volume",
-          "${ijentLinuxBinary.toBindMount()}:/ijent:ro",
-          "--name",
-          containerName,
-          dockerImage,
-          *getIjentGrpcArgv("/ijent").toTypedArray(),
-        )
-        .redirectInput(ProcessBuilder.Redirect.PIPE)
-        .redirectOutput(ProcessBuilder.Redirect.PIPE)
-        .redirectError(ProcessBuilder.Redirect.PIPE)
-        .start()
-    }
-
-    return IjentSessionProvider.connect(childScope, process) to "IJent on Docker $dockerImage"
-  }
-
-  /**
-   * https://docs.docker.com/desktop/troubleshoot/topics/#path-conversion-on-windows
-   */
-  private fun Path.toBindMount(): String =
-    if (SystemInfo.isWindows)
-      absolutePathString()
-        .replace("\\", "/")
-        .let { "/${it.substring(0, 1).lowercase()}${it.substring(2)}" }
-    else
-      absolutePathString()
-}
-
+/**
+ * Base class for internal smoke testing of IJent.
+ * It was deliberately put into the core part, near WSL utilities, in order to be sure that IJent can be integrated into WSL support.
+ */
+@Internal
 abstract class AbstractIjentVerificationAction : DumbAwareAction() {
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 
