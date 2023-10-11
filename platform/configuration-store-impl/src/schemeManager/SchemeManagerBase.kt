e73b466e8544ed06457ca86badc6c7e5fcd31770
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2023-10-11 13:43:46 +0200

    diagnostics: add more debug-level logging to SchemeManagerBase and KeymapManagerImpl
    ...and enable it in RemoteDevConfigShareTest to find the cause of flaky test (CWM-8547).
    
    GitOrigin-RevId: a3cbbefc36a850b6c588a0cdd776e353fa093f0d

--- platform/configuration-store-impl/src/schemeManager/SchemeManagerBase.kt
+++ platform/configuration-store-impl/src/schemeManager/SchemeManagerBase.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.configurationStore.schemeManager
 
+import com.intellij.configurationStore.LOG
+import com.intellij.openapi.diagnostic.debug
 import com.intellij.openapi.options.Scheme
 import com.intellij.openapi.options.SchemeManager
 import com.intellij.openapi.options.SchemeProcessor
@@ -21,6 +23,7 @@ abstract class SchemeManagerBase<T: Scheme, in MUTABLE_SCHEME : T>(internal val 
     set(schemeName) = setCurrentSchemeName(schemeName, true)
 
   internal fun processPendingCurrentSchemeName(newScheme: T): Boolean {
+    LOG.debug { "processPendingCurrentSchemeName: newScheme=$newScheme, currentPendingSchemeName=$currentPendingSchemeName"}
     if (processor.getSchemeKey(newScheme) == currentPendingSchemeName) {
       setCurrent(newScheme, false)
       return true
@@ -29,6 +32,7 @@ abstract class SchemeManagerBase<T: Scheme, in MUTABLE_SCHEME : T>(internal val 
   }
 
   override fun setCurrent(scheme: T?, notify: Boolean, processChangeSynchronously: Boolean) {
+    LOG.debug { "setCurrent: scheme=$scheme" }
     currentPendingSchemeName = null
 
     val oldCurrent = activeScheme
@@ -40,8 +44,10 @@ abstract class SchemeManagerBase<T: Scheme, in MUTABLE_SCHEME : T>(internal val 
 
   override fun setCurrentSchemeName(schemeName: String?, notify: Boolean) {
     currentPendingSchemeName = schemeName
+    LOG.debug { "setCurrentSchemeName: schemeName=$schemeName" }
 
     val scheme = schemeName?.let { findSchemeByName(it) }
+    LOG.debug { "setCurrentSchemeName: scheme=$scheme" }
     // don't set a current scheme if no scheme by name - pending resolution (see currentSchemeName field comment)
     if (scheme != null || schemeName == null) {
       setCurrent(scheme, notify)
