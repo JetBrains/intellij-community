d899f054839663e44128aebcd6eed9dc5cd9dd66
Aleksey Pivovarov <AMPivovarov@gmail.com>
2023-10-12 15:18:03 +0200

    vcs: convert VcsLocaleHelper to Kotlin
    GitOrigin-RevId: 5459516118592b1b47c6687fb302d53bfd4f4153

--- platform/vcs-impl/src/com/intellij/vcs/VcsLocaleHelper.kt
+++ platform/vcs-impl/src/com/intellij/vcs/VcsLocaleHelper.kt
@@ -1,40 +1,34 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.vcs;
+package com.intellij.vcs
 
-import com.intellij.openapi.util.registry.Registry;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.util.registry.Registry
+import org.jetbrains.annotations.NonNls
+import java.util.*
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.MissingResourceException;
+object VcsLocaleHelper {
+  private val DEFAULT_EXECUTABLE_LOCALE_VALUE: @NonNls String = "en_US.UTF-8"
+  private val REGISTRY_KEY_SUFFIX: @NonNls String = ".executable.locale"
 
-public final class VcsLocaleHelper {
-
-  private static final @NonNls String DEFAULT_EXECUTABLE_LOCALE_VALUE = "en_US.UTF-8";
-  private static final @NonNls String REGISTRY_KEY_SUFFIX = ".executable.locale";
-
-  @NonNls
-  @NotNull
-  public static String getDefaultLocaleFromRegistry(@NotNull @NonNls String prefix) {
-    String registryKey = prefix + REGISTRY_KEY_SUFFIX;
+  @JvmStatic
+  fun getDefaultLocaleFromRegistry(prefix: @NonNls String): @NonNls String {
     try {
-      return Registry.stringValue(registryKey);
+      return Registry.stringValue(prefix + REGISTRY_KEY_SUFFIX)
     }
-    catch (MissingResourceException e) {
-      return DEFAULT_EXECUTABLE_LOCALE_VALUE;
+    catch (e: MissingResourceException) {
+      return DEFAULT_EXECUTABLE_LOCALE_VALUE
     }
   }
 
-  @NotNull
-  public static Map<String, String> getDefaultLocaleEnvironmentVars(@NotNull @NonNls String prefix) {
-    Map<String, String> envMap = new LinkedHashMap<>();
-    String defaultLocale = getDefaultLocaleFromRegistry(prefix);
+  @JvmStatic
+  fun getDefaultLocaleEnvironmentVars(prefix: @NonNls String): Map<String, String> {
+    val envMap = LinkedHashMap<String, String>()
+    val defaultLocale = getDefaultLocaleFromRegistry(prefix)
     if (defaultLocale.isEmpty()) { // let skip locale definition if needed
-      return envMap;
+      return envMap
     }
-    envMap.put("LANGUAGE", ""); //NON-NLS
-    envMap.put("LC_ALL", defaultLocale); //NON-NLS
-    return envMap;
+
+    envMap["LANGUAGE"] = "" //NON-NLS
+    envMap["LC_ALL"] = defaultLocale //NON-NLS
+    return envMap
   }
 }
