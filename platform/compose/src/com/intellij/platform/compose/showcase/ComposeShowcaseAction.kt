71107088eb30f7d6c509cfb7489b6928c44a2d5c
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2023-10-13 17:56:14 +0200

    [compose] Move files in compose module to proper package
    GitOrigin-RevId: 68a44f536a0e18da72813dfc7c68564ad441e7ab

--- platform/compose/src/com/intellij/platform/compose/showcase/ComposeShowcaseAction.kt
+++ platform/compose/src/com/intellij/platform/compose/showcase/ComposeShowcaseAction.kt
@@ -1,12 +1,12 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.compose.showcase
+package com.intellij.platform.compose.showcase
 
-import com.intellij.compose.JBComposePanel
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.util.NlsSafe
+import com.intellij.platform.compose.JBComposePanel
 import com.intellij.ui.PaintingParent.Wrapper
 import java.awt.Dimension
 import javax.swing.JComponent
