2093c0322aff2cb0e10cece7f32788817a53ad71
Yann Cébron <yann.cebron@jetbrains.com>
2023-10-16 18:03:43 +0200

    [spellchecker] annotate Topic level
    GitOrigin-RevId: 03d69ff330a1e9935541b95299e94fe61b6737c8

--- spellchecker/src/com/intellij/spellchecker/settings/CustomDictionaryPathListener.kt
+++ spellchecker/src/com/intellij/spellchecker/settings/CustomDictionaryPathListener.kt
@@ -1,3 +1,4 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.spellchecker.settings
 
 import com.intellij.util.messages.Topic
@@ -7,6 +8,7 @@ interface CustomDictionaryPathListener {
   fun dictionariesChanged(paths: List<String>)
 
   companion object {
+    @Topic.AppLevel
     val TOPIC: Topic<CustomDictionaryPathListener> = Topic(CustomDictionaryPathListener::class.java)
   }
 }
