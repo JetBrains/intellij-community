a9c0652d81fe03c5577fe049d1e75e1df9ea82e7
Ivan Posti <ivan.posti@jetbrains.com>
2023-10-16 12:33:54 +0200

    [Markdown] Extract scratch file checks to a dedicated method
    GitOrigin-RevId: 893c858f3f27275ed0724a2a218d506b9da67af3

--- plugins/markdown/core/src/org/intellij/plugins/markdown/ui/floating/AddFloatingToolbarTextEditorCustomizer.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/ui/floating/AddFloatingToolbarTextEditorCustomizer.kt
@@ -1,12 +1,10 @@
 package org.intellij.plugins.markdown.ui.floating
 
-import com.intellij.ide.scratch.ScratchUtil
-import com.intellij.lang.LanguageUtil
 import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.fileEditor.impl.text.TextEditorCustomizer
 import com.intellij.openapi.options.advanced.AdvancedSettings
 import com.intellij.openapi.util.Disposer
-import org.intellij.plugins.markdown.lang.MarkdownLanguage
+import org.intellij.plugins.markdown.editor.isMarkdownScratchFile
 import org.intellij.plugins.markdown.lang.hasMarkdownType
 import org.intellij.plugins.markdown.util.MarkdownPluginScope
 
@@ -28,6 +26,6 @@ private class AddFloatingToolbarTextEditorCustomizer: TextEditorCustomizer {
   private fun shouldAcceptScratchFile(editor: TextEditor): Boolean {
     val file = editor.file
     val project = editor.editor.project ?: return false
-    return ScratchUtil.isScratch(file) && LanguageUtil.getLanguageForPsi(project, file, file.fileType) == MarkdownLanguage.INSTANCE
+    return isMarkdownScratchFile(project, file)
   }
 }
--- plugins/markdown/core/src/org/intellij/plugins/markdown/ui/preview/MarkdownPreviewFileEditorProvider.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/ui/preview/MarkdownPreviewFileEditorProvider.kt
@@ -1,29 +1,19 @@
 package org.intellij.plugins.markdown.ui.preview
 
-import com.intellij.ide.scratch.ScratchUtil
-import com.intellij.lang.LanguageUtil
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorPolicy
 import com.intellij.openapi.fileEditor.WeighedFileEditorProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import org.intellij.plugins.markdown.editor.isMarkdownScratchFile
 import org.intellij.plugins.markdown.lang.hasMarkdownType
-import org.intellij.plugins.markdown.lang.isMarkdownLanguage
 
 internal class MarkdownPreviewFileEditorProvider: WeighedFileEditorProvider() {
   override fun accept(project: Project, file: VirtualFile): Boolean {
     if (!MarkdownHtmlPanelProvider.hasAvailableProviders()) {
       return false
     }
-    return file.hasMarkdownType() || isMarkdownScratch(project, file)
-  }
-
-  private fun isMarkdownScratch(project: Project, file: VirtualFile): Boolean {
-    if (!ScratchUtil.isScratch(file)) {
-      return false
-    }
-    val language = LanguageUtil.getLanguageForPsi(project, file) ?: return false
-    return language.isMarkdownLanguage()
+    return file.hasMarkdownType() || isMarkdownScratchFile(project, file)
   }
 
   override fun createEditor(project: Project, file: VirtualFile): FileEditor {
