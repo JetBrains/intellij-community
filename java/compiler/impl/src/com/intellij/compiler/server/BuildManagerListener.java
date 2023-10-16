62d8759f01b3b9bceea193855cbf51ec76aba39b
Yann Cébron <yann.cebron@jetbrains.com>
2023-10-16 18:02:42 +0200

    [java] annotate Topic level
    GitOrigin-RevId: b64ccde9cf72f39247c9450c4d80a3daac1a1b9f

--- java/compiler/impl/src/com/intellij/compiler/server/BuildManagerListener.java
+++ java/compiler/impl/src/com/intellij/compiler/server/BuildManagerListener.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.compiler.server;
 
 import com.intellij.openapi.project.Project;
@@ -11,6 +11,8 @@ import java.util.UUID;
  * @author Eugene Zhuravlev
  */
 public interface BuildManagerListener {
+
+  @Topic.AppLevel
   Topic<BuildManagerListener> TOPIC = new Topic<>(BuildManagerListener.class, Topic.BroadcastDirection.TO_DIRECT_CHILDREN);
 
   default void beforeBuildProcessStarted(@NotNull Project project, @NotNull UUID sessionId) {}
