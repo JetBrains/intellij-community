f13c1be649c57a0825474aae0ef0835c23ee5261
Dmitry Pogrebnoy <dmitry.pogrebnoy@jetbrains.com>
2023-10-17 14:47:42 +0200

    [Feedback] KTIJ-27425 Introduce Kotlin Onboarding Satisfaction survey
    (cherry picked from commit a8a2a8fefb1b83cd036f3f217f19838f1d797266)
    (cherry picked from commit 751b6e259193255c1fbeb488fd97ac46c89ca295)
    
    IJ-CR-117015
    
    GitOrigin-RevId: 44beeb53193eeddaf3538706e8dbb9eeb14de7b6

---
+++ plugins/kotlin/onboarding/src/org/jetbrains/kotlin/onboarding/FeedbackDialog.kt
@@ -0,0 +1,57 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.onboarding
+
+import com.intellij.openapi.project.Project
+import com.intellij.platform.feedback.dialog.BlockBasedFeedbackDialog
+import com.intellij.platform.feedback.dialog.CommonFeedbackSystemData
+import com.intellij.platform.feedback.dialog.showFeedbackSystemInfoDialog
+import com.intellij.platform.feedback.dialog.uiBlocks.*
+import com.intellij.platform.feedback.impl.notification.ThanksForFeedbackNotification
+
+class FeedbackDialog(
+    project: Project?,
+    forTest: Boolean
+) : BlockBasedFeedbackDialog<CommonFeedbackSystemData>(project, forTest) {
+
+    /** Increase the additional number when feedback format is changed */
+    override val myFeedbackJsonVersion: Int = super.myFeedbackJsonVersion + 1
+    override val myFeedbackReportId: String = "kotlin_onboarding"
+
+    override val mySystemInfoData: CommonFeedbackSystemData by lazy {
+        CommonFeedbackSystemData.getCurrentData()
+    }
+
+    override val myShowFeedbackSystemInfoDialog: () -> Unit = {
+        showFeedbackSystemInfoDialog(myProject, mySystemInfoData)
+    }
+
+    override val myTitle: String = FeedbackBundle.message("dialog.top.title")
+    override val myBlocks: List<FeedbackBlock> = listOf(
+        TopLabelBlock(FeedbackBundle.message("dialog.title")),
+        RatingBlock(
+            FeedbackBundle.message("dialog.satisfaction.rating.label"),
+            "satisfaction_rating"
+        ),
+        ComboBoxBlock(
+            FeedbackBundle.message("dialog.previous.language.label"),
+            List(14) { FeedbackBundle.message("dialog.previous.language.${it + 1}") },
+            "previous_language"
+        ),
+        TextAreaBlock(
+            FeedbackBundle.message("dialog.suggestion.to.improve.label"),
+            "suggestion_to_improve"
+        )
+    )
+
+    init {
+        init()
+    }
+
+    override fun showThanksNotification() {
+        ThanksForFeedbackNotification(
+            description = FeedbackBundle.message(
+                "notification.thanks.feedback.content"
+            )
+        ).notify(myProject)
+    }
+}
