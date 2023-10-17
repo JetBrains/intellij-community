9207d989efc6abd720fa9c619ef7c64f921aa9e9
Yann Cébron <yann.cebron@jetbrains.com>
2023-10-17 11:10:13 +0200

    [platform] annotate Topic level
    GitOrigin-RevId: 8d926bc05ac5eb7691bb3b8b3469fa70a941c347

--- platform/credential-store/src/PasswordSafeSettings.kt
+++ platform/credential-store/src/PasswordSafeSettings.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.credentialStore
 
 import com.intellij.ide.passwordSafe.impl.getDefaultKeePassDbFile
@@ -17,6 +17,7 @@ private val defaultProviderType: ProviderType
 class PasswordSafeSettings : PersistentStateComponentWithModificationTracker<PasswordSafeOptions> {
   companion object {
     @JvmField
+    @Topic.AppLevel
     val TOPIC: Topic<PasswordSafeSettingsListener> = Topic("PasswordSafeSettingsListener", PasswordSafeSettingsListener::class.java)
   }
 
