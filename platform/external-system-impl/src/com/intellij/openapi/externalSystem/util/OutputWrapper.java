94f8449eac69a410362614be1a60adb1a7074ef1
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2023-10-17 16:04:43 +0400

    [gradle] do not send empty task output when empty buffer is flushed
    GitOrigin-RevId: 2a7c063c9a53994dbb1deb8ade1d740c55563dc7

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/util/OutputWrapper.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/util/OutputWrapper.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.externalSystem.util;
 
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId;
@@ -44,7 +44,7 @@ public class OutputWrapper extends OutputStream {
   }
 
   private void doFlush() {
-    if (myBuffer == null) {
+    if (myBuffer == null || myBuffer.isEmpty()) {
       return;
     }
     myListener.onTaskOutput(myTaskId, myBuffer.toString(), myStdOut);
