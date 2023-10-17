7f34739d2bb74cdadeaca933b20381cf09eacbb2
Ivan Posti <ivan.posti@jetbrains.com>
2023-10-17 15:02:19 +0200

    [Markdown] Mark ImagePsiElementFactory with @ApiStatus.Internal
    GitOrigin-RevId: 5e4971714adb9788d4bf40532d277aec46cd93b6

--- plugins/markdown/images/src/main/kotlin/org/intellij/plugins/markdown/images/editor/ImagePsiElementFactory.kt
+++ plugins/markdown/images/src/main/kotlin/org/intellij/plugins/markdown/images/editor/ImagePsiElementFactory.kt
@@ -3,8 +3,10 @@ package org.intellij.plugins.markdown.images.editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import org.intellij.plugins.markdown.lang.psi.MarkdownPsiElementFactory
+import org.jetbrains.annotations.ApiStatus
 
-internal object ImagePsiElementFactory {
+@ApiStatus.Internal
+object ImagePsiElementFactory {
   fun createImage(project: Project, description: String?, path: String, title: String?): PsiElement {
     val text = ImageUtils.createMarkdownImageText(description.orEmpty(), path, title.orEmpty())
     return MarkdownPsiElementFactory.createFile(project, text).firstChild.firstChild
