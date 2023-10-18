404c3c8728d800140004510ba99777073907c067
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2023-10-18 14:41:40 +0300

    [Kotlin] K2: Decompile built-ins from plugin classloader without filtering
    Split stub versions for .kotlin_builtins with K1 and K2 IDE to force
    reindexing between switches.
    
    The built-ins from plugin classloader are used by default with the lowest
    priority in modules of any platform, including common MPP modules.
    kotlin-stdlib-common doesn't contain some of the built-in declarations
    in .kotlin_metadata files and doesn't have .kotlin_builtins of its own.
    Not providing the declarations in the decompiled .kotlin_builtins leads
    to unresolved references for some of the built-in classes, like kotlin.Unit.
    
    KT-61757
    
    GitOrigin-RevId: dae0e6fa191987ca54b61a1bbcf0962405115865

---
+++ plugins/kotlin/base/indices/src/org/jetbrains/kotlin/idea/stubindex/IdeKotlinBuiltInStubVersionOffsetProvider.kt
@@ -0,0 +1,23 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+package org.jetbrains.kotlin.idea.stubindex
+
+import org.jetbrains.kotlin.analysis.decompiler.psi.KotlinBuiltInStubVersionOffsetProvider
+import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginKind
+import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginKindProvider
+
+/**
+ * Applies no changes to the K1 IDE stub version and adds a big constant offset to the K2 IDE stub version for .kotlin_builtins files.
+ * It should be practically impossible to get a big enough stub version with K1 for it to clash with the K2 version range.
+ * See the comment in [KotlinBuiltInStubVersionOffsetProvider] for the reasons why the offset is needed.
+ */
+internal class IdeKotlinBuiltInStubVersionOffsetProvider : KotlinBuiltInStubVersionOffsetProvider {
+    override fun getVersionOffset(): Int {
+        return when (KotlinPluginKindProvider.currentPluginKind) {
+            KotlinPluginKind.FE10_PLUGIN -> 0
+            KotlinPluginKind.FIR_PLUGIN -> K2_BUILTINS_STUB_VERSION_OFFSET
+        }
+    }
+}
+
+private const val K2_BUILTINS_STUB_VERSION_OFFSET = 100000
