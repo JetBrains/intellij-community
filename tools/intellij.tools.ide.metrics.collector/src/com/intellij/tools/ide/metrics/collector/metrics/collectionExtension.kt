c5c3beddf067723616940d8d7539bdcae67f8caa
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2023-10-18 12:33:44 +0300

    [metrics] IJ-CR-116910 Taking into account even and odd collection size for median calculation
    GitOrigin-RevId: 5f06b9a7d0b15e1f5252c69c213af485976bcdc2

--- tools/intellij.tools.ide.metrics.collector/src/com/intellij/tools/ide/metrics/collector/metrics/collectionExtension.kt
+++ tools/intellij.tools.ide.metrics.collector/src/com/intellij/tools/ide/metrics/collector/metrics/collectionExtension.kt
@@ -4,8 +4,16 @@ package com.intellij.tools.ide.metrics.collector.metrics
 fun Iterable<Long>.median(): Long {
   val size = this.count()
   require(size > 0) { "Cannot calculate median value because collection is empty" }
+  val sortedList = this.sorted()
 
-  return this.sorted()[size / 2]
+  // odd collection size
+  return if (size % 2 == 1) {
+    sortedList[size / 2]
+  }
+  // even collection size
+  else {
+    (sortedList[size / 2 - 1] + sortedList[size / 2]) / 2
+  }
 }
 
 fun Iterable<PerformanceMetrics.Metric>.medianValue(): Long = this.map { it.value }.median()
