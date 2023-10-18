b9f0c63ecaf24355fc47a8e08f1494810f7c1c9d
Igor.Davidenko <igor.davidenko@jetbrains.com>
2023-10-18 16:09:27 +0200

    [full-line] ML-1555 Add top context features for inline completion
    GitOrigin-RevId: 564f2e9f6fe80200e8db19e2877af825930b9060

---
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/logs/CharCategory.kt
@@ -0,0 +1,19 @@
+package com.intellij.codeInsight.inline.completion.logs
+
+@Suppress("unused")
+enum class CharCategory(val chars: CharSequence) {
+  LETTER(('a'..'z').joinToString("")),
+  CAPITAL_LETTER(('A'..'Z').joinToString("")),
+  UNDERSCORE("_"),
+  NUMBER(('0'..'9').joinToString("")),
+  QUOTE("\"\'`"),
+  OPENING_BRACKET("([{"),
+  CLOSING_BRACKET(")]}"),
+  SIGN("=+-></\\|"),
+  PUNCTUATION(".,:;?!"),
+  SYMBOL("#$%&*^@~");
+
+  companion object {
+    fun find(char: Char): CharCategory? = entries.find { it.chars.contains(char) }
+  }
+}
