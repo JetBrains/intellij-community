ae9f7f0882c4fec4451ba45a2880c26eb9c7fce3
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-10-19 20:21:53 +0200

    Drop AllowDifferentMembersInActual from stdlib
    KT-62656
    Caused by: https://jetbrains.team/p/kt/reviews/12658/timeline
    
    GitOrigin-RevId: 388c13d4472df83d28199e4ef44f7a17797575bf

--- plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/regress/ktij22345SyntheticJavaProperties/src/jvmMain/kotlin/jvm.kt
+++ plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/regress/ktij22345SyntheticJavaProperties/src/jvmMain/kotlin/jvm.kt
@@ -1,9 +1,9 @@
 //region Test configuration
 // - hidden: line markers
 //endregion
-@file:Suppress("ACTUAL_CLASSIFIER_MUST_HAVE_THE_SAME_SUPERTYPES_AS_NON_FINAL_EXPECT_CLASSIFIER", "ACTUAL_CLASSIFIER_MUST_HAVE_THE_SAME_MEMBERS_AS_NON_FINAL_EXPECT_CLASSIFIER")
-<!HIGHLIGHTING("severity='WARNING'; descr='[EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING] 'expect'/'actual' classes (including interfaces, objects, annotations, enums, and 'actual' typealiases) are in Beta. You can use -Xexpect-actual-classes flag to suppress this warning. Also see: https://youtrack.jetbrains.com/issue/KT-61573'")!>actual<!> open class MyFile1 actual constructor(userPath: String): MyJavaFile(userPath)
-<!HIGHLIGHTING("severity='WARNING'; descr='[EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING] 'expect'/'actual' classes (including interfaces, objects, annotations, enums, and 'actual' typealiases) are in Beta. You can use -Xexpect-actual-classes flag to suppress this warning. Also see: https://youtrack.jetbrains.com/issue/KT-61573'")!>actual<!> typealias MyFile2 = java.io.File
+actual open class MyFile1 actual constructor(userPath: String): MyJavaFile(userPath)
+
+actual typealias MyFile2 = java.io.File
 
 fun testFile1(file: KotlinFile1) {
     file.getAbsolutePath()
