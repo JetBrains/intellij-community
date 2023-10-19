72540716ec01abc2a6c3f58b046e8e4147b73f07
Artem.Bukhonov <artem.bukhonov@jetbrains.com>
2023-10-19 20:26:41 +0200

    RDCT-633 Alt-Insert doesn't work in Project View
    Markdown promoter wrongly promoted actions in the case when no MD file is active (the condition was invalid). This prevended New element action to work in Remote Dev mode
    
    GitOrigin-RevId: b63f7caeae694867b9bbc145e26532e6f925be94

--- plugins/markdown/core/src/org/intellij/plugins/markdown/ui/actions/MarkdownActionPromoter.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/ui/actions/MarkdownActionPromoter.kt
@@ -18,9 +18,9 @@ internal class MarkdownActionPromoter: ActionPromoter {
 
   override fun promote(actions: List<AnAction>, context: DataContext): List<AnAction> {
     val file = context.getData(PlatformCoreDataKeys.FILE_EDITOR)?.file
-    if (file?.hasMarkdownType() == false) {
-      return emptyList()
+    if (file?.hasMarkdownType() == true) {
+      return actions.filter { ActionManager.getInstance().getId(it) in promotedActions }
     }
-    return actions.filter { ActionManager.getInstance().getId(it) in promotedActions }
+    return emptyList()
   }
 }
