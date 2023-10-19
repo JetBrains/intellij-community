edbbeaa330660de595d373728a276f63a819ac0e
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2023-10-19 23:18:29 +0200

    Fix test cases without tests
    Either by using `@Ignore` or making open class abstract
    
    GitOrigin-RevId: bae4d594cf8c326d10dbfffc730f0eeefdba65b1

--- platform/workspace/jps/tests/testSrc/com/intellij/workspaceModel/ide/VirtualFileUrlManagerTest.kt
+++ platform/workspace/jps/tests/testSrc/com/intellij/workspaceModel/ide/VirtualFileUrlManagerTest.kt
@@ -1,14 +1,12 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.workspaceModel.ide
 
+import com.intellij.platform.workspace.storage.url.VirtualFileUrlManager
 import com.intellij.testFramework.ApplicationRule
 import com.intellij.testFramework.rules.ProjectModelRule
-import com.intellij.platform.workspace.storage.url.VirtualFileUrlManager
-import org.junit.Assert
-import org.junit.Before
-import org.junit.ClassRule
-import org.junit.Rule
+import org.junit.*
 
+@Ignore
 class VirtualFileUrlManagerTest {
   @Rule
   @JvmField
@@ -21,18 +19,18 @@ class VirtualFileUrlManagerTest {
     virtualFileManager = VirtualFileUrlManager.getInstance(projectModel.project)
   }
 
-  //@Test
-  //fun `check isEqualOrParentOf`() {
-  //  assertIsEqualOrParentOf(true, "temp:///src", "temp:///src/my")
-  //  assertIsEqualOrParentOf(true, "temp:///src", "temp:///src/my/")
-  //  assertIsEqualOrParentOf(false, "temp:///src", "temp:///srC/my")
-  //  assertIsEqualOrParentOf(false, "temp:///src/x", "temp:///src/y")
-  //  assertIsEqualOrParentOf(false, "file:///src/my", "temp:///src/my")
-  //  assertIsEqualOrParentOf(false, "file:///src/my", "temp:///src/my")
-  //  assertIsEqualOrParentOf(false, "", "temp:///src/my")
-  //  assertIsEqualOrParentOf(false, "temp:///src/my", "")
-  //  assertIsEqualOrParentOf(true, "temp://", "temp:///src/my")
-  //}
+  @Test
+  fun `check isEqualOrParentOf`() {
+    assertIsEqualOrParentOf(true, "temp:///src", "temp:///src/my")
+    assertIsEqualOrParentOf(true, "temp:///src", "temp:///src/my/")
+    assertIsEqualOrParentOf(false, "temp:///src", "temp:///srC/my")
+    assertIsEqualOrParentOf(false, "temp:///src/x", "temp:///src/y")
+    assertIsEqualOrParentOf(false, "file:///src/my", "temp:///src/my")
+    assertIsEqualOrParentOf(false, "file:///src/my", "temp:///src/my")
+    assertIsEqualOrParentOf(false, "", "temp:///src/my")
+    assertIsEqualOrParentOf(false, "temp:///src/my", "")
+    assertIsEqualOrParentOf(true, "temp://", "temp:///src/my")
+  }
 
   private fun assertIsEqualOrParentOf(expectedResult: Boolean, parentString: String, childString: String) {
     val parent = virtualFileManager.fromUrl(parentString)
