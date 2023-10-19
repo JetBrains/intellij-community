b91b8ddf111fb501a48b3422a5eea419357308d4
Yann Cébron <yann.cebron@jetbrains.com>
2023-10-19 14:37:12 +0200

    [editorconfig] EditorConfigPatternEnumerationRedundancyInspection: cleanup
    GitOrigin-RevId: 09a34e70fd65f149c0bef0bcbda18dba086cd204

--- plugins/editorconfig/src/org/editorconfig/language/psi/base/EditorConfigHeaderBase.kt
+++ plugins/editorconfig/src/org/editorconfig/language/psi/base/EditorConfigHeaderBase.kt
@@ -4,8 +4,8 @@ package org.editorconfig.language.psi.base
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiErrorElement
 import com.intellij.psi.util.PsiTreeUtil
-import org.editorconfig.language.codeinsight.inspections.EditorConfigPatternEnumerationRedundancyInspection
 import org.editorconfig.language.codeinsight.inspections.hasNumerousWildcards
+import org.editorconfig.language.codeinsight.inspections.hasRedundancy
 import org.editorconfig.language.codeinsight.inspections.isEmptyHeader
 import org.editorconfig.language.psi.EditorConfigEnumerationPattern
 import org.editorconfig.language.psi.EditorConfigHeader
@@ -20,13 +20,10 @@ abstract class EditorConfigHeaderBase(node: ASTNode) : EditorConfigHeaderElement
     if (nextSibling is PsiErrorElement) return false
     if (header.isEmptyHeader()) return false
     if (header.hasNumerousWildcards()) return false
-    if (PsiTreeUtil.findChildrenOfAnyType(header, EditorConfigEnumerationPattern::class.java).any(patternChecker)) return false
+    if (PsiTreeUtil.findChildrenOfAnyType(header, EditorConfigEnumerationPattern::class.java).any({ it.hasRedundancy() })) return false
     return true
   }
 
   final override fun getReference() = EditorConfigHeaderReference(this)
 
-  private companion object {
-    private val patternChecker = EditorConfigPatternEnumerationRedundancyInspection.Companion::containsIssue
-  }
 }
