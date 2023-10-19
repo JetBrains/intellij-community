9acdc82e6a768b872d1994990e925b902ec46bd9
Yann Cébron <yann.cebron@jetbrains.com>
2023-10-19 11:38:12 +0200

    [platform] UnknownSdkInspectionCommandLineConfigurator: cleanup
    GitOrigin-RevId: bd5ce8d71a404aee2e10d9e39f8649b82800ba78

--- platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkInspectionCommandLineConfigurator.kt
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkInspectionCommandLineConfigurator.kt
@@ -9,47 +9,10 @@ import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.withPushPop
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectBundle
-import com.intellij.openapi.util.registry.Registry
 
-class UnknownSdkInspectionCommandLineConfigurator : CommandLineInspectionProjectConfigurator {
-  companion object {
-    private val LOG: Logger = logger<UnknownSdkInspectionCommandLineConfigurator>()
+private val LOG: Logger = logger<UnknownSdkInspectionCommandLineConfigurator>()
 
-    fun configureUnknownSdks(project: Project, indicator: ProgressIndicator) {
-      require(!ApplicationManager.getApplication().isWriteIntentLockAcquired) {
-        "The code below uses the same GUI thread to complete operations. Running from EDT would deadlock"
-      }
-      ApplicationManager.getApplication().assertIsNonDispatchThread()
-
-
-      indicator.withPushPop {
-        indicator.text = ProjectBundle.message("config.unknown.progress.scanning")
-
-        val problems = UnknownSdkTracker
-          .getInstance(project)
-          .collectUnknownSdks(UnknownSdkCollector(project), indicator)
-
-        if (problems.isEmpty()) return@withPushPop
-        indicator.isIndeterminate = false
-
-        for ((i, problem) in problems.withIndex()) {
-          val fix = problem.suggestedFixAction
-
-          if (fix == null) {
-            LOG.warn("Failed to resolve ${problem.sdkTypeAndNameText}: ${problem.notificationText}")
-            continue
-          }
-
-          LOG.info("Resolving ${problem.sdkTypeAndNameText}")
-          indicator.withPushPop {
-            indicator.fraction = i.toDouble() / problems.size
-            indicator.text = ProjectBundle.message("config.unknown.progress.configuring", problem.sdkTypeAndNameText)
-            fix.applySuggestionBlocking(indicator)
-          }
-        }
-      }
-    }
-  }
+internal class UnknownSdkInspectionCommandLineConfigurator : CommandLineInspectionProjectConfigurator {
 
   override fun getName(): String = "sdk"
 
@@ -64,5 +27,40 @@ class UnknownSdkInspectionCommandLineConfigurator : CommandLineInspectionProject
 
   override fun configureProject(project: Project, context: CommandLineInspectionProjectConfigurator.ConfiguratorContext) {
     configureUnknownSdks(project, context.progressIndicator)
+  }
+
+  private fun configureUnknownSdks(project: Project, indicator: ProgressIndicator) {
+    require(!ApplicationManager.getApplication().isWriteIntentLockAcquired) {
+      "The code below uses the same GUI thread to complete operations. Running from EDT would deadlock"
+    }
+    ApplicationManager.getApplication().assertIsNonDispatchThread()
+
+
+    indicator.withPushPop {
+      indicator.text = ProjectBundle.message("config.unknown.progress.scanning")
+
+      val problems = UnknownSdkTracker
+        .getInstance(project)
+        .collectUnknownSdks(UnknownSdkCollector(project), indicator)
+
+      if (problems.isEmpty()) return@withPushPop
+      indicator.isIndeterminate = false
+
+      for ((i, problem) in problems.withIndex()) {
+        val fix = problem.suggestedFixAction
+
+        if (fix == null) {
+          LOG.warn("Failed to resolve ${problem.sdkTypeAndNameText}: ${problem.notificationText}")
+          continue
+        }
+
+        LOG.info("Resolving ${problem.sdkTypeAndNameText}")
+        indicator.withPushPop {
+          indicator.fraction = i.toDouble() / problems.size
+          indicator.text = ProjectBundle.message("config.unknown.progress.configuring", problem.sdkTypeAndNameText)
+          fix.applySuggestionBlocking(indicator)
+        }
+      }
+    }
   }
 }
