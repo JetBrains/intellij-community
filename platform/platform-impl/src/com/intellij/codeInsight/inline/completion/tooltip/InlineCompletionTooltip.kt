7bf13812037234fa0448cdfdccb5e28e7d206deb
Petr Surkov <petr.surkov@jetbrains.com>
2023-10-20 15:22:44 +0200

    [full-line] ML-1593 Implement inline completion tooltip shortcut change button
    GitOrigin-RevId: 2652a6ddef398c359d64ca456e0fa4a4329c741a

--- platform/platform-impl/src/com/intellij/codeInsight/inline/completion/tooltip/InlineCompletionTooltip.kt
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/tooltip/InlineCompletionTooltip.kt
@@ -3,22 +3,32 @@ package com.intellij.codeInsight.inline.completion.tooltip
 
 import com.intellij.codeInsight.hint.HintManager
 import com.intellij.codeInsight.hint.HintManagerImpl
-import com.intellij.codeInsight.inline.completion.InlineCompletion
 import com.intellij.codeInsight.inline.completion.session.InlineCompletionSession
 import com.intellij.codeInsight.lookup.LookupManager
 import com.intellij.ide.IdeBundle
-import com.intellij.openapi.actionSystem.IdeActions
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.command.CommandProcessor
+import com.intellij.ide.lightEdit.LightEditCompatible
+import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.actionSystem.impl.ActionButtonWithText
+import com.intellij.openapi.keymap.KeymapManager
 import com.intellij.openapi.keymap.KeymapUtil
+import com.intellij.openapi.keymap.impl.ui.KeymapPanel
+import com.intellij.openapi.options.ShowSettingsUtil
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.openapi.ui.popup.util.PopupUtil
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.NlsActions
 import com.intellij.ui.LightweightHint
 import com.intellij.ui.dsl.builder.RightGap
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.util.preferredHeight
 import com.intellij.util.concurrency.annotations.RequiresEdt
 import com.intellij.util.ui.JBUI
+import java.awt.Insets
+import java.awt.event.KeyEvent
+
 
 internal object InlineCompletionTooltip {
   private val tooltipKey = Key<Unit>("EDITOR_HAS_INLINE_TOOLTIP")
@@ -34,26 +44,7 @@ internal object InlineCompletionTooltip {
       return
     }
 
-    val insertShortcut = KeymapUtil.getFirstKeyboardShortcutText(IdeActions.ACTION_INSERT_INLINE_COMPLETION)
-    val panel = panel {
-      row {
-        link(insertShortcut) {
-          // TODO refactor: direct action call
-          ApplicationManager.getApplication().invokeLater {
-            ApplicationManager.getApplication().runWriteAction {
-              CommandProcessor.getInstance().executeCommand(editor.project, {
-                InlineCompletion.getHandlerOrNull(editor)?.insert()
-              }, null, null, editor.document)
-            }
-          }
-        }.gap(RightGap.SMALL)
-        @Suppress("DialogTitleCapitalization")
-        text(IdeBundle.message("inline.completion.tooltip.shortcuts.accept.description")).gap(RightGap.SMALL)
-        cell(session.provider.providerPresentation.getTooltip(editor.project))
-      }
-    }.apply {
-      border = JBUI.Borders.empty(2)
-    }
+    val panel = createPanel(session)
 
     val hint = LightweightHint(panel).apply {
       setForceShowAsPopup(true)
@@ -71,7 +62,7 @@ internal object InlineCompletionTooltip {
       hint,
       editor,
       location,
-      HintManager.HIDE_BY_ANY_KEY or HintManager.HIDE_BY_TEXT_CHANGE or HintManager.HIDE_BY_SCROLLING,
+      HintManager.HIDE_BY_CARET_MOVE or HintManager.HIDE_BY_TEXT_CHANGE or HintManager.HIDE_BY_SCROLLING,
       0,
       false,
       HintManagerImpl.createHintHint(editor, location, hint, HintManager.ABOVE).setContentActive(false)
@@ -83,6 +74,80 @@ internal object InlineCompletionTooltip {
     }
     Disposer.register(session) {
       hint.hide()
+    }
+  }
+
+  private class InplaceChangeInlineCompletionShortcutAction(@NlsActions.ActionText text: String, private val shortcut: Shortcut) :
+    AnAction(text), DumbAware, LightEditCompatible {
+
+    lateinit var updateAfterActionPerformed: ActionButtonWithText
+    override fun actionPerformed(e: AnActionEvent) {
+      KeymapManager.getInstance().activeKeymap.removeAllActionShortcuts(IdeActions.ACTION_INSERT_INLINE_COMPLETION)
+      KeymapManager.getInstance().activeKeymap.addShortcut(
+        IdeActions.ACTION_INSERT_INLINE_COMPLETION,
+        shortcut
+      )
+      updateAfterActionPerformed.update()
+    }
+
+  }
+
+  private class ChangeToCustomInlineCompletionAction : AnAction(
+    IdeBundle.message("inline.completion.tooltip.shortcuts.accept.select.custom"),
+  ), DumbAware, LightEditCompatible {
+    override fun actionPerformed(e: AnActionEvent) {
+      ShowSettingsUtil.getInstance().showSettingsDialog(e.project, KeymapPanel::class.java)
+    }
+  }
+
+  private fun createPanel(session: InlineCompletionSession): DialogPanel {
+    val panel = panel {
+      row {
+        cell(createSelectInsertShortcutAction()).gap(RightGap.SMALL)
+        @Suppress("DialogTitleCapitalization")
+        text(IdeBundle.message("inline.completion.tooltip.shortcuts.accept.description")).gap(RightGap.SMALL)
+        cell(session.provider.providerPresentation.getTooltip(session.context.editor.project))
+      }
+    }.apply {
+      border = JBUI.Borders.empty(2)
+    }
+    return panel
+  }
+
+  private class SelectShortcutActionGroup(private val actions: Array<AnAction>) : ActionGroup(), DumbAware {
+    init {
+      isPopup = true
+      templatePresentation.isPerformGroup = true
+    }
+
+    override fun getChildren(e: AnActionEvent?): Array<AnAction> = actions
+
+    override fun actionPerformed(e: AnActionEvent) {
+      val popup = JBPopupFactory.getInstance().createActionGroupPopup(null, this, e.dataContext,
+                                                                      JBPopupFactory.ActionSelectionAid.MNEMONICS, true)
+      PopupUtil.showForActionButtonEvent(popup, e)
+    }
+  }
+
+  private fun createSelectInsertShortcutAction(): ActionButtonWithText {
+    val actions = arrayOf<AnAction>(
+      @Suppress("HardCodedStringLiteral")
+      InplaceChangeInlineCompletionShortcutAction(KeymapUtil.getKeyText(KeyEvent.VK_TAB), KeyboardShortcut.fromString("TAB")),
+      @Suppress("HardCodedStringLiteral")
+      InplaceChangeInlineCompletionShortcutAction(KeymapUtil.getKeyText(KeyEvent.VK_ENTER), KeyboardShortcut.fromString("ENTER")),
+      ChangeToCustomInlineCompletionAction(),
+    )
+    val group = SelectShortcutActionGroup(actions)
+    return object : ActionButtonWithText(group, group.templatePresentation.clone(), ActionPlaces.UNKNOWN, JBUI.emptySize()) {
+      override fun getMargins(): Insets = JBUI.emptyInsets()
+      override fun shallPaintDownArrow() = true
+      override fun update() {
+        presentation.text = KeymapUtil.getFirstKeyboardShortcutText(IdeActions.ACTION_INSERT_INLINE_COMPLETION)
+      }
+    }.also {
+      it.isFocusable = false
+      actions.filterIsInstance<InplaceChangeInlineCompletionShortcutAction>()
+        .forEach { action -> action.updateAfterActionPerformed = it }
     }
   }
 }
