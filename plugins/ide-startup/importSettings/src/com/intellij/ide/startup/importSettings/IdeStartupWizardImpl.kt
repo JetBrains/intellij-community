979abaddd16a3d2c6233d6d0b77c77bf9e938235
Ivan Migalev <ivan.migalev@jetbrains.com>
2023-10-20 18:55:16 +0200

    (IJPL-186) New Startup Experience: add dialog title
    GitOrigin-RevId: a44497ed342c26043a433fee84b39c9ec92496e1

--- plugins/ide-startup/importSettings/src/com/intellij/ide/startup/importSettings/IdeStartupWizardImpl.kt
+++ plugins/ide-startup/importSettings/src/com/intellij/ide/startup/importSettings/IdeStartupWizardImpl.kt
@@ -4,8 +4,8 @@ package com.intellij.ide.startup.importSettings
 import com.intellij.ide.startup.importSettings.chooser.productChooser.ProductChooserDialog
 import com.intellij.ide.startup.importSettings.chooser.ui.MultiplePageDialog
 import com.intellij.ide.startup.importSettings.data.SettingsService
+import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.openapi.application.EDT
-import com.intellij.openapi.components.service
 import com.intellij.openapi.extensions.ExtensionNotApplicableException
 import com.intellij.platform.ide.bootstrap.IdeStartupWizard
 import kotlinx.coroutines.Dispatchers
@@ -27,7 +27,11 @@ internal class IdeStartupWizardImpl : IdeStartupWizard {
       async { settingsService.getExternalService().warmUp() }
 
       withContext(Dispatchers.EDT) {
-        MultiplePageDialog.show(ProductChooserDialog(), { settingsService.cancelImport() })
+        MultiplePageDialog.show(
+          ProductChooserDialog(),
+          { settingsService.cancelImport() },
+          title = ApplicationNamesInfo.getInstance().fullProductName
+        )
       }
     }
   }
