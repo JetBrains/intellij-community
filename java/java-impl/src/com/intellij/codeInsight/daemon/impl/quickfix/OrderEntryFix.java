db91e334f8acf226944879319cfb8d527ade7bff
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2023-10-20 12:41:52 +0200

    [java] Move `ImportActionInfo` logic into `AddImportAction`
    GitOrigin-RevId: dfae872c7fb735c54d4c9acd668191c8d46d0791

--- java/java-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/OrderEntryFix.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/OrderEntryFix.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.daemon.impl.quickfix;
 
-import com.intellij.codeInsight.daemon.impl.actions.AddImportAction;
 import com.intellij.codeInsight.daemon.quickFix.ExternalLibraryResolver;
 import com.intellij.codeInsight.daemon.quickFix.ExternalLibraryResolver.ExternalClassResolveResult;
 import com.intellij.codeInsight.intention.IntentionAction;
@@ -10,9 +9,7 @@ import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.MoveToTestRootFix;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.lang.java.JavaLanguage;
-import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.*;
 import com.intellij.openapi.roots.libraries.Library;
@@ -31,8 +28,6 @@ import com.intellij.psi.util.PsiUtil;
 import com.intellij.util.Function;
 import com.intellij.util.SmartList;
 import com.intellij.util.ThreeState;
-import com.intellij.util.concurrency.annotations.RequiresEdt;
-import com.intellij.util.concurrency.annotations.RequiresReadLock;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -345,29 +340,6 @@ public abstract class OrderEntryFix implements IntentionAction, LocalQuickFix {
       uElement = uElement.getUastParent();
     }
     return ThreeState.NO;
-  }
-
-  protected record ImportActionInfo(@NotNull PsiClass aClass, @NotNull PsiReference reference) {
-  }
-
-  @RequiresReadLock
-  protected static @Nullable ImportActionInfo getImportActionInfo(
-    @NotNull Module currentModule,
-    @NotNull PsiReference reference,
-    @NotNull String className
-  ) {
-    Project project = currentModule.getProject();
-    return DumbService.getInstance(project).computeWithAlternativeResolveEnabled(() -> {
-      GlobalSearchScope scope = GlobalSearchScope.moduleWithLibrariesScope(currentModule);
-      PsiClass aClass = JavaPsiFacade.getInstance(project).findClass(className, scope);
-      if (aClass == null) return null;
-      return new ImportActionInfo(aClass, reference);
-    });
-  }
-
-  @RequiresEdt
-  protected static void importReference(@NotNull Project project, @NotNull Editor editor, @NotNull ImportActionInfo info) {
-    new AddImportAction(project, info.reference, editor, info.aClass).execute();
   }
 
   public static void addJarToRoots(@NotNull String jarPath, final @NotNull Module module, @Nullable PsiElement location) {
