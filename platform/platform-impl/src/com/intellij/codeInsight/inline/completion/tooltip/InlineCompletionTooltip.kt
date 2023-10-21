dfcdcf300bf4a67642e3ad4c38af85c61c89c6d3
Petr Surkov <petr.surkov@jetbrains.com>
2023-10-21 10:28:10 +0200

    [inline-completion] Properly modify keymap
    GitOrigin-RevId: a39583d4bfe5f1f4d2f2b8ee9ee5b72a5750db37

--- platform/platform-impl/src/com/intellij/codeInsight/inline/completion/tooltip/InlineCompletionTooltip.kt
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/tooltip/InlineCompletionTooltip.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.inline.completion.tooltip
 
+import com.intellij.application.options.schemes.SchemeNameGenerator
 import com.intellij.codeInsight.hint.HintManager
 import com.intellij.codeInsight.hint.HintManagerImpl
 import com.intellij.codeInsight.inline.completion.session.InlineCompletionSession
@@ -11,10 +12,10 @@ import com.intellij.ide.lightEdit.LightEditCompatible
 import com.intellij.idea.ActionsBundle
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.impl.ActionButtonWithText
+import com.intellij.openapi.keymap.KeyMapBundle
 import com.intellij.openapi.keymap.KeymapManager
 import com.intellij.openapi.keymap.KeymapUtil
-import com.intellij.openapi.keymap.impl.ui.KeymapPanel
-import com.intellij.openapi.options.ShowSettingsUtil
+import com.intellij.openapi.keymap.ex.KeymapManagerEx
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.popup.JBPopupFactory
@@ -85,8 +86,24 @@ internal object InlineCompletionTooltip {
 
     lateinit var updateAfterActionPerformed: ActionButtonWithText
     override fun actionPerformed(e: AnActionEvent) {
-      KeymapManager.getInstance().activeKeymap.removeAllActionShortcuts(IdeActions.ACTION_INSERT_INLINE_COMPLETION)
-      KeymapManager.getInstance().activeKeymap.addShortcut(
+      if (!KeymapManager.getInstance().activeKeymap.canModify()) {
+        val managerEx = KeymapManager.getInstance() as KeymapManagerEx
+        val currentKeymap = managerEx.activeKeymap
+        val allKeymaps = managerEx.allKeymaps
+        val name = SchemeNameGenerator.getUniqueName(
+          KeyMapBundle.message("keymap.with.patched.inline.insert.proposal.name", currentKeymap.getPresentableName())) { name: String ->
+          allKeymaps.any { name == it.name || name == it.presentableName }
+        }
+        val newKeymap = currentKeymap.deriveKeymap(name)
+        managerEx.schemeManager.addScheme(newKeymap)
+        managerEx.activeKeymap = newKeymap
+      }
+      val currentKeymap = KeymapManager.getInstance().activeKeymap
+      check(currentKeymap.canModify()) {
+        "Cannot modify ${currentKeymap.presentableName} keymap"
+      }
+      currentKeymap.removeAllActionShortcuts(IdeActions.ACTION_INSERT_INLINE_COMPLETION)
+      currentKeymap.addShortcut(
         IdeActions.ACTION_INSERT_INLINE_COMPLETION,
         shortcut
       )
@@ -149,7 +166,7 @@ internal object InlineCompletionTooltip {
       override fun getMargins(): Insets = JBUI.emptyInsets()
       override fun shallPaintDownArrow() = true
       override fun update() {
-        presentation.text = KeymapUtil.getFirstKeyboardShortcutText(IdeActions.ACTION_INSERT_INLINE_COMPLETION)
+        presentation.text = KeymapUtil.getFirstKeyboardShortcutText(IdeActions.ACTION_INSERT_INLINE_COMPLETION).ifEmpty { " " }
       }
     }.also {
       it.isFocusable = false
