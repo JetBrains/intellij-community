e1cbde061db1c56de83a1d4dae1d136d2dad49f6
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2023-10-23 17:56:21 +0300

    IDEA-323860 Work around for ActionMenu.fillMenu cancellation
    The fillMenu() call in ActionMenu.setPopupMenuVisible() can produce
    a cancellation exception (when expanding the action group).
    Throwing exceptions during Swing event processing is likely
    a bad idea in itself, but until we fix it, this simple workaround
    will have to do. Remember the last passed boolean value
    and check it when checking whether the currently selected menu
    item is expanded in IdeJMenuBar.isActivated(), which is
    used to determine whether the main menu should collapse
    when hovering over the expanded items in full screen.
    
    GitOrigin-RevId: ec633157ca9631885e25e2964cb8568bde573e9c

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionMenu.kt
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionMenu.kt
@@ -67,6 +67,9 @@ class ActionMenu constructor(private val context: DataContext?,
 
   private var specialMenu: JPopupMenu? = null
 
+  internal var isTryingToShowPopupMenu = false
+    private set
+
   override fun removeNotify() {
     super.removeNotify()
     if (disposable != null) {
@@ -346,6 +349,7 @@ class ActionMenu constructor(private val context: DataContext?,
   }
 
   override fun setPopupMenuVisible(value: Boolean) {
+    isTryingToShowPopupMenu = value
     if (value && !(SystemInfo.isMacSystemMenu && ActionPlaces.MAIN_MENU == place)) {
       fillMenu()
       if (!isSelected) {
