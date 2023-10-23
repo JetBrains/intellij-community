52ee233333fb1f2b581981988bfc6ea540319e09
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-10-23 19:51:19 +0200

    IJPL-274 remove `ProgressReporter` and `RawProgressReporter` from `com.intellij.openapi.progress` package
    GitOrigin-RevId: a482e7a33764ced9c5ebf1afec49db3736fce956

--- platform/core-api/src/com/intellij/openapi/progress/RawProgressReporter.kt
+++
@@ -1,9 +0,0 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.progress
-
-import com.intellij.platform.util.progress.RawProgressReporter
-import org.jetbrains.annotations.ApiStatus.NonExtendable
-
-@NonExtendable
-@Deprecated("Moved to com.intellij.platform.util.progress", level = DeprecationLevel.ERROR)
-interface RawProgressReporter : RawProgressReporter
