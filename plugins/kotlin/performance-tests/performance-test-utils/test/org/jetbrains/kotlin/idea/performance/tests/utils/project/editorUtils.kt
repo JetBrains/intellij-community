a66d2f699110e4defa5e62b91c8539f99ab89b41
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2023-10-23 18:55:38 +0200

    [kotlin] Added replica project for the stress perf test
    #KTIJ-26506
    
    GitOrigin-RevId: c828f04bfc9f48216bb05a4ff318a75211748d99

--- plugins/kotlin/performance-tests/performance-test-utils/test/org/jetbrains/kotlin/idea/performance/tests/utils/project/editorUtils.kt
+++ plugins/kotlin/performance-tests/performance-test-utils/test/org/jetbrains/kotlin/idea/performance/tests/utils/project/editorUtils.kt
@@ -27,9 +27,7 @@ fun openInEditor(project: Project, vFile: VirtualFile): EditorFile {
 
     TestCase.assertTrue("file $vFile is not indexed yet", FileIndexFacade.getInstance(project).isInContent(vFile))
 
-    runInEdtAndWait {
-        fileEditorManager.openFile(vFile, true)
-    }
+    fileEditorManager.openFile(vFile, true)
     val document = fileDocumentManager.getDocument(vFile) ?: error("no document for $vFile found")
 
     TestCase.assertNotNull("doc not found for $vFile", EditorFactory.getInstance().getEditors(document))
