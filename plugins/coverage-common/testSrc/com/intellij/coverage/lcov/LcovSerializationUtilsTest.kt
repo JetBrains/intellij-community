0b51c1ded83d783a1ca5a2c2171dddd60f52bbf1
Dima Golovinov <dmitry.golovinov@jetbrains.com>
2023-10-23 10:02:48 +0200

    [qodana] Lcov support: assert linedata in projectdata
    (cherry picked from commit 9ba08d4d1176b3f0d93ab1d5d0a75636e6af10d0)
    
    IJ-MR-117699
    
    GitOrigin-RevId: 3891124a0c91c8b3623dfba945f216af32d74549

--- plugins/coverage-common/testSrc/com/intellij/coverage/lcov/LcovSerializationUtilsTest.kt
+++ plugins/coverage-common/testSrc/com/intellij/coverage/lcov/LcovSerializationUtilsTest.kt
@@ -1,6 +1,5 @@
 package com.intellij.coverage.lcov
 
-import com.intellij.rt.coverage.util.CoverageReport
 import com.intellij.testFramework.assertEqualsToFile
 import org.junit.Test
 import java.io.File
@@ -14,9 +13,15 @@ class LcovSerializationUtilsTest {
     val lcovLines = lcov.info.values.joinToString("\n")
     assertEqualsToFile("LCOV report read incorrectly", File("testData/lcov/line-hints.expected"), lcovLines)
     val projectData = LcovSerializationUtils.convertToProjectData(lcov, Function.identity())
-    val dataFile = File.createTempFile("dataFile", "").also { it.deleteOnExit() }
-    val sourceMapFile = File.createTempFile("sourceMap", "").also { it.deleteOnExit() }
-    CoverageReport.save(projectData, dataFile, sourceMapFile)
-    assertEqualsToFile("Project data saved incorrectly", File("testData/lcov/project.data"), dataFile.readText())
+    for ((file, hints) in lcov.info) {
+      val data = projectData.getClassData(file)
+      assert(data != null)
+      for (hint in hints) {
+        val lineData = data.getLineData(hint.lineNumber)
+        assert(lineData != null)
+        assert(lineData!!.hits == hint.hits)
+        assert(lineData.methodSignature == hint.functionName)
+      }
+    }
   }
 }
