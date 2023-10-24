ef1bf9f6fea3353be0fbde050217cbbb8693e9a7
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-10-24 22:56:51 +0200

    [ultimate] IDEA-321299 Java Profiler configurable
    GitOrigin-RevId: d09ca1846d3ac8aa4874f19b3cb6e2657e537acf

--- java/idea-ui/src/com/intellij/ide/ultimatepromo/configurables.kt
+++ java/idea-ui/src/com/intellij/ide/ultimatepromo/configurables.kt
@@ -2,11 +2,13 @@
 package com.intellij.ide.ultimatepromo
 
 import com.intellij.icons.AllIcons
+import com.intellij.ide.JavaUiBundle
 import com.intellij.java.ui.icons.JavaUIIcons
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.options.ConfigurableProvider
 import com.intellij.openapi.options.ConfigurableWithId
 import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.*
+import com.intellij.openapi.util.NlsContexts
 import com.intellij.openapi.util.registry.Registry
 import javax.swing.Icon
 import javax.swing.JComponent
@@ -26,8 +28,20 @@ internal abstract class UltimateConfigurableProvider(private val clazz: KClass<o
   }
 }
 
+private fun featurePage(@NlsContexts.Label title: String, items: List<PromoFeatureListItem>, pluginId: String): JComponent {
+  return PromoPages.build(PromoFeaturePage(
+    JavaUIIcons.IdeaUltimatePromo,
+    PluginAdvertiserService.ideaUltimate,
+    title,
+    items,
+    FeaturePromoBundle.message("free.trial.hint"),
+    pluginId
+  ))
+}
+
 internal class PromoDatabaseConfigurableProvider : UltimateConfigurableProvider(PromoDatabaseConfigurable::class)
 internal class PromoKubernetesConfigurableProvider : UltimateConfigurableProvider(PromoKubernetesConfigurable::class)
+internal class PromoProfilerConfigurableProvider : UltimateConfigurableProvider(PromoProfilerConfigurable::class)
 
 internal class PromoDatabaseConfigurable : UltimatePromoConfigurable() {
   override fun getId(): String = "promo.database"
@@ -37,19 +51,14 @@ internal class PromoDatabaseConfigurable : UltimatePromoConfigurable() {
   }
 
   override fun createComponent(): JComponent {
-    return PromoPages.build(
-      PromoFeaturePage(
-        JavaUIIcons.IdeaUltimatePromo,
-        PluginAdvertiserService.ideaUltimate,
-        FeaturePromoBundle.message("feature.database.description.html"),
-        listOf(
-          PromoFeatureListItem(AllIcons.Nodes.DataTables, FeaturePromoBundle.message("feature.database.create.and.manage")),
-          PromoFeatureListItem(AllIcons.Actions.Run_anything, FeaturePromoBundle.message("feature.database.run")),
-          PromoFeatureListItem(AllIcons.ToolbarDecorator.Import, FeaturePromoBundle.message("feature.database.export"))
-        ),
-        FeaturePromoBundle.message("free.trial.hint"),
-        "com.intellij.database"
-      )
+    return featurePage(
+      FeaturePromoBundle.message("feature.database.description.html"),
+      listOf(
+        PromoFeatureListItem(AllIcons.Nodes.DataTables, FeaturePromoBundle.message("feature.database.create.and.manage")),
+        PromoFeatureListItem(AllIcons.Actions.Run_anything, FeaturePromoBundle.message("feature.database.run")),
+        PromoFeatureListItem(AllIcons.ToolbarDecorator.Import, FeaturePromoBundle.message("feature.database.export"))
+      ),
+      "com.intellij.database"
     )
   }
 }
@@ -62,19 +71,34 @@ internal class PromoKubernetesConfigurable : UltimatePromoConfigurable() {
   }
 
   override fun createComponent(): JComponent {
-    return PromoPages.build(
-      PromoFeaturePage(
-        JavaUIIcons.IdeaUltimatePromo,
-        PluginAdvertiserService.ideaUltimate,
-        FeaturePromoBundle.message("feature.kubernetes.description.html"),
-        listOf(
-          PromoFeatureListItem(AllIcons.Nodes.Deploy, FeaturePromoBundle.message("feature.kubernetes.deploy")),
-          PromoFeatureListItem(AllIcons.Nodes.Console, FeaturePromoBundle.message("feature.kubernetes.logs")),
-          PromoFeatureListItem(AllIcons.FileTypes.Properties, FeaturePromoBundle.message("feature.kubernetes.editor"))
-        ),
-        FeaturePromoBundle.message("free.trial.hint"),
-        "com.intellij.kubernetes"
-      )
+    return featurePage(
+      FeaturePromoBundle.message("feature.kubernetes.description.html"),
+      listOf(
+        PromoFeatureListItem(AllIcons.Nodes.Deploy, FeaturePromoBundle.message("feature.kubernetes.deploy")),
+        PromoFeatureListItem(AllIcons.Nodes.Console, FeaturePromoBundle.message("feature.kubernetes.logs")),
+        PromoFeatureListItem(AllIcons.FileTypes.Properties, FeaturePromoBundle.message("feature.kubernetes.editor"))
+      ),
+      "com.intellij.kubernetes"
+    )
+  }
+}
+
+internal class PromoProfilerConfigurable : UltimatePromoConfigurable() {
+  override fun getId(): String = "promo.profiler"
+
+  override fun getDisplayName(): String {
+    return JavaUiBundle.message("promo.configurable.profiler")
+  }
+
+  override fun createComponent(): JComponent {
+    return featurePage(
+      JavaUiBundle.message("feature.profiler.description.html"),
+      listOf(
+        PromoFeatureListItem(AllIcons.Actions.ProfileCPU, JavaUiBundle.message("feature.profiler.cpu")),
+        PromoFeatureListItem(AllIcons.Actions.ProfileMemory, JavaUiBundle.message("feature.profiler.memory")),
+        PromoFeatureListItem(AllIcons.Actions.ProfileRed, JavaUiBundle.message("feature.profiler.hints"))
+      ),
+      "com.intellij.LineProfiler"
     )
   }
 }
