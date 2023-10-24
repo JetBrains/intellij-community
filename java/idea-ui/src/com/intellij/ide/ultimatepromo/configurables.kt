30f5afeaa8d1d560a33b0d2e17e6b7f65ccd79dd
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-10-24 22:37:30 +0200

    [ultimate] IDEA-321299 Kubernetes configurable
    GitOrigin-RevId: 8c251e9a573ad7f6fa4e34899e79acd944755b35

--- java/idea-ui/src/com/intellij/ide/ultimatepromo/configurables.kt
+++ java/idea-ui/src/com/intellij/ide/ultimatepromo/configurables.kt
@@ -10,16 +10,26 @@ import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.*
 import com.intellij.openapi.util.registry.Registry
 import javax.swing.Icon
 import javax.swing.JComponent
+import kotlin.reflect.KClass
 
-internal class PromoDatabaseConfigurableProvider : ConfigurableProvider() {
-  override fun createConfigurable(): Configurable? {
+internal abstract class UltimatePromoConfigurable : ConfigurableWithId, Configurable.Promo {
+  override fun isModified(): Boolean = false
+  override fun apply() = Unit
+  override fun getPromoIcon(): Icon = AllIcons.Ultimate.Lock
+}
+
+internal abstract class UltimateConfigurableProvider(private val clazz: KClass<out Configurable>) : ConfigurableProvider() {
+  final override fun createConfigurable(): Configurable? {
     if (!Registry.`is`("idea.ultimate.features.hints.enabled")) return null
 
-    return PromoDatabaseConfigurable()
+    return clazz.java.getConstructor().newInstance()
   }
 }
 
-internal class PromoDatabaseConfigurable : ConfigurableWithId, Configurable.Promo {
+internal class PromoDatabaseConfigurableProvider : UltimateConfigurableProvider(PromoDatabaseConfigurable::class)
+internal class PromoKubernetesConfigurableProvider : UltimateConfigurableProvider(PromoKubernetesConfigurable::class)
+
+internal class PromoDatabaseConfigurable : UltimatePromoConfigurable() {
   override fun getId(): String = "promo.database"
 
   override fun getDisplayName(): String {
@@ -33,29 +43,38 @@ internal class PromoDatabaseConfigurable : ConfigurableWithId, Configurable.Prom
         PluginAdvertiserService.ideaUltimate,
         FeaturePromoBundle.message("feature.database.description.html"),
         listOf(
-          PromoFeatureListItem(
-            AllIcons.Nodes.DataTables,
-            FeaturePromoBundle.message("feature.database.create.and.manage")
-          ),
-          PromoFeatureListItem(
-            AllIcons.Actions.Run_anything,
-            FeaturePromoBundle.message("feature.database.run")
-          ),
-          PromoFeatureListItem(
-            AllIcons.ToolbarDecorator.Import,
-            FeaturePromoBundle.message("feature.database.export")
-          )
+          PromoFeatureListItem(AllIcons.Nodes.DataTables, FeaturePromoBundle.message("feature.database.create.and.manage")),
+          PromoFeatureListItem(AllIcons.Actions.Run_anything, FeaturePromoBundle.message("feature.database.run")),
+          PromoFeatureListItem(AllIcons.ToolbarDecorator.Import, FeaturePromoBundle.message("feature.database.export"))
         ),
         FeaturePromoBundle.message("free.trial.hint"),
         "com.intellij.database"
       )
     )
   }
+}
 
-  override fun isModified(): Boolean = false
+internal class PromoKubernetesConfigurable : UltimatePromoConfigurable() {
+  override fun getId(): String = "promo.kubernetes"
 
-  override fun apply() {
+  override fun getDisplayName(): String {
+    return FeaturePromoBundle.message("promo.configurable.kubernetes")
   }
 
-  override fun getPromoIcon(): Icon = AllIcons.Ultimate.Lock
+  override fun createComponent(): JComponent {
+    return PromoPages.build(
+      PromoFeaturePage(
+        JavaUIIcons.IdeaUltimatePromo,
+        PluginAdvertiserService.ideaUltimate,
+        FeaturePromoBundle.message("feature.kubernetes.description.html"),
+        listOf(
+          PromoFeatureListItem(AllIcons.Nodes.Deploy, FeaturePromoBundle.message("feature.kubernetes.deploy")),
+          PromoFeatureListItem(AllIcons.Nodes.Console, FeaturePromoBundle.message("feature.kubernetes.logs")),
+          PromoFeatureListItem(AllIcons.FileTypes.Properties, FeaturePromoBundle.message("feature.kubernetes.editor"))
+        ),
+        FeaturePromoBundle.message("free.trial.hint"),
+        "com.intellij.kubernetes"
+      )
+    )
+  }
 }
