b5ec48e6508487b64a886dd995294ddd5d418ecd
Georgii.Zorabov <georgii.zorabov@jetbrains.com>
2023-10-24 21:33:00 +0000

    DS-5284-fixes
    fixes for visualisations in table header. Percentage threshold is moved from 70 to 60, size of visualisations is decreased, font of percentage values is changed, colours are properly named, so everything works good when theme is switched and describe command processing is refactored  to correctly avoid possible warnings in its text for pandas and polars, also, added bars to histograms with height 0 to get rid of empty space.
    
    
    Merge-request: IJ-MR-117579
    Merged-by: Georgii Zorabov <georgii.zorabov@jetbrains.com>
    
    GitOrigin-RevId: cf9e75fa51d827266c5b63116769bc662cc0d26b

--- python/helpers/pydev/_pydevd_bundle/pydevd_tables.py
+++ python/helpers/pydev/_pydevd_bundle/pydevd_tables.py
@@ -49,6 +49,7 @@ def exec_table_command(init_command, command_type, start_index, end_index, f_glo
         res.append(table_provider.get_value_counts(table))
         res.append(NEXT_VALUE_SEPARATOR)
         res.append(table_provider.get_value_occurrences_count(table))
+        res.append(NEXT_VALUE_SEPARATOR)
 
 
     elif command_type == TableCommandType.SLICE:
