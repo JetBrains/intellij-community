af156480fe665b8146549ab9013c43352c058aa5
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-10-24 13:45:41 +0200

    [KTIJ-27485] Drop multiplatform tests for 'enableKgpDependencyResolution=false'
    GitOrigin-RevId: a81d7dd854afaebb13d70d100a7276faabe81ea0

--- plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/oldImport/tier1/androidAppConsumesKmmLibBinary/kmmLib/src/commonMain/kotlin/test/kmmLib_commonMain_api.kt
+++
@@ -1,8 +0,0 @@
-package test
-
-expect class <!LINE_MARKER("descr='Has actuals in [project.kmmLib.iosMain, project.kmmLib.main] modules'; targets=[(text=project.kmmLib.iosMain); (text=project.kmmLib.main)]")!>CommonMainExpect<!> {
-    fun <!LINE_MARKER("descr='Has actuals in [project.kmmLib.iosMain, project.kmmLib.main] modules'; targets=[(text=project.kmmLib.iosMain); (text=project.kmmLib.main)]")!>commonApi<!>()
-}
-
-fun produceCommonMainExpect(): CommonMainExpect = null!!
-fun consumeCommonMainExpect(e: CommonMainExpect) { }
--- plugins/kotlin/idea/tests/testData/gradle/multiplatform/core/oldImport/tier1/kotlinJvmConsumesKmmLibSource/kmmLib/src/commonMain/kotlin/test/kmmLib_commonMain_api.kt
+++
@@ -1,8 +0,0 @@
-package test
-
-expect class <!LINE_MARKER("descr='Has actuals in [project.kmmLib.jvmMain, project.kmmLib.iosMain] modules'; targets=[(text=project.kmmLib.jvmMain); (text=project.kmmLib.iosMain)]")!>CommonMainExpect<!> {
-    fun <!LINE_MARKER("descr='Has actuals in [project.kmmLib.jvmMain, project.kmmLib.iosMain] modules'; targets=[(text=project.kmmLib.jvmMain); (text=project.kmmLib.iosMain)]")!>commonApi<!>()
-}
-
-fun produceCommonMainExpect(): CommonMainExpect = null!!
-fun consumeCommonMainExpect(e: CommonMainExpect) { }
