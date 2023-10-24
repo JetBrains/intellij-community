2901cbc01b6c10d1d0ad3bd8843a3821f27a453f
vladimir.petrenko <vladimir.petrenko@jetbrains.com>
2023-10-24 17:20:51 +0700

    RDCT-555 Sync language level to the client: review fixes (IJ-MR-116786)
    - more info log.error
    - handle more possible exceptions
    - in *.xml-s: use more modern "content" approach to include xml from another module. Add `package` attribute because it is necessary in case of including via "content"
    - use "suspend-friendly" read action invocator
    - little concurrency problem fix
    - remove `ModalityUiUtil.invokeLaterIfNeeded` because its initial purpose to ensure to work with protocol on EDT is not actual in this version of code
    
    (cherry picked from commit 69a4c27ffdbffd7c5e6b63d3e87fedb3dcb4bdb4)
    
    IJ-MR-118792
    
    GitOrigin-RevId: cacbc03456d05ade08eb28cfd65ef023a40c76c9

--- java/java-impl/src/com/intellij/openapi/roots/impl/JavaLangLevelVirtualFileExtrasProvider.kt
+++ java/java-impl/src/com/intellij/openapi/roots/impl/JavaLangLevelVirtualFileExtrasProvider.kt
@@ -1,21 +1,18 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.roots.impl
 
-import com.intellij.openapi.application.ModalityState
-import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.LanguageLevelProjectExtension
 import com.intellij.openapi.roots.NullableLanguageLevelHolder
-import com.intellij.openapi.util.ThrowableComputable
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileExtrasProvider
 import com.intellij.pom.java.LanguageLevel
 import com.intellij.psi.PsiJavaFile
 import com.intellij.psi.PsiManager
-import com.intellij.util.ModalityUiUtil
 import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
@@ -51,12 +48,11 @@ class JavaLangLevelVirtualFileProviderProjectService(val project: Project) {
     }
 
     return callbackFlow {
-      // FIXME [V.Petrenko] fix "Method 'compute' annotated with @RequiresBlockingContext. It is not designed to be called in suspend functions "
-      val fileJavaLevel = ReadAction.compute(ThrowableComputable {
+      val fileJavaLevel = readAction {
         val psiFile = PsiManager.getInstance(project).findFile(virtualFile)
-        if (psiFile !is PsiJavaFile) return@ThrowableComputable null
+        if (psiFile !is PsiJavaFile) return@readAction null
         psiFile.languageLevel
-      })
+      }
 
       if (fileJavaLevel != null) {
         val projectJavaLevel = LanguageLevelProjectExtension.getInstance(project).languageLevel
@@ -65,23 +61,29 @@ class JavaLangLevelVirtualFileProviderProjectService(val project: Project) {
         this.trySend(encoded)
       }
 
-      syncedVirtualFiles2sendChannels[virtualFile] = this
+      synchronized(syncedVirtualFiles2sendChannels) {
+        syncedVirtualFiles2sendChannels[virtualFile] = this
+      }
 
-      awaitClose { syncedVirtualFiles2sendChannels.remove(virtualFile) }
+      awaitClose {
+        synchronized(syncedVirtualFiles2sendChannels) {
+          syncedVirtualFiles2sendChannels.remove(virtualFile)
+        }
+      }
     }
   }
 
   private fun startListenToFileJavaLevelChange() {
     project.messageBus.connect().subscribe(VirtualFileJavaLevelChangeListener.TOPIC, object : VirtualFileJavaLevelChangeListener {
       override fun levelChanged(virtualFile: VirtualFile, newLevel: LanguageLevel?) {
-        ModalityUiUtil.invokeLaterIfNeeded(ModalityState.defaultModalityState(), Runnable {
-          val channel = syncedVirtualFiles2sendChannels[virtualFile] ?: return@Runnable
-          //LOG.trace { "VirtualFileJavaLevelChangeListener.levelChanged called for synced file: file=${virtualFile.name} newLevel=$newLevel" }
-          val projectJavaLevel = LanguageLevelProjectExtension.getInstance(project).languageLevel
-          val langLevelToSend = if (newLevel == projectJavaLevel) null else newLevel
-          val encoded = NullableLanguageLevelHolder(langLevelToSend)
-          channel.trySend(encoded)
-        })
+        val channel = synchronized(syncedVirtualFiles2sendChannels) {
+          syncedVirtualFiles2sendChannels[virtualFile]
+        } ?: return
+        //LOG.trace { "VirtualFileJavaLevelChangeListener.levelChanged called for synced file: file=${virtualFile.name} newLevel=$newLevel" }
+        val projectJavaLevel = LanguageLevelProjectExtension.getInstance(project).languageLevel
+        val langLevelToSend = if (newLevel == projectJavaLevel) null else newLevel
+        val encoded = NullableLanguageLevelHolder(langLevelToSend)
+        channel.trySend(encoded)
       }
     })
   }
