4eafa46845120a007eedcae230707f90de439284
Nikita.Lyubimov <nikita.lyubimov@jetbrains.com>
2023-10-25 14:47:06 +0200

    [RIDER] Add @Internal annotation to MarkupGraveSuppressor
    A @Internal annotation has been added to the MarkupGraveSuppressor class. This is indicative of its internal use, restricting its access and visibility to other parts of the code that are not intended to use it.
    
    (cherry picked from commit 12168dcbd735b75e86a89db72fe96e002ea09857)
    
    IJ-CR-117615
    
    GitOrigin-RevId: e72be96944dbfa5a23dbdb02ff8f8056f59809d5

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/MarkupGraveSuppressor.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/MarkupGraveSuppressor.kt
@@ -2,8 +2,10 @@ package com.intellij.codeInsight.daemon
 
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.vfs.VirtualFile
+import org.jetbrains.annotations.ApiStatus.Internal
 
 
+@Internal
 open class MarkupGraveSuppressor {
   open fun shouldSuppress(file: VirtualFile, document: Document): Boolean = false
 }
