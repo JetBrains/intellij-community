e342f747e2636499be74e35bcffd7475c8bcf371
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2023-10-25 12:48:37 +0200

    [platform] IJPL-170: Squash modules
    GitOrigin-RevId: 589ef6c26e9bba1fcf9c50156981fb85b52ff043

--- platform/backend/observation/src/com/intellij/platform/backend/observation/Observation.kt
+++ platform/backend/observation/src/com/intellij/platform/backend/observation/Observation.kt
@@ -1,8 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.backend.observation.impl
+package com.intellij.platform.backend.observation
 
 import com.intellij.openapi.project.Project
-import com.intellij.platform.backend.observation.ActivityInProgressWitness
 
 object Observation {
 
