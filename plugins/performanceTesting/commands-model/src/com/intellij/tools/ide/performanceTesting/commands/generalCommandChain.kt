48b4564132b9a11c93eccc7291f24426877f6471
Alexey <alexey.kryuchkov@jetbrains.com>
2023-10-25 22:59:35 +0400

    Vcs log tab filtering test + refactoring of naming for git log indexing tests
    GitOrigin-RevId: 2850fabe4ffd010a208d99f57d51b043f05cd6ab

--- plugins/performanceTesting/commands-model/src/com/intellij/tools/ide/performanceTesting/commands/generalCommandChain.kt
+++ plugins/performanceTesting/commands-model/src/com/intellij/tools/ide/performanceTesting/commands/generalCommandChain.kt
@@ -575,6 +575,10 @@ fun <T : CommandChain> T.showFileHistory(): T = apply {
   addCommand("${CMD_PREFIX}showFileHistory")
 }
 
+fun <T : CommandChain> T.filterVcsLogTab(userName: String): T = apply {
+  addCommand("${CMD_PREFIX}filterVcsLogTab $userName")
+}
+
 fun <T : CommandChain> T.chooseCompletionCommand(completionName: String): T = apply {
   addCommand("${CMD_PREFIX}chooseCompletionCommand ${completionName}")
 }
