0c7ef3615516adcbdf84c507668de825510367f7
Lev Serebryakov <lev.serebryakov@jetbrains.com>
2023-10-25 16:53:22 +0200

    Revert "IJPL-53: Extract all threading and lock operations from Application and ApplicationEx to separate interface."
    This reverts commit ba9f971084af4061f01967cedffc025adb91d4c1.
    
    GitOrigin-RevId: 367a4b722436ccf9b991f8dc2aaeb9de9947fe00

--- platform/platform-impl/src/com/intellij/openapi/application/impl/RwLockHolder.kt
+++ platform/platform-impl/src/com/intellij/openapi/application/impl/RwLockHolder.kt
@@ -1,18 +1,17 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.application.impl
 
-import com.intellij.openapi.application.ThreadingSupport
 import com.intellij.openapi.util.ThrowableComputable
 import com.intellij.platform.ide.bootstrap.isImplicitReadOnEDTDisabled
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
-class RwLockHolder(writeThread: Thread) : ThreadingSupport {
+class RwLockHolder(writeThread: Thread) {
   @JvmField
   internal val lock: ReadMostlyRWLock = ReadMostlyRWLock(writeThread)
 
   // @Throws(E::class)
-  override fun <T, E : Throwable?> runWriteIntentReadAction(computation: ThrowableComputable<T, E>): T {
+  fun <T, E : Throwable?> runWriteIntentReadAction(computation: ThrowableComputable<T, E>): T {
     val writeIntentLock = acquireWriteIntentLock(computation.javaClass.getName())
     try {
       return computation.compute()
@@ -24,7 +23,7 @@ class RwLockHolder(writeThread: Thread) : ThreadingSupport {
     }
   }
 
-  override fun acquireWriteIntentLock(ignored: String?): Boolean {
+  fun acquireWriteIntentLock(ignored: String?): Boolean {
     if (lock.isWriteThread && (lock.isWriteIntentLocked || lock.isWriteAcquired)) {
       return false
     }
@@ -32,15 +31,15 @@ class RwLockHolder(writeThread: Thread) : ThreadingSupport {
     return true
   }
 
-  override fun releaseWriteIntentLock() {
+  fun releaseWriteIntentLock() {
     lock.writeIntentUnlock()
   }
 
-  override fun isWriteIntentLocked(): Boolean {
+  fun isWriteIntentLocked(): Boolean {
     return lock.isWriteThread && (lock.isWriteIntentLocked || lock.isWriteAcquired)
   }
 
-  override fun runWithoutImplicitRead(runnable: Runnable) {
+  fun runWithoutImplicitRead(runnable: Runnable) {
     if (isImplicitReadOnEDTDisabled) {
       runnable.run()
       return
@@ -60,7 +59,7 @@ class RwLockHolder(writeThread: Thread) : ThreadingSupport {
     }
   }
 
-  override fun runWithImplicitRead(runnable: Runnable) {
+  fun runWithImplicitRead(runnable: Runnable) {
     if (!isImplicitReadOnEDTDisabled) {
       runnable.run()
       return
