683e466fb57dca74f7b7d4c2647d0bf274397075
Maria Filipanova <maria.filipanova@jetbrains.com>
2023-10-25 15:16:59 +0200

    GTW-6193: delete RunWidgetExecutionActionMarker, rely on present ExecutorActionStatus instead
    (cherry picked from commit 63849d5b9122b20cb26c859d180063ed09b38b73)
    
    IJ-MR-117880
    
    GitOrigin-RevId: 928d0c945414028ec3914e598ba28d15b557e847

--- platform/execution-impl/src/com/intellij/execution/RunWidgetExecutionActionMarker.java
+++
@@ -1,5 +0,0 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.execution;
-
-/** A marker interface to identify action as execution like and apply icon/color modifications in the run widget */
-public interface RunWidgetExecutionActionMarker { }
