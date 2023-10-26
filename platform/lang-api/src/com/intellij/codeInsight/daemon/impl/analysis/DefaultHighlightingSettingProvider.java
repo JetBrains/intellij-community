a93e09d7e2583d39e6a3d3423708693331c1800d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-10-26 08:29:42 +0200

    IJPL-310 cleanup
    GitOrigin-RevId: 70529cb03c51b64ef88f25ae0458509e02b11aeb

--- platform/lang-api/src/com/intellij/codeInsight/daemon/impl/analysis/DefaultHighlightingSettingProvider.java
+++ platform/lang-api/src/com/intellij/codeInsight/daemon/impl/analysis/DefaultHighlightingSettingProvider.java
@@ -1,5 +1,4 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-
 package com.intellij.codeInsight.daemon.impl.analysis;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
@@ -8,15 +7,15 @@ import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-
 /**
  * Implementation can provide default level of highlighting (one of "none", "syntax checks", "inspections") for a file.
- * This level can be overridden by user for a file via Hector-the-inspector component.
+ * User can override this level for a file via Hector-the-inspector component.
  * If implementation returns {@code null}, next one is checked. If nobody returns anything, "Inspections" level will be used
  * Implement {@link com.intellij.openapi.project.DumbAware} interface to allow implementation to be called in dumb mode
  */
 public abstract class DefaultHighlightingSettingProvider {
-  public static final ExtensionPointName<DefaultHighlightingSettingProvider> EP_NAME = ExtensionPointName.create("com.intellij.defaultHighlightingSettingProvider");
+  public static final ExtensionPointName<DefaultHighlightingSettingProvider> EP_NAME =
+    new ExtensionPointName<>("com.intellij.defaultHighlightingSettingProvider");
 
   public abstract @Nullable FileHighlightingSetting getDefaultSetting(@NotNull Project project, @NotNull VirtualFile file);
 }
