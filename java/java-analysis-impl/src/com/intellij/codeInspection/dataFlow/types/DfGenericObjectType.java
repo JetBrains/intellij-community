c3084a8611b4ba14f34ce39bd7284605f0c9f23d
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-10-26 12:46:10 +0200

    [java-dfa] DfGenericObjectType: cache hashCode
    (cherry picked from commit 0d680d5a2459cd1cfea225b8c3c5458b47128a0e)
    
    IJ-CR-118123
    
    GitOrigin-RevId: a27bdf1720df452404b1d5f6220d51cb151fae4d

--- java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/types/DfGenericObjectType.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/types/DfGenericObjectType.java
@@ -21,6 +21,7 @@ final class DfGenericObjectType extends DfAntiConstantType<Object> implements Df
   private final @Nullable SpecialField mySpecialField;
   private final @NotNull DfType mySpecialFieldType;
   private final boolean myLocal;
+  private int hashCode;
 
   DfGenericObjectType(@NotNull Set<Object> notValues,
                       @NotNull TypeConstraint constraint,
@@ -425,15 +426,18 @@ final class DfGenericObjectType extends DfAntiConstantType<Object> implements Df
 
   @Override
   public int hashCode() {
-    int result = 1;
-    result = 31 * result + myConstraint.hashCode();
-    result = 31 * result + myNullability.hashCode();
-    result = 31 * result + myMutability.hashCode();
-    result = 31 * result + (mySpecialField == null ? 0 : mySpecialField.hashCode());
-    result = 31 * result + mySpecialFieldType.hashCode();
-    result = 31 * result + Boolean.hashCode(myLocal);
-    result = 31 * result + myNotValues.hashCode();
-    return result;
+    if (hashCode == 0) {
+      int result = 1;
+      result = 31 * result + myConstraint.hashCode();
+      result = 31 * result + myNullability.hashCode();
+      result = 31 * result + myMutability.hashCode();
+      result = 31 * result + (mySpecialField == null ? 0 : mySpecialField.hashCode());
+      result = 31 * result + mySpecialFieldType.hashCode();
+      result = 31 * result + Boolean.hashCode(myLocal);
+      result = 31 * result + myNotValues.hashCode();
+      hashCode = result;
+    }
+    return hashCode;
   }
 
   @Override
