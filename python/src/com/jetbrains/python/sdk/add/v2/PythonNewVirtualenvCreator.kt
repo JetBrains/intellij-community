4c76f4590855985a27c63d7e8644833bf006b265
Alexander Koshevoy <alexander.koshevoy@jetbrains.com>
2023-10-26 19:42:05 +0200

    Fix UI freezes on editing "Location" field in fresh "New Project" dialog
    (cherry picked from commit dd228a45b4cefd1f1b7123912cbed0a1d4e1ed56)
    
    IJ-CR-118392
    
    GitOrigin-RevId: ad860723344cd4b5f488a67f66766d65a0886e50

--- python/src/com/jetbrains/python/sdk/add/v2/PythonNewVirtualenvCreator.kt
+++ python/src/com/jetbrains/python/sdk/add/v2/PythonNewVirtualenvCreator.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.sdk.add.v2
 
+import com.intellij.execution.wsl.WslPath.Companion.parseWindowsUncPath
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.application.asContextElement
@@ -9,7 +10,10 @@ import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.ui.ComboBox
+import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.io.OSAgnosticPathUtil
 import com.intellij.ui.dsl.builder.*
 import com.intellij.util.PathUtil
 import com.jetbrains.python.PyBundle.message
@@ -22,6 +26,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.nio.file.InvalidPathException
 import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.io.path.exists
@@ -45,7 +50,7 @@ class PythonNewVirtualenvCreator(presenter: PythonAddInterpreterPresenter) : Pyt
           .whenTextChangedFromUi { locationModified = true }
           .cellValidation {
             addInputRule("This directory already exists") {
-              Paths.get(it.text).exists() // todo validation with custom component, once available
+              it.doesPathExist() // todo validation with custom component, once available
             }
           }
           .align(Align.FILL)
@@ -124,5 +129,39 @@ class PythonNewVirtualenvCreator(presenter: PythonAddInterpreterPresenter) : Pyt
       val userHome = fetchUserHomeDirectory()
       return userHome?.resolve(DEFAULT_VIRTUALENVS_DIR)?.resolve(suggestedVirtualEnvName)?.toString().orEmpty()
     }
+
+    /**
+     * Checks if [this] field's text points to an existing file or directory. Calls [Path.exists] from EDT with care to avoid freezing the
+     * UI.
+     *
+     * In case of Windows machine, this method skips testing UNC paths on existence (except WSL paths) to prevent [Path.exists] checking the
+     * network resources. Such checks might freeze the UI for several seconds when performed from EDT. The freezes reveal themselves when
+     * a user starts typing a WSL path with `\\w` and continues symbol by symbol (`\\ws`, `\\wsl`, etc.) all the way to the root WSL path.
+     *
+     * @see [Path.exists]
+     */
+    private fun TextFieldWithBrowseButton.doesPathExist(): Boolean =
+      text.let { probablyIncompletePath ->
+        if (SystemInfo.isWindows && OSAgnosticPathUtil.isUncPath(probablyIncompletePath)) {
+          val parseWindowsUncPath = parseWindowsUncPath(probablyIncompletePath)
+          if (parseWindowsUncPath?.linuxPath?.isNotBlank() == true) {
+            probablyIncompletePath.safeCheckCorrespondingPathExist()
+          }
+          else {
+            false
+          }
+        }
+        else {
+          probablyIncompletePath.safeCheckCorrespondingPathExist()
+        }
+      }
+
+    private fun String.safeCheckCorrespondingPathExist() =
+      try {
+        Paths.get(this).exists()
+      }
+      catch (e: InvalidPathException) {
+        false
+      }
   }
 }
