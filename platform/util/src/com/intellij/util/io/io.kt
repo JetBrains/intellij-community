db7ded29a553c880fdb054965a9bdb7a5c16f04a
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2023-10-26 14:06:39 +0200

    IJ-CR-113985 computeDetached accepts CoroutineContext, copyToAsync uses that
    GitOrigin-RevId: f050817402f603ca4addd5a39f45477a813ada0d

--- platform/util/src/com/intellij/util/io/io.kt
+++ platform/util/src/com/intellij/util/io/io.kt
@@ -5,7 +5,6 @@ import com.intellij.util.SmartList
 import com.intellij.util.text.CharSequenceBackedByChars
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.withContext
 import kotlinx.coroutines.yield
 import org.jetbrains.annotations.ApiStatus
 import java.io.InputStream
@@ -15,6 +14,7 @@ import java.net.SocketTimeoutException
 import java.net.URLEncoder
 import java.nio.ByteBuffer
 import java.util.*
+import kotlin.coroutines.CoroutineContext
 import kotlin.math.min
 
 fun Reader.readCharSequence(length: Int): CharSequence {
@@ -109,31 +109,34 @@ inline fun String.decodeBase64(): ByteArray = Base64.getDecoder().decode(this)
  */
 @ApiStatus.Experimental
 @OptIn(DelicateCoroutinesApi::class)
-suspend fun InputStream.copyToAsync(outputStream: OutputStream, bufferSize: Int = DEFAULT_BUFFER_SIZE, limit: Long = Long.MAX_VALUE) {
-  computeDetached {
-    withContext(CoroutineName("copyToAsync: $this => $outputStream")) {
-      val buffer = ByteArray(bufferSize)
-      var totalRead = 0L
-      while (totalRead < limit) {
-        yield()
-        val read =
-          try {
-            read(buffer, 0, min(limit - totalRead, buffer.size.toLong()).toInt())
-          }
-          catch (ignored: SocketTimeoutException) {
-            continue
-          }
-        when {
-          read < 0 -> break
-          read > 0 -> {
-            totalRead += read
-            yield()
-            // According to Javadoc, Socket.soTimeout doesn't have any influence on SocketOutputStream.
-            // Had timeout affected sends, it would have impossible to distinguish if the packets were delivered or not in case of timeout.
-            outputStream.write(buffer, 0, read)
-          }
-          else -> Unit
+suspend fun InputStream.copyToAsync(
+  outputStream: OutputStream,
+  bufferSize: Int = DEFAULT_BUFFER_SIZE,
+  limit: Long = Long.MAX_VALUE,
+  context: CoroutineContext = CoroutineName("copyToAsync: $this => $outputStream")
+) {
+  computeDetached(context = context) {
+    val buffer = ByteArray(bufferSize)
+    var totalRead = 0L
+    while (totalRead < limit) {
+      yield()
+      val read =
+        try {
+          read(buffer, 0, min(limit - totalRead, buffer.size.toLong()).toInt())
         }
+        catch (ignored: SocketTimeoutException) {
+          continue
+        }
+      when {
+        read < 0 -> break
+        read > 0 -> {
+          totalRead += read
+          yield()
+          // According to Javadoc, Socket.soTimeout doesn't have any influence on SocketOutputStream.
+          // Had timeout affected sends, it would have impossible to distinguish if the packets were delivered or not in case of timeout.
+          outputStream.write(buffer, 0, read)
+        }
+        else -> Unit
       }
     }
   }
--- platform/util/src/com/intellij/util/io/process.kt
+++ platform/util/src/com/intellij/util/io/process.kt
@@ -7,6 +7,7 @@ import org.jetbrains.annotations.ApiStatus.Internal
 import java.io.BufferedReader
 import java.util.concurrent.CancellationException
 import java.util.concurrent.TimeUnit
+import kotlin.coroutines.CoroutineContext
 import kotlin.time.Duration
 
 /**
@@ -31,7 +32,8 @@ suspend fun Process.awaitExit(): Int {
  * Computes and returns result of the [action] which may block for an unforeseeable amount of time.
  *
  * The [action] does not inherit coroutine context from the calling coroutine, use [withContext] to install proper context if needed.
- * The [action] is executed on a special unlimited dispatcher to avoid starving [Dispatchers.IO].
+ * The [action] is executed on a special unlimited dispatcher to avoid starving [Dispatchers.IO], even if [context] is assigned
+ * to some other dispatcher.
  * The [action] is cancelled if the calling coroutine is cancelled,
  * but this function immediately resumes with CancellationException without waiting for completion of the [action],
  * which means that this function **breaks structured concurrency**.
@@ -42,8 +44,14 @@ suspend fun Process.awaitExit(): Int {
 @DelicateCoroutinesApi // require explicit opt-in
 @IntellijInternalApi
 @Internal
-suspend fun <T> computeDetached(action: suspend CoroutineScope.() -> T): T {
-  val deferred = GlobalScope.async(blockingDispatcher, block = action)
+suspend fun <T> computeDetached(
+  context: CoroutineContext? = null,
+  action: suspend CoroutineScope.() -> T,
+): T {
+  val finalContext =
+    if (context != null) context + blockingDispatcher
+    else blockingDispatcher
+  val deferred = GlobalScope.async(finalContext, block = action)
   try {
     return deferred.await()
   }
