cc69a520a58c6cacd3e19cc2ab0097f428e2df07
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-10-26 08:54:04 +0200

    IJPL-310 cleanup
    GitOrigin-RevId: 7336800b3c5d2f9ac6a7616e51588e157daa680c

--- platform/projectModel-impl/src/com/intellij/openapi/roots/AnnotationOrderRootType.java
+++ platform/projectModel-impl/src/com/intellij/openapi/roots/AnnotationOrderRootType.java
@@ -1,9 +1,8 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.roots;
 
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.ArrayUtilRt;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -46,10 +45,11 @@ public class AnnotationOrderRootType extends PersistentOrderRootType {
       }
 
       @Override
-      public List<VirtualFile> visitModuleSourceOrderEntry(@NotNull final ModuleSourceOrderEntry orderEntry,
-                                                           final List<VirtualFile> value) {
-        Collections
-          .addAll(value, orderEntry.getRootModel().getModuleExtension(JavaModuleExternalPaths.class).getExternalAnnotationsRoots());
+      public List<VirtualFile> visitModuleSourceOrderEntry(@NotNull ModuleSourceOrderEntry orderEntry, List<VirtualFile> value) {
+        JavaModuleExternalPaths moduleExtension = orderEntry.getRootModel().getModuleExtension(JavaModuleExternalPaths.class);
+        if (moduleExtension != null) {
+          Collections.addAll(value, moduleExtension.getExternalAnnotationsRoots());
+        }
         return value;
       }
     };
@@ -57,7 +57,7 @@ public class AnnotationOrderRootType extends PersistentOrderRootType {
     return VfsUtilCore.toVirtualFileArray(result);
   }
 
-  public static String @NotNull [] getUrls(@NotNull OrderEntry entry) {
+  public static @NotNull List<String> getUrls(@NotNull OrderEntry entry) {
     List<String> result = new ArrayList<>();
     RootPolicy<List<String>> policy = new RootPolicy<>() {
       @Override
@@ -73,13 +73,12 @@ public class AnnotationOrderRootType extends PersistentOrderRootType {
       }
 
       @Override
-      public List<String> visitModuleSourceOrderEntry(@NotNull final ModuleSourceOrderEntry orderEntry,
-                                                      final List<String> value) {
+      public List<String> visitModuleSourceOrderEntry(@NotNull ModuleSourceOrderEntry orderEntry, List<String> value) {
         Collections.addAll(value, orderEntry.getRootModel().getModuleExtension(JavaModuleExternalPaths.class).getExternalAnnotationsUrls());
         return value;
       }
     };
     entry.accept(policy, result);
-    return ArrayUtilRt.toStringArray(result);
+    return result;
   }
 }
