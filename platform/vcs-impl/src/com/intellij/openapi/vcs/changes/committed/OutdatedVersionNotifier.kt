841a9d3e3cbc0255c4c01afdce7f8a88c036b668
Yann Cébron <yann.cebron@jetbrains.com>
2023-10-26 17:02:01 +0200

    [vcs] IncomingChangesViewProvider: cleanup
    GitOrigin-RevId: 8e7fd3935682df87d0d2e2709ee70efe620d85ad

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/OutdatedVersionNotifier.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/OutdatedVersionNotifier.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vcs.changes.committed
 
 import com.intellij.openapi.fileEditor.FileEditor
@@ -17,7 +17,7 @@ import org.jetbrains.annotations.Nls
 import java.util.function.Function
 import javax.swing.JComponent
 
-class OutdatedVersionNotifier : EditorNotificationProvider {
+internal class OutdatedVersionNotifier : EditorNotificationProvider {
   override fun collectNotificationData(project: Project, file: VirtualFile): Function<in FileEditor, out JComponent?>? {
     val cache = CommittedChangesCache.getInstanceIfCreated(project) ?: return null
     val (incomingChangeList, incomingChange) = cache.getIncomingChangeList(file) ?: return null
@@ -28,7 +28,7 @@ class OutdatedVersionNotifier : EditorNotificationProvider {
     }
   }
 
-  class IncomingChangesListener(private val project: Project) : CommittedChangesListener {
+  internal class IncomingChangesListener(private val project: Project) : CommittedChangesListener {
     override fun incomingChangesUpdated(receivedChanges: List<CommittedChangeList>?) {
       val cache = CommittedChangesCache.getInstance(project)
 
