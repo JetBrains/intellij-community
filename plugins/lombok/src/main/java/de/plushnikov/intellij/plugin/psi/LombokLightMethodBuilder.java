eda6e6a478d4cecfc81636ad434910bed3003a62
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-10-26 13:39:35 +0200

    [lombok] Cache @Contract annotations
    GitOrigin-RevId: d91eb695fe4fc56071ce374ed35f7670eba6c7b0

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/psi/LombokLightMethodBuilder.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/psi/LombokLightMethodBuilder.java
@@ -3,12 +3,16 @@ package de.plushnikov.intellij.plugin.psi;
 import com.intellij.codeInspection.dataFlow.JavaMethodContractUtil;
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.java.JavaLanguage;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.ModificationTracker;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.CheckUtil;
 import com.intellij.psi.impl.light.LightMethodBuilder;
 import com.intellij.psi.impl.light.LightModifierList;
 import com.intellij.psi.impl.light.LightTypeParameterListBuilder;
+import com.intellij.psi.util.CachedValueProvider;
+import com.intellij.psi.util.CachedValuesManager;
 import com.intellij.util.IncorrectOperationException;
 import de.plushnikov.intellij.plugin.extension.LombokInferredAnnotationProvider;
 import icons.LombokIcons;
@@ -120,11 +124,23 @@ public class LombokLightMethodBuilder extends LightMethodBuilder implements Synt
     return this;
   }
 
-  public LombokLightMethodBuilder withContract(@NotNull String parameters) {
-    putUserData(LombokInferredAnnotationProvider.CONTRACT_ANNOTATION,
-                JavaPsiFacade.getElementFactory(getProject())
-                  .createAnnotationFromText('@' + JavaMethodContractUtil.ORG_JETBRAINS_ANNOTATIONS_CONTRACT + "(" + parameters + ")",
-                                            this));
+  public LombokLightMethodBuilder withPureContract() {
+    Project project = myManager.getProject();
+    PsiAnnotation annotation = CachedValuesManager.getManager(project).getCachedValue(project, () ->
+      CachedValueProvider.Result.create(JavaPsiFacade.getElementFactory(project).
+          createAnnotationFromText('@' + JavaMethodContractUtil.ORG_JETBRAINS_ANNOTATIONS_CONTRACT + "(pure=true)", null),
+        ModificationTracker.NEVER_CHANGED));
+    putUserData(LombokInferredAnnotationProvider.CONTRACT_ANNOTATION, annotation);
+    return this;
+  }
+
+  public LombokLightMethodBuilder withMutatesThisContract() {
+    Project project = myManager.getProject();
+    PsiAnnotation annotation = CachedValuesManager.getManager(project).getCachedValue(project, () ->
+      CachedValueProvider.Result.create(JavaPsiFacade.getElementFactory(project).
+          createAnnotationFromText('@' + JavaMethodContractUtil.ORG_JETBRAINS_ANNOTATIONS_CONTRACT + "(mutates=\"this\")", null),
+        ModificationTracker.NEVER_CHANGED));
+    putUserData(LombokInferredAnnotationProvider.CONTRACT_ANNOTATION, annotation);
     return this;
   }
 
