91d8a807a1653f48a18494b612c03705cf86a756
Ivan Migalev <ivan.migalev@jetbrains.com>
2023-10-27 19:32:32 +0200

    (IDEA-327207) Settings Transfer: take plugin compatibility into account when importing the plugins
    GitOrigin-RevId: 9343494ad9b70014e9560b6df27ab52eb10cf899

--- plugins/ide-startup/importSettings/src/com/intellij/ide/startup/importSettings/transfer/SettingTransferService.kt
+++ plugins/ide-startup/importSettings/src/com/intellij/ide/startup/importSettings/transfer/SettingTransferService.kt
@@ -4,12 +4,15 @@ package com.intellij.ide.startup.importSettings.transfer
 import com.intellij.ide.customize.transferSettings.DefaultTransferSettingsConfiguration
 import com.intellij.ide.customize.transferSettings.TransferSettingsDataProvider
 import com.intellij.ide.customize.transferSettings.controllers.TransferSettingsListener
+import com.intellij.ide.customize.transferSettings.models.FeatureInfo
 import com.intellij.ide.customize.transferSettings.models.IdeVersion
+import com.intellij.ide.customize.transferSettings.models.PluginFeature
 import com.intellij.ide.customize.transferSettings.models.Settings
 import com.intellij.ide.customize.transferSettings.models.SettingsPreferencesKind
 import com.intellij.ide.customize.transferSettings.providers.PluginInstallationState
 import com.intellij.ide.customize.transferSettings.providers.TransferSettingsPerformContext
 import com.intellij.ide.customize.transferSettings.providers.vscode.VSCodeTransferSettingsProvider
+import com.intellij.ide.plugins.marketplace.MarketplaceRequests
 import com.intellij.ide.startup.importSettings.ImportSettingsBundle
 import com.intellij.ide.startup.importSettings.data.BaseSetting
 import com.intellij.ide.startup.importSettings.data.DataForSave
@@ -26,7 +29,9 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.diagnostic.runAndLogException
 import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.openapi.rd.util.withSyncIOBackgroundContext
 import com.intellij.util.containers.nullize
 import com.intellij.util.text.nullize
 import com.jetbrains.rd.util.reactive.OptProperty
@@ -58,9 +63,50 @@ class SettingTransferService : ExternalService {
     ),
     shouldDisplayFailedVersions = false
   )
+
+  data class ThirdPartyProductSettings(
+    val originalSettings: Settings,
+    val compatibleFeatures: Deferred<List<FeatureInfo>>
+  ) {
+    val laf
+      get() = originalSettings.laf
+    val keymap
+      get() = originalSettings.keymap
+    val recentProjects
+      get() = originalSettings.recentProjects
+
+    /**
+     * If this is unable to get the compatible plugins "quickly", then it just returns all the plugins.
+     */
+    fun getCompatiblePluginsQuickly(): List<FeatureInfo> =
+      @Suppress("RAW_RUN_BLOCKING")
+      runBlocking {
+        logger.runAndLogException {
+          withTimeout(1.seconds) {
+            compatibleFeatures.await()
+          }
+        } ?: originalSettings.plugins
+    }
+  }
+
+  data class ThirdPartyProductInfo(
+    val product: IdeVersion,
+    val settings: Deferred<ThirdPartyProductSettings>
+  ) {
+    fun getSettingsQuickly(): ThirdPartyProductSettings? =
+      @Suppress("RAW_RUN_BLOCKING")
+      runBlocking {
+        logger.runAndLogException {
+          withTimeout(5.seconds) {
+            settings.await()
+          }
+        }
+      }
+  }
+
   @Volatile
-  private var ideVersions: Deferred<Map<String, IdeVersion>>? = null
-  private fun CoroutineScope.loadIdeVersionsAsync(): Deferred<Map<String, IdeVersion>> {
+  private var ideVersions: Deferred<Map<String, ThirdPartyProductInfo>>? = null
+  private fun CoroutineScope.loadIdeVersionsAsync(): Deferred<Map<String, ThirdPartyProductInfo>> {
     ideVersions?.let { return it }
     logger.info("Refreshing the transfer settings data provider.")
     var versions = async {
@@ -68,12 +114,35 @@ class SettingTransferService : ExternalService {
         refresh()
         orderedIdeVersions
           .filterIsInstance<IdeVersion>()
-          .map { version -> version.id to version }
+          .map { version -> ThirdPartyProductInfo(version, async { loadIdeVersionSettingsAsync(version) }) }
+          .map { info -> info.product.id to info }
           .toMap()
       }
     }
     ideVersions = versions
     return versions
+  }
+
+  private suspend fun CoroutineScope.loadIdeVersionSettingsAsync(version: IdeVersion): ThirdPartyProductSettings {
+    val settings = version.settingsCache
+    val pluginById = settings.plugins.filterIsInstance<PluginFeature>().map { it.pluginId to it }.toMap()
+    val pluginIds = pluginById.keys.map { PluginId.getId(it) }.toSet()
+    val actualFeatures = async {
+      val compatiblePluginIds =
+        logger.runAndLogException {
+          withSyncIOBackgroundContext {
+            val compatiblePlugins = MarketplaceRequests.getLastCompatiblePluginUpdate(pluginIds)
+            compatiblePlugins.mapNotNull { it.pluginId.toString() }
+          }
+        } ?: pluginIds
+      settings.plugins.filter {
+        when (it) {
+          is PluginFeature -> compatiblePluginIds.contains(it.pluginId)
+          else -> true
+        }
+      }
+    }
+    return ThirdPartyProductSettings(settings, actualFeatures)
   }
 
   override suspend fun warmUp() {
@@ -83,7 +152,7 @@ class SettingTransferService : ExternalService {
   }
 
   @OptIn(ExperimentalCoroutinesApi::class)
-  private fun loadIdeVersions(): Map<String, IdeVersion> {
+  private fun loadProductInfos(): Map<String, ThirdPartyProductInfo> {
     ideVersions?.let { if (it.isCompleted) return it.getCompleted() }
 
     @Suppress("RAW_RUN_BLOCKING")
@@ -104,20 +173,20 @@ class SettingTransferService : ExternalService {
 
   override fun products(): List<Product> {
     return logger.runAndLogException {
-      val versions = loadIdeVersions().values
-      versions.map(ExternalProductInfo::ofIdeVersion)
+      val versions = loadProductInfos().values
+      versions.map { ExternalProductInfo.ofIdeVersion(it.product) }
     } ?: emptyList()
   }
 
   override fun getSettings(itemId: String): List<BaseSetting> {
     return logger.runAndLogException {
-      val versions = loadIdeVersions()
-      val version = versions[itemId] ?: return emptyList()
-      val settings = version.settingsCache // TODO: Preload in background
+      val versions = loadProductInfos()
+      val product = versions[itemId] ?: return emptyList()
+      val settings = product.getSettingsQuickly() ?: error("Cannot load settings for $itemId quickly.")
       buildList {
         settings.laf?.let(TransferableSetting::uiTheme)?.let(::add)
         settings.keymap?.let(TransferableSetting::keymap)?.let(::add)
-        settings.plugins.nullize()?.let(TransferableSetting::plugins)?.let(::add)
+        settings.getCompatiblePluginsQuickly().nullize()?.let(TransferableSetting::plugins)?.let(::add)
         settings.recentProjects.nullize()?.let { TransferableSetting.recentProjects() }?.let(::add)
       }
     } ?: emptyList()
@@ -126,20 +195,21 @@ class SettingTransferService : ExternalService {
   override fun getProductIcon(itemId: String,
                               size: IconProductSize): Icon? {
     return logger.runAndLogException {
-      val version = loadIdeVersions()[itemId] ?: return null
-      return version.transferableId.icon(size)
+      val info = loadProductInfos()[itemId] ?: return null
+      return info.product.transferableId.icon(size)
     }
   }
 
   override fun importSettings(productId: String, data: List<DataForSave>): DialogImportData {
     try {
-      val ideVersion = loadIdeVersions()[productId] ?: error(ImportSettingsBundle.message("transfer.error.product-not-found", productId))
-      applyPreferences(ideVersion, data)
-      val importData = TransferSettingsProgress(ideVersion)
+      val info = loadProductInfos()[productId] ?: error(ImportSettingsBundle.message("transfer.error.product-not-found", productId))
+      val product = info.product
+      applyPreferences(product, data)
+      val importData = TransferSettingsProgress(product)
       config.controller.addListener(TransferSettingsWizardListener())
       config.controller.performImport(
         null,
-        ideVersion,
+        product,
         importData.createProgressIndicatorAdapter()
       )
       return importData
@@ -154,16 +224,16 @@ class SettingTransferService : ExternalService {
       return dummyImportData()
     }
   }
-}
 
-private fun applyPreferences(ideVersion: IdeVersion, toApply: List<DataForSave>) {
-  val selectedIds = toApply.asSequence().map { it.id }.toSet()
-  val preferences = ideVersion.settingsCache.preferences
-  preferences[SettingsPreferencesKind.Laf] = selectedIds.contains(TransferableSetting.UI_ID)
-  preferences[SettingsPreferencesKind.SyntaxScheme] = selectedIds.contains(TransferableSetting.UI_ID)
-  preferences[SettingsPreferencesKind.Keymap] = selectedIds.contains(TransferableSetting.KEYMAP_ID)
-  preferences[SettingsPreferencesKind.Plugins] = selectedIds.contains(TransferableSetting.PLUGINS_ID)
-  preferences[SettingsPreferencesKind.RecentProjects] = selectedIds.contains(TransferableSetting.RECENT_PROJECTS_ID)
+  private fun applyPreferences(product: IdeVersion, toApply: List<DataForSave>) {
+    val selectedIds = toApply.asSequence().map { it.id }.toSet()
+    val preferences = product.settingsCache.preferences
+    preferences[SettingsPreferencesKind.Laf] = selectedIds.contains(TransferableSetting.UI_ID)
+    preferences[SettingsPreferencesKind.SyntaxScheme] = selectedIds.contains(TransferableSetting.UI_ID)
+    preferences[SettingsPreferencesKind.Keymap] = selectedIds.contains(TransferableSetting.KEYMAP_ID)
+    preferences[SettingsPreferencesKind.Plugins] = selectedIds.contains(TransferableSetting.PLUGINS_ID)
+    preferences[SettingsPreferencesKind.RecentProjects] = selectedIds.contains(TransferableSetting.RECENT_PROJECTS_ID)
+  }
 }
 
 internal class TransferSettingsWizardListener : TransferSettingsListener {
