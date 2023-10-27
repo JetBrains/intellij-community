2b9218d19ad669a65263532f5f83719ad0d8ae5e
Artem.Bukhonov <artem.bukhonov@jetbrains.com>
2023-10-27 19:36:35 +0200

    RDCT-555 Sync language level to the client
    Improve per file reporting
    
    (cherry picked from commit 03e5ec345f3adc225da3d6bf7de8f184d7dc80a1)
    
    IJ-MR-118792
    
    GitOrigin-RevId: 3f6240dc3ad6cda5e32c4c8b00830ccffe2d67b8

--- java/java-impl/src/com/intellij/openapi/roots/impl/JavaLangLevelVirtualFileExtrasProvider.kt
+++ java/java-impl/src/com/intellij/openapi/roots/impl/JavaLangLevelVirtualFileExtrasProvider.kt
@@ -2,21 +2,17 @@
 package com.intellij.openapi.roots.impl
 
 import com.intellij.openapi.application.readAction
-import com.intellij.openapi.components.Service
-import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.LanguageLevelProjectExtension
 import com.intellij.openapi.roots.NullableLanguageLevelHolder
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileExtrasProvider
 import com.intellij.pom.java.LanguageLevel
 import com.intellij.psi.PsiJavaFile
 import com.intellij.psi.PsiManager
-import kotlinx.coroutines.channels.SendChannel
-import kotlinx.coroutines.channels.awaitClose
+import com.intellij.util.messages.impl.subscribeAsFlow
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.map
 import kotlin.reflect.KType
 import kotlin.reflect.typeOf
 
@@ -31,60 +27,20 @@ class JavaLangLevelVirtualFileExtrasProvider : VirtualFileExtrasProvider<Nullabl
     get() = typeOf<NullableLanguageLevelHolder>()
 
   override fun getValues(project: Project, virtualFile: VirtualFile): Flow<NullableLanguageLevelHolder> {
-    return project.service<JavaLangLevelVirtualFileProviderProjectService>().getValues(virtualFile)
-  }
-}
-
-@Service(Service.Level.PROJECT)
-class JavaLangLevelVirtualFileProviderProjectService(val project: Project) {
-  private val syncedVirtualFiles2sendChannels = hashMapOf<VirtualFile, SendChannel<NullableLanguageLevelHolder>>()
-  private var fileJavaLevelChangedListenerCreated = false
-
-  fun getValues(virtualFile: VirtualFile): Flow<NullableLanguageLevelHolder> {
-    if (!fileJavaLevelChangedListenerCreated) {
-      //LOG.trace("syncStarted: create file java level change listener")
-      startListenToFileJavaLevelChange()
-      fileJavaLevelChangedListenerCreated = true
-    }
-
-    return callbackFlow {
-      val fileJavaLevel = readAction {
-        val psiFile = PsiManager.getInstance(project).findFile(virtualFile)
-        if (psiFile !is PsiJavaFile) return@readAction null
-        psiFile.languageLevel
-      }
-
-      if (fileJavaLevel != null) {
-        val projectJavaLevel = LanguageLevelProjectExtension.getInstance(project).languageLevel
-        val langLevelToSend = if (fileJavaLevel == projectJavaLevel) null else fileJavaLevel
-        val encoded = NullableLanguageLevelHolder(langLevelToSend)
-        this.trySend(encoded)
-      }
-
-      synchronized(syncedVirtualFiles2sendChannels) {
-        syncedVirtualFiles2sendChannels[virtualFile] = this
-      }
-
-      awaitClose {
-        synchronized(syncedVirtualFiles2sendChannels) {
-          syncedVirtualFiles2sendChannels.remove(virtualFile)
+    return project.messageBus.subscribeAsFlow(VirtualFileJavaLanguageLevelListener.TOPIC) {
+      trySend(getFileJavaLanguageLevel(virtualFile, project))
+      object : VirtualFileJavaLanguageLevelListener {
+        override fun levelChanged(changedFile: VirtualFile, newLevel: LanguageLevel?) {
+          if (changedFile != virtualFile) return
+          trySend(newLevel)
         }
       }
-    }
+    }.map { NullableLanguageLevelHolder(it) }
   }
 
-  private fun startListenToFileJavaLevelChange() {
-    project.messageBus.connect().subscribe(VirtualFileJavaLevelChangeListener.TOPIC, object : VirtualFileJavaLevelChangeListener {
-      override fun levelChanged(virtualFile: VirtualFile, newLevel: LanguageLevel?) {
-        val channel = synchronized(syncedVirtualFiles2sendChannels) {
-          syncedVirtualFiles2sendChannels[virtualFile]
-        } ?: return
-        //LOG.trace { "VirtualFileJavaLevelChangeListener.levelChanged called for synced file: file=${virtualFile.name} newLevel=$newLevel" }
-        val projectJavaLevel = LanguageLevelProjectExtension.getInstance(project).languageLevel
-        val langLevelToSend = if (newLevel == projectJavaLevel) null else newLevel
-        val encoded = NullableLanguageLevelHolder(langLevelToSend)
-        channel.trySend(encoded)
-      }
-    })
+  private suspend fun getFileJavaLanguageLevel(virtualFile: VirtualFile, project: Project) = readAction {
+    val psiFile = PsiManager.getInstance(project).findFile(virtualFile)
+    if (psiFile !is PsiJavaFile) return@readAction null
+    return@readAction psiFile.languageLevel
   }
 }
