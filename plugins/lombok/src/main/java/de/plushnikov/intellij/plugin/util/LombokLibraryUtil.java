f76b507c7a22fe520acb405eaed7d926c6e624af
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-10-29 19:51:00 +0100

    [lombok] IDEA-336483 Lombok inspections work in modules without Lombok dependency
    GitOrigin-RevId: 4e3c9179fa7491cf1b6eb19454553a0245aaa414

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/util/LombokLibraryUtil.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/util/LombokLibraryUtil.java
@@ -3,6 +3,7 @@ package de.plushnikov.intellij.plugin.util;
 import com.intellij.java.library.JavaLibraryUtil;
 import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.OrderEntry;
@@ -19,6 +20,7 @@ import com.intellij.psi.PsiPackage;
 import com.intellij.psi.util.CachedValueProvider.Result;
 import com.intellij.psi.util.CachedValuesManager;
 import com.intellij.util.concurrency.ThreadingAssertions;
+import de.plushnikov.intellij.plugin.LombokClassNames;
 import de.plushnikov.intellij.plugin.Version;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -37,6 +39,10 @@ public final class LombokLibraryUtil {
     ThreadingAssertions.assertReadAccess();
     return JavaLibraryUtil.hasLibraryJar(project, "org.projectlombok:lombok")
            || detectLombokJarsSlow(project);
+  }
+
+  public static boolean hasLombokClasses(@Nullable Module module) {
+    return JavaLibraryUtil.hasLibraryClass(module, LombokClassNames.GETTER);
   }
 
   private static boolean detectLombokJarsSlow(Project project) {
