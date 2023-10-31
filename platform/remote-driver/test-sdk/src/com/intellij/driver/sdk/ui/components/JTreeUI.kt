1a764aeff74002d6f6ec75071e64809b8825079e
eugene.nizienko <eugene.nizienko@jetbrains.com>
2023-10-31 12:51:11 +0100

    [remote driver] expand tree path in projectView
    GitOrigin-RevId: 50c2d8892fa97965325ee7933d2478c412b2f2a4

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/JTreeUI.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/JTreeUI.kt
@@ -16,17 +16,42 @@ class JTreeUiComponent(data: ComponentData) : UiComponent(data) {
   private val fixture by lazy {  driver.new(JTreeFixtureRef::class, robotService.robot, component) }
 
   private fun clickRow(row: Int) = fixture.clickRow(row)
+  private fun rightClickRow(row: Int) = fixture.rightClickRow(row)
   private fun doubleClickRow(row: Int) = fixture.doubleClickRow(row)
   fun clickPath(vararg path: String, fullMatch: Boolean = true) {
+    expandPath(*path, fullMatch = fullMatch)
     findExpandedPath(*path, fullMatch = fullMatch)?.let {
       clickRow(it.row)
     } ?: throw PathNotFoundException(path.toList())
   }
 
+  fun rightClickPath(vararg path: String, fullMatch: Boolean = true) {
+    expandPath(*path, fullMatch = fullMatch)
+    findExpandedPath(*path, fullMatch = fullMatch)?.let {
+      rightClickRow(it.row)
+    } ?: throw PathNotFoundException(path.toList())
+  }
+
   fun doubleClickPath(vararg path: String, fullMatch: Boolean = true) {
+    expandPath(*path, fullMatch = fullMatch)
     findExpandedPath(*path, fullMatch = fullMatch)?.let {
       doubleClickRow(it.row)
     } ?: throw PathNotFoundException(path.toList())
+  }
+
+  private fun expandPath(vararg path: String, fullMatch: Boolean = true) {
+    val expandedPath = mutableListOf<String>()
+    path.forEach {
+      var currentPathPath = findExpandedPath(*(expandedPath + listOf(it)).toTypedArray(), fullMatch = fullMatch)
+      if (currentPathPath == null) {
+        doubleClickPath(*expandedPath.toTypedArray(), fullMatch = fullMatch)
+      }
+      currentPathPath = findExpandedPath(*(expandedPath + listOf(it)).toTypedArray(), fullMatch = fullMatch)
+      if (currentPathPath == null) {
+        throw PathNotFoundException(expandedPath + listOf(it) + " path not found")
+      }
+      expandedPath.add(it)
+    }
   }
 
   private fun findExpandedPath(vararg path: String, fullMatch: Boolean): TreePathToRow? = collectExpandedPaths().singleOrNull { expandedPath ->
