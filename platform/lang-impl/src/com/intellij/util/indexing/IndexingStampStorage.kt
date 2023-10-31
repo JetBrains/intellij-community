08ea6236a167f78b78676fb0c793f0b8d0a339d7
Andrei.Kuznetsov <andrei.kuznetsov@jetbrains.com>
2023-10-31 14:08:49 +0100

    IDEA-336540: move "fast_index_stamps" storages from <system>/caches/fast_index_stamps to <indexes>/fast_index_stamps
    Otherwise, clearing <indexes> directory will only drop enumerator, but not IndexingStamps themselves. Enumerator is currently implemented over "keystream" files, where all the enumerated object are placed one after another into a single file, and enumerated value (int) is the offset inside this file. Failing to clear IndexingStamp (VFS) will result in wrong access to enumerator's "keystream" file (indexes)
    
    GitOrigin-RevId: 1a576b50d92b71cab384bd80bd75672e51553b63

--- platform/lang-impl/src/com/intellij/util/indexing/IndexingStampStorage.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/IndexingStampStorage.kt
@@ -2,13 +2,10 @@
 package com.intellij.util.indexing
 
 import com.intellij.openapi.application.PathManager
-import com.intellij.openapi.diagnostic.thisLogger
-import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.newvfs.FileAttribute
 import com.intellij.openapi.vfs.newvfs.persistent.FSRecords
 import com.intellij.util.indexing.TimestampsImmutable.Companion.readTimestamps
-import com.intellij.util.indexing.impl.perFileVersion.IntFileAttribute
-import com.intellij.util.io.CachingEnumerator
+import com.intellij.util.indexing.impl.perFileVersion.EnumeratedFastFileAttribute
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.VisibleForTesting
 import java.io.Closeable
@@ -64,45 +61,26 @@ class IndexingStampStorageOverRegularAttributes : IndexingStampStorage {
 internal class IndexingStampStorageOverFastAttributes : IndexingStampStorage {
   companion object {
     val PERSISTENCE: FileAttribute = FileAttribute("__fast_index_stamps__", 0, true)
-    private const val MARKER_FILE_ID = 1
   }
 
-  private val enumerator: PersistentTimestampsEnumerator
-  private val enumeratorCache: CachingEnumerator<TimestampsImmutable>
-  private val persistence = IntFileAttribute.overFastAttribute(PERSISTENCE)
+  private val persistence: EnumeratedFastFileAttribute<TimestampsImmutable>
 
   init {
-    // Don't put this to getIndexRoot()/fast_index_stamps. Shared indexes don't like it. Check SharedIndexesTest
-    val dir = PathManager.getSystemDir().resolve("caches/fast_index_stamps")
+    val dir = PathManager.getIndexRoot().resolve("fast_index_stamps")
 
-    val recreated = persistence.readInt(MARKER_FILE_ID)
-    if (recreated == 0) { // Mark absent. Probably, VFS was recreated
-      thisLogger().info("Recreating IndexingStampStorageOverFastAttributes because VFS was recreated")
-      FileUtil.deleteWithRenamingIfExists(dir)
-      persistence.writeInt(MARKER_FILE_ID, 1)
+    persistence = EnumeratedFastFileAttribute(dir, PERSISTENCE, TimestampsKeyDescriptor()) { enumeratorPath ->
+      PersistentTimestampsEnumerator(enumeratorPath)
     }
-
-    enumerator = PersistentTimestampsEnumerator(dir.resolve("fast_index_stamps_keys"))
-    enumeratorCache = CachingEnumerator(enumerator, TimestampsKeyDescriptor())
-
-    // this is exactly the same way as persistence:IntFileAttribute is closed
-    FSRecords.getInstance().addCloseable(enumerator)
   }
 
   @Throws(IOException::class)
   override fun writeTimestamps(fileId: Int, timestamps: TimestampsImmutable) {
-    val value = enumeratorCache.enumerate(timestamps)
-    // use force to stay on safe side (especially noticeable in tests, where runnables submitted via FSRecords.addCloseable
-    // are not invoked, and enumerator is not flushed)
-    enumerator.force()
-    persistence.writeInt(fileId, value)
+    persistence.writeEnumerated(fileId, timestamps)
   }
 
   override fun readTimestamps(fileId: Int): TimestampsImmutable? {
     try {
-      val value = persistence.readInt(fileId)
-      if (value == 0) return null
-      return enumeratorCache.valueOf(value)
+      return persistence.readEnumerated(fileId)
     }
     catch (e: IOException) {
       throw FSRecords.handleError(e)
@@ -111,6 +89,5 @@ internal class IndexingStampStorageOverFastAttributes : IndexingStampStorage {
 
   override fun close() {
     persistence.close()
-    enumerator.close()
   }
 }
