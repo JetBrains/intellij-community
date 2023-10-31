65f8b3a286d8cc25dc699315a2e0fe38ab7aa2dc
Nikita Ashihmin <Nikita.Ashihmin@jetbrains.com>
2023-10-31 22:15:09 +0400

    [bdt] BDIDE-5203 PySpark(feat): Add columns info to Hint Documentation
    GitOrigin-RevId: 41a5912a85254c6ea53c8be1805884b0a2491718

--- python/python-psi-api/src/com/jetbrains/python/documentation/PythonDocumentationQuickInfoProvider.java
+++ python/python-psi-api/src/com/jetbrains/python/documentation/PythonDocumentationQuickInfoProvider.java
@@ -17,6 +17,7 @@ package com.jetbrains.python.documentation;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -37,5 +38,15 @@ public interface PythonDocumentationQuickInfoProvider {
    * @return info (if exists) or null (if another provider should be checked)
    */
   @Nullable
-  @Nls String getQuickInfo(@NotNull PsiElement originalElement);
+  @Nls
+  default String getQuickInfo(@NotNull PsiElement originalElement) {
+    return null;
+  }
+
+  @ApiStatus.Experimental
+  @Nullable
+  @Nls
+  default String getHoverAdditionalQuickInfo(@Nullable PsiElement originalElement) {
+    return null;
+  }
 }
