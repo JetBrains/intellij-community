63ff61d3c45b05ca36c83d2e79ad7377111de66e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-11-02 10:50:03 +0100

    IJPL-90 DynamicMembersStartupActivity - convert to modern language
    GitOrigin-RevId: aae4d74761df9d42a2c67c6e59ed3a02ab1a8c92

--- plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/intentions/dynamic/DynamicMembersStartupActivity.kt
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/intentions/dynamic/DynamicMembersStartupActivity.kt
@@ -1,28 +1,26 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.groovy.annotator.intentions.dynamic;
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.annotator.intentions.dynamic
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.extensions.ExtensionNotApplicableException;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.openapi.wm.ToolWindowManager;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.extensions.ExtensionNotApplicableException
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.wm.ToolWindowManager.Companion.getInstance
 
-public final class DynamicMembersStartupActivity implements StartupActivity.DumbAware {
-  public DynamicMembersStartupActivity() {
-    if (ApplicationManager.getApplication().isHeadlessEnvironment()) {
-      throw ExtensionNotApplicableException.create();
+class DynamicMembersStartupActivity : StartupActivity.DumbAware {
+  init {
+    if (ApplicationManager.getApplication().isHeadlessEnvironment) {
+      throw ExtensionNotApplicableException.create()
     }
   }
 
-  @Override
-  public void runActivity(@NotNull Project project) {
-    DynamicManager manager = DynamicManager.getInstance(project);
-    if (!manager.getRootElement().getContainingClasses().isEmpty()) {
-      ToolWindowManager.getInstance(project).invokeLater(() -> {
+  override fun runActivity(project: Project) {
+    val manager = DynamicManager.getInstance(project)
+    if (!manager.rootElement.getContainingClasses().isEmpty()) {
+      getInstance(project).invokeLater {
         // initialize toolWindow
-        DynamicToolWindowWrapper.getInstance(project).getToolWindow();
-      });
+        DynamicToolWindowWrapper.getInstance(project).toolWindow
+      }
     }
   }
 }
