b11a4d5bfb38f16727e860c6dd97f47da833d8cc
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-11-02 11:02:07 +0100

    IJPL-90 DynamicMembersStartupActivity - use ProjectActivity
    GitOrigin-RevId: 7958f4e9c41bac0ab2c7515da7e2003d3b9add28

--- plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/intentions/dynamic/DynamicMembersStartupActivity.kt
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/intentions/dynamic/DynamicMembersStartupActivity.kt
@@ -2,24 +2,27 @@
 package org.jetbrains.plugins.groovy.annotator.intentions.dynamic
 
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.service
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.extensions.ExtensionNotApplicableException
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
-import com.intellij.openapi.wm.ToolWindowManager.Companion.getInstance
+import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.openapi.wm.ToolWindowManager
 
-class DynamicMembersStartupActivity : StartupActivity.DumbAware {
+private class DynamicMembersStartupActivity : ProjectActivity {
   init {
     if (ApplicationManager.getApplication().isHeadlessEnvironment) {
       throw ExtensionNotApplicableException.create()
     }
   }
 
-  override fun runActivity(project: Project) {
-    val manager = DynamicManager.getInstance(project)
+  override suspend fun execute(project: Project) {
+    val manager = project.serviceAsync<DynamicManager>()
     if (!manager.rootElement.getContainingClasses().isEmpty()) {
-      getInstance(project).invokeLater {
+      project.serviceAsync<ToolWindowManager>().invokeLater {
         // initialize toolWindow
-        DynamicToolWindowWrapper.getInstance(project).toolWindow
+        @Suppress("SimplifiableServiceRetrieving")
+        project.service<DynamicToolWindowWrapper>().toolWindow
       }
     }
   }
