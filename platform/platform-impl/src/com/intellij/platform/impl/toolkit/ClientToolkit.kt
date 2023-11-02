e983c8fdf7f344dd624d74ea58f6e9206aa5501d
Sviatoslav Vlasov <sviatoslav.vlasov@jetbrains.com>
2023-11-02 22:25:01 +0000

    [lux] Lux subsystem refactoring, change visibility and size sending order, separate adapters, simplify peers, also fix GTW-6209
    Merge-request: IJ-MR-117054
    Merged-by: Sviatoslav Vlasov <Sviatoslav.Vlasov@jetbrains.com>
    
    GitOrigin-RevId: a1017a88fd23aa470596e0a597b9222cd800ef53

--- platform/platform-impl/src/com/intellij/platform/impl/toolkit/ClientToolkit.kt
+++ platform/platform-impl/src/com/intellij/platform/impl/toolkit/ClientToolkit.kt
@@ -2,13 +2,16 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.impl.toolkit
 
+import com.intellij.openapi.components.service
 import java.awt.*
 import java.awt.peer.DialogPeer
 import java.awt.peer.FramePeer
 import java.awt.peer.WindowPeer
 
 interface ClientToolkit {
-  fun createPanelWindow(panel: Component, target: Window, realParent: Container?): WindowPeer
+  companion object {
+    fun getInstance(): ClientToolkit = service()
+  }
   fun createWindow(target: Window): WindowPeer
   fun createDialog(target: Dialog): DialogPeer
   fun createFrame(target: Frame): FramePeer
