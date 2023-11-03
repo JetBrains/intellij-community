2537b2060958b0c05f2d562f5c1074bf4a09fcc0
vladimir.petrenko <vladimir.petrenko@jetbrains.com>
2023-11-03 18:43:44 +0700

    RDCT-555 Sync language level to the client: fix "Root package for module ... must be ..." test failure
    (cherry picked from commit fe70ca9ba25a98f5d3bf900f93db469ded4adc42)
    
    IJ-MR-118792
    
    GitOrigin-RevId: 7af940f99d43954cbe26bf307bb09360babecdc3

--- java/java-frontback-psi-impl/src/com/intellij/java/frontback/psi/impl/JavaLangLevelProjectCustomDataSynchronizer.kt
+++ java/java-frontback-psi-impl/src/com/intellij/java/frontback/psi/impl/JavaLangLevelProjectCustomDataSynchronizer.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.lang.java
+package com.intellij.java.frontback.psi.impl
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectCustomDataSynchronizer
