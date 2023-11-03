ec855cf1f4235a03ad930c6bb05cfc97b52623f8
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2023-11-03 17:32:40 +0100

    IDEA-335203, IDEA-210758 Add reproducer
    See KT-63454 for corresponding fix in compiler.
    
    #IDEA-335203 Fixed
    #IDEA-210758 Fixed
    
    GitOrigin-RevId: 8b8b17647b2e93a71a8e1be17f1d9599074ea22b

--- plugins/kotlin/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/IrKotlinEvaluateExpressionTestGenerated.java
+++ plugins/kotlin/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/IrKotlinEvaluateExpressionTestGenerated.java
@@ -1371,6 +1371,11 @@ public abstract class IrKotlinEvaluateExpressionTestGenerated extends AbstractIr
                 runTest("testData/evaluation/singleBreakpoint/inlineMethodsInSignature.kt");
             }
 
+            @TestMetadata("inlineWithAnonymousObject.kt")
+            public void testInlineWithAnonymousObject() throws Exception {
+                runTest("testData/evaluation/singleBreakpoint/inlineWithAnonymousObject.kt");
+            }
+
             @TestMetadata("innerClass.kt")
             public void testInnerClass() throws Exception {
                 runTest("testData/evaluation/singleBreakpoint/innerClass.kt");
