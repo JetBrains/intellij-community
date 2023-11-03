0f6a51f5539caa404811775a9bcecf2e193e79af
Aleksei Kniazev <aleksei.kniazev@jetbrains.com>
2023-11-03 17:12:25 +0100

    [python] navigation for python New Project dialog via validation-suggested fix (PY-60214)
    (cherry picked from commit e5e604178669fdb35febcfa602efeddbee5a19bd)
    
    IJ-CR-119043
    
    GitOrigin-RevId: 89a9344b6b0f9d993bfa0ddce49e6a4165a57ce9

--- python/src/com/jetbrains/python/sdk/add/v2/PythonAddInterpreterState.kt
+++ python/src/com/jetbrains/python/sdk/add/v2/PythonAddInterpreterState.kt
@@ -15,8 +15,12 @@ class PythonAddInterpreterState(
   val basePythonSdks: ObservableMutableProperty<List<Sdk>>,
   val allExistingSdks: ObservableMutableProperty<List<Sdk>>,
   val basePythonVersion: ObservableMutableProperty<Sdk?>,
+  val selectedVenv: ObservableMutableProperty<Sdk?>,
   val condaExecutable: ObservableMutableProperty<String>,
 ) {
   val basePythonHomePath = basePythonVersion.transform({ sdk -> sdk?.homePath ?: "" },
                                                        { path -> basePythonSdks.get().find { it.homePath == path }!! })
+
+  val selectedVenvPath = selectedVenv.transform({ sdk -> sdk?.homePath ?: "" },
+                                                     { path -> allExistingSdks.get().find { it.homePath == path }!! })
 }
--- python/src/com/jetbrains/python/sdk/add/v2/PythonLocalEnvironmentCreator.kt
+++ python/src/com/jetbrains/python/sdk/add/v2/PythonLocalEnvironmentCreator.kt
@@ -13,7 +13,7 @@ import com.intellij.ui.dsl.builder.bindItem
 import com.jetbrains.python.PyBundle.message
 import com.jetbrains.python.sdk.add.v2.PythonSupportedEnvironmentManagers.*
 
-class PythonLocalEnvironmentCreator(presenter: PythonAddInterpreterPresenter) : PythonTargetEnvironmentInterpreterCreator {
+class PythonLocalEnvironmentCreator(val presenter: PythonAddInterpreterPresenter) : PythonTargetEnvironmentInterpreterCreator {
 
   private val propertyGraph = presenter.state.propertyGraph
   private val selectionMethod = propertyGraph.property(PythonInterpreterSelectionMethod.CREATE_NEW)
@@ -36,16 +36,25 @@ class PythonLocalEnvironmentCreator(presenter: PythonAddInterpreterPresenter) : 
 
 
   override fun buildPanel(outerPanel: Panel, validationRequestor: DialogValidationRequestor) {
+    with(presenter) {
+      navigator.selectionMethod = selectionMethod
+      navigator.newEnvManager = newInterpreterManager
+      navigator.existingEnvManager = existingInterpreterManager
+    }
+
     with(outerPanel) {
       buttonsGroup {
         row(message("sdk.create.custom.env.creation.type")) {
-          radioButton(message("sdk.create.custom.generate.new"), PythonInterpreterSelectionMethod.CREATE_NEW).onChanged {
+          val newRadio = radioButton(message("sdk.create.custom.generate.new"), PythonInterpreterSelectionMethod.CREATE_NEW).onChanged {
             selectionMethod.set(
               if (it.isSelected) PythonInterpreterSelectionMethod.CREATE_NEW else PythonInterpreterSelectionMethod.SELECT_EXISTING)
-          }
-          radioButton(message("sdk.create.custom.select.existing"), PythonInterpreterSelectionMethod.SELECT_EXISTING).onChanged {
-            selectionMethod.set(
-              if (it.isSelected) PythonInterpreterSelectionMethod.SELECT_EXISTING else PythonInterpreterSelectionMethod.CREATE_NEW)
+          }.component
+
+          val existingRadio = radioButton(message("sdk.create.custom.select.existing"), PythonInterpreterSelectionMethod.SELECT_EXISTING).component
+
+          selectionMethod.afterChange {
+            newRadio.isSelected = it == PythonInterpreterSelectionMethod.CREATE_NEW
+            existingRadio.isSelected = it == PythonInterpreterSelectionMethod.SELECT_EXISTING
           }
         }
       }.bind({ selectionMethod.get() }, { selectionMethod.set(it) })
