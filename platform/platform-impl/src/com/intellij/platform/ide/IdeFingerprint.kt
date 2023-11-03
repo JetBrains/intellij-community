5a85efe6555eadf018cc225ce40480f7a845d7fd
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-11-03 10:31:06 +0100

    IJPL-195 no need to use intermediate map to compute plugin fingerprints
    GitOrigin-RevId: 103dd4fdfe021826482248e794ce6f72a1c003f8

--- platform/platform-impl/src/com/intellij/platform/ide/IdeFingerprint.kt
+++ platform/platform-impl/src/com/intellij/platform/ide/IdeFingerprint.kt
@@ -1,12 +1,23 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.ide
 
+import com.dynatrace.hash4j.hashing.HashStream64
 import com.dynatrace.hash4j.hashing.Hashing
+import com.intellij.ide.plugins.IdeaPluginDescriptor
+import com.intellij.ide.plugins.IdeaPluginDescriptorImpl
 import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.idea.AppMode
 import com.intellij.openapi.application.impl.ApplicationInfoImpl
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.progress.ProgressManager
 import org.jetbrains.annotations.ApiStatus.Internal
+import java.nio.file.Files
+import java.nio.file.LinkOption
+import java.nio.file.NoSuchFileException
+import java.nio.file.Path
+import java.nio.file.attribute.BasicFileAttributes
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.walk
 
 @Internal
 fun ideFingerprint(debugHelperToken: Int = 0): Long {
@@ -26,12 +37,10 @@ fun ideFingerprint(debugHelperToken: Int = 0): Long {
     hasher.putLong(System.currentTimeMillis())
   }
   else {
-    val pluginHasher = PluginHasher(expectedSize = loadedPlugins.size)
     for (plugin in loadedPlugins) {
       // no need to check bundled plugins - handled by taking build time and version into account
       if (!plugin.isBundled) {
-        ProgressManager.checkCanceled()
-        pluginHasher.addPluginFingerprint(plugin = plugin, hasher = hasher)
+        addPluginFingerprint(plugin = plugin, hasher = hasher)
       }
     }
   }
@@ -42,7 +51,70 @@ fun ideFingerprint(debugHelperToken: Int = 0): Long {
 
   val durationMs = System.currentTimeMillis() - startTime
   val fingerprintString = java.lang.Long.toUnsignedString(fingerprint, Character.MAX_RADIX)
-  Logger.getInstance("com.intellij.platform.ide.IdeFingerprint").info("Calculated dependencies fingerprint in $durationMs ms " +
+  Logger.getInstance("com.intellij.platform.ide.IdeFingerprint")
+    .info("Calculated dependencies fingerprint in $durationMs ms " +
           "(hash=$fingerprintString, buildTime=${appInfo.buildTime.toEpochSecond()}, appVersion=${appInfo.build.asString()})")
   return fingerprint
+}
+
+private fun addPluginFingerprint(plugin: IdeaPluginDescriptor, hasher: HashStream64) {
+  hasher.putString(plugin.pluginId.idString)
+  hasher.putString(plugin.version)
+  if (plugin.version.contains("SNAPSHOT", ignoreCase = true)) {
+    hashByFileContent(plugin as IdeaPluginDescriptorImpl, hasher)
+  }
+}
+
+@OptIn(ExperimentalPathApi::class)
+private fun hashByFileContent(descriptor: IdeaPluginDescriptorImpl, hasher: HashStream64) {
+  val isDevMode = AppMode.isDevServer()
+  if (descriptor.useCoreClassLoader) {
+    hasher.putLong(0)
+    return
+  }
+
+  ProgressManager.checkCanceled()
+
+  val files = descriptor.jarFiles!!
+  hasher.putInt(files.size)
+  for (file in files) {
+    if (isDevMode) {
+      val path = file.toString()
+      if (path.endsWith(".jar")) {
+        hashFile(file, hasher, path)
+      }
+      else {
+        // classpath.index cannot be used as it is created not before but during IDE launch
+        // .unmodified must be present
+        try {
+          hashFile(file = file.resolve(".unmodified"), hasher = hasher, path = path)
+        }
+        catch (e: NoSuchFileException) {
+          Logger.getInstance("com.intellij.platform.ide.IdeFingerprint")
+            .warn(".unmodified doesn't exist, cannot compute module fingerprint", e)
+        }
+      }
+    }
+    else {
+      // if the path is not a directory, only "this" file will be visited
+      // if the path is a directory, all the regular files will be visited
+      // note, that symlinks are not followed
+      file.walk().sorted().forEach { f ->
+        // /tmp/byteBuddyAgent12978532094762450051.jar:1261:2023-09-21T12:46:17.196727952Z <= this file is always different :(
+        // see also https://youtrack.jetbrains.com/issue/IJPL-166
+        val absolutePathString = f.toAbsolutePath().toString()
+        if (!absolutePathString.startsWith("/tmp/byteBuddyAgent")) {
+          hashFile(file = f, hasher = hasher, path = absolutePathString)
+        }
+      }
+    }
+  }
+}
+
+private fun hashFile(file: Path, hasher: HashStream64, path: String) {
+  val attributes = Files.readAttributes(file, BasicFileAttributes::class.java, LinkOption.NOFOLLOW_LINKS)
+  hasher.putString(path)
+  hasher.putLong(attributes.size())
+  hasher.putLong(attributes.lastModifiedTime().toMillis())
+  //println(DateTimeFormatter.ISO_LOCAL_TIME.format(attributes.lastModifiedTime().toInstant().atZone(ZoneId.systemDefault())))
 }
