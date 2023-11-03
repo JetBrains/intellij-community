e2a810d5cdf5ecc733cb49f5c96c9ecc5c2f0bcd
Aleksei Kniazev <aleksei.kniazev@jetbrains.com>
2023-11-03 15:33:26 +0100

    [platform] add inline mode for validationTooltip (IDEA-334530)
    GitOrigin-RevId: c7136b88b3e26ef0a4119a4a6bdecd2ab102d7b4

--- platform/platform-impl/src/com/intellij/ui/dsl/builder/components/ValidationTooltip.kt
+++ platform/platform-impl/src/com/intellij/ui/dsl/builder/components/ValidationTooltip.kt
@@ -3,11 +3,14 @@ package com.intellij.ui.dsl.builder.components
 
 
 import com.intellij.icons.AllIcons
+import com.intellij.openapi.observable.properties.AtomicProperty
+import com.intellij.openapi.observable.properties.ObservableMutableProperty
 import com.intellij.ui.JBColor
 import com.intellij.ui.components.ActionLink
 import com.intellij.ui.dsl.builder.Align
 import com.intellij.ui.dsl.builder.Cell
 import com.intellij.ui.dsl.builder.Row
+import com.intellij.ui.dsl.builder.bindText
 import com.intellij.ui.dsl.gridLayout.UnscaledGaps
 import com.intellij.util.ui.JBUI
 import org.jetbrains.annotations.ApiStatus
@@ -53,12 +56,21 @@ private class RoundedLineBorder(color: Color,
   }
 }
 
+fun Row.validationTooltip(@Nls message: String,
+                          firstActionLink: ActionLink? = null,
+                          secondActionLink: ActionLink? = null,
+                          validationType: ValidationType = ValidationType.ERROR,
+                          inline: Boolean = false): Cell<JPanel> {
+return validationTooltip(AtomicProperty(message), firstActionLink, secondActionLink, validationType, inline)
+}
+
 @ApiStatus.Experimental
 fun Row.validationTooltip(
-  validationType: ValidationType,
-  @Nls message: String,
+  textProperty: ObservableMutableProperty<@Nls String>,
   firstActionLink: ActionLink? = null,
   secondActionLink: ActionLink? = null,
+  validationType: ValidationType = ValidationType.ERROR,
+  inline: Boolean = false,
 ) : Cell<JPanel> {
   val hasActionLinks = firstActionLink != null
   val (backgroundColor, foregroundColor, borderColor) = when (validationType) {
@@ -79,25 +91,40 @@ fun Row.validationTooltip(
   }
 
   val customGaps = when {
+    inline -> UnscaledGaps(top = 8, left = 8, bottom = 8, right = 8)
     hasActionLinks -> UnscaledGaps(top = 10, left = 12, bottom = 12, right = 12)
     else -> UnscaledGaps(top = 8, left = 8, bottom = 8, right = 8)
   }
   return cell(com.intellij.ui.dsl.builder.panel {
     row {
       icon(icon).customize(UnscaledGaps(left = customGaps.left,
-                           top = customGaps.top,
-                           bottom = if (hasActionLinks) 4 else customGaps.bottom,
-                           right = 8))
-      label(message)
+                                        top = customGaps.top,
+                                        bottom = if (hasActionLinks && !inline) 4 else customGaps.bottom,
+                                        right = 8))
+      label(textProperty.get())
+        .bindText(textProperty)
         .align(Align.FILL)
         .customize(UnscaledGaps(left = 0,
                                 top = customGaps.top,
-                                bottom = if (hasActionLinks) 4 else customGaps.bottom,
-                                right = customGaps.right)).also {
-        it.component.foreground = foregroundColor
+                                bottom = if (hasActionLinks && !inline) 4 else customGaps.bottom,
+                                right = customGaps.right))
+        .applyToComponent { foreground = foregroundColor }
+        .resizableColumn()
+
+      if (inline && hasActionLinks) {
+        cell(firstActionLink!!).customize(UnscaledGaps(left = 0,
+                                                       top = customGaps.top,
+                                                       bottom = customGaps.bottom,
+                                                       right = if (secondActionLink == null) 12 else customGaps.right))
+        if (secondActionLink != null) {
+          cell(secondActionLink).customize(UnscaledGaps(left = customGaps.left,
+                                                        top = customGaps.top,
+                                                        bottom = customGaps.bottom,
+                                                        right = 12))
+        }
       }
     }
-    if (hasActionLinks) {
+    if (hasActionLinks && !inline) {
       row {
         cell(firstActionLink!!).customize(UnscaledGaps(top = 4,
                                                        left = customGaps.left + 8 + 16, // gap between icon and text + icon width
@@ -112,7 +139,7 @@ fun Row.validationTooltip(
         }
       }
     }
-  }.also {
-    it.border = RoundedLineBorder(borderColor, backgroundColor)
-  }).customize(UnscaledGaps(top = 4, bottom = 4))
+  })
+    .customize(UnscaledGaps(top = 4, bottom = 4))
+    .applyToComponent { border = RoundedLineBorder(borderColor, backgroundColor) }
 }
