69da9c1467fb23a2b96413867ef17171fe13c60d
Igor.Davidenko <igor.davidenko@jetbrains.com>
2023-11-05 11:28:23 +0100

    [full-line] IJ-MR-118508 Remove libraries count feature
    GitOrigin-RevId: abc69007eec9b7f2e11ac51f31ddb446a24c78e0

--- platform/platform-impl/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionUsageTracker.kt
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionUsageTracker.kt
@@ -50,7 +50,6 @@ object InlineCompletionUsageTracker : CounterUsagesCollector() {
       InlineContextFeatures.PREVIOUS_NON_EMPTY_LINE_LENGTH,
       InlineContextFeatures.FOLLOWING_EMPTY_LINES_COUNT,
       InlineContextFeatures.FOLLOWING_NON_EMPTY_LINE_LENGTH,
-      InlineContextFeatures.LIBRARIES_COUNT,
       InlineContextFeatures.FIRST_PARENT,
       InlineContextFeatures.SECOND_PARENT,
       InlineContextFeatures.TIME_SINCE_LAST_TYPING,
