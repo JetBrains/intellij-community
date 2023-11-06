18808332aa9b415382b889ad7154e7a0830b0d07
Tagir Valeev <tagir.valeev@jetbrains.com>
2023-11-06 15:00:28 +0100

    [mod-command] Fix isIntentionPreviewActive() for mod-command preview
    GitOrigin-RevId: 21f42117b379693d18a140ee3452050c08479c8b

--- platform/lang-impl/src/com/intellij/codeInsight/intention/impl/preview/IntentionPreviewComputable.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/intention/impl/preview/IntentionPreviewComputable.kt
@@ -160,9 +160,12 @@ class IntentionPreviewComputable(private val project: Project,
 
   private fun getModActionPreview(origFile: PsiFile, origEditor: Editor): IntentionPreviewInfo {
     val unwrapped = action.asModCommandAction() ?: return IntentionPreviewInfo.EMPTY
-    val info = SideEffectGuard.computeWithoutSideEffects {
+    var info: IntentionPreviewInfo = IntentionPreviewInfo.EMPTY
+    SideEffectGuard.computeWithoutSideEffects {
       val context = ActionContext.from(origEditor, origFile).applyIf(problemOffset >= 0) { withOffset(problemOffset) }
-      unwrapped.generatePreview(context)
+      IntentionPreviewUtils.previewSession(origEditor) {
+        info = unwrapped.generatePreview(context)
+      }
     }
     return convertResult(info, origFile, origFile, false) ?: IntentionPreviewInfo.EMPTY
   }
