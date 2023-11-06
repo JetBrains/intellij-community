70b8c2bd4fa81e3ea751da31440e99d7e3c75724
Aleksandr Izmailov <aleksandr.izmaylov@jetbrains.com>
2023-11-06 13:36:14 +0100

    [http-client] IDEA-337171 IJ-CR-118883: do not check for version, it is always true
    (cherry picked from commit 063ac28d1e39ed8424f33180353dd38ae78fa845)
    Review: IJ-CR-118883
    
    GitOrigin-RevId: c5630f556183a24e63465d4408b649b12758ce77

--- platform/platform-impl/src/com/intellij/openapi/application/migrations/HttpClientPostmanConverter233.kt
+++ platform/platform-impl/src/com/intellij/openapi/application/migrations/HttpClientPostmanConverter233.kt
@@ -2,16 +2,11 @@
 package com.intellij.openapi.application.migrations
 
 import com.intellij.openapi.application.PluginMigrationOptions
-import com.intellij.openapi.util.text.StringUtil
 
 class HttpClientPostmanConverter233 {
   private val POSTMAN_CONVERTER_ID = "com.intellij.restClient.postmanConverter"
 
   fun migratePlugins(options: PluginMigrationOptions) {
-    if (StringUtil.compareVersionNumbers(options.currentProductVersion, "233") >= 0 &&
-        options.pluginsToMigrate.any { it.pluginId.idString == POSTMAN_CONVERTER_ID }
-    ) {
-      options.pluginsToMigrate.removeIf { it.pluginId.idString == POSTMAN_CONVERTER_ID }
-    }
+    options.pluginsToMigrate.removeIf { it.pluginId.idString == POSTMAN_CONVERTER_ID }
   }
 }
