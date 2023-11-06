daa5ece08508961d886cdae861817f57f47e649f
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2023-11-06 18:21:43 +0100

    IDEA-333698 TBE branding icon: Extend API in SettingsEntryPointAction
    GitOrigin-RevId: f1c4f9c7c38052ece247f59cd2892cbe4206556b

--- platform/util/ui/src/com/intellij/util/ui/JBUI.java
+++ platform/util/ui/src/com/intellij/util/ui/JBUI.java
@@ -2265,9 +2265,10 @@ public final class JBUI {
     }
 
     public static final class ManagedIde {
-      public static final Color BADGE_BORDER = JBColor.namedColor("ManagedIdeBadgeBorder", 0x5A5D6B, 0x4E5157);
-      private static final Color BADGE_BACKGROUND = JBColor.namedColor("ManagedIdeBadgeBackground", 0x383A42, 0x393B40);
-      private static final Color BADGE_BACKGROUND_HOVER = JBColor.namedColor("ManagedIdeBadgeBackgroundHover", 0x494B57, 0x43454A);
+      public static final Color BADGE_BORDER = JBColor.namedColor("ManagedIdeBadgeBorder", CustomFrameDecorations.separatorForeground());
+      private static final Color BADGE_BACKGROUND = JBColor.namedColor("ManagedIdeBadgeBackground", CustomFrameDecorations.paneBackground());
+      private static final Color BADGE_BACKGROUND_HOVER = JBColor.namedColor("ManagedIdeBadgeBackgroundHover", JBColor.namedColor(
+        "MainToolbar.Dropdown.transparentHoverBackground", UIManager.getColor("MainToolbar.Icon.hoverBackground")));
       public static final Color MENU_ITEM_HOVER = JBColor.namedColor("ManagedIdeMenuItemHover", 0xEBECF0, 0x43454A);
 
       public static @NotNull Color getBadgeBackground(boolean hover) {
