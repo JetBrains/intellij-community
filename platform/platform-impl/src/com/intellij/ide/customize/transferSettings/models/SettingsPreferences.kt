185e5c452a5907e0760f6e32b397d71079525bf3
Ivan Migalev <ivan.migalev@jetbrains.com>
2023-11-07 18:45:14 +0100

    Settings Transfer: get rid of "other settings"
    GitOrigin-RevId: 516a8b6cc67b3ec7142eed8aecbd3232eb45938d

--- platform/platform-impl/src/com/intellij/ide/customize/transferSettings/models/SettingsPreferences.kt
+++ platform/platform-impl/src/com/intellij/ide/customize/transferSettings/models/SettingsPreferences.kt
@@ -14,7 +14,6 @@ data class SettingsPreferences(
   var laf: Boolean = true,
   var syntaxScheme: Boolean = true,
   var keymap: Boolean = true,
-  var otherSettings: Boolean = true,
   var recentProjects: Boolean = true,
   var plugins: Boolean = true
 ) {
