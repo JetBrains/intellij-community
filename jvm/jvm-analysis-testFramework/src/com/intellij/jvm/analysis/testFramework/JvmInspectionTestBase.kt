d78141974f0ae0062b9536d2d1f1f350d25242e5
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2023-11-07 16:44:12 +0100

    [jvm] Support go to related symbol for method source
    #IDEA-325969 Fixed
    
    GitOrigin-RevId: 7ae47a6e558b484cf397c7192abf66dc6eebdf22

--- jvm/jvm-analysis-testFramework/src/com/intellij/jvm/analysis/testFramework/JvmInspectionTestBase.kt
+++ jvm/jvm-analysis-testFramework/src/com/intellij/jvm/analysis/testFramework/JvmInspectionTestBase.kt
@@ -4,17 +4,12 @@ import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.codeInspection.InspectionProfileEntry
 import com.intellij.codeInspection.InspectionsBundle
 import com.intellij.codeInspection.ex.QuickFixWrapper
-import com.intellij.openapi.application.PathManager
 import com.intellij.pom.java.LanguageLevel
 import com.intellij.testFramework.InspectionTestUtil
 import com.intellij.testFramework.LightProjectDescriptor
 import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
-import java.io.File
 
-abstract class JvmInspectionTestBase : LightJavaCodeInsightFixtureTestCase() {
-  override fun getTestDataPath(): String = PathManager.getCommunityHomePath().replace(File.separatorChar, '/') + basePath
-
+abstract class JvmInspectionTestBase : LightJvmCodeInsightFixtureTestCase() {
   abstract val inspection: InspectionProfileEntry
 
   override fun setUp() {
@@ -159,8 +154,6 @@ abstract class JvmInspectionTestBase : LightJavaCodeInsightFixtureTestCase() {
     configureByFile(file)
     assertEmpty("Quickfix '$hint' is available but should not", myFixture.filterAvailableIntentions(hint))
   }
-
-  protected fun generateFileName() = getTestName(false).replace("[^a-zA-Z0-9\\.\\-]", "_")
 
   override fun tearDown() {
     try {
