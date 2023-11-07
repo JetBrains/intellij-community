7cf14272f79dbcd37f5a56b078f08c3fe98fe0f5
Yann Cébron <yann.cebron@jetbrains.com>
2023-11-07 14:27:35 +0100

    [platform] ExportToXMLAction: cleanup
    GitOrigin-RevId: 034800cca9fd23540c566eafa293ff9a19ec8b45

--- platform/lang-impl/src/com/intellij/codeInspection/ui/actions/ExportToXMLAction.kt
+++ platform/lang-impl/src/com/intellij/codeInspection/ui/actions/ExportToXMLAction.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInspection.ui.actions
 
 import com.intellij.codeInspection.InspectionsBundle
@@ -18,10 +18,57 @@ import java.io.IOException
 import java.nio.file.Path
 import java.util.function.Supplier
 
-@Suppress("ComponentNotRegistered")
-class ExportToXMLAction : InspectionResultsExportActionProvider(Supplier { "XML" },
-                                                                InspectionsBundle.messagePointer("inspection.action.export.xml.description"),
-                                                                AllIcons.FileTypes.Xml) {
+fun dumpToXml(profile: InspectionProfileImpl,
+              tree: InspectionTree,
+              project: Project,
+              globalInspectionContext: GlobalInspectionContextImpl,
+              outputPath: Path) {
+  fun getWrappersForAllScopes(shortName: String,
+                              context: GlobalInspectionContextImpl): Collection<InspectionToolWrapper<*, *>> {
+    return when (val tools = context.tools[shortName]) {
+      null -> emptyList() //dummy entry points tool
+      else -> ContainerUtil.map(tools.tools) { obj: ScopeToolState -> obj.tool }
+    }
+  }
+
+  val singleTool = profile.singleTool
+  val shortName2Wrapper = MultiMap<String, InspectionToolWrapper<*, *>>()
+  if (singleTool != null) {
+    shortName2Wrapper.put(singleTool, getWrappersForAllScopes(singleTool, globalInspectionContext))
+  }
+  else {
+    val model: InspectionTreeModel = tree.inspectionTreeModel
+    model
+      .traverse(model.root)
+      .filter(InspectionNode::class.java)
+      .filter { !it.isExcluded }
+      .map { obj: InspectionNode -> obj.toolWrapper }
+      .forEach { w: InspectionToolWrapper<*, *> -> shortName2Wrapper.putValue(w.shortName, w) }
+  }
+
+  for (entry in shortName2Wrapper.entrySet()) {
+    val shortName: String = entry.key
+    val wrappers: Collection<InspectionToolWrapper<*, *>> = entry.value
+    InspectionsResultUtil.writeInspectionResult(project, shortName, wrappers, outputPath) { wrapper: InspectionToolWrapper<*, *> ->
+      globalInspectionContext.getPresentation(wrapper)
+    }
+  }
+
+  val descriptionsFile: Path = outputPath.resolve(InspectionsResultUtil.DESCRIPTIONS + InspectionsResultUtil.XML_EXTENSION)
+  try {
+    InspectionsResultUtil.describeInspections(descriptionsFile, profile.name, profile)
+  }
+  catch (e: javax.xml.stream.XMLStreamException) {
+    throw IOException(e)
+  }
+
+}
+
+@Suppress("ComponentNotRegistered") // false positive: AnAction inheritor vs EP
+internal class ExportToXMLAction : InspectionResultsExportActionProvider(Supplier { "XML" },
+                                                                         InspectionsBundle.messagePointer(
+                                                                           "inspection.action.export.xml.description"),
+                                                                         AllIcons.FileTypes.Xml) {
   override val progressTitle: String = InspectionsBundle.message("inspection.generating.xml.progress.title")
   override fun writeResults(tree: InspectionTree,
                             profile: InspectionProfileImpl,
@@ -31,46 +78,4 @@ class ExportToXMLAction : InspectionResultsExportActionProvider(Supplier { "XML"
     dumpToXml(profile, tree, project, globalInspectionContext, outputPath)
   }
 
-  companion object {
-    fun dumpToXml(profile: InspectionProfileImpl, tree: InspectionTree, project: Project, globalInspectionContext: GlobalInspectionContextImpl, outputPath: Path) {
-      val singleTool = profile.singleTool
-      val shortName2Wrapper = MultiMap<String, InspectionToolWrapper<*, *>>()
-      if (singleTool != null) {
-        shortName2Wrapper.put(singleTool, getWrappersForAllScopes(singleTool, globalInspectionContext))
-      }
-      else {
-        val model: InspectionTreeModel = tree.inspectionTreeModel
-        model
-          .traverse(model.root)
-          .filter(InspectionNode::class.java)
-          .filter { !it.isExcluded }
-          .map { obj: InspectionNode -> obj.toolWrapper }
-          .forEach { w: InspectionToolWrapper<*, *> -> shortName2Wrapper.putValue(w.shortName, w) }
-      }
-
-      for (entry in shortName2Wrapper.entrySet()) {
-        val shortName: String = entry.key
-        val wrappers: Collection<InspectionToolWrapper<*, *>> = entry.value
-        InspectionsResultUtil.writeInspectionResult(project, shortName, wrappers, outputPath) {
-          wrapper: InspectionToolWrapper<*, *> -> globalInspectionContext.getPresentation(wrapper)
-        }
-      }
-
-      val descriptionsFile: Path = outputPath.resolve(InspectionsResultUtil.DESCRIPTIONS + InspectionsResultUtil.XML_EXTENSION)
-      try {
-        InspectionsResultUtil.describeInspections(descriptionsFile, profile.name, profile)
-      }
-      catch (e: javax.xml.stream.XMLStreamException) {
-        throw IOException(e)
-      }
-    }
-
-    private fun getWrappersForAllScopes(shortName: String,
-                                        context: GlobalInspectionContextImpl): Collection<InspectionToolWrapper<*, *>> {
-      return when (val tools = context.tools[shortName]) {
-        null -> emptyList() //dummy entry points tool
-        else -> ContainerUtil.map(tools.tools) { obj: ScopeToolState -> obj.tool }
-      }
-    }
-  }
 }
