d48788eda92e3437cc7e8e8562fa447c7fdcb878
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-11-07 10:00:14 +0100

    IDEA-337376 convert ProjectViewToolWindowFactory to modern language
    GitOrigin-RevId: 08d4fd6ceeca7f58af406eb00bc438b9c4ec0435

--- platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewToolWindowFactory.kt
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewToolWindowFactory.kt
@@ -1,68 +1,63 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ide.projectView.impl;
+package com.intellij.ide.projectView.impl
 
-import com.intellij.icons.AllIcons;
-import com.intellij.ide.projectView.ProjectView;
-import com.intellij.ide.util.RunOnceUtil;
-import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl;
-import com.intellij.openapi.module.GeneralModuleType;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectUtil;
-import com.intellij.openapi.util.registry.Registry;
-import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.util.ui.tree.TreeUtil;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.icons.AllIcons
+import com.intellij.ide.projectView.ProjectView
+import com.intellij.ide.util.RunOnceUtil.runOnceForProject
+import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl
+import com.intellij.openapi.module.GeneralModuleType
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.isNotificationSilentMode
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.util.ui.tree.TreeUtil
+import org.jetbrains.annotations.VisibleForTesting
+import javax.swing.Icon
+import javax.swing.tree.TreePath
 
-import javax.swing.*;
-
-public final class ProjectViewToolWindowFactory implements ToolWindowFactory, DumbAware {
-  @Override
-  public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-    ((ProjectViewImpl)ProjectView.getInstance(project)).setupImpl(toolWindow);
+@VisibleForTesting
+class ProjectViewToolWindowFactory : ToolWindowFactory, DumbAware {
+  override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+    (ProjectView.getInstance(project) as ProjectViewImpl).setupImpl(toolWindow)
   }
 
-  @Override
-  public @NotNull Icon getIcon() {
-    return AllIcons.Toolwindows.ToolWindowProject;
-  }
+  override val icon: Icon
+    get() = AllIcons.Toolwindows.ToolWindowProject
 
-  @Override
-  public void init(@NotNull ToolWindow window) {
-    if (!Registry.is("ide.open.project.view.on.startup", true)) {
-      return;
+  override fun init(window: ToolWindow) {
+    if (!Registry.`is`("ide.open.project.view.on.startup", true)) {
+      return
     }
 
-    Project project = window.getProject();
-    if (ProjectUtil.isNotificationSilentMode(project)) {
-      return;
+    val project = window.project
+    if (isNotificationSilentMode(project)) {
+      return
     }
 
-    ToolWindowManager manager = ToolWindowManager.getInstance(project);
-    manager.invokeLater(() -> {
-      RunOnceUtil.runOnceForProject(project, "OpenProjectViewOnStart", () -> {
-        if (manager.getActiveToolWindowId() != null ||
-            !Boolean.TRUE.equals(project.getUserData(FileEditorManagerImpl.NOTHING_WAS_OPENED_ON_START))) {
-          return;
+    val manager = ToolWindowManager.getInstance(project)
+    manager.invokeLater {
+      runOnceForProject(project, "OpenProjectViewOnStart") {
+        if (manager.activeToolWindowId != null ||
+            java.lang.Boolean.TRUE != project.getUserData<Boolean>(FileEditorManagerImpl.NOTHING_WAS_OPENED_ON_START)) {
+          return@runOnceForProject
         }
-
-        window.activate(() -> {
-          Module[] modules = ModuleManager.getInstance(project).getModules();
-          if (modules.length == 1 && GeneralModuleType.TYPE_ID.equals(modules[0].getModuleTypeName())) {
-            return;
+        window.activate {
+          val modules = ModuleManager.getInstance(project).modules
+          if (modules.size == 1 && GeneralModuleType.TYPE_ID == modules[0].moduleTypeName) {
+            return@activate
           }
 
-          AbstractProjectViewPane pane = ProjectView.getInstance(project).getCurrentProjectViewPane();
-          JTree tree = pane == null ? null : pane.getTree();
+          val pane = ProjectView.getInstance(project).currentProjectViewPane
+          val tree = pane?.tree
           if (tree != null) {
-            TreeUtil.promiseSelectFirst(tree).onSuccess(tree::expandPath);
+            TreeUtil.promiseSelectFirst(tree).onSuccess { path: TreePath? -> tree.expandPath(path) }
           }
-        });
-      });
-    });
+        }
+      }
+    }
   }
 }
