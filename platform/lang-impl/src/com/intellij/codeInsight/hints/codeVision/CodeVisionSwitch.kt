a4baea50e679318f157d937c6d38701b900b3a5d
Yann Cébron <yann.cebron@jetbrains.com>
2023-11-07 14:10:50 +0100

    [platform] CodeVisionSwitch: cleanup
    GitOrigin-RevId: f3fd4ca4295e5b39512410a45407b0d0c9342ea2

--- platform/lang-impl/src/com/intellij/codeInsight/hints/codeVision/CodeVisionSwitch.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/hints/codeVision/CodeVisionSwitch.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.hints.codeVision
 
 import com.intellij.codeInsight.codeVision.CodeVisionHost
@@ -8,14 +8,7 @@ import com.intellij.codeInsight.daemon.impl.InlayHintsPassFactoryInternal
 import com.intellij.codeInsight.hints.InlayHintsSwitch
 import com.intellij.openapi.project.Project
 
-class CodeVisionSwitch : InlayHintsSwitch {
-  companion object {
-    fun setCodeVisionEnabled(project: Project, value: Boolean) {
-      CodeVisionSettings.instance().codeVisionEnabled = value
-      InlayHintsPassFactoryInternal.restartDaemonUpdatingHints(project)
-      CodeVisionInitializer.getInstance(project).getCodeVisionHost().invalidateProviderSignal.fire(CodeVisionHost.LensInvalidateSignal(null))
-    }
-  }
+internal class CodeVisionSwitch : InlayHintsSwitch {
 
   override fun isEnabled(project: Project): Boolean {
     return CodeVisionSettings.instance().codeVisionEnabled
@@ -23,5 +16,11 @@ class CodeVisionSwitch : InlayHintsSwitch {
 
   override fun setEnabled(project: Project, value: Boolean) {
     setCodeVisionEnabled(project, value)
+  }
+
+  private fun setCodeVisionEnabled(project: Project, value: Boolean) {
+    CodeVisionSettings.instance().codeVisionEnabled = value
+    InlayHintsPassFactoryInternal.restartDaemonUpdatingHints(project)
+    CodeVisionInitializer.getInstance(project).getCodeVisionHost().invalidateProviderSignal.fire(CodeVisionHost.LensInvalidateSignal(null))
   }
 }
