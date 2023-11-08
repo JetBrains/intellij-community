664d2073adc24afd5fde7eb91ce0eb6568ff2430
Anna Kozlova <anna.kozlova@jetbrains.com>
2023-11-08 22:52:59 +0100

    [kotlin] k2: fix quick documentation for libraries
    ^KTIJ-27751 fixed
    
    GitOrigin-RevId: 1b543c2714108562be9c12db738201cea84f44db

--- plugins/kotlin/fir/testData/quickDoc/DecompiledSuper.kt
+++ plugins/kotlin/fir/testData/quickDoc/DecompiledSuper.kt
@@ -4,4 +4,4 @@ fun f() {
   C.val<caret>ues()
 }
 
-//INFO: <div class='definition'><pre>enum class C : List&lt;*&gt;</pre></div><div class='bottom'><icon src="file"/>&nbsp;DecompiledSuper.kt<br/></div>
+//INFO: <div class='definition'><pre>enum class C : List&lt;*&gt;</pre></div><div class='content'><p style='margin-top:0;padding-top:0;'>Returns an array containing the constants of this enum type, in the order they're declared. This method may be used to iterate over the constants.</p></div><table class='sections'></table><div class='bottom'><icon src="file"/>&nbsp;DecompiledSuper.kt<br/></div>
--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/editor/quickDoc/AbstractQuickDocProviderTest.java
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/editor/quickDoc/AbstractQuickDocProviderTest.java
@@ -10,14 +10,14 @@ import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.rt.execution.junit.FileComparisonFailure;
-import com.intellij.testFramework.LightProjectDescriptor;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.kotlin.idea.completion.test.IdeaTestUtilsKt;
 import org.jetbrains.kotlin.idea.test.InTextDirectivesUtils;
 import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase;
-import org.jetbrains.kotlin.idea.test.ProjectDescriptorWithStdlibSources;
+import org.jetbrains.kotlin.idea.test.KotlinLightProjectDescriptor;
+import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor;
 
 import java.io.File;
 import java.util.List;
@@ -110,9 +110,8 @@ public abstract class AbstractQuickDocProviderTest extends KotlinLightCodeInsigh
     }
 
 
-    @NotNull
     @Override
-    protected LightProjectDescriptor getProjectDescriptor() {
-        return ProjectDescriptorWithStdlibSources.getInstanceWithStdlibSources();
+    protected @NotNull KotlinLightProjectDescriptor getProjectDescriptor() {
+        return KotlinWithJdkAndRuntimeLightProjectDescriptor.getInstance();
     }
 }
