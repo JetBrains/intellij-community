b4485b1237ef5d0b7c1d67e7cd6d82acb2e62308
Gregory.Shrago <gregory.shrago@jetbrains.com>
2023-11-09 20:22:45 +0400

    drop unnecessary 'ide.error.reporter.group.reports' logic
    Also fixes IDEA-337655.
    
    (cherry picked from commit b45d039373095e5316b3f2c4e546fa9a03ce7501)
    
    IJ-CR-119429
    
    GitOrigin-RevId: 730893ba74fb3f49fb60a598b4710655a1ad42ff

--- platform/platform-impl/src/com/intellij/diagnostic/MessagePool.java
+++ platform/platform-impl/src/com/intellij/diagnostic/MessagePool.java
@@ -4,26 +4,18 @@ package com.intellij.diagnostic;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Attachment;
 import com.intellij.openapi.diagnostic.IdeaLoggingEvent;
-import com.intellij.openapi.util.registry.Registry;
 import com.intellij.util.SlowOperations;
-import com.intellij.util.concurrency.AppExecutorUtil;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 public final class MessagePool {
   public enum State { NoErrors, ReadErrors, UnreadErrors }
 
   private static final int MAX_POOL_SIZE = 100;
-  private static final int MAX_GROUP_SIZE = 20;
-  private static final int GROUP_TIME_SPAN_MS = 1000;
 
   private static final class MessagePoolHolder {
     private static final MessagePool ourInstance = new MessagePool();
@@ -35,7 +27,6 @@ public final class MessagePool {
 
   private final List<AbstractMessage> myErrors = ContainerUtil.createLockFreeCopyOnWriteList();
   private final List<MessagePoolListener> myListeners = ContainerUtil.createLockFreeCopyOnWriteList();
-  private final MessageGrouper myGrouper = new MessageGrouper();
 
   private MessagePool() { }
 
@@ -49,13 +40,10 @@ public final class MessagePool {
     else if (myErrors.size() == MAX_POOL_SIZE) {
       message = new LogMessage(new TooManyErrorsException(), null, Collections.emptyList());
     }
-    else return;
-    if (Registry.is("ide.error.reporter.group.reports", false)) {
-      myGrouper.addToGroup(message);
-    }
     else {
-      doAddMessage(message);
+      return;
     }
+    doAddMessage(message);
   }
 
   public @NotNull State getState() {
@@ -118,59 +106,6 @@ public final class MessagePool {
     }
     myErrors.add(message);
     notifyEntryAdded();
-  }
-
-  private final class MessageGrouper implements Runnable {
-    private final List<AbstractMessage> myMessages = new ArrayList<>();
-    private Future<?> myAlarm = CompletableFuture.completedFuture(null);
-
-    @Override
-    public void run() {
-      synchronized (myMessages) {
-        if (!myMessages.isEmpty()) {
-          post();
-        }
-      }
-    }
-
-    private void post() {
-      AbstractMessage message = myMessages.size() == 1 ? myMessages.get(0) : groupMessages();
-      myMessages.clear();
-      doAddMessage(message);
-    }
-
-    private AbstractMessage groupMessages() {
-      AbstractMessage first = myMessages.get(0);
-
-      List<Attachment> attachments = new ArrayList<>(first.getAllAttachments());
-      StringBuilder stacktraces = new StringBuilder("Following exceptions happened soon after this one, most probably they are induced.");
-      @SuppressWarnings("SpellCheckingInspection") SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-      for (int i = 1; i < myMessages.size(); i++) {
-        AbstractMessage next = myMessages.get(i);
-        stacktraces.append("\n\n\n").append(format.format(next.getDate())).append('\n');
-        String message = next.getMessage();
-        if (message != null && !message.isBlank()) {
-          stacktraces.append(message).append('\n');
-        }
-        stacktraces.append(next.getThrowableText());
-      }
-      attachments.add(new Attachment("induced.txt", stacktraces.toString()));
-
-      return new LogMessage(first.getThrowable(), first.getMessage(), attachments);
-    }
-
-    private void addToGroup(@NotNull AbstractMessage message) {
-      synchronized (myMessages) {
-        myMessages.add(message);
-        if (myMessages.size() >= MAX_GROUP_SIZE) {
-          post();
-        }
-        else {
-          myAlarm.cancel(false);
-          myAlarm = AppExecutorUtil.getAppScheduledExecutorService().schedule(this, GROUP_TIME_SPAN_MS, TimeUnit.MILLISECONDS);
-        }
-      }
-    }
   }
 
   public static final class TooManyErrorsException extends Exception {
