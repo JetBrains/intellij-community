a2eea6a4b14193a733a016f32cd04f3daba21e96
Ivan Semenov <ivan.semenov@jetbrains.com>
2023-11-10 17:32:29 +0100

    [collab/gitlab] extract base class for review diff file
    GitOrigin-RevId: 93794b43c4048942e92d8949970e0fc3408981bd

---
+++ platform/collaboration-tools/src/com/intellij/collaboration/file/codereview/CodeReviewDiffVirtualFile.kt
@@ -0,0 +1,71 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.collaboration.file.codereview
+
+import com.intellij.diff.editor.DiffFileType
+import com.intellij.diff.editor.DiffVirtualFile
+import com.intellij.diff.tools.combined.CombinedDiffModelBuilder
+import com.intellij.diff.tools.combined.CombinedDiffVirtualFile
+import com.intellij.ide.actions.SplitAction
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.vfs.VirtualFilePathWrapper
+import com.intellij.vcs.editor.ComplexPathVirtualFileSystem
+import org.jetbrains.annotations.ApiStatus
+
+@ApiStatus.Experimental
+abstract class CodeReviewDiffVirtualFile(name: String)
+  : DiffVirtualFile(name),
+    VirtualFilePathWrapper {
+
+  init {
+    @Suppress("LeakingThis")
+    putUserData(SplitAction.FORBID_TAB_SPLIT, true)
+  }
+
+  abstract override fun getPresentableName(): String
+
+  abstract override fun getPath(): String
+  abstract override fun getPresentablePath(): String
+
+  final override fun enforcePresentableName() = true
+
+  abstract override fun isValid(): Boolean
+
+  abstract override fun getFileSystem(): ComplexPathVirtualFileSystem<*>
+  final override fun getFileType(): FileType = DiffFileType.INSTANCE
+
+  final override fun getLength(): Long = 0L
+  final override fun contentsToByteArray(): Nothing = throw UnsupportedOperationException()
+  final override fun getInputStream(): Nothing = throw UnsupportedOperationException()
+  final override fun getOutputStream(requestor: Any?, newModificationStamp: Long, newTimeStamp: Long): Nothing =
+    throw UnsupportedOperationException()
+}
+
+@ApiStatus.Experimental
+abstract class CodeReviewCombinedDiffVirtualFile(sourceId: String, name: String)
+  : CombinedDiffVirtualFile(sourceId, name),
+    CombinedDiffModelBuilder,
+    VirtualFilePathWrapper {
+
+  init {
+    @Suppress("LeakingThis")
+    putUserData(SplitAction.FORBID_TAB_SPLIT, true)
+  }
+
+  abstract override fun getPresentableName(): String
+
+  abstract override fun getPath(): String
+  abstract override fun getPresentablePath(): String
+
+  final override fun enforcePresentableName() = true
+
+  abstract override fun isValid(): Boolean
+
+  abstract override fun getFileSystem(): ComplexPathVirtualFileSystem<*>
+  final override fun getFileType(): FileType = DiffFileType.INSTANCE
+
+  final override fun getLength(): Long = 0L
+  final override fun contentsToByteArray(): Nothing = throw UnsupportedOperationException()
+  final override fun getInputStream(): Nothing = throw UnsupportedOperationException()
+  final override fun getOutputStream(requestor: Any?, newModificationStamp: Long, newTimeStamp: Long): Nothing =
+    throw UnsupportedOperationException()
+}
--- plugins/gitlab/src/org/jetbrains/plugins/gitlab/mergerequest/file/GitLabMergeRequestDiffFile.kt
+++ plugins/gitlab/src/org/jetbrains/plugins/gitlab/mergerequest/file/GitLabMergeRequestDiffFile.kt
@@ -1,72 +1,105 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gitlab.mergerequest.file
 
-import com.intellij.diff.editor.DiffVirtualFile
+import com.intellij.collaboration.file.codereview.CodeReviewCombinedDiffVirtualFile
+import com.intellij.collaboration.file.codereview.CodeReviewDiffVirtualFile
+import com.intellij.collaboration.ui.codereview.diff.CodeReviewDiffHandlerHelper
 import com.intellij.diff.impl.DiffRequestProcessor
-import com.intellij.ide.actions.SplitAction
+import com.intellij.diff.tools.combined.CombinedDiffModelImpl
+import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
-import com.intellij.openapi.fileTypes.FileType
-import com.intellij.openapi.fileTypes.FileTypes
+import com.intellij.openapi.components.serviceIfCreated
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFilePathWrapper
-import com.intellij.openapi.vfs.VirtualFileSystem
+import com.intellij.vcs.editor.ComplexPathVirtualFileSystem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import org.jetbrains.plugins.gitlab.api.GitLabProjectCoordinates
+import org.jetbrains.plugins.gitlab.mergerequest.diff.GitLabMergeRequestDiffViewModel
+import org.jetbrains.plugins.gitlab.mergerequest.ui.toolwindow.model.GitLabToolWindowViewModel
 import org.jetbrains.plugins.gitlab.util.GitLabBundle
 
-class GitLabMergeRequestDiffFile(override val connectionId: String,
-                                 private val project: Project,
-                                 private val glProject: GitLabProjectCoordinates,
-                                 val mergeRequestIid: String)
-  : DiffVirtualFile(GitLabBundle.message("merge.request.diff.file.name", mergeRequestIid)),
+internal data class GitLabMergeRequestDiffFile(
+  override val connectionId: String,
+  private val project: Project,
+  private val glProject: GitLabProjectCoordinates,
+  private val mergeRequestIid: String
+) : CodeReviewDiffVirtualFile(getFileName(mergeRequestIid)),
     VirtualFilePathWrapper,
     GitLabVirtualFile {
 
-  init {
-    putUserData(SplitAction.FORBID_TAB_SPLIT, true)
-  }
+  override fun getFileSystem(): ComplexPathVirtualFileSystem<*> = GitLabVirtualFileSystem.getInstance()
 
-  override fun enforcePresentableName() = true
+  override fun getPath(): String = (fileSystem as GitLabVirtualFileSystem).getPath(connectionId, project, glProject, mergeRequestIid, true)
+  override fun getPresentablePath(): String = getPresentablePath(glProject, mergeRequestIid)
+  override fun getPresentableName(): String = GitLabBundle.message("merge.request.diff.file.name", mergeRequestIid)
 
-  override fun isValid(): Boolean = GitLabMergeRequestDiffService.isDiffFileValid(project, connectionId)
-
-  override fun getPath(): String =
-    (fileSystem as GitLabVirtualFileSystem).getPath(connectionId, project, glProject, mergeRequestIid, true)
-
-  override fun getPresentablePath(): String = presentablePath(glProject, mergeRequestIid)
+  override fun isValid(): Boolean = isFileValid(project, connectionId)
 
   override fun createProcessor(project: Project): DiffRequestProcessor =
     project.service<GitLabMergeRequestDiffService>().createDiffRequestProcessor(connectionId, mergeRequestIid)
-
-  override fun getFileSystem(): VirtualFileSystem = GitLabVirtualFileSystem.getInstance()
-  override fun getFileType(): FileType = FileTypes.UNKNOWN
-
-  override fun getLength() = 0L
-  override fun contentsToByteArray() = throw UnsupportedOperationException()
-  override fun getInputStream() = throw UnsupportedOperationException()
-  override fun getOutputStream(requestor: Any?, newModificationStamp: Long, newTimeStamp: Long) = throw UnsupportedOperationException()
-
-  override fun equals(other: Any?): Boolean {
-    if (this === other) return true
-    if (other !is GitLabMergeRequestDiffFile) return false
-
-    if (connectionId != other.connectionId) return false
-    if (project != other.project) return false
-    if (glProject != other.glProject) return false
-    return mergeRequestIid == other.mergeRequestIid
-  }
-
-  override fun hashCode(): Int {
-    var result = connectionId.hashCode()
-    result = 31 * result + project.hashCode()
-    result = 31 * result + glProject.hashCode()
-    result = 31 * result + mergeRequestIid.hashCode()
-    return result
-  }
-
-  override fun toString(): String =
-    "GitLabMergeRequestDiffFile(connectionId='$connectionId', project=$project, glProject=$glProject, mergeRequestId=$mergeRequestIid)"
 }
 
+internal data class GitLabMergeRequestCombinedDiffFile(
+  override val connectionId: String,
+  private val project: Project,
+  private val glProject: GitLabProjectCoordinates,
+  private val mergeRequestIid: String
+) : CodeReviewCombinedDiffVirtualFile(createSourceId(connectionId, glProject, mergeRequestIid), getFileName(mergeRequestIid)),
+    VirtualFilePathWrapper,
+    GitLabVirtualFile {
 
-internal fun presentablePath(glProject: GitLabProjectCoordinates, mergeRequestIid: String): String =
+  override fun getFileSystem(): ComplexPathVirtualFileSystem<*> = GitLabVirtualFileSystem.getInstance()
+
+  override fun getPath(): String = (fileSystem as GitLabVirtualFileSystem).getPath(connectionId, project, glProject, mergeRequestIid, true)
+  override fun getPresentablePath(): String = getPresentablePath(glProject, mergeRequestIid)
+  override fun getPresentableName(): String = GitLabBundle.message("merge.request.diff.file.name", mergeRequestIid)
+
+  override fun isValid(): Boolean = isFileValid(project, connectionId)
+
+  override fun createModel(id: String): CombinedDiffModelImpl =
+    project.service<GitLabMergeRequestDiffService>().createGitLabCombinedDiffModel(connectionId, mergeRequestIid)
+}
+
+private fun createSourceId(connectionId: String, glProject: GitLabProjectCoordinates, mergeRequestIid: String) =
+  "GitLabMergeRequest:$connectionId:$glProject/$mergeRequestIid"
+
+private fun getFileName(mergeRequestIid: String): String = "$mergeRequestIid.diff"
+
+private fun getPresentablePath(glProject: GitLabProjectCoordinates, mergeRequestIid: String): String =
   "$glProject/mergerequests/${mergeRequestIid}.diff"
+
+private fun isFileValid(project: Project, connectionId: String): Boolean =
+  project.serviceIfCreated<GitLabToolWindowViewModel>()?.projectVm?.value.takeIf { it?.connectionId == connectionId } != null
+
+@Service(Service.Level.PROJECT)
+private class GitLabMergeRequestDiffService(private val project: Project, parentCs: CoroutineScope) {
+  private val base = CodeReviewDiffHandlerHelper(project, parentCs)
+
+  fun createDiffRequestProcessor(connectionId: String, mergeRequestIid: String): DiffRequestProcessor {
+    val vm = findDiffVm(project, connectionId, mergeRequestIid)
+    return base.createDiffRequestProcessor(vm, GitLabMergeRequestDiffViewModel.KEY)
+  }
+
+  fun createGitLabCombinedDiffModel(connectionId: String, mergeRequestIid: String): CombinedDiffModelImpl {
+    val vm = findDiffVm(project, connectionId, mergeRequestIid)
+    return base.createCombinedDiffModel(vm, GitLabMergeRequestDiffViewModel.KEY)
+  }
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+private fun findDiffVm(project: Project, connectionId: String, mergeRequestIid: String): Flow<GitLabMergeRequestDiffViewModel?> {
+  val projectVm = project.serviceIfCreated<GitLabToolWindowViewModel>()?.projectVm ?: return flowOf(null)
+  return projectVm.flatMapLatest {
+    if (it != null && it.connectionId == connectionId) {
+      it.getDiffViewModel(mergeRequestIid).map { it.getOrNull() }
+    }
+    else {
+      flowOf(null)
+    }
+  }
+}
