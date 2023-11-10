db5c251333eea513b41d4f646ca516c0382249a3
Andrey.Matveev <andrey.matveev@jetbrains.com>
2023-11-10 15:29:12 +0200

    PY-63273 Fix tests
    GitOrigin-RevId: 6c26cc44b2ab1080cb3c1d61b7a7053622ce0e51

--- python/testData/inspections/PyTypeCheckerInspection/InitializingTypeVar.py
+++ python/testData/inspections/PyTypeCheckerInspection/InitializingTypeVar.py
@@ -4,4 +4,4 @@ TypeVar("T", int, str, bound=int, covariant=True, contravariant=True)
 TypeVar("T", int, str, bound='int', covariant=True, contravariant=True)
 TypeVar("T", int, 'str', bound=int, covariant=True, contravariant=True)
 TypeVar("T", 'int', 'str', bound=int, covariant=True, contravariant=True)
-TypeVar<warning descr="Unexpected type(s):(LiteralString, int, int, int, int, int)Possible type(s):(str, Any | None, bool, bool, Any, Any)(str, Any | None, bool, bool, Any, Any)" textAttributesKey="WARNING_ATTRIBUTES">("T", 0, 1, bound=2, covariant=3, contravariant=4)</warning>
+TypeVar<warning descr="Unexpected type(s):(str, int, int, int, int, int)Possible type(s):(str, Any | None, bool, bool, Any, Any)(str, Any | None, bool, bool, Any, Any)" textAttributesKey="WARNING_ATTRIBUTES">("T", 0, 1, bound=2, covariant=3, contravariant=4)</warning>
