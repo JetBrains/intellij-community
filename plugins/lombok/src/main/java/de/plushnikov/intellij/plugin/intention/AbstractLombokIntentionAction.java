d568544d5a08a1393b04379fa0f16158f98e65b0
Michail Plushnikov <michail.plushnikov@jetbrains.com>
2023-11-13 21:16:20 +0100

    [lombok] IDEA-326806 intensions should be handled similar to inspections, check module scope for lombok library presence
    GitOrigin-RevId: c62cbcaa095563c7b618c7c7a70553fddccb3873

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/intention/AbstractLombokIntentionAction.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/intention/AbstractLombokIntentionAction.java
@@ -2,8 +2,9 @@ package de.plushnikov.intellij.plugin.intention;
 
 import com.intellij.modcommand.ActionContext;
 import com.intellij.modcommand.Presentation;
-import com.intellij.modcommand.PsiBasedModCommandAction;
 import com.intellij.modcommand.PsiUpdateModCommandAction;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.psi.PsiElement;
 import de.plushnikov.intellij.plugin.util.LombokLibraryUtil;
 import org.jetbrains.annotations.NotNull;
@@ -17,7 +18,8 @@ public abstract class AbstractLombokIntentionAction extends PsiUpdateModCommandA
 
   @Override
   protected @Nullable Presentation getPresentation(@NotNull ActionContext context, @NotNull PsiElement element) {
-    if (!LombokLibraryUtil.hasLombokLibrary(context.project())) return null;
+    final Module module = ModuleUtilCore.findModuleForFile(element.getContainingFile());
+    if (!LombokLibraryUtil.hasLombokClasses(module)) return null;
     return super.getPresentation(context, element);
   }
 }
