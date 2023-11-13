9e279b127eebcb2d6551766012ffd387194b40e9
Eugene Zhuravlev <eugene.zhuravlev@jetbrains.com>
2023-11-13 13:35:57 +0100

    introduce 'platformProductCode' attribute
    (cherry picked from commit 9d58b4e0f34f1dd7615d38976e64b8560c037bf7)
    
    IJ-CR-119743
    
    GitOrigin-RevId: 726065a70e5331e6d6863f2970c417ef286536b7

--- platform/platform-impl/src/com/intellij/ui/LicensingFacade.java
+++ platform/platform-impl/src/com/intellij/ui/LicensingFacade.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 public final class LicensingFacade {
+  public String platformProductCode;
   public String licensedTo;
   public @NlsSafe String licenseeEmail;
   public List<String> restrictions;
