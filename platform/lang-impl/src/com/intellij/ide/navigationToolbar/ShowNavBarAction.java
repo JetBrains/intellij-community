a13ae7f4a9d75704358121eb9833b1209738460f
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-11-14 17:41:36 +0100

    IJPL-140 inline `ide.navBar.v2` to `true`
    GitOrigin-RevId: 4b254dc9d99e97c24ee609f596a5e67589a044a4

--- platform/lang-impl/src/com/intellij/ide/navigationToolbar/ShowNavBarAction.java
+++ platform/lang-impl/src/com/intellij/ide/navigationToolbar/ShowNavBarAction.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.navigationToolbar;
 
-import com.intellij.ide.IdeEventQueue;
 import com.intellij.ide.navbar.ide.NavBarIdeUtil;
 import com.intellij.ide.navbar.ide.NavBarService;
 import com.intellij.ide.ui.NavBarLocation;
@@ -9,13 +8,9 @@ import com.intellij.ide.ui.UISettings;
 import com.intellij.idea.ActionsBundle;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification;
-import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
-import com.intellij.ui.ComponentUtil;
 import org.jetbrains.annotations.NotNull;
-
-import java.awt.*;
 
 /**
  * @author Konstantin Bulenkov
@@ -32,31 +27,14 @@ final class ShowNavBarAction extends AnAction implements DumbAware, PopupAction,
   }
 
   private static void showNavBar(@NotNull AnActionEvent e, @NotNull Project project) {
-    final DataContext context = e.getDataContext();
     UISettings uiSettings = UISettings.getInstance();
     if (NavBarIdeUtil.isNavbarShown(uiSettings)
         && (uiSettings.getShowStatusBar() || uiSettings.getNavBarLocation() != NavBarLocation.BOTTOM)) {
       SelectInNavBarTarget.selectInNavBar(true);
     }
     else {
-      if (NavBarIdeUtil.isNavbarV2Enabled()) {
-        NavBarService.getInstance(project).showFloatingNavbar(e.getDataContext());
-        return;
-      }
-      final Component component = PlatformCoreDataKeys.CONTEXT_COMPONENT.getData(context);
-      if (!isInsideNavBar(component)) {
-        IdeEventQueue.getInstance().getPopupManager().closeAllPopups(false);
-        final Editor editor = CommonDataKeys.EDITOR.getData(context);
-        final NavBarPanel toolbarPanel = new NavBarPanel(project, false);
-        toolbarPanel.showHint(editor, context);
-      }
+      NavBarService.getInstance(project).showFloatingNavbar(e.getDataContext());
     }
-  }
-
-  private static boolean isInsideNavBar(Component c) {
-    return c == null
-           || c instanceof NavBarPanel
-           || ComponentUtil.getParentOfType(NavBarListWrapper.class, c) != null;
   }
 
   @Override
