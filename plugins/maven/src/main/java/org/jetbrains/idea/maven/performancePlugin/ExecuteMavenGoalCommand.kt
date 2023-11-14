4f713b56c12019f4d5a3ad4367e7fbab66517a5c
Nikita Barkov <nikita.barkov@jetbrains.com>
2023-11-14 18:50:51 +0100

    [perform_tests]AT-31. Updated ExecuteMavenGoalCommand. Implemented test for maven. Updated guava project.
    GitOrigin-RevId: fd49a8afce65bbd028aecfd53bea26651253accb

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/performancePlugin/ExecuteMavenGoalCommand.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/performancePlugin/ExecuteMavenGoalCommand.kt
@@ -2,10 +2,8 @@ package org.jetbrains.idea.maven.performancePlugin
 
 import com.intellij.execution.ExecutionListener
 import com.intellij.execution.ExecutionManager
-import com.intellij.execution.process.ProcessAdapter
-import com.intellij.execution.process.ProcessEvent
+import com.intellij.execution.process.ProcessHandler
 import com.intellij.execution.runners.ExecutionEnvironment
-import com.intellij.execution.runners.ProgramRunner
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.playback.PlaybackContext
@@ -29,9 +27,9 @@ class ExecuteMavenGoalCommand(text: String, line: Int) : AbstractCommand(text, l
 
   override fun _execute(context: PlaybackContext): Promise<Any?> {
     val promise = AsyncPromise<Any?>()
-    val split = extractCommandArgument(PREFIX).split(" ")
-    val moduleName = split[0]
-    val goal = split[1]
+    val args = extractCommandArgument(PREFIX)
+    val moduleName = "(?<=moduleName)(.*)(?=goalName)".toRegex().find(args)!!.value.trim()
+    val goal = args.substringAfter("goalName").trim()
     val project = context.getProject()
     project.messageBus.connect().subscribe(ExecutionManager.EXECUTION_TOPIC, object : ExecutionListener {
       override fun processNotStarted(executorId: String, env: ExecutionEnvironment, cause: Throwable?) {
@@ -39,25 +37,27 @@ class ExecuteMavenGoalCommand(text: String, line: Int) : AbstractCommand(text, l
           promise.setError(cause)
         }
       }
+
+      override fun processTerminated(executorId: String, env: ExecutionEnvironment, handler: ProcessHandler, exitCode: Int) {
+        if (exitCode != 0) {
+          promise.setError("Process finished with code exit code $exitCode")
+        }
+        else {
+          promise.setResult(null)
+        }
+      }
     })
 
-    perform(project, moduleName, goal) { descriptor ->
-      descriptor?.processHandler?.addProcessListener(
-        object : ProcessAdapter() {
-          override fun processTerminated(event: ProcessEvent) {
-            promise.setResult(null)
-          }
-        })
-    }
+    perform(project, moduleName, goal)
     return promise
   }
 
-  private fun perform(project: Project, moduleName: String, goal: String, callback: ProgramRunner.Callback) {
+  private fun perform(project: Project, moduleName: String, goal: String) {
     ApplicationManager.getApplication().invokeLater {
       val projectsManager = MavenProjectsManager.getInstance(project)
       if (projectsManager == null) return@invokeLater
 
-      val mavenProject = projectsManager.projects.first { it.displayName.equals(moduleName) }
+      val mavenProject = projectsManager.projects.firstOrNull { it.displayName.equals(moduleName) }
       if (mavenProject == null) return@invokeLater
 
       val explicitProfiles = projectsManager.getExplicitProfiles()
@@ -68,7 +68,7 @@ class ExecuteMavenGoalCommand(text: String, line: Int) : AbstractCommand(text, l
                                          listOf(goal),
                                          explicitProfiles.enabledProfiles,
                                          explicitProfiles.disabledProfiles)
-      MavenRunConfigurationType.runConfiguration(project, params, callback)
+      MavenRunConfigurationType.runConfiguration(project, params, null)
     }
   }
 }
