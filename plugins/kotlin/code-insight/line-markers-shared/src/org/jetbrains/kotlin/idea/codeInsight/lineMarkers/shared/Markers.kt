d4b8eb59c5754812036354a54b158d0d3087452a
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-11-14 12:51:25 +0100

    [Kotlin] Move 'KtDeclaration.findAllActualForExpect' into 'kotlin.searching.k2'
    GitOrigin-RevId: 1cdad3a34ec403b9bdcddf568484db450ca087fd

--- plugins/kotlin/code-insight/line-markers-shared/src/org/jetbrains/kotlin/idea/codeInsight/lineMarkers/shared/Markers.kt
+++ plugins/kotlin/code-insight/line-markers-shared/src/org/jetbrains/kotlin/idea/codeInsight/lineMarkers/shared/Markers.kt
@@ -3,17 +3,13 @@ package org.jetbrains.kotlin.idea.codeInsight.lineMarkers.shared
 
 import com.intellij.codeInsight.navigation.impl.PsiTargetPresentationRenderer
 import com.intellij.icons.AllIcons
-import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.roots.libraries.Library
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.SmartPsiElementPointer
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.search.SearchScope
 import com.intellij.psi.util.parentOfType
 import com.intellij.refactoring.suggested.createSmartPointer
-import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.Nls
 import org.jetbrains.kotlin.analysis.api.analyze
@@ -24,8 +20,7 @@ import org.jetbrains.kotlin.idea.base.projectStructure.moduleInfo
 import org.jetbrains.kotlin.idea.base.projectStructure.moduleInfo.LibraryInfo
 import org.jetbrains.kotlin.idea.base.projectStructure.moduleInfo.LibrarySourceInfo
 import org.jetbrains.kotlin.idea.base.projectStructure.moduleInfo.ModuleSourceInfo
-import org.jetbrains.kotlin.idea.stubindex.*
-import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.idea.searching.kmp.findAllActualForExpect
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.containingClass
 import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
@@ -244,65 +239,7 @@ internal fun KtDeclaration.expectedDeclarationIfAny(): List<SmartPsiElementPoint
     }
 }
 
-@RequiresBackgroundThread(generateAssertion = false)
-internal fun KtDeclaration.findAllExpectForActual(searchScope: SearchScope = runReadAction { useScope }): Sequence<SmartPsiElementPointer<KtDeclaration>> {
-    val declaration = this
-    val scope = searchScope as? GlobalSearchScope ?: return emptySequence()
-    val containingClassOrObjectOrSelf = containingClassOrObjectOrSelf()
-    // covers cases like classes, class functions and class properties
-    containingClassOrObjectOrSelf?.fqName?.let { fqName ->
-        val fqNameAsString = fqName.asString()
-        val targetDeclarations: List<KtDeclaration> = KotlinFullClassNameIndex.getAllElements(fqNameAsString, project, scope, filter = {
-            it.matchesWithActual(containingClassOrObjectOrSelf)
-        }) + KotlinTopLevelTypeAliasFqNameIndex.getAllElements(fqNameAsString, project, scope, filter = {
-            it.matchesWithActual(containingClassOrObjectOrSelf)
-        })
-
-        return targetDeclarations.asSequence().mapNotNull { targetDeclaration ->
-            when (declaration) {
-                is KtClassOrObject -> targetDeclaration
-                is KtNamedDeclaration ->
-                    when (targetDeclaration) {
-                        is KtClassOrObject -> targetDeclaration.declarations.firstOrNull {
-                            it is KtNamedDeclaration && it.name == declaration.name && it.matchesWithActual(declaration)
-                        }
-                        else -> null
-                    }
-
-                else -> null
-            }?.createSmartPointer()
-        }
-    }
-    // top level functions
-    val packageFqName = declaration.containingKtFile.packageFqName
-    val name = declaration.name ?: return emptySequence()
-    val topLevelFqName = packageFqName.child(Name.identifier(name!!)).asString()
-    return when (declaration) {
-        is KtNamedFunction -> {
-            KotlinTopLevelFunctionFqnNameIndex.getAllElements(topLevelFqName, project, scope) {
-                it.matchesWithActual(declaration)
-            }.asSequence().map(KtNamedFunction::createSmartPointer)
-        }
-
-        is KtProperty -> {
-            KotlinTopLevelPropertyFqnNameIndex.getAllElements(topLevelFqName, project, scope) {
-                it.matchesWithActual(declaration)
-            }.asSequence().map(KtProperty::createSmartPointer)
-        }
-
-        else -> emptySequence()
-    }
-}
-
-private fun KtDeclaration.matchesWithActual(actualDeclaration: KtDeclaration): Boolean {
-    val declaration = this
-    return declaration.hasActualModifier() && analyze(declaration) {
-        val symbol: KtDeclarationSymbol = declaration.getSymbol()
-        return symbol.getExpectsForActual().any { it.psi == actualDeclaration }
-    }
-}
-
 fun KtDeclaration.allNavigatableActualDeclarations(): Collection<SmartPsiElementPointer<KtDeclaration>> =
-    findAllExpectForActual().toSet() + findMarkerBoundDeclarations().flatMap { it.findAllExpectForActual() }
+    findAllActualForExpect().toSet() + findMarkerBoundDeclarations().flatMap { it.findAllActualForExpect() }
 
 fun KtElement.containingClassOrObjectOrSelf(): KtClassOrObject? = parentOfType(withSelf = true)
