789c2e9214b1deacb53d35c309377d5897eaba2b
Ivan Semenov <ivan.semenov@jetbrains.com>
2023-11-17 12:18:42 +0100

    [github] extract timeline editor creation to service
    GitOrigin-RevId: c4b52decd9c929d5cda645a23a684a43738cdc89

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/GHPREditorProvider.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/GHPREditorProvider.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.pullrequest
 
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorPolicy
 import com.intellij.openapi.fileEditor.FileEditorProvider
@@ -13,10 +15,23 @@ import org.jetbrains.plugins.github.pullrequest.data.GHPRDataContextRepository
 internal class GHPREditorProvider : FileEditorProvider, DumbAware {
 
   override fun accept(project: Project, file: VirtualFile): Boolean =
-    file is GHPRTimelineVirtualFile && GHPRDataContextRepository.getInstance(project).findContext(file.repository) != null
+    file is GHPRTimelineVirtualFile && project.service<GHPREditorProviderService>().canCreateTimelineEditor(file)
 
   override fun createEditor(project: Project, file: VirtualFile): FileEditor {
     file as GHPRTimelineVirtualFile
+    return project.service<GHPREditorProviderService>().createTimelineEditor(file)
+  }
+
+  override fun getEditorTypeId(): String = "GHPR"
+  override fun getPolicy(): FileEditorPolicy = FileEditorPolicy.HIDE_DEFAULT_EDITOR
+}
+
+@Service(Service.Level.PROJECT)
+private class GHPREditorProviderService(private val project: Project) {
+  fun canCreateTimelineEditor(file: GHPRTimelineVirtualFile): Boolean =
+    GHPRDataContextRepository.getInstance(project).findContext(file.repository) != null
+
+  fun createTimelineEditor(file: GHPRTimelineVirtualFile): FileEditor {
     val dataContext = GHPRDataContextRepository.getInstance(project).findContext(file.repository)!!
 
     val dataDisposable = Disposer.newDisposable()
@@ -26,7 +41,4 @@ internal class GHPREditorProvider : FileEditorProvider, DumbAware {
       Disposer.register(it, dataDisposable)
     }
   }
-
-  override fun getEditorTypeId(): String = "GHPR"
-  override fun getPolicy(): FileEditorPolicy = FileEditorPolicy.HIDE_DEFAULT_EDITOR
 }
