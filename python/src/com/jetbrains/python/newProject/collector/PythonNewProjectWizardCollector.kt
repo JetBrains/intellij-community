74468e510721572bc1e09b4c28900f02ab5b77aa
Aleksei Kniazev <aleksei.kniazev@jetbrains.com>
2023-11-17 19:25:52 +0000

    [python] added interpreter creation mode to python.new.project.wizard group (PY-64142)
    (cherry picked from commit f08588647f6d28fd5a9fab885ed148f2105e1d28)
    
    IJ-CR-119664
    
    GitOrigin-RevId: 4d255258ea56c58edc7c024fbcad4fed6269cc0d

--- python/src/com/jetbrains/python/newProject/collector/PythonNewProjectWizardCollector.kt
+++ python/src/com/jetbrains/python/newProject/collector/PythonNewProjectWizardCollector.kt
@@ -13,7 +13,7 @@ object PythonNewProjectWizardCollector : CounterUsagesCollector() {
     return GROUP
   }
 
-  private val GROUP = EventLogGroup("python.new.project.wizard", 3)
+  private val GROUP = EventLogGroup("python.new.project.wizard", 4)
   private val INHERIT_GLOBAL_SITE_PACKAGE_FIELD = EventFields.Boolean("inherit_global_site_package")
   private val MAKE_AVAILABLE_TO_ALL_PROJECTS = EventFields.Boolean("make_available_to_all_projects")
   private val PREVIOUSLY_CONFIGURED = EventFields.Boolean("previously_configured")
@@ -22,6 +22,7 @@ object PythonNewProjectWizardCollector : CounterUsagesCollector() {
   private val PROJECT_GENERATED_EVENT = GROUP.registerVarargEvent("project.generated",
                                                                   INTERPRETER_TYPE,
                                                                   EXECUTION_TYPE,
+                                                                  INTERPRETER_CREATION_MODE,
                                                                   PYTHON_VERSION,
                                                                   GENERATOR_FIELD,
                                                                   INHERIT_GLOBAL_SITE_PACKAGE_FIELD,
