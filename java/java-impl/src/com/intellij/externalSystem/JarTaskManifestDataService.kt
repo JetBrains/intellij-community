c0150a09cac6fcd85089b640a42df397aa94d9e4
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-11-20 22:03:51 +0100

    [gradle] IDEA-308190 introduce experimental WorkspaceDataService
    Unlike ProjectDataService, it uses Workspace Model API instead of IdeModifiableModelsProvider
    
    GitOrigin-RevId: f2d965df10b7d87457b3b9c33059949c9c8b25e2

--- java/java-impl/src/com/intellij/externalSystem/JarTaskManifestDataService.kt
+++ java/java-impl/src/com/intellij/externalSystem/JarTaskManifestDataService.kt
@@ -9,15 +9,14 @@ import com.intellij.openapi.externalSystem.model.Key
 import com.intellij.openapi.externalSystem.model.ProjectKeys
 import com.intellij.openapi.externalSystem.model.project.ModuleData
 import com.intellij.openapi.externalSystem.model.project.ProjectData
-import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
-import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProviderImpl
 import com.intellij.openapi.externalSystem.service.project.manage.AbstractModuleDataService
-import com.intellij.openapi.externalSystem.service.project.manage.AbstractProjectDataService
+import com.intellij.openapi.externalSystem.service.project.manage.WorkspaceDataService
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.platform.workspace.jps.entities.ModuleId
+import com.intellij.platform.workspace.storage.MutableEntityStorage
 
-internal class JarTaskManifestDataService : AbstractProjectDataService<JarTaskManifestData, Project?>() {
+internal class JarTaskManifestDataService : WorkspaceDataService<JarTaskManifestData> {
   private val GRADLE_MAIN_SUFFIX = ".main"
   override fun getTargetDataKey(): Key<JarTaskManifestData> = JarTaskManifestData.KEY
 
@@ -25,37 +24,31 @@ internal class JarTaskManifestDataService : AbstractProjectDataService<JarTaskMa
     toImport: Collection<DataNode<JarTaskManifestData>>,
     projectData: ProjectData?,
     project: Project,
-    modelsProvider: IdeModifiableModelsProvider
+    mutableStorage: MutableEntityStorage
   ) {
-    if (projectData == null) return
     for (jarTaskManifestNode in toImport) {
       val moduleNode = jarTaskManifestNode.getParent(ModuleData::class.java) ?: continue
       val module = moduleNode.getUserData(AbstractModuleDataService.MODULE_KEY) ?: continue
       val manifestAttributes = jarTaskManifestNode.data.manifestAttributes
 
-      importAutomaticModuleName(module, manifestAttributes, modelsProvider)
+      importManifestAttributes(module, manifestAttributes, mutableStorage)
     }
   }
 
-  private fun importAutomaticModuleName(module: Module, manifestAttributes: Map<String, String>, modelsProvider: IdeModifiableModelsProvider) {
-    // TODO: fix broken encapsulation
-    if (modelsProvider !is IdeModifiableModelsProviderImpl) return
-
-    val diff = modelsProvider.actualStorageBuilder
-
+  private fun importManifestAttributes(module: Module, manifestAttributes: Map<String, String>, mutableStorage: MutableEntityStorage) {
     val moduleName = module.name + GRADLE_MAIN_SUFFIX
 
-    val moduleEntity = diff.resolve(ModuleId(moduleName)) ?: return
+    val moduleEntity = mutableStorage.resolve(ModuleId(moduleName)) ?: return
     val javaSettings = moduleEntity.javaSettings
     if (javaSettings != null) {
-      diff.modifyEntity(javaSettings) {
+      mutableStorage.modifyEntity(javaSettings) {
         this.manifestAttributes = manifestAttributes
       }
     }
     else {
-      diff addEntity JavaModuleSettingsEntity(inheritedCompilerOutput = true,
-                                              excludeOutput = true,
-                                              entitySource = moduleEntity.entitySource) {
+      mutableStorage addEntity JavaModuleSettingsEntity(inheritedCompilerOutput = true,
+                                                        excludeOutput = true,
+                                                        entitySource = moduleEntity.entitySource) {
         this.manifestAttributes = manifestAttributes
         this.module = moduleEntity
       }
