a86f50ebab28ea0a7aa3c9a85ae26ac9b3aaccc0
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2023-11-20 16:28:36 +0100

    [refactoring] IDEA-336540: mapped-file based implementation of IndexingStamp Enumerator
    + mapped-file based enumerators are likely more reliable, less chance of corruption in case of IDE crash/improper shutdown
    
    GitOrigin-RevId: aec6f3ad197b7511645532d9fd667dd4398a16c9

--- platform/lang-impl/src/com/intellij/util/indexing/PersistentIndexingStampEnumerator.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/PersistentIndexingStampEnumerator.kt
@@ -1,8 +1,14 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.indexing
 
+import com.intellij.openapi.vfs.newvfs.persistent.dev.appendonlylog.AppendOnlyLogFactory
+import com.intellij.openapi.vfs.newvfs.persistent.dev.enumerator.DurableEnumeratorFactory
+import com.intellij.util.io.DurableDataEnumerator
+import com.intellij.util.io.IOUtil
 import com.intellij.util.io.KeyDescriptor
 import com.intellij.util.io.PersistentEnumerator
+import com.intellij.util.io.dev.appendonlylog.AppendOnlyLog
+import com.intellij.util.io.dev.enumerator.KeyDescriptorEx
 import java.io.ByteArrayOutputStream
 import java.io.DataInput
 import java.io.DataOutput
@@ -10,6 +16,24 @@ import java.io.DataOutputStream
 import java.nio.ByteBuffer
 import java.nio.file.Path
 
+
+
+class PersistentTimestampsEnumerator(path: Path) :
+  PersistentEnumerator<TimestampsImmutable>(path, TimestampsKeyDescriptor(), 1024, null, 1) {
+}
+
+class DurableTimestampEnumerator(path: Path)
+  : DurableDataEnumerator<TimestampsImmutable> by DurableEnumeratorFactory
+  .defaultWithInMemoryMap(TimestampsKeyDescriptorEx())
+  .valuesLogFactory(
+    AppendOnlyLogFactory.withDefaults()
+      .pageSize(256 * IOUtil.KiB) //smaller page size: we expect just (100s..1000s) records in the enumerator
+  )
+  .open(path) {
+
+}
+
+/** Descriptor for [PersistentEnumerator] */
 class TimestampsKeyDescriptor : KeyDescriptor<TimestampsImmutable> {
   override fun isEqual(val1: TimestampsImmutable, val2: TimestampsImmutable): Boolean {
     return val1 == val2
@@ -32,9 +56,31 @@ class TimestampsKeyDescriptor : KeyDescriptor<TimestampsImmutable> {
     dataIn.readFully(data)
     return TimestampsImmutable.readTimestamps(ByteBuffer.wrap(data))
   }
-
 }
 
-class PersistentTimestampsEnumerator(path: Path) :
-  PersistentEnumerator<TimestampsImmutable>(path, TimestampsKeyDescriptor(), 1024, null, 1) {
+/** Descriptor for [com.intellij.openapi.vfs.newvfs.persistent.dev.enumerator.DurableEnumerator] */
+class TimestampsKeyDescriptorEx : KeyDescriptorEx<TimestampsImmutable> {
+
+  override fun areEqual(timestamp1: TimestampsImmutable, timestamp2: TimestampsImmutable): Boolean {
+    return timestamp1 == timestamp2
+  }
+
+  override fun hashCodeOf(timestamp: TimestampsImmutable): Int {
+    return timestamp.hashCode()
+  }
+
+  //RC: beware of binary format difference between this and TimestampsKeyDescriptor:
+  //    here we don't prefix record with (int32) size, because append-only-log
+  //    already keeps the record size internally
+
+  override fun read(input: ByteBuffer): TimestampsImmutable {
+    return TimestampsImmutable.readTimestamps(input)
+  }
+
+  override fun saveToLog(timestamps: TimestampsImmutable,
+                         log: AppendOnlyLog): Long {
+    val outStream = ByteArrayOutputStream(64)
+    DataOutputStream(outStream).use { timestamps.writeToStream(it) }
+    return log.append(outStream.toByteArray())
+  }
 }
