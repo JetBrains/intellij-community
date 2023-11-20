59c6c558295aa8bf8757c64dd6d8a46f85516ea6
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-11-20 16:09:21 +0100

    [maven] IDEA-308190 test populate Automatic-Module-Name during import
    GitOrigin-RevId: 655bba518acf31c00c2422a86ba1fef4a516ac8e

---
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenJarManifestImportingTest.kt
@@ -0,0 +1,42 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.idea.maven.importing
+
+import com.intellij.java.workspace.entities.javaSettings
+import com.intellij.maven.testFramework.MavenMultiVersionImportingTestCase
+import com.intellij.platform.backend.workspace.WorkspaceModel
+import com.intellij.platform.workspace.jps.entities.ModuleId
+import kotlinx.coroutines.runBlocking
+import org.junit.Assume.assumeTrue
+import org.junit.Test
+
+class MavenJarManifestImportingTest : MavenMultiVersionImportingTestCase() {
+  override fun runInDispatchThread() = false
+
+  @Test
+  fun `test jar manifest automatic module name`() = runBlocking {
+    assumeTrue(isWorkspaceImport)
+    importProjectAsync("""
+                    <groupId>test</groupId>
+                    <artifactId>project</artifactId>
+                    <version>1</version>
+                    <build>
+                      <plugins>
+                        <plugin>
+                          <groupId>org.apache.maven.plugins</groupId>
+                          <artifactId>maven-jar-plugin</artifactId>
+                          <configuration>
+                            <archive>
+                              <manifestEntries>
+                                <Automatic-Module-Name>my.module.name</Automatic-Module-Name>
+                              </manifestEntries>
+                            </archive>
+                          </configuration>
+                        </plugin>
+                      </plugins>
+                    </build>
+                    """.trimIndent())
+
+    val automaticModuleName = WorkspaceModel.getInstance(myProject).currentSnapshot.resolve(ModuleId("project"))?.javaSettings?.automaticModuleName
+    assertEquals("my.module.name", automaticModuleName)
+  }
+}
