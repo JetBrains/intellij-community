7fb0647556fbfb102212aadc99994a3912de045a
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2023-11-21 17:43:32 +0100

    IDEA-333683 [Project|Trust] cleanup: localized trusted project checks in their services
    GitOrigin-RevId: 9229c66524fbad3aa388b584d287024c2d683f00

--- platform/platform-impl/src/com/intellij/ide/trustedProjects/TrustedProjects.kt
+++ platform/platform-impl/src/com/intellij/ide/trustedProjects/TrustedProjects.kt
@@ -3,7 +3,6 @@ package com.intellij.ide.trustedProjects
 
 import com.intellij.ide.impl.TrustedPaths
 import com.intellij.ide.impl.TrustedPathsSettings
-import com.intellij.ide.impl.TrustedProjectsStatistics
 import com.intellij.ide.lightEdit.LightEdit
 import com.intellij.ide.trustedProjects.TrustedProjectsLocator.LocatedProject
 import com.intellij.openapi.application.ApplicationManager
@@ -18,36 +17,30 @@ internal object TrustedProjects {
   }
 
   fun getProjectTrustedState(locatedProject: LocatedProject): ThreeState {
-    val explicitTrustedState = getExplicitTrustedProjectState(locatedProject)
+    val explicitTrustedState = TrustedPaths.getInstance().getProjectTrustedState(locatedProject)
     if (explicitTrustedState != ThreeState.UNSURE) {
       return explicitTrustedState
     }
-    return getImplicitTrustedProjectState(locatedProject)
+    if (isTrustedCheckDisabledForProduct()) {
+      return ThreeState.YES
+    }
+    if (LightEdit.owns(locatedProject.project)) {
+      return ThreeState.YES
+    }
+    return TrustedPathsSettings.getInstance().getProjectTrustedState(locatedProject)
   }
 
   fun setProjectTrusted(locatedProject: LocatedProject, isTrusted: Boolean) {
-    val oldState = getExplicitTrustedProjectState(locatedProject)
-    setExplicitTrustedProjectState(locatedProject, isTrusted)
-    val newState = getExplicitTrustedProjectState(locatedProject)
+    val trustedPaths = TrustedPaths.getInstance()
+    val oldState = trustedPaths.getProjectTrustedState(locatedProject)
+    trustedPaths.setProjectTrustedState(locatedProject, isTrusted)
+    val newState = trustedPaths.getProjectTrustedState(locatedProject)
     val syncPublisher = ApplicationManager.getApplication().messageBus.syncPublisher(TrustedProjectsListener.TOPIC)
     if (oldState != newState) {
       when (isTrusted) {
         true -> syncPublisher.onProjectTrusted(locatedProject)
         else -> syncPublisher.onProjectUntrusted(locatedProject)
       }
-    }
-  }
-
-  private fun getExplicitTrustedProjectState(locatedProject: LocatedProject): ThreeState {
-    val trustedPaths = TrustedPaths.getInstance()
-    val trustedStates = locatedProject.projectRoots.map { trustedPaths.getProjectPathTrustedState(it) }
-    return mergeTrustedProjectStates(trustedStates)
-  }
-
-  private fun setExplicitTrustedProjectState(locatedProject: LocatedProject, isTrusted: Boolean) {
-    val trustedPaths = TrustedPaths.getInstance()
-    for (projectRoot in locatedProject.projectRoots) {
-      trustedPaths.setProjectPathTrusted(projectRoot, isTrusted)
     }
   }
 
@@ -62,31 +55,4 @@ internal object TrustedProjects {
   private fun isTrustedCheckDisabledForProduct(): Boolean =
     isTrustedCheckDisabled() ||
     java.lang.Boolean.getBoolean("idea.trust.disabled")
-
-  private fun getImplicitTrustedProjectState(locatedProject: LocatedProject?): ThreeState {
-    if (isTrustedCheckDisabledForProduct()) {
-      return ThreeState.YES
-    }
-    if (LightEdit.owns(locatedProject?.project)) {
-      return ThreeState.YES
-    }
-    val trustedPaths = TrustedPathsSettings.getInstance()
-    if (locatedProject != null && locatedProject.projectRoots.all { trustedPaths.isPathTrusted(it) }) {
-      TrustedProjectsStatistics.PROJECT_IMPLICITLY_TRUSTED_BY_PATH.log(locatedProject.project)
-      return ThreeState.YES
-    }
-    return ThreeState.UNSURE
-  }
-
-  private fun mergeTrustedProjectStates(states: List<ThreeState>): ThreeState {
-    return states.fold(ThreeState.YES) { acc, it ->
-      when {
-        acc == ThreeState.UNSURE -> ThreeState.UNSURE
-        it == ThreeState.UNSURE -> ThreeState.UNSURE
-        acc == ThreeState.NO -> ThreeState.NO
-        it == ThreeState.NO -> ThreeState.NO
-        else -> ThreeState.YES
-      }
-    }
-  }
 }
