17abc6fe1c74b390c018437de1099b5b075b023c
Evgeny Abramov <evgeny.abramov@jetbrains.com>
2023-11-21 17:55:01 +0200

    ML in SE: reformat the code of SemanticActionSearchEverywhereContributor
    GitOrigin-RevId: e7ad1a2e8b43247b042cd6ce8c0793a14aa55e63

--- plugins/search-everywhere-ml/semantics/src/com/intellij/searchEverywhereMl/semantics/contributors/SemanticActionSearchEverywhereContributor.kt
+++ plugins/search-everywhere-ml/semantics/src/com/intellij/searchEverywhereMl/semantics/contributors/SemanticActionSearchEverywhereContributor.kt
@@ -95,58 +95,58 @@ class SemanticActionSearchEverywhereContributor(defaultContributor: ActionSearch
                                          progressIndicator: ProgressIndicator,
                                          consumer: Processor<in FoundItemDescriptor<MatchedValue>>): Unit = runBlockingCancellable {
     runUpdateSessionForActionSearch(model.updateSession) { presentationProvider ->
-    val knownItems = mutableListOf<FoundItemDescriptor<MatchedValue>>()
+      val knownItems = mutableListOf<FoundItemDescriptor<MatchedValue>>()
 
-    val mutex = Mutex()
-    val readyChannel = Channel<Unit>()
-    val standardContributorFinishedChannel = Channel<Unit>()
+      val mutex = Mutex()
+      val readyChannel = Channel<Unit>()
+      val standardContributorFinishedChannel = Channel<Unit>()
 
-    val itemsProvider = SemanticSearchSettings.getInstance().run {
-      if (getUseRemoteActionsServer()) ServerSemanticActionsProvider(model, presentationProvider)
-      else LocalSemanticActionsProvider(model, presentationProvider)
-    }
-    itemsProvider.includeDisabledActions = myDisabledActions
-    val searchStart = System.nanoTime()
-    launch {
-      var foundItemsCount = 0
-      val cachedDescriptors = mutableListOf<FoundItemDescriptor<MatchedValue>>()
+      val itemsProvider = SemanticSearchSettings.getInstance().run {
+        if (getUseRemoteActionsServer()) ServerSemanticActionsProvider(model, presentationProvider)
+        else LocalSemanticActionsProvider(model, presentationProvider)
+      }
+      itemsProvider.includeDisabledActions = myDisabledActions
+      val searchStart = System.nanoTime()
+      launch {
+        var foundItemsCount = 0
+        val cachedDescriptors = mutableListOf<FoundItemDescriptor<MatchedValue>>()
 
-      val semanticMatches = itemsProvider.streamSearchIfEnabled(pattern, priorityThresholds[DescriptorPriority.LOW]).toList()
-      standardContributorFinishedChannel.receiveCatching()
-      for (priority in ORDERED_PRIORITIES) {
-        val iterator = if (priority == DescriptorPriority.HIGH) semanticMatches.iterator()
-        else cachedDescriptors.filter { it.findPriority() == priority }.iterator()
+        val semanticMatches = itemsProvider.streamSearchIfEnabled(pattern, priorityThresholds[DescriptorPriority.LOW]).toList()
+        standardContributorFinishedChannel.receiveCatching()
+        for (priority in ORDERED_PRIORITIES) {
+          val iterator = if (priority == DescriptorPriority.HIGH) semanticMatches.iterator()
+          else cachedDescriptors.filter { it.findPriority() == priority }.iterator()
 
-        while (iterator.hasNext()) {
-          ensureActive()
-          val descriptor = iterator.next()
-          if (priority == DescriptorPriority.HIGH && descriptor.findPriority() != priority) {
-            cachedDescriptors.add(descriptor)
-            continue
+          while (iterator.hasNext()) {
+            ensureActive()
+            val descriptor = iterator.next()
+            if (priority == DescriptorPriority.HIGH && descriptor.findPriority() != priority) {
+              cachedDescriptors.add(descriptor)
+              continue
+            }
+
+            val prepareDescriptor = prepareSemanticDescriptor(descriptor, knownItems, TimeoutUtil.getDurationMillis(searchStart))
+            mutex.withLock { prepareDescriptor() }?.let {
+              consumer.process(it)
+              foundItemsCount++
+            }
+            if (priority != DescriptorPriority.HIGH && foundItemsCount >= desiredResultsCount) break
           }
-
-          val prepareDescriptor = prepareSemanticDescriptor(descriptor, knownItems, TimeoutUtil.getDurationMillis(searchStart))
-          mutex.withLock { prepareDescriptor() }?.let {
-            consumer.process(it)
-            foundItemsCount++
-          }
-          if (priority != DescriptorPriority.HIGH && foundItemsCount >= desiredResultsCount) break
+          if (progressIndicator.isCanceled || foundItemsCount >= desiredResultsCount) break
         }
-        if (progressIndicator.isCanceled || foundItemsCount >= desiredResultsCount) break
+        readyChannel.close()
       }
-      readyChannel.close()
-    }
 
-    coroutineToIndicator {
-      defaultFetchElements(pattern, progressIndicator) {
-        val prepareDescriptor = prepareStandardDescriptor(it, knownItems)
-        val descriptor = runBlockingCancellable { mutex.withLock { prepareDescriptor() } }
-        consumer.process(descriptor)
+      coroutineToIndicator {
+        defaultFetchElements(pattern, progressIndicator) {
+          val prepareDescriptor = prepareStandardDescriptor(it, knownItems)
+          val descriptor = runBlockingCancellable { mutex.withLock { prepareDescriptor() } }
+          consumer.process(descriptor)
+        }
       }
+      standardContributorFinishedChannel.close()
+      readyChannel.receiveCatching()
     }
-    standardContributorFinishedChannel.close()
-    readyChannel.receiveCatching()
-  }
   }
 
   private fun mergeOrSkipAction(newItem: FoundItemDescriptor<MatchedValue>,
