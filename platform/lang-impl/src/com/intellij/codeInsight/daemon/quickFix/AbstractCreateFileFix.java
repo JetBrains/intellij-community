c4d82d295fe90adceafb30b29ea07ea6cc8aafc9
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-11-22 17:34:02 +0100

    [platform] EA-943597 IDEA-338814 Fix CreateFilePathFix opening files under write action
    GitOrigin-RevId: d66cb4d4a204897de7f841f30b5f50e4cb3f433e

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/quickFix/AbstractCreateFileFix.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/quickFix/AbstractCreateFileFix.java
@@ -8,7 +8,7 @@ import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo;
 import com.intellij.codeInspection.LocalQuickFixAndIntentionActionOnPsiElement;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectFileIndex;
@@ -28,15 +28,13 @@ import com.intellij.ui.SimpleListCellRenderer;
 import com.intellij.util.IconUtil;
 import com.intellij.util.IncorrectOperationException;
 import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.PropertyKey;
+import org.jetbrains.annotations.*;
 
 import javax.swing.*;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.function.Supplier;
 
 import static com.intellij.openapi.project.ProjectUtilCore.displayUrlRelativeToProject;
 import static com.intellij.openapi.util.io.FileUtil.toSystemDependentName;
@@ -116,7 +114,7 @@ public abstract class AbstractCreateFileFix extends LocalQuickFixAndIntentionAct
         }
 
         if (editor == null || ApplicationManager.getApplication().isUnitTestMode()) {
-          // run on first item of sorted list in batch mode
+          // run on the first item of a sorted list in batch mode
           apply(myStartElement.getProject(), directories.get(0), editor);
         }
         else {
@@ -129,35 +127,57 @@ public abstract class AbstractCreateFileFix extends LocalQuickFixAndIntentionAct
   private void apply(@NotNull Project project, @NotNull TargetDirectory directory, @Nullable Editor editor) {
     myIsAvailableTimeStamp = 0; // to revalidate applicability
 
-    PsiDirectory currentDirectory = directory.getDirectory();
-    if (currentDirectory == null) {
-      return;
-    }
-
     try {
-      for (String pathPart : directory.getPathToCreate()) {
-        currentDirectory = findOrCreateSubdirectory(currentDirectory, pathPart);
-      }
-      for (String pathPart : mySubPath) {
-        currentDirectory = findOrCreateSubdirectory(currentDirectory, pathPart);
-      }
-      if (currentDirectory == null) {
-        if (editor != null) {
-          HintManager hintManager = HintManager.getInstance();
-          hintManager.showErrorHint(editor, CodeInsightBundle.message("create.file.incorrect.path.hint", myNewFileName));
+      Supplier<PsiDirectory> toDirectory = () -> {
+        PsiDirectory currentDirectory = directory.getDirectory();
+        if (currentDirectory == null) {
+          return null;
         }
-        return;
-      }
 
-      apply(project, currentDirectory, editor);
+        for (String pathPart : directory.getPathToCreate()) {
+          currentDirectory = findOrCreateSubdirectory(currentDirectory, pathPart);
+        }
+
+        for (String pathPart : mySubPath) {
+          currentDirectory = findOrCreateSubdirectory(currentDirectory, pathPart);
+        }
+
+        if (currentDirectory == null) {
+          if (editor != null) {
+            HintManager hintManager = HintManager.getInstance();
+            hintManager.showErrorHint(editor, CodeInsightBundle.message("create.file.incorrect.path.hint", myNewFileName));
+          }
+          return null;
+        }
+        return currentDirectory;
+      };
+
+      apply(project, toDirectory, editor);
     }
     catch (IncorrectOperationException e) {
       myIsAvailable = false;
     }
   }
 
-  protected abstract void apply(@NotNull Project project, @NotNull PsiDirectory targetDirectory, @Nullable Editor editor)
-    throws IncorrectOperationException;
+  /**
+   * @deprecated override {@link #apply(Project, Supplier, Editor)} instead
+   */
+  @SuppressWarnings("unused")
+  @Deprecated(forRemoval = true)
+  protected void apply(@NotNull Project project, @NotNull PsiDirectory targetDirectory, @Nullable Editor editor)
+    throws IncorrectOperationException {
+  }
+
+  protected void apply(@NotNull Project project, @NotNull Supplier<? extends @Nullable PsiDirectory> targetDirectory, @Nullable Editor editor)
+    throws IncorrectOperationException {
+
+    // only for compatibility with 3-rd party plugins, not used
+    PsiDirectory directory = WriteAction.compute(() -> targetDirectory.get());
+    if (directory != null) {
+      // for compatibility with plugins
+      apply(project, directory, editor);
+    }
+  }
 
   protected @Nullable HtmlChunk getDescription(@NotNull Icon itemIcon) {
     Path filePath;
@@ -172,7 +192,8 @@ public abstract class AbstractCreateFileFix extends LocalQuickFixAndIntentionAct
           filePath = filePath.resolve(component);
         }
         filePath = filePath.resolve(myNewFileName);
-      } else {
+      }
+      else {
         filePath = Path.of("", mySubPath).resolve(myNewFileName);
       }
     }
@@ -239,12 +260,7 @@ public abstract class AbstractCreateFileFix extends LocalQuickFixAndIntentionAct
       .setRequestFocus(true)
       .setRenderer(renderer)
       .setNamerForFiltering(item -> item.getPresentablePath())
-      .setItemChosenCallback(chosenValue -> {
-
-        WriteCommandAction.writeCommandAction(project)
-          .withName(CodeInsightBundle.message(myKey, myNewFileName))
-          .run(() -> apply(project, chosenValue.getTarget(), editor));
-      })
+      .setItemChosenCallback(chosenValue -> apply(project, chosenValue.getTarget(), editor))
       .addListener(new JBPopupListener() {
         @Override
         public void onClosed(@NotNull LightweightWindowEvent event) {
