9f7af88e5121836cf8ecda52a20101da580ebeed
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2023-11-22 18:39:59 +0200

    [kotlin] Update JS test data
    KTIJ-27566
    
    GitOrigin-RevId: b616137d0f47ba58abfa3127a7900072babfdcb8

--- plugins/kotlin/idea/tests/testData/quickfix/autoImports/libraryTopLevelFunctionImportJsRuntime.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/autoImports/libraryTopLevelFunctionImportJsRuntime.kt
@@ -7,4 +7,4 @@ fun testFun() {
 }
 
 // FUS_QUICKFIX_NAME: org.jetbrains.kotlin.idea.quickfix.ImportFix
-/* IGNORE_K2 */
+// FUS_K2_QUICKFIX_NAME: org.jetbrains.kotlin.idea.quickfix.importFix.ImportQuickFix
