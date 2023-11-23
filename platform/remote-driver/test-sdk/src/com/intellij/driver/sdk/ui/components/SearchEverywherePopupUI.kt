2004e95a0f85b837274bea0ed442a139d182f91b
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2023-11-23 12:37:00 +0100

    IDEA-332876 UI tests: extract common functions to sdk
    GitOrigin-RevId: 382108561b82520608797c80c3d67b1d4adb29f5

---
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/SearchEverywherePopupUI.kt
@@ -0,0 +1,23 @@
+package com.intellij.driver.sdk.ui.components
+
+import com.intellij.driver.sdk.ui.Finder
+import com.intellij.driver.sdk.ui.should
+import org.intellij.lang.annotations.Language
+
+
+fun Finder.searchEverywherePopup(@Language("xpath") xpath: String? = null) = x(xpath ?: "//div[@class='HeavyWeightWindow']",
+                                                                               SearchEverywherePopupUI::class.java)
+
+class SearchEverywherePopupUI(data: ComponentData): PopupUiComponent(data) {
+  private val searchField = x("//div[@class='SearchField']")
+  private val resultsList = x("//div[@class='JBList']")
+
+  fun searchAndChooseFirst(text: String) {
+    searchField.click()
+    keyboard {
+      enterText(text)
+      resultsList.should(15) { hasText(text) }
+      enter()
+    }
+  }
+}
