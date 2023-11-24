7a80e0fbe752a1047e0b8927afafa615142f76c2
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2023-11-24 10:26:32 +0100

    IDEA-333683 [Platform|Trust] new: added test for project trust checks with thousands of roots
    (cherry picked from commit 45067f779f4364b4697e15fdc6e66bd8f8d4ba47)
    
    IJ-CR-120854
    
    GitOrigin-RevId: 2bc9a05a03a2a7e97f57f2647843c662feed4b0a

--- platform/platform-tests/testSrc/com/intellij/ide/impl/TrustedProjectsHeavyTest.kt
+++ platform/platform-tests/testSrc/com/intellij/ide/impl/TrustedProjectsHeavyTest.kt
@@ -1,26 +1,33 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.impl
 
+import com.intellij.ide.trustedProjects.TrustedProjects
 import com.intellij.ide.trustedProjects.TrustedProjectsLocator
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.modules
+import com.intellij.openapi.project.rootManager
+import com.intellij.openapi.util.io.NioPathPrefixTreeFactory
 import com.intellij.testFramework.useProjectAsync
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import java.nio.file.Path
 import kotlin.time.Duration.Companion.milliseconds
 
 class TrustedProjectsHeavyTest : TrustedProjectsHeavyTestCase() {
 
   @ParameterizedTest
   @CsvSource(
-    "1,      1,      1,      1000,     100",
-    "1,      1,      5000,   1000,     100",
-    "1,      5000,   1,      1000,     100",
-    "5000,   1,      1,      1000,     100",
-    "5,      100,    10,     1000,     100"
+    "1,      1,       1000,    100",
+    "1,      5000,    1000,    100",
+    "10,     1000,    1000,    100",
+    "100,    100,     1000,    100",
+    "1000,   10,      1000,    100",
+    "5000,   1,       1000,    100",
   )
-  fun `test performance of collecting project roots`(
-    numProjects: Int,
+  fun `test performance of locating roots of JPS project`(
     numModules: Int,
     numContentRoots: Int,
     numExecutions: Int,
@@ -29,22 +36,88 @@ class TrustedProjectsHeavyTest : TrustedProjectsHeavyTestCase() {
     runBlocking {
       createProjectAsync("project")
         .useProjectAsync { project ->
-          testPerformance("Generate modules x${numProjects}x${numModules}x$numContentRoots") {
-            generateProjectAsync(project, numProjects, numModules, numContentRoots)
+          testPerformance("Generate modules ${numModules}*$numContentRoots") {
+            generateProjectAsync(project, numModules, numContentRoots)
           }
           testPerformance("Collect project roots x$numExecutions", maxDuration.milliseconds) {
             repeat(numExecutions) {
-              Assertions.assertTrue(project.isTrusted())
+              val locatedProject = TrustedProjectsLocator.locateProject(project)
+              Assertions.assertEquals(1, locatedProject.projectRoots.size)
             }
           }
         }
     }
   }
 
+  @ParameterizedTest
+  @CsvSource(
+    "1,       1,        10000,    1000,    100",
+    "1,       10000,    10000,    1000,    100",
+    "5,       2000,     10000,    1000,    100",
+    "10,      1000,     10000,    1000,    100",
+    "100,     100,      10000,    100,     100",
+    "1000,    10,       10000,    10,      100",
+    "10000,   1,        10000,    1,       100",
+  )
+  fun `test performance of checking trusted state of roots`(
+    numProjects: Int,
+    numModules: Int,
+    numOtherProjects: Int,
+    numExecutions: Int,
+    maxDuration: Int
+  ) {
+    runBlocking {
+      val otherProjectRoots = testPerformance("Generate $numOtherProjects other project roots") {
+        generatePaths("other-project", numOtherProjects)
+      }
+      testPerformance("Set trusted state for other-project with ${otherProjectRoots.size} roots") {
+        TrustedPaths.getInstance().state = TrustedPaths.State(otherProjectRoots.associate { it.toString() to true })
+      }
+    }
+    runBlocking {
+      val projectRoots = testPerformance("Generate $numProjects project roots") {
+        generatePaths("project", numProjects)
+      }
+      val moduleRoots = testPerformance("Generate $numProjects*$numModules module roots") {
+        generatePaths(projectRoots, "module", numModules)
+      }
+      registerProjectLocator(object : TrustedProjectsLocator {
+        override fun getProjectRoots(project: Project): List<Path> = projectRoots
+        override fun getProjectRoots(projectRoot: Path, project: Project?): List<Path> = projectRoots
+      })
+      registerProjectLocator(object : TrustedProjectsLocator {
+        override fun getProjectRoots(project: Project): List<Path> = moduleRoots
+        override fun getProjectRoots(projectRoot: Path, project: Project?): List<Path> = moduleRoots
+      })
+      val locatedProject = testPerformance("Locate project with ${projectRoots.size} roots") {
+        TrustedProjectsLocator.locateProject(projectRoots.first(), null)
+      }
+      testPerformance("Set trusted state for project with ${projectRoots.size} roots") {
+        TrustedProjects.setProjectTrusted(locatedProject, true)
+      }
+      testPerformance("Check trusted state $numExecutions times", maxDuration.milliseconds) {
+        repeat(numExecutions) {
+          Assertions.assertTrue(TrustedProjects.isProjectTrusted(locatedProject))
+        }
+      }
+    }
+  }
+
   @Test
   fun `test collecting project roots`() {
-    TrustedProjectsLocator.EP_NAME.point.registerExtension(TestTrustedProjectsLocator(), testDisposable)
     runBlocking {
+      registerProjectLocator(object : TrustedProjectsLocator {
+        override fun getProjectRoots(projectRoot: Path, project: Project?): List<Path> = emptyList()
+        override fun getProjectRoots(project: Project): List<Path> {
+          val index = NioPathPrefixTreeFactory.createSet()
+          for (module in project.modules) {
+            for (contentRoot in module.rootManager.contentRoots) {
+              index.add(contentRoot.toNioPath())
+            }
+          }
+          return index.getRoots().toList()
+        }
+      })
       createProjectAsync("project")
         .useProjectAsync {
           assertProjectRoots(it, "project")
