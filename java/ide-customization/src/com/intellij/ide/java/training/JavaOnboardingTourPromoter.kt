c9ef3bcc4ddcbff2675c7885c466bd5cf864f509
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2023-11-24 11:07:20 +0200

    IFT-525 Use single ID for Java and Kotlin onboarding lessons
    Then, if one of these lessons is passed, the other will also be marked as passed.
    Also, implement lesson passed state migration, so the Java onboarding lesson will stay passed after ID renaming, if it was passed before.
    
    GitOrigin-RevId: 20b949355b5492c8626d764a4a60da9dcd974800

--- java/ide-customization/src/com/intellij/ide/java/training/JavaOnboardingTourPromoter.kt
+++ java/ide-customization/src/com/intellij/ide/java/training/JavaOnboardingTourPromoter.kt
@@ -2,13 +2,14 @@
 package com.intellij.ide.java.training
 
 import com.intellij.java.ift.JavaLessonsBundle
+import com.intellij.java.ift.lesson.essential.ideaOnboardingLessonId
 import com.intellij.openapi.util.IconLoader
 import com.intellij.util.PlatformUtils
 import training.ui.welcomeScreen.OnboardingLessonPromoter
 import javax.swing.Icon
 
 class JavaOnboardingTourPromoter : OnboardingLessonPromoter(
-  "java.onboarding", "JAVA", JavaLessonsBundle.message("java.onboarding.lesson.name")
+  ideaOnboardingLessonId, "JAVA", JavaLessonsBundle.message("java.onboarding.lesson.name")
 ) {
   override val promoImage: Icon
     get() = IconLoader.getIcon("img/idea-onboarding-tour.png", JavaOnboardingTourPromoter::class.java.classLoader)
