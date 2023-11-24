e4b5ebd6b1e9b52b269b483dffa71a9fbd8c5a22
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2023-11-24 12:38:49 +0100

    IDEA-333683 [Platform|Trust] fix: index all locations in trusted state storages
    The evaluation complexity of trusted check in indexed storage is O(N) instead of O(N*M) where N is the number of project roots and M is the number of trusted locations.
    
    (cherry picked from commit fac4ff1883df361a8dd5d931c84f2ed0c54d2791)
    
    IJ-CR-120854
    
    GitOrigin-RevId: c35fc8b1919676736dec67f75e89924e0729012b

--- platform/platform-impl/src/com/intellij/ide/impl/TrustedPaths.kt
+++ platform/platform-impl/src/com/intellij/ide/impl/TrustedPaths.kt
@@ -1,11 +1,12 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-@file:Suppress("ReplacePutWithAssignment")
-
 package com.intellij.ide.impl
 
 import com.intellij.ide.trustedProjects.TrustedProjectsLocator.LocatedProject
+import com.intellij.ide.trustedProjects.TrustedProjectsStateStorage
 import com.intellij.openapi.components.*
+import com.intellij.openapi.util.io.NioPathPrefixTreeFactory
 import com.intellij.util.ThreeState
+import com.intellij.util.containers.prefix.map.PrefixTreeMap
 import com.intellij.util.xmlb.annotations.OptionTag
 import org.jetbrains.annotations.ApiStatus
 import java.nio.file.Path
@@ -13,7 +14,8 @@ import java.nio.file.Path
 @ApiStatus.Internal
 @State(name = "Trusted.Paths", storages = [Storage(value = "trusted-paths.xml", roamingType = RoamingType.DISABLED)])
 @Service(Service.Level.APP)
-class TrustedPaths : SerializablePersistentStateComponent<TrustedPaths.State>(State()) {
+class TrustedPaths : TrustedProjectsStateStorage<TrustedPaths.State>(State()) {
+
   companion object {
     @JvmStatic
     fun getInstance(): TrustedPaths = service()
@@ -23,46 +25,31 @@ class TrustedPaths : SerializablePersistentStateComponent<TrustedPaths.State>(St
     @JvmField
     @field:OptionTag("TRUSTED_PROJECT_PATHS")
     val trustedPaths: Map<String, Boolean> = emptyMap()
-  )
+  ) : TrustedProjectsStateStorage.State {
+
+    @delegate:Transient
+    override val trustedState: PrefixTreeMap<Path, Boolean> by lazy {
+      NioPathPrefixTreeFactory.createMap(
+        trustedPaths.entries.map { Path.of(it.key) to it.value }
+      )
+    }
+  }
 
   fun getProjectPathTrustedState(path: Path): ThreeState {
-    val trustedPaths = state.trustedPaths
-    val closestAncestor = trustedPaths.keys.asSequence()
-      .map { path.fileSystem.getPath(it) }
-      .filter { path.startsWith(it) }
-      .maxByOrNull { it.nameCount }
-    if (closestAncestor == null) {
-      return ThreeState.UNSURE
-    }
-    return when (trustedPaths[closestAncestor.toString()]) {
-      true -> ThreeState.YES
-      false -> ThreeState.NO
-      null -> ThreeState.UNSURE
-    }
+    return getProjectTrustedState(path)
   }
 
   fun setProjectPathTrusted(path: Path, value: Boolean) {
-    updateState { currentState ->
-      State(currentState.trustedPaths + (path.toString() to value))
-    }
-  }
-
-  fun getProjectTrustedState(locatedProject: LocatedProject): ThreeState {
-    val trustedStates = locatedProject.projectRoots.map { getProjectPathTrustedState(it) }
-    return trustedStates.fold(ThreeState.YES) { acc, it ->
-      when {
-        acc == ThreeState.UNSURE -> ThreeState.UNSURE
-        it == ThreeState.UNSURE -> ThreeState.UNSURE
-        acc == ThreeState.NO -> ThreeState.NO
-        it == ThreeState.NO -> ThreeState.NO
-        else -> ThreeState.YES
-      }
+    updateState {
+      State(it.trustedPaths + (path.toString() to value))
     }
   }
 
   fun setProjectTrustedState(locatedProject: LocatedProject, isTrusted: Boolean) {
-    for (projectRoot in locatedProject.projectRoots) {
-      setProjectPathTrusted(projectRoot, isTrusted)
+    val additionalTrustedState = locatedProject.projectRoots
+      .associate { it.toString() to isTrusted }
+    updateState {
+      State(it.trustedPaths + additionalTrustedState)
     }
   }
 }
---
+++ platform/platform-impl/src/com/intellij/ide/trustedProjects/TrustedProjectsStateStorage.kt
@@ -0,0 +1,41 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide.trustedProjects
+
+import com.intellij.ide.trustedProjects.TrustedProjectsStateStorage.State
+import com.intellij.openapi.components.SerializablePersistentStateComponent
+import com.intellij.util.ThreeState
+import com.intellij.util.containers.prefix.map.PrefixTreeMap
+import org.jetbrains.annotations.ApiStatus
+import java.nio.file.Path
+
+@ApiStatus.Internal
+abstract class TrustedProjectsStateStorage<S: State>(state: S) : SerializablePersistentStateComponent<S>(state) {
+
+  interface State {
+
+    val trustedState: PrefixTreeMap<Path, Boolean>
+  }
+
+  open fun getProjectTrustedState(path: Path): ThreeState {
+    val closestAncestorState = state.trustedState.getAncestorEntries(path)
+      .maxByOrNull { it.key.nameCount }
+    if (closestAncestorState != null) {
+      return ThreeState.fromBoolean(closestAncestorState.value)
+    }
+    return ThreeState.UNSURE
+  }
+
+  open fun getProjectTrustedState(locatedProject: TrustedProjectsLocator.LocatedProject): ThreeState {
+    return locatedProject.projectRoots.asSequence()
+      .map { getProjectTrustedState(it) }
+      .fold(ThreeState.YES) { acc, it ->
+        when {
+          acc == ThreeState.UNSURE -> ThreeState.UNSURE
+          it == ThreeState.UNSURE -> ThreeState.UNSURE
+          acc == ThreeState.NO -> ThreeState.NO
+          it == ThreeState.NO -> ThreeState.NO
+          else -> ThreeState.YES
+        }
+      }
+  }
+}
