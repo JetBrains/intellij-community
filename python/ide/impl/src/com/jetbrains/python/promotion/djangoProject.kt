f02b6c493683100eb4b9b06c62560708dc87397b
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2023-11-25 16:55:46 +0100

    [PY-62397] add promo to the new dialog
    (cherry picked from commit 6df553aa0516d80a90402db02607f3018c2c5f33)
    
    IJ-CR-120792
    
    GitOrigin-RevId: 9b24765a3931d86b5212bda63fb2c755a325c059

--- python/ide/impl/src/com/jetbrains/python/promotion/djangoProject.kt
+++ python/ide/impl/src/com/jetbrains/python/promotion/djangoProject.kt
@@ -1,17 +1,10 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.promotion
 
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-
 import com.intellij.icons.AllIcons
-import com.intellij.ide.util.projectWizard.*
 import com.intellij.ide.wizard.withVisualPadding
-import com.intellij.openapi.Disposable
-import com.intellij.openapi.module.ModuleType
 import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.*
 import com.intellij.openapi.util.NlsSafe
-import com.intellij.openapi.util.registry.Registry
-import com.jetbrains.python.PythonModuleTypeBase
 import com.jetbrains.python.newProject.PyNewProjectSettings
 import com.jetbrains.python.newProject.PythonProjectGenerator
 import com.jetbrains.python.newProject.PythonPromoProjectGenerator
@@ -21,8 +14,6 @@ import javax.swing.JPanel
 
 @NlsSafe
 private const val DJANGO_NAME = "Django"
-private const val SPRING_PLUGIN_ID = "com.intellij.spring"
-
 
 internal class DjangoPromoProjectGenerator : PythonProjectGenerator<PyNewProjectSettings>(), PythonPromoProjectGenerator {
   override fun getName(): String {
@@ -43,28 +34,9 @@ internal class DjangoPromoProjectGenerator : PythonProjectGenerator<PyNewProject
         ),
         djangoPromoFeatureList,
         FeaturePromoBundle.message("free.trial.hint"),
-        SPRING_PLUGIN_ID
+        null,
       ),
       FUSEventSource.NEW_PROJECT_WIZARD
     ).withVisualPadding()
-  }
-}
-
-
-internal class DjangoPromoModuleBuilder : ModuleBuilder(), PromoModuleBuilder {
-  override fun isAvailable(): Boolean = Registry.`is`("idea.ultimate.features.hints.enabled")
-
-  override fun getModuleType(): ModuleType<*> = PythonModuleTypeBase.getInstance()
-  override fun getWeight(): Int = JVM_WEIGHT
-
-  override fun getBuilderId(): String = "promo-spring"
-  override fun getNodeIcon(): Icon = PythonIcons.Django.DjangoLogo
-  override fun getPresentableName(): String = DJANGO_NAME
-  override fun getDescription(): String = FeaturePromoBundle.message("promo.configurable.django")
-
-  override fun modifyProjectTypeStep(settingsStep: SettingsStep): ModuleWizardStep? = null
-
-  override fun getCustomOptionsStep(context: WizardContext?, parentDisposable: Disposable?): ModuleWizardStep {
-    TODO()
   }
 }
