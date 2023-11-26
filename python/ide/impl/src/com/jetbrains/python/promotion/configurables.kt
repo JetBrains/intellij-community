6829ec3726ee4949b7b3f18ad70a918dd7e7fe9b
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2023-11-26 19:50:50 +0100

    [PY-62397] add pycharm specific statistics since not all promoted features have a valid plugin id
    (cherry picked from commit 7a3249e18f653fe8201243f095b2b5e65b957e22)
    
    IJ-CR-120792
    
    GitOrigin-RevId: 4a9522952cc921729e2b6b923f52012cd954b2fb

--- python/ide/impl/src/com/jetbrains/python/promotion/configurables.kt
+++ python/ide/impl/src/com/jetbrains/python/promotion/configurables.kt
@@ -5,9 +5,8 @@ import com.intellij.icons.AllIcons
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.options.ConfigurableProvider
 import com.intellij.openapi.options.ConfigurableWithId
-import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.*
-import com.intellij.openapi.util.NlsContexts
-import icons.PythonIcons
+import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.FeaturePromoBundle
+import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.PromoFeatureListItem
 import javax.swing.Icon
 import javax.swing.JComponent
 import kotlin.reflect.KClass
@@ -24,17 +23,6 @@ internal abstract class ProPromoConfigurableProvider(private val clazz: KClass<o
   }
 }
 
-private fun featurePage(@NlsContexts.Label title: String, items: List<PromoFeatureListItem>, pluginId: String): JComponent {
-  return PromoPages.build(PromoFeaturePage(
-    PythonIcons.Python.Pycharm,
-    PluginAdvertiserService.pyCharmProfessional,
-    title,
-    items,
-    FeaturePromoBundle.message("free.trial.hint"),
-    pluginId
-  ))
-}
-
 internal class PromoDatabaseConfigurableProvider : ProPromoConfigurableProvider(PromoDatabaseConfigurable::class)
 internal class PromoJSConfigurableProvider : ProPromoConfigurableProvider(PromoJSConfigurable::class)
 internal class PromoTSConfigurableProvider : ProPromoConfigurableProvider(PromoTSConfigurable::class)
@@ -43,68 +31,43 @@ internal class PromoJupyterConfigurableProvider : ProPromoConfigurableProvider(P
 internal class PromoRemoteSshConfigurableProvider : ProPromoConfigurableProvider(PromoRemoteSshConfigurable::class)
 
 
-internal fun databaseFeatures(): JComponent = featurePage(
-  FeaturePromoBundle.message("feature.database.description.html", "https://www.jetbrains.com/help/idea/relational-databases.html"),
-  listOf(
-    PromoFeatureListItem(AllIcons.Actions.ReformatCode, FeaturePromoBundle.message("feature.database.code")),
-    PromoFeatureListItem(AllIcons.Nodes.DataTables, FeaturePromoBundle.message("feature.database.create.and.manage")),
-    PromoFeatureListItem(AllIcons.Actions.Run_anything, FeaturePromoBundle.message("feature.database.run")),
-    PromoFeatureListItem(AllIcons.ToolbarDecorator.Import, FeaturePromoBundle.message("feature.database.export"))
-  ),
-  "com.intellij.database"
-)
-
 internal class PromoDatabaseConfigurable : ProPromoConfigurable() {
   override fun getId(): String = "promo.database"
   override fun getDisplayName(): String = FeaturePromoBundle.message("promo.configurable.database")
 
   override fun createComponent(): JComponent {
-    return databaseFeatures()
+    return databaseFeatures(PromoEventSource.SETTINGS)
   }
-}
-
-internal fun javascriptFeatures(): JComponent {
-  @Suppress("DialogTitleCapitalization")
-  return featurePage(
-    FeaturePromoBundle.message("feature.javascript.description.html",
-                               "https://www.jetbrains.com/help/idea/javascript-specific-guidelines.html"),
-    listOf(
-      PromoFeatureListItem(AllIcons.Actions.ReformatCode, FeaturePromoBundle.message("feature.javascript.code")),
-      PromoFeatureListItem(AllIcons.Actions.SuggestedRefactoringBulb, FeaturePromoBundle.message("feature.javascript.refactor")),
-      PromoFeatureListItem(AllIcons.FileTypes.UiForm, FeaturePromoBundle.message("feature.javascript.frameworks"))
-    ),
-    "JavaScript"
-  )
 }
 
 internal class PromoJSConfigurable : ProPromoConfigurable() {
   override fun getId(): String = "promo.javascript"
   override fun getDisplayName(): String = FeaturePromoBundle.message("promo.configurable.javascript")
-  override fun createComponent(): JComponent = javascriptFeatures()
+  override fun createComponent(): JComponent = javascriptFeatures(PromoEventSource.SETTINGS, PromoTopic.JavaScript)
 }
 
 internal class PromoTSConfigurable : ProPromoConfigurable() {
   override fun getId(): String = "promo.typescript"
   override fun getDisplayName(): String = FeaturePromoBundle.message("promo.configurable.typescript")
-  override fun createComponent(): JComponent = javascriptFeatures()
+  override fun createComponent(): JComponent = javascriptFeatures(PromoEventSource.SETTINGS, PromoTopic.TypeScript)
 }
 
 internal class PromoDjangoConfigurable : ProPromoConfigurable() {
   override fun getId(): String = "promo.django"
   override fun getDisplayName(): String = FeaturePromoBundle.message("promo.configurable.django")
-  override fun createComponent(): JComponent =  djangoFeatures()
+  override fun createComponent(): JComponent =  djangoFeatures(PromoEventSource.SETTINGS)
 }
 
 internal class PromoJupyterConfigurable : ProPromoConfigurable() {
   override fun getId(): String = "promo.jupyter"
   override fun getDisplayName(): String = FeaturePromoBundle.message("promo.configurable.jupyter")
-  override fun createComponent(): JComponent =  jupyterFeatures()
+  override fun createComponent(): JComponent =  jupyterFeatures(PromoEventSource.SETTINGS)
 }
 
 internal class PromoRemoteSshConfigurable : ProPromoConfigurable() {
   override fun getId(): String = "promo.remoteSsh"
   override fun getDisplayName(): String = FeaturePromoBundle.message("promo.configurable.remoteSsh")
-  override fun createComponent(): JComponent = sshFeatures()
+  override fun createComponent(): JComponent = sshFeatures(PromoEventSource.SETTINGS)
 }
 
 internal val djangoPromoFeatureList = listOf(
@@ -112,41 +75,3 @@ internal val djangoPromoFeatureList = listOf(
   PromoFeatureListItem(AllIcons.FileTypes.Html, FeaturePromoBundle.message("feature.django.djangoTemplates")),
   PromoFeatureListItem(AllIcons.General.Web, FeaturePromoBundle.message("feature.django.endpoints"))
 )
-
-internal fun djangoFeatures(): JComponent {
-  return featurePage(
-    FeaturePromoBundle.message("feature.django.description.html",
-                               "https://www.jetbrains.com/help/pycharm/django-support.html"),
-    djangoPromoFeatureList,
-    "Django"
-  )
-}
-
-internal fun jupyterFeatures(): JComponent {
-  return featurePage(
-    FeaturePromoBundle.message("feature.jupyter.description",
-                               "https://www.jetbrains.com/help/pycharm/jupyter-notebook-support.html"),
-    listOf(
-      PromoFeatureListItem(AllIcons.Actions.ReformatCode, FeaturePromoBundle.message("feature.jupyter.code")),
-      PromoFeatureListItem(AllIcons.Actions.StartDebugger, FeaturePromoBundle.message("feature.jupyter.debugger")),
-      PromoFeatureListItem(AllIcons.Nodes.DataSchema, FeaturePromoBundle.message("feature.jupyter.tables")),
-      PromoFeatureListItem(AllIcons.Vcs.Branch, FeaturePromoBundle.message("feature.jupyter.vcs"))
-
-    ),
-    "Jupyter"
-  )
-}
-
-
-internal fun sshFeatures(): JComponent {
-  return featurePage(
-    FeaturePromoBundle.message("feature.remoteSsh.description.html",
-                               "https://www.jetbrains.com/help/pycharm/create-ssh-configurations.html"),
-    listOf(
-      PromoFeatureListItem(AllIcons.Actions.Execute, FeaturePromoBundle.message("feature.remoteSsh.run")),
-      PromoFeatureListItem(AllIcons.Nodes.Deploy, FeaturePromoBundle.message("feature.remoteSsh.deploy")),
-      PromoFeatureListItem(AllIcons.Toolwindows.SettingSync, FeaturePromoBundle.message("feature.remoteSsh.sync"))
-    ),
-    "JavaScript"
-  )
-}
