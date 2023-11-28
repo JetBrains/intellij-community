5509e8657a2c896014aed9dd700cb4d39c9c28b7
Ivan Posti <ivan.posti@jetbrains.com>
2023-11-28 14:34:06 +0100

    [Grazie] Move static fields and methods away from the companion object
    GitOrigin-RevId: a24bb97a0fbf19d5a51d34fd7cfbba35d6189913

--- plugins/grazie/src/main/kotlin/com/intellij/grazie/grammar/LanguageToolChecker.kt
+++ plugins/grazie/src/main/kotlin/com/intellij/grazie/grammar/LanguageToolChecker.kt
@@ -20,6 +20,7 @@ import com.intellij.util.containers.Interner
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.html.*
+import org.jetbrains.annotations.ApiStatus
 import org.languagetool.JLanguageTool
 import org.languagetool.Languages
 import org.languagetool.markup.AnnotatedTextBuilder
@@ -30,6 +31,9 @@ import java.util.*
 import java.util.function.Predicate
 
 open class LanguageToolChecker : TextChecker() {
+  @ApiStatus.Internal
+  class TestChecker: LanguageToolChecker()
+
   override fun getRules(locale: Locale): Collection<Rule> {
     val language = Languages.getLanguageForLocale(locale)
     val lang = Lang.values().find { it.jLanguage == language } ?: return emptyList()
@@ -170,145 +174,140 @@ open class LanguageToolChecker : TextChecker() {
     private fun isAbstractCategory(id: String) =
       id == RuleGroup.SENTENCE_END_PUNCTUATION || id == RuleGroup.SENTENCE_START_CASE || id == RuleGroup.UNLIKELY_OPENING_PUNCTUATION
   }
+}
 
-  companion object {
-    private val logger = LoggerFactory.getLogger(LanguageToolChecker::class.java)
-    private val cacheKey = Key.create<List<Problem>>("grazie.LT.problem.cache")
-    private val interner = Interner.createWeakInterner<String>()
-    private val sentenceSeparationRules = setOf("LC_AFTER_PERIOD", "PUNT_GEEN_HL", "KLEIN_NACH_PUNKT")
-    private val openClosedRangeStart = Regex("[\\[(].+?(\\.\\.|:|,).+[])]")
-    private val openClosedRangeEnd = Regex(".*" + openClosedRangeStart.pattern)
+private val logger = LoggerFactory.getLogger(LanguageToolChecker::class.java)
+private val cacheKey = Key.create<List<LanguageToolChecker.Problem>>("grazie.LT.problem.cache")
+private val interner = Interner.createWeakInterner<String>()
+private val sentenceSeparationRules = setOf("LC_AFTER_PERIOD", "PUNT_GEEN_HL", "KLEIN_NACH_PUNKT")
+private val openClosedRangeStart = Regex("[\\[(].+?(\\.\\.|:|,).+[])]")
+private val openClosedRangeEnd = Regex(".*" + openClosedRangeStart.pattern)
 
-    internal fun grammarRules(tool: JLanguageTool, lang: Lang): List<LanguageToolRule> {
-      return tool.allRules.asSequence()
-        .distinctBy { it.id }
-        .filter { r -> !r.isDictionaryBasedSpellingRule }
-        .map { LanguageToolRule(lang, it) }
-        .toList()
+internal fun grammarRules(tool: JLanguageTool, lang: Lang): List<LanguageToolRule> {
+  return tool.allRules.asSequence()
+    .distinctBy { it.id }
+    .filter { r -> !r.isDictionaryBasedSpellingRule }
+    .map { LanguageToolRule(lang, it) }
+    .toList()
+}
+
+/**
+ * Git adds "cherry picked from", which doesn't seem entirely grammatical,
+ * but zillions of tools depend on this message, and it's unlikely to be changed.
+ * So we ignore this pattern in commit messages and literals (which might be used for parsing git output)
+ */
+private fun isGitCherryPickedFrom(match: RuleMatch, text: TextContent): Boolean {
+  return match.rule.id == "EN_COMPOUNDS" && match.fromPos > 0 && text.startsWith("(cherry picked from", match.fromPos - 1) &&
+         (text.domain == TextContent.TextDomain.LITERALS ||
+          text.domain == TextContent.TextDomain.PLAIN_TEXT && runReadAction { CommitMessage.isCommitMessage(text.containingFile) })
+}
+
+private fun isKnownLTBug(match: RuleMatch, text: TextContent): Boolean {
+  if (match.rule is GenericUnpairedBracketsRule) {
+    if (match.fromPos > 0 &&
+        (text.startsWith("\")", match.fromPos - 1) || text.subSequence(0, match.fromPos).contains("(\""))) {
+      return true //https://github.com/languagetool-org/languagetool/issues/5269
     }
-
-    /**
-     * Git adds "cherry picked from", which doesn't seem entirely grammatical,
-     * but zillions of tools depend on this message, and it's unlikely to be changed.
-     * So we ignore this pattern in commit messages and literals (which might be used for parsing git output)
-     */
-    private fun isGitCherryPickedFrom(match: RuleMatch, text: TextContent): Boolean {
-      return match.rule.id == "EN_COMPOUNDS" && match.fromPos > 0 && text.startsWith("(cherry picked from", match.fromPos - 1) &&
-             (text.domain == TextContent.TextDomain.LITERALS ||
-              text.domain == TextContent.TextDomain.PLAIN_TEXT && runReadAction { CommitMessage.isCommitMessage(text.containingFile) })
+    if (text.startsWith("'", match.fromPos) && text.subSequence(match.fromPos + 1, text.length).contains("'")) {
+      return true // https://github.com/languagetool-org/languagetool/issues/7249
     }
-
-    private fun isKnownLTBug(match: RuleMatch, text: TextContent): Boolean {
-      if (match.rule is GenericUnpairedBracketsRule) {
-        if (match.fromPos > 0 &&
-            (text.startsWith("\")", match.fromPos - 1) || text.subSequence(0, match.fromPos).contains("(\""))) {
-          return true //https://github.com/languagetool-org/languagetool/issues/5269
-        }
-        if (text.startsWith("'", match.fromPos) && text.subSequence(match.fromPos + 1, text.length).contains("'")) {
-          return true // https://github.com/languagetool-org/languagetool/issues/7249
-        }
-        if (text.substring(match.fromPos, match.toPos) == "\"" && text.subSequence(0, match.fromPos).contains("\"")) {
-          return true // e.g. commented raise ValueError(f"a very long text so that the vicinity of the error doesn't seem like code")
-        }
-        if (couldBeOpenClosedRange(text, match.fromPos)) {
-          return true
-        }
-      }
-
-      if (match.rule.id == "ARTICLE_ADJECTIVE_OF" && text.substring(match.fromPos, match.toPos).equals("iterable", ignoreCase = true)) {
-        return true // https://github.com/languagetool-org/languagetool/issues/5270
-      }
-
-      if (match.rule.id.endsWith("DOUBLE_PUNCTUATION") &&
-          (isNumberRange(match.fromPos, match.toPos, text) || isPathPart(match.fromPos, match.toPos, text))) {
-        return true
-      }
-
-      return false
+    if (text.substring(match.fromPos, match.toPos) == "\"" && text.subSequence(0, match.fromPos).contains("\"")) {
+      return true // e.g. commented raise ValueError(f"a very long text so that the vicinity of the error doesn't seem like code")
     }
-
-    // https://github.com/languagetool-org/languagetool/issues/6566
-    private fun couldBeOpenClosedRange(text: TextContent, index: Int): Boolean {
-      val unpaired = text[index]
-      return "([".contains(unpaired) && openClosedRangeStart.matchesAt(text, index) ||
-             ")]".contains(unpaired) && openClosedRangeEnd.matches(text.subSequence(0, index + 1))
-    }
-
-    // https://github.com/languagetool-org/languagetool/issues/5230
-    private fun isNumberRange(startOffset: Int, endOffset: Int, text: TextContent): Boolean {
-      return startOffset > 0 && endOffset < text.length && text[startOffset - 1].isDigit() && text[endOffset].isDigit()
-    }
-
-    // https://github.com/languagetool-org/languagetool/issues/5883
-    private fun isPathPart(startOffset: Int, endOffset: Int, text: TextContent): Boolean {
-      return text.subSequence(0, startOffset).endsWith('/') || text.subSequence(endOffset, text.length).startsWith('/')
-    }
-
-    @NlsSafe
-    private fun toTooltipTemplate(match: RuleMatch): String {
-      val html = html {
-        val withCorrections = match.rule.incorrectExamples.filter { it.corrections.isNotEmpty() }.takeIf { it.isNotEmpty() }
-        val incorrectExample = (withCorrections ?: match.rule.incorrectExamples).minByOrNull { it.example.length }
-        p {
-          incorrectExample?.let {
-            style = "padding-bottom: 8px;"
-          }
-
-          +match.messageSanitized
-          nbsp()
-        }
-
-        table {
-          cellpading = "0"
-          cellspacing = "0"
-
-          incorrectExample?.let {
-            tr {
-              td {
-                valign = "top"
-                style = "padding-right: 5px; color: gray; vertical-align: top; white-space: nowrap;"
-                +" "
-                +GrazieBundle.message("grazie.settings.grammar.rule.incorrect")
-                +" "
-                nbsp()
-              }
-              td {
-                style = "width: 100%;"
-                toIncorrectHtml(it)
-                nbsp()
-              }
-            }
-
-            if (it.corrections.isNotEmpty()) {
-              tr {
-                td {
-                  valign = "top"
-                  style = "padding-top: 5px; padding-right: 5px; color: gray; vertical-align: top; white-space: nowrap;"
-                  +" "
-                  +GrazieBundle.message("grazie.settings.grammar.rule.correct")
-                  +" "
-                  nbsp()
-                }
-                td {
-                  style = "padding-top: 5px; width: 100%;"
-                  toCorrectHtml(it)
-                  nbsp()
-                }
-              }
-            }
-          }
-        }
-
-        p {
-          style = "text-align: left; font-size: x-small; color: gray; padding-top: 10px; padding-bottom: 0px;"
-          +" "
-          +GrazieBundle.message("grazie.tooltip.powered-by-language-tool")
-        }
-      }
-      return interner.intern(html)
+    if (couldBeOpenClosedRange(text, match.fromPos)) {
+      return true
     }
   }
 
-  class TestChecker: LanguageToolChecker()
+  if (match.rule.id == "ARTICLE_ADJECTIVE_OF" && text.substring(match.fromPos, match.toPos).equals("iterable", ignoreCase = true)) {
+    return true // https://github.com/languagetool-org/languagetool/issues/5270
+  }
 
+  if (match.rule.id.endsWith("DOUBLE_PUNCTUATION") &&
+      (isNumberRange(match.fromPos, match.toPos, text) || isPathPart(match.fromPos, match.toPos, text))) {
+    return true
+  }
+
+  return false
+}
+
+// https://github.com/languagetool-org/languagetool/issues/6566
+private fun couldBeOpenClosedRange(text: TextContent, index: Int): Boolean {
+  val unpaired = text[index]
+  return "([".contains(unpaired) && openClosedRangeStart.matchesAt(text, index) ||
+         ")]".contains(unpaired) && openClosedRangeEnd.matches(text.subSequence(0, index + 1))
+}
+
+// https://github.com/languagetool-org/languagetool/issues/5230
+private fun isNumberRange(startOffset: Int, endOffset: Int, text: TextContent): Boolean {
+  return startOffset > 0 && endOffset < text.length && text[startOffset - 1].isDigit() && text[endOffset].isDigit()
+}
+
+// https://github.com/languagetool-org/languagetool/issues/5883
+private fun isPathPart(startOffset: Int, endOffset: Int, text: TextContent): Boolean {
+  return text.subSequence(0, startOffset).endsWith('/') || text.subSequence(endOffset, text.length).startsWith('/')
+}
+
+@NlsSafe
+private fun toTooltipTemplate(match: RuleMatch): String {
+  val html = html {
+    val withCorrections = match.rule.incorrectExamples.filter { it.corrections.isNotEmpty() }.takeIf { it.isNotEmpty() }
+    val incorrectExample = (withCorrections ?: match.rule.incorrectExamples).minByOrNull { it.example.length }
+    p {
+      incorrectExample?.let {
+        style = "padding-bottom: 8px;"
+      }
+
+      +match.messageSanitized
+      nbsp()
+    }
+
+    table {
+      cellpading = "0"
+      cellspacing = "0"
+
+      incorrectExample?.let {
+        tr {
+          td {
+            valign = "top"
+            style = "padding-right: 5px; color: gray; vertical-align: top; white-space: nowrap;"
+            +" "
+            +GrazieBundle.message("grazie.settings.grammar.rule.incorrect")
+            +" "
+            nbsp()
+          }
+          td {
+            style = "width: 100%;"
+            toIncorrectHtml(it)
+            nbsp()
+          }
+        }
+
+        if (it.corrections.isNotEmpty()) {
+          tr {
+            td {
+              valign = "top"
+              style = "padding-top: 5px; padding-right: 5px; color: gray; vertical-align: top; white-space: nowrap;"
+              +" "
+              +GrazieBundle.message("grazie.settings.grammar.rule.correct")
+              +" "
+              nbsp()
+            }
+            td {
+              style = "padding-top: 5px; width: 100%;"
+              toCorrectHtml(it)
+              nbsp()
+            }
+          }
+        }
+      }
+    }
+
+    p {
+      style = "text-align: left; font-size: x-small; color: gray; padding-top: 10px; padding-bottom: 0px;"
+      +" "
+      +GrazieBundle.message("grazie.tooltip.powered-by-language-tool")
+    }
+  }
+  return interner.intern(html)
 }
