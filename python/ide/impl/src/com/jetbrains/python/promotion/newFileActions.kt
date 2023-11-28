3a3804963c0c93504be346c395afa35342df23cf
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2023-11-28 19:22:01 +0100

    [PY-64340] fix size problem with DialogBuilder, do not use any decorators
    GitOrigin-RevId: 21fee0981f661e7889b95c8ad3e9e17338607a36

--- python/ide/impl/src/com/jetbrains/python/promotion/newFileActions.kt
+++ python/ide/impl/src/com/jetbrains/python/promotion/newFileActions.kt
@@ -4,11 +4,18 @@ package com.jetbrains.python.promotion
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.DialogBuilder
+import com.intellij.ui.PopupBorder
 import javax.swing.JComponent
+import javax.swing.JDialog
+import javax.swing.JRootPane
 
 internal abstract class PycharmNewFilePromoAction(private val promoTopic: PromoTopic) : AnAction() {
   override fun actionPerformed(e: AnActionEvent) {
     val builder = DialogBuilder()
+    builder.dialogWrapper.setUndecorated(true)
+    val rootPane = (builder.window as JDialog).rootPane
+    rootPane.windowDecorationStyle = JRootPane.NONE
+    rootPane.border = PopupBorder.Factory.create(true, true)
     builder.setCenterPanel(createCentralComponent())
     val isOk = builder.showAndGet()
     if (isOk) {
