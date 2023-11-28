9300fe8c45328a797992ccf4990b9f7e155a9709
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2023-11-28 23:25:34 +0200

    PY-60104 Clean up in tests about type inference for decorators
    Removed a number of tests in Py3TypeTest duplicating those of PyDecoratedFunctionTypeProviderTest.
    
    Removed the tests about PY-23067 in Py3ArgumentListInspectionTest and Py3CompletionTest because
    this issue was actually not addressed in 05e8ed4df0c7faa24bd972e1b422f664d708b510, and the behavior
    some of them assert is not what users wanted.
    
    More consistent naming of tests in PyDecoratedFunctionTypeProviderTest and PyParameterInfoTest.
    Removed there excess tests too similar to others or checking scenarios not relevant to
    the current approach to type inference for decorators, e.g. presense of @functools.wraps and
    alternatives inside decorators — we don't even analyze their bodies anymore.
    
    Add a few extra tests illustrating problems with the current approach:
    - testNotAnnotatedDecoratorChangingFunctionSignatureIsIgnored
    - testInStackOfDecoratorsChangingFunctionSignatureOnlyAnnotatedAreConsidered
    - testInStackOfImportedDecoratorsChangingFunctionSignatureOnlyAnnotatedAreConsidered
    - testNotAnnotatedDecoratorRetainsParametersOfOriginalFunctionEvenIfItChangesItsSignature
    
    GitOrigin-RevId: 0bf5070fc523b88dcc9d3009786dd028bdfa0feb

--- python/testData/types/ImportDecoratorNotChangeSignatureNoAnnotation/ext.py
+++
@@ -1,6 +0,0 @@
-from typing import Callable
-
-def dec_not_change(fun):
-  def wrapper(name_in_wrapper: str):
-    return 42
-  return wrapper
--- python/testData/types/ImportTwoDecoratorsNotChangeSignatureWithAnnotation/ext.py
+++
@@ -1,8 +0,0 @@
-from typing import Callable
-
-def first_dec_same(fun) -> Callable[[str], int]:
-  return fun
-
-
-def second_dec_same(fun) -> Callable[[str], int]:
-  return fun
