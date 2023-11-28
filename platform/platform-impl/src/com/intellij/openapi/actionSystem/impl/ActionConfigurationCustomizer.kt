e2706c840d7d899e2888680f2d0e86e445ad9c81
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-11-28 10:12:45 +0100

    IDEA-337454 convert ActionConfigurationCustomizer to modern language
    GitOrigin-RevId: 76070548b88c952fa386920d050f76df234419ee

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionConfigurationCustomizer.kt
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionConfigurationCustomizer.kt
@@ -1,21 +1,21 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.actionSystem.impl;
+package com.intellij.openapi.actionSystem.impl
 
-import com.intellij.openapi.actionSystem.ActionManager;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.ActionManager
+import org.jetbrains.annotations.ApiStatus
 
 /**
- * Allows customizing actions during the {@link ActionManager} service initialization.
- * <p>
- * Register in {@code com.intellij.actionConfigurationCustomizer} extension point.
+ * Allows customizing actions during the [ActionManager] service initialization.
+ *
+ *
+ * Register in `com.intellij.actionConfigurationCustomizer` extension point.
  *
  * @see DynamicActionConfigurationCustomizer
  */
 @ApiStatus.Internal
-public interface ActionConfigurationCustomizer {
+interface ActionConfigurationCustomizer {
   /**
    * @param actionManager action manager being initialized
    */
-  void customize(@NotNull ActionManager actionManager);
+  fun customize(actionManager: ActionManager)
 }
