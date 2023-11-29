e23556591f1b55db0d7760ff5ac94055b16802f6
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2023-11-29 13:57:34 +0100

    [kotlin][scripting] KTIJ-27924 K2: MISSING_DEPENDENCY_CLASS on build.gradle.kts highlighting
    Solution introduced to solve the KTIJ-27280 problem (8ab666a7) lacks an important point.
    The thing is that `buildSrc` dependencies intersect with the ones from the `build.gradle.kts`.
    During the analysis, PSI-element of a script binary dependency is "attached" to the `buildSrc`
    module (this is how [1] function works). Given that, we calculate corresponding `KtModule`
    incorrectly.
    
    ---------------------------------------------------------------------------------------------
    [1]: ModuleUtilCore.findModuleForPsiElement()
    [2]: ProjectStructureProviderIdeImplKt.calculateKtModule()
    
    ^KTIJ-27924 fixed
    
    GitOrigin-RevId: 62d6f3953474b2acda684030f8995a780927718a

--- plugins/kotlin/gradle/gradle/src/org/jetbrains/kotlin/idea/gradle/GradleProjectStructureInsightsProvider.kt
+++ plugins/kotlin/gradle/gradle/src/org/jetbrains/kotlin/idea/gradle/GradleProjectStructureInsightsProvider.kt
@@ -4,12 +4,15 @@ package org.jetbrains.kotlin.idea.gradle
 import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.idea.base.projectStructure.ProjectStructureInsightsProvider
+import org.jetbrains.kotlin.idea.base.projectStructure.RootKindFilter
+import org.jetbrains.kotlin.idea.base.projectStructure.matches
 import org.jetbrains.plugins.gradle.util.GradleUtil
 import org.jetbrains.plugins.gradle.util.isBuildSrcModule
 import org.jetbrains.plugins.gradle.util.isIncludedBuild
 
 class GradleProjectStructureInsightsProvider : ProjectStructureInsightsProvider {
     override fun isInSpecialSrcDirectory(psiElement: PsiElement): Boolean {
+        if (!RootKindFilter.projectSources.matches(psiElement)) return false
         val module = ModuleUtilCore.findModuleForPsiElement(psiElement) ?: return false
         val moduleData = GradleUtil.findGradleModuleData(module)?.data ?: return false
         return moduleData.isBuildSrcModule() || moduleData.isIncludedBuild
