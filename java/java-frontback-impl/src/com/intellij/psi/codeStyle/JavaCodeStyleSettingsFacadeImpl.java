2e1c6343f25a4ea025d0cf0393162b29d815e119
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2023-12-01 17:44:34 +0000

    [java-rd] IDEA-339292 Move setting to fronback part to make them available for remote dev
    Merge-request: IJ-MR-121171
    Merged-by: Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
    
    GitOrigin-RevId: ae28b85856cb723e7b51bea3e3cbef91e6de634d

--- java/java-frontback-impl/src/com/intellij/psi/codeStyle/JavaCodeStyleSettingsFacadeImpl.java
+++ java/java-frontback-impl/src/com/intellij/psi/codeStyle/JavaCodeStyleSettingsFacadeImpl.java
@@ -1,7 +1,6 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.codeStyle;
 
-import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
