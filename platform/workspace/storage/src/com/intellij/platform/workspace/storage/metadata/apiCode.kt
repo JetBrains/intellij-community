181cf7486e8ee5bae518d3b8505045ab06963777
Stepan Bagritsevich <stsiapan.bahrytsevich@jetbrains.com>
2023-12-01 11:01:25 +0100

    [Workspace model] IDEA-338900 Add `MetadataStorageBridge` to the storage
    Regenerate `MetadataStorage` implementations according to the new `codegen-api` version `0.0.6` and new `codegen-impl` version `2.3.5`.
    
    GitOrigin-RevId: 3df18508edc65bfcfefe92d222a6535dc42f911f

--- platform/workspace/storage/src/com/intellij/platform/workspace/storage/metadata/apiCode.kt
+++ platform/workspace/storage/src/com/intellij/platform/workspace/storage/metadata/apiCode.kt
@@ -2,7 +2,6 @@
 package com.intellij.platform.workspace.storage.metadata
 
 import com.intellij.platform.workspace.storage.EntityStorage
-import com.intellij.platform.workspace.storage.metadata.exceptions.MissingTypeMetadataException
 import com.intellij.platform.workspace.storage.metadata.exceptions.MissingTypeMetadataHashException
 import com.intellij.platform.workspace.storage.metadata.model.StorageTypeMetadata
 
@@ -27,6 +26,20 @@ public interface MetadataStorage {
   public fun getMetadataHashByTypeFqn(fqName: String): MetadataHash =
     getMetadataHashByTypeFqnOrNull(fqName) ?: throw MissingTypeMetadataHashException(fqName)
 }
+
+
+/**
+ * Its implementation is generated with entities implementation in each package.
+ * It is a bridge to [MetadataStorage] implementation, which stores metadata of all types from the entire module
+ *
+ * Used to speed up [EntityStorage] deserialization:
+ * * During serialization [MetadataStorage] fqn's, that store metadata for the entire module, are saved in the cache.
+ * * During deserialization we need to load for each module only one [MetadataStorage] whose fqn is stored in the cache.
+ * In this way we speed up deserialization because we do not load so many [MetadataStorage] classes
+ *
+ * See [EntityStorageSerializerImpl]
+ */
+public abstract class MetadataStorageBridge(public val metadataStorage: MetadataStorage): MetadataStorage by metadataStorage
 
 
 internal typealias MetadataHash = Int
