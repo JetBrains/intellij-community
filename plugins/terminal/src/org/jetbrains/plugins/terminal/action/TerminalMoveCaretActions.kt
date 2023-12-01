e48ba8f39bc87a56438ec3abe01533cb53ed0ec7
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2023-12-01 12:13:09 +0200

    [terminal] Move focus to prompt on Ctrl+A/E actions invocations in the output
    GitOrigin-RevId: 52a40102ea6ff6c71b34438702a0f5f6b078a7d8

--- plugins/terminal/src/org/jetbrains/plugins/terminal/action/TerminalMoveCaretActions.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/action/TerminalMoveCaretActions.kt
@@ -1,17 +1,15 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.terminal.action
 
-import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification
 import com.intellij.openapi.application.runWriteAction
-import org.jetbrains.plugins.terminal.exp.TerminalDataContextUtils.editor
-import org.jetbrains.plugins.terminal.exp.TerminalDataContextUtils.isPromptEditor
-import org.jetbrains.plugins.terminal.exp.TerminalPromotedDumbAwareAction
+import com.intellij.openapi.editor.Caret
+import com.intellij.openapi.editor.Editor
+import org.jetbrains.plugins.terminal.exp.TerminalPromotedEditorAction
 
-abstract class TerminalMoveCaretAction(private val moveToStart: Boolean) : TerminalPromotedDumbAwareAction(), ActionRemoteBehaviorSpecification.Disabled {
-  override fun actionPerformed(e: AnActionEvent) {
-    val editor = e.editor ?: return
+private class TerminalMoveCaretActionHandler(private val moveToStart: Boolean) : TerminalPromptEditorActionHandler() {
+  override fun executeAction(editor: Editor, caret: Caret?, dataContext: DataContext) {
     val lineIndex = editor.offsetToLogicalPosition(editor.caretModel.offset).line
     val newOffset = if (moveToStart) {
       editor.document.getLineStartOffset(lineIndex)
@@ -22,14 +20,10 @@ abstract class TerminalMoveCaretAction(private val moveToStart: Boolean) : Termi
       editor.caretModel.moveToOffset(newOffset)
     }
   }
-
-  override fun update(e: AnActionEvent) {
-    e.presentation.isEnabledAndVisible = e.editor?.isPromptEditor == true
-  }
-
-  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 }
 
-class TerminalMoveCaretToLineStartAction : TerminalMoveCaretAction(moveToStart = true)
+class TerminalMoveCaretToLineStartAction : TerminalPromotedEditorAction(TerminalMoveCaretActionHandler(moveToStart = true)),
+                                           ActionRemoteBehaviorSpecification.Disabled
 
-class TerminalMoveCaretToLineEndAction : TerminalMoveCaretAction(moveToStart = false)
+class TerminalMoveCaretToLineEndAction : TerminalPromotedEditorAction(TerminalMoveCaretActionHandler(moveToStart = false)),
+                                         ActionRemoteBehaviorSpecification.Disabled
