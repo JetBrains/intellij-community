eed983b302486b87900087af1e36c228b5dffb35
Alexey Kalina <alexey.kalina@jetbrains.com>
2023-12-02 09:59:40 +0200

    [evaluation-plugin] fix config deserialization
    GitOrigin-RevId: f098bfb61b37e7705d101edd66e6dfa4d6374ae4

--- plugins/evaluation-plugin/core/src/com/intellij/cce/workspace/ConfigFactory.kt
+++ plugins/evaluation-plugin/core/src/com/intellij/cce/workspace/ConfigFactory.kt
@@ -51,7 +51,9 @@ object ConfigFactory {
     val languageName = map.getAs<String>("language")
     return Config.build(map.handleEnv("projectPath"), languageName) {
       outputDir = map.handleEnv("outputDir")
-      projectName = map.handleEnv("projectName")
+      if (map.containsKey("projectName")) {
+        projectName = map.handleEnv("projectName")
+      }
       deserializeStrategy(map.getIfExists("strategy"), strategySerializer, languageName, this)
       deserializeActionsGeneration(map.getIfExists("actions"), languageName, this)
       deserializeActionsInterpretation(map.getIfExists("interpret"), this)
