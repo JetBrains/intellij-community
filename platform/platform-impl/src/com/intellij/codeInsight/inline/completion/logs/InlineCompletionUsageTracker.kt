d6b38103d75b50d28b2ef7048f6462cbe102631e
Igor.Davidenko <igor.davidenko@jetbrains.com>
2023-12-04 21:23:28 +0100

    [full-line] ML-1933 Implement arguments features for JS
    GitOrigin-RevId: 2568b9bc44c02eaacf8a33cdb4685af2c26e8661

--- platform/platform-impl/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionUsageTracker.kt
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionUsageTracker.kt
@@ -18,7 +18,7 @@ import kotlin.concurrent.withLock
 import kotlin.coroutines.cancellation.CancellationException
 
 object InlineCompletionUsageTracker : CounterUsagesCollector() {
-  private val GROUP = EventLogGroup("inline.completion", 18)
+  private val GROUP = EventLogGroup("inline.completion", 19)
 
   const val INVOKED_EVENT_ID = "invoked"
   internal object InvokedEvents {
