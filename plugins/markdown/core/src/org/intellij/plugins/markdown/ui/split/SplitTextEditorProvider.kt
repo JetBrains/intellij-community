bb27e3e8d716f74ecd189561f0d17fd14bddd72d
Ivan Posti <ivan.posti@jetbrains.com>
2023-12-07 15:53:18 +0100

    [Markdown][IDEA-338098] Ensure backend and frontend split editor providers return the same editor type id
    (cherry picked from commit a0c3ec6ec97ffa7e6b6639c0208c43a92d2f3ea4)
    (cherry picked from commit 311c58c33031a4291a52c83304655d46f10a8ea9)
    
    IJ-MR-122348
    
    GitOrigin-RevId: 0cecb83cfc4995b90bf105e43bc9b10cfbe90738

--- plugins/markdown/core/src/org/intellij/plugins/markdown/ui/split/SplitTextEditorProvider.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/ui/split/SplitTextEditorProvider.kt
@@ -21,7 +21,7 @@ abstract class SplitTextEditorProvider(
   private val firstProvider: FileEditorProvider,
   private val secondProvider: FileEditorProvider
 ): AsyncFileEditorProvider, DumbAware {
-  private val editorTypeId = "split-provider[${firstProvider.getEditorTypeId()};${secondProvider.getEditorTypeId()}]"
+  private val editorTypeId = createSplitEditorProviderTypeId(firstProvider.editorTypeId, secondProvider.editorTypeId)
 
   override fun accept(project: Project, file: VirtualFile): Boolean {
     return firstProvider.accept(project, file) && secondProvider.accept(project, file)
@@ -155,4 +155,9 @@ private suspend fun createEditorBuilderAsync(
       return provider.createEditor(project, file)
     }
   }
+}
+
+@ApiStatus.Internal
+fun createSplitEditorProviderTypeId(first: String, second: String): String {
+  return "split-provider[$first;$second]"
 }
