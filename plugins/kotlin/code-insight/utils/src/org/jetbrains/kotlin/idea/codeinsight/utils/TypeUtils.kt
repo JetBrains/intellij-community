54672e47d02de3e0e58898740063ed88647aba69
Roman Golyshev <roman.golyshev@jetbrains.com>
2023-12-07 02:27:07 +0100

    KTIJ-28067 [llm][kotlin] Always render flexible types as upper bounds for `KotlinInlineCompletionContextCollector`
    Flexible types, when rendered like `String!` or
    `String..String?` cannot be passed into a KtPsiFactory,
    because they are not well-formed Kotlin types.
    To avoid errors, all flexible types are reduced to
    their upper (nullable) bounds during rendering in
    `renderAndProcessType`
    
     Added a temporary `KtFlexibleTypeAsUpperBoundRenderer` to
     the kotlin code-insight.utils module until KT-64138 is
     implemented - then it can be removed
    
     Had to put it there and not to the `KotlinInlineCompletionContextCollectorUtils.kt`
     because the ml.llm.kotlin does not have Context Receivers
     enabled, and they are used in `KtFlexibleTypeRenderer`
    
     ^KTIJ-28067 Fixed
    
    GitOrigin-RevId: cb023ea291939de8c3990a97da24189715e06525

--- plugins/kotlin/code-insight/utils/src/org/jetbrains/kotlin/idea/codeinsight/utils/TypeUtils.kt
+++ plugins/kotlin/code-insight/utils/src/org/jetbrains/kotlin/idea/codeinsight/utils/TypeUtils.kt
@@ -5,10 +5,14 @@
 package org.jetbrains.kotlin.idea.codeinsight.utils
 
 import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
+import org.jetbrains.kotlin.analysis.api.renderer.types.KtTypeRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.types.renderers.KtFlexibleTypeRenderer
 import org.jetbrains.kotlin.analysis.api.symbols.KtClassKind
 import org.jetbrains.kotlin.analysis.api.symbols.KtClassOrObjectSymbol
+import org.jetbrains.kotlin.analysis.api.types.KtFlexibleType
 import org.jetbrains.kotlin.analysis.api.types.KtNonErrorClassType
 import org.jetbrains.kotlin.analysis.api.types.KtType
+import org.jetbrains.kotlin.analysis.utils.printer.PrettyPrinter
 
 context(KtAnalysisSession)
 fun KtType.isNullableAnyType() = isAny && isMarkedNullable
@@ -21,4 +25,16 @@ fun KtType.isEnum(): Boolean {
     if (this !is KtNonErrorClassType) return false
     val classSymbol = classSymbol
     return classSymbol is KtClassOrObjectSymbol && classSymbol.classKind == KtClassKind.ENUM_CLASS
+}
+
+/**
+ * Always renders flexible type as its upper bound.
+ *
+ * TODO should be moved to [KtFlexibleTypeRenderer] and removed from here, see KT-64138
+ */
+object KtFlexibleTypeAsUpperBoundRenderer : KtFlexibleTypeRenderer {
+    context(KtAnalysisSession, KtTypeRenderer)
+    override fun renderType(type: KtFlexibleType, printer: PrettyPrinter) {
+        renderType(type.upperBound, printer)
+    }
 }
