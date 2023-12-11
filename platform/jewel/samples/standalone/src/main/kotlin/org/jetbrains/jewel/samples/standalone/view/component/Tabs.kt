c616b98475d6ef4f9f184f708669726c5c0290fe
Sebastiano Poggi <sebp@google.com>
2023-12-11 14:15:36 -0800

    Clean up Tabs example code
    GitOrigin-RevId: 1d4178fcf26a7fa99c4727c21bcec00a7212b526

--- platform/jewel/samples/standalone/src/main/kotlin/org/jetbrains/jewel/samples/standalone/view/component/Tabs.kt
+++ platform/jewel/samples/standalone/src/main/kotlin/org/jetbrains/jewel/samples/standalone/view/component/Tabs.kt
@@ -2,33 +2,24 @@
 
 package org.jetbrains.jewel.samples.standalone.view.component
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.HoverInteraction
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import org.jetbrains.jewel.foundation.theme.JewelTheme
 import org.jetbrains.jewel.samples.standalone.StandaloneSampleIcons
 import org.jetbrains.jewel.samples.standalone.viewmodel.View
-import org.jetbrains.jewel.ui.NoIndication
 import org.jetbrains.jewel.ui.component.Icon
+import org.jetbrains.jewel.ui.component.IconButton
 import org.jetbrains.jewel.ui.component.TabData
 import org.jetbrains.jewel.ui.component.TabStrip
 import org.jetbrains.jewel.ui.component.Text
@@ -51,9 +42,9 @@ private fun DefaultTabShowcase() {
     var selectedTabIndex by remember { mutableStateOf(0) }
 
     var tabIds by remember { mutableStateOf((1..12).toList()) }
-    val maxId by derivedStateOf { tabIds.maxOrNull() ?: 0 }
+    val maxId = remember(tabIds) { tabIds.maxOrNull() ?: 0 }
 
-    val tabs by derivedStateOf {
+    val tabs = remember(tabIds, selectedTabIndex) {
         tabIds.mapIndexed { index, id ->
             TabData.Default(
                 selected = index == selectedTabIndex,
@@ -86,9 +77,9 @@ private fun EditorTabShowcase() {
     var selectedTabIndex by remember { mutableStateOf(0) }
 
     var tabIds by remember { mutableStateOf((1..12).toList()) }
-    val maxId by derivedStateOf { tabIds.maxOrNull() ?: 0 }
+    val maxId = remember(tabIds) { tabIds.maxOrNull() ?: 0 }
 
-    val tabs by derivedStateOf {
+    val tabs = remember(tabIds, selectedTabIndex) {
         tabIds.mapIndexed { index, id ->
             TabData.Editor(
                 selected = index == selectedTabIndex,
@@ -124,38 +115,9 @@ private fun TabStripWithAddButton(
     Row(verticalAlignment = Alignment.CenterVertically) {
         TabStrip(tabs, modifier = Modifier.weight(1f))
 
-        Spacer(Modifier.width(8.dp))
-
-        var isHovered by remember { mutableStateOf(false) }
-        val interactionSource = remember { MutableInteractionSource() }
-
-        LaunchedEffect(interactionSource) {
-            interactionSource.interactions.collect { interaction ->
-                when (interaction) {
-                    is HoverInteraction.Enter -> isHovered = true
-                    is HoverInteraction.Exit -> isHovered = false
-                }
-            }
-        }
-
-        // TODO create an IconButton instead of this hack
-        val backgroundColor = if (isHovered) {
-            JewelTheme.defaultTabStyle.colors.backgroundHovered
-        } else {
-            JewelTheme.defaultTabStyle.colors.background
-        }
-
-        Box(
-            modifier = Modifier.size(JewelTheme.defaultTabStyle.metrics.tabHeight)
-                .clickable(
-                    onClick = onAddClick,
-                    onClickLabel = "Add a tab",
-                    role = Role.Button,
-                    interactionSource = interactionSource,
-                    indication = NoIndication,
-                )
-                .background(backgroundColor),
-            contentAlignment = Alignment.Center,
+        IconButton(
+            onClick = onAddClick,
+            modifier = Modifier.size(JewelTheme.defaultTabStyle.metrics.tabHeight),
         ) {
             Icon(
                 resource = "expui/general/add.svg",
