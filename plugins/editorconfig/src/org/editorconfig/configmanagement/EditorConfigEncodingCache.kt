422a4f4d3997b7f9791c5cdd57ffd748358e5db8
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-12-11 17:37:30 +0100

    IJPL-136 EditorConfigEncodingCache - use new settings controller API
    GitOrigin-RevId: 45dd1645ea2c390519cbad30e0c468a25c52b813

--- plugins/editorconfig/src/org/editorconfig/configmanagement/EditorConfigEncodingCache.kt
+++ plugins/editorconfig/src/org/editorconfig/configmanagement/EditorConfigEncodingCache.kt
@@ -1,110 +1,102 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:Suppress("ReplacePutWithAssignment")
+
 package org.editorconfig.configmanagement
 
-import com.intellij.openapi.components.*
+import com.intellij.configurationStore.SettingsSavingComponent
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.components.serviceAsync
+import com.intellij.openapi.extensions.PluginId
+import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectLocator
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileEvent
 import com.intellij.openapi.vfs.VirtualFileListener
 import com.intellij.openapi.vfs.impl.BulkVirtualFileListenerAdapter
+import com.intellij.platform.settings.CacheStateTag
+import com.intellij.platform.settings.MapSettingValueSerializerDelegate
+import com.intellij.platform.settings.SettingsController
+import com.intellij.platform.settings.settingDescriptorFactoryFactory
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import org.editorconfig.Utils
 import org.editorconfig.Utils.configValueForKey
 import org.editorconfig.plugincomponents.EditorConfigPropertiesService
-import org.jdom.Attribute
-import org.jdom.DataConversionException
-import org.jdom.Element
 import java.nio.charset.Charset
+import java.util.*
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicBoolean
 
-@State(name = "editorConfigEncodings", storages = [Storage(StoragePathMacros.CACHE_FILE)])
-internal class EditorConfigEncodingCache : PersistentStateComponent<Element?> {
-  private val charsetMap: MutableMap<String, CharsetData> = ConcurrentHashMap()
+@Service
+internal class EditorConfigEncodingCache : SettingsSavingComponent {
+  private val charsetMap: ConcurrentHashMap<String, CharsetData>
+  private val isChanged = AtomicBoolean()
 
-  override fun getState(): Element {
-    val root = Element("encodings")
-    for (url in charsetMap.keys) {
-      val charsetData = charsetMap[url] ?: continue
-        val charsetStr = ConfigEncodingCharsetUtil.toString(charsetData.charset, charsetData.isUseBom)
-        if (charsetStr != null) {
-          val entryElement = Element(ENTRY_ELEMENT)
-          val urlAttr = Attribute(URL_ATTR, url)
-          val charsetAttr = Attribute(CHARSET_ATTR, charsetStr)
-          entryElement.setAttribute(urlAttr)
-          entryElement.setAttribute(charsetAttr)
-          if (charsetData.isIgnored) {
-            entryElement.setAttribute(IGNORE_ATTR, java.lang.Boolean.toString(
-              charsetData.isIgnored))
-          }
-          root.addContent(entryElement)
-        }
+  @Suppress("SpellCheckingInspection")
+  private val setting = settingDescriptorFactoryFactory(PluginId.getId("org.editorconfig.editorconfigjetbrains"))
+    .settingDescriptor("encodings", MapSettingValueSerializerDelegate(String::class.java, CharsetData::class.java)) {
+      tags = listOf(CacheStateTag)
     }
-    return root
-  }
 
-  override fun loadState(state: Element) {
-    charsetMap.clear()
-    for (fileElement in state.getChildren(ENTRY_ELEMENT)) {
-      val urlAttr = fileElement.getAttribute(URL_ATTR)
-      val charsetAttr = fileElement.getAttribute(CHARSET_ATTR)
-      if (urlAttr != null && charsetAttr != null) {
-        val url = urlAttr.value
-        val charsetStr = charsetAttr.value
-        val charset = ConfigEncodingCharsetUtil.toCharset(charsetStr)
-        val useBom = ConfigEncodingCharsetUtil.UTF8_BOM_ENCODING == charsetStr
-        if (charset != null) {
-          val charsetData = CharsetData(charset, useBom)
-          charsetMap[url] = charsetData
-          val ignoreAttr = fileElement.getAttribute(IGNORE_ATTR)
-          if (ignoreAttr != null) {
-            try {
-              charsetData.isIgnored = ignoreAttr.booleanValue
-            }
-            catch (e: DataConversionException) {
-              // Ignore, do not set
-            }
-          }
-        }
-      }
+  init {
+    charsetMap = runBlockingCancellable {
+      ConcurrentHashMap(serviceAsync<SettingsController>().getItem(setting) ?: emptyMap())
     }
   }
 
-  fun getUseUtf8Bom(project: Project?, virtualFile: VirtualFile): Boolean = getCharsetData(project, virtualFile, true)?.isUseBom ?: false
+  override suspend fun save() {
+    if (isChanged.compareAndSet(true, false)) {
+      serviceAsync<SettingsController>().setItem(setting, TreeMap(charsetMap))
+    }
+  }
+
+  fun getUseUtf8Bom(project: Project?, virtualFile: VirtualFile): Boolean {
+    return getCharsetData(project = project, virtualFile = virtualFile, withCache = true)?.isUseBom ?: false
+  }
 
   fun getCharsetData(project: Project?, virtualFile: VirtualFile, withCache: Boolean): CharsetData? {
-    if (project == null || !Utils.isEnabledFor (project, virtualFile)) return null
-    if (withCache) {
-      val cached = getCachedCharsetData(virtualFile)
-      if (cached != null) return cached
+    if (project == null || !Utils.isEnabledFor(project, virtualFile)) {
+      return null
     }
-    return computeCharsetData(project, virtualFile)
+
+    if (withCache) {
+      getCachedCharsetData(virtualFile)?.let {
+        return it
+      }
+    }
+    return computeCharsetData(project = project, virtualFile = virtualFile)
   }
 
   fun computeAndCacheEncoding(project: Project, virtualFile: VirtualFile) {
     val key = getKey(virtualFile)
-    val charsetData = getCharsetData(project, virtualFile, false)
+    val charsetData = getCharsetData(project = project, virtualFile = virtualFile, withCache = false)
     if (charsetData != null) {
-      charsetMap[key] = charsetData
-      virtualFile.charset = charsetData.charset
+      charsetMap.put(key, charsetData)
+      isChanged.set(true)
+      charsetData.getCharset()?.let {
+        virtualFile.charset = it
+      }
     }
   }
 
   fun getCachedEncoding(virtualFile: VirtualFile): Charset? {
     val charsetData = getCachedCharsetData(virtualFile)
-    return if (charsetData != null && !charsetData.isIgnored) charsetData.charset else null
+    return if (charsetData == null || charsetData.isIgnored) null else charsetData.getCharset()
   }
 
   private fun getCachedCharsetData(virtualFile: VirtualFile): CharsetData? = charsetMap[getKey(virtualFile)]
 
-  fun isIgnored(virtualFile: VirtualFile): Boolean =
-    getCachedCharsetData(virtualFile).let { it != null && it.isIgnored }
+  fun isIgnored(virtualFile: VirtualFile): Boolean = getCachedCharsetData(virtualFile).let { it != null && it.isIgnored }
 
   fun setIgnored(virtualFile: VirtualFile) {
     var charsetData = getCachedCharsetData(virtualFile)
     if (charsetData == null) {
-      charsetData = CharsetData(Charset.defaultCharset(), false)
+      charsetData = CharsetData(charset = CharsetRef(id = null))
       charsetData.isIgnored = true
-      charsetMap[getKey(virtualFile)] = charsetData
+      charsetMap.put(getKey(virtualFile), charsetData)
+      isChanged.set(true)
     }
     else {
       charsetData.isIgnored = true
@@ -113,10 +105,7 @@ internal class EditorConfigEncodingCache : PersistentStateComponent<Element?> {
 
   fun reset() {
     charsetMap.clear()
-  }
-
-  class CharsetData(val charset: Charset, val isUseBom: Boolean) {
-    var isIgnored = false
+    isChanged.set(true)
   }
 
   class VfsListener : BulkVirtualFileListenerAdapter(object : VirtualFileListener {
@@ -130,27 +119,39 @@ internal class EditorConfigEncodingCache : PersistentStateComponent<Element?> {
   })
 
   companion object {
-    private const val ENTRY_ELEMENT = "file"
-    private const val URL_ATTR = "url"
-    private const val CHARSET_ATTR = "charset"
-    private const val IGNORE_ATTR = "ignore"
-
-    @JvmStatic
     fun getInstance(): EditorConfigEncodingCache = service()
 
     private fun computeCharsetData(project: Project, virtualFile: VirtualFile): CharsetData? {
       val properties = EditorConfigPropertiesService.getInstance(project).getProperties(virtualFile)
-      val charsetStr = properties.configValueForKey(ConfigEncodingCharsetUtil.charsetKey)
-      if (!charsetStr.isEmpty()) {
-        val charset = ConfigEncodingCharsetUtil.toCharset(charsetStr)
-        val useBom = ConfigEncodingCharsetUtil.UTF8_BOM_ENCODING == charsetStr
-        if (charset != null) {
-          return CharsetData(charset, useBom)
-        }
-      }
-      return null
+      val charsetStr = properties.configValueForKey(ConfigEncodingCharsetUtil.charsetKey).takeIf { it.isNotEmpty() } ?: return null
+      val charset = ConfigEncodingCharsetUtil.toCharset(charsetStr) ?: return null
+      val charsetRef = CharsetRef(charsetStr)
+      charsetRef.charset = charset
+      return CharsetData(charset = charsetRef)
     }
 
     private fun getKey(virtualFile: VirtualFile): String = virtualFile.url
+  }
+}
+
+@Serializable
+internal data class CharsetRef(@JvmField val id: String?) {
+  @Transient
+  @JvmField
+  var charset: Charset? = null
+}
+
+@Serializable
+internal class CharsetData(@JvmField val charset: CharsetRef, @JvmField var isIgnored: Boolean = false) {
+  val isUseBom: Boolean
+    get() = charset.id == ConfigEncodingCharsetUtil.UTF8_BOM_ENCODING
+
+  fun getCharset(): Charset? {
+    var result = charset.charset
+    if (result == null) {
+      result = charset.id?.let { ConfigEncodingCharsetUtil.toCharset(charset.id) }
+      charset.charset = result
+    }
+    return result
   }
 }
