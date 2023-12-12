aa35a06958e82ed1b7b2258975c7e6cd896d8b37
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2023-12-12 19:48:56 +0100

    `intellij.platform.util.rt.java8`: review & dump API
    GitOrigin-RevId: 47d1e1e1838d6bb2910bedb05577b757728ee160

---
+++ platform/util/rt-java8/src/com/intellij/util/lang/package-info.java
@@ -0,0 +1,5 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@Internal
+package com.intellij.util.lang;
+
+import org.jetbrains.annotations.ApiStatus.Internal;
