f3dff1bcde4899a1c21b4cd42672234058471692
Vladislav.Yaroshchuk <vladislav.yaroshchuk@jetbrains.com>
2023-12-12 09:08:04 +0400

    IDEA-340564 Revert "[maven] IDEA-340220 fix flaky test MavenImportWizardTest.testImportProjectWithWrapper"
    This reverts commit 621c2f600d199a1f41e7fbfd2e1dd121535d06e0.
    
    GitOrigin-RevId: 882814ec28b1ae60cb37d35e4a887d0f0f48168e

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/wizards/MavenProjectWizardTestCase.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/wizards/MavenProjectWizardTestCase.kt
@@ -4,22 +4,15 @@ package org.jetbrains.idea.maven.wizards
 import com.intellij.ide.projectWizard.ProjectWizardTestCase
 import com.intellij.ide.util.newProjectWizard.AbstractProjectWizard
 import com.intellij.maven.testFramework.MavenTestCase
-import com.intellij.maven.testFramework.assertWithinTimeout
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.module.Module
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.impl.JavaAwareProjectJdkTableImpl
-import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
 import com.intellij.util.io.write
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
-import org.jetbrains.idea.maven.project.MavenSyncListener
 import org.jetbrains.idea.maven.server.MavenServerManager
-import org.jetbrains.idea.maven.utils.MavenLog
 import java.nio.file.Path
-import java.util.concurrent.atomic.AtomicBoolean
 
 abstract class MavenProjectWizardTestCase : ProjectWizardTestCase<AbstractProjectWizard>() {
   override fun runInDispatchThread() = false
@@ -58,48 +51,15 @@ abstract class MavenProjectWizardTestCase : ProjectWizardTestCase<AbstractProjec
   }
 
   protected suspend fun importProjectFrom(path: Path): Module {
-    return waitForImportWithinTimeout {
-      withContext(Dispatchers.EDT) {
-        importProjectFrom(path.toString(), null, MavenProjectImportProvider())
-      }
+    return withContext(Dispatchers.EDT) {
+      importProjectFrom(path.toString(), null, MavenProjectImportProvider())
     }
   }
 
   protected suspend fun importModuleFrom(path: Path): Module {
-    return waitForImportWithinTimeout {
-      withContext(Dispatchers.EDT) {
-        importModuleFrom(MavenProjectImportProvider(), path.toString())
-      }
+    return withContext(Dispatchers.EDT) {
+      importModuleFrom(MavenProjectImportProvider(), path.toString())
     }
-  }
-
-  @RequiresBackgroundThread
-  protected suspend fun <T> waitForImportWithinTimeout(action: suspend () -> T): T {
-    MavenLog.LOG.warn("waitForImportWithinTimeout started")
-    val syncStarted = AtomicBoolean(false)
-    val syncFinished = AtomicBoolean(false)
-    ApplicationManager.getApplication().messageBus.connect(testRootDisposable)
-      .subscribe(MavenSyncListener.TOPIC, object : MavenSyncListener {
-        override fun syncStarted(project: Project) {
-          syncStarted.set(true)
-        }
-
-        override fun syncFinished(project: Project) {
-          syncFinished.set(true)
-        }
-      })
-
-    val result = action()
-
-    assertWithinTimeout {
-      assertTrue(
-        syncStarted.get()
-        && syncFinished.get()
-      )
-      MavenLog.LOG.warn("waitForImportWithinTimeout finished")
-    }
-
-    return result
   }
 
 }
