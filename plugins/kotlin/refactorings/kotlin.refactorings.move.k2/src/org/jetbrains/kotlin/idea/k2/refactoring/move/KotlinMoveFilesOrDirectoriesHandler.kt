48c288644cca85fb6e77bbe5a8c60ef103707631
Anna Kozlova <anna.kozlova@jetbrains.com>
2023-12-16 17:41:01 +0100

    [kotlin] k2: prepare pure move kotlin file from project view
    to disable smart move, comment K2MoveHandler and K2MoveFilesHandler, and uncomment KotlinMoveFilesOrDirectoriesHandler
    ^KTIJ-28234 fixed
    
    GitOrigin-RevId: e689c2b1adcd37ccc60419b29dc4433c4e7695b0

---
+++ plugins/kotlin/refactorings/kotlin.refactorings.move.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/move/KotlinMoveFilesOrDirectoriesHandler.kt
@@ -0,0 +1,43 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.k2.refactoring.move
+
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileSystemItem
+import com.intellij.psi.PsiReference
+import com.intellij.refactoring.move.moveFilesOrDirectories.MoveFilesOrDirectoriesHandler
+import org.jetbrains.kotlin.psi.KtClassOrObject
+import org.jetbrains.kotlin.psi.KtFile
+
+/**
+ * Currently not registered, requited for the case we would like to disable smart move in the last moment
+ */
+class KotlinMoveFilesOrDirectoriesHandler : MoveFilesOrDirectoriesHandler() {
+    private fun adjustElements(elements: Array<out PsiElement>): Array<PsiFileSystemItem>? {
+        return elements.map {
+            when {
+                it is PsiFile -> it
+                it is PsiDirectory -> it
+                it is KtClassOrObject && it.parent is KtFile -> it.parent as KtFile
+                else -> return null
+            }
+        }.toTypedArray()
+    }
+
+    override fun canMove(elements: Array<out PsiElement>, targetContainer: PsiElement?, reference: PsiReference?): Boolean {
+        val adjustedElements = adjustElements(elements) ?: return false
+        if (adjustedElements.none { it is KtFile }) return false
+
+        return super.canMove(adjustedElements, targetContainer, reference)
+    }
+
+    override fun adjustForMove(
+        project: Project,
+        sourceElements: Array<out PsiElement>,
+        targetElement: PsiElement?
+    ): Array<PsiFileSystemItem>? {
+        return adjustElements(sourceElements)
+    }
+}
