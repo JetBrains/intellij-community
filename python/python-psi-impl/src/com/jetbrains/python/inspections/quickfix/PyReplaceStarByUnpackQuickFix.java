58cf31d9f80ea811e427dd38aa23b70d6adf37ba
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2023-12-18 14:39:57 +0200

    Migrate PyRemoveWithTargetQuickFix and PyReplaceStarByUnpackQuickFix to ModCommand
    PY-65297
    
    GitOrigin-RevId: 1530cfd4c31b48f980953538f3211c41902076bd

--- python/python-psi-impl/src/com/jetbrains/python/inspections/quickfix/PyReplaceStarByUnpackQuickFix.java
+++ python/python-psi-impl/src/com/jetbrains/python/inspections/quickfix/PyReplaceStarByUnpackQuickFix.java
@@ -1,7 +1,7 @@
 package com.jetbrains.python.inspections.quickfix;
 
-import com.intellij.codeInspection.LocalQuickFix;
-import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.modcommand.ModPsiUpdater;
+import com.intellij.modcommand.PsiUpdateModCommandQuickFix;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -10,7 +10,7 @@ import com.jetbrains.python.psi.PyExpression;
 import com.jetbrains.python.psi.PyStarExpression;
 import org.jetbrains.annotations.NotNull;
 
-public class PyReplaceStarByUnpackQuickFix implements LocalQuickFix {
+public class PyReplaceStarByUnpackQuickFix extends PsiUpdateModCommandQuickFix {
   @NotNull
   @Override
   public String getFamilyName() {
@@ -18,11 +18,9 @@ public class PyReplaceStarByUnpackQuickFix implements LocalQuickFix {
   }
 
   @Override
-  public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
-    PsiElement element = descriptor.getPsiElement();
-    if (!(element instanceof PyStarExpression)) return;
+  public void applyFix(@NotNull Project project, @NotNull PsiElement element, @NotNull ModPsiUpdater updater) {
+    if (!(element instanceof PyStarExpression starExpression)) return;
 
-    PyStarExpression starExpression = (PyStarExpression)element;
     PyExpression expression = starExpression.getExpression();
     if (expression == null) return;
     PsiFile file = starExpression.getContainingFile();
