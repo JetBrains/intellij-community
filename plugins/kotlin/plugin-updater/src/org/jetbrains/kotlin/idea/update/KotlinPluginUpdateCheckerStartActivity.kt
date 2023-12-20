b2cfbd80b04a82346f58f030c94c9537b3a65773
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-12-20 10:43:46 +0100

    [kotlin] 1.7.20 EAP notification is outdated
    GitOrigin-RevId: 46233e2c2a19d9015a76bb476c23edae5429e959

--- plugins/kotlin/plugin-updater/src/org/jetbrains/kotlin/idea/update/KotlinPluginUpdateCheckerStartActivity.kt
+++ plugins/kotlin/plugin-updater/src/org/jetbrains/kotlin/idea/update/KotlinPluginUpdateCheckerStartActivity.kt
@@ -1,10 +1,6 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.update
 
-import com.intellij.ide.util.RunOnceUtil
-import com.intellij.notification.BrowseNotificationAction
-import com.intellij.notification.NotificationGroupManager
-import com.intellij.notification.NotificationType
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.event.DocumentListener
@@ -12,16 +8,13 @@ import com.intellij.openapi.extensions.ExtensionNotApplicableException
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
-import org.jetbrains.kotlin.idea.KotlinIcons
 import org.jetbrains.kotlin.idea.KotlinPluginUpdater
-import org.jetbrains.kotlin.idea.compiler.configuration.IdeKotlinVersion
-import org.jetbrains.kotlin.idea.compiler.configuration.KotlinPluginLayout
 import org.jetbrains.kotlin.idea.core.KotlinPluginDisposable
 import org.jetbrains.kotlin.idea.util.application.isHeadlessEnvironment
 import org.jetbrains.kotlin.idea.util.application.isUnitTestMode
 import org.jetbrains.kotlin.idea.util.isKotlinFileType
 
-class KotlinPluginUpdateCheckerStartActivity : ProjectActivity {
+internal class KotlinPluginUpdateCheckerStartActivity : ProjectActivity {
     init {
         if (isUnitTestMode() || isHeadlessEnvironment()) {
             throw ExtensionNotApplicableException.create()
@@ -34,7 +27,6 @@ class KotlinPluginUpdateCheckerStartActivity : ProjectActivity {
                 FileDocumentManager.getInstance().getFile(e.document)?.let { virtualFile ->
                     if (virtualFile.isKotlinFileType() && virtualFile.isInLocalFileSystem) {
                         KotlinPluginUpdater.getInstance().pluginUsed()
-                        showEapAdvertisementNotification()
                     }
                 }
             }
@@ -42,31 +34,5 @@ class KotlinPluginUpdateCheckerStartActivity : ProjectActivity {
 
         EditorFactory.getInstance().eventMulticaster
             .addDocumentListener(documentListener, KotlinPluginDisposable.getInstance(project))
-    }
-}
-
-private fun showEapAdvertisementNotification() {
-    val compilerVersion = KotlinPluginLayout.ideCompilerVersion
-    if (compilerVersion.kotlinVersion != KotlinVersion(major = 1, minor = 7, patch = 20)) return
-    if (compilerVersion.kind != IdeKotlinVersion.Kind.Beta(number = 1) && compilerVersion.kind != IdeKotlinVersion.Kind.Beta(number = null)) return
-
-    RunOnceUtil.runOnceForApp("kotlin.eap.advertisement.was.shown.once") {
-        NotificationGroupManager.getInstance()
-            .getNotificationGroup("Kotlin EAP")
-            .createNotification(
-                KotlinPluginUpdaterBundle.message("kotlin.eap.advertisement.notification.title"),
-                KotlinPluginUpdaterBundle.message("kotlin.eap.advertisement.notification.text"),
-                NotificationType.INFORMATION,
-            )
-            .addAction(
-                BrowseNotificationAction(
-                    KotlinPluginUpdaterBundle.message("kotlin.eap.advertisement.notification.action"),
-                    KotlinPluginUpdaterBundle.message("kotlin.eap.advertisement.notification.link"),
-                )
-            )
-            .setSuggestionType(true)
-            .setIcon(KotlinIcons.SMALL_LOGO)
-            .setImportant(true)
-            .notify(null)
     }
 }
