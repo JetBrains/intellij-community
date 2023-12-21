83698da9238059efaeaaa03d01d5a7e2cd454abb
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2023-12-21 11:10:16 +0100

    IDEA-341052 Welcome Screen: Reset zoom and decrease IDE zoom behave differently
    The bug was happening, because UpdateScaleHelper used JBUIScale value to updateUI whenever it changed. But JBUIScale value is discrete by 0.25 step. So 100% and 110% zoom levels had the same user scale value and updateUI wasn't called.
    
    GitOrigin-RevId: 62a2a0938825715dc2a305abb478e72b3406a575

--- platform/util/ui/src/com/intellij/util/ui/UpdateScaleHelper.kt
+++ platform/util/ui/src/com/intellij/util/ui/UpdateScaleHelper.kt
@@ -1,13 +1,15 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.ui
 
-import com.intellij.ui.scale.JBUIScale
 import org.jetbrains.annotations.ApiStatus.Internal
 import javax.swing.JComponent
+import javax.swing.UIManager
 
 @Internal
-open class UpdateScaleHelper(val forceInitialRun: Boolean = false, val currentScale: (() -> Float) = { JBUIScale.scale(1f) }) {
-  private var savedScale: Float = currentScale()
+open class UpdateScaleHelper(val forceInitialRun: Boolean = false, val currentValue: (() -> Float) = {
+  UIManager.getFont("Label.font").size2D
+}) {
+  private var savedValue: Float = currentValue()
   private var initialRunPerformed = false
 
   fun saveScaleAndRunIfChanged(runnable: Runnable): Boolean =
@@ -15,14 +17,14 @@ open class UpdateScaleHelper(val forceInitialRun: Boolean = false, val currentSc
 
   fun saveScaleAndRunIfChanged(block: () -> Unit): Boolean {
     if ((!forceInitialRun || initialRunPerformed)
-        && savedScale == currentScale()) return false
+        && savedValue == currentValue()) return false
 
     try {
       block()
     }
     finally {
       initialRunPerformed = true
-      savedScale = currentScale()
+      savedValue = currentValue()
     }
     return true
   }
