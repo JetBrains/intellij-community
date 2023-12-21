6a308b21a00908dc62e7339da90c57ce348da54e
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2023-12-21 21:28:46 +0100

    Migrate python copyright to v2
    GitOrigin-RevId: bb3d02627319556681d4a192d83df4a2bfed21e1

--- python/python-copyright/src/com/intellij/python/copyright/PyCopyright.kt
+++ python/python-copyright/src/com/intellij/python/copyright/PyCopyright.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.jetbrains.python.copyright
+package com.intellij.python.copyright
 
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.module.Module
--- python/testSrc/com/intellij/python/copyright/PyCopyrightTest.kt
+++ python/testSrc/com/intellij/python/copyright/PyCopyrightTest.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.jetbrains.python.copyright
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.python.copyright
 
 import com.jetbrains.python.fixtures.PyTestCase
 import com.maddyhome.idea.copyright.CopyrightProfile
