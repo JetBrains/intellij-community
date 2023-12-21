b32b9c45284f3c92a1fcd72d5afd2fafdc990a13
Jacob Logsdon <jlogs@google.com>
2023-12-21 12:45:05 -0800

    Revert "Bind created menu bar to the IdeFrame"
    This reverts commit be24add1742188e6ec574c8d80c37c77b8f0a3cb.
    
    This actually breaks UI tests on 2023.3.2.
    
    Fixes: 316226315
    Fixes: 317389186
    Change-Id: Iaff48d1a4b901ed01ac68efe7782e2a7d7d937c5

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/IdeRootPane.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/IdeRootPane.kt
@@ -855,15 +855,7 @@ private fun createMacAwareMenuBar(frame: JFrame,
     return ideMenu
   }
   else {
-    // Android Studio (b/316226315): this is a temporary workaround for missing menu bars in UI tests.
-    // The root cause is IDEA-337548: "No main menu on simple linux with Xvfb".
-    // This workaround should be reverted as soon as IDEA-337548 is fixed upstream.
-    val menuBar = createMenuBar(coroutineScope, frame, mainMenuActionGroup)
-    if (System.getProperty("idea.gui.test.port") != null) {
-      frame.jMenuBar = menuBar
-      component.jMenuBar = menuBar
-    }
-    return menuBar
+    return createMenuBar(coroutineScope, frame, mainMenuActionGroup)
   }
 }
 
