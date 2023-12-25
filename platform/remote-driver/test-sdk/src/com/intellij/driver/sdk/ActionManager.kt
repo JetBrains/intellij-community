75086ab44635b77e3947fe9f69ae10928fb24f41
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-12-25 20:23:19 +0100

    [testing] Cleanup ActionManager
    GitOrigin-RevId: e798aff67d2648e6b848aa877cf3927cc73fd4e4

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ActionManager.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ActionManager.kt
@@ -2,23 +2,15 @@ package com.intellij.driver.sdk
 
 import com.intellij.driver.client.Driver
 import com.intellij.driver.client.Remote
-import com.intellij.driver.client.utility
+import com.intellij.driver.client.service
 import com.intellij.driver.sdk.ui.remote.Component
 import java.awt.event.InputEvent
-
-
-fun Driver.invokeAction(actionId: String, now: Boolean = true) {
-  val actionManager = utility<ActionManager>().getInstance()
-  val action = actionManager.getAction(actionId)
-  withWriteAction {
-    actionManager.tryToExecute(action, null, null, null, now)
-  }
-}
 
 @Remote(value = "com.intellij.openapi.actionSystem.ActionManager")
 interface ActionManager {
   fun getInstance(): ActionManager
   fun getAction(actionId: String): AnAction
+
   fun tryToExecute(action: AnAction,
                    inputEvent: InputEvent?,
                    contextComponent: Component?,
@@ -28,5 +20,14 @@ interface ActionManager {
 
 @Remote(value = "com.intellij.openapi.actionSystem.AnAction")
 interface AnAction
+
 @Remote(value = "com.intellij.openapi.util.ActionCallback")
 interface ActionCallback
+
+fun Driver.invokeAction(actionId: String) {
+  withWriteAction {
+    val actionManager = service<ActionManager>()
+    val action = actionManager.getAction(actionId)
+    actionManager.tryToExecute(action, null, null, null, true)
+  }
+}
