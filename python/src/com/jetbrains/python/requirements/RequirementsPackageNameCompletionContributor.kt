9e5f2fa1f7acec47e0e45613a073c3fcf1ef828b
lada.gagina <lada.gagina@jetbrains.com>
2023-12-26 19:52:16 +0100

    [python] Add completion for poetry dependencies
    Add completion for package names and versions in [tool.poetry.dependencies] table
    
    GitOrigin-RevId: 090b15e2be829511b0d0147a8948b2d0529c4568

--- python/src/com/jetbrains/python/requirements/RequirementsPackageNameCompletionContributor.kt
+++ python/src/com/jetbrains/python/requirements/RequirementsPackageNameCompletionContributor.kt
@@ -1,14 +1,11 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.requirements
 
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionResultSet
-import com.intellij.codeInsight.completion.PrioritizedLookupElement
-import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.jetbrains.python.packaging.management.PythonPackageManager
+import com.intellij.python.community.impl.requirements.completePackageNames
 import com.jetbrains.python.requirements.psi.SimpleName
-import com.jetbrains.python.sdk.pythonSdk
-import icons.PythonIcons
 
 class RequirementsPackageNameCompletionContributor : CompletionContributor() {
 
@@ -18,14 +15,7 @@ class RequirementsPackageNameCompletionContributor : CompletionContributor() {
     val project = position.project
 
     if (parent is SimpleName) {
-      val repositoryManager = PythonPackageManager.forSdk(project, project.pythonSdk ?: return).repositoryManager
-      val packages = repositoryManager.allPackages()
-      val maxPriority = packages.size
-      packages.asSequence().map {
-        LookupElementBuilder.create(it.lowercase()).withIcon(PythonIcons.Python.Python)
-      }.mapIndexed { index, lookupElementBuilder ->
-        PrioritizedLookupElement.withPriority(lookupElementBuilder, (maxPriority - index).toDouble())
-      }.forEach { result.addElement(it) }
+      completePackageNames(project, result)
     }
   }
 }
