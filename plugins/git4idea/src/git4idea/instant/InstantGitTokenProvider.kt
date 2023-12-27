f4d2f4398ebaeef10ced9ecf38ac7ddaa5d34d46
Semyon Proshev <semyon.proshev@jetbrains.com>
2023-12-27 20:21:25 +0100

    Update InstantGitTokenProvider API to support providing auth headers (TBE-3092)
    GitOrigin-RevId: 4918b25ac60c75b24fa0dfaf73068561365ba6fc

--- plugins/git4idea/src/git4idea/instant/InstantGitTokenProvider.kt
+++ plugins/git4idea/src/git4idea/instant/InstantGitTokenProvider.kt
@@ -7,14 +7,21 @@ import org.jetbrains.annotations.ApiStatus.Internal
 
 @Experimental
 @Internal
+/**
+ * If authentication is done via bearer token, [git4idea.instant.InstantGitTokenProvider.getToken] could be implemented,
+ * otherwise, [git4idea.instant.InstantGitTokenProvider.getAuthHeaders] should provide authentication headers.
+ */
 interface InstantGitTokenProvider {
 
   companion object {
     val EP_NAME = ExtensionPointName.create<InstantGitTokenProvider>("Git4Idea.instantGitTokenProvider")
   }
 
-  /**
-   * Use it as "Authorization" to "Bearer $token" header.
-   */
-  fun getToken(): String?
+  fun getToken(): String? {
+    return null
+  }
+
+  fun getAuthHeaders(): Map<String, String>? {
+    return getToken()?.let { mapOf("Authorization" to "Bearer $it") }
+  }
 }
