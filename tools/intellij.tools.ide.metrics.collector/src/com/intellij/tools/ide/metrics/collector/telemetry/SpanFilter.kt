9f55b30c8328d54f015ca4aa8cb39e478d047581
Alexey Kryuchkov <alexey.kryuchkov@jetbrains.com>
2023-12-28 14:55:06 +0400

    AT-692 Enabling + collecting of SmallDataPack for vcs commit test
    GitOrigin-RevId: f603b118829af9f457418469a7c4dcbaef293c91

--- tools/intellij.tools.ide.metrics.collector/src/com/intellij/tools/ide/metrics/collector/telemetry/SpanFilter.kt
+++ tools/intellij.tools.ide.metrics.collector/src/com/intellij/tools/ide/metrics/collector/telemetry/SpanFilter.kt
@@ -4,6 +4,7 @@ class SpanFilter(val filter: (SpanElement) -> Boolean) {
   companion object {
     fun nameEquals(name: String) = SpanFilter { it.name == name }
     fun containsNameIn(names: List<String>) = SpanFilter { it.name in names }
+    fun containsNameIn(vararg names: String) = SpanFilter { it.name in names }
     fun nameContains(substring: String) = SpanFilter { it.name.contains(substring) }
   }
 }
