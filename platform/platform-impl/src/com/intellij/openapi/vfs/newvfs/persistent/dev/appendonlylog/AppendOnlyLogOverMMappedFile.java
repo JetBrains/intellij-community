2a0462cc3d0a172146296f9c34297addad902670
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2024-01-02 21:16:14 +0100

    IJPL-428: [wip] AppendOnlyLogOverMMappedFile recover recordsCount() on crash
    + fixed omission
    
    GitOrigin-RevId: 80404d2913f3c6f24f1182764aa0dd3b32808d08

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/dev/appendonlylog/AppendOnlyLogOverMMappedFile.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/dev/appendonlylog/AppendOnlyLogOverMMappedFile.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vfs.newvfs.persistent.dev.appendonlylog;
 
 import com.intellij.openapi.util.IntRef;
@@ -416,6 +416,7 @@ public final class AppendOnlyLogOverMMappedFile implements AppendOnlyLog, Unmapp
       //          count is actually lagging behind.
       IntRef recordsCount = new IntRef(0);
       forEachRecord((recordId, buffer) -> {
+        recordsCount.inc();
         return true;
       }, successfullyRecoveredUntil);
       setIntHeaderField(HeaderLayout.RECORDS_COUNT_OFFSET, recordsCount.get());
