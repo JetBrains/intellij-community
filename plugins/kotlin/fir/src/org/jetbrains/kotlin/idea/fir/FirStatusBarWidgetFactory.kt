3174321568f03f7c5975030d0821d8d8c5a6d92f
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2024-01-02 12:26:02 +0100

    [kotlin] consider that `kotlin.k2.show.fir.statusbar.icon` registry key is set to `false` when it's not defined
    ^KTIJ-28331=
    
    GitOrigin-RevId: b9a088a36489d1cab704568f2ac43903846c574d

--- plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/fir/FirStatusBarWidgetFactory.kt
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/fir/FirStatusBarWidgetFactory.kt
@@ -22,7 +22,7 @@ private class FirStatusBarWidgetFactory : StatusBarWidgetFactory {
     override fun getDisplayName(): String = DISPLAY_NAME
 
     override fun isAvailable(project: Project): Boolean {
-        return Registry.`is`(REGISTRY_KEY)
+        return Registry.`is`(REGISTRY_KEY, /* defaultValue = */ false)
     }
 
     override fun createWidget(project: Project): StatusBarWidget = Widget()
