954f816a75bdbb78faf81a63d5a48e7031c41e3c
Alex Plate <aleksei.plate@jetbrains.com>
2024-01-04 19:11:35 +0200

    [Workspace Model] [IDEA-342174] Comment out debug comment line because it may cause disposed exception
    GitOrigin-RevId: 29a7fbd604442e63edb66fdf5c546ea998d3ff16

--- platform/projectModel-impl/src/com/intellij/workspaceModel/ide/impl/legacyBridge/library/GlobalLibraryTableDelegate.kt
+++ platform/projectModel-impl/src/com/intellij/workspaceModel/ide/impl/legacyBridge/library/GlobalLibraryTableDelegate.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.workspaceModel.ide.impl.legacyBridge.library
 
 import com.intellij.openapi.Disposable
@@ -96,7 +96,7 @@ internal class GlobalLibraryTableDelegate(private val libraryTable: LibraryTable
 
     for (change in removeChanges) {
       val library = event.storageBefore.libraryMap.getDataByEntity(change.entity)
-      LOG.debug { "Fire 'beforeLibraryRemoved' event for ${change.entity.name}, library = $library" }
+      //LOG.debug { "Fire 'beforeLibraryRemoved' event for ${change.entity.name}, library = $library" }
       if (library != null) {
         dispatcher.multicaster.beforeLibraryRemoved(library)
       }
