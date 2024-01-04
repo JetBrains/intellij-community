5ffef21950d2d3ac7d11090ddafd79dac0eeb257
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2024-01-04 14:55:51 +0100

    [maven] IDEA-93855 test maven shade plugin uber jar dependency
    GitOrigin-RevId: 8b874922e4e8d056fce18adf51e134b25d4b4219

---
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenShadePluginConfiguratorTest.kt
@@ -0,0 +1,61 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.idea.maven.importing
+
+import com.intellij.maven.testFramework.MavenMultiVersionImportingTestCase
+import kotlinx.coroutines.runBlocking
+import org.junit.Assume.assumeTrue
+import org.junit.Test
+import java.nio.file.Files
+import java.nio.file.Path
+
+class MavenShadePluginConfiguratorTest : MavenMultiVersionImportingTestCase() {
+  @Test
+  fun `test maven shade plugin uber jar dependency`() = runBlocking {
+    assumeTrue(isWorkspaceImport)
+
+    createProjectPom("""
+                       <groupId>test</groupId>
+                       <artifactId>project</artifactId>
+                       <packaging>pom</packaging>
+                       <version>1</version>
+                       <modules>
+                         <module>m1</module>
+                         <module>m2</module>
+                       </modules>
+                       """.trimIndent())
+
+    createModulePom("m1", """
+      <groupId>test</groupId>
+      <artifactId>m1</artifactId>
+      <version>1</version>
+      <dependencies>
+        <dependency>
+          <groupId>test</groupId>
+          <artifactId>m2</artifactId>
+          <version>1</version>
+        </dependency>
+      </dependencies>
+      """.trimIndent())
+
+    val m2 = createModulePom("m2", """
+      <groupId>test</groupId>
+      <artifactId>m2</artifactId>
+      <version>1</version>
+      <build>
+        <plugins>
+          <plugin>
+            <groupId>org.apache.maven.plugins</groupId>
+            <artifactId>maven-shade-plugin</artifactId>
+          </plugin>                  
+        </plugins>    
+      </build>  
+      """.trimIndent())
+
+    importProjectAsync()
+
+    val uberJarPath = m2.parent.path + "/target/m2-1.jar"
+    assertModuleLibDep("m1", "Maven Shade: test:m2:1",
+                       "jar://$uberJarPath!/")
+    assertTrue(Files.exists(Path.of(uberJarPath)));
+  }
+}
