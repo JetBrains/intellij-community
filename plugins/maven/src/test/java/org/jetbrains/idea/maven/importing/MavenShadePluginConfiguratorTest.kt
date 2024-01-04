fc641d1b832c407999042f717ca35980bb36897f
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2024-01-04 23:46:27 +0100

    [maven] IDEA-93855 do not generate uber jar by default
    GitOrigin-RevId: 591107f8418f17963af561dba7db4ae3ad874d90

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenShadePluginConfiguratorTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenShadePluginConfiguratorTest.kt
@@ -2,6 +2,7 @@
 package org.jetbrains.idea.maven.importing
 
 import com.intellij.maven.testFramework.MavenMultiVersionImportingTestCase
+import com.intellij.openapi.util.registry.Registry
 import kotlinx.coroutines.runBlocking
 import org.junit.Assume.assumeTrue
 import org.junit.Test
@@ -56,6 +57,9 @@ class MavenShadePluginConfiguratorTest : MavenMultiVersionImportingTestCase() {
     val uberJarPath = m2.parent.path + "/target/m2-1.jar"
     assertModuleLibDep("m1", "Maven Shade: test:m2:1",
                        "jar://$uberJarPath!/")
+
+    Registry.get("maven.shade.plugin.generate.uber.jar").setValue("true", testRootDisposable)
+    updateAllProjects()
     assertTrue(Files.exists(Path.of(uberJarPath)));
   }
 }
