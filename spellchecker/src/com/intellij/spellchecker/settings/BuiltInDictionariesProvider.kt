86c8fa3545bfc50593e3ccc57ff126bae7a5b970
Den Mukhametianov <den.mukhametianov@gmail.com>
2024-01-05 15:04:13 +0100

    [spellchecker] added extension point for specifying additional built-in dictionaries
    Used in Rider to show and edit R# spell checker user words
    
    GitOrigin-RevId: b1ee8bdf064364800a712400af1c3e7095f306ac

---
+++ spellchecker/src/com/intellij/spellchecker/settings/BuiltInDictionariesProvider.kt
@@ -0,0 +1,16 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.spellchecker.settings
+
+import com.intellij.openapi.extensions.ExtensionPointName
+
+abstract class BuiltInDictionariesProvider {
+  abstract fun getDictionaries(): List<BuiltInDictionary>
+
+  companion object {
+    val EP_NAME = ExtensionPointName.create<BuiltInDictionariesProvider>("com.intellij.spellchecker.builtInDictionariesProvider")
+
+    fun getAll(): List<BuiltInDictionariesProvider> {
+      return EP_NAME.extensionList
+    }
+  }
+}
---
+++ spellchecker/src/com/intellij/spellchecker/settings/BuiltInDictionary.kt
@@ -0,0 +1,9 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.spellchecker.settings
+
+import com.intellij.openapi.project.Project
+
+interface BuiltInDictionary {
+  fun openDictionaryInEditor(project: Project)
+  val name: String
+}
