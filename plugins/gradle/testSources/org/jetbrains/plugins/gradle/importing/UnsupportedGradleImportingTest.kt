a43cf590c868622d1af5e8ea6dcbabd6589f9125
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2024-01-05 18:52:23 +0100

    IDEA-342292 [Gradle|Sync] cleanup: removed tests with Gradle versions earlier than 4.5 from execution pull
    GitOrigin-RevId: 2a88240e936fffbd2b2d9c309bee91b5d5bc8104

--- plugins/gradle/testSources/org/jetbrains/plugins/gradle/importing/UnsupportedGradleImportingTest.kt
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/importing/UnsupportedGradleImportingTest.kt
@@ -38,7 +38,10 @@ class UnsupportedGradleImportingTest : BuildViewMessagesImportingTestCase() {
     private val OLD_GRADLE_VERSIONS = arrayOf(
       "0.9", /*..., */"0.9.2",
       "1.0", /*"1.1", "1.2", ..., */"1.12",
-      "2.0", /*"2.1", "2.2", ..., */"2.5", "2.6", "2.14.1")
+      "2.0", /*"2.1", "2.2", ..., */"2.5", "2.6", "2.14.1",
+      "3.0", /*"3.1", "3.2", "3.3", "3.4",*/ "3.5",
+      "4.0", /*"4.1", "4.2", "4.3",*/ "4.4"
+    )
 
     /**
      * Run the test against very old not-supported Gradle versions also
