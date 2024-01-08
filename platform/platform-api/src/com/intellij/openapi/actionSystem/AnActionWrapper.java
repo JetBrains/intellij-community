a7c02dc65d878ef84a2b2ed7a5aeeb173f74d036
Gregory.Shrago <gregory.shrago@jetbrains.com>
2024-01-09 02:26:53 +0400

    support CustomComponentAction wrappers
    Fixes "Trying to reset custom component in a presentation" assertion.
    See `WelcomeScreenActionsUtil.ToolbarTextButtonWrapper` action.
    
    GitOrigin-RevId: 4c2c87475f147e8ce5829a4d685d18b8b36a874e

--- platform/platform-api/src/com/intellij/openapi/actionSystem/AnActionWrapper.java
+++ platform/platform-api/src/com/intellij/openapi/actionSystem/AnActionWrapper.java
@@ -1,7 +1,10 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.actionSystem;
 
+import com.intellij.openapi.actionSystem.ex.CustomComponentAction;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
 
 public class AnActionWrapper extends AnAction implements ActionWithDelegate<AnAction>, PerformWithDocumentsCommitted {
   private final AnAction myDelegate;
@@ -20,8 +23,11 @@ public class AnActionWrapper extends AnAction implements ActionWithDelegate<AnAc
   @Override
   public void update(@NotNull AnActionEvent e) {
     UpdateSession session = e.getUpdateSession();
+    JComponent customComponent =
+      session == UpdateSession.EMPTY || !(this instanceof CustomComponentAction) ? null :
+      e.getPresentation().getClientProperty(CustomComponentAction.COMPONENT_KEY);
     if (session == UpdateSession.EMPTY) myDelegate.update(e);
-    else e.getPresentation().copyFrom(session.presentation(myDelegate), null, true);
+    else e.getPresentation().copyFrom(session.presentation(myDelegate), customComponent, true);
   }
 
   @Override
