98d3ab7fece217b2c8842bc16f4cbc3efe89b36e
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-01-08 17:28:56 +0100

    Java: improve parser error message (IDEA-57673 follow-up)
    GitOrigin-RevId: 541e85510652f308b3547b6a94ac88c8b44ce3c9

--- java/java-frontback-psi-impl/src/com/intellij/lang/java/parser/BasicDeclarationParser.java
+++ java/java-frontback-psi-impl/src/com/intellij/lang/java/parser/BasicDeclarationParser.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.lang.java.parser;
 
 import com.intellij.core.JavaPsiBundle;
@@ -484,7 +484,7 @@ public class BasicDeclarationParser {
                                                        boolean constructor) {
     parseParameterList(builder);
 
-    eatBrackets(builder, constructor ? "expected.semicolon" : null);
+    eatBrackets(builder, constructor ? "expected.lbrace" : null);
 
     myParser.getReferenceParser()
       .parseReferenceList(builder, JavaTokenType.THROWS_KEYWORD, myJavaElementTypeContainer.THROWS_LIST, JavaTokenType.COMMA);
