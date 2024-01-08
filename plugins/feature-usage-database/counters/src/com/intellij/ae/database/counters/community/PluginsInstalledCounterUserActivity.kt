160013f30f9c2a1f381b964b783e3eaeb5b9c695
Sasha Blashenkov <Sasha.Blashenkov@jetbrains.com>
2024-01-08 03:09:23 +0100

    [feature-usage-database] AE-65 simpler plugin installation detection logic
    GitOrigin-RevId: 489eea7dc2b92cdc5f5ea54a7a29f50c58620d0a

--- plugins/feature-usage-database/counters/src/com/intellij/ae/database/counters/community/PluginsInstalledCounterUserActivity.kt
+++ plugins/feature-usage-database/counters/src/com/intellij/ae/database/counters/community/PluginsInstalledCounterUserActivity.kt
@@ -3,19 +3,11 @@ package com.intellij.ae.database.counters.community
 
 import com.intellij.ae.database.core.activities.WritableDatabaseBackedCounterUserActivity
 import com.intellij.ae.database.core.baseEvents.fus.FusEventCatcher
-import com.intellij.ae.database.core.dbs.counter.CounterUserActivityDatabase
 import com.intellij.ae.database.core.runUpdateEvent
-import com.intellij.ide.AppLifecycleListener
-import com.intellij.ide.plugins.PluginManager
-import com.intellij.openapi.components.Service
-import com.intellij.openapi.components.service
-import com.intellij.openapi.components.serviceAsync
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import com.intellij.ide.plugins.InstalledPluginsState
+import com.intellij.openapi.extensions.PluginId
 import java.time.Instant
-
-// todo record plugins during shutdown that were not recorded
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * Plugin is considered to be installed if it wasn't uninstalled in 2 minutes after installation.
@@ -25,9 +17,19 @@ object PluginsInstalledCounterUserActivity : WritableDatabaseBackedCounterUserAc
   override val id: String
     get() = "plugin.installed"
 
+  private val installedPlugins = ConcurrentHashMap.newKeySet<String>()
+
   internal suspend fun writeInstallation(pluginId: String, time: Instant) {
-    // todo: record plugin id, but not important for now
-    submit(1, time)
+    if (!InstalledPluginsState.getInstance().wasUpdated(PluginId.getId(pluginId))) {
+      installedPlugins.add(pluginId)
+      submit(1, time)
+    }
+  }
+
+  internal suspend fun writeUninstallation(pluginId: String) {
+    if (installedPlugins.remove(pluginId)) {
+      submit(-1)
+    }
   }
 }
 
@@ -40,7 +42,10 @@ internal class PluginInstalledFusListener : FusEventCatcher(), FusEventCatcher.F
 
   override suspend fun onEvent(fields: Map<String, Any>, eventTime: Instant) {
     val pluginId = fields["plugin"] as? String ?: return
-    PluginInstalledUserActivityService.getInstanceAsync().pluginInstalled(pluginId, eventTime)
+
+    FeatureUsageDatabaseCountersScopeProvider.getScope().runUpdateEvent(PluginsInstalledCounterUserActivity) {
+      it.writeInstallation(pluginId, eventTime)
+    }
   }
 }
 
@@ -53,59 +58,9 @@ internal class PluginUninstalledFusListener : FusEventCatcher(), FusEventCatcher
 
   override suspend fun onEvent(fields: Map<String, Any>, eventTime: Instant) {
     val pluginId = fields["plugin"] as? String ?: return
-    PluginInstalledUserActivityService.getInstanceAsync().pluginUninstalled(pluginId)
-  }
-}
 
-@Service
-internal class PluginInstalledUserActivityService(private val cs: CoroutineScope) {
-  companion object {
-    fun getInstance() = service<PluginInstalledUserActivityService>()
-    suspend fun getInstanceAsync() = serviceAsync<PluginInstalledUserActivityService>()
-  }
-
-  private val mutex = Mutex()
-  private val installedPluginsIds = mutableSetOf<String>()
-  private val pluginsToReport = mutableMapOf<String, Instant>()
-
-  init {
-    CounterUserActivityDatabase.getInstance().executeBeforeConnectionClosed {
-      cs.runUpdateEvent(PluginsInstalledCounterUserActivity) {
-        mutex.withLock {
-          for ((pluginId, installedAt) in pluginsToReport) {
-            it.writeInstallation(pluginId, installedAt)
-          }
-          pluginsToReport.clear()
-        }
-      }
+    FeatureUsageDatabaseCountersScopeProvider.getScope().runUpdateEvent(PluginsInstalledCounterUserActivity) {
+      it.writeUninstallation(pluginId)
     }
-  }
-
-  fun addAllPlugins() {
-    installedPluginsIds.addAll(PluginManager.getPlugins().asSequence()
-                                 .map { it.pluginId.idString }.toMutableSet())
-  }
-
-  suspend fun pluginInstalled(id: String, installedAt: Instant) {
-    // if a plugin is already installed and is installed again, it means it's an update, and we need to skip it
-    mutex.withLock {
-      if (!pluginsToReport.containsKey(id) && !installedPluginsIds.contains(id)) {
-        pluginsToReport[id] = installedAt
-        installedPluginsIds.add(id)
-      }
-    }
-  }
-
-  suspend fun pluginUninstalled(id: String) {
-    mutex.withLock {
-      installedPluginsIds.remove(id)
-    }
-    pluginsToReport.remove(id)
-  }
-}
-
-internal class PluginInstalledUserActivityServiceInitializer : AppLifecycleListener {
-  override fun appFrameCreated(commandLineArgs: MutableList<String>) {
-    PluginInstalledUserActivityService.getInstance().addAllPlugins()
   }
 }
