39d5b1914b1154340ed868d2a2cf16a9aaf086d7
Dmitry Yudin <dmitry.yudin@jetbrains.com>
2024-01-09 14:55:32 +0100

    [compose] TextField with browse button and IconButton improvements. Add TooltipAreaSimple.
    GitOrigin-RevId: 4abe5042cbb9835140b072a03d5ae15a51851703

--- platform/compose/src/com/intellij/platform/compose/showcase/ComposeShowcase.kt
+++ platform/compose/src/com/intellij/platform/compose/showcase/ComposeShowcase.kt
@@ -1,23 +1,40 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.compose.showcase
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.focusProperties
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.*
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.intellij.icons.ExpUiIcons
 import com.intellij.openapi.fileChooser.FileChooser
-import com.intellij.openapi.fileChooser.FileChooserDescriptor
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.keymap.KeymapUtil
 import com.intellij.ui.UIBundle
+import com.intellij.util.ui.JBUI
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import org.jetbrains.jewel.bridge.toComposeColor
+import org.jetbrains.jewel.foundation.modifier.onHover
+import org.jetbrains.jewel.foundation.theme.JewelTheme
+import org.jetbrains.jewel.foundation.theme.OverrideDarkMode
 import org.jetbrains.jewel.ui.Orientation
 import org.jetbrains.jewel.ui.Outline
 import org.jetbrains.jewel.ui.component.*
+import org.jetbrains.jewel.ui.theme.tooltipStyle
+import org.jetbrains.jewel.ui.util.isDark
 import java.awt.event.InputEvent
 import java.awt.event.KeyEvent
 import java.io.File
@@ -45,6 +62,7 @@ internal fun ComposeShowcase() {
         LinkLabels()
         TextFieldSimple()
         TextFieldWithButton()
+        TooltipAreaSimple()
       }
 
       val adapter = rememberScrollbarAdapter(scrollState)
@@ -148,8 +166,15 @@ private fun Buttons() {
 
     Row(verticalAlignment = Alignment.CenterVertically) {
       var state3 by remember { mutableStateOf(0) }
-      IconButton(onClick = { state3++ } /* Modifier.size(18.dp) */) {
-        Icon(painter = painterResource("expui/image/fitContent.svg"), null)
+      var focused by remember { mutableStateOf(false) }
+      IconButton(
+        onClick = { state3++ },
+        modifier = Modifier
+          .size(18.dp)
+          .onFocusEvent { focused = it.isFocused }
+          .background(if (focused) JBUI.CurrentTheme.Focus.focusColor().toComposeColor() else Color.Unspecified, RoundedCornerShape(4.dp))
+      ) {
+        Icon("expui/image/fitContent.svg", contentDescription = null, iconClass = ExpUiIcons::class.java)
       }
       Text("← Click me #$state3")
     }
@@ -206,15 +231,31 @@ private fun TextFieldSimple() {
   }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun TextFieldWithButton() {
 
   var textFieldState by remember { mutableStateOf("") }
-  val buttonTooltip = UIBundle.message("component.with.browse.button.browse.button.tooltip.text") + " (${
+  var fileExists by remember { mutableStateOf(true) }
+
+  LaunchedEffect(textFieldState) {
+    delay(300)
+    withContext(IO) {
+      fileExists = textFieldState.isEmpty() || File(textFieldState).exists()
+    }
+  }
+
+  fun chooseFile() {
+    val descriptor = FileChooserDescriptorFactory.createSingleFileOrFolderDescriptor()
+    descriptor.title = UIBundle.message("file.chooser.default.title")
+    FileChooser.chooseFile(descriptor, null, null) {
+      textFieldState = it.path
+    }
+  }
+
+  val openFileChooserHint = UIBundle.message("component.with.browse.button.browse.button.tooltip.text") + " (${
     KeymapUtil.getKeystrokeText(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, InputEvent.SHIFT_DOWN_MASK))
   })"
-  val descriptor = FileChooserDescriptor(true, true, false, false, false, false).apply { title = UIBundle.message("file.chooser.default.title") }
-  val onClick = { FileChooser.chooseFile(descriptor, null, null) { textFieldState = it.path } }
 
   Row(
     horizontalArrangement = Arrangement.spacedBy(5.dp),
@@ -224,21 +265,80 @@ private fun TextFieldWithButton() {
     TextField(
       value = textFieldState,
       onValueChange = { textFieldState = it },
-      modifier = Modifier.padding(5.dp).height(28.dp).onKeyEvent {
-        if (it.isShiftPressed && it.key == Key.Enter) {
-          onClick.invoke()
-          true
-        }
-        else {
-          false
-        }
+      modifier = Modifier
+        .padding(5.dp)
+        .height(28.dp)
+        .onKeyEvent {
+          if (it.isShiftPressed && it.key == Key.Enter) true.also { chooseFile() } else false
+        },
+      outline = if (fileExists) Outline.None else Outline.Error,
+      placeholder = {
+        Text(
+          text = openFileChooserHint,
+          color = JBUI.CurrentTheme.ContextHelp.FOREGROUND.toComposeColor(),
+          fontSize = (JewelTheme.textStyle.fontSize.value - 2).sp,
+        )
       },
-      outline = if ( textFieldState.isEmpty() || File(textFieldState).exists()) Outline.None else Outline.Error,
       trailingIcon = {
-        IconButton(onClick, Modifier.size(18.dp)) {
-          Icon(painter = painterResource("expui/inline/browse.svg"), contentDescription = buttonTooltip)
+        TooltipArea(
+          tooltip = { TooltipSimple { Text(openFileChooserHint, color = JewelTheme.tooltipStyle.colors.content) } }
+        ) {
+          IconButton({ chooseFile() }, Modifier.size(18.dp).pointerHoverIcon(PointerIcon.Hand).focusProperties { canFocus = false }) {
+            Icon("expui/inline/browse.svg", openFileChooserHint, iconClass = ExpUiIcons::class.java)
+          }
         }
       },
     )
+  }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun TooltipAreaSimple() {
+  Row(verticalAlignment = Alignment.CenterVertically) {
+    Text("Hover with tooltip example:")
+    TooltipArea({ TooltipSimple { Text("Sample text") } }) {
+
+      var hovered by remember { mutableStateOf(false) }
+      val cornerSize = animateDpAsState(if (hovered) 28.dp else 4.dp)
+
+      Box(Modifier
+            .onHover { hovered = it }
+            .border(
+              width = 2.dp,
+              color = JBUI.CurrentTheme.Button.focusBorderColor(true).toComposeColor(),
+              shape = RoundedCornerShape(cornerSize.value),
+            )
+      ) {
+        Text("Hovered: $hovered", Modifier.padding(horizontal = 12.dp, vertical = 6.dp))
+      }
+    }
+  }
+}
+
+@Composable
+private fun TooltipSimple(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+  Box(
+    modifier = modifier
+      .shadow(
+        elevation = JewelTheme.tooltipStyle.metrics.shadowSize,
+        shape = RoundedCornerShape(JewelTheme.tooltipStyle.metrics.cornerSize),
+        ambientColor = JewelTheme.tooltipStyle.colors.shadow,
+        spotColor = Color.Transparent,
+      )
+      .background(
+        color = JewelTheme.tooltipStyle.colors.background,
+        shape = RoundedCornerShape(JewelTheme.tooltipStyle.metrics.cornerSize),
+      )
+      .border(
+        width = JewelTheme.tooltipStyle.metrics.borderWidth,
+        color = JewelTheme.tooltipStyle.colors.border,
+        shape = RoundedCornerShape(JewelTheme.tooltipStyle.metrics.cornerSize),
+      )
+      .padding(JewelTheme.tooltipStyle.metrics.contentPadding),
+  ) {
+    OverrideDarkMode(JewelTheme.tooltipStyle.colors.background.isDark()) {
+      content()
+    }
   }
 }
