1315e48bc664a7d2c5518cdb411043bc128b698f
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2024-01-09 20:34:30 +0100

    [lvcs] remove duplication between actions
    GitOrigin-RevId: ea3f7d50b16ccd8b97f10b3615a98f21e361b120

--- platform/lvcs-impl/src/com/intellij/platform/lvcs/impl/actions/CreatePatchAction.kt
+++ platform/lvcs-impl/src/com/intellij/platform/lvcs/impl/actions/CreatePatchAction.kt
@@ -1,25 +1,18 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.lvcs.impl.actions
 
-import com.intellij.history.integration.LocalHistoryImpl
+import com.intellij.history.integration.IdeaGateway
 import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.changes.actions.CreatePatchFromChangesAction
+import com.intellij.platform.lvcs.impl.ActivityScope
+import com.intellij.platform.lvcs.impl.RevisionSelection
 import com.intellij.platform.lvcs.impl.getChanges
-import com.intellij.platform.lvcs.impl.toRevisionSelection
-import com.intellij.platform.lvcs.impl.ui.ActivityViewDataKeys
 
 class CreatePatchAction : RevisionSelectionAction() {
 
-  override fun actionPerformed(e: AnActionEvent) {
-    val project = e.project ?: return
-    val activityScope = e.getRequiredData(ActivityViewDataKeys.SCOPE)
-    val activitySelection = e.getRequiredData(ActivityViewDataKeys.SELECTION)
-
-    val gateway = LocalHistoryImpl.getInstanceImpl().gateway ?: return
-    val selection = activitySelection.toRevisionSelection(activityScope) ?: return
-
+  override fun actionPerformed(project: Project, gateway: IdeaGateway, activityScope: ActivityScope, selection: RevisionSelection) {
     // see com.intellij.history.integration.ui.models.HistoryDialogModel.canPerformCreatePatch
     if (selection.leftEntry?.hasUnavailableContent() == true || selection.rightEntry?.hasUnavailableContent() == true) {
       thisLogger<CreatePatchAction>().error("Unavailable content")
--- platform/lvcs-impl/src/com/intellij/platform/lvcs/impl/actions/RevisionSelectionAction.kt
+++ platform/lvcs-impl/src/com/intellij/platform/lvcs/impl/actions/RevisionSelectionAction.kt
@@ -1,8 +1,13 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.lvcs.impl.actions
 
+import com.intellij.history.integration.IdeaGateway
+import com.intellij.history.integration.LocalHistoryImpl
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.project.Project
+import com.intellij.platform.lvcs.impl.ActivityScope
+import com.intellij.platform.lvcs.impl.RevisionSelection
 import com.intellij.platform.lvcs.impl.toRevisionSelection
 import com.intellij.platform.lvcs.impl.ui.ActivityViewDataKeys
 
@@ -21,4 +26,17 @@ abstract class RevisionSelectionAction : DumbAwareAction() {
     e.presentation.isVisible = true
     e.presentation.isEnabled = activitySelection.toRevisionSelection(activityScope) != null
   }
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project ?: return
+    val activityScope = e.getRequiredData(ActivityViewDataKeys.SCOPE)
+    val activitySelection = e.getRequiredData(ActivityViewDataKeys.SELECTION)
+
+    val gateway = LocalHistoryImpl.getInstanceImpl().gateway ?: return
+    val selection = activitySelection.toRevisionSelection(activityScope) ?: return
+
+    actionPerformed(project, gateway, activityScope, selection)
+  }
+
+  abstract fun actionPerformed(project: Project, gateway: IdeaGateway, activityScope: ActivityScope, selection: RevisionSelection)
 }
