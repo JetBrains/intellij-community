29c0f78a5b0d5c52d8bcbf9cefea4b29857b8436
Alexander.Likhachev <alexander.likhachev@jetbrains.com>
2024-01-09 16:24:54 +0100

    [CoroutineDebugger] IDEA-340299 Apply the debugger setup only for Gradle 4.6+
    KTIJ-26064 changed the setup to use `CommandLineArgumentProvider` which is available since Gradle 4.6. As Kotlin has stricter supported Gradle versions range than IDEA in general, this fix just stop applying the setup for Kotlin coroutines debugger for Gradle < 4.6 in order not to break non-Kotlin projects
    IJ-CR-123626
    
    GitOrigin-RevId: 9936db0d9571d589366c004fd67cf289575b8761

--- plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/KotlinGradleCoroutineDebugProjectResolver.kt
+++ plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/configuration/KotlinGradleCoroutineDebugProjectResolver.kt
@@ -5,30 +5,43 @@ package org.jetbrains.kotlin.idea.gradleJava.configuration
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.externalSystem.rt.execution.ForkedDebuggerHelper
 import com.intellij.util.Consumer
+import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.idea.extensions.KotlinJvmDebuggerFacade
 import org.jetbrains.plugins.gradle.service.project.AbstractProjectResolverExtension
+import org.jetbrains.plugins.gradle.service.project.GradleProjectResolverExtension
 
 class KotlinGradleCoroutineDebugProjectResolver : AbstractProjectResolverExtension() {
     val log = Logger.getInstance(this::class.java)
+    private val MIN_SUPPORTED_GRADLE_VERSION = "4.6"
 
     override fun enhanceTaskProcessing(taskNames: MutableList<String>, initScriptConsumer: Consumer<String>, parameters: Map<String, String>) {
         try {
             val allowCoroutineAgent = KotlinJvmDebuggerFacade.instance?.isCoroutineAgentAllowedInDebug ?: false
-            if (allowCoroutineAgent) {
-                setupCoroutineAgentForJvmForkedTestTasks(initScriptConsumer)
+            val gradleVersion = parameters[GradleProjectResolverExtension.GRADLE_VERSION]?.let { GradleVersion.version(it) }
+            if (allowCoroutineAgent && (gradleVersion == null || gradleVersion >= GradleVersion.version(MIN_SUPPORTED_GRADLE_VERSION))) {
+                setupCoroutineAgentForJvmForkedTestTasks(initScriptConsumer, gradleVersion == null)
             }
         } catch (e: Exception) {
             log.error("Gradle: not possible to attach a coroutine debugger agent.", e)
         }
     }
 
-    private fun setupCoroutineAgentForJvmForkedTestTasks(initScriptConsumer: Consumer<String>) {
+    private fun setupCoroutineAgentForJvmForkedTestTasks(initScriptConsumer: Consumer<String>, shouldCheckGradleVersion: Boolean) {
+        val gradleVersionCheck = if (shouldCheckGradleVersion) {
+            //language=Gradle
+            """
+                if (org.gradle.util.GradleVersion.current() < org.gradle.util.GradleVersion.version("$MIN_SUPPORTED_GRADLE_VERSION")) return
+            """.trimIndent()
+        } else {
+            ""
+        }
         val script =
             //language=Gradle
             """
             gradle.taskGraph.whenReady { TaskExecutionGraph taskGraph ->
                 taskGraph.allTasks.each { Task task ->
                     if (!(task instanceof Test || task instanceof JavaExec)) return
+                    $gradleVersionCheck
                     FileCollection taskClasspath = task.classpath
                     task.jvmArgumentProviders.add(new CommandLineArgumentProvider() {
                         private static def VERSION_PATTERN = java.util.regex.Pattern.compile(/(\d+)\.(\d+)(\.(\d+))?.*/)
