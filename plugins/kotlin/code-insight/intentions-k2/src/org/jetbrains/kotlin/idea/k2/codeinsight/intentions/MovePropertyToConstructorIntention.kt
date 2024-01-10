14f269fd2964b8d5ba6aa47ef0e5c6655258eb68
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2024-01-10 21:36:10 +0100

    [kotlin] Migrate MovePropertyToConstructorIntention to ModCommand API
    #KTIJ-26974
    
    GitOrigin-RevId: 7d1bd111e68ccfc94b3c6a153799a3350fcfd476

--- plugins/kotlin/code-insight/intentions-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/intentions/MovePropertyToConstructorIntention.kt
+++ plugins/kotlin/code-insight/intentions-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/intentions/MovePropertyToConstructorIntention.kt
@@ -2,11 +2,11 @@
 
 package org.jetbrains.kotlin.idea.k2.codeinsight.intentions
 
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
+import com.intellij.modcommand.ModPsiUpdater
 import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.AbstractKotlinApplicableIntentionWithContext
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.AbstractKotlinModCommandWithContext
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.AnalysisActionContext
 import org.jetbrains.kotlin.idea.codeinsight.api.applicators.KotlinApplicabilityRange
 import org.jetbrains.kotlin.idea.codeinsights.impl.base.applicators.ApplicabilityRanges
 import org.jetbrains.kotlin.idea.codeinsights.impl.base.intentions.MovePropertyToConstructorInfo
@@ -15,7 +15,7 @@ import org.jetbrains.kotlin.idea.codeinsights.impl.base.intentions.MovePropertyT
 import org.jetbrains.kotlin.psi.KtProperty
 
 internal class MovePropertyToConstructorIntention
-    : AbstractKotlinApplicableIntentionWithContext<KtProperty, MovePropertyToConstructorInfo>(KtProperty::class) {
+    : AbstractKotlinModCommandWithContext<KtProperty, MovePropertyToConstructorInfo>(KtProperty::class) {
 
     override fun getFamilyName(): String = KotlinBundle.message("move.to.constructor")
 
@@ -31,7 +31,7 @@ internal class MovePropertyToConstructorIntention
     override fun prepareContext(element: KtProperty): MovePropertyToConstructorInfo? =
         MovePropertyToConstructorInfo.create(element)
 
-    override fun apply(element: KtProperty, context: MovePropertyToConstructorInfo, project: Project, editor: Editor?) {
-        element.moveToConstructor(context)
+    override fun apply(element: KtProperty, context: AnalysisActionContext<MovePropertyToConstructorInfo>, updater: ModPsiUpdater) {
+        element.moveToConstructor(context.analyzeContext.toWritable(updater))
     }
 }
