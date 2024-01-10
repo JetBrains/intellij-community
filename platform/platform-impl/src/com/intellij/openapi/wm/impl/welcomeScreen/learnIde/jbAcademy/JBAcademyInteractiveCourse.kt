63fa02d1724298371996d23e20751f4abe1d01a0
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2024-01-10 16:31:14 +0100

    EDU-6422 Android Studio 232 | Learn tab on Welcome screen shows only Help and Resources
    GitOrigin-RevId: 814054bf4971b01893ac73d9438141abda2707cd

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/learnIde/jbAcademy/JBAcademyInteractiveCourse.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/learnIde/jbAcademy/JBAcademyInteractiveCourse.kt
@@ -4,15 +4,9 @@ package com.intellij.openapi.wm.impl.welcomeScreen.learnIde.jbAcademy
 import com.intellij.icons.AllIcons
 import com.intellij.ide.plugins.PluginManager
 import com.intellij.ide.plugins.PluginManagerCore
-import com.intellij.openapi.actionSystem.ActionPlaces
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.extensions.PluginId
-import com.intellij.openapi.progress.util.StandardProgressIndicatorBase
 import com.intellij.openapi.wm.InteractiveCourseData
 import com.intellij.openapi.wm.InteractiveCourseFactory
-import com.intellij.openapi.wm.impl.welcomeScreen.learnIde.getBrowseCoursesAction
 import com.intellij.util.PlatformUtils
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
@@ -30,7 +24,8 @@ internal class JBAcademyInteractiveCourse : InteractiveCourseFactory {
             PlatformUtils.isCLion() ||
             PlatformUtils.isGoIde() ||
             PlatformUtils.isPhpStorm() ||
-            PlatformUtils.isRustRover()
+            PlatformUtils.isRustRover() ||
+            PlatformUtils.getPlatformPrefix() == "AndroidStudio"
 
   override val isEnabled: Boolean = true
 
