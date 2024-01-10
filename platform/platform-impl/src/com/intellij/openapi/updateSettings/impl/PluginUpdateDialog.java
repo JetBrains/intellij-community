cfe5f5faceaa607bac47b74ff86fa49c64b7be59
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2024-01-10 16:26:01 +0100

    Revert "IDEA-273418 plugin update dialog: select for update only enabled plugins by default"
    This reverts commit ac9072cc181cc37bb0837b3127d8c8e0967fa6eb.
    
    GitOrigin-RevId: 2388556f04e1bcd09e226d68d515cb423b9c950c

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/PluginUpdateDialog.java
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/PluginUpdateDialog.java
@@ -121,7 +121,6 @@ final class PluginUpdateDialog extends DialogWrapper {
         }
         @SuppressWarnings("unchecked") ListPluginComponent component = new ListPluginComponent(myPluginModel, descriptor, group, LinkListener.NULL, true);
         component.setOnlyUpdateMode();
-        component.getChooseUpdateButton().setSelected(myPluginModel.isEnabled(descriptor));
         component.getChooseUpdateButton().addActionListener(e -> updateButtons());
         return component;
       }
