9bc6efff836aece8556baf60355341914ef2189b
Maksim Zuev <maksim.zuev@jetbrains.com>
2024-01-10 09:59:18 +0100

    [debugger] Fix test for IDEA-338583
    GitOrigin-RevId: b83cd15bf1c1c619a07f637da4cb2e11a74fcb05

--- plugins/kotlin/jvm-debugger/test/testData/stepping/custom/smartStepIntoLambdaWithparametersDestructuring.kt
+++ plugins/kotlin/jvm-debugger/test/testData/stepping/custom/smartStepIntoLambdaWithparametersDestructuring.kt
@@ -57,7 +57,7 @@ fun testInlineDestruction() {
 }
 
 fun Ax.foo(f: (Ax) -> Unit) = f(this)
-fun Ax.inlineFoo(f: (Ax) -> Unit) = f(this)
+inline fun Ax.inlineFoo(f: (Ax) -> Unit) = f(this)
 data class Ax(val x: Int, val y: Int)
 
 // IGNORE_K2
