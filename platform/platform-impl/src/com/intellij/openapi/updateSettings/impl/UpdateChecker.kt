537de6b6ecfbe9e66c94b29a047456b37b1079a4
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2024-01-10 17:21:43 +0100

    IDEA-273418 (233 adjusted) plugin updates dialog: exclude disabled plugins from results
    (cherry picked from commit a2ba379c54522403f8597f921962a0de2bec539b)
    
    IJ-CR-122950
    
    GitOrigin-RevId: 68a9f5e4e51d311d107e6f2e4ed832cecc66d872

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateChecker.kt
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateChecker.kt
@@ -681,7 +681,9 @@ private fun doUpdateAndShowResult(
     fun nonIgnored(downloaders: Collection<PluginDownloader>) = downloaders.filterNot { UpdateChecker.isIgnored(it.descriptor) }
 
     val updatesForEnabledPlugins = nonIgnored(pluginUpdates.allEnabled)
-    val updatesForPlugins = updatesForEnabledPlugins + nonIgnored(pluginUpdates.allDisabled)
+    // disabled plugins are excluded from updates, see IDEA-273418, TODO refactor
+    // probably it can lead to disabled plugins becoming incompatible without a notification in platform update dialog
+    val updatesForPlugins = updatesForEnabledPlugins // + nonIgnored(pluginUpdates.allDisabled)
 
     val forceDialog = preferDialog || userInitiated && !notificationsEnabled()
 
