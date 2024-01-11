60d7bb06f7e4be6d1ffb04d42e4099a8ff7366b2
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2024-01-11 20:38:05 +0400

    [workspace model] IDEA-342272 Regenerate metadata for the `LegacyCustomLibraryEntitySource` and get rid of the libraries dispose after the loading from cache
    (cherry picked from commit 637eec32cf9c4dd1c9a31b491e53ea1b2c7a50fa)
    
    IJ-CR-123873
    
    GitOrigin-RevId: 747f418861c4dbb548785ddbcf5f36df74b50a0c

---
+++ platform/projectModel-impl/gen/com/intellij/workspaceModel/ide/impl/legacyBridge/library/MetadataStorageImpl.kt
@@ -0,0 +1,27 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.workspaceModel.ide.impl.legacyBridge.library
+
+import com.intellij.platform.workspace.storage.metadata.impl.MetadataStorageBase
+import com.intellij.platform.workspace.storage.metadata.model.FinalClassMetadata
+import com.intellij.platform.workspace.storage.metadata.model.OwnPropertyMetadata
+import com.intellij.platform.workspace.storage.metadata.model.StorageTypeMetadata
+import com.intellij.platform.workspace.storage.metadata.model.ValueTypeMetadata
+
+object MetadataStorageImpl: MetadataStorageBase() {
+    override fun initializeMetadata() {
+        val primitiveTypeStringNotNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = false, type = "String")
+        
+        var typeMetadata: StorageTypeMetadata
+        
+        typeMetadata = FinalClassMetadata.ClassMetadata(fqName = "com.intellij.workspaceModel.ide.impl.legacyBridge.library.LegacyCustomLibraryEntitySource", properties = listOf(OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "levelId", valueType = primitiveTypeStringNotNullable, withDefault = false),
+OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "virtualFileUrl", valueType = ValueTypeMetadata.SimpleType.CustomType(isNullable = true, typeMetadata = FinalClassMetadata.KnownClass(fqName = "com.intellij.platform.workspace.storage.url.VirtualFileUrl")), withDefault = false)), supertypes = listOf("com.intellij.platform.workspace.storage.EntitySource"))
+        
+        addMetadata(typeMetadata)
+    }
+
+    override fun initializeMetadataHash() {
+        addMetadataHash(typeFqn = "com.intellij.platform.workspace.storage.EntitySource", metadataHash = 2042955165)
+        addMetadataHash(typeFqn = "com.intellij.workspaceModel.ide.impl.legacyBridge.library.LegacyCustomLibraryEntitySource", metadataHash = 1116896941)
+    }
+
+}
