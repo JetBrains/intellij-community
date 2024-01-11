76e07d4b6275768eb8e15008a7439777c525779c
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-01-11 12:39:00 +0100

    Make Generate Copyright dumb aware (IDEA-342761)
    GitOrigin-RevId: 7b8503dc1916e032fbda68e95e00976f21273264

--- plugins/copyright/src/com/maddyhome/idea/copyright/actions/GenerateCopyrightAction.java
+++ plugins/copyright/src/com/maddyhome/idea/copyright/actions/GenerateCopyrightAction.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.maddyhome.idea.copyright.actions;
 
 import com.intellij.copyright.CopyrightBundle;
@@ -7,6 +7,7 @@ import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.options.ShowSettingsUtil;
+import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.PsiDocumentManager;
@@ -16,7 +17,7 @@ import com.maddyhome.idea.copyright.util.FileTypeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class GenerateCopyrightAction extends AnAction {
+public class GenerateCopyrightAction extends DumbAwareAction {
   @Override
   public void update(@NotNull AnActionEvent e) {
     Presentation presentation = e.getPresentation();
