b7eac8fd63ae6f9220469138d917b11652ab86fa
Sasha Blashenkov <Sasha.Blashenkov@jetbrains.com>
2024-01-11 21:36:39 +0100

    [feature-usage-database] AE-84 Uninstalled plugin isn't recorded with -1 to db
    GitOrigin-RevId: f8ffc89a95ba8d29e2840bda81783af271321889

--- plugins/feature-usage-database/counters/src/com/intellij/ae/database/counters/community/PluginsInstalledCounterUserActivity.kt
+++ plugins/feature-usage-database/counters/src/com/intellij/ae/database/counters/community/PluginsInstalledCounterUserActivity.kt
@@ -7,35 +7,90 @@ import com.intellij.ae.database.core.runUpdateEvent
 import com.intellij.ide.plugins.DynamicPluginListener
 import com.intellij.ide.plugins.IdeaPluginDescriptor
 import com.intellij.ide.plugins.InstalledPluginsState
+import com.intellij.openapi.components.*
 import com.intellij.openapi.extensions.PluginId
 import java.time.Instant
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicLong
 
 /**
- * Plugin is considered to be installed if it wasn't uninstalled in 2 minutes after installation.
+ * Plugin is considered to be installed if it wasn't uninstalled in 10 minutes after installation.
  * Plugin updates are ignored
  */
 object PluginsInstalledCounterUserActivity : WritableDatabaseBackedCounterUserActivity() {
   override val id: String
     get() = "plugin.installed"
 
-  private val installedPlugins = ConcurrentHashMap.newKeySet<String>()
 
   internal suspend fun writeInstallation(pluginId: String, time: Instant) {
-    if (!InstalledPluginsState.getInstance().wasUpdated(PluginId.getId(pluginId)) && !installedPlugins.contains(pluginId)) {
-      installedPlugins.add(pluginId)
+    val installedPlugins = InstalledPluginsPersistentState.getInstanceAsync()
+    if (!InstalledPluginsState.getInstance().wasUpdated(PluginId.getId(pluginId)) && installedPlugins.add(pluginId)) {
       submit(1, time)
+    }
+    else {
+      installedPlugins.removeUpdated(pluginId)
     }
   }
 
   internal suspend fun writeUninstallation(pluginId: String) {
+    val installedPlugins = InstalledPluginsPersistentState.getInstanceAsync()
     if (installedPlugins.remove(pluginId)) {
       submit(-1)
     }
   }
+}
 
-  internal fun writeUpdate(id: String) {
-    installedPlugins.add(id)
+@Service
+@State(name = "aeInstalledPlugins", storages = [(Storage("aeInstalledPlugins.xml"))])
+class InstalledPluginsPersistentState : PersistentStateComponentWithModificationTracker<InstalledPluginsPersistentState.State> {
+  companion object {
+    fun getInstance() = service<InstalledPluginsPersistentState>()
+    suspend fun getInstanceAsync() = serviceAsync<InstalledPluginsPersistentState>()
+  }
+  data class State(var myPlugins: Map<String, Long> = HashMap())
+
+  private val PLUGIN_INSTALL_TTL_MS = 10 * 60 * 1000
+
+  private val installedPlugins = ConcurrentHashMap<String, Long>()
+  private val updatedPlugins = ConcurrentHashMap.newKeySet<String>()
+  private val modTracker = AtomicLong(0)
+
+  override fun getState() = State(installedPlugins.filterValues { System.currentTimeMillis() - it < PLUGIN_INSTALL_TTL_MS })
+
+  override fun getStateModificationCount() = modTracker.get()
+
+  override fun loadState(state: State) {
+    for ((k, v) in state.myPlugins) {
+      installedPlugins[k] = v
+    }
+  }
+
+  fun add(s: String): Boolean {
+    val installedPluginContains = installedPlugins[s]?.let { System.currentTimeMillis() - it < PLUGIN_INSTALL_TTL_MS } == true
+    if (installedPluginContains || updatedPlugins.contains(s)) {
+      return false
+    }
+    modTracker.incrementAndGet()
+    installedPlugins[s] = System.currentTimeMillis()
+
+    return true
+  }
+
+  fun addUpdated(s: String) {
+    updatedPlugins.add(s)
+  }
+
+  fun removeUpdated(s: String) {
+    updatedPlugins.remove(s)
+  }
+
+  // plugin will be recorded as removed if it was removed within [HALF_HOUR_MS] milliseconds
+  fun remove(s: String): Boolean {
+    modTracker.incrementAndGet()
+    val currTime = System.currentTimeMillis()
+    updatedPlugins.remove(s)
+
+    return installedPlugins.remove(s)?.let { currTime - it < PLUGIN_INSTALL_TTL_MS } == true
   }
 }
 
@@ -43,7 +98,7 @@ internal class MyDynamicPluginListener : DynamicPluginListener {
   // we don't want to treat theme updates as new installations, workaround for IDEA-342821
   override fun beforePluginUnload(pluginDescriptor: IdeaPluginDescriptor, isUpdate: Boolean) {
     if (isUpdate) {
-      PluginsInstalledCounterUserActivity.writeUpdate(pluginDescriptor.pluginId.idString)
+      InstalledPluginsPersistentState.getInstance().addUpdated(pluginDescriptor.pluginId.idString)
     }
   }
 }
