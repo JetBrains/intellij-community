8c5a1c6c2e266a980cef9aa9bc83f138bcd861e8
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2024-01-11 16:16:47 +0100

    [python] commandInterface moved to `com.jetbrains.python` package, as whole `community.python.impl` must be there
    GitOrigin-RevId: 31505dd3be92004f717f2554cdd2f2e1952e281e

--- python/src/com/jetbrains/python/commandInterfaceConsole/ProcessModeConsumer.java
+++ python/src/com/jetbrains/python/commandInterfaceConsole/ProcessModeConsumer.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.python.community.impl.commandInterfaceConsole;
+package com.jetbrains.python.commandInterfaceConsole;
 
 import com.intellij.execution.console.ProcessBackedConsoleExecuteActionHandler;
 import com.intellij.execution.process.ProcessHandler;
--- python/src/com/jetbrains/python/commandInterfaceConsole/package-info.java
+++ python/src/com/jetbrains/python/commandInterfaceConsole/package-info.java
@@ -4,17 +4,16 @@
  * <h1>Command line console</h1>
  * <p>
  * Package to display command line console on the Terminal ToolWindow. It supports history, execution, syntax highlighting and so on.
- * Entry point is {@link com.intellij.python.community.impl.commandInterfaceConsole.CommandLineConsoleApiKt}.
+ * Entry point is {@link com.jetbrains.python.commandInterfaceConsole.CommandLineConsoleApiKt}.
  * </p>
  * <p/>
  * <h2>Technical details</h2>
  * <p>
- * This package uses {@link com.intellij.python.community.impl.commandInterfaceConsole.CommandConsole} and it can work in 2 modes: "command-mode"
+ * This package uses {@link com.jetbrains.python.commandInterfaceConsole.CommandConsole} and it can work in 2 modes: "command-mode"
  *  to accept, highlight and execute commands, and in "process-mode" to pass stdin/stdout to process it execute.
  * See class documentation for details.
  * </p>
  *
  * @author Ilya.Kazakevich
- * @see com.intellij.python.community.impl.commandInterfaceConsole.CommandLineConsoleApiKt
  */
-package com.intellij.python.community.impl.commandInterfaceConsole;
+package com.jetbrains.python.commandInterfaceConsole;
