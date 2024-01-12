29949520be4e960804c3a2e32aafd341924e53b7
Karina.Kazaryan <karina.kazaryan@jetbrains.com>
2024-01-12 11:30:57 +0100

    Add @IJIgnore annotation to GradleSourceSetsTest (IDEA-340503)
    GitOrigin-RevId: 8ec37b0d68d612cfc81f3e4de996515fe92a7a23

--- plugins/gradle/java/testSources/dsl/GradleSourceSetsTest.kt
+++ plugins/gradle/java/testSources/dsl/GradleSourceSetsTest.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.dsl
 
+import com.intellij.idea.IJIgnore
 import org.gradle.util.GradleVersion
 import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.GRADLE_API_SOURCE_SET
 import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.GRADLE_API_SOURCE_SET_CONTAINER
@@ -81,6 +82,7 @@ class GradleSourceSetsTest : GradleCodeInsightTestCase() {
     }
   }
 
+  @IJIgnore(issue = "IDEA-340503")
   @ParameterizedTest
   @AllGradleVersionsSource("$DECORATORS, buildscript")
   fun `test source set via qualified method call`(gradleVersion: GradleVersion, decorator: String) {
