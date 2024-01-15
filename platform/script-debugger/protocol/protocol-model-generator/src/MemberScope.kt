5619000580c6cb079ec2b910785ee15bd63f5f05
Shumaf Lovpache <shumaf.lovpache@jetbrains.com>
2024-01-15 12:27:12 +0200

    [JavaScript Debugger] Update protocol model
    Add Browser and Emulation domains for generation.
    Fix name collisions for method parameter enums that are enums.
    Change generated reader naming strategy to more VCS friendly.
    
    GitOrigin-RevId: 1f5708cd84ef9d74318d7b06d3afdb6deb7f7f5b

--- platform/script-debugger/protocol/protocol-model-generator/src/MemberScope.kt
+++ platform/script-debugger/protocol/protocol-model-generator/src/MemberScope.kt
@@ -11,9 +11,21 @@ import org.jetbrains.protocolReader.appendEnums
 internal open class MemberScope(private val classScope: ClassScope, protected val memberName: String) : ResolveAndGenerateScope {
   override fun <T : ItemDescriptor> resolveType(typedObject: T) = classScope.generator.generator.resolveType(typedObject, this)
 
+  private val commonEnumNames = listOf("Type", "Format")
+
   fun generateEnum(description: String?, enumConstants: List<String>): BoxableType {
     var enumName = capitalizeFirstChar(memberName)
-    if (StringUtil.equalsIgnoreCase(enumName, "TYPE") &&
+    if (getTypeDirection() == TypeData.Direction.OUTPUT) {
+      // Enums used as a parameter type (Direction.OUTPUT) do not have a separate type in the protocol.
+      // Therefore, during generation, collisions sometimes occur (i.e., the parameter name 'type' is used very often).
+      // In such cases, we prepend the enum name with the name of the method.
+      // Although this heuristic is not perfect, it allows us to reduce the number of enumerations that will receive ugly names.
+      val commonName = commonEnumNames.firstOrNull { StringUtil.equalsIgnoreCase(enumName, it) }
+      if (commonName != null) {
+        enumName = classScope.classContextNamespace.lastComponent + enumName
+      }
+    }
+    else if (StringUtil.equalsIgnoreCase(enumName, "TYPE") &&
         classScope.classContextNamespace.lastComponent.endsWith("EventData") ) {
       // to avoid same name with companion object TYPE
       enumName = "EventType"
