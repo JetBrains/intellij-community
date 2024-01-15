baa3d528a14c77b7004b51bd81ff9d71feb0891e
Nikita Katkov <nikita.katkov@jetbrains.com>
2024-01-15 16:33:34 +0100

    [json] IDEA-339628 Performance optimization for json schema HttpVirtualFiles
    Introduced expirable cache for downloadable schema files which is aimed to reduce cpu/memory usage during instantiation of remote virtual files. Previously each url validity call resulted in an attempt to create and load file from the provided url causing performance degradations for files with large amount of links inside. The cache should prevent making several attempts to load the same referenced schema file
    
    GitOrigin-RevId: 28170b331b75e4eefb3d5d4fc7d2aa87d6fc2a62

--- json/src/com/jetbrains/jsonSchema/remote/JsonFileResolver.java
+++ json/src/com/jetbrains/jsonSchema/remote/JsonFileResolver.java
@@ -1,9 +1,10 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.jsonSchema.remote;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Couple;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
@@ -15,16 +16,16 @@ import com.intellij.openapi.vfs.ex.temp.TempFileSystem;
 import com.intellij.openapi.vfs.impl.http.HttpVirtualFile;
 import com.intellij.openapi.vfs.impl.http.RemoteFileInfo;
 import com.intellij.openapi.vfs.impl.http.RemoteFileState;
-import com.intellij.util.UriUtil;
 import com.intellij.util.Url;
 import com.intellij.util.Urls;
+import com.intellij.util.concurrency.AppExecutorUtil;
 import com.jetbrains.jsonSchema.JsonSchemaCatalogProjectConfiguration;
-import com.jetbrains.jsonSchema.impl.JsonSchemaObject;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 public final class JsonFileResolver {
 
@@ -36,8 +37,8 @@ public final class JsonFileResolver {
   }
 
   public static @Nullable VirtualFile urlToFile(@NotNull String urlString) {
-    if (urlString.startsWith(JsonSchemaObject.TEMP_URL)) {
-      return TempFileSystem.getInstance().findFileByPath(urlString.substring(JsonSchemaObject.TEMP_URL.length() - 1));
+    if (urlString.startsWith(TEMP_URL)) {
+      return TempFileSystem.getInstance().findFileByPath(urlString.substring(TEMP_URL.length() - 1));
     }
     return VirtualFileManager.getInstance().findFileByUrl(FileUtil.toSystemIndependentName(replaceUnsafeSchemaStoreUrls(urlString)));
   }
@@ -72,22 +73,39 @@ public final class JsonFileResolver {
       // relative path
       VirtualFile parent = currentFile == null ? null : currentFile.getParent();
       schemaUrl = parent == null ? null :
-                  parent.getUrl().startsWith(JsonSchemaObject.TEMP_URL) ? ("temp:///" + parent.getPath() + "/" + schemaUrl) :
+                  parent.getUrl().startsWith(TEMP_URL) ? ("temp:///" + parent.getPath() + "/" + schemaUrl) :
                   VfsUtilCore.pathToUrl(parent.getPath() + File.separator + schemaUrl);
     }
 
-    if (schemaUrl != null) {
-      VirtualFile virtualFile = urlToFile(schemaUrl);
-      // validate the URL before returning the file
-      if (virtualFile instanceof HttpVirtualFile) {
-        String url = virtualFile.getUrl();
-        Url parse = Urls.parse(url, false);
-        if (parse == null || StringUtil.isEmpty(parse.getAuthority()) || StringUtil.isEmpty(parse.getPath())) return null;
-      }
-      if (virtualFile != null) return virtualFile;
+    if (StringUtil.isEmpty(schemaUrl)) {
+      return null;
     }
+    else if (!schemaUrl.startsWith("http")) {
+      return urlToFile(schemaUrl);
+    }
+    else {
+      return getOrComputeVirtualFileForValidUrlOrNull(schemaUrl);
+    }
+  }
 
-    return null;
+  private static @Nullable VirtualFile getOrComputeVirtualFileForValidUrlOrNull(@NotNull String maybeUrl) {
+    return urlValidityCache.get(maybeUrl);
+  }
+
+  // Expirable cache used for cases when:
+  //  - user is typing the url
+  //  - url became invalid by the time
+  private static final LoadingCache<String, VirtualFile> urlValidityCache =
+    Caffeine.newBuilder()
+      .expireAfterAccess(1, TimeUnit.MINUTES)
+      .maximumSize(1000)
+      .executor(AppExecutorUtil.getAppExecutorService())
+      .build(JsonFileResolver::computeVirtualFileForValidUrlOrNull);
+
+  private static @Nullable VirtualFile computeVirtualFileForValidUrlOrNull(@NotNull String url) {
+    Url parse = Urls.parse(url, false);
+    if (parse == null || StringUtil.isEmpty(parse.getAuthority()) || StringUtil.isEmpty(parse.getPath())) return null;
+    return urlToFile(url);
   }
 
   public static void startFetchingHttpFileIfNeeded(@Nullable VirtualFile path, Project project) {
@@ -106,8 +124,7 @@ public final class JsonFileResolver {
   }
 
   public static boolean isHttpPath(@NotNull String schemaFieldText) {
-    Couple<String> couple = UriUtil.splitScheme(schemaFieldText);
-    return couple.first.startsWith("http");
+    return schemaFieldText.startsWith("http://") || schemaFieldText.startsWith("https://");
   }
 
   public static boolean isAbsoluteUrl(@NotNull String path) {
