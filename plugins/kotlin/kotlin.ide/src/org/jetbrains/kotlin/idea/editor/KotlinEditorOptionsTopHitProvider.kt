3da5ff2451ac8d401b8cdf2e111faeb1dfda153e
Anna Kozlova <anna.kozlova@jetbrains.com>
2024-01-15 09:07:10 +0100

    [kotlin] move editor extension to `kotlin.ide` module
    part of KTIJ-28142
    
    GitOrigin-RevId: 35da54cd1651b60197efd52be3dd7406f84886df

--- plugins/kotlin/kotlin.ide/src/org/jetbrains/kotlin/idea/editor/KotlinEditorOptionsTopHitProvider.kt
+++ plugins/kotlin/kotlin.ide/src/org/jetbrains/kotlin/idea/editor/KotlinEditorOptionsTopHitProvider.kt
@@ -1,9 +1,12 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.editor
 
 import com.intellij.ide.ui.OptionsSearchTopHitProvider
 import com.intellij.ide.ui.search.OptionDescription
 import com.intellij.openapi.options.ex.ConfigurableWrapper
+import kotlin.sequences.filterIsInstance
+import kotlin.sequences.map
+import kotlin.sequences.toList
 
 class KotlinEditorOptionsTopHitProvider : OptionsSearchTopHitProvider.ApplicationLevelProvider {
     override fun getId(): String = ID
