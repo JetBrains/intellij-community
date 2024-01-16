9403288898a72fa368e1f482e5ec4251889b857d
Olga Klisho <olga.klisho@jetbrains.com>
2024-01-16 17:59:50 +0100

    [rdct] CWM-8911 making packLogs cancellable
    GitOrigin-RevId: 64716516863d873ca078791cafb8117183859c17

--- platform/platform-impl/src/com/intellij/ide/actions/CollectZippedLogsAction.kt
+++ platform/platform-impl/src/com/intellij/ide/actions/CollectZippedLogsAction.kt
@@ -1,78 +1,92 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ide.actions;
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide.actions
 
-import com.intellij.CommonBundle;
-import com.intellij.ide.IdeBundle;
-import com.intellij.ide.logsUploader.LogsPacker;
-import com.intellij.ide.util.PropertiesComponent;
-import com.intellij.idea.ActionsBundle;
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationType;
-import com.intellij.openapi.actionSystem.ActionUpdateThread;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.DoNotAskOption;
-import com.intellij.openapi.ui.MessageDialogBuilder;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.util.ui.IoErrorText;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.CommonBundle
+import com.intellij.ide.IdeBundle
+import com.intellij.ide.logsUploader.LogsPacker.packLogs
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.idea.ActionsBundle
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.runBlockingCancellable
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.ui.DoNotAskOption
+import com.intellij.openapi.ui.MessageDialogBuilder.Companion.okCancel
+import com.intellij.openapi.ui.Messages
+import com.intellij.platform.ide.progress.ModalTaskOwner
+import com.intellij.platform.ide.progress.TaskCancellation
+import com.intellij.platform.ide.progress.withModalProgress
+import com.intellij.util.ui.IoErrorText
+import java.io.IOException
 
-import java.io.IOException;
-import java.nio.file.Path;
+class CollectZippedLogsAction : AnAction(), DumbAware {
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project
 
-public final class CollectZippedLogsAction extends AnAction implements DumbAware {
-  private static final String CONFIRMATION_DIALOG = "zipped.logs.action.show.confirmation.dialog";
-  public static final String NOTIFICATION_GROUP = "Collect Zipped Logs";
-
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
-    Project project = e.getProject();
-
-    boolean doNotShowDialog = PropertiesComponent.getInstance().getBoolean(CONFIRMATION_DIALOG);
+    val doNotShowDialog = PropertiesComponent.getInstance().getBoolean(CONFIRMATION_DIALOG)
     if (!doNotShowDialog) {
-      String title = IdeBundle.message("collect.logs.sensitive.title");
-      String message = IdeBundle.message("collect.logs.sensitive.text");
-      boolean confirmed = MessageDialogBuilder.okCancel(title, message)
+      val title = IdeBundle.message("collect.logs.sensitive.title")
+      val message = IdeBundle.message("collect.logs.sensitive.text")
+      val confirmed = okCancel(title, message)
         .yesText(ActionsBundle.message("action.RevealIn.name.other", RevealFileAction.getFileManagerName()))
         .noText(CommonBundle.getCancelButtonText())
         .icon(Messages.getWarningIcon())
-        .doNotAsk(new DoNotAskOption.Adapter() {
-          @Override
-          public void rememberChoice(boolean selected, int exitCode) {
-            PropertiesComponent.getInstance().setValue(CONFIRMATION_DIALOG, selected);
+        .doNotAsk(object : DoNotAskOption.Adapter() {
+          override fun rememberChoice(selected: Boolean, exitCode: Int) {
+            PropertiesComponent.getInstance().setValue(CONFIRMATION_DIALOG, selected)
           }
         })
-        .ask(project);
+        .ask(project)
       if (!confirmed) {
-        return;
+        return
       }
     }
 
-    ProgressManager.getInstance().runProcessWithProgressSynchronously(() -> {
-      try {
-        Path logs = LogsPacker.packLogs(project);
-        if (RevealFileAction.isSupported()) {
-          RevealFileAction.openFile(logs);
+    val modalTaskOwner = if (project == null) {
+      ModalTaskOwner.guess()
+    }
+    else {
+      ModalTaskOwner.project(project)
+    }
+
+    ProgressManager.getInstance().runProcessWithProgressSynchronously(
+      {
+        try {
+          val logs = runBlockingCancellable {
+            withModalProgress(modalTaskOwner, IdeBundle.message("collect.logs.progress.title"), TaskCancellation.cancellable()) {
+              packLogs(project)
+            }
+          }
+          if (RevealFileAction.isSupported()) {
+            RevealFileAction.openFile(logs)
+          }
+          else {
+            Notification(NOTIFICATION_GROUP, IdeBundle.message(
+              "collect.logs.notification.success", logs),
+                         NotificationType.INFORMATION).notify(project)
+          }
         }
-        else {
-          new Notification(NOTIFICATION_GROUP, IdeBundle.message("collect.logs.notification.success", logs), NotificationType.INFORMATION).notify(project);
+        catch (x: IOException) {
+          Logger.getInstance(javaClass).warn(x)
+          val message = IdeBundle.message("collect.logs.notification.error",
+                                          IoErrorText.message(x))
+          Notification(NOTIFICATION_GROUP, message,
+                       NotificationType.ERROR).notify(project)
         }
-      }
-      catch (IOException x) {
-        Logger.getInstance(getClass()).warn(x);
-        String message = IdeBundle.message("collect.logs.notification.error", IoErrorText.message(x));
-        new Notification(NOTIFICATION_GROUP, message, NotificationType.ERROR).notify(project);
-      }
-    }, IdeBundle.message("collect.logs.progress.title"), true, project);
+      }, IdeBundle.message("collect.logs.progress.title"), true, project)
   }
 
+  override fun getActionUpdateThread(): ActionUpdateThread {
+    return ActionUpdateThread.BGT
+  }
 
-  @Override
-  public @NotNull ActionUpdateThread getActionUpdateThread() {
-    return ActionUpdateThread.BGT;
+  companion object {
+    private const val CONFIRMATION_DIALOG = "zipped.logs.action.show.confirmation.dialog"
+    const val NOTIFICATION_GROUP: String = "Collect Zipped Logs"
   }
 }
