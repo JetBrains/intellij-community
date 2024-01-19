9809357dcb576e22a3175314d18ec063ec6d55db
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2024-01-19 10:54:49 +0100

    [maven] convert MavenArtifactCoordinatesGroupIdConverter and MavenArtifactCoordinatesArtifactIdConverter to kotlin
    GitOrigin-RevId: 58604a2ab51c5d223fa7bee359236c96ef615c4b

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/dom/converters/MavenArtifactCoordinatesArtifactIdConverter.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/dom/converters/MavenArtifactCoordinatesArtifactIdConverter.kt
@@ -1,53 +1,44 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.maven.dom.converters;
+package org.jetbrains.idea.maven.dom.converters
 
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.xml.ConvertContext;
-import org.jetbrains.idea.maven.indices.MavenIndicesManager;
-import org.jetbrains.idea.maven.model.MavenArtifact;
-import org.jetbrains.idea.maven.model.MavenId;
-import org.jetbrains.idea.maven.project.MavenProject;
-import org.jetbrains.idea.maven.project.MavenProjectsManager;
-import org.jetbrains.idea.maven.utils.MavenArtifactUtilKt;
-import org.jetbrains.idea.reposearch.DependencySearchService;
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.util.xml.ConvertContext
+import org.jetbrains.idea.maven.indices.MavenIndicesManager
+import org.jetbrains.idea.maven.model.MavenId
+import org.jetbrains.idea.maven.project.MavenProjectsManager
+import org.jetbrains.idea.maven.utils.resolved
+import org.jetbrains.idea.reposearch.DependencySearchService
 
-import java.util.Collections;
-import java.util.Set;
+class MavenArtifactCoordinatesArtifactIdConverter : MavenArtifactCoordinatesConverter() {
+  override fun doIsValid(id: MavenId, manager: MavenIndicesManager, context: ConvertContext): Boolean {
+    if (StringUtil.isEmpty(id.groupId) || StringUtil.isEmpty(id.artifactId)) return false
 
-
-public class MavenArtifactCoordinatesArtifactIdConverter extends MavenArtifactCoordinatesConverter {
-  @Override
-  protected boolean doIsValid(MavenId id, MavenIndicesManager manager, ConvertContext context) {
-    if (StringUtil.isEmpty(id.getGroupId()) || StringUtil.isEmpty(id.getArtifactId())) return false;
-
-    MavenProjectsManager projectsManager = MavenProjectsManager.getInstance(context.getProject());
-    if (StringUtil.isNotEmpty(id.getVersion())) {
-      if (projectsManager.findProject(id) != null) return true;
-    } else {
-      for (MavenProject project : projectsManager.getProjects()) {
-        MavenId mavenId = project.getMavenId();
-        if (id.getGroupId().equals(mavenId.getGroupId()) && id.getArtifactId().equals(mavenId.getArtifactId())) return true;
+    val projectsManager = MavenProjectsManager.getInstance(context.project)
+    if (StringUtil.isNotEmpty(id.version)) {
+      if (projectsManager.findProject(id) != null) return true
+    }
+    else {
+      for (project in projectsManager.projects) {
+        val mavenId = project.mavenId
+        if (id.groupId == mavenId.groupId && id.artifactId == mavenId.artifactId) return true
       }
     }
 
     // Check if artifact was found on importing.
-    VirtualFile projectFile = getMavenProjectFile(context);
-    MavenProject mavenProject = projectFile == null ? null : projectsManager.findProject(projectFile);
+    val projectFile = getMavenProjectFile(context)
+    val mavenProject = if (projectFile == null) null else projectsManager.findProject(projectFile)
     if (mavenProject != null) {
-      for (MavenArtifact artifact : mavenProject.findDependencies(id.getGroupId(), id.getArtifactId())) {
-        if (MavenArtifactUtilKt.resolved(artifact)) {
-          return true;
+      for (artifact in mavenProject.findDependencies(id.groupId, id.artifactId)) {
+        if (artifact.resolved()) {
+          return true
         }
       }
     }
 
-    return manager.hasLocalArtifactId(id.getGroupId(), id.getArtifactId());
+    return manager.hasLocalArtifactId(id.groupId, id.artifactId)
   }
 
-  @Override
-  protected Set<String> doGetVariants(MavenId id, DependencySearchService searchService) {
-    return Collections.emptySet();
+  override fun doGetVariants(id: MavenId, searchService: DependencySearchService): Set<String> {
+    return emptySet()
   }
-
 }
--- plugins/maven/src/main/java/org/jetbrains/idea/maven/dom/converters/MavenArtifactCoordinatesGroupIdConverter.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/dom/converters/MavenArtifactCoordinatesGroupIdConverter.kt
@@ -1,58 +1,47 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.maven.dom.converters;
+package org.jetbrains.idea.maven.dom.converters
 
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.xml.ConvertContext;
-import org.jetbrains.idea.maven.indices.MavenIndicesManager;
-import org.jetbrains.idea.maven.model.MavenArtifact;
-import org.jetbrains.idea.maven.model.MavenId;
-import org.jetbrains.idea.maven.project.MavenProject;
-import org.jetbrains.idea.maven.project.MavenProjectsManager;
-import org.jetbrains.idea.maven.utils.MavenArtifactUtilKt;
-import org.jetbrains.idea.reposearch.DependencySearchService;
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.util.xml.ConvertContext
+import org.jetbrains.idea.maven.indices.MavenIndicesManager
+import org.jetbrains.idea.maven.model.MavenId
+import org.jetbrains.idea.maven.project.MavenProjectsManager
+import org.jetbrains.idea.maven.utils.resolved
+import org.jetbrains.idea.reposearch.DependencySearchService
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Set;
+class MavenArtifactCoordinatesGroupIdConverter : MavenArtifactCoordinatesConverter(), MavenSmartConverter<String?> {
+  override fun doIsValid(id: MavenId, manager: MavenIndicesManager, context: ConvertContext): Boolean {
+    if (StringUtil.isEmpty(id.groupId)) return false
 
-public class MavenArtifactCoordinatesGroupIdConverter extends MavenArtifactCoordinatesConverter implements MavenSmartConverter<String> {
-  @Override
-  protected boolean doIsValid(MavenId id, MavenIndicesManager manager, ConvertContext context) {
-    if (StringUtil.isEmpty(id.getGroupId())) return false;
-
-    MavenProjectsManager projectsManager = MavenProjectsManager.getInstance(context.getProject());
-    if (StringUtil.isNotEmpty(id.getArtifactId()) && StringUtil.isNotEmpty(id.getVersion())) {
-      if (projectsManager.findProject(id) != null) return true;
-    } else {
-      for (MavenProject project : projectsManager.getProjects()) {
-        if (id.getGroupId().equals(project.getMavenId().getGroupId())) return true;
+    val projectsManager = MavenProjectsManager.getInstance(context.project)
+    if (StringUtil.isNotEmpty(id.artifactId) && StringUtil.isNotEmpty(id.version)) {
+      if (projectsManager.findProject(id) != null) return true
+    }
+    else {
+      for (project in projectsManager.projects) {
+        if (id.groupId == project.mavenId.groupId) return true
       }
     }
 
     // Check if artifact was found on importing.
-    VirtualFile projectFile = getMavenProjectFile(context);
-    MavenProject mavenProject = projectFile == null ? null : projectsManager.findProject(projectFile);
+    val projectFile = getMavenProjectFile(context)
+    val mavenProject = if (projectFile == null) null else projectsManager.findProject(projectFile)
     if (mavenProject != null) {
-      for (MavenArtifact artifact : mavenProject.findDependencies(id.getGroupId(), id.getArtifactId())) {
-        if (MavenArtifactUtilKt.resolved(artifact)) {
-          return true;
+      for (artifact in mavenProject.findDependencies(id.groupId, id.artifactId)) {
+        if (artifact.resolved()) {
+          return true
         }
       }
     }
 
-    return manager.hasLocalGroupId(id.getGroupId());
+    return manager.hasLocalGroupId(id.groupId)
   }
 
-  @Override
-  protected Set<String> doGetVariants(MavenId id, DependencySearchService searchService) {
-    return Collections.emptySet();
+  override fun doGetVariants(id: MavenId, searchService: DependencySearchService): Set<String> {
+    return emptySet()
   }
 
-  @Override
-  public Collection<String> getSmartVariants(ConvertContext convertContext) {
-    return Collections.emptySet();
+  override fun getSmartVariants(convertContext: ConvertContext): Collection<String> {
+    return emptySet()
   }
-
-
 }
