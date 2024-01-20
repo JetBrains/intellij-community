3e3d88aaaa49bdfe284e57d46c4d461f632a5aff
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-01-20 12:34:17 +0100

    Access swing model on UI thread in IDE Errors dialog
    GitOrigin-RevId: 0ff87806090155ea116a95e237f85fcb4df51c6d

--- platform/platform-impl/src/com/intellij/diagnostic/IdeErrorsDialog.kt
+++ platform/platform-impl/src/com/intellij/diagnostic/IdeErrorsDialog.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.diagnostic
 
 import com.intellij.CommonBundle
@@ -169,8 +169,10 @@ open class IdeErrorsDialog @JvmOverloads internal constructor(
   }
 
   private fun setDevelopers(developers: DeveloperList) {
-    myAssigneeCombo.model = CollectionComboBoxModel(developers.developers)
-    myDevListTimestamp = developers.timestamp
+    UIUtil.invokeLaterIfNeeded {
+      myAssigneeCombo.model = CollectionComboBoxModel(developers.developers)
+      myDevListTimestamp = developers.timestamp
+    }
   }
 
   private fun selectMessage(defaultMessage: LogMessage?): Int {
