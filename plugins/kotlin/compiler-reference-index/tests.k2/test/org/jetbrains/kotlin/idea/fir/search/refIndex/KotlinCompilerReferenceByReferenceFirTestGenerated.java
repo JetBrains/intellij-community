380ddc53d2a0a205dbe97ade2b41d9794387b2b7
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2024-01-21 20:14:08 +0100

    [kotlin] Enable kotlin.compiler-reference-index.tests.k2 tests in aggregator
    #KTIJ-28522
    
    GitOrigin-RevId: 9beb088cff3a6bf30308b1fe8818d459bdad5224

--- plugins/kotlin/compiler-reference-index/tests.k2/test/org/jetbrains/kotlin/idea/fir/search/refIndex/KotlinCompilerReferenceByReferenceFirTestGenerated.java
+++ plugins/kotlin/compiler-reference-index/tests.k2/test/org/jetbrains/kotlin/idea/fir/search/refIndex/KotlinCompilerReferenceByReferenceFirTestGenerated.java
@@ -1,6 +1,6 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
-package org.jetbrains.kotlin.idea.search.refIndex;
+package org.jetbrains.kotlin.idea.fir.search.refIndex;
 
 import com.intellij.testFramework.TestDataPath;
 import org.jetbrains.kotlin.idea.test.JUnit3RunnerWithInners;
