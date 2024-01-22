cce27bfd8270e19845e4f4fcf7772b717c04c946
Alexey Belkov <alexey.belkov@jetbrains.com>
2024-01-22 12:24:16 +0400

    [kotlin] J2K: Add more tests for parenthesized multiline binary expression
    KTIJ-28517
    IJ-CR-124321
    
    GitOrigin-RevId: 04a239295ecb54e0036e9171d2ee65f1b1991a79

--- plugins/kotlin/j2k/k1.new/tests/testData/newJ2k/binaryExpression/multiLine2.java
+++ plugins/kotlin/j2k/k1.new/tests/testData/newJ2k/binaryExpression/multiLine2.java
@@ -1,10 +1,10 @@
 public class A {
-    public static void main(String[] args) {
+    public void mixedOperators() {
         int x = 1
                 + 1 - 2;
 
         int x2 = 1 + 1
-                - 2;
+                 - 2;
 
         int x3 = 1
                  + 1
@@ -18,5 +18,26 @@ public class A {
         int x5 = 1
                  + 1 - 2
                  + 3;
+    }
+
+    public void nestedParentheses() {
+        int x = (1
+                + 1) - 2;
+
+        int x2 = (1 + 1)
+                 - 2;
+
+        int x3 = (1
+                 + (1
+                 - 2))
+                 + 3;
+
+        int x4 = 1
+                 + ((1
+                 - 2) + 3);
+
+        int x5 = (1
+                 + ((1 - 2)
+                 + 3));
     }
 }
