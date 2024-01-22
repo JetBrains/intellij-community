d1a5e42b357829889fb81e423b1c38cf42d63e1f
Evgeny Abramov <evgeny.abramov@jetbrains.com>
2024-01-22 17:51:43 +0200

    (IJPL-515) Increment usage trigger collector version
    GitOrigin-RevId: 864d788389a3919b749602333fb128c8d1856865

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/statistics/SearchEverywhereUsageTriggerCollector.java
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/statistics/SearchEverywhereUsageTriggerCollector.java
@@ -42,7 +42,7 @@ public final class SearchEverywhereUsageTriggerCollector extends CounterUsagesCo
                                                             "third.party", "Vcs.Git");
   public static final StringEventField CURRENT_TAB_FIELD = EventFields.String("currentTabId", ourTabs);
 
-  private static final EventLogGroup GROUP = new EventLogGroup("searchEverywhere", 10);
+  private static final EventLogGroup GROUP = new EventLogGroup("searchEverywhere", 11);
 
   public static final EventId2<String, AnActionEvent> DIALOG_OPEN = GROUP.registerEvent("dialogOpen",
                                                                                         CONTRIBUTOR_ID_FIELD,
