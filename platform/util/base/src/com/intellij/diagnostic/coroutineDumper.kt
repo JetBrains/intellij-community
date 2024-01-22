e2cea41d2bb41d46ec9cef5aa1d96ff75ca9a18f
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2024-01-22 18:20:07 +0100

    fix duplicated coroutine name in dumps
    GitOrigin-RevId: d79378aa671dd20e39fb6e2deef12d20801af4ea

--- platform/util/base/src/com/intellij/diagnostic/coroutineDumper.kt
+++ platform/util/base/src/com/intellij/diagnostic/coroutineDumper.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 @file:Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER")
 @file:OptIn(ExperimentalCoroutinesApi::class, InternalCoroutinesApi::class)
 
@@ -261,7 +261,7 @@ private fun JobTree.toRepresentation(stripTrace: Boolean): JobRepresentationTree
     // see kotlinx.coroutines.AbstractCoroutine.nameString
     null
   }
-  else if (job.javaClass.name == "com.intellij.util.ChildScope") {
+  else if (job.javaClass.name == "com.intellij.platform.util.coroutines.ChildScope") {
     // ChildScope already renders the name in its `toString()`
     null
   }
