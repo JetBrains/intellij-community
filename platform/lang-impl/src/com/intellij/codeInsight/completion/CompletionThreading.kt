e4d604935a36e7018ede097c1e863b73ab8e7c7b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-01-22 15:45:57 +0100

    IDEA-343642 cleanup
    GitOrigin-RevId: ce709a5f49854aa2046a976dc0c1292869714503

--- platform/lang-impl/src/com/intellij/codeInsight/completion/CompletionThreading.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/completion/CompletionThreading.kt
@@ -1,168 +1,167 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.codeInsight.completion;
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.codeInsight.completion
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ex.ApplicationManagerEx;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.progress.ProcessCanceledException;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.progress.util.ProgressWrapper;
-import com.intellij.openapi.util.Computable;
-import com.intellij.util.Consumer;
-import com.intellij.util.concurrency.Semaphore;
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ex.ApplicationManagerEx
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.util.ProgressWrapper
+import com.intellij.openapi.util.Computable
+import com.intellij.openapi.util.Computable.PredefinedValueComputable
+import com.intellij.util.concurrency.Semaphore
+import java.util.concurrent.*
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.*;
+internal sealed interface CompletionThreading {
+  fun startThread(progressIndicator: ProgressIndicator?, runnable: Runnable): Future<*>?
 
-interface CompletionThreading {
-
-  Future<?> startThread(final ProgressIndicator progressIndicator, Runnable runnable);
-
-  WeighingDelegate delegateWeighing(CompletionProgressIndicator indicator);
+  fun delegateWeighing(indicator: CompletionProgressIndicator): WeighingDelegate
 }
 
-interface WeighingDelegate extends Consumer<CompletionResult> {
-  void waitFor();
+@Suppress("UsagesOfObsoleteApi")
+internal interface WeighingDelegate : com.intellij.util.Consumer<CompletionResult> {
+  fun waitFor()
 }
 
-final class SyncCompletion extends CompletionThreadingBase {
-  private final List<CompletionResult> myBatchList = new ArrayList<>();
+internal class SyncCompletion : CompletionThreadingBase() {
+  private val batchList = ArrayList<CompletionResult>()
 
-  @Override
-  public Future<?> startThread(final ProgressIndicator progressIndicator, Runnable runnable) {
-    ProgressManager.getInstance().runProcess(runnable, progressIndicator);
-    return CompletableFuture.completedFuture(true);
+  override fun startThread(progressIndicator: ProgressIndicator?, runnable: Runnable): Future<*> {
+    ProgressManager.getInstance().runProcess(runnable, progressIndicator)
+    return CompletableFuture.completedFuture(true)
   }
 
-  @Override
-  public WeighingDelegate delegateWeighing(final CompletionProgressIndicator indicator) {
-    return new WeighingDelegate() {
-      @Override
-      public void waitFor() {
-        indicator.addDelayedMiddleMatches();
+  override fun delegateWeighing(indicator: CompletionProgressIndicator): WeighingDelegate {
+    return object : WeighingDelegate {
+      override fun waitFor() {
+        indicator.addDelayedMiddleMatches()
       }
 
-      @Override
-      public void consume(CompletionResult result) {
-        if (ourIsInBatchUpdate.get().booleanValue()) {
-          myBatchList.add(result);
-        } else {
-          indicator.addItem(result);
-        }
-      }
-    };
-  }
-
-  @Override
-  protected void flushBatchResult(CompletionProgressIndicator indicator) {
-    try {
-      indicator.withSingleUpdate(() -> {
-        for (CompletionResult result : myBatchList) {
-          indicator.addItem(result);
-        }
-      });
-    } finally {
-      myBatchList.clear();
-    }
-  }
-}
-
-final class AsyncCompletion extends CompletionThreadingBase {
-  private static final Logger LOG = Logger.getInstance(AsyncCompletion.class);
-  private final ArrayList<CompletionResult> myBatchList = new ArrayList<>();
-  private final LinkedBlockingQueue<Computable<Boolean>> myQueue = new LinkedBlockingQueue<>();
-
-  @Override
-  public Future<?> startThread(final ProgressIndicator progressIndicator, final Runnable runnable) {
-    final Semaphore startSemaphore = new Semaphore();
-    startSemaphore.down();
-    Future<?> future = ApplicationManager.getApplication().executeOnPooledThread(() -> ProgressManager.getInstance().runProcess(() -> {
-      try {
-        startSemaphore.up();
-        ProgressManager.checkCanceled();
-        runnable.run();
-      }
-      catch (ProcessCanceledException ignored) {
-      }
-    }, progressIndicator));
-    startSemaphore.waitFor();
-    return future;
-  }
-
-  @Override
-  public WeighingDelegate delegateWeighing(final CompletionProgressIndicator indicator) {
-
-    final class WeighItems implements Runnable {
-      @Override
-      public void run() {
-        try {
-          while (true) {
-            Computable<Boolean> next = myQueue.poll(30, TimeUnit.MILLISECONDS);
-            if (next != null && !next.compute()) {
-              tryReadOrCancel(indicator, () -> indicator.addDelayedMiddleMatches());
-              return;
-            }
-            indicator.checkCanceled();
-          }
-        } catch (InterruptedException e) {
-          LOG.error(e);
-        }
-      }
-    }
-
-    final Future<?> future = startThread(ProgressWrapper.wrap(indicator), new WeighItems());
-    return new WeighingDelegate() {
-      @Override
-      public void waitFor() {
-        myQueue.offer(new Computable.PredefinedValueComputable<>(false));
-        try {
-          future.get();
-        }
-        catch (InterruptedException | ExecutionException e) {
-          LOG.error(e);
-        }
-      }
-
-      @Override
-      public void consume(final CompletionResult result) {
-        if (ourIsInBatchUpdate.get().booleanValue()) {
-          myBatchList.add(result);
+      override fun consume(result: CompletionResult) {
+        if (isInBatchUpdate.get()) {
+          batchList.add(result)
         }
         else {
-          myQueue.offer(() -> {
-            tryReadOrCancel(indicator, () -> indicator.addItem(result));
-            return true;
-          });
+          indicator.addItem(result)
         }
       }
-    };
+    }
   }
 
-  @Override
-  protected void flushBatchResult(CompletionProgressIndicator indicator) {
-    ArrayList<CompletionResult> batchListCopy = new ArrayList<>(myBatchList);
-    myBatchList.clear();
-
-    myQueue.offer(() -> {
-      tryReadOrCancel(indicator, () ->
-      indicator.withSingleUpdate(() -> {
-        for (CompletionResult result : batchListCopy) {
-          indicator.addItem(result);
+  override fun flushBatchResult(indicator: CompletionProgressIndicator) {
+    try {
+      indicator.withSingleUpdate {
+        for (result in batchList) {
+          indicator.addItem(result)
         }
-      }));
-      return true;
-    });
+      }
+    }
+    finally {
+      batchList.clear()
+    }
+  }
+}
+
+internal class AsyncCompletion : CompletionThreadingBase() {
+  private val batchList = ArrayList<CompletionResult>()
+  private val queue = LinkedBlockingQueue<Computable<Boolean>>()
+
+  companion object {
+    private val LOG = logger<AsyncCompletion>()
+
+    @JvmStatic
+    fun tryReadOrCancel(indicator: ProgressIndicator, runnable: Runnable) {
+      if (!ApplicationManagerEx.getApplicationEx().tryRunReadAction {
+          indicator.checkCanceled()
+          runnable.run()
+        }) {
+        indicator.cancel()
+        indicator.checkCanceled()
+      }
+    }
   }
 
-  static void tryReadOrCancel(ProgressIndicator indicator, Runnable runnable) {
-    if (!ApplicationManagerEx.getApplicationEx().tryRunReadAction(() -> {
-      indicator.checkCanceled();
-      runnable.run();
-    })) {
-      indicator.cancel();
-      indicator.checkCanceled();
+  override fun startThread(progressIndicator: ProgressIndicator?, runnable: Runnable): Future<*> {
+    val startSemaphore = Semaphore()
+    startSemaphore.down()
+    val future = ApplicationManager.getApplication().executeOnPooledThread {
+      ProgressManager.getInstance().runProcess(
+        {
+          try {
+            startSemaphore.up()
+            ProgressManager.checkCanceled()
+            runnable.run()
+          }
+          catch (ignored: ProcessCanceledException) {
+          }
+        }, progressIndicator)
+    }
+    startSemaphore.waitFor()
+    return future
+  }
+
+  override fun delegateWeighing(indicator: CompletionProgressIndicator): WeighingDelegate {
+    class WeighItems : Runnable {
+      override fun run() {
+        try {
+          while (true) {
+            val next = queue.poll(30, TimeUnit.MILLISECONDS)
+            if (next != null && !next.compute()) {
+              tryReadOrCancel(indicator) { indicator.addDelayedMiddleMatches() }
+              return
+            }
+            indicator.checkCanceled()
+          }
+        }
+        catch (e: InterruptedException) {
+          LOG.error(e)
+        }
+      }
+    }
+
+    val future = startThread(ProgressWrapper.wrap(indicator), WeighItems())
+    return object : WeighingDelegate {
+      override fun waitFor() {
+        queue.offer(PredefinedValueComputable(false))
+        try {
+          future.get()
+        }
+        catch (e: InterruptedException) {
+          LOG.error(e)
+        }
+        catch (e: ExecutionException) {
+          LOG.error(e)
+        }
+      }
+
+      override fun consume(result: CompletionResult) {
+        if (isInBatchUpdate.get()) {
+          batchList.add(result)
+        }
+        else {
+          queue.offer(Computable {
+            tryReadOrCancel(indicator) { indicator.addItem(result) }
+            true
+          })
+        }
+      }
+    }
+  }
+
+  override fun flushBatchResult(indicator: CompletionProgressIndicator) {
+    val batchListCopy = ArrayList(batchList)
+    batchList.clear()
+
+    queue.offer {
+      tryReadOrCancel(indicator) {
+        indicator.withSingleUpdate {
+          for (result in batchListCopy) {
+            indicator.addItem(result)
+          }
+        }
+      }
+      true
     }
   }
 }
