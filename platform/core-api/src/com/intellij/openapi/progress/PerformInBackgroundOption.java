ea1b68b317b3e2e1c237e85a9a1189603dab637b
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2024-01-23 16:46:25 +0100

    IJPL-384 deprecate `PerformInBackgroundOption`
    GitOrigin-RevId: 09b6f13d6db5a8bc674fe88c26446b69c5c4e4b6

--- platform/core-api/src/com/intellij/openapi/progress/PerformInBackgroundOption.java
+++ platform/core-api/src/com/intellij/openapi/progress/PerformInBackgroundOption.java
@@ -1,6 +1,10 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.progress;
 
+/**
+ * @deprecated This is used only inside {@link BackgroundTaskQueue}
+ */
+@Deprecated
 public interface PerformInBackgroundOption {
   /**
    * In this mode the corresponding {@link ProgressIndicator} will be shown in progress dialog with "Background" button.
