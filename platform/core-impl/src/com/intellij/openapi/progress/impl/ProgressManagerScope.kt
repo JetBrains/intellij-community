bdda2317fe9564e5cff7edb20a81d762cc03d990
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2024-01-23 17:00:58 +0100

    mark `ProgressManagerScope` internal as it was intended
    GitOrigin-RevId: 4a5ab9495a43873253eeafcbcf59ab5cb2321780

--- platform/core-impl/src/com/intellij/openapi/progress/impl/ProgressManagerScope.kt
+++ platform/core-impl/src/com/intellij/openapi/progress/impl/ProgressManagerScope.kt
@@ -1,8 +1,10 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.progress.impl
 
 import com.intellij.platform.diagnostic.telemetry.PlatformMetrics
 import com.intellij.platform.diagnostic.telemetry.Scope
+import org.jetbrains.annotations.ApiStatus.Internal
 
+@Internal
 @JvmField
 val ProgressManagerScope: Scope = Scope("progressManager", PlatformMetrics)
