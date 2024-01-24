5404c8e7bc76a9ed5c6f7bb4da81676fa9460595
Ivan Semenov <ivan.semenov@jetbrains.com>
2024-01-24 12:54:06 +0100

    [github] fix review submit button presentation
    GitOrigin-RevId: b5690693f4a15227c671050a3fb81258e20ae600

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/action/GHPRReviewSubmitAction.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/action/GHPRReviewSubmitAction.kt
@@ -3,12 +3,10 @@ package org.jetbrains.plugins.github.pullrequest.action
 
 import com.intellij.collaboration.messages.CollaborationToolsBundle
 import com.intellij.ide.ui.laf.darcula.ui.DarculaButtonUI
-import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.actionSystem.ex.CustomComponentAction
-import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.ui.ClientProperty
@@ -25,33 +23,37 @@ class GHPRReviewSubmitAction : JButtonAction(StringUtil.ELLIPSIS, GithubBundle.m
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
 
   override fun update(e: AnActionEvent) {
-    val vm = e.getData(GHPRReviewViewModel.DATA_KEY)
-    if (vm == null) {
-      e.presentation.isEnabledAndVisible = false
-      return
-    }
+    with(e.presentation) {
+      description = GithubBundle.message("pull.request.review.submit.action.description")
+      val vm = e.getData(GHPRReviewViewModel.DATA_KEY)
+      if (vm == null) {
+        isEnabledAndVisible = false
+        return
+      }
 
-    e.presentation.isVisible = true
-    val pendingReviewResult = vm.pendingReview.value
-    e.presentation.isEnabled = !pendingReviewResult.isInProgress
-    e.presentation.putClientProperty(PROP_PREFIX, getPrefix(e.place))
+      isVisible = true
+      val pendingReviewResult = vm.pendingReview.value
+      isEnabled = !pendingReviewResult.isInProgress
 
-    if (e.presentation.isEnabledAndVisible) {
-      val review = pendingReviewResult.result?.getOrNull()
-      e.presentation.text = getText(review?.commentsCount)
-      e.presentation.putClientProperty(DarculaButtonUI.DEFAULT_STYLE_KEY, review != null)
+      if (isEnabledAndVisible) {
+        val review = pendingReviewResult.result?.getOrNull()
+        text = getText(review?.commentsCount)
+        putClientProperty(DarculaButtonUI.DEFAULT_STYLE_KEY, review != null)
+      }
+      else {
+        text = getText(null)
+        putClientProperty(DarculaButtonUI.DEFAULT_STYLE_KEY, false)
+      }
     }
   }
 
-  private fun getPrefix(place: String) = if (place == ActionPlaces.DIFF_TOOLBAR) GithubBundle.message("pull.request.review.submit")
-  else CollaborationToolsBundle.message("review.start.submit.action")
-
   @NlsSafe
-  private fun getText(pendingComments: Int?): String {
-    val builder = StringBuilder()
-    if (pendingComments != null) builder.append(" ($pendingComments)")
-    builder.append(StringUtil.ELLIPSIS)
-    return builder.toString()
+  private fun getText(pendingComments: Int?): String =
+    if (pendingComments == null) {
+      CollaborationToolsBundle.message("review.start.submit.action.short")
+    }
+    else {
+      CollaborationToolsBundle.message("review.start.submit.action.short.with.comments", pendingComments)
   }
 
   override fun actionPerformed(e: AnActionEvent) {
@@ -68,12 +70,6 @@ class GHPRReviewSubmitAction : JButtonAction(StringUtil.ELLIPSIS, GithubBundle.m
 
   override fun updateButtonFromPresentation(button: JButton, presentation: Presentation) {
     super.updateButtonFromPresentation(button, presentation)
-    val prefix = presentation.getClientProperty(PROP_PREFIX) ?: CollaborationToolsBundle.message("review.start.submit.action")
-    button.text = prefix + presentation.text
     ClientProperty.put(button, DarculaButtonUI.DEFAULT_STYLE_KEY, presentation.getClientProperty(DarculaButtonUI.DEFAULT_STYLE_KEY))
-  }
-
-  companion object {
-    private val PROP_PREFIX: Key<String> = Key("PREFIX")
   }
 }
