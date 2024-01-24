2bbec47e16ddb4c23e99d5634d61ea28d9f24a31
anna.gromova <anna.gromova@jetbrains.com>
2024-01-24 15:20:23 +0100

    RIDER-102481 keymap
    GitOrigin-RevId: 435e799840e8b20095640a748c32837a441cfe89

--- plugins/ide-startup/importSettings/src/com/intellij/ide/startup/importSettings/wizard/keymapChooser/KeymapPane.kt
+++ plugins/ide-startup/importSettings/src/com/intellij/ide/startup/importSettings/wizard/keymapChooser/KeymapPane.kt
@@ -1,24 +1,23 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.startup.importSettings.wizard.keymapChooser
 
+import com.intellij.ide.startup.importSettings.chooser.ui.FilledRoundedBorder
+import com.intellij.ide.startup.importSettings.chooser.ui.RoundedPanel
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.ui.JBColor
-import com.intellij.ui.RoundedLineBorder
-import com.intellij.ui.dsl.builder.AlignX
-import com.intellij.ui.dsl.builder.RowLayout
-import com.intellij.ui.dsl.builder.panel
-import com.intellij.ui.dsl.gridLayout.UnscaledGaps
+import com.intellij.ui.components.panels.VerticalLayout
+import com.intellij.util.ui.JBFont
 import com.intellij.util.ui.JBUI
 import java.awt.Color
-import java.awt.font.TextAttribute
+import java.awt.Dimension
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
 import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.SwingConstants
 
 class KeymapPane(val keymap: Keymap) {
-  companion object {
-    val SELECTED_BORDER_COLOR = JBColor(0x3574F0, 0x3574F0)
-    val BORDER_COLOR = JBColor(0xD3D5DB, 0x43454A)
-
-  }
+  private val KEY_BACKGROUND = JBColor(0xDFE1E5, 0x4E5157)
 
   private val names = mutableListOf<JLabel>()
 
@@ -30,38 +29,73 @@ class KeymapPane(val keymap: Keymap) {
       update()
     }
 
+  private val background = JBColor.namedColor("WelcomeScreen.Details.background", JBColor(Color.white, Color(0x313335)))
 
-  private fun update() {
-    names.forEach { it.isVisible = active }
-    pane.border = RoundedLineBorder(if (active) SELECTED_BORDER_COLOR else BORDER_COLOR)
-    pane.background = JBColor.namedColor("WelcomeScreen.Details.background", JBColor(Color.white, Color(0x313335)))
-  }
+  private val activeBorder = FilledRoundedBorder({ RoundedPanel.SELECTED_BORDER_COLOR }, { background }, RoundedPanel.thickness,
+                                                 RoundedPanel.RADIUS)
+  private val regularBorder = FilledRoundedBorder({ RoundedPanel.BORDER_COLOR }, { background }, RoundedPanel.thickness,
+                                                  RoundedPanel.RADIUS)
 
-  val pane = panel {
-    row {
-      label(keymap.name).align(AlignX.CENTER).customize(UnscaledGaps(10, 3, 10, 3)).applyToComponent {
-        font = JBUI.Fonts.label().deriveFont(mapOf(
-          TextAttribute.WEIGHT to TextAttribute.WEIGHT_SEMIBOLD
-        ))
-      }
-    }
-    panel {
+
+  val pane = JPanel().apply {
+    layout = VerticalLayout(0)
+    border = regularBorder
+
+    add(JLabel(keymap.name).apply {
+      font = JBFont.label().asBold()
+      border = JBUI.Borders.empty(11, 0, 13, 0)
+      horizontalAlignment = SwingConstants.CENTER
+      minimumSize = Dimension(0, 0)
+    })
+
+    add(JPanel(GridBagLayout()).apply {
+      isOpaque = false
+      minimumSize = Dimension(0, 0)
+      val gbc1 = GridBagConstraints()
+      gbc1.insets = JBUI.insetsBottom(14)
+      gbc1.gridy = 0
+
       keymap.shortcut.forEach {
-        row {
-          names.add(label(it.name).component)
-          label(it.value)
-        }.layout(RowLayout.PARENT_GRID)
+        gbc1.weightx = 1.0
+        gbc1.weighty = 0.0
+        gbc1.gridx = 0
+        gbc1.anchor = GridBagConstraints.CENTER
+        gbc1.fill = GridBagConstraints.HORIZONTAL
+        val name = JLabel(it.name).apply {
+          names.add(this)
+        }
+        add(name, gbc1)
+
+        gbc1.gridx = 1
+        gbc1.weightx = 0.0
+        gbc1.weighty = 0.0
+        gbc1.fill = GridBagConstraints.NONE
+
+        val keyPanel = JPanel(VerticalLayout(0)).apply {
+          isOpaque = false
+          add(JLabel(it.value).apply {
+            font = JBFont.medium()
+            border = JBUI.Borders.empty(0, 4)
+          })
+          border = FilledRoundedBorder({KEY_BACKGROUND}, {KEY_BACKGROUND}, 2, 7)
+        }
+        border = JBUI.Borders.empty(0, 8)
+
+        add(keyPanel, gbc1)
+        gbc1.gridy += 1
       }
-    }.align(AlignX.CENTER)
+    })
   }
 
-  /*  val pane_ = JPanel(BorderLayout()).apply {
-      add(panel, BorderLayout.CENTER)
-    }*/
   init {
     update()
   }
 
+
+  private fun update() {
+    names.forEach { it.isVisible = active }
+    pane.border = if (active) activeBorder else regularBorder
+  }
 }
 
 data class Keymap(val id: String, val name: @NlsSafe String, val shortcut: List<ShortcutValue>)
