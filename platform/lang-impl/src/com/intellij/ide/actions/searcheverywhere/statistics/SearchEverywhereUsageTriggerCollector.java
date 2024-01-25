46064fd93bb01127fe33709edc66856086f0fb5f
Boris Krylov <boris.krylov@jetbrains.com>
2024-01-25 17:08:00 +0200

    [rider] Adjusting collectors versions according to changes
    GitOrigin-RevId: 42ad3ffc0934dec68d361c1bd19bd48bf4ae4c1f

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/statistics/SearchEverywhereUsageTriggerCollector.java
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/statistics/SearchEverywhereUsageTriggerCollector.java
@@ -43,7 +43,7 @@ public final class SearchEverywhereUsageTriggerCollector extends CounterUsagesCo
                                                             "third.party", "Vcs.Git");
   public static final StringEventField CURRENT_TAB_FIELD = EventFields.String("currentTabId", ourTabs);
 
-  private static final EventLogGroup GROUP = new EventLogGroup("searchEverywhere", 11);
+  private static final EventLogGroup GROUP = new EventLogGroup("searchEverywhere", 12);
 
   public static final EventId2<String, AnActionEvent> DIALOG_OPEN = GROUP.registerEvent("dialogOpen",
                                                                                         CONTRIBUTOR_ID_FIELD,
