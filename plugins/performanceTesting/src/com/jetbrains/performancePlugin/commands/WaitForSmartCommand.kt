3a07a70f418171a9c5b8457053b74fd0d9abfd45
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-01-25 17:07:46 +0100

    IJPL-166 convert WaitForSmartCommand to modern language
    GitOrigin-RevId: 930e73d3a09e041dfce9f85578a12c91a82345a3

--- plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/WaitForSmartCommand.kt
+++ plugins/performanceTesting/src/com/jetbrains/performancePlugin/commands/WaitForSmartCommand.kt
@@ -1,57 +1,60 @@
-package com.jetbrains.performancePlugin.commands;
+package com.jetbrains.performancePlugin.commands
 
+import com.intellij.openapi.project.DumbService.Companion.getInstance
+import com.intellij.openapi.project.DumbService.Companion.isDumb
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.playback.PlaybackContext
+import com.intellij.openapi.ui.playback.commands.AbstractCommand
+import com.intellij.util.Alarm
+import org.jetbrains.concurrency.AsyncPromise
+import org.jetbrains.concurrency.Promise
 
-import com.intellij.openapi.project.DumbService;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.playback.PlaybackContext;
-import com.intellij.openapi.ui.playback.commands.AbstractCommand;
-import com.intellij.util.Alarm;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.concurrency.AsyncPromise;
-import org.jetbrains.concurrency.Promise;
+class WaitForSmartCommand(text: String, line: Int) : AbstractCommand(text, line) {
+  private val myAlarm = Alarm()
 
-public final class WaitForSmartCommand extends AbstractCommand {
-  public static final String PREFIX = CMD_PREFIX + "waitForSmart";
-
-  /**
-   * Wait for 3 seconds after the dumb mode completes.
-   * <p/>
-   * There are background IDE processes (<backgroundPostStartupActivity/>)
-   * that start after 5 seconds of project opening, or in general, at random time.
-   * <p/>
-   * We would like to maximize the chances that the IDE and the project are fully ready for work,
-   * to make our tests predictable and reproducible.
-   * <p/>
-   * This is not a guaranteed solution: there may be services that schedule background tasks
-   * (by means of {@code AppExecutorUtil#getAppExecutorService()}) and it may be hard to know that they all have completed.
-   * <p/>
-   * TODO: a better place for this awaiting is in the ProjectLoaded test script runner.
-   * But for now we use this `waitForSmart` only in indexes-related tests and
-   * call it before every "comparing" command (checkIndices/compareIndexes/etc)
-   */
-  private static final int SMART_MODE_MINIMUM_DELAY = 3000;
-
-  private final Alarm myAlarm = new Alarm();
-
-  public WaitForSmartCommand(@NotNull String text, int line) {
-    super(text, line);
+  override fun _execute(context: PlaybackContext): Promise<Any> {
+    val completion = AsyncPromise<Any>()
+    completeWhenSmartModeIsLongEnough(context.project, completion)
+    return completion
   }
 
-  @Override
-  protected @NotNull Promise<Object> _execute(final @NotNull PlaybackContext context) {
-    AsyncPromise<Object> completion = new AsyncPromise<>();
-    completeWhenSmartModeIsLongEnough(context.getProject(), completion);
-    return completion;
+  private fun completeWhenSmartModeIsLongEnough(project: Project, completion: AsyncPromise<Any>) {
+    getInstance(project).runWhenSmart {
+      myAlarm.addRequest(
+        {
+          if (isDumb(project)) {
+            completeWhenSmartModeIsLongEnough(project, completion)
+          }
+          else {
+            completion.setResult(null)
+          }
+        }, SMART_MODE_MINIMUM_DELAY)
+    }
   }
 
-  private void completeWhenSmartModeIsLongEnough(@NotNull Project project, @NotNull AsyncPromise<Object> completion) {
-    DumbService.getInstance(project).runWhenSmart(() -> myAlarm.addRequest(() -> {
-      if (DumbService.isDumb(project)) {
-        completeWhenSmartModeIsLongEnough(project, completion);
-      }
-      else {
-        completion.setResult(null);
-      }
-    }, SMART_MODE_MINIMUM_DELAY));
+  companion object {
+    const val PREFIX: String = CMD_PREFIX + "waitForSmart"
+
+    /**
+     * Wait for 3 seconds after the dumb mode completes.
+     *
+     *
+     * There are background IDE processes (<backgroundPostStartupActivity></backgroundPostStartupActivity>)
+     * that start after 5 seconds of project opening, or in general, at random time.
+     *
+     *
+     * We would like to maximize the chances that the IDE and the project are fully ready for work,
+     * to make our tests predictable and reproducible.
+     *
+     *
+     * This is not a guaranteed solution: there may be services that schedule background tasks
+     * (by means of `AppExecutorUtil#getAppExecutorService()`) and it may be hard to know that they all have completed.
+     *
+     *
+     * TODO: a better place for this awaiting is in the ProjectLoaded test script runner.
+     * But for now we use this `waitForSmart` only in indexes-related tests and
+     * call it before every "comparing" command (checkIndices/compareIndexes/etc)
+     */
+    private const val SMART_MODE_MINIMUM_DELAY = 3000
   }
 }
