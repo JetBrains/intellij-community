609b161c645f0799a389b028640c5307eb1466e5
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2024-01-25 20:58:54 +0100

    [python] convert poetry to separate module to access from outside module
    GitOrigin-RevId: 0b1f65214832b7ce99f2f3c6762835b16b3cdcf1

--- python/poetry/src/com/intellij/python/community/impl/poetry/PoetryPackageVersionsInspection.kt
+++ python/poetry/src/com/intellij/python/community/impl/poetry/PoetryPackageVersionsInspection.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.jetbrains.python.poetry
+package com.intellij.python.community.impl.poetry
 
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.LocalInspectionToolSession
