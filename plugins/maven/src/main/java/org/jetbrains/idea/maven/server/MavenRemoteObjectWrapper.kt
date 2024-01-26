d671ae5dcff7abe127250e94b006a00d1eca8ea5
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2024-01-26 19:34:02 +0100

    [maven] IDEA-340501 convert MavenRemoteObjectWrapper to kotlin
    GitOrigin-RevId: d8a74f9fd4908bdf227bc8e90783b6d0b55fa74d

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/server/MavenRemoteObjectWrapper.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/server/MavenRemoteObjectWrapper.kt
@@ -1,72 +1,56 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.maven.server;
+package org.jetbrains.idea.maven.server
 
-import com.intellij.openapi.util.NlsContexts;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.idea.maven.server.security.MavenToken;
-import org.jetbrains.idea.maven.utils.MavenProgressIndicator;
+import com.intellij.openapi.util.NlsContexts
+import org.jetbrains.idea.maven.server.security.MavenToken
+import org.jetbrains.idea.maven.utils.MavenProgressIndicator
+import java.io.IOException
+import java.rmi.server.UnicastRemoteObject
+import java.util.*
 
-import java.io.IOException;
-import java.rmi.Remote;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.UUID;
+abstract class MavenRemoteObjectWrapper<T> protected constructor(parent: RemoteObjectWrapper<*>?) : RemoteObjectWrapper<T>(parent) {
+  private class RemoteMavenServerProgressIndicator(private val myProcess: MavenProgressIndicator) : MavenRemoteObject(), MavenServerProgressIndicator {
+    override fun setText(text: @NlsContexts.ProgressText String?) {
+      myProcess.setText(text)
+    }
 
-public abstract class MavenRemoteObjectWrapper<T> extends RemoteObjectWrapper<T> {
+    override fun setText2(text: @NlsContexts.ProgressDetails String?) {
+      myProcess.setText2(text)
+    }
 
-  public static final MavenToken ourToken = new MavenToken(UUID.randomUUID().toString());
+    override fun isCanceled(): Boolean {
+      return myProcess.isCanceled
+    }
 
-  protected MavenRemoteObjectWrapper(@Nullable RemoteObjectWrapper<?> parent) {
-    super(parent);
+    override fun setIndeterminate(value: Boolean) {
+      myProcess.indicator.isIndeterminate = value
+    }
+
+    override fun setFraction(fraction: Double) {
+      myProcess.setFraction(fraction)
+    }
   }
 
-  static <Some extends MavenRemoteObject> Some doWrapAndExport(Some object) {
-    try {
-      Remote remote = UnicastRemoteObject
-        .exportObject(object, 0);
-      if (remote == null) {
-        return null;
+  companion object {
+    @JvmField
+    val ourToken: MavenToken = MavenToken(UUID.randomUUID().toString())
+
+    fun <Some : MavenRemoteObject?> doWrapAndExport(`object`: Some): Some? {
+      try {
+        val remote = UnicastRemoteObject
+          .exportObject(`object`, 0)
+        if (remote == null) {
+          return null
+        }
+        return `object`
       }
-      return object;
-    }
-    catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  public static MavenServerProgressIndicator wrapAndExport(final MavenProgressIndicator indicator) {
-    return doWrapAndExport(new RemoteMavenServerProgressIndicator(indicator));
-  }
-
-  private static class RemoteMavenServerProgressIndicator extends MavenRemoteObject implements MavenServerProgressIndicator {
-    private final MavenProgressIndicator myProcess;
-
-    RemoteMavenServerProgressIndicator(MavenProgressIndicator process) {
-      myProcess = process;
+      catch (e: IOException) {
+        throw RuntimeException(e)
+      }
     }
 
-    @Override
-    public void setText(@NlsContexts.ProgressText String text) {
-      myProcess.setText(text);
-    }
-
-    @Override
-    public void setText2(@NlsContexts.ProgressDetails String text) {
-      myProcess.setText2(text);
-    }
-
-    @Override
-    public boolean isCanceled() {
-      return myProcess.isCanceled();
-    }
-
-    @Override
-    public void setIndeterminate(boolean value) {
-      myProcess.getIndicator().setIndeterminate(value);
-    }
-
-    @Override
-    public void setFraction(double fraction) {
-      myProcess.setFraction(fraction);
+    fun wrapAndExport(indicator: MavenProgressIndicator): MavenServerProgressIndicator {
+      return doWrapAndExport(RemoteMavenServerProgressIndicator(indicator))!!
     }
   }
 }
