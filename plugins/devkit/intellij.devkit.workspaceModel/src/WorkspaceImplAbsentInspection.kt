24b45b896b7e4b0332f541fe6e89d5fbbdb4344f
Stepan Bagritsevich <stsiapan.bahrytsevich@jetbrains.com>
2024-01-26 13:41:02 +0100

    [Workspace Model] IDEA-344060 Fix generator inspections in `devkit.workspaceModel`
    GitOrigin-RevId: 63915516f6b65b0442030467961f10c34f0a4baf

--- plugins/devkit/intellij.devkit.workspaceModel/src/WorkspaceImplAbsentInspection.kt
+++ plugins/devkit/intellij.devkit.workspaceModel/src/WorkspaceImplAbsentInspection.kt
@@ -3,28 +3,51 @@ package com.intellij.devkit.workspaceModel
 
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.LocalQuickFixOnPsiElement
+import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.platform.workspace.storage.WorkspaceEntity
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.workspaceModel.codegen.engine.SKIPPED_TYPES
 import org.jetbrains.kotlin.idea.stubindex.KotlinClassShortNameIndex
-import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtVisitorVoid
 
-internal class WorkspaceImplAbsentInspection: LocalInspectionTool() {
+internal class WorkspaceImplGenerationInspection: LocalInspectionTool() {
 
   override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) = object : KtVisitorVoid() {
     override fun visitClass(klass: KtClass) {
-      if (!klass.isWorkspaceEntity()) return
-      if (klass.name in SKIPPED_TYPES) return
-      if (klass.isAbstractEntity()) return
-      if (klass.name == "Builder") return
-      val foundImplClasses = KotlinClassShortNameIndex.get("${klass.name}Impl", klass.project, GlobalSearchScope.allScope(klass.project))
-      if (!foundImplClasses.isEmpty()) return
-      holder.registerProblem(klass.nameIdentifier!!, DevKitWorkspaceModelBundle.message("inspection.workspace.absent.model.display.name"),
-                             GenerateWorkspaceModelFix(klass.nameIdentifier!!))
+      val superTypeFqn = klass.getWorkspaceModelSuperType()
+      if (superTypeFqn == null) {
+        return
+      }
+
+      val highlightType: ProblemHighlightType
+      val descriptionTemplate: String
+      if (superTypeFqn == WorkspaceEntity::class.qualifiedName) { //is WorkspaceEntity implementation
+        if (klass.name in SKIPPED_TYPES) return
+        if (klass.isAbstractEntity()) return
+        if (klass.name == "Builder") return
+        val foundImplClasses = KotlinClassShortNameIndex["${klass.name}Impl", klass.project, GlobalSearchScope.allScope(klass.project)]
+        if (foundImplClasses.isEmpty()) {
+          descriptionTemplate = DevKitWorkspaceModelBundle.message("inspection.workspace.msg.absent.implementation")
+          highlightType = ProblemHighlightType.GENERIC_ERROR_OR_WARNING
+        } else {
+          descriptionTemplate = DevKitWorkspaceModelBundle.message("inspection.workspace.msg.regenerate.implementation")
+          highlightType = ProblemHighlightType.INFORMATION
+        }
+      } else {
+        descriptionTemplate = DevKitWorkspaceModelBundle.message("inspection.workspace.msg.collect.class.metadata")
+        highlightType = ProblemHighlightType.INFORMATION
+      }
+
+      holder.registerProblem(
+        klass.nameIdentifier!!, descriptionTemplate, highlightType,
+        GenerateWorkspaceModelFix(klass.nameIdentifier!!)
+      )
     }
   }
 }
