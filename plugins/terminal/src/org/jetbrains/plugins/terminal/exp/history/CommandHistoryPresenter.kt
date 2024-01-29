385d782cc55f4e0d3e51cb27448e63c57de36d7b
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2024-01-29 12:47:49 +0200

    [terminal] IDEA-344146 Do not match partial word occurrences in command search
    With the new matcher, the history will be filtered by whole words of the query. So, the history item will match the query if all query words are contained in the item words honoring the query words position.
    
    GitOrigin-RevId: 2175d4923731bef2665ea32e88cbb12b3a20c197

--- plugins/terminal/src/org/jetbrains/plugins/terminal/exp/history/CommandHistoryPresenter.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/exp/history/CommandHistoryPresenter.kt
@@ -1,8 +1,9 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.terminal.exp.history
 
-import com.intellij.codeInsight.lookup.*
-import com.intellij.codeInsight.lookup.impl.LookupImpl
+import com.intellij.codeInsight.lookup.Lookup
+import com.intellij.codeInsight.lookup.LookupEvent
+import com.intellij.codeInsight.lookup.LookupListener
 import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.diagnostic.thisLogger
@@ -10,7 +11,6 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.UserDataHolder
-import com.intellij.util.containers.map2Array
 import org.jetbrains.plugins.terminal.exp.TerminalCommandExecutor
 
 internal class CommandHistoryPresenter(private val project: Project,
@@ -21,12 +21,9 @@ internal class CommandHistoryPresenter(private val project: Project,
   fun showCommandHistory(history: List<String>) {
     val command = editor.document.text
     initialCommand = command
-    val arranger = LookupArranger.DefaultArranger()
     // Reverse the history to move the most recent values to the top.
     // It will be reversed again internally to show them at the bottom.
-    val elements = history.asReversed().map2Array { LookupElementBuilder.create(it) }
-    val lookup = LookupManager.getInstance(project).createLookup(editor, elements, command, arranger) as LookupImpl
-
+    val lookup = CommandHistoryUtil.createLookup(project, editor, command.trim(), history.asReversed())
     lookup.putUserData(IS_COMMAND_HISTORY_LOOKUP_KEY, true)
 
     lookup.addLookupListener(object : LookupListener {
--- plugins/terminal/src/org/jetbrains/plugins/terminal/exp/history/CommandSearchPresenter.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/exp/history/CommandSearchPresenter.kt
@@ -2,24 +2,17 @@
 package org.jetbrains.plugins.terminal.exp.history
 
 import com.intellij.codeInsight.lookup.Lookup
-import com.intellij.codeInsight.lookup.LookupArranger
-import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.codeInsight.lookup.LookupManager
-import com.intellij.codeInsight.lookup.impl.LookupImpl
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.UserDataHolder
-import com.intellij.util.containers.map2Array
 
 class CommandSearchPresenter(private val project: Project, private val editor: Editor) {
   fun showCommandSearch(history: List<String>) {
-    val command = editor.document.text
-    val arranger = LookupArranger.DefaultArranger()
+    val command = editor.document.text.trim()
     // Reverse the history to move the most recent values to the top.
     // It will be reversed again internally to show them at the bottom.
-    val elements = history.asReversed().map2Array { LookupElementBuilder.create(it) }
-    val lookup = LookupManager.getInstance(project).createLookup(editor, elements, command, arranger) as LookupImpl
+    val lookup = CommandHistoryUtil.createLookup(project, editor, command, history.asReversed())
     lookup.putUserData(IS_COMMAND_SEARCH_LOOKUP_KEY, true)
 
     if (lookup.showLookup()) {
