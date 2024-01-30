8d66efd81b085a0dec5d0955f88b7446ac0ee909
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-01-30 17:43:39 +0100

    IDEA-168388 IDEA-306772 QuickDoc + completion: regain editor focus on click within lookup
    GitOrigin-RevId: a2e97abd1455d09edaaa94e99d64bffd509fb069

--- platform/lang-impl/src/com/intellij/codeInsight/lookup/impl/LookupImpl.java
+++ platform/lang-impl/src/com/intellij/codeInsight/lookup/impl/LookupImpl.java
@@ -884,6 +884,7 @@ public class LookupImpl extends LightweightHint implements LookupEx, Disposable,
       public boolean onClick(@NotNull MouseEvent e, int clickCount) {
         setLookupFocusDegree(LookupFocusDegree.FOCUSED);
         markSelectionTouched();
+        editorComponent.requestFocus();
 
         if (clickCount == 2){
           CommandProcessor.getInstance().executeCommand(myProject, () -> finishLookup(NORMAL_SELECT_CHAR), "", null, myEditor.getDocument());
