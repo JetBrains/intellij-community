582e026652a45ec3f066a5c77eb9120a34aa027c
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2024-01-30 12:15:22 +0100

    [maven] IDEA-343266 fix MavenJGitBuildNumberTest.testNoPluginHighlighting
    GitOrigin-RevId: f7e7603ad5d2ec5db1d83763787672ef64e7b51e

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenJGitBuildNumberTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenJGitBuildNumberTest.kt
@@ -125,14 +125,6 @@ class MavenJGitBuildNumberTest : MavenDomTestCase() {
                     """.trimIndent()
     )
 
-    createProjectPom("""
-                       <groupId>test</groupId>
-                       <artifactId>project</artifactId>
-                       <version>1</version>
-                       <properties>
-                         <aaa>${'$'}{<error>git.commitsCount</error>}</aaa></properties>
-                       """.trimIndent())
-
-    checkHighlighting()
+    checkHighlighting(projectPom, Highlight(text = "git.commitsCount"))
   }
 }
