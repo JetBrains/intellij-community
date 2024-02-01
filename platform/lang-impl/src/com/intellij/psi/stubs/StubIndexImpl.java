4706087c1b42e2f669fd456ffeed9c39d6ff48e7
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2024-02-01 15:23:51 +0100

    `waitUntilStubIndexedInitialized`: ignore `AlreadyDisposedException` because it means that there is nothing to await
    GitOrigin-RevId: 1bdef5dfb82efd7556fce4b3b0f25d2ae9d488b6

--- platform/lang-impl/src/com/intellij/psi/stubs/StubIndexImpl.java
+++ platform/lang-impl/src/com/intellij/psi/stubs/StubIndexImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.stubs;
 
 import com.google.common.util.concurrent.Futures;
@@ -104,7 +104,12 @@ public final class StubIndexImpl extends StubIndexEx {
   @ApiStatus.Internal
   @TestOnly
   public void waitUntilStubIndexedInitialized() {
-    getAsyncState();
+    try {
+      getAsyncState();
+    }
+    catch (AlreadyDisposedException ade) {
+      // it's ok, nothing to await
+    }
   }
 
   @Override
