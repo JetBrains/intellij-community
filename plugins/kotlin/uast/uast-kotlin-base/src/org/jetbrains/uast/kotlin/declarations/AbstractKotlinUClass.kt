35490f4d6adc51ee00d5c1028b39fda762b7c234
Jinseong Jeon <jsjeon@google.com>
2024-01-31 16:03:25 -0800

    K2 UAST: create fake PSI for declarations with value class in its signature
    ^KTIJ-26853
    
    GitOrigin-RevId: 97ceb26999e94ffc0d3a6b1f6e6ab00505c38691

--- plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/declarations/AbstractKotlinUClass.kt
+++ plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/declarations/AbstractKotlinUClass.kt
@@ -155,8 +155,19 @@ abstract class AbstractKotlinUClass(
         return maybeFakeGetter to maybeFakeSetter
     }
 
-    private fun PropertyAccessorsPsiMethods.needsFakeAccessors(property: KtProperty): Pair<Boolean, Boolean> {
-        if (property.annotationEntries.isEmpty()) return false to false
+    private fun PropertyAccessorsPsiMethods.needsFakeAccessors(property: KtProperty): NeedFakeAccessors {
+
+        fun needsFakeGetter() = getter == null
+
+        fun needsFakeSetter() = property.isVar && setter == null
+
+        // In K2/SLC, declarations whose signature has value class are not modeled.
+        if (baseResolveProviderService.hasTypeForValueClassInSignature(property)) {
+            return NeedFakeAccessors(needsFakeGetter(), needsFakeSetter())
+        }
+
+        // Deprecated
+        if (property.annotationEntries.isEmpty()) return NeedFakeAccessors.none()
         var (needsFakeGetter, needsFakeSetter) = false to false
         for (entry in property.annotationEntries) {
             if (!entry.isDeprecated()) {
@@ -165,19 +176,28 @@ abstract class AbstractKotlinUClass(
             // Instead of checking attribute value for deprecation level, use if LC generates accessors or not.
             when (entry.useSiteTarget?.getAnnotationUseSiteTarget()) {
                 AnnotationUseSiteTarget.PROPERTY_GETTER -> {
-                    needsFakeGetter = getter == null
+                    needsFakeGetter = needsFakeGetter()
                 }
                 AnnotationUseSiteTarget.PROPERTY_SETTER -> {
-                    needsFakeSetter = property.isVar && setter == null
+                    needsFakeSetter = needsFakeSetter()
                 }
                 null -> {
-                    needsFakeGetter = getter == null
-                    needsFakeSetter = property.isVar && setter == null
+                    needsFakeGetter = needsFakeGetter()
+                    needsFakeSetter = needsFakeSetter()
                 }
                 else -> {}
             }
         }
-        return needsFakeGetter to needsFakeSetter
+        return NeedFakeAccessors(needsFakeGetter, needsFakeSetter)
+    }
+
+    private data class NeedFakeAccessors(
+        val needGetter: Boolean,
+        val needSetter: Boolean,
+    ) {
+        companion object {
+            fun none(): NeedFakeAccessors = NeedFakeAccessors(false, false)
+        }
     }
 
     private fun KtAnnotationEntry.isDeprecated(): Boolean {
