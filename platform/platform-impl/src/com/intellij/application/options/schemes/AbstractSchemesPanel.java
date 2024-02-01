5aad493053aa6739d7be79a416797f812f40c40d
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2024-02-01 12:20:13 +0100

    IDEA-336414 Remove redundant comment
    GitOrigin-RevId: 4d223494368bd931f36b0d46634d3a9b8d37883b

--- platform/platform-impl/src/com/intellij/application/options/schemes/AbstractSchemesPanel.java
+++ platform/platform-impl/src/com/intellij/application/options/schemes/AbstractSchemesPanel.java
@@ -1,8 +1,6 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.application.options.schemes;
 
-//import com.intellij.application.options.colors.GroupInfo;
-
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.HelpTooltip;
 import com.intellij.ide.IdeBundle;
