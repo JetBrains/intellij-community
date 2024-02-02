c6988c71d991c23dd6aeb168f76263d01309fb6a
Alexey Kudravtsev <cdr@intellij.com>
2024-02-02 12:25:32 +0100

    cleanup (part of KTIJ-26893 K2: Create*FromUsageFix fix)
    GitOrigin-RevId: 6d6041885922d00fbac6bfe6ada6bc9d10f639da

--- plugins/kotlin/code-insight/kotlin.code-insight.k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/quickFixes/createFromUsage/CreateKotlinCallableAction.kt
+++ plugins/kotlin/code-insight/kotlin.code-insight.k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/quickFixes/createFromUsage/CreateKotlinCallableAction.kt
@@ -43,7 +43,7 @@ internal class CreateKotlinCallableAction(
     private val containerClassFqName: FqName? = (getContainer() as? KtClassOrObject)?.fqName
 
     // Note that this property must be initialized after initializing above properties, because it has dependency on them.
-    private val callableDefinitionAsString = buildCallableAsString()
+    private val callableDefinitionAsString: String? = buildCallableAsString()
 
     override fun getActionGroup(): JvmActionGroup = if (abstract) CreateAbstractMethodActionGroup else CreateMethodActionGroup
 
@@ -65,9 +65,9 @@ internal class CreateKotlinCallableAction(
     override fun getText(): String = myText
 
     override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
-        callableDefinitionAsString?.let { callableDefinition ->
+        if (callableDefinitionAsString != null) {
             val callableInfo = NewCallableInfo(
-                callableDefinition,
+                callableDefinitionAsString,
                 candidatesOfParameterNames,
                 candidatesOfRenderedParameterTypes,
                 candidatesOfRenderedReturnType,
