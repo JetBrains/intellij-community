d2a30837d98bc9a03d56799c3e1ce013799a95e6
Anna Kozlova <anna.kozlova@jetbrains.com>
2024-02-02 17:58:30 +0100

    [kotlin] k2 inline function: extract common code
    KTIJ-28664
    
    GitOrigin-RevId: cf8a0209cbd8d9b6800639c8824f01cb476abb0d

--- plugins/kotlin/refactorings/kotlin.refactorings.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/inline/KotlinInlineFunctionHandler.kt
+++ plugins/kotlin/refactorings/kotlin.refactorings.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/inline/KotlinInlineFunctionHandler.kt
@@ -3,9 +3,12 @@ package org.jetbrains.kotlin.idea.k2.refactoring.inline
 
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.refactoring.RefactoringBundle
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.refactoring.inline.AbstractKotlinInlineFunctionHandler
+import org.jetbrains.kotlin.idea.refactoring.inline.codeInliner.findSimpleNameReference
+import org.jetbrains.kotlin.idea.util.application.isUnitTestMode
 import org.jetbrains.kotlin.psi.KtFunction
 import org.jetbrains.kotlin.psi.KtNamedFunction
 
@@ -22,5 +25,25 @@ class KotlinInlineFunctionHandler: AbstractKotlinInlineFunctionHandler<KtNamedFu
         )
 
         return showErrorHint(project, editor, message)
+        //todo
+        val nameReference = editor?.findSimpleNameReference()
+
+        val recursive = true
+        val dialog = KotlinInlineNamedFunctionDialog(
+            function,
+            nameReference,
+            allowToInlineThisOnly = recursive,
+            editor = editor,
+        )
+
+        if (!isUnitTestMode()) {
+            dialog.show()
+        } else {
+            try {
+                dialog.doAction()
+            } finally {
+                dialog.close(DialogWrapper.OK_EXIT_CODE, true)
+            }
+        }
     }
 }
--- plugins/kotlin/refactorings/kotlin.refactorings.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/inline/KotlinInlineNamedFunctionDialog.kt
+++ plugins/kotlin/refactorings/kotlin.refactorings.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/inline/KotlinInlineNamedFunctionDialog.kt
@@ -1,16 +1,11 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
-package org.jetbrains.kotlin.idea.refactoring.inline
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.k2.refactoring.inline
 
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.help.HelpManager
 import com.intellij.psi.PsiReference
 import com.intellij.refactoring.BaseRefactoringProcessor
-import com.intellij.refactoring.HelpID
-import org.jetbrains.kotlin.idea.refactoring.KotlinCommonRefactoringSettings
-import org.jetbrains.kotlin.psi.KtConstructor
+import org.jetbrains.kotlin.idea.refactoring.inline.AbstractKotlinInlineNamedFunctionDialog
 import org.jetbrains.kotlin.psi.KtNamedFunction
-import kotlin.reflect.KMutableProperty1
 
 class KotlinInlineNamedFunctionDialog(
     function: KtNamedFunction,
