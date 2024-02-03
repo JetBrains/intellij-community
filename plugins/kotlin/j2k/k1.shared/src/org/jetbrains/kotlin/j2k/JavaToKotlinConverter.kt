431f2bca0eb521a937cff1cb6b6c2e5bd04a139c
Alexey Belkov <alexey.belkov@jetbrains.com>
2024-02-03 23:41:50 +0400

    [kotlin] J2K: Update copyright
    GitOrigin-RevId: e0b7c02e46f30c585632aace1c84e78e0da6face

--- plugins/kotlin/j2k/k1.shared/src/org/jetbrains/kotlin/j2k/JavaToKotlinConverter.kt
+++ plugins/kotlin/j2k/k1.shared/src/org/jetbrains/kotlin/j2k/JavaToKotlinConverter.kt
@@ -1,4 +1,5 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
 package org.jetbrains.kotlin.j2k
 
 import com.intellij.openapi.progress.EmptyProgressIndicator
