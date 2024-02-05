05a82a0f4702b555d7b84c9d64d4396a1d0c79f4
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2024-02-05 15:06:47 +0100

    [jvm] Set SDK in `JvmSdkInspectionTestBase`
    GitOrigin-RevId: 79158e96a0ec0fa9bc6dba7357d5eb3a44354bc8

--- jvm/jvm-analysis-internal-testFramework/src/com/intellij/jvm/analysis/internal/testFramework/JvmSdkInspectionTestBase.kt
+++ jvm/jvm-analysis-internal-testFramework/src/com/intellij/jvm/analysis/internal/testFramework/JvmSdkInspectionTestBase.kt
@@ -4,12 +4,15 @@ import com.intellij.jvm.analysis.testFramework.JvmInspectionTestBase
 import com.intellij.openapi.roots.LanguageLevelProjectExtension
 import com.intellij.pom.java.LanguageLevel
 import com.intellij.testFramework.IdeaTestUtil
+import com.intellij.testFramework.LightProjectDescriptor
 import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture
 
 abstract class JvmSdkInspectionTestBase : JvmInspectionTestBase() {
   open val languageLevel: LanguageLevel = LanguageLevel.HIGHEST
 
   open val sdkLevel: LanguageLevel = LanguageLevel.HIGHEST
+
+  override fun getProjectDescriptor(): LightProjectDescriptor = ProjectDescriptor(sdkLevel)
 
   protected fun JavaCodeInsightTestFixture.setLanguageLevel(languageLevel: LanguageLevel) {
     LanguageLevelProjectExtension.getInstance(project).languageLevel = languageLevel
