1a7600bee27c081cb67677f8d1c4214f9557af67
Alexey Belkov <alexey.belkov@jetbrains.com>
2024-02-05 18:58:18 +0400

    [kotlin] J2K: JKSymbolProvider cleanup
    GitOrigin-RevId: e302b1b94975598d8d4bf1fb2b200b1858c5c445

--- plugins/kotlin/j2k/k1.new/src/org/jetbrains/kotlin/nj2k/JKSymbolProvider.kt
+++ plugins/kotlin/j2k/k1.new/src/org/jetbrains/kotlin/nj2k/JKSymbolProvider.kt
@@ -14,61 +14,54 @@ import org.jetbrains.kotlin.nj2k.types.JKTypeFactory
 import org.jetbrains.kotlin.psi.*
 
 internal class JKSymbolProvider(private val resolver: JKResolver) {
-    private val symbolsByFqName = mutableMapOf<String, JKSymbol>()
-    private val symbolsByFqNameWithExactSignature = mutableMapOf<List<String>, JKSymbol>()
-    val symbolsByPsi = mutableMapOf<PsiElement, JKSymbol>()
-    private val symbolsByJK = mutableMapOf<JKDeclaration, JKSymbol>()
-
-    private val elementVisitor = ElementVisitor()
     lateinit var typeFactory: JKTypeFactory
 
-    fun preBuildTree(inputElements: List<PsiElement>) {
-        inputElements.forEach { it.accept(elementVisitor) }
-    }
+    val symbolsByPsi: MutableMap<PsiElement, JKSymbol> = mutableMapOf()
 
-    private fun symbolForNonKotlinElement(psi: PsiElement) =
-        when (psi) {
-            is PsiTypeParameter -> JKMultiverseTypeParameterSymbol(psi, typeFactory)
-            is KtTypeParameter -> JKMultiverseKtTypeParameterSymbol(psi, typeFactory)
-            is KtEnumEntry -> JKMultiverseKtEnumEntrySymbol(psi, typeFactory)
-            is PsiClass -> JKMultiverseClassSymbol(psi, typeFactory)
-            is KtClassOrObject -> JKMultiverseKtClassSymbol(psi, typeFactory)
-            is PsiMethod -> JKMultiverseMethodSymbol(psi, typeFactory)
-            is PsiField -> JKMultiverseFieldSymbol(psi, typeFactory)
-            is KtFunction -> JKMultiverseFunctionSymbol(psi, typeFactory)
-            is KtProperty -> JKMultiversePropertySymbol(psi, typeFactory)
-            is KtParameter -> JKMultiversePropertySymbol(psi, typeFactory)
-            is PsiParameter -> JKMultiverseFieldSymbol(psi, typeFactory)
-            is PsiLocalVariable -> JKMultiverseFieldSymbol(psi, typeFactory)
-            is PsiPackage -> JKMultiversePackageSymbol(psi, typeFactory)
-            is KtTypeAlias -> JKTypeAliasKtClassSymbol(psi, typeFactory)
-            else -> TODO(psi::class.toString())
+    private val symbolsByFqName: MutableMap<String, JKSymbol> = mutableMapOf()
+    private val symbolsByFqNameWithExactSignature: MutableMap<List<String>, JKSymbol> = mutableMapOf()
+    private val symbolsByJK: MutableMap<JKDeclaration, JKSymbol> = mutableMapOf()
+
+    fun preBuildTree(inputElements: List<PsiElement>) {
+        val visitor = SymbolProviderVisitor(symbolProvider = this)
+        for (element in inputElements) {
+            element.accept(visitor)
         }
+    }
 
     fun provideDirectSymbol(psi: PsiElement): JKSymbol {
         return symbolsByPsi.getOrPut(psi) {
-            if (psi is KtLightDeclaration<*, *>)
-                psi.kotlinOrigin
-                    ?.let { provideDirectSymbol(it) }
-                    ?: symbolForNonKotlinElement(psi)
-            else symbolForNonKotlinElement(psi)
+            if (psi is KtLightDeclaration<*, *>) {
+                psi.kotlinOrigin?.let { provideDirectSymbol(it) } ?: createDirectSymbol(psi)
+            } else {
+                createDirectSymbol(psi)
+            }
         }
     }
 
-    internal inline fun <reified T : JKSymbol> provideSymbolForReference(reference: PsiReference): T {
+    inline fun <reified T : JKSymbol> provideSymbolForReference(reference: PsiReference): T {
         val target = reference.resolve()
         if (target is LightRecordMethod) {
             val field = getFieldForComponent(target.recordComponent)
             if (field != null) return provideDirectSymbol(field) as T
         }
         if (target != null) return provideDirectSymbol(target) as T
-        return (if (isAssignable<T, JKUnresolvedField>()) JKUnresolvedField(
-            reference.canonicalText,
-            typeFactory
-        ) else JKUnresolvedMethod(reference, typeFactory)) as T
+        val unresolvedSymbol =
+            if (isAssignable<T, JKUnresolvedField>()) JKUnresolvedField(reference.canonicalText, typeFactory)
+            else JKUnresolvedMethod(reference, typeFactory)
+        return unresolvedSymbol as T
     }
 
-    fun provideUniverseSymbol(psi: PsiElement, declaration: JKDeclaration): JKSymbol = provideUniverseSymbol(psi).also { symbol ->
+    fun transferSymbol(to: JKDeclaration, from: JKDeclaration): JKSymbol? =
+        symbolsByJK[from]?.also {
+            @Suppress("UNCHECKED_CAST")
+            it as JKUniverseSymbol<JKDeclaration>
+            it.target = to
+            symbolsByJK[to] = it
+        }
+
+    fun provideUniverseSymbol(psi: PsiElement, declaration: JKDeclaration): JKSymbol {
+        val symbol = provideUniverseSymbol(psi)
         when (symbol) {
             is JKUniverseClassSymbol -> symbol.target = declaration as JKClass
             is JKUniverseFieldSymbol -> symbol.target = declaration as JKVariable
@@ -77,6 +70,7 @@ internal class JKSymbolProvider(private val resolver: JKResolver) {
             is JKUniversePackageSymbol -> symbol.target = declaration as JKPackageDeclaration
         }
         symbolsByJK[declaration] = symbol
+        return symbol
     }
 
     fun provideUniverseSymbol(psi: PsiElement): JKSymbol =
@@ -87,16 +81,9 @@ internal class JKSymbolProvider(private val resolver: JKResolver) {
                 is PsiTypeParameter -> JKUniverseTypeParameterSymbol(typeFactory)
                 is PsiClass -> JKUniverseClassSymbol(typeFactory)
                 is PsiPackageStatement -> JKUniversePackageSymbol(typeFactory)
-                else -> TODO()
+                else -> error("Unexpected argument type: ${psi::class}")
             }
         }
-
-    fun transferSymbol(to: JKDeclaration, from: JKDeclaration) = symbolsByJK[from]?.also {
-        @Suppress("UNCHECKED_CAST")
-        it as JKUniverseSymbol<JKDeclaration>
-        it.target = to
-        symbolsByJK[to] = it
-    }
 
     fun provideUniverseSymbol(jk: JKClass): JKClassSymbol = symbolsByJK.getOrPut(jk) {
         JKUniverseClassSymbol(typeFactory).also { it.target = jk }
@@ -125,7 +112,6 @@ internal class JKSymbolProvider(private val resolver: JKResolver) {
         is JKTypeParameter -> provideUniverseSymbol(jk)
         else -> null
     } as? JKUniverseSymbol<*>
-
 
     fun provideClassSymbol(fqName: FqName): JKClassSymbol =
         symbolsByFqName.getOrPutIfNotNull(fqName.asString()) {
@@ -161,6 +147,27 @@ internal class JKSymbolProvider(private val resolver: JKResolver) {
         } as? JKMethodSymbol ?: JKUnresolvedMethod(methodFqName, typeFactory)
     }
 
+    fun provideFieldSymbol(fqName: String): JKFieldSymbol =
+        provideFieldSymbol(fqName.asSafeFqName())
+
+    private fun createDirectSymbol(psi: PsiElement): JKSymbol = when (psi) {
+        is PsiTypeParameter -> JKMultiverseTypeParameterSymbol(psi, typeFactory)
+        is KtTypeParameter -> JKMultiverseKtTypeParameterSymbol(psi, typeFactory)
+        is KtEnumEntry -> JKMultiverseKtEnumEntrySymbol(psi, typeFactory)
+        is PsiClass -> JKMultiverseClassSymbol(psi, typeFactory)
+        is KtClassOrObject -> JKMultiverseKtClassSymbol(psi, typeFactory)
+        is PsiMethod -> JKMultiverseMethodSymbol(psi, typeFactory)
+        is PsiField -> JKMultiverseFieldSymbol(psi, typeFactory)
+        is KtFunction -> JKMultiverseFunctionSymbol(psi, typeFactory)
+        is KtProperty -> JKMultiversePropertySymbol(psi, typeFactory)
+        is KtParameter -> JKMultiversePropertySymbol(psi, typeFactory)
+        is PsiParameter -> JKMultiverseFieldSymbol(psi, typeFactory)
+        is PsiLocalVariable -> JKMultiverseFieldSymbol(psi, typeFactory)
+        is PsiPackage -> JKMultiversePackageSymbol(psi, typeFactory)
+        is KtTypeAlias -> JKTypeAliasKtClassSymbol(psi, typeFactory)
+        else -> error("Unexpected argument type: ${psi::class}")
+    }
+
     private fun provideFieldSymbol(fqName: FqName): JKFieldSymbol =
         symbolsByFqName.getOrPutIfNotNull(fqName.asString()) {
             resolver.resolveField(fqName)?.let {
@@ -168,63 +175,60 @@ internal class JKSymbolProvider(private val resolver: JKResolver) {
             }
         } as? JKFieldSymbol ?: JKUnresolvedField(fqName.asString(), typeFactory)
 
-    fun provideFieldSymbol(fqName: String): JKFieldSymbol =
-        provideFieldSymbol(fqName.asSafeFqName())
+    private inline fun <reified A, reified B> isAssignable(): Boolean = A::class.java.isAssignableFrom(B::class.java)
 
-    private inner class ElementVisitor : JavaElementVisitor() {
-        override fun visitElement(element: PsiElement) {
-            element.acceptChildren(this)
-        }
-
-        override fun visitClass(aClass: PsiClass) {
-            provideUniverseSymbol(aClass)
-            aClass.acceptChildren(this)
-        }
-
-        override fun visitField(field: PsiField) {
-            provideUniverseSymbol(field)
-        }
-
-        override fun visitParameter(parameter: PsiParameter) {
-            provideUniverseSymbol(parameter)
-        }
-
-        override fun visitMethod(method: PsiMethod) {
-            provideUniverseSymbol(method)
-            method.acceptChildren(this)
-        }
-
-        override fun visitEnumConstant(enumConstant: PsiEnumConstant) {
-            provideUniverseSymbol(enumConstant)
-            enumConstant.acceptChildren(this)
-        }
-
-        override fun visitTypeParameter(classParameter: PsiTypeParameter) {
-            provideUniverseSymbol(classParameter)
-        }
-
-        override fun visitPackageStatement(statement: PsiPackageStatement) {
-            provideUniverseSymbol(statement)
-        }
-
-        override fun visitFile(file: PsiFile) {
-            file.acceptChildren(this)
+    private inline fun <K, V : Any> MutableMap<K, V>.getOrPutIfNotNull(key: K, defaultValue: () -> V?): V? {
+        val value = get(key)
+        return if (value == null) {
+            val answer = defaultValue() ?: return null
+            put(key, answer)
+            answer
+        } else {
+            value
         }
     }
 
-    internal inline fun <reified A, reified B> isAssignable(): Boolean = A::class.java.isAssignableFrom(B::class.java)
+    private fun String.asSafeFqName(): FqName =
+        FqName(replace('/', '.'))
 }
 
-private inline fun <K, V : Any> MutableMap<K, V>.getOrPutIfNotNull(key: K, defaultValue: () -> V?): V? {
-    val value = get(key)
-    return if (value == null) {
-        val answer = defaultValue() ?: return null
-        put(key, answer)
-        answer
-    } else {
-        value
+private class SymbolProviderVisitor(private val symbolProvider: JKSymbolProvider) : JavaElementVisitor() {
+    override fun visitElement(element: PsiElement) {
+        element.acceptChildren(this)
+    }
+
+    override fun visitClass(aClass: PsiClass) {
+        symbolProvider.provideUniverseSymbol(aClass)
+        aClass.acceptChildren(this)
+    }
+
+    override fun visitField(field: PsiField) {
+        symbolProvider.provideUniverseSymbol(field)
+    }
+
+    override fun visitParameter(parameter: PsiParameter) {
+        symbolProvider.provideUniverseSymbol(parameter)
+    }
+
+    override fun visitMethod(method: PsiMethod) {
+        symbolProvider.provideUniverseSymbol(method)
+        method.acceptChildren(this)
+    }
+
+    override fun visitEnumConstant(enumConstant: PsiEnumConstant) {
+        symbolProvider.provideUniverseSymbol(enumConstant)
+        enumConstant.acceptChildren(this)
+    }
+
+    override fun visitTypeParameter(classParameter: PsiTypeParameter) {
+        symbolProvider.provideUniverseSymbol(classParameter)
+    }
+
+    override fun visitPackageStatement(statement: PsiPackageStatement) {
+        symbolProvider.provideUniverseSymbol(statement)
+    }
+
+    override fun visitFile(file: PsiFile) {
+        file.acceptChildren(this)
     }
 }
-
-private fun String.asSafeFqName() =
-    FqName(replace('/', '.'))
