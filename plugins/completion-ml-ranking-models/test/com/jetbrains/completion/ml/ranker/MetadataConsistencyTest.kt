8d8e6176c50ec8a780c481895f9f121016e8c0a9
Alexey Kalina <alexey.kalina@jetbrains.com>
2024-02-05 16:30:39 +0100

    [ml-completion] ML-2636 Integrate completion ranking model for new terminal
    GitOrigin-RevId: 0ad89ea2de8595001d8697a5dc1d437dee89a0b6

--- plugins/completion-ml-ranking-models/test/com/jetbrains/completion/ml/ranker/MetadataConsistencyTest.kt
+++ plugins/completion-ml-ranking-models/test/com/jetbrains/completion/ml/ranker/MetadataConsistencyTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.completion.ml.ranker
 
 import org.junit.Test
@@ -51,4 +51,7 @@ class MetadataConsistencyTest {
 
   @Test
   fun testCSSMetadata() = ExperimentalCSSMLRankingProvider().assertModelMetadataConsistent()
+
+  @Test
+  fun testShMetadata() = ExperimentalShMLRankingProvider().assertModelMetadataConsistent()
 }
