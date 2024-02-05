87a77676af4eff53ca1b8d606c249854b649c59f
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2024-02-05 15:33:02 +0200

    [terminal] IDEA-344702 Iterate over results on Ctrl+R in command search popup
    GitOrigin-RevId: 395a2f6c0a975c77f03de9806edfbc350935c6ab

--- plugins/terminal/src/org/jetbrains/plugins/terminal/exp/history/CommandSearchAction.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/exp/history/CommandSearchAction.kt
@@ -2,15 +2,18 @@
 package org.jetbrains.plugins.terminal.exp.history
 
 import com.intellij.codeInsight.lookup.LookupManager
+import com.intellij.codeInsight.lookup.impl.LookupImpl
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification
 import com.intellij.openapi.editor.Caret
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler
+import com.intellij.ui.ScrollingUtil
 import org.jetbrains.plugins.terminal.exp.TerminalDataContextUtils.isPromptEditor
 import org.jetbrains.plugins.terminal.exp.TerminalDataContextUtils.promptController
 import org.jetbrains.plugins.terminal.exp.TerminalPromotedEditorAction
+import org.jetbrains.plugins.terminal.exp.history.CommandSearchPresenter.Companion.isTerminalCommandSearch
 
 internal class CommandSearchAction : TerminalPromotedEditorAction(Handler()), ActionRemoteBehaviorSpecification.Disabled {
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
@@ -23,5 +26,22 @@ internal class CommandSearchAction : TerminalPromotedEditorAction(Handler()), Ac
     override fun isEnabledForCaret(editor: Editor, caret: Caret, dataContext: DataContext?): Boolean {
       return editor.isPromptEditor && LookupManager.getActiveLookup(editor) == null
     }
+  }
+}
+
+internal class CommandSearchSelectNextHandler(private val originalHandler: EditorActionHandler) : EditorActionHandler() {
+  override fun doExecute(editor: Editor, caret: Caret?, dataContext: DataContext?) {
+    val lookup = LookupManager.getActiveLookup(editor) as? LookupImpl
+    if (lookup?.isTerminalCommandSearch == true) {
+      ScrollingUtil.moveUp(lookup.list, 0)
+      lookup.markSelectionTouched()
+      lookup.refreshUi(false, true)
+    }
+    else originalHandler.execute(editor, caret, dataContext)
+  }
+
+  override fun isEnabledForCaret(editor: Editor, caret: Caret, dataContext: DataContext?): Boolean {
+    return editor.isPromptEditor && LookupManager.getActiveLookup(editor)?.isTerminalCommandSearch == true
+           || originalHandler.isEnabled(editor, caret, dataContext)
   }
 }
