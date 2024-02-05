ab8f5f5b3a5efc24eb8a6f8d0042b85c6254e753
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2024-02-05 12:09:22 +0100

    [lvcs] record a different name for the amend
    GitOrigin-RevId: 6f61814cb3562bc5e5a9ff568e4094fe07223470

--- platform/vcs-impl/src/com/intellij/vcs/commit/LocalChangesCommitter.kt
+++ platform/vcs-impl/src/com/intellij/vcs/commit/LocalChangesCommitter.kt
@@ -1,10 +1,11 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.commit
 
 import com.intellij.history.LocalHistory
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.NlsContexts
 import com.intellij.openapi.vcs.AbstractVcs
 import com.intellij.openapi.vcs.VcsBundle
 import com.intellij.openapi.vcs.VcsRoot
@@ -13,6 +14,7 @@ import com.intellij.openapi.vcs.changes.ChangesUtil.processChangesByVcs
 import com.intellij.openapi.vcs.changes.committed.CommittedChangesCache
 import com.intellij.openapi.vcs.update.RefreshVFsSynchronously
 import com.intellij.vcs.VcsActivity
+import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.Nls
 
 private val COMMIT_WITHOUT_CHANGES_ROOTS_KEY = Key.create<Collection<VcsRoot>>("Vcs.Commit.CommitWithoutChangesRoots")
@@ -87,7 +89,7 @@ open class LocalChangesCommitter(
 
       VcsDirtyScopeManager.getInstance(project).filePathsDirty(pathsToRefresh, null)
 
-      LocalHistory.getInstance().putEventLabel(project, VcsBundle.message("activity.name.commit.message", commitMessage), VcsActivity.Commit)
+      LocalHistory.getInstance().putEventLabel(project, getLocalHistoryEventName(commitContext, commitMessage), VcsActivity.Commit)
     }
     finally {
       ChangeListManager.getInstance(project).invokeAfterUpdate(true) { fireAfterRefresh() }
@@ -126,4 +128,10 @@ private class ChangeListDataCleaner(val committer: LocalChangesCommitter) : Comm
       changeListManager.editChangeListData(listName, null)
     }
   }
+}
+
+@ApiStatus.Internal
+fun getLocalHistoryEventName(commitContext: CommitContext, commitMessage: String): @NlsContexts.Label String {
+  if (commitContext.isAmendCommitMode) return VcsBundle.message("activity.name.amend.message", commitMessage)
+  return VcsBundle.message("activity.name.commit.message", commitMessage)
 }
