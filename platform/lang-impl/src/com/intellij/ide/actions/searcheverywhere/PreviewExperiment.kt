7309efe3380ac7ebb947399a3ed03a260c0679b6
Dmitry Kozhevnikov <dmitry.kozhevnikov@jetbrains.com>
2024-02-06 12:24:42 +0100

    RIDER-105221, CPP-37368: Exclude Rider and CLion from Search Everywhere Preview
    (cherry picked from commit 7f9b58e9a93ba651d025376b4ec7ea00baf4b557)
    
    IJ-CR-125435
    
    GitOrigin-RevId: d4314b646bc755710acfab81d46f933ce947c03d

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/PreviewExperiment.kt
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/PreviewExperiment.kt
@@ -4,9 +4,12 @@ package com.intellij.ide.actions.searcheverywhere
 import com.intellij.internal.statistic.eventLog.EventLogConfiguration
 import com.intellij.internal.statistic.utils.StatisticsUploadAssistant
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.util.PlatformUtils
 
 object PreviewExperiment {
   val isExperimentEnabled = StatisticsUploadAssistant.isSendAllowed()
                             && ApplicationManager.getApplication().isEAP
                             && EventLogConfiguration.getInstance().bucket < 128
+                            && !PlatformUtils.isRider() // RIDER-105221
+                            && !PlatformUtils.isCLion() // CPP-37368
 }
