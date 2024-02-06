2c1f92d396b58714e35a8a922a8d5296c02791d8
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2024-02-06 10:26:05 +0200

    [python] Cleanup in PyStringConcatenationToFormatIntention
    - use more modern API where applicable
    - give more descriptive names to variables and methods
    - remove unnecessary abstraction of Lists as Collections
    - remove a usage of PyClassTypeImpl
    - remove a redundant counter variable
    - remove casts
    - remove now discouraged final modifiers
    - add nullability annotations
    
    GitOrigin-RevId: bfcd08760d1901417a6f662f8ea0205351fd28e5

--- python/python-psi-impl/src/com/jetbrains/python/codeInsight/intentions/PyStringConcatenationToFormatIntention.java
+++ python/python-psi-impl/src/com/jetbrains/python/codeInsight/intentions/PyStringConcatenationToFormatIntention.java
@@ -11,7 +11,7 @@ import com.jetbrains.python.PyPsiBundle;
 import com.jetbrains.python.PyTokenTypes;
 import com.jetbrains.python.psi.*;
 import com.jetbrains.python.psi.impl.PyBuiltinCache;
-import com.jetbrains.python.psi.types.PyClassTypeImpl;
+import com.jetbrains.python.psi.types.PyClassType;
 import com.jetbrains.python.psi.types.PyType;
 import com.jetbrains.python.psi.types.PyTypeChecker;
 import com.jetbrains.python.psi.types.TypeEvalContext;
@@ -19,7 +19,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
 
@@ -44,29 +43,29 @@ public final class PyStringConcatenationToFormatIntention extends PsiUpdateModCo
 
     PyBinaryExpression topmostBinaryExpr = getTopmostBinaryExpression(element);
 
-    final Collection<PyElementType> operators = getOperators(topmostBinaryExpr);
+    List<PyElementType> operators = getOperators(topmostBinaryExpr);
     for (PyElementType operator : operators) {
       if (operator != PyTokenTypes.PLUS) {
         return null;
       }
     }
 
-    final Collection<PyExpression> expressions = getSimpleExpressions(topmostBinaryExpr);
-    if (expressions.isEmpty()) {
+    List<PyExpression> operands = getOperands(topmostBinaryExpr);
+    if (operands.isEmpty()) {
       return null;
     }
     LanguageLevel languageLevel = LanguageLevel.forElement(element);
     TypeEvalContext typeEvalContext = TypeEvalContext.codeAnalysis(context.project(), context.file());
-    final PyBuiltinCache cache = PyBuiltinCache.getInstance(element);
-    for (PyExpression expression : expressions) {
-      if (expression == null) {
+    PyType stringType = PyBuiltinCache.getInstance(element).getStringType(languageLevel);
+    for (PyExpression operand : operands) {
+      if (operand == null) {
         return null;
       }
-      if (expression instanceof PyStringLiteralExpression) {
+      if (operand instanceof PyStringLiteralExpression) {
         continue;
       }
-      final PyType type = typeEvalContext.getType(expression);
-      final boolean isStringReference = type != null && PyTypeChecker.match(cache.getStringType(languageLevel), type, typeEvalContext);
+      PyType operandType = typeEvalContext.getType(operand);
+      boolean isStringReference = operandType != null && PyTypeChecker.match(stringType, operandType, typeEvalContext);
       if (!isStringReference) {
         return null;
       }
@@ -83,70 +82,63 @@ public final class PyStringConcatenationToFormatIntention extends PsiUpdateModCo
   protected void invoke(@NotNull ActionContext actionContext, @NotNull PyBinaryExpression element, @NotNull ModPsiUpdater updater) {
     PyBinaryExpression topmostBinaryExpr = getTopmostBinaryExpression(element);
 
-    final LanguageLevel languageLevel = LanguageLevel.forElement(topmostBinaryExpr);
-    final boolean useFormatMethod = languageLevel.isAtLeast(LanguageLevel.PYTHON27);
+    LanguageLevel languageLevel = LanguageLevel.forElement(topmostBinaryExpr);
+    boolean useFormatMethod = languageLevel.isAtLeast(LanguageLevel.PYTHON27);
 
     Function<String, String> escaper = StringUtil.escaper(false, "\"'\\");
-    StringBuilder stringLiteral = new StringBuilder();
+    StringBuilder stringFormatExpr = new StringBuilder();
     List<String> parameters = new ArrayList<>();
     Pair<String, String> quotes = Pair.create("\"", "\"");
     boolean quotesDetected = false;
-    final TypeEvalContext context = TypeEvalContext.userInitiated(actionContext.project(), actionContext.file());
-    int paramCount = 0;
+    TypeEvalContext context = TypeEvalContext.userInitiated(actionContext.project(), actionContext.file());
     boolean isUnicode = false;
-    final PyClassTypeImpl unicodeType = PyBuiltinCache.getInstance(topmostBinaryExpr).getObjectType("unicode");
+    PyClassType unicodeType = PyBuiltinCache.getInstance(topmostBinaryExpr).getObjectType("unicode");
 
-    for (PyExpression expression : getSimpleExpressions(topmostBinaryExpr)) {
-      if (expression instanceof PyStringLiteralExpression) {
-        final PyType type = context.getType(expression);
+    for (PyExpression operand : getOperands(topmostBinaryExpr)) {
+      if (operand instanceof PyStringLiteralExpression stringLiteralOperand) {
+        PyType type = context.getType(operand);
         if (type != null && type.equals(unicodeType)) {
           isUnicode = true;
         }
         if (!quotesDetected) {
-          quotes = PyStringLiteralCoreUtil.getQuotes(expression.getText());
+          quotes = PyStringLiteralCoreUtil.getQuotes(operand.getText());
           quotesDetected = true;
         }
-        String value = ((PyStringLiteralExpression)expression).getStringValue();
+        String value = stringLiteralOperand.getStringValue();
         if (!useFormatMethod) {
           value = value.replace("%", "%%");
         }
-        stringLiteral.append(escaper.apply(value));
+        stringFormatExpr.append(escaper.apply(value));
       }
       else {
-        addParamToString(stringLiteral, paramCount, useFormatMethod);
-        parameters.add(expression.getText());
-        ++paramCount;
+        addParamToString(stringFormatExpr, parameters.size(), useFormatMethod);
+        parameters.add(operand.getText());
       }
     }
     if (quotes == null) {
       quotes = Pair.create("\"", "\"");
     }
-    stringLiteral.insert(0, quotes.getFirst());
+    stringFormatExpr.insert(0, quotes.getFirst());
     if (isUnicode && !StringUtil.toLowerCase(quotes.getFirst()).contains("u")) {
-      stringLiteral.insert(0, "u");
+      stringFormatExpr.insert(0, "u");
     }
-    stringLiteral.append(quotes.getSecond());
+    stringFormatExpr.append(quotes.getSecond());
 
     PyElementGenerator elementGenerator = PyElementGenerator.getInstance(actionContext.project());
 
     if (!parameters.isEmpty()) {
       if (useFormatMethod) {
-        stringLiteral.append(".format(").append(StringUtil.join(parameters, ",")).append(")");
+        stringFormatExpr.append(".format(").append(StringUtil.join(parameters, ",")).append(")");
       }
       else {
-        final String paramString = parameters.size() > 1 ? "(" + StringUtil.join(parameters, ",") + ")"
-                                                         : StringUtil.join(parameters, ",");
-        stringLiteral.append(" % ").append(paramString);
+        String paramString = parameters.size() > 1 ? "(" + StringUtil.join(parameters, ",") + ")"
+                                                   : StringUtil.join(parameters, ",");
+        stringFormatExpr.append(" % ").append(paramString);
       }
-      final PyExpression expression = elementGenerator.createFromText(LanguageLevel.getDefault(),
-                                                                      PyExpressionStatement.class, stringLiteral.toString())
-        .getExpression();
-      topmostBinaryExpr.replace(expression);
+      topmostBinaryExpr.replace(elementGenerator.createExpressionFromText(languageLevel, stringFormatExpr.toString()));
     }
     else {
-      PyStringLiteralExpression stringLiteralExpression =
-        elementGenerator.createStringLiteralAlreadyEscaped(stringLiteral.toString());
-      topmostBinaryExpr.replace(stringLiteralExpression);
+      topmostBinaryExpr.replace(elementGenerator.createStringLiteralAlreadyEscaped(stringFormatExpr.toString()));
     }
   }
 
@@ -158,16 +150,16 @@ public final class PyStringConcatenationToFormatIntention extends PsiUpdateModCo
     return result;
   }
 
-  private static Collection<PyExpression> getSimpleExpressions(@NotNull PyBinaryExpression expression) {
+  private static @NotNull List<PyExpression> getOperands(@NotNull PyBinaryExpression expression) {
     List<PyExpression> res = new ArrayList<>();
-    if (expression.getLeftExpression() instanceof PyBinaryExpression) {
-      res.addAll(getSimpleExpressions((PyBinaryExpression)expression.getLeftExpression()));
+    if (expression.getLeftExpression() instanceof PyBinaryExpression leftBinaryOperand) {
+      res.addAll(getOperands(leftBinaryOperand));
     }
     else {
       res.add(expression.getLeftExpression());
     }
-    if (expression.getRightExpression() instanceof PyBinaryExpression) {
-      res.addAll(getSimpleExpressions((PyBinaryExpression)expression.getRightExpression()));
+    if (expression.getRightExpression() instanceof PyBinaryExpression rightBinaryOperand) {
+      res.addAll(getOperands(rightBinaryOperand));
     }
     else {
       res.add(expression.getRightExpression());
@@ -175,21 +167,21 @@ public final class PyStringConcatenationToFormatIntention extends PsiUpdateModCo
     return res;
   }
 
-  private static Collection<PyElementType> getOperators(@NotNull PyBinaryExpression expression) {
+  private static @NotNull List<PyElementType> getOperators(@NotNull PyBinaryExpression expression) {
     List<PyElementType> res = new ArrayList<>();
-    if (expression.getLeftExpression() instanceof PyBinaryExpression) {
-      res.addAll(getOperators((PyBinaryExpression)expression.getLeftExpression()));
+    if (expression.getLeftExpression() instanceof PyBinaryExpression leftBinaryOperand) {
+      res.addAll(getOperators(leftBinaryOperand));
     }
-    if (expression.getRightExpression() instanceof PyBinaryExpression) {
-      res.addAll(getOperators((PyBinaryExpression)expression.getRightExpression()));
+    if (expression.getRightExpression() instanceof PyBinaryExpression rightBinaryOperand) {
+      res.addAll(getOperators(rightBinaryOperand));
     }
     res.add(expression.getOperator());
     return res;
   }
 
-  private static void addParamToString(StringBuilder stringLiteral, int i, boolean useFormatMethod) {
+  private static void addParamToString(@NotNull StringBuilder stringLiteral, int index, boolean useFormatMethod) {
     if (useFormatMethod) {
-      stringLiteral.append("{").append(i).append("}");
+      stringLiteral.append("{").append(index).append("}");
     }
     else {
       stringLiteral.append("%s");
