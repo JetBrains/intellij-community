82deb5011f9af1b8de50f102e2fd88b68d24e9d9
Tagir Valeev <tagir.valeev@jetbrains.com>
2024-02-06 18:03:31 +0100

    [jvm-inspections] KotlinEmptyMethodInspection: moved to JVM category, test superclass updated
    GitOrigin-RevId: 6f93e40ea1a12bb988ad93f9ad384223122da7bf

--- jvm/jvm-analysis-kotlin-tests/testSrc/com/intellij/codeInspection/tests/kotlin/KotlinEmptyMethodInspectionTest.kt
+++ jvm/jvm-analysis-kotlin-tests/testSrc/com/intellij/codeInspection/tests/kotlin/KotlinEmptyMethodInspectionTest.kt
@@ -3,13 +3,13 @@ package com.intellij.codeInspection.tests.kotlin
 import com.intellij.codeInspection.emptyMethod.EmptyMethodInspection
 import com.intellij.codeInspection.ex.GlobalInspectionToolWrapper
 import com.intellij.jvm.analysis.KotlinJvmAnalysisTestUtil
-import com.intellij.jvm.analysis.internal.testFramework.JvmSdkInspectionTestBase
+import com.intellij.jvm.analysis.testFramework.JvmInspectionTestBase
 import com.intellij.testFramework.TestDataPath
 
 private const val inspectionPath = "/codeInspection/emptyMethod"
 
 @TestDataPath("\$CONTENT_ROOT/testData$inspectionPath")
-class KotlinEmptyMethodInspectionTest : JvmSdkInspectionTestBase() {
+class KotlinEmptyMethodInspectionTest : JvmInspectionTestBase() {
   override var inspection = EmptyMethodInspection()
   
   override fun getBasePath() = KotlinJvmAnalysisTestUtil.TEST_DATA_PROJECT_RELATIVE_BASE_PATH + inspectionPath
