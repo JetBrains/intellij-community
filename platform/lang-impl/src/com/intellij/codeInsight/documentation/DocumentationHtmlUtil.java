49fb97a407881f612494f898b409c3b9b9f4a9d0
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-02-07 09:53:04 +0100

    IDEA-334229 Add an option to download documentation into Quickdoc popup - fix content padding and margins
    (cherry picked from commit c5612bf0d8880adb89b23cac4203c74391def5cc)
    
    IJ-CR-125519
    
    GitOrigin-RevId: 661f6ad0d421a6cd1aac352bbca0fecfcb9487f1

--- platform/lang-impl/src/com/intellij/codeInsight/documentation/DocumentationHtmlUtil.java
+++ platform/lang-impl/src/com/intellij/codeInsight/documentation/DocumentationHtmlUtil.java
@@ -43,25 +43,27 @@ public final class DocumentationHtmlUtil {
   }
 
   public static @NotNull List<String> getDocumentationPaneDefaultCssRules(Color background) {
-    int leftPadding = 13;
-    int definitionTopPadding = 4;
+    int leftPadding = 0;
     String linkColor = ColorUtil.toHtmlColor(JBUI.CurrentTheme.Link.Foreground.ENABLED);
     String borderColor = ColorUtil.toHtmlColor(UIUtil.getTooltipSeparatorColor());
     String sectionColor = ColorUtil.toHtmlColor(SECTION_COLOR);
 
     // When updating styles here, consider updating styles in DocRenderer#getStyleSheet
     var result = ContainerUtil.newLinkedList(
-      "html { padding: 10px; }",
+      "html { padding: 10px; margin: 0px }",
+      "body { padding: 0px; margin: 0px }",
       "pre  {white-space: pre-wrap}",  // supported by JetBrains Runtime
       "a { color: " + linkColor + "; text-decoration: none;}",
-      ".definition { padding: " + definitionTopPadding + "px 17px 1px " + leftPadding + "px;" +
+      ".definition { padding: 0px 17px 8px " + leftPadding + "px;" +
+      "              margin-bottom: 2px;"+
       "              border-bottom: thin solid " + borderColor + "; }",
-      ".definition-only { padding: " + definitionTopPadding + "px 10px 0 " + leftPadding + "px; }",
-      ".definition-only pre { margin-bottom: 0 }",
+      ".definition pre { margin: 0px; padding: 0px; }",
+      ".definition-only { padding: 0px 10px 0 " + leftPadding + "px; }",
+      ".definition-only pre { margin: 0px; padding: 0px }",
       ".content { padding: 5px 16px 0 " + leftPadding + "px; max-width: 100% }",
       ".content-separated { padding: 5px 16px 5px " + leftPadding + "px; max-width: 100%;" +
       "                     border-bottom: thin solid " + borderColor + "; }",
-      ".content-only { padding: 8px 16px 0 " + leftPadding + "px; max-width: 100% }",
+      ".content-only { padding: 0px 16px 0 " + leftPadding + "px; max-width: 100% }",
       ".separated { padding: 0 0 4px 0; margin: 0; max-width: 100%;" +
       "             border-bottom: thin solid " + borderColor + "; }",
       ".bottom { padding: 3px 16px 0 " + leftPadding + "px; }",
