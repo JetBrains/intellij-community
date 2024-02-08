6c13102043b3829241e1f26dc477f33104c592fd
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2024-02-08 20:09:36 +0100

    [lvcs] remove the gap between items
    GitOrigin-RevId: 1d7b1d29dc43fda1544cc57520463dde46054b60

--- platform/lvcs-impl/src/com/intellij/platform/lvcs/impl/ui/ActivityItemRenderer.kt
+++ platform/lvcs-impl/src/com/intellij/platform/lvcs/impl/ui/ActivityItemRenderer.kt
@@ -7,24 +7,21 @@ import com.intellij.platform.lvcs.impl.ActivityPresentation
 import com.intellij.ui.JBColor
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBList
-import com.intellij.ui.components.JBPanel
 import com.intellij.ui.components.panels.VerticalLayout
 import com.intellij.ui.hover.ListHoverListener
+import com.intellij.ui.popup.list.SelectablePanel
 import com.intellij.util.text.DateFormatUtil
-import com.intellij.util.ui.GraphicsUtil
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
 import com.intellij.util.ui.components.BorderLayoutPanel
 import java.awt.Color
 import java.awt.Component
-import java.awt.Graphics
 import javax.swing.Icon
 import javax.swing.JList
 import javax.swing.ListCellRenderer
 import javax.swing.SwingConstants
 
 private const val ROW_LEFT_RIGHT_BORDER = 5
-private const val ROW_TOP_BOTTOM_BORDER = 3
 private const val ROW_LEFT_RIGHT_INSETS = 10
 private const val ROW_TOP_BOTTOM_INSETS = 6
 private val BACKGROUND_ARC = JBUI.value(8f)
@@ -35,7 +32,7 @@ internal class ActivityItemRenderer(private val presentationFunction: (item: Act
   private val contentPanel = BorderLayoutPanel().apply {
     isOpaque = true
     putClientProperty(JBList.IGNORE_LIST_ROW_HEIGHT, true)
-    border = JBUI.Borders.empty(ROW_TOP_BOTTOM_BORDER, ROW_LEFT_RIGHT_BORDER)
+    border = JBUI.Borders.empty(0, ROW_LEFT_RIGHT_BORDER)
   }
 
   override fun getListCellRendererComponent(list: JList<out ActivityItem>, value: ActivityItem, index: Int, isSelected: Boolean,
@@ -44,41 +41,28 @@ internal class ActivityItemRenderer(private val presentationFunction: (item: Act
     contentPanel.background = list.background
     val activityPresentation = presentationFunction(value) ?: return contentPanel
 
+    val isSelectionTop = isSelected && (index == 0 || !list.isSelectedIndex(index - 1))
+    val isSelectionBottom = isSelected && (index == list.model.size - 1 || !list.isSelectedIndex(index + 1))
+
     val cellBackgroundColor = if (isSelected) list.selectionBackground
     else if (index == ListHoverListener.getHoveredIndex(list)) JBUI.CurrentTheme.List.Hover.background(cellHasFocus)
     else null
+
     val rowComponent = createRowComponent(list, activityPresentation.text, activityPresentation.icon, value.timestamp,
-                                          cellBackgroundColor, isSelected)
+                                          cellBackgroundColor, isSelected, isSelectionTop, isSelectionBottom)
     contentPanel.addToCenter(rowComponent)
 
     return contentPanel
   }
 }
 
-private class RoundedPanel(var roundedBackgroundColor: Color?) : JBPanel<RoundedPanel>() {
-  init {
-    isOpaque = roundedBackgroundColor != null
-  }
-
-  override fun paintComponent(g: Graphics) {
-    super.paintComponent(g)
-    if (roundedBackgroundColor != null) {
-      val config = GraphicsUtil.setupAAPainting(g)
-
-      val arc = BACKGROUND_ARC.get()
-      if (roundedBackgroundColor != null) {
-        g.color = roundedBackgroundColor
-        g.fillRoundRect(0, 0, width, height, arc, arc)
-      }
-
-      config.restore()
-    }
-  }
-}
-
 private fun createRowComponent(list: JList<*>, @NlsContexts.Label text: String, icon: Icon?, timestamp: Long,
-                               backgroundColor: Color?, isSelected: Boolean): RoundedPanel {
-  val content = RoundedPanel(backgroundColor)
+                               backgroundColor: Color?, isSelected: Boolean, isSelectionTop: Boolean, isSelectionBottom: Boolean): SelectablePanel {
+  val content = SelectablePanel()
+  content.selectionColor = backgroundColor
+  content.selectionArc = if (backgroundColor != null) BACKGROUND_ARC.get() else 0
+  content.selectionArcCorners = if (isSelected) getSelectionArcCorners(isSelectionTop, isSelectionBottom)
+  else if (backgroundColor != null) SelectablePanel.SelectionArcCorners.ALL else SelectablePanel.SelectionArcCorners.NONE
   content.layout = VerticalLayout(VGAP.get(), SwingConstants.LEFT)
   content.font = list.font
   content.foreground = if (isSelected) list.selectionForeground else list.foreground
@@ -97,4 +81,11 @@ private fun createRowComponent(list: JList<*>, @NlsContexts.Label text: String, 
   }
 
   return content
+}
+
+private fun getSelectionArcCorners(isSelectionTop: Boolean, isSelectionBottom: Boolean): SelectablePanel.SelectionArcCorners {
+  return if (isSelectionTop && isSelectionBottom) SelectablePanel.SelectionArcCorners.ALL
+  else if (isSelectionTop) SelectablePanel.SelectionArcCorners.TOP
+  else if (isSelectionBottom) SelectablePanel.SelectionArcCorners.BOTTOM
+  else SelectablePanel.SelectionArcCorners.NONE
 }
