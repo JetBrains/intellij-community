5ecec62a01e2809054e0f46d9884a82437029bdd
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2024-02-08 17:35:06 +0100

    extract `DefaultJava11Shim`
    GitOrigin-RevId: 8f185934c671e41dcd1c9813ddfeab03d85c1f5b

--- platform/util/src/com/intellij/util/Java11Shim.kt
+++ platform/util/src/com/intellij/util/Java11Shim.kt
@@ -1,45 +1,17 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-@file:Suppress("ReplacePutWithAssignment", "ReplaceJavaStaticMethodWithKotlinAnalog")
-
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util
 
 import com.intellij.util.containers.ConcurrentLongObjectMap
 import org.jetbrains.annotations.ApiStatus
-import java.util.*
 
+/**
+ * The implementation of `copyOf` is allowed to not do copy - it can return the same map, read `copyOf` as `immutable`.
+ */
 @ApiStatus.Internal
-// implementation of `copyOf` is allowed to not do copy - it can return the same map, read `copyOf` as `immutable`
 abstract class Java11Shim {
   companion object {
     @JvmField
-    var INSTANCE: Java11Shim = object : Java11Shim() {
-      override fun <K : Any, V> copyOf(map: Map<K, V>) = Collections.unmodifiableMap(map)
-
-      override fun <K : Any, V> mapOf(k: K, v: V): Map<K, V> = Collections.singletonMap(k, v)
-
-      override fun <K : Any, V> mapOf(k: K, v: V, k2: K, v2: V): Map<K, V> {
-        val map = HashMap<K, V>(2)
-        map.put(k, v)
-        map.put(k2, v2)
-        return map
-      }
-
-      override fun <E> copyOf(collection: Collection<E>): Set<E> = Collections.unmodifiableSet(HashSet(collection))
-
-      override fun <V : Any> createConcurrentLongObjectMap(): ConcurrentLongObjectMap<V> {
-        return ConcurrentLongObjectHashMap()
-      }
-
-      override fun <K : Any, V> mapOf(): Map<K, V> = Collections.emptyMap()
-
-      override fun <E> listOf(): List<E> = Collections.emptyList()
-
-      override fun <E> listOf(element: E): List<E> = Collections.singletonList(element)
-
-      override fun <E> listOf(e1: E, e2: E): List<E> = Arrays.asList(e1, e2)
-
-      override fun <E> listOf(array: Array<E>, size: Int) = if (array.size == size) array.asList() else array.asList().subList(0, size)
-    }
+    var INSTANCE: Java11Shim = DefaultJava11Shim()
   }
 
   abstract fun <K : Any, V> copyOf(map: Map<K, V>): Map<K, V>
