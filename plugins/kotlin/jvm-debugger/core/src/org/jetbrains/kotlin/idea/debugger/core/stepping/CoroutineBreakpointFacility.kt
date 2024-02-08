8f96fa929cb8a6df6e418d64cf626c854a921c43
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2024-02-08 19:20:14 +0100

    [debugger] Refactor the scheduling of step over commands for suspend switched: remove redundant class
    GitOrigin-RevId: e49b661209b33f61ed128760fc117c335343b0f3

--- plugins/kotlin/jvm-debugger/core/src/org/jetbrains/kotlin/idea/debugger/core/stepping/CoroutineBreakpointFacility.kt
+++ plugins/kotlin/jvm-debugger/core/src/org/jetbrains/kotlin/idea/debugger/core/stepping/CoroutineBreakpointFacility.kt
@@ -2,15 +2,19 @@
 
 package org.jetbrains.kotlin.idea.debugger.core.stepping
 
+import com.intellij.debugger.DebuggerManagerEx
+import com.intellij.debugger.engine.DebugProcessImpl
 import com.intellij.debugger.engine.StepIntoMethodBreakpoint
 import com.intellij.debugger.engine.SuspendContextImpl
 import com.intellij.debugger.engine.events.SuspendContextCommandImpl
+import com.intellij.debugger.settings.DebuggerSettings
 import com.sun.jdi.Location
 import com.sun.jdi.Method
 import com.sun.jdi.event.LocatableEvent
+import com.sun.jdi.request.EventRequest
 
-object CoroutineBreakpointFacility : AbstractCoroutineBreakpointFacility() {
-    override fun installCoroutineResumedBreakpoint(context: SuspendContextImpl, location: Location, method: Method): Boolean {
+object CoroutineBreakpointFacility {
+    fun installCoroutineResumedBreakpoint(context: SuspendContextImpl, location: Location, method: Method): Boolean {
         val debugProcess = context.debugProcess
         val project = debugProcess.project
 
@@ -19,13 +23,25 @@ object CoroutineBreakpointFacility : AbstractCoroutineBreakpointFacility() {
                 val result = super.processLocatableEvent(action, event)
                 if (!result) return false
 
-                stepOverSuspendSwitch(action, debugProcess)
+                val suspendContextImpl = action.suspendContext ?: return true
+                return scheduleStepOverCommandForSuspendSwitch(suspendContextImpl)
+            }
+
+            private fun scheduleStepOverCommandForSuspendSwitch(it: SuspendContextImpl): Boolean {
+                DebuggerSteppingHelper.createStepOverCommandForSuspendSwitch(it).prepareSteppingRequestsAndHints(it)
+                it.debugProcess.requestsManager.deleteRequest(this)
                 // false return value will resume the execution in the `DebugProcessEvents` and
                 // the scheduled above steps will perform stepping through the coroutine switch until line location.
                 return false
             }
         }
-        breakpoint.setSuspendPolicy(context)
+
+        breakpoint.suspendPolicy = when (context.suspendPolicy) {
+            EventRequest.SUSPEND_ALL -> DebuggerSettings.SUSPEND_ALL
+            EventRequest.SUSPEND_EVENT_THREAD -> DebuggerSettings.SUSPEND_THREAD
+            EventRequest.SUSPEND_NONE -> DebuggerSettings.SUSPEND_NONE
+            else -> DebuggerSettings.SUSPEND_ALL
+        }
         applyEmptyThreadFilter(debugProcess)
         breakpoint.createRequest(debugProcess)
         debugProcess.setSteppingBreakpoint(breakpoint)
@@ -36,4 +52,11 @@ object CoroutineBreakpointFacility : AbstractCoroutineBreakpointFacility() {
 
 fun SuspendContextImpl.getLocationCompat(): Location? {
     return this.location
+}
+
+private fun applyEmptyThreadFilter(debugProcess: DebugProcessImpl) {
+    // TODO this is nasty. Find a way to apply an empty thread filter only to the newly created breakpoint
+    // TODO consider moving this filtering to event loop?
+    val breakpointManager = DebuggerManagerEx.getInstanceEx(debugProcess.project).breakpointManager
+    breakpointManager.removeThreadFilter(debugProcess)
 }
