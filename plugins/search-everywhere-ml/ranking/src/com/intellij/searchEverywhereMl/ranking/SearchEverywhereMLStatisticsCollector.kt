83bf28c5b59252e88bdb2a95d15470295e31c48b
Evgeny Abramov <evgeny.abramov@jetbrains.com>
2024-02-09 14:45:58 +0400

    ML in SE: update event group version after enabling semantic search in Files and Classes tabs
    GitOrigin-RevId: 3ea9df9d90202cf91e1533b03de84532d54d843c

--- plugins/search-everywhere-ml/ranking/src/com/intellij/searchEverywhereMl/ranking/SearchEverywhereMLStatisticsCollector.kt
+++ plugins/search-everywhere-ml/ranking/src/com/intellij/searchEverywhereMl/ranking/SearchEverywhereMLStatisticsCollector.kt
@@ -285,7 +285,7 @@ object SearchEverywhereMLStatisticsCollector : CounterUsagesCollector() {
     return true
   }
 
-  private val GROUP = EventLogGroup("mlse.log", 88, MLSE_RECORDER_ID)
+  private val GROUP = EventLogGroup("mlse.log", 89, MLSE_RECORDER_ID)
 
   private val IS_INTERNAL = EventFields.Boolean("isInternal")
   private val ORDER_BY_ML_GROUP = EventFields.Boolean("orderByMl")
