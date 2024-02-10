9afa6b4f828f7b41355d434b6a946217a778eb2b
Bogdan Kirilenko <bogdan.kirilenko@jetbrains.com>
2024-02-10 16:14:31 +0100

    [pycharm] PY-63670 trying md->html conversion using com.intellij.markdown.utils.convertMarkdownToHtml
    GitOrigin-RevId: a420ac55511c57825b83913bf481fdfb56f9ce94

--- python/huggingFace/src/com/intellij/python/community/impl/huggingFace/documentation/HuggingFaceMarkdownToHtmlConverter.kt
+++ python/huggingFace/src/com/intellij/python/community/impl/huggingFace/documentation/HuggingFaceMarkdownToHtmlConverter.kt
@@ -1,19 +1,9 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.python.community.impl.huggingFace.documentation
 
-import com.vladsch.flexmark.ext.definition.DefinitionExtension
-import com.vladsch.flexmark.ext.footnotes.FootnoteExtension
-import com.vladsch.flexmark.ext.gfm.strikethrough.SubscriptExtension
-import com.vladsch.flexmark.ext.gitlab.GitLabExtension
-import com.vladsch.flexmark.ext.superscript.SuperscriptExtension
-import com.vladsch.flexmark.ext.tables.TablesExtension
-import com.vladsch.flexmark.ext.typographic.TypographicExtension
-import com.vladsch.flexmark.ext.wikilink.WikiLinkExtension
-import com.vladsch.flexmark.html.HtmlRenderer
-import com.vladsch.flexmark.parser.Parser
-import com.vladsch.flexmark.util.data.MutableDataSet
 import com.intellij.openapi.util.NlsSafe
 import org.jetbrains.annotations.Nls
+import com.intellij.markdown.utils.convertMarkdownToHtml
 
 
 class HuggingFaceMarkdownToHtmlConverter {
@@ -36,29 +26,6 @@ class HuggingFaceMarkdownToHtmlConverter {
     val htmlWithFixedQuotes = addDivToBlockquotes(htmlWithFixedPTags)
     val htmlFixedTables = formatTablesInHtml(htmlWithFixedQuotes)
     return wrapPythonCodeInContainer(htmlFixedTables)
-  }
-
-  private fun convertMarkdownToHtml(markdown: String): String {
-    val options = MutableDataSet()
-    options.set(HtmlRenderer.SOFT_BREAK, "<br>\n")
-
-    options.set(
-      Parser.EXTENSIONS,
-      listOf(
-        TablesExtension.create(),
-        TypographicExtension.create(),
-        SuperscriptExtension.create(),
-        GitLabExtension.create(),
-        FootnoteExtension.create(),
-        DefinitionExtension.create(),
-        WikiLinkExtension.create(),
-        SubscriptExtension.create()
-      )
-    )
-    val parser = Parser.builder().build()
-    val document = parser.parse(markdown)
-    val renderer = HtmlRenderer.builder(options).build()
-    return renderer.render(document)
   }
 
   private fun addDivToBlockquotes(html: String): String {
