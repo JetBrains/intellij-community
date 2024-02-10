b455ab1512a0de30aa1a12de5352326f2bfdc64b
Petr <petr.golubev@jetbrains.com>
2024-02-10 15:19:05 +0100

    PY-61639 Introduced syntax highlighter for a thin client
    GitOrigin-RevId: 464dc0a8c63c978ead1d69e2149c8ef450019529

--- python/src/com/jetbrains/python/highlighting/PySyntaxHighlighterFactory.java
+++ python/src/com/jetbrains/python/highlighting/PySyntaxHighlighterFactory.java
@@ -20,51 +20,17 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Map;
 
 
-public final class PySyntaxHighlighterFactory extends SyntaxHighlighterFactory {
-  private final Map<LanguageLevel, PyHighlighter> myMap = FactoryMap.create(key -> new PyHighlighter(key));
-
-  private final Map<LanguageLevel, PyHighlighter> myConsoleMap = FactoryMap.create(key -> new PyHighlighter(key) {
-    @Override
-    protected PythonHighlightingLexer createHighlightingLexer(LanguageLevel languageLevel) {
-      return new PyConsoleHighlightingLexer(languageLevel);
-    }
-  });
-
+public final class PySyntaxHighlighterFactory extends PySyntaxHighlighterFactoryBase {
   @Override
   @NotNull
-  public SyntaxHighlighter getSyntaxHighlighter(@Nullable final Project project, @Nullable final VirtualFile virtualFile) {
-    final LanguageLevel level = getLanguageLevel(project, virtualFile);
-    if (useConsoleLexer(project, virtualFile)) {
-      return myConsoleMap.get(level);
-    }
-    return getSyntaxHighlighterForLanguageLevel(level);
-  }
-
-  /**
-   * Returns a syntax highlighter for Python console.
-   */
-  @NotNull
-  public SyntaxHighlighter getConsoleSyntaxHighlighter(@Nullable final Project project, @Nullable final VirtualFile virtualFile) {
-    final LanguageLevel level = getLanguageLevel(project, virtualFile);
-    return myConsoleMap.get(level);
-  }
-
-  /**
-   * Returns a syntax highlighter targeting the specified version of Python.
-   */
-  @NotNull
-  public SyntaxHighlighter getSyntaxHighlighterForLanguageLevel(@NotNull LanguageLevel level) {
-    return myMap.get(level);
-  }
-
-  @NotNull
-  private static LanguageLevel getLanguageLevel(@Nullable final Project project, @Nullable final VirtualFile virtualFile) {
+  protected LanguageLevel getLanguageLevel(@Nullable final Project project, @Nullable final VirtualFile virtualFile) {
     return project != null && virtualFile != null ?
            PythonLanguageLevelPusher.getLanguageLevelForVirtualFile(project, virtualFile) :
            LanguageLevel.getDefault();
   }
 
-  private static boolean useConsoleLexer(@Nullable final Project project, @Nullable final VirtualFile virtualFile) {
+  @Override
+  protected boolean useConsoleLexer(@Nullable final Project project, @Nullable final VirtualFile virtualFile) {
     if (virtualFile == null || project == null || virtualFile instanceof VirtualFileWindow) {
       return false;
     }
