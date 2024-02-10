3c8f0c01d1c0a65ba131e6bf72c9a81d944da574
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-02-10 19:18:53 +0100

    do not execute HuggingFaceProjectStartupActivity in unit test mode
    GitOrigin-RevId: 2c372f8637c764d2e0bc283011ea3fb6ab7f8b07

--- python/huggingFace/src/com/intellij/python/community/impl/huggingFace/service/HuggingFaceStartupActivity.kt
+++ python/huggingFace/src/com/intellij/python/community/impl/huggingFace/service/HuggingFaceStartupActivity.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.python.community.impl.huggingFace.service
 
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.extensions.ExtensionNotApplicableException
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.python.community.impl.huggingFace.HuggingFaceConstants
@@ -9,8 +11,12 @@ import com.intellij.python.community.impl.huggingFace.api.HuggingFaceApi
 import com.intellij.python.community.impl.huggingFace.cache.HuggingFaceDatasetsCache
 import com.intellij.python.community.impl.huggingFace.cache.HuggingFaceModelsCache
 
-
-class HuggingFaceProjectStartupActivity : ProjectActivity {
+private class HuggingFaceProjectStartupActivity : ProjectActivity {
+  init {
+    if (ApplicationManager.getApplication().isUnitTestMode) {
+      throw ExtensionNotApplicableException.create()
+    }
+  }
 
   override suspend fun execute(project: Project) {
     HuggingFaceApi.fillCacheWithBasicApiData(HuggingFaceEntityKind.MODEL,
