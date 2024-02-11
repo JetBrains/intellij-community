d4c4833f47490c795bdecc10db3a5924210f7f8a
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2024-02-12 00:56:59 +0100

    extract LanguageServiceWidgetContext class to a separate file
    GitOrigin-RevId: 3d6b129f88b2f205b2011516d599816bd877a7dd

--- platform/lang-api/src/com/intellij/platform/lang/lsWidget/LanguageServiceWidgetItemsProvider.kt
+++ platform/lang-api/src/com/intellij/platform/lang/lsWidget/LanguageServiceWidgetItemsProvider.kt
@@ -1,10 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.lang.lsWidget
 
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.extensions.ExtensionPointName
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Experimental
@@ -16,11 +13,3 @@ abstract class LanguageServiceWidgetItemsProvider {
 
   abstract fun getWidgetItems(context: LanguageServiceWidgetContext): List<LanguageServiceWidgetItem>
 }
-
-@ApiStatus.Experimental
-data class LanguageServiceWidgetContext(
-  val project: Project,
-  val currentFile: VirtualFile?,
-  val updateWidget: () -> Unit,
-  val widgetDisposable: Disposable,
-)
