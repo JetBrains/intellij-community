b36edb9c51a43502d321022b5bb5adaa18414f47
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2024-02-12 18:52:48 +0100

    fus: add implementation note
    GitOrigin-RevId: 057598d97ca3c0dff2eaabea6bbc23223af9a3cb

--- platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/actions/persistence/ActionIdsHolder.kt
+++ platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/actions/persistence/ActionIdsHolder.kt
@@ -10,6 +10,10 @@ import org.jetbrains.annotations.ApiStatus.Internal
  * Such actions usually used in places where actual action represented with lambda like [com.intellij.notification.NotificationAction].
  * If actions are registered in XML or a part of predefined allowlist it will be reported automatically.
  *
+ * **Note**: when implement a new id holder,
+ * the related collectors which use [ActionsEventLogGroup.ACTION_ID] explicitly or implicitly
+ * (e.g., by "action_id" name) should increment theirs group version.
+ *
  * @see ActionsBuiltInAllowedlist.isAllowedActionId
  * @see ActionIdProvider
  */
