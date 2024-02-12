c56dbec5f88af9c1f203947e353ffc15cbf3425d
Tagir Valeev <tagir.valeev@jetbrains.com>
2024-02-12 16:10:22 +0100

    [java] Replace CreateMissingDeconstructionRecordClassBranchesFix20Test and ReplaceCaseDefaultWithDefault20FixTest with stubs
    Cannot remove the tests yet, as their removal triggers re-bucketing on CI, which results in unrelated test failures
    IDEA-345498 Remove explicit Java 20-preview support and parenthesized patterns (in progress)
    
    GitOrigin-RevId: e84e4cb7f26ecc45f81bb0b04904540a9e3cac0d

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/impl/quickfix/ReplaceCaseDefaultWithDefault20FixTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/impl/quickfix/ReplaceCaseDefaultWithDefault20FixTest.java
@@ -1,25 +1,14 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.daemon.impl.quickfix;
 
-import com.intellij.codeInsight.daemon.quickFix.LightQuickFixParameterizedTestCase;
-import com.intellij.pom.java.LanguageLevel;
-import com.intellij.testFramework.LightProjectDescriptor;
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
-import org.jetbrains.annotations.NotNull;
+import org.junit.Ignore;
+import org.junit.Test;
 
-public class ReplaceCaseDefaultWithDefault20FixTest extends LightQuickFixParameterizedTestCase {
-  @Override
-  protected String getBasePath() {
-    return "/codeInsight/daemonCodeAnalyzer/quickFix/replaceCaseDefaultWithDefault20";
-  }
-
-  @Override
-  protected LanguageLevel getLanguageLevel() {
-    return LanguageLevel.JDK_20;
-  }
-
-  @Override
-  protected @NotNull LightProjectDescriptor getProjectDescriptor() {
-    return LightJavaCodeInsightFixtureTestCase.JAVA_20;
+// Temporarily necessary, as deletion of this test causes re-bucketing on CI, which results in unrelated failures 
+@Ignore
+public class ReplaceCaseDefaultWithDefault20FixTest {
+  @Test
+  public void bogusTest() {
+    
   }
 }
