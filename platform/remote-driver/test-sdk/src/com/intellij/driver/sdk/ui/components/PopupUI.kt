7e958b7a36185d814ad3287267cc1d6552fb0936
Mikhail Kuznetsov <mikhail.kuznetsov@jetbrains.com>
2024-02-13 18:37:04 +0200

    [smoke][tests] new AI class documentation test
    GitOrigin-RevId: f89949e1ddcc5e0288a32afcd6439edfa9d41b78

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/PopupUI.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/PopupUI.kt
@@ -3,7 +3,18 @@ package com.intellij.driver.sdk.ui.components
 import com.intellij.driver.sdk.ui.Finder
 import org.intellij.lang.annotations.Language
 
-fun Finder.popup(@Language("xpath") xpath: String? = null) = x(xpath ?: "//div[@class='HeavyWeightWindow']",
-                                                               PopupUiComponent::class.java)
+fun Finder.popup(@Language("xpath") xpath: String? = null) =
+  x(xpath ?: "//div[@class='HeavyWeightWindow']", PopupUiComponent::class.java)
 
-open class PopupUiComponent(data: ComponentData) : UiComponent(data)
+fun Finder.popupMenus(@Language("xpath") xpath: String? = null) =
+  xx(xpath ?: "//div[@class='JBPopupMenu']", PopupUiComponent::class.java).list()
+
+open class PopupUiComponent(data: ComponentData) : UiComponent(data) {
+
+  fun select(item: String, vararg subItem: String) {
+    findText(item).click()
+    subItem.forEach { i ->
+      popupMenus().map { it.findAllText() }.flatten().first { it.text == i }.click()
+    }
+  }
+}
