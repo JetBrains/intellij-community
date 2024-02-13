410249cb34939d6a86b14807898f7516c6260497
Aleksey Pivovarov <AMPivovarov@gmail.com>
2024-02-13 12:40:32 +0100

    vcs: convert VcsLogChangeProcessor to Kotlin
    GitOrigin-RevId: cb8b8d00a5b8e78a651955b812b6ee39c141d643

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/frame/VcsLogChangeProcessor.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/frame/VcsLogChangeProcessor.kt
@@ -1,93 +1,79 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.vcs.log.ui.frame;
+package com.intellij.vcs.log.ui.frame
 
-import com.intellij.diff.FrameDiffTool;
-import com.intellij.diff.chains.DiffRequestProducer;
-import com.intellij.diff.util.DiffPlaces;
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.vcs.changes.Change;
-import com.intellij.openapi.vcs.changes.ChangeViewDiffRequestProcessor;
-import com.intellij.openapi.vcs.changes.ui.ChangesBrowserChangeNode;
-import com.intellij.openapi.vcs.changes.ui.ChangesBrowserNode;
-import com.intellij.openapi.vcs.changes.ui.VcsTreeModelData;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.diff.FrameDiffTool
+import com.intellij.diff.chains.DiffRequestProducer
+import com.intellij.diff.util.DiffPlaces
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.vcs.changes.Change
+import com.intellij.openapi.vcs.changes.ChangeViewDiffRequestProcessor
+import com.intellij.openapi.vcs.changes.ui.ChangesBrowserChangeNode
+import com.intellij.openapi.vcs.changes.ui.ChangesBrowserNode
+import com.intellij.openapi.vcs.changes.ui.VcsTreeModelData
 
-public class VcsLogChangeProcessor extends ChangeViewDiffRequestProcessor {
-  private final @NotNull VcsLogChangesBrowser myBrowser;
+class VcsLogChangeProcessor internal constructor(project: Project,
+                                                 private val myBrowser: VcsLogChangesBrowser,
+                                                 private val myIsInEditor: Boolean,
+                                                 disposable: Disposable)
+  : ChangeViewDiffRequestProcessor(project, if (myIsInEditor) DiffPlaces.DEFAULT else DiffPlaces.VCS_LOG_VIEW) {
 
-  private final boolean myIsInEditor;
+  init {
+    Disposer.register(disposable, this)
 
-  VcsLogChangeProcessor(@NotNull Project project, @NotNull VcsLogChangesBrowser browser, boolean isInEditor,
-                        @NotNull Disposable disposable) {
-    super(project, isInEditor ? DiffPlaces.DEFAULT : DiffPlaces.VCS_LOG_VIEW);
-    myIsInEditor = isInEditor;
-    myBrowser = browser;
-    Disposer.register(disposable, this);
-
-    myBrowser.addListener(() -> updatePreviewLater(), this);
-    myBrowser.getViewer().addSelectionListener(this::updatePreviewLater, this);
+    myBrowser.addListener({ updatePreviewLater() }, this)
+    myBrowser.viewer.addSelectionListener({ this.updatePreviewLater() }, this)
   }
 
-  @Override
-  protected boolean shouldAddToolbarBottomBorder(@NotNull FrameDiffTool.ToolbarComponents toolbarComponents) {
-    return !myIsInEditor || super.shouldAddToolbarBottomBorder(toolbarComponents);
+  override fun shouldAddToolbarBottomBorder(toolbarComponents: FrameDiffTool.ToolbarComponents): Boolean {
+    return !myIsInEditor || super.shouldAddToolbarBottomBorder(toolbarComponents)
   }
 
-  @Override
-  public @NotNull Iterable<Wrapper> iterateSelectedChanges() {
-    return wrap(VcsTreeModelData.selected(myBrowser.getViewer()));
+  override fun iterateSelectedChanges(): Iterable<Wrapper> {
+    return wrap(VcsTreeModelData.selected(myBrowser.viewer))
   }
 
-  @Override
-  public @NotNull Iterable<Wrapper> iterateAllChanges() {
-    return wrap(VcsTreeModelData.all(myBrowser.getViewer()));
+  override fun iterateAllChanges(): Iterable<Wrapper> {
+    return wrap(VcsTreeModelData.all(myBrowser.viewer))
   }
 
-  private @NotNull Iterable<Wrapper> wrap(@NotNull VcsTreeModelData modelData) {
-    return wrap(myBrowser, modelData);
+  private fun wrap(modelData: VcsTreeModelData): Iterable<Wrapper> {
+    return wrap(myBrowser, modelData)
   }
 
-  static @NotNull Iterable<Wrapper> wrap(@NotNull VcsLogChangesBrowser browser, @NotNull VcsTreeModelData modelData) {
-    return modelData.iterateNodes()
-      .filter(ChangesBrowserChangeNode.class)
-      .map(n -> new MyChangeWrapper(browser, n.getUserObject(), browser.getTag(n.getUserObject())));
+  override fun selectChange(change: Wrapper) {
+    myBrowser.selectChange(change.userObject, change.tag)
   }
 
-  @Override
-  protected void selectChange(@NotNull Wrapper change) {
-    myBrowser.selectChange(change.getUserObject(), change.getTag());
+  private fun updatePreviewLater() {
+    ApplicationManager.getApplication().invokeLater { updatePreview(myIsInEditor || component.isShowing) }
   }
 
-  private void updatePreviewLater() {
-    ApplicationManager.getApplication().invokeLater(() -> updatePreview(myIsInEditor || getComponent().isShowing()));
-  }
-
-  public void updatePreview(boolean state) {
+  fun updatePreview(state: Boolean) {
     // We do not have local changes here, so it's OK to always use `fromModelRefresh == false`
-    updatePreview(state, false);
+    updatePreview(state, false)
   }
 
-  public static @NotNull VcsTreeModelData getSelectedOrAll(VcsLogChangesBrowser changesBrowser) {
-    boolean hasSelection = changesBrowser.getViewer().getSelectionModel().getSelectionCount() != 0;
-    return hasSelection ? VcsTreeModelData.selected(changesBrowser.getViewer())
-                        : VcsTreeModelData.all(changesBrowser.getViewer());
+  private class MyChangeWrapper(private val myBrowser: VcsLogChangesBrowser, change: Change, tag: ChangesBrowserNode.Tag?)
+    : ChangeWrapper(change, tag) {
+    override fun createProducer(project: Project?): DiffRequestProducer? {
+      return myBrowser.getDiffRequestProducer(change, true)
+    }
   }
 
-  private static class MyChangeWrapper extends ChangeWrapper {
-    private final @NotNull VcsLogChangesBrowser myBrowser;
-
-    MyChangeWrapper(@NotNull VcsLogChangesBrowser browser, @NotNull Change change, @Nullable ChangesBrowserNode.Tag tag) {
-      super(change, tag);
-      myBrowser = browser;
+  companion object {
+    fun wrap(browser: VcsLogChangesBrowser, modelData: VcsTreeModelData): Iterable<Wrapper> {
+      return modelData.iterateNodes()
+        .filter(ChangesBrowserChangeNode::class.java)
+        .map<Wrapper> { n: ChangesBrowserChangeNode -> MyChangeWrapper(browser, n.userObject, browser.getTag(n.userObject)) }
     }
 
-    @Override
-    public @Nullable DiffRequestProducer createProducer(@Nullable Project project) {
-      return myBrowser.getDiffRequestProducer(change, true);
+    fun getSelectedOrAll(changesBrowser: VcsLogChangesBrowser): VcsTreeModelData {
+      val hasSelection = changesBrowser.viewer.selectionModel.selectionCount != 0
+      return if (hasSelection) VcsTreeModelData.selected(changesBrowser.viewer)
+      else VcsTreeModelData.all(changesBrowser.viewer)
     }
   }
 }
