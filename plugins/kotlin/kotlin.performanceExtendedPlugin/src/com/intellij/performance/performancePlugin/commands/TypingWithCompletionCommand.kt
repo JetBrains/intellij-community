1d80d00423a1a7510fd4473efe6dd666bc73fb30
Roman.Ivanitskii <roman.ivanitskii@jetbrains.com>
2024-02-13 17:11:32 +0100

    [kotlin-plugin][performance-tests] Add typing and completion e2e test
    ^AT-739
    
    GitOrigin-RevId: fd3a54df39d62ec02fd96604fc093729139891e2

--- plugins/kotlin/kotlin.performanceExtendedPlugin/src/com/intellij/performance/performancePlugin/commands/TypingWithCompletionCommand.kt
+++ plugins/kotlin/kotlin.performanceExtendedPlugin/src/com/intellij/performance/performancePlugin/commands/TypingWithCompletionCommand.kt
@@ -1,6 +1,7 @@
 package com.intellij.performance.performancePlugin.commands
 
 import com.intellij.codeInsight.completion.impl.CompletionServiceImpl.Companion.currentCompletionProgressIndicator
+import com.intellij.codeInsight.lookup.Lookup
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.application.writeAction
 import com.intellij.openapi.editor.impl.EditorComponentImpl
@@ -9,8 +10,11 @@ import com.intellij.openapi.ui.playback.PlaybackContext
 import com.intellij.openapi.ui.playback.commands.AlphaNumericTypeCommand
 import com.jetbrains.performancePlugin.commands.PerformanceCommandCoroutineAdapter
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.selects.select
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.jetbrains.annotations.NonNls
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -27,18 +31,19 @@ class TypingWithCompletionCommand(text: String, line: Int) : PerformanceCommandC
     }
 
     override suspend fun doExecute(context: PlaybackContext) {
-        val input = """file.declarations.flatMap { it.annotations }
+        val input = """return file.declarations.flatMap { it.annotations }
     .flatMap { it.entries }
     .filter { it.valueArguments.isNotEmpty() }
     .mapNotNull { it.calleeExpression }
     .map { it.text }"""
         val tokens = splitInput(input)
-        val delayDuration = 200.milliseconds
-
+        val delayDuration = 300.milliseconds
+        val mutex = Mutex()
         val editor = (AlphaNumericTypeCommand.findTarget(context) as EditorComponentImpl?)?.editor
         if (editor == null) return
+        val span = startSpan(NAME)
+        var completionCount = 0L
         coroutineScope {
-
             //job to propagate typing each DELAY
             val tick = Channel<Unit>()
             val tickJob = launch {
@@ -50,31 +55,33 @@ class TypingWithCompletionCommand(text: String, line: Int) : PerformanceCommandC
 
             //job for typing
             val currentTokenChannel = Channel<String>()
-            val wasCompleted = Channel<Unit>()
-            launch {
+            val wasCompleted = Channel<Unit>(onBufferOverflow = BufferOverflow.SUSPEND)
+            val typing = launch {
                 var index = 0
                 while (index < tokens.size) {
                     val token = tokens[index]
                     index++
                     if (token == "→") {
-                        moveCaretRight(editor)
+                        moveCaretRight(editor, mutex)
                         continue
                     }
                     currentTokenChannel.send(token)
                     for (char in token) {
-                        withContext(Dispatchers.EDT) {
-                            editor.type(char.toString())
+                        mutex.withLock {
+                            withContext(Dispatchers.EDT) {
+                                editor.type(char.toString())
+                            }
                         }
                         val completed = select {
                             wasCompleted.onReceive {
                                 //add caret move right otherwise there will be double closing parentheses
-                                if(tokens.getOrNull(index) == "{") {
+                                if (tokens.getOrNull(index) == "{") {
                                     tokens.removeAt(index)
                                     val indexOfClosing = tokens.drop(index).indexOfFirst { s -> s == "}" } + index
                                     tokens.add(indexOfClosing, "→")
                                 }
                                 //skip parenthesis if method call was completed
-                                if(tokens.getOrNull(index) == "(") {
+                                if (tokens.getOrNull(index) == "(") {
                                     tokens.removeAt(index)
                                     tokens.removeAt(index)
                                 }
@@ -104,33 +111,47 @@ class TypingWithCompletionCommand(text: String, line: Int) : PerformanceCommandC
                         break
                     }
                     if (currentToken != null) {
-                        completeTokenIfExists(currentToken, wasCompleted)
+                        if (completeTokenIfExists(currentToken, wasCompleted, mutex)) {
+                            completionCount++
+                        }
                     }
                 }
             }
+            typing.join()
+            span.setAttribute("completionCount", completionCount)
+            span.end()
         }
     }
 
-    private suspend fun completeTokenIfExists(currentToken: String?, wasCompleted: Channel<Unit>) {
+    private suspend fun completeTokenIfExists(currentToken: String?, wasCompleted: Channel<Unit>, mutex: Mutex): Boolean {
         val numberOfTopElementsToSearch = 3
         val items = currentCompletionProgressIndicator?.lookup?.items
         val lookupElement = items?.take(numberOfTopElementsToSearch)?.firstOrNull { it.lookupString == currentToken }
         if (lookupElement != null) {
-            wasCompleted.send(Unit)
-            withContext(Dispatchers.EDT) {
-                currentCompletionProgressIndicator?.lookup?.finishLookup('\n', lookupElement)
+            val result = wasCompleted.trySend(Unit)
+            if (!result.isSuccess) {
+                return false
+            }
+            mutex.withLock {
+                return withContext(Dispatchers.EDT) {
+                    currentCompletionProgressIndicator?.lookup?.finishLookup(Lookup.NORMAL_SELECT_CHAR, lookupElement)
+                    return@withContext true
+                }
             }
         }
+        return false
     }
 
-    private suspend fun moveCaretRight(editor: EditorImpl) {
-        writeAction {
-            val caretModel = editor.caretModel
-            val document = editor.document
-            val currentCaretPosition = caretModel.offset
+    private suspend fun moveCaretRight(editor: EditorImpl, mutex: Mutex) {
+        mutex.withLock {
+            writeAction {
+                val caretModel = editor.caretModel
+                val document = editor.document
+                val currentCaretPosition = caretModel.offset
 
-            if (currentCaretPosition < document.textLength) {
-                caretModel.moveToOffset(currentCaretPosition + 1)
+                if (currentCaretPosition < document.textLength) {
+                    caretModel.moveToOffset(currentCaretPosition + 1)
+                }
             }
         }
     }
