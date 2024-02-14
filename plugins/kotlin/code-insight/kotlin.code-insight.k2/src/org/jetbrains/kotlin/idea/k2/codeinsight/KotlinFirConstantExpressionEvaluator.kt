cb36bb2128f0005492b03753bd5e66ab7281e1d7
Anna Kozlova <anna.kozlova@jetbrains.com>
2024-02-14 12:00:02 +0100

    [kotlin] k2: registry to allow analysis on EDT for computing constants
    - still test with the flag disabled
    
    ^KTIJ-28805 fixed
    
    GitOrigin-RevId: cc675691bd78028dd8a34829b97784fa66c927a1

--- plugins/kotlin/code-insight/kotlin.code-insight.k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/KotlinFirConstantExpressionEvaluator.kt
+++ plugins/kotlin/code-insight/kotlin.code-insight.k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/KotlinFirConstantExpressionEvaluator.kt
@@ -1,12 +1,17 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.k2.codeinsight
 
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.psi.PsiConstantEvaluationHelper.AuxEvaluator
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.ConstantExpressionEvaluator
+import org.jetbrains.kotlin.analysis.api.KtAllowAnalysisFromWriteAction
+import org.jetbrains.kotlin.analysis.api.KtAllowAnalysisOnEdt
 import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.base.KtConstantValue
 import org.jetbrains.kotlin.analysis.api.components.KtConstantEvaluationMode
+import org.jetbrains.kotlin.analysis.api.lifetime.allowAnalysisFromWriteAction
+import org.jetbrains.kotlin.analysis.api.lifetime.allowAnalysisOnEdt
 import org.jetbrains.kotlin.asJava.unwrapped
 import org.jetbrains.kotlin.psi.KtExpression
 
@@ -24,9 +29,22 @@ class KotlinFirConstantExpressionEvaluator : ConstantExpressionEvaluator {
         auxEvaluator: AuxEvaluator?
     ): Any? {
         val ktExpression = expression.unwrapped as? KtExpression ?: return null
-        analyze(ktExpression) {
-            return ktExpression.evaluate(KtConstantEvaluationMode.CONSTANT_LIKE_EXPRESSION_EVALUATION)
-                ?.takeUnless { it is KtConstantValue.KtErrorConstantValue }?.value
+        val analyze = {
+            analyze(ktExpression) {
+                ktExpression.evaluate(KtConstantEvaluationMode.CONSTANT_LIKE_EXPRESSION_EVALUATION)
+                    ?.takeUnless { it is KtConstantValue.KtErrorConstantValue }?.value
+            }
+        }
+
+        if (!Registry.`is`("kotlin.fir.allow.constant.computation.on.EDT")) {
+            return analyze()
+        }
+
+        @OptIn(KtAllowAnalysisOnEdt::class, KtAllowAnalysisFromWriteAction::class)
+        return allowAnalysisFromWriteAction {
+            allowAnalysisOnEdt {
+                analyze()
+            }
         }
     }
 }
