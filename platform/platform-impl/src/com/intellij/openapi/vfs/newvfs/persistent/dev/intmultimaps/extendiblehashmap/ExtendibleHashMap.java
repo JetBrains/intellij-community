458f9970238febecbc59c45fc8610a5fb59502fa
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2024-02-14 16:53:43 +0700

    [cleanup] small code cleanup in ExtendibleHashMap
    GitOrigin-RevId: 2f4c0c2e4b4d92b3dbb461ea8d7404445b97baed

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/dev/intmultimaps/extendiblehashmap/ExtendibleHashMap.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/dev/intmultimaps/extendiblehashmap/ExtendibleHashMap.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vfs.newvfs.persistent.dev.intmultimaps.extendiblehashmap;
 
 import com.intellij.openapi.util.Pair;
@@ -876,6 +876,7 @@ public class ExtendibleHashMap implements DurableIntToMultiIntMap, Unmappable {
 
   /** Abstracts data storage for open-addressing hash-table implementation */
   public interface HashTableData {
+    /** entry = (key, value) pair */
     int entriesCount();
 
     int aliveEntriesCount();
@@ -926,7 +927,7 @@ public class ExtendibleHashMap implements DurableIntToMultiIntMap, Unmappable {
                       int key,
                       ValueAcceptor valuesAcceptor) throws IOException {
       checkNotNoValue("key", key);
-      int capacity = table.entriesCount();
+      int capacity = capacity(table);
       int startIndex = Math.abs(hash(key) % capacity);
       for (int probe = 0; probe < capacity; probe++) {
         int slotIndex = (startIndex + probe) % capacity;
