658b4af9bc5cc4ac8158bbd35a0e964689c30b38
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2024-02-14 17:08:28 +0100

    [java] IDEA-331682 convert GeneralAutoPopupTest to java
    GitOrigin-RevId: 58fd79e8bc443b7fba25dc283951eae5e51eb1eb

--- java/java-tests/testSrc/com/intellij/java/codeInsight/completion/GeneralAutoPopupTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/completion/GeneralAutoPopupTest.java
@@ -1,134 +1,136 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.java.codeInsight.completion
+package com.intellij.java.codeInsight.completion;
 
-import com.intellij.codeInsight.completion.CompletionContributor
-import com.intellij.codeInsight.completion.CompletionParameters
-import com.intellij.codeInsight.completion.CompletionResultSet
-import com.intellij.codeInsight.completion.JavaCompletionAutoPopupTestCase
-import com.intellij.openapi.actionSystem.IdeActions
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.extensions.LoadingOrder
-import com.intellij.psi.InjectedLanguagePlaces
-import com.intellij.psi.LanguageInjector
-import com.intellij.psi.PsiLanguageInjectionHost
-import com.intellij.testFramework.DumbModeTestUtils
-import com.intellij.testFramework.ExtensionTestUtil
-import com.intellij.testFramework.NeedsIndex
-import groovy.transform.CompileStatic
-import org.jetbrains.annotations.NotNull
+import com.intellij.codeInsight.completion.CompletionContributor;
+import com.intellij.codeInsight.completion.CompletionParameters;
+import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.completion.JavaCompletionAutoPopupTestCase;
+import com.intellij.openapi.actionSystem.IdeActions;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.extensions.LoadingOrder;
+import com.intellij.psi.LanguageInjector;
+import com.intellij.testFramework.DumbModeTestUtils;
+import com.intellij.testFramework.ExtensionTestUtil;
+import com.intellij.testFramework.NeedsIndex;
+import com.intellij.testFramework.UsefulTestCase;
+import com.intellij.util.ThrowableRunnable;
+import groovy.lang.Reference;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.concurrent.atomic.AtomicInteger
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * For tests checking platform behavior not related to Java language (but they may still use Java for code samples)
  */
 @NeedsIndex.SmartMode(reason = "AutoPopup shouldn't work in dumb mode")
-@CompileStatic
-class GeneralAutoPopupTest extends JavaCompletionAutoPopupTestCase {
-  void "test no autopopup in the middle of word when the only variant is already in the editor"() {
-    myFixture.configureByText 'a.java', 'class Foo { private boolean ignoredProperty; public boolean isIgnoredP<caret>operty() {}}'
-    type 'r'
-    assert !lookup
+public class GeneralAutoPopupTest extends JavaCompletionAutoPopupTestCase {
+  public void testNoAutopopupInTheMiddleOfWordWhenTheOnlyVariantIsAlreadyInTheEditor() {
+    myFixture.configureByText("a.java", "class Foo { private boolean ignoredProperty; public boolean isIgnoredP<caret>operty() {}}");
+    type("r");
+    assertNull(getLookup());
   }
 
-  void "test no lookup after typing a letter and then quickly overtyping a quote"() {
-    myFixture.configureByText 'a.html', '<a href="<caret>">'
-    myFixture.type('a')
-    type '"'
-    assert !lookup
+  public void testNoLookupAfterTypingALetterAndThenQuicklyOvertypingAQuote() {
+    myFixture.configureByText("a.html", "<a href=\"<caret>\">");
+    myFixture.type("a");
+    type("\"");
+    assertNull(getLookup());
   }
 
-  void "test no lookup after typing and quickly moving caret to another place"() {
-    myFixture.configureByText 'a.java', 'class Foo { <caret> }'
-    edt {
-      myFixture.type('F')
-      myFixture.editor.caretModel.moveToOffset(myFixture.caretOffset + 1)
-    }
+  public void testNoLookupAfterTypingAndQuicklyMovingCaretToAnotherPlace() throws Throwable {
+    myFixture.configureByText("a.java", "class Foo { <caret> }");
+    UsefulTestCase.edt((ThrowableRunnable<Throwable>)() -> {
+      myFixture.type("F");
+      myFixture.getEditor().getCaretModel().moveToOffset(myFixture.getCaretOffset() + 1);
+    });
 
-    myTester.joinAutopopup()
-    myTester.joinCompletion()
+    myTester.joinAutopopup();
+    myTester.joinCompletion();
 
-    assert !lookup
+    assertNull(getLookup());
   }
 
-  void "test injectors are not run in EDT"() {
-    boolean injectorCalled = false
-    LanguageInjector injector = new LanguageInjector() {
-      @Override
-      void getLanguagesToInject(@NotNull PsiLanguageInjectionHost host, @NotNull InjectedLanguagePlaces injectionPlacesRegistrar) {
-        injectorCalled = true
-        ApplicationManager.getApplication().assertIsNonDispatchThread();
-      }
-    }
-    ExtensionTestUtil.maskExtensions(LanguageInjector.EXTENSION_POINT_NAME, [injector] as List<LanguageInjector>, myFixture.testRootDisposable)
+  public void testInjectorsAreNotRunInEdt() {
+    final Reference<Boolean> injectorCalled = new Reference<>(false);
+    LanguageInjector injector = (host, injectionPlacesRegistrar) -> {
+      injectorCalled.set(true);
+      ApplicationManager.getApplication().assertIsNonDispatchThread();
+    };
+    ExtensionTestUtil.maskExtensions(LanguageInjector.EXTENSION_POINT_NAME,
+                                     Arrays.asList(injector),
+                                     myFixture.getTestRootDisposable());
 
-    myFixture.configureByText 'a.java', 'class Foo { String s = <caret>; }'
-    assert !injectorCalled
-    type '"'
+    myFixture.configureByText("a.java", "class Foo { String s = <caret>; }");
+    assertFalse(injectorCalled.get());
+    type("\"");
 
-    injectorCalled = false
-    type 'abc'
+    injectorCalled.set(false);
+    type("abc");
 
-    assert injectorCalled
-    assert !lookup
+    assertTrue(injectorCalled.get());
+    assertNull(getLookup());
   }
 
-  void "test don't close lookup when starting a new line after dot"() {
-    myFixture.configureByText 'a.java', 'class Foo {{ "abc"<caret> }}'
-    type '.'
-    assert lookup
-    edt {
-      myFixture.performEditorAction(IdeActions.ACTION_EDITOR_START_NEW_LINE)
-      assert lookup
-      assert lookup.lookupStart == myFixture.editor.caretModel.offset
-      assert myFixture.editor.document.text.contains('\n')
-    }
+  public void testDonTCloseLookupWhenStartingANewLineAfterDot() throws Throwable {
+    myFixture.configureByText("a.java", "class Foo {{ \"abc\"<caret> }}");
+    type(".");
+    assertNotNull(getLookup());
+    UsefulTestCase.edt((ThrowableRunnable<Throwable>)() -> {
+      myFixture.performEditorAction(IdeActions.ACTION_EDITOR_START_NEW_LINE);
+      assertNotNull(getLookup());
+      assertEquals(getLookup().getLookupStart(), myFixture.getEditor().getCaretModel().getOffset());
+      assertTrue(myFixture.getEditor().getDocument().getText().contains("\n"));
+    });
   }
 
-  void "test close lookup when starting a new line after having typed an identifier manually"() {
-    myFixture.configureByText 'a.java', 'class Foo { int a, ab; { new Foo()<caret> }}'
-    type '.a'
-    assert lookup
-    edt {
-      myFixture.performEditorAction(IdeActions.ACTION_EDITOR_START_NEW_LINE)
-      assert !lookup
-      assert myFixture.editor.document.text.contains('\n')
-    }
+  public void testCloseLookupWhenStartingANewLineAfterHavingTypedAnIdentifierManually() throws Throwable {
+    myFixture.configureByText("a.java", "class Foo { int a, ab; { new Foo()<caret> }}");
+    type(".a");
+    assertNotNull(getLookup());
+    UsefulTestCase.edt((ThrowableRunnable<Throwable>)() -> {
+      myFixture.performEditorAction(IdeActions.ACTION_EDITOR_START_NEW_LINE);
+      assertNull(getLookup());
+      assertTrue(myFixture.getEditor().getDocument().getText().contains("\n"));
+    });
   }
 
-  void "test do not restart autopopup completion when appending prefix which already yielded no results to save CPU"() {
-    JavaAutoPopupTest.registerCompletionContributor(CountingContributor, myFixture.testRootDisposable, LoadingOrder.FIRST)
-    CountingContributor.ourCount.set(0)
+  public void testDoNotRestartAutopopupCompletionWhenAppendingPrefixWhichAlreadyYieldedNoResultsToSaveCpu() {
+    JavaAutoPopupTest.registerCompletionContributor(CountingContributor.class, myFixture.getTestRootDisposable(), LoadingOrder.FIRST);
+    CountingContributor.getOurCount().set(0);
 
-    myFixture.configureByText 'a.txt', ''
-    type 'a'
-    assert !lookup
-    assert CountingContributor.ourCount.get() == 1
+    myFixture.configureByText("a.txt", "");
+    type("a");
+    assertNull(getLookup());
+    assertEquals(1, CountingContributor.getOurCount().get());
 
-    type 'bcd'
-    assert !lookup
-    assert CountingContributor.ourCount.get() == 1
+    type("bcd");
+    assertNull(getLookup());
+    assertEquals(1, CountingContributor.getOurCount().get());
 
-    type ' a'
-    assert CountingContributor.ourCount.get() == 2
+    type(" a");
+    assertEquals(2, CountingContributor.getOurCount().get());
   }
 
-  static class CountingContributor extends CompletionContributor {
-    static final AtomicInteger ourCount = new AtomicInteger()
+  public void testSkipAutopopupIfConfidenceNeedsNonReadyIndex() {
+    myFixture.configureByText("a.java", "class C { int abc; { getClass().getDeclaredField(\"<caret>x\"); }}");
+    DumbModeTestUtils.runInDumbModeSynchronously(getProject(), () -> {
+      type("a");
+      assertNull(getLookup());
+    });
+    myFixture.completeBasic();
+    myFixture.assertPreferredCompletionItems(0, "abc");
+  }
 
+  public static class CountingContributor extends CompletionContributor {
     @Override
-    void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
-      ourCount.incrementAndGet()
+    public void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
+      ourCount.incrementAndGet();
     }
-  }
 
-  void "test skip autopopup if confidence needs non-ready index"() {
-    myFixture.configureByText 'a.java', 'class C { int abc; { getClass().getDeclaredField("<caret>x"); }}'
-    DumbModeTestUtils.runInDumbModeSynchronously(project) {
-      type 'a'
-      assert !lookup
+    public static AtomicInteger getOurCount() {
+      return ourCount;
     }
-    myFixture.completeBasic()
-    myFixture.assertPreferredCompletionItems 0, 'abc'
+
+    private static final AtomicInteger ourCount = new AtomicInteger();
   }
 }
