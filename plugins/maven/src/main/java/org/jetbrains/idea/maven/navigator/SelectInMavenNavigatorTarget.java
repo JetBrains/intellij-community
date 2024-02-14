8efa2622f977478aa817227f5be8725facf5a865
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2024-02-14 19:44:10 +0100

    Maven: IJPL-486 Some options in "Select In" are only available after indexing complete
    GitOrigin-RevId: bf54c0252589f273ff79ebbdc22fc2adc4126611

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/navigator/SelectInMavenNavigatorTarget.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/navigator/SelectInMavenNavigatorTarget.java
@@ -1,23 +1,10 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.maven.navigator;
 
 import com.intellij.ide.SelectInContext;
 import com.intellij.ide.SelectInTarget;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.ToolWindow;
@@ -26,7 +13,7 @@ import org.jetbrains.idea.maven.project.MavenProject;
 import org.jetbrains.idea.maven.project.MavenProjectBundle;
 import org.jetbrains.idea.maven.project.MavenProjectsManager;
 
-public class SelectInMavenNavigatorTarget implements SelectInTarget {
+public class SelectInMavenNavigatorTarget implements SelectInTarget, DumbAware {
   @Override
   public boolean canSelect(SelectInContext context) {
     return getMavenProject(context) != null;
