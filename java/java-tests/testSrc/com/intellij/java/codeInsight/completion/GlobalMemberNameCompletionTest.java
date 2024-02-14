e70a49744b67c7e371a5f8282e557e3d3e37fa47
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2024-02-14 17:34:50 +0100

    [java] IDEA-331682 convert GlobalMemberNameCompletionTest to java
    GitOrigin-RevId: b4e10d5d4eed9e5eaf75275959c6a7620a7b5ee3

--- java/java-tests/testSrc/com/intellij/java/codeInsight/completion/GlobalMemberNameCompletionTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/completion/GlobalMemberNameCompletionTest.java
@@ -1,319 +1,330 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.intellij.java.codeInsight.completion
+package com.intellij.java.codeInsight.completion;
 
-import com.intellij.application.options.CodeStyle
-import com.intellij.codeInsight.JavaProjectCodeInsightSettings
-import com.intellij.codeInsight.completion.CompletionType
-import com.intellij.codeInsight.completion.StaticallyImportable
-import com.intellij.codeInsight.lookup.LookupElement
-import com.intellij.lang.java.JavaLanguage
-import com.intellij.openapi.util.ClassConditionKey
-import com.intellij.testFramework.NeedsIndex
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
-import groovy.transform.CompileStatic
+import com.intellij.application.options.CodeStyle;
+import com.intellij.codeInsight.JavaProjectCodeInsightSettings;
+import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.codeInsight.completion.StaticallyImportable;
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementPresentation;
+import com.intellij.lang.java.JavaLanguage;
+import com.intellij.openapi.util.ClassConditionKey;
+import com.intellij.psi.PsiFile;
+import com.intellij.testFramework.NeedsIndex;
+import com.intellij.testFramework.UsefulTestCase;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 
-import static com.intellij.java.codeInsight.completion.NormalCompletionTestCase.renderElement
+import java.util.List;
 
-@CompileStatic
-class GlobalMemberNameCompletionTest extends LightJavaCodeInsightFixtureTestCase {
-
+public class GlobalMemberNameCompletionTest extends LightJavaCodeInsightFixtureTestCase {
   @NeedsIndex.Full
-  void testMethodName() throws Exception {
+  public void testMethodName() {
     myFixture.addClass("""
-package foo;
+      package foo;
+      
+      public class Foo {
+        public static int abcmethod() {}
+        static void methodThatsNotVisible() {}
+      }""");
 
-public class Foo {
-  public static int abcmethod() {}
-  static void methodThatsNotVisible() {}
-}
-""")
-
-    doTest "class Bar {{ abcm<caret> }}", true, """import static foo.Foo.abcmethod;
-
-class Bar {{ abcmethod()<caret> }}"""
+    doTest("class Bar {{ abcm<caret> }}", true,
+           """
+             import static foo.Foo.abcmethod;
+             
+             class Bar {{ abcmethod()<caret> }}""");
   }
 
   @NeedsIndex.Full
-  void testFieldName() throws Exception {
+  public void testFieldName() {
     myFixture.addClass("""
-package foo;
+                         
+                         package foo;
+                         
+                         public class Foo {
+                           public static int abcfield = 2;
+                           static final int fieldThatsNotVisible = 3;
+                         }
+                         """);
 
-public class Foo {
-  public static int abcfield = 2;
-  static final int fieldThatsNotVisible = 3;
-}
-""")
+    doTest("class Bar {{ abcf<caret> }}", true,
+           """
+             import static foo.Foo.abcfield;
 
-    doTest "class Bar {{ abcf<caret> }}", true, """import static foo.Foo.abcfield;
-
-class Bar {{ abcfield<caret> }}"""
+             class Bar {{ abcfield<caret> }}""");
   }
 
   @NeedsIndex.Full
-  void testFieldNameQualified() throws Exception {
+  public void testFieldNameQualified() {
     myFixture.addClass("""
-package foo;
+                         package foo;
+                         
+                         public class Foo {
+                           public static int abcfield = 2;
+                           static final int fieldThatsNotVisible = 3;
+                         }
+                         """);
 
-public class Foo {
-  public static int abcfield = 2;
-  static final int fieldThatsNotVisible = 3;
-}
-""")
+    doTest("class Bar {{ abcf<caret> }}", false,
+           """
+             import foo.Foo;
 
-    doTest "class Bar {{ abcf<caret> }}", false, """import foo.Foo;
-
-class Bar {{ Foo.abcfield<caret> }}"""
+             class Bar {{ Foo.abcfield<caret> }}""");
   }
 
   @NeedsIndex.Full
-  void testFieldNamePresentation() {
+  public void testFieldNamePresentation() {
     myFixture.addClass("""
-package foo;
-
-public class Foo {
-  public static int abcfield = 2;
-  static final int fieldThatsNotVisible = 3;
-}
-""")
-    myFixture.configureByText "a.java", "class Bar {{ abcf<caret> }}"
-    def element = complete()[0]
-    def presentation = renderElement(element)
-    assert 'Foo.abcfield' == presentation.itemText
-    assert ' (foo)' == presentation.tailText
-    assert 'int' == presentation.typeText
+                         package foo;
+                         
+                         public class Foo {
+                           public static int abcfield = 2;
+                           static final int fieldThatsNotVisible = 3;
+                         }
+                         """);
+    myFixture.configureByText("a.java", "class Bar {{ abcf<caret> }}");
+    LookupElement element = complete()[0];
+    LookupElementPresentation presentation = NormalCompletionTestCase.renderElement(element);
+    assertEquals("Foo.abcfield", presentation.getItemText());
+    assertEquals(" (foo)", presentation.getTailText());
+    assertEquals("int", presentation.getTypeText());
   }
 
   private LookupElement[] complete() {
-    myFixture.complete(CompletionType.BASIC, 2)
+    return myFixture.complete(CompletionType.BASIC, 2);
   }
 
   @NeedsIndex.Full
-  void testQualifiedMethodName() throws Exception {
+  public void testQualifiedMethodName() {
     myFixture.addClass("""
-package foo;
 
-public class Foo {
-  public static int abcmethod() {}
-}
-""")
+                         package foo;
 
-    doTest "class Bar {{ abcm<caret> }}", false, """import foo.Foo;
+                         public class Foo {
+                           public static int abcmethod() {}
+                         }
+                         """);
 
-class Bar {{ Foo.abcmethod()<caret> }}"""
+    doTest("class Bar {{ abcm<caret> }}", false, """
+      import foo.Foo;
+
+      class Bar {{ Foo.abcmethod()<caret> }}""");
   }
 
   @NeedsIndex.Full
-  void testIfThereAreAlreadyStaticImportsWithThatClass() throws Exception {
+  public void testIfThereAreAlreadyStaticImportsWithThatClass() {
     myFixture.addClass("""
-package foo;
 
-public class Foo {
-  public static int anotherMethod(int a) {}
-  public static int abcmethod() {}
-  void methodThatsNotVisible() {}
-}
-""")
+                         package foo;
 
-    doTest """import static foo.Foo.abcmethod;
+                         public class Foo {
+                           public static int anotherMethod(int a) { }
 
-class Bar {{ abcmethod(); anoMe<caret> }}""", false,
-           """import static foo.Foo.abcmethod;
-import static foo.Foo.anotherMethod;
+                           public static int abcmethod() { }
 
-class Bar {{ abcmethod(); anotherMethod(<caret>) }}"""
+                           void methodThatsNotVisible() { }
+                         }
+                         """);
+
+    doTest("""
+             import static foo.Foo.abcmethod;
+             
+             class Bar {{ abcmethod(); anoMe<caret> }}""", false,
+              """
+              import static foo.Foo.abcmethod;
+              import static foo.Foo.anotherMethod;
+              
+              class Bar {{ abcmethod(); anotherMethod(<caret>) }}""");
   }
 
   @NeedsIndex.Full
-  void testExcludeClassFromCompletion() throws Exception {
-    myFixture.addClass("""package foo;
-      public class Foo {
-        public static int abcmethod() {}
-      }
-    """)
-    myFixture.addClass("""package foo;
-      public class Excl {
-        public static int abcmethod2() {}
-      }
-    """)
+  public void testExcludeClassFromCompletion() {
+    myFixture.addClass("""
+                         
+                         package foo;
+                         public class Foo {
+                           public static int abcmethod() {}
+                         }""");
+    myFixture.addClass("""
+                         package foo;
+                         public class Excl {
+                           public static int abcmethod2() { }
+                         }""");
 
-    JavaProjectCodeInsightSettings.setExcludedNames(project, myFixture.testRootDisposable, "*Excl")
+    JavaProjectCodeInsightSettings.setExcludedNames(getProject(), myFixture.getTestRootDisposable(), "*Excl");
 
-    doTest "class Bar {{ abcm<caret> }}", true, """import static foo.Foo.abcmethod;
+    doTest("class Bar {{ abcm<caret> }}", true, """
+      import static foo.Foo.abcmethod;
 
-class Bar {{ abcmethod()<caret> }}"""
+      class Bar {{ abcmethod()<caret> }}""");
   }
 
   @NeedsIndex.Full
-  void testExcludeMethodFromCompletion() throws Exception {
-    myFixture.addClass("""package foo;
-      public class Foo {
-        public static int abcmethod1() {}
-        public static int abcmethodExcluded() {}
-      }
-    """)
+  public void testExcludeMethodFromCompletion() {
+    myFixture.addClass(
+"""
+        package foo;
+              public class Foo {
+                public static int abcmethod1() {}
+                public static int abcmethodExcluded() {}
+              }
+        """);
 
-    JavaProjectCodeInsightSettings.setExcludedNames(project, myFixture.testRootDisposable, "foo.Foo.abcmethodExcluded")
+    JavaProjectCodeInsightSettings.setExcludedNames(getProject(), myFixture.getTestRootDisposable(), "foo.Foo.abcmethodExcluded");
 
-    doTest "class Bar {{ abcm<caret> }}", true, """import static foo.Foo.abcmethod1;
+    doTest("class Bar {{ abcm<caret> }}", true, """
+      import static foo.Foo.abcmethod1;
 
-class Bar {{ abcmethod1()<caret> }}"""
+      class Bar {{ abcmethod1()<caret> }}""");
   }
 
   @NeedsIndex.Full
-  void testMergeOverloads() throws Exception {
-    myFixture.addClass("""package foo;
+  public void testMergeOverloads() {
+    myFixture.addClass(
+    """
+      package foo;
       public class Foo {
         public static int abcmethod(int a) {}
         public static int abcmethod(boolean a) {}
         public static int abcmethod1(boolean a) {}
       }
-    """)
+      """);
 
-    myFixture.configureByText("a.java", "class Bar {{ abcm<caret> }}")
-    def element = complete()[0]
+    myFixture.configureByText("a.java", "class Bar {{ abcm<caret> }}");
+    LookupElement element = complete()[0];
 
-    def tail = renderElement(element).tailFragments
-    assert tail[0].text == '(...)'
-    assert !tail[0].grayed
-    assert tail[1].text == ' (foo)'
-    assert tail[1].grayed
-
-    assertOrderedEquals myFixture.lookupElementStrings, "abcmethod", "abcmethod1"
+    List<LookupElementPresentation.TextFragment> tail = NormalCompletionTestCase.renderElement(element).getTailFragments();
+    assertEquals("(...)", tail.get(0).text);
+    assertFalse(tail.get(0).isGrayed());
+    assertEquals(" (foo)", tail.get(1).text);
+    assertTrue(tail.get(1).isGrayed());
+    UsefulTestCase.assertOrderedEquals(myFixture.getLookupElementStrings(), "abcmethod", "abcmethod1");
   }
 
-  void testMethodFromTheSameClass() {
-    myFixture.configureByText("a.java", """
-class A {
-  static void foo() {}
-
-  static void goo() {
-    f<caret>
-  }
-}
-""")
-    def element = complete()[0]
-    def presentation = renderElement(element)
-    assert 'foo' == presentation.itemText
-    myFixture.type '\n'
-    myFixture.checkResult '''
-class A {
-  static void foo() {}
-
-  static void goo() {
-    foo();<caret>
-  }
-}
-'''
+  public void testMethodFromTheSameClass() {
+    myFixture.configureByText("a.java",
+  """
+      class A {
+        static void foo() {}
+      
+        static void goo() {
+          f<caret>
+        }
+      }
+      """);
+    LookupElement element = complete()[0];
+    LookupElementPresentation presentation = NormalCompletionTestCase.renderElement(element);
+    assert "foo".equals(presentation.getItemText());
+    myFixture.type("\n");
+    myFixture.checkResult(
+    """
+    class A {
+      static void foo() {}
+    
+      static void goo() {
+        foo();<caret>
+      }
+    }
+    """);
   }
 
   @NeedsIndex.Full
-  void "test static import before an identifier"() {
-    myFixture.addClass '''
-package test.t1;
-
-public enum DemoEnum
-{
-        XXONE,
-        TWO
-}'''
-    doTest """
-import test.t1.DemoEnum;
-
-public class Demo {
-
-        public static void doStuff(DemoEnum enumValue, String value) {}
-        public static void main(String[] args)
-        {
-                String val = "anyValue";
-                doStuff(XXON<caret>val);
-        }
-}
-""", true, """
-import test.t1.DemoEnum;
-
-import static test.t1.DemoEnum.XXONE;
-
-public class Demo {
-
-        public static void doStuff(DemoEnum enumValue, String value) {}
-        public static void main(String[] args)
-        {
-                String val = "anyValue";
-                doStuff(XXONE<caret>val);
-        }
-}
-"""
+  public void testStaticImportBeforeAnIdentifier() {
+    myFixture.addClass(
+      """
+      package test.t1;
+      
+      public enum DemoEnum
+      {
+              XXONE,
+              TWO
+      }
+      """);
+    doTest(
+      """
+             import test.t1.DemoEnum;
+             
+             public class Demo {
+             
+                     public static void doStuff(DemoEnum enumValue, String value) {}
+                     public static void main(String[] args)
+                     {
+                             String val = "anyValue";
+                             doStuff(XXON<caret>val);
+                     }
+             }
+             """, true,
+           """
+             import test.t1.DemoEnum;
+             
+             import static test.t1.DemoEnum.XXONE;
+             
+             public class Demo {
+             
+                     public static void doStuff(DemoEnum enumValue, String value) {}
+                     public static void main(String[] args)
+                     {
+                             String val = "anyValue";
+                             doStuff(XXONE<caret>val);
+                     }
+             }
+             """);
   }
 
   private void doTest(String input, boolean importStatic, String output) {
-    myFixture.configureByText("a.java", input)
+    myFixture.configureByText("a.java", input);
 
-    def item = assertOneElement(complete())
+    LookupElement item = UsefulTestCase.assertOneElement(complete());
     if (importStatic) {
-      ClassConditionKey<StaticallyImportable> key = ClassConditionKey.create(StaticallyImportable.class)
-      item.'as'(key).shouldBeImported = true
+      ClassConditionKey<StaticallyImportable> key = ClassConditionKey.create(StaticallyImportable.class);
+      item.as(key).setShouldBeImported(true);
     }
-    myFixture.type('\n')
-    myFixture.checkResult output
+
+    myFixture.type("\n");
+    myFixture.checkResult(output);
   }
 
-  void "test no results in incomplete static import"() {
-    def text = """
-package p;
-
-import static Foo.ba<caret>
-
-class Foo {
-  static void bar() {}
-}
-"""
-    def file = myFixture.addFileToProject("p/Foo.java", text)
-    myFixture.configureFromExistingVirtualFile(file.virtualFile)
-    myFixture.completeBasic()
-    myFixture.checkResult text
+  public void testNoResultsInIncompleteStaticImport() {
+    String text = """
+      package p;
+      
+      import static Foo.ba<caret>
+      
+      class Foo {
+        static void bar() {}
+      }
+      """;
+    PsiFile file = myFixture.addFileToProject("p/Foo.java", text);
+    myFixture.configureFromExistingVirtualFile(file.getVirtualFile());
+    myFixture.completeBasic();
+    myFixture.checkResult(text);
   }
 
   @NeedsIndex.Full
-  void "test no global reformatting"() {
-    CodeStyle.getSettings(project).getCommonSettings(JavaLanguage.INSTANCE).ALIGN_MULTILINE_BINARY_OPERATION = true
+  public void testNoGlobalReformatting() {
+    CodeStyle.getSettings(getProject()).getCommonSettings(JavaLanguage.INSTANCE).ALIGN_MULTILINE_BINARY_OPERATION = true;
 
-    myFixture.addClass("package foo; public interface Foo { int XCONST = 42; }")
-    def file = myFixture.addFileToProject "a.java", '''
-class Zoo {
-    void zoo(int i) {
-        if (i == XCONS<caret> ||
-            CONTAINERS.contains(myElementType)) {
-        } 
-    }
-}
-'''
-    myFixture.configureFromExistingVirtualFile(file.virtualFile)
-    myFixture.complete(CompletionType.BASIC, 2)
-    myFixture.type('\n')
-    myFixture.checkResult '''\
-import foo.Foo;
-
-class Zoo {
-    void zoo(int i) {
-        if (i == Foo.XCONST<caret> ||
-            CONTAINERS.contains(myElementType)) {
-        } 
-    }
-}
-'''
+    myFixture.addClass("package foo; public interface Foo { int XCONST = 42; }");
+    PsiFile file = myFixture.addFileToProject("a.java", """
+      class Zoo {
+          void zoo(int i) {
+              if (i == XCONS<caret> ||
+                  CONTAINERS.contains(myElementType)) {
+              }
+          }
+      }
+    """);
+    myFixture.configureFromExistingVirtualFile(file.getVirtualFile());
+    myFixture.complete(CompletionType.BASIC, 2);
+    myFixture.type("\n");
+    myFixture.checkResult("""
+                            import foo.Foo;
+                            
+                            class Zoo {
+                                  void zoo(int i) {
+                                      if (i == Foo.XCONST<caret> ||
+                                          CONTAINERS.contains(myElementType)) {
+                                      }
+                                  }
+                              }
+                            """);
   }
 }
