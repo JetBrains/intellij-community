a334c9a4d3accc919e8cb2b9813028690c34d9c3
Alex Plate <aleksei.plate@jetbrains.com>
2024-02-15 16:20:08 +0200

    [Workspace Model] Remove unused perf test
    GitOrigin-RevId: e4062a17e03bf4d0dabac8d873e9c129fc08e3ce

--- platform/workspace/jps/tests/testSrc/com/intellij/workspaceModel/ide/WorkspaceModelBenchmarksPerformanceTest.kt
+++ platform/workspace/jps/tests/testSrc/com/intellij/workspaceModel/ide/WorkspaceModelBenchmarksPerformanceTest.kt
@@ -1028,48 +1028,6 @@ class WorkspaceModelBenchmarksPerformanceTest {
       .attempts(1).start()
   }
 
-  @Test
-  fun `find rate when cache is better on adding entities`() {
-    // This test finds a percentage of changes when the cache calculation remains faster than full recalculation of data
-    TracedSnapshotCache.LOG_QUEUE_MAX_SIZE = 2_000_000
-    listOf(1000, 10_000, 100_000, 500_000).forEach { size ->
-      val builder = MutableEntityStorage.create()
-      repeat(size) {
-        builder addEntity NamedEntity("Name$it", MySource)
-      }
-      val baseSnapshot = builder.toSnapshot()
-
-      val q = entities<NamedEntity>().map { it.myName }
-
-      baseSnapshot.cached(q)
-
-      val perentages = ArrayList<Int>()
-      val timesCalc = ArrayList<Duration>()
-      val timesCache = ArrayList<Duration>()
-      (1..100).forEach { percent ->
-        val intBuilder = baseSnapshot.toBuilder()
-        repeat((size / 100) * percent) { entitiesBatch ->
-          intBuilder addEntity NamedEntity("MyEnt$entitiesBatch", MySource)
-        }
-        val newSnapshot = intBuilder.toSnapshot()
-
-        val timeCalc = measureTime { newSnapshot.entities<NamedEntity>().map { it.myName }.toList() }
-        val timeCached = measureTime {
-          newSnapshot.cached(q)
-        }
-        timesCalc += timeCalc
-        timesCache += timeCached
-        if (timeCached < timeCalc) {
-          perentages.add(percent)
-        }
-      }
-      val averageCache = timesCache.map { it.inWholeMilliseconds }.average()
-      val averageCalc = timesCalc.map { it.inWholeMilliseconds }.average()
-      val maxPerc = perentages.sortedDescending().take(5)
-      println("Size: $size, average cache: $averageCache, average calc: $averageCalc, maxPerc: $maxPerc")
-    }
-  }
-
   @ParameterizedTest
   @ValueSource(booleans = [false, true])
   fun `find rate when cache is better on adding entities`(preInitializeEntities: Boolean) {
