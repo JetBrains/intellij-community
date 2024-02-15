510f358714af434371fdc2c3988de0d169cfbbf1
Chris Lemaire <chris.lemaire@jetbrains.com>
2024-02-15 12:02:56 +0100

    [gitlab] Rename getRemoteDescriptor to getSourceRemoteDescriptor
    More accurately describes the project it should describe.
    
    (cherry picked from commit 8eb5db8006af823fc206d1740e6269aebfe74152)
    
    IJ-CR-130159
    
    GitOrigin-RevId: 4a1d9424aa7d89c8bdcfbcad5841f0266df25fe3

--- plugins/gitlab/src/org/jetbrains/plugins/gitlab/mergerequest/util/GitLabMergeRequestBranchUtil.kt
+++ plugins/gitlab/src/org/jetbrains/plugins/gitlab/mergerequest/util/GitLabMergeRequestBranchUtil.kt
@@ -3,21 +3,21 @@ package org.jetbrains.plugins.gitlab.mergerequest.util
 
 import git4idea.remote.hosting.GitRemoteBranchesUtil
 import org.jetbrains.plugins.gitlab.mergerequest.data.GitLabMergeRequestFullDetails
-import org.jetbrains.plugins.gitlab.mergerequest.data.getRemoteDescriptor
+import org.jetbrains.plugins.gitlab.mergerequest.data.getSourceRemoteDescriptor
 import org.jetbrains.plugins.gitlab.util.GitLabProjectMapping
 
 object GitLabMergeRequestBranchUtil {
   private const val FORK_BRANCH_PREFIX = "fork"
 
   suspend fun fetchAndCheckoutBranch(mapping: GitLabProjectMapping, details: GitLabMergeRequestFullDetails) {
-    val remoteDescriptor = details.getRemoteDescriptor(mapping.repository.serverPath) ?: return
+    val remoteDescriptor = details.getSourceRemoteDescriptor(mapping.repository.serverPath) ?: return
     val isFork = details.sourceProject?.fullPath != details.targetProject.fullPath
     val localPrefix = if (isFork) FORK_BRANCH_PREFIX else null
     GitRemoteBranchesUtil.fetchAndCheckoutRemoteBranch(mapping.gitRepository, remoteDescriptor, details.sourceBranch, localPrefix)
   }
 
   suspend fun fetchAndShowRemoteBranchInLog(mapping: GitLabProjectMapping, details: GitLabMergeRequestFullDetails) {
-    val remoteDescriptor = details.getRemoteDescriptor(mapping.repository.serverPath) ?: return
+    val remoteDescriptor = details.getSourceRemoteDescriptor(mapping.repository.serverPath) ?: return
     GitRemoteBranchesUtil.fetchAndShowRemoteBranchInLog(mapping.gitRepository, remoteDescriptor, details.sourceBranch, details.targetBranch)
   }
 }
