c095ac545e7d06e3811b6f3d008fe556b01785ae
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2024-02-15 22:37:57 +0200

    [Java. Logging] Add tests for lombok based logger
    IDEA-345098
    
    GitOrigin-RevId: 41691d38bd4403f9befde7ea338113c50e7c6199

---
+++ plugins/lombok/src/test/java/com/intellij/java/lomboktest/LombokLoggerCompletionTest.kt
@@ -0,0 +1,50 @@
+package com.intellij.java.lomboktest
+
+import com.intellij.codeInsight.completion.JvmLoggerLookupElement
+import com.intellij.codeInsight.completion.LightFixtureCompletionTestCase
+import com.intellij.java.codeInsight.JvmLoggerTestSetupUtil
+import com.intellij.testFramework.LightProjectDescriptor
+import de.plushnikov.intellij.plugin.LombokTestUtil
+import de.plushnikov.intellij.plugin.logging.LombokLoggingUtils
+import junit.framework.TestCase
+
+class LombokLoggerCompletionTest : LightFixtureCompletionTestCase() {
+  fun testSlf4j() {
+    JvmLoggerTestSetupUtil.setupSlf4j(myFixture)
+
+    doTest(LombokLoggingUtils.SLF4J_ANNOTATION, "long", "log", "log", "clone")
+  }
+
+  fun testLog4j2() {
+    JvmLoggerTestSetupUtil.setupLog4j2(myFixture)
+
+    doTest(LombokLoggingUtils.LOG4J2_ANNOTATION, "long", "log", "log", "clone")
+  }
+
+  fun testLog4j() {
+    JvmLoggerTestSetupUtil.setupLog4j(myFixture)
+
+    doTest(LombokLoggingUtils.LOG4J_ANNOTATION, "long", "log", "log", "clone")
+  }
+
+  fun testApacheCommons() {
+    JvmLoggerTestSetupUtil.setupApacheCommons(myFixture)
+
+    doTest(LombokLoggingUtils.COMMONS_ANNOTATION, "long", "log", "log", "clone")
+  }
+
+  override fun getBasePath(): String = "community/plugins/lombok/testData/completion/logger"
+
+  override fun getProjectDescriptor(): LightProjectDescriptor = LombokTestUtil.LOMBOK_OLD_DESCRIPTOR
+
+  private fun doTest(typeName: String, vararg names: String) {
+    val name = getTestName(false)
+    configureByFile("before$name.java")
+    assertStringItems(*names)
+
+    val item = lookup.items.find { it is JvmLoggerLookupElement && it.typeName == typeName }
+    TestCase.assertNotNull(item)
+    selectItem(item)
+    checkResultByFile("after$name.java")
+  }
+}
