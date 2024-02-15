50f77224448dcdb9cfaf235993359ef490887573
Liudmila Kornilova <kornilova203@gmail.com>
2024-02-15 17:31:24 +0100

    IDEA-345401 Log when scanning happens without filling the filter
    GitOrigin-RevId: c3cf29f7d1f794762e0b919edac10c2ca30af92f

--- platform/lang-impl/src/com/intellij/util/indexing/FilesFilterScanningHandler.java
+++ platform/lang-impl/src/com/intellij/util/indexing/FilesFilterScanningHandler.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.indexing;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.util.indexing.projectFilter.ProjectIndexableFilesFilterHolder;
 import org.jetbrains.annotations.NotNull;
@@ -35,6 +36,7 @@ public interface FilesFilterScanningHandler {
   }
 
   class IdleFilesFilterScanningHandler implements FilesFilterScanningHandler {
+    private static final Logger LOG = Logger.getInstance(IdleFilesFilterScanningHandler.class);
     private final ProjectIndexableFilesFilterHolder myFilterHolder;
 
     public IdleFilesFilterScanningHandler(@NotNull ProjectIndexableFilesFilterHolder filterHolder) {
@@ -51,6 +53,7 @@ public interface FilesFilterScanningHandler {
 
     @Override
     public void scanningStarted(@NotNull Project project, boolean update) {
+      LOG.info("Scanning will happen without filling of project indexable files filter");
     }
   }
 }
