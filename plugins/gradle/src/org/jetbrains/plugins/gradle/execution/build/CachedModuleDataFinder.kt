8a88fbbdd6b5e67be7f9343a2c2e508393487a58
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2024-02-15 17:13:17 +0100

    IDEA-346297 [Gradle|DataNode] cleanup: converted CachedModuleDataFinder to Kotlin
    GitOrigin-RevId: 3ff2407ee67ac524384994e4416650c122be5ef9

--- plugins/gradle/src/org/jetbrains/plugins/gradle/execution/build/CachedModuleDataFinder.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/execution/build/CachedModuleDataFinder.kt
@@ -1,114 +1,90 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.gradle.execution.build;
+package org.jetbrains.plugins.gradle.execution.build
 
-import com.intellij.openapi.externalSystem.model.DataNode;
-import com.intellij.openapi.externalSystem.model.ExternalProjectInfo;
-import com.intellij.openapi.externalSystem.model.ProjectKeys;
-import com.intellij.openapi.externalSystem.model.project.ModuleData;
-import com.intellij.openapi.externalSystem.model.project.ProjectData;
-import com.intellij.openapi.externalSystem.service.project.ProjectDataManager;
-import com.intellij.openapi.externalSystem.service.project.manage.ExternalProjectsDataStorage;
-import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.psi.util.CachedValueProvider;
-import com.intellij.psi.util.CachedValuesManager;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.plugins.gradle.model.data.GradleSourceSetData;
-import org.jetbrains.plugins.gradle.util.GradleConstants;
-import org.jetbrains.plugins.gradle.util.GradleModuleData;
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import com.intellij.openapi.externalSystem.model.DataNode
+import com.intellij.openapi.externalSystem.model.ProjectKeys
+import com.intellij.openapi.externalSystem.model.project.ModuleData
+import com.intellij.openapi.externalSystem.service.project.manage.ExternalProjectsDataStorage
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
+import org.jetbrains.plugins.gradle.model.data.GradleSourceSetData
+import org.jetbrains.plugins.gradle.util.GradleConstants
+import org.jetbrains.plugins.gradle.util.GradleModuleData
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * @author Vladislav.Soroka
  */
-public class CachedModuleDataFinder {
+class CachedModuleDataFinder private constructor(private val project: Project) {
 
-  private final @NotNull Project myProject;
-
-  private CachedModuleDataFinder(@NotNull Project project) {
-    myProject = project;
+  fun findGradleModuleData(module: Module): GradleModuleData? {
+    val moduleData = findMainModuleData(module) ?: return null
+    return GradleModuleData(moduleData)
   }
 
-  public static CachedModuleDataFinder getInstance(@NotNull Project project) {
-    return new CachedModuleDataFinder(project);
-  }
-
-  @ApiStatus.Experimental
-  public static @Nullable GradleModuleData getGradleModuleData(@NotNull Module module) {
-    DataNode<? extends ModuleData> moduleData = getInstance(module.getProject()).findMainModuleData(module);
-    return moduleData != null ? new GradleModuleData(moduleData) : null;
-  }
-
-  public static @Nullable DataNode<? extends ModuleData> findModuleData(@NotNull Project project, @NotNull String modulePath) {
-    var projectNode = ExternalSystemApiUtil.findProjectNode(project, GradleConstants.SYSTEM_ID, modulePath);
-    if (projectNode == null) return null;
-
-    return getInstance(project).findModuleData(projectNode, modulePath);
-  }
-
-  @Nullable
-  private DataNode<? extends ModuleData> findModuleData(final DataNode<ProjectData> projectNode, final String projectPath) {
-    DataNode<? extends ModuleData> cachedNode = getCache().get(projectPath);
-    if (cachedNode != null) return cachedNode;
-
-    return ExternalSystemApiUtil.find(projectNode, ProjectKeys.MODULE, node -> {
-      String externalProjectPath = node.getData().getLinkedExternalProjectPath();
-      getCache().put(externalProjectPath, node);
-      return StringUtil.equals(projectPath, node.getData().getLinkedExternalProjectPath());
-    });
-  }
-
-  @Nullable
-  public DataNode<? extends ModuleData> findModuleData(@NotNull Module module) {
-    DataNode<? extends ModuleData> mainModuleData = findMainModuleData(module);
-    if (mainModuleData == null) return null;
-
-    boolean isSourceSet = GradleConstants.GRADLE_SOURCE_SET_MODULE_TYPE_KEY.equals(ExternalSystemApiUtil.getExternalModuleType(module));
-    if (!isSourceSet) {
-      return mainModuleData;
+  private fun findModuleData(module: Module): DataNode<out ModuleData>? {
+    val moduleType = ExternalSystemApiUtil.getExternalModuleType(module)
+    if (GradleConstants.GRADLE_SOURCE_SET_MODULE_TYPE_KEY != moduleType) {
+      return findMainModuleData(module)
     }
-    else {
-      String projectId = ExternalSystemApiUtil.getExternalProjectId(module);
-      DataNode<? extends ModuleData> cachedNode = getCache().get(projectId);
-      if (cachedNode != null) return cachedNode;
 
-      return ExternalSystemApiUtil.find(mainModuleData, GradleSourceSetData.KEY, node -> {
-        String id = node.getData().getId();
-        getCache().put(id, node);
-        return StringUtil.equals(projectId, id);
-      });
+    val projectId = ExternalSystemApiUtil.getExternalProjectId(module) ?: return null
+    val cachedNode = getCache(project)[projectId]
+    if (cachedNode != null) return cachedNode
+
+    val mainModuleData = findMainModuleData(module) ?: return null
+    return ExternalSystemApiUtil.findChild(mainModuleData, GradleSourceSetData.KEY) {
+      val id = it.data.id
+      getCache(project)[id] = it
+      StringUtil.equals(projectId, id)
     }
   }
 
-  @Nullable
-  public DataNode<? extends ModuleData> findMainModuleData(@NotNull Module module) {
-    final String rootProjectPath = ExternalSystemApiUtil.getExternalRootProjectPath(module);
-    if (rootProjectPath == null) return null;
+  fun findMainModuleData(module: Module): DataNode<out ModuleData>? {
+    ExternalSystemApiUtil.getExternalRootProjectPath(module) ?: return null
+    ExternalSystemApiUtil.getExternalProjectId(module) ?: return null
 
-    final String projectId = ExternalSystemApiUtil.getExternalProjectId(module);
-    if (projectId == null) return null;
-    final String externalProjectPath = ExternalSystemApiUtil.getExternalProjectPath(module);
-    if (externalProjectPath == null) return null;
-
-    ExternalProjectInfo projectData =
-      ProjectDataManager.getInstance().getExternalProjectData(module.getProject(), GradleConstants.SYSTEM_ID, rootProjectPath);
-    if (projectData == null) return null;
-
-    DataNode<ProjectData> projectStructure = projectData.getExternalProjectStructure();
-    if (projectStructure == null) return null;
-
-    return findModuleData(projectStructure, externalProjectPath);
+    val externalProjectPath = ExternalSystemApiUtil.getExternalProjectPath(module) ?: return null
+    return findModuleData(module.project, externalProjectPath)
   }
 
-  private Map<String, DataNode<? extends ModuleData>> getCache() {
-    return CachedValuesManager.getManager(myProject).getCachedValue(myProject, () -> {
-      return CachedValueProvider.Result.create(new ConcurrentHashMap<>(), ExternalProjectsDataStorage.getInstance(myProject));
-    });
+  fun findModuleData(project: Project, modulePath: String): DataNode<out ModuleData>? {
+    val cachedNode = getCache(project)[modulePath]
+    if (cachedNode != null) return cachedNode
+
+    val projectNode = ExternalSystemApiUtil.findProjectNode(project, GradleConstants.SYSTEM_ID, modulePath) ?: return null
+    return ExternalSystemApiUtil.findChild(projectNode, ProjectKeys.MODULE) {
+      val externalProjectPath = it.data.linkedExternalProjectPath
+      getCache(project)[externalProjectPath] = it
+      modulePath == externalProjectPath
+    }
+  }
+
+  companion object {
+
+    @JvmStatic
+    fun getInstance(project: Project): CachedModuleDataFinder {
+      return CachedModuleDataFinder(project)
+    }
+
+    @JvmStatic
+    fun getGradleModuleData(module: Module): GradleModuleData? {
+      return getInstance(module.project).findGradleModuleData(module)
+    }
+
+    fun findModuleData(project: Project, modulePath: String): DataNode<out ModuleData>? {
+      return getInstance(project).findModuleData(project, modulePath)
+    }
+
+    @JvmStatic
+    private fun getCache(project: Project): ConcurrentHashMap<String, DataNode<out ModuleData>> {
+      return CachedValuesManager.getManager(project).getCachedValue(project) {
+        CachedValueProvider.Result.create(ConcurrentHashMap(), ExternalProjectsDataStorage.getInstance(project))
+      }
+    }
   }
 }
