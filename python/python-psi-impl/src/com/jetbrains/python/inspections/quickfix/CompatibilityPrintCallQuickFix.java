6b661047642eae2998e843fdef1e83214758a979
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2024-02-16 12:59:19 +0200

    Add support to replace old print to the new one in different files for CompatibilityPrintCallQuickFix
    PY-65297
    
    GitOrigin-RevId: 6e35127178c19e3197b7442bcadc274114c5571f

--- python/python-psi-impl/src/com/jetbrains/python/inspections/quickfix/CompatibilityPrintCallQuickFix.java
+++ python/python-psi-impl/src/com/jetbrains/python/inspections/quickfix/CompatibilityPrintCallQuickFix.java
@@ -2,23 +2,31 @@
 package com.jetbrains.python.inspections.quickfix;
 
 import com.intellij.codeInspection.ProblemDescriptor;
-import com.intellij.modcommand.*;
+import com.intellij.modcommand.ActionContext;
+import com.intellij.modcommand.ModCommand;
+import com.intellij.modcommand.ModCommandBatchQuickFix;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.containers.MultiMap;
 import com.jetbrains.python.PyPsiBundle;
 import com.jetbrains.python.codeInsight.imports.AddImportHelper;
-import com.jetbrains.python.psi.*;
+import com.jetbrains.python.psi.LanguageLevel;
+import com.jetbrains.python.psi.PyElement;
+import com.jetbrains.python.psi.PyElementGenerator;
+import com.jetbrains.python.psi.PyExpression;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 /**
  * User: catherine
- *
+ * <p>
  * QuickFix to replace statement that has no effect with function call
  */
 public class CompatibilityPrintCallQuickFix extends ModCommandBatchQuickFix {
@@ -32,19 +40,24 @@ public class CompatibilityPrintCallQuickFix extends ModCommandBatchQuickFix {
   public @NotNull ModCommand perform(@NotNull Project project, @NotNull List<ProblemDescriptor> descriptors) {
     PyElementGenerator elementGenerator = PyElementGenerator.getInstance(project);
     return ModCommand.psiUpdate(ActionContext.from(descriptors.get(0)), updater -> {
-      List<PsiElement> elements = ContainerUtil.map(descriptors, d -> updater.getWritable(d.getStartElement()));
-      PsiFile file = elements.get(0).getContainingFile();
-      for (PsiElement element : elements) {
-        if (element.isValid()) {
-          replace(element, elementGenerator);
+      List<PsiElement> extractedElements = ContainerUtil.map(descriptors, d -> d.getStartElement());
+      MultiMap<String, PsiElement> groupedElements =
+        ContainerUtil.groupBy(extractedElements, element -> element.getContainingFile().getVirtualFile().getPresentableUrl());
+      for (Map.Entry<String, Collection<PsiElement>> entry : groupedElements.entrySet()) {
+        List<PsiElement> writableElements = ContainerUtil.map(entry.getValue(), element -> updater.getWritable(element));
+        PsiFile file = writableElements.get(0).getContainingFile();
+        for (PsiElement element : writableElements) {
+          if (element.isValid()) {
+            replace(element, elementGenerator);
+          }
         }
+        AddImportHelper.addOrUpdateFromImportStatement(file,
+                                                       "__future__",
+                                                       "print_function",
+                                                       null,
+                                                       AddImportHelper.ImportPriority.FUTURE,
+                                                       null);
       }
-      AddImportHelper.addOrUpdateFromImportStatement(file,
-                                                     "__future__",
-                                                     "print_function",
-                                                     null,
-                                                     AddImportHelper.ImportPriority.FUTURE,
-                                                     null);
     });
   }
 
