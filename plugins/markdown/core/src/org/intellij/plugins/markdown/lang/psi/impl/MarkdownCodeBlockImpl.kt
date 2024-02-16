46d86804f745aff348589fd1dd49caf1f76d04a6
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-02-16 14:30:20 +0100

    [markdown] API: mark for removal unused deprecated API (IJPL-275)
    GitOrigin-RevId: 7fe597bb8c7cf4cb29c4c95e48b542976854550f

--- plugins/markdown/core/src/org/intellij/plugins/markdown/lang/psi/impl/MarkdownCodeBlockImpl.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/lang/psi/impl/MarkdownCodeBlockImpl.kt
@@ -3,9 +3,11 @@ package org.intellij.plugins.markdown.lang.psi.impl
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
 import org.intellij.plugins.markdown.lang.psi.MarkdownPsiElement
+import org.jetbrains.annotations.ApiStatus
 
 /**
  * Please use [MarkdownCodeBlock] instead.
  */
 @Deprecated("Please use {@link MarkdownCodeBlock} instead.", ReplaceWith("MarkdownCodeBlock"))
+@ApiStatus.ScheduledForRemoval
 abstract class MarkdownCodeBlockImpl(node: ASTNode): ASTWrapperPsiElement(node), MarkdownPsiElement
