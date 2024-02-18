133c84ddb12e18e5009cde5bfa6fa2acb441fdeb
Eve Matthaey <ermatt@meta.com>
2024-02-18 12:55:30 -0500

    [kotlin] J2K: Beef up tests for simplifying assert statements
    KTIJ-28575
    
    https://github.com/JetBrains/intellij-community/pull/2678
    
    GitOrigin-RevId: 86222324ca01be095108b6f189162c880c101f8a

--- plugins/kotlin/j2k/k1.new/tests/testData/newJ2k/assertStatement/assertNotNull.kt
+++ plugins/kotlin/j2k/k1.new/tests/testData/newJ2k/assertStatement/assertNotNull.kt
@@ -1,8 +1,19 @@
 internal abstract class C {
+    var mMemberVariable: String? = null
+
     fun foo() {
         val s1 = f()!!
         val s2 = g() ?: error("g should not return null")
+        val doNotMergeDueToPossibleSideEffects = g()
+        assert(s2.hashCode() == 42)
         val h = s2.hashCode()
+
+        val s3 = "sss"
+        assert(s3.hashCode() != null)
+        assert(doNotMergeDueToPossibleSideEffects != null)
+        assert(mMemberVariable != null)
+        val doNotTouchDifferentAssert = f()
+        assert(doNotTouchDifferentAssert == null)
     }
 
     abstract fun f(): String?
