817792bddf875db30f2759d58edfb045e3f2557f
Denes Daniel <pantherdd@gmail.com>
2024-02-18 20:13:09 +0100

    IDEA-338741 - Add test for scope conflict handling on Gradle import
    Signed-off-by: Denes Daniel <pantherdd@gmail.com>

--- plugins/gradle/testSources/org/jetbrains/plugins/gradle/importing/GradleDependenciesImportingTest.java
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/importing/GradleDependenciesImportingTest.java
@@ -1861,6 +1861,60 @@ public class GradleDependenciesImportingTest extends GradleImportingTestCase {
   }
 
   @Test
+  public void testScopeConflictOnDependencyWithMultipleConfigurations() throws Exception {
+    createProjectSubFile("repo/depGroup/depArtifact/1.0-SNAPSHOT/ivy-1.0-SNAPSHOT.xml",
+                         """
+                           <?xml version="1.0" encoding="ISO-8859-1"?>
+                           <ivy-module version="1.0">
+                             <info organisation="depGroup" module="depArtifact" revision="1.0-SNAPSHOT" status="integration"/>
+                             <configurations>
+                               <conf name="api"/>
+                               <conf name="runtime" extends="api"/>
+                             </configurations>
+                             <publications>
+                               <artifact name="depArtifact-api" ext="jar" type="jar" conf="api"/>
+                               <artifact name="depArtifact-runtime" ext="jar" type="jar" conf="runtime"/>
+                             </publications>
+                             <dependencies/>
+                           </ivy-module>
+                           """);
+    String apiJar = createProjectJarSubFile("repo/depGroup/depArtifact/1.0-SNAPSHOT/depArtifact-api-1.0-SNAPSHOT.jar").getUrl();
+    String runtimeJar =  createProjectJarSubFile("repo/depGroup/depArtifact/1.0-SNAPSHOT/depArtifact-runtime-1.0-SNAPSHOT.jar").getUrl();
+
+    importProject(
+      createBuildScriptBuilder()
+        .withJavaPlugin()
+        .addPrefix("repositories { ivy { url = file('repo') } }")
+        .addPrefix("""
+                     dependencies {
+                       implementation 'depGroup:depArtifact:1.0-SNAPSHOT' targetConfiguration 'api'
+                       runtimeOnly 'depGroup:depArtifact:1.0-SNAPSHOT' targetConfiguration 'runtime'
+                     }
+                     """)
+        .withIdeaPlugin()
+        .generate()
+    );
+
+    assertModules("project", "project.main", "project.test");
+
+    assertModuleModuleDepScope("project.test", "project.main", DependencyScope.COMPILE);
+
+    // Gradle itself can correctly resolve the above dependencies as follows:
+    // - compileClasspath: listOf(apiJar)
+    // - runtimeClasspath: listOf(apiJar, runtimeJar)
+    // Unfortunately, since the dependencies' GAV coordinates are the same, IntelliJ merges the 2 JAR artifacts into a single dependency.
+    // For lack of a better option, such a dependency should have a scope that covers all the scopes of its artifacts (e.g. if apiJar is in
+    // COMPILE scope and runtimeJar is in RUNTIME, then the merged dependency should inherit the broader COMPILE scope). This allows the IDE
+    // to provide assistance to the developer when they're writing code that uses the dependency.
+    // For context, see IDEA-338741
+    final String depName = "Gradle: depGroup:depArtifact:1.0-SNAPSHOT";
+    assertModuleLibDep("project.main", depName, List.of(apiJar, runtimeJar), null, null);
+    assertModuleLibDepScope("project.main", depName, DependencyScope.COMPILE);
+    assertModuleLibDep("project.test", depName, List.of(apiJar, runtimeJar), null, null);
+    assertModuleLibDepScope("project.test", depName, DependencyScope.COMPILE);
+  }
+
+  @Test
   @TargetVersions("4.6+")
   public void testAnnotationProcessorDependencies() throws Exception {
     var lombok = "org.projectlombok:lombok:1.16.2";
