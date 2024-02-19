38f48e474f2bc85acb5b5f5328c623bd23fea9a0
Liudmila Kornilova <kornilova203@gmail.com>
2024-02-19 17:24:50 +0100

    IDEA-345401 Make healthCheckFuture volatile
    GitOrigin-RevId: b6173445807407a945430455a162ba5fb2710570

--- platform/lang-impl/src/com/intellij/util/indexing/projectFilter/ProjectIndexableFilesFilterHealthCheck.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/projectFilter/ProjectIndexableFilesFilterHealthCheck.kt
@@ -24,6 +24,7 @@ import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
 
 internal class ProjectIndexableFilesFilterHealthCheck(private val project: Project, private val filter: ProjectIndexableFilesFilter) {
+  @Volatile
   private var healthCheckFuture: ScheduledFuture<*>? = null
 
   fun setUpHealthCheck() {
