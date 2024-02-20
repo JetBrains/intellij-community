99cc2afd17c90e376332257e4350c9594f72333e
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2024-02-20 20:32:16 +0200

    [Kotlin] Fix KMP line marker test for K2
    Set the registry key to enable JS module analysis.
    Remove no longer rendered icon references from test data.
    Rectify dependencies in the project structure.
    
    GitOrigin-RevId: 11ae413163d4ce50ba0bbc2f2d9b193bcf0494f1

--- plugins/kotlin/code-insight/testData/linemarkers/multiplatform/common/common.kt
+++ plugins/kotlin/code-insight/testData/linemarkers/multiplatform/common/common.kt
@@ -1,18 +1,18 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package sample
 
-expect class <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=jvm; icon=nodes/Module.svg); (text=js; icon=nodes/Module.svg)]")!>Sample<!>() {
-    fun <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=jvm; icon=nodes/Module.svg); (text=js; icon=nodes/Module.svg)]")!>checkMe<!>(): Int
+expect class <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=jvm); (text=js)]")!>Sample<!>() {
+    fun <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=jvm); (text=js)]")!>checkMe<!>(): Int
 }
 
-expect object <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=jvm; icon=nodes/Module.svg); (text=js; icon=nodes/Module.svg)]")!>Platform<!> {
-    val <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=jvm; icon=nodes/Module.svg); (text=js; icon=nodes/Module.svg)]")!>name<!>: String
+expect object <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=jvm); (text=js)]")!>Platform<!> {
+    val <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=jvm); (text=js)]")!>name<!>: String
 }
 
 fun hello(): String = "Hello from ${Platform.name}"
 
-expect fun <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=jvm; icon=nodes/Module.svg); (text=js; icon=nodes/Module.svg)]")!>foo<!>()
+expect fun <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=jvm); (text=js)]")!>foo<!>()
 
-expect annotation class <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=jvm; icon=nodes/Module.svg); (text=js; icon=nodes/Module.svg)]")!>Preview<!>()
+expect annotation class <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=js); (text=jvm)]")!>Preview<!>()
 
-expect object <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=jvm; icon=nodes/Module.svg); (text=js; icon=nodes/Module.svg)]")!>SomeObject<!>()
+expect object <!LINE_MARKER("descr='Has actuals in js, jvm modules'; targets=[(text=js); (text=jvm)]")!>SomeObject<!>()
