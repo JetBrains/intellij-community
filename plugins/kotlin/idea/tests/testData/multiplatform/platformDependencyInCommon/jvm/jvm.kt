7e619f6ed694922f42153ba224f1e0cf2719262b
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2024-02-20 15:56:00 +0200

    [Kotlin] test data fix for KMP analysis test
    Change @Metadata to synchronized(), which is different between JVM, JS and common.
    
    GitOrigin-RevId: f832884e513bc9dc7da024cc70b9535ba1b3fe3a

--- plugins/kotlin/idea/tests/testData/multiplatform/platformDependencyInCommon/jvm/jvm.kt
+++ plugins/kotlin/idea/tests/testData/multiplatform/platformDependencyInCommon/jvm/jvm.kt
@@ -2,5 +2,7 @@ fun checkCloneable(array: Array<String>) {
     array.clone()
 }
 
-<!EXPLICIT_METADATA_IS_DISALLOWED!>@Metadata<!>
-class MetaWithError
+fun checkSynchronizedIsResolvedInJvm() {
+    synchronized(Any()) {
+    }
+}
