12a2c47ff3b5d924f37d816babee9047760357ce
Alexandr Evstigneev <alexandr.evstigneev@jetbrains.com>
2024-02-21 18:06:24 +0400

    Fixed problem resolving composite shared scopes
    Prior this change platform looked for sub-scopes in local scopes, and ignored shared scopes.
    
    Now we are looking in local scopes and if we can't find a sub-scope, looking in shared scopes, but not vice versa.
    
    (cherry picked from commit 0c262a38fd08de2c62bd7ca4b8a8dec838f714e6)
    
    IJ-CR-127128
    
    GitOrigin-RevId: 768203b0ff360601c4203dd88319e3e1f4f8542f

--- platform/analysis-api/src/com/intellij/psi/search/scope/packageSet/NamedPackageSetReference.java
+++ platform/analysis-api/src/com/intellij/psi/search/scope/packageSet/NamedPackageSetReference.java
@@ -1,9 +1,10 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.search.scope.packageSet;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.packageDependencies.DependencyValidationManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,13 +18,28 @@ public final class NamedPackageSetReference extends PackageSetBase {
   @Override
   public boolean contains(@NotNull VirtualFile file, @NotNull Project project, @Nullable NamedScopesHolder holder) {
     if (holder == null) return false;
-    final NamedScope scope = holder.getScope(myName);
-    if (scope != null) {
-      final PackageSet packageSet = scope.getValue();
-      if (packageSet != null) {
-        return packageSet instanceof PackageSetBase ? ((PackageSetBase)packageSet).contains(file, project, holder) : packageSet.contains(getPsiFile(file, project), holder);
+    NamedScope scope = holder.getScope(myName);
+
+    if (scope == null) {
+      if (!(holder instanceof NamedScopeManager)) {
+        return false;
       }
+
+      var sharedScopesHolder = DependencyValidationManager.getInstance(project);
+      scope = sharedScopesHolder.getScope(myName);
+      if (scope == null) {
+        return false;
+      }
+      holder = sharedScopesHolder;
     }
+
+    final PackageSet packageSet = scope.getValue();
+    if (packageSet != null) {
+      return packageSet instanceof PackageSetBase
+             ? ((PackageSetBase)packageSet).contains(file, project, holder)
+             : packageSet.contains(getPsiFile(file, project), holder);
+    }
+
     return false;
   }
 
