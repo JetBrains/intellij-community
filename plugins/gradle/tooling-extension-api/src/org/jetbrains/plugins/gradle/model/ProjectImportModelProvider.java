f788d9f3c6e61339e26d5551184a10d6f5378f41
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2024-02-21 14:13:22 +0100

    [Gradle|Sync] cleanup: java doc for the ProjectImportModelProvider
    GitOrigin-RevId: 12ca38be0f44388d88a3fb579dd3dba4ae2b5234

--- plugins/gradle/tooling-extension-api/src/org/jetbrains/plugins/gradle/model/ProjectImportModelProvider.java
+++ plugins/gradle/tooling-extension-api/src/org/jetbrains/plugins/gradle/model/ProjectImportModelProvider.java
@@ -13,15 +13,14 @@ import org.jetbrains.annotations.NotNull;
 import java.io.Serializable;
 
 /**
- * Allows the {@link ProjectImportAction} to be extended to allow extra flexibility to extensions when requesting the models.
+ * Allows the Gradle model fetch action to be extended to allow extra flexibility to extensions when requesting the models.
  * <p>
- * {@link #populateProjectModels(BuildController, Model, ProjectModelConsumer)} is called once for each {@link GradleProject} obtained
+ * {@link #populateProjectModels} is called once for each {@link GradleProject} obtained
  * from the Gradle Tooling API (this includes projects from included builds).
  * <p>
- * {@link #populateBuildModels(BuildController, GradleBuild, BuildModelConsumer)} is called once for each {@link GradleBuild} that is
+ * {@link #populateBuildModels} is called once for each {@link GradleBuild} that is
  * obtained from the Gradle Tooling API, for none-composite builds this will be called exactly once, for composite builds this will be
- * called once for each included build and once for the name build. This will always be called after
- * {@link #populateProjectModels(BuildController, Model, ProjectModelConsumer)}.
+ * called once for each included build and once for the name build. This will always be called after {@link #populateProjectModels}.
  */
 public interface ProjectImportModelProvider extends Serializable {
 
