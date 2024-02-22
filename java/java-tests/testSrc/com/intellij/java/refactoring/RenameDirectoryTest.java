ab2d8579a48ddb0711534e9086a20fc54bd0d786
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-02-22 14:34:43 +0100

    [java] convert RenameDirectoryTest to Java (IDEA-331682)
    GitOrigin-RevId: e8c40eb76e027efae801af415a63ca5dd578f627

--- java/java-tests/testSrc/com/intellij/java/refactoring/RenameDirectoryTest.java
+++ java/java-tests/testSrc/com/intellij/java/refactoring/RenameDirectoryTest.java
@@ -1,51 +1,36 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.intellij.java.refactoring
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.java.refactoring;
 
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiClass;
+import com.intellij.refactoring.rename.RenameProcessor;
+import com.intellij.testFramework.PsiTestUtil;
+import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase;
 
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.refactoring.rename.RenameProcessor
-import com.intellij.testFramework.PsiTestUtil
-import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase
-import groovy.transform.CompileStatic
+import java.io.IOException;
 
-@CompileStatic
-class RenameDirectoryTest extends JavaCodeInsightFixtureTestCase {
+public class RenameDirectoryTest extends JavaCodeInsightFixtureTestCase {
+  public void testRenameSrcRootWithTextOccurrences() throws IOException {
+    VirtualFile srcRoot = myFixture.getTempDirFixture().findOrCreateDir("src2");
+    PsiTestUtil.removeAllRoots(getModule(), null);
+    PsiTestUtil.addSourceRoot(getModule(), srcRoot);
 
-  void testRenameSrcRootWithTextOccurrences() {
-    VirtualFile srcRoot = myFixture.tempDirFixture.findOrCreateDir("src2")
-    PsiTestUtil.removeAllRoots(module, null)
-    PsiTestUtil.addSourceRoot(module, srcRoot)
+    PsiClass fooClass = myFixture.addClass("""
+                                             // PsiPackage:
+                                             class Foo {
+                                               String s1 = "PsiPackage:"
+                                             }
+                                             """);
+    myFixture.configureFromExistingVirtualFile(fooClass.getContainingFile().getVirtualFile());
 
-    def fooClass = myFixture.addClass("""
-// PsiPackage:
-class Foo {
-  String s1 = "PsiPackage:"
-}
-""")
-    myFixture.configureFromExistingVirtualFile(fooClass.containingFile.virtualFile)
+    new RenameProcessor(getProject(), getPsiManager().findDirectory(srcRoot), "newName", true, true).run();
 
-    new RenameProcessor(getProject(), psiManager.findDirectory(srcRoot), "newName", true, true).run()
-
-    assert srcRoot.path.endsWith("newName")
-    myFixture.checkResult """
-// PsiPackage:
-class Foo {
-  String s1 = "PsiPackage:"
-}
-"""
+    assert srcRoot.getPath().endsWith("newName");
+    myFixture.checkResult("""
+                            // PsiPackage:
+                            class Foo {
+                              String s1 = "PsiPackage:"
+                            }
+                            """);
   }
 }
