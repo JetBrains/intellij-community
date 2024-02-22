d019dcfd5ba2858d3f7a9901d03a910e21d738e3
Alex Plate <aleksei.plate@jetbrains.com>
2024-02-22 10:49:17 +0200

    [Workspace Model] Get rid of `SuperSimpleEntity` in tests
    GitOrigin-RevId: 8701857d1e8456501547f13c9b6eedcd7cc8f9f3

--- platform/workspace/storage/tests/testSrc/com/intellij/platform/workspace/storage/tests/GeneratedCodeVersionsTest.kt
+++ platform/workspace/storage/tests/testSrc/com/intellij/platform/workspace/storage/tests/GeneratedCodeVersionsTest.kt
@@ -1,35 +1,29 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.workspace.storage.tests
 
-import com.intellij.platform.workspace.storage.*
-import com.intellij.platform.workspace.storage.impl.ConnectionId
-import com.intellij.platform.workspace.storage.impl.ModifiableWorkspaceEntityBase
-import com.intellij.platform.workspace.storage.impl.WorkspaceEntityBase
-import com.intellij.platform.workspace.storage.impl.WorkspaceEntityData
-import com.intellij.platform.workspace.storage.instrumentation.EntityStorageInstrumentation
-import com.intellij.platform.workspace.storage.instrumentation.EntityStorageInstrumentationApi
-import com.intellij.platform.workspace.storage.metadata.model.EntityMetadata
+import com.intellij.platform.workspace.storage.CodeGeneratorVersions
+import com.intellij.platform.workspace.storage.testEntities.entities.MySource
+import com.intellij.platform.workspace.storage.testEntities.entities.ParentEntity
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import kotlin.test.assertContains
 import kotlin.test.assertTrue
 import kotlin.test.fail
 
 class GeneratedCodeVersionsTest {
-  private var prev_api = 0
-  private var prev_impl = 0
+  private var prevApi = 0
+  private var prevImpl = 0
 
   @BeforeEach
   fun setUp() {
-    prev_api = CodeGeneratorVersions.API_VERSION
-    prev_impl = CodeGeneratorVersions.IMPL_VERSION
+    prevApi = CodeGeneratorVersions.API_VERSION
+    prevImpl = CodeGeneratorVersions.IMPL_VERSION
   }
 
   @AfterEach
   fun tearDown() {
-    CodeGeneratorVersions.API_VERSION = prev_api
-    CodeGeneratorVersions.IMPL_VERSION = prev_impl
+    CodeGeneratorVersions.API_VERSION = prevApi
+    CodeGeneratorVersions.IMPL_VERSION = prevImpl
     CodeGeneratorVersions.checkApiInImpl = true
     CodeGeneratorVersions.checkApiInInterface = true
   }
@@ -37,12 +31,15 @@ class GeneratedCodeVersionsTest {
   @Test
   fun `test api builder interface`() {
     val emptyBuilder = createEmptyBuilder()
+    assertTrue(CodeGeneratorVersions.checkApiInInterface)
+    CodeGeneratorVersions.API_VERSION = 10_000
     try {
-      emptyBuilder.addEntity(SuperSimpleEntity {})
+      emptyBuilder.addEntity(ParentEntity("", MySource))
     }
     catch (e: AssertionError) {
       assertTrue("API" in e.message!!)
-      assertTrue("'1000000'" in e.message!!)
+      assertTrue("'10000'" in e.message!!)
+      assertTrue(prevApi.toString() in e.message!!)
       return
     }
     fail("No exception thrown")
@@ -50,15 +47,18 @@ class GeneratedCodeVersionsTest {
 
   @Test
   fun `test api builder impl code`() {
-    CodeGeneratorVersions.API_VERSION = 1000000
+    CodeGeneratorVersions.checkApiInInterface = false
+    assertTrue(CodeGeneratorVersions.checkApiInImpl)
+    CodeGeneratorVersions.API_VERSION = 10_000
     val emptyBuilder = createEmptyBuilder()
     try {
-      emptyBuilder.addEntity(SuperSimpleEntity {})
+      emptyBuilder.addEntity(ParentEntity("", MySource))
     }
     catch (e: AssertionError) {
-      assertContains(e.message!!, "API")
-      assertContains(e.message!!, "'1000000'")
-      assertContains(e.message!!, "'1000001'")
+      assertTrue("API" in e.message!!)
+      assertTrue("'10000'" in e.message!!)
+      assertTrue(prevApi.toString() in e.message!!)
+      assertTrue("implementation" in e.message!!)
       return
     }
     fail("No exception thrown")
@@ -68,199 +68,18 @@ class GeneratedCodeVersionsTest {
   fun `test impl builder impl code`() {
     CodeGeneratorVersions.checkApiInInterface = false
     CodeGeneratorVersions.checkApiInImpl = false
+    assertTrue(CodeGeneratorVersions.checkImplInImpl)
+    CodeGeneratorVersions.IMPL_VERSION = 10_000
     val emptyBuilder = createEmptyBuilder()
     try {
-      emptyBuilder.addEntity(SuperSimpleEntity {})
+      emptyBuilder.addEntity(ParentEntity("", MySource))
     }
     catch (e: AssertionError) {
-      assertContains(e.message!!, "IMPL")
-      assertContains(e.message!!, "'1000002'")
+      assertTrue("IMPL" in e.message!!)
+      assertTrue("'10000'" in e.message!!)
+      assertTrue(prevImpl.toString() in e.message!!)
       return
     }
     fail("No exception thrown")
-  }
-}
-
-interface SuperSimpleEntity : WorkspaceEntity {
-  //region generated code
-  //@formatter:off
-  @GeneratedCodeApiVersion(1000000)
-  interface Builder: SuperSimpleEntity, WorkspaceEntity.Builder<SuperSimpleEntity> {
-  }
-
-  companion object: EntityType<SuperSimpleEntity, Builder>() {
-    operator fun invoke(init: (Builder.() -> Unit)? = null):SuperSimpleEntity {
-      val builder = builder()
-      init?.invoke(builder)
-      return builder
-    }
-  }
-  //@formatter:on
-  //endregion
-
-}
-
-
-@GeneratedCodeApiVersion(1000001)
-@GeneratedCodeImplVersion(1000002)
-open class SuperSimpleEntityImpl : SuperSimpleEntity, WorkspaceEntityBase(null) {
-
-
-  class Builder(result: SuperSimpleEntityData?) : ModifiableWorkspaceEntityBase<SuperSimpleEntity, SuperSimpleEntityData>(
-    result), SuperSimpleEntity.Builder {
-    constructor() : this(SuperSimpleEntityData())
-
-    override fun applyToBuilder(builder: MutableEntityStorage) {
-      if (this.diff != null) {
-        if (existsInBuilder(builder)) {
-          this.diff = builder
-          return
-        }
-        else {
-          error("Entity SuperSimpleEntity is already created in a different builder")
-        }
-      }
-
-      this.diff = builder
-      this.snapshot = builder
-      addToBuilder()
-      this.id = getEntityData().createEntityId()
-
-      /*
-      // Process entities from extension fields
-      val keysToRemove = ArrayList<ExtRefKey>()
-      for ((key, entity) in extReferences) {
-        if (!key.isChild()) {
-          continue
-        }
-        if (entity is List<*>) {
-          for (item in entity) {
-            if (item is ModifiableWorkspaceEntityBase<*, *>) {
-              builder.addEntity(item)
-            }
-          }
-          entity as List<WorkspaceEntity>
-          val (withBuilder_entity, woBuilder_entity) = entity.partition { it is ModifiableWorkspaceEntityBase<*, *> && it.diff != null }
-          applyRef(key.getConnectionId(), withBuilder_entity)
-          keysToRemove.add(key)
-        }
-        else {
-          entity as WorkspaceEntity
-          builder.addEntity(entity)
-          applyRef(key.getConnectionId(), entity)
-          keysToRemove.add(key)
-        }
-      }
-      for (key in keysToRemove) {
-        extReferences.remove(key)
-      }
-      */
-
-      /*
-      // Adding parents and references to the parent
-      val parentKeysToRemove = ArrayList<ExtRefKey>()
-      for ((key, entity) in extReferences) {
-        if (key.isChild()) {
-          continue
-        }
-        if (entity is List<*>) {
-          error("Cannot have parent lists")
-        }
-        else {
-          entity as WorkspaceEntity
-          builder.addEntity(entity)
-          applyParentRef(key.getConnectionId(), entity)
-          parentKeysToRemove.add(key)
-        }
-      }
-      for (key in parentKeysToRemove) {
-        extReferences.remove(key)
-      }
-      */
-      checkInitialization() // TODO uncomment and check failed tests
-    }
-
-    fun checkInitialization() {
-      val _diff = diff
-    }
-
-
-    override fun connectionIdList(): List<ConnectionId> {
-      TODO("Not yet implemented")
-    }
-
-    override fun getEntityClass(): Class<SuperSimpleEntity> = SuperSimpleEntity::class.java
-    override var entitySource: EntitySource
-      get() = TODO("Not yet implemented")
-      set(value) {}
-  }
-
-  // TODO: Fill with the data from the current entity
-  override fun connectionIdList(): List<ConnectionId> {
-    TODO("Not yet implemented")
-  }
-
-  override val entitySource: EntitySource
-    get() = TODO("Not yet implemented")
-}
-
-class SuperSimpleEntityData : WorkspaceEntityData<SuperSimpleEntity>() {
-
-  override fun getMetadata(): EntityMetadata {
-    TODO("Not yet implemented")
-  }
-
-  override fun wrapAsModifiable(diff: MutableEntityStorage): WorkspaceEntity.Builder<SuperSimpleEntity> {
-    val modifiable = SuperSimpleEntityImpl.Builder(null)
-    modifiable.allowModifications {
-      modifiable.diff = diff
-      modifiable.snapshot = diff
-      modifiable.id = createEntityId()
-      modifiable.entitySource = this.entitySource
-    }
-    return modifiable
-  }
-
-  override fun getEntityInterface(): Class<out WorkspaceEntity> {
-    return SuperSimpleEntity::class.java
-  }
-
-  @OptIn(EntityStorageInstrumentationApi::class)
-  override fun createEntity(snapshot: EntityStorageInstrumentation): SuperSimpleEntity {
-    val entity = SuperSimpleEntityImpl()
-    entity.snapshot = snapshot
-    entity.id = createEntityId()
-    return entity
-  }
-
-  override fun equals(other: Any?): Boolean {
-    if (other == null) return false
-    if (this::class != other::class) return false
-
-    other as SuperSimpleEntityData
-
-    return true
-  }
-
-  override fun equalsIgnoringEntitySource(other: Any?): Boolean {
-    if (other == null) return false
-    if (this::class != other::class) return false
-
-    other as SuperSimpleEntityData
-
-    return true
-  }
-
-  override fun hashCode(): Int {
-    var result = entitySource.hashCode()
-    return result
-  }
-
-  override fun serialize(ser: EntityInformation.Serializer) {
-    TODO("Not yet implemented")
-  }
-
-  override fun deserialize(de: EntityInformation.Deserializer) {
-    TODO("Not yet implemented")
   }
 }
