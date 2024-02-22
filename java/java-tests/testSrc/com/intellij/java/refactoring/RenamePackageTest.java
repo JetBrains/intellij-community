def59b0e8747370ab08743088297f96d77a03b91
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-02-22 12:53:58 +0100

    [java] convert RenamePackageTest to Java (IDEA-331682)
    GitOrigin-RevId: e16390c30115bdce0e77af63f19ae161cab4da9c

--- java/java-tests/testSrc/com/intellij/java/refactoring/RenamePackageTest.java
+++ java/java-tests/testSrc/com/intellij/java/refactoring/RenamePackageTest.java
@@ -1,61 +1,65 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.java.refactoring
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.java.refactoring;
 
-import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.roots.ModuleRootManager
-import com.intellij.openapi.vfs.VfsUtilCore
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.testFramework.PsiTestUtil
-import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase
-import org.jetbrains.jps.model.java.JavaSourceRootType
-import org.jetbrains.jps.model.java.JpsJavaExtensionService
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.vfs.VfsUtilCore;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.testFramework.PsiTestUtil;
+import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase;
+import org.jetbrains.jps.model.java.JavaSourceRootType;
+import org.jetbrains.jps.model.java.JpsJavaExtensionService;
 
-class RenamePackageTest extends JavaCodeInsightFixtureTestCase {
-  void "test rename in package prefix"() {
-    VirtualFile a = myFixture.addFileToProject("srcPrefix/foo/A.java", "package p.foo; class A extends B {}").virtualFile
-    VirtualFile b = myFixture.addFileToProject("src/p/foo/B.java", "package p.foo; class B { }").virtualFile
-    VirtualFile c = myFixture.addFileToProject("src/bar/C.java", "package bar; class C { p.foo.A a; p.foo.B b; }").virtualFile
+import java.io.IOException;
 
-    PsiTestUtil.removeSourceRoot(module, ModuleRootManager.getInstance(module).sourceRoots[0])
-    PsiTestUtil.addSourceRoot(module, myFixture.tempDirFixture.getFile('src'))
-    PsiTestUtil.addSourceRoot(module, myFixture.tempDirFixture.getFile('srcPrefix'), JavaSourceRootType.SOURCE,
-                              JpsJavaExtensionService.instance.createSourceRootProperties("p"))
+public class RenamePackageTest extends JavaCodeInsightFixtureTestCase {
+  public void testRenameInPackagePrefix() throws IOException {
+    VirtualFile a = myFixture.addFileToProject("srcPrefix/foo/A.java", "package p.foo; class A extends B {}").getVirtualFile();
+    VirtualFile b = myFixture.addFileToProject("src/p/foo/B.java", "package p.foo; class B { }").getVirtualFile();
+    VirtualFile c = myFixture.addFileToProject("src/bar/C.java", "package bar; class C { p.foo.A a; p.foo.B b; }").getVirtualFile();
 
-    myFixture.renameElement(myFixture.findPackage('p'), 'p1')
-    FileDocumentManager.instance.saveAllDocuments()
+    PsiTestUtil.removeSourceRoot(getModule(), ModuleRootManager.getInstance(getModule()).getSourceRoots()[0]);
+    PsiTestUtil.addSourceRoot(getModule(), myFixture.getTempDirFixture().getFile("src"));
+    PsiTestUtil.addSourceRoot(getModule(), myFixture.getTempDirFixture().getFile("srcPrefix"), JavaSourceRootType.SOURCE,
+                              JpsJavaExtensionService.getInstance().createSourceRootProperties("p"));
 
-    assert a.path.endsWith('srcPrefix/foo/A.java')
-    assert b.path.endsWith('src/p1/foo/B.java')
-    assert c.path.endsWith('src/bar/C.java')
+    myFixture.renameElement(myFixture.findPackage("p"), "p1");
+    FileDocumentManager.getInstance().saveAllDocuments();
 
-    assert VfsUtilCore.loadText(a) == "package p1.foo; class A extends B {}"
-    assert VfsUtilCore.loadText(b) == "package p1.foo; class B { }"
-    assert VfsUtilCore.loadText(c) == "package bar; class C { p1.foo.A a; p1.foo.B b; }"
+    assertTrue(a.getPath().endsWith("srcPrefix/foo/A.java"));
+    assertTrue(b.getPath().endsWith("src/p1/foo/B.java"));
+    assertTrue(c.getPath().endsWith("src/bar/C.java"));
 
-    assert myFixture.javaFacade.findPackage('p') == null
-    assert myFixture.javaFacade.findPackage('p1').directories.size() == 2
+    assertEquals("package p1.foo; class A extends B {}", VfsUtilCore.loadText(a));
+    assertEquals("package p1.foo; class B { }", VfsUtilCore.loadText(b));
+    assertEquals("package bar; class C { p1.foo.A a; p1.foo.B b; }", VfsUtilCore.loadText(c));
+
+    assertNull(myFixture.getJavaFacade().findPackage("p"));
+    assertEquals(2, myFixture.getJavaFacade().findPackage("p1").getDirectories().length);
   }
 
-  void "test rename in resources"() {
-    myFixture.addFileToProject("src/p/p/m.txt", "").virtualFile
-    VirtualFile c = myFixture.addFileToProject("src/bar/C.java", """package bar; 
-class C { 
-  {
-     C.class.getResource("/p/p/m.txt");
-  } 
-}""").virtualFile
+  public void testRenameInResources() throws IOException {
+    myFixture.addFileToProject("src/p/p/m.txt", "").getVirtualFile();
+    VirtualFile c = myFixture.addFileToProject("src/bar/C.java", """
+      package bar;\s
+      class C {\s
+        {
+           C.class.getResource("/p/p/m.txt");
+        }\s
+      }""").getVirtualFile();
 
-    PsiTestUtil.removeSourceRoot(module, ModuleRootManager.getInstance(module).sourceRoots[0])
-    PsiTestUtil.addSourceRoot(module, myFixture.tempDirFixture.getFile('src'))
+    PsiTestUtil.removeSourceRoot(getModule(), ModuleRootManager.getInstance(getModule()).getSourceRoots()[0]);
+    PsiTestUtil.addSourceRoot(getModule(), myFixture.getTempDirFixture().getFile("src"));
 
-    myFixture.renameElement(myFixture.findPackage('p.p'), 'p.p1')
-    FileDocumentManager.instance.saveAllDocuments()
+    myFixture.renameElement(myFixture.findPackage("p.p"), "p.p1");
+    FileDocumentManager.getInstance().saveAllDocuments();
 
-    assert VfsUtilCore.loadText(c) == """package bar; 
-class C { 
-  {
-     C.class.getResource("/p/p1/m.txt");
-  } 
-}"""
+    assertEquals("""
+                   package bar;\s
+                   class C {\s
+                     {
+                        C.class.getResource("/p/p1/m.txt");
+                     }\s
+                   }""", VfsUtilCore.loadText(c));
   }
 }
