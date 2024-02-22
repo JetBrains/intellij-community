fb27b2d2c23dadc99e10124360061301336055e4
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2024-02-22 10:43:00 +0100

    Convert CheckBoxWithDescription to Kotlin UI DSL
    GitOrigin-RevId: 7a682687114b304318c78bf447fc480e0fdcd036

--- platform/util/ui/src/com/intellij/openapi/ui/CheckBoxWithDescription.kt
+++ platform/util/ui/src/com/intellij/openapi/ui/CheckBoxWithDescription.kt
@@ -1,36 +1,24 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.ui;
+package com.intellij.openapi.ui
 
-import org.jetbrains.annotations.Nullable;
+import java.awt.BorderLayout
+import javax.swing.JCheckBox
+import javax.swing.JPanel
+import javax.swing.UIManager
+import javax.swing.border.EmptyBorder
 
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import java.awt.*;
 
-/**
- * @deprecated Use Kotlin UI DSL 2, for description use {@link com.intellij.ui.dsl.builder.Cell#comment}
- */
-@Deprecated(forRemoval = true)
-public final class CheckBoxWithDescription extends JPanel {
-
-  private final JCheckBox myCheckBox;
-
-  public CheckBoxWithDescription(JCheckBox box, @Nullable String description) {
-    myCheckBox = box;
-
-    setLayout(new BorderLayout());
-    add(myCheckBox, BorderLayout.NORTH);
+@Deprecated("Use Kotlin UI DSL 2, for description use {@link com.intellij.ui.dsl.builder.Cell#comment}")
+class CheckBoxWithDescription(val checkBox: JCheckBox, description: String?) : JPanel() {
+  init {
+    layout = BorderLayout()
+    add(checkBox, BorderLayout.NORTH)
 
     if (description != null) {
-      final int iconSize = box.getPreferredSize().height;
-
-      final DescriptionLabel desc = new DescriptionLabel(description);
-      desc.setBorder(new EmptyBorder(0, iconSize + UIManager.getInt("CheckBox.textIconGap"), 0, 0));
-      add(desc, BorderLayout.CENTER);
+      val iconSize = checkBox.preferredSize.height
+      val desc = DescriptionLabel(description)
+      desc.border = EmptyBorder(0, iconSize + UIManager.getInt("CheckBox.textIconGap"), 0, 0)
+      add(desc, BorderLayout.CENTER)
     }
-  }
-
-  public JCheckBox getCheckBox() {
-    return myCheckBox;
   }
 }
