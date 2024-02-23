e045f327b2a8e93d32e0d73125b0ca068141fe7f
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-02-23 11:04:45 +0100

    [java] convert LightResolveClassTest to Java (IDEA-331682)
    GitOrigin-RevId: e256d25711f5800560e0045fb897ecc7ef987800

--- java/java-tests/testSrc/com/intellij/java/psi/resolve/LightResolveClassTest.java
+++ java/java-tests/testSrc/com/intellij/java/psi/resolve/LightResolveClassTest.java
@@ -1,30 +1,30 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.java.psi.resolve
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.java.psi.resolve;
 
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.impl.source.PsiFileImpl;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
 
-import com.intellij.psi.impl.source.PsiFileImpl
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
-import groovy.transform.CompileStatic
+public class LightResolveClassTest extends LightJavaCodeInsightFixtureTestCase {
+  public void testNoLoadingForStarImportedClassWhenNamedImportMatches() {
+    PsiFileImpl unnamedFile =
+      DefaultGroovyMethods.asType(myFixture.addFileToProject("unnamed/Bar.java", "package unnamed; public class Bar {}"),
+                                  PsiFileImpl.class);
+    PsiClass named = myFixture.addClass("package named; public class Bar {}");
+    PsiClass foo = myFixture.addClass("""
+                                        import unnamed.*;
+                                        import named.Bar;
+                                        class Foo extends Bar {}
+                                        """);
 
-@CompileStatic
-class LightResolveClassTest extends LightJavaCodeInsightFixtureTestCase {
-
-  void "test no loading for star imported class when named import matches"() {
-    def unnamedFile = myFixture.addFileToProject('unnamed/Bar.java', 'package unnamed; public class Bar {}') as PsiFileImpl
-    def named = myFixture.addClass 'package named; public class Bar {}'
-    def foo = myFixture.addClass '''
-import unnamed.*;
-import named.Bar;
-class Foo extends Bar {}
-'''
-    
-    assertContentsNotLoaded(unnamedFile)
-    assert named == foo.superClass
-    assertContentsNotLoaded(unnamedFile)
+    assertContentsNotLoaded(unnamedFile);
+    assertEquals(named, foo.getSuperClass());
+    assertContentsNotLoaded(unnamedFile);
   }
 
   private static void assertContentsNotLoaded(PsiFileImpl unnamedFile) {
-    assert unnamedFile.derefStub() == null
-    assert unnamedFile.treeElement == null
+    assertNull(unnamedFile.derefStub());
+    assertNull(unnamedFile.getTreeElement());
   }
 }
