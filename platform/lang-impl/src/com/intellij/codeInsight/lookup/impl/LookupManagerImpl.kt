46bfcecd5dd308d318c7a4990f4c6ef4586880b8
Artem.Bukhonov <artem.bukhonov@jetbrains.com>
2024-02-23 23:00:22 +0100

    RDCT-1100 Refactor LookupManagerImpl to make it per-client in the platform
    Convert LookupManagerImpl.java to Kotlin
    
    GitOrigin-RevId: b1fd8f12fe545189f00e554353b4fb77978fc892

--- platform/lang-impl/src/com/intellij/codeInsight/lookup/impl/LookupManagerImpl.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/lookup/impl/LookupManagerImpl.kt
@@ -1,220 +1,198 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.codeInsight.lookup.impl;
+package com.intellij.codeInsight.lookup.impl
 
-import com.intellij.codeInsight.completion.impl.CamelHumpMatcher;
-import com.intellij.codeInsight.hint.EditorHintListener;
-import com.intellij.codeInsight.hint.HintManager;
-import com.intellij.codeInsight.lookup.*;
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.EditorFactory;
-import com.intellij.openapi.editor.event.EditorFactoryEvent;
-import com.intellij.openapi.editor.event.EditorFactoryListener;
-import com.intellij.openapi.project.DumbService;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.util.Key;
-import com.intellij.ui.HintHint;
-import com.intellij.ui.LightweightHint;
-import com.intellij.util.BitUtil;
-import com.intellij.util.concurrency.ThreadingAssertions;
-import com.intellij.util.messages.MessageBusConnection;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.TestOnly;
+import com.intellij.codeInsight.completion.impl.CamelHumpMatcher
+import com.intellij.codeInsight.hint.EditorHintListener
+import com.intellij.codeInsight.hint.HintManager
+import com.intellij.codeInsight.lookup.*
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.editor.event.EditorFactoryEvent
+import com.intellij.openapi.editor.event.EditorFactoryListener
+import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.util.Key
+import com.intellij.ui.HintHint
+import com.intellij.ui.LightweightHint
+import com.intellij.util.BitUtil
+import com.intellij.util.concurrency.ThreadingAssertions
+import org.jetbrains.annotations.TestOnly
+import java.beans.PropertyChangeListener
+import java.beans.PropertyChangeSupport
 
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
+open class LookupManagerImpl(private val myProject: Project) : LookupManager() {
+  private var myActiveLookup: LookupImpl? = null
+  private var myActiveLookupEditor: Editor? = null
 
-public class LookupManagerImpl extends LookupManager {
-  private static final Logger LOG = Logger.getInstance(LookupManagerImpl.class);
-  private final Project myProject;
-  private LookupImpl myActiveLookup = null;
-  private Editor myActiveLookupEditor = null;
-  private final PropertyChangeSupport myPropertyChangeSupport = new PropertyChangeSupport(this);
+  @Deprecated("Deprecated as the methods using the field")
+  private val myPropertyChangeSupport = PropertyChangeSupport(this)
 
-  public static final Key<Boolean> SUPPRESS_AUTOPOPUP_JAVADOC = Key.create("LookupManagerImpl.suppressAutopopupJavadoc");
-
-  public LookupManagerImpl(@NotNull Project project) {
-    myProject = project;
-
-    MessageBusConnection connection = project.getMessageBus().connect();
-    connection.subscribe(EditorHintListener.TOPIC, new EditorHintListener() {
-      @Override
-      public void hintShown(@NotNull Editor editor, @NotNull LightweightHint hint, int flags, @NotNull HintHint hintInfo) {
-        if (editor.getProject() == myProject) {
-          Lookup lookup = getActiveLookup();
+  init {
+    val connection = myProject.messageBus.connect()
+    connection.subscribe(EditorHintListener.TOPIC, object : EditorHintListener {
+      override fun hintShown(editor: Editor, hint: LightweightHint, flags: Int, hintInfo: HintHint) {
+        if (editor.project === myProject) {
+          val lookup: Lookup? = activeLookup
           if (lookup != null && BitUtil.isSet(flags, HintManager.HIDE_BY_LOOKUP_ITEM_CHANGE)) {
-            lookup.addLookupListener(new LookupListener() {
-              @Override
-              public void currentItemChanged(@NotNull LookupEvent event) {
-                hint.hide();
+            lookup.addLookupListener(object : LookupListener {
+              override fun currentItemChanged(event: LookupEvent) {
+                hint.hide()
               }
 
-              @Override
-              public void itemSelected(@NotNull LookupEvent event) {
-                hint.hide();
+              override fun itemSelected(event: LookupEvent) {
+                hint.hide()
               }
 
-              @Override
-              public void lookupCanceled(@NotNull LookupEvent event) {
-                hint.hide();
+              override fun lookupCanceled(event: LookupEvent) {
+                hint.hide()
               }
-            });
+            })
           }
         }
       }
-    });
+    })
 
-    connection.subscribe(DumbService.DUMB_MODE, new DumbService.DumbModeListener() {
-      @Override
-      public void enteredDumbMode() {
-        hideActiveLookup();
+    connection.subscribe(DumbService.DUMB_MODE, object : DumbService.DumbModeListener {
+      override fun enteredDumbMode() {
+        hideActiveLookup()
       }
 
-      @Override
-      public void exitDumbMode() {
-        hideActiveLookup();
+      override fun exitDumbMode() {
+        hideActiveLookup()
       }
-    });
+    })
 
 
-    EditorFactory.getInstance().addEditorFactoryListener(new EditorFactoryListener() {
-      @Override
-      public void editorReleased(@NotNull EditorFactoryEvent event) {
-        if (event.getEditor() == myActiveLookupEditor) {
-          hideActiveLookup();
+    EditorFactory.getInstance().addEditorFactoryListener(object : EditorFactoryListener {
+      override fun editorReleased(event: EditorFactoryEvent) {
+        if (event.editor === myActiveLookupEditor) {
+          hideActiveLookup()
         }
       }
-    }, myProject);
+    }, myProject)
   }
 
-  @Override
-  @Nullable
-  public LookupEx showLookup(@NotNull final Editor editor,
-                           LookupElement @NotNull [] items,
-                           @NotNull final String prefix,
-                           @NotNull final LookupArranger arranger) {
-    for (LookupElement item : items) {
-      assert item != null;
+  override fun showLookup(editor: Editor,
+                          items: Array<LookupElement>,
+                          prefix: String,
+                          arranger: LookupArranger): LookupEx? {
+    for (item in items) {
+      checkNotNull(item)
     }
 
-    LookupImpl lookup = createLookup(editor, items, prefix, arranger);
-    return lookup.showLookup() ? lookup : null;
+    val lookup = createLookup(editor, items, prefix, arranger)
+    return if (lookup.showLookup()) lookup else null
   }
 
-  @NotNull
-  @Override
-  public LookupImpl createLookup(@NotNull final Editor editor,
-                                 LookupElement @NotNull [] items,
-                                 @NotNull final String prefix,
-                                 @NotNull final LookupArranger arranger) {
-    hideActiveLookup();
+  override fun createLookup(editor: Editor,
+                            items: Array<LookupElement>,
+                            prefix: String,
+                            arranger: LookupArranger): LookupImpl {
+    hideActiveLookup()
 
-    final LookupImpl lookup = createLookup(editor, arranger, myProject);
+    val lookup = createLookup(editor, arranger, myProject)
 
-    ThreadingAssertions.assertEventDispatchThread();
+    ThreadingAssertions.assertEventDispatchThread()
 
-    myActiveLookup = lookup;
-    myActiveLookupEditor = editor;
-    Disposer.register(lookup, new Disposable() {
-      @Override
-      public void dispose() {
-        myActiveLookup = null;
-        myActiveLookupEditor = null;
-        fireActiveLookupChanged(lookup, null);
-      }
-    });
-
-    if (items.length > 0) {
-      CamelHumpMatcher matcher = new CamelHumpMatcher(prefix);
-      for (LookupElement item : items) {
-        myActiveLookup.addItem(item, matcher);
-      }
-      myActiveLookup.refreshUi(true, true);
+    myActiveLookup = lookup
+    myActiveLookupEditor = editor
+    Disposer.register(lookup) {
+      myActiveLookup = null
+      myActiveLookupEditor = null
+      fireActiveLookupChanged(lookup, null)
     }
 
-    fireActiveLookupChanged(null, myActiveLookup);
-    return lookup;
+    if (items.size > 0) {
+      val matcher = CamelHumpMatcher(prefix)
+      for (item in items) {
+        myActiveLookup!!.addItem(item, matcher)
+      }
+      myActiveLookup!!.refreshUi(true, true)
+    }
+
+    fireActiveLookupChanged(null, myActiveLookup)
+    return lookup
   }
 
-  void fireActiveLookupChanged(LookupImpl oldLookup, LookupImpl newLookup) {
-    myPropertyChangeSupport.firePropertyChange(PROP_ACTIVE_LOOKUP, oldLookup, newLookup);
-    myProject.getMessageBus().syncPublisher(LookupManagerListener.TOPIC).activeLookupChanged(oldLookup, newLookup);
+  fun fireActiveLookupChanged(oldLookup: LookupImpl?, newLookup: LookupImpl?) {
+    myPropertyChangeSupport.firePropertyChange(PROP_ACTIVE_LOOKUP, oldLookup, newLookup)
+    myProject.messageBus.syncPublisher(LookupManagerListener.TOPIC).activeLookupChanged(oldLookup, newLookup)
   }
 
-  public static boolean isAutoPopupJavadocSupportedBy(@NotNull LookupElement lookupElement) {
-    return lookupElement.getUserData(SUPPRESS_AUTOPOPUP_JAVADOC) == null;
+  protected open fun createLookup(editor: Editor, arranger: LookupArranger, project: Project): LookupImpl {
+    return LookupImpl(project, editor, arranger)
   }
 
-  @NotNull
-  protected LookupImpl createLookup(@NotNull Editor editor, @NotNull LookupArranger arranger, Project project) {
-    return new LookupImpl(project, editor, arranger);
-  }
-
-  @Override
-  public void hideActiveLookup() {
-    LookupImpl lookup = myActiveLookup;
+  override fun hideActiveLookup() {
+    val lookup = myActiveLookup
     if (lookup != null) {
-      lookup.checkValid();
-      lookup.hide();
-      LOG.assertTrue(lookup.isLookupDisposed(), "Should be disposed");
+      lookup.checkValid()
+      lookup.hide()
+      LOG.assertTrue(lookup.isLookupDisposed, "Should be disposed")
     }
   }
 
-  @Override
-  public LookupEx getActiveLookup() {
-    if (myActiveLookup != null && myActiveLookup.isLookupDisposed()) {
-      LookupImpl lookup = myActiveLookup;
-      myActiveLookup = null;
-      lookup.checkValid();
+  override fun getActiveLookup(): LookupEx? {
+    val activeLookup = myActiveLookup
+    if (activeLookup != null && activeLookup.isLookupDisposed) {
+      val lookup: LookupImpl = activeLookup
+      myActiveLookup = null
+      lookup.checkValid()
     }
 
-    return myActiveLookup;
+    return activeLookup
   }
 
-  @Override
-  public void addPropertyChangeListener(@NotNull PropertyChangeListener listener) {
-    myPropertyChangeSupport.addPropertyChangeListener(listener);
+  override fun addPropertyChangeListener(listener: PropertyChangeListener) {
+    myPropertyChangeSupport.addPropertyChangeListener(listener)
   }
 
-  @Override
-  public void addPropertyChangeListener(@NotNull final PropertyChangeListener listener, @NotNull Disposable disposable) {
-    addPropertyChangeListener(listener);
-    Disposer.register(disposable, new Disposable() {
-      @Override
-      public void dispose() {
-        removePropertyChangeListener(listener);
-      }
-    });
+  override fun addPropertyChangeListener(listener: PropertyChangeListener, disposable: Disposable) {
+    addPropertyChangeListener(listener)
+    Disposer.register(disposable) { removePropertyChangeListener(listener) }
   }
 
-  @Override
-  public void removePropertyChangeListener(@NotNull PropertyChangeListener listener) {
-    myPropertyChangeSupport.removePropertyChangeListener(listener);
+  override fun removePropertyChangeListener(listener: PropertyChangeListener) {
+    myPropertyChangeSupport.removePropertyChangeListener(listener)
   }
 
 
   @TestOnly
-  public void forceSelection(char completion, int index){
-    if(myActiveLookup == null) throw new RuntimeException("There are no items in this lookup");
-    final LookupElement lookupItem = myActiveLookup.getItems().get(index);
-    myActiveLookup.setCurrentItem(lookupItem);
-    myActiveLookup.finishLookup(completion);
+  fun forceSelection(completion: Char, index: Int) {
+    if (myActiveLookup == null) throw RuntimeException("There are no items in this lookup")
+    val lookupItem = myActiveLookup!!.items[index]
+    myActiveLookup!!.currentItem = lookupItem
+    myActiveLookup!!.finishLookup(completion)
   }
 
   @TestOnly
-  public void forceSelection(char completion, LookupElement item){
-    myActiveLookup.setCurrentItem(item);
-    myActiveLookup.finishLookup(completion);
+  fun forceSelection(completion: Char, item: LookupElement?) {
+    myActiveLookup!!.currentItem = item
+    myActiveLookup!!.finishLookup(completion)
   }
 
   @TestOnly
-  public void clearLookup() {
+  fun clearLookup() {
     if (myActiveLookup != null) {
-      myActiveLookup.hide();
-      myActiveLookup = null;
+      myActiveLookup!!.hide()
+      myActiveLookup = null
     }
+  }
+
+  companion object {
+    private val LOG = Logger.getInstance(LookupManagerImpl::class.java)
+    val SUPPRESS_AUTOPOPUP_JAVADOC: Key<Boolean> = Key.create("LookupManagerImpl.suppressAutopopupJavadoc")
+
+    @JvmStatic
+    fun isAutoPopupJavadocSupportedBy(lookupElement: LookupElement): Boolean {
+      return lookupElement.getUserData(SUPPRESS_AUTOPOPUP_JAVADOC) == null
+    }
+
+    fun getActiveLookup(editor: Editor?): LookupEx? = LookupManager.getActiveLookup(editor)
+
+    fun getInstance(project: Project): LookupManager = LookupManager.getInstance(project)
   }
 }
