c5636b46a6bd250edd8ff8a530b2385753651bc7
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2024-02-23 15:15:15 +0100

    [file-history] remove the registry key to disable new file history without the indexes
    Also remove the notification for the old file history which suggested enabling indexing.
    
    GitOrigin-RevId: f145758325d3544f630f1670ecd3a30b8d819201

--- plugins/git4idea/src/git4idea/history/GitHistoryProvider.java
+++ plugins/git4idea/src/git4idea/history/GitHistoryProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.history;
 
 import com.intellij.openapi.actionSystem.ActionManager;
@@ -53,8 +53,7 @@ public final class GitHistoryProvider implements VcsHistoryProviderEx,
 
   @Override
   public VcsDependentHistoryComponents getUICustomization(final VcsHistorySession session, JComponent forShortcutRegistration) {
-    return new VcsDependentHistoryComponents(ColumnInfo.EMPTY_ARRAY, null, null,
-                                             GitHistoryNotificationPanel.create(myProject, session));
+    return VcsDependentHistoryComponents.createOnlyColumns(ColumnInfo.EMPTY_ARRAY);
   }
 
   @Override
