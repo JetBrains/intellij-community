da18854f9e1ed75202e35223b35fad7216f0facb
vladislav.shishov <vladislav.shishov@jetbrains.com>
2024-02-23 15:42:23 +0200

    [remote-driver] add LicenseKeyActivationUiTest
    GitOrigin-RevId: a91cfa44b092385068217047ead8f79643a5ead6

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/Locators.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/Locators.kt
@@ -4,7 +4,11 @@ import java.awt.Component
 
 object Locators {
   fun byAccessibleName(name: String): String = "//div[@accessiblename='$name']"
-  fun byClass(cls: String): String = "//div[@class='$cls']"
+  fun byTitle(title: String): String = byAttribute("@title", title)
+  fun byTitleContains(title: String): String = byAttributeContains("@title", title)
+  fun byClass(cls: String): String = byAttribute("@class", cls)
   fun <T : Component> byType(type: Class<T>) = byType(type.name)
   fun byType(type: String) = """//div[@javaclass="$type" or contains(@classhierarchy, "$type") or contains(@classhierarchy, " $type ")]"""
+  fun byAttribute(name: String, value: String) = "//div[$name='$value']"
+  fun byAttributeContains(name: String, value: String) = "//div[contains($name,'$value')]"
 }
