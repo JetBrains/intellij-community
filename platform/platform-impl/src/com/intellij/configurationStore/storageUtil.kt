3cca387693670af630ada35e5c899d54335fb436
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-02-23 10:54:38 +0100

    Cleanup (a clarifying comment for IJ-CR-126711)
    (cherry picked from commit 7e95d5896a3596ad49f54be433fe50eac77a9e05)
    
    IJ-CR-126711
    
    GitOrigin-RevId: aeab2dbc49fc7b6d584341978bfa638787ccaaf5

--- platform/platform-impl/src/com/intellij/configurationStore/storageUtil.kt
+++ platform/platform-impl/src/com/intellij/configurationStore/storageUtil.kt
@@ -184,6 +184,7 @@ class UnknownMacroNotification(
   }
 }
 
+/** Used in constructed configuration store events to trigger VFS content reloading for files updated via NIO. */
 @ApiStatus.Internal
 @JvmField
 val RELOADING_STORAGE_WRITE_REQUESTOR = object : StorageManagerFileWriteRequestor { }
