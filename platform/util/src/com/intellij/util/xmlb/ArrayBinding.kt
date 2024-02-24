b6e13171d5123ffd8704aa0578c8a1eaabfece4d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-02-24 10:31:57 +0100

    IJPL-136 convert ArrayBinding to modern language
    (cherry picked from commit c07e7c578ea5b773e6f8f555372fcc486671016a)
    
    IJ-CR-126571
    
    GitOrigin-RevId: ebe395a8d77a81f47e8dab97eceffc8251b10b64

--- platform/util/src/com/intellij/util/xmlb/ArrayBinding.kt
+++ platform/util/src/com/intellij/util/xmlb/ArrayBinding.kt
@@ -1,53 +1,38 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.util.xmlb;
+package com.intellij.util.xmlb
 
-import com.intellij.serialization.MutableAccessor;
-import com.intellij.util.ArrayUtil;
-import com.intellij.util.xml.dom.XmlElement;
-import org.jdom.Element;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.serialization.MutableAccessor
+import com.intellij.util.ArrayUtil
+import com.intellij.util.xml.dom.XmlElement
+import org.jdom.Element
+import java.util.*
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
-final class ArrayBinding extends AbstractCollectionBinding  {
-  ArrayBinding(@NotNull Class<?> valueClass, @Nullable MutableAccessor accessor, @NotNull Serializer serializer) {
-    super(valueClass.getComponentType(), accessor, serializer);
+internal class ArrayBinding(valueClass: Class<*>, accessor: MutableAccessor?, serializer: Serializer) : AbstractCollectionBinding(
+  valueClass.componentType, accessor, serializer) {
+  override fun getCollectionTagName(target: Any?): String {
+    return "array"
   }
 
-  @Override
-  protected @NotNull String getCollectionTagName(@Nullable Object target) {
-    return "array";
-  }
-
-  @Override
-  protected @NotNull Object doDeserializeJdomList(@Nullable Object context, @NotNull List<? extends Element> elements) {
-    int size = elements.size();
-    Object[] result = ArrayUtil.newArray(itemType, size);
-    for (int i = 0; i < size; i++) {
-      result[i] = deserializeItem(elements.get(i), context);
+  override fun doDeserializeJdomList(context: Any?, elements: List<Element?>): Any {
+    val size = elements.size
+    val result: Array<Any> = ArrayUtil.newArray(itemType, size)
+    for (i in 0 until size) {
+      result[i] = deserializeItem(elements[i]!!, context)
     }
-    return result;
+    return result
   }
 
-  @Override
-  protected @NotNull Object doDeserializeList(@Nullable Object context, @NotNull List<XmlElement> elements) {
-    int size = elements.size();
-    Object[] result = (Object[])Array.newInstance(itemType, size);
-    for (int i = 0; i < size; i++) {
-      result[i] = deserializeItem(elements.get(i), context);
+  override fun doDeserializeList(context: Any?, elements: List<XmlElement>): Any {
+    val size = elements.size
+    val result = java.lang.reflect.Array.newInstance(itemType, size) as Array<Any>
+    for (i in 0 until size) {
+      result[i] = deserializeItem(elements[i], context)
     }
-    return result;
+    return result
   }
 
-  @NotNull
-  @Override
-  Collection<Object> getCollection(@NotNull Object bean) {
-    Object[] list = (Object[])bean;
-    return list.length == 0 ? Collections.emptyList() : Arrays.asList(list);
+  public override fun getCollection(bean: Any): Collection<Any?> {
+    val list = bean as Array<Any>
+    return if (list.size == 0) emptyList<Any>() else Arrays.asList(*list)
   }
 }
