694216178bb1d3292b166b7c9cf32f194482340a
Andrei Ogurtsov <andrei.ogurtsov@jetbrains.com>
2024-02-24 20:04:42 +0000

    [YAML] IDEA-344938, IDEA-344961 - Issue Fix
    Fix issue tickets:
    IDEA-344938 Yaml: on inline expanded form conversion values with commas are lost
    IDEA-344961 YAML: 'Inline Collection' intention treats the comments incorrectly
    
    
    Merge-request: IJ-MR-126980
    Merged-by: Andrei Ogurtsov <andrei.ogurtsov@jetbrains.com>
    
    GitOrigin-RevId: d9f84017f9177cc09a0aa453ab2115ba42f8b661

--- plugins/yaml/testSrc/org/jetbrains/yaml/formatting/inlineExpandConversation/YAMLInlineExpandTest.kt
+++ plugins/yaml/testSrc/org/jetbrains/yaml/formatting/inlineExpandConversation/YAMLInlineExpandTest.kt
@@ -41,7 +41,8 @@ class YAMLInlineExpandTest : BasePlatformTestCase() {
     myFixture.checkPreviewAndLaunchAction(expandAllAction)
     try {
       myFixture.checkResult(testCaseAnswer)
-    } finally {
+    }
+    finally {
       CodeStyle.getIndentOptions(myFixture.file).INDENT_SIZE = startIndent
     }
   }
@@ -1263,4 +1264,66 @@ class YAMLInlineExpandTest : BasePlatformTestCase() {
                         123: 19
     """.trimIndent()
   )
+
+  fun `test inline works with comments`() {
+    doInlineActionTest("""
+    root: # root comment
+      props<caret>: # props comment
+        aaa: foo # aaa comment
+        bbb: bar # bbb comment
+        ccc: # ccc comment
+          - v1 # array value 1
+          - v2
+          - v3
+    """.trimIndent(), """
+    root: # root comment
+      props: { aaa: foo, bbb: bar, ccc: [ v1, v2, v3 ] }
+    """.trimIndent())
+    doInlineActionTest("""
+    root<caret>: # root comment
+      props: # props comment
+        aaa: foo # aaa comment
+        bbb: bar # bbb comment
+        ccc: # ccc comment
+          - v1 # array value 1
+          - v2
+          - v3
+    """.trimIndent(), """
+    root: { props: { aaa: foo, bbb: bar, ccc: [ v1, v2, v3 ] } }
+    """.trimIndent())
+  }
+
+  fun `test inline works with split plain text`() {
+    doInlineActionTest("""
+    foo:
+       bar:  one, two, three
+    """.trimIndent(), """
+    foo: { bar: "one, two, three" }
+    """.trimIndent())
+    doInlineActionTest("""
+    foo:
+      bar: one, two, three
+      bar1: one,  two,  three
+      bar2: one,two,three
+    """.trimIndent(), """
+    foo: { bar: "one, two, three", bar1: "one,  two,  three", bar2: "one,two,three" }
+    """.trimIndent())
+    doInlineActionTest("""
+    foo<caret>:
+      with_space:
+        bar: one, two, three
+        bar1: 'one, two, three'
+        bar2: "one, two, three"
+      with_no_space:
+        bar3: one,two,three
+        bar4: 'one,two,three'
+        bar5: "one,two,three"
+      just_value:
+        baz: 345
+        baz1: '345'
+        baz2: "345"
+    """.trimIndent(), """
+    foo: { with_space: { bar: "one, two, three", bar1: 'one, two, three', bar2: "one, two, three" }, with_no_space: { bar3: "one,two,three", bar4: 'one,two,three', bar5: "one,two,three" }, just_value: { baz: 345, baz1: '345', baz2: "345" } }
+    """.trimIndent())
+  }
 }
