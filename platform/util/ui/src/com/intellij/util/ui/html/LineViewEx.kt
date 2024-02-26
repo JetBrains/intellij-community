74b1371dbf160a0b3dfc4cc2eec158475af04a1d
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-02-26 17:34:28 +0100

    Line/ParagraphViewEx: Update implementation
    GitOrigin-RevId: ff1440cfb2769c842274a7cdfdbbc5323ba32ee9

--- platform/util/ui/src/com/intellij/util/ui/html/LineViewEx.kt
+++ platform/util/ui/src/com/intellij/util/ui/html/LineViewEx.kt
@@ -1,33 +1,6 @@
-/*
- * Copyright (c) 1997, 2014, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
 package com.intellij.util.ui.html
 
-import java.awt.Toolkit
 import javax.swing.text.Element
-import javax.swing.text.StyleConstants
-import javax.swing.text.StyledDocument
 import javax.swing.text.html.CSS
 
 /**
@@ -35,137 +8,30 @@ import javax.swing.text.html.CSS
  */
 internal class LineViewEx(elem: Element) : ParagraphViewEx(elem) {
 
-  /** Last place painted at.  */
-  private var wrap = false
+  private var preWrap = false
 
-  /**
-   * Preformatted lines are not suppressed if they
-   * have only whitespace, so they are always visible.
-   */
-  override fun isVisible(): Boolean {
-    return true
-  }
+  override fun isVisible(): Boolean = true
 
-  /**
-   * Determines the minimum span for this view along an
-   * axis.  The preformatted line should refuse to be
-   * sized less than the preferred size.
-   *
-   * @param axis may be either `View.X_AXIS` or
-   * `View.Y_AXIS`
-   * @return  the minimum span the view can be rendered into
-   * @see View.getPreferredSpan
-   */
-  override fun getMinimumSpan(axis: Int): Float {
-    return if (wrap) super.getMinimumSpan(axis) else getPreferredSpan(axis)
-  }
+  override fun getMinimumSpan(axis: Int): Float =
+    if (preWrap) super.getMinimumSpan(axis) else getPreferredSpan(axis)
 
-  /**
-   * Gets the resize weight for the specified axis.
-   *
-   * @param axis may be either X_AXIS or Y_AXIS
-   * @return the weight
-   */
-  override fun getResizeWeight(axis: Int): Int {
-    return when (axis) {
+  override fun getResizeWeight(axis: Int): Int =
+    when (axis) {
       X_AXIS -> 1
       Y_AXIS -> 0
       else -> throw IllegalArgumentException("Invalid axis: $axis")
     }
-  }
 
-  /**
-   * Gets the alignment for an axis.
-   *
-   * @param axis may be either X_AXIS or Y_AXIS
-   * @return the alignment
-   */
-  override fun getAlignment(axis: Int): Float {
-    if (axis == X_AXIS) {
-      return 0f
-    }
-    return super.getAlignment(axis)
-  }
+  override fun getAlignment(axis: Int): Float =
+    if (axis == X_AXIS) 0f else super.getAlignment(axis)
 
-  /**
-   * Lays out the children.  If the layout span has changed,
-   * the rows are rebuilt.  The superclass functionality
-   * is called after checking and possibly rebuilding the
-   * rows.  If the height has changed, the
-   * `preferenceChanged` method is called
-   * on the parent since the vertical preference is
-   * rigid.
-   *
-   * @param width  the width to lay out against >= 0.  This is
-   * the width inside of the inset area.
-   * @param height the height to lay out against >= 0 (not used
-   * by paragraph, but used by the superclass).  This
-   * is the height inside of the inset area.
-   */
   override fun layout(width: Int, height: Int) {
-    super.layout(if (wrap) width else Int.MAX_VALUE - 1, height)
+    super.layout(if (preWrap) width else Int.MAX_VALUE - 1, height)
   }
-
-  /**
-   * Returns the next tab stop position given a reference position.
-   * This view implements the tab coordinate system, and calls
-   * `getTabbedSpan` on the logical children in the process
-   * of layout to determine the desired span of the children.  The
-   * logical children can delegate their tab expansion upward to
-   * the paragraph which knows how to expand tabs.
-   * `LabelView` is an example of a view that delegates
-   * its tab expansion needs upward to the paragraph.
-   *
-   *
-   * This is implemented to try and locate a `TabSet`
-   * in the paragraph element's attribute set.  If one can be
-   * found, its settings will be used, otherwise a default expansion
-   * will be provided.  The base location for tab expansion
-   * is the left inset from the paragraphs most recent allocation
-   * (which is what the layout of the children is based upon).
-   *
-   * @param x the X reference position
-   * @param tabOffset the position within the text stream
-   * that the tab occurred at >= 0.
-   * @return the trailing end of the tab expansion >= 0
-   */
-  override fun nextTabStop(x: Float, tabOffset: Int): Float {
-    // If the text isn't left justified, offset by 10 pixels!
-    if (tabSet == null &&
-        StyleConstants.getAlignment(attributes) ==
-        StyleConstants.ALIGN_LEFT
-    ) {
-      return getPreTab(x, tabOffset)
-    }
-    return super.nextTabStop(x, tabOffset)
-  }
-
-  /**
-   * Returns the location for the tab.
-   */
-  private fun getPreTab(x: Float, tabOffset: Int): Float {
-    val d = document
-    val v = getViewAtPosition(tabOffset, null)
-    if ((d is StyledDocument) && v != null) {
-      // Assume f is fixed point.
-      val f = d.getFont(v.attributes)
-      val c = container
-      val fm = if ((c != null)) c.getFontMetrics(f) else Toolkit.getDefaultToolkit().getFontMetrics(f)
-      val width = CHARS_PER_TAB * fm.charWidth('W')
-      val tb = tabBase.toInt()
-      return (((x.toInt() - tb) / width + 1) * width + tb).toFloat()
-    }
-    return 10.0f + x
-  }
-
 
   override fun setPropertiesFromAttributes() {
     super.setPropertiesFromAttributes()
-    val parent = parent
-    wrap = parent != null && parent.attributes.containsAttribute(CSS.Attribute.WHITE_SPACE, "pre-wrap")
+    preWrap = parent?.attributes?.containsAttribute(CSS.Attribute.WHITE_SPACE, "pre-wrap") == true
   }
 
-  companion object {
-    private const val CHARS_PER_TAB: Int = 8
-  }
 }
--- platform/util/ui/src/com/intellij/util/ui/html/ParagraphViewEx.kt
+++ platform/util/ui/src/com/intellij/util/ui/html/ParagraphViewEx.kt
@@ -4,33 +4,27 @@ package com.intellij.util.ui.html
 import com.intellij.util.asSafely
 import java.awt.Shape
 import java.awt.Toolkit
-import java.util.*
 import javax.swing.SizeRequirements
 import javax.swing.text.*
 import javax.swing.text.Position.Bias
 import javax.swing.text.html.CSS
 import javax.swing.text.html.ParagraphView
 import kotlin.math.max
-import kotlin.math.min
 
 /**
- * Supports line-height (%, px and no-unit) property in paragraphs.
+ * Supports line-height (%, px and no-unit) property in paragraphs. No support for justification.
  */
 open class ParagraphViewEx(elem: Element) : ParagraphView(elem) {
 
-  @Suppress("ProtectedInFinal")
   @JvmField
   protected var fixedLineHeight: Float? = null
 
-  @Suppress("ProtectedInFinal")
   @JvmField
   protected var scaledLineHeight: Float? = null
 
-  @Suppress("ProtectedInFinal")
   @JvmField
   protected var fontLineHeight: Int? = null
 
-  @Suppress("ProtectedInFinal")
   @JvmField
   protected var justification: Int = 0
 
@@ -70,27 +64,16 @@ open class ParagraphViewEx(elem: Element) : ParagraphView(elem) {
   }
 
   override fun createRow(): View {
-    return Row(element)
+    return ParagraphRow(element)
   }
 
-  inner class Row internal constructor(elem: Element) : BoxView(elem, X_AXIS) {
-    /**
-     * This is reimplemented to do nothing since the
-     * paragraph fills in the row with its needed
-     * children.
-     */
+  inner class ParagraphRow internal constructor(elem: Element) : BoxView(elem, X_AXIS) {
+
     override fun loadChildren(f: ViewFactory) {
     }
 
-    /**
-     * Fetches the attributes to use when rendering.  This view
-     * isn't directly responsible for an element so it returns
-     * the outer classes attributes.
-     */
-    override fun getAttributes(): AttributeSet? {
-      val p = parent
-      return if ((p != null)) p.attributes else null
-    }
+    override fun getAttributes(): AttributeSet? =
+      parent?.attributes
 
     override fun getAlignment(axis: Int): Float {
       if (axis == X_AXIS) {
@@ -98,41 +81,16 @@ open class ParagraphViewEx(elem: Element) : ParagraphView(elem) {
           StyleConstants.ALIGN_LEFT -> return 0f
           StyleConstants.ALIGN_RIGHT -> return 1f
           StyleConstants.ALIGN_CENTER -> return 0.5f
-          StyleConstants.ALIGN_JUSTIFIED -> {
-            var rv = 0.5f
-            //if we can justifiy the content always align to
-            //the left.
-            if (isJustifiableDocument) {
-              rv = 0f
-            }
-            return rv
-          }
+          StyleConstants.ALIGN_JUSTIFIED -> return 0f
         }
       }
       return super.getAlignment(axis)
     }
 
-    /**
-     * Provides a mapping from the document model coordinate space
-     * to the coordinate space of the view mapped to it.  This is
-     * implemented to let the superclass find the position along
-     * the major axis and the allocation of the row is used
-     * along the minor axis, so that even though the children
-     * are different heights they all get the same caret height.
-     *
-     * @param pos the position to convert
-     * @param a the allocated region to render into
-     * @return the bounding box of the given position
-     * @exception BadLocationException  if the given position does not represent a
-     * valid location in the associated document
-     * @see View.modelToView
-     */
-    @Throws(BadLocationException::class)
     override fun modelToView(pos: Int, a: Shape, b: Bias): Shape {
       var r = a.bounds
       val v = getViewAtPosition(pos, r)
       if ((v != null) && (!v.element.isLeaf)) {
-        // Don't adjust the height if the view represents a branch.
         return super.modelToView(pos, a, b)
       }
       r = a.bounds
@@ -144,246 +102,22 @@ open class ParagraphViewEx(elem: Element) : ParagraphView(elem) {
       return bounds
     }
 
-    /**
-     * Range represented by a row in the paragraph is only
-     * a subset of the total range of the paragraph element.
-     */
-    override fun getStartOffset(): Int {
-      var offs = Int.MAX_VALUE
-      val n = viewCount
-      for (i in 0 until n) {
-        val v = getView(i)
-        offs = min(offs.toDouble(), v.startOffset.toDouble()).toInt()
-      }
-      return offs
-    }
+    override fun getStartOffset(): Int =
+      (0 until viewCount).asSequence()
+        .map { getView(it) }.minOfOrNull { it.startOffset } ?: Int.MAX_VALUE
 
-    override fun getEndOffset(): Int {
-      var offs = 0
-      val n = viewCount
-      for (i in 0 until n) {
-        val v = getView(i)
-        offs = max(offs.toDouble(), v.endOffset.toDouble()).toInt()
-      }
-      return offs
-    }
+    override fun getEndOffset(): Int =
+      (0 until viewCount).asSequence()
+        .map { getView(it) }.maxOfOrNull { it.endOffset } ?: 0
 
-    /**
-     * Perform layout for the minor axis of the box (i.e. the
-     * axis orthogonal to the axis that it represents).  The results
-     * of the layout should be placed in the given arrays which represent
-     * the allocations to the children along the minor axis.
-     *
-     *
-     * This is implemented to do a baseline layout of the children
-     * by calling BoxView.baselineLayout.
-     *
-     * @param targetSpan the total span given to the view, which
-     * would be used to layout the children.
-     * @param axis the axis being layed out.
-     * @param offsets the offsets from the origin of the view for
-     * each of the child views.  This is a return value and is
-     * filled in by the implementation of this method.
-     * @param spans the span of each child view.  This is a return
-     * value and is filled in by the implementation of this method.
-     * @return the offset and span for each child view in the
-     * offsets and spans parameters
-     */
     override fun layoutMinorAxis(targetSpan: Int, axis: Int, offsets: IntArray, spans: IntArray) {
       baselineLayout(targetSpan, axis, offsets, spans)
     }
 
-    override fun calculateMinorAxisRequirements(axis: Int,
-                                                r: SizeRequirements?): SizeRequirements {
-      return baselineRequirements(axis, r)
-    }
+    override fun calculateMinorAxisRequirements(axis: Int, r: SizeRequirements?): SizeRequirements =
+      baselineRequirements(axis, r)
 
-
-    private val isLastRow: Boolean
-      get() {
-        var parent: View
-        return ((getParent().also { parent = it }) == null
-                || this === parent.getView(parent.viewCount - 1))
-      }
-
-    private val isBrokenRow: Boolean
-      get() {
-        var rv = false
-        val viewsCount = viewCount
-        if (viewsCount > 0) {
-          val lastView = getView(viewsCount - 1)
-          if (lastView.getBreakWeight(X_AXIS, 0f, 0f) >=
-              ForcedBreakWeight) {
-            rv = true
-          }
-        }
-        return rv
-      }
-
-    private val isJustifiableDocument: Boolean
-      get() = (true != document.getProperty("i18n"))
-
-    private val isJustifyEnabled: Boolean
-      /**
-       * Whether we need to justify this `Row`.
-       * At this time (jdk1.6) we support justification on for non
-       * 18n text.
-       *
-       * @return `true` if this `Row` should be justified.
-       */
-      get() {
-        var ret = (justification == StyleConstants.ALIGN_JUSTIFIED)
-
-        //no justification for i18n documents
-        ret = ret && isJustifiableDocument
-
-        //no justification for the last row
-        ret = ret && !isLastRow
-
-        //no justification for the broken rows
-        ret = ret && !isBrokenRow
-
-        return ret
-      }
-
-
-    //Calls super method after setting spaceAddon to 0.
-    //Justification should not affect MajorAxisRequirements
-    override fun calculateMajorAxisRequirements(axis: Int,
-                                                r: SizeRequirements?): SizeRequirements {
-      val oldJustficationData = justificationData
-      justificationData = null
-      val ret = super.calculateMajorAxisRequirements(axis, r)
-      if (isJustifyEnabled) {
-        justificationData = oldJustficationData
-      }
-      return ret
-    }
-
-    override fun layoutMajorAxis(targetSpan: Int, axis: Int,
-                                 offsets: IntArray, spans: IntArray) {
-      val oldJustficationData = justificationData
-      justificationData = null
-      super.layoutMajorAxis(targetSpan, axis, offsets, spans)
-      if (!isJustifyEnabled) {
-        return
-      }
-
-      var currentSpan = 0
-      for (span in spans) {
-        currentSpan += span
-      }
-      if (currentSpan == targetSpan) {
-        //no need to justify
-        return
-      }
-
-      // we justify text by enlarging spaces by the {@code spaceAddon}.
-      // justification is started to the right of the rightmost TAB.
-      // leading and trailing spaces are not extendable.
-      //
-      // GlyphPainter1 uses
-      // justificationData
-      // for all painting and measurement.
-      var extendableSpaces = 0
-      var startJustifiableContent = -1
-      var endJustifiableContent = -1
-      var lastLeadingSpaces = 0
-
-      val rowStartOffset = startOffset
-      val rowEndOffset = endOffset
-      val spaceMap = IntArray(rowEndOffset - rowStartOffset)
-      Arrays.fill(spaceMap, 0)
-      for (i in viewCount - 1 downTo 0) {
-        val view = getView(i)
-        if (view is GlyphView) {
-          val justificationInfo = view.getJustificationInfo(rowStartOffset)
-          val viewStartOffset = view.getStartOffset()
-          val offset = viewStartOffset - rowStartOffset
-          for (j in 0 until justificationInfo.spaceMap.length()) {
-            if (justificationInfo.spaceMap[j]) {
-              spaceMap[j + offset] = 1
-            }
-          }
-          if (startJustifiableContent > 0) {
-            if (justificationInfo.end >= 0) {
-              extendableSpaces += justificationInfo.trailingSpaces
-            }
-            else {
-              lastLeadingSpaces += justificationInfo.trailingSpaces
-            }
-          }
-          if (justificationInfo.start >= 0) {
-            startJustifiableContent =
-              justificationInfo.start + viewStartOffset
-            extendableSpaces += lastLeadingSpaces
-          }
-          if (justificationInfo.end >= 0
-              && endJustifiableContent < 0) {
-            endJustifiableContent =
-              justificationInfo.end + viewStartOffset
-          }
-          extendableSpaces += justificationInfo.contentSpaces
-          lastLeadingSpaces = justificationInfo.leadingSpaces
-          if (justificationInfo.hasTab) {
-            break
-          }
-        }
-      }
-      if (extendableSpaces <= 0) {
-        //there is nothing we can do to justify
-        return
-      }
-      val adjustment = targetSpan - currentSpan
-      val spaceAddon = adjustment / extendableSpaces
-      var spaceAddonLeftoverEnd = -1
-      var i = startJustifiableContent - rowStartOffset
-      var leftover = adjustment - spaceAddon * extendableSpaces
-      while (leftover > 0
-      ) {
-        spaceAddonLeftoverEnd = i
-        leftover -= spaceMap[i]
-        i++
-      }
-      if (spaceAddon > 0 || spaceAddonLeftoverEnd >= 0) {
-        justificationData = if ((oldJustficationData != null)
-        ) oldJustficationData
-        else IntArray(END_JUSTIFIABLE + 1)
-        justificationData!![SPACE_ADDON] = spaceAddon
-        justificationData!![SPACE_ADDON_LEFTOVER_END] =
-          spaceAddonLeftoverEnd
-        justificationData!![START_JUSTIFIABLE] =
-          startJustifiableContent - rowStartOffset
-        justificationData!![END_JUSTIFIABLE] =
-          endJustifiableContent - rowStartOffset
-        super.layoutMajorAxis(targetSpan, axis, offsets, spans)
-      }
-    }
-
-    //for justified row we assume the maximum horizontal span
-    //is MAX_VALUE.
-    override fun getMaximumSpan(axis: Int): Float {
-      val ret = if (X_AXIS == axis
-                    && isJustifyEnabled) {
-        Float.MAX_VALUE
-      }
-      else {
-        super.getMaximumSpan(axis)
-      }
-      return ret
-    }
-
-    /**
-     * Fetches the child view index representing the given position in
-     * the model.
-     *
-     * @param pos the position &gt;= 0
-     * @return  index of the view representing the given position, or
-     * -1 if no view represents that position
-     */
     override fun getViewIndexAtPosition(pos: Int): Int {
-      // This is expensive, but are views are not necessarily layed
-      // out in model order.
       if (pos < startOffset || pos >= endOffset) return -1
       for (counter in viewCount - 1 downTo 0) {
         val v = getView(counter)
@@ -395,11 +129,6 @@ open class ParagraphViewEx(elem: Element) : ParagraphView(elem) {
       return -1
     }
 
-    /**
-     * Gets the left inset.
-     *
-     * @return the inset
-     */
     override fun getLeftInset(): Short {
       var parentView: View
       var adjustment = 0
@@ -427,19 +156,6 @@ open class ParagraphViewEx(elem: Element) : ParagraphView(elem) {
         val actualLineHeight = minorRequest?.preferred ?: 0
         return max(0f, expectedLineHeight - actualLineHeight).toInt()
       }
-
-    private var justificationData: IntArray? = null
-
   }
-
-  companion object {
-    const val SPACE_ADDON: Int = 0
-    const val SPACE_ADDON_LEFTOVER_END: Int = 1
-    const val START_JUSTIFIABLE: Int = 2
-
-    //this should be the last index in justificationData
-    const val END_JUSTIFIABLE: Int = 3
-  }
-
 
 }
