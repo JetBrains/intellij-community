a89e4870c6090398b28d3be79b61636535592865
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-02-26 16:03:39 +0100

    [refactoring] cosmetic fix to tree rendering in conflicts dialog
    GitOrigin-RevId: 4de8360189b42899cb21fa346cad7742c7ff1177

--- platform/lang-impl/src/com/intellij/refactoring/ui/ConflictsDialog.java
+++ platform/lang-impl/src/com/intellij/refactoring/ui/ConflictsDialog.java
@@ -469,7 +469,9 @@ public class ConflictsDialog extends DialogWrapper implements ConflictsDialogBas
         int start = 0;
         int refStart = myConflictDescription.indexOf(CODE_START);
         while (refStart > 0) {
-          chunks.add(new TextChunk(REGULAR_ATTRIBUTES.toTextAttributes(), myConflictDescription.substring(start, refStart)));
+          // workaround for UsageViewTreeCellRenderer adding a space after the first chunk
+          String substring = myConflictDescription.substring(start, chunks.isEmpty() ? refStart : refStart + 1);
+          chunks.add(new TextChunk(REGULAR_ATTRIBUTES.toTextAttributes(), substring));
           int refEnd = myConflictDescription.indexOf(CODE_END, refStart);
           if (refEnd < 0) {
             return new TextChunk[]{new TextChunk(REGULAR_ATTRIBUTES.toTextAttributes(), myConflictDescription)};
