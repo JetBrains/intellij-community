42ce5bb976ccdb15743ced35109477419bbdc340
Yann Cébron <yann.cebron@jetbrains.com>
2024-02-26 10:28:28 +0100

    [devkit] IntelliJPlatformProductTest: speedup (IDEA-336017)
    GitOrigin-RevId: bd048ef600b8a705a82df799af0c563e145b38bb

--- plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/projectRoots/IntelliJPlatformProductTest.java
+++ plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/projectRoots/IntelliJPlatformProductTest.java
@@ -1,11 +1,11 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.projectRoots;
 
-import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase;
+import com.intellij.testFramework.UsefulTestCase;
 
 import static org.jetbrains.idea.devkit.projectRoots.IntelliJPlatformProduct.fromMavenCoordinates;
 
-public class IntelliJPlatformProductTest extends JavaCodeInsightFixtureTestCase {
+public class IntelliJPlatformProductTest extends UsefulTestCase {
 
   public void testFromMavenCoordinates() {
     assertSame(fromMavenCoordinates("com.jetbrains.intellij.idea", "ideaIU"), IntelliJPlatformProduct.IDEA);
