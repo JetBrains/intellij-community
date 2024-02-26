1530af3d667812e2ccfa1c739b34a538c8b525a1
Ilia Zakoulov <ilia.zakoulov@jetbrains.com>
2024-02-26 18:13:37 +0100

    PY-66522: Merge PyConsoleFrameworkParametersCustomizer.kt to PyConsoleOptionsProvider.java
    GitOrigin-RevId: 4e2ad7bfa422d0ac9ac25a2dab2122c54cb8df30

--- python/src/com/jetbrains/python/console/PyConsoleOptionsProvider.java
+++ python/src/com/jetbrains/python/console/PyConsoleOptionsProvider.java
@@ -2,16 +2,25 @@
 package com.jetbrains.python.console;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.NlsContexts;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 
 public interface PyConsoleOptionsProvider {
   ExtensionPointName<PyConsoleOptionsProvider> EP_NAME = ExtensionPointName.create("Pythonid.consoleOptionsProvider");
 
   boolean isApplicableTo(Project project);
+
   @NlsContexts.ConfigurableName
   String getName();
+
   String getHelpTopic();
+
   PyConsoleOptions.PyConsoleSettings getSettings(Project project);
+
+  @Nullable
+  PyConsoleParameters getConsoleParameters(@NotNull Module module);
 }
