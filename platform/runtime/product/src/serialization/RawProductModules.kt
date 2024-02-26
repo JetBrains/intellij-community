13ce6380444f87d3099395944a37a994cf93e253
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-02-26 14:30:56 +0100

    [modular loader] refactoring: extract the result of parsing product-modules.xml to a separate class (RawProductModules)
    This is needed to support inclusions in product-modules.xml (IJPL-755).
    
    (cherry picked from commit b3e6df78289da975614d4fe756f1cccff66c2c89, IJ-CR-127684)
    
    GitOrigin-RevId: 40ae7f96db1ea8f3e93bfa3024913bd7558cb0dc

---
+++ platform/runtime/product/src/serialization/RawProductModules.kt
@@ -0,0 +1,9 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.runtime.product.serialization
+
+import com.intellij.platform.runtime.repository.RuntimeModuleId
+
+class RawProductModules(
+  val mainGroupModules: List<RawIncludedRuntimeModule>,
+  val bundledPluginMainModules: List<RuntimeModuleId>,
+)
