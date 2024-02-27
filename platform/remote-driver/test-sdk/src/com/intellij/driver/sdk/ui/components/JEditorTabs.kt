25a79b5fec97dac6ab5c8bd927c3924bbe4d84df
Mikhail Kuznetsov <mikhail.kuznetsov@jetbrains.com>
2024-02-27 15:19:04 +0200

    [ui tests][AT-789] test for readme markdown
    GitOrigin-RevId: 12e30ecca9284bcc8e5e6f5fe47ad65a234cb6e7

---
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/JEditorTabs.kt
@@ -0,0 +1,31 @@
+package com.intellij.driver.sdk.ui.components
+
+import com.intellij.driver.client.Remote
+import com.intellij.driver.sdk.ui.Finder
+import org.intellij.lang.annotations.Language
+
+
+fun Finder.editorTabs(@Language("xpath") xpath: String? = null) =
+  x(xpath ?: "//div[@class='EditorTabs']", EditorTabsUiComponent::class.java)
+
+class EditorTabsUiComponent(data: ComponentData) : UiComponent(data) {
+
+  private val fixture by lazy { driver.cast(component, EditorTabsRef::class) }
+
+  fun getTabs() = fixture.getTabs().map { Tab(it) }
+
+  inner class Tab(private val data: TabInfoRef) {
+    val text: String
+      get() = data.getText()
+  }
+}
+
+@Remote("com.intellij.openapi.fileEditor.impl.EditorTabs")
+interface EditorTabsRef {
+  fun getTabs(): List<TabInfoRef>
+}
+
+@Remote("com.intellij.ui.tabs.TabInfo")
+interface TabInfoRef {
+  fun getText(): String
+}
