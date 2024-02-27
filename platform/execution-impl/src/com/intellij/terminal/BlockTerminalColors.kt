bc02b64fae735dd79fb166ab010b52aadc1ca3c2
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2024-02-27 17:51:23 +0100

    terminal: use `@JvmField` in `BlockTerminalColors` for better Kotlin-Java interop
    (cherry picked from commit 1d18bd94342d72058ecfaf86d3f01924feca32ab)
    
    IJ-CR-127955
    
    GitOrigin-RevId: 3dd7e2785833feb86bd6f578028c057bc57a2659

--- platform/execution-impl/src/com/intellij/terminal/BlockTerminalColors.kt
+++ platform/execution-impl/src/com/intellij/terminal/BlockTerminalColors.kt
@@ -7,37 +7,37 @@ import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
 object BlockTerminalColors {
-  @JvmStatic val DEFAULT_FOREGROUND: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_DEFAULT_FOREGROUND")
-  @JvmStatic val DEFAULT_BACKGROUND: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_DEFAULT_BACKGROUND")
+  @JvmField val DEFAULT_FOREGROUND: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_DEFAULT_FOREGROUND")
+  @JvmField val DEFAULT_BACKGROUND: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_DEFAULT_BACKGROUND")
 
-  @JvmStatic val BLOCK_BACKGROUND_START: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_BLOCK_BACKGROUND_START")
-  @JvmStatic val BLOCK_BACKGROUND_END: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_BLOCK_BACKGROUND_END")
+  @JvmField val BLOCK_BACKGROUND_START: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_BLOCK_BACKGROUND_START")
+  @JvmField val BLOCK_BACKGROUND_END: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_BLOCK_BACKGROUND_END")
 
-  @JvmStatic val SELECTED_BLOCK_BACKGROUND: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_SELECTED_BLOCK_BACKGROUND")
-  @JvmStatic val SELECTED_BLOCK_STROKE_COLOR: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_SELECTED_BLOCK_STROKE_COLOR")
+  @JvmField val SELECTED_BLOCK_BACKGROUND: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_SELECTED_BLOCK_BACKGROUND")
+  @JvmField val SELECTED_BLOCK_STROKE_COLOR: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_SELECTED_BLOCK_STROKE_COLOR")
 
-  @JvmStatic val INACTIVE_SELECTED_BLOCK_BACKGROUND: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_INACTIVE_SELECTED_BLOCK_BACKGROUND")
-  @JvmStatic val INACTIVE_SELECTED_BLOCK_STROKE_COLOR: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_INACTIVE_SELECTED_BLOCK_STROKE_COLOR")
+  @JvmField val INACTIVE_SELECTED_BLOCK_BACKGROUND: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_INACTIVE_SELECTED_BLOCK_BACKGROUND")
+  @JvmField val INACTIVE_SELECTED_BLOCK_STROKE_COLOR: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_INACTIVE_SELECTED_BLOCK_STROKE_COLOR")
 
-  @JvmStatic val ERROR_BLOCK_STROKE_COLOR: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_ERROR_BLOCK_STROKE_COLOR")
+  @JvmField val ERROR_BLOCK_STROKE_COLOR: ColorKey = ColorKey.createColorKey("BLOCK_TERMINAL_ERROR_BLOCK_STROKE_COLOR")
 
-  @JvmStatic val BLACK: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_BLACK")
-  @JvmStatic val RED: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_RED")
-  @JvmStatic val GREEN: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_GREEN")
-  @JvmStatic val YELLOW: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_YELLOW")
-  @JvmStatic val BLUE: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_BLUE")
-  @JvmStatic val MAGENTA: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_MAGENTA")
-  @JvmStatic val CYAN: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_CYAN")
-  @JvmStatic val WHITE: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_WHITE")
+  @JvmField val BLACK: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_BLACK")
+  @JvmField val RED: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_RED")
+  @JvmField val GREEN: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_GREEN")
+  @JvmField val YELLOW: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_YELLOW")
+  @JvmField val BLUE: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_BLUE")
+  @JvmField val MAGENTA: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_MAGENTA")
+  @JvmField val CYAN: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_CYAN")
+  @JvmField val WHITE: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_WHITE")
 
-  @JvmStatic val BLACK_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_BLACK_BRIGHT")
-  @JvmStatic val RED_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_RED_BRIGHT")
-  @JvmStatic val GREEN_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_GREEN_BRIGHT")
-  @JvmStatic val YELLOW_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_YELLOW_BRIGHT")
-  @JvmStatic val BLUE_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_BLUE_BRIGHT")
-  @JvmStatic val MAGENTA_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_MAGENTA_BRIGHT")
-  @JvmStatic val CYAN_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_CYAN_BRIGHT")
-  @JvmStatic val WHITE_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_WHITE_BRIGHT")
+  @JvmField val BLACK_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_BLACK_BRIGHT")
+  @JvmField val RED_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_RED_BRIGHT")
+  @JvmField val GREEN_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_GREEN_BRIGHT")
+  @JvmField val YELLOW_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_YELLOW_BRIGHT")
+  @JvmField val BLUE_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_BLUE_BRIGHT")
+  @JvmField val MAGENTA_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_MAGENTA_BRIGHT")
+  @JvmField val CYAN_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_CYAN_BRIGHT")
+  @JvmField val WHITE_BRIGHT: TextAttributesKey = textAttributesKey("BLOCK_TERMINAL_WHITE_BRIGHT")
 
   val KEYS: Array<TextAttributesKey>
     get() = arrayOf(
