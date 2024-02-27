eb3af5935196a8fec544623501e712fb9766936b
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-02-27 17:53:49 +0100

    Convert DelayTypeCommand to Kotlin
    GitOrigin-RevId: 32b6fd12ffcb9948c91bf18c77a5d25397903510

--- plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/commands/DelayTypeCommand.kt
+++ plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/commands/DelayTypeCommand.kt
@@ -1,133 +1,127 @@
-package com.jetbrains.performancePlugin.commands;
+package com.jetbrains.performancePlugin.commands
 
-import com.intellij.internal.performance.LatencyRecord;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.actionSystem.LatencyListener;
-import com.intellij.openapi.ui.TypingTarget;
-import com.intellij.openapi.ui.playback.PlaybackContext;
-import com.intellij.openapi.ui.playback.commands.KeyCodeTypeCommand;
-import com.intellij.openapi.util.Ref;
-import com.intellij.platform.diagnostic.telemetry.helpers.TraceUtil;
-import com.intellij.util.ConcurrencyUtil;
-import com.jetbrains.performancePlugin.PerformanceTestSpan;
-import com.jetbrains.performancePlugin.utils.DaemonCodeAnalyzerListener;
-import com.jetbrains.performancePlugin.utils.DaemonCodeAnalyzerResult;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.context.Context;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.concurrency.AsyncPromise;
-import org.jetbrains.concurrency.Promise;
-
-import java.awt.*;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import com.intellij.internal.performance.LatencyRecord
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.editor.actionSystem.LatencyListener
+import com.intellij.openapi.ui.playback.PlaybackContext
+import com.intellij.openapi.ui.playback.commands.KeyCodeTypeCommand
+import com.intellij.openapi.util.Ref
+import com.intellij.platform.diagnostic.telemetry.helpers.TraceUtil
+import com.intellij.util.ConcurrencyUtil
+import com.jetbrains.performancePlugin.PerformanceTestSpan
+import com.jetbrains.performancePlugin.commands.Waiter.checkCondition
+import com.jetbrains.performancePlugin.utils.DaemonCodeAnalyzerListener.listen
+import com.jetbrains.performancePlugin.utils.DaemonCodeAnalyzerResult
+import io.opentelemetry.api.trace.Span
+import io.opentelemetry.context.Context
+import org.jetbrains.concurrency.AsyncPromise
+import org.jetbrains.concurrency.Promise
+import java.awt.KeyboardFocusManager
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
 
 /**
  * Command types text with some delay between typing.
  * Text and delay are being set as parameters.
  * Syntax: %delayType <delay in ms>|<Text to type>
  * Example: %delayType 150|Sample text for typing scenario
- */
-public class DelayTypeCommand extends KeyCodeTypeCommand {
+</Text></delay> */
+class DelayTypeCommand(text: String, line: Int) : KeyCodeTypeCommand(text, line) {
+  private val myExecutor: ScheduledExecutorService = ConcurrencyUtil.newSingleScheduledThreadExecutor("Performance plugin delayed type")
 
-  public static final String PREFIX = CMD_PREFIX + "delayType";
-  public static final char END_CHAR = '#';
-  public static final String SPAN_NAME = "typing";
-  public static final String CODE_ANALYSIS_SPAN_NAME = "typingCodeAnalyzing";
+  override fun _execute(context: PlaybackContext): Promise<Any> {
+    val result = AsyncPromise<Any>()
 
-  private final ScheduledExecutorService myExecutor = ConcurrencyUtil.newSingleScheduledThreadExecutor("Performance plugin delayed type");
+    val input = extractCommandArgument(PREFIX)
+    val delayText = input.split("\\|".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+    val delay = delayText[0].toLong()
+    val text = delayText[1] + END_CHAR
+    val calculateAnalyzesTime = delayText.size > 2 && delayText[2].toBoolean()
+    val spanRef = Ref<Span>()
+    val projectConnection = context.project.messageBus.simpleConnect()
+    val applicationConnection = ApplicationManager.getApplication().messageBus.connect()
 
-  public DelayTypeCommand(@NotNull String text, int line) {
-    super(text, line);
-  }
+    val latencyRecorder = LatencyRecord()
+    applicationConnection.subscribe(LatencyListener.TOPIC, LatencyListener { _, _, latencyMs ->
+      latencyRecorder.update(latencyMs.toInt())
+    })
 
-  @Override
-  public @NotNull Promise<Object> _execute(final @NotNull PlaybackContext context) {
-    final AsyncPromise<Object> result = new AsyncPromise<>();
+    val job = Ref<DaemonCodeAnalyzerResult>()
+    ApplicationManager.getApplication().executeOnPooledThread(Context.current().wrap(
+      Runnable {
+        TraceUtil.runWithSpanThrows<RuntimeException>(PerformanceTestSpan.TRACER, SPAN_NAME) { span: Span ->
+          span.addEvent("Finding typing target")
+          try {
+            checkCondition { findTarget(context) != null }.await(1, TimeUnit.MINUTES)
+          }
+          catch (e: InterruptedException) {
+            span.recordException(e)
+            result.setError(e)
+            return@runWithSpanThrows
+          }
 
-    String input = extractCommandArgument(PREFIX);
-    String[] delayText = input.split("\\|");
-    final long delay = Integer.parseInt(delayText[0]);
-    final String text = delayText[1] + END_CHAR;
-    final boolean calculateAnalyzesTime = delayText.length > 2 && Boolean.parseBoolean(delayText[2]);
-    Ref<Span> spanRef = new Ref<>();
-    var projectConnection = context.getProject().getMessageBus().simpleConnect();
-    var applicationConnection = ApplicationManager.getApplication().getMessageBus().connect();
+          val allScheduled = CountDownLatch(1)
+          for (i in text.indices) {
+            val currentChar = text[i]
+            val nextCharIsTheLast = ((i + 1) < text.length) && (text[i + 1] == END_CHAR)
+            myExecutor.schedule({
+                                  ApplicationManager.getApplication().invokeAndWait(Context.current().wrap(
+                                    Runnable {
+                                      if (nextCharIsTheLast && calculateAnalyzesTime) {
+                                        job.set(listen(projectConnection, spanRef, 0, null))
+                                        val spanBuilder = PerformanceTestSpan.TRACER.spanBuilder(CODE_ANALYSIS_SPAN_NAME).setParent(
+                                          Context.current().with(span))
+                                        spanRef.set(spanBuilder.startSpan())
+                                      }
+                                      if (currentChar == END_CHAR) {
+                                        allScheduled.countDown()
+                                        myExecutor.shutdown()
+                                      }
+                                      else {
+                                        span.addEvent("Calling find target second time in DelayTypeCommand")
+                                        val typingTarget = findTarget(context)
+                                        if (typingTarget != null) {
+                                          span.addEvent("Typing $currentChar")
+                                          typingTarget.type(currentChar.toString())
+                                        }
+                                        else {
+                                          span.addEvent("Focus was lost")
+                                          result.setError(
+                                            "Focus was lost during typing. Current focus is in: " + KeyboardFocusManager.getCurrentKeyboardFocusManager().focusOwner.javaClass)
+                                        }
+                                      }
+                                    }))
+                                }, i * delay, TimeUnit.MILLISECONDS)
+          }
+          try {
+            allScheduled.await()
+            myExecutor.awaitTermination(1, TimeUnit.MINUTES)
 
-    var latencyRecorder = new LatencyRecord();
-    applicationConnection.subscribe(LatencyListener.TOPIC, new LatencyListener() {
-      @Override
-      public void recordTypingLatency(@NotNull Editor editor, String action, long latencyMs) {
-        latencyRecorder.update(((int)latencyMs));
-      }
-    });
-
-    Ref<DaemonCodeAnalyzerResult> job = new Ref<>();
-    ApplicationManager.getApplication().executeOnPooledThread(Context.current().wrap(() -> {
-      TraceUtil.runWithSpanThrows(PerformanceTestSpan.TRACER, SPAN_NAME, span -> {
-        span.addEvent("Finding typing target");
-        try {
-          Waiter.checkCondition(() -> findTarget(context) != null).await(1, TimeUnit.MINUTES);
-        }
-        catch (InterruptedException e) {
-          span.recordException(e);
-          result.setError(e);
-          return;
-        }
-
-        CountDownLatch allScheduled = new CountDownLatch(1);
-        for (int i = 0; i < text.length(); i++) {
-          char currentChar = text.charAt(i);
-          boolean nextCharIsTheLast = ((i + 1) < text.length()) && (text.charAt(i + 1) == END_CHAR);
-          myExecutor.schedule(() -> ApplicationManager.getApplication().invokeAndWait(Context.current().wrap(
-            () -> {
-              if (nextCharIsTheLast && calculateAnalyzesTime) {
-                job.set(DaemonCodeAnalyzerListener.INSTANCE.listen(projectConnection, spanRef, 0, null));
-                var spanBuilder = PerformanceTestSpan.TRACER.spanBuilder(CODE_ANALYSIS_SPAN_NAME).setParent(Context.current().with(span));
-                spanRef.set(spanBuilder.startSpan());
-              }
-              if (currentChar == END_CHAR) {
-                allScheduled.countDown();
-                myExecutor.shutdown();
-              }
-              else {
-                span.addEvent("Calling find target second time in DelayTypeCommand");
-                TypingTarget typingTarget = findTarget(context);
-                if (typingTarget != null) {
-                  span.addEvent("Typing " + currentChar);
-                  typingTarget.type(String.valueOf(currentChar));
-                } else {
-                  span.addEvent("Focus was lost");
-                  result.setError("Focus was lost during typing. Current focus is in: " + KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusOwner().getClass());
-                }
-              }
-            })), i * delay, TimeUnit.MILLISECONDS);
-        }
-        try {
-          allScheduled.await();
-          myExecutor.awaitTermination(1, TimeUnit.MINUTES);
-
-          if (!latencyRecorder.getSamples().isEmpty()) {
-            span.setAttribute("latency#max", latencyRecorder.getMaxLatency());
-            span.setAttribute("latency#p90", latencyRecorder.percentile(90));
-            span.setAttribute("latency#mean_value", latencyRecorder.getAverageLatency());
+            if (!latencyRecorder.samples.isEmpty) {
+              span.setAttribute("latency#max", latencyRecorder.maxLatency.toLong())
+              span.setAttribute("latency#p90", latencyRecorder.percentile(90).toLong())
+              span.setAttribute("latency#mean_value", latencyRecorder.averageLatency)
+            }
+          }
+          catch (e: InterruptedException) {
+            result.setError(e)
           }
         }
-        catch (InterruptedException e) {
-          result.setError(e);
+        if (calculateAnalyzesTime) {
+          job.get().blockingWaitForComplete()
         }
-      });
+        applicationConnection.disconnect()
+        result.setResult(null)
+      }))
 
-      if (calculateAnalyzesTime) {
-        job.get().blockingWaitForComplete();
-      }
-      applicationConnection.disconnect();
-      result.setResult(null);
-    }));
-
-    return result;
+    return result
   }
 
+  companion object {
+    const val PREFIX: String = CMD_PREFIX + "delayType"
+    const val END_CHAR: Char = '#'
+    const val SPAN_NAME: String = "typing"
+    const val CODE_ANALYSIS_SPAN_NAME: String = "typingCodeAnalyzing"
+  }
 }
