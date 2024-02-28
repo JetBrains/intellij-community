2a2c426fa365e1334afabfbcb0695f9cfb952285
Vladimir Plyashkun <vladimir.plyashkun@jetbrains.com>
2024-02-28 08:49:25 +0200

    CPP-35929 - add javadoc
    GitOrigin-RevId: 913e61f4ecc38f9ccecac3a4f7b1d88a5764ad67

--- platform/lang-impl/src/com/intellij/codeInsight/intention/impl/AbstractIntentionPopup.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/intention/impl/AbstractIntentionPopup.kt
@@ -5,8 +5,31 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.ui.popup.ListPopup
 import com.intellij.ui.awt.RelativePoint
 
+/**
+ * Represents an abstract popup interface that provides functionality to override
+ * default popup behavior with custom logic, for example, to include intentions from different backends.
+ */
 interface AbstractIntentionPopup : Disposable {
+  /**
+   * Determines if the popup is currently visible.
+   *
+   * @return {@code true} if the popup is visible, {@code false} otherwise.
+   */
   fun isVisible(): Boolean
+  /**
+   * Displays the popup with the specified customization options.
+   *
+   * @param hintComponent The component relative to which the popup will be displayed.
+   * @param relativePoint The point relative to the {@code hintComponent} where the popup
+   *                      will be shown. If {@code null}, the popup is shown at a default
+   *                      position relative to the {@code hintComponent}.
+   * @param listPopupCustomization An optional customization function that can be applied
+   *                               to the {@link ListPopup} just before it is shown.
+   *                               It can be {@code null} if no customization is needed.
+   */
   fun show(hintComponent: IntentionHintComponent, relativePoint: RelativePoint?, listPopupCustomization: ((ListPopup) -> Unit)?)
+  /**
+   * Closes the popup if it is currently visible.
+   */
   fun close()
 }
