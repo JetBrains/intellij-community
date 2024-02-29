9eaf34e1250f9df7f4f3e7cc5284f5e684d5b45a
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2024-02-29 23:23:10 +0100

    terminal: remove "Run Commands using IDE" option from the More menu and Context menu (IDEA-347563)
    (cherry picked from commit c1387a8b2c11664482885151f369a04141ecf216)
    
    IJ-CR-127612
    
    GitOrigin-RevId: 194b03d4dc281164658d82ba0a02fc9e457e2c15

--- plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalToolWindowFactory.java
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalToolWindowFactory.java
@@ -1,19 +1,15 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.terminal;
 
-import com.intellij.openapi.actionSystem.ActionUpdateThread;
-import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.DumbAwareToggleAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.openapi.wm.ex.ToolWindowEx;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.terminal.TerminalCommandHandlerCustomizer.TerminalCommandHandlerOptions;
 import org.jetbrains.plugins.terminal.action.EnableBlockTerminalUiAction;
 import org.jetbrains.plugins.terminal.arrangement.TerminalArrangementManager;
 
@@ -28,40 +24,11 @@ public final class TerminalToolWindowFactory implements ToolWindowFactory, DumbA
 
     TerminalToolWindowManager terminalToolWindowManager = TerminalToolWindowManager.getInstance(project);
     terminalToolWindowManager.initToolWindow((ToolWindowEx)toolWindow);
-    TerminalCommandHandlerOptions options = new TerminalCommandHandlerOptions(project);
-    toolWindow.setAdditionalGearActions(new DefaultActionGroup(
-      new EnableBlockTerminalUiAction(),
-      new SmartCommandExecutionToggleAction(options)
-    ));
+    toolWindow.setAdditionalGearActions(new DefaultActionGroup(new EnableBlockTerminalUiAction()));
 
     TerminalArrangementManager terminalArrangementManager = TerminalArrangementManager.getInstance(project);
     terminalToolWindowManager.restoreTabs(terminalArrangementManager.getArrangementState());
     // allow to save tabs after the tabs are restored
     terminalArrangementManager.setToolWindow(toolWindow);
-  }
-
-  private static final class SmartCommandExecutionToggleAction extends DumbAwareToggleAction {
-    private final TerminalCommandHandlerOptions myOptions;
-
-    private SmartCommandExecutionToggleAction(TerminalCommandHandlerOptions options) {
-      //noinspection DialogTitleCapitalization
-      super(TerminalBundle.message("settings.terminal.smart.command.handling"));
-      myOptions = options;
-    }
-
-    @Override
-    public boolean isSelected(@NotNull AnActionEvent e) {
-      return myOptions.getEnabled();
-    }
-
-    @Override
-    public void setSelected(@NotNull AnActionEvent e, boolean state) {
-      myOptions.setEnabled(state);
-    }
-
-    @Override
-    public @NotNull ActionUpdateThread getActionUpdateThread() {
-      return ActionUpdateThread.BGT;
-    }
   }
 }
