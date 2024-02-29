8b3d6afb4a58785b38423566449d1b277ebed258
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2024-02-29 10:55:31 +0100

    [test] Don't parse expected/actuals for very long messages
    The default threshold is 10_000 characters but can be changed by using setting the `idea.expected.message.length.threshold` property key. #IDEA-347460 Fixed
    
    GitOrigin-RevId: 2c5ab995aab90eed2be001a2962bb8a42e984a7a

--- plugins/junit/java-tests/test/com/intellij/execution/junit/JUnitExpectedPatternsTest.java
+++ plugins/junit/java-tests/test/com/intellij/execution/junit/JUnitExpectedPatternsTest.java
@@ -1,22 +1,10 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.junit;
 
 import com.intellij.junit4.ExpectedPatterns;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.rt.execution.junit.ComparisonFailureData;
+import com.intellij.rt.execution.testFrameworks.AbstractExpectedPatterns;
 import org.junit.Assert;
 import org.junit.ComparisonFailure;
 import org.junit.Test;
@@ -44,6 +32,38 @@ public class JUnitExpectedPatternsTest {
                                               reason
                                               Expected: is "aaa\\naa"
                                                    but:    was "bbb\\nbb\""""));
+  }
+
+  @Test
+  public void testMessageTooLongDefaultThreshold() {
+    String longMessage = StringUtil.repeat("a", AbstractExpectedPatterns.DEFAULT_MESSAGE_LENGTH_THRESHOLD);
+    Assert.assertNull(createNotification("""
+                                           reason
+                                           Expected: is "aaa\\naa"
+                                                but:    was \"""" + longMessage + "\""));
+  }
+
+  @Test
+  public void testMessageTooLongOverrideThreshold() {
+    try {
+      System.setProperty(
+        AbstractExpectedPatterns.MESSAGE_LENGTH_THRESHOLD_PROPERTY,
+        Integer.toString(AbstractExpectedPatterns.DEFAULT_MESSAGE_LENGTH_THRESHOLD * 2)
+      );
+      String shortEnoughMessage = StringUtil.repeat("a", AbstractExpectedPatterns.DEFAULT_MESSAGE_LENGTH_THRESHOLD);
+      Assert.assertNotNull(createNotification("""
+                                                reason
+                                                Expected: is "aaa\\naa"
+                                                     but:    was \"""" + shortEnoughMessage + "\""));
+      String longMessage = StringUtil.repeat("a", AbstractExpectedPatterns.DEFAULT_MESSAGE_LENGTH_THRESHOLD * 2);
+      Assert.assertNull(createNotification("""
+                                             reason
+                                             Expected: is "aaa\\naa"
+                                                  but:    was \"""" + longMessage + "\""));
+    }
+    finally {
+      System.clearProperty(AbstractExpectedPatterns.MESSAGE_LENGTH_THRESHOLD_PROPERTY);
+    }
   }
 
   @Test
