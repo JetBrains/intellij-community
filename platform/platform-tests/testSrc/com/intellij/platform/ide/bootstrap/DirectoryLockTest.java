ef9ffa18af0c9fbd06f9e916fae4dcc31de641ba
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-03-01 12:00:21 +0100

    [platform] distinguishing "unresponsive IDE" and "a command taking long to complete" cases (IDEA-344102)
    (cherry-picked from commit b420305ae81a1ccdc7062eb011ee8900ddfd1eb6)
    
    IJ-CR-127650
    
    GitOrigin-RevId: ab8bb66aa8e7597c3cbf0040d460c4c9c4dc26fa

--- platform/platform-tests/testSrc/com/intellij/platform/ide/bootstrap/DirectoryLockTest.java
+++ platform/platform-tests/testSrc/com/intellij/platform/ide/bootstrap/DirectoryLockTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.ide.bootstrap;
 
 import com.intellij.ide.CliResult;
@@ -13,7 +13,6 @@ import com.intellij.testFramework.TestLoggerFactory;
 import com.intellij.testFramework.rules.InMemoryFsRule;
 import com.intellij.testFramework.rules.TempDirectory;
 import com.intellij.util.Suppressions;
-import com.intellij.util.TimeoutUtil;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -26,7 +25,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -233,17 +231,5 @@ public abstract sealed class DirectoryLockTest {
     Files.writeString(configDir.resolve(SpecialConfigFiles.LOCK_FILE), Long.toString(ProcessHandle.current().pid()));
     var lock = createLock(configDir, testDir.resolve("s"));
     assertThatThrownBy(() -> lock.lockOrActivate(currentDir, List.of())).isInstanceOf(CannotActivateException.class);
-  }
-
-  @Test
-  public void responseTimeout() throws Exception {
-    var timeoutMs = 300;
-    var lock = new DirectoryLock(testDir.resolve("c"), testDir.resolve("s"), args -> { TimeoutUtil.sleep(10_000L); return CliResult.OK; })
-      .withConnectTimeout(timeoutMs);
-    activeLocks.add(lock);
-    assertNull(lock.lockOrActivate(currentDir, List.of()));
-    var t = System.nanoTime();
-    assertThatThrownBy(() -> lock.lockOrActivate(currentDir, List.of())).isInstanceOf(CannotActivateException.class);
-    assertThat(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - t)).isGreaterThanOrEqualTo(timeoutMs);
   }
 }
