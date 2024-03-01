505b0f9b85b2dcca59e17c2778bd1f2c26de1eed
Dmitrii Denisov <dmitrii.denisov@jetbrains.com>
2024-03-01 13:36:14 +0100

    Revert "[rdct-tests]: GTW-7787: fix missing locally build installers"
    This reverts commit ff379d2b064dd299360f2fac06eb117fd4b54c1f.
    
    GitOrigin-RevId: dc50921b078cb0c767b16098ff25b43d31c719b6

--- platform/remoteDev-util/src/com/intellij/remoteDev/downloader/JetBrainsClientDownloaderConfigurationProvider.kt
+++ platform/remoteDev-util/src/com/intellij/remoteDev/downloader/JetBrainsClientDownloaderConfigurationProvider.kt
@@ -234,8 +234,6 @@ class TestJetBrainsClientDownloaderConfigurationProvider : JetBrainsClientDownlo
     val server = tarGzServer
     require(server != null)
 
-    thisLogger().info("Serving file $file from ${server.address}")
-
     server.createContext("/${file.name}", HttpHandler { httpExchange ->
       httpExchange.sendResponseHeaders(200, file.fileSize())
       httpExchange.responseBody.use { responseBody ->
