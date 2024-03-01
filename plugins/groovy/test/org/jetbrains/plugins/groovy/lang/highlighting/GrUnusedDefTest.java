e9775d8eb65af3e34f69116f37305294ae70cfa0
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-03-01 09:58:02 +0100

    [Groovy] convert GrUnusedDefTest to Java (IDEA-327339)
    GitOrigin-RevId: 71ad5ccb2552540dd745e3b000ef4fc4c70e8853

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrUnusedDefTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrUnusedDefTest.java
@@ -1,171 +1,163 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.groovy.lang.highlighting
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.highlighting;
 
-import com.intellij.codeInspection.InspectionProfileEntry
-import com.intellij.codeInspection.deadCode.UnusedDeclarationInspectionBase
-import org.jetbrains.plugins.groovy.codeInspection.GroovyUnusedDeclarationInspection
-import org.jetbrains.plugins.groovy.codeInspection.confusing.GrUnusedIncDecInspection
-import org.jetbrains.plugins.groovy.codeInspection.unusedDef.UnusedDefInspection
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.codeInspection.InspectionProfileEntry;
+import com.intellij.codeInspection.deadCode.UnusedDeclarationInspectionBase;
+import junit.framework.TestCase;
+import org.jetbrains.plugins.groovy.codeInspection.GroovyUnusedDeclarationInspection;
+import org.jetbrains.plugins.groovy.codeInspection.confusing.GrUnusedIncDecInspection;
+import org.jetbrains.plugins.groovy.codeInspection.unusedDef.UnusedDefInspection;
 
 /**
  * @author Max Medvedev
  */
-class GrUnusedDefTest extends GrHighlightingTestBase {
+public class GrUnusedDefTest extends GrHighlightingTestBase {
   @Override
-  InspectionProfileEntry[] getCustomInspections() {
-    [new UnusedDefInspection(),
-     new GrUnusedIncDecInspection(),
-     new GroovyUnusedDeclarationInspection(),
-     new UnusedDeclarationInspectionBase(true)]
+  public InspectionProfileEntry[] getCustomInspections() {
+    return new InspectionProfileEntry[]{new UnusedDefInspection(), new GrUnusedIncDecInspection(), new GroovyUnusedDeclarationInspection(),
+      new UnusedDeclarationInspectionBase(true)};
   }
 
-  void testUnusedVariable() { doTest() }
+  public void testUnusedVariable() { doTest(); }
 
-  void testDefinitionUsedInClosure() { doTest() }
+  public void testDefinitionUsedInClosure() { doTest(); }
 
-  void testDefinitionUsedInClosure2() { doTest() }
+  public void testDefinitionUsedInClosure2() { doTest(); }
 
-  void testDefinitionUsedInSwitchCase() { doTest() }
+  public void testDefinitionUsedInSwitchCase() { doTest(); }
 
-  void testUnusedDefinitionForMethodMissing() { doTest() }
+  public void testUnusedDefinitionForMethodMissing() { doTest(); }
 
-  void testPrefixIncrementCfa() { doTest() }
+  public void testPrefixIncrementCfa() { doTest(); }
 
-  void testIfIncrementElseReturn() { doTest() }
+  public void testIfIncrementElseReturn() { doTest(); }
 
-  void testSwitchControlFlow() { doTest() }
+  public void testSwitchControlFlow() { doTest(); }
 
-  void testUsageInInjection() { doTest() }
+  public void testUsageInInjection() { doTest(); }
 
-  void testUnusedDefsForArgs() { doTest() }
+  public void testUnusedDefsForArgs() { doTest(); }
 
-  void testUsedDefBeforeTry1() { doTest() }
+  public void testUsedDefBeforeTry1() { doTest(); }
 
-  void testUsedDefBeforeTry2() { doTest() }
+  public void testUsedDefBeforeTry2() { doTest(); }
 
-  void testUnusedInc() { doTest() }
+  public void testUnusedInc() { doTest(); }
 
-  void testUsedInCatch() { doTest() }
+  public void testUsedInCatch() { doTest(); }
 
-  void testGloballyUnusedSymbols() { doTest() }
+  public void testGloballyUnusedSymbols() { doTest(); }
 
-  void testGloballyUnusedInnerMethods() {
-    myFixture.addClass 'package junit.framework; public class TestCase {}'
-    doTest()
+  public void testGloballyUnusedInnerMethods() {
+    myFixture.addClass("package junit.framework; public class TestCase {}");
+    doTest();
   }
 
-  void testUnusedParameter() { doTest() }
+  public void testUnusedParameter() { doTest(); }
 
-  void testSuppressUnusedMethod() {
-    doTestHighlighting('''\
-class <warning descr="Class Foo is unused">Foo</warning> {
-    @SuppressWarnings("GroovyUnusedDeclaration")
-    static def foo(int x) {
-        print 2
-    }
+  public void testSuppressUnusedMethod() {
+    doTestHighlighting("""
+                         class <warning descr="Class Foo is unused">Foo</warning> {
+                             @SuppressWarnings("GroovyUnusedDeclaration")
+                             static def foo(int x) {
+                                 print 2
+                             }
 
-    static def <warning descr="Method bar is unused">bar</warning>() {}
-}
-''')
+                             static def <warning descr="Method bar is unused">bar</warning>() {}
+                         }
+                         """);
   }
 
-  void testUsedVar() {
-    doTestHighlighting '''
-      def <warning descr="Method foo is unused">foo</warning>(xxx) {
-        if ((xxx = 5) || xxx) {
-          <warning descr="Assignment is not used">xxx</warning>=4
-        }
-      }
+  public void testUsedVar() {
+    doTestHighlighting("""
+                            def <warning descr="Method foo is unused">foo</warning>(xxx) {
+                              if ((xxx = 5) || xxx) {
+                                <warning descr="Assignment is not used">xxx</warning>=4
+                              }
+                            }
 
-      def <warning descr="Method foxo is unused">foxo</warning>(doo) {
-        def xxx = 'asdf'
-        if (!doo) {
-          println xxx
-          <warning descr="Assignment is not used">xxx</warning>=5
-        }
-      }
-    '''
+                            def <warning descr="Method foxo is unused">foxo</warning>(doo) {
+                              def xxx = 'asdf'
+                              if (!doo) {
+                                println xxx
+                                <warning descr="Assignment is not used">xxx</warning>=5
+                              }
+                            }
+                         \s""");
   }
 
-  void testFallthroughInSwitch() {
-    doTestHighlighting('''\
-def <warning descr="Method f is unused">f</warning>(String foo, int mode) {
-    switch (mode) {
-        case 0: foo = foo.reverse()
-        case 1: return foo
-    }
-}
+  public void testFallthroughInSwitch() {
+    doTestHighlighting("""
+                         def <warning descr="Method f is unused">f</warning>(String foo, int mode) {
+                             switch (mode) {
+                                 case 0: foo = foo.reverse()
+                                 case 1: return foo
+                             }
+                         }
 
-def <warning descr="Method f2 is unused">f2</warning>(String foo, int mode) {
-    switch (mode) {
-        case 0: <warning descr="Assignment is not used">foo</warning> = foo.reverse()
-        case 1: return 2
-    }
-}
-''')
+                         def <warning descr="Method f2 is unused">f2</warning>(String foo, int mode) {
+                             switch (mode) {
+                                 case 0: <warning descr="Assignment is not used">foo</warning> = foo.reverse()
+                                 case 1: return 2
+                             }
+                         }
+                         """);
   }
 
-  void testUnusedUnassignedVar() {
-    doTestHighlighting('''\
-def <warning descr="Variable is not used">abc</warning>
-''')
+  public void testUnusedUnassignedVar() {
+    doTestHighlighting("def <warning descr=\"Variable is not used\">abc</warning>");
   }
 
-  void 'test method referenced via incapplicable call is used'() {
-    doTestHighlighting '''\
-static boolean fsdasdfsgsdsfadfgs(a, b) { a == b }
-def bar() { fsdasdfsgsdsfadfgs("s") }
-bar()
-'''
+  public void testMethodReferencedViaIncapplicableCallIsUsed() {
+    doTestHighlighting("""
+                         static boolean fsdasdfsgsdsfadfgs(a, b) { a == b }
+                         def bar() { fsdasdfsgsdsfadfgs("s") }
+                         bar()
+                         """);
   }
 
-  void 'test delegate'() {
-    fixture.addClass '''
-package groovy.lang;
-@Target({ElementType.FIELD, ElementType.METHOD})
-public @interface Delegate {}
-'''
+  public void testDelegate() {
+    getFixture().addClass("""
+                             package groovy.lang;
+                             @Target({ElementType.FIELD, ElementType.METHOD})
+                             public @interface Delegate {}
+                             """);
 
-    doTestHighlighting '''\
-class Foo {
-  @Delegate
-  Integer i
-  @Delegate
-  String bar() {}
-}
+    doTestHighlighting("""
+                         class Foo {
+                           @Delegate
+                           Integer i
+                           @Delegate
+                           String bar() {}
+                         }
 
-new Foo()
-'''
+                         new Foo()
+                         """);
   }
 
-  void 'test "unused" suppresses warning'() {
-    doTestHighlighting '''\
-@SuppressWarnings("unused")
-class Aaaa {} 
-'''
+  public void testUnusedSuppressesWarning() {
+    doTestHighlighting("""
+                         @SuppressWarnings("unused")
+                         class Aaaa {}\s
+                         """);
   }
 
-  void 'test suppress with "unused"'() {
-    doTestHighlighting '''\
-class <caret><warning descr="Class Aaaa is unused">Aaaa</warning> {}
-'''
-    def action = fixture.findSingleIntention 'Suppress for class'
-    fixture.launchAction(action)
+  public void testSuppressWithUnused_() {
+    doTestHighlighting("class <caret><warning descr=\"Class Aaaa is unused\">Aaaa</warning> {}");
+    IntentionAction action = getFixture().findSingleIntention("Suppress for class");
+    getFixture().launchAction(action);
 
-    fixture.checkResult '''\
-@SuppressWarnings('unused')
-class <caret>Aaaa {}
-'''
+    getFixture().checkResult("""
+                               @SuppressWarnings('unused')
+                               class <caret>Aaaa {}""");
   }
 
-  void 'test safe delete preview with single class in file'() {
-    doTestHighlighting '''\
-class <caret><warning descr="Class Aaaa is unused">Aaaa</warning> {}
-'''
-    def action = fixture.findSingleIntention 'Safe delete \'Aaaa\''
-    def actualPreview = fixture.getIntentionPreviewText(action)
+  public void testSafeDeletePreviewWithSingleClassInFile() {
+    doTestHighlighting("class <caret><warning descr=\"Class Aaaa is unused\">Aaaa</warning> {}");
+    IntentionAction action = getFixture().findSingleIntention("Safe delete 'Aaaa'");
+    String actualPreview = getFixture().getIntentionPreviewText(action);
 
-    assertEquals('''\
-''', actualPreview)
+    TestCase.assertEquals("", actualPreview);
   }
 }
