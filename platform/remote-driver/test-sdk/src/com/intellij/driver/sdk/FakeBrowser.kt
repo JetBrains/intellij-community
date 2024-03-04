d1f82694cdca3f8c6fbe92dc421e0d5fbb089c6d
vladislav.shishov <vladislav.shishov@jetbrains.com>
2024-03-04 19:14:00 +0200

    [remote-driver] replace ide browser via driver
    GitOrigin-RevId: 1666bd20336a5b7033e27cb23388e402c3227870

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/FakeBrowser.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/FakeBrowser.kt
@@ -1,8 +1,42 @@
 package com.intellij.driver.sdk
 
+import com.intellij.driver.client.Driver
 import com.intellij.driver.client.Remote
+
+fun Driver.replaceIdeBrowser(): FakeBrowser {
+  val fakeBrowser = new(FakeBrowser::class)
+  utility(ApplicationManagerRef::class)
+    .getApplication()
+    .replaceServiceInstance(utility(ClassRef::class).forName("com.intellij.ide.browsers.BrowserLauncher"),
+                            fakeBrowser,
+                            utility(DisposerRef::class).newDisposable())
+  return fakeBrowser
+}
 
 @Remote("com.jetbrains.performancePlugin.FakeBrowser", plugin = "com.jetbrains.performancePlugin")
 interface FakeBrowser {
   fun getLatestUrl(): String?
+}
+
+@Remote("com.intellij.openapi.application.ApplicationManager")
+interface ApplicationManagerRef {
+  fun getApplication(): ApplicationImplRef
+}
+
+@Remote("com.intellij.openapi.application.impl.ApplicationImpl")
+interface ApplicationImplRef {
+  fun replaceServiceInstance(cls: ClassRef, instance: Any, parentDisposable: DisposableRef)
+}
+
+@Remote("com.intellij.openapi.util.Disposer")
+interface DisposerRef {
+  fun newDisposable(): DisposableRef
+}
+
+@Remote("com.intellij.openapi.Disposable")
+interface DisposableRef
+
+@Remote("java.lang.Class")
+interface ClassRef {
+  fun forName(cls: String): ClassRef
 }
