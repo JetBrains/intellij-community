4bef6037466c779e8b7a809f9283e50abd66b1a7
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2024-03-04 20:59:14 +0100

    [vcs-log-graph] convert GraphLayoutBuilder to an object
    GitOrigin-RevId: 255e8e2ac28c380978da27800bf94a9a719a224d

--- platform/vcs-log/graph/src/com/intellij/vcs/log/graph/impl/permanent/GraphLayoutBuilder.kt
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/graph/impl/permanent/GraphLayoutBuilder.kt
@@ -11,68 +11,58 @@ import it.unimi.dsi.fastutil.ints.IntArrayList
 import it.unimi.dsi.fastutil.ints.IntComparator
 import it.unimi.dsi.fastutil.ints.IntList
 
-class GraphLayoutBuilder private constructor(private val graph: LinearGraph, private val headNodeIndex: IntList) {
-  private val layoutIndex = IntArray(graph.nodesCount())
-  private val startLayoutIndexForHead = IntArray(headNodeIndex.size)
-  private var currentLayoutIndex = 1
+object GraphLayoutBuilder {
+  private val LOG = Logger.getInstance(GraphLayoutBuilder::class.java)
 
-  private fun dfs(nodeIndex: Int) {
-    walk(nodeIndex) { currentNode: Int ->
+  @JvmStatic
+  fun build(graph: LinearGraph, comparator: IntComparator): GraphLayoutImpl {
+    val heads = getSortedHeads(graph, comparator)
+
+    val layoutIndex = IntArray(graph.nodesCount())
+    val startLayoutIndexForHead = IntArray(heads.size)
+    var currentLayoutIndex = 1
+    for (i in heads.indices) {
+      startLayoutIndexForHead[i] = currentLayoutIndex
+      currentLayoutIndex = dfs(graph, heads.getInt(i), currentLayoutIndex, layoutIndex)
+    }
+    return GraphLayoutImpl(layoutIndex, heads, startLayoutIndexForHead)
+  }
+
+  private fun getSortedHeads(graph: LinearGraph, comparator: IntComparator): IntList {
+    val heads: IntList = IntArrayList()
+    for (i in 0 until graph.nodesCount()) {
+      if (LinearGraphUtils.getUpNodes(graph, i).isEmpty()) {
+        heads.add(i)
+      }
+    }
+    try {
+      heads.sort(comparator)
+    }
+    catch (pce: ProcessCanceledException) {
+      throw pce
+    }
+    catch (e: Exception) {
+      // protection against possible comparator flaws
+      LOG.error(e)
+    }
+    return heads
+  }
+
+  private fun dfs(graph: LinearGraph, startNodeIndex: Int, startLayoutIndex: Int, layoutIndex: IntArray): Int {
+    var currentLayoutIndex = startLayoutIndex
+
+    walk(startNodeIndex) { currentNode: Int ->
       val firstVisit = layoutIndex[currentNode] == 0
       if (firstVisit) layoutIndex[currentNode] = currentLayoutIndex
 
-      var childWithoutLayoutIndex = -1
-      for (childNodeIndex in LinearGraphUtils.getDownNodes(graph, currentNode)) {
-        if (layoutIndex[childNodeIndex] == 0) {
-          childWithoutLayoutIndex = childNodeIndex
-          break
-        }
-      }
-      if (childWithoutLayoutIndex == -1) {
+      val childWithoutLayoutIndex = LinearGraphUtils.getDownNodes(graph, currentNode).firstOrNull { layoutIndex[it] == 0 }
+      if (childWithoutLayoutIndex == null) {
         if (firstVisit) currentLayoutIndex++
-
         return@walk Dfs.NextNode.NODE_NOT_FOUND
       }
-      else {
-        return@walk childWithoutLayoutIndex
-      }
-    }
-  }
-
-  private fun build(): GraphLayoutImpl {
-    for (i in headNodeIndex.indices) {
-      val headNodeIndex = headNodeIndex.getInt(i)
-      startLayoutIndexForHead[i] = currentLayoutIndex
-
-      dfs(headNodeIndex)
+      return@walk childWithoutLayoutIndex
     }
 
-    return GraphLayoutImpl(layoutIndex, headNodeIndex, startLayoutIndexForHead)
-  }
-
-  companion object {
-    private val LOG = Logger.getInstance(GraphLayoutBuilder::class.java)
-
-    @JvmStatic
-    fun build(graph: LinearGraph, headNodeIndexComparator: IntComparator): GraphLayoutImpl {
-      val heads: IntList = IntArrayList()
-      for (i in 0 until graph.nodesCount()) {
-        if (LinearGraphUtils.getUpNodes(graph, i).isEmpty()) {
-          heads.add(i)
-        }
-      }
-      try {
-        heads.sort(headNodeIndexComparator)
-      }
-      catch (pce: ProcessCanceledException) {
-        throw pce
-      }
-      catch (e: Exception) {
-        // protection against possible comparator flaws
-        LOG.error(e)
-      }
-      val builder = GraphLayoutBuilder(graph, heads)
-      return builder.build()
-    }
+    return currentLayoutIndex
   }
 }
