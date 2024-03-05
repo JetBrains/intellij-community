9f6a3dd429b21fbc692d50516a430745a368a5df
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2024-03-05 09:20:20 +0100

    [jps] Collect time spend in pre-compile tasks in FUS
    #IDEA-344147
    
    (cherry picked from commit e2b6fa748c0f4dc7db61fd649c8005efe5ccbd75)
    
    IJ-CR-128311
    
    GitOrigin-RevId: dc424e669a71dcb06bd75ed0058921f4e8a90ef8

--- java/compiler/impl/src/com/intellij/compiler/impl/BuildUsageCollector.java
+++ java/compiler/impl/src/com/intellij/compiler/impl/BuildUsageCollector.java
@@ -7,7 +7,13 @@ import com.intellij.internal.statistic.eventLog.events.EventId1;
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector;
 
 public final class BuildUsageCollector extends CounterUsagesCollector {
-  private static final EventLogGroup GROUP = new EventLogGroup("build.jps", 2);
+  private static final EventLogGroup GROUP = new EventLogGroup("build.jps", 3);
+
+  private static final EventId1<Long> PRE_COMPILE_BEFORE_TASKS_COMPLETED =
+    GROUP.registerEvent("precompile.before.tasks.completed", EventFields.DurationMs);
+  private static final EventId1<Long> PRE_COMPILE_AFTER_TASKS_COMPLETED =
+    GROUP.registerEvent("precompile.after.tasks.completed", EventFields.DurationMs);
+
   private static final EventId1<Long> REBUILD_COMPLETED = GROUP.registerEvent("rebuild.completed", EventFields.DurationMs);
   private static final EventId1<Long> BUILD_COMPLETED = GROUP.registerEvent("build.completed", EventFields.DurationMs);
   private static final EventId1<Long> AUTO_BUILD_COMPLETED = GROUP.registerEvent("autobuild.completed", EventFields.DurationMs);
@@ -17,7 +23,11 @@ public final class BuildUsageCollector extends CounterUsagesCollector {
     return GROUP;
   }
 
+  public static void logPreCompileCompleted(long durationMs, boolean beforeTasks) {
+    (beforeTasks ? PRE_COMPILE_BEFORE_TASKS_COMPLETED : PRE_COMPILE_AFTER_TASKS_COMPLETED).log(durationMs);
+  }
+
   public static void logBuildCompleted(long durationMs, boolean isRebuild, boolean isAutomake) {
-    (isAutomake? AUTO_BUILD_COMPLETED : isRebuild? REBUILD_COMPLETED : BUILD_COMPLETED).log(durationMs);
+    (isAutomake ? AUTO_BUILD_COMPLETED : isRebuild ? REBUILD_COMPLETED : BUILD_COMPLETED).log(durationMs);
   }
 }
