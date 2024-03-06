214778e4df72ab3958897974d56ffd37689a40b1
Nikita Barkov <nikita.barkov@jetbrains.com>
2024-03-06 14:48:49 +0100

    [perf_tests]AT-818/AT-817. Implemented ExecuteMavenGoalsTest
    GitOrigin-RevId: 5942d87cd24002671aa96080a0417b0c1b93b322

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/performancePlugin/ExecuteMavenGoalCommand.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/performancePlugin/ExecuteMavenGoalCommand.kt
@@ -4,11 +4,15 @@ import com.intellij.execution.ExecutionListener
 import com.intellij.execution.ExecutionManager
 import com.intellij.execution.process.ProcessHandler
 import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.playback.PlaybackContext
 import com.jetbrains.performancePlugin.commands.PerformanceCommand
 import org.jetbrains.concurrency.AsyncPromise
 import org.jetbrains.concurrency.Promise
+import org.jetbrains.idea.maven.execution.MavenRunAnythingProvider
 import org.jetbrains.idea.maven.execution.MavenRunConfigurationType
 import org.jetbrains.idea.maven.model.MavenConstants
 import org.jetbrains.idea.maven.performancePlugin.dto.MavenGoalConfigurationDto
@@ -16,8 +20,10 @@ import org.jetbrains.idea.maven.performancePlugin.utils.MavenConfigurationUtils.
 
 
 /**
- * The command executes a maven goals in module
+ * The command executes a maven goals in module.
  * Argument is serialized [MavenGoalConfigurationDto] as json
+ * runAnything false - executes like double click form lifecycle
+ * runAnything true - executes like double control and execute mvn [goals]
  * @see [MavenConstants.PHASES]
  */
 class ExecuteMavenGoalCommand(text: String, line: Int) : PerformanceCommand(text, line) {
@@ -26,10 +32,23 @@ class ExecuteMavenGoalCommand(text: String, line: Int) : PerformanceCommand(text
     const val PREFIX = "$CMD_PREFIX$NAME"
   }
 
+  private fun executeGoalAsRunAnything(project: Project, settings: MavenGoalConfigurationDto) {
+    val context = SimpleDataContext.builder()
+      .add<Project>(CommonDataKeys.PROJECT, project)
+      .build()
+    MavenRunAnythingProvider().execute(context, "mvn ${settings.goals.joinToString(" ")}")
+  }
+
+  private fun executeGoalsFromLifecycle(project: Project, settings: MavenGoalConfigurationDto) {
+    val params = createRunnerParams(project, settings)
+    MavenRunConfigurationType.runConfiguration(project, params, null)
+  }
+
   override fun _execute(context: PlaybackContext): Promise<Any?> {
+    val project = context.getProject()
     val promise = AsyncPromise<Any?>()
     val settings = deserializeOptionsFromJson(extractCommandArgument(PREFIX), MavenGoalConfigurationDto::class.java)
-    val project = context.getProject()
+
     project.messageBus.connect().subscribe(ExecutionManager.EXECUTION_TOPIC, object : ExecutionListener {
       override fun processNotStarted(executorId: String, env: ExecutionEnvironment, cause: Throwable?) {
         if (cause != null) {
@@ -47,10 +66,13 @@ class ExecuteMavenGoalCommand(text: String, line: Int) : PerformanceCommand(text
       }
     })
 
+
     ApplicationManager.getApplication().invokeLater {
       try {
-        val params = createRunnerParams(project, settings)
-        MavenRunConfigurationType.runConfiguration(project, params, null)
+        if (settings.runAnything)
+          executeGoalAsRunAnything(project, settings)
+        else
+          executeGoalsFromLifecycle(project, settings)
       }
       catch (t: Throwable) {
         promise.setError(t)
