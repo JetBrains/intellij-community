eff2a79ba09bed9edda514cb0fd547236545baa4
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-03-07 16:46:58 +0100

    IJPL-800 QuickDoc: differently colored stripe at the top in same cases
    (cherry picked from commit 2745dd8433ffe08208877bd1baabf85b27b66036)
    
    IJ-CR-128283
    
    GitOrigin-RevId: 139be7fe1f18d7e382409d7819708f61c9c18709

--- platform/lang-impl/src/com/intellij/lang/documentation/ide/ui/DocumentationUI.kt
+++ platform/lang-impl/src/com/intellij/lang/documentation/ide/ui/DocumentationUI.kt
@@ -95,7 +95,9 @@ internal class DocumentationUI(
     }
     scrollPane.setViewportView(editorPane, locationLabel)
     trackDocumentationBackgroundChange(this) {
-      scrollPane.viewport.background = it
+      // Force update of the background color for scroll pane
+      @Suppress("UseJBColor")
+      scrollPane.viewport.background = Color(it.rgb)
       locationLabel.background = it
       switcherToolbarComponent.background = it
     }
@@ -193,7 +195,9 @@ internal class DocumentationUI(
   fun updateSwitcherVisibility() {
     val visible = switcher.elements.count() > 1
     switcherToolbarComponent.isVisible = visible
-    scrollPane.border = JBUI.Borders.emptyTop(if (visible) 5 else 10)
+    editorPane.border = JBUI.Borders.emptyTop(
+      if (visible) 5 else 10
+    )
   }
 
   private suspend fun handleContent(presentation: TargetPresentation, pageContent: DocumentationPageContent?) {
