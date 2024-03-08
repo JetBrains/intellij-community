c64ad1446f396673d52ac80c1cb8072b50f361cf
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2024-03-08 12:23:54 +0100

    [maven] IDEA-343387 refactor static sync tests to separate common static test logic
    GitOrigin-RevId: 1136412b8491a7314ec7b3d05f3fdc152a3c39e5

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenStaticSyncTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenStaticSyncTest.kt
@@ -26,31 +26,8 @@ import org.junit.Test
 import java.io.File
 import java.util.function.Predicate
 
-class MavenStaticSyncTest : MavenMultiVersionImportingTestCase() {
+class MavenStaticSyncTest : AbstractMavenStaticSyncTest() {
 
-  private lateinit var disposable: Disposable
-  override fun setUp() {
-    super.setUp()
-    Assume.assumeTrue(isWorkspaceImport)
-
-    disposable = Disposer.newDisposable("Real maven protector for MavenPreimportingTest")
-    val syncViewManager = object : SyncViewManager(project) {
-      override fun onEvent(buildId: Any, event: BuildEvent) {
-        noRealMavenAllowed()
-      }
-    }
-    project.replaceService(SyncViewManager::class.java, syncViewManager, disposable)
-    ApplicationManager.getApplication().replaceService(MavenServerManager::class.java, NoRealMavenServerManager(), disposable)
-
-
-  }
-
-  override fun tearDown() {
-    runAll(
-      { Disposer.dispose(disposable) },
-      { super.tearDown() }
-    )
-  }
 
   @Test
   fun testImportLibraryDependency() = runBlocking {
@@ -673,66 +650,4 @@ class MavenStaticSyncTest : MavenMultiVersionImportingTestCase() {
     assertModuleModuleDeps("m2", "m1")
 
   }
-
-  override suspend fun importProjectsAsync(files: List<VirtualFile>) {
-    val activity = ProjectImportCollector.IMPORT_ACTIVITY.started(project)
-    try {
-      MavenProjectStaticImporter.getInstance(project)
-        .syncStatic(files, null, mavenImporterSettings, mavenGeneralSettings, true, activity)
-    }
-    finally {
-      activity.finished()
-    }
-  }
-}
-
-class NoRealMavenServerManager : MavenServerManager {
-  override fun dispose() {
-  }
-
-  override fun getAllConnectors(): MutableCollection<MavenServerConnector> {
-    noRealMavenAllowed()
-  }
-
-  override fun restartMavenConnectors(project: Project, wait: Boolean, condition: Predicate<MavenServerConnector>) {
-    noRealMavenAllowed()
-  }
-
-  override fun getConnectorBlocking(project: Project, workingDirectory: String): MavenServerConnector {
-    noRealMavenAllowed()
-  }
-
-  override suspend fun getConnector(project: Project, workingDirectory: String): MavenServerConnector {
-    noRealMavenAllowed()
-  }
-
-  override fun shutdownConnector(connector: MavenServerConnector, wait: Boolean): Boolean {
-    noRealMavenAllowed()
-  }
-
-  override fun closeAllConnectorsAndWait() {
-    noRealMavenAllowed()
-  }
-
-  override fun getMavenEventListener(): File {
-    noRealMavenAllowed()
-  }
-
-  override fun createEmbedder(project: Project, alwaysOnline: Boolean, multiModuleProjectDirectory: String): MavenEmbedderWrapper {
-    noRealMavenAllowed()
-  }
-
-  @Deprecated("Deprecated in Java")
-  override fun createIndexer(project: Project): MavenIndexerWrapper {
-    noRealMavenAllowed()
-  }
-
-  override fun createIndexer(): MavenIndexerWrapper {
-    noRealMavenAllowed()
-  }
-
-}
-
-private fun noRealMavenAllowed(): Nothing {
-  throw IllegalStateException("No real maven in this test class!")
 }
