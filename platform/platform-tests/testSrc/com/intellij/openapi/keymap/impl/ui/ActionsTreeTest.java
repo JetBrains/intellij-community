35a217de9aa19b246e11960373f14cbb3ac80e3f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-03-08 08:28:49 +0100

    ActionsTreeTest - don't check for ActivateProjectToolWindow (empty if a project is disposed, don't fail the test)
    GitOrigin-RevId: dd36311d21b6a8d0d1f3eba7f40906d87f00dc56

--- platform/platform-tests/testSrc/com/intellij/openapi/keymap/impl/ui/ActionsTreeTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/keymap/impl/ui/ActionsTreeTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.keymap.impl.ui;
 
 import com.intellij.diagnostic.PluginException;
@@ -191,7 +191,8 @@ public class ActionsTreeTest {
 
     List<NamedFailure> failures = new ArrayList<>();
     for (String id : ContainerUtil.sorted(manager.getActionIdList(""))) {
-      if (ACTION_WITHOUT_TEXT_AND_DESCRIPTION.equals(id)) {
+      // ActivateProjectToolWindow - empty if a project is disposed, don't fail the test
+      if (ACTION_WITHOUT_TEXT_AND_DESCRIPTION.equals(id) || id.equals("ActivateProjectToolWindow")) {
         continue;
       }
 
