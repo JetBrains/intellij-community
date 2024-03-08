b4d4a34859c1315263b88b9d48b3f3cb5f230475
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2024-03-08 12:32:59 +0100

    [kotlin] KTIJ-28597: scripts highlighting test infrastructure: testData + generated tests
    ^KTIJ-28597 fixed
    
    GitOrigin-RevId: da37b702175dd3fb4264718fb13fba54d86a5dc5

---
+++ plugins/kotlin/idea/tests/testData/gradle/highlighting/gradle7/gradleSampleAndroidApps/settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+    }
+}
+
+rootProject.name = "my-app"
+
+include(":app")
--- plugins/kotlin/idea/tests/testData/gradle/highlighting/javaLibraryPlugin/settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-include("lib")
-
-enableFeaturePreview("VERSION_CATALOGS")
