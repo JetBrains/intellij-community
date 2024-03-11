9afbd467d543bd191e04d232bc41aa8356426901
Tagir Valeev <tagir.valeev@jetbrains.com>
2024-03-11 14:41:57 +0100

    [java-tests] ConcurrentIndexTest: convert from Groovy to Java
    GitOrigin-RevId: 7ca49376a22dbb236139aca7da3166aafa69bbbe

--- java/java-tests/testSrc/com/intellij/java/index/ConcurrentIndexTest.java
+++ java/java-tests/testSrc/com/intellij/java/index/ConcurrentIndexTest.java
@@ -1,208 +1,262 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.java.index
+package com.intellij.java.index;
 
-import com.intellij.lang.FCTSBackedLighterAST
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.ReadAction
-import com.intellij.openapi.command.WriteCommandAction
-import com.intellij.openapi.progress.ProgressIndicator
-import com.intellij.openapi.progress.ProgressManager
-import com.intellij.openapi.progress.Task
-import com.intellij.openapi.progress.impl.CoreProgressManager
-import com.intellij.psi.*
-import com.intellij.psi.impl.PsiDocumentManagerBase
-import com.intellij.psi.impl.search.JavaNullMethodArgumentUtil
-import com.intellij.psi.impl.source.PsiFileImpl
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.testFramework.BombedProgressIndicator
-import com.intellij.testFramework.SkipSlowTestLocally
-import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase
-import com.intellij.util.ref.GCWatcher
-import groovy.transform.CompileStatic
-import org.jetbrains.annotations.NotNull
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.FCTSBackedLighterAST;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.progress.impl.CoreProgressManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiJavaFile;
+import com.intellij.psi.impl.PsiDocumentManagerBase;
+import com.intellij.psi.impl.search.JavaNullMethodArgumentUtil;
+import com.intellij.psi.impl.source.PsiFileImpl;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.testFramework.BombedProgressIndicator;
+import com.intellij.testFramework.SkipSlowTestLocally;
+import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase;
+import com.intellij.util.ref.GCWatcher;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.Future
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 @SkipSlowTestLocally
-@CompileStatic
-class ConcurrentIndexTest extends JavaCodeInsightFixtureTestCase {
+public class ConcurrentIndexTest extends JavaCodeInsightFixtureTestCase {
   @Override
   protected void setUp() throws Exception {
-    super.setUp()
-    ((PsiDocumentManagerBase)PsiDocumentManager.getInstance(project)).disableBackgroundCommit(getProject())
+    super.setUp();
+    ((PsiDocumentManagerBase)PsiDocumentManager.getInstance(getProject())).disableBackgroundCommit(getProject());
   }
 
-  void "test concurrent switching with checkCanceled"() {
-    def N = Math.max(2, (int)(Runtime.runtime.availableProcessors()))
-    def halfN = N / 2
-    for (iteration in 1..200) {
-      def name = "Foo" + iteration
-//      if (iteration % 10 == 0) println "Finding $name"
+  public void test_concurrent_switching_with_checkCanceled() throws ExecutionException, InterruptedException {
+    int N = Math.max(2, Runtime.getRuntime().availableProcessors());
+    int halfN = N / 2;
+    for (int iteration = 1; iteration <= 200; iteration++) {
+      final String name = "Foo" + iteration;
 
-      //noinspection GroovyUnusedAssignment
-      PsiFile file = myFixture.addFileToProject("${name}.java", "class $name {}")
-      List<Future> futuresToWait = []
-      def sameStartCondition = new CountDownLatch(N)
+      myFixture.addFileToProject(name + ".java", "class " + name + " {}");
+      List<Future<?>> futuresToWait = new ArrayList<>();
+      final CountDownLatch sameStartCondition = new CountDownLatch(N);
 
-      for(i in 1.. halfN) {
-        futuresToWait.add(ApplicationManager.application.executeOnPooledThread {
-          sameStartCondition.countDown()
-          sameStartCondition.await()
-          for (j in 0..10) {
-            new BombedProgressIndicator(10).runBombed {
-              ApplicationManager.application.runReadAction {
-                checkFindClass(name)
-              }
-            }
+      for (int i = 1; i <= halfN; i++) {
+        futuresToWait.add(ApplicationManager.getApplication().executeOnPooledThread(() -> {
+          sameStartCondition.countDown();
+          try {
+            sameStartCondition.await();
           }
-        })
+          catch (InterruptedException e) {
+            throw new RuntimeException(e);
+          }
+          for (int j = 0; j <= 10; j++) {
+            new BombedProgressIndicator(10).runBombed(() -> {
+              ApplicationManager.getApplication().runReadAction(() -> checkFindClass(name));
+            });
+          }
+        }));
       }
 
-      for(i in 1..(N - halfN)) {
-        futuresToWait.add(ApplicationManager.application.executeOnPooledThread {
-          sameStartCondition.countDown()
-          sameStartCondition.await()
-          ApplicationManager.application.runReadAction {
-            checkFindClass(name)
+
+      for (int i = 1; i <= N - halfN; i++) {
+        futuresToWait.add(ApplicationManager.getApplication().executeOnPooledThread(() -> {
+          sameStartCondition.countDown();
+          try {
+            sameStartCondition.await();
           }
-        })
+          catch (InterruptedException e) {
+            throw new RuntimeException(e);
+          }
+          ApplicationManager.getApplication().runReadAction(() -> checkFindClass(name));
+        }));
       }
 
-      for(future in futuresToWait) future.get()
+      for (Future<?> future : futuresToWait) future.get();
     }
   }
 
   private void checkFindClass(String name) {
-    PsiClass clazz = myFixture.findClass(name)
-    def node = clazz.node
-    if (node == null) {
-      assert false
-    }
+    PsiClass clazz = myFixture.findClass(name);
+    ASTNode node = clazz.getNode();
+    assertNotNull(node);
   }
 
-  void "test cancellable and non-cancellable progress"() {
-    def N = Math.max(2, (int)(Runtime.runtime.availableProcessors()))
-    def halfN = N / 2
-    PsiFileImpl file = (PsiFileImpl) myFixture.addFileToProject("Foo.java", "class Foo {" + ("public void foo() {}\n") * 1000 + "}")
-    assert myFixture.findClass("Foo").node
+  public void test_cancellable_and_non_cancellable_progress() throws ExecutionException, InterruptedException {
+    int N = Math.max(2, Runtime.getRuntime().availableProcessors());
+    int halfN = N / 2;
+    final PsiFileImpl file = (PsiFileImpl)myFixture.addFileToProject(
+      "Foo.java",
+      "class Foo {" +
+      "public void foo() {}\n".repeat(1000) +
+      "}");
+    assertNotNull(myFixture.findClass("Foo").getNode());
 
-    for (i in 1..20) {
-      println "iteration $i"
-      WriteCommandAction.runWriteCommandAction(project) {
-        ((PsiJavaFile) file).importList.add(JavaPsiFacade.getElementFactory(project).createImportStatementOnDemand("foo.bar$i"))
+    for (int i = 1; i <= 20; i++) {
+      System.out.println("iteration " + i);
+      int finalI = i;
+      WriteCommandAction.runWriteCommandAction(getProject(), () -> {
+        ((PsiJavaFile)file).getImportList()
+          .add(JavaPsiFacade.getElementFactory(getProject()).createImportStatementOnDemand("foo.bar" + finalI));
+      });
+      GCWatcher.tracking(file.getNode()).ensureCollected();
+      assertFalse(file.isContentsLoaded());
+
+      List<Future<?>> futuresToWait = new ArrayList<>();
+      final CountDownLatch sameStartCondition = new CountDownLatch(N);
+
+      for (int j = 1; j <= halfN; j++) {
+        futuresToWait.add(ApplicationManager.getApplication().executeOnPooledThread(() -> {
+          return new BombedProgressIndicator(10).runBombed(() -> {
+            ApplicationManager.getApplication().runReadAction(() -> {
+              sameStartCondition.countDown();
+              try {
+                sameStartCondition.await();
+              }
+              catch (InterruptedException e) {
+                throw new RuntimeException(e);
+              }
+              assertNotNull(myFixture.findClass("Foo").getNode());
+            });
+          });
+        }));
       }
-      GCWatcher.tracking(file.node).ensureCollected()
-      assert !file.contentsLoaded
 
-      List<Future> futuresToWait = []
-      def sameStartCondition = new CountDownLatch(N)
 
-      for(j in 1..halfN) {
-        futuresToWait.add(ApplicationManager.application.executeOnPooledThread {
-          new BombedProgressIndicator(10).runBombed {
-            ApplicationManager.application.runReadAction {
-              sameStartCondition.countDown()
-              sameStartCondition.await()
-              assert myFixture.findClass("Foo").node
+      for (int j = 1; j <= N - halfN; j++) {
+        futuresToWait.add(ApplicationManager.getApplication().executeOnPooledThread(() -> {
+          ApplicationManager.getApplication().runReadAction(() -> {
+            sameStartCondition.countDown();
+            try {
+              sameStartCondition.await();
             }
-          }
-        })
+            catch (InterruptedException e) {
+              throw new RuntimeException(e);
+            }
+            assertNotNull(myFixture.findClass("Foo").getNode());
+          });
+        }));
       }
 
-      for(j in 1..(N - halfN)) {
-        futuresToWait.add(ApplicationManager.application.executeOnPooledThread {
-          ApplicationManager.application.runReadAction {
-            sameStartCondition.countDown()
-            sameStartCondition.await()
-            assert myFixture.findClass("Foo").node
-          }
-        })
-      }
 
-      for(future in futuresToWait) future.get()
+      for (Future<?> future : futuresToWait) future.get();
     }
   }
 
-  void "test forceUpdateAffectsReadOfDataForUnsavedDocuments"() {
-    def N = Math.max(2, (int)(Runtime.runtime.availableProcessors()))
-    def halfN = N / 2
-    PsiFileImpl file = (PsiFileImpl) myFixture.addFileToProject("Foo.java", "class Foo {" + ("public void foo() {}\n") * 1000 + "}")
-    assert myFixture.findClass("Foo").node
+  public void test_forceUpdateAffectsReadOfDataForUnsavedDocuments() throws ExecutionException, InterruptedException {
+    int N = Math.max(2, Runtime.getRuntime().availableProcessors());
+    int halfN = N / 2;
+    final PsiFileImpl file = (PsiFileImpl)myFixture.addFileToProject("Foo.java", "class Foo {" +
+                                                                                 "public void foo() {}\n".repeat(1000) +
+                                                                                 "}");
+    assertNotNull(myFixture.findClass("Foo").getNode());
 
-    for (i in 1..20) {
-      println "iteration $i"
-      WriteCommandAction.runWriteCommandAction(project) {
-        ((PsiJavaFile) file).importList.add(JavaPsiFacade.getElementFactory(project).createImportStatementOnDemand("foo.bar$i"))
+    for (int i = 1; i <= 20; i++) {
+      System.out.println("iteration " + i);
+      int finalI = i;
+      WriteCommandAction.runWriteCommandAction(getProject(), () -> {
+        ((PsiJavaFile)file).getImportList()
+          .add(JavaPsiFacade.getElementFactory(getProject()).createImportStatementOnDemand("foo.bar" + finalI));
+      });
+      GCWatcher.tracking(file.getNode()).ensureCollected();
+      assertFalse(file.isContentsLoaded());
+
+      myFixture.addFileToProject("Foo" + i + ".java",
+                                 "class Foo" + i + " {" + "public void foo() {}\n".repeat(1000) + "}");
+
+      List<Future<?>> futuresToWait = new ArrayList<>();
+      final CountDownLatch sameStartCondition = new CountDownLatch(N);
+
+      for (int j = 1; j <= halfN; j++) {
+        futuresToWait.add(ApplicationManager.getApplication().executeOnPooledThread(() -> {
+          ApplicationManager.getApplication().runReadAction(() -> {
+            sameStartCondition.countDown();
+            try {
+              sameStartCondition.await();
+            }
+            catch (InterruptedException e) {
+              throw new RuntimeException(e);
+            }
+            assertNotNull(myFixture.getJavaFacade().findClass("Foo", GlobalSearchScope.fileScope(file)).getNode());
+          });
+        }));
       }
-      GCWatcher.tracking(file.node).ensureCollected()
-      assert !file.contentsLoaded
 
-      myFixture.addFileToProject("Foo" + i + ".java", "class Foo" + i + " {" + ("public void foo() {}\n") * 1000 + "}")
 
-      List<Future> futuresToWait = []
-      def sameStartCondition = new CountDownLatch(N)
-
-      for(j in 1..halfN) {
-        futuresToWait.add(ApplicationManager.application.executeOnPooledThread {
-          ApplicationManager.application.runReadAction {
-            sameStartCondition.countDown()
-            sameStartCondition.await()
-            assert myFixture.getJavaFacade().findClass("Foo", GlobalSearchScope.fileScope(file)).node
-          }
-        })
+      for (int j = 1; j <= N - halfN; j++) {
+        futuresToWait.add(ApplicationManager.getApplication().executeOnPooledThread(() -> {
+          ApplicationManager.getApplication().runReadAction(() -> {
+            sameStartCondition.countDown();
+            try {
+              sameStartCondition.await();
+            }
+            catch (InterruptedException e) {
+              throw new RuntimeException(e);
+            }
+            assertNotNull(myFixture.findClass("Foo" + finalI).getNode());
+          });
+        }));
       }
 
-      for(j in 1..(N - halfN)) {
-        futuresToWait.add(ApplicationManager.application.executeOnPooledThread {
-          ApplicationManager.application.runReadAction {
-            sameStartCondition.countDown()
-            sameStartCondition.await()
-            assert myFixture.findClass("Foo" + i).node
-          }
-        })
-      }
-
-      for(future in futuresToWait) future.get()
+      for (Future<?> future : futuresToWait) future.get();
     }
   }
 
-  void "test concurrent light AST access during uncommitted document indexing"() {
-    def clazz = myFixture.addClass('class Bar { void foo(Object o) {}}')
+  public void test_concurrent_light_AST_access_during_uncommitted_document_indexing() throws ExecutionException, InterruptedException {
+    final PsiClass clazz = myFixture.addClass("class Bar { void foo(Object o) {}}");
 
-    def text = " foo(null);"
-    for (i in 0..20) {
-      text = "new Runnable() { void run() {\n " + text + "\n}}.run();"
+    String text = " foo(null);";
+    for (int i = 0; i <= 20; i++) {
+      //noinspection StringConcatenationInLoop
+      text = "new Runnable() { void run() {\n " + text + "\n}}.run();";
     }
-    text = "class Foo {{ " + text * 200 + "}}"
 
-    def file = myFixture.addFileToProject('a.java', text) as PsiFileImpl
-    def document = file.viewProvider.document
-    for (i in 1..5) {
-      WriteCommandAction.runWriteCommandAction project, {
-        document.insertString(document.text.indexOf('null') + 1, ' ')
-        document.insertString(document.text.indexOf('(null') + 1, ' ')
-        PsiDocumentManager.getInstance(project).commitAllDocuments()
+    text = "class Foo {{ " + text.repeat(200) + "}}";
+
+    PsiFileImpl file = (PsiFileImpl)myFixture.addFileToProject("a.java", text);
+    final Document document = file.getViewProvider().getDocument();
+    for (int i = 1; i <= 5; i++) {
+      WriteCommandAction.runWriteCommandAction(getProject(), () -> {
+        document.insertString(document.getText().indexOf("null") + 1, " ");
+        document.insertString(document.getText().indexOf("(null") + 1, " ");
+        PsiDocumentManager.getInstance(getProject()).commitAllDocuments();
+      });
+      GCWatcher.tracking(file.getNode()).ensureCollected();
+      assertFalse(file.isContentsLoaded());
+
+      assertTrue(file.getNode().getLighterAST() instanceof FCTSBackedLighterAST);
+      List<Future<?>> futures = new ArrayList<>();
+      futures.add(((CoreProgressManager)ProgressManager.getInstance()).runProcessWithProgressAsynchronously(
+        new Task.Backgroundable(myFixture.getProject(), "hasNull") {
+          @Override
+          public void run(@NotNull ProgressIndicator indicator) {
+            ReadAction.run(() -> {
+              assertFalse(JavaNullMethodArgumentUtil.hasNullArgument(clazz.getMethods()[0], 0));
+              });
+          }
+        }));
+      final Project project = getProject();// https://issues.apache.org/jira/browse/GROOVY-9562
+      futures.add(((CoreProgressManager)ProgressManager.getInstance()).runProcessWithProgressAsynchronously(
+        new Task.Backgroundable(myFixture.getProject(), "findClass") {
+          @Override
+          public void run(@NotNull ProgressIndicator indicator) {
+            ReadAction.run(() -> {
+              assertNotNull(JavaPsiFacade.getInstance(project).findClass("Foo", GlobalSearchScope.allScope(project)));
+              });
+          }
+        }));
+      for (Future<?> future : futures) {
+        future.get();
       }
-      GCWatcher.tracking(file.node).ensureCollected()
-      assert !file.contentsLoaded
-
-      assert file.node.lighterAST instanceof FCTSBackedLighterAST
-      List<Future> futures = []
-      futures << ((CoreProgressManager)ProgressManager.instance).runProcessWithProgressAsynchronously(new Task.Backgroundable(myFixture.project, "hasNull") {
-        @Override
-        void run(@NotNull ProgressIndicator indicator) { ReadAction.run {
-          assert !JavaNullMethodArgumentUtil.hasNullArgument(clazz.methods[0], 0)
-        }}
-      })
-      def project = project // https://issues.apache.org/jira/browse/GROOVY-9562
-      futures << ((CoreProgressManager)ProgressManager.instance).runProcessWithProgressAsynchronously(new Task.Backgroundable(myFixture.project, "findClass") {
-        @Override
-        void run(@NotNull ProgressIndicator indicator) { ReadAction.run {
-          assert JavaPsiFacade.getInstance(project).findClass('Foo', GlobalSearchScope.allScope(project))
-        }}
-      })
-      futures.each { it.get() }
     }
   }
 }
