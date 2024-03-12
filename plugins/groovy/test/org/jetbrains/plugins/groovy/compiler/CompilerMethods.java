290b9280a6126884bf7d623dfe1a33676f638f74
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2024-03-12 13:40:12 +0100

    [groovy] Convert more tests to java
    GitOrigin-RevId: c08a34641693da4bdc27b895a3704108533c3911

--- plugins/groovy/test/org/jetbrains/plugins/groovy/compiler/CompilerMethods.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/compiler/CompilerMethods.java
@@ -1,62 +1,58 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.compiler
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.compiler;
 
-import com.intellij.execution.ExecutionException
-import com.intellij.execution.Executor
-import com.intellij.execution.configurations.RunProfile
-import com.intellij.execution.process.ProcessHandler
-import com.intellij.execution.process.ProcessListener
-import com.intellij.execution.runners.ExecutionEnvironment
-import com.intellij.execution.runners.ExecutionEnvironmentBuilder
-import com.intellij.execution.runners.ProgramRunner
-import com.intellij.execution.ui.RunContentDescriptor
-import com.intellij.openapi.Disposable
-import com.intellij.openapi.project.Project
-import com.intellij.util.concurrency.Semaphore
-import groovy.transform.CompileStatic
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.Executor;
+import com.intellij.execution.configurations.RunProfile;
+import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.process.ProcessListener;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.runners.ExecutionEnvironmentBuilder;
+import com.intellij.execution.runners.ProgramRunner;
+import com.intellij.execution.ui.RunContentDescriptor;
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.extensions.Extensions;
+import com.intellij.util.concurrency.Semaphore;
+import com.intellij.testFramework.EdtTestUtil;
 
-import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.atomic.AtomicReference;
 
-import static com.intellij.testFramework.EdtTestUtil.runInEdtAndWait
+public interface CompilerMethods {
+  Project getProject();
 
-@CompileStatic
-trait CompilerMethods {
-  abstract Project getProject()
+  Disposable disposeOnTearDown(Disposable disposable);
 
-  abstract Disposable disposeOnTearDown(Disposable disposable)
-
-  ProcessHandler runConfiguration(Class<? extends Executor> executorClass,
+  default ProcessHandler runConfiguration(Class<? extends Executor> executorClass,
                                   final ProcessListener listener,
                                   RunProfile configuration,
-                                  ProgramRunner runner = null) throws ExecutionException {
-    final Executor executor = Executor.EXECUTOR_EXTENSION_NAME.findExtension(executorClass)
-    def builder = new ExecutionEnvironmentBuilder(getProject(), executor).runProfile(configuration)
-    if (runner) {
-      builder.runner(runner)
+                                  ProgramRunner runner) throws ExecutionException {
+    final Executor executor = Extensions.findExtension(Executor.EXECUTOR_EXTENSION_NAME, executorClass);
+    ExecutionEnvironmentBuilder builder = new ExecutionEnvironmentBuilder(getProject(), executor).runProfile(configuration);
+    if (runner != null) {
+      builder.runner(runner);
     }
 
-    Semaphore semaphore = new Semaphore()
-    semaphore.down()
-    AtomicReference<ProcessHandler> processHandler = new AtomicReference<ProcessHandler>()
+    Semaphore semaphore = new Semaphore();
+    semaphore.down();
+    AtomicReference<ProcessHandler> processHandler = new AtomicReference<>();
 
-    ExecutionEnvironment environment = builder.build({ RunContentDescriptor descriptor ->
-          if (descriptor == null) {
-            throw new AssertionError((Object)"Null descriptor!")
-          }
-          disposeOnTearDown(descriptor)
-          final ProcessHandler handler = descriptor.getProcessHandler()
-          assert handler != null
-          handler.addProcessListener(listener)
-          processHandler.set(handler)
-          semaphore.up()
-        })
+    ExecutionEnvironment environment = builder.build(descriptor -> {
+      if (descriptor == null) {
+        throw new AssertionError((Object)"Null descriptor!");
+      }
+      disposeOnTearDown(descriptor);
+      final ProcessHandler handler = descriptor.getProcessHandler();
+      assert handler != null;
+      handler.addProcessListener(listener);
+      processHandler.set(handler);
+      semaphore.up();
+    });
 
-    runInEdtAndWait {
-      environment.runner.execute(environment)
-    }
+    EdtTestUtil.runInEdtAndWait(() -> environment.getRunner().execute(environment));
     if (!semaphore.waitFor(20000)) {
-      throw new AssertionError((Object)"Process took too long")
+      throw new AssertionError("Process took too long");
     }
-    return processHandler.get()
+    return processHandler.get();
   }
 }
