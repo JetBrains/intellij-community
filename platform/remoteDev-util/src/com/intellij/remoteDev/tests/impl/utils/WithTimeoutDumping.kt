2e4543df69873675115d1193a6dbe325e5ad2384
Ilya.Usov <Ilya.Usov@jetbrains.com>
2024-03-12 13:47:15 +0100

    Improve withTimeoutDumping
    1) fix warnings (use namedChildScope)
    2) use blockingDispatcher to make sure this assert will work even in case Dispatchers.IO is starving
    3) print full coroutine dump and thread dump to stdout
    
    GitOrigin-RevId: e6337666d421425cc5f3b4e7b5e13e9110eb7fa5

--- platform/remoteDev-util/src/com/intellij/remoteDev/tests/impl/utils/WithTimeoutDumping.kt
+++ platform/remoteDev-util/src/com/intellij/remoteDev/tests/impl/utils/WithTimeoutDumping.kt
@@ -2,10 +2,11 @@ package com.intellij.remoteDev.tests.impl
 
 import com.intellij.diagnostic.ThreadDumper
 import com.intellij.diagnostic.dumpCoroutines
+import com.intellij.platform.util.coroutines.namedChildScope
+import com.intellij.util.io.blockingDispatcher
 import kotlinx.coroutines.*
 import org.jetbrains.annotations.ApiStatus
 import java.util.concurrent.TimeoutException
-import kotlin.coroutines.coroutineContext
 import kotlin.time.Duration
 
 @ApiStatus.Internal
@@ -18,34 +19,41 @@ val threadsDumpPrefix = "ThreadsDump:"
 suspend fun <T> withTimeoutDumping(title: String,
                                    timeout: Duration,
                                    failMessageProducer: (() -> String)? = null,
-                                   action: suspend () -> T): T {
-  val outerScope = CoroutineScope(coroutineContext + CoroutineName(title))
+                                   action: suspend () -> T): T = coroutineScope {
+  val outerScope = namedChildScope(title)
 
   val deferred = outerScope.async { action() }
-  coroutineScope {
-    launch(Dispatchers.IO) {
-      try {
-        withTimeout(timeout) { deferred.await() }
-      }
-      catch (e: TimeoutCancellationException) {
-        val coroutinesDump = dumpCoroutines(outerScope)
-        val threadsDump = ThreadDumper.dumpThreadsToString()
-        deferred.cancel(e)
-        throw TimeoutException(buildString {
-          append("$title: Has not finished in $timeout.\n")
-          val failMessage = failMessageProducer?.invoke()
-          if (!failMessage.isNullOrBlank()) {
-            append("$failMessage\n")
-          }
-          append("------------\n")
-          append("$coroutineDumpPrefix\n")
-          append("$coroutinesDump\n")
-          append("$threadsDumpPrefix\n")
-          append("$threadsDump\n")
-          append("------------")
-        })
-      }
+  @OptIn(DelicateCoroutinesApi::class)
+  withContext(blockingDispatcher) {
+    try {
+      withTimeout(timeout) { deferred.await() }
     }
+    catch (e: TimeoutCancellationException) {
+      val message = "$title: Has not finished in $timeout."
+
+      println(buildString {
+        appendLine(message)
+        // see com.intellij.testFramework.common.TimeoutKt.timeoutRunBlocking
+        appendLine(ThreadDumper.getThreadDumpInfo(ThreadDumper.getThreadInfos(), false).rawDump)
+      })
+
+      val coroutinesDump = dumpCoroutines(outerScope)
+      val threadsDump = ThreadDumper.dumpThreadsToString()
+      throw TimeoutException(buildString {
+        appendLine(message)
+        val failMessage = failMessageProducer?.invoke()
+        if (!failMessage.isNullOrBlank()) {
+          appendLine(failMessage)
+        }
+        appendLine("------------")
+        appendLine(coroutineDumpPrefix)
+        appendLine(coroutinesDump)
+        appendLine(threadsDumpPrefix)
+        appendLine(threadsDump)
+        append("------------")
+      })
+    }
+  }.also {
+    outerScope.cancel()
   }
-  return deferred.await()
 }
