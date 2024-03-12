79b5112e3c7210c849236e1448e8e3ef3f7574a6
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2024-03-12 17:35:27 +0200

    [terminal] IDEA-348915 Create feedback dialog for the New Terminal survey
    (cherry picked from commit 53439a5d79eea6b8df9d21dbae768fabd02a435c)
    
    IJ-CR-130113
    
    GitOrigin-RevId: b75f798a8431399c4b621ec130299a6ad6d3006e

---
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/exp/feedback/BlockTerminalFeedbackSurvey.kt
@@ -0,0 +1,52 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.terminal.exp.feedback
+
+import com.intellij.openapi.project.Project
+import com.intellij.platform.feedback.FeedbackSurvey
+import com.intellij.platform.feedback.FeedbackSurveyType
+import com.intellij.platform.feedback.InIdeFeedbackSurveyConfig
+import com.intellij.platform.feedback.InIdeFeedbackSurveyType
+import com.intellij.platform.feedback.dialog.BlockBasedFeedbackDialog
+import com.intellij.platform.feedback.dialog.SystemDataJsonSerializable
+import com.intellij.platform.feedback.impl.notification.RequestFeedbackNotification
+import com.intellij.util.PlatformUtils
+import kotlinx.datetime.LocalDate
+import org.jetbrains.plugins.terminal.TerminalBundle
+
+internal class BlockTerminalFeedbackSurvey : FeedbackSurvey() {
+  override val feedbackSurveyType: FeedbackSurveyType<*> = InIdeFeedbackSurveyType(BlockTerminalSurveyConfig())
+}
+
+internal class BlockTerminalSurveyConfig : InIdeFeedbackSurveyConfig {
+  override val surveyId: String = "new_terminal"
+
+  override fun createFeedbackDialog(project: Project, forTest: Boolean): BlockBasedFeedbackDialog<out SystemDataJsonSerializable> {
+    return BlockTerminalFeedbackDialog(project, forTest)
+  }
+
+  override fun updateStateAfterDialogClosedOk(project: Project) {
+    // do nothing
+  }
+
+  // Last date is an approximate time of 2024.2 release
+  override val lastDayOfFeedbackCollection: LocalDate = LocalDate(2024, 8, 1)
+
+  override val requireIdeEAP: Boolean = false
+
+  override fun checkIdeIsSuitable(): Boolean = PlatformUtils.isJetBrainsProduct()
+
+  override fun checkExtraConditionSatisfied(project: Project): Boolean {
+    // todo: check that user executed some number of the commands in the new terminal
+    return true
+  }
+
+  override fun createNotification(project: Project, forTest: Boolean): RequestFeedbackNotification {
+    return RequestFeedbackNotification("Feedback In IDE",
+                                       TerminalBundle.message("feedback.notification.title"),
+                                       TerminalBundle.message("feedback.notification.text"))
+  }
+
+  override fun updateStateAfterNotificationShowed(project: Project) {
+    // do nothing
+  }
+}
