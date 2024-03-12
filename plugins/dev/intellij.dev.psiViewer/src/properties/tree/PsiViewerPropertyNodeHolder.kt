fad5b08ee4d0df27aeb99ce1e37dcf8d15b285f8
Mikhail Shagvaliev <mikhail.shagvaliev@jetbrains.com>
2024-03-12 10:21:04 +0100

    [qodana] QD-8215 FlexInspect: Kotlin analysis api
    - do not depend on fe10, instead use and provide in PSI viewer analysis api
    
    (cherry picked from commit 4f1239680306a0d12b9d9406db0a47cfe8d762de)
    (cherry picked from commit 222039d3f365fa3c7551beed0d3fb7a9ae89cfb8)
    
    IJ-CR-129454
    
    GitOrigin-RevId: cf63adf07b322dab5c23e593b910a347943fca3d

--- plugins/dev/intellij.dev.psiViewer/src/properties/tree/PsiViewerPropertyNodeHolder.kt
+++ plugins/dev/intellij.dev.psiViewer/src/properties/tree/PsiViewerPropertyNodeHolder.kt
@@ -1,35 +1,50 @@
 package com.intellij.dev.psiViewer.properties.tree
 
+import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.progress.checkCancelled
 import com.intellij.openapi.vcs.changes.issueLinks.TreeLinkMouseListener
 import kotlinx.coroutines.*
 
 class PsiViewerPropertyNodeHolder(
   val node: PsiViewerPropertyNode,
+  private val nodeContext: PsiViewerPropertyNode.Context,
   private val depth: Int,
   private val scope: CoroutineScope
 ) : TreeLinkMouseListener.IsLeafProvider {
   val childrenListAsync: Deferred<List<PsiViewerPropertyNodeHolder>> = childrenListAsync()
 
   private fun childrenListAsync(): Deferred<List<PsiViewerPropertyNodeHolder>> {
-    return when(val children = node.children) {
-      is PsiViewerPropertyNode.Children.Enumeration -> {
-        CompletableDeferred(children.childrenList.map { childNodeHolder(it) })
-      }
-      is PsiViewerPropertyNode.Children.Async -> {
-        scope.async(Dispatchers.Default, start = CoroutineStart.LAZY) {
-          checkCancelled()
-          if (depth > 100) return@async emptyList()
-
-          children.computeChildren().map { childNodeHolder(it) }
+    return scope.async(Dispatchers.Default, start = CoroutineStart.LAZY) {
+      checkCancelled()
+      val mainChildren = async {
+        when (val children = node.children) {
+          is PsiViewerPropertyNode.Children.Enumeration -> {
+            children.childrenList
+          }
+          is PsiViewerPropertyNode.Children.Async -> {
+            if (depth > 100) emptyList() else children.computeChildren()
+          }
         }
       }
+      val additionalChildren: Deferred<List<PsiViewerPropertyNode>> = async {
+        PsiViewerPropertyNodeAppender.EP_NAME.extensionList.flatMap { it.appendChildren(nodeContext, parent = node) }
+      }
+      val allChildren = mainChildren.await() + additionalChildren.await()
+      allChildren.map { childNodeHolder(it) }
     }
   }
 
   private fun childNodeHolder(childNode: PsiViewerPropertyNode): PsiViewerPropertyNodeHolder {
-    return PsiViewerPropertyNodeHolder(childNode, depth + 1, scope)
+    return PsiViewerPropertyNodeHolder(childNode, nodeContext, depth + 1, scope)
   }
 
   override fun isLeaf(): Boolean = node.isLeaf
+}
+
+interface PsiViewerPropertyNodeAppender {
+  companion object {
+    val EP_NAME = ExtensionPointName<PsiViewerPropertyNodeAppender>("com.intellij.dev.psiViewer.propertyNodeAppender")
+  }
+
+  suspend fun appendChildren(nodeContext: PsiViewerPropertyNode.Context, parent: PsiViewerPropertyNode): List<PsiViewerPropertyNode>
 }
