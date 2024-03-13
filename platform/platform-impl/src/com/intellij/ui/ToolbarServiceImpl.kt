2cb2d265ea5b9e948569dd0298e623af2ac09428
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2024-03-13 14:22:08 +0100

    IDEA-344208 Paint background image on Dialog header
    GitOrigin-RevId: 6354392a87014fedfbf88fc9325b068fa75f8c14

--- platform/platform-impl/src/com/intellij/ui/ToolbarServiceImpl.kt
+++ platform/platform-impl/src/com/intellij/ui/ToolbarServiceImpl.kt
@@ -4,10 +4,12 @@ package com.intellij.ui
 import com.intellij.openapi.util.SystemInfoRt
 import com.intellij.ui.paint.LinePainter2D
 import com.intellij.util.ui.JBInsets
+import com.intellij.util.ui.JBSwingUtilities
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
 import java.awt.*
 import java.beans.PropertyChangeListener
+import javax.swing.JComponent
 import javax.swing.JRootPane
 import javax.swing.RootPaneContainer
 import javax.swing.UIManager
@@ -49,7 +51,10 @@ open class ToolbarServiceImpl : ToolbarService {
           return
         }
 
-        val graphics = g.create() as Graphics2D
+        val graphics = (g.create() as Graphics2D).let {
+          if (c is JComponent) JBSwingUtilities.runGlobalCGTransform(c, it) else it
+        }
+
         try {
           val headerRectangle = Rectangle(0, 0, c.width, topWindowInset.top)
           graphics.color = JBColor.PanelBackground
