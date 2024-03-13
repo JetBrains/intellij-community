3de87f77bb11cb2e412d5224136689f425566746
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-03-13 20:54:25 +0100

    CodeVisionInitializerStartupActivity - use serviceAsync
    GitOrigin-RevId: 958298d243849007d6031789ab0b0c1dd10f58c2

--- platform/lang-impl/src/com/intellij/codeInsight/codeVision/CodeVisionInitializer.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/codeVision/CodeVisionInitializer.kt
@@ -1,8 +1,9 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.codeVision
 
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.components.service
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.progress.blockingContext
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
@@ -23,7 +24,7 @@ open class CodeVisionInitializer(project: Project) {
 
   internal class CodeVisionInitializerStartupActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
-      val visionInitializer = getInstance(project)
+      val visionInitializer = project.serviceAsync<CodeVisionInitializer>()
       withContext(Dispatchers.EDT) {
         blockingContext {
           visionInitializer.host.initialize()
