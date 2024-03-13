df3bd0ff5dc5a43fe9a87f80f6f8578003b46818
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-03-13 17:53:19 +0100

    [Groovy] convert GrSuppressionTest to Java (IDEA-327339)
    GitOrigin-RevId: 27288277382636e3904933b7ddcc6768fbb1cf3e

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrSuppressionTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrSuppressionTest.java
@@ -1,76 +1,74 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.lang.highlighting
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.highlighting;
 
-import com.intellij.analysis.AnalysisBundle
-import com.intellij.spellchecker.inspections.SpellCheckingInspection
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.util.GroovyLatestTest
-import org.jetbrains.plugins.groovy.util.HighlightingTest
-import org.junit.Test
+import com.intellij.analysis.AnalysisBundle;
+import com.intellij.spellchecker.inspections.SpellCheckingInspection;
+import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture;
+import org.jetbrains.plugins.groovy.util.GroovyLatestTest;
+import org.jetbrains.plugins.groovy.util.HighlightingTest;
+import org.junit.Test;
 
-@SuppressWarnings('SpellCheckingInspection')
-@CompileStatic
-class GrSuppressionTest extends GroovyLatestTest implements HighlightingTest {
-
+public class GrSuppressionTest extends GroovyLatestTest implements HighlightingTest {
   private void doTest(String before, String after) {
-    fixture.enableInspections SpellCheckingInspection
-    configureByText before
-    fixture.checkHighlighting()
-    fixture.launchAction fixture.findSingleIntention(AnalysisBundle.message("suppress.inspection.file"))
-    fixture.checkResult after
+    JavaCodeInsightTestFixture fixture = getFixture();
+    fixture.enableInspections(SpellCheckingInspection.class);
+    configureByText(before);
+    fixture.checkHighlighting();
+    fixture.launchAction(fixture.findSingleIntention(AnalysisBundle.message("suppress.inspection.file")));
+    fixture.checkResult(after);
   }
 
   @Test
-  void 'suppress by file level comment'() {
-    doTest '''\
+  public void suppressByFileLevelComment() {
+    doTest("""
 
 
 
-println("<TYPO><caret>abcdef</TYPO>")
-''', '''\
-//file:noinspection SpellCheckingInspection
+             println("<TYPO><caret>abcdef</TYPO>")
+             """, """
+             //file:noinspection SpellCheckingInspection
 
 
 
-println("abcdef")
-'''
+             println("abcdef")
+             """);
   }
 
   @Test
-  void 'suppress by file level comment after another comment'() {
-    doTest '''\
-/* some other comment */
+  public void suppressByFileLevelCommentAfterAnotherComment() {
+    doTest("""
+             /* some other comment */
 
 
 
-println("<TYPO><caret>abcdef</TYPO>")
-''', '''\
-/* some other comment */
-//file:noinspection SpellCheckingInspection
+             println("<TYPO><caret>abcdef</TYPO>")
+             """, """
+             /* some other comment */
+             //file:noinspection SpellCheckingInspection
 
 
 
-println("abcdef")
-'''
+             println("abcdef")
+             """);
   }
 
   @Test
-  void 'suppress by file level comment after hash bang'() {
-    doTest '''\
-#!/usr/bin/env groovy
-// some other comment
+  public void suppressByFileLevelCommentAfterHashBang() {
+    doTest("""
+             #!/usr/bin/env groovy
+             // some other comment
 
 
 
-println("<TYPO><caret>abcdef</TYPO>")
-''', '''\
-#!/usr/bin/env groovy
-// some other comment
-//file:noinspection SpellCheckingInspection
+             println("<TYPO><caret>abcdef</TYPO>")
+             """, """
+             #!/usr/bin/env groovy
+             // some other comment
+             //file:noinspection SpellCheckingInspection
 
 
 
-println("abcdef")
-'''
+             println("abcdef")
+             """);
   }
 }
