3fa3c38561b8cf4ed4027dbac0e60122dd4397ae
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-03-13 17:16:20 +0100

    [Groovy] convert GrUnassignedVariableAccessTest to Java (IDEA-327339)
    GitOrigin-RevId: 4c9843a7692d55aea5284b82b088b93a3c904e4b

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrUnassignedVariableAccessTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrUnassignedVariableAccessTest.java
@@ -1,96 +1,99 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.groovy.lang.highlighting
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.highlighting;
 
-import com.intellij.codeInspection.InspectionProfileEntry
-import com.intellij.testFramework.LightProjectDescriptor
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.codeInspection.unassignedVariable.UnassignedVariableAccessInspection
+import com.intellij.codeInspection.InspectionProfileEntry;
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.codeInspection.unassignedVariable.UnassignedVariableAccessInspection;
 
 /**
  * @author Max Medvedev
  */
-class GrUnassignedVariableAccessTest extends GrHighlightingTestBase {
+public class GrUnassignedVariableAccessTest extends GrHighlightingTestBase {
+  @Override
+  public InspectionProfileEntry[] getCustomInspections() {
+    return new InspectionProfileEntry[]{new UnassignedVariableAccessInspection()};
+  }
 
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_3_0
+  public void testUnassigned1() { doTest(); }
+
+  public void testUnassigned2() { doTest(); }
+
+  public void testUnassigned3() { doTest(); }
+
+  public void testUnassigned4() { doTest(); }
+
+  public void testUnassignedTryFinally() { doTest(); }
+
+  public void testVarIsNotInitialized() {
+    doTestHighlighting("""
+                         def xxx() {
+                           def category = null
+                           for (def update : updateIds) {
+                             def p = update
+
+                             if (something) {
+                               category = p
+                             }
+
+                             print p
+                           }
+                         }
+                         """);
+  }
+
+  public void testSimple() {
+    doTestHighlighting("""
+                         def bar() {
+                           def p
+                           print <warning descr="Variable 'p' might not be assigned">p</warning>
+                         }
+                         """);
+  }
+
+  public void testAssignedAfterReadInLoop() {
+    doTestHighlighting("""
+                         def xxx() {
+                           def p
+                           for (def update : updateIds) {
+                             print <warning descr="Variable 'p' might not be assigned">p</warning>
+                             p = 1\s
+                           }
+                         }
+                         """);
+  }
+
+  public void testUnassignedAccessInCheck() {
+    UnassignedVariableAccessInspection inspection = new UnassignedVariableAccessInspection();
+    inspection.myIgnoreBooleanExpressions = true;
+
+    myFixture.configureByText("_.groovy", """
+      def foo
+      if (foo) print 'fooo!!!'
+
+      def bar
+      if (bar!=null) print 'foo!!!'
+
+      def baz
+      if (<warning descr="Variable 'baz' might not be assigned">baz</warning> + 2) print "fooooo!"
+      """);
+    myFixture.enableInspections(inspection);
+    myFixture.testHighlighting(true, false, true);
+  }
+
+  public void testVarNotAssigned() { doTest(); }
+
+  public void testMultipleVarNotAssigned() { doTest(); }
+
+  public void testForLoopWithNestedEndlessLoop() { doTest(); }
+
+  public void testVariableAssignedOutsideForLoop() { doTest(); }
+
+  public void testTryResource() { doTest(); }
 
   @Override
-  InspectionProfileEntry[] getCustomInspections() {
-    [new UnassignedVariableAccessInspection()] as InspectionProfileEntry[]
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_3_0;
   }
-
-  void testUnassigned1() { doTest() }
-
-  void testUnassigned2() { doTest() }
-
-  void testUnassigned3() { doTest() }
-
-  void testUnassigned4() { doTest() }
-
-  void testUnassignedTryFinally() { doTest() }
-
-  void testVarIsNotInitialized() {
-    doTestHighlighting '''\
-def xxx() {
-  def category = null
-  for (def update : updateIds) {
-    def p = update
-
-    if (something) {
-      category = p
-    }
-
-    print p
-  }
-}
-'''
-  }
-
-  void 'test simple'() {
-    doTestHighlighting '''\
-def bar() {
-  def p
-  print <warning descr="Variable 'p' might not be assigned">p</warning>
-}
-'''
-  }
-
-  void 'test assigned after read in loop'() {
-    doTestHighlighting '''\
-def xxx() {
-  def p
-  for (def update : updateIds) {
-    print <warning descr="Variable 'p' might not be assigned">p</warning>
-    p = 1 
-  }
-}
-'''
-  }
-
-  void testUnassignedAccessInCheck() {
-    def inspection = new UnassignedVariableAccessInspection()
-    inspection.myIgnoreBooleanExpressions = true
-
-    myFixture.configureByText('_.groovy', '''\
-def foo
-if (foo) print 'fooo!!!'
-
-def bar
-if (bar!=null) print 'foo!!!'
-
-def baz
-if (<warning descr="Variable 'baz' might not be assigned">baz</warning> + 2) print "fooooo!"
-''')
-    myFixture.enableInspections(inspection)
-    myFixture.testHighlighting(true, false, true)
-  }
-
-  void testVarNotAssigned() { doTest() }
-
-  void testMultipleVarNotAssigned() { doTest() }
-
-  void testForLoopWithNestedEndlessLoop() { doTest() }
-
-  void testVariableAssignedOutsideForLoop() { doTest() }
-
-  void testTryResource() { doTest() }
 }
