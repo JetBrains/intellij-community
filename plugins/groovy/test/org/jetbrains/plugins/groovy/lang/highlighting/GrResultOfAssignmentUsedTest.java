a1bb299ae5757a6ed5c1dfcca205a95e27944bbe
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-03-13 20:31:28 +0100

    [java] convert GrResultOfAssignmentUsedTest to Java (IDEA-331682)
    GitOrigin-RevId: 9578b60148157831674048071735e104c4858dad

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrResultOfAssignmentUsedTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrResultOfAssignmentUsedTest.java
@@ -1,88 +1,88 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.groovy.lang.highlighting
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.highlighting;
 
-import com.intellij.codeInspection.InspectionProfileEntry
-import org.jetbrains.plugins.groovy.codeInspection.assignment.GroovyResultOfAssignmentUsedInspection
+import com.intellij.codeInspection.InspectionProfileEntry;
+import org.jetbrains.plugins.groovy.codeInspection.assignment.GroovyResultOfAssignmentUsedInspection;
 
-class GrResultOfAssignmentUsedTest extends GrHighlightingTestBase {
-  final inspection = new GroovyResultOfAssignmentUsedInspection()
+public class GrResultOfAssignmentUsedTest extends GrHighlightingTestBase {
+
+  private final GroovyResultOfAssignmentUsedInspection inspection = new GroovyResultOfAssignmentUsedInspection();
 
   @Override
-  InspectionProfileEntry[] getCustomInspections() { [inspection] }
-
-  final warnStart = '<warning descr="Usage of assignment expression result">'
-  final warnEnd = '</warning>'
-
-  void testUsedVar() {
-    doTestHighlighting """
-      def foo(a) {
-        if ((${warnStart}a = 5${warnEnd}) || a) {
-          ${warnStart}a = 4${warnEnd}
-        }
-      }
-
-      def foo2(a) {
-        def b = 'b'
-        if (!a) {
-          println b
-          b = 5
-        }                            
-        return 0 // make b = 5 not a return statement
-      }
-
-      def bar(a) {
-        print ((${warnStart}a = 5${warnEnd})?:a)
-      }
-
-      def a(b) {
-        if (2 && (${warnStart}b = 5${warnEnd})) {
-          b
-        }
-      }
-    """
+  public InspectionProfileEntry[] getCustomInspections() {
+    return new InspectionProfileEntry[]{inspection};
   }
 
-  void testResultOfAssignmentUsedInspection() {
-    doTestHighlighting """
-      if ((${warnStart}a = b${warnEnd}) == null) {
-      }
-    """
+  public void testUsedVar() {
+    doTestHighlighting("""
+                         def foo(a) {
+                           if ((<warning descr="Usage of assignment expression result">a = 5</warning>) || a) {
+                             <warning descr="Usage of assignment expression result">a = 4</warning>
+                           }
+                         }
 
-    doTestHighlighting """
-      while (${warnStart}a = b${warnEnd}) {
-      }
-    """
+                         def foo2(a) {
+                           def b = 'b'
+                           if (!a) {
+                             println b
+                             b = 5
+                           }
+                           return 0 // make b = 5 not a return statement
+                         }
 
-    doTestHighlighting """
-      for (i = 0; ${warnStart}a = b${warnEnd}; i++) {
-      }
-    """
+                         def bar(a) {
+                           print ((<warning descr="Usage of assignment expression result">a = 5</warning>)?:a)
+                         }
 
-    doTestHighlighting """
-      System.out.println(${warnStart}a = b${warnEnd})
-    """
-
-    doTestHighlighting """
-      (${warnStart}a = b${warnEnd}).each {}
-    """
+                         def a(b) {
+                           if (2 && (<warning descr="Usage of assignment expression result">b = 5</warning>)) {
+                             b
+                           }
+                         }
+                       """);
   }
 
-  void testInspectClosuresOption_isTrue() {
-    inspection.inspectClosures = true
-    doTestHighlighting 'a = 1 '
-    doTestHighlighting "a(0, { ${warnStart}b = 1${warnEnd} }, 2)"
-    doTestHighlighting "def a = { ${warnStart}b = 1${warnEnd} }"
-    doTestHighlighting "a(0, ${warnStart}b = 1${warnEnd}, 2)"
-    doTestHighlighting "def a() { ${warnStart}b = 1${warnEnd} }"
-    inspection.inspectClosures = false
+  public void testResultOfAssignmentUsedInspection() {
+    doTestHighlighting("""
+                         if ((<warning descr="Usage of assignment expression result">a = b</warning>) == null) {
+                         }
+                       """);
+
+    doTestHighlighting("""
+                         while (<warning descr="Usage of assignment expression result">a = b</warning>) {
+                         }
+                       """);
+
+    doTestHighlighting("""
+                         for (i = 0;<warning descr="Usage of assignment expression result">a = b</warning>; i++) {
+                         }
+                       """);
+
+    doTestHighlighting("""
+                         System.out.println(<warning descr="Usage of assignment expression result">a = b</warning>)
+                       """);
+
+    doTestHighlighting("""
+                         (<warning descr="Usage of assignment expression result">a = b</warning>                         ).each {}
+                       """);
   }
 
-  void testInspectClosuresOption_isFalse() {
-    inspection.inspectClosures = false
-    doTestHighlighting 'a = 1 '
-    doTestHighlighting 'a(0, { b = 1 }, 2)'
-    doTestHighlighting "def a = { b = 1 }"
-    doTestHighlighting "a(0, ${warnStart}b = 1${warnEnd}, 2)"
-    doTestHighlighting "def a() { ${warnStart}b = 1${warnEnd} }"
+  public void testInspectClosuresOptionIsTrue() {
+    inspection.inspectClosures = true;
+    doTestHighlighting("a = 1 ");
+    doTestHighlighting("a(0, { <warning descr=\"Usage of assignment expression result\">b = 1</warning> }, 2)");
+    doTestHighlighting("def a = { <warning descr=\"Usage of assignment expression result\">b = 1</warning> }");
+    doTestHighlighting("a(0, <warning descr=\"Usage of assignment expression result\">b = 1</warning>, 2)");
+    doTestHighlighting("def a() { <warning descr=\"Usage of assignment expression result\">b = 1</warning> }");
+    inspection.inspectClosures = false;
+  }
+
+  public void testInspectClosuresOptionIsFalse() {
+    inspection.inspectClosures = false;
+    doTestHighlighting("a = 1 ");
+    doTestHighlighting("a(0, { b = 1 }, 2)");
+    doTestHighlighting("def a = { b = 1 }");
+    doTestHighlighting("a(0, <warning descr=\"Usage of assignment expression result\">b = 1</warning>, 2)");
+    doTestHighlighting("def a() { <warning descr=\"Usage of assignment expression result\">b = 1</warning> }");
   }
 }
