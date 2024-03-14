b7bd624d852b5a4fddb56cdf1b49bed35ad435f0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-03-14 18:54:14 +0100

    JvmDebuggerAddFilterStartupActivity - use serviceAsync
    GitOrigin-RevId: 2c5f8cc4a62e52838dea6708c1ebd09d7f8c7bb3

--- plugins/kotlin/jvm-debugger/core/src/org/jetbrains/kotlin/idea/debugger/core/filter/JvmDebuggerAddFilterStartupActivity.kt
+++ plugins/kotlin/jvm-debugger/core/src/org/jetbrains/kotlin/idea/debugger/core/filter/JvmDebuggerAddFilterStartupActivity.kt
@@ -1,8 +1,9 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.debugger.core.filter
 
 import com.intellij.debugger.settings.DebuggerSettings
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.ui.classFilter.ClassFilter
@@ -13,24 +14,24 @@ private const val COMPOSE_RUNTIME_FILTER = "androidx.compose.runtime.*"
 
 private class JvmDebuggerAddFilterStartupActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
-        val settings = DebuggerSettings.getInstance() ?: return
-        settings.addSteppingFilterIfNeeded(KOTLIN_STDLIB_FILTER)
-        settings.addSteppingFilterIfNeeded(KOTLINX_FILTER)
-        settings.addSteppingFilterIfNeeded(COMPOSE_RUNTIME_FILTER)
+        val settings = serviceAsync<DebuggerSettings>()
+        addSteppingFilterIfNeeded(settings, KOTLIN_STDLIB_FILTER)
+        addSteppingFilterIfNeeded(settings, KOTLINX_FILTER)
+        addSteppingFilterIfNeeded(settings, COMPOSE_RUNTIME_FILTER)
     }
 }
 
-private fun DebuggerSettings.addSteppingFilterIfNeeded(pattern: String) {
-    val steppingFilters = this.steppingFilters
+private fun addSteppingFilterIfNeeded(debuggerSettings: DebuggerSettings, pattern: String) {
+    val steppingFilters = debuggerSettings.steppingFilters
     when (val occurrencesNum = steppingFilters.count { it.pattern == pattern }) {
-        0 -> setSteppingFilters(steppingFilters + ClassFilter(pattern))
+        0 -> debuggerSettings.steppingFilters = steppingFilters + ClassFilter(pattern)
         1 -> return
-        else -> leaveOnlyFirstOccurenceOfSteppingFilter(pattern, occurrencesNum)
+        else -> leaveOnlyFirstOccurenceOfSteppingFilter(debuggerSettings, pattern, occurrencesNum)
     }
 }
 
-private fun DebuggerSettings.leaveOnlyFirstOccurenceOfSteppingFilter(pattern: String, occurrencesNum: Int) {
-    val steppingFilters = this.steppingFilters
+private fun leaveOnlyFirstOccurenceOfSteppingFilter(debuggerSettings: DebuggerSettings, pattern: String, occurrencesNum: Int) {
+    val steppingFilters = debuggerSettings.steppingFilters
     val newFilters = ArrayList<ClassFilter>(steppingFilters.size - occurrencesNum + 1)
 
     var firstOccurrenceFound = false
@@ -45,5 +46,5 @@ private fun DebuggerSettings.leaveOnlyFirstOccurenceOfSteppingFilter(pattern: St
         }
     }
 
-    setSteppingFilters(newFilters.toTypedArray())
+    debuggerSettings.steppingFilters = newFilters.toTypedArray()
 }
