251273a8bfb3b65cf69e5fc89e9b31c498c3de20
eugene.nizienko <eugene.nizienko@jetbrains.com>
2024-03-14 18:09:37 +0100

    [remote-driver] save UI hierarchy whith driverError screenshot
    GitOrigin-RevId: 3eb2379ede2e9aad5cbe237cfd71654daee998a7

--- platform/remote-driver/core/src/com/intellij/driver/impl/Invoker.java
+++ platform/remote-driver/core/src/com/intellij/driver/impl/Invoker.java
@@ -38,7 +38,6 @@ import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
