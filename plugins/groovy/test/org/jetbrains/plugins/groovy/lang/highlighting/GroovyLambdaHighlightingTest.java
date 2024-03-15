f4950dc39df3a3ddbf2afb67f19b8aa10ea026e0
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-03-15 15:40:49 +0100

    [Groovy] convert GroovyLambdaHighlightingTest to Java (IDEA-327339)
    GitOrigin-RevId: 39a9054c104db76e77599386616b26c10a89d113

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovyLambdaHighlightingTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovyLambdaHighlightingTest.java
@@ -1,128 +1,139 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.lang.highlighting
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.highlighting;
 
-import com.intellij.codeInspection.InspectionProfileEntry
-import com.intellij.testFramework.LightProjectDescriptor
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.codeInspection.assignment.GroovyAssignabilityCheckInspection
-import org.jetbrains.plugins.groovy.codeInspection.untypedUnresolvedAccess.GrUnresolvedAccessInspection
-import org.jetbrains.plugins.groovy.util.HighlightingTest
-import org.jetbrains.plugins.groovy.util.TestUtils
+import com.intellij.codeInspection.InspectionProfileEntry;
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.codeInspection.assignment.GroovyAssignabilityCheckInspection;
+import org.jetbrains.plugins.groovy.codeInspection.untypedUnresolvedAccess.GrUnresolvedAccessInspection;
+import org.jetbrains.plugins.groovy.util.HighlightingTest;
+import org.jetbrains.plugins.groovy.util.TestUtils;
 
-@CompileStatic
-class GroovyLambdaHighlightingTest extends GrHighlightingTestBase implements HighlightingTest {
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_3_0
-  final String basePath = TestUtils.testDataPath + 'highlighting/lambda/'
+public class GroovyLambdaHighlightingTest extends GrHighlightingTestBase implements HighlightingTest {
+  public void testLambdaAssignment() {
+    highlightingTest("""
+                       import groovy.transform.CompileStatic
 
-  InspectionProfileEntry[] customInspections = [new GrUnresolvedAccessInspection(), new GroovyAssignabilityCheckInspection()]
-
-  void 'test lambda assignment'() {
-    highlightingTest '''
-import groovy.transform.CompileStatic
-
-@CompileStatic
-def a() {
-    Closure cl = () -> {}
-}
-'''
+                       @CompileStatic
+                       def a() {
+                           Closure cl = () -> {}
+                       }
+                       """);
   }
 
-  void 'test lambda argument'() {
-    highlightingTest '''
-import groovy.transform.CompileStatic
-def m(Closure cl) {
-}
-@CompileStatic
-def a() {
-   m( () -> {})
-}
-'''
+  public void testLambdaArgument() {
+    highlightingTest("""
+                       import groovy.transform.CompileStatic
+                       def m(Closure cl) {
+                       }
+                       @CompileStatic
+                       def a() {
+                          m( () -> {})
+                       }
+                       """);
   }
 
-  void testLambdaApplicability() { doTest() }
+  public void testLambdaApplicability() { doTest(); }
 
-  void testCastLambdaToInterface() { doTest() }
+  public void testCastLambdaToInterface() { doTest(); }
 
-  void _testCastInsideLambda() { doTest() }
+  public void _testCastInsideLambda() { doTest(); }
 
-  void testAssignabilityOfCategoryMethod() { doTest() }
+  public void testAssignabilityOfCategoryMethod() { doTest(); }
 
-  void testPathCallIsNotApplicable() { doTest() }
+  public void testPathCallIsNotApplicable() { doTest(); }
 
-  void testCallIsNotApplicable() { doTest() }
+  public void testCallIsNotApplicable() { doTest(); }
 
-  void testCategoryWithPrimitiveType() { doTest() }
+  public void testCategoryWithPrimitiveType() { doTest(); }
 
-  void testCollectionAssignments() { doTest() }
+  public void testCollectionAssignments() { doTest(); }
 
-  void testCurrying() { doTest() }
+  public void testCurrying() { doTest(); }
 
-  void testAnotherCurrying() { doTest() }
+  public void testAnotherCurrying() { doTest(); }
 
-  void testLambdaWithDefaultParameters() { doTest() }
+  public void testLambdaWithDefaultParameters() { doTest(); }
 
-  void testFromStringWithGenericPlaceholderFromClass() { doTest() }
+  public void testFromStringWithGenericPlaceholderFromClass() { doTest(); }
 
-  void testFromStringWithSimpleType() { doTest() }
+  public void testFromStringWithSimpleType() { doTest(); }
 
-  void testFromStringWithGeneric() { doTest() }
+  public void testFromStringWithGeneric() { doTest(); }
 
-  void testFromStringWithGenericPlaceholderFromClassWithTwoGenericsAndPolymorphicSignature() { doTest() }
+  public void testFromStringWithGenericPlaceholderFromClassWithTwoGenericsAndPolymorphicSignature() { doTest(); }
 
-  void testFromStringWithDirectGenericPlaceholder() { doTest() }
+  public void testFromStringWithDirectGenericPlaceholder() { doTest(); }
 
-  void testInferenceOnNonExtensionMethod() { doTest() }
+  public void testInferenceOnNonExtensionMethod() { doTest(); }
 
-  void testFromStringWithGenericPlaceholderFromClassWithTwoGenericsAndNoExplicitSignature() { doTest() }
+  public void testFromStringWithGenericPlaceholderFromClassWithTwoGenericsAndNoExplicitSignature() { doTest(); }
 
-  void testFromStringWithGenericPlaceholderFromClassWithTwoGenerics() { doTest() }
+  public void testFromStringWithGenericPlaceholderFromClassWithTwoGenerics() { doTest(); }
 
-  void testFromStringWithGenericPlaceholderFromClassWithTwoGenericsAndNoExplicitSignatureAndNoFQN() { doTest() }
+  public void testFromStringWithGenericPlaceholderFromClassWithTwoGenericsAndNoExplicitSignatureAndNoFQN() { doTest(); }
 
-  void testFromStringWithGenericPlaceholder() { doTest() }
+  public void testFromStringWithGenericPlaceholder() { doTest(); }
 
-  void testFromStringWithGenericPlaceholderFromClassWithTwoGenericsAndNoExplicitSignatureAndNoFQNAndReferenceToSameUnitClass() { doTest() }
+  public void testFromStringWithGenericPlaceholderFromClassWithTwoGenericsAndNoExplicitSignatureAndNoFQNAndReferenceToSameUnitClass() { doTest(); }
 
-  void testFromStringWithGenericPlaceholderFromClassWithTwoGenericsAndNoExplicitSignatureAndNoFQNAndReferenceToSameUnitClassAndTwoArgs() {
-    doTest()
+  public void testFromStringWithGenericPlaceholderFromClassWithTwoGenericsAndNoExplicitSignatureAndNoFQNAndReferenceToSameUnitClassAndTwoArgs() {
+    doTest();
   }
 
+  public void testCastArgumentError() { doTest(); }
 
-  //GrClosureParamsTest
-  void testCastArgumentError() { doTest() }
-  void testCastArgument() { doTest() }
-  void testCastSeveralArgumentError() { doTest() }
-  void testSeveralSignatures() { doTest() }
-  void testFromStringWithNonFqnOptionsNoError() {
-    addBigInteger()
-    myFixture.addClass '''
-import groovy.lang.Closure;
-import groovy.transform.stc.ClosureParams;
-import groovy.transform.stc.FromString;
+  public void testCastArgument() { doTest(); }
 
-public class A {
-  public static void foo(@ClosureParams(value = FromString.class, options = "BigInteger") Closure c) {}
-}
-'''
-    doTest()
+  public void testCastSeveralArgumentError() { doTest(); }
+
+  public void testSeveralSignatures() { doTest(); }
+
+  public void testFromStringWithNonFqnOptionsNoError() {
+    addBigInteger();
+    myFixture.addClass("""
+                         import groovy.lang.Closure;
+                         import groovy.transform.stc.ClosureParams;
+                         import groovy.transform.stc.FromString;
+
+                         public class A {
+                           public static void foo(@ClosureParams(value = FromString.class, options = "BigInteger") Closure c) {}
+                         }
+                         """);
+    doTest();
   }
 
-  void testFromStringResolveDefaultPackageGeneric() {
-    myFixture.addClass '''
-package com.foo.baz;
+  public void testFromStringResolveDefaultPackageGeneric() {
+    myFixture.addClass("""
+                         package com.foo.baz;
 
-import groovy.lang.Closure;
-import groovy.transform.stc.ClosureParams;
-import groovy.transform.stc.FromString;
+                         import groovy.lang.Closure;
+                         import groovy.transform.stc.ClosureParams;
+                         import groovy.transform.stc.FromString;
+                         
+                         public class A<T> {
+                             public void bar(@ClosureParams(value = FromString.class, options = "MyClass<T>") Closure c) {}
+                         }
+                         """);
+    myFixture.addClass("""
+                         public class MyClass<U> {}
+                         """);
+    doTest();
+  }
 
-public class A<T> {
-    public void bar(@ClosureParams(value = FromString.class, options = "MyClass<T>") Closure c) {}
-}
-'''
-    myFixture.addClass '''
-public class MyClass<U> {}
-'''
-  doTest()
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_3_0;
+  }
+
+  @Override
+  public final String getBasePath() {
+    return TestUtils.getTestDataPath() + "highlighting/lambda/";
+  }
+
+  @Override
+  public InspectionProfileEntry[] getCustomInspections() {
+    return new InspectionProfileEntry[]{new GrUnresolvedAccessInspection(), new GroovyAssignabilityCheckInspection()};
   }
 }
