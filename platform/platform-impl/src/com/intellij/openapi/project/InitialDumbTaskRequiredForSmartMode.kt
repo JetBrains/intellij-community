b242d89a1dee206c6a4f3edff19621ff1e61ea10
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-03-15 01:09:37 +0100

    cleanup
    GitOrigin-RevId: c508594705d0cfa56e56a566d5893e4d94b6cfed

--- platform/platform-impl/src/com/intellij/openapi/project/InitialDumbTaskRequiredForSmartMode.kt
+++ platform/platform-impl/src/com/intellij/openapi/project/InitialDumbTaskRequiredForSmartMode.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.project
 
 import com.intellij.openapi.diagnostic.logger
@@ -6,8 +6,7 @@ import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.DumbServiceImpl.Companion.REQUIRED_FOR_SMART_MODE_STARTUP_ACTIVITY
 
-
-class InitialDumbTaskRequiredForSmartMode(private val project: Project) : DumbModeTask() {
+internal class InitialDumbTaskRequiredForSmartMode(private val project: Project) : DumbModeTask() {
   override fun performInDumbMode(indicator: ProgressIndicator) {
     val activities = REQUIRED_FOR_SMART_MODE_STARTUP_ACTIVITY.extensionList
     val logger = logger<InitialDumbTaskRequiredForSmartMode>()
