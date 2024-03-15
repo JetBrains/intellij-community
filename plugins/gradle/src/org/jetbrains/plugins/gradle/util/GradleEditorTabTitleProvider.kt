0d5f321db04eeed2bb3e6669190732d3e905a55f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-03-15 02:04:41 +0100

    fix RA required
    GitOrigin-RevId: 884af6efea21a668050b1344ef6b7361fd3a1150

--- plugins/gradle/src/org/jetbrains/plugins/gradle/util/GradleEditorTabTitleProvider.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/util/GradleEditorTabTitleProvider.kt
@@ -1,26 +1,30 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.util
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.externalSystem.ExternalSystemModulePropertyManager
 import com.intellij.openapi.fileEditor.impl.EditorTabTitleProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.util.Computable
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VirtualFile
 
-/**
- * @author Vladislav.Soroka
- */
-class GradleEditorTabTitleProvider : EditorTabTitleProvider {
+private class GradleEditorTabTitleProvider : EditorTabTitleProvider {
   override fun getEditorTabTitle(project: Project, file: VirtualFile): String? {
-    if (!GradleConstants.KNOWN_GRADLE_FILES.contains(file.name)) return null
+    if (!GradleConstants.KNOWN_GRADLE_FILES.contains(file.name)) {
+      return null
+    }
 
     val fileParent = file.parent ?: return null
 
-    val module = ProjectFileIndex.getInstance(project).getModuleForFile(file) ?: return null
+    val projectFileIndex = ProjectFileIndex.getInstance(project)
+    val module = ApplicationManager.getApplication().runReadAction(Computable { projectFileIndex.getModuleForFile(file) }) ?: return null
     val manager = ExternalSystemModulePropertyManager.getInstance(module)
 
-    if (manager.getExternalSystemId() != GradleConstants.SYSTEM_ID.id) return null
+    if (manager.getExternalSystemId() != GradleConstants.SYSTEM_ID.id) {
+      return null
+    }
 
     val projectPath = manager.getLinkedProjectPath() ?: return null
 
