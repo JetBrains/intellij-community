f569a814f65fa0467cd3c93b0bb0a3f4d20a7b93
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-03-15 23:55:18 +0100

    UniqueNameEditorTabTitleProvider - add read action
    GitOrigin-RevId: 3e61fbe04d93bb0794210f32705297be7dc5afaa

--- platform/ide-core-impl/src/com/intellij/openapi/fileEditor/impl/UniqueNameEditorTabTitleProvider.kt
+++ platform/ide-core-impl/src/com/intellij/openapi/fileEditor/impl/UniqueNameEditorTabTitleProvider.kt
@@ -1,8 +1,8 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.fileEditor.impl
 
 import com.intellij.ide.ui.UISettings
-import com.intellij.ide.ui.UISettings.Companion.instanceOrNull
+import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.fileEditor.UniqueVFilePathBuilder
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
@@ -26,17 +26,20 @@ open class UniqueNameEditorTabTitleProvider : EditorTabTitleProvider {
   }
 
   override fun getEditorTabTitle(project: Project, file: VirtualFile): String? {
-    val uiSettings = instanceOrNull
+    val uiSettings = UISettings.instanceOrNull
     if (uiSettings == null || !uiSettings.showDirectoryForNonUniqueFilenames || DumbService.isDumb(project)) {
       return null
     }
 
     // Even though this is a 'tab title provider' it is used also when tabs are not shown, namely for building IDE frame title.
-    var uniqueName: String = if (uiSettings.editorTabPlacement == UISettings.TABS_NONE) {
-      UniqueVFilePathBuilder.getInstance().getUniqueVirtualFilePath(project, file)
-    }
-    else {
-      UniqueVFilePathBuilder.getInstance().getUniqueVirtualFilePathWithinOpenedFileEditors(project, file)
+    val uniqueFilePathBuilder = UniqueVFilePathBuilder.getInstance()
+    var uniqueName = ReadAction.compute<String?, Throwable> {
+      if (uiSettings.editorTabPlacement == UISettings.TABS_NONE) {
+        uniqueFilePathBuilder.getUniqueVirtualFilePath(project, file)
+      }
+      else {
+        uniqueFilePathBuilder.getUniqueVirtualFilePathWithinOpenedFileEditors(project, file)
+      }
     }
     uniqueName = getEditorTabText(uniqueName, File.separator, uiSettings.hideKnownExtensionInTabs)!!
     return if (uniqueName == file.name) null else uniqueName
