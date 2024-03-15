820c12f77593a363761fed2dd2ca7ccca0520248
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-03-15 15:07:59 +0100

    [java] convert GroovyRecordHighlightingTest to Java (IDEA-331682)
    GitOrigin-RevId: c61c47cafc6cf98d8b43b53ee558f5794e0a9434

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovyRecordHighlightingTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovyRecordHighlightingTest.java
@@ -1,164 +1,155 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.lang.highlighting
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.highlighting;
 
-import com.intellij.testFramework.LightProjectDescriptor
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
-import org.jetbrains.plugins.groovy.codeInspection.assignment.GroovyAssignabilityCheckInspection
-import org.jetbrains.plugins.groovy.codeInspection.bugs.GroovyAccessibilityInspection
-import org.jetbrains.plugins.groovy.codeInspection.control.finalVar.GrFinalVariableAccessInspection
-import org.jetbrains.plugins.groovy.codeInspection.cs.GrPOJOInspection
-import org.jetbrains.plugins.groovy.lang.psi.impl.GroovyFileImpl
-import org.jetbrains.plugins.groovy.util.HighlightingTest
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.LightGroovyTestCase;
+import org.jetbrains.plugins.groovy.codeInspection.assignment.GroovyAssignabilityCheckInspection;
+import org.jetbrains.plugins.groovy.codeInspection.bugs.GroovyAccessibilityInspection;
+import org.jetbrains.plugins.groovy.codeInspection.control.finalVar.GrFinalVariableAccessInspection;
+import org.jetbrains.plugins.groovy.codeInspection.cs.GrPOJOInspection;
+import org.jetbrains.plugins.groovy.lang.psi.impl.GroovyFileImpl;
+import org.jetbrains.plugins.groovy.util.HighlightingTest;
 
-@CompileStatic
-class GroovyRecordHighlightingTest extends LightGroovyTestCase implements HighlightingTest {
+public class GroovyRecordHighlightingTest extends LightGroovyTestCase implements HighlightingTest {
+  public void testPojoWithoutCs() {
+    highlightingTest("""
+                       import groovy.transform.stc.POJO
 
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_4_0_REAL_JDK
-
-  void 'test pojo without cs'() {
-    highlightingTest '''
-import groovy.transform.stc.POJO
-
-<warning>@POJO</warning>
-class A {}
-''', GrPOJOInspection
+                       <warning>@POJO</warning>
+                       class A {}
+                       """, GrPOJOInspection.class);
   }
 
-  void 'test record definition'() {
-    highlightingTest '''
-record R(int a) {}'''
+  public void testRecordDefinition() {
+    highlightingTest("""
+                       record R(int a) {}""");
   }
 
-  void 'test record field'() {
-    highlightingTest '''
-record R(int a) {}
-def x = new R(10)
-x.a()
-'''
+  public void testRecordField() {
+    highlightingTest("""
+                       record R(int a) {}
+                       def x = new R(10)
+                       x.a()
+                       """);
   }
 
-  void 'test final record field'() {
-    highlightingTest '''
-record R(int a) {}
-def x = new R(10)
-<warning>x.a</warning> = 20
-''', GrFinalVariableAccessInspection
+  public void testFinalRecordField() {
+    highlightingTest("""
+                       record R(int a) {}
+                       def x = new R(10)
+                       <warning>x.a</warning> = 20
+                       """, GrFinalVariableAccessInspection.class);
   }
 
-  void 'test default getter'() {
-    highlightingTest '''
-record X(String a) {}
+  public void testDefaultGetter() {
+    highlightingTest("""
+                       record X(String a) {}
 
-def x = new X(a: "200")
-println x.a()
-'''
+                       def x = new X(a: "200")
+                       println x.a()
+                       """);
   }
 
-  void 'test custom getter'() {
-    highlightingTest '''
-record X(String a) {
-    String a() {
-        return a + "20"
-    }
-}
+  public void testCustomGetter() {
+    highlightingTest("""
+                       record X(String a) {
+                           String a() {
+                               return a + "20"
+                           }
+                       }
 
-def x = new X(a: "200")
-println x.a()
-'''
+                       def x = new X(a: "200")
+                       println x.a()
+                       """);
   }
 
-  void 'test private record field'() {
-    highlightingTest '''
-record R(int a) {}
-def x = new R(10)
-x.<warning>a</warning>
-x.a()
-''', GroovyAccessibilityInspection
+  public void testPrivateRecordField() {
+    highlightingTest("""
+                       record R(int a) {}
+                       def x = new R(10)
+                       x.<warning>a</warning>
+                       x.a()
+                       """, GroovyAccessibilityInspection.class);
   }
 
-  void 'test GROOVY-10305'() {
-    highlightingTest """
-record X(String a, int s) {
-    private X {
-        println 20
-    }
+  public void testGROOVY_10305() {
+    highlightingTest("""
+                       record X(String a, int s) {
+                           private X {
+                               println 20
+                           }
 
-    public X(String a, int s) {}
-}"""
+                           public X(String a, int s) {}
+                       }""");
   }
 
-  void 'test sealed record'() {
-    highlightingTest """
-<error>sealed</error> record X(int a) {
-}"""
+  public void testSealedRecord() {
+    highlightingTest("""
+                       <error>sealed</error> record X(int a) {
+                       }""");
   }
 
-  void 'test compact constructor'() {
-    highlightingTest """
-record X(int a) {
-  <error>X</error> {}
-}
-"""
+  public void testCompactConstructor() {
+    highlightingTest("""
+                       record X(int a) {
+                         <error>X</error> {}
+                       }
+                       """);
   }
 
-  void 'test static field'() {
-    highlightingTest """
-record X(static int a, String b)
-{}
+  public void testStaticField() {
+    highlightingTest("""
+                       record X(static int a, String b)
+                       {}
 
-new X("")
-"""
+                       new X("")
+                       """);
   }
 
-  void 'test no accessor for static field'() {
-    highlightingTest """
-record X(static int a, String b)
-{}
+  public void testNoAccessorForStaticField() {
+    highlightingTest("""
+                       record X(static int a, String b)
+                       {}
 
-new X("").a<warning>()</warning>
-""", GroovyAssignabilityCheckInspection
+                       new X("").a<warning>()</warning>
+                       """, GroovyAssignabilityCheckInspection.class);
   }
 
+  public void testMapConstructor() {
+    highlightingTest("""
+                       record X(static int a, String b)
+                       {}
 
-  void 'test map constructor'() {
-    highlightingTest """
-record X(static int a, String b)
-{}
-
-new X(b : "")
-"""
+                       new X(b : "")
+                       """);
   }
 
-  void 'test non-immutable field'() {
-    highlightingTest """
-record X(<error>b</error>) {}
-"""
+  public void testNonImmutableField() {
+    highlightingTest("""
+                       record X(<error>b</error>) {}
+                       """);
   }
 
+  public void testForcedImmutableField() {
+    highlightingTest("""
+                       import groovy.transform.ImmutableOptions
 
-  void 'test forced immutable field'() {
-    highlightingTest """
-import groovy.transform.ImmutableOptions
-
-@ImmutableOptions(knownImmutables = ['b'])
-record X(b) {}
-"""
+                       @ImmutableOptions(knownImmutables = ['b'])
+                       record X(b) {}
+                       """);
   }
 
-  void 'test do not unnecessarily load files containing records'() {
-    myFixture.with {
-      def file = addFileToProject'A.groovy', """
-record R(int a, String c) {}
-"""
+  public void testDoNotUnnecessarilyLoadFilesContainingRecords() {
+    GroovyFileImpl file = (GroovyFileImpl)myFixture.addFileToProject("A.groovy", "record R(int a, String c) {}");
+    myFixture.configureByText("b.groovy", "R r = new R(1, \"\")");
+    myFixture.checkHighlighting();
+    assertFalse(file.isContentsLoaded());
+  }
 
-      configureByText 'b.groovy', """
-R r = new R(1, "")
-"""
-      checkHighlighting()
-
-      assert !(file as GroovyFileImpl).contentsLoaded
-    }
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_4_0_REAL_JDK;
   }
 }
