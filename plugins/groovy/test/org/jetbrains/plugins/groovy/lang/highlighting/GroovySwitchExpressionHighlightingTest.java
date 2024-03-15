fa2a888bbe1cc6f80b486405ae11850a81e66c16
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-03-15 12:29:29 +0100

    [Groovy] convert GroovySwitchExpressionHighlightingTest to Java (IDEA-327339)
    GitOrigin-RevId: 7dd2eaddc3357cb5be2e8e2c4cca6c269a0615e6

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovySwitchExpressionHighlightingTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovySwitchExpressionHighlightingTest.java
@@ -1,260 +1,260 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.lang.highlighting
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.highlighting;
 
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.LightGroovyTestCase;
+import org.jetbrains.plugins.groovy.codeInspection.bugs.GrSwitchExhaustivenessCheckInspection;
+import org.jetbrains.plugins.groovy.util.HighlightingTest;
 
-import com.intellij.testFramework.LightProjectDescriptor
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
-import org.jetbrains.plugins.groovy.codeInspection.bugs.GrSwitchExhaustivenessCheckInspection
-import org.jetbrains.plugins.groovy.util.HighlightingTest
-
-@CompileStatic
-class GroovySwitchExpressionHighlightingTest extends LightGroovyTestCase implements HighlightingTest {
-
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_4_0
-
-  void doTest(String text) {
-    myFixture.configureByText("_.groovy", text)
-    myFixture.doHighlighting()
-    myFixture.checkResult(text)
+public class GroovySwitchExpressionHighlightingTest extends LightGroovyTestCase implements HighlightingTest {
+  public void doTest(String text) {
+    myFixture.configureByText("_.groovy", text);
+    myFixture.doHighlighting();
+    myFixture.checkResult(text);
   }
 
-  void 'test no mixing arrows and colons'() {
-    doTest '''
-def x = switch (10) {
-  case 20 <error>-></error> 10
-  case 50<error>:</error>
-    yield 5
-}'''
+  public void testNoMixingArrowsAndColons() {
+    doTest("""
+             def x = switch (10) {
+               case 20 <error>-></error> 10
+               case 50<error>:</error>
+                 yield 5
+             }""");
   }
 
-  void 'test require yield in colon-style switch expression'() {
-    doTest '''
-def x = switch (10) {
-  <error>case</error> 20:
-    40
-}'''
+  public void testRequireYieldInColonStyleSwitchExpression() {
+    doTest("""
+             def x = switch (10) {
+               <error>case</error> 20:
+                 40
+             }""");
   }
 
-  void 'test forbid return in colon-style switch expression'() {
-    doTest '''
-def x = switch (10) {
-  <error>case</error> 20:
-    <error>return 40</error>
-}'''
+  public void testForbidReturnInColonStyleSwitchExpression() {
+    doTest("""
+             def x = switch (10) {
+               <error>case</error> 20:
+                 <error>return 40</error>
+             }""");
   }
 
-  void 'test throw in colon-style switch expression'() {
-    doTest '''
-def x = switch (10) {
-    case 20: 
-        throw new IOException()
-}'''
+  public void testThrowInColonStyleSwitchExpression() {
+    doTest("""
+             def x = switch (10) {
+                 case 20:\s
+                     throw new IOException()
+             }""");
   }
 
-  void 'test empty switch'() {
-    doTest '''
-def x = <error>switch</error> (10) {}'''
+  public void testEmptySwitch() {
+    doTest("""
+             def x = <error>switch</error> (10) {}""");
   }
 
-  void 'test check boolean exhaustiveness'() {
-    highlightingTest """
-def foo(boolean b) {
-  def x = switch (b) {
-    case true -> 1
-    case false -> 1
-  }
-}
-""", GrSwitchExhaustivenessCheckInspection
+  public void testCheckBooleanExhaustiveness() {
+    highlightingTest("""
+                       def foo(boolean b) {
+                         def x = switch (b) {
+                           case true -> 1
+                           case false -> 1
+                         }
+                       }
+                       """, GrSwitchExhaustivenessCheckInspection.class);
 
-    highlightingTest """
-def foo(boolean b) {
-  def x = <weak_warning>switch</weak_warning> (b) {
-    case false -> 1
-  }
-}
-""", GrSwitchExhaustivenessCheckInspection
+    highlightingTest("""
+                       def foo(boolean b) {
+                         def x = <weak_warning>switch</weak_warning> (b) {
+                           case false -> 1
+                         }
+                       }
+                       """, GrSwitchExhaustivenessCheckInspection.class);
 
-    highlightingTest """
-def foo(boolean b) {
-  def x = <weak_warning>switch</weak_warning> (b) {
-    case true -> 1
-  }
-}
-""", GrSwitchExhaustivenessCheckInspection
+    highlightingTest("""
+                       def foo(boolean b) {
+                         def x = <weak_warning>switch</weak_warning> (b) {
+                           case true -> 1
+                         }
+                       }
+                       """, GrSwitchExhaustivenessCheckInspection.class);
 
-    highlightingTest """
-def foo(boolean b) {
-  def x = switch (b) {
-    default -> 1
-  }
-}
-""", GrSwitchExhaustivenessCheckInspection
-  }
-
-  void 'test numerical range exhaustiveness'() {
-    highlightingTest """
-def foo(byte b) {
-  def x = switch (b) {
-    case -128..<0 -> 1
-    case 0..127 -> 1
-  }
-}
-""", GrSwitchExhaustivenessCheckInspection
-
-    highlightingTest """
-def foo(byte b) {
-  def x = <weak_warning>switch</weak_warning> (b) {
-    case -128..<0 -> 1
-    case 0..126 -> 1
-  }
-}
-""", GrSwitchExhaustivenessCheckInspection
+    highlightingTest("""
+                       def foo(boolean b) {
+                         def x = switch (b) {
+                           default -> 1
+                         }
+                       }
+                       """, GrSwitchExhaustivenessCheckInspection.class);
   }
 
-  void 'test incomplete enum'() {
-    highlightingTest """
-enum A { X, Y }
+  public void testNumericalRangeExhaustiveness() {
+    highlightingTest("""
+                       def foo(byte b) {
+                         def x = switch (b) {
+                           case -128..<0 -> 1
+                           case 0..127 -> 1
+                         }
+                       }
+                       """, GrSwitchExhaustivenessCheckInspection.class);
 
-def foo(A b) {
-  def x = <weak_warning>switch</weak_warning> (b) {
-    case A.X -> 1
-  }
-}
-""", GrSwitchExhaustivenessCheckInspection
-  }
-
-  void 'test complete enum'() {
-    highlightingTest """
-enum A { X, Y }
-
-def foo(A b) {
-  def x = switch (b) {
-    case A.X -> 1
-    case A.Y -> 2
-  }
-}
-""", GrSwitchExhaustivenessCheckInspection
+    highlightingTest("""
+                       def foo(byte b) {
+                         def x = <weak_warning>switch</weak_warning> (b) {
+                           case -128..<0 -> 1
+                           case 0..126 -> 1
+                         }
+                       }
+                       """, GrSwitchExhaustivenessCheckInspection.class);
   }
 
-  void 'test incomplete sealed class'() {
-    highlightingTest """
-sealed class A {}
-class B extends A {}
-class C extends A {}
+  public void testIncompleteEnum() {
+    highlightingTest("""
+                       enum A { X, Y }
 
-def foo(A b) {
-  def x = <weak_warning>switch</weak_warning> (b) {
-    case B -> 1
-  }
-}
-""", GrSwitchExhaustivenessCheckInspection
+                       def foo(A b) {
+                         def x = <weak_warning>switch</weak_warning> (b) {
+                           case A.X -> 1
+                         }
+                       }
+                       """, GrSwitchExhaustivenessCheckInspection.class);
   }
 
-  void 'test complete sealed class'() {
-    highlightingTest """
-abstract sealed class A {}
-class B extends A {}
-class C extends A {}
+  public void testCompleteEnum() {
+    highlightingTest("""
+                       enum A { X, Y }
 
-def foo(A b) {
-  def x = switch (b) {
-    case B -> 1
-    case C -> 2
-  }
-}
-""", GrSwitchExhaustivenessCheckInspection
+                       def foo(A b) {
+                         def x = switch (b) {
+                           case A.X -> 1
+                           case A.Y -> 2
+                         }
+                       }
+                       """, GrSwitchExhaustivenessCheckInspection.class);
   }
 
-  void 'test complete matching on types'() {
-    highlightingTest """
-def foo(IOException b) {
-  def x = switch (b) {
-    case Throwable -> 1
-  }
-}
-""", GrSwitchExhaustivenessCheckInspection
-  }
+  public void testIncompleteSealedClass() {
+    highlightingTest("""
+                       sealed class A {}
+                       class B extends A {}
+                       class C extends A {}
 
-  void 'test untyped condition'() {
-    highlightingTest"""
-def foo(b) {
-  def x = <weak_warning>switch</weak_warning> (b) {
-    case 10 -> 1
-  }
-}
-""", GrSwitchExhaustivenessCheckInspection
+                       def foo(A b) {
+                         def x = <weak_warning>switch</weak_warning> (b) {
+                           case B -> 1
+                         }
+                       }
+                       """, GrSwitchExhaustivenessCheckInspection.class);
   }
 
-  void 'test plain switch statement is not highligted'() {
-    highlightingTest """
-void main(String[] args) {
-       switch (10) {
-           case 20:
-               return
-       }
-    }"""
+  public void testCompleteSealedClass() {
+    highlightingTest("""
+                       abstract sealed class A {}
+                       class B extends A {}
+                       class C extends A {}
+
+                       def foo(A b) {
+                         def x = switch (b) {
+                           case B -> 1
+                           case C -> 2
+                         }
+                       }
+                       """, GrSwitchExhaustivenessCheckInspection.class);
   }
 
-  void 'test unmatched null'() {
-    def inspection = new GrSwitchExhaustivenessCheckInspection()
-    inspection.enableNullCheck()
-    highlightingTest"""
-enum A { B }
-def foo(A b) {
-  def x = <weak_warning>switch</weak_warning> (b) {
-    case A.B -> 1
-  }
-}
-""", inspection
+  public void testCompleteMatchingOnTypes() {
+    highlightingTest("""
+                       def foo(IOException b) {
+                         def x = switch (b) {
+                           case Throwable -> 1
+                         }
+                       }
+                       """, GrSwitchExhaustivenessCheckInspection.class);
   }
 
-  void 'test explicitly matched null'() {
-    def inspection = new GrSwitchExhaustivenessCheckInspection()
-    inspection.enableNullCheck()
-    highlightingTest"""
-enum A { B }
-def foo(A b) {
-  def x = switch (b) {
-    case A.B -> 1
-    case null -> 2
-  }
-}
-""", inspection
+  public void testUntypedCondition() {
+    highlightingTest("""
+                       def foo(b) {
+                         def x = <weak_warning>switch</weak_warning> (b) {
+                           case 10 -> 1
+                         }
+                       }
+                       """, GrSwitchExhaustivenessCheckInspection.class);
   }
 
-  void 'test highlight with only one subclass matched'() {
-    highlightingTest """
-class A {
-
-}
-
-class B extends A {}
-
-
-def foo(A a) {
-    def x = <weak_warning>switch</weak_warning> (a) {
-        case B -> 30
-    }
-}""", GrSwitchExhaustivenessCheckInspection
+  public void testPlainSwitchStatementIsNotHighligted() {
+    highlightingTest("""
+                       void main(String[] args) {
+                              switch (10) {
+                                  case 20:
+                                      return
+                              }
+                           }""");
   }
 
-  void 'test nested sealed'() {
-    highlightingTest"""
-abstract sealed class A {}
-class B extends A {}
-abstract sealed class C extends A {}
-class D extends C {}
-class E extends C {}
-
-def foo(A a) {
-  def x = switch (a) {
-    case B -> 30
-    case D -> 20
-    case E -> 40
+  public void testUnmatchedNull() {
+    GrSwitchExhaustivenessCheckInspection inspection = new GrSwitchExhaustivenessCheckInspection();
+    inspection.enableNullCheck();
+    highlightingTest("""
+                       enum A { B }
+                       def foo(A b) {
+                         def x = <weak_warning>switch</weak_warning> (b) {
+                           case A.B -> 1
+                         }
+                       }
+                       """, inspection);
   }
-}
-""", GrSwitchExhaustivenessCheckInspection
+
+  public void testExplicitlyMatchedNull() {
+    GrSwitchExhaustivenessCheckInspection inspection = new GrSwitchExhaustivenessCheckInspection();
+    inspection.enableNullCheck();
+    highlightingTest("""
+                       enum A { B }
+                       def foo(A b) {
+                         def x = switch (b) {
+                           case A.B -> 1
+                           case null -> 2
+                         }
+                       }
+                       """, inspection);
+  }
+
+  public void testHighlightWithOnlyOneSubclassMatched() {
+    highlightingTest("""
+                       class A {
+
+                       }
+
+                       class B extends A {}
+
+
+                       def foo(A a) {
+                           def x = <weak_warning>switch</weak_warning> (a) {
+                               case B -> 30
+                           }
+                       }""", GrSwitchExhaustivenessCheckInspection.class);
+  }
+
+  public void testNestedSealed() {
+    highlightingTest("""
+                       abstract sealed class A {}
+                       class B extends A {}
+                       abstract sealed class C extends A {}
+                       class D extends C {}
+                       class E extends C {}
+
+                       def foo(A a) {
+                         def x = switch (a) {
+                           case B -> 30
+                           case D -> 20
+                           case E -> 40
+                         }
+                       }
+                       """, GrSwitchExhaustivenessCheckInspection.class);
+  }
+
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_4_0;
   }
 }
