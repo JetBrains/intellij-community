0555b73cd25f912ebc36c3bef20a389ae31df804
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-03-15 17:05:10 +0100

    [Groovy] convert GroovyHighlightUsagesTest to Java (IDEA-327339)
    GitOrigin-RevId: 7e1406e1bfb87360ff30c72e57f38507e957f53b

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovyHighlightUsagesTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovyHighlightUsagesTest.java
@@ -1,61 +1,74 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.lang.highlighting
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.highlighting;
 
-import com.intellij.codeInsight.daemon.impl.HighlightInfoType
-import com.intellij.codeInsight.daemon.impl.SeveritiesProvider
-import com.intellij.testFramework.LightProjectDescriptor
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
-import org.jetbrains.plugins.groovy.util.TestUtils
+import com.intellij.codeInsight.daemon.impl.HighlightInfoType;
+import com.intellij.codeInsight.daemon.impl.IdentifierHighlighterPassFactory;
+import com.intellij.codeInsight.daemon.impl.SeveritiesProvider;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.LightGroovyTestCase;
+import org.jetbrains.plugins.groovy.util.TestUtils;
 
-import static com.intellij.codeInsight.daemon.impl.HighlightInfoType.ELEMENT_UNDER_CARET_READ
-import static com.intellij.codeInsight.daemon.impl.HighlightInfoType.ELEMENT_UNDER_CARET_WRITE
-import static com.intellij.codeInsight.daemon.impl.IdentifierHighlighterPassFactory.doWithHighlightingEnabled
+import java.util.List;
 
-@CompileStatic
-class GroovyHighlightUsagesTest extends LightGroovyTestCase {
-
+public class GroovyHighlightUsagesTest extends LightGroovyTestCase {
   private static final SeveritiesProvider SEVERITIES_PROVIDER = new SeveritiesProvider() {
-    final List<HighlightInfoType> severitiesHighlightInfoTypes = [ELEMENT_UNDER_CARET_READ, ELEMENT_UNDER_CARET_WRITE]
-  }
-
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_LATEST
-
-  final String basePath = TestUtils.testDataPath + 'highlighting/usages/'
+    @Override
+    public @NotNull List<HighlightInfoType> getSeveritiesHighlightInfoTypes() {
+      return List.of(HighlightInfoType.ELEMENT_UNDER_CARET_READ, HighlightInfoType.ELEMENT_UNDER_CARET_WRITE);
+    }
+  };
 
   @Override
-  void setUp() throws Exception {
-    super.setUp()
-    myFixture.setReadEditorMarkupModel(true)
+  public void setUp() throws Exception {
+    super.setUp();
+    myFixture.setReadEditorMarkupModel(true);
   }
 
-  private void doTest(boolean directoryTest = false) {
-    SeveritiesProvider.EP_NAME.getPoint().registerExtension(SEVERITIES_PROVIDER, testRootDisposable)
-    doWithHighlightingEnabled(getProject(), getTestRootDisposable()) {
-      def name = getTestName()
+  private void doTest(boolean directoryTest) {
+    SeveritiesProvider.EP_NAME.getPoint().registerExtension(SEVERITIES_PROVIDER, getTestRootDisposable());
+    IdentifierHighlighterPassFactory.doWithHighlightingEnabled(getProject(), getTestRootDisposable(), () -> {
+      String name = getTestName();
+      JavaCodeInsightTestFixture fixture = getFixture();
       if (directoryTest) {
-        fixture.copyDirectoryToProject(name, "")
-        fixture.configureByFile("$name/test.groovy")
+        fixture.copyDirectoryToProject(name, "");
+        fixture.configureByFile(name + "/test.groovy");
       }
       else {
-        fixture.configureByFile("${name}.groovy")
+        fixture.configureByFile(name + ".groovy");
       }
-      fixture.checkHighlighting()
-    }
+
+      fixture.checkHighlighting();
+    });
   }
 
-  void 'test constructor usages 1'() { doTest() }
+  private void doTest() {
+    doTest(false);
+  }
 
-  void 'test constructor usages 2'() { doTest() }
+  public void testConstructorUsages1() { doTest(); }
 
-  void 'test constructor usages 3'() { doTest() }
+  public void testConstructorUsages2() { doTest(); }
 
-  void 'test constructor usages 4'() { doTest(true) }
+  public void testConstructorUsages3() { doTest(); }
 
-  void 'test class usages 1'() { doTest() }
+  public void testConstructorUsages4() { doTest(true); }
 
-  void 'test class usages 2'() { doTest() }
+  public void testClassUsages1() { doTest(); }
 
-  void 'test binding variable'() { doTest() }
+  public void testClassUsages2() { doTest(); }
+
+  public void testBindingVariable() { doTest(); }
+
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_LATEST;
+  }
+
+  @Override
+  public final String getBasePath() {
+    return TestUtils.getTestDataPath() + "highlighting/usages/";
+  }
 }
