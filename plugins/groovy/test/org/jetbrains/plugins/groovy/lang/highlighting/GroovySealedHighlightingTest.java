3c2ede2a5a2b35c1c18d67bfe036b4860fe2d203
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-03-15 14:42:05 +0100

    [Groovy] convert GroovySealedHighlightingTest to Java (IDEA-327339)
    GitOrigin-RevId: a1358886d27a2979111a42fabe500dbb1298b308

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovySealedHighlightingTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovySealedHighlightingTest.java
@@ -1,109 +1,103 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.groovy.lang.highlighting
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.highlighting;
 
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.LightGroovyTestCase;
+import org.jetbrains.plugins.groovy.codeInspection.bugs.GrPermitsClauseInspection;
+import org.jetbrains.plugins.groovy.util.HighlightingTest;
 
-import com.intellij.testFramework.LightProjectDescriptor
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
-import org.jetbrains.plugins.groovy.codeInspection.bugs.GrPermitsClauseInspection
-import org.jetbrains.plugins.groovy.util.HighlightingTest
-
-@CompileStatic
-class GroovySealedHighlightingTest  extends LightGroovyTestCase implements HighlightingTest {
-
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_4_0_REAL_JDK
-
-  void 'test permits without sealed'() {
-    highlightingTest '''
-class A <error>permits</error> B {}
-class B extends A {}
-'''
+public class GroovySealedHighlightingTest extends LightGroovyTestCase implements HighlightingTest {
+  public void testPermitsWithoutSealed() {
+    highlightingTest("""
+                       class A <error>permits</error> B {}
+                       class B extends A {}
+                       """);
   }
 
-  void 'test exclusiveness'() {
-    highlightingTest '''
-<error>sealed</error> <error>non-sealed</error> class A {}
-class B extends A {}'''
+  public void testExclusiveness() {
+    highlightingTest("""
+                       <error>sealed</error> <error>non-sealed</error> class A {}
+                       class B extends A {}""");
   }
 
-  void 'test sealed enum'() {
-    highlightingTest '''
-<error>sealed</error> enum A {}'''
+  public void testSealedEnum() {
+    highlightingTest("<error>sealed</error> enum A {}");
   }
 
-  void 'test sealed class without permitted subclasses'() {
-    highlightingTest '''
-<error>sealed</error> interface A {}'''
+  public void testSealedClassWithoutPermittedSubclasses() {
+    highlightingTest("<error>sealed</error> interface A {}");
   }
 
-  void 'test permits with non-extending reference'() {
-    highlightingTest '''
-sealed class A permits <error>B</error> {}
-class B {}
-''', GrPermitsClauseInspection
+  public void testPermitsWithNonExtendingReference() {
+    highlightingTest("""
+                       sealed class A permits <error>B</error> {}
+                       class B {}
+                       """, GrPermitsClauseInspection.class);
   }
 
-  void 'test extending without permission'() {
-    highlightingTest '''
-sealed class A permits B {}
-class B extends A {}
-class C extends <error>A</error> {}'''
+  public void testExtendingWithoutPermission() {
+    highlightingTest("""
+                       sealed class A permits B {}
+                       class B extends A {}
+                       class C extends <error>A</error> {}""");
   }
 
-  void 'test implementing without permission'() {
-    highlightingTest '''
-sealed trait A permits B, C {}
-class B implements A {}
-interface C extends A {}
-class D implements <error>A</error> {}'''
+  public void testImplementingWithoutPermission() {
+    highlightingTest("""
+                       sealed trait A permits B, C {}
+                       class B implements A {}
+                       interface C extends A {}
+                       class D implements <error>A</error> {}""");
   }
 
-  void 'test non-sealed class without sealed superclass'() {
-    highlightingTest '''
-<error>non-sealed</error> class A {}
-'''
+  public void testNonSealedClassWithoutSealedSuperclass() {
+    highlightingTest("<error>non-sealed</error> class A {}");
   }
 
-  void 'test sealed annotation'() {
-    highlightingTest '''
-import groovy.transform.Sealed
+  public void testSealedAnnotation() {
+    highlightingTest("""
+                       import groovy.transform.Sealed
 
-<error>@Sealed</error>
-<error>non-sealed</error> class A {}
-'''
+                       <error>@Sealed</error>
+                       <error>non-sealed</error> class A {}
+                       """);
   }
 
-  void 'test mention class in annotation'() {
-    highlightingTest '''
-import groovy.transform.Sealed
+  public void testMentionClassInAnnotation() {
+    highlightingTest("""
+                       import groovy.transform.Sealed
 
+                       @Sealed(permittedSubclasses = [B])
+                       class A {}
 
-@Sealed(permittedSubclasses = [B])
-class A {}
-
-class B extends A {}
-class C extends <error>A</error> {}'''
+                       class B extends A {}
+                       class C extends <error>A</error> {}""");
   }
 
-  void 'test non-extending class in annotation'() {
-    highlightingTest '''
-import groovy.transform.Sealed
+  public void testNonExtendingClassInAnnotation() {
+    highlightingTest("""
+                       import groovy.transform.Sealed
 
+                       @Sealed(permittedSubclasses = [<error>B</error>])
+                       class A {}
 
-@Sealed(permittedSubclasses = [<error>B</error>])
-class A {}
-
-class B {}
-''', GrPermitsClauseInspection
+                       class B {}
+                       """, GrPermitsClauseInspection.class);
   }
 
-  void 'test generic sealed interface'() {
-    highlightingTest '''
-sealed interface A<T> {}
+  public void testGenericSealedInterface() {
+    highlightingTest("""
+                       sealed interface A<T> {}
 
-class B implements A {}
-class C<T> implements A<T> {}
-'''
+                       class B implements A {}
+                       class C<T> implements A<T> {}
+                       """);
+  }
+
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_4_0_REAL_JDK;
   }
 }
