4c15a2df711b21921447171d4e502faf2ce725e5
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2024-03-16 22:40:26 +0100

    [yaml] IDEA-348764 YAML: Complete child attributes by similar attributes in the document and neighbours
    GitOrigin-RevId: 2788f58d390f246b632aa64319b07c305913164e

--- plugins/yaml/src/org/jetbrains/yaml/psi/yamlPsiUtil.kt
+++ plugins/yaml/src/org/jetbrains/yaml/psi/yamlPsiUtil.kt
@@ -12,16 +12,16 @@ import org.jetbrains.yaml.meta.model.*
 fun getKeysInBetween(value: YAMLValue, topSeq: YAMLSequence): List<String> =
   value.parents(false).takeWhile { it !== topSeq }.filterIsInstance<YAMLKeyValue>().map { it.keyText }.toList().reversed()
 
-fun findStructuralSiblings(value: YAMLValue): Sequence<YAMLValue> {
+fun findStructuralSiblings(value: YAMLValue): Sequence<YAMLPsiElement> {
   val topSeq = value.parentOfType<YAMLSequence>() ?: return emptySequence()
   return findStructuralSiblings(topSeq, getKeysInBetween(value, topSeq))
 }
 
-fun findStructuralSiblings(topSeq: YAMLSequence, keys: List<String>): Sequence<YAMLValue> {
+fun findStructuralSiblings(topSeq: YAMLSequence, keys: List<String>): Sequence<YAMLPsiElement> {
   if (keys.isEmpty()) return topSeq.items.asSequence().mapNotNull { it.value }
 
-  fun collect(root: YAMLKeyValue, keys: List<String>): Sequence<YAMLValue> {
-    val (head, tail) = keys.headTailOrNull() ?: return emptySequence()
+  fun collect(root: YAMLKeyValue, keys: List<String>): Sequence<YAMLPsiElement> {
+    val (head, tail) = keys.headTailOrNull() ?: return sequenceOf(root)
     if (root.keyText != head) return emptySequence()
 
     return when (val v = root.value) {
