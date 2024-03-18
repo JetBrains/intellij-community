0aa280bd9fba0320f604db5153b31d13f5847506
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2024-03-18 11:53:59 +0100

    [maven ][IDEA-348826] [refactor] make static sync run only static sync aware importers
    GitOrigin-RevId: 79e82e9d1adbccebee101848ab123d5f91c71d99

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/workspaceModel/StaticWorkspaceProjectImporter.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/workspaceModel/StaticWorkspaceProjectImporter.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.maven.importing.workspaceModel
 
+import com.intellij.internal.statistic.StructuredIdeActivity
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
@@ -18,11 +19,18 @@ internal class StaticWorkspaceProjectImporter(
 ) : WorkspaceProjectImporter(projectsTree, projectsToImportWithChanges, importingSettings, modifiableModelsProvider, project) {
 
   override fun workspaceConfigurators(): List<MavenWorkspaceConfigurator> {
-    return super.workspaceConfigurators()
+    return super.workspaceConfigurators().filter { it is MavenStaticSyncAware }
   }
 
   override fun addAfterImportTask(postTasks: ArrayList<MavenProjectsProcessorTask>,
                                   contextData: UserDataHolderBase,
                                   appliedProjectsWithModules: List<MavenProjectWithModulesData<Module>>) {
+  }
+
+  override fun configLegacyFacets(mavenProjectsWithModules: List<MavenProjectWithModulesData<Module>>,
+                                        moduleNameByProject: Map<MavenProject, String>,
+                                        postTasks: List<MavenProjectsProcessorTask>,
+                                        activity: StructuredIdeActivity) {
+
   }
 }
