f2ea993aa92912a86301d91f7c6adc340d54e021
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-03-19 09:22:06 +0100

    [platform] fix zero tolerance inspection warning
    GitOrigin-RevId: 1881cbbee30a6513e0f0f60870a2a797a5110c39

--- platform/platform-impl/src/com/intellij/diagnostic/ui/EventWatcherToolWindowFactory.java
+++ platform/platform-impl/src/com/intellij/diagnostic/ui/EventWatcherToolWindowFactory.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.diagnostic.ui;
 
 import com.intellij.diagnostic.DiagnosticBundle;
@@ -35,7 +35,7 @@ final class EventWatcherToolWindowFactory implements ToolWindowFactory, DumbAwar
 
     project.getMessageBus()
       .connect(project)
-      .subscribe(listener.TOPIC, listener);
+      .subscribe(RunnablesListener.TOPIC, listener);
 
     ContentManager manager = toolWindow.getContentManager();
     ContentFactory contentFactory = ContentFactory.getInstance();
