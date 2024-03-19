9ab1b4adeef18606ebb9cc25375dab034ec62b1b
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2024-03-19 16:21:27 +0100

    IDEA-349417 add .ips crash reports into IDE diagnostic zip on MacOS
    GitOrigin-RevId: 3599571b36d8154e5c455f960aafbe96f5a929c4

--- platform/platform-impl/src/com/intellij/ide/logsUploader/LogPacker.kt
+++ platform/platform-impl/src/com/intellij/ide/logsUploader/LogPacker.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonFactory
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
+import com.intellij.diagnostic.MacOSDiagnosticReportDirectories
 import com.intellij.diagnostic.PerformanceWatcher.Companion.getInstance
 import com.intellij.ide.IdeBundle
 import com.intellij.ide.actions.CollectZippedLogsAction
@@ -18,6 +19,7 @@ import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.progress.*
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.SystemInfoRt
 import com.intellij.openapi.util.io.BufferExposingByteArrayOutputStream
 import com.intellij.platform.util.progress.indeterminateStep
 import com.intellij.platform.util.progress.withRawProgressReporter
@@ -39,11 +41,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.io.path.exists
-import kotlin.io.path.fileSize
-import kotlin.io.path.forEachDirectoryEntry
-import kotlin.io.path.inputStream
-import kotlin.io.path.name
+import kotlin.io.path.*
 
 @ApiStatus.Internal
 object LogPacker {
@@ -107,6 +105,18 @@ object LogPacker {
           val name = path.name
           if ((name.startsWith("java_error_in") || name.startsWith("jbr_err_pid")) && !name.endsWith("hprof") && Files.isRegularFile(path)) {
             zip.addFile(name, path)
+          }
+        }
+
+        if (SystemInfoRt.isMac) {
+          for (reportDir in MacOSDiagnosticReportDirectories) {
+            Path.of(reportDir).forEachDirectoryEntry { path ->
+              coroutineContext.ensureActive()
+              val name = path.name
+              if (name.endsWith(".ips") && Files.isRegularFile(path)) {
+                zip.addFile("MacOS_DiagnosticReports/$name", path)
+              }
+            }
           }
         }
       }
