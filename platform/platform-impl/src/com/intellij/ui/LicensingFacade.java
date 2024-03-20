3e7e7a8e356588541f2b11a1d0b3efd77cf1a81a
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2024-03-20 17:22:15 +0100

    LLM-2129: LicensingFacade: Add `LicenseStateListener`for tracking updates
    (cherry picked from commit eb7bb80c3daf3d1368156b6a10f23daf8a506fd7)
    
    GitOrigin-RevId: a377380a280cee1605eb019d1fff196b625e0495

--- platform/platform-impl/src/com/intellij/ui/LicensingFacade.java
+++ platform/platform-impl/src/com/intellij/ui/LicensingFacade.java
@@ -3,15 +3,16 @@ package com.intellij.ui;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.PermanentInstallationID;
 import com.intellij.openapi.util.NlsSafe;
+import com.intellij.util.messages.MessageBus;
+import com.intellij.util.messages.Topic;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public final class LicensingFacade {
   public String platformProductCode;
@@ -31,6 +32,13 @@ public final class LicensingFacade {
 
   public static @Nullable LicensingFacade getInstance() {
     return INSTANCE;
+  }
+
+  @ApiStatus.Internal
+  public static void setInstance(@Nullable LicensingFacade instance) {
+    INSTANCE = instance;
+    final MessageBus messageBus = ApplicationManager.getApplication().getMessageBus();
+    messageBus.syncPublisher(LicenseStateListener.TOPIC).licenseStateChanged(instance);
   }
 
   public @Nullable String getLicensedToMessage() {
@@ -125,4 +133,8 @@ public final class LicensingFacade {
     isUnusedSignalled = value;
   }
 
+  public interface LicenseStateListener extends EventListener {
+    @NotNull Topic<LicenseStateListener> TOPIC = new Topic<>(LicenseStateListener.class);
+    void licenseStateChanged(@Nullable LicensingFacade newState);
+  }
 }
