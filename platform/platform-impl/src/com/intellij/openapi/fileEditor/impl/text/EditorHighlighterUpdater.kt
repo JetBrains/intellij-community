c93dfcec885261f46ba197a991af3d01227994ac
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-03-20 10:45:58 +0100

    IJPL-846 convert EditorHighlighterUpdater to kotlin
    GitOrigin-RevId: a3ab1f8b6d4a6305ba504e3d467bd7795c32391b

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/EditorHighlighterUpdater.kt
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/EditorHighlighterUpdater.kt
@@ -1,188 +1,182 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.fileEditor.impl.text;
+package com.intellij.openapi.fileEditor.impl.text
 
-import com.intellij.ide.plugins.DynamicPluginListener;
-import com.intellij.ide.plugins.IdeaPluginDescriptor;
-import com.intellij.ide.plugins.PluginManagerCore;
-import com.intellij.ide.plugins.cl.PluginAwareClassLoader;
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ModalityState;
-import com.intellij.openapi.application.ReadAction;
-import com.intellij.openapi.application.impl.NonBlockingReadActionImpl;
-import com.intellij.openapi.editor.HighlighterColors;
-import com.intellij.openapi.editor.colors.EditorColorsManager;
-import com.intellij.openapi.editor.ex.EditorEx;
-import com.intellij.openapi.editor.ex.util.EmptyEditorHighlighter;
-import com.intellij.openapi.editor.highlighter.EditorHighlighter;
-import com.intellij.openapi.editor.highlighter.EditorHighlighterFactory;
-import com.intellij.openapi.extensions.ExtensionPointListener;
-import com.intellij.openapi.extensions.ExtensionPointName;
-import com.intellij.openapi.extensions.KeyedFactoryEPBean;
-import com.intellij.openapi.extensions.PluginDescriptor;
-import com.intellij.openapi.fileTypes.*;
-import com.intellij.openapi.fileTypes.impl.AbstractFileType;
-import com.intellij.openapi.project.DumbService;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.KeyedLazyInstance;
-import com.intellij.util.concurrency.NonUrgentExecutor;
-import com.intellij.util.concurrency.ThreadingAssertions;
-import com.intellij.util.messages.MessageBusConnection;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.TestOnly;
+import com.intellij.ide.plugins.DynamicPluginListener
+import com.intellij.ide.plugins.IdeaPluginDescriptor
+import com.intellij.ide.plugins.PluginManagerCore.getPlugin
+import com.intellij.ide.plugins.cl.PluginAwareClassLoader
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.application.impl.NonBlockingReadActionImpl
+import com.intellij.openapi.editor.HighlighterColors
+import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.editor.ex.EditorEx
+import com.intellij.openapi.editor.ex.util.EmptyEditorHighlighter
+import com.intellij.openapi.editor.highlighter.EditorHighlighter
+import com.intellij.openapi.editor.highlighter.EditorHighlighterFactory
+import com.intellij.openapi.extensions.ExtensionPointListener
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.extensions.KeyedFactoryEPBean
+import com.intellij.openapi.extensions.PluginDescriptor
+import com.intellij.openapi.fileEditor.impl.text.AsyncEditorLoader.Companion.isEditorLoaded
+import com.intellij.openapi.fileTypes.*
+import com.intellij.openapi.fileTypes.impl.AbstractFileType
+import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.KeyedLazyInstance
+import com.intellij.util.concurrency.NonUrgentExecutor
+import com.intellij.util.concurrency.ThreadingAssertions
+import com.intellij.util.messages.MessageBusConnection
+import org.jetbrains.annotations.TestOnly
 
-public class EditorHighlighterUpdater {
-  protected final @NotNull Project project;
-  protected final @NotNull EditorEx editor;
-  private final @Nullable VirtualFile file;
-
-  public EditorHighlighterUpdater(@NotNull Project project, @NotNull Disposable parentDisposable, @NotNull EditorEx editor, @Nullable VirtualFile file) {
-    this.project = project;
-    this.editor = editor;
-    this.file = file;
-    connect(parentDisposable, project.getMessageBus().connect(parentDisposable));
+open class EditorHighlighterUpdater(
+  @JvmField protected val project: Project,
+  parentDisposable: Disposable,
+  @JvmField protected val editor: EditorEx,
+  private val file: VirtualFile?,
+) {
+  init {
+    connect(parentDisposable, project.messageBus.connect(parentDisposable))
   }
 
-  private void connect(@NotNull Disposable parentDisposable, @NotNull MessageBusConnection connection) {
-    connection.subscribe(FileTypeManager.TOPIC, new MyFileTypeListener());
-    connection.subscribe(DumbService.DUMB_MODE, new DumbService.DumbModeListener() {
-      @Override
-      public void enteredDumbMode() {
-        updateHighlighters();
+  companion object {
+    @TestOnly
+    fun completeAsyncTasks() {
+      NonBlockingReadActionImpl.waitForAsyncTaskCompletion()
+    }
+  }
+
+  private fun connect(parentDisposable: Disposable, connection: MessageBusConnection) {
+    connection.subscribe(FileTypeManager.TOPIC, MyFileTypeListener())
+    connection.subscribe(DumbService.DUMB_MODE, object : DumbService.DumbModeListener {
+      override fun enteredDumbMode() {
+        updateHighlighters()
       }
 
-      @Override
-      public void exitDumbMode() {
-        updateHighlighters();
+      override fun exitDumbMode() {
+        updateHighlighters()
       }
-    });
+    })
 
-    updateHighlightersOnExtensionChange(parentDisposable, LanguageSyntaxHighlighters.EP_NAME);
-    updateHighlightersOnExtensionChange(parentDisposable, SyntaxHighlighterLanguageFactory.EP_NAME);
-    updateHighlightersOnExtensionChange(parentDisposable, FileTypeEditorHighlighterProviders.EP_NAME);
+    updateHighlightersOnExtensionChange(parentDisposable, LanguageSyntaxHighlighters.EP_NAME)
+    updateHighlightersOnExtensionChange(parentDisposable, SyntaxHighlighterLanguageFactory.EP_NAME)
+    updateHighlightersOnExtensionChange(parentDisposable, FileTypeEditorHighlighterProviders.EP_NAME)
 
-    SyntaxHighlighter.EP_NAME.addExtensionPointListener(new ExtensionPointListener<>() {
-      @Override
-      public void extensionAdded(@NotNull KeyedFactoryEPBean extension, @NotNull PluginDescriptor pluginDescriptor) {
-        checkUpdateHighlighters(extension.key, false);
+    SyntaxHighlighter.EP_NAME.addExtensionPointListener(object : ExtensionPointListener<KeyedFactoryEPBean> {
+      override fun extensionAdded(extension: KeyedFactoryEPBean, pluginDescriptor: PluginDescriptor) {
+        checkUpdateHighlighters(key = extension.key, updateSynchronously = false)
       }
 
-      @Override
-      public void extensionRemoved(@NotNull KeyedFactoryEPBean extension, @NotNull PluginDescriptor pluginDescriptor) {
-        checkUpdateHighlighters(extension.key, true);
+      override fun extensionRemoved(extension: KeyedFactoryEPBean, pluginDescriptor: PluginDescriptor) {
+        checkUpdateHighlighters(key = extension.key, updateSynchronously = true)
       }
-    }, parentDisposable);
+    }, parentDisposable)
 
-    connection.subscribe(DynamicPluginListener.TOPIC, new DynamicPluginListener() {
-      @Override
-      public void beforePluginUnload(@NotNull IdeaPluginDescriptor pluginDescriptor, boolean isUpdate) {
-        IdeaPluginDescriptor loadedPluginDescriptor = PluginManagerCore.getPlugin(pluginDescriptor.getPluginId());
-        ClassLoader pluginClassLoader = loadedPluginDescriptor != null ? loadedPluginDescriptor.getPluginClassLoader() : null;
-        if (EditorHighlighterUpdater.this.file != null && pluginClassLoader instanceof PluginAwareClassLoader) {
-          FileType fileType = EditorHighlighterUpdater.this.file.getFileType();
-          if (fileType.getClass().getClassLoader() == pluginClassLoader ||
-              (fileType instanceof LanguageFileType && ((LanguageFileType) fileType).getClass().getClassLoader() == pluginClassLoader)) {
-            setupHighlighter(createHighlighter(true));
+    connection.subscribe(DynamicPluginListener.TOPIC, object : DynamicPluginListener {
+      override fun beforePluginUnload(pluginDescriptor: IdeaPluginDescriptor, isUpdate: Boolean) {
+        val loadedPluginDescriptor = getPlugin(pluginDescriptor.pluginId)
+        val pluginClassLoader = loadedPluginDescriptor?.pluginClassLoader
+        if (this@EditorHighlighterUpdater.file != null && pluginClassLoader is PluginAwareClassLoader) {
+          val fileType = file.fileType
+          if (fileType.javaClass.classLoader === pluginClassLoader ||
+              (fileType is LanguageFileType && fileType.javaClass.classLoader === pluginClassLoader)) {
+            setupHighlighter(createHighlighter(true))
           }
         }
       }
-    });
+    })
   }
 
-  private <T> void updateHighlightersOnExtensionChange(@NotNull Disposable parentDisposable, @NotNull ExtensionPointName<KeyedLazyInstance<T>> epName) {
-    epName.addExtensionPointListener(new ExtensionPointListener<>() {
-      @Override
-      public void extensionAdded(@NotNull KeyedLazyInstance<T> extension, @NotNull PluginDescriptor pluginDescriptor) {
-        checkUpdateHighlighters(extension.getKey(), false);
+  private fun <T> updateHighlightersOnExtensionChange(parentDisposable: Disposable, epName: ExtensionPointName<KeyedLazyInstance<T>>) {
+    epName.addExtensionPointListener(object : ExtensionPointListener<KeyedLazyInstance<T>> {
+      override fun extensionAdded(extension: KeyedLazyInstance<T>, pluginDescriptor: PluginDescriptor) {
+        checkUpdateHighlighters(extension.key, false)
       }
 
-      @Override
-      public void extensionRemoved(@NotNull KeyedLazyInstance<T> extension, @NotNull PluginDescriptor pluginDescriptor) {
-        checkUpdateHighlighters(extension.getKey(), true);
+      override fun extensionRemoved(extension: KeyedLazyInstance<T>, pluginDescriptor: PluginDescriptor) {
+        checkUpdateHighlighters(extension.key, true)
       }
-    }, parentDisposable);
+    }, parentDisposable)
   }
 
-  private void checkUpdateHighlighters(String key, boolean updateSynchronously) {
+  private fun checkUpdateHighlighters(key: String, updateSynchronously: Boolean) {
     if (file != null) {
-      FileType fileType = file.getFileType();
-      boolean needUpdate = (fileType.getName().equals(key) ||
-                            (fileType instanceof LanguageFileType && ((LanguageFileType)fileType).getLanguage().getID().equals(key)));
-      if (!needUpdate) return;
+      val fileType = file.fileType
+      val needUpdate = (fileType.name == key || (fileType is LanguageFileType && fileType.language.id == key))
+      if (!needUpdate) {
+        return
+      }
     }
 
-    if (updateSynchronously && ApplicationManager.getApplication().isDispatchThread()) {
-      updateHighlightersSynchronously();
+    if (updateSynchronously && ApplicationManager.getApplication().isDispatchThread) {
+      updateHighlightersSynchronously()
     }
     else {
-      updateHighlighters();
+      updateHighlighters()
     }
   }
 
-  public void updateHighlightersAsync() {
-    if (!AsyncEditorLoader.isEditorLoaded(editor)) {
-      return;
+  fun updateHighlightersAsync() {
+    if (!isEditorLoaded(editor)) {
+      return
     }
 
     ReadAction
-      .nonBlocking(() -> createHighlighter(false))
+      .nonBlocking<EditorHighlighter> { createHighlighter(false) }
       .expireWith(project)
-      .expireWhen(() -> (file != null && !file.isValid()) || editor.isDisposed())
-      .coalesceBy(EditorHighlighterUpdater.class, editor)
-      .finishOnUiThread(ModalityState.any(), highlighter -> setupHighlighter(highlighter))
-      .submit(NonUrgentExecutor.getInstance());
+      .expireWhen { (file != null && !file.isValid) || editor.isDisposed }
+      .coalesceBy(EditorHighlighterUpdater::class.java, editor)
+      .finishOnUiThread(ModalityState.any(), ::setupHighlighter)
+      .submit(NonUrgentExecutor.getInstance())
   }
 
-  protected @NotNull EditorHighlighter createHighlighter(boolean forceEmpty) {
-    EditorHighlighter highlighter = file != null && !forceEmpty
-                                    ? EditorHighlighterFactory.getInstance().createEditorHighlighter(project, file)
-                                    : new EmptyEditorHighlighter(EditorColorsManager.getInstance().getGlobalScheme(),
-                                                                 HighlighterColors.TEXT);
-    highlighter.setText(editor.getDocument().getImmutableCharSequence());
-    return highlighter;
+  protected open fun createHighlighter(forceEmpty: Boolean): EditorHighlighter {
+    val highlighter = if (file != null && !forceEmpty) {
+      EditorHighlighterFactory.getInstance().createEditorHighlighter(project, file)
+    }
+    else {
+      EmptyEditorHighlighter(EditorColorsManager.getInstance().globalScheme, HighlighterColors.TEXT)
+    }
+    highlighter.setText(editor.document.immutableCharSequence)
+    return highlighter
   }
 
-  protected void setupHighlighter(@NotNull EditorHighlighter highlighter) {
-    editor.setHighlighter(highlighter);
+  protected open fun setupHighlighter(highlighter: EditorHighlighter) {
+    editor.highlighter = highlighter
   }
 
   /**
    * Updates editors' highlighters. This should be done when the opened file changes its file type.
    */
-  public void updateHighlighters() {
-    if (!project.isDisposed() && !editor.isDisposed()) {
-      updateHighlightersAsync();
+  fun updateHighlighters() {
+    if (!project.isDisposed && !editor.isDisposed) {
+      updateHighlightersAsync()
     }
   }
 
-  private void updateHighlightersSynchronously() {
-    if (!project.isDisposed() && !editor.isDisposed()) {
-      setupHighlighter(createHighlighter(false));
+  private fun updateHighlightersSynchronously() {
+    if (!project.isDisposed && !editor.isDisposed) {
+      setupHighlighter(createHighlighter(false))
     }
-  }
-
-  @TestOnly
-  public static void completeAsyncTasks() {
-    NonBlockingReadActionImpl.waitForAsyncTaskCompletion();
   }
 
   /**
    * Listen to changes of file types. When the type of the file changes, we need to also change highlighter.
    */
-  private final class MyFileTypeListener implements FileTypeListener {
-    @Override
-    public void fileTypesChanged(final @NotNull FileTypeEvent event) {
-      ThreadingAssertions.assertEventDispatchThread();
+  private inner class MyFileTypeListener : FileTypeListener {
+    override fun fileTypesChanged(event: FileTypeEvent) {
+      ThreadingAssertions.assertEventDispatchThread()
       // File can be invalid after file type changing. The editor should be removed by the FileEditorManager if it's invalid.
-      FileType type = event.getRemovedFileType();
-      if (type != null && !(type instanceof AbstractFileType)) {
+      val type = event.removedFileType
+      if (type != null && type !is AbstractFileType) {
         // Plugin is being unloaded, so we need to release plugin classes immediately
-        updateHighlightersSynchronously();
+        updateHighlightersSynchronously()
       }
       else {
-        updateHighlighters();
+        updateHighlighters()
       }
     }
   }
