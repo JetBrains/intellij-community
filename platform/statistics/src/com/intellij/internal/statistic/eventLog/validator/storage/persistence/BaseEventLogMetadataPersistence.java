90af5d0a3e124b67b8edf8dcb7c3d0b64733657d
Ivan Migalev <ivan.migalev@jetbrains.com>
2024-03-20 17:37:24 +0100

    FUS: allow setting the custom schema location via system properties
    GitOrigin-RevId: 62033a79d7b0b0ae9eeae27441e1f3a7fdabcc1d

--- platform/statistics/src/com/intellij/internal/statistic/eventLog/validator/storage/persistence/BaseEventLogMetadataPersistence.java
+++ platform/statistics/src/com/intellij/internal/statistic/eventLog/validator/storage/persistence/BaseEventLogMetadataPersistence.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.internal.statistic.eventLog.validator.storage.persistence;
 
 import com.intellij.openapi.application.PathManager;
@@ -17,6 +17,8 @@ abstract public class BaseEventLogMetadataPersistence {
 
   public static final String DEPRECATED_FUS_METADATA_DIR = "event-log-whitelist";
   public static final String FUS_METADATA_DIR = "event-log-metadata";
+
+  private static final String CUSTOM_FUS_SCHEMA_DIR_PROPERTY = "intellij.fus.custom.schema.dir";
 
   @Nullable
   public abstract String getCachedEventsScheme();
@@ -62,6 +64,11 @@ abstract public class BaseEventLogMetadataPersistence {
 
   @NotNull
   private static Path getMetadataConfigRoot(@NotNull String dir) {
+    String customFusPath = System.getProperty(CUSTOM_FUS_SCHEMA_DIR_PROPERTY);
+    if (!StringUtil.isEmpty(customFusPath)) {
+      return Path.of(customFusPath);
+    }
+
     return PathManager.getConfigDir().resolve(dir);
   }
 }
