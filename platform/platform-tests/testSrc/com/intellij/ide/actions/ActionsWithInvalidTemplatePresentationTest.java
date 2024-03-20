ffd217e2bfa134abb42ffa99b38b1d26667a0f71
Gregory.Shrago <gregory.shrago@jetbrains.com>
2024-03-20 22:53:26 +0400

    improve ActionsWithInvalidTemplatePresentationTest
    And fix all the newly reported actions.
    
    GitOrigin-RevId: 7e386afd5de679b52c47f9500becb5aa7cc6fec8

--- platform/platform-tests/testSrc/com/intellij/ide/actions/ActionsWithInvalidTemplatePresentationTest.java
+++ platform/platform-tests/testSrc/com/intellij/ide/actions/ActionsWithInvalidTemplatePresentationTest.java
@@ -6,28 +6,40 @@ import com.intellij.openapi.actionSystem.ActionStub;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.actionSystem.impl.ActionManagerImpl;
-import com.intellij.testFramework.LightPlatformTestCase;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.testFramework.junit5.DynamicTests;
+import com.intellij.testFramework.junit5.NamedFailure;
+import com.intellij.testFramework.junit5.TestApplication;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
-public class ActionsWithInvalidTemplatePresentationTest extends LightPlatformTestCase {
-  private static final List<String> KNOWN_FALSE_POSITIVES = Arrays.asList(
+@TestMethodOrder(MethodOrderer.MethodName.class)
+@TestApplication
+public class ActionsWithInvalidTemplatePresentationTest {
+  // TODO fix and remove the field
+  private static final List<String> ACTIONS_TO_FIX = Arrays.asList(
     "InsertRubyInjection",
     "InsertRubyInjectionWithoutOutput"
   );
 
-  public void testActionsPresentations() {
+  @TestFactory
+  public List<DynamicTest> testActionsPresentations() {
     ActionManagerImpl actionManager = (ActionManagerImpl)ActionManager.getInstance();
 
-    List<String> failed = new ArrayList<>();
+    List<NamedFailure> failures = new ArrayList<>();
     for (Iterator<AnAction> it = actionManager.actions(true).iterator(); it.hasNext(); ) {
       AnAction action = it.next();
       String id = actionManager.getId(action);
-      if (KNOWN_FALSE_POSITIVES.contains(id)) {
+      if (ACTIONS_TO_FIX.contains(id)) {
         continue;
       }
 
@@ -35,19 +47,42 @@ public class ActionsWithInvalidTemplatePresentationTest extends LightPlatformTes
       String text = presentation.getText();
       String description = presentation.getDescription();
 
-      if (!isValidText(text) || !isValidText(description)) {
-        Object aClass = action instanceof ActionStub ? "class "+((ActionStub)action).getClassName() : action.getClass();
-        failed.add(id + "; " + aClass + "; text: '" + text + "'; description: '" + description + "'\n");
+      if (hasTemplates(text) || hasTemplates(description)) {
+        failures.add(newFailure(action, id, text, description, "string template markup"));
+      }
+
+      if (hasHtmlTags(text) || hasHtmlTags(description)) {
+        failures.add(newFailure(action, id, text, description, "HTML markup"));
+      }
+
+      if (StringUtil.isEmptyOrSpaces(text) && !StringUtil.isEmptyOrSpaces(description)) {
+        failures.add(newFailure(action, id, text, description, "empty text but description is ok"));
       }
     }
-    System.err.println(failed);
-    assertEmpty("The following actions might have invalid template presentation:\n", failed);
+    return DynamicTests.asDynamicTests(failures, "incorrect project settings");
   }
 
-  private static boolean isValidText(@Nullable String text) {
-    if (text == null) return true;
-    if (text.contains("{")) return false; // MessageFormat template
-    if (text.contains("<")) return false; // HTML
-    return true;
+  private static @NotNull NamedFailure newFailure(@NotNull AnAction action,
+                                                  @NotNull String id,
+                                                  @Nullable String text,
+                                                  @Nullable String description,
+                                                  @NotNull String comment) {
+    Object aClass = action instanceof ActionStub ? "class " + ((ActionStub)action).getClassName() : action.getClass();
+    String message = id + "; " + aClass + "; text: '" + text + "'; description: '" + description + "'";
+    return new NamedFailure("Action '" + id + "': " + comment, message);
+  }
+
+  /**
+   * Potential MessageFormat template
+   */
+  private static boolean hasTemplates(@Nullable String text) {
+    return text != null && text.contains("{");
+  }
+
+  /**
+   * Potential HTML
+   */
+  private static boolean hasHtmlTags(@Nullable String text) {
+    return text != null && text.contains("<");
   }
 }
