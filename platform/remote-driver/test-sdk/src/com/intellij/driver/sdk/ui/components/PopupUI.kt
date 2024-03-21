e3d1e28ab04d6e31a15c57ebb2e3fc26616591c2
Mikhail Kuznetsov <mikhail.kuznetsov@jetbrains.com>
2024-03-21 16:07:51 +0200

    [ui-tests] icons of popup items
    GitOrigin-RevId: a6e42555ca6087ad6f29727786886d0d68461dde

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/PopupUI.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/PopupUI.kt
@@ -13,10 +13,15 @@ fun Finder.popupMenu(@Language("xpath") xpath: String? = null) =
   x(xpath ?: "//div[@class='MyMenu']", PopupMenuUiComponent::class.java)
 
 class PopupMenuUiComponent(data: ComponentData) : UiComponent(data) {
-  private val menuItems = xx("//div[@class='ActionMenuItem' or @class='ActionMenu']", PopupItemUiComponent::class.java)
+  private val menuItems =
+    xx("//div[@class='ActionMenuItem' or @class='ActionMenu']", PopupItemUiComponent::class.java)
 
   fun findMenuItemByText(text: String) = menuItems.list().firstOrNull { it.getText() == text}
                                          ?: throw AssertionError("No item with text '$text' found in popup '${this.searchContext}'")
+
+  fun getItemByIndex(index: Int) = menuItems.list()[index]
+
+  fun getItemByText(text: String) = menuItems.list().first { it.getText() == text }
 
   fun select(vararg items: String) {
     items.forEach { item ->
@@ -32,8 +37,12 @@ open class PopupUiComponent(data: ComponentData) : UiComponent(data)
 
 @Remote("com.intellij.openapi.actionSystem.impl.ActionMenuItem")
 interface PopupItemRef {
+
   fun isSelected(): Boolean
+
   fun getText(): String
+
+  fun getIcon(): Icon
 }
 
 class PopupItemUiComponent(data: ComponentData) : UiComponent(data) {
@@ -41,5 +50,9 @@ class PopupItemUiComponent(data: ComponentData) : UiComponent(data) {
   private val popupComponent by lazy { driver.cast(component, PopupItemRef::class) }
 
   fun getText() = popupComponent.getText()
+
   fun isSelected() = popupComponent.isSelected()
+
+  fun getIconPath() = "path=(.*),".toRegex().find(popupComponent.getIcon().toString())?.let { it.groups.last()?.value ?: "empty" }
+                      ?: "empty"
 }
