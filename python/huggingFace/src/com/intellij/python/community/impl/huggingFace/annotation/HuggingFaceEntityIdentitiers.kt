a72f5ce9a289d8490901e664831b5eadb25e23d3
Bogdan Kirilenko <bogdan.kirilenko@jetbrains.com>
2024-03-21 14:59:10 +0100

    [pycharm] PY-63670 - fixes according to IJ-CR-125957, 2 - HuggingFaceEntityBasicApiData (de)serialization using jacksn
    (cherry picked from commit 220757a7c672c1b34cb9615c4efefef01acd5e12)
    
    GitOrigin-RevId: b298515919fa951ca9ae961a69de277ec3335f9c

--- python/huggingFace/src/com/intellij/python/community/impl/huggingFace/annotation/HuggingFaceEntityIdentitiers.kt
+++ python/huggingFace/src/com/intellij/python/community/impl/huggingFace/annotation/HuggingFaceEntityIdentitiers.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.python.community.impl.huggingFace.annotation
 
-@JvmInline value class HuggingFaceModelId(val id: String)
-@JvmInline value class HuggingFaceDatasetId(val id: String)
-@JvmInline value class HuggingFaceEntityId(val id: String)
+import org.jetbrains.annotations.ApiStatus
+
+@ApiStatus.Internal @JvmInline value class HuggingFaceModelIdentifier(val value: String)
+@ApiStatus.Internal @JvmInline value class HuggingFaceDatasetIdentifier(val value: String)
+@ApiStatus.Internal @JvmInline value class HuggingFaceEntityIdentifier(val value: String)
