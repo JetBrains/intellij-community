78be43bdb155e35e554e9a4ee7cfc5a918b446d8
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2024-03-22 17:15:53 +0100

    Kotlinize class (required for PY-64402 to make fields bindable)
    GitOrigin-RevId: d940f4984827399da77b2c2a0afbd57f01e7d2c1

--- python/ide/impl/src/com/intellij/pycharm/community/ide/impl/newProject/steps/PythonBaseProjectGenerator.kt
+++ python/ide/impl/src/com/intellij/pycharm/community/ide/impl/newProject/steps/PythonBaseProjectGenerator.kt
@@ -1,64 +1,45 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.pycharm.community.ide.impl.newProject.steps;
+package com.intellij.pycharm.community.ide.impl.newProject.steps
 
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.progress.ProcessCanceledException;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.VerticalFlowLayout;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.jetbrains.python.PyBundle;
-import com.jetbrains.python.newProject.PyNewProjectSettings;
-import com.jetbrains.python.newProject.PythonProjectGenerator;
-import com.intellij.pycharm.community.ide.impl.newProject.welcome.PyWelcomeGenerator;
-import com.jetbrains.python.psi.icons.PythonPsiApiIcons;
-import com.jetbrains.python.remote.PyProjectSynchronizer;
-import com.jetbrains.python.sdk.PySdkExtKt;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.VerticalFlowLayout
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.pycharm.community.ide.impl.newProject.welcome.PyWelcomeGenerator.createWelcomeSettingsPanel
+import com.intellij.pycharm.community.ide.impl.newProject.welcome.PyWelcomeGenerator.welcomeUser
+import com.jetbrains.python.PyBundle
+import com.jetbrains.python.newProject.PyNewProjectSettings
+import com.jetbrains.python.newProject.PythonProjectGenerator
+import com.jetbrains.python.psi.icons.PythonPsiApiIcons
+import com.jetbrains.python.remote.PyProjectSynchronizer
+import com.jetbrains.python.sdk.pythonSdk
+import org.jetbrains.annotations.Nls
+import javax.swing.Icon
+import javax.swing.JPanel
 
-import javax.swing.*;
+class PythonBaseProjectGenerator : PythonProjectGenerator<PyNewProjectSettings?>(true) {
+  override fun getName(): @Nls String = PyBundle.message("pure.python.project")
 
-public final class PythonBaseProjectGenerator extends PythonProjectGenerator<PyNewProjectSettings> {
-  public PythonBaseProjectGenerator() {
-    super(true);
+  @Throws(ProcessCanceledException::class)
+  override fun extendBasePanel(): JPanel = JPanel(VerticalFlowLayout(3, 0)).apply {
+    add(createWelcomeSettingsPanel())
   }
 
-  @NotNull
-  @Nls
-  @Override
-  public String getName() {
-    return PyBundle.message("pure.python.project");
-  }
+  override fun getLogo(): Icon = PythonPsiApiIcons.Python
 
-  @Override
-  public @NotNull JPanel extendBasePanel() throws ProcessCanceledException {
-    final JPanel panel = new JPanel(new VerticalFlowLayout(3, 0));
-    panel.add(PyWelcomeGenerator.INSTANCE.createWelcomeSettingsPanel());
-    return panel;
-  }
-
-  @Override
-  public @NotNull Icon getLogo() {
-    return PythonPsiApiIcons.Python;
-  }
-
-  @Override
-  public void configureProject(@NotNull final Project project, @NotNull VirtualFile baseDir, @NotNull final PyNewProjectSettings settings,
-                               @NotNull final Module module, @Nullable final PyProjectSynchronizer synchronizer) {
+  public override fun configureProject(project: Project,
+                                       baseDir: VirtualFile,
+                                       settings: PyNewProjectSettings,
+                                       module: Module,
+                                       synchronizer: PyProjectSynchronizer?) {
     // Super should be called according to its contract unless we sync project explicitly (we do not, so we call super)
-    super.configureProject(project, baseDir, settings, module, synchronizer);
-    PySdkExtKt.setPythonSdk(module, settings.getSdk());
-    PyWelcomeGenerator.INSTANCE.welcomeUser(project, baseDir, module);
+    super.configureProject(project, baseDir, settings, module, synchronizer)
+    module.pythonSdk = settings.sdk
+    welcomeUser(project, baseDir, module)
   }
 
-  @Override
-  public @NotNull String getNewProjectPrefix() {
-    return "pythonProject";
-  }
+  override fun getNewProjectPrefix(): String = "pythonProject"
 
-  @Override
-  public boolean supportsWelcomeScript() {
-    return true;
-  }
+  override fun supportsWelcomeScript(): Boolean = true
 }
