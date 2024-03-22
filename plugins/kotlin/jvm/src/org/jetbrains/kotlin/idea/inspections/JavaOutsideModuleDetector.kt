4bbf267593657ea08cb7b38a5c0e563596b73098
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2024-03-22 22:01:37 +0100

    [kotlin] Fix java outside of java source root check
    #KTIJ-1233 Fixed
    
    GitOrigin-RevId: 76ca3f21ff5bb055fac5e28398e846df12af4552

--- plugins/kotlin/jvm/src/org/jetbrains/kotlin/idea/inspections/JavaOutsideModuleDetector.kt
+++ plugins/kotlin/jvm/src/org/jetbrains/kotlin/idea/inspections/JavaOutsideModuleDetector.kt
@@ -6,15 +6,13 @@ import com.intellij.icons.AllIcons
 import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileTypes.FileTypeRegistry
-import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.EditorNotificationPanel
 import com.intellij.ui.EditorNotificationProvider
+import org.jetbrains.jps.model.java.JavaModuleSourceRootTypes
 import org.jetbrains.kotlin.idea.KotlinJvmBundle
-import org.jetbrains.kotlin.idea.base.util.isGradleModule
-import org.jetbrains.kotlin.idea.facet.KotlinFacet
-import org.jetbrains.kotlin.idea.util.sourceRoots
 import java.util.function.Function
 import javax.swing.JComponent
 
@@ -24,12 +22,9 @@ class JavaOutsideModuleDetector : EditorNotificationProvider {
         if (file.extension != JavaFileType.DEFAULT_EXTENSION && !FileTypeRegistry.getInstance().isFileOfType(file, JavaFileType.INSTANCE)) {
             return null
         }
-        val module = ModuleUtilCore.findModuleForFile(file, project)?.takeIf { it.isGradleModule } ?: return null
-        val facetSettings = KotlinFacet.get(module)?.configuration?.settings ?: return null
 
-        val filePath = file.path
-        val nonKotlinPath = module.sourceRoots.map { it.path } - facetSettings.pureKotlinSourceFolders
-        if (nonKotlinPath.any { filePath.startsWith(it) }) return null
+        val fileIndex = ProjectRootManager.getInstance(project).getFileIndex()
+        if (!fileIndex.isInSourceContent(file) || fileIndex.isUnderSourceRootOfType(file, JavaModuleSourceRootTypes.SOURCES)) return null
 
         return Function {
             EditorNotificationPanel(it, EditorNotificationPanel.Status.Warning).apply {
