a4e4045712f8e7d007b0cef9e59805b77f1f413e
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-03-22 10:53:07 +0100

    [Groovy] convert GrLibrarySourceHighlightingTest to Java (IDEA-327339)
    GitOrigin-RevId: 5a502d1ad5be47c64fd3f15153928e2f2990e930

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrLibrarySourceHighlightingTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrLibrarySourceHighlightingTest.java
@@ -1,56 +1,53 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jetbrains.plugins.groovy.lang.highlighting
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.highlighting;
 
-import com.intellij.openapi.module.Module
-import com.intellij.openapi.roots.ContentEntry
-import com.intellij.openapi.roots.ModifiableRootModel
-import com.intellij.openapi.roots.OrderRootType
-import com.intellij.openapi.vfs.JarFileSystem
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.psi.JavaPsiFacade
-import com.intellij.psi.impl.compiled.ClsClassImpl
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.testFramework.LightProjectDescriptor
-import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor
-import org.jetbrains.annotations.NotNull
-import org.jetbrains.plugins.groovy.util.TestUtils
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.roots.ContentEntry;
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.roots.OrderRootType;
+import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.vfs.JarFileSystem;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.impl.compiled.ClsClassImpl;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.util.TestUtils;
 
-class GrLibrarySourceHighlightingTest extends GrHighlightingTestBase {
-
-  final LightProjectDescriptor projectDescriptor = new DefaultLightProjectDescriptor() {
-    @Override
-    void configureModule(@NotNull Module module, @NotNull ModifiableRootModel model, @NotNull ContentEntry contentEntry) {
-      final absoluteBasePath = "${TestUtils.absoluteTestDataPath}${basePath}"
-      final lib = JarFileSystem.instance.refreshAndFindFileByPath("$absoluteBasePath/some-library.jar!/")
-      final src = LocalFileSystem.instance.refreshAndFindFileByPath("$absoluteBasePath/src/")
-
-      final modifiableModel = model.moduleLibraryTable.createLibrary("some-library").modifiableModel
-      modifiableModel.addRoot lib, OrderRootType.CLASSES
-      modifiableModel.addRoot src, OrderRootType.SOURCES
-      modifiableModel.commit()
-    }
+public class GrLibrarySourceHighlightingTest extends GrHighlightingTestBase {
+  public void testNoErrorsTraitHighlighting() {
+    PsiClass clazz =
+      JavaPsiFacade.getInstance(getProject()).findClass("somepackage.CC", GlobalSearchScope.moduleWithLibrariesScope(getModule()));
+    PsiFile psiFile = (DefaultGroovyMethods.asType(clazz, ClsClassImpl.class)).getSourceMirrorClass().getContainingFile();
+    myFixture.configureFromExistingVirtualFile(psiFile.getVirtualFile());
+    myFixture.testHighlighting();
   }
 
-  final String basePath = "highlighting/librarySources"
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return new DefaultLightProjectDescriptor() {
+      @Override
+      public void configureModule(@NotNull Module module, @NotNull ModifiableRootModel model, @NotNull ContentEntry contentEntry) {
+        final String absoluteBasePath = TestUtils.getAbsoluteTestDataPath() + getBasePath();
+        final VirtualFile lib = JarFileSystem.getInstance().refreshAndFindFileByPath(absoluteBasePath + "/some-library.jar!/");
+        final VirtualFile src = LocalFileSystem.getInstance().refreshAndFindFileByPath(absoluteBasePath + "/src/");
 
-  void "test no errors trait highlighting"() {
-    def clazz = JavaPsiFacade.getInstance(project).findClass("somepackage.CC", GlobalSearchScope.moduleWithLibrariesScope(module))
-    def psiFile = (clazz as ClsClassImpl).sourceMirrorClass.containingFile
-    myFixture.configureFromExistingVirtualFile(psiFile.virtualFile)
-    myFixture.testHighlighting()
+        final Library.ModifiableModel modifiableModel = model.getModuleLibraryTable().createLibrary("some-library").getModifiableModel();
+        modifiableModel.addRoot(lib, OrderRootType.CLASSES);
+        modifiableModel.addRoot(src, OrderRootType.SOURCES);
+        modifiableModel.commit();
+      }
+    };
+  }
+
+  @Override
+  public final String getBasePath() {
+    return "highlighting/librarySources";
   }
 }
