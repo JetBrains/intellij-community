a2edf45a5fcdf586fd760fe0358ee07f4b3c257b
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2024-03-23 15:18:14 +0100

    [vcs-log] cleanup: remove redundant suppressions
    GitOrigin-RevId: 6c7273df0eb1c88f4ce44ea785ca5b47a57f0b7e

--- platform/vcs-log/impl/src/com/intellij/vcs/log/data/index/PhmVcsLogStorageBackend.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/data/index/PhmVcsLogStorageBackend.kt
@@ -1,6 +1,4 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-@file:Suppress("ReplaceGetOrSet")
-
 package com.intellij.vcs.log.data.index
 
 import com.intellij.openapi.Disposable
