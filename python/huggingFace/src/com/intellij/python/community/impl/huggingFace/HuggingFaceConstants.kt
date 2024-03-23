52041c68af17c3114f1bcb8a88eb75504f0818ff
Bogdan Kirilenko <bogdan.kirilenko@jetbrains.com>
2024-03-23 01:17:23 +0100

    [pycharm] PY-63670 fixed HF icons class + removed custom CSS styles + minor updates
    (cherry picked from commit 880afa74bc9a34ce192966fd6e69610107bcb0c8)
    
    GitOrigin-RevId: 736fac81acc16c4c92dba7304bd9e8dc6d5e1029

--- python/huggingFace/src/com/intellij/python/community/impl/huggingFace/HuggingFaceConstants.kt
+++ python/huggingFace/src/com/intellij/python/community/impl/huggingFace/HuggingFaceConstants.kt
@@ -8,7 +8,6 @@ object HuggingFaceConstants {
   const val HF_API_FETCH_PAGE_SIZE = 500
   const val MAX_MODELS_IN_CACHE = HF_API_FETCH_PAGE_SIZE * 20
   const val MAX_DATASETS_IN_CACHE = HF_API_FETCH_PAGE_SIZE * 20
-  const val HF_EMOJI = "\uD83E\uDD17"
   const val API_FETCH_SORT_KEY = "likes"
   const val DATASET_FAKE_PIPELINE_TAG = "dataset"
   const val UNDEFINED_PIPELINE_TAG = "undefined"
