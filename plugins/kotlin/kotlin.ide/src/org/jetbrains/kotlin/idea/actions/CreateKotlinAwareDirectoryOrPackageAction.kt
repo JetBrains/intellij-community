3b241602628a700da1ebe2834e951644e70305dd
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2024-03-23 19:04:46 +0100

    [kotlin] Adjust isPackage
    #KTIJ-12018
    
    GitOrigin-RevId: e74ff16a2e60bbe58eedbe9375cb614f01d0d8a3

--- plugins/kotlin/kotlin.ide/src/org/jetbrains/kotlin/idea/actions/CreateKotlinAwareDirectoryOrPackageAction.kt
+++ plugins/kotlin/kotlin.ide/src/org/jetbrains/kotlin/idea/actions/CreateKotlinAwareDirectoryOrPackageAction.kt
@@ -5,14 +5,16 @@ import com.intellij.ide.actions.CreateDirectoryOrPackageAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.psi.PsiDirectory
-import org.jetbrains.kotlin.idea.base.util.KOTLIN_AWARE_SOURCE_ROOT_TYPES
+import org.jetbrains.kotlin.idea.base.util.KOTLIN_SOURCE_ROOT_TYPES
 
 class CreateKotlinAwareDirectoryOrPackageAction: CreateDirectoryOrPackageAction() {
     override fun isPackage(project: Project, directories: List<PsiDirectory>): Boolean {
+        if (super.isPackage(project, directories)) return true
+
         val fileIndex = ProjectRootManager.getInstance(project).getFileIndex()
         return directories.any {
             val virtualFile = it.getVirtualFile()
-            fileIndex.isUnderSourceRootOfType(virtualFile, KOTLIN_AWARE_SOURCE_ROOT_TYPES)
+            fileIndex.isUnderSourceRootOfType(virtualFile, KOTLIN_SOURCE_ROOT_TYPES)
         }
     }
 }
