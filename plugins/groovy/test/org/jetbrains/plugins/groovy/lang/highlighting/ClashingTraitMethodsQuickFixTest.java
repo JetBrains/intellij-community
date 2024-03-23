115e4d012a4c36ac5feb1f3e143b18c892c3e0f0
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-03-23 12:53:26 +0100

    [Groovy] convert ClashingTraitMethodsQuickFixTest to Java (IDEA-327339)
    GitOrigin-RevId: 2ae2ad48b74dd142666a22b3660209ac9572c4f3

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/ClashingTraitMethodsQuickFixTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/ClashingTraitMethodsQuickFixTest.java
@@ -1,45 +1,50 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.lang.highlighting
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.highlighting;
 
-import org.jetbrains.plugins.groovy.GroovyBundle
-import org.jetbrains.plugins.groovy.codeInspection.confusing.ClashingTraitMethodsInspection
+import org.jetbrains.plugins.groovy.GroovyBundle;
+import org.jetbrains.plugins.groovy.codeInspection.confusing.ClashingTraitMethodsInspection;
+import org.jetbrains.plugins.groovy.intentions.GrIntentionTestCase;
+
 /**
  * Created by Max Medvedev on 09/06/14
  */
-class ClashingTraitMethodsQuickFixTest /*extends GrIntentionTestCase*/ {
-  ClashingTraitMethodsQuickFixTest() {
-    super(GroovyBundle.message("declare.explicit.implementations.of.trait"), ClashingTraitMethodsInspection)
+public class ClashingTraitMethodsQuickFixTest extends GrIntentionTestCase {
+  public ClashingTraitMethodsQuickFixTest() {
+    super(GroovyBundle.message("declare.explicit.implementations.of.trait"), ClashingTraitMethodsInspection.class);
   }
 
-  void testQuickFix() {
-    doTextTest('''\
-trait T1 {
-    def foo(){}
-}
+  /**
+   * Somewhat incorrect, the overriding method return type should be `def` instead of `Object`
+   */
+  public void testQuickFix() {
+    doTextTest("""
+                 trait T1 {
+                     def foo(){}
+                 }
 
-trait T2 {
-    def foo(){}
-}
+                 trait T2 {
+                     def foo(){}
+                 }
 
-class <caret>A implements T1, T2 {
+                 class <caret>A implements T1, T2 {
 
-}
-''', '''\
-trait T1 {
-    def foo(){}
-}
+                 }
+                 """, """
+                 trait T1 {
+                     def foo(){}
+                 }
 
-trait T2 {
-    def foo(){}
-}
+                 trait T2 {
+                     def foo(){}
+                 }
 
-class A implements T1, T2 {
+                 class A implements T1, T2 {
 
-    @Override
-    def foo() {
-        return T2.super.foo()
-    }
-}
-''')
+                     @Override
+                     Object foo() {
+                         return T2.super.foo()
+                     }
+                 }
+                 """);
   }
 }
