7cecc1ed7443c88aa2b2dba18a2e0b05960cb829
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2024-03-25 04:11:54 +0100

    [vcs-log-graph] extract LinearGraphWrapper class to represent a graph with added or removed edges
    GitOrigin-RevId: 1d7c15ed869c1ff03a8617fbc2f80231fff61377

--- platform/vcs-log/graph/src/com/intellij/vcs/log/graph/linearBek/LinearBekController.kt
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/graph/linearBek/LinearBekController.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.log.graph.linearBek
 
 import com.intellij.openapi.diagnostic.Logger
@@ -95,8 +95,8 @@ class LinearBekController(controller: BekBaseController, permanentGraphInfo: Per
     return object : LinearGraphAnswer(GraphChangesUtil.SOME_CHANGES) {
       override fun getGraphUpdater(): Runnable {
         return Runnable {
-          compiledGraph.myDottedEdges.removeAll()
-          compiledGraph.myHiddenEdges.removeAll()
+          compiledGraph.dottedEdges.removeAll()
+          compiledGraph.hiddenEdges.removeAll()
         }
       }
     }
