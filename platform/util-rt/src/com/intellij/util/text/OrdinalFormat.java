8fd9255ad4d2dfbe16478286258a1b49dec1e904
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2024-03-25 16:13:26 +0100

    IJPL-797 `intellij.platform.util.rt`: clean up
    GitOrigin-RevId: eb1f96ce180ea8ac596d33cb376733e6e92209dd

--- platform/util-rt/src/com/intellij/util/text/OrdinalFormat.java
+++ platform/util-rt/src/com/intellij/util/text/OrdinalFormat.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.text;
 
 import java.text.*;
@@ -24,7 +24,7 @@ public final class OrdinalFormat {
     if (locale != null) {
       String language = locale.getLanguage();
       if ("en".equals(language) ||
-          language != null && language.length() == 0 /*bundle fallback locale*/) {
+          language != null && language.isEmpty() /*the bundle fallback locale*/) {
         return new EnglishOrdinalFormat();
       }
     }
