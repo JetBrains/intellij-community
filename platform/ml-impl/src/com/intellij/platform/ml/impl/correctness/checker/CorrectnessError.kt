2bf59d9bba8f953b7c35231229105815df9de73c
Petr Surkov <petr.surkov@jetbrains.com>
2024-03-27 15:09:42 +0100

    [full-line] ML-2942 Move red code checks and ImportFixer interfaces to ai.impl module
    GitOrigin-RevId: 6a55bd0bee8e873c3d1ca3d6d5cbb83672a019be

--- platform/ml-impl/src/com/intellij/platform/ml/impl/correctness/checker/CorrectnessError.kt
+++
@@ -1,8 +0,0 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.ml.impl.correctness.checker
-
-import com.intellij.openapi.util.TextRange
-import org.jetbrains.annotations.ApiStatus
-
-@ApiStatus.Internal
-data class CorrectnessError(val location: TextRange, val severity: Severity, val debugName: String)
