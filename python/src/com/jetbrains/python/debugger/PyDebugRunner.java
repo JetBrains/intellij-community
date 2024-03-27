18529bef9bee61b6221eda8284980ef26c255c91
Andrey Lisin <andrey.lisin@jetbrains.com>
2024-03-27 10:26:31 +0100

    PY-71546 Add registry flag for enabling debugger diagnostic prints
    GitOrigin-RevId: 27347f89d702def97c95a1c6c782a63f66351a2d

--- python/src/com/jetbrains/python/debugger/PyDebugRunner.java
+++ python/src/com/jetbrains/python/debugger/PyDebugRunner.java
@@ -97,6 +97,7 @@ public class PyDebugRunner implements ProgramRunner<RunnerSettings> {
   public static final @NonNls String PYDEVD_FILTERS = "PYDEVD_FILTERS";
   public static final @NonNls String PYDEVD_FILTER_LIBRARIES = "PYDEVD_FILTER_LIBRARIES";
   public static final @NonNls String PYDEVD_USE_CYTHON = "PYDEVD_USE_CYTHON";
+  public static final @NonNls String PYCHARM_DEBUG = "PYCHARM_DEBUG";
   public static final @NonNls String USE_LOW_IMPACT_MONITORING = "USE_LOW_IMPACT_MONITORING";
   public static final @NonNls String CYTHON_EXTENSIONS_DIR = new File(PathManager.getSystemPath(), "cythonExtensions").toString();
 
@@ -687,6 +688,10 @@ public class PyDebugRunner implements ProgramRunner<RunnerSettings> {
 
     if (!RegistryManager.getInstance().is("python.debug.enable.cython.speedups")) {
       environmentController.putFixedValue(PYDEVD_USE_CYTHON, "NO");
+    }
+
+    if (RegistryManager.getInstance().is("python.debug.enable.diagnostic.prints")) {
+      environmentController.putFixedValue(PYCHARM_DEBUG, "True");
     }
   }
 
