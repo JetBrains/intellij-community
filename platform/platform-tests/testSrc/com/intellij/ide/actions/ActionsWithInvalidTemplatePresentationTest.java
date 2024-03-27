3488280088c9d632a98e1e2b3f9ce61a50cf673c
Aleksey Pivovarov <AMPivovarov@gmail.com>
2024-03-27 12:55:47 +0100

    improve ActionsWithInvalidTemplatePresentationTest
    Catch potential 'Bugs & Fixes' escaping issues in bundled strings ('Bugs \\&\\& Fixes').
    
    GitOrigin-RevId: 9c594089e89a7c484afcc7d1cd03d01eb0ea5906

--- platform/platform-tests/testSrc/com/intellij/ide/actions/ActionsWithInvalidTemplatePresentationTest.java
+++ platform/platform-tests/testSrc/com/intellij/ide/actions/ActionsWithInvalidTemplatePresentationTest.java
@@ -7,6 +7,7 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.actionSystem.impl.ActionManagerImpl;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.util.text.TextWithMnemonic;
 import com.intellij.testFramework.junit5.DynamicTests;
 import com.intellij.testFramework.junit5.NamedFailure;
 import com.intellij.testFramework.junit5.TestApplication;
@@ -21,10 +22,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 @TestApplication
 public class ActionsWithInvalidTemplatePresentationTest {
+  private final Pattern ALLOWED_MNEMONICS = Pattern.compile("[0-9A-Za-z]");
+
   // TODO fix and remove the field
   private static final List<String> ACTIONS_TO_FIX = Arrays.asList(
     "InsertRubyInjection",
@@ -46,6 +50,7 @@ public class ActionsWithInvalidTemplatePresentationTest {
       Presentation presentation = action.getTemplatePresentation();
       String text = presentation.getText();
       String description = presentation.getDescription();
+      TextWithMnemonic fullText = presentation.getTextWithPossibleMnemonic().get();
 
       if (hasTemplates(text) || hasTemplates(description)) {
         failures.add(newFailure(action, id, text, description, "string template markup"));
@@ -57,6 +62,11 @@ public class ActionsWithInvalidTemplatePresentationTest {
 
       if (StringUtil.isEmptyOrSpaces(text) && !StringUtil.isEmptyOrSpaces(description)) {
         failures.add(newFailure(action, id, text, description, "empty text but description is ok"));
+      }
+
+      if (fullText != null && fullText.hasMnemonic() &&
+          !ALLOWED_MNEMONICS.matcher("" + fullText.getMnemonicChar()).matches()) {
+        failures.add(newFailure(action, id, text, description, "invalid mnemonic: '" + fullText.getMnemonicChar() + "'"));
       }
     }
     return DynamicTests.asDynamicTests(failures, "incorrect project settings");
