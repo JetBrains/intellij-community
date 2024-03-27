d24874fa761b6b4d7211128ff408424ca3517b13
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2024-03-27 09:32:08 +0100

    [vfs] IDEA-350058: system property to control VFS protection from concurrent access by another process
    - By default: VFS fails IDE startup if detects that it's storage is currently in use by another (alive) process
    -`-Dvfs.fail-if-used-by-another-process=false` to disable that -- error will be logged instead
    
    GitOrigin-RevId: 6d6707a5add2113680adc989fe14522894300d83

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/PersistentFSRecordsStorageFactory.kt
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/PersistentFSRecordsStorageFactory.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vfs.newvfs.persistent
 
+import com.intellij.util.SystemProperties
 import com.intellij.util.io.*
 import com.intellij.util.io.dev.StorageFactory
 import com.intellij.util.io.dev.mmapped.MMappedFileStorageFactory
@@ -26,29 +27,51 @@ abstract class PersistentFSRecordsStorageFactory(val id: Int) : StorageFactory<P
         val recordsStorage = PersistentFSRecordsLockFreeOverMMappedFile(mappedFileStorage)
 
         if (acquireStorageOwnership) {
-          val ownPid = ProcessHandle.current().pid().toInt()
-          val acquiredByPid = recordsStorage.tryAcquireExclusiveAccess(ownPid, /*forcibly: */ false)
-          if (acquiredByPid != ownPid) {
+          val currentProcessPid = ProcessHandle.current().pid().toInt()
+          val acquiredByPid = recordsStorage.tryAcquireExclusiveAccess(currentProcessPid, /*forcibly: */ false)
+          if (acquiredByPid != currentProcessPid) {
             val ownerProcess = ProcessHandle.of(acquiredByPid.toLong()).getOrNull()
             if (ownerProcess != null && ownerProcess.isAlive) {
               //OSes re-use process ids, so false positives (pid collision) are possible.
               //But this branch is reached only after improper shutdown, and we can't 100% guarantee VFS recovery after such
               // a shutdowns anyway. So pid collision is just another case of bad luck -- it won't be a recovery, but a VFS
               // rebuild.
-              //MAYBE RC: we could do a bit better by checking (how?) that an ownerProcess runs JVM -- this should greatly
-              //          reduce collision probability
-              throw StorageAlreadyInUseException("Records storage [$storagePath] is in use by another process [pid: $acquiredByPid, info: ${ownerProcess.info()}]")
+              //MAYBE RC: we could do a bit better by checking that an ownerProcess runs a JVM -- this should greatly
+              //          reduce collision probability. But how to do that?
+              //          1. Check ownerProcess.info().command()=='java': not reliable, it could be 'idea', or something else.
+              //          2. Check .command() == thisProcess.command(): not reliable, it could any _other_ JB app process using
+              //             this process's VFS -- this is exactly the scenario we're trying to catch with that 'storage ownership'
+              //             check
+              //          3. Check .commandLine().contains('JetBrains'): better, since most of our tools has 'JetBrains' either in
+              //             exec path, or somewhere in classpath -- but again, it leaves out rare cases there VFS is acquired by
+              //             something _unusual_
+              //          4. Timestamp: store not only the pid, but also a _timestamp_ of VFS acquisition -- compare this timestamp
+              //             with ownerProcess.startInstant. If ownerProcess happened to be started _after_ VFS was acquired -- it
+              //             is definitely a false positive, just a pid collision: original process that really acquired VFS has
+              //             already died, and it's pid is now re-used by a new process. This seems to be the most reliable option,
+              //             but it needs additional development (i.e. additional header field in records to store timestamp)
+
+              val message = "Records storage [$storagePath] is in use by another process [pid: $acquiredByPid, info: ${ownerProcess.info()}]"
+              val failIfUsedByAnotherProcess = SystemProperties.getBooleanProperty("vfs.fail-if-used-by-another-process", true)
+              if (failIfUsedByAnotherProcess) {
+                throw StorageAlreadyInUseException(message)
+              }
+              else {
+                FSRecords.LOG.error(message)
+              }
+            }
+            else {
+              FSRecords.LOG.warn("Records storage [$storagePath] was in use by process [pid: $acquiredByPid] which is not exist now (wasn't closed properly/crashed?) -> re-acquiring forcibly")
             }
 
             //Previous owner process is terminated, i.e. storage wasn't closed properly on an app termination (crash?)
             // => acquire storage forcibly
-            val acquiredByPidForcibly = recordsStorage.tryAcquireExclusiveAccess(ownPid, /*forcibly: */ true)
-            if (acquiredByPidForcibly != ownPid) {
-              //yet another process acquired records concurrently => whole scenario looks too damned, just fail:
+            val acquiredByPidForcibly = recordsStorage.tryAcquireExclusiveAccess(currentProcessPid, /*forcibly: */ true)
+            if (acquiredByPidForcibly != currentProcessPid) {
+              //yet another process acquired records concurrently => too suspicious, just fail:
               val concurrentlyOwnedProcess = ProcessHandle.of(acquiredByPidForcibly.toLong()).getOrNull()
               throw StorageAlreadyInUseException("Records storage [$storagePath] is in use by another process [pid: $acquiredByPidForcibly, info: ${concurrentlyOwnedProcess?.info()}]")
             }
-            FSRecords.LOG.warn("Records storage [$storagePath] was in use by process [pid: $acquiredByPid] which is not exist now (wasn't closed properly/crashed?) -> re-acquired forcibly")
           }
         }
 
