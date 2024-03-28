9d07acce13966ee5f47f56f51a512e81957f5f49
Alexander Zolotov <zolotov@jetbrains.com>
2024-03-29 00:21:04 +0100

    [textmate] convert java to kotlin
    GitOrigin-RevId: 32ab284c735f2b853467e2e82619e43abd9e9b93

--- plugins/textmate/core/src/org/jetbrains/plugins/textmate/plist/JsonPlistReader.kt
+++ plugins/textmate/core/src/org/jetbrains/plugins/textmate/plist/JsonPlistReader.kt
@@ -1,98 +1,90 @@
-package org.jetbrains.plugins.textmate.plist;
+package org.jetbrains.plugins.textmate.plist
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.json.JsonReadFeature;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.fasterxml.jackson.core.JsonFactory
+import com.fasterxml.jackson.core.json.JsonReadFeature
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.nio.charset.StandardCharsets
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import static org.jetbrains.plugins.textmate.plist.PListValue.value;
-
-public final class JsonPlistReader implements PlistReader {
-  @Override
-  public Plist read(@NotNull InputStream inputStream) throws IOException {
-    return internalRead(createJsonReader().readValue(new InputStreamReader(inputStream, StandardCharsets.UTF_8), Object.class));
+class JsonPlistReader : PlistReader {
+  @Throws(IOException::class)
+  override fun read(inputStream: InputStream): Plist {
+    return internalRead(createJsonReader().readValue(InputStreamReader(inputStream, StandardCharsets.UTF_8), Any::class.java))
   }
 
-  private static Plist internalRead(Object root) throws IOException {
-    if (!(root instanceof Map)) {
-      throw new IOException("Unknown json format. Root element is '" + root + "'");
+  companion object {
+    @Throws(IOException::class)
+    private fun internalRead(root: Any): Plist {
+      if (root !is Map<*, *>) {
+        throw IOException("Unknown json format. Root element is '$root'")
+      }
+
+      return readDict(root as Map<String, Any>).value as Plist
     }
 
-    //noinspection unchecked
-    return (Plist)readDict((Map<String, Object>)root).getValue();
-  }
+    private fun readDict(map: Map<String, Any>): PListValue {
+      val dict = Plist()
+      for ((key, value1) in map) {
+        val value = readValue(value1)
+        if (value != null) {
+          dict.setEntry(key, value)
+        }
+      }
 
-  private static PListValue readDict(@NotNull Map<String, Object> map) {
-    Plist dict = new Plist();
-    for (Map.Entry<String, Object> entry : map.entrySet()) {
-      PListValue value = readValue(entry.getValue());
-      if (value != null) {
-        dict.setEntry(entry.getKey(), value);
+      return PListValue.value(dict, PlistValueType.DICT)
+    }
+
+    private fun readValue(value: Any): PListValue? {
+      return if (value is Map<*, *>) {
+        readDict(value as Map<String, Any>)
+      }
+      else if (value is ArrayList<*>) {
+        readArray(value)
+      }
+      else {
+        readBasicValue(value)
       }
     }
 
-    return value(dict, PlistValueType.DICT);
-  }
-
-  @Nullable
-  private static PListValue readValue(Object value) {
-    if (value instanceof Map) {
-      //noinspection unchecked
-      return readDict((Map<String, Object>)value);
-    }
-    else if (value instanceof ArrayList) {
-      return readArray((ArrayList<?>)value);
-    }
-    else {
-      return readBasicValue(value);
-    }
-  }
-
-  private static PListValue readArray(ArrayList<?> list) {
-    List<Object> result = new ArrayList<>();
-    for (Object o : list) {
-      PListValue value = readValue(o);
-      if (value != null) {
-        result.add(value);
+    private fun readArray(list: ArrayList<*>): PListValue {
+      val result: MutableList<Any> = ArrayList()
+      for (o in list) {
+        val value = readValue(o)
+        if (value != null) {
+          result.add(value)
+        }
       }
+      return PListValue.value(result, PlistValueType.ARRAY)
     }
-    return value(result, PlistValueType.ARRAY);
-  }
 
-  @Nullable
-  private static PListValue readBasicValue(Object value) {
-    if (value instanceof String) {
-      return value(value, PlistValueType.STRING);
+    private fun readBasicValue(value: Any): PListValue? {
+      if (value is String) {
+        return PListValue.value(value, PlistValueType.STRING)
+      }
+      else if (value is Boolean) {
+        return PListValue.value(value, PlistValueType.BOOLEAN)
+      }
+      else if (value is Int) {
+        return PListValue.value(value, PlistValueType.INTEGER)
+      }
+      else if (value is Double) {
+        return PListValue.value(value, PlistValueType.REAL)
+      }
+      return null
     }
-    else if (value instanceof Boolean) {
-      return value(value, PlistValueType.BOOLEAN);
-    }
-    else if (value instanceof Integer) {
-      return value(Long.valueOf((Integer)value), PlistValueType.INTEGER);
-    }
-    else if (value instanceof Double) {
-      return value(value, PlistValueType.REAL);
-    }
-    return null;
-  }
 
-  public static ObjectMapper createJsonReader() {
-    JsonFactory factory = JsonFactory.builder()
-      .enable(JsonReadFeature.ALLOW_JAVA_COMMENTS)
-      .enable(JsonReadFeature.ALLOW_TRAILING_COMMA)
-      .enable(JsonReadFeature.ALLOW_SINGLE_QUOTES)
-      .enable(JsonReadFeature.ALLOW_UNQUOTED_FIELD_NAMES)
-      .build();
-    return new ObjectMapper(factory).configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    @JvmStatic
+    fun createJsonReader(): ObjectMapper {
+      val factory = JsonFactory.builder()
+        .enable(JsonReadFeature.ALLOW_JAVA_COMMENTS)
+        .enable(JsonReadFeature.ALLOW_TRAILING_COMMA)
+        .enable(JsonReadFeature.ALLOW_SINGLE_QUOTES)
+        .enable(JsonReadFeature.ALLOW_UNQUOTED_FIELD_NAMES)
+        .build()
+      return ObjectMapper(factory).configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    }
   }
 }
