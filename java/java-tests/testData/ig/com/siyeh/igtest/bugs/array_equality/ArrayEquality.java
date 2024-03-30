cf8671367566d1fa25c56ccc013c7088d50d0c69
Rishabh Singh <56164824+rishabhsdev@users.noreply.github.com>
2024-03-30 20:48:15 +0530

    Update ArrayEquality.java
    Fix incorrect warning in ArrayEquality comparison.

--- java/java-tests/testData/ig/com/siyeh/igtest/bugs/array_equality/ArrayEquality.java
+++ java/java-tests/testData/ig/com/siyeh/igtest/bugs/array_equality/ArrayEquality.java
@@ -1,7 +1,7 @@
 class ArrayEquality {
 
   boolean a(String[] a, String[] b) {
-    return a <warning descr="Array objects are compared using '==', not 'Arrays.equals()'">==</warning> b;
+    return a <warning descr="Array objects are compared using 'Arrays.equals()', not '=='">==</warning> b;
   }
 
   boolean b(Number[] n) {
