9e3a9e9d3f5a393809448482a64cdf30d2f566dd
Alex Plate <aleksei.plate@jetbrains.com>
2024-04-02 17:29:34 +0300

    [Workspace Model] Get rid of the second resolve of the module during updating facets
    This "Second resolve" is not necessary and was accidentally added during the refactoring in ab6c5674d189b9449e3caccd0d370fc4f721f6db
    
    GitOrigin-RevId: c2837f149f9f95fd78fe68996eb4ad847fa299bf

--- plugins/kotlin/base/facet/src/org/jetbrains/kotlin/idea/workspaceModel/KotlinFacetBridge.kt
+++ plugins/kotlin/base/facet/src/org/jetbrains/kotlin/idea/workspaceModel/KotlinFacetBridge.kt
@@ -5,7 +5,6 @@ import com.intellij.openapi.externalSystem.ExternalSystemModulePropertyManager
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.roots.ExternalProjectSystemRegistry
 import com.intellij.openapi.roots.ProjectModelExternalSource
-import com.intellij.platform.workspace.jps.entities.ModuleEntity
 import com.intellij.platform.workspace.jps.entities.modifyEntity
 import com.intellij.platform.workspace.storage.MutableEntityStorage
 import com.intellij.workspaceModel.ide.impl.legacyBridge.facet.FacetBridge
@@ -24,35 +23,32 @@ class KotlinFacetBridge(
 
     override fun updateExistingEntityInStorage(existingFacetEntity: KotlinSettingsEntity, mutableStorage: MutableEntityStorage) {
         val moduleEntity = mutableStorage.resolve(existingFacetEntity.moduleId)!!
-        mutableStorage.modifyEntity(moduleEntity) module1@{
-            val kotlinSettingsEntity = config.getEntityBuilder(this@module1)
-            val resolvedModule: ModuleEntity = mutableStorage.resolve(kotlinSettingsEntity.moduleId)!!
-            mutableStorage.modifyEntity(resolvedModule) module2@{
-                mutableStorage.modifyEntity(existingFacetEntity) {
-                    if (kotlinSettingsEntity.flushNeeded) flushNeeded = false
-                    name = kotlinSettingsEntity.name
-                    sourceRoots = kotlinSettingsEntity.sourceRoots.toMutableList()
-                    configFileItems = kotlinSettingsEntity.configFileItems.toMutableList()
-                    moduleId = kotlinSettingsEntity.moduleId
-                    module = this@module2
-                    useProjectSettings = kotlinSettingsEntity.useProjectSettings
-                    implementedModuleNames = kotlinSettingsEntity.implementedModuleNames.toMutableList()
-                    dependsOnModuleNames = kotlinSettingsEntity.dependsOnModuleNames.toMutableList()
-                    additionalVisibleModuleNames = kotlinSettingsEntity.additionalVisibleModuleNames.toMutableSet()
-                    productionOutputPath = kotlinSettingsEntity.productionOutputPath
-                    testOutputPath = kotlinSettingsEntity.testOutputPath
-                    sourceSetNames = kotlinSettingsEntity.sourceSetNames.toMutableList()
-                    isTestModule = kotlinSettingsEntity.isTestModule
-                    externalProjectId = kotlinSettingsEntity.externalProjectId
-                    isHmppEnabled = kotlinSettingsEntity.isHmppEnabled
-                    pureKotlinSourceFolders = kotlinSettingsEntity.pureKotlinSourceFolders.toMutableList()
-                    kind = kotlinSettingsEntity.kind
-                    compilerArguments = kotlinSettingsEntity.compilerArguments
-                    compilerSettings = kotlinSettingsEntity.compilerSettings
-                    targetPlatform = kotlinSettingsEntity.targetPlatform
-                    externalSystemRunTasks = kotlinSettingsEntity.externalSystemRunTasks.toMutableList()
-                    version = kotlinSettingsEntity.version
-                }
+        mutableStorage.modifyEntity(moduleEntity) module@{
+            val kotlinSettingsEntity = config.getEntityBuilder(this@module)
+            mutableStorage.modifyEntity(existingFacetEntity) {
+                if (kotlinSettingsEntity.flushNeeded) flushNeeded = false
+                name = kotlinSettingsEntity.name
+                sourceRoots = kotlinSettingsEntity.sourceRoots.toMutableList()
+                configFileItems = kotlinSettingsEntity.configFileItems.toMutableList()
+                moduleId = kotlinSettingsEntity.moduleId
+                module = this@module
+                useProjectSettings = kotlinSettingsEntity.useProjectSettings
+                implementedModuleNames = kotlinSettingsEntity.implementedModuleNames.toMutableList()
+                dependsOnModuleNames = kotlinSettingsEntity.dependsOnModuleNames.toMutableList()
+                additionalVisibleModuleNames = kotlinSettingsEntity.additionalVisibleModuleNames.toMutableSet()
+                productionOutputPath = kotlinSettingsEntity.productionOutputPath
+                testOutputPath = kotlinSettingsEntity.testOutputPath
+                sourceSetNames = kotlinSettingsEntity.sourceSetNames.toMutableList()
+                isTestModule = kotlinSettingsEntity.isTestModule
+                externalProjectId = kotlinSettingsEntity.externalProjectId
+                isHmppEnabled = kotlinSettingsEntity.isHmppEnabled
+                pureKotlinSourceFolders = kotlinSettingsEntity.pureKotlinSourceFolders.toMutableList()
+                kind = kotlinSettingsEntity.kind
+                compilerArguments = kotlinSettingsEntity.compilerArguments
+                compilerSettings = kotlinSettingsEntity.compilerSettings
+                targetPlatform = kotlinSettingsEntity.targetPlatform
+                externalSystemRunTasks = kotlinSettingsEntity.externalSystemRunTasks.toMutableList()
+                version = kotlinSettingsEntity.version
             }
         }
     }
