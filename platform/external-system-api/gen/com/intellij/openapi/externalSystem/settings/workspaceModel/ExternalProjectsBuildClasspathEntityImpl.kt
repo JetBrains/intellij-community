3b379057385d56a0a9149bdf419757161c6bd341
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2024-04-02 12:26:49 +0200

    [external system][IDEA-306074] Regenerated workspace entities
    GitOrigin-RevId: ffb3cfdd85eedf512ea33155d82cb290a895dbe9

--- platform/external-system-api/gen/com/intellij/openapi/externalSystem/settings/workspaceModel/ExternalProjectsBuildClasspathEntityImpl.kt
+++ platform/external-system-api/gen/com/intellij/openapi/externalSystem/settings/workspaceModel/ExternalProjectsBuildClasspathEntityImpl.kt
@@ -61,7 +61,6 @@ open class ExternalProjectsBuildClasspathEntityImpl(private val dataSource: Exte
       }
 
       this.diff = builder
-      this.snapshot = builder
       addToBuilder()
       this.id = getEntityData().createEntityId()
       // After adding entity data to the builder, we need to unbind it and move the control over entity data to builder
@@ -125,7 +124,6 @@ class ExternalProjectsBuildClasspathEntityData : WorkspaceEntityData<ExternalPro
   override fun wrapAsModifiable(diff: MutableEntityStorage): WorkspaceEntity.Builder<ExternalProjectsBuildClasspathEntity> {
     val modifiable = ExternalProjectsBuildClasspathEntityImpl.Builder(null)
     modifiable.diff = diff
-    modifiable.snapshot = diff
     modifiable.id = createEntityId()
     return modifiable
   }
