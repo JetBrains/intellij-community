a99bdabaa446a320b6b3e33801c077b56f86dc21
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2024-04-03 12:43:31 +0300

    IDEA-249914 Fix macOS resolution scaling in troubleshooting.txt
    Not perfect, but it'll have to do, and better than it was,
    because at least it'll correctly report retina display resolutions
    now. For scaled resolutions, there seems to be no way to figure
    them out, but it doesn't seem necessary for debugging either.
    
    GitOrigin-RevId: 929ee84255dc3a78b6cad742db0ea589e4a8ec74

--- platform/platform-impl/src/com/intellij/ide/troubleshooting/DisplayInfo.kt
+++ platform/platform-impl/src/com/intellij/ide/troubleshooting/DisplayInfo.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.troubleshooting
 
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.ui.ScreenUtil
 import com.intellij.ui.scale.JBUIScale
 import java.awt.GraphicsDevice
@@ -15,24 +16,49 @@ internal data class DisplayInfo(
   companion object {
     @JvmStatic
     fun get(): DisplayInfo = DisplayInfo(
-      GraphicsEnvironment.getLocalGraphicsEnvironment().screenDevices.map { ScreenInfo(it) }
+      GraphicsEnvironment.getLocalGraphicsEnvironment().screenDevices.map { screenInfo(it) }
     )
   }
 }
+
+private fun screenInfo(device: GraphicsDevice): ScreenInfo {
+  val scale = JBUIScale.sysScale(device.defaultConfiguration)
+  val displayMode = device.displayMode
+  val resolutionScale = if (SystemInfo.isMac) {
+    // On macOS, displayMode reports "fake" (scaled) resolution that matches user-space coordinates.
+    // Scaling it will yield the correct resolution on a true retina display.
+    // For "intermediate" displays, such as 27-32" 4K ones, it'll also be fake,
+    // but at least it'll match, e.g., screenshot resolutions.
+    // There seems to be no way to retrieve the actual resolution.
+    // It isn't needed, though, as macOS performs raster scaling on its own.
+    // Therefore, for all intents and purposes, a 32" 4K (3840x2160) monitor
+    // set to 2560x1440 in the macOS settings is "actually" a 5K (5120x2880) monitor scaled at 200%,
+    // as far as our code (both the platform and JBR) is concerned.
+    scale
+  }
+  else {
+    // On Windows, display modes are reliable and return the actual screen resolutions. No need to scale them.
+    // On Linux, it's unpredictable and depends on the environment. In the best case, it works correctly,
+    // otherwise it usually returns the scaled bounds, in the worst case without a way to unscale them
+    // because it reports 200% scaling instead of the actual value.
+    // So the best we can do here is to trust it and hope for the best.
+    1.0f
+  }
+  val resolution = Resolution(scale(displayMode.width, resolutionScale), scale(displayMode.height, resolutionScale))
+  val scaling = Scaling(scale)
+  val bounds = device.defaultConfiguration.bounds
+  val insets = ScreenUtil.getScreenInsets(device.defaultConfiguration)
+  return ScreenInfo(resolution, scaling, bounds, insets)
+}
+
+fun scale(value: Int, scale: Float): Int = (value * scale).roundToInt()
 
 internal data class ScreenInfo(
   val resolution: Resolution,
   val scaling: Scaling,
   val bounds: Rectangle,
   val insets: Insets,
-) {
-  constructor(graphicsDevice: GraphicsDevice) : this(
-    graphicsDevice.displayMode.let { Resolution(it.width, it.height) },
-    Scaling(JBUIScale.sysScale(graphicsDevice.defaultConfiguration)),
-    graphicsDevice.defaultConfiguration.bounds,
-    ScreenUtil.getScreenInsets(graphicsDevice.defaultConfiguration),
-  )
-}
+)
 
 internal data class Resolution(val width: Int, val height: Int) {
   override fun toString(): String = "${width}x${height}"
