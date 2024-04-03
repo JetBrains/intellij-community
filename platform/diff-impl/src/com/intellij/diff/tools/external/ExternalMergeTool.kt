c717fde7829cc7d9d609f3439ac7b18e5666ca70
Aleksey Pivovarov <AMPivovarov@gmail.com>
2024-04-03 12:30:41 +0200

    diff: convert ExternalMergeTool to Kotlin
    GitOrigin-RevId: c397f231ebc5253527b910d3e7c20dcd5f847446

--- platform/diff-impl/src/com/intellij/diff/tools/external/ExternalMergeTool.kt
+++ platform/diff-impl/src/com/intellij/diff/tools/external/ExternalMergeTool.kt
@@ -1,70 +1,69 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.diff.tools.external;
+package com.intellij.diff.tools.external
 
-import com.intellij.diff.DiffManagerEx;
-import com.intellij.diff.contents.DiffContent;
-import com.intellij.diff.contents.DocumentContent;
-import com.intellij.diff.contents.FileContent;
-import com.intellij.diff.merge.MergeRequest;
-import com.intellij.diff.merge.ThreesideMergeRequest;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.diff.DiffBundle;
-import com.intellij.openapi.progress.ProcessCanceledException;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.diff.DiffManagerEx
+import com.intellij.diff.contents.DiffContent
+import com.intellij.diff.contents.DocumentContent
+import com.intellij.diff.contents.FileContent
+import com.intellij.diff.merge.MergeRequest
+import com.intellij.diff.merge.ThreesideMergeRequest
+import com.intellij.diff.tools.external.ExternalDiffSettings.ExternalTool
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diff.DiffBundle
+import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
 
-import java.util.List;
+object ExternalMergeTool {
+  private val LOG = Logger.getInstance(ExternalMergeTool::class.java)
 
-public final class ExternalMergeTool {
-  private static final Logger LOG = Logger.getInstance(ExternalMergeTool.class);
-
-  public static boolean isEnabled() {
-    return ExternalDiffSettings.getInstance().isExternalToolsEnabled();
+  @JvmStatic
+  fun isEnabled(): Boolean {
+    return ExternalDiffSettings.instance.isExternalToolsEnabled
   }
 
-  public static boolean isDefault() {
-    return isEnabled() && ExternalDiffSettings.isNotBuiltinMergeTool();
-  }
+  @JvmStatic
+  fun isDefault(): Boolean = isEnabled() && ExternalDiffSettings.isNotBuiltinMergeTool()
 
-  public static void show(@Nullable final Project project,
-                          @NotNull ExternalDiffSettings.ExternalTool externalTool,
-                          @NotNull final MergeRequest request) {
+  @JvmStatic
+  fun show(project: Project?,
+           externalTool: ExternalTool,
+           request: MergeRequest) {
     try {
       if (canShow(request)) {
-        ExternalDiffToolUtil.executeMerge(project, externalTool, (ThreesideMergeRequest)request, null);
+        ExternalDiffToolUtil.executeMerge(project, externalTool, (request as ThreesideMergeRequest), null)
       }
       else {
-        DiffManagerEx.getInstance().showMergeBuiltin(project, request);
+        DiffManagerEx.getInstance().showMergeBuiltin(project, request)
       }
     }
-    catch (ProcessCanceledException ignore) {
+    catch (ignore: ProcessCanceledException) {
     }
-    catch (Throwable e) {
-      LOG.warn(e);
-      Messages.showErrorDialog(project, e.getMessage(), DiffBundle.message("can.t.show.merge.in.external.tool"));
+    catch (e: Throwable) {
+      LOG.warn(e)
+      Messages.showErrorDialog(project, e.message, DiffBundle.message("can.t.show.merge.in.external.tool"))
     }
   }
 
-  public static boolean canShow(@NotNull MergeRequest request) {
-    if (request instanceof ThreesideMergeRequest) {
-      DiffContent outputContent = ((ThreesideMergeRequest)request).getOutputContent();
-      if (!canProcessOutputContent(outputContent)) return false;
+  @JvmStatic
+  fun canShow(request: MergeRequest): Boolean {
+    if (request is ThreesideMergeRequest) {
+      val outputContent = request.outputContent
+      if (!canProcessOutputContent(outputContent)) return false
 
-      List<? extends DiffContent> contents = ((ThreesideMergeRequest)request).getContents();
-      if (contents.size() != 3) return false;
-      for (DiffContent content : contents) {
-        if (!ExternalDiffToolUtil.canCreateFile(content)) return false;
+      val contents = request.contents
+      if (contents.size != 3) return false
+      for (content in contents) {
+        if (!ExternalDiffToolUtil.canCreateFile(content)) return false
       }
-      return true;
+      return true
     }
-    return false;
+    return false
   }
 
-  private static boolean canProcessOutputContent(@NotNull DiffContent content) {
-    if (content instanceof DocumentContent) return true;
-    if (content instanceof FileContent && ((FileContent)content).getFile().isInLocalFileSystem()) return true;
-    return false;
+  private fun canProcessOutputContent(content: DiffContent): Boolean {
+    if (content is DocumentContent) return true
+    if (content is FileContent && content.file.isInLocalFileSystem) return true
+    return false
   }
 }
