2837db2ba3e7eac9ce72d301398f68d2398703a1
Ivan Semenov <ivan.semenov@jetbrains.com>
2024-04-04 13:55:41 +0200

    [ui] fix RoundedLineBorder hidpi painting
    Painting a rounded rectangle with a simple "drawRoundedRect" leads to clipped/misaligned lines on hi-dpi devices.
    
    GitOrigin-RevId: 3e6ca784b6b4a62176b8400eaf8aa120e5e6a980

--- platform/util/ui/src/com/intellij/ui/RoundedLineBorder.kt
+++ platform/util/ui/src/com/intellij/ui/RoundedLineBorder.kt
@@ -1,36 +1,28 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui
 
+import com.intellij.ui.paint.LinePainter2D
+import com.intellij.ui.paint.RectanglePainter2D
+import org.jetbrains.annotations.ApiStatus
 import java.awt.*
 import javax.swing.border.LineBorder
 
-open class RoundedLineBorder : LineBorder {
-  private var myArcSize = 1
-
-  constructor(color: Color?) : super(color)
-
-  @JvmOverloads
-  constructor(color: Color?, arcSize: Int, thickness: Int = 1) : super(color, thickness) {
-    myArcSize = arcSize
+@ApiStatus.NonExtendable
+open class RoundedLineBorder @JvmOverloads constructor(
+  color: Color?,
+  private val arcDiameter: Int = 1,
+  thickness: Int = 1
+) : LineBorder(color, thickness) {
+  fun setColor(color: Color) {
+    lineColor = color
   }
 
   override fun paintBorder(c: Component, g: Graphics, x: Int, y: Int, width: Int, height: Int) {
-    val g2 = g as Graphics2D
-
-    val oldAntialiasing = g2.getRenderingHint(RenderingHints.KEY_ANTIALIASING)
-    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-    val oldColor = g2.color
-    g2.color = lineColor
-
-    for (i in 0 until thickness) {
-      g2.drawRoundRect(x + i, y + i, width - i - i - 1, height - i - i - 1, myArcSize, myArcSize)
-    }
-
-    g2.color = oldColor
-    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, oldAntialiasing)
-  }
-
-  fun setColor(color: Color) {
-    lineColor = color
+    val g2d = g as? Graphics2D ?: return
+    g2d.color = lineColor
+    RectanglePainter2D.DRAW.paint(g2d,
+                                  x.toDouble(), y.toDouble(), width.toDouble(), height.toDouble(),
+                                  arcDiameter.toDouble(), LinePainter2D.StrokeType.CENTERED, thickness.toDouble(),
+                                  RenderingHints.VALUE_ANTIALIAS_ON)
   }
 }
