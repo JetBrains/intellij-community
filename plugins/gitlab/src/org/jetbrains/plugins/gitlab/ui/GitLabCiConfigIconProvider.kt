e97722e991a9ecf0df654678f9cf1732d876da0c
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2024-04-04 12:45:03 +0200

    [gitlab] IDEA-350674 Get rid of YAML plugin dependency in GitHub and GitLab plugins
    Removed YAML plugin dependency in favor of simple file mask check, also check JSON schema as a backup option
    
    (cherry picked from commit c704bded148be8e404b27f9416ac93f3f2a9459b)
    
    IJ-CR-130700
    
    GitOrigin-RevId: dcf408af1356e30a1d68f78e358d7678a28e6e82

--- plugins/gitlab/src/org/jetbrains/plugins/gitlab/ui/GitLabCiConfigIconProvider.kt
+++ plugins/gitlab/src/org/jetbrains/plugins/gitlab/ui/GitLabCiConfigIconProvider.kt
@@ -3,29 +3,37 @@ package org.jetbrains.plugins.gitlab.ui
 
 import com.intellij.ide.FileIconProvider
 import com.intellij.openapi.components.service
-import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.jsonSchema.ide.JsonSchemaService
 import org.jetbrains.plugins.gitlab.GitlabIcons
-import org.jetbrains.yaml.YAMLFileType
 import javax.swing.Icon
 
 class GitLabCiConfigIconProvider : FileIconProvider {
-
+  private val PARSE_DELAY = 250L
+  private val GITLAB_CI_FILE_MASK = Regex(""".*\.gitlab-ci(\..*)?\.(yaml|yml)""")
   private val GITLAB_SCHEMAS = setOf("ci.json")
+  private val yamlExtensions = setOf("yml", "yaml")
 
   override fun getIcon(file: VirtualFile, flags: Int, project: Project?): Icon? {
 
-    if (project == null || !FileTypeRegistry.getInstance().isFileOfType(file, YAMLFileType.YML)) {
-      return null
-    }
+    val extension = file.extension ?: return null
 
-    val schemaFiles = project.service<JsonSchemaService>().getSchemaFilesForFile(file)
-    if (schemaFiles.any { GITLAB_SCHEMAS.contains(it.name) }) {
+    if (!yamlExtensions.contains(extension.lowercase())) return null
+
+    if (isGitlabCiFile(file)) {
       return GitlabIcons.GitLabLogo
+    } else if (project != null) {
+      val schemaFiles = project.service<JsonSchemaService>().getSchemaFilesForFile(file)
+      if (schemaFiles.any { GITLAB_SCHEMAS.contains(it.name) }) {
+        return GitlabIcons.GitLabLogo
+      }
     }
-
     return null
+  }
+
+  private fun isGitlabCiFile(file: VirtualFile): Boolean {
+    return GITLAB_CI_FILE_MASK.matches(StringUtil.newBombedCharSequence(file.name, PARSE_DELAY))
   }
 }
