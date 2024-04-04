03f3534ab4f35a08d8303128ca318dbd3d232ac7
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-04-04 17:34:46 +0200

    IdDataConsumer - use charArray if possible for improved performance
    GitOrigin-RevId: f41a371675ef606b489d62e7a901b7736e51bf8e

--- platform/indexing-impl/src/com/intellij/psi/impl/cache/impl/id/IdDataConsumer.java
+++ platform/indexing-impl/src/com/intellij/psi/impl/cache/impl/id/IdDataConsumer.java
@@ -3,7 +3,6 @@
 package com.intellij.psi.impl.cache.impl.id;
 
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.util.text.CharArrayCharSequence;
 import it.unimi.dsi.fastutil.ints.Int2IntMap;
 import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 import it.unimi.dsi.fastutil.ints.IntIterator;
@@ -111,17 +110,22 @@ public final class IdDataConsumer {
   }
 
   public void addOccurrence(char[] chars, int start, int end, int occurrenceMask) {
-    addOccurrence(new CharArrayCharSequence(chars), start, end, occurrenceMask);
+    addOccurrence(null, chars, start, end, occurrenceMask);
   }
 
   public void addOccurrence(CharSequence charSequence, int start, int end, int occurrenceMask) {
+    addOccurrence(charSequence, null, start, end, occurrenceMask);
+  }
+
+  private void addOccurrence(CharSequence charSequence, char[] chars, int start, int end, int occurrenceMask) {
     if (end == start || occurrenceMask == 0) return;
     int hash, hashNoCase;
+    boolean hasArray = chars != null;
     if (IdIndexEntry.useStrongerHash()) {
       hash = hashNoCase = 0;
       boolean different = false;
       for (int off = start; off < end; off++) {
-        char c = charSequence.charAt(off);
+        char c = hasArray ? chars[off] : charSequence.charAt(off);
         char lowerC = StringUtil.toLowerCase(c);
         if (!different && c != lowerC) {
           different = true;
@@ -137,12 +141,12 @@ public final class IdDataConsumer {
       }
     }
     else {
-      char firstChar = charSequence.charAt(start);
-      char lastChar = charSequence.charAt(end - 1);
+      char firstChar = hasArray ? chars[start] : charSequence.charAt(start);
+      char lastChar = hasArray ? chars[end - 1] : charSequence.charAt(end - 1);
       hash = (firstChar << 8) + (lastChar << 4) + end - start;
       char firstCharLower = StringUtil.toLowerCase(firstChar);
       char lastCharLower = StringUtil.toLowerCase(lastChar);
-      hashNoCase = (firstCharLower == firstChar && lastCharLower == lastChar) ? hash : 
+      hashNoCase = (firstCharLower == firstChar && lastCharLower == lastChar) ? hash :
                    (firstCharLower << 8) + (lastCharLower << 4) + end - start;
     }
     myOccurrences.mergeInt(hash, occurrenceMask, (prev, cur) -> prev | cur);
