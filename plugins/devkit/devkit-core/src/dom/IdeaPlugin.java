0aeb6c91635a8b8ec56cf523d9b7f7081948be05
Yann Cébron <yann.cebron@jetbrains.com>
2024-04-04 12:03:29 +0200

    [devkit] DOM: remove obsolete "on-demand" attribute (IDEA-297005)
    GitOrigin-RevId: aca260207fa8830f5ea5e682b9b59a92ba2ba9cf

--- plugins/devkit/devkit-core/src/dom/IdeaPlugin.java
+++ plugins/devkit/devkit-core/src/dom/IdeaPlugin.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.dom;
 
 import com.intellij.ide.plugins.PluginManagerCore;
@@ -62,9 +62,6 @@ public interface IdeaPlugin extends DomElement {
   @NotNull GenericAttributeValue<Boolean> getAllowBundledUpdate();
 
   @NotNull GenericAttributeValue<Boolean> getImplementationDetail();
-
-  @ApiStatus.Experimental
-  @NotNull GenericAttributeValue<Boolean> getOnDemand();
 
   @NotNull GenericAttributeValue<Boolean> getRequireRestart();
 
@@ -153,7 +150,7 @@ public interface IdeaPlugin extends DomElement {
   @NotNull List<? extends Listeners> getProjectListeners();
 
   /**
-   * @deprecated the corresponding tag in plugin.xml is not supported anymore, this method is used to highlight occurrences of such tag  
+   * @deprecated the corresponding tag in plugin.xml is not supported anymore, this method is used to highlight occurrences of such tag
    */
   @Deprecated
   @NotNull List<? extends Helpset> getHelpsets();
