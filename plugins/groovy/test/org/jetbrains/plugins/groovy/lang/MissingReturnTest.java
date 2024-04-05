466698df5541c47f8a11c983bfaae2ed05a5545c
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-04-05 12:48:32 +0200

    [Groovy] convert MissingReturnTest to Java (IDEA-327339)
    GitOrigin-RevId: 23b4223ef04f7488e3f7ced4c5a08d6ff648b1da

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/MissingReturnTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/MissingReturnTest.java
@@ -1,331 +1,313 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jetbrains.plugins.groovy.lang
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang;
 
+import org.jetbrains.plugins.groovy.LightGroovyTestCase;
+import org.jetbrains.plugins.groovy.codeInspection.noReturnMethod.MissingReturnInspection;
+import org.jetbrains.plugins.groovy.util.TestUtils;
 
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
-import org.jetbrains.plugins.groovy.codeInspection.noReturnMethod.MissingReturnInspection
-import org.jetbrains.plugins.groovy.util.TestUtils
-
-class MissingReturnTest extends LightGroovyTestCase {
-
+public class MissingReturnTest extends LightGroovyTestCase {
   @Override
   protected String getBasePath() {
-    return "${TestUtils.testDataPath}highlighting/missingReturn"
+    return TestUtils.getTestDataPath() + "highlighting/missingReturn";
   }
 
-  void testMissingReturnWithLastLoop() throws Throwable { doTest() }
+  public void testMissingReturnWithLastLoop() { doTest(); }
 
-  void testMissingReturnWithUnknownCall() throws Throwable { doTest() }
+  public void testMissingReturnWithUnknownCall() { doTest(); }
 
-  void testMissingReturnWithIf() throws Throwable { doTest() }
+  public void testMissingReturnWithIf() { doTest(); }
 
-  void testMissingReturnWithAssertion() throws Throwable { doTest() }
+  public void testMissingReturnWithAssertion() { doTest(); }
 
-  void testMissingReturnThrowException() throws Throwable { doTest() }
+  public void testMissingReturnThrowException() { doTest(); }
 
-  void testMissingReturnTryCatch() throws Throwable { doTest() }
+  public void testMissingReturnTryCatch() { doTest(); }
 
-  void testMissingReturnLastNull() throws Throwable { doTest() }
+  public void testMissingReturnLastNull() { doTest(); }
 
-  void testMissingReturnImplicitReturns() throws Throwable { doTest() }
+  public void testMissingReturnImplicitReturns() { doTest(); }
 
-  void testMissingReturnOvertReturnType() throws Throwable { doTest() }
+  public void testMissingReturnOvertReturnType() { doTest(); }
 
-  void testMissingReturnFromClosure() throws Throwable { doTest() }
+  public void testMissingReturnFromClosure() { doTest(); }
 
-  void testReturnsWithoutValue() throws Throwable { doTest() }
+  public void testReturnsWithoutValue() { doTest(); }
 
-  void testEndlessLoop() throws Throwable { doTest() }
+  public void testEndlessLoop() { doTest(); }
 
-  void testEndlessLoop2() throws Throwable { doTest() }
+  public void testEndlessLoop2() { doTest(); }
 
-  void testExceptionWithFinally() throws Throwable { doTest() }
+  public void testExceptionWithFinally() { doTest(); }
 
-  void testOnlyAssert() throws Throwable { doTest() }
+  public void testOnlyAssert() { doTest(); }
 
-  void testImplicitReturnNull() throws Throwable { doTest() }
+  public void testImplicitReturnNull() { doTest(); }
 
-  void testMissingReturnInClosure() { doTest() }
+  public void testMissingReturnInClosure() { doTest(); }
 
-  void testFinally() { doTest() }
+  public void testFinally() { doTest(); }
 
-  void testClosureWithExplicitExpectedType() { doTest() }
+  public void testClosureWithExplicitExpectedType() { doTest(); }
 
-  void testAssert() {
-    doTextText('''\
-Integer.with {
-  assert valueof('1') == 1
-}                         //no error
+  public void testAssert() {
+    doTextText("""
+                 Integer.with {
+                   assert valueof('1') == 1
+                 }                         //no error
 
-Integer.with {
-  if (foo) {
-    return 2
-  }
-  else {
-    print 1
-  }
-<warning descr="Not all execution paths return a value">}</warning>
-''')
-  }
-
-
-  void testInterruptFlowInElseBranch() {
-    doTextText('''\
-//correct
-public int foo(int bar) {
-    if (bar < 0) {
-        return -1
-    }
-    else if (bar > 0) {
-        return 12
-    }
-    else {
-        throw new IllegalArgumentException('bar cannot be zero!')
-    }
-}
-
-//incorrect
-public int foo2(int bar) {
-    if (bar < 0) {
-        return -1
-    }
-    else if (bar > 0) {
-        return 12
-    }
-<warning descr="Not all execution paths return a value">}</warning>
-''')
+                 Integer.with {
+                   if (foo) {
+                     return 2
+                   }
+                   else {
+                     print 1
+                   }
+                 <warning descr="Not all execution paths return a value">}</warning>
+                 """);
   }
 
-  void testSwitch() {
-    doTextText('''\
-//correct
-String foo(e) {
-    switch(e) {
-        case 1: 1; break
-        case 2: return 2; break
-        default: 3
-    }
-}
+  public void testInterruptFlowInElseBranch() {
+    doTextText("""
+                 //correct
+                 public int foo(int bar) {
+                     if (bar < 0) {
+                         return -1
+                     }
+                     else if (bar > 0) {
+                         return 12
+                     }
+                     else {
+                         throw new IllegalArgumentException('bar cannot be zero!')
+                     }
+                 }
 
-//incorrect
-String foo2(e) {
-    switch(e) {
-        case 1: 1; break
-        case 2: break
-        default: 3
-    }
-<warning descr="Not all execution paths return a value">}</warning>
-''')
+                 //incorrect
+                 public int foo2(int bar) {
+                     if (bar < 0) {
+                         return -1
+                     }
+                     else if (bar > 0) {
+                         return 12
+                     }
+                 <warning descr="Not all execution paths return a value">}</warning>
+                 """);
   }
 
-  void testSwitchWithIf() {
-    doTextText('''\
-//correct
-String foo(e) {
-    switch(e) {
-        case 1:
-            if (e=='a') {
-                return 'a'
-            }
-            else {
-                'c'
-            }
-            break
-        default: ''
-    }
-}
+  public void testSwitch() {
+    doTextText("""
+                 //correct
+                 String foo(e) {
+                     switch(e) {
+                         case 1: 1; break
+                         case 2: return 2; break
+                         default: 3
+                     }
+                 }
 
-//incorrect
-String foo2(e) {
-    switch(e) {
-        case 1:
-            if (e=='a') {
-                return 'a'
-            }
-            else {
-           //     'c'
-            }
-            break
-        default: ''
-    }
-<warning descr="Not all execution paths return a value">}</warning>
-''')
+                 //incorrect
+                 String foo2(e) {
+                     switch(e) {
+                         case 1: 1; break
+                         case 2: break
+                         default: 3
+                     }
+                 <warning descr="Not all execution paths return a value">}</warning>
+                 """);
   }
 
-  void testConditional() {
-    doTextText('''\
-//correct
-List createFilters1() {
-    abc ? [1] : []
-}
+  public void testSwitchWithIf() {
+    doTextText("""
+                 //correct
+                 String foo(e) {
+                     switch(e) {
+                         case 1:
+                             if (e=='a') {
+                                 return 'a'
+                             }
+                             else {
+                                 'c'
+                             }
+                             break
+                         default: ''
+                     }
+                 }
 
-//correct
-List createFilters2() {
-    abc ?: [1]
-}
-
-//incorrect
-List createFilters3() {
-<warning descr="Not all execution paths return a value">}</warning>
-''')
+                 //incorrect
+                 String foo2(e) {
+                     switch(e) {
+                         case 1:
+                             if (e=='a') {
+                                 return 'a'
+                             }
+                             else {
+                            //     'c'
+                             }
+                             break
+                         default: ''
+                     }
+                 <warning descr="Not all execution paths return a value">}</warning>
+                 """);
   }
 
-  void testReturnWithoutValue0() {
-    doTextText('''\
-int foo() {
-  if (abc) {
-    return
+  public void testConditional() {
+    doTextText("""
+                 //correct
+                 List createFilters1() {
+                     abc ? [1] : []
+                 }
+
+                 //correct
+                 List createFilters2() {
+                     abc ?: [1]
+                 }
+
+                 //incorrect
+                 List createFilters3() {
+                 <warning descr="Not all execution paths return a value">}</warning>
+                 """);
   }
 
-  return 2
-<warning>}</warning>
-''')
+  public void testReturnWithoutValue0() {
+    doTextText("""
+                 int foo() {
+                   if (abc) {
+                     return
+                   }
+
+                   return 2
+                 <warning>}</warning>
+                 """);
   }
 
-  void testReturnWithoutValue1() {
-    doTextText('''\
-int foo() {
-  return
-<warning>}</warning>
-''')
+  public void testReturnWithoutValue1() {
+    doTextText("""
+                 int foo() {
+                   return
+                 <warning>}</warning>
+                 """);
   }
 
-  void testReturnWithoutValue2() {
-    doTextText('''\
-void foo() {
-  if (abc) {
-    return
+  public void testReturnWithoutValue2() {
+    doTextText("""
+                 void foo() {
+                   if (abc) {
+                     return
+                   }
+
+                   print 2
+                 } //no error
+                 """);
   }
 
-  print 2
-} //no error
-''')
+  public void testReturnWithoutValue3() {
+    doTextText("""
+                 void foo() {
+                   return
+                 } //no error
+                 """);
   }
 
-  void testReturnWithoutValue3() {
-    doTextText('''\
-void foo() {
-  return
-} //no error
-''')
+  public void testSingleThrow1() {
+    doTextText("""
+                 int foo() {
+                   throw new RuntimeException()
+                 } //correct
+                 """);
   }
 
-  void testSingleThrow1() {
-    doTextText('''
-      int foo() {
-        throw new RuntimeException()
-      } //correct
-    ''')
+  public void testSingleThrow2() {
+    doTextText("""
+                 void foo() {
+                   throw new RuntimeException()
+                 } //correct
+                 """);
   }
 
-  void testSingleThrow2() {
-    doTextText('''
-      void foo() {
-        throw new RuntimeException()
-      } //correct
-    ''')
+  public void testSingleThrow3() {
+    doTextText("""
+                 def foo() {
+                   throw new RuntimeException()
+                 } //correct
+                 """);
   }
 
-  void testSingleThrow3() {
-    doTextText('''
-      def foo() {
-        throw new RuntimeException()
-      } //correct
-    ''')
+  public void testThrowFromIf1() {
+    doTextText("""
+                 int foo() {
+                   if (1) {
+                     throw new RuntimeException()
+                   }
+                 <warning>}</warning>
+                 """);
   }
 
-  void testThrowFromIf1() {
-    doTextText('''
-      int foo() {
-        if (1) {
-          throw new RuntimeException()
-        }
-      <warning>}</warning>
-    ''')
+  public void testThrowFromIf2() {
+    doTextText("""
+                 void foo() {
+                   if (1) {
+                     throw new RuntimeException()
+                   }
+                 } //correct
+                 """);
   }
 
-  void testThrowFromIf2() {
-    doTextText('''
-      void foo() {
-        if (1) {
-          throw new RuntimeException()
-        }
-      } //correct
-    ''')
+  public void testThrowFromIf3() {
+    doTextText("""
+                 def foo() {
+                   if (1) {
+                     throw new RuntimeException()
+                   }
+                 } //correct
+                 """);
   }
 
-  void testThrowFromIf3() {
-    doTextText('''
-      def foo() {
-        if (1) {
-          throw new RuntimeException()
-        }
-      } //correct
-    ''')
+  public void testThrowFromIf4() {
+    doTextText("""
+                 int foo() {
+                   if (1) {
+                     throw new RuntimeException()
+                   }
+                   else {
+                     return 1
+                   }
+                 }
+                 """);
   }
 
-  void testThrowFromIf4() {
-    doTextText('''
-      int foo() {
-        if (1) {
-          throw new RuntimeException()
-        }
-        else {
-          return 1
-        }
-      }
-    ''')
+  public void testThrowFromIf5() {
+    doTextText("""
+                 void foo() {
+                   if (1) {
+                     throw new RuntimeException()
+                   }
+                   else {
+                     throw new RuntimeException()
+                   }
+                 } //correct
+                 """);
   }
 
-  void testThrowFromIf5() {
-    doTextText('''
-      void foo() {
-        if (1) {
-          throw new RuntimeException()
-        }
-        else {
-          throw new RuntimeException()
-        }
-      } //correct
-    ''')
+  public void testThrowFromIf6() {
+    doTextText("""
+                 def foo() {
+                   if (1) {
+                     throw new RuntimeException()
+                   }
+                   else {
+                     return 1
+                   }
+                 } //correct
+                 """);
   }
 
-  void testThrowFromIf6() {
-    doTextText('''
-      def foo() {
-        if (1) {
-          throw new RuntimeException()
-        }
-        else {
-          return 1
-        }
-      } //correct
-    ''')
-  }
-
-  void doTextText(String text) {
-    myFixture.configureByText('___.groovy', text)
-    myFixture.enableInspections(MissingReturnInspection)
-    myFixture.testHighlighting(true, false, false)
+  public void doTextText(String text) {
+    myFixture.configureByText("___.groovy", text);
+    myFixture.enableInspections(MissingReturnInspection.class);
+    myFixture.testHighlighting(true, false, false);
   }
 
   private void doTest() {
-    myFixture.enableInspections(new MissingReturnInspection())
-    myFixture.testHighlighting(true, false, false, getTestName(false) + ".groovy")
+    myFixture.enableInspections(new MissingReturnInspection());
+    myFixture.testHighlighting(true, false, false, getTestName(false) + ".groovy");
   }
-
 }
