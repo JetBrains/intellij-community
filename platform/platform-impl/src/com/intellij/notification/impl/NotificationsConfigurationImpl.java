00dd142554b57305c880f73df0ba154ccdf6c7c6
Vyacheslav Moklev <vyacheslav.moklev@jetbrains.com>
2024-04-05 14:20:46 +0300

    [RDCT] RDCT-1224: Fix NotificationsConfigurationImpl so settings can load true values
    IJ-MR-130819
    
    GitOrigin-RevId: 65fb8975b8005a7f211c3d3a63addb406928305b

--- platform/platform-impl/src/com/intellij/notification/impl/NotificationsConfigurationImpl.java
+++ platform/platform-impl/src/com/intellij/notification/impl/NotificationsConfigurationImpl.java
@@ -222,15 +222,11 @@ public final class NotificationsConfigurationImpl extends NotificationsConfigura
     }
     doRemove("Log Only");
 
-    if ("false".equals(state.getAttributeValue(SHOW_BALLOONS_ATTRIBUTE))) {
-      //noinspection NonPrivateFieldAccessedInSynchronizedContext
-      SHOW_BALLOONS = false;
-    }
+    //noinspection NonPrivateFieldAccessedInSynchronizedContext
+    SHOW_BALLOONS = !"false".equals(state.getAttributeValue(SHOW_BALLOONS_ATTRIBUTE));
 
-    if ("false".equals(state.getAttributeValue(SYSTEM_NOTIFICATIONS_ATTRIBUTE))) {
-      //noinspection NonPrivateFieldAccessedInSynchronizedContext
-      SYSTEM_NOTIFICATIONS = false;
-    }
+    //noinspection NonPrivateFieldAccessedInSynchronizedContext
+    SYSTEM_NOTIFICATIONS = !"false".equals(state.getAttributeValue(SYSTEM_NOTIFICATIONS_ATTRIBUTE));
 
     NotificationAnnouncingMode announcingMode = NotificationAnnouncingMode.get(state.getAttributeValue(NOTIFICATION_ANNOUNCING_MODE_ATTRIBUTE));
     if (announcingMode != null) {
