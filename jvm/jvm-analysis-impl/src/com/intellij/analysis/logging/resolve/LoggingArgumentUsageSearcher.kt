df9b489f0d774554301d256a559393d70fbbc1bf
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2024-04-05 16:09:16 +0300

    [Java. Logging] Represent usage search for the logging argument symbols as collection of queries
    IDEA-342484
    
    GitOrigin-RevId: ef33bddec45ca221d9e81ce26209aff115afabda

--- jvm/jvm-analysis-impl/src/com/intellij/analysis/logging/resolve/LoggingArgumentUsageSearcher.kt
+++ jvm/jvm-analysis-impl/src/com/intellij/analysis/logging/resolve/LoggingArgumentUsageSearcher.kt
@@ -6,16 +6,18 @@ import com.intellij.find.usages.api.Usage
 import com.intellij.find.usages.api.UsageSearchParameters
 import com.intellij.find.usages.api.UsageSearcher
 import com.intellij.model.psi.PsiSymbolReference
+import com.intellij.util.Query
 import com.siyeh.ig.format.DefUsage
 
 class LoggingArgumentUsageSearcher : UsageSearcher {
-  override fun collectImmediateResults(parameters: UsageSearchParameters): Collection<Usage> {
+  override fun collectSearchRequests(parameters: UsageSearchParameters): Collection<Query<out Usage>> {
     val target = parameters.target
     if (target !is LoggingArgumentSymbol) return emptyList()
     val uLiteralExpression = target.getPlaceholderString() ?: return emptyList()
     return getLogArgumentReferences(uLiteralExpression)?.let {
-      it.filter { ref: PsiSymbolReference -> ref.resolvesTo(target) }
-        .map(PsiUsage::textUsage) + DefUsage(target.expression)
+      val psiUsageList = it.filter { ref: PsiSymbolReference -> ref.resolvesTo(target) }
+                           .map(PsiUsage::textUsage) + DefUsage(target.expression)
+      psiUsageList.map { psiUsage -> LoggingArgumentPsiUsageQuery(psiUsage) }
     } ?: emptyList()
   }
 }
