00b96d94c9cec28dd31cdfd4972ccee5040c170f
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-04-05 10:13:31 +0200

    [Groovy] convert MethodReferenceTest to Java (IDEA-327339)
    GitOrigin-RevId: 461bc0e2d2670622af7311a3a4b6e2341d91da9d

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/MethodReferenceTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/MethodReferenceTest.java
@@ -1,135 +1,148 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.lang
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang;
 
-import com.intellij.codeInspection.LocalInspectionTool
-import com.intellij.psi.search.searches.MethodReferencesSearch
-import com.intellij.testFramework.LightProjectDescriptor
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.codeInspection.assignment.GroovyAssignabilityCheckInspection
-import org.jetbrains.plugins.groovy.codeInspection.untypedUnresolvedAccess.GrUnresolvedAccessInspection
-import org.jetbrains.plugins.groovy.lang.psi.impl.statements.typedef.members.GrConstructorImpl
-import org.jetbrains.plugins.groovy.lang.psi.impl.statements.typedef.members.GrMethodImpl
-import org.jetbrains.plugins.groovy.lang.psi.impl.synthetic.DefaultConstructor
-import org.jetbrains.plugins.groovy.lang.resolve.GroovyResolveTestCase
-import org.jetbrains.plugins.groovy.util.HighlightingTest
-import org.jetbrains.plugins.groovy.util.ResolveTest
-import org.jetbrains.plugins.groovy.util.TestUtils
-import org.jetbrains.plugins.groovy.util.TypingTest
+import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.search.searches.MethodReferencesSearch;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.util.containers.ContainerUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.LightGroovyTestCase;
+import org.jetbrains.plugins.groovy.codeInspection.assignment.GroovyAssignabilityCheckInspection;
+import org.jetbrains.plugins.groovy.lang.psi.api.GroovyResolveResult;
+import org.jetbrains.plugins.groovy.lang.psi.impl.statements.typedef.members.GrConstructorImpl;
+import org.jetbrains.plugins.groovy.lang.psi.impl.statements.typedef.members.GrMethodImpl;
+import org.jetbrains.plugins.groovy.lang.psi.impl.synthetic.DefaultConstructor;
+import org.jetbrains.plugins.groovy.util.HighlightingTest;
+import org.jetbrains.plugins.groovy.util.ResolveTest;
+import org.jetbrains.plugins.groovy.util.TestUtils;
+import org.jetbrains.plugins.groovy.util.TypingTest;
 
-@CompileStatic
-class MethodReferenceTest extends GroovyResolveTestCase implements TypingTest, ResolveTest, HighlightingTest {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_3_0
-  final String basePath = TestUtils.testDataPath + 'lang/methodReferences/'
-  final Collection<Class<? extends LocalInspectionTool>> inspections = [GrUnresolvedAccessInspection as Class<? extends LocalInspectionTool>, GroovyAssignabilityCheckInspection]
-
-  void 'test method reference in static context'() {
-    fixture.addFileToProject 'classes.groovy', '''\
-class C { 
-  C(int a) {}
-  static foo() {}
-}
-'''
-    def results = multiResolveByText 'C::<caret>foo'
-    assert results.size() == 1
-    assert results[0].element instanceof GrMethodImpl
+public class MethodReferenceTest extends LightGroovyTestCase implements TypingTest, ResolveTest, HighlightingTest {
+  public void testMethodReferenceInStaticContext() {
+    myFixture.addFileToProject("classes.groovy", """
+      class C {\s
+        C(int a) {}
+        static foo() {}
+      }
+      """);
+    Collection<? extends GroovyResolveResult> results = multiResolveByText("C::<caret>foo");
+    assertEquals(1, results.size());
+    assertTrue(ContainerUtil.getFirstItem(results).getElement() instanceof GrMethodImpl);
   }
 
-  void 'test multiple constructors'() {
-    def results = multiResolveByText '''\
-class D {
-  D(a) {}
-  D(a, b) {}
-}
-D::<caret>new
-'''
-    assert results.size() == 2
-    for (result in results) {
-      def element = results[0].element
-      assert element instanceof GrConstructorImpl
+  public void testMultipleConstructors() {
+    Collection<? extends GroovyResolveResult> results = multiResolveByText("""
+                                                                             class D {
+                                                                               D(a) {}
+                                                                               D(a, b) {}
+                                                                             }
+                                                                             D::<caret>new
+                                                                             """);
+    assertEquals(2, results.size());
+    for (GroovyResolveResult result : results) {
+      assertTrue(result.getElement() instanceof GrConstructorImpl);
     }
   }
 
-  void 'test constructor in static context'() {
-    def results = multiResolveByText '''\
-class D { D() {} }
-D::<caret>new
-'''
-    assert results.size() == 1
-    assert results[0].element instanceof GrConstructorImpl
+  public void testConstructorInStaticContext() {
+    Collection<? extends GroovyResolveResult> results = multiResolveByText("""
+                                                                             class D { D() {} }
+                                                                             D::<caret>new
+                                                                             """);
+    assertEquals(1, results.size());
+    assertTrue(ContainerUtil.getFirstItem(results).getElement() instanceof GrConstructorImpl);
   }
 
-  void 'test static method in static context'() {
-    def results = multiResolveByText '''\
-class D { static 'new'() {} }
-D::<caret>new
-'''
-    assert results.size() == 2
-    assert results[0].element instanceof GrMethodImpl
-    assert results[1].element instanceof DefaultConstructor
+  public void testStaticMethodInStaticContext() {
+    Collection<? extends GroovyResolveResult> results = multiResolveByText("""
+                                                                             class D { static 'new'() {} }
+                                                                             D::<caret>new
+                                                                             """);
+    assertEquals(2, results.size());
+    Iterator<? extends GroovyResolveResult> iterator = results.iterator();
+    assertTrue(iterator.next().getElement() instanceof GrMethodImpl);
+    assertTrue(iterator.next().getElement() instanceof DefaultConstructor);
   }
 
-
-  void 'test constructor in instance context'() {
-    def results = multiResolveByText '''\
-class D { D() {} }
-def d = new D()
-d::<caret>new
-'''
-    assert results.size() == 0
+  public void testConstructorInInstanceContext() {
+    Collection<? extends GroovyResolveResult> results = multiResolveByText("""
+                                                                             class D { D() {} }
+                                                                             def d = new D()
+                                                                             d::<caret>new
+                                                                             """);
+    assertEmpty(results);
   }
 
-  void 'test instance method in instance context'() {
-    def results = multiResolveByText '''\
-class D { def 'new'() {} }
-def d = new D()
-d::<caret>new
-'''
-    assert results.size() == 1
-    assert results[0].element instanceof GrMethodImpl
+  public void testInstanceMethodInInstanceContext() {
+    Collection<? extends GroovyResolveResult> results = multiResolveByText("""
+                                                                             class D { def 'new'() {} }
+                                                                             def d = new D()
+                                                                             d::<caret>new
+                                                                             """);
+    assertEquals(1, results.size());
+    assertTrue(ContainerUtil.getFirstItem(results).getElement() instanceof GrMethodImpl);
   }
 
-  void 'test typing'() {
-    fixture.addFileToProject 'classes.groovy', '''\
-class D {
-  def prop1
-  D() {}
-  D(int a) {}
-  static long 'new'(String b) { 42l }
-}
-'''
-    expressionTypeTest 'def ref = D::new; ref(1)', 'D'
-    expressionTypeTest 'def ref = D::new; ref("hello")', 'long'
+  public void testTyping() {
+    myFixture.addFileToProject("classes.groovy", """
+      class D {
+        def prop1
+        D() {}
+        D(int a) {}
+        static long 'new'(String b) { 42l }
+      }
+      """);
+    expressionTypeTest("def ref = D::new; ref(1)", "D");
+    expressionTypeTest("def ref = D::new; ref(\"hello\")", "long");
   }
 
-  void 'test array typing'() {
-    expressionTypeTest 'Integer[]::new(0)', 'java.lang.Integer[]'
-    expressionTypeTest 'int[][]::new(0)', 'int[][]'
-    expressionTypeTest 'int[][]::new(1, 2)', 'int[][]'
+  public void testArrayTyping() {
+    expressionTypeTest("Integer[]::new(0)", "java.lang.Integer[]");
+    expressionTypeTest("int[][]::new(0)", "int[][]");
+    expressionTypeTest("int[][]::new(1, 2)", "int[][]");
   }
 
-  void 'test constructors highlighting'() { fileHighlightingTest() }
+  public void testConstructorsHighlighting() { fileHighlightingTest(); }
 
-  void 'test array constructors highlighting'() { fileHighlightingTest() }
+  public void testArrayConstructorsHighlighting() { fileHighlightingTest(); }
 
-  void 'test constructor search and rename'() {
-    fixture.addFileToProject 'classes.groovy', 'class A { A(String p){} }'
-    configureByText '''\
-<caret>A::new
-A::'new'
-A::"new"
-'''
+  public void testConstructorSearchAndRename() {
+    myFixture.addFileToProject("classes.groovy", "class A { A(String p){} }");
+    configureByText("""
+                      <caret>A::new
+                      A::'new'
+                      A::"new"
+                      """);
 
-    def clazz = fixture.findClass 'A'
-    def constructor = clazz.constructors[0]
-    assert MethodReferencesSearch.search(constructor).size() == 3
+    PsiClass clazz = myFixture.findClass("A");
+    PsiMethod constructor = clazz.getConstructors()[0];
+    assertEquals(3, MethodReferencesSearch.search(constructor).findAll().size());
 
-    fixture.renameElementAtCaret 'B'
-    fixture.checkResult '''\
-B::new
-B::'new'
-B::"new"
-'''
+    myFixture.renameElementAtCaret("B");
+    myFixture.checkResult("""
+                               B::new
+                               B::'new'
+                               B::"new"
+                               """);
+  }
+
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_3_0;
+  }
+
+  public final String getBasePath() {
+    return TestUtils.getTestDataPath() + "lang/methodReferences/";
+  }
+
+  public final @NotNull List<Class<? extends LocalInspectionTool>> getInspections() {
+    return List.of(GroovyAssignabilityCheckInspection.class);
   }
 }
