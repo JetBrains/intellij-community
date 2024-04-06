5251fd206a202a9d081048d464fcc3220d9361f1
Mikhail Sokolov <mikhail.sokolov@jetbrains.com>
2024-04-06 22:47:50 +0200

    IDEA-350938 "Now is commercial" banner
    GitOrigin-RevId: ce848668a84936fcb3c2ba20a3a27d7294754dc8

---
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/BannerImageProviderImpl.kt
@@ -0,0 +1,39 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.wm.impl
+
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.progress.util.BackgroundTaskUtil
+import com.intellij.openapi.ui.BannerImageProvider
+import com.intellij.util.SVGLoader
+import java.awt.Image
+import java.io.IOException
+import java.net.URL
+
+private const val TIMEOUT = 500L
+private val LOG = Logger.getInstance(OnboardingBackgroundImageProviderBase::class.java)
+
+open class BannerImageProviderImpl: BannerImageProvider {
+
+  protected open fun getImageUrl(): URL? = null
+
+  override fun getIDEBanner(): Image? {
+    return getImageUrl()?.let {
+      BackgroundTaskUtil.tryComputeFast(
+        { progressIndicator ->
+          try {
+            return@tryComputeFast SVGLoader.load(it, 1f)
+          }
+          catch (e: IOException) {
+            LOG.warn("Onboarding image loading failed: $e")
+            return@tryComputeFast null
+          }
+          finally {
+            if (progressIndicator.isCanceled) {
+              LOG.warn("Onboarding image loading failed: it took longer than $TIMEOUT ms")
+            }
+          }
+        }, TIMEOUT)
+    }
+
+  }
+}
