faf35d030972a1cb4d84ae295a4fb1081c43fc49
Tagir Valeev <tagir.valeev@jetbrains.com>
2024-04-08 11:26:28 +0200

    [java-refactoring] Extract delegate: fix when field has eol-comment (IDEA-350071)
    GitOrigin-RevId: 6647c1d115ec2b49334f647ffaf8c0981d24862d

---
+++ java/java-tests/testData/refactoring/extractClass/innerClass2/after/Test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class test {
+
+    public static class Extracted {
+        List<String> mylist = new ArrayList<String>(); // this is my comment
+
+        public Extracted() {
+        }
+    }
+}
