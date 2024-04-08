c5f82b2ccf9c41202ac5beb1d6a7b7303598b64f
Lev Serebryakov <lev.serebryakov@jetbrains.com>
2024-04-08 22:11:34 +0200

    IJPL-53: Enable new lock by default.
    Make "true" default, but leave the possibility to set option back to "false" in system properties.
    
    GitOrigin-RevId: 93e9720258ad422b330c033e4f149ff89e574e0e

--- platform/core-api/src/com/intellij/openapi/application/RuntimeFlags.kt
+++ platform/core-api/src/com/intellij/openapi/application/RuntimeFlags.kt
@@ -7,4 +7,4 @@ private const val ENABLE_NEW_LOCK_PROPERTY = "idea.enable.new.lock"
 
 @get:ApiStatus.Internal
 val isNewLockEnabled: Boolean
-  get() = System.getProperty(ENABLE_NEW_LOCK_PROPERTY, "false").toBoolean()
+  get() = System.getProperty(ENABLE_NEW_LOCK_PROPERTY, "true").toBoolean()
