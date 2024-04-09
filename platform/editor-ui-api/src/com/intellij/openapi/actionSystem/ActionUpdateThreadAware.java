6c19703b0a5c5d45c3e9741981c630ec37af6449
Yann Cébron <yann.cebron@jetbrains.com>
2024-04-09 11:30:00 +0200

    [platform] ActionUpdateThread: javadoc cleanup
    GitOrigin-RevId: 588ecf08e0bc899dcc4a4b3e114662c2b72ba9c1

--- platform/editor-ui-api/src/com/intellij/openapi/actionSystem/ActionUpdateThreadAware.java
+++ platform/editor-ui-api/src/com/intellij/openapi/actionSystem/ActionUpdateThreadAware.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.actionSystem;
 
 import com.intellij.diagnostic.PluginException;
@@ -12,7 +12,7 @@ import org.jetbrains.annotations.NotNull;
 public interface ActionUpdateThreadAware {
   /**
    * Specifies the thread and the way {@link AnAction#update(AnActionEvent)},
-   * {@link ActionGroup#getChildren(AnActionEvent)} or other update-like method shall be called.
+   * {@link ActionGroup#getChildren(AnActionEvent)} or other update-like methods shall be called.
    */
   default @NotNull ActionUpdateThread getActionUpdateThread() {
     if (this instanceof UpdateInBackground && ((UpdateInBackground)this).isUpdateInBackground()) {
@@ -20,7 +20,7 @@ public interface ActionUpdateThreadAware {
     }
     PluginException.reportDeprecatedUsage(
       getClass(), "ActionUpdateThread.OLD_EDT",
-      "'" + getClass().getName() + "' must override `getActionUpdateThread` and chose EDT or BGT. See ActionUpdateThread javadoc.");
+      "'" + getClass().getName() + "' must override `getActionUpdateThread()` and chose EDT or BGT. See ActionUpdateThread javadoc.");
     return ActionUpdateThread.OLD_EDT;
   }
 
