a710fe7b9d58e46dbecc8b46e5bd3d347cc87383
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-04-09 12:18:45 +0200

    KTIJ-29329 Failed to save settings: "Cannot get KotlinCompilationErrorFileTimeStamps component state"
    GitOrigin-RevId: af1eafe4088ff6b033c00b55999b5e940b85de15

--- plugins/kotlin/base/statistics/src/org/jetbrains/kotlin/idea/statistics/KotlinLibraryUsageImportProcessor.kt
+++ plugins/kotlin/base/statistics/src/org/jetbrains/kotlin/idea/statistics/KotlinLibraryUsageImportProcessor.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.statistics
 
 import com.intellij.internal.statistic.libraryUsage.LibraryUsageImportProcessor
@@ -8,9 +8,11 @@ import org.jetbrains.kotlin.idea.references.mainReference
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtImportDirective
 import org.jetbrains.kotlin.psi.psiUtil.getQualifiedElementSelector
+import org.jetbrains.kotlin.utils.addToStdlib.UnsafeCastFunction
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 class KotlinLibraryUsageImportProcessor : LibraryUsageImportProcessor<KtImportDirective> {
+    @OptIn(UnsafeCastFunction::class)
     override fun imports(file: PsiFile): List<KtImportDirective> = file.safeAs<KtFile>()?.importDirectives.orEmpty()
 
     override fun isSingleElementImport(import: KtImportDirective): Boolean = !import.isAllUnder
