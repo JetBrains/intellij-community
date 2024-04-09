4fc2449557faa08f51a0e51aa3ee388f4a7c2297
Christopher Chianelli <christopher@timefold.ai>
2024-04-09 01:50:54 -0400

    fix: Add bounds check and adjust argument index for class methods

--- python/python-psi-impl/src/com/jetbrains/python/codeInsight/lambda/PyLambdaTypeProvider.java
+++ python/python-psi-impl/src/com/jetbrains/python/codeInsight/lambda/PyLambdaTypeProvider.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.codeInsight.lambda;
 
+import com.jetbrains.python.ast.PyAstFunction;
 import com.jetbrains.python.psi.*;
 import com.jetbrains.python.psi.types.PyType;
 import com.jetbrains.python.psi.types.PyTypeProviderBase;
@@ -30,7 +31,13 @@ public class PyLambdaTypeProvider extends PyTypeProviderBase {
             if (callee instanceof PyReferenceExpression referenceExpression) {
               var reference = referenceExpression.getReference().resolve();
               if (reference instanceof PyFunction function) {
-                return function.getParameters(context).get(argumentIndex).getType(context);
+                if (function.getModifier() != PyAstFunction.Modifier.STATICMETHOD && function.getContainingClass() != null) {
+                  argumentIndex++;
+                }
+                var functionParameters = function.getParameters(context);
+                if (argumentIndex < functionParameters.size()) {
+                  return functionParameters.get(argumentIndex).getType(context);
+                }
               }
             }
           }
