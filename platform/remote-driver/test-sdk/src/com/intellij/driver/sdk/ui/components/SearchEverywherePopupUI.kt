5e8ec2560364573c178e4c6cf7c872d6f8c13d01
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2024-04-09 19:06:38 +0200

    ui-tests: polish SearchEverywherePopupUI
    GitOrigin-RevId: 40812bf13ae9de1c06cb2f855e8b7545b4a0d866

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/SearchEverywherePopupUI.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/SearchEverywherePopupUI.kt
@@ -5,12 +5,12 @@ import com.intellij.driver.sdk.ui.should
 import org.intellij.lang.annotations.Language
 
 
-fun Finder.searchEverywherePopup(@Language("xpath") xpath: String? = null) = x(xpath ?: "//div[@class='HeavyWeightWindow']",
+fun Finder.searchEverywherePopup(@Language("xpath") xpath: String? = null) = x(xpath ?: "//div[@class='HeavyWeightWindow'][//div[contains(@class, 'SearchEverywhereUI')]]",
                                                                                SearchEverywherePopupUI::class.java)
 
 class SearchEverywherePopupUI(data: ComponentData): PopupUiComponent(data) {
   private val searchField = x("//div[@class='SearchField']")
-  private val resultsList = x("//div[@class='JBList']")
+  val resultsList = x("//div[@class='JBList']", JListUiComponent::class.java)
 
   fun searchAndChooseFirst(text: String) {
     keyboard {
