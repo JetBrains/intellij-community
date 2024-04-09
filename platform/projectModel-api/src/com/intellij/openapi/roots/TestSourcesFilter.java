b0dee19d841ccd159d054e10132c501ece122642
Yann Cébron <yann.cebron@jetbrains.com>
2024-04-09 15:36:56 +0200

    [platform] TestSourcesFilter: javadoc cleanup
    GitOrigin-RevId: 32f4cc32f9fef533a4242d2f5f4ec4701131bbc7

--- platform/projectModel-api/src/com/intellij/openapi/roots/TestSourcesFilter.java
+++ platform/projectModel-api/src/com/intellij/openapi/roots/TestSourcesFilter.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.roots;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
@@ -9,19 +9,20 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.model.module.JpsModuleSourceRootType;
 
 /**
- * Implementations of this extension point can tell IDE whether some particular file is a test file.
+ * Determine whether some particular file is a test file.
  * <p>
  * By default, IntelliJ Platform considers files as tests only if they are located under test
  * sources root {@link FileIndex#isInTestSourceContent(VirtualFile)}.
  * <p>
- * However there are plenty frameworks and languages which keep test files just nearby production files.
- * E.g. *_test.go files are test files in Go language and some js/dart files are test files depending
- * on their content. The extensions allow IDE to highlight such files with a green background,
+ * However, there are plenty of frameworks and languages which keep test files just nearby production files.
+ * E.g. {@code *_test.go} files are test files in Go language, and some JS/Dart files are test files depending
+ * on their content.
+ * The extensions allow the IDE to highlight such files with a green background,
  * properly check if they are included in built-in search scopes, etc.
  *
+ * @author zolotov
  * @see FileIndex#isInTestSourceContent(VirtualFile)
  * @see JpsModuleSourceRootType#isForTests()
- * @author zolotov
  */
 public abstract class TestSourcesFilter {
   private static final ExtensionPointName<TestSourcesFilter> EP_NAME = ExtensionPointName.create("com.intellij.testSourcesFilter");
