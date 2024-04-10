96caf789a89e2535c4292e0298e2abd4ff60ca1a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-04-10 08:04:57 +0200

    Reapply "IJPL-973 refactor ModuleGraph and PluginSetBuilder classes (don't expose)"
    This reverts commit e67e7b9a7857873d1705dae6d39ae2c5c0580bc7.
    
    GitOrigin-RevId: 4933b129128c72c1c84a8b1987eef826c94c19f0

--- platform/core-impl/src/com/intellij/ide/plugins/ModuleGraph.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/ModuleGraph.kt
@@ -10,47 +10,40 @@ import org.jetbrains.annotations.ApiStatus
 import java.util.*
 
 @ApiStatus.Internal
-interface ModuleGraph : Graph<IdeaPluginDescriptorImpl> {
-  fun getDependencies(descriptor: IdeaPluginDescriptorImpl): Collection<IdeaPluginDescriptorImpl>
-
-  fun getDependents(descriptor: IdeaPluginDescriptorImpl): Collection<IdeaPluginDescriptorImpl>
-}
-
-@ApiStatus.Internal
-open class ModuleGraphBase protected constructor(
+class ModuleGraph internal constructor(
+  @JvmField val topologicalComparator: Comparator<IdeaPluginDescriptorImpl>,
   private val modules: Collection<IdeaPluginDescriptorImpl>,
   private val directDependencies: Map<IdeaPluginDescriptorImpl, Collection<IdeaPluginDescriptorImpl>>,
   private val directDependents: Map<IdeaPluginDescriptorImpl, Collection<IdeaPluginDescriptorImpl>>,
-) : ModuleGraph {
+) : Graph<IdeaPluginDescriptorImpl> {
   override fun getNodes(): Collection<IdeaPluginDescriptorImpl> = Collections.unmodifiableCollection(modules)
 
-  override fun getDependencies(descriptor: IdeaPluginDescriptorImpl): Collection<IdeaPluginDescriptorImpl> {
+  fun getDependencies(descriptor: IdeaPluginDescriptorImpl): Collection<IdeaPluginDescriptorImpl> {
     return getOrEmpty(directDependencies, descriptor)
   }
 
   override fun getIn(descriptor: IdeaPluginDescriptorImpl): Iterator<IdeaPluginDescriptorImpl> = getDependencies(descriptor).iterator()
 
-  override fun getDependents(descriptor: IdeaPluginDescriptorImpl): Collection<IdeaPluginDescriptorImpl> {
-    return getOrEmpty(directDependents, descriptor)
-  }
+  fun getDependents(descriptor: IdeaPluginDescriptorImpl): Collection<IdeaPluginDescriptorImpl> = getOrEmpty(directDependents, descriptor)
 
   override fun getOut(descriptor: IdeaPluginDescriptorImpl): Iterator<IdeaPluginDescriptorImpl> = getDependents(descriptor).iterator()
 
   fun builder(): DFSTBuilder<IdeaPluginDescriptorImpl> = DFSTBuilder(this, null, true)
 
-  internal fun sorted(builder: DFSTBuilder<IdeaPluginDescriptorImpl> = builder()): SortedModuleGraph {
-    return SortedModuleGraph(
-      topologicalComparator = toCoreAwareComparator(builder.comparator()),
-      modules = modules,
-      directDependencies = directDependencies,
-      directDependents = directDependents,
+  internal fun sorted(builder: DFSTBuilder<IdeaPluginDescriptorImpl> = builder()): ModuleGraph {
+    val topologicalComparator = toCoreAwareComparator(builder.comparator())
+    return ModuleGraph(
+      topologicalComparator = topologicalComparator,
+      modules = modules.sortedWith(topologicalComparator),
+      directDependencies = copySorted(directDependencies, topologicalComparator),
+      directDependents = copySorted(directDependents, topologicalComparator)
     )
   }
 }
 
 private val VCS_ALIAS_ID = PluginId.getId("com.intellij.modules.vcs")
 
-internal fun createModuleGraph(plugins: Collection<IdeaPluginDescriptorImpl>): ModuleGraphBase {
+internal fun createModuleGraph(plugins: Collection<IdeaPluginDescriptorImpl>): ModuleGraph {
   val moduleMap = HashMap<String, IdeaPluginDescriptorImpl>(plugins.size * 2)
   val modules = ArrayList<IdeaPluginDescriptorImpl>(moduleMap.size)
   for (module in plugins) {
@@ -116,7 +109,12 @@ internal fun createModuleGraph(plugins: Collection<IdeaPluginDescriptorImpl>): M
     }
   }
 
-  return object : ModuleGraphBase(modules = modules, directDependencies = directDependencies, directDependents = directDependents) {}
+  return ModuleGraph(
+    topologicalComparator = Comparator { _, _ -> 0 },
+    modules = modules,
+    directDependencies = directDependencies,
+    directDependents = directDependents,
+  )
 }
 
 // alias in most cases points to Core plugin, so, we cannot use computed dependencies to check
@@ -137,21 +135,12 @@ private fun toCoreAwareComparator(comparator: Comparator<IdeaPluginDescriptorImp
   }
 }
 
-private fun getOrEmpty(map: Map<IdeaPluginDescriptorImpl, Collection<IdeaPluginDescriptorImpl>>,
-                       descriptor: IdeaPluginDescriptorImpl): Collection<IdeaPluginDescriptorImpl> {
+private fun getOrEmpty(
+  map: Map<IdeaPluginDescriptorImpl, Collection<IdeaPluginDescriptorImpl>>,
+  descriptor: IdeaPluginDescriptorImpl,
+): Collection<IdeaPluginDescriptorImpl> {
   return map.getOrDefault(descriptor, Collections.emptyList())
 }
-
-class SortedModuleGraph(
-  val topologicalComparator: Comparator<IdeaPluginDescriptorImpl>,
-  modules: Collection<IdeaPluginDescriptorImpl>,
-  directDependencies: Map<IdeaPluginDescriptorImpl, Collection<IdeaPluginDescriptorImpl>>,
-  directDependents: Map<IdeaPluginDescriptorImpl, Collection<IdeaPluginDescriptorImpl>>,
-) : ModuleGraphBase(
-  modules = modules.sortedWith(topologicalComparator),
-  directDependencies = copySorted(directDependencies, topologicalComparator),
-  directDependents = copySorted(directDependents, topologicalComparator)
-)
 
 private fun copySorted(
   map: Map<IdeaPluginDescriptorImpl, Collection<IdeaPluginDescriptorImpl>>,
