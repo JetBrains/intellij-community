5075c129ddae91fd2fdf1db4c5fb3fbcca9c0845
Sebastiano Poggi <poggos@gmail.com>
2024-04-10 10:28:08 +0200

    Bump version to 0.17.1
    GitOrigin-RevId: 7cf7f01e014c0e60d08e19c724b4c6a1c82024a8

--- platform/jewel/samples/standalone/src/main/kotlin/org/jetbrains/jewel/samples/standalone/view/markdown/MarkdownEditor.kt
+++ platform/jewel/samples/standalone/src/main/kotlin/org/jetbrains/jewel/samples/standalone/view/markdown/MarkdownEditor.kt
@@ -1,7 +1,6 @@
 package org.jetbrains.jewel.samples.standalone.view.markdown
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,7 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import com.darkrockstudios.libraries.mpfilepicker.FilePicker
