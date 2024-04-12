5a72177ac5fa94aa4b7b7df3486bd68960660372
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-04-12 11:42:56 +0200

    EA-1143924 Fix NPE and add a fallback
    GitOrigin-RevId: f2a6ba00321d96c291565917cb6d5ecd52f28f3b

--- platform/lang-impl/src/com/intellij/lang/documentation/QuickDocHighlightingHelper.kt
+++ platform/lang-impl/src/com/intellij/lang/documentation/QuickDocHighlightingHelper.kt
@@ -2,8 +2,10 @@
 package com.intellij.lang.documentation
 
 import com.intellij.lang.Language
+import com.intellij.lang.LanguageParserDefinitions
 import com.intellij.lang.documentation.DocumentationMarkup.*
 import com.intellij.lang.documentation.DocumentationSettings.InlineCodeHighlightingMode
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.editor.colors.EditorColorsScheme
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.editor.markup.TextAttributes
@@ -287,10 +289,16 @@ object QuickDocHighlightingHelper {
                                                   code: CharSequence, isForRenderedDoc: Boolean, trim: Boolean): StringBuilder {
     val processedCode = code.toString().trim('\n', '\r').replace(' ', ' ')
       .applyIf(trim) { trimEnd() }
-    if (language != null && doHighlighting) {
-      HtmlSyntaxInfoUtil.appendHighlightedByLexerAndEncodedAsHtmlCodeSnippet(
-        this, project, language, processedCode,
-        trim, DocumentationSettings.getHighlightingSaturation(isForRenderedDoc))
+    if (language != null && doHighlighting && LanguageParserDefinitions.INSTANCE.forLanguage(language) != null) {
+      try {
+        HtmlSyntaxInfoUtil.appendHighlightedByLexerAndEncodedAsHtmlCodeSnippet(
+          this, project, language, processedCode,
+          trim, DocumentationSettings.getHighlightingSaturation(isForRenderedDoc))
+      }
+      catch (e: Exception) {
+        thisLogger().error("Failed to highlight code fragment with language $language", e)
+        append(XmlStringUtil.escapeString(processedCode.applyIf(trim) { trimIndent() }))
+      }
     }
     else {
       append(XmlStringUtil.escapeString(processedCode.applyIf(trim) { trimIndent() }))
