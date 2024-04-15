83bd46a634926bbbae39fff7f83d198fb2c30964
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2024-04-15 22:29:06 +0200

    IJPL-872 fix library licences
    GitOrigin-RevId: 629c63da06a2d5d1982cb0a3a045c0c640ef408b

--- platform/build-scripts/src/org/jetbrains/intellij/build/CommunityLibraryLicenses.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/CommunityLibraryLicenses.kt
@@ -247,6 +247,8 @@ object CommunityLibraryLicenses {
     LibraryLicense(name = "batik", libraryName = "batik-transcoder", url = "https://xmlgraphics.apache.org/batik/")
       .apache("https://xmlgraphics.apache.org/batik/license.html")
       .suppliedByOrganizations("The Apache Software Foundation"),
+    LibraryLicense(name = "bifurcan", libraryName = "io.lacuna:bifurcan", url = "https://github.com/lacuna/bifurcan")
+      .mit("https://github.com/lacuna/bifurcan/blob/master/LICENSE"),
     LibraryLicense(libraryName = "blockmap",
                    url = "https://github.com/JetBrains/plugin-blockmap-patches")
       .apache("https://github.com/JetBrains/plugin-blockmap-patches/blob/master/LICENSE"),
@@ -1224,12 +1226,12 @@ object CommunityLibraryLicenses {
     jetbrainsLibrary("find-classes-model-experimental"),
     jetbrainsLibrary("find-file-model"),
     jetbrainsLibrary("find-file-model-experimental"),
-    jetbrainsLibrary("jetbrains.fleet.kernel"),
-    jetbrainsLibrary("jetbrains.fleet.rpc"),
-    jetbrainsLibrary("jetbrains.fleet.rpc.server"),
     jetbrainsLibrary("git-learning-project"),
     jetbrainsLibrary("intellij.remoterobot.remote.fixtures"),
     jetbrainsLibrary("intellij.remoterobot.robot.server.core"),
+    jetbrainsLibrary("jetbrains.fleet.kernel"),
+    jetbrainsLibrary("jetbrains.fleet.rpc"),
+    jetbrainsLibrary("jetbrains.fleet.rpc.server"),
     jetbrainsLibrary("jetbrains.intellij.deps.rwmutex.idea"),
     jetbrainsLibrary("jshell-frontend"),
     jetbrainsLibrary("jvm-native-trusted-roots"),
