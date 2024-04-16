a36f9d5d9790f01b39f1941629f8428523d5a61c
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2024-04-16 16:14:29 +0200

    IJPL-850 rename `intellij.platform.navbar.compatibility.monolith` -> `intellij.platform.navbar.monolith.compatibility`
    GitOrigin-RevId: 6638905a769319a11e048d77925458a87fcef0bc

--- platform/navbar/monolith/compatibility/src/MonolithNavBarServiceDelegate.kt
+++ platform/navbar/monolith/compatibility/src/MonolithNavBarServiceDelegate.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.navbar.compatibility.monolith
+package com.intellij.platform.navbar.monolith.compatibility
 
 import com.intellij.codeInsight.navigation.actions.navigateRequest
 import com.intellij.openapi.actionSystem.DataContext
