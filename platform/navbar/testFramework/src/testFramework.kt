bfa6619891699658f86a7bf8bdf7726a67bc6efd
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2024-04-16 16:35:52 +0200

    IJPL-850 rename `intellij.platform.navbar.compatibility.testFramework` -> `intellij.platform.navbar.testFramework`
    GitOrigin-RevId: 7bf6be563d0fbe1d7b79d0968fd8b3a63fef3fcd

--- platform/navbar/testFramework/src/testFramework.kt
+++ platform/navbar/testFramework/src/testFramework.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.navbar.compatibility.testFramework
+package com.intellij.platform.navbar.testFramework
 
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.actionSystem.DataKey
