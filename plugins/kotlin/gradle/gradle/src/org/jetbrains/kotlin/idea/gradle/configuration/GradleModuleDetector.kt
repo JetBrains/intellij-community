f1b702a9379c5912874d89e328579853c49eaecb
Joffrey.Bion <joffrey.bion@jetbrains.com>
2024-04-17 12:38:56 +0200

    [amper][kotlin] Add Amper and GradleAmper as build systems
    This will be necessary to make quick fix actions available for
    Amper files, and allow them to work generically like with other
    build systems.
    
    GitOrigin-RevId: 605d1d0f66066d9dd52b2db96a53d383fc68261e

--- plugins/kotlin/gradle/gradle/src/org/jetbrains/kotlin/idea/gradle/configuration/GradleModuleDetector.kt
+++ plugins/kotlin/gradle/gradle/src/org/jetbrains/kotlin/idea/gradle/configuration/GradleModuleDetector.kt
@@ -2,10 +2,14 @@
 package org.jetbrains.kotlin.idea.gradle.configuration
 
 import com.intellij.facet.FacetManager
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.module.Module
+import com.intellij.openapi.vfs.VfsUtil
 import org.jetbrains.kotlin.idea.base.util.isGradleModule
 import org.jetbrains.kotlin.idea.configuration.BuildSystemType
 import org.jetbrains.kotlin.idea.configuration.BuildSystemTypeDetector
+import java.nio.file.Path
+import kotlin.io.path.Path
 
 class GradleModuleDetector : BuildSystemTypeDetector {
     override fun detectBuildSystemType(module: Module): BuildSystemType? {
@@ -13,8 +17,36 @@ class GradleModuleDetector : BuildSystemTypeDetector {
             if (FacetManager.getInstance(module).allFacets.any { it.name == "Android" }) {
                 return BuildSystemType.AndroidGradle
             }
+            // We need to detect AmperGradle here (in addition to AmperBuildSystemTypeDetector from the Amper plugin).
+            // If we didn't, we would just return BuildSystemType.Gradle here when the AmperBuildSystemTypeDetector does not run first
+            // (this would depend on the entrypoint order, and on whether the Amper plugin is enabled).
+            if (module.isConfiguredViaAmperFiles()) {
+                return BuildSystemType.AmperGradle
+            }
             return BuildSystemType.Gradle
         }
         return null
     }
 }
+
+private fun Module.isConfiguredViaAmperFiles(): Boolean {
+    // We can't rely on the presence of the Amper plugin in settings.gradle.kts. Even if the Amper plugin is there, each 
+    // subproject can either be pure Gradle or Gradle-based Amper depending on the presence of Amper module files.
+    // That's why we have to check if the module itself has Amper files.
+    
+    // Not all modules represent Gradle projects, some synthetic modules are generated for Gradle source sets (e.g. my-module.commonMain).
+    // This is why we need to get the actual Gradle project path before looking for Amper files.
+    val gradleProjectDir = gradleProjectDir?.let { VfsUtil.findFile(it, false) }
+
+    // It doesn't seem possible to detect Gradle-based Amper files without hardcoding the names nor depending on the Amper plugin.
+    return gradleProjectDir?.findChild("module.yaml") != null || gradleProjectDir?.findChild("module.amper") != null
+}
+
+/**
+ * Returns path to the directory of the Gradle project containing this [Module].
+ * 
+ * Not all modules represent Gradle projects, some synthetic modules are generated for Gradle source sets (e.g. my-module.commonMain).
+ * This property is the actual Gradle project path even when called on a synthetic source set module.
+ */
+private val Module.gradleProjectDir: Path?
+    get() = ExternalSystemApiUtil.getExternalProjectPath(this)?.let { Path(it) }
