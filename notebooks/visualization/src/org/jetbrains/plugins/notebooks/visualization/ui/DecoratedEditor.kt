ac4bfc41051715b0a4f609dabed45de066250ec0
Anton Efimchuk <anton.efimchuk@jetbrains.com>
2024-04-17 18:10:59 +0200

    PY-72039 Wrap scrolling position keeper invocations with read action
    GitOrigin-RevId: a6559134e752e34e0741ef06ba34aa4c6c783e32

--- notebooks/visualization/src/org/jetbrains/plugins/notebooks/visualization/ui/DecoratedEditor.kt
+++ notebooks/visualization/src/org/jetbrains/plugins/notebooks/visualization/ui/DecoratedEditor.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.notebooks.visualization.ui
 
+import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.editor.ex.util.EditorScrollingPositionKeeper
@@ -134,9 +135,11 @@ fun decorateTextEditor(textEditor: TextEditor): TextEditor {
 }
 
 internal fun keepScrollingPositionWhile(editor: Editor, task: Runnable) {
-  EditorScrollingPositionKeeper(editor).use { keeper ->
-    keeper.savePosition()
-    task.run()
-    keeper.restorePosition(false)
+  ReadAction.run<Nothing> {
+    EditorScrollingPositionKeeper(editor).use { keeper ->
+      keeper.savePosition()
+      task.run()
+      keeper.restorePosition(false)
+    }
   }
 }
