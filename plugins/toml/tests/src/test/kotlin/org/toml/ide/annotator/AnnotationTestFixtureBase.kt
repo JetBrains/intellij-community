eaa5c6ec26760adc3e5f1ba53930bbe5601f4e87
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2024-04-19 15:53:06 +0200

    IJPL-104160: fix java.lang.NoSuchMethodException
    Drop hack to launch tests for annotator in batch mode since it's not possible anymore.
    Also, these changes drop the corresponding test
    
    GitOrigin-RevId: 9bf4f7525263022180e1e58493780267947aef69

--- plugins/toml/tests/src/test/kotlin/org/toml/ide/annotator/AnnotationTestFixtureBase.kt
+++ plugins/toml/tests/src/test/kotlin/org/toml/ide/annotator/AnnotationTestFixtureBase.kt
@@ -5,17 +5,14 @@
 
 package org.toml.ide.annotator
 
-import com.intellij.codeInsight.daemon.impl.HighlightVisitor
 import com.intellij.codeInsight.daemon.impl.SeveritiesProvider
 import com.intellij.codeInspection.InspectionProfileEntry
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.project.Project
-import com.intellij.testFramework.ExtensionTestUtil
 import com.intellij.testFramework.InspectionTestUtil
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture
 import com.intellij.testFramework.fixtures.impl.BaseFixture
 import junit.framework.TestCase
-import org.toml.findAnnotationInstance
 import kotlin.reflect.KClass
 
 abstract class AnnotationTestFixtureBase(
@@ -35,29 +32,6 @@ abstract class AnnotationTestFixtureBase(
         annotatorClasses.forEach { AnnotatorBase.enableAnnotator(it.java, testRootDisposable) }
         enabledInspections = InspectionTestUtil.instantiateTools(inspectionClasses.map { it.java })
         codeInsightFixture.enableInspections(*enabledInspections.toTypedArray())
-        if (testCase.findAnnotationInstance<BatchMode>() != null) {
-            enableBatchMode()
-        }
-    }
-
-    private fun enableBatchMode() {
-        // Unfortunately, `DefaultHighlightVisitor` has package private visibility
-        // so use reflection to create its instance
-        val visitorClass = Class.forName("com.intellij.codeInsight.daemon.impl.DefaultHighlightVisitor")
-        val constructor = visitorClass.getDeclaredConstructor(
-            Project::class.java,
-            Boolean::class.java,
-            Boolean::class.java,
-            Boolean::class.java
-        )
-        constructor.isAccessible = true
-        val visitor = constructor.newInstance(
-            project,
-            /* highlightErrorElements = */ true,
-            /* runAnnotators = */ true,
-            /* batchMode = */ true
-        ) as HighlightVisitor
-        ExtensionTestUtil.maskExtensions(HighlightVisitor.EP_HIGHLIGHT_VISITOR, listOf(visitor), testRootDisposable, true, project)
     }
 
     protected fun replaceCaretMarker(text: String) = text.replace("/*caret*/", "<caret>")
