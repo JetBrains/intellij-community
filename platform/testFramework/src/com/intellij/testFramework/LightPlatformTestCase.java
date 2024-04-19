31a28b56590b4a4a73fda2e29ad2b178eda935f9
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2024-04-19 14:38:15 +0200

    [test] clean LightPlatformTestCase.ourSourceRoot if not yet
    + .ourSourceRoot is static: not-cleaned but already invalid value could be inherited by the next test => test-order-sensitive failure ('alien file')
    
    GitOrigin-RevId: b7ff85d33579ee4ea067b3783077b889e62f7907

--- platform/testFramework/src/com/intellij/testFramework/LightPlatformTestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/LightPlatformTestCase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.testFramework;
 
 import com.intellij.application.options.CodeStyle;
@@ -167,6 +167,9 @@ public abstract class LightPlatformTestCase extends UsefulTestCase implements Da
 
     if (ourProject != null) {
       closeAndDeleteProject();
+    }
+    if (ourSourceRoot != null) {
+      ourSourceRoot = null;
     }
     ApplicationManager.getApplication().runWriteAction(() -> cleanPersistedVFSContent());
 
