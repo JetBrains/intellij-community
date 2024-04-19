ce739c240d58417d007fbb89853633a333389fb9
Ivan Semenov <ivan.semenov@jetbrains.com>
2024-04-19 18:49:39 +0200

    [github] use default dispatcher for PR view models
    GitOrigin-RevId: 6c7c68e9f80eb7ddd226c2359cf4e380190ec722

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/data/GHPRFilesManager.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/data/GHPRFilesManager.kt
@@ -3,21 +3,28 @@ package org.jetbrains.plugins.github.pullrequest.data
 
 import com.intellij.diff.editor.DiffVirtualFileBase
 import com.intellij.openapi.Disposable
+import com.intellij.util.concurrency.annotations.RequiresEdt
 import org.jetbrains.plugins.github.api.data.pullrequest.GHPullRequestShort
 import org.jetbrains.plugins.github.pullrequest.GHPRTimelineVirtualFile
 
 internal interface GHPRFilesManager : Disposable {
   val id: String
 
+  @RequiresEdt
   fun createOrGetNewPRDiffFile(): DiffVirtualFileBase
 
+  @RequiresEdt
   fun createAndOpenTimelineFile(pullRequest: GHPRIdentifier, requestFocus: Boolean)
 
+  @RequiresEdt
   fun createAndOpenDiffFile(pullRequest: GHPRIdentifier, requestFocus: Boolean)
 
+  @RequiresEdt
   fun findTimelineFile(pullRequest: GHPRIdentifier): GHPRTimelineVirtualFile?
 
+  @RequiresEdt
   fun findDiffFile(pullRequest: GHPRIdentifier): DiffVirtualFileBase?
 
+  @RequiresEdt
   fun updateTimelineFilePresentation(details: GHPullRequestShort)
 }
