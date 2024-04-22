6ad1f346f4b4d3bd0c6969e8b3adb3f7e373e2d7
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-04-22 12:07:49 +0200

    Cleanup (typos; formatting)
    GitOrigin-RevId: d7eb67c845e7d578d0383174d6c2ed03c12699d5

--- platform/ide-core/src/com/intellij/notification/NotificationAction.java
+++ platform/ide-core/src/com/intellij/notification/NotificationAction.java
@@ -6,7 +6,6 @@ import com.intellij.openapi.actionSystem.ActionWithDelegate;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
-import com.intellij.openapi.util.NlsSafe;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -17,7 +16,6 @@ import java.util.function.Supplier;
 import static com.intellij.openapi.util.NlsContexts.NotificationContent;
 
 /**
- * @author Alexander Lobas
  * @see Notification#addAction(AnAction)
  */
 public abstract class NotificationAction extends DumbAwareAction {
@@ -37,24 +35,32 @@ public abstract class NotificationAction extends DumbAwareAction {
 
   public abstract void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification);
 
-  public static @NotNull NotificationAction create(@NotNull @NotificationContent String text,
-                                                   @NotNull @NlsSafe String actionId,
-                                                   @NotNull BiConsumer<? super AnActionEvent, ? super Notification> action) {
-    return new Simple(text, action, false, action, actionId);
-  }
-
-  public static @NotNull NotificationAction create(@NotNull @NotificationContent String text,
-                                                   @NotNull BiConsumer<? super AnActionEvent, ? super Notification> action) {
+  public static @NotNull NotificationAction create(
+    @NotNull @NotificationContent String text,
+    @NotNull BiConsumer<? super AnActionEvent, ? super Notification> action
+  ) {
     return new Simple(text, action, false, action, null);
   }
 
-  public static @NotNull NotificationAction create(@NotNull Supplier<@NotificationContent String> dynamicText,
-                                                   @NotNull BiConsumer<? super AnActionEvent, ? super Notification> action) {
+  public static @NotNull NotificationAction create(
+    @NotNull @NotificationContent String text,
+    @NotNull String actionId,
+    @NotNull BiConsumer<? super AnActionEvent, ? super Notification> action
+  ) {
+    return new Simple(text, action, false, action, actionId);
+  }
+
+  public static @NotNull NotificationAction create(
+    @NotNull Supplier<@NotificationContent String> dynamicText,
+    @NotNull BiConsumer<? super AnActionEvent, ? super Notification> action
+  ) {
     return new Simple(dynamicText, action, false, action, null);
   }
 
-  public static @NotNull NotificationAction createExpiring(@NotNull @NotificationContent String text,
-                                                           @NotNull BiConsumer<? super AnActionEvent, ? super Notification> action) {
+  public static @NotNull NotificationAction createExpiring(
+    @NotNull @NotificationContent String text,
+    @NotNull BiConsumer<? super AnActionEvent, ? super Notification> action
+  ) {
     return new Simple(text, action, true, action, null);
   }
 
@@ -66,15 +72,16 @@ public abstract class NotificationAction extends DumbAwareAction {
     return new Simple(dynamicText, (event, notification) -> action.run(), false, action, null);
   }
 
-
-  public static @NotNull NotificationAction createSimpleExpiring(@NotNull @NotificationContent String text,
-                                                                 @NotNull @NlsSafe String actionId,
-                                                                 @NotNull Runnable action) {
-    return new Simple(text, (event, notification) -> action.run(), true, action, actionId);
-  }
-
   public static @NotNull NotificationAction createSimpleExpiring(@NotNull @NotificationContent String text, @NotNull Runnable action) {
     return new Simple(text, (event, notification) -> action.run(), true, action, null);
+  }
+
+  public static @NotNull NotificationAction createSimpleExpiring(
+    @NotNull @NotificationContent String text,
+    @NotNull String actionId,
+    @NotNull Runnable action
+  ) {
+    return new Simple(text, (event, notification) -> action.run(), true, action, actionId);
   }
 
   @ApiStatus.Internal
@@ -82,13 +89,15 @@ public abstract class NotificationAction extends DumbAwareAction {
     private final BiConsumer<? super AnActionEvent, ? super Notification> myAction;
     private final boolean myExpire;
     private final Object myActionInstance;  // for FUS
-    private final @Nullable @NlsSafe String myActionId;  // for FUS
+    private final @Nullable String myActionId;  // for FUS
 
-    private Simple(@NotificationContent String text,
-                   BiConsumer<? super AnActionEvent, ? super Notification> action,
-                   boolean expire,
-                   Object actionInstance,
-                   @Nullable @NlsSafe String actionId) {
+    private Simple(
+      @NotificationContent String text,
+      BiConsumer<? super AnActionEvent, ? super Notification> action,
+      boolean expire,
+      Object actionInstance,
+      @Nullable String actionId
+    ) {
       super(text);
       myAction = action;
       myExpire = expire;
@@ -96,11 +105,13 @@ public abstract class NotificationAction extends DumbAwareAction {
       myActionId = actionId;
     }
 
-    private Simple(Supplier<@NotificationContent String> dynamicText,
-                   BiConsumer<? super AnActionEvent, ? super Notification> action,
-                   boolean expire,
-                   Object actionInstance,
-                   @Nullable @NlsSafe String actionId) {
+    private Simple(
+      Supplier<@NotificationContent String> dynamicText,
+      BiConsumer<? super AnActionEvent, ? super Notification> action,
+      boolean expire,
+      Object actionInstance,
+      @Nullable String actionId
+    ) {
       super(dynamicText);
       myAction = action;
       myExpire = expire;
@@ -127,7 +138,7 @@ public abstract class NotificationAction extends DumbAwareAction {
         return myActionId;
       }
       if (myActionInstance instanceof ActionIdProvider actionIdProvider) {
-        String providerId = actionIdProvider.getId();
+        var providerId = actionIdProvider.getId();
         if (providerId != null) {
           return providerId;
         }
