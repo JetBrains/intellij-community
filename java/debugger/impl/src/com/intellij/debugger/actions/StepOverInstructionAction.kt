a68e197ca1b2d10b7750f75c5b623159d46b0cbd
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2024-04-22 18:08:37 +0200

    [debugger] Refactoring: extract base class for session related actions
    GitOrigin-RevId: 309fdbf8d7e2a5b30bc8bdd0b78a1a6e3c78275f

--- java/debugger/impl/src/com/intellij/debugger/actions/StepOverInstructionAction.kt
+++ java/debugger/impl/src/com/intellij/debugger/actions/StepOverInstructionAction.kt
@@ -1,24 +1,12 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.debugger.actions
 
-import com.intellij.debugger.DebuggerManagerEx
-import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.DumbAware
 
-class StepOverInstructionAction : DebuggerAction(), DumbAware {
+class StepOverInstructionAction : SessionActionBase(), DumbAware {
   override fun actionPerformed(e: AnActionEvent) {
     debuggerSession(e)?.stepOverInstruction() ?: thisLogger().error("Inconsistent update")
   }
-
-  override fun update(e: AnActionEvent) {
-    e.presentation.isEnabledAndVisible = debuggerSession(e) != null
-  }
-
-  override fun getActionUpdateThread() = ActionUpdateThread.BGT
-
-  private fun debuggerSession(e: AnActionEvent) =
-    e.project?.let { DebuggerManagerEx.getInstanceEx(it).context.debuggerSession }
 }
-
