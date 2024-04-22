a5bcb57526eaf04b78055f8e59aa6e7b4b6a9bb3
Yann Cébron <yann.cebron@jetbrains.com>
2024-04-22 10:05:43 +0200

    IJPL-950 Action System: add ApiStatus.OverrideOnly in relevant API
    add AnAction.beforeActionPerformedUpdate
    
    GitOrigin-RevId: 19093a42d97214655bec41e448926e0557bfbb2d

--- platform/editor-ui-api/src/com/intellij/openapi/actionSystem/AnAction.java
+++ platform/editor-ui-api/src/com/intellij/openapi/actionSystem/AnAction.java
@@ -327,6 +327,7 @@ public abstract class AnAction implements PossiblyDumbAware, ActionUpdateThreadA
    *
    * @see #actionPerformed(AnActionEvent)
    */
+  @ApiStatus.OverrideOnly
   public void beforeActionPerformedUpdate(@NotNull AnActionEvent e) {
     update(e);
   }
