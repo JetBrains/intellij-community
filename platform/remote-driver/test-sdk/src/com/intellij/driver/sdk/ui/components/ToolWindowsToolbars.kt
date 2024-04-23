676ab6ab84d671432b822597264eca7e39c493f5
vladislav.shishov <vladislav.shishov@jetbrains.com>
2024-04-23 19:54:19 +0300

    [remote-driver] fix StripeButtonUi#open
    GitOrigin-RevId: 072234d5753f53294269dcde995d7680e32a9f7b

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/ToolWindowsToolbars.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/ToolWindowsToolbars.kt
@@ -2,6 +2,7 @@ package com.intellij.driver.sdk.ui.components
 
 import com.intellij.driver.client.Remote
 import com.intellij.driver.model.OnDispatcher
+import com.intellij.driver.sdk.invokeAction
 import com.intellij.driver.sdk.ui.Finder
 import com.intellij.driver.sdk.ui.Locators
 
@@ -27,16 +28,29 @@ class StripeButtonUi(data: ComponentData) : UiComponent(data) {
   }
 
   fun open() {
-    repeat(2) {
-      if (!isSelected()) {
-        click()
-      }
+    val toolWindow = button.getToolWindow()
+    if (!toolWindow.isActive()) {
+      val activateToolWindowAction = driver.utility(ActivateToolWindowActionManager::class)
+        .getActionIdForToolWindow(toolWindow.getId())
+      driver.invokeAction(activateToolWindowAction)
     }
   }
 
   @Remote("com.intellij.openapi.wm.impl.SquareStripeButton")
   interface StripeButtonComponent {
+    fun getToolWindow(): ToolWindowRef
     fun isSelected(): Boolean
+  }
+
+  @Remote("com.intellij.openapi.wm.impl.ToolWindowImpl")
+  interface ToolWindowRef {
+    fun getId(): String
+    fun isActive(): Boolean
+  }
+
+  @Remote("com.intellij.ide.actions.ActivateToolWindowAction\$Manager")
+  interface ActivateToolWindowActionManager {
+    fun getActionIdForToolWindow(id: String): String
   }
 }
 
