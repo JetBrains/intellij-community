805ad3119d24ca08d6b0d21707d8622ac4af30d6
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2024-04-23 14:57:54 +0200

    PY-71801 @ApiStatus.Experimental for just introduced SelfKiller.tryDestroyGracefully
    GitOrigin-RevId: c59a00b248a34b4a401824ef9a67fcca7a8e9e42

--- platform/platform-util-io/src/com/intellij/execution/process/SelfKiller.java
+++ platform/platform-util-io/src/com/intellij/execution/process/SelfKiller.java
@@ -1,6 +1,8 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.process;
 
+import org.jetbrains.annotations.ApiStatus;
+
 /**
  * Marker interface that represents a process that kills itself, for example a remote process, that can't be killed by the local OS.
  *
@@ -15,6 +17,7 @@ public interface SelfKiller {
    * <p>
    * If this method returns false, the caller must destroy the process in a less gentle way, like SIGTERM or SIGKILL.
    */
+  @ApiStatus.Experimental
   default boolean tryDestroyGracefully() {
     return false;
   }
