a76bc2eb45973f37932287b827a068623b0b9258
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2024-04-24 16:33:30 +0300

    IJPL-148249 allow modules without package in tests
    GitOrigin-RevId: 48477f7a3fc62b6f26127f1e43bfddf6f8a7229b

--- platform/core-impl/src/com/intellij/ide/plugins/PluginDescriptorLoader.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/PluginDescriptorLoader.kt
@@ -1027,7 +1027,7 @@ private fun loadDescriptorFromResource(
         val subDescriptorFile = module.configFile ?: "${module.name}.xml"
         val subRaw = pathResolver.resolveModuleFile(readContext = context, dataLoader = dataLoader, path = subDescriptorFile, readInto = null)
         val subDescriptor = descriptor.createSub(raw = subRaw, descriptorPath = subDescriptorFile, context = context, moduleName = module.name)
-        if (pathResolver.isRunningFromSources && subDescriptor.packagePrefix == null) {
+        if ((pathResolver.isRunningFromSources || isUnitTestMode) && subDescriptor.packagePrefix == null) {
           // no package in run from sources - load module from main classpath
           subDescriptor.jarFiles = Collections.emptyList()
         }
