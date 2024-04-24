984ac95a841712718267997cd12a557d2538fdea
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2024-04-24 17:02:26 +0200

    [kotlin] KTIJ-28619 kotlin.base.fir.code-insight migrated to the new plugin model
    GitOrigin-RevId: 31a944507b83b3740d3b27b88b01610a1dabaf87

--- plugins/kotlin/code-insight/kotlin.code-insight.k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/MultiplatformSupportAvailability.kt
+++ plugins/kotlin/code-insight/kotlin.code-insight.k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/MultiplatformSupportAvailability.kt
@@ -1,23 +1,23 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.base.fir.codeInsight
+package org.jetbrains.kotlin.idea.k2.codeinsight
 
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.psi.util.parents
+import org.jetbrains.annotations.Nls
 import org.jetbrains.kotlin.idea.base.facet.JvmOnlyProjectChecker
 import org.jetbrains.kotlin.idea.base.facet.platform.platform
+import org.jetbrains.kotlin.idea.codeInsight.KotlinCodeInsightBundle
 import org.jetbrains.kotlin.idea.codeinsight.utils.KotlinSupportAvailability
 import org.jetbrains.kotlin.platform.isCommon
 import org.jetbrains.kotlin.platform.jvm.isJvm
-import org.jetbrains.kotlin.psi.KtDeclaration
-import org.jetbrains.kotlin.psi.KtElement
-import org.jetbrains.kotlin.psi.KtFile
-import org.jetbrains.kotlin.psi.KtFunction
-import org.jetbrains.kotlin.psi.KtPureClassOrObject
+import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.hasActualModifier
 import org.jetbrains.kotlin.psi.psiUtil.hasExpectModifier
 
-class MultiplatformSupportAvailability: KotlinSupportAvailability {
-    override fun name(): String = KotlinBaseFirCodeInsight.message("kmp.support.availability.name")
+internal class MultiplatformSupportAvailability : KotlinSupportAvailability {
+
+    @Nls
+    override fun name(): String = KotlinCodeInsightBundle.message("kmp.support.availability.name")
 
     override fun isSupported(ktElement: KtElement): Boolean {
         if (Registry.`is`("kotlin.k2.kmp.enabled", true) || JvmOnlyProjectChecker.getInstance(ktElement.project).value()) return true
