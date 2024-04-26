bf71645352fdc1f58bacaae3a4cf62c403d5d5b3
Alex Pshe <alexandra.psheborovskaya@jetbrains.com>
2024-04-26 11:03:03 +0200

    [aqua] E2E tests: add title to Dialog
    GitOrigin-RevId: 4c5167f6ffd5982fca5500a5e0bb3302d5aff674

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/Dialog.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/Dialog.kt
@@ -6,8 +6,14 @@ import com.intellij.driver.sdk.waitFor
 import org.intellij.lang.annotations.Language
 import kotlin.time.Duration.Companion.seconds
 
-fun Finder.dialog(@Language("xpath") xpath: String? = null) =
-  x(xpath ?: "//div[@class='MyDialog']", DialogUiComponent::class.java)
+fun Finder.dialog(@Language("xpath") xpath: String? = null, title: String? = null, action: UiComponent.() -> Unit = {}): DialogUiComponent {
+  val dialogXpath = when {
+    xpath != null -> xpath
+    title != null -> "//div[@title='$title']"
+    else -> "//div[@class='MyDialog']"
+  }
+  return x(dialogXpath, DialogUiComponent::class.java).apply(action)
+}
 
 fun Finder.isDialogOpened(@Language("xpath") xpath: String? = null) =
   xx(xpath ?: "//div[@class='MyDialog']", DialogUiComponent::class.java).list().isNotEmpty()
@@ -23,12 +29,12 @@ fun Finder.waitForNoOpenedDialogs() {
 
 fun WelcomeScreenUI.aboutDialog(action: AboutDialogUi.() -> Unit) = x(Locators.byTitleContains("About"), AboutDialogUi::class.java).apply(action)
 
-class AboutDialogUi(data: ComponentData): UiComponent(data) {
+class AboutDialogUi(data: ComponentData) : UiComponent(data) {
   val closeButton: UiComponent
     get() = x(Locators.byAccessibleName("Close"))
 }
 
-class DialogUiComponent(data: ComponentData): UiComponent(data) {
+class DialogUiComponent(data: ComponentData) : UiComponent(data) {
 
   fun pressButton(text: String) = x("//div[@class='JButton' and @visible_text='$text']").click()
 }
