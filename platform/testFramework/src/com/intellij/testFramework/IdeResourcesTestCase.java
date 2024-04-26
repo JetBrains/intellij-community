ae90372f52eae69f58e22817059b47703a6a1d1f
Vera Petrenkova <Vera.Petrenkova@jetbrains.com>
2024-04-26 09:08:45 +0200

    IJPL-83056 Update Tips content and support bundled localization
    GitOrigin-RevId: 55b0cdde9d4ab9046f48ea021d820ae74d16dc1c

--- platform/testFramework/src/com/intellij/testFramework/IdeResourcesTestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/IdeResourcesTestCase.java
@@ -28,8 +28,9 @@ public abstract class IdeResourcesTestCase extends LightPlatformTestCase {
 
   public void testTipFilesDuplicates() {
     Collection<String> errors = new TreeSet<>();
-    TipAndTrickBean[] tips = TipAndTrickBean.EP_NAME.getExtensions();
-    assertNotEmpty(Arrays.asList(tips));
+    List<TipAndTrickBean> tips = new ArrayList<>(TipAndTrickBean.EP_NAME.getExtensionList());
+    assertNotEmpty(tips);
+    tips.removeIf(it -> !it.fileName.endsWith(".html"));
     Set<String> visited = new LinkedHashSet<>();
     for (TipAndTrickBean tip : tips) {
       if (!visited.add(tip.fileName)) {
