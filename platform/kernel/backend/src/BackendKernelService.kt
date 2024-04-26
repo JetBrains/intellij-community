0174bea12011329293c001ebe74441d60af43be6
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2024-04-26 16:55:46 +0200

    IJPL-872 single `KernelService` implementation for all backends
    This service, when loaded as part of the CWM plugin, tries to override `MonolithKernelService`.
    We are avoiding service overrides with a proper module structure.
    
    CWM runs as monolith+plugin, so `MonolithKernelService` is loaded,
    but it does not install the workspace clock like `BackendKernelService` does.
    
    Solution 0: reliable and correct.
    It should be possible to register `MonolithKernelService` for the local client ID,
    while CWM plugin would register `BackendKernelService` for remote clients.
    In split backend mode, there should be no monolith registration at all,
    and it should not be needed because all clients are remote.
    
    Solution 1: okay.
    A single service implementation must be used in both monolith and split host.
    This way there will be no conflict between the monolith service and the CWM plugin service.
    However, the workspace clock will also be installed in the monolith even if it might be unneeded,
    e.g., when the CWM plugin is not loaded.
    
    Solution 1 is used in this commit.
    
    GitOrigin-RevId: bcb75ac0dabc3328bd09c30312510a93bbead11b

---
+++ platform/kernel/backend/src/BackendKernelService.kt
@@ -0,0 +1,63 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.kernel.backend
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.platform.kernel.KernelService
+import com.intellij.platform.kernel.backend.RemoteApiProvider.RemoteApiDescriptor
+import com.intellij.platform.kernel.util.CommonInstructionSet
+import com.intellij.platform.kernel.util.KernelRpcSerialization
+import com.intellij.platform.kernel.util.ReadTracker
+import com.intellij.platform.kernel.util.withKernel
+import com.intellij.platform.util.coroutines.childScope
+import fleet.kernel.Kernel
+import fleet.kernel.kernel
+import fleet.kernel.rebase.*
+import fleet.kernel.rete.Rete
+import kotlinx.coroutines.*
+
+@Service
+private class RemoteKernelScopeHolder(coroutineScope: CoroutineScope) {
+  val remoteKernelScope: CoroutineScope = coroutineScope.childScope("RemoteKernelScope", KernelService.kernelCoroutineContext)
+}
+
+internal class RemoteKernelProvider : RemoteApiProvider {
+
+  override fun getApis(): List<RemoteApiDescriptor<*>> {
+    return listOf(RemoteApiDescriptor(RemoteKernel::class) {
+      RemoteKernelImpl(
+        KernelService.instance.kernel,
+        ApplicationManager.getApplication().service<RemoteKernelScopeHolder>().remoteKernelScope,
+        CommonInstructionSet.decoder(),
+        KernelRpcSerialization
+      )
+    })
+  }
+}
+
+internal class BackendKernelService(coroutineScope: CoroutineScope) : KernelService {
+
+  override val kernel: Kernel
+  override val rete: Rete
+
+  init {
+    val kernelDeferred: CompletableDeferred<Kernel> = CompletableDeferred()
+    val reteDeferred : CompletableDeferred<Rete> = CompletableDeferred()
+
+    coroutineScope.launch(start = CoroutineStart.UNDISPATCHED) {
+      withKernel(middleware = LeaderKernelMiddleware(KernelRpcSerialization, CommonInstructionSet.encoder())) {
+        coroutineScope {
+          kernelDeferred.complete(currentCoroutineContext()[Kernel]!!)
+          reteDeferred.complete(currentCoroutineContext()[Rete]!!)
+          kernel().changeSuspend {
+            initWorkspaceClock()
+          }
+          ReadTracker.subscribeForChanges()
+        }
+      }
+    }
+    kernel = kernelDeferred.getCompleted()
+    rete = reteDeferred.getCompleted()
+  }
+}
