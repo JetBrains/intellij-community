b38b4a1685a30c868cfc10310fe8ef323a9a075e
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2024-04-29 17:29:07 +0200

    [gradle] add event stat about toggling parallel model fetch option from the UI IDEA-350589
    GitOrigin-RevId: 25012169dcf1cedf698e770d437ea21d2ad55994

--- plugins/gradle/src/org/jetbrains/plugins/gradle/statistics/GradleActionsUsagesCollector.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/statistics/GradleActionsUsagesCollector.kt
@@ -1,7 +1,8 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.statistics
 
 import com.intellij.internal.statistic.eventLog.EventLogGroup
+import com.intellij.internal.statistic.eventLog.events.EventFields
 import com.intellij.internal.statistic.eventLog.events.EventId
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
 import com.intellij.openapi.project.Project
@@ -9,7 +10,7 @@ import com.intellij.openapi.project.Project
 internal object GradleActionsUsagesCollector : CounterUsagesCollector() {
   override fun getGroup(): EventLogGroup = GROUP
 
-  private val GROUP = EventLogGroup("build.gradle.actions", 3)
+  private val GROUP = EventLogGroup("build.gradle.actions", 4)
 
   @JvmField
   val REFRESH_DAEMONS = GROUP.registerEvent("refreshDaemons")
@@ -23,6 +24,11 @@ internal object GradleActionsUsagesCollector : CounterUsagesCollector() {
   val PASTE_MAVEN_DEPENDENCY = GROUP.registerEvent("PasteMvnDependency")
   @JvmField
   val SHOW_GRADLE_DAEMONS_ACTION = GROUP.registerEvent("showGradleDaemonsAction")
+  @JvmField
+  val TOGGLE_PARALLEL_FETCH = GROUP.registerEvent("toggleParallelFetch",
+                                                  EventFields.Boolean("new_value", "newly set value"),
+                                                  "Parallel model fetch flag was switched by a user to the new value. " +
+                                                  "Only fires when the old value differs from the new boolean value")
 
   @JvmStatic
   fun trigger(project: Project?, action: EventId) {
