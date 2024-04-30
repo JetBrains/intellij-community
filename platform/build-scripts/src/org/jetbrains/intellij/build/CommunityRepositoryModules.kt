9275b0be7f3470644c2bb1d17b34d1a95b3e5da0
Nikita Katkov <nikita.katkov@jetbrains.com>
2024-04-30 15:26:12 +0200

    [yaml + remdev] IJPL-149085 Specify yaml plugin layout explicitly in the build scripts
    GitOrigin-RevId: 69af63b1bdb09740d96738fe8cf8515585b5841e

--- platform/build-scripts/src/org/jetbrains/intellij/build/CommunityRepositoryModules.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/CommunityRepositoryModules.kt
@@ -25,6 +25,8 @@ object CommunityRepositoryModules {
    * Specifies non-trivial layout for all plugins that sources are located in 'community' and 'contrib' repositories
    */
   val COMMUNITY_REPOSITORY_PLUGINS: PersistentList<PluginLayout> = persistentListOf(
+    pluginAuto(listOf("intellij.yaml", "intellij.yaml.frontback")),
+    pluginAuto(listOf("intellij.yaml.frontend", "intellij.yaml.frontback")),
     plugin("intellij.ant") { spec ->
       spec.mainJarName = "antIntegration.jar"
       spec.withModule("intellij.ant.jps", "ant-jps.jar")
