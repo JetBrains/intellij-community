7032ef2e1c9e1556169e6a13f256445f7114b1ed
Nikita.Ashihmin <nikita.ashihmin@jetbrains.com>
2024-05-02 23:21:04 +0400

    [pycharm] PY-72340 Jupyter(feat): Support "File <file>:<line>" format for highlight of stacktraces, resolve path with ~
    GitOrigin-RevId: 8ba99b238db19eaf1cf714df2ac388a649a61d88

--- python/src/com/jetbrains/python/run/PyTracebackParser.kt
+++ python/src/com/jetbrains/python/run/PyTracebackParser.kt
@@ -1,32 +1,36 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.jetbrains.python.run;
+package com.jetbrains.python.run
 
-import com.jetbrains.python.traceBackParsers.LinkInTrace;
-import com.jetbrains.python.traceBackParsers.TraceBackParserAdapter;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.jetbrains.python.traceBackParsers.LinkInTrace
+import com.jetbrains.python.traceBackParsers.TraceBackParserAdapter
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 /**
  * Finds links in default python traceback
  *
  * @author Ilya.Kazakevich
  */
-public class PyTracebackParser extends TraceBackParserAdapter {
+open class PyTracebackParser : TraceBackParserAdapter(Pattern.compile(
+  "(File \"(?<file>[^0-9][^\"]{0,200})\", line (?<line>\\d{1,8}))|(File (?<file2>[^0-9][^\"]{0,200}):(?<line2>\\d{1,8}))")) {
+  override fun findLinkInTrace(line: String, matchedMatcher: Matcher): LinkInTrace {
+    val file1 = matchedMatcher.group("file")
+    val file2 = matchedMatcher.group("file2")
+    val fileName = (file1 ?: file2).replace('\\', '/')
 
+    val lineNumber1 = matchedMatcher.group("line")
+    val lineNumber2 = matchedMatcher.group("line2")
+    val lineNumber = (lineNumber1 ?: lineNumber2).toInt()
 
-  public PyTracebackParser() {
-    // File name can't start with number, can't be more then 200 chars long (its insane) and line number is also limited to int maxvalue
-    super(Pattern.compile("File \"([^0-9][^\"]{0,200})\", line (\\d{1,8})"));
-  }
-
-  @Override
-  protected @NotNull LinkInTrace findLinkInTrace(final @NotNull String line, final @NotNull Matcher matchedMatcher) {
-    final String fileName = matchedMatcher.group(1).replace('\\', '/');
-    final int lineNumber = Integer.parseInt(matchedMatcher.group(2));
-    final int startPos = line.indexOf('\"') + 1;
-    final int endPos = line.indexOf('\"', startPos);
-    return new LinkInTrace(fileName, lineNumber, startPos, endPos);
+    if (file1 != null && lineNumber1 != null) {
+      val startPos = line.indexOf('\"') + 1
+      val endPos = line.indexOf('\"', startPos)
+      return LinkInTrace(fileName, lineNumber, startPos, endPos)
+    }
+    else {
+      val startPos = matchedMatcher.start("file2")
+      val endPos = matchedMatcher.end("line2")
+      return LinkInTrace(fileName, lineNumber, startPos, endPos)
+    }
   }
 }
