48f933afc646339eaeeb1281ab33a7a3f4d06e67
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2024-05-02 09:23:08 +0200

    IJPL-121215 Use CollectionFactory.createConcurrentWeakIdentityMap
    GitOrigin-RevId: ee054f6890970c8e8126499cdb26fa514f565010

--- platform/platform-api/src/com/intellij/openapi/actionSystem/ex/ActionCopiedShortcutsTracker.kt
+++ platform/platform-api/src/com/intellij/openapi/actionSystem/ex/ActionCopiedShortcutsTracker.kt
@@ -4,20 +4,18 @@ package com.intellij.openapi.actionSystem.ex
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
+import com.intellij.util.containers.CollectionFactory
 import org.jetbrains.annotations.ApiStatus.Internal
-import java.util.*
 
 @Service
 @Internal
 class ActionCopiedShortcutsTracker {
-  private val actionToSourceId = WeakHashMap<AnAction, String>()
+  private val actionToSourceId = CollectionFactory.createConcurrentWeakIdentityMap<AnAction, String>()
 
-  @Synchronized
   fun onActionCopiedFromId(action: AnAction, sourceId: String) {
     actionToSourceId[action] = sourceId
   }
 
-  @Synchronized
   fun getSourceId(action: AnAction): String? = actionToSourceId[action]
 
   companion object {
