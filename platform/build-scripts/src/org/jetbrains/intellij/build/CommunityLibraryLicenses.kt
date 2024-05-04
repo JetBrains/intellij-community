a6c586566d7b75f4eda51c790993be268c4fdc20
Vladislav.Yaroshchuk <vladislav.yaroshchuk@jetbrains.com>
2024-05-04 19:27:54 +0400

    [safe-push] IJI-1733 Implement new "Patronus" Safe-Push backend
    Emergency push is not supported yet
    
    (cherry picked from commit 88ddc27e1d57d5e037c24b86bb1a311576cbec0d)
    (cherry picked from commit 9d690efb28283b3f720e9cb5470f180a2bd413fc)
    
    GitOrigin-RevId: 7a8568ffae54561bc2e107c86b26b3797b96c1fd

--- platform/build-scripts/src/org/jetbrains/intellij/build/CommunityLibraryLicenses.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/CommunityLibraryLicenses.kt
@@ -780,6 +780,10 @@ object CommunityLibraryLicenses {
                    url = "https://github.com/ktorio/ktor")
       .apache("https://github.com/ktorio/ktor/blob/main/LICENSE")
       .suppliedByOrganizations(Suppliers.JETBRAINS),
+    LibraryLicense(libraryName = "ktor-client-java-engine",
+                   url = "https://github.com/ktorio/ktor")
+      .apache("https://github.com/ktorio/ktor/blob/main/LICENSE")
+      .suppliedByOrganizations(Suppliers.JETBRAINS),
     LibraryLicense(libraryName = "ktor-client-jvm",
                    url = "https://github.com/ktorio/ktor")
       .apache("https://github.com/ktorio/ktor/blob/main/LICENSE")
