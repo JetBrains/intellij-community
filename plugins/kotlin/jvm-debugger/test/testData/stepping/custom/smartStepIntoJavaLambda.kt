a351b3b6f77f8652ba900342c3a4c0b293e44d75
Maksim Zuev <maksim.zuev@jetbrains.com>
2024-05-06 16:28:10 +0200

    [debugger] IDEA-347753 Fix smart step into java lambdas with generics
    GitOrigin-RevId: 5ba594af3fe89170d2bfa6e1508f5e12435a1784

--- plugins/kotlin/jvm-debugger/test/testData/stepping/custom/smartStepIntoJavaLambda.kt
+++ plugins/kotlin/jvm-debugger/test/testData/stepping/custom/smartStepIntoJavaLambda.kt
@@ -5,17 +5,32 @@ import forTests.JavaUtil
 
 fun main() {
     // SMART_STEP_INTO_BY_INDEX: 2
-    //Breakpoint!
+    // RESUME: 1
+    //Breakpoint!, lambdaOrdinal = -1
     JavaUtil.lambdaFun {
         println()
+    }
+
+    // SMART_STEP_INTO_BY_INDEX: 2
+    // RESUME: 1
+    //Breakpoint!, lambdaOrdinal = -1
+    JavaUtil.lambdaGenericFun {
+        println(it)
+        it + 1
     }
 }
 
 // FILE: forTests/JavaUtil.java
 package forTests;
 
+import java.util.function.Function;
+
 public class JavaUtil {
     public static void lambdaFun(Runnable r) {
         r.run();
+    }
+
+    public static void lambdaGenericFun(Function<? super Integer, ? extends Integer> r) {
+        r.apply(1);
     }
 }
