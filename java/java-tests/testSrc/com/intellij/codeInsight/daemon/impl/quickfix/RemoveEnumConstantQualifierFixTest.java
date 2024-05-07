719a33a2878e1ee73ce491bd862d6a73b38f1a3c
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2024-05-07 09:56:29 +0200

    [java] fix RemoveEnumConstantQualifierFixTest
    - reuse LightQuickFixParameterizedTestCase
    
    GitOrigin-RevId: 8ae12acee3041dc3ff2e2b6bb3fae9f4f396cf33

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/impl/quickfix/RemoveEnumConstantQualifierFixTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/impl/quickfix/RemoveEnumConstantQualifierFixTest.java
@@ -1,14 +1,16 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.daemon.impl.quickfix;
 
-import com.intellij.codeInsight.daemon.quickFix.LightQuickFixParameterizedTestCase5;
+import com.intellij.codeInsight.daemon.quickFix.LightQuickFixParameterizedTestCase;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 import org.jetbrains.annotations.NotNull;
 
-import static com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase.JAVA_20;
+public class RemoveEnumConstantQualifierFixTest extends LightQuickFixParameterizedTestCase {
 
-public class RemoveEnumConstantQualifierFixTest extends LightQuickFixParameterizedTestCase5 {
-  public RemoveEnumConstantQualifierFixTest() {
-    super(JAVA_20);
+  @Override
+  protected @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return LightJavaCodeInsightFixtureTestCase.JAVA_20;
   }
 
   @NotNull
