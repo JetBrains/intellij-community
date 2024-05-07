4485d1b5ff4733dc8840e5ed6a35f2415a6d7f1b
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2024-05-07 18:30:08 +0300

    [terminal] IJPL-101589 Add tests for loading light spec and replacing json-based spec
    GitOrigin-RevId: 23d98cd2b6be3f9f1158e7aadd9c9b4c905e1e75

--- plugins/terminal/tests/org/jetbrains/plugins/terminal/exp/ShellCommandSpecManagerTest.kt
+++ plugins/terminal/tests/org/jetbrains/plugins/terminal/exp/ShellCommandSpecManagerTest.kt
@@ -7,10 +7,14 @@ import com.intellij.terminal.block.completion.spec.ShellRuntimeContext
 import com.intellij.testFramework.ExtensionTestUtil
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import kotlinx.coroutines.runBlocking
+import org.jetbrains.plugins.terminal.block.completion.spec.ShellCommandSpec
+import org.jetbrains.plugins.terminal.block.completion.spec.ShellCommandSpecConflictStrategy
+import org.jetbrains.plugins.terminal.block.completion.spec.ShellCommandSpecInfo
 import org.jetbrains.plugins.terminal.block.completion.spec.ShellCommandSpecsProvider
 import org.jetbrains.plugins.terminal.block.completion.spec.impl.IJShellRuntimeContext
 import org.jetbrains.plugins.terminal.block.completion.spec.impl.ShellGeneratorCommandsRunner
 import org.jetbrains.plugins.terminal.exp.completion.IJShellCommandSpecsManager
+import org.jetbrains.plugins.terminal.exp.util.TestCommandSpecsProvider
 import org.jetbrains.plugins.terminal.exp.util.TestJsonCommandSpecsProvider
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,20 +22,33 @@ import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
 class ShellCommandSpecManagerTest : BasePlatformTestCase() {
-  override fun setUp() {
-    super.setUp()
-    ExtensionTestUtil.maskExtensions(ShellCommandSpecsProvider.EP_NAME, listOf(TestJsonCommandSpecsProvider()), testRootDisposable)
+  private val commandName: String = "main"
+
+  private val commandSpecsManager: IJShellCommandSpecsManager
+    get() = IJShellCommandSpecsManager.getInstance()
+
+  /** In this test, real context is not required */
+  private val runtimeContext: ShellRuntimeContext = createDummyRuntimeContext()
+
+  @Test
+  fun `check that there are no subcommands in a light spec`() = runBlocking {
+    mockCommandSpecProviders(TestJsonCommandSpecsProvider())
+
+    val lightSpec = commandSpecsManager.getLightCommandSpec(commandName)
+    assertNotNull("Failed to find $commandName command spec", lightSpec)
+
+    val subcommands = lightSpec!!.subcommandsGenerator.generate(runtimeContext)
+    assertEmpty("There should be no subcommands in a light spec", subcommands)
   }
 
   @Test
   fun `load command spec for subcommand from separate file`() = runBlocking {
-    val commandSpecsManager = IJShellCommandSpecsManager.getInstance()
+    mockCommandSpecProviders(TestJsonCommandSpecsProvider())
 
-    val spec = commandSpecsManager.getCommandSpec("main")
-    assertNotNull("Failed to load main command spec", spec)
+    val spec = commandSpecsManager.getCommandSpec(commandName)
+    assertNotNull("Failed to load $commandName command spec", spec)
 
-    val context = createDummyRuntimeContext()  // in this case, real context is not required
-    val subcommands = spec!!.subcommandsGenerator.generate(context)
+    val subcommands = spec!!.subcommandsGenerator.generate(runtimeContext)
     assertTrue("Subcommands are empty: $spec", subcommands.isNotEmpty())
 
     val subcommand = subcommands.first()
@@ -39,6 +56,20 @@ class ShellCommandSpecManagerTest : BasePlatformTestCase() {
 
     val loadedSubcommand = commandSpecsManager.getFullCommandSpec(subcommand)
     assertTrue("The subcommand is not fully loaded: $loadedSubcommand", loadedSubcommand.options.isNotEmpty())
+  }
+
+  @Test
+  fun `replace default json-based spec with kotlin-based spec`() = runBlocking {
+    val replacingSpec = ShellCommandSpec(commandName) {}
+    val replacingSpecProvider = TestCommandSpecsProvider(ShellCommandSpecInfo.create(replacingSpec, ShellCommandSpecConflictStrategy.REPLACE))
+    mockCommandSpecProviders(TestJsonCommandSpecsProvider(), replacingSpecProvider)
+
+    val spec = commandSpecsManager.getCommandSpec(commandName)
+    assertEquals("Json based spec is not replaced with kotlin-based", replacingSpec, spec)
+  }
+
+  private fun mockCommandSpecProviders(vararg newProviders: ShellCommandSpecsProvider) {
+    ExtensionTestUtil.maskExtensions(ShellCommandSpecsProvider.EP_NAME, newProviders.toList(), testRootDisposable)
   }
 
   private fun createDummyRuntimeContext(): ShellRuntimeContext {
