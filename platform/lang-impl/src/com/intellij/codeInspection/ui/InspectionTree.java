638f2da13b91b09a00c7fd8dfefb21dd1107e756
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-05-07 12:27:39 +0200

    Fix problems with inspection results navigation (IDEA-353104)
    - don't navigate through "Entry Points" (these are not problems)
    - next/previous is available when no node selected
    - next/previous is available when the root node is selected
    - nodes without problems are skipped
    - nodes with problems are not skipped anymore
    - previous goes to "Unused declarations" problems in the right order
    
    GitOrigin-RevId: ead9fabcba6884d8e1af67a0c90af66511573d04

--- platform/lang-impl/src/com/intellij/codeInspection/ui/InspectionTree.java
+++ platform/lang-impl/src/com/intellij/codeInspection/ui/InspectionTree.java
@@ -10,6 +10,7 @@ import com.intellij.codeInspection.ex.GlobalInspectionContextImpl;
 import com.intellij.codeInspection.ex.InspectionProfileImpl;
 import com.intellij.codeInspection.ex.InspectionToolWrapper;
 import com.intellij.codeInspection.reference.RefElement;
+import com.intellij.codeInspection.reference.RefElementImpl;
 import com.intellij.codeInspection.reference.RefEntity;
 import com.intellij.codeInspection.ui.util.SynchronizedBidiMultiMap;
 import com.intellij.ide.DataManager;
@@ -223,6 +224,29 @@ public final class InspectionTree extends Tree {
       }
     }
     return resultWrapper;
+  }
+
+  private static @Nullable InspectionToolWrapper<?,?> getToolWrapper(InspectionTreeNode node) {
+    while (node != null) {
+      if (node instanceof InspectionNode) {
+        return ((InspectionNode)node).getToolWrapper();
+      }
+      else if (node instanceof SuppressableInspectionTreeNode) {
+        return ((SuppressableInspectionTreeNode)node).getPresentation().getToolWrapper();
+      }
+      node = node.getParent();
+    }
+    return null;
+  }
+
+  private @Nullable InspectionToolWrapper<?, ?> getSingleToolWrapper() {
+    InspectionProfileImpl profile = myView.getCurrentProfile();
+    if (profile == null) return null;
+    String singleToolName = profile.getSingleTool();
+    if (singleToolName == null) return null;
+    InspectionToolWrapper<?,?> tool = profile.getInspectionTool(singleToolName, myView.getProject());
+    LOG.assertTrue(tool != null);
+    return tool;
   }
 
   @Override
@@ -604,12 +628,12 @@ public final class InspectionTree extends Tree {
   private final class MyOccurrenceNavigator implements OccurenceNavigator {
     @Override
     public boolean hasNextOccurence() {
-      return getNextNode(true) != null;
+      return getNextOccurrence(true) != null;
     }
 
     @Override
     public boolean hasPreviousOccurence() {
-      return getNextNode(false) != null;
+      return getNextOccurrence(false) != null;
     }
 
     @Override
@@ -623,7 +647,7 @@ public final class InspectionTree extends Tree {
     }
 
     private @Nullable OccurenceInfo goNextOccurrence(boolean next) {
-      InspectionTreeNode node = getNextNode(next);
+      InspectionTreeNode node = getNextOccurrence(next);
       if (node == null) return null;
       selectNode(node);
       return Registry.is("ide.usages.next.previous.occurrence.only.show.in.preview") && InspectionTree.this.isShowing()
@@ -631,6 +655,15 @@ public final class InspectionTree extends Tree {
              : new OccurenceInfo(createDescriptorForNode(node), -1, -1);
     }
 
+    private @Nullable InspectionTreeNode getNextOccurrence(boolean next) {
+      InspectionTreeNode node = ObjectUtils.notNull(getSelectedNode(), getRoot());
+      while (true) {
+         node = next ? next(node) : prev(node);
+        if (node == null) return null;
+        if (isOccurrenceNode(node)) return node;
+      }
+    }
+    
     @NotNull
     @Override
     public String getNextOccurenceActionName() {
@@ -643,12 +676,52 @@ public final class InspectionTree extends Tree {
       return InspectionsBundle.message(ExperimentalUI.isNewUI() ? "inspection.action.go.prev.new" : "inspection.action.go.prev");
     }
 
-    private InspectionTreeNode getNextNode(boolean next) {
-      InspectionTreeNode node = getSelectedNode();
-      if (node == null) {
-        return null;
+    /**
+     * Next node (depth-first pre-order traversal)
+     * @param node  the node to start from
+     * @return the next node, or null if the specified node was the last node in the tree.
+     */
+    private static InspectionTreeNode next(InspectionTreeNode node) {
+      InspectionTreeNode.Children children = node.myChildren;
+      // if node has children: take first child
+      if (children != null && children.myChildren.length > 0) return children.myChildren[0];
+      
+      while (true) {
+        // otherwise: take next sibling (or next sibling of parent (or next sibling of parent))
+        InspectionTreeNode parent = node.myParent;
+        if (parent == null) return null;
+        InspectionTreeNode.Children siblings = parent.myChildren;
+        assert siblings != null;
+        int index = Arrays.binarySearch(siblings.myChildren, node, InspectionResultsViewComparator.INSTANCE);
+        assert index >= 0;
+        index++;
+        if (siblings.myChildren.length > index) return siblings.myChildren[index];
+        node = parent;
       }
-      return myModel.traverseFrom(node, next).filter(n -> n != node).filter(n -> isOccurrenceNode(n)).first();
+    }
+
+    /**
+     * Previous node (depth-first post-order traversal)
+     * @param node  the node to start from
+     * @return the previous node, or null if the specified node was the first (root) node in the tree.
+     */
+    private static InspectionTreeNode prev(InspectionTreeNode node) {
+      InspectionTreeNode parent = node.myParent;
+      if (parent == null) return null;
+      InspectionTreeNode.Children siblings = parent.myChildren;
+      assert siblings != null;
+      int index = Arrays.binarySearch(siblings.myChildren, node, InspectionResultsViewComparator.INSTANCE);
+      assert index >= 0;
+      index--;
+      if (index < 0) return parent; // if no sibling: go up.
+      InspectionTreeNode sibling = siblings.myChildren[index];
+      InspectionTreeNode.Children children = sibling.myChildren;
+      while (children != null && children.myChildren.length > 0) {
+        // if sibling: get its last child (of last child (of last child))
+        sibling = children.myChildren[children.myChildren.length - 1];
+        children = sibling.myChildren;
+      }
+      return sibling;
     }
 
     private InspectionTreeNode getSelectedNode() {
@@ -657,25 +730,27 @@ public final class InspectionTree extends Tree {
       return (InspectionTreeNode)path.getLastPathComponent();
     }
 
-    private static boolean isOccurrenceNode(@NotNull InspectionTreeNode node) {
-      if (node.isExcluded()) {
-        return false;
-      }
+    private boolean isOccurrenceNode(@NotNull InspectionTreeNode node) {
+      if (node.isExcluded()) return false;
       if (node instanceof RefElementNode refNode) {
-        if (hasDescriptorUnder(refNode)) return false;
-        final RefEntity element = refNode.getElement();
-        return element != null && element.isValid();
+        if (!(refNode.getElement() instanceof RefElementImpl element) ||
+            !element.isValid() ||
+            !element.isSuspicious() ||
+            !element.getRefManager().isDeclarationsFound() ||
+            element.isEntry()) {
+          return false;
+        }
+        InspectionToolWrapper<?, ?> wrapper = getToolWrapper(node);
+        if (wrapper == null) wrapper = getSingleToolWrapper();
+        return wrapper != null && wrapper.getShortName().contains("unused");
       }
       return node instanceof ProblemDescriptionNode;
     }
 
     @Nullable
     private static Navigatable createDescriptorForNode(@NotNull InspectionTreeNode node) {
-      if (node.isExcluded()) {
-        return null;
-      }
+      if (node.isExcluded()) return null;
       if (node instanceof RefElementNode refNode) {
-        if (hasDescriptorUnder(refNode)) return null;
         final RefEntity element = refNode.getElement();
         if (element == null || !element.isValid()) return null;
         if (element instanceof RefElement) {
@@ -691,7 +766,6 @@ public final class InspectionTree extends Tree {
       }
       return null;
     }
-
 
     @Nullable
     private static Navigatable navigate(final CommonProblemDescriptor descriptor) {
@@ -709,16 +783,5 @@ public final class InspectionTree extends Tree {
       return PsiNavigationSupport.getInstance().createNavigatable(refElement.getRefManager().getProject(), file,
                                                                   psiElement.getTextOffset());
     }
-
-    private static boolean hasDescriptorUnder(@NotNull RefElementNode node) {
-      InspectionTreeNode current = node;
-      while (current != null && current.getChildCount() != 0) {
-        current = current.getChildAt(0);
-        if (current instanceof ProblemDescriptionNode) return true;
-      }
-      return false;
-    }
-    
-    public void asdfasdfasdfasdf() {}
   }
 }
