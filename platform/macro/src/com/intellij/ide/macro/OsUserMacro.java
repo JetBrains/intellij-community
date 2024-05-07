2b67172b8a70f40e725c0b9baa4a9e0dd9c0c7c4
Alexander.Kass <Alexander.Kass@jetbrains.com>
2024-05-07 15:45:40 +0300

    data source: allow using macroses in init scripts
    GitOrigin-RevId: eae36fe56e32a6a10edcffa02be3a10d993489f5

---
+++ platform/macro/src/com/intellij/ide/macro/OsUserMacro.java
@@ -0,0 +1,28 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ package com.intellij.ide.macro;
+
+import com.intellij.ide.IdeCoreBundle;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.util.SystemProperties;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+class OsUserMacro extends Macro {
+  @NotNull
+  @Override
+  public String getName() {
+    return "OSUser";
+  }
+
+  @NotNull
+  @Override
+  public String getDescription() {
+    return IdeCoreBundle.message("macro.os.name");
+  }
+
+  @Nullable
+  @Override
+  public String expand(@NotNull DataContext dataContext) {
+    return SystemProperties.getUserName();
+  }
+}
