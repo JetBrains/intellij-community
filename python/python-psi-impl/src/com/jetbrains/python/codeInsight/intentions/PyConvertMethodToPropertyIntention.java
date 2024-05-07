788390047b8b73dc8b0094c5b3a6e7b3c529749e
Daniil Kalinin <Daniil.Kalinin@jetbrains.com>
2024-05-07 13:38:41 +0200

    PY-64326 fixes for intentions affected by reparseable PyStatementList implementation
    GitOrigin-RevId: 9a5954767e062dd95d6eab02283c63b1c39fba49

--- python/python-psi-impl/src/com/jetbrains/python/codeInsight/intentions/PyConvertMethodToPropertyIntention.java
+++ python/python-psi-impl/src/com/jetbrains/python/codeInsight/intentions/PyConvertMethodToPropertyIntention.java
@@ -7,6 +7,7 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiInvalidElementAccessException;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.usageView.UsageInfo;
 import com.intellij.util.IncorrectOperationException;
@@ -97,11 +98,30 @@ public final class PyConvertMethodToPropertyIntention extends PyBaseIntentionAct
       final PsiElement usageElement = usage.getElement();
       if (usageElement instanceof PyReferenceExpression) {
         final PsiElement parent = usageElement.getParent();
-        if (parent instanceof PyCallExpression) {
-          final PyArgumentList argumentList = ((PyCallExpression)parent).getArgumentList();
-          if (argumentList != null) argumentList.delete();
+        if (parent instanceof PyCallExpression callExpression) {
+          convertCallExpToRefExpr(callExpression);
         }
       }
+    }
+  }
+
+  private static void convertCallExpToRefExpr(PyCallExpression callExpression) {
+    PyExpression callee = callExpression.getCallee();
+    if (callee == null) {
+      throw new PsiInvalidElementAccessException(callExpression);
+    }
+
+    String newReferenceExpressionText = callee.getText();
+
+    PyExpression newExpression = PyElementGenerator.getInstance(callExpression.getProject())
+      .createExpressionFromText(LanguageLevel.forElement(callExpression), newReferenceExpressionText);
+
+    if (newExpression instanceof PyReferenceExpression referenceExpression) {
+      callExpression.replace(referenceExpression);
+    }
+    else {
+      throw new IllegalStateException(
+        "Failed to create reference expression from call expression with text: \"" + callExpression.getText() + "\"");
     }
   }
 }
