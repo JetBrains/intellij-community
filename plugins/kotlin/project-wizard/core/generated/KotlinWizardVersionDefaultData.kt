fd56eddb5a3ce93016cd7593707cff44fc0e089c
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2024-05-08 20:12:18 +0200

    [Kotlin] Updated Kotlin wizard and JPS version to 1.9.24, upgraded foojay version
    ^KTIJ-29892 fixed
    
    GitOrigin-RevId: e2909f2b77d2917f423f704b40b97b13b9d0cded

--- plugins/kotlin/project-wizard/core/generated/KotlinWizardVersionDefaultData.kt
+++ plugins/kotlin/project-wizard/core/generated/KotlinWizardVersionDefaultData.kt
@@ -9,12 +9,12 @@ import org.jetbrains.kotlin.tools.projectWizard.compatibility.KotlinWizardVersio
  * DO NOT EDIT IT BY HAND, run "Generate Kotlin Wizard Default Data" configuration instead
  */
 internal val DEFAULT_KOTLIN_WIZARD_VERSIONS = KotlinWizardVersionState(
-    kotlinPluginVersion = "1.9.23",
+    kotlinPluginVersion = "1.9.24",
     kotlinForComposeVersion = "1.7.20",
     composeCompilerExtension = "1.4.3",
     minGradleFoojayVersion = "7.6",
     minKotlinFoojayVersion = "1.5.30",
-    foojayVersion = "0.5.0",
+    foojayVersion = "0.8.0",
     failsafeVersion = "2.22.2",
     surefireVersion = "2.22.2",
     gradleAndroidVersion = "8.1.0",
