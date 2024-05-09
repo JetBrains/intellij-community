efe1e51c1d0d88fc86942490888fff0fb4462eac
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2024-05-09 14:52:55 +0300

    [terminal] IJPL-101589 Create code-based command spec for cd command
    There is a problem with interpretation of json-based command spec for `cd`. It specifies that there should be directory suggestions + some additional hardcoded values (`-` and `~`).
    So, our completion engine is suggesting hardcoded values even if user already typed some path. For example, we should not suggest `-` and `~` in this case: `cd src/<caret>`.
    There is no easy way of solving it on the completion engine side, so better to fix in on the client side - create our own spec for `cd`, that takes into account this problem.
    
    GitOrigin-RevId: c4231769932d66e582a5234eb12d0c25f79a9c80

---
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/block/completion/spec/specs/ShellCdCommandSpec.kt
@@ -0,0 +1,37 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.terminal.block.completion.spec.specs
+
+import com.intellij.terminal.block.completion.spec.ShellCompletionSuggestion
+import org.jetbrains.plugins.terminal.TerminalBundle
+import org.jetbrains.plugins.terminal.block.completion.spec.ShellCommandSpec
+import org.jetbrains.plugins.terminal.block.completion.spec.ShellCompletionSuggestion
+import org.jetbrains.plugins.terminal.block.completion.spec.ShellDataGenerators.getParentPath
+import org.jetbrains.plugins.terminal.block.completion.spec.ShellRuntimeDataGenerator
+import org.jetbrains.plugins.terminal.block.completion.spec.getFileSuggestions
+
+internal fun cdCommandSpec() = ShellCommandSpec("cd") {
+  description = TerminalBundle.messagePointer("cd.command.description")
+  argument {
+    displayName = TerminalBundle.messagePointer("cd.command.arg.displayName")
+
+    val generator = ShellRuntimeDataGenerator(
+      debugName = "cd suggestions",
+      getCacheKey = { "cd suggestions:${getParentPath(it.typedPrefix)}" }
+    ) { context ->
+      val path = getParentPath(context.typedPrefix)
+      val directorySuggestions = context.getFileSuggestions(path, onlyDirectories = true)
+      // Do not add additional suggestions if we are completing the nested path.
+      // For example, if typedPrefix is like this: 'src/incompletePath'
+      if (path.isEmpty()) {
+        directorySuggestions + additionalSuggestions()
+      }
+      else directorySuggestions
+    }
+    suggestions(generator)
+  }
+}
+
+private fun additionalSuggestions(): List<ShellCompletionSuggestion> = listOf(
+  ShellCompletionSuggestion("-", description = TerminalBundle.messagePointer("cd.command.arg.dash.description")),
+  ShellCompletionSuggestion("~", description = TerminalBundle.messagePointer("cd.command.arg.tilda.description"))
+)
