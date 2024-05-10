a8d44c05097ee759d4cfc642f08aafd1c6c577ac
Leonid Shalupov <leonid@shalupov.com>
2024-05-10 13:33:48 +0200

    usages: do not leak child coroutine scopes
    create scope only once and use it every time
    
    GitOrigin-RevId: 3041812815644a4e4a6e92058d703e7337541960

--- platform/usageView-impl/src/com/intellij/usages/impl/UsageViewCoroutineScopeProvider.java
+++ platform/usageView-impl/src/com/intellij/usages/impl/UsageViewCoroutineScopeProvider.java
@@ -3,6 +3,8 @@ package com.intellij.usages.impl;
 
 import com.intellij.openapi.components.Service;
 import com.intellij.openapi.project.Project;
+import com.intellij.platform.util.coroutines.CoroutineScopeKt;
+import kotlin.coroutines.EmptyCoroutineContext;
 import kotlinx.coroutines.CoroutineScope;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
@@ -13,15 +15,20 @@ public final class UsageViewCoroutineScopeProvider {
   private final CoroutineScope myCoroutineScope;
 
   public UsageViewCoroutineScopeProvider(@NotNull CoroutineScope coroutineScope) {
-    myCoroutineScope = coroutineScope;
+    // Create a child supervisor scope because we don't want this scope to cancel when some child coroutine fails.
+    // We want it to LOG.error.
+    myCoroutineScope = CoroutineScopeKt.childScope(coroutineScope, getClass().getName(), EmptyCoroutineContext.INSTANCE, true);
   }
 
   public static UsageViewCoroutineScopeProvider getInstance(@NotNull Project project) {
     return project.getService(UsageViewCoroutineScopeProvider.class);
   }
 
-  // This service lifetime, could be used in UI components
-  // as coroutine scope for various BGT activities
+  /**
+   *  This service lifetime could be used in UI components
+   *  as coroutine scope for various BGT activities.
+   *  This scope is supervised, so failures under it won't cancel the entire provider scope.
+   */
   public CoroutineScope getCoroutineScope() {
     return myCoroutineScope;
   }
