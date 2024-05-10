77fbbab79530cee036fc4b7c2df0700dc6a49782
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-05-10 15:25:34 +0200

    [vcs] IJPL-116992 Use new API for PatchFileDropHandler
    GitOrigin-RevId: 97093fbf0f91b6df0bbe4e4208c787f427c1b3c3

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/patch/PatchFileDropHandler.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/patch/PatchFileDropHandler.kt
@@ -1,51 +1,32 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.intellij.openapi.vcs.changes.patch;
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.vcs.changes.patch
 
-import com.intellij.ide.dnd.FileCopyPasteUtil;
-import com.intellij.openapi.editor.CustomFileDropHandler;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.fileTypes.FileTypeRegistry;
-import com.intellij.openapi.project.Project;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.FileDropHandler
+import com.intellij.openapi.fileTypes.FileTypeRegistry
+import com.intellij.openapi.project.Project
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.awt.datatransfer.Transferable
+import java.io.File
 
-import java.awt.datatransfer.Transferable;
-import java.io.File;
-import java.util.List;
+internal class PatchFileDropHandler : FileDropHandler {
+  override suspend fun handleDrop(project: Project,
+                                  t: Transferable,
+                                  files: Collection<File>,
+                                  editor: Editor?): Boolean {
+    if (files.size != 1) return false
+    if (!looksLikePatchFile(files.first())) return false
+    val patchFile = ApplyPatchUtil.getPatchFile(files.first()) ?: return false
 
-final class PatchFileDropHandler extends CustomFileDropHandler {
-  @Override
-  public boolean canHandle(@NotNull Transferable t, @Nullable Editor editor) {
-    List<File> list = FileCopyPasteUtil.getFileList(t);
-    if (list == null || list.size() != 1) return false;
-    return looksLikePatchFile(list.get(0));
+    withContext(Dispatchers.EDT) {
+      ApplyPatchUtil.showAndGetApplyPatch(project, patchFile)
+    }
+    return true
   }
 
-  @Override
-  public boolean handleDrop(@NotNull Transferable t, @Nullable Editor editor, @NotNull Project project) {
-    List<File> list = FileCopyPasteUtil.getFileList(t);
-    if (list == null || list.size() != 1) return false;
-    return ApplyPatchUtil.showAndGetApplyPatch(project, list.get(0));
-  }
-
-  private static boolean looksLikePatchFile(@NotNull File file) {
-    // do not use VFS here to detect the file type, may lead to freezes
-    FileType fileType = FileTypeRegistry.getInstance().getFileTypeByFileName(file.getName());
-    return PatchFileType.INSTANCE == fileType;
+  private fun looksLikePatchFile(file: File): Boolean {
+    return FileTypeRegistry.getInstance().getFileTypeByFileName(file.name) === PatchFileType.INSTANCE
   }
 }
