cbf3c8c1b22cbff914fc562bd8d98e2e63f00e45
Leonid Shalupov <leonid@shalupov.com>
2024-05-12 10:58:29 +0200

    usages: do not explicitly request childScope in UsageViewCoroutineScopeProvider
    CoroutineScope in a service is already supervised and won't be canceled because of child failures
    
    GitOrigin-RevId: 81a8dd3c00152fed9434715973223c879e8555a6

--- platform/usageView-impl/src/com/intellij/usages/impl/UsageViewCoroutineScopeProvider.java
+++ platform/usageView-impl/src/com/intellij/usages/impl/UsageViewCoroutineScopeProvider.java
@@ -3,8 +3,6 @@ package com.intellij.usages.impl;
 
 import com.intellij.openapi.components.Service;
 import com.intellij.openapi.project.Project;
-import com.intellij.platform.util.coroutines.CoroutineScopeKt;
-import kotlin.coroutines.EmptyCoroutineContext;
 import kotlinx.coroutines.CoroutineScope;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
@@ -15,20 +13,15 @@ public final class UsageViewCoroutineScopeProvider {
   private final CoroutineScope myCoroutineScope;
 
   public UsageViewCoroutineScopeProvider(@NotNull CoroutineScope coroutineScope) {
-    // Create a child supervisor scope because we don't want this scope to cancel when some child coroutine fails.
-    // We want it to LOG.error.
-    myCoroutineScope = CoroutineScopeKt.childScope(coroutineScope, getClass().getName(), EmptyCoroutineContext.INSTANCE, true);
+    myCoroutineScope = coroutineScope;
   }
 
   public static UsageViewCoroutineScopeProvider getInstance(@NotNull Project project) {
     return project.getService(UsageViewCoroutineScopeProvider.class);
   }
 
-  /**
-   *  This service lifetime could be used in UI components
-   *  as coroutine scope for various BGT activities.
-   *  This scope is supervised, so failures under it won't cancel the entire provider scope.
-   */
+  // This service lifetime, could be used in UI components
+  // as coroutine scope for various BGT activities
   public CoroutineScope getCoroutineScope() {
     return myCoroutineScope;
   }
