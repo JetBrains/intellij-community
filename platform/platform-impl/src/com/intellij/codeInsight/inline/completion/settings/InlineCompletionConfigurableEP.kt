d21ac97ab1f0273e196f3584259b2798163a6f45
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2024-05-13 17:16:07 +0200

    [ai-completion] ML-3270: create general configurable for inline completion
    GitOrigin-RevId: 3009a968bfaaa48bfbac142d17bad89744577974

---
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/settings/InlineCompletionConfigurableEP.kt
@@ -0,0 +1,13 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.codeInsight.inline.completion.settings
+
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.options.ConfigurableEP
+import com.intellij.openapi.options.UnnamedConfigurable
+
+class InlineCompletionConfigurableEP : ConfigurableEP<UnnamedConfigurable>() {
+
+  companion object {
+    val EP_NAME = ExtensionPointName<InlineCompletionConfigurableEP>("com.intellij.inlineCompletionConfigurable")
+  }
+}
