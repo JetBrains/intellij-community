8d2248dd245c1fecda8aebb61784f8f96be7b54c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-05-13 07:55:37 +0200

    cleanup
    GitOrigin-RevId: 055e786f1fd72ea3046af5d9c7ddfe2ba7917bcb

--- plugins/kotlin/code-insight/override-implement-k1/src/org/jetbrains/kotlin/idea/core/overrideImplement/Fe10OverrideImplementFacility.kt
+++ plugins/kotlin/code-insight/override-implement-k1/src/org/jetbrains/kotlin/idea/core/overrideImplement/Fe10OverrideImplementFacility.kt
@@ -1,11 +1,11 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.core.overrideImplement
 
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.idea.codeInsight.overrideImplement.OverrideImplementFacility
 
-class Fe10OverrideImplementFacility : OverrideImplementFacility {
+private class Fe10OverrideImplementFacility : OverrideImplementFacility {
     override fun override(editor: Editor, file: PsiFile, implementAll: Boolean) {
         OverrideMembersHandler().invoke(file.project, editor, file, implementAll)
     }
