56e67eb4a7b6d3d4a90ab381e308c73b1196aa5d
Ivan Migalev <ivan.migalev@jetbrains.com>
2024-05-14 19:40:23 +0200

    (RIDER-112399) ExperimentalUIImpl: do not fail startup on exceptions from ExperimentalUI.Listener
    GitOrigin-RevId: 4dc6e1628d3096a170997f4de404f04a80c8c670

--- platform/lang-impl/src/com/intellij/ui/ExperimentalUIImpl.kt
+++ platform/lang-impl/src/com/intellij/ui/ExperimentalUIImpl.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 @file:Suppress("ReplaceGetOrSet", "ReplacePutWithAssignment")
 
 package com.intellij.ui
@@ -80,8 +80,8 @@ private class ExperimentalUIImpl : ExperimentalUI() {
 
     isResetLaf = true
 
-    for (listener in EP_LISTENER.extensionList) {
-      listener.changeUI(enabled)
+    EP_LISTENER.forEachExtensionSafe {
+      it.changeUI(enabled)
     }
 
     try {
