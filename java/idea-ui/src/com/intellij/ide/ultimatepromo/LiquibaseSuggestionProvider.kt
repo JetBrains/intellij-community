b37b1a3edff457c31bfcfdfdc4917c6fb96929a5
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-05-14 12:51:58 +0200

    [liquibase] Unused imports in LiquibaseSuggestionProvider
    GitOrigin-RevId: a1eae8e56b1197ce8ec705c1ef1a727d9cf02743

--- java/idea-ui/src/com/intellij/ide/ultimatepromo/LiquibaseSuggestionProvider.kt
+++ java/idea-ui/src/com/intellij/ide/ultimatepromo/LiquibaseSuggestionProvider.kt
@@ -1,15 +1,12 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.ultimatepromo
 
-import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.PluginSuggestion
 import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.PluginSuggestionProvider
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.xml.XmlFile
-import com.intellij.ui.EditorNotificationPanel
-import java.util.function.Function
 
 internal class LiquibaseSuggestionProvider : PluginSuggestionProvider {
   override fun getSuggestion(project: Project, file: VirtualFile): PluginSuggestion? {
