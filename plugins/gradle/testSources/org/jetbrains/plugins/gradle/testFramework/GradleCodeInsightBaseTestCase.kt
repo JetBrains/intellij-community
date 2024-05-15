0b68a902e87488a1eeb24df0d0130816237fa67f
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2024-05-15 15:14:00 +0200

    [gradle][test] initialize GradleBuildClasspathManager in Gradle code insight test fixture.
    GitOrigin-RevId: 91ce907e0163e5ffaad1f57e32ccaada9bfa51db

--- plugins/gradle/testSources/org/jetbrains/plugins/gradle/testFramework/GradleCodeInsightBaseTestCase.kt
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/testFramework/GradleCodeInsightBaseTestCase.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.testFramework
 
 import com.intellij.openapi.module.Module
@@ -8,6 +8,7 @@ import com.intellij.testFramework.fixtures.IdeaProjectTestFixture
 import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture
 import com.intellij.testFramework.fixtures.impl.JavaCodeInsightTestFixtureImpl
 import com.intellij.testFramework.fixtures.impl.TempDirTestFixtureImpl
+import org.jetbrains.plugins.gradle.service.GradleBuildClasspathManager
 import org.jetbrains.plugins.gradle.testFramework.fixtures.GradleProjectTestFixture
 import org.jetbrains.plugins.groovy.util.BaseTest
 
@@ -54,6 +55,11 @@ abstract class GradleCodeInsightBaseTestCase : GradleProjectTestCase(), BaseTest
 
     init {
       setVirtualFileFilter(null)
+    }
+
+    override fun setUp() {
+      super.setUp()
+      GradleBuildClasspathManager.getInstance(project).reload()
     }
   }
 }
