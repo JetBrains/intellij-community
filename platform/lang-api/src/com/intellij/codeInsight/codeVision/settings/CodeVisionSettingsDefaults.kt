53e3f75977f993f932b8eb543d53a3eaea70bd6a
Andy Magee <andymagee@google.com>
2024-05-15 13:36:18 -0700

    IJPL-149990 Allow custom defaults for CodeVisionSettings
    (cherry picked from commit 6de7daf202db864746269ec32b0f23abf7e80ab0)
    
    GitOrigin-RevId: 29e9c7881943a4c0c05f02cf948f803b93952aed
    Bug: 339003051
    Change-Id: I0b2582d81b376536c4c7e9464a9a362976cb00f3

---
+++ platform/lang-api/src/com/intellij/codeInsight/codeVision/settings/CodeVisionSettingsDefaults.kt
@@ -0,0 +1,26 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.codeInsight.codeVision.settings
+
+import com.intellij.openapi.extensions.ExtensionPointName
+
+/**
+ * Extension allowing customization of defaults for [CodeVisionSettings].
+ */
+interface CodeVisionSettingsDefaults {
+  companion object {
+    private val EP_NAME =
+      ExtensionPointName.create<CodeVisionSettingsDefaults>("com.intellij.codeInsight.codeVision.settings.defaults")
+
+    fun getInstance(): CodeVisionSettingsDefaults = EP_NAME.extensionList.firstOrNull() ?: NONE
+  }
+
+  /**
+   * Specified the default enabled state for each given provider. Any providers not specified are assumed to be
+   * enabled by default.
+   */
+  val defaultEnablementForProviderId: Map<String, Boolean>
+
+  private object NONE : CodeVisionSettingsDefaults {
+    override val defaultEnablementForProviderId: Map<String, Boolean> = emptyMap()
+  }
+}
