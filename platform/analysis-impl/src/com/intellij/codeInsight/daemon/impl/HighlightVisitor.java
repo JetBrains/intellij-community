1cbf59ebf465c3f00a863cf8b06cb945b2ea94e3
Yann Cébron <yann.cebron@jetbrains.com>
2024-05-16 10:05:28 +0200

    [platform] HighlightVisitor: add note about usage
    GitOrigin-RevId: 4ff168a7e53bdd5927f5caf3953298d763e1573b

--- platform/analysis-impl/src/com/intellij/codeInsight/daemon/impl/HighlightVisitor.java
+++ platform/analysis-impl/src/com/intellij/codeInsight/daemon/impl/HighlightVisitor.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.daemon.impl;
 
 import com.intellij.codeInsight.daemon.impl.analysis.HighlightInfoHolder;
@@ -9,6 +9,11 @@ import com.intellij.psi.PsiFile;
 import com.intellij.util.ArrayFactory;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Apart from {@link com.intellij.codeInsight.daemon.RainbowVisitor},
+ * prefer using {@link com.intellij.lang.annotation.Annotator} or {@link com.intellij.codeInspection.LocalInspectionTool}
+ * to provide additional highlighting.
+ */
 public interface HighlightVisitor extends PossiblyDumbAware {
   HighlightVisitor [] EMPTY_ARRAY = new HighlightVisitor[0];
   ArrayFactory<HighlightVisitor> ARRAY_FACTORY = count -> count == 0 ? EMPTY_ARRAY : new HighlightVisitor[count];
