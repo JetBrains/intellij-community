e7979392443f2e2621b8214f60c141162ede068f
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2024-05-16 11:47:14 +0300

    IJPL-797 'intellij.platform.usageView.impl' review internal API
    Make ReadAccessFilteringRule internal. No external usages.
    
    GitOrigin-RevId: a65fe8a5bcf17c37f5d794fa2959c81ade0ce4ae

--- platform/usageView-impl/src/com/intellij/usages/impl/rules/ReadAccessFilteringRule.kt
+++ platform/usageView-impl/src/com/intellij/usages/impl/rules/ReadAccessFilteringRule.kt
@@ -5,7 +5,9 @@ import com.intellij.usages.ReadWriteAccessUsage
 import com.intellij.usages.Usage
 import com.intellij.usages.UsageTarget
 import com.intellij.usages.rules.UsageFilteringRule
+import org.jetbrains.annotations.ApiStatus.Internal
 
+@Internal
 object ReadAccessFilteringRule : UsageFilteringRule {
 
   override fun getActionId(): String = "UsageFiltering.ReadAccess"
