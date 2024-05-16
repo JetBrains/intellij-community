be4ed760f07fad0451b845c889e00d629ec2ce72
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2024-05-16 12:43:47 +0200

    [ai-completion] ML-3270: implement common ML Completion settings
    GitOrigin-RevId: 06f621412e2f2ae97efc1eed078cdaa10635cc6e

--- platform/platform-impl/src/com/intellij/codeInsight/inline/completion/settings/InlineCompletionConfigurable.kt
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/settings/InlineCompletionConfigurable.kt
@@ -3,7 +3,6 @@ package com.intellij.codeInsight.inline.completion.settings
 
 import com.intellij.application.options.editor.EditorOptionsProvider
 import com.intellij.openapi.application.ApplicationBundle
-import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.extensions.BaseExtensionPointName
 import com.intellij.openapi.options.BoundCompositeConfigurable
 import com.intellij.openapi.options.Configurable.WithEpDependencies
@@ -11,6 +10,8 @@ import com.intellij.openapi.options.UnnamedConfigurable
 import com.intellij.openapi.options.ex.ConfigurableWrapper
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.util.ui.JBUI
+import javax.swing.Box
 
 class InlineCompletionConfigurable : BoundCompositeConfigurable<UnnamedConfigurable>(
   ApplicationBundle.message("title.inline.completion"),
@@ -29,13 +30,20 @@ class InlineCompletionConfigurable : BoundCompositeConfigurable<UnnamedConfigura
 
   override fun createPanel(): DialogPanel {
     return panel {
-      configurables.forEach { appendDslConfigurable(it) }
+      configurables.forEachIndexed { index, configurable ->
+        appendDslConfigurable(configurable)
+        if (index != configurables.lastIndex) {
+          repeat(2) {
+            row {
+              cell(Box.Filler(JBUI.size(0), JBUI.size(0), JBUI.size(Integer.MAX_VALUE, 0)))
+            }
+          }
+        }
+      }
     }
   }
 
   companion object {
     const val ID: String = "editor.preferences.inline.completion"
-
-    private val LOG = logger<InlineCompletionConfigurable>()
   }
 }
