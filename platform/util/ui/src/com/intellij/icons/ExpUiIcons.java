ffea726c064444a73fcd24f7fd14970d03232c91
Dima Golovkov <dmitry.golovkov@jetbrains.com>
2024-05-19 16:52:18 +0300

    Add cwmShare icon for IntUI for CWM
    GitOrigin-RevId: c4c951671271fa2417babb4b6142ef5837c00e8b

--- platform/util/ui/src/com/intellij/icons/ExpUiIcons.java
+++ platform/util/ui/src/com/intellij/icons/ExpUiIcons.java
@@ -173,6 +173,7 @@ public final class ExpUiIcons {
     /** 16x16 */ public static final @NotNull Icon CwmScreenInBrowserOn = load("expui/codeWithMe/cwmScreenInBrowserOn.svg", 522769420, 2);
     /** 16x16 */ public static final @NotNull Icon CwmScreenOff = load("expui/codeWithMe/cwmScreenOff.svg", 1209105362, 2);
     /** 16x16 */ public static final @NotNull Icon CwmScreenOn = load("expui/codeWithMe/cwmScreenOn.svg", 2032381401, 2);
+    /** 16x16 */ public static final @NotNull Icon CwmShare = load("expui/codeWithMe/cwmShare.svg", -1525682652, 2);
     /** 11x11 */ public static final @NotNull Icon CwmSharingAvatarOn = load("expui/codeWithMe/cwmSharingAvatarOn.svg", 104822006, 2);
     /** 16x16 */ public static final @NotNull Icon CwmUsers = load("expui/codeWithMe/cwmUsers.svg", 374429727, 2);
     /** 20x20 */ public static final @NotNull Icon CwmUsers_20x20 = load("expui/codeWithMe/cwmUsers@20x20.svg", 1716201367, 2);
