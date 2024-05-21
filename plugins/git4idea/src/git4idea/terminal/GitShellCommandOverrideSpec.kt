3322621879fd229bf47e7959f3aff6e532a63676
Chris Lemaire <chris.lemaire@jetbrains.com>
2024-05-21 18:21:35 +0200

    [git-term] Add an initial terminal git autocompletion specification
    GitOrigin-RevId: 74057c98261777f7b260e13c15a706ef388e48d5

---
+++ plugins/git4idea/src/git4idea/terminal/GitShellCommandOverrideSpec.kt
@@ -0,0 +1,13 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:Suppress("HardCodedStringLiteral")
+
+package git4idea.terminal
+
+import com.intellij.terminal.completion.spec.ShellCommandParserOptions
+import org.jetbrains.plugins.terminal.block.completion.spec.ShellCommandSpec
+
+internal val gitOverrideSpec = ShellCommandSpec("git") {
+  subcommands {
+    // ...
+  }
+}
