7a5cb2f41945934d021727226e6a88f71605eb44
Chris Lemaire <chris.lemaire@jetbrains.com>
2024-05-21 18:31:56 +0200

    [git-term] Add custom completion for remotes (IJPL-101545)
    Adds completion suggestions for remotes taken from GitRepository if possible.
    
    This applies to terminal commands such as `git remote rm [remote]`,
    where `[remote]` can be completed with `main` for instance.
    
    GitOrigin-RevId: c0d506ba8e66be5c20e77cdbf51fa0a4777b2da3

--- plugins/git4idea/src/git4idea/terminal/GitShellCommandOverrideSpec.kt
+++ plugins/git4idea/src/git4idea/terminal/GitShellCommandOverrideSpec.kt
@@ -3,12 +3,125 @@
 
 package git4idea.terminal
 
+import com.intellij.openapi.vcs.LocalFilePath
+import com.intellij.terminal.completion.spec.ShellCommandParserOptions
+import com.intellij.terminal.completion.spec.ShellCompletionSuggestion
+import com.intellij.terminal.completion.spec.ShellRuntimeContext
+import com.intellij.terminal.completion.spec.ShellRuntimeDataGenerator
+import git4idea.GitUtil
+import git4idea.repo.GitRepository
 import org.jetbrains.plugins.terminal.block.completion.spec.ShellCommandSpec
+import org.jetbrains.plugins.terminal.block.completion.spec.ShellCompletionSuggestion
+import org.jetbrains.plugins.terminal.block.completion.spec.ShellRuntimeDataGenerator
+import org.jetbrains.plugins.terminal.block.completion.spec.project
+
+internal val ShellRuntimeContext.repository: GitRepository?
+  get() = GitUtil.getRepositoryManager(project)
+    .getRepositoryForFileQuick(LocalFilePath(currentDirectory, true))
+
+// Find remote generators in the git.json file by searching for scripts doing:
+// remote -v
+private val remotesGenerator: ShellRuntimeDataGenerator<List<ShellCompletionSuggestion>> = ShellRuntimeDataGenerator { context ->
+  val repository = context.repository
+
+  if (repository != null) {
+    repository.remotes.map {
+      ShellCompletionSuggestion(
+        name = it.name,
+        description = it.firstUrl ?: ""
+      )
+    }
+  }
+  else {
+    val result = context.runShellCommand("git --no-optional-locks remote -v")
+    if (result.exitCode != 0) return@ShellRuntimeDataGenerator listOf()
+
+    result.output.lines()
+      .mapNotNull { line ->
+        val remoteLineParts = line.split("\t")
+        if (line.isEmpty() || remoteLineParts.isEmpty()) return@mapNotNull null
+
+        val description = remoteLineParts.getOrNull(1)?.split(" ")?.firstOrNull() ?: ""
+        ShellCompletionSuggestion(
+          name = remoteLineParts.first(),
+          description = description
+        )
+      }
+      .distinctBy { it.name }
+  }
+}
 
 internal val gitOverrideSpec = ShellCommandSpec("git") {
   addGitAliases()
 
   subcommands {
-    // ...
+    subcommand("push") {
+      argument {
+        displayName("remote")
+        suggestions(remotesGenerator)
+        isOptional = true
+      }
+      argument {
+        displayName("branch")
+        isOptional = true
+      }
+    }
+    subcommand("pull") {
+      option("--rebase") {
+        description("Fetch the remote’s copy of current branch and rebases it into the local copy")
+        argument {
+          displayName("remote")
+          suggestions("false", "true", "merges", "preserve", "interactive")
+          suggestions(remotesGenerator)
+          isOptional = true
+        }
+      }
+
+      argument {
+        displayName("remote")
+        suggestions(remotesGenerator)
+        isOptional = true
+      }
+      argument {
+        displayName("branch")
+        isOptional = true
+      }
+    }
+    subcommand("remote") {
+      subcommands {
+        subcommand("rm", "remove") {
+          argument {
+            displayName("remote")
+            suggestions(remotesGenerator)
+          }
+        }
+
+        subcommand("rename") {
+          description("Renames given remote [name]") // Seems it's currently wrong in the JSON
+          argument {
+            displayName("old remote")
+            suggestions(remotesGenerator)
+          }
+          argument {
+            displayName("new remote name")
+          }
+        }
+      }
+    }
+    subcommand("fetch") {
+      argument {
+        displayName("remote")
+        suggestions(remotesGenerator)
+        isOptional = true
+      }
+      argument {
+        displayName("branch")
+        isOptional = true
+      }
+      argument {
+        displayName("refspec")
+        isOptional = true
+      }
+    }
   }
 }
