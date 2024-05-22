4317e0a1ed76a441ab996d6f3e1613242de084e0
Petr <petr.golubev@jetbrains.com>
2024-05-22 18:21:44 +0200

    PY-34617 Restore PyIfPartIf, PyIfPartElif interfaces
    GitOrigin-RevId: 3283be157d6f030840103fba5b8c7cfcee508e2b

--- python/python-psi-impl/src/com/jetbrains/python/psi/impl/PyIfPartElifImpl.java
+++ python/python-psi-impl/src/com/jetbrains/python/psi/impl/PyIfPartElifImpl.java
@@ -2,13 +2,12 @@
 package com.jetbrains.python.psi.impl;
 
 import com.intellij.lang.ASTNode;
-import com.jetbrains.python.ast.PyAstIfPartElif;
-import com.jetbrains.python.psi.PyIfPart;
+import com.jetbrains.python.psi.PyIfPartElif;
 
 /**
  * PyIfPart that represents an 'elif' part.
  */
-public class PyIfPartElifImpl extends PyConditionalStatementPartImpl implements PyAstIfPartElif, PyIfPart {
+public class PyIfPartElifImpl extends PyConditionalStatementPartImpl implements PyIfPartElif {
   public PyIfPartElifImpl(ASTNode astNode) {
     super(astNode);
   }
