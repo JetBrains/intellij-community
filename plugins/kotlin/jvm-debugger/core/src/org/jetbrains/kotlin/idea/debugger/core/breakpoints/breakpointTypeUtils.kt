fd313be3e7442931d017579179e2bdd88bc100d0
Maksim Zuev <maksim.zuev@jetbrains.com>
2024-05-22 10:47:50 +0200

    [kotlin] IDEA-353736 Move line range check to the util function
    (cherry picked from commit fe0303a6153fedb4b3fa85c2e93d1d678fcf9f18)
    
    IJ-CR-134989
    
    GitOrigin-RevId: 0ac48a22bf94acee9e6cc2823ca3a364c846f1ff

--- plugins/kotlin/jvm-debugger/core/src/org/jetbrains/kotlin/idea/debugger/core/breakpoints/breakpointTypeUtils.kt
+++ plugins/kotlin/jvm-debugger/core/src/org/jetbrains/kotlin/idea/debugger/core/breakpoints/breakpointTypeUtils.kt
@@ -145,12 +145,8 @@ fun getLambdasAtLineIfAny(file: KtFile, line: Int): List<KtFunction> {
 }
 
 internal fun getLambdasAtLine(file: KtFile, line: Int): List<KtFunction> {
-    if (!DocumentUtil.isValidLine(line, file.fileDocument)) return emptyList()
-    val start = file.getLineStartOffset(line)
-    val end = file.getLineEndOffset(line)
-    if (start == null || end == null) {
-        return emptyList()
-    }
+    val start = file.getLineStartOffset(line) ?: return emptyList()
+    val end = file.getLineEndOffset(line) ?: return emptyList()
     val result = mutableSetOf<KtFunction>()
 
     var offset: Int = start
