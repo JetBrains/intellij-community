0d59b47abd587a0af5e259e1c18bfec91fd5474d
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2024-05-22 16:15:07 +0200

    diff: move GenericDataProvider to API module
    GitOrigin-RevId: fcb31df1c259c95f867215df1e5c1a048d46e118

--- platform/diff-api/src/com/intellij/openapi/diff/impl/GenericDataProvider.java
+++ platform/diff-api/src/com/intellij/openapi/diff/impl/GenericDataProvider.java
@@ -1,15 +1,17 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.diff.impl;
 
 import com.intellij.openapi.actionSystem.DataKey;
 import com.intellij.openapi.actionSystem.DataProvider;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Prefer using {@link com.intellij.diff.util.DiffUtil#putDataKey} instead
+ */
 public class GenericDataProvider implements DataProvider {
   private final Map<String, Object> myGenericData;
   private final DataProvider myParentProvider;
@@ -23,10 +25,6 @@ public class GenericDataProvider implements DataProvider {
     myGenericData = new HashMap<>();
   }
 
-  /**
-   * @see com.intellij.diff.util.DiffUtil#putDataKey
-   */
-  @ApiStatus.Internal
   public <T> void putData(DataKey<T> key, T value) {
     myGenericData.put(key.getName(), value);
   }
