410c2e853f9d684222f86f325f2706ab3c7c780b
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2024-05-23 20:27:47 +0200

    [kotlin] Adapt code to naming changes in Analysis API (Kt -> Ka): fix compilation
    KT-67996
    
    GitOrigin-RevId: 0eef1175ce608df8c1d3350ad0ded62766946303

--- plugins/kotlin/code-insight/fixes-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/fixes/ConvertCollectionLiteralToIntArrayOfFixFactory.kt
+++ plugins/kotlin/code-insight/fixes-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/fixes/ConvertCollectionLiteralToIntArrayOfFixFactory.kt
@@ -1,13 +1,13 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.k2.codeinsight.fixes
 
-import org.jetbrains.kotlin.analysis.api.fir.diagnostics.KtFirDiagnostic
+import org.jetbrains.kotlin.analysis.api.fir.diagnostics.KaFirDiagnostic
 import org.jetbrains.kotlin.idea.codeinsight.api.applicators.fixes.KotlinQuickFixFactory
 import org.jetbrains.kotlin.idea.quickfix.ConvertCollectionLiteralToIntArrayOfFix
 
 internal object ConvertCollectionLiteralToIntArrayOfFixFactory {
 
-    val convertCollectionLiteralToIntArrayOfFixFactory = KotlinQuickFixFactory.ModCommandBased { diagnostic: KtFirDiagnostic.Unsupported ->
+    val convertCollectionLiteralToIntArrayOfFixFactory = KotlinQuickFixFactory.ModCommandBased { diagnostic: KaFirDiagnostic.Unsupported ->
         val action = ConvertCollectionLiteralToIntArrayOfFix.createIfApplicable(
             element = diagnostic.psi,
             unsupportedFeature = diagnostic.unsupported
