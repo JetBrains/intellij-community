4566b2afe73e5a9b478d983f4139a52030a929ae
Ekaterina Berezhko <ekaterina.berezhko@jetbrains.com>
2024-05-23 13:48:11 +0300

    [Markdown][IJPL-92197] Allow markdown numbering with only ones
    GitOrigin-RevId: abee14e354659224b878ea09a1d7f61817850266

--- plugins/markdown/core/src/org/intellij/plugins/markdown/settings/MarkdownCodeInsightSettings.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/settings/MarkdownCodeInsightSettings.kt
@@ -1,7 +1,9 @@
 package org.intellij.plugins.markdown.settings
 
 import com.intellij.openapi.components.*
+import org.intellij.plugins.markdown.MarkdownBundle
 import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.Nls
 
 @ApiStatus.Experimental
 @State(name = "MarkdownCodeInsightSettings",
@@ -16,7 +18,16 @@ class MarkdownCodeInsightSettings: SimplePersistentStateComponent<MarkdownCodeIn
     var adjustListIndentation: Boolean by property(true)
     var smartEnterAndBackspace: Boolean by property(true)
     var renumberListsOnType: Boolean by property(false)
+    var listNumberingType by enum(ListNumberingType.SEQUENTIAL)
     var enableFileDrop: Boolean by property(true)
+  }
+
+  enum class ListNumberingType(@Nls private val text: String) {
+    SEQUENTIAL("markdown.smart.keys.configurable.lists.numbering.sequential"),
+    ONES("markdown.smart.keys.configurable.lists.numbering.all.ones"),
+    PREVIOUS_NUMBER("markdown.smart.keys.configurable.lists.numbering.previous.number");
+
+    override fun toString(): String = MarkdownBundle.message(text)
   }
 
   override fun noStateLoaded() {
