edd8e727fed9e762906523621229abc7c05dcf7e
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2024-05-24 15:18:39 +0200

    [fragments] maven reimport
    GitOrigin-RevId: 71e03f90163864de0ef0b8ddeaddabcedcda78bc

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/project/preimport/SimpleStructureProjectVisitor.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/project/preimport/SimpleStructureProjectVisitor.kt
@@ -8,19 +8,6 @@ import java.nio.charset.Charset
 import java.nio.file.Path
 
 class SimpleStructureProjectVisitor : MavenStructureProjectVisitor {
-  override suspend fun readXml(rootProjectFile: Path): Element? {
-    return MavenJDOMUtil.read(rootProjectFile, Charset.defaultCharset(), null)
-  }
-
-  override fun child(aggregatorProjectFile: Path, moduleName: String): Path? {
-    val resolve = aggregatorProjectFile.parent.resolve(moduleName)
-    var file = resolve.toFile()
-    if (file.isDirectory) {
-      file = file.resolve("pom.xml")
-    }
-    if (file.isFile) return file.toPath();
-    return null
-  }
 
   override fun map(allProjects: ArrayList<MavenProject>) {
   }
