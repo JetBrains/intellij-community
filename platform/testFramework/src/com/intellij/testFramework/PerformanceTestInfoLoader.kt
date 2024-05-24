a32dddda1f43e7b2dd4fbc047b115360e1459e6f
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-05-24 14:11:07 +0200

    [perf unit tests] Don't return the same instance of PerformanceTestInfo since instance has state and must be recreated
    For example, assertions like "assert this.setup == null" fail
    
    GitOrigin-RevId: a666f85697a8ec794e58f6cf06d104b2ab2315a0

--- platform/testFramework/src/com/intellij/testFramework/PerformanceTestInfoLoader.kt
+++ platform/testFramework/src/com/intellij/testFramework/PerformanceTestInfoLoader.kt
@@ -2,36 +2,33 @@
 package com.intellij.testFramework
 
 import com.intellij.openapi.diagnostic.logger
-import com.intellij.util.concurrency.SynchronizedClearableLazy
 import java.util.ServiceConfigurationError
 import java.util.ServiceLoader
 
 class PerformanceTestInfoLoader {
   companion object {
-    private val instance: SynchronizedClearableLazy<PerformanceTestInfo> = SynchronizedClearableLazy {
-      val log = logger<PerformanceTestInfo>()
+    val instance: PerformanceTestInfo
+      get() {
+        val log = logger<PerformanceTestInfo>()
 
-      val instance = run {
-        val aClass = PerformanceTestInfo::class.java
-        val implementations = ServiceLoader.load(aClass, aClass.classLoader).toList()
-        if (implementations.isEmpty()) {
-          throw ServiceConfigurationError("No implementations of ${aClass.name} found. Make sure to include intellij.tools.ide.metrics.benchmark module" +
-                                          " containing the implementation in the classpath.")
+        val instance = run {
+          val aClass = PerformanceTestInfo::class.java
+          val implementations = ServiceLoader.load(aClass, aClass.classLoader).toList()
+          if (implementations.isEmpty()) {
+            throw ServiceConfigurationError("No implementations of ${aClass.name} found. Make sure to include intellij.tools.ide.metrics.benchmark module" +
+                                            " containing the implementation in the classpath.")
+          }
+          else if (implementations.size > 1) {
+            throw ServiceConfigurationError("More than one implementation for ${aClass.simpleName} found: ${implementations.map { it::class.qualifiedName }}")
+          }
+          else {
+            implementations.single()
+          }
         }
-        else if (implementations.size > 1) {
-          throw ServiceConfigurationError("More than one implementation for ${aClass.simpleName} found: ${implementations.map { it::class.qualifiedName }}")
-        }
-        else {
-          implementations.single()
-        }
+
+        log.info("Loaded PerformanceTestInfo implementation ${instance::class.java.name}")
+        return instance
       }
-
-      log.info("Loaded PerformanceTestInfo implementation ${instance::class.java.name}")
-      instance
-    }
-
-    fun getInstance(): PerformanceTestInfo = instance.value
   }
-
 }
 
