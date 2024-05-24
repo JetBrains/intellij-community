7237d1629932d1a83dad434dad22771615d4d6fe
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2024-05-24 18:32:38 +0200

    [indexes] PersistentHashMap: don't initialize Wal executor if Wal is disabled
    + creates issues in benchmarks since it starts AppScheduledExecutorService, which is then hard to stop correctly
    
    GitOrigin-RevId: b883b38a8a0a66baa18b1776372442933b8231c4

--- platform/util/src/com/intellij/util/indexing/impl/MapIndexStorage.java
+++ platform/util/src/com/intellij/util/indexing/impl/MapIndexStorage.java
@@ -123,12 +123,16 @@ public class MapIndexStorage<Key, Value> implements IndexStorage<Key, Value>, Me
       creationOptions = creationOptions.setHasNoChunks();
     }
     return creationOptions.with(() -> {
-      return new PersistentMapImpl<>(PersistentMapBuilder
-                                       .newBuilder(getStorageFile(), keyDescriptor, valueContainerExternalizer)
-                                       .withWal(myEnableWal && ENABLE_WAL && !isReadOnly)
-                                       .withWalExecutor(SequentialTaskExecutor.createSequentialApplicationPoolExecutor("Index Wal Pool"))
-                                       .withReadonly(isReadOnly)
-                                       .withCompactOnClose(compactOnClose));
+      PersistentMapBuilder<Key, UpdatableValueContainer<Value>> builder = PersistentMapBuilder
+        .newBuilder(getStorageFile(), keyDescriptor, valueContainerExternalizer)
+        .withReadonly(isReadOnly)
+        .withCompactOnClose(compactOnClose);
+      if (myEnableWal && ENABLE_WAL && !isReadOnly) {
+        builder
+          .withWal(true)
+          .withWalExecutor(SequentialTaskExecutor.createSequentialApplicationPoolExecutor("Index Wal Pool"));
+      }
+      return new PersistentMapImpl<>(builder);
     });
   }
 
