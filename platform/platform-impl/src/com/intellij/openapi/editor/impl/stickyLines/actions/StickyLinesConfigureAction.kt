1d2af7616a00f97c3b53e49f43cdc663a7a35c63
Alexandr Trushev <aleksandr.trushev@jetbrains.com>
2024-05-24 22:55:52 +0200

    IJPL-449 Sticky lines: refactor actions moving to separate package
    GitOrigin-RevId: cb9aae4ec4ce3b2cd4fa2fd5dedd54b735996c5a

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/stickyLines/actions/StickyLinesConfigureAction.kt
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/stickyLines/actions/StickyLinesConfigureAction.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.editor.impl.stickyLines
+package com.intellij.openapi.editor.impl.stickyLines.actions
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 
