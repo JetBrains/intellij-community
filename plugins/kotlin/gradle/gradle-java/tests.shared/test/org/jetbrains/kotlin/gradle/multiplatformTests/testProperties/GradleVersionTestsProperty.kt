123143feabbb1e3b42c9d45909b74c107a5a95a0
Aleksei.Cherepanov <aleksei.cherepanov@jetbrains.com>
2024-05-27 17:17:31 +0000

    [MPP][Tests] Bump AGP and Gradle versions
    ^KTIJ-29642 Fixed
    
    
    Merge-request: IJ-MR-135198
    Merged-by: Aleksei Cherepanov <aleksei.cherepanov@jetbrains.com>
    
    GitOrigin-RevId: 28835d36bfe5c0b75e5f3f0a6042daebf35f75c2

--- plugins/kotlin/gradle/gradle-java/tests.shared/test/org/jetbrains/kotlin/gradle/multiplatformTests/testProperties/GradleVersionTestsProperty.kt
+++ plugins/kotlin/gradle/gradle-java/tests.shared/test/org/jetbrains/kotlin/gradle/multiplatformTests/testProperties/GradleVersionTestsProperty.kt
@@ -8,9 +8,9 @@ object GradleVersionTestsProperty : org.jetbrains.kotlin.gradle.multiplatformTes
 
     enum class Value(val acronym: String, val version: String) {
         ForMinAgp("REQUIRED_FOR_MIN_AGP", "7.5.1"),
-        ForStableAgp("REQUIRED_FOR_STABLE_AGP", "8.0.2"),
-        ForBetaAgp("REQUIRED_FOR_BETA_AGP", "8.0.2"),
-        ForAlphaAgp("REQUIRED_FOR_ALPHA_AGP", "8.1.1")
+        ForStableAgp("REQUIRED_FOR_STABLE_AGP", "8.4"),
+        ForBetaAgp("REQUIRED_FOR_BETA_AGP", "8.7"),
+        ForAlphaAgp("REQUIRED_FOR_ALPHA_AGP", "8.7")
     }
 
     override val valuesByAcronyms: Map<String, String> = Value.values().map { it.acronym to it.version }.toMap()
