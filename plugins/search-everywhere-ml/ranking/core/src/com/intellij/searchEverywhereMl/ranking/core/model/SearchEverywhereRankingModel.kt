27d11d2a2a4ef0602592e2f9ca5282d2cbcab4cf
Samuel Soukup <samuel.soukup@jetbrains.com>
2024-05-27 12:34:49 +0200

    [ML in SE] IJ-CR-135091: Add documentation to the ExactMatchSearchEverywhereRankingModel.
    GitOrigin-RevId: 9e1415b476943893270f33fd8a4944bbd47c6999

--- plugins/search-everywhere-ml/ranking/core/src/com/intellij/searchEverywhereMl/ranking/core/model/SearchEverywhereRankingModel.kt
+++ plugins/search-everywhere-ml/ranking/core/src/com/intellij/searchEverywhereMl/ranking/core/model/SearchEverywhereRankingModel.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.searchEverywhereMl.ranking.core.model
 
 import com.intellij.internal.ml.DecisionFunction
@@ -28,13 +28,24 @@ internal class SimpleSearchEverywhereRankingModel(model: DecisionFunction) : Sea
 internal class ExactMatchSearchEverywhereRankingModel(model: DecisionFunction) : SearchEverywhereRankingModel(model) {
   private val exactMatchKey = "prefixExact"
   private val extensionMatchKey = "fileTypeMatchesQuery"
+  /**
+   * Predict the preference adjusted for exact matches (mainly in Files and Classes tabs)
+   * The value will be:
+   *  - `> 0.99 for exact matches with a matching extension
+   *  - 0.9-0.99 for exact match without the matching extension
+   *  - `< 0.9 for non-exact matches
+   *
+   *  In these categories, the value is ordered by the ML model predicted value
+   */
   override fun predict(features: Map<String, Any?>): Double {
+
     val isExactMatch = features.getOrDefault(exactMatchKey, false) == true
     val extensionMatch = features.getOrDefault(extensionMatchKey, false) == true
     val mlPrediction = model.predict(buildArray(model.featuresOrder, features))
     return if (isExactMatch) {
-      if (extensionMatch) 0.99 + mlPrediction * 0.01 else 0.9 + mlPrediction * 0.09
+      if (extensionMatch) 0.99 + mlPrediction * 0.01 // Name + extension matches -> return preference is > 0.99
+      else 0.9 + mlPrediction * 0.09 // Name matches but the extension doesn't -> return preference is 0.9-0.99.
     }
-    else mlPrediction * 0.9
+    else mlPrediction * 0.9 // No exact match -> return < 0.9
   }
 }
