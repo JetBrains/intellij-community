19b9a4f75a375c52a5f6e55cc6273bf3c426e941
Nikita Biriukov <nikita.biriukov@jetbrains.com>
2024-05-27 16:43:04 +0200

    [gradle][kotlin] IDEA-316453 add tests for navigation from Version catalog usage in `build.gradle.kts`to its declaration
    GitOrigin-RevId: fa271140c2b0a74a6a57c8c29245c97aac3ee53c

---
+++ plugins/kotlin/gradle/gradle-java/tests.k2/test/org/jetbrains/kotlin/idea/k2/codeInsight/gradle/versionCatalog/KotlinGradleVersionCatalogNavigationTest.kt
@@ -0,0 +1,41 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.k2.codeInsight.gradle.versionCatalog
+
+import com.intellij.openapi.actionSystem.IdeActions
+import com.intellij.testFramework.TestDataPath
+import org.jetbrains.kotlin.idea.base.test.TestRoot
+import org.jetbrains.kotlin.idea.k2.codeInsight.gradle.AbstractGradleCodeInsightTest
+import org.jetbrains.kotlin.idea.test.JUnit3RunnerWithInners
+import org.jetbrains.kotlin.test.TestMetadata
+import org.junit.runner.RunWith
+
+/**
+ * @see org.jetbrains.kotlin.idea.gradleCodeInsightCommon.versionCatalog.KotlinGradleVersionCatalogGotoDeclarationHandler
+ */
+@TestRoot("gradle/gradle-java/tests.k2")
+@RunWith(JUnit3RunnerWithInners::class)
+@TestDataPath("\$CONTENT_ROOT")
+@TestMetadata("../../../idea/tests/testData/gradle/versionCatalog/navigation")
+class KotlinGradleVersionCatalogNavigationTest : AbstractGradleCodeInsightTest() {
+
+    @TestMetadata("fromVersionUsageToItsDeclarationInToml.test")
+    fun testVersionCatalogFromVersionUsageToItsDeclarationInToml() {
+        verifyNavigationFromCaretToExpected()
+    }
+
+    @TestMetadata("fromLibraryUsageToItsDeclarationInToml.test")
+    fun testVersionCatalogFromLibraryUsageToItsDeclarationInToml() {
+        verifyNavigationFromCaretToExpected()
+    }
+
+    @TestMetadata("fromLibraryUsageWithGetToItsDeclarationInToml.test")
+    fun testVersionCatalogFromLibraryUsageWithGetToItsDeclarationInToml() {
+        verifyNavigationFromCaretToExpected()
+    }
+
+    private fun verifyNavigationFromCaretToExpected() {
+        fixture.performEditorAction(IdeActions.ACTION_GOTO_DECLARATION)
+        val text = document.text
+        assertTrue("Actual text:\n\n$text", text.startsWith("# EXPECTED"))
+    }
+}
