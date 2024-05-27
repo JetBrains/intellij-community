1b1a4bd64da24f44b38cbf7d22dd7e305f2637db
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2024-05-27 16:29:50 +0200

    [completion] ML-3270: rename inline completion configurable package
    GitOrigin-RevId: 2973e4902f44b7489beab11ab077b01336e29499

--- platform/platform-impl/src/com/intellij/codeInsight/inline/completion/options/InlineCompletionConfigurable.kt
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/options/InlineCompletionConfigurable.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.codeInsight.inline.completion.settings
+package com.intellij.codeInsight.inline.completion.options
 
 import com.intellij.application.options.editor.EditorOptionsProvider
 import com.intellij.openapi.application.ApplicationBundle
