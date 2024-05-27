552381c09f9f7a1dcdc288f3c77041071563802e
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2024-05-28 01:47:53 +0400

    [kotlin] Introduce `RemovePartsFromPropertyUtils`
    to share some functions between the K1 and K2 implementations of RemovePartsFromPropertyFix.
    
    ^KTIJ-29526
    
    GitOrigin-RevId: b4bce3a577dd291f6b01d60ddfc3e3df5730ad8e

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/RemovePartsFromPropertyFix.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/RemovePartsFromPropertyFix.kt
@@ -7,9 +7,8 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.diagnostics.Errors
-import org.jetbrains.kotlin.idea.base.compilerPreferences.KotlinBaseCompilerConfigurationUiBundle
-import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.codeinsight.api.classic.quickfixes.KotlinQuickFixAction
+import org.jetbrains.kotlin.idea.codeinsight.utils.RemovePartsFromPropertyUtils
 import org.jetbrains.kotlin.idea.intentions.SpecifyTypeExplicitlyIntention
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtProperty
@@ -20,28 +19,9 @@ open class RemovePartsFromPropertyFix(
     private val removeGetter: Boolean,
     private val removeSetter: Boolean
 ) : KotlinQuickFixAction<KtProperty>(element) {
-    override fun getText(): String {
-        val chunks = ArrayList<String>(3).apply {
-            if (removeGetter) add(KotlinBundle.message("text.getter"))
-            if (removeSetter) add(KotlinBundle.message("text.setter"))
-            if (removeInitializer) add(KotlinBundle.message("text.initializer"))
-        }
+    override fun getText(): String = RemovePartsFromPropertyUtils.getActionName(removeInitializer, removeGetter, removeSetter)
 
-        fun concat(head: String, tail: String): String {
-            return head + " " + KotlinBaseCompilerConfigurationUiBundle.message("configuration.text.and") + " " + tail
-        }
-
-        val partsText = when (chunks.size) {
-            0 -> ""
-            1 -> chunks.single()
-            2 -> concat(chunks[0], chunks[1])
-            else -> concat(chunks.dropLast(1).joinToString(", "), chunks.last())
-        }
-
-        return KotlinBundle.message("remove.0.from.property", partsText)
-    }
-
-    override fun getFamilyName(): String = KotlinBundle.message("remove.parts.from.property")
+    override fun getFamilyName(): String = RemovePartsFromPropertyUtils.getFamilyName()
 
     public override fun invoke(project: Project, editor: Editor?, file: KtFile) {
         val element = this.element ?: return
