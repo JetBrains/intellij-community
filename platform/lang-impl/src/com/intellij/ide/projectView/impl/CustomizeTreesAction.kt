30045e3c55880a9a3b77dfc9b7f781f0fe4c45d2
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2024-05-28 11:56:15 +0300

    GTW-8813 [RDCT] Fix CustomizeTreesAction remote behavior spec
    It obviously must be the same as ShowSettingsAction.
    
    GitOrigin-RevId: 0228e06d17ad658fa42ae8abb27a9b9e307fd138

--- platform/lang-impl/src/com/intellij/ide/projectView/impl/CustomizeTreesAction.kt
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/CustomizeTreesAction.kt
@@ -5,11 +5,12 @@ import com.intellij.ide.IdeBundle.message
 import com.intellij.ide.actions.ShowSettingsUtilImpl
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification
 import com.intellij.openapi.project.DumbAwareAction
 import org.jetbrains.annotations.ApiStatus.Internal
 
 @Internal
-class CustomizeTreesAction : DumbAwareAction() {
+class CustomizeTreesAction : DumbAwareAction(), ActionRemoteBehaviorSpecification.Frontend {
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 
   override fun actionPerformed(e: AnActionEvent) {
