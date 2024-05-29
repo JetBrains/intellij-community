382a5509fee878ad8f0a448e4f63d3d6fa8a0ea2
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2024-05-30 00:42:03 +0200

    [vcs-log] use an inner class for keeping painting parameters
    GitOrigin-RevId: b7df0011a002002dfc07d27b08558d1f06402e1f

--- platform/vcs-log/impl/src/com/intellij/vcs/log/paint/SimpleGraphCellPainter.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/paint/SimpleGraphCellPainter.kt
@@ -23,23 +23,24 @@ internal open class SimpleGraphCellPainter(private val colorGenerator: ColorGene
   override fun paint(g2: Graphics2D, printElements: Collection<PrintElement>) {
     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
 
+    val painter = MyPainter(rowHeight)
     val selected: MutableList<PrintElement> = SmartList()
     for (printElement in printElements) {
       if (printElement.isSelected) {
         selected.add(printElement) // to draw later
       }
       else {
-        paintElement(g2, printElement, colorGenerator.getColor(printElement.colorId), false)
+        painter.paintElement(g2, printElement, colorGenerator.getColor(printElement.colorId), false)
       }
     }
 
     // draw selected elements
     for (printElement in selected) {
-      paintElement(g2, printElement, MARK_COLOR, true)
+      painter.paintElement(g2, printElement, MARK_COLOR, true)
     }
 
     for (printElement in selected) {
-      paintElement(g2, printElement, colorGenerator.getColor(printElement.colorId), false)
+      painter.paintElement(g2, printElement, colorGenerator.getColor(printElement.colorId), false)
     }
   }
 
@@ -80,88 +81,93 @@ internal open class SimpleGraphCellPainter(private val colorGenerator: ColorGene
     return distance(x0, y0, x, y) <= circleRadius
   }
 
-  private val ordinaryStroke: BasicStroke
-    get() = BasicStroke(PaintParameters.getLineThickness(rowHeight), BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL)
+  private class MyPainter(private val rowHeight: Int) {
+    private val elementWidth = PaintParameters.getElementWidth(rowHeight)
 
-  private val selectedStroke: BasicStroke
-    get() = BasicStroke(PaintParameters.getSelectedLineThickness(rowHeight), BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL)
+    private val circleRadius = PaintParameters.getCircleRadius(rowHeight)
+    private val lineThickness = PaintParameters.getLineThickness(rowHeight)
 
-  private fun getDashedStroke(dash: FloatArray): Stroke {
-    return BasicStroke(PaintParameters.getLineThickness(rowHeight), BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL, 0f, dash,
-                       dash[0] / 2)
-  }
+    private val selectedCircleRadius = PaintParameters.getSelectedCircleRadius(rowHeight)
+    private val selectedLineThickness = PaintParameters.getSelectedLineThickness(rowHeight)
 
-  private fun getSelectedDashedStroke(dash: FloatArray): Stroke {
-    return BasicStroke(PaintParameters.getSelectedLineThickness(rowHeight), BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL, 0f, dash,
-                       dash[0] / 2)
-  }
+    private val ordinaryStroke = BasicStroke(lineThickness, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL)
+    private val selectedStroke = BasicStroke(selectedLineThickness, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL)
 
-  private fun paintLine(g2: Graphics2D, x1: Int, y1: Int, x2: Int, y2: Int, startArrowX: Int, startArrowY: Int,
-                        hasArrow: Boolean, isUsual: Boolean, isSelected: Boolean) {
-    g2.stroke = if (isUsual || hasArrow) {
-      if (isSelected) selectedStroke else ordinaryStroke
-    }
-    else {
-      val edgeLength = if (x1 == x2) rowHeight.toDouble() else hypot((x1 - x2).toDouble(), (y1 - y2).toDouble())
-      val dashLength = getDashLength(edgeLength, rowHeight)
-      if (isSelected) getSelectedDashedStroke(dashLength) else getDashedStroke(dashLength)
+    private fun getDashedStroke(dash: FloatArray): Stroke {
+      return BasicStroke(lineThickness, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL, 0f, dash,
+                         dash[0] / 2)
     }
 
-    g2.drawLine(x1, y1, x2, y2)
-    if (hasArrow) {
-      val (endArrowX1, endArrowY1) = rotate(x1.toDouble(), y1.toDouble(), startArrowX.toDouble(), startArrowY.toDouble(),
-                                            sqrt(ARROW_ANGLE_COS2), sqrt(1 - ARROW_ANGLE_COS2),
-                                            ARROW_LENGTH * rowHeight)
-      val (endArrowX2, endArrowY2) = rotate(x1.toDouble(), y1.toDouble(), startArrowX.toDouble(), startArrowY.toDouble(),
-                                            sqrt(ARROW_ANGLE_COS2), -sqrt(1 - ARROW_ANGLE_COS2),
-                                            ARROW_LENGTH * rowHeight)
-      g2.drawLine(startArrowX, startArrowY, endArrowX1, endArrowY1)
-      g2.drawLine(startArrowX, startArrowY, endArrowX2, endArrowY2)
+    private fun getSelectedDashedStroke(dash: FloatArray): Stroke {
+      return BasicStroke(selectedLineThickness, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL, 0f, dash,
+                         dash[0] / 2)
     }
-  }
 
-  private fun paintEdge(g2: Graphics2D, edge: EdgePrintElement, isSelected: Boolean) {
-    val isDown = edge.type == EdgePrintElement.Type.DOWN
-    val isUsual = edge.lineStyle == EdgePrintElement.LineStyle.SOLID
-    val hasArrow = edge.hasArrow()
-    val isTerminal = edge is TerminalEdgePrintElement
+    private fun paintLine(g2: Graphics2D, x1: Int, y1: Int, x2: Int, y2: Int, startArrowX: Int, startArrowY: Int,
+                          hasArrow: Boolean, isUsual: Boolean, isSelected: Boolean) {
+      g2.stroke = if (isUsual || hasArrow) {
+        if (isSelected) selectedStroke else ordinaryStroke
+      }
+      else {
+        val edgeLength = if (x1 == x2) rowHeight.toDouble() else hypot((x1 - x2).toDouble(), (y1 - y2).toDouble())
+        val dashLength = getDashLength(edgeLength, rowHeight)
+        if (isSelected) getSelectedDashedStroke(dashLength) else getDashedStroke(dashLength)
+      }
 
-    val from = edge.positionInCurrentRow
-    val to = edge.positionInOtherRow
-
-    val elementWidth = PaintParameters.getElementWidth(rowHeight)
-    val x1 = elementWidth * from + elementWidth / 2
-    val y1 = rowHeight / 2
-    if (from == to) {
-      val arrowGap = if (isTerminal) PaintParameters.getCircleRadius(rowHeight) / 2 + 1 else 0
-      val y2 = if (isDown) rowHeight - arrowGap else arrowGap
-      paintLine(g2, x1, y1, x1, y2, x1, y2, hasArrow, isUsual, isSelected)
+      g2.drawLine(x1, y1, x2, y2)
+      if (hasArrow) {
+        val (endArrowX1, endArrowY1) = rotate(x1.toDouble(), y1.toDouble(), startArrowX.toDouble(), startArrowY.toDouble(),
+                                              sqrt(ARROW_ANGLE_COS2), sqrt(1 - ARROW_ANGLE_COS2),
+                                              ARROW_LENGTH * rowHeight)
+        val (endArrowX2, endArrowY2) = rotate(x1.toDouble(), y1.toDouble(), startArrowX.toDouble(), startArrowY.toDouble(),
+                                              sqrt(ARROW_ANGLE_COS2), -sqrt(1 - ARROW_ANGLE_COS2),
+                                              ARROW_LENGTH * rowHeight)
+        g2.drawLine(startArrowX, startArrowY, endArrowX1, endArrowY1)
+        g2.drawLine(startArrowX, startArrowY, endArrowX2, endArrowY2)
+      }
     }
-    else {
-      assert(!isTerminal)
-      // paint non-vertical lines twice the size to make them dock with each other well
-      val x2 = elementWidth * to + elementWidth / 2
-      val y2 = if (isDown) rowHeight + rowHeight / 2 else -rowHeight / 2
-      paintLine(g2, x1, y1, x2, y2, (x1 + x2) / 2, (y1 + y2) / 2, hasArrow, isUsual, isSelected)
+
+    private fun paintEdge(g2: Graphics2D, edge: EdgePrintElement, isSelected: Boolean) {
+      val isDown = edge.type == EdgePrintElement.Type.DOWN
+      val isUsual = edge.lineStyle == EdgePrintElement.LineStyle.SOLID
+      val hasArrow = edge.hasArrow()
+      val isTerminal = edge is TerminalEdgePrintElement
+
+      val from = edge.positionInCurrentRow
+      val to = edge.positionInOtherRow
+
+      val elementWidth = PaintParameters.getElementWidth(rowHeight)
+      val x1 = elementWidth * from + elementWidth / 2
+      val y1 = rowHeight / 2
+      if (from == to) {
+        val arrowGap = if (isTerminal) PaintParameters.getCircleRadius(rowHeight) / 2 + 1 else 0
+        val y2 = if (isDown) rowHeight - arrowGap else arrowGap
+        paintLine(g2, x1, y1, x1, y2, x1, y2, hasArrow, isUsual, isSelected)
+      }
+      else {
+        assert(!isTerminal)
+        // paint non-vertical lines twice the size to make them dock with each other well
+        val x2 = elementWidth * to + elementWidth / 2
+        val y2 = if (isDown) rowHeight + rowHeight / 2 else -rowHeight / 2
+        paintLine(g2, x1, y1, x2, y2, (x1 + x2) / 2, (y1 + y2) / 2, hasArrow, isUsual, isSelected)
+      }
     }
-  }
 
-  private fun paintCircle(g2: Graphics2D, position: Int, isSelected: Boolean) {
-    val elementWidth = PaintParameters.getElementWidth(rowHeight)
+    private fun paintCircle(g2: Graphics2D, position: Int, isSelected: Boolean) {
+      val x0 = elementWidth * position + elementWidth / 2
+      val y0 = rowHeight / 2
+      val r = if (isSelected) selectedCircleRadius else circleRadius
 
-    val x0 = elementWidth * position + elementWidth / 2
-    val y0 = rowHeight / 2
-    val r = if (isSelected) PaintParameters.getSelectedCircleRadius(rowHeight) else PaintParameters.getCircleRadius(rowHeight)
+      val circle = Ellipse2D.Double(x0 - r + 0.5, y0 - r + 0.5, (2 * r).toDouble(), (2 * r).toDouble())
+      g2.fill(circle)
+    }
 
-    val circle = Ellipse2D.Double(x0 - r + 0.5, y0 - r + 0.5, (2 * r).toDouble(), (2 * r).toDouble())
-    g2.fill(circle)
-  }
-
-  private fun paintElement(g2: Graphics2D, element: PrintElement, color: Color, isSelected: Boolean) {
-    g2.color = color
-    when (element) {
-      is EdgePrintElement -> paintEdge(g2, element, isSelected)
-      is NodePrintElement -> paintCircle(g2, element.positionInCurrentRow, isSelected)
+    fun paintElement(g2: Graphics2D, element: PrintElement, color: Color, isSelected: Boolean) {
+      g2.color = color
+      when (element) {
+        is EdgePrintElement -> paintEdge(g2, element, isSelected)
+        is NodePrintElement -> paintCircle(g2, element.positionInCurrentRow, isSelected)
+      }
     }
   }
 
