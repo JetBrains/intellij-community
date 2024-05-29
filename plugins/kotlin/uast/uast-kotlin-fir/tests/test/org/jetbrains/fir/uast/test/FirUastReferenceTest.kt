8bb0d127e20ce7b19a1d040953ba7d9ad40d99bf
Jinseong Jeon <jsjeon@google.com>
2024-05-28 22:26:27 -0700

    KT UAST: commonize UAST reference test
    ^KTIJ-30097
    
    GitOrigin-RevId: bf1a69c6e8eafcbd0304bd5569ea7efb1ff14404

---
+++ plugins/kotlin/uast/uast-kotlin-fir/tests/test/org/jetbrains/fir/uast/test/FirUastReferenceTest.kt
@@ -0,0 +1,19 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.fir.uast.test
+
+import com.intellij.testFramework.LightProjectDescriptor
+import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
+import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
+import org.jetbrains.uast.test.common.kotlin.UastReferenceTestBase
+
+class FirUastReferenceTest : UastReferenceTestBase, KotlinLightCodeInsightFixtureTestCase() {
+    override val isFirUastPlugin: Boolean = true
+
+    override fun isFirPlugin(): Boolean = true
+
+    override fun getProjectDescriptor(): LightProjectDescriptor = KotlinWithJdkAndRuntimeLightProjectDescriptor.getInstance()
+
+    fun `test original getter is visible when reference is under renaming`() {
+        `check original getter is visible when reference is under renaming`(myFixture)
+    }
+}
