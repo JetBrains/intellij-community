e765c8f2189786e2ac5b02f34a1171b1f6da4162
Jinseong Jeon <jsjeon@google.com>
2024-05-28 22:26:27 -0700

    KT UAST: commonize UAST reference test
    ^KTIJ-30097

---
+++ plugins/kotlin/uast/uast-kotlin-base/tests/test/org/jetbrains/uast/test/common/kotlin/UastReferenceTestBase.kt
@@ -0,0 +1,91 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.uast.test.common.kotlin
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.extensions.DefaultPluginDescriptor
+import com.intellij.openapi.util.Disposer
+import com.intellij.patterns.uast.injectionHostUExpression
+import com.intellij.psi.*
+import com.intellij.psi.impl.source.resolve.reference.PsiReferenceContributorEP
+import com.intellij.psi.util.PropertyUtil
+import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture
+import org.jetbrains.uast.UExpression
+import org.jetbrains.uast.evaluateString
+import org.jetbrains.uast.toUElementOfType
+import kotlin.test.fail
+
+interface UastReferenceTestBase : UastPluginSelection {
+    fun `check original getter is visible when reference is under renaming`(myFixture: JavaCodeInsightTestFixture) {
+        class GetterReference(
+            val className: String,
+            psiElement: PsiElement,
+        ) : PsiReferenceBase<PsiElement>(psiElement) {
+            override fun resolve(): PsiMethod? {
+                val psiClass = JavaPsiFacade.getInstance(element.project).findClass(className, element.resolveScope) ?: return null
+                val name = element.toUElementOfType<UExpression>()?.evaluateString() ?: return null
+                return PropertyUtil.getGetters(psiClass, name).firstOrNull()
+            }
+
+            override fun handleElementRename(newElementName: String): PsiElement {
+                val resolve = resolve()
+                    ?: fail("can't resolve during rename, looks like someone renamed or removed the source element before updating references")
+
+                val newName =
+                    if (PropertyUtil.getPropertyName(resolve) != null)
+                        PropertyUtil.getPropertyName(newElementName) ?: newElementName
+                    else newElementName
+
+                return super.handleElementRename(newName)
+            }
+
+            override fun getVariants(): Array<Any> = emptyArray()
+        }
+
+        class MockPsiReferenceContributor : PsiReferenceContributor() {
+            override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
+                registrar.registerUastReferenceProvider(
+                    injectionHostUExpression(),
+                    uastInjectionHostReferenceProvider { _, psiLanguageInjectionHost ->
+                        arrayOf(GetterReference("KotlinBean", psiLanguageInjectionHost))
+                    })
+            }
+        }
+
+        val myDisposable = Disposer.newDisposable("MockPsiReferenceContributor")
+        try {
+            registerReferenceContributor(myDisposable, MockPsiReferenceContributor::class.java)
+
+            myFixture.configureByText(
+                "KotlinBean.kt", """
+                data class KotlinBean(val myF<caret>ield: String)
+
+                val reference = "myField"
+
+                """.trimIndent()
+            )
+
+            myFixture.renameElementAtCaret("myRenamedField")
+
+            myFixture.checkResult(
+                """
+                data class KotlinBean(val myRenamedField: String)
+
+                val reference = "myRenamedField"
+
+                """.trimIndent()
+            )
+        } finally {
+            Disposer.dispose(myDisposable)
+        }
+    }
+
+    private fun registerReferenceContributor(disposable: Disposable, clazz: Class<out PsiReferenceContributor>) {
+        PsiReferenceContributor.EP_NAME.point.registerExtension(
+            PsiReferenceContributorEP().apply {
+                implementationClass = clazz.name
+                pluginDescriptor = DefaultPluginDescriptor("kotlin-uast-test")
+            },
+            disposable,
+        )
+    }
+}
--- plugins/kotlin/uast/uast-kotlin-fir/tests/test/org/jetbrains/fir/uast/test/FirUastReferenceTest.kt
+++ plugins/kotlin/uast/uast-kotlin-fir/tests/test/org/jetbrains/fir/uast/test/FirUastReferenceTest.kt
@@ -1,22 +1,18 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
-package org.jetbrains.uast.test.kotlin
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.fir.uast.test
 
 import com.intellij.testFramework.LightProjectDescriptor
 import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
 import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
 import org.jetbrains.uast.test.common.kotlin.UastReferenceTestBase
-import org.junit.Test
-import org.junit.internal.runners.JUnit38ClassRunner
-import org.junit.runner.RunWith
 
-@RunWith(JUnit38ClassRunner::class)
-class KotlinUastReferencesTest : UastReferenceTestBase, KotlinLightCodeInsightFixtureTestCase() {
-    override val isFirUastPlugin: Boolean = false
+class FirUastReferenceTest : UastReferenceTestBase, KotlinLightCodeInsightFixtureTestCase() {
+    override val isFirUastPlugin: Boolean = true
+
+    override fun isFirPlugin(): Boolean = true
 
     override fun getProjectDescriptor(): LightProjectDescriptor = KotlinWithJdkAndRuntimeLightProjectDescriptor.getInstance()
 
-    @Test
     fun `test original getter is visible when reference is under renaming`() {
         `check original getter is visible when reference is under renaming`(myFixture)
     }
