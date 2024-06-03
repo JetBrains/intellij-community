95dee461414b8c1acdfa7016e8ecaea4fe784f06
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2024-06-03 19:03:54 +0200

    IDEA-354428 [Recovery] fix: don't show project frame chooser in the recovery actions that reopen projects
    GitOrigin-RevId: 6567b531e0d7fa4c2b6895a38ff37c94d133bae9

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/ExternalProjectsDataRecoveryAction.kt
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/ExternalProjectsDataRecoveryAction.kt
@@ -1,38 +1,25 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.externalSystem.service.project.manage
 
-import com.intellij.ide.actions.cache.*
-import com.intellij.ide.impl.OpenProjectTask
-import com.intellij.ide.impl.ProjectUtil
-import com.intellij.ide.impl.ProjectUtilCore
-import com.intellij.openapi.application.EDT
-import com.intellij.openapi.components.Service
-import com.intellij.openapi.components.service
+import com.intellij.ide.actions.cache.AsyncRecoveryResult
+import com.intellij.ide.actions.cache.RecoveryScope
+import com.intellij.ide.actions.cache.ReopenProjectRecoveryAction
 import com.intellij.openapi.externalSystem.settings.workspaceModel.ExternalProjectsBuildClasspathEntity
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.externalSystem.util.ExternalSystemBundle
 import com.intellij.openapi.externalSystem.util.ExternalSystemUtil
 import com.intellij.openapi.progress.blockingContext
 import com.intellij.openapi.progress.blockingContextScope
-import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.util.io.NioFiles
 import com.intellij.platform.backend.workspace.workspaceModel
 import com.intellij.platform.workspace.storage.entities
-import com.intellij.util.application
 import com.intellij.workspaceModel.ide.impl.WorkspaceModelCacheImpl
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.future.asCompletableFuture
 import kotlinx.coroutines.withContext
 import org.jetbrains.annotations.ApiStatus
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
-import java.util.concurrent.CompletableFuture
 
 @ApiStatus.Internal
-class ExternalProjectsDataRecoveryAction : RecoveryAction {
+class ExternalProjectsDataRecoveryAction : ReopenProjectRecoveryAction() {
 
   override val performanceRate: Int
     get() = 0
@@ -43,32 +30,24 @@ class ExternalProjectsDataRecoveryAction : RecoveryAction {
   override val actionKey: String
     get() = "ExternalSystem.RecoveryAction"
 
-  override fun canBeApplied(recoveryScope: RecoveryScope): Boolean {
-    return recoveryScope is ProjectRecoveryScope && recoveryScope.project.basePath?.let {
-      Files.isDirectory(Paths.get(it))
-    } == true
-  }
+  override suspend fun performAsync(recoveryScope: RecoveryScope): AsyncRecoveryResult {
+    invalidateLocalExternalSystemCache(recoveryScope)
+    invalidateProjectBuildClasspathCache(recoveryScope)
+    invalidateExternalSystemDataStorage(recoveryScope)
+    invalidateExternalSystemToolwindow(recoveryScope)
+    WorkspaceModelCacheImpl.invalidateCaches()
 
-  override fun perform(recoveryScope: RecoveryScope): CompletableFuture<AsyncRecoveryResult> {
-    return CoroutineScopeService.coroutineScope().async {
-      invalidateLocalExternalSystemCache(recoveryScope)
-      invalidateProjectBuildClasspathCache(recoveryScope)
-      invalidateExternalSystemDataStorage(recoveryScope)
-      invalidateExternalSystemToolwindow(recoveryScope)
-      WorkspaceModelCacheImpl.invalidateCaches()
+    val externalSystemCache = ExternalProjectsDataStorage.getProjectConfigurationDir(recoveryScope.project)
 
-      val externalSystemCache = ExternalProjectsDataStorage.getProjectConfigurationDir(recoveryScope.project)
+    val projectPath = closeProject(recoveryScope)
 
-      val projectPath = closeProject(recoveryScope)
+    withContext(Dispatchers.IO) {
+      NioFiles.deleteRecursively(externalSystemCache)
+    }
 
-      withContext(Dispatchers.IO) {
-        NioFiles.deleteRecursively(externalSystemCache)
-      }
+    val newRecoveryScope = openProject(projectPath)
 
-      val newRecoveryScope = openProject(projectPath)
-
-      AsyncRecoveryResult(newRecoveryScope, emptyList())
-    }.asCompletableFuture()
+    return AsyncRecoveryResult(newRecoveryScope, emptyList())
   }
 
   private suspend fun invalidateLocalExternalSystemCache(recoveryScope: RecoveryScope) {
@@ -109,38 +88,6 @@ class ExternalProjectsDataRecoveryAction : RecoveryAction {
       for (manager in ExternalSystemApiUtil.getAllManagers()) {
         val systemId = manager.getSystemId()
         ExternalSystemUtil.scheduleExternalViewStructureUpdate(project, systemId)
-      }
-    }
-  }
-
-  private suspend fun closeProject(recoveryScope: RecoveryScope): Path {
-    val projectPath = Path.of(recoveryScope.project.basePath!!)
-    withContext(Dispatchers.EDT) {
-      blockingContext {
-        val projectManager = ProjectManager.getInstance()
-        projectManager.closeAndDispose(recoveryScope.project)
-      }
-    }
-    return projectPath
-  }
-
-  private suspend fun openProject(projectPath: Path): RecoveryScope {
-    val project = ProjectUtil.openOrImportAsync(
-      file = projectPath,
-      options = OpenProjectTask {
-        runConfigurators = true
-        isNewProject = !ProjectUtilCore.isValidProjectPath(projectPath)
-        useDefaultProjectAsTemplate = true
-      }
-    )!!
-    return ProjectRecoveryScope(project)
-  }
-
-  @Service(Service.Level.APP)
-  private class CoroutineScopeService(val coroutineScope: CoroutineScope) {
-    companion object {
-      fun coroutineScope(): CoroutineScope {
-        return application.service<CoroutineScopeService>().coroutineScope
       }
     }
   }
