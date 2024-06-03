e5d1cd9095b5e6df1ded42efe2aa667f26566c91
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2024-06-03 15:59:13 +0200

    [Kotlin] Upgrade Kotlin compose wizard version to use Kotlin 2.0.0
    KTIJ-30010
    
    (cherry picked from commit 3f6a66042d8786b198127d6aeb72b24c25421131)
    
    GitOrigin-RevId: 80f443ceeb8ba5f98a70aad1a205d2950d1e3af3

--- plugins/kotlin/project-wizard/compose/testData/etalons/desktop/settings.gradle.kts
+++ plugins/kotlin/project-wizard/compose/testData/etalons/desktop/settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
     plugins {
         kotlin("jvm").version(extra["kotlin.version"] as String)
         id("org.jetbrains.compose").version(extra["compose.version"] as String)
+        id("org.jetbrains.kotlin.plugin.compose").version(extra["kotlin.version"] as String)
     }
 }
 
