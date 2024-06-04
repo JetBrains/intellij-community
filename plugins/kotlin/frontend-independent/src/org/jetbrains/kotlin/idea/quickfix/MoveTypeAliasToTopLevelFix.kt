317cfcaba1f975210f79185f0aced7625227e618
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2024-06-04 22:55:21 +0400

    [kotlin] Convert MoveTypeAliasToTopLevelFix to ModCommandAction
    ^KTIJ-29516
    
    GitOrigin-RevId: e3b9a11d666796284783aed0e8892d17d7b88d72

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/MoveTypeAliasToTopLevelFix.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/MoveTypeAliasToTopLevelFix.kt
@@ -1,27 +1,32 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.quickfix
 
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
+import com.intellij.modcommand.ActionContext
+import com.intellij.modcommand.ModPsiUpdater
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.quickfixes.KotlinQuickFixAction
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.KotlinPsiUpdateModCommandAction
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtPsiFactory
 import org.jetbrains.kotlin.psi.KtTypeAlias
 import org.jetbrains.kotlin.psi.psiUtil.parents
 
-class MoveTypeAliasToTopLevelFix(element: KtTypeAlias) : KotlinQuickFixAction<KtTypeAlias>(element) {
-    override fun getText() = KotlinBundle.message("fix.move.typealias.to.top.level")
+class MoveTypeAliasToTopLevelFix(
+    element: KtTypeAlias,
+) : KotlinPsiUpdateModCommandAction.ElementBased<KtTypeAlias, Unit>(element, Unit) {
 
-    override fun getFamilyName() = text
+    override fun getFamilyName() = KotlinBundle.message("fix.move.typealias.to.top.level")
 
-    override fun invoke(project: Project, editor: Editor?, file: KtFile) {
-        val typeAlias = element ?: return
-        val parents = typeAlias.parents.toList().reversed()
+    override fun invoke(
+        actionContext: ActionContext,
+        element: KtTypeAlias,
+        elementContext: Unit,
+        updater: ModPsiUpdater,
+    ) {
+        val parents = element.parents.toList().reversed()
         val containingFile = parents.firstOrNull() as? KtFile ?: return
         val target = parents.getOrNull(1) ?: return
-        containingFile.addAfter(typeAlias, target)
-        containingFile.addAfter(KtPsiFactory(project).createNewLine(2), target)
-        typeAlias.delete()
+        containingFile.addAfter(element, target)
+        containingFile.addAfter(KtPsiFactory(actionContext.project).createNewLine(2), target)
+        element.delete()
     }
 }
--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/MoveTypeAliasToTopLevelFix.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/MoveTypeAliasToTopLevelFix.kt
@@ -2,9 +2,14 @@
 
 package org.jetbrains.kotlin.idea.quickfix
 
+import com.intellij.codeInsight.intention.IntentionAction
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.psi.KtTypeAlias
 
 internal object MoveTypeAliasToTopLevelFixFactory : KotlinSingleIntentionActionFactory() {
-    override fun createAction(diagnostic: Diagnostic) = (diagnostic.psiElement as? KtTypeAlias)?.let { MoveTypeAliasToTopLevelFix(it) }
+    override fun createAction(diagnostic: Diagnostic): IntentionAction? {
+        return (diagnostic.psiElement as? KtTypeAlias)
+            ?.let { MoveTypeAliasToTopLevelFix(it) }
+            ?.asIntention()
+    }
 }
