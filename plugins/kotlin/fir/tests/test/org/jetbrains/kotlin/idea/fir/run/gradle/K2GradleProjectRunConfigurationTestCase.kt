6cc7d0b6fcda42cf7a252a3560ed679812f9a409
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2024-06-04 15:12:00 +0200

    [kotlin] KTIJ-30181 package fixed
    test has not been found on the buildserver, the package doesn't match the corresponding rule (see MultiplatformTestsComposite)
    
    GitOrigin-RevId: 9f1c9187b3f3168ed8e1792d1ccb6665311bfe54

--- plugins/kotlin/fir/tests/test/org/jetbrains/kotlin/idea/fir/run/gradle/K2GradleProjectRunConfigurationTestCase.kt
+++ plugins/kotlin/fir/tests/test/org/jetbrains/kotlin/idea/fir/run/gradle/K2GradleProjectRunConfigurationTestCase.kt
@@ -1,7 +1,8 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.run.gradle
+package org.jetbrains.kotlin.idea.fir.run.gradle
 
 import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginMode
+import org.jetbrains.kotlin.idea.run.gradle.KotlinGradleProjectRunConfigurationTestCase
 
 class K2GradleProjectRunConfigurationTestCase : KotlinGradleProjectRunConfigurationTestCase() {
 
