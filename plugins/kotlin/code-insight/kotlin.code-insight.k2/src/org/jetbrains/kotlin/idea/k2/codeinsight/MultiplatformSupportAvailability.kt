892caf001e6f24f9e7d7b7dab48bfb3b2d79fb9f
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2024-06-04 13:50:48 +0200

    [kotlin] K2: Add notification for WASM targets that they are not supported
    #KTIJ-30155 Fixed
    
    GitOrigin-RevId: ab5a2319b11b1e20f6b71f5961e17595940644a1

--- plugins/kotlin/code-insight/kotlin.code-insight.k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/MultiplatformSupportAvailability.kt
+++ plugins/kotlin/code-insight/kotlin.code-insight.k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/MultiplatformSupportAvailability.kt
@@ -2,52 +2,19 @@
 package org.jetbrains.kotlin.idea.k2.codeinsight
 
 import com.intellij.openapi.util.registry.Registry
-import com.intellij.psi.util.parents
 import org.jetbrains.annotations.Nls
-import org.jetbrains.kotlin.idea.base.facet.JvmOnlyProjectChecker
-import org.jetbrains.kotlin.idea.base.facet.platform.platform
 import org.jetbrains.kotlin.idea.codeInsight.KotlinCodeInsightBundle
-import org.jetbrains.kotlin.idea.codeinsight.utils.KotlinSupportAvailability
-import org.jetbrains.kotlin.idea.util.application.isUnitTestMode
+import org.jetbrains.kotlin.platform.TargetPlatform
 import org.jetbrains.kotlin.platform.isCommon
 import org.jetbrains.kotlin.platform.jvm.isJvm
-import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.psi.psiUtil.hasActualModifier
-import org.jetbrains.kotlin.psi.psiUtil.hasExpectModifier
 
-internal class MultiplatformSupportAvailability : KotlinSupportAvailability {
+internal class MultiplatformSupportAvailability : AbstractMultiplatformSupportAvailability() {
 
     @Nls
     override fun name(): String = KotlinCodeInsightBundle.message("kmp.support.availability.name")
 
-    override fun isSupported(ktElement: KtElement): Boolean {
-        if (isUnitTestMode()) {
-            // we want to test KMP in our unit tests
-            return true
-        }
-        if (Registry.`is`("kotlin.k2.kmp.enabled", true) || JvmOnlyProjectChecker.getInstance(ktElement.project).value()) return true
-        return when (ktElement) {
-            is KtFile -> {
-                val platform = ktElement.platform
-                platform.isJvm() || platform.isCommon()
-            }
+    override fun isEnabledByRegistry(): Boolean = Registry.`is`("kotlin.k2.kmp.enabled", true)
 
-            else -> {
-                var result: Boolean = true
-                for (declaration in ktElement.parents(true).filterIsInstance<KtDeclaration>()) {
-                    if ((declaration as? KtPureClassOrObject)?.isLocal == true || (declaration as? KtFunction)?.isLocal == true) {
-                        result = true
-                        break
-                    }
+    override fun isSupportedPlatform(platform: TargetPlatform): Boolean = platform.isJvm() || platform.isCommon()
 
-                    if (declaration.hasActualModifier() || declaration.hasExpectModifier()) {
-                        result = false
-                        break
-                    }
-                }
-                result
-            }
-
-        }
-    }
 }
