53c04c9095a78294f87123fd02d6e66ca0f0a39a
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2024-06-04 21:12:20 +0400

    [kotlin] Convert RemoveDefaultParameterValueFix to ModCommandAction
    ^KTIJ-29521
    
    GitOrigin-RevId: 416142e589d81bd80f45736fb99cf33326fb9c05

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RemoveDefaultParameterValueFix.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RemoveDefaultParameterValueFix.kt
@@ -1,26 +1,29 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.quickfix
 
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
+import com.intellij.modcommand.ActionContext
+import com.intellij.modcommand.ModPsiUpdater
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.quickfixes.KotlinQuickFixAction
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.KotlinPsiUpdateModCommandAction
 import org.jetbrains.kotlin.idea.util.CommentSaver
-import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtParameter
 
-class RemoveDefaultParameterValueFix(parameter: KtParameter) : KotlinQuickFixAction<KtParameter>(parameter) {
+class RemoveDefaultParameterValueFix(
+    element: KtParameter,
+) : KotlinPsiUpdateModCommandAction.ElementBased<KtParameter, Unit>(element, Unit) {
 
-    override fun getText() = KotlinBundle.message("remove.default.parameter.value")
+    override fun getFamilyName() = KotlinBundle.message("remove.default.parameter.value")
 
-    override fun getFamilyName() = text
-
-    override fun invoke(project: Project, editor: Editor?, file: KtFile) {
-        val parameter = element ?: return
-        val typeReference = parameter.typeReference ?: return
-        val defaultValue = parameter.defaultValue ?: return
-        val commentSaver = CommentSaver(parameter)
-        parameter.deleteChildRange(typeReference.nextSibling, defaultValue)
-        commentSaver.restore(parameter)
+    override fun invoke(
+        actionContext: ActionContext,
+        element: KtParameter,
+        elementContext: Unit,
+        updater: ModPsiUpdater,
+    ) {
+        val typeReference = element.typeReference ?: return
+        val defaultValue = element.defaultValue ?: return
+        val commentSaver = CommentSaver(element)
+        element.deleteChildRange(typeReference.nextSibling, defaultValue)
+        commentSaver.restore(element)
     }
 }
