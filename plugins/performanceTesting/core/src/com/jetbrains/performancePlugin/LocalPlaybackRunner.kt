a7f3f30b31480747249c95419e6b8c3b0f5d5096
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-06-04 16:48:14 +0200

    [performancePlugin] Make performance span current for commands executed via driver
    Commands via Driver are executed in a different thread (JMX one) so ThreadLocal variable with current context is missing and all spans from the commands appear in the root.
    
    GitOrigin-RevId: 646c4a5b1fa6610ce5c97bc109e74824bc32d7ad

--- plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/LocalPlaybackRunner.kt
+++ plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/LocalPlaybackRunner.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.project.Project
 
 class LocalPlaybackRunner: PerformancePlaybackRunner {
   override fun runScript(project: Project, script: String) {
+    PerformanceTestSpan.makeTestSpanCurrent()
     PlaybackRunnerExtended(script, CommandLogger(), project).runBlocking(0)
   }
 }
