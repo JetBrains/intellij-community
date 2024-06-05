ee22f9a1ed335fc8df9e74dec58133ea202b3047
Alex Plate <aleksei.plate@jetbrains.com>
2024-06-05 17:23:49 +0300

    [Workspace Model] [IJPL-149805] Rollback parallel module loading due to concurrency issue
    This is a revert of 95df3f48978b4534d8f0b13bd57deffc95f22767 that was done for IJPL-149482
    
    This change leads to a concurrency issue that should be separately investigated
    
    GitOrigin-RevId: d6969bfef67fa0e629b049424876ff421d5522d2

--- platform/lang-impl/src/com/intellij/workspaceModel/ide/impl/legacyBridge/module/ModuleManagerComponentBridge.kt
+++ platform/lang-impl/src/com/intellij/workspaceModel/ide/impl/legacyBridge/module/ModuleManagerComponentBridge.kt
@@ -38,8 +38,6 @@ import com.intellij.workspaceModel.ide.impl.legacyBridge.project.ModuleRootListe
 import com.intellij.workspaceModel.ide.legacyBridge.ModuleBridge
 import com.intellij.workspaceModel.ide.toPath
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import java.io.IOException
 import java.nio.file.Path
@@ -93,25 +91,23 @@ internal class ModuleManagerComponentBridge(private val project: Project, corout
     val moduleChanges = (event[ModuleEntity::class.java] as? List<EntityChange<ModuleEntity>>) ?: emptyList()
     // `runBlocking` usage approved: https://jetbrains.team/im/thread/2dJ00M3nYBxT/DA2Jg0U6sRs?message=D9Bh40U795a&channel=1Dx4720YRoCU
     runBlocking {
-      val moduleFutures = moduleChanges.mapNotNull {
+      val moduleBridgeAndEntityList = moduleChanges.mapNotNull {
         if (it !is EntityChange.Added<ModuleEntity>) return@mapNotNull null
         if (it.entity.findModule(builder) != null) return@mapNotNull null
 
-        async(Dispatchers.Default) {
-          val plugins = PluginManagerCore.getPluginSet().getEnabledModules()
-          val bridge = createModuleInstance(moduleEntity = it.entity,
-                                            versionedStorage = entityStore,
-                                            diff = builder,
-                                            isNew = true,
-                                            precomputedExtensionModel = null,
-                                            plugins = plugins,
-                                            corePlugin = plugins.firstOrNull { it.pluginId == PluginManagerCore.CORE_ID })
-          bridge to it.entity
-        }
+        val plugins = PluginManagerCore.getPluginSet().getEnabledModules()
+        val bridge = createModuleInstance(moduleEntity = it.entity,
+                                          versionedStorage = entityStore,
+                                          diff = builder,
+                                          isNew = true,
+                                          precomputedExtensionModel = null,
+                                          plugins = plugins,
+                                          corePlugin = plugins.firstOrNull { it.pluginId == PluginManagerCore.CORE_ID })
+        bridge to it.entity
       }
 
-      for (moduleFuture in moduleFutures) {
-        val (bridge, entity) = moduleFuture.await()
+      for (moduleBridgeAndEntity in moduleBridgeAndEntityList) {
+        val (bridge, entity) = moduleBridgeAndEntity
         builder.mutableModuleMap.addMapping(entity, bridge)
       }
     }
