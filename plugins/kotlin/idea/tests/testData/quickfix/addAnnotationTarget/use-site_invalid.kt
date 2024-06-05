12783dd7c88f57252e07731f8b6f8b53e940c9a8
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2024-06-05 19:34:03 +0200

    [Kotlin] Deleted quick-fix tests for fully qualifying calls with ambiguous resolution.
    This is no longer needed when using JPS 2.0 because the K2 compiler has changed the resolution to the new way.
    
    KTIJ-30704
    
    GitOrigin-RevId: 98f33cb4238b9c2ad50e5ca40bf1ef9cfd7ca1b2

--- plugins/kotlin/idea/tests/testData/quickfix/addAnnotationTarget/use-site_invalid.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/addAnnotationTarget/use-site_invalid.kt
@@ -3,6 +3,7 @@
 // ACTION: Extract 'Test' from current file
 // ACTION: Make internal
 // ACTION: Make private
+// ERROR: '@get:' annotations could be applied only to property declarations
 // ERROR: This annotation is not applicable to target 'class' and use site target '@get'
 annotation class Ann
 
