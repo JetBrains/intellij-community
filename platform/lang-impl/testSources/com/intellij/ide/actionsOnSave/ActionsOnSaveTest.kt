2abb8d677454a69eadea2a0916e7d43b97746f56
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2024-06-05 16:08:41 +0200

    [Actions on Save] Add new Actions on Save API, aimed at running them in background instead of under modal progress
    Main class: com.intellij.ide.actionsOnSave.impl.ActionsOnSaveFileDocumentManagerListener.DocumentUpdatingActionOnSave
    
    Needed for RUBY-29110, WEB-61752, IDEA-322963, IJPL-61470, IJPL-29913
    
    GitOrigin-RevId: b0992131a128a6dc39293bec5f4cd44ace59dbca

---
+++ platform/lang-impl/testSources/com/intellij/ide/actionsOnSave/ActionsOnSaveTest.kt
@@ -0,0 +1,177 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide.actionsOnSave
+
+import com.intellij.ide.actionsOnSave.impl.ActionsOnSaveFileDocumentManagerListener
+import com.intellij.ide.actionsOnSave.impl.ActionsOnSaveManager
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.command.writeCommandAction
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.project.Project
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.intellij.testFramework.utils.coroutines.waitCoroutinesBlocking
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class ActionsOnSaveTest : BasePlatformTestCase() {
+  @Service(Service.Level.PROJECT)
+  private class ActionsOnSaveTestService(val cs: CoroutineScope) {
+    var onBgtActionOnSave1Started: (suspend (Document) -> Unit)? = null
+    fun bgtActionOnSave1Started(document: Document) = onBgtActionOnSave1Started?.let { cs.launch { it(document) } }
+  }
+
+  private data object EdtActionOnSave1 : EdtActionOnSave(1)
+  private data object EdtActionOnSave2 : EdtActionOnSave(2)
+
+  private data object BgtActionOnSave1 : BgtActionOnSave(1) {
+    override suspend fun updateDocument(project: Project, document: Document) {
+      project.service<ActionsOnSaveTestService>().bgtActionOnSave1Started(document)
+      super.updateDocument(project, document)
+    }
+  }
+
+  private data object BgtActionOnSave2 : BgtActionOnSave(2)
+
+  private sealed class EdtActionOnSave(val index: Int) : ActionsOnSaveFileDocumentManagerListener.ActionOnSave() {
+    override fun isEnabledForProject(project: Project): Boolean = true
+    override fun processDocuments(project: Project, documents: Array<Document>) {
+      documents
+        .filter { FileDocumentManager.getInstance().getFile(it)?.name?.endsWith(".txt") == true }
+        .takeIf { it.isNotEmpty() }
+        ?.let { docs ->
+          WriteCommandAction.runWriteCommandAction(project) {
+            docs.forEach { document -> document.insertString(document.textLength, "\nEdtActionOnSave$index") }
+          }
+        }
+    }
+  }
+
+  private sealed class BgtActionOnSave(val index: Int) : ActionsOnSaveFileDocumentManagerListener.DocumentUpdatingActionOnSave() {
+    override val presentableName: String = "BgtActionOnSave$index"
+    override fun isEnabledForProject(project: Project): Boolean = true
+
+    override suspend fun updateDocument(project: Project, document: Document) {
+      if (FileDocumentManager.getInstance().getFile(document)?.name?.endsWith(".txt") != true) return
+
+      delay(100)
+      writeCommandAction(project, "BgtActionOnSave$index(1)") {
+        document.insertString(document.textLength, "\nBgtActionOnSave$index(1)")
+      }
+
+      delay(100)
+      writeCommandAction(project, "BgtActionOnSave$index(2)") {
+        document.insertString(document.textLength, "\nBgtActionOnSave$index(2)")
+      }
+    }
+  }
+
+  override fun setUp() {
+    super.setUp()
+
+    val ep = ExtensionPointName<ActionsOnSaveFileDocumentManagerListener.ActionOnSave>("com.intellij.actionOnSave").point
+    ep.registerExtension(EdtActionOnSave1, testRootDisposable)
+    ep.registerExtension(EdtActionOnSave2, testRootDisposable)
+    ep.registerExtension(BgtActionOnSave1, testRootDisposable)
+    ep.registerExtension(BgtActionOnSave2, testRootDisposable)
+  }
+
+  private val file1Name = "file1.txt"
+  private val file2Name = "file2.txt"
+  private fun isDocument1(document: Document) = FileDocumentManager.getInstance().getFile(document)?.name == file1Name
+  private fun isDocument2(document: Document) = FileDocumentManager.getInstance().getFile(document)?.name == file2Name
+
+  private val textAfterAllActions = """
+    initial text
+    EdtActionOnSave1
+    EdtActionOnSave2
+    BgtActionOnSave1(1)
+    BgtActionOnSave1(2)
+    BgtActionOnSave2(1)
+    BgtActionOnSave2(2)
+  """.trimIndent()
+
+  private fun addDocument(fileName: String): Document {
+    val document = myFixture.addFileToProject(fileName, "").fileDocument
+    WriteCommandAction.runWriteCommandAction(project) {
+      document.insertString(0, "initial text")
+    }
+    return document
+  }
+
+  private suspend fun typeInDocumentAfterDelay(document: Document, delayMillis: Long) {
+    delay(delayMillis)
+    writeCommandAction(project, "manual typing") {
+      document.insertString(document.textLength, "\nmanual typing")
+    }
+  }
+
+  private fun doTestWithTwoFiles(
+    doc1ExpectedText: String,
+    doc2ExpectedText: String,
+    onBgtActionOnSave1Started: (suspend (document: Document) -> Unit)? = null,
+  ) {
+    val testService = project.service<ActionsOnSaveTestService>()
+    testService.onBgtActionOnSave1Started = onBgtActionOnSave1Started
+
+    val doc1 = addDocument(file1Name)
+    val doc2 = addDocument(file2Name)
+
+    FileDocumentManager.getInstance().saveAllDocuments()
+
+    testService.cs.launch {
+      service<ActionsOnSaveManager>().awaitPendingActions()
+    }
+    waitCoroutinesBlocking(testService.cs)
+
+    checkResult(doc1, doc1ExpectedText, doc2, doc2ExpectedText)
+  }
+
+  private fun checkResult(doc1: Document, doc1ExpectedText: String, doc2: Document, doc2ExpectedText: String) {
+    assertEquals(doc1ExpectedText, doc1.text)
+    assertEquals(doc2ExpectedText, doc2.text)
+
+    val doc1ExpectedSaved = doc1ExpectedText == textAfterAllActions
+    val doc2ExpectedSaved = doc2ExpectedText == textAfterAllActions
+    assertEquals("File 1 should be ${if (doc1ExpectedSaved) "saved" else "unsaved"}",
+                 doc1ExpectedSaved, !FileDocumentManager.getInstance().isDocumentUnsaved(doc1))
+    assertEquals("File 2 should be ${if (doc2ExpectedSaved) "saved" else "unsaved"}",
+                 doc2ExpectedSaved, !FileDocumentManager.getInstance().isDocumentUnsaved(doc2))
+  }
+
+  fun testAllActionsFinished() =
+    doTestWithTwoFiles(textAfterAllActions, textAfterAllActions)
+
+  fun testFirstFileEditedAfterAllActionsFinished() {
+    val doc1ExpectedText = "$textAfterAllActions\nmanual typing"
+    doTestWithTwoFiles(doc1ExpectedText, textAfterAllActions) {
+      if (isDocument1(it)) typeInDocumentAfterDelay(it, 450)
+    }
+  }
+
+  fun testSecondFileEditedAfter50ms() {
+    val doc2ExpectedText = "initial text\nEdtActionOnSave1\nEdtActionOnSave2\nmanual typing"
+    doTestWithTwoFiles(textAfterAllActions, doc2ExpectedText) {
+      if (isDocument2(it)) typeInDocumentAfterDelay(it, 50)
+    }
+  }
+
+  fun testFirstFileEditedAfter150ms() {
+    val doc1ExpectedText = "initial text\nEdtActionOnSave1\nEdtActionOnSave2\nBgtActionOnSave1(1)\nmanual typing"
+    doTestWithTwoFiles(doc1ExpectedText, textAfterAllActions) {
+      if (isDocument1(it)) typeInDocumentAfterDelay(it, 150)
+    }
+  }
+
+  fun testFirstFileEditedAfter250msSecondAfter350() {
+    val doc1ExpectedText = "initial text\nEdtActionOnSave1\nEdtActionOnSave2\nBgtActionOnSave1(1)\nBgtActionOnSave1(2)\nmanual typing"
+    val doc2ExpectedText = "initial text\nEdtActionOnSave1\nEdtActionOnSave2\nBgtActionOnSave1(1)\nBgtActionOnSave1(2)\nBgtActionOnSave2(1)\nmanual typing"
+    doTestWithTwoFiles(doc1ExpectedText, doc2ExpectedText) {
+      if (isDocument1(it)) typeInDocumentAfterDelay(it, 250)
+      if (isDocument2(it)) typeInDocumentAfterDelay(it, 350)
+    }
+  }
+}
--- platform/platform-impl/src/com/intellij/ide/actionsOnSave/impl/ActionsOnSaveFileDocumentManagerListener.kt
+++ platform/platform-impl/src/com/intellij/ide/actionsOnSave/impl/ActionsOnSaveFileDocumentManagerListener.kt
@@ -2,37 +2,50 @@
 package com.intellij.ide.actionsOnSave.impl
 
 import com.intellij.codeWithMe.ClientId
+import com.intellij.concurrency.currentThreadContext
+import com.intellij.ide.IdeBundle
 import com.intellij.ide.actions.SaveDocumentAction
+import com.intellij.ide.actionsOnSave.impl.ActionsOnSaveFileDocumentManagerListener.DocumentUpdatingActionOnSave
 import com.intellij.ide.impl.runUnderModalProgressIfIsEdt
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.AnActionResult
 import com.intellij.openapi.actionSystem.ex.AnActionListener
-import com.intellij.openapi.application.EDT
-import com.intellij.openapi.application.ModalityState
-import com.intellij.openapi.application.asContextElement
-import com.intellij.openapi.application.readAction
+import com.intellij.openapi.application.*
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.event.DocumentEvent
+import com.intellij.openapi.editor.event.DocumentListener
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileDocumentManagerListener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.platform.ide.progress.withBackgroundProgress
+import com.intellij.platform.util.progress.reportProgress
+import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
 import com.intellij.util.concurrency.annotations.RequiresEdt
 import com.intellij.util.concurrency.annotations.RequiresReadLock
 import kotlinx.coroutines.*
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.TestOnly
+import kotlin.coroutines.AbstractCoroutineContextElement
+import kotlin.coroutines.CoroutineContext
 
 private val EP_NAME = ExtensionPointName<ActionsOnSaveFileDocumentManagerListener.ActionOnSave>("com.intellij.actionOnSave")
 
 class ActionsOnSaveFileDocumentManagerListener : FileDocumentManagerListener {
+  /**
+   * **Note:** If the Action on Save is going to update file contents, for example, to format a file on save,
+   * then extend [DocumentUpdatingActionOnSave].
+   * Unlike [ActionOnSave.processDocuments], which assumes modal progress,
+   * [DocumentUpdatingActionOnSave.updateDocument] runs with cancelable background progress.
+   */
   abstract class ActionOnSave {
     /**
-     * Invoked in EDT, maybe inside write action. Should be fast. It's ok to return `true` and then do nothing in [.processDocuments]
+     * Invoked in EDT, maybe inside write action. Should be fast. It's ok to return `true` and then do nothing in [processDocuments]
      *
      * @param project it's initialized, open, not disposed, and not the default one; no need to double-check in implementations
      */
@@ -40,12 +53,63 @@ class ActionsOnSaveFileDocumentManagerListener : FileDocumentManagerListener {
     open fun isEnabledForProject(project: Project): Boolean = false
 
     /**
-     * Invoked in EDT, not inside write action. Potentially long implementations should run with modal progress synchronously.
-     * Implementations don't need to save modified documents. Note that the passed documents may be unsaved if already modified by some other save action.
+     * **Note:** If the Action on Save is going to update file contents, for example, to format a file on save,
+     * then implement [DocumentUpdatingActionOnSave.updateDocument] instead of this function.
+     * Unlike this function, [DocumentUpdatingActionOnSave.updateDocument] runs with cancelable background progress.
+     *
+     * This function is invoked in EDT, not inside write action.
+     * Potentially long implementations should run with modal progress synchronously.
+     * Implementations don't need to save modified documents.
+     * Note that the passed documents may be unsaved if already modified by some other save action.
      */
     @RequiresEdt
     open fun processDocuments(project: Project, documents: Array<Document>) {
     }
+  }
+
+  /**
+   * Action on Save that updates file contents, for example, formats a file on save.
+   * Unlike [ActionOnSave.processDocuments], which assumes modal progress,
+   * [DocumentUpdatingActionOnSave.updateDocument] runs with cancelable background progress.
+   */
+  abstract class DocumentUpdatingActionOnSave : ActionOnSave() {
+    final override fun processDocuments(project: Project, documents: Array<Document>) {}
+
+    /**
+     * For progress reporting: `"${actionOnSave.presentableName}: processing $fileName"`
+     */
+    abstract val presentableName: String
+
+    /**
+     * Typical implementation:
+     *
+     *    override suspend fun updateDocument(project: Project, document: Document) {
+     *      // To support cancellability, implementations of `prepareChange()`
+     *      // should call `checkCanceled()` directly or indirectly often enough.
+     *      val changeInfo = prepareChange(project, document)
+     *      writeCommandAction(project, "Format file") {
+     *        applyChange(project, document, changeInfo)
+     *      }
+     *    }
+     *
+     * Another example for the case when a `readAction` is needed to calculate the document change:
+     *
+     *    override suspend fun updateDocument(project: Project, document: Document) {
+     *      readAndWriteAction {
+     *        // To support cancellability, implementations of `prepareChange()`
+     *        // should call `checkCanceled()` directly or indirectly often enough.
+     *        val changeInfo = prepareChange(project, document)
+     *        writeAction {
+     *          executeCommand(project, "Format file") {
+     *            applyChange(project, document, changeInfo)
+     *          }
+     *        }
+     *      }
+     *    }
+     *
+     */
+    @RequiresBackgroundThread
+    abstract suspend fun updateDocument(project: Project, document: Document)
   }
 
   override fun beforeDocumentSaving(document: Document) {
@@ -86,7 +150,7 @@ class ActionsOnSaveFileDocumentManagerListener : FileDocumentManagerListener {
 }
 
 @Service(Service.Level.APP)
-class ActionsOnSaveManager(val coroutineScope: CoroutineScope) {
+class ActionsOnSaveManager(private val coroutineScope: CoroutineScope) {
   /**
    * Not empty state of this set means that processing has been scheduled (invokeLater(...)) but not yet performed.
    */
@@ -104,12 +168,11 @@ class ActionsOnSaveManager(val coroutineScope: CoroutineScope) {
     }
   }
 
+  @RequiresEdt
   private suspend fun processSavedDocuments() {
-    val documentsAndModStamps = documentsToProcess.map { it to it.modificationStamp }
+    val documentsAndModStamps = documentsToProcess.associateWith { it.modificationStamp }
     documentsToProcess.clear()
 
-    val manager = FileDocumentManager.getInstance()
-    val processedDocuments = ArrayList<Document>()
     for (project in ProjectManager.getInstance().openProjects) {
       if (project.isDisposed) {
         continue
@@ -119,27 +182,170 @@ class ActionsOnSaveManager(val coroutineScope: CoroutineScope) {
       val projectDocuments = withContext(Dispatchers.Default) {
         readAction {
           documentsAndModStamps.mapNotNull {
-            val document = it.first
-            val modStamp = it.second
+            val document = it.key
+            val modStamp = it.value
             if (document.modificationStamp != modStamp) return@mapNotNull null // already edited after save
-            val file = manager.getFile(document) ?: return@mapNotNull null
+            val file = FileDocumentManager.getInstance().getFile(document) ?: return@mapNotNull null
             if (index.isInContent(file)) document else null
+          }.toMutableList()
+        }
+      }
+
+      // filter out documents that have been already manually edited after save
+      projectDocuments.removeAll { it.modificationStamp != documentsAndModStamps[it] }
+
+      if (!project.isDisposed && projectDocuments.isNotEmpty()) {
+        runActionsOnSave(project, projectDocuments)
+      }
+    }
+  }
+
+  /**
+   * Runs Actions on Save and saves documents, for which all applicable Actions on Save have finished successfully.
+   * Manual editing of a document cancels all not-yet-started and not-yet-finished Actions on Save for this document,
+   * but Actions on Save for other documents keep running.
+   *
+   * There are two APIs that the plugins may use to implement an Action on Save:
+   * - [ActionsOnSaveFileDocumentManagerListener.ActionOnSave.processDocuments].
+   * It is invoked synchronously in EDT.
+   * This API is ok for actions like "schedule file upload" or "start file compilation".
+   * But it's not recommended to use this API for actions that update the saved file contents.
+   * Such actions are potentially long, so running them synchronously needs modal progress, and this is not what people like.
+   * - [DocumentUpdatingActionOnSave.processDocuments].
+   * It's invoked in a background thread with a cancelable background progress bar.
+   * This API is preferred for actions that update the saved file contents, for example, format files on save.
+   */
+  @RequiresEdt
+  private suspend fun runActionsOnSave(project: Project, projectDocuments: List<Document>) {
+    val projectActionsOnSave = EP_NAME.extensionList.filter { it.isEnabledForProject(project) }
+
+    projectActionsOnSave.forEach { it.processDocuments(project, projectDocuments.toTypedArray()) }
+
+    val documentUpdatingActionsOnSave = projectActionsOnSave.filterIsInstance<DocumentUpdatingActionOnSave>()
+    if (documentUpdatingActionsOnSave.isNotEmpty()) {
+      // Document saving in this `if` branch is not needed because they will be saved in `runDocumentUpdatingActionsOnSaveAndSaveDocument`
+      val documentsToModStamps = projectDocuments.associateWith { it.modificationStamp }
+      withContext(Dispatchers.Default) {
+        runDocumentUpdatingActionsOnSave(project, documentUpdatingActionsOnSave, documentsToModStamps)
+      }
+    }
+    else {
+      projectDocuments.forEach(FileDocumentManager.getInstance()::saveDocument)
+    }
+  }
+
+  /**
+   * Runs [actionsOnSave] for [documents][documentsToModStamps],
+   * which haven't been manually edited before the Actions on Save get a chance to start or to complete.
+   *
+   * Manual editing of a document cancels all not-yet-started and not-yet-finished Actions on Save for this document,
+   * but Actions on Save for other documents keep running.
+   *
+   * When all Actions on Save finish successfully for the document, it is saved to the disk
+   * (because users expect files to end up in saved state after pressing Ctrl+S/Cmd+S).
+   * Manually edited documents (with canceled Actions on Save) will not be saved.
+   *
+   * @param documentsToModStamps Actions on Save must not be started for the `document`
+   * if it has been already manually edited after save but before Actions on Save have been started.
+   * This can be checked by comparing the current modification stamp with the one from this map.
+   */
+  @RequiresBackgroundThread
+  private suspend fun runDocumentUpdatingActionsOnSave(
+    project: Project,
+    actionsOnSave: List<DocumentUpdatingActionOnSave>,
+    documentsToModStamps: Map<Document, Long>,
+  ) {
+    @Suppress("DialogTitleCapitalization") val progressTitle = IdeBundle.message("actions.on.save.background.progress")
+    withBackgroundProgress(project, progressTitle) {
+      reportProgress(size = documentsToModStamps.size) { progressReporter ->
+        for (documentToModStamp in documentsToModStamps) {
+          progressReporter.itemStep {
+            // On manual document editing, we should cancel only Actions on Save for the edited document
+            // but keep running Actions on Save for other documents.
+            // So, a separate child coroutine for each document is needed.
+            val documentJob = launch(ActionOnSaveContextElement) {
+              val document = documentToModStamp.key
+              val modStamp = documentToModStamp.value
+              runDocumentUpdatingActionsOnSaveAndSaveDocument(project, actionsOnSave, document, modStamp, documentScope = this)
+            }
+
+            // Given that there may be a lot of documents
+            // and that Actions on Save may perform heavy operations or start external processes,
+            // the safer path is to run Actions on Save one by one, not in parallel.
+            documentJob.join()
           }
         }
       }
-      if (project.isDisposed || projectDocuments.isEmpty()) {
-        continue
-      }
-      for (saveAction in EP_NAME.extensionList) {
-        if (saveAction.isEnabledForProject(project)) {
-          processedDocuments.addAll(projectDocuments)
-          saveAction.processDocuments(project, projectDocuments.toTypedArray())
+    }
+  }
+
+  /**
+   * Runs [actionsOnSave] for the given [document]
+   * unless it has been manually edited before the Actions on Save get a chance to start or to complete.
+   *
+   * Manual editing of a document cancels all not-yet-started and not-yet-finished Actions on Save for this document,
+   * but Actions on Save for other documents keep running.
+   *
+   * When all Actions on Save finish successfully for the document, it is saved to the disk
+   * (because users expect files to end up in saved state after pressing Ctrl+S/Cmd+S).
+   * Manually edited documents (with canceled Actions on Save) will not be saved.
+   *
+   * @param modStamp Actions on Save must not be started on the [document] if it has been manually edited.
+   * This is guaranteed by cancelling the [documentScope] in the `DocumentListener` (see the function implementation).
+   * The passed [modStamp] helps to make sure that the [document] hasn't been edited before the `DocumentListener` has been added.
+   *
+   * @param documentScope the [CoroutineScope] in which the [actionsOnSave] run for the given [document].
+   * Used to cancel Actions on Save for this document if it has been manually edited before the Actions on Save have finished.
+   */
+  @RequiresBackgroundThread
+  private suspend fun runDocumentUpdatingActionsOnSaveAndSaveDocument(
+    project: Project,
+    actionsOnSave: List<DocumentUpdatingActionOnSave>,
+    document: Document,
+    modStamp: Long,
+    documentScope: CoroutineScope,
+  ) {
+    val documentListener = object : DocumentListener {
+      override fun beforeDocumentChange(event: DocumentEvent) {
+        // If the change in the document is not caused by the running Action on Save,
+        // then cancel all not yet completed Actions on Save for the current document but keep running them for other documents.
+        val docChangedByActionOnSave = currentThreadContext()[ActionOnSaveContextElement.Key] != null
+        if (!docChangedByActionOnSave) {
+          documentScope.cancel()
         }
       }
     }
-    for (document in processedDocuments) {
-      manager.saveDocument(document)
+
+    try {
+      document.addDocumentListener(documentListener)
+
+      // Check if the document has been already edited before the DocumentListener has been added and cancel Actions on Save if so.
+      if (document.modificationStamp != modStamp) {
+        documentScope.cancel()
+      }
+
+      reportProgress(size = actionsOnSave.size) { progressReporter ->
+        val fileName = FileDocumentManager.getInstance().getFile(document)?.name
+        for (actionOnSave in actionsOnSave) {
+          progressReporter.itemStep(IdeBundle.message("actions.on.save.processing.file", actionOnSave.presentableName, fileName)) {
+            actionOnSave.updateDocument(project, document)
+          }
+        }
+      }
+
+      writeAction {
+        // All Actions on Save have completed successfully for the document, so need to save it.
+        // Otherwise, users would see unsaved documents after pressing Ctrl+S/Cmd+S, which would be unexpected.
+        FileDocumentManager.getInstance().saveDocument(document)
+      }
     }
+    finally {
+      document.removeDocumentListener(documentListener)
+    }
+  }
+
+  private object ActionOnSaveContextElement : AbstractCoroutineContextElement(Key) {
+    object Key : CoroutineContext.Key<ActionOnSaveContextElement>
   }
 
   /**
