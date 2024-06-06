d9f46d0074bf7e6f4a8a0ce750b00cb8c4fff0e2
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2024-06-06 14:56:34 +0200

    [workspace] Manage Workspace... action dropped
    workspace management is available via ProjectView actions
    
    GitOrigin-RevId: 0f8fa9b166d3ca6ae4480537c4b7ab2dd352c616

--- plugins/gradle/java/testSources/setup/WorkspaceTest.kt
+++ plugins/gradle/java/testSources/setup/WorkspaceTest.kt
@@ -26,7 +26,7 @@ class WorkspaceTest: GradleTestCase() {
       initProject(projectInfo)
       openProject("linked_project").useProject {
         val event = TestActionEvent.createTestEvent(SimpleDataContext.getProjectContext(it))
-        ActionManager.getInstance().getAction("ManageWorkspace").update(event)
+        ActionManager.getInstance().getAction("AddToWorkspace").update(event)
         assertFalse(event.presentation.isEnabledAndVisible)
       }
     }
