f557d50e671c0b99e00b563bfa2ee56e610e88c6
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-06-06 12:46:11 +0200

    [devkit] Attach IC sources to IntelliJ Platform resolved from CDN.
    This commit introduces IntelliJ Platform CDN coordinates required to map CDN artifacts to actual products.
    In addition, it adds new products to the list: Aqua, RustRover, Writerside
    
    GitOrigin-RevId: b519396b19096a1dd217afa0aafbeeaa85020c17

--- plugins/devkit/intellij.devkit.gradle/src/IntelliJPlatformAttachSourcesProvider.kt
+++ plugins/devkit/intellij.devkit.gradle/src/IntelliJPlatformAttachSourcesProvider.kt
@@ -16,7 +16,6 @@ import com.intellij.psi.PsiFile
 import org.jetbrains.idea.devkit.projectRoots.IntelliJPlatformProduct
 import org.jetbrains.plugins.gradle.util.GradleDependencySourceDownloader
 import java.io.File
-import kotlin.io.path.Path
 
 /**
  * Attaches sources to the IntelliJ Platform dependencies in projects using IntelliJ Platform Gradle Plugin 2.x.
@@ -31,6 +30,7 @@ class IntelliJPlatformAttachSourcesProvider : AttachSourcesProvider {
       it.library?.getMavenCoordinates()
     }.firstNotNullOfOrNull { coordinates ->
       val product = IntelliJPlatformProduct.fromMavenCoordinates(coordinates.groupId, coordinates.artifactId)
+                    ?: IntelliJPlatformProduct.fromCdnCoordinates(coordinates.groupId, coordinates.artifactId)
       if (product == null) {
         return@firstNotNullOfOrNull null
       }
