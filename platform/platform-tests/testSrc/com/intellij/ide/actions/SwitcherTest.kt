cf50037f0eadd19282fe65fe52b59db765ff75dc
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-06-06 07:47:52 +0200

    IJPL-796 refactor SwitcherTest
    GitOrigin-RevId: 5b902ee3ef9f92406ff3458edd94eb24eafa5aa0

--- platform/platform-tests/testSrc/com/intellij/ide/actions/SwitcherTest.kt
+++ platform/platform-tests/testSrc/com/intellij/ide/actions/SwitcherTest.kt
@@ -1,42 +1,40 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ide.actions;
+package com.intellij.ide.actions
 
-import com.intellij.ide.ui.UISettings;
-import com.intellij.openapi.fileEditor.impl.FileEditorOpenOptions;
-import com.intellij.testFramework.FileEditorManagerTestCase;
-import com.intellij.testFramework.PlatformTestUtil;
+import com.intellij.ide.actions.Switcher.SwitcherPanel.Companion.getFilesSelectedIndexForTest
+import com.intellij.ide.actions.Switcher.SwitcherPanel.Companion.getFilesToShowForTest
+import com.intellij.ide.ui.UISettings
+import com.intellij.ide.ui.UISettings.Companion.getInstance
+import com.intellij.openapi.fileEditor.impl.FileEditorOpenOptions
+import com.intellij.testFramework.FileEditorManagerTestCase
+import com.intellij.testFramework.PlatformTestUtil
+import javax.swing.SwingConstants
 
-import javax.swing.*;
-import java.util.List;
+class SwitcherTest : FileEditorManagerTestCase() {
+  override fun getTestDataPath(): String = PlatformTestUtil.getPlatformTestDataPath() + "fileEditorManager"
 
-public class SwitcherTest extends FileEditorManagerTestCase {
-  @Override
-  protected String getTestDataPath() {
-    return PlatformTestUtil.getPlatformTestDataPath() + "fileEditorManager";
+  fun testSwitcherPanelWithTabs() {
+    testTabPlacement(tabPlacement = SwingConstants.TOP, goForward = true)
+    testTabPlacement(tabPlacement = SwingConstants.TOP, goForward = false)
   }
 
-  public void testSwitcherPanelWithTabs() {
-    testTabPlacement(SwingConstants.TOP, true);
-    testTabPlacement(SwingConstants.TOP, false);
+  fun testSwitcherPanelWithoutTabs() {
+    testTabPlacement(tabPlacement = UISettings.TABS_NONE, goForward = true)
+    testTabPlacement(tabPlacement = UISettings.TABS_NONE, goForward = false)
   }
 
-  public void testSwitcherPanelWithoutTabs() {
-    testTabPlacement(UISettings.TABS_NONE, true);
-    testTabPlacement(UISettings.TABS_NONE, false);
-  }
+  private fun testTabPlacement(tabPlacement: Int, goForward: Boolean) {
+    getInstance().state.editorTabPlacement = tabPlacement
+    manager!!.openFile(getFile("/src/1.txt"), null, FileEditorOpenOptions(requestFocus = true))
+    manager!!.openFile(getFile("/src/2.txt"), null, FileEditorOpenOptions(requestFocus = true))
+    manager!!.openFile(getFile("/src/3.txt"), null, FileEditorOpenOptions(requestFocus = true))
+    val filesToShow = getFilesToShowForTest(project)
+    val selectedItem = getFilesSelectedIndexForTest(project, goForward)
 
-  private void testTabPlacement(int tabPlacement, boolean goForward) {
-    UISettings.getInstance().getState().setEditorTabPlacement(tabPlacement);
-    manager.openFile(getFile("/src/1.txt"), null, new FileEditorOpenOptions().withRequestFocus());
-    manager.openFile(getFile("/src/2.txt"), null, new FileEditorOpenOptions().withRequestFocus());
-    manager.openFile(getFile("/src/3.txt"), null, new FileEditorOpenOptions().withRequestFocus());
-    List<?> filesToShow = Switcher.SwitcherPanel.getFilesToShowForTest(getProject());
-    int selectedItem = Switcher.SwitcherPanel.getFilesSelectedIndexForTest(getProject(), goForward);
-
-    assertEquals(goForward ? 1 : 2, selectedItem);
-    assertEquals(3, filesToShow.size());
-    assertEquals(getFile("/src/3.txt"), filesToShow.get(0));
-    assertEquals(getFile("/src/2.txt"), filesToShow.get(1));
-    assertEquals(getFile("/src/1.txt"), filesToShow.get(2));
+    assertEquals(if (goForward) 1 else 2, selectedItem)
+    assertEquals(3, filesToShow.size)
+    assertEquals(getFile("/src/3.txt"), filesToShow[0])
+    assertEquals(getFile("/src/2.txt"), filesToShow[1])
+    assertEquals(getFile("/src/1.txt"), filesToShow[2])
   }
 }
