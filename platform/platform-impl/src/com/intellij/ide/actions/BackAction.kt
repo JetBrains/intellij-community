20220dd28d35f18e6e1191b701606184eb07af50
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-06-07 11:46:37 +0200

    IJPL-796 refactor BackAction
    GitOrigin-RevId: eb6830196e01e1dfd7e6691cd126a1107112e409

--- platform/platform-impl/src/com/intellij/ide/actions/BackAction.kt
+++ platform/platform-impl/src/com/intellij/ide/actions/BackAction.kt
@@ -1,52 +1,44 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide.actions
 
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ide.actions;
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformCoreDataKeys
+import com.intellij.openapi.fileEditor.ex.IdeDocumentHistory
+import com.intellij.openapi.project.DumbAware
+import com.intellij.ui.navigation.History
 
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.fileEditor.ex.IdeDocumentHistory;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import com.intellij.ui.navigation.History;
-import org.jetbrains.annotations.NotNull;
-
-public final class BackAction extends AnAction implements DumbAware {
-  public BackAction() {
-    setEnabledInModalContext(true);
+internal class BackAction : AnAction(), DumbAware {
+  init {
+    isEnabledInModalContext = true
   }
 
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
-    Project project = e.getProject();
-    History history = e.getData(History.KEY);
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project
+    val history = e.getData(History.KEY)
 
     if (history != null) {
-      history.back();
+      history.back()
     }
     else if (project != null) {
-      IdeDocumentHistory.getInstance(project).back();
+      IdeDocumentHistory.getInstance(project).back()
     }
   }
 
-  @Override
-  public void update(@NotNull AnActionEvent e) {
-    Project project = e.getProject();
-    History history = e.getData(History.KEY);
-    boolean isModalContext = e.getData(PlatformCoreDataKeys.IS_MODAL_CONTEXT) == Boolean.TRUE;
+  override fun update(e: AnActionEvent) {
+    val project = e.project
+    val history = e.getData(History.KEY)
 
-    Presentation presentation = e.getPresentation();
-    if (history != null) {
-      presentation.setEnabled(history.canGoBack());
-    }
-    else if (project != null && !project.isDisposed()) {
-      presentation.setEnabled(!isModalContext && IdeDocumentHistory.getInstance(project).isBackAvailable());
-    }
-    else {
-      presentation.setEnabled(false);
+    e.presentation.isEnabled = when {
+      history != null -> history.canGoBack()
+      project != null && !project.isDisposed -> {
+        val isModalContext = e.getData(PlatformCoreDataKeys.IS_MODAL_CONTEXT) == true
+        !isModalContext && IdeDocumentHistory.getInstance(project).isBackAvailable
+      }
+      else -> false
     }
   }
 
-  @Override
-  public @NotNull ActionUpdateThread getActionUpdateThread() {
-    return ActionUpdateThread.BGT;
-  }
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 }
