241e706eb3e87e7bfc86a04bf04c14f9048c0e2f
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-06-07 10:49:21 +0200

    [remote dev] dropping legacy property support from `LocalUserSettings`
    GitOrigin-RevId: 072d15c4a6bde551d10d6550039047e42c5dda49

--- platform/remote-core/src/ide/users/LocalUserSettings.kt
+++ platform/remote-core/src/ide/users/LocalUserSettings.kt
@@ -5,26 +5,9 @@ import com.intellij.ide.util.PropertiesComponent
 import com.intellij.util.SystemProperties
 
 object LocalUserSettings {
-
-  private const val userNameKey = "local.user.name"
-  private const val userNameObsoleteKey = "code.with.me.user.name"
+  private const val USER_NAME_KEY = "local.user.name"
 
   var userName: String
-    get() {
-      var value = PropertiesComponent.getInstance().getValue(userNameKey)
-      if (value == null) {
-        // Compatibility with old code-with-me key name
-        value = PropertiesComponent.getInstance().getValue(userNameObsoleteKey)
-        if (value != null) {
-          PropertiesComponent.getInstance().setValue(userNameKey, value)
-          PropertiesComponent.getInstance().setValue(userNameObsoleteKey, null)
-        }
-      }
-      if (!value.isNullOrBlank()) return value
-      return SystemProperties.getUserName()
-    }
-    set(value) {
-      PropertiesComponent.getInstance().setValue(userNameKey, value)
-    }
-
+    get() = PropertiesComponent.getInstance().getValue(USER_NAME_KEY).takeIf { !it.isNullOrBlank() } ?: SystemProperties.getUserName()
+    set(value) = PropertiesComponent.getInstance().setValue(USER_NAME_KEY, value)
 }
