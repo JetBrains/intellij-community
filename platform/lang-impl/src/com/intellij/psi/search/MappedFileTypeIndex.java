b9ee59de61f03fd977aa92551c561cb3b95fa064
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2024-06-11 15:27:05 +0200

    [indexes] IJP-1365: disable 'skip-saving' benchmark, added offloadIO benchmark
    + benchmark with FakeIndexStorage violates some internal indexing invariants => disabled it until sorted out
    + added benchmark with index writing IO offloaded to Dispatchers.IO
    
    GitOrigin-RevId: e3bb8c037abae8788f6907a914f9214a9af3be7e

--- platform/lang-impl/src/com/intellij/psi/search/MappedFileTypeIndex.java
+++ platform/lang-impl/src/com/intellij/psi/search/MappedFileTypeIndex.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.search;
 
 import com.intellij.diagnostic.LoadingState;
@@ -287,6 +287,7 @@ public final class MappedFileTypeIndex extends FileTypeIndexImplBase {
       if (data != 0) {
         myInvertedIndex.computeIfAbsent(data, __ -> createContainerForInvertedIndex()).add(inputId);
       }
+      //TODO RC: should we do the notification under the lock?
       triggerOnInvertedIndexChangeCallback(data, indexedData);
     }
 
