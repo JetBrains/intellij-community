829ca1cf70a58245d3d9f0233c8cdbebb96c3f9a
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-06-11 09:08:56 +0200

    IJPL-797 `intellij.platform.jps.model.impl` review internal API
    Reduce visibility and mark as internal elements which aren't supposed to be used from plugins. This is also needed to migrate the JPS model to delegate to the workspace model (IJPL-409).
    
    GitOrigin-RevId: 8e2db16140a23f4ba073235f46f48a338a6c2292

--- jps/model-impl/src/org/jetbrains/jps/model/java/impl/JavaSdkUtil.java
+++ jps/model-impl/src/org/jetbrains/jps/model/java/impl/JavaSdkUtil.java
@@ -16,6 +16,9 @@ import java.util.List;
 import java.util.Set;
 
 public final class JavaSdkUtil {
+  private JavaSdkUtil() {
+  }
+
   public static @NotNull List<Path> getJdkClassesRoots(@NotNull Path home, boolean isJre) {
     Path[] jarDirs;
     Path fileName = home.getFileName();
