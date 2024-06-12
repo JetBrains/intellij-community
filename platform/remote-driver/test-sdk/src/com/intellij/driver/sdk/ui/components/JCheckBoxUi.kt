631a0de8019d07ef0516aca732e6e3b0a6d2be5f
Lev Zagnetin <lev.zagnetin@jetbrains.com>
2024-06-12 12:42:47 +0300

    [driver][ui-tests] Add getText() method for JCheckBox
    (cherry picked from commit cccceb62f1150acf59e40519671571d1e59029f2)
    
    GitOrigin-RevId: a7bd33031b3114c6e9b8e9c5ff4819101fe659cd

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/JCheckBoxUi.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/JCheckBoxUi.kt
@@ -14,20 +14,27 @@ fun Finder.checkBox(locator: QueryBuilder.() -> String) = x(JCheckBoxUi::class.j
 class JCheckBoxUi(data: ComponentData) : UiComponent(data) {
   private val checkboxComponent by lazy { driver.cast(component, JCheckBox::class) }
 
+  val text by lazy {
+    checkboxComponent.getText()
+  }
+
   fun check() {
     if (!isSelected()) {
       click()
     }
   }
+
   fun uncheck() {
     if (isSelected()) {
       click()
     }
   }
+
   fun isSelected() = checkboxComponent.isSelected()
 }
 
 @Remote("javax.swing.JCheckBox", plugin = REMOTE_ROBOT_MODULE_ID)
 interface JCheckBox {
   fun isSelected(): Boolean
+  fun getText(): String
 }
