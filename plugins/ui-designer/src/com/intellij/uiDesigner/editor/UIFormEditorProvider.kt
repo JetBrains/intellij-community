7a4e2527e5b2d184ce34319fabece8cb03a2f575
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-06-12 12:47:56 +0200

    IJPL-796 refactor UIFormEditorProvider
    GitOrigin-RevId: 06227aca8368db3dd2a53cc4be405e7451c9886b

--- plugins/ui-designer/src/com/intellij/uiDesigner/editor/UIFormEditorProvider.kt
+++ plugins/ui-designer/src/com/intellij/uiDesigner/editor/UIFormEditorProvider.kt
@@ -1,56 +1,38 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.uiDesigner.editor;
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.uiDesigner.editor
 
-import com.intellij.openapi.application.AccessToken;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.fileEditor.FileEditor;
-import com.intellij.openapi.fileEditor.FileEditorPolicy;
-import com.intellij.openapi.fileEditor.FileEditorProvider;
-import com.intellij.openapi.fileEditor.FileEditorState;
-import com.intellij.openapi.fileTypes.FileTypeRegistry;
-import com.intellij.openapi.module.ModuleUtilCore;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.testFramework.LightVirtualFile;
-import com.intellij.uiDesigner.GuiFormFileType;
-import com.intellij.util.ArrayUtilRt;
-import com.intellij.util.SlowOperations;
-import org.jdom.Element;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.fileEditor.FileEditor
+import com.intellij.openapi.fileEditor.FileEditorPolicy
+import com.intellij.openapi.fileEditor.FileEditorProvider
+import com.intellij.openapi.fileEditor.FileEditorState
+import com.intellij.openapi.fileTypes.FileTypeRegistry
+import com.intellij.openapi.module.ModuleUtilCore
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.testFramework.LightVirtualFile
+import com.intellij.uiDesigner.GuiFormFileType
+import com.intellij.util.ArrayUtilRt
+import com.intellij.util.SlowOperations
+import org.jdom.Element
 
-final class UIFormEditorProvider implements FileEditorProvider, DumbAware {
-  private static final Logger LOG = Logger.getInstance(UIFormEditorProvider.class);
-
-  @Override
-  public boolean accept(final @NotNull Project project, final @NotNull VirtualFile file){
-    try (AccessToken ignore = SlowOperations.knownIssue("IDEA-307701, EA-762786")) {
-      return
-        FileTypeRegistry.getInstance().isFileOfType(file, GuiFormFileType.INSTANCE) &&
-        !GuiFormFileType.INSTANCE.isBinary() &&
-        (ModuleUtilCore.findModuleForFile(file, project) != null || file instanceof LightVirtualFile);
+private class UIFormEditorProvider : FileEditorProvider, DumbAware {
+  override fun accept(project: Project, file: VirtualFile): Boolean {
+    SlowOperations.knownIssue("IDEA-307701, EA-762786").use {
+      return FileTypeRegistry.getInstance().isFileOfType(file, GuiFormFileType.INSTANCE) &&
+             !GuiFormFileType.INSTANCE.isBinary &&
+             (ModuleUtilCore.findModuleForFile(file, project) != null || file is LightVirtualFile)
     }
   }
 
-  @Override
-  public @NotNull FileEditor createEditor(final @NotNull Project project, final @NotNull VirtualFile file){
-    LOG.assertTrue(accept(project, file));
-    return new UIFormEditor(project, file);
-  }
+  override fun createEditor(project: Project, file: VirtualFile): FileEditor = UIFormEditor(project, file)
 
-  @Override
-  public @NotNull FileEditorState readState(@NotNull Element element, final @NotNull Project project, final @NotNull VirtualFile file){
+  override fun readState(element: Element, project: Project, file: VirtualFile): FileEditorState {
     //TODO[anton,vova] implement
-    return new MyEditorState(-1, ArrayUtilRt.EMPTY_STRING_ARRAY);
+    return MyEditorState(-1, ArrayUtilRt.EMPTY_STRING_ARRAY)
   }
 
-  @Override
-  public @NotNull String getEditorTypeId(){
-    return "ui-designer";
-  }
+  override fun getEditorTypeId(): String = "ui-designer"
 
-  @Override
-  public @NotNull FileEditorPolicy getPolicy() {
-    return FileEditorPolicy.HIDE_DEFAULT_EDITOR;
-  }
+  override fun getPolicy(): FileEditorPolicy = FileEditorPolicy.HIDE_DEFAULT_EDITOR
 }
