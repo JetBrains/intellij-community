cf7dbf817a10fac80c9424505a1b9105b8b88f9a
Yann Cébron <yann.cebron@jetbrains.com>
2024-06-12 16:42:28 +0200

    [dev] PsiViewerExtension: javadoc
    GitOrigin-RevId: 8ebc44d94d45bb6eb4fb59331dbb4ad51a1656a0

--- plugins/dev/intellij.dev.psiViewer/src/PsiViewerExtension.java
+++ plugins/dev/intellij.dev.psiViewer/src/PsiViewerExtension.java
@@ -1,17 +1,22 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.dev.psiViewer;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
+/**
+ * Allows extending PSI Viewer dialog (<em>Tools | Show PSI Structure</em>) for language-specific variants.
+ */
 public interface PsiViewerExtension {
 
+  @ApiStatus.Internal
   ExtensionPointName<PsiViewerExtension> EP_NAME = ExtensionPointName.create("com.intellij.dev.psiViewer.extension");
 
   @Nls(capitalization = Nls.Capitalization.Title)
