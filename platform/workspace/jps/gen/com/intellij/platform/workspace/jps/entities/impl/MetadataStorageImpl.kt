bf1496b7d6fd7919cdb136d3b5c028b280e118ab
Mikhail Mazurkevich <Mikhail.Mazurkevich@jetbrains.com>
2024-06-12 21:56:15 +0400

    [workspace model] Regenerate entities
    GitOrigin-RevId: 0dcde6804277fef014d5af4d6d80bfb0d6e3a7fe

---
+++ platform/workspace/jps/gen/com/intellij/platform/workspace/jps/entities/impl/MetadataStorageImpl.kt
@@ -0,0 +1,7 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.workspace.jps.entities.impl
+
+import com.intellij.java.workspace.entities.impl.MetadataStorageImpl
+import com.intellij.platform.workspace.storage.metadata.MetadataStorageBridge
+
+internal object MetadataStorageImpl: MetadataStorageBridge(MetadataStorageImpl)
