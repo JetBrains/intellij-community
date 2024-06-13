6d045d067db34651be76f461230aef47fd6ffcf1
Mikhail Mazurkevich <Mikhail.Mazurkevich@jetbrains.com>
2024-06-13 22:18:19 +0400

    [workspace model] IJPL-797 `intellij.platform.workspace.storage` review internal API (hide `MetadataGenerator` from API)
    GitOrigin-RevId: f870934da432532a84935767bc77d0f950447578

--- platform/workspace/storage/src/com/intellij/platform/workspace/storage/impl/MetadataGenerator.kt
+++ platform/workspace/storage/src/com/intellij/platform/workspace/storage/impl/MetadataGenerator.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.workspace.storage.metadata.utils
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.workspace.storage.impl
 
 import com.intellij.platform.workspace.storage.metadata.model.*
 import java.io.File
