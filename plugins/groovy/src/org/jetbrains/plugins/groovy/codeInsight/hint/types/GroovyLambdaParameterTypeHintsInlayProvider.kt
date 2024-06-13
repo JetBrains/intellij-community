fec028fd33c4e54c9db07942c3c96a6f0054c29c
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2024-06-13 10:32:33 +0300

    [Groovy] Replace usage of deprecated InlayTreeSink#addPresentation to the newer overload
    IDEA-351784
    
    GitOrigin-RevId: 206bf17622a4a7d28b4c31ab0266b5ed42383ff2

--- plugins/groovy/src/org/jetbrains/plugins/groovy/codeInsight/hint/types/GroovyLambdaParameterTypeHintsInlayProvider.kt
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/codeInsight/hint/types/GroovyLambdaParameterTypeHintsInlayProvider.kt
@@ -39,7 +39,7 @@ class GroovyLambdaParameterTypeHintsInlayProvider : InlayHintsProvider {
         sink: InlayTreeSink,
         suffixText: String) {
         val type = getRepresentableType(parameter) ?: return
-        sink.addPresentation(InlineInlayPosition(offset, relatedToPrevious = true), hasBackground = true) {
+        sink.addPresentation(InlineInlayPosition(offset, relatedToPrevious = true), null, null, HintColorKind.Default) {
           JavaTypeHintsFactory.typeHint(type, this)
           text(suffixText)
         }
