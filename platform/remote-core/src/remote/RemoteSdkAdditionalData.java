119116b38d3bdf2b6c759c9e6fea8a8ff3de619f
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-06-13 14:56:51 +0200

    Cleanup (dead code; typos; formatting)
    GitOrigin-RevId: 25c904dd8109ce3dde7e2fc029028339a19ed353

--- platform/remote-core/src/remote/RemoteSdkAdditionalData.java
+++ platform/remote-core/src/remote/RemoteSdkAdditionalData.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.remote;
 
 import com.intellij.openapi.projectRoots.SdkAdditionalData;
@@ -13,7 +13,7 @@ public interface RemoteSdkAdditionalData<T extends RemoteSdkCredentials>
 
   <C> void setCredentials(Key<C> key, C credentials);
 
-  CredentialsType getRemoteConnectionType();
+  CredentialsType<?> getRemoteConnectionType();
 
-  void switchOnConnectionType(CredentialsCase... cases);
+  void switchOnConnectionType(CredentialsCase<?>... cases);
 }
