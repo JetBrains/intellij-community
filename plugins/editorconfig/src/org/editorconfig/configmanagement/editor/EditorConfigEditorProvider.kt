7cfd052dac1713e637e3a7c3b3c76d2177b52c0b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-06-13 18:43:00 +0200

    IJPL-796 EditorConfigEditorProvider - implement async API
    GitOrigin-RevId: 44bbc6bce0d3c04afa65635a2d0f47868ca5201b

--- plugins/editorconfig/src/org/editorconfig/configmanagement/editor/EditorConfigEditorProvider.kt
+++ plugins/editorconfig/src/org/editorconfig/configmanagement/editor/EditorConfigEditorProvider.kt
@@ -3,6 +3,9 @@ package org.editorconfig.configmanagement.editor
 
 import com.intellij.application.options.CodeStyle
 import com.intellij.lang.Language
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.readAction
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.ex.EditorEx
@@ -22,13 +25,15 @@ import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.codeStyle.CodeStyleSettingsManager
 import com.intellij.psi.codeStyle.LanguageCodeStyleSettingsProvider
+import com.intellij.util.concurrency.annotations.RequiresEdt
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.editorconfig.configmanagement.editor.EditorConfigEditorProvider.Companion.MAX_PREVIEW_LENGTH
 import org.editorconfig.configmanagement.editor.EditorConfigEditorProvider.Companion.getLanguage
 import org.editorconfig.language.filetype.EditorConfigFileType
 import org.editorconfig.settings.EditorConfigSettings
 import java.io.IOException
-
-private val mainEditorProvider = PsiAwareTextEditorProvider()
 
 internal class EditorConfigEditorProvider : AsyncFileEditorProvider {
   companion object {
@@ -40,50 +45,90 @@ internal class EditorConfigEditorProvider : AsyncFileEditorProvider {
     }
   }
 
-  override fun createEditorAsync(project: Project, file: VirtualFile): AsyncFileEditorProvider.Builder = MyEditorBuilder(project, file)
+  override suspend fun createFileEditor(
+    project: Project,
+    file: VirtualFile,
+    document: Document?,
+    editorCoroutineScope: CoroutineScope,
+  ): FileEditor {
+    val precomputedState = EditorBuilderState(
+      encodings = readAction { EditorConfigStatusListener.extractEncodings(project, file) },
+      contextFile = project.serviceAsync<EditorConfigPreviewManager>().getAssociatedPreviewFile(file),
+    )
+
+    val result: FileEditor
+    val contextFile = precomputedState.contextFile
+    if (contextFile != null && CodeStyle.getSettings(project).getCustomSettings(EditorConfigSettings::class.java).ENABLED) {
+      withContext(Dispatchers.EDT) {
+        @Suppress("NAME_SHADOWING") val document = EditorFactory.getInstance().createDocument(getPreviewText(contextFile))
+        val disposable = Disposer.newDisposable()
+        val previewFile = EditorConfigPreviewFile(project, contextFile, document, disposable)
+        val previewEditor = createPreviewEditor(document = document, previewFile = previewFile, project = project)
+        val ecTextEditor = TextEditorProvider.getInstance().createEditor(project, file) as TextEditor
+        result = EditorConfigEditorWithPreview(file, project, ecTextEditor, previewEditor)
+        Disposer.register(result, disposable)
+      }
+    }
+    else {
+      result = PsiAwareTextEditorProvider().createFileEditor(project, file, document, editorCoroutineScope)
+    }
+    CodeStyleSettingsManager.getInstance(project).subscribe(EditorConfigStatusListener(project, file, precomputedState.encodings), result)
+    return result
+  }
 
   override fun accept(project: Project, file: VirtualFile): Boolean {
     return FileTypeRegistry.getInstance().isFileOfType(file, EditorConfigFileType)
   }
 
-  override fun createEditor(project: Project, file: VirtualFile): FileEditor = MyEditorBuilder(project, file).build()
+  override fun createEditor(project: Project, file: VirtualFile): FileEditor {
+    return build(
+      project = project,
+      file = file,
+      precomputedState = EditorBuilderState(
+        encodings = EditorConfigStatusListener.extractEncodings(project, file),
+        contextFile = EditorConfigPreviewManager.getInstance(project).getAssociatedPreviewFile(file),
+      ),
+    )
+  }
 
   override fun getEditorTypeId(): String = EDITOR_TYPE_ID
 
   override fun getPolicy(): FileEditorPolicy = FileEditorPolicy.HIDE_DEFAULT_EDITOR
 }
 
-private class MyEditorBuilder(val project: Project, val file: VirtualFile) : AsyncFileEditorProvider.Builder() {
-  private val encodings: Set<String> = EditorConfigStatusListener.extractEncodings(project, file)
+private class EditorBuilderState(
+  @JvmField val encodings: Set<String>,
+  @JvmField val contextFile: VirtualFile?,
+)
 
-  override fun build(): FileEditor {
-    val result: FileEditor
-    val contextFile = EditorConfigPreviewManager.getInstance(project).getAssociatedPreviewFile(file)
-    if (contextFile != null && CodeStyle.getSettings(project).getCustomSettings(EditorConfigSettings::class.java).ENABLED) {
-      val document = EditorFactory.getInstance().createDocument(getPreviewText(contextFile))
-      val disposable = Disposer.newDisposable()
-      val previewFile = EditorConfigPreviewFile(project, contextFile, document, disposable)
-      val previewEditor = createPreviewEditor(document, previewFile)
-      val ecTextEditor = TextEditorProvider.getInstance().createEditor(project, file) as TextEditor
-      result = EditorConfigEditorWithPreview(file, project, ecTextEditor, previewEditor)
-      Disposer.register(result, disposable)
-    }
-    else {
-      result = mainEditorProvider.createEditor(project, file)
-    }
-    val statusListener = EditorConfigStatusListener(project, file, encodings)
-    CodeStyleSettingsManager.getInstance(project).subscribe(statusListener, result)
-    return result
+@RequiresEdt
+private fun build(project: Project, file: VirtualFile, precomputedState: EditorBuilderState): FileEditor {
+  val result: FileEditor
+  val contextFile = precomputedState.contextFile
+  if (contextFile != null && CodeStyle.getSettings(project).getCustomSettings(EditorConfigSettings::class.java).ENABLED) {
+    val document = EditorFactory.getInstance().createDocument(getPreviewText(contextFile))
+    val disposable = Disposer.newDisposable()
+    val previewFile = EditorConfigPreviewFile(project, contextFile, document, disposable)
+    val previewEditor = createPreviewEditor(document = document, previewFile = previewFile, project = project)
+    val ecTextEditor = TextEditorProvider.getInstance().createEditor(project, file) as TextEditor
+    result = EditorConfigEditorWithPreview(file, project, ecTextEditor, previewEditor)
+    Disposer.register(result, disposable)
   }
+  else {
+    result = PsiAwareTextEditorProvider().createEditor(project, file)
+  }
+  val statusListener = EditorConfigStatusListener(project, file, precomputedState.encodings)
+  CodeStyleSettingsManager.getInstance(project).subscribe(statusListener, result)
+  return result
+}
 
-  fun createPreviewEditor(document: Document, previewFile: EditorConfigPreviewFile): FileEditor {
-    val previewEditor = EditorFactory.getInstance().createEditor(document, project)
-    if (previewEditor is EditorEx) {
-      val highlighter = EditorHighlighterFactory.getInstance().createEditorHighlighter(project, previewFile)
-      previewEditor.highlighter = highlighter
-    }
-    return EditorConfigPreviewFileEditor(previewEditor, previewFile)
+private fun createPreviewEditor(document: Document, previewFile: EditorConfigPreviewFile, project: Project): FileEditor {
+  val previewEditor = EditorFactory.getInstance().createEditor(document, project)
+  if (previewEditor is EditorEx) {
+    val highlighter = EditorHighlighterFactory.getInstance().createEditorHighlighter(project, previewFile)
+    previewEditor.highlighter = highlighter
   }
+  return EditorConfigPreviewFileEditor(previewEditor, previewFile)
 }
 
 private const val EDITOR_TYPE_ID = "org.editorconfig.configmanagement.editor"
