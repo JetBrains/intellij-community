7d90cf9d3397a627465a8804305ba7ff2ed1d2dc
Mikhail Mazurkevich <Mikhail.Mazurkevich@jetbrains.com>
2024-06-13 08:13:19 +0400

    [workspace model] Update minor codegen version
    GitOrigin-RevId: e54b5b6d2bbb8611efa1e54e68a9275ee8e7e724

--- platform/workspace/storage/src/com/intellij/platform/workspace/storage/generatedCodeCompatibility.kt
+++ platform/workspace/storage/src/com/intellij/platform/workspace/storage/generatedCodeCompatibility.kt
@@ -9,7 +9,7 @@ public object CodeGeneratorVersions {
   /** these constants are accessed from WorkspaceImplObsoleteInspection */
   private const val API_VERSION_INTERNAL = 3
   private const val IMPL_MAJOR_VERSION_INTERNAL = 5
-  private const val IMPL_MINOR_VERSION_INTERNAL = 20
+  private const val IMPL_MINOR_VERSION_INTERNAL = 21
 
   @set:TestOnly
   public var API_VERSION: Int = API_VERSION_INTERNAL
