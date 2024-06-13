5ee5610922408dac0f8c27f617e726c45b51fb66
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2024-06-14 01:53:47 +0400

    [kotlin] Introduce `ConvertStringToCharLiteralUtils`
    to share the `prepareCharLiteral` function between K1 and K2 implementations of ConvertStringToCharLiteralFix.
    
    ^KTIJ-29624
    
    GitOrigin-RevId: 1ce9c7ea4a7bd4cc90b7ec5feac6034a0a234816

---
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/ConvertStringToCharLiteralUtils.kt
@@ -0,0 +1,21 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.quickfix
+
+import org.jetbrains.kotlin.psi.*
+
+
+object ConvertStringToCharLiteralUtils {
+
+    fun prepareCharLiteral(element: KtStringTemplateExpression): KtConstantExpression? {
+        val stringTemplateEntry = element.applicableEntry ?: return null
+        return stringTemplateEntry.charLiteral()
+    }
+}
+
+private fun KtStringTemplateEntry.charLiteral(): KtConstantExpression? {
+    val text = text.replace("'", "\\'").replace("\\\"", "\"")
+    return KtPsiFactory(project).createExpression("'$text'") as? KtConstantExpression
+}
+
+private val KtStringTemplateExpression.applicableEntry: KtStringTemplateEntry?
+    get() = entries.singleOrNull().takeIf { it is KtLiteralStringTemplateEntry || it is KtEscapeStringTemplateEntry }
