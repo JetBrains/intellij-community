567e0b0d051b453c6075d69c39e6127b85a6f709
Tagir Valeev <tagir.valeev@jetbrains.com>
2024-06-14 15:31:34 +0200

    [java-intentions] IDEA-270206 Nullability annotation at type parameter/wildcard: add a quick-fix to move it to the bound
    GitOrigin-RevId: e8e56c2d27a77414045064c784f48ff712ad0d2d

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/impl/quickfix/AnnotateMethodTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/impl/quickfix/AnnotateMethodTest.java
@@ -1,13 +1,19 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.daemon.impl.quickfix;
 
+import com.intellij.codeInsight.Nullability;
 import com.intellij.codeInsight.NullableNotNullManager;
+import com.intellij.codeInsight.annoPackages.AnnotationPackageSupport;
 import com.intellij.codeInsight.daemon.quickFix.LightQuickFixParameterizedTestCase;
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.nullable.NullableStuffInspection;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.pom.java.LanguageLevel;
+import com.intellij.testFramework.ServiceContainerUtil;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class AnnotateMethodTest extends LightQuickFixParameterizedTestCase {
   @Override
@@ -19,16 +25,28 @@ public class AnnotateMethodTest extends LightQuickFixParameterizedTestCase {
   protected void setUp() throws Exception {
     super.setUp();
     if (getTestName(false).contains("TypeUse")) {
+      AnnotationPackageSupport mySupport = new AnnotationPackageSupport() {
+        @Override
+        public @NotNull List<String> getNullabilityAnnotations(@NotNull Nullability nullability) {
+          return switch (nullability) {
+            case NOT_NULL -> List.of("typeUse.NotNull");
+            case NULLABLE -> List.of("typeUse.Nullable");
+            case UNKNOWN -> List.of();
+          };
+        }
+
+        @Override
+        public boolean isTypeUseAnnotationLocationRestricted() {
+          return true;
+        }
+      }; 
+      ServiceContainerUtil.registerExtension(ApplicationManager.getApplication(), AnnotationPackageSupport.EP_NAME, mySupport, getTestRootDisposable());
       NullableNotNullManager nnnManager = NullableNotNullManager.getInstance(getProject());
       String prevNullable = nnnManager.getDefaultNullable();
       String prevNotNull = nnnManager.getDefaultNotNull();
-      nnnManager.setNotNulls("typeUse.NotNull");
-      nnnManager.setNullables("typeUse.Nullable");
       nnnManager.setDefaultNotNull("typeUse.NotNull");
       nnnManager.setDefaultNullable("typeUse.Nullable");
       Disposer.register(getTestRootDisposable(), () -> {
-        nnnManager.setNotNulls();
-        nnnManager.setNullables();
         nnnManager.setDefaultNotNull(prevNotNull);
         nnnManager.setDefaultNullable(prevNullable);
       });
