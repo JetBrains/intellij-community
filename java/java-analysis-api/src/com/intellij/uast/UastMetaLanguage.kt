6334491a9325968c5383890976472e684e5c5fb0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-06-14 13:09:09 +0200

    refactor UastMetaLanguage
    GitOrigin-RevId: c619a349e34f476351deeba9f92c7019c6919601

--- java/java-analysis-api/src/com/intellij/uast/UastMetaLanguage.kt
+++ java/java-analysis-api/src/com/intellij/uast/UastMetaLanguage.kt
@@ -1,53 +1,43 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.uast;
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.uast
 
-import com.intellij.java.analysis.JavaAnalysisBundle;
-import com.intellij.lang.Language;
-import com.intellij.lang.MetaLanguage;
-import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Unmodifiable;
-import org.jetbrains.uast.UastLanguagePlugin;
+import com.intellij.java.analysis.JavaAnalysisBundle
+import com.intellij.lang.Language
+import com.intellij.lang.MetaLanguage
+import com.intellij.util.containers.ContainerUtil
+import org.jetbrains.annotations.Unmodifiable
+import org.jetbrains.uast.UastLanguagePlugin
+import org.jetbrains.uast.UastLanguagePlugin.Companion.extensionPointName
+import org.jetbrains.uast.UastLanguagePlugin.Companion.getInstances
+import kotlin.concurrent.Volatile
 
-import java.util.Collection;
-import java.util.Set;
+class UastMetaLanguage private constructor() : MetaLanguage("UAST") {
+  internal object Holder {
+    @Volatile
+    var myLanguages: @Unmodifiable MutableSet<Language> = initLanguages()
 
-public final class UastMetaLanguage extends MetaLanguage {
-  static final class Holder {
-    private static volatile @Unmodifiable Set<Language> myLanguages = initLanguages();
-
-    static {
-      UastLanguagePlugin.Companion.getExtensionPointName().addChangeListener(() -> myLanguages = initLanguages(), null);
+    init {
+      extensionPointName.addChangeListener(
+        { myLanguages = initLanguages() }, null)
     }
 
-    @NotNull
-    private static @Unmodifiable Set<Language> initLanguages() {
-      return Set.of(ContainerUtil.map2Array(UastLanguagePlugin.Companion.getInstances(), Language.EMPTY_ARRAY, plugin -> plugin.getLanguage()));
+    private fun initLanguages(): @Unmodifiable MutableSet<Language> {
+      return java.util.Set.of(*ContainerUtil.map2Array(getInstances(), EMPTY_ARRAY) { plugin: UastLanguagePlugin -> plugin.language })
     }
 
-    @NotNull
-    static Set<Language> getLanguages() {
-      return myLanguages;
-    }
+    val languages: Set<Language>
+      get() = myLanguages
   }
 
-  private UastMetaLanguage() {
-    super("UAST");
+  override fun matchesLanguage(language: Language): Boolean {
+    return Holder.myLanguages.contains(language)
   }
 
-  @Override
-  public boolean matchesLanguage(@NotNull Language language) {
-    return Holder.myLanguages.contains(language);
+  override fun getMatchingLanguages(): Collection<Language> {
+    return Holder.myLanguages
   }
 
-  @NotNull
-  @Override
-  public Collection<Language> getMatchingLanguages() {
-    return Holder.myLanguages;
-  }
-
-  @Override
-  public @NotNull String getDisplayName() {
-    return JavaAnalysisBundle.message("uast.language.display.name");
+  override fun getDisplayName(): String {
+    return JavaAnalysisBundle.message("uast.language.display.name")
   }
 }
