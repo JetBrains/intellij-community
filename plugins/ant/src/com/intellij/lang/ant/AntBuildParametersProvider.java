d04fc81a80289471983d6d77775159b3e02c2198
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-06-14 10:14:10 +0200

    [jps] IDEA-346188 Ant: Cannot build artifact using 2024.1
    Fix quotes for build parameter
    
    GitOrigin-RevId: 294e8a4c74b1e4cf4988a9622167df0b633632fe

--- plugins/ant/src/com/intellij/lang/ant/AntBuildParametersProvider.java
+++ plugins/ant/src/com/intellij/lang/ant/AntBuildParametersProvider.java
@@ -3,6 +3,7 @@ package com.intellij.lang.ant;
 
 import com.intellij.compiler.server.BuildProcessParametersProvider;
 import com.intellij.lang.ant.config.impl.GlobalAntConfiguration;
+import com.intellij.openapi.diagnostic.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -13,10 +14,12 @@ final class AntBuildParametersProvider extends BuildProcessParametersProvider {
   public @NotNull List<String> getVMArguments() {
     try {
       File bundledAntHome = GlobalAntConfiguration.getBundledAntHome();
-      return List.of("-Djps.bundled.ant.path=\"" + bundledAntHome.getAbsolutePath() + "\"");
+      return List.of("-Djps.bundled.ant.path=" + bundledAntHome.getAbsolutePath());
     }
     catch (IllegalStateException e) {
       // seems broken installation, try to work at least for non-bundled Ant
+      Logger.getInstance(AntBuildParametersProvider.class).warn("Unable to find bundled Ant for JPS build", e);
+
       return List.of();
     }
   }
