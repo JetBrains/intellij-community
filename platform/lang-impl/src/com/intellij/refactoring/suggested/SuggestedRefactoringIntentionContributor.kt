9cd97256f91cfdf4b3ba9681bdeb66f15dbbb98c
Andrey.Matveev <andrey.matveev@jetbrains.com>
2024-06-17 12:21:15 +0300

    PY-43781 Put SuggestedRefactoringIntentionContributor always to inspectionFixesToShow
    GitOrigin-RevId: ba76ef8a0107015d0e98c14a7098f841e5517299

--- platform/lang-impl/src/com/intellij/refactoring/suggested/SuggestedRefactoringIntentionContributor.kt
+++ platform/lang-impl/src/com/intellij/refactoring/suggested/SuggestedRefactoringIntentionContributor.kt
@@ -42,8 +42,7 @@ class SuggestedRefactoringIntentionContributor : IntentionMenuContributor {
 
     // we add it into 'errorFixesToShow' if it's not empty to always be at the top of the list
     // we don't add into it if it's empty to keep the color of the bulb
-    val collectionToAdd = intentions.errorFixesToShow.takeIf { it.isNotEmpty() }
-                          ?: intentions.inspectionFixesToShow
+    val collectionToAdd = intentions.inspectionFixesToShow
     collectionToAdd.add(HighlightInfo.IntentionActionDescriptor(intention, null, null, icon, null, null, null))
   }
 
