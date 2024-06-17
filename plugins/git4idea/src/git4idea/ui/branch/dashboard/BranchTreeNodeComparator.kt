c519ec4a36867aae7e929c88ca4ded4b59aef6b1
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2024-06-17 17:46:02 +0200

    [git] Simplify BranchTreeNodeComparator
    GitOrigin-RevId: 3c316944bb7a03f26545dc5bac8ff3917efc8349

--- plugins/git4idea/src/git4idea/ui/branch/dashboard/BranchTreeNodeComparator.kt
+++ plugins/git4idea/src/git4idea/ui/branch/dashboard/BranchTreeNodeComparator.kt
@@ -1,36 +1,27 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.ui.branch.dashboard
 
-internal object BranchTreeNodeComparator : Comparator<BranchNodeDescriptor> {
-  override fun compare(d1: BranchNodeDescriptor, d2: BranchNodeDescriptor): Int {
-    val weight1 = getOrderWeight(d1)
-    val weight2 = getOrderWeight(d2)
-    val weightDelta = weight1.compareTo(weight2)
-    if (weightDelta != 0) return weightDelta
+import com.intellij.openapi.util.text.NaturalComparator
 
-    val displayText1 = d1.getDisplayText()
-    val displayText2 = d2.getDisplayText()
-    if (displayText1 != null && displayText2 != null) {
-      return displayText1.compareTo(displayText2, ignoreCase = true)
-    }
-    if (displayText1 != null) return -1
-    if (displayText2 != null) return 1
+internal val BranchTreeNodeComparator = compareBy<BranchNodeDescriptor> {
+  getOrderWeight(it)
+} then compareBy(NaturalComparator.INSTANCE) {
+  it.getDisplayText()
+} then compareBy {
+  it.type
+}
 
-    return d1.type.compareTo(d2.type)
-  }
+private fun getOrderWeight(descriptor: BranchNodeDescriptor): Int {
+  val branchInfo = descriptor.branchInfo
 
-  private fun getOrderWeight(descriptor: BranchNodeDescriptor): Int {
-    val branchInfo = descriptor.branchInfo
+  val isCurrent = branchInfo != null && branchInfo.isCurrent
+  if (isCurrent) return 0
 
-    val isCurrent = branchInfo != null && branchInfo.isCurrent
-    if (isCurrent) return 0
+  val isFavorite = branchInfo != null && branchInfo.isFavorite
+  if (isFavorite) return 1
 
-    val isFavorite = branchInfo != null && branchInfo.isFavorite
-    if (isFavorite) return 1
+  val isGroupNode = descriptor.type == NodeType.GROUP_NODE
+  if (isGroupNode) return 2
 
-    val isGroupNode = descriptor.type == NodeType.GROUP_NODE
-    if (isGroupNode) return 2
-
-    return 3
-  }
+  return 3
 }
