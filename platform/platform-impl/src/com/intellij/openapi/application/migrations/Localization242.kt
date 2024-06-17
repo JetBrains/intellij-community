46adc16b456c57ed343e0409d707572f71978379
Vera Petrenkova <Vera.Petrenkova@jetbrains.com>
2024-06-17 12:30:24 +0200

    IJPL-150222 [i18n] fix l10n migration condition
    GitOrigin-RevId: d07e2a99d5f90cfbcf1ad76b3b6a19143a4d04ec

--- platform/platform-impl/src/com/intellij/openapi/application/migrations/Localization242.kt
+++ platform/platform-impl/src/com/intellij/openapi/application/migrations/Localization242.kt
@@ -9,7 +9,7 @@ import com.intellij.openapi.util.BuildNumber
 import java.nio.file.Path
 
 internal fun enableL10nIfPluginInstalled(previousVersion: String?, oldPluginsDir: Path, bundledPluginPath: Path?, brokenPluginVersions: Map<PluginId, Set<String>>?, compatibleBuildNumber: BuildNumber?) {
-  if (previousVersion == null || Version.fromString(previousVersion) > Version.fromString("2024.2")) return
+  if (previousVersion == null || Version.fromString(previousVersion) >= Version.fromString("2024.2")) return
   val loadedDescriptors = loadDescriptorsFromOtherIde(oldPluginsDir, bundledPluginPath, brokenPluginVersions, compatibleBuildNumber)
   val bundledL10nPluginsIds = LocalizationUtil.l10nPluginIdToLanguageTag.keys
   val l10nPluginId = loadedDescriptors.getIdMap().keys.firstOrNull { bundledL10nPluginsIds.contains(it.idString) }
