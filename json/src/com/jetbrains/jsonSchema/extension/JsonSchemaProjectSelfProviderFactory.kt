55780a7d30ddf718ddb072a8bc66cd3766f7ba0d
Nikita Katkov <nikita.katkov@jetbrains.com>
2024-06-17 19:02:16 +0200

    [json] IJPL-63554 Refactor json schema self provider
    Removed strange looking schema determination by bundled file name, split presentable name and id, simplified code
    
    GitOrigin-RevId: 92ffa6c906dcecf978321ab0b826df7b2bfe021f

--- json/src/com/jetbrains/jsonSchema/extension/JsonSchemaProjectSelfProviderFactory.kt
+++ json/src/com/jetbrains/jsonSchema/extension/JsonSchemaProjectSelfProviderFactory.kt
@@ -1,103 +1,83 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.jetbrains.jsonSchema.extension;
+package com.jetbrains.jsonSchema.extension
 
-import com.intellij.json.JsonBundle;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.jetbrains.jsonSchema.ide.JsonSchemaService;
-import com.jetbrains.jsonSchema.impl.JsonSchemaVersion;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.json.JsonBundle
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.jsonSchema.ide.JsonSchemaService
+import com.jetbrains.jsonSchema.impl.JsonSchemaVersion
+import org.jetbrains.annotations.Nls
 
-import java.util.Arrays;
-import java.util.List;
-
-public final class JsonSchemaProjectSelfProviderFactory implements JsonSchemaProviderFactory, DumbAware {
-  public static final int TOTAL_PROVIDERS = 3;
-  private static final String SCHEMA_JSON_FILE_NAME = "schema.json";
-  private static final String SCHEMA06_JSON_FILE_NAME = "schema06.json";
-  private static final String SCHEMA07_JSON_FILE_NAME = "schema07.json";
-
-  @Override
-  public @NotNull List<JsonSchemaFileProvider> getProviders(final @NotNull Project project) {
-    return Arrays
-      .asList(new MyJsonSchemaFileProvider(project, SCHEMA_JSON_FILE_NAME), new MyJsonSchemaFileProvider(project, SCHEMA06_JSON_FILE_NAME),
-              new MyJsonSchemaFileProvider(project, SCHEMA07_JSON_FILE_NAME));
+class JsonSchemaProjectSelfProviderFactory : JsonSchemaProviderFactory, DumbAware {
+  override fun getProviders(project: Project): List<JsonSchemaFileProvider> {
+    return listOf(
+      MyJsonSchemaFileProvider(project, BundledJsonSchemaInfo(JsonSchemaVersion.SCHEMA_4,
+                                                              "schema.json",
+                                                              "4",
+                                                              "http://json-schema.org/draft-04/schema")),
+      MyJsonSchemaFileProvider(project, BundledJsonSchemaInfo(JsonSchemaVersion.SCHEMA_6,
+                                                              "schema06.json",
+                                                              "6",
+                                                              "http://json-schema.org/draft-06/schema")),
+      MyJsonSchemaFileProvider(project, BundledJsonSchemaInfo(JsonSchemaVersion.SCHEMA_7,
+                                                              "schema07.json",
+                                                              "7",
+                                                              "http://json-schema.org/draft-07/schema")),
+      MyJsonSchemaFileProvider(project, BundledJsonSchemaInfo(JsonSchemaVersion.SCHEMA_2019_09,
+                                                              "schema201909.json",
+                                                              "2019-09",
+                                                              "https://json-schema.org/draft/2019-09/schema")),
+      MyJsonSchemaFileProvider(project, BundledJsonSchemaInfo(JsonSchemaVersion.SCHEMA_2020_12,
+                                                              "schema202012.json",
+                                                              "2020-12",
+                                                              "https://json-schema.org/draft/2020-12/schema"))
+    )
   }
 
-  public static final class MyJsonSchemaFileProvider implements JsonSchemaFileProvider {
-    private final @NotNull Project myProject;
-    private final @NotNull @Nls String myFileName;
+  data class BundledJsonSchemaInfo(
+    val version: JsonSchemaVersion,
+    val bundledResourceFileName: String,
+    val presentableSchemaId: @Nls String,
+    val remoteSourceUrl: String
+  )
 
-    public boolean isSchemaV4() {
-      return SCHEMA_JSON_FILE_NAME.equals(myFileName);
-    }
-    public boolean isSchemaV6() {
-      return SCHEMA06_JSON_FILE_NAME.equals(myFileName);
-    }
-    public boolean isSchemaV7() {
-      return SCHEMA07_JSON_FILE_NAME.equals(myFileName);
-    }
+  class MyJsonSchemaFileProvider(private val myProject: Project, private val myBundledSchema: BundledJsonSchemaInfo) : JsonSchemaFileProvider {
 
-    private MyJsonSchemaFileProvider(@NotNull Project project, @NotNull @Nls String fileName) {
-      myProject = project;
-      myFileName = fileName;
-    }
-
-    @Override
-    public boolean isAvailable(@NotNull VirtualFile file) {
-      if (myProject.isDisposed()) return false;
-      JsonSchemaService service = JsonSchemaService.Impl.get(myProject);
-      if (!service.isApplicableToFile(file)) return false;
-      JsonSchemaVersion schemaVersion = service.getSchemaVersion(file);
-      if (schemaVersion == null) return false;
-      return switch (schemaVersion) {
-        case SCHEMA_4 -> isSchemaV4();
-        case SCHEMA_6 -> isSchemaV6();
-        case SCHEMA_7 -> isSchemaV7();
-      };
+    override fun isAvailable(file: VirtualFile): Boolean {
+      if (myProject.isDisposed) return false
+      val service = JsonSchemaService.Impl.get(myProject)
+      if (!service.isApplicableToFile(file)) return false
+      val instanceSchemaVersion = service.getSchemaVersion(file)
+      if (instanceSchemaVersion == null) return false
+      return instanceSchemaVersion == myBundledSchema.version
     }
 
-    @Override
-    public JsonSchemaVersion getSchemaVersion() {
-      return isSchemaV4() ? JsonSchemaVersion.SCHEMA_4 : isSchemaV7() ? JsonSchemaVersion.SCHEMA_7 : JsonSchemaVersion.SCHEMA_6;
+    override fun getSchemaVersion(): JsonSchemaVersion {
+      return myBundledSchema.version
     }
 
-    @Override
-    public @NotNull String getName() {
-      return myFileName;
+    override fun getSchemaFile(): VirtualFile? {
+      return JsonSchemaProviderFactory.getResourceFile(
+        JsonSchemaProjectSelfProviderFactory::class.java,
+        "/jsonSchema/${myBundledSchema.bundledResourceFileName}"
+      )
     }
 
-    @Override
-    public @Nullable VirtualFile getSchemaFile() {
-      return JsonSchemaProviderFactory.getResourceFile(JsonSchemaProjectSelfProviderFactory.class, "/jsonSchema/" + myFileName);
+    override fun getSchemaType(): SchemaType {
+      return SchemaType.schema
     }
 
-    @Override
-    public @NotNull SchemaType getSchemaType() {
-      return SchemaType.schema;
+    override fun getRemoteSource(): String {
+      return myBundledSchema.remoteSourceUrl
     }
 
-    @Override
-    public @Nullable String getRemoteSource() {
-      return switch (myFileName) {
-        case SCHEMA_JSON_FILE_NAME -> "http://json-schema.org/draft-04/schema";
-        case SCHEMA06_JSON_FILE_NAME -> "http://json-schema.org/draft-06/schema";
-        case SCHEMA07_JSON_FILE_NAME -> "http://json-schema.org/draft-07/schema";
-        default -> null;
-      };
+    override fun getPresentableName(): String {
+      return JsonBundle.message("schema.of.version", myBundledSchema.presentableSchemaId)
     }
 
-    @Override
-    public @NotNull String getPresentableName() {
-      return switch (myFileName) {
-        case SCHEMA_JSON_FILE_NAME -> JsonBundle.message("schema.of.version", 4);
-        case SCHEMA06_JSON_FILE_NAME -> JsonBundle.message("schema.of.version", 6);
-        case SCHEMA07_JSON_FILE_NAME -> JsonBundle.message("schema.of.version", 7);
-        default -> getName();
-      };
+    override fun getName(): String {
+      return presentableName
     }
   }
 }
