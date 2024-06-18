419cc440fa1ddb41baf3f643c4dcb2e3c1a2fa3c
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2024-06-18 15:17:32 +0200

    [R-1508] refactor SaveOutputAction
    (cherry picked from commit 6e5dd7cde70ae93bcd2f2786930ba8c3742fe6c6)
    
    IJ-MR-137158
    
    GitOrigin-RevId: 4149715646b0eff5e8fb178e2977d6edf77fc45b

--- notebooks/visualization/src/org/jetbrains/plugins/notebooks/visualization/r/inlays/components/InlayOutputToolbarActions.kt
+++ notebooks/visualization/src/org/jetbrains/plugins/notebooks/visualization/r/inlays/components/InlayOutputToolbarActions.kt
@@ -4,10 +4,35 @@
 
 package org.jetbrains.plugins.notebooks.visualization.r.inlays.components
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformCoreDataKeys
+import com.intellij.openapi.project.DumbAwareAction
+
 import org.jetbrains.plugins.notebooks.visualization.r.ui.DumbAwareActionAdapter
 
 class ClearOutputAction : DumbAwareActionAdapter()
 
-class SaveOutputAction : DumbAwareActionAdapter()
+internal class SaveOutputAction : DumbAwareAction() {
+  override fun update(e: AnActionEvent) {
+    val toolbarPane = getToolbarPaneOrNull(e)
+    e.presentation.isEnabledAndVisible = toolbarPane != null
+  }
+
+  override fun getActionUpdateThread(): ActionUpdateThread =
+    ActionUpdateThread.EDT
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val toolbarPane = getToolbarPaneOrNull(e) ?: return
+    toolbarPane.inlayOutput.saveAs()
+  }
+
+  private fun getToolbarPaneOrNull(e: AnActionEvent): ToolbarPane? =
+    e.getData(PlatformCoreDataKeys.CONTEXT_COMPONENT) as? ToolbarPane
+
+  companion object {
+    const val ID = "org.jetbrains.plugins.notebooks.visualization.r.inlays.components.SaveOutputAction"
+  }
+}
 
 class CopyImageToClipboardAction : DumbAwareActionAdapter()
