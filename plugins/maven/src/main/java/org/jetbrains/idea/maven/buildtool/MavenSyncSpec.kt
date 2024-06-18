7e9f87b0f728a7e9f47b58e84631000fd7943bda
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2024-06-18 22:01:50 +0200

    [maven] IDEA-355046 convert MavenSyncSpec to kotlin
    GitOrigin-RevId: 65655ec037774ebc47e2b7641adb7a59448d3d1b

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/buildtool/MavenSyncSpec.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/buildtool/MavenSyncSpec.kt
@@ -1,63 +1,47 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.idea.maven.buildtool;
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.idea.maven.buildtool
 
-import com.intellij.openapi.util.registry.Registry;
-import org.jetbrains.annotations.ApiStatus;
+import com.intellij.openapi.util.registry.Registry
+import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.NonExtendable
 @ApiStatus.Experimental
-public interface MavenSyncSpec {
-  static MavenSyncSpec incremental(String description) {
-    return incremental(description, false);
+interface MavenSyncSpec {
+  fun forceReading(): Boolean
+
+  fun resolveIncrementally(): Boolean
+
+  val isExplicit: Boolean
+
+  companion object {
+    @JvmStatic
+    @JvmOverloads
+    fun incremental(description: String, explicit: Boolean = false): MavenSyncSpec {
+      return MavenSyncSpecImpl(true, explicit, description)
+    }
+
+    @JvmStatic
+    @JvmOverloads
+    fun full(description: String, explicit: Boolean = false): MavenSyncSpec {
+      return MavenSyncSpecImpl(false, explicit, description)
+    }
   }
-
-  static MavenSyncSpec incremental(String description, boolean explicit) {
-    return new MavenSyncSpecImpl(true, explicit, description);
-  }
-
-  static MavenSyncSpec full(String description) {
-    return full(description, false);
-  }
-
-  static MavenSyncSpec full(String description, boolean explicit) {
-    return new MavenSyncSpecImpl(false, explicit, description);
-  }
-
-  boolean forceReading();
-
-  boolean resolveIncrementally();
-
-  boolean isExplicit();
 }
 
-class MavenSyncSpecImpl implements MavenSyncSpec {
-  private final boolean incremental;
-  private final boolean explicit;
-  private final String description;
-
-  MavenSyncSpecImpl(boolean incremental, boolean explicit, String description) {
-    this.incremental = incremental;
-    this.explicit = explicit;
-    this.description = description;
+internal class MavenSyncSpecImpl(
+  private val incremental: Boolean,
+  override val isExplicit: Boolean,
+  private val description: String
+) : MavenSyncSpec {
+  override fun forceReading(): Boolean {
+    return !incremental
   }
 
-  @Override
-  public boolean forceReading() {
-    return !incremental;
+  override fun resolveIncrementally(): Boolean {
+    return incremental && Registry.`is`("maven.incremental.sync.resolve.dependencies.incrementally")
   }
 
-  @Override
-  public boolean resolveIncrementally() {
-    return incremental && Registry.is("maven.incremental.sync.resolve.dependencies.incrementally");
-  }
-
-  @Override
-  public boolean isExplicit() {
-    return explicit;
-  }
-
-  @Override
-  public String toString() {
-    return "incremental=" + incremental + ", " + description;
+  override fun toString(): String {
+    return "incremental=$incremental, $description"
   }
 }
