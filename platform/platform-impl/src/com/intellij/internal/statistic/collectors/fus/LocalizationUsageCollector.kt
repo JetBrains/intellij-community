ee1b4692afb069f44b01e2fee65d4056aa693c07
Vera Petrenkova <Vera.Petrenkova@jetbrains.com>
2024-06-18 14:37:12 +0200

    IJPL-149912 replace enum metric to string for language in FUS
    GitOrigin-RevId: c122a9bf0e4078b57a7d38cbffbaf9db61fa5aee

--- platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/LocalizationUsageCollector.kt
+++ platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/LocalizationUsageCollector.kt
@@ -6,30 +6,18 @@ import com.intellij.ide.RegionSettings
 import com.intellij.internal.statistic.beans.MetricEvent
 import com.intellij.internal.statistic.eventLog.EventLogGroup
 import com.intellij.internal.statistic.eventLog.events.EventFields
+import com.intellij.internal.statistic.eventLog.events.StringEventField
 import com.intellij.internal.statistic.service.fus.collectors.ApplicationUsagesCollector
 import com.intellij.l10n.LocalizationUtil
-import java.util.*
 
 class LocalizationUsageCollector : ApplicationUsagesCollector() {
-  private val GROUP = EventLogGroup("localization.info", 1)
-  private val selectedLanguage = GROUP.registerEvent("selected.language", EventFields.Enum("value", IdeLanguage::class.java))
+  private val GROUP = EventLogGroup("localization.info", 2)
+  private val selectedLanguage = GROUP.registerEvent("selected.language", StringEventField.ValidatedByRegexp("value", ".*{2,5}"))
   private val selectedRegion = GROUP.registerEvent("selected.region", EventFields.Enum("value", Region::class.java))
-
-  @Suppress("EnumEntryName")
-  private enum class IdeLanguage {
-    chinese, japanese, korean, default, other
-  }
 
   override fun getMetrics(): Set<MetricEvent> {
     val result = mutableSetOf<MetricEvent>()
-    val language = when (LocalizationUtil.getLocale()) {
-      Locale.SIMPLIFIED_CHINESE -> IdeLanguage.chinese
-      Locale.JAPANESE -> IdeLanguage.japanese
-      Locale.KOREAN -> IdeLanguage.korean
-      Locale.ENGLISH -> IdeLanguage.default
-      else -> IdeLanguage.other
-    }
-    result.add(selectedLanguage.metric(language))
+    result.add(selectedLanguage.metric(LocalizationUtil.getLocale().toLanguageTag()))
     val region = RegionSettings.getRegion()
     result.add(selectedRegion.metric(region))
     return result
