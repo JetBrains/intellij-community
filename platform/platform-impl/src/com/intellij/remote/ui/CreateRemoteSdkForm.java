8fb13c649ad3100f3bbd4542d272c9f34cf16de1
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-06-18 21:44:48 +0200

    [project] getting rid of `RemoteSdkCredentials` usages in `RemoteSdkUpdatedCallback`
    GitOrigin-RevId: 6d9bfe76afe7b950111c27f93c56c73a997324d1

--- platform/platform-impl/src/com/intellij/remote/ui/CreateRemoteSdkForm.java
+++ platform/platform-impl/src/com/intellij/remote/ui/CreateRemoteSdkForm.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.remote.ui;
 
 import com.intellij.execution.ExecutionBundle;
@@ -15,10 +15,7 @@ import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.NlsContexts;
 import com.intellij.openapi.util.NlsSafe;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.remote.CredentialsType;
-import com.intellij.remote.RemoteSdkAdditionalData;
-import com.intellij.remote.RemoteSdkCredentials;
-import com.intellij.remote.RemoteSdkException;
+import com.intellij.remote.*;
 import com.intellij.remote.ext.*;
 import com.intellij.ui.ComponentUtil;
 import com.intellij.ui.ContextHelpLabel;
@@ -428,7 +425,7 @@ public abstract class CreateRemoteSdkForm<T extends RemoteSdkAdditionalData> ext
     }
   }
 
-  public void updateModifiedValues(RemoteSdkCredentials data) {
+  public void updateModifiedValues(RemoteSdkProperties data) {
     myHelpersPathField.setText(data.getHelpersPath());
   }
 
