c202e40564c60dff07f3702633b4d15d50a14e52
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2024-06-18 11:44:26 +0200

    IJPL-797 `intellij.platform.ide.util.io` review internal API: `EnumeratorLongDescriptor` is internal
    It looks like an implementation detail of the platform. Also, it's quite easy to implement an analog.
    
    No external usages were found.
    
    GitOrigin-RevId: 7aa1fd8a220434255f1fab9d6c789c1357d8dc40

--- platform/platform-util-io/src/com/intellij/util/io/EnumeratorLongDescriptor.kt
+++ platform/platform-util-io/src/com/intellij/util/io/EnumeratorLongDescriptor.kt
@@ -1,9 +1,11 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.io
 
+import org.jetbrains.annotations.ApiStatus
 import java.io.DataInput
 import java.io.DataOutput
 
+@ApiStatus.Internal
 object EnumeratorLongDescriptor: KeyDescriptor<Long> {
 
     override fun getHashCode(value: Long): Int = value.hashCode()
