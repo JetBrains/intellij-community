337ff3dc708ba466660ddde002f4929e32d688ec
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-06-18 12:55:01 +0200

    [platform] adding remote credentials producers to `RemoteSdkAdditionalData`
    GitOrigin-RevId: cf099ee47cc60421ffa58a593402226c2d9a51c8

--- platform/remote-core/src/remote/RemoteSdkAdditionalData.java
+++ platform/remote-core/src/remote/RemoteSdkAdditionalData.java
@@ -1,10 +1,15 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.remote;
 
+import com.intellij.execution.ExecutionException;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.SdkAdditionalData;
 import com.intellij.openapi.util.Key;
 import com.intellij.remote.ext.CredentialsCase;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
 
 public interface RemoteSdkAdditionalData<T extends RemoteSdkCredentials>
   extends SdkAdditionalData, RemoteSdkCredentialsProducer<T>, RemoteSdkProperties {
@@ -16,4 +21,12 @@ public interface RemoteSdkAdditionalData<T extends RemoteSdkCredentials>
   CredentialsType<?> getRemoteConnectionType();
 
   void switchOnConnectionType(CredentialsCase<?>... cases);
+
+  default RemoteCredentials getRemoteCredentials(@Nullable Project project, boolean allowSynchronousInteraction) throws InterruptedException, ExecutionException {
+    return getRemoteSdkCredentials(project, allowSynchronousInteraction);
+  }
+
+  default void produceRemoteCredentials(@Nullable Project project, boolean allowSynchronousInteraction, Consumer<RemoteCredentials> consumer) {
+    produceRemoteSdkCredentials(project, allowSynchronousInteraction, consumer);
+  }
 }
