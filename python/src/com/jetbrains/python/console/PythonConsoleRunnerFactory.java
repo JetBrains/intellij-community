56241ab394e9985464f64d383d4d5894d360ce18
Egor.Eliseev <egor.eliseev@jetbrains.com>
2024-06-20 21:51:17 +0000

    PY-73411 Pycharm performance tests failed
    Merge-request: IJ-MR-137368
    Merged-by: Egor Eliseev <Egor.Eliseev@jetbrains.com>
    
    GitOrigin-RevId: 23a7e9d443606b1a9a028c5e3275c6c408c0d796

--- python/src/com/jetbrains/python/console/PythonConsoleRunnerFactory.java
+++ python/src/com/jetbrains/python/console/PythonConsoleRunnerFactory.java
@@ -1,7 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.console;
 
-import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.python.run.PythonRunConfiguration;
@@ -9,8 +9,11 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class PythonConsoleRunnerFactory {
+  private static final ExtensionPointName<PythonConsoleRunnerFactory> EP_NAME =
+    ExtensionPointName.create("com.jetbrains.python.console.runnerFactory");
+
   public static @NotNull PythonConsoleRunnerFactory getInstance() {
-    return ApplicationManager.getApplication().getService(PythonConsoleRunnerFactory.class);
+    return EP_NAME.getExtensionList().get(0);
   }
 
   public abstract @NotNull PydevConsoleRunner createConsoleRunner(@NotNull Project project, @Nullable Module contextModule);
