de3c5578e489c5037edec21de5cc8646e3557dc7
Yann Cébron <yann.cebron@jetbrains.com>
2024-06-20 16:32:03 +0200

    [devkit] move to appropriate package
    GitOrigin-RevId: 1931031045177ffe0715fa3f308e1b7495be9b18

--- plugins/devkit/devkit-core/src/run/FailedTestDebugLogConsoleFolding.java
+++ plugins/devkit/devkit-core/src/run/FailedTestDebugLogConsoleFolding.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.devkit;
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.idea.devkit.run;
 
 import com.intellij.execution.ConsoleFolding;
 import com.intellij.openapi.project.Project;
