61773b0cbf1757ecc5cba4c1b2e89df469288d17
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2024-06-20 15:51:53 +0200

    IJPL-149476: JarPackagerDependencyHelper: Extract variable for readability/debuggability
    GitOrigin-RevId: 1031865113490a1ea3cc27d2686c02320ada5d20

--- platform/build-scripts/src/org/jetbrains/intellij/build/JarPackagerDependencyHelper.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/JarPackagerDependencyHelper.kt
@@ -30,7 +30,8 @@ internal class JarPackagerDependencyHelper(private val context: BuildContext) {
   }
 
   fun isPluginModulePackedIntoSeparateJar(module: JpsModule, layout: PluginLayout?): Boolean {
-    return !(layout?.modulesWithExcludedModuleLibraries ?: emptySet()).contains(module.name) &&
+    val modulesWithExcludedModuleLibraries = layout?.modulesWithExcludedModuleLibraries ?: emptySet()
+    return module.name !in modulesWithExcludedModuleLibraries &&
            getLibraryDependencies(module).any { it.libraryReference.parentReference is JpsModuleReference }
   }
 
