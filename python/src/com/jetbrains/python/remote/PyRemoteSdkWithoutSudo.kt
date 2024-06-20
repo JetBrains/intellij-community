f6b13ee0537dce277b137a737dec2e06a0ded1bf
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-06-20 17:25:50 +0200

    Cleanup (minor optimization; dead code; typos; formatting)
    GitOrigin-RevId: 21a096b3495e04639a888acb1d7f606e1b571b02

--- python/src/com/jetbrains/python/remote/PyRemoteSdkWithoutSudo.kt
+++ python/src/com/jetbrains/python/remote/PyRemoteSdkWithoutSudo.kt
@@ -6,32 +6,22 @@ import com.intellij.openapi.projectRoots.SdkAdditionalData
 import java.lang.IllegalStateException
 
 /**
- * This is ugly but necessary workaround. Sometimes we should't execute remote
- * python scripts with sudo even if user wants it.
+ * This is an ugly but necessary workaround.
+ * Sometimes we shouldn't execute remote Python scripts with sudo even if user wants it.
  *
  * For example, './manage.py startapp' from Django creates new files and directories.
- * If runs with sudo then files will be owned by root. Such behavior will break
- * WebDeployment plugin because it uses SFTP as a backend and SFTP always tries
- * to do changes with user privileges.
+ * If runs with sudo, then root will own files.
+ * Such behavior will break the WebDeployment plugin because it uses SFTP as a backend,
+ * and SFTP always tries to do changes with user privileges.
  */
 class PyRemoteSdkWithoutSudo(private val forward: Sdk) : Sdk by forward {
-  override fun getSdkAdditionalData(): SdkAdditionalData? {
-    return forward.sdkAdditionalData?.let {
-      if (it is PyRemoteSdkAdditionalDataBase) PyRemoteSdkAdditionalDataWithoutSudo(it) else it
-    }
-  }
-
-  companion object {
-    @JvmStatic
-    fun wrapNullable(sdk: Sdk?): Sdk? = sdk?.let { PyRemoteSdkWithoutSudo(it) }
+  override fun getSdkAdditionalData(): SdkAdditionalData? = forward.sdkAdditionalData?.let {
+    if (it is PyRemoteSdkAdditionalDataBase) PyRemoteSdkAdditionalDataWithoutSudo(it) else it
   }
 }
 
-class PyRemoteSdkAdditionalDataWithoutSudo(forward: PyRemoteSdkAdditionalDataBase) :
-  PyRemoteSdkAdditionalDataBase by forward {
-  override fun isRunAsRootViaSudo(): Boolean {
-    return false
-  }
+class PyRemoteSdkAdditionalDataWithoutSudo(forward: PyRemoteSdkAdditionalDataBase) : PyRemoteSdkAdditionalDataBase by forward {
+  override fun isRunAsRootViaSudo(): Boolean = false
 
   override fun setRunAsRootViaSudo(runAsRootViaSudo: Boolean) {
     throw IllegalStateException("Tried to set runAsRootViaSudo in ${javaClass}")
