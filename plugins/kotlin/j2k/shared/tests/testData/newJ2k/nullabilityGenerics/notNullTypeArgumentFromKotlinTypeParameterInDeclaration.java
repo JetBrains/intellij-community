713072b892e3c8562e5c14cae863c407e18a616c
Alexey Belkov <alexey.belkov@jetbrains.com>
2024-06-21 16:44:27 +0400

    [kotlin] K2 J2K: Initial generic nullability inference
    TODOs are listed in the J2KNullityInferrer Javadoc.
    
    KTIJ-29147
    
    GitOrigin-RevId: 2949b6d7c6b72b586ea6d3af89d0f2e655edcc6d

---
+++ plugins/kotlin/j2k/shared/tests/testData/newJ2k/nullabilityGenerics/notNullTypeArgumentFromKotlinTypeParameterInDeclaration.java
@@ -0,0 +1,12 @@
+public class J {
+    private static final K<String> ka = new K<String>();
+    private static final K<String> kb = new K<>();
+    private static final K<Integer> kc = new K<>();
+    private static final K<Object> kd = new K<Object>();
+
+    private static final K2<String, String> k2a = new K2<String, String>();
+    private static final K2<String, String> k2b = new K2<>();
+    private static final K2<String, Integer> k2c = new K2<>();
+    private static final K2<Integer, String> k2d = new K2<>();
+    private static final K2<Object, Object> k2e = new K2<Object, Object>();
+}
