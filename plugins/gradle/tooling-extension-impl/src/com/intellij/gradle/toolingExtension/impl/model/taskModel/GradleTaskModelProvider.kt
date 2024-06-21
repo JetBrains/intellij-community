1b138c6ba2ac9261b00cb4ef5684669bb06ce2e0
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2024-06-21 17:17:42 +0200

    [Gradle|Sync] cleanup: converted GradleTaskModelProvider to Kotlin
    GitOrigin-RevId: 0ceb210bf1ab57a26f60f460d95dd7c782da4cec

--- plugins/gradle/tooling-extension-impl/src/com/intellij/gradle/toolingExtension/impl/model/taskModel/GradleTaskModelProvider.kt
+++ plugins/gradle/tooling-extension-impl/src/com/intellij/gradle/toolingExtension/impl/model/taskModel/GradleTaskModelProvider.kt
@@ -1,29 +1,27 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.gradle.toolingExtension.impl.model.taskModel;
+package com.intellij.gradle.toolingExtension.impl.model.taskModel
 
-import com.intellij.gradle.toolingExtension.impl.model.taskIndex.GradleTaskRequest;
-import com.intellij.gradle.toolingExtension.impl.util.GradleModelProviderUtil;
-import com.intellij.gradle.toolingExtension.modelAction.GradleModelFetchPhase;
-import org.gradle.tooling.BuildController;
-import org.gradle.tooling.model.gradle.GradleBuild;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.gradle.model.ProjectImportModelProvider;
+import com.intellij.gradle.toolingExtension.impl.model.taskIndex.GradleTaskRequest
+import com.intellij.gradle.toolingExtension.impl.util.GradleModelProviderUtil
+import com.intellij.gradle.toolingExtension.modelAction.GradleModelFetchPhase
+import org.gradle.tooling.BuildController
+import org.gradle.tooling.model.gradle.GradleBuild
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.plugins.gradle.model.ProjectImportModelProvider
+import org.jetbrains.plugins.gradle.model.ProjectImportModelProvider.GradleModelConsumer
 
 @ApiStatus.Internal
-public class GradleTaskModelProvider implements ProjectImportModelProvider {
+class GradleTaskModelProvider : ProjectImportModelProvider {
 
-  @Override
-  public GradleModelFetchPhase getPhase() {
-    return GradleModelFetchPhase.WARM_UP_PHASE;
+  override fun getPhase(): GradleModelFetchPhase {
+    return GradleModelFetchPhase.WARM_UP_PHASE
   }
 
-  @Override
-  public void populateBuildModels(
-    @NotNull BuildController controller,
-    @NotNull GradleBuild buildModel,
-    @NotNull GradleModelConsumer modelConsumer
+  override fun populateBuildModels(
+    controller: BuildController,
+    buildModel: GradleBuild,
+    modelConsumer: GradleModelConsumer,
   ) {
-    GradleModelProviderUtil.buildModels(controller, buildModel, GradleTaskRequest.class, GradleModelConsumer.NOOP);
+    GradleModelProviderUtil.buildModels(controller, buildModel, GradleTaskRequest::class.java, GradleModelConsumer.NOOP)
   }
 }
