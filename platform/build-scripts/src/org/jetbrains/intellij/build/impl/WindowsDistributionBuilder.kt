41bb80db8b426a78275bb0c272f03e5066e8466b
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2024-06-22 13:29:15 +0200

    IJPL-149878 Ability to add specific VM options into Windows distributions
    Required for enabling IJent FS for WSL.
    
    GitOrigin-RevId: b2ee7a1fc721f37184360f5e300bc02b3fce113a

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/WindowsDistributionBuilder.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/WindowsDistributionBuilder.kt
@@ -6,17 +6,16 @@ import com.intellij.openapi.util.io.NioFiles
 import com.intellij.openapi.util.text.StringUtilRt
 import com.intellij.platform.diagnostic.telemetry.helpers.useWithScope
 import com.jetbrains.plugin.structure.base.utils.exists
-import org.jetbrains.intellij.build.impl.qodana.generateQodanaLaunchData
 import io.opentelemetry.api.common.AttributeKey
 import io.opentelemetry.api.trace.Span
 import kotlinx.coroutines.*
 import org.jetbrains.intellij.build.*
-import org.jetbrains.intellij.build.NativeBinaryDownloader
 import org.jetbrains.intellij.build.TraceManager.spanBuilder
 import org.jetbrains.intellij.build.impl.OsSpecificDistributionBuilder.Companion.suffix
 import org.jetbrains.intellij.build.impl.client.ADDITIONAL_EMBEDDED_CLIENT_VM_OPTIONS
 import org.jetbrains.intellij.build.impl.client.createJetBrainsClientContextForLaunchers
 import org.jetbrains.intellij.build.impl.productInfo.*
+import org.jetbrains.intellij.build.impl.qodana.generateQodanaLaunchData
 import org.jetbrains.intellij.build.impl.support.RepairUtilityBuilder
 import org.jetbrains.intellij.build.io.*
 import java.nio.file.Files
@@ -418,7 +417,7 @@ internal class WindowsDistributionBuilder(
 
   private fun writeWindowsVmOptions(distBinDir: Path, context: BuildContext): Path {
     val vmOptionsFile = distBinDir.resolve("${context.productProperties.baseFileName}64.exe.vmoptions")
-    val vmOptions = VmOptionsGenerator.computeVmOptions(context)
+    val vmOptions = VmOptionsGenerator.computeVmOptions(context, OsFamily.WINDOWS)
     writeVmOptions(vmOptionsFile, vmOptions, separator = "\r\n")
     return vmOptionsFile
   }
