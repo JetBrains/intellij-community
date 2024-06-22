1f59ba5467cfe0af5ebb698735c73189c1270bdd
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2024-06-22 18:35:10 +0200

    [inline-completion] IJPL-149157: introduce `InlineCompletionQuoteHandlerEx` for handling quotes when partially accepting
    GitOrigin-RevId: 3c45f5bc8165e2e9914271033e0b41f34083f821

--- platform/lang-impl/src/com/intellij/codeInsight/inline/completion/suggestion/InlineCompletionPartialAcceptHandlerImpl.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/inline/completion/suggestion/InlineCompletionPartialAcceptHandlerImpl.kt
@@ -1,13 +1,9 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.codeInsight.inline.completion
+package com.intellij.codeInsight.inline.completion.suggestion
 
-import com.intellij.codeInsight.editorActions.QuoteHandler
-import com.intellij.codeInsight.editorActions.TypedHandler
 import com.intellij.codeInsight.highlighting.BraceMatcher
 import com.intellij.codeInsight.highlighting.BraceMatchingUtil
-import com.intellij.codeInsight.inline.completion.InlineCompletionPartialAcceptHandlerImpl.SearchState.*
 import com.intellij.codeInsight.inline.completion.elements.*
-import com.intellij.codeInsight.inline.completion.suggestion.InlineCompletionPartialAcceptHandler
 import com.intellij.codeInsight.inline.completion.utils.insertSkipElementsAt
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.editor.Editor
@@ -15,6 +11,7 @@ import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.highlighter.HighlighterIterator
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
 import org.jetbrains.annotations.ApiStatus
@@ -23,7 +20,7 @@ import kotlin.collections.plusAssign
 import kotlin.collections.sorted
 
 @ApiStatus.Experimental
-internal class InlineCompletionPartialAcceptHandlerImpl : InlineCompletionPartialAcceptHandler {
+private class InlineCompletionPartialAcceptHandlerImpl : InlineCompletionPartialAcceptHandler {
 
   override fun insertNextWord(
     editor: Editor,
@@ -63,9 +60,9 @@ internal class InlineCompletionPartialAcceptHandlerImpl : InlineCompletionPartia
     val fileType = originalFile.fileType
     val iterator = editorWithCompletion.highlighter.createIterator(offset)
     val braceMatcher = BraceMatchingUtil.getBraceMatcher(fileType, iterator)
-    val quoteHandler = TypedHandler.getQuoteHandler(originalFile, originalEditor)
+    val quoteHandlerEx = InlineCompletionQuoteHandlerEx.getAdapter(originalFile, originalEditor)
     var insertedPrefixLength = 0
-    var searchState = INIT
+    var searchState = SearchState.INIT
     val skipOffsetsAfterInsertion = mutableListOf<Int>()
     val stuckDetector = IteratorStuckDetector(iterator)
     iteratorLabel@ while (!iterator.atEnd()) {
@@ -75,16 +72,16 @@ internal class InlineCompletionPartialAcceptHandlerImpl : InlineCompletionPartia
       val (startOffset, _, tokenText) = iterator.getTokenInfoRelativelyTo(completion, offset) ?: continue
       val foundQuotePair = iterator.checkForOpenQuoteAndReturnPair(
         tokenOffset = startOffset,
-        tokenText = tokenText,
         initialOffset = offset,
         textWithCompletion = textWithCompletion,
-        quoteHandler = quoteHandler
+        quoteHandlerEx = quoteHandlerEx
       )
       if (foundQuotePair != null) {
-        insertedPrefixLength += foundQuotePair.quote.length
-        if (foundQuotePair.closingQuoteOffset != null) {
-          repeat(foundQuotePair.quote.length) {
-            skipOffsetsAfterInsertion += foundQuotePair.closingQuoteOffset + it - offset
+        insertedPrefixLength += foundQuotePair.openingQuoteRange.length
+        val closingRange = foundQuotePair.closingQuoteRange
+        if (closingRange != null) {
+          for (i in closingRange.startOffset until closingRange.endOffset) {
+            skipOffsetsAfterInsertion += i - offset
           }
         }
         break
@@ -109,9 +106,12 @@ internal class InlineCompletionPartialAcceptHandlerImpl : InlineCompletionPartia
         break
       }
       for ((index, sym) in tokenText.withIndex()) {
-        if (quoteHandler != null && quoteHandler.isClosingQuote(iterator, maxOf(startOffset, offset) + index)) {
-          insertedPrefixLength += tokenText.length - index
-          break@iteratorLabel
+        if (quoteHandlerEx != null) {
+          val closingRange = quoteHandlerEx.getClosingQuoteRange(textWithCompletion, iterator, maxOf(startOffset, offset) + index)
+          if (closingRange != null) {
+            insertedPrefixLength += closingRange.length
+            break@iteratorLabel
+          }
         }
         searchState = searchState.updateWith(sym) ?: break@iteratorLabel
         insertedPrefixLength++
@@ -143,7 +143,7 @@ internal class InlineCompletionPartialAcceptHandlerImpl : InlineCompletionPartia
     var iterator = editorWithCompletion.highlighter.createIterator(offset)
     val fileType = originalFile.fileType
     val braceMatcher = BraceMatchingUtil.getBraceMatcher(fileType, iterator)
-    val quoteHandler = TypedHandler.getQuoteHandler(originalFile, originalEditor)
+    val quoteHandlerEx = InlineCompletionQuoteHandlerEx.getAdapter(originalFile, originalEditor)
     val stuckDetector = IteratorStuckDetector(iterator)
     while (!iterator.atEnd() && iterator.start < offset + prefixLength) {
       if (!stuckDetector.iterateIfStuck()) {
@@ -152,19 +152,18 @@ internal class InlineCompletionPartialAcceptHandlerImpl : InlineCompletionPartia
       val (startOffset, _, tokenText) = iterator.getTokenInfoRelativelyTo(completion, offset) ?: continue
       val foundQuotePair = iterator.checkForOpenQuoteAndReturnPair(
         tokenOffset = startOffset,
-        tokenText = tokenText,
         initialOffset = offset,
         textWithCompletion = textWithCompletion,
-        quoteHandler = quoteHandler
+        quoteHandlerEx = quoteHandlerEx
       )
       if (foundQuotePair != null) {
-        val closingOffset = foundQuotePair.closingQuoteOffset
-        if (closingOffset == null) {
+        val closingRange = foundQuotePair.closingQuoteRange
+        if (closingRange == null) {
           break // The literal covers the left of the completion
         }
-        if (closingOffset >= offset + prefixLength) {
-          repeat(foundQuotePair.quote.length) {
-            skipOffsetsAfterInsertion += closingOffset + it - offset
+        if (closingRange.startOffset >= offset + prefixLength) {
+          for (i in closingRange.startOffset until closingRange.endOffset) {
+            skipOffsetsAfterInsertion += i - offset
           }
           break // no other braces and literals are expected
         }
@@ -233,47 +232,14 @@ internal class InlineCompletionPartialAcceptHandlerImpl : InlineCompletionPartia
 
   private fun HighlighterIterator.checkForOpenQuoteAndReturnPair(
     tokenOffset: Int,
-    tokenText: String,
     initialOffset: Int,
     textWithCompletion: String,
-    quoteHandler: QuoteHandler?
+    quoteHandlerEx: InlineCompletionQuoteHandlerEx.Adapter?
   ): FoundQuotesPair? {
-    if (quoteHandler == null || initialOffset > tokenOffset || !quoteHandler.isOpeningQuote(this, tokenOffset)) {
-      return null
-    }
-    val quote = tokenText.takeWhile { it == tokenText[0] }
-    val quoteEndOffset = findClosingQuoteOffset(quoteHandler)
-    val closingQuote = quoteEndOffset?.let { quoteEndOffset ->
-      textWithCompletion.substring(
-        quoteEndOffset,
-        minOf(quoteEndOffset + quote.length, textWithCompletion.length)
-      )
-    }
-    return FoundQuotesPair(quote, quoteEndOffset?.takeIf { quote == closingQuote })
-  }
-
-  private fun HighlighterIterator.findClosingQuoteOffset(quoteHandler: QuoteHandler): Int? {
-    var balance = 0
-    while (!atEnd()) {
-      val startOffset = start
-      val endOffset = end
-      for (i in startOffset until endOffset) {
-        if (quoteHandler.isOpeningQuote(this, i)) {
-          balance++
-        }
-        else if (quoteHandler.isClosingQuote(this, i)) {
-          balance--
-          if (balance < 0) {
-            return null
-          }
-          if (balance == 0) {
-            return i
-          }
-        }
-      }
-      advance()
-    }
-    return null
+    if (quoteHandlerEx == null || initialOffset > tokenOffset) return null
+    val quoteOpeningRange = quoteHandlerEx.getOpeningQuoteRange(textWithCompletion, this, tokenOffset) ?: return null
+    val quoteClosingRange = quoteHandlerEx.findClosingQuoteRange(textWithCompletion, this, tokenOffset)
+    return FoundQuotesPair(quoteOpeningRange, quoteClosingRange)
   }
 
   private fun doInsert(
@@ -585,20 +551,20 @@ internal class InlineCompletionPartialAcceptHandlerImpl : InlineCompletionPartia
   private fun SearchState.updateWith(symbol: Char): SearchState? {
     return if (symbol.isLetterOrDigit()) {
       when (this) {
-        INIT -> LETTER_OR_DIGIT
-        LETTER_OR_DIGIT -> LETTER_OR_DIGIT
-        SYMBOLS -> LETTER_OR_DIGIT_AFTER_SYMBOLS
-        LETTER_OR_DIGIT_AFTER_SYMBOLS -> LETTER_OR_DIGIT_AFTER_SYMBOLS
-        SYMBOLS_AFTER_LETTER_OR_DIGIT -> null
+        SearchState.INIT -> SearchState.LETTER_OR_DIGIT
+        SearchState.LETTER_OR_DIGIT -> SearchState.LETTER_OR_DIGIT
+        SearchState.SYMBOLS -> SearchState.LETTER_OR_DIGIT_AFTER_SYMBOLS
+        SearchState.LETTER_OR_DIGIT_AFTER_SYMBOLS -> SearchState.LETTER_OR_DIGIT_AFTER_SYMBOLS
+        SearchState.SYMBOLS_AFTER_LETTER_OR_DIGIT -> null
       }
     }
     else {
       when (this) {
-        INIT -> SYMBOLS
-        LETTER_OR_DIGIT -> SYMBOLS_AFTER_LETTER_OR_DIGIT
-        SYMBOLS -> SYMBOLS
-        LETTER_OR_DIGIT_AFTER_SYMBOLS -> null
-        SYMBOLS_AFTER_LETTER_OR_DIGIT -> SYMBOLS_AFTER_LETTER_OR_DIGIT
+        SearchState.INIT -> SearchState.SYMBOLS
+        SearchState.LETTER_OR_DIGIT -> SearchState.SYMBOLS_AFTER_LETTER_OR_DIGIT
+        SearchState.SYMBOLS -> SearchState.SYMBOLS
+        SearchState.LETTER_OR_DIGIT_AFTER_SYMBOLS -> null
+        SearchState.SYMBOLS_AFTER_LETTER_OR_DIGIT -> SearchState.SYMBOLS_AFTER_LETTER_OR_DIGIT
       }
     }
   }
@@ -641,7 +607,7 @@ internal class InlineCompletionPartialAcceptHandlerImpl : InlineCompletionPartia
    */
   private data class LineToInsert(val lineNumberInCompletion: Int, val whitespaceStart: Int, val whitespaceEnd: Int)
 
-  private data class FoundQuotesPair(val quote: String, val closingQuoteOffset: Int?)
+  private data class FoundQuotesPair(val openingQuoteRange: TextRange, val closingQuoteRange: TextRange?)
 
   private data class FoundBracesPair(val bracket: String, val closingBracketRange: IntRange?)
 
