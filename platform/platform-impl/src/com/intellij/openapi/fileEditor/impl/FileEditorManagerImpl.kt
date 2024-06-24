9f7fb5b2d301fe2ebaee71fe8b3eb07fd4ec8d91
Maria Filipanova <maria.filipanova@jetbrains.com>
2024-06-24 14:50:32 +0200

    CWM-9453: pass client id to context
    GitOrigin-RevId: 3d8a1e0e9b61bcae2292143d4a71d1dddeb56184

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/FileEditorManagerImpl.kt
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/FileEditorManagerImpl.kt
@@ -6,6 +6,7 @@ package com.intellij.openapi.fileEditor.impl
 
 import com.intellij.codeInsight.intention.preview.IntentionPreviewUtils
 import com.intellij.codeWithMe.ClientId
+import com.intellij.codeWithMe.asContextElement
 import com.intellij.featureStatistics.fusCollectors.FileEditorCollector
 import com.intellij.ide.IdeEventQueue
 import com.intellij.ide.actions.SplitAction
@@ -258,7 +259,7 @@ open class FileEditorManagerImpl(
 
           val publisher = project.messageBus.syncPublisher(FileEditorManagerListener.FILE_EDITOR_MANAGER)
           // expected in EDT
-          withContext(Dispatchers.EDT) {
+          withContext(Dispatchers.EDT + ClientId.current.asContextElement()) {
             runCatching {
               fireSelectionChanged(
                 newState = state,
