82642625199c545b2e8d96ef7682d47133bad980
Ivan Semenov <ivan.semenov@jetbrains.com>
2024-06-24 17:08:14 +0200

    [github] drop deprecated fields
    GitOrigin-RevId: f0c1420a6343388d3df779850ec380890c1ebe51

--- plugins/github/src/org/jetbrains/plugins/github/util/GHGitRepositoryMapping.kt
+++ plugins/github/src/org/jetbrains/plugins/github/util/GHGitRepositoryMapping.kt
@@ -20,12 +20,6 @@ class GHGitRepositoryMapping(override val repository: GHRepositoryCoordinates,
   override val repositoryPath: String
     get() = repository.repositoryPath.repository
 
-  @Deprecated("use repository property", ReplaceWith("repository"))
-  val ghRepositoryCoordinates: GHRepositoryCoordinates = repository
-
-  @Deprecated("use remote property", ReplaceWith("remote"))
-  val gitRemoteUrlCoordinates: org.jetbrains.plugins.github.util.GitRemoteUrlCoordinates = GitRemoteUrlCoordinates(remote)
-
   override fun equals(other: Any?): Boolean {
     if (this === other) return true
     if (other !is GHGitRepositoryMapping) return false
