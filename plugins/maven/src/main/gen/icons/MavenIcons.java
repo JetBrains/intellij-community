2afd590ce44d05b00b63d93c520da376c0125887
Dima Golovkov <dmitry.golovkov@jetbrains.com>
2024-06-24 22:53:33 +0300

    Fix mapping for UX-3145
    GitOrigin-RevId: 2f0f4a2939f4c36232ce50e3539d68e2dc133471

--- plugins/maven/src/main/gen/icons/MavenIcons.java
+++ plugins/maven/src/main/gen/icons/MavenIcons.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package icons;
 
 import com.intellij.ui.IconManager;
@@ -26,7 +26,7 @@ public final class MavenIcons {
   /** 16x16 */ public static final @NotNull Icon MavenProject = load("images/mavenProject.svg", "images/expui/build/mavenProject.svg", -1399986903, 0);
   /** 16x16 */ public static final @NotNull Icon MavenRepoFolder = load("images/mavenRepoFolder.svg", "images/expui/build/mavenProfiles.svg", 205206251, 0);
   /** 16x16 */ public static final @NotNull Icon MavenRepoLocal = load("images/mavenRepoLocal.svg", "images/expui/build/mavenProject.svg", -1519797757, 0);
-  /** 16x16 */ public static final @NotNull Icon MavenRepoRemote = load("images/mavenRepoRemote.svg", 1551467989, 0);
+  /** 16x16 */ public static final @NotNull Icon MavenRepoRemote = load("images/mavenRepoRemote.svg", "images/expui/build/mavenPluginGoal.svg", 1551467989, 0);
   /** 16x16 */ public static final @NotNull Icon ModulesClosed = load("images/modulesClosed.svg", "images/expui/build/mavenModule.svg", -1412181772, 0);
   /** 16x16 */ public static final @NotNull Icon ParentProject = load("images/parentProject.svg", "images/expui/build/mavenParentProjects.svg", -863135840, 2);
   /** 16x16 */ public static final @NotNull Icon PluginGoal = load("images/pluginGoal.svg", "images/expui/build/mavenPluginGoal.svg", 1879670671, 0);
