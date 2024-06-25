38a28db180565a58564cc5d243bbf4349d3024a4
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-06-25 19:53:28 +0200

    Cleanup (moving PasswordSafeException to an appropriate package)
    GitOrigin-RevId: 3fec9940d24f7fa84e16596c51de31e7b14f4cbd

--- platform/credential-store/src/ide/passwordSafe/PasswordSafeException.java
+++ platform/credential-store/src/ide/passwordSafe/PasswordSafeException.java
@@ -1,11 +1,9 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.passwordSafe;
 
-/**
- * @deprecated unused, left for binary compatibility
- */
+/** @deprecated unused, left for binary compatibility */
 @SuppressWarnings("unused")
-@Deprecated
+@Deprecated(forRemoval = true)
 public class PasswordSafeException extends RuntimeException {
   public PasswordSafeException(String message, Throwable cause) {
     super(message, cause);
