1a21a6ce491b20a44073632311b9ad51d8f56a05
Alexey Belkov <alexey.belkov@jetbrains.com>
2024-06-25 10:49:02 +0400

    [kotlin] K2 J2K: bring back printing of lambda parameter types
    Looks like it is not required to avoid red code with Streams.
    
    Partial revert of 3844363e
    
    KTIJ-28575
    
    (cherry picked from commit 8f70cdf772adfe9a044b25d1b13aeb45a4ad1132)
    
    
    (cherry picked from commit 9cb90d0a2848d7b0403eeb3bdc84c957e756bc3c)
    
    IJ-MR-140172
    
    GitOrigin-RevId: 71cc774b422a4092e140633ae9a4111c8b5490df

--- plugins/kotlin/j2k/shared/tests/testData/newJ2k/types/unusedCapturedWildcardTypeInSAM.k2.kt
+++ plugins/kotlin/j2k/shared/tests/testData/newJ2k/types/unusedCapturedWildcardTypeInSAM.k2.kt
@@ -1,13 +1,13 @@
 class Test {
     fun useSamUpper(acceptor: SamAcceptor<out Number?>) {
-        acceptor.acceptSam({ p -> })
+        acceptor.acceptSam({ p: Int -> })
     }
 
     fun useSamLower(acceptor: SamAcceptor<in Number?>) {
-        acceptor.acceptSam({ p -> })
+        acceptor.acceptSam({ p: Int -> })
     }
 
     fun useSam(acceptor: SamAcceptor<*>) {
-        acceptor.acceptSam({ p -> })
+        acceptor.acceptSam({ p: Int -> })
     }
 }
