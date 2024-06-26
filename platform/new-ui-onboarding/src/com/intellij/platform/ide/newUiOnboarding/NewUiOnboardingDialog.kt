945770c203bd1108ede3c4717ba69cd160434681
Vera Petrenkova <Vera.Petrenkova@jetbrains.com>
2024-06-26 11:19:40 +0200

    IJPL-148392 Update New UI onboarding images for popup and toolwindow
    GitOrigin-RevId: bf756157ce99a14b0e703e923cf6f8c019a2a2a7

--- platform/new-ui-onboarding/src/com/intellij/platform/ide/newUiOnboarding/NewUiOnboardingDialog.kt
+++ platform/new-ui-onboarding/src/com/intellij/platform/ide/newUiOnboarding/NewUiOnboardingDialog.kt
@@ -1,36 +1,35 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.ide.newUiOnboarding
 
-import com.intellij.ide.plugins.MultiPanel
 import com.intellij.ide.plugins.PluginManagerConfigurable
 import com.intellij.ide.ui.laf.darcula.ui.DarculaButtonUI
 import com.intellij.openapi.application.ApplicationNamesInfo
-import com.intellij.openapi.application.ModalityState
-import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.util.IconLoader
-import com.intellij.ui.*
+import com.intellij.openapi.wm.WindowManager
+import com.intellij.ui.ClientProperty
+import com.intellij.ui.JBColor
+import com.intellij.ui.PopupBorder
+import com.intellij.ui.WindowRoundedCornersManager
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.gridLayout.UnscaledGaps
 import com.intellij.ui.dsl.gridLayout.UnscaledGapsY
-import com.intellij.ui.jcef.JBCefBrowser
 import com.intellij.ui.scale.JBUIScale
+import com.intellij.util.IconUtil
 import com.intellij.util.ui.JBDimension
 import com.intellij.util.ui.JBFont
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
-import org.cef.browser.CefBrowser
-import org.cef.handler.CefLoadHandlerAdapter
 import java.awt.Color
 import java.awt.Font
-import java.util.*
 import javax.swing.JComponent
 import javax.swing.JLabel
 import javax.swing.JRootPane
 import javax.swing.border.Border
 import javax.swing.event.HyperlinkEvent
+
 
 internal class NewUiOnboardingDialog(private val project: Project)
   : DialogWrapper(project, null, false, IdeModalityType.IDE, false) {
@@ -46,15 +45,18 @@ internal class NewUiOnboardingDialog(private val project: Project)
   }
 
   override fun createCenterPanel(): JComponent {
-    val videoSize = JBDimension(384, 242)
     val contentGaps = UnscaledGaps(28, 32, 22, 32)
-
+     val imageWidth = 384
+     val imageHeight = 242
+     val imageSize = JBDimension(imageWidth, imageHeight)
     val panel = panel {
       row {
-        cell(createAnimationPanel())
+        val img = IconLoader.getIcon(POPUP_IMAGE_PATH, NewUiOnboardingDialog::class.java.classLoader)
+        val scale = JBUI.scale(imageHeight).toFloat() / img.iconHeight
+        cell(JLabel(IconUtil.scale(img, WindowManager.getInstance().getFrame(project), scale)))
           .customize(UnscaledGaps.EMPTY)
           .applyToComponent {
-            preferredSize = videoSize
+            preferredSize = imageSize
           }
       }
       panel {
@@ -66,7 +68,7 @@ internal class NewUiOnboardingDialog(private val project: Project)
             }
         }
         row {
-          val maxWidth = videoSize.width - JBUI.scale(contentGaps.width)
+          val maxWidth = imageWidth - JBUI.scale(contentGaps.width)
           val charWidth = window.getFontMetrics(JBFont.label()).charWidth('0')
           val maxLineLength = maxWidth / charWidth
           text(NewUiOnboardingBundle.message("dialog.text", ApplicationNamesInfo.getInstance().fullProductName), maxLineLength) { event ->
@@ -102,52 +104,7 @@ internal class NewUiOnboardingDialog(private val project: Project)
 
   override fun createContentPaneBorder(): Border? = null
 
-  private fun createAnimationPanel(): JComponent {
-    val browser = JBCefBrowser.createBuilder().setMouseWheelEventEnable(false).build()
-    val browserComponent = browser.component
-    WindowMoveListener(browserComponent).installTo(browserComponent.components.firstOrNull() ?: browserComponent)
-    UIUtil.setNotOpaqueRecursively(browserComponent)
-
-    val banner = IconLoader.findIcon(BANNER_PATH, NewUiOnboardingDialog::class.java.classLoader)
-    val bannerLabel = JLabel(banner)
-    val multiPanel = object : MultiPanel() {
-      override fun create(key: Int): JComponent = when (key) {
-        BANNER_KEY -> bannerLabel
-        BROWSER_KEY -> browserComponent
-        else -> throw IllegalArgumentException("Unknown key: ${key}")
-      }
-    }
-
-    browser.jbCefClient.addLoadHandler(object : CefLoadHandlerAdapter() {
-      override fun onLoadingStateChange(b: CefBrowser?, isLoading: Boolean, canGoBack: Boolean, canGoForward: Boolean) {
-        invokeLater(ModalityState.any()) {
-          if (isLoading) {
-            multiPanel.select(BANNER_KEY, true)
-          }
-          else multiPanel.select(BROWSER_KEY, true)
-        }
-      }
-    }, browser.cefBrowser)
-
-    val videoBase64 = readVideoAsBase64()
-    val pageHtml = WebAnimationUtils.createVideoHtmlPage(videoBase64, backgroundColor)
-    browser.loadHTML(pageHtml)
-
-    multiPanel.select(BROWSER_KEY, true)
-    return multiPanel
-  }
-
-  private fun readVideoAsBase64(): String {
-    val url = NewUiOnboardingDialog::class.java.classLoader.getResource(VIDEO_PATH)
-              ?: error("Failed to find file by path: $VIDEO_PATH")
-    val videoBytes = url.readBytes()
-    return Base64.getEncoder().encodeToString(videoBytes)
-  }
-
   companion object {
-    private const val VIDEO_PATH: String = "newUiOnboarding/DialogVideo.webm"
-    private const val BANNER_PATH: String = "newUiOnboarding/banner.png"
-    private const val BANNER_KEY: Int = 0
-    private const val BROWSER_KEY: Int = 1
+    private const val POPUP_IMAGE_PATH: String = "newUiOnboarding/newUIOnboardingPopup.png"
   }
 }
