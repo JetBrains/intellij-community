7ad88f4705b26f977bb27ca3c3fe77f6813fe218
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-06-26 14:12:59 +0200

    IJPL-157219 refactor FileSearchEverywhereContributor
    (cherry picked from commit 29a0dbf004b923fae484f4cb82585bf3e1123350)
    
    IJ-CR-138219
    
    GitOrigin-RevId: 55e72268cb16a9bc403f1a3342364a6ef35a6f5a

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/FileSearchEverywhereContributor.kt
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/FileSearchEverywhereContributor.kt
@@ -1,194 +1,164 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ide.actions.searcheverywhere;
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide.actions.searcheverywhere
 
-import com.intellij.featureStatistics.FeatureUsageTracker;
-import com.intellij.ide.IdeBundle;
-import com.intellij.ide.actions.OpenInRightSplitAction;
-import com.intellij.ide.actions.SearchEverywherePsiRenderer;
-import com.intellij.ide.util.gotoByName.FileTypeRef;
-import com.intellij.ide.util.gotoByName.FilteringGotoByModel;
-import com.intellij.ide.util.gotoByName.GotoFileConfiguration;
-import com.intellij.ide.util.gotoByName.GotoFileModel;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiFileSystemItem;
-import com.intellij.ui.DirtyUI;
-import com.intellij.util.Processor;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.featureStatistics.FeatureUsageTracker
+import com.intellij.ide.IdeBundle
+import com.intellij.ide.actions.OpenInRightSplitAction.Companion.openInRightSplit
+import com.intellij.ide.actions.SearchEverywherePsiRenderer
+import com.intellij.ide.actions.searcheverywhere.SearchEverywhereFiltersStatisticsCollector.FileTypeFilterCollector
+import com.intellij.ide.actions.searcheverywhere.footer.createPsiExtendedInfo
+import com.intellij.ide.util.gotoByName.FileTypeRef
+import com.intellij.ide.util.gotoByName.FileTypeRef.Companion.forAllFileTypes
+import com.intellij.ide.util.gotoByName.FilteringGotoByModel
+import com.intellij.ide.util.gotoByName.GotoFileConfiguration
+import com.intellij.ide.util.gotoByName.GotoFileModel
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.fileEditor.OpenFileDescriptor
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.io.FileUtilRt
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileSystemItem
+import com.intellij.ui.DirtyUI
+import com.intellij.util.Processor
+import org.jetbrains.annotations.ApiStatus.Internal
+import org.jetbrains.annotations.Nls
+import java.awt.event.InputEvent
+import javax.swing.JList
+import javax.swing.ListCellRenderer
 
-import javax.swing.*;
-import java.awt.event.InputEvent;
-import java.util.ArrayList;
-import java.util.List;
+private val LOG = Logger.getInstance(FileSearchEverywhereContributor::class.java)
 
-import static com.intellij.ide.actions.searcheverywhere.SearchEverywhereFiltersStatisticsCollector.FileTypeFilterCollector;
-import static com.intellij.ide.actions.searcheverywhere.footer.ExtendedInfoImplKt.createPsiExtendedInfo;
+open class FileSearchEverywhereContributor(event: AnActionEvent) : AbstractGotoSEContributor(
+  event), EssentialContributor, SearchEverywherePreviewProvider {
+  private val modelForRenderer: GotoFileModel
+  private val filter: PersistentSearchEverywhereContributorFilter<FileTypeRef>
 
-/**
- * @author Konstantin Bulenkov
- * @author Mikhail Sokolov
- */
-public class FileSearchEverywhereContributor extends AbstractGotoSEContributor implements EssentialContributor,
-                                                                                          SearchEverywherePreviewProvider {
-  private static final Logger LOG = Logger.getInstance(FileSearchEverywhereContributor.class);
-  private final GotoFileModel myModelForRenderer;
-  private final PersistentSearchEverywhereContributorFilter<FileTypeRef> myFilter;
-
-  public FileSearchEverywhereContributor(@NotNull AnActionEvent event) {
-    super(event);
-    Project project = event.getRequiredData(CommonDataKeys.PROJECT);
-    myModelForRenderer = new GotoFileModel(project);
-    myFilter = createFileTypeFilter(project);
+  init {
+    val project = event.getRequiredData(CommonDataKeys.PROJECT)
+    modelForRenderer = GotoFileModel(project)
+    filter = createFileTypeFilter(project)
   }
 
-  @NotNull
-  @Override
-  public String getGroupName() {
-    return IdeBundle.message("search.everywhere.group.name.files");
-  }
-
-  @Override
-  public int getSortWeight() {
-    return 200;
-  }
-
-  @Override
-  public int getElementPriority(@NotNull Object element, @NotNull String searchPattern) {
-    return super.getElementPriority(element, searchPattern) + 2;
-  }
-
-  @NotNull
-  @Override
-  protected FilteringGotoByModel<FileTypeRef> createModel(@NotNull Project project) {
-    GotoFileModel model = new GotoFileModel(project);
-    if (myFilter != null) {
-      model.setFilterItems(myFilter.getSelectedElements());
+  companion object {
+    @JvmStatic
+    fun createFileTypeFilter(project: Project): PersistentSearchEverywhereContributorFilter<FileTypeRef> {
+      val items = forAllFileTypes().toMutableList()
+      items.add(0, GotoFileModel.DIRECTORY_FILE_TYPE_REF)
+      return PersistentSearchEverywhereContributorFilter(items, GotoFileConfiguration.getInstance(project), FileTypeRef::displayName,
+                                                         FileTypeRef::icon)
     }
-    return model;
   }
 
-  @NotNull
-  @Override
-  public List<AnAction> getActions(@NotNull Runnable onChanged) {
-    return doGetActions(myFilter, new FileTypeFilterCollector(), onChanged);
+  override fun getGroupName(): String = IdeBundle.message("search.everywhere.group.name.files")
+
+  override fun getSortWeight(): Int = 200
+
+  @Suppress("OVERRIDE_DEPRECATION")
+  override fun getElementPriority(element: Any, searchPattern: String): Int = super.getElementPriority(element, searchPattern) + 2
+
+  override fun createModel(project: Project): FilteringGotoByModel<FileTypeRef> {
+    val model = GotoFileModel(project)
+    model.setFilterItems(filter.selectedElements)
+    return model
   }
 
-  @NotNull
-  @Override
-  public ListCellRenderer<Object> getElementsRenderer() {
-    return new SearchEverywherePsiRenderer(this) {
+  override fun getActions(onChanged: Runnable): List<AnAction> = doGetActions(filter, FileTypeFilterCollector(), onChanged)
+
+  override fun getElementsRenderer(): ListCellRenderer<Any> {
+    return object : SearchEverywherePsiRenderer(this) {
       @DirtyUI
-      @NotNull
-      @Override
-      public ItemMatchers getItemMatchers(@NotNull JList list, @NotNull Object value) {
-        ItemMatchers defaultMatchers = super.getItemMatchers(list, value);
-        if (!(value instanceof PsiFileSystemItem) || myModelForRenderer == null) {
-          return defaultMatchers;
+      override fun getItemMatchers(list: JList<*>, value: Any): ItemMatchers {
+        val defaultMatchers = super.getItemMatchers(list, value)
+        if (value !is PsiFileSystemItem) {
+          return defaultMatchers
         }
-
-        return GotoFileModel.convertToFileItemMatchers(defaultMatchers, (PsiFileSystemItem)value, myModelForRenderer);
+        return GotoFileModel.convertToFileItemMatchers(defaultMatchers, value, modelForRenderer)
       }
-    };
+    }
   }
 
-  @Override
-  protected boolean processElement(@NotNull ProgressIndicator progressIndicator,
-                                   @NotNull Processor<? super FoundItemDescriptor<Object>> consumer,
-                                   FilteringGotoByModel<?> model, Object element, int degree) {
-    if (progressIndicator.isCanceled()) return false;
+  override fun processElement(
+    progressIndicator: ProgressIndicator,
+    consumer: Processor<in FoundItemDescriptor<Any>?>,
+    model: FilteringGotoByModel<*>,
+    element: Any?,
+    degree: Int,
+  ): Boolean {
+    if (progressIndicator.isCanceled) {
+      return false
+    }
 
     if (element == null) {
-      LOG.error("Null returned from " + model + " in " + this.getClass().getSimpleName());
-      return true;
+      LOG.error("Null returned from $model in ${javaClass.simpleName}")
+      return true
     }
 
-    return consumer.process(new FoundItemDescriptor<>(element, degree));
+    return consumer.process(FoundItemDescriptor(element, degree))
   }
 
-  @Override
-  public boolean processSelectedItem(@NotNull Object selected, int modifiers, @NotNull String searchText) {
-    if (selected instanceof PsiFile) {
-      VirtualFile file = ((PsiFile)selected).getVirtualFile();
-      if (file != null && myProject != null) {
-        Pair<Integer, Integer> pos = getLineAndColumn(searchText);
-        OpenFileDescriptor descriptor = new OpenFileDescriptor(myProject, file, pos.first, pos.second);
-        if (descriptor.canNavigate()) {
-          ApplicationManager.getApplication().invokeLater(() -> {
-            if ((modifiers & InputEvent.SHIFT_MASK) != 0) OpenInRightSplitAction.Companion.openInRightSplit(myProject, file, descriptor, true);
-            else descriptor.navigate(true);
-            if (pos.first > 0) {
-              FeatureUsageTracker.getInstance().triggerFeatureUsed("navigation.goto.file.line");
-            }
-          });
-          return true;
+  override fun processSelectedItem(selected: Any, modifiers: Int, searchText: String): Boolean {
+    if (selected is PsiFile) {
+      val file = selected.virtualFile
+      if (file == null || myProject == null) {
+        return super.processSelectedItem(selected, modifiers, searchText)
+      }
+
+      val pos = getLineAndColumn(searchText)
+      val descriptor = OpenFileDescriptor(myProject, file, pos.first, pos.second)
+      if (descriptor.canNavigate()) {
+        ApplicationManager.getApplication().invokeLater {
+          @Suppress("DEPRECATION")
+          if ((modifiers and InputEvent.SHIFT_MASK) != 0) {
+            openInRightSplit(project = myProject, file = file, element = descriptor, requestFocus = true)
+          }
+          else {
+            descriptor.navigate(true)
+          }
+          if (pos.first > 0) {
+            FeatureUsageTracker.getInstance().triggerFeatureUsed("navigation.goto.file.line")
+          }
         }
+        return true
       }
     }
 
-    return super.processSelectedItem(selected, modifiers, searchText);
+    return super.processSelectedItem(selected, modifiers, searchText)
   }
 
-  @Override
-  public Object getDataForItem(@NotNull Object element, @NotNull String dataId) {
-    if (CommonDataKeys.PSI_FILE.is(dataId) && element instanceof PsiFile) {
-      return element;
+  override fun getDataForItem(element: Any, dataId: String): Any? {
+    if (CommonDataKeys.PSI_FILE.`is`(dataId) && element is PsiFile) {
+      return element
     }
-    return super.getDataForItem(element, dataId);
+    return super.getDataForItem(element, dataId)
   }
 
-  @Override
-  public @Nullable String getItemDescription(@NotNull Object element) {
-    if ((element instanceof PsiFile || element instanceof PsiDirectory) && ((PsiFileSystemItem)element).isValid()) {
-      String path = ((PsiFileSystemItem)element).getVirtualFile().getPath();
-      path = FileUtil.toSystemIndependentName(path);
+  override fun getItemDescription(element: Any): String? {
+    if ((element is PsiFile || element is PsiDirectory) && (element as PsiFileSystemItem).isValid) {
+      var path: String? = FileUtilRt.toSystemIndependentName(element.virtualFile.path)
       if (myProject != null) {
-        String basePath = myProject.getBasePath();
-        if (basePath != null) {
-          path = FileUtil.getRelativePath(basePath, path, '/');
+        myProject.basePath?.let {
+          path = FileUtilRt.getRelativePath(it, path!!, '/')
         }
       }
-      return path;
+      return path
     }
-    return super.getItemDescription(element);
+    return super.getItemDescription(element)
   }
 
-  @Override
-  public boolean isEmptyPatternSupported() {
-    return true;
-  }
+  override fun isEmptyPatternSupported(): Boolean = true
 
-  @Nls
-  @Override
-  public @Nullable ExtendedInfo createExtendedInfo() {
-    return createPsiExtendedInfo();
-  }
+  override fun createExtendedInfo(): @Nls ExtendedInfo? = createPsiExtendedInfo()
+}
 
-  public static final class Factory implements SearchEverywhereContributorFactory<Object> {
-    @NotNull
-    @Override
-    public SearchEverywhereContributor<Object> createContributor(@NotNull AnActionEvent initEvent) {
-      return PSIPresentationBgRendererWrapper.wrapIfNecessary(new FileSearchEverywhereContributor(initEvent));
-    }
-  }
-
-  @NotNull
-  public static PersistentSearchEverywhereContributorFilter<FileTypeRef> createFileTypeFilter(@NotNull Project project) {
-    List<FileTypeRef> items = new ArrayList<>(FileTypeRef.forAllFileTypes());
-    items.add(0, GotoFileModel.DIRECTORY_FILE_TYPE_REF);
-    return new PersistentSearchEverywhereContributorFilter<>(items, GotoFileConfiguration.getInstance(project), FileTypeRef::getDisplayName,
-                                                             FileTypeRef::getIcon);
+@Internal
+class FileSearchEverywhereContributorFactory : SearchEverywhereContributorFactory<Any?> {
+  override fun createContributor(initEvent: AnActionEvent): SearchEverywhereContributor<Any?> {
+    return PSIPresentationBgRendererWrapper.wrapIfNecessary(FileSearchEverywhereContributor(initEvent))
   }
 }
