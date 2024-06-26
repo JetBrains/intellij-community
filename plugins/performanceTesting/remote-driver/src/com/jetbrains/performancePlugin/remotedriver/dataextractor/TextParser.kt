37024f89183935d38c3bb7af7e7a3deda0513e11
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-06-26 12:59:48 +0200

    Don't log PCE and instead rethrow it
    (cherry picked from commit 9f7625564125f10df17bc0ae2a17d5168eaf38e8)
    
    IJ-CR-138515
    
    GitOrigin-RevId: 5cc4cd072cd3bdaae01d232c54825e48d5bc4c90

--- plugins/performanceTesting/remote-driver/src/com/jetbrains/performancePlugin/remotedriver/dataextractor/TextParser.kt
+++ plugins/performanceTesting/remote-driver/src/com/jetbrains/performancePlugin/remotedriver/dataextractor/TextParser.kt
@@ -4,6 +4,7 @@ package com.jetbrains.performancePlugin.remotedriver.dataextractor
 
 import com.intellij.driver.model.TextData
 import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.progress.ProcessCanceledException
 import org.assertj.swing.edt.GuiActionRunner
 import org.assertj.swing.edt.GuiTask
 import java.awt.Component
@@ -54,6 +55,9 @@ object TextParser {
       override fun executeInEDT() {
         try {
           component.paint(g)
+        }
+        catch (ce: ProcessCanceledException){
+          throw ce
         }
         catch (e: Exception) {
           logger.error("Text parsing error. Can't do paint on ${component::class.java.simpleName}", e)
