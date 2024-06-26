93b92929953e63fc498d44e5fbca1784e9ee4180
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2024-06-26 17:29:34 +0200

    [kotlin] Add warning dialog for unsupported KMP move
    #KTIJ-30422 Fixed
    
    (cherry picked from commit 2335d586bd25f198fee24a7e70df3a5d4727385b)
    
    IJ-CR-138010
    
    GitOrigin-RevId: f9565f2d8bffd03b21691aab74ad95e3d690d7ec

--- plugins/kotlin/refactorings/kotlin.refactorings.move.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/move/ui/K2MoveDialog.kt
+++ plugins/kotlin/refactorings/kotlin.refactorings.move.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/move/ui/K2MoveDialog.kt
@@ -3,13 +3,20 @@ package org.jetbrains.kotlin.idea.k2.refactoring.move.ui
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogPanel
+import com.intellij.openapi.ui.Messages
+import com.intellij.psi.util.descendantsOfType
+import com.intellij.refactoring.RefactoringBundle
 import com.intellij.refactoring.move.MoveHandler
 import com.intellij.refactoring.ui.RefactoringDialog
 import com.intellij.ui.dsl.builder.AlignX
 import com.intellij.ui.dsl.builder.AlignY
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.builder.plus
+import org.jetbrains.kotlin.idea.base.psi.isEffectivelyActual
+import org.jetbrains.kotlin.idea.base.psi.isExpectDeclaration
+import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.refactoring.KotlinCommonRefactoringSettings
+import org.jetbrains.kotlin.psi.KtNamedDeclaration
 import javax.swing.JComponent
 
 internal class K2MoveDialog(project: Project, private val model: K2MoveModel) : RefactoringDialog(project, true) {
@@ -48,6 +55,20 @@ internal class K2MoveDialog(project: Project, private val model: K2MoveModel) : 
     override fun doAction() {
         saveSettings()
         val descriptor = model.toDescriptor()
+        val allDeclarations = descriptor.source.elements.flatMap { it.descendantsOfType<KtNamedDeclaration>() }
+        if (allDeclarations.any { it.isExpectDeclaration() || it.isEffectivelyActual() }) {
+            val response = Messages.showDialog(
+                KotlinBundle.message("kmp.move.not.supported.message"),
+                KotlinBundle.message("kmp.move.not.supported.title"),
+                arrayOf(RefactoringBundle.message("cancel.button"), RefactoringBundle.message("refactor.anyway.button")), 1,
+                Messages.getWarningIcon()
+            )
+            if (response == 0) {
+                close(CLOSE_EXIT_CODE)
+                return
+            }
+
+        }
         invokeRefactoring(descriptor.refactoringProcessor())
     }
 
