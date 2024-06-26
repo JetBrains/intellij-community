dea6132b0b1bb7f632efb2e1eee6c9d7444629c9
anton.spilnyy <anton.spilnyy@jetbrains.com>
2024-06-26 06:00:25 +0200

    [aia] evaluation. codegen. LLM-3297 CodeGen, Evaluation Plugin: implement visitor for Python.
    GitOrigin-RevId: 2b7dd97af45d1eaa45dc9a9f73e0fdee1667e56f

--- plugins/evaluation-plugin/languages/python/src/com/intellij/cce/visitor/PythonCodeGenerationVisitor.kt
+++ plugins/evaluation-plugin/languages/python/src/com/intellij/cce/visitor/PythonCodeGenerationVisitor.kt
@@ -2,10 +2,13 @@ package com.intellij.cce.visitor
 
 import com.intellij.cce.core.*
 import com.intellij.cce.visitor.exceptions.PsiConverterException
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiFile
 import com.intellij.psi.util.startOffset
 import com.jetbrains.python.psi.*
 
-class PythonCodeGenerationVisitor : EvaluationVisitor, PyRecursiveElementVisitor() {
+class PythonCodeGenerationVisitor : EvaluationVisitor, PsiElementVisitor() {
   private var codeFragment: CodeFragment? = null
 
   override val language = Language.PYTHON
@@ -13,15 +16,29 @@ class PythonCodeGenerationVisitor : EvaluationVisitor, PyRecursiveElementVisitor
 
   override fun getFile(): CodeFragment = codeFragment
                                          ?: throw PsiConverterException("Invoke 'accept' with visitor on PSI first")
-  override fun visitPyFile(node: PyFile) {
-    codeFragment = CodeFragment(node.textOffset, node.textLength)
-    super.visitPyFile(node)
-  }
 
+  override fun visitFile(node: PsiFile) {
+    codeFragment = CodeFragment(node.textOffset, node.textLength).also {
+      PythonCodeGenerationPsiVisitor(it).visitFile(node)
+    }
+  }
+}
+
+private class PythonCodeGenerationPsiVisitor(private val codeFragment: CodeFragment) : PyRecursiveElementVisitor() {
   override fun visitPyFunction(node: PyFunction) {
-    codeFragment?.addChild(
+    codeFragment.addChild(
       CodeToken(node.text, node.startOffset, SimpleTokenProperties.create(TypeProperty.METHOD, SymbolLocation.PROJECT) {})
     )
     super.visitPyFunction(node)
+  }
+
+  override fun visitElement(element: PsiElement) {
+    if (element is PyStatementList) {
+      codeFragment.addChild(
+        CodeToken(element.text, element.startOffset, SimpleTokenProperties.create(TypeProperty.METHOD_BODY, SymbolLocation.PROJECT) {})
+      )
+    }
+
+    super.visitElement(element)
   }
 }
