798fdf89b92fd8553aff114b91f5ca13cbfca7f5
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2024-06-27 16:28:22 +0200

    IJPL-1015 add background progress for update downloading
    (cherry picked from commit 477130e66e17c324a5113650e18cd1afc2b288f9)
    
    IJ-MR-138268
    
    GitOrigin-RevId: 800bac8e44ee2e93feff069e274e86dd8724364e

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/PluginAutoUpdateService.kt
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/PluginAutoUpdateService.kt
@@ -2,24 +2,28 @@
 package com.intellij.openapi.updateSettings.impl
 
 import com.intellij.ide.AppLifecycleListener
+import com.intellij.ide.IdeBundle
+import com.intellij.ide.impl.ProjectUtil
 import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.application.PluginAutoUpdateRepository
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.diagnostic.debug
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.coroutineToIndicator
-import com.intellij.platform.util.coroutines.childScope
+import com.intellij.platform.ide.progress.withBackgroundProgress
+import com.intellij.platform.util.progress.reportProgress
 import com.intellij.util.io.createDirectories
 import com.intellij.util.io.delete
 import com.intellij.util.io.move
 import com.intellij.util.text.VersionComparatorUtil
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.ensureActive
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.nio.file.Path
@@ -38,20 +42,19 @@ import kotlin.io.path.name
 internal class PluginAutoUpdateService(private val cs: CoroutineScope) {
   private val updatesState: MutableMap<PluginId, DownloadedUpdate> = ConcurrentHashMap()
   private val pendingDownloads: Channel<List<PluginDownloader>> = Channel(capacity = Channel.UNLIMITED)
-  private var downloadManagerScope: CoroutineScope? = null
+  private var downloadManagerJob: Job? = null
 
   fun isAutoUpdateEnabled(): Boolean = Companion.isAutoUpdateEnabled()
 
   private fun setupDownloadManager() {
     synchronized(this) {
       if (isAutoUpdateEnabled()) {
-        if (downloadManagerScope == null || downloadManagerScope?.isActive != true) {
-          val managerScope = cs.childScope("Download manager")
-          managerScope.launch { downloadManager() }
-          downloadManagerScope = managerScope
+        if (downloadManagerJob?.isActive != true) {
+          LOG.debug { "setting up download manager" }
+          downloadManagerJob = cs.launch(CoroutineName("Download manager")) { downloadManager() }
         }
       } else {
-        downloadManagerScope?.cancel()
+        downloadManagerJob?.cancel()
         while (true) { // drain pending downloads
           pendingDownloads.tryReceive().getOrNull() ?: break
         }
@@ -62,42 +65,21 @@ internal class PluginAutoUpdateService(private val cs: CoroutineScope) {
   private suspend fun CoroutineScope.downloadManager() {
     for (downloaders in pendingDownloads) {
       ensureActive()
-      // download updates one by one for now, TODO
-      val downloadedList = mutableListOf<PluginDownloader>()
-      for (downloader in downloaders) {
-        ensureActive()
-        if (!isAutoUpdateEnabled()) {
-          throw CancellationException("auto-update disabled")
+      LOG.debug { "new pluging updates: ${downloaders.joinToString { it.pluginName }}" }
+      val activeProject = ProjectUtil.getActiveProject()
+      val downloadedList = if (activeProject != null) {
+        withBackgroundProgress(activeProject, IdeBundle.message("update.downloading.plugins.progress"), true) {
+          LOG.debug { "downloading with background progress in project $activeProject" }
+          downloadUpdates(downloaders)
         }
-        val existingUpdateState = updatesState[downloader.id]
-        if (existingUpdateState != null && VersionComparatorUtil.compare(existingUpdateState.version, downloader.pluginVersion) >= 0) {
-          continue
-        }
-        val plugin = PluginManagerCore.getPlugin(downloader.id)
-                     ?: continue
-        if (!plugin.isBundled) downloader.setOldFile(plugin.pluginPath)
-        val updatePathInAutoUpdateDir = withContext(Dispatchers.IO) {
-          val updateFile = coroutineToIndicator {
-            downloader.tryDownloadPlugin(ProgressManager.getGlobalProgressIndicator())
-          }
-          val updatePath = updateFile.toPath()
-          val autoUpdateDir = PluginAutoUpdateRepository.getAutoUpdateDirPath()
-          val updatePathInAutoUpdatesDir = autoUpdateDir.resolve(updatePath.fileName)
-          if (!autoUpdateDir.exists()) {
-            autoUpdateDir.createDirectories()
-          }
-          if (updatePathInAutoUpdatesDir.exists()) {
-            LOG.warn("update for plugin ${downloader.id} located in file ${updatePath.fileName} already exists and will be overwritten")
-            updatePathInAutoUpdatesDir.delete()
-          }
-          ensureActive()
-          updatePath.move(updatePathInAutoUpdatesDir)
-          updatePathInAutoUpdatesDir
-        }
-        updatesState[downloader.id] = DownloadedUpdate(downloader.id, downloader.pluginVersion, updatePathInAutoUpdateDir)
-        downloadedList.add(downloader)
+      } else {
+        LOG.debug { "downloading without background progress" }
+        downloadUpdates(downloaders)
       }
+
       if (downloadedList.isNotEmpty()) {
+        LOG.debug { "adding downloaded updates to the repository: ${downloadedList.joinToString { it.pluginName }}" }
+        notifyUpdatesDownloaded(downloadedList)
         withContext(Dispatchers.IO) {
           PluginAutoUpdateRepository.addUpdates(updatesState.mapValues {
             PluginAutoUpdateRepository.PluginUpdateInfo(
@@ -111,12 +93,58 @@ internal class PluginAutoUpdateService(private val cs: CoroutineScope) {
     }
   }
 
+  private suspend fun CoroutineScope.downloadUpdates(downloaders: List<PluginDownloader>): List<PluginDownloader> {
+    val downloadedList = mutableListOf<PluginDownloader>()
+    val downloaders = downloaders.filter { downloader ->
+      val existingUpdateState = updatesState[downloader.id]
+      val pluginExists = PluginManagerCore.getPlugin(downloader.id) != null
+      val thereIsNoActualDownloadedUpdate = existingUpdateState == null || VersionComparatorUtil.compare(existingUpdateState.version, downloader.pluginVersion) < 0
+      pluginExists && thereIsNoActualDownloadedUpdate
+    }
+    reportProgress(downloaders.size) { reporter ->
+      for (downloader in downloaders) {
+        ensureActive()
+        if (!isAutoUpdateEnabled()) {
+          throw CancellationException("auto-update disabled")
+        }
+        reporter.itemStep(IdeBundle.message("progress.downloading.plugin", downloader.pluginName)) {
+          LOG.debug { "downloading ${downloader.pluginName}" }
+          val plugin = PluginManagerCore.getPlugin(downloader.id)
+                       ?: return@itemStep
+          if (!plugin.isBundled) downloader.setOldFile(plugin.pluginPath)
+          val updatePathInAutoUpdateDir = withContext(Dispatchers.IO) {
+            val updateFile = coroutineToIndicator {
+              downloader.tryDownloadPlugin(ProgressManager.getGlobalProgressIndicator())
+            }
+            val updatePath = updateFile.toPath()
+            val autoUpdateDir = PluginAutoUpdateRepository.getAutoUpdateDirPath()
+            val updatePathInAutoUpdatesDir = autoUpdateDir.resolve(updatePath.fileName)
+            if (!autoUpdateDir.exists()) {
+              autoUpdateDir.createDirectories()
+            }
+            if (updatePathInAutoUpdatesDir.exists()) {
+              LOG.warn("update for plugin ${downloader.id} located in file ${updatePath.fileName} already exists and will be overwritten")
+              updatePathInAutoUpdatesDir.delete()
+            }
+            ensureActive()
+            updatePath.move(updatePathInAutoUpdatesDir)
+            updatePathInAutoUpdatesDir
+          }
+          updatesState[downloader.id] = DownloadedUpdate(downloader.id, downloader.pluginVersion, updatePathInAutoUpdateDir)
+          downloadedList.add(downloader)
+        }
+      }
+    }
+    return downloadedList
+  }
+
   private fun notifyUpdatesDownloaded(downloaded: List<PluginDownloader>) {
     LOG.info("updates for plugins ${downloaded.joinToString(", ") { it.pluginName }} were downloaded " +
              "(${updatesState.size} updates are prepared in total)")
   }
 
   fun onPluginUpdatesCheck(updates: List<PluginDownloader>) {
+    LOG.debug { "onPluginUpdateCheck: ${updates.joinToString { it.pluginName }}" }
     val sent = pendingDownloads.trySend(updates)
     if (sent.isFailure) {
       LOG.error("failed to schedule updates for downloading")
