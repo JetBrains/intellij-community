0fc65e532b670a558d5604428e854cc0708d7083
Tagir Valeev <tagir.valeev@jetbrains.com>
2024-06-28 14:22:39 +0200

    Remove unused messageFormat parameter
    GitOrigin-RevId: 8322a90c6e49a74ac82ae508e6451e00b2803f8a

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/processor/handler/singular/SingularGuavaMapHandler.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/processor/handler/singular/SingularGuavaMapHandler.java
@@ -64,11 +64,11 @@ class SingularGuavaMapHandler extends SingularMapHandler {
 
   @Override
   protected String getOneMethodBody(@NotNull String singularName, @NotNull BuilderInfo info) {
-    final String codeBlockTemplate = "if (this.{0} == null) this.{0} = {2}.{3}; \n" +
+    final String codeBlockTemplate = "if (this.{0} == null) this.{0} = {1}.{2}; \n" +
                                      "this.{0}.put(" + LOMBOK_KEY + ", " + LOMBOK_VALUE + ");\n" +
-                                     "return {4};";
+                                     "return {3};";
 
-    return MessageFormat.format(codeBlockTemplate, info.getFieldName(), singularName, collectionQualifiedName,
+    return MessageFormat.format(codeBlockTemplate, info.getFieldName(), collectionQualifiedName,
                                 sortedCollection ? "naturalOrder()" : "builder()", info.getBuilderChainResult());
   }
 
