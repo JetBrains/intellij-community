252e3e12a654a069a832d2b484e130bca7675417
ekaterina.itsenko <ekaterina.itsenko@jetbrains.com>
2024-06-28 12:58:39 +0200

    [pycharm] PY-70312 Fix everything except charts
    (cherry picked from commit dc4ef94d776105260e0b4d0965a08bfaf1165268)
    
    
    (cherry picked from commit 0ca6bd466be6cc34d5b1ffbca6bb6a6676cca738)
    
    IJ-MR-138716
    
    GitOrigin-RevId: 334b7f1714a02ee9b1d38980a6f54c97452a259f

--- python/helpers/pydev/_pydevd_bundle/pydevd_vars.py
+++ python/helpers/pydev/_pydevd_bundle/pydevd_vars.py
@@ -693,6 +693,13 @@ def get_label(label):
 
 DATAFRAME_HEADER_LOAD_MAX_SIZE = 100
 
+class IAtPolarsAccessor:
+    def __init__(self, ps):
+        self.ps = ps
+
+    def __getitem__(self, row):
+        return self.ps[row]
+
 
 def dataframe_to_xml(df, name, roffset, coffset, rows, cols, format):
     """
@@ -707,7 +714,7 @@ def dataframe_to_xml(df, name, roffset, coffset, rows, cols, format):
 
     """
     original_df = df
-    dim = len(df.axes)
+    dim = len(df.axes) if hasattr(df, 'axes') else -1
     num_rows = df.shape[0]
     num_cols = df.shape[1] if dim > 1 else 1
     format = format.replace('%', '')
@@ -719,7 +726,7 @@ def dataframe_to_xml(df, name, roffset, coffset, rows, cols, format):
             except AttributeError:
                 try:
                     kind = df.dtypes[0].kind
-                except (IndexError, KeyError):
+                except (IndexError, KeyError, AttributeError):
                     kind = 'O'
             format = array_default_format(kind)
         else:
@@ -752,19 +759,34 @@ def dataframe_to_xml(df, name, roffset, coffset, rows, cols, format):
             else:
                 bounds = (0, 0)
             col_bounds[col] = bounds
+    elif dim == -1:
+        dtype = '0'
+        dtypes[0] = dtype
+        col_bounds[0] = (df.min(), df.max()) if dtype in NUMPY_NUMERIC_TYPES and df.size != 0 else (0, 0)
     else:
         dtype = df.dtype.kind
         dtypes[0] = dtype
         col_bounds[0] = (df.min(), df.max()) if dtype in NUMPY_NUMERIC_TYPES and df.size != 0 else (0, 0)
 
-    df = df.iloc[roffset: roffset + rows, coffset: coffset + cols] if dim > 1 else df.iloc[roffset: roffset + rows]
+    if dim > 1:
+        df = df.iloc[roffset: roffset + rows, coffset: coffset + cols]
+    elif dim == -1:
+        df = df[roffset: roffset + rows]
+    else:
+        df.iloc[roffset: roffset + rows]
+
     rows = df.shape[0]
     cols = df.shape[1] if dim > 1 else 1
 
     def col_to_format(column_type):
         return get_column_formatter_by_type(format, column_type)
 
-    iat = df.iat if dim == 1 or len(df.columns.unique()) == len(df.columns) else df.iloc
+    if dim == -1:
+        iat = IAtPolarsAccessor(df)
+    elif dim == 1 or len(df.columns.unique()) == len(df.columns):
+        iat = df.iat
+    else:
+        iat = df.iloc
 
     def formatted_row_elements(row):
         return get_formatted_row_elements(row, iat, dim, cols, format, dtypes)
@@ -801,7 +823,7 @@ def header_data_to_xml(rows, cols, dtypes, col_bounds, col_to_format, df, dim):
         xml += '<colheader index=\"%s\" label=\"%s\" type=\"%s\" format=\"%s\" max=\"%s\" min=\"%s\" />\n' % \
                (str(col), col_label, dtypes[col], col_to_format(dtypes[col]), quote(str(col_format % bounds[1])), quote(str(col_format % bounds[0])))
     for row in range(rows):
-        xml += "<rowheader index=\"%s\" label = \"%s\"/>\n" % (str(row), quote(get_label(df.axes[0][row])))
+        xml += "<rowheader index=\"%s\" label = \"%s\"/>\n" % (str(row), quote(get_label(df.axes[0][row] if dim != -1 else str(row))))
     xml += "</headerdata>\n"
     return xml
 
