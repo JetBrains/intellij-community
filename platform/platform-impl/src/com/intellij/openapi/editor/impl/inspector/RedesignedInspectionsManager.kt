d2644af34d6c83228ba592de7f1f1f165299cee9
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-06-30 22:41:21 +0200

    [inspections] Simplify RedesignedInspectionsManager class
    GitOrigin-RevId: 2f809165e07889c18bcb951f5eca483e483d87e6

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/inspector/RedesignedInspectionsManager.kt
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/inspector/RedesignedInspectionsManager.kt
@@ -5,11 +5,9 @@ import com.intellij.openapi.application.Experiments
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
-class RedesignedInspectionsManager {
-  companion object {
-    @JvmStatic
-    fun isAvailable(): Boolean {
-      return Experiments.getInstance().isFeatureEnabled("ide.redesigned.inspector")
-    }
+object RedesignedInspectionsManager {
+  @JvmStatic
+  fun isAvailable(): Boolean {
+    return Experiments.getInstance().isFeatureEnabled("ide.redesigned.inspector")
   }
 }
