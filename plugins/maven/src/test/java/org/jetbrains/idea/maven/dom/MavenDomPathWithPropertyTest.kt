1572709060e24728075b12b898da4626ef95c9b1
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2024-07-01 21:50:26 +0200

    [amper] IDEA-355596 run MavenDomPathWithPropertyTest in background
    GitOrigin-RevId: 29b0e5a45e67a00f1b467859322d9de8d5bd4b01

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/dom/MavenDomPathWithPropertyTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/dom/MavenDomPathWithPropertyTest.kt
@@ -1,15 +1,19 @@
 package org.jetbrains.idea.maven.dom
 
 import com.intellij.maven.testFramework.MavenDomTestCase
+import com.intellij.maven.testFramework.assertWithinTimeout
 import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.readAction
+import com.intellij.openapi.application.writeAction
 import com.intellij.psi.PsiManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.junit.Test
 
 class MavenDomPathWithPropertyTest : MavenDomTestCase() {
   @Test
-  fun testRename() = runBlocking(Dispatchers.EDT) {
+  fun testRename() = runBlocking {
     importProjectAsync(
       """
         <groupId>test</groupId>
@@ -42,22 +46,24 @@ class MavenDomPathWithPropertyTest : MavenDomTestCase() {
         </build>
         """.trimIndent())
 
-    val dir = createProjectSubDir("aaa/bbb/res")
+    withContext(Dispatchers.EDT) {
+      val dir = createProjectSubDir("aaa/bbb/res")
 
-    val bbb = dir.getParent()
-    fixture.renameElement(PsiManager.getInstance(fixture.getProject()).findDirectory(bbb)!!, "Z")
+      val bbb = dir.getParent()
 
+      fixture.renameElement(PsiManager.getInstance(fixture.getProject()).findDirectory(bbb)!!, "Z")
 
-    val text = PsiManager.getInstance(fixture.getProject()).findFile(projectPom)!!.getText()
-    assert(text.contains("<directory>aaa/Z/res</directory>"))
-    assert(text.contains("<directory>aaa/Z/res</directory>"))
-    assert(text.contains("<directory>aaa/Z/res</directory>"))
-    assert(text.contains("<directory>aaa/Z/res</directory>"))
-    assert(text.contains("<directory>aaa/Z/@rrr@</directory>"))
+      val text = PsiManager.getInstance(fixture.getProject()).findFile(projectPom)!!.getText()
+      assert(text.contains("<directory>aaa/Z/res</directory>"))
+      assert(text.contains("<directory>aaa/Z/res</directory>"))
+      assert(text.contains("<directory>aaa/Z/res</directory>"))
+      assert(text.contains("<directory>aaa/Z/res</directory>"))
+      assert(text.contains("<directory>aaa/Z/@rrr@</directory>"))
+    }
   }
 
   @Test
-  fun testCompletionDirectoriesOnly() = runBlocking(Dispatchers.EDT) {
+  fun testCompletionDirectoriesOnly() = runBlocking {
     createProjectPom(
       """
             <groupId>test</groupId>
