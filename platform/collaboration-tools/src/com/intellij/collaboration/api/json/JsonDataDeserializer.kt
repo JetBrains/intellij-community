4077ffa0c91e5dd56ed0a7b2b384dc9ccc787173
Chris Lemaire <chris.lemaire@jetbrains.com>
2024-07-01 08:16:13 +0200

    [gitlab] Disable auto-closing JSON parsers to avoid premature closure (IJPL-157434)
    #IJPL-157434 Fixed
    
    This should prevent READING_LENGTH exceptions caused by
    JsonParsers closing the reader before reading all tokens.
    
    Also, don't check for readiness. `ready` depends on `java.io.InputStream.available`.
    But, `available` does not block to check a correct count of available bytes, so
    it might return 0 even if more bytes are available still.
    Probably best to read to completion regardless of readiness to avoid these errors...
    
    Interesting note: if we use `.factory` instead of the method on `mapper`
    the configurations from `genericConfig` will not be applied to the parser created.
    
    GitOrigin-RevId: 99ff0728d1c33f59e04cf33c5c34e452b8c41d97

--- platform/collaboration-tools/src/com/intellij/collaboration/api/json/JsonDataDeserializer.kt
+++ platform/collaboration-tools/src/com/intellij/collaboration/api/json/JsonDataDeserializer.kt
@@ -6,6 +6,17 @@ import java.io.Reader
 
 @ApiStatus.Experimental
 interface JsonDataDeserializer {
+  /**
+   * Parses a value of the given type from the given reader.
+   *
+   * The reader is not closed by this function. It should be managed by the caller.
+   */
   fun <T> fromJson(bodyReader: Reader, clazz: Class<T>): T?
+  /**
+   * Parses a value of type T = L<A, B> from the given reader.
+   * Type T is given by [clazz], whereas A and B are given through [classArgs].
+   *
+   * The reader is not closed by this function. It should be managed by the caller.
+   */
   fun <T> fromJson(bodyReader: Reader, clazz: Class<T>, vararg classArgs: Class<*>): T?
 }
