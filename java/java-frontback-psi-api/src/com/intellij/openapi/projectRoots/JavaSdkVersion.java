1199999a84285239c436afcf0bbb614c84767186
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-07-02 17:39:10 +0200

    [java] introduce language level for Java 23
    (cherry picked from commit d64d154c243f5c1810bf08790a1df44a6ac948cb)
    
    GitOrigin-RevId: 57f6f6b0d1490888632bafac8305bddc1e8ac7d7

--- java/java-frontback-psi-api/src/com/intellij/openapi/projectRoots/JavaSdkVersion.java
+++ java/java-frontback-psi-api/src/com/intellij/openapi/projectRoots/JavaSdkVersion.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.projectRoots;
 
 import com.intellij.openapi.util.NlsSafe;
@@ -37,7 +37,8 @@ public enum JavaSdkVersion {
   JDK_20(LanguageLevel.JDK_20),
   JDK_21(LanguageLevel.JDK_21),
   JDK_22(LanguageLevel.JDK_22),
-  JDK_23(LanguageLevel.JDK_X);
+  JDK_23(LanguageLevel.JDK_23),
+  JDK_24(LanguageLevel.JDK_X);
 
   private final LanguageLevel myMaxLanguageLevel;
 
