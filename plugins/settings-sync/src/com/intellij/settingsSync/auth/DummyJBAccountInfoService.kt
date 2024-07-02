3f9aae4213da84c3e6f52b88db32da1b2ef2fadb
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2024-07-02 17:19:24 +0200

    LLM-2956: license: Add `JBAccountInfoService.issueTrialLicense()`
    GitOrigin-RevId: 112ccda56ea29d5d92441a07067fe58df73b2567

--- plugins/settings-sync/src/com/intellij/settingsSync/auth/DummyJBAccountInfoService.kt
+++ plugins/settings-sync/src/com/intellij/settingsSync/auth/DummyJBAccountInfoService.kt
@@ -24,9 +24,12 @@ object DummyJBAccountInfoService : JBAccountInfoService {
     TODO("Not yet implemented")
   }
 
-  override fun getAvailableLicenses(
-    productCode: String,
-  ): CompletableFuture<JBAccountInfoService.LicenseListResult> {
+  override fun getAvailableLicenses(productCode: String): CompletableFuture<JBAccountInfoService.LicenseListResult> {
+    TODO("Not yet implemented")
+  }
+
+  override fun issueTrialLicense(productCode: String,
+                                 consentOptions: List<String>): CompletableFuture<JBAccountInfoService.LicenseListResult> {
     TODO("Not yet implemented")
   }
 
