de4ad3f1e38905bdb0351e06fe48ea4e9c446d60
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2024-07-03 15:36:01 +0200

    IJPL-74857 IJent logs: tune logging from argv instead of env, don't log gRPC by default
    Logs of grpc_stdio_server always repeat logs of ijent_service, and they can help only in very rare cases.
    
    GitOrigin-RevId: 00a4cebf560618819441cfe57424d73e10acb81f

--- platform/ijent/src/com/intellij/platform/ijent/IjentProcessUtil.kt
+++ platform/ijent/src/com/intellij/platform/ijent/IjentProcessUtil.kt
@@ -16,21 +16,23 @@ fun getIjentGrpcArgv(
   selfDeleteOnExit: Boolean = false,
   usrBinEnv: String = "/usr/bin/env",
 ): List<String> {
-  val (debuggingLogLevel, backtrace) = when {
-    ApplicationManager.getApplication()?.isUnitTestMode == true -> "trace" to true
-    LOG.isTraceEnabled -> "trace" to true
-    LOG.isDebugEnabled -> "debug" to true
-    else -> "info" to false
+  val debuggingLogLevel = when {
+    LOG.isTraceEnabled &&
+    (ApplicationManager.getApplication()?.isUnitTestMode == true || System.getProperty("ijent.trace.grpc") == "true") ->
+      "trace-with-grpc"
+
+    LOG.isTraceEnabled -> "trace"
+    LOG.isDebugEnabled -> "debug"
+    else -> "info"
   }
 
   return listOfNotNull(
     usrBinEnv,
-    "RUST_LOG=ijent=$debuggingLogLevel",
-    if (backtrace) "RUST_BACKTRACE=1" else null,
     *additionalEnv.entries.map2Array { (k, v) -> "$k=$v" },
     // "gdbserver", "0.0.0.0:12345",  // https://sourceware.org/gdb/onlinedocs/gdb/Connecting.html
     remotePathToIjent,
     "grpc-stdio-server",
+    "--log-level", debuggingLogLevel,
     if (selfDeleteOnExit) "--self-delete-on-exit" else null,
   )
 }
