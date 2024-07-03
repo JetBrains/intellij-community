bfd1db704a4873d605444220cf46ac4f9980e8b1
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-07-03 09:35:32 +0200

    [runtime product] API cleanup: remove ProductMode.NOVA value (IJPL-797)
    ProductMode class is in platform code, it isn't supposed to contain things related to specific products. Also, this new field isn't returned from ProductLoadingStrategy as the other. It was used in StatisticsEventLogger only, and that usage is now inlined.
    
    
    (cherry picked from commit deab69580a7c2b32314f709d18efa838d2f544d5)
    
    IJ-CR-139236
    
    GitOrigin-RevId: 165f5202d9f7720d79d81232c7cb2bddc0648a21

--- platform/runtime/product/src/ProductMode.java
+++ platform/runtime/product/src/ProductMode.java
@@ -22,12 +22,7 @@ public enum ProductMode {
    * Indicates that this process doesn't perform heavy tasks like code analysis, and takes necessary information from another process.
    * Currently, this is used by JetBrains Client process connected to a remote development host or CodeWithMe session.
    */
-  BACKEND("backend"),
-
-  /**
-   * Use the special mode 'nova' to report CLion nova (radler) engine, for local ide only.
-   */
-  NOVA("nova");
+  BACKEND("backend");
 
   private final String myId;
 
