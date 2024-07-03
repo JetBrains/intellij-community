85eb6f8683d5a189e9568a87757a1c930ab20305
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2024-07-03 10:02:56 +0200

    IJPL-157704 When I update a bundled language plugin, it's duplicated in the language list
    GitOrigin-RevId: bf4f2a88cb02962e9b59ee0c7dc4904613e278c4

--- platform/core-api/src/com/intellij/l10n/LocalizationUtil.kt
+++ platform/core-api/src/com/intellij/l10n/LocalizationUtil.kt
@@ -206,7 +206,7 @@ object LocalizationUtil {
 
   @Internal
   fun getAllAvailableLocales(): Pair<List<Locale>, Map<Locale, String>> {
-    val list = ArrayList<Locale>()
+    val list = HashSet<Locale>()
     val map = HashMap<Locale, String>()
 
     for (bundleEP in getAllLanguageBundleExtensions()) {
