a01eb76a76695ec3ad1b06bee6f5bce206889c24
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-07-03 13:56:47 +0200

    [java] disable String Templates for Java 23 (IDEA-355055)
    GitOrigin-RevId: cc2b3dfffd4fe0ba34a91b0d9f342e32304bfd2c

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/LightStringTemplatesHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/LightStringTemplatesHighlightingTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.java.codeInsight.daemon;
 
 import com.intellij.JavaTestUtil;
@@ -23,6 +23,7 @@ public class LightStringTemplatesHighlightingTest extends LightJavaCodeInsightFi
   public void testStringTemplates() { doTest(); }
   
   public void testStringTemplatesJava22() { IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_22_PREVIEW, () -> doTest()); }
+  public void testStringTemplatesJava23() { IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_23_PREVIEW, () -> doTest()); }
 
   private void doTest() {
     myFixture.configureByFile(getTestName(false) + ".java");
