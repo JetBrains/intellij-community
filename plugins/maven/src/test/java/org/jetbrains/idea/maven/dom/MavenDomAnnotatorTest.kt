f9d6cabf6f00632e5600fe73413467cd122105fa
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2024-07-03 21:42:39 +0200

    [maven] IDEA-355596 check file content in MavenDomAnnotatorTest
    GitOrigin-RevId: 2ac6c1ae6e47d79a311204d68dfd6541498b4aae

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/dom/MavenDomAnnotatorTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/dom/MavenDomAnnotatorTest.kt
@@ -3,17 +3,20 @@ package org.jetbrains.idea.maven.dom
 
 import com.intellij.maven.testFramework.MavenDomTestCase
 import com.intellij.openapi.application.EDT
+import com.intellij.openapi.project.modules
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
+import junit.framework.TestCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import org.jetbrains.idea.maven.project.MavenProjectsManager
 import org.junit.Test
 
 class MavenDomAnnotatorTest : MavenDomTestCase() {
   @Test
   fun testAnnotatePlugin() = runBlocking {
-    val modulePom = createModulePom("m", """
+    val modulePomContent = """
 <parent>
   <groupId>test</groupId>
   <artifactId>project</artifactId>
@@ -28,7 +31,8 @@ class MavenDomAnnotatorTest : MavenDomTestCase() {
     </plugin>
   </plugins>
 </build>
-""")
+"""
+    val modulePom = createModulePom("m", modulePomContent)
 
     importProjectAsync("""
 <groupId>test</groupId>
@@ -52,7 +56,16 @@ class MavenDomAnnotatorTest : MavenDomTestCase() {
 </build>
 """)
 
-    checkGutters(modulePom, listOf(
+    withContext(Dispatchers.EDT) {
+      val modules = project.modules
+      assertSize(2, modules)
+      val projectsManager = MavenProjectsManager.getInstance(project)
+      val tree = projectsManager.projectsTree
+      assertSize(2, tree.projects)
+      assertSize(2, tree.nonIgnoredProjects)
+    }
+
+    checkGutters(modulePom, modulePomContent, listOf(
       "<artifactId>maven-compiler-plugin</artifactId>",
       """<parent>
           <groupId>test</groupId>
@@ -63,7 +76,7 @@ class MavenDomAnnotatorTest : MavenDomTestCase() {
 
   @Test
   fun testAnnotateDependency() = runBlocking {
-    val modulePom = createModulePom("m", """
+    val modulePomContent = """
 <parent>
   <groupId>test</groupId>
   <artifactId>project</artifactId>
@@ -77,7 +90,8 @@ class MavenDomAnnotatorTest : MavenDomTestCase() {
     <artifactId>junit</artifactId>                
   </dependency>
 </dependencies>
-""")
+"""
+    val modulePom = createModulePom("m", modulePomContent)
 
     importProjectAsync("""
 <groupId>test</groupId>
@@ -100,7 +114,7 @@ class MavenDomAnnotatorTest : MavenDomTestCase() {
 </dependencyManagement>
 """)
 
-    checkGutters(modulePom, listOf(
+    checkGutters(modulePom, modulePomContent, listOf(
       """<dependency>
          <groupId>junit</groupId>
          <artifactId>junit</artifactId>       
@@ -114,7 +128,7 @@ class MavenDomAnnotatorTest : MavenDomTestCase() {
 
   @Test
   fun testAnnotateDependencyWithEmptyRelativePath() = runBlocking {
-    val modulePom = createModulePom("m", """
+    val modulePomContent = """
 <parent>
   <groupId>test</groupId>
   <artifactId>project</artifactId>
@@ -129,7 +143,8 @@ class MavenDomAnnotatorTest : MavenDomTestCase() {
     <artifactId>junit</artifactId>                
   </dependency>
 </dependencies>
-""")
+"""
+    val modulePom = createModulePom("m", modulePomContent)
 
     importProjectAsync("""
 <groupId>test</groupId>
@@ -152,7 +167,7 @@ class MavenDomAnnotatorTest : MavenDomTestCase() {
 </dependencyManagement>
 """)
 
-    checkGutters(modulePom, listOf(
+    checkGutters(modulePom, modulePomContent, listOf(
       """<dependency>
          <groupId>junit</groupId>
          <artifactId>junit</artifactId>       
@@ -165,12 +180,13 @@ class MavenDomAnnotatorTest : MavenDomTestCase() {
          </parent>"""))
   }
 
-  private suspend fun checkGutters(virtualFile: VirtualFile, expectedProperties: Collection<String>) {
+  private suspend fun checkGutters(virtualFile: VirtualFile, expectedFileContent: String, expectedProperties: Collection<String>) {
     withContext(Dispatchers.EDT) {
       val file = PsiManager.getInstance(project).findFile(virtualFile)!!
-      fixture.configureFromExistingVirtualFile(virtualFile)
-
       val text = file.text
+      TestCase.assertTrue("Unexpected pom content:\n$text", text.contains(expectedFileContent))
+
+      fixture.configureFromExistingVirtualFile(virtualFile)
       val actualProperties = fixture.doHighlighting()
         .filter { it.gutterIconRenderer != null }
         .map { text.substring(it.getStartOffset(), it.getEndOffset()) }
