760f2fcb3b2afe433f2db87613706f9009311e0f
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2024-07-04 14:11:34 +0300

    [Java. Code Formatting] Add javadoc for AnnotationAwareArrangementEntry
    IDEA-110857
    
    GitOrigin-RevId: dcc9cbf63d82f731f617c17c06da5ab70c580728

--- java/java-impl/src/com/intellij/psi/codeStyle/arrangement/AnnotationAwareArrangementEntry.kt
+++ java/java-impl/src/com/intellij/psi/codeStyle/arrangement/AnnotationAwareArrangementEntry.kt
@@ -1,7 +1,11 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.codeStyle.arrangement
 
-interface AnnotationAwareArrangementEntry : ArrangementEntry {
+/**
+ * Interface for arrangement entries that are aware if they have annotations or not.
+ * @see JavaElementArrangementEntry
+ */
+internal interface AnnotationAwareArrangementEntry : ArrangementEntry {
   fun hasAnnotation(): Boolean
 
   fun setHasAnnotation()
