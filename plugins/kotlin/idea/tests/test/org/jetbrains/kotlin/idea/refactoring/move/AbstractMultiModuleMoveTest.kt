bbb3ea00957055b292fff28f10fe2fedeebdbe01
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2024-07-04 10:35:53 +0200

    [kotlin] Enable file check for multi module move test
    GitOrigin-RevId: 995238d90c636323f102d3c314ed1f88b4b77e15

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/refactoring/move/AbstractMultiModuleMoveTest.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/refactoring/move/AbstractMultiModuleMoveTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.refactoring.move
 
@@ -16,12 +16,6 @@ import org.jetbrains.kotlin.idea.test.PluginTestCaseBase
 import java.io.File
 
 abstract class AbstractMultiModuleMoveTest : KotlinMultiFileTestCase() {
-
-    override fun setUp() {
-        super.setUp()
-        myDoCompare = false
-    }
-
     override fun getTestRoot(): String = "/refactoring/moveMultiModule/"
 
     override fun getTestDataDirectory() = IDEA_TEST_DATA_DIR
