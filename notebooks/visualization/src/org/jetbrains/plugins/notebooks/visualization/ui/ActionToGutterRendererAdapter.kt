731c135100ddbe4dcc2262315f3fbf2d067c068e
Bogdan.Kirilenko <bogdan.kirilenko@jetbrains.com>
2024-07-04 22:22:25 +0200

    [jupyter] PY-73383 override fun isNavigateAction() = true for ActionToGutterRendererAdapter - but does not solve it entirely
    GitOrigin-RevId: a0dc0575d6b021dcfdf3a42800ccc8da5f3f11b7

--- notebooks/visualization/src/org/jetbrains/plugins/notebooks/visualization/ui/ActionToGutterRendererAdapter.kt
+++ notebooks/visualization/src/org/jetbrains/plugins/notebooks/visualization/ui/ActionToGutterRendererAdapter.kt
@@ -7,6 +7,7 @@ import javax.swing.Icon
 class ActionToGutterRendererAdapter(private val action: AnAction) : GutterIconRenderer() {
 
   private val icon = action.templatePresentation.icon ?: error("Action has no assigned icon")
+
   override fun equals(other: Any?): Boolean {
     return icon == (other as? ActionToGutterRendererAdapter)?.icon
   }
@@ -20,5 +21,7 @@ class ActionToGutterRendererAdapter(private val action: AnAction) : GutterIconRe
   override fun getClickAction(): AnAction {
     return action
   }
+
+  override fun isNavigateAction() = true
 
 }
