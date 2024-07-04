ff1ce5a0bd04ede8ebe4c8e503c4a61a24898a96
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2024-07-04 14:38:57 +0200

    [java] IDEA-352189 Support JEP 455. Implement increase language level check and fix
    GitOrigin-RevId: d69e01d624fcfc4ddb3af7292693ed056efc88a4

--- java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/PatternsInSwitchBlockHighlightingModel.java
+++ java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/PatternsInSwitchBlockHighlightingModel.java
@@ -189,6 +189,9 @@ public class PatternsInSwitchBlockHighlightingModel extends SwitchBlockHighlight
             info.registerFix(fix, null, null, null, null);
           }
         }
+        if (patternType instanceof PsiPrimitiveType) {
+          HighlightUtil.registerIncreaseLanguageLevelFixes(mySelector, JavaFeature.PRIMITIVE_TYPES_IN_PATTERNS, info);
+        }
         errorSink.accept(info);
         return true;
       }
@@ -206,6 +209,9 @@ public class PatternsInSwitchBlockHighlightingModel extends SwitchBlockHighlight
             (!IncompleteModelUtil.isPotentiallyConvertible(mySelectorType, patternType, label))) {
           HighlightInfo.Builder error =
             HighlightUtil.createIncompatibleTypeHighlightInfo(mySelectorType, patternType, elementToReport.getTextRange(), 0);
+          if (mySelectorType instanceof PsiPrimitiveType) {
+            HighlightUtil.registerIncreaseLanguageLevelFixes(mySelector, JavaFeature.PRIMITIVE_TYPES_IN_PATTERNS, error);
+          }
           errorSink.accept(error);
           return true;
         }
