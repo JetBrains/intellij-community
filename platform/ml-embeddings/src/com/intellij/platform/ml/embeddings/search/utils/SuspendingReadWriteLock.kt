5403fa309ddd8bc26b0c622d73e26bafa1f1a642
Evgeny Abramov <evgeny.abramov@jetbrains.com>
2024-07-05 17:34:28 +0300

    ML in SE: update SuspendingReadWriteLock
    GitOrigin-RevId: bba3c624dba60674d0c0e018680014c10f50f1e5

--- platform/ml-embeddings/src/com/intellij/platform/ml/embeddings/search/utils/SuspendingReadWriteLock.kt
+++ platform/ml-embeddings/src/com/intellij/platform/ml/embeddings/search/utils/SuspendingReadWriteLock.kt
@@ -1,9 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.ml.embeddings.search.utils
 
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.NonCancellable
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
@@ -14,15 +12,15 @@ class SuspendingReadWriteLock {
   private val counterMutex = Mutex()
 
   suspend fun <T> read(
-    action: suspend CoroutineScope.() -> T,
-  ): T = coroutineScope {
+    action: suspend () -> T,
+  ): T {
     counterMutex.withLock {
       if (++counter == 1) {
         roomEmpty.lock()
       }
     }
     try {
-      action()
+      return action()
     }
     finally {
       withContext(NonCancellable) {
@@ -36,8 +34,10 @@ class SuspendingReadWriteLock {
   }
 
   suspend fun <T> write(
-    action: suspend CoroutineScope.() -> T,
-  ): T = coroutineScope {
-    roomEmpty.withLock { action() }
+    action: suspend () -> T,
+  ): T {
+    return roomEmpty.withLock {
+      action()
+    }
   }
 }
