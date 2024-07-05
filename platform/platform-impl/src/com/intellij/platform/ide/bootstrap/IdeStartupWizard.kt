a6a45cf126fd1ac7b8d05fdcb3cf9fa9f920930e
Ivan Migalev <ivan.migalev@jetbrains.com>
2024-07-05 17:08:13 +0200

    (CPP-39345) IDE Startup Wizard: enable for CLion by default
    #CPP-39345 Fixed
    
    
    (cherry picked from commit b5e3838476ff24f0b493b3f25bea72b3e1d98089)
    
    IJ-CR-139060
    
    GitOrigin-RevId: 9b69b2fa5964f786c64571953dcb2fdf83b59610

--- platform/platform-impl/src/com/intellij/platform/ide/bootstrap/IdeStartupWizard.kt
+++ platform/platform-impl/src/com/intellij/platform/ide/bootstrap/IdeStartupWizard.kt
@@ -13,7 +13,6 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.extensions.impl.ExtensionPointImpl
 import com.intellij.platform.diagnostic.telemetry.impl.span
-import com.intellij.util.PlatformUtils
 import kotlinx.coroutines.*
 import org.jetbrains.annotations.ApiStatus.Internal
 import java.time.Duration
@@ -28,8 +27,7 @@ val isIdeStartupDialogEnabled: Boolean
 
 val isIdeStartupWizardEnabled: Boolean
   get() = !ApplicationManagerEx.isInIntegrationTest() &&
-          System.getProperty("intellij.startup.wizard", "true").toBoolean() &&
-          !PlatformUtils.isCLion()
+          System.getProperty("intellij.startup.wizard", "true").toBoolean()
 
 @ExperimentalCoroutinesApi
 internal suspend fun runStartupWizard(isInitialStart: Job, app: Application) {
