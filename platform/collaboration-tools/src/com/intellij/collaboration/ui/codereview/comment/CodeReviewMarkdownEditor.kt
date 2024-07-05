cca61743e32021d8fe5bd2f7176ca968e976eff1
Ivan Semenov <ivan.semenov@jetbrains.com>
2024-07-05 14:33:16 +0200

    [collab] don't show vertical scrollbar for oneline MD editors
    #IJPL-157854 Fixed
    
    GitOrigin-RevId: 4351cd076aba5fd07079d69ad0590a594a54b088

--- platform/collaboration-tools/src/com/intellij/collaboration/ui/codereview/comment/CodeReviewMarkdownEditor.kt
+++ platform/collaboration-tools/src/com/intellij/collaboration/ui/codereview/comment/CodeReviewMarkdownEditor.kt
@@ -60,7 +60,7 @@ internal object CodeReviewMarkdownEditor {
       if (!inline) {
         component.border = EditorFocusBorder()
       }
-      else {
+      else if (!oneLine) {
         setVerticalScrollbarVisible(true)
       }
       contentComponent.setFocusCycleRoot(false)
