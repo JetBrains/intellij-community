2f4622ceea722e4b58e0f3c39b8e22cecd88a40b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-07-08 16:15:27 +0200

    IJPL-158075 AlarmTest - use Junit 5
    GitOrigin-RevId: 506c1ad9855b7770bb4cc60140133f799e6b37e1

--- platform/platform-tests/testSrc/com/intellij/util/AlarmTest.kt
+++ platform/platform-tests/testSrc/com/intellij/util/AlarmTest.kt
@@ -3,70 +3,79 @@ package com.intellij.util
 
 import com.intellij.concurrency.ConcurrentCollectionFactory
 import com.intellij.diagnostic.PerformanceWatcher.Companion.printStacktrace
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.application.impl.LaterInvocator
-import com.intellij.testFramework.LightPlatformTestCase
-import com.intellij.testFramework.LightPlatformTestCase.assertEquals
 import com.intellij.testFramework.LoggedErrorProcessor
 import com.intellij.testFramework.UsefulTestCase
+import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.testFramework.junit5.TestDisposable
 import com.intellij.util.ui.UIUtil
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.fail
+import org.junit.jupiter.api.Test
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 import java.util.concurrent.atomic.AtomicInteger
-import java.util.stream.Stream
 
-class AlarmTest : LightPlatformTestCase() {
-  fun testTwoAddsWithZeroDelayMustExecuteSequentially() {
-    val alarm = Alarm(testRootDisposable)
+@TestApplication
+class AlarmTest {
+  @Test
+  fun twoAddsWithZeroDelayMustExecuteSequentially(@TestDisposable disposable: Disposable) {
+    val alarm = Alarm(disposable)
     assertRequestsExecuteSequentially(alarm)
   }
 
-  fun testAlarmRequestsShouldExecuteSequentiallyEvenInPooledThread() {
-    val alarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, testRootDisposable)
+  @Test
+  fun alarmRequestsShouldExecuteSequentiallyEvenInPooledThread(@TestDisposable disposable: Disposable) {
+    val alarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, disposable)
     assertRequestsExecuteSequentially(alarm)
   }
 
-  fun testOneAlarmDoesNotStartTooManyThreads() {
-    val alarm = Alarm(testRootDisposable)
+  @Test
+  fun oneAlarmDoesNotStartTooManyThreads(@TestDisposable disposable: Disposable) {
+    val alarm = Alarm(disposable)
     val executed = AtomicInteger()
-    val N = 100000
+    val count = 100000
     val used = ConcurrentCollectionFactory.createConcurrentSet<Thread>()
-    for (i in 0 until N) {
+    for (i in 0 until count) {
       alarm.addRequest({
                          executed.incrementAndGet()
                          used.add(Thread.currentThread())
                        }, 10)
     }
-    while (executed.get() != N) {
+    while (executed.get() != count) {
       UIUtil.dispatchAllInvocationEvents()
     }
     if (used.size > 10) {
-      fail(used.size.toString() + " threads created: " + used.joinToString { printStacktrace("", it, it.stackTrace) })
+      throw AssertionError(used.size.toString() + " threads created: " + used.joinToString { printStacktrace("", it, it.stackTrace) })
     }
   }
 
-  fun testManyAlarmsDoNotStartTooManyThreads() {
+  @Test
+  fun manyAlarmsDoNotStartTooManyThreads(@TestDisposable disposable: Disposable) {
     val used = ConcurrentCollectionFactory.createConcurrentSet<Thread>()
     val executed = AtomicInteger()
-    val N = 100000
-    val alarms = Stream.generate { Alarm(testRootDisposable) }.limit(N.toLong()).toList()
-    alarms.forEach(java.util.function.Consumer { alarm: Alarm ->
+    val count = 100000
+    val alarms = Array(count) { Alarm(disposable) }.toList()
+    for (alarm in alarms) {
       alarm.addRequest({
                          executed.incrementAndGet()
                          used.add(Thread.currentThread())
                        }, 10)
-    })
+    }
 
-    while (executed.get() != N) {
+    while (executed.get() != count) {
       UIUtil.dispatchAllInvocationEvents()
     }
     if (used.size > 10) {
-      fail(used.size.toString() + " threads created: " + used.joinToString { printStacktrace("", it, it.stackTrace) })
+      throw AssertionError(used.size.toString() + " threads created: " + used.joinToString { printStacktrace("", it, it.stackTrace) })
     }
   }
 
+  @Test
   fun testOrderIsPreservedAfterModalitySwitching() {
     val alarm = Alarm()
     val sb = StringBuilder()
@@ -78,7 +87,7 @@ class AlarmTest : LightPlatformTestCase() {
       alarm.addRequest({ sb.append("1") }, 0, ModalityState.nonModal())
       alarm.addRequest({ sb.append("2") }, 5, ModalityState.nonModal())
       UIUtil.dispatchAllInvocationEvents()
-      assertEquals("", sb.toString())
+      org.junit.jupiter.api.Assertions.assertEquals("", sb.toString())
     }
     finally {
       LaterInvocator.leaveModal(modal)
@@ -88,23 +97,24 @@ class AlarmTest : LightPlatformTestCase() {
       UIUtil.dispatchAllInvocationEvents()
     }
 
-    assertEquals("12", sb.toString())
+    org.junit.jupiter.api.Assertions.assertEquals("12", sb.toString())
   }
 
-  fun testFlushImmediately() {
+  @Test
+  fun flushImmediately() {
     val alarm = Alarm()
     val sb = StringBuilder()
 
     alarm.addRequest({ sb.append("1") }, 0, ModalityState.nonModal())
     alarm.addRequest({ sb.append("2") }, 5, ModalityState.nonModal())
-    assertEquals("", sb.toString())
+    org.junit.jupiter.api.Assertions.assertEquals("", sb.toString())
     alarm.drainRequestsInTest()
-    assertEquals("12", sb.toString())
+    org.junit.jupiter.api.Assertions.assertEquals("12", sb.toString())
   }
 
-  @Throws(Exception::class)
-  fun testWaitForAllExecutedMustWaitUntilExecutionFinish() {
-    val alarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, testRootDisposable)
+  @Test
+  fun waitForAllExecutedMustWaitUntilExecutionFinish(@TestDisposable disposable: Disposable) {
+    val alarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, disposable)
     val sb = StringBuffer()
     val start = System.currentTimeMillis()
     val delay = 100
@@ -123,8 +133,8 @@ class AlarmTest : LightPlatformTestCase() {
       System.err.println("No no no no this agent is so overloaded I quit")
       return
     }
-    assertEquals(2, alarm.activeRequestCount)
-    assertEquals("", s)
+    org.junit.jupiter.api.Assertions.assertEquals(2, alarm.activeRequestCount)
+    org.junit.jupiter.api.Assertions.assertEquals("", s)
     try {
       // started to execute but not finished yet
       alarm.waitForAllExecuted(1000, TimeUnit.MILLISECONDS)
@@ -135,11 +145,12 @@ class AlarmTest : LightPlatformTestCase() {
 
     alarm.waitForAllExecuted(3000, TimeUnit.MILLISECONDS)
 
-    assertEquals(2, sb.length)
+    org.junit.jupiter.api.Assertions.assertEquals(2, sb.length)
   }
 
-  fun testExceptionDuringAlarmExecutionMustManifestItselfInTests() {
-    val alarm = Alarm(testRootDisposable)
+  @Test
+  fun exceptionDuringAlarmExecutionMustManifestItselfInTests(@TestDisposable disposable: Disposable) {
+    val alarm = Alarm(disposable)
     val error = LoggedErrorProcessor.executeAndReturnLoggedError {
       alarm.addRequest({
                          throw RuntimeException("wtf")
@@ -155,27 +166,26 @@ class AlarmTest : LightPlatformTestCase() {
       }
       assertTrue(caught)
     }
-    assertEquals("wtf", error.message)
+    org.junit.jupiter.api.Assertions.assertEquals("wtf", error.message)
   }
 
-  fun testSingleAlarmMustRefuseToInstantiateWithWrongModality() {
+  @Test
+  fun singleAlarmMustRefuseToInstantiateWithWrongModality() {
     UsefulTestCase.assertThrows(IllegalArgumentException::class.java) {
-      SingleAlarm(
-        {}, 1, null, Alarm.ThreadToUse.SWING_THREAD, null)
+      SingleAlarm(task = {}, delay = 1, parentDisposable = null, threadToUse = Alarm.ThreadToUse.SWING_THREAD, modalityState = null)
     }
   }
 }
 
 private fun assertRequestsExecuteSequentially(alarm: Alarm) {
-  val N = 10000
-  val log = StringBuffer(N * 4)
-  val expected = StringBuilder(N * 4)
+  val count = 10000
+  val log = StringBuffer(count * 4)
+  val expected = StringBuilder(count * 4)
 
-  for (i in 0 until N) {
-    val finalI = i
-    alarm.addRequest({ log.append(finalI).append(" ") }, 0)
+  for (i in 0 until count) {
+    alarm.addRequest({ log.append(i).append(" ") }, 0)
   }
-  for (i in 0 until N) {
+  for (i in 0 until count) {
     expected.append(i).append(" ")
   }
   val future = ApplicationManager.getApplication().executeOnPooledThread {
@@ -191,5 +201,5 @@ private fun assertRequestsExecuteSequentially(alarm: Alarm) {
   }
   future.get()
   Assertions.assertThat(alarm.isEmpty).isTrue()
-  assertEquals(expected.toString(), log.toString())
+  org.junit.jupiter.api.Assertions.assertEquals(expected.toString(), log.toString())
 }
