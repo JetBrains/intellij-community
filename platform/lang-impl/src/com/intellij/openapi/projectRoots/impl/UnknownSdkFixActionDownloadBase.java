a0519e079cf0e8c3215841fdb643380c62fc4811
Louis Vignier <Louis.Vignier@jetbrains.com>
2024-07-08 09:22:32 +0200

    [jdk] Remove 'Remember my choice' option from SDK download consent dialog
    #IDEA-355522
    
    GitOrigin-RevId: 3481cae2cabed9c64a1d95c4cac4c60984c5d5bd

--- platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkFixActionDownloadBase.java
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkFixActionDownloadBase.java
@@ -4,13 +4,10 @@ package com.intellij.openapi.projectRoots.impl;
 import com.intellij.openapi.application.ApplicationInfo;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.options.advanced.AdvancedSettings;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectBundle;
 import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.ui.DoNotAskOption;
 import com.intellij.openapi.ui.MessageDialogBuilder;
 import com.intellij.openapi.util.registry.Registry;
 import org.jetbrains.annotations.NotNull;
@@ -20,22 +17,6 @@ import javax.swing.*;
 import java.lang.reflect.InvocationTargetException;
 
 public abstract class UnknownSdkFixActionDownloadBase extends UnknownSdkFixActionBase {
-
-  public enum DownloadFixReaction {
-    SHOW_POPUP {
-      @Override
-      public String toString() { return ProjectBundle.message("advanced.setting.sdk.configuration.install.request.ask"); }
-    },
-    ALWAYS_ACCEPT {
-      @Override
-      public String toString() { return ProjectBundle.message("advanced.setting.sdk.configuration.install.request.accept"); }
-    },
-    ALWAYS_REJECT {
-      @Override
-      public String toString() { return ProjectBundle.message("advanced.setting.sdk.configuration.install.request.deny"); }
-    }
-  }
-
   private static final Logger LOG = Logger.getInstance(UnknownSdkFixActionDownloadBase.class);
 
   @NotNull
@@ -58,17 +39,10 @@ public abstract class UnknownSdkFixActionDownloadBase extends UnknownSdkFixActio
   }
 
   private UnknownSdkDownloadTask doCreateTask(@Nullable Project project) {
-    final var reactionSetting = AdvancedSettings.getEnum("sdk.configuration.install.request", DownloadFixReaction.class);
-
-    if (hasConsent() || reactionSetting == DownloadFixReaction.ALWAYS_ACCEPT
+    if (hasConsent()
         || Registry.is("sdk.download.consent", false)
         || ApplicationManager.getApplication().isUnitTestMode()) {
       return createTask().withListener(getMulticaster());
-    }
-
-    if (reactionSetting == DownloadFixReaction.ALWAYS_REJECT) {
-      getMulticaster().onResolveCancelled();
-      return null;
     }
 
     collectConsent(project);
@@ -90,27 +64,7 @@ public abstract class UnknownSdkFixActionDownloadBase extends UnknownSdkFixActio
         ProjectBundle.message("dialog.sdk.download.title"),
         lookupReason != null ? ProjectBundle.message("dialog.sdk.download.message.with.reason", lookupReason, description) : description
       )
-      .yesText(ProjectBundle.message("dialog.sdk.download.yes"))
-      .doNotAsk(new DoNotAskOption() {
-        @Override public boolean isToBeShown() { return true; }
-        @Override public boolean canBeHidden() { return true; }
-        @Override public boolean shouldSaveOptionsOnCancel() { return true; }
-
-        @Override
-        public void setToBeShown(boolean value, int exitCode) {
-          if (value) return;
-          AdvancedSettings.setEnum(
-            "sdk.configuration.install.request",
-            exitCode == DialogWrapper.OK_EXIT_CODE ? DownloadFixReaction.ALWAYS_ACCEPT : DownloadFixReaction.ALWAYS_REJECT
-          );
-        }
-
-        @NotNull
-        @Override
-        public String getDoNotShowMessage() {
-          return ProjectBundle.message("dialog.sdk.download.do.not.ask");
-        }
-      });
+      .yesText(ProjectBundle.message("dialog.sdk.download.yes"));
 
     try {
       SwingUtilities.invokeAndWait(() -> {
