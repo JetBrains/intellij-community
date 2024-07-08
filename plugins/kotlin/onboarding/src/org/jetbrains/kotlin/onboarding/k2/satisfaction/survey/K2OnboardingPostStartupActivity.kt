ae25d68b974459b8dda07c4eb1968ad2d0f328d5
Victoria.Petrakovich <victoria.petrakovich@jetbrains.com>
2024-07-08 22:54:20 +0200

    [kotlin] Add "Enable K2 mode" notification
    KTIJ-30264
    
    GitOrigin-RevId: b97e5633c02f9a377521e196a1a6560905ef5b95

--- plugins/kotlin/onboarding/src/org/jetbrains/kotlin/onboarding/k2/satisfaction/survey/K2OnboardingPostStartupActivity.kt
+++ plugins/kotlin/onboarding/src/org/jetbrains/kotlin/onboarding/k2/satisfaction/survey/K2OnboardingPostStartupActivity.kt
@@ -4,9 +4,13 @@ package org.jetbrains.kotlin.onboarding.k2.satisfaction.survey
 import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
+import org.jetbrains.kotlin.onboarding.k2.EnableK2NotificationService
 
-private class CheckKotlinPluginModeWasSwitchedOnRestartPostStartupActivity : ProjectActivity {
+private class K2OnboardingPostStartupActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
         serviceAsync<K2UserTracker>().checkIfKotlinPluginModeWasSwitchedOnRestart()
+
+        val k2UserTrackerState = serviceAsync<K2UserTracker>().state
+        serviceAsync<EnableK2NotificationService>().showEnableK2Notification(project, k2UserTrackerState)
     }
 }
