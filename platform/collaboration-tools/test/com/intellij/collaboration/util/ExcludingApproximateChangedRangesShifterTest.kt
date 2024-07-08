815a16184cec1a20ade2894e587092f7f22943dc
Ivan Semenov <ivan.semenov@jetbrains.com>
2024-07-08 15:31:45 +0200

    [collab] fix review in editor changes ranges mapper
    "later" chunk iterator was not advanced, which resulted in double shifting
    
    #IJPL-157956 Fixed
    
    
    (cherry picked from commit bb5263215d78e3cc7f2facd82f485c13bfe4e381)
    
    IJ-CR-139248
    
    GitOrigin-RevId: fea9e420da0fc2f5ec0f5f37e38029fecd847efa

--- platform/collaboration-tools/test/com/intellij/collaboration/util/ExcludingApproximateChangedRangesShifterTest.kt
+++ platform/collaboration-tools/test/com/intellij/collaboration/util/ExcludingApproximateChangedRangesShifterTest.kt
@@ -1,9 +1,11 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.collaboration.util
 
+import com.intellij.diff.comparison.iterables.DiffIterableUtil
 import com.intellij.diff.util.Range
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.random.Random
 
 class ExcludingApproximateChangedRangesShifterTest {
   @Test
@@ -96,6 +98,61 @@ class ExcludingApproximateChangedRangesShifterTest {
          }, {
            r(14, 19, 25, 30)
          })
+  }
+
+  /*
+  [7, 24) - [7, 25), [33, 41) - [34, 47) ->
+[20, 25) - [20, 39), [38, 38) - [52, 60)
+   */
+  @Test
+  fun `test mixed 1`() {
+    test({
+           r(7, 25, 7, 24)
+           r(34, 47, 33, 41)
+         }, {
+           r(20, 39, 20, 25)
+           r(52, 60, 38, 38)
+         }, {
+           r(7, 20, 7, 24)
+           r(48, 52, 33, 41)
+           r(60, 69, 33, 41)
+         })
+  }
+
+  @Test
+  fun `test random`() {
+    val random = Random(System.currentTimeMillis())
+    for (i in 0..100000) {
+      val early = buildFairRanges(2, random)
+      val later = buildFairRanges(4, random)
+      try {
+        ExcludingApproximateChangedRangesShifter.shift(early, later)
+      }
+      catch (e: Throwable) {
+        throw Exception("$early -> $later", e)
+      }
+    }
+  }
+
+  private fun buildFairRanges(count: Int, random: Random): List<Range> {
+    val result = mutableListOf<Range>()
+    var last1 = 0
+    var last2 = 0
+    for (i in 0 until count) {
+      val unchanged = random.nextInt(20) + 1
+      val start1 = last1 + unchanged
+      last1 = start1 + random.nextInt(20)
+      val start2 = last2 + unchanged
+      last2 = start2 + random.nextInt(20)
+      val range = Range(start1, last1, start2, last2)
+      if (range.isEmpty) continue
+      result += range
+    }
+    DiffIterableUtil.setVerifyEnabled(true)
+    val iterable = DiffIterableUtil.fair(DiffIterableUtil.create(result, last1 + 10, last2 + 10))
+    DiffIterableUtil.verifyFair(iterable)
+    DiffIterableUtil.setVerifyEnabled(false)
+    return result
   }
 
   private class RangesCollector {
