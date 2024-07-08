882005a521378b9a7a6af86359a0c5b6092f8b32
Artem.Bukhonov <artem.bukhonov@jetbrains.com>
2024-07-08 19:18:34 +0200

    RDCT-1543 Whats New can't be open in Split mode
    Forward whats new file opening to a controller
    
    GitOrigin-RevId: 8d2fdf06904f9047ec98f979eb550e0c48b59590

--- platform/whatsNew/src/com/intellij/platform/whatsNew/WhatsNewShowOnStartCheckService.kt
+++ platform/whatsNew/src/com/intellij/platform/whatsNew/WhatsNewShowOnStartCheckService.kt
@@ -1,8 +1,12 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.whatsNew
 
+import com.intellij.codeWithMe.ClientId
+import com.intellij.codeWithMe.asContextElement
 import com.intellij.idea.AppMode
 import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.client.ClientKind
+import com.intellij.openapi.client.ClientSessionsManager
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
@@ -10,6 +14,7 @@ import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.util.SystemProperties
 import com.intellij.util.application
+import kotlinx.coroutines.withContext
 import java.util.concurrent.atomic.AtomicBoolean
 
 internal class WhatsNewShowOnStartCheckService : ProjectActivity {
@@ -27,12 +32,16 @@ internal class WhatsNewShowOnStartCheckService : ProjectActivity {
         || Registry.`is`("expose.ui.hierarchy.url")) return
     logger.info("Checking whether to show the What's New page on startup.")
 
-    val content = WhatsNewContent.getWhatsNewContent()
-    logger.info("Got What's New content: $content")
-    if (content != null) {
-      if (WhatsNewContentVersionChecker.isNeedToShowContent(content).also { logger.info("Should show What's New: $it") }) {
-        val whatsNewAction = service<ActionManager>().getAction("WhatsNewAction") as? WhatsNewAction
-        whatsNewAction?.openWhatsNew(project)
+    // a bit hacky workaround but now we don't have any tools to forward local startup activities to a controller
+    val clientId = ClientSessionsManager.getAppSessions(ClientKind.CONTROLLER).firstOrNull()?.clientId ?: ClientId.localId
+    withContext(clientId.asContextElement()) {
+      val content = WhatsNewContent.getWhatsNewContent()
+      logger.info("Got What's New content: $content")
+      if (content != null) {
+        if (WhatsNewContentVersionChecker.isNeedToShowContent(content).also { logger.info("Should show What's New: $it") }) {
+          val whatsNewAction = service<ActionManager>().getAction("WhatsNewAction") as? WhatsNewAction
+          whatsNewAction?.openWhatsNew(project)
+        }
       }
     }
   }
