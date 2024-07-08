00260b3477bb78bd4eeecd1369240fd4bea28d6e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-07-08 15:58:58 +0200

    IJPL-158075 refactor AlarmTest
    GitOrigin-RevId: d9c9f5ba1e9943e5025ae2a31ee307ff1fd2d409

--- platform/platform-tests/testSrc/com/intellij/util/AlarmTest.kt
+++ platform/platform-tests/testSrc/com/intellij/util/AlarmTest.kt
@@ -1,195 +1,195 @@
- // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.util;
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.util
 
- import com.intellij.concurrency.ConcurrentCollectionFactory;
- import com.intellij.diagnostic.PerformanceWatcher;
- import com.intellij.openapi.application.ApplicationManager;
- import com.intellij.openapi.application.ModalityState;
- import com.intellij.openapi.application.impl.LaterInvocator;
- import com.intellij.testFramework.LightPlatformTestCase;
- import com.intellij.testFramework.LoggedErrorProcessor;
- import com.intellij.util.ui.UIUtil;
- import org.jetbrains.annotations.NotNull;
+import com.intellij.concurrency.ConcurrentCollectionFactory
+import com.intellij.diagnostic.PerformanceWatcher.Companion.printStacktrace
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.impl.LaterInvocator
+import com.intellij.testFramework.LightPlatformTestCase
+import com.intellij.testFramework.LightPlatformTestCase.assertEquals
+import com.intellij.testFramework.LoggedErrorProcessor
+import com.intellij.testFramework.UsefulTestCase
+import com.intellij.util.ui.UIUtil
+import org.assertj.core.api.Assertions
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.stream.Stream
 
- import java.util.List;
- import java.util.Set;
- import java.util.concurrent.ExecutionException;
- import java.util.concurrent.Future;
- import java.util.concurrent.TimeUnit;
- import java.util.concurrent.TimeoutException;
- import java.util.concurrent.atomic.AtomicInteger;
- import java.util.stream.Collectors;
- import java.util.stream.Stream;
-
- import static org.assertj.core.api.Assertions.assertThat;
-
- public class AlarmTest extends LightPlatformTestCase {
-  public void testTwoAddsWithZeroDelayMustExecuteSequentially() throws Exception {
-    Alarm alarm = new Alarm(getTestRootDisposable());
-    assertRequestsExecuteSequentially(alarm);
+class AlarmTest : LightPlatformTestCase() {
+  fun testTwoAddsWithZeroDelayMustExecuteSequentially() {
+    val alarm = Alarm(testRootDisposable)
+    assertRequestsExecuteSequentially(alarm)
   }
 
-  public void testAlarmRequestsShouldExecuteSequentiallyEvenInPooledThread() throws Exception {
-    Alarm alarm = new Alarm(Alarm.ThreadToUse.POOLED_THREAD, getTestRootDisposable());
-    assertRequestsExecuteSequentially(alarm);
+  fun testAlarmRequestsShouldExecuteSequentiallyEvenInPooledThread() {
+    val alarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, testRootDisposable)
+    assertRequestsExecuteSequentially(alarm)
   }
 
-  private static void assertRequestsExecuteSequentially(@NotNull Alarm alarm) throws InterruptedException, ExecutionException {
-    int N = 10000;
-    StringBuffer log = new StringBuffer(N*4);
-    StringBuilder expected = new StringBuilder(N * 4);
-
-    for (int i = 0; i < N; i++) {
-      final int finalI = i;
-      alarm.addRequest(() -> log.append(finalI).append(" "), 0);
-    }
-    for (int i = 0; i < N; i++) {
-      expected.append(i).append(" ");
-    }
-    Future<?> future = ApplicationManager.getApplication().executeOnPooledThread(() -> {
-      try {
-        alarm.waitForAllExecuted(100, TimeUnit.SECONDS);
-      }
-      catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-    });
-    while (!future.isDone()) {
-      UIUtil.dispatchAllInvocationEvents();
-    }
-    future.get();
-    assertThat(alarm.isEmpty()).isTrue();
-    assertEquals(expected.toString(), log.toString());
-  }
-
-  public void testOneAlarmDoesNotStartTooManyThreads() {
-    Alarm alarm = new Alarm(getTestRootDisposable());
-    AtomicInteger executed = new AtomicInteger();
-    int N = 100000;
-    Set<Thread> used = ConcurrentCollectionFactory.createConcurrentSet();
-    for (int i = 0; i < N; i++) {
-      alarm.addRequest(() -> {
-        executed.incrementAndGet();
-        used.add(Thread.currentThread());
-      }, 10);
+  fun testOneAlarmDoesNotStartTooManyThreads() {
+    val alarm = Alarm(testRootDisposable)
+    val executed = AtomicInteger()
+    val N = 100000
+    val used = ConcurrentCollectionFactory.createConcurrentSet<Thread>()
+    for (i in 0 until N) {
+      alarm.addRequest({
+                         executed.incrementAndGet()
+                         used.add(Thread.currentThread())
+                       }, 10)
     }
     while (executed.get() != N) {
-      UIUtil.dispatchAllInvocationEvents();
+      UIUtil.dispatchAllInvocationEvents()
     }
-    if (used.size() > 10) {
-      fail(used.size()+" threads created: "+used.stream().map(t->PerformanceWatcher.printStacktrace("", t, t.getStackTrace())).collect(Collectors.joining()));
+    if (used.size > 10) {
+      fail(used.size.toString() + " threads created: " + used.joinToString { printStacktrace("", it, it.stackTrace) })
     }
   }
 
-  public void testManyAlarmsDoNotStartTooManyThreads() {
-    Set<Thread> used = ConcurrentCollectionFactory.createConcurrentSet();
-    AtomicInteger executed = new AtomicInteger();
-    int N = 100000;
-    List<Alarm> alarms = Stream.generate(() -> new Alarm(getTestRootDisposable())).limit(N).toList();
-    alarms.forEach(alarm -> alarm.addRequest(() -> {
-      executed.incrementAndGet();
-      used.add(Thread.currentThread());
-    }, 10));
+  fun testManyAlarmsDoNotStartTooManyThreads() {
+    val used = ConcurrentCollectionFactory.createConcurrentSet<Thread>()
+    val executed = AtomicInteger()
+    val N = 100000
+    val alarms = Stream.generate { Alarm(testRootDisposable) }.limit(N.toLong()).toList()
+    alarms.forEach(java.util.function.Consumer { alarm: Alarm ->
+      alarm.addRequest({
+                         executed.incrementAndGet()
+                         used.add(Thread.currentThread())
+                       }, 10)
+    })
 
     while (executed.get() != N) {
-      UIUtil.dispatchAllInvocationEvents();
+      UIUtil.dispatchAllInvocationEvents()
     }
-    if (used.size() > 10) {
-      fail(used.size()+" threads created: "+used.stream().map(t->PerformanceWatcher.printStacktrace("", t, t.getStackTrace())).collect(Collectors.joining()));
+    if (used.size > 10) {
+      fail(used.size.toString() + " threads created: " + used.joinToString { printStacktrace("", it, it.stackTrace) })
     }
   }
 
-  public void testOrderIsPreservedAfterModalitySwitching() {
-    Alarm alarm = new Alarm();
-    StringBuilder sb = new StringBuilder();
-    Object modal = new Object();
-    LaterInvocator.enterModal(modal);
+  fun testOrderIsPreservedAfterModalitySwitching() {
+    val alarm = Alarm()
+    val sb = StringBuilder()
+    val modal = Any()
+    LaterInvocator.enterModal(modal)
 
     try {
-      ApplicationManager.getApplication().invokeLater(() -> TimeoutUtil.sleep(10), ModalityState.nonModal());
-      alarm.addRequest(() -> sb.append("1"), 0, ModalityState.nonModal());
-      alarm.addRequest(() -> sb.append("2"), 5, ModalityState.nonModal());
-      UIUtil.dispatchAllInvocationEvents();
-      assertEquals("", sb.toString());
+      ApplicationManager.getApplication().invokeLater({ TimeoutUtil.sleep(10) }, ModalityState.nonModal())
+      alarm.addRequest({ sb.append("1") }, 0, ModalityState.nonModal())
+      alarm.addRequest({ sb.append("2") }, 5, ModalityState.nonModal())
+      UIUtil.dispatchAllInvocationEvents()
+      assertEquals("", sb.toString())
     }
     finally {
-      LaterInvocator.leaveModal(modal);
+      LaterInvocator.leaveModal(modal)
     }
 
-    while (!alarm.isEmpty()) {
-      UIUtil.dispatchAllInvocationEvents();
+    while (!alarm.isEmpty) {
+      UIUtil.dispatchAllInvocationEvents()
     }
 
-    assertEquals("12", sb.toString());
+    assertEquals("12", sb.toString())
   }
 
-  public void testFlushImmediately() {
-    Alarm alarm = new Alarm();
-    StringBuilder sb = new StringBuilder();
+  fun testFlushImmediately() {
+    val alarm = Alarm()
+    val sb = StringBuilder()
 
-    alarm.addRequest(() -> sb.append("1"), 0, ModalityState.nonModal());
-    alarm.addRequest(() -> sb.append("2"), 5, ModalityState.nonModal());
-    assertEquals("", sb.toString());
-    alarm.drainRequestsInTest();
-    assertEquals("12", sb.toString());
+    alarm.addRequest({ sb.append("1") }, 0, ModalityState.nonModal())
+    alarm.addRequest({ sb.append("2") }, 5, ModalityState.nonModal())
+    assertEquals("", sb.toString())
+    alarm.drainRequestsInTest()
+    assertEquals("12", sb.toString())
   }
 
-  public void testWaitForAllExecutedMustWaitUntilExecutionFinish() throws Exception {
-    Alarm alarm = new Alarm(Alarm.ThreadToUse.POOLED_THREAD, getTestRootDisposable());
-    StringBuffer sb = new StringBuffer();
-    long start = System.currentTimeMillis();
-    int delay = 100;
-    alarm.addRequest(() -> {
-      TimeoutUtil.sleep(1000);
-      sb.append("1");
-    }, delay);
-    alarm.addRequest(() -> {
-      TimeoutUtil.sleep(1000);
-      sb.append("2");
-    }, delay*2);
+  @Throws(Exception::class)
+  fun testWaitForAllExecutedMustWaitUntilExecutionFinish() {
+    val alarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, testRootDisposable)
+    val sb = StringBuffer()
+    val start = System.currentTimeMillis()
+    val delay = 100
+    alarm.addRequest({
+                       TimeoutUtil.sleep(1000)
+                       sb.append("1")
+                     }, delay)
+    alarm.addRequest({
+                       TimeoutUtil.sleep(1000)
+                       sb.append("2")
+                     }, delay * 2)
 
-    String s = sb.toString();
-    long elapsed = System.currentTimeMillis() - start;
-    if (elapsed > delay/2) {
-      System.err.println("No no no no this agent is so overloaded I quit");
-      return;
+    val s = sb.toString()
+    val elapsed = System.currentTimeMillis() - start
+    if (elapsed > delay / 2) {
+      System.err.println("No no no no this agent is so overloaded I quit")
+      return
     }
-    assertEquals(2, alarm.getActiveRequestCount());
-    assertEquals("", s);
+    assertEquals(2, alarm.activeRequestCount)
+    assertEquals("", s)
     try {
       // started to execute but not finished yet
-      alarm.waitForAllExecuted(1000, TimeUnit.MILLISECONDS);
-      fail();
+      alarm.waitForAllExecuted(1000, TimeUnit.MILLISECONDS)
+      fail()
     }
-    catch (TimeoutException ignored) {
+    catch (ignored: TimeoutException) {
     }
 
-    alarm.waitForAllExecuted(3000, TimeUnit.MILLISECONDS);
+    alarm.waitForAllExecuted(3000, TimeUnit.MILLISECONDS)
 
-    assertEquals(2, sb.length());
+    assertEquals(2, sb.length)
   }
 
-  public void testExceptionDuringAlarmExecutionMustManifestItselfInTests() {
-    Alarm alarm = new Alarm(getTestRootDisposable());
-    Throwable error = LoggedErrorProcessor.executeAndReturnLoggedError(() -> {
-      alarm.addRequest(() -> {
-        throw new RuntimeException("wtf");
-      }, 1);
-      boolean caught = false;
-      while (!alarm.isEmpty()) {
+  fun testExceptionDuringAlarmExecutionMustManifestItselfInTests() {
+    val alarm = Alarm(testRootDisposable)
+    val error = LoggedErrorProcessor.executeAndReturnLoggedError {
+      alarm.addRequest({
+                         throw RuntimeException("wtf")
+                       }, 1)
+      var caught = false
+      while (!alarm.isEmpty) {
         try {
-          UIUtil.dispatchAllInvocationEvents();
+          UIUtil.dispatchAllInvocationEvents()
         }
-        catch (RuntimeException e) {
-          caught |= "wtf".equals(e.getMessage());
+        catch (e: RuntimeException) {
+          caught = caught or ("wtf" == e.message)
         }
       }
-      assertTrue(caught);
-    });
-    assertEquals("wtf", error.getMessage());
+      assertTrue(caught)
+    }
+    assertEquals("wtf", error.message)
   }
 
-  public void testSingleAlarmMustRefuseToInstantiateWithWrongModality() {
-    assertThrows(IllegalArgumentException.class, () -> new SingleAlarm(() -> {}, 1, null, Alarm.ThreadToUse.SWING_THREAD, null));
+  fun testSingleAlarmMustRefuseToInstantiateWithWrongModality() {
+    UsefulTestCase.assertThrows(IllegalArgumentException::class.java) {
+      SingleAlarm(
+        {}, 1, null, Alarm.ThreadToUse.SWING_THREAD, null)
+    }
   }
+}
+
+private fun assertRequestsExecuteSequentially(alarm: Alarm) {
+  val N = 10000
+  val log = StringBuffer(N * 4)
+  val expected = StringBuilder(N * 4)
+
+  for (i in 0 until N) {
+    val finalI = i
+    alarm.addRequest({ log.append(finalI).append(" ") }, 0)
+  }
+  for (i in 0 until N) {
+    expected.append(i).append(" ")
+  }
+  val future = ApplicationManager.getApplication().executeOnPooledThread {
+    try {
+      alarm.waitForAllExecuted(100, TimeUnit.SECONDS)
+    }
+    catch (e: Exception) {
+      throw RuntimeException(e)
+    }
+  }
+  while (!future.isDone) {
+    UIUtil.dispatchAllInvocationEvents()
+  }
+  future.get()
+  Assertions.assertThat(alarm.isEmpty).isTrue()
+  assertEquals(expected.toString(), log.toString())
 }
