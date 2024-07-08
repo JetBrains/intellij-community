383df78317bffaaca487703a0598cb5808467498
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2024-07-08 17:51:03 +0200

    [whatsnew] exit on AppMode.isRemoteDevHost since remdev doesn't support whatsnew
    GitOrigin-RevId: d08c80e60d0b9a880851ca85d355faae8cfdd41c

--- platform/whatsNew/src/com/intellij/platform/whatsNew/WhatsNewShowOnStartCheckService.kt
+++ platform/whatsNew/src/com/intellij/platform/whatsNew/WhatsNewShowOnStartCheckService.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.whatsNew
 
+import com.intellij.idea.AppMode
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.logger
@@ -16,7 +17,7 @@ internal class WhatsNewShowOnStartCheckService : ProjectActivity {
 
   override suspend fun execute(project: Project) {
     if (ourStarted.getAndSet(true)) return
-    if (application.isHeadlessEnvironment || application.isUnitTestMode || isPlaybackMode) return
+    if (application.isHeadlessEnvironment || application.isUnitTestMode || isPlaybackMode || AppMode.isRemoteDevHost()) return
     logger.info("Checking whether to show the What's New page on startup.")
 
     val content = WhatsNewContent.getWhatsNewContent()
