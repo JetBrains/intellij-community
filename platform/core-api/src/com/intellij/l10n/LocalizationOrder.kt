1521db0e52a8fa56b682b340c91c3d8838d7bf06
Vera Petrenkova <Vera.Petrenkova@jetbrains.com>
2024-07-09 17:51:49 +0200

    IJPL-157370 [i18n] change the priority of translations
    Make the localization folder/suffix translations more prioritized than translations from plugin. It shouldn't affect the platform translations because we don't have folder/suffix localized translations inside the platform
    
    (cherry picked from commit 3fc56f6a87a24eb579cceae31f87853b72f26458)
    
    
    (cherry picked from commit bb2b92900a6755555f9e8c71f841369bdd325214)
    
    IJ-CR-139831
    
    GitOrigin-RevId: c15718f5a25fe3c1f307a2f25857b1de988f751c

--- platform/core-api/src/com/intellij/l10n/LocalizationOrder.kt
+++ platform/core-api/src/com/intellij/l10n/LocalizationOrder.kt
@@ -6,17 +6,30 @@ import java.nio.file.Path
 
 @ApiStatus.Internal
 enum class LocalizationOrder {
-  DEFAULT_PLUGIN,
   FOLDER_REGION_LEVEL_PLATFORM, //localization/zh/CN/
   SUFFIX_REGION_LEVEL_PLATFORM, //name_zh_CN.properties
   FOLDER_LANGUAGE_LEVEL_PLATFORM, //localization/zh/
   SUFFIX_LANGUAGE_LEVEL_PLATFORM,  //name_zh.properties
+  DEFAULT_PLUGIN,  //name.properties inside plugin
   DEFAULT_PLATFORM; //name.properties
 
   companion object {
     fun getLocalizationOrder(orderedPaths: List<Path?>, bundlePath: Path): LocalizationOrder? {
-      val order = orderedPaths.indexOf(bundlePath) + 1
-      return if (order >= 0 && order < entries.size) entries[order] else null
+      return when (orderedPaths.size) {
+        1 ->  DEFAULT_PLATFORM
+        3 -> {
+          when (orderedPaths.indexOf(bundlePath)) {
+            0 ->  FOLDER_LANGUAGE_LEVEL_PLATFORM
+            1 ->  SUFFIX_LANGUAGE_LEVEL_PLATFORM
+            else -> DEFAULT_PLATFORM
+          }
+        }
+        else -> {
+          val order = orderedPaths.indexOf(bundlePath)
+          if (order == DEFAULT_PLUGIN.ordinal) DEFAULT_PLATFORM
+          else if (order >= 0 && order < entries.size) entries[order] else null
+        }
+      }
     }
   }
 }
