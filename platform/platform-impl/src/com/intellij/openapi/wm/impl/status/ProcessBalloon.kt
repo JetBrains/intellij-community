a5bbde2ddff91c74a67ca6497f97722d360d1f78
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-07-10 06:44:09 +0200

    IJPL-158075 refactor ProcessBalloon
    GitOrigin-RevId: 3ceb413652e78f4ab7ff389bd54ef90345d7de5a

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/status/ProcessBalloon.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/status/ProcessBalloon.kt
@@ -1,192 +1,174 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.wm.impl.status;
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.wm.impl.status
 
-import com.intellij.ide.lightEdit.LightEditCompatible;
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.fileEditor.impl.EditorsSplitters;
-import com.intellij.openapi.ui.popup.Balloon;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
-import com.intellij.openapi.ui.popup.JBPopupListener;
-import com.intellij.openapi.ui.popup.LightweightWindowEvent;
-import com.intellij.openapi.wm.IdeFrame;
-import com.intellij.openapi.wm.impl.IdeRootPane;
-import com.intellij.openapi.wm.impl.status.InfoAndProgressPanel.MyInlineProgressIndicator;
-import com.intellij.toolWindow.ToolWindowPane;
-import com.intellij.ui.BalloonLayoutImpl;
-import com.intellij.ui.ComponentUtil;
-import com.intellij.ui.Gray;
-import com.intellij.ui.TabbedPaneWrapper;
-import com.intellij.ui.awt.RelativePoint;
-import com.intellij.ui.scale.JBUIScale;
-import com.intellij.util.ui.JBInsets;
-import com.intellij.util.ui.JBUI;
-import com.intellij.util.ui.PositionTracker;
-import com.intellij.util.ui.UIUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.ide.lightEdit.LightEditCompatible
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.fileEditor.impl.EditorsSplitters
+import com.intellij.openapi.ui.popup.Balloon
+import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.openapi.ui.popup.JBPopupListener
+import com.intellij.openapi.ui.popup.LightweightWindowEvent
+import com.intellij.openapi.wm.IdeFrame
+import com.intellij.openapi.wm.impl.IdeRootPane
+import com.intellij.openapi.wm.impl.status.InfoAndProgressPanel.MyInlineProgressIndicator
+import com.intellij.toolWindow.ToolWindowPane
+import com.intellij.ui.BalloonLayoutImpl
+import com.intellij.ui.ComponentUtil
+import com.intellij.ui.Gray
+import com.intellij.ui.TabbedPaneWrapper
+import com.intellij.ui.awt.RelativePoint
+import com.intellij.ui.scale.JBUIScale.scale
+import com.intellij.util.ui.JBInsets
+import com.intellij.util.ui.JBUI
+import com.intellij.util.ui.PositionTracker
+import com.intellij.util.ui.UIUtil
+import java.awt.Component
+import java.awt.Point
+import javax.swing.JComponent
+import javax.swing.JRootPane
+import javax.swing.SwingUtilities
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
+internal class ProcessBalloon(private val maxVisible: Int) {
+  private val indicators = ArrayList<MyInlineProgressIndicator>()
+  private var isVisible = 0
 
-final class ProcessBalloon {
-  private final List<MyInlineProgressIndicator> myIndicators = new ArrayList<>();
-  private final int myMaxVisible;
-  private int myVisible;
-
-  ProcessBalloon(int visibleCount) {
-    myMaxVisible = visibleCount;
-  }
-
-  public void addIndicator(@Nullable JRootPane pane, @NotNull MyInlineProgressIndicator indicator) {
+  fun addIndicator(pane: JRootPane?, indicator: MyInlineProgressIndicator) {
     if (pane != null) {
-      myIndicators.add(indicator);
-      show(pane);
+      indicators.add(indicator)
+      show(pane)
     }
   }
 
-  public void removeIndicator(@Nullable JRootPane pane, @NotNull MyInlineProgressIndicator indicator) {
-    myIndicators.remove(indicator);
+  fun removeIndicator(pane: JRootPane?, indicator: MyInlineProgressIndicator) {
+    indicators.remove(indicator)
 
     if (indicator.presentationModeProgressPanel != null) {
-      myVisible--;
+      isVisible--
 
-      if (pane != null && !myIndicators.isEmpty()) {
-        show(pane);
+      if (pane != null && !indicators.isEmpty()) {
+        show(pane)
       }
     }
   }
 
-  private void show(@NotNull JRootPane pane) {
-    List<MyInlineProgressIndicator> indicators = new ArrayList<>();
+  private fun show(pane: JRootPane) {
+    val indicators = ArrayList<MyInlineProgressIndicator>()
 
-    for (MyInlineProgressIndicator indicator : myIndicators) {
+    for (indicator in this.indicators) {
       if (indicator.presentationModeProgressPanel == null) {
-        if (myVisible == myMaxVisible) {
-          continue;
+        if (isVisible == maxVisible) {
+          continue
         }
 
-        myVisible++;
+        isVisible++
 
-        indicator.presentationModeProgressPanel = new PresentationModeProgressPanel(indicator);
-        indicator.updateProgressNow();
+        indicator.presentationModeProgressPanel = PresentationModeProgressPanel(indicator)
+        indicator.updateProgressNow()
 
-        indicator.presentationModeBalloon = create(pane, indicator, indicator.presentationModeProgressPanel.getProgressPanel());
-        indicator.presentationModeShowBalloon = true;
+        indicator.presentationModeBalloon = create(pane, indicator, indicator.presentationModeProgressPanel!!.progressPanel)
+        indicator.presentationModeShowBalloon = true
 
-        indicators.add(indicator);
+        indicators.add(indicator)
       }
-      else if (!indicator.presentationModeBalloon.isDisposed()) {
-        indicators.add(indicator);
+      else if (!indicator.presentationModeBalloon!!.isDisposed) {
+        indicators.add(indicator)
       }
     }
 
-    for (MyInlineProgressIndicator indicator : indicators) {
+    for (indicator in indicators) {
       if (indicator.presentationModeShowBalloon) {
-        indicator.presentationModeShowBalloon = false;
+        indicator.presentationModeShowBalloon = false
 
-        indicator.presentationModeBalloon.show(new PositionTracker<>(getAnchor(pane)) {
-          @Override
-          public RelativePoint recalculateLocation(@NotNull Balloon balloon) {
-            Component c = getAnchor(pane);
-            int y = c.getHeight() - JBUIScale.scale(45);
+        indicator.presentationModeBalloon!!.show(object : PositionTracker<Balloon>(getAnchor(pane)) {
+          override fun recalculateLocation(balloon: Balloon): RelativePoint {
+            val c = getAnchor(pane)
+            var y = c.height - scale(45)
 
-            BalloonLayoutImpl balloonLayout = getBalloonLayout(pane);
+            val balloonLayout = getBalloonLayout(pane)
             if (balloonLayout != null && !isBottomSideToolWindowsVisible(pane)) {
-              Component component = balloonLayout.getTopBalloonComponent();
+              val component = balloonLayout.topBalloonComponent
               if (component != null) {
-                y = SwingUtilities.convertPoint(component, 0, -JBUIScale.scale(45), c).y;
+                y = SwingUtilities.convertPoint(component, 0, -scale(45), c).y
               }
             }
 
-            if (myVisible > 1) {
-              int index = myIndicators.indexOf(indicator);
-              int rowHeight = balloon.getPreferredSize().height + JBUI.scale(5);
-              y -= rowHeight * (myVisible - index - 1);
+            if (isVisible > 1) {
+              val index = this@ProcessBalloon.indicators.indexOf(indicator)
+              val rowHeight = balloon.preferredSize.height + JBUI.scale(5)
+              y -= rowHeight * (isVisible - index - 1)
             }
 
-            return new RelativePoint(c, new Point(c.getWidth() - JBUIScale.scale(150), y));
+            return RelativePoint(c, Point(c.width - scale(150), y))
           }
-        }, Balloon.Position.above);
+        }, Balloon.Position.above)
       }
       else {
-        indicator.presentationModeBalloon.revalidate();
+        indicator.presentationModeBalloon!!.revalidate()
       }
     }
   }
+}
 
-  private static @NotNull Balloon create(@NotNull JRootPane pane, @NotNull Disposable parentDisposable, @NotNull JComponent content) {
-    content.putClientProperty(InfoAndProgressPanel.FAKE_BALLOON, new Object());
+private fun create(pane: JRootPane, parentDisposable: Disposable, content: JComponent): Balloon {
+  content.putClientProperty(InfoAndProgressPanel.FAKE_BALLOON, Any())
 
-    Balloon balloon = JBPopupFactory.getInstance().createBalloonBuilder(content)
-      .setFadeoutTime(0)
-      .setFillColor(Gray.TRANSPARENT)
-      .setShowCallout(false)
-      .setBorderColor(Gray.TRANSPARENT)
-      .setBorderInsets(JBInsets.emptyInsets())
-      .setAnimationCycle(0)
-      .setCloseButtonEnabled(false)
-      .setHideOnClickOutside(false)
-      .setDisposable(parentDisposable)
-      .setHideOnFrameResize(false)
-      .setHideOnKeyOutside(false)
-      .setBlockClicksThroughBalloon(true)
-      .setHideOnAction(false)
-      .setShadow(false)
-      .createBalloon();
+  val balloon = JBPopupFactory.getInstance().createBalloonBuilder(content)
+    .setFadeoutTime(0)
+    .setFillColor(Gray.TRANSPARENT)
+    .setShowCallout(false)
+    .setBorderColor(Gray.TRANSPARENT)
+    .setBorderInsets(JBInsets.emptyInsets())
+    .setAnimationCycle(0)
+    .setCloseButtonEnabled(false)
+    .setHideOnClickOutside(false)
+    .setDisposable(parentDisposable)
+    .setHideOnFrameResize(false)
+    .setHideOnKeyOutside(false)
+    .setBlockClicksThroughBalloon(true)
+    .setHideOnAction(false)
+    .setShadow(false)
+    .createBalloon()
 
-    BalloonLayoutImpl balloonLayout = getBalloonLayout(pane);
-    if (balloonLayout != null) {
-      final class MyListener implements JBPopupListener, Runnable {
-        @Override
-        public void beforeShown(@NotNull LightweightWindowEvent event) {
-          balloonLayout.addListener(this);
-        }
+  val balloonLayout = getBalloonLayout(pane) ?: return balloon
+  balloon.addListener(object : JBPopupListener, Runnable {
+    override fun beforeShown(event: LightweightWindowEvent) {
+      balloonLayout.addListener(this)
+    }
 
-        @Override
-        public void onClosed(@NotNull LightweightWindowEvent event) {
-          balloonLayout.removeListener(this);
-        }
+    override fun onClosed(event: LightweightWindowEvent) {
+      balloonLayout.removeListener(this)
+    }
 
-        @Override
-        public void run() {
-          if (!balloon.isDisposed()) {
-            balloon.revalidate();
-          }
-        }
+    override fun run() {
+      if (!balloon.isDisposed) {
+        balloon.revalidate()
       }
-      balloon.addListener(new MyListener());
     }
+  })
+  return balloon
+}
 
-    return balloon;
+private fun getBalloonLayout(pane: JRootPane): BalloonLayoutImpl? {
+  val parent = ComponentUtil.findUltimateParent(pane)
+  return if (parent is IdeFrame) parent.balloonLayout as? BalloonLayoutImpl else null
+}
+
+private fun getAnchor(pane: JRootPane): Component {
+  if (pane is IdeRootPane && pane !is LightEditCompatible) {
+    val component = pane.getToolWindowPane().getDocumentComponent()
+    return if (component == null || !component.isShowing) pane else component
   }
 
-  private static @Nullable BalloonLayoutImpl getBalloonLayout(@NotNull JRootPane pane) {
-    Component parent = ComponentUtil.findUltimateParent(pane);
-    if (parent instanceof IdeFrame) {
-      return (BalloonLayoutImpl)((IdeFrame)parent).getBalloonLayout();
-    }
-    return null;
+  val tabWrapper: Component? = UIUtil.findComponentOfType(pane, TabbedPaneWrapper.TabWrapper::class.java)
+  if (tabWrapper != null && tabWrapper.isShowing) {
+    return tabWrapper
   }
 
-  private static @NotNull Component getAnchor(@NotNull JRootPane pane) {
-    if (pane instanceof IdeRootPane ideRootPane && !(ideRootPane instanceof LightEditCompatible)) {
-      JComponent component = ideRootPane.getToolWindowPane().getDocumentComponent();
-      return component == null || !component.isShowing() ? pane : component;
-    }
+  val splitters = UIUtil.findComponentOfType(pane, EditorsSplitters::class.java)
+  return if (splitters == null || !splitters.isShowing) pane else splitters
+}
 
-    Component tabWrapper = UIUtil.findComponentOfType(pane, TabbedPaneWrapper.TabWrapper.class);
-    if (tabWrapper != null && tabWrapper.isShowing()) {
-      return tabWrapper;
-    }
-
-    EditorsSplitters splitters = UIUtil.findComponentOfType(pane, EditorsSplitters.class);
-    return splitters == null || !splitters.isShowing() ? pane : splitters;
-  }
-
-  private static boolean isBottomSideToolWindowsVisible(@NotNull JRootPane parent) {
-    ToolWindowPane pane = UIUtil.findComponentOfType(parent, ToolWindowPane.class);
-    return pane != null && pane.isBottomSideToolWindowsVisible();
-  }
+private fun isBottomSideToolWindowsVisible(parent: JRootPane): Boolean {
+  val pane = UIUtil.findComponentOfType(parent, ToolWindowPane::class.java)
+  return pane != null && pane.isBottomSideToolWindowsVisible
 }
