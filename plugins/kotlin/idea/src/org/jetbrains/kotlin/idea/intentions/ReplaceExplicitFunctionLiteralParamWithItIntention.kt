8964bcba12918795401ef40ac948ba1cfc4ace37
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2024-07-10 18:07:22 +0400

    [kotlin] Introduce `ReplaceExplicitLambdaParameterWithItUtils`
    to share functions that don't require analysis between K1 and K2 implementations of ReplaceExplicitFunctionLiteralParamWithItIntention.
    
    ^KTIJ-30542
    
    GitOrigin-RevId: 3a4abe5ef00cc53c14136cd8a4853a7812f583f7

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/ReplaceExplicitFunctionLiteralParamWithItIntention.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/ReplaceExplicitFunctionLiteralParamWithItIntention.kt
@@ -5,63 +5,40 @@ package org.jetbrains.kotlin.idea.intentions
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
-import com.intellij.psi.codeStyle.CodeStyleManager
-import com.intellij.refactoring.rename.RenameProcessor
-import com.intellij.usageView.UsageInfo
-import org.jetbrains.kotlin.builtins.StandardNames
 import org.jetbrains.kotlin.descriptors.ParameterDescriptor
 import org.jetbrains.kotlin.descriptors.impl.AnonymousFunctionDescriptor
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.caches.resolve.analyzeAsReplacement
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
-import org.jetbrains.kotlin.idea.base.psi.copied
+import org.jetbrains.kotlin.idea.caches.resolve.analyzeAsReplacement
+import org.jetbrains.kotlin.idea.codeinsights.impl.base.intentions.ReplaceExplicitLambdaParameterWithItUtils
+import org.jetbrains.kotlin.idea.codeinsights.impl.base.intentions.ReplaceExplicitLambdaParameterWithItUtils.ParamRenamingProcessor
 import org.jetbrains.kotlin.idea.references.resolveMainReferenceToDescriptors
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.*
 import org.jetbrains.kotlin.resolve.DescriptorToSourceUtils
 import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
-import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 class ReplaceExplicitFunctionLiteralParamWithItIntention : PsiElementBaseIntentionAction() {
     override fun getFamilyName() = KotlinBundle.message("replace.explicit.lambda.parameter.with.it")
 
     override fun isAvailable(project: Project, editor: Editor, element: PsiElement): Boolean {
         val functionLiteral = targetFunctionLiteral(element, editor.caretModel.offset) ?: return false
-
-        val parameter = functionLiteral.valueParameters.singleOrNull() ?: return false
-        if (parameter.typeReference != null) return false
-        if (parameter.destructuringDeclaration != null) return false
-
-        if (functionLiteral.anyDescendantOfType<KtFunctionLiteral> { literal ->
-                literal.usesName(element.text) && (!literal.hasParameterSpecification() || literal.usesName(StandardNames.IMPLICIT_LAMBDA_PARAMETER_NAME.identifier))
-            }) return false
-
-        val lambda = functionLiteral.parent as? KtLambdaExpression ?: return false
-        val lambdaParent = lambda.parent
-        if (lambdaParent is KtWhenEntry || lambdaParent is KtContainerNodeForControlStructureBody) return false
+        val explicitParameterName = functionLiteral.valueParameters.singleOrNull()?.name ?: return false
+        val lambda = ReplaceExplicitLambdaParameterWithItUtils.getLambda(element, functionLiteral) ?: return false
         val call = lambda.getStrictParentOfType<KtCallExpression>()
         if (call != null) {
             val argumentIndex = call.valueArguments.indexOfFirst { it.getArgumentExpression() == lambda }
             val callOrQualified = call.getQualifiedExpressionForSelectorOrThis()
-            val newCallOrQualified = callOrQualified.copied()
-            val newCall = newCallOrQualified.safeAs<KtQualifiedExpression>()?.callExpression
-                ?: newCallOrQualified as? KtCallExpression
-                ?: return false
-            val newArgument = newCall.valueArguments.getOrNull(argumentIndex) ?: newCall.lambdaArguments.singleOrNull() ?: return false
-            newArgument.replace(KtPsiFactory(project).createLambdaExpression("", "TODO()"))
-            val newContext = newCallOrQualified.analyzeAsReplacement(callOrQualified, callOrQualified.analyze(BodyResolveMode.PARTIAL))
-            if (newCallOrQualified.getResolvedCall(newContext)?.resultingDescriptor == null) return false
+            val analyzableExpression = ReplaceExplicitLambdaParameterWithItUtils.createAnalyzableExpression(project, argumentIndex, callOrQualified)
+                                       ?: return false
+            val newContext = analyzableExpression.analyzeAsReplacement(callOrQualified, callOrQualified.analyze(BodyResolveMode.PARTIAL))
+            if (analyzableExpression.getResolvedCall(newContext)?.resultingDescriptor == null) return false
         }
 
-        text = KotlinBundle.message("replace.explicit.parameter.0.with.it", parameter.name.toString())
+        text = KotlinBundle.message("replace.explicit.parameter.0.with.it", explicitParameterName)
         return true
-    }
-
-    private fun KtFunctionLiteral.usesName(name: String): Boolean = anyDescendantOfType<KtSimpleNameExpression> { nameExpr ->
-        nameExpr.getReferencedName() == name
     }
 
     override fun startInWriteAction(): Boolean = false
@@ -85,32 +62,5 @@ class ReplaceExplicitFunctionLiteralParamWithItIntention : PsiElementBaseIntenti
         val arrow = functionLiteral.arrow ?: return null
         if (caretOffset > arrow.endOffset) return null
         return functionLiteral
-    }
-
-    private class ParamRenamingProcessor(
-        val editor: Editor,
-        val functionLiteral: KtFunctionLiteral,
-        val cursorWasInParameterList: Boolean
-    ) : RenameProcessor(
-        editor.project!!,
-        functionLiteral.valueParameters.single(),
-        StandardNames.IMPLICIT_LAMBDA_PARAMETER_NAME.identifier,
-        false,
-        false
-    ) {
-        override fun performRefactoring(usages: Array<out UsageInfo>) {
-            super.performRefactoring(usages)
-
-            functionLiteral.deleteChildRange(functionLiteral.valueParameterList, functionLiteral.arrow ?: return)
-
-            if (cursorWasInParameterList) {
-                editor.caretModel.moveToOffset(functionLiteral.bodyExpression?.textOffset ?: return)
-            }
-
-            val project = functionLiteral.project
-            PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(editor.document)
-            CodeStyleManager.getInstance(project).adjustLineIndent(functionLiteral.containingFile, functionLiteral.textRange)
-
-        }
     }
 }
