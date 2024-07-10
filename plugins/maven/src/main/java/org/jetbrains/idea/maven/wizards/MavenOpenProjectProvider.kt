bd71423da596f3b6e4dd1c46781422deef73ce64
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2024-07-10 21:32:53 +0200

    [maven] IDEA-356082 MavenOpenProjectProvider.unlinkProject
    GitOrigin-RevId: 134c29039f42301b65e598bfde360d31574903e0

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/wizards/MavenOpenProjectProvider.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/wizards/MavenOpenProjectProvider.kt
@@ -1,18 +1,24 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.maven.wizards
 
+import com.intellij.openapi.application.EDT
 import com.intellij.openapi.externalSystem.importing.AbstractOpenProjectProvider
 import com.intellij.openapi.externalSystem.model.ProjectSystemId
 import com.intellij.openapi.externalSystem.service.project.trusted.ExternalSystemTrustedProjectDialog
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.platform.backend.observation.trackActivity
 import icons.OpenapiIcons
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.idea.maven.project.MavenProjectBundle
+import org.jetbrains.idea.maven.project.MavenProjectsManager
 import org.jetbrains.idea.maven.utils.MavenActivityKey
 import org.jetbrains.idea.maven.utils.MavenUtil
+import java.nio.file.Path
 import javax.swing.Icon
 
 class MavenOpenProjectProvider : AbstractOpenProjectProvider() {
@@ -38,6 +44,15 @@ class MavenOpenProjectProvider : AbstractOpenProjectProvider() {
     val syncProject = !autoImportDisabled
 
     doLinkToExistingProjectAsync(projectFile, project, syncProject)
+  }
+
+  override suspend fun unlinkProject(project: Project, externalProjectPath: String) {
+    val projectsManager = MavenProjectsManager.getInstance(project)
+    val folder = VirtualFileManager.getInstance().findFileByNioPath(Path.of(externalProjectPath)) ?: return
+    val projectPomFile = projectsManager.projects.firstOrNull { it.file.parent == folder }?.file ?: return
+    withContext(Dispatchers.EDT) {
+      projectsManager.removeManagedFiles(listOf(projectPomFile), null, null)
+    }
   }
 
   @ApiStatus.Internal
