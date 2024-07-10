38235a1f93b147dc0eea7b9efdcea7f18bb27217
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-07-10 18:40:19 +0200

    [testing] Fix PluginManagerCore @Remote interfaces
    GitOrigin-RevId: fc530a7600c5594b6da36238d157b66834fcd993

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/PluginManager.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/PluginManager.kt
@@ -3,19 +3,19 @@ package com.intellij.driver.sdk
 import com.intellij.driver.client.Driver
 import com.intellij.driver.client.Remote
 
-fun Driver.getPlugin(id: String) = utility(PluginManagerCoreRef::class).getPlugin(utility(PluginIdRef::class).getId(id))
+fun Driver.getPlugin(id: String) = utility(PluginManagerCore::class).getPlugin(utility(PluginId::class).getId(id))
 
 @Remote("com.intellij.openapi.extensions.PluginId")
-interface PluginIdRef {
-  fun getId(id: String): PluginIdRef
+interface PluginId {
+  fun getId(id: String): PluginId
 }
 
 @Remote("com.intellij.ide.plugins.PluginManagerCore")
-interface PluginManagerCoreRef {
-  fun getPlugin(pluginId: PluginIdRef): PluginDescriptorRef
+interface PluginManagerCore {
+  fun getPlugin(pluginId: PluginId): PluginDescriptor
 }
 
 @Remote("com.intellij.openapi.extensions.PluginDescriptor")
-interface PluginDescriptorRef {
+interface PluginDescriptor {
   fun isEnabled(): Boolean
 }
