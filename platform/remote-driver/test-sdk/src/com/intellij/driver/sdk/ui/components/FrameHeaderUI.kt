a922b020de044a829cfade47b583640839a6229f
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-07-10 14:25:49 +0200

    Revert "[driver][ui-tests] support toolbarHeader locator on linux"
    This reverts commit a2e4627b959ca03329f36066264a9f3a91a10a54.
    
    GitOrigin-RevId: d25efbcbcbde3d1e2e9d889acf78e94cf4e10c6d

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/FrameHeaderUI.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/FrameHeaderUI.kt
@@ -1,13 +1,9 @@
 package com.intellij.driver.sdk.ui.components
 
 import com.intellij.driver.sdk.ui.Finder
-import com.intellij.driver.sdk.ui.xQuery
-import com.intellij.openapi.util.SystemInfo.isLinux
 
-val Finder.toolbarHeader: FrameHeaderUI
-  get() = x(xQuery {
-    or(byClass("MacToolbarFrameHeader"), if (isLinux) byClass("MainToolbar") else byClass("ToolbarFrameHeader"))
-  }, FrameHeaderUI::class.java)
+val Finder.toolbarHeader: FrameHeaderUI get() =
+  x("//div[@class='MacToolbarFrameHeader' or @class='ToolbarFrameHeader']", FrameHeaderUI::class.java)
 
 class FrameHeaderUI(data: ComponentData) : UiComponent(data) {
   val separateRowMenu: UiComponent get() = x("//div[@class='IdeJMenuBar']")
