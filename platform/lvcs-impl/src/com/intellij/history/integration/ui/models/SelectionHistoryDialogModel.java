029dd4b903428d9eda9677e5aa45082540ed9c35
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2024-07-10 15:19:58 +0200

    [lvcs] move the selection history dialog model to model package
    GitOrigin-RevId: 767c24ce55f0b491c798cf2d1a0b8b2e2ba9deb5

--- platform/lvcs-impl/src/com/intellij/history/integration/ui/models/SelectionHistoryDialogModel.java
+++ platform/lvcs-impl/src/com/intellij/history/integration/ui/models/SelectionHistoryDialogModel.java
@@ -1,12 +1,11 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
-package com.intellij.history.integration.ui.views;
+package com.intellij.history.integration.ui.models;
 
 import com.intellij.history.core.LocalHistoryFacade;
 import com.intellij.history.integration.IdeaGateway;
 import com.intellij.history.integration.revertion.Reverter;
 import com.intellij.history.integration.revertion.SelectionReverter;
-import com.intellij.history.integration.ui.models.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.platform.lvcs.impl.statistics.LocalHistoryCounter;
@@ -14,7 +13,6 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
-
 
 public class SelectionHistoryDialogModel extends FileHistoryDialogModel {
   private RevisionSelectionCalculator myCalculatorCache;
