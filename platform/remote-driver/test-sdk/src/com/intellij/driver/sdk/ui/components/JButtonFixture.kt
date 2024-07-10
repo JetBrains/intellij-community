61f5e2d96de506f6ce7f4fdb48cf62e88038c3ae
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2024-07-10 10:44:13 +0200

    [driver-tests] add button search method with QueryBuilder
    GitOrigin-RevId: 8f83a59d6ae3537d4c546e67dc49b38d38e2a295

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/JButtonFixture.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/JButtonFixture.kt
@@ -2,10 +2,13 @@ package com.intellij.driver.sdk.ui.components
 
 import com.intellij.driver.client.Remote
 import com.intellij.driver.sdk.ui.Finder
+import com.intellij.driver.sdk.ui.QueryBuilder
 import org.intellij.lang.annotations.Language
 
 fun Finder.button(text: String) =
   x("//div[@class='JButton' and @visible_text='$text']", JButtonUiComponent::class.java)
+
+fun Finder.button(locator: QueryBuilder.() -> String) = x(JButtonUiComponent::class.java) { locator() }
 
 fun Finder.buttons(@Language("xpath") xpath: String? = null) =
   xx(xpath ?: "//div[@class='JButton']", JButtonUiComponent::class.java)
