7fc0d03238181346d29512c71a7c489805916f8e
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2024-07-11 15:57:08 +0300

    IJPL-43794 Add 'Start Onboarding Tour' action to the help menu
    GitOrigin-RevId: e5209b7b6da1eb0ecc7e8cb5945547b09c78bced

---
+++ platform/new-ui-onboarding/src/com/intellij/platform/ide/newUiOnboarding/newUsers/StartNewUsersOnboardingAction.kt
@@ -0,0 +1,22 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.ide.newUiOnboarding.newUsers
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.platform.ide.newUiOnboarding.newUsers.NewUsersOnboardingStatistics.OnboardingStartingPlace
+
+internal class StartNewUsersOnboardingAction : DumbAwareAction() {
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project ?: return
+    NewUsersOnboardingService.getInstance(project).startOnboarding()
+    NewUsersOnboardingStatistics.logOnboardingStarted(project, OnboardingStartingPlace.ACTION)
+  }
+
+  override fun update(e: AnActionEvent) {
+    val project = e.project
+    e.presentation.isEnabledAndVisible = project != null && NewUsersOnboardingService.getInstance(project).isOnboardingEnabled()
+  }
+
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+}
