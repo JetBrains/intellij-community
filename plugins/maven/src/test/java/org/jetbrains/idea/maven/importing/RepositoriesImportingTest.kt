f211c773704c703d2af428ed08e6771a88a04dbd
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2024-07-11 14:18:32 +0200

    [maven] IDEA-355974 RepositoriesImportingTest: refresh VFS explicitly
    GitOrigin-RevId: d7bd9b44e591bcd97b5190b1a376060df3c317b4

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/RepositoriesImportingTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/RepositoriesImportingTest.kt
@@ -2,9 +2,8 @@
 package org.jetbrains.idea.maven.importing
 
 import com.intellij.jarRepository.RemoteRepositoriesConfiguration
-import com.intellij.jarRepository.RemoteRepositoryDescription
 import com.intellij.maven.testFramework.MavenMultiVersionImportingTestCase
-import com.intellij.util.containers.ContainerUtil
+import com.intellij.openapi.vfs.LocalFileSystem
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
@@ -26,6 +25,7 @@ class RepositoriesImportingTest : MavenMultiVersionImportingTestCase() {
           </mirrors>
         </settings>
         """.trimIndent())
+      LocalFileSystem.getInstance().refreshFiles(listOf(settingsXml))
       mavenGeneralSettings.setUserSettingsFile(settingsXml.canonicalPath)
 
       importProjectAsync("""
@@ -58,6 +58,7 @@ class RepositoriesImportingTest : MavenMultiVersionImportingTestCase() {
           </mirrors>
         </settings>
         """.trimIndent())
+      LocalFileSystem.getInstance().refreshFiles(listOf(settingsXml))
       mavenGeneralSettings.setUserSettingsFile(settingsXml.canonicalPath)
 
       importProjectAsync("""
@@ -90,6 +91,7 @@ class RepositoriesImportingTest : MavenMultiVersionImportingTestCase() {
           </mirrors>
         </settings>
         """.trimIndent())
+      LocalFileSystem.getInstance().refreshFiles(listOf(settingsXml))
       mavenGeneralSettings.setUserSettingsFile(settingsXml.canonicalPath)
 
       importProjectAsync("""
@@ -108,16 +110,14 @@ class RepositoriesImportingTest : MavenMultiVersionImportingTestCase() {
   }
 
   private fun assertDoNotHaveRepositories(vararg repos: String) {
-    val actual = ContainerUtil.map(
-      RemoteRepositoriesConfiguration.getInstance(project).repositories) { it: RemoteRepositoryDescription -> it.url }
+    val actual = RemoteRepositoriesConfiguration.getInstance(project).repositories.map { it.url }
 
     assertDoNotContain(actual, *repos)
   }
 
 
   private fun assertHaveRepositories(vararg repos: String) {
-    val actual = ContainerUtil.map(
-      RemoteRepositoriesConfiguration.getInstance(project).repositories) { it: RemoteRepositoryDescription -> it.url }
+    val actual = RemoteRepositoriesConfiguration.getInstance(project).repositories.map { it.url }
 
     assertContain(actual, *repos)
   }
