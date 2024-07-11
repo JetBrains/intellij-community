ed4a4d69dc76d001b54f916b5fa9aeef835aa04f
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2024-07-12 03:14:29 +0400

    PY-64586, PY-73739, PY-73825: Move jupyter support for python to the Python plugin and DataSpell plugin instead of creating dedicated plugin
    (cherry picked from commit 7d7ef5dc7285bfdc797e1d763254f854ab66b78c)
    
    
    (cherry picked from commit 7df15bb24f198dcd38101dd66735800e63abb235)
    
    IJ-MR-139855
    
    GitOrigin-RevId: 3befd0ce4305b455cafbc8149c06a70f0be1a9b3

--- jupyter/src/com/intellij/notebooks/jupyter/core/jupyter/JupyterLanguage.kt
+++ jupyter/src/com/intellij/notebooks/jupyter/core/jupyter/JupyterLanguage.kt
@@ -1,10 +1,10 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.jupyter.core.jupyter
+package com.intellij.notebooks.jupyter.core.jupyter
 
 import com.intellij.lang.Language
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.fileTypes.LanguageFileType
-import com.intellij.jupyter.core.lang.NotebookLanguage
+import com.intellij.notebooks.jupyter.core.lang.NotebookLanguage
 
 object JupyterLanguage : Language(NotebookLanguage, "Jupyter")
 
