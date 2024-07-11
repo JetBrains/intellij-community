781d0ad5f13def1205becbf027d2e9fac118fe2e
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2024-07-11 05:42:29 +0400

    [kotlin] Convert AddEmptyArgumentListFix to ModCommandAction
    ^KTIJ-29476
    
    GitOrigin-RevId: 51ef64fbd48222f4982885cb3c6ca6267e34eb68

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/AddEmptyArgumentListFix.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/AddEmptyArgumentListFix.kt
@@ -1,28 +1,34 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.idea.quickfix
 
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
+import com.intellij.modcommand.ActionContext
+import com.intellij.modcommand.ModPsiUpdater
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.quickfixes.KotlinQuickFixAction
-import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.KotlinPsiUpdateModCommandAction
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtPsiFactory
+import org.jetbrains.kotlin.psi.createValueArgumentListByPattern
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
-class AddEmptyArgumentListFix(callExpression: KtCallExpression) : KotlinQuickFixAction<KtCallExpression>(callExpression) {
-    override fun getText() = KotlinBundle.message("add.empty.argument.list")
+class AddEmptyArgumentListFix(
+    element: KtCallExpression
+) : KotlinPsiUpdateModCommandAction.ElementBased<KtCallExpression, Unit>(element, Unit) {
+    override fun getFamilyName(): String = KotlinBundle.message("add.empty.argument.list")
 
-    override fun getFamilyName() = text
-
-    override fun invoke(project: Project, editor: Editor?, file: KtFile) {
-        val callExpression = this.element ?: return
-        val calleeExpression = callExpression.calleeExpression ?: return
-        val emptyArgumentList = KtPsiFactory(project).createValueArgumentListByPattern("()")
-        callExpression.addAfter(emptyArgumentList, calleeExpression)
+    override fun invoke(
+        actionContext: ActionContext,
+        element: KtCallExpression,
+        elementContext: Unit,
+        updater: ModPsiUpdater,
+    ) {
+        val calleeExpression = element.calleeExpression ?: return
+        val emptyArgumentList = KtPsiFactory(actionContext.project).createValueArgumentListByPattern("()")
+        element.addAfter(emptyArgumentList, calleeExpression)
     }
 
     companion object : KotlinSingleIntentionActionFactory() {
         override fun createAction(diagnostic: Diagnostic) =
-            diagnostic.psiElement.parent.safeAs<KtCallExpression>()?.let { AddEmptyArgumentListFix(it) }
+            diagnostic.psiElement.parent.safeAs<KtCallExpression>()?.let { AddEmptyArgumentListFix(it) }?.asIntention()
     }
 }
