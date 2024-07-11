2a01d8b35f471999fec0d910645dfda985c58f56
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-07-11 17:22:48 +0200

    IJPL-158097 EssentialHighlightingMode - use API for a hot scenario
    GitOrigin-RevId: c9806a19c45fb7415f0f2ff92f869e2f43fec9b7

--- platform/core-api/src/com/intellij/ide/EssentialHighlightingMode.kt
+++ platform/core-api/src/com/intellij/ide/EssentialHighlightingMode.kt
@@ -1,14 +1,14 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide
 
 import com.intellij.openapi.util.registry.RegistryManager
 
 object EssentialHighlightingMode {
-  fun isEnabled(): Boolean {
-    return RegistryManager.getInstance().`is`("ide.highlighting.mode.essential")
-  }
+  private val handle = RegistryManager.getInstance().get("ide.highlighting.mode.essential")
+
+  fun isEnabled(): Boolean = handle.asBoolean()
 
   fun setEnabled(value: Boolean) {
-    RegistryManager.getInstance().get("ide.highlighting.mode.essential").setValue(value)
+    handle.setValue(value)
   }
 }
