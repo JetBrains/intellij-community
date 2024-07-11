2548dae368f9e31e30631657f2df3430a298ec93
Sergei Tachenov <Sergei.Tachenov@jetbrains.com>
2024-07-11 15:45:00 +0300

    IJPL-157074 Support UnixPtyProcess in UnixProcessManager.getProcessId
    This reflection mess relies on the exact class name. Therefore,
    it doesn't work for custom Process implementations. While we
    still support Java 8 here, the only really safe thing we can do
    here to support UnixPtyProcess is to explicitly add it to the pid()
    white list. It'll do for now until we finally get rid of Java 8
    legacy, which should be soon enough already.
    
    (cherry picked from commit 226db5fe288ecd8a1f80777fc5c6ade9e5f51aeb)
    
    
    (cherry picked from commit 8061b93cd706b70a51a6cbb1dbda4065c02379f1)
    
    IJ-MR-141730
    
    GitOrigin-RevId: c0f50da4e03b4b4405718a952a39993beb821056

--- platform/util/src/com/intellij/execution/process/UnixProcessManager.java
+++ platform/util/src/com/intellij/execution/process/UnixProcessManager.java
@@ -62,7 +62,11 @@ public final class UnixProcessManager {
   @ReviseWhenPortedToJDK("9")
   public static int getProcessId(@NotNull Process process) {
     try {
-      if (JavaVersion.current().feature >= 9 && "java.lang.ProcessImpl".equals(process.getClass().getName())) {
+      if (JavaVersion.current().feature >= 9 &&
+          ("java.lang.ProcessImpl".equals(process.getClass().getName()) ||
+           "com.pty4j.unix.UnixPtyProcess".equals(process.getClass().getName())
+          )
+      ) {
         return ((Long)Process.class.getMethod("pid").invoke(process)).intValue();
       }
       else {
