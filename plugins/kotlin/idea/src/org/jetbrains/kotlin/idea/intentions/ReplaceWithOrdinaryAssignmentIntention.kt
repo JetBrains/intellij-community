1db13278156af6ebc48abcea3d108b694cab4b30
Roman Golyshev <roman.golyshev@jetbrains.com>
2024-07-12 14:21:54 +0200

    KTIJ-30602 [kotlin] Migrate `ReplaceWithOrdinaryAssignmentIntention` to ModCommand API
    Now it does not need to be mentioned in `allowResolveInWriteAction.txt`,
    because it performs the resolve on BG and not on the EDT
    
    GitOrigin-RevId: 2f44eba9a9fba7d0831339fee02fc259bd29084b

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/ReplaceWithOrdinaryAssignmentIntention.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/ReplaceWithOrdinaryAssignmentIntention.kt
@@ -3,23 +3,39 @@
 package org.jetbrains.kotlin.idea.intentions
 
 import com.intellij.codeInsight.intention.LowPriorityAction
+import com.intellij.codeInspection.util.IntentionFamilyName
+import com.intellij.modcommand.ActionContext
+import com.intellij.modcommand.ModPsiUpdater
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.util.TextRange
+import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.caches.resolve.resolveToCall
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.asUnit
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.KotlinApplicableModCommandAction
+import org.jetbrains.kotlin.idea.codeinsight.api.applicators.ApplicabilityRange
 import org.jetbrains.kotlin.idea.codeinsight.api.classic.intentions.SelfTargetingIntention
+import org.jetbrains.kotlin.idea.codeinsights.impl.base.applicators.ApplicabilityRanges
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 import org.jetbrains.kotlin.util.OperatorNameConventions
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
-class ReplaceWithOrdinaryAssignmentIntention : SelfTargetingIntention<KtBinaryExpression>(
-    KtBinaryExpression::class.java,
-    KotlinBundle.lazyMessage("replace.with.ordinary.assignment")
+class ReplaceWithOrdinaryAssignmentIntention : KotlinApplicableModCommandAction<KtBinaryExpression, Unit>(
+    KtBinaryExpression::class,
 ), LowPriorityAction {
-    override fun isApplicableTo(element: KtBinaryExpression, caretOffset: Int): Boolean {
+
+    override fun getFamilyName(): @IntentionFamilyName String = KotlinBundle.message("replace.with.ordinary.assignment")
+
+    override fun getApplicableRanges(element: KtBinaryExpression): List<TextRange> =
+        ApplicabilityRange.single(element) { it.operationReference }
+
+    context(KaSession)
+    override fun prepareContext(element: KtBinaryExpression): Unit? = isApplicableTo(element).asUnit
+
+    private fun isApplicableTo(element: KtBinaryExpression): Boolean {
         val operationReference = element.operationReference
-        if (!operationReference.textRange.containsOffset(caretOffset)) return false
         if (element.operationToken !in KtTokens.AUGMENTED_ASSIGNMENTS) return false
         val left = element.left ?: return false
         if (left.safeAs<KtQualifiedExpression>()?.receiverExpression is KtQualifiedExpression) return false
@@ -29,7 +45,7 @@ class ReplaceWithOrdinaryAssignmentIntention : SelfTargetingIntention<KtBinaryEx
         return resultingDescriptor.name !in OperatorNameConventions.ASSIGNMENT_OPERATIONS
     }
 
-    override fun applyTo(element: KtBinaryExpression, editor: Editor?) {
+    override fun invoke(actionContext: ActionContext, element: KtBinaryExpression, elementContext: Unit, updater: ModPsiUpdater) {
         val left = element.left!!
         val right = element.right!!
         val psiFactory = KtPsiFactory(element.project)
