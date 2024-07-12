81cc21054f244db627f5af4c3d60d53ddc00fa18
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2024-07-12 20:57:28 +0400

    [kotlin] RenameModToRemFix: generate preview
    IJ-CR-139617
    ^KTIJ-29628
    
    GitOrigin-RevId: 0ad0db21fd0d374d308d9052c6a2bb43d6a98d1b

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RenameModToRemFix.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RenameModToRemFix.kt
@@ -1,8 +1,11 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.quickfix
 
+import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.refactoring.rename.RenameProcessor
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.codeinsight.api.classic.quickfixes.KotlinQuickFixAction
@@ -16,6 +19,11 @@ class RenameModToRemFix(element: KtNamedFunction, val newName: Name) : KotlinQui
     override fun getFamilyName(): String = text
 
     override fun startInWriteAction(): Boolean = false
+
+    override fun generatePreview(project: Project, editor: Editor, file: PsiFile): IntentionPreviewInfo {
+        PsiTreeUtil.findSameElementInCopy(element, file)?.setName(newName.asString())
+        return IntentionPreviewInfo.DIFF
+    }
 
     override fun invoke(project: Project, editor: Editor?, file: KtFile) {
         RenameProcessor(project, element ?: return, newName.asString(), false, false).run()
