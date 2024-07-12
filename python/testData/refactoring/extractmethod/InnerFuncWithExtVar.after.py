5174c885a8c68fba63cd4145e2a95202f65c1071
somethingnew179 <kondakov.ilya1@gmail.com>
2024-07-12 14:30:41 +0300

    PY-34626 Fix isMethodContext to exclude inner functions as methods
    closes https://github.com/JetBrains/intellij-community/pull/2811
    
    
    (cherry picked from commit 9268c21cf03158738ca059f8b19c803cb9c368f3)
    
    IJ-CR-143317
    
    GitOrigin-RevId: a4f54bb5fb9d1b1ef4e77f97e66206cfa4c549f2

---
+++ python/testData/refactoring/extractmethod/InnerFuncWithExtVar.after.py
@@ -0,0 +1,11 @@
+class Test:
+    def method(self, x):
+        def func():
+            y = extracted()
+            return y
+
+        def extracted():
+            y = x * 2
+            return y
+
+        pass
