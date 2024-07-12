418c05fdee2f04c61573b18705a34646885a3fad
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2024-07-12 09:49:53 +0300

    IJPL-43794 Show Onboarding dialog on the first IDE start
    (cherry picked from commit 8fc413591da1a1d8b7b8d848c165f1ec4eb443ef)
    
    IJ-CR-140970
    
    GitOrigin-RevId: 44a5e7f4e49e81a44d5b31db01e56acb7c0bcef1

---
+++ platform/new-ui-onboarding/src/com/intellij/platform/ide/newUiOnboarding/newUsers/NewUsersOnboardingStartupActivity.kt
@@ -0,0 +1,29 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.ide.newUiOnboarding.newUsers
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ConfigImportHelper
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.components.serviceAsync
+import com.intellij.openapi.extensions.ExtensionNotApplicableException
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+internal class NewUsersOnboardingStartupActivity : ProjectActivity {
+  init {
+    if (ApplicationManager.getApplication().isUnitTestMode) {
+      throw ExtensionNotApplicableException.create()
+    }
+  }
+
+  override suspend fun execute(project: Project) {
+    val newUsersOnboardingService = project.serviceAsync<NewUsersOnboardingService>()
+    if (newUsersOnboardingService.isOnboardingEnabled() && ConfigImportHelper.isNewUser()) {
+      withContext(Dispatchers.EDT) {
+        newUsersOnboardingService.showOnboardingDialog()
+      }
+    }
+  }
+}
