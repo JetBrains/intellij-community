fd5fce2d2998edb32f1df04f23c883bb228a5521
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2024-07-15 13:33:54 +0200

    [es] IJPL-797 `external.system.api` review internal API
    GitOrigin-RevId: baf55b991daece118772f294c72631d982e6a0c9

--- platform/external-system-api/src/com/intellij/openapi/externalSystem/dependency/analyzer/DependencyAnalyzerUtil.kt
+++ platform/external-system-api/src/com/intellij/openapi/externalSystem/dependency/analyzer/DependencyAnalyzerUtil.kt
@@ -1,9 +1,11 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:ApiStatus.Internal
 package com.intellij.openapi.externalSystem.dependency.analyzer
 
 import com.intellij.buildsystem.model.unified.UnifiedCoordinates
 import com.intellij.buildsystem.model.unified.UnifiedDependency
 import com.intellij.openapi.module.Module
+import org.jetbrains.annotations.ApiStatus
 import com.intellij.openapi.externalSystem.dependency.analyzer.DependencyAnalyzerDependency as Dependency
 
 
--- platform/external-system-api/src/com/intellij/openapi/externalSystem/settings/workspaceModel/entities.kt
+++ platform/external-system-api/src/com/intellij/openapi/externalSystem/settings/workspaceModel/entities.kt
@@ -1,12 +1,9 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:ApiStatus.Internal
 package com.intellij.openapi.externalSystem.settings.workspaceModel
 
-import com.intellij.platform.workspace.storage.EntitySource
-import com.intellij.platform.workspace.storage.EntityType
-import com.intellij.platform.workspace.storage.GeneratedCodeApiVersion
-import com.intellij.platform.workspace.storage.MutableEntityStorage
-import com.intellij.platform.workspace.storage.WorkspaceEntity
-import com.intellij.platform.workspace.storage.impl.containers.toMutableWorkspaceList
+import com.intellij.platform.workspace.storage.*
+import org.jetbrains.annotations.ApiStatus
 
 object ExternalProjectsBuildClasspathEntitySource: EntitySource
 
