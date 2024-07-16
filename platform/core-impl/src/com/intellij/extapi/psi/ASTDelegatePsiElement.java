ec6800fea9d0ef18060be9a744f53551e95411c8
Max Medvedev <maxim.medvedev@jetbrains.com>
2024-07-16 17:06:24 +0200

    assert invalid access if containing file is missing
    GitOrigin-RevId: 7df376985766ae0f7e5150521aee5484bbc5cb5a

--- platform/core-impl/src/com/intellij/extapi/psi/ASTDelegatePsiElement.java
+++ platform/core-impl/src/com/intellij/extapi/psi/ASTDelegatePsiElement.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 /*
  * @author max
@@ -42,7 +42,9 @@ public abstract class ASTDelegatePsiElement extends PsiElementBase {
 
   @Override
   public PsiFile getContainingFile() {
-    return SharedImplUtil.getContainingFile(getNode());
+    PsiFile file = SharedImplUtil.getContainingFile(getNode());
+    if (file == null) throw new PsiInvalidElementAccessException(this);
+    return file;
   }
 
   @Override
