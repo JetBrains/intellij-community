e226446f3dfd2f70ce492bddf0ff90245f6a8822
Max Medvedev <maxim.medvedev@jetbrains.com>
2024-07-16 13:35:45 +0200

    IJPL-158427 simplify IOException handling for MarketplaceRequests#getMarketplacePlugins
    It should not throw IOException at all. Instead it should log it and return an empty result
    
    GitOrigin-RevId: adf14deb80aaebbc2169d629a2595ed27da0e7db

--- platform/platform-impl/src/com/intellij/ide/plugins/marketplace/MarketplaceRequests.kt
+++ platform/platform-impl/src/com/intellij/ide/plugins/marketplace/MarketplaceRequests.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.plugins.marketplace
 
 import com.fasterxml.jackson.core.type.TypeReference
@@ -309,7 +309,6 @@ class MarketplaceRequests(private val coroutineScope: CoroutineScope) : PluginIn
 
   @RequiresBackgroundThread
   @JvmOverloads
-  @Throws(IOException::class)
   fun getMarketplacePlugins(indicator: ProgressIndicator? = null): Set<PluginId> {
     try {
       return readOrUpdateFile(
@@ -320,25 +319,15 @@ class MarketplaceRequests(private val coroutineScope: CoroutineScope) : PluginIn
         ::parseXmlIds,
       )
     }
-    catch (e: UnknownHostException) {
+    catch (e: IOException) {
       LOG.infoOrDebug("Cannot get plugins from Marketplace", e)
-      return emptySet()
-    }
-    catch (es: SocketTimeoutException) {
-      LOG.infoOrDebug("Cannot get plugins from Marketplace", es)
       return emptySet()
     }
   }
 
   override fun loadPlugins(indicator: ProgressIndicator?): Future<Set<PluginId>> {
     return ApplicationManager.getApplication().executeOnPooledThread(Callable {
-      try {
-        getMarketplacePlugins(indicator)
-      }
-      catch (e: IOException) {
-        LOG.infoOrDebug("Cannot get plugins from Marketplace", e)
-        emptySet()
-      }
+      getMarketplacePlugins(indicator)
     })
   }
 
