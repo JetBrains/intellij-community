254e9fa76339226870db3b8574a38c6e2269d7e5
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2024-07-17 14:15:18 +0300

    IJPL-43794 Highlight Learn tool window button
    To make it more noticeable.
    
    (cherry picked from commit 08ff8711e50f0e5cf70cc555f9b7b1b10ce3e212)
    
    IJ-CR-140970
    
    GitOrigin-RevId: 853b65cbbd9763831be138522865792383c23415

--- plugins/ide-features-trainer/onboarding/src/com/intellij/featuresTrainer/onboarding/LearnToolWindowStep.kt
+++ plugins/ide-features-trainer/onboarding/src/com/intellij/featuresTrainer/onboarding/LearnToolWindowStep.kt
@@ -1,16 +1,19 @@
 package com.intellij.featuresTrainer.onboarding
 
+import com.intellij.openapi.actionSystem.impl.ActionButton
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.popup.Balloon
 import com.intellij.openapi.util.CheckedDisposable
 import com.intellij.openapi.util.Disposer
 import com.intellij.platform.ide.newUiOnboarding.NewUiOnboardingStep
 import com.intellij.platform.ide.newUiOnboarding.NewUiOnboardingStepData
+import com.intellij.platform.ide.newUiOnboarding.NewUiOnboardingUtil
 import com.intellij.ui.GotItComponentBuilder
 import com.intellij.ui.awt.RelativePoint
 import com.intellij.util.ui.JBUI
 import kotlinx.coroutines.yield
 import training.learn.LearnBundle
+import training.ui.LearningUiHighlightingManager
 import training.util.learningToolWindow
 import java.awt.Point
 import kotlin.math.min
@@ -19,6 +22,23 @@ internal class LearnToolWindowStep : NewUiOnboardingStep {
   override suspend fun performStep(project: Project, disposable: CheckedDisposable): NewUiOnboardingStepData? {
     val toolWindow = learningToolWindow(project) ?: return null
     toolWindow.show(null)
+
+    val learnStripeButton = NewUiOnboardingUtil.findUiComponent(project) { button: ActionButton ->
+      button.action.templateText == LearnBundle.message("toolwindow.stripe.Learn")
+    }
+    if (learnStripeButton != null) {
+      val options = LearningUiHighlightingManager.HighlightingOptions(
+        highlightBorder = true,
+        highlightInside = false,
+        usePulsation = false,
+        isRoundedCorners = true,
+        thickness = 2,
+      )
+      LearningUiHighlightingManager.highlightComponent(learnStripeButton, options)
+      Disposer.register(disposable) {
+        LearningUiHighlightingManager.clearHighlights()
+      }
+    }
 
     yield() // wait for the tool window to appear
 
