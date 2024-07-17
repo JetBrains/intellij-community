c274285caeea40e9b9d21feae6beae9ab6abf95d
chbndrhnns <chbndrhnns@users.noreply.github.com>
2024-07-17 15:38:34 +0000

    PY-66043: Resolve module references in PYTEST_PLUGINS
    close #2814
    
    
    Merge-request: IJ-MR-140174
    Merged-by: Egor Eliseev <Egor.Eliseev@jetbrains.com>
    
    GitOrigin-RevId: 4a7d46178ceedc283345980f118fbe607d84e685

---
+++ python/testData/testPytestFixtureResolving/testPytestPluginsFixtures/pytest_plugins_as_ref/fixtures/first.py
@@ -0,0 +1,7 @@
+#  Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+import pytest
+
+@pytest.fixture
+def first():
+    return 1
