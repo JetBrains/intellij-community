cfc79d0a668141d68a4ea60953f2601c849ef8e2
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2024-07-17 16:34:34 +0200

    [kotlin] enable KMP by default except for wasm which is explicitly disabled
    ^KTIJ-30150 fixed
    ^KTIJ-30155 fixed
    
    
    (cherry picked from commit 2c6a3def187f36929c3e275c6621b9923bbe36b3)
    
    IJ-MR-140280
    
    GitOrigin-RevId: d3417f5e7dcd897530af98bd311426c9004c395e

--- plugins/kotlin/base/test/test/org/jetbrains/kotlin/idea/test/KmpTestConfigurationUtil.kt
+++ plugins/kotlin/base/test/test/org/jetbrains/kotlin/idea/test/KmpTestConfigurationUtil.kt
@@ -10,9 +10,9 @@ import org.jetbrains.kotlin.idea.base.plugin.USE_K2_PLUGIN_PROPERTY_NAME
 import org.jetbrains.kotlin.idea.base.plugin.useK2Plugin
 
 @TestOnly
-fun <T> T.enableKmpSupport() where T : UsefulTestCase, T : ExpectedPluginModeProvider {
+fun <T> T.enableKmpWasmSupport() where T : UsefulTestCase, T : ExpectedPluginModeProvider {
     if (this.pluginMode == KotlinPluginMode.K2) {
         check(useK2Plugin == true) { "Expected '$USE_K2_PLUGIN_PROPERTY_NAME' to be set, please set up the plugin before enabling KMP" }
-        Registry.get("kotlin.k2.kmp.enabled").setValue(true, testRootDisposable)
+        Registry.get("kotlin.k2.kmp.wasm.enabled").setValue(true, testRootDisposable)
     }
 }
