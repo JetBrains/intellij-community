c931d935cd9d2f0bdb903c5ceab636cabe46329a
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2024-07-17 22:14:02 +0200

    Split kotlin gradle tests into a regular (fast) and integration
    GitOrigin-RevId: 78ada816769b280e24c984d7fae912ed09df2ca4

--- plugins/kotlin/gradle/gradle-java/tests.k2/test/org/jetbrains/kotlin/idea/k2/codeInsight/gradle/highlighting/AbstractK2GradleBuildFileHighlightingTest.kt
+++ plugins/kotlin/gradle/gradle-java/tests.k2/test/org/jetbrains/kotlin/idea/k2/codeInsight/gradle/highlighting/AbstractK2GradleBuildFileHighlightingTest.kt
@@ -1,6 +1,6 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
-package org.jetbrains.kotlin.idea.k2.highlighting
+package org.jetbrains.kotlin.idea.k2.codeInsight.gradle.highlighting
 
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.testFramework.common.runAll
