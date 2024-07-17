ce4308311d3363666e2584a5cef6589744345552
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2024-07-17 10:22:19 +0200

    [platform] IDEA-356265: Limit amount of generated dots in `ChannelingProgressIndicator`
    GitOrigin-RevId: 3f2f279d85a9e883f8f26ea3d8a8752d63b80112

--- platform/platform-impl/src/com/intellij/openapi/project/configuration/ChannelingProgressIndicator.kt
+++ platform/platform-impl/src/com/intellij/openapi/project/configuration/ChannelingProgressIndicator.kt
@@ -49,7 +49,7 @@ class ChannelingProgressIndicator(private val prefix: String) : ProgressIndicato
 
     val v = (100.0 * fraction).toInt()
     val total = 18
-    val completed = (total * fraction).toInt().coerceAtLeast(0)
+    val completed = (total * fraction).toInt().coerceIn(0, total)
     val d = ".".repeat(completed).padEnd(total, ' ')
     val verboseReport = verboseText.take(100).padEnd(105) + "$d $v%"
     return verboseReport
