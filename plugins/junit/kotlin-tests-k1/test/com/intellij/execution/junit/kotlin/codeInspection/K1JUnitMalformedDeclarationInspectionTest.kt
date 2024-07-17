8134aff6eab4064fda1bad11b72c652c1f9ef1bf
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2024-07-17 13:06:34 +0200

    [junit] Run Kotlin JUnit tests on both K1 and K2 frontend
    #IDEA-354809 Fixed
    
    GitOrigin-RevId: 762f49182607ab12adee58053a76606f8a923cfa

---
+++ plugins/junit/kotlin-tests-k1/test/com/intellij/execution/junit/kotlin/codeInspection/K1JUnitMalformedDeclarationInspectionTest.kt
@@ -0,0 +1,12 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.execution.junit.kotlin.codeInspection
+
+import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginMode
+
+class K1JUnitMalformedDeclarationInspectionTestV57 : KotlinJUnitMalformedDeclarationInspectionTestV57() {
+  override val pluginMode: KotlinPluginMode = KotlinPluginMode.K1
+}
+
+class K1JUnitMalformedDeclarationInspectionTestLatest : KotlinJUnitMalformedDeclarationInspectionTestLatest() {
+  override val pluginMode: KotlinPluginMode = KotlinPluginMode.K1
+}
---
+++ plugins/junit/kotlin-tests-shared/test/com/intellij/execution/junit/kotlin/codeInspection/KotlinJUnit3SuperTearDownInspectionTest.kt
@@ -0,0 +1,51 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.execution.junit.kotlin.codeInspection
+
+import com.intellij.junit.testFramework.JUnit3SuperTearDownInspectionTestBase
+import com.intellij.jvm.analysis.testFramework.JvmLanguage
+import org.jetbrains.kotlin.idea.test.ExpectedPluginModeProvider
+
+abstract class KotlinJUnit3SuperTearDownInspectionTest : JUnit3SuperTearDownInspectionTestBase(), ExpectedPluginModeProvider {
+  fun `test teardown in finally no highlighting`() {
+    myFixture.testHighlighting(
+      JvmLanguage.KOTLIN, """
+      class NoProblem : junit.framework.TestCase() {
+        override fun tearDown() {
+          super.tearDown();
+        }
+      }
+      class CalledInFinally : junit.framework.TestCase() {
+        override fun tearDown() {
+          try {
+            System.out.println()
+          } finally {
+            super.tearDown()
+          }
+        }
+      }
+      class SomeTest : junit.framework.TestCase() {
+        override fun setUp() {
+          try {
+            super.setUp()
+          }
+          catch (t: Throwable) {
+            super.tearDown()
+          }
+        }
+        fun test_something() { }
+      }
+    """.trimIndent())
+  }
+
+  fun `test teardown in finally highlighting`() {
+    myFixture.testHighlighting(
+      JvmLanguage.KOTLIN, """
+      class SuperTearDownInFinally : junit.framework.TestCase() {
+        override fun tearDown() {
+          super.<warning descr="'tearDown()' is not called from 'finally' block">tearDown</warning>()
+          System.out.println()
+        }
+      }      
+    """.trimIndent())
+  }
+}
