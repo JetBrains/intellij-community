30047943058283db93d49090f028dc631bbcd02b
Egor.Skrypnikov <egor.skrypnikov@jetbrains.com>
2024-07-17 19:39:58 +0200

    [Rider/IJ] (RIDER-114354) Make semantic file items show extended info in the SEUI gutter
    GitOrigin-RevId: a0751f28ecc186e3cf050fece8252956e1e19e9b

--- plugins/search-everywhere-ml/semantics/src/com/intellij/searchEverywhereMl/semantics/contributors/SemanticFileSearchEverywhereContributor.kt
+++ plugins/search-everywhere-ml/semantics/src/com/intellij/searchEverywhereMl/semantics/contributors/SemanticFileSearchEverywhereContributor.kt
@@ -2,19 +2,38 @@ package com.intellij.searchEverywhereMl.semantics.contributors
 
 import com.intellij.concurrency.SensitiveProgressWrapper
 import com.intellij.ide.actions.SearchEverywherePsiRenderer
-import com.intellij.ide.actions.searcheverywhere.FileSearchEverywhereContributor
-import com.intellij.ide.actions.searcheverywhere.FoundItemDescriptor
-import com.intellij.ide.actions.searcheverywhere.PossibleSlowContributor
-import com.intellij.ide.actions.searcheverywhere.PsiItemWithSimilarity
+import com.intellij.ide.actions.searcheverywhere.*
+import com.intellij.ide.actions.searcheverywhere.PSIPresentationBgRendererWrapper.PsiItemWithPresentation
+import com.intellij.ide.actions.searcheverywhere.footer.createPsiExtendedInfo
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
+import com.intellij.psi.PsiElement
 import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.ide.actions.searcheverywhere.SemanticSearchEverywhereContributor
 import com.intellij.searchEverywhereMl.semantics.providers.SemanticFilesProvider
 import com.intellij.util.Processor
 import org.jetbrains.annotations.ApiStatus
 import java.util.function.Consumer
+
+// equivalent to f1(inp) ?: ... ?: fn(inp)
+@ApiStatus.Internal
+public fun <T, U> makeFirstYieldingNotNullOrNull(vararg fs: (T) -> U?): (T) -> U? =
+  { inp -> fs.firstNotNullOfOrNull { f -> f(inp) } }
+
+@ApiStatus.Internal
+public fun tryPsiElementFromPossiblySematicEntry(entry: Any): PsiElement? =
+  when (entry) {
+    is PSIPresentationBgRendererWrapper.PsiItemWithPresentation -> entry.item
+    is PSIPresentationBgRendererWrapper.ItemWithPresentation<*> -> when (val presUnwprapped = entry.item) {
+      is PsiItemWithSimilarity<*> -> when (val semUnwrapped = presUnwprapped.value) {
+        is PsiItemWithPresentation -> semUnwrapped.item
+        is PsiElement -> semUnwrapped
+        else -> null
+      }
+      else -> null
+    }
+    else -> null
+  }
 
 /**
  * Contributor that adds semantic search functionality when searching for files in Search Everywhere.
@@ -26,6 +45,7 @@ import java.util.function.Consumer
 open class SemanticFileSearchEverywhereContributor(initEvent: AnActionEvent)
   : FileSearchEverywhereContributor(initEvent), SemanticSearchEverywhereContributor,
     SearchEverywhereConcurrentPsiElementsFetcher, PossibleSlowContributor {
+
   override val itemsProvider = SemanticFilesProvider(project)
 
   override var notifyCallback: Consumer<String>? = null
@@ -58,4 +78,8 @@ open class SemanticFileSearchEverywhereContributor(initEvent: AnActionEvent)
     itemsProvider.model = createModel(project)
     itemsProvider.searchScope = myScopeDescriptor.scope as GlobalSearchScope
   }
+
+  override fun createExtendedInfo(): ExtendedInfo? = createPsiExtendedInfo(
+    project = null, file = null, psiElement = ::tryPsiElementFromPossiblySematicEntry
+  )
 }
