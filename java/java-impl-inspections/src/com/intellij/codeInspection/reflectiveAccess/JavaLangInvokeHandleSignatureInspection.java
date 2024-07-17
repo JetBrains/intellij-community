b22bb694b41e16455f601bd32bbf8f3d1b335e05
SirYwell <hannesgreule@outlook.de>
2024-07-17 13:42:02 +0200

    [java-inspections] Remove redundant checks
    PR#2804
    Reviewed-by: Tagir Valeev <tagir.valeev@jetbrains.com>
    
    GitOrigin-RevId: 1cdce1a99e78603f8727f9a0a2cd10d8e51211f3

--- java/java-impl-inspections/src/com/intellij/codeInspection/reflectiveAccess/JavaLangInvokeHandleSignatureInspection.java
+++ java/java-impl-inspections/src/com/intellij/codeInspection/reflectiveAccess/JavaLangInvokeHandleSignatureInspection.java
@@ -26,7 +26,6 @@ import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.invoke.VarHandle;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -208,7 +207,6 @@ public final class JavaLangInvokeHandleSignatureInspection extends AbstractBaseJ
                                   @NotNull PsiReferenceExpression factoryMethodExpression,
                                   @NotNull ProblemsHolder holder) {
     if (!ownerClass.isExact()) return;
-    if (!isStaticExpected && isSignaturePolymorphic(ownerClass, methodName)) return;
     final PsiMethod[] methods = ownerClass.getPsiClass().findMethodsByName(methodName, true);
     if (methods.length == 0) {
       holder.registerProblem(methodNameExpression, JavaErrorBundle.message("cannot.resolve.method", methodName));
@@ -260,23 +258,6 @@ public final class JavaLangInvokeHandleSignatureInspection extends AbstractBaseJ
       }
     }
   }
-
-  private static boolean isSignaturePolymorphic(@NotNull ReflectiveClass ownerClass,
-                                                @NotNull String methodName) {
-    if ("java.lang.invoke.MethodHandle".equals(ownerClass.getPsiClass().getQualifiedName())) {
-      return methodName.equals("invoke") || methodName.equals("invokeExact");
-    }
-    if ("java.lang.invoke.VarHandle".equals(ownerClass.getPsiClass().getQualifiedName())) {
-      try {
-        //noinspection ResultOfMethodCallIgnored
-        VarHandle.AccessMode.valueFromMethodName(methodName);
-        return true;
-      } catch (IllegalArgumentException ignored) {
-      }
-    }
-    return false;
-  }
-
 
   private static void checkSpecial(@NotNull ReflectiveClass ownerClass,
                                    @NotNull PsiExpression callerClassExpression,
