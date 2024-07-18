22f1c82dccc11225b0c2a7eabd7dffb531522b52
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-07-18 15:34:45 +0200

    [platform] making `LocalizationStateService` work when the app is not ready (IJPL-156755)
    GitOrigin-RevId: 5d2520b503f7977ec5f67683800fe8bc719aa944

--- platform/core-api/src/com/intellij/l10n/LocalizationStateService.java
+++ platform/core-api/src/com/intellij/l10n/LocalizationStateService.java
@@ -1,22 +1,26 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.l10n
+package com.intellij.l10n;
 
-import com.intellij.diagnostic.LoadingState
-import com.intellij.openapi.application.ApplicationManager
-import org.jetbrains.annotations.ApiStatus.Internal
+import com.intellij.diagnostic.LoadingState;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
+import org.jetbrains.annotations.ApiStatus.Internal;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 @Internal
-interface LocalizationStateService {
-  companion object {
-    fun getInstance(): LocalizationStateService? {
-      if (!LoadingState.CONFIGURATION_STORE_INITIALIZED.isOccurred) {
-        return null
+public interface LocalizationStateService {
+  static @Nullable LocalizationStateService getInstance() {
+    if (LoadingState.CONFIGURATION_STORE_INITIALIZED.isOccurred()) {
+      Application app = ApplicationManager.getApplication();
+      if (app != null && !app.isDisposed()) {
+        return app.getService(LocalizationStateService.class);
       }
-      return ApplicationManager.getApplication().getService(LocalizationStateService::class.java)
     }
+    return null;
   }
 
-  fun getSelectedLocale(): String
-  fun setSelectedLocale(locale: String)
-  fun getLastSelectedLocale(): String
+  @NotNull String getSelectedLocale();
+  void setSelectedLocale(@NotNull String locale);
+  @NotNull String getLastSelectedLocale();
 }
