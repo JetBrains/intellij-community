d3d04fea2ac44d735ce82761f268e084ec3179b7
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2024-07-18 12:49:07 +0200

    [inline-completion] IJPL-158631: support effect types when rendering inline completion
    GitOrigin-RevId: 8e7c7ad7b0be19810bedea817a92adfcad345163

--- platform/platform-impl/src/com/intellij/codeInsight/inline/completion/render/InlineCompletionLineRenderer.kt
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/render/InlineCompletionLineRenderer.kt
@@ -7,13 +7,12 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorCustomElementRenderer
 import com.intellij.openapi.editor.Inlay
 import com.intellij.openapi.editor.colors.EditorFontType
+import com.intellij.openapi.editor.markup.EffectType
 import com.intellij.openapi.editor.markup.TextAttributes
+import com.intellij.ui.paint.EffectPainter2D
 import com.intellij.ui.paint.RectanglePainter2D
 import org.jetbrains.annotations.ApiStatus
-import java.awt.Graphics
-import java.awt.Graphics2D
-import java.awt.Rectangle
-import java.awt.RenderingHints
+import java.awt.*
 import java.awt.font.TextLayout
 
 /**
@@ -72,15 +71,55 @@ class InlineCompletionLineRenderer(
       g.font = editor.colorsScheme.getFont(EditorFontType.forJavaStyle(block.attributes.fontType))
       val textLayout = TextLayout(block.text, g.font, g.fontRenderContext)
       val textWidth = textLayout.advance.toDouble()
-      block.attributes.backgroundColor?.let {
-        g.color = it
-        RectanglePainter2D.FILL.paint(g, x, targetRegion.y.toDouble(), textWidth, targetRegion.height.toDouble())
-      }
+      val textHeight = targetRegion.height.toDouble()
+      val topY = targetRegion.y
+      val bottomY = topY + editor.ascent
+
+      paintBackground(g, block.attributes.backgroundColor, x, topY.toDouble(), textWidth, textHeight)
+      paintEffect(g, block.attributes.effectColor, block.attributes.effectType, x, bottomY.toDouble(), textWidth)
+
       g.color = block.attributes.foregroundColor
-      textLayout.draw(g, x.toFloat(), (targetRegion.y + editor.ascent).toFloat())
+      textLayout.draw(g, x.toFloat(), bottomY.toFloat())
       x += textWidth
     }
 
     g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, previousRenderingHint)
+  }
+
+  private fun paintBackground(
+    g: Graphics2D,
+    color: Color?,
+    x: Double,
+    y: Double,
+    width: Double,
+    height: Double
+  ) {
+    if (color == null) return
+    g.color = color
+    RectanglePainter2D.FILL.paint(g, x, y, width, height)
+  }
+
+  private fun paintEffect(
+    g: Graphics2D,
+    color: Color?,
+    effectType: EffectType?,
+    x: Double,
+    y: Double,
+    width: Double
+  ) {
+    if (color == null || effectType == null) return
+    val painter = when (effectType) {
+      EffectType.LINE_UNDERSCORE -> EffectPainter2D.LINE_UNDERSCORE
+      EffectType.WAVE_UNDERSCORE -> EffectPainter2D.WAVE_UNDERSCORE
+      EffectType.STRIKEOUT -> EffectPainter2D.STRIKE_THROUGH
+      EffectType.BOLD_LINE_UNDERSCORE -> EffectPainter2D.BOLD_LINE_UNDERSCORE
+      EffectType.BOLD_DOTTED_LINE -> EffectPainter2D.BOLD_DOTTED_UNDERSCORE
+      else -> null
+    }
+
+    if (painter != null) {
+      g.color = color
+      painter.paint(g, x, y, width, g.fontMetrics.descent.toDouble(), g.font)
+    }
   }
 }
