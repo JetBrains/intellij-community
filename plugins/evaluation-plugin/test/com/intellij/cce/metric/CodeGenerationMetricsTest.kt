18cc7383376c48aec7df1e2a017b75276b7c69fc
Alexey Kalina <alexey.kalina@jetbrains.com>
2024-07-18 11:21:40 +0200

    [evaluation-plugin] take into account empty lookups in NotEmptyResultsRatio metric (LME-10)
    GitOrigin-RevId: 839f9efad7adc00ea9c0990a42d9535ddbefdaa2

--- plugins/evaluation-plugin/test/com/intellij/cce/metric/CodeGenerationMetricsTest.kt
+++ plugins/evaluation-plugin/test/com/intellij/cce/metric/CodeGenerationMetricsTest.kt
@@ -9,9 +9,9 @@ class CodeGenerationMetricsTest {
   @Test
   fun `test correct results`() {
     doTest(sessions = listOf(
-      session(result = "not-empty", withSyntaxErrors = false),
-      session(result = "not-empty-2", withSyntaxErrors = false),
-      session(result = "not-empty-3", withSyntaxErrors = false),
+      session(singleSuggestion(result = "not-empty"), withSyntaxErrors = false),
+      session(singleSuggestion(result = "not-empty-2"), withSyntaxErrors = false),
+      session(singleSuggestion(result = "not-empty-3"), withSyntaxErrors = false),
     ), listOf(
       WithoutSyntaxErrorsSessionRatio() to 1.0,
       NotEmptyResultsRatio() to 1.0
@@ -21,10 +21,10 @@ class CodeGenerationMetricsTest {
   @Test
   fun `test incorrect results`() {
     doTest(sessions = listOf(
-      session(result = "", withSyntaxErrors = true),
-      session(result = "", withSyntaxErrors = true),
-      session(result = "with-errors", withSyntaxErrors = true),
-      session(result = "with-errors-2", withSyntaxErrors = true),
+      session(singleSuggestion(result = ""), withSyntaxErrors = true),
+      session(singleSuggestion(result = ""), withSyntaxErrors = true),
+      session(singleSuggestion(result = "with-errors"), withSyntaxErrors = true),
+      session(singleSuggestion(result = "with-errors-2"), withSyntaxErrors = true),
     ), listOf(
       WithoutSyntaxErrorsSessionRatio() to 0.0,
       NotEmptyResultsRatio() to 0.5
@@ -34,13 +34,23 @@ class CodeGenerationMetricsTest {
   @Test
   fun `test mixed results`() {
     doTest(sessions = listOf(
-      session(result = "", withSyntaxErrors = true),
-      session(result = "not-empty", withSyntaxErrors = false),
-      session(result = "not-empty-2", withSyntaxErrors = false),
-      session(result = "with errors", withSyntaxErrors = true),
+      session(singleSuggestion(result = ""), withSyntaxErrors = true),
+      session(singleSuggestion(result = "not-empty"), withSyntaxErrors = false),
+      session(singleSuggestion(result = "not-empty-2"), withSyntaxErrors = false),
+      session(singleSuggestion(result = "with errors"), withSyntaxErrors = true),
     ), listOf(
       WithoutSyntaxErrorsSessionRatio() to 0.5,
       NotEmptyResultsRatio() to 0.75
+    ))
+  }
+
+  @Test
+  fun `test session without suggestions`() {
+    doTest(sessions = listOf(
+      session(emptyList(), withSyntaxErrors = true),
+    ), listOf(
+      WithoutSyntaxErrorsSessionRatio() to 0.0,
+      NotEmptyResultsRatio() to 0.0
     ))
   }
 
@@ -50,12 +60,14 @@ class CodeGenerationMetricsTest {
     }
   }
 
-  private fun session(result: String, withSyntaxErrors: Boolean): Session {
+  private fun singleSuggestion(result: String): List<Suggestion> = listOf(Suggestion(result, result, SuggestionSource.INTELLIJ))
+
+  private fun session(suggestions: List<Suggestion>, withSyntaxErrors: Boolean): Session {
     val session = Session(0, "", 0, TokenProperties.UNKNOWN)
     session.addLookup(Lookup(
       prefix = "",
       offset = 0,
-      suggestions = listOf(Suggestion(result, result, SuggestionSource.INTELLIJ)),
+      suggestions = suggestions,
       latency = 0,
       features = null,
       selectedPosition = 0,
