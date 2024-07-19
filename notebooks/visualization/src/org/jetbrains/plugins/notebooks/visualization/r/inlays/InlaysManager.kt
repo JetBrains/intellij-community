8bab7cfae8552bb02692fb0b807f6cbc8be38075
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2024-07-19 13:49:53 +0200

    [R-1562] remove InlayDescriptorProvider because it has no implementations
    GitOrigin-RevId: 6785aef63b6452bc33de1a0249ed74e00cbcee12

--- notebooks/visualization/src/org/jetbrains/plugins/notebooks/visualization/r/inlays/InlaysManager.kt
+++ notebooks/visualization/src/org/jetbrains/plugins/notebooks/visualization/r/inlays/InlaysManager.kt
@@ -4,54 +4,23 @@
 
 package org.jetbrains.plugins.notebooks.visualization.r.inlays
 
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.event.EditorFactoryEvent
 import com.intellij.openapi.editor.event.EditorFactoryListener
 import com.intellij.openapi.editor.impl.EditorImpl
-import com.intellij.openapi.util.Key
 
 /**
- * Manages inlays.
- *
- * On project load subscribes
- *    on editor opening/closing.
- *    on adding/removing notebook cells
- *    on any document changes
- *    on folding actions
- *
- * On editor open checks the PSI structure and restores saved inlays.
- *
- * ToDo should be split into InlaysManager with all basics and NotebookInlaysManager with all specific.
+ * Obsolete code, currently EditorInlaysManager is not created
  */
-
-private val logger = Logger.getInstance(InlaysManager::class.java)
-
 class InlaysManager : EditorFactoryListener {
-
   override fun editorCreated(event: EditorFactoryEvent) {
     val editor = event.editor
-    val project = editor.project ?: return
-    val descriptor = getDescriptor(editor) ?: return
+    editor.project ?: return
+    /**
+     * may be [InlayDimensions.init] is not needed, this call was missed for a long time
+     */
     InlayDimensions.init(editor as EditorImpl)
-    editor.putUserData(KEY, EditorInlaysManager(project, editor, descriptor))
-  }
-
-  override fun editorReleased(event: EditorFactoryEvent) {
-    event.editor.getUserData(KEY)?.let { manager ->
-      manager.dispose()
-      event.editor.putUserData(KEY, null)
-    }
   }
 }
 
-private val KEY = Key.create<EditorInlaysManager>("org.jetbrains.plugins.notebooks.visualization.r.inlays.editorInlaysManager")
-
-fun getEditorManager(editor: Editor): EditorInlaysManager? = editor.getUserData(KEY)
-
-private fun getDescriptor(editor: Editor): InlayElementDescriptor? {
-  return InlayDescriptorProvider.EP.extensionList
-    .asSequence()
-    .mapNotNull { it.getInlayDescriptor(editor) }
-    .firstOrNull()
-}
+fun getEditorManager(editor: Editor): EditorInlaysManager? = null
