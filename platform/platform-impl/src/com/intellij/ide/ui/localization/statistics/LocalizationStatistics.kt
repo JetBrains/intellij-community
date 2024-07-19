5d6c2eb5af361403ed0ae8d990d62149b7d189ad
Vera Petrenkova <Vera.Petrenkova@jetbrains.com>
2024-07-19 14:24:00 +0200

    JPL-157259 Analytics (FUS) for Region and Language-specific settings
    GitOrigin-RevId: 59b6b0122a1e5a6fd82544291cbe854b36c3de5f

--- platform/platform-impl/src/com/intellij/ide/ui/localization/statistics/LocalizationStatistics.kt
+++ platform/platform-impl/src/com/intellij/ide/ui/localization/statistics/LocalizationStatistics.kt
@@ -5,48 +5,49 @@ import com.intellij.ide.Region
 import com.intellij.internal.statistic.eventLog.EventLogGroup
 import com.intellij.internal.statistic.eventLog.events.*
 import com.intellij.internal.statistic.eventLog.events.EventFields.Long
+import org.jetbrains.annotations.ApiStatus.Internal
 import java.util.*
 
 internal abstract class LocalizationStatistics {
+  protected val localizationActionsGroup = EventLogGroup("localization.actions.info", 1)
+  protected val eventSource: EnumEventField<EventSource> = EventFields.Enum("event.source", EventSource::class.java)
   protected val selectedLang: StringEventField = EventFields.String("selected.language", Region.entries.map { it.externalName() })
   protected val selectedLangPrev: StringEventField = EventFields.String("selected.language.prev", Region.entries.map { it.externalName() })
   protected val selectedRegion: StringEventField = EventFields.String("selected.region", Region.entries.map { it.externalName() })
   protected val selectedRegionPrev: StringEventField = EventFields.String("selected.region.prev", Region.entries.map { it.externalName() })
   protected val eventTimestamp: EventField<Long> = Long("timestamp")
-  private fun getHyperLinkActivatedEvent(): EventId = getGroup().registerEvent("documentation.link.activated")
-  private fun getLanguageExpandEvent(): EventId = getGroup().registerEvent("language.expanded")
-  private fun getLanguageSelectedEvent(): VarargEventId = getGroup().registerVarargEvent("language.selected", selectedLang, selectedLangPrev)
-  private fun getRegionExpandEvent(): EventId = getGroup().registerEvent("region.expanded")
-  private fun getRegionSelectedEvent(): VarargEventId = getGroup().registerVarargEvent("region.selected", selectedRegion, selectedRegionPrev)
-
+  private fun getHyperLinkActivatedEvent(): EventId1<EventSource> = localizationActionsGroup.registerEvent("documentation.link.activated", eventSource)
+  private fun getLanguageExpandEvent(): EventId1<EventSource> = localizationActionsGroup.registerEvent("language.expanded", eventSource)
+  private fun getLanguageSelectedEvent(): VarargEventId = localizationActionsGroup.registerVarargEvent("language.selected", selectedLang, selectedLangPrev, eventSource)
+  private fun getRegionExpandEvent(): EventId1<EventSource> = localizationActionsGroup.registerEvent("region.expanded", eventSource)
+  private fun getRegionSelectedEvent(): VarargEventId = localizationActionsGroup.registerVarargEvent("region.selected", selectedRegion, selectedRegionPrev, eventSource)
+  
   fun languageExpanded() {
-    logEvent(getLanguageExpandEvent())
+    logEvent(getLanguageExpandEvent(), getSource())
   }
 
   fun languageSelected(selected: Locale, prevSelected: Locale) {
     logEvent(getLanguageSelectedEvent(), FieldsListBuilder()
       .with(selectedLang, selected.toLanguageTag())
       .with(selectedLangPrev, prevSelected.toLanguageTag())
+      .with(eventSource, getSource())
       .list())
   }
 
   fun regionExpanded() {
-    logEvent(getRegionExpandEvent())
+    logEvent(getRegionExpandEvent(), getSource())
   }
 
   fun regionSelected(selected: Region, prevSelected: Region) {
     logEvent(getRegionSelectedEvent(), FieldsListBuilder()
       .with(selectedRegion, selected.externalName())
       .with(selectedRegionPrev, prevSelected.externalName())
+      .with(eventSource, getSource())
       .list())
   }
 
   fun hyperLinkActivated() {
-    logEvent(getHyperLinkActivatedEvent())
-  }
-
-  fun registerEvent(group: EventLogGroup, eventId: String, vararg eventFields: EventField<*>): VarargEventId {
-    return group.registerVarargEvent(eventId, *eventFields)
+    logEvent(getHyperLinkActivatedEvent(), getSource())
   }
 
   protected class FieldsListBuilder {
@@ -67,7 +68,14 @@ internal abstract class LocalizationStatistics {
 
   abstract fun logEvent(event: VarargEventId, params: List<EventPair<*>>)
 
-  abstract fun logEvent(event: EventId)
+  abstract fun logEvent(event: EventId1<EventSource>, eventSource: EventSource)
+  
+  abstract fun getSource(): EventSource
+}
 
-  abstract fun getGroup(): EventLogGroup
+@Internal
+enum class EventSource {
+  SETTINGS,
+  WELCOME_SCREEN,
+  PRE_EUA_DIALOG
 }
