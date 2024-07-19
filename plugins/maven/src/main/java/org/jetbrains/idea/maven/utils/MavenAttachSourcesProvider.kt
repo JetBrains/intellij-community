a4b42aa71fa9c12873bf985266675610edeb4ed0
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2024-07-19 12:18:13 +0200

    [maven] IDEA-356571 ReadAction required error when downloading Maven sources
    GitOrigin-RevId: c14bc578b79c8e87193320ff864dbfc26ddc8e32

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/utils/MavenAttachSourcesProvider.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/utils/MavenAttachSourcesProvider.kt
@@ -7,6 +7,7 @@ import com.intellij.notification.Notification
 import com.intellij.notification.NotificationType
 import com.intellij.notification.Notifications
 import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.LibraryOrderEntry
 import com.intellij.openapi.roots.ProjectRootManager
@@ -14,6 +15,7 @@ import com.intellij.openapi.util.ActionCallback
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.text.HtmlBuilder
 import com.intellij.openapi.util.text.HtmlChunk
+import com.intellij.platform.ide.progress.withBackgroundProgress
 import com.intellij.psi.PsiFile
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -44,7 +46,7 @@ internal class MavenAttachSourcesProvider : AttachSourcesProvider {
         val resultWrapper = ActionCallback()
         cs.launch {
           // may have been changed by this time...
-          val mavenProjects = getMavenProjects(psiFile)
+          val mavenProjects = readAction { getMavenProjects(psiFile) }
           if (mavenProjects.isEmpty()) {
             resultWrapper.setRejected()
             return@launch
