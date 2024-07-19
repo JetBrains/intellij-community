43ce40e4c211decc14ba8fb45537ede772a785ea
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2024-07-19 12:50:53 +0200

    [maven] [IDEA-356392] run tests  in smart mode
    GitOrigin-RevId: 49d67ff446a1710caae3924d6524b583e7c46ca7

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/intentions/MavenAddDependencyIntentionTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/intentions/MavenAddDependencyIntentionTest.kt
@@ -2,19 +2,21 @@ package org.jetbrains.idea.maven.intentions
 
 import com.intellij.maven.testFramework.MavenDomTestCase
 import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.ReadAction
 import com.intellij.psi.PsiJavaCodeReferenceElement
 import com.intellij.psi.util.PsiTreeUtil
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.idea.maven.dom.intentions.AddMavenDependencyQuickFix
 import org.junit.Test
+import java.util.concurrent.Callable
 
 class MavenAddDependencyIntentionTest : MavenDomTestCase() {
   override fun setUp() = runBlocking {
     super.setUp()
     importProjectAsync("<groupId>test</groupId>" +
-                  "<artifactId>project</artifactId>" +
-                  "<version>1</version>")
+                       "<artifactId>project</artifactId>" +
+                       "<version>1</version>")
   }
 
   @Test
@@ -85,18 +87,21 @@ class MavenAddDependencyIntentionTest : MavenDomTestCase() {
 
   private fun doTest(classText: String, referenceText: String?) = runBlocking(Dispatchers.EDT) {
     fixture.configureByText("A.java", classText)
-    val element = PsiTreeUtil.getParentOfType(fixture.getFile().findElementAt(fixture.getCaretOffset()),
-                                              PsiJavaCodeReferenceElement::class.java)
+    ReadAction.nonBlocking(Callable {
+      val element = PsiTreeUtil.getParentOfType(fixture.getFile().findElementAt(fixture.getCaretOffset()),
+                                                PsiJavaCodeReferenceElement::class.java)
 
-    assertNull(element!!.resolve())
+      assertNull(element!!.resolve())
 
-    val fix = AddMavenDependencyQuickFix(element)
+      val fix = AddMavenDependencyQuickFix(element)
 
-    if (referenceText == null) {
-      assertFalse(fix.isAvailable(project, fixture.getEditor(), fixture.getFile()))
-    }
-    else {
-      assertEquals(fix.getReferenceText(), referenceText)
-    }
+      if (referenceText == null) {
+        assertFalse(fix.isAvailable(project, fixture.getEditor(), fixture.getFile()))
+      }
+      else {
+        assertEquals(fix.getReferenceText(), referenceText)
+      }
+    }).inSmartMode(project).executeSynchronously()
+
   }
 }
