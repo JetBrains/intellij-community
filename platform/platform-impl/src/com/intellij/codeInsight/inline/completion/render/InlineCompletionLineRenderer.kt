d17d2097d4fd765b1a167c6e5595a605dfbd54f5
Andrey.Matveev <andrey.matveev@jetbrains.com>
2024-07-22 16:26:43 +0300

    PY-64470 Rename paintBackground to beforePaint
    GitOrigin-RevId: 5033898e101f8b2e95721a24903f9cfdca350dba

--- platform/platform-impl/src/com/intellij/codeInsight/inline/completion/render/InlineCompletionLineRenderer.kt
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/render/InlineCompletionLineRenderer.kt
@@ -52,14 +52,14 @@ open class InlineCompletionLineRenderer(
     return maxOf(1, result)
   }
 
-  protected open fun paintBackground(inlay: Inlay<*>, g: Graphics, targetRegion: Rectangle) {}
+  protected open fun beforePaint(inlay: Inlay<*>, g: Graphics, targetRegion: Rectangle) {}
 
   override fun paint(inlay: Inlay<*>, g: Graphics, targetRegion: Rectangle, textAttributes: TextAttributes) {
     if (blocks.isEmpty()) {
       return
     }
 
-    paintBackground(inlay, g, targetRegion)
+    beforePaint(inlay, g, targetRegion)
 
     val previousRenderingHint = (g as Graphics2D).getRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING)
     g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, AntialiasingType.getKeyForCurrentScope(false))
@@ -79,7 +79,7 @@ open class InlineCompletionLineRenderer(
       val topY = targetRegion.y
       val bottomY = topY + editor.ascent
 
-      paintBackground(g, block.attributes.backgroundColor, x, topY.toDouble(), textWidth, textHeight)
+      beforePaint(g, block.attributes.backgroundColor, x, topY.toDouble(), textWidth, textHeight)
       paintEffect(g, block.attributes.effectColor, block.attributes.effectType, x, bottomY.toDouble(), textWidth)
 
       g.color = block.attributes.foregroundColor
@@ -90,7 +90,7 @@ open class InlineCompletionLineRenderer(
     g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, previousRenderingHint)
   }
 
-  private fun paintBackground(
+  private fun beforePaint(
     g: Graphics2D,
     color: Color?,
     x: Double,
