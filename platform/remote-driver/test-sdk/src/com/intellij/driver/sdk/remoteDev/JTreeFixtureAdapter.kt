bff193de41cd05e668a32a515bcc9e2a0c88f549
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2024-07-22 16:15:41 +0200

    [driver remdev] use QueryBuilder
    GitOrigin-RevId: 4bedc42f63bb6f9721e2af212f7b5a3447bd90c8

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/remoteDev/JTreeFixtureAdapter.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/remoteDev/JTreeFixtureAdapter.kt
@@ -5,13 +5,12 @@ import com.intellij.driver.model.TreePathToRowList
 import com.intellij.driver.sdk.ui.components.JTreeFixtureRef
 import com.intellij.driver.sdk.ui.components.JTreeUiComponent
 import com.intellij.driver.sdk.ui.remote.Robot
-import com.intellij.driver.sdk.ui.xQuery
 import javax.swing.JTree
 
 class JTreeFixtureAdapter(robot: Robot, component: BeControlComponentBase) :
   BeControlComponentBase(component.driver, component.frontendComponent, component.backendComponent),
   JTreeFixtureRef {
-  val fixture = onFrontend(xQuery { byType(JTree::class.java.name) }, JTreeUiComponent::class).fixture
+  val fixture = onFrontend(JTreeUiComponent::class) { byType(JTree::class.java.name) }.fixture
 
   override fun clickRow(row: Int): JTreeFixtureRef {
     fixture.clickRow(row)
