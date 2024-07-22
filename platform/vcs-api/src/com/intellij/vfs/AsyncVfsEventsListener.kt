8acb6c27e760a1eb7cba099d5cb191123240c096
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-07-22 18:59:25 +0200

    IJPL-158881 refactor AsyncVfsEventsListener
    GitOrigin-RevId: 5ce42f5790b85c556d1ade39080568fc03ecbd89

--- platform/vcs-api/src/com/intellij/vfs/AsyncVfsEventsListener.kt
+++ platform/vcs-api/src/com/intellij/vfs/AsyncVfsEventsListener.kt
@@ -1,31 +1,27 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.vfs;
+package com.intellij.vfs
 
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.vfs.VirtualFileListener;
-import com.intellij.openapi.vfs.newvfs.BulkFileListener;
-import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent
+import org.jetbrains.annotations.ApiStatus
 
 /**
- * <p>Receives notifications about changes in the virtual file system, just as {@link BulkFileListener} and {@link VirtualFileListener},
- * but on a pooled thread, which allows to off-load the EDT, but requires more care in the listener code due to asynchrony and
- * the absence of read action. For a safer alternative, consider {@link com.intellij.openapi.vfs.AsyncFileListener}.</p>
  *
- * <p>Use the {@link AsyncVfsEventsPostProcessor#addListener(AsyncVfsEventsListener, kotlinx.coroutines.CoroutineScope)} to subscribe.</p>
+ * Receives notifications about changes in the virtual file system, just as [BulkFileListener] and [VirtualFileListener],
+ * but on a pooled thread, which allows to off-load the EDT, but requires more care in the listener code due to asynchrony and
+ * the absence of read action. For a safer alternative, consider [com.intellij.openapi.vfs.AsyncFileListener].
+ *
+ *
+ * Use the [AsyncVfsEventsPostProcessor.addListener] to subscribe.
  *
  * @see AsyncVfsEventsPostProcessor
  */
 @ApiStatus.Experimental
-public interface AsyncVfsEventsListener {
+interface AsyncVfsEventsListener {
   /**
-   * Invoked after the given events were applied to the VFS. <br/><br/>
+   * Invoked after the given events were applied to the VFS. <br></br><br></br>
    *
-   * The call happens on a pooled thread, under a special {@link ProgressIndicator} which is canceled on project disposal,
-   * thus one can call {@code ProgressManager.checkCancelled()} to cancel the background task when the project is disposed.
+   * The call happens on a pooled thread, under a special [ProgressIndicator] which is canceled on project disposal,
+   * thus one can call `ProgressManager.checkCancelled()` to cancel the background task when the project is disposed.
    */
-  void filesChanged(@NotNull List<? extends @NotNull VFileEvent> events);
+  fun filesChanged(events: List<VFileEvent>)
 }
