25852c1a6ccc44b2525eee1580c71363f359bf2a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-07-22 19:29:36 +0200

    IJPL-158881 refactor GitRepositoryUpdater
    GitOrigin-RevId: e2621b4f69f236e7c9721652695c06372e6677ec

--- plugins/git4idea/src/git4idea/repo/GitRepositoryUpdater.kt
+++ plugins/git4idea/src/git4idea/repo/GitRepositoryUpdater.kt
@@ -1,170 +1,165 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package git4idea.repo;
+package git4idea.repo
 
-import com.intellij.dvcs.DvcsUtil;
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.progress.util.BackgroundTaskUtil;
-import com.intellij.openapi.vcs.changes.VcsDirtyScopeManager;
-import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VfsUtilCore;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
-import com.intellij.util.CommonProcessors;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.vcsUtil.VcsUtil;
-import com.intellij.vfs.AsyncVfsEventsListener;
-import com.intellij.vfs.AsyncVfsEventsPostProcessor;
-import git4idea.GitLocalBranch;
-import git4idea.index.vfs.GitIndexFileSystemRefresher;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.*;
+import com.intellij.dvcs.DvcsUtil
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.progress.util.BackgroundTaskUtil
+import com.intellij.openapi.vcs.changes.VcsDirtyScopeManager
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.LocalFileSystem.WatchRequest
+import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent
+import com.intellij.util.CommonProcessors
+import com.intellij.util.containers.ContainerUtil
+import com.intellij.vcsUtil.VcsUtil
+import com.intellij.vfs.AsyncVfsEventsListener
+import com.intellij.vfs.AsyncVfsEventsPostProcessor
+import git4idea.index.vfs.GitIndexFileSystemRefresher.Companion.refreshRoots
+import kotlinx.coroutines.ensureActive
+import kotlin.coroutines.coroutineContext
 
 /**
- * Listens to .git service files changes and updates {@link GitRepository} when needed.
+ * Listens to .git service files changes and updates [GitRepository] when needed.
  */
-final class GitRepositoryUpdater implements Disposable, AsyncVfsEventsListener {
-  private final @NotNull GitRepository myRepository;
-  private final @NotNull Collection<VirtualFile> myRootDirs;
-  private final @NotNull GitRepositoryFiles myRepositoryFiles;
-  private final @Nullable VirtualFile myRemotesDir;
-  private final @Nullable VirtualFile myHeadsDir;
-  private final @Nullable VirtualFile myTagsDir;
-  private final @NotNull Set<LocalFileSystem.WatchRequest> myWatchRequests;
+internal class GitRepositoryUpdater(
+  private val repository: GitRepository,
+  private val repositoryFiles: GitRepositoryFiles
+) : Disposable, AsyncVfsEventsListener {
+  private val rootDirs: Collection<VirtualFile> = repositoryFiles.rootDirs
+  private val remotesDir: VirtualFile?
+  private val headsDir: VirtualFile?
+  private val tagsDir: VirtualFile?
+  private val watchRequests: Set<WatchRequest> = LocalFileSystem.getInstance().addRootsToWatch(rootDirs.map { it.path }, true)
 
-  GitRepositoryUpdater(@NotNull GitRepository repository, @NotNull GitRepositoryFiles gitFiles) {
-    myRepository = repository;
-    myRootDirs = gitFiles.getRootDirs();
-    myWatchRequests = LocalFileSystem.getInstance().addRootsToWatch(ContainerUtil.map(myRootDirs, VirtualFile::getPath), true);
+  init {
+    visitSubDirsInVfs()
+    headsDir = VcsUtil.getVirtualFile(repositoryFiles.refsHeadsFile)
+    remotesDir = VcsUtil.getVirtualFile(repositoryFiles.refsRemotesFile)
+    tagsDir = VcsUtil.getVirtualFile(repositoryFiles.refsTagsFile)
 
-    myRepositoryFiles = gitFiles;
-    visitSubDirsInVfs();
-    myHeadsDir = VcsUtil.getVirtualFile(myRepositoryFiles.getRefsHeadsFile());
-    myRemotesDir = VcsUtil.getVirtualFile(myRepositoryFiles.getRefsRemotesFile());
-    myTagsDir = VcsUtil.getVirtualFile(myRepositoryFiles.getRefsTagsFile());
-
-    AsyncVfsEventsPostProcessor.getInstance().addListener(this, repository.getCoroutineScope());
+    AsyncVfsEventsPostProcessor.getInstance().addListener(this, repository.coroutineScope)
   }
 
-  @Override
-  public void dispose() {
-    LocalFileSystem.getInstance().removeWatchedRoots(myWatchRequests);
+  override fun dispose() {
+    LocalFileSystem.getInstance().removeWatchedRoots(watchRequests)
   }
 
-  @Override
-  public void filesChanged(@NotNull List<? extends @NotNull VFileEvent> events) {
-    GitLocalBranch currentBranch = myRepository.getCurrentBranch();
+  override suspend fun filesChanged(events: List<VFileEvent>) {
+    val currentBranch = repository.currentBranch
 
     // which files in .git were changed
-    boolean configChanged = false;
-    boolean indexChanged = false;
-    boolean headChanged = false;
-    boolean headMoved = false;
-    boolean branchFileChanged = false;
-    boolean currentBranchChanged = false;
-    boolean packedRefsChanged = false;
-    boolean rebaseFileChanged = false;
-    boolean mergeFileChanged = false;
-    boolean externallyCommitted = false;
-    boolean tagChanged = false;
-    boolean gitignoreChanged = false;
+    var configChanged = false
+    var indexChanged = false
+    var headChanged = false
+    var headMoved = false
+    var branchFileChanged = false
+    var currentBranchChanged = false
+    var packedRefsChanged = false
+    var rebaseFileChanged = false
+    var mergeFileChanged = false
+    var externallyCommitted = false
+    var tagChanged = false
+    var gitignoreChanged = false
 
-    Set<VirtualFile> toReloadVfs = new HashSet<>();
-    for (VFileEvent event : events) {
-      String filePath = event.getPath();
+    val toReloadVfs = HashSet<VirtualFile>()
+    for (event in events) {
+      coroutineContext.ensureActive()
+
+      val filePath = event.path
       if (isRootDirChange(event)) {
-        if (myRepositoryFiles.isConfigFile(filePath)) {
-          configChanged = true;
-        }
-        else if (myRepositoryFiles.isIndexFile(filePath)) {
-          indexChanged = true;
-        }
-        else if (myRepositoryFiles.isHeadFile(filePath)) {
-          headChanged = true;
-        }
-        else if (myRepositoryFiles.isOrigHeadFile(filePath)) {
-          headMoved = true;
-        }
-        else if (myRepositoryFiles.isBranchFile(filePath)) {
-          // it is also possible, that a local branch with complex name ("folder/branch") was created => the folder also to be watched.
-          branchFileChanged = true;
-          ContainerUtil.addIfNotNull(toReloadVfs, myHeadsDir);
+        when {
+          repositoryFiles.isConfigFile(filePath) -> {
+            configChanged = true
+          }
+          repositoryFiles.isIndexFile(filePath) -> {
+            indexChanged = true
+          }
+          repositoryFiles.isHeadFile(filePath) -> {
+            headChanged = true
+          }
+          repositoryFiles.isOrigHeadFile(filePath) -> {
+            headMoved = true
+          }
+          repositoryFiles.isBranchFile(filePath) -> {
+            // it is also possible, that a local branch with complex name ("folder/branch") was created => the folder also to be watched.
+            branchFileChanged = true
+            headsDir?.let { toReloadVfs.add(it) }
 
-          if (currentBranch != null && myRepositoryFiles.isBranchFile(filePath, currentBranch.getFullName())) {
-            currentBranchChanged = true;
+            if (currentBranch != null && repositoryFiles.isBranchFile(filePath, currentBranch.fullName)) {
+              currentBranchChanged = true
+            }
+          }
+          repositoryFiles.isRemoteBranchFile(filePath) -> {
+            // it is possible that a branch from a new remote was fetch => we need to add a new remote folder to the VFS
+            branchFileChanged = true
+            ContainerUtil.addIfNotNull(toReloadVfs, remotesDir)
+          }
+          repositoryFiles.isPackedRefs(filePath) -> {
+            packedRefsChanged = true
+          }
+          repositoryFiles.isRebaseFile(filePath) -> {
+            rebaseFileChanged = true
+          }
+          repositoryFiles.isMergeFile(filePath) -> {
+            mergeFileChanged = true
+          }
+          repositoryFiles.isCommitMessageFile(filePath) -> {
+            externallyCommitted = true
+          }
+          repositoryFiles.isTagFile(filePath) -> {
+            tagChanged = true
+            ContainerUtil.addIfNotNull(toReloadVfs, tagsDir)
+          }
+          repositoryFiles.isExclude(filePath) -> {
+            // TODO watch file stored in `core.excludesfile`
+            gitignoreChanged = true
           }
         }
-        else if (myRepositoryFiles.isRemoteBranchFile(filePath)) {
-          // it is possible, that a branch from a new remote was fetch => we need to add new remote folder to the VFS
-          branchFileChanged = true;
-          ContainerUtil.addIfNotNull(toReloadVfs, myRemotesDir);
-        }
-        else if (myRepositoryFiles.isPackedRefs(filePath)) {
-          packedRefsChanged = true;
-        }
-        else if (myRepositoryFiles.isRebaseFile(filePath)) {
-          rebaseFileChanged = true;
-        }
-        else if (myRepositoryFiles.isMergeFile(filePath)) {
-          mergeFileChanged = true;
-        }
-        else if (myRepositoryFiles.isCommitMessageFile(filePath)) {
-          externallyCommitted = true;
-        }
-        else if (myRepositoryFiles.isTagFile(filePath)) {
-          tagChanged = true;
-          ContainerUtil.addIfNotNull(toReloadVfs, myTagsDir);
-        }
-        else if (myRepositoryFiles.isExclude(filePath)) {
-          // TODO watch file stored in core.excludesfile
-          gitignoreChanged = true;
-        }
       }
-      else {
-        if (filePath.endsWith(GitRepositoryFiles.GITIGNORE)) {
-          gitignoreChanged = true;
-        }
+      else if (filePath.endsWith(GitRepositoryFiles.GITIGNORE)) {
+        gitignoreChanged = true
       }
     }
-    for (VirtualFile dir : toReloadVfs) {
-      VfsUtilCore.processFilesRecursively(dir, CommonProcessors.alwaysTrue());
+
+    coroutineContext.ensureActive()
+
+    for (dir in toReloadVfs) {
+      VfsUtilCore.processFilesRecursively(dir, CommonProcessors.alwaysTrue())
     }
 
     if (headChanged || configChanged || branchFileChanged || packedRefsChanged || rebaseFileChanged || mergeFileChanged) {
-      myRepository.update();
+      repository.update()
     }
     if (tagChanged || packedRefsChanged) {
-      myRepository.getTagHolder().reload();
-      BackgroundTaskUtil.syncPublisher(myRepository.getProject(), GitRepository.GIT_REPO_CHANGE).repositoryChanged(myRepository);
+      repository.tagHolder.reload()
+      BackgroundTaskUtil.syncPublisher(repository.project, GitRepository.GIT_REPO_CHANGE).repositoryChanged(repository)
     }
     if (configChanged) {
-      BackgroundTaskUtil.syncPublisher(myRepository.getProject(), GitConfigListener.TOPIC).notifyConfigChanged(myRepository);
+      BackgroundTaskUtil.syncPublisher(repository.project, GitConfigListener.TOPIC).notifyConfigChanged(repository)
     }
     if (indexChanged || externallyCommitted || headMoved || headChanged || currentBranchChanged || gitignoreChanged) {
-      VcsDirtyScopeManager.getInstance(myRepository.getProject()).dirDirtyRecursively(myRepository.getRoot());
-      myRepository.getUntrackedFilesHolder().invalidate();
+      VcsDirtyScopeManager.getInstance(repository.project).dirDirtyRecursively(repository.root)
+      repository.untrackedFilesHolder.invalidate()
     }
     if (indexChanged) {
-      GitIndexFileSystemRefresher.refreshRoots(myRepository.getProject(), Collections.singletonList(myRepository.getRoot()));
+      refreshRoots(repository.project, listOf(repository.root))
     }
   }
 
-  private boolean isRootDirChange(@NotNull VFileEvent event) {
-    VirtualFile file = event.getFile();
-    if (file == null) return true; // can't do fast check, fallback to paths matching
-    for (VirtualFile dir : myRootDirs) {
-      if (VfsUtilCore.isAncestor(dir, file, false)) return true;
-    }
-    return false;
+  private fun isRootDirChange(event: VFileEvent): Boolean {
+    // can't do fast check, fallback to paths matching
+    val file = event.file ?: return true
+    return rootDirs.any { VfsUtilCore.isAncestor(it, file, false) }
   }
 
-  private void visitSubDirsInVfs() {
-    for (VirtualFile rootDir : myRepositoryFiles.getRootDirs()) {
-      rootDir.getChildren();
+  private fun visitSubDirsInVfs() {
+    for (rootDir in repositoryFiles.rootDirs) {
+      rootDir.children
     }
-    for (String path : myRepositoryFiles.getPathsToWatch()) {
-      DvcsUtil.ensureAllChildrenInVfs(LocalFileSystem.getInstance().refreshAndFindFileByPath(path));
+
+    for (path in repositoryFiles.pathsToWatch) {
+      DvcsUtil.ensureAllChildrenInVfs(LocalFileSystem.getInstance().refreshAndFindFileByPath(path))
     }
   }
 }
