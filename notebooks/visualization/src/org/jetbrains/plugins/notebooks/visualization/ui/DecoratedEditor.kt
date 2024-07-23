aaec7fc7edad7abb77aa089e2a8314ccaf86a6eb
Anton Efimchuk <anton.efimchuk@jetbrains.com>
2024-07-23 11:32:32 +0200

    Revert "PY-74049 Improve EditorEmbeddedComponentManager to increase perfomance of notebooks"
    This reverts commit bf54b00845ce4126bbcceb9dfba79753d26f028f.
    
    GitOrigin-RevId: 7a1ce8b476d4328b4b7d979ade2477ceea74d8ee

--- notebooks/visualization/src/org/jetbrains/plugins/notebooks/visualization/ui/DecoratedEditor.kt
+++ notebooks/visualization/src/org/jetbrains/plugins/notebooks/visualization/ui/DecoratedEditor.kt
@@ -9,6 +9,7 @@ import com.intellij.openapi.editor.event.*
 import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.editor.ex.util.EditorScrollingPositionKeeper
 import com.intellij.openapi.editor.impl.EditorComponentImpl
+import com.intellij.openapi.editor.impl.EditorEmbeddedComponentManager.FullEditorWidthRenderer
 import com.intellij.openapi.fileEditor.FileEditorState
 import com.intellij.openapi.fileEditor.FileEditorStateLevel
 import com.intellij.openapi.fileEditor.TextEditor
@@ -286,7 +287,7 @@ internal fun keepScrollingPositionWhile(editor: Editor, task: Runnable) {
 
 class EditorComponentWrapper(
   private val editor: Editor,
-  editorComponent: EditorComponentImpl,
+  private val editorComponent: EditorComponentImpl,
   private val manager: NotebookCellInlayManager,
 ) : JComponent() {
   init {
@@ -297,6 +298,13 @@ class EditorComponentWrapper(
 
   override fun doLayout() {
     super.doLayout()
+    // EditorEmbeddedComponentManager breaks the Swing layout model as it expect that subcomponents will define their own bounds themselves.
+    // Here we invoke FullEditorWidthRenderer#validate to place inlay components correctly after doLayout.
+    editorComponent.components.asSequence()
+      .filterIsInstance<FullEditorWidthRenderer>()
+      .forEach {
+        it.validate()
+      }
     manager.validateCells()
   }
 
