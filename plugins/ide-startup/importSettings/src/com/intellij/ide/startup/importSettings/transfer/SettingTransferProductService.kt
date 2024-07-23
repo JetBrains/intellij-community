7720447187835ba9efc900206d71e6b47f72ca25
Ivan Migalev <ivan.migalev@jetbrains.com>
2024-07-23 20:03:02 +0200

    (RIDER-111570) Setting Import: introduce a mechanism to add new transferable settings
    (cherry picked from commit ff837fe38950a02d07c151b149546a4efc599bb1)
    
    IJ-CR-140840
    
    GitOrigin-RevId: e3bed6b65c800c1288d155834370907e169da1f4

--- plugins/ide-startup/importSettings/src/com/intellij/ide/startup/importSettings/transfer/SettingTransferProductService.kt
+++ plugins/ide-startup/importSettings/src/com/intellij/ide/startup/importSettings/transfer/SettingTransferProductService.kt
@@ -96,6 +96,7 @@ class SettingTransferProductService(
           }
         }
         settings.recentProjects.nullize()?.let(TransferableSetting::recentProjects)?.let(::add)
+        addAll(ThirdPartyProductSettingItemProvider.generateSettingItems(product.product.transferableId, settings))
       }
     } ?: emptyList()
   }
