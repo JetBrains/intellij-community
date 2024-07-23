c0bfe5cb7bf870daff6a51aac18211f46b078a82
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-07-23 22:05:59 +0200

    [idea] IDEA-327244 Unbundle Java Swing UI Designer plugin
    GitOrigin-RevId: 891e3e0d30119aa3510ce6f1ab33ec16f50ac1d6

--- platform/build-scripts/src/org/jetbrains/intellij/build/BaseIdeaProperties.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/BaseIdeaProperties.kt
@@ -68,7 +68,6 @@ val IDEA_BUNDLED_PLUGINS: PersistentList<String> = DEFAULT_BUNDLED_PLUGINS + per
   "intellij.junit",
   "intellij.testng",
   "intellij.java.i18n",
-  "intellij.java.guiForms.designer",
   "intellij.java.byteCodeViewer",
   "intellij.java.coverage",
   "intellij.java.decompiler",
