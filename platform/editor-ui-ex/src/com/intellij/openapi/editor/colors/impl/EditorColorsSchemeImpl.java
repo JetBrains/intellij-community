211016f8267ff7c507f66c759cdfdec8982625a7
Alexey Kudravtsev <cdr@intellij.com>
2024-07-24 16:32:00 +0200

    do not trash the original color scheme on clone() to fix IJPL-158995 Semantic highlighting disappears on mouse hover
    the mouse ctrl-hover tried to display all range highlighters with slightly changed attributes (underline the current reference to show clickability)
    that color scheme called clone() on itself, which called copyTo()
    copyTo() trashed its own temp-attributes map due to a bug
    which caused all highlighters with temp-attributes to clear these attributes
    which looked as if these highlighters are disappeared
    
    (cherry picked from commit 114804fbf77950abd08e7f78b827ccfd771d0bd0)
    
    
    (cherry picked from commit 6638dd1e2cbcf7470ca5b0612e15f8b3990bbe0b)
    
    IJ-CR-144246
    
    GitOrigin-RevId: a852c0bc9e3621f1d5505cf9479b36dcfc4ff3ba

--- platform/editor-ui-ex/src/com/intellij/openapi/editor/colors/impl/EditorColorsSchemeImpl.java
+++ platform/editor-ui-ex/src/com/intellij/openapi/editor/colors/impl/EditorColorsSchemeImpl.java
@@ -60,12 +60,6 @@ public class EditorColorsSchemeImpl extends AbstractColorsScheme implements Exte
   }
 
   @Override
-  public void copyTo(AbstractColorsScheme newScheme) {
-    super.copyTo(newScheme);
-    myAttributesTempMap.clear();
-  }
-
-  @Override
   public void setAttributes(@NotNull TextAttributesKey key, @NotNull TextAttributes attributes) {
     if (TextAttributesKey.isTemp(key)) {
       myAttributesTempMap.put(key.getExternalName(), attributes);
