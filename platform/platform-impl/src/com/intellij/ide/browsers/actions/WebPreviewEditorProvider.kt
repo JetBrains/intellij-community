b0391815670ebc2d8d6170efeee046daa84ff240
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-07-24 16:14:40 +0200

    don't create WebPreviewEditorProvider in EDT (UI is created in init)
    GitOrigin-RevId: b3f213a758ff7bd267cd56e026467c08e5187d15

--- platform/platform-impl/src/com/intellij/ide/browsers/actions/WebPreviewEditorProvider.kt
+++ platform/platform-impl/src/com/intellij/ide/browsers/actions/WebPreviewEditorProvider.kt
@@ -2,8 +2,6 @@
 package com.intellij.ide.browsers.actions
 
 import com.intellij.openapi.application.EDT
-import com.intellij.openapi.application.ModalityState
-import com.intellij.openapi.application.asContextElement
 import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.fileEditor.AsyncFileEditorProvider
@@ -14,7 +12,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 private class WebPreviewEditorProvider : AsyncFileEditorProvider {
   override fun accept(project: Project, file: VirtualFile): Boolean = file is WebPreviewVirtualFile
@@ -27,10 +25,10 @@ private class WebPreviewEditorProvider : AsyncFileEditorProvider {
     document: Document?,
     editorCoroutineScope: CoroutineScope,
   ): FileEditor {
-    val editor = WebPreviewFileEditor(file as WebPreviewVirtualFile)
     val fileDocumentManager = serviceAsync<FileDocumentManager>()
-    editorCoroutineScope.launch(Dispatchers.EDT + ModalityState.nonModal().asContextElement()) {
+    val editor = withContext(Dispatchers.EDT) {
       fileDocumentManager.saveAllDocuments()
+      WebPreviewFileEditor(file as WebPreviewVirtualFile)
     }
     editor.reloadPage()
     return editor
