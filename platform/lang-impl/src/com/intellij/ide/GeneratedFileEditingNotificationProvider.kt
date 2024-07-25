bb0a02eda65fe609c1b35ad6914a23f8a94d54ce
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-07-25 15:41:04 +0200

    refactor GeneratedSourcesFilter and GeneratedFileEditingNotificationProvider
    GitOrigin-RevId: 87a1b24680dc64e073e3becd8ac5317a0f54438d

--- platform/lang-impl/src/com/intellij/ide/GeneratedFileEditingNotificationProvider.kt
+++ platform/lang-impl/src/com/intellij/ide/GeneratedFileEditingNotificationProvider.kt
@@ -1,48 +1,47 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.ide;
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide
 
-import com.intellij.lang.LangBundle;
-import com.intellij.openapi.fileEditor.FileEditor;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.GeneratedSourcesFilter;
-import com.intellij.openapi.util.NlsContexts;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.ui.EditorNotificationPanel;
-import com.intellij.ui.EditorNotificationProvider;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.lang.LangBundle
+import com.intellij.openapi.fileEditor.FileEditor
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.GeneratedSourcesFilter
+import com.intellij.openapi.util.NlsContexts
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.EditorNotificationPanel
+import com.intellij.ui.EditorNotificationProvider
+import java.util.function.Function
+import javax.swing.JComponent
 
-import javax.swing.*;
-import java.util.function.Function;
-
-final class GeneratedFileEditingNotificationProvider implements EditorNotificationProvider, DumbAware {
-  @Override
-  public @Nullable Function<? super @NotNull FileEditor, ? extends @Nullable JComponent> collectNotificationData(@NotNull Project project,
-                                                                                                                 @NotNull VirtualFile file) {
-    if (!GeneratedSourceFileChangeTracker.getInstance(project).isEditedGeneratedFile(file)) return null;
-
-    String notificationText = getText(file, project);
-
-    return fileEditor -> {
-      EditorNotificationPanel panel = new EditorNotificationPanel(fileEditor, EditorNotificationPanel.Status.Warning);
-      panel.setText(notificationText);
-      return panel;
-    };
-  }
-
-  private static @NlsContexts.LinkLabel @NotNull String getText(@NotNull VirtualFile file, @NotNull Project project) {
-    if (!project.isDisposed() && file.isValid()) {
-      for (GeneratedSourcesFilter filter : GeneratedSourcesFilter.EP_NAME.getExtensionList()) {
-        if (!filter.isGeneratedSource(file, project)) {
-          continue;
-        }
-        String text = filter.getNotificationText(file, project);
-        if (text != null) {
-          return text;
-        }
-      }
+internal class GeneratedFileEditingNotificationProvider : EditorNotificationProvider, DumbAware {
+  override fun collectNotificationData(
+    project: Project,
+    file: VirtualFile
+  ): Function<in FileEditor, out JComponent?>? {
+    if (!GeneratedSourceFileChangeTracker.getInstance(project).isEditedGeneratedFile(file)) {
+      return null
     }
-    return LangBundle.message("link.label.generated.source.files");
+
+    val notificationText = getText(file, project)
+    return Function { fileEditor ->
+      val panel = EditorNotificationPanel(fileEditor, EditorNotificationPanel.Status.Warning)
+      panel.text = notificationText
+      panel
+    }
   }
+}
+
+private fun getText(file: VirtualFile, project: Project): @NlsContexts.LinkLabel String {
+  if (project.isDisposed || !file.isValid) return LangBundle.message("link.label.generated.source.files")
+  for (filter in GeneratedSourcesFilter.EP_NAME.extensionList) {
+    if (!filter.isGeneratedSource(file, project)) {
+      continue
+    }
+
+    val text = filter.getNotificationText(file, project)
+    if (text != null) {
+      return text
+    }
+  }
+  return LangBundle.message("link.label.generated.source.files")
 }
