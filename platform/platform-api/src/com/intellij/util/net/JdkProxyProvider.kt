9a18e59ffd2dba2d27620f5a127ebfa9e76e7c87
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2024-07-26 13:02:02 +0200

    IJPL-159122 override JDK proxy selector in ApplicationInitializedListener instead of service preloading
    GitOrigin-RevId: 9f4720a51a50600d6d5b043ea556cca9f344df08

--- platform/platform-api/src/com/intellij/util/net/JdkProxyProvider.kt
+++ platform/platform-api/src/com/intellij/util/net/JdkProxyProvider.kt
@@ -1,9 +1,11 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.net
 
+import com.intellij.ide.ApplicationInitializedListener
 import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.util.proxy.CommonProxyCompatibility
+import kotlinx.coroutines.CoroutineScope
 import java.net.Authenticator
 import java.net.ProxySelector
 
@@ -36,10 +38,8 @@ sealed interface JdkProxyProvider {
   }
 }
 
-private class OverrideDefaultJdkProxy {
-  // TODO: maybe change this from service preloading to ApplicationLoadListener,
-  //  but after the application is registered and we can access services
-  init {
+private class OverrideDefaultJdkProxy : ApplicationInitializedListener {
+  override suspend fun execute(asyncScope: CoroutineScope) {
     CommonProxyCompatibility.register(
       JdkProxyProvider.getInstance().proxySelector,
       JdkProxyProvider.getInstance().authenticator,
