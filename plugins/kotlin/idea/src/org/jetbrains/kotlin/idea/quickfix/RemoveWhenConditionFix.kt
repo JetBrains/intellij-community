5d28b71046704b14a9e84234f1f922fff98722f9
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2024-07-27 21:57:45 +0400

    [kotlin] Convert RemoveWhenConditionFix to ModCommandAction
    ^KTIJ-29531
    
    GitOrigin-RevId: cffd2bc346db414e4e743ed4d2e7e63832afc71a

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/RemoveWhenConditionFix.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/RemoveWhenConditionFix.kt
@@ -2,36 +2,42 @@
 
 package org.jetbrains.kotlin.idea.quickfix
 
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.modcommand.ActionContext
+import com.intellij.modcommand.ModPsiUpdater
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.diagnostics.Errors
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.quickfixes.KotlinQuickFixAction
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.KotlinPsiUpdateModCommandAction
 import org.jetbrains.kotlin.psi.EditCommaSeparatedListHelper
-import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtWhenCondition
 import org.jetbrains.kotlin.psi.KtWhenEntry
 import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
-class RemoveWhenConditionFix(element: KtWhenCondition) : KotlinQuickFixAction<KtWhenCondition>(element) {
+class RemoveWhenConditionFix(
+    element: KtWhenCondition,
+) : KotlinPsiUpdateModCommandAction.ElementBased<KtWhenCondition, Unit>(element, Unit) {
+
     override fun getFamilyName() = KotlinBundle.message("remove.condition")
 
-    override fun getText() = familyName
-
-    override fun invoke(project: Project, editor: Editor?, file: KtFile) {
-        element?.let { EditCommaSeparatedListHelper.removeItem(it) }
+    override fun invoke(
+        actionContext: ActionContext,
+        element: KtWhenCondition,
+        elementContext: Unit,
+        updater: ModPsiUpdater,
+    ) {
+        element.let { EditCommaSeparatedListHelper.removeItem(it) }
     }
 
     companion object : KotlinSingleIntentionActionFactory() {
-        override fun createAction(diagnostic: Diagnostic): RemoveWhenConditionFix? {
+        override fun createAction(diagnostic: Diagnostic): IntentionAction? {
             if (diagnostic.factory != Errors.SENSELESS_NULL_IN_WHEN) return null
             val whenCondition = diagnostic.psiElement.getStrictParentOfType<KtWhenCondition>() ?: return null
             val conditions = whenCondition.parent.safeAs<KtWhenEntry>()?.conditions?.size ?: return null
             if (conditions < 2) return null
 
-            return RemoveWhenConditionFix(whenCondition)
+            return RemoveWhenConditionFix(whenCondition).asIntention()
         }
     }
 }
