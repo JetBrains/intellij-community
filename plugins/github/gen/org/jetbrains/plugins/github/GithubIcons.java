68fac4e25a65cc3e7d54d4a7c812c6a0d34cb2de
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-07-28 12:40:58 +0200

    [icons] IJPL-60678 License all monorepo icons for plugin authors
    GitOrigin-RevId: 7fa4747a995c38a5281f35aaec4031c48c7e0274

--- plugins/github/gen/org/jetbrains/plugins/github/GithubIcons.java
+++ plugins/github/gen/org/jetbrains/plugins/github/GithubIcons.java
@@ -17,15 +17,15 @@ public final class GithubIcons {
   private static @NotNull Icon load(@NotNull String expUIPath, @NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, expUIPath, GithubIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon GithubWarning = load("org/jetbrains/plugins/github/githubWarning.svg", -1077307434, 2);
-  /** 16x16 */ public static final @NotNull Icon IssueClosed = load("org/jetbrains/plugins/github/issueClosed.svg", 1521176788, 0);
-  /** 16x16 */ public static final @NotNull Icon IssueOpened = load("org/jetbrains/plugins/github/issueOpened.svg", 873410228, 0);
-  /** 16x16 */ public static final @NotNull Icon LocalBranch = load("org/jetbrains/plugins/github/localBranch.svg", 1596019706, 2);
-  /** 16x16 */ public static final @NotNull Icon PullRequestDraft = load("org/jetbrains/plugins/github/pullRequestDraft.svg", 1604718817, 0);
-  /** 16x16 */ public static final @NotNull Icon PullRequestMerged = load("org/jetbrains/plugins/github/pullRequestMerged.svg", 240330859, 0);
-  /** 13x13 */ public static final @NotNull Icon PullRequestsToolWindow = load("org/jetbrains/plugins/github/expui/pullRequests.svg", "org/jetbrains/plugins/github/pullRequestsToolWindow.svg", -198411003, 2);
-  /** 16x16 */ public static final @NotNull Icon Review = load("org/jetbrains/plugins/github/review.svg", -352327129, 0);
-  /** 16x16 */ public static final @NotNull Icon ReviewAccepted = load("org/jetbrains/plugins/github/reviewAccepted.svg", 1063742228, 0);
-  /** 16x16 */ public static final @NotNull Icon ReviewRejected = load("org/jetbrains/plugins/github/reviewRejected.svg", -1652779952, 0);
-  /** 16x16 */ public static final @NotNull Icon Timeline = load("org/jetbrains/plugins/github/timeline.svg", -95899456, 0);
+  /** 16x16 */ public static final @NotNull Icon GithubWarning = load("org/jetbrains/plugins/github/githubWarning.svg", 1935282329, 2);
+  /** 16x16 */ public static final @NotNull Icon IssueClosed = load("org/jetbrains/plugins/github/issueClosed.svg", 1057810441, 0);
+  /** 16x16 */ public static final @NotNull Icon IssueOpened = load("org/jetbrains/plugins/github/issueOpened.svg", 1115233377, 0);
+  /** 16x16 */ public static final @NotNull Icon LocalBranch = load("org/jetbrains/plugins/github/localBranch.svg", -549668610, 2);
+  /** 16x16 */ public static final @NotNull Icon PullRequestDraft = load("org/jetbrains/plugins/github/pullRequestDraft.svg", -1818110574, 0);
+  /** 16x16 */ public static final @NotNull Icon PullRequestMerged = load("org/jetbrains/plugins/github/pullRequestMerged.svg", -1216177352, 0);
+  /** 13x13 */ public static final @NotNull Icon PullRequestsToolWindow = load("org/jetbrains/plugins/github/expui/pullRequests.svg", "org/jetbrains/plugins/github/pullRequestsToolWindow.svg", -679752252, 2);
+  /** 16x16 */ public static final @NotNull Icon Review = load("org/jetbrains/plugins/github/review.svg", -1588546357, 0);
+  /** 16x16 */ public static final @NotNull Icon ReviewAccepted = load("org/jetbrains/plugins/github/reviewAccepted.svg", 536493489, 0);
+  /** 16x16 */ public static final @NotNull Icon ReviewRejected = load("org/jetbrains/plugins/github/reviewRejected.svg", 108213108, 0);
+  /** 16x16 */ public static final @NotNull Icon Timeline = load("org/jetbrains/plugins/github/timeline.svg", 425956960, 0);
 }
--- plugins/maven/src/main/gen/icons/MavenIcons.java
+++ plugins/maven/src/main/gen/icons/MavenIcons.java
@@ -17,20 +17,20 @@ public final class MavenIcons {
   private static @NotNull Icon load(@NotNull String expUIPath, @NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, expUIPath, MavenIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon ChildrenProjects = load("images/expui/build/mavenChildrenProjects.svg", "images/childrenProjects.svg", 399507071, 2);
-  /** 16x16 */ public static final @NotNull Icon ExecuteMavenGoal = load("images/executeMavenGoal.svg", 1709385426, 2);
+  /** 16x16 */ public static final @NotNull Icon ChildrenProjects = load("images/expui/build/mavenChildrenProjects.svg", "images/childrenProjects.svg", 2036191801, 2);
+  /** 16x16 */ public static final @NotNull Icon ExecuteMavenGoal = load("images/executeMavenGoal.svg", 1406361689, 2);
   /** 16x16 */ public static final @NotNull Icon MavenIgnored = load("images/expui/fileTypes/mavenIgnored.svg", "images/mavenIgnored.svg", -1578112873, 2);
-  /** 16x16 */ public static final @NotNull Icon MavenLoadChanges = load("images/expui/build/mavenLoadChanges.svg", "images/mavenLoadChanges.svg", 15449020, 2);
+  /** 16x16 */ public static final @NotNull Icon MavenLoadChanges = load("images/expui/build/mavenLoadChanges.svg", "images/mavenLoadChanges.svg", 747097944, 2);
   /** 16x16 */ public static final @NotNull Icon MavenModule = load("images/mavenModule.svg", -1526380836, 2);
-  /** 16x16 */ public static final @NotNull Icon MavenPlugin = load("images/expui/build/mavenPlugin.svg", "images/mavenPlugin.svg", 660249577, 0);
-  /** 16x16 */ public static final @NotNull Icon MavenProject = load("images/expui/build/mavenProject.svg", "images/mavenProject.svg", -1399986903, 0);
-  /** 16x16 */ public static final @NotNull Icon MavenRepoFolder = load("images/expui/build/mavenProfiles.svg", "images/mavenRepoFolder.svg", 205206251, 0);
-  /** 16x16 */ public static final @NotNull Icon MavenRepoLocal = load("images/expui/build/mavenProject.svg", "images/mavenRepoLocal.svg", -1519797757, 0);
-  /** 16x16 */ public static final @NotNull Icon MavenRepoRemote = load("images/expui/build/mavenPluginGoal.svg", "images/mavenRepoRemote.svg", 1551467989, 0);
-  /** 16x16 */ public static final @NotNull Icon ModulesClosed = load("images/expui/build/mavenModule.svg", "images/modulesClosed.svg", -1412181772, 0);
-  /** 16x16 */ public static final @NotNull Icon ParentProject = load("images/expui/build/mavenParentProjects.svg", "images/parentProject.svg", -863135840, 2);
-  /** 16x16 */ public static final @NotNull Icon PluginGoal = load("images/expui/build/mavenPluginGoal.svg", "images/pluginGoal.svg", 1879670671, 0);
-  /** 16x16 */ public static final @NotNull Icon ProfilesClosed = load("images/expui/build/mavenProfiles.svg", "images/profilesClosed.svg", -801368669, 0);
-  /** 13x13 */ public static final @NotNull Icon ToolWindowMaven = load("images/expui/toolwindow/maven.svg", "images/toolWindowMaven.svg", 1575083751, 2);
-  /** 16x16 */ public static final @NotNull Icon UpdateFolders = load("images/expui/build/updateFolders.svg", "images/updateFolders.svg", 1611888062, 2);
+  /** 16x16 */ public static final @NotNull Icon MavenPlugin = load("images/expui/build/mavenPlugin.svg", "images/mavenPlugin.svg", -107121173, 0);
+  /** 16x16 */ public static final @NotNull Icon MavenProject = load("images/expui/build/mavenProject.svg", "images/mavenProject.svg", -927379482, 0);
+  /** 16x16 */ public static final @NotNull Icon MavenRepoFolder = load("images/expui/build/mavenProfiles.svg", "images/mavenRepoFolder.svg", 1149986657, 0);
+  /** 16x16 */ public static final @NotNull Icon MavenRepoLocal = load("images/expui/build/mavenProject.svg", "images/mavenRepoLocal.svg", 1443971238, 0);
+  /** 16x16 */ public static final @NotNull Icon MavenRepoRemote = load("images/expui/build/mavenPluginGoal.svg", "images/mavenRepoRemote.svg", 185792329, 0);
+  /** 16x16 */ public static final @NotNull Icon ModulesClosed = load("images/expui/build/mavenModule.svg", "images/modulesClosed.svg", 1651686179, 0);
+  /** 16x16 */ public static final @NotNull Icon ParentProject = load("images/expui/build/mavenParentProjects.svg", "images/parentProject.svg", -572722352, 2);
+  /** 16x16 */ public static final @NotNull Icon PluginGoal = load("images/expui/build/mavenPluginGoal.svg", "images/pluginGoal.svg", 245799491, 0);
+  /** 16x16 */ public static final @NotNull Icon ProfilesClosed = load("images/expui/build/mavenProfiles.svg", "images/profilesClosed.svg", 1054654952, 0);
+  /** 13x13 */ public static final @NotNull Icon ToolWindowMaven = load("images/expui/toolwindow/maven.svg", "images/toolWindowMaven.svg", 139561039, 2);
+  /** 16x16 */ public static final @NotNull Icon UpdateFolders = load("images/expui/build/updateFolders.svg", "images/updateFolders.svg", 1966363030, 2);
 }
