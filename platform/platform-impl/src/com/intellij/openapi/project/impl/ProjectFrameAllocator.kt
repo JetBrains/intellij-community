063984bb114428023add34be44c1bb093f078fde
Ivan Semenov <ivan.semenov@jetbrains.com>
2024-07-28 16:12:12 +0200

    [wm] add documentation for project frame allocator
    GitOrigin-RevId: 26a324d8a5a2cacb026c9cc8f82558dc6607f7ec

--- platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectFrameAllocator.kt
+++ platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectFrameAllocator.kt
@@ -6,16 +6,46 @@ package com.intellij.openapi.project.impl
 import com.intellij.conversion.CannotConvertException
 import com.intellij.openapi.project.Project
 
+/**
+ * Allows querying project init state
+ * NB: order of pre-init and init is not guaranteed to allow for parallel processing
+ */
 internal sealed interface ProjectInitObservable {
+  /**
+   * Set when all project init activities are done or scheduled
+   */
   val projectInitTimestamp: Long
+
+  /**
+   * Await project pre-init activities completion, like [ProjectFrameAllocator.preInitProject] and workspace preparation
+   */
   suspend fun awaitProjectPreInit(): Project
+
+  /**
+   * Await full project initialization
+   */
   suspend fun awaitProjectInit(): Project
 }
 
 internal interface ProjectFrameAllocator {
+  /**
+   * A job that will be run in parallel with [run] and will be canceled when allocation is complete
+   */
   suspend fun runInBackground(projectInitObservable: ProjectInitObservable)
+
+  /**
+   * Allocate, set up and show the project frame
+   */
   suspend fun run(projectInitObservable: ProjectInitObservable)
+
+  /**
+   * A job that should be run before project components creation
+   */
   suspend fun preInitProject(project: Project)
+
+  /**
+   * Signaled when project was not loaded for any reason like error or cancellation
+   */
   suspend fun projectNotLoaded(cannotConvertException: CannotConvertException?)
 }
 
