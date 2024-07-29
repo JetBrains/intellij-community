90a843fe8a6b48c7ced5eac108c7b6134682bbde
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2024-07-29 15:51:01 +0200

    remove unused `ExtensionPointChangeListener`
    GitOrigin-RevId: 0f0279679a05d05fcd9b71de5cac02b7fcb9e682

--- platform/extensions/src/com/intellij/openapi/extensions/ExtensionPointAdapter.java
+++ platform/extensions/src/com/intellij/openapi/extensions/ExtensionPointAdapter.java
@@ -1,11 +1,8 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.extensions;
 
 import org.jetbrains.annotations.NotNull;
 
-/**
- * @see ExtensionPointChangeListener
- */
 public abstract class ExtensionPointAdapter<T> implements ExtensionPointListener<T> {
   @Override
   public final void extensionAdded(@NotNull T extension, @NotNull PluginDescriptor pluginDescriptor) {
