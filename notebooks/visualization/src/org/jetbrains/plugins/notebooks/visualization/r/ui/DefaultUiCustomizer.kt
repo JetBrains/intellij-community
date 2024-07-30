23e7f5262032687b87afd499d68e43e92bd5a55f
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2024-07-30 14:19:59 +0300

    PY-74294: Make dependency on intellij.platform.images non-essential
    GitOrigin-RevId: 74593ea0b9f72c9aa4ae896f277a3baf004a9f3f

--- notebooks/visualization/src/org/jetbrains/plugins/notebooks/visualization/r/ui/DefaultUiCustomizer.kt
+++ notebooks/visualization/src/org/jetbrains/plugins/notebooks/visualization/r/ui/DefaultUiCustomizer.kt
@@ -1,12 +1,7 @@
 package org.jetbrains.plugins.notebooks.visualization.r.ui
 
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.ui.UIUtil
-import org.intellij.images.editor.ImageEditor
-import org.intellij.images.editor.impl.ImageEditorImpl
-import org.jetbrains.plugins.notebooks.visualization.r.inlays.components.GraphicsPanel
 import org.jetbrains.plugins.notebooks.visualization.r.inlays.components.ToolbarPane
 import org.jetbrains.plugins.notebooks.visualization.r.inlays.components.progress.InlayProgressStatus
 import org.jetbrains.plugins.notebooks.visualization.r.inlays.components.progress.JupyterProgressStatus
@@ -14,9 +9,6 @@ import javax.swing.JComponent
 import javax.swing.JPanel
 
 class DefaultUiCustomizer : UiCustomizer {
-  override fun createImageEditor(project: Project, file: VirtualFile, graphicsPanel: GraphicsPanel): ImageEditor =
-    ImageEditorImpl(project, file)
-
   override fun toolbarPaneProgressComponentChanged(toolbarPane: ToolbarPane, component: JComponent?): Unit = Unit
 
   override fun toolbarPaneToolbarComponentChanged(toolbarPane: ToolbarPane, component: JComponent?): Unit = Unit
