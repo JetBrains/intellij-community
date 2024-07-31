efddc2e52c09e945e8ae83cb07dc85aa7b39fc35
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2024-07-31 17:46:59 +0200

    Revert "[Kotlin] Deleted quick-fix tests for fully qualifying calls with ambiguous resolution."
    This reverts commit 98f33cb4238b9c2ad50e5ca40bf1ef9cfd7ca1b2.
    
    GitOrigin-RevId: 818a7254dbeeb3322bfb33849e27f59843edc42d

---
+++ plugins/kotlin/idea/tests/testData/quickfix/useFullyqualifiedCall/NestedObjectInRoot.kt
@@ -0,0 +1,16 @@
+// "Use fully qualified call" "true"
+
+object Test1 {
+    fun <T> foo(f: () -> T): T = f()
+    fun bar(): Int = 0
+
+    object Scope {
+        fun bar(x: Int = 0): String = ""
+
+        fun test() {
+            val result = foo(::<caret>bar)
+        }
+    }
+}
+
+// FUS_QUICKFIX_NAME: org.jetbrains.kotlin.idea.quickfix.UseFullyQualifiedCallFix
