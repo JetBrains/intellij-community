8e9a4ff83778ee24d54eb2ea3eaf0116530f2eca
Tagir Valeev <tagir.valeev@jetbrains.com>
2024-07-31 12:32:07 +0200

    [java-inspections] InlineVariableFix: avoid PSI writes after highlighting
    Highlighting causes PSI commit and reformatting. It looks like modifying PSI after that may cause problems. Probably we can fix it later in ModCommand API, but for now we can just reorder things.
    Fixes IDEA-355992 "Inline variable" quick-fix doesn't work if the variable initializer is on the next line
    
    
    (cherry picked from commit f0a2418ac5618f91291b1f74ad2168d26c612447)
    
    IJ-CR-141424
    
    GitOrigin-RevId: f97ab2f3e1bef5c6961e5992fca93565232deb03

---
+++ java/java-tests/testData/ig/com/siyeh/igfixes/inline/NewLineInInitializer.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Demo {
+  List<String> test(List<String> list) {
+    List<String> re<caret>sult =
+      list.stream()
+        .map(String::toUpperCase)
+        .map(String::trim)
+        .toList();
+    return result;
+  }
+}
