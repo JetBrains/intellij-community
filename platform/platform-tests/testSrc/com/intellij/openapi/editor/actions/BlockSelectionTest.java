fe155c63f32cd15aaa48ec67018e2dadb9776c47
Alexandr Trushev <aleksandr.trushev@jetbrains.com>
2024-08-01 20:27:38 +0200

    IJPL-1202 get rid of groovy in `BlockSelectionTest`
    GitOrigin-RevId: 5839fb6c6d40fd96f8dc4d7ef97d773726e19f10

--- platform/platform-tests/testSrc/com/intellij/openapi/editor/actions/BlockSelectionTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/editor/actions/BlockSelectionTest.java
@@ -13,45 +13,46 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intellij.openapi.editor.actions
-import com.intellij.ide.ClipboardSynchronizer
-import com.intellij.openapi.actionSystem.IdeActions
-import com.intellij.openapi.editor.LogicalPosition
-import com.intellij.testFramework.fixtures.BasePlatformTestCase
+package com.intellij.openapi.editor.actions;
 
-import java.awt.datatransfer.StringSelection
-class BlockSelectionTest extends BasePlatformTestCase {
+import com.intellij.ide.ClipboardSynchronizer;
+import com.intellij.openapi.actionSystem.IdeActions;
+import com.intellij.openapi.editor.LogicalPosition;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 
-  void "test paste tabs into block selection"() {
-    myFixture.configureByText "a.txt", """\
-<li></li>
-<li></li>
-"""
-    myFixture.editor.selectionModel.setBlockSelection(new LogicalPosition(0, 4), new LogicalPosition(1, 4))
-    StringSelection content = new StringSelection("""\
-\tCo-ed basketball
-\tCo-ed handball""")
-    ClipboardSynchronizer.instance.setContent(content, content)
-    myFixture.performEditorAction(IdeActions.ACTION_EDITOR_PASTE)
-    myFixture.checkResult """\
-<li>\tCo-ed basketball</li>
-<li>\tCo-ed handball</li>
-"""
+import java.awt.datatransfer.StringSelection;
+
+public class BlockSelectionTest extends BasePlatformTestCase {
+
+  public void test_paste_tabs_into_block_selection() {
+    myFixture.configureByText("a.txt", """
+      <li></li>
+      <li></li>
+      """);
+    myFixture.getEditor().getSelectionModel().setBlockSelection(new LogicalPosition(0, 4), new LogicalPosition(1, 4));
+    StringSelection content = new StringSelection("""
+                                                    \tCo-ed basketball
+                                                    \tCo-ed handball""");
+    ClipboardSynchronizer.getInstance().setContent(content, content);
+    myFixture.performEditorAction(IdeActions.ACTION_EDITOR_PASTE);
+    myFixture.checkResult("""
+                            <li>\tCo-ed basketball</li>
+                            <li>\tCo-ed handball</li>
+                            """);
   }
 
-  void "test paste single tabbed string into block selection"() {
-    myFixture.configureByText "a.txt", """\
-<li></li>
-<li></li>
-"""
-    myFixture.editor.selectionModel.setBlockSelection(new LogicalPosition(0, 4), new LogicalPosition(1, 4))
-    StringSelection content = new StringSelection("\tCo-ed handball")
-    ClipboardSynchronizer.instance.setContent(content, content)
-    myFixture.performEditorAction(IdeActions.ACTION_EDITOR_PASTE)
-    myFixture.checkResult """\
-<li>\tCo-ed handball</li>
-<li>\tCo-ed handball</li>
-"""
+  public void test_paste_single_tabbed_string_into_block_selection() {
+    myFixture.configureByText("a.txt", """
+      <li></li>
+      <li></li>
+      """);
+    myFixture.getEditor().getSelectionModel().setBlockSelection(new LogicalPosition(0, 4), new LogicalPosition(1, 4));
+    StringSelection content = new StringSelection("\tCo-ed handball");
+    ClipboardSynchronizer.getInstance().setContent(content, content);
+    myFixture.performEditorAction(IdeActions.ACTION_EDITOR_PASTE);
+    myFixture.checkResult("""
+                            <li>\tCo-ed handball</li>
+                            <li>\tCo-ed handball</li>
+                            """);
   }
-
 }
