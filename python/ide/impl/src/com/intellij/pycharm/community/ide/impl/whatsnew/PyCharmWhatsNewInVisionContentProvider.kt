c3c1f660e360ad48f21484f4bcd146a764560511
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2024-08-01 10:53:08 +0200

    [pycharm] update vision pages and add a separate vision page to CE
    (cherry picked from commit 26018648c26cbbbe618aeacdea73567835718b27)
    
    IJ-CR-141553
    
    GitOrigin-RevId: b13656bf7da5ac3dfc20ada1bf711a296a551c75

--- python/ide/impl/src/com/intellij/pycharm/community/ide/impl/whatsnew/PyCharmWhatsNewInVisionContentProvider.kt
+++ python/ide/impl/src/com/intellij/pycharm/community/ide/impl/whatsnew/PyCharmWhatsNewInVisionContentProvider.kt
@@ -5,49 +5,13 @@ import com.intellij.platform.whatsNew.ContentSource
 import com.intellij.platform.whatsNew.ResourceContentSource
 import com.intellij.platform.whatsNew.WhatsNewInVisionContentProvider
 import com.intellij.util.PlatformUtils.isCommunityEdition
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.io.InputStream
 
 class PyCharmWhatsNewInVisionContentProvider : WhatsNewInVisionContentProvider() {
 
-  companion object {
-    const val endpoints = "Check out the Endpoints tool window"
-    const val endpointsAction = "ActivateEndpointsToolWindow"
-    const val feelTheDifference = "Feel the difference"
-    const val feelTheDifferenceAction = "showWelcomeNotebook"
-    const val plotly ="See Plotly graphs"
-    const val plotlyAction = "ShowPlotlyGraph"
-    const val getPyCharmPro = "Get PyCharm Professional: free 30-day trial"
-    const val promoEndpointsActionId = "PromoEndpointsAction"
-    const val promoNotebookActionId = "PromoNewJupyterNotebook"
-  }
-
   override fun getResource(): ContentSource {
     // return a vision file for the current version
-    val resourceContentSource = ResourceContentSource(PyCharmWhatsNewInVisionContentProvider::class.java.classLoader, "whatsNew/pycharm2024.2.json")
-    if (isCommunityEdition()) {
-      return object : ContentSource {
-        override suspend fun openStream(): InputStream? {
-          return withContext(Dispatchers.IO) {
-            val stream = resourceContentSource.openStream()?.readAllBytes() ?: return@withContext null
-            val string = String(stream)
-              .replace(endpoints, getPyCharmPro)
-              .replace(endpointsAction, promoEndpointsActionId)
-              .replace(feelTheDifference, getPyCharmPro)
-              .replace(feelTheDifferenceAction, promoNotebookActionId)
-              .replace(plotly, getPyCharmPro)
-              .replace(plotlyAction, promoNotebookActionId)
-            string.byteInputStream()
-          }
-        }
-
-        override suspend fun checkAvailability(): Boolean {
-          return resourceContentSource.checkAvailability()
-        }
-      }
-    }
-
+    val resourceName = if (isCommunityEdition()) "whatsNew/pycharmCE2024.2.json" else "whatsNew/pycharm2024.2.json"
+    val resourceContentSource = ResourceContentSource(PyCharmWhatsNewInVisionContentProvider::class.java.classLoader, resourceName)
     return resourceContentSource
   }
 }
