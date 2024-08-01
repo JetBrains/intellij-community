77863617d0590d436271188acdbd80f68db8467d
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2024-08-01 11:14:48 +0300

    [starter metrics publishing] Use the same approach for metrics publishing to minimize confusion how to do it
    GitOrigin-RevId: a7cec06665ac457db31824c09f67a8b3a0e3c273

--- tools/intellij.tools.ide.metrics.collector/src/com/intellij/tools/ide/metrics/collector/OpenTelemetrySpanCollector.kt
+++ tools/intellij.tools.ide.metrics.collector/src/com/intellij/tools/ide/metrics/collector/OpenTelemetrySpanCollector.kt
@@ -2,15 +2,19 @@ package com.intellij.tools.ide.metrics.collector
 
 import com.intellij.tools.ide.metrics.collector.metrics.PerformanceMetrics
 import com.intellij.tools.ide.metrics.collector.telemetry.SpanFilter
-import com.intellij.tools.ide.metrics.collector.telemetry.getMetricsFromSpanAndChildren
 import java.nio.file.Path
+import kotlin.io.path.isDirectory
 
 /**
  * Collect spans from opentelemetry.json (usually located in the log directory) and convert it to metrics objects
  */
-open class OpenTelemetrySpanCollector(val spanNames: List<String>, private val aliases: Map<String, String> = mapOf()) : TelemetryMetricsCollector {
+open class OpenTelemetrySpanCollector(val spanFilter: SpanFilter, private val spanAliases: Map<String, String> = mapOf()) : TelemetryMetricsCollector {
   override fun collect(logsDirPath: Path): List<PerformanceMetrics.Metric> {
-    val telemetryJsonFile = logsDirPath.resolve("opentelemetry.json").toAbsolutePath()
-    return getMetricsFromSpanAndChildren(telemetryJsonFile, SpanFilter.containsNameIn(spanNames), aliases = aliases)
+    val telemetryJsonFile = if (logsDirPath.isDirectory()) {
+      logsDirPath.resolve("opentelemetry.json").toAbsolutePath()
+    }
+    else logsDirPath
+
+    return getMetricsFromSpanAndChildren(telemetryJsonFile, spanFilter, spanAliases = spanAliases)
   }
 }
--- tools/intellij.tools.ide.metrics.collector/src/com/intellij/tools/ide/metrics/collector/telemetry/openTelemetry.kt
+++ tools/intellij.tools.ide.metrics.collector/src/com/intellij/tools/ide/metrics/collector/telemetry/openTelemetry.kt
@@ -4,7 +4,6 @@ package com.intellij.tools.ide.metrics.collector.telemetry
 
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.tools.ide.metrics.collector.metrics.PerformanceMetrics.Metric
-import com.intellij.tools.ide.metrics.collector.metrics.standardDeviation
 import java.nio.file.Path
 
 const val TOTAL_TEST_TIMER_NAME: String = "test"
@@ -16,36 +15,6 @@ data class MetricWithAttributes(
 )
 
 private val logger = logger<OpentelemetrySpanJsonParser>()
-
-/**
- * Reports duration of `nameSpan` and all its children spans.
- * Replaces the names with an alias if one was passed.
- * Besides, all attributes are reported as counters.
- * If there are multiple values with the same name:
- * 1. They will be re-numbered `<value>_1`, `<value>_2`, etc. and the sum will be recorded as `<value>`.
- * 2. In the sum value, mean value and standard deviation of attribute value will be recorded
- * 2a. If attribute ends with `#max`, in a sum the max of max will be recorded
- * 3a. If attribute ends with `#mean_value`, the mean value of mean values will be recorded
- */
-fun getMetricsFromSpanAndChildren(
-  file: Path,
-  filter: SpanFilter,
-  metricSpanProcessor: SpanProcessor<MetricWithAttributes> = MetricSpanProcessor(),
-  aliases: Map<String, String> = mapOf(),
-  metricsPostProcessor: MetricsPostProcessor = CombinedMetricsPostProcessor(),
-): List<Metric> {
-  val spanElements = OpentelemetrySpanJsonParser(filter).getSpanElements(file).map {
-    val name = aliases.getOrDefault(it.name, it.name)
-    if (name != it.name) {
-      return@map it.copy(name = name)
-    }
-    return@map it
-  }
-
-  val spanToMetricMap: Map<String, List<MetricWithAttributes>> = spanElements.mapNotNull { metricSpanProcessor.process(it) }
-    .groupBy { it.metric.id.name }
-  return metricsPostProcessor.process(spanToMetricMap)
-}
 
 /**
  * Returns a list of metrics based on the difference between two spans.
