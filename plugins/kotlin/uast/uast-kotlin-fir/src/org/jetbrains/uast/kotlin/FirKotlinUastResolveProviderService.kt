79b4560d197ce7ca4143dd5dfb2fce31d669c974
Jinseong Jeon <jsjeon@google.com>
2024-07-31 23:54:28 -0700

    K2 UAST: discard Unit as expression type of constructor call

--- plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/FirKotlinUastResolveProviderService.kt
+++ plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/FirKotlinUastResolveProviderService.kt
@@ -616,13 +616,20 @@ interface FirKotlinUastResolveProviderService : BaseKotlinUastResolveProviderSer
         // Here we bail out syntactical statements that are very obvious.
         if (ktExpression is KtLoopExpression || ktExpression is KtWhenEntry)
             return null
+        // Obvious constructor calls
+        val parent = ktExpression.parent
+        // E.g. class Sub : Base(...)
+        if (ktExpression is KtSuperTypeCallEntry || parent is KtSuperTypeCallEntry)
+            return null
+        // E.g. constructor(...) : this(...)
+        if (ktExpression is KtConstructorDelegationCall || parent is KtConstructorDelegationCall)
+            return null
 
         analyzeForUast(ktExpression) {
             val ktType = ktExpression.expressionType ?: return null
             // Again, Analysis API returns [Unit] for statements, so we need to filter out
             // some cases that are not actually expression's return type.
             if (ktType.isUnitType) {
-                val parent = ktExpression.parent
                 // E.g., AnnotationTarget.FIELD, reference to enum class is resolved to the constructor call,
                 // and then returned as Unit expression type. Same for path segments in a fully qualified name
                 if ((parent is KtQualifiedExpression || parent is KtDoubleColonExpression) &&
