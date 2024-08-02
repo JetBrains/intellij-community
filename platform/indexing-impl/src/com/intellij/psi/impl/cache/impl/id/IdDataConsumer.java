edb733a03878dec90134460a73450bb3584faad2
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2024-08-02 13:40:51 +0200

    [indexes] refactor IdDataConsumer
    + extract explicit int->int map wrapper (IdEntryToScopeMapImpl), extract an IdEntryToScopeMap interface for it
    
    GitOrigin-RevId: cd2cc114425121a945bdf82e3e0ff7a5769a52b4

--- platform/indexing-impl/src/com/intellij/psi/impl/cache/impl/id/IdDataConsumer.java
+++ platform/indexing-impl/src/com/intellij/psi/impl/cache/impl/id/IdDataConsumer.java
@@ -3,110 +3,16 @@
 package com.intellij.psi.impl.cache.impl.id;
 
 import com.intellij.openapi.util.text.StringUtil;
-import it.unimi.dsi.fastutil.ints.Int2IntMap;
-import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
-import it.unimi.dsi.fastutil.ints.IntIterator;
-import it.unimi.dsi.fastutil.objects.ObjectIterator;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
-import java.util.function.BiConsumer;
 
 public final class IdDataConsumer {
-  private final @NotNull Int2IntMap myOccurrences = new Int2IntOpenHashMap();
-  private final @NotNull Map<IdIndexEntry, Integer> myResult = new AbstractMap<>() {
-    @Override
-    public int size() {
-      return myOccurrences.size();
-    }
 
-    @Override
-    public boolean containsKey(Object key) {
-      return key instanceof IdIndexEntry entry && myOccurrences.containsKey(entry.getWordHashCode());
-    }
+  private final @NotNull IdEntryToScopeMapImpl hashToScopeMap = new IdEntryToScopeMapImpl();
 
-    @Override
-    public Integer get(Object key) {
-      //noinspection deprecation
-      return key instanceof IdIndexEntry entry ? myOccurrences.get((Object)entry.getWordHashCode()) : null;
-    }
-
-    @NotNull
-    @Override
-    public Set<IdIndexEntry> keySet() {
-      return new AbstractSet<>() {
-        @Override
-        public boolean contains(Object o) {
-          return o instanceof IdIndexEntry entry && myOccurrences.containsKey(entry.getWordHashCode());
-        }
-
-        @Override
-        public Iterator<IdIndexEntry> iterator() {
-          return new Iterator<>() {
-            final IntIterator myIterator = myOccurrences.keySet().iterator();
-
-            @Override
-            public boolean hasNext() {
-              return myIterator.hasNext();
-            }
-
-            @Override
-            public IdIndexEntry next() {
-              return new IdIndexEntry(myIterator.nextInt());
-            }
-          };
-        }
-
-        @Override
-        public int size() {
-          return myOccurrences.size();
-        }
-      };
-    }
-
-    @NotNull
-    @Override
-    public Collection<Integer> values() {
-      return myOccurrences.values();
-    }
-
-    @NotNull
-    @Override
-    public Set<Entry<IdIndexEntry, Integer>> entrySet() {
-      return new AbstractSet<>() {
-        @Override
-        public Iterator<Entry<IdIndexEntry, Integer>> iterator() {
-          return new Iterator<>() {
-            final ObjectIterator<Int2IntMap.Entry> myIterator = myOccurrences.int2IntEntrySet().iterator();
-
-            @Override
-            public boolean hasNext() {
-              return myIterator.hasNext();
-            }
-
-            @Override
-            public Entry<IdIndexEntry, Integer> next() {
-              Int2IntMap.Entry entry = myIterator.next();
-              return Map.entry(new IdIndexEntry(entry.getIntKey()), entry.getIntValue());
-            }
-          };
-        }
-
-        @Override
-        public int size() {
-          return myOccurrences.size();
-        }
-      };
-    }
-
-    @Override
-    public void forEach(BiConsumer<? super IdIndexEntry, ? super Integer> consumer) {
-      //FIXME RC: we allocate huge amount of IdIndexEntry + Integer here -- mostly for nothing
-      myOccurrences.forEach((hash, value) -> consumer.accept(new IdIndexEntry(hash), value));
-    }
-  };
   public @NotNull Map<IdIndexEntry, Integer> getResult() {
-    return myResult;
+    return hashToScopeMap;
   }
 
   public void addOccurrence(char[] chars, int start, int end, int occurrenceMask) {
@@ -119,6 +25,7 @@ public final class IdDataConsumer {
 
   private void addOccurrence(CharSequence charSequence, char[] chars, int start, int end, int occurrenceMask) {
     if (end == start || occurrenceMask == 0) return;
+    //calculate both case-sensitive & case-insensitive hashes:
     int hash, hashNoCase;
     boolean hasArray = chars != null;
     if (IdIndexEntry.useStrongerHash()) {
@@ -149,10 +56,10 @@ public final class IdDataConsumer {
       hashNoCase = (firstCharLower == firstChar && lastCharLower == lastChar) ? hash :
                    (firstCharLower << 8) + (lastCharLower << 4) + end - start;
     }
-    myOccurrences.mergeInt(hash, occurrenceMask, (prev, cur) -> prev | cur);
+    hashToScopeMap.updateMask(hash, occurrenceMask);
 
     if (hashNoCase != hash) {
-      myOccurrences.mergeInt(hashNoCase, occurrenceMask, (prev, cur) -> prev | cur);
+      hashToScopeMap.updateMask(hashNoCase, occurrenceMask);
     }
   }
 }
