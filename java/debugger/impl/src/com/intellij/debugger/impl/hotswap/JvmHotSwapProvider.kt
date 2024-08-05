1f003b6aa0e69c35e7062620e1f0f8a71b815635
Maksim Zuev <maksim.zuev@jetbrains.com>
2024-08-05 17:16:55 +0200

    [debugger] IDEA-356958 Add file extension filter
    GitOrigin-RevId: 13182094277bcd580f5221825c0f8c93ecfd9bab

--- java/debugger/impl/src/com/intellij/debugger/impl/hotswap/JvmHotSwapProvider.kt
+++ java/debugger/impl/src/com/intellij/debugger/impl/hotswap/JvmHotSwapProvider.kt
@@ -3,6 +3,8 @@ package com.intellij.debugger.impl.hotswap
 
 import com.intellij.debugger.impl.DebuggerSession
 import com.intellij.debugger.ui.HotSwapUI
+import com.intellij.lang.Language
+import com.intellij.lang.jvm.JvmMetaLanguage
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.application.readAction
@@ -17,12 +19,17 @@ internal class JvmHotSwapProvider(private val debuggerSession: DebuggerSession) 
     session: HotSwapSession<VirtualFile>,
     coroutineScope: CoroutineScope,
     listener: SourceFileChangesListener,
-  ) = SourceFileChangesCollectorImpl(
-    coroutineScope, listener,
-    InProjectFilter(session.project),
-    // TODO add another scope check
-    //SearchScopeFilter(debuggerSession.searchScope),
-  )
+  ): SourceFileChangesCollector<VirtualFile> {
+    val jvmExtensions = Language.findInstance(JvmMetaLanguage::class.java).getMatchingLanguages()
+      .mapNotNull { it.associatedFileType?.defaultExtension }
+    return SourceFileChangesCollectorImpl(
+      coroutineScope, listener,
+      FileExtensionFilter(jvmExtensions),
+      InProjectFilter(session.project),
+      // TODO add another scope check
+      //SearchScopeFilter(debuggerSession.searchScope),
+    )
+  }
 
   override fun performHotSwap(context: DataContext, session: HotSwapSession<VirtualFile>) {
     val project = context.getData(CommonDataKeys.PROJECT) ?: return
@@ -33,4 +40,8 @@ internal class JvmHotSwapProvider(private val debuggerSession: DebuggerSession) 
 private class InProjectFilter(private val project: Project) : SourceFileChangeFilter<VirtualFile> {
   override suspend fun isApplicable(change: VirtualFile): Boolean =
     readAction { ProjectFileIndex.getInstance(project).isInSource(change) }
+}
+
+private class FileExtensionFilter(private val extensions: List<String>) : SourceFileChangeFilter<VirtualFile> {
+  override suspend fun isApplicable(change: VirtualFile): Boolean = extensions.contains(change.extension)
 }
