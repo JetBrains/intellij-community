471f1dbb81b94d95dc21ce9a0dfebb1515b48321
Dmitriy.Panov <dmitriy.panov@jetbrains.com>
2024-08-05 17:55:24 +0200

    IJPL-158905 git4idea.notification.GitExternalFileNotifierTest.test no notification after branch checkout may time out
    GitOrigin-RevId: 2329ec9dd9a95adcdd255aab5fe563a6a91177b4

--- plugins/git4idea/tests/git4idea/notification/GitExternalFileNotifierTest.kt
+++ plugins/git4idea/tests/git4idea/notification/GitExternalFileNotifierTest.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.notification
 
+import com.intellij.idea.IJIgnore
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vcs.FileStatus
 import com.intellij.openapi.vcs.VcsBundle
@@ -41,6 +42,7 @@ class GitExternalFileNotifierTest : GitSingleRepoTest() {
     assertNotificationByMessage(VcsBundle.message("external.files.add.notification.message", vcs.displayName))
   }
 
+  @IJIgnore(issue = "IJPL-158905")
   fun `test no notification after branch checkout`() {
     val worker = GitBranchWorker(project, git, GitBranchWorkerTest.TestUiHandler(project))
 
