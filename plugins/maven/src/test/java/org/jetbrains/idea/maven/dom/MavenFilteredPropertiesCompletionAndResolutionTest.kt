a6996efb679401924c8474560df0a6458b8d1ebb
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2024-08-05 10:40:16 +0200

    [maven][test] temporarily disable hanging test IDEA-357133
    GitOrigin-RevId: bcd93914a424fb0a85b9767ba0f4ec56818a742b

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/dom/MavenFilteredPropertiesCompletionAndResolutionTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/dom/MavenFilteredPropertiesCompletionAndResolutionTest.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.maven.dom
 
 import com.intellij.openapi.application.EDT
@@ -28,6 +14,7 @@ import kotlinx.coroutines.withContext
 import org.intellij.lang.annotations.Language
 import org.jetbrains.idea.maven.dom.model.MavenDomProjectModel
 import org.jetbrains.idea.maven.dom.references.MavenPropertyPsiReference
+import org.junit.Ignore
 import org.junit.Test
 import java.util.concurrent.atomic.AtomicReference
 
@@ -266,6 +253,7 @@ class MavenFilteredPropertiesCompletionAndResolutionTest : MavenDomWithIndicesTe
     }
   }
 
+  @Ignore("IDEA-357133")
   @Test
   fun testUsingFilters() = runBlocking {
     val filter = createProjectSubFile("filters/filter.properties", "xxx=1")
