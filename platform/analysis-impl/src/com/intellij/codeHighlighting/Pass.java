d688db983b7b93f6cb9de9a27f7c67d74e88c4c8
Max Medvedev <maxim.medvedev@jetbrains.com>
2024-08-06 19:01:42 +0200

    add hints to Pass ids
    GitOrigin-RevId: 2a225eb085bd8db84ab9930bc0b4154d37c59b6f

--- platform/analysis-impl/src/com/intellij/codeHighlighting/Pass.java
+++ platform/analysis-impl/src/com/intellij/codeHighlighting/Pass.java
@@ -1,31 +1,17 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeHighlighting;
 
 public interface Pass {
-  int UPDATE_FOLDING = 1;
-  int POPUP_HINTS = 3;
-  int UPDATE_ALL = 4;
-  int LOCAL_INSPECTIONS = 7;
-  int EXTERNAL_TOOLS = 8;
-  int WOLF = 9;
-  int LINE_MARKERS = 11;
-  int WHOLE_FILE_LOCAL_INSPECTIONS = 12;
-  int SLOW_LINE_MARKERS = 13;
-  int INJECTED_GENERAL = 14;
+  int UPDATE_FOLDING = 1;                                     // CodeFoldingPassFactory
+  int POPUP_HINTS = 3;                                        // ShowIntentionsPassFactory
+  int UPDATE_ALL = 4;                                         // GeneralHighlightingPassFactory
+  int LOCAL_INSPECTIONS = 7;                                  // LocalInspectionsPassFactory
+  int EXTERNAL_TOOLS = 8;                                     // ExternalToolPassFactory
+  int WOLF = 9;                                               // WolfPassFactory
+  int LINE_MARKERS = 11;                                      // LineMarkersPassFactory
+  @Deprecated int WHOLE_FILE_LOCAL_INSPECTIONS = 12;          // outdated, not used anymore
+  int SLOW_LINE_MARKERS = 13;                                 // SlowLineMarkersPassFactory
+  int INJECTED_GENERAL = 14;                                  // InjectedGeneralHighlightingPassFactory
 
   int LAST_PASS = INJECTED_GENERAL;
 }
