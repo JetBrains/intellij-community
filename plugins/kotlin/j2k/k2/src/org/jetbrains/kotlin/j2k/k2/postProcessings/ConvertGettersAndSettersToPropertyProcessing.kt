ed954c5ca9c5a01ca82edfd769abc6dad857b695
Alexey Belkov <alexey.belkov@jetbrains.com>
2024-08-08 20:37:07 +0400

    [kotlin] J2K: Move ConvertGettersAndSettersToPropertyProcessing to K2
    This is an intermediate commit to preserve git file history.
    The code doesn't compile yet.
    
    KTIJ-30693
    
    GitOrigin-RevId: 6a56798bc1b0396b519f15005f59e5cfee08c68b

--- plugins/kotlin/j2k/k2/src/org/jetbrains/kotlin/j2k/k2/postProcessings/ConvertGettersAndSettersToPropertyProcessing.kt
+++ plugins/kotlin/j2k/k2/src/org/jetbrains/kotlin/j2k/k2/postProcessings/ConvertGettersAndSettersToPropertyProcessing.kt
@@ -1,6 +1,6 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
-package org.jetbrains.kotlin.idea.j2k.post.processing.processings
+package org.jetbrains.kotlin.j2k.k2.postProcessings
 
 import com.intellij.openapi.application.runReadAction
 import com.intellij.psi.PsiComment
