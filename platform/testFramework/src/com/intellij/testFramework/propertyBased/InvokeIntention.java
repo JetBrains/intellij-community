a3a0e09aa53f80e859b1fd7d3910551a3e8eb7f7
Louis Vignier <Louis.Vignier@jetbrains.com>
2024-08-13 01:14:10 +0200

    [java-property-tests] Dispatch invocation events after performing a command
    GitOrigin-RevId: 8c5dbe46c98735aae2ae0867c8b19cb9332e0cea

--- platform/testFramework/src/com/intellij/testFramework/propertyBased/InvokeIntention.java
+++ platform/testFramework/src/com/intellij/testFramework/propertyBased/InvokeIntention.java
@@ -28,6 +28,7 @@ import com.intellij.modcommand.*;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.application.impl.NonBlockingReadActionImpl;
 import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.diagnostic.Logger;
@@ -75,6 +76,7 @@ public class InvokeIntention extends ActionOnFile {
     env.logMessage("Go to " + MadTestingUtil.getPositionDescription(offset, getDocument()));
 
     doInvokeIntention(offset, env);
+    NonBlockingReadActionImpl.waitForAsyncTaskCompletion();
   }
 
   @Nullable
