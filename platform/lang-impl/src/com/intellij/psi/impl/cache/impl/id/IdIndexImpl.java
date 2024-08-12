bdfacbe5ffd9a9fa6e19bc657279515697e27ab8
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2024-08-12 21:04:28 +0200

    [indexes] IJP-1365: utilise optimized externalizer for IdIndex
    + `IdIndexEntryMapExternalizer` knows about Int2IntMap really used by IdIndex indexer under the hood, and persist primitive ints without excessive boxing
    
    GitOrigin-RevId: 00211fac7cc1f21dd34843b066a0e27208cf938f

--- platform/lang-impl/src/com/intellij/psi/impl/cache/impl/id/IdIndexImpl.java
+++ platform/lang-impl/src/com/intellij/psi/impl/cache/impl/id/IdIndexImpl.java
@@ -1,13 +1,28 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.impl.cache.impl.id;
 
+import com.intellij.util.indexing.CustomInputMapIndexExtension;
 import com.intellij.util.indexing.CustomInputsIndexFileBasedIndexExtension;
+import com.intellij.util.indexing.InputMapExternalizer;
 import com.intellij.util.io.DataExternalizer;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.Map;
 
-public final class IdIndexImpl extends IdIndex implements CustomInputsIndexFileBasedIndexExtension<IdIndexEntry> {
+public final class IdIndexImpl extends IdIndex implements CustomInputsIndexFileBasedIndexExtension<IdIndexEntry>,
+                                                          CustomInputMapIndexExtension<IdIndexEntry, Integer> {
+  @Override
+  public @NotNull DataExternalizer<Map<IdIndexEntry, Integer>> createInputMapExternalizer() {
+    DataExternalizer<Collection<IdIndexEntry>> keysExternalizer = createExternalizer();
+    InputMapExternalizer<IdIndexEntry, Integer> fallbackExternalizer = new InputMapExternalizer<>(
+      keysExternalizer,
+      getValueExternalizer(),
+      /*valueIsAbsent: */ false
+    );
+    return new IdIndexEntryMapExternalizer(fallbackExternalizer);
+  }
+
   @Override
   public @NotNull DataExternalizer<Collection<IdIndexEntry>> createExternalizer() {
     return new IdIndexEntriesExternalizer();
