3011e367551fb0f70b01b3558796f0e3926c6eac
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2024-08-13 10:48:50 +0200

    [services] IJPL-159884 add expand/collapse actions for type details part
    GitOrigin-RevId: 2f575da4d52bdbe592fd2bae899bc77b8c3b1f12

--- platform/execution.dashboard/src/RunDashboardComponentWrapper.java
+++ platform/execution.dashboard/src/RunDashboardComponentWrapper.java
@@ -1,13 +1,16 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.execution.dashboard;
 
+import com.intellij.openapi.actionSystem.DataSink;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.UiDataProvider;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Key;
 import com.intellij.ui.components.panels.NonOpaquePanel;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public final class RunDashboardComponentWrapper extends NonOpaquePanel {
+public final class RunDashboardComponentWrapper extends NonOpaquePanel implements UiDataProvider {
   public static final Key<Integer> CONTENT_ID_KEY = Key.create("RunDashboardContentId");
   private final Project myProject;
 
@@ -27,5 +30,10 @@ public final class RunDashboardComponentWrapper extends NonOpaquePanel {
 
   public @NotNull Project getProject() {
     return myProject;
+  }
+
+  @Override
+  public void uiDataSnapshot(@NotNull DataSink sink) {
+    sink.set(PlatformDataKeys.TREE_EXPANDER_HIDE_ACTIONS_IF_NO_EXPANDER, true);
   }
 }
--- platform/execution.dashboard/src/RunDashboardTypePanel.kt
+++ platform/execution.dashboard/src/RunDashboardTypePanel.kt
@@ -7,25 +7,30 @@ import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.execution.configurations.ConfigurationType
 import com.intellij.execution.dashboard.RunDashboardManager
 import com.intellij.icons.AllIcons
+import com.intellij.ide.DefaultTreeExpander
+import com.intellij.openapi.actionSystem.DataSink
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.actionSystem.UiDataProvider
 import com.intellij.openapi.project.Project
 import com.intellij.ui.CheckboxTree
 import com.intellij.ui.CheckboxTreeListener
 import com.intellij.ui.CheckedTreeNode
 import com.intellij.ui.ScrollPaneFactory
+import com.intellij.ui.components.panels.NonOpaquePanel
 import com.intellij.util.SmartList
 import com.intellij.util.containers.FactoryMap
 import java.awt.BorderLayout
-import javax.swing.JPanel
 import javax.swing.JTree
 import javax.swing.tree.DefaultTreeModel
 import javax.swing.tree.TreePath
 
-internal class RunDashboardTypePanel(private val project: Project) : JPanel(BorderLayout()) {
+internal class RunDashboardTypePanel(private val project: Project) : NonOpaquePanel(BorderLayout()), UiDataProvider {
   var type: ConfigurationType? = null
     set(value) {
       field = value
       updateTree(value)
     }
+  private var hasFolders = false
 
   private val root = CheckedTreeNode("TypeRoot")
   private val tree = CheckboxTree(object : CheckboxTree.CheckboxTreeCellRenderer() {
@@ -54,6 +59,7 @@ internal class RunDashboardTypePanel(private val project: Project) : JPanel(Bord
       }
     }
   }, root)
+  private val treeExpander = DefaultTreeExpander(tree)
 
   init {
     project.getMessageBus().connect(project).subscribe(RunManagerListener.TOPIC, object : RunManagerListener {
@@ -99,10 +105,12 @@ internal class RunDashboardTypePanel(private val project: Project) : JPanel(Bord
 
     val scrollPane = ScrollPaneFactory.createScrollPane(tree, true)
     add(scrollPane, BorderLayout.CENTER)
+    RunDashboardManagerImpl.setupToolbar(this, scrollPane, project)
   }
 
   private fun updateTree(type: ConfigurationType?) {
     if (type == null) {
+      hasFolders = false
       root.removeAllChildren()
       (tree.model as DefaultTreeModel).setRoot(root)
       return
@@ -133,8 +141,14 @@ internal class RunDashboardTypePanel(private val project: Project) : JPanel(Bord
     folderNodes.forEach(root::add)
     children.forEach(root::add)
     (tree.model as DefaultTreeModel).setRoot(root)
+    hasFolders = folderNodes.isNotEmpty()
     for (folderNode in folderNodes) {
       tree.expandPath(TreePath(folderNode.path))
     }
+  }
+
+  override fun uiDataSnapshot(sink: DataSink) {
+    sink[PlatformDataKeys.TREE_EXPANDER_HIDE_ACTIONS_IF_NO_EXPANDER] = true
+    sink[PlatformDataKeys.TREE_EXPANDER] = if (hasFolders) treeExpander else null
   }
 }
