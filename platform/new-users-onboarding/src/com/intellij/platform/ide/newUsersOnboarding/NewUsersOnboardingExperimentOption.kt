bafe2cc306af2764fb2723081e3eb68c0d050fc0
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2024-08-13 14:20:43 +0300

    IJPL-43794 Perform New Users Onboarding experiment only in 2024.2.1
    In the other versions it won't be active.
    
    IJ-CR-140970
    
    GitOrigin-RevId: 725ca71ca9f66023a693bbcd9d549a030fe09b8e

--- platform/new-users-onboarding/src/com/intellij/platform/ide/newUsersOnboarding/NewUsersOnboardingExperimentOption.kt
+++ platform/new-users-onboarding/src/com/intellij/platform/ide/newUsersOnboarding/NewUsersOnboardingExperimentOption.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.ide.newUsersOnboarding
 
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.platform.experiment.ab.impl.experiment.ABExperimentOption
 import com.intellij.platform.experiment.ab.impl.experiment.ABExperimentOptionId
 import com.intellij.platform.experiment.ab.impl.option.ABExperimentOptionGroupSize
@@ -14,5 +15,11 @@ internal class NewUsersOnboardingExperimentOption : ABExperimentOption {
 
   override fun checkIdeIsSuitable(): Boolean = true
 
-  override fun checkIdeVersionIsSuitable(): Boolean = true
+  /**
+   * Experiment should be available only in 2024.2.1
+   */
+  override fun checkIdeVersionIsSuitable(): Boolean {
+    val appInfo = ApplicationInfo.getInstance()
+    return appInfo.majorVersion == "2024" && appInfo.minorVersion == "2.1"
+  }
 }
