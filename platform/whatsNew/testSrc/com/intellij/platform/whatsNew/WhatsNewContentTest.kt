6facffaa05a5d34ff96c899e9af799e1a5c35022
Ivan Migalev <ivan.migalev@jetbrains.com>
2024-08-13 17:59:10 +0200

    (IJPL-160170) What's New: do not parse version numbers as ints
    #IJPL-160170 Fixed
    
    GitOrigin-RevId: e113dec9b27baf533a5c4fb2ab9ec2a93638f469

--- platform/whatsNew/testSrc/com/intellij/platform/whatsNew/WhatsNewContentTest.kt
+++ platform/whatsNew/testSrc/com/intellij/platform/whatsNew/WhatsNewContentTest.kt
@@ -7,10 +7,10 @@ class WhatsNewContentTest {
 
   @Test
   fun `Content version comparison`() {
-    val nonEap = WhatsNewContent.ContentVersion(2024, 1, eap = null, hash = null)
-    val eap1 = WhatsNewContent.ContentVersion(2024, 1, eap = 1, hash = null)
-    val eap2 = WhatsNewContent.ContentVersion(2024, 1, eap = 2, hash = null)
-    val build242 = WhatsNewContent.ContentVersion(2024, 2, eap = null, hash = null)
+    val nonEap = WhatsNewContent.ContentVersion("2024", "1", eap = null, hash = null)
+    val eap1 = WhatsNewContent.ContentVersion("2024", "1", eap = 1, hash = null)
+    val eap2 = WhatsNewContent.ContentVersion("2024", "1", eap = 2, hash = null)
+    val build242 = WhatsNewContent.ContentVersion("2024", "2", eap = null, hash = null)
     Assert.assertTrue(nonEap > eap1)
     Assert.assertTrue(eap1 < nonEap)
     Assert.assertTrue(eap1 < eap2)
@@ -19,5 +19,13 @@ class WhatsNewContentTest {
     Assert.assertEquals(nonEap, nonEap)
     Assert.assertTrue(build242 > nonEap)
     Assert.assertTrue(build242 > eap1)
+
+    val release = WhatsNewContent.ContentVersion("2024", "1", eap = null, hash = null)
+    val bugfix = WhatsNewContent.ContentVersion("2024", "1.1", eap = null, hash = null)
+    val bigBugfix = WhatsNewContent.ContentVersion("2024", "10.1", eap = null, hash = null)
+    val smallBugfix = WhatsNewContent.ContentVersion("2024", "9.1", eap = null, hash = null)
+    Assert.assertTrue(bugfix > release)
+    Assert.assertTrue(bigBugfix > bugfix)
+    Assert.assertTrue(smallBugfix < bigBugfix)
   }
 }
