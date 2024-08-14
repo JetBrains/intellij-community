1e22ea21b75039b8faa533b9840260da0ef8ff15
Konstantin.Nisht <konstantin.nisht@jetbrains.com>
2024-08-14 18:23:51 +0200

    [ijent] IJPL-159766: Prepare mediator to implementation of Hyper-V sockets
    GitOrigin-RevId: ca1c16dc521620b4d85c3e30539963e6d9c19009

--- platform/ijent/src/com/intellij/platform/ijent/IjentProcessUtil.kt
+++ platform/ijent/src/com/intellij/platform/ijent/IjentProcessUtil.kt
@@ -5,6 +5,7 @@ package com.intellij.platform.ijent
 
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.util.containers.map2Array
 
 /**
@@ -26,15 +27,43 @@ fun getIjentGrpcArgv(
     else -> "info"
   }
 
+  val communicationChannel = when (IjentCommunicationChannelType.getCurrentChannelType()) {
+    IjentCommunicationChannelType.TCP_STDIO, IjentCommunicationChannelType.STDIO -> "stdio"
+    IjentCommunicationChannelType.HYPER_V -> "vsock"
+  }
+
   return listOfNotNull(
     usrBinEnv,
     *additionalEnv.entries.map2Array { (k, v) -> "$k=$v" },
     // "gdbserver", "0.0.0.0:12345",  // https://sourceware.org/gdb/onlinedocs/gdb/Connecting.html
     remotePathToIjent,
     "grpc-stdio-server",
+    "--communication-channel", communicationChannel,
     "--log-level", debuggingLogLevel,
     if (selfDeleteOnExit) "--self-delete-on-exit" else null,
   )
+}
+
+enum class IjentCommunicationChannelType {
+  TCP_STDIO,
+  STDIO,
+  HYPER_V;
+
+  companion object {
+    fun getCurrentChannelType(): IjentCommunicationChannelType {
+      val registryValue = Registry.get("ijent.communication.channel.type")
+      val selected = registryValue.selectedOption ?: registryValue.asString()
+      return when (selected.lowercase()) {
+        "tcp_stdio" -> TCP_STDIO
+        "stdio" -> STDIO
+        "hyperv" -> HYPER_V
+        else -> {
+          LOG.warn("Unexpected registry value for 'ijent.communication.channel.type': $selected. Using `TCP_STDIO` instead.")
+          TCP_STDIO
+        }
+      }
+    }
+  }
 }
 
 private val LOG = Logger.getInstance("com.intellij.platform.ijent.IjentProcessUtil")
