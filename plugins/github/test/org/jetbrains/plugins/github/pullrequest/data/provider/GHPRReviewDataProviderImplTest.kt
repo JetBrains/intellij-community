ff9954f40018de56da7ee46cfd47d0ba930b6768
Ilia Bogdanovich <ilia.bogdanovich@jetbrains.com>
2024-08-14 11:45:09 +0200

    [mockito.kotlin → mockk] Migrated intellij.vcs.github tests from mockito.kotlin to mockk
    GitOrigin-RevId: 32df0857285bf8d5dd7f4182bdc740eebfc2e46c

--- plugins/github/test/org/jetbrains/plugins/github/pullrequest/data/provider/GHPRReviewDataProviderImplTest.kt
+++ plugins/github/test/org/jetbrains/plugins/github/pullrequest/data/provider/GHPRReviewDataProviderImplTest.kt
@@ -4,6 +4,7 @@ package org.jetbrains.plugins.github.pullrequest.data.provider
 import com.intellij.collaboration.api.dto.GraphQLNodesDTO
 import com.intellij.collaboration.util.MainDispatcherRule
 import com.intellij.util.messages.MessageBus
+import io.mockk.*
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import org.jetbrains.plugins.github.api.data.pullrequest.GHPullRequestPendingReviewDTO
@@ -13,16 +14,10 @@ import org.jetbrains.plugins.github.api.data.pullrequest.GHPullRequestReviewThre
 import org.jetbrains.plugins.github.pullrequest.data.GHPRIdentifier
 import org.jetbrains.plugins.github.pullrequest.data.service.GHPRReviewService
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.ClassRule
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import org.mockito.Mock
-import org.mockito.junit.MockitoJUnit
-import org.mockito.junit.MockitoRule
-import org.mockito.kotlin.*
 
 private val PR_ID = GHPRIdentifier("id", 0)
 
@@ -36,74 +31,64 @@ class GHPRReviewDataProviderImplTest {
     internal val mainRule = MainDispatcherRule()
   }
 
-  @Rule
-  @JvmField
-  internal val mockitoRule: MockitoRule = MockitoJUnit.rule()
-
-  @Mock
-  internal lateinit var reviewService: GHPRReviewService
-
-  @Mock
-  internal lateinit var messageBus: MessageBus
-
-  @Mock
-  internal lateinit var listener: GHPRDataOperationsListener
+  private val reviewService = mockk<GHPRReviewService>(relaxUnitFun = true)
+  private val listener = mockk<GHPRDataOperationsListener>(relaxUnitFun = true)
+  private val messageBus = mockk<MessageBus> {
+    every { syncPublisher(GHPRDataOperationsListener.TOPIC) } returns listener
+  }
 
   private fun TestScope.createProvider(): GHPRReviewDataProvider =
-    GHPRReviewDataProviderImpl(backgroundScope, reviewService, mock(), PR_ID, messageBus)
+    GHPRReviewDataProviderImpl(backgroundScope, reviewService, mockk(), PR_ID, messageBus)
 
   private fun createPendingReview(id: String, comments: List<GHPullRequestReviewComment>): GHPullRequestPendingReviewDTO =
     GHPullRequestPendingReviewDTO(id, GHPullRequestReviewState.PENDING, GraphQLNodesDTO(comments, comments.size))
 
-  @Before
-  fun setUp() {
-    whenever(messageBus.syncPublisher(GHPRDataOperationsListener.TOPIC)) doReturn listener
-  }
-
   @Test
   fun testCachingReviewLoad() = runTest {
-    whenever(reviewService.loadPendingReview(PR_ID)) doReturn createPendingReview("", listOf(mock()))
+    coEvery { reviewService.loadPendingReview(eq(PR_ID)) } returns createPendingReview("", listOf(mockk()))
 
     val prv = createProvider()
     val result = prv.loadPendingReview()
     assertEquals(prv.loadPendingReview(), result)
 
-    verify(reviewService, times(1)).loadPendingReview(PR_ID)
+    coVerifyAll { reviewService.loadPendingReview(PR_ID) }
   }
 
   @Test
   fun testCreateAndSubmitReview() = runTest {
-    whenever(reviewService.loadPendingReview(eq(PR_ID))) doReturn null
-    whenever(reviewService.createReview(eq(PR_ID), anyOrNull(), anyOrNull(), anyOrNull(), anyOrNull())) doReturn
-      createPendingReview("", listOf(mock()))
+    coEvery { reviewService.loadPendingReview(eq(PR_ID)) } returns null
+    coEvery { reviewService.createReview(eq(PR_ID), any(), any(), any(), any()) } returns createPendingReview("", listOf(mockk()))
 
     val prv = createProvider()
-    prv.createReview(mock(), "test")
-    verify(reviewService, times(1)).createReview(eq(PR_ID), anyOrNull(), anyOrNull(), anyOrNull(), anyOrNull())
+    prv.createReview(mockk(), "test")
+    coVerify { reviewService.createReview(eq(PR_ID), any(), any(), any(), any()) }
 
     assertEquals(prv.loadPendingReview(), null)
-    verify(reviewService, times(1)).loadPendingReview(PR_ID)
+    coVerify { reviewService.loadPendingReview(PR_ID) }
+    confirmVerified(reviewService)
   }
 
   @Test
   fun testCreatePendingReviewImmediateUpdate() = runTest {
-    val result = createPendingReview("", listOf(mock()))
-    whenever(reviewService.createReview(eq(PR_ID), anyOrNull(), anyOrNull(), anyOrNull(), anyOrNull())) doReturn result
+    val result = createPendingReview("", listOf(mockk()))
+    coEvery { reviewService.createReview(eq(PR_ID), any(), any(), any(), any()) } returns result
 
     val prv = createProvider()
     prv.createReview(null, "test")
-    verify(reviewService, times(1)).createReview(eq(PR_ID), anyOrNull(), anyOrNull(), anyOrNull(), anyOrNull())
-    verify(listener, times(1)).onReviewsChanged()
+    coVerifyAll {
+      reviewService.createReview(eq(PR_ID), any(), any(), any(), any())
+      listener.onReviewsChanged()
+    }
 
     assertEquals(result.toModel(), prv.loadPendingReview())
-    verify(reviewService, never()).loadPendingReview(PR_ID)
+    confirmVerified(reviewService)
   }
 
   @Test
   fun testDeleteReviewUpdatedImmediately() = runTest {
-    val reviewDto = createPendingReview("REVIEW", listOf(mock()))
+    val reviewDto = createPendingReview("REVIEW", listOf(mockk()))
 
-    whenever(reviewService.loadPendingReview(eq(PR_ID))) doReturn reviewDto
+    coEvery { reviewService.loadPendingReview(eq(PR_ID)) } returns reviewDto
 
     val prv = createProvider()
     // fill cache
@@ -111,9 +96,11 @@ class GHPRReviewDataProviderImplTest {
     prv.deleteReview(reviewDto.id)
     assertEquals(null, prv.loadPendingReview())
 
-    verify(reviewService, times(1)).loadPendingReview(eq(PR_ID))
-    verify(reviewService, times(1)).deleteReview(eq(PR_ID), eq(reviewDto.id))
-    verify(listener, times(1)).onReviewsChanged()
+    coVerifySequence {
+      reviewService.loadPendingReview(eq(PR_ID))
+      reviewService.deleteReview(eq(PR_ID), eq(reviewDto.id))
+      listener.onReviewsChanged()
+    }
   }
 
   @Test
@@ -121,24 +108,26 @@ class GHPRReviewDataProviderImplTest {
     val reviewId = "REVIEW"
     val text = "test"
 
-    whenever(reviewService.updateReviewBody(eq(reviewId), eq(text))) doReturn mock()
+    coEvery { reviewService.updateReviewBody(eq(reviewId), eq(text)) } returns mockk(relaxed = true)
 
     val prv = createProvider()
     prv.updateReviewBody(reviewId, text)
 
-    verify(reviewService, times(1)).updateReviewBody(eq(reviewId), eq(text))
-    verify(listener, times(1)).onReviewUpdated(eq(reviewId), eq(text))
+    coVerifySequence {
+      reviewService.updateReviewBody(eq(reviewId), eq(text))
+      listener.onReviewUpdated(eq(reviewId), eq(text))
+    }
   }
 
   @Test
   fun testCachingThreadsLoad() = runTest {
-    val result = mock<List<GHPullRequestReviewThread>>()
-    whenever(reviewService.loadReviewThreads(PR_ID)) doReturn result
+    val result = mockk<List<GHPullRequestReviewThread>>()
+    coEvery { reviewService.loadReviewThreads(PR_ID) } returns result
 
     val prv = createProvider()
     assertEquals(prv.loadThreads(), result)
     assertEquals(prv.loadThreads(), result)
 
-    verify(reviewService, times(1)).loadReviewThreads(PR_ID)
+    coVerifyAll { reviewService.loadReviewThreads(PR_ID) }
   }
 }
