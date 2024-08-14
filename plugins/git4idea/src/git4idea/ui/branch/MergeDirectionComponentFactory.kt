7ade5e01b7c0d7c0f82b4872e76fb16716037dca
Vedana.Voronina <vedana.voronina@jetbrains.com>
2024-08-14 14:58:15 +0000

    [collab/space] IDEA-357146: filter branch names instead of keeping the full list
    Merge-request: IJ-MR-142075
    Merged-by: Veda Voronina <Vedana.Voronina@jetbrains.com>
    
    GitOrigin-RevId: db84beb4deb170a519e1eff2d55812051ef40f35

--- plugins/git4idea/src/git4idea/ui/branch/MergeDirectionComponentFactory.kt
+++ plugins/git4idea/src/git4idea/ui/branch/MergeDirectionComponentFactory.kt
@@ -6,10 +6,7 @@ import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.popup.JBPopup
 import com.intellij.openapi.ui.popup.JBPopupFactory
-import com.intellij.ui.CollectionComboBoxModel
-import com.intellij.ui.ComboboxSpeedSearch
-import com.intellij.ui.MutableCollectionComboBoxModel
-import com.intellij.ui.SimpleListCellRenderer
+import com.intellij.ui.*
 import com.intellij.ui.components.ActionLink
 import com.intellij.ui.components.JBTextField
 import com.intellij.ui.dsl.builder.AlignX
@@ -167,9 +164,9 @@ class MergeDirectionComponentFactory<RepoMapping : GitRepositoryMappingData>(
                                     GitBundle.message("branch.direction.panel.head.repo.label"),
                                     ComboBox(repoModel).apply {
                                       renderer = SimpleListCellRenderer.create("") { it.repositoryPath }
-                                    }.also {
-                                      ComboboxSpeedSearch.installSpeedSearch(it, GitRepositoryMappingData::repositoryPath)
-                                    }) {
+                                      isSwingPopup = false
+                                    }
+      ) {
         applySelection(repoModel.selected, branchModel.selected)
       }
 
@@ -209,10 +206,9 @@ class MergeDirectionComponentFactory<RepoMapping : GitRepositoryMappingData>(
         row(GitBundle.message("branch.direction.panel.branch.label")) {
           branchComponent = comboBox(branchModel, SimpleListCellRenderer.create("", GitBranch::getName))
             .align(AlignX.FILL)
-            .also {
-              ComboboxSpeedSearch.installSpeedSearch(it.component, GitBranch::getName)
+            .component.apply {
+              isSwingPopup = false
             }
-            .component
         }
         row {
           button(GitBundle.message("branch.direction.panel.save.button")) {
