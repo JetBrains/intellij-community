81141ebd78d82e14c4658e966c2c89c7e7dfc406
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-08-14 17:35:37 +0200

    [rdct] import settings from the previous version when the IDE backend starts for the first time (GTW-7436)
    Implicit switching off config importing in remote dev mode is removed (it was added in 2e962a2b8851c6, and the reasons for that are unclear).
    ConfigImportHelper is changed to ensure that Application isn't accessed during importing in remote dev mode.
    Since Gateway first starts the backend with a headless command (cwmHostStatus), and importing doesn't happen while executing headless commands (see IJPL-160167), `merge-configs` is saved to `migrate.config` file to ensure that migration will be started when the backend runs after executing a headless command. Currently it's done only for commands related to remote dev to avoid changing the behavior of regular IDEs, later we can do this for all commands.
    
    
    (cherry picked from commit cf701b817ed041f11c1a2ea3059b83f1859a6029)
    
    IJ-CR-142633
    
    GitOrigin-RevId: 783c8356607ca37f9337fc0ea4c050ca1febec3a

--- platform/platform-impl/src/com/intellij/openapi/application/ConfigImportHelper.java
+++ platform/platform-impl/src/com/intellij/openapi/application/ConfigImportHelper.java
@@ -13,6 +13,7 @@ import com.intellij.ide.plugins.marketplace.MarketplacePluginDownloadService;
 import com.intellij.ide.startup.StartupActionScriptManager;
 import com.intellij.ide.startup.StartupActionScriptManager.ActionCommand;
 import com.intellij.ide.ui.laf.LookAndFeelThemeAdapterKt;
+import com.intellij.idea.AppMode;
 import com.intellij.openapi.application.migrations.AIAssistant241;
 import com.intellij.openapi.application.migrations.NotebooksMigration242;
 import com.intellij.openapi.application.migrations.PythonProMigration242;
@@ -225,6 +226,11 @@ public final class ConfigImportHelper {
         var configImportOptions = new ConfigImportOptions(log);
         configImportOptions.importSettings = settings;
         configImportOptions.mergeVmOptions = customMigrationOption instanceof CustomConfigMigrationOption.MergeConfigs;
+        
+        /* in remote dev host mode UI cannot be shown before `Application` is initialized because it replaces the standard `awt.toolkit` 
+           with com.intellij.platform.impl.toolkit.IdeToolkit which depends on `Application` */
+        configImportOptions.setHeadless(AppMode.isRemoteDevHost());
+        
         if (!guessedOldConfigDirs.fromSameProduct) {
           importScenarioStatistics = IMPORTED_FROM_OTHER_PRODUCT;
         }
@@ -1178,7 +1184,7 @@ public final class ConfigImportHelper {
     if (options.headless) {
       PluginDownloader.runSynchronouslyInBackground(() -> {
         ProgressIndicator progressIndicator =
-          options.headlessProgressIndicator == null ? new EmptyProgressIndicator() : options.headlessProgressIndicator;
+          options.headlessProgressIndicator == null ? new EmptyProgressIndicator(ModalityState.nonModal()) : options.headlessProgressIndicator;
         downloadUpdatesForIncompatiblePlugins(newPluginsDir, options, incompatiblePlugins, progressIndicator);
       });
     }
