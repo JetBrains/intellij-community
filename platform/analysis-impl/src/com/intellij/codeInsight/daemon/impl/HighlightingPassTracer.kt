6c5a0c1d157cbe1b5b929c02a0419f2ed30cc8b6
Max Medvedev <maxim.medvedev@jetbrains.com>
2024-08-15 15:58:15 +0200

    HighlightingPassTracer should not be internal. It's used in intellij.platform.lang.impl
    GitOrigin-RevId: 989df539c628f6a0b90c3e2b79155dad0da3f3aa

--- platform/analysis-impl/src/com/intellij/codeInsight/daemon/impl/HighlightingPassTracer.kt
+++ platform/analysis-impl/src/com/intellij/codeInsight/daemon/impl/HighlightingPassTracer.kt
@@ -1,15 +1,17 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.daemon.impl
 
 import com.intellij.platform.diagnostic.telemetry.IJTracer
 import com.intellij.platform.diagnostic.telemetry.Scope
 import com.intellij.platform.diagnostic.telemetry.TelemetryManager
 import io.opentelemetry.api.common.AttributeKey
+import org.jetbrains.annotations.ApiStatus.Internal
 
 /**
  * Telemetry constants for daemon passes
  */
-internal object HighlightingPassTracer {
+@Internal
+object HighlightingPassTracer {
   @JvmField
   val HIGHLIGHTING_PASS_TRACER: IJTracer = TelemetryManager.getTracer(Scope("HighlightingPasses"))
   @JvmField
