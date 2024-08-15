cc7767f308e8d14a65d6551b4311369fad018916
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2024-08-15 15:25:45 +0200

    [cleanup] comments, todos, and renaming (no functional changes)
    GitOrigin-RevId: 74fa2ddd933a6510b2580ff663dc680630a54a62

--- platform/util/src/com/intellij/util/indexing/InvertedIndex.java
+++ platform/util/src/com/intellij/util/indexing/InvertedIndex.java
@@ -10,17 +10,19 @@ import org.jetbrains.annotations.Nullable;
  * Base interface for the <a href="https://en.wikipedia.org/wiki/Search_engine_indexing#Inverted_indices">inverted indexes</a>.
  */
 public interface InvertedIndex<Key, Value, Input> {
-  @NotNull
-  ValueContainer<Value> getData(@NotNull Key key) throws StorageException;
+  @NotNull ValueContainer<Value> getData(@NotNull Key key) throws StorageException;
 
   /**
    * Maps input as the first stage and returns a computation that does actual index data structure update.
    * It may be used to separate long-running input mapping from writing data to disk.
    * Computable returns `true` if data has been saved without errors, otherwise - `false`.
    */
-  @NotNull
-  StorageUpdate mapInputAndPrepareUpdate(int inputId, @Nullable Input content);
+  @NotNull StorageUpdate mapInputAndPrepareUpdate(int inputId, @Nullable Input content);
 
+  /**
+   * Second part of {@link #mapInputAndPrepareUpdate(int, Object)}): input already parsed and mapped to InputData[Key,Value]
+   * and this method creates an object to apply parsed input to index storages.
+   */
   @NotNull StorageUpdate prepareUpdate(int inputId, @NotNull InputData<Key, Value> data);
 
   void flush() throws StorageException;
