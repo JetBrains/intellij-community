aa3924dc9594e9bbeeaf351bee6a24bc1803c589
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2024-08-16 17:46:03 +0200

    Re-enable new waiting for highlight by default in OpenFileCommand (AT-711)
    GitOrigin-RevId: 04a31b7e1568299ad4a71d1ddca64a9372b7a0f0

--- plugins/performanceTesting/commands-model/src/com/intellij/tools/ide/performanceTesting/commands/generalCommandChain.kt
+++ plugins/performanceTesting/commands-model/src/com/intellij/tools/ide/performanceTesting/commands/generalCommandChain.kt
@@ -76,7 +76,7 @@ fun <T : CommandChain> T.openFile(
   suppressErrors: Boolean = false,
   warmup: Boolean = false,
   disableCodeAnalysis: Boolean = false,
-  useWaitForCodeAnalysisCode: Boolean = false,//todo[lene] temporal revert till 16.08.2024, because new analysis breaks reporting span to IJ-Perf
+  useWaitForCodeAnalysisCode: Boolean = true,
 ): T = apply {
   val command = mutableListOf("${CMD_PREFIX}openFile", "-file ${relativePath.replace(" ", "SPACE_SYMBOL")}")
   if (timeoutInSeconds != 0L) {
