9272e8b558afa289922287ecf643763ef0804c4b
Aleksei.Cherepanov <aleksei.cherepanov@jetbrains.com>
2024-08-16 16:14:19 +0200

    [Kotlin] Simplify JVM builder for enabling parallel model fetching
    1) Remove model parameter as far as the provider should accept only one model.
    2) Use another way of populating models for parallel model fetching
    
    Relates to KTIJ-28043
    
    ^KTIJ-29806 In Progress
    
    GitOrigin-RevId: 389b1302d478ccaf9a4ea6d7ee7f331bf26049b8

--- plugins/kotlin/gradle/gradle-tooling/impl/src/org/jetbrains/kotlin/idea/gradleTooling/KotlinGradleModelBuilder.kt
+++ plugins/kotlin/gradle/gradle-tooling/impl/src/org/jetbrains/kotlin/idea/gradleTooling/KotlinGradleModelBuilder.kt
@@ -5,6 +5,7 @@ package org.jetbrains.kotlin.idea.gradleTooling
 import com.intellij.gradle.toolingExtension.impl.model.dependencyDownloadPolicyModel.GradleDependencyDownloadPolicy
 import com.intellij.gradle.toolingExtension.impl.model.dependencyDownloadPolicyModel.GradleDependencyDownloadPolicyCache
 import com.intellij.gradle.toolingExtension.impl.model.dependencyModel.GradleDependencyResolver
+import com.intellij.gradle.toolingExtension.impl.util.GradleModelProviderUtil
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.artifacts.ConfigurationContainer
@@ -13,6 +14,7 @@ import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.provider.Property
 import org.gradle.tooling.BuildController
 import org.gradle.tooling.model.gradle.BasicGradleProject
+import org.gradle.tooling.model.gradle.GradleBuild
 import org.jetbrains.kotlin.idea.projectModel.KotlinTaskProperties
 import org.jetbrains.kotlin.tooling.core.Interner
 import org.jetbrains.plugins.gradle.model.ProjectImportModelProvider
@@ -94,25 +96,23 @@ abstract class AbstractKotlinGradleModelBuilder : ModelBuilderService {
 
 private const val REQUEST_FOR_NON_ANDROID_MODULES_ONLY = "*"
 
-class AndroidAwareGradleModelProvider<TModel>(
-    private val modelClass: Class<TModel>,
+class AndroidAwareGradleModelProvider(
     private val androidPluginIsRequestingVariantSpecificModels: Boolean
 ) : ProjectImportModelProvider {
-
-    override fun populateProjectModels(
-        controller: BuildController,
-        projectModel: BasicGradleProject,
-        modelConsumer: GradleModelConsumer
-    ) {
-        val model = if (androidPluginIsRequestingVariantSpecificModels) {
-            controller.findModel(projectModel, modelClass, ModelBuilderService.Parameter::class.java) {
+    private val modelClass = KotlinGradleModel::class.java
+    override fun populateBuildModels(controller: BuildController, buildModel: GradleBuild, modelConsumer: GradleModelConsumer) {
+        if (androidPluginIsRequestingVariantSpecificModels) {
+            GradleModelProviderUtil.buildModelsWithParameter(
+                controller,
+                buildModel,
+                modelClass,
+                modelConsumer,
+                ModelBuilderService.Parameter::class.java
+            ) {
                 it.value = REQUEST_FOR_NON_ANDROID_MODULES_ONLY
             }
         } else {
-            controller.findModel(projectModel, modelClass)
-        }
-        if (model != null) {
-            modelConsumer.consumeProjectModel(projectModel, model, modelClass)
+            GradleModelProviderUtil.buildModels(controller, buildModel, modelClass, modelConsumer)
         }
     }
 
