4a64d3c8194cf6c0f8079047ccd576b8e6838fcc
Evgenii Novozhilov <evgenii.novozhilov@jetbrains.com>
2024-08-19 13:28:27 +0300

    fus: move tests to the matching source package
    for some reason tests were a part of statistics.collector package
    while sources are in statistic.collectors.
    
    GitOrigin-RevId: 49495b456ad3759e8901420360cbcd2d2813539d

--- platform/platform-tests/testSrc/com/intellij/internal/statistic/collectors/fus/os/LinuxWindowManagerCollectorTest.kt
+++ platform/platform-tests/testSrc/com/intellij/internal/statistic/collectors/fus/os/LinuxWindowManagerCollectorTest.kt
@@ -1,7 +1,6 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.internal.statistics.collector
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.internal.statistic.collectors.fus.os
 
-import com.intellij.internal.statistic.collectors.fus.os.LinuxWindowManagerUsageCollector
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
