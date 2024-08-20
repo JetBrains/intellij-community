b127aa6fc02c5743b45ee036513a830d361fdf22
Henry Wylde <henry.wylde@jetbrains.com>
2024-08-21 10:45:48 +1200

    RUBY-32242 IJPL-159539 allow left/right click actions on declarative inlay hints to be invoked in dumb mode
    These actions do not require smart mode, and are only available/present for hints that are already dumb aware.
    
    GitOrigin-RevId: 64b0a072519b5b1dc99c6966148588c18deee405

--- platform/lang-impl/src/com/intellij/codeInsight/hints/declarative/impl/DisableDeclarativeInlayAction.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/hints/declarative/impl/DisableDeclarativeInlayAction.kt
@@ -8,8 +8,9 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.DumbAware
 
-internal class DisableDeclarativeInlayAction : AnAction(CodeInsightBundle.message("inlay.hints.declarative.disable.action.no.provider.text")) {
+internal class DisableDeclarativeInlayAction : AnAction(CodeInsightBundle.message("inlay.hints.declarative.disable.action.no.provider.text")), DumbAware {
   override fun getActionUpdateThread(): ActionUpdateThread {
     return ActionUpdateThread.BGT
   }
