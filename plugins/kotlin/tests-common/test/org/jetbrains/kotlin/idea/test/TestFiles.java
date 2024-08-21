827b587ff7e4ff303d2ac245a6b337487e6c8d25
Alexander Udalov <alexander.udalov@jetbrains.com>
2024-08-21 13:36:50 +0200

    [kotlin] Remove CodegenTestCase and related unused test infra
    GitOrigin-RevId: f9d6c19c9d4d5906dcf8a68098fd93ccea13f779

--- plugins/kotlin/tests-common/test/org/jetbrains/kotlin/idea/test/TestFiles.java
+++ plugins/kotlin/tests-common/test/org/jetbrains/kotlin/idea/test/TestFiles.java
@@ -5,13 +5,11 @@ package org.jetbrains.kotlin.idea.test;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.kotlin.idea.TestHelperGeneratorKt;
 
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static org.jetbrains.kotlin.idea.base.test.InTextDirectivesUtils.isDirectiveDefined;
 import static org.jetbrains.kotlin.idea.test.KotlinTestUtils.parseDirectives;
 
 public final class TestFiles {
@@ -29,28 +27,16 @@ public final class TestFiles {
     private static final Pattern MODULE_PATTERN = Pattern.compile("//\\s*MODULE:\\s*([^()\\r\\n]+)(?:\\(([^()]+(?:" + MODULE_DELIMITER + "[^()]+)*)\\))?\\s*(?:\\(([^()]+(?:" + MODULE_DELIMITER + "[^()]+)*)\\))?\\R");
     private static final Pattern FILE_PATTERN = Pattern.compile("//\\s*FILE:\\s*(.*)\\R");
 
-    private static final Pattern LINE_SEPARATOR_PATTERN = Pattern.compile("\\R");
-
     @NotNull
     public static <M extends KotlinBaseTest.TestModule, F> List<F> createTestFiles(
             @Nullable String testFileName,
             String expectedText,
             TestFileFactory<M, ? extends F> factory
     ) {
-        return createTestFiles(testFileName, expectedText, factory, false, false);
-    }
-
-    @NotNull
-    public static <M extends KotlinBaseTest.TestModule, F> List<F> createTestFiles(
-            String testFileName, String expectedText, TestFileFactory<M, ? extends F> factory,
-            boolean preserveLocations, boolean parseDirectivesPerFile
-    ) {
         Map<String, M> modules = new HashMap<>();
         List<F> testFiles = new ArrayList<>();
         Matcher fileMatcher = FILE_PATTERN.matcher(expectedText);
         Matcher moduleMatcher = MODULE_PATTERN.matcher(expectedText);
-        boolean hasModules = false;
-        String commonPrefixOrWholeFile;
 
         boolean fileFound = fileMatcher.find();
         boolean moduleFound = moduleMatcher.find();
@@ -58,21 +44,10 @@ public final class TestFiles {
             assert testFileName != null : "testFileName should not be null if no FILE directive defined";
             // One file
             testFiles.add(factory.createFile(null, testFileName, expectedText, parseDirectives(expectedText)));
-            commonPrefixOrWholeFile = expectedText;
         } else {
-            Directives allFilesOrCommonPrefixDirectives = parseDirectivesPerFile ? null : parseDirectives(expectedText);
+            Directives allFilesOrCommonPrefixDirectives = parseDirectives(expectedText);
             int processedChars = 0;
             M module = null;
-            boolean firstFileProcessed = false;
-
-            int commonStart;
-            if (moduleFound) {
-                commonStart = moduleMatcher.start();
-            } else {
-                commonStart = fileMatcher.start();
-            }
-
-            commonPrefixOrWholeFile = expectedText.substring(0, commonStart);
 
             // Many files
             while (true) {
@@ -82,7 +57,6 @@ public final class TestFiles {
                     String moduleFriends = moduleMatcher.group(3);
                     if (moduleName != null) {
                         moduleName = moduleName.trim();
-                        hasModules = true;
                         module = factory.createModule(moduleName, parseModuleList(moduleDependencies), parseModuleList(moduleFriends));
                         M oldValue = modules.put(moduleName, module);
                         assert oldValue == null : "Module with name " + moduleName + " already present in file";
@@ -106,42 +80,14 @@ public final class TestFiles {
                     else {
                         end = expectedText.length();
                     }
-                    String fileText = preserveLocations ?
-                                      substringKeepingLocations(expectedText, start, end) :
-                                      expectedText.substring(start, end);
-
-
-                    String expectedText1 = firstFileProcessed ? commonPrefixOrWholeFile + fileText : fileText;
-                    testFiles.add(factory.createFile(module, fileName, fileText,
-                                                     parseDirectivesPerFile ?
-                                                     parseDirectives(expectedText1)
-                                                                            : allFilesOrCommonPrefixDirectives));
+                    String fileText = expectedText.substring(start, end);
+                    testFiles.add(factory.createFile(module, fileName, fileText, allFilesOrCommonPrefixDirectives));
                     processedChars = end;
-                    firstFileProcessed = true;
                     if (!nextFileExists && !nextModuleExists) break;
                     if (nextModuleExists && fileMatcher.start() > moduleMatcher.start()) break;
                 }
                 if (!nextModuleExists) break;
             }
-        }
-
-        if (isDirectiveDefined(expectedText, "WITH_COROUTINES")) {
-            M supportModule = hasModules ? factory.createModule("support", Collections.emptyList(), Collections.emptyList()) : null;
-            if (supportModule != null) {
-                M oldValue = modules.put(supportModule.name, supportModule);
-                assert oldValue == null : "Module with name " + supportModule.name + " already present in file";
-            }
-
-            boolean checkStateMachine = isDirectiveDefined(expectedText, "CHECK_STATE_MACHINE");
-            boolean checkTailCallOptimization = isDirectiveDefined(expectedText, "CHECK_TAIL_CALL_OPTIMIZATION");
-
-            testFiles.add(
-                    factory.createFile(
-                            supportModule,
-                            "CoroutineUtil.kt",
-                            TestHelperGeneratorKt.createTextForCoroutineHelpers(checkStateMachine, checkTailCallOptimization),
-                            parseDirectives(commonPrefixOrWholeFile)
-                    ));
         }
 
         for (M module : modules.values()) {
@@ -162,26 +108,6 @@ public final class TestFiles {
 
 
         return testFiles;
-    }
-
-    private static String substringKeepingLocations(String string, int start, int end) {
-        Matcher matcher = LINE_SEPARATOR_PATTERN.matcher(string);
-        StringBuilder prefix = new StringBuilder();
-        int lastLineOffset = 0;
-        while (matcher.find()) {
-            if (matcher.end() > start) {
-                break;
-            }
-
-            lastLineOffset = matcher.end();
-            prefix.append('\n');
-        }
-
-        while (lastLineOffset++ < start) {
-            prefix.append(' ');
-        }
-
-        return prefix + string.substring(start, end);
     }
 
     private static List<String> parseModuleList(@Nullable String dependencies) {
