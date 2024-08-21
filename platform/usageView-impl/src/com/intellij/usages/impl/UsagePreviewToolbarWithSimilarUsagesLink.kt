9b73ef56dd8a5c8f18576f622f40de1bd10f65ea
Max Medvedev <maxim.medvedev@jetbrains.com>
2024-08-21 10:09:16 +0200

    IJPL-160202 cleanup UsagePreviewToolbarWithSimilarUsagesLink
    GitOrigin-RevId: cdeee73d9945eb2b82d19e71b576c66f23c4bba6

--- platform/usageView-impl/src/com/intellij/usages/impl/UsagePreviewToolbarWithSimilarUsagesLink.kt
+++ platform/usageView-impl/src/com/intellij/usages/impl/UsagePreviewToolbarWithSimilarUsagesLink.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.usages.impl
 
 import com.intellij.find.findUsages.similarity.SimilarUsagesComponent
@@ -15,81 +15,90 @@ import com.intellij.usages.UsageView
 import com.intellij.usages.similarity.clustering.ClusteringSearchSession
 import com.intellij.usages.similarity.clustering.UsageCluster
 import com.intellij.usages.similarity.statistics.SimilarUsagesCollector.logLinkToSimilarUsagesLinkFromUsagePreviewClicked
-import com.intellij.usages.similarity.usageAdapter.SimilarUsage
 import com.intellij.util.containers.ContainerUtil
 import com.intellij.util.ui.UIUtil
 import kotlinx.coroutines.*
 import org.jetbrains.annotations.ApiStatus.Internal
 import java.awt.BorderLayout
 import java.awt.FlowLayout
-import java.util.stream.Collectors
 import javax.swing.JPanel
 
 @Internal
-class UsagePreviewToolbarWithSimilarUsagesLink(previewPanel: UsagePreviewPanel,
-                                               private val myUsageView: UsageView,
-                                               selectedInfos: List<UsageInfo>,
-                                               cluster: UsageCluster,
-                                               session: ClusteringSearchSession) : JPanel(FlowLayout(FlowLayout.LEFT)), Disposable {
-  private var myCluster = cluster
-  private var anActionLink = createSimilarUsagesLink(previewPanel, selectedInfos)
-  private val myScope = CoroutineScope(Dispatchers.Default)
+class UsagePreviewToolbarWithSimilarUsagesLink(
+  private val previewPanel: UsagePreviewPanel,
+  private val usageView: UsageView,
+  private val infos: List<UsageInfo>,
+  private var cluster: UsageCluster,
+  session: ClusteringSearchSession,
+) : JPanel(FlowLayout(FlowLayout.LEFT)), Disposable {
+
+  private val scope = CoroutineScope(Dispatchers.Default)
+
+  private val showSimilarUsagesActionLink: AnActionLink = run {
+    val link = AnActionLink(
+      text = UsageViewBundle.message("similar.usages.show.0.similar.usages.title", cluster.usages.size - 1),
+      anAction = OpenSimilarUsagesAction()
+    )
+    link.isVisible = cluster.usages.size > 1
+    link
+  }
 
   init {
     background = UIUtil.getTextFieldBackground()
-    myCluster = cluster
-    add(anActionLink)
-    refreshLink(session, selectedInfos)
+    add(showSimilarUsagesActionLink)
+    refreshLink(session, infos)
   }
 
   private fun refreshLink(session: ClusteringSearchSession, selectedInfos: List<UsageInfo>) {
-    myScope.launch {
+    scope.launch {
       while (true) {
         delay(100)
-        myCluster = session.findCluster(ContainerUtil.getFirstItem(selectedInfos))!!
+        cluster = session.findCluster(ContainerUtil.getFirstItem(selectedInfos))!!
         withContext(Dispatchers.EDT) {
-          anActionLink.text = UsageViewBundle.message("similar.usages.show.0.similar.usages.title", myCluster.usages.size - 1)
+          showSimilarUsagesActionLink.text = UsageViewBundle.message("similar.usages.show.0.similar.usages.title", cluster.usages.size - 1)
         }
       }
     }
   }
 
-  private fun createSimilarUsagesLink(previewPanel: UsagePreviewPanel, infos: List<UsageInfo>): AnActionLink {
-    val similarUsagesLink = AnActionLink(UsageViewBundle.message("similar.usages.show.0.similar.usages.title", myCluster.usages.size - 1),
-                                         createOpenSimilarUsagesAction(previewPanel, infos))
-    similarUsagesLink.isVisible = myCluster.usages.size > 1
-    return similarUsagesLink
-  }
-
-  private fun createOpenSimilarUsagesAction(previewPanel: UsagePreviewPanel,
-                                            infos: List<UsageInfo>) = object : AnAction() {
+  private inner class OpenSimilarUsagesAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-      val onlyValidUsages = myCluster.usages.stream().filter { usage: SimilarUsage -> usage.isValid }.collect(
-        Collectors.toSet())
       previewPanel.removeAll()
       previewPanel.revalidate()
       previewPanel.releaseEditor()
+
       val firstSelectedInfo = ContainerUtil.getFirstItem(infos)!!
-      logLinkToSimilarUsagesLinkFromUsagePreviewClicked(firstSelectedInfo.project, myUsageView)
-      val similarUsagesComponent = SimilarUsagesComponent(myUsageView, firstSelectedInfo)
+      logLinkToSimilarUsagesLinkFromUsagePreviewClicked(firstSelectedInfo.project, usageView)
+
+      val similarUsagesComponent = SimilarUsagesComponent(usageView, firstSelectedInfo)
       Disposer.register(previewPanel, similarUsagesComponent)
-      previewPanel.add(SimilarUsagesToolbar(similarUsagesComponent,
-                                            UsageViewBundle.message("0.similar.usages",
-                                                                    onlyValidUsages.size - 1), null,
-                                            AnActionLink(UsageViewBundle.message(
-                                              "0.similar.usages.back.to.usage.preview",
-                                              UIUtil.leftArrow()), object : AnAction() {
-                                              override fun actionPerformed(e: AnActionEvent) {
-                                                previewPanel.removeAll()
-                                                Disposer.dispose(similarUsagesComponent)
-                                                previewPanel.updateLayout(firstSelectedInfo.project, infos, myUsageView)
-                                              }
-                                            })), BorderLayout.NORTH)
+
+      val onlyValidUsages = cluster.usages.filterTo(mutableSetOf()) { it.isValid }
+
+      val backActionLink = AnActionLink(
+        text = UsageViewBundle.message("0.similar.usages.back.to.usage.preview", UIUtil.leftArrow()),
+        anAction = object : AnAction() {
+          override fun actionPerformed(e: AnActionEvent) {
+            previewPanel.removeAll()
+            Disposer.dispose(similarUsagesComponent)
+            previewPanel.updateLayout(firstSelectedInfo.project, infos, usageView)
+          }
+        }
+      )
+
+      val similarUsagesToolbar = SimilarUsagesToolbar(
+        /* targetComponent = */ similarUsagesComponent,
+        /* text = */ UsageViewBundle.message("0.similar.usages", onlyValidUsages.size - 1),
+        /* refreshAction = */ null,
+        /* backActionLink = */ backActionLink
+      )
+
+      previewPanel.add(similarUsagesToolbar, BorderLayout.NORTH)
       previewPanel.add(similarUsagesComponent.createLazyLoadingScrollPane(onlyValidUsages))
     }
   }
 
   override fun dispose() {
-    myScope.cancel()
+    scope.cancel()
   }
 }
