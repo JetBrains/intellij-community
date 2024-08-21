8fbbe6604269de0de94e438dd86165f5ab242902
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2024-08-21 12:32:05 +0200

    Clean up analyzerStatus
    #KTIJ-27922 Fixed
    
    
    (cherry picked from commit 586331c784bf66fa0b03d59f77efdccaa5cdf5f9)
    
    IJ-CR-143042
    
    GitOrigin-RevId: 75133e7cf98821416ce18282716835b24f7071ae

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorMarkupModelImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorMarkupModelImpl.java
@@ -893,6 +893,8 @@ public final class EditorMarkupModelImpl extends MarkupModelImpl
     myTrafficLightPopup.hidePopup();
     extensionActions.clear();
 
+    analyzerStatus = AnalyzerStatus.getEMPTY();
+
     super.dispose();
   }
 
