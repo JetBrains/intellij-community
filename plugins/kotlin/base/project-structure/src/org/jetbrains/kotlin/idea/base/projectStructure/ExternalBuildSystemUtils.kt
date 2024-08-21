78c01ab98d3da84952f121fa92e542cbabb1809f
Roman Golyshev <roman.golyshev@jetbrains.com>
2024-08-21 10:40:42 +0200

    KTIJ-31045 [kotlin] Introduce `ModuleExternalDetailsCache` to compute `ModuleSourceRootMap` more efficiently
    ^KTIJ-31045 Fixed
    
    GitOrigin-RevId: b29b10eb52f31e982baaac9706303b5f43cadb32

--- plugins/kotlin/base/project-structure/src/org/jetbrains/kotlin/idea/base/projectStructure/ExternalBuildSystemUtils.kt
+++ plugins/kotlin/base/project-structure/src/org/jetbrains/kotlin/idea/base/projectStructure/ExternalBuildSystemUtils.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.idea.base.projectStructure
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.util.Ref
 
 val Module.externalProjectId: String?
     get() {
@@ -21,3 +22,38 @@ val Module.externalProjectPath: String?
 
         return ExternalSystemApiUtil.getExternalProjectPath(this)
     }
+
+/**
+ * A short-lived cache to avoid computing [externalProjectId] or [externalProjectPath] multiple times.
+ *
+ * Supposed to be used in a single read action.
+ * Not thread safe.
+ *
+ * Uses [Ref] to be able to cache `null` values in [projectIdCache] and [projectPathCache].
+ */
+internal class ModuleExternalDetailsCache {
+    private val projectIdCache: MutableMap<Module, Ref<String?>> = mutableMapOf()
+    private val projectPathCache: MutableMap<Module, Ref<String?>> = mutableMapOf()
+
+    fun getExternalProjectIdOrNull(module: Module): String? {
+        ProgressManager.checkCanceled()
+
+        return projectIdCache.getOrPut(module) {
+            Ref(module.externalProjectId)
+        }.get()
+    }
+
+    fun getExternalProjectId(module: Module): String =
+        getExternalProjectIdOrNull(module) ?: error("External project id not found for module $module")
+
+    fun getExternalProjectPathOrNull(module: Module): String? {
+        ProgressManager.checkCanceled()
+
+        return projectPathCache.getOrPut(module) {
+            Ref(module.externalProjectPath)
+        }.get()
+    }
+
+    fun getExternalProjectPath(module: Module): String =
+        getExternalProjectPathOrNull(module) ?: error("External project path not found for module $module")
+}
