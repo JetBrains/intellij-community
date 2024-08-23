a32670f6155c60c8125e6e97bc5edb29fc1d6422
Alexey Kudravtsev <cdr@intellij.com>
2024-08-23 14:49:09 +0200

    IJPL-160883 Apply ChangeLocalityDetector even if PSI reference or declaration is changed
    do not disable ChangeLocalityDetector produced scope when the reference was changed in languages other than Java/Kotlin, because the rules might be different there. Instead, move that logic inside the java/kotlin ChangeLocalityDetectors
    
    (cherry picked from commit 4aafa966186fe038c25e0a2512c80ba1b1d93eeb)
    
    
    (cherry picked from commit bf53a99ad2a53c42cb4025d5e0d2f734aa0424ac)
    
    IJ-CR-144246
    
    GitOrigin-RevId: 5a3517fcee8cd5d9784f45dbb56bad61473f591b

--- plugins/kotlin/base/fir/analysis-api-platform/src/org/jetbrains/kotlin/idea/base/fir/analysisApiPlatform/KotlinChangeLocalityDetector.kt
+++ plugins/kotlin/base/fir/analysis-api-platform/src/org/jetbrains/kotlin/idea/base/fir/analysisApiPlatform/KotlinChangeLocalityDetector.kt
@@ -2,12 +2,17 @@
 package org.jetbrains.kotlin.idea.base.fir.analysisApiPlatform
 
 import com.intellij.codeInsight.daemon.ChangeLocalityDetector
+import com.intellij.codeInsight.daemon.impl.HighlightingPsiUtil
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.analysis.low.level.api.fir.LLFirInternals
 import org.jetbrains.kotlin.analysis.low.level.api.fir.file.structure.LLFirDeclarationModificationService
 
 internal class KotlinChangeLocalityDetector : ChangeLocalityDetector {
     override fun getChangeHighlightingDirtyScopeFor(changedElement: PsiElement): PsiElement? {
+        if (HighlightingPsiUtil.hasReferenceInside(changedElement)) {
+            // turn off optimization when a reference was changed to avoid "unused symbol" false positives
+            return null
+        }
         // we shouldn't process comments here because the default detector will do that for us
 
         @OptIn(LLFirInternals::class)
