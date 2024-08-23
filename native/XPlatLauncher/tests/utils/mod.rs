c6eb8f105146e4fded5bc757f1250bab13e12aa6
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-08-23 10:02:08 +0200

    Cleanup (rearranging test methods; linter warnings; typos)
    GitOrigin-RevId: 2994970f6f993b313638701d5278eb7b96b93ecb

--- native/XPlatLauncher/tests/utils/mod.rs
+++ native/XPlatLauncher/tests/utils/mod.rs
@@ -14,7 +14,7 @@ use log::debug;
 use serde::{Deserialize, Serialize};
 use tempfile::{Builder, TempDir};
 
-use xplat_launcher::{DEBUG_MODE_ENV_VAR, get_config_home, PathExt};
+use xplat_launcher::{DEBUG_MODE_ENV_VAR, PathExt};
 
 static INIT: Once = Once::new();
 static mut SHARED: Option<TestEnvironmentShared> = None;
@@ -208,7 +208,7 @@ fn init_test_environment_once() -> Result<TestEnvironmentShared> {
     let product_info_path = project_root.join(format!("resources/product_info_{}.json", env::consts::OS));
     let vm_options_path = project_root.join("resources/xplat.vmoptions");
 
-    // on build agents, a temp directory may reside in a different filesystem, so copies are needed for later linking
+    // on build agents, a temp directory may reside in a different filesystem, so copies are necessary for later linking
     let temp_dir = Builder::new().prefix("xplat_launcher_shared_").tempdir().context("Failed to create temp directory")?;
     let launcher_path = temp_dir.path().join(launcher_file.file_name().unwrap());
     fs::copy(&launcher_file, &launcher_path).with_context(|| format!("Failed to copy {launcher_file:?} to {launcher_path:?}"))?;
@@ -422,14 +422,6 @@ fn symlink(original: &Path, link: &Path) -> Result<()> {
     result.with_context(|| format!("Failed to create symlink {link:?} -> {original:?}; {message}"))
 }
 
-pub fn get_custom_config_dir() -> PathBuf {
-    get_jetbrains_config_root().join("XPlatLauncherTest")
-}
-
-pub fn get_jetbrains_config_root() -> PathBuf {
-    get_config_home().unwrap().join("JetBrains")
-}
-
 pub struct LauncherRunSpec {
     location: LauncherLocation,
     dump: bool,
@@ -607,7 +599,7 @@ fn read_output_file(path: &Path) -> Result<String> {
     if let Ok(string) = String::from_utf8(bytes.to_owned()) {
         Ok(string)
     } else {
-        for line in bytes.split(|b| *b == '\n' as u8) {
+        for line in bytes.split(|b| *b == b'\n') {
             if let Err(e) = String::from_utf8(line.to_owned()) {
                 bail!("{}: {:?} {:?}", e, line, String::from_utf8_lossy(line))
             }
