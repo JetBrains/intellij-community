cf5ebf15144e882822e42cfe6f92948befc2eb34
Lev Leontev <lev.leontev@jetbrains.com>
2024-08-26 16:59:29 +0200

    [platform] Convert ModuleManagerEx to Kotlin
    GitOrigin-RevId: 40ea036c07b980a654071e62ea03ae27620ceaa5

--- platform/projectModel-impl/src/com/intellij/openapi/module/impl/ModuleManagerEx.kt
+++ platform/projectModel-impl/src/com/intellij/openapi/module/impl/ModuleManagerEx.kt
@@ -1,30 +1,32 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.module.impl;
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.module.impl
 
-import com.intellij.openapi.module.ModuleManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Pair;
-import com.intellij.platform.workspace.storage.MutableEntityStorage;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Collections;
-import java.util.List;
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Pair
+import com.intellij.platform.workspace.storage.MutableEntityStorage
+import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
-public abstract class ModuleManagerEx extends ModuleManager {
-  public static final String IML_EXTENSION = ".iml";
-  public static final String MODULE_GROUP_SEPARATOR = "/";
+abstract class ModuleManagerEx : ModuleManager() {
+  abstract fun areModulesLoaded(): Boolean
 
-  public abstract boolean areModulesLoaded();
-
-  public static ModuleManagerEx getInstanceEx(@NotNull Project project) {
-    return (ModuleManagerEx)getInstance(project);
+  open fun calculateUnloadModules(
+    builder: MutableEntityStorage,
+    unloadedEntityBuilder: MutableEntityStorage,
+  ): Pair<List<String>, List<String>> {
+    return Pair(emptyList<String>(), emptyList<String>())
   }
 
-  public Pair<List<String>, List<String>> calculateUnloadModules(@NotNull MutableEntityStorage builder, @NotNull MutableEntityStorage unloadedEntityBuilder) {
-    return new Pair<>(Collections.emptyList(), Collections.emptyList());
-  }
+  open fun updateUnloadedStorage(modulesToLoad: List<String>, modulesToUnload: List<String>) {}
 
-  public void updateUnloadedStorage(@NotNull List<String> modulesToLoad, @NotNull List<String> modulesToUnload) { }
+  companion object {
+    const val IML_EXTENSION: String = ".iml"
+    const val MODULE_GROUP_SEPARATOR: String = "/"
+
+    @JvmStatic
+    fun getInstanceEx(project: Project): ModuleManagerEx {
+      return getInstance(project) as ModuleManagerEx
+    }
+  }
 }
