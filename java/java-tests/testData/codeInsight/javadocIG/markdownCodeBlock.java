e1cd7dc276e863502620be1a79c23aa88d0c339b
Mathias Boulay <mathias.boulay@jetbrains.com>
2024-08-27 18:25:11 +0200

    fix(JavaDoc): better language highlight on codeblock
    (cherry picked from commit 2445935a3c083ecca24d899a300d02c08378f525)
    
    
    (cherry picked from commit a7ddd852bf38ff2480abd50da4e2497cfdce93fa)
    
    IJ-MR-144043
    
    GitOrigin-RevId: 8f9059ca5214c88608fb38c0c1d21ad2d9046f76

--- java/java-tests/testData/codeInsight/javadocIG/markdownCodeBlock.java
+++ java/java-tests/testData/codeInsight/javadocIG/markdownCodeBlock.java
@@ -1,7 +1,13 @@
 
-
-/// ```
+/// Java code block
+/// ```java
 /// new String();
 /// System.out.println("Hello Markdown");
+/// ```
+/// Xml code block
+/// ```xml
+/// <hello>
+///   <markdown />
+/// </hello>
 /// ```
 class MarkdownCodeBlock {}
