eb7a9fe34d05f478a1598c3f4ea6d4893e397cf4
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2024-08-27 14:04:52 +0200

    [gradle] fix flaky test IDEA-357790
    The previous test (ExternalAnnotationsDataServiceTest) sometimes disposes of the whole project. This should not happen.
    
    GitOrigin-RevId: 35125e51ffdfd59c147878b9dc512c184303fac2

--- plugins/gradle/java/testSources/service/project/data/ExternalAnnotationsDataServiceTest.kt
+++ plugins/gradle/java/testSources/service/project/data/ExternalAnnotationsDataServiceTest.kt
@@ -77,7 +77,7 @@ class ExternalAnnotationsDataServiceTest: LightIdeaTestCase() {
   override fun tearDown() {
     RunAll(
       ThrowableRunnable { modelsProvider.dispose() },
-      ThrowableRunnable { GradleSettings.getInstance(project).unlinkExternalProject(projectData.linkedExternalProjectPath) },
+      ThrowableRunnable { GradleSettings.getInstance(project).linkedProjectsSettings = listOf<GradleProjectSettings>() },
       ThrowableRunnable { super.tearDown() }
     ).run()
   }
