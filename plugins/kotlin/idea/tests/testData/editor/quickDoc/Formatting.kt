e60000ae85c4e2f2dcabd56281cbf0c3e2ad3276
Roman Golyshev <roman.golyshev@jetbrains.com>
2024-08-29 12:39:41 +0200

    KTIJ-29428 [kotlin] Replace absolute paths to Kotlin icons with symbolic ones
    It is now possible after IJPL-43721 was fixed
    
    ^KTIJ-29428 Fixed
    
    GitOrigin-RevId: e63a910e998b45fcb78209255401399d0a345166

--- plugins/kotlin/idea/tests/testData/editor/quickDoc/Formatting.kt
+++ plugins/kotlin/idea/tests/testData/editor/quickDoc/Formatting.kt
@@ -8,4 +8,4 @@ fun <caret>test() {}
 
 //INFO: <div class='definition'><pre><span style="color:#000080;font-weight:bold;">public</span> <span style="color:#000080;font-weight:bold;">fun</span> <span style="color:#000000;">test</span>()<span style="">: </span><span style="color:#000000;"><a href="psi_element://kotlin.Unit">Unit</a></span></pre></div><div class='content'><p style='margin-top:0;padding-top:0;'><em>foo</em> <strong>bar</strong> <del>baz</del></p>
 //INFO:   <blockquote><p>quote line  another quote line</p>
-//INFO: </blockquote></div><table class='sections'></table><div class='bottom'><icon src="/org/jetbrains/kotlin/idea/icons/kotlin_file.svg"/>&nbsp;Formatting.kt<br/></div>
+//INFO: </blockquote></div><table class='sections'></table><div class='bottom'><icon src="KotlinBaseResourcesIcons.Kotlin_file"/>&nbsp;Formatting.kt<br/></div>
--- plugins/kotlin/idea/tests/testData/editor/quickDoc/UnresolvedAnnotation.kt
+++ plugins/kotlin/idea/tests/testData/editor/quickDoc/UnresolvedAnnotation.kt
@@ -2,8 +2,8 @@
 class Anno<caret>tated
 
 //K2_INFO: <div class='definition'><pre><span style="color:#808000;">@</span><span style="border:1px solid;border-color:#ff0000;">`&lt;error&gt;`</span>
-//K2_INFO: <span style="color:#000080;font-weight:bold;">public</span> <span style="color:#000080;font-weight:bold;">final</span> <span style="color:#000080;font-weight:bold;">class</span> <span style="color:#000000;">Annotated</span></pre></div><div class='bottom'><icon src="/org/jetbrains/kotlin/idea/icons/kotlin_file.svg"/>&nbsp;UnresolvedAnnotation.kt<br/></div>
+//K2_INFO: <span style="color:#000080;font-weight:bold;">public</span> <span style="color:#000080;font-weight:bold;">final</span> <span style="color:#000080;font-weight:bold;">class</span> <span style="color:#000000;">Annotated</span></pre></div><div class='bottom'><icon src="KotlinBaseResourcesIcons.Kotlin_file"/>&nbsp;UnresolvedAnnotation.kt<br/></div>
 
 
 //INFO: <div class='definition'><pre><span style="color:#808000;">@</span><span style="color:#808000;">[Error type: Unresolved type for Unresolved]</span>
-//INFO: <span style="color:#000080;font-weight:bold;">public</span> <span style="color:#000080;font-weight:bold;">final</span> <span style="color:#000080;font-weight:bold;">class</span> <span style="color:#000000;">Annotated</span></pre></div><div class='bottom'><icon src="/org/jetbrains/kotlin/idea/icons/kotlin_file.svg"/>&nbsp;UnresolvedAnnotation.kt<br/></div>
+//INFO: <span style="color:#000080;font-weight:bold;">public</span> <span style="color:#000080;font-weight:bold;">final</span> <span style="color:#000080;font-weight:bold;">class</span> <span style="color:#000000;">Annotated</span></pre></div><div class='bottom'><icon src="KotlinBaseResourcesIcons.Kotlin_file"/>&nbsp;UnresolvedAnnotation.kt<br/></div>
