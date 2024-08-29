53f5b23473eae3d23f2035bd4f6d96945b966684
Edem Ibraimov <edem.ibraimov@jetbrains.com>
2024-08-29 11:18:34 +0300

    CPP-40400: WorkspaceBuilderChangeLog: use persistent collections instead of immutable
    (cherry picked from commit 5d8c98a6b165f8336c0126970a4fdc259430bc59)
    
    IJ-CR-144134
    
    GitOrigin-RevId: 40c95f09cf4719929da98d2c55c90c82eb8a9926

--- platform/workspace/storage/tests/testSrc/com/intellij/platform/workspace/storage/tests/propertyBased/PropertyTest.kt
+++ platform/workspace/storage/tests/testSrc/com/intellij/platform/workspace/storage/tests/propertyBased/PropertyTest.kt
@@ -11,6 +11,9 @@ import com.intellij.platform.workspace.storage.testEntities.entities.AnotherSour
 import com.intellij.platform.workspace.storage.testEntities.entities.MySource
 import com.intellij.platform.workspace.storage.tests.createBuilderFrom
 import com.intellij.platform.workspace.storage.tests.createEmptyBuilder
+import kotlinx.collections.immutable.persistentHashMapOf
+import kotlinx.collections.immutable.toPersistentHashSet
+import kotlinx.collections.immutable.toPersistentMap
 import org.jetbrains.jetCheck.Generator
 import org.jetbrains.jetCheck.ImperativeCommand
 import org.jetbrains.jetCheck.PropertyChecker
@@ -150,14 +153,14 @@ private class ApplyChangesFromCheckChangelog(val preBuilder: MutableEntityStorag
   private fun assertEntries(updatedChangelog: Map<EntityId, ChangeEntry>, actualChangelog: Map<EntityId, ChangeEntry>) {
     val left = updatedChangelog.mapValues { (k, v) ->
       if (v is ChangeEntry.ReplaceEntity) {
-        val newRefs = v.references?.copy(childrenOrdering = emptyMap()).takeUnless { it?.isEmpty() == true }
+        val newRefs = v.references?.copy(childrenOrdering = persistentHashMapOf()).takeUnless { it?.isEmpty() == true }
         v.copy(references = newRefs).takeUnless { it.data == null && it.references == null }
       }
       else v
     }.filterValues { it != null }
     val right = actualChangelog.mapValues { (k, v) ->
       if (v is ChangeEntry.ReplaceEntity) {
-        val references = v.references?.copy(childrenOrdering = emptyMap()).takeUnless { it?.isEmpty() == true }
+        val references = v.references?.copy(childrenOrdering = persistentHashMapOf()).takeUnless { it?.isEmpty() == true }
         v.copy(references = references).takeUnless { it.data == null && it.references == null }
       }
       else v
@@ -198,14 +201,14 @@ private class ApplyChangesFromCheckChangelog(val preBuilder: MutableEntityStorag
             }
             newEntry = newEntry.copy(
               references = entry.references?.copy(
-                newChildren = entry.references.newChildren.mapTo(HashSet()) {
+                newChildren = entry.references.newChildren.map {
                   it.copy(second = replaceMap[it.second.id.notThis()]?.id?.asChild() ?: it.second)
-                },
-                removedChildren = entry.references.removedChildren.mapTo(HashSet()) {
+                }.toPersistentHashSet(),
+                removedChildren = entry.references.removedChildren.map {
                   it.copy(second = replaceMap[it.second.id.notThis()]?.id?.asChild() ?: it.second)
-                },
-                newParents = entry.references.newParents.mapValues { (_, v) -> replaceMap[v.id.notThis()]?.id?.asParent() ?: v },
-                removedParents = entry.references.removedParents.mapValues { (_, v) -> replaceMap[v.id.notThis()]?.id?.asParent() ?: v },
+                }.toPersistentHashSet(),
+                newParents = entry.references.newParents.mapValues { (_, v) -> replaceMap[v.id.notThis()]?.id?.asParent() ?: v }.toPersistentMap(),
+                removedParents = entry.references.removedParents.mapValues { (_, v) -> replaceMap[v.id.notThis()]?.id?.asParent() ?: v }.toPersistentMap(),
               )
             )
             put(newId, newEntry)
