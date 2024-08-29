3903d047c23106f5ceafe8e41f7e03309ad23346
Alexander Kuznetsov <aleksander.kuznetsov@jetbrains.com>
2024-08-29 15:53:42 +0200

    Revert "[markdown] provide JCEF-like styling"
    I forgot to link an issue :(
    
    This reverts commit 2fea90d28ce56c551ee3c1bee36ae98aaea6b80d.
    
    GitOrigin-RevId: e7e316d326d26d5961da41bdd4ef64e1d8213e2a

--- plugins/markdown/compose/src/main/kotlin/com/intellij/markdown/compose/preview/MarkdownComposePanel.kt
+++ plugins/markdown/compose/src/main/kotlin/com/intellij/markdown/compose/preview/MarkdownComposePanel.kt
@@ -1,31 +1,30 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.markdown.compose.preview
 
-import androidx.compose.foundation.*
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.intellij.ide.BrowserUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.UserDataHolder
 import com.intellij.openapi.util.UserDataHolderBase
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.compose.JBComposePanel
-import org.intellij.plugins.markdown.compose.preview.JcefLikeMarkdownStyling
 import org.intellij.plugins.markdown.ui.preview.MarkdownHtmlPanel
 import org.intellij.plugins.markdown.ui.preview.MarkdownHtmlPanelEx
 import org.intellij.plugins.markdown.ui.preview.MarkdownUpdateHandler
 import org.intellij.plugins.markdown.ui.preview.MarkdownUpdateHandler.PreviewRequest
-import org.intellij.plugins.markdown.ui.preview.PreviewStyleScheme
 import org.jetbrains.annotations.ApiStatus
-import org.jetbrains.jewel.bridge.toComposeColor
 import org.jetbrains.jewel.foundation.ExperimentalJewelApi
 import org.jetbrains.jewel.intui.markdown.bridge.ProvideMarkdownStyling
 import org.jetbrains.jewel.markdown.Markdown
@@ -44,31 +43,23 @@ class MarkdownComposePanel(
   private val panelComponent by lazy {
     JBComposePanel {
       // TODO temporary styling, we will likely need our own in the future for JCEF-like rendering
-      MarkdownPanel()
+      ProvideMarkdownStyling {
+        MarkdownPanel()
+      }
     }
   }
 
   @Suppress("FunctionName")
   @Composable
   private fun MarkdownPanel() {
-    val scheme = PreviewStyleScheme.fromCurrentTheme()
-    val fontSize = scheme.fontSize.sp / scheme.scale
-    ProvideMarkdownStyling(
-      markdownStyling = JcefLikeMarkdownStyling(scheme, fontSize),
-    ) {
-      Box(
+    Box {
+      val scrollState = rememberScrollState(0)
+      MarkdownPreviewPanel(scrollState)
+      VerticalScrollbar(
         modifier = Modifier
-          .background(scheme.backgroundColor.toComposeColor())
-          .padding(horizontal = fontSize.value.dp * 2)
-      ) {
-        val scrollState = rememberScrollState(0)
-        MarkdownPreviewPanel(scrollState)
-        VerticalScrollbar(
-          modifier = Modifier
-            .align(Alignment.CenterEnd),
-          adapter = rememberScrollbarAdapter(scrollState),
-        )
-      }
+          .align(Alignment.CenterEnd),
+        adapter = rememberScrollbarAdapter(scrollState),
+      )
     }
   }
 
@@ -81,9 +72,9 @@ class MarkdownComposePanel(
         it.content,
         modifier = Modifier
           .fillMaxWidth()
+          .padding(8.dp)
           .verticalScroll(scrollState),
         enabled = true,
-        selectable = true,
         onUrlClick = { url -> BrowserUtil.open(url) },
       )
     }
