7a0749026dadc13d1e34fd8a0aa7b413f2db8cd4
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2024-08-29 20:45:33 +0200

    IJPL-797 `intellij.platform.lang.core` review internal API
    GitOrigin-RevId: ae89a53cc7daee4a17a0b1c75cb0c0d52732c3ad

--- platform/lang-core/src/com/intellij/openapi/module/InternalModuleType.java
+++ platform/lang-core/src/com/intellij/openapi/module/InternalModuleType.java
@@ -1,13 +1,15 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.module;
 
 import com.intellij.ide.util.projectWizard.ModuleBuilder;
+import org.jetbrains.annotations.ApiStatus.Internal;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author gregsh
  */
+@Internal
 public abstract class InternalModuleType<T extends ModuleBuilder> extends ModuleType<T> {
   protected InternalModuleType(@NotNull @NonNls String id) {
     super(id);
