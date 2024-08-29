cf76e7437acf0d12ff410e88fa26e2cf09ccfb5f
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2024-08-29 12:40:50 +0200

    [external system api][IDEA-349510] added a way to pass user data into ExternalSystemExecutionSettings
    GitOrigin-RevId: a127d9836e052649b2f13ff1f28eee8e842913ac

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/internal/AbstractExternalSystemTask.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/internal/AbstractExternalSystemTask.java
@@ -14,9 +14,7 @@ import com.intellij.openapi.externalSystem.util.ExternalSystemBundle;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.NlsContexts;
-import com.intellij.openapi.util.ThrowableComputable;
-import com.intellij.openapi.util.UserDataHolderBase;
+import com.intellij.openapi.util.*;
 import com.intellij.util.ArrayUtil;
 import com.intellij.util.ThrowableRunnable;
 import org.jetbrains.annotations.ApiStatus;
@@ -280,6 +278,18 @@ public abstract class AbstractExternalSystemTask extends UserDataHolderBase impl
   @Override
   public String toString() {
     return String.format("%s task %s: %s", myExternalSystemId.getReadableName(), myId, myState);
+  }
+
+  /**
+   * @see com.intellij.openapi.util.UserDataHolderBase#copyUserDataTo
+   */
+  @ApiStatus.Internal
+  @SuppressWarnings({"unchecked", "rawtypes"})
+  protected void putUserDataTo(@NotNull UserDataHolder dataHolder) {
+    var userMap = getUserMap();
+    for (Key key : userMap.getKeys()) {
+      dataHolder.putUserData(key, userMap.get(key));
+    }
   }
 
   @ApiStatus.Internal
