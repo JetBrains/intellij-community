9e11317d6c364303a1bf7f9254d4f005e24b16b0
Max Medvedev <maxim.medvedev@jetbrains.com>
2024-09-01 14:08:58 +0200

    some docs for inlay implementation
    GitOrigin-RevId: a7f1b4413a57d79438e580cd52844fdd860b7d52

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/BlockInlayImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/BlockInlayImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.editor.impl;
 
 import com.intellij.diagnostic.PluginException;
@@ -14,6 +14,9 @@ import java.awt.*;
 import java.util.List;
 import java.util.function.IntSupplier;
 
+/**
+ * @see com.intellij.openapi.editor.InlayModel#addBlockElement(int, boolean, boolean, int, com.intellij.openapi.editor.EditorCustomElementRenderer)
+ */
 final class BlockInlayImpl<R extends EditorCustomElementRenderer> extends InlayImpl<R, BlockInlayImpl<?>> implements IntSupplier {
   final boolean myShowAbove;
   final boolean myShowWhenFolded;
