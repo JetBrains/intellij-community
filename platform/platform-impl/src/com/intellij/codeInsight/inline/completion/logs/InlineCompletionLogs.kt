45a278d84e9f3ad739dc7bed7ccc681b1d9e2130
Daniil.Bubnov <daniil.bubnov@jetbrains.com>
2024-09-02 17:59:33 +0200

    [ai-completion] ML-3925 Add `first_proposal_len_generated`, `first_proposal_lines_generated` and `lines` (on analyzed)
    (cherry picked from commit b11e51f7f65a6bb25f0a16b51dc4e2bc3fc2e7c9)
    
    IJ-CR-143840
    
    GitOrigin-RevId: 0e3fd50716a55b41730a03a052b3cf5b01a77993

--- platform/platform-impl/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionLogs.kt
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionLogs.kt
@@ -14,7 +14,7 @@ import org.jetbrains.annotations.ApiStatus
 @ApiStatus.Internal
 internal object InlineCompletionLogs : CounterUsagesCollector() {
   // TODO use ML_RECORDER_ID
-  val GROUP = EventLogGroup("inline.completion.v2", 9, recorder = "ML")
+  val GROUP = EventLogGroup("inline.completion.v2", 10, recorder = "ML")
 
   override fun getGroup(): EventLogGroup = GROUP
 
