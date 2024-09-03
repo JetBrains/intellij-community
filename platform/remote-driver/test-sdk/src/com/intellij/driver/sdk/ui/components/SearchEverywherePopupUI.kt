fa224007f79d87db55570bebc11f2ea025d2c77e
vladislav.shishov <vladislav.shishov@jetbrains.com>
2024-09-03 23:22:02 +0300

    [remote-driver] add SearchEveryWhereUiTest#testOpenInFindToolWindow
    GitOrigin-RevId: 0eaf52343cd8a1d670b44d2954366c6c5f8dee8f

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/SearchEverywherePopupUI.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/SearchEverywherePopupUI.kt
@@ -3,6 +3,7 @@ package com.intellij.driver.sdk.ui.components
 import com.intellij.driver.sdk.ui.Finder
 import com.intellij.driver.sdk.ui.pasteText
 import com.intellij.driver.sdk.ui.should
+import com.intellij.driver.sdk.ui.xQuery
 import com.intellij.driver.sdk.ui.ui
 import org.intellij.lang.annotations.Language
 import kotlin.time.Duration.Companion.seconds
@@ -13,6 +14,8 @@ fun Finder.searchEverywherePopup(@Language("xpath") xpath: String? = null) = x(x
 
 class SearchEverywherePopupUI(data: ComponentData): PopupUiComponent(data) {
   val resultsList = x("//div[@class='JBList']", JListUiComponent::class.java)
+  val searchField: JTextFieldUI = textField { byType("com.intellij.ide.actions.BigPopupUI${"$"}SearchField") }
+  val openInFindToolWindowButton: ActionButtonUi = actionButtonByXpath(xQuery { byAccessibleName("Open in Find Tool Window") })
 
   fun searchAndChooseFirst(text: String, exactMatch: Boolean = true) {
     keyboard {
