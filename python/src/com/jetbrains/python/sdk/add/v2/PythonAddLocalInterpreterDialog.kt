78abf73b2cd759cfbf1f46765572cc79d52225b5
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2024-09-03 22:14:56 +0200

    Python NPW: Show add local dialog directly in `DialogWrapper`, fail validation if the interpreter list is empty
    1. `DialogPanel` in `DialogWrapper` supports automatic validation, no need to do that manually.
    
    2. Validate the list of interpreters and fail validation if they weren't loaded
    
    GitOrigin-RevId: a450336ee397a1ad7b1ca9b8f2a2e54d0210def2

--- python/src/com/jetbrains/python/sdk/add/v2/PythonAddLocalInterpreterDialog.kt
+++ python/src/com/jetbrains/python/sdk/add/v2/PythonAddLocalInterpreterDialog.kt
@@ -9,73 +9,54 @@ import com.intellij.openapi.observable.properties.AtomicProperty
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.validation.WHEN_PROPERTY_CHANGED
 import com.intellij.platform.ide.progress.runWithModalProgressBlocking
-import com.intellij.ui.AncestorListenerAdapter
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.util.concurrency.annotations.RequiresEdt
 import com.jetbrains.python.PyBundle
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
-import java.awt.Dimension
-import javax.swing.BoxLayout
 import javax.swing.JComponent
-import javax.swing.JPanel
-import javax.swing.event.AncestorEvent
 
 
 /**
  * @see PythonAddLocalInterpreterPresenter
  */
 class PythonAddLocalInterpreterDialog(private val dialogPresenter: PythonAddLocalInterpreterPresenter) : DialogWrapper(dialogPresenter.moduleOrProject.project) {
-  val outerPanel = JPanel().apply {
-    layout = BoxLayout(this, BoxLayout.X_AXIS)
-    preferredSize = Dimension(500, 250) // todo scale dimensions
-  }
 
-  private lateinit var model: PythonLocalAddInterpreterModel
   private lateinit var mainPanel: PythonAddCustomInterpreter
+  private lateinit var model: PythonLocalAddInterpreterModel
+
+  private val basePath = dialogPresenter.pathForVEnv.toString()
 
   init {
     title = PyBundle.message("python.sdk.add.python.interpreter.title")
     isResizable = true
     init()
-
-    outerPanel.addAncestorListener(object : AncestorListenerAdapter() {
-      override fun ancestorAdded(event: AncestorEvent?) {
-        val basePath = dialogPresenter.pathForVEnv.toString()
-        model = PythonLocalAddInterpreterModel(PyInterpreterModelParams(service<PythonAddSdkService>().coroutineScope,
-                                               Dispatchers.EDT + ModalityState.current().asContextElement(), MutableStateFlow(basePath)))
-        model.navigator.selectionMode = AtomicProperty(PythonInterpreterSelectionMode.CUSTOM)
-        mainPanel = PythonAddCustomInterpreter(model)
-
-        val dialogPanel = panel {
-          mainPanel.buildPanel(this, WHEN_PROPERTY_CHANGED(AtomicProperty(basePath)))
-        }
-
-        dialogPanel.registerValidators(myDisposable) { validations ->
-          val anyErrors = validations.entries.any { (key, value) -> key.isVisible && !value.okEnabled }
-          isOKActionEnabled = !anyErrors
-        }
-
-        outerPanel.add(dialogPanel)
-
-        model.scope.launch(Dispatchers.EDT + ModalityState.current().asContextElement()) {
-          model.initialize()
-          mainPanel.onShown()
-        }
-      }
-    })
-
   }
 
 
   @RequiresEdt
   override fun doOKAction() {
     super.doOKAction()
+    val addEnvironment = mainPanel.currentSdkManager
     runWithModalProgressBlocking(dialogPresenter.moduleOrProject.project, "...") {
-      dialogPresenter.okClicked(mainPanel.currentSdkManager)
+      dialogPresenter.okClicked(addEnvironment)
     }
   }
 
-  override fun createCenterPanel(): JComponent = outerPanel
+  override fun createCenterPanel(): JComponent = panel {
+    model = PythonLocalAddInterpreterModel(PyInterpreterModelParams(service<PythonAddSdkService>().coroutineScope,
+                                                                    // At this moment dialog is not displayed, so there is no modality state
+                                                                    // The whole idea of context passing is doubtful
+                                                                    Dispatchers.EDT + ModalityState.any().asContextElement(), MutableStateFlow(basePath)))
+    model.navigator.selectionMode = AtomicProperty(PythonInterpreterSelectionMode.CUSTOM)
+    mainPanel = PythonAddCustomInterpreter(model)
+    mainPanel.buildPanel(this, WHEN_PROPERTY_CHANGED(AtomicProperty(basePath)))
+
+  }.apply {
+    model.scope.launch(model.uiContext) {
+      model.initialize()
+      mainPanel.onShown()
+    }
+  }
 }
