bf767ab88bdd62104970e84c9d52490e51329d09
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2024-09-03 13:43:59 +0200

    [kotlin] Polish test data example
    #KTIJ-31102
    
    GitOrigin-RevId: a2de8c650f219e1beae5d9481d72c83a6adcc59c

--- plugins/kotlin/idea/tests/testData/codeInsight/hints/types/PrivateAnonymousObject.k2.kt
+++ plugins/kotlin/idea/tests/testData/codeInsight/hints/types/PrivateAnonymousObject.k2.kt
@@ -1,7 +1,6 @@
 // MODE: all
-private val list/*<# : |[kotlin.collections.List:kotlin.fqn.class]List|<|<anonymous>|> #>*/ = listOf(object : Iterable<Int> {
-        override fun iterator()/*<# : |[kotlin.collections.Iterator:kotlin.fqn.class]Iterator|<|[kotlin.Int:kotlin.fqn.class]Int|> #>*/ = object : Iterator<Int> {
-        override fun next()/*<# : |[kotlin.Int:kotlin.fqn.class]Int #>*/ = 1
-        override fun hasNext()/*<# : |[kotlin.Boolean:kotlin.fqn.class]Boolean #>*/ = true
-    })
-}
+interface MyInterface
+
+private val privateAnonymousObjects/*<# : |[kotlin.collections.List:kotlin.fqn.class]List|<|<anonymous>|> #>*/ = listOf(object : MyInterface{})
+
+val publicAnonymousObjects/*<# : |[kotlin.collections.List:kotlin.fqn.class]List|<|[MyInterface:kotlin.fqn.class]MyInterface|> #>*/ = listOf(object : MyInterface{})
--- plugins/kotlin/idea/tests/testData/codeInsight/hints/types/PrivateAnonymousObject.kt
+++ plugins/kotlin/idea/tests/testData/codeInsight/hints/types/PrivateAnonymousObject.kt
@@ -1,7 +1,6 @@
 // MODE: all
-private val list/*<# : |[kotlin.collections.List:kotlin.fqn.class]List|<|[list.<no name provided>:kotlin.fqn.class]`<no name provided>`|> #>*/ = listOf(object : Iterable<Int> {
-        override fun iterator()/*<# : |[kotlin.collections.Iterator:kotlin.fqn.class]Iterator|<|[kotlin.Int:kotlin.fqn.class]Int|> #>*/ = object : Iterator<Int> {
-        override fun next()/*<# : |[kotlin.Int:kotlin.fqn.class]Int #>*/ = 1
-        override fun hasNext()/*<# : |[kotlin.Boolean:kotlin.fqn.class]Boolean #>*/ = true
-    })
-}
+interface MyInterface
+
+private val privateAnonymousObjects/*<# : |[kotlin.collections.List:kotlin.fqn.class]List|<|[privateAnonymousObjects.<no name provided>:kotlin.fqn.class]`<no name provided>`|> #>*/ = listOf(object : MyInterface{})
+
+val publicAnonymousObjects/*<# : |[kotlin.collections.List:kotlin.fqn.class]List|<|[publicAnonymousObjects.<no name provided>:kotlin.fqn.class]`<no name provided>`|> #>*/ = listOf(object : MyInterface{})
