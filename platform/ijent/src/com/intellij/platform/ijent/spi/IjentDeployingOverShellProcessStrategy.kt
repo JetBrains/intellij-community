b889c00ed46cd6729fe59ed2a594e0b245db9313
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2024-09-04 19:58:47 +0200

    IJPL-161095 IJent: message about absent busybox doesn't hide real error anymore
    GitOrigin-RevId: c42bbf3457a3b604973bc735c1e203bcf48a4da7

--- platform/ijent/src/com/intellij/platform/ijent/spi/IjentDeployingOverShellProcessStrategy.kt
+++ platform/ijent/src/com/intellij/platform/ijent/spi/IjentDeployingOverShellProcessStrategy.kt
@@ -216,7 +216,7 @@ private suspend fun createDeployingContext(
 
 @VisibleForTesting
 internal suspend fun createDeployingContext(runWhichCmd: suspend (commands: Collection<String>) -> Collection<String>): DeployingContext {
-  var busybox: Lazy<String>? = null
+  var busybox: String? = null
 
   // This strange at first glance code helps reduce copy-paste errors.
   val commands: Set<String> = setOf(
@@ -240,8 +240,7 @@ internal suspend fun createDeployingContext(runWhichCmd: suspend (commands: Coll
       return directCandidate
     }
 
-    if (name != "busybox") {
-      val busybox = busybox!!.value // Throws an error.
+    if (name != "busybox" && busybox != null) {
       return "$busybox $name"
     }
 
@@ -250,7 +249,7 @@ internal suspend fun createDeployingContext(runWhichCmd: suspend (commands: Coll
 
   outputOfWhich += runWhichCmd(commands)
 
-  busybox = lazy { getCommandPath("busybox") }
+  busybox = outputOfWhich.firstOrNull { it.endsWith("/busybox") }
 
   return DeployingContext(
     chmod = getCommandPath("chmod"),
