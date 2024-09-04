65763178b7e2d0d48ee24601ad10d4fa8042aaf5
Kate Botsman <kate.botsman@jetbrains.com>
2024-09-04 15:32:58 +0200

    RDCT-1444 Add `TaskInfoEntityTypeProvider` to register `TaskInfoEntity`
    GitOrigin-RevId: 1805a3e4af65fee2327c05b6d752349f8f6d9679

--- platform/progress/shared/src/entities.kt
+++ platform/progress/shared/src/entities.kt
@@ -1,9 +1,11 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.ide.progress
 
+import com.intellij.platform.kernel.EntityTypeProvider
 import com.intellij.platform.util.progress.ProgressState
 import com.jetbrains.rhizomedb.EID
 import com.jetbrains.rhizomedb.Entity
+import com.jetbrains.rhizomedb.EntityType
 import fleet.kernel.DurableEntityType
 import kotlinx.serialization.builtins.serializer
 import org.jetbrains.annotations.ApiStatus
