6eedbf4ef00f2212c2f708a58c8053ded80aa00a
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2024-09-04 17:37:09 +0200

    Remove outdated comment
    GitOrigin-RevId: 0ca803d941e2a91ccdf8c0b89a1421bf8483b96b

--- platform/platform-impl/src/com/intellij/openapi/project/SmartModeScheduler.kt
+++ platform/platform-impl/src/com/intellij/openapi/project/SmartModeScheduler.kt
@@ -33,10 +33,6 @@ import java.util.function.Consumer
  * Scheduled runnables will be executed on EDT thread after all three conditions met:
  * 1. There is no scanning in progress, and initial project scanning (on project open) has finished
  * 2. There is no dumb mode
- * 3. All the [com.intellij.openapi.startup.StartupActivity] completed
- *
- * It is important to note that while dumb mode cannot start without write action, scanning can start without write action, so by the
- * moment when runnable executes scanning can run, but it is guaranteed that initial scanning on project open has finished.
  */
 @Internal
 @Service(Service.Level.PROJECT)
