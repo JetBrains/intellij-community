3cc4680f849e27532de7a722e5e05304f1966655
Anton Efimchuk <anton.efimchuk@jetbrains.com>
2024-09-05 11:36:38 +0200

    PY-75427 [Jupyter] Fix wrong cursor appearance over inlays
    GitOrigin-RevId: 4bda35004d1bc6a917c11a905f44ce71fdf45f36

--- notebooks/visualization/src/com/intellij/notebooks/visualization/ui/EditorEmbeddedComponentContainer.kt
+++ notebooks/visualization/src/com/intellij/notebooks/visualization/ui/EditorEmbeddedComponentContainer.kt
@@ -4,37 +4,26 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.util.Key
-import java.awt.event.ComponentAdapter
-import java.awt.event.ComponentEvent
-import javax.swing.JComponent
-import javax.swing.SwingUtilities
+import java.awt.Component
 
 private val COMPONENTS_CONTAINER = Key<EditorEmbeddedComponentContainer>("COMPONENTS_CONTAINER")
 
-internal class EditorEmbeddedComponentContainer(private val editor: EditorEx): JComponent(), Disposable {
-
-  private val contentResizeListener = object : ComponentAdapter() {
-    override fun componentResized(e: ComponentEvent?) {
-      invalidate()
-    }
-  }
+internal class EditorEmbeddedComponentContainer(private val editor: EditorEx) : Disposable {
 
   init {
-    editor.contentComponent.add(this)
-    editor.contentComponent.addComponentListener(contentResizeListener)
+    editor.contentComponent.layout = EditorEmbeddedComponentLayoutManager(editor.scrollPane)
     editor.putUserData(COMPONENTS_CONTAINER, this)
-    layout = EditorEmbeddedComponentLayoutManager(editor.scrollPane)
-    putClientProperty(SelectClickedCellEventHelper.SKIP_CLICK_PROCESSING_FOR_CELL_SELECTION, true)
   }
 
-  override fun doLayout() {
-    bounds = SwingUtilities.calculateInnerArea(editor.contentComponent, null)
-    super.doLayout()
+  fun add(component: Component, constraints: Any) {
+    editor.contentComponent.add(component, constraints)
+  }
+
+  fun remove(component: Component) {
+    editor.contentComponent.remove(component)
   }
 
   override fun dispose() {
-    editor.contentComponent.removeComponentListener(contentResizeListener)
-    editor.contentComponent.remove(this)
     editor.putUserData(COMPONENTS_CONTAINER, null)
   }
 
