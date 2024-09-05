55e67da3f3229b274daca0f0cc3ddc748968d067
Egor Ushakov <egor.ushakov@jetbrains.com>
2024-09-05 15:31:33 +0200

    [kotlin debugger] avoid reloading array classes
    GitOrigin-RevId: ebeda69d11a05c1a6b8c2b806d653fe4ea03297b

--- plugins/kotlin/jvm-debugger/eval4j/src/org/jetbrains/eval4j/jdi/jdiEval.kt
+++ plugins/kotlin/jvm-debugger/eval4j/src/org/jetbrains/eval4j/jdi/jdiEval.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.eval4j.jdi
 
@@ -40,7 +40,7 @@ open class JDIEval(
     }
 
     private fun loadClass(classType: Type, classLoader: ClassLoaderReference?): Value {
-        val loadedClasses = vm.classesByName(classType.jdiName)
+        val loadedClasses = vm.classesByName(classType.className)
         if (loadedClasses.isNotEmpty()) {
             for (loadedClass in loadedClasses) {
                 if (loadedClass.isPrepared && (classType.descriptor in BOOTSTRAP_CLASS_DESCRIPTORS || loadedClass.classLoader() == classLoader)) {
