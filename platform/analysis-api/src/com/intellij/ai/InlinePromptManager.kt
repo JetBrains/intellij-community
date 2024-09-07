3e33fd42da1af6550efb688af745199c21d078af
Max Medvedev <maxim.medvedev@jetbrains.com>
2024-09-07 14:32:43 +0200

    IJPL-161819 use isInlinePromptShown function instead of InlinePromptManager
    (cherry picked from commit a3bc608435b71b800bae1356ece71d58f108acfe)
    
    GitOrigin-RevId: c973a2eab31c04d9ab2ed661cc9fa1325b7cdc2b

--- platform/analysis-api/src/com/intellij/ai/InlinePromptManager.kt
+++ platform/analysis-api/src/com/intellij/ai/InlinePromptManager.kt
@@ -1,8 +1,10 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:JvmName("InlinePrompt")
 package com.intellij.ai
 
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.ApiStatus.Experimental
 
 /**
@@ -11,7 +13,7 @@ import org.jetbrains.annotations.ApiStatus.Experimental
  * When we detect it, we start an inline prompt editing mode.
  * When we edit inline prompt, programming language features should not interfere with the prompt
  */
-@Experimental
+@ApiStatus.Internal
 interface InlinePromptManager {
   companion object {
     fun getInstance(project: Project): InlinePromptManager = project.getService(InlinePromptManager::class.java)
@@ -24,4 +26,18 @@ interface InlinePromptManager {
    * @return true if the inline prompt is shown, false otherwise
    */
   fun isInlinePromptShown(editor: Editor): Boolean
+}
+
+/**
+ * Checks if the inline prompt is currently shown in the specified editor.
+ *
+ * @param editor the editor in which the inline prompt visibility is to be checked
+ * @param project the project associated with the editor, defaults to the project of the editor if not provided
+ * @return true if the inline prompt is shown, false otherwise
+ */
+@Experimental
+@JvmOverloads
+fun isInlinePromptShown(editor: Editor, project: Project? = editor.project): Boolean {
+  if (project == null) return false
+  return InlinePromptManager.getInstance(project).isInlinePromptShown(editor)
 }
