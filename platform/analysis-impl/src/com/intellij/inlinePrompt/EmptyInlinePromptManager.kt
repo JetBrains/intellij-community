3a64069f0e00c3fdecab9b0a9a12d22008317e07
Max Medvedev <maxim.medvedev@jetbrains.com>
2024-09-09 20:16:12 +0200

    IJPL-161819 move EmptyInlinePromptManager to inlinePrompt package
    GitOrigin-RevId: f33398206447c1bd69b2761c844f7d36e2039ed7

--- platform/analysis-impl/src/com/intellij/inlinePrompt/EmptyInlinePromptManager.kt
+++ platform/analysis-impl/src/com/intellij/inlinePrompt/EmptyInlinePromptManager.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ai
+package com.intellij.inlinePrompt
 
-import com.intellij.inlinePrompt.InlinePromptManager
 import com.intellij.openapi.editor.Editor
 import javax.swing.Icon
 
