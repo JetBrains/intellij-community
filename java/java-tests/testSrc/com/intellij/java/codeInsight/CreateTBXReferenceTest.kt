4269e27cb38253ad95f3b2acd4255559296e8f33
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-09-09 09:45:53 +0200

    [platform] rewriting `CopyTBXReferenceAction` to use a URL object instead of string concatenation (IJPL-149928)
    GitOrigin-RevId: 15a5a2c6e401228d51f083f3de656b3d392b740a

--- java/java-tests/testSrc/com/intellij/java/codeInsight/CreateTBXReferenceTest.kt
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/CreateTBXReferenceTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.java.codeInsight
 
 import com.intellij.JavaTestUtil
@@ -6,59 +6,64 @@ import com.intellij.ide.DataManager
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.impl.SimpleDataContext
 import com.intellij.openapi.ide.CopyPasteManager
+import com.intellij.psi.PsiElement
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import org.assertj.core.api.Assertions.assertThat
 import java.awt.datatransfer.DataFlavor
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 
 class CreateTBXReferenceTest : BasePlatformTestCase() {
-  override fun getTestDataPath(): String {
-    return "${JavaTestUtil.getJavaTestDataPath()}/codeInsight/copyTBXReference/"
-  }
+  override fun getTestDataPath(): String = "${JavaTestUtil.getJavaTestDataPath()}/codeInsight/copyTBXReference/"
 
-  fun testMethodReference() {
+  fun testMethodReference() =
     doTest("fqn=C2#C2")
-  }
 
-  fun testSelection() {
+  fun testSelection() =
     doTest("fqn=X&selection=3:3-7:3")
-  }
 
-  fun testMultipleSelections() {
+  fun testMultipleSelections() =
     doTest("path=MultipleSelections.java:11:5&selection1=2:5-4:5&selection2=5:5-7:5&selection3=8:5-10:5&selection4=11:5-13:5")
-  }
 
-  fun testGroovyConstantReference() {
-    doTest("fqn=BuildOptions#USE_COMPILED_CLASSES_PROPERTY", getTestName(false) + ".groovy")
-  }
+  fun testGroovyConstantReference() =
+    doTest("fqn=BuildOptions#USE_COMPILED_CLASSES_PROPERTY", fileName = getTestName(false) + ".groovy")
 
-  fun testPathWithLocation() {
+  fun testPathWithLocation() =
     doTest("path=PathWithLocation.java:4:13")
-  }
 
-  fun testMultipleFiles() {
-    val file1 = myFixture.configureByFile("File1.java")
-    val file2 = myFixture.configureByFile("File2.java")
+  fun testMultipleFiles() =
+    doTest("path1=File1.java&path2=File2.java", fileName = null, psiElements = arrayOf(
+      myFixture.configureByFile("File1.java"),
+      myFixture.configureByFile("File2.java"))
+    )
 
-    doTest("path1=File1.java&path2=File2.java",
-           fileName = null,
-           additionalDataContext = mapOf(LangDataKeys.PSI_ELEMENT_ARRAY.name to arrayOf(file1, file2)))
-  }
+  private fun doTest(
+    expectedParameters: String,
+    fileName: String? = getTestName(false) + ".java",
+    psiElements: Array<PsiElement>? = null
+  ) {
+    fileName?.let {
+      myFixture.configureByFile(it)
+    }
 
-  private fun doTest(expectedURL: String,
-                     fileName: String? = getTestName(false) + ".java",
-                     additionalDataContext: Map<String, Any> = hashMapOf()) {
-    fileName?.let { myFixture.configureByFile(fileName) }
+    val dataContext = SimpleDataContext.builder()
+      .setParent(DataManager.getInstance().getDataContext(myFixture.editor.component))
+      .apply {
+        if (psiElements != null) {
+          add(LangDataKeys.PSI_ELEMENT_ARRAY, psiElements)
+        }
+      }
+      .build()
 
-    var dataContext = DataManager.getInstance().getDataContext(myFixture.editor.component)
-    dataContext = SimpleDataContext.getSimpleContext(additionalDataContext, dataContext)
     val action = ActionManager.getInstance().getAction("CopyTBXReference")
 
     val presentation = Presentation()
     presentation.copyFrom(action.templatePresentation)
-    action.actionPerformed(AnActionEvent(null, dataContext, ActionPlaces.MAIN_MENU, presentation, ActionManager.getInstance(), 0))
+    action.actionPerformed(AnActionEvent(dataContext, presentation, ActionPlaces.MAIN_MENU, ActionUiKind.MAIN_MENU, null, 0, ActionManager.getInstance()))
 
     val content = CopyPasteManager.getInstance().contents?.getTransferData(DataFlavor.stringFlavor) as String
-    assertThat(content).startsWith("jetbrains://idea/navigate/reference?project=" + project.name)
-    assertThat(content).endsWith("&$expectedURL")
+    val url = URLDecoder.decode(content, StandardCharsets.UTF_8)
+    assertThat(url).startsWith("jetbrains://idea/navigate/reference?project=${project.name}")
+    assertThat(url).endsWith("&${expectedParameters}")
   }
 }
