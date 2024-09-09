54d6d273684003e28925f052a9c64c968455992f
Max Medvedev <maxim.medvedev@jetbrains.com>
2024-09-09 16:17:30 +0200

    IJPL-161819 move InlinePromptManager to inlinePrompt package
    GitOrigin-RevId: c360460bc0397e044d5b7b6a6d6a3b343cae96f0

--- platform/analysis-api/src/com/intellij/inlinePrompt/InlinePromptManager.kt
+++ platform/analysis-api/src/com/intellij/inlinePrompt/InlinePromptManager.kt
@@ -1,8 +1,9 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 @file:JvmName("InlinePrompt")
-package com.intellij.ai
 
-import com.intellij.ai.InlinePromptManager.Companion.getInstance
+package com.intellij.inlinePrompt
+
+import com.intellij.inlinePrompt.InlinePromptManager.Companion.getInstance
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import org.jetbrains.annotations.ApiStatus.Experimental
