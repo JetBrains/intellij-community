a9c9e81236c1383b1b37688fb0da19938a6ecfd1
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2024-09-09 16:49:43 +0400

    [kotlin] Convert `InlineTypeParameterFix` to ModCommandAction
    ^KTIJ-29501
    
    GitOrigin-RevId: be530d3940a87daecaeff70dd842d4dd1e255b7a

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/InlineTypeParameterFix.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/InlineTypeParameterFix.kt
@@ -2,50 +2,45 @@
 
 package org.jetbrains.kotlin.idea.quickfix
 
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.modcommand.ActionContext
+import com.intellij.modcommand.ModPsiUpdater
+import com.intellij.modcommand.PsiUpdateModCommandAction
+import com.intellij.psi.SmartPsiElementPointer
+import com.intellij.psi.createSmartPointer
+import com.intellij.psi.util.descendantsOfType
 import com.intellij.psi.util.elementType
 import com.intellij.psi.util.siblings
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.diagnostics.Errors
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.caches.resolve.analyzeWithContent
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.quickfixes.KotlinQuickFixAction
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.psi.psiUtil.forEachDescendantOfType
 import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
 import org.jetbrains.kotlin.resolve.BindingContext
 
-class InlineTypeParameterFix(val typeReference: KtTypeReference) : KotlinQuickFixAction<KtTypeReference>(typeReference) {
-    override fun invoke(project: Project, editor: Editor?, file: KtFile) {
-        val parameterListOwner = typeReference.getStrictParentOfType<KtTypeParameterListOwner>() ?: return
+class InlineTypeParameterFix(
+    element: KtTypeReference,
+    private val typeReferencesToInline: List<SmartPsiElementPointer<KtTypeReference>>,
+) : PsiUpdateModCommandAction<KtTypeReference>(element) {
+
+    private data class ElementContext(
+        val parameter: KtTypeParameter,
+        val bound: KtTypeReference,
+        val constraint: KtElement?,
+    )
+
+    override fun invoke(
+        actionContext: ActionContext,
+        element: KtTypeReference,
+        updater: ModPsiUpdater,
+    ) {
+        val parameterListOwner = element.getStrictParentOfType<KtTypeParameterListOwner>() ?: return
         val parameterList = parameterListOwner.typeParameterList ?: return
-        val (parameter, bound, constraint) = when (val parent = typeReference.parent) {
-            is KtTypeParameter -> {
-                val bound = parent.extendsBound ?: return
-                Triple(parent, bound, null)
-            }
+        val (parameter, bound, constraint) = prepareElementContext(element, parameterList) ?: return
 
-            is KtTypeConstraint -> {
-                val subjectTypeParameterName = parent.subjectTypeParameterName?.text ?: return
-                val parameter = parameterList.parameters.firstOrNull { it.name == subjectTypeParameterName } ?: return
-                val bound = parent.boundTypeReference ?: return
-                Triple(parameter, bound, parent)
-            }
-
-            else -> return
-        }
-
-        val context = parameterListOwner.analyzeWithContent()
-        val parameterDescriptor = context[BindingContext.TYPE_PARAMETER, parameter] ?: return
-        parameterListOwner.forEachDescendantOfType<KtTypeReference> { typeReference ->
-            val typeElement = typeReference.typeElement
-            val type = context[BindingContext.TYPE, typeReference]
-            if (typeElement != null && type != null && type.constructor.declarationDescriptor == parameterDescriptor) {
-                typeReference.replace(bound)
-            }
-        }
+        typeReferencesToInline.mapNotNull { it.element }.map(updater::getWritable).forEach { it.replace(bound) }
 
         if (parameterList.parameters.size == 1) {
             parameterList.delete()
@@ -62,11 +57,49 @@ class InlineTypeParameterFix(val typeReference: KtTypeReference) : KotlinQuickFi
         }
     }
 
-    override fun getText() = KotlinBundle.message("inline.type.parameter")
-
-    override fun getFamilyName() = text
+    override fun getFamilyName() = KotlinBundle.message("inline.type.parameter")
 
     companion object Factory : KotlinSingleIntentionActionFactory() {
-        override fun createAction(diagnostic: Diagnostic) = InlineTypeParameterFix(Errors.FINAL_UPPER_BOUND.cast(diagnostic).psiElement)
+        override fun createAction(diagnostic: Diagnostic): IntentionAction? {
+            val element = Errors.FINAL_UPPER_BOUND.cast(diagnostic).psiElement
+            val parameterListOwner = element.getStrictParentOfType<KtTypeParameterListOwner>() ?: return null
+            val parameterList = parameterListOwner.typeParameterList ?: return null
+            val (parameter, _, _) = prepareElementContext(element, parameterList) ?: return null
+
+            val context = parameterListOwner.analyzeWithContent()
+            val parameterDescriptor = context[BindingContext.TYPE_PARAMETER, parameter] ?: return null
+
+            val typeReferencesToInline = parameterListOwner
+                .descendantsOfType<KtTypeReference>()
+                .filter { typeReference ->
+                    val typeElement = typeReference.typeElement
+                    val type = context[BindingContext.TYPE, typeReference]
+                    typeElement != null && type?.constructor?.declarationDescriptor == parameterDescriptor
+                }.map { it.createSmartPointer() }
+                .toList()
+
+            return InlineTypeParameterFix(element, typeReferencesToInline).asIntention()
+        }
+
+        private fun prepareElementContext(
+            element: KtTypeReference,
+            parameterList: KtTypeParameterList
+        ): ElementContext? {
+            return when (val parent = element.parent) {
+                is KtTypeParameter -> {
+                    val bound = parent.extendsBound ?: return null
+                    ElementContext(parent, bound, null)
+                }
+
+                is KtTypeConstraint -> {
+                    val subjectTypeParameterName = parent.subjectTypeParameterName?.text ?: return null
+                    val parameter = parameterList.parameters.firstOrNull { it.name == subjectTypeParameterName } ?: return null
+                    val bound = parent.boundTypeReference ?: return null
+                    ElementContext(parameter, bound, parent)
+                }
+
+                else -> null
+            }
+        }
     }
 }
