6baca570a5da06987e0dca83e84b420df0784e68
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2024-09-09 14:28:50 +0400

    IJPL-161273 Force use software rendering on Wayland
    GitOrigin-RevId: ad01255dd66423660ed9f5263d0baf4f7303ed80

--- platform/ui.jcef/jcef/JBCefNativeOsrHandler.java
+++ platform/ui.jcef/jcef/JBCefNativeOsrHandler.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ui.jcef;
 
+import com.intellij.openapi.util.SystemInfo;
 import com.intellij.util.JBHiDPIScaledImage;
 import com.intellij.util.RetinaImage;
 import com.jetbrains.JBR;
@@ -24,7 +25,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @SuppressWarnings("NonPrivateFieldAccessedInSynchronizedContext")
 class JBCefNativeOsrHandler extends JBCefOsrHandler implements CefNativeRenderHandler {
   private static final int CLEAN_CACHE_TIME_MS = Integer.getInteger("jcef.remote.osr.clean_cache_time_ms", 10*1000); // 10 sec
-  private static final boolean FORCE_USE_SOFTWARE_RENDERING = Boolean.getBoolean("jcef.remote.force_use_software_rendering");
+  private static final boolean FORCE_USE_SOFTWARE_RENDERING = SystemInfo.isWayland || Boolean.getBoolean("jcef.remote.force_use_software_rendering");
 
   private final Map<String, SharedMemory.WithRaster> mySharedMemCache = new ConcurrentHashMap<>();
   private SharedMemory.WithRaster myCurrentFrame;
