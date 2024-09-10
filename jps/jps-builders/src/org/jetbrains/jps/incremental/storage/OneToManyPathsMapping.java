0a704000da01f625aac1d327896839c100b7b0be
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-09-10 08:46:56 +0200

    createFilePathLinkedSet not needed here
    GitOrigin-RevId: 1a76b4f8b5b60eca0b4ed3659bc3008ce8b9ca9f

--- jps/jps-builders/src/org/jetbrains/jps/incremental/storage/OneToManyPathsMapping.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/storage/OneToManyPathsMapping.java
@@ -1,13 +1,11 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.jps.incremental.storage;
 
-import com.intellij.util.Function;
-import com.intellij.util.containers.CollectionFactory;
-import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.io.DataExternalizer;
 import com.intellij.util.io.IOUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
 import org.jetbrains.jps.incremental.relativizer.PathRelativizerService;
 import org.jetbrains.jps.javac.Iterators;
 
@@ -18,72 +16,100 @@ import java.util.*;
  * @author Eugene Zhuravlev
  */
 public final class OneToManyPathsMapping extends AbstractStateStorage<String, Collection<String>> {
-  private final PathRelativizerService myRelativizer;
+  private final PathRelativizerService relativizer;
 
   public OneToManyPathsMapping(File storePath, PathRelativizerService relativizer) throws IOException {
     super(storePath, PathStringDescriptor.INSTANCE, new PathCollectionExternalizer());
-    myRelativizer = relativizer;
+    this.relativizer = relativizer;
   }
 
   @Override
-  public void update(@NotNull String keyPath, @NotNull Collection<String> boundPaths) throws IOException {
-    super.update(normalizePath(keyPath), normalizePaths(boundPaths));
+  public void update(@NotNull String keyPath, @SuppressWarnings("NullableProblems") @NotNull Collection<String> boundPaths) throws IOException {
+    super.update(normalizePath(keyPath), normalizePaths((List<String>)boundPaths));
   }
 
   public void update(@NotNull String keyPath, @NotNull String boundPath) throws IOException {
-    super.update(normalizePath(keyPath), Collections.singleton(normalizePath(boundPath)));
+    super.update(normalizePath(keyPath), List.of(normalizePath(boundPath)));
   }
 
   public void appendData(@NotNull String keyPath, @NotNull String boundPath) throws IOException {
-    super.appendData(normalizePath(keyPath), Collections.singleton(normalizePath(boundPath)));
+    super.appendData(normalizePath(keyPath), List.of(normalizePath(boundPath)));
   }
 
   @Override
   public void appendData(@NotNull String keyPath, @NotNull Collection<String> boundPaths) throws IOException {
-    super.appendData(normalizePath(keyPath), normalizePaths(boundPaths));
+    super.appendData(normalizePath(keyPath), normalizePaths((List<String>)boundPaths));
   }
 
   @Override
   public @Nullable Collection<String> getState(@NotNull String keyPath) throws IOException {
-    Collection<String> collection = super.getState(normalizePath(keyPath));
-    return collection != null ? ContainerUtil.map(collection, toFull()) : null;
+    List<String> collection = (List<String>)super.getState(relativizer.toRelative(keyPath));
+    if (collection == null || collection.isEmpty()) {
+      return null;
+    }
+    else {
+      String[] result = new String[collection.size()];
+      for (int i = 0, size = collection.size(); i < size; i++) {
+        result[i] = relativizer.toFull(collection.get(i));
+      }
+      return Arrays.asList(result);
+    }
   }
 
   public @NotNull Iterator<String> getStateIterator(@NotNull String keyPath) throws IOException {
-    Collection<String> collection = super.getState(normalizePath(keyPath));
-    return collection == null ? Collections.emptyIterator() : Iterators.map(collection.iterator(), myRelativizer::toFull);
+    List<String> collection = (List<String>)super.getState(relativizer.toRelative(keyPath));
+    return collection == null ? Collections.emptyIterator() : Iterators.map(collection.iterator(), relativizer::toFull);
   }
 
   @Override
   public void remove(@NotNull String keyPath) throws IOException {
-    super.remove(normalizePath(keyPath));
+    super.remove(relativizer.toRelative(keyPath));
   }
 
   @Override
-  public Collection<String> getKeys() throws IOException {
-    return ContainerUtil.map(super.getKeys(), toFull());
+  public @NotNull List<String> getKeys() throws IOException {
+    List<String> collection = (List<String>)super.getKeys();
+    if (collection.isEmpty()) {
+      return List.of();
+    }
+
+    String[] result = new String[collection.size()];
+    for (int i = 0; i < collection.size(); i++) {
+      result[i] = relativizer.toFull(collection.get(i));
+    }
+    return Arrays.asList(result);
   }
 
   @Override
   public Iterator<String> getKeysIterator() throws IOException {
-    return Iterators.map(super.getKeysIterator(), myRelativizer::toFull);
+    return Iterators.map(super.getKeysIterator(), relativizer::toFull);
   }
 
   public void removeData(@NotNull String keyPath, @NotNull String boundPath) throws IOException {
-    final Collection<String> outputPaths = getState(keyPath);
-    if (outputPaths != null) {
-      String normalizedPath = normalizePath(boundPath);
-      List<String> newState = ContainerUtil.filter(outputPaths, path -> !normalizedPath.equals(path));
-      final boolean removed = newState.size() != outputPaths.size();
-      if (removed) {
-        if (newState.isEmpty()) {
-          remove(keyPath);
-        }
-        else {
-          update(keyPath, newState);
-        }
+    String relativeKeyPath = relativizer.toRelative(keyPath);
+    List<String> oldList = (List<String>)super.getState(relativeKeyPath);
+    if (oldList == null || oldList.isEmpty()) {
+      return;
+    }
+
+    String relativePath = relativizer.toRelative(boundPath);
+    int index = oldList.indexOf(relativePath);
+    if (index < 0) {
+      return;
+    }
+
+    if (oldList.size() == 1) {
+      super.remove(relativeKeyPath);
+      return;
+    }
+
+    List<String> newState = new ArrayList<>(oldList.size() - 1);
+    for (int i = 0; i < oldList.size(); i++) {
+      if (index != i) {
+        newState.add(oldList.get(i));
       }
     }
+    super.update(relativeKeyPath, newState);
   }
 
   private static final class PathCollectionExternalizer implements DataExternalizer<Collection<String>> {
@@ -95,31 +121,29 @@ public final class OneToManyPathsMapping extends AbstractStateStorage<String, Co
     }
 
     @Override
-    public Collection<String> read(@NotNull DataInput in) throws IOException {
-      final Set<String> result = CollectionFactory.createFilePathLinkedSet();
-      final DataInputStream stream = (DataInputStream)in;
+    public List<String> read(@NotNull DataInput in) throws IOException {
+      List<String> result = new ArrayList<>();
+      DataInputStream stream = (DataInputStream)in;
+      HashSet<String> guard = new HashSet<>();
       while (stream.available() > 0) {
-        final String str = IOUtil.readUTF(stream);
-        result.add(str);
+        String s = IOUtil.readUTF(stream);
+        if (guard.add(s)) {
+          result.add(s);
+        }
       }
       return result;
     }
   }
 
-  private @NotNull Function<String, String> toFull() {
-    return s -> myRelativizer.toFull(s);
-  }
-
   private String normalizePath(@NotNull String path) {
-    return myRelativizer.toRelative(path);
+    return relativizer.toRelative(path);
   }
 
-  private Collection<String> normalizePaths(Collection<String> outputs) {
-    Collection<String> normalized = new ArrayList<>(outputs.size());
-    for (String out : outputs) {
-      normalized.add(normalizePath(out));
+  private @NotNull @Unmodifiable List<String> normalizePaths(@NotNull List<String> outputs) {
+    String[] normalized = new String[outputs.size()];
+    for (int i = 0; i < normalized.length; i++) {
+      normalized[i] = relativizer.toRelative(outputs.get(i));
     }
-    return normalized;
+    return Arrays.asList(normalized);
   }
-
 }
