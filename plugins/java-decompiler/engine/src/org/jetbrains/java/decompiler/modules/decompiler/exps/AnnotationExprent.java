470daefb6f320ecc4aa7a448a5ac684f0c9afc6c
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2024-09-10 12:41:47 +0200

    [java-decompiler] IDEA-346312 adapt patch 048
    - update Copyrights
    
    GitOrigin-RevId: 9d6fd1f2c40e4d2dc69aae2975f77b466d7ab059

--- plugins/java-decompiler/engine/src/org/jetbrains/java/decompiler/modules/decompiler/exps/AnnotationExprent.java
+++ plugins/java-decompiler/engine/src/org/jetbrains/java/decompiler/modules/decompiler/exps/AnnotationExprent.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.java.decompiler.modules.decompiler.exps;
 
 import org.jetbrains.java.decompiler.main.DecompilerContext;
