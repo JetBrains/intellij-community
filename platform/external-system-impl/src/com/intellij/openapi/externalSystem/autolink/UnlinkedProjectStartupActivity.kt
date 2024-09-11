1760e006dcbff433fcd5f65f9ef9fb2b9fe95fd8
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2024-09-11 18:38:25 +0200

    IDEA-353465 [Auto-Link] Don't show the Build Script Found notification for the just unlinked project
    GitOrigin-RevId: 6b7538ff9dca5ca5914b4bc3c6c7ab51f356904a

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/autolink/UnlinkedProjectStartupActivity.kt
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/autolink/UnlinkedProjectStartupActivity.kt
@@ -142,21 +142,11 @@ class UnlinkedProjectStartupActivity : ProjectActivity {
     val coroutineScope = CoroutineScopeService.getCoroutineScope(project)
     EP_NAME.withEachExtensionSafeAsync(project) { extension, extensionDisposable ->
       extension.subscribe(project, object : ExternalSystemProjectLinkListener {
-
         override fun onProjectLinked(externalProjectPath: String) {
           coroutineScope.launch(extensionDisposable) {
             projectRoots.removeProjectRoot(externalProjectPath)
           }
         }
-
-        override fun onProjectUnlinked(externalProjectPath: String) {
-          coroutineScope.launch(extensionDisposable) {
-            if (!hasLinkedProject(project, externalProjectPath)) {
-              projectRoots.addProjectRoot(externalProjectPath)
-            }
-          }
-        }
-
       }, extensionDisposable)
     }
     return projectRoots
