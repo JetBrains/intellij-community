955e1cb4380c9a903255c8fdf3c3c2990d017c24
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-09-11 15:27:50 +0200

    RegExp: fix saving of custom regex inspection after pattern changes (IJPL-17570)
    GitOrigin-RevId: 65bffa97d4c49b5a87814f224fcdd231f9fb09bc

--- RegExpSupport/src/org/intellij/lang/regexp/inspection/custom/CustomRegExpFakeInspection.java
+++ RegExpSupport/src/org/intellij/lang/regexp/inspection/custom/CustomRegExpFakeInspection.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.intellij.lang.regexp.inspection.custom;
 
 import com.intellij.codeInsight.daemon.HighlightDisplayKey;
@@ -252,9 +252,12 @@ public class CustomRegExpFakeInspection extends LocalInspectionTool {
     saveChangesToProfile(list);
   }
 
-  private static void saveChangesToProfile(JList<InspectionPattern> list) {
+  private void saveChangesToProfile(JList<InspectionPattern> list) {
     final InspectionProfileModifiableModel profile = getInspectionProfile(list);
-    if (profile != null) profile.setModified(true);
+    if (profile == null) return;
+    final CustomRegExpInspection inspection = getRegExpInspection(profile);
+    inspection.updateConfiguration(myConfiguration);
+    profile.setModified(true);
   }
 
   @NotNull
