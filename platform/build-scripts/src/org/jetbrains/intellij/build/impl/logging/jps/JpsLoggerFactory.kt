19f7d72de34999275ce620fadc5e05a67b521c84
Dmitriy.Panov <dmitriy.panov@jetbrains.com>
2024-09-11 22:36:52 +0200

    TW-89630 workaround: flowId is not set
    GitOrigin-RevId: 73ad90ca0287ba227c78e9dd36f7fdcbee18e0bb

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/logging/jps/JpsLoggerFactory.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/logging/jps/JpsLoggerFactory.kt
@@ -13,7 +13,6 @@ import org.jetbrains.annotations.ApiStatus.Internal
 import org.jetbrains.annotations.Nls
 import org.jetbrains.annotations.VisibleForTesting
 import org.jetbrains.intellij.build.CompilationContext
-import org.jetbrains.intellij.build.logging.TeamCityBuildMessageLogger
 import org.jetbrains.jps.builders.BuildTarget
 import org.jetbrains.jps.incremental.MessageHandler
 import org.jetbrains.jps.incremental.messages.*
@@ -87,7 +86,7 @@ internal class JpsMessageHandler(private val context: CompilationContext, privat
   private val compilationFinishTimeForTarget = ConcurrentHashMap<String, Long>()
   private var progress = -1.0f
 
-  override fun processMessage(message: BuildMessage): Unit = TeamCityBuildMessageLogger.withFlow(span) {
+  override fun processMessage(message: BuildMessage) {
     val text = message.messageText
     when (message.kind) {
       BuildMessage.Kind.ERROR, BuildMessage.Kind.INTERNAL_BUILDER_ERROR -> {
