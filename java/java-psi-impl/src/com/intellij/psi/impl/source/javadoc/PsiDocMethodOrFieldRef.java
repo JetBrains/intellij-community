3eace124839f44755dfd5728838fd8491a673b48
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-09-11 18:08:11 +0200

    Java: avoid unnecessary regex compilation and matching
    GitOrigin-RevId: c58621b89c1594d24e7ea23d6dab6f86d7a80112

--- java/java-psi-impl/src/com/intellij/psi/impl/source/javadoc/PsiDocMethodOrFieldRef.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/javadoc/PsiDocMethodOrFieldRef.java
@@ -144,12 +144,7 @@ public class PsiDocMethodOrFieldRef extends CompositePsiElement implements PsiDo
     List<String> types = new ArrayList<>();
     for (PsiElement child = element.getFirstChild(); child != null; child = child.getNextSibling()) {
       if (child.getNode().getElementType() == DOC_TYPE_HOLDER) {
-        final String[] typeStrings = child.getText().split("[, ]");  //avoid param types list parsing hmm method(paramType1, paramType2, ...) -> typeElement1, identifier2, ...
-        for (String type : typeStrings) {
-          if (!type.isEmpty()) {
-            types.add(type);
-          }
-        }
+        types.add(child.getText());
       }
     }
 
