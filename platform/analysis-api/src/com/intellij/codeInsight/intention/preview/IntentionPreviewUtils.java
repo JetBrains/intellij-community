79b364e5aa770021b1cb62104de26a0fe4908e70
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-09-11 14:52:35 +0200

    WEB-68860 Angular Standalone Components are Default from Version 19 onwards
    (cherry picked from commit 945e18b05119661e6d53deb9f0427a133ed89faa)
    
    IJ-CR-144697
    
    GitOrigin-RevId: 74fa768e908465653679feca34c83ac92f3a5bff

--- platform/analysis-api/src/com/intellij/codeInsight/intention/preview/IntentionPreviewUtils.java
+++ platform/analysis-api/src/com/intellij/codeInsight/intention/preview/IntentionPreviewUtils.java
@@ -12,6 +12,7 @@ import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.ThrowableComputable;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.ThrowableRunnable;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Contract;
@@ -28,7 +29,7 @@ public final class IntentionPreviewUtils {
   /**
    * This method is a part of the internal implementation of an intention preview mechanism.
    * It's not intended to be called in the client code.
-   * 
+   *
    * @param file file to get the preview copy
    * @return a preview copy of the file
    */
@@ -41,7 +42,7 @@ public final class IntentionPreviewUtils {
    * This method is a part of the internal implementation of an intention preview mechanism.
    * It's not intended to be called in the client code.
    *
-   * @param file file to get the preview copy
+   * @param file         file to get the preview copy
    * @param originalFile original file. May differ from file if we are inside the injection
    * @return a preview copy of the file
    */
@@ -67,7 +68,7 @@ public final class IntentionPreviewUtils {
    */
   public static @Nullable PsiFile getOriginalFile(@NotNull PsiFile previewFile) {
     return previewFile.getUserData(PREVIEW_MARKER);
-  } 
+  }
 
   /**
    * Prepares an element for writing, taking into account that it could be a preview element
@@ -84,13 +85,14 @@ public final class IntentionPreviewUtils {
    * Performs a preview-aware write-action. Execute action immediately if preview is active; wrap into write action otherwise.
    *
    * @param action action to execute
-   * @param <E> exception the action throws
+   * @param <E>    exception the action throws
    * @throws E if action throws
    */
   public static <E extends Throwable> void write(@NotNull ThrowableRunnable<E> action) throws E {
     if (PREVIEW_EDITOR.get() != null) {
       action.run();
-    } else {
+    }
+    else {
       WriteAction.run(action);
     }
   }
@@ -99,21 +101,23 @@ public final class IntentionPreviewUtils {
    * Performs a preview-aware write-action. Execute action immediately if preview is active; wrap into write action otherwise.
    *
    * @param action action to execute
-   * @param <E> exception the action throws
+   * @param <E>    exception the action throws
    * @return result of the action
    * @throws E if action throws
    */
   public static <T, E extends Throwable> T writeAndCompute(@NotNull ThrowableComputable<T, E> action) throws E {
     if (PREVIEW_EDITOR.get() != null) {
       return action.compute();
-    } else {
+    }
+    else {
       return WriteAction.compute(action);
     }
   }
 
   /**
    * Start a preview session with given editor (generatePreview call should be wrapped)
-   * @param editor preview editor to use
+   *
+   * @param editor   preview editor to use
    * @param runnable action to execute
    */
   @ApiStatus.Internal
@@ -145,12 +149,30 @@ public final class IntentionPreviewUtils {
 
   /**
    * @param modCommand {@link ModCommand} to generate preview for
-   * @param context context in which the action is about to be executed
+   * @param context    context in which the action is about to be executed
    * @return default preview for a given ModCommand
    */
   @ApiStatus.Experimental
   @Contract(pure = true)
   public static @NotNull IntentionPreviewInfo getModCommandPreview(@NotNull ModCommand modCommand, @NotNull ActionContext context) {
     return ModCommandExecutor.getInstance().getPreview(modCommand, context);
+  }
+
+  /**
+   * Tries to convert an element from an original file to the same element in the preview file.
+   * This method should be used only before any modification to the preview file is made.
+   * <p>
+   * If {@code file} is an original file or {@code element} is already a preview element, then {@code element} is returned as is.
+   * Otherwise an attempt to convert the element to a preview element is made. For that to succeed, {@code element} must be from
+   * the original file of the preview {@code file}.
+   *
+   */
+  public static <T extends PsiElement> @Nullable T toPreviewElementIfPreviewFile(@NotNull PsiFile file, @NotNull T element) {
+    if (!isPreviewElement(file) || isPreviewElement(element)) return element;
+    if (getOriginalFile(file) != element.getContainingFile()) return null;
+    var range = element.getTextRange();
+    //noinspection unchecked
+    return (T)PsiTreeUtil.findElementOfClassAtRange(file, range.getStartOffset(), range.getEndOffset(),
+                                                 element.getClass());
   }
 }
