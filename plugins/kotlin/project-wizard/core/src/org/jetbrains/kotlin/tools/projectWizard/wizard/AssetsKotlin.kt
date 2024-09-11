f859d4e73ad67ae2f414a818d3e5d0651cf3a128
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2024-09-11 12:56:27 +0200

    [NPW|Assets] cleanup: replaced AssetsKotlinNewProjectWizardStep class with the AssetsKotlin util file
    GitOrigin-RevId: 3abbe92b5ed9da124c818628a3d90bf0dd4e59aa

---
+++ plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/wizard/AssetsKotlin.kt
@@ -0,0 +1,80 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.tools.projectWizard.wizard
+
+import com.intellij.ide.projectWizard.generators.AssetsNewProjectWizardStep
+import com.intellij.ide.projectWizard.generators.AssetsOnboardingTips.icon
+import com.intellij.ide.projectWizard.generators.AssetsOnboardingTips.shortcut
+import com.intellij.ide.projectWizard.generators.AssetsOnboardingTips.shouldRenderOnboardingTips
+import com.intellij.ide.projectWizard.generators.prepareOnboardingTips
+import com.intellij.openapi.actionSystem.IdeActions
+import com.intellij.openapi.application.ApplicationNamesInfo
+import com.intellij.openapi.keymap.KeymapTextContext
+import com.intellij.openapi.project.Project
+import org.jetbrains.kotlin.tools.projectWizard.KotlinNewProjectWizardBundle
+
+private const val DEFAULT_FILE_NAME = "Main.kt"
+
+fun AssetsNewProjectWizardStep.withKotlinSampleCode(
+    sourceRootPath: String,
+    packageName: String?,
+    generateOnboardingTips: Boolean,
+    shouldOpenFile: Boolean = true
+) {
+    val templateName = when {
+        !generateOnboardingTips -> "KotlinSampleCode"
+        shouldRenderOnboardingTips() -> "KotlinSampleCodeWithRenderedOnboardingTips"
+        else -> "KotlinSampleCodeWithOnboardingTips"
+    }
+
+    val sourcePath = "$sourceRootPath/$DEFAULT_FILE_NAME"
+    withKotlinSampleCode(sourcePath, templateName, packageName, generateOnboardingTips, shouldOpenFile)
+}
+
+fun AssetsNewProjectWizardStep.withKotlinSampleCode(
+    sourcePath: String,
+    templateName: String,
+    packageName: String?,
+    generateOnboardingTips: Boolean,
+    shouldOpenFile: Boolean = true
+) {
+    addTemplateAsset(sourcePath, templateName, buildMap {
+        packageName?.let {
+            put("PACKAGE_NAME", it)
+        }
+        if (generateOnboardingTips) {
+            val tipsContext = KeymapTextContext()
+
+            if (shouldRenderOnboardingTips()) {
+                //@formatter:off
+                put("RunComment1", KotlinNewProjectWizardBundle.message("onboarding.run.comment.render.1", shortcut(IdeActions.ACTION_DEFAULT_RUNNER)))
+                put("RunComment2", KotlinNewProjectWizardBundle.message("onboarding.run.comment.render.2", icon("AllIcons.Actions.Execute")))
+
+                put("ShowIntentionComment1", KotlinNewProjectWizardBundle.message("onboarding.show.intention.tip.comment.render.1", shortcut(IdeActions.ACTION_SHOW_INTENTION_ACTIONS)))
+                put("ShowIntentionComment2", KotlinNewProjectWizardBundle.message("onboarding.show.intention.tip.comment.render.2", ApplicationNamesInfo.getInstance().fullProductName))
+
+                put("DebugComment1", KotlinNewProjectWizardBundle.message("onboarding.debug.comment.render.1", shortcut(IdeActions.ACTION_DEFAULT_DEBUGGER), icon("AllIcons.Debugger.Db_set_breakpoint")))
+                put("DebugComment2", KotlinNewProjectWizardBundle.message("onboarding.debug.comment.render.2", shortcut(IdeActions.ACTION_TOGGLE_LINE_BREAKPOINT)))
+                //@formatter:on
+            } else {
+                //@formatter:off
+                put("RunComment", KotlinNewProjectWizardBundle.message("onboarding.run.comment", tipsContext.getShortcutText(IdeActions.ACTION_DEFAULT_RUNNER)))
+
+                put("ShowIntentionComment1", KotlinNewProjectWizardBundle.message("onboarding.show.intention.tip.comment.1", tipsContext.getShortcutText(IdeActions.ACTION_SHOW_INTENTION_ACTIONS)))
+                put("ShowIntentionComment2", KotlinNewProjectWizardBundle.message("onboarding.show.intention.tip.comment.2", ApplicationNamesInfo.getInstance().fullProductName))
+
+                put("DebugComment1", KotlinNewProjectWizardBundle.message("onboarding.debug.comment.1", tipsContext.getShortcutText(IdeActions.ACTION_DEFAULT_DEBUGGER)))
+                put("DebugComment2", KotlinNewProjectWizardBundle.message("onboarding.debug.comment.2", tipsContext.getShortcutText(IdeActions.ACTION_TOGGLE_LINE_BREAKPOINT)))
+                //@formatter:on
+            }
+        }
+    })
+    if (shouldOpenFile) {
+        addFilesToOpen(sourcePath)
+    }
+}
+
+fun AssetsNewProjectWizardStep.prepareKotlinSampleOnboardingTips(project: Project) {
+    prepareOnboardingTips(project, "KotlinSampleCode", DEFAULT_FILE_NAME) { charsSequence ->
+        charsSequence.indexOf("println(\"i").takeIf { it >= 0 }
+    }
+}
--- plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/wizard/AssetsKotlinNewProjectWizardStep.kt
+++ plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/wizard/AssetsKotlinNewProjectWizardStep.kt
@@ -3,68 +3,21 @@ package org.jetbrains.kotlin.tools.projectWizard.wizard
 
 import com.intellij.ide.projectWizard.generators.AssetsOnboardingTipsProjectWizardStep
 import com.intellij.ide.wizard.NewProjectWizardStep
-import com.intellij.openapi.actionSystem.IdeActions
-import com.intellij.openapi.application.ApplicationNamesInfo
-import com.intellij.openapi.keymap.KeymapTextContext
 import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.tools.projectWizard.KotlinNewProjectWizardBundle
+import org.jetbrains.kotlin.tools.projectWizard.wizard.withKotlinSampleCode as withKotlinSampleCodeImpl
+import org.jetbrains.kotlin.tools.projectWizard.wizard.prepareKotlinSampleOnboardingTips as prepareKotlinSampleOnboardingTipsImpl
 
-private const val generatedFileName = "Main.kt"
-
+@Suppress("unused")
+@Deprecated("Use AssetsKotlin util instead")
 abstract class AssetsKotlinNewProjectWizardStep(parent: NewProjectWizardStep) : AssetsOnboardingTipsProjectWizardStep(parent) {
+
     fun withKotlinSampleCode(
         sourceRootPath: String,
         packageName: String?,
         generateOnboardingTips: Boolean,
         shouldOpenFile: Boolean = true
-    ) {
-        val renderedOnboardingTips = shouldRenderOnboardingTips()
-        val templateName = when {
-            !generateOnboardingTips -> "KotlinSampleCode"
-            renderedOnboardingTips -> "KotlinSampleCodeWithRenderedOnboardingTips"
-            else -> "KotlinSampleCodeWithOnboardingTips"
-        }
+    ) = withKotlinSampleCodeImpl(sourceRootPath, packageName, generateOnboardingTips, shouldOpenFile)
 
-        val sourcePath = "$sourceRootPath/$generatedFileName"
-        addTemplateAsset(sourcePath, templateName, buildMap {
-            packageName?.let {
-                put("PACKAGE_NAME", it)
-            }
-            if (generateOnboardingTips) {
-                val tipsContext = KeymapTextContext()
-
-                if (renderedOnboardingTips) {
-                    //@formatter:off
-                    put("RunComment1", KotlinNewProjectWizardBundle.message("onboarding.run.comment.render.1", shortcut(IdeActions.ACTION_DEFAULT_RUNNER)))
-                    put("RunComment2", KotlinNewProjectWizardBundle.message("onboarding.run.comment.render.2", icon("AllIcons.Actions.Execute")))
-
-                    put("ShowIntentionComment1", KotlinNewProjectWizardBundle.message("onboarding.show.intention.tip.comment.render.1", shortcut(IdeActions.ACTION_SHOW_INTENTION_ACTIONS)))
-                    put("ShowIntentionComment2", KotlinNewProjectWizardBundle.message("onboarding.show.intention.tip.comment.render.2", ApplicationNamesInfo.getInstance().fullProductName))
-
-                    put("DebugComment1", KotlinNewProjectWizardBundle.message("onboarding.debug.comment.render.1", shortcut(IdeActions.ACTION_DEFAULT_DEBUGGER), icon("AllIcons.Debugger.Db_set_breakpoint")))
-                    put("DebugComment2", KotlinNewProjectWizardBundle.message("onboarding.debug.comment.render.2", shortcut(IdeActions.ACTION_TOGGLE_LINE_BREAKPOINT)))
-                    //@formatter:on
-                } else {
-                    //@formatter:off
-                    put("RunComment", KotlinNewProjectWizardBundle.message("onboarding.run.comment", tipsContext.getShortcutText(IdeActions.ACTION_DEFAULT_RUNNER)))
-
-                    put("ShowIntentionComment1", KotlinNewProjectWizardBundle.message("onboarding.show.intention.tip.comment.1", tipsContext.getShortcutText(IdeActions.ACTION_SHOW_INTENTION_ACTIONS)))
-                    put("ShowIntentionComment2", KotlinNewProjectWizardBundle.message("onboarding.show.intention.tip.comment.2", ApplicationNamesInfo.getInstance().fullProductName))
-
-                    put("DebugComment1", KotlinNewProjectWizardBundle.message("onboarding.debug.comment.1", tipsContext.getShortcutText(IdeActions.ACTION_DEFAULT_DEBUGGER)))
-                    put("DebugComment2", KotlinNewProjectWizardBundle.message("onboarding.debug.comment.2", tipsContext.getShortcutText(IdeActions.ACTION_TOGGLE_LINE_BREAKPOINT)))
-                    //@formatter:on
-                }
-            }
-        })
-        if (shouldOpenFile) {
-            addFilesToOpen(sourcePath)
-        }
-    }
-
-    fun prepareOnboardingTips(project: Project) {
-        prepareOnboardingTips(project, "KotlinSampleCode", generatedFileName) { charsSequence ->
-            charsSequence.indexOf("println(\"i").takeIf { it >= 0 }
-        }
-    }
+    fun prepareOnboardingTips(project: Project) =
+        prepareKotlinSampleOnboardingTipsImpl(project)
 }
