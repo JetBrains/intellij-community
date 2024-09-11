03b0cefeba958c74f2ac9c6930373e4d70ca1ce2
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2024-09-11 12:09:40 +0200

    [NPW|Assets] cleanup: replaced AssetsOnboardingTipsProjectWizardStep class by the AssetsOnboardingTips util file
    GitOrigin-RevId: 42c6a405152e66a8f4bfa3def007f2741229f2c7

---
+++ java/idea-ui/src/com/intellij/ide/projectWizard/generators/AssetsOnboardingTips.kt
@@ -0,0 +1,47 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:ApiStatus.Experimental
+
+package com.intellij.ide.projectWizard.generators
+
+import com.intellij.ide.fileTemplates.FileTemplateManager
+import com.intellij.ide.wizard.NewProjectOnboardingTips
+import com.intellij.ide.wizard.OnboardingTipsInstallationInfo
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.registry.Registry
+import org.jetbrains.annotations.ApiStatus
+
+object AssetsOnboardingTips {
+
+  fun rawShortcut(shortcut: String): String {
+    return """<shortcut raw="$shortcut"/>"""
+  }
+
+  fun shortcut(actionId: String): String {
+    return """<shortcut actionId="$actionId"/>"""
+  }
+
+  fun icon(allIconsId: String): String {
+    return """<icon src="$allIconsId"/>"""
+  }
+
+  fun shouldRenderOnboardingTips(): Boolean {
+    return Registry.`is`("doc.onboarding.tips.render")
+  }
+}
+
+fun AssetsNewProjectWizardStep.prepareOnboardingTips(
+  project: Project,
+  templateWithoutTips: String,
+  fileName: String,
+  breakpointSelector: (CharSequence) -> Int?,
+) {
+  val templateManager = FileTemplateManager.getDefaultInstance()
+  val properties = getTemplateProperties()
+  val defaultProperties = templateManager.defaultProperties
+  val template = templateManager.getInternalTemplate(templateWithoutTips)
+  val simpleSampleText = template.getText(defaultProperties + properties)
+  val onboardingInfo = OnboardingTipsInstallationInfo(simpleSampleText, fileName, breakpointSelector)
+  for (extension in NewProjectOnboardingTips.EP_NAME.extensions) {
+    extension.installTips(project, onboardingInfo)
+  }
+}
