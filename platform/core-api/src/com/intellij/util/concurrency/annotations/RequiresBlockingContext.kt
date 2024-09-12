79a4176c5d7325fd9105518d057910618910cca7
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2024-09-12 13:01:20 +0200

    convert `@RequiresBlockingContext` to Kotlin
    GitOrigin-RevId: 4d12a2f301322519ca0b52eb25c1aa9274548de0

--- platform/core-api/src/com/intellij/util/concurrency/annotations/RequiresBlockingContext.kt
+++ platform/core-api/src/com/intellij/util/concurrency/annotations/RequiresBlockingContext.kt
@@ -1,17 +1,18 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.util.concurrency.annotations;
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.util.concurrency.annotations
 
-import org.jetbrains.annotations.ApiStatus;
-
-import java.lang.annotation.*;
+import org.jetbrains.annotations.ApiStatus
 
 /**
- * Methods annotated with {@code RequiresBlockingContext} are not designed to be called in suspend context
- * (where {@code currentCoroutineContext} is available).
+ * Methods annotated with `RequiresBlockingContext` are not designed to be called in suspend context
+ * (where [kotlinx.coroutines.currentCoroutineContext] is available).
  */
-@Documented
-@Retention(RetentionPolicy.SOURCE)
-@Target(ElementType.METHOD)
+@MustBeDocumented
+@Retention(AnnotationRetention.SOURCE)
+@Target(
+  AnnotationTarget.FUNCTION,
+  AnnotationTarget.PROPERTY_GETTER,
+  AnnotationTarget.PROPERTY_SETTER,
+)
 @ApiStatus.Experimental
-public @interface RequiresBlockingContext {
-}
+annotation class RequiresBlockingContext
