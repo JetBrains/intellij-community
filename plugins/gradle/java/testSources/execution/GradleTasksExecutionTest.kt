6c4b31c1740ac9de80c9d1ce64eb4630b251b2af
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2024-09-13 11:58:31 +0200

    [gradle][test][IDEA-359161] disabled the test for the deprecated Gradle feature
    GitOrigin-RevId: b3189ec783e7deb7a153d9e77f985376d94d09a7

--- plugins/gradle/java/testSources/execution/GradleTasksExecutionTest.kt
+++ plugins/gradle/java/testSources/execution/GradleTasksExecutionTest.kt
@@ -24,6 +24,7 @@ import org.jetbrains.plugins.gradle.importing.GradleImportingTestCase
 import org.jetbrains.plugins.gradle.importing.TestGradleBuildScriptBuilder
 import org.jetbrains.plugins.gradle.statistics.GradleTaskExecutionCollector
 import org.jetbrains.plugins.gradle.util.GradleConstants
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runners.Parameterized
 import java.util.concurrent.CompletableFuture
@@ -71,6 +72,7 @@ class GradleTasksExecutionTest : GradleImportingTestCase() {
   }
 
   @Test
+  @Ignore("IDEA-359161")
   fun `run task with specified build file test`() {
     createProjectSubFile("build.gradle", """
       task myTask() { doLast { print 'Hi!' } }
