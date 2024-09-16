3903bc12873e06225fb27a710e268b98f8a23f6d
Nikita.Ashihmin <nikita.ashihmin@jetbrains.com>
2024-09-17 02:48:43 +0400

    PY-75725 Packages(fix): disable update for several packages
    GitOrigin-RevId: 67067773eac05ce3eadf50696bf5e558ea37efbe

--- python/src/com/jetbrains/python/packaging/toolwindow/actions/UpdatePackageToLatestAction.kt
+++ python/src/com/jetbrains/python/packaging/toolwindow/actions/UpdatePackageToLatestAction.kt
@@ -8,6 +8,7 @@ import com.jetbrains.python.PyBundle
 import com.jetbrains.python.packaging.toolwindow.PyPackagingToolWindowService
 import com.jetbrains.python.packaging.toolwindow.model.InstalledPackage
 import com.jetbrains.python.packaging.toolwindow.ui.PyPackagesUiComponents.selectedPackage
+import com.jetbrains.python.packaging.toolwindow.ui.PyPackagesUiComponents.selectedPackages
 import com.jetbrains.python.packaging.utils.PyPackageCoroutine
 import kotlinx.coroutines.launch
 
@@ -25,7 +26,8 @@ internal class UpdatePackageToLatestAction : DumbAwareAction() {
   }
 
   override fun update(e: AnActionEvent) {
-    val pkg = e.selectedPackage as? InstalledPackage
+    val selectedPackage = e.selectedPackages.singleOrNull()
+    val pkg = selectedPackage as? InstalledPackage
 
     val currentVersion = pkg?.currentVersion?.presentableText
     val nextVersion = pkg?.nextVersion?.presentableText
