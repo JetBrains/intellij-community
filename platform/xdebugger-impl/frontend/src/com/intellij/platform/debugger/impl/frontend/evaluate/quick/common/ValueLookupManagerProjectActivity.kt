1cacca0ee0fa5ab04af61d4cc976fae5907b95bb
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2024-09-17 12:30:03 +0200

    [debugger-rd] IJPL-160146: Use service for subscription instead of project activity
    GitOrigin-RevId: a9da65049a4b2bf2a13e2dd8a0d4dc90fb553886

--- platform/xdebugger-impl/frontend/src/com/intellij/platform/debugger/impl/frontend/evaluate/quick/common/ValueLookupManagerProjectActivity.kt
+++ platform/xdebugger-impl/frontend/src/com/intellij/platform/debugger/impl/frontend/evaluate/quick/common/ValueLookupManagerProjectActivity.kt
@@ -1,21 +1,30 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.debugger.impl.frontend.evaluate.quick.common
 
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.coroutineScope
 
 private class ValueLookupManagerProjectActivity : ProjectActivity {
   override suspend fun execute(project: Project) {
-    coroutineScope {
-      val cs = this@coroutineScope
-      subscribeForDebuggingStart(cs, project) {
-        ValueLookupManager.getInstance(project).startListening()
-      }
+    // in production mode we can use coroutineScope { ... } here, since `execute` will be called canceled on project close,
+    // but it won't happen in tests mode, so we need to have a separate service for the subscription.
+    project.serviceAsync<ValueLookupManagerSubscriptionService>().subscribe()
+  }
+}
 
-      subscribeForValueHintHideRequest(cs, project) {
-        ValueLookupManager.getInstance(project).hideHint()
-      }
+@Service(Service.Level.PROJECT)
+private class ValueLookupManagerSubscriptionService(private val project: Project, private val cs: CoroutineScope) {
+  fun subscribe() {
+    subscribeForDebuggingStart(cs, project) {
+      ValueLookupManager.getInstance(project).startListening()
+    }
+
+    subscribeForValueHintHideRequest(cs, project) {
+      ValueLookupManager.getInstance(project).hideHint()
     }
   }
 }
