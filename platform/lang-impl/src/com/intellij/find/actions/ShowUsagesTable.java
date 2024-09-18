10bdb6f46b64b19cd182aadda3980a99e841c74c
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2024-09-18 11:02:32 +0300

    IJPL-162465 Do not dispose selected usages in Show Usages
    When the Show Usages popup is closed, the smart pointers
    contained in usage infos are disposed. Then, AFTER the popup
    is disposed, we navigate to the selected usage(s). But at that
    time it's already disposed, so some functionality may not work,
    e.g. WI-79031.
    
    Fix by maintaining a set of usage infos that must not be disposed
    in UsageViewImpl. We update this list along with the list of selected
    usages. Of course, it's not guaranteed that the selected usages
    will be used at all, and therefore they may never be disposed.
    That's OK, as according to SmartPointerManager.removePointer,
    disposing smart pointers isn't mandatory. And we're talking about
    a single pointer in the vast majority of cases here.
    And anyway, in the pre-regression implementations,
    ALL the selected usages would be copied every time selection
    changes, and were never disposed after that. So this fix is still
    better than that, and it doesn't rely on slow ops or the broken copy()
    implementation.
    
    GitOrigin-RevId: 751c9dc8b99d48874680cddef8b1b62bbc525975

--- platform/lang-impl/src/com/intellij/find/actions/ShowUsagesTable.java
+++ platform/lang-impl/src/com/intellij/find/actions/ShowUsagesTable.java
@@ -24,6 +24,7 @@ import com.intellij.usages.impl.*;
 import com.intellij.util.concurrency.AppExecutorUtil;
 import com.intellij.util.concurrency.ThreadingAssertions;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.containers.SmartHashSet;
 import com.intellij.util.ui.ColumnInfo;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.ListTableModel;
@@ -51,9 +52,9 @@ public final class ShowUsagesTable extends JBTable implements UiDataProvider {
   static final int MIN_COLUMN_WIDTH = 200;
 
   private final ShowUsagesTableCellRenderer myRenderer;
-  private final UsageView myUsageView;
+  private final UsageViewImpl myUsageView;
 
-  ShowUsagesTable(@NotNull ShowUsagesTableCellRenderer renderer, UsageView usageView) {
+  ShowUsagesTable(@NotNull ShowUsagesTableCellRenderer renderer, UsageViewImpl usageView) {
     myRenderer = renderer;
     myUsageView = usageView;
     ScrollingUtil.installActions(this);
@@ -112,6 +113,7 @@ public final class ShowUsagesTable extends JBTable implements UiDataProvider {
       moreUsagesSelected.set(false);
       filteredOutUsagesSelected.set(null);
       java.util.List<Object> usages = null;
+      var nonDisposableUsageInfos = new SmartHashSet<UsageInfo>();
       //todo List<Usage>
       for (int i : getSelectedRows()) {
         Object value = getValueAt(i, 0);
@@ -133,11 +135,25 @@ public final class ShowUsagesTable extends JBTable implements UiDataProvider {
             break;
           }
           if (usages == null) usages = new ArrayList<>();
-          usages.add(usage instanceof UsageInfo2UsageAdapter ? ((UsageInfo2UsageAdapter)usage).getUsageInfo() : usage);
+          if (usage instanceof UsageInfo2UsageAdapter adapter) {
+            var usageInfo = adapter.getUsageInfo();
+            usages.add(usageInfo);
+            nonDisposableUsageInfos.add(usageInfo);
+          }
+          else {
+            usages.add(usage);
+          }
         }
       }
 
       selectedUsages.set(usages);
+      // The callback below is called after the popup is disposed,
+      // and when it happens, it disposes all smart pointers.
+      // This prevents some functionality in navigateTo.
+      // Therefore, we need to preserve the selected usages smart pointers.
+      // According to com.intellij.psi.SmartPointerManager.removePointer,
+      // disposing isn't mandatory so there should be no leaks here.
+      myUsageView.setNonDisposableUsageInfos(nonDisposableUsageInfos);
     });
 
     return () -> {
