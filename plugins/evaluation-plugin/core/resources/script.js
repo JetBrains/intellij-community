c22b191956860098e7900f96e20052aff04d48ee
Alexey Kalina <alexey.kalina@jetbrains.com>
2024-09-19 19:06:33 +0200

    [evaluation-plugin] reuse visualization from code generation in test generation evaluation (LME-102)
    (cherry picked from commit c374b492a84cdc7d3fdef3dcca16bfc7865efc1d)
    
    IJ-MR-145189
    
    GitOrigin-RevId: 3d4f4ea6ca9243cafdd8a7aaa1cff0f8683bc346

--- plugins/evaluation-plugin/core/resources/script.js
+++ plugins/evaluation-plugin/core/resources/script.js
@@ -142,7 +142,10 @@ function updatePopup(sessionDiv) {
   }
   else {
     if (isCodeGeneration) {
-      addDiffView(sessionDiv, popup, lookup, codeElement.innerText);
+      const originalCode  = "aia_original_content" in lookup["additionalInfo"]
+        ? lookup["additionalInfo"]["aia_original_content"]
+        : codeElement.innerText
+      addDiffView(sessionDiv, popup, lookup, originalCode)
     } else {
       addSuggestions(sessionDiv, popup, lookup);
     }
