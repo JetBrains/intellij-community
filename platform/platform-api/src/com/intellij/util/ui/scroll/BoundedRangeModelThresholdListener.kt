6d2ec4fe3a4d312cd295709a2313ddc6d6f4516e
Vedana.Voronina <vedana.voronina@jetbrains.com>
2024-09-19 16:38:59 +0000

    [collab/space] IJ-CR-145088 IDEA-356878: replace listReviewsV2 with listReviewsV3, fix multiplying load requests on list scroll
    Merge-request: IJ-MR-141225
    Merged-by: Veda Voronina <Vedana.Voronina@jetbrains.com>
    
    
    Merge-request: IJ-MR-145133
    Merged-by: Veda Voronina <Vedana.Voronina@jetbrains.com>
    
    GitOrigin-RevId: a085be30936c96c97b7bf7dbfad93794ad37be58

--- platform/platform-api/src/com/intellij/util/ui/scroll/BoundedRangeModelThresholdListener.kt
+++ platform/platform-api/src/com/intellij/util/ui/scroll/BoundedRangeModelThresholdListener.kt
@@ -6,6 +6,12 @@ import javax.swing.JScrollBar
 import javax.swing.event.ChangeEvent
 import javax.swing.event.ChangeListener
 
+/**
+ * Note: Use with caution as for large models the threshold set as a percent value will require more requests for data with each scroll.
+ * E.g., list containing 1000 elements with a threshold set to 0.7 when reaching the bottom of the scroll,
+ * will request data loading until total elements count reaches 1430 elements.
+ * Which for request with batch size of 30 elements is 15 requests.
+ */
 abstract class BoundedRangeModelThresholdListener(
   private val model: BoundedRangeModel,
   private val thresholdPercent: Float
