574869f3ea7d9848d63ec466cf5be8406109cee8
Egor Ushakov <egor.ushakov@jetbrains.com>
2024-09-19 14:40:48 +0200

    IDEA-358233 Evaluate expression with Stream.map when using a protected field under a lambda expression throws `$GeneratedEvaluationClass tried to access protected field`, `is in unnamed module of loader java.security.SecureClassLoader`
    GitOrigin-RevId: 6532c4a65dde0edf8a091b32b735f1724599fef7

--- java/java-impl-refactorings/src/com/intellij/refactoring/extractMethodObject/ExtractLightMethodObjectHandler.java
+++ java/java-impl-refactorings/src/com/intellij/refactoring/extractMethodObject/ExtractLightMethodObjectHandler.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.refactoring.extractMethodObject;
 
 import com.intellij.codeInsight.CodeInsightUtil;
@@ -273,6 +273,7 @@ public final class ExtractLightMethodObjectHandler {
             result[0] = expression;
           }
         }
+        super.visitMethodCallExpression(expression);
       }
     });
     return result[0];
