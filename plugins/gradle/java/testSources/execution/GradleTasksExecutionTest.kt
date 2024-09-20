39d31f3e18e588627ec10baa25ddc25bfb779707
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2024-09-20 14:32:41 +0200

    [gradle][test][IDEA-359126] Use TargetVersions to limit expected versions of Gradle instead of disabling the test
    GitOrigin-RevId: 6cd7dcbcc78539eb1f61393e96bc5b2a27b17f1d

--- plugins/gradle/java/testSources/execution/GradleTasksExecutionTest.kt
+++ plugins/gradle/java/testSources/execution/GradleTasksExecutionTest.kt
@@ -23,8 +23,8 @@ import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.plugins.gradle.importing.GradleImportingTestCase
 import org.jetbrains.plugins.gradle.importing.TestGradleBuildScriptBuilder
 import org.jetbrains.plugins.gradle.statistics.GradleTaskExecutionCollector
+import org.jetbrains.plugins.gradle.tooling.annotation.TargetVersions
 import org.jetbrains.plugins.gradle.util.GradleConstants
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runners.Parameterized
 import java.util.concurrent.CompletableFuture
@@ -72,7 +72,7 @@ class GradleTasksExecutionTest : GradleImportingTestCase() {
   }
 
   @Test
-  @Ignore("IDEA-359161")
+  @TargetVersions("<7.6")
   fun `run task with specified build file test`() {
     createProjectSubFile("build.gradle", """
       task myTask() { doLast { print 'Hi!' } }
