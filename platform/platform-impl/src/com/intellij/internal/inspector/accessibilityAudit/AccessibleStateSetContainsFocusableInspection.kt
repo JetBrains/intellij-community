9ad4682c8449170999fe8977bb3acd302a9a5dde
Valentin.Dorofeev <valentin.dorofeev@jetbrains.com>
2024-09-20 00:02:53 +0000

    IJPL-162309 UI Inspector: add new inspection that checks focusable property
    IJPL-162309 UI Inspector: add new inspection that checks focusable property
    
    IJPL-162309 UI Inspector: add new inspection that checks focusable property
    
    
    Merge-request: IJ-MR-144834
    Merged-by: Valentin Dorofeev <valentin.dorofeev@jetbrains.com>
    
    GitOrigin-RevId: bf3d827ed32c87d3901a9184e17b67201a4c0cec

---
+++ platform/platform-impl/src/com/intellij/internal/inspector/accessibilityAudit/AccessibleStateSetContainsFocusableInspection.kt
@@ -0,0 +1,35 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.internal.inspector.accessibilityAudit
+
+import org.jetbrains.annotations.ApiStatus
+import javax.accessibility.AccessibleContext
+import javax.accessibility.AccessibleRole
+import javax.accessibility.AccessibleState
+
+@ApiStatus.Internal
+@ApiStatus.Experimental
+class AccessibleStateSetContainsFocusableInspection : UiInspectorAccessibilityInspection {
+  override fun passesInspection(context: AccessibleContext): Boolean {
+    if ((context.accessibleStateSet.contains(AccessibleState.ENABLED) &&
+         context.accessibleStateSet.contains(AccessibleState.VISIBLE) &&
+         context.accessibleStateSet.contains(AccessibleState.SHOWING)) &&
+        context.accessibleRole in arrayOf(AccessibleRole.CHECK_BOX,
+                                          AccessibleRole.PUSH_BUTTON,
+                                          AccessibleRole.RADIO_BUTTON,
+                                          AccessibleRole.TOGGLE_BUTTON,
+                                          AccessibleRole.LIST,
+                                          AccessibleRole.LIST_ITEM,
+                                          AccessibleRole.TABLE,
+                                          AccessibleRole.TREE,
+                                          AccessibleRole.PAGE_TAB_LIST,
+                                          AccessibleRole.TEXT,
+                                          AccessibleRole.PASSWORD_TEXT,
+                                          AccessibleRole.HYPERLINK,
+                                          AccessibleRole.POPUP_MENU,
+                                          AccessibleRole.SLIDER)) {
+      return context.accessibleStateSet.contains(AccessibleState.FOCUSABLE)
+    }
+    return true
+  }
+}
+
---
+++ platform/platform-tests/testSrc/com/intellij/internal/inspector/accessibilityAudit/AccessibleStateSetContainsFocusableInspectionTest.kt
@@ -0,0 +1,66 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.internal.inspector.accessibilityAudit
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import javax.accessibility.AccessibleContext
+import javax.accessibility.AccessibleRole
+import javax.swing.JButton
+import javax.swing.JComponent
+import javax.swing.JProgressBar
+
+class AccessibleStateSetContainsFocusableInspectionTest {
+
+  @Test
+  fun `valid role and no focusable in state set`() {
+    val button = object : JButton() {
+      override fun isShowing(): Boolean {
+        return true
+      }
+
+      override fun isVisible(): Boolean {
+        return true
+      }
+
+      override fun isEnabled(): Boolean {
+        return true
+      }
+    }
+
+    button.isFocusable = false
+    val result = AccessibleStateSetContainsFocusableInspection().passesInspection(button.accessibleContext)
+
+    Assertions.assertFalse(result)
+  }
+
+  @Test
+  fun `valid role and focusable in state set`() {
+    val button = object : JButton() {
+      override fun isShowing(): Boolean {
+        return true
+      }
+
+      override fun isVisible(): Boolean {
+        return true
+      }
+
+      override fun isEnabled(): Boolean {
+        return true
+      }
+    }
+
+    button.isFocusable = true
+    val result = AccessibleStateSetContainsFocusableInspection().passesInspection(button.accessibleContext)
+
+    Assertions.assertTrue(result)
+  }
+
+  @Test
+  fun `invalid role`() {
+    val bar = JProgressBar()
+    val result = AccessibleStateSetContainsFocusableInspection().passesInspection(bar.accessibleContext)
+
+    Assertions.assertTrue(result)
+  }
+
+}
