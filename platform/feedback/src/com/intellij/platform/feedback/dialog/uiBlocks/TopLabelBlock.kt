51006ab168d788ad55a06e6c04041545db2e6222
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2024-09-22 14:56:15 +0200

    Survey components changes.
    Refactored the TopLabelBlock added H2LabelBlock, H3LabelBlock, and H4LabelBlock.
    Added support for an "Other" text field in RadioButtonGroupBlock
    Added customizable placeholder text for other field in RadioButtonGroupBlock adn CheckBoxGroupBlock.
    
    GitOrigin-RevId: 5ea1534c0f7155802921bd16e87920d3b969b03a

--- platform/feedback/src/com/intellij/platform/feedback/dialog/uiBlocks/TopLabelBlock.kt
+++ platform/feedback/src/com/intellij/platform/feedback/dialog/uiBlocks/TopLabelBlock.kt
@@ -5,17 +5,20 @@ import com.intellij.openapi.util.NlsContexts
 import com.intellij.ui.dsl.builder.BottomGap
 import com.intellij.ui.dsl.builder.Panel
 import com.intellij.util.ui.JBFont
+import java.awt.Font
 
-class TopLabelBlock(@NlsContexts.Label private val myText: String) : FeedbackBlock, TextDescriptionProvider {
+
+abstract class LabelBlock(@NlsContexts.Label private val myText: String) : FeedbackBlock, TextDescriptionProvider {
 
   private var bottomGap: BottomGap? = null
+
 
   override fun addToPanel(panel: Panel) {
     panel.apply {
       row {
         label(myText)
           .applyToComponent {
-            font = JBFont.h1()
+            font = getLableFont()
           }
       }.apply {
         if (bottomGap != null) {
@@ -32,8 +35,27 @@ class TopLabelBlock(@NlsContexts.Label private val myText: String) : FeedbackBlo
     }
   }
 
-  fun setBottomGap(bottomGap: BottomGap): TopLabelBlock {
+  fun setBottomGap(bottomGap: BottomGap): LabelBlock {
     this.bottomGap = bottomGap
     return this
   }
+
+  abstract fun getLableFont(): Font?
+
+}
+
+class TopLabelBlock(@NlsContexts.Label private val myText: String) : LabelBlock(myText) {
+  override fun getLableFont(): Font? = JBFont.h1()
+}
+
+class H2LabelBlock(@NlsContexts.Label private val myText: String) : LabelBlock(myText) {
+  override fun getLableFont(): Font? = JBFont.h2()
+}
+
+class H3LabelBlock(@NlsContexts.Label private val myText: String) : LabelBlock(myText) {
+  override fun getLableFont(): Font? = JBFont.h3()
+}
+
+class H4LabelBlock(@NlsContexts.Label private val myText: String) : LabelBlock(myText) {
+  override fun getLableFont(): Font? = JBFont.h4()
 }
