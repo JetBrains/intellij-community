e38a9fc6590ae816fa4b021557ec8847b829ee6b
Nikolay Egorov <nikolay.egorov@jetbrains.com>
2024-09-23 11:18:19 +0300

    [kotlin] Gather additional dependencies for scripts which may have non-standard dependency structure
    GitOrigin-RevId: d12de243633e98e0d354957ac258dcabfa807663

--- plugins/kotlin/base/scripting/src/org/jetbrains/kotlin/idea/base/scripting/projectStructure/ScriptModuleInfo.kt
+++ plugins/kotlin/base/scripting/src/org/jetbrains/kotlin/idea/base/scripting/projectStructure/ScriptModuleInfo.kt
@@ -2,17 +2,13 @@
 package org.jetbrains.kotlin.idea.base.scripting.projectStructure
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.OrderEnumerator
-import com.intellij.openapi.roots.impl.libraries.LibraryEx
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.backend.workspace.WorkspaceModel
 import com.intellij.platform.backend.workspace.toVirtualFileUrl
 import com.intellij.platform.workspace.jps.entities.LibraryDependency
-import com.intellij.platform.workspace.jps.entities.LibraryId
 import com.intellij.platform.workspace.jps.entities.ModuleEntity
 import com.intellij.platform.workspace.storage.EntityStorage
 import com.intellij.platform.workspace.storage.WorkspaceEntity
-import com.intellij.platform.workspace.storage.impl.cache.cache
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.workspaceModel.ide.impl.legacyBridge.library.ProjectLibraryTableBridgeImpl.Companion.libraryMap
 import com.intellij.workspaceModel.ide.impl.legacyBridge.module.findModule
@@ -85,16 +81,16 @@ data class ScriptModuleInfo(
 
     private val _dependencies: List<IdeaModuleInfo> by lazy {
         mutableSetOf<IdeaModuleInfo>(this).apply {
+            val scriptDependentLibraries = ScriptAdditionalIdeaDependenciesProvider.getRelatedLibraries(scriptFile, project)
+            val libraryInfoCache = LibraryInfoCache.getInstance(project)
+            scriptDependentLibraries.forEach {
+                addAll(libraryInfoCache[it])
+            }
+
             if (KotlinPluginModeProvider.isK1Mode()) {
                 val scriptDependentModules = ScriptAdditionalIdeaDependenciesProvider.getRelatedModules(scriptFile, project)
                 scriptDependentModules.forEach {
                     addAll(it.sourceModuleInfos)
-                }
-
-                val scriptDependentLibraries = ScriptAdditionalIdeaDependenciesProvider.getRelatedLibraries(scriptFile, project)
-                val libraryInfoCache = LibraryInfoCache.getInstance(project)
-                scriptDependentLibraries.forEach {
-                    addAll(libraryInfoCache[it])
                 }
 
                 val dependenciesInfo = ScriptDependenciesInfo.ForFile(project, scriptFile, scriptDefinition)
