d8edf9e902441c49e9617592acc1bcb382a4e658
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2024-09-23 19:05:40 +0200

    Python remove redundant classes
    GitOrigin-RevId: 05a6480b47113f21416c46db043ab88581202ba6

--- python/src/com/jetbrains/python/configuration/PythonInterpreterDetailsConfigurable.kt
+++ python/src/com/jetbrains/python/configuration/PythonInterpreterDetailsConfigurable.kt
@@ -26,9 +26,6 @@ class PythonInterpreterDetailsConfigurable(project: Project,
 
   override fun isModified(): Boolean = initialSdkName != currentSdkName || underlyingConfigurable.isModified
 
-  /**
-   * TODO Shouldn't be here the association updated (see [com.jetbrains.python.configuration.PythonSdkDetailsDialog.editSdk]).
-   */
   override fun apply() {
     underlyingConfigurable.apply()
 
