acdd8498f71d7354fc1a158e05215f561a81fef7
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2024-09-24 21:06:03 +0200

    IJPL-161026 revert default authenticator behaviour
    it now returns any known credentials by default, as it did before in <24.2
    
    GitOrigin-RevId: ed6fff3480d8c626ebfbfb56527c301482027f8e

--- platform/platform-api/src/com/intellij/util/net/IdeProxyAuthenticator.kt
+++ platform/platform-api/src/com/intellij/util/net/IdeProxyAuthenticator.kt
@@ -7,36 +7,36 @@ import com.intellij.openapi.diagnostic.logger
 import java.net.Authenticator
 import java.net.PasswordAuthentication
 
+
+/**
+ * **If someone decides to change this implementation, keep this in mind**
+ * * For _basic_ authentication scheme JDK internals cache successful proxy authentications against the authenticator instance
+ * (or against null if the default authenticator is used). So for later requests cached credentials will be used by JDK automatically without
+ * calling [getPasswordAuthentication] again. Authentication cache is dropped if the proxy returns 407 and is re-requested again.
+ * * For _SOCKS_, if auth is required, [getPasswordAuthentication] is called for every connection: [java.net.SocksSocketImpl.authenticate].
+ * * Authentication cache that works for _basic_ scheme does not work for _NTLM_ or _Kerberos_. The observed behavior is that
+ * [getPasswordAuthentication] is called for every connection. Probably this happens due to multistage auth.
+ *
+ * This has the following consequences:
+ * It is not possible by the means of [Authenticator] API to distinguish the reason of authentication request in general case:
+ * is it because previous credentials that were valid turned invalid and we are called second+ time for the same original request,
+ * or because the underlying infrastructure just calls [getPasswordAuthentication] for every connection?
+ * In turn, it means that in general we can't really tell if we need to ask the user to provide new credentials (maybe they made a typo in password).
+ *
+ * It does not seem possible to workaround this issue robustly on [Authenticator] level.
+ *
+ * That said, the current implementation returns any already known credentials for every request.
+ * The only help the user can get is the "Check connection" button in the proxy settings page...
+ *
+ * @see ProxyAuthentication
+ */
 class IdeProxyAuthenticator(
   private val proxyAuth: ProxyAuthentication,
 ): Authenticator() {
-  /**
-   * JDK internals cache successful proxy authentications against the authenticator instance (or against null if the default authenticator is used).
-   * Authentication cache is dropped if the proxy returns 407 and is re-requested again.
-   * We want to achieve two goals here:
-   * 1. if user has set the "remember" flag for credentials, they should be reused in the next IDE run;
-   * 2. if the credentials do not suit proxy anymore, we must prompt the user again.
-   * To fulfill the first point, the naive solution would be to provide remembered credentials once and then always prompt the user on every repeated request
-   * (given the caching nature of the jdk's authenticator mechanism). But! If two threads simultaneously perform an http request through a proxy, JDK
-   * will call this method twice since at the point of the request there is no auth data for both requests. We have to distinguish a repeated
-   * auth request due to invalid credentials from just a concurrent request. And we have quite limited sources to do that. Two threads
-   * may request the same URL, so the whole input of this method will be the same for both threads. I guess the only differentiator we can
-   * have here is the thread itself, so a thread local is used.
-   * Hash of the resulting credentials is used too, because consider the following scenario:
-   * 1. both threads start the requests, both first don't use the proxy credentials, both get 407
-   * 2. each of them calls this authenticator, it runs twice and returns known credentials both times
-   * 3. both threads retry the requests, fail again with 407, ask this authenticator again
-   * 4. we now prompt the user for credentials in the first thread
-   * 5. the second thread comes and should reuse the prompt result of the first thread. So it should react to the fact that the credentials
-   *    that were used before are already known to be wrong, and there are other available => no need to prompt again
-   *
-   * NB: there is a JDK internal property: "http.auth.serializeRequests", see [sun.net.www.protocol.http.AuthenticationInfo.serializeAuth]
-   */
-  private val lastRequest = ThreadLocal<String>()
 
   @Synchronized
   override fun getPasswordAuthentication(): PasswordAuthentication? {
-    logger.debug { "$this on ${Thread.currentThread()}, lastRequest=${lastRequest.get()}" }
+    logger.debug { "$this on ${Thread.currentThread()}" }
     // java.base/java/net/SocksSocketImpl.java:176 : there is SOCKS proxy auth, but without RequestorType passing
     val isProxy = RequestorType.PROXY == requestorType || "SOCKS authentication" == requestingPrompt
     if (!isProxy) {
@@ -44,34 +44,8 @@ class IdeProxyAuthenticator(
     }
     val host = getHostNameReliably(requestingHost, requestingSite, requestingURL) ?: "" // TODO remove requestingURL, it is not relevant for proxy _auth_
     val port = requestingPort
-
-    if (requestingScheme.equals("ntlm", ignoreCase = true) ||
-        requestingScheme.equals("Kerberos", ignoreCase = true) ||
-        requestingScheme.equals("Negotiate", ignoreCase = true)) {
-      /**
-       * Always return known credentials for these schemes. Why:
-       * It turns out that the claim about JDK caching valid credentials is not precise ([lastRequest]). For some reason, in case
-       * NTLM or Kerberos auth schemes are used, JDK _may_ ask the authenticator again for credentials, even though the last credentials are
-       * still valid. Probably it happens due to a multistage auth scheme.
-       * If we go the "normal" path instead, we'll conflict with [lastRequest] logic and ask the user credentials again if someone requests
-       * same url twice in a row on current thread.
-       * This is actually the logic of the "old" authenticator implementation ([IdeaWideAuthenticator]).
-       * @see sun.net.www.protocol.http.HttpURLConnection.getHttpProxyAuthentication
-       */
-      return proxyAuth.getOrPromptAuthentication(requestingPrompt, host, port)?.toPasswordAuthentication()
-    }
-
-    var credentials = proxyAuth.getKnownAuthentication(host, port)
-    var key = getKey(credentials)
-    if (credentials == null || lastRequest.get() == key) {
-      credentials = proxyAuth.getPromptedAuthentication(requestingPrompt, host, port)
-      key = getKey(credentials)
-    }
-    lastRequest.set(key)
-    return credentials?.toPasswordAuthentication()
+    return proxyAuth.getOrPromptAuthentication(requestingPrompt, host, port)?.toPasswordAuthentication()
   }
-
-  private fun getKey(credentials: Credentials?): String = "$this;c=${credentials.hash()}"
 
   override fun toString(): String = "Auth@${System.identityHashCode(this).toString(16)}{" +
                                     "type=$requestorType, scheme=$requestingScheme, protocol=$requestingProtocol, " +
@@ -84,13 +58,6 @@ class IdeProxyAuthenticator(
     private fun Credentials.toPasswordAuthentication(): PasswordAuthentication? {
       if (userName == null) return null
       return PasswordAuthentication(userName!!, password?.toCharArray() ?: CharArray(0))
-    }
-
-    private fun Credentials?.hash(): Int {
-      if (this == null) return 0
-      var hash = 1 + (userName?.hashCode() ?: 0)
-      hash = hash * 31 + (password?.hashCode() ?: 0)
-      return hash
     }
   }
 }
