3ec0bd8173c9fc9b23746beff122e48bcfea25f4
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2024-09-24 11:12:12 +0200

    [maven]  add connectors api test
    GitOrigin-RevId: fb4da9d4cf915791bb3b6e66f803d92ad9e36199

---
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/connectors/MavenConnectorApiTest.kt
@@ -0,0 +1,110 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.idea.maven.connectors
+
+import com.intellij.maven.testFramework.MavenMultiVersionImportingTestCase
+import com.intellij.platform.util.progress.RawProgressReporter
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.idea.maven.buildtool.MavenLogEventHandler
+import org.jetbrains.idea.maven.model.MavenExplicitProfiles
+import org.jetbrains.idea.maven.model.MavenId
+import org.jetbrains.idea.maven.model.MavenWorkspaceMap
+import org.jetbrains.idea.maven.server.MavenServerManager
+import org.junit.Test
+import java.util.*
+
+class MavenConnectorApiTest : MavenMultiVersionImportingTestCase() {
+
+
+  override fun setUp() {
+    super.setUp()
+    projectsManager.initForTests()
+    runBlocking {
+      importProjectAsync("""  <groupId>test</groupId>
+                       <artifactId>project</artifactId>
+                       <packaging>pom</packaging>
+                       <version>1</version>""")
+    }
+
+  }
+
+  @Test
+  fun testResolveProjectsWithProfiles() = runBlocking {
+    needFixForMaven4()
+    assumeVersionMoreThan("3.1.0")
+    val project = createProjectPom("""
+                       <groupId>test</groupId>
+                       <artifactId>project</artifactId>
+                       <packaging>pom</packaging>
+                       <version>1</version>
+                       <modules>
+                         <module>m1</module>
+                         <module>m2</module>
+                       </modules>
+                       """.trimIndent())
+
+    val m1 = createModulePom("m1", """
+      <groupId>test</groupId>
+      <artifactId>m1</artifactId>
+      <version>1</version>
+      <profiles>
+        <profile>
+          <id>test</id>
+          <dependencies>
+            <dependency>
+              <groupId>junit</groupId>
+              <artifactId>junit</artifactId>
+              <version>4.0</version>
+            </dependency>
+          </dependencies>
+        </profile>
+      </profiles>
+      """.trimIndent())
+
+    val m2 = createModulePom("m2", """
+      <groupId>test</groupId>
+      <artifactId>m2</artifactId>
+      <version>1</version>
+      <dependencies>
+        <dependency>
+          <groupId>test</groupId>
+          <artifactId>m1</artifactId>
+          <version>1</version>
+        </dependency>
+      </dependencies>
+      """.trimIndent())
+    val embedder = getEmbedder()
+    val map = MavenWorkspaceMap()
+    map.register(MavenId("test:m1:1"), m1.toNioPath().toFile())
+    map.register(MavenId("test:m2:1"), m2.toNioPath().toFile())
+    map.register(MavenId("test:project:1"), project.toNioPath().toFile())
+    val executionResults = embedder.resolveProject(mapOf(project to null, m1 to null, m2 to null),
+                                                   MavenExplicitProfiles(listOf("test"), emptyList()),
+                                                   MockReporter(),
+                                                   MavenLogEventHandler,
+                                                   map,
+                                                   false,
+                                                   Properties())
+
+    assertEquals(3, executionResults.size)
+    val results = executionResults.associateBy { it.projectData!!.mavenModel.mavenId.toString() }
+    val depsOfM1 = results["test:m1:1"]!!.projectData!!.mavenModel.dependencies
+    assertOrderedEquals(depsOfM1.map { it.mavenId.toString() }, "junit:junit:4.0")
+
+    val depsOfM2 = results["test:m2:1"]!!.projectData!!.mavenModel.dependencies
+    assertOrderedEquals(depsOfM2.map { it.mavenId.toString() }, "test:m1:1", "junit:junit:4.0")
+
+  }
+
+  private suspend fun getConnector() =
+    MavenServerManager.getInstance().getConnector(project, projectPath)
+
+  private suspend fun getEmbedder() =
+    MavenServerManager.getInstance().createEmbedder(project, true, projectPath)
+}
+
+private class MockReporter : RawProgressReporter {
+  val out = StringBuilder()
+  override fun text(text: String?) {
+    text?.let { out.append(it) }
+  }
+}
