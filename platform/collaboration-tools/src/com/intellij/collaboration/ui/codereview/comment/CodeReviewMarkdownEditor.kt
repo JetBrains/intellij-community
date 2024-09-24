0530379f617f4d83bc8fb66afe606eb45c024045
Alexander Zamyshlyaev <alexander.zamyshlyaev@jetbrains.com>
2024-09-24 12:55:29 +0300

    [collab/space] IDEA-358221: Lower inspections level for injected code in Markdown editors
    GitOrigin-RevId: d7c1f400d40ff16e85c39b45f06aede97c2db3de

--- platform/collaboration-tools/src/com/intellij/collaboration/ui/codereview/comment/CodeReviewMarkdownEditor.kt
+++ platform/collaboration-tools/src/com/intellij/collaboration/ui/codereview/comment/CodeReviewMarkdownEditor.kt
@@ -1,19 +1,26 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.collaboration.ui.codereview.comment
 
+import com.intellij.codeInsight.daemon.impl.analysis.FileHighlightingSetting.ESSENTIAL
+import com.intellij.codeInsight.daemon.impl.analysis.HighlightLevelUtil
 import com.intellij.ide.ui.laf.darcula.DarculaNewUIUtil
 import com.intellij.ide.ui.laf.darcula.DarculaUIUtil
+import com.intellij.lang.injection.InjectedLanguageManager
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.actions.IncrementalFindAction
+import com.intellij.openapi.editor.event.BulkAwareDocumentListener
+import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.fileEditor.impl.zoomIndicator.ZoomIndicatorManager
 import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.fileTypes.FileTypes
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ErrorBorderCapable
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFileFactory
 import com.intellij.ui.EditorTextField
@@ -28,7 +35,12 @@ import javax.swing.JComponent
 import javax.swing.border.Border
 
 internal object CodeReviewMarkdownEditor {
+  private val INJECTION_PROCESSED = Key.create<Boolean>("CODEREVIEW_INJECTION_PROCESSED")
+
   fun create(project: Project, inline: Boolean = false, oneLine: Boolean = false): Editor {
+    val injectedLanguageManager = InjectedLanguageManager.getInstance(project)
+    val psiDocumentManager = PsiDocumentManager.getInstance(project)
+
     // setup markdown only if plugin is enabled
     val fileType = FileTypeRegistry.getInstance().getFileTypeByExtension("md").takeIf { it != FileTypes.UNKNOWN } ?: FileTypes.PLAIN_TEXT
     val psiFile = PsiFileFactory.getInstance(project)
@@ -36,8 +48,20 @@ internal object CodeReviewMarkdownEditor {
 
     val editorFactory = EditorFactory.getInstance()
     val document = ReadAction.compute<Document, Throwable> {
-      PsiDocumentManager.getInstance(project).getDocument(psiFile)
+      psiDocumentManager.getDocument(psiFile)
     } ?: editorFactory.createDocument("")
+
+    document.addDocumentListener(object : BulkAwareDocumentListener.Simple {
+      override fun documentChangedNonBulk(event: DocumentEvent) {
+        injectedLanguageManager.getCachedInjectedDocumentsInRange(psiFile, TextRange(event.offset, event.offset + event.newLength))
+          .forEach { doc ->
+            if (doc.getUserData(INJECTION_PROCESSED) == true) return@forEach
+            val injectedPsi = psiDocumentManager.getCachedPsiFile(doc) ?: return@forEach
+            HighlightLevelUtil.forceRootHighlighting(injectedPsi, ESSENTIAL)
+            doc.putUserData(INJECTION_PROCESSED, true)
+          }
+      }
+    })
 
     return (editorFactory.createEditor(document, project, fileType, false) as EditorEx).also {
       EditorTextField.setupTextFieldEditor(it)
