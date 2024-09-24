cd9417370ae36efe5ecea38e90016443c073d5ff
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2024-09-24 12:52:49 +0200

    [java-highlighting] IDEA-358686 Moving file from outside of source roots doesn't help to remove banner
    - add listeners to check moved files
    
    GitOrigin-RevId: fcf12908e34d62b981c38ff7ed2469da712e42fc

--- java/java-impl/src/com/intellij/ide/FileNotInSourceRootChecker.kt
+++ java/java-impl/src/com/intellij/ide/FileNotInSourceRootChecker.kt
@@ -10,20 +10,27 @@ import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.java.JavaBundle
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.application.readAction
+import com.intellij.openapi.application.writeAction
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorFactory
-import com.intellij.openapi.editor.event.EditorFactoryEvent
-import com.intellij.openapi.editor.event.EditorFactoryListener
+import com.intellij.openapi.editor.impl.EditorImpl
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.FileEditorManagerListener
+import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.roots.ui.configuration.DefaultModulesProvider
 import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileListener
+import com.intellij.openapi.vfs.VirtualFileMoveEvent
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiJavaFile
@@ -45,17 +52,21 @@ private const val JAVA_DONT_CHECK_OUT_OF_SOURCE_FILES: String = "com.intellij.id
 @Service(Service.Level.PROJECT)
 private class FileNotInSourceRootService(
   private val project: Project,
-  private val coroutineScope: CoroutineScope
+  private val coroutineScope: CoroutineScope,
 ) : Disposable {
   fun init() {
     if (PropertiesComponent.getInstance(project).getBoolean(JAVA_DONT_CHECK_OUT_OF_SOURCE_FILES, false)) return
-    val editorFactory = EditorFactory.getInstance()
-    editorFactory.addEditorFactoryListener(object : EditorFactoryListener {
-      override fun editorCreated(event: EditorFactoryEvent) {
-        checkEditor(event.editor)
+    val messageBus = ApplicationManager.getApplication().messageBus
+    messageBus.connect(this).subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, object : FileEditorManagerListener {
+      override fun fileOpened(source: FileEditorManager, file: VirtualFile) {
+        for (ed in EditorFactory.getInstance().allEditors) {
+          if (ed.virtualFile == file)
+            checkEditor(ed)
+        }
       }
-    }, this)
-    editorFactory.editorList.forEach { checkEditor(it) }
+    })
+
+    EditorFactory.getInstance().editorList.forEach { checkEditor(it) }
   }
 
   private fun checkEditor(editor: Editor) {
@@ -70,9 +81,32 @@ private class FileNotInSourceRootService(
 
       if (infoAndFile != null) {
         withContext(Dispatchers.EDT) {
-          if (!infoAndFile.second.isValid) return@withContext
+          writeAction {
+            val psiFile = infoAndFile.second
+            if (!psiFile.isValid) return@writeAction
+            if (editor is EditorImpl) {
+              val listener = object : VirtualFileListener {
+                override fun fileMoved(event: VirtualFileMoveEvent) {
+                  val file = event.file
+                  if (file == psiFile.virtualFile) {
+                    file.fileSystem.removeVirtualFileListener(this)
+                    if (editor.isDisposed) return
+                    DaemonCodeAnalyzerEx.getInstanceEx(project).cleanFileLevelHighlights(GROUP, psiFile)
+                    checkEditor(editor)
+                  }
+                }
+              }
+              val fileSystem = psiFile.virtualFile.fileSystem
+              fileSystem.addVirtualFileListener(listener)
+              for (fileEditor in FileEditorManager.getInstance(project).getEditors(psiFile.virtualFile)) {
+                if (fileEditor is TextEditor && fileEditor.editor == editor) {
+                  Disposer.register(fileEditor) { fileSystem.removeVirtualFileListener(listener) }
+                }
+              }
+            }
 
-          DaemonCodeAnalyzerEx.getInstanceEx(project).addFileLevelHighlight(GROUP, infoAndFile.first, infoAndFile.second, null)
+            DaemonCodeAnalyzerEx.getInstanceEx(project).addFileLevelHighlight(GROUP, infoAndFile.first, infoAndFile.second, null)
+          }
         }
       }
     }
@@ -88,6 +122,7 @@ private class FileNotInSourceRootService(
     if (fileIndex.getOrderEntriesForFile(virtualFile).isNotEmpty()) return null
 
     val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.document) as? PsiJavaFile ?: return null
+    if (DaemonCodeAnalyzerEx.getInstanceEx(project).hasFileLevelHighlights(GROUP, psiFile)) return null
     val packageName = psiFile.packageName
     val module = fileIndex.getModuleForFile(virtualFile) ?: return null
     val rootModel = DefaultModulesProvider.createForProject(project).getRootModel(module)
@@ -116,10 +151,10 @@ private class FileNotInSourceRootService(
   }
 
   // Separate fix to differentiate in statistics  
-  class MoveFileToSourceRootFix(virtualFile: VirtualFile, root: VirtualFile): 
+  class MoveFileToSourceRootFix(virtualFile: VirtualFile, root: VirtualFile) :
     MoveFileFix(virtualFile, root, JavaBundle.message("fix.move.to.source.root"))
 
-  class DismissFix: IntentionAction {
+  class DismissFix : IntentionAction {
     override fun startInWriteAction(): Boolean = false
     override fun getText(): String = familyName
     override fun getFamilyName(): String = JavaBundle.message("intention.family.name.dismiss")
@@ -131,7 +166,7 @@ private class FileNotInSourceRootService(
     }
   }
 
-  class IgnoreForThisProjectFix: IntentionAction {
+  class IgnoreForThisProjectFix : IntentionAction {
     override fun startInWriteAction(): Boolean = false
     override fun getText(): String = familyName
     override fun getFamilyName(): String = JavaBundle.message("intention.family.name.ignore.project")
