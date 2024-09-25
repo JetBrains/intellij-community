a2ca3fd2e3352f177838fa79a94f2020e3d17fdf
Marat Dinmukhametov <marat.dinmukhametov@jetbrains.com>
2024-09-25 20:46:59 +0300

    [kotlin] IDEA-353900 k2: ShortenReference for uast
    This is necessary for the Reactive-streams plugin, which uses shortening
    
    GitOrigin-RevId: fae6588709134846da0c279e372f59505c7530e0

--- plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/FirKotlinUastCodeGenerationPlugin.kt
+++ plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/FirKotlinUastCodeGenerationPlugin.kt
@@ -2,18 +2,13 @@
 package org.jetbrains.uast.kotlin
 
 import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.idea.base.analysis.api.utils.shortenReferences
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.uast.kotlin.generate.KotlinUastBaseCodeGenerationPlugin
-import org.jetbrains.uast.kotlin.internal.analyzeForUast
 
 abstract class FirKotlinUastCodeGenerationPlugin : KotlinUastBaseCodeGenerationPlugin() {
-  override fun shortenReference(sourcePsi: KtElement): PsiElement {
-    val ktFile = sourcePsi.containingKtFile
-    analyzeForUast(ktFile) {
-        collectPossibleReferenceShortenings(ktFile, sourcePsi.textRange)
+    override fun shortenReference(sourcePsi: KtElement): PsiElement? {
+        shortenReferences(sourcePsi)
+        return sourcePsi
     }
-
-    //todo apply shortening
-    return sourcePsi
-  }
 }
