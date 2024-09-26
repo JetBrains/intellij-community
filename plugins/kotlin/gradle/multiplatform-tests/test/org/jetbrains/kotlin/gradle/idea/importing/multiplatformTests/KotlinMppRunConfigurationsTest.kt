a02a8bc7d3a7a1c3b33fabbf7fdf37d075f773b4
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-09-26 08:59:11 +0200

    [kotlin] Implement integration test for rendering Kotlin/Native test states
    FL-30039
    
    GitOrigin-RevId: abf12ac153a5d50fee2fef769accd83d1ddb51f5

--- plugins/kotlin/gradle/multiplatform-tests/test/org/jetbrains/kotlin/gradle/idea/importing/multiplatformTests/KotlinMppRunConfigurationsTest.kt
+++ plugins/kotlin/gradle/multiplatform-tests/test/org/jetbrains/kotlin/gradle/idea/importing/multiplatformTests/KotlinMppRunConfigurationsTest.kt
@@ -1,22 +1,45 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.gradle.idea.importing.multiplatformTests
 
+import com.intellij.codeInsight.daemon.LineMarkerInfo
+import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl
+import com.intellij.execution.lineMarker.RunLineMarkerContributor
+import com.intellij.icons.AllIcons
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.vfs.findFile
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiManager
+import com.intellij.testFramework.runInEdtAndWait
+import com.intellij.testIntegration.TestRunLineMarkerProvider
+import com.intellij.util.containers.ContainerUtil.filterIsInstance
 import org.jetbrains.kotlin.gradle.multiplatformTests.AbstractKotlinMppGradleImportingTest
 import org.jetbrains.kotlin.gradle.multiplatformTests.TestConfigurationDslScope
 import org.jetbrains.kotlin.gradle.multiplatformTests.testFeatures.CustomChecksDsl
+import org.jetbrains.kotlin.gradle.multiplatformTests.testFeatures.CustomGradleProperties
+import org.jetbrains.kotlin.gradle.multiplatformTests.testFeatures.CustomGradlePropertiesTestFeature
 import org.jetbrains.kotlin.gradle.multiplatformTests.testFeatures.checkers.highlighting.HighlightingChecker
+import org.jetbrains.kotlin.gradle.multiplatformTests.testFeatures.checkers.hooks.TestHooks
 import org.jetbrains.kotlin.gradle.multiplatformTests.testFeatures.checkers.runConfigurations.ExecuteRunConfigurationsChecker
 import org.jetbrains.kotlin.gradle.multiplatformTests.testFeatures.checkers.runConfigurations.RunConfigurationsChecker
+import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginMode
 import org.jetbrains.kotlin.test.TestMetadata
+import org.jetbrains.kotlin.tooling.core.compareTo
 import org.jetbrains.plugins.gradle.tooling.annotation.PluginTargetVersions
 import org.junit.Test
+import javax.swing.Icon
 
 @TestMetadata("multiplatform/core/features/runConfigurations")
 class KotlinMppRunConfigurationsTest : AbstractKotlinMppGradleImportingTest(), CustomChecksDsl {
 
+    override val pluginMode: KotlinPluginMode
+        get() = if (kotlinPluginVersion >= "2.0.20-dev-0") KotlinPluginMode.K2
+        else KotlinPluginMode.K1
+
     override fun TestConfigurationDslScope.defaultTestConfiguration() {
-        onlyCheckers(RunConfigurationsChecker, ExecuteRunConfigurationsChecker)
+        onlyCheckers(RunConfigurationsChecker, ExecuteRunConfigurationsChecker, CustomGradlePropertiesTestFeature, TestHooks)
         disableCheckers(HighlightingChecker)
+        /* When executing tests, we do not care about deprecation warnings in our build output */
+        addCustomGradleProperty("org.gradle.warning.mode", "none")
     }
 
     @PluginTargetVersions(pluginVersion = "1.9.20-dev-6845+")
@@ -49,6 +72,49 @@ class KotlinMppRunConfigurationsTest : AbstractKotlinMppGradleImportingTest(), C
             executeRunConfiguration("IosTest.test in iosTest - failure")
             executeRunConfiguration("JvmTest.test in jvmTest - success")
             executeRunConfiguration("JvmTest.test in jvmTest - failure")
+        }
+    }
+
+    @PluginTargetVersions(pluginVersion = "2.0.20-dev-0+")
+    @Test
+    fun testNativeTest() {
+        doTest {
+            onlyModules(".*nativeTest")
+            executeRunConfiguration("foo.bar.NativeTest")
+            /**
+             * Let's test the line-markers after the test executed
+             *
+             * ```kotlin
+             * class NativeTest { // <- RED!
+             *     @Test
+             *     fun failure() // <- RED!
+             *
+             *     @Test
+             *     fun success() // <- GREEN!
+             * }
+             * ```
+             */
+            runAfterTestExecution {
+                val nativeTestFile = projectRoot.findFile("src/nativeTest/kotlin/NativeTest.kt") ?: error("Missing 'NativeTest.kt'")
+                runInEdtAndWait {
+                    codeInsightTestFixture.openFileInEditor(nativeTestFile)
+                    codeInsightTestFixture.doHighlighting()
+                    val psi = PsiManager.getInstance(myProject).findFile(nativeTestFile) ?: error("Missing 'NativeTest.kt' PsiFile")
+                    val document = PsiDocumentManager.getInstance(myProject).getDocument(psi) ?: error("Missing 'NativeTest.kt' Document")
+                    val lineMarkers = DaemonCodeAnalyzerImpl.getLineMarkers(document, project)
+
+                    fun assertStateAtText(text: String, icon: Icon) {
+                        val lineMarker = lineMarkers.find { marker -> marker.element?.text == text }
+                            ?: error("Missing line marker for '$text'")
+
+                        kotlin.test.assertEquals(icon, lineMarker.icon, "Wrong line-marker at '$text'")
+                    }
+
+                    assertStateAtText("NativeTest", AllIcons.RunConfigurations.TestState.Red2)
+                    assertStateAtText("success", AllIcons.RunConfigurations.TestState.Green2)
+                    assertStateAtText("failure", AllIcons.RunConfigurations.TestState.Red2)
+                }
+            }
         }
     }
 }
