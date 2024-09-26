bdcc4d21489bc46253a15ed0cf0208009af32af0
Marat Dinmukhametov <marat.dinmukhametov@jetbrains.com>
2024-09-26 18:41:24 +0300

    [kotlin] IDEA-353900 KTIJ-31325 k2: Moved shorten references to uast kotlin idea
    GitOrigin-RevId: ceb9c97e9e59e1ccded9857c420fffb482a44660

--- plugins/kotlin/uast/uast-kotlin-idea-fir/src/org/jetbrains/uast/kotlin/IdeaFirKotlinUastCodeGenerationPlugin.kt
+++ plugins/kotlin/uast/uast-kotlin-idea-fir/src/org/jetbrains/uast/kotlin/IdeaFirKotlinUastCodeGenerationPlugin.kt
@@ -3,20 +3,29 @@ package org.jetbrains.uast.kotlin
 
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.analysis.api.permissions.KaAllowAnalysisOnEdt
 import org.jetbrains.kotlin.analysis.api.permissions.allowAnalysisOnEdt
+import org.jetbrains.kotlin.idea.base.analysis.api.utils.shortenReferences
 import org.jetbrains.kotlin.idea.refactoring.moveFunctionLiteralOutsideParentheses
 import org.jetbrains.kotlin.idea.refactoring.rename.KtReferenceMutateServiceBase
 import org.jetbrains.kotlin.idea.references.KtReferenceMutateService
 import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.uast.UElement
 import org.jetbrains.uast.generate.UastCommentSaver
 import org.jetbrains.uast.generate.UastElementFactory
+import org.jetbrains.uast.kotlin.generate.KotlinUastBaseCodeGenerationPlugin
 import org.jetbrains.uast.kotlin.generate.KotlinUastElementFactory
 import org.jetbrains.uast.kotlin.generate.createUastCommentSaver
 
 
-class IdeaFirKotlinUastCodeGenerationPlugin : FirKotlinUastCodeGenerationPlugin(){
+class IdeaFirKotlinUastCodeGenerationPlugin : KotlinUastBaseCodeGenerationPlugin(){
+    override fun shortenReference(sourcePsi: KtElement): PsiElement {
+        shortenReferences(sourcePsi)
+        return sourcePsi
+    }
+
     @OptIn(KaAllowAnalysisOnEdt::class)
     override fun getElementFactory(project: Project): UastElementFactory {
         return object : KotlinUastElementFactory(project) {
