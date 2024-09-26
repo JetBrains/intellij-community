560bc3bb276e9c10b5c9717ecc7f8039e8c65c07
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-09-26 07:52:25 +0200

    use Set.of
    GitOrigin-RevId: 77801415b78215f057fdc985361042e406dd2c1f

--- jps/model-api/src/org/jetbrains/jps/model/java/JavaModuleSourceRootTypes.java
+++ jps/model-api/src/org/jetbrains/jps/model/java/JavaModuleSourceRootTypes.java
@@ -1,14 +1,13 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.jps.model.java;
 
-import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.jps.model.module.JpsModuleSourceRootType;
 
 import java.util.Set;
 
 public final class JavaModuleSourceRootTypes {
-  public static final Set<JavaSourceRootType> SOURCES = ContainerUtil.newHashSet(JavaSourceRootType.SOURCE, JavaSourceRootType.TEST_SOURCE);
-  public static final Set<JavaResourceRootType> RESOURCES = ContainerUtil.newHashSet(JavaResourceRootType.RESOURCE, JavaResourceRootType.TEST_RESOURCE);
-  public static final Set<? extends JpsModuleSourceRootType<?>> PRODUCTION = ContainerUtil.newHashSet(JavaSourceRootType.SOURCE, JavaResourceRootType.RESOURCE);
-  public static final Set<? extends JpsModuleSourceRootType<?>> TESTS = ContainerUtil.newHashSet(JavaSourceRootType.TEST_SOURCE, JavaResourceRootType.TEST_RESOURCE);
+  public static final Set<JavaSourceRootType> SOURCES = Set.of(JavaSourceRootType.SOURCE, JavaSourceRootType.TEST_SOURCE);
+  public static final Set<JavaResourceRootType> RESOURCES = Set.of(JavaResourceRootType.RESOURCE, JavaResourceRootType.TEST_RESOURCE);
+  public static final Set<? extends JpsModuleSourceRootType<?>> PRODUCTION = Set.of(JavaSourceRootType.SOURCE, JavaResourceRootType.RESOURCE);
+  public static final Set<? extends JpsModuleSourceRootType<?>> TESTS = Set.of(JavaSourceRootType.TEST_SOURCE, JavaResourceRootType.TEST_RESOURCE);
 }
