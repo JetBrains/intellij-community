f66b609f3895df812428d11227305522ae97148c
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2024-09-26 12:53:22 +0200

    Eel/IJent cleanup: remove stub of EelPath
    GitOrigin-RevId: 6cf1d3ece47b61d87744c9d394d88340f27d791d

--- platform/eel/src/com/intellij/platform/eel/EelFsApi.kt
+++ platform/eel/src/com/intellij/platform/eel/EelFsApi.kt
@@ -1,13 +1,9 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.eel
 
+import com.intellij.platform.eel.path.EelPath
 import java.nio.file.Path
 
 interface EelFsApi {
   fun getOriginalPath(path: Path): EelPath.Absolute
-}
-
-sealed interface EelPath {
-  data class Relative(val path: String) : EelPath
-  data class Absolute(val path: String) : EelPath
 }
