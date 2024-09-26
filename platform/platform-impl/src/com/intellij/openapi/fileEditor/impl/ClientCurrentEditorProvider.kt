fe39cc7826e2b5545c5865a26ed545b68cbcc688
Ilya.Usov <Ilya.Usov@jetbrains.com>
2024-09-26 14:55:57 +0200

    Fix testRedoBasicCommandAfterForeignTyping
    Do not use FocusBasedCurrentEditorProvider in case of cwm remote clients
    
    GitOrigin-RevId: cf1ce4cd2a6461882890398cd15a3cae7df13794

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/ClientCurrentEditorProvider.kt
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/ClientCurrentEditorProvider.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.fileEditor.impl
 
+import com.intellij.openapi.client.currentSession
 import com.intellij.openapi.editor.ClientEditorManager
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorManager
@@ -10,11 +11,6 @@ import com.intellij.util.ui.UIUtil
 
 private class ClientCurrentEditorProvider : CurrentEditorProvider {
   override fun getCurrentEditor(project: Project?): FileEditor? {
-    FocusBasedCurrentEditorProvider.getCurrentEditorEx()?.let { editor ->
-      // GTW-6595
-      return editor
-    }
-
     if (project == null) {
       // fallback to search by focus
       return ClientEditorManager.getCurrentInstance().editorsSequence()
@@ -22,6 +18,15 @@ private class ClientCurrentEditorProvider : CurrentEditorProvider {
         ?.let { TextEditorProvider.getInstance().getTextEditor(it) }
     }
     else {
+      if (project.currentSession.isController) {
+        // doesn't work in case of other remote clients.
+        // check com.jetbrains.rdct.cwm.distributed.undo.EditorComplicatedUndoTest.testRedoBasicCommandAfterForeignTyping
+        FocusBasedCurrentEditorProvider.getCurrentEditorEx()?.let { editor ->
+          // GTW-6595
+          return editor
+        }
+      }
+
       return FileEditorManager.getInstance(project).selectedEditor
     }
   }
