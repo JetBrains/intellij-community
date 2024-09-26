ca36975afc107afbefa7d77db3c97b61139177b1
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2024-09-26 21:21:49 +0200

    [kotlin] Do not suggest switching to k2 in debug instance IJ
    #KTIJ-31420
    
    GitOrigin-RevId: 8c1803db43ddce0fcb262c78564a5ee43dc7c442

--- plugins/kotlin/highlighting/highlighting-k1/src/org/jetbrains/kotlin/idea/base/fe10/highlighting/suspender/KotlinHighlightingSuspendNotificationProvider.kt
+++ plugins/kotlin/highlighting/highlighting-k1/src/org/jetbrains/kotlin/idea/base/fe10/highlighting/suspender/KotlinHighlightingSuspendNotificationProvider.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.base.fe10.highlighting.suspender
 
+import com.intellij.diagnostic.VMOptions
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.options.ConfigurableWithId
 import com.intellij.openapi.options.ShowSettingsUtil
@@ -15,6 +16,9 @@ import java.util.function.Predicate
 import javax.swing.JComponent
 
 internal class KotlinHighlightingSuspendNotificationProvider : EditorNotificationProvider {
+    // same as [org.jetbrains.kotlin.idea.configuration.ui.KotlinPluginKindSwitcherController.canSwitchKotlinPluginModeViaUI]
+    private fun canSwitchKotlinPluginModeViaUI(): Boolean = VMOptions.canWriteOptions()
+
     override fun collectNotificationData(project: Project, file: VirtualFile): Function<in FileEditor, out JComponent?>? {
         if (!file.isKotlinFileType()) {
             return null
@@ -25,11 +29,13 @@ internal class KotlinHighlightingSuspendNotificationProvider : EditorNotificatio
         return Function {
             EditorNotificationPanel(it, EditorNotificationPanel.Status.Warning).apply {
                 text = KotlinBaseHighlightingBundle.message("highlighting.for.0.is.suspended", file.name)
-                createActionLabel(KotlinBaseHighlightingBundle.message("highlighting.action.text.try.k2")) {
-                    ShowSettingsUtil.getInstance().showSettingsDialog(project, Predicate {
-                        // id of KotlinLanguageConfiguration, to avoid module dependency on `kotlin.preferences` module
-                        (it as? ConfigurableWithId)?.id == "preferences.language.Kotlin"
-                    }, null)
+                if (canSwitchKotlinPluginModeViaUI()) {
+                    createActionLabel(KotlinBaseHighlightingBundle.message("highlighting.action.text.try.k2")) {
+                        ShowSettingsUtil.getInstance().showSettingsDialog(project, Predicate {
+                            // id of KotlinLanguageConfiguration, to avoid module dependency on `kotlin.preferences` module
+                            (it as? ConfigurableWithId)?.id == "preferences.language.Kotlin"
+                        }, null)
+                    }
                 }
                 createActionLabel(KotlinBaseHighlightingBundle.message("highlighting.action.text.ignore")) {
                     KotlinHighlightingSuspender.getInstance(project).unsuspend(file)
