d61150f7b801ba753a0148d94310148b627a32db
Oleg Taratukhin <oleg.taratukhin@jetbrains.com>
2024-09-27 19:43:57 +0200

    [ai-completion] ML-3971 simplify `preProcessLines`
    GitOrigin-RevId: b5150a517cfb4e5aca52c20de7a6f1866f976880

--- plugins/evaluation-plugin/core/src/com/intellij/cce/metric/RelaxedSimilarityMetrics.kt
+++ plugins/evaluation-plugin/core/src/com/intellij/cce/metric/RelaxedSimilarityMetrics.kt
@@ -3,33 +3,19 @@ package com.intellij.cce.metric
 object RelaxedSimilarityUtils {
   private val nonValuable = Regex("[^a-zA-Z0-9_+\\-*%=&|@\$?]")
 
-  private fun String.rightSplit(on: String, default: Pair<String, String>): Pair<String, String> {
-    val index = lastIndexOf(on)
-    if (index == -1) return default
-    return substring(0, index) to substring(index + 1)
-  }
-
-  private fun String.leftSplit(on: String, default: Pair<String, String>): Pair<String, String> {
-    val index = indexOf(on)
-    if (index == -1) return default
-    return substring(0, index) to substring(index + 1)
-  }
-
   fun preProcessLines(completion: String, prefix: String, suffix: String, stripChars: Boolean = true): List<String> {
-    var completionWithPrefix = completion
-
-    if (!prefix.endsWith("\n") && !completion.startsWith("\n")) {
-      val (_, startedLine) = prefix.rightSplit("\n", default = "" to prefix)
-      completionWithPrefix = startedLine + completion
+    val completionWithPrefix = buildString {
+      if (!prefix.endsWith("\n") && !completion.startsWith("\n")) {
+        append(prefix.substringAfterLast("\n"))
+      }
+      append(completion)
+      if (!completion.endsWith("\n") && !suffix.startsWith("\n")) {
+        append(suffix.substringBefore("\n"))
+      }
     }
-    if (!completion.endsWith("\n") && !suffix.startsWith("\n")) {
-      val (startedLine, _) = suffix.leftSplit("\n", default = "" to suffix)
-      completionWithPrefix += startedLine
-    }
 
-    val lines = completionWithPrefix.split("\n")
-    return lines.map { line ->
-      if (stripChars) nonValuable.replace(line, "") else line
-    }.filter { it.isNotBlank() }
+    return completionWithPrefix.lines()
+      .map { if (stripChars) it.replace(nonValuable, "") else it }
+      .filter { it.isNotBlank() }
   }
 }
