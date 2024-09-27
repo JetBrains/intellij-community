b47c41525a4cccc5c4c5007f11009ebfc3de2de8
Oleg Taratukhin <oleg.taratukhin@jetbrains.com>
2024-09-27 20:32:18 +0200

    [ai-completion] ML-3971 add utility for relaxed exact match calculation
    GitOrigin-RevId: 9e7e26eb261d79e5029bf24d5ab7c36535bccaa1

--- plugins/evaluation-plugin/core/src/com/intellij/cce/metric/RelaxedSimilarityMetrics.kt
+++ plugins/evaluation-plugin/core/src/com/intellij/cce/metric/RelaxedSimilarityMetrics.kt
@@ -18,4 +18,27 @@ object RelaxedSimilarityUtils {
       .map { if (stripChars) it.replace(nonValuable, "") else it }
       .filter { it.isNotBlank() }
   }
+
+  enum class RelaxedResult(val weight: Int) { NO(0), ANY(1), MULTI(2) }
+
+  fun computeRelaxedExactMatch(
+    middle: String,
+    completion: String,
+    prefix: String,
+    suffix: String,
+    stripChars: Boolean = false,
+  ): RelaxedResult {
+    if (middle.isBlank() || completion.isBlank()) return RelaxedResult.NO
+    val missingCode = middle + suffix
+    val completionLines = preProcessLines(completion, prefix, suffix, stripChars)
+    val middleLines = preProcessLines(middle, prefix, suffix, stripChars)
+    val prefixMatch = missingCode.startsWith(completion.trim())
+    val hasMatchingLine = middleLines.any { completionLines.any { line -> line in it } }
+    val multilineMatch = completionLines.all { completionLine -> middleLines.any { line -> completionLine in line } }
+    return when {
+      multilineMatch -> RelaxedResult.MULTI
+      hasMatchingLine -> RelaxedResult.ANY
+      else -> if (prefixMatch) RelaxedResult.ANY else RelaxedResult.NO
+    }
+  }
 }
