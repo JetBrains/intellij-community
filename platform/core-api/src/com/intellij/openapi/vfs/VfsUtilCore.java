ecd300887417720100aa678dae910f464954c384
Alexandr Evstigneev <alexandr.evstigneev@jetbrains.com>
2024-09-27 12:57:20 +0400

    RUBY-33281 Properly handle virtual files without a parent
    Some VirtualFile implementations may have no parent (return null). But have a real path. In such cases this comparison method considered files with the same name (but different paths) as equal, which is obviously wrong.
    
    
    IJ-CR-145650
    
    GitOrigin-RevId: 869e84da6e0f6d1cd711810d9f5b76ec3f24bf8b

--- platform/core-api/src/com/intellij/openapi/vfs/VfsUtilCore.java
+++ platform/core-api/src/com/intellij/openapi/vfs/VfsUtilCore.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vfs;
 
 import com.intellij.core.CoreBundle;
@@ -769,6 +769,10 @@ public class VfsUtilCore {
 
       if (v2 == null) {
         return 1;
+      }
+
+      if (v1.getParent() == null && v2.getParent() == null) {
+        return v1.getPath().compareTo(v2.getPath());
       }
 
       VirtualFile[] parents1 = getPathComponents(v1);
