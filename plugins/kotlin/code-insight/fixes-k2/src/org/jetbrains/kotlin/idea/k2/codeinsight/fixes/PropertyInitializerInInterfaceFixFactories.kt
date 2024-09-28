072251e8935683794c395639acc0b53cba6cf9b1
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2024-09-28 12:16:37 +0400

    [kotlin] Unify ConvertPropertyInitializerToGetterFix and ConvertExtensionPropertyInitializerToGetterFix
    ^KTIJ-29493
    ^KTIJ-29489
    
    GitOrigin-RevId: ca55a3f453fddf4e8588673b11bb167f28bf4af0

--- plugins/kotlin/code-insight/fixes-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/fixes/PropertyInitializerInInterfaceFixFactories.kt
+++ plugins/kotlin/code-insight/fixes-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/fixes/PropertyInitializerInInterfaceFixFactories.kt
@@ -1,14 +1,9 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.k2.codeinsight.fixes
 
-import com.intellij.modcommand.ActionContext
-import com.intellij.modcommand.ModPsiUpdater
 import org.jetbrains.kotlin.analysis.api.fir.diagnostics.KaFirDiagnostic
-import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.KotlinPsiUpdateModCommandAction
 import org.jetbrains.kotlin.idea.codeinsight.api.applicators.fixes.KotlinQuickFixFactory
 import org.jetbrains.kotlin.idea.codeinsights.impl.base.CallableReturnTypeUpdaterUtils
-import org.jetbrains.kotlin.idea.codeinsights.impl.base.quickFix.convertPropertyInitializerToGetter
 import org.jetbrains.kotlin.psi.KtProperty
 
 internal object PropertyInitializerInInterfaceFixFactories {
@@ -20,22 +15,5 @@ internal object PropertyInitializerInInterfaceFixFactories {
         listOf(
             ConvertPropertyInitializerToGetterFix(element, elementContext)
         )
-    }
-
-    private class ConvertPropertyInitializerToGetterFix(
-        element: KtProperty,
-        elementContext: CallableReturnTypeUpdaterUtils.TypeInfo,
-    ) : KotlinPsiUpdateModCommandAction.ElementBased<KtProperty, CallableReturnTypeUpdaterUtils.TypeInfo>(element, elementContext) {
-
-        override fun getFamilyName(): String = KotlinBundle.message("convert.property.initializer.to.getter")
-
-        override fun invoke(
-            actionContext: ActionContext,
-            element: KtProperty,
-            elementContext: CallableReturnTypeUpdaterUtils.TypeInfo,
-            updater: ModPsiUpdater,
-        ) {
-            convertPropertyInitializerToGetter(actionContext.project, element, elementContext, updater)
-        }
     }
 }
