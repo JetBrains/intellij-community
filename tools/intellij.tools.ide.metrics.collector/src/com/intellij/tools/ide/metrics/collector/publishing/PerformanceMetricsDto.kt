cdaf3f39b0cafc8b2e9bd2d79309001724b1bbc2
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-09-30 10:34:18 +0200

    Remove additional unused fields from IJPerfMetricsDto
    GitOrigin-RevId: 075e028393113b8405eb194a73eaf9e09089e631

--- tools/intellij.tools.ide.metrics.collector/src/com/intellij/tools/ide/metrics/collector/publishing/PerformanceMetricsDto.kt
+++ tools/intellij.tools.ide.metrics.collector/src/com/intellij/tools/ide/metrics/collector/publishing/PerformanceMetricsDto.kt
@@ -2,18 +2,17 @@ package com.intellij.tools.ide.metrics.collector.publishing
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.intellij.openapi.util.BuildNumber
-import com.intellij.openapi.util.SystemInfo
 import com.intellij.tools.ide.metrics.collector.metrics.PerformanceMetrics
 import com.intellij.tools.ide.metrics.collector.metrics.toJson
-import com.intellij.util.system.OS
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 
 typealias PerformanceMetricsDto = IJPerfMetricsDto<Int>
 /**
- * A JSON schema used to report indexing performance metrics to display on https://ij-perf.jetbrains.com [IDEA-251676].
+ * A JSON schema used to report indexing performance metrics to display on https://ij-perf.jetbrains.com .
  * The generated .json files will be collected by https://github.com/JetBrains/ij-perf-report-aggregator.
+ * See: https://github.com/JetBrains/ij-perf-report-aggregator/blob/master/schemas/performanceMetrics.schema.json
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class IJPerfMetricsDto<T: Number>(
@@ -22,13 +21,8 @@ data class IJPerfMetricsDto<T: Number>(
   val project: String,
   val projectURL: String,
   val projectDescription: String,
-  val os: String,
-  val osFamily: String,
-  val runtime: String,
   val build: String,
   val buildDate: String,
-  val branch: String,
-  val productCode: String,
   val methodName: String,
   val metrics: List<ApplicationMetricDto<T>>,
 ) {
@@ -60,14 +54,9 @@ data class IJPerfMetricsDto<T: Number>(
       generated = generated,
       project = projectName,
       projectURL = projectURL,
-      os = SystemInfo.getOsNameAndVersion(),
-      osFamily = OS.CURRENT.toString(),
-      runtime = SystemInfo.JAVA_VENDOR + " " + SystemInfo.JAVA_VERSION + " " + SystemInfo.JAVA_RUNTIME_VERSION,
       build = buildNumber.asStringWithoutProductCode(),
-      branch = buildNumber.asStringWithoutProductCode().substringBeforeLast("."),
       // the 'buildDate' field is required for https://ij-perf.jetbrains.com; use any value here
       buildDate = ZonedDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME),
-      productCode = buildNumber.productCode,
       metrics = metrics,
       methodName = methodName,
       projectDescription = projectDescription
