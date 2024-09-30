a846672bcd5efc4423ff8210ccd0c627e9f99637
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2024-09-30 05:21:32 +0200

    Unused class removed
    GitOrigin-RevId: 841c2062565760fe21c56f76655662c3f436a61e

--- platform/eel/src/com/intellij/platform/eel/EelFsApi.kt
+++
@@ -1,9 +0,0 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.eel
-
-import com.intellij.platform.eel.path.EelPath
-import java.nio.file.Path
-
-interface EelFsApi {
-  fun getOriginalPath(path: Path): EelPath.Absolute
-}
