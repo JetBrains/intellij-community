c796ecc70118fa1c1d289232e0ddfc66a21043cd
Andrii Zinchenko <andrii.zinchenko@jetbrains.com>
2024-09-30 14:34:24 +0200

    [eel] EelFileSystemApi.userHome: add blocking version
    GitOrigin-RevId: a44471ef62f6f950a4192690b4c9c208996ae1f7

--- platform/eelProvider/src/com/intellij/platform/eel/provider/utils/eelPathUtils.kt
+++ platform/eelProvider/src/com/intellij/platform/eel/provider/utils/eelPathUtils.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.eel.provider.utils
 
+import com.intellij.openapi.progress.runBlockingMaybeCancellable
+import com.intellij.platform.eel.fs.EelFileSystemApi
 import com.intellij.platform.eel.path.EelPath
 import com.intellij.platform.eel.path.EelPathResult
 
@@ -8,5 +10,11 @@ fun <T : EelPath> EelPathResult<T>.unwrap(): T {
   return when (this) {
     is EelPathResult.Ok -> path
     is EelPathResult.Err -> throw RuntimeException(reason)
+  }
+}
+
+fun EelFileSystemApi.userHomeBlocking(): EelPath.Absolute? {
+  return runBlockingMaybeCancellable {
+    userHome()
   }
 }
