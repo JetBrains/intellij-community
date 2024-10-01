8a6deee7351277f8eee2babc4ad993ca85926165
Anna Koehler <anna.marchenko@jetbrains.com>
2024-10-01 08:12:20 +0200

    Refactor structure where UI tests are located: there are mainly three dirs for Platform UI tests: aggregator, nightlySmoke and regression All tests inside these dirs should have correspondent tags. If a test is still in progress - do not report it to Allure until it is finished and stabilized.
    GitOrigin-RevId: 278a20feec4de11738f7c361a19a28498d405bbc

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/MainToolbarUI.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/MainToolbarUI.kt
@@ -33,3 +33,10 @@ class MainToolbarUI(data: ComponentData) : UiComponent(data) {
   fun projectWidget(projectName: String): UiComponent =
     x("//div[@class='ToolbarComboButton' and @visible_text='$projectName']")
 }
+
+val MainToolbarUI.rerunButton get() = x { contains(byAccessibleName("Rerun")) }
+val MainToolbarUI.resumeButton get() = x { contains(byAccessibleName("Resume")) }
+val MainToolbarUI.pauseButton get() = x { contains(byAccessibleName("Pause")) }
+val MainToolbarUI.restartDebugButton get() = x { contains(byAccessibleName("Restart Debug")) }
+val MainToolbarUI.stopButton get() = x { contains(byAccessibleName("Stop")) }
+val MainToolbarUI.stopButtonWith2Configs get() = x { and(contains(byAccessibleName("Stop")), byVisibleText("2")) }
