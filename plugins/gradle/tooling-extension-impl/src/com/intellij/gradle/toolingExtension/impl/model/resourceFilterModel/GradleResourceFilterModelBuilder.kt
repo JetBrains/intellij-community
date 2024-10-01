6aa96a5553b37601c449e601a55670a289f857be
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2024-10-01 14:32:38 +0200

    [gradle] fix code style IDEA-327336
    (cherry picked from commit 06b300ed852a07abaca8a99674a5f3d557c77d8f)
    
    IJ-MR-145885
    
    GitOrigin-RevId: 9c50a51a310476cb9f4ef581883d35f52708a680

--- plugins/gradle/tooling-extension-impl/src/com/intellij/gradle/toolingExtension/impl/model/resourceFilterModel/GradleResourceFilterModelBuilder.kt
+++ plugins/gradle/tooling-extension-impl/src/com/intellij/gradle/toolingExtension/impl/model/resourceFilterModel/GradleResourceFilterModelBuilder.kt
@@ -3,9 +3,8 @@ package com.intellij.gradle.toolingExtension.impl.model.resourceFilterModel
 
 import com.google.gson.GsonBuilder
 import com.intellij.gradle.toolingExtension.impl.modelBuilder.Messages
-import com.intellij.util.ReflectionUtilRt
-import org.codehaus.groovy.runtime.DefaultGroovyMethods
-import org.codehaus.groovy.runtime.StringGroovyMethods
+import com.intellij.util.ReflectionUtilRt.getField
+import org.codehaus.groovy.runtime.DefaultGroovyMethods.*
 import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.api.file.ContentFilterable
@@ -16,8 +15,6 @@ import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.plugins.gradle.model.DefaultExternalFilter
 import org.jetbrains.plugins.gradle.tooling.Message
 import org.jetbrains.plugins.gradle.tooling.ModelBuilderContext
-import java.util.*
-import java.util.function.Function
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
@@ -25,60 +22,48 @@ import java.util.regex.Pattern
 object GradleResourceFilterModelBuilder {
   @JvmStatic
   fun getIncludes(project: Project, taskName: String): MutableSet<String?> {
-    val filterableTask = project.getTasks().findByName(taskName)
+    val filterableTask = project.tasks.findByName(taskName)
     if (filterableTask is PatternFilterable) {
-      return (filterableTask as PatternFilterable).getIncludes()
+      return filterableTask.includes
     }
 
     return LinkedHashSet<String?>()
   }
 
   @JvmStatic
-  fun getExcludes(project: Project, taskName: String): MutableSet<String?> {
-    val filterableTask = project.getTasks().findByName(taskName)
+  fun getExcludes(project: Project, taskName: String): Set<String> {
+    val filterableTask = project.tasks.findByName(taskName)
     if (filterableTask is PatternFilterable) {
-      return (filterableTask as PatternFilterable).getExcludes()
+      return filterableTask.excludes
     }
 
-    return LinkedHashSet<String?>()
+    return LinkedHashSet<String>()
   }
 
   @JvmStatic
-  fun getFilters(project: Project, context: ModelBuilderContext, taskName: String): MutableList<DefaultExternalFilter?> {
-    val filterReaders = ArrayList<DefaultExternalFilter?>()
-    if (StringGroovyMethods.toBoolean(System.getProperty("idea.disable.gradle.resource.filtering", "false"))) {
+  fun getFilters(project: Project, context: ModelBuilderContext, taskName: String): List<DefaultExternalFilter> {
+    val filterReaders = ArrayList<DefaultExternalFilter>()
+    if (System.getProperty("idea.disable.gradle.resource.filtering").toBoolean()) {
       return filterReaders
     }
 
-    val filterableTask = project.getTasks().findByName(taskName)
+    val filterableTask = project.tasks.findByName(taskName)
     if (filterableTask is ContentFilterable &&
-        !DefaultGroovyMethods.getMetaClass(filterableTask).respondsTo(filterableTask, "getMainSpec").isEmpty()
+        !getMetaClass(filterableTask).respondsTo(filterableTask, "getMainSpec").isEmpty()
     ) {
-      val mainSpec = DefaultGroovyMethods.invokeMethod(filterableTask, "getMainSpec", arrayOfNulls<Any>(0))
+      val mainSpec = invokeMethod(filterableTask, "getMainSpec", arrayOfNulls<Any>(0))
       if (mainSpec == null) {
         return filterReaders
       }
 
       var copyActions: Any? = null
-      val allCopyActionsProp = DefaultGroovyMethods.hasProperty(mainSpec, "allCopyActions")
-      val copyActionsProp = DefaultGroovyMethods.hasProperty(mainSpec, "copyActions")
-      if (allCopyActionsProp != null) {
-        copyActions = allCopyActionsProp.getProperty(mainSpec)
-      }
-      else if (copyActionsProp != null) {
-        copyActions = copyActionsProp.getProperty(mainSpec)
-      }
-
-      if (copyActions == null) {
-        return filterReaders
-      }
+      val property = hasProperty(mainSpec, "allCopyActions") ?: hasProperty(mainSpec, "copyActions") ?: return filterReaders
+      copyActions = property.getProperty(mainSpec) ?: return filterReaders
 
       if (copyActions is Iterable<*>) {
-        val copyActionsIterable = copyActions
-        for (o in copyActionsIterable) {
-          if (o is Action<*>) {
-            val action = o
-            val filter = GradleResourceFilterModelBuilder.getFilter(project, context, action)
+        for (action in copyActions) {
+          if (action is Action<*>) {
+            val filter = getFilter(project, context, action)
             if (filter != null) {
               filterReaders.add(filter)
             }
@@ -99,40 +84,43 @@ object GradleResourceFilterModelBuilder {
         return getCommonFilter(action)
       }
     }
-    catch (ignored: Exception) {
-      context.getMessageReporter().createMessage().withGroup(Messages.RESOURCE_FILTER_MODEL_GROUP)
-        .withTitle("Resource configuration warning").withText("Cannot resolve resource filtering of " +
-                                                              action.javaClass.getSimpleName() +
-                                                              ". " +
-                                                              "IDEA may fail to build project. " +
-                                                              "Consider using delegated build (enabled by default).")
-        .withKind(Message.Kind.WARNING).reportMessage(project)
+    catch (_: Exception) {
+      context.getMessageReporter()
+        .createMessage()
+        .withGroup(Messages.RESOURCE_FILTER_MODEL_GROUP)
+        .withTitle("Resource configuration warning")
+        .withText("""
+          | Cannot resolve resource filtering of ${action.javaClass.getSimpleName()}.
+          | IDEA may fail to build project. Consider using delegated build (enabled by default).
+          | """.trimMargin())
+        .withKind(Message.Kind.WARNING)
+        .reportMessage(project)
     }
 
     return null
   }
 
   private fun getCommonFilter(action: Action<*>): DefaultExternalFilter {
-    val filterClass = GradleResourceFilterModelBuilder.findPropertyWithType<Class<*>>(action, Class::class.java, "filterType",
-                                                                                       "val\$filterType", "arg$2", "arg$1")
+    val filterClass = findPropertyWithType<Class<*>>(action, Class::class.java, "filterType",
+                                                     "val\$filterType", "arg$2", "arg$1")
     requireNotNull(filterClass) { "Unsupported action found: " + action.javaClass.getName() }
 
 
     val filterType = filterClass.getName()
-    var properties: java.util.Map<*,*>? = GradleResourceFilterModelBuilder.findPropertyWithType<java.util.Map<*,*>>(action, java.util.Map::class.java, "properties",
-                                                                                              "val\$properties", "arg$1")
+    var properties: java.util.Map<*,*>? = findPropertyWithType<java.util.Map<*,*>>(action, java.util.Map::class.java, "properties",
+                                                                                   "val\$properties", "arg$1")
     if ("org.apache.tools.ant.filters.ExpandProperties" == filterType) {
       if (properties != null && properties.get("project") != null) {
         val  /*org.apache.tools.ant.Project*/ project = properties.get("project")
-        properties = DefaultGroovyMethods.invokeMethod(project, "getProperties", arrayOfNulls<Any>(0)) as java.util.Map<*, *>?
+        properties = invokeMethod(project, "getProperties", arrayOfNulls<Any>(0)) as java.util.Map<*, *>?
       }
     }
 
 
     val filter = DefaultExternalFilter()
-    filter.setFilterType(filterType)
+    filter.filterType = filterType
     if (properties != null) {
-      filter.setPropertiesAsJsonMap(GsonBuilder().create().toJson(properties))
+      filter.propertiesAsJsonMap = GsonBuilder().create().toJson(properties)
     }
 
     return filter
@@ -141,33 +129,27 @@ object GradleResourceFilterModelBuilder {
   private fun getRenamingCopyFilter(action: Action<*>): DefaultExternalFilter? {
     assert("RenamingCopyAction" == action.javaClass.getSimpleName())
 
-
-    //Object transformer = action.transformer;
-    val transformerObject = ReflectionUtilRt.getField<Any?>(RenamingCopyAction::class.java, action, null, "transformer")
+    val transformerObject = getField<Any?>(RenamingCopyAction::class.java, action, null, "transformer")
     if (transformerObject !is RegExpNameMapper) {
       return null
     }
 
     val transformer = transformerObject
 
-    var pattern = Optional.ofNullable<Matcher?>(
-      ReflectionUtilRt.getField<Matcher?>(RegExpNameMapper::class.java, transformer, Matcher::class.java, "matcher"))
-      .map<Pattern?>(Function { obj: Matcher? -> obj!!.pattern() }).map<String?>(Function { obj: Pattern? -> obj!!.pattern() })
+    var pattern = getField<Matcher?>(RegExpNameMapper::class.java, transformer, Matcher::class.java, "matcher")?.pattern()?.pattern()
 
-    if (!pattern.isPresent()) {
-      pattern = Optional.ofNullable<Pattern?>(
-        ReflectionUtilRt.getField<Pattern?>(RegExpNameMapper::class.java, transformer, Pattern::class.java, "pattern"))
-        .map<String?>(Function { obj: Pattern? -> obj!!.pattern() })
+    if (pattern == null) {
+      pattern = getField<Pattern?>(RegExpNameMapper::class.java, transformer, Pattern::class.java, "pattern")?.pattern()
     }
 
-    val replacement = ReflectionUtilRt.getField<String?>(RegExpNameMapper::class.java, transformer, String::class.java, "replacement")
+    val replacement = getField<String?>(RegExpNameMapper::class.java, transformer, String::class.java, "replacement")
 
     val filter = DefaultExternalFilter()
-    filter.setFilterType("RenamingCopyFilter")
+    filter.filterType = "RenamingCopyFilter"
     val map = LinkedHashMap<String?, Any?>(2)
-    map.put("pattern", pattern.orElse(null))
+    map.put("pattern", pattern)
     map.put("replacement", replacement)
-    filter.setPropertiesAsJsonMap(GsonBuilder().create().toJson(map))
+    filter.propertiesAsJsonMap = GsonBuilder().create().toJson(map)
     return filter
   }
 
@@ -175,14 +157,15 @@ object GradleResourceFilterModelBuilder {
     for (name in propertyNames) {
       try {
         val field = self.javaClass.getDeclaredField(name)
-        if (type.isAssignableFrom(field.getType())) {
+        if (type.isAssignableFrom(field.type)) {
           field.setAccessible(true)
+          @Suppress("UNCHECKED_CAST")
           return field.get(self) as T?
         }
       }
-      catch (ignored: NoSuchFieldException) {
+      catch (_: NoSuchFieldException) {
       }
-      catch (ignored: IllegalAccessException) {
+      catch (_: IllegalAccessException) {
       }
     }
 
