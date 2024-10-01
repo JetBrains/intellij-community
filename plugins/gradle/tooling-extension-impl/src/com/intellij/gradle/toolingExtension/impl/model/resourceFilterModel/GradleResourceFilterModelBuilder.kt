18bccbf4a500e08d396d2c8f3e679403d6e2d708
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2024-10-01 14:12:42 +0200

    [gradle] convert java to kotlin IDEA-327336
    (cherry picked from commit 5e125040ddc8ac3b5621fc259c0f7058ef085580)
    
    IJ-MR-145885
    
    GitOrigin-RevId: 3c4e0b6bf31b61157f925216d4d0cae66ecd0772

--- plugins/gradle/tooling-extension-impl/src/com/intellij/gradle/toolingExtension/impl/model/resourceFilterModel/GradleResourceFilterModelBuilder.kt
+++ plugins/gradle/tooling-extension-impl/src/com/intellij/gradle/toolingExtension/impl/model/resourceFilterModel/GradleResourceFilterModelBuilder.kt
@@ -1,193 +1,192 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.gradle.toolingExtension.impl.model.resourceFilterModel;
+package com.intellij.gradle.toolingExtension.impl.model.resourceFilterModel
 
-import com.google.gson.GsonBuilder;
-import com.intellij.gradle.toolingExtension.impl.modelBuilder.Messages;
-import com.intellij.util.ReflectionUtilRt;
-import groovy.lang.MetaProperty;
-import org.codehaus.groovy.runtime.DefaultGroovyMethods;
-import org.codehaus.groovy.runtime.StringGroovyMethods;
-import org.gradle.api.Action;
-import org.gradle.api.Project;
-import org.gradle.api.Task;
-import org.gradle.api.file.ContentFilterable;
-import org.gradle.api.file.FileCopyDetails;
-import org.gradle.api.internal.file.copy.RegExpNameMapper;
-import org.gradle.api.internal.file.copy.RenamingCopyAction;
-import org.gradle.api.tasks.util.PatternFilterable;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.plugins.gradle.model.DefaultExternalFilter;
-import org.jetbrains.plugins.gradle.tooling.Message;
-import org.jetbrains.plugins.gradle.tooling.ModelBuilderContext;
-
-import java.lang.reflect.Field;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static org.codehaus.groovy.runtime.DefaultGroovyMethods.invokeMethod;
+import com.google.gson.GsonBuilder
+import com.intellij.gradle.toolingExtension.impl.modelBuilder.Messages
+import com.intellij.util.ReflectionUtilRt
+import org.codehaus.groovy.runtime.DefaultGroovyMethods
+import org.codehaus.groovy.runtime.StringGroovyMethods
+import org.gradle.api.Action
+import org.gradle.api.Project
+import org.gradle.api.file.ContentFilterable
+import org.gradle.api.file.FileCopyDetails
+import org.gradle.api.internal.file.copy.RegExpNameMapper
+import org.gradle.api.internal.file.copy.RenamingCopyAction
+import org.gradle.api.tasks.util.PatternFilterable
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.plugins.gradle.model.DefaultExternalFilter
+import org.jetbrains.plugins.gradle.tooling.Message
+import org.jetbrains.plugins.gradle.tooling.ModelBuilderContext
+import java.util.*
+import java.util.function.Function
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 @ApiStatus.Internal
-public final class GradleResourceFilterModelBuilder {
-  public static Set<String> getIncludes(Project project, String taskName) {
-    Task filterableTask = project.getTasks().findByName(taskName);
-    if (filterableTask instanceof PatternFilterable) {
-      return ((PatternFilterable)filterableTask).getIncludes();
+object GradleResourceFilterModelBuilder {
+  @JvmStatic
+  fun getIncludes(project: Project, taskName: String): MutableSet<String?> {
+    val filterableTask = project.getTasks().findByName(taskName)
+    if (filterableTask is PatternFilterable) {
+      return (filterableTask as PatternFilterable).getIncludes()
     }
 
-    return new LinkedHashSet<>();
+    return LinkedHashSet<String?>()
   }
 
-  public static Set<String> getExcludes(Project project, String taskName) {
-    Task filterableTask = project.getTasks().findByName(taskName);
-    if (filterableTask instanceof PatternFilterable) {
-      return ((PatternFilterable)filterableTask).getExcludes();
+  @JvmStatic
+  fun getExcludes(project: Project, taskName: String): MutableSet<String?> {
+    val filterableTask = project.getTasks().findByName(taskName)
+    if (filterableTask is PatternFilterable) {
+      return (filterableTask as PatternFilterable).getExcludes()
     }
 
-    return new LinkedHashSet<>();
+    return LinkedHashSet<String?>()
   }
 
-  public static List<DefaultExternalFilter> getFilters(final Project project, final ModelBuilderContext context, String taskName) {
-    final ArrayList<DefaultExternalFilter> filterReaders = new ArrayList<>();
+  @JvmStatic
+  fun getFilters(project: Project, context: ModelBuilderContext, taskName: String): MutableList<DefaultExternalFilter?> {
+    val filterReaders = ArrayList<DefaultExternalFilter?>()
     if (StringGroovyMethods.toBoolean(System.getProperty("idea.disable.gradle.resource.filtering", "false"))) {
-      return filterReaders;
+      return filterReaders
     }
 
-    Task filterableTask = project.getTasks().findByName(taskName);
-    if (filterableTask instanceof ContentFilterable &&
-        !DefaultGroovyMethods.getMetaClass(filterableTask).respondsTo(filterableTask, "getMainSpec").isEmpty()) {
-      //noinspection GrUnresolvedAccess
-      Object mainSpec = invokeMethod(filterableTask, "getMainSpec", new Object[0]);
+    val filterableTask = project.getTasks().findByName(taskName)
+    if (filterableTask is ContentFilterable &&
+        !DefaultGroovyMethods.getMetaClass(filterableTask).respondsTo(filterableTask, "getMainSpec").isEmpty()
+    ) {
+      val mainSpec = DefaultGroovyMethods.invokeMethod(filterableTask, "getMainSpec", arrayOfNulls<Any>(0))
       if (mainSpec == null) {
-        return filterReaders;
+        return filterReaders
       }
 
-      Object copyActions = null;
-      MetaProperty allCopyActionsProp = DefaultGroovyMethods.hasProperty(mainSpec, "allCopyActions");
-      MetaProperty copyActionsProp = DefaultGroovyMethods.hasProperty(mainSpec, "copyActions");
+      var copyActions: Any? = null
+      val allCopyActionsProp = DefaultGroovyMethods.hasProperty(mainSpec, "allCopyActions")
+      val copyActionsProp = DefaultGroovyMethods.hasProperty(mainSpec, "copyActions")
       if (allCopyActionsProp != null) {
-        //noinspection GrUnresolvedAccess
-        copyActions = allCopyActionsProp.getProperty(mainSpec);
+        copyActions = allCopyActionsProp.getProperty(mainSpec)
       }
       else if (copyActionsProp != null) {
-          //noinspection GrUnresolvedAccess
-          copyActions = copyActionsProp.getProperty(mainSpec);
+        copyActions = copyActionsProp.getProperty(mainSpec)
       }
 
       if (copyActions == null) {
-        return filterReaders;
+        return filterReaders
       }
 
-      if (copyActions instanceof Iterable) {
-        Iterable copyActionsIterable = (Iterable)copyActions;
-        for (Object o : copyActionsIterable) {
-          if (o instanceof Action) {
-            Action action = (Action)o;
-            DefaultExternalFilter filter = getFilter(project, context, action);
+      if (copyActions is Iterable<*>) {
+        val copyActionsIterable = copyActions
+        for (o in copyActionsIterable) {
+          if (o is Action<*>) {
+            val action = o
+            val filter = GradleResourceFilterModelBuilder.getFilter(project, context, action)
             if (filter != null) {
-              filterReaders.add(filter);
+              filterReaders.add(filter)
             }
           }
         }
       }
     }
 
-    return filterReaders;
+    return filterReaders
   }
 
-  private static DefaultExternalFilter getFilter(Project project, ModelBuilderContext context, Action<? super FileCopyDetails> action) {
+  private fun getFilter(project: Project, context: ModelBuilderContext, action: Action<in FileCopyDetails?>): DefaultExternalFilter? {
     try {
-      if ("RenamingCopyAction".equals(action.getClass().getSimpleName())) {
-        return getRenamingCopyFilter(action);
+      if ("RenamingCopyAction" == action.javaClass.getSimpleName()) {
+        return getRenamingCopyFilter(action)
       }
       else {
-        return getCommonFilter(action);
+        return getCommonFilter(action)
       }
     }
-    catch (Exception ignored) {
+    catch (ignored: Exception) {
       context.getMessageReporter().createMessage().withGroup(Messages.RESOURCE_FILTER_MODEL_GROUP)
         .withTitle("Resource configuration warning").withText("Cannot resolve resource filtering of " +
-                                                              action.getClass().getSimpleName() +
+                                                              action.javaClass.getSimpleName() +
                                                               ". " +
                                                               "IDEA may fail to build project. " +
                                                               "Consider using delegated build (enabled by default).")
-        .withKind(Message.Kind.WARNING).reportMessage(project);
+        .withKind(Message.Kind.WARNING).reportMessage(project)
     }
 
-    return null;
+    return null
   }
 
-  private static DefaultExternalFilter getCommonFilter(Action<? super FileCopyDetails> action) {
-    Class filterClass = findPropertyWithType(action, Class.class, "filterType", "val$filterType", "arg$2", "arg$1");
-    if (filterClass == null) {
-      throw new IllegalArgumentException("Unsupported action found: " + action.getClass().getName());
-    }
+  private fun getCommonFilter(action: Action<in FileCopyDetails?>): DefaultExternalFilter {
+    val filterClass = GradleResourceFilterModelBuilder.findPropertyWithType<Class<*>?>(action, Class::class.java, "filterType",
+                                                                                       "val\$filterType", "arg$2", "arg$1")
+    requireNotNull(filterClass) { "Unsupported action found: " + action.javaClass.getName() }
 
 
-    String filterType = filterClass.getName();
-    Map properties = findPropertyWithType(action, Map.class, "properties", "val$properties", "arg$1");
-    if ("org.apache.tools.ant.filters.ExpandProperties".equals(filterType)) {
+    val filterType = filterClass.getName()
+    var properties = GradleResourceFilterModelBuilder.findPropertyWithType<MutableMap<*, *>?>(action, MutableMap::class.java, "properties",
+                                                                                              "val\$properties", "arg$1")
+    if ("org.apache.tools.ant.filters.ExpandProperties" == filterType) {
       if (properties != null && properties.get("project") != null) {
-        Object /*org.apache.tools.ant.Project*/ project = properties.get("project");
-        properties = (Map)invokeMethod(project, "getProperties", new Object[0]);
+        val  /*org.apache.tools.ant.Project*/project = properties.get("project")
+        properties = DefaultGroovyMethods.invokeMethod(project, "getProperties", arrayOfNulls<Any>(0)) as MutableMap<*, *>?
       }
     }
 
 
-    DefaultExternalFilter filter = new DefaultExternalFilter();
-    filter.setFilterType(filterType);
+    val filter = DefaultExternalFilter()
+    filter.setFilterType(filterType)
     if (properties != null) {
-      filter.setPropertiesAsJsonMap(new GsonBuilder().create().toJson(properties));
+      filter.setPropertiesAsJsonMap(GsonBuilder().create().toJson(properties))
     }
 
-    return filter;
+    return filter
   }
 
-  private static DefaultExternalFilter getRenamingCopyFilter(Action<? super FileCopyDetails> action) {
-    assert "RenamingCopyAction".equals(action.getClass().getSimpleName());
+  private fun getRenamingCopyFilter(action: Action<in FileCopyDetails?>): DefaultExternalFilter? {
+    assert("RenamingCopyAction" == action.javaClass.getSimpleName())
 
-    //noinspection GrUnresolvedAccess
+
     //Object transformer = action.transformer;
-    Object transformerObject = ReflectionUtilRt.getField(RenamingCopyAction.class, action, null, "transformer");
-    if (!(transformerObject instanceof RegExpNameMapper)) {
-      return null;
+    val transformerObject = ReflectionUtilRt.getField<Any?>(RenamingCopyAction::class.java, action, null, "transformer")
+    if (transformerObject !is RegExpNameMapper) {
+      return null
     }
 
-    RegExpNameMapper transformer = (RegExpNameMapper)transformerObject;
+    val transformer = transformerObject
 
-    Optional<String> pattern
-      = Optional.ofNullable(ReflectionUtilRt.getField(RegExpNameMapper.class, transformer, Matcher.class, "matcher"))
-        .map(Matcher::pattern).map(Pattern::pattern);
+    var pattern = Optional.ofNullable<Matcher?>(
+      ReflectionUtilRt.getField<Matcher?>(RegExpNameMapper::class.java, transformer, Matcher::class.java, "matcher"))
+      .map<Pattern?>(Function { obj: Matcher? -> obj!!.pattern() }).map<String?>(Function { obj: Pattern? -> obj!!.pattern() })
 
     if (!pattern.isPresent()) {
-      pattern = Optional.ofNullable(ReflectionUtilRt.getField(RegExpNameMapper.class, transformer, Pattern.class, "pattern"))
-        .map(Pattern::pattern);
+      pattern = Optional.ofNullable<Pattern?>(
+        ReflectionUtilRt.getField<Pattern?>(RegExpNameMapper::class.java, transformer, Pattern::class.java, "pattern"))
+        .map<String?>(Function { obj: Pattern? -> obj!!.pattern() })
     }
 
-    String replacement = ReflectionUtilRt.getField(RegExpNameMapper.class, transformer, String.class, "replacement");
+    val replacement = ReflectionUtilRt.getField<String?>(RegExpNameMapper::class.java, transformer, String::class.java, "replacement")
 
-    DefaultExternalFilter filter = new DefaultExternalFilter();
-    filter.setFilterType("RenamingCopyFilter");
-    LinkedHashMap<String, Object> map = new LinkedHashMap<>(2);
-    map.put("pattern", pattern.orElse(null));
-    map.put("replacement", replacement);
-    filter.setPropertiesAsJsonMap(new GsonBuilder().create().toJson(map));
-    return filter;
+    val filter = DefaultExternalFilter()
+    filter.setFilterType("RenamingCopyFilter")
+    val map = LinkedHashMap<String?, Any?>(2)
+    map.put("pattern", pattern.orElse(null))
+    map.put("replacement", replacement)
+    filter.setPropertiesAsJsonMap(GsonBuilder().create().toJson(map))
+    return filter
   }
 
-  public static <T> T findPropertyWithType(Object self, Class<T> type, String... propertyNames) {
-    for (String name : propertyNames) {
+  fun <T> findPropertyWithType(self: Any, type: Class<T?>, vararg propertyNames: String): T? {
+    for (name in propertyNames) {
       try {
-        Field field = self.getClass().getDeclaredField(name);
+        val field = self.javaClass.getDeclaredField(name)
         if (type.isAssignableFrom(field.getType())) {
-          field.setAccessible(true);
-          return (T)field.get(self);
+          field.setAccessible(true)
+          return field.get(self) as T?
         }
       }
-      catch (NoSuchFieldException | IllegalAccessException ignored) {
+      catch (ignored: NoSuchFieldException) {
+      }
+      catch (ignored: IllegalAccessException) {
       }
     }
 
-    return null;
+    return null
   }
 }
