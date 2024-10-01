6dff58efc64306289eb7e52c338cbf0e71ee415a
Chris Lemaire <chris.lemaire@jetbrains.com>
2024-10-01 12:19:18 +0200

    [collab] Remove and replace classAsCoroutineName
    It's basically unused and deprecated since CoroutineName is no longer used.
    We practically always make a child scope with the class name.
    Better to just use the class name in this case.
    
    GitOrigin-RevId: 4827600b509f0ca804f5be1d05b35b11f8df470f

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/data/provider/GHPRDetailsDataProviderImpl.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/data/provider/GHPRDetailsDataProviderImpl.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.pullrequest.data.provider
 
-import com.intellij.collaboration.async.classAsCoroutineName
 import com.intellij.collaboration.util.CollectionDelta
 import com.intellij.platform.util.coroutines.childScope
 import com.intellij.util.messages.MessageBus
--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/editor/GHPRReviewFileEditorThreadViewModel.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/editor/GHPRReviewFileEditorThreadViewModel.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.github.pullrequest.ui.editor
 
-import com.intellij.collaboration.async.classAsCoroutineName
 import com.intellij.collaboration.async.stateInNow
 import com.intellij.platform.util.coroutines.childScope
 import kotlinx.coroutines.CoroutineScope
