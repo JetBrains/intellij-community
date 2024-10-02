e94ab16f1e55b74a7004c975ae264b8983561ce1
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-10-02 16:11:38 +0200

    [vcs] IJPL-158578 The label of the "Get from VCS" button is not clear to the users
    Replace "Get from VCS" with "Clone Repository"
    
    GitOrigin-RevId: 091a051350691f0b8cf7811f18f305342ac67f07

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/WelcomeScreenUI.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/WelcomeScreenUI.kt
@@ -13,7 +13,7 @@ fun Driver.welcomeScreen(action: WelcomeScreenUI.() -> Unit = {}) = this.ui.welc
 class WelcomeScreenUI(data: ComponentData) : UiComponent(data) {
   val createNewProjectButton = x("//div[(@accessiblename='New Project' and @class='JButton') or (@visible_text='New Project' and @class!='JBLabel')]")
   val openProjectButton = x("//div[(@accessiblename='Open' and @class='JButton')  or (@visible_text='Open' and @class!='JBLabel')]")
-  val fromVcsButton = x("//div[@accessiblename='Get from VCS' and @class='JButton']")
+  val fromVcsButton = x("//div[@accessiblename='Clone Repository' and @class='JButton']")
 
   private val leftItems = tree("//div[@class='Tree']")
 
