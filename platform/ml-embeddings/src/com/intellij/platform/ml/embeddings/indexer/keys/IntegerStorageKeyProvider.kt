96dd441f72e5c6d552b15e98eb734d82d9615b36
Evgeny Abramov <evgeny.abramov@jetbrains.com>
2024-10-02 18:19:33 +0300

    (IJPL-161917) find the correct enumerator location when a project is absent
    GitOrigin-RevId: 60743034a259e5f4b99b3b88da000360ad0ad56e

--- platform/ml-embeddings/src/com/intellij/platform/ml/embeddings/indexer/keys/IntegerStorageKeyProvider.kt
+++ platform/ml-embeddings/src/com/intellij/platform/ml/embeddings/indexer/keys/IntegerStorageKeyProvider.kt
@@ -22,18 +22,20 @@ class IntegerStorageKeyProvider : EmbeddingStorageKeyProvider<Long>, Disposable 
   private val mutex = Mutex()
 
   override suspend fun findKey(project: Project?, indexId: IndexId, entity: IndexableEntity): Long {
-    return getEnumerator(project!!, indexId).enumerate(entity.id.id + "$$$" + entity.indexableRepresentation).toLong()
+    return getEnumerator(project, indexId).enumerate(entity.id.id + "$$$" + entity.indexableRepresentation).toLong()
   }
 
   override suspend fun findEntityId(project: Project?, indexId: IndexId, key: Long): String {
-    return getEnumerator(project!!, indexId).valueOf(key.toInt())?.split("$$$", limit = 2)?.first() ?: ""
+    return getEnumerator(project, indexId).valueOf(key.toInt())?.split("$$$", limit = 2)?.first() ?: ""
   }
 
-  private suspend fun getEnumerator(project: Project, indexId: IndexId): PersistentStringEnumerator {
+  private suspend fun getEnumerator(project: Project?, indexId: IndexId): PersistentStringEnumerator {
     val path = IndexPath(project, indexId)
     return mutex.withLock {
       enumerators.getOrPut(path) {
-        val projectIndexRoot = LocalArtifactsManager.indicesRoot / project.getProjectCacheFileName() / indexId.toString()
+        var projectIndexRoot = LocalArtifactsManager.indicesRoot
+        if (project != null) projectIndexRoot /= project.getProjectCacheFileName()
+        projectIndexRoot /= indexId.toString()
         val enumeratorRoot = (projectIndexRoot / ENUMERATOR_FOLDER / ENUMERATOR_FILE).also { Files.createDirectories(it.parent) }
         PersistentStringEnumerator(enumeratorRoot, 1024 * 1024, true, null)
       }
@@ -58,4 +60,4 @@ class IntegerStorageKeyProvider : EmbeddingStorageKeyProvider<Long>, Disposable 
   }
 }
 
-data class IndexPath(val project: Project, val indexId: IndexId)
+data class IndexPath(val project: Project?, val indexId: IndexId)
