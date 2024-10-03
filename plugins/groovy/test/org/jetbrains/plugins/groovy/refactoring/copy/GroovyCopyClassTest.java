8c63d89607bcd423f4514afd597153b3917ea1e5
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-10-03 18:52:34 +0200

    [Groovy] convert GroovyCopyClassTest to Java (IDEA-327339)
    GitOrigin-RevId: 78e552ed63b0c6be28d21c482222dbb2bf98c89b

--- plugins/groovy/test/org/jetbrains/plugins/groovy/refactoring/copy/GroovyCopyClassTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/refactoring/copy/GroovyCopyClassTest.java
@@ -1,42 +1,50 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.refactoring.copy
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.refactoring.copy;
 
-import com.intellij.openapi.command.WriteCommandAction
-import com.intellij.psi.PsiClass
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.refactoring.copy.CopyClassesHandler
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
-import org.jetbrains.plugins.groovy.util.TestUtils
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.util.Computable;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.refactoring.copy.CopyClassesHandler;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import org.jetbrains.plugins.groovy.util.TestUtils;
 
-class GroovyCopyClassTest extends LightJavaCodeInsightFixtureTestCase {
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+public class GroovyCopyClassTest extends LightJavaCodeInsightFixtureTestCase {
   @Override
   protected String getBasePath() {
-    return "${TestUtils.testDataPath}refactoring/copy/"
+    return TestUtils.getTestDataPath() + "refactoring/copy/";
   }
 
-  void testBetweenPackages() throws Throwable {
-    final String testName = getTestName(false)
-    myFixture.copyFileToProject("${testName}.groovy", "foo/${testName}.groovy")
-    myFixture.addClass("package foo; public class Bar {}")
-    myFixture.addClass("package bar; public class Bar {}")
+  public void testBetweenPackages() {
+    final String testName = getTestName(false);
+    myFixture.copyFileToProject(testName + ".groovy", "foo/" + testName + ".groovy");
+    myFixture.addClass("package foo; public class Bar {}");
+    myFixture.addClass("package bar; public class Bar {}");
 
-    final PsiClass srcClass = myFixture.javaFacade.findClass("foo.$testName", GlobalSearchScope.allScope(project))
-    assertTrue(CopyClassesHandler.canCopyClass(srcClass))
-    WriteCommandAction.runWriteCommandAction(project, {
-        def map = Collections.singletonMap(srcClass.navigationElement.containingFile, [srcClass] as PsiClass[])
-        def dir = srcClass.manager.findDirectory(myFixture.tempDirFixture.getFile("bar"))
-        CopyClassesHandler.doCopyClasses(map, "${testName}_after", dir, project)
-      })
+    final PsiClass srcClass = myFixture.getJavaFacade().findClass("foo." + testName, GlobalSearchScope.allScope(getProject()));
+    assertTrue(CopyClassesHandler.canCopyClass(srcClass));
+    WriteCommandAction.runWriteCommandAction(getProject(), (Computable<Collection<PsiFile>>)() -> {
+        Map<PsiFile, PsiClass[]> map = Collections.singletonMap(srcClass.getNavigationElement().getContainingFile(), new PsiClass[]{srcClass});
+        PsiDirectory dir = srcClass.getManager().findDirectory(myFixture.getTempDirFixture().getFile("bar"));
+        return CopyClassesHandler.doCopyClasses(map, testName + "_after", dir, getProject());
+    });
 
-    myFixture.checkResultByFile("bar/${testName}_after.groovy", "${testName}_after.groovy", true)
+    myFixture.checkResultByFile("bar/" + testName + "_after.groovy", testName + "_after.groovy", true);
   }
 
-  void testCopyScript() throws Throwable {
-    final String testName = getTestName(false)
-    def file = myFixture.copyFileToProject("${testName}.groovy", "foo/${testName}.groovy")
-    def psiFile = myFixture.psiManager.findFile(file)
+  public void testCopyScript() {
+    final String testName = getTestName(false);
+    VirtualFile file = myFixture.copyFileToProject(testName + ".groovy", "foo/" + testName + ".groovy");
+    PsiFile psiFile = myFixture.getPsiManager().findFile(file);
     //would be copied as file
-    assertFalse(CopyClassesHandler.canCopyClass(myFixture.javaFacade.findClass("foo.$testName", GlobalSearchScope.allScope(project))))
-    assertFalse(CopyClassesHandler.canCopyClass(psiFile))
+    assertFalse(CopyClassesHandler.canCopyClass(myFixture.getJavaFacade().findClass("foo." + testName, GlobalSearchScope.allScope(getProject()))));
+    assertFalse(CopyClassesHandler.canCopyClass(psiFile));
   }
 }
