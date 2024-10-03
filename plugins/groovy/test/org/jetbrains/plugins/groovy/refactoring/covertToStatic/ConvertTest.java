14f5708b1680dedb23b778b5cfb095e94e9a08d5
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-10-03 15:56:30 +0200

    [Groovy] convert ConvertTest to Java (IDEA-327339)
    GitOrigin-RevId: d41563256db7835be501d393f1cd2a7bf607b8b3

--- plugins/groovy/test/org/jetbrains/plugins/groovy/refactoring/covertToStatic/ConvertTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/refactoring/covertToStatic/ConvertTest.java
@@ -1,82 +1,85 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.refactoring.covertToStatic
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.refactoring.covertToStatic;
 
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
-import org.jetbrains.plugins.groovy.lang.psi.GroovyFile
-import org.jetbrains.plugins.groovy.refactoring.GroovyRefactoringBundle
-import org.jetbrains.plugins.groovy.refactoring.convertToStatic.ConvertToStaticProcessor
-import org.jetbrains.plugins.groovy.util.TestUtils
+import org.jetbrains.plugins.groovy.LightGroovyTestCase;
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFile;
+import org.jetbrains.plugins.groovy.refactoring.GroovyRefactoringBundle;
+import org.jetbrains.plugins.groovy.refactoring.convertToStatic.ConvertToStaticProcessor;
+import org.jetbrains.plugins.groovy.util.TestUtils;
 
-class ConvertTest extends LightGroovyTestCase {
-  final String basePath = TestUtils.testDataPath + 'refactoring/convertToStatic'
-
+public class ConvertTest extends LightGroovyTestCase {
   private void doTest() {
-    final String testName = getTestName(true)
-    final GroovyFile file = myFixture.configureByFile("${testName}.groovy") as GroovyFile
-    new ConvertToStaticProcessor(project, file).run()
-    myFixture.checkResultByFile("${testName}_after.groovy")
+    final String testName = getTestName(true);
+    final GroovyFile file = (GroovyFile)myFixture.configureByFile(testName + ".groovy");
+    new ConvertToStaticProcessor(getProject(), file).run();
+    myFixture.checkResultByFile(testName + "_after.groovy");
   }
 
   private void doIntentionTest() {
-    final String testName = getTestName(true)
-    myFixture.configureByFile("${testName}.groovy") as GroovyFile
-    myFixture.launchAction myFixture.findSingleIntention(GroovyRefactoringBundle.message("intention.converting.to.static"))
-    myFixture.checkResultByFile("${testName}_after.groovy")
+    final String testName = getTestName(true);
+    myFixture.configureByFile(testName + ".groovy");
+    myFixture.launchAction(myFixture.findSingleIntention(GroovyRefactoringBundle.message("intention.converting.to.static")));
+    myFixture.checkResultByFile(testName + "_after.groovy");
   }
 
-  void testIgnoreMetaprogramming() {
-    doTest()
+  public void testIgnoreMetaprogramming() {
+    doTest();
   }
 
-  void testAddSkippedTypes() {
-    doTest()
+  public void testAddSkippedTypes() {
+    doTest();
   }
 
-  void testAddNecessaryConverts() {
-    doTest()
+  public void testAddNecessaryConverts() {
+    doTest();
   }
 
-  void testSpreadArguments() {
-    doTest()
+  public void testSpreadArguments() {
+    doTest();
   }
 
-  void testConvertGString() {
-    doTest()
+  public void testConvertGString() {
+    doTest();
   }
 
-  void testMultiAssignment() {
-    doTest()
+  public void testMultiAssignment() {
+    doTest();
   }
 
-  void testMarkupPrintScript() {
-    doTest()
+  public void testMarkupPrintScript() {
+    doTest();
   }
 
-  void testDontAddAnnotationTwice() {
-    doTest()
+  public void testDontAddAnnotationTwice() {
+    doTest();
   }
 
-  void testCompileDynamicClass() {
-    doTest()
+  public void testCompileDynamicClass() {
+    doTest();
   }
 
-  void testNestedClasses() {
-    doTest()
+  public void testNestedClasses() {
+    doTest();
   }
 
-  void testIntentionOnUnresolvedRefs() {
-    doIntentionTest()
+  public void testIntentionOnUnresolvedRefs() {
+    doIntentionTest();
   }
 
-  void testIntentionOnUnresolvedWithMethodQualifier() {
-    doIntentionTest()
+  public void testIntentionOnUnresolvedWithMethodQualifier() {
+    doIntentionTest();
   }
 
-  void testIntentionOnProperties() {
-    doIntentionTest()
+  public void testIntentionOnProperties() {
+    doIntentionTest();
   }
 
-  void testIntentionNecessaryConverts() {
-    doIntentionTest()
+  public void testIntentionNecessaryConverts() {
+    doIntentionTest();
+  }
+
+  @Override
+  public final String getBasePath() {
+    return TestUtils.getTestDataPath() + "refactoring/convertToStatic";
   }
 }
