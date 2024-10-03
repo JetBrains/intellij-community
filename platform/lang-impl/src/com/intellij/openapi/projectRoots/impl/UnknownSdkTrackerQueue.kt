df4747930351014db4c4bfebebb4e2a7b43030e6
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-10-03 09:40:53 +0200

    IJPL-162718 UnknownSdkCollector - use new non-blocking read action
    GitOrigin-RevId: 5cc2efa55c967d6d08b0d0b168fa6a44b2f0ddcd

--- platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkTrackerQueue.kt
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkTrackerQueue.kt
@@ -1,13 +1,19 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.projectRoots.impl
 
-import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
+import com.intellij.openapi.progress.runBlockingMaybeCancellable
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Computable
 import com.intellij.util.ui.update.MergingUpdateQueue
 import com.intellij.util.ui.update.Update
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.jetbrains.annotations.ApiStatus
 
 @Service(Service.Level.PROJECT)
@@ -17,20 +23,39 @@ internal class UnknownSdkTrackerQueue(coroutineScope: CoroutineScope) : UnknownS
   }
 }
 
-internal abstract class UnknownSdkCollectorQueue(mergingTimeSpaceMillis : Int, coroutineScope: CoroutineScope) : Disposable {
-  private val myUpdateQueue = MergingUpdateQueue.mergingUpdateQueue(
+internal abstract class UnknownSdkCollectorQueue(mergingTimeSpaceMillis : Int, coroutineScope: CoroutineScope) {
+  @Suppress("DEPRECATION")
+  private val updateQueue = MergingUpdateQueue.mergingUpdateQueue(
     name = javaClass.simpleName,
     mergingTimeSpan = mergingTimeSpaceMillis,
     coroutineScope = coroutineScope,
   ).usePassThroughInUnitTestMode()
 
-  override fun dispose(): Unit = Unit
-
   fun queue(task: UnknownSdkTrackerTask) {
-    myUpdateQueue.queue(object : Update(this) {
-      override fun run() {
+    if (ApplicationManager.getApplication().isUnitTestMode) {
+      val collector = task.createCollector() ?: return
+      val result = ApplicationManager.getApplication().runReadAction(Computable { collector.collectSdksUnderReadAction() })
+      task.onLookupCompleted(result)
+      return
+    }
+
+    updateQueue.queue(object : Update(this) {
+      override suspend fun execute() {
         val collector = task.createCollector() ?: return
-        collector.run { collectSdksPromise(this@UnknownSdkCollectorQueue) { task.onLookupCompleted(it) } }
+        val result = readAction {
+          collector.collectSdksUnderReadAction()
+        }
+
+        withContext(Dispatchers.EDT) {
+          task.onLookupCompleted(result)
+        }
+      }
+
+      // not expected to be called in production
+      override fun run() {
+        runBlockingMaybeCancellable {
+         execute()
+        }
       }
     })
   }
@@ -44,8 +69,7 @@ interface UnknownSdkTrackerTask {
   fun createCollector(): UnknownSdkCollector?
 
   /**
-   * Executed only when collector has completed and a given
-   * task is not merged with others.
+   * Executed only when the collector has completed and a given task is not merged with others.
    *
    * NOTE: this callback happened in EDT
    */
