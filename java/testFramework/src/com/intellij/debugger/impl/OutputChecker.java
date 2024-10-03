84910492b8f3a2890eba823ec9f384d5f6b12d36
Egor Ushakov <egor.ushakov@jetbrains.com>
2024-10-03 18:46:53 +0200

    IDEA-360126 Memory leak in com.intellij.rt.debugger.agent.CaptureStorage.ConcurrentIdentityWeakHashMap - disabled exceptions instrumentation
    GitOrigin-RevId: 6f325e954fecceeb70e022e7f887f23f729a73f7

--- java/testFramework/src/com/intellij/debugger/impl/OutputChecker.java
+++ java/testFramework/src/com/intellij/debugger/impl/OutputChecker.java
@@ -264,6 +264,7 @@ public class OutputChecker {
       result = result.replace("-Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 ", "");
       result = result.replace("-Dkotlinx.coroutines.debug.enable.creation.stack.trace=false ", "");
       result = result.replace("-Ddebugger.agent.enable.coroutines=true ", "");
+      result = result.replace("-Ddebugger.agent.support.throwable=false ", "");
       result = result.replaceAll("\\((.*):\\d+\\)", "($1:!LINE_NUMBER!)");
 
       result = fixSlashes(result, JDK_HOME_STR);
