a98bbb7633a30c313f633b14010fdc4d57536236
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-10-04 17:08:57 +0200

    [textmate] API cleanup: remove unused deprecated API (IJPL-156972)
    GitOrigin-RevId: 7157aa702cfcd633f36509cbee3ac551b4ef87cc

--- plugins/textmate/core/src/org/jetbrains/plugins/textmate/language/preferences/ShellVariablesRegistryImpl.java
+++ plugins/textmate/core/src/org/jetbrains/plugins/textmate/language/preferences/ShellVariablesRegistryImpl.java
@@ -2,11 +2,8 @@ package org.jetbrains.plugins.textmate.language.preferences;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.plugins.textmate.Constants;
 import org.jetbrains.plugins.textmate.language.TextMateScopeComparator;
 import org.jetbrains.plugins.textmate.language.syntax.lexer.TextMateScope;
-import org.jetbrains.plugins.textmate.plist.PListValue;
-import org.jetbrains.plugins.textmate.plist.Plist;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -21,24 +18,6 @@ public final class ShellVariablesRegistryImpl implements ShellVariablesRegistry 
     ADD_VARIABLE_FACTORY = key -> Collections.synchronizedList(new CopyOnWriteArrayList<>());
 
   @NotNull private final Map<String, Collection<TextMateShellVariable>> myVariables = new ConcurrentHashMap<>();
-
-  /**
-   * Append table with new variables
-   *
-   * @deprecated use {@link this#addVariable(TextMateShellVariable)} instead
-   */
-  @Deprecated(forRemoval = true)
-  public void fillVariablesFromPlist(@NotNull CharSequence scopeName, @NotNull Plist plist) {
-    final PListValue shellVariables = plist.getPlistValue(Constants.SHELL_VARIABLES_KEY);
-    if (shellVariables != null) {
-      for (PListValue variable : shellVariables.getArray()) {
-        Plist variablePlist = variable.getPlist();
-        String name = variablePlist.getPlistValue(Constants.NAME_KEY, "").getString();
-        String value = variablePlist.getPlistValue(Constants.VALUE_KEY, "").getString();
-        addVariable(new TextMateShellVariable(scopeName, name, value));
-      }
-    }
-  }
 
   public void addVariable(@NotNull TextMateShellVariable variable) {
     if (!variable.name.isEmpty()) {
