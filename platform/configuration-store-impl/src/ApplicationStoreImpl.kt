2a4136e03c49c7b7f34f1fb9ef35d94618024872
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-10-04 17:22:43 +0200

    [platform] API cleanup: remove unused deprecated API NamedJDOMExternalizable (IJPL-156972)
    GitOrigin-RevId: 60a233ca2304d8830228d3961a40e1f291e0afec

--- platform/configuration-store-impl/src/ApplicationStoreImpl.kt
+++ platform/configuration-store-impl/src/ApplicationStoreImpl.kt
@@ -94,10 +94,7 @@ open class ApplicationStoreImpl(private val app: Application) : ComponentStoreWi
 class ApplicationStateStorageManager(pathMacroManager: PathMacroManager? = null, controller: SettingsController?)
   : StateStorageManagerImpl(rootTagName = "application", pathMacroManager?.createTrackingSubstitutor(), componentManager = null, controller)
 {
-  override fun getOldStorageSpec(component: Any, componentName: String, operation: StateStorageOperation): String =
-    @Suppress("DEPRECATION")
-    if (component is com.intellij.openapi.util.NamedJDOMExternalizable) "${component.externalFileName}${PathManager.DEFAULT_EXT}"
-    else StoragePathMacros.NON_ROAMABLE_FILE
+  override fun getOldStorageSpec(component: Any, componentName: String, operation: StateStorageOperation): String = StoragePathMacros.NON_ROAMABLE_FILE
 
   override val isUseXmlProlog: Boolean
     get() = false
