a1ec2591bf62e60920f8590ff11497fea2b03090
Egor Ushakov <egor.ushakov@jetbrains.com>
2024-10-04 19:21:52 +0200

    [debugger] do not call scheduleReload for synthetic breakpoints
    GitOrigin-RevId: 5458f255f65d399e6503e026cfe843c230a5ae52

--- java/debugger/impl/src/com/intellij/debugger/ui/breakpoints/SyntheticLineBreakpoint.java
+++ java/debugger/impl/src/com/intellij/debugger/ui/breakpoints/SyntheticLineBreakpoint.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.debugger.ui.breakpoints;
 
 import com.intellij.debugger.engine.DebugProcessImpl;
@@ -67,6 +67,10 @@ public class SyntheticLineBreakpoint extends LineBreakpoint<JavaLineBreakpointPr
 
   @Override
   public void reload() {
+  }
+
+  @Override
+  void scheduleReload() {
   }
 
   @Override
