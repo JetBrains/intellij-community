9b0f5ef7206e5df8634142724c0e92f1cb970653
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-10-04 13:50:15 +0200

    [Groovy] convert InplaceRenameTest to Java (IDEA-327339)
    GitOrigin-RevId: 05e8b4e98c984b0d7cc298c4b5181d6cfaad3d39

--- plugins/groovy/test/org/jetbrains/plugins/groovy/refactoring/rename/InplaceRenameTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/refactoring/rename/InplaceRenameTest.java
@@ -1,175 +1,178 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.refactoring.rename
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.refactoring.rename;
 
-import com.intellij.testFramework.LightProjectDescriptor
-import com.intellij.testFramework.fixtures.CodeInsightTestUtil
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
-import org.jetbrains.plugins.groovy.refactoring.rename.inplace.GrMethodInplaceRenameHandler
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.CodeInsightTestUtil;
+import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.LightGroovyTestCase;
+import org.jetbrains.plugins.groovy.refactoring.rename.inplace.GrMethodInplaceRenameHandler;
 
-@CompileStatic
-class InplaceRenameTest extends LightGroovyTestCase {
-
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_LATEST
-
+public class InplaceRenameTest extends LightGroovyTestCase {
   private void doTest(String before, String newName, String after) {
-    fixture.configureByText '_.groovy', before
-    CodeInsightTestUtil.doInlineRename(new GrMethodInplaceRenameHandler(), newName, fixture)
-    fixture.checkResult after
+    JavaCodeInsightTestFixture fixture = getFixture();
+    fixture.configureByText("_.groovy", before);
+    CodeInsightTestUtil.doInlineRename(new GrMethodInplaceRenameHandler(), newName, fixture);
+    fixture.checkResult(after);
   }
 
-  void 'test inplace rename method add space'() {
-    doTest(/\
-def fo<caret>o() {}
-
-foo()
-/, 'foo bar', /\
-def 'foo bar'() {}
-
-'foo bar'()
-/)
+  public void testInplaceRenameMethodAddSpace() {
+    doTest("""
+             def fo<caret>o() {}
+             
+             foo()
+             """, "foo bar", """
+             def 'foo bar'() {}
+             
+             'foo bar'()
+             """);
   }
 
-  void "test inplace rename method remove space '"() {
-    doTest(/\
-def 'foo<caret> bar'() {}
-
-'foo bar'()
-'''foo bar'''()
-"foo bar"()
-"""foo bar"""()
-/, 'foo', /\
-def foo() {}
-
-foo()
-foo()
-foo()
-foo()
-/)
+  public void testInplaceRenameMethodRemoveSpaceSingleSingleQuote() {
+    doTest("""
+             def 'foo<caret> bar'() {}
+             
+             'foo bar'()
+             '''foo bar'''()
+             "foo bar"()
+             ""\"foo bar""\"()
+             """, "foo", """
+             def foo() {}
+             
+             foo()
+             foo()
+             foo()
+             foo()
+             """);
   }
 
-  void "test inplace rename method remove space '''"() {
-    doTest(/\
-def '''foo<caret> bar'''() {}
-'foo bar'()
-'''foo bar'''()
-"foo bar"()
-"""foo bar"""()
-/, 'foo', /\
-def foo() {}
-foo()
-foo()
-foo()
-foo()
-/)
+  public void testInplaceRenameMethodRemoveSpaceTripleSingleQuote() {
+    doTest("""
+             def '''foo<caret> bar'''() {}
+             'foo bar'()
+             '''foo bar'''()
+             "foo bar"()
+             ""\"foo bar""\"()
+             """, "foo", """
+             def foo() {}
+             foo()
+             foo()
+             foo()
+             foo()
+             """);
   }
 
-  void 'test inplace rename method remove space "'() {
-    doTest(/\
-def "foo<caret> bar"() {}
-
-'foo bar'()
-'''foo bar'''()
-"foo bar"()
-"""foo bar"""()
-/, 'foo', /\
-def foo() {}
-
-foo()
-foo()
-foo()
-foo()
-/)
+  public void testInplaceRenameMethodRemoveSpaceSingeDoubleQuote() {
+    doTest("""
+             def "foo<caret> bar"() {}
+             
+             'foo bar'()
+             '''foo bar'''()
+             "foo bar"()
+             ""\"foo bar""\"()
+             """, "foo", """
+             def foo() {}
+             
+             foo()
+             foo()
+             foo()
+             foo()
+             """);
   }
 
-  void 'test inplace rename method remove space """'() {
-    doTest(/\
-def """foo<caret> bar"""() {}
-
-'foo bar'()
-'''foo bar'''()
-"foo bar"()
-"""foo bar"""()
-/, 'foo', /\
-def foo() {}
-
-foo()
-foo()
-foo()
-foo()
-/)
+  public void testInplaceRenameMethodRemoveSpaceTripleDoubleQoute() {
+    doTest("""
+             def ""\"foo<caret> bar""\"() {}
+             
+             'foo bar'()
+             '''foo bar'''()
+             "foo bar"()
+             ""\"foo bar""\"()
+             """, "foo", """
+             def foo() {}
+             
+             foo()
+             foo()
+             foo()
+             foo()
+             """);
   }
 
-  void "test inplace rename method '"() {
-    doTest(/\
-def 'foo<caret> bar'() {}
-
-'foo bar'()
-'''foo bar'''()
-"foo bar"()
-"""foo bar"""()
-/, 'foo baz', /\
-def 'foo baz'() {}
-
-'foo baz'()
-'foo baz'()
-'foo baz'()
-'foo baz'()
-/)
+  public void testInplaceRenameMethodSingleSingleQuote() {
+    doTest("""
+             def 'foo<caret> bar'() {}
+             
+             'foo bar'()
+             '''foo bar'''()
+             "foo bar"()
+             ""\"foo bar""\"()
+             """, "foo baz", """
+             def 'foo baz'() {}
+             
+             'foo baz'()
+             'foo baz'()
+             'foo baz'()
+             'foo baz'()
+             """);
   }
 
-  void "test inplace rename method '''"() {
-    doTest(/\
-def '''foo<caret> bar'''() {}
-
-'foo bar'()
-'''foo bar'''()
-"foo bar"()
-"""foo bar"""()
-/, 'foo baz', /\
-def 'foo baz'() {}
-
-'foo baz'()
-'foo baz'()
-'foo baz'()
-'foo baz'()
-/)
+  public void testInplaceRenameMethodTripleSingleQuote() {
+    doTest("""
+             def '''foo<caret> bar'''() {}
+             
+             'foo bar'()
+             '''foo bar'''()
+             "foo bar"()
+             ""\"foo bar""\"()
+             """, "foo baz", """
+             def 'foo baz'() {}
+             
+             'foo baz'()
+             'foo baz'()
+             'foo baz'()
+             'foo baz'()
+             """);
   }
 
-  void 'test inplace rename method "'() {
-    doTest(/\
-def "foo<caret> bar"() {}
-
-'foo bar'()
-'''foo bar'''()
-"foo bar"()
-"""foo bar"""()
-/, 'foo baz', /\
-def 'foo baz'() {}
-
-'foo baz'()
-'foo baz'()
-'foo baz'()
-'foo baz'()
-/)
+  public void testInplaceRenameMethodSingleDoubleQuote() {
+    doTest("""
+             def "foo<caret> bar"() {}
+             
+             'foo bar'()
+             '''foo bar'''()
+             "foo bar"()
+             ""\"foo bar""\"()
+             """, "foo baz", """
+             def 'foo baz'() {}
+             
+             'foo baz'()
+             'foo baz'()
+             'foo baz'()
+             'foo baz'()
+             """);
   }
 
-  void 'test inplace rename method """'() {
-    doTest(/\
-def """foo<caret> bar"""() {}
+  public void testInplaceRenameMethodTripleDoubleQuote() {
+    doTest("""
+             def ""\"foo<caret> bar""\"() {}
+             
+             'foo bar'()
+             '''foo bar'''()
+             "foo bar"()
+             ""\"foo bar""\"()
+             """, "foo baz", """
+             def 'foo baz'() {}
+             
+             'foo baz'()
+             'foo baz'()
+             'foo baz'()
+             'foo baz'()
+             """);
+  }
 
-'foo bar'()
-'''foo bar'''()
-"foo bar"()
-"""foo bar"""()
-/, 'foo baz', /\
-def 'foo baz'() {}
-
-'foo baz'()
-'foo baz'()
-'foo baz'()
-'foo baz'()
-/)
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_LATEST;
   }
 }
