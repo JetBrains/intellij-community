c7510fd475d83154f2c68882932eea0eccab05fb
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-10-06 20:04:43 +0200

    [Groovy] convert ExtractMethodTest to Java (IDEA-327339)
    GitOrigin-RevId: 52e0d58a1aa95871ee90aa2e362a4aa1aa98eb7a

--- plugins/groovy/test/org/jetbrains/plugins/groovy/refactoring/extract/method/ExtractMethodTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/refactoring/extract/method/ExtractMethodTest.java
@@ -1,302 +1,314 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.groovy.refactoring.extract.method
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.refactoring.extract.method;
 
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.util.registry.Registry
-import com.intellij.openapi.util.text.StringUtil
-import com.intellij.psi.*
-import com.intellij.psi.impl.source.PostprocessReformattingAspect
-import com.intellij.refactoring.BaseRefactoringProcessor.ConflictsInTestsException
-import com.intellij.refactoring.util.CommonRefactoringUtil
-import com.intellij.testFramework.LightProjectDescriptor
-import org.jetbrains.annotations.NotNull
-import org.jetbrains.plugins.groovy.GroovyFileType
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
-import org.jetbrains.plugins.groovy.intentions.style.inference.MethodParameterAugmenter
-import org.jetbrains.plugins.groovy.refactoring.extract.InitialInfo
-import org.jetbrains.plugins.groovy.util.TestUtils
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.util.registry.Registry;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.*;
+import com.intellij.psi.impl.source.PostprocessReformattingAspect;
+import com.intellij.refactoring.BaseRefactoringProcessor;
+import com.intellij.refactoring.util.CommonRefactoringUtil;
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyFileType;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.LightGroovyTestCase;
+import org.jetbrains.plugins.groovy.intentions.style.inference.MethodParameterAugmenter;
+import org.jetbrains.plugins.groovy.refactoring.extract.InitialInfo;
+import org.jetbrains.plugins.groovy.util.TestUtils;
 
-class ExtractMethodTest extends LightGroovyTestCase {
+import java.util.List;
 
-  final String basePath = TestUtils.testDataPath + 'groovy/refactoring/extractMethod/'
-
+public class ExtractMethodTest extends LightGroovyTestCase {
   @NotNull
   @Override
   protected LightProjectDescriptor getProjectDescriptor() {
-    return GroovyProjectDescriptors.GROOVY_LATEST
+    return GroovyProjectDescriptors.GROOVY_LATEST;
   }
 
   private void doAntiTest(String errorMessage) {
-    GroovyExtractMethodHandler handler = configureFromText(readInput()[0], "testMethod")
+    GroovyExtractMethodHandler handler = configureFromText(readInput().get(0), "testMethod");
     try {
-      handler.invoke(project, myFixture.editor, myFixture.file, null)
-      assertTrue(false)
+      handler.invoke(getProject(), myFixture.getEditor(), myFixture.getFile(), null);
+      fail();
     }
     catch (CommonRefactoringUtil.RefactoringErrorHintException e) {
-      assertEquals(errorMessage, e.localizedMessage)
+      assertEquals(errorMessage, e.getLocalizedMessage());
     }
   }
 
   private List<String> readInput() {
-    return TestUtils.readInput(testDataPath + getTestName(true) + ".test")
+    return TestUtils.readInput(getTestDataPath() + getTestName(true) + ".test");
   }
 
-  private void doTest(String name = 'testMethod') {
-    final List<String> data = readInput()
-    final String before = data[0]
-    def after = StringUtil.trimEnd(data[1], '\n')
-
-    doTest(name, before, after)
+  private void doTest() {
+    doTest("testMethod");
   }
 
-  private void doTest(String name = 'testMethod', String before, String after) {
-    GroovyExtractMethodHandler handler = configureFromText(before, name)
+  private void doTest(String name) {
+    final List<String> data = readInput();
+    final String before = data.get(0);
+    String after = StringUtil.trimEnd(data.get(1), "\n");
+
+    doTest(name, before, after);
+  }
+
+  private void doTest(String before, String after) {
+    doTest("testMethod", before, after);
+  }
+
+  private void doTest(String name, String before, String after) {
+    GroovyExtractMethodHandler handler = configureFromText(before, name);
     try {
-      handler.invoke(project, myFixture.editor, myFixture.file, null)
-      PostprocessReformattingAspect.getInstance(project).doPostponedFormatting()
-      myFixture.checkResult(after)
+      handler.invoke(getProject(), myFixture.getEditor(), myFixture.getFile(), null);
+      PostprocessReformattingAspect.getInstance(getProject()).doPostponedFormatting();
+      myFixture.checkResult(after);
     }
-    catch (ConflictsInTestsException e) {
-      ApplicationManager.application.runWriteAction {
-        myFixture.getDocument(myFixture.file).text = e.message
-        PsiDocumentManager.getInstance(myFixture.project).commitAllDocuments()
-      }
+    catch (BaseRefactoringProcessor.ConflictsInTestsException e) {
+      ApplicationManager.getApplication().runWriteAction(() -> {
+        myFixture.getDocument(myFixture.getFile()).setText(e.getMessage());
+        PsiDocumentManager.getInstance(myFixture.getProject()).commitAllDocuments();
+      });
 
-      myFixture.checkResult(after)
+      myFixture.checkResult(after);
     }
   }
 
   private GroovyExtractMethodHandler configureFromText(String fileText, final String name) {
-    final caret = fileText.indexOf(TestUtils.CARET_MARKER)
+    final int caret = fileText.indexOf(TestUtils.CARET_MARKER);
     if (caret >= 0) {
-      myFixture.configureByText(GroovyFileType.GROOVY_FILE_TYPE, fileText)
+      myFixture.configureByText(GroovyFileType.GROOVY_FILE_TYPE, fileText);
     }
     else {
-      int startOffset = fileText.indexOf(TestUtils.BEGIN_MARKER)
-      fileText = TestUtils.removeBeginMarker(fileText)
-      int endOffset = fileText.indexOf(TestUtils.END_MARKER)
-      fileText = TestUtils.removeEndMarker(fileText)
-      myFixture.configureByText(GroovyFileType.GROOVY_FILE_TYPE, fileText)
-      myFixture.editor.selectionModel.setSelection(startOffset, endOffset)
+      int startOffset = fileText.indexOf(TestUtils.BEGIN_MARKER);
+      fileText = TestUtils.removeBeginMarker(fileText);
+      int endOffset = fileText.indexOf(TestUtils.END_MARKER);
+      fileText = TestUtils.removeEndMarker(fileText);
+      myFixture.configureByText(GroovyFileType.GROOVY_FILE_TYPE, fileText);
+      myFixture.getEditor().getSelectionModel().setSelection(startOffset, endOffset);
     }
 
     return new GroovyExtractMethodHandler() {
       @Override
       protected ExtractMethodInfoHelper getSettings(@NotNull InitialInfo initialInfo, PsiClass owner) {
-        final ExtractMethodInfoHelper helper = new ExtractMethodInfoHelper(initialInfo, name, owner, true)
-        final PsiType type = helper.getOutputType()
+        final ExtractMethodInfoHelper helper = new ExtractMethodInfoHelper(initialInfo, name, owner, true);
+        final PsiType type = helper.getOutputType();
         if (type.equalsToText(CommonClassNames.JAVA_LANG_OBJECT) || PsiTypes.voidType().equals(type)) {
-          helper.setSpecifyType(false)
+          helper.setSpecifyType(false);
         }
-        return helper
+        return helper;
       }
-    }
+    };
   }
 
-  void testIf1() throws Throwable { doTest() }
+  public void testIf1() { doTest(); }
 
-  void testCaptured1() throws Throwable { doTest() }
+  public void testCaptured1() { doTest(); }
 
-  void testCaptured2() throws Throwable { doTest() }
+  public void testCaptured2() { doTest(); }
 
-  void testOuterClosureReference() throws Throwable { doTest() }
+  public void testOuterClosureReference() { doTest(); }
 
-  void testClos_em() throws Throwable { doTest() }
+  public void testClos_em() { doTest(); }
 
-  void testEm1() throws Throwable { doTest() }
+  public void testEm1() { doTest(); }
 
-  void testEnum1() throws Throwable { doTest() }
+  public void testEnum1() { doTest(); }
 
-  void testErr1() throws Throwable { doTest() }
+  public void testErr1() { doTest(); }
 
-  void testExpr1() throws Throwable { doTest() }
+  public void testExpr1() { doTest(); }
 
-  void testExpr2() throws Throwable { doTest() }
+  public void testExpr2() { doTest(); }
 
-  void testExpr3() throws Throwable { doTest() }
+  public void testExpr3() { doTest(); }
 
-  void testInput1() throws Throwable { doTest() }
+  public void testInput1() { doTest(); }
 
-  void testInput2() throws Throwable { doTest() }
+  public void testInput2() { doTest(); }
 
-  void testInter1() throws Throwable { doTest() }
+  public void testInter1() { doTest(); }
 
-  void testInter2() throws Throwable { doAntiTest("Refactoring is not supported when return statement interrupts the execution flow") }
+  public void testInter2() { doAntiTest("Refactoring is not supported when return statement interrupts the execution flow"); }
 
-  void testInter3() throws Throwable { doAntiTest("Refactoring is not supported when return statement interrupts the execution flow") }
+  public void testInter3() { doAntiTest("Refactoring is not supported when return statement interrupts the execution flow"); }
 
-  void testInter4() throws Throwable { doTest() }
+  public void testInter4() { doTest(); }
 
-  void testMeth_em1() throws Throwable { doTest() }
+  public void testMeth_em1() { doTest(); }
 
-  void testMeth_em2() throws Throwable { doTest() }
+  public void testMeth_em2() { doTest(); }
 
-  void testMeth_em3() throws Throwable { doTest() }
+  public void testMeth_em3() { doTest(); }
 
-  void testOutput1() throws Throwable { doTest() }
+  public void testOutput1() { doTest(); }
 
-  void testResul1() throws Throwable { doTest() }
+  public void testResul1() { doTest(); }
 
-  void testRet1() throws Throwable { doTest() }
+  public void testRet1() { doTest(); }
 
-  void testRet2() throws Throwable { doTest() }
+  public void testRet2() { doTest(); }
 
-  void testRet3() throws Throwable { doTest() }
+  public void testRet3() { doTest(); }
 
-  void testRet4() throws Throwable { doAntiTest("Refactoring is not supported when return statement interrupts the execution flow") }
+  public void testRet4() { doAntiTest("Refactoring is not supported when return statement interrupts the execution flow"); }
 
-  void testVen1() throws Throwable { doTest() }
+  public void testVen1() { doTest(); }
 
-  void testVen2() throws Throwable { doTest() }
+  public void testVen2() { doTest(); }
 
-  void testVen3() throws Throwable { doTest() }
+  public void testVen3() { doTest(); }
 
-  void testForIn() throws Throwable { doTest() }
+  public void testForIn() { doTest(); }
 
-  void testInCatch() { doTest() }
+  public void testInCatch() { doTest(); }
 
-  void testClosureIt() throws Throwable { doTest() }
+  public void testClosureIt() { doTest(); }
 
-  void testImplicitReturn() { doTest() }
+  public void testImplicitReturn() { doTest(); }
 
-  void testMultiOutput1() { doTest() }
+  public void testMultiOutput1() { doTest(); }
 
-  void testMultiOutput2() { doTest() }
+  public void testMultiOutput2() { doTest(); }
 
-  void testMultiOutput3() { doTest() }
+  public void testMultiOutput3() { doTest(); }
 
-  void testMultiOutput4() { doTest() }
+  public void testMultiOutput4() { doTest(); }
 
-  void testMultiOutput5() { doTest() }
+  public void testMultiOutput5() { doTest(); }
 
-  void testDontShortenRefsIncorrect() { doTest() }
+  public void testDontShortenRefsIncorrect() { doTest(); }
 
-  void testLastBlockStatementInterruptsControlFlow() { doTest() }
+  public void testLastBlockStatementInterruptsControlFlow() { doTest(); }
 
-  void testAOOBE() { doTest() }
+  public void testAOOBE() { doTest(); }
 
-  void testWildCardReturnType() { doTest() }
+  public void testWildCardReturnType() { doTest(); }
 
-  void testParamChangedInsideExtractedMethod() { doTest() }
+  public void testParamChangedInsideExtractedMethod() { doTest(); }
 
-  void testTerribleAppStatement() { doTest() }
+  public void testTerribleAppStatement() { doTest(); }
 
-  void testArgsUsedOnlyInClosure() { doTest() }
+  public void testArgsUsedOnlyInClosure() { doTest(); }
 
-  void testArgsUsedOnlyInAnonymousClass() { doTest() }
+  public void testArgsUsedOnlyInAnonymousClass() { doTest(); }
 
-  void testTwoVars() { doTest() }
+  public void testTwoVars() { doTest(); }
 
-  void testContextConflicts() { doTest() }
+  public void testContextConflicts() { doTest(); }
 
-  void testNoContextConflicts() { doTest() }
+  public void testNoContextConflicts() { doTest(); }
 
-  void testTupleDeclaration() { doTest() }
+  public void testTupleDeclaration() { doTest(); }
 
-  void testNonIdentifierName() { doTest('f*f') }
+  public void testNonIdentifierName() { doTest("f*f"); }
 
-  void testAutoSelectExpression() { doTest() }
+  public void testAutoSelectExpression() { doTest(); }
 
-  void testUnassignedVar() { doTest() }
+  public void testUnassignedVar() { doTest(); }
 
-  void testForInLoop() {
-    def registryValue = Registry.is(MethodParameterAugmenter.GROOVY_COLLECT_METHOD_CALLS_FOR_INFERENCE)
-    Registry.get(MethodParameterAugmenter.GROOVY_COLLECT_METHOD_CALLS_FOR_INFERENCE).setValue(true)
+  public void testForInLoop() {
+    boolean registryValue = Registry.is(MethodParameterAugmenter.GROOVY_COLLECT_METHOD_CALLS_FOR_INFERENCE);
+    Registry.get(MethodParameterAugmenter.GROOVY_COLLECT_METHOD_CALLS_FOR_INFERENCE).setValue(true);
     try {
-      doTest()
+      doTest();
     }
     finally {
-      Registry.get(MethodParameterAugmenter.GROOVY_COLLECT_METHOD_CALLS_FOR_INFERENCE).setValue(registryValue)
+      Registry.get(MethodParameterAugmenter.GROOVY_COLLECT_METHOD_CALLS_FOR_INFERENCE).setValue(registryValue);
     }
   }
 
-  void testStringPart0() {
-    doTest('''\
-def foo() {
-    print 'a<begin>b<end>c'
-}
-''', '''\
-def foo() {
-    print 'a' +<caret> testMethod() + 'c'
-}
-
-private String testMethod() {
-    return 'b'
-}
-''')
+  public void testStringPart0() {
+    doTest("""
+             def foo() {
+                 print 'a<begin>b<end>c'
+             }
+             """, """
+             def foo() {
+                 print 'a' +<caret> testMethod() + 'c'
+             }
+             
+             private String testMethod() {
+                 return 'b'
+             }
+             """);
   }
 
-  void testSingleExpressionAsReturnValue() {
-    doTest('''\
-int foo() {
-    <begin>1<end>
-}
-''', '''\
-int foo() {
-    testMethod()
-}
-
-private int testMethod() {
-    return 1
-}
-''')
+  public void testSingleExpressionAsReturnValue() {
+    doTest("""
+             int foo() {
+                 <begin>1<end>
+             }
+             """, """
+             int foo() {
+                 testMethod()
+             }
+             
+             private int testMethod() {
+                 return 1
+             }
+             """);
   }
 
-  void testExtractMethodFromStaticFieldClosureInitializer() {
-    doTest('''\
-class Foo {
-    static constraints = {
-        bar validator: { val, obj ->
-            <begin>println "validating ${obj}.$val"<end>
-        }
-    }
-}
-''', '''\
-class Foo {
-    static constraints = {
-        bar validator: { val, obj ->
-            testMethod(obj, val)
-        }
-    }
-
-    private static testMethod(obj, val) {
-        println "validating ${obj}.$val"
-    }
-}
-''')
+  public void testExtractMethodFromStaticFieldClosureInitializer() {
+    doTest("""
+             class Foo {
+                 static constraints = {
+                     bar validator: { val, obj ->
+                         <begin>println "validating ${obj}.$val"<end>
+                     }
+                 }
+             }
+             """, """
+             class Foo {
+                 static constraints = {
+                     bar validator: { val, obj ->
+                         testMethod(obj, val)
+                     }
+                 }
+             
+                 private static testMethod(obj, val) {
+                     println "validating ${obj}.$val"
+                 }
+             }
+             """);
   }
 
-  void testExtractMethodFromStaticFieldInitializer() {
-    doTest('''\
-class Foo {
-    static constraints = <begin>2<end>
-}
-''', '''\
-class Foo {
-    static constraints = testMethod()
-
-    private static int testMethod() {
-        return 2
-    }
-}
-''')
+  public void testExtractMethodFromStaticFieldInitializer() {
+    doTest("""
+             class Foo {
+                 static constraints = <begin>2<end>
+             }
+             """, """
+             class Foo {
+                 static constraints = testMethod()
+             
+                 private static int testMethod() {
+                     return 2
+                 }
+             }
+             """);
   }
 
-  void testExtractMethodFromStringPart() {
-    doTest('-', '''\
-print 'a<begin>b<end>c'
-''', '''\
-print 'a' + '-'() + 'c'
-
-private String '-'() {
-    return 'b'
-}
-''')
+  public void testExtractMethodFromStringPart() {
+    doTest("-", """
+      print 'a<begin>b<end>c'
+      """, """
+             print 'a' + '-'() + 'c'
+             
+             private String '-'() {
+                 return 'b'
+             }
+             """);
   }
 
-  void testClassVsDefaultGDKClass() {
-    myFixture.addClass('package javax.sound.midi; public class Sequence { }')
-    doTest()
+  public void testClassVsDefaultGDKClass() {
+    myFixture.addClass("package javax.sound.midi; public class Sequence { }");
+    doTest();
+  }
+
+  @Override
+  public final String getBasePath() {
+    return TestUtils.getTestDataPath() + "groovy/refactoring/extractMethod/";
   }
 }
