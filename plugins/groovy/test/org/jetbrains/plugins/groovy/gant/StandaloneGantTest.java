663686fb7b4fcec18a165582c8447b1b5af8897c
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-10-08 20:10:12 +0200

    [Groovy] convert StandaloneGantTest to Java (IDEA-327339)
    GitOrigin-RevId: 44c3b8c453b854f09be2b8aa9430adc031103a3e

--- plugins/groovy/test/org/jetbrains/plugins/groovy/gant/StandaloneGantTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/gant/StandaloneGantTest.java
@@ -1,89 +1,89 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.gant
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.gant;
 
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.io.FileUtil
-import com.intellij.testFramework.LightProjectDescriptor
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
-import org.jetbrains.annotations.NotNull
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.util.SdkHomeBean
-import org.jetbrains.plugins.groovy.util.TestUtils
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.util.SdkHomeBean;
+import org.jetbrains.plugins.groovy.util.TestUtils;
 
-class StandaloneGantTest extends LightJavaCodeInsightFixtureTestCase {
-
+public class StandaloneGantTest extends LightJavaCodeInsightFixtureTestCase {
   @NotNull
   @Override
   protected LightProjectDescriptor getProjectDescriptor() {
-    return GroovyProjectDescriptors.GROOVY_1_7
+    return GroovyProjectDescriptors.GROOVY_1_7;
   }
 
   @Override
   protected String getBasePath() {
-    return TestUtils.getTestDataPath() + "gant/completion"
+    return TestUtils.getTestDataPath() + "gant/completion";
   }
 
-  @Override protected void setUp() {
-    super.setUp()
-    final SdkHomeBean state = new SdkHomeBean()
-    state.setSdkHome(FileUtil.toSystemIndependentName("${TestUtils.absoluteTestDataPath}mockGantLib"))
-    GantSettings.getInstance(getProject()).loadState state
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    final SdkHomeBean state = new SdkHomeBean();
+    state.setSdkHome(FileUtil.toSystemIndependentName(TestUtils.getAbsoluteTestDataPath() + "mockGantLib"));
+    GantSettings.getInstance(getProject()).loadState(state);
   }
 
-  @Override protected void tearDown() {
+  @Override
+  protected void tearDown() throws Exception {
     try {
-      Project project = getProject()
+      Project project = getProject();
       if (project != null) {
-        GantSettings.getInstance(project).loadState new SdkHomeBean()
+        GantSettings.getInstance(project).loadState(new SdkHomeBean());
       }
+    }
+    catch (Throwable e) {
+      addSuppressedException(e);
     }
     finally {
-      super.tearDown()
+      super.tearDown();
     }
   }
 
-  void checkVariants(String text, String... items) {
-    myFixture.configureByText "a.gant", text
-    myFixture.completeBasic()
-    assertSameElements myFixture.lookupElementStrings, items
+  public void checkVariants(String text, String... items) {
+    myFixture.configureByText("a.gant", text);
+    myFixture.completeBasic();
+    assertSameElements(myFixture.getLookupElementStrings(), items);
   }
 
-  void testDep() throws Throwable {
-    checkVariants """
-target(aaa: "") {
-    depend<caret>
-}
-""", "depends", "dependset"
+  public void testDep() {
+    checkVariants("""
+                    target(aaa: "") {
+                        depend<caret>
+                    }
+                    """, "depends", "dependset");
   }
 
-  void testPatternset() throws Exception {
-    checkVariants "ant.patt<caret>t", "patternset"
+  public void testPatternset() {
+    checkVariants("ant.patt<caret>t", "patternset");
   }
 
-  void testOptionalArgumentsHighlighting() throws Exception {
-    myFixture.configureByText "a.gant", """
-    ant.java(classname: "com.intellij.util.io.zip.ReorderJarsMain", fork: "true") {
-      arg(value: "aaa")
-      classpath {
-        pathelement(location: "sss")
+  public void testOptionalArgumentsHighlighting() {
+    myFixture.configureByText("a.gant", """
+      ant.java(classname: "com.intellij.util.io.zip.ReorderJarsMain", fork: "true") {
+        arg(value: "aaa")
+        classpath {
+          pathelement(location: "sss")
+        }
       }
-    }
-"""
-    myFixture.checkHighlighting(true, false, false)
+      """);
+    myFixture.checkHighlighting(true, false, false);
   }
 
-  void testPathElement() throws Exception {
-    checkVariants """
-    ant.java(classname: "com.intellij.util.io.zip.ReorderJarsMain", fork: "true") {
-      arg(value: "aaa")
-      classpath {
-        pathele<caret>ment(location: "sss")
-      }
-    }
-""", "pathelement"
+  public void testPathElement() {
+    checkVariants("""
+                    ant.java(classname: "com.intellij.util.io.zip.ReorderJarsMain", fork: "true") {
+                      arg(value: "aaa")
+                      classpath {
+                        pathele<caret>ment(location: "sss")
+                      }
+                    }
+                    """, "pathelement");
   }
-
-  static final def GANT_JARS = ["gant.jar", "ant.jar", "ant-junit.jar", "ant-launcher.jar", "commons.jar"]
-
 }
-
