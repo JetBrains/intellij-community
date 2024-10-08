b096e96c04cc07635bd7b8d03a20e4a60d8bdbfb
Yann Cébron <yann.cebron@jetbrains.com>
2024-10-08 12:35:00 +0200

    [devkit] ListenerImplementationMustNotBeDisposableInspection: migrate to JvmInspection
    GitOrigin-RevId: 8286276193767057c6e09c6f9a755307c44cf3d9

--- plugins/devkit/devkit-core/src/inspections/ListenerImplementationMustNotBeDisposableInspection.kt
+++ plugins/devkit/devkit-core/src/inspections/ListenerImplementationMustNotBeDisposableInspection.kt
@@ -1,44 +1,41 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.inspections
 
-import com.intellij.codeInspection.InspectionManager
-import com.intellij.codeInspection.ProblemDescriptor
-import com.intellij.codeInspection.registerUProblem
-import com.intellij.lang.jvm.JvmClassKind
+import com.intellij.lang.jvm.DefaultJvmElementVisitor
+import com.intellij.lang.jvm.JvmClass
+import com.intellij.lang.jvm.JvmElementVisitor
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiClass
 import com.intellij.psi.util.InheritanceUtil
-import com.intellij.psi.util.PsiUtil
 import org.jetbrains.idea.devkit.DevKitBundle
 import org.jetbrains.idea.devkit.dom.index.IdeaPluginRegistrationIndex
 import org.jetbrains.idea.devkit.util.PluginRelatedLocatorsUtils
-import org.jetbrains.uast.UClass
 
-internal class ListenerImplementationMustNotBeDisposableInspection : DevKitUastInspectionBase(UClass::class.java) {
+internal class ListenerImplementationMustNotBeDisposableInspection : DevKitJvmInspection() {
 
-  override fun checkClass(aClass: UClass, manager: InspectionManager, isOnTheFly: Boolean): Array<ProblemDescriptor>? {
-    val psiClass = aClass.javaPsi
-
-    if (psiClass.classKind != JvmClassKind.CLASS ||
-        PsiUtil.isAbstractClass(psiClass) ||
-        PsiUtil.isLocalOrAnonymousClass(psiClass) ||
-        PsiUtil.isInnerClass(psiClass) ||
-        psiClass.qualifiedName == null) return ProblemDescriptor.EMPTY_ARRAY
-
-    if (!InheritanceUtil.isInheritor(psiClass, Disposable::class.java.canonicalName)) return ProblemDescriptor.EMPTY_ARRAY
-
-    if (!isRegisteredListener(psiClass)) return ProblemDescriptor.EMPTY_ARRAY
-
-    val holder = createProblemsHolder(aClass, manager, isOnTheFly)
-    holder.registerUProblem(
-      aClass,
-      DevKitBundle.message("inspections.listener.implementation.must.not.implement.disposable")
-    )
-    return holder.resultsArray
+  override fun buildVisitor(project: Project, sink: HighlightSink, isOnTheFly: Boolean): JvmElementVisitor<Boolean?>? {
+    return object : DefaultJvmElementVisitor<Boolean> {
+      override fun visitClass(clazz: JvmClass): Boolean {
+        val sourceElement = clazz.sourceElement
+        if (sourceElement !is PsiClass) return true
+        checkClass(project, sourceElement, sink)
+        return true
+      }
+    }
   }
 
-  private fun isRegisteredListener(psiClass: PsiClass): Boolean {
-    val project = psiClass.project
+  private fun checkClass(project: Project, psiClass: PsiClass, sink: HighlightSink) {
+    if (!ExtensionUtil.isExtensionPointImplementationCandidate(psiClass)) return
+
+    if (!InheritanceUtil.isInheritor(psiClass, Disposable::class.java.canonicalName)) return
+
+    if (!isRegisteredListener(project, psiClass)) return
+
+    sink.highlight(DevKitBundle.message("inspections.listener.implementation.must.not.implement.disposable"))
+  }
+
+  private fun isRegisteredListener(project: Project, psiClass: PsiClass): Boolean {
     return !IdeaPluginRegistrationIndex.processListener(
       project,
       psiClass,
