4452027130e09188be9991e18828aad4ae0cf0ce
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-10-08 20:05:43 +0200

    [Groovy] convert GroovyGoToTypeDeclarationTest to Java (IDEA-327339)
    GitOrigin-RevId: 5f2d3cf2fced29a2baa1ac0fff471a1453869070

--- plugins/groovy/test/org/jetbrains/plugins/groovy/GroovyGoToTypeDeclarationTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/GroovyGoToTypeDeclarationTest.java
@@ -1,67 +1,52 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jetbrains.plugins.groovy
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy;
 
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
-import com.intellij.codeInsight.navigation.actions.GotoTypeDeclarationAction
-import com.intellij.psi.PsiClass
+import com.intellij.codeInsight.navigation.actions.GotoTypeDeclarationAction;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 
-class GroovyGoToTypeDeclarationTest extends LightJavaCodeInsightFixtureTestCase {
-
-  void testGoToTypeDeclarationMethod() {
+public class GroovyGoToTypeDeclarationTest extends LightJavaCodeInsightFixtureTestCase {
+  public void testGoToTypeDeclarationMethod() {
     myFixture.configureByText("g.groovy", """
-class A {
+      class A {
+      
+        public def method() {
+          return new B();
+        }
+      
+        {
+          method<caret>()
+        }
+      }
+      
+      class B {
+      
+      }
+      """);
 
-  public def method() {
-    return new B();
+    PsiElement res = GotoTypeDeclarationAction.findSymbolType(myFixture.getEditor(), myFixture.getCaretOffset());
+    assertInstanceOf(res, PsiClass.class);
+    assertEquals("B", ((PsiClass)res).getName());
   }
 
-  {
-    method<caret>()
-  }
-}
-
-class B {
-
-}
-""")
-
-    def res = GotoTypeDeclarationAction.findSymbolType(myFixture.editor, myFixture.caretOffset)
-    assertInstanceOf(res, PsiClass.class)
-    assertEquals("B", ((PsiClass)res).getName())
-  }
-
-  void testGoToTypeDeclarationVariable() {
+  public void testGoToTypeDeclarationVariable() {
     myFixture.configureByText("g.groovy", """
-class A {
+      class A {
+      
+        {
+          def a = new B()
+          println(a<caret>)
+        }
+      }
+      
+      class B {
+      
+      }
+      """);
 
-  {
-    def a = new B()
-    println(a<caret>)
+    PsiElement res = GotoTypeDeclarationAction.findSymbolType(myFixture.getEditor(), myFixture.getCaretOffset());
+    assertInstanceOf(res, PsiClass.class);
+    assertEquals("B", ((PsiClass)res).getName());
   }
-}
-
-class B {
-
-}
-""")
-
-    def res = GotoTypeDeclarationAction.findSymbolType(myFixture.editor, myFixture.caretOffset)
-    assertInstanceOf(res, PsiClass.class)
-    assertEquals("B", ((PsiClass)res).getName())
-  }
-
 }
