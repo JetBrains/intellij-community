a99902c6189e27eac212b8ac0c96a96b039680c3
Yann Cébron <yann.cebron@jetbrains.com>
2024-10-08 12:43:08 +0200

    [devkit] JComponentDataProviderInspection: migrate to JvmInspection
    GitOrigin-RevId: 48b536e22c56f9f35f5f69d3a347c828f5c74645

--- plugins/devkit/devkit-core/src/inspections/JComponentDataProviderInspection.kt
+++ plugins/devkit/devkit-core/src/inspections/JComponentDataProviderInspection.kt
@@ -1,42 +1,50 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.inspections
 
-import com.intellij.codeInspection.InspectionManager
-import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.codeInspection.registerUProblem
+import com.intellij.lang.jvm.DefaultJvmElementVisitor
+import com.intellij.lang.jvm.JvmClass
 import com.intellij.lang.jvm.JvmClassKind
+import com.intellij.lang.jvm.JvmElementVisitor
 import com.intellij.openapi.actionSystem.DataProvider
 import com.intellij.openapi.actionSystem.UiCompatibleDataProvider
 import com.intellij.openapi.actionSystem.UiDataProvider
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiClass
 import com.intellij.psi.util.InheritanceUtil
 import org.jetbrains.idea.devkit.DevKitBundle
-import org.jetbrains.uast.UClass
 import javax.swing.JComponent
 
-internal class JComponentDataProviderInspection : DevKitUastInspectionBase(UClass::class.java) {
+internal class JComponentDataProviderInspection : DevKitJvmInspection() {
 
   override fun isAllowed(holder: ProblemsHolder): Boolean {
     return super.isAllowed(holder) &&
            DevKitInspectionUtil.isClassAvailable(holder, UiDataProvider::class.java.name)
   }
 
-  override fun checkClass(uClass: UClass, manager: InspectionManager, isOnTheFly: Boolean): Array<out ProblemDescriptor?>? {
-    val psiClass = uClass.javaPsi
-    if (psiClass.classKind != JvmClassKind.CLASS) return null
+  override fun buildVisitor(project: Project, sink: HighlightSink, isOnTheFly: Boolean): JvmElementVisitor<Boolean?>? {
+    return object : DefaultJvmElementVisitor<Boolean> {
+      override fun visitClass(clazz: JvmClass): Boolean {
+        val sourceElement = clazz.sourceElement
+        if (sourceElement !is PsiClass) return true
+        checkClass(sourceElement, sink)
+        return true
+      }
+    }
+  }
+
+  private fun checkClass(psiClass: PsiClass, sink: HighlightSink) {
+    if (psiClass.classKind != JvmClassKind.CLASS) return
 
     @Suppress("UsagesOfObsoleteApi")
-    if (!InheritanceUtil.isInheritor(psiClass, DataProvider::class.java.canonicalName)) return null
+    if (!InheritanceUtil.isInheritor(psiClass, DataProvider::class.java.canonicalName)) return
 
     @Suppress("UsagesOfObsoleteApi")
-    if (InheritanceUtil.isInheritor(psiClass, UiCompatibleDataProvider::class.java.canonicalName)) return null
+    if (InheritanceUtil.isInheritor(psiClass, UiCompatibleDataProvider::class.java.canonicalName)) return
 
-    if (!InheritanceUtil.isInheritor(psiClass, JComponent::class.java.canonicalName)) return null
+    if (!InheritanceUtil.isInheritor(psiClass, JComponent::class.java.canonicalName)) return
 
-    val holder = createProblemsHolder(uClass, manager, isOnTheFly)
-    holder.registerUProblem(uClass,
-                            DevKitBundle.message("inspections.jcomponent.data.provider.use.uidataprovider.instead.of.dataprovider"))
-    return holder.resultsArray
+    sink.highlight(DevKitBundle.message("inspections.jcomponent.data.provider.use.uidataprovider.instead.of.dataprovider"))
   }
 
 }
