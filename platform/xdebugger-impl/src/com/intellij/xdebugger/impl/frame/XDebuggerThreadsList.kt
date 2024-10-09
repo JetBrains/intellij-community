b55bf5d4723fa1a1e7d272ed7f812d5d6b630e83
Alexey Korovin <Alexey.Korovin@jetbrains.com>
2024-10-09 12:23:02 +0200

    Use `XExecutionStack.SELECTED_STACKS` constant in NavigateToParallelStacksFromStackFrameAction
    GitOrigin-RevId: 2d49a1dc43db36d8d3b8e1247f1f2abbb53ab198

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/frame/XDebuggerThreadsList.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/frame/XDebuggerThreadsList.kt
@@ -30,10 +30,7 @@ class XDebuggerThreadsList(private val renderer: ListCellRenderer<StackInfo>
 ) : JBList<StackInfo>(CollectionListModel()), UiDataProvider {
     private var mySelectedFrame: StackInfo? = null
 
-    //NOTE(Korovin): This field is here to allow actions to show a context menu without switching an active thread and still provide a
-    //context for actions which depend on a specific thread.
-    var stackUnderMouse: StackInfo? = null
-      private set
+    private var stackUnderMouse: StackInfo? = null
 
     val elementCount: Int
         get() = model.size
