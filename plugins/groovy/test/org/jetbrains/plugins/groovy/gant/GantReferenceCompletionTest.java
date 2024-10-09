df28aab46a344c01f02c355e056f3feb6911f1fb
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-10-09 09:28:47 +0200

    [Groovy] convert GantReferenceCompletionTest to Java (IDEA-327339)
    GitOrigin-RevId: 3ccb9ddf96f06143def910ec7e70bb7bf106a61b

--- plugins/groovy/test/org/jetbrains/plugins/groovy/gant/GantReferenceCompletionTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/gant/GantReferenceCompletionTest.java
@@ -1,143 +1,148 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.gant
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.gant;
 
-import com.intellij.testFramework.LightProjectDescriptor
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.LibraryLightProjectDescriptor
-import org.jetbrains.plugins.groovy.RepositoryTestLibrary
-import org.jetbrains.plugins.groovy.codeInspection.assignment.GroovyAssignabilityCheckInspection
-import org.jetbrains.plugins.groovy.codeInspection.untypedUnresolvedAccess.GroovyUntypedAccessInspection
-import org.jetbrains.plugins.groovy.util.TestUtils
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.LibraryLightProjectDescriptor;
+import org.jetbrains.plugins.groovy.RepositoryTestLibrary;
+import org.jetbrains.plugins.groovy.codeInspection.assignment.GroovyAssignabilityCheckInspection;
+import org.jetbrains.plugins.groovy.codeInspection.untypedUnresolvedAccess.GroovyUntypedAccessInspection;
+import org.jetbrains.plugins.groovy.util.TestUtils;
 
-import static org.jetbrains.plugins.groovy.GroovyProjectDescriptors.LIB_GROOVY_1_7
+import java.util.List;
 
-@CompileStatic
-class GantReferenceCompletionTest extends LightJavaCodeInsightFixtureTestCase {
-
-  private static final LightProjectDescriptor GANT_PROJECT = new LibraryLightProjectDescriptor(
-    LIB_GROOVY_1_7 + new RepositoryTestLibrary('org.codehaus.gant:gant_groovy1.7:1.9.7')
-  )
-
-  final LightProjectDescriptor projectDescriptor = GANT_PROJECT
-  final String basePath = TestUtils.testDataPath + "gant/completion"
-
-  void complete(String text) {
-    myFixture.configureByText "a.gant", text
-    myFixture.completeBasic()
+public class GantReferenceCompletionTest extends LightJavaCodeInsightFixtureTestCase {
+  private static final LightProjectDescriptor GANT_PROJECT =
+    new LibraryLightProjectDescriptor(GroovyProjectDescriptors.LIB_GROOVY_1_7.plus(
+      new RepositoryTestLibrary("org.codehaus.gant:gant_groovy1.7:1.9.7")));
+  
+  public void complete(String text) {
+    myFixture.configureByText("a.gant", text);
+    myFixture.completeBasic();
   }
 
-  void checkVariants(String text, String... items) {
-    complete text
-    assert myFixture.lookupElementStrings.containsAll(items as List)
+  public void checkVariants(String text, String... items) {
+    complete(text);
+    assertTrue(myFixture.getLookupElementStrings().containsAll(DefaultGroovyMethods.asType(items, List.class)));
   }
 
-  void testDep() {
-    checkVariants """
-target(aaa: "") {
-    dep<caret>
-}
-""", "depends", "dependset"
+  public void testDep() {
+    checkVariants("""
+                    target(aaa: "") {
+                        dep<caret>
+                    }
+                    """, "depends", "dependset");
   }
 
-  void testAntBuilderJavac() {
-    checkVariants """
-target(aaa: "") {
-    ant.jav<caret>
-}""", "java", "javac", "javadoc", "javadoc2", "javaresource"
+  public void testAntBuilderJavac() {
+    checkVariants("""
+                    target(aaa: "") {
+                        ant.jav<caret>
+                    }""", "java", "javac", "javadoc", "javadoc2", "javaresource");
   }
 
-  void testAntJavacTarget() {
-    checkVariants """
-target(aaa: "") {
-    jav<caret>
-}""", "java", "javac", "javadoc", "javadoc2", "javaresource"
+  public void testAntJavacTarget() {
+    checkVariants("""
+                    target(aaa: "") {
+                        jav<caret>
+                    }""", "java", "javac", "javadoc", "javadoc2", "javaresource");
   }
 
-  void testInclude() {
-    checkVariants "inc<caret>", "include", "includeTool", "includeTargets"
+  public void testInclude() {
+    checkVariants("inc<caret>", "include", "includeTool", "includeTargets");
   }
 
-  void testMutual() {
-    checkVariants """
-target(genga: "") { }
-target(aaa: "") {
-    depends(geng<caret>x)
-}""", 'genga'
+  public void testMutual() {
+    checkVariants("""
+                    target(genga: "") { }
+                    target(aaa: "") {
+                        depends(geng<caret>x)
+                    }""", "genga");
   }
 
-  void testUnknownQualifier() {
-    complete """
-target(aaa: "") {
-    foo.jav<caret>
-}"""
+  public void testUnknownQualifier() {
+    complete("""
+               target(aaa: "") {
+                   foo.jav<caret>
+               }""");
   }
 
-  void testTopLevelNoAnt() {
-    complete "jav<caret>"
+  public void testTopLevelNoAnt() {
+    complete("jav<caret>");
   }
 
-  void testInMethodNoAnt() {
-    complete """
-target(aaa: "") {
-  foo()
-}
-
-def foo() {
-  jav<caret>
-}
-"""
+  public void testInMethodNoAnt() {
+    complete("""
+               target(aaa: "") {
+                 foo()
+               }
+               
+               def foo() {
+                 jav<caret>
+               }
+               """);
   }
 
-  void testPatternset() throws Exception {
-    checkVariants "ant.patt<caret>t", "patternset"
+  public void testPatternset() {
+    checkVariants("ant.patt<caret>t", "patternset");
   }
 
-  void testTagsInsideTags() throws Exception {
-    myFixture.configureByText "a.groovy", """
-AntBuilder ant
-ant.zip {
-  patternset {
-    includ<caret>
-  }
-}"""
-    myFixture.completeBasic()
-    assertSameElements myFixture.lookupElementStrings, "include", "includesfile"
-  }
-
-  void testTagsInsideTagsInGantTarget() throws Exception {
-    checkVariants """
-target(aaa: "") {
-  zip {
-    patternset {
-      includ<caret>
-    }
-  }
-}""", "include", "includesfile", "includeTargets", "includeTool"
+  public void testTagsInsideTags() {
+    myFixture.configureByText("a.groovy", """
+      AntBuilder ant
+      ant.zip {
+        patternset {
+          includ<caret>
+        }
+      }""");
+    myFixture.completeBasic();
+    assertSameElements(myFixture.getLookupElementStrings(), "include", "includesfile");
   }
 
-  void testUntypedTargets() throws Exception {
-    myFixture.enableInspections(new GroovyUntypedAccessInspection())
+  public void testTagsInsideTagsInGantTarget() {
+    checkVariants("""
+                    target(aaa: "") {
+                      zip {
+                        patternset {
+                          includ<caret>
+                        }
+                      }
+                    }""", "include", "includesfile", "includeTargets", "includeTool");
+  }
 
-    myFixture.configureByText "a.gant", """
-target (default : '') {
+  public void testUntypedTargets() {
+    myFixture.enableInspections(new GroovyUntypedAccessInspection());
+    myFixture.configureByText("a.gant", """
+      target (default : '') {
+              echo(message: 'Echo task.')
+              copy(file: 'from.txt', tofile: 'to.txt')
+              delete(file: 'to.txt')
+      }""");
+    myFixture.checkHighlighting(true, false, false);
+  }
+
+  public void testStringTargets() {
+    myFixture.enableInspections(new GroovyAssignabilityCheckInspection());
+    myFixture.configureByText("a.gant", """
+      target (default : '') {
+        echo("hello2")
         echo(message: 'Echo task.')
-        copy(file: 'from.txt', tofile: 'to.txt')
-        delete(file: 'to.txt')
-}"""
-    myFixture.checkHighlighting(true, false, false)
+        ant.fail('Failure reason')
+        ant.junit(fork:'yes') {}
+      }""");
+    myFixture.checkHighlighting(true, false, false);
   }
 
-  void testStringTargets() throws Exception {
-    myFixture.enableInspections(new GroovyAssignabilityCheckInspection())
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GANT_PROJECT;
+  }
 
-    myFixture.configureByText "a.gant", """
-target (default : '') {
-  echo("hello2")
-  echo(message: 'Echo task.')
-  ant.fail('Failure reason')
-  ant.junit(fork:'yes') {}
-}"""
-    myFixture.checkHighlighting(true, false, false)
+  @Override
+  public final String getBasePath() {
+    return TestUtils.getTestDataPath() + "gant/completion";
   }
 }
