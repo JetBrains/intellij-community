1512d52efd539c0fd6a0893ab7118232d2f0b716
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2024-10-10 17:13:27 +0200

    IJPF-CR-24873 IJ-CR-145218 [java-inspections] IDEA-201932 Provide inspection to highlight redundant creation operations in java date time api
    GitOrigin-RevId: 5af6c994aca832256f73eaa301cc21f6bb738316

--- java/java-tests/testData/ig/com/siyeh/igtest/redundancy/redundant_java_time_operations/beforeChronoConverterPreview.java
+++ java/java-tests/testData/ig/com/siyeh/igtest/redundancy/redundant_java_time_operations/beforeChronoConverterPreview.java
@@ -5,6 +5,6 @@ import java.time.temporal.ChronoField;
 class Main {
 
   public static int test(OffsetDateTime offsetDateTime) {
-    return offsetDateTime.<warning descr="Calls with explicit 'ChronoField' or 'ChronoUnit' arguments call can be simplified">get<caret></warning>(ChronoField.NANO_OF_SECOND);
+    return offsetDateTime.<warning descr="Calls with explicit 'ChronoField' or 'ChronoUnit' arguments can be simplified">get<caret></warning>(ChronoField.NANO_OF_SECOND);
   }
 }
--- java/java-tests/testData/ig/com/siyeh/igtest/redundancy/redundant_java_time_operations/beforeOffsetDateTimeConvert.java
+++ java/java-tests/testData/ig/com/siyeh/igtest/redundancy/redundant_java_time_operations/beforeOffsetDateTimeConvert.java
@@ -6,13 +6,13 @@ import java.time.temporal.ChronoField;
 class Main {
 
   public static int test(OffsetDateTime offsetDateTime) {
-    return offsetDateTime.<warning descr="Calls with explicit 'ChronoField' or 'ChronoUnit' arguments call can be simplified">get<caret></warning>(ChronoField.NANO_OF_SECOND);
+    return offsetDateTime.<warning descr="Calls with explicit 'ChronoField' or 'ChronoUnit' arguments can be simplified">get<caret></warning>(ChronoField.NANO_OF_SECOND);
   }
   public static int test2(OffsetDateTime offsetDateTime) {
     return offsetDateTime.get(ChronoField.INSTANT_SECONDS);
   }
   public static int test3(OffsetDateTime offsetDateTime) {
-    return offsetDateTime.<warning descr="Calls with explicit 'ChronoField' or 'ChronoUnit' arguments call can be simplified">get</warning>(ChronoField.YEAR);
+    return offsetDateTime.<warning descr="Calls with explicit 'ChronoField' or 'ChronoUnit' arguments can be simplified">get</warning>(ChronoField.YEAR);
   }
   public static void test4(OffsetTime time) {
     time.get(1, <error descr="Cannot resolve symbol 'ChronoUnit'">ChronoUnit</error>.YEARS);
