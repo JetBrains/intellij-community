5d493a6231acff01ef6877cbf17d7cef890df348
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-10-10 20:50:38 +0200

    [analysis] API cleanup: mark deprecated unused API for removal (IJPL-156972)
    GitOrigin-RevId: d2fb148fffcb020b618e16ad97f7c2e726dc7456

--- platform/analysis-impl/src/com/intellij/codeInspection/ex/InspectionProfile.kt
+++ platform/analysis-impl/src/com/intellij/codeInspection/ex/InspectionProfile.kt
@@ -7,7 +7,9 @@ import com.intellij.openapi.project.Project
 import com.intellij.profile.ProfileEx
 import com.intellij.profile.codeInspection.BaseInspectionProfileManager
 import com.intellij.profile.codeInspection.InspectionProfileManager
+import org.jetbrains.annotations.ApiStatus
 
+@ApiStatus.ScheduledForRemoval
 @Deprecated("Use 'InspectionProfile.DEFAULT_PROFILE_NAME'", replaceWith = ReplaceWith("InspectionProfile.DEFAULT_PROFILE_NAME"))
 const val DEFAULT_PROFILE_NAME: String = InspectionProfile.DEFAULT_PROFILE_NAME
 
