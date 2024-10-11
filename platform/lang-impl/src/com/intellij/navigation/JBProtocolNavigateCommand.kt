98070f2047f68668fa5916d76aee1688417848af
Nikita Katkov <nikita.katkov@jetbrains.com>
2024-10-11 16:21:14 +0200

    [split mode] GTW-9500 Delegate processing JB protocol navigation command to the backend IDE in case of split mode
    GitOrigin-RevId: 0169568ed4a6c177f741c162a51c04320e90814c

--- platform/lang-impl/src/com/intellij/navigation/JBProtocolNavigateCommand.kt
+++ platform/lang-impl/src/com/intellij/navigation/JBProtocolNavigateCommand.kt
@@ -1,11 +1,8 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.navigation
 
-import com.intellij.ide.IdeBundle
+import com.intellij.ide.protocolHandler.ProtocolNavigationCommandProcessor
 import com.intellij.openapi.application.JBProtocolCommand
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.LogicalPosition
-import com.intellij.openapi.project.waitForSmartMode
 import com.intellij.openapi.util.NlsContexts.DialogMessage
 import org.jetbrains.annotations.ApiStatus
 
@@ -24,23 +21,6 @@ open class JBProtocolNavigateCommand : JBProtocolCommand(NAVIGATE_COMMAND) {
    *    -(?<line2>[\\d]+):(?<column2>[\\d]+))*
    */
   override suspend fun execute(target: String?, parameters: Map<String, String>, fragment: String?): @DialogMessage String? {
-    if (target != REFERENCE_TARGET) {
-      return IdeBundle.message("jb.protocol.navigate.target", target)
-    }
-
-    val project = when(val openProjectResult = openProject(parameters)) {
-      is ProtocolOpenProjectResult.Success -> openProjectResult.project
-      is ProtocolOpenProjectResult.Error -> return openProjectResult.message
-    }
-    project.waitForSmartMode()
-    NavigatorWithinProject(project, parameters, ::locationToOffset).navigate(listOf(
-      NavigatorWithinProject.NavigationKeyPrefix.FQN,
-      NavigatorWithinProject.NavigationKeyPrefix.PATH
-    ))
-
-    return null
+    return ProtocolNavigationCommandProcessor.EP_NAME.lazySequence().firstOrNull()?.execute(target, parameters, fragment)
   }
-
-  private fun locationToOffset(locationInFile: LocationInFile, editor: Editor): Int =
-    editor.logicalPositionToOffset(LogicalPosition(locationInFile.line, locationInFile.column))
 }
