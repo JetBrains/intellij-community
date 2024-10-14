0533a6641463f24338eed1011a1bb40fa4a1d9ff
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2024-10-14 14:05:34 +0200

    [maven] IDEA-359646 Add stats for the Run full sync in build output
    (cherry picked from commit 84a22f5d0989912d277d790ba76707816501d1f3)
    
    IJ-CR-146730
    
    GitOrigin-RevId: 952cf47b353f89370e16eacbb00107dd0212ee10

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/buildtool/quickfix/MavenFullSyncQuickFix.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/buildtool/quickfix/MavenFullSyncQuickFix.kt
@@ -2,18 +2,46 @@
 package org.jetbrains.idea.maven.buildtool.quickfix
 
 import com.intellij.build.issue.BuildIssueQuickFix
-import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.actionSystem.ex.ActionUtil
 import com.intellij.openapi.project.Project
-import org.jetbrains.idea.maven.project.MavenProjectsManager
+import org.jetbrains.idea.maven.project.actions.ReimportAction
 import java.util.concurrent.CompletableFuture
 
 class MavenFullSyncQuickFix : BuildIssueQuickFix {
   override val id: String = ID
 
   override fun runQuickFix(project: Project, dataContext: DataContext): CompletableFuture<*> {
-    val manager = MavenProjectsManager.getInstance(project)
-    manager.forceUpdateAllProjectsOrFindAllAvailablePomFiles()
+    val action = ReimportAction()
+    val event = createEvent(action, dataContext)
+    invokeAction(action, event)
     return CompletableFuture.completedFuture(null)
+  }
+
+  private fun createEvent(action: AnAction, dataContext: DataContext): AnActionEvent {
+    val p = action.templatePresentation.clone()
+    return createEvent(dataContext, p)
+  }
+
+  private fun createEvent(dataContext: DataContext, presentation: Presentation): AnActionEvent {
+    return AnActionEvent(
+      null,
+      dataContext,
+      "",
+      presentation,
+      ActionManager.getInstance(),
+      0,
+    )
+  }
+
+  private fun invokeAction(action: AnAction, event: AnActionEvent) {
+    if (ActionUtil.lastUpdateAndCheckDumb(action, event, false)) {
+      try {
+        ActionUtil.performActionDumbAwareWithCallbacks(action, event)
+      }
+      finally {
+      }
+    }
   }
 
   companion object {
