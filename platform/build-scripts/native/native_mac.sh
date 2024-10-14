e13b1b7965b8e0d510da7f2ebb0de6a166b81c69
Eugene Yakavets <yakavets@google.com>
2024-10-14 14:58:06 -0700

    Better error handling for mac native tools
    Have script to continue even if one binary build fails.
    This was original intention, but having sub-shell breaks
    `command || ($error_count++))` pattern
    
    Also log XCode and CommandLineTool versions
    
    Bug: 367800209
    Change-Id: Ic3b4ac5889c924d08ebf80048a8b171756520963

--- platform/build-scripts/native/native_mac.sh
+++ platform/build-scripts/native/native_mac.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -eu
 
+(
+  echo Xcode: $(pkgutil --pkg-info=com.apple.pkg.Xcode | awk '/version:/ {print $2}')
+  echo CommandLineTools: $(pkgutil --pkg-info=com.apple.pkg.CLTools_Executables | awk '/version:/ {print $2}')
+  xcode-select -p
+  xcodebuild -version
+  softwareupdate --history
+)
+
 # Build Intellij native tools for Mac
 
 function realpath() {
@@ -14,35 +22,23 @@ declare -r build_number="$3"
 declare -r script_dir=$(realpath "$(dirname "$0")")
 declare exit_code=0
 
-(
-  echo "Building MacTouchBar..."
-  ${script_dir}/build-mac-touchbar.sh -o ${out_dir}/MacTouchBar -d ${dist_dir} || ((exit_code++))
-)
+echo "Building MacTouchBar..."
+${script_dir}/build-mac-touchbar.sh -o ${out_dir}/MacTouchBar -d ${dist_dir} || ((exit_code++))
 
-(
-  echo "Building MacEnvReader..."
-  ${script_dir}/build-mac-envreader.sh -o ${out_dir}/MacEnvReader -d ${dist_dir} || ((exit_code++))
-)
+echo "Building MacEnvReader..."
+${script_dir}/build-mac-envreader.sh -o ${out_dir}/MacEnvReader -d ${dist_dir} || ((exit_code++))
 
-(
-  echo "Building MacScreenMenu..."
-  ${script_dir}/build-mac-screenmenu.sh -o ${out_dir}/MacScreenMenu -d ${dist_dir} || ((exit_code++))
-)
+echo "Building MacScreenMenu..."
+${script_dir}/build-mac-screenmenu.sh -o ${out_dir}/MacScreenMenu -d ${dist_dir} || ((exit_code++))
 
-(
-  echo "Building fsNotifier..."
-  ${script_dir}/build-mac-fsnotifier.sh -o ${out_dir}/fsNotifier -d ${dist_dir} || ((exit_code++))
-)
+echo "Building fsNotifier..."
+${script_dir}/build-mac-fsnotifier.sh -o ${out_dir}/fsNotifier -d ${dist_dir} || ((exit_code++))
 
-(
-  echo "Building xplat-launcher..."
-  ${script_dir}/build-mac-xplat-launcher.sh -o ${out_dir}/xplat-launcher -d ${dist_dir} || ((exit_code++))
-)
+echo "Building xplat-launcher..."
+${script_dir}/build-mac-xplat-launcher.sh -o ${out_dir}/xplat-launcher -d ${dist_dir} || ((exit_code++))
 
-(
-  echo "Building restarter..."
-  ${script_dir}/build-mac-restarter.sh -o ${out_dir}/restarter -d ${dist_dir} || ((exit_code++))
-)
+echo "Building restarter..."
+${script_dir}/build-mac-restarter.sh -o ${out_dir}/restarter -d ${dist_dir} || ((exit_code++))
 
 ls -lha $dist_dir
 
