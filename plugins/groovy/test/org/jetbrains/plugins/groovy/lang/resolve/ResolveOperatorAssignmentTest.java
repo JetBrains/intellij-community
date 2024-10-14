6fcd419f1d9273c73229f9f393c98b98db22557c
Louis Vignier <Louis.Vignier@jetbrains.com>
2024-10-14 23:12:01 +0200

    [groovy] Convert groovy classes to java
    GitOrigin-RevId: d05c456dbdcad604a87fc12bf99a84065b096607

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/resolve/ResolveOperatorAssignmentTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/resolve/ResolveOperatorAssignmentTest.java
@@ -1,76 +1,111 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.lang.resolve
+// Copyright 2000-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.plugins.groovy.lang.resolve;
 
-import com.intellij.psi.PsiMethod
-import com.intellij.testFramework.LightProjectDescriptor
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.codeInspection.GroovyUnusedDeclarationInspection
-import org.jetbrains.plugins.groovy.codeInspection.untypedUnresolvedAccess.GrUnresolvedAccessInspection
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression
+import com.intellij.psi.PsiMethod;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture;
+import groovy.lang.Closure;
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.codeInspection.GroovyUnusedDeclarationInspection;
+import org.jetbrains.plugins.groovy.codeInspection.untypedUnresolvedAccess.GrUnresolvedAccessInspection;
+import org.jetbrains.plugins.groovy.lang.psi.api.GroovyResolveResult;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression;
 
-class ResolveOperatorAssignmentTest extends GroovyResolveTestCase {
-
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_LATEST
-
-  void 'test resolve both getter & setter'() {
-    fixture.with {
-      configureByText '_.groovy', '''\
-class A {
-  C plus(B b) {b}
-}
-class B {}
-class C {}
-class Foo {
-  A getProp() {}
-  Long setProp(C c) {c}
-}
-def foo = new Foo()
-foo.pr<caret>op += new B()
-'''
-      enableInspections GrUnresolvedAccessInspection, GroovyUnusedDeclarationInspection
-      checkHighlighting()
-      def ref = file.findReferenceAt(editor.caretModel.offset) as GrReferenceExpression
-      def results = ref.multiResolve(false)
-      assert results.size() == 2
-      for (result in results) {
-        assert result.isValidResult()
+public class ResolveOperatorAssignmentTest extends GroovyResolveTestCase {
+  public void test_resolve_both_getter___setter() {
+    DefaultGroovyMethods.with(getFixture(), new Closure<Void>(this, this) {
+      public void doCall(JavaCodeInsightTestFixture it) {
+        configureByText("_.groovy", """
+          class A {
+            C plus(B b) {b}
+          }
+          class B {}
+          class C {}
+          class Foo {
+            A getProp() {}
+            Long setProp(C c) {c}
+          }
+          def foo = new Foo()
+          foo.pr<caret>op += new B()
+          """);
+        myFixture.enableInspections(GrUnresolvedAccessInspection.class, GroovyUnusedDeclarationInspection.class);
+        myFixture.checkHighlighting();
+        GrReferenceExpression ref =
+          DefaultGroovyMethods.asType(getFile().findReferenceAt(getEditor().getCaretModel().getOffset()), GrReferenceExpression.class);
+        GroovyResolveResult[] results = ref.multiResolve(false);
+        assert DefaultGroovyMethods.size(results) == 2;
+        for (GroovyResolveResult result : results) {
+          assert result.isValidResult();
+        }
       }
-    }
-  }
 
-  void 'test resolve primitive getter & setter'() {
-    fixture.with {
-      addClass '''\
-class Nothing {
-    private int value;
-    public void setValue(int value) { this.value = value; }
-    public int getValue() { return this.value; }
-}
-'''
-      def ref = configureByText('''\
-new Nothing().val<caret>ue += 42
-''') as GrReferenceExpression
-      def results = ref.multiResolve(false)
-      assert results.size() == 2
-      results.each {
-        assert it.validResult
-        assert it.element instanceof PsiMethod
+      public void doCall() {
+        doCall(null);
       }
-    }
+    });
   }
 
-  void 'test type reassigned'() {
-    fixture.with {
-      def expression = configureByText('''\
-class ClassWithPlus {
-  AnotherClass plus(a) {new AnotherClass()}
-}
-class AnotherClass {}
-def c = new ClassWithPlus()
-c += 1
-<caret>c
-''') as GrReferenceExpression
-      assert expression.type.equalsToText("AnotherClass")
-    }
+  public void test_resolve_primitive_getter___setter() {
+    DefaultGroovyMethods.with(getFixture(), new Closure<GroovyResolveResult[]>(this, this) {
+      public GroovyResolveResult[] doCall(JavaCodeInsightTestFixture it) {
+        myFixture.addClass("""
+                   class Nothing {
+                       private int value;
+                       public void setValue(int value) { this.value = value; }
+                       public int getValue() { return this.value; }
+                   }
+                   """);
+        GrReferenceExpression ref = DefaultGroovyMethods.asType(configureByText("""
+                                                                                  new Nothing().val<caret>ue += 42
+                                                                                  """), GrReferenceExpression.class);
+        GroovyResolveResult[] results = ref.multiResolve(false);
+        assert DefaultGroovyMethods.size(results) == 2;
+        return DefaultGroovyMethods.each(results,
+                                         new Closure<Void>(ResolveOperatorAssignmentTest.this, ResolveOperatorAssignmentTest.this) {
+                                           public void doCall(GroovyResolveResult it) {
+                                             assert it.isValidResult();
+                                             assert it.getElement() instanceof PsiMethod;
+                                           }
+
+                                           public void doCall() {
+                                             doCall(null);
+                                           }
+                                         });
+      }
+
+      public GroovyResolveResult[] doCall() {
+        return doCall(null);
+      }
+    });
   }
+
+  public void test_type_reassigned() {
+    DefaultGroovyMethods.with(getFixture(), new Closure<Void>(this, this) {
+      public void doCall(JavaCodeInsightTestFixture it) {
+        GrReferenceExpression expression = DefaultGroovyMethods.asType(configureByText("""
+                                                                                         class ClassWithPlus {
+                                                                                           AnotherClass plus(a) {new AnotherClass()}
+                                                                                         }
+                                                                                         class AnotherClass {}
+                                                                                         def c = new ClassWithPlus()
+                                                                                         c += 1
+                                                                                         <caret>c
+                                                                                         """), GrReferenceExpression.class);
+        assert expression.getType().equalsToText("AnotherClass");
+      }
+
+      public void doCall() {
+        doCall(null);
+      }
+    });
+  }
+
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return projectDescriptor;
+  }
+
+  private final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_LATEST;
 }
