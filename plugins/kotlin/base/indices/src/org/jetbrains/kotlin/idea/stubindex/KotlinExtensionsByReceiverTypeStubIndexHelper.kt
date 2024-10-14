3248bb09e319cf7cd8e9dee259676897476eeba9
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2024-10-14 14:10:20 +0200

    [kotlin] restore binary compatibility with `com.gmail.blueboxware.extsee`
    restore `org.jetbrains.kotlin.idea.stubindex.KotlinTopLevelExtensionsByReceiverTypeIndex.Helper.receiverTypeNameFromKey(String) : String`
    
    GitOrigin-RevId: 1d9818c875ed5c37ebca137cd4573810ce869c76

--- plugins/kotlin/base/indices/src/org/jetbrains/kotlin/idea/stubindex/KotlinExtensionsByReceiverTypeStubIndexHelper.kt
+++ plugins/kotlin/base/indices/src/org/jetbrains/kotlin/idea/stubindex/KotlinExtensionsByReceiverTypeStubIndexHelper.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.idea.stubindex
 
+import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtCallableDeclaration
@@ -8,6 +9,12 @@ import org.jetbrains.kotlin.psi.KtCallableDeclaration
 abstract class KotlinExtensionsByReceiverTypeStubIndexHelper : KotlinStringStubIndexHelper<KtCallableDeclaration>(
     KtCallableDeclaration::class.java
 ) {
+    // Used by third-party plugin `com.gmail.blueboxware.extsee`
+    @Suppress("unused")
+    @Deprecated("Use org.jetbrains.kotlin.idea.stubindex.KotlinExtensionsByReceiverTypeStubIndexHelper.Companion.Key instead")
+    @ApiStatus.ScheduledForRemoval
+    fun receiverTypeNameFromKey(key: String): String = key.substringBefore(SEPARATOR, "")
+
 
     companion object {
 
