1a14d64c8603f91551ef311e4925ce8e5b2de7ee
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 15:16:38 +0200

    [Groovy] convert GroovyCompletion40Test to Java (IDEA-327339)
    GitOrigin-RevId: 66629911ae011965269817030a2e679845d93a01

--- plugins/groovy/test/org/jetbrains/plugins/groovy/completion/GroovyCompletion40Test.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/completion/GroovyCompletion40Test.java
@@ -1,52 +1,64 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.completion
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.completion;
 
-import com.intellij.codeInsight.CodeInsightSettings
-import com.intellij.testFramework.LightProjectDescriptor
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
+import com.intellij.codeInsight.CodeInsightSettings;
+import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
 
-class GroovyCompletion40Test extends GroovyCompletionTestBase {
+public class GroovyCompletion40Test extends GroovyCompletionTestBase {
+  @Override
+  protected void tearDown() throws Exception {
+    try {
+      CodeInsightSettings.getInstance().setCompletionCaseSensitive(CodeInsightSettings.FIRST_LETTER);
+      CodeInsightSettings.getInstance().AUTOCOMPLETE_ON_CODE_COMPLETION = true;
+    }
+    catch (Throwable e) {
+      addSuppressedException(e);
+    }
+    finally {
+      super.tearDown();
+    }
+  }
 
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_4_0_REAL_JDK
+  public void test_basic() {
+    doBasicTest("""
+                  def x = sw<caret>
+                  """, """
+                  def x = switch (<caret>)
+                  """);
+  }
+
+  public void test_case_inside_switch_expression() {
+    doBasicTest("""
+                  def x = switch (10) {
+                    ca<caret>
+                  }
+                  """, """
+                  def x = switch (10) {
+                    case <caret>
+                  }
+                  """);
+  }
+
+  public void test_in_switch_block() {
+    doBasicTest("""
+                  
+                  def x = switch (10) {
+                    case 10 -> {
+                      yie<caret>
+                    }
+                  }""", """
+                  
+                  def x = switch (10) {
+                    case 10 -> {
+                      yield <caret>
+                    }
+                  }""");
+  }
 
   @Override
-  protected void tearDown() {
-    CodeInsightSettings.instance.completionCaseSensitive = CodeInsightSettings.FIRST_LETTER
-    CodeInsightSettings.instance.AUTOCOMPLETE_ON_CODE_COMPLETION = true
-    super.tearDown()
-  }
-
-  void "test basic"() {
-    doBasicTest('''\
-def x = sw<caret>
-''', '''\
-def x = switch (<caret>)
-''')
-  }
-
-  void "test case inside switch expression"() {
-    doBasicTest('''\
-def x = switch (10) {
-  ca<caret>
-}
-''', '''\
-def x = switch (10) {
-  case <caret>
-}
-''')
-  }
-
-  void "test in switch block"() {
-    doBasicTest("""
-def x = switch (10) {
-  case 10 -> {
-    yie<caret>
-  }
-}""", """
-def x = switch (10) {
-  case 10 -> {
-    yield <caret>
-  }
-}""")
+  public final @NotNull DefaultLightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_4_0_REAL_JDK;
   }
 }
