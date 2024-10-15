48fc03f0555fdb33d5c16596da796b29de4be0d2
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 15:30:41 +0200

    [Groovy] convert InjectedGroovyTest to Java (IDEA-327339)
    GitOrigin-RevId: b179dde49bcd6c74761ca74faec7a54c914b06dd

--- plugins/groovy/test/org/jetbrains/plugins/groovy/completion/InjectedGroovyTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/completion/InjectedGroovyTest.java
@@ -1,101 +1,95 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jetbrains.plugins.groovy.completion
-import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiReference
-import com.intellij.psi.impl.source.tree.injected.InjectedLanguageUtil
-import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.psi.xml.XmlText
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
-import org.intellij.plugins.intelliLang.inject.TemporaryPlacesRegistry
-import org.jetbrains.plugins.groovy.GroovyLanguage
-class InjectedGroovyTest extends LightJavaCodeInsightFixtureTestCase {
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.completion;
 
-  void testTabMethodParentheses() {
-    myFixture.configureByText("a.xml", """<groovy>
-String s = "foo"
-s.codePo<caret>charAt(0)
-</groovy>""")
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiLanguageInjectionHost;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.impl.source.tree.injected.InjectedLanguageUtilBase;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.xml.XmlText;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import junit.framework.TestCase;
+import org.intellij.plugins.intelliLang.inject.TemporaryPlacesRegistry;
+import org.jetbrains.plugins.groovy.GroovyLanguage;
 
-    def host = PsiTreeUtil.findElementOfClassAtOffset(myFixture.file, myFixture.editor.caretModel.offset, XmlText, false)
-    TemporaryPlacesRegistry.getInstance(project).getLanguageInjectionSupport().addInjectionInPlace(GroovyLanguage.INSTANCE, host)
+public class InjectedGroovyTest extends LightJavaCodeInsightFixtureTestCase {
+  public void testTabMethodParentheses() {
+    myFixture.configureByText("a.xml", """
+      <groovy>
+      String s = "foo"
+      s.codePo<caret>charAt(0)
+      </groovy>""");
 
-    myFixture.completeBasic()
-    myFixture.type('\t')
-    myFixture.checkResult("""<groovy>
-String s = "foo"
-s.codePointAt(<caret>0)
-</groovy>""")
+    XmlText host =
+      PsiTreeUtil.findElementOfClassAtOffset(myFixture.getFile(), myFixture.getEditor().getCaretModel().getOffset(), XmlText.class, false);
+    TemporaryPlacesRegistry.getInstance(getProject()).getLanguageInjectionSupport().addInjectionInPlace(GroovyLanguage.INSTANCE,
+                                                                                                        (PsiLanguageInjectionHost)host);
+
+    myFixture.completeBasic();
+    myFixture.type("\t");
+    myFixture.checkResult("""
+                            <groovy>
+                            String s = "foo"
+                            s.codePointAt(<caret>0)
+                            </groovy>""");
   }
 
-  void testIntelliLangInjections() throws Exception {
-    myFixture.addClass("package groovy.lang; public class GroovyShell { public void evaluate(String s) { }}")
-    final PsiFile psiFile = myFixture.configureByText("script.groovy", 'new groovy.lang.GroovyShell().evaluate("s = new String()")')
-    assertNotNull(psiFile)
+  public void testIntelliLangInjections() {
+    myFixture.addClass("package groovy.lang; public class GroovyShell { public void evaluate(String s) { }}");
+    final PsiFile psiFile = myFixture.configureByText("script.groovy", "new groovy.lang.GroovyShell().evaluate(\"s = new String()\")");
+    TestCase.assertNotNull(psiFile);
 
-    def offset = psiFile.getText().indexOf('"') + 1
-    assertNotNull(psiFile.findElementAt(offset))
-    assert InjectedLanguageUtil.findInjectedPsiNoCommit(psiFile, offset)
+    int offset = psiFile.getText().indexOf("\"") + 1;
+    TestCase.assertNotNull(psiFile.findElementAt(offset));
+    assertNotNull(InjectedLanguageUtilBase.findInjectedPsiNoCommit(psiFile, offset));
   }
 
-  void testRegexInjections() {
-    myFixture.addClass("package groovy.lang; public class GroovyShell { public void evaluate(String s) { }}")
-    final PsiFile psiFile = myFixture.configureByText("script.groovy", 'new groovy.lang.GroovyShell().evaluate(/ blah-blah-blah \\ language won\'t be injected here /)')
-    assertNotNull(psiFile)
+  public void testRegexInjections() {
+    myFixture.addClass("package groovy.lang; public class GroovyShell { public void evaluate(String s) { }}");
+    final PsiFile psiFile = myFixture.configureByText("script.groovy",
+                                                      "new groovy.lang.GroovyShell().evaluate(/ blah-blah-blah \\ language won't be injected here /)");
+    TestCase.assertNotNull(psiFile);
 
-    assert InjectedLanguageUtil.findInjectedPsiNoCommit(psiFile, psiFile.getText().indexOf('blah') + 1)
-    assert InjectedLanguageUtil.findInjectedPsiNoCommit(psiFile, psiFile.getText().indexOf('injected') + 1)
+    assertNotNull(InjectedLanguageUtilBase.findInjectedPsiNoCommit(psiFile, psiFile.getText().indexOf("blah") + 1));
+    assertNotNull(InjectedLanguageUtilBase.findInjectedPsiNoCommit(psiFile, psiFile.getText().indexOf("injected") + 1));
   }
 
-  void testResolveAnnotationsInInjectedCode() {
-    myFixture.addClass("package foo; @interface Bar{}")
+  public void testResolveAnnotationsInInjectedCode() {
+    myFixture.addClass("package foo; @interface Bar{}");
 
-    myFixture.addClass("package groovy.lang; public class GroovyShell { public void evaluate(String s) { }}")
+    myFixture.addClass("package groovy.lang; public class GroovyShell { public void evaluate(String s) { }}");
     final PsiFile psiFile = myFixture.configureByText("script.groovy", """
-new groovy.lang.GroovyShell().evaluate('''
-import foo.Bar
-
-@Ba<caret>r
-def abc = null
-''')
-""")
-    assertNotNull(psiFile)
-    PsiReference ref = psiFile.findReferenceAt(myFixture.editor.caretModel.offset)
-    assert ref.resolve() instanceof PsiClass
-    assert ref.resolve().qualifiedName == 'foo.Bar'
-
+      
+      new groovy.lang.GroovyShell().evaluate('''
+      import foo.Bar
+      
+      @Ba<caret>r
+      def abc = null
+      ''')
+      """);
+    TestCase.assertNotNull(psiFile);
+    PsiReference ref = psiFile.findReferenceAt(myFixture.getEditor().getCaretModel().getOffset());
+    assert ref.resolve() instanceof PsiClass;
+    assert ((PsiClass)ref.resolve()).getQualifiedName().equals("foo.Bar");
   }
 
-  void testResolveAnnotationsInInjectedCodeInMethodCall() {
-    myFixture.addClass("package foo; @interface Bar{}")
+  public void testResolveAnnotationsInInjectedCodeInMethodCall() {
+    myFixture.addClass("package foo; @interface Bar{}");
 
-    myFixture.addClass("package groovy.lang; public class GroovyShell { public void evaluate(String s) { }}")
+    myFixture.addClass("package groovy.lang; public class GroovyShell { public void evaluate(String s) { }}");
     final PsiFile psiFile = myFixture.configureByText("script.groovy", """
-new groovy.lang.GroovyShell().evaluate '''
-import foo.Bar
-
-@Ba<caret>r
-def abc = null
-'''
-""")
-    assertNotNull(psiFile)
-    PsiReference ref = psiFile.findReferenceAt(myFixture.editor.caretModel.offset)
-    assert ref.resolve() instanceof PsiClass
-    assert ref.resolve().qualifiedName == 'foo.Bar'
+      
+      new groovy.lang.GroovyShell().evaluate '''
+      import foo.Bar
+      
+      @Ba<caret>r
+      def abc = null
+      '''
+      """);
+    TestCase.assertNotNull(psiFile);
+    PsiReference ref = psiFile.findReferenceAt(myFixture.getEditor().getCaretModel().getOffset());
+    assertTrue(ref.resolve() instanceof PsiClass);
+    assertEquals("foo.Bar", ((PsiClass)ref.resolve()).getQualifiedName());
   }
-
 }
