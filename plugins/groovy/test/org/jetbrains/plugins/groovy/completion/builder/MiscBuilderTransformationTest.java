1ccf867478b7eb2cd047b919bead2951d80bea04
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 16:01:52 +0200

    [Groovy] convert MiscBuilderTransformationTest to Java (IDEA-327339)
    GitOrigin-RevId: 187c85593ae0090e46337cf83dc5454c2752af6e

--- plugins/groovy/test/org/jetbrains/plugins/groovy/completion/builder/MiscBuilderTransformationTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/completion/builder/MiscBuilderTransformationTest.java
@@ -1,64 +1,70 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.completion.builder
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.completion.builder;
 
-import com.intellij.psi.OriginInfoAwareElement
-import com.intellij.psi.impl.light.LightElement
-import com.intellij.testFramework.LightProjectDescriptor
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
-import org.jetbrains.plugins.groovy.lang.resolve.ast.builder.BuilderAnnotationContributor
+import com.intellij.psi.OriginInfoAwareElement;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.impl.light.LightElement;
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.LightGroovyTestCase;
+import org.jetbrains.plugins.groovy.lang.resolve.ast.builder.BuilderAnnotationContributor;
 
-@CompileStatic
-class MiscBuilderTransformationTest extends LightGroovyTestCase {
-
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_LATEST
-
-  void 'test resolve inner class from java'() {
-    myFixture.addFileToProject 'sample/Bean.groovy', '''\
-package sample
-
-@groovy.transform.builder.Builder
-class Bean {
-    String prop1
-    Integer prop2
-}
-'''
-    myFixture.configureByText 'JavaConsumer.java', '''\
-import sample.Bean;
-
-class JavaConsumer {
-    void foo(Bean.Bean<caret>Builder b) {}
-}
-'''
-    def resolved = file.findReferenceAt(myFixture.caretOffset)?.resolve()
-    assert resolved instanceof LightElement
-    assert (resolved as OriginInfoAwareElement).originInfo == BuilderAnnotationContributor.ORIGIN_INFO
+public class MiscBuilderTransformationTest extends LightGroovyTestCase {
+  public void test_resolve_inner_class_from_java() {
+    myFixture.addFileToProject("sample/Bean.groovy", """
+      package sample
+      
+      @groovy.transform.builder.Builder
+      class Bean {
+          String prop1
+          Integer prop2
+      }
+      """);
+    myFixture.configureByText("JavaConsumer.java", """
+      import sample.Bean;
+      
+      class JavaConsumer {
+          void foo(Bean.Bean<caret>Builder b) {}
+      }
+      """);
+    PsiElement resolved = getFile().findReferenceAt(myFixture.getCaretOffset()).resolve();
+    assertTrue(resolved instanceof OriginInfoAwareElement);
+    assertTrue(resolved instanceof LightElement);
+    assertEquals(BuilderAnnotationContributor.ORIGIN_INFO, ((OriginInfoAwareElement)resolved).getOriginInfo());
   }
 
-  void 'test find class'() {
-    myFixture.addFileToProject 'sample/Bean.groovy', '''\
-package sample
+  public void test_find_class() {
+    myFixture.addFileToProject("sample/Bean.groovy", """
+      package sample
+      
+      @groovy.transform.builder.Builder
+      class Bean {
+          String prop1
+          Integer prop2
+      }
+      """);
+    myFixture.configureByText("JavaConsumer.java", """
+      import sample.Bean;
+      
+      class JavaConsumer {
+          void main() {
+              Bean b = Bean.builder().prop1("").prop2(1).build();
+          }
+      }
+      """);
+    myFixture.checkHighlighting();
 
-@groovy.transform.builder.Builder
-class Bean {
-    String prop1
-    Integer prop2
-}
-'''
-    myFixture.configureByText 'JavaConsumer.java', '''\
-import sample.Bean;
+    PsiClass clazz = myFixture.findClass("sample.Bean.BeanBuilder");
 
-class JavaConsumer {
-    void main() {
-        Bean b = Bean.builder().prop1("").prop2(1).build();
-    }
-}
-'''
-    myFixture.checkHighlighting()
+    assertTrue(clazz instanceof OriginInfoAwareElement);
+    assertTrue(clazz instanceof LightElement);
+    assertEquals(BuilderAnnotationContributor.ORIGIN_INFO, ((OriginInfoAwareElement)clazz).getOriginInfo());
+  }
 
-    def clazz = myFixture.findClass('sample.Bean.BeanBuilder')
-    assert clazz in LightElement
-    assert (clazz as OriginInfoAwareElement).originInfo == BuilderAnnotationContributor.ORIGIN_INFO
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_LATEST;
   }
 }
