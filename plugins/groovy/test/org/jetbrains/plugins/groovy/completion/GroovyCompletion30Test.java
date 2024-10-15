bf0295fc478519d4a4d736b3cb90f9d215ac586e
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 14:59:00 +0200

    [Groovy] convert GroovyCompletion30Test to Java (IDEA-327339)
    GitOrigin-RevId: 7657c968f36fa62fff70f0465895c84a5eb7da63

--- plugins/groovy/test/org/jetbrains/plugins/groovy/completion/GroovyCompletion30Test.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/completion/GroovyCompletion30Test.java
@@ -1,130 +1,145 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.completion
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.completion;
 
-import com.intellij.codeInsight.CodeInsightSettings
-import com.intellij.openapi.util.registry.Registry
-import com.intellij.testFramework.LightProjectDescriptor
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.intentions.style.inference.MethodParameterAugmenter
+import com.intellij.codeInsight.CodeInsightSettings;
+import com.intellij.openapi.util.registry.Registry;
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.intentions.style.inference.MethodParameterAugmenter;
 
-class GroovyCompletion30Test extends GroovyCompletionTestBase {
-
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_3_0
-
+public class GroovyCompletion30Test extends GroovyCompletionTestBase {
   @Override
-  protected void setUp() {
-    Registry.get(MethodParameterAugmenter.GROOVY_COLLECT_METHOD_CALLS_FOR_INFERENCE).setValue(true)
-    super.setUp()
+  protected void setUp() throws Exception {
+    Registry.get(MethodParameterAugmenter.GROOVY_COLLECT_METHOD_CALLS_FOR_INFERENCE).setValue(true);
+    super.setUp();
   }
 
   @Override
-  protected void tearDown() {
-    CodeInsightSettings.instance.COMPLETION_CASE_SENSITIVE = CodeInsightSettings.FIRST_LETTER
-    CodeInsightSettings.instance.AUTOCOMPLETE_ON_CODE_COMPLETION = true
-    Registry.get(MethodParameterAugmenter.GROOVY_COLLECT_METHOD_CALLS_FOR_INFERENCE).resetToDefault()
-    super.tearDown()
+  protected void tearDown() throws Exception {
+    try {
+      CodeInsightSettings.getInstance().COMPLETION_CASE_SENSITIVE = CodeInsightSettings.FIRST_LETTER;
+      CodeInsightSettings.getInstance().AUTOCOMPLETE_ON_CODE_COMPLETION = true;
+      Registry.get(MethodParameterAugmenter.GROOVY_COLLECT_METHOD_CALLS_FOR_INFERENCE).resetToDefault();
+    }
+    catch (Throwable e) {
+      addSuppressedException(e);
+    }
+    finally {
+      super.tearDown();
+    }
   }
 
-
-  void testInferArgumentTypeFromClosure() {
-    doBasicTest('''\
-def foo(a, closure) {
-  closure(a)
-}
-
-foo(1) { it.byt<caret> }
-''', '''\
-def foo(a, closure) {
-  closure(a)
-}
-
-foo(1) { it.byteValue()<caret> }
-''')
+  public void testInferArgumentTypeFromClosure() {
+    doBasicTest("""
+                  def foo(a, closure) {
+                    closure(a)
+                  }
+                  
+                  foo(1) { it.byt<caret> }
+                  """, """
+                  def foo(a, closure) {
+                    closure(a)
+                  }
+                  
+                  foo(1) { it.byteValue()<caret> }
+                  """);
   }
 
-  void testInferArgumentTypeFromLambda() {
-    doBasicTest('''
-def foo(a, closure) {
-  closure(a)
-}
-
-foo(1, (it) -> it.byt<caret> )
-''', '''
-def foo(a, closure) {
-  closure(a)
-}
-
-foo(1, (it) -> it.byteValue()<caret> )
-''')
+  public void testInferArgumentTypeFromLambda() {
+    doBasicTest("""
+                  def foo(a, closure) {
+                    closure(a)
+                  }
+                  
+                  foo(1, (it) -> it.byt<caret> )
+                  """, """
+                  def foo(a, closure) {
+                    closure(a)
+                  }
+                  
+                  foo(1, (it) -> it.byteValue()<caret> )
+                  """);
   }
 
-  void testInferArgumentTypeFromClosureInsideClass() {
-    doBasicTest '''
-class K {
-  def foo(a, closure) {
-    closure(a)
+  public void testInferArgumentTypeFromClosureInsideClass() {
+    doBasicTest("""
+                  
+                  class K {
+                    def foo(a, closure) {
+                      closure(a)
+                    }
+                  
+                    def bar() {
+                      foo(1) { it.byt<caret> }
+                    }
+                  }
+                  """, """
+                  
+                  class K {
+                    def foo(a, closure) {
+                      closure(a)
+                    }
+                  
+                    def bar() {
+                      foo(1) { it.byteValue()<caret> }
+                    }
+                  }
+                  """);
   }
 
-  def bar() {
-    foo(1) { it.byt<caret> }
-  }
-}
-''', '''
-class K {
-  def foo(a, closure) {
-    closure(a)
-  }
-
-  def bar() {
-    foo(1) { it.byteValue()<caret> }
-  }
-}
-'''
-  }
-
-  void testInferArgumentTypeForClosure() {
-    doBasicTest '''
-def foo(a, b) { b(a) }
-
-foo(1) { a -> a.byteValue() }
-foo('q') { it.len<caret>}
-''', '''
-def foo(a, b) { b(a) }
-
-foo(1) { a -> a.byteValue() }
-foo('q') { it.length()<caret>}
-'''
+  public void testInferArgumentTypeForClosure() {
+    doBasicTest("""
+                  
+                  def foo(a, b) { b(a) }
+                  
+                  foo(1) { a -> a.byteValue() }
+                  foo('q') { it.len<caret>}
+                  """, """
+                  
+                  def foo(a, b) { b(a) }
+                  
+                  foo(1) { a -> a.byteValue() }
+                  foo('q') { it.length()<caret>}
+                  """);
   }
 
-  void testInferArgumentTypeFromMethod() {
-    doBasicTest('''\
-def foo(a) {
-  a.byt<caret>
-}
-
-foo 1
-''', '''\
-def foo(a) {
-  a.byteValue()<caret>
-}
-
-foo 1
-''')
+  public void testInferArgumentTypeFromMethod() {
+    doBasicTest("""
+                  def foo(a) {
+                    a.byt<caret>
+                  }
+                  
+                  foo 1
+                  """, """
+                  def foo(a) {
+                    a.byteValue()<caret>
+                  }
+                  
+                  foo 1
+                  """);
   }
 
-  void testCompletionAfterComplexStatement() {
-    doBasicTest '''
-def x(List<Integer> l){}
+  public void testCompletionAfterComplexStatement() {
+    doBasicTest("""
+                  
+                  def x(List<Integer> l){}
+                  
+                  void m(l) {
+                      x([l])
+                      l.by<caret>
+                  }""", """
+                  
+                  def x(List<Integer> l){}
+                  
+                  void m(l) {
+                      x([l])
+                      l.byteValue()<caret>
+                  }""");
+  }
 
-void m(l) {
-    x([l])
-    l.by<caret>
-}''', '''
-def x(List<Integer> l){}
-
-void m(l) {
-    x([l])
-    l.byteValue()<caret>
-}'''
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_3_0;
   }
 }
