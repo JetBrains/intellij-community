964b3bf4771ce307c66da374030587156483876a
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 15:38:25 +0200

    [Groovy] convert JavaWithGroovyCompletionTest to Java (IDEA-327339)
    GitOrigin-RevId: 64dffc2ee6fc679627a02137c96a25b9e9fb18a3

--- plugins/groovy/test/org/jetbrains/plugins/groovy/completion/JavaWithGroovyCompletionTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/completion/JavaWithGroovyCompletionTest.java
@@ -1,32 +1,36 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.completion
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.completion;
 
-import com.intellij.codeInsight.completion.CompletionType
-import com.intellij.psi.PsiMethod
+import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.psi.PsiMethod;
+import com.intellij.util.containers.ContainerUtil;
 
-class JavaWithGroovyCompletionTest extends GroovyCompletionTestBase {
-
-  void "test using java keywords in member names"() {
-    myFixture.addFileToProject 'a.groovy', '''
-class Foo {
-  static void "const"() {}
-  static final int "continue" = 2;
-}
-'''
-    myFixture.configureByText 'a.java', 'class Bar {{ con<caret> }}'
-    myFixture.complete(CompletionType.BASIC, 2)
-    assert !(myFixture.lookupElementStrings.contains('const'))
-    assert !(myFixture.lookupElementStrings.contains('continue'))
+public class JavaWithGroovyCompletionTest extends GroovyCompletionTestBase {
+  public void test_using_java_keywords_in_member_names() {
+    myFixture.addFileToProject("a.groovy", """
+      class Foo {
+        static void "const"() {}
+        static final int "continue" = 2;
+      }
+      """);
+    myFixture.configureByText("a.java", "class Bar {{ con<caret> }}");
+    myFixture.complete(CompletionType.BASIC, 2);
+    assert !(myFixture.getLookupElementStrings().contains("const"));
+    assert !(myFixture.getLookupElementStrings().contains("continue"));
   }
 
-  void "test using java expression keywords in member names"() {
-    myFixture.addFileToProject 'a.groovy', '''
-class Foo {
-  static void "this"() {}
-}
-'''
-    myFixture.configureByText 'a.java', 'class Bar {{ this<caret> }}'
-    myFixture.complete(CompletionType.BASIC, 2)
-    assert !myFixture.lookupElements.find { it.lookupString == 'this' && it.object instanceof PsiMethod }
+  public void test_using_java_expression_keywords_in_member_names() {
+    myFixture.addFileToProject("a.groovy", """
+      class Foo {
+        static void "this"() {}
+      }
+      """);
+    myFixture.configureByText("a.java", "class Bar {{ this<caret> }}");
+    myFixture.complete(CompletionType.BASIC, 2);
+    LookupElement[] elements = myFixture.getLookupElements();
+    assertTrue(elements == null || ContainerUtil.find(elements,
+                                                      element -> element.getLookupString().equals("this") &&
+                                                                 element.getObject() instanceof PsiMethod) == null);
   }
 }
