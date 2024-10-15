4599d1978578b9e205e8a0f36363a0996e84bd04
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 14:18:49 +0200

    [Groovy] convert GrLambdaCompletionTest to Java (IDEA-327339)
    GitOrigin-RevId: a45a03faf99ce29300b5a03511eed91da1ae2583

--- plugins/groovy/test/org/jetbrains/plugins/groovy/completion/GrLambdaCompletionTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/completion/GrLambdaCompletionTest.java
@@ -1,13 +1,19 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.completion
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.completion;
 
-import com.intellij.testFramework.LightProjectDescriptor
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.util.TestUtils
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.util.TestUtils;
 
-@CompileStatic
-class GrLambdaCompletionTest extends GrFunctionalExpressionCompletionTest {
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_3_0
-  final String basePath = TestUtils.testDataPath + "groovy/completion/lambda"
+public class GrLambdaCompletionTest extends GrFunctionalExpressionCompletionTest {
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_3_0;
+  }
+
+  @Override
+  public final @NotNull String getBasePath() {
+    return TestUtils.getTestDataPath() + "groovy/completion/lambda";
+  }
 }
