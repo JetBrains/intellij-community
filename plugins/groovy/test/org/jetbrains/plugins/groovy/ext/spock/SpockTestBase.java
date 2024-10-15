aef76570349e58e896828b98b36f10dedbe8e13f
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 13:06:49 +0200

    [Groovy] convert SpockTestBase to Java (IDEA-327339)
    GitOrigin-RevId: 449a4c42489e92647a701a405c93725b8ad345aa

--- plugins/groovy/test/org/jetbrains/plugins/groovy/ext/spock/SpockTestBase.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/ext/spock/SpockTestBase.java
@@ -1,20 +1,20 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.groovy.ext.spock
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.ext.spock;
 
-import com.intellij.testFramework.LightProjectDescriptor
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.LibraryLightProjectDescriptor
-import org.jetbrains.plugins.groovy.RepositoryTestLibrary
-import org.jetbrains.plugins.groovy.util.LightProjectTest
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.LibraryLightProjectDescriptor;
+import org.jetbrains.plugins.groovy.RepositoryTestLibrary;
+import org.jetbrains.plugins.groovy.util.LightProjectTest;
 
-abstract class SpockTestBase extends LightProjectTest {
+public class SpockTestBase extends LightProjectTest {
 
   static final LightProjectDescriptor SPOCK_PROJECT = new LibraryLightProjectDescriptor(
-    GroovyProjectDescriptors.LIB_GROOVY_2_4 + new RepositoryTestLibrary("org.spockframework:spock-core:2.0-groovy-3.0")
-  )
+    GroovyProjectDescriptors.LIB_GROOVY_2_4.plus(new RepositoryTestLibrary("org.spockframework:spock-core:2.0-groovy-3.0"))
+  );
 
   @Override
-  final LightProjectDescriptor getProjectDescriptor() {
-    SPOCK_PROJECT
+  public final LightProjectDescriptor getProjectDescriptor() {
+    return SPOCK_PROJECT;
   }
 }
