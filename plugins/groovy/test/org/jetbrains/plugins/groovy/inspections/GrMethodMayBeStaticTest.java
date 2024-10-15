004926fb259af1f64740b6b5e10ad924a4495ba7
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 16:43:41 +0200

    [Groovy] convert GrMethodMayBeStaticTest to Java (IDEA-327339)
    GitOrigin-RevId: d068697273e064f712d9b4f1cee86f9eaab4c8db

--- plugins/groovy/test/org/jetbrains/plugins/groovy/inspections/GrMethodMayBeStaticTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/inspections/GrMethodMayBeStaticTest.java
@@ -1,165 +1,169 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.inspections
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.inspections;
 
-import com.intellij.testFramework.LightProjectDescriptor
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
-import org.jetbrains.plugins.groovy.codeInspection.declaration.GrMethodMayBeStaticInspection
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.LightGroovyTestCase;
+import org.jetbrains.plugins.groovy.codeInspection.declaration.GrMethodMayBeStaticInspection;
 
 /**
  * @author Max Medvedev
  */
-@CompileStatic
-class GrMethodMayBeStaticTest extends LightGroovyTestCase {
-
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_LATEST
-  final GrMethodMayBeStaticInspection inspection = new GrMethodMayBeStaticInspection()
-
-  void testSimple() {
-    doTest('''\
-class A {
-  def <warning descr="Method may be static">foo</warning>() {
-    print 2
+public class GrMethodMayBeStaticTest extends LightGroovyTestCase {
+  public void testSimple() {
+    doTest("""
+             class A {
+               def <warning descr="Method may be static">foo</warning>() {
+                 print 2
+               }
+             
+               def <warning descr="Method may be static">bar</warning>() {
+                 [1, 2 ].each {3}
+               }
+             
+               def <warning descr="Method may be static">abc</warning>() {
+                 new A().bar()
+               }
+             
+               def cdef() {
+                 bar()
+               }
+             
+               def x() {
+                 Inner ref = null
+               }
+             
+               def <warning descr="Method may be static">y</warning>() {
+                 staticMethod()
+               }
+             
+               def <warning descr="Method may be static">z</warning>() {
+                 StaticInner i = null
+               }
+             
+               def q() {
+                 staticMethod()
+                 Inner i
+               }
+             
+               class Inner {}
+             
+               static class StaticInner{}
+             
+               static staticMethod(){}
+             }
+             """);
   }
 
-  def <warning descr="Method may be static">bar</warning>() {
-    [1, 2 ].each {3}
-  }
-
-  def <warning descr="Method may be static">abc</warning>() {
-    new A().bar()
-  }
-
-  def cdef() {
-    bar()
-  }
-
-  def x() {
-    Inner ref = null
-  }
-
-  def <warning descr="Method may be static">y</warning>() {
-    staticMethod()
-  }
-
-  def <warning descr="Method may be static">z</warning>() {
-    StaticInner i = null
-  }
-
-  def q() {
-    staticMethod()
-    Inner i
-  }
-
-  class Inner {}
-
-  static class StaticInner{}
-
-  static staticMethod(){}
-}
-''')
-  }
-
-  void 'test abstract method with code block no error'() {
-    doTest '''
-abstract class A {
-  abstract foo() <error descr="Abstract methods must not have body">{
-    1 + 2
-  }</error>
-}
-'''
+  public void test_abstract_method_with_code_block_no_error() {
+    doTest("""
+             abstract class A {
+               abstract foo() <error descr="Abstract methods must not have body">{
+                 1 + 2
+               }</error>
+             }
+             """);
   }
 
   private void doTest(final String text) {
-    myFixture.configureByText('_.groovy', text)
-    myFixture.enableInspections(inspection)
-    myFixture.checkHighlighting(true, false, false)
+    myFixture.configureByText("_.groovy", text);
+    myFixture.enableInspections(new GrMethodMayBeStaticInspection());
+    myFixture.checkHighlighting(true, false, false);
   }
 
-  void testOtherClasses() {
-    myFixture.addFileToProject('Fooo.groovy', '''\
-class Fooo {
-  static def foo() {}
-}
-''')
-    doTest('''\
-class Bar {
-  static def abc() {}
-  def <warning descr="Method may be static">bar</warning>() {
-    abc()
-    Fooo.foo()
-  }
-}
-''')
-  }
-
-  void testThis() {
-    doTest('''\
-class Foo {
-  void <warning descr="Method may be static">barx</warning>() {
-    this.abc()
+  public void testOtherClasses() {
+    myFixture.addFileToProject("Fooo.groovy", """
+      class Fooo {
+        static def foo() {}
+      }
+      """);
+    doTest("""
+             class Bar {
+               static def abc() {}
+               def <warning descr="Method may be static">bar</warning>() {
+                 abc()
+                 Fooo.foo()
+               }
+             }
+             """);
   }
 
-  void cde() {
-    this.bar()
+  public void testThis() {
+    doTest("""
+             class Foo {
+               void <warning descr="Method may be static">barx</warning>() {
+                 this.abc()
+               }
+             
+               void cde() {
+                 this.bar()
+               }
+             
+               static abc(){}
+               void bar(){}
+             }
+             """);
   }
 
-  static abc(){}
-  void bar(){}
-}
-''')
+  public void testSuper() {
+    doTest("""
+             class Base {
+                 void var() {}
+             
+                 static foo() {}
+             }
+             
+             class Bar extends Base {
+                 Bar <warning descr="Method may be static">b</warning>() {
+                     super.foo()
+                 }
+             
+                 Bar c() {
+                     super.var()
+                 }
+             }
+             """);
   }
 
-  void testSuper() {
-    doTest('''\
-class Base {
-    void var() {}
-
-    static foo() {}
-}
-
-class Bar extends Base {
-    Bar <warning descr="Method may be static">b</warning>() {
-        super.foo()
-    }
-
-    Bar c() {
-        super.var()
-    }
-}
-''')
+  public void testUnresolvedRef() {
+    doTest("""
+             class Bar {
+               def b() {
+                 print unresolved
+               }
+               def <warning descr="Method may be static">abc</warning>() {
+                 print 2
+               }
+             }
+             """);
   }
 
-  void testUnresolvedRef() {
-    doTest('''\
-class Bar {
-  def b() {
-    print unresolved
-  }
-  def <warning descr="Method may be static">abc</warning>() {
-    print 2
-  }
-}
-''')
+  public void test_trait_methods() {
+    doTest("""
+             trait A {
+               def foo() {1}
+               abstract bar()
+             }
+             """);
   }
 
-  void 'test trait methods'() {
-    doTest '''\
-trait A {
-  def foo() {1}
-  abstract bar()
-}
-'''
+  public void test_trait_methods_with() {
+    new GrMethodMayBeStaticInspection().myIgnoreTraitMethods = false;
+    doTest("""
+             trait A {
+               def <warning descr="Method may be static">foo</warning>() {1}
+               abstract bar()
+             }
+             """);
   }
 
-  void 'test trait methods with'() {
-    inspection.myIgnoreTraitMethods = false
-    doTest '''\
-trait A {
-  def <warning descr="Method may be static">foo</warning>() {1}
-  abstract bar()
-}
-'''
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_LATEST;
+  }
+
+  public static GrMethodMayBeStaticInspection getInspection() {
+    return new GrMethodMayBeStaticInspection();
   }
 }
