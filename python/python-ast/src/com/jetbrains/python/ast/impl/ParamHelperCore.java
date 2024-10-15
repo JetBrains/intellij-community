9034b138800cfe68447a9c11a5307dd46411e357
Petr <petr.golubev@jetbrains.com>
2024-10-15 20:06:46 +0200

    PY-75537 Implement PyAstNamedParameter.getRepr()(PyFrontendElementTypesFacadeImpl.kt)
    GitOrigin-RevId: ef38af29fd176324f4c1bec503a116a5b4f351b4

--- python/python-ast/src/com/jetbrains/python/ast/impl/ParamHelperCore.java
+++ python/python-ast/src/com/jetbrains/python/ast/impl/ParamHelperCore.java
@@ -1,15 +1,62 @@
 package com.jetbrains.python.ast.impl;
 
 import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.text.StringUtil;
 import com.jetbrains.python.ast.PyAstExpression;
+import com.jetbrains.python.ast.PyAstNamedParameter;
 import com.jetbrains.python.ast.PyAstStringLiteralExpression;
 import com.jetbrains.python.psi.PyStringLiteralCoreUtil;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @ApiStatus.Experimental
 public final class ParamHelperCore {
   private ParamHelperCore() {
+  }
+
+  @NotNull
+  public static String getNameInSignature(@NotNull PyAstNamedParameter parameter) {
+    final StringBuilder sb = new StringBuilder();
+
+    if (parameter.isPositionalContainer()) sb.append("*");
+    else if (parameter.isKeywordContainer()) sb.append("**");
+
+    final String name = parameter.getName();
+    sb.append(name != null ? name : "...");
+
+    return sb.toString();
+  }
+
+  /**
+   * @param defaultValue             string returned by {@link PyCallableParameter#getDefaultValueText()} or {@link PyParameter#getDefaultValueText()}.
+   * @param parameterRenderedAsTyped true if parameter is rendered with type annotation.
+   * @return equal sign (surrounded with spaces if {@code parameterRenderedAsTyped}) +
+   * {@code defaultValue} (with body escaped if it is a string literal)
+   */
+  @Nullable
+  @Contract("null, _->null")
+  public static String getDefaultValuePartInSignature(@Nullable String defaultValue, boolean parameterRenderedAsTyped) {
+    if (defaultValue == null) return null;
+
+    final StringBuilder sb = new StringBuilder();
+
+    // According to PEP 8 equal sign should be surrounded by spaces if annotation is present
+    sb.append(parameterRenderedAsTyped ? " = " : "=");
+
+    final Pair<String, String> quotes = PyStringLiteralCoreUtil.getQuotes(defaultValue);
+    if (quotes != null) {
+      final String value = defaultValue.substring(quotes.getFirst().length(), defaultValue.length() - quotes.getSecond().length());
+      sb.append(quotes.getFirst());
+      StringUtil.escapeStringCharacters(value.length(), value, sb);
+      sb.append(quotes.getSecond());
+    }
+    else {
+      sb.append(defaultValue);
+    }
+
+    return sb.toString();
   }
 
   /**
