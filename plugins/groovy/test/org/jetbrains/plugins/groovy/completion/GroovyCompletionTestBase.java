40dff61c4ba53f00af92b3b534641dc26b58b91b
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 15:14:51 +0200

    [Groovy] convert GroovyCompletionTestBase to Java (IDEA-327339)
    GitOrigin-RevId: bc5d213ed1bcfd7e6c84e2cb847044c65844e00e

--- plugins/groovy/test/org/jetbrains/plugins/groovy/completion/GroovyCompletionTestBase.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/completion/GroovyCompletionTestBase.java
@@ -1,121 +1,177 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.completion
-import com.intellij.codeInsight.CodeInsightSettings
-import com.intellij.codeInsight.completion.CompletionType
-import com.intellij.codeInsight.lookup.LookupManager
-import com.intellij.codeInsight.lookup.impl.LookupImpl
-import com.intellij.openapi.application.impl.NonBlockingReadActionImpl
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.completion;
 
-abstract class GroovyCompletionTestBase extends LightJavaCodeInsightFixtureTestCase {
+import com.intellij.codeInsight.CodeInsightSettings;
+import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.codeInsight.lookup.LookupManager;
+import com.intellij.codeInsight.lookup.impl.LookupImpl;
+import com.intellij.openapi.application.impl.NonBlockingReadActionImpl;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class GroovyCompletionTestBase extends LightJavaCodeInsightFixtureTestCase {
   protected void doSmartTest() {
-    doCompletionTest(CompletionType.SMART)
+    doCompletionTest(CompletionType.SMART);
   }
 
-  protected void doBasicTest(String before =  null, String after = null) {
-    doCompletionTest(before, after, CompletionType.BASIC)
+  protected void doBasicTest(String before, String after) {
+    doCompletionTest(before, after, CompletionType.BASIC);
+  }
+
+  protected void doBasicTest(String before) {
+    doBasicTest(before, null);
+  }
+
+  protected void doBasicTest() {
+    doBasicTest(null, null);
   }
 
   protected void doSmartTest(String before, String after) {
-    doCompletionTest(before, after, CompletionType.SMART)
+    doCompletionTest(before, after, CompletionType.SMART);
   }
 
   protected void checkResult() {
-    myFixture.checkResultByFile(getTestName(false) + "_after.groovy", true)
+    myFixture.checkResultByFile(getTestName(false) + "_after.groovy", true);
   }
 
-  protected void doCompletionTest(String before = null, String after = null, String type = "", CompletionType ct) {
+  protected void doCompletionTest(String before, String after, String type, CompletionType ct) {
     if (before == null) {
-      myFixture.configureByFile(getTestName(false) + ".groovy")
+      myFixture.configureByFile(getTestName(false) + ".groovy");
     }
     else {
-      myFixture.configureByText(getTestName(false) + ".groovy", before)
+      myFixture.configureByText(getTestName(false) + ".groovy", before);
     }
 
-    myFixture.complete(ct)
-    type.each { myFixture.type(it) }
+
+    myFixture.complete(ct);
+    for (char ch : type.toCharArray()) {
+      myFixture.type(ch);
+    }
     NonBlockingReadActionImpl.waitForAsyncTaskCompletion();
-
-    assertNull(myFixture.lookupElementStrings as String, myFixture.lookupElements)
+    String msg = "<empty>";
+    if (myFixture.getLookupElementStrings() != null) {
+      msg = String.join(", ", myFixture.getLookupElementStrings());
+    }
+    assertNull(msg, myFixture.getLookupElements());
     if (after == null) {
-      myFixture.checkResultByFile(getTestName(false) + "_after.groovy", true)
+      myFixture.checkResultByFile(getTestName(false) + "_after.groovy", true);
     }
     else {
-      myFixture.checkResult(after, true)
+      myFixture.checkResult(after, true);
     }
   }
 
-  protected void doVariantableTest(String before = null, String type = "", CompletionType ct, CompletionResult testType = CompletionResult.equal, int completionCount = 1, String... variants) {
+  protected void doCompletionTest(String before, String after, CompletionType ct) {
+    doCompletionTest(before, after, "", ct);
+  }
+
+  protected void doCompletionTest(String before, CompletionType ct) {
+    doCompletionTest(before, null, "", ct);
+  }
+
+  protected void doCompletionTest(CompletionType ct) {
+    doCompletionTest(null, null, "", ct);
+  }
+
+  protected void doVariantableTest(String before,
+                                   String type,
+                                   CompletionType ct,
+                                   CompletionResult testType,
+                                   int completionCount,
+                                   String... variants) {
     if (before == null) {
-      myFixture.configureByFile(getTestName(false) + ".groovy")
+      myFixture.configureByFile(getTestName(false) + ".groovy");
     }
     else {
-      myFixture.configureByText(getTestName(false) + ".groovy", before)
+      myFixture.configureByText(getTestName(false) + ".groovy", before);
     }
 
-    myFixture.complete(ct, completionCount)
-    type.each { myFixture.type(it) }
+    myFixture.complete(ct, completionCount);
+    for (char ch : type.toCharArray()) {
+      myFixture.type(ch);
+    }
+    assertNotNull(myFixture.getLookupElements());
 
-    assertNotNull(myFixture.lookupElements)
-
-    final actual = myFixture.lookupElementStrings
+    final List<String> actual = myFixture.getLookupElementStrings();
+    String msg = "<empty>";
+    if (myFixture.getLookupElementStrings() != null) {
+      msg = String.join(", ", myFixture.getLookupElementStrings());
+    }
     switch (testType) {
-      case CompletionResult.contain:
-        assertTrue(myFixture.lookupElementStrings as String, actual.containsAll(variants))
-        break
-      case CompletionResult.equal:
-        if (!variants) {
-          assert !actual
-        } else {
-          myFixture.assertPreferredCompletionItems(0, variants)
+      case contain:
+        assertTrue(msg, actual.containsAll(Arrays.asList(variants)));
+        break;
+      case equal:
+        if (variants.length == 0) {
+          assertTrue(actual.isEmpty());
         }
-        break
-      case CompletionResult.notContain:
-        variants.each {
-          assertFalse(myFixture.lookupElementStrings as String, actual.contains(it))
+        else {
+          myFixture.assertPreferredCompletionItems(0, variants);
         }
+        break;
+      case notContain:
+        for (String variant : variants) {
+          assertFalse(msg, actual.contains(variant));
+        }
+        break;
     }
   }
 
-
-  void doVariantableTest(String... variants) {
-    doVariantableTest(CompletionType.BASIC, variants)
+  protected void doVariantableTest(String before, String type, CompletionType ct, CompletionResult testType, String... variants) {
+    doVariantableTest(before, type, ct, testType, 1, variants);
   }
 
-  void doHasVariantsTest(String... variants) {
-    doVariantableTest(null, "", CompletionType.BASIC, CompletionResult.contain, variants)
+  protected void doVariantableTest(String before, String type, CompletionType ct, String... variants) {
+    doVariantableTest(before, type, ct, CompletionResult.equal, 1, variants);
   }
 
-  void doSmartCompletion(String... variants) {
-    doVariantableTest(CompletionType.SMART, variants)
+  protected void doVariantableTest(String before, CompletionType ct, String... variants) {
+    doVariantableTest(before, "", ct, CompletionResult.equal, 1, variants);
   }
 
-  void checkCompletion(String before, String type, String after) {
-    doCompletionTest(before, after, type, CompletionType.BASIC)
+  protected void doVariantableTest(CompletionType ct, String... variants) {
+    doVariantableTest(null, "", ct, CompletionResult.equal, 1, variants);
   }
 
-  void checkSingleItemCompletion(String before, String after) {
-    doCompletionTest(before, after, CompletionType.BASIC)
+  public void doVariantableTest(String... variants) {
+    doVariantableTest(CompletionType.BASIC, variants);
   }
 
-  void doNoVariantsTest(String before, String... excludedVariants) {
-    doVariantableTest(before, "", CompletionType.BASIC, CompletionResult.notContain, excludedVariants)
+  public void doHasVariantsTest(String... variants) {
+    doVariantableTest(null, "", CompletionType.BASIC, CompletionResult.contain, variants);
   }
 
-  protected static def caseSensitiveNone() {
-    CodeInsightSettings.instance.COMPLETION_CASE_SENSITIVE = CodeInsightSettings.NONE
+  public void doSmartCompletion(String... variants) {
+    doVariantableTest(CompletionType.SMART, variants);
   }
 
-  LookupImpl getLookup() {
-    LookupManager.getActiveLookup(myFixture.editor)
+  public void checkCompletion(String before, String type, String after) {
+    doCompletionTest(before, after, type, CompletionType.BASIC);
   }
 
-  void addCompileStatic() {
-    myFixture.addClass('''\
-package groovy.transform;
-public @interface CompileStatic{
-}
-''')
+  public void checkSingleItemCompletion(String before, String after) {
+    doCompletionTest(before, after, CompletionType.BASIC);
+  }
+
+  public void doNoVariantsTest(String before, String... excludedVariants) {
+    doVariantableTest(before, "", CompletionType.BASIC, CompletionResult.notContain, excludedVariants);
+  }
+
+  protected static int caseSensitiveNone() {
+    return CodeInsightSettings.getInstance().COMPLETION_CASE_SENSITIVE = CodeInsightSettings.NONE;
+  }
+
+  public LookupImpl getLookup() {
+    return ((LookupImpl)(LookupManager.getActiveLookup(myFixture.getEditor())));
+  }
+
+  public void addCompileStatic() {
+    myFixture.addClass("""
+                         package groovy.transform;
+                         public @interface CompileStatic{
+                         }
+                         """);
   }
 }
