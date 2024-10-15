6d53c5da43d3de3db9b3e2b8d3f05f4447f87707
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 13:41:47 +0200

    [Groovy] convert GrClosureCompletionTest to Java (IDEA-327339)
    GitOrigin-RevId: 4d6c54863fa28a1e989f12ae6bf3c0441273893f

--- plugins/groovy/test/org/jetbrains/plugins/groovy/completion/GrClosureCompletionTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/completion/GrClosureCompletionTest.java
@@ -1,13 +1,21 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.completion
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.completion;
 
-import com.intellij.testFramework.LightProjectDescriptor
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.util.TestUtils
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.util.TestUtils;
 
-@CompileStatic
-class GrClosureCompletionTest extends GrFunctionalExpressionCompletionTest {
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_1_7
-  final String basePath = TestUtils.testDataPath + "groovy/completion/"
+public class GrClosureCompletionTest extends GrFunctionalExpressionCompletionTest {
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_1_7;
+  }
+
+  @Override
+  public final String getBasePath() {
+    return basePath;
+  }
+
+  private final String basePath = TestUtils.getTestDataPath() + "groovy/completion/";
 }
