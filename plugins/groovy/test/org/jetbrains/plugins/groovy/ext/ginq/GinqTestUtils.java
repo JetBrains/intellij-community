db71f3a7bbb599a6a09aeb5e2197511fb41609ab
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 13:15:16 +0200

    [Groovy] convert GinqTestUtils to Java (IDEA-327339)
    GitOrigin-RevId: 6a258c398d8558a5a403aa53f72558a7551a6f56

--- plugins/groovy/test/org/jetbrains/plugins/groovy/ext/ginq/GinqTestUtils.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/ext/ginq/GinqTestUtils.java
@@ -1,24 +1,25 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.groovy.ext.ginq
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.ext.ginq;
 
-import com.intellij.openapi.projectRoots.JavaSdk
-import com.intellij.openapi.projectRoots.Sdk
-import com.intellij.testFramework.IdeaTestUtil
-import com.intellij.testFramework.LightProjectDescriptor
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.LibraryLightProjectDescriptor
-import org.jetbrains.plugins.groovy.RepositoryTestLibrary
+import com.intellij.openapi.projectRoots.JavaSdk;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.testFramework.IdeaTestUtil;
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.LibraryLightProjectDescriptor;
+import org.jetbrains.plugins.groovy.RepositoryTestLibrary;
 
-@Singleton
-class GinqTestUtils {
-
-  private static final RepositoryTestLibrary LIB_GINQ = new RepositoryTestLibrary("org.apache.groovy:groovy-ginq:4.0.0")
-
-  static final LightProjectDescriptor projectDescriptor = new LibraryLightProjectDescriptor(GroovyProjectDescriptors.LIB_GROOVY_4_0 + LIB_GINQ) {
+public final class GinqTestUtils {
+  private static final RepositoryTestLibrary LIB_GINQ = new RepositoryTestLibrary("org.apache.groovy:groovy-ginq:4.0.0");
+  private static final LightProjectDescriptor PROJECT_DESCRIPTOR = new LibraryLightProjectDescriptor(
+    GroovyProjectDescriptors.LIB_GROOVY_4_0.plus(LIB_GINQ)) {
     @Override
-    Sdk getSdk() {
-      return JavaSdk.getInstance().createJdk("TEST_JDK", IdeaTestUtil.requireRealJdkHome(), false)
+    public Sdk getSdk() {
+      return JavaSdk.getInstance().createJdk("TEST_JDK", IdeaTestUtil.requireRealJdkHome(), false);
     }
-  }
+  };
 
+  public static LightProjectDescriptor getProjectDescriptor() {
+    return PROJECT_DESCRIPTOR;
+  }
 }
