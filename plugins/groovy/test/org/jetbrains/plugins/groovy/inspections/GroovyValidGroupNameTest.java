5279180ea345e6628d766c704fcd703833d80ac2
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 16:53:45 +0200

    [Groovy] convert GroovyValidGroupNameTest to Java (IDEA-327339)
    GitOrigin-RevId: ac4552c59997008492d7cab3d090c24133a3005e

--- plugins/groovy/test/org/jetbrains/plugins/groovy/inspections/GroovyValidGroupNameTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/inspections/GroovyValidGroupNameTest.java
@@ -1,29 +1,21 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jetbrains.plugins.groovy.inspections
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.inspections;
 
-import com.intellij.testFramework.InspectionTestUtil
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
-import com.siyeh.ig.dataflow.UnnecessaryLocalVariableInspection
-import org.jetbrains.plugins.groovy.codeInspection.unusedDef.UnusedDefInspection
+import com.intellij.codeInspection.InspectionProfileEntry;
+import com.intellij.testFramework.InspectionTestUtil;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import com.siyeh.ig.dataflow.UnnecessaryLocalVariableInspection;
+import org.jetbrains.plugins.groovy.codeInspection.unusedDef.UnusedDefInspection;
 
-class GroovyValidGroupNameTest extends LightJavaCodeInsightFixtureTestCase {
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-  void testGroupNamesIsSame() {
-    def tools = InspectionTestUtil.instantiateTools([UnusedDefInspection, UnnecessaryLocalVariableInspection])
-    assert tools.collect { it.groupDisplayName }.toSet().size() == 1 // all tools have same groupDisplayName
+public class GroovyValidGroupNameTest extends LightJavaCodeInsightFixtureTestCase {
+  public void testGroupNamesIsSame() {
+    List<InspectionProfileEntry> tools = InspectionTestUtil.instantiateTools(List.of(UnusedDefInspection.class,
+                                                                                     UnnecessaryLocalVariableInspection.class));
+    Set<String> result = tools.stream().map(it -> it.getGroupDisplayName()).collect(Collectors.toSet());
+    assertEquals(1, result.size());
   }
 }
