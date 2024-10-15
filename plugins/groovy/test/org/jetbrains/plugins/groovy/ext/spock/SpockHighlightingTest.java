c197d51a2679d25b9b33e05895a26d4256a3c630
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 13:06:29 +0200

    [Groovy] convert SpockHighlightingTest to Java (IDEA-327339)
    GitOrigin-RevId: 38fdb8466ef0b50a1779c8ad1abef418006bddb0

--- plugins/groovy/test/org/jetbrains/plugins/groovy/ext/spock/SpockHighlightingTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/ext/spock/SpockHighlightingTest.java
@@ -1,31 +1,33 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.groovy.ext.spock
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.ext.spock;
 
-import com.intellij.codeInspection.LocalInspectionTool
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.codeInspection.assignment.GroovyAssignabilityCheckInspection
-import org.jetbrains.plugins.groovy.codeInspection.confusing.GroovyPointlessArithmeticInspection
-import org.jetbrains.plugins.groovy.codeInspection.confusing.GroovyPointlessBooleanInspection
-import org.jetbrains.plugins.groovy.codeInspection.style.JavaStylePropertiesInvocationInspection
-import org.jetbrains.plugins.groovy.codeInspection.untypedUnresolvedAccess.GrUnresolvedAccessInspection
-import org.jetbrains.plugins.groovy.util.HighlightingTest
-import org.junit.Test
+import com.intellij.codeInspection.LocalInspectionTool;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.codeInspection.assignment.GroovyAssignabilityCheckInspection;
+import org.jetbrains.plugins.groovy.codeInspection.confusing.GroovyPointlessArithmeticInspection;
+import org.jetbrains.plugins.groovy.codeInspection.confusing.GroovyPointlessBooleanInspection;
+import org.jetbrains.plugins.groovy.codeInspection.style.JavaStylePropertiesInvocationInspection;
+import org.jetbrains.plugins.groovy.codeInspection.untypedUnresolvedAccess.GrUnresolvedAccessInspection;
+import org.jetbrains.plugins.groovy.util.HighlightingTest;
+import org.junit.Test;
 
-@CompileStatic
-class SpockHighlightingTest extends SpockTestBase implements HighlightingTest {
+import java.util.Arrays;
+import java.util.Collection;
+
+public class SpockHighlightingTest extends SpockTestBase implements HighlightingTest {
 
   @Override
-  Collection<? extends Class<? extends LocalInspectionTool>> getInspections() {
-    return [
-      GroovyAssignabilityCheckInspection,
-      GroovyPointlessBooleanInspection,
-      GroovyPointlessArithmeticInspection,
-    ]
+  public @NotNull Collection<Class<? extends LocalInspectionTool>> getInspections() {
+    return Arrays.asList(
+      GroovyAssignabilityCheckInspection.class,
+      GroovyPointlessBooleanInspection.class,
+      GroovyPointlessArithmeticInspection.class
+    );
   }
 
   @Test
-  void 'single pipe column separator'() {
-    highlightingTest '''\
+  public void singlePipeColumnSeparator() {
+    highlightingTest("""
 class FooSpec extends spock.lang.Specification {
   def feature(int a, boolean b) {
     where:
@@ -43,12 +45,12 @@ class FooSpec extends spock.lang.Specification {
     2 | _
   }
 }
-'''
+""");
   }
 
   @Test
-  void 'double pipe column separator'() {
-    highlightingTest '''\
+  public void doublePipeColumnSeparator() {
+    highlightingTest("""
 class FooSpec extends spock.lang.Specification {
   
   def feature() {
@@ -63,13 +65,13 @@ class FooSpec extends spock.lang.Specification {
     true || false
   }
 }
-'''
+""");
   }
 
   @Test
-  void 'interactions'() {
-    enableInspectionAsWarning(new JavaStylePropertiesInvocationInspection())
-    highlightingTest '''\
+  public void interactions() {
+    enableInspectionAsWarning(new JavaStylePropertiesInvocationInspection());
+    highlightingTest("""
 interface Subscriber {
   def receive(String event)
   void setIncludeFollowUp(o)
@@ -88,26 +90,26 @@ class FooSpec extends spock.lang.Specification {
     0 * sub.setIncludeFollowUp(1)
   }
 }
-'''
+""");
   }
 
   @Test
-  void 'dot in method name'() {
-    highlightingTest '''\
+  public void dotInMethodName() {
+    highlightingTest("""
 class FooSpec extends spock.lang.Specification {
   def <warning descr="Method name contains illegal character(s): '.'">'not.a.feature'</warning>() {}
   def 'a.feature'() {
     when:
     42
-  }    
+  }
 }
-'''
+""");
   }
 
   @Test
-  void 'indirect extending'() {
-    fixture.enableInspections(GrUnresolvedAccessInspection)
-    highlightingTest '''\
+  public void indirectExtending() {
+    getFixture().enableInspections(new GrUnresolvedAccessInspection());
+    highlightingTest("""
 class A extends spock.lang.Specification {}
 
 class FooSpec extends A {
@@ -118,15 +120,15 @@ class FooSpec extends A {
     where:
     a     | b
     "foo" | "bar"
-  }    
+  }
 }
-'''
+""");
   }
 
   @Test
-  void 'Use annotation'() {
-    fixture.enableInspections(GrUnresolvedAccessInspection)
-    highlightingTest '''\
+  public void useAnnotation() {
+    getFixture().enableInspections(new GrUnresolvedAccessInspection());
+    highlightingTest("""
 import spock.lang.Specification
 import spock.util.mop.Use
 
@@ -143,6 +145,6 @@ class HelloSpockSpec extends Specification {
         1.foo() == "abs"
     }
 }
-'''
+""");
   }
 }
