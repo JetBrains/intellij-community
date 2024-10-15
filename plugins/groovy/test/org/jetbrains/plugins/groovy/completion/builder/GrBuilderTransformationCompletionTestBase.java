32b0d234750de34fc7797464ef6b7b1d8d2313ad
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-15 15:52:16 +0200

    [Groovy] convert GrBuilderTransformationCompletionTestBase to Java (IDEA-327339)
    GitOrigin-RevId: 0966fc18161d3fb93e20a5467598fae100325ccc

--- plugins/groovy/test/org/jetbrains/plugins/groovy/completion/builder/GrBuilderTransformationCompletionTestBase.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/completion/builder/GrBuilderTransformationCompletionTestBase.java
@@ -1,29 +1,25 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.completion.builder
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.completion.builder;
 
-import com.intellij.testFramework.LightProjectDescriptor
-import groovy.transform.CompileStatic
-import org.jetbrains.annotations.NotNull
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.completion.CompletionResult
-import org.jetbrains.plugins.groovy.completion.GroovyCompletionTestBase
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.completion.CompletionResult;
+import org.jetbrains.plugins.groovy.completion.GroovyCompletionTestBase;
 
-import static com.intellij.codeInsight.completion.CompletionType.BASIC
+import static com.intellij.codeInsight.completion.CompletionType.BASIC;
 
-@CompileStatic
-abstract class GrBuilderTransformationCompletionTestBase extends GroovyCompletionTestBase {
-
+public abstract class GrBuilderTransformationCompletionTestBase extends GroovyCompletionTestBase {
   @Override
-  @NotNull
-  protected LightProjectDescriptor getProjectDescriptor() {
-    return GroovyProjectDescriptors.GROOVY_LATEST
+  protected @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_LATEST;
   }
 
-  void doCompletionTest(String text, String... args) {
-    doCompletionTest(text, CompletionResult.contain, args)
+  public void doCompletionTest(String text, String... args) {
+    doCompletionTest(text, CompletionResult.contain, args);
   }
 
-  void doCompletionTest(String text, CompletionResult cr, String... args) {
-    doVariantableTest(text, '', BASIC, cr, 1, args)
+  public void doCompletionTest(String text, CompletionResult cr, String... args) {
+    doVariantableTest(text, "", BASIC, cr, 1, args);
   }
 }
