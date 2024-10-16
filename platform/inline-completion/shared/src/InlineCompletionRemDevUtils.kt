390b0a378ff915ea208380baf3223dcc08663387
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2024-10-16 18:49:51 +0200

    [inline-completion] IJPL-161906: create a feature flag for new implementation
    (cherry picked from commit dab6314153ee2f68138338f3b5a0ad7a8b15599f)
    
    IJ-MR-147762
    
    GitOrigin-RevId: 9199d30b132ca349421a8b8146f47d71b9e03b78

---
+++ platform/inline-completion/shared/src/InlineCompletionRemDevUtils.kt
@@ -0,0 +1,13 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.codeInsight.inline.completion
+
+import com.intellij.openapi.util.registry.Registry
+import org.jetbrains.annotations.ApiStatus
+
+@ApiStatus.Internal
+object InlineCompletionRemDevUtils {
+
+  fun useRhizome(): Boolean {
+    return Registry.`is`("inline.completion.rem.dev.use.rhizome", false)
+  }
+}
