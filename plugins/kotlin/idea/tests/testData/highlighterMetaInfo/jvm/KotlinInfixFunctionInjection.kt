5c19977d4d9a9dc83eac9ed1f026c7f0c1f02f6d
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2024-10-16 17:23:56 +0200

    [kotlin] Improve test data
    #KTIJ-1001
    
    GitOrigin-RevId: 4a90442d055326a800875e5ef3aaae03fcac390e

--- plugins/kotlin/idea/tests/testData/highlighterMetaInfo/jvm/KotlinInfixFunctionInjection.kt
+++ plugins/kotlin/idea/tests/testData/highlighterMetaInfo/jvm/KotlinInfixFunctionInjection.kt
@@ -3,12 +3,15 @@
 // HIGHLIGHTER_ATTRIBUTES_KEY
 import org.intellij.lang.annotations.Language
 
-infix fun String.xml(@Language("kotlin") content: String){}
+infix fun String.nonInjected(content: String){}
 
-infix @Language("kotlin") fun String.xml2(@Language("kotlin") content: String){}
+infix fun String.injected(@Language("kotlin") content: String){}
+
+infix @Language("kotlin") fun String.injectedWithReceiver(@Language("kotlin") content: String){}
 
 fun main() {
-    "foo" xml "fun foo() {}"
-    "foo".xml("fun foo() {}")
-    "fun bar() {}" xml2 "fun foo() {}"
+    "fun f0() {}" nonInjected "fun f1() {}"
+    "fun f2() {}" injected "fun f3() {}"
+    "fun f4() {}".injected("fun f5() {}")
+    "fun f6() {}" injectedWithReceiver "fun f7() {}"
 }
