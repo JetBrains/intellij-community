e3b1be0c23090f0f19a6cc7d757198ee235324eb
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2024-10-16 22:05:19 +0200

    [inline-completion] IJPL-161906: initial implementation of synchronizing between frontend and backend
    GitOrigin-RevId: a6b3cfb5ca4d016d521d47f127fa4222e8a87568

--- platform/inline-completion/shared/src/InlineCompletionRemDevUtils.kt
+++ platform/inline-completion/shared/src/InlineCompletionRemDevUtils.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.inline.completion
 
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.impl.editorIdOrNull
 import com.intellij.openapi.util.registry.Registry
 import org.jetbrains.annotations.ApiStatus
 
@@ -9,5 +11,9 @@ object InlineCompletionRemDevUtils {
 
   fun useRhizome(): Boolean {
     return Registry.`is`("inline.completion.rem.dev.use.rhizome", false)
+  }
+
+  fun isSupported(editor: Editor): Boolean {
+    return editor.editorIdOrNull() != null
   }
 }
---
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/RemDevAggregatorInlineCompletionProvider.kt
@@ -0,0 +1,9 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.codeInsight.inline.completion
+
+import org.jetbrains.annotations.ApiStatus
+
+// TODO docs
+@ApiStatus.Internal
+@ApiStatus.NonExtendable
+interface RemDevAggregatorInlineCompletionProvider : InlineCompletionProvider
