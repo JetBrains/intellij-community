60e67877baaff95f217ba316f3016ce4bd51b3ce
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2024-10-16 18:02:43 +0200

    [inline-completion] IJPL-161906: allow customizing `InlineCompletionHandler`
    GitOrigin-RevId: d5f24399d5ab135ad226b333495eecbbc3194e09

---
+++ platform/inline-completion/frontend/src/FrontendInlineCompletionHandlerInitializer.kt
@@ -0,0 +1,15 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.codeInsight.inline.completion.frontend
+
+import com.intellij.codeInsight.inline.completion.InlineCompletionHandler
+import com.intellij.codeInsight.inline.completion.InlineCompletionHandlerInitializer
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.editor.Editor
+import kotlinx.coroutines.CoroutineScope
+
+internal class FrontendInlineCompletionHandlerInitializer : InlineCompletionHandlerInitializer {
+  override fun initialize(editor: Editor, scope: CoroutineScope, disposable: Disposable): InlineCompletionHandler? {
+    // TODO
+    return object : InlineCompletionHandler(scope, editor, disposable) {}
+  }
+}
