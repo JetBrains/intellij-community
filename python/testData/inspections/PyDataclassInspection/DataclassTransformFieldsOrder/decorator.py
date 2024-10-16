8ddebc4e919ed84cf9343dff9ce1fd786143bdab
Daniil Kalinin <Daniil.Kalinin@jetbrains.com>
2024-10-16 14:28:41 +0200

    [python] dataclass_transform: use the annotated class as a resolve anchor for resolving the field specifiers
    GitOrigin-RevId: f15a07836e7aeac7c46b489b4742e8248a0e6ef4

--- python/testData/inspections/PyDataclassInspection/DataclassTransformFieldsOrder/decorator.py
+++ python/testData/inspections/PyDataclassInspection/DataclassTransformFieldsOrder/decorator.py
@@ -11,6 +11,12 @@ class MyField:
     def __init__(self, **kwargs):
         ...
 
+class registry:
+    @dataclass_transform(field_specifiers=(my_field, MyField,))
+    def mapped_as_dataclass(**kwargs) -> Callable[[type[T]], type[T]]:
+        ...
+
+
 
 @dataclass_transform(field_specifiers=(my_field, MyField,))
 def my_dataclass(**kwargs) -> Callable[[type[T]], type[T]]:
