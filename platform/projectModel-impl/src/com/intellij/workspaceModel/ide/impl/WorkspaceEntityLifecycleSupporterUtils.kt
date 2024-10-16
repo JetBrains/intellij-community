9d8c45af9b307dd60b39e2be703eda46f6a1d14f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-10-16 07:49:35 +0200

    IJPL-162718 WorkspaceEntitiesLifecycleActivity - get rid of WriteAction.runAndWait
    GitOrigin-RevId: 4879c07b3e8f4db3b4663a1bbfd098a4fe5ff0d5

--- platform/projectModel-impl/src/com/intellij/workspaceModel/ide/impl/WorkspaceEntityLifecycleSupporterUtils.kt
+++ platform/projectModel-impl/src/com/intellij/workspaceModel/ide/impl/WorkspaceEntityLifecycleSupporterUtils.kt
@@ -1,8 +1,8 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.workspaceModel.ide.impl
 
-import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.application.backgroundWriteAction
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Ref
 import com.intellij.platform.backend.workspace.WorkspaceEntityLifecycleSupporter
@@ -15,39 +15,38 @@ import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
 object WorkspaceEntityLifecycleSupporterUtils {
-  fun ensureEntitiesInWorkspaceAreAsProviderDefined(project: Project, provider: WorkspaceEntityLifecycleSupporter<*, *>) {
-    val workspaceModel = WorkspaceModel.getInstance(project)
-    val builderRef: Ref<MutableEntityStorage?> = Ref.create()
+  suspend fun ensureEntitiesInWorkspaceAreAsProviderDefined(project: Project, provider: WorkspaceEntityLifecycleSupporter<*, *>) {
+    val workspaceModel = project.serviceAsync<WorkspaceModel>()
+    val builderRef = Ref<MutableEntityStorage>()
     val snapshot = workspaceModel.currentSnapshot
+    ensureInitialized(project = project, provider = provider, snapshot = snapshot, builderRef = builderRef)
 
-    ensureInitialized(project, provider, snapshot, builderRef)
-
-    builderRef.get()?.also { builder -> WriteAction.runAndWait<RuntimeException> { writeBuilder(workspaceModel, builder) } }
+    builderRef.get()?.let { backgroundWriteAction { writeBuilder(workspaceModel, it) } }
   }
 
   suspend fun ensureAllEntitiesInWorkspaceAreAsProvidersDefined(project: Project) {
-    val workspaceModel = WorkspaceModel.getInstance(project)
+    val workspaceModel = project.serviceAsync<WorkspaceModel>()
     val snapshot = workspaceModel.currentSnapshot
-    val builderRef: Ref<MutableEntityStorage?> = Ref.create()
+    val builderRef = Ref<MutableEntityStorage>()
 
     WorkspaceEntityLifecycleSupporter.EP_NAME.forEachExtensionSafe { provider ->
-      ensureInitialized(project, provider, snapshot, builderRef)
+      ensureInitialized(project = project, provider = provider, snapshot = snapshot, builderRef = builderRef)
     }
-    builderRef.get()?.also { builder -> backgroundWriteAction { writeBuilder(workspaceModel, builder) } }
+    builderRef.get()?.let { backgroundWriteAction { writeBuilder(workspaceModel, it) } }
   }
 
-
-  private fun writeBuilder(workspaceModel: WorkspaceModel,
-                           builder: MutableEntityStorage) {
-    workspaceModel.updateProjectModel("ConstantEntitiesCheckActivity") { tempBuilder: MutableEntityStorage ->
-      tempBuilder.applyChangesFrom(builder)
+  private fun writeBuilder(workspaceModel: WorkspaceModel, builder: MutableEntityStorage) {
+    workspaceModel.updateProjectModel("ConstantEntitiesCheckActivity") {
+      it.applyChangesFrom(builder)
     }
   }
 
-  private fun <E : WorkspaceEntity, M : WorkspaceEntity.Builder<E>> ensureInitialized(project: Project,
-                                                                                      provider: WorkspaceEntityLifecycleSupporter<E, M>,
-                                                                                      snapshot: ImmutableEntityStorage,
-                                                                                      builderRef: Ref<MutableEntityStorage?>) {
+  private fun <E : WorkspaceEntity, M : WorkspaceEntity.Builder<E>> ensureInitialized(
+    project: Project,
+    provider: WorkspaceEntityLifecycleSupporter<E, M>,
+    snapshot: ImmutableEntityStorage,
+    builderRef: Ref<MutableEntityStorage>,
+  ) {
     val expectedEntity = provider.createSampleEntity(project)
     val actualEntities = snapshot.entities(provider.getEntityClass()).toList()
 
@@ -59,10 +58,11 @@ object WorkspaceEntityLifecycleSupporterUtils {
       return
     }
 
-    if (builderRef.isNull) {
-      builderRef.set(snapshot.toBuilder())
+    var builder = builderRef.get()
+    if (builder == null) {
+      builder = snapshot.toBuilder()
+      builderRef.set(builder)
     }
-    val builder = builderRef.get()!!
 
     for (oldEntity in actualEntities) {
       builder.removeEntity(oldEntity)
