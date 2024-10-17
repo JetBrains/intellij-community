e30b0b981ff55a914a4f8f966c797eced44863ce
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-17 10:04:46 +0200

    [Groovy] convert GroovyMoveLeftRightHandlerTest to Java (IDEA-327339)
    GitOrigin-RevId: 761a67082d7bd5c91802d5820efc393a05bd2b0c

--- plugins/groovy/test/org/jetbrains/plugins/groovy/codeInsight/editorActions/GroovyMoveLeftRightHandlerTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/codeInsight/editorActions/GroovyMoveLeftRightHandlerTest.java
@@ -1,125 +1,113 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.codeInsight.editorActions
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.codeInsight.editorActions;
 
-import com.intellij.openapi.actionSystem.IdeActions
-import com.intellij.testFramework.LightProjectDescriptor
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
+import com.intellij.openapi.actionSystem.IdeActions;
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.LightGroovyTestCase;
 
-@CompileStatic
-class GroovyMoveLeftRightHandlerTest extends LightGroovyTestCase {
-
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_LATEST
-
-  private void doTest(String before, String after = null) {
-    fixture.with {
-      configureByText '_.groovy', before
-      performEditorAction(IdeActions.MOVE_ELEMENT_RIGHT)
-      if (after != null) {
-        checkResult after
-        performEditorAction(IdeActions.MOVE_ELEMENT_LEFT)
-      }
-      checkResult before
+public class GroovyMoveLeftRightHandlerTest extends LightGroovyTestCase {
+  private void doTest(String before, String after) {
+    getFixture().configureByText("_.groovy", before);
+    getFixture().performEditorAction(IdeActions.MOVE_ELEMENT_RIGHT);
+    if (after != null) {
+      getFixture().checkResult(after);
+      getFixture().performEditorAction(IdeActions.MOVE_ELEMENT_LEFT);
     }
+    getFixture().checkResult(before);
   }
 
-  void 'test annotation argument'() {
-    doTest '@MyAnno(a = <caret>"e", b = "f") def a',
-           '@MyAnno(b = "f", a = <caret>"e") def a'
-    doTest '@MyAnno(a = "e", <caret>b = "f") def a'
+  private void doTest(String before) {
+    doTest(before, null);
   }
 
-  void 'test annotation array initializer'() {
-    doTest '@MyAnno([<caret>1, 2, 3]) def a',
-           '@MyAnno([2, <caret>1, 3]) def a'
-    doTest '@MyAnno(a = [1, 2, <caret>3], b = 2) def a'
+  public void test_annotation_argument() {
+    doTest("@MyAnno(a = <caret>\"e\", b = \"f\") def a", "@MyAnno(b = \"f\", a = <caret>\"e\") def a");
+    doTest("@MyAnno(a = \"e\", <caret>b = \"f\") def a");
   }
 
-  void 'test argument list'() {
-    doTest 'def foo(def...a) {}; foo(1, <caret>2, 3)',
-           'def foo(def...a) {}; foo(1, 3, <caret>2)'
-    doTest 'def foo(def...a) {}; foo(1, <caret>a: 2, 3)',
-           'def foo(def...a) {}; foo(1, 3, <caret>a: 2)'
+  public void test_annotation_array_initializer() {
+    doTest("@MyAnno([<caret>1, 2, 3]) def a", "@MyAnno([2, <caret>1, 3]) def a");
+    doTest("@MyAnno(a = [1, 2, <caret>3], b = 2) def a");
   }
 
-  void 'test enum definition'() {
-    doTest 'enum E {ONE, <caret>TWO, THREE}',
-           'enum E {ONE, THREE, <caret>TWO}'
-    doTest 'enum E {ONE, TWO, <caret>THREE}'
+  public void test_argument_list() {
+    doTest("def foo(def...a) {}; foo(1, <caret>2, 3)", "def foo(def...a) {}; foo(1, 3, <caret>2)");
+    doTest("def foo(def...a) {}; foo(1, <caret>a: 2, 3)", "def foo(def...a) {}; foo(1, 3, <caret>a: 2)");
   }
 
-  void 'test list or map'() {
-    doTest '[<caret>1, 2, 3]',
-           '[2, <caret>1, 3]'
-    doTest '[<caret>a: 1, b: 2, c: 3]',
-           '[b: 2, <caret>a: 1, c: 3]'
-    doTest '[<caret>1, a: 2, 3]',
-           '[a: 2, <caret>1, 3]'
+  public void test_enum_definition() {
+    doTest("enum E {ONE, <caret>TWO, THREE}", "enum E {ONE, THREE, <caret>TWO}");
+    doTest("enum E {ONE, TWO, <caret>THREE}");
   }
 
-  void 'test method modifier list'() {
-    doTest '<caret>synchronized @Deprecated def foo() {}',
-           '@Deprecated <caret>synchronized def foo() {}'
-    doTest 'synchronized @Deprecated <caret>def foo() {}'
+  public void test_list_or_map() {
+    doTest("[<caret>1, 2, 3]", "[2, <caret>1, 3]");
+    doTest("[<caret>a: 1, b: 2, c: 3]", "[b: 2, <caret>a: 1, c: 3]");
+    doTest("[<caret>1, a: 2, 3]", "[a: 2, <caret>1, 3]");
   }
 
-  void 'test class modifier list'() {
-    doTest '<caret>public @Deprecated class A {}',
-           '@Deprecated <caret>public class A {}'
-    doTest 'public <caret>@Deprecated class A {}'
+  public void test_method_modifier_list() {
+    doTest("<caret>synchronized @Deprecated def foo() {}", "@Deprecated <caret>synchronized def foo() {}");
+    doTest("synchronized @Deprecated <caret>def foo() {}");
   }
 
-  void 'test parameter list'() {
-    doTest 'def foo(<caret>a, b = 2, c){}',
-           'def foo(b = 2, <caret>a, c){}'
-    doTest 'def foo(a, b = 2, <caret>c){}'
+  public void test_class_modifier_list() {
+    doTest("<caret>public @Deprecated class A {}", "@Deprecated <caret>public class A {}");
+    doTest("public <caret>@Deprecated class A {}");
   }
 
-  void 'test implements list'() {
-    doTest 'class A implements <caret>Foo, Bar, Baz {}',
-           'class A implements Bar, <caret>Foo, Baz {}'
-    doTest 'class A implements Foo, Bar, <caret>Baz {}'
+  public void test_parameter_list() {
+    doTest("def foo(<caret>a, b = 2, c){}", "def foo(b = 2, <caret>a, c){}");
+    doTest("def foo(a, b = 2, <caret>c){}");
   }
 
-  void 'test extends list'() {
-    doTest 'class B extends <caret>Foo, Bar, Baz {}',
-           'class B extends Bar, <caret>Foo, Baz {}'
-    doTest 'class B extends Foo, Bar, <caret>Baz {}'
+  public void test_implements_list() {
+    doTest("class A implements <caret>Foo, Bar, Baz {}", "class A implements Bar, <caret>Foo, Baz {}");
+    doTest("class A implements Foo, Bar, <caret>Baz {}");
   }
 
-  void 'test throws list'() {
-    doTest 'def foo() throws <caret>Foo, Bar, Baz {}',
-           'def foo() throws Bar, <caret>Foo, Baz {}'
-    doTest 'def foo() throws Foo, Bar, <caret>Baz {}'
+  public void test_extends_list() {
+    doTest("class B extends <caret>Foo, Bar, Baz {}", "class B extends Bar, <caret>Foo, Baz {}");
+    doTest("class B extends Foo, Bar, <caret>Baz {}");
   }
 
-  void 'test type parameter list'() {
-    doTest 'class A<<caret>T, U, K> {}',
-           'class A<U, <caret>T, K> {}'
-    doTest 'class A<T, U, <caret>K> {}'
+  public void test_throws_list() {
+    doTest("def foo() throws <caret>Foo, Bar, Baz {}", "def foo() throws Bar, <caret>Foo, Baz {}");
+    doTest("def foo() throws Foo, Bar, <caret>Baz {}");
   }
 
-  void 'test type argument list'() {
-    doTest 'new A<<caret>K,V>()', 'new A<V,<caret>K>()'
-    doTest 'new A<K,<caret>V>()'
+  public void test_type_parameter_list() {
+    doTest("class A<<caret>T, U, K> {}", "class A<U, <caret>T, K> {}");
+    doTest("class A<T, U, <caret>K> {}");
   }
 
-  void 'test variable declaration'() {
-    doTest 'def a = 1, <caret>b = 2, c', 'def a = 1, c, <caret>b = 2'
-    doTest 'def a = 1, b = 2, <caret>c'
+  public void test_type_argument_list() {
+    doTest("new A<<caret>K,V>()", "new A<V,<caret>K>()");
+    doTest("new A<K,<caret>V>()");
   }
 
-  void 'test binary expression'() {
-    doTest '<caret>1 + 2 + 3'
-    doTest '1 + <caret>2 + 3'
+  public void test_variable_declaration() {
+    doTest("def a = 1, <caret>b = 2, c", "def a = 1, c, <caret>b = 2");
+    doTest("def a = 1, b = 2, <caret>c");
   }
 
-  void 'test for update'() {
-    doTest 'for (;; <caret>1, 2, 3) {}', 'for (;; 2, <caret>1, 3) {}'
+  public void test_binary_expression() {
+    doTest("<caret>1 + 2 + 3");
+    doTest("1 + <caret>2 + 3");
   }
 
-  void 'test array initializer'() {
-    doTest 'new int[]{<caret>1, 2, 3}', 'new int[]{2, <caret>1, 3}'
+  public void test_for_update() {
+    doTest("for (;; <caret>1, 2, 3) {}", "for (;; 2, <caret>1, 3) {}");
+  }
+
+  public void test_array_initializer() {
+    doTest("new int[]{<caret>1, 2, 3}", "new int[]{2, <caret>1, 3}");
+  }
+
+  @Override
+  public final @NotNull LightProjectDescriptor getProjectDescriptor() {
+    return GroovyProjectDescriptors.GROOVY_LATEST;
   }
 }
