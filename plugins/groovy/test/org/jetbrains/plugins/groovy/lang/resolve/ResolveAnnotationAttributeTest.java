e8815bc2c6a4c5cad26ac3383836adc3dbf08afc
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-17 13:56:03 +0200

    [Groovy] convert ResolveAnnotationAttributeTest to Java (IDEA-327339)
    GitOrigin-RevId: 41ee03b551da06843dca7a27cf66621d721c42e7

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/resolve/ResolveAnnotationAttributeTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/resolve/ResolveAnnotationAttributeTest.java
@@ -1,91 +1,81 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jetbrains.plugins.groovy.lang.resolve
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.resolve;
 
-import com.intellij.psi.PsiPolyVariantReference
-import org.jetbrains.plugins.groovy.lang.psi.api.GroovyResolveResult
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.members.GrAnnotationMethod
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.members.GrMethod
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiPolyVariantReference;
+import com.intellij.psi.ResolveResult;
+import com.intellij.testFramework.UsefulTestCase;
+import junit.framework.TestCase;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.members.GrAnnotationMethod;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.members.GrMethod;
 
 /**
  * @author Max Medvedev
  */
-class ResolveAnnotationAttributeTest extends GroovyResolveTestCase {
+public class ResolveAnnotationAttributeTest extends GroovyResolveTestCase {
+  public void testSimpleAttribute() {
+    final PsiPolyVariantReference ref = (PsiPolyVariantReference)configureByText(
+      """
+        @interface A {
+          String foo()
+        }
+        @A(f<caret>oo = '3')
+        class A {}
+        """);
 
-  void testSimpleAttribute() {
-    final ref = this.configureByText('''
-@interface A {
-  String foo()
-}
-
-@A(f<caret>oo = '3')
-class A {}
-''') as PsiPolyVariantReference
-
-    assertNotNull(ref.resolve())
+    TestCase.assertNotNull(ref.resolve());
   }
 
-  void testAliasAttribute() {
-    final ref = this.configureByText('''
-@interface A {
-  String foo()
-}
+  public void testAliasAttribute() {
+    final PsiPolyVariantReference ref = (PsiPolyVariantReference)configureByText(
+      """
+        @interface A {
+          String foo()
+        }
+        
+        @groovy.transform.AnnotationCollector([A])
+        @interface Alias {
+          String foo()
+        }
+        
+        @Alias(f<caret>oo = '3')
+        class X {}
+        """);
 
-@groovy.transform.AnnotationCollector([A])
-@interface Alias {
-  String foo()
-}
-
-@Alias(f<caret>oo = '3')
-class X {}
-''') as PsiPolyVariantReference
-
-    final resolved = ref.resolve()
-    assertNotNull(resolved)
-    assertInstanceOf(resolved, GrMethod)
-    assertEquals('A', resolved.containingClass.qualifiedName)
+    final PsiElement resolved = ref.resolve();
+    TestCase.assertNotNull(resolved);
+    UsefulTestCase.assertInstanceOf(resolved, GrMethod.class);
+    assertEquals("A", ((GrMethod)resolved).getContainingClass().getQualifiedName());
   }
 
-  void testMultiAliasAttribute() {
-    final ref = this.configureByText('''
-@interface A {
-  String foo()
-}
-@interface B {
-  String foo()
-}
+  public void testMultiAliasAttribute() {
+    final PsiPolyVariantReference ref = (PsiPolyVariantReference)configureByText(
+      """
+        @interface A {
+          String foo()
+        }
+        @interface B {
+          String foo()
+        }
+        
+        @groovy.transform.AnnotationCollector([A, B])
+        @interface Alias {
+          String foo()
+        }
+        
+        @Alias(f<caret>oo = '3')
+        class X {}
+        """);
 
-@groovy.transform.AnnotationCollector([A, B])
-@interface Alias {
-  String foo()
-}
+    final PsiElement resolved = ref.resolve();
+    assertNull(resolved);
 
-@Alias(f<caret>oo = '3')
-class X {}
-''') as PsiPolyVariantReference
+    for (ResolveResult result : ref.multiResolve(false)) {
+      final PsiElement r = result.getElement();
 
-    final resolved = ref.resolve()
-    assertNull(resolved)
-
-    for (GroovyResolveResult result : ref.multiResolve(false)) {
-      final r = result.element
-
-      assertInstanceOf(r, GrAnnotationMethod)
-      assertTrue('Alias' != r.containingClass.qualifiedName)
-
+      UsefulTestCase.assertInstanceOf(r, GrAnnotationMethod.class);
+      //noinspection SimplifiableAssertion
+      assertFalse("Alias".equals(((GrAnnotationMethod)r).getContainingClass().getQualifiedName()));
     }
   }
 }
