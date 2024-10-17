10cb932d57a529562d62baefbe0994e022c9c2ac
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-17 10:47:08 +0200

    [Groovy] convert GrEclipseTestBase to Java (IDEA-327339)
    GitOrigin-RevId: 619ba60741353971731aec6c0bcd93c50bfdc637

--- plugins/groovy/test/org/jetbrains/plugins/groovy/compiler/GrEclipseTestBase.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/compiler/GrEclipseTestBase.java
@@ -1,35 +1,38 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.groovy.compiler
+package org.jetbrains.plugins.groovy.compiler;
 
-import com.intellij.compiler.CompilerConfiguration
-import com.intellij.compiler.CompilerConfigurationImpl
-import com.intellij.jarRepository.RemoteRepositoryDescription
-import com.intellij.openapi.vfs.JarFileSystem
-import com.intellij.testFramework.JUnit38AssumeSupportRunner
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.RepositoryTestLibrary
-import org.junit.runner.RunWith
+import com.intellij.compiler.CompilerConfiguration;
+import com.intellij.compiler.CompilerConfigurationImpl;
+import com.intellij.jarRepository.RemoteRepositoryDescription;
+import com.intellij.openapi.vfs.JarFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.testFramework.JUnit38AssumeSupportRunner;
+import org.jetbrains.plugins.groovy.RepositoryTestLibrary;
+import org.junit.runner.RunWith;
 
-@CompileStatic
+import java.util.ArrayList;
+import java.util.List;
+
 @RunWith(JUnit38AssumeSupportRunner.class)
-abstract class GrEclipseTestBase extends GroovyCompilerTest {
-  public static final RemoteRepositoryDescription GROOVY_PLUGIN_RELEASE = new RemoteRepositoryDescription(
-    "groovy.plugin.release",
-    "JFrog Groovy Plugin Release repository",
-    "https://groovy.jfrog.io/artifactory/plugins-release-local/"
-  );
-
-  protected abstract String getGrEclipseArtifactID()
+public abstract class GrEclipseTestBase extends GroovyCompilerTest {
+  protected abstract String getGrEclipseArtifactID();
 
   @Override
-  protected void setUp() {
-    super.setUp()
-    ((CompilerConfigurationImpl)CompilerConfiguration.getInstance(project)).defaultCompiler = new GreclipseIdeaCompiler(project)
-    def jarRoot = RepositoryTestLibrary.loadRoots(project, grEclipseArtifactID, GROOVY_PLUGIN_RELEASE).first().file
-    GreclipseIdeaCompilerSettings.getSettings(project).greclipsePath = JarFileSystem.instance.getVirtualFileForJar(jarRoot).path
+  protected void setUp() throws Exception {
+    super.setUp();
+    ((CompilerConfigurationImpl)CompilerConfiguration.getInstance(getProject())).setDefaultCompiler(
+      new GreclipseIdeaCompiler(getProject()));
+    VirtualFile jarRoot = RepositoryTestLibrary.loadRoots(getProject(), getGrEclipseArtifactID(), GROOVY_PLUGIN_RELEASE)
+      .iterator().next().getFile();
+    GreclipseIdeaCompilerSettings.getSettings(getProject()).greclipsePath =
+      JarFileSystem.getInstance().getVirtualFileForJar(jarRoot).getPath();
   }
 
   protected List<String> chunkRebuildMessage(String builder) {
-    return []
+    return new ArrayList<>();
   }
+
+  public static final RemoteRepositoryDescription GROOVY_PLUGIN_RELEASE =
+    new RemoteRepositoryDescription("groovy.plugin.release", "JFrog Groovy Plugin Release repository",
+                                    "https://groovy.jfrog.io/artifactory/plugins-release-local/");
 }
