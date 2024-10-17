d67d4b38547475e16bf629e21919b36aa41c7f09
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-17 13:46:23 +0200

    [Groovy] convert GrAnnotationCollectorTest to Java (IDEA-327339)
    GitOrigin-RevId: 08f6df98835bc5e8b6403f798749c47097bd1f24

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/aliasAnnotations/GrAnnotationCollectorTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/aliasAnnotations/GrAnnotationCollectorTest.java
@@ -1,143 +1,137 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.lang.aliasAnnotations
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.aliasAnnotations;
 
-import com.intellij.psi.util.PsiTreeUtil
-import org.jetbrains.annotations.NonNls
-import org.jetbrains.annotations.NotNull
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
-import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.annotation.GrAnnotation
-import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.annotation.GrAnnotationNameValuePair
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrTypeDefinition
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import junit.framework.TestCase;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.LightGroovyTestCase;
+import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.annotation.GrAnnotation;
+import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.annotation.GrAnnotationNameValuePair;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrTypeDefinition;
 
 /**
  * @author Max Medvedev
  */
-class GrAnnotationCollectorTest extends LightGroovyTestCase {
-
-  void testAnnotatedAlias() {
-    doTest('''\
-import groovy.transform.*
-
-@AnnotationCollector([ToString, EqualsAndHashCode, Immutable])
-@interface Alias {}
-
-@Alias(excludes = ["a"])
-class F<caret>oo {
-    Integer a, b
-}
-''', '''\
-@ToString(excludes = ["a"])
-@EqualsAndHashCode(excludes = ["a"])
-@Immutable
-''')
+public class GrAnnotationCollectorTest extends LightGroovyTestCase {
+  public void testAnnotatedAlias() {
+    doTest("""
+             import groovy.transform.*
+             
+             @AnnotationCollector([ToString, EqualsAndHashCode, Immutable])
+             @interface Alias {}
+             
+             @Alias(excludes = ["a"])
+             class F<caret>oo {
+                 Integer a, b
+             }
+             """, """
+             @ToString(excludes = ["a"])
+             @EqualsAndHashCode(excludes = ["a"])
+             @Immutable
+             """);
   }
 
-  void testAliasWithProperties() {
-    doTest('''\
-import groovy.transform.*
-
-@AnnotationCollector([ToString, EqualsAndHashCode, Immutable])
-@interface Alias {}
-
-@Alias(excludes = ["a"])
-class F<caret>oo {
-    Integer a, b
-}
-''', '''\
-@ToString(excludes = ["a"])
-@EqualsAndHashCode(excludes = ["a"])
-@Immutable
-''')
+  public void testAliasWithProperties() {
+    doTest("""
+             import groovy.transform.*
+             
+             @AnnotationCollector([ToString, EqualsAndHashCode, Immutable])
+             @interface Alias {}
+             
+             @Alias(excludes = ["a"])
+             class F<caret>oo {
+                 Integer a, b
+             }
+             
+             """, """
+             @ToString(excludes = ["a"])
+             @EqualsAndHashCode(excludes = ["a"])
+             @Immutable
+             """);
   }
 
-  void testMixedProperties() {
-    doTest('''\
-import groovy.transform.*
-
-@ToString(excludes = ['a', 'b'])
-@AnnotationCollector([EqualsAndHashCode, Immutable])
-@interface Alias {}
-
-@Alias(excludes = ["a"])
-class F<caret>oo {
-    Integer a, b
-}
-''', '''\
-@EqualsAndHashCode(excludes = ["a"])
-@Immutable
-@ToString(excludes = ["a"])
-''')
+  public void testMixedProperties() {
+    doTest("""
+             import groovy.transform.*
+             
+             @ToString(excludes = ['a', 'b'])
+             @AnnotationCollector([EqualsAndHashCode, Immutable])
+             @interface Alias {}
+             
+             @Alias(excludes = ["a"])
+             class F<caret>oo {
+                 Integer a, b
+             }
+             """, """
+             @EqualsAndHashCode(excludes = ["a"])
+             @Immutable
+             @ToString(excludes = ["a"])
+             """);
   }
 
-  void testAliasDeclarationWithoutParams() {
-    doTest('''\
-@interface X {}
-@interface Y {}
-
-@X @Y
-@groovy.transform.AnnotationCollector
-@interface Alias {}
-
-@Alias
-class F<caret>oo {}
-''', '''\
-@X
-@Y
-''')
+  public void testAliasDeclarationWithoutParams() {
+    doTest("""
+             @interface X {}
+             @interface Y {}
+             
+             @X @Y
+             @groovy.transform.AnnotationCollector
+             @interface Alias {}
+             
+             @Alias
+             class F<caret>oo {}
+             """, """
+             @X
+             @Y
+             """);
   }
 
+  public void doTest(@NotNull String text, @NotNull String expectedAnnotations) {
+    addAnnotationCollector();
+    myFixture.configureByText("_a.groovy", text);
+    final PsiElement atCaret = myFixture.getFile().findElementAt(myFixture.getEditor().getCaretModel().getOffset());
+    final GrTypeDefinition clazz = PsiTreeUtil.getParentOfType(atCaret, GrTypeDefinition.class);
+    TestCase.assertNotNull(clazz);
 
-  void doTest(@NotNull String text, @NotNull String expectedAnnotations) {
-    addAnnotationCollector()
-    myFixture.configureByText('_a.groovy', text)
-    final atCaret = myFixture.file.findElementAt(myFixture.editor.caretModel.offset)
-    final clazz = PsiTreeUtil.getParentOfType(atCaret, GrTypeDefinition.class)
-    assertNotNull(clazz)
-
-    final actual = getActualAnnotations(clazz)
-
-    assertEquals(expectedAnnotations, actual)
+    final String actual = getActualAnnotations(clazz);
+    TestCase.assertEquals(expectedAnnotations, actual);
   }
 
   @NotNull
   @NonNls
-  static String getActualAnnotations(@NotNull GrTypeDefinition clazz) {
-    StringBuilder buffer = new StringBuilder()
-
-    for (GrAnnotation annotation : clazz.modifierList.annotations) {
-      buffer << '@' << annotation.shortName
-
-      final GrAnnotationNameValuePair[] attributes = annotation.parameterList.attributes
+  public static String getActualAnnotations(@NotNull GrTypeDefinition clazz) {
+    StringBuilder buffer = new StringBuilder();
+    for (GrAnnotation annotation : clazz.getModifierList().getAnnotations()) {
+      buffer.append("@").append(annotation.getShortName());
+      final GrAnnotationNameValuePair[] attributes = annotation.getParameterList().getAttributes();
       if (attributes.length > 0) {
-        buffer << '('
-
+        buffer.append("(");
         for (GrAnnotationNameValuePair pair : attributes) {
-          final name = pair.name
+          final String name = pair.getName();
           if (name != null) {
-            buffer << name << " = "
+            buffer.append(name).append(" = ");
           }
-
-          buffer << pair.value.text
+          buffer.append(pair.getValue().getText());
         }
-
-        buffer << ')'
+        buffer.append(")");
       }
-      buffer << '\n'
+      buffer.append("\n");
     }
-
-    return buffer.toString()
+    return buffer.toString();
   }
 
-  void 'test recursive alias'() {
-    fixture.configureByText '_.groovy', '''\
-@Alias
-@groovy.transform.AnnotationCollector
-@interface Alias {}
-
-@Alias
-class <caret>Usage {}
-'''
-    fixture.checkHighlighting()
-    assert getActualAnnotations(fixture.findClass("Usage") as GrTypeDefinition).isEmpty()
+  public void test_recursive_alias() {
+    getFixture().configureByText("_.groovy", """
+      @Alias
+      @groovy.transform.AnnotationCollector
+      @interface Alias {}
+      
+      @Alias
+      class <caret>Usage {}
+      """);
+    getFixture().checkHighlighting();
+    assertEmpty(getActualAnnotations((GrTypeDefinition)getFixture().findClass("Usage")));
   }
 }
