ccd1b810eb6347a23fb7929d806424a351bb7968
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-17 10:38:38 +0200

    [Groovy] convert GroovyDynamicMembersTest to Java (IDEA-327339)
    GitOrigin-RevId: 623bf94f06a0b7e654d3a387552b5091f3aa55bc

--- plugins/groovy/test/org/jetbrains/plugins/groovy/GroovyDynamicMembersTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/GroovyDynamicMembersTest.java
@@ -1,56 +1,52 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy;
 
-import com.intellij.psi.PsiField
-import com.intellij.psi.PsiMethod
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.annotator.intentions.dynamic.DynamicManager
-import org.jetbrains.plugins.groovy.annotator.intentions.dynamic.elements.DRootElement
-import org.jetbrains.plugins.groovy.annotator.intentions.dynamic.ui.DynamicElementSettings
-import org.jetbrains.plugins.groovy.util.GroovyLatestTest
-import org.jetbrains.plugins.groovy.util.ResolveTest
-import org.junit.After
-import org.junit.Before
-import org.junit.Test
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiMethod;
+import org.jetbrains.plugins.groovy.annotator.intentions.dynamic.DynamicManager;
+import org.jetbrains.plugins.groovy.annotator.intentions.dynamic.elements.DRootElement;
+import org.jetbrains.plugins.groovy.annotator.intentions.dynamic.ui.DynamicElementSettings;
+import org.jetbrains.plugins.groovy.util.GroovyLatestTest;
+import org.jetbrains.plugins.groovy.util.ResolveTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-import static com.intellij.psi.CommonClassNames.JAVA_LANG_STRING
+import java.util.ArrayList;
 
-@CompileStatic
-class GroovyDynamicMembersTest extends GroovyLatestTest implements ResolveTest {
+import static com.intellij.psi.CommonClassNames.JAVA_LANG_STRING;
 
+public class GroovyDynamicMembersTest extends GroovyLatestTest implements ResolveTest {
   @Before
-  void 'init state'() {
-    def methodDescriptor = new DynamicElementSettings().with {
-      containingClassName = JAVA_LANG_STRING
-      name = 'foo'
-      type = 'void'
-      method = true
-      params = [] // Collections.emptyList() // https://issues.apache.org/jira/browse/GROOVY-8961
-      it
-    }
-    def propertyDescriptor = new DynamicElementSettings().with {
-      containingClassName = JAVA_LANG_STRING
-      name = 'foo'
-      it
-    }
-    DynamicManager.getInstance(project).with {
-      addMethod(methodDescriptor)
-      addProperty(propertyDescriptor)
-    }
+  public void init_state() {
+    DynamicElementSettings methodDescriptor = new DynamicElementSettings();
+    methodDescriptor.setContainingClassName(JAVA_LANG_STRING);
+    methodDescriptor.setName("foo");
+    methodDescriptor.setType("void");
+    methodDescriptor.setMethod(true);
+    methodDescriptor.setParams(new ArrayList<>());// Collections.emptyList() // https://issues.apache.org/jira/browse/GROOVY-8961
+
+    final DynamicElementSettings propertyDescriptor = new DynamicElementSettings();
+    propertyDescriptor.setContainingClassName(JAVA_LANG_STRING);
+    propertyDescriptor.setName("foo");
+
+    DynamicManager manager = DynamicManager.getInstance(getProject());
+    manager.addMethod(methodDescriptor);
+    manager.addProperty(propertyDescriptor);
   }
 
   @After
-  void 'clear state'() {
-    DynamicManager.getInstance(project).loadState(new DRootElement())
+  public void clear_state() {
+    DynamicManager.getInstance(getProject()).loadState(new DRootElement());
   }
 
   @Test
-  void 'resolve to dynamic method'() {
-    resolveTest '"hi".<caret>foo()', PsiMethod
+  public void resolve_to_dynamic_method() {
+    resolveTest("\"hi\".<caret>foo()", PsiMethod.class);
   }
 
   @Test
-  void 'resolve to dynamic property'() {
-    resolveTest '"hi".<caret>foo', PsiField
+  public void resolve_to_dynamic_property() {
+    resolveTest("\"hi\".<caret>foo", PsiField.class);
   }
 }
