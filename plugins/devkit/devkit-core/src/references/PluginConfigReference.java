7df74448c13dfed03bcc93d5d4b99bc531029e9d
Yann Cébron <yann.cebron@jetbrains.com>
2024-10-17 12:03:55 +0200

    [devkit] UnresolvedPluginConfigReferenceInspection: handle PsiPolyVariantReferences (required for IJPL-163148)
    GitOrigin-RevId: 84a922bd909dc85f091b1b9dd2ce71292de5af64

--- plugins/devkit/devkit-core/src/references/PluginConfigReference.java
+++ plugins/devkit/devkit-core/src/references/PluginConfigReference.java
@@ -1,12 +1,17 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.references;
 
 import com.intellij.codeInsight.daemon.EmptyResolveMessageProvider;
 import com.intellij.codeInsight.highlighting.HighlightedReference;
+import com.intellij.psi.PsiPolyVariantReference;
 import org.jetbrains.idea.devkit.inspections.UnresolvedPluginConfigReferenceInspection;
 
 /**
  * Marker interface for resolve highlighting via {@link UnresolvedPluginConfigReferenceInspection}.
+ * <p>
+ * NOTE: While references may implement {@link PsiPolyVariantReference}, this should rarely be the case as
+ * they usually must have unique declarations.
+ * </p>
  */
 public interface PluginConfigReference extends HighlightedReference, EmptyResolveMessageProvider {
 }
