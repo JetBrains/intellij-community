93cb5fe79f91925a833374d368224cbf568c6e8b
Petr Surkov <petr.surkov@jetbrains.com>
2024-10-18 00:32:34 +0200

    [ai-completion] ML-4092 Log skipReason
    (cherry picked from commit cb84a9aac560cffb981597fe72c9385b71991340)
    
    IJ-CR-147240
    
    GitOrigin-RevId: 52ec609ee3f3d6ba3e2d692eba7f20c011859637

--- platform/platform-impl/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionLogs.kt
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionLogs.kt
@@ -14,7 +14,7 @@ import org.jetbrains.annotations.ApiStatus
 @ApiStatus.Internal
 object InlineCompletionLogs : CounterUsagesCollector() {
   // TODO use ML_RECORDER_ID
-  val GROUP = EventLogGroup("inline.completion.v2", 22, recorder = "ML")
+  val GROUP = EventLogGroup("inline.completion.v2", 23, recorder = "ML")
 
   override fun getGroup(): EventLogGroup = GROUP
 
