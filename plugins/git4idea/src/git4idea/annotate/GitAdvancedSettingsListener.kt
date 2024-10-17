91e0dcbc9648470e4d3eb8a588607d794b511e3c
Aleksey Pivovarov <AMPivovarov@gmail.com>
2024-10-17 12:56:20 +0200

    git: convert to Kotlin
    GitOrigin-RevId: ebf4e42a8fd2a6720fd5ede85055d8dc35e0fab0

--- plugins/git4idea/src/git4idea/annotate/GitAdvancedSettingsListener.kt
+++ plugins/git4idea/src/git4idea/annotate/GitAdvancedSettingsListener.kt
@@ -1,27 +1,25 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package git4idea.annotate;
+package git4idea.annotate
 
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.options.advanced.AdvancedSettingsChangeListener;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.ProjectLevelVcsManager;
-import git4idea.util.GitFileUtils;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.options.advanced.AdvancedSettingsChangeListener
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.ProjectLevelVcsManager
+import git4idea.util.GitFileUtils
 
+object GitAdvancedSettingsListener {
+  @JvmStatic
+  fun registerListener(project: Project, disposable: Disposable) {
+    val appBusConnection = ApplicationManager.getApplication().getMessageBus().connect(disposable)
+    appBusConnection.subscribe(AdvancedSettingsChangeListener.TOPIC, SettingsListener(project))
+  }
 
-public final class GitAdvancedSettingsListener {
-
-  public static void registerListener(@NotNull Project project, @NotNull Disposable disposable) {
-    ApplicationManager.getApplication().getMessageBus()
-      .connect(disposable)
-      .subscribe(AdvancedSettingsChangeListener.TOPIC, new AdvancedSettingsChangeListener() {
-        @Override
-        public void advancedSettingChanged(@NotNull String id, @NotNull Object oldValue, @NotNull Object newValue) {
-          if (id.equals(GitFileUtils.READ_CONTENT_WITH)) {
-            ProjectLevelVcsManager.getInstance(project).getContentRevisionCache().clearConstantCache();
-          }
-        }
-      });
+  private class SettingsListener(val project: Project) : AdvancedSettingsChangeListener {
+    override fun advancedSettingChanged(id: String, oldValue: Any, newValue: Any) {
+      if (id == GitFileUtils.READ_CONTENT_WITH) {
+        ProjectLevelVcsManager.getInstance(project).getContentRevisionCache().clearConstantCache()
+      }
+    }
   }
 }
--- plugins/git4idea/src/git4idea/annotate/GitAnnotationsListener.kt
+++ plugins/git4idea/src/git4idea/annotate/GitAnnotationsListener.kt
@@ -1,40 +1,45 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package git4idea.annotate;
+package git4idea.annotate
 
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.progress.util.BackgroundTaskUtil;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.vcs.changes.VcsAnnotationRefresher;
-import com.intellij.vcs.log.impl.CommonUiProperties;
-import com.intellij.vcs.log.impl.VcsLogApplicationSettings;
-import com.intellij.vcs.log.impl.VcsLogUiProperties;
-import git4idea.actions.GitToggleAnnotationOptionsActionProvider;
-import git4idea.repo.GitRepository;
-import git4idea.repo.GitRepositoryChangeListener;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.service
+import com.intellij.openapi.progress.util.BackgroundTaskUtil
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.vcs.changes.VcsAnnotationRefresher
+import com.intellij.vcs.log.impl.CommonUiProperties
+import com.intellij.vcs.log.impl.VcsLogApplicationSettings
+import com.intellij.vcs.log.impl.VcsLogUiProperties.PropertiesChangeListener
+import com.intellij.vcs.log.impl.VcsLogUiProperties.VcsLogUiProperty
+import git4idea.actions.GitToggleAnnotationOptionsActionProvider
+import git4idea.repo.GitRepository
+import git4idea.repo.GitRepositoryChangeListener
 
-public final class GitAnnotationsListener {
-  public static void registerListener(@NotNull Project project, @NotNull Disposable disposable) {
-    project.getMessageBus().connect(disposable).subscribe(GitRepository.GIT_REPO_CHANGE, new GitRepositoryChangeListener() {
-      @Override
-      public void repositoryChanged(@NotNull GitRepository repository) {
-        final VcsAnnotationRefresher refresher = BackgroundTaskUtil.syncPublisher(project, VcsAnnotationRefresher.LOCAL_CHANGES_CHANGED);
-        refresher.dirtyUnder(repository.getRoot());
+object GitAnnotationsListener {
+  @JvmStatic
+  fun registerListener(project: Project, disposable: Disposable) {
+    val busConnection = project.getMessageBus().connect(disposable)
+    busConnection.subscribe(GitRepository.GIT_REPO_CHANGE, RepoListener(project))
+
+    val logSettingsListener = LogSettingsListener(project)
+    val logSettings = ApplicationManager.getApplication().service<VcsLogApplicationSettings>()
+    logSettings.addChangeListener(logSettingsListener)
+    Disposer.register(disposable, Disposable { logSettings.removeChangeListener(logSettingsListener) })
+  }
+
+  private class RepoListener(val project: Project) : GitRepositoryChangeListener {
+    override fun repositoryChanged(repository: GitRepository) {
+      val refresher = BackgroundTaskUtil.syncPublisher<VcsAnnotationRefresher>(project, VcsAnnotationRefresher.LOCAL_CHANGES_CHANGED)
+      refresher.dirtyUnder(repository.getRoot())
+    }
+  }
+
+  private class LogSettingsListener(val project: Project) : PropertiesChangeListener {
+    override fun <T> onPropertyChanged(property: VcsLogUiProperty<T>) {
+      if (property.equals(CommonUiProperties.PREFER_COMMIT_DATE)) {
+        GitToggleAnnotationOptionsActionProvider.resetAllAnnotations(project, false)
       }
-    });
-
-    VcsLogApplicationSettings logSettings = ApplicationManager.getApplication().getService(VcsLogApplicationSettings.class);
-    VcsLogUiProperties.PropertiesChangeListener logSettingsListener = new VcsLogUiProperties.PropertiesChangeListener() {
-      @Override
-      public <T> void onPropertyChanged(VcsLogUiProperties.@NotNull VcsLogUiProperty<T> property) {
-        if (property.equals(CommonUiProperties.PREFER_COMMIT_DATE)) {
-          GitToggleAnnotationOptionsActionProvider.resetAllAnnotations(project, false);
-        }
-      }
-    };
-    logSettings.addChangeListener(logSettingsListener);
-    Disposer.register(disposable, () -> logSettings.removeChangeListener(logSettingsListener));
+    }
   }
 }
