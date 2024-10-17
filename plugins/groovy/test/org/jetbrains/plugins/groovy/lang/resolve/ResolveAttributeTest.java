2701f05f15cf7985d40102f32295509eb0a772a5
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-17 14:10:13 +0200

    [Groovy] convert ResolveAttributeTest to Java (IDEA-327339)
    GitOrigin-RevId: 92b3693b5c4d34ed998c9af9a33a39852befb07c

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/resolve/ResolveAttributeTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/resolve/ResolveAttributeTest.java
@@ -1,91 +1,92 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.lang.resolve
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.resolve;
 
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrField
-import org.jetbrains.plugins.groovy.util.GroovyLatestTest
-import org.jetbrains.plugins.groovy.util.ExpressionTest
-import org.junit.Before
-import org.junit.Test
+import org.jetbrains.plugins.groovy.lang.psi.api.GroovyResolveResult;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrField;
+import org.jetbrains.plugins.groovy.util.ExpressionTest;
+import org.jetbrains.plugins.groovy.util.GroovyLatestTest;
+import org.junit.Before;
+import org.junit.Test;
 
-@CompileStatic
-class ResolveAttributeTest extends GroovyLatestTest implements ExpressionTest {
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
 
+public class ResolveAttributeTest extends GroovyLatestTest implements ExpressionTest {
   @Before
-  void addClasses() {
-    fixture.addFileToProject 'A.groovy', '''
-class A {
-  static staticProp = 'static prop'
-  public static staticField = 'static field'
-  
-  def instanceProp = 'instance prop'
-  public instanceField = 'instance field'
-  
-  def getAccessorOnly() { 'accessor only' } 
-}
-'''
+  public void addClasses() {
+    getFixture().addFileToProject("A.groovy", """
+      class A {
+        static staticProp = 'static prop'
+        public static staticField = 'static field'
+      
+        def instanceProp = 'instance prop'
+        public instanceField = 'instance field'
+      
+        def getAccessorOnly() { 'accessor only' }
+      }
+      """);
   }
 
   @Test
-  void 'instance field in instance context'() {
-    resolveTest 'new A().@<caret>instanceField', GrField
+  public void instance_field_in_instance_context() {
+    resolveTest("new A().@<caret>instanceField", GrField.class);
   }
 
   @Test
-  void 'instance property in instance context'() {
-    def result = advancedResolveByText 'new A().@<caret>instanceProp'
-    assert result.element instanceof GrField
-    assert !result.accessible
-    assert !result.validResult
+  public void instance_property_in_instance_context() {
+    GroovyResolveResult result = advancedResolveByText("new A().@<caret>instanceProp");
+    assertTrue(result.getElement() instanceof GrField);
+    assertFalse(result.isAccessible());
+    assertFalse(result.isValidResult());
   }
 
   @Test
-  void 'dont resolve accessor'() {
-    resolveTest 'new A().@<caret>accessorOnly', null
+  public void dont_resolve_accessor() {
+    resolveTest("new A().@<caret>accessorOnly", null);
   }
 
   @Test
-  void 'static field in static context'() {
-    resolveTest 'A.@<caret>staticField', GrField
-    resolveTest 'A.class.@<caret>staticField', GrField
+  public void static_field_in_static_context() {
+    resolveTest("A.@<caret>staticField", GrField.class);
+    resolveTest("A.class.@<caret>staticField", GrField.class);
   }
 
   @Test
-  void 'static property in static context'() {
-    resolveTest 'A.@<caret>staticProp', GrField
-    resolveTest 'A.class.@<caret>staticProp', GrField
+  public void static_property_in_static_context() {
+    resolveTest("A.@<caret>staticProp", GrField.class);
+    resolveTest("A.class.@<caret>staticProp", GrField.class);
   }
 
   @Test
-  void 'static property in instance context'() {
-    resolveTest 'new A().@<caret>staticProp', GrField
+  public void static_property_in_instance_context() {
+    resolveTest("new A().@<caret>staticProp", GrField.class);
   }
 
   @Test
-  void 'instance property in static context'() {
-    def result = advancedResolveByText 'A.@<caret>instanceProp'
-    assert result.element instanceof GrField
-    assert !result.staticsOK
-    assert !result.validResult
+  public void instance_property_in_static_context() {
+    GroovyResolveResult result = advancedResolveByText("A.@<caret>instanceProp");
+    assertTrue(result.getElement() instanceof GrField);
+    assertFalse(result.isStaticsOK());
+    assertFalse(result.isValidResult());
   }
 
   @Test
-  void 'attribute in call'() {
-    resolveTest 'new A().@<caret>instanceField()', GrField
+  public void attribute_in_call() {
+    resolveTest("new A().@<caret>instanceField()", GrField.class);
   }
 
   @Test
-  void 'spread attribute'() {
-    referenceExpressionTest '[new A()]*.@instanceField', GrField, 'java.util.ArrayList<java.lang.String>'
+  public void spread_attribute() {
+    referenceExpressionTest("[new A()]*.@instanceField", GrField.class, "java.util.ArrayList<java.lang.String>");
   }
 
   @Test
-  void 'spread attribute deep'() {
-    referenceExpressionTest '[[new A()]]*.@instanceField', null, 'java.util.List'
+  public void spread_attribute_deep() {
+    referenceExpressionTest("[[new A()]]*.@instanceField", null, "java.util.List");
   }
 
   @Test
-  void 'spread static attribute'() {
-    referenceExpressionTest '[A]*.@staticField', GrField, 'java.util.ArrayList<java.lang.String>'
+  public void spread_static_attribute() {
+    referenceExpressionTest("[A]*.@staticField", GrField.class, "java.util.ArrayList<java.lang.String>");
   }
 }
