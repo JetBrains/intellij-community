1e3b1c11583fe804cd417a49beb9bd60744b352c
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-17 10:31:03 +0200

    [Groovy] convert GroovyTransformationsTest to Java (IDEA-327339)
    GitOrigin-RevId: b4a62e575fc7f3c6a7f156edf1cd2816be33895f

--- plugins/groovy/test/org/jetbrains/plugins/groovy/dsl/GroovyTransformationsTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/dsl/GroovyTransformationsTest.java
@@ -1,40 +1,43 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.dsl
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.dsl;
 
-import com.intellij.testFramework.LightProjectDescriptor
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
-import org.jetbrains.annotations.NotNull
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.util.TestUtils
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.util.TestUtils;
 
-class GroovyTransformationsTest extends LightJavaCodeInsightFixtureTestCase {
-
+public class GroovyTransformationsTest extends LightJavaCodeInsightFixtureTestCase {
   @Override
   protected String getBasePath() {
-    TestUtils.getTestDataPath() + "groovy/dsl/transform"
+    return TestUtils.getTestDataPath() + "groovy/dsl/transform";
   }
 
-  void doPlainTest(String type = "") throws Throwable {
-    myFixture.testCompletionTyping(getTestName(false) + ".groovy", type, getTestName(false) + "_after.groovy")
+  public void doPlainTest(String type) throws Throwable {
+    myFixture.testCompletionTyping(getTestName(false) + ".groovy", type, getTestName(false) + "_after.groovy");
   }
 
-  void doVariantsTest(String... variants) throws Throwable {
-    myFixture.testCompletionVariants(getTestName(false) + ".groovy", variants)
+  public void doPlainTest() throws Throwable {
+    doPlainTest("");
+  }
+
+  public void doVariantsTest(String... variants) throws Throwable {
+    myFixture.testCompletionVariants(getTestName(false) + ".groovy", variants);
   }
 
   @NotNull
   @Override
   protected LightProjectDescriptor getProjectDescriptor() {
-    return GroovyProjectDescriptors.GROOVY_1_7
+    return GroovyProjectDescriptors.GROOVY_1_7;
   }
 
-  void testDelegateAnnotation() throws Throwable { doPlainTest() }
+  public void testDelegateAnnotation() throws Throwable { doPlainTest(); }
 
-  void testCategoryTransform() throws Throwable { doVariantsTest('name', 'getName') }
+  public void testCategoryTransform() throws Throwable { doVariantsTest("name", "getName"); }
 
-  void testMixinTransform() throws Throwable { doPlainTest() }
+  public void testMixinTransform() throws Throwable { doPlainTest(); }
 
-  void testBindableTransform() throws Throwable { doPlainTest() }
+  public void testBindableTransform() throws Throwable { doPlainTest(); }
 
-  void testVetoableTransform() throws Throwable { doPlainTest() }
+  public void testVetoableTransform() throws Throwable { doPlainTest(); }
 }
