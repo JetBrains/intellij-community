d7ff5a412cceefdfe7d98e9720fa1891181dc954
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2024-10-17 13:04:26 +0200

    [Gradle|Execution] cleanup: removed the redundant GradleTaskResultListener extension point
    GitOrigin-RevId: 07914e85869a7b6822670c93730696179ddb0784

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/task/GradleTaskManager.java
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/task/GradleTaskManager.java
@@ -125,14 +125,16 @@ public class GradleTaskManager implements ExternalSystemTaskManager<GradleExecut
     }
   }
 
-  private void executeTasks(@NotNull ExternalSystemTaskId id,
-                            @NotNull List<String> tasks,
-                            @NotNull String projectPath,
-                            @NotNull GradleExecutionSettings settings,
-                            @Nullable String jvmParametersSetup,
-                            @NotNull ExternalSystemTaskNotificationListener listener,
-                            @NotNull ProjectConnection connection,
-                            @NotNull CancellationToken cancellationToken) {
+  private static void executeTasks(
+    @NotNull ExternalSystemTaskId id,
+    @NotNull List<String> tasks,
+    @NotNull String projectPath,
+    @NotNull GradleExecutionSettings settings,
+    @Nullable String jvmParametersSetup,
+    @NotNull ExternalSystemTaskNotificationListener listener,
+    @NotNull ProjectConnection connection,
+    @NotNull CancellationToken cancellationToken
+  ) {
     BuildEnvironment buildEnvironment = null;
     try {
       buildEnvironment = GradleExecutionHelper.getBuildEnvironment(connection, id, listener, cancellationToken, settings);
@@ -165,7 +167,6 @@ public class GradleTaskManager implements ExternalSystemTaskManager<GradleExecut
       else {
         ((TestLauncher)operation).run();
       }
-      GradleTaskResultListener.EP_NAME.forEachExtensionSafe(ext -> ext.onSuccess(id, projectPath));
     }
     catch (RuntimeException e) {
       LOG.debug("Gradle build launcher error", e);
