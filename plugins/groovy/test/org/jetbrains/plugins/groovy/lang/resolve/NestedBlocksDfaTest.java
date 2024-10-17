8638a7e181c50d1d55ba5394ea4f66d1be9da903
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-17 13:48:13 +0200

    [Groovy] convert NestedBlocksDfaTest to Java (IDEA-327339)
    GitOrigin-RevId: c5ec243956de158d221a04d75dafd1010d830133

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/resolve/NestedBlocksDfaTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/resolve/NestedBlocksDfaTest.java
@@ -1,186 +1,181 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.lang.resolve
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.groovy.lang.resolve;
 
-import com.intellij.openapi.util.RecursionManager
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.util.Groovy30Test
-import org.jetbrains.plugins.groovy.util.HighlightingTest
-import org.jetbrains.plugins.groovy.util.TypingTest
-import org.junit.Before
-import org.junit.Test
+import com.intellij.openapi.util.RecursionManager;
+import org.jetbrains.plugins.groovy.util.Groovy30Test;
+import org.jetbrains.plugins.groovy.util.HighlightingTest;
+import org.jetbrains.plugins.groovy.util.TypingTest;
+import org.junit.Before;
+import org.junit.Test;
 
-import static com.intellij.psi.CommonClassNames.JAVA_LANG_INTEGER
-import static com.intellij.psi.CommonClassNames.JAVA_LANG_STRING
+import static com.intellij.psi.CommonClassNames.JAVA_LANG_INTEGER;
+import static com.intellij.psi.CommonClassNames.JAVA_LANG_STRING;
 
-@CompileStatic
-class NestedBlocksDfaTest extends Groovy30Test implements TypingTest, HighlightingTest {
-
+public class NestedBlocksDfaTest extends Groovy30Test implements TypingTest, HighlightingTest {
   @Before
-  void disableRecursion() {
-    RecursionManager.assertOnRecursionPrevention(fixture.testRootDisposable)
+  public void disableRecursion() {
+    RecursionManager.assertOnRecursionPrevention(getFixture().getTestRootDisposable());
   }
 
   @Test
-  void 'outer reference'() {
-    typingTest '''
-def a = 1;
-{
-  <caret>a
-}
-''', JAVA_LANG_INTEGER
+  public void outer_reference() {
+    typingTest("""                 
+                 def a = 1;
+                 {
+                   <caret>a
+                 }
+                 """, JAVA_LANG_INTEGER);
   }
 
   @Test
-  void 'reassign in nested block'() {
-    typingTest '''
-def a = 1;
-{
-  a = 'str'
-}
-<caret>a
-''', JAVA_LANG_STRING
+  public void reassign_in_nested_block() {
+    typingTest("""                 
+                 def a = 1;
+                 {
+                   a = 'str'
+                 }
+                 <caret>a
+                 """, JAVA_LANG_STRING);
   }
 
   @Test
-  void 'reassign in double nested block'() {
-    typingTest '''
-def a = 1;
-{
-  {
-    a = 'str'
-  }
-}
-<caret>a
-''', JAVA_LANG_STRING
-  }
-
-  @Test
-  void 'defined in nested block'() {
-    typingTest '''
-{
-  def a = 'str'
-}
-<caret>a
-''', null
+  public void reassign_in_double_nested_block() {
+    typingTest("""                 
+                 def a = 1;
+                 {
+                   {
+                     a = 'str'
+                   }
+                 }
+                 <caret>a
+                 """, JAVA_LANG_STRING);
   }
 
   @Test
-  void 'defined in while block'() {
-    typingTest '''
-while ("".equals("sad")) {
-  def a = 'str'
-}
-<caret>a
-''', null
+  public void defined_in_nested_block() {
+    typingTest("""                 
+                 {
+                   def a = 'str'
+                 }
+                 <caret>a
+                 """, null);
   }
 
   @Test
-  void 'defined in try block'() {
-    typingTest '''
-try {
-    def a = 1
-} finally {
-    <caret>a
-}
-''', null
+  public void defined_in_while_block() {
+    typingTest("""                 
+                 while ("".equals("sad")) {
+                   def a = 'str'
+                 }
+                 <caret>a
+                 """, null);
   }
 
   @Test
-  void 'twice defined in nested blocks'() {
-    typingTest '''
-{
-    def a = 1
-} 
-
-{
-    def a = ""
-}
-<caret>a
-''', null
+  public void defined_in_try_block() {
+    typingTest("""                 
+                 try {
+                     def a = 1
+                 } finally {
+                     <caret>a
+                 }
+                 """, null);
   }
 
   @Test
-  void 'binding defined in nested blocks'() {
-    typingTest '''
-{
-    a = 1
-} 
-
-{
-    a = ""
-}
-<caret>a
-''', JAVA_LANG_STRING
+  public void twice_defined_in_nested_blocks() {
+    typingTest("""                 
+                 {
+                     def a = 1
+                 }
+                 
+                 {
+                     def a = ""
+                 }
+                 <caret>a
+                 """, null);
   }
 
   @Test
-  void 'binding and local var defined in nested blocks'() {
-    typingTest '''
-{
-    a = 1
-} 
-
-{
-   def a = ""
-}
-<caret>a
-''', JAVA_LANG_INTEGER
+  public void binding_defined_in_nested_blocks() {
+    typingTest("""                 
+                 {
+                     a = 1
+                 }
+                 
+                 {
+                     a = ""
+                 }
+                 <caret>a
+                 """, JAVA_LANG_STRING);
   }
 
   @Test
-  void 'filed and local var defined in nested block'() {
-    typingTest '''
-class A {
-    def a = "String"
-    def m() {
-        {
-            def a = 1
-        }
-        <caret>a
-
-    }
-}
-''', JAVA_LANG_STRING
+  public void binding_and_local_var_defined_in_nested_blocks() {
+    typingTest("""                 
+                 {
+                     a = 1
+                 }
+                 
+                 {
+                    def a = ""
+                 }
+                 <caret>a
+                 """, JAVA_LANG_INTEGER);
   }
 
   @Test
-  void 'filed and local var defined in nested block 2'() {
-    typingTest '''
-class A {
-    def a = "String"
-    def m() {
-        {
-            def a = 1
-            <caret>a
-        }
-    }
-}
-''', JAVA_LANG_INTEGER
+  public void filed_and_local_var_defined_in_nested_block() {
+    typingTest("""                 
+                 class A {
+                     def a = "String"
+                     def m() {
+                         {
+                             def a = 1
+                         }
+                         <caret>a
+                 
+                     }
+                 }
+                 """, JAVA_LANG_STRING);
   }
 
   @Test
-  void 'highlight local field initialized twice'() {
-    highlightingTest '''
-
-    def a = "String";
-    {
-        def <error descr="Variable 'a' already defined">a</error> = 1
-    }
-'''
+  public void filed_and_local_var_defined_in_nested_block_2() {
+    typingTest("""                 
+                 class A {
+                     def a = "String"
+                     def m() {
+                         {
+                             def a = 1
+                             <caret>a
+                         }
+                     }
+                 }
+                 """, JAVA_LANG_INTEGER);
   }
 
   @Test
-  void 'highlight local field in nested blocks'() {
-    highlightingTest '''
-{
-    def a = "String";
-}   
+  public void highlight_local_field_initialized_twice() {
+    highlightingTest("""                       
+                       
+                           def a = "String";
+                           {
+                               def <error descr="Variable 'a' already defined">a</error> = 1
+                           }
+                       """);
+  }
 
-{
-    def a = 1
-}
-'''
+  @Test
+  public void highlight_local_field_in_nested_blocks() {
+    highlightingTest("""                       
+                       {
+                           def a = "String";
+                       }
+                       
+                       {
+                           def a = 1
+                       }
+                       """);
   }
 }
-
-
