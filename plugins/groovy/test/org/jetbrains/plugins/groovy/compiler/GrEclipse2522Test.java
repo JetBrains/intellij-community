164371912a9b81b9143bc421738a93d86d576354
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2024-10-17 12:58:13 +0200

    [Groovy] convert GrEclipse2522Test to Java (IDEA-327339)
    GitOrigin-RevId: 2914d48012e61d3239639de99bc4e4f58aab65c0

--- plugins/groovy/test/org/jetbrains/plugins/groovy/compiler/GrEclipse2522Test.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/compiler/GrEclipse2522Test.java
@@ -1,20 +1,17 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.groovy.compiler
+package org.jetbrains.plugins.groovy.compiler;
 
-import groovy.transform.CompileStatic
-import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.TestLibrary
+import org.jetbrains.plugins.groovy.GroovyProjectDescriptors;
+import org.jetbrains.plugins.groovy.TestLibrary;
 
-@CompileStatic
-class GrEclipse2522Test extends GrEclipseTestBase {
-
+public class GrEclipse2522Test extends GrEclipseTestBase {
   @Override
   protected TestLibrary getGroovyLibrary() {
-    GroovyProjectDescriptors.LIB_GROOVY_2_5
+    return GroovyProjectDescriptors.LIB_GROOVY_2_5;
   }
 
   @Override
   protected String getGrEclipseArtifactID() {
-    "org.codehaus.groovy:groovy-eclipse-batch:2.5.22-01"
+    return "org.codehaus.groovy:groovy-eclipse-batch:2.5.22-01";
   }
 }
