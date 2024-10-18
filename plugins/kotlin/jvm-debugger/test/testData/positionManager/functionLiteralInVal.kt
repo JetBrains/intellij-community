55e9c76fc0970cf7888aebe385ae9f27d4db99d6
Alexander Udalov <alexander.udalov@jetbrains.com>
2024-10-18 22:05:20 +0200

    [kotlin] Update K2PositionManagerTestGenerated test data
    Somehow this test used parts of old JVM backend to resolve class names
    which have a certain line number in the bytecode. There have been
    numerous changes with regards to generation of lambdas and local
    classes/functions since the old JVM backend was deprecated. For example,
    local functions and lambdas no longer need a separate synthetic class in
    most cases.
    
     #KT-71197
    
    GitOrigin-RevId: e07b722d74436c15729727e9d41e81949a4b3c04

--- plugins/kotlin/jvm-debugger/test/testData/positionManager/functionLiteralInVal.kt
+++ plugins/kotlin/jvm-debugger/test/testData/positionManager/functionLiteralInVal.kt
@@ -2,7 +2,7 @@ class A {
     fun foo() {
         val a = {
             fun innerFoo() {
-                val b = 1   // A\$foo\$a\$1\$1
+                val b = 1   // A
             }
             innerFoo()
         }()
