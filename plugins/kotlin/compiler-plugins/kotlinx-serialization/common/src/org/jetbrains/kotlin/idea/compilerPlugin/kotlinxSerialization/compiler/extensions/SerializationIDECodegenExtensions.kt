a2ec8d9d8af19a862d0867edebddce1ff4ebf995
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-10-18 12:05:51 +0200

    [JS] Drop support for JS legacy backend in kotlinx-serialization compiler plugin
    KT-70223
    
    GitOrigin-RevId: fed4f36d74db3b8fca940669fb86964777c976dd

--- plugins/kotlin/compiler-plugins/kotlinx-serialization/common/src/org/jetbrains/kotlin/idea/compilerPlugin/kotlinxSerialization/compiler/extensions/SerializationIDECodegenExtensions.kt
+++ plugins/kotlin/compiler-plugins/kotlinx-serialization/common/src/org/jetbrains/kotlin/idea/compilerPlugin/kotlinxSerialization/compiler/extensions/SerializationIDECodegenExtensions.kt
@@ -3,27 +3,10 @@
 package org.jetbrains.kotlin.idea.compilerPlugin.kotlinxSerialization.compiler.extensions
 
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
-import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.idea.compilerPlugin.kotlinxSerialization.runIfEnabledIn
-import org.jetbrains.kotlin.idea.compilerPlugin.kotlinxSerialization.runIfEnabledOn
 import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
-import org.jetbrains.kotlin.js.translate.context.TranslationContext
-import org.jetbrains.kotlin.js.translate.declaration.DeclarationBodyVisitor
-import org.jetbrains.kotlin.psi.KtPureClassOrObject
-import org.jetbrains.kotlinx.serialization.compiler.extensions.SerializationJsExtension
 import org.jetbrains.kotlinx.serialization.compiler.extensions.SerializationLoweringExtension
-
-class SerializationIDEJsExtension : SerializationJsExtension() {
-    override fun generateClassSyntheticParts(
-        declaration: KtPureClassOrObject,
-        descriptor: ClassDescriptor,
-        translator: DeclarationBodyVisitor,
-        context: TranslationContext
-    ) = runIfEnabledOn(descriptor) {
-        super.generateClassSyntheticParts(declaration, descriptor, translator, context)
-    }
-}
 
 class SerializationIDEIrExtension : SerializationLoweringExtension() {
     override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
