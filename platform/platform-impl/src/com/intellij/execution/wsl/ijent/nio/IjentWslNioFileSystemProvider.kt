cf72336a4e3cec3a5b4df76eeb3e9ae247079228
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2024-10-21 19:49:53 +0200

    [ijent] IJPL-164677: Cleanup
    (cherry picked from commit 180a8e9573b6b9ce5c4303c10500bd6b2544ca2a)
    
    IJ-CR-147062
    
    GitOrigin-RevId: b8ad4ff68c513e4504ccbde413601e65a54d83b2

--- platform/platform-impl/src/com/intellij/execution/wsl/ijent/nio/IjentWslNioFileSystemProvider.kt
+++ platform/platform-impl/src/com/intellij/execution/wsl/ijent/nio/IjentWslNioFileSystemProvider.kt
@@ -262,7 +262,7 @@ class IjentWslNioFileSystemProvider(
     if (conversionResult2 != null) {
       return conversionResult2
     }
-    // so both paths are now
+    // so both paths are now located in WSL
     return ijentFsProvider.isSameFile(path.toIjentPath(), path2.toIjentPath())
   }
 
