a7772128b7c4c3358a4603e9d343823efd8a89af
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2024-10-21 17:59:41 +0200

    [inline-completion] IJPL-161906: fix `ApiCheckTest`
    (cherry picked from commit a4315003b7e57e9d52320190ddfabe2025541ca0)
    
    IJ-MR-147762
    
    GitOrigin-RevId: 083f8bfb58380aa196ab7d6462f6131a3d94aebb

--- platform/inline-completion/frontend/src/FrontendInlineCompletionHandler.kt
+++ platform/inline-completion/frontend/src/FrontendInlineCompletionHandler.kt
@@ -10,7 +10,7 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.Editor
 import kotlinx.coroutines.CoroutineScope
 
-class FrontendInlineCompletionHandler(
+internal class FrontendInlineCompletionHandler(
   scope: CoroutineScope,
   editor: Editor,
   parentDisposable: Disposable
