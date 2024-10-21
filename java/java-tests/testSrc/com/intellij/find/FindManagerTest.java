20553eb629ee3216304fb562240d20269151f490
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-10-21 17:01:57 +0200

    [search] fix test
    GitOrigin-RevId: 0e58a311f990d65db4e672139850b09e9ffb0b7a

--- java/java-tests/testSrc/com/intellij/find/FindManagerTest.java
+++ java/java-tests/testSrc/com/intellij/find/FindManagerTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.find;
 
 import com.intellij.JavaTestUtil;
@@ -1102,7 +1102,7 @@ public class FindManagerTest extends DaemonAnalyzerTestCase {
     model.setProjectScope(false);
     model.setCustomScopeName("initialScopeName");
 
-    checkContext(model, myProject, directory, module, false, dirName, moduleName, false);
+    checkContext(model, myProject, directory, module, false, dirName, moduleName, true);
     checkContext(model, myProject, directory, null, false, dirName, moduleName, false);//prev directory state
     checkContext(model, myProject, null, null, false, dirName, moduleName, false);//prev module and dir state
     model.setCustomScope(scope);
