0ad7df328926d304a27b65209a457acd89e41b97
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-10-22 17:16:16 +0200

    [editor] IJPL-164140 Floating code toolbar is not configurable for JSON, XML, YAML and SQL files
    (cherry picked from commit 9d3a74bf1433284ed0fb7e6e2b554926966110e7)
    IJ-CR-148640
    
    GitOrigin-RevId: 3c215330677d9e7730aaa04fbdd9fcffdbf3d7ba

--- platform/platform-impl/src/com/intellij/ui/codeFloatingToolbar/FloatingToolbarCustomizer.kt
+++ platform/platform-impl/src/com/intellij/ui/codeFloatingToolbar/FloatingToolbarCustomizer.kt
@@ -4,12 +4,79 @@ package com.intellij.ui.codeFloatingToolbar
 import com.intellij.lang.IdeLanguageCustomization
 import com.intellij.lang.Language
 import com.intellij.lang.LanguageExtension
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.extensions.PluginAware
+import com.intellij.openapi.extensions.PluginDescriptor
+import com.intellij.openapi.extensions.RequiredElement
+import com.intellij.util.xmlb.annotations.Attribute
 import org.jetbrains.annotations.ApiStatus
 
-private val EP = LanguageExtension<FloatingToolbarCustomizer>("com.intellij.lang.floatingToolbarCustomizer")
+@Deprecated("Use lang.floatingToolbar instead")
+private val DEPRECATED_EP = LanguageExtension<FloatingToolbarCustomizer>("com.intellij.lang.floatingToolbarCustomizer")
+private val EP: ExtensionPointName<FloatingToolbarLanguageBean> = ExtensionPointName.create("com.intellij.lang.floatingToolbar")
+
+private const val FLOATING_CODE_TOOLBAR_GROUP_ID = "Floating.CodeToolbar"
+
+private fun forLanguage(language: Language): FloatingToolbarLanguageBean? {
+  return EP.extensionList.firstOrNull { bean -> bean.language == language.id }
+}
 
 internal fun findActionGroupFor(language: Language): String? {
-  return EP.allForLanguage(language).firstNotNullOfOrNull { it.getActionGroup() }
+  val bean = forLanguage(language)
+  if (bean != null) {
+    if (bean.isMinimal) {
+      // check if any of the primary languages have full toolbar available
+      val hasPrimaryToolbar = IdeLanguageCustomization.getInstance().primaryIdeLanguages.any {
+        val bean = forLanguage(it)
+        bean != null && !bean.isMinimal
+      }
+
+      if (!hasPrimaryToolbar) return null
+    }
+
+    val customization = bean.getCustomization()
+    if (customization != null && !customization.isToolbarAvailable()) {
+      return null
+    }
+
+    return FLOATING_CODE_TOOLBAR_GROUP_ID
+  }
+
+  return DEPRECATED_EP.allForLanguage(language)
+    .firstNotNullOfOrNull { it.getActionGroup() }
+}
+
+internal fun hasMinimalFloatingToolbar(language: Language): Boolean {
+  return forLanguage(language)?.isMinimal == true
+}
+
+@ApiStatus.Experimental
+interface FloatingToolbarCustomization {
+  fun isToolbarAvailable(): Boolean
+}
+
+internal class FloatingToolbarLanguageBean : PluginAware {
+  private var pluginDescriptor: PluginDescriptor? = null
+
+  override fun setPluginDescriptor(pluginDescriptor: PluginDescriptor) {
+    this.pluginDescriptor = pluginDescriptor
+  }
+
+  @Attribute("language")
+  @RequiredElement
+  var language: String? = null
+
+  @Attribute("minimal")
+  var isMinimal: Boolean = false
+
+  @Attribute("customizationClass")
+  var customizationClass: String? = null
+
+  fun getCustomization(): FloatingToolbarCustomization? {
+    if (customizationClass == null) return null
+    return ApplicationManager.getApplication().instantiateClass(customizationClass!!, pluginDescriptor!!)
+  }
 }
 
 /**
@@ -17,9 +84,9 @@ internal fun findActionGroupFor(language: Language): String? {
  *
  * @see CodeFloatingToolbar
  * @see [FloatingToolbarCustomizer.DefaultGroup]
- * @see [FloatingToolbarCustomizer.MinimalGroup]
  */
 @ApiStatus.Experimental
+@Deprecated("Use lang.floatingToolbar instead")
 interface FloatingToolbarCustomizer {
   /**
    * @return id of the action group to be shown in the toolbar, or null if the toolbar shouldn't be available
@@ -27,25 +94,8 @@ interface FloatingToolbarCustomizer {
   fun getActionGroup(): String?
 
   @ApiStatus.Experimental
+  @Deprecated("Use lang.floatingToolbar instead")
   class DefaultGroup : FloatingToolbarCustomizer {
-    override fun getActionGroup(): String {
-      return "Floating.CodeToolbar"
-    }
-  }
-
-  /**
-   * Actions for languages that do not support refactoring capabilities such as Extract and Surround, e.g., JSON and XML.
-   */
-  @ApiStatus.Experimental
-  open class MinimalGroup : FloatingToolbarCustomizer {
-    override fun getActionGroup(): String? {
-      val hasPrimaryToolbar = IdeLanguageCustomization.getInstance().primaryIdeLanguages
-        .any { EP.allForLanguage(it).isNotEmpty() }
-
-      // primary language does not support floating toolbar, ignore in this IDE
-      if (!hasPrimaryToolbar) return null
-
-      return "Floating.CodeToolbar.Minimal"
-    }
+    override fun getActionGroup(): String = FLOATING_CODE_TOOLBAR_GROUP_ID
   }
 }
