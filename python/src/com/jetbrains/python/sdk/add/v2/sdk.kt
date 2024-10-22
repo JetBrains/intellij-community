f084632921192b5f1e3bdaab9001b884810df1c3
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2024-10-23 01:24:10 +0200

    Python: Function extracted from presenter to the library to be reused
    GitOrigin-RevId: 37ead17ec2677676f4d09e3548493cb089dba65d

--- python/src/com/jetbrains/python/sdk/add/v2/sdk.kt
+++ python/src/com/jetbrains/python/sdk/add/v2/sdk.kt
@@ -4,7 +4,39 @@ package com.jetbrains.python.sdk.add.v2
 import com.intellij.openapi.application.writeAction
 import com.intellij.openapi.projectRoots.ProjectJdkTable
 import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.projectRoots.impl.SdkConfigurationUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.python.sdk.PythonSdkType
+import com.jetbrains.python.sdk.suggestAssociatedSdkName
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.nio.file.Path
 
+/**
+ * Creates and persists sdk.
+ * [projectPath] is used to suggest name
+ */
+suspend fun createSdk(
+  pythonBinaryPath: VirtualFile,
+  projectPath: Path,
+  existingSdks: Array<Sdk>,
+): Sdk {
+  val newSdk = withContext(Dispatchers.IO) {
+    // "suggest name" calls external process and can't be called from EDT
+    val suggestedName = /*suggestedSdkName ?:*/ suggestAssociatedSdkName(pythonBinaryPath.path, projectPath.toString())
+    SdkConfigurationUtil.setupSdk(existingSdks, pythonBinaryPath,
+                                  PythonSdkType.getInstance(),
+                                  null, suggestedName)
+  }
+
+  addSdk(newSdk)
+  return newSdk
+}
+
+
+/**
+ * Persists [sdk]
+ */
 internal suspend fun addSdk(sdk: Sdk) {
   writeAction {
     ProjectJdkTable.getInstance().addJdk(sdk)
