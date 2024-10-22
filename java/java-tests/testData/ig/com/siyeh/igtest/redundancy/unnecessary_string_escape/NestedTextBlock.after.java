a00802204adee238ef88e24b1486e173fba91922
Bas Leijdekkers <basleijdekkers@gmail.com>
2024-10-22 18:12:37 +0200

    Java: add more test cases for "Unnecessarily escaped character" inspection
    GitOrigin-RevId: 9c4f0f03acb071adb37fe7949e809ba4a6d36096

--- java/java-tests/testData/ig/com/siyeh/igtest/redundancy/unnecessary_string_escape/NestedTextBlock.after.java
+++ java/java-tests/testData/ig/com/siyeh/igtest/redundancy/unnecessary_string_escape/NestedTextBlock.after.java
@@ -3,5 +3,8 @@ class NestedTextBlock {
     String s = \"<caret>""
         test
     \""";
+        // \"""
+        // "\""
+        // \"""
 """;
 }
--- java/java-tests/testData/ig/com/siyeh/igtest/redundancy/unnecessary_string_escape/NestedTextBlock.java
+++ java/java-tests/testData/ig/com/siyeh/igtest/redundancy/unnecessary_string_escape/NestedTextBlock.java
@@ -3,5 +3,8 @@ class NestedTextBlock {
     String s = \"<warning descr="'\\\\"' is unnecessarily escaped"><caret>\"</warning><warning descr="'\\\\"' is unnecessarily escaped">\"</warning>
         test
     \"<warning descr="'\\\\"' is unnecessarily escaped">\"</warning><warning descr="'\\\\"' is unnecessarily escaped">\"</warning>;
+        // \""<warning descr="'\\\\"' is unnecessarily escaped">\"</warning>
+        // "\"<warning descr="'\\\\"' is unnecessarily escaped">\"</warning>
+        // \"<warning descr="'\\\\"' is unnecessarily escaped">\"</warning>"
 """;
 }
