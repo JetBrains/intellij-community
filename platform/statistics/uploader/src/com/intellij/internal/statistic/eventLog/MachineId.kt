6411b9ecf14f382ef0a517dd7b2091b70ea0400c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-10-22 20:19:44 +0200

    Cleanup (formatting)
    GitOrigin-RevId: d1ffc45c340d9fcb4e41d2b34e7a28287c07ee62

--- platform/statistics/uploader/src/com/intellij/internal/statistic/eventLog/MachineId.kt
+++ platform/statistics/uploader/src/com/intellij/internal/statistic/eventLog/MachineId.kt
@@ -1,14 +1,11 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.internal.statistic.eventLog
 
 import com.intellij.internal.statistic.config.EventLogOptions
 
 data class MachineId(val id: String, val revision: Int) {
   companion object {
-    @JvmField
-    val DISABLED = MachineId(EventLogOptions.MACHINE_ID_DISABLED, EventLogOptions.DEFAULT_ID_REVISION)
-
-    @JvmField
-    val UNKNOWN = MachineId(EventLogOptions.MACHINE_ID_UNKNOWN, EventLogOptions.DEFAULT_ID_REVISION)
+    @JvmField val DISABLED: MachineId = MachineId(EventLogOptions.MACHINE_ID_DISABLED, EventLogOptions.DEFAULT_ID_REVISION)
+    @JvmField val UNKNOWN: MachineId = MachineId(EventLogOptions.MACHINE_ID_UNKNOWN, EventLogOptions.DEFAULT_ID_REVISION)
   }
 }
