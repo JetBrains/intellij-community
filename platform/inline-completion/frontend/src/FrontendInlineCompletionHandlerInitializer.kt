93fb9b76457a402900505029fc98ee968aeee0a4
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2024-10-22 15:17:38 +0200

    [inline-completion] IJPL-161906: check locality of a client instead of `AppMode`
    GitOrigin-RevId: 8c264f6933b5801073ea45e42c2595a67d938c12

--- platform/inline-completion/frontend/src/FrontendInlineCompletionHandlerInitializer.kt
+++ platform/inline-completion/frontend/src/FrontendInlineCompletionHandlerInitializer.kt
@@ -4,14 +4,15 @@ package com.intellij.codeInsight.inline.completion.frontend
 import com.intellij.codeInsight.inline.completion.InlineCompletionHandler
 import com.intellij.codeInsight.inline.completion.InlineCompletionHandlerInitializer
 import com.intellij.codeInsight.inline.completion.InlineCompletionRemDevUtils
-import com.intellij.idea.AppMode
+import com.intellij.codeWithMe.ClientId
+import com.intellij.codeWithMe.ClientId.Companion.isLocal
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.Editor
 import kotlinx.coroutines.CoroutineScope
 
 internal class FrontendInlineCompletionHandlerInitializer : InlineCompletionHandlerInitializer {
   override fun initialize(editor: Editor, scope: CoroutineScope, disposable: Disposable): InlineCompletionHandler? {
-    if (AppMode.isRemoteDevHost() && InlineCompletionRemDevUtils.isRhizomeUsed()) {
+    if (!ClientId.currentOrNull.isLocal && InlineCompletionRemDevUtils.isRhizomeUsed()) {
       return null
     }
     return FrontendInlineCompletionHandler(scope, editor, disposable)
