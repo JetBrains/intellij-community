a399d2c6ad5f1f9b694ff868d6196c2b63022b9b
Nikita Katkov <nikita.katkov@jetbrains.com>
2024-10-22 16:31:10 +0200

    [yaml] IJ-CR-147338 Add documentation to the recently introduced extension point
    GitOrigin-RevId: e524f4c59adab9ef6877df039d53ad57cd5cc2a8

--- plugins/yaml/editing/src/org/jetbrains/yaml/psi/YamlTagRecogniser.kt
+++ plugins/yaml/editing/src/org/jetbrains/yaml/psi/YamlTagRecogniser.kt
@@ -2,12 +2,23 @@ package org.jetbrains.yaml.psi
 
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.util.NlsSafe
+import org.jetbrains.annotations.ApiStatus
 
+/**
+ * Interface for recognising known domain-specific YAML tags.
+ *
+ * This interface allows implementations to inform the IDE about YAML tags that are expected in a particular YAML-based framework.
+ * If a tag is recognised by any implementation, no warnings will be reported in the editor.
+ */
+@ApiStatus.Experimental
 interface YamlTagRecogniser {
   companion object {
     @JvmField
     val EP_NAME = ExtensionPointName.create<YamlTagRecogniser>("com.intellij.yaml.tagRecogniser")
   }
 
+  /**
+   * Checks if the provided tag text is recognised as a known domain-specific YAML tag.
+   */
   fun isRecognizedTag(tagText: @NlsSafe String): Boolean
 }
