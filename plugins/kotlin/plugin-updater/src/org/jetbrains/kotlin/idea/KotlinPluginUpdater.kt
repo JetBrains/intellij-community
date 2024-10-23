50049f3e377a7605b107524d7b86733ad243129d
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2024-10-23 12:31:37 +0200

    [kotlin] cleanup, remove the unused class `org.jetbrains.kotlin.idea.KotlinPluginReleaseDateProvider`
    GitOrigin-RevId: f2703bfa2c3af7f495477e14fdc1b8fd80dfe16d

--- plugins/kotlin/plugin-updater/src/org/jetbrains/kotlin/idea/KotlinPluginUpdater.kt
+++ plugins/kotlin/plugin-updater/src/org/jetbrains/kotlin/idea/KotlinPluginUpdater.kt
@@ -2,21 +2,11 @@
 
 package org.jetbrains.kotlin.idea
 
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonIOException
-import com.google.gson.JsonSyntaxException
 import com.intellij.ide.plugins.*
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
-import com.intellij.openapi.extensions.PluginId
-import com.intellij.util.io.HttpRequests
 import org.jetbrains.kotlin.idea.compiler.configuration.KotlinIdePlugin
 import org.jetbrains.kotlin.idea.update.verify
-import java.io.IOException
-import java.time.DateTimeException
-import java.time.Instant
-import java.time.LocalDate
-import java.time.ZoneOffset
 
 
 @Service
@@ -43,48 +33,3 @@ class KotlinPluginUpdater : StandalonePluginUpdateChecker(
     }
 }
 
-object KotlinPluginReleaseDateProvider {
-    class ResponseParseException(message: String, cause: Exception? = null) : IllegalStateException(message, cause)
-
-    @Suppress("SpellCheckingInspection")
-    private class PluginDTO {
-        var cdate: String? = null
-        var channel: String? = null
-
-        // `true` if the version is seen in plugin site and available for download.
-        // Maybe be `false` if author requested version deletion.
-        var listed: Boolean = true
-
-        // `true` if version is approved and verified
-        var approve: Boolean = true
-    }
-
-    @Throws(IOException::class, ResponseParseException::class)
-    fun fetchPluginReleaseDate(pluginId: PluginId, version: String, channel: String?): LocalDate? {
-        val url = "https://plugins.jetbrains.com/api/plugins/${pluginId.idString}/updates?version=$version"
-
-        val pluginDTOs: Array<PluginDTO> = try {
-            HttpRequests.request(url).connect {
-                GsonBuilder().create().fromJson(it.inputStream.reader(), Array<PluginDTO>::class.java)
-            }
-        } catch (ioException: JsonIOException) {
-            throw IOException(ioException)
-        } catch (syntaxException: JsonSyntaxException) {
-            throw ResponseParseException("Can't parse json response", syntaxException)
-        }
-
-        val selectedPluginDTO = pluginDTOs.firstOrNull {
-            it.listed && it.approve && (it.channel == channel || (it.channel == "" && channel == null))
-        } ?: return null
-
-        val dateString = selectedPluginDTO.cdate ?: throw ResponseParseException("Empty cdate")
-        return try {
-            val dateLong = dateString.toLong()
-            Instant.ofEpochMilli(dateLong).atZone(ZoneOffset.UTC).toLocalDate()
-        } catch (e: NumberFormatException) {
-            throw ResponseParseException("Can't parse long date", e)
-        } catch (e: DateTimeException) {
-            throw ResponseParseException("Can't convert to date", e)
-        }
-    }
-}
