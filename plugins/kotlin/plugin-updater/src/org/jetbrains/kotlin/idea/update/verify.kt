36455ea68e6e4f721fd81980d9a1e0afb26182dc
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2024-10-23 13:49:47 +0200

    [kotlin] cleanup, remove non-existing registry key check
    The registry key is not defined neither in IntelliJ nor in Android Studio
    
    GitOrigin-RevId: 56ebcbe1445089cc686560f27e8583f8cbf6cca8

--- plugins/kotlin/plugin-updater/src/org/jetbrains/kotlin/idea/update/verify.kt
+++ plugins/kotlin/plugin-updater/src/org/jetbrains/kotlin/idea/update/verify.kt
@@ -4,17 +4,11 @@ package org.jetbrains.kotlin.idea.update
 
 import com.intellij.ide.plugins.IdeaPluginDescriptor
 import com.intellij.ide.plugins.PluginUpdateStatus
-import com.intellij.openapi.util.registry.Registry
 import org.jetbrains.kotlin.idea.base.util.KotlinPlatformUtils
 
 // Do an additional verification with PluginUpdateVerifier. Enabled only in AS 3.2+
 fun verify(updateStatus: PluginUpdateStatus.Update): PluginUpdateStatus {
     if (!KotlinPlatformUtils.isAndroidStudio) {
-        return updateStatus
-    }
-    @Suppress("UnresolvedPluginConfigReference")
-    val pluginVerifierEnabled = Registry.`is`("kotlin.plugin.update.verifier.enabled", true)
-    if (!pluginVerifierEnabled) {
         return updateStatus
     }
 
