3533a8c463917f0ce2a8a39cc4d294000c9bd272
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2024-10-23 15:06:23 +0200

    [opentelemetry][gradle][IDEA-360301] use a platform-provided way to collect telemetry from Gradle daemon
    GitOrigin-RevId: 39edffa2db5bcc8cfc66189f83530ee07c5bba36

---
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/execution/telemetry/GradleTelemetryAgentProvidingExecutionHelperExtension.kt
@@ -0,0 +1,49 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.gradle.service.execution.telemetry
+
+import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.platform.diagnostic.telemetry.OtlpConfiguration
+import com.intellij.platform.diagnostic.telemetry.impl.agent.AgentConfiguration
+import com.intellij.platform.diagnostic.telemetry.impl.agent.AgentConfiguration.Settings
+import com.intellij.platform.diagnostic.telemetry.impl.agent.TelemetryAgentProvider
+import com.intellij.platform.diagnostic.telemetry.impl.agent.TelemetryAgentResolver
+import com.intellij.platform.diagnostic.telemetry.rt.context.TelemetryContext
+import org.gradle.tooling.LongRunningOperation
+import org.gradle.tooling.model.build.BuildEnvironment
+import org.jetbrains.plugins.gradle.service.project.GradleExecutionHelperExtension
+import org.jetbrains.plugins.gradle.settings.GradleExecutionSettings
+import org.jetbrains.plugins.gradle.util.GradleConstants
+import org.jetbrains.plugins.gradle.util.telemetry.GradleDaemonOpenTelemetryUtil
+import kotlin.time.Duration.Companion.seconds
+
+class GradleTelemetryAgentProvidingExecutionHelperExtension : GradleExecutionHelperExtension {
+
+  override fun prepareForExecution(
+    id: ExternalSystemTaskId,
+    operation: LongRunningOperation,
+    settings: GradleExecutionSettings,
+    buildEnvironment: BuildEnvironment?,
+  ) {
+    if (!GradleDaemonOpenTelemetryUtil.isDaemonTracingEnabled(settings)) {
+      return
+    }
+    if (!Registry.`is`("gradle.daemon.opentelemetry.agent.enabled", false)) {
+      return
+    }
+    val traceEndpoint = OtlpConfiguration.getTraceEndpoint() ?: return
+    val agentLocation = TelemetryAgentResolver.getAgentLocation() ?: return
+    val configuration = AgentConfiguration.forService(
+      serviceName = GradleConstants.GRADLE_NAME,
+      context = TelemetryContext.current(),
+      traceEndpoint = traceEndpoint,
+      agentLocation = agentLocation,
+      settings = Settings.default(
+        debug = true,
+        metricExportInterval = 5.seconds
+      )
+    )
+    val jvmArgs = TelemetryAgentProvider.getJvmArgs(configuration)
+    operation.addJvmArguments(jvmArgs)
+  }
+}
