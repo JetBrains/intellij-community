32a6d7bcc3c042eae02918ce4c25fdc4cfdf9383
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2024-10-24 19:25:06 +0200

    [inline-completion] IJPL-161906: add docs for `InlineCompletionFilteringEventListener`
    GitOrigin-RevId: 7e9a9d3ab514d9cdaf3fe528a29f13e7cbb17ab6

--- platform/platform-impl/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionFilteringEventListener.kt
+++ platform/platform-impl/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionFilteringEventListener.kt
@@ -5,13 +5,18 @@ import com.intellij.codeInsight.inline.completion.InlineCompletionEventAdapter
 import com.intellij.codeInsight.inline.completion.InlineCompletionEventType
 import java.util.concurrent.atomic.AtomicBoolean
 
+/**
+ * If a request is applicable corresponding to [isApplicable], then everything works as usual.
+ *
+ * If a request is not applicable, the listener doesn't call any methods on all the events connected to the request.
+ */
 internal abstract class InlineCompletionFilteringEventListener : InlineCompletionEventAdapter {
 
   private val isApplicable = AtomicBoolean(true)
 
   protected abstract fun isApplicable(requestEvent: InlineCompletionEventType.Request): Boolean
 
-  override fun on(event: InlineCompletionEventType) {
+  final override fun on(event: InlineCompletionEventType) {
     if (event is InlineCompletionEventType.Request) {
       isApplicable.set(isApplicable(event))
     }
