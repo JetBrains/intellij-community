19a27ca75b83844e9c44f48c334d7bfd9bb77389
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-10-24 18:00:05 +0200

    IJPL-165034 Freeze for several minutes on opening a big text file
    Use optimized check on isThreadDump. Avoid normalization which is very memory hungry and instead rely on the fact that there should be thread states.
    
    GitOrigin-RevId: 28ca3bd4dbb99dea198576d2356711416f04bac2

--- plugins/devkit/intellij.devkit.stacktrace/src/org/jetbrains/idea/devkit/stacktrace/editor/StackTraceFileEditorProvider.kt
+++ plugins/devkit/intellij.devkit.stacktrace/src/org/jetbrains/idea/devkit/stacktrace/editor/StackTraceFileEditorProvider.kt
@@ -7,14 +7,14 @@ import com.intellij.openapi.fileEditor.WeighedFileEditorProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VfsUtilCore.loadText
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.unscramble.UnscrambleUtils
+import java.util.regex.Pattern
 
 /**
  * Java stacktrace weighed file editor provider.
  */
 class StackTraceFileEditorProvider : WeighedFileEditorProvider() {
   override fun accept(project: Project, file: VirtualFile): Boolean {
-    return file.extension == "txt" && file.length < 10_000 && UnscrambleUtils.isStackTrace(loadText(file))
+    return file.extension == "txt" && isThreadDump(loadText(file))
   }
 
   override fun createEditor(project: Project, file: VirtualFile): FileEditor = StackTraceFileEditor(project, file)
@@ -22,4 +22,33 @@ class StackTraceFileEditorProvider : WeighedFileEditorProvider() {
   override fun getEditorTypeId(): String = "stacktrace-preview-editor"
 
   override fun getPolicy(): FileEditorPolicy = FileEditorPolicy.PLACE_AFTER_DEFAULT_EDITOR
+
+
+  private companion object {
+    private fun isThreadDump(text: String): Boolean {
+      val stackTracePattern = Pattern.compile(
+        """\t*at [_\w/+.]+[_\w$0-9/]+\.[_\w/]+\([_\w/]+\.(java|kt):\d+\)+[ ~*\[\w.:/\]]*"""
+      )
+      val threadStateNames = Thread.State.entries.map { it.name }.toSet()
+      val matcher = stackTracePattern.matcher("")
+
+      var consecutiveStackLines = 0
+      var hasThreadState = false
+
+      for (line in text.split("\n")) {
+        val trimmedLine = line.trim()
+        if (trimmedLine.isEmpty()) continue
+        if (trimmedLine.startsWith("java.lang.Thread.State:")) {
+          hasThreadState = threadStateNames.any { state -> trimmedLine.contains(state) }
+        }
+        if (matcher.reset(trimmedLine).matches()) {
+          consecutiveStackLines++
+        } else {
+          consecutiveStackLines = 0
+        }
+        if (consecutiveStackLines > 2 && hasThreadState) return true
+      }
+      return false
+    }
+  }
 }
