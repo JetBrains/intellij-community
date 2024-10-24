c761db4a4983725af85d143ad7a67e277a61193e
Roman Vasiliev <roman.vasiliev@jetbrains.com>
2024-10-24 15:56:23 +0200

    [evaluation-plugin] Fix build problems because of files with the same FqName
    Both Constants.kt from different modules have FqName equal to com.intellij.cce.evaluable.chat.ConstantsKt which breaks incremental compilation
    
    GitOrigin-RevId: 1e95ad81d60ebcdb4c4813349b3c78249957f5e2

--- plugins/evaluation-plugin/core/src/com/intellij/cce/evaluable/chat/Constants.kt
+++ plugins/evaluation-plugin/core/src/com/intellij/cce/evaluable/chat/Constants.kt
@@ -3,3 +3,5 @@ package com.intellij.cce.evaluable.chat
 const val PROMPT_PROPERTY = "prompt"
 const val INTERNAL_API_CALLS_PROPERTY = "internal_api_calls"
 const val INTERNAL_RELEVANT_FILES_PROPERTY = "internal_relevant_files"
+
+const val METHOD_NAME_PROPERTY = "method_name"
