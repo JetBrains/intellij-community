f8f7eb39d17db81d7249f2f3d1c02b2e5a3110f5
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2024-10-26 01:33:00 +0200

    Eel tunnels: Rename EelIpPreference to make it part of API
    GitOrigin-RevId: ed53d0e4d1c97f05cf30b13de64a8034d8799018

--- platform/eel/src/com/intellij/platform/eel/impl/EelIpPreference.kt
+++ platform/eel/src/com/intellij/platform/eel/impl/EelIpPreference.kt
@@ -1,6 +1,6 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.eel.impl
 
-enum class IpProtocolPreferenceImpl {
+enum class EelIpPreference {
   PREFER_V4, PREFER_V6, USE_SYSTEM_DEFAULT
 }
