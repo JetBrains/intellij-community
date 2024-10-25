7bfc1fc58aa8b95b59d29a7226475553c696a812
Bogdan Kirilenko <bogdan.kirilenko@jetbrains.com>
2024-10-25 12:35:16 +0200

    [PyCharm] PY-72283 Jupyter additional toolbar minor design fixes + registry key jupyter.cell.additional.toolbar added
    GitOrigin-RevId: 88ec8eb240a3a15abbc8c8768c3b5d07f86e3c2f

--- notebooks/notebook-ui/src/com/intellij/notebooks/ui/jupyterToolbar/JupyterAdditionalToolbar.kt
+++ notebooks/notebook-ui/src/com/intellij/notebooks/ui/jupyterToolbar/JupyterAdditionalToolbar.kt
@@ -2,6 +2,7 @@ package com.intellij.notebooks.ui.jupyterToolbar
 
 import com.intellij.openapi.actionSystem.ActionGroup
 import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.util.ui.JBUI
 import org.jetbrains.annotations.ApiStatus
 import javax.swing.JComponent
@@ -13,6 +14,11 @@ class JupyterAdditionalToolbar(  // PY-72283
   place: String = ActionPlaces.EDITOR_INLAY
 ) : JupyterAbstractAboveCellToolbar(actionGroup, target, place) {
 
-  override fun getArcSize(): Int = JBUI.scale(6)
-  override fun getOuterPadding(): Int = JBUI.scale(1)
+  init {
+    background = EditorColorsManager.getInstance().globalScheme.defaultBackground
+  }
+
+  override fun getArcSize(): Int = JBUI.scale(8)
+  override fun getHorizontalPadding() = JBUI.scale(4)
+  override fun getVerticalPadding() = JBUI.scale(1)
 }
