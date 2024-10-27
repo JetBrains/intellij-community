545c64d28abb8b9843664fa746cfcddaff2705b9
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2024-10-27 22:54:14 +0100

    EelApi: Func to convert JVM IP address to Eel address
    GitOrigin-RevId: ec3a1ca70f81980bf2ac19bda8b8e77a57e51bc6

--- platform/eel/src/com/intellij/platform/eel/impl/IpAddressesImpl.kt
+++ platform/eel/src/com/intellij/platform/eel/impl/IpAddressesImpl.kt
@@ -2,6 +2,37 @@
 package com.intellij.platform.eel.impl
 
 import com.intellij.platform.eel.EelTunnelsApi
+import com.intellij.platform.eel.EelTunnelsApi.ResolvedSocketAddress
+import java.net.Inet4Address
+import java.net.Inet6Address
+import java.net.InetSocketAddress
+import java.net.SocketAddress
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
-data class ResolvedV4SocketAddressImpl(override val port: UShort, override val bits: UInt) : EelTunnelsApi.ResolvedSocketAddress.V4
+data class ResolvedV4SocketAddressImpl(override val port: UShort, override val bits: UInt) : ResolvedSocketAddress.V4
 data class ResolvedV6SocketAddressImpl(override val port: UShort, override val higherBits: ULong, override val lowerBits: ULong) : EelTunnelsApi.ResolvedSocketAddress.V6
+
+
+val SocketAddress.asResolvedSocketAddress: ResolvedSocketAddress
+  get() {
+    if (this !is InetSocketAddress) error("Socket family $this not supported")
+    val inetAddress = this.address
+    assert(port >= 0 && port < UShort.MAX_VALUE.toInt()) { "port $port is wrong" }
+    val port = port.toUShort()
+    val buffer = ByteBuffer.allocate(inetAddress.address.size)
+      .order(ByteOrder.BIG_ENDIAN) // network order
+      .put(inetAddress.address)
+      .rewind()
+    return when (inetAddress) {
+      is Inet4Address -> {
+        assert(buffer.limit() == 4)
+        ResolvedV4SocketAddressImpl(port, buffer.int.toUInt())
+      }
+      is Inet6Address -> {
+        assert(buffer.limit() == 16)
+        ResolvedV6SocketAddressImpl(port, buffer.long.toULong(), buffer.long.toULong())
+      }
+      else -> error("Inet family $address not supported")
+    }
+  }
