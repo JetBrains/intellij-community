e2bcd20060c17bad1dd71e2816e777b6d5151f1a
Alexey Belkov <alexey.belkov@jetbrains.com>
2024-10-28 13:47:47 +0400

    [kotlin] J2K copy-paste: extract common functions to a separate file
    KTIJ-28714
    
    GitOrigin-RevId: 84d711f777defd8e3833ee734023eff93e601dfb

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/conversion/copy/ConvertJavaCopyPasteProcessor.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/conversion/copy/ConvertJavaCopyPasteProcessor.kt
@@ -4,7 +4,6 @@ package org.jetbrains.kotlin.idea.conversion.copy
 
 import com.intellij.codeInsight.editorActions.CopyPastePostProcessor
 import com.intellij.codeInsight.editorActions.TextBlockTransferableData
-import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.diagnostic.ControlFlowException
 import com.intellij.openapi.diagnostic.Logger
@@ -12,36 +11,30 @@ import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.RangeMarker
 import com.intellij.openapi.editor.asTextRange
-import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Ref
 import com.intellij.openapi.util.TextRange
-import com.intellij.openapi.util.ThrowableComputable
-import com.intellij.openapi.util.text.StringUtil
-import com.intellij.psi.*
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiJavaFile
 import org.jetbrains.annotations.TestOnly
-import org.jetbrains.kotlin.idea.actions.JavaToKotlinAction
-import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.base.util.module
 import org.jetbrains.kotlin.idea.caches.resolve.resolveImportReference
 import org.jetbrains.kotlin.idea.codeInsight.KotlinCopyPasteReferenceProcessor
 import org.jetbrains.kotlin.idea.codeInsight.KotlinReferenceData
-import org.jetbrains.kotlin.idea.configuration.ExperimentalFeatures.NewJ2k
 import org.jetbrains.kotlin.idea.editor.KotlinEditorOptions
 import org.jetbrains.kotlin.idea.statistics.ConversionType
 import org.jetbrains.kotlin.idea.statistics.J2KFusCollector
 import org.jetbrains.kotlin.idea.util.ImportInsertHelper
-import org.jetbrains.kotlin.j2k.*
+import org.jetbrains.kotlin.j2k.ConverterContext
+import org.jetbrains.kotlin.j2k.J2kConverterExtension
 import org.jetbrains.kotlin.j2k.J2kConverterExtension.Kind.K1_NEW
-import org.jetbrains.kotlin.j2k.J2kConverterExtension.Kind.K1_OLD
+import org.jetbrains.kotlin.j2k.ParseContext
 import org.jetbrains.kotlin.j2k.ParseContext.CODE_BLOCK
 import org.jetbrains.kotlin.j2k.ParseContext.TOP_LEVEL
-import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.name.FqName
-import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.psi.psiUtil.endOffset
-import org.jetbrains.kotlin.psi.psiUtil.parentsWithSelf
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtPsiFactory
 import java.awt.datatransfer.Transferable
 import kotlin.system.measureTimeMillis
 
@@ -269,151 +262,5 @@ private class J2KCopyPasteConverter(
         }
 
         return rangeMarker.asTextRange
-    }
-}
-
-
-///// Below are common functions shared between ConvertJavaCopyPasteProcessor and ConvertTextJavaCopyPasteProcessor /////
-
-internal data class ConversionResult(
-    val text: String,
-    val parseContext: ParseContext,
-    val importsToAdd: Set<FqName>,
-    val isTextChanged: Boolean,
-    val converterContext: ConverterContext?
-)
-
-internal fun ElementAndTextList.convertCodeToKotlin(
-    project: Project,
-    targetFile: KtFile,
-    j2kKind: J2kConverterExtension.Kind
-): ConversionResult {
-    val converter = J2kConverterExtension.extension(j2kKind).createJavaToKotlinConverter(
-        project,
-        targetFile.module,
-        ConverterSettings.defaultSettings,
-        targetFile
-    )
-
-    val inputElements = this.toList().filterIsInstance<PsiElement>()
-    val (results, _, converterContext) = ProgressManager.getInstance().runProcessWithProgressSynchronously(
-        ThrowableComputable {
-            runReadAction { converter.elementsToKotlin(inputElements) }
-        },
-        JavaToKotlinAction.Handler.title,
-        true,
-        project
-    )
-
-    val importsToAdd = mutableSetOf<FqName>()
-    val convertedCodeBuilder = StringBuilder()
-    val originalCodeBuilder = StringBuilder()
-    var parseContext: ParseContext? = null
-    var resultIndex = 0
-
-    this.process(object : ElementsAndTextsProcessor {
-        override fun processElement(element: PsiElement) {
-            val originalText = element.text
-            originalCodeBuilder.append(originalText)
-
-            val result = results[resultIndex]
-            resultIndex++
-
-            if (result != null) {
-                convertedCodeBuilder.append(result.text.trimEnd('\n'))
-                if (parseContext == null) {
-                    // Use the parse context of the first converted element as a parse context for the whole text
-                    parseContext = result.parseContext
-                }
-                importsToAdd.addAll(result.importsToAdd)
-            } else {
-                // Failed to convert the element to Kotlin, insert the Java text as is
-                convertedCodeBuilder.append(originalText)
-            }
-        }
-
-        override fun processText(text: String) {
-            originalCodeBuilder.append(text)
-            convertedCodeBuilder.append(text)
-        }
-    })
-
-    val convertedCode = convertedCodeBuilder.toString()
-    val originalCode = originalCodeBuilder.toString()
-    val textChanged = convertedCode != originalCode
-    return ConversionResult(
-        convertedCode,
-        parseContext ?: CODE_BLOCK,
-        importsToAdd,
-        textChanged,
-        converterContext
-    )
-}
-
-/**
- * Does it make sense to run J2K on pasted code in this particular place in the file?
- */
-internal fun isConversionSupportedAtPosition(file: KtFile, offset: Int): Boolean {
-    if (offset == 0) return true
-    val token = file.findElementAt(offset - 1) ?: return false
-
-    if (token !is PsiWhiteSpace && token.endOffset != offset) {
-        // pasting into the middle of token
-        return false
-    }
-
-    for (element in token.parentsWithSelf) {
-        when (element) {
-            is PsiComment -> return element.node.elementType != KtTokens.EOL_COMMENT && offset == element.endOffset
-            is KtStringTemplateEntryWithExpression -> return true
-            is KtStringTemplateExpression -> return false
-        }
-    }
-
-    return true
-}
-
-internal fun confirmConvertJavaOnPaste(project: Project, isPlainText: Boolean): Boolean {
-    if (KotlinEditorOptions.getInstance().isDonTShowConversionDialog) return true
-    val dialog = KotlinPasteFromJavaDialog(project, isPlainText)
-    dialog.show()
-    return dialog.isOK
-}
-
-internal fun ElementAndTextList.lineCount(): Int {
-    val elements = this.toList().filterIsInstance<PsiElement>()
-    return elements.sumOf { StringUtil.getLineBreakCount(it.text) }
-}
-
-internal fun getJ2kKind(targetFile: KtFile): J2kConverterExtension.Kind =
-    if (targetFile is KtCodeFragment || !NewJ2k.isEnabled) K1_OLD else K1_NEW
-
-internal fun runPostProcessing(
-    project: Project,
-    file: KtFile,
-    bounds: TextRange?,
-    converterContext: ConverterContext?,
-    j2kKind: J2kConverterExtension.Kind
-) {
-    val postProcessor = J2kConverterExtension.extension(j2kKind).createPostProcessor()
-    if (j2kKind == K1_NEW) {
-        val runnable = {
-            val processor = J2kConverterExtension.extension(kind = K1_NEW).createWithProgressProcessor(
-                ProgressManager.getInstance().progressIndicator!!,
-                emptyList(),
-                postProcessor.phasesCount
-            )
-            J2KPostProcessingRunner.run(postProcessor, file, converterContext, bounds) { phase, description ->
-                processor.updateState(0, phase, description)
-            }
-        }
-        ProgressManager.getInstance().runProcessWithProgressSynchronously(
-            runnable,
-            KotlinBundle.message("copy.text.convert.java.to.kotlin.title"),
-            /* canBeCanceled = */ true,
-            project
-        )
-    } else {
-        J2KPostProcessingRunner.run(postProcessor, file, converterContext, bounds)
     }
 }
---
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/conversion/copy/CopyPasteConversionUtils.kt
@@ -0,0 +1,172 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.conversion.copy
+
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.ThrowableComputable
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.PsiComment
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiWhiteSpace
+import org.jetbrains.kotlin.idea.actions.JavaToKotlinAction
+import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
+import org.jetbrains.kotlin.idea.base.util.module
+import org.jetbrains.kotlin.idea.configuration.ExperimentalFeatures.NewJ2k
+import org.jetbrains.kotlin.idea.editor.KotlinEditorOptions
+import org.jetbrains.kotlin.j2k.*
+import org.jetbrains.kotlin.j2k.J2kConverterExtension.Kind.K1_NEW
+import org.jetbrains.kotlin.j2k.J2kConverterExtension.Kind.K1_OLD
+import org.jetbrains.kotlin.j2k.ParseContext.CODE_BLOCK
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtCodeFragment
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtStringTemplateEntryWithExpression
+import org.jetbrains.kotlin.psi.KtStringTemplateExpression
+import org.jetbrains.kotlin.psi.psiUtil.endOffset
+import org.jetbrains.kotlin.psi.psiUtil.parentsWithSelf
+
+data class ConversionResult(
+    val text: String,
+    val parseContext: ParseContext,
+    val importsToAdd: Set<FqName>,
+    val isTextChanged: Boolean,
+    val converterContext: ConverterContext?
+)
+
+fun ElementAndTextList.convertCodeToKotlin(
+    project: Project,
+    targetFile: KtFile,
+    j2kKind: J2kConverterExtension.Kind
+): ConversionResult {
+    val converter = J2kConverterExtension.extension(j2kKind).createJavaToKotlinConverter(
+        project,
+        targetFile.module,
+        ConverterSettings.defaultSettings,
+        targetFile
+    )
+
+    val inputElements = this.toList().filterIsInstance<PsiElement>()
+    val (results, _, converterContext) = ProgressManager.getInstance().runProcessWithProgressSynchronously(
+        ThrowableComputable {
+            runReadAction { converter.elementsToKotlin(inputElements) }
+        },
+        JavaToKotlinAction.Handler.title,
+        true,
+        project
+    )
+
+    val importsToAdd = mutableSetOf<FqName>()
+    val convertedCodeBuilder = StringBuilder()
+    val originalCodeBuilder = StringBuilder()
+    var parseContext: ParseContext? = null
+    var resultIndex = 0
+
+    this.process(object : ElementsAndTextsProcessor {
+        override fun processElement(element: PsiElement) {
+            val originalText = element.text
+            originalCodeBuilder.append(originalText)
+
+            val result = results[resultIndex]
+            resultIndex++
+
+            if (result != null) {
+                convertedCodeBuilder.append(result.text.trimEnd('\n'))
+                if (parseContext == null) {
+                    // Use the parse context of the first converted element as a parse context for the whole text
+                    parseContext = result.parseContext
+                }
+                importsToAdd.addAll(result.importsToAdd)
+            } else {
+                // Failed to convert the element to Kotlin, insert the Java text as is
+                convertedCodeBuilder.append(originalText)
+            }
+        }
+
+        override fun processText(text: String) {
+            originalCodeBuilder.append(text)
+            convertedCodeBuilder.append(text)
+        }
+    })
+
+    val convertedCode = convertedCodeBuilder.toString()
+    val originalCode = originalCodeBuilder.toString()
+    val textChanged = convertedCode != originalCode
+    return ConversionResult(
+        convertedCode,
+        parseContext ?: CODE_BLOCK,
+        importsToAdd,
+        textChanged,
+        converterContext
+    )
+}
+
+/**
+ * Does it make sense to run J2K on pasted code in this particular place in the file?
+ */
+fun isConversionSupportedAtPosition(file: KtFile, offset: Int): Boolean {
+    if (offset == 0) return true
+    val token = file.findElementAt(offset - 1) ?: return false
+
+    if (token !is PsiWhiteSpace && token.endOffset != offset) {
+        // pasting into the middle of a token
+        return false
+    }
+
+    for (element in token.parentsWithSelf) {
+        when (element) {
+            is PsiComment -> return element.node.elementType != KtTokens.EOL_COMMENT && offset == element.endOffset
+            is KtStringTemplateEntryWithExpression -> return true
+            is KtStringTemplateExpression -> return false
+        }
+    }
+
+    return true
+}
+
+fun confirmConvertJavaOnPaste(project: Project, isPlainText: Boolean): Boolean {
+    if (KotlinEditorOptions.getInstance().isDonTShowConversionDialog) return true
+    val dialog = KotlinPasteFromJavaDialog(project, isPlainText)
+    dialog.show()
+    return dialog.isOK
+}
+
+fun ElementAndTextList.lineCount(): Int {
+    val elements = this.toList().filterIsInstance<PsiElement>()
+    return elements.sumOf { StringUtil.getLineBreakCount(it.text) }
+}
+
+fun getJ2kKind(targetFile: KtFile): J2kConverterExtension.Kind =
+    if (targetFile is KtCodeFragment || !NewJ2k.isEnabled) K1_OLD else K1_NEW
+
+fun runPostProcessing(
+    project: Project,
+    file: KtFile,
+    bounds: TextRange?,
+    converterContext: ConverterContext?,
+    j2kKind: J2kConverterExtension.Kind
+) {
+    val postProcessor = J2kConverterExtension.extension(j2kKind).createPostProcessor()
+    if (j2kKind == K1_NEW) {
+        val runnable = {
+            val processor = J2kConverterExtension.extension(kind = K1_NEW).createWithProgressProcessor(
+                ProgressManager.getInstance().progressIndicator!!,
+                emptyList(),
+                postProcessor.phasesCount
+            )
+            J2KPostProcessingRunner.run(postProcessor, file, converterContext, bounds) { phase, description ->
+                processor.updateState(0, phase, description)
+            }
+        }
+        ProgressManager.getInstance().runProcessWithProgressSynchronously(
+            runnable,
+            KotlinBundle.message("copy.text.convert.java.to.kotlin.title"),
+            /* canBeCanceled = */ true,
+            project
+        )
+    } else {
+        J2KPostProcessingRunner.run(postProcessor, file, converterContext, bounds)
+    }
+}
