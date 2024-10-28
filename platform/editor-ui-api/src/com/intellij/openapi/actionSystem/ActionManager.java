0d84f433a1ac1c7c67b1a552924ff7feec5d143c
Yann Cébron <yann.cebron@jetbrains.com>
2024-10-28 10:35:54 +0100

    [platform] ActionManager: add a link to reference docs
    GitOrigin-RevId: d480fe61fd0040ff23f1600e7baabc7cffe91fbb

--- platform/editor-ui-api/src/com/intellij/openapi/actionSystem/ActionManager.java
+++ platform/editor-ui-api/src/com/intellij/openapi/actionSystem/ActionManager.java
@@ -20,6 +20,7 @@ import java.util.List;
  * Used to register and unregister actions, it also contains utility methods to easily fetch action by id and id by action.
  *
  * @see AnAction
+ * @see <a href="https://plugins.jetbrains.com/docs/intellij/basic-action-system.html#buildingToolbarPopupMenu">Building a Toolbar/Popup Menu from Actions (IntelliJ Platform Docs)</a>
  */
 public abstract class ActionManager {
   /**
