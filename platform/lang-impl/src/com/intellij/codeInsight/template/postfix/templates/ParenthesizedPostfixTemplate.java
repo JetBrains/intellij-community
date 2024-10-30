75daab8eb345e069d99c9301de041d31121fb9c2
Yann Cébron <yann.cebron@jetbrains.com>
2024-10-30 17:13:25 +0100

    [platform] suppress PostfixTemplateDescriptionNotFound inspection
    GitOrigin-RevId: f940d27cdb851cd7d0bc70ee4b36d15e85e076fa

--- platform/lang-impl/src/com/intellij/codeInsight/template/postfix/templates/ParenthesizedPostfixTemplate.java
+++ platform/lang-impl/src/com/intellij/codeInsight/template/postfix/templates/ParenthesizedPostfixTemplate.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.template.postfix.templates;
 
 import com.intellij.openapi.editor.Editor;
@@ -6,6 +6,7 @@ import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+@SuppressWarnings("PostfixTemplateDescriptionNotFound")
 public class ParenthesizedPostfixTemplate extends PostfixTemplateWithExpressionSelector {
 
   private final PostfixTemplatePsiInfo myPsiInfo;
