6dd2d96bc3b4ef27cdb49e106dc00cc31926ad53
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2024-10-30 11:00:39 +0100

    [kotlin] Fixed spelling mistakes in the new Kotlin Gradle wizard templates found by proofreaders
    ^KTIJ-31816 fixed
    
    
    (cherry picked from commit 0ef15e11dcc4a5df2cbbcab192ea905566670d9a)
    
    IJ-CR-147990
    
    GitOrigin-RevId: 48d4cc3854389fe6e4c73fb086790d020799fe04

--- plugins/kotlin/project-wizard/tests/testData/gradleNewProjectWizard/multiModuleProjectKts/buildSrc/settings.gradle.kts
+++ plugins/kotlin/project-wizard/tests/testData/gradleNewProjectWizard/multiModuleProjectKts/buildSrc/settings.gradle.kts
@@ -1,12 +1,12 @@
 dependencyResolutionManagement {
 
-    // Use Maven Central and Gradle Plugin Portal for resolving dependencies in the shared build logic ("buildSrc") project
+    // Use Maven Central and the Gradle Plugin Portal for resolving dependencies in the shared build logic (`buildSrc`) project.
     @Suppress("UnstableApiUsage")
     repositories {
         mavenCentral()
     }
 
-    // Re-use the version catalog from the main build
+    // Reuse the version catalog from the main build.
     versionCatalogs {
         create("libs") {
             from(files("../gradle/libs.versions.toml"))
