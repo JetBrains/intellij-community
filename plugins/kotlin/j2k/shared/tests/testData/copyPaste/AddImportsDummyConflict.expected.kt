235f3a2012481837912015a50d1c4898f247f664
Alexey Belkov <alexey.belkov@jetbrains.com>
2024-10-31 18:18:55 +0400

    [kotlin] J2K copy-paste tests: stop dumping errors
    Almost every copy-paste test file has expected errors
    (usually, unresolved references), so it looks noisy to dump them all.
    
    Also, this will help to share expected results between K1 and K2,
    because they often differ only in exact wording of error messages.
    
    KTIJ-28714
    
    (cherry picked from commit 4d2e181ee3d4781f3064f046f4f6ac3005d1e9fc)
    
    GitOrigin-RevId: 69501d6a2916dc8959707a4c7ada3a3322f8209e

--- plugins/kotlin/j2k/shared/tests/testData/copyPaste/AddImportsDummyConflict.expected.kt
+++ plugins/kotlin/j2k/shared/tests/testData/copyPaste/AddImportsDummyConflict.expected.kt
@@ -1,4 +1,3 @@
-// ERROR: Type mismatch: inferred type is dummy0 but Unit was expected
 import ppp.dummy0
 
 fun f() {
--- plugins/kotlin/j2k/shared/tests/testData/copyPaste/UnconvertedMethodReference.expected.kt
+++ plugins/kotlin/j2k/shared/tests/testData/copyPaste/UnconvertedMethodReference.expected.kt
@@ -1,5 +1,3 @@
-// ERROR: None of the following functions can be called with the arguments supplied:  public open fun printf(l: Locale!, format: String!, vararg args: Any!): PrintStream! defined in java.io.PrintStream public open fun printf(format: String!, vararg args: Any!): PrintStream! defined in java.io.PrintStream
-// ERROR: Function invocation 'printf(...)' expected
 fun foo() {
     System.out.printf
 }
