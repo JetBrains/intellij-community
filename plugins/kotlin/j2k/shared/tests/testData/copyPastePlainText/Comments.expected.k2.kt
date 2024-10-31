80d5d88bfdea577dc6510d46b1777180f8f7947e
Alexey Belkov <alexey.belkov@jetbrains.com>
2024-10-31 22:52:53 +0400

    [kotlin] J2K copy-paste tests: K2-specific testdata
    Mostly small differences in nullability, mutability
    and some other things.
    
    KTIJ-28714
    
    (cherry picked from commit 37aadb47d4f04c66c41778e5b8b9bc4bc98465d0)
    
    GitOrigin-RevId: 57956284eca03441dbb9fe72df3df8d7e81855ca

---
+++ plugins/kotlin/j2k/shared/tests/testData/copyPastePlainText/Comments.expected.k2.kt
@@ -0,0 +1,21 @@
+fun foo() {
+
+    //comment 1
+    val a = 5
+
+
+    /*comment 2*/
+
+    //comment 3
+    class A {
+        // comment 4
+        internal inner class B {
+            //comment 5
+            fun foo() {
+                // comment 6
+            }
+        } // comment 7
+    }
+
+
+}
