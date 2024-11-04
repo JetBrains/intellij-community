66796e15d8eeaa638e28b4b7edde92b3ae2f747c
Alexey.Merkulov <alexey.merkulov@jetbrains.com>
2024-11-04 17:11:05 +0100

    [debugger] Improve statistics about Kotlin code fragment compilation
    IDEA-362361
    
    (cherry picked from commit b91dbbf519d67861ef91558eb8a6265583697ab0)
    
    
    IJ-CR-151042
    
    GitOrigin-RevId: cd5a091a3223609027a0343fe4506e03eb0ecfca

--- plugins/kotlin/jvm-debugger/evaluation/src/org/jetbrains/kotlin/idea/debugger/evaluate/KotlinDebuggerEvaluatorStatisticsCollector.kt
+++ plugins/kotlin/jvm-debugger/evaluation/src/org/jetbrains/kotlin/idea/debugger/evaluate/KotlinDebuggerEvaluatorStatisticsCollector.kt
@@ -1,21 +1,29 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.debugger.evaluate
 
+import com.intellij.debugger.engine.evaluation.EvaluateException
+import com.intellij.debugger.engine.evaluation.EvaluationContextImpl
 import com.intellij.internal.statistic.eventLog.EventLogGroup
 import com.intellij.internal.statistic.eventLog.events.EventFields
 import com.intellij.internal.statistic.eventLog.events.EventPair
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
 import com.intellij.openapi.project.Project
+import com.intellij.xdebugger.impl.ui.tree.nodes.XEvaluationOrigin
+import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.kotlin.idea.debugger.evaluate.compilation.CodeFragmentCompilationStats
+import java.util.concurrent.ExecutionException
+
+internal val EvaluationContextImpl.origin: XEvaluationOrigin get() = XEvaluationOrigin.getOrigin(this)
 
 object KotlinDebuggerEvaluatorStatisticsCollector : CounterUsagesCollector() {
 
     override fun getGroup(): EventLogGroup = GROUP
 
-    private val GROUP = EventLogGroup("kotlin.debugger.evaluator", 7)
+    private val GROUP = EventLogGroup("kotlin.debugger.evaluator", 8)
 
     // fields
     private val compilerField = EventFields.Enum<CompilerType>("compiler")
+    private val originField = EventFields.Enum<XEvaluationOrigin>("origin")
     private val compilationResultField = EventFields.Enum<EvaluationCompilerResult>("compilation_result")
     private val resultField = EventFields.Enum<StatisticsEvaluationResult>("result")
     private val wrapTimeMsField = EventFields.Long("wrap_time_ms")
@@ -23,12 +31,12 @@ object KotlinDebuggerEvaluatorStatisticsCollector : CounterUsagesCollector() {
     private val compilationTimeMsField = EventFields.Long("compilation_time_ms")
     private val wholeTimeField = EventFields.Long("whole_time_field")
     private val interruptionsField = EventFields.Int("total_interruptions")
-    private val compilerFailTypeField = EventFields.Enum<CompilerFailType>("compiler_fail_type")
+    private val compilerExceptionField = EventFields.Class("exception")
 
     // events
     private val analysisCompilationEvent = GROUP.registerVarargEvent(
-        "analysis.compilation.result", compilerField, compilationResultField,
-        wrapTimeMsField, analysisTimeMsField, compilationTimeMsField, wholeTimeField, interruptionsField, compilerFailTypeField
+        "analysis.compilation.result", compilerField, originField, compilationResultField,
+        wrapTimeMsField, analysisTimeMsField, compilationTimeMsField, wholeTimeField, interruptionsField, compilerExceptionField
     )
     // no need to record evaluation time, as it reflects what user evaluates, not how effective our evaluation is
     private val evaluationEvent = GROUP.registerEvent("evaluation.result", resultField, compilerField)
@@ -38,17 +46,17 @@ object KotlinDebuggerEvaluatorStatisticsCollector : CounterUsagesCollector() {
         project: Project?, compilerType: CompilerType, compilationResult: EvaluationCompilerResult,
         stats: CodeFragmentCompilationStats
     ) {
-        analysisCompilationEvent.log(
-            project,
+        val parameters = listOf(
             EventPair(compilerField, compilerType),
+            EventPair(originField, stats.origin),
             EventPair(compilationResultField, compilationResult),
             EventPair(wrapTimeMsField, stats.wrapTimeMs),
             EventPair(analysisTimeMsField, stats.analysisTimeMs),
             EventPair(compilationTimeMsField, stats.compilationTimeMs),
             EventPair(wholeTimeField, System.currentTimeMillis() - stats.startTimeMs),
             EventPair(interruptionsField, stats.interruptions),
-            EventPair(compilerFailTypeField, stats.compilerFailType),
-        )
+        ) + (stats.compilerFailExceptionClass?.let { listOf(EventPair(compilerExceptionField, it)) } ?: emptyList())
+        analysisCompilationEvent.log(project, parameters)
     }
 
     @JvmStatic
@@ -63,4 +71,15 @@ enum class EvaluationCompilerResult {
 
 enum class StatisticsEvaluationResult {
     SUCCESS, FAILURE
+}
+
+@ApiStatus.Internal
+fun extractExceptionCauseClass(e: Throwable): Class<out Throwable> {
+    val cause = (e as? ExecutionException)?.cause ?: e
+
+    return if (cause is EvaluateException) {
+        cause.cause?.javaClass ?: cause.javaClass
+    } else {
+        cause.javaClass
+    }
 }
