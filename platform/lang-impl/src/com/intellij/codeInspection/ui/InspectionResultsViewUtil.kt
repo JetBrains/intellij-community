43268486b8d1859f9ad1cfe07f92e8e3d713c129
Louis Vignier <Louis.Vignier@jetbrains.com>
2024-11-04 12:46:28 +0100

    [codeInspection.ui] Convert InspectionResultsViewUtil to Kotlin
    GitOrigin-RevId: 84bf91735db348ddceea29ad63756fa1460d7922

--- platform/lang-impl/src/com/intellij/codeInspection/ui/InspectionResultsViewUtil.kt
+++ platform/lang-impl/src/com/intellij/codeInspection/ui/InspectionResultsViewUtil.kt
@@ -1,85 +1,86 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.codeInspection.ui;
+package com.intellij.codeInspection.ui
 
-import com.intellij.codeInspection.InspectionsBundle;
-import com.intellij.codeInspection.ex.InspectionToolWrapper;
-import com.intellij.codeInspection.reference.RefElement;
-import com.intellij.codeInspection.reference.RefEntity;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.EditorFactory;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.pom.Navigatable;
-import com.intellij.psi.NavigatablePsiElement;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.ui.components.JBLabel;
-import com.intellij.util.ui.JBUI;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
+import com.intellij.codeInspection.InspectionsBundle
+import com.intellij.codeInspection.ex.InspectionToolWrapper
+import com.intellij.codeInspection.reference.RefElement
+import com.intellij.codeInspection.reference.RefEntity
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.fileEditor.OpenFileDescriptor
+import com.intellij.pom.Navigatable
+import com.intellij.psi.NavigatablePsiElement
+import com.intellij.ui.components.JBLabel
+import com.intellij.util.ui.JBUI
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.Nls
+import javax.swing.JComponent
+import javax.swing.JLabel
+import javax.swing.SwingConstants
 
 /**
  * @author Dmitry Batkovich
  */
 @ApiStatus.Internal
-public final class InspectionResultsViewUtil {
-  static void releaseEditor(@Nullable Editor editor) {
+object InspectionResultsViewUtil {
+  @JvmStatic
+  fun releaseEditor(editor: Editor?) {
     if (editor != null && !editor.isDisposed()) {
-      EditorFactory.getInstance().releaseEditor(editor);
+      EditorFactory.getInstance().releaseEditor(editor)
     }
   }
 
-  static @Nullable Navigatable getNavigatableForInvalidNode(ProblemDescriptionNode node) {
-    RefEntity element = node.getElement();
+  @JvmStatic
+  fun getNavigatableForInvalidNode(node: ProblemDescriptionNode): Navigatable? {
+    var element = node.element
     while (element != null && !element.isValid()) {
-      element = element.getOwner();
+      element = element.getOwner()
     }
-    if (!(element instanceof RefElement)) return null;
-    PsiElement containingElement = ((RefElement)element).getPsiElement();
-    if (!(containingElement instanceof NavigatablePsiElement) || !containingElement.isValid()) return null;
+    if (element !is RefElement) return null
+    val containingElement = element.psiElement
+    if (containingElement !is NavigatablePsiElement || !containingElement.isValid()) return null
 
-    final int lineNumber = node.getLineNumber();
+    val lineNumber = node.getLineNumber()
     if (lineNumber != -1) {
-      final PsiFile containingFile = containingElement.getContainingFile();
+      val containingFile = containingElement.getContainingFile()
       if (containingFile != null) {
-        final VirtualFile file = containingFile.getVirtualFile();
-        final Document document = FileDocumentManager.getInstance().getDocument(file);
+        val file = containingFile.getVirtualFile()
+        val document = FileDocumentManager.getInstance().getDocument(file)
         if (document != null && document.getLineCount() > lineNumber) {
-          return new OpenFileDescriptor(containingElement.getProject(), file, lineNumber, 0);
+          return OpenFileDescriptor(containingElement.getProject(), file, lineNumber, 0)
         }
       }
     }
-    return (Navigatable)containingElement;
+    return containingElement as Navigatable
   }
 
-  static @NotNull JLabel getNothingToShowTextLabel() {
-    return createLabelForText(InspectionViewNavigationPanel.getTitleText(false));
+  @JvmStatic
+  val nothingToShowTextLabel: JLabel
+    get() = createLabelForText(InspectionViewNavigationPanel.getTitleText(false))
+
+  @JvmStatic
+  fun getInvalidEntityLabel(entity: RefEntity): JComponent {
+    val name = entity.getName()
+    return createLabelForText(InspectionsBundle.message("inspections.view.invalid.label", name))
   }
 
-  static @NotNull JComponent getInvalidEntityLabel(@NotNull RefEntity entity) {
-    final String name = entity.getName();
-    return createLabelForText(InspectionsBundle.message("inspections.view.invalid.label", name));
+  @JvmStatic
+  fun getPreviewIsNotAvailable(entity: RefEntity): JComponent {
+    val name = entity.getQualifiedName()
+    return createLabelForText(InspectionsBundle.message("inspections.view.no.preview.label", name))
   }
 
-  public static JComponent getPreviewIsNotAvailable(@NotNull RefEntity entity) {
-    final String name = entity.getQualifiedName();
-    return createLabelForText(InspectionsBundle.message("inspections.view.no.preview.label", name));
+  @JvmStatic
+  fun getApplyingFixLabel(wrapper: InspectionToolWrapper<*, *>): JComponent {
+    return createLabelForText(InspectionsBundle.message("inspections.view.applying.quick.label", wrapper.getDisplayName()))
   }
 
-  static @NotNull JComponent getApplyingFixLabel(@NotNull InspectionToolWrapper wrapper) {
-    return createLabelForText(InspectionsBundle.message("inspections.view.applying.quick.label", wrapper.getDisplayName()));
-  }
-
-  static @NotNull JLabel createLabelForText(@Nls String text) {
-    final JLabel multipleSelectionLabel = new JBLabel(text);
-    multipleSelectionLabel.setVerticalAlignment(SwingConstants.TOP);
-    multipleSelectionLabel.setBorder(JBUI.Borders.empty(16, 12, 0, 0));
-    return multipleSelectionLabel;
+  @JvmStatic
+  fun createLabelForText(text: @Nls String): JLabel {
+    val multipleSelectionLabel: JLabel = JBLabel(text)
+    multipleSelectionLabel.setVerticalAlignment(SwingConstants.TOP)
+    multipleSelectionLabel.setBorder(JBUI.Borders.empty(16, 12, 0, 0))
+    return multipleSelectionLabel
   }
 }
