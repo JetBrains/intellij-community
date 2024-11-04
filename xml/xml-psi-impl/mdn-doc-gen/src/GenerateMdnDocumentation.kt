afb73b5fb4baa4c8572e55a8d4ac05604360b6d7
Claas Augner <caugner@mozilla.com>
2024-11-04 13:07:12 +0100

    fix: replace "Mozzila" with "Mozilla"
    closes https://github.com/JetBrains/intellij-community/pull/2874
    
    GitOrigin-RevId: f076512d5e4894d288613061d44f8b34a0251362

--- xml/xml-psi-impl/mdn-doc-gen/src/GenerateMdnDocumentation.kt
+++ xml/xml-psi-impl/mdn-doc-gen/src/GenerateMdnDocumentation.kt
@@ -340,7 +340,7 @@ class GenerateMdnDocumentation : BasePlatformTestCase() {
         "url" to "https://creativecommons.org/licenses/by-sa/2.5/",
       ),
       "author" to mapOf(
-        "name" to "Mozzila Contributors",
+        "name" to "Mozilla Contributors",
         "url" to "https://github.com/mdn/content"
       ),
       "lang" to BUILT_LANG,
