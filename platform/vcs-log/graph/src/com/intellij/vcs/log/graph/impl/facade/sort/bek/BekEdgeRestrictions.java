5fd2a03ad86f1a4e2ca8817680c2599aa4f79bed
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2024-11-05 17:08:48 +0100

    [log] Replace custom compressing collections with fastutil
    Extra calculations required for their initializing and elements extraction (given intense read load profile)
    outweigh memory consumption gains even for huge repositories.
    
    GitOrigin-RevId: 89ea78023e3c256424aa1288e21acdc755c91150

--- platform/vcs-log/graph/src/com/intellij/vcs/log/graph/impl/facade/sort/bek/BekEdgeRestrictions.java
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/graph/impl/facade/sort/bek/BekEdgeRestrictions.java
@@ -1,13 +1,13 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.log.graph.impl.facade.sort.bek;
 
-import com.intellij.util.containers.MultiMap;
+import com.intellij.vcs.log.graph.utils.IntIntMultiMap;
 import org.jetbrains.annotations.NotNull;
 
 class BekEdgeRestrictions {
-  @NotNull private final MultiMap<Integer, Integer> myUpToEdge = new MultiMap<>();
+  @NotNull private final IntIntMultiMap myUpToEdge = new IntIntMultiMap();
 
-  @NotNull private final MultiMap<Integer, Integer> myDownToEdge = new MultiMap<>();
+  @NotNull private final IntIntMultiMap myDownToEdge = new IntIntMultiMap();
 
   void addRestriction(int upNode, int downNode) {
     myUpToEdge.putValue(upNode, downNode);
