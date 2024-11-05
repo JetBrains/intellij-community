b46e432133db367b37c82fd720bf97a27c95b4ab
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-11-05 16:15:22 +0900

    Add header and description to dialog

--- plugins/kotlin/j2k/shared/src/org/jetbrains/kotlin/nj2k/gui/filepicker/FilePicker.kt
+++ plugins/kotlin/j2k/shared/src/org/jetbrains/kotlin/nj2k/gui/filepicker/FilePicker.kt
@@ -4,8 +4,12 @@ package org.jetbrains.kotlin.nj2k.gui.filepicker
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.components.JBLabel
 import com.intellij.ui.dsl.builder.Align
+import com.intellij.ui.dsl.builder.BottomGap
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.util.ui.JBFont
+import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import java.awt.Dimension
 import javax.swing.JComponent
 
@@ -18,7 +22,7 @@ class FilePicker(
     private val fileViewer = FileViewerPanel(project, rootFile)
 
     init {
-        title = "Java to Kotlin"
+        title = KotlinBundle.message("action.j2k.gui.title")
         init()
     }
 
@@ -30,6 +34,13 @@ class FilePicker(
         // ビューア
         fileViewer.preferredSize = Dimension(600, 400)
         return panel {
+            row{
+                cell(JBLabel(KotlinBundle.message("action.j2k.gui.file_picker.header")).apply { font = JBFont.h3().asBold() })
+            }
+            row{
+                label(KotlinBundle.message("action.j2k.gui.file_picker.description"))
+                bottomGap(BottomGap.SMALL)
+            }
             row {
                 cell(filePicker).align(Align.FILL).resizableColumn()
                 cell(fileViewer).align(Align.FILL)
