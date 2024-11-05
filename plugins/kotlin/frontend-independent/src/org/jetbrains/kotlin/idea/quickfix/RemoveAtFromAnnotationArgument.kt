a299c8372393194c110630182f511eab87c4e782
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2024-11-05 19:32:54 +0400

    [kotlin] Move `RemoveAtFromAnnotationArgument` to frontend independent module
    ^KTIJ-29520
    
    GitOrigin-RevId: 8b6b88f36cc264aef19d292c77d604d8667048bb

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RemoveAtFromAnnotationArgument.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RemoveAtFromAnnotationArgument.kt
@@ -1,13 +1,10 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.quickfix
 
-import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.modcommand.ActionContext
 import com.intellij.modcommand.ModPsiUpdater
 import com.intellij.modcommand.PsiUpdateModCommandAction
 import com.intellij.psi.util.firstLeaf
-import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 
@@ -27,10 +24,5 @@ class RemoveAtFromAnnotationArgument(
             "Expected '@' at the beginning of the annotation argument, but found '${firstLeaf.text}'"
         }
         firstLeaf.delete()
-    }
-
-    companion object : KotlinSingleIntentionActionFactory() {
-        override fun createAction(diagnostic: Diagnostic): IntentionAction? =
-            RemoveAtFromAnnotationArgument(diagnostic.psiElement as KtAnnotationEntry).asIntention()
     }
 }
