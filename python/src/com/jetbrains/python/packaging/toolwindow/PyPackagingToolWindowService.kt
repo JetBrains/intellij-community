094694d4ce9b2c93d444c1b8f1c82f65fea56294
Aleksandr Sorotskii <aleksandr.sorotskii@jetbrains.com>
2024-11-05 17:30:36 +0100

    Filter out installed packages from the repository packages list; #PY-75726 Fixed
    (cherry picked from commit 2597ec8095672a8e08bd0101042392f7ad6174af)
    
    IJ-MR-148587
    
    GitOrigin-RevId: 4f3168bf3cf5ff208e3b01f57cc330c24919e369

--- python/src/com/jetbrains/python/packaging/toolwindow/PyPackagingToolWindowService.kt
+++ python/src/com/jetbrains/python/packaging/toolwindow/PyPackagingToolWindowService.kt
@@ -95,7 +95,7 @@ class PyPackagingToolWindowService(val project: Project, val serviceScope: Corou
     }
     else {
       val packagesByRepository = manager.repositoryManager.packagesByRepository().map { (repository, packages) ->
-        val shownPackages = packages.asSequence().limitDisplayableResult(repository)
+        val shownPackages = packages.asSequence().limitResultAndFilterOutInstalled(repository)
         PyPackagesViewData(repository, shownPackages, moreItems = packages.size - PACKAGES_LIMIT)
       }.toList()
 
@@ -269,7 +269,7 @@ class PyPackagingToolWindowService(val project: Project, val serviceScope: Corou
 
     val shownPackages = packageNames.asSequence()
       .sortedWith(comparator)
-      .limitDisplayableResult(repository, skipItems)
+      .limitResultAndFilterOutInstalled(repository, skipItems)
     val exactMatch = shownPackages.indexOfFirst { StringUtil.equalsIgnoreCase(it.name, query) }
     return PyPackagesViewData(repository, shownPackages, exactMatch, packageNames.size - shownPackages.size)
   }
@@ -326,15 +326,16 @@ class PyPackagingToolWindowService(val project: Project, val serviceScope: Corou
     }
     else {
       val packagesFromRepo = manager.repositoryManager.packagesFromRepository(repository)
-      val page = packagesFromRepo.asSequence().limitDisplayableResult(repository, skipItems)
+      val page = packagesFromRepo.asSequence().limitResultAndFilterOutInstalled(repository, skipItems)
       return PyPackagesViewData(repository, page, moreItems = packagesFromRepo.size - (PACKAGES_LIMIT + skipItems))
     }
   }
 
-  private fun Sequence<String>.limitDisplayableResult(repository: PyPackageRepository, skipItems: Int = 0): List<DisplayablePackage> {
+  private fun Sequence<String>.limitResultAndFilterOutInstalled(repository: PyPackageRepository, skipItems: Int = 0): List<DisplayablePackage> {
     return drop(skipItems)
       .take(PACKAGES_LIMIT)
-      .map { pkg -> installedPackages.values.find { it.name.lowercase() == pkg.lowercase() } ?: InstallablePackage(pkg, repository) }
+      .filter { pkg -> installedPackages.values.find { it.name.lowercase() == pkg.lowercase() } == null }
+      .map { pkg -> InstallablePackage(pkg, repository) }
       .toList()
   }
 
