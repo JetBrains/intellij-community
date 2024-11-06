a75f1c2a5ea7e9f6a6faaee415cf9087255e6dbd
Nikita Pavlenko <nikita.pavlenko@jetbrains.com>
2024-11-06 16:52:54 +0100

    [PyCharm] PY-74366 Jupyter (refactor): Reworked NotebookCellTagLabel, a lot of code removed. #PY-77220 Fixed
    (cherry picked from commit 2cbceb46968cec790bc8732010ab952a2dc79aaa)
    
    GitOrigin-RevId: 83a5316e699b911c3091eb3bf32c55b82b5e5efe

--- notebooks/notebook-ui/src/com/intellij/notebooks/ui/visualization/NotebookCellTagLabel.kt
+++ notebooks/notebook-ui/src/com/intellij/notebooks/ui/visualization/NotebookCellTagLabel.kt
@@ -1,16 +1,11 @@
 package com.intellij.notebooks.ui.visualization
 
-import com.intellij.openapi.actionSystem.ActionGroup
-import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.ActionPlaces
-import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.ui.Gray
 import com.intellij.ui.JBColor
 import com.intellij.ui.PopupHandler
 import com.intellij.util.ui.JBFont
 import com.intellij.util.ui.JBUI
-import java.awt.Component
 import java.awt.Graphics
 import java.awt.Graphics2D
 import java.awt.RenderingHints
@@ -18,51 +13,32 @@ import javax.swing.BorderFactory
 import javax.swing.JLabel
 
 class NotebookCellTagLabel(@NlsSafe val tag: String, val cellNum: Int) : JLabel(tag) {  // PY-72712
-  private var backgroundColor = JBColor.namedColor("Tag.background", Gray.xDF)
-  private var foregroundColor = JBColor.BLACK
-
   init {
     font = JBFont.small()
-    foreground = foregroundColor
+    foreground = JBColor.BLACK
     isOpaque = false
     border = BorderFactory.createEmptyBorder(TEXT_VERTICAL_PADDING, TEXT_HORIZONTAL_PADDING, TEXT_VERTICAL_PADDING, TEXT_HORIZONTAL_PADDING)
 
-    addMouseListener(object : PopupHandler() {
-      override fun invokePopup(comp: Component, x: Int, y: Int) = showPopup( { createPopupGroup() }, comp, x, y)
-    })
-  }
-
-  private fun showPopup(popupBuilder: () -> ActionGroup, component: Component, x: Int, y: Int) {
-    val popupMenu = ActionManager.getInstance().createActionPopupMenu(ActionPlaces.EDITOR_POPUP, popupBuilder.invoke())
-    popupMenu.component.show(component, x, y)
-  }
-
-  private fun createPopupGroup(): DefaultActionGroup {
-    val group = DefaultActionGroup()
-    val action = ActionManager.getInstance().getAction(ACTION_ID)
-    group.add(action)
-    return group
+    PopupHandler.installPopupMenu(this, "JupyterCellTagActionGroup", "NotebookCellTagPopup")
   }
 
   override fun paintComponent(g: Graphics) {
-    (g as? Graphics2D)?.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-
-    g.color = backgroundColor
-    g.fillRoundRect(0, 0, width, height, ARC_SIZE , ARC_SIZE)
-
-    val fm = g.fontMetrics
-    val textHeight = fm.height
-    val textWidth =  fm.stringWidth(text)
-    val textX = (width - textWidth) / 2
-    val textY = (height - textHeight) / 2 + fm.ascent
-    g.color = foregroundColor
-    g.drawString(text, textX, textY)
+    val g2 = g.create() as Graphics2D
+    try {
+      g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+      g2.color = backgroundColor
+      g2.fillRoundRect(0, 0, width, height, ARC_SIZE, ARC_SIZE)
+    }
+    finally {
+      g2.dispose()
+    }
+    super.paintComponent(g)
   }
 
   companion object {
     private val TEXT_VERTICAL_PADDING = JBUI.scale(2)
     private val TEXT_HORIZONTAL_PADDING = JBUI.scale(8)
     private val ARC_SIZE = JBUI.scale(20)
-    private const val ACTION_ID = "JupyterCellRemoveTagAction"
+    private var backgroundColor = JBColor.namedColor("Tag.background", Gray.xDF)
   }
 }
