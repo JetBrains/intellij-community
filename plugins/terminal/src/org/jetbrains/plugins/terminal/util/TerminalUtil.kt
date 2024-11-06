2c37bc885077fcd6300894f59fc15e5e4642dc1b
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2024-11-06 12:16:06 +0100

    [terminal] stop TTY reading only after process termination to destroy Bash 5 session correctly (IJPL-166302)
    GitOrigin-RevId: 7864f227b19ff92b534d0b0ff55b1271cd769590

--- plugins/terminal/src/org/jetbrains/plugins/terminal/util/TerminalUtil.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/util/TerminalUtil.kt
@@ -2,9 +2,21 @@
 package org.jetbrains.plugins.terminal.util
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
 import com.intellij.openapi.util.Disposer
+import com.jediterm.terminal.TtyConnector
 import com.jediterm.terminal.model.TerminalModelListener
 import com.jediterm.terminal.model.TerminalTextBuffer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.time.withTimeout
+import org.jetbrains.plugins.terminal.ShellTerminalWidget
+import java.time.Duration
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.time.toKotlinDuration
 
 internal fun TerminalTextBuffer.addModelListener(parentDisposable: Disposable, listener: TerminalModelListener) {
   addModelListener(listener)
@@ -12,3 +24,48 @@ internal fun TerminalTextBuffer.addModelListener(parentDisposable: Disposable, l
     removeModelListener(listener)
   }
 }
+
+internal fun TtyConnector.waitFor(timeout: Duration, callback: () -> Unit) {
+  if (!this.isConnected) {
+    callback()
+    return
+  }
+  val processTtyConnector = ShellTerminalWidget.getProcessTtyConnector(this)
+  if (processTtyConnector != null) {
+    val done = AtomicBoolean(false)
+    val onceCallback = {
+      if (done.compareAndSet(false, true)) {
+        callback()
+      }
+    }
+    val onExit = processTtyConnector.process.onExit()
+    val job = service<InternalTerminalCoroutineService>().coroutineScope.launch(Dispatchers.IO) {
+      delay(timeout.toKotlinDuration())
+      onExit.cancel(false)
+      onceCallback()
+    }
+    onExit.whenComplete { _, _ ->
+      onceCallback()
+      job.cancel()
+    }
+  }
+  else {
+    val connector = this
+    service<InternalTerminalCoroutineService>().coroutineScope.launch(Dispatchers.IO) {
+      try {
+        withTimeout(timeout) {
+          connector.waitFor()
+        }
+      }
+      finally {
+        callback()
+      }
+    }
+  }
+}
+
+@Service(Service.Level.APP)
+private class InternalTerminalCoroutineService(val coroutineScope: CoroutineScope)
+
+@JvmField
+internal val STOP_EMULATOR_TIMEOUT: Duration = Duration.ofMillis(1500)
