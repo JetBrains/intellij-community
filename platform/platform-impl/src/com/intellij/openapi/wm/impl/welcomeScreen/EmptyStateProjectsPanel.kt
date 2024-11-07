a6b3c02a41a88582ee4c761f17a880ef3afc8229
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2024-11-07 15:37:26 +0100

    PY-75549: Refactor code to move methods to the top level.
    This change is required for the next change
    
    GitOrigin-RevId: a72b3c713f425b9dd95a0d3e83e1d91ac708b210

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/EmptyStateProjectsPanel.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/EmptyStateProjectsPanel.kt
@@ -45,131 +45,131 @@ internal class EmptyStateProjectsPanel(parentDisposable: Disposable) : BorderLay
     mainPanel.add(Wrapper(FlowLayout(), moreToolbar.component))
     addToCenter(mainPanel)
   }
+}
 
-  // Returns main actions, more actions
-  private fun createActionToolbars(parentDisposable: Disposable): Pair<ActionToolbarImpl, ActionToolbarImpl> {
-    val actionManager = ActionManager.getInstance()
-    val baseGroup = actionManager.getAction(IdeActions.GROUP_WELCOME_SCREEN_QUICKSTART_EMPTY_STATE) as ActionGroup
-    val moreActionGroup = DefaultActionGroup(IdeBundle.message("welcome.screen.more.actions.link.text"), true)
+// Returns main actions, more actions
+private fun createActionToolbars(parentDisposable: Disposable): Pair<ActionToolbarImpl, ActionToolbarImpl> {
+  val actionManager = ActionManager.getInstance()
+  val baseGroup = actionManager.getAction(IdeActions.GROUP_WELCOME_SCREEN_QUICKSTART_EMPTY_STATE) as ActionGroup
+  val moreActionGroup = DefaultActionGroup(IdeBundle.message("welcome.screen.more.actions.link.text"), true)
 
-    val toolbarGroup = object : ActionGroupWrapper(baseGroup) {
-      override fun postProcessVisibleChildren(e: AnActionEvent, visibleChildren: List<AnAction>): List<AnAction> {
-        moreActionGroup.removeAll()
-        val mapped = visibleChildren.mapIndexedNotNull { index, action ->
-          when {
-            index >= getWelcomeScreenPrimaryButtonsNum() -> {
-              moreActionGroup.add(action)
-              null
-            }
-            action is ActionGroup && action is ActionsWithPanelProvider -> {
-              val p = e.updateSession.presentation(action)
-              val wrapper = p.getClientProperty(ActionUtil.INLINE_ACTIONS)?.first()
-                            ?: ActionGroupPanelWrapper.wrapGroups(action, parentDisposable).also {
-                              p.putClientProperty(ActionUtil.INLINE_ACTIONS, listOf(it))
-                            }
-              e.updateSession.presentation(wrapper)
-              wrapper
-            }
-            else -> action
+  val toolbarGroup = object : ActionGroupWrapper(baseGroup) {
+    override fun postProcessVisibleChildren(e: AnActionEvent, visibleChildren: List<AnAction>): List<AnAction> {
+      moreActionGroup.removeAll()
+      val mapped = visibleChildren.mapIndexedNotNull { index, action ->
+        when {
+          index >= getWelcomeScreenPrimaryButtonsNum() -> {
+            moreActionGroup.add(action)
+            null
           }
+          action is ActionGroup && action is ActionsWithPanelProvider -> {
+            val p = e.updateSession.presentation(action)
+            val wrapper = p.getClientProperty(ActionUtil.INLINE_ACTIONS)?.first()
+                          ?: ActionGroupPanelWrapper.wrapGroups(action, parentDisposable).also {
+                            p.putClientProperty(ActionUtil.INLINE_ACTIONS, listOf(it))
+                          }
+            e.updateSession.presentation(wrapper)
+            wrapper
+          }
+          else -> action
         }
-        mapped.forEach { action ->
-          e.updateSession.presentation(action).putClientProperty(
-            ActionUtil.COMPONENT_PROVIDER, WelcomeScreenActionsUtil.createBigIconWithTextAction(action))
-        }
-        moreActionGroup.templatePresentation.text = when {
-          moreActionGroup.childrenCount == 1 ->
-            e.updateSession.presentation(moreActionGroup.getChildren(e.actionManager)[0]).text
-          else -> IdeBundle.message("welcome.screen.more.actions.link.text")
-        }
-        return mapped
       }
+      mapped.forEach { action ->
+        e.updateSession.presentation(action).putClientProperty(
+          ActionUtil.COMPONENT_PROVIDER, WelcomeScreenActionsUtil.createBigIconWithTextAction(action))
+      }
+      moreActionGroup.templatePresentation.text = when {
+        moreActionGroup.childrenCount == 1 ->
+          e.updateSession.presentation(moreActionGroup.getChildren(e.actionManager)[0]).text
+        else -> IdeBundle.message("welcome.screen.more.actions.link.text")
+      }
+      return mapped
     }
-    val actionsToolbar: ActionToolbarImpl = createActionsToolbar(toolbarGroup)
-
-    moreActionGroup.templatePresentation.putClientProperty(ActionUtil.COMPONENT_PROVIDER, object : CustomComponentAction {
-      val ENABLED = Key.create<Boolean>("ENABLED")
-      var alienUpdateScheduled = false
-      override fun createCustomComponent(presentation: Presentation, place: String): JComponent {
-        return object : DropDownLink<String>(presentation.text, { link: DropDownLink<String> ->
-          JBPopupFactory.getInstance().createActionGroupPopup(
-            null, moreActionGroup, DataManager.getInstance().getDataContext(link),
-            JBPopupFactory.ActionSelectionAid.SPEEDSEARCH, true)
-        }) {
-          override fun performAction() {
-            if (moreActionGroup.childrenCount == 1) {
-              WelcomeScreenActionsUtil.performAnActionForComponent(moreActionGroup.getChildren(actionManager)[0], this)
-            }
-            else {
-              super.performAction()
-            }
-          }
-        }
-      }
-
-      override fun updateCustomComponent(component: JComponent, presentation: Presentation) {
-        component as? DropDownLink<*> ?: return
-        val single = moreActionGroup.getChildren(actionManager).singleOrNull()
-        if (!alienUpdateScheduled) {
-          alienUpdateScheduled = true
-          (single as? OpenAlienProjectAction)?.scheduleUpdate { enabled ->
-            component.isVisible = enabled
-            presentation.putClientProperty(ENABLED, enabled)
-          }
-        }
-        component.isVisible = moreActionGroup.childrenCount > 0 &&
-                              presentation.getClientProperty(ENABLED) != false
-        component.text = moreActionGroup.templateText
-        if (single != null) component.icon = null
-        else component.setDropDownLinkIcon()
-      }
-    })
-    val moreToolbar = ActionToolbarImpl(ActionPlaces.WELCOME_SCREEN, DefaultActionGroup(moreActionGroup), true)
-    moreToolbar.setBorder(JBUI.Borders.emptyTop(5))
-    moreToolbar.targetComponent = moreToolbar.component
-    moreToolbar.isOpaque = false
-    return Pair(actionsToolbar, moreToolbar)
   }
+  val actionsToolbar: ActionToolbarImpl = createActionsToolbar(toolbarGroup)
 
-  private fun createActionsToolbar(actionGroup: ActionGroup): ActionToolbarImpl {
-    val actionToolbar: ActionToolbarImpl = object : ActionToolbarImpl(ActionPlaces.WELCOME_SCREEN, actionGroup, true) {
-      private var wasFocusRequested = false
-
-      override fun isSecondaryAction(action: AnAction, actionIndex: Int): Boolean {
-        return actionIndex >= getWelcomeScreenPrimaryButtonsNum()
-      }
-
-      override fun actionsUpdated(forced: Boolean, newVisibleActions: MutableList<out AnAction>) {
-        super.actionsUpdated(forced, newVisibleActions)
-        if (forced && !newVisibleActions.isEmpty() && componentCount > 0 && !wasFocusRequested) {
-          val obj = FocusUtil.findFocusableComponentIn(components[0], null)
-          if (obj != null) {
-            wasFocusRequested = true
-            IdeFocusManager.getGlobalInstance().requestFocus(obj, true)
+  moreActionGroup.templatePresentation.putClientProperty(ActionUtil.COMPONENT_PROVIDER, object : CustomComponentAction {
+    val ENABLED = Key.create<Boolean>("ENABLED")
+    var alienUpdateScheduled = false
+    override fun createCustomComponent(presentation: Presentation, place: String): JComponent {
+      return object : DropDownLink<String>(presentation.text, { link: DropDownLink<String> ->
+        JBPopupFactory.getInstance().createActionGroupPopup(
+          null, moreActionGroup, DataManager.getInstance().getDataContext(link),
+          JBPopupFactory.ActionSelectionAid.SPEEDSEARCH, true)
+      }) {
+        override fun performAction() {
+          if (moreActionGroup.childrenCount == 1) {
+            WelcomeScreenActionsUtil.performAnActionForComponent(moreActionGroup.getChildren(actionManager)[0], this)
+          }
+          else {
+            super.performAction()
           }
         }
       }
     }
-    actionToolbar.setLayoutStrategy(ToolbarLayoutStrategy.NOWRAP_STRATEGY)
-    actionToolbar.setTargetComponent(actionToolbar.component)
-    actionToolbar.setBorder(JBUI.Borders.emptyTop(27))
-    actionToolbar.setOpaque(false)
-    return actionToolbar
-  }
 
-  private fun createTitle(): JBLabel {
-    val titleLabel = JBLabel(WelcomeScreenComponentFactory.getApplicationTitle(), SwingConstants.CENTER)
-    titleLabel.setOpaque(false)
-    val componentFont = titleLabel.getFont()
-    titleLabel.setFont(componentFont.deriveFont(componentFont.getSize() + scale(13).toFloat()).deriveFont(Font.BOLD))
-    titleLabel.setBorder(JBUI.Borders.emptyBottom(17))
-    return titleLabel
-  }
+    override fun updateCustomComponent(component: JComponent, presentation: Presentation) {
+      component as? DropDownLink<*> ?: return
+      val single = moreActionGroup.getChildren(actionManager).singleOrNull()
+      if (!alienUpdateScheduled) {
+        alienUpdateScheduled = true
+        (single as? OpenAlienProjectAction)?.scheduleUpdate { enabled ->
+          component.isVisible = enabled
+          presentation.putClientProperty(ENABLED, enabled)
+        }
+      }
+      component.isVisible = moreActionGroup.childrenCount > 0 &&
+                            presentation.getClientProperty(ENABLED) != false
+      component.text = moreActionGroup.templateText
+      if (single != null) component.icon = null
+      else component.setDropDownLinkIcon()
+    }
+  })
+  val moreToolbar = ActionToolbarImpl(ActionPlaces.WELCOME_SCREEN, DefaultActionGroup(moreActionGroup), true)
+  moreToolbar.setBorder(JBUI.Borders.emptyTop(5))
+  moreToolbar.targetComponent = moreToolbar.component
+  moreToolbar.isOpaque = false
+  return Pair(actionsToolbar, moreToolbar)
+}
 
-  fun createCommentLabel(text: @NlsContexts.HintText String): JBLabel {
-    val commentFirstLabel = JBLabel(text, SwingConstants.CENTER)
-    commentFirstLabel.setOpaque(false)
-    commentFirstLabel.setForeground(UIUtil.getContextHelpForeground())
-    return commentFirstLabel
+private fun createActionsToolbar(actionGroup: ActionGroup): ActionToolbarImpl {
+  val actionToolbar: ActionToolbarImpl = object : ActionToolbarImpl(ActionPlaces.WELCOME_SCREEN, actionGroup, true) {
+    private var wasFocusRequested = false
+
+    override fun isSecondaryAction(action: AnAction, actionIndex: Int): Boolean {
+      return actionIndex >= getWelcomeScreenPrimaryButtonsNum()
+    }
+
+    override fun actionsUpdated(forced: Boolean, newVisibleActions: MutableList<out AnAction>) {
+      super.actionsUpdated(forced, newVisibleActions)
+      if (forced && !newVisibleActions.isEmpty() && componentCount > 0 && !wasFocusRequested) {
+        val obj = FocusUtil.findFocusableComponentIn(components[0], null)
+        if (obj != null) {
+          wasFocusRequested = true
+          IdeFocusManager.getGlobalInstance().requestFocus(obj, true)
+        }
+      }
+    }
   }
+  actionToolbar.setLayoutStrategy(ToolbarLayoutStrategy.NOWRAP_STRATEGY)
+  actionToolbar.setTargetComponent(actionToolbar.component)
+  actionToolbar.setBorder(JBUI.Borders.emptyTop(27))
+  actionToolbar.setOpaque(false)
+  return actionToolbar
+}
+
+private fun createTitle(): JBLabel {
+  val titleLabel = JBLabel(WelcomeScreenComponentFactory.getApplicationTitle(), SwingConstants.CENTER)
+  titleLabel.setOpaque(false)
+  val componentFont = titleLabel.getFont()
+  titleLabel.setFont(componentFont.deriveFont(componentFont.getSize() + scale(13).toFloat()).deriveFont(Font.BOLD))
+  titleLabel.setBorder(JBUI.Borders.emptyBottom(17))
+  return titleLabel
+}
+
+fun createCommentLabel(text: @NlsContexts.HintText String): JBLabel {
+  val commentFirstLabel = JBLabel(text, SwingConstants.CENTER)
+  commentFirstLabel.setOpaque(false)
+  commentFirstLabel.setForeground(UIUtil.getContextHelpForeground())
+  return commentFirstLabel
 }
