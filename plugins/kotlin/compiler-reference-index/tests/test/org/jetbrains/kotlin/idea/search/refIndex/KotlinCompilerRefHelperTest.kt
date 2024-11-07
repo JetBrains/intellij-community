043d0aa82a0bfa225e08f0f2b4118192da2ca8b1
Anna Kozlova <anna.kozlova@jetbrains.com>
2024-11-07 11:00:46 +0100

    [kotlin] compiler index: add KtEnumEntry treatment
    ^KTIJ-32058 fixed
    
    GitOrigin-RevId: 1d1d5e2fd78ec00e7a77fdf7e340f65d29272069

--- plugins/kotlin/compiler-reference-index/tests/test/org/jetbrains/kotlin/idea/search/refIndex/KotlinCompilerRefHelperTest.kt
+++ plugins/kotlin/compiler-reference-index/tests/test/org/jetbrains/kotlin/idea/search/refIndex/KotlinCompilerRefHelperTest.kt
@@ -4,7 +4,10 @@ package org.jetbrains.kotlin.idea.search.refIndex
 import com.intellij.compiler.CompilerReferenceService
 import com.intellij.java.compiler.CompilerReferencesTestBase
 import com.intellij.psi.CommonClassNames
+import com.intellij.psi.util.descendantsOfType
 import com.intellij.testFramework.SkipSlowTestLocally
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtEnumEntry
 
 @SkipSlowTestLocally
 open class KotlinCompilerRefHelperTest : CompilerReferencesTestBase() {
@@ -56,6 +59,45 @@ open class KotlinCompilerRefHelperTest : CompilerReferencesTestBase() {
         assertTrue(javaFile.virtualFile in scope) // dirty and with reference
         assertTrue("non-java files must be presented in scope", kotlinFile.virtualFile in scope) // not java and dirty
         assertTrue("non-java files must be presented in scope", textFile.virtualFile in scope) // not java and dirty
+    }
+
+    fun `test enum scope`() {
+        installCompiler()
+        val javaFile = myFixture.addFileToProject(
+            "Main.java", """
+            public class Main {
+    public static void main(String[] args) {
+        JustKotlinEnum justKotlinEnum = JustKotlinEnum.JustFirstValue;
+    }
+}
+        """.trimIndent()
+        )
+
+        val kotlinFile = myFixture.addFileToProject(
+            "JustKotlinEnum.kt", """
+                enum class JustKotlinEnum {
+    JustFirstValue
+}""".trimIndent()
+        )
+        val ktClass = kotlinFile.descendantsOfType<KtClass>().firstOrNull() ?: error("ktClass not found")
+        assertTrue(ktClass.isEnum())
+        val ktEnumEntry = ktClass.descendantsOfType<KtEnumEntry>().firstOrNull() ?: error("ktEnumEntry not found")
+        rebuildProject()
+
+        val scope = CompilerReferenceService.getInstance(this.project)
+            .getScopeWithCodeReferences(ktEnumEntry)
+            ?: error("Scope not found")
+        assertTrue("file with references must be in scope ", javaFile.virtualFile in scope) // with reference
+        assertTrue("non-java files must be presented in scope", kotlinFile.virtualFile in scope) // not java
+
+
+        val newName = "JustFirstValue1"
+        myFixture.renameElement(ktEnumEntry, newName)
+
+        assertNull(
+            "Dirty scope", CompilerReferenceService.getInstance(project)
+                .getScopeWithCodeReferences(kotlinFile)
+        )
     }
 
     private fun findScope() = CompilerReferenceService.getInstance(project)
