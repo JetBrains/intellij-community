e694ef46dddbe3de5012caea670fd7e110833364
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2024-11-08 16:50:24 +0100

    IJPL-165414: vcs marker: Rework the popup opening
    Delegate the popup building and opening to the `LineStatusMarkerPopupService` and control that opened popup is disposed
    before creating the new marker panel
    
    GitOrigin-RevId: fff881f00d75ce4d4b9fa0fc5fa2e2407f7e8813

--- platform/diff-impl/src/com/intellij/openapi/vcs/ex/LineStatusMarkerPopupService.kt
+++ platform/diff-impl/src/com/intellij/openapi/vcs/ex/LineStatusMarkerPopupService.kt
@@ -19,7 +19,10 @@ import com.intellij.openapi.editor.event.*
 import com.intellij.openapi.fileEditor.FileEditorManagerEvent
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.util.Disposer
-import com.intellij.ui.*
+import com.intellij.ui.EditorTextComponent
+import com.intellij.ui.HintHint
+import com.intellij.ui.HintListener
+import com.intellij.ui.LightweightHint
 import com.intellij.util.concurrency.annotations.RequiresEdt
 import com.intellij.util.ui.UIUtil
 import java.awt.Point
@@ -28,11 +31,29 @@ import java.awt.event.ComponentEvent
 import java.awt.event.MouseAdapter
 
 @Service(Service.Level.APP)
-internal class LineStatusMarkerPopupService {
-  private var lastKnownHint: Hint? = null
+class LineStatusMarkerPopupService {
+  private var activePopupDisposable: Disposable? = null
 
   @RequiresEdt
-  fun showPopupAt(
+  fun buildAndShowPopup(parentDisposable: Disposable, editor: Editor, mousePosition: Point?, builder: (popupDisposable: Disposable) -> LineStatusMarkerPopupPanel) {
+    val popupDisposable = getNextPopupDisposable(parentDisposable)
+    val markerPopupPanel = builder(popupDisposable)
+    showPopupAt(editor, markerPopupPanel, mousePosition, popupDisposable)
+  }
+
+  private fun getNextPopupDisposable(parentDisposable: Disposable): Disposable {
+    closeActivePopup()
+    activePopupDisposable = Disposer.newDisposable(parentDisposable, "LineStatusMarkerPopup")
+
+    return activePopupDisposable!!
+  }
+
+  fun closeActivePopup() {
+    if (activePopupDisposable != null) Disposer.dispose(activePopupDisposable!!)
+  }
+
+  @RequiresEdt
+  private fun showPopupAt(
     editor: Editor,
     panel: LineStatusMarkerPopupPanel,
     mousePosition: Point?,
@@ -42,15 +63,13 @@ internal class LineStatusMarkerPopupService {
     Disposer.register(popupDisposable, Disposable {
       UIUtil.invokeLaterIfNeeded {
         hint.hide()
-        resetLastHint()
       }
     })
-    hint.addHintListener(HintListener { Disposer.dispose(popupDisposable) })
+    hint.addHintListener(HintListener { Disposer.dispose(popupDisposable) }) // doesn't need it anymore?
     hint.setForceLightweightPopup(true)
 
     // if there are no listeners, events are passed to the top level
     panel.addMouseListener(object : MouseAdapter() {})
-
 
     val line = editor.getCaretModel().logicalPosition.line
     val point = HintManagerImpl.getHintPosition(hint, editor, LogicalPosition(line, 0), HintManager.UNDER)
@@ -63,30 +82,12 @@ internal class LineStatusMarkerPopupService {
     point.x -= panel.editorTextOffset // align a main editor with the one in popup
 
     trackInnerEditorResizing(panel, hint, popupDisposable)
-    setupEditorListeners(editor, popupDisposable, hint)
-
-    beforeShowNewHint(hint)
+    setupEditorListeners(editor, popupDisposable)
     showHint(hint, editor, point)
 
     if (!hint.isVisible()) {
       Disposer.dispose(popupDisposable)
     }
-  }
-
-  internal fun hidePopup() {
-    lastKnownHint?.hide()
-  }
-
-  private fun beforeShowNewHint(newHint: LightweightHint) {
-    if (lastKnownHint != null) {
-      lastKnownHint!!.hide()
-      resetLastHint()
-    }
-    lastKnownHint = newHint
-  }
-
-  private fun resetLastHint() {
-    lastKnownHint = null
   }
 
   private fun showHint(hint: LightweightHint, editor: Editor, point: Point) {
@@ -121,26 +122,26 @@ internal class LineStatusMarkerPopupService {
       })
     }
 
-    private fun setupEditorListeners(editor: Editor, popupDisposable: Disposable, hint: LightweightHint) {
-      trackFileEditorChange(popupDisposable, hint)
-      trackDocumentChange(editor, popupDisposable, hint)
-      trackSelection(editor, popupDisposable, hint)
-      trackCaretPosition(editor, popupDisposable, hint)
-      trackScrolling(editor, popupDisposable, hint)
-      trackMouseClick(editor, popupDisposable, hint)
-      trackEditorLookup(editor, popupDisposable, hint)
+    private fun setupEditorListeners(editor: Editor, popupDisposable: Disposable) {
+      trackFileEditorChange(popupDisposable)
+      trackDocumentChange(editor, popupDisposable)
+      trackSelection(editor, popupDisposable)
+      trackCaretPosition(editor, popupDisposable)
+      trackScrolling(editor, popupDisposable)
+      trackMouseClick(editor, popupDisposable)
+      trackEditorLookup(editor, popupDisposable)
     }
 
-    private fun trackFileEditorChange(popupDisposable: Disposable, hint: LightweightHint) {
+    private fun trackFileEditorChange(popupDisposable: Disposable) {
       ApplicationManager.getApplication().getMessageBus().connect(popupDisposable)
         .subscribe<FileEditorManagerListener>(FileEditorManagerListener.FILE_EDITOR_MANAGER, object : FileEditorManagerListener {
           override fun selectionChanged(event: FileEditorManagerEvent) {
-            hint.hide()
+            closeActivePopup()
           }
         })
     }
 
-    private fun trackDocumentChange(editor: Editor, popupDisposable: Disposable, hint: LightweightHint) {
+    private fun trackDocumentChange(editor: Editor, popupDisposable: Disposable) {
       editor.getDocument().addDocumentListener(object : BulkAwareDocumentListener {
         override fun documentChangedNonBulk(event: DocumentEvent) {
           if (event.getOldLength() != 0 || event.getNewLength() != 0) onDocumentChange()
@@ -151,48 +152,48 @@ internal class LineStatusMarkerPopupService {
         }
 
         fun onDocumentChange() {
-          hint.hide()
+          closeActivePopup()
         }
       }, popupDisposable)
     }
 
-    private fun trackSelection(editor: Editor, popupDisposable: Disposable, hint: LightweightHint) {
+    private fun trackSelection(editor: Editor, popupDisposable: Disposable) {
       editor.getSelectionModel().addSelectionListener(object : SelectionListener {
         override fun selectionChanged(e: SelectionEvent) {
-          hint.hide()
+          closeActivePopup()
         }
       }, popupDisposable)
     }
 
-    private fun trackCaretPosition(editor: Editor, popupDisposable: Disposable, hint: LightweightHint) {
+    private fun trackCaretPosition(editor: Editor, popupDisposable: Disposable) {
       editor.getCaretModel().addCaretListener(object : CaretListener {
         override fun caretPositionChanged(event: CaretEvent) {
-          hint.hide()
+          closeActivePopup()
         }
       }, popupDisposable)
     }
 
-    private fun trackScrolling(editor: Editor, popupDisposable: Disposable, hint: LightweightHint) {
+    private fun trackScrolling(editor: Editor, popupDisposable: Disposable) {
       editor.getScrollingModel().addVisibleAreaListener(object : VisibleAreaListener {
         override fun visibleAreaChanged(e: VisibleAreaEvent) {
           val old = e.oldRectangle
           val new = e.newRectangle
           if (old != null && old.x == new.x && old.y == new.y) return
 
-          hint.hide()
+          closeActivePopup()
         }
       }, popupDisposable)
     }
 
-    private fun trackMouseClick(editor: Editor, popupDisposable: Disposable, hint: LightweightHint) {
+    private fun trackMouseClick(editor: Editor, popupDisposable: Disposable) {
       editor.addEditorMouseListener(object : EditorMouseListener {
         override fun mousePressed(event: EditorMouseEvent) {
-          hint.hide()
+          closeActivePopup()
         }
       }, popupDisposable)
     }
 
-    private fun trackEditorLookup(editor: Editor, popupDisposable: Disposable, hint: LightweightHint) {
+    private fun trackEditorLookup(editor: Editor, popupDisposable: Disposable) {
       val project = editor.project
       if (project != null) LookupManager.hideActiveLookup(project) // reset current
 
@@ -201,7 +202,7 @@ internal class LineStatusMarkerPopupService {
           override fun hintShown(sourceEditor: Editor, editorHint: LightweightHint, flags: Int, hintInfo: HintHint) {
             if (sourceEditor !== editor) return
             if (editorHint is Lookup) {
-              hint.hide()
+              closeActivePopup()
             }
           }
         })
@@ -214,13 +215,17 @@ internal class LineStatusMarkerPopupService {
   }
 }
 
+private fun closeActivePopup() {
+  LineStatusMarkerPopupService.instance.closeActivePopup()
+}
+
 private class LineStatusMakerEscEditorHandler(private val delegate: EditorActionHandler) : EditorActionHandler() {
   override fun isEnabledForCaret(editor: Editor, caret: Caret, dataContext: DataContext?): Boolean {
     return delegate.isEnabled(editor, caret, dataContext)
   }
 
   override fun doExecute(editor: Editor, caret: Caret?, dataContext: DataContext?) {
-    LineStatusMarkerPopupService.instance.hidePopup()
+    closeActivePopup()
     delegate.execute(editor, caret, dataContext)
   }
 }
