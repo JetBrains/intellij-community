9c16be438e86f12397e8d4150827b08a1c71ac8b
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-11-08 16:16:27 +0900

    Create base dialog for preview screen

---
+++ plugins/kotlin/j2k/shared/src/org/jetbrains/kotlin/nj2k/gui/previewer/Previewer.kt
@@ -0,0 +1,20 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.nj2k.gui.previewer
+
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.ui.dsl.builder.panel
+import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
+import javax.swing.JComponent
+
+// 変換プレビュー
+class Previewer() : DialogWrapper(true) {
+
+    init {
+        title = KotlinBundle.message("action.j2k.gui.title")
+        init()
+    }
+
+    override fun createCenterPanel(): JComponent {
+        return panel {  }
+    }
+}
