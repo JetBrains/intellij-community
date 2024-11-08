2e8d12cd58f6b290b56cc463590ecf54db5fd2b6
Alexander Kuznetsov <aleksander.kuznetsov@jetbrains.com>
2024-11-08 14:28:25 +0100

    Cleanup: remove unused class
    GitOrigin-RevId: 847b4d6f4ea3dc6c3be3bfe5a20098bae3d4028b

--- java/debugger/impl/src/com/intellij/debugger/engine/evaluation/EvaluationUtil.java
+++
@@ -1,7 +0,0 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.debugger.engine.evaluation;
-
-public class EvaluationUtil {
-
-  public static void createExp() {}
-}
