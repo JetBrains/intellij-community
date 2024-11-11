394fd9081f2715e7121d242655d2ed31a2953fc5
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2024-11-11 12:18:43 +0100

    Convert VcsFreezingProcess to kt
    GitOrigin-RevId: c97009ac8990b330466097dc4447f59a9b7f9432

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/VcsFreezingProcess.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/VcsFreezingProcess.kt
@@ -1,98 +1,89 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.openapi.vcs.changes;
+package com.intellij.openapi.vcs.changes
 
-import com.intellij.configurationStore.StoreReloadManager;
-import com.intellij.ide.SaveAndSyncHandler;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.VcsBundle;
-import com.intellij.util.messages.Topic;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.configurationStore.StoreReloadManager.Companion.getInstance
+import com.intellij.ide.SaveAndSyncHandler.Companion.getInstance
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.VcsBundle
+import com.intellij.util.messages.Topic
+import org.jetbrains.annotations.Nls
 
 /**
  * Executes an action surrounding it with freezing-unfreezing of the ChangeListManager
  * and blocking/unblocking save/sync on frame de/activation.
  */
-public class VcsFreezingProcess {
-  private static final Logger LOG = Logger.getInstance(VcsFreezingProcess.class);
+open class VcsFreezingProcess(private val myProject: Project, private val myOperationTitle: @Nls String, private val myRunnable: Runnable) {
+  private val myChangeListManager: ChangeListManagerEx = ChangeListManagerEx.getInstanceEx(myProject)
 
-  @NotNull private final Project myProject;
-  @NotNull private final String myOperationTitle;
-  @NotNull private final Runnable myRunnable;
-
-  @NotNull private final ChangeListManagerEx myChangeListManager;
-
-  public VcsFreezingProcess(@NotNull Project project, @NotNull @Nls String operationTitle, @NotNull Runnable runnable) {
-    myProject = project;
-    myOperationTitle = operationTitle;
-    myRunnable = runnable;
-
-    myChangeListManager = ChangeListManagerEx.getInstanceEx(project);
-  }
-
-  public void execute() {
-    LOG.debug("starting");
+  open fun execute() {
+    LOG.debug("starting")
     try {
-      LOG.debug("saving documents, blocking project autosync");
-      saveAndBlockInAwt();
+      LOG.debug("saving documents, blocking project autosync")
+      saveAndBlockInAwt()
       try {
-        LOG.debug("freezing the ChangeListManager");
-        freeze();
-        LOG.debug("running the operation");
-        myRunnable.run();
-        LOG.debug("operation completed.");
+        LOG.debug("freezing the ChangeListManager")
+        freeze()
+        LOG.debug("running the operation")
+        myRunnable.run()
+        LOG.debug("operation completed.")
       }
       finally {
-        LOG.debug("unfreezing the ChangeListManager");
-        unfreeze();
+        LOG.debug("unfreezing the ChangeListManager")
+        unfreeze()
       }
     }
     finally {
-      LOG.debug("unblocking project autosync");
-      unblockInAwt();
+      LOG.debug("unblocking project autosync")
+      unblockInAwt()
     }
-    LOG.debug("finished.");
+    LOG.debug("finished.")
   }
 
-  private void saveAndBlockInAwt() {
-    ApplicationManager.getApplication().invokeAndWait(() -> {
-      StoreReloadManager.Companion.getInstance(myProject).blockReloadingProjectOnExternalChanges();
-      FileDocumentManager.getInstance().saveAllDocuments();
+  private fun saveAndBlockInAwt() {
+    ApplicationManager.getApplication().invokeAndWait(Runnable {
+      getInstance(myProject).blockReloadingProjectOnExternalChanges()
+      FileDocumentManager.getInstance().saveAllDocuments()
 
-      SaveAndSyncHandler saveAndSyncHandler = SaveAndSyncHandler.getInstance();
-      saveAndSyncHandler.blockSaveOnFrameDeactivation();
-      saveAndSyncHandler.blockSyncOnFrameActivation();
-    });
+      val saveAndSyncHandler = getInstance()
+      saveAndSyncHandler.blockSaveOnFrameDeactivation()
+      saveAndSyncHandler.blockSyncOnFrameActivation()
+    })
   }
 
-  private void unblockInAwt() {
-    ApplicationManager.getApplication().invokeAndWait(() -> {
-      StoreReloadManager.Companion.getInstance(myProject).unblockReloadingProjectOnExternalChanges();
-
-      SaveAndSyncHandler saveAndSyncHandler = SaveAndSyncHandler.getInstance();
-      saveAndSyncHandler.unblockSaveOnFrameDeactivation();
-      saveAndSyncHandler.unblockSyncOnFrameActivation();
-    });
+  private fun unblockInAwt() {
+    ApplicationManager.getApplication().invokeAndWait(Runnable {
+      getInstance(myProject).unblockReloadingProjectOnExternalChanges()
+      val saveAndSyncHandler = getInstance()
+      saveAndSyncHandler.unblockSaveOnFrameDeactivation()
+      saveAndSyncHandler.unblockSyncOnFrameActivation()
+    })
   }
 
-  private void freeze() {
-    myProject.getMessageBus().syncPublisher(Listener.TOPIC).onFreeze();
-    myChangeListManager.freeze(VcsBundle.message("local.changes.freeze.message", myOperationTitle));
+  private fun freeze() {
+    myProject.getMessageBus().syncPublisher<Listener>(Listener.TOPIC).onFreeze()
+    myChangeListManager.freeze(VcsBundle.message("local.changes.freeze.message", myOperationTitle))
   }
 
-  private void unfreeze() {
-    myProject.getMessageBus().syncPublisher(Listener.TOPIC).onUnfreeze();
-    myChangeListManager.unfreeze();
+  private fun unfreeze() {
+    myProject.getMessageBus().syncPublisher<Listener>(Listener.TOPIC).onUnfreeze()
+    myChangeListManager.unfreeze()
   }
 
-  public interface Listener {
-    Topic<Listener> TOPIC = Topic.create("Change List Manager Freeze", Listener.class);
+  interface Listener {
+    fun onFreeze() {}
 
-    default void onFreeze() {}
+    fun onUnfreeze() {}
 
-    default void onUnfreeze() {}
+    companion object {
+      @JvmField
+      val TOPIC: Topic<Listener> = Topic.create<Listener>("Change List Manager Freeze", Listener::class.java)
+    }
+  }
+
+  private companion object {
+    private val LOG = Logger.getInstance(VcsFreezingProcess::class.java)
   }
 }
