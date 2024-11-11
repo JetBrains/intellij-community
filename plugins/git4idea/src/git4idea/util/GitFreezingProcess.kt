fe89123666bd3dbf521bfcfab9e598827210fa1b
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2024-11-11 13:13:58 +0100

    Convert GitFreezingProcess to kt
    GitOrigin-RevId: daa2e41cb0ce51ba77f965a0d7b02a14fe6ca024

--- plugins/git4idea/src/git4idea/util/GitFreezingProcess.kt
+++ plugins/git4idea/src/git4idea/util/GitFreezingProcess.kt
@@ -1,17 +1,13 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package git4idea.util;
+package git4idea.util
 
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.changes.VcsFreezingProcess;
-import git4idea.i18n.GitBundle;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.changes.VcsFreezingProcess
+import git4idea.i18n.GitBundle
+import org.jetbrains.annotations.Nls
 
 /**
  * @see VcsFreezingProcess
  */
-public class GitFreezingProcess extends VcsFreezingProcess {
-  public GitFreezingProcess(@NotNull Project project, @NotNull @Nls String operationTitle, @NotNull Runnable runnable) {
-    super(project, GitBundle.message("local.changes.freeze.message.git.operation.prefix", operationTitle), runnable);
-  }
-}
+class GitFreezingProcess(project: Project, operationTitle: @Nls String, runnable: Runnable) :
+  VcsFreezingProcess(project, GitBundle.message("local.changes.freeze.message.git.operation.prefix", operationTitle), runnable)
