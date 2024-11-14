467736512041d4a68d773371d28c8a05c3c23571
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2024-11-14 17:19:51 +0100

    [kotlin] increase `kotlin.project.configuration` statistics collector version
    as there were changes in `org.jetbrains.kotlin.config.LanguageFeature` in Kotlin compiler
    
    GitOrigin-RevId: c776df0f036b559b06d4c012b763036bb88c4e3f

--- plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/statistics/ProjectConfigurationCollector.kt
+++ plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/statistics/ProjectConfigurationCollector.kt
@@ -61,7 +61,7 @@ internal class ProjectConfigurationCollector : ProjectUsagesCollector() {
         }
     }
 
-    private val GROUP = EventLogGroup("kotlin.project.configuration", 13)
+    private val GROUP = EventLogGroup("kotlin.project.configuration", 14)
 
     private val systemField = EventFields.String("system", listOf("JPS", "Maven", "Gradle", "unknown"))
     private val platformField = EventFields.String("platform", composePlatformFields())
