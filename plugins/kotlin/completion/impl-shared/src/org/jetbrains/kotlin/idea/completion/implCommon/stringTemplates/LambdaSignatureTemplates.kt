196c92d75e69a029aa18ba9ee9e040d0e8edf012
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2024-11-15 15:55:21 +0100

    [kotlin] KTIJ-32187, KTIJ-32178 trailing lambda template fully disabled
    IJ-CR-149484
    
    (cherry picked from commit 1622f0a0bf63651c86cb9892d9e453dc35ecf07f)
    (cherry picked from commit 9e5c6f21979a4b28b1d42b42452ebfb8e12bd6b9)
    
    GitOrigin-RevId: a4b55b87ce12ab257716f3cc05509a1bdbe5aafc

--- plugins/kotlin/completion/impl-shared/src/org/jetbrains/kotlin/idea/completion/implCommon/stringTemplates/LambdaSignatureTemplates.kt
+++ plugins/kotlin/completion/impl-shared/src/org/jetbrains/kotlin/idea/completion/implCommon/stringTemplates/LambdaSignatureTemplates.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.completion.implCommon.stringTemplates
 
-import com.intellij.codeInsight.daemon.impl.quickfix.EmptyExpression
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.codeInsight.template.Expression
@@ -14,11 +13,7 @@ import org.jetbrains.kotlin.analysis.api.renderer.base.annotations.KaRendererAnn
 import org.jetbrains.kotlin.analysis.api.renderer.types.KaTypeRenderer
 import org.jetbrains.kotlin.analysis.api.renderer.types.impl.KaTypeRendererForSource
 import org.jetbrains.kotlin.analysis.api.types.KaFunctionType
-import org.jetbrains.kotlin.analysis.api.types.KaType
-import org.jetbrains.kotlin.idea.base.codeInsight.KotlinNameSuggester
-import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.renderer.render
 
 @KaExperimentalApi
 private val NoAnnotationsTypeRenderer: KaTypeRenderer = KaTypeRendererForSource.WITH_QUALIFIED_NAMES.with {
@@ -36,64 +31,65 @@ fun Template.build(
     isToShortenLongNames = true
     // isToReformat = true //TODO
 
-    for (index in 0 until parametersCount) {
-        if (index == 0) {
-            addTextSegment("(")
-        }
-
-        addVariable(
-            /* expression = */ EmptyExpression(),
-            /* isAlwaysStopAt = */ true,
-        )
-
-        addTextSegment(if (index == parametersCount - 1) ")" else ", ")
-    }
-
-    val typeNames = mutableMapOf<KaType, MutableSet<String>>()
-    val kotlinNameSuggester = KotlinNameSuggester()
+    //for (index in 0 until parametersCount) {
+    //    if (index == 0) {
+    //        addTextSegment("(")
+    //    }
+    //
+    //    addVariable(
+    //        /* expression = */ EmptyExpression(),
+    //        /* isAlwaysStopAt = */ true,
+    //    )
+    //
+    //    addTextSegment(if (index == parametersCount - 1) ")" else ", ")
+    //}
+    //
+    //val typeNames = mutableMapOf<KaType, MutableSet<String>>()
+    //val kotlinNameSuggester = KotlinNameSuggester()
 
     addTextSegment(" { ")
-    val parameterTypes = trailingFunctionType.parameterTypes
-        .zip(suggestedParameterNames)
-    val iterator = parameterTypes.iterator()
-    while (iterator.hasNext()) {
-        val (parameterType, suggestedName) = iterator.next()
 
-        //TODO: check for names in scope
-        val validator = typeNames.getOrPut(parameterType) {
-            mutableSetOf()
-        }::add
-
-        val items = (suggestedName?.let { sequenceOf(it.render()) }
-            ?: kotlinNameSuggester.suggestTypeNames(parameterType))
-            .map { KotlinNameSuggester.suggestNameByName(it, validator) }
-            .toList()
-
-        addVariable(
-            /* expression = */ LambdaParameterExpression(items),
-            /* isAlwaysStopAt = */ true,
-        )
-
-        //parameterType.render(
-        //    renderer = NoAnnotationsTypeRenderer,
-        //    position = Variance.INVARIANT,
-        //).let { typeText ->
-        //    addTextSegment(KtTokens.COLON.value)
-        //    addTextSegment(" ")
-        //    addTextSegment(typeText)
-        //}
-
-        if (iterator.hasNext()) {
-            addTextSegment(KtTokens.COMMA.value)
-            addTextSegment(" ")
-        }
-    }
-
-    if (parameterTypes.isNotEmpty()) {
-        addTextSegment(" ")
-        addTextSegment(KtTokens.ARROW.value)
-        addTextSegment(" ")
-    }
+    //val parameterTypes = trailingFunctionType.parameterTypes
+    //    .zip(suggestedParameterNames)
+    //val iterator = parameterTypes.iterator()
+    //while (iterator.hasNext()) {
+    //    val (parameterType, suggestedName) = iterator.next()
+    //
+    //    //TODO: check for names in scope
+    //    val validator = typeNames.getOrPut(parameterType) {
+    //        mutableSetOf()
+    //    }::add
+    //
+    //    val items = (suggestedName?.let { sequenceOf(it.render()) }
+    //        ?: kotlinNameSuggester.suggestTypeNames(parameterType))
+    //        .map { KotlinNameSuggester.suggestNameByName(it, validator) }
+    //        .toList()
+    //
+    //    addVariable(
+    //        /* expression = */ LambdaParameterExpression(items),
+    //        /* isAlwaysStopAt = */ true,
+    //    )
+    //
+    //    parameterType.render(
+    //        renderer = NoAnnotationsTypeRenderer,
+    //        position = Variance.INVARIANT,
+    //    ).let { typeText ->
+    //        addTextSegment(KtTokens.COLON.value)
+    //        addTextSegment(" ")
+    //        addTextSegment(typeText)
+    //    }
+    //
+    //    if (iterator.hasNext()) {
+    //        addTextSegment(KtTokens.COMMA.value)
+    //        addTextSegment(" ")
+    //    }
+    //}
+    //
+    //if (parameterTypes.isNotEmpty()) {
+    //    addTextSegment(" ")
+    //    addTextSegment(KtTokens.ARROW.value)
+    //    addTextSegment(" ")
+    //}
     addEndVariable()
     addTextSegment(" }")
 }
