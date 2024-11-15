9dafa8fd3c7a2cb3d76348e04bcf6e6621e9ac50
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2024-11-15 16:43:40 +0100

    IJPL-171655 IJ-CR-149489 IJent WSL FS: Dirty hack: lazy initialization of `IjentFailSafeFileSystemPosixApi`
    (cherry picked from commit d04fd7e48c715bf3ef04670145bed99d242bb965)
    
    GitOrigin-RevId: bdd5322a29e7d8fff309b59e7c923202be302c21

--- platform/ijent/impl/src/com/intellij/platform/ijent/community/impl/IjentFailSafeFileSystemPosixApi.kt
+++ platform/ijent/impl/src/com/intellij/platform/ijent/community/impl/IjentFailSafeFileSystemPosixApi.kt
@@ -40,8 +40,7 @@ suspend fun IjentFailSafeFileSystemPosixApi(
   delegateFactory: suspend () -> IjentPosixApi,
 ): IjentFileSystemApi {
   val holder = DelegateHolder<IjentPosixApi, IjentFileSystemPosixApi>(coroutineScope, delegateFactory)
-  val user = holder.withDelegateRetrying { user }
-  return IjentFailSafeFileSystemPosixApiImpl(user, holder)
+  return IjentFailSafeFileSystemPosixApiImpl(holder)
 }
 
 private class DelegateHolder<I : IjentApi, F : IjentFileSystemApi>(
@@ -98,13 +97,18 @@ private class DelegateHolder<I : IjentApi, F : IjentFileSystemApi>(
  * so implementing a similar class for Windows will require a full copy-paste of this class.
  */
 private class IjentFailSafeFileSystemPosixApiImpl(
-  override val user: EelUserPosixInfo,
   private val holder: DelegateHolder<IjentPosixApi, IjentFileSystemPosixApi>,
 ) : IjentFileSystemPosixApi {
-  override suspend fun userHome(): EelPath.Absolute? =
-    holder.withDelegateRetrying {
-      userHome()
+  // TODO Make user suspendable again?
+  override val user: EelUserPosixInfo by lazy {
+    runBlocking {
+      holder.withDelegateRetrying { user }
     }
+  }
+
+  override suspend fun userHome(): EelPath.Absolute? = holder.withDelegateRetrying {
+    userHome()
+  }
 
   override suspend fun listDirectory(
     path: EelPath.Absolute,
