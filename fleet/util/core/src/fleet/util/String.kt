66474a1aff9208915cd16fdd902567b760b1c548
Sergei Sysoev <Sergey.Sysoev@jetbrains.com>
2024-11-16 01:23:29 +0100

    [fleet] replace fleet.util.os.Os usages with fleet.util.Os
    GitOrigin-RevId: 1bd9e5b5c67416b98b883a14085621b1ec5d5a87

--- fleet/util/core/src/fleet/util/String.kt
+++ fleet/util/core/src/fleet/util/String.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package fleet.util
 
-import fleet.util.os.Os
 import kotlin.streams.asSequence
 
 
