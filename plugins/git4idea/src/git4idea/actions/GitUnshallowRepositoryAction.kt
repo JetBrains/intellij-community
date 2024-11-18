a878bfeab53f7b4166b0ae42c39ed21c730470c0
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2024-11-18 14:46:10 +0100

    [git] Expire unshallow notifications
    GitOrigin-RevId: 3966d314ffbe1cde609a3af943270209f3fc5813

--- plugins/git4idea/src/git4idea/actions/GitUnshallowRepositoryAction.kt
+++ plugins/git4idea/src/git4idea/actions/GitUnshallowRepositoryAction.kt
@@ -2,16 +2,19 @@
 package git4idea.actions
 
 import com.intellij.dvcs.DvcsUtil
+import com.intellij.notification.NotificationsManager
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.vcs.ex.ProjectLevelVcsManagerEx
 import git4idea.GitVcs
 import git4idea.fetch.GitFetchSupport
 import git4idea.i18n.GitBundle
 import git4idea.repo.GitRepository
 import git4idea.repo.GitRepositoryManager
+import git4idea.repo.ShallowRepositoryNotification
 
 internal class GitUnshallowRepositoryAction : DumbAwareAction() {
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
@@ -31,17 +34,26 @@ internal class GitUnshallowRepositoryAction : DumbAwareAction() {
       DvcsUtil.guessRepositoryForOperation(project, GitRepositoryManager.getInstance(project), e.dataContext)
       ?: return
 
-    val fetcher = GitFetchSupport.fetchSupport(project)
-    val remote = fetcher.getDefaultRemoteToFetch(repository) ?: return
-
-    GitVcs.runInBackground(object : Task.Backgroundable(project, GitBundle.message("action.Git.Unshallow.progress.title")) {
-      override fun run(indicator: ProgressIndicator) {
-        fetcher.fetchUnshallow(repository, remote).showNotificationIfFailed(GitBundle.message("action.Git.Unshallow.failure.title"))
-      }
-    })
+    unshallowRepository(repository)
   }
 
   internal companion object {
     const val ACTION_ID = "Git.Unshallow"
+
+    @JvmStatic
+    fun unshallowRepository(repository: GitRepository) {
+      val fetcher = GitFetchSupport.fetchSupport(repository.project)
+      val remote = fetcher.getDefaultRemoteToFetch(repository) ?: return
+
+      NotificationsManager.getNotificationsManager()
+        .getNotificationsOfType(ShallowRepositoryNotification::class.java, repository.project)
+        .forEach(ShallowRepositoryNotification::expire)
+
+      GitVcs.runInBackground(object : Task.Backgroundable(repository.project, GitBundle.message("action.Git.Unshallow.progress.title")) {
+        override fun run(indicator: ProgressIndicator) {
+          fetcher.fetchUnshallow(repository, remote).showNotificationIfFailed(GitBundle.message("action.Git.Unshallow.failure.title"))
+        }
+      })
+    }
   }
 }
