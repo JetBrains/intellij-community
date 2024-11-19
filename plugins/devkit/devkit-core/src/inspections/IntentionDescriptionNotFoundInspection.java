8aa71b99d6696d51922d9131211249a4329acbf8
Yann Cébron <yann.cebron@jetbrains.com>
2024-11-19 17:22:10 +0100

    [devkit] false positives from Plugin DevKit | Description file | Intention description checker (IJPL-166110)
    GitOrigin-RevId: ebd720780f5caf7c87fffd670bca9269d2c4a9ba

--- plugins/devkit/devkit-core/src/inspections/IntentionDescriptionNotFoundInspection.java
+++ plugins/devkit/devkit-core/src/inspections/IntentionDescriptionNotFoundInspection.java
@@ -1,21 +1,25 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.inspections;
 
 import com.intellij.openapi.module.Module;
 import com.intellij.psi.PsiClass;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.xml.DomElement;
-import com.intellij.util.xml.DomManager;
+import com.intellij.util.CommonProcessors;
+import com.intellij.util.Processor;
+import com.intellij.util.xml.DomUtil;
 import com.intellij.util.xml.GenericDomValue;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.devkit.DevKitBundle;
 import org.jetbrains.idea.devkit.dom.Extension;
+import org.jetbrains.idea.devkit.dom.ExtensionPoint;
 import org.jetbrains.idea.devkit.util.DevKitDomUtil;
 import org.jetbrains.idea.devkit.util.ExtensionCandidate;
 import org.jetbrains.idea.devkit.util.ExtensionLocatorKt;
 
+import java.util.List;
+
 final class IntentionDescriptionNotFoundInspection extends DescriptionNotFoundInspectionBase {
+
+  public static final String INTENTION_ACTION_EXTENSION_POINT = "com.intellij.intentionAction";
 
   IntentionDescriptionNotFoundInspection() {
     super(DescriptionType.INTENTION);
@@ -23,19 +27,40 @@ final class IntentionDescriptionNotFoundInspection extends DescriptionNotFoundIn
 
   @Override
   protected boolean skipIfNotRegistered(PsiClass epClass) {
-    return findCandidate(epClass) == null;
+    List<ExtensionCandidate> candidates = ExtensionLocatorKt.locateExtensionsByPsiClass(epClass);
+
+    // 1. not registered at all
+    if (candidates.isEmpty()) {
+      return true;
+    }
+
+    // 2. find registration under EP name
+    return processIntentionActionExtension(candidates, CommonProcessors.alwaysFalse());
   }
 
   @Override
   protected boolean skipOptionalBeforeAfter(PsiClass epClass) {
-    ExtensionCandidate candidate = findCandidate(epClass);
-    assert candidate != null;
-    DomElement domElement = DomManager.getDomManager(epClass.getProject()).getDomElement(candidate.pointer.getElement());
-    if (!(domElement instanceof Extension)) return false;
+    return processIntentionActionExtension(ExtensionLocatorKt.locateExtensionsByPsiClass(epClass), extension -> {
+      @SuppressWarnings("unchecked") GenericDomValue<Boolean> skipBeforeAfterValue =
+        (GenericDomValue<Boolean>)DevKitDomUtil.getTag(extension, "skipBeforeAfter");
+      return skipBeforeAfterValue != null && Boolean.TRUE.equals(skipBeforeAfterValue.getValue());
+    });
+  }
 
-    @SuppressWarnings("unchecked") GenericDomValue<Boolean> skipBeforeAfterValue =
-      (GenericDomValue<Boolean>)DevKitDomUtil.getTag(domElement, "skipBeforeAfter");
-    return skipBeforeAfterValue != null && Boolean.TRUE.equals(skipBeforeAfterValue.getValue());
+  private static boolean processIntentionActionExtension(List<ExtensionCandidate> candidates, Processor<Extension> processor) {
+    for (ExtensionCandidate candidate : candidates) {
+      Extension extension = DomUtil.findDomElement(candidate.pointer.getElement(), Extension.class, false);
+      assert extension != null;
+      ExtensionPoint extensionPoint = extension.getExtensionPoint();
+      if (extensionPoint == null) continue;
+
+      String epFqn = extensionPoint.getEffectiveQualifiedName();
+      if (epFqn.equals(INTENTION_ACTION_EXTENSION_POINT)) {
+        return processor.process(extension);
+      }
+    }
+
+    return true;
   }
 
   @Override
@@ -48,10 +73,5 @@ final class IntentionDescriptionNotFoundInspection extends DescriptionNotFoundIn
   @NotNull
   protected String getHasNotBeforeAfterError() {
     return DevKitBundle.message("inspections.intention.description.no.before.after.template");
-  }
-
-  @Nullable
-  private static ExtensionCandidate findCandidate(PsiClass epClass) {
-    return ContainerUtil.getOnlyItem(ExtensionLocatorKt.locateExtensionsByPsiClass(epClass));
   }
 }
--- plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/inspections/IntentionDescriptionNotFoundInspectionTest.java
+++ plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/inspections/IntentionDescriptionNotFoundInspectionTest.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.inspections;
 
+import com.intellij.codeInsight.daemon.impl.analysis.ErrorFixExtensionPoint;
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInsight.intention.IntentionActionBean;
 import com.intellij.openapi.editor.Editor;
@@ -29,6 +30,7 @@ public class IntentionDescriptionNotFoundInspectionTest extends JavaCodeInsightF
     moduleBuilder.addLibrary("analysis-api", PathUtil.getJarPathForClass(IntentionActionBean.class));
     moduleBuilder.addLibrary("platform-rt", PathUtil.getJarPathForClass(IncorrectOperationException.class));
     moduleBuilder.addLibrary("platform-util", PathUtil.getJarPathForClass(Iconable.class));
+    moduleBuilder.addLibrary("java-analysis-impl", PathUtil.getJarPathForClass(ErrorFixExtensionPoint.class));
   }
 
   @Override
@@ -44,6 +46,10 @@ public class IntentionDescriptionNotFoundInspectionTest extends JavaCodeInsightF
 
   public void testHighlightingNotRegisteredInPluginXml() {
     myFixture.testHighlighting("MyIntentionActionNotRegisteredInPluginXml.java");
+  }
+
+  public void testHighlightingJavaErrorFixExtension() {
+    myFixture.testHighlighting("MyJavaErrorFix.java");
   }
 
   public void testNoHighlighting() {
