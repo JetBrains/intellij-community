3183223c12047c3bf33d3e3cfacaefeb3c84af14
Aleksei.Cherepanov <aleksei.cherepanov@jetbrains.com>
2024-11-20 14:22:30 +0100

    KTIJ-31334 Revert "Enable classpath mode for import by default"
    This reverts commit 214626f432e042d02b707e313b772ba0299f42d8.
    
    (#KTIJ-31718, #IDEA-363166, #KTIJ-31709) Fixed
    
    GitOrigin-RevId: e3f5e2d058d383aba778a649b82af9e7501eaeab
    
    (cherry picked from commit 39085c0a2236080b9dd9b4f17f9df068a245cdb2)
    
    Change-Id: I39085c0a2236080b9dd9b4f17f9df068a245cdb2
    Bug: 379853239

--- plugins/kotlin/gradle/gradle/src/org/jetbrains/kotlin/idea/gradle/scripting/importing/KotlinDslScriptModelResolverCommon.kt
+++ plugins/kotlin/gradle/gradle/src/org/jetbrains/kotlin/idea/gradle/scripting/importing/KotlinDslScriptModelResolverCommon.kt
@@ -8,7 +8,6 @@ import com.intellij.openapi.util.IntellijInternalApi
 import com.intellij.openapi.util.Pair
 import org.gradle.tooling.model.kotlin.dsl.KotlinDslModelsParameters.*
 import org.gradle.tooling.model.kotlin.dsl.KotlinDslScriptsModel
-import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.idea.gradleTooling.KotlinDslScriptAdditionalTask
 import org.jetbrains.plugins.gradle.service.project.AbstractProjectResolverExtension
 
@@ -27,18 +26,5 @@ abstract class KotlinDslScriptModelResolverCommon : AbstractProjectResolverExten
 
     override fun getExtraCommandLineArgs(): List<String> {
         return listOf("-P$CORRELATION_ID_GRADLE_PROPERTY_NAME=${System.nanoTime()}")
-    }
-
-    override fun getExtraJvmArgs(): List<Pair<String, String>> {
-        return if (GradleVersion.current() >= GradleVersion.version("7.6")) {
-            listOf(
-                Pair(
-                    PROVIDER_MODE_SYSTEM_PROPERTY_NAME,
-                    CLASSPATH_MODE_SYSTEM_PROPERTY_VALUE
-                )
-            )
-        } else {
-            super.extraJvmArgs
-        }
     }
 }
