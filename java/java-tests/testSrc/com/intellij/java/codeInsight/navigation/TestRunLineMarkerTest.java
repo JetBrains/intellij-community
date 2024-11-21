dfa683d35b59941989c5cb19eea621364c71ddcf
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2024-11-21 14:30:25 +0200

    [Java. Tests. JUnit] Support test state refreshing for Retrying test annotation
    IDEA-363599
    
    GitOrigin-RevId: 408be1c0c2e7fdc0664b7297508726da3c47a637

--- java/java-tests/testSrc/com/intellij/java/codeInsight/navigation/TestRunLineMarkerTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/navigation/TestRunLineMarkerTest.java
@@ -27,7 +27,9 @@ import com.intellij.testFramework.DumbModeTestUtils;
 import com.intellij.testFramework.TestActionEvent;
 import com.intellij.testIntegration.TestRunLineMarkerProvider;
 import com.intellij.util.containers.ContainerUtil;
+import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
 import java.util.Date;
 import java.util.List;
 
@@ -44,26 +46,15 @@ public class TestRunLineMarkerTest extends LineMarkerTestCase {
   }
 
   public void testNestedTestClass() {
-    TestStateStorage stateStorage = TestStateStorage.getInstance(getProject());
     String testUrl = "java:suite://Main$MainTest";
-    try {
-      stateStorage.writeState(testUrl, new TestStateStorage.Record(TestStateInfo.Magnitude.FAILED_INDEX.getValue(), new Date(), 0, 0, "",
-                                                                   "", ""));
-      myFixture.addClass("package junit.framework; public class TestCase {}");
-      PsiFile file = myFixture.configureByText("MainTest.java", """
+    myFixture.addClass("package junit.framework; public class TestCase {}");
+    myFixture.configureByText("MainTest.java", """
         public class Main {
           public static class Main<caret>Test extends junit.framework.TestCase {
             public void testFoo() {
             }
           }}""");
-
-      RunLineMarkerContributor.Info info = new TestRunLineMarkerProvider().getInfo(file.findElementAt(myFixture.getCaretOffset()));
-      assertNotNull(info);
-      assertEquals(AllIcons.RunConfigurations.TestState.Red2, info.icon);
-    }
-    finally {
-      stateStorage.removeState(testUrl);
-    }
+    doTestState(testUrl, TestStateInfo.Magnitude.FAILED_INDEX.getValue(), AllIcons.RunConfigurations.TestState.Red2);
   }
 
   public void testTestAnnotationInSuperMethodOnly() {
@@ -227,6 +218,23 @@ public class TestRunLineMarkerTest extends LineMarkerTestCase {
     doTest();
   }
 
+  public void testRetryingTestAnnotationSupportGradle() {
+    JUnit5TestFrameworkSetupUtil.setupJunit5WithExtensionLibrary(myFixture);
+
+    myFixture.configureByText("ClassWithRetryingTest.java", """
+      import org.junitpioneer.jupiter.RetryingTest;
+      
+      public class ClassWithRetryingTest {
+          @RetryingTest(value = 2)
+          void <caret>retryingTest() {
+          }
+      }
+      """);
+    setupRunConfiguration(new MockGradleRunConfiguration(myFixture.getProject(), "ClassWithRetryingTest"));
+    doTestState("java:suite://ClassWithRetryingTest/retryingTest", TestStateInfo.Magnitude.PASSED_INDEX.getValue(), AllIcons.RunConfigurations.TestState.Green2);
+
+  }
+
   private void doTest() {
     List<GutterMark> markList = myFixture.findGuttersAtCaret();
     assertSize(1, markList);
@@ -273,15 +281,26 @@ public class TestRunLineMarkerTest extends LineMarkerTestCase {
     JUnit5TestFrameworkSetupUtil.setupJUnit5Library(myFixture);
     myFixture.addClass("package org.junit.jupiter.api; public @interface Disabled {}");
 
-    RunManager manager = RunManager.getInstance(myFixture.getProject());
-    RunnerAndConfigurationSettings runnerAndConfigurationSettings =
-      new RunnerAndConfigurationSettingsImpl((RunManagerImpl)manager,configuration);
-    manager.addConfiguration(runnerAndConfigurationSettings);
-    myTempSettings.add(runnerAndConfigurationSettings);
-    manager.setSelectedConfiguration(runnerAndConfigurationSettings);
+    setupRunConfiguration(configuration);
 
     myFixture.configureByText("DisabledMethodTest.java", testClass);
     List<GutterMark> marks = myFixture.findGuttersAtCaret();
     assertEquals(marksCount, marks.size());
+  }
+
+  private void doTestState(String testUrl, int indexValue, @NotNull Icon expectedIcon) {
+    TestStateStorage stateStorage = TestStateStorage.getInstance(getProject());
+    try {
+      stateStorage.writeState(testUrl, new TestStateStorage.Record(indexValue, new Date(), 0, 0, "",
+                                                                   "", ""));
+
+
+      RunLineMarkerContributor.Info info = new TestRunLineMarkerProvider().getInfo(myFixture.getFile().findElementAt(myFixture.getCaretOffset()));
+      assertNotNull(info);
+      assertEquals(expectedIcon, info.icon);
+    }
+    finally {
+      stateStorage.removeState(testUrl);
+    }
   }
 }
