ba0d361f04f8275cf4c6bb7262458d3e35746bc8
Anna Kozlova <anna.kozlova@jetbrains.com>
2024-11-21 20:30:37 +0100

    [kotlin] k2: specify k2 explicitly
    otherwise, k1 plugin would be loaded for this test, meaning k1 would be tested locally or if executed as first test in the bucket, all next tests would fail with plugin mode inconsistency
    
    GitOrigin-RevId: c645c409a1969035bec533df2a292d37015a1f37

--- plugins/kotlin/code-insight/structural-search-k2/tests/test/org/jetbrains/kotlin/idea/k2/codeinsight/structuralsearch/KotlinOptimizedSearchScanTest.kt
+++ plugins/kotlin/code-insight/structural-search-k2/tests/test/org/jetbrains/kotlin/idea/k2/codeinsight/structuralsearch/KotlinOptimizedSearchScanTest.kt
@@ -5,8 +5,17 @@ import com.intellij.codeInsight.daemon.quickFix.LightQuickFixTestCase
 import com.intellij.structuralsearch.MatchOptions
 import com.intellij.structuralsearch.impl.matcher.compiler.PatternCompiler
 import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginMode
+import org.jetbrains.kotlin.idea.test.ExpectedPluginModeProvider
+import org.jetbrains.kotlin.idea.test.setUpWithKotlinPlugin
 
-class KotlinOptimizedSearchScanTest : LightQuickFixTestCase() {
+class KotlinOptimizedSearchScanTest : LightQuickFixTestCase(), ExpectedPluginModeProvider {
+    override val pluginMode: KotlinPluginMode = KotlinPluginMode.K2
+
+    override fun setUp() {
+        setUpWithKotlinPlugin { super.setUp() }
+    }
+
     private fun getSearchPlan(query: String): String {
         val matchOptions = MatchOptions()
         matchOptions.fillSearchCriteria(query)
