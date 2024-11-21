f0b23d1992a3ec58f1fb2cdae18dc3c24ffc924a
Max Medvedev <maxim.medvedev@jetbrains.com>
2024-11-21 17:33:50 +0100

    Kotlin: ensure psi is valid when requesting its IdeaModuleInfo
    GitOrigin-RevId: d0df979b1338c791fe3fa1b3ce71fa50177eb096

--- plugins/kotlin/base/project-structure/src/org/jetbrains/kotlin/idea/base/projectStructure/ModuleInfoProvider.kt
+++ plugins/kotlin/base/project-structure/src/org/jetbrains/kotlin/idea/base/projectStructure/ModuleInfoProvider.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.base.projectStructure
 
 import com.intellij.openapi.application.runReadAction
@@ -18,6 +18,7 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiUtilCore
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.jps.model.module.JpsModuleSourceRootType
 import org.jetbrains.kotlin.analysis.api.KaImplementationDetail
@@ -125,6 +126,7 @@ class ModuleInfoProvider(private val project: Project) {
     }
 
     private fun SeqScope<Result<IdeaModuleInfo>>.collectByElement(element: PsiElement, config: Configuration) {
+        PsiUtilCore.ensureValid(element)
         val containingFile = element.containingFile
 
         if (containingFile != null) {
