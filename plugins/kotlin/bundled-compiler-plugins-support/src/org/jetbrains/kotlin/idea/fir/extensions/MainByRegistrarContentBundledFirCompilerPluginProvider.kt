d98d889579cdb591f80a1d28205be2a7b2a98254
Roman Golyshev <roman.golyshev@jetbrains.com>
2024-11-22 18:12:28 +0100

    KTIJ-32221 [kotlin] Rename default compiler plugin provider to `MainByRegistrarContentBundledFirCompilerPluginProvider`
    Make the KDoc more accurate
    
    GitOrigin-RevId: 1cc42190dbfd37d33e0e720956f002520c92faee

--- plugins/kotlin/bundled-compiler-plugins-support/src/org/jetbrains/kotlin/idea/fir/extensions/MainByRegistrarContentBundledFirCompilerPluginProvider.kt
+++ plugins/kotlin/bundled-compiler-plugins-support/src/org/jetbrains/kotlin/idea/fir/extensions/MainByRegistrarContentBundledFirCompilerPluginProvider.kt
@@ -4,11 +4,12 @@ package org.jetbrains.kotlin.idea.fir.extensions
 import java.nio.file.Path
 
 /**
- * A default implementation of [KotlinBundledFirCompilerPluginProvider].
+ * A main implementation of [KotlinBundledFirCompilerPluginProvider].
  *
- * Provides compiler plugins listed in [KotlinK2BundledCompilerPlugins].
+ * Provides compiler plugins listed in [KotlinK2BundledCompilerPlugins] based on the content of the registrar's content which is read
+ * directly from the jar.
  */
-internal class DefaultBundledFirCompilerPluginProvider : KotlinBundledFirCompilerPluginProvider {
+internal class MainByRegistrarContentBundledFirCompilerPluginProvider : KotlinBundledFirCompilerPluginProvider {
     override fun provideBundledPluginJar(userSuppliedPluginJar: Path): Path? {
         val registrarContent = CompilerPluginRegistrarUtils.readRegistrarContent(userSuppliedPluginJar) ?: return null
         val matchingPlugin = KotlinK2BundledCompilerPlugins.entries.firstOrNull { it.registrarClassName in registrarContent }
