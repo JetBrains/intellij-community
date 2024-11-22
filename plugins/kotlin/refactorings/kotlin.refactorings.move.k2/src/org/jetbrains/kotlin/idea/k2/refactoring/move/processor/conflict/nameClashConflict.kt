a74b30135d69c11c0c5f08497e0aefe8981fe4e7
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2024-11-22 21:27:21 +0100

    [kotlin] Fixed violation of the analyze contract in nameClashConflict checker
    For comparing signatures between different modules, we now store the relevant data for symbols and classes from one analysis session and then compare it to the data when walking the declarations.
    
    ^KTIJ-32265 fixed
    
    GitOrigin-RevId: 1c07192197fd8d0c2a4637fff689ad4158e18c13

--- plugins/kotlin/refactorings/kotlin.refactorings.move.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/move/processor/conflict/nameClashConflict.kt
+++ plugins/kotlin/refactorings/kotlin.refactorings.move.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/move/processor/conflict/nameClashConflict.kt
@@ -14,113 +14,181 @@ import org.jetbrains.kotlin.analysis.api.types.KaTypeParameterType
 import org.jetbrains.kotlin.idea.base.analysis.api.utils.getSymbolContainingMemberDeclarations
 import org.jetbrains.kotlin.idea.base.psi.kotlinFqName
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
+import org.jetbrains.kotlin.idea.k2.refactoring.move.processor.conflict.SymbolData.*
+import org.jetbrains.kotlin.idea.k2.refactoring.move.processor.conflict.TypeData.*
 import org.jetbrains.kotlin.idea.k2.refactoring.move.processor.renderForConflict
+import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtNamedDeclaration
+
+private sealed interface TypeData {
+    object AnyOrTypeParameter : TypeData
+    class ClassType(val classId: ClassId, val typeArguments: List<TypeData>) : TypeData
+    object UnknownType : TypeData
+}
+
+private sealed interface SymbolData {
+    val name: Name?
+    val containingSymbolFqName: FqName?
+
+    class PropertySymbolData(
+        override val name: Name?,
+        override val containingSymbolFqName: FqName?
+    ) : SymbolData
+
+    class FunctionSymbolData(
+        override val name: Name?,
+        val valueParameters: List<TypeData>,
+        override val containingSymbolFqName: FqName?
+    ) : SymbolData
+
+    class ClassSymbolData(
+        override val name: Name?,
+        override val containingSymbolFqName: FqName?
+    ) : SymbolData
+
+    object UnknownSignature : SymbolData {
+        override val name: Name? = null
+        override val containingSymbolFqName: FqName? = null
+    }
+}
+
+context(KaSession)
+private fun KaType.toTypeInfo(): TypeData {
+    if (isAnyOrTypeParameter()) {
+        return AnyOrTypeParameter
+    }
+    return when (this) {
+        is KaClassType -> ClassType(
+            classId = classId,
+            typeArguments = typeArguments.map { it.type?.toTypeInfo() ?: UnknownType }
+        )
+
+        else -> UnknownType
+    }
+}
+
+context(KaSession)
+private fun KaSymbol.getContainingSymbolFqn(): FqName? {
+    // This breaks if certain modules are used: KT-68810
+    runCatching {
+        containingDeclaration?.importableFqName?.let { return it }
+    }
+    return psi?.kotlinFqName?.parent()
+}
+
+context(KaSession)
+private fun KaType.isAnyOrTypeParameter(): Boolean {
+    if (isAnyType) return true
+    return this is KaTypeParameterType
+}
+
+context(KaSession)
+private fun KaSymbol.toSignatureData(): SymbolData {
+    val containingSymbolFqName = getContainingSymbolFqn()
+    return when (this) {
+        is KaClassSymbol -> ClassSymbolData(name, containingSymbolFqName)
+        is KaFunctionSymbol -> FunctionSymbolData(name, valueParameters.map { it.returnType.toTypeInfo() }, containingSymbolFqName)
+        is KaPropertySymbol -> PropertySymbolData(name, containingSymbolFqName)
+        else -> UnknownSignature
+    }
+}
 
 /**
  * This conflict checker checks for conflicts arising from declarations with the same name already existing in the [targetPkg].
+ * Because we are moving declarations between different useSiteModules, we cannot compare the symbols in the same analysis session.
+ * Instead, we first create a representation of the symbol using [SymbolData] and [TypeData] that we then compare with
+ * the existing symbols within the target scope.
  */
 @OptIn(KaExperimentalApi::class)
-fun checkNameClashConflicts(
+internal fun checkNameClashConflicts(
     allDeclarationsToMove: Iterable<KtNamedDeclaration>,
     targetPkg: FqName,
     targetKaModule: KaModule,
 ): MultiMap<PsiElement, String> {
-    fun KaSession.isAnyOrTypeParameter(type: KaType): Boolean {
-        if (type.isAnyType) return true
-        return type is KaTypeParameterType
-    }
 
-    fun KaSession.equivalent(a: KaType, b: KaType): Boolean {
-        val aSupertypes = a.allSupertypes.toList()
-        val bSupertypes = b.allSupertypes.toList()
+    // Note: This does not detect 100% of conflicts currently
+    fun KaSession.equivalent(a: KaType, b: TypeData): Boolean {
         return when {
             // Possible type erasure conflict
-            isAnyOrTypeParameter(a) && isAnyOrTypeParameter(b) ->         // a = T(?) | Any(?), b = T(?) | Any(?)
+            a.isAnyOrTypeParameter() && b is AnyOrTypeParameter ->         // a = T(?) | Any(?), b = T(?) | Any(?)
                 true                                                    // => 100% clash
-            aSupertypes.size == 1 && bSupertypes.size == 1 ->           // a = T: T1, b = T: T2
-                equivalent(aSupertypes.first(), bSupertypes.first())    // equivalent(T1, T2) => clash
-            a is KaClassType && b is KaClassType &&
-                    a.typeArguments.isNotEmpty() && b.typeArguments.isNotEmpty() -> // a = Something<....>, b = SomethingElse<....>
-                a.classId.shortClassName == b.classId.shortClassName  // equivalent(Something, SomethingElse) => clash
-            else -> a.semanticallyEquals(b)
+            a is KaClassType && b is ClassType -> {
+                if (a.classId != b.classId || a.typeArguments.size != b.typeArguments.size) return false
+                a.typeArguments.zip(b.typeArguments).all {
+                    val type = it.first.type ?: return@all true
+                    equivalent(type, it.second)
+                }
+            }
+            else -> return false
         }
     }
 
-    fun KaSession.equivalent(a: KaSymbol, b: KaSymbol): Boolean = when (a) {
+    fun KaSession.equivalent(a: KaSymbol, b: SymbolData): Boolean = when (a) {
         is KaDeclarationSymbol -> when (a) {
-            is KaFunctionSymbol -> b is KaFunctionSymbol && a.name == b.name &&
+            is KaFunctionSymbol -> b is FunctionSymbolData && a.name == b.name &&
                     a.valueParameters.size == b.valueParameters.size &&
-                    a.valueParameters.zip(b.valueParameters).all { equivalent(it.first, it.second) }
+                    a.valueParameters.zip(b.valueParameters).all { equivalent(it.first.returnType, it.second) }
 
-            is KaValueParameterSymbol -> b is KaValueParameterSymbol
-                    && equivalent(a.returnType, b.returnType)
-
-            else -> b is KaDeclarationSymbol && a.name == b.name
+            else -> a.name == b.name
         }
 
         else -> false
     }
 
-    fun KaSession.getContainingSymbolFqn(symbol: KaSymbol): FqName? {
-        // This breaks if certain modules are used: KT-68810
-        runCatching {
-            symbol.containingDeclaration?.importableFqName?.let { return it }
-        }
-        return symbol.psi?.kotlinFqName?.parent()
-    }
-
     fun KaSession.walkDeclarations(
         currentScope: KaSymbol,
-        declaration: KaDeclarationSymbol,
+        signatureToCheck: SymbolData,
         report: (KaDeclarationSymbol, KaSymbol) -> Unit
     ) {
-        val declarationName = declaration.name ?: return
+        val declarationName = signatureToCheck.name
         when (currentScope) {
             is KaPackageSymbol -> {
-                if (getContainingSymbolFqn(declaration) == currentScope.fqName) {
+                if (signatureToCheck.containingSymbolFqName == currentScope.fqName) {
                     return
                 }
                 currentScope
                     .packageScope
                     .declarations
                     .filter { it.name == declarationName }
-                    .filter { equivalent(it, declaration) }
+                    .filter { equivalent(it, signatureToCheck) }
                     .forEach { report(it, currentScope) }
                 return
             }
 
             is KaClassSymbol -> {
-                if (getContainingSymbolFqn(declaration) != currentScope.classId?.packageFqName) {
+                if (signatureToCheck.containingSymbolFqName != currentScope.classId?.packageFqName) {
                     currentScope.getSymbolContainingMemberDeclarations()
                         ?.memberScope
                         ?.declarations
                         ?.filter { it.name == declarationName }
-                        ?.filter { equivalent(it, declaration) }
+                        ?.filter { equivalent(it, signatureToCheck) }
                         ?.forEach { report(it, currentScope) }
                 }
                 return
             }
 
         }
-        currentScope.containingDeclaration?.let { walkDeclarations(it, declaration, report) }
+        currentScope.containingDeclaration?.let { walkDeclarations(it, signatureToCheck, report) }
     }
-
 
     val conflicts = MultiMap<PsiElement, String>()
     allDeclarationsToMove
         .forEach { declaration ->
+            val signatureData = analyze(declaration) {
+                declaration.symbol.toSignatureData()
+            }
             analyze(targetKaModule) {
-                // TODO(KTIJ-32265): logic should rewritten to support unrelated modules
-                if (!declaration.canBeAnalysed()) return@analyze
-
-                val declarationSymbol = declaration.symbol
                 val packageSymbol = findPackage(targetPkg) ?: return@analyze
-                walkDeclarations(packageSymbol, declarationSymbol) { conflictingSymbol, conflictingScope ->
+                walkDeclarations(packageSymbol, signatureData) { conflictingSymbol, conflictingScope ->
+                    val renderedDeclaration = analyze(declaration) {
+                        // Needs to be a nested declaration
+                        declaration.symbol.renderForConflict()
+                    }
                     val message = KotlinBundle.message(
                         "text.declarations.clash.move.0.destination.1.declared.in.scope.2",
-                        declarationSymbol.renderForConflict(),
+                        renderedDeclaration,
                         conflictingSymbol.renderForConflict(),
                         conflictingScope.renderForConflict(),
                     )
