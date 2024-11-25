bc7afa3ed8ae4109dcb883d33d94529e1bc2c315
Yann Cébron <yann.cebron@jetbrains.com>
2024-11-25 14:29:11 +0100

    [devkit] plugin.xml: resolve shortcut elements 'keymap' to the keymap.xml file
    GitOrigin-RevId: 4469a920544f8629e0f36b56d87deb48458ff20b

--- plugins/devkit/devkit-core/src/dom/impl/KeymapConverter.java
+++ plugins/devkit/devkit-core/src/dom/impl/KeymapConverter.java
@@ -1,8 +1,9 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.dom.impl;
 
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.openapi.fileEditor.UniqueVFilePathBuilder;
 import com.intellij.openapi.util.io.FileUtilRt;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.JavaPsiFacade;
@@ -45,7 +46,7 @@ public class KeymapConverter extends ResolvingConverter<XmlFile> {
   @Nullable
   @Override
   public LookupElement createLookupElement(XmlFile file) {
-    return LookupElementBuilder.create(getKeymapName(file));
+    return LookupElementBuilder.create(file, getKeymapName(file));
   }
 
   @Override
