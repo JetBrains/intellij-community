d2dd328766494c78ffa725db0c797ddb17be6ba2
Pavel Karateev <pavel.karateev@jetbrains.com>
2024-11-25 17:39:47 +0100

    PY-77656 Fix import path
    (cherry picked from commit 099f29c803e78cb1b84f76fc08dac64c1c2cda13)
    
    IJ-MR-150144
    
    GitOrigin-RevId: 6ddb50e5cdaedd4b76ca0f225f0db19cba0ea5d4

--- python/src/com/jetbrains/python/sdk/uv/UvExt.kt
+++ python/src/com/jetbrains/python/sdk/uv/UvExt.kt
@@ -43,7 +43,7 @@ internal fun suggestedSdkName(basePath: Path): @NlsSafe String {
   return "UV (${PathUtil.getFileName(basePath.pathString)})"
 }
 
-val UV_ICON = PythonIcons.Python.UV
+val UV_ICON = PythonIcons.UV
 val UV_LOCK: String = "uv.lock"
 
 // FIXME: move pyprojecttoml code out to common package
