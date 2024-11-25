9950b7693e0d9e894297b57eb8067f8903ca55b6
Madalin Valceleanu <vmadalin@google.com>
2024-11-25 21:14:32 +0000

    [Gradle|Daemon|JVM] new: Implement different quickfixes for Daemon toolchain related exceptions
    closes https://github.com/JetBrains/intellij-community/pull/2881
    
    GitOrigin-RevId: eb0794bf807d63379f4801f63106b969aa86fdc6

---
+++ plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/issues/quickfix/GradleAddDaemonToolchainCriteriaQuickFix.kt
@@ -0,0 +1,25 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.gradleJava.issues.quickfix
+
+import com.intellij.build.issue.BuildIssueQuickFix
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.project.Project
+import kotlinx.coroutines.future.asCompletableFuture
+import kotlinx.coroutines.launch
+import org.jetbrains.plugins.gradle.service.coroutine.GradleCoroutineScopeProvider
+import org.jetbrains.plugins.gradle.service.execution.GradleDaemonJvmHelper.updateProjectDaemonJvmCriteria
+import java.util.concurrent.CompletableFuture
+
+class GradleAddDaemonToolchainCriteriaQuickFix(
+    private val externalProjectPath: String
+) : BuildIssueQuickFix {
+
+    override val id: String = "add_daemon_toolchain_criteria"
+
+    override fun runQuickFix(project: Project, dataContext: DataContext): CompletableFuture<*> {
+        return GradleCoroutineScopeProvider.getInstance(project).cs
+            .launch {
+                updateProjectDaemonJvmCriteria(project, externalProjectPath, null)
+            }.asCompletableFuture()
+    }
+}
---
+++ plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/issues/quickfix/GradleAddDownloadToolchainRepositoryQuickFix.kt
@@ -0,0 +1,29 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.gradleJava.issues.quickfix
+
+import com.intellij.build.issue.BuildIssueQuickFix
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.command.writeCommandAction
+import com.intellij.openapi.project.Project
+import kotlinx.coroutines.future.asCompletableFuture
+import kotlinx.coroutines.launch
+import org.jetbrains.kotlin.idea.gradleCodeInsightCommon.GradleBuildScriptSupport
+import org.jetbrains.kotlin.idea.gradleCodeInsightCommon.getTopLevelBuildScriptSettingsPsiFile
+import org.jetbrains.plugins.gradle.service.coroutine.GradleCoroutineScopeProvider
+import java.util.concurrent.CompletableFuture
+
+object GradleAddDownloadToolchainRepositoryQuickFix : BuildIssueQuickFix {
+
+    override val id: String = "add_download_toolchain_repository"
+
+    override fun runQuickFix(project: Project, dataContext: DataContext): CompletableFuture<*> {
+        return GradleCoroutineScopeProvider.getInstance(project).cs
+            .launch {
+                writeCommandAction(project, "Applying Foojay Plugin to Project") {
+                    val topLevelBuildScript = project.getTopLevelBuildScriptSettingsPsiFile()!!
+                    val buildScriptSupport = GradleBuildScriptSupport.getManipulator(topLevelBuildScript)
+                    buildScriptSupport.addFoojayPlugin(topLevelBuildScript)
+                }
+            }.asCompletableFuture()
+    }
+}
