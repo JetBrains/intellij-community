88c2a42b1956bd67f7c201b905cd3233d7d23715
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-11-25 13:15:11 +0100

    Cleanup (minor optimization; typos; formatting)
    GitOrigin-RevId: 6b29ba37fb1a74af33bbbf78fb4d353bb1f76a60

--- jupyter/viewOnly/src/com/intellij/jupyter/viewOnly/JupyterViewOnlyHandler.kt
+++ jupyter/viewOnly/src/com/intellij/jupyter/viewOnly/JupyterViewOnlyHandler.kt
@@ -1,11 +1,10 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.jupyter.viewOnly
 
 import com.intellij.notebooks.jupyter.core.jupyter.preview.JupyterCefHttpHandlerBase
 
-class JupyterViewOnlyHandler : JupyterCefHttpHandlerBase(absolutePathFiles = listOf(
-  "/ipywidgets.html",
-  "/ipywidgets.css",
-  "/ipywidgets.js")) {
+class JupyterViewOnlyHandler : JupyterCefHttpHandlerBase(
+  absolutePathFiles = hashSetOf("/ipywidgets.html", "/ipywidgets.css", "/ipywidgets.js")
+) {
   override val appName: String = "jupyter-view-only"
 }
