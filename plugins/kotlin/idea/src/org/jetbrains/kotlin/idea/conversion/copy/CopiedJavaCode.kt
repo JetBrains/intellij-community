1e20e7acaedacd3d00e315aee88a508cfacc9cda
Alexey Belkov <alexey.belkov@jetbrains.com>
2024-11-25 14:44:22 +0400

    [kotlin] J2K copy-paste: cleanup CopiedJavaCode, CopiedKotlinCode
    KTIJ-28714
    
    (cherry picked from commit 09a2ab6a7376e570c59a6cd2f7d297b9f86ca0a2)
    
    GitOrigin-RevId: 465cd1dcfd0f12611f041750fbc6976dd5772696

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/conversion/copy/CopiedJavaCode.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/conversion/copy/CopiedJavaCode.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.conversion.copy
 
@@ -7,8 +7,7 @@ import com.intellij.codeInsight.editorActions.TextBlockTransferableData
 import java.awt.datatransfer.DataFlavor
 
 class CopiedJavaCode(val fileText: String, val startOffsets: IntArray, val endOffsets: IntArray) : TextBlockTransferableData {
-
-    override fun getFlavor() = DATA_FLAVOR
+    override fun getFlavor(): DataFlavor = DATA_FLAVOR
 
     companion object {
         val DATA_FLAVOR: DataFlavor = DataFlavor(ConvertJavaCopyPasteProcessor::class.java, "class: ConvertJavaCopyPasteProcessor")
