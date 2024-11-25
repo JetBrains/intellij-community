a514754889ed01a489f4b44d9e7dc37febd75843
Alexey Belkov <alexey.belkov@jetbrains.com>
2024-11-25 16:36:39 +0400

    [kotlin] J2K copy-paste: extract J2KTextCopyPasteConverter to a separate file
    KTIJ-28714
    
    (cherry picked from commit 851a0d060413ffa4a555b76ddf573f9309e5bfc9)
    
    GitOrigin-RevId: 28e0eae1c94cc32b3ffb99a3fb41f1641e345e7d

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/conversion/copy/ConvertTextJavaCopyPasteProcessor.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/conversion/copy/ConvertTextJavaCopyPasteProcessor.kt
@@ -6,15 +6,11 @@ import com.intellij.codeInsight.editorActions.CopyPastePostProcessor
 import com.intellij.codeInsight.editorActions.TextBlockTransferable
 import com.intellij.codeInsight.editorActions.TextBlockTransferableData
 import com.intellij.ide.highlighter.JavaFileType
-import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.diagnostic.ControlFlowException
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.RangeMarker
-import com.intellij.openapi.editor.asTextRange
 import com.intellij.openapi.fileTypes.LanguageFileType
-import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Ref
@@ -27,12 +23,10 @@ import org.jetbrains.kotlin.idea.KotlinFileType
 import org.jetbrains.kotlin.idea.editor.KotlinEditorOptions
 import org.jetbrains.kotlin.idea.statistics.ConversionType
 import org.jetbrains.kotlin.idea.statistics.J2KFusCollector
-import org.jetbrains.kotlin.j2k.J2kConverterExtension
 import org.jetbrains.kotlin.j2k.J2kConverterExtension.Kind.K1_NEW
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.anyDescendantOfType
-import org.jetbrains.kotlin.psi.psiUtil.endOffset
 import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.Transferable
@@ -281,67 +275,5 @@ class ConvertTextJavaCopyPasteProcessor : CopyPastePostProcessor<TextBlockTransf
     object Util {
         @get:TestOnly
         var conversionPerformed: Boolean = false
-    }
-}
-
-/**
- * Runs J2K on the pasted code and updates [targetFile] as a side effect.
- * Used by [ConvertTextJavaCopyPasteProcessor].
- */
-private class J2KTextCopyPasteConverter(
-    private val project: Project,
-    private val editor: Editor,
-    private val dataForConversion: DataForConversion,
-    private val j2kKind: J2kConverterExtension.Kind,
-    private val targetFile: KtFile,
-    private val targetBounds: RangeMarker,
-    private val targetDocument: Document
-) {
-    fun convert() {
-        val additionalImports = tryToResolveImports(dataForConversion, targetFile)
-        ProgressManager.checkCanceled()
-
-        val importsInsertOffset = targetFile.importList?.endOffset ?: 0
-        var convertedImportsText = additionalImports.convertCodeToKotlin(project, targetFile, j2kKind).text
-        if (targetFile.importDirectives.isEmpty() && importsInsertOffset > 0) {
-            convertedImportsText = "\n" + convertedImportsText
-        }
-
-        val conversionResult = dataForConversion.elementsAndTexts.convertCodeToKotlin(project, targetFile, j2kKind)
-        val convertedText = conversionResult.text
-        ProgressManager.checkCanceled()
-
-        val boundsAfterReplace = runWriteAction {
-            if (convertedImportsText.isNotBlank()) {
-                targetDocument.insertString(importsInsertOffset, convertedImportsText)
-            }
-            targetDocument.replaceString(targetBounds.startOffset, targetBounds.endOffset, convertedText)
-
-            val endOffsetAfterReplace = targetBounds.startOffset + convertedText.length
-            editor.caretModel.moveToOffset(endOffsetAfterReplace)
-
-            targetDocument.createRangeMarker(targetBounds.startOffset, endOffsetAfterReplace)
-        }
-
-        PsiDocumentManager.getInstance(project).commitAllDocuments()
-
-        if (j2kKind == K1_NEW) {
-            val postProcessor = J2kConverterExtension.extension(kind = K1_NEW).createPostProcessor()
-            for (fqName in conversionResult.importsToAdd) {
-                postProcessor.insertImport(targetFile, fqName)
-            }
-        }
-
-        ProgressManager.checkCanceled()
-        runPostProcessing(project, targetFile, boundsAfterReplace.asTextRange, conversionResult.converterContext, j2kKind)
-    }
-
-    private fun tryToResolveImports(dataForConversion: DataForConversion, targetFile: KtFile): ElementAndTextList {
-        val imports = PlainTextPasteImportResolver(dataForConversion, targetFile).generateRequiredImports()
-        val newlineSeparatedImports = imports.flatMap { importStatement ->
-            listOf("\n", importStatement)
-        } + "\n\n"
-
-        return ElementAndTextList(newlineSeparatedImports)
     }
 }
