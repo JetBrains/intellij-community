36d3d826a10726aaa151534018e5634d2af0ec2e
Artemy Pestretsov <artemy.pestretsov@jetbrains.com>
2024-11-25 10:47:30 +0100

    IJ-CR-151119 [dotenv] add missing commits
    - migrate to Symbols API
    - add new logo
    
    Co-authored-by: adelf <adel.faiz@gmail.com>
    (cherry picked from commit 38df2c3c89446bb5c3ff2901280245b718225fe9)
    
    GitOrigin-RevId: e2ea805b4f88e9d791bb54f7570c51cf1029c67d

--- plugins/env-files-support/src/main/java/ru/adelf/idea/dotenv/php/PhpEnvironmentCallsVisitor.java
+++ plugins/env-files-support/src/main/java/ru/adelf/idea/dotenv/php/PhpEnvironmentCallsVisitor.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package ru.adelf.idea.dotenv.php;
 
 import com.intellij.psi.PsiElement;
@@ -39,7 +40,7 @@ class PhpEnvironmentCallsVisitor extends PsiRecursiveElementVisitor {
 
         String key = ((StringLiteralExpression)parameters[0]).getContents();
 
-        collectedItems.add(new KeyUsagePsiElement(key, parameters[0]));
+        collectedItems.add(new KeyUsagePsiElement(key, expression));
     }
 
     private void visitArrayAccess(ArrayAccessExpression expression) {
