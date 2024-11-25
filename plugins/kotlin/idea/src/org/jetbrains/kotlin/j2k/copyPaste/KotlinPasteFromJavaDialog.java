9cae14134a2f82d1b5e6c82f8971bb955ea5be14
Alexey Belkov <alexey.belkov@jetbrains.com>
2024-11-26 01:24:22 +0400

    [kotlin] J2K copy-paste: move code to "org.jetbrains.kotlin.j2k.copyPaste" package
    KTIJ-28714
    
    (cherry picked from commit 9c60d6ee078e5751bbfa32dbdb3b42f21f288c62)
    
    GitOrigin-RevId: 680bdbed1d5a0ac658f367850ce2a5975b599f41

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/j2k/copyPaste/KotlinPasteFromJavaDialog.java
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/j2k/copyPaste/KotlinPasteFromJavaDialog.java
@@ -1,6 +1,6 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
-package org.jetbrains.kotlin.idea.conversion.copy;
+package org.jetbrains.kotlin.j2k.copyPaste;
 
 import com.intellij.CommonBundle;
 import com.intellij.openapi.project.Project;
