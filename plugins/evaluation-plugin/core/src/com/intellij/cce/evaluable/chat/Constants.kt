a98a796e11e923baa5646d73a49153dfba5583a2
Aleksandr.Slapoguzov <aleksandr.slapoguzov@jetbrains.com>
2024-11-26 03:23:00 +0100

    LLM-11651: eval for chat context collection on intellij monorepo
    GitOrigin-RevId: 925f021aa3bbd1b9949eb483fa2d507eacee077d

--- plugins/evaluation-plugin/core/src/com/intellij/cce/evaluable/chat/Constants.kt
+++ plugins/evaluation-plugin/core/src/com/intellij/cce/evaluable/chat/Constants.kt
@@ -3,5 +3,6 @@ package com.intellij.cce.evaluable.chat
 const val PROMPT_PROPERTY = "prompt"
 const val INTERNAL_API_CALLS_PROPERTY = "internal_api_calls"
 const val INTERNAL_RELEVANT_FILES_PROPERTY = "internal_relevant_files"
+const val INTERNAL_CONTEXT_ITEMS = "internal_context_items"
 
 const val METHOD_NAME_PROPERTY = "method_name"
