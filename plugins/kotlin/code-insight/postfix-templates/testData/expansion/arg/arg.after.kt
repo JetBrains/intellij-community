644185f057542441a7e63812c8613b79fddadb5a
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2024-11-26 10:07:42 +0100

    [kotlin] Port arg postfix template to K2
    #KTIJ-23979
    
    GitOrigin-RevId: ee177816f458607939f9adcc00ff29c9de5d0e26

--- plugins/kotlin/code-insight/postfix-templates/testData/expansion/arg/arg.after.kt
+++ plugins/kotlin/code-insight/postfix-templates/testData/expansion/arg/arg.after.kt
@@ -1,4 +1,4 @@
 // TEMPLATE: \tfunctionCall\t
 fun foo(s: String) {
-    functionCall(s<caret>)
+    functionCall(s)
 }
--- plugins/kotlin/code-insight/postfix-templates/testData/expansion/oldTestData/arg.kt
+++ plugins/kotlin/code-insight/postfix-templates/testData/expansion/oldTestData/arg.kt
@@ -1,4 +1,3 @@
-// IGNORE_K2
 // TEMPLATE: \tfunctionCall\t
 fun foo(s: String) {
     s.arg<caret>
