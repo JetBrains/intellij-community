c9cf9ef185472380417a8f991202b16d428d2bbc
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2024-11-26 13:45:01 +0100

    [platform] deprecating pointless `DelegatingHttpRequestHandlerBase` intermediary
    GitOrigin-RevId: f4eb1295a8e5f8747cd884f05f07e0badcf22f42

--- platform/built-in-server/src/org/jetbrains/io/DelegatingHttpRequestHandler.kt
+++ platform/built-in-server/src/org/jetbrains/io/DelegatingHttpRequestHandler.kt
@@ -1,10 +1,13 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.io
 
+import com.intellij.openapi.diagnostic.debug
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.util.io.isWriteFromBrowserWithoutOrigin
 import io.netty.channel.ChannelHandler
 import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.http.FullHttpRequest
+import io.netty.handler.codec.http.HttpResponseStatus
 import io.netty.handler.codec.http.QueryStringDecoder
 import io.netty.util.AttributeKey
 import org.jetbrains.ide.HttpRequestHandler
@@ -13,14 +16,22 @@ import java.lang.ref.WeakReference
 private val PREV_HANDLER = AttributeKey.valueOf<WeakReference<HttpRequestHandler>>("DelegatingHttpRequestHandler.handler")
 
 @ChannelHandler.Sharable
-internal class DelegatingHttpRequestHandler : DelegatingHttpRequestHandlerBase() {
-  override fun process(context: ChannelHandlerContext,
-                       request: FullHttpRequest,
-                       urlDecoder: QueryStringDecoder
-  ): Boolean {
-    fun HttpRequestHandler.checkAndProcess(): Boolean {
-      return isSupported(request) && !request.isWriteFromBrowserWithoutOrigin() && isAccessible(request) && process(urlDecoder, request, context)
+internal class DelegatingHttpRequestHandler() : SimpleChannelInboundHandlerAdapter<FullHttpRequest>() {
+  override fun messageReceived(context: ChannelHandlerContext, request: FullHttpRequest) {
+    logger<BuiltInServer>().debug { "\n\nIN HTTP: $request\n\n" }
+
+    if (!process(context, request, QueryStringDecoder(request.uri()))) {
+      createStatusResponse(HttpResponseStatus.NOT_FOUND, request).send(context.channel(), request)
     }
+  }
+
+  private fun process(context: ChannelHandlerContext, request: FullHttpRequest, urlDecoder: QueryStringDecoder): Boolean {
+    if (request.isWriteFromBrowserWithoutOrigin()) {
+      return false
+    }
+
+    fun HttpRequestHandler.checkAndProcess(): Boolean =
+      isSupported(request) && isAccessible(request) && process(urlDecoder, request, context)
 
     val prevHandlerAttribute = context.channel().attr(PREV_HANDLER)
     val connectedHandler = prevHandlerAttribute.get()?.get()
@@ -43,14 +54,12 @@ internal class DelegatingHttpRequestHandler : DelegatingHttpRequestHandlerBase()
     } != null
   }
 
-  @Suppress("OverridingDeprecatedMember")
   override fun exceptionCaught(context: ChannelHandlerContext, cause: Throwable) {
     try {
       context.channel().attr(PREV_HANDLER).set(null)
     }
     finally {
-      @Suppress("DEPRECATION")
-      super.exceptionCaught(context, cause)
+      NettyUtil.logAndClose(cause, logger<BuiltInServer>(), context.channel())
     }
   }
 }
