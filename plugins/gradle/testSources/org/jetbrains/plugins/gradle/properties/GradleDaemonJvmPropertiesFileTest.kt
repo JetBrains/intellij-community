58352281d85837074cf2d70dab101ea7952d415d
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2024-11-28 12:26:14 +0100

    [Gradle|Exec] cleanup: migrate GradleDaemonJvmPropertiesFileTest on Junit 5
    GitOrigin-RevId: 501d9a6f6b34e079a49f6544d45593865aabfaa4

--- plugins/gradle/testSources/org/jetbrains/plugins/gradle/properties/GradleDaemonJvmPropertiesFileTest.kt
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/properties/GradleDaemonJvmPropertiesFileTest.kt
@@ -1,77 +1,52 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.properties
 
-import com.intellij.openapi.util.io.FileUtil
-import com.intellij.testFramework.LightPlatformTestCase
-import java.io.File
-import java.nio.file.Paths
-import java.util.*
-import kotlin.io.path.Path
+import com.intellij.testFramework.junit5.TestApplication
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 
-class GradleDaemonJvmPropertiesFileTest : LightPlatformTestCase() {
+@TestApplication
+class GradleDaemonJvmPropertiesFileTest : GradleDaemonJvmPropertiesFileTestCase() {
 
-  private val projectPath by lazy { project.basePath.orEmpty() }
-  private val externalProjectPath by lazy { Path(projectPath) }
-  private val gradleDaemonJvmPropertiesPath by lazy {
-    externalProjectPath.resolve(Paths.get(GRADLE_FOLDER, "gradle-daemon-jvm.properties")).toString()
-  }
-
+  @Test
   fun testNotPresentProjectGradleDaemonJvmPropertiesFile() {
-    assertNull(GradleDaemonJvmPropertiesFile.getProperties(externalProjectPath))
+    Assertions.assertNull(GradleDaemonJvmPropertiesFile.getProperties(projectPath))
   }
 
-  fun testEmptyProjectGradleDaemonJvmPropertiesFile() = testGradleDaemonJvmProperties(
-    assertion = {
-      GradleDaemonJvmPropertiesFile.getProperties(externalProjectPath)!!.run {
-        assertNull(version)
-        assertNull(vendor)
-      }
+  @Test
+  fun testEmptyProjectGradleDaemonJvmPropertiesFile() {
+    createGradleDaemonJvmPropertiesFile {}
+    assertGradleDaemonJvmPropertiesFile {
+      Assertions.assertNull(version)
+      Assertions.assertNull(vendor)
     }
-  )
+  }
 
-  fun testUnexpectedPropertiesInProjectGradleDaemonJvmPropertiesFile() = testGradleDaemonJvmProperties(
-    configure = {
+  @Test
+  fun testUnexpectedPropertiesInProjectGradleDaemonJvmPropertiesFile() {
+    createGradleDaemonJvmPropertiesFile {
       setProperty("another.property.1", "value1")
       setProperty("another.property.2", "value2")
-    },
-    assertion = {
-      GradleDaemonJvmPropertiesFile.getProperties(externalProjectPath)!!.run {
-        assertNull(version)
-        assertNull(vendor)
-      }
     }
-  )
+    assertGradleDaemonJvmPropertiesFile {
+      Assertions.assertNull(version)
+      Assertions.assertNull(vendor)
+    }
+  }
 
-  fun testPropertiesInProjectGradleDaemonJvmPropertiesFile() = testGradleDaemonJvmProperties(
-    configure = {
+  @Test
+  fun testPropertiesInProjectGradleDaemonJvmPropertiesFile() {
+    createGradleDaemonJvmPropertiesFile {
       setProperty("another.property.1", "value1")
       setProperty("toolchainVersion", "value2")
       setProperty("another.property.2", "value3")
       setProperty("toolchainVendor", "value4")
-    },
-    assertion = {
-      GradleDaemonJvmPropertiesFile.getProperties(externalProjectPath)!!.run {
-        assertEquals("value2", version?.value)
-        assertEquals(gradleDaemonJvmPropertiesPath, version?.location)
-        assertEquals("value4", vendor?.value)
-        assertEquals(gradleDaemonJvmPropertiesPath, vendor?.location)
-      }
     }
-  )
-
-  private fun testGradleDaemonJvmProperties(
-    configure: Properties.() -> Unit = {},
-    assertion: () -> Unit
-  ) {
-    val gradleDaemonJvmPropertiesFile = File(gradleDaemonJvmPropertiesPath)
-    FileUtil.createIfNotExists(gradleDaemonJvmPropertiesFile)
-    gradleDaemonJvmPropertiesFile.outputStream().use {
-      Properties().run {
-        configure()
-        store(it, null)
-      }
+    assertGradleDaemonJvmPropertiesFile {
+      Assertions.assertEquals("value2", version?.value)
+      Assertions.assertEquals(projectPropertiesPath.toString(), version?.location)
+      Assertions.assertEquals("value4", vendor?.value)
+      Assertions.assertEquals(projectPropertiesPath.toString(), vendor?.location)
     }
-    assertion()
-    gradleDaemonJvmPropertiesFile.delete()
   }
 }
