4cf792fe801a36838b4cd5e304902e3b7fac2322
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2024-11-28 09:13:28 +0100

    Convert DiffEditorTitleCustomizer to kt
    GitOrigin-RevId: 806511755fe71e5d933646004d1662a880df94ef

--- platform/diff-api/src/com/intellij/diff/DiffEditorTitleCustomizer.kt
+++ platform/diff-api/src/com/intellij/diff/DiffEditorTitleCustomizer.kt
@@ -1,14 +1,11 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.diff;
+package com.intellij.diff
 
-import org.jetbrains.annotations.Nullable;
+import javax.swing.JComponent
 
-import javax.swing.*;
-
-public interface DiffEditorTitleCustomizer {
+fun interface DiffEditorTitleCustomizer {
   /**
    * @return component that will be used to replace an editor title or null if the title should not be displayed
    */
-  @Nullable
-  JComponent getLabel();
+  fun getLabel(): JComponent?
 }
