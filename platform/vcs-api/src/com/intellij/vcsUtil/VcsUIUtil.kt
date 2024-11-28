f3716f44298959b52288cb2494c8919b98de39b6
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2024-11-28 11:05:02 +0200

    IJPL-171509 Use JBPopup.showAboveOf in VcsUIUtil
    GitOrigin-RevId: 13f61b46bdfa811daea919f4ba984640de69f188

--- platform/vcs-api/src/com/intellij/vcsUtil/VcsUIUtil.kt
+++ platform/vcs-api/src/com/intellij/vcsUtil/VcsUIUtil.kt
@@ -4,6 +4,7 @@ package com.intellij.vcsUtil
 import com.intellij.openapi.ui.popup.JBPopup
 import com.intellij.openapi.ui.popup.JBPopupListener
 import com.intellij.openapi.ui.popup.LightweightWindowEvent
+import com.intellij.openapi.ui.popup.PopupShowOptions
 import com.intellij.ui.awt.RelativePoint
 import com.intellij.util.ui.JBUI
 import java.awt.Component
@@ -17,32 +18,11 @@ object VcsUIUtil {
   }
 
   fun showPopupAbove(popup: JBPopup, component: Component, minHeight: Int?) {
-    val northWest = RelativePoint(component, Point())
-
-    popup.addListener(object : JBPopupListener {
-      override fun beforeShown(event: LightweightWindowEvent) {
-        val gap = JBUI.scale(4)
-        val anchorY = northWest.screenPoint.y
-
-        val popupLocation = popup.locationOnScreen
-        val popupSize = popup.size
-        popupLocation.y = anchorY - popup.size.height
-
-        if (anchorY >= popup.size.height + gap) {
-          popupLocation.y = anchorY - popup.size.height
-          popup.setLocation(popupLocation)
-        }
-        else if (minHeight != null && anchorY > minHeight) {
-          popupLocation.y = gap
-          popupSize.height = anchorY - gap
-          popup.setSize(popupLocation, popupSize)
-        }
-        else {
-          // keep the default position
-        }
-        popup.removeListener(this)
-      }
-    })
-    popup.show(northWest)
+    popup.showAboveOf(
+      component,
+      PopupShowOptions()
+        .withPopupComponentUnscaledGap(4)
+        .withMinimumHeight(minHeight)
+    )
   }
 }
