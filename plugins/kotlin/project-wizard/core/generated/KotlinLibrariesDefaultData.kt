5f6c72fff11dd103eed32caa53e6364d68eaebef
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2024-11-29 19:03:53 +0100

    [kotlin] Updated Kotlin library versions compatibility
    KTIJ-32255
    
    GitOrigin-RevId: f63fdd27ffc0608ec5b6fa8c9321acc7b0743c02

--- plugins/kotlin/project-wizard/core/generated/KotlinLibrariesDefaultData.kt
+++ plugins/kotlin/project-wizard/core/generated/KotlinLibrariesDefaultData.kt
@@ -12,8 +12,8 @@ internal val DEFAULT_KOTLIN_LIBRARIES_DATA = KotlinLibrariesCompatibilityState(
             groupId = "org.jetbrains.kotlinx",
             artifactId = "kotlinx-coroutines-core",
             versions = mapOf(
-                    "2.0" to "1.8.1",
-                    "2.1" to "1.8.1",
+                    "2.0" to "1.9.0",
+                    "2.1" to "1.9.0",
                     "1.5" to "1.5.2",
                     "1.7" to "1.6.4",
                     "1.9" to "1.8.1",
@@ -26,6 +26,7 @@ internal val DEFAULT_KOTLIN_LIBRARIES_DATA = KotlinLibrariesCompatibilityState(
             artifactId = "kotlinx-datetime",
             versions = mapOf(
                     "2.0" to "0.6.1",
+                    "2.1" to "0.6.1",
                     "1.5" to "0.3.1",
                     "1.7" to "0.4.0",
                     "1.9" to "0.6.1",
@@ -37,7 +38,8 @@ internal val DEFAULT_KOTLIN_LIBRARIES_DATA = KotlinLibrariesCompatibilityState(
             groupId = "org.jetbrains.kotlinx",
             artifactId = "kotlinx-serialization-json",
             versions = mapOf(
-                    "2.0" to "1.7.2",
+                    "2.0" to "1.7.3",
+                    "2.1" to "1.7.3",
                     "1.5" to "1.2.2",
                     "1.7" to "1.4.1",
                     "1.9" to "1.6.3",
