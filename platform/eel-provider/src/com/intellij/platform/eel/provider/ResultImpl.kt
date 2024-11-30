f995eb5daf6f841467cbb5f68b70c06ffec3ef06
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2024-11-30 01:37:07 +0100

    Eel: Move result impl to provider.
    All utils must be there
    
    GitOrigin-RevId: 351011f2c68a1f5ce7eebbefe9468b114e3c300c

--- platform/eel-provider/src/com/intellij/platform/eel/provider/ResultImpl.kt
+++ platform/eel-provider/src/com/intellij/platform/eel/provider/ResultImpl.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.eel.impl
+package com.intellij.platform.eel.provider
 
 import com.intellij.platform.eel.EelResult
 
