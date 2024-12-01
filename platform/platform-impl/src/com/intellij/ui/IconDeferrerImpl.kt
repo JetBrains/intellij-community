7b19abf3300a0d35dce7b793fea0c909734fbd11
Max Medvedev <maxim.medvedev@jetbrains.com>
2024-12-01 14:20:44 +0100

    IJPL-166213 clear IconDeferrerImpl every 30 sec if the cache contains more than 1000 items
    GitOrigin-RevId: 6168ad42fffae949243a9d0cfde765ab2fb373b2

--- platform/platform-impl/src/com/intellij/ui/IconDeferrerImpl.kt
+++ platform/platform-impl/src/com/intellij/ui/IconDeferrerImpl.kt
@@ -1,10 +1,12 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 @file:Suppress("ReplaceGetOrSet", "ReplacePutWithAssignment")
 
 package com.intellij.ui
 
 import com.intellij.ide.ui.VirtualFileAppearanceListener
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.debug
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectCloseListener
 import com.intellij.openapi.util.LowMemoryWatcher
@@ -12,13 +14,13 @@ import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.openapi.vfs.newvfs.BulkFileListener
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
 import com.intellij.psi.util.PsiModificationTracker
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.asContextElement
-import kotlinx.coroutines.job
-import kotlinx.coroutines.withContext
+import com.intellij.util.SystemProperties
+import kotlinx.coroutines.*
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.LongAdder
 import javax.swing.Icon
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.DurationUnit
 
 internal class IconDeferrerImpl(coroutineScope: CoroutineScope) : IconDeferrer() {
   companion object {
@@ -35,37 +37,73 @@ internal class IconDeferrerImpl(coroutineScope: CoroutineScope) : IconDeferrer()
         isEvaluationInProgress.set(false)
       }
     }
+
+    private val log = logger<IconDeferrerImpl>()
   }
 
   // Due to a critical bug (https://youtrack.jetbrains.com/issue/IDEA-320644/Improve-Smart-PSI-pointer-equals-implementation),
   // we are not using "caffeine".
-  // Furthermore,  a size-bounded cache is unnecessary for us because our application has frequent cache clearances,
-  // such as during PSI modifications.
   private val iconCache = ConcurrentHashMap<Any, Icon>()
   @Volatile
   private var mightBePopulated = false // used to avoid multiple calls CHM#clear() which might be expensive, no need to be atomic or something else
   private val lastClearTimestamp = LongAdder()
 
+  // we restrict max cache size for cases when a user reads code only.
+  private val maxCacheSize = SystemProperties.getLongProperty("ide.icons.deferrerCacheSize", 1000)
+  private val deferrerCacheClearingCheckPeriod= SystemProperties.getLongProperty("ide.icons.deferrerCacheClearingCheckPeriod.ms", 30.seconds.toLong(DurationUnit.MILLISECONDS))
+
   init {
     val connection = ApplicationManager.getApplication().messageBus.simpleConnect()
-    connection.subscribe(PsiModificationTracker.TOPIC, PsiModificationTracker.Listener(::clearCache))
+    connection.subscribe(PsiModificationTracker.TOPIC, PsiModificationTracker.Listener {
+      log.debug("clearing icon deferrer cache after psi modification")
+      clearCache()
+    })
+
     // update "locked" icon
     connection.subscribe(VirtualFileManager.VFS_CHANGES, object : BulkFileListener {
       override fun after(events: List<VFileEvent>) {
+        log.debug("clearing icon deferrer cache after vfs events")
         clearCache()
       }
     })
     connection.subscribe(ProjectCloseListener.TOPIC, object : ProjectCloseListener {
       override fun projectClosed(project: Project) {
+        log.debug("clearing icon deferrer cache after project closing")
         clearCache()
       }
     })
-    connection.subscribe(VirtualFileAppearanceListener.TOPIC, VirtualFileAppearanceListener { clearCache() })
+    connection.subscribe(VirtualFileAppearanceListener.TOPIC, VirtualFileAppearanceListener {
+      log.debug("clearing icon deferrer cache after VirtualFileAppearanceListener event")
+      clearCache()
+    })
 
-    val lowMemoryWatcher = LowMemoryWatcher.register(::clearCache)
+    val lowMemoryWatcher = LowMemoryWatcher.register {
+      log.debug("Clearing icon deferrer cache because after low memory signal")
+      clearCache()
+    }
+
+    scheduleCacheClearingTask(coroutineScope)
+
     coroutineScope.coroutineContext.job.invokeOnCompletion {
       connection.disconnect()
       lowMemoryWatcher.stop()
+    }
+  }
+
+  /** clears cache once in a while if the cache gets too big */
+  private fun scheduleCacheClearingTask(coroutineScope: CoroutineScope) {
+    coroutineScope.launch {
+      while (true) {
+        delay(deferrerCacheClearingCheckPeriod)
+        val currentCacheSize = iconCache.size
+        if (currentCacheSize > maxCacheSize) {
+          log.debug { "Clearing icon deferrer cache because it's too big: $currentCacheSize > $maxCacheSize" }
+          clearCache()
+        }
+        else {
+          log.debug { "icon cache size $currentCacheSize" }
+        }
+      }
     }
   }
 
