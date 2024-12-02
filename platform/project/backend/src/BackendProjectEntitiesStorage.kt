ac8bf92912ee5094499fb9c880c1557a4a02f03f
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2024-12-02 15:29:13 +0100

    Revert: "[rd] IJPL-171239: Frontend should wait for ProjectEntity creation on the backend"
    Revert everything, even modules' dependencies, since tests started to fail
    
    GitOrigin-RevId: 73d145c4c97f41f7566e6c071634acd3704632c0

--- platform/project/backend/src/BackendProjectEntitiesStorage.kt
+++ platform/project/backend/src/BackendProjectEntitiesStorage.kt
@@ -15,7 +15,8 @@ import fleet.kernel.transactor
 
 internal class BackendProjectEntitiesStorage : ProjectEntitiesStorage() {
   override suspend fun createEntityImpl(project: Project) {
-    val projectId = project.projectId()
+    // migrate to the implementation below when IJPL-172500 is investigated and a fix has been found
+    //val projectId = project.projectId()
     //change {
     //  shared {
     //    ProjectEntity.new {
@@ -23,31 +24,8 @@ internal class BackendProjectEntitiesStorage : ProjectEntitiesStorage() {
     //    }
     //  }
     //}
+    //LOG.info("Project entity created for $projectId")
 
-    // migrate to the implementation above when IJPL-172500 is investigated and a fix has been found
-    change {
-      shared {
-        /*
-        This check is added to ensure that only one ProjectEntity is going to be created in split mode.
-        Two entities are possible due to a different flow in creating a project in split mode.
-
-        First, a project is created on the backend (ProjectEntity is created at the same time).
-        Then a signal about project creation is sent to the frontend via RD protocol.
-        At the same time, the shared part of Rhizome DB (where ProjectEntity is stored) sends the changes to the frontend.
-
-        Events which are coming via RD protocol are not synced with events coming via Rhizome DB.
-        So it can happen that while on the backend the signal is sent strictly after ProjectEntity creation,
-        on the frontend the signal can be received before there is ProjectEntity available in DB.
-
-        If it happens that the entity has not been found and the frontend creates a new one, Rhizome DB will perform a "rebase"
-        which basically re-invokes the whole "change" block either on the backend or the frontend side.
-        */
-        ProjectEntity.upsert(ProjectEntity.ProjectIdValue, projectId) {
-          it[ProjectEntity.ProjectIdValue] = projectId
-        }
-      }
-    }
-    LOG.info("Project entity created for $projectId")
   }
 
   override suspend fun removeProjectEntity(project: Project): Unit = withKernel {
