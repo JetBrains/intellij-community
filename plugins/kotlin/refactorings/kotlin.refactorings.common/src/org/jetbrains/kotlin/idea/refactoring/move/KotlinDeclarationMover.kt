a7c9736c02ca16aef6f287a0b795c0e227fb18e2
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2024-12-02 22:04:23 +0100

    [kotlin] Added frontend-independent KotlinMover replacement
    The implementation is the exact same as for K1, but the old one is left in-place for compatibility reasons.
    
    KTIJ-32332
    
    GitOrigin-RevId: 1eec795372b7701a8a599795ea36048bb28c8e28

--- plugins/kotlin/refactorings/kotlin.refactorings.common/src/org/jetbrains/kotlin/idea/refactoring/move/KotlinDeclarationMover.kt
+++ plugins/kotlin/refactorings/kotlin.refactorings.common/src/org/jetbrains/kotlin/idea/refactoring/move/KotlinDeclarationMover.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.refactoring.move
 
 import com.intellij.psi.search.LocalSearchScope
@@ -9,18 +9,28 @@ import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
 import org.jetbrains.kotlin.psi.psiUtil.getElementTextWithContext
 import org.jetbrains.kotlin.utils.KotlinExceptionWithAttachments
 
-@Deprecated("This declaration is K1 specific, please use the frontend-independent KotlinDeclarationMover instead.")
-interface KotlinMover : (KtNamedDeclaration, KtElement) -> KtNamedDeclaration {
-    @Deprecated("This declaration is K1 specific, please use the frontend-independent KotlinDeclarationMover instead.")
-    object Default : KotlinMover {
-        override fun invoke(originalElement: KtNamedDeclaration, targetContainer: KtElement): KtNamedDeclaration {
-            @Suppress("DuplicatedCode")
+interface KotlinDeclarationMover {
+    /**
+     * Moves the [originalElement] from its original location into the given [targetContainer].
+     * Implementations may work differently depending on the source or target and may
+     * implement post-processing such as deleting empty source containers after moving.
+     */
+    fun moveDeclaration(originalElement: KtNamedDeclaration, targetContainer: KtElement): KtNamedDeclaration?
+
+    /**
+     * This mover moves the given declaration into its target.
+     * If the source container is a companion object that is empty after moving, it is deleted after the move.
+     */
+    object Default : KotlinDeclarationMover {
+        override fun moveDeclaration(originalElement: KtNamedDeclaration, targetContainer: KtElement): KtNamedDeclaration? {
+            @Suppress("DuplicatedCode") // this is a duplicate of the K1 counterpart, which remains for compatibility reasons
             return when (targetContainer) {
                 is KtFile -> {
                     val declarationContainer: KtElement =
                         if (targetContainer.isScript()) targetContainer.script!!.blockExpression else targetContainer
                     declarationContainer.add(originalElement) as KtNamedDeclaration
                 }
+
                 is KtClassOrObject -> targetContainer.addDeclaration(originalElement)
                 else -> throw KotlinExceptionWithAttachments("Unexpected element")
                     .withAttachment("context", targetContainer.getElementTextWithContext())
