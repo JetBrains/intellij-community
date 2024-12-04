1d23eb2da635e47e22190076d8260e1a24c4c361
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2024-12-04 20:57:31 +0200

    [Kotlin] Replace wrapped intention quick fix with a mod command
    KTIJ-28971
    
    GitOrigin-RevId: 244dadc16c48319f4765bd4cc5d32851f0339c52

--- plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/NestedLambdaShadowedImplicitParameterInspection.kt
+++ plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/NestedLambdaShadowedImplicitParameterInspection.kt
@@ -3,7 +3,6 @@
 package org.jetbrains.kotlin.idea.k2.codeinsight.inspections
 
 import com.intellij.codeInspection.InspectionManager
-import com.intellij.codeInspection.IntentionWrapper
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
@@ -20,27 +19,19 @@ import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.builtins.StandardNames
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.codeInsight.intentions.fake.ReplaceItWithExplicitFunctionLiteralParamIntention
 import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinApplicableInspectionBase
 import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinModCommandQuickFix
-import org.jetbrains.kotlin.idea.codeinsight.utils.addExplicitItParameter
-import org.jetbrains.kotlin.idea.codeinsight.utils.callExpression
-import org.jetbrains.kotlin.idea.codeinsight.utils.getImplicitLambdaParameterSymbol
-import org.jetbrains.kotlin.idea.codeinsight.utils.isCalling
-import org.jetbrains.kotlin.idea.codeinsight.utils.nonShadowingScopeFunctions
-import org.jetbrains.kotlin.psi.KtLambdaExpression
-import org.jetbrains.kotlin.psi.KtNameReferenceExpression
-import org.jetbrains.kotlin.psi.KtQualifiedExpression
-import org.jetbrains.kotlin.psi.KtVisitor
+import org.jetbrains.kotlin.idea.codeinsight.utils.*
+import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.anyDescendantOfType
 import org.jetbrains.kotlin.psi.psiUtil.getParentOfTypesAndPredicate
 import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
-import org.jetbrains.kotlin.psi.referenceExpressionVisitor
 
 
 internal class NestedLambdaShadowedImplicitParameterInspection :
     KotlinApplicableInspectionBase<KtNameReferenceExpression, NestedLambdaShadowedImplicitParameterInspection.Context>() {
     class Context(
+        val ownerLambdaPointer: SmartPsiElementPointer<KtLambdaExpression>,
         val outerLambdaPointer: SmartPsiElementPointer<KtLambdaExpression>,
     )
 
@@ -60,8 +51,15 @@ internal class NestedLambdaShadowedImplicitParameterInspection :
             /* descriptionTemplate = */ getProblemDescription(),
             /* highlightType = */ ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
             /* onTheFly = */ onTheFly,
-            /* ...fixes = */ RenameOuterLambdaParameterFix(context.outerLambdaPointer),
-            IntentionWrapper(ReplaceItWithExplicitFunctionLiteralParamIntention()),
+            /* ...fixes = */
+            RenameLambdaParameterFix(
+                context.ownerLambdaPointer,
+                KotlinBundle.message("replace.it.with.explicit.parameter"),
+            ),
+            RenameLambdaParameterFix(
+                context.outerLambdaPointer,
+                KotlinBundle.message("add.explicit.parameter.to.outer.lambda.fix.text"),
+            ),
         )
     }
 
@@ -87,18 +85,18 @@ internal class NestedLambdaShadowedImplicitParameterInspection :
     /**
      * Report warning if:
      * — referenced expression is an implicit `it` reference
-     * — some outer lambda also has an implicit `it` that has usages
-     * — owner of the `it` parameter is not a scope function (`also`, `let`, `takeIf`, `takeUnless`) called on an outer `it`
+     * — some other outer lambda also has an implicit `it` that has usages
+     * — owner of the referenced `it` parameter is not a scope function call (`also`, `let`, `takeIf`, `takeUnless`) on an outer `it`
      */
     context(KaSession)
     override fun prepareContext(element: KtNameReferenceExpression): Context? {
         analyze(element) {
             val enclosingLambdaImplicitItSymbol = element.getImplicitLambdaParameterSymbol()
             if (enclosingLambdaImplicitItSymbol == null) return null
-            val enclosingLambda = enclosingLambdaImplicitItSymbol.psi?.getStrictParentOfType<KtLambdaExpression>() ?: return null
-            if (enclosingLambda.isAllowedScopeFunctionLambdaArgument()) return null
+            val ownerLambda = enclosingLambdaImplicitItSymbol.psi?.getStrictParentOfType<KtLambdaExpression>() ?: return null
+            if (ownerLambda.isAllowedScopeFunctionLambdaArgument()) return null
 
-            val outermostLambdaWithoutParams = enclosingLambda.findTopmostParentInFile {
+            val outermostLambdaWithoutParams = ownerLambda.findTopmostParentInFile {
                 it is KtLambdaExpression && it.valueParameters.isEmpty()
             } as? KtLambdaExpression ?: return null
 
@@ -106,7 +104,7 @@ internal class NestedLambdaShadowedImplicitParameterInspection :
             outermostLambdaWithoutParams.anyDescendantOfType<KtNameReferenceExpression> { referenceExpression ->
                 if (referenceExpression.getReferencedNameAsName() != StandardNames.IMPLICIT_LAMBDA_PARAMETER_NAME)
                     return@anyDescendantOfType false
-                if (PsiTreeUtil.isAncestor(enclosingLambda, referenceExpression, false))
+                if (PsiTreeUtil.isAncestor(ownerLambda, referenceExpression, false))
                     return@anyDescendantOfType false
                 val implicitItSymbol = referenceExpression.getImplicitLambdaParameterSymbol()
                 if (implicitItSymbol == null)
@@ -118,7 +116,9 @@ internal class NestedLambdaShadowedImplicitParameterInspection :
                     false
                 }
             }
-            return parentLambdaWithUsedImplicitIt?.let { Context(it.createSmartPointer()) }
+            return parentLambdaWithUsedImplicitIt?.let { outerLambda ->
+                Context(ownerLambda.createSmartPointer(), outerLambda.createSmartPointer())
+            }
         }
     }
 
@@ -130,19 +130,18 @@ internal class NestedLambdaShadowedImplicitParameterInspection :
     }
 }
 
-private class RenameOuterLambdaParameterFix(
-    private val outerLambda: SmartPsiElementPointer<KtLambdaExpression>
+private class RenameLambdaParameterFix(
+    private val lambdaExpressionReference: SmartPsiElementPointer<KtLambdaExpression>,
+    private val familyName: @IntentionFamilyName String,
 ) : KotlinModCommandQuickFix<KtNameReferenceExpression>() {
-    override fun getFamilyName(): @IntentionFamilyName String {
-        return KotlinBundle.message("add.explicit.parameter.to.outer.lambda.fix.text")
-    }
+    override fun getFamilyName(): @IntentionFamilyName String = familyName
 
     override fun applyFix(
         project: Project,
         element: KtNameReferenceExpression,
         updater: ModPsiUpdater
     ) {
-        val lambdaExpression = outerLambda.dereference()?.let(updater::getWritable) ?: return
+        val lambdaExpression = lambdaExpressionReference.dereference()?.let(updater::getWritable) ?: return
         val lambdaParameter = lambdaExpression.addExplicitItParameter()
         updater.rename(lambdaParameter, listOf())
     }
