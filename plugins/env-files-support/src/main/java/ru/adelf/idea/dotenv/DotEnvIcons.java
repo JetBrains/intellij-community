4c79c184bb114286414e7200a32a4f23dd0e5e6c
Artemy Pestretsov <artemy.pestretsov@jetbrains.com>
2024-12-04 22:58:19 +0100

    IJ-CR-151119 [dotenv] regenerate plugin icon
    (cherry picked from commit 6633e5311ed551d26f9119d407c432e3b17f10bf)
    
    GitOrigin-RevId: 05e7196bc26bcc3e1bafc1d96409f1b884a51771

--- plugins/env-files-support/src/main/java/ru/adelf/idea/dotenv/DotEnvIcons.java
+++
@@ -1,9 +0,0 @@
-package ru.adelf.idea.dotenv;
-
-import com.intellij.openapi.util.IconLoader;
-
-import javax.swing.*;
-
-public class DotEnvIcons {
-    public static final Icon icon = IconLoader.getIcon("/icons/env.png", DotEnvIcons.class);
-}
