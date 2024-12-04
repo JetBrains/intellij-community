b8a3be453b2a9b835090e92cc98777cb280937e9
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2024-12-04 13:06:25 +0100

    [maven] IDEA-364046 convert MavenEnvironmentFormTest to kotlin
    GitOrigin-RevId: a59570747a7dd0b816673a43ff2273b4e31af306

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/project/MavenEnvironmentFormTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/project/MavenEnvironmentFormTest.kt
@@ -1,70 +1,75 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.maven.project;
+package org.jetbrains.idea.maven.project
 
-import com.intellij.maven.testFramework.MavenTestCase;
-import com.intellij.openapi.command.impl.DummyProject;
-import com.intellij.testFramework.UsefulTestCase;
-import com.intellij.ui.TextFieldWithHistory;
-import com.intellij.util.Consumer;
-import com.intellij.util.ReflectionUtil;
-import org.jetbrains.idea.maven.server.MavenDistributionsCache;
-import org.jetbrains.idea.maven.server.MavenServerManager;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import com.intellij.maven.testFramework.MavenTestCase
+import com.intellij.openapi.command.impl.DummyProject
+import com.intellij.ui.TextFieldWithHistory
+import com.intellij.util.ReflectionUtil
+import junit.framework.TestCase
+import org.jetbrains.idea.maven.project.BundledMaven3.title
+import org.jetbrains.idea.maven.server.MavenDistributionsCache
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-import java.lang.reflect.Field;
-import java.util.Collections;
-
-@RunWith(JUnit4.class)
-public class MavenEnvironmentFormTest extends MavenTestCase {
-
+@RunWith(JUnit4::class)
+class MavenEnvironmentFormTest : MavenTestCase() {
   @Test
-  public void shouldNotShowDuplicatedBundledMavenHome() {
-    MavenGeneralPanel panel = new MavenGeneralPanel();
-    assertThat(panel,
-               t -> assertContainsElements(t.getHistory(),
-                                           Collections.singleton(BundledMaven3.INSTANCE.getTitle())));
-    assertThat(panel,
-               t -> assertDoesntContain(t.getHistory(),
-                                        MavenDistributionsCache.resolveEmbeddedMavenHome().getMavenHome().toAbsolutePath().toString()));
-  }
+  fun shouldNotShowDuplicatedBundledMavenHome() {
+    val panel = MavenGeneralPanel()
 
-  @Test
-  public void shouldSetBundledMavenIfSetAbsolutePath() {
-    MavenGeneralSettings settings = new MavenGeneralSettings();
-    MavenGeneralPanel panel = new MavenGeneralPanel();
-    settings.setMavenHomeType(BundledMaven3.INSTANCE);
-    panel.initializeFormData(settings,  DummyProject.getInstance());
-    assertThat(panel,
-               t -> assertEquals("Absolute path to bundled maven should resolve to bundle", BundledMaven3.INSTANCE.getTitle(),
-                                 t.getText()));
-  }
-
-  @Test
-  public void shouldNotSetBundledMavenIfAnotherMavenSet() {
-    MavenGeneralSettings settings = new MavenGeneralSettings();
-    MavenGeneralPanel panel = new MavenGeneralPanel();
-    settings.setMavenHomeType(new MavenInSpecificPath("/path/to/maven/home"));
-    panel.initializeFormData(settings, DummyProject.getInstance());
-    assertThat(panel,
-               t -> assertEquals("/path/to/maven/home", t.getText()));
-  }
-
-  private void assertThat(MavenGeneralPanel configurable, Consumer<TextFieldWithHistory> checker) {
-    MavenEnvironmentForm form = getValue(MavenEnvironmentForm.class, configurable, "mavenPathsForm");
-    TextFieldWithHistory mavenHomeField = getValue(TextFieldWithHistory.class, form, "mavenHomeField");
-    checker.consume(mavenHomeField);
-  }
-
-  protected <T> T getValue(Class<T> fieldClass, Object object, String name) {
-    try {
-      Field field = ReflectionUtil.findAssignableField(object.getClass(), fieldClass, name);
-      return ReflectionUtil.getFieldValue(field, object);
+    assertThat(panel) { t ->
+      assertContainsElements(
+        t!!.history,
+        setOf(title)
+      )
     }
-    catch (NoSuchFieldException e) {
-      fail("No such field " + name + " in " + object);
-      return null;
+
+    assertThat(panel) { t ->
+      assertDoesntContain(
+        t!!.history,
+        MavenDistributionsCache.resolveEmbeddedMavenHome().mavenHome.toAbsolutePath().toString()
+      )
+    }
+  }
+
+  @Test
+  fun shouldSetBundledMavenIfSetAbsolutePath() {
+    val settings = MavenGeneralSettings()
+    val panel = MavenGeneralPanel()
+    settings.mavenHomeType = BundledMaven3
+    panel.initializeFormData(settings, DummyProject.getInstance())
+    assertThat(panel) { t: TextFieldWithHistory? ->
+      TestCase.assertEquals("Absolute path to bundled maven should resolve to bundle", title, t!!.text)
+    }
+  }
+
+  @Test
+  fun shouldNotSetBundledMavenIfAnotherMavenSet() {
+    val settings = MavenGeneralSettings()
+    val panel = MavenGeneralPanel()
+    settings.mavenHomeType = MavenInSpecificPath("/path/to/maven/home")
+    panel.initializeFormData(settings, DummyProject.getInstance())
+    assertThat(panel) { t: TextFieldWithHistory? -> TestCase.assertEquals("/path/to/maven/home", t!!.text) }
+  }
+
+  private fun assertThat(
+    configurable: MavenGeneralPanel,
+    checker: (TextFieldWithHistory?) -> Unit,
+  ) {
+    val form = getValue(MavenEnvironmentForm::class.java, configurable, "mavenPathsForm")
+    val mavenHomeField = getValue(TextFieldWithHistory::class.java, form, "mavenHomeField")
+    checker(mavenHomeField)
+  }
+
+  protected fun <T> getValue(fieldClass: Class<T>, o: Any?, name: String): T? {
+    try {
+      val field = ReflectionUtil.findAssignableField(o!!.javaClass, fieldClass, name)
+      return ReflectionUtil.getFieldValue<T>(field, o)
+    }
+    catch (_: NoSuchFieldException) {
+      fail("No such field $name in $o")
+      return null
     }
   }
 }
