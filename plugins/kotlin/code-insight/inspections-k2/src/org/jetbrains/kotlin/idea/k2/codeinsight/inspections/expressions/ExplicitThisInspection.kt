ce583147faae503ddfe9be93da12ef01f5ee9fc6
Anna Kozlova <anna.kozlova@jetbrains.com>
2024-12-05 16:04:42 +0100

    [kotlin] k2: ExplicitThisInspection cleanups
    KTIJ-31879
    
    GitOrigin-RevId: 09a261dd4470c86be51c92c2e76b17e5024223a0

--- plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/expressions/ExplicitThisInspection.kt
+++ plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/expressions/ExplicitThisInspection.kt
@@ -9,6 +9,7 @@ import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.components.ShortenOptions
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.asUnit
 import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinApplicableInspectionBase
 import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinModCommandQuickFix
 import org.jetbrains.kotlin.idea.k2.refactoring.util.ConvertReferenceToLambdaUtil
@@ -20,7 +21,7 @@ import org.jetbrains.kotlin.psi.KtPsiFactory
 import org.jetbrains.kotlin.psi.KtThisExpression
 import org.jetbrains.kotlin.psi.KtVisitorVoid
 
-class ExplicitThisInspection : KotlinApplicableInspectionBase.Simple<KtThisExpression, Unit>() {
+internal class ExplicitThisInspection : KotlinApplicableInspectionBase.Simple<KtThisExpression, Unit>() {
     override fun getProblemDescription(element: KtThisExpression, context: Unit): String =
         KotlinBundle.message("redundant.explicit.this")
 
@@ -37,14 +38,14 @@ class ExplicitThisInspection : KotlinApplicableInspectionBase.Simple<KtThisExpre
     }
 
     context(KaSession)
-    fun checkShortening(el: KtElement): Unit? {
+    private fun checkShortening(el: KtElement): Unit? {
         val shortenCommand = collectPossibleReferenceShortenings(
             el.containingKtFile,
             el.parent.textRange,
             shortenOptions = ShortenOptions.ALL_ENABLED
         )
         val hasShortening = shortenCommand.listOfQualifierToShortenInfo.any { it.qualifierToShorten.element?.receiverExpression == el }
-        return if (!hasShortening) null else Unit
+        return hasShortening.asUnit
     }
 
     context(KaSession)
@@ -53,9 +54,10 @@ class ExplicitThisInspection : KotlinApplicableInspectionBase.Simple<KtThisExpre
 
         if (parent is KtCallableReferenceExpression) {
             val lambdaExpressionText = ConvertReferenceToLambdaUtil.prepareLambdaExpressionText(parent) ?: return null
-            val expression =
-                (KtPsiFactory(element.project).createExpressionCodeFragment(lambdaExpressionText, element)
-                    .getContentElement() as? KtLambdaExpression)?.bodyExpression?.statements?.firstOrNull() as? KtDotQualifiedExpression ?: return null
+            val lambdaCodeFragment = KtPsiFactory(element.project).createExpressionCodeFragment(lambdaExpressionText, element)
+            val lambdaExpression = lambdaCodeFragment.getContentElement() as? KtLambdaExpression
+            val lambdaBodyStatements = lambdaExpression?.bodyExpression?.statements
+            val expression = lambdaBodyStatements?.firstOrNull() as? KtDotQualifiedExpression ?: return null
             return analyze(expression) {
                 checkShortening(expression.receiverExpression)
             }
@@ -70,23 +72,17 @@ class ExplicitThisInspection : KotlinApplicableInspectionBase.Simple<KtThisExpre
     ): KotlinModCommandQuickFix<KtThisExpression> = ExplicitThisExpressionFix(element.text)
 }
 
-class ExplicitThisExpressionFix(private val text: String) : KotlinModCommandQuickFix<KtThisExpression>() {
+internal class ExplicitThisExpressionFix(private val text: String) : KotlinModCommandQuickFix<KtThisExpression>() {
     override fun getFamilyName(): String = KotlinBundle.message("explicit.this.expression.fix.family.name", text)
 
     override fun applyFix(
         project: Project,
-        element: KtThisExpression,
+        thisExpression: KtThisExpression,
         updater: ModPsiUpdater
     ) {
-        removeExplicitThisExpression(element)
-    }
-
-    companion object {
-        fun removeExplicitThisExpression(thisExpression: KtThisExpression) {
-            when (val parent = thisExpression.parent) {
-                is KtDotQualifiedExpression -> parent.replace(parent.selectorExpression ?: return)
-                is KtCallableReferenceExpression -> thisExpression.delete()
-            }
+        when (val parent = thisExpression.parent) {
+            is KtDotQualifiedExpression -> parent.replace(parent.selectorExpression ?: return)
+            is KtCallableReferenceExpression -> thisExpression.delete()
         }
     }
 }
