f873c09975f3262558093855d04474598356d2a5
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2024-12-09 19:20:03 +0100

    diff: move diff in editor & combined diff actions on top in the settings popup
    GitOrigin-RevId: 003ba35922d17136f86d1f5563dab6e4e1f4c9ab

--- platform/ide-core/src/com/intellij/openapi/actionSystem/IdeActions.java
+++ platform/ide-core/src/com/intellij/openapi/actionSystem/IdeActions.java
@@ -191,6 +191,7 @@ public interface IdeActions {
   String DIFF_VIEWER_POPUP = "Diff.ViewerPopupMenu";
   String DIFF_VIEWER_TOOLBAR = "Diff.ViewerToolbar";
   String GROUP_DIFF_EDITOR_GUTTER_POPUP = "Diff.EditorGutterPopupMenu";
+  String GROUP_DIFF_EDITOR_MODES = "Diff.EditorGutterPopupMenu.DiffModes";
   String GROUP_DIFF_EDITOR_SETTINGS = "Diff.EditorGutterPopupMenu.EditorSettings";
 
   String ACTION_EXPAND_ALL = "ExpandAll";
