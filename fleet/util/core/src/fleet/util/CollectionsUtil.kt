d848944d9fde96cc7302279ce30340be642ed92e
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2024-12-09 11:16:12 +0100

    [fleet, collections] introduce fleet toSortedSet util function
    - Provides a pure Kotlin implementation of toSortedSet()
    - We could use expect/actuals and jvm's toSortedSet instead
    
    GitOrigin-RevId: 58a8deac9b980e4fac591079b195e3cd8e517672

--- fleet/util/core/src/fleet/util/CollectionsUtil.kt
+++ fleet/util/core/src/fleet/util/CollectionsUtil.kt
@@ -1,10 +1,14 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package fleet.util
 
+import fleet.util.bifurcan.SortedMap
 import fleet.util.logging.KLogger
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.transformWhile
+import kotlin.collections.component1
+import kotlin.collections.component2
+import kotlin.collections.forEach
 
 inline fun <T> Iterable<T>.forEachSafely(logger: KLogger, f: (T) -> Unit) {
   forEach {
@@ -135,4 +139,46 @@ fun <T : Comparable<T>> max(c1: T, c2: T): T {
     x < 0 -> c2
     else -> c1
   }
+}
+
+fun <T> Iterable<T>.toSortedSet(comparator: Comparator<T>): SortedSet<T> {
+  val result = SortedMap<T, Unit?>(comparator).linear()
+  forEach { e -> result.put(e, Unit) }
+  return SortedSet<T>(result.forked())
+}
+
+fun <T : Comparable<T>> Iterable<T>.toSortedSet(): SortedSet<T> {
+  val result = SortedMap<T, Unit?>().linear()
+  forEach { e -> result.put(e, Unit) }
+  return SortedSet<T>(result.forked())
+}
+
+fun <K : Comparable<K>, V> Map<out K, V>.toSortedMap(): SortedMap<out K, V> {
+  return SortedMap.from(this)
+}
+
+fun <K, V> Map<out K, V>.toSortedMap(comparator: Comparator<K>): SortedMap<K, V> {
+  val result = SortedMap<K, V>(comparator).linear()
+  forEach { (key, value) -> result.put(key, value) }
+  return result.forked()
+}
+
+fun <K : Comparable<K>, V> sortedMapOf(vararg pairs: Pair<K, V>): SortedMap<K, V> {
+  val result = SortedMap<K, V>().linear()
+  pairs.forEach { (key, value) -> result.put(key, value) }
+  return result.forked()
+}
+
+inline fun <K, V, R, M : SortedMap<in K, in R>> Map<out K, V>.mapValuesTo(destination: M, transform: (Map.Entry<K, V>) -> R): M {
+  return entries.associateByTo(destination, { it.key }, transform)
+}
+
+inline fun <T, K, V, M : SortedMap<in K, in V>> Iterable<T>.associateByTo(destination: M, keySelector: (T) -> K, valueTransform: (T) -> V): M {
+  val forked = destination.linear()
+  for (element in this) {
+    forked.put(keySelector(element), valueTransform(element))
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  return forked.forked() as M
 }
