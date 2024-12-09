2a4bd600830995912a4763519da21c130bcfb0f4
Alexey Belkov <alexey.belkov@jetbrains.com>
2024-12-09 15:28:15 +0400

    [kotlin] SimplifyWhenWithBooleanConstantConditionInspection: Analysis API, Mod command
    KTIJ-31943
    
    GitOrigin-RevId: 51dffe6dc7872e4082d4b94c65f3175bb06a42a5

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/SimplifyWhenWithBooleanConstantConditionInspection.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/SimplifyWhenWithBooleanConstantConditionInspection.kt
@@ -1,54 +1,67 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.inspections
 
-import com.intellij.codeInspection.*
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.codeInspection.util.InspectionMessage
+import com.intellij.modcommand.ModPsiUpdater
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.util.startOffset
+import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.caches.resolve.analyze
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.inspections.AbstractKotlinInspection
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinApplicableInspectionBase
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinModCommandQuickFix
+import org.jetbrains.kotlin.idea.codeinsight.utils.ConstantConditionIfUtils.replaceWithBranch
 import org.jetbrains.kotlin.idea.codeinsight.utils.isFalseConstant
 import org.jetbrains.kotlin.idea.codeinsight.utils.isTrueConstant
-import org.jetbrains.kotlin.idea.util.replaceWithBranchAndMoveCaret
+import org.jetbrains.kotlin.idea.codeinsights.impl.base.applicators.ApplicabilityRanges
+import org.jetbrains.kotlin.idea.inspections.SimplifyWhenWithBooleanConstantConditionInspection.Context
 import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
-import org.jetbrains.kotlin.resolve.bindingContextUtil.isUsedAsExpression
 
-internal class SimplifyWhenWithBooleanConstantConditionInspection : AbstractKotlinInspection() {
-    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean, session: LocalInspectionToolSession): PsiElementVisitor {
-        return whenExpressionVisitor(fun(whenExpression: KtWhenExpression) {
-            if (whenExpression.closeBrace == null) return
-            if (whenExpression.subjectExpression != null) return
-            if (whenExpression.entries.none { it.isTrueConstantCondition() || it.isFalseConstantCondition() }) return
+internal class SimplifyWhenWithBooleanConstantConditionInspection : KotlinApplicableInspectionBase.Simple<KtWhenExpression, Context>() {
+    data class Context(val isUsedAsExpression: Boolean)
 
-            holder.registerProblem(
-                whenExpression.whenKeyword,
-                KotlinBundle.message("this.when.is.simplifiable"),
-                ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
-                SimplifyWhenFix()
-            )
-        })
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): KtVisitorVoid = object : KtVisitorVoid() {
+        override fun visitWhenExpression(expression: KtWhenExpression) {
+            visitTargetElement(expression, holder, isOnTheFly)
+        }
     }
+
+    override fun getProblemDescription(element: KtWhenExpression, context: Context): @InspectionMessage String =
+        KotlinBundle.message("this.when.is.simplifiable")
+
+    override fun getApplicableRanges(element: KtWhenExpression): List<TextRange> =
+        ApplicabilityRanges.whenKeyword(element)
+
+    override fun isApplicableByPsi(element: KtWhenExpression): Boolean {
+        if (element.closeBrace == null) return false
+        if (element.subjectExpression != null) return false
+        if (element.entries.none { it.isTrueConstantCondition() || it.isFalseConstantCondition() }) return false
+        return true
+    }
+
+    context(KaSession)
+    override fun prepareContext(element: KtWhenExpression): Context {
+        return Context(element.isUsedAsExpression)
+    }
+
+    override fun createQuickFix(element: KtWhenExpression, context: Context): KotlinModCommandQuickFix<KtWhenExpression> =
+        SimplifyFix(context)
 }
 
-private class SimplifyWhenFix : LocalQuickFix {
-    override fun getName(): String = KotlinBundle.message("simplify.when.fix.text")
+private class SimplifyFix(private val context: Context) : KotlinModCommandQuickFix<KtWhenExpression>() {
+    override fun getFamilyName(): String = KotlinBundle.message("simplify.when.fix.text")
 
-    override fun getFamilyName(): String = name
-
-    override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
-        val whenExpression = descriptor.psiElement.getStrictParentOfType<KtWhenExpression>() ?: return
-        val closeBrace = whenExpression.closeBrace ?: return
+    override fun applyFix(project: Project, element: KtWhenExpression, updater: ModPsiUpdater) {
         val factory = KtPsiFactory(project)
-        val isUsedAsExpression = whenExpression.isUsedAsExpression(whenExpression.analyze())
-        whenExpression.deleteFalseEntries(isUsedAsExpression)
-        whenExpression.replaceTrueEntry(isUsedAsExpression, closeBrace, factory)
+        val closeBrace = element.closeBrace ?: return
+        element.deleteFalseEntries(context.isUsedAsExpression, updater)
+        element.replaceTrueEntry(context.isUsedAsExpression, closeBrace, factory, updater)
     }
 }
 
-private fun KtWhenExpression.deleteFalseEntries(isUsedAsExpression: Boolean) {
+private fun KtWhenExpression.deleteFalseEntries(isUsedAsExpression: Boolean, updater: ModPsiUpdater) {
     for (entry in entries) {
         if (entry.isFalseConstantCondition()) {
             entry.delete()
@@ -59,11 +72,21 @@ private fun KtWhenExpression.deleteFalseEntries(isUsedAsExpression: Boolean) {
     if (entries.isEmpty() && !isUsedAsExpression) {
         delete()
     } else if (entries.singleOrNull()?.isElse == true) {
-        elseExpression?.let { replaceWithBranchAndMoveCaret(it, isUsedAsExpression) }
+        elseExpression?.let {
+            val replacedBranch = replaceWithBranch(it, isUsedAsExpression)
+            if (replacedBranch != null) {
+                updater.moveCaretTo(replacedBranch.startOffset)
+            }
+        }
     }
 }
 
-private fun KtWhenExpression.replaceTrueEntry(isUsedAsExpression: Boolean, closeBrace: PsiElement, factory: KtPsiFactory) {
+private fun KtWhenExpression.replaceTrueEntry(
+    isUsedAsExpression: Boolean,
+    closeBrace: PsiElement,
+    factory: KtPsiFactory,
+    updater: ModPsiUpdater
+) {
     val entries = entries
     val trueIndex = entries.indexOfFirst { it.isTrueConstantCondition() }
     if (trueIndex == -1) return
@@ -71,7 +94,10 @@ private fun KtWhenExpression.replaceTrueEntry(isUsedAsExpression: Boolean, close
     val expression = entries[trueIndex].expression ?: return
 
     if (trueIndex == 0) {
-        replaceWithBranchAndMoveCaret(expression, isUsedAsExpression)
+        val replacedBranch = replaceWithBranch(expression, isUsedAsExpression)
+        if (replacedBranch != null) {
+            updater.moveCaretTo(replacedBranch.startOffset)
+        }
     } else {
         val elseEntry = factory.createWhenEntry("else -> ${expression.text}")
         for (entry in entries.subList(trueIndex, entries.size)) {
