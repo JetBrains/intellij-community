434808c7cfe62310522be70af4fa4de25a5e7a9f
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-12-10 15:18:58 +0100

    Set since version to 233
    GitOrigin-RevId: bb6a525a0b3f4e2632a6dfd4e1184467cfe03165

--- plugins/mcp-server/build.gradle.kts
+++ plugins/mcp-server/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 intellijPlatform {
     pluginConfiguration {
         ideaVersion {
-            sinceBuild.set("241")
+            sinceBuild.set("233")
             untilBuild.set("243.*")
         }
     }
