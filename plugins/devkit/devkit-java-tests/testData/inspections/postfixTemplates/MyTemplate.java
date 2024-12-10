47d19319593bf3193df9fe4d6b5c1580b3168e88
Yann Cébron <yann.cebron@jetbrains.com>
2024-12-10 10:30:26 +0100

    [devkit] set default level=ERROR for all description inspections (IJPL-173322)
    GitOrigin-RevId: e6aa39b0fb4f17e689aff81a4914d9b14059b113

--- plugins/devkit/devkit-java-tests/testData/inspections/postfixTemplates/MyTemplate.java
+++ plugins/devkit/devkit-java-tests/testData/inspections/postfixTemplates/MyTemplate.java
@@ -1,6 +1,6 @@
 import com.intellij.codeInsight.template.postfix.templates.PostfixTemplate;
 
-public class <warning descr="Postfix template does not have a description">MyTemplate</warning> extends PostfixTemplate  {
+public class <error descr="Postfix template does not have a description">MyTemplate</error> extends PostfixTemplate  {
 
 
 }
