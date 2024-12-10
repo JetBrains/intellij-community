26873cf77acc3d86d1d50d800e873ac0dc761915
roman.ivanitskii <roman.ivanitskii@jetbrains.com>
2024-12-10 11:34:24 +0100

    [performance-plugin] Add read action to the assertOpenedFileInRoot
    GitOrigin-RevId: 97361b35d12ac798b9cf5046302efe4f0499898b

--- plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/commands/AssertOpenedFileInSpecificRoot.kt
+++ plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/commands/AssertOpenedFileInSpecificRoot.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.performancePlugin.commands
 
 import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.ui.playback.PlaybackContext
 import com.intellij.openapi.ui.playback.commands.PlaybackCommandCoroutineAdapter
@@ -21,8 +22,10 @@ class AssertOpenedFileInSpecificRoot(text: String, line: Int) : PlaybackCommandC
       val filePath = text.replace(PREFIX, "").trim()
       val file = findFile(filePath, project) ?: error(PerformanceTestingBundle.message("command.file.not.found", filePath))
       val index = ProjectFileIndex.getInstance(project)
-      if (!index.isInSource(file) && !index.isInTestSourceContent(file)) {
-        throw IllegalStateException("File $file not in test/source root")
+      readAction {
+        if (!index.isInSource(file) && !index.isInTestSourceContent(file)) {
+          throw IllegalStateException("File $file not in test/source root")
+        }
       }
     }
   }
