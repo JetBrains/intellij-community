14a7bbc710b227d5f8df8b7bf5fb95232edfb3fe
Victoria.Petrakovich <victoria.petrakovich@jetbrains.com>
2024-12-10 14:46:12 +0100

    [kotlin] Inherit from `PsiUpdateModCommandAction` in `RenameUnderscoreFix` because we don't use context (it's Unit)
    KTIJ-32041
    
    GitOrigin-RevId: 1cc37a23668f7e3ab98961fb7bd14586ebe892ae

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RenameUnderscoreFix.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RenameUnderscoreFix.kt
@@ -4,20 +4,20 @@ package org.jetbrains.kotlin.idea.quickfix
 import com.intellij.codeInspection.util.IntentionFamilyName
 import com.intellij.modcommand.ActionContext
 import com.intellij.modcommand.ModPsiUpdater
+import com.intellij.modcommand.PsiUpdateModCommandAction
 import com.intellij.psi.PsiNameIdentifierOwner
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.KotlinPsiUpdateModCommandAction
 import org.jetbrains.kotlin.psi.KtDeclaration
 
-class RenameUnderscoreFix(element: KtDeclaration) : KotlinPsiUpdateModCommandAction.ElementBased<KtDeclaration, Unit>(element, Unit) {
+class RenameUnderscoreFix(element: KtDeclaration) : PsiUpdateModCommandAction<KtDeclaration>(element) {
+
     override fun getFamilyName(): @IntentionFamilyName String {
         return KotlinBundle.message("rename.identifier.fix.text")
     }
 
     override fun invoke(
-        actionContext: ActionContext,
+        context: ActionContext,
         element: KtDeclaration,
-        elementContext: Unit,
         updater: ModPsiUpdater
     ) {
         if (element !is PsiNameIdentifierOwner) return
