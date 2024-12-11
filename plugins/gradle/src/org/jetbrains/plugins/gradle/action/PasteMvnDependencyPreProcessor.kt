2cef747dedf3b5398cf241eb3ab1f24c183c56af
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2024-12-11 18:29:36 +0100

    [gradle] convert java -> kotlin
    IDEA-340866
    
    closes https://github.com/JetBrains/intellij-community/pull/2868
    
    GitOrigin-RevId: 03994f136820f6402f4d8857a076e5c0dc236703

--- plugins/gradle/src/org/jetbrains/plugins/gradle/action/PasteMvnDependencyPreProcessor.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/action/PasteMvnDependencyPreProcessor.kt
@@ -1,158 +1,154 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.gradle.action;
+package org.jetbrains.plugins.gradle.action
 
-import com.intellij.codeInsight.editorActions.CopyPastePreProcessor;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.RawText;
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiFile;
-import com.intellij.util.JavaXmlDocumentKt;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.plugins.gradle.statistics.GradleActionsUsagesCollector;
-import org.jetbrains.plugins.gradle.util.GradleConstants;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
+import com.intellij.codeInsight.editorActions.CopyPastePreProcessor
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.RawText
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import com.intellij.util.createDocumentBuilder
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.plugins.gradle.statistics.GradleActionsUsagesCollector
+import org.jetbrains.plugins.gradle.statistics.GradleActionsUsagesCollector.trigger
+import org.jetbrains.plugins.gradle.util.GradleConstants
+import org.w3c.dom.Document
+import org.w3c.dom.NodeList
+import org.xml.sax.InputSource
+import org.xml.sax.SAXException
+import java.io.IOException
+import java.io.StringReader
 
-import javax.xml.parsers.DocumentBuilder;
-import java.io.IOException;
-import java.io.StringReader;
+class PasteMvnDependencyPreProcessor : CopyPastePreProcessor {
 
-public final class PasteMvnDependencyPreProcessor implements CopyPastePreProcessor {
-
-  @Nullable
-  @Override
-  public String preprocessOnCopy(PsiFile file, int[] startOffsets, int[] endOffsets, String text) {
-    return null;
+  override fun preprocessOnCopy(file: PsiFile?, startOffsets: IntArray?, endOffsets: IntArray?, text: String?): String? {
+    return null
   }
 
-  @NotNull
-  @Override
-  public String preprocessOnPaste(Project project, PsiFile file, Editor editor, String text, RawText rawText) {
+  override fun preprocessOnPaste(project: Project?, file: PsiFile, editor: Editor?, text: String, rawText: RawText?): String {
     if (isApplicable(file) && isMvnDependency(text)) {
-      GradleActionsUsagesCollector.trigger(project, GradleActionsUsagesCollector.PASTE_MAVEN_DEPENDENCY);
-      boolean isKotlinDsl = isKotlinBuildScriptFile(file.getName());
-      return toGradleDependency(text, isKotlinDsl);
+      trigger(project, GradleActionsUsagesCollector.PASTE_MAVEN_DEPENDENCY)
+      val isKotlinDsl: Boolean = isKotlinBuildScriptFile(file.getName())
+      return toGradleDependency(text, isKotlinDsl)
     }
-    return text;
+    return text
   }
 
-  private boolean isApplicable(PsiFile file) {
-    return file.getName().endsWith('.' + GradleConstants.EXTENSION) || isKotlinBuildScriptFile(file.getName());
+  private fun isApplicable(file: PsiFile): Boolean {
+    return file.getName().endsWith('.'.toString() + GradleConstants.EXTENSION) || isKotlinBuildScriptFile(file.getName())
   }
 
-  private static boolean isKotlinBuildScriptFile(String filename) {
-    return filename.endsWith('.' + GradleConstants.KOTLIN_DSL_SCRIPT_EXTENSION);
+  override fun requiresAllDocumentsToBeCommitted(editor: Editor, project: Project): Boolean {
+    return false
   }
 
-  private static @NotNull String formatGradleDependency(@NotNull String groupId,
-                                                        @NotNull String artifactId,
-                                                        @NotNull String version,
-                                                        @NotNull String scope,
-                                                        @NotNull String classifier,
-                                                        boolean isKotlinDsl) {
-    String gradleClassifier = classifier.isEmpty() ? "" : ":" + classifier;
-    String gradleVersion = version.isEmpty() ? "" : ":" + version;
-    StringBuilder dependency = new StringBuilder()
-      .append(scope)
-      .append(isKotlinDsl ? "(\"" : " '")
-      .append(groupId).append(':').append(artifactId)
-      .append(gradleVersion)
-      .append(gradleClassifier)
-      .append(isKotlinDsl ? "\")" : "'");
-
-    return dependency.toString();
-  }
-
-  @ApiStatus.Internal
-  public static @NotNull String toGradleDependency(@NotNull String mavenDependency, boolean isKotlinDsl) {
-    try {
-      DocumentBuilder builder = JavaXmlDocumentKt.createDocumentBuilder();
-      Document document = builder.parse(new InputSource(new StringReader(mavenDependency)));
-      String gradleDependency = extractGradleDependency(document, isKotlinDsl);
-      return gradleDependency != null ? gradleDependency : mavenDependency;
-    }
-    catch (SAXException | IOException ignored) {
+  companion object {
+    private fun isKotlinBuildScriptFile(filename: String): Boolean {
+      return filename.endsWith('.'.toString() + GradleConstants.KOTLIN_DSL_SCRIPT_EXTENSION)
     }
 
-    return mavenDependency;
-  }
+    private fun formatGradleDependency(
+      groupId: String,
+      artifactId: String,
+      version: String,
+      scope: String,
+      classifier: String,
+      isKotlinDsl: Boolean
+    ): String {
+      val gradleClassifier = if (classifier.isEmpty()) "" else ":" + classifier
+      val gradleVersion = if (version.isEmpty()) "" else ":" + version
+      val dependency = StringBuilder()
+        .append(scope)
+        .append(if (isKotlinDsl) "(\"" else " '")
+        .append(groupId).append(':').append(artifactId)
+        .append(gradleVersion)
+        .append(gradleClassifier)
+        .append(if (isKotlinDsl) "\")" else "'")
 
-  @Nullable
-  private static String extractGradleDependency(Document document, boolean isKotlinDsl) {
-    String groupId = getGroupId(document);
-    String artifactId = getArtifactId(document);
-    String version = getVersion(document);
-    String scope = getScope(document);
-    String classifier = getClassifier(document);
-
-    if (groupId.isEmpty() || artifactId.isEmpty()) {
-      return null;
+      return dependency.toString()
     }
-    return formatGradleDependency(groupId, artifactId, version, scope, classifier, isKotlinDsl);
-  }
 
-  @NotNull
-  private static String getScope(@NotNull Document document) {
-    String scope = firstOrEmpty(document.getElementsByTagName("scope"));
-    return switch (scope) {
-      case "test" -> "testImplementation";
-      case "provided" -> "compileOnly";
-      case "runtime" -> "runtime";
-      case "compile" -> "implementation";
-      default -> "implementation";
-    };
-  }
+    @JvmStatic
+    @ApiStatus.Internal
+    fun toGradleDependency(mavenDependency: String, isKotlinDsl: Boolean): String {
+      try {
+        val builder = createDocumentBuilder()
+        val document = builder.parse(InputSource(StringReader(mavenDependency)))
+        val gradleDependency: String? = extractGradleDependency(document, isKotlinDsl)
+        return if (gradleDependency != null) gradleDependency else mavenDependency
+      }
+      catch (ignored: SAXException) {
+      }
+      catch (ignored: IOException) {
+      }
 
-  private static String getVersion(@NotNull Document document) {
-    return firstOrEmpty(document.getElementsByTagName("version"));
-  }
-
-  private static String getArtifactId(@NotNull Document document) {
-    return firstOrEmpty(document.getElementsByTagName("artifactId"));
-  }
-
-  private static String getGroupId(@NotNull Document document) {
-    return firstOrEmpty(document.getElementsByTagName("groupId"));
-  }
-
-  private static String getClassifier(@NotNull Document document) {
-    return firstOrEmpty(document.getElementsByTagName("classifier"));
-  }
-
-  private static String firstOrEmpty(@NotNull NodeList list) {
-    Node first = list.item(0);
-    return first != null ? first.getTextContent() : "";
-  }
-
-  private static boolean isMvnDependency(String text) {
-    String trimmed = trimLeadingComment(text.trim());
-    if (trimmed.startsWith("<dependency>") && trimmed.endsWith("</dependency>")) {
-      return true;
+      return mavenDependency
     }
-    return false;
-  }
 
-  /**
-   * Removes leading comment, usually it exists if dependency was copied from maven central site
-   */
-  private static String trimLeadingComment(String text) {
-    int start = text.indexOf("<!--");
-    int end = text.indexOf("-->");
-    if (start == 0 && end > 0) {
-      return text.substring(end + "-->".length()).trim();
-    }
-    else {
-      return text;
-    }
-  }
+    private fun extractGradleDependency(document: Document, isKotlinDsl: Boolean): String? {
+      val groupId: String = getGroupId(document)
+      val artifactId: String = getArtifactId(document)
+      val version: String = getVersion(document)
+      val scope: String = getScope(document)
+      val classifier: String = getClassifier(document)
 
-  @Override
-  public boolean requiresAllDocumentsToBeCommitted(@NotNull Editor editor, @NotNull Project project) {
-    return false;
+      if (groupId.isEmpty() || artifactId.isEmpty()) {
+        return null
+      }
+      return formatGradleDependency(groupId, artifactId, version, scope, classifier, isKotlinDsl)
+    }
+
+    private fun getScope(document: Document): String {
+      val scope: String = firstOrEmpty(document.getElementsByTagName("scope"))
+      return when (scope) {
+        "test" -> "testImplementation"
+        "provided" -> "compileOnly"
+        "runtime" -> "runtime"
+        "compile" -> "implementation"
+        else -> "implementation"
+      }
+    }
+
+    private fun getVersion(document: Document): String {
+      return firstOrEmpty(document.getElementsByTagName("version"))
+    }
+
+    private fun getArtifactId(document: Document): String {
+      return firstOrEmpty(document.getElementsByTagName("artifactId"))
+    }
+
+    private fun getGroupId(document: Document): String {
+      return firstOrEmpty(document.getElementsByTagName("groupId"))
+    }
+
+    private fun getClassifier(document: Document): String {
+      return firstOrEmpty(document.getElementsByTagName("classifier"))
+    }
+
+    private fun firstOrEmpty(list: NodeList): String {
+      val first = list.item(0)
+      return if (first != null) first.getTextContent() else ""
+    }
+
+    private fun isMvnDependency(text: String): Boolean {
+      val trimmed: String = trimLeadingComment(text.trim { it <= ' ' })
+      if (trimmed.startsWith("<dependency>") && trimmed.endsWith("</dependency>")) {
+        return true
+      }
+      return false
+    }
+
+    /**
+     * Removes leading comment, usually it exists if dependency was copied from maven central site
+     */
+    private fun trimLeadingComment(text: String): String {
+      val start = text.indexOf("<!--")
+      val end = text.indexOf("-->")
+      if (start == 0 && end > 0) {
+        return text.substring(end + "-->".length).trim { it <= ' ' }
+      }
+      else {
+        return text
+      }
+    }
   }
 }
