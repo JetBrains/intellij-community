181f35e9a3f8476ef74a9d97958d8ef6fdc8381d
Maksim Zuev <maksim.zuev@jetbrains.com>
2024-12-12 10:54:34 +0100

    [debugger] Convert PossiblySyncCommand to Kotlin
    GitOrigin-RevId: 37575e7e0b5870682f6e7a4ea90319c8b39acff5

--- java/debugger/impl/src/com/intellij/debugger/engine/PossiblySyncCommand.kt
+++ java/debugger/impl/src/com/intellij/debugger/engine/PossiblySyncCommand.kt
@@ -1,49 +1,42 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.debugger.engine;
+package com.intellij.debugger.engine
 
-import com.intellij.debugger.engine.events.SuspendContextCommandImpl;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.util.registry.Registry;
-import com.intellij.util.concurrency.AppExecutorUtil;
-import com.jetbrains.jdi.VirtualMachineImpl;
-import com.sun.jdi.VirtualMachine;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.debugger.engine.events.SuspendContextCommandImpl
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.util.registry.Registry.Companion.intValue
+import com.intellij.util.concurrency.AppExecutorUtil
+import com.jetbrains.jdi.VirtualMachineImpl
+import java.util.concurrent.TimeUnit
 
-import java.util.concurrent.TimeUnit;
+/**
+ * The commands inheriting this class are known to be synchronous, so they can prevent
+ * other asynchronous commands to be executed efficiently.
+ * To mitigate such cases, this command is trying to postpone until other async commands are complete.
+ */
+abstract class PossiblySyncCommand protected constructor(suspendContext: SuspendContextImpl?) : SuspendContextCommandImpl(suspendContext) {
+  private var myRetries = intValue("debugger.sync.commands.max.retries")
 
-public abstract class PossiblySyncCommand extends SuspendContextCommandImpl {
-  private int myRetries = Registry.intValue("debugger.sync.commands.max.retries");
-
-  protected PossiblySyncCommand(@Nullable SuspendContextImpl suspendContext) {
-    super(suspendContext);
+  @Throws(Exception::class)
+  final override fun contextAction(suspendContext: SuspendContextImpl) {
+    if (shouldBeRescheduled(suspendContext)) return
+    syncAction(suspendContext)
   }
 
-  @Override
-  public final void contextAction(@NotNull SuspendContextImpl suspendContext) throws Exception {
-    if (!rescheduleIfNotIdle(suspendContext)) {
-      syncAction(suspendContext);
-    }
-  }
+  abstract fun syncAction(suspendContext: SuspendContextImpl)
 
-  public abstract void syncAction(@NotNull SuspendContextImpl suspendContext);
+  private fun shouldBeRescheduled(suspendContext: SuspendContextImpl): Boolean {
+    if (ApplicationManager.getApplication().isUnitTestMode()) return false
 
-  private boolean rescheduleIfNotIdle(@NotNull SuspendContextImpl suspendContext) {
-    int delay = ApplicationManager.getApplication().isUnitTestMode() ? -1 : Registry.intValue("debugger.sync.commands.reschedule.delay");
-    if (delay < 0 || myRetries-- <= 0) {
-      return false;
-    }
-    DebuggerManagerThreadImpl managerThread = suspendContext.getManagerThread();
-    VirtualMachine virtualMachine = suspendContext.getVirtualMachineProxy().getVirtualMachine();
-    if (!(virtualMachine instanceof VirtualMachineImpl) ||
-        !managerThread.hasAsyncCommands() && ((VirtualMachineImpl)virtualMachine).isIdle()) {
-      return false;
-    }
-    else {
-      // reschedule with a small delay
-      hold();
-      AppExecutorUtil.getAppScheduledExecutorService().schedule(() -> managerThread.schedule(this), delay, TimeUnit.MILLISECONDS);
-      return true;
-    }
+    val delay = intValue("debugger.sync.commands.reschedule.delay")
+    if (delay < 0 || myRetries-- <= 0) return false
+
+    val managerThread = suspendContext.managerThread
+    val virtualMachine = suspendContext.virtualMachineProxy.virtualMachine
+    if (virtualMachine !is VirtualMachineImpl || !managerThread.hasAsyncCommands() && virtualMachine.isIdle) return false
+
+    hold()
+    // reschedule with a small delay
+    AppExecutorUtil.getAppScheduledExecutorService().schedule({ managerThread.schedule(this) }, delay.toLong(), TimeUnit.MILLISECONDS)
+    return true
   }
 }
