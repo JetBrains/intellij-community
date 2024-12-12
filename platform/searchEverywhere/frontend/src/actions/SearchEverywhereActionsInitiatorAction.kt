2b876892516dd00687b63e04637e40bb6dda28d6
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2024-12-12 21:31:36 +0100

    IJPL-158622 Make SearchEverywhereActionsInitiatorAction running on frontend
    GitOrigin-RevId: 485d04cc6fc04f85a00e5a02801ed764ec5cad05

--- platform/searchEverywhere/frontend/src/actions/SearchEverywhereActionsInitiatorAction.kt
+++ platform/searchEverywhere/frontend/src/actions/SearchEverywhereActionsInitiatorAction.kt
@@ -1,13 +1,14 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.searchEverywhere.frontend.actions
 
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification
+import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.platform.searchEverywhere.frontend.SearchEverywhereFrontendService
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
-class SearchEverywhereActionsInitiatorAction : AnAction() {
+class SearchEverywhereActionsInitiatorAction : DumbAwareAction(), ActionRemoteBehaviorSpecification.Frontend {
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.project ?: return
     val service = SearchEverywhereFrontendService.getInstance(project)
