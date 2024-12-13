01df691f0481f3bc988c8b9bbf70321071d26a8b
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-12-13 15:15:33 +0100

    Add not show again action for Node/Claude app
    Node depends a lot on PATH variable and it's tricky to really check whether everything is fine on all OSes
    
    GitOrigin-RevId: de1ec4cf5acadc55eac75365fd6cce97fc18cc62

--- plugins/mcp-server/src/main/kotlin/org/jetbrains/mcpserverplugin/MCPServerStartupValidator.kt
+++ plugins/mcp-server/src/main/kotlin/org/jetbrains/mcpserverplugin/MCPServerStartupValidator.kt
@@ -4,9 +4,11 @@ import com.intellij.ide.BrowserUtil
 import com.intellij.notification.NotificationAction
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import org.jetbrains.mcpserverplugin.notification.ClaudeConfigManager
+import org.jetbrains.mcpserverplugin.settings.PluginSettings
 
 internal class MCPServerStartupValidator : ProjectActivity {
     private val GROUP_ID = "MCPServerPlugin"
@@ -30,7 +32,8 @@ internal class MCPServerStartupValidator : ProjectActivity {
 
     override suspend fun execute(project: Project) {
         val notificationGroup = NotificationGroupManager.getInstance().getNotificationGroup(GROUP_ID)
-        if (!ClaudeConfigManager.isClaudeClientInstalled()) {
+        val settingsService = service<PluginSettings>()
+        if (!ClaudeConfigManager.isClaudeClientInstalled() && settingsService.state.shouldShowClaudeNotification) {
             val notification = notificationGroup.createNotification(
                 "Claude Client is not installed",
                 NotificationType.INFORMATION
@@ -38,10 +41,14 @@ internal class MCPServerStartupValidator : ProjectActivity {
             notification.addAction(NotificationAction.createSimpleExpiring("Open Installation Instruction") {
                 BrowserUtil.open("https://claude.ai/download")
             })
+            notification.addAction(NotificationAction.createSimpleExpiring("Don't Show Again") {
+                settingsService.state.shouldShowClaudeNotification = false
+                notification.expire()
+            })
             notification.notify(project)
         }
 
-        if(!isNpxInstalled()){
+        if (!isNpxInstalled() && settingsService.state.shouldShowNodeNotification) {
             val notification = notificationGroup.createNotification(
                 "Node is not installed",
                 "MCP Server Proxy requires Node.js to be installed",
@@ -50,16 +57,24 @@ internal class MCPServerStartupValidator : ProjectActivity {
             notification.addAction(NotificationAction.createSimpleExpiring("Open Installation Instruction") {
                 BrowserUtil.open("https://nodejs.org/en/download/package-manager")
             })
+            notification.addAction(NotificationAction.createSimpleExpiring("Don't Show Again") {
+                settingsService.state.shouldShowNodeNotification = false
+                notification.expire()
+            })
             notification.notify(project)
         }
 
-        if (ClaudeConfigManager.isClaudeClientInstalled() && isNpxInstalled() && !ClaudeConfigManager.isProxyConfigured()) {
+        if (ClaudeConfigManager.isClaudeClientInstalled() && isNpxInstalled() && !ClaudeConfigManager.isProxyConfigured() && settingsService.state.shouldShowClaudeSettingsNotification) {
             val notification = notificationGroup.createNotification(
                 "MCP Server Proxy is not configured",
                 NotificationType.INFORMATION
             )
             notification.addAction(NotificationAction.createSimpleExpiring("Install MCP Server Proxy") {
                 ClaudeConfigManager.modifyClaudeSettings()
+            })
+            notification.addAction(NotificationAction.createSimpleExpiring("Don't Show Again") {
+                settingsService.state.shouldShowClaudeSettingsNotification = false
+                notification.expire()
             })
             notification.notify(project)
         }
