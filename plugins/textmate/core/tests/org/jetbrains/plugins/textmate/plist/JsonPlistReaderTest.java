9d868f48141ce013509f7c369274b766abd17df2
Alexander Zolotov <zolotov@jetbrains.com>
2024-12-13 13:05:04 +0100

    [textmate] split plist reader tests
    GitOrigin-RevId: 6012f4126022526e9f7d600a5fb727b6010b619d

--- plugins/textmate/core/tests/org/jetbrains/plugins/textmate/plist/JsonPlistReaderTest.java
+++ plugins/textmate/core/tests/org/jetbrains/plugins/textmate/plist/JsonPlistReaderTest.java
@@ -1,23 +1,21 @@
 package org.jetbrains.plugins.textmate.plist;
 
-import com.intellij.openapi.util.text.Strings;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 import static org.jetbrains.plugins.textmate.plist.PListValue.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
-public class JsonPlistReaderTest extends PlistReaderTestCase {
-  @Override
-  protected PlistReader createReader() {
-    return new JsonPlistReader();
-  }
+public class JsonPlistReaderTest {
 
-  @Override
   @Test
-  public void parseArray() throws Exception {
+  public void parseArray() {
     Plist plist = read("{list:[\"alex\",\"zolotov\",42]}");
     HashMap<String, PListValue> map = new HashMap<>() {{
       put("list", array(string("alex"), string("zolotov"), integer(Integer.valueOf(42))));
@@ -25,31 +23,68 @@ public class JsonPlistReaderTest extends PlistReaderTestCase {
     assertEquals(Plist.fromMap(map), plist);
   }
 
-
-  @Override
-  public void parseDate() {
-    // not supported in JSON
+  @Test
+  public void getStringMethod() {
+    Plist plist = read("{someKey: \"someValue\"}");
+    assertEquals("someValue", plist.getPlistValue("someKey").getString());
+    assertEquals("default", plist.getPlistValue("unknown", "default").getString());
   }
 
-  @Override
-  public void plistWithoutDictRoot() {
-    // not supported in JSON
+  @Test
+  public void parseString() {
+    Plist plist = read("{someKey: \"someValue\",anotherKey: \">\"}");
+    assertEquals(2, plist.entries().size());
+    assertEquals(string("someValue"), plist.getPlistValue("someKey"));
+    assertEquals(string(">"), plist.getPlistValue("anotherKey"));
+    assertEquals(string("default"), plist.getPlistValue("unknown", "default"));
+    assertNull(plist.getPlistValue("unknown"));
   }
 
-  @Override
-  public void invalidPlist() {
-    // not supported in JSON
+  @Test
+  public void parseBoolean() {
+    Plist plist = read("{true: true,false: false}");
+    assertEquals(2, plist.entries().size());
+    assertEquals(bool(true), plist.getPlistValue("true"));
+    assertEquals(bool(false), plist.getPlistValue("false"));
+    assertNull(plist.getPlistValue("unknown"));
+    assertEquals(bool(true), plist.getPlistValue("unknown", true));
+    assertEquals(bool(false), plist.getPlistValue("unknown", false));
   }
 
-  @NotNull
-  @Override
-  protected String prepareText(String string) {
-    return Strings.unescapeXmlEntities(string.replace("<dict><key>", "{").replace("</dict>", "}").
-                                         replace("<key>", ",").replace("</key>", ": ").
-                                         replace("<integer>", "").replace("</integer>", "").
-                                         replace("<real>", "").replace("</real>", "").
-                                         replace("<string>", "\"").replace("</string>", "\"").
-                                         replace("<array>", "[").replace("<array>", "]").
-                                         replace("<true/>", "true").replace("<false/>", "false"));
+  @Test
+  public void parseInteger() {
+    Plist plist = read("{int: 124}");
+    assertEquals(1, plist.entries().size());
+    assertEquals(integer(Integer.valueOf(124)), plist.getPlistValue("int"));
+    assertNull(plist.getPlistValue("unknown"));
+    assertEquals(integer(Integer.valueOf(124)), plist.getPlistValue("unknown", Integer.valueOf(124)));
+  }
+
+  @Test
+  public void parseReal() {
+    Plist plist = read("{real: 145.3}");
+    assertEquals(1, plist.entries().size());
+    assertEquals(real(Double.valueOf(145.3)), plist.getPlistValue("real"));
+    assertEquals(real(Double.valueOf(120.0)), plist.getPlistValue("unknown", 120.0));
+    assertNull(plist.getPlistValue("unknown"));
+  }
+
+
+  @Test
+  public void parseInnerDict() {
+    Plist plist = read("{dict: {name: \"alex\",lastname: \"zolotov\",age: 22}}");
+    HashMap<String, PListValue> inner = new HashMap<>() {{
+      put("name", string("alex"));
+      put("lastname", string("zolotov"));
+      put("age", integer(Integer.valueOf(22)));
+    }};
+    HashMap<String, PListValue> map = new HashMap<>() {{
+      put("dict", dict(Plist.fromMap(inner)));
+    }};
+    assertEquals(Plist.fromMap(map), plist);
+  }
+
+  protected Plist read(String string) {
+    return new JsonPlistReader().read(new ByteArrayInputStream(string.getBytes(StandardCharsets.UTF_8)));
   }
 }
