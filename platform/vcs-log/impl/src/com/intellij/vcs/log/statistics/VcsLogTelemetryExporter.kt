303ba7718f05e0496b84450fc62785cdae39fa2d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-12-14 09:41:50 +0100

    update opentelemetry lib
    GitOrigin-RevId: 454d6cb7a233b98a675bb355f1f3b35cc977136b

--- platform/vcs-log/impl/src/com/intellij/vcs/log/statistics/VcsLogTelemetryExporter.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/statistics/VcsLogTelemetryExporter.kt
@@ -33,63 +33,63 @@ import com.intellij.vcs.log.statistics.VcsLogPerformanceStatisticsCollector.VCS_
 import io.opentelemetry.api.trace.StatusCode
 import io.opentelemetry.sdk.metrics.export.MetricExporter
 import io.opentelemetry.sdk.trace.data.SpanData
-import io.opentelemetry.sdk.trace.internal.data.ExceptionEventData
+import io.opentelemetry.sdk.trace.data.ExceptionEventData
 
 private class VcsLogTelemetryExporter : OpenTelemetryExporterProvider {
   override fun getSpanExporters(): List<AsyncSpanExporter> {
     return listOf(LogHistorySpanExporter, LogFilterSpanExporter)
   }
 
-  private object LogHistorySpanExporter : AsyncSpanExporter {
-    override suspend fun export(spans: Collection<SpanData>) {
-      for (span in spans.vcsSpans()) {
-        if (span.hasErrors()) continue
-        val historySpan = LogHistory.entries.find { it.getName() == span.name } ?: continue
-
-        val vcsName = span.attributes[VCS_NAME].orEmpty()
-        when (historySpan) {
-          LogHistory.Computing -> {
-            val indexComputing = "index" == span.attributes[FILE_HISTORY_TYPE]
-            if (span.attributes[FILE_HISTORY_IS_INITIAL] == true) {
-              FILE_HISTORY_COMPUTING.log(vcsName, indexComputing, span.valueInMillis)
-            }
-          }
-          LogHistory.CollectingRenames -> FILE_HISTORY_COLLECTING_RENAMES.log(vcsName, span.valueInMillis)
-          else -> {}
-        }
-      }
-    }
-  }
-
-  private object LogFilterSpanExporter : AsyncSpanExporter {
-    override suspend fun export(spans: Collection<SpanData>) {
-      for (span in spans.vcsSpans()) {
-        if (span.hasErrors() || span.name != LogFilter.getName()) continue
-
-        val filtersList = span.attributes[VCS_LOG_FILTERS_LIST]?.toStringList()
-        if (filtersList.isNullOrEmpty()) continue
-
-        val vcsList = span.attributes[VCS_LIST]?.toStringList() ?: continue
-        val graphOptionsType = span.attributes[VCS_LOG_GRAPH_OPTIONS_TYPE] ?: continue
-        val sortType = span.attributes[VCS_LOG_SORT_TYPE]
-
-        val commitCount = span.attributes[VCS_LOG_FILTERED_COMMIT_COUNT] ?: continue
-        val repositoryCommitCount = span.attributes[VCS_LOG_REPOSITORY_COMMIT_COUNT]
-        val filterKind = span.attributes[VCS_LOG_FILTER_KIND] ?: continue
-
-        val events = mutableListOf(VCS_LIST_FIELD.with(vcsList), FILTERS_FIELD.with(filtersList), GRAPH_OPTIONS_TYPE_FIELD.with(graphOptionsType),
-                                   FILTERED_COMMIT_COUNT_FIELD.with(commitCount), REPOSITORY_COMMIT_COUNT_FIELD.with(repositoryCommitCount),
-                                   FILTER_KIND_FIELD.with(filterKind), EventFields.DurationMs.with(span.valueInMillis))
-        if (sortType != null) {
-          events.add(SORT_TYPE_FIELD.with(sortType))
-        }
-
-        VCS_LOG_FILTER.log(events)
-      }
-    }
-  }
-
   override fun getMetricsExporters() = emptyList<MetricExporter>()
+}
+
+private object LogHistorySpanExporter : AsyncSpanExporter {
+  override suspend fun export(spans: Collection<SpanData>) {
+    for (span in spans.vcsSpans()) {
+      if (span.hasErrors()) continue
+      val historySpan = LogHistory.entries.find { it.getName() == span.name } ?: continue
+
+      val vcsName = span.attributes[VCS_NAME].orEmpty()
+      when (historySpan) {
+        LogHistory.Computing -> {
+          val indexComputing = "index" == span.attributes[FILE_HISTORY_TYPE]
+          if (span.attributes[FILE_HISTORY_IS_INITIAL] == true) {
+            FILE_HISTORY_COMPUTING.log(vcsName, indexComputing, span.valueInMillis)
+          }
+        }
+        LogHistory.CollectingRenames -> FILE_HISTORY_COLLECTING_RENAMES.log(vcsName, span.valueInMillis)
+        else -> {}
+      }
+    }
+  }
+}
+
+private object LogFilterSpanExporter : AsyncSpanExporter {
+  override suspend fun export(spans: Collection<SpanData>) {
+    for (span in spans.vcsSpans()) {
+      if (span.hasErrors() || span.name != LogFilter.getName()) continue
+
+      val filtersList = span.attributes[VCS_LOG_FILTERS_LIST]?.toStringList()
+      if (filtersList.isNullOrEmpty()) continue
+
+      val vcsList = span.attributes[VCS_LIST]?.toStringList() ?: continue
+      val graphOptionsType = span.attributes[VCS_LOG_GRAPH_OPTIONS_TYPE] ?: continue
+      val sortType = span.attributes[VCS_LOG_SORT_TYPE]
+
+      val commitCount = span.attributes[VCS_LOG_FILTERED_COMMIT_COUNT] ?: continue
+      val repositoryCommitCount = span.attributes[VCS_LOG_REPOSITORY_COMMIT_COUNT]
+      val filterKind = span.attributes[VCS_LOG_FILTER_KIND] ?: continue
+
+      val events = mutableListOf(VCS_LIST_FIELD.with(vcsList), FILTERS_FIELD.with(filtersList), GRAPH_OPTIONS_TYPE_FIELD.with(graphOptionsType),
+                                 FILTERED_COMMIT_COUNT_FIELD.with(commitCount), REPOSITORY_COMMIT_COUNT_FIELD.with(repositoryCommitCount),
+                                 FILTER_KIND_FIELD.with(filterKind), EventFields.DurationMs.with(span.valueInMillis))
+      if (sortType != null) {
+        events.add(SORT_TYPE_FIELD.with(sortType))
+      }
+
+      VCS_LOG_FILTER.log(events)
+    }
+  }
 }
 
 private fun Collection<SpanData>.vcsSpans() = asSequence().filter { span -> span.instrumentationScopeInfo.name == VcsScope.name }
