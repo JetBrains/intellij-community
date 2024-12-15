b66b9c0e8e10ababcf2136e2f264ec1ed7d06249
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2024-12-15 22:49:54 +0100

    [freezeAnalyzer] IJI-1676 Support running tests on compilation output jars: fix FreezeAnalyzerTest
    GitOrigin-RevId: a0ca60c52ddfedf8c2a38689cc9519cb2080c57c

--- platform/diagnostic/freezeAnalyzer/testSrc/com/intellij/platform/diagnostic/freezeAnalyzer/FreezeAnalyzerTest.kt
+++ platform/diagnostic/freezeAnalyzer/testSrc/com/intellij/platform/diagnostic/freezeAnalyzer/FreezeAnalyzerTest.kt
@@ -1,3 +1,4 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.diagnostic.freezeAnalyzer
 
 import io.kotest.assertions.assertSoftly
@@ -6,107 +7,115 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.string.shouldStartWith
 import org.junit.jupiter.api.Test
-import java.io.File
-import kotlin.io.path.readText
+import java.io.FileNotFoundException
 
 class FreezeAnalyzerTest {
+  private fun getResourceContent(path: String): String {
+    val classLoader = this::class.java.classLoader
+    val stream = classLoader.getResourceAsStream(path)
+    if (stream == null) {
+      throw FileNotFoundException("Not found '$path' in classloader '$classLoader' classpath")
+    }
+    return stream.reader().use { it.readText() }
+  }
+
   @Test
   fun testAWTFreeze1() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/awtFreeze/IDEA-344485.txt")!!.path).toPath().readText()
+    val threadDump = getResourceContent("freezes/awtFreeze/IDEA-344485.txt")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("EDT is busy with com.intellij.vcs.log.data.VcsLogUserResolverBase.resolveCurrentUser")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.threads?.first()?.stackTrace?.lineSequence()?.first().shouldBe("\"AWT-EventQueue-0\" prio=0 tid=0x0 nid=0x0 runnable")
   }
 
   @Test
   fun testAWTFreeze2() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/awtFreeze/IDEA-342509.txt")!!.path).toPath().readText()
+    val threadDump = getResourceContent("freezes/awtFreeze/IDEA-342509.txt")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("EDT is busy with com.intellij.ui.tree.ui.DefaultTreeUI.getRenderer")
   }
 
   @Test
   fun testAWTFreeze3() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/awtFreeze/KTIJ-28701.txt")!!.path).toPath().readText()
+    val threadDump = getResourceContent("freezes/awtFreeze/KTIJ-28701.txt")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("EDT is busy with org.jetbrains.kotlin.idea.base.projectStructure.KotlinProjectStructureUtils\$hasKotlinJvmRuntime\$1.invoke")
   }
 
   @Test
   fun testAWTFreeze4() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/awtFreeze/IDEA-344941.txt")!!.path).toPath().readText()
+    val threadDump = getResourceContent("freezes/awtFreeze/IDEA-344941.txt")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("EDT is busy with com.intellij.notification.impl.NotificationsManagerImpl.calculateContentHeight")
   }
 
   @Test
   fun testAWTFreeze5() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/awtFreeze/IDEA-344178.txt")!!.path).toPath().readText()
+    val threadDump = getResourceContent("freezes/awtFreeze/IDEA-344178.txt")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("EDT is busy with com.intellij.configurationStore.ComponentSerializationUtil.getStateClass")
   }
 
   @Test
   fun testFreezeInExcluded() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/awtFreeze/FreezeInExcluded.txt")!!.path).toPath().readText()
+    val threadDump = getResourceContent("freezes/awtFreeze/FreezeInExcluded.txt")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("EDT is busy with com.intellij.util.indexing.dependencies.ProjectIndexingDependenciesStorage.writeIncompleteScanningMark")
   }
 
   @Test
   fun testLockFreeze1() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/readWriteLock/ML-2562.txt")!!.path).toPath().readText()
+    val threadDump = getResourceContent("freezes/readWriteLock/ML-2562.txt")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("Long read action in org.jetbrains.completion.full.line.local.generation.SimilarContextRetriever.getSimilarity")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.threads?.joinToString { it -> it.stackTrace }.shouldContain("\"DefaultDispatcher-worker-27\" prio=0 tid=0x0 nid=0x0 runnable")
   }
 
   @Test
   fun testLockFreeze2() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/readWriteLock/KTIJ-23464.txt")!!.path).toPath().readText().replace("2.", ".")
+    val threadDump = getResourceContent("freezes/readWriteLock/KTIJ-23464.txt").replace("2.", ".")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("Long read action in org.jetbrains.kotlin.org.eclipse.aether.internal.impl.synccontext.named.DiscriminatingNameMapper.getHostname")
   }
 
   @Test
   fun testLockFreeze3() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/readWriteLock/IDEA-344522.txt")!!.path).toPath().readText().replace("2.", ".")
+    val threadDump = getResourceContent("freezes/readWriteLock/IDEA-344522.txt").replace("2.", ".")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("Long read action in com.jetbrains.jsonSchema.impl.PatternProperties.<init>")
   }
 
   @Test
   fun testLockFreeze4() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/readWriteLock/WEB-65320.txt")!!.path).toPath().readText().replace("2.", ".")
+    val threadDump = getResourceContent("freezes/readWriteLock/WEB-65320.txt").replace("2.", ".")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("Long read action in com.intellij.lang.javascript.buildTools.npm.PackageJsonUtil.findChildPackageJsonFile")
   }
 
   @Test
   fun testLockFreeze5() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/readWriteLock/KT-65652.txt")!!.path).toPath().readText().replace("2.", ".")
+    val threadDump = getResourceContent("freezes/readWriteLock/KT-65652.txt").replace("2.", ".")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("Long read action in org.jetbrains.kotlin.idea.fir.extensions.KotlinK2BundledCompilerPluginsKt.readFileContentFromJar")
   }
 
   @Test
   fun testGeneralLockFreeze() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/generalLock/generalLock.txt")!!.path).toPath().readText()
+    val threadDump = getResourceContent("freezes/generalLock/generalLock.txt")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("EDT is blocked on com.intellij.codeInsight.completion.CompletionProgressIndicator.blockingWaitForFinish")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.additionalMessage?.shouldBe("Possibly locked by com.intellij.codeInsight.completion.JavaMethodCallElement.<init> in DefaultDispatcher-worker-55")
   }
 
   @Test
   fun testGeneralLock2Freeze() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/generalLock/generalLock2.txt")!!.path).toPath().readText()
+    val threadDump = getResourceContent("freezes/generalLock/generalLock2.txt")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("EDT is blocked on org.jetbrains.kotlin.idea.base.projectStructure.KotlinProjectStructureUtils\$hasKotlinJvmRuntime\$1.invoke")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.additionalMessage.shouldBe("Possibly locked by com.intellij.lang.javascript.psi.stubs.JSUsedRemoteModulesIndex.getUsedModules in ApplicationImpl pooled thread 10")
   }
 
   @Test
   fun testGeneralLockFreezeWithLockId() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/generalLock/generalLockWithLockId.txt")!!.path).toPath().readText()
+    val threadDump = getResourceContent("freezes/generalLock/generalLockWithLockId.txt")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("Possible deadlock. Read lock is taken by com.intellij.execution.impl.RunManagerImpl.getSelectedConfiguration, but the thread is blocked by com.android.tools.idea.gradle.project.GradleProjectInfo.isBuildWithGradle which is waiting on RWLock")
   }
 
   @Test
   fun testReadWriteLockInCoreClasses() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/readWriteLock/NullLock.txt")!!.path).toPath().readText()
+    val threadDump = getResourceContent("freezes/readWriteLock/NullLock.txt")
     FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("Long read action in com.intellij.util.indexing.dependencies.AppIndexingDependenciesStorage.writeRequestId")
   }
 
   @Test
   fun testReadWriteNewLock() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/readWriteLock/NewLock.txt")!!.path).toPath().readText()
+    val threadDump = getResourceContent("freezes/readWriteLock/NewLock.txt")
     withClue("") {
       assertSoftly {
         FreezeAnalyzer.analyzeFreeze(threadDump)?.message.shouldBe("Long read action in com.intellij.openapi.roots.impl.PackageDirectoryCacheImpl\$PackageInfo.lambda\$new\$0")
@@ -117,19 +126,19 @@ class FreezeAnalyzerTest {
 
   @Test
   fun testNoFreeze() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/noFreeze/lowMemory.txt")!!.path).toPath().readText().replace("2.", ".")
+    val threadDump = getResourceContent("freezes/noFreeze/lowMemory.txt").replace("2.", ".")
     FreezeAnalyzer.analyzeFreeze(threadDump, testName = "kotlin/testCase")?.message.shouldBe("kotlin/testCase: EDT is not blocked/busy (freeze can be the result of extensive GC)")
   }
 
   @Test
   fun testNoFreeze2() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/noFreeze/lowMemory2.txt")!!.path).toPath().readText().replace("2.", ".")
+    val threadDump = getResourceContent("freezes/noFreeze/lowMemory2.txt").replace("2.", ".")
     FreezeAnalyzer.analyzeFreeze(threadDump, testName = "kotlin/testCase").shouldBe(null)
   }
 
   @Test
   fun testNoFreezeOutsideOfTest() {
-    val threadDump = File(this::class.java.classLoader.getResource("freezes/noFreeze/lowMemory.txt")!!.path).toPath().readText().replace("2.", ".")
+    val threadDump = getResourceContent("freezes/noFreeze/lowMemory.txt").replace("2.", ".")
     FreezeAnalyzer.analyzeFreeze(threadDump, testName = null)?.shouldBe(null)
   }
 }
