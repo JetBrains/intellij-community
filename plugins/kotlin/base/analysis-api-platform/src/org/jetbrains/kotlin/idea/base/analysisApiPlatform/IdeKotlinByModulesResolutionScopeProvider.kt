5d90e751ed53e68a53c810544f3baf07bc35e594
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2024-12-16 14:48:53 +0100

    [kotlin] remove dependency on `ModuleInfo` from `IdeKotlinByModulesResolutionScopeProvider`
    KTIJ-31422
    
    GitOrigin-RevId: 99a8884e4ded94a8b2289dee28a41247afac7763

--- plugins/kotlin/base/analysis-api-platform/src/org/jetbrains/kotlin/idea/base/analysisApiPlatform/IdeKotlinByModulesResolutionScopeProvider.kt
+++ plugins/kotlin/base/analysis-api-platform/src/org/jetbrains/kotlin/idea/base/analysisApiPlatform/IdeKotlinByModulesResolutionScopeProvider.kt
@@ -4,102 +4,42 @@ package org.jetbrains.kotlin.idea.base.analysisApiPlatform
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.kotlin.analysis.api.platform.projectStructure.KotlinGlobalSearchScopeMerger
-import org.jetbrains.kotlin.analysis.api.platform.projectStructure.KotlinResolutionScopeEnlarger
 import org.jetbrains.kotlin.analysis.api.platform.projectStructure.KotlinResolutionScopeProvider
-import org.jetbrains.kotlin.analysis.api.projectStructure.*
+import org.jetbrains.kotlin.analysis.api.projectStructure.KaBuiltinsModule
+import org.jetbrains.kotlin.analysis.api.projectStructure.KaLibrarySourceModule
+import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
+import org.jetbrains.kotlin.analysis.api.projectStructure.allDirectDependencies
 import org.jetbrains.kotlin.analysis.decompiler.psi.BuiltinsVirtualFileProvider
-import org.jetbrains.kotlin.idea.base.analysis.builtins.hasCommonKotlinStdlib
-import org.jetbrains.kotlin.idea.base.projectStructure.*
-import org.jetbrains.kotlin.idea.base.projectStructure.moduleInfo.ModuleSourceInfo
-import org.jetbrains.kotlin.idea.base.projectStructure.moduleInfo.ModuleTestSourceInfo
-import org.jetbrains.kotlin.idea.base.projectStructure.modules.KaSourceModuleForOutsider
-import org.jetbrains.kotlin.idea.base.util.K1ModeProjectStructureApi
-import org.jetbrains.kotlin.idea.base.util.fileScope
-import org.jetbrains.kotlin.idea.base.util.minus
 
-internal class IdeKotlinByModulesResolutionScopeProvider(private val project: Project) : KotlinResolutionScopeProvider {
+internal class IdeKotlinByModulesResolutionScopeProvider(
+    private val project: Project
+) : KotlinResolutionScopeProvider {
+
     override fun getResolutionScope(module: KaModule): GlobalSearchScope {
-        val scope = when (module) {
-            is KaSourceModule ->
-                @OptIn(K1ModeProjectStructureApi::class)
-                run {
-                    val moduleInfo = module.moduleInfo as ModuleSourceInfo
-                    val includeTests = moduleInfo is ModuleTestSourceInfo
-                    val scope = excludeIgnoredModulesByKotlinProjectModel(moduleInfo, module, includeTests)
-                    adjustScope(scope, module)
-                }
+        val moduleWithDependentScopes = getResolutionScopes(module)
+        return KotlinGlobalSearchScopeMerger.getInstance(project).union(moduleWithDependentScopes)
+    }
 
-            is KaDanglingFileModule -> {
-                val scopes = listOf(
-                    module.file.fileScope(),
-                    KotlinResolutionScopeEnlarger.getEnlargedResolutionScope(module.contextModule)
-                )
-
-                GlobalSearchScope.union(scopes)
-            }
-
-            else -> {
-                val allModules = buildList {
-                    add(module)
-                    if (module is KaLibrarySourceModule) {
-                        add(module.binaryLibrary)
-                    }
-
-                    addAll(module.allDirectDependencies())
-                }
-
-                KotlinGlobalSearchScopeMerger.getInstance(project).union(allModules.map { it.contentScope })
+    private fun getResolutionScopes(module: KaModule): List<GlobalSearchScope> {
+        val modules = buildSet {
+            add(module)
+            addAll(module.allDirectDependencies())
+            if (module is KaLibrarySourceModule) {
+                add(module.binaryLibrary)
             }
         }
 
-        // A dangling module already has builtins in the context module
-        val adjustedScope = if (module !is KaDanglingFileModule) {
-            // Workaround for KT-72988
-            scope.withBuiltInsScope(project)
-        } else {
-            scope
+        return buildList {
+            modules.mapTo(this) { it.contentScope }
+            if (modules.none { it is KaBuiltinsModule }) {
+                // Workaround for KT-72988
+                // after fixed, it should probably only be added if `module.targetPlatform.hasCommonKotlinStdlib()`
+                add(createBuiltinsScope())
+            }
         }
-
-        return adjustedScope
     }
 
-    private fun adjustScope(baseScope: GlobalSearchScope, module: KaSourceModule): GlobalSearchScope {
-        var scope = baseScope
-
-        if (module is KaSourceModuleForOutsider) {
-            scope = module.adjustContentScope(scope)
-        }
-
-        if (module.targetPlatform.hasCommonKotlinStdlib()) {
-            // we do not have builtins in common stdlib
-            scope = scope.withBuiltInsScope(module.project)
-        }
-
-        return scope
-    }
-
-    private fun GlobalSearchScope.withBuiltInsScope(project: Project): GlobalSearchScope {
-        val builtinsScope = BuiltinsVirtualFileProvider.getInstance().createBuiltinsScope(project)
-        return KotlinGlobalSearchScopeMerger.getInstance(project).union(listOf(this, builtinsScope))
-    }
-
-    /**
-     * Some dependencies from order entities might be filtered by [org.jetbrains.kotlin.idea.base.projectStructure.moduleInfo.SourceModuleDependenciesFilter]
-     * Those entries would still be present in the [GlobalSearchScope.moduleWithDependenciesAndLibrariesScope].
-     * Analysis API should know nothing about such dependencies as it works only by KaModule (which itself works by ModuleInfo). So, we exclude such dependencies
-     */
-    @K1ModeProjectStructureApi
-    private fun excludeIgnoredModulesByKotlinProjectModel(
-        moduleInfo: ModuleSourceInfo,
-        module: KaSourceModule,
-        includeTests: Boolean
-    ): GlobalSearchScope {
-        val ignored = moduleInfo
-            .collectDependencies(ModuleDependencyCollector.CollectionMode.COLLECT_IGNORED)
-            .map { it.contentScope }
-
-        val searchScope = GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module.openapiModule, includeTests)
-        if (ignored.isEmpty()) return searchScope
-        return searchScope - KotlinGlobalSearchScopeMerger.getInstance(project).union(ignored)
+    private fun createBuiltinsScope(): GlobalSearchScope {
+        return BuiltinsVirtualFileProvider.getInstance().createBuiltinsScope(project)
     }
 }
