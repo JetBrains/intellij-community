54a95d9dbadfc5e3c25cb6901802a84b59d1082b
Oleg Taratukhin <oleg.taratukhin@jetbrains.com>
2024-12-17 20:37:33 +0100

    [ai-completion] LLM-13498 extend extra step provider to preliminary steps
    GitOrigin-RevId: 2687372b49778d810d72c77657c5f095a5e64196

--- plugins/evaluation-plugin/src/com/intellij/cce/evaluation/step/ExtraEvaluationStepProvider.kt
+++ plugins/evaluation-plugin/src/com/intellij/cce/evaluation/step/ExtraEvaluationStepProvider.kt
@@ -6,10 +6,15 @@ import com.intellij.cce.evaluation.EvaluationStep
 import com.intellij.openapi.extensions.ExtensionPointName
 
 interface ExtraEvaluationStepProvider {
-  fun extraStepsFor(language: Language, strategy: EvaluationStrategy): List<EvaluationStep>
+  fun extraStepsFor(language: Language, strategy: EvaluationStrategy): List<EvaluationStep> = emptyList()
+  fun extraPreliminarySteps(): List<EvaluationStep> = emptyList()
 
   companion object {
     public val EP_NAME: ExtensionPointName<ExtraEvaluationStepProvider> =
       ExtensionPointName.Companion.create<ExtraEvaluationStepProvider>("com.intellij.cce.extraEvaluationStepProvider")
   }
+}
+
+fun MutableList<EvaluationStep>.addExtraPreliminarySteps(): Unit = ExtraEvaluationStepProvider.EP_NAME.forEachExtensionSafe {
+  addAll(it.extraPreliminarySteps())
 }
