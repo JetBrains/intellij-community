6aedab6f4ccfb601b8fb5d0cfbdd078805aa6d21
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-12-18 07:32:38 +0100

    [performance] IJPL-173892 FUS: add project count, oom error flag and last action to low.memory events
    GitOrigin-RevId: 048a9136700b3d9741cd21cb74260e9a8e62a16b

--- platform/platform-impl/src/com/intellij/diagnostic/DefaultIdeaErrorLogger.java
+++ platform/platform-impl/src/com/intellij/diagnostic/DefaultIdeaErrorLogger.java
@@ -70,7 +70,7 @@ public final class DefaultIdeaErrorLogger {
       var kind = getOOMErrorKind(event.getThrowable());
       if (kind != null) {
         ourOomOccurred = true;
-        LowMemoryNotifier.showNotification(kind, true, true);
+        LowMemoryNotifier.showNotification(kind, true);
       }
       else if (!ourOomOccurred) {
         MessagePool.getInstance().addIdeFatalMessage(event);
