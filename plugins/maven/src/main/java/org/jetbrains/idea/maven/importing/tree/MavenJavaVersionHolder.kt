bb3fa95e7129f88aacd4991c8a09e0f578052b8d
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2024-12-18 15:26:55 +0100

    [maven] [IDEA-202136] convert JavaVersionHolder to kotlin
    GitOrigin-RevId: 95080a7096876f59a8ecdd7e951387503b12a3ec

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/tree/MavenJavaVersionHolder.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/tree/MavenJavaVersionHolder.kt
@@ -1,30 +1,17 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.idea.maven.importing.tree;
+package org.jetbrains.idea.maven.importing.tree
 
-import com.intellij.pom.java.LanguageLevel;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.pom.java.LanguageLevel
 
-public class MavenJavaVersionHolder {
-  @Nullable public final LanguageLevel sourceLevel;
-  @Nullable public final LanguageLevel targetLevel;
-  @Nullable public final LanguageLevel testSourceLevel;
-  @Nullable public final LanguageLevel testTargetLevel;
-  public final boolean hasExecutionsForTests;
-
-  public MavenJavaVersionHolder(@Nullable LanguageLevel sourceLevel,
-                                @Nullable LanguageLevel targetLevel,
-                                @Nullable LanguageLevel testSourceLevel,
-                                @Nullable LanguageLevel testTargetLevel,
-                                boolean hasExecutionsForTests) {
-    this.sourceLevel = sourceLevel;
-    this.targetLevel = targetLevel;
-    this.testSourceLevel = testSourceLevel;
-    this.testTargetLevel = testTargetLevel;
-    this.hasExecutionsForTests = hasExecutionsForTests;
-  }
-
-  public boolean needSeparateTestModule() {
-    return hasExecutionsForTests || (testSourceLevel != null && !testSourceLevel.equals(sourceLevel))
-           || (testTargetLevel != null && !testTargetLevel.equals(targetLevel));
+class MavenJavaVersionHolder(
+  @JvmField val sourceLevel: LanguageLevel?,
+  @JvmField val targetLevel: LanguageLevel?,
+  @JvmField val testSourceLevel: LanguageLevel?,
+  @JvmField val testTargetLevel: LanguageLevel?,
+  @JvmField val hasExecutionsForTests: Boolean
+) {
+  fun needSeparateTestModule(): Boolean {
+    return hasExecutionsForTests || (testSourceLevel != null && testSourceLevel != sourceLevel)
+           || (testTargetLevel != null && testTargetLevel != targetLevel)
   }
 }
