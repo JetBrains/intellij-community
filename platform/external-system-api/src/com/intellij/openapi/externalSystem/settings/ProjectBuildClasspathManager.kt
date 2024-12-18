69eeec09b599dc321952e9e994050adc6ce6d92a
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2024-12-18 18:41:17 +0100

    [es] Allow modifying ProjectBuildClasspathManager state from EDT IDEA-361978
    Android tests set up calls parts of our Gradle Sync on EDT. In this case, `ProjectDataManager.importData()` and all the data services, including those that import project build classpath
    
    GitOrigin-RevId: e62d799972c7c0eb654a41f4b852d707609d5253

--- platform/external-system-api/src/com/intellij/openapi/externalSystem/settings/ProjectBuildClasspathManager.kt
+++ platform/external-system-api/src/com/intellij/openapi/externalSystem/settings/ProjectBuildClasspathManager.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.externalSystem.settings
 
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.externalSystem.model.project.ExternalProjectBuildClasspathPojo
 import com.intellij.openapi.externalSystem.settings.workspaceModel.ExternalProjectsBuildClasspathEntity
@@ -12,6 +13,8 @@ import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.Project
 import com.intellij.platform.backend.workspace.WorkspaceModel
 import com.intellij.platform.backend.workspace.workspaceModel
+import com.intellij.platform.workspace.storage.MutableEntityStorage
+import com.intellij.util.ui.EDT.*
 import kotlinx.coroutines.CoroutineScope
 import org.jetbrains.annotations.ApiStatus
 import java.util.*
@@ -26,25 +29,45 @@ class ProjectBuildClasspathManager(val project: Project, val coroutineScope: Cor
     .map { it.path }
 
   fun setProjectBuildClasspathSync(value: Map<String, ExternalProjectBuildClasspathPojo>) {
-    runBlockingCancellable {
-      setProjectBuildClasspath(value)
+    // Android tests' setup undate project build classpath on EDTs
+    if (isCurrentThreadEdt()) {
+      runWriteAction { setProjectBuildClasspathInWriteAction(value) }
+    } else {
+      runBlockingCancellable {
+        setProjectBuildClasspath(value)
+      }
     }
   }
 
   suspend fun setProjectBuildClasspath(value: Map<String, ExternalProjectBuildClasspathPojo>) {
-      project.workspaceModel
-      .update("AbstractExternalSystemLocalSettings update") { storage ->
-        val newClasspathEntity = getExternalProjectsBuildClasspathEntity(value)
-        val buildClasspathEntity = storage.entities(ExternalProjectsBuildClasspathEntity::class.java).firstOrNull()
-
-        if (buildClasspathEntity != null) {
-          storage.modifyExternalProjectsBuildClasspathEntity(buildClasspathEntity) {
-            projectsBuildClasspath = newClasspathEntity.projectsBuildClasspath
-          }
-        } else {
-          storage.addEntity(newClasspathEntity)
-        }
+    project.workspaceModel
+      .update("AbstractExternalSystemLocalSettings async update") { storage ->
+        updateOrAddValueToStorage(value, storage)
       }
+  }
+
+  fun setProjectBuildClasspathInWriteAction(value: Map<String, ExternalProjectBuildClasspathPojo>) {
+    project.workspaceModel
+      .updateProjectModel("AbstractExternalSystemLocalSettings explicit update") { storage ->
+        updateOrAddValueToStorage(value, storage)
+      }
+  }
+
+  private fun updateOrAddValueToStorage(
+    value: Map<String, ExternalProjectBuildClasspathPojo>,
+    storage: MutableEntityStorage,
+  ) {
+    val newClasspathEntity = getExternalProjectsBuildClasspathEntity(value)
+    val buildClasspathEntity = storage.entities(ExternalProjectsBuildClasspathEntity::class.java).firstOrNull()
+
+    if (buildClasspathEntity != null) {
+      storage.modifyExternalProjectsBuildClasspathEntity(buildClasspathEntity) {
+        projectsBuildClasspath = newClasspathEntity.projectsBuildClasspath
+      }
+    }
+    else {
+      storage.addEntity(newClasspathEntity)
+    }
   }
 
   fun getProjectBuildClasspath(): Map<String, ExternalProjectBuildClasspathPojo> {
