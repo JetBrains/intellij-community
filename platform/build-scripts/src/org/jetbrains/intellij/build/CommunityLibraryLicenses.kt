1431338c2724fe33e418d69d0397e8b0766dab4c
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2024-12-19 14:02:29 +0100

    rename kotlinc.poko-compiler-plugin to poko-compiler-plugin
    Only artifacts directly built from Kotlin repo may have
    the `kotlinc` prefix. `poko-compiler-plugin` is a third-party
    plugin and is not related to Kotlin.
    Most likely it shouldn't be bundled to the Kotlin plugin as well
    
    GitOrigin-RevId: 23f185e92a915afad843274130fa3724451a0ac9

--- platform/build-scripts/src/org/jetbrains/intellij/build/CommunityLibraryLicenses.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/CommunityLibraryLicenses.kt
@@ -780,7 +780,7 @@ object CommunityLibraryLicenses {
       .apache("https://github.com/Kotlin/kotlinx.serialization/blob/master/LICENSE.txt")
       .suppliedByOrganizations(Suppliers.JETBRAINS),
 
-    LibraryLicense("Kotlin POKO Compiler", libraryName = "kotlinc.poko-compiler-plugin", url = "https://github.com/drewhamilton/Poko")
+    LibraryLicense("Kotlin POKO Compiler", libraryName = "poko-compiler-plugin", url = "https://github.com/drewhamilton/Poko")
       .apache("https://github.com/drewhamilton/Poko/blob/main/LICENSE"),
 
     LibraryLicense("Kotlin reflection library", libraryName = "kotlin-reflect", url = "https://github.com/JetBrains/kotlin")
