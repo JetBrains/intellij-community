81179b99a0f7f7d6176c098d7272f7acb8a51d28
Ivan Morgillo <imorgillo@gmail.com>
2024-12-23 13:23:53 +0100

    move the thenIf modifier from ui to foundation module
    reference: https://github.com/JetBrains/jewel/pull/736
    
    closes https://github.com/JetBrains/intellij-community/pull/2895
    
    GitOrigin-RevId: 5eb5ef99d59723360c63eb9e41920e732d75b2b2

--- platform/jewel/foundation/src/main/kotlin/org/jetbrains/jewel/foundation/modifier/ModifierExtensions.kt
+++ platform/jewel/foundation/src/main/kotlin/org/jetbrains/jewel/foundation/modifier/ModifierExtensions.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.jewel.ui.util
+package org.jetbrains.jewel.foundation.modifier
 
 import androidx.compose.ui.Modifier
 
@@ -6,9 +6,5 @@ import androidx.compose.ui.Modifier
  * Conditionally applies the [action] to the receiver [Modifier], if [precondition] is true. Returns the receiver as-is
  * otherwise.
  */
-@Deprecated(
-    message = "This modifier has been moved to org.jetbrains.jewel.foundation.util. Please update your imports.",
-    ReplaceWith("thenIf(precondition, action)", "org.jetbrains.jewel.foundation.util"),
-)
 public inline fun Modifier.thenIf(precondition: Boolean, action: Modifier.() -> Modifier): Modifier =
     if (precondition) action() else this
