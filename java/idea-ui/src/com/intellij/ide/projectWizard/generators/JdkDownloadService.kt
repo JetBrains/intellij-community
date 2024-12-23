122e86a4ec14e258d3d069de528042c3e3218794
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2024-12-23 18:52:59 +0100

    [JDK|Download] cleanup: extract and move the JDL download utility class from the java to the lang module
    GitOrigin-RevId: 006c8e67013274c8d3fe83bde9cd6cde1d1eceff

--- java/idea-ui/src/com/intellij/ide/projectWizard/generators/JdkDownloadService.kt
+++ java/idea-ui/src/com/intellij/ide/projectWizard/generators/JdkDownloadService.kt
@@ -2,38 +2,26 @@
 package com.intellij.ide.projectWizard.generators
 
 import com.intellij.ide.JavaUiBundle
-import com.intellij.openapi.application.EDT
 import com.intellij.openapi.application.writeAction
 import com.intellij.openapi.components.Service
-import com.intellij.openapi.diagnostic.logger
-import com.intellij.openapi.diagnostic.runAndLogException
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.JavaSdk
-import com.intellij.openapi.projectRoots.ProjectJdkTable
 import com.intellij.openapi.projectRoots.Sdk
-import com.intellij.openapi.projectRoots.SdkType
 import com.intellij.openapi.projectRoots.impl.jdkDownloader.JdkDownloadTask
-import com.intellij.openapi.projectRoots.impl.jdkDownloader.JdkInstaller
-import com.intellij.openapi.projectRoots.impl.jdkDownloader.JdkItem
+import com.intellij.openapi.projectRoots.impl.jdkDownloader.JdkDownloadUtil
 import com.intellij.openapi.roots.ModuleRootModificationUtil
 import com.intellij.openapi.roots.ProjectRootManager
-import com.intellij.openapi.roots.ui.configuration.projectRoot.ProjectSdksModel
 import com.intellij.openapi.roots.ui.configuration.projectRoot.SdkDownloadTask
-import com.intellij.openapi.roots.ui.configuration.projectRoot.SdkDownloadTracker
-import com.intellij.openapi.ui.Messages
-import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.util.use
 import com.intellij.platform.ide.progress.withBackgroundProgress
 import com.intellij.util.application
 import com.intellij.util.concurrency.annotations.RequiresEdt
-import com.intellij.util.concurrency.annotations.RequiresWriteLock
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.future.asCompletableFuture
+import kotlinx.coroutines.launch
 import org.jetbrains.annotations.ApiStatus
-import java.nio.file.Path
 import java.util.concurrent.CompletableFuture
-import kotlin.coroutines.resume
 
 @Service(Service.Level.PROJECT)
 @ApiStatus.Internal
@@ -42,59 +30,14 @@ class JdkDownloadService(private val project: Project, private val coroutineScop
   @RequiresEdt
   fun setupInstallableSdk(downloadTask: SdkDownloadTask): Sdk {
     return application.runWriteAction<Sdk> {
-      createDownloadSdkInternal(JavaSdk.getInstance(), downloadTask)
+      JdkDownloadUtil.createDownloadSdkInternal(JavaSdk.getInstance(), downloadTask)
     }
   }
 
   fun downloadSdk(sdk: Sdk) {
     coroutineScope.launch {
       withBackgroundProgress(project, JavaUiBundle.message("progress.title.downloading", sdk.name)) {
-        downloadSdkInternal(sdk)
-      }
-    }
-  }
-
-  private suspend fun createDownloadTask(jdkItem: JdkItem, jdkHome: Path): SdkDownloadTask? {
-    val (selectedFile, error) = JdkInstaller.getInstance().validateInstallDir(jdkHome.toString())
-    if (selectedFile == null) {
-      withContext(Dispatchers.EDT) {
-        Messages.showErrorDialog(project,
-                                 JavaUiBundle.message("jdk.download.error.message", error),
-                                 JavaUiBundle.message("jdk.download.error.title")
-        )
-      }
-      return null
-    }
-
-    val downloadRequest = LOG.runAndLogException {
-      JdkInstaller.getInstance().prepareJdkInstallation(jdkItem, selectedFile)
-    } ?: return null
-
-    return JdkDownloadTask(jdkItem, downloadRequest, project)
-  }
-
-  @RequiresWriteLock
-  private fun createDownloadSdkInternal(sdkType: SdkType, sdkDownloadTask: SdkDownloadTask): Sdk {
-    val sdkTable = ProjectJdkTable.getInstance()
-    val sdks = sdkTable.allJdks.asList()
-    val sdk = ProjectSdksModel.createDownloadSdkInternal(sdkType, sdkDownloadTask, sdks)
-    sdkTable.addJdk(sdk)
-    return sdk
-  }
-
-  private suspend fun downloadSdkInternal(sdk: Sdk): Boolean {
-    return withContext(Dispatchers.EDT) {
-      Disposer.newDisposable("The JdkDownloader#downloadSdk lifecycle").use { disposable ->
-        val tracker = SdkDownloadTracker.getInstance()
-        tracker.startSdkDownloadIfNeeded(sdk)
-        suspendCancellableCoroutine { continuation ->
-          val registered = tracker.tryRegisterDownloadingListener(sdk, disposable, null) {
-            continuation.resume(it)
-          }
-          if (!registered) {
-            continuation.resume(false)
-          }
-        }
+        JdkDownloadUtil.downloadSdk(sdk)
       }
     }
   }
@@ -103,16 +46,14 @@ class JdkDownloadService(private val project: Project, private val coroutineScop
     return coroutineScope.async {
       withBackgroundProgress(project, JavaUiBundle.message("progress.title.downloading", sdkDownloadTask.suggestedSdkName)) {
 
-        val downloadTask = createDownloadTask(sdkDownloadTask.jdkItem, sdkDownloadTask.request.installDir)
+        val downloadTask = JdkDownloadUtil.createDownloadTask(project, sdkDownloadTask.jdkItem, sdkDownloadTask.request.installDir)
                            ?: return@withBackgroundProgress false
 
-        val sdk = writeAction {
-          createDownloadSdkInternal(JavaSdk.getInstance(), downloadTask)
-        }
+        val sdk = JdkDownloadUtil.createDownloadSdk(JavaSdk.getInstance(), downloadTask)
 
         onJdkCreated(sdk)
 
-        downloadSdkInternal(sdk)
+        JdkDownloadUtil.downloadSdk(sdk)
       }
     }.asCompletableFuture()
   }
@@ -138,9 +79,5 @@ class JdkDownloadService(private val project: Project, private val coroutineScop
     return scheduleDownloadJdk(sdkDownloadTask) { sdk ->
       ModuleRootModificationUtil.setModuleSdk(module, sdk)
     }
-  }
-
-  companion object {
-    private val LOG = logger<JdkDownloadService>()
   }
 }
---
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/JdkDownloadUtil.kt
@@ -0,0 +1,83 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.projectRoots.impl.jdkDownloader
+
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.writeAction
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.diagnostic.runAndLogException
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectBundle
+import com.intellij.openapi.projectRoots.ProjectJdkTable
+import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.projectRoots.SdkType
+import com.intellij.openapi.roots.ui.configuration.projectRoot.ProjectSdksModel
+import com.intellij.openapi.roots.ui.configuration.projectRoot.SdkDownloadTask
+import com.intellij.openapi.roots.ui.configuration.projectRoot.SdkDownloadTracker
+import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.util.use
+import com.intellij.util.concurrency.annotations.RequiresWriteLock
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
+import org.jetbrains.annotations.ApiStatus
+import java.nio.file.Path
+import kotlin.coroutines.resume
+
+@ApiStatus.Internal
+object JdkDownloadUtil {
+
+  private val LOG = logger<JdkDownloadUtil>()
+
+  suspend fun createDownloadTask(project: Project, jdkItem: JdkItem, jdkHome: Path): SdkDownloadTask? {
+    val (selectedFile, error) = JdkInstaller.getInstance().validateInstallDir(jdkHome.toString())
+    if (selectedFile == null) {
+      withContext(Dispatchers.EDT) {
+        Messages.showErrorDialog(project,
+                                 ProjectBundle.message("error.message.text.jdk.download.failed", error),
+                                 ProjectBundle.message("error.message.title.download.jdk")
+        )
+      }
+      return null
+    }
+
+    val downloadRequest = LOG.runAndLogException {
+      JdkInstaller.getInstance().prepareJdkInstallation(jdkItem, selectedFile)
+    } ?: return null
+
+    return JdkDownloadTask(jdkItem, downloadRequest, project)
+  }
+
+  suspend fun createDownloadSdk(sdkType: SdkType, sdkDownloadTask: SdkDownloadTask): Sdk {
+    return writeAction {
+      createDownloadSdkInternal(sdkType, sdkDownloadTask)
+    }
+  }
+
+  @ApiStatus.Internal
+  @RequiresWriteLock
+  fun createDownloadSdkInternal(sdkType: SdkType, sdkDownloadTask: SdkDownloadTask): Sdk {
+    val sdkTable = ProjectJdkTable.getInstance()
+    val sdks = sdkTable.allJdks.asList()
+    val sdk = ProjectSdksModel.createDownloadSdkInternal(sdkType, sdkDownloadTask, sdks)
+    sdkTable.addJdk(sdk)
+    return sdk
+  }
+
+  suspend fun downloadSdk(sdk: Sdk): Boolean {
+    return withContext(Dispatchers.EDT) {
+      Disposer.newDisposable("The JdkDownloader#downloadSdk lifecycle").use { disposable ->
+        val tracker = SdkDownloadTracker.getInstance()
+        tracker.startSdkDownloadIfNeeded(sdk)
+        suspendCancellableCoroutine { continuation ->
+          val registered = tracker.tryRegisterDownloadingListener(sdk, disposable, null) {
+            continuation.resume(it)
+          }
+          if (!registered) {
+            continuation.resume(false)
+          }
+        }
+      }
+    }
+  }
+}
