b21ead9fa6ff467fe1b54c9cb4067705c3a4a37f
Dmitry Drobotov <dmitry.drobotov@jetbrains.com>
2024-12-23 19:14:30 +0100

    IJPL-162856 Fix tag button accessible name
    GitOrigin-RevId: 5ff9ac44338b77f3e41223961381d3c5058bb83b

--- platform/platform-api/src/com/intellij/execution/ui/TagButton.java
+++ platform/platform-api/src/com/intellij/execution/ui/TagButton.java
@@ -86,7 +86,7 @@ public class TagButton extends JBLayeredPane implements Disposable {
       .setTitle(OptionsBundle.message("tag.button.tooltip"))
       .setShortcut(new KeyboardShortcut(KeyStroke.getKeyStroke(KeyEvent.VK_BACK_SPACE, 0), null))
       .installOn(myCloseButton);
-    myCloseButton.getAccessibleContext().setAccessibleName("tag.button.tooltip");
+    myCloseButton.getAccessibleContext().setAccessibleName(OptionsBundle.message("tag.button.tooltip"));
     add(myCloseButton, JLayeredPane.POPUP_LAYER);
 
     layoutButtons();
