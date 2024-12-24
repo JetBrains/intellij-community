3c250be0e9650350528a3987c49abb787ecc9cb7
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2024-12-24 12:23:42 +0100

    [es] convert ExternalSystemTestUtil to Kotlin IDEA-361978
    This step is required to implement ad-hoc data import in tests with `timeoutRunBlocking`
    
    GitOrigin-RevId: c108d4924f4a5bc63e5ec1780c9772cc6cee872b

--- platform/external-system-api/testFramework/src/com/intellij/platform/externalSystem/testFramework/ExternalSystemTestUtil.kt
+++ platform/external-system-api/testFramework/src/com/intellij/platform/externalSystem/testFramework/ExternalSystemTestUtil.kt
@@ -1,17 +1,15 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.externalSystem.testFramework;
+package com.intellij.platform.externalSystem.testFramework
 
-import com.intellij.openapi.externalSystem.model.ProjectSystemId;
-import com.intellij.util.messages.Topic;
+import com.intellij.openapi.externalSystem.model.ProjectSystemId
+import com.intellij.util.messages.Topic
 
-public final class ExternalSystemTestUtil {
+object ExternalSystemTestUtil {
+  @JvmField
+  val TEST_EXTERNAL_SYSTEM_ID: ProjectSystemId = ProjectSystemId("TEST_EXTERNAL_SYSTEM_ID")
 
-  public static final ProjectSystemId TEST_EXTERNAL_SYSTEM_ID = new ProjectSystemId("TEST_EXTERNAL_SYSTEM_ID");
-
-  public static final Topic<TestExternalSystemSettingsListener> SETTINGS_TOPIC = Topic.create(
-    "TEST_EXTERNAL_SYSTEM_SETTINGS", TestExternalSystemSettingsListener.class
-  );
-
-  private ExternalSystemTestUtil() {
-  }
+  @JvmField
+  val SETTINGS_TOPIC: Topic<TestExternalSystemSettingsListener?> = Topic.create<TestExternalSystemSettingsListener?>(
+    "TEST_EXTERNAL_SYSTEM_SETTINGS", TestExternalSystemSettingsListener::class.java
+  )
 }
