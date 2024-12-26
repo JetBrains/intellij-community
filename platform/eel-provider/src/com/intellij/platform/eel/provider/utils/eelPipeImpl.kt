c0f14719ffc7bc0a6164dd41f0691335b2a16ba4
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2024-12-26 17:32:28 +0100

    ijent and eel: use `sendWholeBuffer` in ijent, wait for send completion in pipes.
    Client might read smaller chunks so `send` isn't enough (tail might be lost), we use `sendWholeBuffer`.
    
    `sendWholeBuffer` assumes that `send` returns when some data read: we use `sendLocks` for that.
    
    GitOrigin-RevId: cb88a290cb772cd17b5dc94ace0993b8eb6e5e09

--- platform/eel-provider/src/com/intellij/platform/eel/provider/utils/eelPipeImpl.kt
+++ platform/eel-provider/src/com/intellij/platform/eel/provider/utils/eelPipeImpl.kt
@@ -8,11 +8,13 @@ import com.intellij.platform.eel.channels.EelReceiveChannel
 import com.intellij.platform.eel.channels.EelSendChannel
 import com.intellij.platform.eel.provider.ResultErrImpl
 import com.intellij.platform.eel.provider.ResultOkImpl
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ClosedReceiveChannelException
 import kotlinx.coroutines.channels.ClosedSendChannelException
 import kotlinx.coroutines.delay
 import java.nio.ByteBuffer
+import java.util.concurrent.ConcurrentLinkedDeque
 import java.util.concurrent.atomic.AtomicInteger
 
 
@@ -22,7 +24,7 @@ internal class EelPipeImpl() : EelPipe, EelSendChannel<ErrorString>, EelReceiveC
     val OK_NOT_EOF = ResultOkImpl(ReadResult.NOT_EOF)
   }
 
-  private val channel = Channel<ByteBuffer>()
+  private val channel = Channel<Pair<ByteBuffer, CompletableDeferred<Unit>>>()
 
   /**
    * Number of bytes currently waiting to be received by [source].
@@ -33,12 +35,16 @@ internal class EelPipeImpl() : EelPipe, EelSendChannel<ErrorString>, EelReceiveC
 
   override val sink: EelSendChannel<ErrorString> = this
   override val source: EelReceiveChannel<ErrorString> = this
+  private val sendLocks = ConcurrentLinkedDeque<CompletableDeferred<Unit>>()
 
   override suspend fun send(src: ByteBuffer): EelResult<Unit, ErrorString> {
     val remaining = src.remaining()
+    val sendLock = CompletableDeferred<Unit>()
+    // `send` should return when buffer is read
+    sendLocks.add(sendLock)
     try {
       _bytesInQueue.addAndGet(remaining)
-      channel.send(src)
+      channel.send(Pair(src, sendLock))
       return OK_UNIT
     }
     catch (_: ClosedSendChannelException) {
@@ -51,11 +57,13 @@ internal class EelPipeImpl() : EelPipe, EelSendChannel<ErrorString>, EelReceiveC
     }
     finally {
       _bytesInQueue.addAndGet(-remaining)
+      sendLock.await() //wait for buffer to read
+      sendLocks.remove(sendLock)
     }
   }
 
   override suspend fun receive(dst: ByteBuffer): EelResult<ReadResult, ErrorString> {
-    val src = try {
+    val (src, sendLock) = try {
       channel.receive()
     }
     catch (_: ClosedReceiveChannelException) {
@@ -77,6 +85,7 @@ internal class EelPipeImpl() : EelPipe, EelSendChannel<ErrorString>, EelReceiveC
       dst.put(src.limit(src.position() + dst.remaining()))
       src.limit(l)
     }
+    sendLock.complete(Unit) //buffer read
     return OK_NOT_EOF
   }
 
@@ -96,6 +105,10 @@ internal class EelPipeImpl() : EelPipe, EelSendChannel<ErrorString>, EelReceiveC
     }
     channel.close(error?.let { PipeBrokenException(it) })
     channel.cancel() //If there is a coroutine near `send`, it must get an error
+    for (deferred in sendLocks) {
+      deferred.complete(Unit)
+    }
+    sendLocks.clear()
   }
 }
 
