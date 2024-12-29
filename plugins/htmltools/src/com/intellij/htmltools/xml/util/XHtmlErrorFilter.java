144e50ce8868714e5195b6a8b1d37bf5ac8470ab
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-12-29 22:10:00 +0100

    [xml] IJPL-108856 Do not report Multiple root tags error in injected fragments
    GitOrigin-RevId: 6b12c318cea9b943dca92b703f08f2a1892d3220

--- plugins/htmltools/src/com/intellij/htmltools/xml/util/XHtmlErrorFilter.java
+++ plugins/htmltools/src/com/intellij/htmltools/xml/util/XHtmlErrorFilter.java
@@ -2,18 +2,24 @@ package com.intellij.htmltools.xml.util;
 
 import com.intellij.codeInsight.highlighting.HighlightErrorFilter;
 import com.intellij.lang.injection.InjectedLanguageManager;
-import com.intellij.lang.xhtml.XHTMLLanguage;
-import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiErrorElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.xml.parsing.XmlParserBundle;
 import org.jetbrains.annotations.NotNull;
 
-public final class XHtmlErrorFilter extends HighlightErrorFilter {
+import java.util.Objects;
+
+final class XHtmlErrorFilter extends HighlightErrorFilter {
   @Override
   public boolean shouldHighlightErrorElement(@NotNull PsiErrorElement element) {
     PsiFile containingFile = element.getContainingFile();
-    return containingFile.getLanguage() != XHTMLLanguage.INSTANCE ||
-           !(InjectedLanguageManager.getInstance(containingFile.getProject()).getInjectionHost(containingFile) instanceof PsiComment) ||
-           !element.getErrorDescription().contains("Multiple root tags");
+    if (containingFile == null) return true;
+
+    if (InjectedLanguageManager.getInstance(containingFile.getProject()).isInjectedFragment(containingFile)
+        && Objects.equals(XmlParserBundle.message("xml.parsing.multiple.root.tags"), element.getErrorDescription())) {
+      return false;
+    }
+
+    return true;
   }
 }
