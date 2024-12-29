1621e68ad218587d64438357993cbf911f1b8a3d
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-12-29 21:41:20 +0100

    [html] IJPL-149618 Disable required element inspection inside injected fragments
    GitOrigin-RevId: dccf771ceaefe87ececad0561b86fd4593980e89

--- plugins/htmltools/src/com/intellij/htmltools/codeInspection/htmlInspections/HtmlRequiredTitleElementInspection.java
+++ plugins/htmltools/src/com/intellij/htmltools/codeInspection/htmlInspections/HtmlRequiredTitleElementInspection.java
@@ -5,6 +5,8 @@ import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.codeInspection.htmlInspections.HtmlLocalInspectionTool;
 import com.intellij.htmltools.HtmlToolsBundle;
+import com.intellij.lang.injection.InjectedLanguageManager;
+import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.xml.XmlCustomElementDescriptor;
 import com.intellij.xml.util.HtmlUtil;
@@ -19,11 +21,22 @@ public final class HtmlRequiredTitleElementInspection extends HtmlLocalInspectio
   private static final String HEAD = "head";
 
   @Override
+  public @NotNull PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
+    if (InjectedLanguageManager.getInstance(holder.getProject()).isInjectedFragment(holder.getFile())) {
+      // not inside injected code
+      return PsiElementVisitor.EMPTY_VISITOR;
+    }
+
+    return super.buildVisitor(holder, isOnTheFly);
+  }
+
+  @Override
   protected void checkTag(@NotNull XmlTag tag, @NotNull ProblemsHolder holder, boolean isOnTheFly) {
     if (!HtmlUtil.isHtmlTagContainingFile(tag)) return;
     if (!tag.getName().equalsIgnoreCase(HEAD)) return;
     if (XmlCustomElementDescriptor.isCustomElement(tag)) return;
-    final XmlTag[] subTags = tag.getSubTags();
+
+    XmlTag[] subTags = tag.getSubTags();
     long count = Arrays.stream(subTags).filter((element) -> element.getName().equalsIgnoreCase(TITLE)).count();
     if (count > 0) return;
     List<LocalQuickFix> fixes = new ArrayList<>();
@@ -31,10 +44,5 @@ public final class HtmlRequiredTitleElementInspection extends HtmlLocalInspectio
 
     InspectionUtils.RegisterProblem(tag, holder, fixes, HtmlToolsBundle.message("html.inspections.check.required.title.element"),
                                     ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
-  }
-
-  @Override
-  public @NotNull String getShortName() {
-    return "HtmlRequiredTitleElement";
   }
 }
