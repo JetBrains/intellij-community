4d1e7c81619d1ab56759d4db979d741bced31d7e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-12-31 11:50:00 +0100

    fix api dump
    GitOrigin-RevId: 9dbac7bb016577b225e30d14ff06e7db318f8fcb

--- jps/jps-builders/src/org/jetbrains/jps/javac/ExternalJavacManagerKey.java
+++ jps/jps-builders/src/org/jetbrains/jps/javac/ExternalJavacManagerKey.java
@@ -1,8 +1,10 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.jps.javac;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.jps.incremental.GlobalContextKey;
 
+@ApiStatus.Internal
 public final class ExternalJavacManagerKey {
   private ExternalJavacManagerKey() {}
 
