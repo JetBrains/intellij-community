91fac51b235abc76feaf1925cbc5bb4afd4b7809
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-01-03 15:35:08 +0100

    [kotlin] Fix test data after #KT-73755
    GitOrigin-RevId: 589df105d1ec2e045ecb68888c7b6e3ec914240a

--- plugins/kotlin/j2k/shared/tests/testData/newJ2k/nullabilityGenerics/notNullTypeArgumentFromKotlinTypeParameterInCall.kt
+++ plugins/kotlin/j2k/shared/tests/testData/newJ2k/nullabilityGenerics/notNullTypeArgumentFromKotlinTypeParameterInCall.kt
@@ -1,5 +1,5 @@
-// ERROR: Argument type mismatch: actual type is 'Topic<kotlin.String?>', but 'Topic<T (of fun <T : Any> f)>' was expected.
-// ERROR: Argument type mismatch: actual type is 'Topic<kotlin.String?>', but 'Topic<T (of fun <T : Any> f)>' was expected.
+// ERROR: Argument type mismatch: actual type is 'Topic<kotlin.String?>', but 'Topic<kotlin.String>' was expected.
+// ERROR: Argument type mismatch: actual type is 'Topic<kotlin.String?>', but 'Topic<kotlin.String>' was expected.
 internal class J {
     var topic: Topic<String?> = Topic<String?>()
 
