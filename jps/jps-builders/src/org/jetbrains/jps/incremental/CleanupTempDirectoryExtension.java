59ea324834185652835c53878ed1d2f10158b0db
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-01-03 09:24:09 +0100

    CleanupTempDirectoryExtension - use NIO
    GitOrigin-RevId: a00c5bd3d2fdea3a354fd55ae4fd483820da2c44

--- jps/jps-builders/src/org/jetbrains/jps/incremental/CleanupTempDirectoryExtension.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/CleanupTempDirectoryExtension.java
@@ -1,9 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.jps.incremental;
 
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.io.FileUtilRt;
-import com.intellij.openapi.util.text.Strings;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.jps.builders.PreloadedDataExtension;
@@ -11,7 +9,12 @@ import org.jetbrains.jps.cmdline.PreloadedData;
 import org.jetbrains.jps.cmdline.ProjectDescriptor;
 import org.jetbrains.jps.service.JpsServiceManager;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.RunnableFuture;
@@ -50,34 +53,45 @@ public final class CleanupTempDirectoryExtension implements PreloadedDataExtensi
 
   static @Nullable Future<Void> startTempDirectoryCleanupTask(@NotNull ProjectDescriptor projectDescriptor) {
     String tempPath = System.getProperty("java.io.tmpdir", null);
-    if (Strings.isEmptyOrSpaces(tempPath)) {
+    if (tempPath == null || tempPath.isBlank()) {
       return null;
     }
 
-    File tempDir = new File(tempPath);
-    File dataRoot = projectDescriptor.dataManager.getDataPaths().getDataStorageRoot();
-    if (!FileUtil.isAncestor(dataRoot.getPath(), tempDir.getPath(), true)) {
+    Path tempDir = Path.of(tempPath).toAbsolutePath().normalize();
+    Path dataRoot = projectDescriptor.dataManager.getDataPaths().getDataStorageRoot().toPath();
+    if (!tempDir.startsWith(dataRoot)) {
       // cleanup only 'local' temp
       return null;
     }
 
-    File[] files = tempDir.listFiles();
-    if (files == null) {
-      // ensure the directory exists
-      tempDir.mkdirs();
+    //noinspection DuplicatedCode
+    List<Path> files = new ArrayList<>();
+    try (DirectoryStream<Path> stream = Files.newDirectoryStream(tempDir)) {
+      for (Path path : stream) {
+        files.add(path);
+      }
     }
-    else if (files.length > 0) {
-      RunnableFuture<Void> task = new FutureTask<>(() -> {
-        for (File tempFile : files) {
-          FileUtilRt.delete(tempFile);
+    catch (IOException ignored) {
+      return null;
+    }
+
+    if (files.isEmpty()) {
+      return null;
+    }
+
+    RunnableFuture<Void> task = new FutureTask<>(() -> {
+      for (Path tempFile : files) {
+        try {
+          FileUtilRt.deleteRecursively(tempFile);
         }
-      }, null);
-      Thread thread = new Thread(task, "Temp directory cleanup");
-      thread.setPriority(Thread.MIN_PRIORITY);
-      thread.setDaemon(true);
-      thread.start();
-      return task;
-    }
-    return null;
+        catch (IOException ignored) {
+        }
+      }
+    }, null);
+    Thread thread = new Thread(task, "Temp directory cleanup");
+    thread.setPriority(Thread.MIN_PRIORITY);
+    thread.setDaemon(true);
+    thread.start();
+    return task;
   }
 }
