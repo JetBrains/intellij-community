1ad98f1540a36e88f8e4deaa3affcb83928b57fc
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-01-05 16:13:00 +0100

    Deprecate com.intellij.lang.WhitespacesAndCommentsBinder.RecursiveBinder
    GitOrigin-RevId: d287a1403f37064ccb9c3f855efe42d49053b5da

--- platform/core-impl/src/com/intellij/lang/impl/PsiBuilderImpl.java
+++ platform/core-impl/src/com/intellij/lang/impl/PsiBuilderImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.lang.impl;
 
 import com.intellij.lang.*;
@@ -1197,7 +1197,7 @@ public class PsiBuilderImpl extends UnprotectedUserDataHolder implements PsiBuil
       }
       int lexemeIndex = item.getLexemeIndex(done);
 
-      boolean recursive = binder instanceof WhitespacesAndCommentsBinder.RecursiveBinder;
+      boolean recursive = binder.isRecursive();
       int prevProductionLexIndex;
       if (recursive) {
         prevProductionLexIndex = 0;
