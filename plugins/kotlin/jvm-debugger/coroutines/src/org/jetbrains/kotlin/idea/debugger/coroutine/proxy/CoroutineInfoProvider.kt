cc2d37d9ccf9380264dafa75559f2c3fbbf7b6c6
Maria Sokolova <maria.sokolova@jetbrains.com>
2025-01-06 18:30:07 +0100

    [coroutines-debugger] Refactoring: moved all coroutine info providers to one file, implementing one sealed interface
    GitOrigin-RevId: 71b10311c449717bce7a90eb71a792be3c1e6250

--- plugins/kotlin/jvm-debugger/coroutines/src/org/jetbrains/kotlin/idea/debugger/coroutine/proxy/CoroutineInfoProvider.kt
+++ plugins/kotlin/jvm-debugger/coroutines/src/org/jetbrains/kotlin/idea/debugger/coroutine/proxy/CoroutineInfoProvider.kt
@@ -2,8 +2,158 @@
 
 package org.jetbrains.kotlin.idea.debugger.coroutine.proxy
 
-import org.jetbrains.kotlin.idea.debugger.coroutine.data.CoroutineInfoData
+import com.google.gson.Gson
+import com.intellij.debugger.engine.DebuggerManagerThreadImpl
+import com.sun.jdi.ArrayReference
+import com.sun.jdi.ObjectReference
+import com.sun.jdi.StringReference
+import com.sun.jdi.ThreadReference
+import org.jetbrains.annotations.VisibleForTesting
+import org.jetbrains.kotlin.idea.debugger.base.util.evaluate.DefaultExecutionContext
+import org.jetbrains.kotlin.idea.debugger.coroutine.data.*
+import org.jetbrains.kotlin.idea.debugger.coroutine.proxy.mirror.*
+import org.jetbrains.kotlin.idea.debugger.coroutine.util.logger
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
+import kotlin.reflect.typeOf
 
-interface CoroutineInfoProvider {
-    fun dumpCoroutinesInfo(): List<CoroutineInfoData>
+sealed interface CoroutineInfoProvider {
+    fun dumpCoroutinesInfo(): List<CoroutineInfoData>?
+}
+
+class CoroutinesInfoFromJsonAndReferencesProvider(
+    private val executionContext: DefaultExecutionContext,
+    private val debugProbesImpl: DebugProbesImpl
+) : CoroutineInfoProvider {
+    private val stackFramesProvider = CoroutineStackFramesProvider(executionContext)
+    private val jobHierarchyProvider = CoroutineJobHierarchyProvider()
+
+    override fun dumpCoroutinesInfo(): List<CoroutineInfoData> {
+        val array = debugProbesImpl.dumpCoroutinesInfoAsJsonAndReferences(executionContext)
+            ?: return emptyList()
+
+        val arrayValues = array.values // fetch all values at once
+
+        if (arrayValues.size != 4) {
+            error("The result array of 'dumpCoroutinesInfoAsJSONAndReferences' should be of size 4")
+        }
+
+        val coroutinesInfoAsJsonString = arrayValues[0].safeAs<StringReference>()?.value()
+            ?: error("The first element of the result array must be a string")
+        val lastObservedThreadRefs = arrayValues[1].safeAs<ArrayReference>()?.toTypedList<ThreadReference?>()
+            ?: error("The second element of the result array must be an array")
+        val lastObservedFrameRefs = arrayValues[2].safeAs<ArrayReference>()?.toTypedList<ObjectReference?>()
+            ?: error("The third element of the result array must be an array")
+        val coroutineInfoRefs = arrayValues[3].safeAs<ArrayReference>()?.toTypedList<ObjectReference>()
+            ?: error("The fourth element of the result array must be an array")
+        val coroutinesInfo = Gson().fromJson(coroutinesInfoAsJsonString, Array<CoroutineInfoFromJson>::class.java)
+
+        if (coroutineInfoRefs.size != lastObservedFrameRefs.size ||
+            lastObservedFrameRefs.size != coroutinesInfo.size ||
+            coroutinesInfo.size != lastObservedThreadRefs.size) {
+            error("Arrays must have equal sizes")
+        }
+
+        return calculateCoroutineInfoData(coroutinesInfo, coroutineInfoRefs, lastObservedThreadRefs, lastObservedFrameRefs)
+    }
+
+    private fun calculateCoroutineInfoData(
+        coroutineInfos: Array<CoroutineInfoFromJson>,
+        coroutineInfoRefs: List<ObjectReference>,
+        lastObservedThreadRefs: List<ThreadReference?>,
+        lastObservedFrameRefs: List<ObjectReference?>
+    ): List<CoroutineInfoData> {
+        val result = mutableListOf<CoroutineInfoData>()
+        for ((i, info) in coroutineInfos.withIndex()) {
+            result.add(
+                getLazyCoroutineInfoData(
+                    info,
+                    coroutineInfoRefs[i],
+                    lastObservedThreadRefs[i],
+                    lastObservedFrameRefs[i],
+                    stackFramesProvider,
+                    jobHierarchyProvider
+                )
+            )
+        }
+        return result
+    }
+
+    private fun getLazyCoroutineInfoData(
+        info: CoroutineInfoFromJson,
+        coroutineInfoRef: ObjectReference,
+        lastObservedThreadRef: ThreadReference?,
+        lastObservedFrameRef: ObjectReference?,
+        stackTraceProvider: CoroutineStackFramesProvider,
+        jobHierarchyProvider: CoroutineJobHierarchyProvider
+    ): CoroutineInfoData {
+        DebuggerManagerThreadImpl.assertIsManagerThread()
+
+        // coroutineInfo is a DebugCoroutineInfo. Need to get coroutineInfoRef.context to pass in to CoroutineContext
+        val contextRef = CoroutineInfo.instance(executionContext)?.getContextRef(coroutineInfoRef)
+        val coroutineContextMirror = contextRef?.let {
+            CoroutineContext(executionContext).fetchMirror(info.name, info.id, info.dispatcher, it, executionContext)
+        } ?: MirrorOfCoroutineContext(
+            info.name,
+            info.id,
+            info.dispatcher,
+            null,
+            null
+        )
+        val coroutineInfoMirror = debugProbesImpl.getCoroutineInfo(
+            coroutineInfoRef,
+            executionContext,
+            coroutineContextMirror,
+            info.sequenceNumber,
+            info.state,
+            lastObservedThreadRef,
+            lastObservedFrameRef
+        )
+
+        return createCoroutineInfoDataFromMirror(coroutineInfoMirror, stackTraceProvider)
+    }
+
+    private data class CoroutineInfoFromJson(
+        val name: String?,
+        val id: Long?,
+        val dispatcher: String?,
+        val sequenceNumber: Long?,
+        val state: String?
+    )
+
+    companion object {
+        fun instance(executionContext: DefaultExecutionContext, debugProbesImpl: DebugProbesImpl): CoroutinesInfoFromJsonAndReferencesProvider? {
+            if (debugProbesImpl.canDumpCoroutinesInfoAsJsonAndReferences()) {
+                return CoroutinesInfoFromJsonAndReferencesProvider(executionContext, debugProbesImpl)
+            }
+            return null
+        }
+
+        val log by logger
+    }
+}
+
+internal class CoroutineLibraryAgent2Proxy(
+    private val executionContext: DefaultExecutionContext,
+    private val debugProbesImpl: DebugProbesImpl
+) : CoroutineInfoProvider {
+    private val stackFramesProvider = CoroutineStackFramesProvider(executionContext)
+
+    override fun dumpCoroutinesInfo(): List<CoroutineInfoData> {
+        val result = debugProbesImpl.dumpCoroutinesInfo(executionContext)
+        return result.map {
+            createCoroutineInfoDataFromMirror(it, stackFramesProvider)
+        }
+    }
+}
+
+private inline fun <reified T> ArrayReference.toTypedList() = values.toTypedList<T>()
+
+@VisibleForTesting
+inline fun <reified T> List<Any?>.toTypedList(): List<T> = map {
+    if (it == null) {
+        require(typeOf<T>().isMarkedNullable) { "Value should not be null" }
+        return@map null as T
+    }
+    require(it is T) { "Value has type ${it::class.java}, but ${T::class.java} was expected" }
+    it
 }
