b1fefb91fa7eae91f806bee4488c929597c1cdd7
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-01-07 12:41:50 +0100

    MacroTokenType is implementation detail
    GitOrigin-RevId: d91c5e1fa93bde100519e58605ecf2355885872b

--- platform/analysis-impl/src/com/intellij/codeInsight/template/impl/MacroTokenType.java
+++ platform/analysis-impl/src/com/intellij/codeInsight/template/impl/MacroTokenType.java
@@ -1,12 +1,13 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package com.intellij.codeInsight.template.impl;
 
 import com.intellij.lang.Language;
 import com.intellij.psi.TokenType;
 import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.ApiStatus;
 
-
+@ApiStatus.Internal
 public interface MacroTokenType {
   IElementType WHITE_SPACE = TokenType.WHITE_SPACE;
   IElementType IDENTIFIER = new IElementType("IDENTIFIER", Language.ANY);
