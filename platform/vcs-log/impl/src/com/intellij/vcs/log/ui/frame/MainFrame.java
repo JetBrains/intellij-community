be1e12b122866f12e3806feddc970aaa1abc6d45
Ivan Semenov <ivan.semenov@jetbrains.com>
2025-01-07 20:10:38 +0100

    [vcs-log] Extract Log table components from MainFrame
    GitOrigin-RevId: 58364ee82b8305024e218530a6e42be813e6ac78

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/frame/MainFrame.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/frame/MainFrame.java
@@ -1,39 +1,33 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.log.ui.frame;
 
-import com.google.common.primitives.Ints;
 import com.intellij.diff.impl.DiffEditorViewer;
-import com.intellij.ide.ui.customization.CustomActionsSchema;
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.actionSystem.ex.ActionUtil;
+import com.intellij.openapi.actionSystem.DataSink;
+import com.intellij.openapi.actionSystem.UiDataProvider;
 import com.intellij.openapi.progress.util.ProgressIndicatorWithDelayedPresentation;
-import com.intellij.openapi.ui.SimpleToolWindowPanel;
 import com.intellij.openapi.ui.Splitter;
 import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.util.EmptyRunnable;
-import com.intellij.openapi.util.NlsActions;
 import com.intellij.openapi.util.NlsContexts;
 import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.VcsDataKeys;
 import com.intellij.openapi.vcs.changes.Change;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.ui.*;
+import com.intellij.ui.EditorNotificationPanel;
+import com.intellij.ui.JBColor;
+import com.intellij.ui.OnePixelSplitter;
 import com.intellij.ui.components.JBLoadingPanel;
-import com.intellij.ui.components.panels.Wrapper;
 import com.intellij.ui.navigation.History;
-import com.intellij.ui.switcher.QuickActionProvider;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
 import com.intellij.util.ui.components.BorderLayoutPanel;
 import com.intellij.util.ui.table.ComponentsListFocusTraversalPolicy;
-import com.intellij.vcs.log.VcsLogBundle;
 import com.intellij.vcs.log.data.VcsLogData;
 import com.intellij.vcs.log.impl.CommonUiProperties;
 import com.intellij.vcs.log.impl.MainVcsLogUiProperties;
 import com.intellij.vcs.log.impl.VcsLogNavigationUtil;
-import com.intellij.vcs.log.ui.*;
+import com.intellij.vcs.log.ui.AbstractVcsLogUi;
+import com.intellij.vcs.log.ui.VcsLogColorManager;
 import com.intellij.vcs.log.ui.details.CommitDetailsListPanel;
 import com.intellij.vcs.log.ui.details.commit.CommitDetailsPanel;
 import com.intellij.vcs.log.ui.filter.VcsLogFilterUiEx;
@@ -42,7 +36,6 @@ import com.intellij.vcs.log.util.VcsLogUiUtil;
 import com.intellij.vcs.log.util.VcsLogUtil;
 import com.intellij.vcs.log.visible.VisiblePack;
 import kotlin.Unit;
-import net.miginfocom.swing.MigLayout;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -50,18 +43,16 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.*;
 import javax.swing.border.CompoundBorder;
 import java.awt.*;
-import java.util.*;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
-import static com.intellij.openapi.vfs.VfsUtilCore.toVirtualFileArray;
 import static com.intellij.util.ObjectUtils.notNull;
-import static com.intellij.util.containers.ContainerUtil.getOnlyItem;
 
 public class MainFrame extends JPanel implements UiDataProvider, Disposable {
   private static final @NonNls String DIFF_SPLITTER_PROPORTION = "vcs.log.diff.splitter.proportion";
   private static final @NonNls String DETAILS_SPLITTER_PROPORTION = "vcs.log.details.splitter.proportion";
   private static final @NonNls String CHANGES_SPLITTER_PROPORTION = "vcs.log.changes.splitter.proportion";
-  private static final @NonNls String HELP_ID = "reference.changesToolWindow.log";
 
   private final @NotNull VcsLogData myLogData;
   private final @NotNull MainVcsLogUiProperties myUiProperties;
@@ -97,14 +88,7 @@ public class MainFrame extends JPanel implements UiDataProvider, Disposable {
 
     myFilterUi = filterUi;
 
-    myGraphTable = new VcsLogMainGraphTable(logUi, logData, logUi.getProperties(), colorManager,
-                                            () -> logUi.getRefresher().onRefresh(), () -> logUi.requestMore(EmptyRunnable.INSTANCE),
-                                            filterUi,
-                                            disposable);
-    String vcsDisplayName = VcsLogUtil.getVcsDisplayName(logData.getProject(), logData.getLogProviders().values());
-    myGraphTable.getAccessibleContext().setAccessibleName(VcsLogBundle.message("vcs.log.table.accessible.name", vcsDisplayName));
-
-    PopupHandler.installPopupMenu(myGraphTable, VcsLogActionIds.POPUP_ACTION_GROUP, ActionPlaces.VCS_LOG_TABLE_PLACE);
+    myGraphTable = VcsLogComponents.createTable(logData, logUi, filterUi, colorManager, disposable);
 
     myDetailsPanel = new CommitDetailsListPanel(logData.getProject(), this, () -> {
       return new CommitDetailsPanel(commit -> {
@@ -191,7 +175,6 @@ public class MainFrame extends JPanel implements UiDataProvider, Disposable {
 
     Disposer.register(disposable, this);
 
-    myGraphTable.resetDefaultFocusTraversalKeys();
     setFocusCycleRoot(true);
     setFocusTraversalPolicy(new MyFocusPolicy());
   }
@@ -223,84 +206,15 @@ public class MainFrame extends JPanel implements UiDataProvider, Disposable {
   }
 
   protected @NotNull JComponent createActionsToolbar() {
-    ActionManager actionManager = ActionManager.getInstance();
-
-    DefaultActionGroup toolbarGroup = (DefaultActionGroup)actionManager.getAction(VcsLogActionIds.TOOLBAR_ACTION_GROUP);
-
-    DefaultActionGroup mainGroup = new DefaultActionGroup();
-    mainGroup.add(myFilterUi.createActionGroup());
-    mainGroup.addSeparator();
-    mainGroup.add(toolbarGroup);
-    ActionToolbar toolbar = actionManager.createActionToolbar(ActionPlaces.VCS_LOG_TOOLBAR_PLACE, mainGroup, true);
-    toolbar.setTargetComponent(this);
-
-    Wrapper textFilter = new Wrapper(myFilterUi.getTextFilterComponent().getComponent());
-    textFilter.setVerticalSizeReferent(toolbar.getComponent());
-    String vcsDisplayName = VcsLogUtil.getVcsDisplayName(myLogData.getProject(), myLogData.getLogProviders().values());
-    textFilter.getAccessibleContext().setAccessibleName(VcsLogBundle.message("vcs.log.text.filter.accessible.name", vcsDisplayName));
-
-    ActionGroup rightCornerGroup = (ActionGroup)Objects.requireNonNull(
-      CustomActionsSchema.getInstance().getCorrectedAction(VcsLogActionIds.TOOLBAR_RIGHT_CORNER_ACTION_GROUP));
-    ActionToolbar rightCornerToolbar = actionManager.createActionToolbar(ActionPlaces.VCS_LOG_TOOLBAR_PLACE, rightCornerGroup, true);
-    rightCornerToolbar.setTargetComponent(this);
-    rightCornerToolbar.setReservePlaceAutoPopupIcon(false);
-
-    JPanel panel = new JPanel(new MigLayout("ins 0, fill", "[left]0[left, fill]push[pref:pref, right]", "center"));
-    GuiUtils.installVisibilityReferent(panel, toolbar.getComponent());
-    panel.add(textFilter);
-    panel.add(toolbar.getComponent());
-    panel.add(rightCornerToolbar.getComponent());
-    return panel;
+    return VcsLogComponents.createActionsToolbar(myGraphTable, myFilterUi);
   }
 
   @Override
   public void uiDataSnapshot(@NotNull DataSink sink) {
-    Collection<VirtualFile> roots = getSelectedRoots();
     Change[] changes = myChangesBrowser.getDirectChanges().toArray(Change.EMPTY_CHANGE_ARRAY);
     sink.set(VcsDataKeys.CHANGES, changes);
     sink.set(VcsDataKeys.SELECTED_CHANGES, changes);
-    sink.set(VcsLogInternalDataKeys.LOG_UI_PROPERTIES, myUiProperties);
-    sink.set(CommonDataKeys.VIRTUAL_FILE_ARRAY, toVirtualFileArray(roots));
-    VirtualFile onlyRoot = getOnlyItem(roots);
-    if (onlyRoot != null) {
-      sink.set(VcsLogInternalDataKeys.LOG_DIFF_HANDLER,
-               myLogData.getLogProvider(onlyRoot).getDiffHandler());
-    }
-    sink.set(VcsLogInternalDataKeys.VCS_LOG_VISIBLE_ROOTS,
-             VcsLogUtil.getAllVisibleRoots(myLogData.getRoots(), myFilterUi.getFilters()));
-    sink.set(PlatformCoreDataKeys.HELP_ID, HELP_ID);
-    sink.set(History.KEY, myHistory);
-    sink.set(QuickActionProvider.KEY, new QuickActionProvider() {
-      @Override
-      public @NotNull List<AnAction> getActions(boolean originalProvider) {
-        AnAction textFilterAction = ActionUtil.wrap(VcsLogActionIds.VCS_LOG_FOCUS_TEXT_FILTER);
-        textFilterAction.getTemplatePresentation().setText(VcsLogBundle.message("vcs.log.text.filter.action.text"));
-        List<AnAction> actions = new ArrayList<>();
-        actions.add(textFilterAction);
-        actions.addAll(SimpleToolWindowPanel.collectActions(myToolbar));
-        return actions;
-      }
-
-      @Override
-      public JComponent getComponent() {
-        return MainFrame.this;
-      }
-
-      @Override
-      public @NlsActions.ActionText @Nullable String getName() {
-        return null;
-      }
-    });
-  }
-
-  private @NotNull Collection<VirtualFile> getSelectedRoots() {
-    Collection<VirtualFile> roots = myLogData.getRoots();
-    if (roots.size() == 1) return roots;
-    int[] selectedRows = myGraphTable.getSelectedRows();
-    if (selectedRows.length == 0 || selectedRows.length > VcsLogUtil.MAX_SELECTED_COMMITS) {
-      return VcsLogUtil.getAllVisibleRoots(roots, myFilterUi.getFilters());
-    }
-    return ContainerUtil.map2Set(Ints.asList(selectedRows), row -> myGraphTable.getModel().getRootAtRow(row));
+    VcsLogComponents.collectLogKeys(sink, myUiProperties, myGraphTable, myHistory, myFilterUi, myToolbar, this);
   }
 
   public @NotNull JComponent getToolbar() {
