b77ef70c15876bc9204c834635b98d70799985d0
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2025-01-08 12:41:25 +0100

    clean up `mocks.kt`
    GitOrigin-RevId: edc5e4c6e60acca2471e30b73e9799bce2111290

--- platform/testFramework/common/src/common/mock/mocks.kt
+++ platform/testFramework/common/src/common/mock/mocks.kt
@@ -1,9 +1,9 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:JvmName("Mocks")
+
 package com.intellij.testFramework.common.mock
 
 import com.intellij.util.ReflectionUtil
-import java.lang.reflect.InvocationHandler
-import java.lang.reflect.Method
 
 /**
  * This function creates the Java proxy that throws [NotImplementedError] on any method invocation.
@@ -14,15 +14,9 @@ inline fun <reified T> notImplemented(): T {
   return notImplemented(T::class.java)
 }
 
-fun <T> notImplemented(aClass: Class<T>): T {
-  return ReflectionUtil.proxy(aClass, NotImplementedInvocationHandler())
-}
-
-private class NotImplementedInvocationHandler : InvocationHandler {
-  override fun invoke(proxy: Any, method: Method, args: Array<out Any?>?): Any? {
-    val className = method.declaringClass.canonicalName
-    val methodName = method.name
-    val methodParameterTypes = method.parameterTypes.joinToString { it.simpleName }
-    throw NotImplementedError("Method '$className#$methodName($methodParameterTypes)' isn't implemented")
-  }
+fun <T> notImplemented(aClass: Class<T>): T = ReflectionUtil.proxy(aClass) { _, method, _ ->
+  val className = method.declaringClass.canonicalName
+  val methodName = method.name
+  val methodParameterTypes = method.parameterTypes.joinToString { it.simpleName }
+  throw NotImplementedError("Method '$className#$methodName($methodParameterTypes)' isn't implemented")
 }
