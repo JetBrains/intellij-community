f434159b9b171baa45b23f265baa16e72f88621e
Anna Rakhmukova <anna.rakhmukova@jetbrains.com>
2025-01-08 19:07:03 +0100

    [github] Reusable GHPRDetailsComponentFactory
    GitOrigin-RevId: 2224ad35e75b3576ee7ef225862629da4b515a53

--- plugins/github/github-core/src/org/jetbrains/plugins/github/pullrequest/ui/toolwindow/GHPRViewComponentFactory.kt
+++ plugins/github/github-core/src/org/jetbrains/plugins/github/pullrequest/ui/toolwindow/GHPRViewComponentFactory.kt
@@ -4,26 +4,19 @@ package org.jetbrains.plugins.github.pullrequest.ui.toolwindow
 import com.intellij.collaboration.async.nestedDisposable
 import com.intellij.collaboration.ui.CollaborationToolsUIUtil
 import com.intellij.collaboration.ui.LoadingLabel
-import com.intellij.collaboration.ui.codereview.CodeReviewProgressTreeModelFromDetails
-import com.intellij.collaboration.ui.codereview.changes.CodeReviewChangeListComponentFactory
-import com.intellij.collaboration.ui.codereview.details.model.CodeReviewChangeListViewModel
 import com.intellij.collaboration.ui.codereview.list.error.ErrorStatusPanelFactory
 import com.intellij.collaboration.ui.codereview.list.error.ErrorStatusPresenter
 import com.intellij.collaboration.ui.util.bindContentIn
 import com.intellij.ide.DataManager
-import com.intellij.openapi.actionSystem.ActionGroup
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.project.Project
-import com.intellij.ui.ScrollPaneFactory
 import com.intellij.ui.components.panels.Wrapper
 import com.intellij.util.ui.UIUtil
 import kotlinx.coroutines.CoroutineScope
 import org.jetbrains.plugins.github.i18n.GithubBundle
 import org.jetbrains.plugins.github.pullrequest.action.GHPRActionKeys
 import org.jetbrains.plugins.github.pullrequest.ui.details.GHPRDetailsComponentFactory
-import org.jetbrains.plugins.github.pullrequest.ui.details.model.GHPRChangeListViewModel
 import org.jetbrains.plugins.github.pullrequest.ui.details.model.GHPRDetailsLoadingViewModel
-import org.jetbrains.plugins.github.pullrequest.ui.details.model.impl.GHPRChangesViewModel
 import org.jetbrains.plugins.github.pullrequest.ui.details.model.impl.GHPRDetailsViewModel
 import org.jetbrains.plugins.github.pullrequest.ui.toolwindow.model.GHPRInfoViewModel
 import javax.swing.JComponent
@@ -64,8 +57,7 @@ internal class GHPRViewComponentFactory(actionManager: ActionManager,
   private fun CoroutineScope.createInfoComponent(detailsVm: GHPRDetailsViewModel): JComponent {
     return GHPRDetailsComponentFactory.create(this,
                                               project,
-                                              detailsVm,
-                                              createChangesComponent(detailsVm.changesVm)).apply {
+                                              detailsVm).apply {
       reloadDetailsAction.registerCustomShortcutSet(this, nestedDisposable())
     }.let {
       CollaborationToolsUIUtil.wrapWithProgressStripe(this, detailsVm.isUpdating, it)
@@ -76,53 +68,6 @@ internal class GHPRViewComponentFactory(actionManager: ActionManager,
     val errorPresenter = ErrorStatusPresenter.simple(
       GithubBundle.message("cannot.load.details"),
       actionProvider = vm.detailsLoadingErrorHandler::getActionForError
-    )
-    val errorPanel = ErrorStatusPanelFactory.create(error, errorPresenter)
-    return CollaborationToolsUIUtil.moveToCenter(errorPanel)
-  }
-
-  private fun CoroutineScope.createChangesComponent(changesVm: GHPRChangesViewModel): JComponent {
-    val cs = this
-    return Wrapper(LoadingLabel()).apply {
-      bindContentIn(cs, changesVm.changeListVm) { res ->
-        res.result?.let {
-          it.fold(onSuccess = {
-            createChangesPanel(it)
-          }, onFailure = {
-            createChangesErrorComponent(changesVm, it)
-          })
-        } ?: LoadingLabel()
-      }
-    }
-  }
-
-  private fun CoroutineScope.createChangesPanel(changeListVm: GHPRChangeListViewModel): JComponent {
-    val progressModel = CodeReviewProgressTreeModelFromDetails(this, changeListVm)
-    val tree = CodeReviewChangeListComponentFactory.createIn(this, changeListVm, progressModel,
-                                                             GithubBundle.message("pull.request.does.not.contain.changes"))
-
-    val scrollPane = ScrollPaneFactory.createScrollPane(tree, true)
-
-    DataManager.registerDataProvider(scrollPane) { dataId ->
-      when {
-        tree.isShowing ->
-          when {
-            GHPRChangeListViewModel.DATA_KEY.`is`(dataId) -> changeListVm
-            CodeReviewChangeListViewModel.DATA_KEY.`is`(dataId) -> changeListVm
-            else -> null
-          } ?: tree.getData(dataId)
-        else -> null
-      }
-    }
-    tree.installPopupHandler(ActionManager.getInstance().getAction("Github.PullRequest.Changes.Popup") as ActionGroup)
-
-    return scrollPane
-  }
-
-  private fun createChangesErrorComponent(changesVm: GHPRChangesViewModel, error: Throwable): JComponent {
-    val errorPresenter = ErrorStatusPresenter.simple(
-      GithubBundle.message("cannot.load.changes"),
-      actionProvider = changesVm.changesLoadingErrorHandler::getActionForError
     )
     val errorPanel = ErrorStatusPanelFactory.create(error, errorPresenter)
     return CollaborationToolsUIUtil.moveToCenter(errorPanel)
