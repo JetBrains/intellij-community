aa2a8928f7b9aef4ce4e75e9523e1b9b23072152
Andrey Zaytsev <andrey.zaytsev@jetbrains.com>
2025-01-08 14:38:35 +0100

    [wobbler] register problem reporter
    GitOrigin-RevId: 9a91562f803fa9b22e2d3ae23461937860c07cc2

--- platform/core-impl/src/com/intellij/diagnostic/PluginProblemReporterImpl.java
+++ platform/core-impl/src/com/intellij/diagnostic/PluginProblemReporterImpl.java
@@ -7,7 +7,7 @@ import com.intellij.openapi.extensions.PluginId;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-final class PluginProblemReporterImpl implements PluginProblemReporter {
+public final class PluginProblemReporterImpl implements PluginProblemReporter {
   @Override
   public @NotNull PluginException createPluginExceptionByClass(@NotNull String errorMessage,
                                                                @Nullable Throwable cause,
