3c62067d71febb70791f6bb397fe0c82321d2795
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2025-01-08 14:44:55 +0100

    Cleanup (typos; formatting)
    GitOrigin-RevId: 1062aa55742bf7a88614a1ce95961e4afb0ee7cd

--- platform/platform-api/src/com/intellij/openapi/editor/actionSystem/TypedAction.java
+++ platform/platform-api/src/com/intellij/openapi/editor/actionSystem/TypedAction.java
@@ -21,8 +21,10 @@ import java.lang.reflect.Constructor;
  * Provides services for registering actions which are activated by typing in the editor.
  */
 public abstract class TypedAction {
-  private static final ExtensionPointName<EditorTypedHandlerBean> EP_NAME = new ExtensionPointName<>("com.intellij.editorTypedHandler");
-  private static final ExtensionPointName<EditorTypedHandlerBean> RAW_EP_NAME = new ExtensionPointName<>("com.intellij.rawEditorTypedHandler");
+  @SuppressWarnings("removal") private static final ExtensionPointName<EditorTypedHandlerBean> EP_NAME =
+    new ExtensionPointName<>("com.intellij.editorTypedHandler");
+  @SuppressWarnings("removal") private static final ExtensionPointName<EditorTypedHandlerBean> RAW_EP_NAME =
+    new ExtensionPointName<>("com.intellij.rawEditorTypedHandler");
 
   private TypedActionHandler myRawHandler;
   private @NotNull TypedActionHandler myHandler;
@@ -43,7 +45,7 @@ public abstract class TypedAction {
 
     myHandlersLoaded = true;
     EP_NAME.processWithPluginDescriptor((bean, pluginDescriptor) -> {
-      TypedActionHandler handler = getOrCreateHandler(bean, myHandler, pluginDescriptor);
+      var handler = getOrCreateHandler(bean, myHandler, pluginDescriptor);
       if (handler != null) {
         myHandler = handler;
       }
@@ -51,16 +53,19 @@ public abstract class TypedAction {
     });
   }
 
-  private static @Nullable TypedActionHandler getOrCreateHandler(@SuppressWarnings("deprecation") @NotNull EditorTypedHandlerBean bean,
-                                                                 @NotNull TypedActionHandler originalHandler,
-                                                                 @NotNull PluginDescriptor pluginDescriptor) {
-    TypedActionHandler handler = bean.handler;
+  @SuppressWarnings("removal")
+  private static @Nullable TypedActionHandler getOrCreateHandler(
+    @SuppressWarnings("removal") EditorTypedHandlerBean bean,
+    TypedActionHandler originalHandler,
+    PluginDescriptor pluginDescriptor
+  ) {
+    var handler = bean.handler;
     if (handler != null) {
        return handler;
      }
 
     try {
-      Class<TypedActionHandler> aClass = ApplicationManager.getApplication().loadClass(bean.implementationClass, pluginDescriptor);
+      var aClass = ApplicationManager.getApplication().<TypedActionHandler>loadClass(bean.implementationClass, pluginDescriptor);
       Constructor<TypedActionHandler> constructor;
       try {
         constructor = aClass.getDeclaredConstructor(TypedActionHandler.class);
@@ -68,7 +73,6 @@ public abstract class TypedAction {
       catch (NoSuchMethodException ignore) {
         constructor = null;
       }
-
       if (constructor == null) {
         constructor = aClass.getDeclaredConstructor();
         constructor.setAccessible(true);
@@ -95,7 +99,7 @@ public abstract class TypedAction {
 
   private void loadRawHandlers() {
     RAW_EP_NAME.processWithPluginDescriptor((bean, pluginDescriptor) -> {
-      TypedActionHandler handler = getOrCreateHandler(bean, myRawHandler, pluginDescriptor);
+      var handler = getOrCreateHandler(bean, myRawHandler, pluginDescriptor);
       if (handler != null) {
         myRawHandler = handler;
       }
@@ -110,10 +114,10 @@ public abstract class TypedAction {
         return;
       }
 
-      Document doc = editor.getDocument();
+      var doc = editor.getDocument();
       doc.startGuardedBlockChecking();
       try {
-        final String str = String.valueOf(charTyped);
+        final var str = String.valueOf(charTyped);
         CommandProcessor.getInstance().setCurrentCommandName(EditorBundle.message("typing.in.editor.command.name"));
         EditorModificationUtil.typeInStringAtCaretHonorMultipleCarets(editor, str, true);
       }
@@ -127,9 +131,7 @@ public abstract class TypedAction {
   }
 
   /**
-   * Gets the current typing handler.
-   *
-   * @return the current typing handler.
+   * Returns the current typing handler.
    */
   public @NotNull TypedActionHandler getHandler() {
     ensureHandlersLoaded();
@@ -137,8 +139,8 @@ public abstract class TypedAction {
   }
 
   /**
-   * Replaces the typing handler with the specified handler. The handler should pass
-   * unprocessed typing to the previously registered handler.
+   * Replaces the typing handler with the specified handler.
+   * The handler should pass unprocessed typing to the previously registered handler.
    *
    * @param handler the handler to set.
    * @return the previously registered handler.
@@ -147,27 +149,26 @@ public abstract class TypedAction {
   @Deprecated
   public @NotNull TypedActionHandler setupHandler(@NotNull TypedActionHandler handler) {
     ensureHandlersLoaded();
-    TypedActionHandler tmp = myHandler;
+    var tmp = myHandler;
     myHandler = handler;
     return tmp;
   }
 
   /**
-   * Gets the current 'raw' typing handler.
-   *
-   * @see #setupRawHandler(TypedActionHandler)
+   * Returns the current 'raw' typing handler.
    */
   public @NotNull TypedActionHandler getRawHandler() {
     return myRawHandler;
   }
 
   /**
-   * Replaces current 'raw' typing handler with the specified handler. The handler should pass unprocessed typing to the
-   * previously registered 'raw' handler.
+   * Replaces the current 'raw' typing handler with the specified handler.
+   * The handler should pass unprocessed typing to the previously registered 'raw' handler.
    * <p>
-   * 'Raw' handler is a handler directly invoked by the code which handles typing in editor. Default 'raw' handler
-   * performs some generic logic that has to be done on typing (like checking whether file has write access, creating a command
-   * instance for undo subsystem, initiating write action, etc), but delegates to 'normal' handler for actual typing logic.
+   * 'Raw' handler is a handler directly invoked by the code which handles typing in the editor.
+   * Default 'raw' handler performs some generic logic that has to be done on typing (like checking whether a file has write access,
+   * creating a command instance for the undo subsystem, initiating write action, etc.),
+   * but delegates to 'normal' handler for actual typing logic.
    *
    * @param handler the handler to set.
    * @return the previously registered handler.
@@ -177,7 +178,7 @@ public abstract class TypedAction {
    * @see #setupHandler(TypedActionHandler)
    */
   public TypedActionHandler setupRawHandler(@NotNull TypedActionHandler handler) {
-    TypedActionHandler tmp = myRawHandler;
+    var tmp = myRawHandler;
     myRawHandler = handler;
     if (tmp == null) {
       loadRawHandlers();
@@ -191,7 +192,7 @@ public abstract class TypedAction {
     }
   }
 
-  public final void actionPerformed(final @NotNull Editor editor, final char charTyped, @NotNull DataContext dataContext) {
+  public final void actionPerformed(@NotNull Editor editor, char charTyped, @NotNull DataContext dataContext) {
     try (var ignored = SlowOperations.startSection(SlowOperations.ACTION_PERFORM)) {
       myRawHandler.execute(editor, charTyped, dataContext);
     }
