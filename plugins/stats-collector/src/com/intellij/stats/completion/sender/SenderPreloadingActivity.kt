bdaef0b79504c2b1004be4d8480f0dc3afe389bd
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2025-01-08 14:47:29 +0100

    [stats-collector] eliminating unneeded abstractions over service status
    GitOrigin-RevId: 72afd295fa24447b7a29541ac98f3eae39eb90b2

--- plugins/stats-collector/src/com/intellij/stats/completion/sender/SenderPreloadingActivity.kt
+++ plugins/stats-collector/src/com/intellij/stats/completion/sender/SenderPreloadingActivity.kt
@@ -1,60 +1,72 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.stats.completion.sender
 
 import com.intellij.ide.ApplicationActivity
 import com.intellij.internal.statistic.utils.StatisticsUploadAssistant
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.service
-import com.intellij.openapi.diagnostic.getOrLogException
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.extensions.ExtensionNotApplicableException
-import com.intellij.stats.completion.network.status.WebServiceStatusManager
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.stats.completion.network.service.RequestService
+import com.intellij.stats.completion.network.status.bean.AnalyticsPlatformSettingsDeserializer
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.withContext
-import kotlin.coroutines.coroutineContext
 import kotlin.time.Duration.Companion.minutes
 
-private fun isSendAllowed(): Boolean {
-  return isCompletionLogsSendAllowed() && StatisticsUploadAssistant.isSendAllowed()
-}
+internal fun isCompletionLogsSendAllowed(): Boolean =
+  ApplicationManager.getApplication().isEAP && System.getProperty("completion.stats.send.logs", "true").toBoolean()
 
-internal fun isCompletionLogsSendAllowed(): Boolean {
-  return ApplicationManager.getApplication().isEAP && System.getProperty("completion.stats.send.logs", "true").toBoolean()
-}
+private const val USE_ANALYTICS_PLATFORM_KEY = "completion.stats.analytics.platform.send"
+private const val ANALYTICS_PLATFORM_URL_KEY = "completion.stats.analytics.platform.url"
+
+private val LOG = logger<SenderPreloadingActivity>()
 
 private class SenderPreloadingActivity : ApplicationActivity {
+  private val statusUrl: String
+
   init {
     val app = ApplicationManager.getApplication()
-    if (app.isUnitTestMode || app.isHeadlessEnvironment) {
+    if (app.isUnitTestMode || app.isHeadlessEnvironment || !Registry.`is`(USE_ANALYTICS_PLATFORM_KEY, false)) {
       throw ExtensionNotApplicableException.create()
     }
+    statusUrl = Registry.get(ANALYTICS_PLATFORM_URL_KEY).takeIf { it.isChangedFromDefault() }?.asString()
+                ?: "https://resources.jetbrains.com/storage/ap/mlcc/config/v1/${ApplicationInfo.getInstance().build.productCode}.json"
   }
 
   override suspend fun execute() {
     // do not check right after the start - avoid getting UsageStatisticsPersistenceComponent too early
     delay(5.minutes)
-    while (isSendAllowed()) {
+    while (isCompletionLogsSendAllowed() && StatisticsUploadAssistant.isSendAllowed()) {
       delay(5.minutes)
-      runCatching {
-        send()
-      }.getOrLogException(logger<SenderPreloadingActivity>())
+      withContext(Dispatchers.IO) {
+        runCatching {
+          updateAndGetUrl()?.let { url ->
+            service<StatisticSender>().sendStatsData(url)
+          }
+        }.onFailure {
+          LOG.error(it)
+        }
+      }
     }
   }
 
-  private suspend fun send() {
-    for (status in WebServiceStatusManager.getAllStatuses()) {
-      coroutineContext.ensureActive()
+  private fun updateAndGetUrl(): String? {
+    val response = service<RequestService>().get(statusUrl)
+    if (response == null || !response.isOK()) return null
 
-      runCatching {
-        status.update()
-        if (status.isServerOk()) {
-          withContext(Dispatchers.IO) {
-            service<StatisticSender>().sendStatsData(status.dataServerUrl())
-          }
-        }
-      }.getOrLogException(logger<SenderPreloadingActivity>())
+    val settings = AnalyticsPlatformSettingsDeserializer.deserialize(response.text) ?: return null
+
+    val satisfyingEndpoints = settings.versions.filter { it.satisfies() && it.endpoint != null }
+    if (satisfyingEndpoints.isEmpty()) {
+      LOG.debug("Analytics Platform completion web service status. No satisfying endpoints.")
+      return null
     }
+    if (satisfyingEndpoints.size > 1) {
+      LOG.error("Analytics Platform completion web service status. More than one satisfying endpoints. First one will be used.")
+    }
+    return satisfyingEndpoints.first().endpoint!!
   }
 }
