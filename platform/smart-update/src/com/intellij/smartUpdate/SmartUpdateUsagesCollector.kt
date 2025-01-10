997b4a4272c1f6055a5aed56650188a7bf8bcf0a
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2025-01-10 22:04:58 +0100

    [build] IJPL-173517 Extract Smart Update to a plugin
    Fix statistics metadata
    
    GitOrigin-RevId: 63d11bae77a797999ce4e7a9f76b764142acf87a

--- platform/smart-update/src/com/intellij/smartUpdate/SmartUpdateUsagesCollector.kt
+++ platform/smart-update/src/com/intellij/smartUpdate/SmartUpdateUsagesCollector.kt
@@ -5,7 +5,7 @@ import com.intellij.internal.statistic.eventLog.events.EventFields
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
 
 internal object SmartUpdateUsagesCollector : CounterUsagesCollector() {
-  private val GROUP = EventLogGroup("smart.update", 2)
+  private val GROUP = EventLogGroup("smart.update", 3)
   private val updatedEvent = GROUP.registerEvent("vcs.update", EventFields.DurationMs)
   private val buildEvent = GROUP.registerEvent("build.project", EventFields.DurationMs, EventFields.Boolean("success"))
   private val scheduledEvent = GROUP.registerEvent("scheduled")
