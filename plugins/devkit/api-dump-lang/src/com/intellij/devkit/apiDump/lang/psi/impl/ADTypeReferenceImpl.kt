601704422b2ba7eed9332e7d7ed29aed68601e25
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-01-10 20:35:25 +0100

    allow dollar in type references
    GitOrigin-RevId: 61d6b325f30bb24f8eb7030ed2f4e4816f2cbd04

--- plugins/devkit/api-dump-lang/src/com/intellij/devkit/apiDump/lang/psi/impl/ADTypeReferenceImpl.kt
+++ plugins/devkit/api-dump-lang/src/com/intellij/devkit/apiDump/lang/psi/impl/ADTypeReferenceImpl.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.devkit.apiDump.lang.psi.impl
 
+import com.intellij.devkit.apiDump.lang.psi.ADTypeReference
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.util.UserDataHolderEx
@@ -10,14 +11,10 @@ import com.intellij.psi.impl.source.resolve.reference.impl.providers.JavaClassRe
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.JavaClassReferenceSet
 import com.intellij.psi.tree.IElementType
 
-internal open class ADTypeReferenceImpl(type: IElementType) : ADPsiElementImpl(type) {
+internal abstract class ADTypeReferenceImpl(type: IElementType) : ADPsiElementImpl(type), ADTypeReference {
 
   private val referenceSet: JavaClassReferenceSet
-    get() = (this as UserDataHolderEx).getOrCreateUserData(refKey) {
-      val provider = JavaClassReferenceProvider()
-      val referenceSet = JavaClassReferenceSet(text, this, 0, true, provider)
-      referenceSet
-    }
+    get() = (this as UserDataHolderEx).getOrCreateUserData(refKey) { ReferenceSet(this) }
 
   override fun getReferences(): Array<PsiReference> =
     referenceSet.references
@@ -27,6 +24,10 @@ internal open class ADTypeReferenceImpl(type: IElementType) : ADPsiElementImpl(t
     val range = TextRange(0, this.textLength)
     referenceSet.reparse(this, range)
   }
+}
+
+class ReferenceSet(ref: ADTypeReference) : JavaClassReferenceSet(ref.text, ref, 0, true, JavaClassReferenceProvider()) {
+  override fun isAllowDollarInNames(): Boolean = true
 }
 
 private val refKey = Key.create<JavaClassReferenceSet>("JavaClassReferenceSetKey")
