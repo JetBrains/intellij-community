74cd2c01be1ca3801403b2e221197232c0f15d8e
Bogdan Kirilenko <bogdan.kirilenko@jetbrains.com>
2025-01-13 14:47:56 +0100

    [PyCharm] PY-74106 minor rm unused JComponent.yOffsetFromEditor
    GitOrigin-RevId: 0f4b68f06ef2487e176afc876faeb121fb692348

--- notebooks/visualization/src/com/intellij/notebooks/visualization/ui/NotebookEditorUiUtil.kt
+++ notebooks/visualization/src/com/intellij/notebooks/visualization/ui/NotebookEditorUiUtil.kt
@@ -110,8 +110,3 @@ fun registerEditorSizeWatcher(
 
 val EditorEx.textEditingAreaWidth: Int
   get() = scrollingModel.visibleArea.width - scrollPane.verticalScrollBar.width
-
-fun JComponent.yOffsetFromEditor(editor: Editor): Int? =
-  SwingUtilities.convertPoint(this, 0, 0, editor.contentComponent).y
-    .takeIf { it >= 0 }
-    ?.let { it + insets.top }
