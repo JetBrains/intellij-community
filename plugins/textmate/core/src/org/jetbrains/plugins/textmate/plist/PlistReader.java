48bbc1a45a79b80ff501cd53354f2e60fc94b102
Alexander Zolotov <zolotov@jetbrains.com>
2025-01-13 10:20:21 +0100

    [textmate] deprecate file-based api
    GitOrigin-RevId: 0211af7beaaf00e7c20fdb2a054dc6f409337af3

--- plugins/textmate/core/src/org/jetbrains/plugins/textmate/plist/PlistReader.java
+++ plugins/textmate/core/src/org/jetbrains/plugins/textmate/plist/PlistReader.java
@@ -5,6 +5,10 @@ import org.jetbrains.annotations.NotNull;
 import java.io.*;
 
 public interface PlistReader {
+  /**
+   * @deprecated use {@link #read(InputStream)}
+   */
+  @Deprecated
   default Plist read(@NotNull File file) throws IOException {
     try (InputStream in = new BufferedInputStream(new FileInputStream(file))) {
       return read(in);
