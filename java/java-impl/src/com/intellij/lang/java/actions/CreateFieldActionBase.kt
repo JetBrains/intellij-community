da80ed1498b39a07b919811152dce854b0486e43
Bas Leijdekkers <basleijdekkers@gmail.com>
2025-01-14 22:31:20 +0100

    Java: don't offer to create field from usage when there is a private field of that name in the class (IDEA-117168)
    GitOrigin-RevId: e43a077d32e8acc67313b6ba505fd8a934a06165

--- java/java-impl/src/com/intellij/lang/java/actions/CreateFieldActionBase.kt
+++ java/java-impl/src/com/intellij/lang/java/actions/CreateFieldActionBase.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.lang.java.actions
 
 import com.intellij.codeInsight.daemon.QuickFixBundle.message
@@ -23,6 +23,7 @@ internal abstract class CreateFieldActionBase(
 
   override fun isAvailable(project: Project, file: PsiFile, target: PsiClass): Boolean {
     if (!super.isAvailable(project, file, target)) return false
+    if (target.findFieldByName(request.fieldName, false) != null) return false;
     return isClassBodyValid(target)
   }
 
