fa47ad09c5ecfe0ba0b9881a8281a500e1e21e5b
Ivan Morgillo <imorgillo@gmail.com>
2025-01-14 18:25:26 +0100

    Reallocate styles in appropriate files
    Reference https://github.com/JetBrains/jewel/pull/713
    
    Signed-off-by: Ivan Morgillo <imorgillo@gmail.com>
    
    closes https://github.com/JetBrains/intellij-community/pull/2910
    
    (cherry picked from commit 1deb160659741c2256d49ace9460a515f504384f)
    
    
    (cherry picked from commit b463f469e8a25e762634540e9208129f5ac16e09)
    
    IJ-MR-155570
    
    GitOrigin-RevId: cdd83dd9853b0daaea4be9ce1bb546f3b6b9c9b7

--- platform/jewel/ide-laf-bridge/src/main/kotlin/org/jetbrains/jewel/bridge/theme/IntUiBridgeHorizontalProgressBar.kt
+++ platform/jewel/ide-laf-bridge/src/main/kotlin/org/jetbrains/jewel/bridge/theme/IntUiBridgeHorizontalProgressBar.kt
@@ -1,24 +1,12 @@
 package org.jetbrains.jewel.bridge.theme
 
 import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.takeOrElse
 import androidx.compose.ui.unit.dp
 import kotlin.time.Duration.Companion.milliseconds
 import org.jetbrains.jewel.bridge.retrieveColorOrUnspecified
-import org.jetbrains.jewel.ui.component.styling.CircularProgressStyle
 import org.jetbrains.jewel.ui.component.styling.HorizontalProgressBarColors
 import org.jetbrains.jewel.ui.component.styling.HorizontalProgressBarMetrics
 import org.jetbrains.jewel.ui.component.styling.HorizontalProgressBarStyle
-
-internal fun readCircularProgressStyle(isDark: Boolean) =
-    CircularProgressStyle(
-        frameTime = 125.milliseconds,
-        color =
-            retrieveColorOrUnspecified("ProgressIcon.color").takeOrElse {
-                if (isDark) Color(0xFF6F737A) else Color(0xFFA8ADBD)
-            },
-    )
 
 internal fun readHorizontalProgressBarStyle() =
     HorizontalProgressBarStyle(
