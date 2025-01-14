5d0ca7db1c4beddfab561f2766ccc328a2054b3a
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-01-14 16:16:38 +0100

    Add foldings for api dumps
    GitOrigin-RevId: 06dfcf1e54ac1d456ab28052b3842bc19951592b

--- plugins/devkit/api-dump-lang/src/com/intellij/devkit/apiDump/lang/psi/impl/ADFileImpl.kt
+++ plugins/devkit/api-dump-lang/src/com/intellij/devkit/apiDump/lang/psi/impl/ADFileImpl.kt
@@ -1,12 +1,13 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.devkit.apiDump.lang.psi.impl
 
+import com.intellij.devkit.apiDump.lang.ADFileType
+import com.intellij.devkit.apiDump.lang.ADLanguage
+import com.intellij.devkit.apiDump.lang.psi.ADPsiElement
 import com.intellij.extapi.psi.PsiFileBase
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.psi.FileViewProvider
-import com.intellij.devkit.apiDump.lang.ADFileType
-import com.intellij.devkit.apiDump.lang.ADLanguage
 
-internal class ADFileImpl(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, ADLanguage) {
+internal class ADFileImpl(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, ADLanguage), ADPsiElement {
   override fun getFileType(): FileType = ADFileType.INSTANCE
 }
