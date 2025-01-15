415bf7bba4709cb706ed8267666711488d69cd50
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2025-01-15 11:45:27 +0100

    Enable IncorrectCancellationExceptionHandlingInspection by default
    GitOrigin-RevId: f7f25e936f5300d3bf3f3cf656b0c9efdeb81428

--- plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/inspections/DevkitInspectionsRegistrationCheckTest.java
+++ plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/inspections/DevkitInspectionsRegistrationCheckTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.inspections;
 
 import com.intellij.codeInspection.LocalInspectionEP;
@@ -30,7 +30,6 @@ public class DevkitInspectionsRegistrationCheckTest extends BasePlatformTestCase
               "CancellationCheckInLoops",
               "ThreadingConcurrency",
               "CallingMethodShouldBeRequiresBlockingContext",
-              "IncorrectCancellationExceptionHandling",
               "PotentialDeadlockInServiceInitialization"
     ).sorted().toList();
 
