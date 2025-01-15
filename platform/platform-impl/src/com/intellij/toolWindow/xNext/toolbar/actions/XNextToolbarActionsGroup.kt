2c824fd35a72323a44e7030a155efc1b60ca4c87
Anna.Gromova <anna.gromova@jetbrains.com>
2025-01-15 12:23:57 +0100

    Refactor(Module Extraction) remove XNextHideAllToolWindowsAction
    GitOrigin-RevId: 435f0f2ee18ab86b69096163258e6c33b7d58961

--- platform/platform-impl/src/com/intellij/toolWindow/xNext/toolbar/actions/XNextToolbarActionsGroup.kt
+++ platform/platform-impl/src/com/intellij/toolWindow/xNext/toolbar/actions/XNextToolbarActionsGroup.kt
@@ -1,17 +1,24 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.toolWindow.xNext.toolbar.actions
 
-import com.intellij.ide.actions.XNextHideAllToolWindowsAction
-import com.intellij.openapi.actionSystem.ActionGroup
-import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.project.DumbAware
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
 internal class XNextToolbarActionsGroup : ActionGroup(), DumbAware {
-  private val arrayOf = arrayOf<AnAction>(ActionManager.getInstance().getAction("SearchEverywhere"), ActionManager.getInstance().getAction("MiniAiChat.Toggle"), XNextHideAllToolWindowsAction())
+  private val arrayOf = arrayListOf<AnAction>(ActionManager.getInstance().getAction("SearchEverywhere"), ActionManager.getInstance().getAction("MiniAiChat.Toggle"))
 
-  override fun getChildren(e: AnActionEvent?): Array<out AnAction?> = arrayOf
+  init {
+    ActionManager.getInstance().getAction("HideAllWindows")?.let {
+      arrayOf.add(object : AnActionWrapper(it) {
+        init {
+          templatePresentation.icon = AllIcons.General.FitContent
+        }
+      })
+    }
+  }
+
+  override fun getChildren(e: AnActionEvent?): Array<out AnAction?> = arrayOf.toTypedArray()
 }
