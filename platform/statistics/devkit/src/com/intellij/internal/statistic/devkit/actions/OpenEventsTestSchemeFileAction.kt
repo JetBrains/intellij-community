37299f4d195dd472b6b65ac212c3538817eb5144
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2025-01-15 11:30:25 +0100

    [platform] IJPL-173084 Cleanup texts in ActionsBundle.properties
    GitOrigin-RevId: 79d39527664e31d49e01f583252f636171bafd10

--- platform/statistics/devkit/src/com/intellij/internal/statistic/devkit/actions/OpenEventsTestSchemeFileAction.kt
+++ platform/statistics/devkit/src/com/intellij/internal/statistic/devkit/actions/OpenEventsTestSchemeFileAction.kt
@@ -1,21 +1,16 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.internal.statistic.devkit.actions
 
-import com.intellij.icons.AllIcons
-import com.intellij.idea.ActionsBundle
-import com.intellij.internal.statistic.StatisticsBundle
 import com.intellij.internal.statistic.devkit.StatisticsDevKitUtil
-import com.intellij.internal.statistic.devkit.actions.OpenEventsSchemeFileAction.Companion.openFileInEditor
+import com.intellij.internal.statistic.devkit.actions.OpenEventsSchemeFileAction.Manager.openFileInEditor
 import com.intellij.internal.statistic.eventLog.validator.storage.persistence.EventLogTestMetadataPersistence
 import com.intellij.internal.statistic.utils.StatisticsRecorderUtil
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 
-private class OpenEventsTestSchemeFileAction(private val myRecorderId: String = StatisticsDevKitUtil.DEFAULT_RECORDER)
-  : DumbAwareAction(StatisticsBundle.message("stats.open.0.test.scheme.file", myRecorderId),
-                    ActionsBundle.message("group.OpenEventsTestSchemeFileAction.description"),
-                    AllIcons.FileTypes.Any_type) {
+private class OpenEventsTestSchemeFileAction() : DumbAwareAction() {
+  private val myRecorderId: String = StatisticsDevKitUtil.DEFAULT_RECORDER
 
   override fun getActionUpdateThread() = ActionUpdateThread.BGT
 
