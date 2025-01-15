573eb82cc27b296cd379667e36c693ca2e85f36a
Ilia Brovarnik <ilia.brovarnik@jetbrains.com>
2025-01-15 21:16:01 +0100

    [fleet] FL-31311 Add quick fixes and gutter icons
    GitOrigin-RevId: ee42b20744237f687be18fef12f1a0223732db83

--- plugins/devkit/intellij.fleet.devkit/src/DevKitFleetBundle.kt
+++ plugins/devkit/intellij.fleet.devkit/src/DevKitFleetBundle.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.idea.devkit.fleet
 
 import com.intellij.DynamicBundle
+import com.intellij.codeInspection.util.IntentionName
 import org.jetbrains.annotations.Nls
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
@@ -11,13 +12,14 @@ private const val BUNDLE_FQN: @NonNls String = "messages.DevKitFleetBundle"
 object DevKitFleetBundle {
   private val BUNDLE = DynamicBundle(DevKitFleetBundle::class.java, BUNDLE_FQN)
 
+  @Nls
   fun message(
     @NonNls @PropertyKey(resourceBundle = BUNDLE_FQN) key: String,
     vararg params: Any,
-  ): @Nls String = BUNDLE.getMessage(key, *params)
+  ): String = BUNDLE.getMessage(key, *params)
 
   fun messagePointer(
     @NonNls @PropertyKey(resourceBundle = BUNDLE_FQN) key: String,
     vararg params: Any,
-  ): Supplier<@Nls String> = BUNDLE.getLazyMessage(key, *params)
+  ): Supplier<String> = BUNDLE.getLazyMessage(key, *params)
 }
--- plugins/devkit/intellij.fleet.devkit/src/inspections/SettingRegistrationDocumentationInspection.kt
+++ plugins/devkit/intellij.fleet.devkit/src/inspections/SettingRegistrationDocumentationInspection.kt
@@ -1,10 +1,17 @@
 package org.jetbrains.idea.devkit.fleet.inspections
 
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.modcommand.ModPsiUpdater
+import com.intellij.ide.projectView.actions.MarkRootsManager
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ContentEntry
 import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.findOrCreateDirectory
 import com.intellij.openapi.vfs.readText
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
@@ -13,13 +20,14 @@ import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
 import com.intellij.psi.util.parentsOfType
+import org.jetbrains.idea.devkit.fleet.DevKitFleetBundle
 import org.jetbrains.jps.model.java.JavaModuleSourceRootTypes
+import org.jetbrains.jps.model.java.JavaResourceRootType
 import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
 import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.resolution.*
 import org.jetbrains.kotlin.analysis.api.symbols.KaPropertySymbol
-import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinModCommandQuickFix
 import org.jetbrains.kotlin.idea.codeinsight.api.classic.inspections.AbstractKotlinInspection
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.*
@@ -31,146 +39,193 @@ internal class SettingRegistrationDocumentationInspection : AbstractKotlinInspec
   override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor =
     object : KtVisitorVoid() {
       override fun visitCallExpression(expression: KtCallExpression) {
-        val error = analyzeCallExpression(expression)
-        if (error != null) {
-          holder.registerProblem(expression.getCallNameExpression(), error)
+        analyzeCallExpression(expression)?.let {
+          if (it.message != null) {
+            val anchor = expression.getCallNameExpression() ?: expression
+            if(it.fix != null) {
+              holder.registerProblem(anchor, it.message, it.fix)
+            } else {
+              holder.registerProblem(anchor, it.message)
+            }
+          }
         }
       }
     }
+}
 
-  private fun analyzeCallExpression(element: KtCallExpression): String? {
-    analyze(element) {
-      val call = element.resolveToCall()?.successfulFunctionCallOrNull() ?: return null
-      if (call.isSettingRegistrationCall()) {
-        return visitSettingRegistrationCall(call, element)
+internal fun analyzeCallExpression(element: KtCallExpression): DocumentationSearchResult? {
+  analyze(element) {
+    val call = element.resolveToCall()?.successfulFunctionCallOrNull()
+    call?.let {
+      if (it.isSettingRegistrationCall()) {
+        return visitSettingRegistrationCall(it, element)
       }
-      if (call.isRunTaskRegistrationCall()) {
-        return visitRunTaskRegistrationCall(call, element)
+      if (it.isRunTaskRegistrationCall()) {
+        return visitRunTaskRegistrationCall(it, element)
       }
-      if (call.symbol.callableId?.asSingleFqName() != FqName("fleet.common.settings.SettingsKey")) return null
-      val property = element.parent as? KtProperty ?: return null
-      val search = ReferencesSearch.search(property, GlobalSearchScope.projectScope(property.project))
-      return search.mapNotNull {
-        val callExpr = it.element.parentsOfType<KtCallExpression>().firstOrNull() ?: return@mapNotNull null
-        val kaCall = callExpr.resolveToCall()?.successfulFunctionCallOrNull() ?: return@mapNotNull null
-        if (kaCall.isSettingRegistrationCall()) {
-          visitSettingRegistrationCall(kaCall, callExpr)
-        }
-        else null
-      }.firstOrNull()
     }
-  }
-
-  private fun KaFunctionCall<*>.isSettingRegistrationCall(): Boolean {
-    return symbol.callableId?.asSingleFqName() == FqName("fleet.frontend.settings.SettingsRegistrar.setting")
-  }
-
-  private fun KaFunctionCall<*>.isRunTaskRegistrationCall(): Boolean {
-    return symbol.callableId?.asSingleFqName() == FqName("fleet.frontend.run.api.RunTaskRegistrar.task")
-  }
-
-  context(KaSession)
-  @OptIn(KaExperimentalApi::class)
-  private fun visitSettingRegistrationCall(call: KaFunctionCall<*>, element: KtCallExpression): String? {
-    val argument = call.argumentMapping.firstNotNullOfOrNull { (expr, par) ->
-      if (par.name.toString() == "setting") expr
-      else null
-    } ?: return null
-    val variable = argument.resolveToCall()?.successfulVariableAccessCall()?.symbol ?: return null
-    val settingsKeyCall = (variable as? KaPropertySymbol)?.initializer?.initializerPsi?.resolveToCall()?.successfulFunctionCallOrNull()
-                          ?: return null
-    val keyArgument = settingsKeyCall.argumentMapping.firstNotNullOfOrNull { (expr, par) ->
-      if (par.name.toString() == "key") expr
-      else null
-    } ?: return null
-    val key = keyArgument.getStringValue() ?: return null
-    val documentationStatus = getDocumentation(element)
-    return if (documentationStatus is DocumentationStatus.Exists) {
-      if (key !in documentationStatus.documentedSettings) {
-        "Setting isn't documented"
+    val fqName = call?.symbol?.callableId?.asSingleFqName()
+                 ?: element.resolveToCall()?.successfulConstructorCallOrNull()?.symbol?.containingClassId?.asSingleFqName()
+                 ?: return null
+    if (fqName != FqName("fleet.common.settings.SettingsKey")) return null
+    val property = element.parent as? KtProperty ?: return null
+    val search = ReferencesSearch.search(property, GlobalSearchScope.projectScope(property.project))
+    return search.mapNotNull {
+      val callExpr = it.element.parentsOfType<KtCallExpression>().firstOrNull() ?: return@mapNotNull null
+      val kaCall = callExpr.resolveToCall()?.successfulFunctionCallOrNull() ?: return@mapNotNull null
+      if (kaCall.isSettingRegistrationCall()) {
+        visitSettingRegistrationCall(kaCall, callExpr)
       }
       else null
-    }
-    else {
-      "no documentation found"
-    }
-  }
-
-  context(KaSession)
-  @OptIn(KaExperimentalApi::class)
-  private fun visitRunTaskRegistrationCall(call: KaFunctionCall<*>, element: KtCallExpression): String? {
-    val argument = call.argumentMapping.firstNotNullOfOrNull { (expr, par) ->
-      if (par.name.toString() == "taskType") expr
-      else null
-    } ?: return null
-    val variable = argument.resolveToCall()?.successfulVariableAccessCall()?.symbol ?: return null
-    val taskTypeCall = (variable as? KaPropertySymbol)?.initializer?.initializerPsi?.resolveToCall()?.successfulFunctionCallOrNull()
-                          ?: return null
-    val idArgument = taskTypeCall.argumentMapping.firstNotNullOfOrNull { (expr, par) ->
-      if (par.name.toString() == "id") expr
-      else null
-    } ?: return null
-    val id = idArgument.getStringValue() ?: return null
-    val documentationStatus = getDocumentation(element)
-    return if (documentationStatus is DocumentationStatus.Exists) {
-      if (id !in documentationStatus.documentedRunConfigs) {
-        "Run Task isn't documented"
-      }
-      else null
-    }
-    else {
-      "no documentation found"
-    }
-  }
-
-  context(KaSession)
-  @OptIn(KaExperimentalApi::class)
-  private fun KtExpression.getStringValue(): String? {
-    (this as? KtStringTemplateExpression)?.plainContent?.let { return it }
-    val variable = resolveToCall()?.successfulVariableAccessCall()?.symbol ?: return null
-    return (variable as? KaPropertySymbol)?.initializer?.initializerPsi?.getStringValue()
-  }
-
-  sealed interface DocumentationStatus {
-    data class Exists(val documentedSettings: Set<String>, val documentedRunConfigs: Set<String>) : DocumentationStatus
-    data object Missing : DocumentationStatus
-  }
-
-  private fun getDocumentation(element: PsiElement): DocumentationStatus? {
-    val module = ModuleUtilCore.findModuleForPsiElement(element) ?: return null
-    val moduleRootManager = ModuleRootManager.getInstance(module)
-    val resources = moduleRootManager.getSourceRoots(JavaModuleSourceRootTypes.RESOURCES)
-    val file = resources.firstOrNull { it.name == "resources" }?.children?.firstOrNull { it.name == "documentation.yaml" } ?: return DocumentationStatus.Missing
-    val documentation = CachedValuesManager.getManager(module.project).getCachedValue(module) {
-      val res = file?.let {
-        val configText = it.readText()
-        val config = Yaml().load<Any>(configText)
-        config?.let {
-          val settings = (config as? Map<*, *>)?.get("settings")?.let {
-            (it as? Map<String, *>)?.keys
-          } ?: emptySet()
-          val runConfigs = (config as? Map<*, *>)?.get("runConfigurations")?.let {
-            (it as? Map<String, *>)?.keys
-          } ?: emptySet()
-          DocumentationStatus.Exists(settings, runConfigs)
-        }
-      } ?: DocumentationStatus.Missing
-      CachedValueProvider.Result.create(res, file)
-    }
-    return documentation
-  }
-
-  internal class TodoFix() : KotlinModCommandQuickFix<KtCallExpression>() {
-    override fun getName(): String = "TODO"
-
-    override fun getFamilyName(): String = name
-
-    override fun applyFix(
-      project: Project,
-      element: KtCallExpression,
-      updater: ModPsiUpdater,
-    ) {
-      element.delete()
-    }
+    }.firstOrNull()
   }
 }
+
+private fun KaFunctionCall<*>.isSettingRegistrationCall(): Boolean {
+  return symbol.callableId?.asSingleFqName() == FqName("fleet.frontend.settings.SettingsRegistrar.setting")
+}
+
+private fun KaFunctionCall<*>.isRunTaskRegistrationCall(): Boolean {
+  return symbol.callableId?.asSingleFqName() == FqName("fleet.frontend.run.api.RunTaskRegistrar.task")
+}
+
+context(KaSession)
+@OptIn(KaExperimentalApi::class)
+private fun visitSettingRegistrationCall(call: KaFunctionCall<*>, element: KtCallExpression): DocumentationSearchResult? {
+  val argument = call.argumentMapping.firstNotNullOfOrNull { (expr, par) ->
+    if (par.name.toString() == "setting") expr
+    else null
+  } ?: return null
+  val variable = argument.resolveToCall()?.successfulVariableAccessCall()?.symbol ?: return null
+  val settingsKeyCall = (variable as? KaPropertySymbol)?.initializer?.initializerPsi?.resolveToCall()?.successfulFunctionCallOrNull()
+                        ?: return null
+  val keyArgument = settingsKeyCall.argumentMapping.firstNotNullOfOrNull { (expr, par) ->
+    if (par.name.toString() == "key") expr
+    else null
+  } ?: return null
+  val key = keyArgument.getStringValue() ?: return null
+  val documentationStatus = getDocumentation(element)
+  val module = ModuleUtilCore.findModuleForPsiElement(element) ?: return null
+  return if (documentationStatus is DocumentationStatus.Exists) {
+    if (key !in documentationStatus.documentedSettings) {
+      DocumentationSearchResult("Setting \"$key\" isn't documented", NavigateToDocumentationFix(module), documentationStatus.file)
+    }
+    else DocumentationSearchResult(file = documentationStatus.file)
+  }
+  else {
+    DocumentationSearchResult("documentation.yaml not found", CreateDocumentationFileFix(module))
+  }
+}
+
+context(KaSession)
+@OptIn(KaExperimentalApi::class)
+private fun visitRunTaskRegistrationCall(call: KaFunctionCall<*>, element: KtCallExpression): DocumentationSearchResult? {
+  val argument = call.argumentMapping.firstNotNullOfOrNull { (expr, par) ->
+    if (par.name.toString() == "taskType") expr
+    else null
+  } ?: return null
+  val variable = argument.resolveToCall()?.successfulVariableAccessCall()?.symbol ?: return null
+  val taskTypeCall = (variable as? KaPropertySymbol)?.initializer?.initializerPsi?.resolveToCall()?.successfulFunctionCallOrNull()
+                     ?: return null
+  val idArgument = taskTypeCall.argumentMapping.firstNotNullOfOrNull { (expr, par) ->
+    if (par.name.toString() == "id") expr
+    else null
+  } ?: return null
+  val id = idArgument.getStringValue() ?: return null
+  val documentationStatus = getDocumentation(element)
+  val module = ModuleUtilCore.findModuleForPsiElement(element) ?: return null
+  return if (documentationStatus is DocumentationStatus.Exists) {
+    if (id !in documentationStatus.documentedRunConfigs) {
+      DocumentationSearchResult("Run Configuration \"$id\" isn't documented", NavigateToDocumentationFix(module), documentationStatus.file)
+    }
+    else DocumentationSearchResult(file = documentationStatus.file)
+  }
+  else {
+    DocumentationSearchResult("documentation.yaml not found", CreateDocumentationFileFix(module))
+  }
+}
+
+context(KaSession)
+@OptIn(KaExperimentalApi::class)
+private fun KtExpression.getStringValue(): String? {
+  (this as? KtStringTemplateExpression)?.plainContent?.let { return it }
+  val variable = resolveToCall()?.successfulVariableAccessCall()?.symbol ?: return null
+  return (variable as? KaPropertySymbol)?.initializer?.initializerPsi?.getStringValue()
+}
+
+sealed interface DocumentationStatus {
+  data class Exists(val file: VirtualFile, val documentedSettings: Set<String>, val documentedRunConfigs: Set<String>) : DocumentationStatus
+  data object Missing : DocumentationStatus
+}
+
+private fun getDocumentation(element: PsiElement): DocumentationStatus? {
+  val module = ModuleUtilCore.findModuleForPsiElement(element) ?: return null
+  val moduleRootManager = ModuleRootManager.getInstance(module)
+  val resources = moduleRootManager.getSourceRoots(JavaModuleSourceRootTypes.RESOURCES)
+  val file = resources.firstOrNull { it.name == "resources" }?.children?.firstOrNull { it.name == "documentation.yaml" }
+             ?: return DocumentationStatus.Missing
+  val documentation = CachedValuesManager.getManager(module.project).getCachedValue(module) {
+    val res = file.let {
+      val configText = it.readText()
+      val config = Yaml().load<Any>(configText)
+      config?.let {
+        val settings = (config as? Map<*, *>)?.get("settings")?.let {
+          (it as? Map<String, *>)?.keys
+        } ?: emptySet()
+        val runConfigs = (config as? Map<*, *>)?.get("runConfigurations")?.let {
+          (it as? Map<String, *>)?.keys
+        } ?: emptySet()
+        DocumentationStatus.Exists(file, settings, runConfigs)
+      }
+    } ?: DocumentationStatus.Missing
+    CachedValueProvider.Result.create(res, file)
+  }
+  return documentation
+}
+
+internal data class DocumentationSearchResult(val message: String? = null, val fix: LocalQuickFix? = null, val file: VirtualFile? = null)
+
+private class CreateDocumentationFileFix(val module: Module) : LocalQuickFix {
+  override fun getName() = DevKitFleetBundle.message("intention.name.create.documentation.yaml.file")
+  override fun getFamilyName() = DevKitFleetBundle.message("intention.family.name.create.documentation")
+
+  override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+    val moduleRootManager = ModuleRootManager.getInstance(module)
+    val resourcesRoot =
+      moduleRootManager.getSourceRoots(JavaModuleSourceRootTypes.RESOURCES).firstOrNull()
+      ?: let {
+        val dir = moduleRootManager.contentRoots.firstOrNull()?.findOrCreateDirectory("resources") ?: return
+        MarkRootsManager.modifyRoots(module, arrayOf(dir)) { file: VirtualFile, entry: ContentEntry ->
+          entry.addSourceFolder(file, JavaResourceRootType.RESOURCE)
+        }
+        dir
+      }
+    val virtualFile = resourcesRoot.createChildData(this, "documentation.yaml")
+    virtualFile.setBinaryContent("""
+      settings:
+        # Add your settings here
+      runConfigurations:
+        # Add your run configurations here
+    """.trimIndent().toByteArray())
+
+    val fileEditorManager = FileEditorManager.getInstance(project)
+    fileEditorManager.openFile(virtualFile, true)
+  }
+}
+
+private class NavigateToDocumentationFix(val module: Module) : LocalQuickFix {
+  override fun getName() = DevKitFleetBundle.message("intention.name.navigate.to.documentation.yaml")
+  override fun getFamilyName() = DevKitFleetBundle.message("intention.family.name.navigate.to.documentation")
+
+  override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+    val moduleRootManager = ModuleRootManager.getInstance(module)
+    val virtualFile = moduleRootManager.getSourceRoots(JavaModuleSourceRootTypes.RESOURCES)
+                        .flatMap { it.children.toList() }
+                        .firstOrNull { it.name == "documentation.yaml" }
+                      ?: return
+
+    val fileEditorManager = FileEditorManager.getInstance(project)
+    fileEditorManager.openFile(virtualFile, true)
+  }
+}
+
