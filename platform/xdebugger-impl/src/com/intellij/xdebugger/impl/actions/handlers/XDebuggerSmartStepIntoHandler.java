dab23d1f917d8003f34198a83ea84ba956dc9e5d
Vyacheslav Moklev <vyacheslav.moklev@jetbrains.com>
2025-01-15 23:51:03 +0900

    [RDCT] IJPL-175187: Fix Smart Step Into availability hints
    This commit reverts some changes from `5c6005b604542ebf9c04be576c32d812e5b5b950`. Action availability hints must be added from the inside of `addRangeHighlighterAndChangeAttributes` lambda, otherwise they might not work.
    
    GitOrigin-RevId: ef1326eb73a44e2c45da49240c733edff0b061a2

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/handlers/XDebuggerSmartStepIntoHandler.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/handlers/XDebuggerSmartStepIntoHandler.java
@@ -241,26 +241,16 @@ public class XDebuggerSmartStepIntoHandler extends XDebuggerSuspendedActionHandl
                                                                                       editor.getDocument().getTextLength(),
                                                                                       HighlighterLayer.LAST,
                                                                                       HighlighterTargetArea.EXACT_RANGE, false, h -> {
+          // this hints should be added in this lambda in order to be serialized by RD markup machinery
+          EditorActionAvailabilityHintKt.addActionAvailabilityHint(h,
+                                                                   new EditorActionAvailabilityHint(IdeActions.ACTION_EDITOR_ENTER, EditorActionAvailabilityHint.AvailabilityCondition.CaretInside),
+                                                                   new EditorActionAvailabilityHint(IdeActions.ACTION_EDITOR_TAB, EditorActionAvailabilityHint.AvailabilityCondition.CaretInside),
+                                                                   new EditorActionAvailabilityHint(IdeActions.ACTION_EDITOR_ESCAPE, EditorActionAvailabilityHint.AvailabilityCondition.CaretInside),
+                                                                   new EditorActionAvailabilityHint(IdeActions.ACTION_EDITOR_MOVE_CARET_UP, EditorActionAvailabilityHint.AvailabilityCondition.CaretInside),
+                                                                   new EditorActionAvailabilityHint(IdeActions.ACTION_EDITOR_MOVE_CARET_DOWN, EditorActionAvailabilityHint.AvailabilityCondition.CaretInside),
+                                                                   new EditorActionAvailabilityHint(IdeActions.ACTION_EDITOR_MOVE_CARET_RIGHT, EditorActionAvailabilityHint.AvailabilityCondition.CaretInside),
+                                                                   new EditorActionAvailabilityHint(IdeActions.ACTION_EDITOR_MOVE_CARET_LEFT, EditorActionAvailabilityHint.AvailabilityCondition.CaretInside));
         });
-    // this hints should be added in this lambda in order to be serialized by RD markup machinery
-    EditorActionAvailabilityHintKt.addActionAvailabilityHint(highlighter,
-                                                             new EditorActionAvailabilityHint(IdeActions.ACTION_EDITOR_ENTER,
-                                                                                              EditorActionAvailabilityHint.AvailabilityCondition.CaretInside),
-                                                             new EditorActionAvailabilityHint(IdeActions.ACTION_EDITOR_TAB,
-                                                                                              EditorActionAvailabilityHint.AvailabilityCondition.CaretInside),
-                                                             new EditorActionAvailabilityHint(IdeActions.ACTION_EDITOR_ESCAPE,
-                                                                                              EditorActionAvailabilityHint.AvailabilityCondition.CaretInside),
-                                                             new EditorActionAvailabilityHint(IdeActions.ACTION_EDITOR_MOVE_CARET_UP,
-                                                                                              EditorActionAvailabilityHint.AvailabilityCondition.CaretInside),
-                                                             new EditorActionAvailabilityHint(
-                                                               IdeActions.ACTION_EDITOR_MOVE_CARET_DOWN,
-                                                               EditorActionAvailabilityHint.AvailabilityCondition.CaretInside),
-                                                             new EditorActionAvailabilityHint(
-                                                               IdeActions.ACTION_EDITOR_MOVE_CARET_RIGHT,
-                                                               EditorActionAvailabilityHint.AvailabilityCondition.CaretInside),
-                                                             new EditorActionAvailabilityHint(
-                                                               IdeActions.ACTION_EDITOR_MOVE_CARET_LEFT,
-                                                               EditorActionAvailabilityHint.AvailabilityCondition.CaretInside));
     data.myActionHintSyntheticHighlighter = highlighter;
 
     session.updateExecutionPosition();
