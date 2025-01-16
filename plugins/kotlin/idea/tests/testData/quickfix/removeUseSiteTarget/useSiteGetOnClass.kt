f7d9e4880ee6f0b26969e25d3cc6f4831291a0fb
Roman Golyshev <roman.golyshev@jetbrains.com>
2025-01-16 21:37:58 +0100

    KTIJ-32748 [kotlin] Strip ACTION directives from `useSiteGetOnClass.kt` testdata
    The set of available actions is different here between K1/K2,
    and this information is not very important.
    
    GitOrigin-RevId: 9966a26e94819872488e371fdcc7a0f4adb1228f

--- plugins/kotlin/idea/tests/testData/quickfix/removeUseSiteTarget/useSiteGetOnClass.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/removeUseSiteTarget/useSiteGetOnClass.kt
@@ -1,7 +1,4 @@
 // "Remove 'get:' to make the annotation effective. (changes semantics. See: https://youtrack.jetbrains.com/issue/KT-48141)" "false"
-// ACTION: Create test
-// ACTION: Make internal
-// ACTION: Make private
 // WITH_STDLIB
 // ERROR: '@get:' annotations could be applied only to property declarations
 // ERROR: This annotation is not applicable to target 'class' and use site target '@get'
