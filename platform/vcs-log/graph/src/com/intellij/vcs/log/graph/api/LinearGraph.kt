df80432e211f8ff24f23e8673644710f0ebaa1ff
Anna Rakhmukova <anna.rakhmukova@jetbrains.com>
2025-01-16 13:36:14 +0100

    [vcs-log] Convert LinearGraph to kotlin
    GitOrigin-RevId: d3ee518dbedfe62aefcb41550d3687f577bf1347

--- platform/vcs-log/graph/src/com/intellij/vcs/log/graph/api/LinearGraph.kt
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/graph/api/LinearGraph.kt
@@ -1,39 +1,18 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.log.graph.api;
 
-import com.intellij.vcs.log.graph.api.elements.GraphEdge;
-import com.intellij.vcs.log.graph.api.elements.GraphNode;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.vcs.log.graph.api.elements.GraphEdge
+import com.intellij.vcs.log.graph.api.elements.GraphNode
 
-import java.util.List;
+interface LinearGraph {
+  fun nodesCount(): Int
 
-public interface LinearGraph {
-  int nodesCount();
+  fun getAdjacentEdges(nodeIndex: Int, filter: EdgeFilter): List<GraphEdge>
 
-  @NotNull
-  List<GraphEdge> getAdjacentEdges(int nodeIndex, @NotNull EdgeFilter filter);
+  fun getGraphNode(nodeIndex: Int): GraphNode
 
-  @NotNull
-  GraphNode getGraphNode(int nodeIndex);
-
-  int getNodeId(int nodeIndex);
+  fun getNodeId(nodeIndex: Int): Int
 
   // return null, if node doesn't exist
-  @Nullable
-  Integer getNodeIndex(int nodeId);
+  fun getNodeIndex(nodeId: Int): Int?
 }
