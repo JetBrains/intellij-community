25d1303fdc4d039a323f362f8dda0935a47ef1ec
Aleksandr Sorotskii <aleksandr.sorotskii@jetbrains.com>
2025-01-20 18:11:34 +0100

    fixup
    GitOrigin-RevId: 473257ec0830ee58c32e4f5ac07a4ba5c9509fd9

--- python/src/com/jetbrains/python/sdk/add/v2/EnvironmentCreatorPoetry.kt
+++ python/src/com/jetbrains/python/sdk/add/v2/EnvironmentCreatorPoetry.kt
@@ -27,6 +27,7 @@ import com.jetbrains.python.sdk.poetry.poetryToml
 import com.jetbrains.python.sdk.poetry.pyProjectToml
 import com.jetbrains.python.sdk.poetry.setupPoetrySdkUnderProgress
 import com.jetbrains.python.statistics.InterpreterType
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.withContext
