d19fdb0bcb8b1b0ee7f9dc6ecc52d93c349bf022
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2025-01-20 14:06:16 +0100

    [platform] restoring the routing FS security manager hack to keep the project working in 243, take 2
    GitOrigin-RevId: 72ca2c1c756ba56a4e280fbb95e425c47c0d925f

--- platform/core-nio-fs/src/com/intellij/platform/core/nio/fs/CoreBootstrapSecurityManager.java
+++ platform/core-nio-fs/src/com/intellij/platform/core/nio/fs/CoreBootstrapSecurityManager.java
@@ -2,11 +2,19 @@
 package com.intellij.platform.core.nio.fs;
 
 import java.security.Permission;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /** @deprecated <not needed anymore; kept for compatibility with pre-251 DevKit versions */
 @Deprecated(forRemoval = true)
 @SuppressWarnings("removal")
 public class CoreBootstrapSecurityManager extends SecurityManager {
+  private final AtomicBoolean initialised = new AtomicBoolean(false);
+
   @Override
-  public void checkPermission(Permission permission) { }
+  public void checkPermission(Permission permission) {
+    if (initialised.compareAndSet(false, true)) {
+      System.clearProperty("java.security.manager");
+      System.setSecurityManager(null);
+    }
+  }
 }
