98bb46d8ea00f0ba6a9dcf8eb923e9daf12fe64b
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2025-01-21 17:13:13 +0100

    IJPL-50006 Registry key to disable scroll-as-you-type in editor search
    GitOrigin-RevId: 436bf20162da0ded664d12ddeb5516d340e970cf

--- platform/lang-impl/src/com/intellij/find/impl/livePreview/SearchResults.java
+++ platform/lang-impl/src/com/intellij/find/impl/livePreview/SearchResults.java
@@ -23,6 +23,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.ActionCallback;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.ArrayUtil;
@@ -455,7 +456,12 @@ public class SearchResults implements DocumentListener, CaretListener {
     notifyChanged();
     if (myCursor == null || !myCursor.equals(oldCursorRange)) {
       if (toChangeSelection) {
-        updateSelection(true, true, true);
+        if (Registry.is("ide.find.auto.scroll.to.next.occurrence")) {
+          updateSelection(true, true, true);
+        }
+        else {
+          updateSelection(false, false, false);
+        }
       }
       notifyCursorMoved();
     }
