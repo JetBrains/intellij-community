70167829bb04240ffdab87d514365a4c48eef38a
Alexander Zolotov <zolotov@jetbrains.com>
2025-01-21 10:40:12 +0100

    [textmate] do not depend on joni in SyntaxMatchUtilsTest
    GitOrigin-RevId: 792479b4ef158525af67ef819d5e1169886e9a2a

--- plugins/textmate/core/tests/org/jetbrains/plugins/textmate/language/syntax/lexer/SyntaxMatchUtilsTest.kt
+++ plugins/textmate/core/tests/org/jetbrains/plugins/textmate/language/syntax/lexer/SyntaxMatchUtilsTest.kt
@@ -3,9 +3,7 @@ package org.jetbrains.plugins.textmate.language.syntax.lexer
 import org.jetbrains.plugins.textmate.language.syntax.lexer.SyntaxMatchUtils.replaceGroupsWithMatchDataInCaptures
 import org.jetbrains.plugins.textmate.language.syntax.lexer.SyntaxMatchUtils.replaceGroupsWithMatchDataInRegex
 import org.jetbrains.plugins.textmate.regex.MatchData
-import org.jetbrains.plugins.textmate.regex.RegexFacade
 import org.jetbrains.plugins.textmate.regex.TextMateString.Companion.fromString
-import org.jetbrains.plugins.textmate.regex.joni.JoniRegexFactory
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -13,41 +11,35 @@ class SyntaxMatchUtilsTest {
   @Test
   fun testReplaceGroupWithMatchData() {
     val string = fromString("first-second")
-    val data: MatchData = regex("([A-z]+)-([A-z]+)").match(string, null)
+    val data = MatchData(matched = true, offsets = intArrayOf(0, 12, 0, 5, 6, 12))
     assertEquals("first+second+first", replaceGroupsWithMatchDataInRegex("\\1+\\2+\\1", string, data))
   }
 
   @Test
   fun testReplaceGroupWithMatchDataInCaptures() {
     val string = fromString("first-second")
-    val data: MatchData = regex("([A-z]+)-([A-z]+)").match(string, null)
+    val data = MatchData(matched = true, offsets = intArrayOf(0, 12, 0, 5, 6, 12))
     assertEquals("first+second+first", replaceGroupsWithMatchDataInCaptures("$1+$2+$1", string, data))
   }
 
   @Test
   fun testReplaceWithUppercase() {
     val string = fromString("first-second")
-    val data: MatchData = regex("([A-z]+)-([A-z]+)").match(string, null)
+    val data = MatchData(matched = true, offsets = intArrayOf(0, 12, 0, 5, 6, 12))
     assertEquals("FIRST+second+first", replaceGroupsWithMatchDataInCaptures("\${1:/upcase}+$2+$1", string, data))
   }
 
   @Test
   fun testReplaceWithDowncase() {
     val string = fromString("FIRST-second")
-    val data: MatchData = regex("([A-z]+)-([A-z]+)").match(string, null)
+    val data = MatchData(matched = true, offsets = intArrayOf(0, 12, 0, 5, 6, 12))
     assertEquals("first+second+FIRST", replaceGroupsWithMatchDataInCaptures("\${1:/downcase}+$2+$1", string, data))
   }
 
   @Test
   fun testReplaceWithDollarSign() {
     val string = fromString("first-$")
-    val data: MatchData = regex("([A-z]+)-([A-z$]+)").match(string, null)
+    val data = MatchData(matched = true, offsets = intArrayOf(0, 7, 0, 5, 6, 7))
     assertEquals("first+\\$+first", replaceGroupsWithMatchDataInRegex("\\1+\\2+\\1", string, data))
-  }
-
-  companion object {
-    private fun regex(pattern: String): RegexFacade {
-      return JoniRegexFactory().regex(pattern)
-    }
   }
 }
