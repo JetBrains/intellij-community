256804206e8199c35784fd1f7b0a65b415d6618c
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-01-21 20:02:03 +0100

    [debugger] Convert XMarkObjectActionHandler to Kotlin
    GitOrigin-RevId: 8b5f767b824333609ac6b0b9bb575e18641530cb

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/handlers/XMarkObjectActionHandler.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/handlers/XMarkObjectActionHandler.kt
@@ -1,102 +1,95 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.xdebugger.impl.actions.handlers;
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.xdebugger.impl.actions.handlers
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.project.Project;
-import com.intellij.ui.ComponentUtil;
-import com.intellij.util.ui.UIUtil;
-import com.intellij.xdebugger.XDebugSession;
-import com.intellij.xdebugger.frame.XValue;
-import com.intellij.xdebugger.impl.XDebugSessionImpl;
-import com.intellij.xdebugger.impl.actions.MarkObjectActionHandler;
-import com.intellij.xdebugger.impl.frame.XValueMarkers;
-import com.intellij.xdebugger.impl.ui.DebuggerUIUtil;
-import com.intellij.xdebugger.impl.ui.tree.ValueMarkerPresentationDialog;
-import com.intellij.xdebugger.impl.ui.tree.ValueMarkup;
-import com.intellij.xdebugger.impl.ui.tree.XDebuggerTreeState;
-import com.intellij.xdebugger.impl.ui.tree.actions.XDebuggerTreeActionBase;
-import com.intellij.xdebugger.impl.ui.tree.nodes.XValueNodeImpl;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.concurrency.Promise;
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformCoreDataKeys
+import com.intellij.openapi.project.Project
+import com.intellij.ui.ComponentUtil
+import com.intellij.util.ui.UIUtil
+import com.intellij.xdebugger.impl.XDebugSessionImpl
+import com.intellij.xdebugger.impl.actions.MarkObjectActionHandler
+import com.intellij.xdebugger.impl.frame.XValueMarkers
+import com.intellij.xdebugger.impl.ui.DebuggerUIUtil
+import com.intellij.xdebugger.impl.ui.tree.ValueMarkerPresentationDialog
+import com.intellij.xdebugger.impl.ui.tree.XDebuggerTreeState
+import com.intellij.xdebugger.impl.ui.tree.actions.XDebuggerTreeActionBase
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.concurrency.Promise
+import java.awt.Component
+import java.util.function.Consumer
+import javax.swing.JDialog
+import javax.swing.JFrame
 
-import javax.swing.*;
-import java.awt.*;
+internal class XMarkObjectActionHandler : MarkObjectActionHandler() {
+  override fun perform(project: Project, event: AnActionEvent) {
+    val session = DebuggerUIUtil.getSession(event)
+    if (session == null) return
 
-import static com.intellij.openapi.actionSystem.PlatformCoreDataKeys.CONTEXT_COMPONENT;
+    val markers = (session as XDebugSessionImpl).getValueMarkers()
+    val node = XDebuggerTreeActionBase.getSelectedNode(event.dataContext)
+    if (markers == null || node == null) return
+    val value = node.valueContainer
 
-@ApiStatus.Internal
-public class XMarkObjectActionHandler extends MarkObjectActionHandler {
-  @Override
-  public void perform(@NotNull Project project, @NotNull AnActionEvent event) {
-    XDebugSession session = DebuggerUIUtil.getSession(event);
-    if (session == null) return;
+    val detachedView = DebuggerUIUtil.isInDetachedTree(event)
+    val treeState = XDebuggerTreeState.saveState(node.tree)
 
-    XValueMarkers<?, ?> markers = ((XDebugSessionImpl)session).getValueMarkers();
-    XValueNodeImpl node = XDebuggerTreeActionBase.getSelectedNode(event.getDataContext());
-    if (markers == null || node == null) return;
-    XValue value = node.getValueContainer();
-
-    boolean detachedView = DebuggerUIUtil.isInDetachedTree(event);
-    XDebuggerTreeState treeState = XDebuggerTreeState.saveState(node.getTree());
-
-    ValueMarkup existing = markers.getMarkup(value);
-    Promise<Object> markPromise;
+    val existing = markers.getMarkup(value)
+    val markPromise: Promise<Any?>
     if (existing != null) {
-      markPromise = markers.unmarkValue(value);
+      markPromise = markers.unmarkValue(value)
     }
     else {
-      Component component = event.getData(CONTEXT_COMPONENT);
-      Window window = ComponentUtil.getWindow(component);
-      if (!(window instanceof JFrame) && !(window instanceof JDialog)) {
-        component = window.getOwner();
+      var component = event.getData<Component?>(PlatformCoreDataKeys.CONTEXT_COMPONENT)
+      val window = ComponentUtil.getWindow(component)
+      if (window !is JFrame && window !is JDialog) {
+        component = window!!.owner
       }
-      ValueMarkerPresentationDialog dialog = new ValueMarkerPresentationDialog(
-        component, node.getName(), markers.getAllMarkers().values());
-      dialog.show();
-      ValueMarkup markup = dialog.getConfiguredMarkup();
-      if (dialog.isOK() && markup != null) {
-        markPromise = markers.markValue(value, markup);
-      } else {
-        return;
+      val dialog = ValueMarkerPresentationDialog(
+        component, node.name, markers.getAllMarkers().values)
+      dialog.show()
+      val markup = dialog.getConfiguredMarkup()
+      if (dialog.isOK && markup != null) {
+        markPromise = markers.markValue(value, markup)
+      }
+      else {
+        return
       }
     }
-    markPromise.onSuccess(__ ->
-      UIUtil.invokeLaterIfNeeded(() -> {
+    markPromise.onSuccess(Consumer { _: Any? ->
+      UIUtil.invokeLaterIfNeeded(Runnable {
         if (detachedView) {
-          node.getTree().rebuildAndRestore(treeState);
+          node.tree.rebuildAndRestore(treeState)
         }
-        session.rebuildViews();
+        session.rebuildViews()
       })
-    );
+    }
+    )
   }
 
-  @Override
-  public boolean isEnabled(@NotNull Project project, @NotNull AnActionEvent event) {
-    XValueMarkers<?, ?> markers = getValueMarkers(event);
-    if (markers == null) return false;
+  override fun isEnabled(project: Project, event: AnActionEvent): Boolean {
+    val markers: XValueMarkers<*, *>? = getValueMarkers(event)
+    if (markers == null) return false
 
-    XValue value = XDebuggerTreeActionBase.getSelectedValue(event.getDataContext());
-    return value != null && markers.canMarkValue(value);
+    val value = XDebuggerTreeActionBase.getSelectedValue(event.getDataContext())
+    return value != null && markers.canMarkValue(value)
   }
 
-  @Override
-  public boolean isMarked(@NotNull Project project, @NotNull AnActionEvent event) {
-    XValueMarkers<?, ?> markers = getValueMarkers(event);
-    if (markers == null) return false;
+  override fun isMarked(project: Project, event: AnActionEvent): Boolean {
+    val markers: XValueMarkers<*, *>? = getValueMarkers(event)
+    if (markers == null) return false
 
-    XValue value = XDebuggerTreeActionBase.getSelectedValue(event.getDataContext());
-    return value != null && markers.getMarkup(value) != null;
+    val value = XDebuggerTreeActionBase.getSelectedValue(event.getDataContext())
+    return value != null && markers.getMarkup(value) != null
   }
 
-  @Override
-  public boolean isHidden(@NotNull Project project, @NotNull AnActionEvent event) {
-    return getValueMarkers(event) == null;
+  override fun isHidden(project: Project, event: AnActionEvent): Boolean {
+    return getValueMarkers(event) == null
   }
 
-  private static @Nullable XValueMarkers<?, ?> getValueMarkers(AnActionEvent event) {
-    XDebugSession session = DebuggerUIUtil.getSession(event);
-    return session != null ? ((XDebugSessionImpl)session).getValueMarkers() : null;
+  companion object {
+    private fun getValueMarkers(event: AnActionEvent): XValueMarkers<*, *>? {
+      val session = DebuggerUIUtil.getSession(event)
+      return if (session != null) (session as XDebugSessionImpl).getValueMarkers() else null
+    }
   }
 }
