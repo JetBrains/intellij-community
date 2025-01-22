937c84d555d2000b12f39c05204ed3b19f548e51
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-01-23 00:49:52 +0100

    [kotlin] unmute passed test on context parameters
    ^KT-72639
    
    GitOrigin-RevId: 31a341d44a14ccdf6e4df749dbcf62319b95cc82

--- plugins/kotlin/idea/tests/testData/inspectionsLocal/unusedReceiverParameter/contextParameter.kt
+++ plugins/kotlin/idea/tests/testData/inspectionsLocal/unusedReceiverParameter/contextParameter.kt
@@ -1,8 +1,6 @@
 // PROBLEM: none
 // COMPILER_ARGUMENTS: -Xcontext-parameters
 // IGNORE_K1
-// IGNORE_K2
-// Obtaining context parameters is not yet implemented in the analysis so we ignore
 
 context(_: Int)
 fun other() {
