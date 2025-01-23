681ac567135231d260ebf0c4b2c802d20f0f6ffb
Anna.Gromova <anna.gromova@jetbrains.com>
2025-01-23 13:05:35 +0100

    Remove unused and redundant stripe button management logic.
    GitOrigin-RevId: 364f75af97a7954b3360d0723d17cad199338037

--- platform/platform-impl/src/com/intellij/toolWindow/xNext/toolbar/actions/XNextToolWindowButtonManager.kt
+++ platform/platform-impl/src/com/intellij/toolWindow/xNext/toolbar/actions/XNextToolWindowButtonManager.kt
@@ -1,12 +1,6 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.toolWindow.xNext.toolbar.actions
 
-import com.intellij.openapi.wm.RegisterToolWindowTask
-import com.intellij.openapi.wm.ToolWindowAnchor
-import com.intellij.openapi.wm.WindowInfo
-import com.intellij.openapi.wm.impl.AbstractDroppableStripe
-import com.intellij.openapi.wm.impl.ToolWindowImpl
-import com.intellij.toolWindow.StripeButtonManager
 import com.intellij.toolWindow.ToolWindowPane
 import com.intellij.toolWindow.ToolWindowPaneNewButtonManager
 import com.intellij.toolWindow.xNext.toolbar.XNextBar
@@ -26,31 +20,6 @@ internal class XNextToolWindowButtonManager(paneId: String, isPrimary: Boolean) 
     xNextToolbar.topStripe.bottomAnchorDropAreaComponent = pane
     xNextToolbar.bottomStripe.bottomAnchorDropAreaComponent = pane
     super.setupToolWindowPane(pane)
-  }
-
-  override fun createStripeButton(toolWindow: ToolWindowImpl, info: WindowInfo, task: RegisterToolWindowTask?): StripeButtonManager {
-    val manager = super.createStripeButton(toolWindow)
-    if(toolWindow.id == "Project" || toolWindow.id == "Version Control") {
-      xNextToolbar.topStripe.addButton(manager)
-    } else if(toolWindow.id == "Database" || toolWindow.id == "Structure" || toolWindow.id == "AIAssistant" || toolWindow.id == "Debug") {
-      xNextToolbar.bottomStripe.addButton(manager)
-    }
-    return super.createStripeButton(toolWindow, info, task)
-  }
-
-/*  private fun createXNextStripeButton(toolWindow: ToolWindowImpl): StripeButtonManager {
-    return object : StripeButtonManager {
-      override val id: String = toolWindow.id
-      override val toolWindow: ToolWindowImpl = toolWindow
-
-    }
-  }*/
-
-
-  override fun getStripeFor(anchor: ToolWindowAnchor, isSplit: Boolean?): AbstractDroppableStripe {
-    //при драге определяем страйп над которым шевелимся
-
-    return super.getStripeFor(anchor, isSplit)
   }
 
   override fun wrapWithControls(pane: ToolWindowPane): JComponent {
