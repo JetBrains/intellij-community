1155bbd57e5d76a38b34a7c41faabfc75fed7ae4
Nikita Barkov <nikita.barkov@jetbrains.com>
2025-01-23 09:33:37 +0100

    [update_ide_tests]Updated updateAndShowResult method
    GitOrigin-RevId: c87d471fc44d7079df75dccb14881c2d41695960

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/UpdateChecker.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/UpdateChecker.kt
@@ -3,10 +3,10 @@ package com.intellij.driver.sdk
 import com.intellij.driver.client.Driver
 import com.intellij.driver.client.Remote
 
-fun Driver.updateAndShowResult(project: Project?) = utility(UpdateChecker::class).updateAndShowResult(project)
+fun Driver.updateAndShowResult() = utility(UpdateChecker::class).updateAndShowResult()
 
 
 @Remote("com.intellij.openapi.updateSettings.impl.UpdateChecker")
 interface UpdateChecker {
-  fun updateAndShowResult(project: Project?)
+  fun updateAndShowResult()
 }
