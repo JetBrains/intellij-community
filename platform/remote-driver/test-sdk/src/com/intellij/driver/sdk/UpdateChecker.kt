ec18433e0ff3c6cba4e13262dd9ec5e9c8c6e661
Nikita Barkov <nikita.barkov@jetbrains.com>
2025-01-23 17:00:08 +0100

    [update_ide_tests]Added waitForProjectOpen
    GitOrigin-RevId: 1cc9e826052a92fe5ca2c5e323751bb1359f2036

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/UpdateChecker.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/UpdateChecker.kt
@@ -3,10 +3,10 @@ package com.intellij.driver.sdk
 import com.intellij.driver.client.Driver
 import com.intellij.driver.client.Remote
 
-fun Driver.updateAndShowResult() = utility(UpdateChecker::class).updateAndShowResult()
+fun Driver.updateAndShowResult(project: Project?) = utility(UpdateChecker::class).updateAndShowResult(project)
 
 
 @Remote("com.intellij.openapi.updateSettings.impl.UpdateChecker")
 interface UpdateChecker {
-  fun updateAndShowResult()
+  fun updateAndShowResult(project: Project?)
 }
