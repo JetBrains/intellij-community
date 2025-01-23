c29b7e547c994bfdc767d985a5692c5f07b82d86
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2025-01-24 03:34:43 +0400

    [kotlin] VariableInitializerIsRedundant inspection is added
    Tests will be added along with the fix for KTIJ-29527.
    
    ^KTIJ-32848 Fixed
    
    GitOrigin-RevId: 3599c951c6ff4e4f936cf7ff8c1f44b973fb9e2d

---
+++ plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/diagnosticBased/VariableInitializerIsRedundantInspection.kt
@@ -0,0 +1,47 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.k2.codeinsight.inspections.diagnosticBased
+
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.codeInspection.util.InspectionMessage
+import org.jetbrains.kotlin.analysis.api.KaSession
+import org.jetbrains.kotlin.analysis.api.fir.diagnostics.KaFirDiagnostic
+import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinModCommandQuickFix
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinPsiDiagnosticBasedInspectionBase
+import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtVisitor
+import org.jetbrains.kotlin.psi.KtVisitorVoid
+import kotlin.reflect.KClass
+
+internal class VariableInitializerIsRedundantInspection : KotlinPsiDiagnosticBasedInspectionBase<KtElement, KaFirDiagnostic.VariableInitializerIsRedundant, Unit>() {
+    override val diagnosticType: KClass<KaFirDiagnostic.VariableInitializerIsRedundant>
+        get() = KaFirDiagnostic.VariableInitializerIsRedundant::class
+
+    context(KaSession@KaSession)
+    override fun prepareContextByDiagnostic(
+        element: KtElement,
+        diagnostic: KaFirDiagnostic.VariableInitializerIsRedundant,
+    ): Unit = Unit
+
+    override fun getProblemDescription(
+        element: KtElement,
+        context: Unit,
+    ): @InspectionMessage String = KotlinBundle.message("initializer.is.redundant")
+
+    override fun getProblemHighlightType(element: KtElement, context: Unit): ProblemHighlightType = ProblemHighlightType.LIKE_UNUSED_SYMBOL
+
+    override fun createQuickFix(
+        element: KtElement,
+        context: Unit,
+    ): KotlinModCommandQuickFix<KtElement>? = null // KTIJ-29527
+
+    override fun buildVisitor(
+        holder: ProblemsHolder,
+        isOnTheFly: Boolean,
+    ): KtVisitor<*, *> = object : KtVisitorVoid() {
+        override fun visitKtElement(element: KtElement) {
+            visitTargetElement(element, holder, isOnTheFly)
+        }
+    }
+}
