fa76ea8db383f1d744e870bfbddcb916c31dfefe
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2025-01-24 21:44:34 +0400

    [kotlin] Move `RedundantUnitReturnTypeInspection` to the ` diagnosticBased ` package
    ^KTIJ-32858
    
    GitOrigin-RevId: 701baa9e7e075d3472f357d4f0b7433f2a785927

--- plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/diagnosticBased/RedundantUnitReturnTypeInspection.kt
+++ plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/diagnosticBased/RedundantUnitReturnTypeInspection.kt
@@ -1,6 +1,5 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-
-package org.jetbrains.kotlin.idea.k2.codeinsight.inspections.declarations
+package org.jetbrains.kotlin.idea.k2.codeinsight.inspections.diagnosticBased
 
 import com.intellij.codeInspection.CleanupLocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
@@ -11,14 +10,13 @@ import org.jetbrains.kotlin.analysis.api.fir.diagnostics.KaFirDiagnostic
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinKtDiagnosticBasedInspectionBase
 import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinModCommandQuickFix
-import org.jetbrains.kotlin.idea.codeinsights.impl.base.CallableReturnTypeUpdaterUtils.TypeInfo
-import org.jetbrains.kotlin.idea.codeinsights.impl.base.CallableReturnTypeUpdaterUtils.updateType
+import org.jetbrains.kotlin.idea.codeinsights.impl.base.CallableReturnTypeUpdaterUtils
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
 import kotlin.reflect.KClass
 
 internal class RedundantUnitReturnTypeInspection :
-    KotlinKtDiagnosticBasedInspectionBase<KtElement, KaFirDiagnostic.RedundantReturnUnitType, TypeInfo>(),
+    KotlinKtDiagnosticBasedInspectionBase<KtElement, KaFirDiagnostic.RedundantReturnUnitType, CallableReturnTypeUpdaterUtils.TypeInfo>(),
     CleanupLocalInspectionTool {
 
     override val diagnosticType: KClass<KaFirDiagnostic.RedundantReturnUnitType>
@@ -33,19 +31,19 @@ internal class RedundantUnitReturnTypeInspection :
         }
     }
 
-    override fun getProblemDescription(element: KtElement, context: TypeInfo): String =
+    override fun getProblemDescription(element: KtElement, context: CallableReturnTypeUpdaterUtils.TypeInfo): String =
         KotlinBundle.message("inspection.redundant.unit.return.type.display.name")
 
     context(KaSession@KaSession)
     override fun prepareContextByDiagnostic(
         element: KtElement,
         diagnostic: KaFirDiagnostic.RedundantReturnUnitType,
-    ): TypeInfo? {
+    ): CallableReturnTypeUpdaterUtils.TypeInfo? {
         val typeReference = element as? KtTypeReference ?: return null
         val returnType = typeReference.type.fullyExpandedType
 
         if (!returnType.isMarkedNullable && returnType.isUnitType) {
-            return TypeInfo(TypeInfo.UNIT)
+            return CallableReturnTypeUpdaterUtils.TypeInfo(CallableReturnTypeUpdaterUtils.TypeInfo.Companion.UNIT)
         }
 
         return null
@@ -53,7 +51,7 @@ internal class RedundantUnitReturnTypeInspection :
 
     override fun createQuickFix(
         element: KtElement,
-        context: TypeInfo,
+        context: CallableReturnTypeUpdaterUtils.TypeInfo,
     ): KotlinModCommandQuickFix<KtElement> = object : KotlinModCommandQuickFix<KtElement>() {
 
         override fun getFamilyName(): String = KotlinBundle.message("inspection.redundant.unit.return.type.action.name")
@@ -64,7 +62,7 @@ internal class RedundantUnitReturnTypeInspection :
             updater: ModPsiUpdater,
         ) {
             val function = element.getParentOfType<KtNamedFunction>(strict = true) ?: return
-            updateType(function, context, project, updater)
+            CallableReturnTypeUpdaterUtils.updateType(function, context, project, updater)
         }
     }
 }
