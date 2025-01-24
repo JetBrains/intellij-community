4ec1eeefa0188291ca953cf1422375fea641ef54
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2025-01-24 15:11:35 +0100

    [gradle][daemon][IDEA-364986] run GradleDaemonActionTest on Gradle 8.12
    GitOrigin-RevId: 7aa65a4dce567a769f1dfa8ad00dae344f40cb67

--- plugins/gradle/testSources/org/jetbrains/plugins/gradle/daemon/GradleDaemonActionTest.kt
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/daemon/GradleDaemonActionTest.kt
@@ -23,7 +23,7 @@ class GradleDaemonActionTest : GradleImportingTestCase() {
   }
 
   @Test
-  @TargetVersions("6.0+", "!8.12")// The Gradle Daemon below version 6.0 is unstable and causes test fluctuations
+  @TargetVersions("6.0+")// The Gradle Daemon below version 6.0 is unstable and causes test fluctuations
   fun `test get daemon status`() {
     runDaemon()
   }
@@ -37,7 +37,7 @@ class GradleDaemonActionTest : GradleImportingTestCase() {
   }
 
   @Test
-  @TargetVersions("6.0+", "!8.12") // The Gradle Daemon below version 6.0 is unstable and causes test fluctuations
+  @TargetVersions("6.0+") // The Gradle Daemon below version 6.0 is unstable and causes test fluctuations
   fun `test stop gradle daemon when idle`() {
     runDaemon()
     gracefulStopDaemons(gradleUserHomesToCheck)
