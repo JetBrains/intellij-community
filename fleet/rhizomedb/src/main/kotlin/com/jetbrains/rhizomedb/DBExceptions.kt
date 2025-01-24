e67834bc4840dca5541c7390d5bd3cf0569b91cc
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-01-24 11:51:42 +0100

    [platform] IJPL-175634: Add info for IJ developers when OutOfDbContext is thrown
    GitOrigin-RevId: fc48888bb5e1f3e516058f9e74080455da477c26

--- fleet/rhizomedb/src/main/kotlin/com/jetbrains/rhizomedb/DBExceptions.kt
+++ fleet/rhizomedb/src/main/kotlin/com/jetbrains/rhizomedb/DBExceptions.kt
@@ -1,3 +1,4 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.rhizomedb
 
 /**
@@ -6,7 +7,13 @@ package com.jetbrains.rhizomedb
 class TxValidationException(message: String) : RuntimeException(message)
 
 class OutOfDbContext :
-  RuntimeException("Free entities require context db to be used. Open transaction with db.tx or setup db to read with asOf(db)")
+  RuntimeException("""
+    Free entities require context db to be used. Open transaction with db.tx or setup db to read with asOf(db).
+    
+    For IntelliJ: Most probably RhizomeDB context is not properly propagated. By default DB context is propagated for EDT and Services' coroutine scopes.
+    If you created your own CoroutineScope or used `GlobalScope.childScope()`, then you have to use `withKernel { ... }` wrapper, so DB will be properly propagated there.
+    But, please, avoid creating your own CoroutineScopes and don't use `GlobalScope`
+  """.trimIndent())
 
 class OutOfMutableDbContext :
   RuntimeException("Free entities require mutable context db to be updated. Open transaction with db.tx or setup db to read with asOf(db)")
