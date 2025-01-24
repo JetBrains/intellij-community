34122e7716bba4208bd63859682f2ccd398847cc
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2025-01-24 17:00:41 +0200

    PY-74114 Fix a race in PyUpdateSdkTask.run causing flaky setup of bundled .pyi stubs
    Previously, PythonPackageManagerExt.launchReload launched an asynchronous
    update of the installed packages cache. Then, because the information about
    which packages were installed was not available by the time
    PyUpdateSdkTask.addBundledPyiStubsToInterpreterPaths was run,
    we sometimes didn't enable the corresponding bundled stubs.
    
    GitOrigin-RevId: db4bcc902919858571acc2cad3e22758d7f55b97

--- python/src/com/jetbrains/python/packaging/management/PythonPackageManagerExt.kt
+++ python/src/com/jetbrains/python/packaging/management/PythonPackageManagerExt.kt
@@ -8,12 +8,11 @@ import com.intellij.execution.process.CapturingProcessHandler
 import com.intellij.execution.target.TargetProgressIndicator
 import com.intellij.execution.target.local.LocalTargetEnvironmentRequest
 import com.intellij.execution.target.value.targetPath
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.components.ComponentManagerEx
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.progress.EmptyProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.blockingContext
+import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.platform.ide.progress.withBackgroundProgress
@@ -31,13 +30,12 @@ import com.jetbrains.python.run.buildTargetedCommandLine
 import com.jetbrains.python.run.ensureProjectSdkAndModuleDirsAreOnTarget
 import com.jetbrains.python.run.prepareHelperScriptExecution
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.jetbrains.annotations.Nls
 import kotlin.math.min
 
 fun PythonPackageManager.launchReload() {
-  (ApplicationManager.getApplication() as ComponentManagerEx).getCoroutineScope().launch {
+  runBlockingCancellable {
     runPackagingOperationOrShowErrorDialog(sdk, PyBundle.message("python.packaging.operation.failed.title")) {
       reloadPackages()
     }
