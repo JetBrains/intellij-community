55e516ab90b7d72e8e43c6e98747ee370a11d279
Alexey Kryuchkov <alexey.kryuchkov@jetbrains.com>
2025-01-24 11:25:55 +0400

    Check traffic light only when daemon was stopped in WaitForFinishedCodeAnalysis
    GitOrigin-RevId: 891c6c73622de9afa6f9cca7644e668ccf39b33e

--- plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/commands/WaitForFinishedCodeAnalysis.kt
+++ plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/commands/WaitForFinishedCodeAnalysis.kt
@@ -293,7 +293,7 @@ class CodeAnalysisStateListener(val project: Project, val cs: CoroutineScope) {
         val (editor, exceptionWithTime) = iterator.next()
         val highlightedEditor = highlightedEditors[editor]
 
-        if (checkTrafficLightRenderer() && !isTrafficLightExists(editor.editor)) {
+        if (status == "stopped" && checkTrafficLightRenderer() && !isTrafficLightExists(editor.editor)) {
           LOG.error("Highlighting traffic light should be shown in the top right corner of the editor, in case of $status")
           takeFullScreenshot("traffic-light-screenshot")
         }
