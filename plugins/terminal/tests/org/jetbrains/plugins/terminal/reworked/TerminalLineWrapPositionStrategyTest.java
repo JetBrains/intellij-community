8e0cb1e850306556034d473adeae7990c4ce7c5a
Vera Petrenkova <vera.petrenkova@jetbrains.com>
2025-01-27 12:28:44 +0100

    IJPL-164425 [terminal] move test for soft-wraps to terminal module
    GitOrigin-RevId: d77aacfb0119a011f6a0207c4dfb2fe8bcd4e65c

--- plugins/terminal/tests/org/jetbrains/plugins/terminal/reworked/TerminalLineWrapPositionStrategyTest.java
+++ plugins/terminal/tests/org/jetbrains/plugins/terminal/reworked/TerminalLineWrapPositionStrategyTest.java
@@ -1,25 +1,23 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.editor;
+package org.jetbrains.plugins.terminal.reworked;
 
+import com.intellij.openapi.editor.LineWrapPositionStrategy;
+import com.intellij.openapi.editor.impl.SoftWrapEngine;
 import org.jetbrains.plugins.terminal.block.reworked.lang.TerminalLineWrapPositionStrategy;
-import org.junit.Before;
 import org.junit.Test;
 
-public class TerminalLineWrapPositionStrategyTest extends AbstractLineWrapPositionStrategyTest {
-  private LineWrapPositionStrategy myStrategy;
+import static org.junit.Assert.assertSame;
 
-  @Override
-  @Before
-  public void prepare() {
-    super.prepare();
-    myStrategy = new TerminalLineWrapPositionStrategy();
-  }
+public class TerminalLineWrapPositionStrategyTest {
+  private final LineWrapPositionStrategy myStrategy= new TerminalLineWrapPositionStrategy();;
 
   //uD852 and uDF62 are surrogate pair - 2 characters that made 1 "𤭢"; soft wrap shouldn't be made between them
   //the expected result is to move the 3rd symbol to the next line, so we will have  uD852 uDF62\n uD852 uDF62 = 𤭢\n𤭢
   @Test
   public void preventWrapInsideOfSurrogatePairs() {
     String text = "\uD852\uDF62\uD852\uDF62";//𤭢𤭢
-    doTestDefaultWrap(myStrategy, text, 3);
+    int expectedResult = 3;
+    int offset = SoftWrapEngine.findWrapPosition(text, text.length() - 1, 1, myStrategy);
+    assertSame(expectedResult, offset);
   }
 }
