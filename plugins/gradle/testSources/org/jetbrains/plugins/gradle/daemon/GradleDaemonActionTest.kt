e916ab18b1e5dac0c9e12b6722389004209c4d81
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2025-01-27 18:22:08 +0100

    [gradle][test][IDEA-364986] wait for daemon termination in `GradleDaemonActionTest`
    GitOrigin-RevId: 077227ea4218f9d6e5a0cc6f350134bc94c42c70

--- plugins/gradle/testSources/org/jetbrains/plugins/gradle/daemon/GradleDaemonActionTest.kt
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/daemon/GradleDaemonActionTest.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.daemon
 
+import com.intellij.gradle.toolingExtension.util.GradleVersionUtil
 import com.intellij.openapi.util.io.toCanonicalPath
 import org.jetbrains.plugins.gradle.importing.GradleImportingTestCase
 import org.jetbrains.plugins.gradle.internal.daemon.DaemonState
@@ -10,6 +11,8 @@ import org.jetbrains.plugins.gradle.internal.daemon.stopDaemons
 import org.jetbrains.plugins.gradle.testFramework.util.importProject
 import org.jetbrains.plugins.gradle.tooling.annotation.TargetVersions
 import org.junit.Test
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 class GradleDaemonActionTest : GradleImportingTestCase() {
 
@@ -23,17 +26,25 @@ class GradleDaemonActionTest : GradleImportingTestCase() {
   }
 
   @Test
-  @TargetVersions("6.0+")// The Gradle Daemon below version 6.0 is unstable and causes test fluctuations
+  @TargetVersions("6.0+") // The Gradle Daemon below version 6.0 is unstable and causes test fluctuations
   fun `test get daemon status`() {
     runDaemon()
   }
 
   @Test
-  @TargetVersions("6.0+", "!8.12") // The Gradle Daemon below version 6.0 is unstable and causes test fluctuations
+  @TargetVersions("6.0+") // The Gradle Daemon below version 6.0 is unstable and causes test fluctuations
   fun `test stop gradle daemon`() {
     runDaemon()
     stopDaemons(gradleUserHomesToCheck)
-    assertNoDaemonRunning()
+    // Gradle prior 8.12 uses blocking API for shutdown.
+    // As a result, no daemon should be available after stop being executed.
+    // Since 8.12, Gradle uses message bus to propagate the stop command.
+    if (GradleVersionUtil.isCurrentGradleOlderThan("8.12")) {
+      assertNoDaemonRunning()
+    }
+    else {
+      awaitDaemonTermination(30.seconds)
+    }
   }
 
   @Test
@@ -52,6 +63,17 @@ class GradleDaemonActionTest : GradleImportingTestCase() {
     assertDaemonRunning()
   }
 
+  private fun awaitDaemonTermination(duration: Duration) {
+    val deadline = System.currentTimeMillis() + duration.inWholeMilliseconds
+    while (System.currentTimeMillis() < deadline) {
+      if (findDaemon() == null) {
+        return
+      }
+      Thread.sleep(1.seconds.inWholeMilliseconds)
+    }
+    throw IllegalStateException("Gradle daemon is still running after ${duration.inWholeMilliseconds}ms of waiting for shutdown")
+  }
+
   private fun assertNoDaemonRunning() {
     val activeDaemon = findDaemon()
     assertNull(activeDaemon)
@@ -65,7 +87,7 @@ class GradleDaemonActionTest : GradleImportingTestCase() {
   private fun findDaemon(): DaemonState? {
     val daemons = getDaemonsStatus(gradleUserHomesToCheck)
     if (daemons.isEmpty()) {
-       LOG.warn("No running daemons were found in the required user home!")
+      LOG.warn("No running daemons were found in the required user home!")
       return null
     }
     return daemons.find {
