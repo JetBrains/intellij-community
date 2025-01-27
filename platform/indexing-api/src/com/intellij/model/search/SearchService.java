2c2226e4bc4204e179cf66c5a7cdd90d289c5ab4
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2025-01-27 13:12:04 +0100

    IJPL-175872 drop the ability to search for all references or declarations of a symbol
    The generic ability does not make sense because each feature (Find usages, Rename, etc.) is expected to implement its
    own search. We've already had `ReferencesSearch` and tried to implement different features on top of it, and it turned
    out that it's too generic. For instance, one needs to filter out some of its results during the Rename, but include them
    in Find Usages. Instead, we require language support to implement different searchers for different features. If the
    code is actually share-able, it's up to the language plugin implementation to share it.
    
    GitOrigin-RevId: 65f719838645d780effcbe80a1aafaf93e756ae0

--- platform/indexing-api/src/com/intellij/model/search/SearchService.java
+++ platform/indexing-api/src/com/intellij/model/search/SearchService.java
@@ -1,12 +1,8 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.model.search;
 
-import com.intellij.model.Symbol;
-import com.intellij.model.psi.PsiSymbolDeclaration;
-import com.intellij.model.psi.PsiSymbolReference;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.search.SearchScope;
 import com.intellij.util.Query;
 import org.jetbrains.annotations.NotNull;
 
@@ -31,20 +27,6 @@ public interface SearchService {
    * Creates new builder of text occurrences query.
    */
   @NotNull SearchWordQueryBuilder searchWord(@NotNull Project project, @NotNull String word);
-
-  /**
-   * Creates new query for searching PSI references by symbol.
-   */
-  @NotNull Query<@NotNull PsiSymbolReference> searchPsiSymbolReferences(@NotNull Project project,
-                                                                        @NotNull Symbol symbol,
-                                                                        @NotNull SearchScope searchScope);
-
-  /**
-   * Creates new query for searching PSI declarations by symbol.
-   */
-  @NotNull Query<@NotNull PsiSymbolDeclaration> searchPsiSymbolDeclarations(@NotNull Project project,
-                                                                            @NotNull Symbol symbol,
-                                                                            @NotNull SearchScope searchScope);
 
   /**
    * Merges a list of queries into a single query.
--- plugins/markdown/model/src/main/kotlin/org/intellij/plugins/markdown/model/psi/headers/html/MarkdownHtmlAnchorPsiReferenceSearcher.kt
+++ plugins/markdown/model/src/main/kotlin/org/intellij/plugins/markdown/model/psi/headers/html/MarkdownHtmlAnchorPsiReferenceSearcher.kt
@@ -5,7 +5,10 @@ import com.intellij.model.psi.PsiExternalReferenceHost
 import com.intellij.model.psi.PsiSymbolReference
 import com.intellij.model.psi.PsiSymbolReferenceHints
 import com.intellij.model.psi.PsiSymbolReferenceService
-import com.intellij.model.search.*
+import com.intellij.model.search.LeafOccurrence
+import com.intellij.model.search.LeafOccurrenceMapper
+import com.intellij.model.search.SearchContext
+import com.intellij.model.search.SearchService
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.SearchScope
 import com.intellij.psi.util.walkUp
@@ -16,14 +19,17 @@ import org.intellij.plugins.markdown.model.psi.headers.HeaderAnchorLinkDestinati
  * Needed only for resolving of [HtmlAnchorSymbol].
  * Searches only for [HeaderAnchorLinkDestinationReference].
  */
-internal class MarkdownHtmlAnchorPsiReferenceSearcher: PsiSymbolReferenceSearcher {
-  override fun collectSearchRequests(parameters: PsiSymbolReferenceSearchParameters): Collection<Query<out PsiSymbolReference>> {
-    val symbol = parameters.symbol as? HtmlAnchorSymbol ?: return emptyList()
-    val project = parameters.project
-    val searchScope = parameters.searchScope
-    val request = buildSearchRequest(project, symbol, searchScope)
-    return listOf(request)
-  }
+internal class MarkdownHtmlAnchorPsiReferenceSearcher
+  //: PsiSymbolReferenceSearcher
+{
+
+  //override fun collectSearchRequests(parameters: PsiSymbolReferenceSearchParameters): Collection<Query<out PsiSymbolReference>> {
+  //  val symbol = parameters.symbol as? HtmlAnchorSymbol ?: return emptyList()
+  //  val project = parameters.project
+  //  val searchScope = parameters.searchScope
+  //  val request = buildSearchRequest(project, symbol, searchScope)
+  //  return listOf(request)
+  //}
 
   companion object {
     fun buildSearchRequest(project: Project, symbol: HtmlAnchorSymbol, searchScope: SearchScope): Query<out PsiSymbolReference> {
