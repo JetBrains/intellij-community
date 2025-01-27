ff06bae2e72a6bad6c43b2040bdb2a0b7c9017a7
Pouria.Derakhshanfar <Pouria.Derakhshanfar@jetbrains.com>
2025-01-27 11:52:48 +0100

    [evaluation-plugin] Update ExecutionMode options and add resolve method
    (cherry picked from commit d591d410dc9d1209f0556e339e296ef7e597439b)
    
    IJ-MR-155917
    
    GitOrigin-RevId: 1d681ec57976f5e6a4ac03fe2709bf64f4054a1e

--- plugins/evaluation-plugin/languages/src/com/intellij/cce/execution/ExecutionMode.kt
+++ plugins/evaluation-plugin/languages/src/com/intellij/cce/execution/ExecutionMode.kt
@@ -1,6 +1,14 @@
 package com.intellij.cce.execution
 
 enum class ExecutionMode(val displayName: String) {
+  NONE("none"),
   LOCAL("local"),
-  DOCKER("docker")
+  DOCKER("docker");
+
+  companion object {
+    fun resolve(displayName: String?): ExecutionMode {
+      return values().find { it.displayName == displayName }
+             ?: NONE
+    }
+  }
 }
