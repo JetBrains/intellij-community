5813561261f63cf9f46fffe8247ffd46644305a6
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2025-01-27 13:45:02 +0100

    [kotlin] make the existing `KDocUnresolvedReferenceInspection` to be K1 only
    For K2, there will be an inspection with a quickfix
    
    KTIJ-5138
    
    GitOrigin-RevId: 1092f13152d8d5a34b4f305abbd494c6da649a6a

--- plugins/kotlin/code-insight/inspections-k1/src/org/jetbrains/kotlin/idea/k1/codeinsight/inspections/KDocUnresolvedReferenceInspection.kt
+++ plugins/kotlin/code-insight/inspections-k1/src/org/jetbrains/kotlin/idea/k1/codeinsight/inspections/KDocUnresolvedReferenceInspection.kt
@@ -1,14 +1,16 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.codeInsight.inspections.shared
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.k1.codeinsight.inspections
 
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
+import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.kotlin.idea.codeinsight.api.classic.inspections.AbstractKotlinInspection
 import org.jetbrains.kotlin.idea.references.mainReference
 import org.jetbrains.kotlin.kdoc.psi.impl.KDocName
 
-internal class KDocUnresolvedReferenceInspection : AbstractKotlinInspection() {
+@ApiStatus.Internal
+class KDocUnresolvedReferenceInspection : AbstractKotlinInspection() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor =
         KDocUnresolvedReferenceVisitor(holder)
 
--- plugins/kotlin/code-insight/inspections-shared/tests/k1/test/org/jetbrains/kotlin/idea/codeInsight/inspections/shared/idea/kdoc/AbstractSharedK1KDocHighlightingTest.java
+++ plugins/kotlin/code-insight/inspections-shared/tests/k1/test/org/jetbrains/kotlin/idea/codeInsight/inspections/shared/idea/kdoc/AbstractSharedK1KDocHighlightingTest.java
@@ -1,8 +1,8 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.codeInsight.inspections.shared.idea.kdoc;
 
-import org.jetbrains.kotlin.idea.codeInsight.inspections.shared.KDocUnresolvedReferenceInspection;
 import org.jetbrains.kotlin.idea.highlighter.AbstractHighlightingTest;
+import org.jetbrains.kotlin.idea.k1.codeinsight.inspections.KDocUnresolvedReferenceInspection;
 
 public abstract class AbstractSharedK1KDocHighlightingTest extends AbstractHighlightingTest {
     @Override
