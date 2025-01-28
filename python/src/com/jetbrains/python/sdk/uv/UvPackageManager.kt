4d5f00202108bd22fab4879ae79ef6b211a0ef95
Aleksandr Sorotskii <aleksandr.sorotskii@jetbrains.com>
2025-01-28 19:20:10 +0100

    separate uv package/dependency management interface. fix uv additional data mismatch warning.
    GitOrigin-RevId: f04258f12c4f342a41ee473b9f79c17c12edae3f

--- python/src/com/jetbrains/python/sdk/uv/UvPackageManager.kt
+++ python/src/com/jetbrains/python/sdk/uv/UvPackageManager.kt
@@ -14,7 +14,7 @@ import com.jetbrains.python.sdk.uv.impl.createUvCli
 import com.jetbrains.python.sdk.uv.impl.createUvLowLevel
 import java.nio.file.Path
 
-internal class UvPackageManager(project: Project, sdk: Sdk, val uv: UvLowLevel) : PythonPackageManager(project, sdk) {
+internal class UvPackageManager(project: Project, sdk: Sdk, private val uv: UvLowLevel) : PythonPackageManager(project, sdk) {
   override var installedPackages: List<PythonPackage> = emptyList()
   override val repositoryManager: PythonRepositoryManager = PipRepositoryManager(project, sdk)
 
@@ -22,7 +22,14 @@ internal class UvPackageManager(project: Project, sdk: Sdk, val uv: UvLowLevel) 
   var outdatedPackages: Map<String, PythonOutdatedPackage> = emptyMap()
 
   override suspend fun installPackageCommand(specification: PythonPackageSpecification, options: List<String>): Result<String> {
-    uv.installPackage(specification, options, (sdk.sdkAdditionalData as? UvSdkAdditionalData)?.usePip ?: false).getOrElse {
+    val result = if (sdk.uvUsePackageManagement) {
+      uv.installPackage(specification, emptyList())
+    }
+    else {
+      uv.addDependency(specification, emptyList())
+    }
+
+    result.getOrElse {
       return Result.failure(it)
     }
 
@@ -31,7 +38,7 @@ internal class UvPackageManager(project: Project, sdk: Sdk, val uv: UvLowLevel) 
   }
 
   override suspend fun updatePackageCommand(specification: PythonPackageSpecification): Result<String> {
-    uv.installPackage(specification, emptyList(), (sdk.sdkAdditionalData as? UvSdkAdditionalData)?.usePip ?: false).getOrElse {
+    installPackageCommand(specification, emptyList()).getOrElse {
       return Result.failure(it)
     }
 
@@ -40,7 +47,14 @@ internal class UvPackageManager(project: Project, sdk: Sdk, val uv: UvLowLevel) 
   }
 
   override suspend fun uninstallPackageCommand(pkg: PythonPackage): Result<String> {
-    uv.uninstallPackage(pkg, (sdk.sdkAdditionalData as? UvSdkAdditionalData)?.usePip ?: false).getOrElse {
+    val result = if (sdk.uvUsePackageManagement) {
+      uv.uninstallPackage(pkg)
+    }
+    else {
+      uv.removeDependency(pkg)
+    }
+
+    result.getOrElse {
       return Result.failure(it)
     }
 
@@ -64,7 +78,7 @@ class UvPackageManagerProvider : PythonPackageManagerProvider {
       return null
     }
 
-    val uvWorkingDirectory = (sdk.sdkAdditionalData as? UvSdkAdditionalData)?.uvWorkingDirectory ?: Path.of(project.basePath!!)
+    val uvWorkingDirectory = (sdk.sdkAdditionalData as UvSdkAdditionalData).uvWorkingDirectory ?: Path.of(project.basePath!!)
     val uv = createUvLowLevel(uvWorkingDirectory, createUvCli())
     return UvPackageManager(project, sdk, uv)
   }
--- python/src/com/jetbrains/python/sdk/uv/UvSdkFlavorAndData.kt
+++ python/src/com/jetbrains/python/sdk/uv/UvSdkFlavorAndData.kt
@@ -29,8 +29,15 @@ class UvSdkAdditionalData : PythonSdkAdditionalData {
   override fun save(element: Element) {
     super.save(element)
     element.setAttribute(IS_UV, "true")
-    element.setAttribute(UV_WORKING_DIR, uvWorkingDirectory?.pathString ?: "")
-    element.setAttribute(USE_PIP, usePip.toString())
+
+    // keep backward compatibility with old data
+    if (uvWorkingDirectory?.pathString?.isNotBlank() == true) {
+      element.setAttribute(UV_WORKING_DIR, uvWorkingDirectory.pathString)
+    }
+
+    if (usePip) {
+      element.setAttribute(USE_PIP, usePip.toString())
+    }
   }
 
   companion object {
