2830a79f8379374e9b5512fb157e08c12cf86e02
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-01-29 05:25:23 +0100

    [debugger] Convert XJumpToSourceActionBase to Kotlin
    GitOrigin-RevId: b9def1ff0721b1883431fefadd7fec0c5158366b

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/tree/actions/XJumpToSourceActionBase.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/tree/actions/XJumpToSourceActionBase.kt
@@ -1,37 +1,35 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.xdebugger.impl.ui.tree.actions;
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.xdebugger.impl.ui.tree.actions
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.AppUIExecutor;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.util.registry.Registry;
-import com.intellij.xdebugger.frame.XNavigatable;
-import com.intellij.xdebugger.frame.XValue;
-import com.intellij.xdebugger.impl.evaluate.XDebuggerEvaluationDialog;
-import com.intellij.xdebugger.impl.ui.tree.nodes.XValueNodeImpl;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.AppUIExecutor
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.util.registry.Registry.Companion.`is`
+import com.intellij.xdebugger.XSourcePosition
+import com.intellij.xdebugger.frame.XNavigatable
+import com.intellij.xdebugger.frame.XValue
+import com.intellij.xdebugger.impl.evaluate.XDebuggerEvaluationDialog
+import com.intellij.xdebugger.impl.ui.tree.nodes.XValueNodeImpl
+import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
-public abstract class XJumpToSourceActionBase extends XDebuggerTreeActionBase {
-  @Override
-  protected void perform(final XValueNodeImpl node, final @NotNull String nodeName, final AnActionEvent e) {
-    XValue value = node.getValueContainer();
-    final XDebuggerEvaluationDialog dialog = e.getData(XDebuggerEvaluationDialog.KEY);
-    XNavigatable navigatable = sourcePosition -> {
+abstract class XJumpToSourceActionBase : XDebuggerTreeActionBase() {
+  override fun perform(node: XValueNodeImpl, nodeName: String, e: AnActionEvent) {
+    val value = node.valueContainer
+    val dialog = e.getData<XDebuggerEvaluationDialog?>(XDebuggerEvaluationDialog.KEY)
+    val navigatable = XNavigatable { sourcePosition: XSourcePosition? ->
       if (sourcePosition != null) {
-        final Project project = node.getTree().getProject();
-        AppUIExecutor.onUiThread().expireWith(project).submit(() -> {
-          sourcePosition.createNavigatable(project).navigate(true);
-          if (dialog != null && Registry.is("debugger.close.dialog.on.navigate")) {
-            dialog.close(DialogWrapper.CANCEL_EXIT_CODE);
+        val project = node.tree.project
+        AppUIExecutor.onUiThread().expireWith(project).submit(Runnable {
+          sourcePosition.createNavigatable(project).navigate(true)
+          if (dialog != null && `is`("debugger.close.dialog.on.navigate")) {
+            dialog.close(DialogWrapper.CANCEL_EXIT_CODE)
           }
-        });
+        })
       }
-    };
-    startComputingSourcePosition(value, navigatable);
+    }
+    startComputingSourcePosition(value, navigatable)
   }
 
-  protected abstract void startComputingSourcePosition(XValue value, XNavigatable navigatable);
+  protected abstract fun startComputingSourcePosition(value: XValue?, navigatable: XNavigatable?)
 }
