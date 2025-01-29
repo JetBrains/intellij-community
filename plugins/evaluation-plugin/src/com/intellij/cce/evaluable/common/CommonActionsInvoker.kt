6cafd1d7661cffd951f40a3d42b5b2b240928962
Alexandr Suhinin <alexandr.suhinin@jetbrains.com>
2025-01-29 09:34:13 +0200

    [code generation eval] LLM-13484: move caret action should reset editor selection
    GitOrigin-RevId: e37046e08c975636236d0848ebfda5de6bfd35fe

--- plugins/evaluation-plugin/src/com/intellij/cce/evaluable/common/CommonActionsInvoker.kt
+++ plugins/evaluation-plugin/src/com/intellij/cce/evaluable/common/CommonActionsInvoker.kt
@@ -36,6 +36,7 @@ class CommonActionsInvoker(private val project: Project) : ActionsInvoker {
 
   override fun moveCaret(offset: Int) = onEdt {
     val editor = getEditorSafe(project)
+    editor.selectionModel.removeSelection()
     editor.caretModel.moveToOffset(offset)
     editor.scrollingModel.scrollToCaret(ScrollType.MAKE_VISIBLE)
   }
