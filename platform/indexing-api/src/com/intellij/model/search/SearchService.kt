32ea747ed5d3db971bc4c71a607db9279f0b8ecb
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2025-01-29 13:12:09 +0100

    IJPL-175872 convert `SearchService` to kt for better typing
    GitOrigin-RevId: 9b4fb7c800209b8019fcb8b75f3c64f1c838847b

--- platform/indexing-api/src/com/intellij/model/search/SearchService.kt
+++ platform/indexing-api/src/com/intellij/model/search/SearchService.kt
@@ -1,35 +1,35 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.model.search;
+package com.intellij.model.search
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.util.Query;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.intellij.util.Query
 
 /**
  * Service for building search queries.
  */
-public interface SearchService {
-
-  static @NotNull SearchService getInstance() {
-    return ApplicationManager.getApplication().getService(SearchService.class);
-  }
+interface SearchService {
 
   /**
    * Creates a query which doesn't perform any search on itw own,
-   * and instead collects search requests from {@linkplain Searcher searchers}.
+   * and instead collects search requests from [searchers][Searcher].
    */
-  <T> @NotNull Query<T> searchParameters(@NotNull SearchParameters<T> parameters);
+  fun <T> searchParameters(parameters: SearchParameters<T>): Query<T>
 
   /**
    * Creates new builder of text occurrences query.
    */
-  @NotNull SearchWordQueryBuilder searchWord(@NotNull Project project, @NotNull String word);
+  fun searchWord(project: Project, word: String): SearchWordQueryBuilder
 
   /**
    * Merges a list of queries into a single query.
    */
-  <T> @NotNull Query<? extends T> merge(@NotNull List<? extends @NotNull Query<? extends T>> queries);
+  fun <T> merge(queries: List<Query<out T>>): Query<out T>
+
+  companion object {
+
+    @JvmStatic
+    fun getInstance(): SearchService = ApplicationManager.getApplication().service()
+  }
 }
