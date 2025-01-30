0b3c04b70a5ca12dd8462b546d5f35a1319f7990
Louis Vignier <Louis.Vignier@jetbrains.com>
2025-01-30 23:33:27 +0100

    [jdk] Do not warn about JDK arch for CpuArch.OTHER
    #IDEA-366540 Fixed
    
    GitOrigin-RevId: 3bfb31597558a6b4ad59da49db3e730002a3cc2b

--- java/compiler/impl/src/com/intellij/compiler/server/BuildManagerVersionChecker.kt
+++ java/compiler/impl/src/com/intellij/compiler/server/BuildManagerVersionChecker.kt
@@ -36,7 +36,8 @@ internal class BuildManagerVersionChecker(val project: Project, val scope: Corou
 
       val jdkArch = versionInfo.arch ?: return@launch
 
-      if (jdkArch != CpuArch.CURRENT) {
+      if (jdkArch in listOf(CpuArch.X86, CpuArch.X86_64) && CpuArch.CURRENT in listOf(CpuArch.ARM32, CpuArch.ARM64)
+          || jdkArch in listOf(CpuArch.ARM32, CpuArch.ARM64) && CpuArch.CURRENT in listOf(CpuArch.X86, CpuArch.X86_64)) {
         NotificationGroupManager.getInstance()
           .getNotificationGroup("JDK Arch Check")
           .createNotification(
