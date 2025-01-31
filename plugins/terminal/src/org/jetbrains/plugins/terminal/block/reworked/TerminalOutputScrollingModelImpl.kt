7022ac572189e56d7f3178423324938f1945b966
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-01-31 10:18:50 +0200

    [terminal] IJPL-176464 Scroll terminal down till the last non-blank line or cursor
    Previously, we took into account only the cursor position, but there might be a case when some meaningful output appears below the cursor position. For example, completion output in Zsh.
    So, I had to adjust the logic to scroll to the last non-blank line or cursor position, depending on what is lower.
    Also, I had to add the listener that updates the scroll position on document updates, because in case of completion cursor doesn't move, so we don't update the scroll.
    
    GitOrigin-RevId: f33682a253bf42cddfe7de25e6cb459422733267

--- plugins/terminal/src/org/jetbrains/plugins/terminal/block/reworked/TerminalOutputScrollingModelImpl.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/block/reworked/TerminalOutputScrollingModelImpl.kt
@@ -5,6 +5,8 @@ import com.intellij.openapi.application.EDT
 import com.intellij.openapi.editor.event.VisibleAreaEvent
 import com.intellij.openapi.editor.event.VisibleAreaListener
 import com.intellij.openapi.editor.ex.EditorEx
+import com.intellij.openapi.editor.impl.EditorImpl
+import com.intellij.openapi.editor.impl.view.VisualLinesIterator
 import com.intellij.util.asDisposable
 import com.intellij.util.concurrency.annotations.RequiresEdt
 import com.intellij.util.ui.JBUI
@@ -16,7 +18,8 @@ import kotlin.math.max
 
 /**
  * Manages the vertical scroll offset of the terminal output:
- * 1. Adjusts it to follow the cursor offset if the user does not modify scrolling position manually.
+ * 1. Adjusts it to follow the cursor offset or last non-blank line (depending on what is located lower)
+ * if the user does not modify scrolling position manually.
  * 2. Provides an ability to scroll to cursor forcefully: [scrollToCursor]
  *
  * Lifecycle is bound to the provided Coroutine Scope.
@@ -36,6 +39,17 @@ internal class TerminalOutputScrollingModelImpl(
         }
       }
     }
+
+    outputModel.addListener(coroutineScope.asDisposable(), object : TerminalOutputModelListener {
+      override fun afterContentChanged(startOffset: Int) {
+        if (shouldScrollToCursor) {
+          // We already called in an EDT, but let's update the scroll later to not block output model updates.
+          coroutineScope.launch(Dispatchers.EDT) {
+            scrollToCursor(outputModel.cursorOffsetState.value)
+          }
+        }
+      }
+    })
 
     editor.scrollingModel.addVisibleAreaListener(object : VisibleAreaListener {
       override fun visibleAreaChanged(e: VisibleAreaEvent) {
@@ -68,11 +82,11 @@ internal class TerminalOutputScrollingModelImpl(
    * The visible area of the terminal output is bound to the screen area - the last output lines that fit into the screen height.
    * But given that we have blocks with additional vertical insets, the same number of lines may require more height than we have.
    * The terminal should show the first screen line at the top of the viewport.
-   * But if we follow this rule, the line with cursor can occur below the viewport bounds
+   * But if we follow this rule, the line with cursor or just the last non-blank line can occur below the viewport bounds
    * because actual height is increased by the block insets.
    *
    * So, this method is trying to adjust the scroll offset to put the first line of the screen to the top of the viewport.
-   * But if the cursor becomes out of viewport, we increase the offset to make the cursor visible.
+   * But if the cursor or last non-blank line becomes out of viewport, we increase the offset to make them visible.
    */
   private fun scrollToCursor(offset: Int) {
     val screenRows = editor.calculateTerminalSize()?.rows ?: return
@@ -83,16 +97,39 @@ internal class TerminalOutputScrollingModelImpl(
     val topInset = JBUI.scale(TerminalUi.blockTopInset)
     val bottomInset = JBUI.scale(TerminalUi.blockBottomInset)
 
+    val lastNotBlankVisualLine = findLastNotBlankVisualLine(screenTopVisualLine)
+    val lastNotBlankLineBottomY = editor.visualLineToY(lastNotBlankVisualLine) + editor.lineHeight + bottomInset
     val cursorBottomY = editor.offsetToXY(offset).y + editor.lineHeight + bottomInset
+    val screenBottomY = max(lastNotBlankLineBottomY, cursorBottomY)
+
     val screenTopY = editor.visualLineToY(screenTopVisualLine) - topInset
     val screenHeight = editor.scrollingModel.visibleArea.height
 
-    val scrollY = max(cursorBottomY - screenHeight, screenTopY)
+    val scrollY = max(screenBottomY - screenHeight, screenTopY)
 
     editor.doTerminalOutputScrollChangingAction {
       editor.doWithoutScrollingAnimation {
         editor.scrollingModel.scrollVertically(scrollY)
       }
     }
+  }
+
+  private fun findLastNotBlankVisualLine(startVisualLine: Int): Int {
+    var lastNotBlankVisualLine = startVisualLine
+
+    // We need to find the last non-blank line, so it would be better to search from the end.
+    // But I didn't find such an option, so let's search from the start.
+    // It should not be a big deal since screen height is usually small.
+    val iterator = VisualLinesIterator(editor as EditorImpl, startVisualLine)
+    while (!iterator.atEnd()) {
+      val startOffset = iterator.visualLineStartOffset
+      val endOffset = iterator.visualLineEndOffset
+      if (editor.document.charsSequence.subSequence(startOffset, endOffset).isNotBlank()) {
+        lastNotBlankVisualLine = iterator.getVisualLine()
+      }
+      iterator.advance()
+    }
+
+    return lastNotBlankVisualLine
   }
 }
