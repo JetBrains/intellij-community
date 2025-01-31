4ec869dbd9d285f66d743f2e246164cd020286dd
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-01-31 10:31:26 +0100

    [threading] IJPL-155995: Run write action on EDT
    GitOrigin-RevId: 691220608482abe5c7c96740fb2c481482817911

--- plugins/devkit/devkit-core/src/projectRoots/IdeaJdk.java
+++ plugins/devkit/devkit-core/src/projectRoots/IdeaJdk.java
@@ -253,7 +253,9 @@ public final class IdeaJdk extends JavaDependentSdkType implements JavaSdkType {
       }
     }
 
-    ApplicationManager.getApplication().runWriteAction(() -> sdkModificator.commitChanges());
+    ApplicationManager.getApplication().invokeAndWait(() -> {
+      ApplicationManager.getApplication().runWriteAction(() -> sdkModificator.commitChanges());
+    });
     return result;
   }
 
