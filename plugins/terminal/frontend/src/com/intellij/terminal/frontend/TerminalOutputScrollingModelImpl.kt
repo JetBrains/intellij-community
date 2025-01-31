89128b9ca2a7c55bfdd8f6be24b20e70baf74274
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-01-31 11:46:25 +0200

    [terminal] IJPL-157242 Fix imports
    GitOrigin-RevId: cf53bd3a2c64682af710383165f6099231e0ff25

--- plugins/terminal/frontend/src/com/intellij/terminal/frontend/TerminalOutputScrollingModelImpl.kt
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/TerminalOutputScrollingModelImpl.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.jetbrains.plugins.terminal.block.reworked.TerminalOutputModel
+import org.jetbrains.plugins.terminal.block.reworked.TerminalOutputModelListener
 import org.jetbrains.plugins.terminal.block.ui.*
 import kotlin.math.max
 
