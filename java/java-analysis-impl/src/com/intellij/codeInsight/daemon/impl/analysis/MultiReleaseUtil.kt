feb5f2c448139e75d66e333b2589262f6dd1c97e
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2025-02-01 13:50:47 +0100

    [maven] IDEA-255308 simplify MultiReleaseUtil
    GitOrigin-RevId: ff2078c0a19ab5a1125debc2cae17ffee4ab94cd

--- java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/MultiReleaseUtil.kt
+++ java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/MultiReleaseUtil.kt
@@ -16,28 +16,15 @@ private val javaVersionPattern: Pattern by lazy { Pattern.compile("java\\d+") }
 
 @Internal
 fun areMainAndAdditionalMultiReleaseModules(mainModule: Module, additionalModule: Module): Boolean {
-  // Maven
-  val project = additionalModule.project
-  val storage = project.workspaceModel.currentSnapshot
-  val additionalModuleName = additionalModule.name
-  val mainModuleName = mainModule.name
-  val additionalModuleExOptions = storage.resolve(ModuleId(additionalModuleName))?.exModuleOptions
-  val mainModuleExOptions = storage.resolve(ModuleId(mainModuleName))?.exModuleOptions
-  if (additionalModuleExOptions?.externalSystem == MAVEN_EXTERNAL_SOURCE_ID
-      && mainModuleExOptions?.externalSystem == MAVEN_EXTERNAL_SOURCE_ID) {
-    val baseModuleName = mainModuleName.substringBeforeLast('.')
-    if (additionalModuleName.startsWith(baseModuleName)
-        && additionalModuleExOptions.externalSystemModuleType == "MAIN_ONLY_ADDITIONAL" // StandardMavenModuleType.MAIN_ONLY_ADDITIONAL
-        && mainModuleExOptions.externalSystemModuleType == "MAIN_ONLY" // StandardMavenModuleType.MAIN_ONLY
-    ) {
-      return true
-    }
+  if (getMainMultiReleaseModule(additionalModule) == mainModule) {
+    return true
   }
 
-  // Gradle
+  // Fallback: Gradle and JPS
+  val mainModuleName = mainModule.name
   if (mainModuleName.endsWith(".$MAIN")) {
     val baseModuleName = mainModuleName.substringBeforeLast(MAIN)
-    return javaVersionPattern.matcher(additionalModuleName.substringAfter(baseModuleName)).matches()
+    return javaVersionPattern.matcher(additionalModule.name.substringAfter(baseModuleName)).matches()
   }
   return false
 }
