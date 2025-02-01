63d3f411db09af3049b4c8cea99ebf323eff876b
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2025-02-01 20:58:43 +0100

    [api] Inlay PresentationFactory is no longer Experimental
    GitOrigin-RevId: 9e36baa0b5d3c13ae24269cdf5023b7c6e5eb07c

--- platform/lang-impl/src/com/intellij/codeInsight/hints/presentation/PresentationFactory.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/hints/presentation/PresentationFactory.kt
@@ -34,10 +34,6 @@ import java.util.*
 import javax.swing.Icon
 import kotlin.math.max
 
-/**
- * Contains non-stable and not well-designed API. Will be changed in 2020.2
- */
-@ApiStatus.Experimental
 class PresentationFactory(private val editor: Editor) : InlayPresentationFactory {
   private val textMetricsStorage = InlayHintsUtils.getTextMetricStorage(editor)
   private val offsetFromTopProvider = object : InsetValueProvider {
@@ -71,7 +67,6 @@ class PresentationFactory(private val editor: Editor) : InlayPresentationFactory
     return MouseHandlingPresentation(base, clickListener, hoverListener)
   }
 
-  @ApiStatus.Experimental
   @Contract(pure = true)
   fun textSpacePlaceholder(length: Int, isSmall: Boolean) : InlayPresentation {
     return TextPlaceholderPresentation(length, textMetricsStorage, isSmall)
@@ -178,7 +173,7 @@ class PresentationFactory(private val editor: Editor) : InlayPresentationFactory
 
   /**
    * Creates node, that can be collapsed/expanded by clicking on prefix/suffix.
-   * If presentation is collapsed, clicking to content will expand it.
+   * If presentation is collapsed, clicking on content will expand it.
    */
   @Contract(pure = true)
   fun collapsible(
@@ -219,7 +214,7 @@ class PresentationFactory(private val editor: Editor) : InlayPresentationFactory
   }
 
   /**
-   * On hover of any of [presentations] changes all the presentations with a given decorator.
+   * On hover of [presentations] changes all the presentations with a given decorator.
    * This presentation is stateless.
    */
   @Contract(pure = true)
@@ -495,7 +490,6 @@ class PresentationFactory(private val editor: Editor) : InlayPresentationFactory
       CommandProcessor.getInstance().executeCommand(target.project, { target.navigate(true) }, null, null)
     }
   }
-
 
   companion object {
     var customToStringProvider: ((PsiElement) -> String)? = null
