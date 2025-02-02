0627fa7b16a28b49f23122226d73e46f2c859ed0
Alexey Kryuchkov <alexey.kryuchkov@jetbrains.com>
2025-02-03 01:08:37 +0400

    Implementation of CloseLookupCommand
    (cherry picked from commit c9ee249937308b645582a3f96c4ec0e1cd9d2cb8)
    
    IJ-MR-154263
    
    GitOrigin-RevId: fb6039ec7516f83c62eaece18ac3a40c3d6e6949

---
+++ plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/commands/CloseLookupCommand.kt
@@ -0,0 +1,42 @@
+package com.jetbrains.performancePlugin.commands
+
+import com.intellij.codeInsight.lookup.LookupManager
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.playback.PlaybackContext
+import com.intellij.openapi.ui.playback.commands.PlaybackCommandCoroutineAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
+class CloseLookupCommand(text: String, line: Int) : PlaybackCommandCoroutineAdapter(text, line) {
+
+  companion object {
+    const val PREFIX: String = "${CMD_PREFIX}closeLookup"
+
+    /**
+     * Closes the currently active lookup in the selected text editor, if one exists.
+     *
+     * @return A boolean value indicating whether the active lookup was successfully closed.
+     *         Returns `true` if an active lookup was found and closed; `false` otherwise.
+     */
+    @JvmStatic
+    suspend fun closeLookup(project: Project): Boolean =
+      FileEditorManager.getInstance(project).getSelectedTextEditor().let { editor ->
+        return withContext(Dispatchers.EDT) {
+          val activeLookup = LookupManager.getActiveLookup(editor)
+          if (activeLookup != null) {
+            activeLookup.hideLookup(true)
+            return@withContext true
+          }
+          return@withContext false
+        }
+      }
+  }
+
+  override suspend fun doExecute(context: PlaybackContext) {
+    while (closeLookup(context.project)) delay(1000)
+  }
+
+}
