413797404af172baf05d99e5e6f394d9d7d3876a
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2025-02-03 15:48:18 +0100

    Revert "[kotlin] do not go into stubs in `K2ScriptingKaModuleFactory` for checking for scripts"
    This reverts commit 0e59efd463f110f458e9e43fb48dd056ef2c453f.
    
    ^KTIJ-32922 fixed
    ^KTIJ-32912 open
    
    GitOrigin-RevId: c47c96cec4b3c1055cbff3cd594893fca6562444

--- plugins/kotlin/base/fir/scripting/src/org/jetbrains/kotlin/base/fir/scripting/projectStructure/K2ScriptingKaModuleFactory.kt
+++ plugins/kotlin/base/fir/scripting/src/org/jetbrains/kotlin/base/fir/scripting/projectStructure/K2ScriptingKaModuleFactory.kt
@@ -2,41 +2,27 @@
 package org.jetbrains.kotlin.base.fir.scripting.projectStructure
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.platform.workspace.jps.entities.LibraryEntity
 import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaLibraryModule
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
 import org.jetbrains.kotlin.base.fir.scripting.projectStructure.modules.KaScriptDependencyLibraryModuleImpl
 import org.jetbrains.kotlin.base.fir.scripting.projectStructure.modules.KaScriptModuleImpl
-import org.jetbrains.kotlin.idea.KotlinFileType
 import org.jetbrains.kotlin.idea.base.fir.projectStructure.K2KaModuleFactory
 import org.jetbrains.kotlin.idea.core.script.KotlinScriptEntitySource
 import org.jetbrains.kotlin.psi.KtFile
-
 
 internal class K2ScriptingKaModuleFactory : K2KaModuleFactory {
     override fun createKaModuleByPsiFile(file: PsiFile): KaModule? {
         val ktFile = file as? KtFile ?: return null
 
-        if (ktFile.kotlinParserWillCreateKtScriptHere()) {
+        if (ktFile.isScript()) {
             val project = file.project
             val virtualFile = file.originalFile.virtualFile
             return KaScriptModuleImpl(project, virtualFile)
         }
 
         return null
-    }
-
-    /**
-     * From https://github.com/JetBrains/kotlin/blob/b4b1c7cd698c1e8276a0bed504f22b93582d4f2e/compiler/psi/src/org/jetbrains/kotlin/parsing/KotlinParser.java#L46
-     *
-     * to avoid accessing stubs inside
-     */
-    private fun KtFile.kotlinParserWillCreateKtScriptHere(): Boolean {
-        val extension = FileUtilRt.getExtension(name)
-        val isRegularKtFile = extension.isEmpty() || extension == KotlinFileType.EXTENSION || isCompiled
-        return !isRegularKtFile
     }
 
     override fun createSpecialLibraryModule(libraryEntity: LibraryEntity, project: Project): KaLibraryModule? {
