a8a6244f563d6bccb4260fb4305cf3f0d6a13345
Victor Turansky <victor.turansky@jetbrains.com>
2025-02-04 16:18:25 +0200

    WEB-68929 Convert `EmbeddedHtmlContentElementType` to Kotlin
    GitOrigin-RevId: 1408d8f230237f059e4738cd7446ca85bd233045

--- xml/xml-psi-impl/src/com/intellij/psi/xml/EmbeddedHtmlContentElementType.kt
+++ xml/xml-psi-impl/src/com/intellij/psi/xml/EmbeddedHtmlContentElementType.kt
@@ -1,30 +1,25 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.psi.xml;
+package com.intellij.psi.xml
 
-import com.intellij.lang.LighterASTNode;
-import com.intellij.lang.LighterLazyParseableNode;
-import com.intellij.lang.PsiBuilder;
-import com.intellij.lang.PsiBuilderFactory;
-import com.intellij.lang.html.HTMLLanguage;
-import com.intellij.lang.html.HTMLParser;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.tree.ILazyParseableElementType;
-import com.intellij.psi.tree.ILightLazyParseableElementType;
-import com.intellij.util.diff.FlyweightCapableTreeStructure;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.lang.LighterASTNode
+import com.intellij.lang.LighterLazyParseableNode
+import com.intellij.lang.PsiBuilderFactory
+import com.intellij.lang.html.HTMLLanguage
+import com.intellij.lang.html.HTMLParser
+import com.intellij.psi.tree.ILazyParseableElementType
+import com.intellij.psi.tree.ILightLazyParseableElementType
+import com.intellij.util.diff.FlyweightCapableTreeStructure
 
-class EmbeddedHtmlContentElementType extends ILazyParseableElementType implements ILightLazyParseableElementType {
-  public EmbeddedHtmlContentElementType() {
-    super("HTML_EMBEDDED_CONTENT", HTMLLanguage.INSTANCE);
-  }
+internal class EmbeddedHtmlContentElementType :
+  ILazyParseableElementType("HTML_EMBEDDED_CONTENT", HTMLLanguage.INSTANCE),
+  ILightLazyParseableElementType {
 
-  @Override
-  public @NotNull FlyweightCapableTreeStructure<LighterASTNode> parseContents(@NotNull LighterLazyParseableNode chameleon) {
-    final PsiFile file = chameleon.getContainingFile();
-    assert file != null : chameleon;
-
-    final PsiBuilder builder = PsiBuilderFactory.getInstance().createBuilder(file.getProject(), chameleon);
-    new HTMLParser().parseWithoutBuildingTree(XmlElementType.HTML_FILE, builder);
-    return builder.getLightTree();
+  override fun parseContents(
+    chameleon: LighterLazyParseableNode,
+  ): FlyweightCapableTreeStructure<LighterASTNode> {
+    val file = checkNotNull(chameleon.containingFile) { chameleon }
+    val builder = PsiBuilderFactory.getInstance().createBuilder(file.project, chameleon)
+    HTMLParser().parseWithoutBuildingTree(XmlElementType.HTML_FILE, builder)
+    return builder.lightTree
   }
 }
