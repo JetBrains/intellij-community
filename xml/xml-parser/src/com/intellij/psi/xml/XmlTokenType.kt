d67a93d4a3ac8288fc5b83e1bbac970ffcc5cae2
Victor Turansky <victor.turansky@jetbrains.com>
2025-02-05 00:38:08 +0200

    WEB-68929 Convert `XmlTokenType` to Kotlin
    GitOrigin-RevId: 02b93e0c836db9067a6155cc8d8837053ba85408

--- xml/xml-parser/src/com/intellij/psi/xml/XmlTokenType.kt
+++ xml/xml-parser/src/com/intellij/psi/xml/XmlTokenType.kt
@@ -13,93 +13,274 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intellij.psi.xml;
+package com.intellij.psi.xml
 
-import com.intellij.psi.TokenType;
-import com.intellij.psi.tree.IElementType;
-import com.intellij.psi.tree.TokenSet;
-import com.intellij.psi.tree.xml.IXmlLeafElementType;
+import com.intellij.psi.TokenType
+import com.intellij.psi.tree.IElementType
+import com.intellij.psi.tree.TokenSet
+import com.intellij.psi.tree.xml.IXmlLeafElementType
 
 /**
  * Specifies XML token types.
  */
-public interface XmlTokenType {
-  IElementType XML_START_TAG_START = new IXmlLeafElementType("XML_START_TAG_START");
-  IElementType XML_END_TAG_START = new IXmlLeafElementType("XML_END_TAG_START");
-  IElementType XML_TAG_END = new IXmlLeafElementType("XML_TAG_END");
-  IElementType XML_EMPTY_ELEMENT_END = new IXmlLeafElementType("XML_EMPTY_ELEMENT_END");
-  IElementType XML_TAG_NAME = new IXmlLeafElementType("XML_TAG_NAME");
-  IElementType XML_NAME = new IXmlLeafElementType("XML_NAME");
-  IElementType XML_ATTRIBUTE_VALUE_TOKEN = new IXmlLeafElementType("XML_ATTRIBUTE_VALUE_TOKEN");
-  IElementType XML_ATTRIBUTE_VALUE_START_DELIMITER = new IXmlLeafElementType("XML_ATTRIBUTE_VALUE_START_DELIMITER");
-  IElementType XML_ATTRIBUTE_VALUE_END_DELIMITER = new IXmlLeafElementType("XML_ATTRIBUTE_VALUE_END_DELIMITER");
-  IElementType XML_EQ = new IXmlLeafElementType("XML_EQ");
-  IElementType XML_DATA_CHARACTERS = new IXmlLeafElementType("XML_DATA_CHARACTERS");
-  IElementType XML_TAG_CHARACTERS = new IXmlLeafElementType("XML_TAG_CHARACTERS");
-  IElementType XML_WHITE_SPACE = TokenType.WHITE_SPACE;
-  IElementType XML_REAL_WHITE_SPACE = new IXmlLeafElementType("XML_WHITE_SPACE");
-  IElementType XML_COMMENT_START = new IXmlLeafElementType("XML_COMMENT_START");
-  IElementType XML_COMMENT_END = new IXmlLeafElementType("XML_COMMENT_END");
-  IElementType XML_COMMENT_CHARACTERS = new IXmlLeafElementType("XML_COMMENT_CHARACTERS");
+object XmlTokenType {
+  @JvmField
+  val XML_START_TAG_START: IElementType =
+    IXmlLeafElementType("XML_START_TAG_START")
 
-  IElementType XML_DECL_START = new IXmlLeafElementType("XML_DECL_START");
-  IElementType XML_DECL_END = new IXmlLeafElementType("XML_DECL_END");
+  @JvmField
+  val XML_END_TAG_START: IElementType =
+    IXmlLeafElementType("XML_END_TAG_START")
 
-  IElementType XML_DOCTYPE_START = new IXmlLeafElementType("XML_DOCTYPE_START");
-  IElementType XML_DOCTYPE_END = new IXmlLeafElementType("XML_DOCTYPE_END");
-  IElementType XML_DOCTYPE_SYSTEM = new IXmlLeafElementType("XML_DOCTYPE_SYSTEM");
-  IElementType XML_DOCTYPE_PUBLIC = new IXmlLeafElementType("XML_DOCTYPE_PUBLIC");
+  @JvmField
+  val XML_TAG_END: IElementType =
+    IXmlLeafElementType("XML_TAG_END")
 
-  IElementType XML_MARKUP_START = new IXmlLeafElementType("XML_MARKUP_START");
-  IElementType XML_MARKUP_END = new IXmlLeafElementType("XML_MARKUP_END");
+  @JvmField
+  val XML_EMPTY_ELEMENT_END: IElementType =
+    IXmlLeafElementType("XML_EMPTY_ELEMENT_END")
 
-  IElementType XML_CDATA_START = new IXmlLeafElementType("XML_CDATA_START");
-  IElementType XML_CONDITIONAL_SECTION_START = new IXmlLeafElementType("XML_CONDITIONAL_SECTION_START");
-  IElementType XML_CONDITIONAL_INCLUDE = new IXmlLeafElementType("XML_CONDITIONAL_INCLUDE");
-  IElementType XML_CONDITIONAL_IGNORE = new IXmlLeafElementType("XML_CONDITIONAL_IGNORE");
-  IElementType XML_CDATA_END = new IXmlLeafElementType("XML_CDATA_END");
-  IElementType XML_CONDITIONAL_SECTION_END = new IXmlLeafElementType("XML_CONDITIONAL_SECTION_END");
+  @JvmField
+  val XML_TAG_NAME: IElementType =
+    IXmlLeafElementType("XML_TAG_NAME")
 
-  IElementType XML_ELEMENT_DECL_START = new IXmlLeafElementType("XML_ELEMENT_DECL_START");
-  IElementType XML_NOTATION_DECL_START = new IXmlLeafElementType("XML_NOTATION_DECL_START");
-  IElementType XML_ATTLIST_DECL_START = new IXmlLeafElementType("XML_ATTLIST_DECL_START");
-  IElementType XML_ENTITY_DECL_START = new IXmlLeafElementType("XML_ENTITY_DECL_START");
+  @JvmField
+  val XML_NAME: IElementType =
+    IXmlLeafElementType("XML_NAME")
 
-  IElementType XML_PCDATA = new IXmlLeafElementType("XML_PCDATA");
-  IElementType XML_LEFT_PAREN = new IXmlLeafElementType("XML_LEFT_PAREN");
-  IElementType XML_RIGHT_PAREN = new IXmlLeafElementType("XML_RIGHT_PAREN");
-  IElementType XML_CONTENT_EMPTY = new IXmlLeafElementType("XML_CONTENT_EMPTY");
-  IElementType XML_CONTENT_ANY = new IXmlLeafElementType("XML_CONTENT_ANY");
-  IElementType XML_QUESTION = new IXmlLeafElementType("XML_QUESTION");
-  IElementType XML_STAR = new IXmlLeafElementType("XML_STAR");
-  IElementType XML_PLUS = new IXmlLeafElementType("XML_PLUS");
-  IElementType XML_BAR = new IXmlLeafElementType("XML_BAR");
-  IElementType XML_COMMA = new IXmlLeafElementType("XML_COMMA");
-  IElementType XML_AMP = new IXmlLeafElementType("XML_AMP");
-  IElementType XML_SEMI = new IXmlLeafElementType("XML_SEMI");
-  IElementType XML_PERCENT = new IXmlLeafElementType("XML_PERCENT");
+  @JvmField
+  val XML_ATTRIBUTE_VALUE_TOKEN: IElementType =
+    IXmlLeafElementType("XML_ATTRIBUTE_VALUE_TOKEN")
 
-  IElementType XML_ATT_IMPLIED = new IXmlLeafElementType("XML_ATT_IMPLIED");
-  IElementType XML_ATT_REQUIRED = new IXmlLeafElementType("XML_ATT_REQUIRED");
-  IElementType XML_ATT_FIXED = new IXmlLeafElementType("XML_ATT_FIXED");
+  @JvmField
+  val XML_ATTRIBUTE_VALUE_START_DELIMITER: IElementType =
+    IXmlLeafElementType("XML_ATTRIBUTE_VALUE_START_DELIMITER")
 
-  IElementType XML_ENTITY_REF_TOKEN = new IXmlLeafElementType("XML_ENTITY_REF_TOKEN");
+  @JvmField
+  val XML_ATTRIBUTE_VALUE_END_DELIMITER: IElementType =
+    IXmlLeafElementType("XML_ATTRIBUTE_VALUE_END_DELIMITER")
 
-  IElementType TAG_WHITE_SPACE = new IXmlLeafElementType("TAG_WHITE_SPACE");
+  @JvmField
+  val XML_EQ: IElementType =
+    IXmlLeafElementType("XML_EQ")
 
-  IElementType XML_PI_START = new IXmlLeafElementType("XML_PI_START");
-  IElementType XML_PI_END = new IXmlLeafElementType("XML_PI_END");
-  IElementType XML_PI_TARGET = new IXmlLeafElementType("XML_PI_TARGET");
+  @JvmField
+  val XML_DATA_CHARACTERS: IElementType =
+    IXmlLeafElementType("XML_DATA_CHARACTERS")
 
-  IElementType XML_CHAR_ENTITY_REF = new IXmlLeafElementType("XML_CHAR_ENTITY_REF");
+  @JvmField
+  val XML_TAG_CHARACTERS: IElementType =
+    IXmlLeafElementType("XML_TAG_CHARACTERS")
 
-  IElementType XML_BAD_CHARACTER = new IXmlLeafElementType("XML_BAD_CHARACTER");
+  @JvmField
+  val XML_WHITE_SPACE: IElementType =
+    TokenType.WHITE_SPACE
 
-  IElementType XML_CONDITIONAL_COMMENT_START = new IXmlLeafElementType("CONDITIONAL_COMMENT_START");
-  IElementType XML_CONDITIONAL_COMMENT_START_END = new IXmlLeafElementType("CONDITIONAL_COMMENT_START_END");
-  IElementType XML_CONDITIONAL_COMMENT_END_START = new IXmlLeafElementType("CONDITIONAL_COMMENT_END_START");
-  IElementType XML_CONDITIONAL_COMMENT_END = new IXmlLeafElementType("CONDITIONAL_COMMENT_END");
+  @JvmField
+  val XML_REAL_WHITE_SPACE: IElementType =
+    IXmlLeafElementType("XML_WHITE_SPACE")
 
-  TokenSet COMMENTS = TokenSet.create(XML_COMMENT_START, XML_COMMENT_CHARACTERS, XML_COMMENT_END);
-  TokenSet WHITESPACES = TokenSet.create(XML_WHITE_SPACE);
+  @JvmField
+  val XML_COMMENT_START: IElementType =
+    IXmlLeafElementType("XML_COMMENT_START")
+
+  @JvmField
+  val XML_COMMENT_END: IElementType =
+    IXmlLeafElementType("XML_COMMENT_END")
+
+  @JvmField
+  val XML_COMMENT_CHARACTERS: IElementType =
+    IXmlLeafElementType("XML_COMMENT_CHARACTERS")
+
+  @JvmField
+  val XML_DECL_START: IElementType =
+    IXmlLeafElementType("XML_DECL_START")
+
+  @JvmField
+  val XML_DECL_END: IElementType =
+    IXmlLeafElementType("XML_DECL_END")
+
+  @JvmField
+  val XML_DOCTYPE_START: IElementType =
+    IXmlLeafElementType("XML_DOCTYPE_START")
+
+  @JvmField
+  val XML_DOCTYPE_END: IElementType =
+    IXmlLeafElementType("XML_DOCTYPE_END")
+
+  @JvmField
+  val XML_DOCTYPE_SYSTEM: IElementType =
+    IXmlLeafElementType("XML_DOCTYPE_SYSTEM")
+
+  @JvmField
+  val XML_DOCTYPE_PUBLIC: IElementType =
+    IXmlLeafElementType("XML_DOCTYPE_PUBLIC")
+
+  @JvmField
+  val XML_MARKUP_START: IElementType =
+    IXmlLeafElementType("XML_MARKUP_START")
+
+  @JvmField
+  val XML_MARKUP_END: IElementType =
+    IXmlLeafElementType("XML_MARKUP_END")
+
+  @JvmField
+  val XML_CDATA_START: IElementType =
+    IXmlLeafElementType("XML_CDATA_START")
+
+  @JvmField
+  val XML_CONDITIONAL_SECTION_START: IElementType =
+    IXmlLeafElementType("XML_CONDITIONAL_SECTION_START")
+
+  @JvmField
+  val XML_CONDITIONAL_INCLUDE: IElementType =
+    IXmlLeafElementType("XML_CONDITIONAL_INCLUDE")
+
+  @JvmField
+  val XML_CONDITIONAL_IGNORE: IElementType =
+    IXmlLeafElementType("XML_CONDITIONAL_IGNORE")
+
+  @JvmField
+  val XML_CDATA_END: IElementType =
+    IXmlLeafElementType("XML_CDATA_END")
+
+  @JvmField
+  val XML_CONDITIONAL_SECTION_END: IElementType =
+    IXmlLeafElementType("XML_CONDITIONAL_SECTION_END")
+
+  @JvmField
+  val XML_ELEMENT_DECL_START: IElementType =
+    IXmlLeafElementType("XML_ELEMENT_DECL_START")
+
+  @JvmField
+  val XML_NOTATION_DECL_START: IElementType =
+    IXmlLeafElementType("XML_NOTATION_DECL_START")
+
+  @JvmField
+  val XML_ATTLIST_DECL_START: IElementType =
+    IXmlLeafElementType("XML_ATTLIST_DECL_START")
+
+  @JvmField
+  val XML_ENTITY_DECL_START: IElementType =
+    IXmlLeafElementType("XML_ENTITY_DECL_START")
+
+  @JvmField
+  val XML_PCDATA: IElementType =
+    IXmlLeafElementType("XML_PCDATA")
+
+  @JvmField
+  val XML_LEFT_PAREN: IElementType =
+    IXmlLeafElementType("XML_LEFT_PAREN")
+
+  @JvmField
+  val XML_RIGHT_PAREN: IElementType =
+    IXmlLeafElementType("XML_RIGHT_PAREN")
+
+  @JvmField
+  val XML_CONTENT_EMPTY: IElementType =
+    IXmlLeafElementType("XML_CONTENT_EMPTY")
+
+  @JvmField
+  val XML_CONTENT_ANY: IElementType =
+    IXmlLeafElementType("XML_CONTENT_ANY")
+
+  @JvmField
+  val XML_QUESTION: IElementType =
+    IXmlLeafElementType("XML_QUESTION")
+
+  @JvmField
+  val XML_STAR: IElementType =
+    IXmlLeafElementType("XML_STAR")
+
+  @JvmField
+  val XML_PLUS: IElementType =
+    IXmlLeafElementType("XML_PLUS")
+
+  @JvmField
+  val XML_BAR: IElementType =
+    IXmlLeafElementType("XML_BAR")
+
+  @JvmField
+  val XML_COMMA: IElementType =
+    IXmlLeafElementType("XML_COMMA")
+
+  @JvmField
+  val XML_AMP: IElementType =
+    IXmlLeafElementType("XML_AMP")
+
+  @JvmField
+  val XML_SEMI: IElementType =
+    IXmlLeafElementType("XML_SEMI")
+
+  @JvmField
+  val XML_PERCENT: IElementType =
+    IXmlLeafElementType("XML_PERCENT")
+
+  @JvmField
+  val XML_ATT_IMPLIED: IElementType =
+    IXmlLeafElementType("XML_ATT_IMPLIED")
+
+  @JvmField
+  val XML_ATT_REQUIRED: IElementType =
+    IXmlLeafElementType("XML_ATT_REQUIRED")
+
+  @JvmField
+  val XML_ATT_FIXED: IElementType =
+    IXmlLeafElementType("XML_ATT_FIXED")
+
+  @JvmField
+  val XML_ENTITY_REF_TOKEN: IElementType =
+    IXmlLeafElementType("XML_ENTITY_REF_TOKEN")
+
+  @JvmField
+  val TAG_WHITE_SPACE: IElementType =
+    IXmlLeafElementType("TAG_WHITE_SPACE")
+
+  @JvmField
+  val XML_PI_START: IElementType =
+    IXmlLeafElementType("XML_PI_START")
+
+  @JvmField
+  val XML_PI_END: IElementType =
+    IXmlLeafElementType("XML_PI_END")
+
+  @JvmField
+  val XML_PI_TARGET: IElementType =
+    IXmlLeafElementType("XML_PI_TARGET")
+
+  @JvmField
+  val XML_CHAR_ENTITY_REF: IElementType =
+    IXmlLeafElementType("XML_CHAR_ENTITY_REF")
+
+  @JvmField
+  val XML_BAD_CHARACTER: IElementType =
+    IXmlLeafElementType("XML_BAD_CHARACTER")
+
+  @JvmField
+  val XML_CONDITIONAL_COMMENT_START: IElementType =
+    IXmlLeafElementType("CONDITIONAL_COMMENT_START")
+
+  @JvmField
+  val XML_CONDITIONAL_COMMENT_START_END: IElementType =
+    IXmlLeafElementType("CONDITIONAL_COMMENT_START_END")
+
+  @JvmField
+  val XML_CONDITIONAL_COMMENT_END_START: IElementType =
+    IXmlLeafElementType("CONDITIONAL_COMMENT_END_START")
+
+  @JvmField
+  val XML_CONDITIONAL_COMMENT_END: IElementType =
+    IXmlLeafElementType("CONDITIONAL_COMMENT_END")
+
+  @JvmField
+  val COMMENTS: TokenSet = TokenSet.create(
+    XML_COMMENT_START,
+    XML_COMMENT_CHARACTERS,
+    XML_COMMENT_END,
+  )
+
+  @JvmField
+  val WHITESPACES: TokenSet = TokenSet.create(
+    XML_WHITE_SPACE,
+  )
 }
