ccd585eeb8f0894454513e6714697de71140e3eb
Victor Turansky <victor.turansky@jetbrains.com>
2025-02-04 15:28:38 +0200

    WEB-68929 Convert `XmlElementType` to Kotlin
    GitOrigin-RevId: 56ff00adeafcb44105a45af00c47b09c69e4883c

--- xml/xml-psi-impl/src/com/intellij/psi/xml/XmlElementType.kt
+++ xml/xml-psi-impl/src/com/intellij/psi/xml/XmlElementType.kt
@@ -1,52 +1,135 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.psi.xml;
+package com.intellij.psi.xml
 
-import com.intellij.html.embedding.HtmlRawTextElementType;
-import com.intellij.lang.dtd.DTDLanguage;
-import com.intellij.lang.html.HTMLParserDefinition;
-import com.intellij.lang.xhtml.XHTMLLanguage;
-import com.intellij.lang.xml.XMLLanguage;
-import com.intellij.psi.tree.IElementType;
-import com.intellij.psi.tree.IFileElementType;
-import com.intellij.psi.tree.xml.IXmlElementType;
+import com.intellij.html.embedding.HtmlRawTextElementType
+import com.intellij.lang.dtd.DTDLanguage
+import com.intellij.lang.html.HTMLParserDefinition
+import com.intellij.lang.xhtml.XHTMLLanguage
+import com.intellij.lang.xml.XMLLanguage
+import com.intellij.psi.tree.IElementType
+import com.intellij.psi.tree.IFileElementType
+import com.intellij.psi.tree.xml.IXmlElementType
 
-public interface XmlElementType {
-  IElementType XML_DOCUMENT = new IXmlElementType("XML_DOCUMENT");
-  IElementType XML_PROLOG = new IXmlElementType("XML_PROLOG");
-  IElementType XML_DECL = new IXmlElementType("XML_DECL");
-  IElementType XML_DOCTYPE = new IXmlElementType("XML_DOCTYPE");
-  IElementType XML_ATTRIBUTE = new XmlAttributeElementType();
-  IElementType XML_COMMENT = new IXmlElementType("XML_COMMENT");
-  IElementType XML_TAG = new XmlTagElementType("XML_TAG");
-  IElementType XML_ELEMENT_DECL = new IXmlElementType("XML_ELEMENT_DECL");
-  IElementType XML_CONDITIONAL_SECTION = new IXmlElementType("XML_CONDITIONAL_SECTION");
+object XmlElementType {
+  @JvmField
+  val XML_DOCUMENT: IElementType =
+    IXmlElementType("XML_DOCUMENT")
 
-  IElementType XML_ATTLIST_DECL = new IXmlElementType("XML_ATTLIST_DECL");
-  IElementType XML_NOTATION_DECL = new IXmlElementType("XML_NOTATION_DECL");
-  IElementType XML_ENTITY_DECL = new IXmlElementType("XML_ENTITY_DECL");
-  IElementType XML_ELEMENT_CONTENT_SPEC = new IXmlElementType("XML_ELEMENT_CONTENT_SPEC");
-  IElementType XML_ELEMENT_CONTENT_GROUP = new IXmlElementType("XML_ELEMENT_CONTENT_GROUP");
-  IElementType XML_ATTRIBUTE_DECL = new IXmlElementType("XML_ATTRIBUTE_DECL");
-  IElementType XML_ATTRIBUTE_VALUE = new IXmlElementType("XML_ATTRIBUTE_VALUE");
-  IElementType XML_ENTITY_REF = new IXmlElementType("XML_ENTITY_REF");
-  IElementType XML_ENUMERATED_TYPE = new IXmlElementType("XML_ENUMERATED_TYPE");
-  IElementType XML_PROCESSING_INSTRUCTION = new IXmlElementType("XML_PROCESSING_INSTRUCTION");
-  IElementType XML_CDATA = new IXmlElementType("XML_CDATA");
+  @JvmField
+  val XML_PROLOG: IElementType =
+    IXmlElementType("XML_PROLOG")
+
+  @JvmField
+  val XML_DECL: IElementType =
+    IXmlElementType("XML_DECL")
+
+  @JvmField
+  val XML_DOCTYPE: IElementType =
+    IXmlElementType("XML_DOCTYPE")
+
+  @JvmField
+  val XML_ATTRIBUTE: IElementType =
+    XmlAttributeElementType()
+
+  @JvmField
+  val XML_COMMENT: IElementType =
+    IXmlElementType("XML_COMMENT")
+
+  @JvmField
+  val XML_TAG: IElementType =
+    XmlTagElementType("XML_TAG")
+
+  @JvmField
+  val XML_ELEMENT_DECL: IElementType =
+    IXmlElementType("XML_ELEMENT_DECL")
+
+  @JvmField
+  val XML_CONDITIONAL_SECTION: IElementType =
+    IXmlElementType("XML_CONDITIONAL_SECTION")
+
+  @JvmField
+  val XML_ATTLIST_DECL: IElementType =
+    IXmlElementType("XML_ATTLIST_DECL")
+
+  @JvmField
+  val XML_NOTATION_DECL: IElementType =
+    IXmlElementType("XML_NOTATION_DECL")
+
+  @JvmField
+  val XML_ENTITY_DECL: IElementType =
+    IXmlElementType("XML_ENTITY_DECL")
+
+  @JvmField
+  val XML_ELEMENT_CONTENT_SPEC: IElementType =
+    IXmlElementType("XML_ELEMENT_CONTENT_SPEC")
+
+  @JvmField
+  val XML_ELEMENT_CONTENT_GROUP: IElementType =
+    IXmlElementType("XML_ELEMENT_CONTENT_GROUP")
+
+  @JvmField
+  val XML_ATTRIBUTE_DECL: IElementType =
+    IXmlElementType("XML_ATTRIBUTE_DECL")
+
+  @JvmField
+  val XML_ATTRIBUTE_VALUE: IElementType =
+    IXmlElementType("XML_ATTRIBUTE_VALUE")
+
+  @JvmField
+  val XML_ENTITY_REF: IElementType =
+    IXmlElementType("XML_ENTITY_REF")
+
+  @JvmField
+  val XML_ENUMERATED_TYPE: IElementType =
+    IXmlElementType("XML_ENUMERATED_TYPE")
+
+  @JvmField
+  val XML_PROCESSING_INSTRUCTION: IElementType =
+    IXmlElementType("XML_PROCESSING_INSTRUCTION")
+
+  @JvmField
+  val XML_CDATA: IElementType =
+    IXmlElementType("XML_CDATA")
 
   //todo: move to html
-  IElementType HTML_DOCUMENT = new IXmlElementType("HTML_DOCUMENT");
-  IElementType HTML_TAG = new XmlTagElementType("HTML_TAG");
-  IFileElementType HTML_FILE = HTMLParserDefinition.FILE_ELEMENT_TYPE;
-  IElementType HTML_EMBEDDED_CONTENT = new EmbeddedHtmlContentElementType();
-  IElementType HTML_RAW_TEXT = HtmlRawTextElementType.INSTANCE;
+  @JvmField
+  val HTML_DOCUMENT: IElementType =
+    IXmlElementType("HTML_DOCUMENT")
 
-  IElementType XML_TEXT = new XmlTextElementType();
+  @JvmField
+  val HTML_TAG: IElementType =
+    XmlTagElementType("HTML_TAG")
 
-  IFileElementType XML_FILE = new IFileElementType(XMLLanguage.INSTANCE);
-  IElementType XHTML_FILE = new IFileElementType(XHTMLLanguage.INSTANCE);
+  @JvmField
+  val HTML_FILE: IFileElementType =
+    HTMLParserDefinition.FILE_ELEMENT_TYPE
+
+  @JvmField
+  val HTML_EMBEDDED_CONTENT: IElementType =
+    EmbeddedHtmlContentElementType()
+
+  @JvmField
+  val HTML_RAW_TEXT: IElementType =
+    HtmlRawTextElementType
+
+  @JvmField
+  val XML_TEXT: IElementType =
+    XmlTextElementType()
+
+  @JvmField
+  val XML_FILE: IFileElementType =
+    IFileElementType(XMLLanguage.INSTANCE)
+
+  @JvmField
+  val XHTML_FILE: IElementType =
+    IFileElementType(XHTMLLanguage.INSTANCE)
 
 
-  IFileElementType DTD_FILE = new IFileElementType("DTD_FILE", DTDLanguage.INSTANCE);
+  @JvmField
+  val DTD_FILE: IFileElementType =
+    IFileElementType("DTD_FILE", DTDLanguage.INSTANCE)
 
-  IElementType XML_MARKUP_DECL = new XmlMarkupDeclElementType();
+  @JvmField
+  val XML_MARKUP_DECL: IElementType =
+    XmlMarkupDeclElementType()
 }
