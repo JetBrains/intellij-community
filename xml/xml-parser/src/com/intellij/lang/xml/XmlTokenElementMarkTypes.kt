3f8f648fc475050f8df4bc20305604593c61a245
Victor Turansky <victor.turansky@jetbrains.com>
2025-02-05 01:19:27 +0200

    WEB-68929 Convert `XmlTokenElementMarkTypes` to Kotlin
    GitOrigin-RevId: 677ca027b4d9db5748ef49e5e642a4a6fbbb45bd

--- xml/xml-parser/src/com/intellij/lang/xml/XmlTokenElementMarkTypes.kt
+++ xml/xml-parser/src/com/intellij/lang/xml/XmlTokenElementMarkTypes.kt
@@ -1,8 +1,10 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.lang.xml;
+package com.intellij.lang.xml
 
-import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.IElementType
 
-public interface XmlTokenElementMarkTypes {
-  IElementType XML_WHITE_SPACE_MARK = new IElementType("XML_WHITE_SPACE_MARK", XMLLanguage.INSTANCE);
+object XmlTokenElementMarkTypes {
+  @JvmField
+  val XML_WHITE_SPACE_MARK: IElementType =
+    IElementType("XML_WHITE_SPACE_MARK", XMLLanguage.INSTANCE)
 }
