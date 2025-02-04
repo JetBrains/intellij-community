5b2cb7de054e34b2e13bdacf8d328a2038c1512d
Victor Turansky <victor.turansky@jetbrains.com>
2025-02-05 00:52:34 +0200

    WEB-68929 Convert `IXmlElementType` to Kotlin
    GitOrigin-RevId: 955c9733c9c19817172f9a34a9aeb0c8321b8fc3

--- xml/xml-parser/src/com/intellij/psi/tree/xml/IXmlElementType.kt
+++ xml/xml-parser/src/com/intellij/psi/tree/xml/IXmlElementType.kt
@@ -1,12 +1,10 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.psi.tree.xml;
+package com.intellij.psi.tree.xml
 
-import com.intellij.lang.xml.XMLLanguage;
-import com.intellij.psi.tree.IElementType;
-import org.jetbrains.annotations.NonNls;
+import com.intellij.lang.xml.XMLLanguage
+import com.intellij.psi.tree.IElementType
+import org.jetbrains.annotations.NonNls
 
-public class IXmlElementType extends IElementType {
-  public IXmlElementType(@NonNls String debugName) {
-    super(debugName, XMLLanguage.INSTANCE);
-  }
-}
+open class IXmlElementType(
+  debugName: @NonNls String,
+) : IElementType(debugName, XMLLanguage.INSTANCE)
