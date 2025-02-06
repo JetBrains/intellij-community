7abd0520629b3e065177bfacd8d1c5232ccbd8dc
roman.ivanitskii <roman.ivanitskii@jetbrains.com>
2025-02-06 22:47:33 +0100

    [remote-driver][ui-tests] Add `assertContainsText` method to UiComponent
    GitOrigin-RevId: 5331278b4c1b653cd96d36aa69276bc0f46460fd

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/UiComponent.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/UiComponent.kt
@@ -218,6 +218,12 @@ open class UiComponent(private val data: ComponentData) : Finder, WithKeyboard {
     )
   }
 
+  fun assertContainsText(text: String, message: String? = null, timeout: Duration = DEFAULT_FIND_TIMEOUT) {
+    waitFor(message = message ?: "Finding one text '$text' in $this", timeout = timeout) {
+      allTextAsString().contains(text)
+    }
+  }
+
   /**
    * Waits for one UiText matching predicate.
    */
