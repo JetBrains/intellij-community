042d9e4c1dc0d6bd226aec4550b6124274f0b30a
Alexey Kudravtsev <cdr@intellij.com>
2025-02-06 12:08:02 +0100

    remove unnecessary obsolete api
    GitOrigin-RevId: 59e1072ee8c1edb8c1f76a09042144e37b7a7347

--- platform/analysis-api/src/com/intellij/codeInsight/daemon/QuickFixActionRegistrar.java
+++ platform/analysis-api/src/com/intellij/codeInsight/daemon/QuickFixActionRegistrar.java
@@ -8,15 +8,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public interface QuickFixActionRegistrar {
-  QuickFixActionRegistrar IGNORE_ALL = new QuickFixActionRegistrar() {
-    @Override
-    public void register(@NotNull IntentionAction action) {
-    }
-    @Override
-    public void register(@NotNull TextRange fixRange, @NotNull IntentionAction action, @Nullable HighlightDisplayKey key) {
-    }
-  };
-
   void register(@NotNull IntentionAction action);
 
   void register(@NotNull TextRange fixRange, @NotNull IntentionAction action, @Nullable HighlightDisplayKey key);
