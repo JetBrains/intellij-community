58e1305e24c4562ae3f432c2f3a42bbf14d7837e
Victor Turansky <victor.turansky@jetbrains.com>
2025-02-06 12:38:53 +0200

    WEB-68929 Convert `XMLParserDefinition` to Kotlin
    GitOrigin-RevId: 6265f3b6d2eb991f11657f5311f68a69a3ece312

--- xml/xml-psi-impl/src/com/intellij/lang/xml/XMLParserDefinition.kt
+++ xml/xml-psi-impl/src/com/intellij/lang/xml/XMLParserDefinition.kt
@@ -1,89 +1,84 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.lang.xml;
+package com.intellij.lang.xml
 
-import com.intellij.lang.ASTNode;
-import com.intellij.lang.ParserDefinition;
-import com.intellij.lang.PsiParser;
-import com.intellij.lexer.Lexer;
-import com.intellij.lexer.XmlLexer;
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.FileViewProvider;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.impl.source.parsing.xml.XmlParser;
-import com.intellij.psi.impl.source.xml.XmlFileImpl;
-import com.intellij.psi.impl.source.xml.stub.XmlStubBasedElementType;
-import com.intellij.psi.tree.IFileElementType;
-import com.intellij.psi.tree.TokenSet;
-import com.intellij.psi.util.UnsupportedNodeElementTypeException;
-import com.intellij.psi.xml.XmlElementType;
-import com.intellij.psi.xml.XmlTokenType;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.lang.ASTNode
+import com.intellij.lang.ParserDefinition
+import com.intellij.lang.ParserDefinition.SpaceRequirements
+import com.intellij.lang.PsiParser
+import com.intellij.lexer.Lexer
+import com.intellij.lexer.XmlLexer
+import com.intellij.openapi.project.Project
+import com.intellij.psi.FileViewProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.impl.source.parsing.xml.XmlParser
+import com.intellij.psi.impl.source.xml.XmlFileImpl
+import com.intellij.psi.impl.source.xml.stub.XmlStubBasedElementType
+import com.intellij.psi.tree.IFileElementType
+import com.intellij.psi.tree.TokenSet
+import com.intellij.psi.util.UnsupportedNodeElementTypeException
+import com.intellij.psi.xml.XmlElementType
+import com.intellij.psi.xml.XmlTokenType
 
-public class XMLParserDefinition implements ParserDefinition {
-  @Override
-  public @NotNull Lexer createLexer(Project project) {
-    return new XmlLexer();
-  }
+open class XMLParserDefinition :
+  ParserDefinition {
 
-  @Override
-  public @NotNull IFileElementType getFileNodeType() {
-    return XmlElementType.XML_FILE;
-  }
+  override fun createLexer(project: Project?): Lexer =
+    XmlLexer()
 
-  @Override
-  public @NotNull TokenSet getWhitespaceTokens() {
-    return XmlTokenType.WHITESPACES;
-  }
+  override fun getFileNodeType(): IFileElementType =
+    XmlElementType.XML_FILE
 
-  @Override
-  public @NotNull TokenSet getCommentTokens() {
-    return XmlTokenType.COMMENTS;
-  }
+  override fun getWhitespaceTokens(): TokenSet =
+    XmlTokenType.WHITESPACES
 
-  @Override
-  public @NotNull TokenSet getStringLiteralElements() {
-    return TokenSet.EMPTY;
-  }
+  override fun getCommentTokens(): TokenSet =
+    XmlTokenType.COMMENTS
 
-  @Override
-  public @NotNull PsiParser createParser(final Project project) {
-    return new XmlParser();
-  }
+  override fun getStringLiteralElements(): TokenSet =
+    TokenSet.EMPTY
 
-  @Override
-  public @NotNull PsiElement createElement(ASTNode node) {
-    if (node.getElementType() instanceof XmlStubBasedElementType) {
-      //noinspection rawtypes
-      return ((XmlStubBasedElementType)node.getElementType()).createPsi(node);
+  override fun createParser(project: Project?): PsiParser =
+    XmlParser()
+
+  override fun createElement(node: ASTNode): PsiElement {
+    val elementType = node.elementType
+
+    return when (elementType) {
+      is XmlStubBasedElementType<*, *>,
+        -> elementType.createPsi(node)
+
+      else -> throw UnsupportedNodeElementTypeException(node)
     }
-    throw new UnsupportedNodeElementTypeException(node);
   }
 
-  @Override
-  public @NotNull PsiFile createFile(@NotNull FileViewProvider viewProvider) {
-    return new XmlFileImpl(viewProvider, XmlElementType.XML_FILE);
-  }
+  override fun createFile(viewProvider: FileViewProvider): PsiFile =
+    XmlFileImpl(viewProvider, XmlElementType.XML_FILE)
 
-  @Override
-  public @NotNull SpaceRequirements spaceExistenceTypeBetweenTokens(ASTNode left, ASTNode right) {
-    return canStickTokensTogether(left, right);
-  }
+  override fun spaceExistenceTypeBetweenTokens(left: ASTNode, right: ASTNode): SpaceRequirements =
+    canStickTokensTogether(left, right)
 
-  public static SpaceRequirements canStickTokensTogether(final ASTNode left, final ASTNode right) {
-    if (left.getElementType() == XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN ||
-        right.getElementType() == XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN) {
-      return SpaceRequirements.MUST_NOT;
-    }
-    if (left.getElementType() == XmlTokenType.XML_ATTRIBUTE_VALUE_END_DELIMITER && right.getElementType() == XmlTokenType.XML_NAME) {
-      return SpaceRequirements.MUST;
-    }
-    if (left.getElementType() == XmlTokenType.XML_NAME && right.getElementType() == XmlTokenType.XML_NAME) {
-      return SpaceRequirements.MUST;
-    }
-    if (left.getElementType() == XmlTokenType.XML_TAG_NAME && right.getElementType() == XmlTokenType.XML_NAME) {
-      return SpaceRequirements.MUST;
-    }
-    return SpaceRequirements.MAY;
+  companion object {
+    @JvmStatic
+    fun canStickTokensTogether(left: ASTNode, right: ASTNode): SpaceRequirements =
+      when {
+        left.elementType === XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN
+        || right.elementType === XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN
+          -> SpaceRequirements.MUST_NOT
+
+        left.elementType === XmlTokenType.XML_ATTRIBUTE_VALUE_END_DELIMITER
+        && right.elementType === XmlTokenType.XML_NAME
+          -> SpaceRequirements.MUST
+
+        left.elementType === XmlTokenType.XML_NAME
+        && right.elementType === XmlTokenType.XML_NAME
+          -> SpaceRequirements.MUST
+
+        left.elementType === XmlTokenType.XML_TAG_NAME
+        && right.elementType === XmlTokenType.XML_NAME
+          -> SpaceRequirements.MUST
+
+        else -> SpaceRequirements.MAY
+      }
   }
 }
