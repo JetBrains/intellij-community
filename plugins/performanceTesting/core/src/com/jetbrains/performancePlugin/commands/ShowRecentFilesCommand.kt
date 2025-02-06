865e8b010c06d8982e48b983bf887b2cd51e34db
Nikita Katkov <nikita.katkov@jetbrains.com>
2025-02-06 22:14:13 +0100

    [recent files] IJPL-177136 Refactor existing switcher usages
    Update the performance testing plugin and remove switcher elements selection unit test completely because this logic does not belong to the switcher, but to the SelectionModel instead. We already have a dedicated integration test, RecentEditsTest namely
    
    GitOrigin-RevId: 14ddc6c7266ca50e972677cbc0bf631c37359d5d

--- plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/commands/ShowRecentFilesCommand.kt
+++ plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/commands/ShowRecentFilesCommand.kt
@@ -13,7 +13,7 @@ import org.jetbrains.annotations.NonNls
 /**
  * Usage: %showRecentFiles <seconds to wait before close>
  */
-class ShowRecentFilesCommand(text: String, line: Int): PerformanceCommandCoroutineAdapter(text, line) {
+class ShowRecentFilesCommand(text: String, line: Int) : PerformanceCommandCoroutineAdapter(text, line) {
 
   companion object {
     const val NAME: @NonNls String = "showRecentFiles"
@@ -21,12 +21,12 @@ class ShowRecentFilesCommand(text: String, line: Int): PerformanceCommandCorouti
   }
 
   override suspend fun doExecute(context: PlaybackContext) {
-    val secondsToWaitBeforeClose = extractCommandArgument(PREFIX).runCatching { this.toInt()  }.getOrDefault(5)
+    val secondsToWaitBeforeClose = extractCommandArgument(PREFIX).runCatching { this.toInt() }.getOrDefault(5)
     withContext(Dispatchers.EDT) {
       val switcher = Switcher.SWITCHER_KEY.get(context.project)?.cbShowOnlyEditedFiles?.apply { isSelected = !isSelected }
-      ?: Switcher.SwitcherPanel(context.project, IdeBundle.message("title.popup.recent.files"), null, false, true)
+                     ?: createAndShowNewSwitcherSuspend(false, null, IdeBundle.message("title.popup.recent.files"), context.project)
       delay(secondsToWaitBeforeClose * 1000L)
-      if(switcher is Switcher.SwitcherPanel){
+      if (switcher is Switcher.SwitcherPanel) {
         switcher.cancel()
       }
     }
