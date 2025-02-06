052a1379224955c6fb3562bdc1c62cd29673b429
Victor Turansky <victor.turansky@jetbrains.com>
2025-02-06 12:25:26 +0200

    WEB-68929 Convert `XhtmlSyntaxHighlighterFactory` to Kotlin
    GitOrigin-RevId: cad0ac322aa3f8fbfb09786046b7bcd3d7b255f8

--- xml/xml-psi-impl/src/com/intellij/lang/xhtml/XhtmlSyntaxHighlighterFactory.kt
+++ xml/xml-psi-impl/src/com/intellij/lang/xhtml/XhtmlSyntaxHighlighterFactory.kt
@@ -1,14 +1,13 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.lang.xhtml;
+package com.intellij.lang.xhtml
 
-import com.intellij.ide.highlighter.XmlFileHighlighter;
-import com.intellij.openapi.fileTypes.SingleLazyInstanceSyntaxHighlighterFactory;
-import com.intellij.openapi.fileTypes.SyntaxHighlighter;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.ide.highlighter.XmlFileHighlighter
+import com.intellij.openapi.fileTypes.SingleLazyInstanceSyntaxHighlighterFactory
+import com.intellij.openapi.fileTypes.SyntaxHighlighter
 
-public class XhtmlSyntaxHighlighterFactory extends SingleLazyInstanceSyntaxHighlighterFactory {
-  @Override
-  protected @NotNull SyntaxHighlighter createHighlighter() {
-    return new XmlFileHighlighter(false, true);
-  }
+class XhtmlSyntaxHighlighterFactory :
+  SingleLazyInstanceSyntaxHighlighterFactory() {
+
+  override fun createHighlighter(): SyntaxHighlighter =
+    XmlFileHighlighter(false, true)
 }
