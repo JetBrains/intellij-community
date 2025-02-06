f8a8fac2933b76a31a1162ffab807dc485946f67
Victor Turansky <victor.turansky@jetbrains.com>
2025-02-06 10:22:58 +0200

    WEB-68929 Convert `XmlLexer` to Kotlin
    GitOrigin-RevId: 3694449136b2a5a67e5739cc39f7735baa78344e

--- xml/xml-parser/src/com/intellij/lexer/XmlLexer.kt
+++ xml/xml-parser/src/com/intellij/lexer/XmlLexer.kt
@@ -1,25 +1,23 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.lexer;
+package com.intellij.lexer
 
-import com.intellij.psi.tree.TokenSet;
-import com.intellij.psi.xml.XmlTokenType;
+import com.intellij.psi.tree.TokenSet
+import com.intellij.psi.xml.XmlTokenType
 
-public class XmlLexer extends MergingLexerAdapter {
-  private static final TokenSet TOKENS_TO_MERGE = TokenSet.create(XmlTokenType.XML_DATA_CHARACTERS,
-                                                                  XmlTokenType.XML_TAG_CHARACTERS,
-                                                                  XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN,
-                                                                  XmlTokenType.XML_PI_TARGET,
-                                                                  XmlTokenType.XML_COMMENT_CHARACTERS);
+class XmlLexer(baseLexer: Lexer) :
+  MergingLexerAdapter(baseLexer, TOKENS_TO_MERGE) {
 
-  public XmlLexer() {
-    this(false);
-  }
+  @JvmOverloads
+  constructor(conditionalCommentsSupport: Boolean = false) :
+    this(_XmlLexer(__XmlLexer(null), conditionalCommentsSupport))
 
-  public XmlLexer(final boolean conditionalCommentsSupport) {
-    this(new _XmlLexer(new __XmlLexer(null), conditionalCommentsSupport));
-  }
-
-  public XmlLexer(Lexer baseLexer) {
-    super(baseLexer, TOKENS_TO_MERGE);
+  private companion object {
+    private val TOKENS_TO_MERGE = TokenSet.create(
+      XmlTokenType.XML_DATA_CHARACTERS,
+      XmlTokenType.XML_TAG_CHARACTERS,
+      XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN,
+      XmlTokenType.XML_PI_TARGET,
+      XmlTokenType.XML_COMMENT_CHARACTERS,
+    )
   }
 }
