15b34005888a4aad9a6fdf7258ba309b350a02a0
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2025-02-07 13:18:00 +0100

    [java, jigsaw] Refactor module stub building with a builder pattern
    GitOrigin-RevId: aef7aa340bdb84a26ac9b4a13204f177d200012f

--- java/java-psi-impl/src/com/intellij/psi/impl/compiled/ModuleStubBuildingVisitor.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/compiled/ModuleStubBuildingVisitor.java
@@ -1,10 +1,11 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.impl.compiled;
 
 import com.intellij.psi.impl.cache.ModifierFlags;
 import com.intellij.psi.impl.java.stubs.PsiJavaFileStub;
 import com.intellij.psi.impl.java.stubs.PsiJavaModuleStub;
 import com.intellij.psi.impl.java.stubs.impl.*;
+import com.intellij.psi.stubs.IStubElementType;
 import com.intellij.util.ArrayUtilRt;
 import com.intellij.util.Function;
 import org.jetbrains.org.objectweb.asm.AnnotationVisitor;
@@ -12,7 +13,9 @@ import org.jetbrains.org.objectweb.asm.ClassVisitor;
 import org.jetbrains.org.objectweb.asm.ModuleVisitor;
 import org.jetbrains.org.objectweb.asm.Opcodes;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static com.intellij.psi.impl.java.stubs.JavaStubElementTypes.*;
 import static com.intellij.util.BitUtil.isSet;
@@ -21,67 +24,62 @@ import static com.intellij.util.containers.ContainerUtil.map2Array;
 public class ModuleStubBuildingVisitor extends ClassVisitor {
   private static final Function<String, String> NAME_MAPPER = name1 -> name1.replace('/', '.');
 
-  private final PsiJavaFileStub myParent;
-  private PsiJavaModuleStub myResult;
-  private PsiModifierListStubImpl myModList;
+  private final ModuleStubBuilder myBuilder;
 
   public ModuleStubBuildingVisitor(PsiJavaFileStub parent) {
     super(Opcodes.API_VERSION);
-    myParent = parent;
+    myBuilder = new ModuleStubBuilder(parent);
   }
 
   public PsiJavaModuleStub getResult() {
-    return myResult;
+    return myBuilder.build();
   }
 
   @Override
   public ModuleVisitor visitModule(String name, int access, String version) {
-    myResult = new PsiJavaModuleStubImpl(myParent, name);
-
-    myModList = new PsiModifierListStubImpl(myResult, moduleFlags(access));
-
+    myBuilder.name(name).flags(moduleFlags(access));
     return new ModuleVisitor(Opcodes.API_VERSION) {
       @Override
       public void visitRequire(String module, int access, String version) {
         if (!isGenerated(access)) {
-          PsiRequiresStatementStubImpl statementStub = new PsiRequiresStatementStubImpl(myResult, module);
-          new PsiModifierListStubImpl(statementStub, requiresFlags(access));
+          myBuilder.addRequires(module, requiresFlags(access));
         }
       }
 
       @Override
       public void visitExport(String packageName, int access, String... modules) {
         if (!isGenerated(access)) {
-          new PsiPackageAccessibilityStatementStubImpl(
-            myResult, EXPORTS_STATEMENT, NAME_MAPPER.fun(packageName), modules == null ? null : Arrays.asList(modules));
+          myBuilder.addPackageAccessibility(EXPORTS_STATEMENT, packageName, modules);
         }
       }
 
       @Override
       public void visitOpen(String packageName, int access, String... modules) {
         if (!isGenerated(access)) {
-          new PsiPackageAccessibilityStatementStubImpl(
-            myResult, OPENS_STATEMENT, NAME_MAPPER.fun(packageName), modules == null ? null : Arrays.asList(modules));
+          myBuilder.addPackageAccessibility(OPENS_STATEMENT, packageName, modules);
         }
       }
 
       @Override
       public void visitUse(String service) {
-        new PsiUsesStatementStubImpl(myResult, NAME_MAPPER.fun(service));
+        myBuilder.addUses(service);
       }
 
       @Override
       public void visitProvide(String service, String... providers) {
-        PsiProvidesStatementStubImpl statementStub = new PsiProvidesStatementStubImpl(myResult, NAME_MAPPER.fun(service));
-        String[] names = map2Array(providers, String.class, NAME_MAPPER);
-        new PsiClassReferenceListStubImpl(PROVIDES_WITH_LIST, statementStub, names.length == 0 ? ArrayUtilRt.EMPTY_STRING_ARRAY : names);
+        myBuilder.addProvide(service, providers);
       }
     };
   }
 
   @Override
   public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
-    return StubBuildingVisitor.getAnnotationTextCollector(desc, text -> new PsiAnnotationStubImpl(myModList, text));
+    return StubBuildingVisitor.getAnnotationTextCollector(desc, text -> myBuilder.addAnnotation(text));
+  }
+
+  @Override
+  public void visitEnd() {
+    super.visitEnd();
   }
 
   private static boolean isGenerated(int access) {
@@ -97,5 +95,118 @@ public class ModuleStubBuildingVisitor extends ClassVisitor {
     if (isSet(access, Opcodes.ACC_TRANSITIVE)) flags |= ModifierFlags.TRANSITIVE_MASK;
     if (isSet(access, Opcodes.ACC_STATIC_PHASE)) flags |= ModifierFlags.STATIC_MASK;
     return flags;
+  }
+
+  private static class ModuleStubBuilder {
+    private final PsiJavaFileStub myParent;
+
+    private volatile PsiJavaModuleStub myResult;
+
+    private volatile String myName;
+    private volatile int myFlags;
+
+    private final List<Requires> myRequires = new ArrayList<>();
+    private final List<PackageAccessibility> myPackageAccessibilities = new ArrayList<>();
+    private final List<Provide> myProvides = new ArrayList<>();
+    private final List<String> myServices = new ArrayList<>();
+    private final List<String> myAnnotations = new ArrayList<>();
+
+    ModuleStubBuilder(PsiJavaFileStub parent) { myParent = parent; }
+
+    ModuleStubBuilder name(String name) {
+      myName = name;
+      return this;
+    }
+
+    void flags(int flags) {
+      myFlags = flags;
+    }
+
+    void addRequires(String module, int flags) {
+      myRequires.add(new Requires(module, flags));
+    }
+
+    void addPackageAccessibility(IStubElementType type, String packageName, String[] modules) {
+      myPackageAccessibilities.add(new PackageAccessibility(type, packageName, modules));
+    }
+
+    PsiJavaModuleStub build() {
+      if (myResult == null) {
+        synchronized (this) {
+          if (myResult == null) {
+            PsiJavaModuleStub result = new PsiJavaModuleStubImpl(myParent, myName);
+            PsiModifierListStubImpl modifiers = new PsiModifierListStubImpl(result, myFlags);
+            for (Requires require : myRequires) {
+              PsiRequiresStatementStubImpl req = new PsiRequiresStatementStubImpl(result, require.name);
+              new PsiModifierListStubImpl(req, require.flags);
+            }
+            for (PackageAccessibility accessibility : myPackageAccessibilities) {
+              new PsiPackageAccessibilityStatementStubImpl(result, accessibility.myType, NAME_MAPPER.fun(accessibility.myPackageName),
+                                                           accessibility.myModules);
+            }
+            for (String service : myServices) {
+              new PsiUsesStatementStubImpl(result, NAME_MAPPER.fun(service));
+            }
+            for (Provide provide : myProvides) {
+              PsiProvidesStatementStubImpl statementStub = new PsiProvidesStatementStubImpl(result, NAME_MAPPER.fun(provide.myService));
+              String[] names = map2Array(provide.myProviders, String.class, NAME_MAPPER);
+              new PsiClassReferenceListStubImpl(PROVIDES_WITH_LIST, statementStub,
+                                                names.length == 0 ? ArrayUtilRt.EMPTY_STRING_ARRAY : names);
+            }
+            for (String annotation : myAnnotations) {
+              new PsiAnnotationStubImpl(modifiers, annotation);
+            }
+
+            myResult = result;
+          }
+        }
+      }
+      return myResult;
+    }
+
+    void addUses(String service) {
+      myServices.add(service);
+    }
+
+    void addProvide(String service, String[] providers) {
+      myProvides.add(new Provide(service, providers));
+    }
+
+    void addAnnotation(String annotationName) {
+      myAnnotations.add(annotationName);
+    }
+
+    private static class Requires {
+      final String name;
+      final int flags;
+
+      Requires(String name, int flags) {
+        this.name = name;
+        this.flags = flags;
+      }
+    }
+
+    private static class Provide {
+      private final String myService;
+      private final List<String> myProviders;
+
+      Provide(String service, String[] providers) {
+        myService = service;
+        myProviders = providers == null ? null : Arrays.asList(providers);
+        ;
+      }
+    }
+
+    private static class PackageAccessibility {
+      final IStubElementType myType;
+      final String myPackageName;
+      final List<String> myModules;
+
+      PackageAccessibility(IStubElementType type, String packageName, String[] modules) {
+        myType = type;
+        myPackageName = packageName;
+        myModules = modules == null ? null : Arrays.asList(modules);
+      }
+    }
   }
 }
