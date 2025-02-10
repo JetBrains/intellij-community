1f22db95bfde7a6a853680babf165b1e5e4de7ec
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2025-02-10 15:43:49 +0100

    [ai-completion] LLM-14791: do not use folding, when rendering multiline, on backend split
    GitOrigin-RevId: 96f9d9e83515a260a09fde7835fbbd8f7a82bed0

--- platform/platform-impl/codeinsight-inline/src/com/intellij/codeInsight/inline/completion/render/InlineCompletionFoldingManager.kt
+++ platform/platform-impl/codeinsight-inline/src/com/intellij/codeInsight/inline/completion/render/InlineCompletionFoldingManager.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.inline.completion.render
 
+import com.intellij.codeWithMe.ClientId
+import com.intellij.codeWithMe.ClientId.Companion.isLocal
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.editor.Document
@@ -147,6 +149,10 @@ internal class InlineCompletionFoldingManager private constructor(private val ed
     private val LOG = thisLogger()
 
     private fun isFoldingSupported(editor: Editor): Boolean {
+      if (!ClientId.currentOrNull.isLocal) {
+        // Folding will happen on frontend
+        return false
+      }
       val foldingModel = editor.foldingModel
       return foldingModel is FoldingModelEx && foldingModel.isFoldingEnabled
     }
