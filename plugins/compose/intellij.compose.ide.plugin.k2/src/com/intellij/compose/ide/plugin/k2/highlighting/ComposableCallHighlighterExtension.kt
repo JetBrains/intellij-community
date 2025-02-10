1e2dc1942e667d7cb94efa15dfdc7aee0999af6b
Nebojsa Vuksic <nebojsa.vuksic@jetbrains.com>
2025-02-10 13:53:33 +0100

    KMT-519 Add K2 highlighting support for Composable function callables
    GitOrigin-RevId: 7863fa64ffc65d6c57fe8376aa9129548881cf6f

---
+++ plugins/compose/intellij.compose.ide.plugin.k2/src/com/intellij/compose/ide/plugin/k2/highlighting/ComposableCallHighlighterExtension.kt
@@ -0,0 +1,48 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.compose.ide.plugin.k2.highlighting
+
+import com.intellij.codeInsight.daemon.impl.HighlightInfoType
+import com.intellij.compose.ide.plugin.shared.COMPOSABLE_ANNOTATION_CLASS_ID
+import com.intellij.compose.ide.plugin.shared.highlighting.COMPOSABLE_CALL_TEXT_TYPE
+import com.intellij.compose.ide.plugin.shared.isComposeEnabledInModule
+import com.intellij.compose.ide.plugin.shared.isElementInLibrarySource
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
+import org.jetbrains.kotlin.analysis.api.KaSession
+import org.jetbrains.kotlin.analysis.api.annotations.KaAnnotated
+import org.jetbrains.kotlin.analysis.api.resolution.KaCall
+import org.jetbrains.kotlin.analysis.api.resolution.KaCallableMemberCall
+import org.jetbrains.kotlin.analysis.api.resolution.symbol
+import org.jetbrains.kotlin.analysis.api.symbols.KaCallableSymbol
+import org.jetbrains.kotlin.analysis.api.symbols.KaNamedFunctionSymbol
+import org.jetbrains.kotlin.analysis.api.symbols.KaPropertySymbol
+import org.jetbrains.kotlin.idea.highlighting.KotlinCallHighlighterExtension
+
+internal class ComposableCallHighlighterExtension : KotlinCallHighlighterExtension {
+  context(KaSession)
+  override fun highlightCall(elementToHighlight: PsiElement, call: KaCall): HighlightInfoType? {
+    val memberCall = call as? KaCallableMemberCall<*, *> ?: return null
+    val callableSymbol = memberCall.symbol
+    if (!isComposableInvocation(callableSymbol)) return null
+
+    return if (isComposeEnabledInModule(elementToHighlight) || isElementInLibrarySource(elementToHighlight))
+      COMPOSABLE_CALL_TEXT_TYPE
+    else null
+  }
+
+  @OptIn(KaExperimentalApi::class)
+  private fun isComposableInvocation(callableSymbol: KaCallableSymbol): Boolean {
+    fun hasComposableAnnotation(annotated: KaAnnotated?): Boolean {
+      return annotated != null && COMPOSABLE_ANNOTATION_CLASS_ID in annotated.annotations
+    }
+
+    val type = callableSymbol.returnType
+    if (hasComposableAnnotation(type)) return true
+
+    return when (callableSymbol) {
+      is KaNamedFunctionSymbol -> hasComposableAnnotation(callableSymbol)
+      is KaPropertySymbol -> hasComposableAnnotation(callableSymbol.getter)
+      else -> false
+    }
+  }
+}
--- plugins/compose/intellij.compose.ide.plugin.shared/src/com/intellij/compose/ide/plugin/shared/PsiUtils.kt
+++ plugins/compose/intellij.compose.ide.plugin.shared/src/com/intellij/compose/ide/plugin/shared/PsiUtils.kt
@@ -1,20 +1,66 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.compose.ide.plugin.shared
 
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.roots.ProjectRootModificationTracker
+import com.intellij.openapi.roots.impl.ProjectFileIndexFacade
 import com.intellij.openapi.util.Key
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.CachedValue
 import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
+import com.intellij.util.concurrency.annotations.RequiresReadLock
+import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.resolution.singleConstructorCallOrNull
 import org.jetbrains.kotlin.analysis.api.resolution.symbol
+import org.jetbrains.kotlin.idea.stubindex.KotlinFullClassNameIndex
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.psi.KtAnnotated
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.psi.KtNamedFunction
+
+/**
+ * Determines if the given [PsiElement] element is part of a library source.
+ *
+ * @param element The [PsiElement] element to check.
+ * @return true if the element is in a library source; false otherwise.
+ */
+@ApiStatus.Internal
+@RequiresReadLock
+fun isElementInLibrarySource(element: PsiElement): Boolean {
+  val virtualFile = element.containingFile.virtualFile ?: return false
+
+  return ProjectFileIndexFacade.getInstance(element.project)
+    .isInLibrarySource(virtualFile)
+}
+
+/**
+ * Checks if the Compose functionality is enabled within the module associated with the given [PsiElement].
+ * Compose functionality is enabled if the Compose annotation is available in the evaluated module's classpath.
+ *
+ * @param element - the [PsiElement] for which the module should be evaluated.
+ * @return true if the Compose annotation class is found in the module's classpath; false otherwise.
+ */
+@ApiStatus.Internal
+fun isComposeEnabledInModule(element: PsiElement): Boolean {
+  val module = ModuleUtilCore.findModuleForPsiElement(element) ?: return false
+  return isComposeEnabledInModule(module)
+}
+/**
+ * Checks if the Compose functionality is enabled in the module.
+ * Compose functionality is enabled if the Compose annotation is available in the evaluated module's classpath.
+ *
+ * @param module - the [Module] which should be evaluated.
+ * @return true if the Compose annotation class is found in the module's classpath; false otherwise.
+ */
+internal fun isComposeEnabledInModule(module: Module): Boolean {
+  val moduleScope = module.getModuleWithDependenciesAndLibrariesScope(/*includeTests = */true)
+  val foundClasses = KotlinFullClassNameIndex[COMPOSABLE_ANNOTATION_CLASS_ID.asFqNameString(), module.project, moduleScope]
+  return foundClasses.isNotEmpty()
+}
 
 internal fun PsiElement.isComposableFunction(): Boolean =
   (this as? KtNamedFunction)?.getAnnotationWithCaching(COMPOSABLE_FUNCTION_KEY) { it.isComposableAnnotation() } != null
