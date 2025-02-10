1730e02b9c984bb1586d7077773c35d65754fe34
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-02-10 12:54:01 +0100

    fix JpsRootElementBase visibility
    GitOrigin-RevId: a9712fcb7f7dfe85a695a16a6c2664022ad611c2

--- jps/model-impl/src/org/jetbrains/jps/model/impl/JpsRootElementBase.java
+++ jps/model-impl/src/org/jetbrains/jps/model/impl/JpsRootElementBase.java
@@ -1,11 +1,13 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.jps.model.impl;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.model.JpsModel;
 import org.jetbrains.jps.model.ex.JpsCompositeElementBase;
 
-abstract class JpsRootElementBase<E extends JpsRootElementBase<E>> extends JpsCompositeElementBase<E> {
+@ApiStatus.Internal
+public abstract class JpsRootElementBase<E extends JpsRootElementBase<E>> extends JpsCompositeElementBase<E> {
   private final JpsModel myModel;
 
   protected JpsRootElementBase(@NotNull JpsModel model) {
@@ -14,12 +16,12 @@ abstract class JpsRootElementBase<E extends JpsRootElementBase<E>> extends JpsCo
   }
 
   @Override
-  public @NotNull JpsModel getModel() {
+  public final @NotNull JpsModel getModel() {
     return myModel;
   }
 
   @Override
-  public @NotNull E createCopy() {
+  public final @NotNull E createCopy() {
     throw new UnsupportedOperationException("'createCopy' not implemented in " + getClass().getName());
   }
 }
