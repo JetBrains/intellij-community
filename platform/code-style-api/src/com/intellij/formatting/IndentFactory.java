0222866a0c9cebfebcc3f5ef8fd7a7fd929e55f8
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-02-10 14:51:25 +0100

    fix IndentFactory visibility
    GitOrigin-RevId: 6e8f14a721623f3dd05edb49b7566a1f440a9af5

--- platform/code-style-api/src/com/intellij/formatting/IndentFactory.java
+++ platform/code-style-api/src/com/intellij/formatting/IndentFactory.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.formatting;
 
 import org.jetbrains.annotations.ApiStatus;
@@ -22,22 +8,34 @@ import org.jetbrains.annotations.NotNull;
  * Internal interface for creating indent instances.
  * <p/>
  * Methods of this interface define contract for implementing {@link Indent} factory methods, so, feel free to check
- * their contracts. 
+ * their contracts.
  */
-interface IndentFactory {
+@ApiStatus.Internal
+public interface IndentFactory {
   Indent getNormalIndent(boolean relativeToDirectParent);
+
   Indent getNoneIndent();
+
   Indent getAbsoluteNoneIndent();
+
   Indent getAbsoluteLabelIndent();
+
   Indent getLabelIndent();
+
   Indent getContinuationIndent(boolean relativeToDirectParent);
+
   Indent getContinuationWithoutFirstIndent(boolean relativeToDirectParent);
+
   Indent getSpaceIndent(final int spaces, boolean relativeToDirectParent);
+
   Indent getIndent(@NotNull Indent.Type type, boolean relativeToDirectParent, boolean enforceIndentToChildren);
+
   Indent getIndent(@NotNull Indent.Type type, int spaces, boolean relativeToDirectParent, boolean enforceIndentToChildren);
 
   @ApiStatus.Experimental
   Indent getIndentEnforcedToChildrenToBeRelativeToMe(@NotNull Indent.Type type, int spaces);
+
   Indent getSmartIndent(@NotNull Indent.Type type);
+
   Indent getSmartIndent(@NotNull Indent.Type type, boolean relativeToDirectParent);
 }
