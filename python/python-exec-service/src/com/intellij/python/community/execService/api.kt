d1bac97eb84d487952ef7e4a9b2e5cce6b4b0552
Vitaly Legchilkin <vitaly.legchilkin@jetbrains.com>
2025-02-11 11:27:05 +0100

    [python] ExecService / PyError adjustments (PY-60410)
    + replace processDescription and timeout fields with ExecOptions
    + add workingDirectory and env variables to exec options
    + support ProcessOutputTransformer for cases when both stdout/stderr and custom error handling is needed
    + make PyError classes open to have custom business errors
    
    GitOrigin-RevId: 99c969e730cdbf23df4c9b176a43f78ede43001a

--- python/python-exec-service/src/com/intellij/python/community/execService/api.kt
+++ python/python-exec-service/src/com/intellij/python/community/execService/api.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.python.community.execService
 
+import com.intellij.execution.process.ProcessOutput
+import com.intellij.openapi.util.NlsSafe
 import com.intellij.platform.eel.EelApi
 import com.intellij.python.community.execService.impl.ExecServiceImpl
 import com.jetbrains.python.PythonBinary
@@ -14,22 +16,62 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 
 /**
+ * Error is an optional additionalMessage, that will be used instead of a default one for the [ExecException] in the [com.jetbrains.python.execution.PyExecutionFailure].
+ */
+typealias ProcessOutputTransformer<T> = (ProcessOutput) -> Result<T, @NlsSafe String?>
+
+object ZeroCodeStdoutTransformer : ProcessOutputTransformer<String> {
+  override fun invoke(processOutput: ProcessOutput): Result<String, String?> =
+    if (processOutput.exitCode == 0) Result.success(processOutput.stdout) else Result.failure(null)
+}
+
+/**
  * Service is a thin wrapper over [EelApi] to execute python tools on local or remote Eel.
  * to obtain service, use function with same name.
  */
 @ApiStatus.Internal
 interface ExecService {
   /**
-   * Execute [whatToExec] with [args] and get stdout if `errorCode != 0`. Get error otherwise.
-   *
-   * Process gets killed after [timeout], and might have optional [processDescription] (to be displayed to user).
+   * Execute [whatToExec] with [args] and get both stdout/stderr outputs if `errorCode != 0`, gets error otherwise.
    * If you want to show a modal window with progress, use `withModalProgress`.
+   *
+   * @param[args] command line arguments
+   * @param[options]  customizable process run options like timeout or environment variables to use
    * @return stdout or error. It is recommended to put this error into [com.jetbrains.python.errorProcessing.ErrorSink], but feel free to match and process it.
    */
-  @ApiStatus.Internal
   @CheckReturnValue
-  suspend fun execGetStdout(whatToExec: WhatToExec, args: List<String> = emptyList<String>(), processDescription: @Nls String? = null, timeout: Duration = 1.minutes): Result<String, ExecException>
+  suspend fun <T> execute(
+    whatToExec: WhatToExec,
+    args: List<String> = emptyList(),
+    options: ExecOptions = ExecOptions(),
+    processOutputTransformer: ProcessOutputTransformer<T>,
+  ): Result<T, ExecException>
+
+  @CheckReturnValue
+  suspend fun execGetStdout(
+    whatToExec: WhatToExec,
+    args: List<String> = emptyList(),
+    options: ExecOptions = ExecOptions(),
+  ): Result<String, ExecException> = execute(
+    whatToExec = whatToExec,
+    args = args,
+    options = options,
+    processOutputTransformer = ZeroCodeStdoutTransformer
+  )
 }
+
+/**
+ * @property[workingDirectory] Directory where to run the process (PWD)
+ * @property[env] Environment variables to be applied with the process run
+ * @property[timeout] Process gets killed after this timeout
+ * @property[processDescription] optional description to be displayed to user
+ */
+data class ExecOptions(
+  val env: Map<String, String> = emptyMap(),
+  val workingDirectory: Path? = null,
+  val processDescription: @Nls String? = null,
+  val timeout: Duration = 1.minutes,
+)
 
 sealed interface WhatToExec {
   /**
