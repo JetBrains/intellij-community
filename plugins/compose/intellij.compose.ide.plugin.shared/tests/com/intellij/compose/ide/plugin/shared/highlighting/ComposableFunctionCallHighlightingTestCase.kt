74741de57d97ac3ded407ebd5fb96b581c821ba2
Nebojsa Vuksic <nebojsa.vuksic@jetbrains.com>
2025-02-11 08:58:37 +0100

    KMT-519 Rename test data for K2 Composable function highlighting tests
    GitOrigin-RevId: 3abad16bb28a6cfd04f53fb3f25ed2227434db4f

--- plugins/compose/intellij.compose.ide.plugin.shared/tests/com/intellij/compose/ide/plugin/shared/highlighting/ComposableFunctionCallHighlightingTestCase.kt
+++ plugins/compose/intellij.compose.ide.plugin.shared/tests/com/intellij/compose/ide/plugin/shared/highlighting/ComposableFunctionCallHighlightingTestCase.kt
@@ -6,49 +6,61 @@ abstract class ComposableFunctionCallHighlightingTestCase : BaseComposableCallHi
     get() = "composableFunction"
 
   fun `test Composable function call within Composable function with Compose enable`() {
-    val testFileToHighlight = myFixture.configureByFile("testComposableFunctionCallWithinComposableFunctionBody.kt")
+    val testFileToHighlight = myFixture.configureByFile("testComposableCallInComposableFunction.kt")
 
     doTestHighlightingWithEnabledCompose(testFileToHighlight, COMPOSABLE_CALL_TEXT_TYPE)
   }
 
   fun `test Composable function call within Composable function with Compose disabled`() {
-    val testFileToHighlight = myFixture.configureByFile("testComposableFunctionCallWithinComposableFunctionBody.kt")
+    val testFileToHighlight = myFixture.configureByFile("testComposableCallInComposableFunction.kt")
 
     doTestHighlightingWithDisabledCompose(testFileToHighlight)
   }
 
   fun `test Composable function call within non-Composable function with Compose enabled`() {
-    val testFileToHighlight = myFixture.configureByFile("testComposableFunctionCallWithinNonComposableFunctionBody.kt")
+    val testFileToHighlight = myFixture.configureByFile("testComposableCallInNonComposableFunction.kt")
 
     doTestHighlightingWithEnabledCompose(testFileToHighlight, COMPOSABLE_CALL_TEXT_TYPE)
   }
 
   fun `test Composable function call within non-Composable function with Compose disabled`() {
-    val testFileToHighlight = myFixture.configureByFile("testComposableFunctionCallWithinNonComposableFunctionBody.kt")
+    val testFileToHighlight = myFixture.configureByFile("testComposableCallInNonComposableFunction.kt")
 
     doTestHighlightingWithDisabledCompose(testFileToHighlight)
   }
 
   fun `test non-Composable function call within non-Composable function with Compose enabled`() {
-    val testFileToHighlight = myFixture.configureByFile("testNonComposableFunctionCallWithinNonComposableFunctionBody.kt")
+    val testFileToHighlight = myFixture.configureByFile("testNonComposableCallInNonComposableFunction.kt")
 
     doTestHighlightingWithEnabledCompose(testFileToHighlight, null)
   }
 
   fun `test non-Composable function call within non-Composable function with Compose disabled`() {
-    val testFileToHighlight = myFixture.configureByFile("testNonComposableFunctionCallWithinNonComposableFunctionBody.kt")
+    val testFileToHighlight = myFixture.configureByFile("testNonComposableCallInNonComposableFunction.kt")
+
+    doTestHighlightingWithDisabledCompose(testFileToHighlight)
+  }
+
+  fun `test non-Composable function call within Composable function with Compose enabled`() {
+    val testFileToHighlight = myFixture.configureByFile("testNonComposableCallInComposableFunction.kt")
+
+    doTestHighlightingWithEnabledCompose(testFileToHighlight, null)
+  }
+
+  fun `test non-Composable function call within Composable function with Compose disabled`() {
+    val testFileToHighlight = myFixture.configureByFile("testNonComposableCallInComposableFunction.kt")
 
     doTestHighlightingWithDisabledCompose(testFileToHighlight)
   }
 
   fun `test Composable function call within Composable class member function with Compose enabled`() {
-    val testFileToHighlight = myFixture.configureByFile("testComposableFunctionCallWithinClassMemberComposableFunctionBody.kt")
+    val testFileToHighlight = myFixture.configureByFile("testComposableCallInComposableMember.kt")
 
     doTestHighlightingWithDisabledCompose(testFileToHighlight)
   }
 
   fun `test Composable function call within Composable class member function with Compose disabled`() {
-    val testFileToHighlight = myFixture.configureByFile("testComposableFunctionCallWithinClassMemberComposableFunctionBody.kt")
+    val testFileToHighlight = myFixture.configureByFile("testComposableCallInComposableMember.kt")
 
     doTestHighlightingWithDisabledCompose(testFileToHighlight)
   }
