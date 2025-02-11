65f1c9b5e4ea47053b73e1e93c8e101c285b9b95
Alexandru Resiga <alexandru.resiga@jetbrains.com>
2025-02-11 12:09:26 +0200

    KMT-499 [compose resources] refactor Compose resources support for Gradle-less accessors generation
    - don't the Gradle tasks to generate accessors, emulate the process
    - use logic similar to R class Android resources to track PSI changes
    - use logic from CMP plugin to generate accessors
    - handle both formats of Accessors, which are regenerated after each project build
    - support hierarchical source set structures when navigating to resource files
    - remove old ComposeResourcesService which was just a port from Fleet
    - introduce ComposeResourcesManager.kt to handle fetching/updating composeResources in case of project imports
    - display notifications for exceptions while generating accessors
    
    (cherry picked from commit dfe1fa6daa6c840f8d72a3a96e822cb5858e9e0e)
    
    GitOrigin-RevId: 0163b4878b30445b8d2f924de9c6aef8437a77d7

---
+++ plugins/compose/intellij.compose.ide.plugin.resources/src/com/intellij/compose/ide/plugin/resources/psi/ComposeResourcesAccessors.kt
@@ -0,0 +1,214 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.compose.ide.plugin.resources.psi
+
+import com.intellij.compose.ide.plugin.resources.ResourceType
+import com.intellij.openapi.application.runUndoTransparentWriteAction
+import com.intellij.openapi.application.writeAction
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.util.concurrency.annotations.RequiresWriteLock
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.*
+
+private const val ITEMS_PER_FILE_LIMIT = 500
+private const val RESOURCE_ITEM_CLASS = "ResourceItem"
+private const val LANGUAGE_QUALIFIER = "LanguageQualifier"
+private const val REGION_QUALIFIER = "RegionQualifier"
+private const val THEME_QUALIFIER = "ThemeQualifier"
+private const val DENSITY_QUALIFIER = "DensityQualifier"
+
+internal suspend fun getAccessorsSpecs(
+  //type -> id -> items
+  resources: Map<ResourceType, Map<String, List<ResourceAccessorItem>>>,
+  packageName: String,
+  sourceSetName: String,
+  moduleDir: String,
+) {
+
+
+  //we need to sort it to generate the same code on different platforms
+  // todo[alexandru.resiga] this can lead to "file cache conflict" while renaming
+  sortResources(resources).forEach { (type, idToResources) ->
+    val chunks = idToResources.keys.chunked(ITEMS_PER_FILE_LIMIT)
+
+    chunks.forEachIndexed { index, ids ->
+      getChunkFileSpec(
+        type,
+        type.accessorName.uppercaseFirstChar() + index + "." + sourceSetName + ".kt",
+        packageName,
+        moduleDir,
+        idToResources.subMap(ids.first(), true, ids.last(), true)
+      )
+    }
+  }
+
+}
+
+private suspend fun getChunkFileSpec(
+  type: ResourceType,
+  fileName: String,
+  packageName: String,
+  moduleDir: String,
+  idToResources: Map<String, List<ResourceAccessorItem>>,
+) {
+  val content = buildString {
+    append("""
+        @file:OptIn(org.jetbrains.compose.resources.InternalResourceApi::class)
+
+        package $packageName
+
+        import kotlin.OptIn
+        import org.jetbrains.compose.resources.${type.resourceName}
+        import org.jetbrains.compose.resources.$RESOURCE_ITEM_CLASS
+        import org.jetbrains.compose.resources.$LANGUAGE_QUALIFIER
+        import org.jetbrains.compose.resources.$REGION_QUALIFIER
+        import org.jetbrains.compose.resources.$THEME_QUALIFIER
+        import org.jetbrains.compose.resources.$DENSITY_QUALIFIER
+        
+      """.trimIndent())
+    appendLine()
+    idToResources.forEach { (resName, items) ->
+      appendLine(
+        buildString {
+          append("internal val Res.${type.accessorName}.${resName}: ${type.resourceName} get() = ")
+          append("${type.resourceName}(\"${type.typeName}:${resName}\", ${if (type.isStringType) "\"$resName\", " else ""}")
+          append(items.joinToString(prefix = "setOf(", postfix = "))") { item ->
+            buildString {
+              append("${RESOURCE_ITEM_CLASS}(")
+              append("setOf(${item.addQualifiers()}),")
+              append("\"${item.path}\",")
+              append("${item.offset},")
+              append(item.size)
+              append(")")
+            }
+          })
+        }
+      )
+    }
+  }
+
+  writeAccessors(moduleDir, fileName, content)
+}
+
+/**
+ * Writes the specified content to a file within the given module directory. This operation is performed
+ * under a write lock and includes file system updates.
+ *
+ * @param moduleDir the directory path of the module where the file should be written
+ * @param fileName the name of the file to write the content to
+ * @param content the content to be written into the specified file
+ */
+@RequiresWriteLock
+private suspend fun writeAccessors(moduleDir: String, fileName: String, content: String): Unit = writeAction {
+  val path = Path.of(moduleDir, fileName)
+  runUndoTransparentWriteAction {
+    Files.writeString(path, content)
+  }
+
+  path.toVirtualFile()?.let { virtualFile ->
+    virtualFile.refresh(false, false)
+    FileDocumentManager.getInstance().reloadFiles(virtualFile)
+  }
+}
+
+
+private fun sortResources(
+  resources: Map<ResourceType, Map<String, List<ResourceAccessorItem>>>,
+): TreeMap<ResourceType, TreeMap<String, List<ResourceAccessorItem>>> {
+  val result = TreeMap<ResourceType, TreeMap<String, List<ResourceAccessorItem>>>()
+  resources
+    .entries
+    .forEach { (type, items) ->
+      val typeResult = TreeMap<String, List<ResourceAccessorItem>>()
+      items
+        .entries
+        .forEach { (name, resItems) ->
+          typeResult[name] = resItems.sortedBy { it.name }
+        }
+      result[type] = typeResult
+    }
+  return result
+}
+
+
+internal fun String.uppercaseFirstChar(): String =
+  transformFirstCharIfNeeded(
+    shouldTransform = { it.isLowerCase() },
+    transform = { it.uppercaseChar() }
+  )
+
+private inline fun String.transformFirstCharIfNeeded(
+  shouldTransform: (Char) -> Boolean,
+  transform: (Char) -> Char,
+): String {
+  if (isNotEmpty()) {
+    val firstChar = this[0]
+    if (shouldTransform(firstChar)) {
+      val sb = StringBuilder(length)
+      sb.append(transform(firstChar))
+      sb.append(this, 1, length)
+      return sb.toString()
+    }
+  }
+  return this
+}
+
+private fun ResourceAccessorItem.addQualifiers(): String = buildString {
+  val languageRegex = Regex("[a-z]{2,3}")
+  val regionRegex = Regex("r[A-Z]{2}")
+
+  val qualifiersMap = mutableMapOf<String, String>()
+
+  fun saveQualifier(className: String, qualifier: String) {
+    qualifiersMap[className]?.let {
+      error("${path} contains repetitive qualifiers: '$it' and '$qualifier'.")
+    }
+    qualifiersMap[className] = qualifier
+  }
+
+  qualifiers.forEach { q ->
+    when (q) {
+      "light",
+      "dark",
+        -> {
+        saveQualifier(THEME_QUALIFIER, q)
+      }
+
+      "mdpi",
+      "hdpi",
+      "xhdpi",
+      "xxhdpi",
+      "xxxhdpi",
+      "ldpi",
+        -> {
+        saveQualifier(DENSITY_QUALIFIER, q)
+      }
+
+      else -> when {
+        q.matches(languageRegex) -> {
+          saveQualifier(LANGUAGE_QUALIFIER, q)
+        }
+
+        q.matches(regionRegex) -> {
+          saveQualifier(REGION_QUALIFIER, q)
+        }
+
+        else -> error("${path} contains unknown qualifier: '$q'.")
+      }
+    }
+  }
+  qualifiersMap[THEME_QUALIFIER]?.let { q -> append("$THEME_QUALIFIER.${q.uppercase()}, ") }
+  qualifiersMap[DENSITY_QUALIFIER]?.let { q -> append("$DENSITY_QUALIFIER.${q.uppercase()}, ") }
+  qualifiersMap[LANGUAGE_QUALIFIER]?.let { q -> append("$LANGUAGE_QUALIFIER(\"$q\"), ") }
+  qualifiersMap[REGION_QUALIFIER]?.let { q ->
+    val lang = qualifiersMap[LANGUAGE_QUALIFIER]
+    if (lang == null) {
+      error("Region qualifier must be used only with language.\nFile: ${path}")
+    }
+    val langAndRegion = "$lang-$q"
+    if (!path.toString().contains("-$langAndRegion")) {
+      error("Region qualifier must be declared after language: '$langAndRegion'.\nFile: ${path}")
+    }
+    append("$REGION_QUALIFIER(\"${q.takeLast(2)}\"), ")
+  }
+}
---
+++ plugins/compose/intellij.compose.ide.plugin.resources/src/com/intellij/compose/ide/plugin/resources/psi/ComposeResourcesPsiChangesListener.kt
@@ -0,0 +1,129 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.compose.ide.plugin.resources.psi
+
+import com.intellij.compose.ide.plugin.resources.ComposeResourcesGenerationService
+import com.intellij.compose.ide.plugin.resources.ComposeResourcesManager
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.intellij.psi.*
+import com.intellij.psi.xml.*
+import org.jetbrains.kotlin.asJava.namedUnwrappedElement
+import org.jetbrains.kotlin.idea.KotlinLanguage
+
+/**
+ * Heavily inspired by [com.android.tools.idea.res.ResourceNotificationManager]
+ */
+private val PsiTreeChangeEvent.isIgnorable: Boolean
+  get() {
+    if (file?.language == KotlinLanguage.INSTANCE) return true
+    val parentName = file?.parent?.name /* for string values */ ?: parent?.namedUnwrappedElement?.name // for files
+    if (!listOf("drawable", "values", "font").any { parentName?.startsWith(it) == true }) return true
+
+    // We can ignore edits in whitespace, XML error nodes, and modification in comments.
+    // (Note that editing text in an attribute value, including whitespace characters,
+    // is not a PsiWhiteSpace element; it's an XmlToken of token type XML_ATTRIBUTE_VALUE_TOKEN
+    // Moreover, We only ignore the modification of commented texts (in such case the type of
+    // parent is XmlComment), because the user may *mark* some components/attributes as comments
+    // for debugging purpose. In that case the child is instance of XmlComment but parent isn't,
+    // so we will NOT ignore the event.
+    if (child is PsiErrorElement || parent is XmlComment) return true
+
+    // Editing text or whitespace has no effect outside values files.
+    if (child is PsiWhiteSpace || child is XmlText || parent is XmlText) return true
+
+    val file = this.file
+    // Spurious events from the IDE doing internal things, such as the formatter using a light
+    // virtual filesystem to process text formatting chunks etc.
+    return file != null && (file.parent == null || !file.viewProvider.isPhysical)
+  }
+
+class ComposeResourcesPsiChangesListener(private val project: Project) : PsiTreeChangeAdapter() {
+  private var ignoreChildrenChanged = false
+
+  override fun beforeChildrenChange(event: PsiTreeChangeEvent) {
+    ignoreChildrenChanged = false
+  }
+
+  override fun childAdded(event: PsiTreeChangeEvent) {
+    ignoreChildrenChanged = true
+    if (event.isIgnorable) return
+
+    val child = event.child ?: return
+    val parent = event.parent ?: return
+
+    if (child is XmlAttribute && parent is XmlTag) {
+      // Typing in a new attribute. Don't need to do any rendering until there is an actual value.
+      if (child.valueElement == null) return
+    }
+    else if (parent is XmlAttribute && child is XmlAttributeValue) {
+      if (child.value.isEmpty()) return // Just added a new blank attribute; nothing to render yet.
+    }
+
+    notice(event)
+  }
+
+  // todo allow to trigger generation in case misplaced files
+  override fun childRemoved(event: PsiTreeChangeEvent) {
+    ignoreChildrenChanged = true
+    if (event.isIgnorable && event.parent?.namedUnwrappedElement?.name != "composeResources") return
+
+    val child = event.child ?: return
+    val parent = event.parent ?: return
+    if (parent is XmlAttribute && child is XmlToken) {
+      // Typing in attribute name. Don't need to do any rendering until there is an actual
+      // value.
+      val valueElement = parent.valueElement
+      if (valueElement == null || valueElement.value.isEmpty()) {
+        return
+      }
+    }
+
+    notice(event)
+  }
+
+  override fun childReplaced(event: PsiTreeChangeEvent) {
+    ignoreChildrenChanged = true
+    if (event.isIgnorable) return
+
+    // skip changes in text values, they don't influence accessors
+    if (event.parent is XmlText) return
+
+    val child = event.child
+    if (child is PsiWhiteSpace) return
+    val parent = event.parent
+    if (parent is XmlAttribute && child is XmlToken) {
+      // Typing in attribute name. Don't need to do any rendering until there is an actual value.
+      val valueElement = parent.valueElement
+      if (valueElement == null || valueElement.value.isEmpty()) return
+    }
+    else if (parent is XmlAttributeValue && child is XmlToken && event.oldChild != null) {
+      val newText = child.getText()
+      val prevText = event.oldChild.text
+      if (newText.isEmpty() && prevText.isEmpty()) return
+    }
+
+    notice(event)
+  }
+
+  override fun childMoved(event: PsiTreeChangeEvent) {
+    if (event.isIgnorable) return
+    notice(event)
+  }
+
+  override fun childrenChanged(event: PsiTreeChangeEvent) {
+    if (ignoreChildrenChanged) return
+    notice(event)
+  }
+
+  private fun notice(event: PsiTreeChangeEvent) {
+    if (project.isDisposed) return
+    if (event.isIgnorable) return
+    val virtualFile = event.file?.virtualFile /* for string values */ ?: (event.parent as? PsiDirectory)?.virtualFile /* for files */
+                      ?: return
+    val composeResourcesGenerationService = project.service<ComposeResourcesGenerationService>()
+    val composeResourcesManager = project.service<ComposeResourcesManager>()
+    composeResourcesManager.findComposeResourcesDirFor(virtualFile.path)?.let { composeResourcesDir ->
+      composeResourcesGenerationService.tryEmit(composeResourcesDir)
+    }
+  }
+}
