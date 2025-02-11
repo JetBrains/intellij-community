253df4dda351906c8814771cf8bbfd60c7f2804e
Louis Vignier <louis.vignier@jetbrains.com>
2025-02-11 13:22:47 +0100

    [sdk] Convert UnknownSdkEditorNotification to Kotlin
    GitOrigin-RevId: f683a02ac9df3a0f1ce9eeff22eb9cd4603f0f6b

--- platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkEditorNotification.kt
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkEditorNotification.kt
@@ -1,72 +1,74 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.projectRoots.impl;
+package com.intellij.openapi.projectRoots.impl
 
-import com.intellij.openapi.components.Service;
-import com.intellij.openapi.fileEditor.FileEditor;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.VerticalFlowLayout;
-import com.intellij.openapi.util.registry.Registry;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.ui.EditorNotificationPanel;
-import com.intellij.ui.EditorNotificationProvider;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Function;
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.fileEditor.FileEditor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.VerticalFlowLayout
+import com.intellij.openapi.util.registry.Registry.Companion.`is`
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.EditorNotificationPanel
+import com.intellij.ui.EditorNotificationProvider
+import kotlinx.coroutines.CoroutineScope
+import org.jetbrains.annotations.ApiStatus
+import java.util.List
+import java.util.concurrent.atomic.AtomicReference
+import java.util.function.Function
+import javax.swing.JComponent
+import javax.swing.JPanel
 
 @ApiStatus.Internal
 @Service(Service.Level.PROJECT)
-public final class UnknownSdkEditorNotification {
-  public static @NotNull UnknownSdkEditorNotification getInstance(@NotNull Project project) {
-    return project.getService(UnknownSdkEditorNotification.class);
+class UnknownSdkEditorNotification(val scope: CoroutineScope) {
+  private val notifications = AtomicReference<MutableList<UnknownSdkFix>>(mutableListOf<UnknownSdkFix>())
+
+  fun allowProjectSdkNotifications(): Boolean {
+    return notifications.get().isEmpty()
   }
 
-  private final AtomicReference<List<UnknownSdkFix>> notifications = new AtomicReference<>(Collections.emptyList());
-
-  public boolean allowProjectSdkNotifications() {
-    return notifications.get().isEmpty();
+  fun getNotifications(): MutableList<UnknownSdkFix> {
+    return notifications.get()
   }
 
-  public @NotNull List<UnknownSdkFix> getNotifications() {
-    return notifications.get();
-  }
-
-  public void showNotifications(@NotNull List<? extends UnknownSdkFix> notifications) {
-    if (!notifications.isEmpty() && !Registry.is("unknown.sdk.show.editor.actions")) {
-      notifications = Collections.emptyList();
+  fun showNotifications(notifications: MutableList<out UnknownSdkFix?>) {
+    var notifications = notifications
+    if (!notifications.isEmpty() && !`is`("unknown.sdk.show.editor.actions")) {
+      notifications = mutableListOf<UnknownSdkFix?>()
     }
-    this.notifications.set(List.copyOf(notifications));
+    this.notifications.set(List.copyOf<UnknownSdkFix?>(notifications))
+  }
+
+  companion object {
+    @JvmStatic
+    fun getInstance(project: Project): UnknownSdkEditorNotification {
+      return project.getService(UnknownSdkEditorNotification::class.java)
+    }
   }
 }
 
-final class UnknownSdkEditorNotificationsProvider implements EditorNotificationProvider {
-  @Override
-  public @NotNull Function<? super @NotNull FileEditor, ? extends @Nullable JComponent> collectNotificationData(@NotNull Project project,
-                                                                                                                @NotNull VirtualFile file) {
-    return editor -> {
-      final var sdkService = UnknownSdkEditorNotification.getInstance(project);
-      List<EditorNotificationPanel> panels = new ArrayList<>();
-      for (UnknownSdkFix info : sdkService.getNotifications()) {
+internal class UnknownSdkEditorNotificationsProvider : EditorNotificationProvider {
+  override fun collectNotificationData(
+    project: Project,
+    file: VirtualFile
+  ): Function<in FileEditor, out JComponent?>? {
+    return Function { editor: FileEditor ->
+      val sdkService: UnknownSdkEditorNotification = UnknownSdkEditorNotification.Companion.getInstance(project)
+      val panels: MutableList<EditorNotificationPanel?> = ArrayList()
+      for (info in sdkService.getNotifications()) {
         if (info.isRelevantFor(project, file)) {
-          panels.add(new UnknownSdkEditorPanel(project, editor, info));
+          panels.add(UnknownSdkEditorPanel(project, editor, info))
         }
       }
-      if (panels.isEmpty()) {
-        return null;
-      }
-      if (panels.size() == 1) {
-        return panels.get(0);
-      }
 
-      final var panel = new JPanel(new VerticalFlowLayout(0, 0));
-      EditorNotificationPanel.wrapPanels(panels, panel, EditorNotificationPanel.Status.Warning);
-      return panel;
-    };
+      when {
+        panels.isEmpty() -> null
+        panels.size == 1 -> panels[0]
+        else -> {
+          val panel = JPanel(VerticalFlowLayout(0, 0))
+          EditorNotificationPanel.wrapPanels(panels, panel, EditorNotificationPanel.Status.Warning)
+          panel
+        }
+      }
+    }
   }
 }
