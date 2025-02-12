fee37b08731f3ed7f2f05f4614fb6b462195fd90
Sergei Sysoev <Sergey.Sysoev@jetbrains.com>
2025-02-12 16:27:34 +0100

    [fleet] require at least one key in Query<T>.intern
    GitOrigin-RevId: a131ca2afd868b71073fabdd0c1600b49916c2c6

--- fleet/kernel/src/fleet/kernel/rete/Queries.kt
+++ fleet/kernel/src/fleet/kernel/rete/Queries.kt
@@ -444,8 +444,8 @@ data class BoundQuery<T>(val key: Any, val query: Query<T>) : Query<T> by query
  * [Producer]s of interned queries are shared between their dependant queries, allowing to share the work and save memory.
  * It makes sense to [intern] query if it is used to construct more than one other query.
  * */
-fun <T> Query<T>.intern(vararg key: Any): Query<T> =
-  internImpl(listOf(*key))
+fun <T> Query<T>.intern(firstKey: Any, vararg keys: Any): Query<T> =
+  internImpl(listOf(firstKey, *keys))
 
 @PublishedApi
 internal fun <T> Query<T>.internImpl(key: Any): Query<T> = 
