6d09ee8153cd985b05098b7db833238a18e923fd
Kajus Kuncaitis <kajus.kuncaitis@jetbrains.com>
2025-02-12 19:39:36 +0100

    [gradle][IDEA-366033] Implemented sync contributor for simple Gradle Declarative projects, expanded tests
    GitOrigin-RevId: 924503a93447fc72281c3f0b5d2915921740f007

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/syncContributor/entitites/GradleDeclarativeEntitySource.java
+++
@@ -1,8 +0,0 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.gradle.service.syncContributor.entitites;
-
-import com.intellij.platform.workspace.storage.url.VirtualFileUrl;
-
-public record GradleDeclarativeEntitySource(VirtualFileUrl projectRootUrl) implements GradleEntitySource {
-
-}
