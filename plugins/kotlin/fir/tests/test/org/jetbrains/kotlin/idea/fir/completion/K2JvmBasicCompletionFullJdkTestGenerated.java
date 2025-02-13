15c313e98f249c3dc47e04b3a1d82ab81a1c9a6d
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2025-02-13 15:51:27 +0100

    [kotlin] KTIJ-33063 isFirstStatement replaced with an explicit equality check in the target block
    next steps: parameter name completion before an existing arrow
    
    GitOrigin-RevId: 8a85ed731effec4bf33cdd3de186768ac125e08f

--- plugins/kotlin/fir/tests/test/org/jetbrains/kotlin/idea/fir/completion/K2JvmBasicCompletionFullJdkTestGenerated.java
+++ plugins/kotlin/fir/tests/test/org/jetbrains/kotlin/idea/fir/completion/K2JvmBasicCompletionFullJdkTestGenerated.java
@@ -33,6 +33,31 @@ public abstract class K2JvmBasicCompletionFullJdkTestGenerated extends AbstractK
             KotlinTestUtils.runTest(this::doTest, this, testDataFilePath);
         }
 
+        @TestMetadata("NoParameterName1.kt")
+        public void testNoParameterName1() throws Exception {
+            runTest("../../completion/testData/basic/fullJdk/lambdaSignature/NoParameterName1.kt");
+        }
+
+        @TestMetadata("NoParameterName2.kt")
+        public void testNoParameterName2() throws Exception {
+            runTest("../../completion/testData/basic/fullJdk/lambdaSignature/NoParameterName2.kt");
+        }
+
+        @TestMetadata("NoRemainingParameterName1.kt")
+        public void testNoRemainingParameterName1() throws Exception {
+            runTest("../../completion/testData/basic/fullJdk/lambdaSignature/NoRemainingParameterName1.kt");
+        }
+
+        @TestMetadata("NoRemainingParameterName2.kt")
+        public void testNoRemainingParameterName2() throws Exception {
+            runTest("../../completion/testData/basic/fullJdk/lambdaSignature/NoRemainingParameterName2.kt");
+        }
+
+        @TestMetadata("NoRemainingParameterName3.kt")
+        public void testNoRemainingParameterName3() throws Exception {
+            runTest("../../completion/testData/basic/fullJdk/lambdaSignature/NoRemainingParameterName3.kt");
+        }
+
         @TestMetadata("ParameterName1.kt")
         public void testParameterName1() throws Exception {
             runTest("../../completion/testData/basic/fullJdk/lambdaSignature/ParameterName1.kt");
