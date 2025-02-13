be2a54503c286627e90284718deec52231be418b
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2025-02-13 13:02:11 +0200

    [Kotlin] K2: implement 'redundant interpolation prefix' inspection
    KTIJ-30319
    
    GitOrigin-RevId: 8ba247639e1e153916536c5410f3df9ea2d939c6

---
+++ plugins/kotlin/idea/tests/testData/quickfix/redundantInterpolationPrefix/prefix2.kt
@@ -0,0 +1,8 @@
+// "Remove redundant interpolation prefix" "true"
+// COMPILER_ARGUMENTS: -Xmulti-dollar-interpolation
+
+fun test() {
+    <caret>$$"sample text"
+}
+
+// FUS_K2_QUICKFIX_NAME: org.jetbrains.kotlin.idea.k2.codeinsight.inspections.diagnosticBased.RemoveRedundantInterpolationQuickFix
