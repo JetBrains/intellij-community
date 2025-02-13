73ad8995f6c31a9a80529d250478108542b05526
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-02-13 14:27:15 +0100

    mark UpdateResult as internal API
    GitOrigin-RevId: a32b0e83e2bb2c7a357abfa7a5452aae91cd04c0

--- platform/platform-impl/src/com/intellij/openapi/keymap/impl/IdeKeyEventDispatcher.kt
+++ platform/platform-impl/src/com/intellij/openapi/keymap/impl/IdeKeyEventDispatcher.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 @file:Suppress("ReplacePutWithAssignment", "ReplaceGetOrSet")
 
 package com.intellij.openapi.keymap.impl
@@ -804,7 +804,8 @@ private fun hasMnemonicInBalloons(container: Container?, code: Int): Boolean {
   return false
 }
 
-data class UpdateResult(val action: AnAction, val event: AnActionEvent, val startedAt: Long)
+@ApiStatus.Internal
+data class UpdateResult(@JvmField val action: AnAction, @JvmField val event: AnActionEvent, @JvmField val startedAt: Long)
 
 private suspend fun doUpdateActionsInner(actions: List<AnAction>,
                                          updater: suspend (AnAction) -> Presentation,
