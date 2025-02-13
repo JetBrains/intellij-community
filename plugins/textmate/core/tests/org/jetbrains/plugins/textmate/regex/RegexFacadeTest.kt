50ba760ca510560771d703df90aa031c7067054a
Alexander Zolotov <zolotov@jetbrains.com>
2025-02-13 14:25:20 +0100

    [textmate] add tests for not matching begin position
    GitOrigin-RevId: 0a6f98b9d7ac22582bc922bb64766b478d8cdd11

--- plugins/textmate/core/tests/org/jetbrains/plugins/textmate/regex/RegexFacadeTest.kt
+++ plugins/textmate/core/tests/org/jetbrains/plugins/textmate/regex/RegexFacadeTest.kt
@@ -1,7 +1,10 @@
 package org.jetbrains.plugins.textmate.regex
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 abstract class RegexFacadeTest {
   @Test
@@ -55,6 +58,45 @@ abstract class RegexFacadeTest {
     val match = regex.match(textMateString, null)
     val range = match.charRange(textMateString)
     assertEquals("мир", string.substring(range.start, range.end))
+  }
+
+  @Test
+  fun matchBeginPosition() {
+    val regex = regex("\\Gbar")
+    val string = TextMateString.fromString("foo bar")
+    val noBeginMatch = regex.match(string,
+                                   4,
+                                   matchBeginPosition = false,
+                                   matchBeginString = true,
+                                   null)
+    assertFalse(noBeginMatch.matched)
+
+    val beginMatch = regex.match(string,
+                                 4,
+                                 matchBeginPosition = true,
+                                 matchBeginString = true,
+                                 null)
+    assertTrue(beginMatch.matched)
+  }
+
+  @Disabled("Joni doesn't support disabling \\A")
+  @Test
+  fun matchBeginString() {
+    val regex = regex("\\Afoo")
+    val string = TextMateString.fromString("foo bar")
+    val noBeginMatch = regex.match(string,
+                                   0,
+                                   matchBeginPosition = true,
+                                   matchBeginString = false,
+                                   null)
+    assertFalse(noBeginMatch.matched)
+
+    val beginMatch = regex.match(string,
+                                 0,
+                                 matchBeginPosition = true,
+                                 matchBeginString = true,
+                                 null)
+    assertTrue(beginMatch.matched)
   }
 
   protected abstract fun regex(s: String): RegexFacade
