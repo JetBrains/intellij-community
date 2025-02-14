c6dca8191af77436d66fe4dffb1223672b3d7502
Victor Turansky <victor.turansky@jetbrains.com>
2025-02-14 21:58:52 +0200

    [html] WEB-68929 Convert `HTMLParserDefinition` to Kotlin
    GitOrigin-RevId: 000add0b0f1a3c8fc147581e226b638c39d47791

--- xml/xml-psi-impl/src/com/intellij/lang/html/HTMLParserDefinition.kt
+++ xml/xml-psi-impl/src/com/intellij/lang/html/HTMLParserDefinition.kt
@@ -1,83 +1,73 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.lang.html;
+package com.intellij.lang.html
 
-import com.intellij.html.embedding.HtmlCustomEmbeddedContentTokenType;
-import com.intellij.lang.ASTNode;
-import com.intellij.lang.ParserDefinition;
-import com.intellij.lang.PsiParser;
-import com.intellij.lang.xml.XMLParserDefinition;
-import com.intellij.lexer.HtmlLexer;
-import com.intellij.lexer.Lexer;
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.FileViewProvider;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.impl.source.html.HtmlEmbeddedContentImpl;
-import com.intellij.psi.impl.source.html.HtmlFileImpl;
-import com.intellij.psi.impl.source.xml.stub.XmlStubBasedElementType;
-import com.intellij.psi.tree.IFileElementType;
-import com.intellij.psi.tree.TokenSet;
-import com.intellij.psi.util.UnsupportedNodeElementTypeException;
-import com.intellij.psi.xml.HtmlFileElementType;
-import com.intellij.psi.xml.XmlElementType;
-import com.intellij.psi.xml.XmlTokenType;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.html.embedding.HtmlCustomEmbeddedContentTokenType
+import com.intellij.lang.ASTNode
+import com.intellij.lang.ParserDefinition
+import com.intellij.lang.ParserDefinition.SpaceRequirements
+import com.intellij.lang.PsiParser
+import com.intellij.lang.xml.XMLParserDefinition.Companion.canStickTokensTogether
+import com.intellij.lexer.HtmlLexer
+import com.intellij.lexer.Lexer
+import com.intellij.openapi.project.Project
+import com.intellij.psi.FileViewProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.impl.source.html.HtmlEmbeddedContentImpl
+import com.intellij.psi.impl.source.html.HtmlFileImpl
+import com.intellij.psi.impl.source.xml.stub.XmlStubBasedElementType
+import com.intellij.psi.tree.IFileElementType
+import com.intellij.psi.tree.TokenSet
+import com.intellij.psi.util.UnsupportedNodeElementTypeException
+import com.intellij.psi.xml.HtmlFileElementType
+import com.intellij.psi.xml.XmlElementType
+import com.intellij.psi.xml.XmlTokenType
 
-public class HTMLParserDefinition implements ParserDefinition {
-  public static final IFileElementType FILE_ELEMENT_TYPE = new HtmlFileElementType();
+open class HTMLParserDefinition :
+  ParserDefinition {
 
-  @Override
-  public @NotNull Lexer createLexer(Project project) {
-    return new HtmlLexer();
-  }
+  override fun createLexer(project: Project?): Lexer =
+    HtmlLexer()
 
-  @Override
-  public @NotNull IFileElementType getFileNodeType() {
-    return XmlElementType.HTML_FILE;
-  }
+  override fun getFileNodeType(): IFileElementType =
+    XmlElementType.HTML_FILE
 
-  @Override
-  public @NotNull TokenSet getWhitespaceTokens() {
-    return XmlTokenType.WHITESPACES;
-  }
+  override fun getWhitespaceTokens(): TokenSet =
+    XmlTokenType.WHITESPACES
 
-  @Override
-  public @NotNull TokenSet getCommentTokens() {
-    return XmlTokenType.COMMENTS;
-  }
+  override fun getCommentTokens(): TokenSet =
+    XmlTokenType.COMMENTS
 
-  @Override
-  public @NotNull TokenSet getStringLiteralElements() {
-    return TokenSet.EMPTY;
-  }
+  override fun getStringLiteralElements(): TokenSet =
+    TokenSet.EMPTY
 
-  @Override
-  public @NotNull PsiParser createParser(final Project project) {
-    return new HTMLParser();
-  }
+  override fun createParser(project: Project?): PsiParser =
+    HTMLParser()
 
-  @Override
-  public @NotNull PsiElement createElement(ASTNode node) {
-    if (node.getElementType() instanceof XmlStubBasedElementType) {
-      //noinspection rawtypes
-      return ((XmlStubBasedElementType)node.getElementType()).createPsi(node);
+  override fun createElement(node: ASTNode): PsiElement {
+    val elementType = node.elementType
+
+    return when {
+      elementType is XmlStubBasedElementType<*, *>
+        -> elementType.createPsi(node)
+
+      elementType is HtmlCustomEmbeddedContentTokenType
+        -> elementType.createPsi(node)
+
+      elementType === XmlElementType.HTML_EMBEDDED_CONTENT
+        -> HtmlEmbeddedContentImpl(node)
+
+      else -> throw UnsupportedNodeElementTypeException(node)
     }
-    if (node.getElementType() instanceof HtmlCustomEmbeddedContentTokenType) {
-      return ((HtmlCustomEmbeddedContentTokenType)node.getElementType()).createPsi(node);
-    }
-    if (node.getElementType() == XmlElementType.HTML_EMBEDDED_CONTENT) {
-      return new HtmlEmbeddedContentImpl(node);
-    }
-    throw new UnsupportedNodeElementTypeException(node);
   }
 
-  @Override
-  public @NotNull PsiFile createFile(@NotNull FileViewProvider viewProvider) {
-    return new HtmlFileImpl(viewProvider);
-  }
+  override fun createFile(viewProvider: FileViewProvider): PsiFile =
+    HtmlFileImpl(viewProvider)
 
-  @Override
-  public @NotNull SpaceRequirements spaceExistenceTypeBetweenTokens(ASTNode left, ASTNode right) {
-    return XMLParserDefinition.canStickTokensTogether(left, right);
+  override fun spaceExistenceTypeBetweenTokens(left: ASTNode, right: ASTNode): SpaceRequirements =
+    canStickTokensTogether(left, right)
+
+  companion object {
+    val FILE_ELEMENT_TYPE: IFileElementType = HtmlFileElementType()
   }
 }
