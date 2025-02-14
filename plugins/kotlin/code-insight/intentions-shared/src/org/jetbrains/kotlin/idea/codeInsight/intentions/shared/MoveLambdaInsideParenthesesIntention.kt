213593a5361170d26c713164e3afaa8f6f688b66
Jinseong Jeon <jsjeon@google.com>
2025-02-14 00:12:57 -0800

    [kotlin] port MoveLambdaInsideParenthesesIntention to Analysis API
    ^KTIJ-31980 fixed

--- plugins/kotlin/code-insight/intentions-shared/src/org/jetbrains/kotlin/idea/codeInsight/intentions/shared/MoveLambdaInsideParenthesesIntention.kt
+++ plugins/kotlin/code-insight/intentions-shared/src/org/jetbrains/kotlin/idea/codeInsight/intentions/shared/MoveLambdaInsideParenthesesIntention.kt
@@ -1,26 +1,58 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
-package org.jetbrains.kotlin.idea.intentions
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.codeInsight.intentions.shared
 
 import com.intellij.codeInsight.intention.LowPriorityAction
-import com.intellij.openapi.editor.Editor
+import com.intellij.codeInspection.util.IntentionFamilyName
+import com.intellij.modcommand.ActionContext
+import com.intellij.modcommand.ModPsiUpdater
+import org.jetbrains.kotlin.analysis.api.KaSession
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.resolution.successfulFunctionCallOrNull
+import org.jetbrains.kotlin.idea.base.psi.moveInsideParenthesesAndReplaceWith
+import org.jetbrains.kotlin.idea.base.psi.shouldLambdaParameterBeNamed
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.caches.resolve.safeAnalyzeNonSourceRootCode
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.intentions.SelfTargetingIntention
-import org.jetbrains.kotlin.idea.core.moveInsideParentheses
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.KotlinApplicableModCommandAction
+import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtLambdaArgument
-import org.jetbrains.kotlin.psi.psiUtil.containsInside
+import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.utils.KotlinExceptionWithAttachments
 
-class MoveLambdaInsideParenthesesIntention : SelfTargetingIntention<KtLambdaArgument>(
-    KtLambdaArgument::class.java, KotlinBundle.lazyMessage("move.lambda.argument.into.parentheses")
-), LowPriorityAction {
-    override fun isApplicableTo(element: KtLambdaArgument, caretOffset: Int): Boolean {
-        val body = element.getLambdaExpression()?.bodyExpression ?: return true
-        return !body.textRange.containsInside(caretOffset)
+internal class MoveLambdaInsideParenthesesIntention :
+    KotlinApplicableModCommandAction<KtLambdaArgument, MoveLambdaInsideParenthesesIntention.Context>(KtLambdaArgument::class),
+    LowPriorityAction {
+    override fun invoke(
+        actionContext: ActionContext,
+        element: KtLambdaArgument,
+        elementContext: Context,
+        updater: ModPsiUpdater
+    ) {
+        val ktExpression = element.getArgumentExpression()?.let(updater::getWritable)
+            ?: throw KotlinExceptionWithAttachments("no argument expression for $this")
+                .withPsiAttachment("lambdaExpression", element)
+        element.moveInsideParenthesesAndReplaceWith(ktExpression, elementContext.lambdaArgumentName)
     }
 
-    override fun applyTo(element: KtLambdaArgument, editor: Editor?) {
-        element.moveInsideParentheses(element.safeAnalyzeNonSourceRootCode())
+    override fun getFamilyName(): @IntentionFamilyName String =
+        KotlinBundle.message("move.lambda.argument.into.parentheses")
+
+    override fun KaSession.prepareContext(element: KtLambdaArgument): Context? {
+        if (element.getArgumentName() != null) {
+            // Already used as named argument
+            return null
+        }
+        val lambdaArgumentName = if (shouldLambdaParameterBeNamed(element)) {
+            val callExpression = element.parent as KtCallExpression
+            element.getArgumentExpression()?.let { expr ->
+                analyze(callExpression) {
+                    callExpression.resolveToCall()?.successfulFunctionCallOrNull()?.argumentMapping[expr]?.name
+                }
+            }
+
+        } else null
+        return Context(lambdaArgumentName)
     }
+
+    data class Context(
+        val lambdaArgumentName: Name?,
+    )
 }
-
