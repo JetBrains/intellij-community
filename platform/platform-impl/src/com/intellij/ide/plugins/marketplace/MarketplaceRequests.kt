454c71c25705783cef525b5dc887226243ab6efd
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-02-14 15:50:27 +0100

    [plugins] IJPL-166096 omit machine id in a request to Marketplace during config-import: requires a loaded service
    GitOrigin-RevId: fa1ecd56d2f09ce9fe0313ddd94f0dbe5c3fb61f

--- platform/platform-impl/src/com/intellij/ide/plugins/marketplace/MarketplaceRequests.kt
+++ platform/platform-impl/src/com/intellij/ide/plugins/marketplace/MarketplaceRequests.kt
@@ -177,8 +177,10 @@ class MarketplaceRequests(private val coroutineScope: CoroutineScope) : PluginIn
 
         val url = URI(MarketplaceUrls.getSearchPluginsUpdatesUrl())
         val os = URLEncoder.encode(OS.CURRENT.name + " " + OS.CURRENT.version, CharsetToolkit.UTF8)
-        val machineId = MachineIdManager.getAnonymizedMachineId("JetBrainsUpdates") // same as regular updates
-          .takeIf { !PropertiesComponent.getInstance().getBoolean(UpdateChecker.MACHINE_ID_DISABLED_PROPERTY, false) }
+        val machineId = if (LoadingState.COMPONENTS_LOADED.isOccurred) {
+          MachineIdManager.getAnonymizedMachineId("JetBrainsUpdates") // same as regular updates
+            .takeIf { !PropertiesComponent.getInstance().getBoolean(UpdateChecker.MACHINE_ID_DISABLED_PROPERTY, false) }
+        } else null
 
         val query = buildString {
           append("build=${ApplicationInfoImpl.orFromPluginCompatibleBuild(buildNumber)}")
