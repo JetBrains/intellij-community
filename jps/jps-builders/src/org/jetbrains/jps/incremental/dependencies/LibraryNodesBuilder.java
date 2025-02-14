7815e2fdf24965a9411d2dd78007761b1a43a0f8
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-02-14 17:00:54 +0100

    [jps lib tracker] fix "java.nio.file.FileSystemAlreadyExistsException      	at jdk.zipfs/jdk.nio.zipfs.ZipFileSystemProvider.newFileSystem(ZipFileSystemProvider.java:104)"
    GitOrigin-RevId: 6d7fbc9cc85ad748508ba03a24dcc4839344e9c4

--- jps/jps-builders/src/org/jetbrains/jps/incremental/dependencies/LibraryNodesBuilder.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/dependencies/LibraryNodesBuilder.java
@@ -2,7 +2,8 @@
 package org.jetbrains.jps.incremental.dependencies;
 
 import com.intellij.compiler.instrumentation.FailSafeClassReader;
-import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.util.lang.HashMapZipFile;
+import com.intellij.util.lang.ImmutableZipEntry;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.dependency.GraphConfiguration;
 import org.jetbrains.jps.dependency.Node;
@@ -11,13 +12,9 @@ import org.jetbrains.jps.dependency.java.JVMClassNode;
 import org.jetbrains.jps.dependency.java.JvmClassNodeBuilder;
 
 import java.io.IOException;
-import java.net.URI;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 final class LibraryNodesBuilder {
   private final GraphConfiguration myGraphConfig;
@@ -26,40 +23,42 @@ final class LibraryNodesBuilder {
     myGraphConfig = graphConfig;
   }
 
-  public Iterable<Node<?, ?>> processLibraryRoot(final String namespace, NodeSource libRoot) throws IOException {
+  public Iterable<Node<?, ?>> processLibraryRoot(@NotNull String namespace, NodeSource libRoot) throws IOException {
     if (!LibraryDef.isLibraryPath(libRoot)) {
-      return Collections.emptyList();
+      return List.of();
     }
+
     Path jarPath = myGraphConfig.getPathMapper().toPath(libRoot);
+    String filePrefix = jarPath + "/";
+    HashMapZipFile zipFile = HashMapZipFile.loadIfNotEmpty(jarPath);
+    if (zipFile == null) {
+      return List.of();
+    }
+
     List<Node<?, ?>> nodes = new ArrayList<>();
-    URI uri = URI.create("jar:" + jarPath.toUri());
-    try (FileSystem fs = FileSystems.newFileSystem(uri, Map.of())) {
-      for (Path root : fs.getRootDirectories()) {
-        Files.walkFileTree(root, new SimpleFileVisitor<>() {
-          @Override
-          public FileVisitResult visitFile(@NotNull Path file, BasicFileAttributes attrs) throws IOException {
-            if (LibraryDef.isClassFile(getFileName(file))) {
-              addNode(file, namespace, nodes);
-            }
-            return FileVisitResult.CONTINUE;
-          }
-        });
+    try (zipFile) {
+      for (ImmutableZipEntry entry : zipFile.getEntries()) {
+        if (!entry.isDirectory() && LibraryDef.isClassFile(entry.getName())) {
+          addNode(entry.getData(zipFile), filePrefix + entry.getName(), namespace, nodes);
+        }
       }
+    }
+    catch (IOException e) {
+      throw e;
+    }
+    catch (Exception e) {
+      throw new RuntimeException(e);
     }
     return nodes;
   }
 
-  private static void addNode(@NotNull Path classFile, String namespace, List<Node<?, ?>> acc) throws IOException {
-    FailSafeClassReader reader = new FailSafeClassReader(Files.readAllBytes(classFile));
-    JVMClassNode<?, ?> node = JvmClassNodeBuilder.createForLibrary("$" + namespace + FileUtil.toSystemIndependentName(classFile.toString()), reader).getResult();
+  private static void addNode(byte @NotNull [] classFileData, @NotNull String classFileName, @NotNull String namespace, List<Node<?, ?>> acc)
+    throws IOException {
+    FailSafeClassReader reader = new FailSafeClassReader(classFileData);
+    JVMClassNode<?, ?> node = JvmClassNodeBuilder.createForLibrary("$" + namespace + classFileName, reader).getResult();
     if (node.getFlags().isPublic()) {
       // todo: maybe too restrictive
       acc.add(node);
     }
-  }
-
-  private static String getFileName(@NotNull Path path) {
-    Path fname = path.getFileName();
-    return fname == null? "" : fname.toString();
   }
 }
