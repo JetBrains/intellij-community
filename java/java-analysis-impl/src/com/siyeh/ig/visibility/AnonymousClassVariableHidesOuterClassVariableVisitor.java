49758f2c4a8ca11054ad0aa2c8c8256a33b9c246
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-02-15 11:09:25 +0100

    fix AnonymousClassVariableHidesOuterClassVariableVisitor visibility
    GitOrigin-RevId: 8a8dcd7c4156120d38cc2cf1ee62795bd90138ee

--- java/java-analysis-impl/src/com/siyeh/ig/visibility/AnonymousClassVariableHidesOuterClassVariableVisitor.java
+++ java/java-analysis-impl/src/com/siyeh/ig/visibility/AnonymousClassVariableHidesOuterClassVariableVisitor.java
@@ -1,23 +1,10 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.siyeh.ig.visibility;
 
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.siyeh.ig.BaseInspectionVisitor;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -25,8 +12,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-class AnonymousClassVariableHidesOuterClassVariableVisitor extends BaseInspectionVisitor {
-
+@ApiStatus.Internal
+public final class AnonymousClassVariableHidesOuterClassVariableVisitor extends BaseInspectionVisitor {
   @Override
   public void visitAnonymousClass(@NotNull PsiAnonymousClass aClass) {
     super.visitAnonymousClass(aClass);
@@ -74,8 +61,7 @@ class AnonymousClassVariableHidesOuterClassVariableVisitor extends BaseInspectio
     }
   }
 
-  private static class VariableCollector extends JavaRecursiveElementWalkingVisitor {
-
+  private static final class VariableCollector extends JavaRecursiveElementWalkingVisitor {
     private static final PsiVariable[] EMPTY_VARIABLE_LIST = {};
 
     private final Map<String, List<PsiVariable>> variableMap = new HashMap<>();
