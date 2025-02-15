31126e7b203b8484627a986bdd5b1be90e5a5248
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-02-15 13:18:03 +0100

    fix PsiLoopStatementImpl visibility
    GitOrigin-RevId: 3657b84b3c586292389d52ba933d243e5e00eebe

--- java/java-psi-impl/src/com/intellij/psi/impl/source/tree/java/PsiLoopStatementImpl.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/tree/java/PsiLoopStatementImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.impl.source.tree.java;
 
 import com.intellij.lang.ASTNode;
@@ -7,9 +7,11 @@ import com.intellij.psi.PsiLoopStatement;
 import com.intellij.psi.impl.source.tree.CompositePsiElement;
 import com.intellij.psi.impl.source.tree.TreeElement;
 import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-abstract class PsiLoopStatementImpl extends CompositePsiElement implements PsiLoopStatement {
+@ApiStatus.Internal
+public abstract class PsiLoopStatementImpl extends CompositePsiElement implements PsiLoopStatement {
   protected PsiLoopStatementImpl(IElementType type) {
     super(type);
   }
