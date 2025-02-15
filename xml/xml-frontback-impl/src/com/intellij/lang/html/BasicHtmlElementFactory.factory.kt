86ac5fe3bbed9b02654f2364012265ea51021924
Victor Turansky <victor.turansky@jetbrains.com>
2025-02-15 19:32:44 +0200

    [xml] WEB-68929 Remove redundant element factory adapters
    GitOrigin-RevId: cb6972929932ef53ac757440b13e0bfb120c82cd

--- xml/xml-frontback-impl/src/com/intellij/lang/html/BasicHtmlElementFactory.factory.kt
+++
@@ -1,7 +0,0 @@
-package com.intellij.lang.html
-
-fun BasicHtmlElementFactory(): BasicHtmlElementFactory =
-  Class.forName("com.intellij.lang.html.BackendHtmlElementFactory")
-    .getConstructor()
-    .newInstance() as BasicHtmlElementFactory
-
--- xml/xml-frontback-impl/src/com/intellij/lang/xml/BasicXmlElementFactory.factory.kt
+++
@@ -1,6 +0,0 @@
-package com.intellij.lang.xml
-
-fun BasicXmlElementFactory(): BasicXmlElementFactory =
-  Class.forName("com.intellij.lang.xml.BackendXmlElementFactory")
-    .getConstructor()
-    .newInstance() as BasicXmlElementFactory
