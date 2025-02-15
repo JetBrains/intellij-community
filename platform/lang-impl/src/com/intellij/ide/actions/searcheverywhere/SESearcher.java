8f08ffaac0344e3090fa7cb1959a40614d761c41
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-02-15 08:47:00 +0100

    fix SESearcher visibility
    GitOrigin-RevId: 1a67c4c476838322e5a8c01662aaf12c42b7481d

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SESearcher.java
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SESearcher.java
@@ -1,13 +1,15 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.actions.searcheverywhere;
 
 import com.intellij.openapi.progress.ProgressIndicator;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.Map;
 
-interface SESearcher {
+@ApiStatus.Internal
+public interface SESearcher {
   ProgressIndicator search(@NotNull Map<? extends SearchEverywhereContributor<?>, Integer> contributorsAndLimits,
                            @NotNull String pattern);
 
