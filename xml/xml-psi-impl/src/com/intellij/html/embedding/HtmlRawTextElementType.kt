bfcfb63365e3ed152e063e00de1f1f3e0ccc2fb3
Victor Turansky <victor.turansky@jetbrains.com>
2025-02-18 01:21:11 +0200

    [html] WEB-68929 Use `BasicHtmlRawTextElementFactory` service in `HtmlRawTextElementType`
    GitOrigin-RevId: 39e5627c9277829dbe4e551a6f0d9143f01e93cc

--- xml/xml-psi-impl/src/com/intellij/html/embedding/HtmlRawTextElementType.kt
+++ xml/xml-psi-impl/src/com/intellij/html/embedding/HtmlRawTextElementType.kt
@@ -6,8 +6,8 @@ import com.intellij.lang.PsiBuilder
 import com.intellij.lang.html.HTMLLanguage
 import com.intellij.lexer.HtmlRawTextLexer
 import com.intellij.lexer.Lexer
+import com.intellij.openapi.components.service
 import com.intellij.psi.PsiElement
-import com.intellij.psi.impl.source.html.HtmlRawTextImpl
 import com.intellij.psi.tree.IStrongWhitespaceHolderElementType
 import com.intellij.psi.xml.XmlElementType.XML_TEXT
 
@@ -23,7 +23,10 @@ object HtmlRawTextElementType :
     start.done(XML_TEXT)
   }
 
-  override fun createLexer(): Lexer = HtmlRawTextLexer()
+  override fun createLexer(): Lexer =
+    HtmlRawTextLexer()
 
-  override fun createPsi(node: ASTNode): PsiElement = HtmlRawTextImpl(node)
+  override fun createPsi(node: ASTNode): PsiElement =
+    service<BasicHtmlRawTextElementFactory>()
+      .createRawTextElement(node)
 }
