312100bc24038856caebff61bf99950377a651b5
Victor Turansky <victor.turansky@jetbrains.com>
2025-02-18 01:22:22 +0200

    [html] WEB-68929 Tests. Register `BasicHtmlRawTextElementFactory` service
    GitOrigin-RevId: 431b5bf011205fb7e1e3d7dbb68d8d910b146fd5

--- xml/testFramework/src/com/intellij/xml/testFramework/XmlElementTypeServiceHelper.kt
+++ xml/testFramework/src/com/intellij/xml/testFramework/XmlElementTypeServiceHelper.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.xml.testFramework
 
+import com.intellij.html.embedding.BasicHtmlRawTextElementFactory
 import com.intellij.lang.html.BackendHtmlElementFactory
 import com.intellij.lang.html.BasicHtmlElementFactory
 import com.intellij.lang.xml.BackendXmlElementFactory
@@ -22,6 +23,12 @@ object XmlElementTypeServiceHelper {
 
     application.registerService(
       BasicHtmlElementFactory::class.java,
+      BackendHtmlElementFactory(),
+      testRootDisposable,
+    )
+
+    application.registerService(
+      BasicHtmlRawTextElementFactory::class.java,
       BackendHtmlElementFactory(),
       testRootDisposable,
     )
