8d4b54bbaee28d6c2a3c3e9caccf3a816653e005
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2025-02-17 20:27:54 +0400

    [kotlin] Extract objects into separate files
    #KTIJ-31809
    
    GitOrigin-RevId: 4c03828746d901d7bff7fa403960b639354abfdb

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RemoveUselessIsCheckFix.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RemoveUselessIsCheckFix.kt
@@ -1,19 +1,14 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.quickfix
 
-import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.codeinsight.api.classic.quickfixes.KotlinPsiOnlyQuickFixAction
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.quickfixes.PsiElementSuitabilityCheckers
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.quickfixes.QuickFixesPsiBasedFactory
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtIsExpression
 import org.jetbrains.kotlin.psi.KtPsiFactory
-import org.jetbrains.kotlin.psi.psiUtil.getNonStrictParentOfType
 import org.jetbrains.kotlin.psi.psiUtil.isNull
 
 class RemoveUselessIsCheckFix(element: KtIsExpression, val result: Boolean? = null) : KotlinPsiOnlyQuickFixAction<KtIsExpression>(element) {
@@ -26,13 +21,6 @@ class RemoveUselessIsCheckFix(element: KtIsExpression, val result: Boolean? = nu
             val expressionsText = result?.toString() ?: if (leftHandSide.isNull()) isNegated.toString() else isNegated.not().toString()
             val newExpression = KtPsiFactory(project).createExpression(expressionsText)
             replace(newExpression)
-        }
-    }
-
-    companion object : QuickFixesPsiBasedFactory<PsiElement>(PsiElement::class, PsiElementSuitabilityCheckers.ALWAYS_SUITABLE) {
-        override fun doCreateQuickFix(psiElement: PsiElement): List<IntentionAction> {
-            val expression = psiElement.getNonStrictParentOfType<KtIsExpression>() ?: return emptyList()
-            return listOf(RemoveUselessIsCheckFix(expression))
         }
     }
 }
--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RemoveUselessIsCheckFixForWhen.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RemoveUselessIsCheckFixForWhen.kt
@@ -1,18 +1,12 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.quickfix
 
-import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.codeinsight.api.classic.quickfixes.KotlinPsiOnlyQuickFixAction
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.quickfixes.PsiElementSuitabilityCheckers
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.quickfixes.QuickFixesPsiBasedFactory
 import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.psi.psiUtil.getNonStrictParentOfType
-import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
 
 class RemoveUselessIsCheckFixForWhen(element: KtWhenConditionIsPattern, val compileTimeCheckResult: Boolean? = null) : KotlinPsiOnlyQuickFixAction<KtWhenConditionIsPattern>(element) {
     override fun getFamilyName(): String = KotlinBundle.message("remove.useless.is.check")
@@ -32,14 +26,6 @@ class RemoveUselessIsCheckFixForWhen(element: KtWhenConditionIsPattern, val comp
             val whenEntryExpression = whenEntry.expression ?: return
             val newEntry = KtPsiFactory(project).createWhenEntry("else -> ${whenEntryExpression.text}")
             whenExpression.addBefore(newEntry, whenExpression.closeBrace)
-        }
-    }
-
-    companion object : QuickFixesPsiBasedFactory<PsiElement>(PsiElement::class, PsiElementSuitabilityCheckers.ALWAYS_SUITABLE) {
-        override fun doCreateQuickFix(psiElement: PsiElement): List<IntentionAction> {
-            val expression = psiElement.getNonStrictParentOfType<KtWhenConditionIsPattern>() ?: return emptyList()
-            if (expression.getStrictParentOfType<KtWhenEntry>()?.guard != null) return emptyList()
-            return listOf(RemoveUselessIsCheckFixForWhen(expression))
         }
     }
 }
