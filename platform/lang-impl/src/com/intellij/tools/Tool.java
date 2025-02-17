ed367f0ec5466fadc58255182dd885d56ae9edac
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-02-17 10:05:52 +0100

    [platform] EA-1678975: Run tools under modal progress
    GitOrigin-RevId: b0e19282701cd995f8e35a96c6f9099ebdfe6259

--- platform/lang-impl/src/com/intellij/tools/Tool.java
+++ platform/lang-impl/src/com/intellij/tools/Tool.java
@@ -320,7 +320,8 @@ public class Tool implements SchemeElement {
         if (commandLine == null) {
           return false;
         }
-        OSProcessHandler handler = new OSProcessHandler(commandLine);
+        OSProcessHandler handler =
+          UtilKt.runToolOnBgtWithModality(project, commandLine, myName != null ? myName : commandLine.getExePath());
         handler.addProcessListener(new ToolProcessAdapter(project, synchronizeAfterExecution(), getName()));
         if (processListener != null) {
           handler.addProcessListener(processListener);
