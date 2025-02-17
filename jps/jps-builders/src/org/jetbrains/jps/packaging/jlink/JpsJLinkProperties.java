85102da3c7eb3ec15b0f42344089a6d66954f7ff
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-02-17 18:53:08 +0100

    fix JpsJLinkProperties visibility
    GitOrigin-RevId: 4f698ca3936fde51a0780d05f7b387abf7e7a9a6

--- jps/jps-builders/src/org/jetbrains/jps/packaging/jlink/JpsJLinkProperties.java
+++ jps/jps-builders/src/org/jetbrains/jps/packaging/jlink/JpsJLinkProperties.java
@@ -1,14 +1,16 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.jps.packaging.jlink;
 
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.xmlb.Converter;
 import com.intellij.util.xmlb.annotations.OptionTag;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.jps.model.ex.JpsElementBase;
 
-final class JpsJLinkProperties extends JpsElementBase<JpsJLinkProperties> {
+@ApiStatus.Internal
+public final class JpsJLinkProperties extends JpsElementBase<JpsJLinkProperties> {
   @OptionTag(converter = CompressionLevelConverter.class)
   public CompressionLevel compressionLevel = CompressionLevel.ZERO;
   public boolean verbose;
@@ -35,7 +37,8 @@ final class JpsJLinkProperties extends JpsElementBase<JpsJLinkProperties> {
     verbose = copy.verbose;
   }
 
-  enum CompressionLevel {
+  @ApiStatus.Internal
+  public enum CompressionLevel {
     ZERO(0),
     FIRST(1),
     SECOND(2);
