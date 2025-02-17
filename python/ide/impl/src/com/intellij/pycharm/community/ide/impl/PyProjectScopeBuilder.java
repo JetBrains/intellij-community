268bc9307fc1a416172b95ab68dce8fb2fa5f49d
Vladimir.Koshelev <vladimir.koshelev@jetbrains.com>
2025-02-17 13:24:35 +0100

    [python] move PyProjectScopeBuilder to pycharm community customization
    GitOrigin-RevId: fb38f3038a19bd66302920ce435d03da891a5fb5

--- python/ide/impl/src/com/intellij/pycharm/community/ide/impl/PyProjectScopeBuilder.java
+++ python/ide/impl/src/com/intellij/pycharm/community/ide/impl/PyProjectScopeBuilder.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.jetbrains.python.psi.search;
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.pycharm.community.ide.impl;
 
 import com.intellij.injected.editor.VirtualFileWindow;
 import com.intellij.openapi.project.Project;
