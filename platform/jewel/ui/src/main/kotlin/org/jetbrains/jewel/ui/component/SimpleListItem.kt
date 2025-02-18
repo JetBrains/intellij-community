1466fb490b51f750c9cb3e7f938f19e353752c49
Ivan Morgillo <imorgillo@gmail.com>
2025-02-18 12:56:37 +0100

    revert logic change in SimpleListItem

--- platform/jewel/ui/src/main/kotlin/org/jetbrains/jewel/ui/component/SimpleListItem.kt
+++ platform/jewel/ui/src/main/kotlin/org/jetbrains/jewel/ui/component/SimpleListItem.kt
@@ -130,7 +130,7 @@ public fun SimpleListItem(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(style.metrics.iconTextGap),
     ) {
-        if (icon != null && iconContentDescription != null) {
+        if (icon != null) {
             Icon(modifier = iconModifier.size(16.dp), key = icon, contentDescription = iconContentDescription)
         }
         content()
