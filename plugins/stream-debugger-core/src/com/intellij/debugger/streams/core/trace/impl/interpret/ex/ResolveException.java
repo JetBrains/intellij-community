6eca2cc6ffd4a002a130c1a19f726eda776699ca
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-02-18 16:25:47 +0100

    fix ResolveException visibility
    GitOrigin-RevId: 84472bde22e92af95e5e9aeee4ebb70cb5a40532

--- plugins/stream-debugger-core/src/com/intellij/debugger/streams/core/trace/impl/interpret/ex/ResolveException.java
+++ plugins/stream-debugger-core/src/com/intellij/debugger/streams/core/trace/impl/interpret/ex/ResolveException.java
@@ -1,12 +1,11 @@
 // Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger.streams.core.trace.impl.interpret.ex;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * @author Vitaliy.Bibaev
- */
-class ResolveException extends IllegalStateException {
+@ApiStatus.Internal
+public class ResolveException extends IllegalStateException {
   ResolveException(@NotNull String s) {
     super(s);
   }
