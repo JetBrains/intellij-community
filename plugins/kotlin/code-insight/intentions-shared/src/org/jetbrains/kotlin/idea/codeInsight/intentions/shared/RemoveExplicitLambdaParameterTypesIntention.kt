3f5581929f8bf87890ba74fb33d06e819eedd234
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2025-02-18 20:29:17 +0400

    [kotlin] Migrate to ModCommand API
    #KTIJ-31809
    
    GitOrigin-RevId: 35997b3bb43218dd2d2bc5d9060fb105312261ec

--- plugins/kotlin/code-insight/intentions-shared/src/org/jetbrains/kotlin/idea/codeInsight/intentions/shared/RemoveExplicitLambdaParameterTypesIntention.kt
+++ plugins/kotlin/code-insight/intentions-shared/src/org/jetbrains/kotlin/idea/codeInsight/intentions/shared/RemoveExplicitLambdaParameterTypesIntention.kt
@@ -1,25 +1,40 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.codeInsight.intentions.shared
 
-import com.intellij.openapi.editor.Editor
+import com.intellij.codeInspection.util.IntentionFamilyName
+import com.intellij.modcommand.ActionContext
+import com.intellij.modcommand.ModPsiUpdater
+import com.intellij.openapi.util.TextRange
+import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.intentions.SelfTargetingIntention
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.KotlinApplicableModCommandAction
 import org.jetbrains.kotlin.psi.KtLambdaExpression
 import org.jetbrains.kotlin.psi.KtPsiFactory
 import org.jetbrains.kotlin.psi.psiUtil.endOffset
 
-class RemoveExplicitLambdaParameterTypesIntention : SelfTargetingIntention<KtLambdaExpression>(
-    KtLambdaExpression::class.java,
-    KotlinBundle.lazyMessage("remove.explicit.lambda.parameter.types.may.break.code")
+class RemoveExplicitLambdaParameterTypesIntention : KotlinApplicableModCommandAction<KtLambdaExpression, Unit>(
+    KtLambdaExpression::class
 ) {
-    override fun isApplicableTo(element: KtLambdaExpression, caretOffset: Int): Boolean {
-        if (element.valueParameters.none { it.typeReference != null }) return false
-        val arrow = element.functionLiteral.arrow ?: return false
-        return caretOffset <= arrow.endOffset
+    override fun getFamilyName(): @IntentionFamilyName String = KotlinBundle.message("remove.explicit.lambda.parameter.types.may.break.code")
+
+    override fun isApplicableByPsi(element: KtLambdaExpression): Boolean {
+        return element.valueParameters.any { it.typeReference != null }
     }
 
-    override fun applyTo(element: KtLambdaExpression, editor: Editor?) {
+    override fun getApplicableRanges(element: KtLambdaExpression): List<TextRange> {
+        val arrow = element.functionLiteral.arrow ?: return emptyList()
+        return listOf(TextRange(0, arrow.endOffset - element.startOffset))
+    }
+
+    override fun KaSession.prepareContext(element: KtLambdaExpression): Unit = Unit
+
+    override fun invoke(
+        actionContext: ActionContext,
+        element: KtLambdaExpression,
+        elementContext: Unit,
+        updater: ModPsiUpdater,
+    ) {
         val oldParameterList = element.functionLiteral.valueParameterList!!
 
         val parameterString = oldParameterList.parameters.asSequence().map {
