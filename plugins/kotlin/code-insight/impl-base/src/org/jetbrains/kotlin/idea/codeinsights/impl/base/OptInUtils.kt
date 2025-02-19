97f4af2b1e5ce9d8c5daaec3459fb9979ebe3dd5
Justin Paupore <jpaupore@google.com>
2025-02-18 16:45:50 -0800

    [K2] Port UnnecessaryOptInAnnotationInspection.
    Rewrite UnnecessaryOptInAnnotationInspection targeting Analysis API.
    This logic has been written from scratch, largely mirroring logic in
    FirOptInUsageBaseChecker and friends.
    
    ^KTIJ-31915 fixed

--- plugins/kotlin/code-insight/impl-base/src/org/jetbrains/kotlin/idea/codeinsights/impl/base/OptInUtils.kt
+++ plugins/kotlin/code-insight/impl-base/src/org/jetbrains/kotlin/idea/codeinsights/impl/base/OptInUtils.kt
@@ -5,7 +5,12 @@ import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
 import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.analysis.api.annotations.KaAnnotationValue
+import org.jetbrains.kotlin.analysis.api.symbols.KaCallableSymbol
+import org.jetbrains.kotlin.analysis.api.symbols.KaClassLikeSymbol
+import org.jetbrains.kotlin.analysis.api.symbols.KaClassSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.markers.KaAnnotatedSymbol
+import org.jetbrains.kotlin.analysis.api.types.KaClassType
+import org.jetbrains.kotlin.analysis.api.types.KaType
 import org.jetbrains.kotlin.config.AnalysisFlags
 import org.jetbrains.kotlin.config.ApiVersion
 import org.jetbrains.kotlin.idea.base.projectStructure.languageVersionSettings
@@ -18,44 +23,67 @@ import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 /**
  * Checks whether there's an element lexically above in the tree, annotated with @[OptIn]`(X::class)`, or a declaration
- * annotated with `@X` where [annotationClassId] is the [ClassId] of X.
+ * annotated with `@X` where [optInMarker] is the [ClassId] of X.
  *
  * This is K2 implementation which is very similar to [org.jetbrains.kotlin.resolve.checkers.OptInUsageChecker.Companion.isOptInAllowed].
  * One difference is that check of [SubclassOptInRequired] is not implemented here since it is not needed for current method usages.
  */
 context(KaSession)
-fun KtElement.isOptInAllowed(annotationClassId: ClassId): Boolean {
-    if (annotationClassId.asFqNameString() in languageVersionSettings.getFlag(AnalysisFlags.optIn)) return true
+fun KtElement.isOptInAllowed(optInMarker: ClassId): Boolean {
+    if (optInMarker.asFqNameString() in languageVersionSettings.getFlag(AnalysisFlags.optIn)) return true
 
     return parentsWithSelf.any {
-        it.isDeclarationAnnotatedWith(annotationClassId) ||
-                it.isElementAnnotatedWithOptIn(annotationClassId)
+        it.isDeclarationAnnotatedWith(optInMarker) ||
+                it.isElementAnnotatedWithOptIn(optInMarker)
     }
 }
 
 context(KaSession)
-private fun PsiElement.isDeclarationAnnotatedWith(annotationClassId: ClassId): Boolean =
-    this is KtDeclaration && symbol.annotations.contains(annotationClassId)
+private fun PsiElement.isDeclarationAnnotatedWith(optInMarker: ClassId): Boolean =
+    this is KtDeclaration && symbol.annotations.contains(optInMarker)
 
 context(KaSession)
 private fun KtAnnotationEntry.getAnnotatedOptIns(): Set<ClassId> {
     if (typeReference?.type?.isClassType(OptInNames.OPT_IN_CLASS_ID) != true) return emptySet()
 
-    return valueArguments.mapNotNull { it.getAnnotationClassValue() }.toSet()
+    return valueArguments.mapNotNull {
+        (it.getArgumentExpression() as? KtClassLiteralExpression)?.classLiteralId
+    }.toSet()
 }
 
 context(KaSession)
-@OptIn(KaExperimentalApi::class)
-private fun ValueArgument.getAnnotationClassValue(): ClassId? =
-    getArgumentExpression()
-        ?.evaluateAsAnnotationValue()
-        ?.safeAs<KaAnnotationValue.ClassLiteralValue>()
-        ?.classId
+val KtClassLiteralExpression.classLiteralId: ClassId?
+    get() = (receiverType as? KaClassType)?.classId
 
 context(KaSession)
-private fun PsiElement.isElementAnnotatedWithOptIn(annotationClassId: ClassId): Boolean =
-    this is KtAnnotated && annotationEntries.any { annotationClassId in it.getAnnotatedOptIns() }
+private fun PsiElement.isElementAnnotatedWithOptIn(optInMarker: ClassId): Boolean =
+    this is KtAnnotated && annotationEntries.any { optInMarker in it.getAnnotatedOptIns() }
 
+context(KaSession)
+private fun KaClassLikeSymbol.getSubclassOptInRequiredMarkers(): Set<ClassId> =
+    annotations[OptInNames.SUBCLASS_OPT_IN_REQUIRED_CLASS_ID].flatMapTo(mutableSetOf()) { anno ->
+        anno.arguments.flatMap { arg ->
+            val expression = arg.expression
+            if (expression is KaAnnotationValue.ArrayValue && arg.name == OptInNames.OPT_IN_ANNOTATION_CLASS) {
+                expression.values.mapNotNull {
+                    (it as? KaAnnotationValue.ClassLiteralValue)?.classId
+                }
+            } else {
+                emptyList()
+            }
+        }
+    }
+
+/**
+ * Check the opt-in markers directly required to use the given symbol.
+ *
+ * This does not include any opt-ins necessary surrounding the use of the symbol, such as opt-ins
+ * for type parameters, value parameter types, receiver types, return types, and so on.
+ *
+ * @receiver The symbol for the type or callable being used.
+ * @param possibleOptIns The universe of possible opt-ins to check.
+ * @param moduleApiVersion The Kotlin version in use at the use site, for [WasExperimental] handling.
+ */
 context(KaSession)
 fun KaAnnotatedSymbol.getRequiredOptIns(possibleOptIns: Set<ClassId>, moduleApiVersion: ApiVersion): Set<ClassId> {
     val requiredOptIns = annotations.classIds.toMutableSet()
@@ -73,6 +101,13 @@ fun KaAnnotatedSymbol.getRequiredOptIns(possibleOptIns: Set<ClassId>, moduleApiV
     requiredOptIns.retainAll(possibleOptIns)
 
     return requiredOptIns
+}
+
+context(KaSession)
+fun KaClassLikeSymbol.getRequiredOptInsToSubclass(
+    possibleOptIns: Set<ClassId>,
+): Set<ClassId> {
+    return getSubclassOptInRequiredMarkers() intersect possibleOptIns
 }
 
 context(KaSession)
