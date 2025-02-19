7ebd9b2dc028f1c05b8cedc0eaffa088790705e4
Vladimir.Koshelev <vladimir.koshelev@jetbrains.com>
2025-02-19 14:27:27 +0100

    [plugins] details view: fix gear behavior enable status
    GitOrigin-RevId: 5692487bb59da7fa4ddc31be40597903481f4cc2

--- platform/platform-impl/src/com/intellij/ide/plugins/newui/PluginDetailsPageComponent.kt
+++ platform/platform-impl/src/com/intellij/ide/plugins/newui/PluginDetailsPageComponent.kt
@@ -479,8 +479,6 @@ class PluginDetailsPageComponent @JvmOverloads constructor(
         { this.createUninstallAction() })
       nameAndButtons.addButtonComponent(enableDisableController!!.button.also { gearButton = it })
       nameAndButtons.addButtonComponent(enableDisableController!!.bundledButton.also { myEnableDisableButton = it })
-      myEnableDisableButton?.isEnabled = showComponent?.isNotFreeInFreeMode == true
-      gearButton?.isEnabled = showComponent?.isNotFreeInFreeMode == true
     }
     else {
       gearButton = SelectionBasedPluginModelAction.createGearButton(
@@ -1403,7 +1401,7 @@ class PluginDetailsPageComponent @JvmOverloads constructor(
         val bundled = plugin!!.isBundled
         val isEssential = ApplicationInfo.getInstance().isEssentialPlugin(
           plugin!!.pluginId)
-        gearButton!!.isVisible = !uninstalled && !bundled
+        gearButton!!.isVisible = !uninstalled && !bundled && showComponent?.isNotFreeInFreeMode != true
         myEnableDisableButton!!.isVisible = bundled
         myEnableDisableButton!!.isEnabled = !isEssential && showComponent?.isNotFreeInFreeMode != true
       }
