bc5dc3f2c12bde05845b33d0a83f00eda73f66ce
Alexander Kuznetsov <aleksander.kuznetsov@jetbrains.com>
2025-02-19 17:11:33 +0100

    [rd][debugger] IJPL-177087 XFramesView: prepare for proxy instead of XDebugSession
    GitOrigin-RevId: 000833d51105b41e8e1bae43fec2f41b357ab81e

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/frame/XDebugSessionProxy.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/frame/XDebugSessionProxy.kt
@@ -2,11 +2,53 @@
 package com.intellij.xdebugger.impl.frame
 
 import com.intellij.xdebugger.XDebugSession
+import com.intellij.xdebugger.XSourcePosition
+import com.intellij.xdebugger.frame.XExecutionStack
+import com.intellij.xdebugger.frame.XStackFrame
+import com.intellij.xdebugger.frame.XSuspendContext
+import com.intellij.xdebugger.impl.XDebugSessionImpl
+import com.intellij.xdebugger.impl.XSteppingSuspendContext
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
 interface XDebugSessionProxy {
 
+  fun getFrameSourcePosition(frame: XStackFrame): XSourcePosition?
+  fun getCurrentExecutionStack(): XExecutionStack?
+  fun getCurrentStackFrame(): XStackFrame?
+  fun setCurrentStackFrame(executionStack: XExecutionStack, frame: XStackFrame, isTopFrame: Boolean = executionStack.topFrame == frame)
+  fun hasSuspendContext(): Boolean
+  fun isSteppingSuspendContext(): Boolean
+  fun computeExecutionStacks(provideContainer: () -> XSuspendContext.XExecutionStackContainer)
+
   // TODO WeakReference<XDebugSession>?
-  class Monolith(val session: XDebugSession) : XDebugSessionProxy
+  class Monolith(val session: XDebugSession) : XDebugSessionProxy {
+    override fun getFrameSourcePosition(frame: XStackFrame): XSourcePosition? {
+      return (session as? XDebugSessionImpl)?.getFrameSourcePosition(frame)
+    }
+
+    override fun getCurrentExecutionStack(): XExecutionStack? {
+      return (session as? XDebugSessionImpl)?.currentExecutionStack
+    }
+
+    override fun getCurrentStackFrame(): XStackFrame? {
+      return session.currentStackFrame
+    }
+
+    override fun setCurrentStackFrame(executionStack: XExecutionStack, frame: XStackFrame, isTopFrame: Boolean) {
+      (session as? XDebugSessionImpl)?.setCurrentStackFrame(executionStack, frame, isTopFrame)
+    }
+
+    override fun hasSuspendContext(): Boolean {
+      return session.suspendContext != null
+    }
+
+    override fun isSteppingSuspendContext(): Boolean {
+      return session.suspendContext is XSteppingSuspendContext
+    }
+
+    override fun computeExecutionStacks(provideContainer: () -> XSuspendContext.XExecutionStackContainer) {
+      session.suspendContext?.computeExecutionStacks(provideContainer())
+    }
+  }
 }
