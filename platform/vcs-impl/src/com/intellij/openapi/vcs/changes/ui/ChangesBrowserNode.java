5d0d180b8d41f598a2d13a3c0dee58e23a879fa0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-02-19 18:42:53 +0100

    fix NodeWithCollapsedParents visibility
    GitOrigin-RevId: 09209e3734db49995466d18bc33f145e7ef5491d

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesBrowserNode.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesBrowserNode.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vcs.changes.ui;
 
 import com.intellij.diagnostic.PluginException;
@@ -484,7 +484,8 @@ public abstract class ChangesBrowserNode<T> extends DefaultMutableTreeNode imple
     FilePath getNodeFilePath();
   }
 
-  interface NodeWithCollapsedParents {
+  @ApiStatus.Internal
+  public interface NodeWithCollapsedParents {
     @ApiStatus.Internal
     void addCollapsedParent(@NotNull ChangesBrowserNode<?> parentNode);
   }
