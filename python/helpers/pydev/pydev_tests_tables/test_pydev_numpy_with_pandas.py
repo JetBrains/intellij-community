1fb957615ea0815a8e392d4b4f8d3b8453c5de77
Natalia.Murycheva <natalia.murycheva@jetbrains.com>
2025-02-20 17:53:32 +0100

    [PyCharm Tables] PY-71041 Fixed tests for Python 3
    GitOrigin-RevId: 8e2c6e1cb1a8e2a93f1f1528f6120d596dcbcb30

--- python/helpers/pydev/pydev_tests_tables/test_pydev_numpy_with_pandas.py
+++ python/helpers/pydev/pydev_tests_tables/test_pydev_numpy_with_pandas.py
@@ -140,6 +140,7 @@ def test_get_tables_display_options():
     max_cols, max_colwidth, max_rows = numpy_tables_helpers.__get_tables_display_options()
     assert max_cols is None
     assert max_rows is None
+    import pandas as pd
     if int(pd.__version__.split('.')[0]) < 1:
         assert max_colwidth == numpy_tables_helpers.MAX_COLWIDTH
     else:
