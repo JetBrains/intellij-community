10011c9b792456809cdf314b1ba1c2e19084ddfe
Bas Leijdekkers <basleijdekkers@gmail.com>
2025-02-20 16:08:47 +0100

    Ignore single space before star in comments (IJPL-178777)
    GitOrigin-RevId: 1cddf384644a076d0a71f1a6487e72a78ed25e54

--- java/java-tests/testSrc/com/siyeh/ig/style/ProblematicWhitespaceInspectionTest.java
+++ java/java-tests/testSrc/com/siyeh/ig/style/ProblematicWhitespaceInspectionTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.siyeh.ig.style;
 
 import com.intellij.application.options.CodeStyle;
@@ -58,6 +44,23 @@ public class ProblematicWhitespaceInspectionTest extends LightJavaInspectionTest
              \tString s;
              }
              """);
+  }
+
+  public void testDocComments() {
+    final CodeStyleSettings settings = CodeStyle.getSettings(getProject());
+    settings.getIndentOptions(JavaFileType.INSTANCE).USE_TAB_CHARACTER = true;
+    myFixture.configureByText("X.java", """
+             /**
+              * This is class X.
+              */
+             class X {
+             \t/**
+             \t * This is field s.
+             \t */
+             \tString s;
+             }
+             """);
+    myFixture.testHighlighting(true, false, false);
   }
 
   public void testSpacesInFile() {
