24bc4c891bf21e1a7dcd01648f81e327fb3e5941
Aleksandr Slapoguzov <aleksandr.slapoguzov@jetbrains.com>
2025-02-21 15:53:06 +0100

    [evaluation-plugin] report context collection duration
    GitOrigin-RevId: 91a7beea00bdad606da732e383dcf53fe59a0bf4

--- plugins/evaluation-plugin/core/src/com/intellij/cce/evaluable/AiaConstants.kt
+++ plugins/evaluation-plugin/core/src/com/intellij/cce/evaluable/AiaConstants.kt
@@ -1,6 +1,7 @@
 package com.intellij.cce.evaluable
 
 const val AIA_CONTEXT = "aia_context"
+const val AIA_CONTEXT_COLLECTION_DURATION_MS = "aia_context_collection_duration_ms"
 const val AIA_USER_PROMPT = "aia_user_prompt"
 const val AIA_RESPONSE = "aia_response"
 const val AIA_ORIGINAL_CONTENT = "aia_original_content"
