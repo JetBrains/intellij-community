a1f2ccbf8f723f9c34c59ffcdc009615c78d7596
Roman Golyshev <roman.golyshev@jetbrains.com>
2025-02-21 17:07:10 +0100

    [kotlin] Refactor `ImportQuickFixProvider` and `ImportQuickFixFactories`
    Reuse `ImportQuickFixProvider` as a factory for the more specific factories
    in `ImportQuickFixFactories`
    
    Related issues:
    KTIJ-29655
    KTIJ-32771
    KTIJ-32739
    KTIJ-32143
    
    (cherry picked from commit 547bb0a9e374d114b1bbb6d616ccf87f42d6554f)
    
    IJ-CR-158539
    
    GitOrigin-RevId: ff005eefa896924523bc8fffab1f7796e8223679

--- plugins/kotlin/code-insight/fixes-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/fixes/imprt/ImportQuickFixFactories.kt
+++ plugins/kotlin/code-insight/fixes-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/fixes/imprt/ImportQuickFixFactories.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.k2.codeinsight.fixes.imprt
 
+import org.jetbrains.kotlin.analysis.api.diagnostics.KaDiagnosticWithPsi
 import org.jetbrains.kotlin.analysis.api.fir.diagnostics.KaFirDiagnostic
 import org.jetbrains.kotlin.idea.codeinsight.api.applicators.fixes.KotlinQuickFixFactory
 
@@ -9,23 +10,31 @@ import org.jetbrains.kotlin.idea.codeinsight.api.applicators.fixes.KotlinQuickFi
  * by [org.jetbrains.kotlin.idea.k2.codeinsight.fixes.KotlinFirUnresolvedReferenceQuickFixProvider]
  */
 object ImportQuickFixFactories {
-    val invisibleReferenceFactory = KotlinQuickFixFactory.IntentionBased { diagnostic: KaFirDiagnostic.InvisibleReference ->
-        ImportQuickFixProvider.getFixes(diagnostic)
-    }
+    val invisibleReferenceFactory: KotlinQuickFixFactory.IntentionBased<KaFirDiagnostic.InvisibleReference> =
+        ImportQuickFixProvider.upcast()
 
-    val unresolvedReferenceFactory = KotlinQuickFixFactory.IntentionBased { diagnostic: KaFirDiagnostic.UnresolvedReference ->
-        ImportQuickFixProvider.getFixes(diagnostic)
-    }
+    val unresolvedReferenceFactory: KotlinQuickFixFactory.IntentionBased<KaFirDiagnostic.UnresolvedReference> =
+        ImportQuickFixProvider.upcast()
 
-    val unresolvedReferenceWrongReceiverFactory = KotlinQuickFixFactory.IntentionBased { diagnostic: KaFirDiagnostic.UnresolvedReferenceWrongReceiver ->
-        ImportQuickFixProvider.getFixes(diagnostic)
-    }
-    
-    val delegateSpecialFunctionMissingFactory = KotlinQuickFixFactory.IntentionBased { diagnostic: KaFirDiagnostic.DelegateSpecialFunctionMissing ->
-        ImportQuickFixProvider.getFixes(diagnostic)
-    }
+    val unresolvedReferenceWrongReceiverFactory: KotlinQuickFixFactory.IntentionBased<KaFirDiagnostic.UnresolvedReferenceWrongReceiver> =
+        ImportQuickFixProvider.upcast()
 
-    val delegateSpecialFunctionNoneApplicableFactory = KotlinQuickFixFactory.IntentionBased { diagnostic: KaFirDiagnostic.DelegateSpecialFunctionNoneApplicable ->
-        ImportQuickFixProvider.getFixes(diagnostic)
-    }
+    val delegateSpecialFunctionMissingFactory: KotlinQuickFixFactory.IntentionBased<KaFirDiagnostic.DelegateSpecialFunctionMissing> =
+        ImportQuickFixProvider.upcast()
+
+    val delegateSpecialFunctionNoneApplicableFactory: KotlinQuickFixFactory.IntentionBased<KaFirDiagnostic.DelegateSpecialFunctionNoneApplicable> =
+        ImportQuickFixProvider.upcast()
 }
+
+/**
+ * A utility function to "cast" [KotlinQuickFixFactory.IntentionBased] with [BASE] type
+ * to more specific [DERIVED] type.
+ * 
+ * This should be a safe cast, since the generic in [KotlinQuickFixFactory.IntentionBased] class
+ * is covariant-like (`in`), even though it's not explicitly declared as such.
+ * 
+ * If this changes at some point, this function will no longer be needed.
+ */
+@Suppress("UNCHECKED_CAST")
+private fun <BASE : KaDiagnosticWithPsi<*>, DERIVED : BASE> KotlinQuickFixFactory.IntentionBased<BASE>.upcast(): KotlinQuickFixFactory.IntentionBased<DERIVED> =
+    this as KotlinQuickFixFactory.IntentionBased<DERIVED> 
