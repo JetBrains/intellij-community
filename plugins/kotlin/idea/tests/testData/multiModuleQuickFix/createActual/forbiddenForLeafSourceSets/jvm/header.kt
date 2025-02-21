c03b5f1da5a0bff159c27a3ba28f72f7f57ddb1a
Anna Kozlova <anna.kozlova@jetbrains.com>
2025-02-21 21:08:31 +0100

    [kotlin] k2: provide fix for missed actuals for expect
    ^KTIJ-32214 fixed
    
    GitOrigin-RevId: a127a705515f10cc6d693e6b4655670d76905a39

--- plugins/kotlin/idea/tests/testData/multiModuleQuickFix/createActual/forbiddenForLeafSourceSets/jvm/header.kt
+++ plugins/kotlin/idea/tests/testData/multiModuleQuickFix/createActual/forbiddenForLeafSourceSets/jvm/header.kt
@@ -1,6 +1,7 @@
 // "Add missing actual declarations" "false"
 // ACTION: Convert function to property
 // ERROR: Expected function 'foo' has no actual declaration in module testModule_JVM for JVM
-// IGNORE_K2
+// K2_ACTION: "Create actual in 'testModule_JVM'" "true"
+// K2_TOOL: org.jetbrains.kotlin.idea.k2.codeinsight.inspections.KotlinNoActualForExpectInspection
 
 expect fun <caret>foo(): Int
