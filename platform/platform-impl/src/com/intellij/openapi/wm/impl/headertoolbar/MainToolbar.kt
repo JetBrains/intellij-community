53606adae5e0044db2f767b4e1d17269ff84a9fa
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-02-21 13:40:15 +0200

    IJPL-178858 Adjust main toolbar icons for both dark and light headers
    The origin of the issue is not clear, but for some reason dark icons
    get stuck in the dark state when the IDE was started in a dark theme.
    Because IconLoader has both the dark and the light replacers,
    though, always using it regardless of the current theme seems
    to fix the issue.
    
    There should be no performance issues because these icons
    are cached anyway.
    
    GitOrigin-RevId: 152a21c1793e8c92c25b0a22d2aebea62840bd29

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/headertoolbar/MainToolbar.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/headertoolbar/MainToolbar.kt
@@ -532,7 +532,7 @@ private fun getMainToolbarGroups(): Sequence<GroupInfo> {
 
 internal fun isDarkHeader(): Boolean = ColorUtil.isDark(JBColor.namedColor("MainToolbar.background"))
 
-fun adjustIconForHeader(icon: Icon): Icon = if (isDarkHeader()) IconLoader.getDarkIcon(icon = icon, dark = true) else icon
+fun adjustIconForHeader(icon: Icon): Icon = IconLoader.getDarkIcon(icon = icon, dark = isDarkHeader())
 
 private class HeaderIconUpdater {
   val iconCache = WeakHashMap<Icon, WeakReference<Icon>>()
