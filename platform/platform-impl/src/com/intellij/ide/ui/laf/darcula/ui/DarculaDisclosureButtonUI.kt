181d33c9dc3690e3dc78b34c737cfa15720d6429
Sasha Blashenkov <sasha.blashenkov@jetbrains.com>
2025-02-24 20:39:18 +0100

    [llm] LLM-15059 Toolwindow style refresh: transparent DisclosureButton
    (cherry picked from commit 0ca51bf8a723f95569587465876ef26fc2501835)
    
    GitOrigin-RevId: 9bf9856840678b2e114a5f701e67b9037ed2df25

--- platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaDisclosureButtonUI.kt
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaDisclosureButtonUI.kt
@@ -1,9 +1,11 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.ui.laf.darcula.ui
 
+import com.intellij.icons.AllIcons
 import com.intellij.ide.ui.laf.darcula.DarculaNewUIUtil
 import com.intellij.ui.JBColor
 import com.intellij.ui.components.DisclosureButton
+import com.intellij.ui.components.DisclosureButtonKind
 import com.intellij.ui.scale.JBUIScale
 import com.intellij.ui.util.height
 import com.intellij.ui.util.width
@@ -12,6 +14,7 @@ import com.intellij.util.ui.JBInsets
 import com.intellij.util.ui.JBValue
 import com.intellij.util.ui.UIUtilities
 import org.jetbrains.annotations.ApiStatus
+import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics
 import java.awt.Rectangle
@@ -22,28 +25,55 @@ import kotlin.math.max
 import kotlin.math.min
 
 @ApiStatus.Internal
+interface DarculaDisclosureButtonUIConfig {
+  val ARC: JBValue.UIInteger
+  val TEXT_RIGHT_ICON_GAP: JBValue.UIInteger
+  val DEFAULT_BACKGROUND: JBColor?
+  val HOVER_BACKGROUND: JBColor
+  val PRESSED_BACKGROUND: JBColor
+  val LEFT_MARGIN: Int
+  val RIGHT_MARGIN: Int
+  val HEIGHT: Int
+}
+
+private object DarculaDisclosureButtonUIDefaultConfig : DarculaDisclosureButtonUIConfig {
+  override val ARC = JBValue.UIInteger("DisclosureButton.arc", 16)
+  override val TEXT_RIGHT_ICON_GAP = JBValue.UIInteger("DisclosureButton.textRightIconGap", 8)
+  override val DEFAULT_BACKGROUND = JBColor.namedColor("DisclosureButton.defaultBackground")
+  override val HOVER_BACKGROUND = JBColor.namedColor("DisclosureButton.hoverOverlay")
+  override val PRESSED_BACKGROUND = JBColor.namedColor("DisclosureButton.pressedOverlay")
+  override val LEFT_MARGIN = 14
+  override val RIGHT_MARGIN = 12
+  override val HEIGHT = 34
+}
+
+private object DarculaDisclosureButtonUISimpleConfig : DarculaDisclosureButtonUIConfig {
+  override val ARC = JBValue.UIInteger("DisclosureButton.simple.arc", 16)
+  override val TEXT_RIGHT_ICON_GAP = JBValue.UIInteger("DisclosureButton.simple.textRightIconGap", 8)
+  override val DEFAULT_BACKGROUND = null
+  override val HOVER_BACKGROUND = JBColor.namedColor("DisclosureButton.hoverOverlay")
+  override val PRESSED_BACKGROUND = JBColor.namedColor("DisclosureButton.pressedOverlay")
+  override val LEFT_MARGIN = 9
+  override val RIGHT_MARGIN = 9
+  override val HEIGHT = 28
+}
+
+private val DRAW_DEBUG_LINES = false
+
+@ApiStatus.Internal
 class DarculaDisclosureButtonUI : BasicButtonUI() {
-
   companion object {
-
     @Suppress("UNUSED_PARAMETER")
     @JvmStatic
     fun createUI(c: JComponent): DarculaDisclosureButtonUI = DarculaDisclosureButtonUI()
 
-    internal val ARC = JBValue.UIInteger("DisclosureButton.arc", 16)
-    internal val TEXT_RIGHT_ICON_GAP = JBValue.UIInteger("DisclosureButton.textRightIconGap", 8)
-    internal val DEFAULT_BACKGROUND = JBColor.namedColor("DisclosureButton.defaultBackground")
-    internal val HOVER_BACKGROUND = JBColor.namedColor("DisclosureButton.hoverOverlay")
-    internal val PRESSED_BACKGROUND = JBColor.namedColor("DisclosureButton.pressedOverlay")
-
-    private var LEFT_MARGIN = 14
-    private var RIGHT_MARGIN = 12
+    fun getConfig(c: JComponent?): DarculaDisclosureButtonUIConfig = if ((c as? DisclosureButton)?.kind == DisclosureButtonKind.Klein) DarculaDisclosureButtonUISimpleConfig else DarculaDisclosureButtonUIDefaultConfig
   }
 
-  override fun installDefaults(b: AbstractButton?) {
+  override fun installDefaults(b: AbstractButton) {
     super.installDefaults(b)
 
-    b?.border = DarculaDisclosureButtonBorder()
+    b.border = DarculaDisclosureButtonBorder()
   }
 
   override fun paint(g: Graphics?, c: JComponent?) {
@@ -56,43 +86,64 @@ class DarculaDisclosureButtonUI : BasicButtonUI() {
 
     super.paint(g, c)
 
+    val arrowIconRect = Rectangle()
     c.arrowIcon?.let {
       val insets = c.getInsets()
-      it.paintIcon(c, g, c.width - insets.right - JBUIScale.scale(RIGHT_MARGIN) - it.iconWidth,
-                   insets.top + (c.height - insets.height - it.iconHeight) / 2)
+      arrowIconRect.x = c.width - insets.right - JBUIScale.scale(getConfig(c).RIGHT_MARGIN) - it.iconWidth
+      arrowIconRect.y = (c.height - it.iconHeight) / 2
+      arrowIconRect.width = it.iconWidth
+      arrowIconRect.height = it.iconHeight
+      it.paintIcon(c, g, arrowIconRect.x, arrowIconRect.y)
+    }
+
+    if (DRAW_DEBUG_LINES) {
+      g.color = Color.RED
+      g.drawRect(arrowIconRect.x, arrowIconRect.y, arrowIconRect.width, arrowIconRect.height)
     }
   }
 
-  override fun paintIcon(g: Graphics?, c: JComponent?, iconRect: Rectangle?) {
-    iconRect?.let {
-      it.x += JBUIScale.scale(LEFT_MARGIN)
+  override fun paintIcon(g: Graphics, c: JComponent, iconRect: Rectangle) {
+    iconRect.x += JBUIScale.scale(getConfig(c).LEFT_MARGIN)
+
+    if (DRAW_DEBUG_LINES) {
+      g.color = Color.RED
+      g.drawRect(iconRect.x, iconRect.y, iconRect.width, iconRect.height)
     }
 
     super.paintIcon(g, c, iconRect)
   }
 
-  override fun paintText(g: Graphics?, c: JComponent?, textRect: Rectangle?, text: String?) {
-    if (g == null || textRect == null) {
-      return
-    }
-
+  override fun paintText(g: Graphics, c: JComponent?, textRect: Rectangle, text: String?) {
     if (c !is DisclosureButton) {
       super.paintText(g, c, textRect, text)
       return
     }
 
-    textRect.x += JBUIScale.scale(LEFT_MARGIN)
-    textRect.width = c.width - textRect.x - getExtraIconsSize(c).width - JBUIScale.scale(RIGHT_MARGIN)
+    textRect.x += JBUIScale.scale(getConfig(c).LEFT_MARGIN)
+    textRect.width = c.width - textRect.x - getExtraIconsSize(c).width - JBUIScale.scale(getConfig(c).RIGHT_MARGIN)
 
     val fm = c.getFontMetrics(c.font)
     val clippedText = UIUtilities.clipStringIfNecessary(c as JComponent, fm, text, textRect.width)
     super.paintText(g, c as JComponent, textRect, clippedText)
 
+    if (DRAW_DEBUG_LINES) {
+      g.color = Color.RED
+      g.drawRect(textRect.x, textRect.y, textRect.width, textRect.height)
+    }
+
+    val rightIconRect = Rectangle()
     c.rightIcon?.let {
       val textWidth = fm.stringWidth(clippedText)
-      val x = textRect.x + min(textRect.width, textWidth) + TEXT_RIGHT_ICON_GAP.get()
       val insets = c.getInsets()
-      it.paintIcon(c, g, x, insets.top + (c.height - insets.height - it.iconHeight) / 2)
+      rightIconRect.x = textRect.x + min(textRect.width, textWidth) + getConfig(c).TEXT_RIGHT_ICON_GAP.get()
+      rightIconRect.y = insets.top + (c.height - insets.height - it.iconHeight) / 2
+      rightIconRect.width = it.iconWidth
+      rightIconRect.height = it.iconHeight
+      it.paintIcon(c, g, rightIconRect.x, rightIconRect.y)
+    }
+    if (DRAW_DEBUG_LINES) {
+      g.color = Color.RED
+      g.drawRect(rightIconRect.x, rightIconRect.y, rightIconRect.width, rightIconRect.height)
     }
   }
 
@@ -105,7 +156,7 @@ class DarculaDisclosureButtonUI : BasicButtonUI() {
       val extraSize = getExtraIconsSize(c)
       result.width += extraSize.width
       result.height = max(result.height, extraSize.height)
-      result.width = max(result.width + JBUIScale.scale(LEFT_MARGIN) + JBUIScale.scale(RIGHT_MARGIN), minimumSize.width) + insets.width
+      result.width = max(result.width + JBUIScale.scale(getConfig(c).LEFT_MARGIN) + JBUIScale.scale(getConfig(c).RIGHT_MARGIN), minimumSize.width) + insets.width
       result.height = max(result.height, minimumSize.height) + insets.height
     }
 
@@ -113,32 +164,39 @@ class DarculaDisclosureButtonUI : BasicButtonUI() {
   }
 
   override fun getMinimumSize(c: JComponent?): Dimension {
-    return JBDimension(72, 34)
+    return JBDimension(72, getConfig(c).HEIGHT)
   }
 
   private fun paintBackground(g: Graphics, c: DisclosureButton) {
     val r = Rectangle(0, 0, c.width, c.height)
     JBInsets.removeFrom(r, c.insets)
-    DarculaNewUIUtil.fillRoundedRectangle(g, r, c.buttonBackground ?: DEFAULT_BACKGROUND, arc = ARC.float)
+
+    val defaultBg = c.buttonBackground ?: getConfig(c).DEFAULT_BACKGROUND
+    if (defaultBg != null) {
+      DarculaNewUIUtil.fillRoundedRectangle(g, r, defaultBg, arc = getConfig(c).ARC.float)
+    }
 
     val model = c.model
-    val overlay =
-      when {
-        model.isArmed && model.isPressed -> PRESSED_BACKGROUND
-        model.isRollover -> HOVER_BACKGROUND
-        else -> return
-      }
-    DarculaNewUIUtil.fillRoundedRectangle(g, r, overlay, ARC.float)
+    val overlay = when {
+      model.isArmed && model.isPressed -> getConfig(c).PRESSED_BACKGROUND
+      model.isRollover -> getConfig(c).HOVER_BACKGROUND
+      else -> return
+    }
+    DarculaNewUIUtil.fillRoundedRectangle(g, r, overlay, getConfig(c).ARC.float)
   }
 
   private fun getExtraIconsSize(b: DisclosureButton): Dimension {
     val result = Dimension()
     b.rightIcon?.let {
-      result.width += TEXT_RIGHT_ICON_GAP.get() + it.iconWidth
+      result.width += getConfig(b).TEXT_RIGHT_ICON_GAP.get() + it.iconWidth
       result.height = max(result.height, it.iconHeight)
     }
     b.arrowIcon?.let {
       result.width += b.iconTextGap + it.iconWidth
+      // Chevron is in square even though it is rectangular. Subtract some width to compensate for that
+      if (it === AllIcons.General.ChevronRight) {
+        result.width -= 8
+      }
       result.height = max(result.height, it.iconHeight)
     }
     return result
