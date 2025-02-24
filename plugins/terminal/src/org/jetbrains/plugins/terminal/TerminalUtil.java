5e7216e44934cbb2512bdeceafdbb63b19a49e54
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-02-24 13:16:04 +0200

    [terminal] IJPL-177869 Hide Gen1 terminal setting for users who had it disabled on the first project opening in 251/252
    If New Terminal (Gen1) was enabled at the moment of the first startup, leave this setting visible.
    
    
    (cherry picked from commit a02d1f204e78c01abdc395a32738502654ae5963)
    
    IJ-CR-155952
    
    GitOrigin-RevId: c599189d30053234348742bfcd4d34f2f9546ffb

--- plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalUtil.java
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalUtil.java
@@ -3,6 +3,7 @@ package org.jetbrains.plugins.terminal;
 
 import com.intellij.execution.configurations.PathEnvironmentVariableUtil;
 import com.intellij.execution.process.UnixProcessManager;
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
@@ -236,6 +237,25 @@ public final class TerminalUtil {
    */
   @ApiStatus.Internal
   public static boolean isGenOneTerminalOptionVisible() {
-    return ExperimentalUI.isNewUI();
+    return ExperimentalUI.isNewUI() && getGenOneTerminalVisibilityValue() == Boolean.TRUE;
+  }
+
+  /**
+   * Internal helper setting to control whether Gen1 terminal options should be visible or not.
+   */
+  private static final String GEN_ONE_OPTION_VISIBLE_PROPERTY = "terminal.gen.one.option.visible";
+
+  @ApiStatus.Internal
+  public static @Nullable Boolean getGenOneTerminalVisibilityValue() {
+    String value = PropertiesComponent.getInstance().getValue(GEN_ONE_OPTION_VISIBLE_PROPERTY);
+    if (value != null) {
+      return Boolean.parseBoolean(value);
+    }
+    return null;
+  }
+
+  @ApiStatus.Internal
+  public static void setGenOneTerminalVisibilityValue(boolean isVisible) {
+    PropertiesComponent.getInstance().setValue(GEN_ONE_OPTION_VISIBLE_PROPERTY, Boolean.toString(isVisible));
   }
 }
