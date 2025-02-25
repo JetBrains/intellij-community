27176d09b123e2ab54a6e1d4a6bbc43fe670f17e
Anna Kozlova <anna.kozlova@jetbrains.com>
2025-02-25 15:12:29 +0100

    [kotlin] k2: disable change parameter type of functions from libraries
    - fix false fix name
    
    KTIJ-33219
    
    GitOrigin-RevId: 14450e6b445cd6efef9b66654ae45e8ee87b6d79

--- plugins/kotlin/idea/tests/testData/quickfix/typeMismatch/disabledOnLibraryCall.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/typeMismatch/disabledOnLibraryCall.kt
@@ -1,11 +1,11 @@
-// "Change parameter 'transformer' of function 'map' to 'String'" "false"
+// "Change parameter 'transform' type of function 'map' to 'String'" "false"
 // K2_AFTER_ERROR: Argument type mismatch: actual type is 'String', but 'Function1<Int, R (of fun <T, R> Iterable<T>.map)>' was expected.
 // K2_AFTER_ERROR: Cannot infer type for this parameter. Specify it explicitly.
 // ERROR: Type mismatch: inferred type is String but (TypeVariable(T)) -> TypeVariable(R) was expected
 // WITH_STDLIB
 
-fun foo(transformer: String) {
-    listOf(1, 2, 3).map(tra<caret>nsformer)
+fun foo(param: String) {
+    listOf(1, 2, 3).map(para<caret>m)
 }
 
 // FUS_QUICKFIX_NAME: org.jetbrains.kotlin.idea.quickfix.ChangeParameterTypeFix
