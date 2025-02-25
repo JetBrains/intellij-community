763726020c5c4872aa472da5b68aad378236f33a
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2025-02-25 19:20:53 +0200

    [eel] hotfix for IJPL-181399 by cherry-picking local implementation of IJPL-163265 (instead of calling where.exe)
    GitOrigin-RevId: 1a7a1faa6f81033f6767212ede117e7ca00f7451

--- platform/eel-provider/src/com/intellij/platform/eel/provider/utils/eelProcessUtils.kt
+++ platform/eel-provider/src/com/intellij/platform/eel/provider/utils/eelProcessUtils.kt
@@ -60,46 +60,8 @@ suspend fun EelProcess.awaitProcessResult(): EelProcessExecutionResult {
   }
 }
 
-fun EelApi.whereBlocking(exe: String): EelPath? {
-  return runBlockingMaybeCancellable { exec.where(exe) }
-}
-
-/**
- * Function that attempts to locate the full path of the specified executable in the system's PATH.
- *
- * This function uses platform-specific commands to search for the executable:
- * - On POSIX systems, it uses the `which` command.
- * - On Windows systems, it uses `where.exe`.
- *
- * @param exe The name of the executable to search for.
- * @return The full path to the executable if found; `null` if the executable is not found or an error occurs.
- * @throws IllegalArgumentException If the operating system is unsupported.
- *
- * @example
- * ```kotlin
- * val path = eelApi.where("git")
- * if (path != null) {
- *     println("Git is located at: $path")
- * } else {
- *     println("Git executable not found.")
- * }
- * ```
- */
 suspend fun EelExecApi.where(exe: String): EelPath? {
-  val tool = when (descriptor.operatingSystem) {
-    EelPath.OS.UNIX -> "which"
-    EelPath.OS.WINDOWS -> "where.exe"
-  }
-
-  val result = execute(tool) { env(fetchLoginShellEnvVariables()).args(exe) }.getOrThrow().awaitProcessResult()
-
-  if (result.exitCode != 0) {
-    // TODO: log error?/throw Exception?
-    return null
-  }
-  else {
-    return EelPath.parse(result.stdoutString.trim(), descriptor)
-  }
+  return this.findExeFilesInPath(exe).firstOrNull()
 }
 
 /**
