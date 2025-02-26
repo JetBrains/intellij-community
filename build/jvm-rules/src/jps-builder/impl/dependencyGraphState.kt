92c80df0f4ca645d234c92a96e0d8bb1b4bb6fd7
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-02-26 09:24:44 +0100

    bazel lib tracking (part 7)
    GitOrigin-RevId: 4f8a2cb2629855927381b2bbfdc44c7f09563d9a

--- build/jvm-rules/src/jps-builder/impl/dependencyGraphState.kt
+++ build/jvm-rules/src/jps-builder/impl/dependencyGraphState.kt
@@ -3,7 +3,6 @@
 
 package org.jetbrains.bazel.jvm.jps.impl
 
-import com.intellij.openapi.util.Key
 import io.opentelemetry.api.common.AttributeKey
 import io.opentelemetry.api.common.Attributes
 import io.opentelemetry.api.trace.Span
@@ -17,20 +16,15 @@ import org.jetbrains.jps.dependency.NodeSource
 import org.jetbrains.jps.dependency.impl.DifferentiateParametersBuilder
 import org.jetbrains.jps.dependency.impl.PathSource
 import org.jetbrains.jps.incremental.CompileContext
-import org.jetbrains.jps.incremental.FSOperations
 import org.jetbrains.jps.incremental.RebuildRequestedException
 import org.jetbrains.jps.incremental.Utils
 import org.jetbrains.jps.incremental.dependencies.LibraryDef
 import org.jetbrains.jps.incremental.dependencies.checkDependencies
 import org.jetbrains.jps.incremental.fs.BuildFSState.CURRENT_ROUND_DELTA_KEY
-import org.jetbrains.jps.incremental.fs.CompilationRound
 import org.jetbrains.jps.incremental.storage.PathTypeAwareRelativizer
 import org.jetbrains.jps.incremental.storage.RelativePathType
 import org.jetbrains.jps.model.java.JpsJavaExtensionService
-import java.io.FileFilter
 import java.nio.file.Path
-
-private val ALL_AFFECTED_NODE_SOURCES_KEY = Key.create<MutableSet<NodeSource>>("_all_compiled_node_sources_")
 
 internal fun fileToNodeSource(file: Path, relativizer: PathTypeAwareRelativizer): NodeSource {
   return PathSource(relativizer.toRelative(file, RelativePathType.SOURCE))
@@ -67,7 +61,7 @@ internal suspend fun markDirtyDependenciesForInitialRound(
     true
   }
 
-  val deletedSources = getRemovedPaths(chunk, dirtyFilesHolder, relativizer)
+  val deletedSources = getRemovedPaths(target, dirtyFilesHolder, relativizer)
   if (toCompile.isEmpty() && deletedSources.isEmpty()) {
     return
   }
@@ -82,8 +76,6 @@ internal suspend fun markDirtyDependenciesForInitialRound(
       context = context,
       delta = delta,
       chunk = chunk,
-      markDirtyRound = CompilationRound.CURRENT,
-      skipMarkDirtyFilter = null,
       dataProvider = dataProvider,
       target = target,
       span = span,
@@ -92,37 +84,26 @@ internal suspend fun markDirtyDependenciesForInitialRound(
 }
 
 private fun getRemovedPaths(
-  chunk: ModuleChunk,
+  target: BazelModuleBuildTarget,
   dirtyFilesHolder: BazelDirtyFileHolder,
   relativizer: PathTypeAwareRelativizer,
 ): List<NodeSource> {
   if (!dirtyFilesHolder.hasRemovedFiles()) {
     return emptyList()
   }
-
-  val removed = mutableListOf<NodeSource>()
-  for (target in chunk.targets) {
-    for (file in dirtyFilesHolder.getRemoved(target)) {
-      removed.add(fileToNodeSource(file, relativizer))
-    }
-  }
-  return removed
+  return dirtyFilesHolder.getRemoved(target).map { fileToNodeSource(it, relativizer) }
 }
 
 /**
  * @param context        compilation context
  * @param delta          registered delta files in this round
- * @param markDirtyRound compilation round at which dirty files should be visible to builders
  * @return true if additional compilation pass is required, false otherwise
  */
-@Suppress("SameParameterValue")
 private fun updateDependencyGraph(
   context: BazelCompileContext,
   delta: Delta?,
   chunk: ModuleChunk,
   target: BazelModuleBuildTarget,
-  markDirtyRound: CompilationRound,
-  skipMarkDirtyFilter: FileFilter?,
   span: Span,
   dataProvider: BazelBuildDataProvider,
 ): Boolean {
@@ -140,36 +121,12 @@ private fun updateDependencyGraph(
   val differentiateParams = params.get()
   val diffResult = dependencyGraph.differentiate(delta, differentiateParams)
 
-  val compilingIncrementally = context.scope.isIncrementalCompilation
-
-  if (compilingIncrementally && !errorsDetected && differentiateParams.isCalculateAffected && diffResult.isIncremental) {
-    // Some compilers (and compiler plugins) may produce different outputs for the same set of inputs.
-    // This might cause corresponding graph Nodes to be considered as always 'changed'.
-    // In some scenarios, this may lead to endless build loops.
-    // This fallback logic detects such loops and recompiles the whole module chunk instead.
-    val affectedForChunk = diffResult.affectedSources.filterTo(hashSet()) { differentiateParams.belongsToCurrentCompilationChunk().test(it) }
-    if (!affectedForChunk.isEmpty() && !getOrCreate(context, ALL_AFFECTED_NODE_SOURCES_KEY) { hashSet() }.addAll(affectedForChunk)) {
-      // All affected files in this round have already been affected in previous rounds.
-      // This might indicate a build cycle => recompiling the whole chunk.
-      span.addEvent("build cycle detected for ${chunk.name}; recompiling whole module chunk")
-      // turn on non-incremental mode for all targets from the current chunk =>
-      // next time the whole chunk is recompiled and affected files won't be calculated anymore
-      for (target in chunk.targets) {
-        context.markNonIncremental(target)
-      }
-      FSOperations.markDirty(context, markDirtyRound, chunk, null)
-      return true
-    }
-  }
-
   if (!diffResult.isIncremental) {
     // non-incremental mode
     throw RebuildRequestedException(RuntimeException("diffResult is non incremental: $diffResult"))
   }
 
   val relativizer = dataProvider.relativizer
-
-  assert(skipMarkDirtyFilter == null)
   val affectedFiles = diffResult.affectedSources.mapTo(hashSet()) { relativizer.toAbsoluteFile(it.toString(), RelativePathType.SOURCE) }
 
   if (differentiateParams.isCalculateAffected) {
@@ -189,22 +146,11 @@ private fun updateDependencyGraph(
     )
   }
 
-  ALL_AFFECTED_NODE_SOURCES_KEY.set(context, null)
-
   if (!errorsDetected) {
     dependencyGraph.integrate(diffResult)
   }
 
   return false
-}
-
-private inline fun <T : Any> getOrCreate(context: CompileContext, @Suppress("SameParameterValue") dataKey: Key<T>, factory: () -> T): T {
-  var result = context.getUserData(dataKey)
-  if (result == null) {
-    result = factory()
-    context.putUserData(dataKey, result)
-  }
-  return result
 }
 
 internal fun markAffectedFilesDirty(
