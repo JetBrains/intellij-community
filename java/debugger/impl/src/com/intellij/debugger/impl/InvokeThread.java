ba8c068112b5f26606f0826a489e6407e66c444d
Egor Ushakov <egor.ushakov@jetbrains.com>
2025-02-26 18:01:03 +0100

    [debugger] fixed endless exceptions causing many failed tests at once
    GitOrigin-RevId: 227bc8407175ef60728530bccabe71bc5e4775f7

--- java/debugger/impl/src/com/intellij/debugger/impl/InvokeThread.java
+++ java/debugger/impl/src/com/intellij/debugger/impl/InvokeThread.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.debugger.impl;
 
 import com.intellij.debugger.engine.DebuggerManagerThreadImpl;
@@ -149,7 +149,7 @@ public abstract class InvokeThread<E extends PrioritizedTask> {
     if (currentRequest != threadRequest) {
       String message =
         "Expected worker request " + threadRequest + " instead of " + currentRequest + " closed=" + thread.myEvents.isClosed();
-      LOG.error(message, new IllegalStateException(message));
+      reportCommandError(new IllegalStateException(message)); // do not throw AssertionError in tests, we need to return false here
       return false;
     }
     return true;
