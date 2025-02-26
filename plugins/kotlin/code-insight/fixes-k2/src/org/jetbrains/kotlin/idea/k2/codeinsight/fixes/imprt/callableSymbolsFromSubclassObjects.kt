3538c8738550efc4c9721a30439274503f913968
Roman Golyshev <roman.golyshev@jetbrains.com>
2025-02-26 12:57:13 +0100

    KTIJ-33249 [kotlin] Introduce a cache for `getKotlinSubclassObjectsByNameFilter` call
    This call is not cheap, since it has to pull all subclassed objects from the indices.
    If there are a lot of unresolved references in the file, this can be quite expensive.
    
    See the KDoc for `SUBCLASS_OBJECTS_SYMBOLS_CACHE` about why this cache is safe
    regarding `KaSession`'s validity.
    
    GitOrigin-RevId: 156ce24d413a3739876055d6d509db64e29f18bd

--- plugins/kotlin/code-insight/fixes-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/fixes/imprt/callableSymbolsFromSubclassObjects.kt
+++ plugins/kotlin/code-insight/fixes-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/fixes/imprt/callableSymbolsFromSubclassObjects.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.k2.codeinsight.fixes.imprt
 
+import com.intellij.util.containers.CollectionFactory
 import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.analysis.api.symbols.KaCallableSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KaClassSymbol
@@ -10,15 +11,38 @@ import org.jetbrains.kotlin.analysis.api.types.KaTypeNullability
 import org.jetbrains.kotlin.idea.base.analysis.api.utils.KtSymbolFromIndexProvider
 import org.jetbrains.kotlin.idea.base.analysis.api.utils.isPossiblySubTypeOf
 import org.jetbrains.kotlin.name.Name
+import java.util.concurrent.ConcurrentMap
+
+/**
+ * Ad-hoc cache to avoid multiple repeated calls to [KtSymbolFromIndexProvider.getKotlinSubclassObjectsByNameFilter] during
+ * unresolved references analysis in the auto-import.
+ * 
+ * This cache relies on the following conditions: 
+ * - [KaSession.analysisScope] is stable during [KaSession]'s lifetime, 
+ * so the results of [KtSymbolFromIndexProvider.getKotlinSubclassObjectsByNameFilter] should also be stable.
+ * - It's safe to store references to [KaClassSymbol] in a cache as long as they are re-used in the same exact [KaSession].
+ * This is achieved by using weak identity keys to store [KaSession].
+ */
+private val SUBCLASS_OBJECTS_SYMBOLS_CACHE: ConcurrentMap<KaSession, List<KaClassSymbol>> =
+    CollectionFactory.createConcurrentWeakIdentityMap()
+
+context(KaSession)
+private fun KtSymbolFromIndexProvider.getKotlinSubclassObjectsSymbolsCached(): List<KaClassSymbol> =
+    SUBCLASS_OBJECTS_SYMBOLS_CACHE.getOrPut(this@KaSession) {
+        getKotlinSubclassObjectsByNameFilter(
+            scope = analysisScope,
+            nameFilter = { true },
+        ).toList()
+    }
 
 /**
  * Consider moving this to [KtSymbolFromIndexProvider] when there is a good API to represent inherited callables.
  */
 context(KaSession)
 internal fun KtSymbolFromIndexProvider.getCallableSymbolsFromSubclassObjects(name: Name): Sequence<Pair<KaClassSymbol, KaCallableSymbol>> {
-    val allObjects = getKotlinSubclassObjectsByNameFilter(nameFilter = { true })
+    val allObjects = getKotlinSubclassObjectsSymbolsCached()
 
-    return allObjects.flatMap { objectSymbol ->
+    return allObjects.asSequence().flatMap { objectSymbol ->
         val callablesByName = objectSymbol.memberScope.callables(name)
         callablesByName.map { objectSymbol to it }
     }
