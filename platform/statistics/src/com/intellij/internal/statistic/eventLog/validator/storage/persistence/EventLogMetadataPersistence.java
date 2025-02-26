0f5a27f1468fd0cd1db0dc88958a184c48c2ab05
Liubov Afanaseva <Liubov.Afanaseva@jetbrains.com>
2025-02-26 07:15:54 +0100

    Revert "IJPL-39448: use bundled metadata instead of the local one if its version is newer"
    This reverts commit 9041f177e5db7955f4b039ad573cdeeecc7f9910.
    
    GitOrigin-RevId: 36650b75362b866a50a716e1895a9131a5b3a38f

--- platform/statistics/src/com/intellij/internal/statistic/eventLog/validator/storage/persistence/EventLogMetadataPersistence.java
+++ platform/statistics/src/com/intellij/internal/statistic/eventLog/validator/storage/persistence/EventLogMetadataPersistence.java
@@ -1,18 +1,12 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.internal.statistic.eventLog.validator.storage.persistence;
 
-import com.intellij.internal.statistic.eventLog.connection.metadata.EventLogMetadataParseException;
-import com.intellij.internal.statistic.eventLog.connection.metadata.EventLogMetadataUtils;
-import com.intellij.openapi.application.ApplicationInfo;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.util.BuildNumber;
-import com.jetbrains.fus.reporting.model.metadata.EventGroupRemoteDescriptors;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -49,78 +43,11 @@ public class EventLogMetadataPersistence extends BaseEventLogMetadataPersistence
     }
     else {
       Path file = getDefaultFile();
-      if (shouldBeRewrittenByBuiltinGroups()) {
+      if (!Files.exists(file)) {
         initBuiltinMetadata(file);
       }
       return file;
     }
-  }
-
-  /**
-   *  Use bundled metadata instead of the default local one if its version is newer,
-   *  the persistence build number is null or less than the current build number.
-   */
-  private boolean shouldBeRewrittenByBuiltinGroups() {
-    try {
-      Path defaultFile = getDefaultFile();
-      if (!Files.exists(defaultFile)) {
-        return true;
-      }
-
-      BuildNumber currentBuild = ApplicationInfo.getInstance().getBuild();
-      String previousBuildNumberStr = EventLogMetadataSettingsPersistence.getInstance().getBuildNumber(myRecorderId);
-      BuildNumber previousBuildNumber = BuildNumber.fromString(previousBuildNumberStr);
-
-      if (previousBuildNumber != null && previousBuildNumber.compareTo(currentBuild) >= 0) {
-        return false;
-      }
-
-      EventLogMetadataSettingsPersistence.getInstance().setBuildNumber(myRecorderId, currentBuild.asString());
-      Path builtinFile = getBuiltinFile();
-      if (builtinFile == null) {
-        return false;
-      }
-
-      String builtinEventsScheme = readEventScheme(builtinFile);
-      EventGroupRemoteDescriptors builtinEventGroupRemoteDescriptors = getEventGroupRemoteDescriptors(builtinEventsScheme);
-      if (builtinEventGroupRemoteDescriptors == null || builtinEventGroupRemoteDescriptors.version == null) {
-        return false;
-      }
-
-      String defaultEventScheme = readEventScheme(defaultFile);
-      EventGroupRemoteDescriptors defaultEventGroupRemoteDescriptors = getEventGroupRemoteDescriptors(defaultEventScheme);
-      if (defaultEventGroupRemoteDescriptors == null || defaultEventGroupRemoteDescriptors.version == null) {
-        return true;
-      }
-
-      return builtinEventGroupRemoteDescriptors.version.compareTo(defaultEventGroupRemoteDescriptors.version) > 0;
-    }
-    catch (IOException e) {
-      LOG.error(e);
-    }
-    return false;
-  }
-
-  private static @Nullable String readEventScheme(@NotNull Path file) {
-    try {
-      if (Files.exists(file) && Files.isRegularFile(file)) {
-        return Files.readString(file);
-      }
-    }
-    catch (IOException e) {
-      LOG.error(e);
-    }
-    return null;
-  }
-
-  private static @Nullable EventGroupRemoteDescriptors getEventGroupRemoteDescriptors(@Nullable String data) {
-    try {
-      return EventLogMetadataUtils.parseGroupRemoteDescriptors(data);
-    }
-    catch (EventLogMetadataParseException e) {
-      LOG.error(e);
-    }
-    return null;
   }
 
   public void cacheEventsScheme(@NotNull String eventsSchemeJson, long lastModified) {
@@ -133,16 +60,6 @@ public class EventLogMetadataPersistence extends BaseEventLogMetadataPersistence
     catch (IOException e) {
       LOG.error(e);
     }
-  }
-
-  private @Nullable Path getBuiltinFile() throws IOException {
-    try (InputStream stream = getClass().getClassLoader().getResourceAsStream(builtinEventSchemePath())) {
-      if (stream == null) {
-        return null;
-      }
-    }
-    URL buildinUrl = getClass().getClassLoader().getResource(builtinEventSchemePath());
-    return buildinUrl == null ? null : Path.of(buildinUrl.getPath());
   }
 
   private void initBuiltinMetadata(@NotNull Path file) throws IOException {
