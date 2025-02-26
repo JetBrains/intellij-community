1a74166ce2ccd8f92bec95ae0e435e816e0a21f1
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2025-02-26 16:48:10 +0100

    [groovy] Fix resolve on ambiguous reference
    Also removes some object allocation from hot path (resolve). #IDEA-368272 Fixed
    
    GitOrigin-RevId: 1cc7fb72887254e86f2151a92f23d551ce522456

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/javaView/GroovyClassFinder.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/javaView/GroovyClassFinder.java
@@ -1,21 +1,17 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.plugins.groovy.lang.psi.impl.javaView;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElementFinder;
-import com.intellij.psi.PsiFile;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiClassUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.plugins.groovy.lang.psi.GroovyFileBase;
 import org.jetbrains.plugins.groovy.lang.stubs.GroovyShortNamesCache;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class GroovyClassFinder extends PsiElementFinder {
@@ -30,13 +26,13 @@ public class GroovyClassFinder extends PsiElementFinder {
     final List<PsiClass> classes = myCache.getClassesByFQName(qualifiedName, scope, true);
     if (classes.isEmpty()) return null;
     if (classes.size() == 1) return classes.get(0);
-    return Collections.min(classes, PsiClassUtil
-      .createScopeComparator(scope)
-      .thenComparing(c -> c.getQualifiedName(), Comparator.nullsLast(Comparator.naturalOrder()))
-      .thenComparing(c -> {
-        PsiFile file = c.getContainingFile();
-        return file instanceof GroovyFileBase ? ((GroovyFileBase)file).getPackageName() : "";
-      }));
+    PsiClass bestClass = null;
+    for (PsiClass candidateClass : classes) {
+      if (bestClass == null || PsiClassUtil.createScopeComparator(scope).compare(candidateClass, bestClass) < 0) {
+        bestClass = candidateClass;
+      }
+    }
+    return bestClass;
   }
 
   @Override
