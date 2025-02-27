4c68fc1e8c54231cfb6a1003ef9e7ea8cef084ff
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2025-02-27 08:24:03 +0100

    [maven] IDEA-366760 convert MavenEmbeddersManager to kotlin
    GitOrigin-RevId: e7775b3604d949c50aa795289fb7a8b49c91a535

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/project/MavenEmbeddersManager.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/project/MavenEmbeddersManager.kt
@@ -1,138 +1,141 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.idea.maven.project;
+package org.jetbrains.idea.maven.project
 
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Key;
-import com.intellij.openapi.util.Pair;
-import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.TestOnly;
-import org.jetbrains.idea.maven.server.MavenEmbedderWrapper;
-import org.jetbrains.idea.maven.server.MavenServerManager;
-import org.jetbrains.idea.maven.utils.MavenLog;
-import org.jetbrains.idea.maven.utils.MavenUtil;
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.Pair
+import com.intellij.util.containers.ContainerUtil
+import org.jetbrains.annotations.TestOnly
+import org.jetbrains.idea.maven.server.MavenEmbedderWrapper
+import org.jetbrains.idea.maven.server.MavenServerManager.Companion.getInstance
+import org.jetbrains.idea.maven.utils.MavenLog
+import org.jetbrains.idea.maven.utils.MavenUtil.getBaseDir
+import java.nio.file.Files
+import java.nio.file.Path
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+class MavenEmbeddersManager(private val myProject: Project) {
+  private val myPool: MutableMap<Pair<Key<*>, String>, MavenEmbedderWrapper> = ContainerUtil.createSoftValueMap<Pair<Key<*>, String>, MavenEmbedderWrapper>()
+  private val myEmbeddersInUse: MutableSet<MavenEmbedderWrapper?> = HashSet<MavenEmbedderWrapper?>()
 
-public class MavenEmbeddersManager {
-  public static final Key FOR_DEPENDENCIES_RESOLVE = Key.create(MavenEmbeddersManager.class + ".FOR_DEPENDENCIES_RESOLVE");
-  public static final Key FOR_PLUGINS_RESOLVE = Key.create(MavenEmbeddersManager.class + ".FOR_PLUGINS_RESOLVE");
-  public static final Key FOR_FOLDERS_RESOLVE = Key.create(MavenEmbeddersManager.class + ".FOR_FOLDERS_RESOLVE");
-  public static final Key FOR_POST_PROCESSING = Key.create(MavenEmbeddersManager.class + ".FOR_POST_PROCESSING");
-  public static final Key FOR_MODEL_READ = Key.create(MavenEmbeddersManager.class + ".FOR_MODEL_READ");
-
-  // will always regardless to 'work offline' setting
-  public static final Key FOR_DOWNLOAD = Key.create(MavenEmbeddersManager.class + ".FOR_DOWNLOAD");
-
-  private final Project myProject;
-
-  private final Map<Pair<Key, String>, MavenEmbedderWrapper> myPool = ContainerUtil.createSoftValueMap();
-  private final Set<MavenEmbedderWrapper> myEmbeddersInUse = new HashSet<>();
-
-  public MavenEmbeddersManager(Project project) {
-    myProject = project;
-  }
-
-  public synchronized void reset() {
-    releasePooledEmbedders(false);
+  @Synchronized
+  fun reset() {
+    releasePooledEmbedders(false)
   }
 
   // used in third-party plugins
-  public synchronized @NotNull MavenEmbedderWrapper getEmbedder(@NotNull MavenProject mavenProject, Key kind) {
-    String baseDir = MavenUtil.getBaseDir(mavenProject.getDirectoryFile()).toString();
-    return getEmbedder(kind, baseDir);
+  @Synchronized
+  fun getEmbedder(mavenProject: MavenProject, kind: Key<*>?): MavenEmbedderWrapper {
+    val baseDir = getBaseDir(mavenProject.directoryFile).toString()
+    return getEmbedder(kind, baseDir)
   }
 
-  public synchronized @NotNull MavenEmbedderWrapper getEmbedder(Key kind, @NotNull String multiModuleProjectDirectory) {
-    String embedderDir = guessExistingEmbedderDir(multiModuleProjectDirectory);
+  @Synchronized
+  fun getEmbedder(kind: Key<*>?, multiModuleProjectDirectory: String): MavenEmbedderWrapper {
+    val embedderDir = guessExistingEmbedderDir(multiModuleProjectDirectory)
 
-    Pair<Key, String> key = Pair.create(kind, embedderDir);
-    MavenEmbedderWrapper result = myPool.get(key);
-    boolean alwaysOnline = kind == FOR_DOWNLOAD;
+    val key = Pair.create<Key<*>, String>(kind, embedderDir)
+    var result = myPool.get(key)
+    val alwaysOnline = kind === FOR_DOWNLOAD
 
     if (result == null) {
-      result = createEmbedder(embedderDir, alwaysOnline);
-      myPool.put(key, result);
+      result = createEmbedder(embedderDir, alwaysOnline)
+      myPool.put(key, result)
     }
 
     if (myEmbeddersInUse.contains(result)) {
-      MavenLog.LOG.warn("embedder " + key + " is already used");
-      return createEmbedder(embedderDir, alwaysOnline);
+      MavenLog.LOG.warn("embedder " + key + " is already used")
+      return createEmbedder(embedderDir, alwaysOnline)
     }
 
-    myEmbeddersInUse.add(result);
-    return result;
+    myEmbeddersInUse.add(result)
+    return result
   }
 
-  private String guessExistingEmbedderDir(@NotNull String multiModuleProjectDirectory) {
-    var dir = multiModuleProjectDirectory;
-    if (dir.isBlank()) {
-      MavenLog.LOG.warn("Maven project directory is blank. Using project base path");
-      dir = myProject.getBasePath();
+  private fun guessExistingEmbedderDir(multiModuleProjectDirectory: String): String {
+    var dir: String? = multiModuleProjectDirectory
+    if (dir!!.isBlank()) {
+      MavenLog.LOG.warn("Maven project directory is blank. Using project base path")
+      dir = myProject.getBasePath()
     }
     if (null == dir || dir.isBlank()) {
-      MavenLog.LOG.warn("Maven project directory is blank. Using tmp dir");
-      dir = System.getProperty("java.io.tmpdir");
+      MavenLog.LOG.warn("Maven project directory is blank. Using tmp dir")
+      dir = System.getProperty("java.io.tmpdir")
     }
-    Path originalPath = Path.of(dir).toAbsolutePath();
-    Path path = originalPath;
+    val originalPath = Path.of(dir).toAbsolutePath()
+    var path: Path? = originalPath
     while (null != path && !Files.exists(path)) {
-      MavenLog.LOG.warn(String.format("Maven project %s directory does not exist. Using parent", path));
-      path = path.getParent();
+      MavenLog.LOG.warn(String.format("Maven project %s directory does not exist. Using parent", path))
+      path = path.getParent()
     }
     if (null == path) {
-      MavenLog.LOG.warn("Could not determine maven project directory: " + multiModuleProjectDirectory);
-      return originalPath.toString();
+      MavenLog.LOG.warn("Could not determine maven project directory: " + multiModuleProjectDirectory)
+      return originalPath.toString()
     }
-    return path.toString();
+    return path.toString()
   }
 
-  private @NotNull MavenEmbedderWrapper createEmbedder(@NotNull String multiModuleProjectDirectory, boolean alwaysOnline) {
-    return MavenServerManager.getInstance().createEmbedder(myProject, alwaysOnline, multiModuleProjectDirectory);
+  private fun createEmbedder(multiModuleProjectDirectory: String, alwaysOnline: Boolean): MavenEmbedderWrapper {
+    return getInstance().createEmbedder(myProject, alwaysOnline, multiModuleProjectDirectory)
   }
 
-  /**
-   * @deprecated use {@link MavenEmbeddersManager#getEmbedder(Key, String)} instead
-   */
   // used in third-party plugins
-  @Deprecated(forRemoval = true)
-  public synchronized @NotNull MavenEmbedderWrapper getEmbedder(Key kind, String ignoredWorkingDirectory, @NotNull String multiModuleProjectDirectory) {
-    return getEmbedder(kind, multiModuleProjectDirectory);
+  @Deprecated("use {@link MavenEmbeddersManager#getEmbedder(Key, String)} instead")
+  @Synchronized
+  fun getEmbedder(kind: Key<*>, ignoredWorkingDirectory: String?, multiModuleProjectDirectory: String): MavenEmbedderWrapper {
+    return getEmbedder(kind, multiModuleProjectDirectory)
   }
 
-  public synchronized void release(@NotNull MavenEmbedderWrapper embedder) {
+  @Synchronized
+  fun release(embedder: MavenEmbedderWrapper) {
     if (!myEmbeddersInUse.contains(embedder)) {
-      embedder.release();
-      return;
+      embedder.release()
+      return
     }
 
-    myEmbeddersInUse.remove(embedder);
+    myEmbeddersInUse.remove(embedder)
   }
 
   @TestOnly
-  public synchronized void releaseInTests() {
+  @Synchronized
+  fun releaseInTests() {
     if (!myEmbeddersInUse.isEmpty()) {
-      MavenLog.LOG.warn("embedders should be release first");
+      MavenLog.LOG.warn("embedders should be release first")
     }
-    releasePooledEmbedders(false);
+    releasePooledEmbedders(false)
   }
 
-  public synchronized void releaseForcefullyInTests() {
-    releasePooledEmbedders(true);
+  @Synchronized
+  fun releaseForcefullyInTests() {
+    releasePooledEmbedders(true)
   }
 
-  private synchronized void releasePooledEmbedders(boolean includeInUse) {
-    for (var each : myPool.keySet()) {
-      MavenEmbedderWrapper embedder = myPool.get(each);
-      if (embedder == null) continue; // collected
-      if (!includeInUse && myEmbeddersInUse.contains(embedder)) continue;
-      embedder.release();
+  @Synchronized
+  private fun releasePooledEmbedders(includeInUse: Boolean) {
+    for (each in myPool.keys) {
+      val embedder = myPool.get(each)
+      if (embedder == null) continue  // collected
+
+      if (!includeInUse && myEmbeddersInUse.contains(embedder)) continue
+      embedder.release()
     }
-    myPool.clear();
-    myEmbeddersInUse.clear();
+    myPool.clear()
+    myEmbeddersInUse.clear()
+  }
+
+  companion object {
+    @JvmField
+    val FOR_DEPENDENCIES_RESOLVE: Key<*> = Key.create<Any?>(MavenEmbeddersManager::class.java.toString() + ".FOR_DEPENDENCIES_RESOLVE")
+    @JvmField
+    val FOR_PLUGINS_RESOLVE: Key<*> = Key.create<Any?>(MavenEmbeddersManager::class.java.toString() + ".FOR_PLUGINS_RESOLVE")
+    @JvmField
+    val FOR_FOLDERS_RESOLVE: Key<*> = Key.create<Any?>(MavenEmbeddersManager::class.java.toString() + ".FOR_FOLDERS_RESOLVE")
+    @JvmField
+    val FOR_POST_PROCESSING: Key<*> = Key.create<Any?>(MavenEmbeddersManager::class.java.toString() + ".FOR_POST_PROCESSING")
+    @JvmField
+    val FOR_MODEL_READ: Key<*> = Key.create<Any?>(MavenEmbeddersManager::class.java.toString() + ".FOR_MODEL_READ")
+
+    // will always regardless to 'work offline' setting
+    @JvmField
+    val FOR_DOWNLOAD: Key<*> = Key.create<Any?>(MavenEmbeddersManager::class.java.toString() + ".FOR_DOWNLOAD")
   }
 }
