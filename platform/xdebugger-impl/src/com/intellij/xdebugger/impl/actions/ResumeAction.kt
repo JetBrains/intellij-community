c34df1b8a6bcfce0c77f93af10a8e40ccf4b5c5d
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-02-27 11:12:45 +0100

    [debugger] Rewrite ResumeAction, so it doesn't use XDebuggerActionBase and DebuggerSupport
    GitOrigin-RevId: f98a3ce5cee6bd1140bac12d2f5729db86088868

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/ResumeAction.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/ResumeAction.kt
@@ -1,21 +1,30 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.xdebugger.impl.actions
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.project.DumbAware
-import com.intellij.xdebugger.XDebugSession
-import com.intellij.xdebugger.impl.DebuggerSupport
+import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.xdebugger.impl.XDebugSessionImpl
-import com.intellij.xdebugger.impl.actions.handlers.XDebuggerActionHandler
+import com.intellij.xdebugger.impl.XDebuggerUtilImpl.performDebuggerAction
 import com.intellij.xdebugger.impl.ui.DebuggerUIUtil
 import java.awt.event.KeyEvent
 
-open class ResumeAction : XDebuggerActionBase(), DumbAware {
-  override fun isEnabled(e: AnActionEvent): Boolean {
-    val project = e.project
-    if (project == null) return false
+open class ResumeAction : DumbAwareAction() {
+  override fun update(e: AnActionEvent) {
+    if (PauseAction.isPauseResumeMerged()) {
+      e.presentation.isEnabledAndVisible = isEnabled(e)
+      return
+    }
 
+    e.presentation.isVisible = true
+    e.presentation.isEnabled = isEnabled(e)
+  }
+
+  private fun isEnabled(e: AnActionEvent): Boolean {
+    val project = e.project
+    if (project == null) {
+      return false
+    }
     val session = DebuggerUIUtil.getSession(e) as XDebugSessionImpl?
     if (session != null && !session.isStopped) {
       return !session.isReadOnly && session.isPaused
@@ -24,22 +33,14 @@ open class ResumeAction : XDebuggerActionBase(), DumbAware {
     return e.inputEvent is KeyEvent
   }
 
-  override fun isHidden(event: AnActionEvent): Boolean {
-    if (!PauseAction.isPauseResumeMerged()) {
-      return super.isHidden(event)
-    }
-    return super.isHidden(event) || !isEnabled(event)
+  override fun getActionUpdateThread(): ActionUpdateThread {
+    return ActionUpdateThread.BGT
   }
 
-  override fun getHandler(debuggerSupport: DebuggerSupport): DebuggerActionHandler {
-    return object : XDebuggerActionHandler() {
-      override fun isEnabled(session: XDebugSession, dataContext: DataContext): Boolean {
-        return session.isPaused()
-      }
-
-      override fun perform(session: XDebugSession, dataContext: DataContext) {
-        session.resume()
-      }
+  override fun actionPerformed(e: AnActionEvent) {
+    val session = DebuggerUIUtil.getSession(e) ?: return
+    performDebuggerAction(e) {
+      session.resume()
     }
   }
 }
