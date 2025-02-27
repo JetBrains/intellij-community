9131e29041b04ee44d03d684b6a85bbd486ffcea
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2025-02-27 21:26:41 +0200

    PY-79488 Prototype multimodule Poetry project support
    Whenever a pyproject.toml file is found in a new IJ project root
    (without .idea) we register the corresponding root as managed by Poetry
    in `.idea/poetry.xml` (so called "linking") and then read all
    pyproject.toml in the project tree setting up the corresponding IJ
    modules and dependencies between them (so called "syncing").
    The changes are persisted in the workspace model cache, there are
    no additional config files, besides `.idea/poetry.xml`.
    
    If a new pyproject.xml module is added to the root of an existing
    project, we ask user whether its project model should be applied.
    
    When a pyproject.toml is then changed in the editor or externally,
    we either automatically apply the changes (reloading) or ask
    a user about it, depending on the settings in
    "Settings | Build, Execution, Deployment | Build Tools".
    
    If automatic linking and syncing doesn't work for some reason
    (e.g. there is no top-level pyproject.toml as in grazie-ml),
    there are manual actions "Link All Poetry Projects" and
    "Sync All Poetry Projects".
    
    GitOrigin-RevId: 77a480cdf56d45f22c943acbad3a7c20d5eb56a5

---
+++ python/src/com/jetbrains/python/projectModel/poetry/PoetryOpenProcessor.kt
@@ -0,0 +1,50 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.jetbrains.python.projectModel.poetry
+
+import com.intellij.ide.impl.runUnderModalProgressIfIsEdt
+import com.intellij.openapi.extensions.ExtensionNotApplicableException
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.projectImport.ProjectOpenProcessor
+import com.jetbrains.python.PyBundle
+import org.jetbrains.annotations.Nls
+
+/**
+ * Automatically configures a new project without `.idea/` as a project managed by Poetry if there is
+ * a top-level pyproject.toml at the project root. 
+ * The user will be asked if
+ * - There are several possible build systems for the project.
+ * - The top-level pyproject.toml is added afterward in a project with existing `.idea/`.
+ * - pyproject.toml files are found in non-top-level directories (requires IJPL-180733).
+ */
+class PoetryOpenProcessor: ProjectOpenProcessor() {
+  init {
+    if (!Registry.`is`("python.project.model.poetry")) {
+      throw ExtensionNotApplicableException.create()
+    }
+  }  
+  
+  private val importProvider = PoetryOpenProvider()
+  
+  override val name: @Nls String = PyBundle.message("python.project.model.poetry")
+
+  override fun canOpenProject(file: VirtualFile): Boolean = importProvider.canOpenProject(file)
+
+  override fun doOpenProject(virtualFile: VirtualFile, projectToClose: Project?, forceOpenInNewFrame: Boolean): Project? {
+    return runUnderModalProgressIfIsEdt { importProvider.openProject(virtualFile, projectToClose, forceOpenInNewFrame) }
+  }
+
+  override suspend fun openProjectAsync(virtualFile: VirtualFile,
+                                        projectToClose: Project?,
+                                        forceOpenInNewFrame: Boolean): Project? {
+    return importProvider.openProject(virtualFile, projectToClose, forceOpenInNewFrame)
+  }
+
+  override fun canImportProjectAfterwards(): Boolean = true
+
+  // TODO Requires IJPL-180733 
+  override suspend fun importProjectAfterwardsAsync(project: Project, file: VirtualFile) {
+    importProvider.linkToExistingProjectAsync(file, project)
+  }
+}
---
+++ python/testSrc/com/jetbrains/python/projectModel/poetry/PyPoetryOpenIntegrationTest.kt
@@ -0,0 +1,38 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.jetbrains.python.projectModel.poetry
+
+import com.intellij.openapi.components.service
+import com.intellij.openapi.externalSystem.testFramework.fixtures.multiProjectFixture
+import com.intellij.platform.testFramework.assertion.collectionAssertion.CollectionAssertions
+import com.intellij.platform.testFramework.assertion.moduleAssertion.ModuleAssertions
+import com.intellij.testFramework.common.timeoutRunBlocking
+import com.intellij.testFramework.junit5.RegistryKey
+import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.testFramework.junit5.fixture.tempPathFixture
+import com.intellij.testFramework.useProjectAsync
+import com.intellij.testFramework.utils.io.createFile
+import org.junit.jupiter.api.Test
+import kotlin.io.path.writeText
+import kotlin.time.Duration.Companion.seconds
+
+@RegistryKey("python.project.model.poetry", "true")
+@TestApplication
+class PyPoetryOpenIntegrationTest {
+  private val testRootFixture = tempPathFixture()
+  private val testRoot by testRootFixture
+  private val multiprojectFixture by multiProjectFixture(testRootFixture)
+
+  @Test
+  fun `project without dot-idea with pyproject-toml is automatically linked`() = timeoutRunBlocking(timeout = 20.seconds) {
+    testRoot.createFile("project/pyproject.toml").writeText("""
+      [tool.poetry]
+      name = "project"
+    """.trimIndent())
+    
+    multiprojectFixture.openProject("project").useProjectAsync { project -> 
+      ModuleAssertions.assertModules(project, "project")
+      CollectionAssertions.assertEqualsUnordered(listOf(testRoot.resolve("project")),
+                                                 project.service<PoetrySettings>().getLinkedProjects())
+    }
+  }
+}
