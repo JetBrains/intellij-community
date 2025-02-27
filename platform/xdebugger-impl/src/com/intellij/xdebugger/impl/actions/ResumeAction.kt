8883b577957f4c3afa69c6f4d9e159fea32dfba8
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-02-27 10:52:55 +0100

    [debugger] Convert ResumeAction to Kotlin
    GitOrigin-RevId: 0061030bbd84a5a7949358cddc35ba11ff61a759

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/ResumeAction.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/ResumeAction.kt
@@ -1,52 +1,45 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.xdebugger.impl.actions;
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.xdebugger.impl.actions
 
-import com.intellij.execution.actions.ChooseDebugConfigurationPopupAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.DumbService;
-import com.intellij.openapi.project.Project;
-import com.intellij.xdebugger.impl.DebuggerSupport;
-import com.intellij.xdebugger.impl.XDebugSessionImpl;
-import com.intellij.xdebugger.impl.ui.DebuggerUIUtil;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.execution.actions.ChooseDebugConfigurationPopupAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.DumbService.Companion.isDumb
+import com.intellij.xdebugger.impl.DebuggerSupport
+import com.intellij.xdebugger.impl.XDebugSessionImpl
+import com.intellij.xdebugger.impl.ui.DebuggerUIUtil
+import java.awt.event.KeyEvent
 
-import java.awt.event.KeyEvent;
+open class ResumeAction : XDebuggerActionBase(), DumbAware {
+  override fun isEnabled(e: AnActionEvent): Boolean {
+    val project = e.project
+    if (project == null) return false
 
-public class ResumeAction extends XDebuggerActionBase implements DumbAware {
-  @Override
-  protected boolean isEnabled(@NotNull AnActionEvent e) {
-    Project project = e.getProject();
-    if (project == null) return false;
-
-    XDebugSessionImpl session = (XDebugSessionImpl)DebuggerUIUtil.getSession(e);
-    if (session != null && !session.isStopped()) {
-      return !session.isReadOnly() && session.isPaused();
+    val session = DebuggerUIUtil.getSession(e) as XDebugSessionImpl?
+    if (session != null && !session.isStopped) {
+      return !session.isReadOnly && session.isPaused
     }
     // disable visual representation but leave the shortcut action enabled
-    return e.getInputEvent() instanceof KeyEvent;
+    return e.inputEvent is KeyEvent
   }
 
-  @Override
-  protected boolean isHidden(@NotNull AnActionEvent event) {
+  override fun isHidden(event: AnActionEvent): Boolean {
     if (!PauseAction.isPauseResumeMerged()) {
-      return super.isHidden(event);
+      return super.isHidden(event)
     }
-    return super.isHidden(event) || !isEnabled(event);
+    return super.isHidden(event) || !isEnabled(event)
   }
 
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
+  override fun actionPerformed(e: AnActionEvent) {
     if (!performWithHandler(e)) {
-      Project project = getEventProject(e);
-      if (project != null && !DumbService.isDumb(project)) {
-        new ChooseDebugConfigurationPopupAction().actionPerformed(e);
+      val project = getEventProject(e)
+      if (project != null && !isDumb(project)) {
+        ChooseDebugConfigurationPopupAction().actionPerformed(e)
       }
     }
   }
 
-  @Override
-  protected @NotNull DebuggerActionHandler getHandler(final @NotNull DebuggerSupport debuggerSupport) {
-    return debuggerSupport.getResumeActionHandler();
+  override fun getHandler(debuggerSupport: DebuggerSupport): DebuggerActionHandler {
+    return debuggerSupport.resumeActionHandler
   }
 }
