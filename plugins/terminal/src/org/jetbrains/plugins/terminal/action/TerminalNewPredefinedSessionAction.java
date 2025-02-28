333575a209d12410cbf4808cc8d1de750f132560
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-02-28 12:26:19 +0200

    [terminal] IJPL-179380 Refactor: extract shells detection to the single place
    But this change is not exactly the refactoring, because it also introduces the following minor additions, caused by the generalization of the logic:
    1. WSL distributions now will be also shown in the settings, in the shell path popup.
    2. In the settings, we no more use the ENVs from the corresponding ENV variables text field to detect the shells. We just use the ENVs of the IDE process. But it should not be a big deal since the only usage was to get the `CMDER_ROOT` value if it is missing the IDE ENVs.
    
    
    (cherry picked from commit 11ed9e7cb856b5f7228d44d43801fbf57d011e88)
    
    IJ-CR-156676
    
    GitOrigin-RevId: 7d52cf6ac2591d483020b860065e6b0788f27f8e

--- plugins/terminal/src/org/jetbrains/plugins/terminal/action/TerminalNewPredefinedSessionAction.java
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/action/TerminalNewPredefinedSessionAction.java
@@ -1,9 +1,6 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.terminal.action;
 
-import com.intellij.execution.configurations.PathEnvironmentVariableUtil;
-import com.intellij.execution.wsl.WSLDistribution;
-import com.intellij.execution.wsl.WslDistributionManager;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.IdeBundle;
 import com.intellij.openapi.actionSystem.*;
@@ -18,42 +15,24 @@ import com.intellij.openapi.ui.popup.ListPopup;
 import com.intellij.openapi.ui.popup.util.PopupUtil;
 import com.intellij.openapi.util.NlsActions;
 import com.intellij.openapi.util.NlsSafe;
-import com.intellij.openapi.util.SystemInfo;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.awt.RelativePoint;
-import com.intellij.util.EnvironmentUtil;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.plugins.terminal.TerminalOptionsConfigurableKt;
-import org.jetbrains.plugins.terminal.TerminalTabState;
-import org.jetbrains.plugins.terminal.TerminalToolWindowManager;
-import org.jetbrains.plugins.terminal.runner.LocalTerminalStartCommandBuilder;
+import org.jetbrains.plugins.terminal.*;
 import org.jetbrains.plugins.terminal.ui.OpenPredefinedTerminalActionProvider;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.InputEvent;
 import java.awt.event.MouseEvent;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class TerminalNewPredefinedSessionAction extends DumbAwareAction implements ActionRemoteBehaviorSpecification.Frontend {
-
-  private static final List<String> UNIX_BINARIES_DIRECTORIES = List.of(
-    "/bin",
-    "/usr/bin",
-    "/usr/local/bin",
-    "/opt/homebrew/bin"
-  );
-
-  private static final List<String> UNIX_SHELL_NAMES = List.of("bash", "zsh", "fish", "pwsh");
-
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
     Project project = e.getProject();
@@ -61,10 +40,9 @@ public final class TerminalNewPredefinedSessionAction extends DumbAwareAction im
     RelativePoint popupPoint = getPreferredPopupPoint(e);
     ApplicationManager.getApplication().executeOnPooledThread(() -> {
       List<OpenShellAction> shells = detectShells();
-      List<OpenShellAction> wsl = listOpenWslShellActions();
       List<AnAction> customActions = OpenPredefinedTerminalActionProvider.collectAll(project);
       ApplicationManager.getApplication().invokeLater(() -> {
-        ListPopup popup = createPopup(shells, wsl, customActions, e.getDataContext());
+        ListPopup popup = createPopup(shells, customActions, e.getDataContext());
         if (popupPoint != null) {
           popup.show(popupPoint);
         }
@@ -91,14 +69,12 @@ public final class TerminalNewPredefinedSessionAction extends DumbAwareAction im
   }
 
   private static @NotNull ListPopup createPopup(@NotNull List<OpenShellAction> shells,
-                                                @NotNull List<OpenShellAction> wsl,
                                                 @NotNull List<AnAction> customActions,
                                                 @NotNull DataContext dataContext) {
     DefaultActionGroup group = new DefaultActionGroup();
     group.addAll(shells);
-    group.addAll(wsl);
     group.addAll(customActions);
-    if (shells.size() + wsl.size() + customActions.size() > 0) {
+    if (shells.size() + customActions.size() > 0) {
       group.addSeparator();
     }
     group.add(new TerminalSettingsAction());
@@ -106,74 +82,32 @@ public final class TerminalNewPredefinedSessionAction extends DumbAwareAction im
                                                                false, true, false, null, -1, null);
   }
 
-  private static @NotNull List<OpenShellAction> listOpenWslShellActions() {
-    if (WSLDistribution.findWslExe() == null) return List.of();
-    List<WSLDistribution> distributions = WslDistributionManager.getInstance().getInstalledDistributions();
-    return ContainerUtil.map(distributions, (d) -> {
-      return new OpenShellAction(() -> d.getMsId(), List.of("wsl.exe", "-d", d.getMsId()), AllIcons.RunConfigurations.Wsl);
-    });
-  }
-
   private static @NotNull List<OpenShellAction> detectShells() {
-    List<OpenShellAction> actions = new ArrayList<>();
-    if (SystemInfo.isUnix) {
-      // Iterate over all combinations of path+shell to find executables.
-      for (String unixShellName : UNIX_SHELL_NAMES) {
-        List<String> validExecutablesDirectories = new ArrayList<>();
-        for (String executablesDirectory : UNIX_BINARIES_DIRECTORIES) {
-          var shellPath = executablesDirectory + "/" + unixShellName;
-          if (Files.exists(Path.of(shellPath))) {
-            validExecutablesDirectories.add(executablesDirectory);
-          }
+    return TerminalShellsDetector.detectShells()
+      .stream()
+      .collect(Collectors.groupingBy(DetectedShellInfo::getName, LinkedHashMap::new, Collectors.toList()))
+      .values()
+      .stream()
+      .flatMap(shellInfos -> {
+        if (shellInfos.size() > 1) {
+          return shellInfos.stream().map(info -> {
+            return createOpenShellAction(info.getPath(), info.getOptions(), info.getName() + " (" + info.getPath() + ")");
+          });
         }
-        if (validExecutablesDirectories.size() > 1) {
-          for (String executablesDirectory : validExecutablesDirectories) {
-            // i.e. /bin/zsh -> zsh (/bin)
-            ContainerUtil.addIfNotNull(actions, create(executablesDirectory + "/" + unixShellName, List.of(), unixShellName + " (" + executablesDirectory + ")"));
-          }
-        } else if (validExecutablesDirectories.size() == 1) {
-          // If only 1 shell of type fount - then there is no need to specify path.
-          ContainerUtil.addIfNotNull(actions, create(validExecutablesDirectories.get(0) + "/" + unixShellName, List.of(), unixShellName));
+        else {
+          DetectedShellInfo info = shellInfos.get(0);
+          return Stream.of(createOpenShellAction(info.getPath(), info.getOptions(), info.getName()));
         }
-      }
-    }
-    else if (SystemInfo.isWindows) {
-      File powershell = PathEnvironmentVariableUtil.findInPath("powershell.exe");
-      if (powershell != null && StringUtil.startsWithIgnoreCase(powershell.getAbsolutePath(), "C:\\Windows\\System32\\WindowsPowerShell\\")) {
-        ContainerUtil.addIfNotNull(actions, create(powershell.getAbsolutePath(), List.of(), "Windows PowerShell"));
-      }
-      File cmd = PathEnvironmentVariableUtil.findInPath("cmd.exe");
-      if (cmd != null && StringUtil.startsWithIgnoreCase(cmd.getAbsolutePath(), "C:\\Windows\\System32\\")) {
-        ContainerUtil.addIfNotNull(actions, create(cmd.getAbsolutePath(), List.of(), "Command Prompt"));
-      }
-      File pwsh = PathEnvironmentVariableUtil.findInPath("pwsh.exe");
-      if (pwsh != null && StringUtil.startsWithIgnoreCase(pwsh.getAbsolutePath(), "C:\\Program Files\\PowerShell\\")) {
-        ContainerUtil.addIfNotNull(actions, create(pwsh.getAbsolutePath(), List.of(), "PowerShell"));
-      }
-      File gitBashGlobal = new File("C:\\Program Files\\Git\\bin\\bash.exe");
-      File gitBashLocal = new File(System.getenv("LocalAppData") + "\\Programs\\Git\\bin\\bash.exe");
-      File gitBash = gitBashLocal.isFile() ? gitBashLocal : (gitBashGlobal.isFile() ? gitBashGlobal : null);
-      if (gitBash != null) {
-        ContainerUtil.addIfNotNull(actions, create(gitBash.getAbsolutePath(), List.of(), "Git Bash"));
-      }
-      String cmderRoot = EnvironmentUtil.getValue("CMDER_ROOT");
-      if (cmderRoot != null && cmd != null && StringUtil.startsWithIgnoreCase(cmd.getAbsolutePath(), "C:\\Windows\\System32\\")) {
-        File cmderInitBat = new File(cmderRoot, "vendor\\init.bat");
-        if (cmderInitBat.isFile()) {
-          ContainerUtil.addIfNotNull(actions, create(cmd.getAbsolutePath(), List.of("/k", cmderInitBat.getAbsolutePath()), "Cmder"));
-        }
-      }
-    }
-    return actions;
+      })
+      .toList();
   }
 
-  private static @Nullable OpenShellAction create(@NotNull String shellPath, @NotNull List<String> shellOptions, @NlsSafe String presentableName) {
-    if (Files.exists(Path.of(shellPath))) {
-      List<String> shellCommand = LocalTerminalStartCommandBuilder.convertShellPathToCommand(shellPath);
-      List<String> otherOptions = shellOptions.stream().filter(opt -> !shellCommand.contains(opt)).toList();
-      return new OpenShellAction(() -> presentableName, ContainerUtil.concat(shellCommand, otherOptions), null);
-    }
-    return null;
+  private static @NotNull OpenShellAction createOpenShellAction(@NotNull String shellPath,
+                                                                @NotNull List<String> shellOptions,
+                                                                @NlsSafe String presentableName) {
+    List<String> shellCommand = ContainerUtil.concat(List.of(shellPath), shellOptions);
+    Icon icon = shellPath.endsWith("wsl.exe") ? AllIcons.RunConfigurations.Wsl : null;
+    return new OpenShellAction(presentableName, shellCommand, icon);
   }
 
   private static final class TerminalSettingsAction extends DumbAwareAction {
@@ -199,11 +133,9 @@ public final class TerminalNewPredefinedSessionAction extends DumbAwareAction im
   private static final class OpenShellAction extends DumbAwareAction {
 
     private final List<String> myCommand;
-    private final Supplier<@NlsActions.ActionText String> myPresentableName;
 
-    private OpenShellAction(@NotNull Supplier<@NlsActions.ActionText String> presentableName, @NotNull List<String> command, @Nullable Icon icon) {
-      super(presentableName, icon);
-      myPresentableName = presentableName;
+    private OpenShellAction(@NotNull @NlsActions.ActionText String presentableName, @NotNull List<String> command, @Nullable Icon icon) {
+      super(presentableName, null, icon);
       myCommand = command;
     }
 
@@ -212,7 +144,7 @@ public final class TerminalNewPredefinedSessionAction extends DumbAwareAction im
       Project project = e.getProject();
       if (project != null) {
         TerminalTabState tabState = new TerminalTabState();
-        tabState.myTabName = myPresentableName.get();
+        tabState.myTabName = getTemplatePresentation().getText();
         tabState.myShellCommand = myCommand;
         TerminalToolWindowManager.getInstance(project).createNewSession(tabState);
       }
