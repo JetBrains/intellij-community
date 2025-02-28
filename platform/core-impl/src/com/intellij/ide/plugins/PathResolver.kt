1b91573e8076254c82d21bd4afc8727be7993f54
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-02-28 18:00:11 +0100

    [plugins] IJPL-179572 rename PathResolver file
    GitOrigin-RevId: 77ecbfb585c14e031c0acfa926daf18cb5b5cf42

--- platform/core-impl/src/com/intellij/ide/plugins/PathResolver.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/PathResolver.kt
@@ -1,6 +1,4 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-@file:JvmName("PathBasedJdomXIncluder")
-
 package com.intellij.ide.plugins
 
 import com.intellij.ide.plugins.parser.RawPluginDescriptor
