6c89c5666d7caff251d3c7fdf2e12aff554bf9bd
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2025-02-28 15:56:06 +0100

    [jps build] force rebuild if class instrumenters become enabled or disabled (IJPL-171493)
    Threading assertions, not-null assertions, and pattern validation assertions now use JvmClassFileInstrumenter API to trigger rebuild when they become enabled or disabled (e.g., when the corresponding plugin is enabled or disabled).
    
    GitOrigin-RevId: c6894b1438263903680a58c529eeda0170fcd1f9

--- plugins/IntelliLang/intellilang-jps-plugin/src/org/jetbrains/jps/intellilang/instrumentation/PatternValidatorBuilder.java
+++ plugins/IntelliLang/intellilang-jps-plugin/src/org/jetbrains/jps/intellilang/instrumentation/PatternValidatorBuilder.java
@@ -6,23 +6,43 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.jps.ModuleChunk;
 import org.jetbrains.jps.builders.JpsBuildBundle;
+import org.jetbrains.jps.cmdline.ProjectDescriptor;
 import org.jetbrains.jps.incremental.BinaryContent;
 import org.jetbrains.jps.incremental.CompileContext;
 import org.jetbrains.jps.incremental.CompiledClass;
+import org.jetbrains.jps.incremental.JvmClassFileInstrumenter;
 import org.jetbrains.jps.incremental.instrumentation.BaseInstrumentingBuilder;
 import org.jetbrains.jps.incremental.messages.BuildMessage;
 import org.jetbrains.jps.incremental.messages.CompilerMessage;
 import org.jetbrains.jps.intellilang.model.JpsIntelliLangConfiguration;
 import org.jetbrains.jps.intellilang.model.JpsIntelliLangExtensionService;
 import org.jetbrains.jps.model.JpsGlobal;
+import org.jetbrains.jps.model.module.JpsModule;
 import org.jetbrains.org.objectweb.asm.ClassReader;
 import org.jetbrains.org.objectweb.asm.ClassWriter;
 
 /**
  * @author Eugene Zhuravlev
  */
-public final class PatternValidatorBuilder extends BaseInstrumentingBuilder {
+public final class PatternValidatorBuilder extends BaseInstrumentingBuilder implements JvmClassFileInstrumenter {
   public PatternValidatorBuilder() { }
+
+  @Override
+  public @NotNull String getId() {
+    return "intelli-lang-pattern-assertions";
+  }
+
+  @Override
+  public boolean isEnabled(@NotNull ProjectDescriptor projectDescriptor, @NotNull JpsModule module) {
+    JpsGlobal jpsGlobal = projectDescriptor.getModel().getGlobal();
+    JpsIntelliLangConfiguration config = JpsIntelliLangExtensionService.getInstance().getConfiguration(jpsGlobal);
+    return config.getInstrumentationType() != InstrumentationType.NONE;
+  }
+
+  @Override
+  public int getVersion() {
+    return 0;
+  }
 
   @Override
   public @NotNull String getPresentableName() {
