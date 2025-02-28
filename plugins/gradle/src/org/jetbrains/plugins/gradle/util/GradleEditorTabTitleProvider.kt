4e5419adf4198c6113fbebb216112ef43b9753da
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-02-28 10:57:21 +0200

    [gradle] IJPL-178126 Provide title from GradleEditorTabTitleProvider using suspend function
    So, it will perform non-blocking read action.
    
    
    (cherry picked from commit 6cb3be8df6cd8ba1e9409692241753af86405f0c)
    
    IJ-CR-156418
    
    GitOrigin-RevId: 09836eb86a907e9a1ebb5b959dae9cbeaa9efe4a

--- plugins/gradle/src/org/jetbrains/plugins/gradle/util/GradleEditorTabTitleProvider.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/util/GradleEditorTabTitleProvider.kt
@@ -2,11 +2,15 @@
 package org.jetbrains.plugins.gradle.util
 
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.readAction
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.externalSystem.ExternalSystemModulePropertyManager
 import com.intellij.openapi.fileEditor.impl.EditorTabTitleProvider
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.util.Computable
+import com.intellij.openapi.util.NlsContexts
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VirtualFile
 
@@ -19,7 +23,27 @@ private class GradleEditorTabTitleProvider : EditorTabTitleProvider {
     val fileParent = file.parent ?: return null
 
     val projectFileIndex = ProjectFileIndex.getInstance(project)
-    val module = ApplicationManager.getApplication().runReadAction(Computable { projectFileIndex.getModuleForFile(file) }) ?: return null
+    val module = ApplicationManager.getApplication().runReadAction(Computable { projectFileIndex.getModuleForFile(file) })
+                 ?: return null
+
+    return getCustomTitle(module, file, fileParent)
+  }
+
+  override suspend fun getEditorTabTitleAsync(project: Project, file: VirtualFile): @NlsContexts.TabTitle String? {
+    if (!GradleConstants.KNOWN_GRADLE_FILES.contains(file.name)) {
+      return null
+    }
+
+    val fileParent = file.parent ?: return null
+
+    val projectFileIndex = project.serviceAsync<ProjectFileIndex>()
+    val module = readAction { projectFileIndex.getModuleForFile(file) }
+                 ?: return null
+
+    return getCustomTitle(module, file, fileParent)
+  }
+
+  private fun getCustomTitle(module: Module, file: VirtualFile, fileParent: VirtualFile): String? {
     val manager = ExternalSystemModulePropertyManager.getInstance(module)
 
     if (manager.getExternalSystemId() != GradleConstants.SYSTEM_ID.id) {
