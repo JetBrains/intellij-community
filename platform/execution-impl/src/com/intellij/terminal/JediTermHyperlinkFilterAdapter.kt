e20b3ec3ceb51f8317f65ae2977e4b93115f1113
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2025-03-01 22:43:47 +0100

    [terminal] IJ-CR-155762 migrate to `Channel` to not buffer `replay` elements
    `Channel` as a hot data source is more tailored to this producer-consumer task. Also, `Channel` is simpler than `MutableSharedFlow`.
    
    The problem of buffering `replay` elements could also be fixed with `MutableSharedFlow` and `launch(start = CoroutineStart.UNDISPATCHED)`, but it feels more like a workaround and makes reasoning about code correctness more difficult.
    
    GitOrigin-RevId: 023a772e351bbc11096d99bd4c2a63a4d0de6b15

--- platform/execution-impl/src/com/intellij/terminal/JediTermHyperlinkFilterAdapter.kt
+++ platform/execution-impl/src/com/intellij/terminal/JediTermHyperlinkFilterAdapter.kt
@@ -30,7 +30,7 @@ import com.jediterm.terminal.ui.hyperlinks.LinkInfoEx.HoverConsumer
 import com.jediterm.terminal.ui.hyperlinks.LinkInfoEx.PopupMenuGroupProvider
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.BufferOverflow
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.channels.Channel
 import java.awt.Rectangle
 import java.awt.event.MouseEvent
 import java.util.concurrent.CompletableFuture
@@ -44,12 +44,7 @@ internal class JediTermHyperlinkFilterAdapter(
 
   private val filterWrapper = CompositeFilterWrapper(project, console, widget)
 
-  private val requestFlow: MutableSharedFlow<Request> = MutableSharedFlow(
-    // don't lose initial requests due to adding a subscriber asynchronously
-    replay = MAX_BUFFERED_REQUESTS,
-    extraBufferCapacity = 0,
-    BufferOverflow.DROP_OLDEST
-  )
+  private val requestChannel: Channel<Request> = Channel(MAX_BUFFERED_REQUESTS, BufferOverflow.DROP_OLDEST)
 
   init {
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -62,10 +57,11 @@ internal class JediTermHyperlinkFilterAdapter(
     )
     Disposer.register(widget) {
       coroutineScope.cancel()
+      requestChannel.close()
     }
     coroutineScope.launch {
-      requestFlow.collect {
-        processRequest(it.lineInfo, it.future)
+      for (request in requestChannel) {
+        processRequest(request.lineInfo, request.future)
       }
     }
   }
@@ -76,7 +72,7 @@ internal class JediTermHyperlinkFilterAdapter(
 
   override fun apply(lineInfo: AsyncHyperlinkFilter.LineInfo): CompletableFuture<LinkResult?> {
     val future = CompletableFuture<LinkResult?>()
-    requestFlow.tryEmit(Request(lineInfo, future))
+    requestChannel.trySend(Request(lineInfo, future))
     return future
   }
 
