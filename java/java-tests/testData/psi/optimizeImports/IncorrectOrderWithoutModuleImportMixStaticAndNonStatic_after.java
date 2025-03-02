30bd4c00b852b185ced76e73e80654f0014a8243
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2025-03-02 08:03:13 +0100

    [java-imports] IDEA-368382 incorrect place for module import
    - support editorconfig
    - more tests
    
    
    (cherry picked from commit d7fd604e95b1dd197a3efbdbfcd54e81564b2490)
    
    IJ-CR-156496
    
    GitOrigin-RevId: 57a07f9cc0cf312fed460b96300b88a32771eeee

---
+++ java/java-tests/testData/psi/optimizeImports/IncorrectOrderWithoutModuleImportMixStaticAndNonStatic_after.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import ccc.CCC;
+import java.util.ArrayList;
+import java.util.List;
+import static java.util.Arrays.asList;
+
+class IncorrectOrderWithoutModuleImport {
+  public static void main(String[] args) {
+    List<String> a = new ArrayList<String>();
+    a.add("a");
+    final List<String> list = asList(args);
+    new CCC();
+  }
+}
