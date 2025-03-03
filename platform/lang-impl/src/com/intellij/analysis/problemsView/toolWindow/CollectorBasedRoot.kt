905971e50214f5e1bc058b6f7dbef6926326948a
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2025-03-04 00:20:06 +0100

    [ui] [vcs-ai] make open to reuse
    GitOrigin-RevId: da75dbb342542b0dfa0ddeb83087e77d7abb47c1

--- platform/lang-impl/src/com/intellij/analysis/problemsView/toolWindow/CollectorBasedRoot.kt
+++ platform/lang-impl/src/com/intellij/analysis/problemsView/toolWindow/CollectorBasedRoot.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.analysis.problemsView.toolWindow
 
 import com.intellij.analysis.problemsView.Problem
@@ -8,7 +8,7 @@ import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
-class CollectorBasedRoot(panel: ProblemsViewPanel, val collector: ProblemsCollector) : Root(panel) {
+open class CollectorBasedRoot(panel: ProblemsViewPanel, val collector: ProblemsCollector) : Root(panel) {
   internal constructor(panel: ProblemsViewPanel) : this(panel, ProblemsCollector.getInstance(panel.project)) {
     panel.project.messageBus.connect(this).subscribe(ProblemsListener.TOPIC, this)
   }
