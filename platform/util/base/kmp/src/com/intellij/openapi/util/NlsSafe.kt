efe64e5a834e76479c1448e31122bf3658b6b265
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-03-03 08:33:34 +0100

    IJPL-162853 move NlsSafe to `intellij.platform.util.kmp`
    it's not applicable to expressions anymore because Kotlin forbids it when Binary Retention is selected
    
    GitOrigin-RevId: e0f892c1cd2ced64a09e05eb35054a39dde327b0

--- platform/util/base/kmp/src/com/intellij/openapi/util/NlsSafe.kt
+++ platform/util/base/kmp/src/com/intellij/openapi/util/NlsSafe.kt
@@ -1,10 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.util;
+package com.intellij.openapi.util
 
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NonNls;
-
-import java.lang.annotation.*;
+import org.jetbrains.annotations.ApiStatus
 
 /**
  * Marker annotation for strings that don't require localization but still could be displayed in UI.
@@ -17,7 +14,7 @@ import java.lang.annotation.*;
  *   <li>Programming language or framework name</li>
  * </ul>
  * Avoid using NlsSafe just to suppress the "hardcoded string" inspection warning. Use {@link NonNls @NonNls}
- * if something is not intended to be displayed to the user: internal identifier, XML tag attribute, 
+ * if something is not intended to be displayed to the user: internal identifier, XML tag attribute,
  * substring to be searched in the external process output, etc.
  * <p>
  *   This annotation is primarily intended to be used on variable or method return values (in "producer" context).
@@ -26,8 +23,14 @@ import java.lang.annotation.*;
  * </p>
  */
 @ApiStatus.Experimental
-@Retention(RetentionPolicy.CLASS)
-@Documented
-@Target({ElementType.TYPE_USE, ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD, ElementType.LOCAL_VARIABLE})
-public @interface NlsSafe {
-}
+@Retention(AnnotationRetention.BINARY)
+@MustBeDocumented
+@Target(AnnotationTarget.TYPE,
+        AnnotationTarget.FUNCTION,
+        AnnotationTarget.VALUE_PARAMETER,
+        AnnotationTarget.FIELD,
+        AnnotationTarget.LOCAL_VARIABLE,
+        AnnotationTarget.PROPERTY_GETTER,
+        AnnotationTarget.PROPERTY_SETTER,
+)
+annotation class NlsSafe
