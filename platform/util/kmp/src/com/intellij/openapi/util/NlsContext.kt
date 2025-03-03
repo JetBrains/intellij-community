1cd868f22540541d4070247840412c25223cb95f
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-03-03 09:01:03 +0100

    IJPL-162853 move NlsContexts to KMP
    GitOrigin-RevId: 96be5bde839261ed089a9ec44b9d457e3e2cadb7

--- platform/util/kmp/src/com/intellij/openapi/util/NlsContext.kt
+++ platform/util/kmp/src/com/intellij/openapi/util/NlsContext.kt
@@ -1,25 +1,20 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.util;
+package com.intellij.openapi.util
 
-import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NonNls
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Retention(RetentionPolicy.CLASS)
-@Target(ElementType.ANNOTATION_TYPE)
-public @interface NlsContext {
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.ANNOTATION_CLASS)
+annotation class NlsContext(
   /**
    * Provide a neat property key prefix that unambiguously defines literal usage context.
    * E.g. "button", "button.tooltip" for button text and tooltip correspondingly, "action.text" for action text
    */
-  @NonNls String prefix() default "";
+  val prefix: @NonNls String = "",
 
   /**
    * Provide a neat property key suffix that unambiguously defines literal usage context.
    * E.g. "description" for action/intention description
    */
-  @NonNls String suffix() default "";
-}
+  val suffix: @NonNls String = "",
+)
