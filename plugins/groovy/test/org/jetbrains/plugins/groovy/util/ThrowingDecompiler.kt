802f3c56ad04f789ee14e7648e065b79e18a606f
Andrey Zaytsev <andrey.zaytsev@jetbrains.com>
2025-03-03 16:22:10 +0100

    ClassFileDecompilers.EP_NAME is static now
    GitOrigin-RevId: 96e82d3d78e09e0e0346b7631f360e5886900235

--- plugins/groovy/test/org/jetbrains/plugins/groovy/util/ThrowingDecompiler.kt
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/util/ThrowingDecompiler.kt
@@ -8,7 +8,7 @@ import com.intellij.psi.compiled.ClassFileDecompilers
 object ThrowingDecompiler : ClassFileDecompilers.Light() {
   @JvmStatic
   fun disableDecompilers(parentDisposable: Disposable) {
-    ClassFileDecompilers.getInstance().EP_NAME.point.registerExtension(this, parentDisposable)
+    ClassFileDecompilers.EP_NAME.point.registerExtension(this, parentDisposable)
   }
 
   override fun accepts(file: VirtualFile): Boolean = true
