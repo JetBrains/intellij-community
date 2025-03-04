b2d269f19b024a084133ae6db4ec889043877085
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-03-04 10:01:17 +0200

    [terminal] IJPL-179295 Add comments to the TerminalSession
    Clarify the choice of the `SendChannel` to send the input events.
    
    GitOrigin-RevId: 33c03d236fa6317a844da28a4cf264f50461ca00

--- platform/execution-impl/src/com/intellij/terminal/session/TerminalSession.kt
+++ platform/execution-impl/src/com/intellij/terminal/session/TerminalSession.kt
@@ -7,7 +7,19 @@ import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
 interface TerminalSession {
+  /**
+   * Use this channel to send the input events to the Terminal session.
+   *
+   * The channel is used here to mimic the behavior of the simple output stream:
+   * 1. Send operation returns immediately without waiting for response from the receiver.
+   * 2. It is guaranteed that events will be received in the same order they are sent.
+   */
   suspend fun getInputChannel(): SendChannel<TerminalInputEvent>
 
+  /**
+   * Use this flow to handle the output events of the Terminal session.
+   *
+   * Underlying logic should continue reading the PTYs output stream only if there is some collector of this flow.
+   */
   suspend fun getOutputFlow(): Flow<List<TerminalOutputEvent>>
 }
