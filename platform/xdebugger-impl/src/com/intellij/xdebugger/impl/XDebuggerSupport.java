45619c23006593c5c90299c2a2fcd157926332a3
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-03-04 12:41:22 +0100

    [debugger] Restore XDebuggerSupport, so it will be passed in XDebuggerActionBase
    GitOrigin-RevId: 4d0657f6a1988688d981bbf30fd3f28be4f88ca9

---
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/XDebuggerSupport.java
@@ -0,0 +1,9 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.xdebugger.impl;
+
+import org.jetbrains.annotations.ApiStatus;
+
+// Don't delete this class, so it will be passed in XDebuggerActionBase
+@ApiStatus.Internal
+class XDebuggerSupport extends DebuggerSupport {
+}
