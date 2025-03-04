c63994201b91e7e1dfbf213a1fee045fb4df81a5
Alex Grönholm <alex.gronholm@jetbrains.com>
2025-03-04 23:51:22 +0000

    PY-32478: Suggest adding "async" to a function with the error "'await' outside async function"
    Merge-request: IJ-MR-156125
    Merged-by: Andrey Vokin <andrey.vokin@jetbrains.com>
    
    (cherry picked from commit f3a2bc86331013879716b6991da26307c63b10fc)
    
    IJ-MR-156125
    
    GitOrigin-RevId: 10f0b3a8f60469af64bfd97613f258a282c9d0e2

--- python/python-psi-impl/src/com/jetbrains/python/validation/PyAsyncAwaitAnnotator.java
+++ python/python-psi-impl/src/com/jetbrains/python/validation/PyAsyncAwaitAnnotator.java
@@ -1,5 +1,10 @@
 package com.jetbrains.python.validation;
 
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.modcommand.ActionContext;
+import com.intellij.modcommand.ModPsiUpdater;
+import com.intellij.modcommand.PsiUpdateModCommandAction;
 import com.jetbrains.python.PyPsiBundle;
 import com.jetbrains.python.PyTokenTypes;
 import com.jetbrains.python.codeInsight.dataflow.scope.ScopeUtil;
@@ -14,8 +19,31 @@ public final class PyAsyncAwaitAnnotator extends PyAnnotator {
     if (node.getOperator() == PyTokenTypes.AWAIT_KEYWORD) {
       var scopeOwner = ScopeUtil.getScopeOwner(node);
       if (!(scopeOwner instanceof PyFunction pyFunction && pyFunction.isAsync())) {
-        markError(node.getFirstChild(), PyPsiBundle.message("ANN.await.outside.async.function"));
+        var annotation = getHolder()
+          .newAnnotation(HighlightSeverity.ERROR, PyPsiBundle.message("ANN.await.outside.async.function"))
+          .range(node.getFirstChild());
+        if (scopeOwner instanceof PyFunction pyFunction) {
+            annotation = annotation.newFix(new ConvertIntoAsyncFunctionFix(pyFunction)).registerFix();
+        }
+        annotation.create();
       }
+    }
+  }
+
+  private static class ConvertIntoAsyncFunctionFix extends PsiUpdateModCommandAction<PyFunction> {
+    protected ConvertIntoAsyncFunctionFix(@NotNull PyFunction element) {
+      super(element);
+    }
+
+    @Override
+    protected void invoke(@NotNull ActionContext context, @NotNull PyFunction element, @NotNull ModPsiUpdater updater) {
+      ASTNode defKeyword = element.getNode().findChildByType(PyTokenTypes.DEF_KEYWORD);
+      element.getNode().addLeaf(PyTokenTypes.ASYNC_KEYWORD, "async ", defKeyword);
+    }
+
+    @Override
+    public @NotNull String getFamilyName() {
+      return PyPsiBundle.message("QFIX.convert.into.async.function");
     }
   }
 }
