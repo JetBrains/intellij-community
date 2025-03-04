060e7b600786be3d2a0c428c20a2d761255dca9f
Alexander Zolotov <zolotov@jetbrains.com>
2025-03-04 12:44:29 +0100

    [fleet] do not depend on fleet.preferences in community modules
    GitOrigin-RevId: b706c1e3ef5ffc30ec8f23a43cd032ff2316edd8

--- fleet/reporting/shared/src/module-info.java
+++ fleet/reporting/shared/src/module-info.java
@@ -4,9 +4,7 @@ module fleet.reporting.shared {
   requires kotlinx.coroutines.core;
   requires kotlinx.serialization.json;
   requires kotlinx.serialization.core;
-  requires fleet.preferences;
 
-  exports fleet.reporting.shared.config;
   exports fleet.reporting.shared.fahrplan;
   exports fleet.reporting.shared.tracing;
   exports fleet.reporting.shared.runtime;
