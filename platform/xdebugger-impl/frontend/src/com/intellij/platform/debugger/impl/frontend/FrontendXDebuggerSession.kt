9715ad43992116c1e6f3a472bea6b7299c7091f2
Alexander Kuznetsov <aleksander.kuznetsov@jetbrains.com>
2025-03-04 11:25:24 +0100

    [rd][debugger] IJPL-177088 FrontendXDebuggerSession as a frontend-side session proxy
    Only a carcass; all implemented methods throw NotImplementedError
    
    GitOrigin-RevId: 49d27f267ea9f54b7414d25c478067daf1d89a16

--- platform/xdebugger-impl/frontend/src/com/intellij/platform/debugger/impl/frontend/FrontendXDebuggerSession.kt
+++ platform/xdebugger-impl/frontend/src/com/intellij/platform/debugger/impl/frontend/FrontendXDebuggerSession.kt
@@ -1,26 +1,38 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.debugger.impl.frontend
 
+import com.intellij.execution.process.ProcessHandler
+import com.intellij.execution.ui.ConsoleView
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.DataSink
+import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.project.Project
 import com.intellij.platform.debugger.impl.frontend.evaluate.quick.FrontendXDebuggerEvaluator
 import com.intellij.platform.debugger.impl.frontend.evaluate.quick.FrontendXValue
 import com.intellij.platform.debugger.impl.frontend.evaluate.quick.createFrontendXDebuggerEvaluator
 import com.intellij.platform.util.coroutines.childScope
+import com.intellij.xdebugger.XDebugSessionListener
 import com.intellij.xdebugger.XSourcePosition
 import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider
+import com.intellij.xdebugger.frame.XExecutionStack
+import com.intellij.xdebugger.frame.XStackFrame
+import com.intellij.xdebugger.frame.XSuspendContext
 import com.intellij.xdebugger.impl.frame.XDebugSessionProxy
 import com.intellij.xdebugger.impl.frame.XValueMarkers
 import com.intellij.xdebugger.impl.rpc.*
+import com.intellij.xdebugger.impl.ui.XDebugSessionData
 import com.intellij.xdebugger.impl.ui.XDebugSessionTab
+import com.intellij.xdebugger.ui.XDebugTabLayouter
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 
 internal class FrontendXDebuggerSession(
-  private val project: Project,
+  override val project: Project,
   scope: CoroutineScope,
   sessionDto: XDebugSessionDto,
-) {
+) : XDebugSessionProxy {
   private val cs = scope.childScope("Session ${sessionDto.id}")
   private val localEditorsProvider = sessionDto.editorsProviderDto.editorsProvider
   val id = sessionDto.id
@@ -114,9 +126,88 @@ internal class FrontendXDebuggerSession(
     }
   }
 
+  // TODO all of the methods below
+  // TODO pass in DTO?
+  override val sessionName: String
+    get() = TODO("Not yet implemented")
+  override val sessionData: XDebugSessionData?
+    get() = TODO("Not yet implemented")
+  override val consoleView: ConsoleView?
+    get() = TODO("Not yet implemented")
+  override val restartActions: List<AnAction>
+    get() = TODO("Not yet implemented")
+  override val extraActions: List<AnAction>
+    get() = TODO("Not yet implemented")
+  override val extraStopActions: List<AnAction>
+    get() = TODO("Not yet implemented")
+  override val processHandler: ProcessHandler
+    get() = TODO("Not yet implemented")
+  override val coroutineScope: CoroutineScope
+    get() = TODO("Not yet implemented")
+
+  override fun getFrameSourcePosition(frame: XStackFrame): XSourcePosition? {
+    TODO("Not yet implemented")
+  }
+
+  override fun getCurrentExecutionStack(): XExecutionStack? {
+    TODO("Not yet implemented")
+  }
+
+  override fun getCurrentStackFrame(): XStackFrame? {
+    TODO("Not yet implemented")
+  }
+
+  override fun setCurrentStackFrame(executionStack: XExecutionStack, frame: XStackFrame, isTopFrame: Boolean) {
+    TODO("Not yet implemented")
+  }
+
+  override fun hasSuspendContext(): Boolean {
+    TODO("Not yet implemented")
+  }
+
+  override fun isSteppingSuspendContext(): Boolean {
+    TODO("Not yet implemented")
+  }
+
+  override fun computeExecutionStacks(provideContainer: () -> XSuspendContext.XExecutionStackContainer) {
+    TODO("Not yet implemented")
+  }
+
+  override fun createTabLayouter(): XDebugTabLayouter {
+    TODO("Not yet implemented")
+  }
+
+  override fun addSessionListener(listener: XDebugSessionListener, disposable: Disposable) {
+    TODO("Not yet implemented")
+  }
+
+  override fun rebuildViews() {
+    TODO("Not yet implemented")
+  }
+
+  override fun registerAdditionalActions(leftToolbar: DefaultActionGroup, topLeftToolbar: DefaultActionGroup, settings: DefaultActionGroup) {
+    TODO("Not yet implemented")
+  }
+
+  override fun putKey(sink: DataSink) {
+    TODO("Not yet implemented")
+  }
+
+  override fun updateExecutionPosition() {
+    TODO("Not yet implemented")
+  }
+
+  override fun onTabInitialized(tab: XDebugSessionTab) {
+    TODO("Not yet implemented")
+  }
+
+  override suspend fun sessionId(): XDebugSessionId {
+    TODO("Not yet implemented")
+  }
+
   fun closeScope() {
     cs.cancel()
   }
 }
 
-private fun createProxy(session: FrontendXDebuggerSession): XDebugSessionProxy = TODO()
+private fun createProxy(session: FrontendXDebuggerSession): XDebugSessionProxy = session
