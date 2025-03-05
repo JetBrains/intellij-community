7964ea8d1bad3b5cf1c1f9a58ea1108f8a64db96
Nikita Katkov <nikita.katkov@jetbrains.com>
2025-03-05 13:50:40 +0100

    [recent files] IJPL-180214 Do not try to parse URL as path and employ safe path computation to avoid uncaught exceptions
    GitOrigin-RevId: 669f8806c89e106fa4149d60df4cafa318895e7b

--- platform/platform-impl/src/com/intellij/ide/actions/Switcher.kt
+++ platform/platform-impl/src/com/intellij/ide/actions/Switcher.kt
@@ -40,6 +40,7 @@ import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.io.toNioPathOrNull
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.util.text.Strings
@@ -579,11 +580,13 @@ object Switcher : BaseSwitcherAction(null) {
       )
       ReadAction.nonBlocking<List<ListItemData>> {
         items.map {
-          val parentPath = Path(it.file.presentableUrl).parent
+          val parentPath = it.file.parent?.path?.toNioPathOrNull()
           val sameNameFiles = FilenameIndex.getVirtualFilesByName(it.file.name, GlobalSearchScope.projectScope(project))
           val result = if (parentPath == null ||
                            parentPath.nameCount == 0 ||
-                           sameNameFiles.size <= 1) ""
+                           sameNameFiles.size <= 1) {
+            ""
+          }
           else {
             val filePath = parentPath.pathString
             val projectPath = project.basePath?.let { FileUtil.toSystemDependentName(it) }
