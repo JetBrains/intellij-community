256401d3e7e5b32d99f1475f3c59f7b24d721eea
Ilya.Komolkin <ilya.komolkin@jetbrains.com>
2025-03-05 21:26:29 +0000

    ik/kotlin-new-project-tests
    fix the IDE context name
    
    it makes the correct structure of the resulting TC artifacts
    
    minor
    
    don't check parent module's facets
    
    fix JVM target for Gradle only
    
    get JVM taget version from jvmToolchain of the build script
    
    fix file name in the report
    
    make screenshot on failure
    
    + add the test project to report
    
    timeout ++
    
    TODOs
    
    double the project import timeout
    
    use Test suffix in class names
    
    + test group
    
    minor
    
    improve facet checks
    
    format
    
    clean up
    
    use checkbox for sampleCode and multi module options
    
    build file highlighting test
    
    critical
    
    fix after rebasing
    
    fix after rebasing
    
    fix facet test
    
    + module test
    
    + facets test
    
    fix test data
    
    new Kotlin project tests
    
    
    Merge-request: IJ-MR-156129
    Merged-by: Ilya Komolkin <Ilya.Komolkin@jetbrains.com>
    
    GitOrigin-RevId: f298c7a023703fac51716992a3377e2c09618c5b

---
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ModuleManager.kt
@@ -0,0 +1,17 @@
+package com.intellij.driver.sdk
+
+import com.intellij.driver.client.Driver
+import com.intellij.driver.client.Remote
+import com.intellij.driver.client.service
+
+@Remote("com.intellij.openapi.module.ModuleManager")
+interface ModuleManager {
+  fun getModules(): Array<Module>
+}
+
+@Remote("com.intellij.openapi.module.Module")
+interface Module {
+  fun getName(): String
+}
+
+fun Driver.getModules(project: Project? = null) = service<ModuleManager>(project ?: singleProject()).getModules().toList()
