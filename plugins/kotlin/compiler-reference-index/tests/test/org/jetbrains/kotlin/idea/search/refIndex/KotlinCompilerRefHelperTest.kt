8b8628c806c4790d2a1e7e67147797236c406945
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2025-03-05 12:59:08 +0100

    [kotlin] Removed unnecessary tuneFixture in KotlinCompilerRefHelperTest
    KTIJ-32943
    
    
    (cherry picked from commit 923dc489c9016e82934d776d4431e06d1bfef5b3)
    
    IJ-CR-155769
    
    GitOrigin-RevId: 0f41a5631d245f9a3124d14fc2adfff5089e2bc8

--- plugins/kotlin/compiler-reference-index/tests/test/org/jetbrains/kotlin/idea/search/refIndex/KotlinCompilerRefHelperTest.kt
+++ plugins/kotlin/compiler-reference-index/tests/test/org/jetbrains/kotlin/idea/search/refIndex/KotlinCompilerRefHelperTest.kt
@@ -6,10 +6,7 @@ import com.intellij.java.compiler.CompilerReferencesTestBase
 import com.intellij.psi.CommonClassNames
 import com.intellij.psi.util.descendantsOfType
 import com.intellij.testFramework.SkipSlowTestLocally
-import com.intellij.testFramework.builders.JavaModuleFixtureBuilder
 import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginMode
-import org.jetbrains.kotlin.idea.base.plugin.artifacts.KotlinArtifactNames
-import org.jetbrains.kotlin.idea.base.plugin.artifacts.TestKotlinArtifacts
 import org.jetbrains.kotlin.idea.test.ConfigLibraryUtil
 import org.jetbrains.kotlin.idea.test.ExpectedPluginModeProvider
 import org.jetbrains.kotlin.idea.test.setUpWithKotlinPlugin
@@ -28,17 +25,6 @@ open class KotlinCompilerRefHelperTest : CompilerReferencesTestBase(), ExpectedP
             ConfigLibraryUtil.configureKotlinRuntime(module)
         } else {
             project.enableK1Compiler()
-        }
-    }
-
-    override fun tuneFixture(moduleBuilder: JavaModuleFixtureBuilder<*>) {
-        super.tuneFixture(moduleBuilder)
-        if (pluginMode == KotlinPluginMode.K2) {
-            moduleBuilder.addLibrary(KotlinArtifactNames.KOTLIN_STDLIB, TestKotlinArtifacts.kotlinStdlib.path)
-        } else {
-            // For the K1 tests we want to use a Kotlin 1.x library because Kotlin 2 libraries might not be able to
-            // be consumed by the K1 compiler.
-            moduleBuilder.addMavenLibrary(JavaModuleFixtureBuilder.MavenLib("org.jetbrains.kotlin:kotlin-stdlib:1.9.25"))
         }
     }
 
