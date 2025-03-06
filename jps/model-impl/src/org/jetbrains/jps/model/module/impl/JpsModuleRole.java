b7bc760b2f8d3bd235c4731172d72ef24b837ea5
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2025-03-06 17:23:51 +0100

    [jps model] speed up JpsNamedElementReferenceBase::resolve method (IDEA-368571)
    Before it iterated over all elements in the collection to find the element by name. Now, a special interface JpsNamedElementCollection with corresponding JpsNamedElementCollectionRole is introduced. Its implementation maintains a map from names to elements, allowing quickly finding an element by its name.
    
    GitOrigin-RevId: c9e83186586f81c25cab67b261ab3a8b74102f99

--- jps/model-impl/src/org/jetbrains/jps/model/module/impl/JpsModuleRole.java
+++ jps/model-impl/src/org/jetbrains/jps/model/module/impl/JpsModuleRole.java
@@ -3,12 +3,12 @@ package org.jetbrains.jps.model.module.impl;
 
 import org.jetbrains.jps.model.JpsElementChildRole;
 import org.jetbrains.jps.model.ex.JpsElementChildRoleBase;
-import org.jetbrains.jps.model.ex.JpsElementCollectionRole;
+import org.jetbrains.jps.model.ex.JpsNamedElementCollectionRole;
 import org.jetbrains.jps.model.module.JpsModule;
 
 public final class JpsModuleRole extends JpsElementChildRoleBase<JpsModule> {
   private static final JpsElementChildRole<JpsModule> INSTANCE = new JpsModuleRole();
-  public static final JpsElementCollectionRole<JpsModule> MODULE_COLLECTION_ROLE = JpsElementCollectionRole.create(INSTANCE);
+  public static final JpsNamedElementCollectionRole<JpsModule> MODULE_COLLECTION_ROLE = JpsNamedElementCollectionRole.create(INSTANCE);
 
   private JpsModuleRole() {
     super("module");
