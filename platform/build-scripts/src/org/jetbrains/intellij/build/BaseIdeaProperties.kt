f8893984762a584b0d034389eb78228be81bc611
Kajus Kuncaitis <kajus.kuncaitis@jetbrains.com>
2025-03-06 16:57:58 +0100

    [gradle][IDEA-366033] Remove plugin from distribution
    GitOrigin-RevId: a2d98be1903df02cc025e42d0aac0328df63798a

--- platform/build-scripts/src/org/jetbrains/intellij/build/BaseIdeaProperties.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/BaseIdeaProperties.kt
@@ -34,7 +34,6 @@ val IDEA_BUNDLED_PLUGINS: PersistentList<String> = DEFAULT_BUNDLED_PLUGINS + seq
   "intellij.maven.server",
   "intellij.gradle",
   "intellij.gradle.dependencyUpdater",
-  "intellij.gradle.declarativeSync",
   "intellij.android.gradle.declarative.lang.ide",
   "intellij.android.gradle.dsl",
   "intellij.gradle.java",
