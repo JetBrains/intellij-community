6c2df8d946695ad4659e478f7d315457fc9bec05
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-03-06 14:45:51 +0200

    IJPL-177414 Make TerminalFontSettings immutable
    Making them mutable was confusing, as the user might
    think that changing a property would be applied immediately.
    Instead make it immutable and make copies to change
    the retrieved value in the settings.
    
    GitOrigin-RevId: ab13cce0d3ad5fb4f2bf17df86d2415c98e363a7

--- plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalOptionsConfigurable.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalOptionsConfigurable.kt
@@ -48,7 +48,7 @@ internal class TerminalOptionsConfigurable(private val project: Project) : Bound
     val optionsProvider = TerminalOptionsProvider.instance
     val projectOptionsProvider = TerminalProjectOptionsProvider.getInstance(project)
     val blockTerminalOptions = BlockTerminalOptions.getInstance()
-    val fontPreferences = TerminalFontOptions.getInstance().getTerminalFontSettings()
+    var fontPreferences = TerminalFontOptions.getInstance().getTerminalFontSettings()
 
     return panel {
       lateinit var terminalEngineComboBox: ComboBox<TerminalEngine>
@@ -159,7 +159,7 @@ internal class TerminalOptionsConfigurable(private val project: Project) : Bound
               componentSet = {comboBox, value -> comboBox.fontName = value },
               MutableProperty(
                 getter = { fontPreferences.fontFamily },
-                setter = { fontPreferences.fontFamily = it },
+                setter = { fontPreferences = fontPreferences.copy(fontFamily = it) },
               ).toNullableProperty()
             )
         }
@@ -170,21 +170,21 @@ internal class TerminalOptionsConfigurable(private val project: Project) : Bound
             .columns(4)
             .bindText(
               getter = { fontPreferences.fontSize.fontSizeToString() },
-              setter = { fontPreferences.fontSize = it.parseFontSize() },
+              setter = { fontPreferences = fontPreferences.copy(fontSize = it.parseFontSize()) },
             )
           textField()
             .label(message("settings.line.height"))
             .columns(4)
             .bindText(
               getter = { fontPreferences.lineSpacing.spacingToString() },
-              setter = { fontPreferences.lineSpacing = it.parseSpacing() },
+              setter = { fontPreferences = fontPreferences.copy(lineSpacing = it.parseSpacing()) },
             )
           textField()
             .label(message("settings.column.width"))
             .columns(4)
             .bindText(
               getter = { fontPreferences.columnSpacing.spacingToString() },
-              setter = { fontPreferences.columnSpacing = it.parseSpacing() },
+              setter = { fontPreferences = fontPreferences.copy(columnSpacing = it.parseSpacing()) },
             )
         }
 
