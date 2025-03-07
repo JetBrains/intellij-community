7f68d4db21c9cf5defa4d40f1bfce64ee9109dd8
Vitaly Legchilkin <vitaly.legchilkin@jetbrains.com>
2025-03-07 16:54:38 +0100

    [python] adjust welcome script location to use source root if available (PY-79446)
    (cherry picked from commit fb3b8690d2b441f8bc9ee708389913c1c353ba1b)
    
    
    (cherry picked from commit d0bde37fcd7170d70160b0ecc309e4d039e20f02)
    
    IJ-MR-157664
    
    GitOrigin-RevId: 0d6ed0c8a60b6ef0996f151c60304d1c8f76651b

--- python/ide/impl/src/com/intellij/pycharm/community/ide/impl/newProjectWizard/impl/emptyProject/PyV3EmptyProjectSettings.kt
+++ python/ide/impl/src/com/intellij/pycharm/community/ide/impl/newProjectWizard/impl/emptyProject/PyV3EmptyProjectSettings.kt
@@ -5,12 +5,13 @@ import com.intellij.openapi.application.EDT
 import com.intellij.openapi.application.edtWriteAction
 import com.intellij.openapi.application.writeIntentReadAction
 import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.rootManager
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.vfs.VirtualFile
-import com.jetbrains.python.newProjectWizard.PyV3ProjectTypeSpecificSettings
 import com.intellij.pycharm.community.ide.impl.newProjectWizard.welcome.PyWelcome
 import com.jetbrains.python.Result
 import com.jetbrains.python.errorProcessing.PyError
+import com.jetbrains.python.newProjectWizard.PyV3ProjectTypeSpecificSettings
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -18,8 +19,10 @@ class PyV3EmptyProjectSettings(var generateWelcomeScript: Boolean = false) : PyV
 
   override suspend fun generateProject(module: Module, baseDir: VirtualFile, sdk: Sdk): Result<Unit, PyError> {
     if (!generateWelcomeScript) return Result.success(Unit)
+
+    val sourceRoot = module.rootManager.sourceRoots.firstOrNull() ?: baseDir
     val file = edtWriteAction {
-      PyWelcome.prepareFile(module.project, baseDir)
+      PyWelcome.prepareFile(module.project, sourceRoot)
     }
     withContext(Dispatchers.EDT) {
       writeIntentReadAction {
