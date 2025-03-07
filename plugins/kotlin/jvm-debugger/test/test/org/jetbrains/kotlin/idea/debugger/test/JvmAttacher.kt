c0297c34d5d841c2e85b38784b09a16e4ddd92b0
Egor Ushakov <egor.ushakov@jetbrains.com>
2025-03-07 17:11:36 +0100

    [debugger] delete the debugger agent file after every test finish
    GitOrigin-RevId: 1b62bebebc5bb1d03104c5d539fe79037748c428

--- plugins/kotlin/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/JvmAttacher.kt
+++ plugins/kotlin/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/JvmAttacher.kt
@@ -1,6 +1,7 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.debugger.test
 
+import com.intellij.debugger.engine.AsyncStacksUtils
 import com.intellij.debugger.impl.DebuggerSession
 import com.intellij.debugger.impl.GenericDebuggerRunnerSettings
 import com.intellij.debugger.impl.RemoteConnectionBuilder
@@ -34,8 +35,10 @@ internal class JvmAttacher : VmAttacher {
                 debuggerRunnerSettings.debugPort
             )
                 .checkValidity(true)
-                .asyncAgent(true)
+                .asyncAgent(false) // add manually to allow early tmp folder deletion
                 .create(javaCommandLineState.javaParameters)
+
+        AsyncStacksUtils.addDebuggerAgent(javaParameters, testCase.project, true, testCase.testRootDisposable)
 
         val env = javaCommandLineState.environment
 
