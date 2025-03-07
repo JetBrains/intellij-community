c6f7c649b482010cd34461bd054d82850572aba5
Alexander Zolotov <zolotov@jetbrains.com>
2025-03-07 12:30:41 +0100

    [fleet] all methods of concurrent hash map should be delegated to ConcurrentHashMap from JDK, even if they have implementation in Map and MutableMap classes
    GitOrigin-RevId: 18e3f126578e48e1b1af0546aa297a32423afe03

--- fleet/multiplatform.shims/srcJvmMain/fleet/multiplatform/shims/ConcurrentHashMap.jvm.kt
+++ fleet/multiplatform.shims/srcJvmMain/fleet/multiplatform/shims/ConcurrentHashMap.jvm.kt
@@ -7,50 +7,22 @@ import java.util.concurrent.ConcurrentHashMap as JavaConcurrentHashMap
 
 
 @Actual(linkedTo = "ConcurrentHashMap")
-internal fun <K, V> ConcurrentHashMapJvm(): ConcurrentHashMap<K, V> =
-  object : ConcurrentHashMap<K, V> {
-  val hashMap = JavaConcurrentHashMap<K, V>()
+internal fun <K, V> ConcurrentHashMapJvm(): ConcurrentHashMap<K, V> = MultiplatformConcurrentHashMap(JavaConcurrentHashMap())
 
-  override fun remove(key: K): V? = hashMap.remove(key)
-
-  override fun putIfAbsent(key: K, value: V): V? = hashMap.putIfAbsent(key, value)
-
-  override fun computeIfAbsent(key: K, f: (K) -> V): V = hashMap.computeIfAbsent(key, f)
-
-  override fun compute(key: K, f: (K, V?) -> V?): V? = hashMap.compute(key, f)
-
-  override fun remove(key: K, value: V): Boolean = hashMap.remove(key, value)
-
-    override val entries: MutableSet<MutableMap.MutableEntry<K, V>>
-    get() = hashMap.entries
-  override val keys: MutableSet<K>
-    get() = hashMap.keys
-  override val size: Int
-    get() = hashMap.size
-  override val values: MutableCollection<V>
-    get() = hashMap.values
-
-  override fun clear() {
-    hashMap.clear()
+private class MultiplatformConcurrentHashMap<K, V>(val hashMap: JavaConcurrentHashMap<K, V>) : MutableMap<K, V> by hashMap, ConcurrentHashMap<K, V> {
+  override fun remove(key: K, value: V): Boolean {
+    return hashMap.remove(key, value)
   }
 
-  override fun isEmpty(): Boolean = hashMap.isEmpty()
-
-  override fun putAll(from: Map<out K, V>) {
-    hashMap.putAll(from)
+  override fun putIfAbsent(key: K, value: V): V? {
+    return hashMap.putIfAbsent(key, value)
   }
 
-  override fun put(key: K, value: V): V? {
-    return if (value == null) {
-      hashMap.remove(key)
-    } else {
-      hashMap.put(key!!, value)
-    }
+  override fun computeIfAbsent(key: K, f: (K) -> V): V {
+    return hashMap.computeIfAbsent(key, f)
   }
 
-  override fun get(key: K): V? = hashMap[key]
-
-  override fun containsValue(value: V): Boolean = hashMap.containsValue(value)
-
-  override fun containsKey(key: K): Boolean = hashMap.containsKey(key)
+  override fun compute(key: K, f: (K, V?) -> V?): V? {
+    return hashMap.compute(key, f)
+  }
 }
