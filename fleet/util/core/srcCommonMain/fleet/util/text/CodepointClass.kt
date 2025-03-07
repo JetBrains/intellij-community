eca50a4b5f3a7d6ab2a62e8bc949bc8ff8aa852f
Sergei Sysoev <Sergey.Sysoev@jetbrains.com>
2025-03-07 02:02:58 +0100

    [fleet] binary search impl for IntArray (it's absent in common Kotlin)
    GitOrigin-RevId: 2a32205774a730127d91c7160c153eb46e011b3c

--- fleet/util/core/srcCommonMain/fleet/util/text/CodepointClass.kt
+++ fleet/util/core/srcCommonMain/fleet/util/text/CodepointClass.kt
@@ -66,6 +66,25 @@ private fun isLuCategory(codepoint: Int): Boolean {
   }
 }
 
+internal fun IntArray.binarySearch(element: Int): Int {
+  var l = 0
+  var r = size - 1
+
+  while (l <= r) {
+    val m = (l + r) / 2
+    val midElement = get(m)
+
+    if (midElement < element) {
+      l = m + 1
+    } else if (midElement > element) {
+      r = m - 1
+    } else {
+      return m
+    }
+  }
+  return -(l + 1)
+}
+
 // Code points are derived from:
 // https://unicode.org/Public/UNIDATA/PropList.txt
 private fun isOtherUppercase(codepoint: Int): Boolean {
