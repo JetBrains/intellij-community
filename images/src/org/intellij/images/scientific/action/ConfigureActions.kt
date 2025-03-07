3a1b0ddb5b57f8b8c2af45794e1d06ccba46d116
ekaterina.itsenko <ekaterina.itsenko@jetbrains.com>
2025-03-07 11:04:00 +0100

    [pycharm] PY-79603 Debugger: cleanup + set the key to true
    (cherry picked from commit c82e91cff86d01ea3a853010f3bd4979767853f8)
    
    GitOrigin-RevId: 1912c863be87f574ec157cc301e467399f4e4404

--- images/src/org/intellij/images/scientific/action/ConfigureActions.kt
+++ images/src/org/intellij/images/scientific/action/ConfigureActions.kt
@@ -4,7 +4,6 @@ package org.intellij.images.scientific.action
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import org.intellij.images.ImagesBundle
