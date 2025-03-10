e804b7c38c8cee5933ab483e9bab56bd6ebd535a
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-03-10 15:52:16 +0200

    IJPL-164422 Optimized soft wraps for cell grid editors
    When outputting large text into a terminal, it spends about 30-40%
    of the time calculating soft wraps. Most lines, however, don't have
    inlays, and because characters are aligned to cells, no font metrics
    is required to calculate soft wraps. We only need to know the number
    of columns and for every character wither it's a double-width one
    or not.
    
    Introduce a special soft wrap mode that automatically kicks in
    as soon as the grid mode is enabled and there are no inlays in the range.
    
    To avoid creating an iterator just to check whether there are inlays,
    extract inlay calculation from the constructor.
    
    To avoid performance issues caused by repetitive codePointAt() invocations,
    make a copy of the whole thing using toString() instead. This will create garbage,
    of course, but the performance benefit seems to be very well worth it.
    
    GitOrigin-RevId: d34e7522e32691faf70e5dd3e921dfb4856bbac7

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/view/CharacterGrid.kt
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/view/CharacterGrid.kt
@@ -1,7 +1,9 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.editor.impl.view
 
+import com.intellij.openapi.editor.codePointAt
 import com.intellij.openapi.editor.impl.EditorImpl
+import com.intellij.openapi.util.NlsSafe
 import org.jetbrains.annotations.ApiStatus
 import java.awt.Dimension
 
@@ -48,6 +50,69 @@ interface CharacterGrid {
    */
   fun codePointWidth(codePoint: Int): Float
 
+  /**
+   * Creates a cell iterator for the given document range.
+   *
+   * The iterator will be positioned on [startOffset], the column number will be initialized to zero.
+   */
+  fun iterator(startOffset: Int, endOffset: Int): CharacterGridIterator
+
+}
+
+/**
+ * An iterator over grid cells in some document range.
+ *
+ * At any given moment the iterator is positioned over some cell.
+ * The cell has a start offset ([cellStartOffset]), an end offset ([cellEndOffset]),
+ * a start column ([cellStartColumn]) and an end column ([cellEndColumn]).
+ * The start offset is the offset of the code point corresponding to the current character.
+ * The end offset will be the start offset plus one or two depending on whether
+ * the current code point corresponds to a BMP or an SMP character (a surrogate pair).
+ * The start column is the number of columns to the left of the character, zero for the first character in a line.
+ * The end column will be the start column plus one or two depending on whether the current character is a double-width one.
+ *
+ * A special case is when an iterator is positioned over a line break.
+ * Then the start offset will be the offset just past the end of the line,
+ * the end offset will be the start offset plus one or two (in the case of a CRLF line break).
+ * The start column will be the number of cells in the line,
+ * the end column will be zero.
+ */
+@ApiStatus.Internal
+interface CharacterGridIterator {
+  /**
+   * The iterator reached the end offset.
+   */
+  val isAtEnd: Boolean
+
+  /**
+   * The iterator is currently at a line break.
+   */
+  val isLineBreak: Boolean
+
+  /**
+   * The current cell start offset.
+   */
+  val cellStartOffset: Int
+
+  /**
+   * The current cell end offset.
+   */
+  val cellEndOffset: Int
+
+  /**
+   * The current cell start column.
+   */
+  val cellStartColumn: Int
+
+  /**
+   * The current cell end column.
+   */
+  val cellEndColumn: Int
+
+  /**
+   * Advances by one cell.
+   */
+  fun advance()
 }
 
 internal class CharacterGridImpl(
@@ -87,4 +152,87 @@ internal class CharacterGridImpl(
     return (if (doubleWidthCharacterStrategy.isDoubleWidth(codePoint)) 2.0f else 1.0f) * charWidth
   }
 
+  override fun iterator(startOffset: Int, endOffset: Int): CharacterGridIterator =
+    CharacterGridIteratorImpl(editor.document.immutableCharSequence, doubleWidthCharacterStrategy, startOffset, endOffset)
+
+}
+
+private class CharacterGridIteratorImpl(
+  text: @NlsSafe CharSequence,
+  private val doubleWidthCharacterStrategy: DoubleWidthCharacterStrategy,
+  private val startOffset: Int,
+  private val endOffset: Int,
+) : CharacterGridIterator {
+
+  private val substring: String = text.substring(startOffset, endOffset)
+  private val textLength: Int = text.length
+
+  private var codePoint: Int = 0
+
+  override var isAtEnd: Boolean = false
+    private set
+
+  override var isLineBreak: Boolean = false
+    private set
+
+  override var cellStartOffset: Int = startOffset
+    private set
+
+  override var cellEndOffset: Int = 0
+    private set
+
+  override var cellStartColumn: Int = 0
+    private set
+
+  override var cellEndColumn: Int = 0
+    private set
+
+  init {
+    setOffset(startOffset)
+    advanceColumn()
+  }
+
+  override fun advance() {
+    setOffset(cellEndOffset)
+    advanceColumn()
+  }
+
+  private fun setOffset(newOffset: Int) {
+    if (newOffset >= endOffset) {
+      codePoint = 0
+      isAtEnd = true
+      isLineBreak = true
+      cellStartOffset = endOffset
+      cellEndOffset = endOffset
+    }
+    else {
+      val char = substring.codePointAt(newOffset - startOffset)
+      codePoint = char
+      isAtEnd = false
+      isLineBreak = char == '\n'.code || char == '\r'.code
+      val bmp = Character.isBmpCodePoint(char)
+      cellStartOffset = newOffset
+      cellEndOffset = if (bmp) {
+        if (char == '\r'.code && newOffset + 1 < textLength && substring.codePointAt(newOffset - startOffset + 1) == '\n'.code) {
+          newOffset + 2 // the \r\n case
+        }
+        else {
+          newOffset + 1 // a regular BMP char
+        }
+      }
+      else {
+        newOffset + 2 // a surrogate pair
+      }
+    }
+  }
+
+  private fun advanceColumn() {
+    cellStartColumn = cellEndColumn
+    if (isLineBreak) {
+      cellEndColumn = 0
+    }
+    else {
+      cellEndColumn += if (doubleWidthCharacterStrategy.isDoubleWidth(codePoint)) 2 else 1
+    }
+  }
 }
