90ca354d13db833b031058824f2b73f3af3e37c6
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2025-03-10 18:00:41 +0100

    [maven] [IDEA-341835] add plugin jar into test data
    GitOrigin-RevId: a46d7d4d338f944d4fd8dce925c0f40e095326ed

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/plugins/compatibility/MavenPluginM2ELifecycles.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/plugins/compatibility/MavenPluginM2ELifecycles.kt
@@ -5,7 +5,7 @@ import org.jdom.Element
 import org.jetbrains.idea.maven.utils.MavenJDOMUtil
 
 
-class MavenPluginM2ELifecycles(entities: List<MavenPluginLifecycleEntity>) {
+class MavenPluginM2ELifecycles(val prefix: String, vararg entities: MavenPluginLifecycleEntity) {
   private val runOnInc = HashSet<String>()
   private val runOnConfig = HashSet<String>()
 
@@ -24,22 +24,27 @@ data class MavenPluginLifecycleEntity(val goals: List<String>, val runOnIncBuild
 
 
 class MavenLifecycleMetadataReader {
-  fun read(data: ByteArray?): MavenPluginM2ELifecycles {
-    if (data == null) return MavenPluginM2ELifecycles(mutableListOf<MavenPluginLifecycleEntity>())
-    val root = MavenJDOMUtil.read(data, null)
-    val pluginExecutions =
-      MavenJDOMUtil.findChildrenByPath(root, "pluginExecutions", "pluginExecution")
+  companion object {
+    @JvmStatic
+    fun read(prefix: String, data: ByteArray?): MavenPluginM2ELifecycles? {
+      if (data == null) return null
+      val root = MavenJDOMUtil.read(data, null)
+      val pluginExecutions =
+        MavenJDOMUtil.findChildrenByPath(root, "pluginExecutions", "pluginExecution")
 
-    val entities = pluginExecutions.map { parseEntities(it) }
-    return MavenPluginM2ELifecycles(entities)
+      val entities = pluginExecutions.map { parseEntities(it) }
+      if (entities.isEmpty()) return null
+      return MavenPluginM2ELifecycles(prefix, *entities.toTypedArray())
+    }
+
+    private fun parseEntities(el: Element): MavenPluginLifecycleEntity {
+      val goals = MavenJDOMUtil.findChildrenValuesByPath(el, "pluginExecutionFilter.goals", "goal");
+      var action = MavenJDOMUtil.findChildByPath(el, "action");
+      if (action == null || action.getChild("ignore") != null) return MavenPluginLifecycleEntity(goals, false, false);
+      var runOnInc = MavenJDOMUtil.findChildValueByPath(action, "execute.runOnIncremental").toBoolean()
+      var runOnConfig = MavenJDOMUtil.findChildValueByPath(action, "execute.runOnConfiguration").toBoolean()
+      return MavenPluginLifecycleEntity(goals, runOnInc, runOnConfig);
+    }
   }
 
-  private fun parseEntities(el: Element): MavenPluginLifecycleEntity {
-    val goals = MavenJDOMUtil.findChildrenValuesByPath(el, "pluginExecutionFilter.goals", "goal");
-    var action = MavenJDOMUtil.findChildByPath(el, "action");
-    if (action == null || action.getChild("ignore") != null) return MavenPluginLifecycleEntity(goals, false, false);
-    var runOnInc = MavenJDOMUtil.findChildValueByPath(action, "execute.runOnIncremental").toBoolean()
-    var runOnConfig = MavenJDOMUtil.findChildValueByPath(action, "execute.runOnConfiguration").toBoolean()
-    return MavenPluginLifecycleEntity(goals, runOnInc, runOnConfig);
-  }
 }
