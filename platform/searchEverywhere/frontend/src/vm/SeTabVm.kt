c413baa8d5ad9e1e9bc981c3b9df74a7cc028f23
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2025-03-10 17:11:41 +0100

    IJPL-158622 Fix review comments: use stateFlow for restarting search on Dumb mode change
    GitOrigin-RevId: 5f77b51d95a1c91923b43bd8efcb06faf67639ec

--- platform/searchEverywhere/frontend/src/vm/SeTabVm.kt
+++ platform/searchEverywhere/frontend/src/vm/SeTabVm.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.searchEverywhere.frontend.vm
 
-import com.intellij.openapi.application.EDT
 import com.intellij.openapi.options.ObservableOptionEditor
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
@@ -10,8 +9,10 @@ import com.intellij.platform.searchEverywhere.SeItemData
 import com.intellij.platform.searchEverywhere.SeParams
 import com.intellij.platform.searchEverywhere.frontend.SeTab
 import com.intellij.platform.searchEverywhere.frontend.resultsProcessing.SeResultsSorter
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import org.jetbrains.annotations.ApiStatus
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -28,50 +29,44 @@ class SeTabVm(
 
   private val shouldLoadMoreFlow: MutableStateFlow<Boolean> = MutableStateFlow(false)
   var shouldLoadMore: Boolean
-    get() = shouldLoadMoreFlow.value;
+    get() = shouldLoadMoreFlow.value
     set(value) { shouldLoadMoreFlow.value = value }
 
   private val _searchResults: MutableStateFlow<Flow<SeResultListEvent>> = MutableStateFlow(emptyFlow())
   private val isActiveFlow: MutableStateFlow<Boolean> = MutableStateFlow(false)
-  private val dumbModeExitFlow = flow<Any> {
+  private val dumbModeStateFlow = MutableStateFlow(DumbService.isDumb(project)).also {
     project.messageBus.connect(coroutineScope).subscribe(DumbService.DUMB_MODE, object : DumbService.DumbModeListener {
-      override fun exitDumbMode() {
-        coroutineScope.launch {
-          withContext(Dispatchers.EDT) {
-            emit("")
-          }
-        }
-      }
+      override fun enteredDumbMode() { it.value = true }
+      override fun exitDumbMode() { it.value = false }
     })
   }
+
   private val resultsSorter = SeResultsSorter(tab)
 
   init {
     coroutineScope.launch {
-      isActiveFlow.collectLatest { isActive ->
+      isActiveFlow.combine(dumbModeStateFlow) { isActive, _ ->
+        isActive
+      }.collectLatest { isActive ->
         if (!isActive) return@collectLatest
 
-        val startSearchFlow = merge(flowOf(""), dumbModeExitFlow)
+        combine(searchPattern, filterEditor?.resultFlow ?: flowOf(null)) { searchPattern, filterData ->
+          Pair(searchPattern, filterData ?: SeFilterState.Empty)
+        }.mapLatest { (searchPattern, filterData) ->
+          val params = SeParams(searchPattern, filterData)
 
-        startSearchFlow.collectLatest {
-          combine(searchPattern, filterEditor?.resultFlow ?: flowOf(null)) { searchPattern, filterData ->
-            Pair(searchPattern, filterData ?: SeFilterState.Empty)
-          }.mapLatest { (searchPattern, filterData) ->
-            val params = SeParams(searchPattern, filterData)
-
-            flow {
-              resultsSorter.getItems(params).map { item ->
-                shouldLoadMoreFlow.first { it }
-                item
-              }.onCompletion {
-                emit(SeResultListStopEvent)
-              }.collect {
-                emit(SeResultListUpdateEvent(it))
-              }
+          flow {
+            resultsSorter.getItems(params).map { item ->
+              shouldLoadMoreFlow.first { it }
+              item
+            }.onCompletion {
+              emit(SeResultListStopEvent)
+            }.collect {
+              emit(SeResultListUpdateEvent(it))
             }
-          }.collect {
-            _searchResults.value = it
           }
+        }.collect {
+          _searchResults.value = it
         }
       }
     }
