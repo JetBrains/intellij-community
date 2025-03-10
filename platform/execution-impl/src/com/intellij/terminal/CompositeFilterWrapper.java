c085bc7c453ec305c1a29047920595afe21cf625
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2025-03-10 16:26:52 +0100

    [terminal] IJPL-180734 get rid of caching `myConsoleFilterProvidersAdded`; it doesn't work with non-blocking read action
    GitOrigin-RevId: 3ca207e6ebf5e04e689a9689ab1a30de732bf5e5

--- platform/execution-impl/src/com/intellij/terminal/CompositeFilterWrapper.java
+++ platform/execution-impl/src/com/intellij/terminal/CompositeFilterWrapper.java
@@ -6,24 +6,23 @@ import com.intellij.execution.filters.ConsoleFilterProvider;
 import com.intellij.execution.filters.Filter;
 import com.intellij.execution.impl.ConsoleViewUtil;
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.concurrency.annotations.RequiresReadLock;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 final class CompositeFilterWrapper {
 
   private final Project myProject;
   private final TerminalExecutionConsole myConsole;
-  private final List<Filter> myFilters = new CopyOnWriteArrayList<>();
+  private final List<Filter> myCustomFilters = new CopyOnWriteArrayList<>();
   private volatile CompositeFilter myCompositeFilter;
-  private final AtomicBoolean myConsoleFilterProvidersAdded = new AtomicBoolean(false);
 
   CompositeFilterWrapper(@NotNull Project project, @Nullable TerminalExecutionConsole console, @NotNull Disposable disposable) {
     myProject = project;
@@ -34,32 +33,29 @@ final class CompositeFilterWrapper {
   }
 
   void addFilter(@NotNull Filter filter) {
-    myFilters.add(filter);
+    myCustomFilters.add(filter);
     myCompositeFilter = null;
   }
 
-  private @NotNull List<Filter> createCompositeFilters() {
+  @RequiresReadLock
+  private @NotNull List<Filter> computeConsoleFilters() {
     if (myProject.isDefault()) {
       return Collections.emptyList();
     }
-    return ReadAction.compute(() -> {
-      if (myProject.isDisposed()) {
-        return Collections.emptyList();
-      }
-      return ConsoleViewUtil.computeConsoleFilters(myProject, myConsole, GlobalSearchScope.allScope(myProject));
-    });
+    if (myProject.isDisposed()) {
+      return Collections.emptyList();
+    }
+    return ConsoleViewUtil.computeConsoleFilters(myProject, myConsole, GlobalSearchScope.allScope(myProject));
   }
 
-  @NotNull
-  CompositeFilter getCompositeFilter() {
+  @RequiresReadLock
+  @NotNull CompositeFilter getCompositeFilter() {
     CompositeFilter filter = myCompositeFilter;
     if (filter != null) {
       return filter;
     }
-    if (myConsoleFilterProvidersAdded.compareAndSet(false, true)) {
-      myFilters.addAll(createCompositeFilters());
-    }
-    filter = new CompositeFilter(myProject, myFilters);
+    List<Filter> predefinedFilters = computeConsoleFilters();
+    filter = new CompositeFilter(myProject, ContainerUtil.concat(myCustomFilters, predefinedFilters));
     filter.setForceUseAllFilters(true);
     myCompositeFilter = filter;
     return filter;
