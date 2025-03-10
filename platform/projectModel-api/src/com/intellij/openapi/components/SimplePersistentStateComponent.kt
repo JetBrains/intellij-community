04373d0eb0942bf77ec28e1128f4da0bec3aed4c
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2025-03-10 16:31:04 +0100

    [devkit] IJPL-180611 provide modern Kotlin templates for plugin building blocks
    GitOrigin-RevId: a419300dfb1bf571ed97463d7474531fb546a2de

--- platform/projectModel-api/src/com/intellij/openapi/components/SimplePersistentStateComponent.kt
+++ platform/projectModel-api/src/com/intellij/openapi/components/SimplePersistentStateComponent.kt
@@ -1,6 +1,9 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.components
 
+/**
+ * @see SerializablePersistentStateComponent for more idiomatic API.
+ */
 abstract class SimplePersistentStateComponent<T : BaseState>(initialState: T) : PersistentStateComponentWithModificationTracker<T> {
   @Volatile
   private var state: T = initialState
