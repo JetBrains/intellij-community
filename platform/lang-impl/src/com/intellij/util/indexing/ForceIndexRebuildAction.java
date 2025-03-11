f910d7d3c711cd34a08c3a38c50515ef5167bad5
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2025-03-11 18:48:22 +0100

    [indexing] Better confirmation for Reindex Everything action
    GitOrigin-RevId: b7178831bb147bd773eecd6168dd68dc25751bb7

--- platform/lang-impl/src/com/intellij/util/indexing/ForceIndexRebuildAction.java
+++ platform/lang-impl/src/com/intellij/util/indexing/ForceIndexRebuildAction.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.indexing;
 
+import com.intellij.idea.ActionsBundle;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
@@ -13,12 +14,13 @@ import org.jetbrains.annotations.NotNull;
 public final class ForceIndexRebuildAction extends DumbAwareAction {
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
-    //noinspection DialogTitleCapitalization
     boolean exitConfirmed = ApplicationManager.getApplication().isUnitTestMode() ||
-                            MessageDialogBuilder.okCancel(e.getPresentation().getText(), e.getPresentation().getText()).ask(e.getProject());
+                            MessageDialogBuilder.okCancel(e.getPresentation().getText(),
+                                                          ActionsBundle.message("action.ForceIndexRebuild.warning")).ask(e.getProject());
     if (!exitConfirmed) {
       return;
     }
+
     FileBasedIndexTumbler tumbler = new FileBasedIndexTumbler("Force index rebuild");
     tumbler.turnOff();
     try {
