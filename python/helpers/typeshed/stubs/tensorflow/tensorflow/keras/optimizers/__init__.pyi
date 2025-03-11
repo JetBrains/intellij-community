8d4198bd816c2d051d7d9eed8450b9a5e494134f
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2025-03-11 09:29:46 +0100

    PY-53599 Manually update tensorflow Typeshed. Enable `enable.tensorflow.pyi.stubs` by default
    New Tensorflow Typeshed has updated stubs for the keras module, as well as for other Tensorflow modules. So PY-65966 should not be reproducible anymore
    
    
    (cherry picked from commit 929e1da40c82bf6dd26efd227c0421087df3b75c)
    
    IJ-MR-157295
    
    GitOrigin-RevId: 4bf2396c3cd62aea0339db51fbec058f1d765a05

--- python/helpers/typeshed/stubs/tensorflow/tensorflow/keras/optimizers/__init__.pyi
+++ python/helpers/typeshed/stubs/tensorflow/tensorflow/keras/optimizers/__init__.pyi
@@ -2,4 +2,6 @@ from _typeshed import Incomplete
 
 from tensorflow.keras.optimizers import legacy as legacy, schedules as schedules
 
+Optimizer = Incomplete
+
 def __getattr__(name: str) -> Incomplete: ...
