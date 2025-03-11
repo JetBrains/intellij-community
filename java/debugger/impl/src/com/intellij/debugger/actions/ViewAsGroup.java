5e2d3ca4b98f29f06591e03e3d88ca9657c697e6
Egor Ushakov <egor.ushakov@jetbrains.com>
2025-03-11 16:03:42 +0100

    IDEA-368308 Debug: View As | Hex/Binary/Primitive on variable does not close popup
    (cherry picked from commit b1e1cdbb0977b25a411d5f2e5e8d089493396631)
    
    IJ-CR-157474
    
    GitOrigin-RevId: b28d7c956a6a1f7878b2215dbacf035f9904eb90

--- java/debugger/impl/src/com/intellij/debugger/actions/ViewAsGroup.java
+++ java/debugger/impl/src/com/intellij/debugger/actions/ViewAsGroup.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.debugger.actions;
 
 import com.intellij.debugger.engine.DebugProcessImpl;
@@ -47,6 +47,13 @@ public class ViewAsGroup extends ActionGroup implements DumbAware {
     RendererAction(NodeRenderer nodeRenderer) {
       super(nodeRenderer.getName());
       myNodeRenderer = nodeRenderer;
+    }
+
+    @Override
+    public @NotNull Presentation createTemplatePresentation() {
+      Presentation presentation = super.createTemplatePresentation();
+      presentation.setKeepPopupOnPerform(KeepPopupOnPerform.Never);
+      return presentation;
     }
 
     @Override
