e4af43f3d7d55a501e1cf96f574dffcc005b7266
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-03-12 10:10:09 +0100

    IJPL-162853 introduce MutableBitSet
    GitOrigin-RevId: fce8e9a51e3013b8a895cf541b8b0a50fb3285e6

--- platform/syntax/syntax-api/test/com/intellij/platform/syntax/impl/builder/BitSetTest.kt
+++ platform/syntax/syntax-api/test/com/intellij/platform/syntax/impl/builder/BitSetTest.kt
@@ -1,12 +1,13 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.syntax.impl.builder
 
+import com.intellij.platform.syntax.impl.util.MutableBitSet
 import org.junit.jupiter.api.Test
 
 internal class BitSetTest {
   @Test
   fun `test empty`() {
-    val set = MarkerOptionalData()
+    val set = MutableBitSet()
     assert(!set.contains(0))
     assert(!set.contains(1))
     assert(!set.contains(2))
@@ -16,7 +17,7 @@ internal class BitSetTest {
   fun `test add and remove`() {
     for (i in 0..1000) {
       try {
-        val set = MarkerOptionalData()
+        val set = MutableBitSet()
         set.add(i)
         assert(set.contains(i))
         assert(!set.contains(i + 1))
@@ -35,7 +36,7 @@ internal class BitSetTest {
 
   @Test
   fun `test add many`() {
-    val set = MarkerOptionalData()
+    val set = MutableBitSet()
     for (i in 0..1000) {
       set.add(i)
     }
