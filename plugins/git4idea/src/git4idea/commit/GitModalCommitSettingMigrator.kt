c638b8abbcda99a434cdab9583759640311e36dd
Ivan Semenov <ivan.semenov@jetbrains.com>
2025-03-12 23:10:27 +0100

    [git] ensure correct threading for modal commit settings migration
    Without this commit, installing 251 onto 243 with modal commit enabled leads to exceptions and broken UI.
    
    This has to be fixed on listener level, but for now we just switch the activity to EDT to simply fix the errors.
    
    
    (cherry picked from commit 01e1abc136980cef5d278e52c88fc77bf965d0e7)
    
    IJ-CR-157595
    
    GitOrigin-RevId: 04c74d74d9a75aa19dcc18d149f7b0b4ebac24a0

--- plugins/git4idea/src/git4idea/commit/GitModalCommitSettingMigrator.kt
+++ plugins/git4idea/src/git4idea/commit/GitModalCommitSettingMigrator.kt
@@ -2,17 +2,24 @@
 package git4idea.commit
 
 import com.intellij.ide.util.runOnceForApp
+import com.intellij.openapi.application.EDT
 import com.intellij.openapi.options.advanced.AdvancedSettings
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.vcs.VcsApplicationSettings
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 internal class GitModalCommitSettingMigrator : ProjectActivity {
   override suspend fun execute(project: Project) {
     runOnceForApp("migrate-modal-commit-setting") {
-      if (!VcsApplicationSettings.getInstance().COMMIT_FROM_LOCAL_CHANGES) {
-        AdvancedSettings.setBoolean("vcs.commit.tool.window", false)
-        AdvancedSettings.setBoolean("vcs.non.modal.commit.toggle.ui", true)
+      // need EDT because changing the settings fires listeners that must be executed on EDT
+      // TODO: fix listeners
+      withContext(Dispatchers.EDT) {
+        if (!VcsApplicationSettings.getInstance().COMMIT_FROM_LOCAL_CHANGES) {
+          AdvancedSettings.setBoolean("vcs.commit.tool.window", false)
+          AdvancedSettings.setBoolean("vcs.non.modal.commit.toggle.ui", true)
+        }
       }
     }
   }
