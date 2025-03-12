c53584709c2ba4effab95cfba7910a81e346b7e8
Nikita Pavlenko <nikita.pavlenko@jetbrains.com>
2025-03-12 18:53:00 +0100

    [trial] IJPL-178926: "Enable all"/"Disable all" linkButton will not be shown for groups without plugins with proper icons. Also, all group actions will not affect the Ultimate plugins in Core License.
    - If in Plugins list in group we have only Ultimate plugins, but current license is not Ultimate, "Enable all"/"Disable all" group actions will be hidden.
    
    - All group actions like  "Enable all"/"Disable all" in plugin groups and also "Disable All Downloaded Plugins" /  "Enable All Downloaded Plugins" in plugins "gear" popup, will not affect the state of Ultimate plugins while we do not have proper license.
    
    GitOrigin-RevId: 4bc8a1acdc45f7906f22c8553e2751867378b0cc

--- platform/platform-impl/src/com/intellij/ide/plugins/PluginManagerConfigurable.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/PluginManagerConfigurable.java
@@ -81,7 +81,9 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+import static com.intellij.ide.plugins.PluginManagerCoreKt.pluginRequiresUltimatePluginButItsDisabled;
 import static com.intellij.ide.plugins.newui.PluginsViewCustomizerKt.getPluginsViewCustomizer;
+import static com.intellij.util.containers.ContainerUtil.exists;
 
 @ApiStatus.Internal
 public final class PluginManagerConfigurable
@@ -1381,6 +1383,7 @@ public final class PluginManagerConfigurable
     addGroup(groups, groupName, PluginsGroupType.SUGGESTED, "", plugins, group -> false);
   }
 
+
   private final class ComparablePluginsGroup extends PluginsGroup
     implements Comparable<ComparablePluginsGroup> {
 
@@ -1396,7 +1399,7 @@ public final class PluginManagerConfigurable
       rightAction = new LinkLabelButton<>("",
                                           null,
                                           (__, ___) -> setEnabledState());
-
+      rightAction.setVisible(hasPluginsForEnableDisable(descriptors));
       titleWithEnabled(myPluginModel);
     }
 
@@ -1413,12 +1416,32 @@ public final class PluginManagerConfigurable
     }
 
     private void setEnabledState() {
-      if (myIsEnable) {
-        myPluginModel.enable(descriptors);
-      }
-      else {
-        myPluginModel.disable(descriptors);
-      }
+      setState(myPluginModel, descriptors, myIsEnable);
+    }
+
+    /** Returns true, if in descriptors list not only Ultimate plugins while we are on Core license.
+     * (Any plugin exists, for which we can change the enabled / disable state) */
+    private static boolean hasPluginsForEnableDisable(List<? extends IdeaPluginDescriptor> descriptors){
+      var idMap = PluginManagerCore.INSTANCE.buildPluginIdMap();
+      return exists(descriptors, descriptor -> !pluginRequiresUltimatePluginButItsDisabled(descriptor.getPluginId(), idMap));
+    }
+  }
+
+  /** Modifies the state of the plugin list, excluding Ultimate plugins when the Ultimate license is not active. */
+  private static void setState(MyPluginModel pluginModel, Collection<IdeaPluginDescriptor> descriptors, boolean isEnable) {
+    if (descriptors.isEmpty()) return;
+
+    var idMap = PluginManagerCore.INSTANCE.buildPluginIdMap();
+    var suitableDescriptors = descriptors.stream().
+      filter(descriptor -> !pluginRequiresUltimatePluginButItsDisabled(descriptor.getPluginId(), idMap)).toList();
+
+    if (suitableDescriptors.isEmpty()) return;
+
+    if (isEnable) {
+      pluginModel.enable(suitableDescriptors);
+    }
+    else {
+      pluginModel.disable(suitableDescriptors);
     }
   }
 
@@ -1886,14 +1909,7 @@ public final class PluginManagerConfigurable
         }
       }
 
-      if (!descriptors.isEmpty()) {
-        if (myEnable) {
-          myPluginModel.enable(descriptors);
-        }
-        else {
-          myPluginModel.disable(descriptors);
-        }
-      }
+      setState(myPluginModel, descriptors, myEnable);
     }
   }
 
