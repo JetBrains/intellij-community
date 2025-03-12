f2703c1d54de266aed7444733871df109ee610ee
Ahmet Kerim Senol <akerim@google.com>
2025-03-12 14:06:52 +0000

    Always use compiler version as the max value for api level when known
    For figuring out the API level, we currently use the minimum of compiler version and the stdlib version module's library dependencies. This is an expensive operation and it can be avoided when we know the answer beforehand. My understanding is that we know the answer when compiler version is set explicitly (this is at least true for Gradle import case). In all other cases we can still use the old logic.
    
    closes https://github.com/JetBrains/intellij-community/pull/2956
    
    relates to KTIJ-33413
    
    Merge-request: IJ-MR-157187
    Merged-by: Aleksei Cherepanov <aleksei.cherepanov@jetbrains.com>
    
    GitOrigin-RevId: 85d6908b7886e2174479273d91296d870aa77433

--- plugins/kotlin/base/facet/src/org/jetbrains/kotlin/idea/facet/KotlinFacetUtils.kt
+++ plugins/kotlin/base/facet/src/org/jetbrains/kotlin/idea/facet/KotlinFacetUtils.kt
@@ -28,7 +28,6 @@ import org.jetbrains.kotlin.platform.IdePlatformKind
 import org.jetbrains.kotlin.platform.TargetPlatform
 import org.jetbrains.kotlin.platform.idePlatformKind
 import org.jetbrains.kotlin.platform.impl.JvmIdePlatformKind
-import org.jetbrains.kotlin.platform.impl.isKotlinNative
 import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 import org.jetbrains.kotlin.psi.NotNullableUserDataProperty
 import kotlin.reflect.KProperty1
@@ -84,29 +83,22 @@ fun IKotlinFacetSettings.initializeIfNeeded(
 
     if (shouldInferAPILevel) {
         apiLevel = when {
-            useProjectSettings -> {
-                LanguageVersion.fromVersionString(commonArguments.apiVersion) ?: languageLevel
-            }
-            /*
-            The below 'getLibraryVersion' call tries to figure out apiVersion by inspecting the stdlib available in dependencies.
-            This is not supported for K/N (07.2023), we therefore just fallback to the compiler version
-             */
-            targetPlatform?.idePlatformKind?.isKotlinNative == true && compilerVersion != null -> {
-                languageLevel?.coerceAtMostVersion(compilerVersion)
-            }
-
-            else -> run apiLevel@{
-                val maximumValue = getKotlinStdlibVersionOrNull(
-                    module,
-                    rootModel,
-                    this.targetPlatform?.idePlatformKind,
-                    coerceRuntimeLibraryVersionToReleased = compilerVersion == null
-                ) ?: compilerVersion ?: getDefaultVersion()
-                languageLevel?.coerceAtMostVersion(maximumValue)
-            }
+            useProjectSettings -> LanguageVersion.fromVersionString(commonArguments.apiVersion) ?: languageLevel
+            else -> languageLevel?.coerceAtMostVersion(getMaximumValueForApiLevel(module, rootModel, compilerVersion))
         }
     }
 }
+
+private fun IKotlinFacetSettings.getMaximumValueForApiLevel(
+    module: Module,
+    rootModel: ModuleRootModel?,
+    compilerVersion: IdeKotlinVersion?
+) = compilerVersion ?: getKotlinStdlibVersionOrNull(
+    module,
+    rootModel,
+    targetPlatform?.idePlatformKind,
+    coerceRuntimeLibraryVersionToReleased = true
+) ?: getDefaultVersion(explicitVersion = null, coerceRuntimeLibraryVersionToReleased = true)
 
 private fun getDefaultTargetPlatform(module: Module, rootModel: ModuleRootModel?): TargetPlatform {
     val platformKind = IdePlatformKind.ALL_KINDS.firstOrNull {
