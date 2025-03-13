d69af15eb655bc807d3c35e92a51056fd7b55858
Matthew Gharrity <gharrma@google.com>
2025-03-13 12:57:38 -0700

    Fix Android Studio version for N.1 Canary 1
    Fixes: 403314521
    Test: added a build assertion
    Change-Id: I7c0c7abeec7be02ee41a5299e0139197630a2f4e

--- build/src/AndroidStudioBuildTarget.kt
+++ build/src/AndroidStudioBuildTarget.kt
@@ -1,5 +1,6 @@
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.intellij.build.AndroidStudioProperties
+import org.jetbrains.intellij.build.BuildContext
 import org.jetbrains.intellij.build.IdeaProjectLoaderUtil
 import org.jetbrains.intellij.build.createBuildTasks
 import org.jetbrains.intellij.build.impl.BuildContextImpl
@@ -12,8 +13,21 @@ object AndroidStudioBuildTarget {
       val home = IdeaProjectLoaderUtil.guessCommunityHome(javaClass)
       val properties = AndroidStudioProperties(home.communityRoot)
       val buildContext = BuildContextImpl.createContext(home.communityRoot, properties)
+      validateProductVersion(buildContext)
       val tasks = createBuildTasks(buildContext)
       tasks.buildDistributions()
+    }
+  }
+
+  /**
+   * Checks that the product version in AndroidStudioApplicationInfo.xml is consistent with the build number in build.txt
+   * For example, product version  "2025.1.X.X" is consistent with build number "251.XXX" because 251 is shorthand for 2025.1.
+   */
+  private fun validateProductVersion(buildContext: BuildContext) {
+    val appInfo = buildContext.applicationInfo
+    val buildNum = buildContext.buildNumber
+    check(buildNum.substringBefore('.') == "${appInfo.majorVersion.drop(2)}${appInfo.minorVersion}") {
+      "The version specified in AndroidStudioApplicationInfo.xml is inconsistent with the build number in build.txt"
     }
   }
 }
