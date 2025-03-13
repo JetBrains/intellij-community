5bb3e7fd0298d87df30ce155a4e88353e40a8927
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-03-13 09:45:22 +0100

    [threading] Fix typo
    GitOrigin-RevId: 52b171ca1298760b449634674a876959326bd56c

--- platform/core-api/src/com/intellij/openapi/application/coroutines.kt
+++ platform/core-api/src/com/intellij/openapi/application/coroutines.kt
@@ -242,7 +242,7 @@ suspend fun <T> edtWriteAction(action: () -> T): T {
 /**
  * Runs [action] under [write lock][com.intellij.openapi.application.Application.runWriteAction].
  *
- * This function is deprecated in favor of [edtWriteAction]. This deprecation is needed to free the name [edtWriteAction], as we are
+ * This function is deprecated in favor of [edtWriteAction]. This deprecation is needed to free the name [writeAction], as we are
  * planning to schedule all write actions to background by default.
  *
  * NB This function is an API stub. The implementation will change once running write actions would be allowed on other threads. This
