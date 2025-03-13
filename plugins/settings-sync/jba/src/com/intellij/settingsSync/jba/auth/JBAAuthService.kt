9515d70c2f0f3d0cc4be60ceba8d16aca4fdc10b
Sergey Pak <sergey.pak@jetbrains.com>
2025-03-13 22:15:34 +0100

    [settingsSync] migrate from runWithModalProgressBlocking to self-made progress dialog
    GitOrigin-RevId: 7dd8dcb58e2d88b4531e03ce44f1ae6fcca0986c

--- plugins/settings-sync/jba/src/com/intellij/settingsSync/jba/auth/JBAAuthService.kt
+++ plugins/settings-sync/jba/src/com/intellij/settingsSync/jba/auth/JBAAuthService.kt
@@ -1,24 +1,46 @@
 package com.intellij.settingsSync.jba.auth
 
+import com.intellij.CommonBundle
 import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.ActionUiKind
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.Presentation
+import com.intellij.openapi.actionSystem.ex.ActionUtil
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.asContextElement
 import com.intellij.openapi.application.ex.ApplicationManagerEx
 import com.intellij.openapi.diagnostic.logger
-import com.intellij.platform.ide.progress.ModalTaskOwner
-import com.intellij.platform.ide.progress.TaskCancellation
-import com.intellij.platform.ide.progress.withModalProgress
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.ui.DialogWrapper.OK_EXIT_CODE
+import com.intellij.openapi.ui.ExitActionType
+import com.intellij.openapi.util.SystemInfoRt
 import com.intellij.settingsSync.core.SettingsSyncEvents
 import com.intellij.settingsSync.core.auth.SettingsSyncAuthService
 import com.intellij.settingsSync.core.communicator.SettingsSyncUserData
 import com.intellij.settingsSync.jba.SettingsSyncJbaBundle
 import com.intellij.settingsSync.jba.SettingsSyncPromotion
 import com.intellij.ui.JBAccountInfoService
+import com.intellij.ui.dsl.builder.AlignY
+import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.gridLayout.UnscaledGaps
+import com.intellij.ui.scale.JBUIScale.scale
+import com.intellij.util.ui.UIUtil
 import kotlinx.coroutines.*
 import java.awt.Component
+import java.awt.Dimension
 import java.util.concurrent.CancellationException
+import javax.swing.Action
+import javax.swing.JComponent
+import javax.swing.JProgressBar
+import javax.swing.JRootPane
+import javax.swing.event.HyperlinkEvent
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
-internal class JBAAuthService : SettingsSyncAuthService {
+internal class JBAAuthService() : SettingsSyncAuthService {
 
   companion object {
     private val LOG = logger<JBAAuthService>()
@@ -76,64 +98,83 @@ internal class JBAAuthService : SettingsSyncAuthService {
   override val icon = AllIcons.Ultimate.IdeaUltimatePromo
 
   override suspend fun login(parentComponent: Component?): SettingsSyncUserData? {
-    val modalTaskOwner = if (parentComponent != null)
-      ModalTaskOwner.component(parentComponent)
-    else
-      ModalTaskOwner.guess()
-    return withModalProgress(modalTaskOwner, SettingsSyncJbaBundle.message("login.manual.helper.text"), TaskCancellation.cancellable()) {
-      val accountInfoService = getAccountInfoService()
-      val loginMetadata = hashMapOf(
-        "from.settings.sync" to "true"
-      )
-      if (SettingsSyncPromotion.promotionShownThisSession) {
-        loginMetadata["from.settings.sync.promotion"] = "true"
-      }
-      if (accountInfoService == null) {
-        LOG.error("JBA auth service is not available!")
-        return@withModalProgress null
-      }
-      if (isTokenValid(accountInfoService.idToken)) {
-        return@withModalProgress fromJBAData(accountInfoService.userData)
-      }
-      suspendCancellableCoroutine<SettingsSyncUserData?> { cont ->
-        try {
-          cont.invokeOnCancellation {
-            cont.resume(null)
-          }
-          accountInfoService
-            .startLoginSession(JBAccountInfoService.LoginMode.AUTO, null, loginMetadata)
-            .onCompleted()
-            .exceptionally { exc ->
-            if (exc is CancellationException) {
-              LOG.warn("Login cancelled")
-            }
-            else {
-              LOG.warn("Login failed", exc)
-            }
-            cont.resume(null)
-            null
-          }.thenApply { loginResult ->
-            val result: SettingsSyncUserData? = when (loginResult) {
-              is JBAccountInfoService.LoginResult.LoginSuccessful -> {
-                fromJBAData(loginResult.jbaUser)
-              }
-              is JBAccountInfoService.LoginResult.LoginFailed -> {
-                LOG.warn("Login failed: ${loginResult.errorMessage}")
-                null
-              }
-              else -> {
-                LOG.warn("Unknown login result: $loginResult")
-                null
-              }
-            }
-            cont.resume(result)
+    val accountInfoService = getAccountInfoService()
+    val loginMetadata = hashMapOf(
+      "from.settings.sync" to "true"
+    )
+    if (SettingsSyncPromotion.promotionShownThisSession) {
+      loginMetadata["from.settings.sync.promotion"] = "true"
+    }
+    if (accountInfoService == null) {
+      LOG.error("JBA auth service is not available!")
+      return null
+    }
+    if (isTokenValid(accountInfoService.idToken)) {
+      return fromJBAData(accountInfoService.userData)
+    }
+
+
+    return coroutineScope {
+      val job: Job? = coroutineContext[Job]
+      var dialog: LogInProgressDialog? = null
+      val executionModalityState: ModalityState = withContext(Dispatchers.EDT) { ModalityState.current() }
+      try {
+        launch {
+          withContext(Dispatchers.EDT + executionModalityState.asContextElement()) {
+            dialog = LogInProgressDialog(parentComponent as JComponent)
+            dialog.show()
           }
         }
-        catch (e: Throwable) {
-          LOG.error(e)
-          cont.resumeWithException(e)
+        val retval = suspendCancellableCoroutine<SettingsSyncUserData?> { cont ->
+          try {
+            if (job != null)
+              dialog?.addJobToCancel(job)
+            cont.invokeOnCancellation {
+              cont.resume(null)
+            }
+            accountInfoService
+              .startLoginSession(JBAccountInfoService.LoginMode.AUTO, null, loginMetadata)
+              .onCompleted()
+              .exceptionally { exc ->
+                if (exc is CancellationException) {
+                  LOG.warn("Login cancelled")
+                }
+                else {
+                  LOG.warn("Login failed", exc)
+                }
+                cont.resume(null)
+                null
+              }.thenApply { loginResult ->
+                val result: SettingsSyncUserData? = when (loginResult) {
+                  is JBAccountInfoService.LoginResult.LoginSuccessful -> {
+                    fromJBAData(loginResult.jbaUser)
+                  }
+                  is JBAccountInfoService.LoginResult.LoginFailed -> {
+                    LOG.warn("Login failed: ${loginResult.errorMessage}")
+                    null
+                  }
+                  else -> {
+                    LOG.warn("Unknown login result: $loginResult")
+                    null
+                  }
+                }
+                cont.resume(result)
+              }
+          }
+          catch (e: Throwable) {
+            LOG.error(e)
+            cont.resumeWithException(e)
+          }
+        }
+        return@coroutineScope retval
+      } catch (ex: Throwable) {
+        LOG.error("An exception occurred while logging in to JBA", ex)
+      } finally {
+        withContext(Dispatchers.EDT + executionModalityState.asContextElement() ) {
+          dialog?.close(OK_EXIT_CODE, ExitActionType.OK);
         }
       }
+      return@coroutineScope null
     }
   }
 
@@ -151,5 +192,58 @@ internal class JBAAuthService : SettingsSyncAuthService {
       return DummyJBAccountInfoService
     }
     return JBAccountInfoService.getInstance()
+  }
+}
+
+private class LogInProgressDialog(parent: JComponent) : DialogWrapper(parent, false) {
+  private var job2Cancel: Job? = null
+
+  init {
+    title = SettingsSyncJbaBundle.message("login.title")
+    init()
+    rootPane.windowDecorationStyle = JRootPane.FRAME
+  }
+
+  fun addJobToCancel(job: Job) {
+    job2Cancel = job
+  }
+
+  override fun isProgressDialog() = true
+
+  override fun createCenterPanel(): JComponent {
+    val panel = panel {
+      row {
+        val progressBar = JProgressBar()
+        cell(progressBar).resizableColumn().applyToComponent {
+          isIndeterminate = true
+        }.customize(UnscaledGaps(0, 0, 0, 10))
+        progressBar.preferredSize = Dimension(if (SystemInfoRt.isMac) 350 else scale(450), 4)
+        button(CommonBundle.getCancelButtonText()) {
+          job2Cancel?.cancel()
+          this@LogInProgressDialog.doCancelAction()
+        }
+      }
+      row {
+        text(SettingsSyncJbaBundle.message("login.troubles.message")).applyToComponent {
+          addHyperlinkListener {
+            if (it.eventType == HyperlinkEvent.EventType.ACTIVATED) {
+              job2Cancel?.cancel()
+              val action = ActionUtil.getAction("Register")!!
+              ActionUtil.performActionDumbAwareWithCallbacks(action, AnActionEvent(
+                DataContext.EMPTY_CONTEXT, Presentation(), "", ActionUiKind.NONE, null, 0, ActionManager.getInstance()
+              ))
+            }
+          }
+          if (SystemInfoRt.isMac) {
+            UIUtil.applyStyle(UIUtil.ComponentStyle.SMALL, this)
+          }
+        }.align(AlignY.TOP).customize(UnscaledGaps(0, 0, 0, 10))
+      }
+    }
+    return panel
+  }
+
+  override fun createActions(): Array<out Action?> {
+    return emptyArray()
   }
 }
