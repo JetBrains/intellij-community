9257bc337eabe512ec0ad7d5277c8e953ebc9fb1
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2025-03-14 12:22:24 +0100

    git: confirmation for "Revert Resolved Conflicts" action
    Follow-up: 8fd4eb4733ec394304086f2e8f5f270c414e3f9a
    
    GitOrigin-RevId: 27a324b32e49f53c38fa1bc7e65aaec6b72126db

--- plugins/git4idea/src/git4idea/actions/GitRevertResolvedAction.kt
+++ plugins/git4idea/src/git4idea/actions/GitRevertResolvedAction.kt
@@ -1,12 +1,14 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.actions
 
+import com.intellij.CommonBundle
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.MessageDialogBuilder
 import com.intellij.openapi.vcs.VcsDataKeys
 import com.intellij.openapi.vcs.changes.VcsDirtyScopeManager
 import com.intellij.openapi.vcs.merge.MergeConflictManager
@@ -39,6 +41,18 @@ internal class GitRevertResolvedAction : DumbAwareAction() {
   companion object {
     @JvmStatic
     fun rollbackFiles(project: Project, files: List<VirtualFile>) {
+      val message =
+        GitBundle.message("action.Git.RevertResolved.confirmation",
+                          files.size, files.joinToString(separator = "\n") { it.presentableUrl })
+      val yes = CommonBundle.getYesButtonText()
+      val no = CommonBundle.getNoButtonText()
+      val result = MessageDialogBuilder.Message(GitBundle.message("action.Git.RevertResolved.confirmation.title"), message)
+        .buttons(yes, no)
+        .defaultButton(no)
+        .focusedButton(no)
+        .show(project)
+      if (result != yes) return
+
       object : Task.Backgroundable(project, GitBundle.message("action.Git.RevertResolved.progress"), true) {
         override fun run(indicator: ProgressIndicator) {
           GitMergeUtil.revertMergedFiles(project, files)
