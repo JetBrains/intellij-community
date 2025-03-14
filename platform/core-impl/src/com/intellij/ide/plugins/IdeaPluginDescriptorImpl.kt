8a8b52afc2bee63230ad46874c2742c933b6a2ff
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-03-14 18:36:32 +0100

    [plugins] IJPL-181065 IdeaPluginDescriptorImpl: extract UntilBuildDeprecation
    GitOrigin-RevId: 32ac3115eff01ec878a97aecaadae6cb4fade7a5

--- platform/core-impl/src/com/intellij/ide/plugins/IdeaPluginDescriptorImpl.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/IdeaPluginDescriptorImpl.kt
@@ -12,7 +12,6 @@ import com.intellij.openapi.extensions.ExtensionDescriptor
 import com.intellij.openapi.extensions.LoadingOrder
 import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.extensions.impl.ExtensionPointImpl
-import com.intellij.openapi.util.BuildNumber
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.platform.plugins.parser.impl.PluginDescriptorBuilder
 import com.intellij.platform.plugins.parser.impl.RawPluginDescriptor
@@ -745,30 +744,3 @@ private val CE_PLUGIN_CARDS: Map<String, PluginCardInfo> = mapOf(
     "Provides an easy way to install AI assistant to your IDE"
   )
 )
-
-private object UntilBuildDeprecation {
-  private const val MINIMAL_API_VERSION = 251
-  private val forceHonorUntilBuild = System.getProperty("idea.plugins.honor.until.build", "false").toBoolean()
-
-  fun nullizeIfTargets243OrLater(untilBuild: String?, diagnosticId: String?): String? {
-    if (forceHonorUntilBuild || untilBuild == null) {
-      return untilBuild
-    }
-    try {
-      val untilBuildNumber = BuildNumber.fromStringOrNull(untilBuild)
-      if (untilBuildNumber != null && untilBuildNumber.baselineVersion >= MINIMAL_API_VERSION) {
-        if (untilBuildNumber < PluginManagerCore.buildNumber) {
-          // log only if it would fail the compatibility check without the deprecation in place
-          LOG.info("Plugin ${diagnosticId ?: "<no name>"} has until-build set to $untilBuild. " +
-                   "Until-build _from plugin configuration file (plugin.xml)_ for plugins targeting ${MINIMAL_API_VERSION}+ is ignored. " +
-                   "Effective until-build value can be set via the Marketplace.")
-        }
-        return null
-      }
-    }
-    catch (e: Throwable) {
-      LOG.warn("failed to parse until-build number", e)
-    }
-    return untilBuild
-  }
-}
