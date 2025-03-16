d94f3457dc233a812cda7d5862e34d7f9819c417
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2025-03-16 10:09:00 +0100

    PY-76782: Remove special notification for Community products: they now support Jupyter notebooks to some extent.
    Code was also significantly refactored
    
    GitOrigin-RevId: 2bda66fa7ba5fa33096e9536b23b48b20f05d1a3

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/suggestions/JupyterNotebookSuggestionProvider.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/suggestions/JupyterNotebookSuggestionProvider.kt
@@ -2,93 +2,135 @@
 package org.jetbrains.kotlin.idea.suggestions
 
 import com.google.gson.JsonParser
+import com.intellij.ide.IdeBundle
 import com.intellij.ide.plugins.PluginManager
+import com.intellij.ide.plugins.PluginManagerConfigurable
 import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.extensions.PluginId
+import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.PluginAdvertiserService.Companion.isCommunityIde
+import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.FUSEventSource
 import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.PluginSuggestion
 import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.PluginSuggestionProvider
-import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.tryUltimateIsDisabled
+import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.installAndEnable
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.readText
+import com.intellij.ui.EditorNotificationPanel
+import com.intellij.ui.EditorNotifications
+
+
+private val JupyterAndNotebookFilesEnablementChecker: PluginEnablementChecker = PluginEnablementChecker(
+    notebookLanguages = null,
+    dismissedKey = "jupyter.suggestion.dismissed",
+    suggestionText = IdeBundle.message("plugins.advertiser.plugins.found", "*.ipynb"),
+    suggestionActionText = IdeBundle.message("plugins.advertiser.action.enable.plugins"),
+    requiresIdeRestart = true,
+    pluginIds = listOf(
+        "intellij.jupyter",
+        "com.intellij.notebooks.core",
+    )
+)
+
+private val KotlinNotebookEnablementChecker: PluginEnablementChecker = PluginEnablementChecker(
+    notebookLanguages = setOf("kotlin"),
+    dismissedKey = "notebook.kotlin.suggestion.dismissed",
+    suggestionText = IdeBundle.message("plugins.advertiser.plugins.found", "Kotlin Notebook (*.ipynb)"),
+    suggestionActionText = IdeBundle.message("plugins.advertiser.action.install.plugin.name", "Kotlin Notebook"),
+    requiresIdeRestart = true,
+    pluginIds = listOf(
+        "org.jetbrains.plugins.kotlin.jupyter",
+        "org.jetbrains.kotlin",
+    ) + JupyterAndNotebookFilesEnablementChecker.pluginIds,
+)
+
+private val enablementCheckers = listOf(
+    KotlinNotebookEnablementChecker,
+    JupyterAndNotebookFilesEnablementChecker,
+)
 
 /**
  * Suggestion provider for the Notebooks-related plugins (Kotlin Notebook or Jupyter + Notebook Files).
  */
 internal class JupyterNotebookSuggestionProvider : PluginSuggestionProvider {
 
-    companion object {
-        const val KOTLIN_NOTEBOOKS_PRIMARY_PLUGIN_ID = "org.jetbrains.plugins.kotlin.jupyter"
-        const val JUPYTER_PLUGIN_ID = "intellij.jupyter"
-        const val NOTEBOOK_FILES_PLUGIN_ID = "com.intellij.notebooks.core"
-
-        const val KOTLIN_NOTEBOOKS_PLUGIN_NAME: String = "Kotlin Notebook"
-        const val KOTLIN_NOTEBOOKS_PLUGIN_FILES: String = "Kotlin Notebook (*.ipynb)"
-        const val KOTLIN_NOTEBOOKS_PLUGIN_SUGGESTION_DISMISSED_KEY: String = "notebook.kotlin.suggestion.dismissed"
-
-        const val JUPYTER_NOTEBOOKS_PLUGIN_FILES: String = "*.ipynb"
-        const val JUPYTER_PLUGIN_SUGGESTION_DISMISSED_KEY: String = "jupyter.suggestion.dismissed"
-        const val NOTEBOOK_FILES_PLUGIN_SUGGESTION_DISMISSED_KEY: String = "notebook.files.suggestion.dismissed"
-    }
-
-    private fun isKotlinNotebooksPluginSuggestionDismissed(): Boolean {
-        return PropertiesComponent.getInstance().isTrueValue(KOTLIN_NOTEBOOKS_PLUGIN_SUGGESTION_DISMISSED_KEY)
-    }
-
-    private fun isJupyterNotebooksPluginSuggestionDismissed(): Boolean {
-        return PropertiesComponent.getInstance().isTrueValue(JUPYTER_PLUGIN_SUGGESTION_DISMISSED_KEY) && PropertiesComponent.getInstance()
-            .isTrueValue(NOTEBOOK_FILES_PLUGIN_SUGGESTION_DISMISSED_KEY)
-    }
-
-    private fun requiredPluginsKotlinNotebooksInstalled(): Boolean {
-        val pluginsToInstall = listOf(KOTLIN_NOTEBOOKS_PRIMARY_PLUGIN_ID, JUPYTER_PLUGIN_ID, NOTEBOOK_FILES_PLUGIN_ID)
-        return pluginsToInstall.all { pluginIdStr ->
-            val pluginId = PluginId.getId(pluginIdStr)
-            PluginManager.isPluginInstalled(pluginId) && !PluginManagerCore.isDisabled(pluginId)
-        }
-    }
-
-    private fun requiredPluginsJupyterNotebooksInstalled(): Boolean {
-        val pluginsToInstall = listOf(JUPYTER_PLUGIN_ID, NOTEBOOK_FILES_PLUGIN_ID)
-        return pluginsToInstall.all { pluginIdStr ->
-            val pluginId = PluginId.getId(pluginIdStr)
-            PluginManager.isPluginInstalled(pluginId) && !PluginManagerCore.isDisabled(pluginId)
-        }
-    }
-
     override fun getSuggestion(project: Project, file: VirtualFile): PluginSuggestion? {
         // Copy the file extension from `com.intellij.jupyter.core.jupyter.JupyterFileType` to avoid
         // having a dependency on the Jupyter module.
-        if (file.extension != "ipynb") {
-            return null
-        }
-
-        if (requiredPluginsKotlinNotebooksInstalled() || requiredPluginsJupyterNotebooksInstalled()) {
-            return null
-        }
-
-        if (isKotlinNotebooksPluginSuggestionDismissed() || isJupyterNotebooksPluginSuggestionDismissed() || tryUltimateIsDisabled()) {
-            return null
-        }
+        if (file.extension != "ipynb") return null
+        if (enablementCheckers.any { it.pluginsInstalledAndEnabled() }) return null
+        if (enablementCheckers.any { it.isSuggestionDismissed() }) return null
 
         // We should only consider notebooks configured to use the Kotlin kernel.
         // Any error in the JSON file or format will be ignored as it is handled as part
         // of opening the file in the editor.
-        try {
+        val notebookLanguage = try {
             val json = JsonParser.parseString(file.readText())
-            val notebookLanguage = json.asJsonObject["metadata"]
+            json.asJsonObject["metadata"]
                 ?.asJsonObject?.get("language_info")
                 ?.asJsonObject?.get("name")
                 ?.asString
-            if (!notebookLanguage.equals("kotlin", ignoreCase = true)) {
-                return JupyterAndNotebookFilesPluginsSuggestion(project, isCommunityIde())
-            }
+                ?.lowercase()
         } catch (_: Throwable) {
             return null
         }
 
-        return KotlinNotebookPluginSuggestion(project, isCommunityIde())
+        val enablementChecker = enablementCheckers.first { checker ->
+            checker.notebookLanguages == null ||
+                    notebookLanguage != null && notebookLanguage in checker.notebookLanguages
+        }
+        return JupyterPluginSuggestion(project, enablementChecker)
+    }
+}
+
+private class JupyterPluginSuggestion(
+    private val project: Project,
+    private val checker: PluginEnablementChecker,
+): PluginSuggestion {
+    override val pluginIds: List<String> = checker.pluginIds
+
+    override fun apply(fileEditor: FileEditor): EditorNotificationPanel {
+        val status = EditorNotificationPanel.Status.Info
+        val panel = EditorNotificationPanel(fileEditor, status)
+        setupPluginSuggestion(panel)
+
+        panel.createActionLabel(IdeBundle.message("plugins.advertiser.action.ignore.ultimate")) {
+            FUSEventSource.EDITOR.logIgnoreExtension(project)
+            checker.dismissSuggestion()
+            EditorNotifications.getInstance(project).updateAllNotifications()
+        }
+
+        return panel
+    }
+
+    private fun setupPluginSuggestion(panel: EditorNotificationPanel) {
+        panel.text = checker.suggestionText
+        panel.createActionLabel(checker.suggestionActionText) {
+            FUSEventSource.EDITOR.logInstallPlugins(pluginIds, project)
+            installAndEnable(project, pluginIds.map(PluginId::getId).toSet(), true) {
+                EditorNotifications.getInstance(project).updateAllNotifications()
+            }
+            if (checker.requiresIdeRestart) {
+                PluginManagerConfigurable.shutdownOrRestartApp()
+            }
+        }
+    }
+}
+
+private class PluginEnablementChecker(
+    val notebookLanguages: Set<String>?,
+    private val dismissedKey: String,
+    val suggestionText: String,
+    val suggestionActionText: String,
+    val requiresIdeRestart: Boolean,
+    val pluginIds: List<String>,
+) {
+    fun isSuggestionDismissed(): Boolean = PropertiesComponent.getInstance().isTrueValue(dismissedKey)
+    fun dismissSuggestion(): Unit = PropertiesComponent.getInstance().setValue(dismissedKey, true)
+    fun pluginsInstalledAndEnabled(): Boolean {
+        return pluginIds.all { pluginIdStr ->
+            val pluginId = PluginId.getId(pluginIdStr)
+            PluginManager.isPluginInstalled(pluginId) && !PluginManagerCore.isDisabled(pluginId)
+        }
     }
 }
