98e46b67a737900d6f6423415a2bded4bc6363f9
Alexander.Danilov <alexander.danilov@jetbrains.com>
2025-03-17 11:52:57 +0100

    [driver-sdk] fix double action call in runToolWindow
    GitOrigin-RevId: 4890778e91c7f9d7718de8882ff2d496cdbdd369

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/IdeaFrameUiExt.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/IdeaFrameUiExt.kt
@@ -9,7 +9,7 @@ private const val TOOL_WINDOW_ROOT_COMPONENT_CLASS = "com.intellij.toolWindow.In
 
 fun IdeaFrameUI.buildToolWindow(action: UiComponent.() -> Unit = {}): UiComponent = x { byType("com.intellij.build.BuildView") }.apply(action)
 
-fun IdeaFrameUI.runToolWindow(action: UiComponent.() -> Unit = {}): UiComponent = toolWindow("Main", action).apply(action)
+fun IdeaFrameUI.runToolWindow(action: UiComponent.() -> Unit = {}): UiComponent = toolWindow("Main", action)
 
 fun IdeaFrameUI.notificationsToolWindow(action: UiComponent.() -> Unit = {}): UiComponent = toolWindow("Notifications", action)
 
