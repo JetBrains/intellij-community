5821f42c722d859cd8302e53bec4a14f2c5730f7
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2025-03-17 14:06:11 +0200

    [kotlin] Refactor "add use-site target" inspections and intentions
    Make the mod command abstract to remove the obscure constructor contract.
    Stop using the non-mod-command utility for choosing the target in K2 implementations.
    Code cleanup.
    
    KTIJ-33338
    
    GitOrigin-RevId: 59e62294938a8e93948c45c5b29a62aaba48de56

--- plugins/kotlin/code-insight/impl-base/src/org/jetbrains/kotlin/idea/codeinsights/impl/base/quickFix/AddAnnotationUseSiteTargetModCommandAction.kt
+++ plugins/kotlin/code-insight/impl-base/src/org/jetbrains/kotlin/idea/codeinsights/impl/base/quickFix/AddAnnotationUseSiteTargetModCommandAction.kt
@@ -3,20 +3,11 @@
 package org.jetbrains.kotlin.idea.codeinsights.impl.base.quickFix
 
 import com.intellij.codeInspection.util.IntentionFamilyName
-import com.intellij.modcommand.ActionContext
-import com.intellij.modcommand.ModCommand
-import com.intellij.modcommand.ModCommand.chooseAction
-import com.intellij.modcommand.ModPsiUpdater
-import com.intellij.modcommand.PsiBasedModCommandAction
-import com.intellij.modcommand.PsiUpdateModCommandAction
+import com.intellij.modcommand.*
 import com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.analysis.api.analyze
-import org.jetbrains.kotlin.analysis.api.analyzeCopy
-import org.jetbrains.kotlin.analysis.api.projectStructure.KaDanglingFileResolutionMode
 import org.jetbrains.kotlin.descriptors.annotations.AnnotationUseSiteTarget
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.codeinsights.impl.base.intentions.AddAnnotationUseSiteTargetUtils.addUseSiteTarget
-import org.jetbrains.kotlin.idea.codeinsights.impl.base.intentions.AddAnnotationUseSiteTargetUtils.getApplicableUseSiteTargets
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.psi.KtBlockExpression
 
@@ -24,24 +15,25 @@ import org.jetbrains.kotlin.psi.KtBlockExpression
  * A mod command action for adding an explicit use-site target to a [KtAnnotationEntry].
  * If there are one or more applicable targets, [com.intellij.modcommand.ModChooseAction] is used.
  * Otherwise, the action is no-op.
- *
- * @param targets precomputed use-site targets. If null is passed, the applicable targets will
- * be computed based on the [KtAnnotationEntry] passed to the mod command action. Note that it's the
- * responsibility of the caller to make sure the passed non-null targets match the passed element.
  */
-open class AddAnnotationUseSiteTargetModCommandAction(
-    val targets: List<AnnotationUseSiteTarget>?,
-) : PsiBasedModCommandAction<KtAnnotationEntry>(null, KtAnnotationEntry::class.java) {
+abstract class AddAnnotationUseSiteTargetModCommandAction() :
+    PsiBasedModCommandAction<KtAnnotationEntry>(null, KtAnnotationEntry::class.java) {
+
+    abstract fun getAnnotationTargets(
+        context: ActionContext,
+        element: KtAnnotationEntry,
+    ): List<AnnotationUseSiteTarget>
+
     override fun perform(
         context: ActionContext,
         element: KtAnnotationEntry,
     ): ModCommand {
-        val targets = targets ?: getAnnotationTargets(element)
-        when (targets.size) {
-            0 -> return ModCommand.nop()
-            else -> return chooseAction(
+        val targets = getAnnotationTargets(context, element)
+        return when (targets.size) {
+            0 -> ModCommand.nop()
+            else -> ModCommand.chooseAction(
                 KotlinBundle.message("title.choose.use.site.target"),
-                targets.map { AddSingleTargetAction(element, it) }
+                targets.map { AddSingleTargetModCommandAction(element, it) },
             )
         }
     }
@@ -53,30 +45,23 @@ open class AddAnnotationUseSiteTargetModCommandAction(
         context: ActionContext,
     ): Boolean = element is KtBlockExpression
 
-    override fun isElementApplicable(element: KtAnnotationEntry, context: ActionContext): Boolean {
-        val targets = targets ?: getAnnotationTargets(element)
-        return targets.isNotEmpty()
-    }
-
-    private fun getAnnotationTargets(element: KtAnnotationEntry): List<AnnotationUseSiteTarget> {
-        return if (element.isPhysical) analyze(element) {
-            element.getApplicableUseSiteTargets()
-        } else analyzeCopy(element, KaDanglingFileResolutionMode.PREFER_SELF) {
-            element.getApplicableUseSiteTargets()
-        }
-    }
+    override fun isElementApplicable(
+        element: KtAnnotationEntry,
+        context: ActionContext,
+    ): Boolean = getAnnotationTargets(context, element).isNotEmpty()
 }
 
-private class AddSingleTargetAction(
-    entry: KtAnnotationEntry,
+private class AddSingleTargetModCommandAction(
+    element: KtAnnotationEntry,
     private val target: AnnotationUseSiteTarget,
-) : PsiUpdateModCommandAction<KtAnnotationEntry>(entry) {
+) : PsiUpdateModCommandAction<KtAnnotationEntry>(element) {
+
     override fun invoke(
         context: ActionContext,
         element: KtAnnotationEntry,
-        updater: ModPsiUpdater
+        updater: ModPsiUpdater,
     ) {
-        element.addUseSiteTarget(listOf(target), null)
+        element.addUseSiteTarget(target)
     }
 
     override fun getFamilyName(): @IntentionFamilyName String =
