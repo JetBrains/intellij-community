f9bf970c56422bcc60e60d5b68899d3a28fdebc0
Tagir Valeev <tagir.valeev@jetbrains.com>
2025-03-17 14:22:47 +0100

    [java-highlighting] IDEA-369310 NPE in error highlighter
    GitOrigin-RevId: 0944d4f1c9594d138df85041748ff72fb56e436d

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/annotations/incomplete.java
@@ -0,0 +1,3 @@
+// IDEA-369310
+@SuppressWarnings(<error descr="Incompatible types. Found: 'null', required: 'java.lang.String[]'">"unused".</error><error descr="Identifier expected">)</error>
+class X {}
