787f41f4ba7a17f684175cba9f8045393e14dffb
Bas Leijdekkers <basleijdekkers@gmail.com>
2025-03-18 10:23:34 +0100

    Java: avoid constant or null expressions when convertion conditional to boolean expression (IDEA-369367)
    GitOrigin-RevId: fa3adf57e11ce16ed39459ac296f737d895ac57a

--- java/java-impl/src/com/siyeh/ipp/conditional/ReplaceConditionalWithBooleanExpressionIntention.java
+++ java/java-impl/src/com/siyeh/ipp/conditional/ReplaceConditionalWithBooleanExpressionIntention.java
@@ -1,8 +1,10 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.siyeh.ipp.conditional;
 
 import com.intellij.codeInspection.util.IntentionName;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.psi.*;
+import com.intellij.psi.util.PsiUtil;
 import com.siyeh.IntentionPowerPackBundle;
 import com.siyeh.ig.PsiReplacementUtil;
 import com.siyeh.ig.psiutils.BoolUtils;
@@ -18,7 +20,7 @@ import static com.siyeh.ig.psiutils.ParenthesesUtils.AND_PRECEDENCE;
 /**
  * @author Bas Leijdekkers
  */
-public final class ReplaceConditionalWithBooleanExpressionIntention extends MCIntention {
+public final class ReplaceConditionalWithBooleanExpressionIntention extends MCIntention implements DumbAware {
 
   @Override
   public @NotNull String getFamilyName() {
@@ -35,10 +37,13 @@ public final class ReplaceConditionalWithBooleanExpressionIntention extends MCIn
     return new PsiElementPredicate() {
       @Override
       public boolean satisfiedBy(PsiElement element) {
-        if (!(element instanceof PsiConditionalExpression conditionalExpression)) {
+        if (!(element instanceof PsiConditionalExpression conditional)
+            || isConstantOrNull(conditional.getThenExpression())
+            || isConstantOrNull(conditional.getElseExpression())) {
+          // case with constants is handled by com.siyeh.ig.controlflow.SimplifiableConditionalExpressionInspection
           return false;
         }
-        final PsiType type = conditionalExpression.getType();
+        final PsiType type = conditional.getType();
         return PsiTypes.booleanType().equals(type) || type != null && type.equalsToText(JAVA_LANG_BOOLEAN);
       }
     };
@@ -49,16 +54,19 @@ public final class ReplaceConditionalWithBooleanExpressionIntention extends MCIn
     final PsiConditionalExpression conditionalExpression = (PsiConditionalExpression)element;
     final PsiExpression condition = conditionalExpression.getCondition();
     CommentTracker tracker = new CommentTracker();
-    final String replacementText = condition.getText() + "&&" + getText(conditionalExpression.getThenExpression(), tracker) + "||" +
-                                   BoolUtils.getNegatedExpressionText(condition, AND_PRECEDENCE, tracker) + "&&" +
-                                   getText(conditionalExpression.getElseExpression(), tracker);
+    final String replacementText = condition.getText() + "&&" + getText(conditionalExpression.getThenExpression(), tracker) + "||"
+                                   + BoolUtils.getNegatedExpressionText(condition, AND_PRECEDENCE, tracker) + "&&"
+                                   + getText(conditionalExpression.getElseExpression(), tracker);
     PsiReplacementUtil.replaceExpression((PsiExpression)element, replacementText, tracker);
   }
 
   private static String getText(@Nullable PsiExpression expression, CommentTracker tracker) {
-    if (expression == null) {
-      return "";
-    }
-    return tracker.text(expression, AND_PRECEDENCE);
+    return expression == null ? "" : tracker.text(expression, AND_PRECEDENCE);
+  }
+
+  private static boolean isConstantOrNull(@Nullable PsiExpression expression) {
+    expression = PsiUtil.deparenthesizeExpression(expression);
+    return expression instanceof PsiLiteralExpression && PsiUtil.isJavaToken(expression.getFirstChild(), JavaTokenType.NULL_KEYWORD)
+           || PsiUtil.isConstantExpression(expression);
   }
 }
