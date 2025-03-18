c402eea444e163411c811abd0af140d10736c828
Alexander Zolotov <zolotov@jetbrains.com>
2025-03-18 19:30:06 +0100

    [fleet, logging] introduced logging.shared module, move Fleet-specific MDC there
    GitOrigin-RevId: 44e91dbbbf8b29922d7d4645e187b9c022a322d0

--- fleet/util/logging/api/src/fleet/util/logging/KLoggers.kt
+++ fleet/util/logging/api/src/fleet/util/logging/KLoggers.kt
@@ -11,6 +11,9 @@ object KLoggers {
   fun logger(owner: KClass<*>): KLogger = loggerFactory.logger(owner)
   fun logger(owner: Any): KLogger = loggerFactory.logger(owner)
   fun logger(name: String): KLogger = loggerFactory.logger(name)
+
+  fun setLoggingContext(map: Map<String, String>?): Unit = loggerFactory.setLoggingContext(map)
+  fun getLoggingContext(): Map<String, String>? = loggerFactory.getLoggingContext()
 }
 
 inline fun <reified T> logger(): KLogger = KLoggers.logger(T::class)
