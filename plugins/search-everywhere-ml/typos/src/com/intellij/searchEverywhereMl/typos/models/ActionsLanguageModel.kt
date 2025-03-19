215066f22aaa35fa4062184ddb7b80fecbe58452
yana.hrynevich <yana.hrynevich@jetbrains.com>
2025-03-19 19:53:48 +0100

    MLP-66 Refactor `ActionsLanguageModel` to compute words directly from the corpus.
    GitOrigin-RevId: f49b4e56d6f7ad855b915b065ca5dddd76c808ed

--- plugins/search-everywhere-ml/typos/src/com/intellij/searchEverywhereMl/typos/models/ActionsLanguageModel.kt
+++ plugins/search-everywhere-ml/typos/src/com/intellij/searchEverywhereMl/typos/models/ActionsLanguageModel.kt
@@ -1,27 +1,8 @@
 package com.intellij.searchEverywhereMl.typos.models
 
-import com.intellij.ide.ui.search.SearchableOptionsRegistrar
-import com.intellij.ide.ui.search.SearchableOptionsRegistrarImpl
-import com.intellij.openapi.actionSystem.ActionGroup
-import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.ActionPlaces
-import com.intellij.openapi.actionSystem.impl.ActionManagerImpl
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
-import com.intellij.openapi.components.serviceAsync
-import com.intellij.openapi.diagnostic.thisLogger
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.ProjectManager
-import com.intellij.openapi.util.NlsActions
-import com.intellij.openapi.util.NlsContexts
-import com.intellij.openapi.wm.IdeFrame
-import com.intellij.openapi.wm.WindowManager
-import com.intellij.openapi.wm.ex.WindowManagerEx
-import com.intellij.platform.ide.progress.withBackgroundProgress
-import com.intellij.searchEverywhereMl.typos.SearchEverywhereStringToken
-import com.intellij.searchEverywhereMl.typos.TyposBundle
 import com.intellij.searchEverywhereMl.typos.isTypoFixingEnabled
-import com.intellij.searchEverywhereMl.typos.splitText
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
@@ -42,63 +23,25 @@ internal class ActionsLanguageModel(val coroutineScope: CoroutineScope) {
   }
 
   val deferredDictionary: Deferred<LanguageModelDictionary> = coroutineScope.async {
-    val project = guessProject()
-    if (project == null) {
-      computeLanguageModelDictionary()
-    }
-    else {
-      withBackgroundProgress(project,
-                             TyposBundle.getMessage("progress.title.computing.actions.language.model"),
-                             cancellable = false) {
-        computeLanguageModelDictionary()
-      }
-    }
+    val corpus = CorpusBuilder.getInstance()?.deferredCorpus?.await() ?: emptySet()
+    computeLanguageModelDictionary(corpus)
   }
 
-  // Accept any word that consist of only alphabetical characters, that are between 3 and 45 characters long
-  private val acceptableWordsPattern = Pattern.compile("^[a-zA-Z]{3,45}\$")
+  // Accept any word that is between 3 and 45 characters long
+  private val acceptableWordsPattern = Pattern.compile("^.{3,45}$")
 
-  private suspend fun getWordsFromActions(): Sequence<@NlsActions.ActionText String> {
-    return (serviceAsync<ActionManager>() as ActionManagerImpl).actionsOrStubs()
-      .filterNot { it is ActionGroup && !it.isSearchable }
-      .mapNotNull {
-        val presentation = it.templatePresentation
-        it.applyTextOverride(ActionPlaces.ACTION_SEARCH, presentation)
-        return@mapNotNull presentation.text
-      }
-  }
+  private fun computeLanguageModelDictionary(corpus: Set<List<String>>): LanguageModelDictionary {
+    val cleanedSentences = corpus.map { sentence ->
+      sentence.filter { word -> acceptableWordsPattern.matcher(word).matches() }
+    }.filter { it.isNotEmpty() }
 
-  private suspend fun getWordsFromSettings(): Sequence<@NlsContexts.ConfigurableName CharSequence> {
-    val registrar = serviceAsync<SearchableOptionsRegistrar>() as? SearchableOptionsRegistrarImpl
-    if (registrar == null) {
-      thisLogger().warn("Failed to cast SearchableOptionsRegistrar")
-      return emptySequence()
-    }
+    val validWords = cleanedSentences.flatten()
 
-    registrar.initialize()
-    return registrar.getAllOptionNames().asSequence()
-  }
-
-  private suspend fun computeLanguageModelDictionary(): LanguageModelDictionary {
-    return (getWordsFromActions() + getWordsFromSettings())
-      .flatMap {
-        splitText(it)
-          .filter { token -> token is SearchEverywhereStringToken.Word }
-          .map { token -> token.value }
-      }
-      .filter { acceptableWordsPattern.matcher(it).matches() }
-      .map { it.lowercase() }
-      .groupingBy { it }.eachCount()
+    val dictionary = validWords.groupingBy { it }
+      .eachCount()
       .let { SimpleLanguageModelDictionary(it) }
+
+    return dictionary
   }
 
-  private suspend fun guessProject(): Project? {
-    val recentFocusedWindow = (serviceAsync<WindowManager>() as WindowManagerEx).mostRecentFocusedWindow
-    if (recentFocusedWindow is IdeFrame) {
-      return recentFocusedWindow.project
-    }
-    else {
-      return serviceAsync<ProjectManager>().openProjects.firstOrNull { o -> o.isInitialized && !o.isDisposed }
-    }
-  }
 }
