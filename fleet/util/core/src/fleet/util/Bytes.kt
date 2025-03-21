6f7237fde8054bef79d48fd6eed532c65bd37187
Alexander Shparun <alexander.shparun@jetbrains.com>
2025-03-21 09:02:31 +0100

    [fleet] authorize webSockets by query parameters instead of websockets
    browser does not allow to set custom headers for websocket requests
    
    GitOrigin-RevId: 6d099b1b6fd0d962cff59f0a874085c8847fda68

--- fleet/util/core/src/fleet/util/Bytes.kt
+++ fleet/util/core/src/fleet/util/Bytes.kt
@@ -9,20 +9,20 @@ import kotlin.io.encoding.ExperimentalEncodingApi
  */
 
 // Replaces this.toByteArray(Charset.UTF_8)
-fun String.encodeToByteArrayUtf8() = encodeToByteArray()
+fun String.encodeToByteArrayUtf8(): ByteArray = encodeToByteArray()
 
 // Replaces String(this, ..., Charset.UTF_8)
-fun ByteArray.decodeToStringUtf8() = decodeToString()
+fun ByteArray.decodeToStringUtf8(): String = decodeToString()
 fun ByteArray.decodeToStringUtf8(
   startIndex: Int = 0,
   endIndex: Int = this.size,
-  throwOnInvalidSequence: Boolean = false
+  throwOnInvalidSequence: Boolean = false,
 ): String = decodeToString(startIndex, endIndex, throwOnInvalidSequence)
 
 // Replaces java.util.Base64.getDecoder(), which did not require padding on decoding
 @OptIn(ExperimentalEncodingApi::class)
-val Base64WithOptionalPadding = Base64.withPadding(Base64.PaddingOption.PRESENT_OPTIONAL)
+val Base64WithOptionalPadding: Base64 = Base64.withPadding(Base64.PaddingOption.PRESENT_OPTIONAL)
 
 // Replaces java.util.Base64.getUrlDecoder(), which did not require padding on decoding
 @OptIn(ExperimentalEncodingApi::class)
-val UrlSafeBase64WithOptionalPadding = Base64.UrlSafe.withPadding(Base64.PaddingOption.PRESENT_OPTIONAL)
+val UrlSafeBase64WithOptionalPadding: Base64 = Base64.UrlSafe.withPadding(Base64.PaddingOption.PRESENT_OPTIONAL)
