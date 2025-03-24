056968bb3ed9ac38a288f010bf5cadf540c2389b
Michael Golubev <michael.golubev@jetbrains.com>
2025-03-24 16:39:04 +0100

    IJPL-181857 - Expose some `remoteServer` classes, @Internal -> @Obsolete
    (cherry picked from commit 0b8e3b6f4417b4559f075227312e75bf0b4d65c3)
    
    IJ-CR-158260
    
    GitOrigin-RevId: 5e1bf4efe90a9a339b6e327ab9e4827e6b21c3fe

--- platform/remote-servers/impl/src/com/intellij/remoteServer/util/package-info.java
+++ platform/remote-servers/impl/src/com/intellij/remoteServer/util/package-info.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-@Internal
+@Obsolete
 package com.intellij.remoteServer.util;
 
-import org.jetbrains.annotations.ApiStatus.Internal;
+import org.jetbrains.annotations.ApiStatus.Obsolete;
