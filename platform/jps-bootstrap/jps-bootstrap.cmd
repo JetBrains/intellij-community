1404068f3a4568e96d2326ad129fa4c76154963a
Ilia Kirianovskii <Ilia.Kirianovskii@jetbrains.com>
2025-03-24 21:45:47 +0100

    jps-bootstrap: Upgrade jbrsdk to v17.0.14b1376.4
    Also switches to jbrsdk .zip on Windows.
    
    GitOrigin-RevId: 010b9a3a6aa0e3f880f288cf5d21e54ea65abb05

--- platform/jps-bootstrap/jps-bootstrap.cmd
+++ platform/jps-bootstrap/jps-bootstrap.cmd
@@ -9,8 +9,8 @@ set JPS_BOOTSTRAP_PREPARE_DIR=%JPS_BOOTSTRAP_COMMUNITY_HOME%out\jps-bootstrap\
 
 setlocal
 
-set JBR_VERSION=17.0.4.1
-set JBR_BUILD=b597.1
+set JBR_VERSION=17.0.14
+set JBR_BUILD=b1376.4
 if "%PROCESSOR_ARCHITECTURE%" == "ARM64" (
   set JBR_ARCH=windows-aarch64
 ) else (
@@ -20,8 +20,8 @@ set SCRIPT_VERSION=jps-bootstrap-cmd-v1
 set COMPANY_NAME=JetBrains
 set TARGET_DIR=%LOCALAPPDATA%\Temp\%COMPANY_NAME%\
 set JVM_TARGET_DIR=%TARGET_DIR%%JBR_VERSION%%JBR_BUILD%-%JBR_ARCH%-%SCRIPT_VERSION%\
-set JVM_TEMP_FILE=jvm-%JBR_ARCH%.tar.gz
-set JVM_URL=https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk-%JBR_VERSION%-%JBR_ARCH%-%JBR_BUILD%.tar.gz
+set JVM_TEMP_FILE=jvm-%JBR_ARCH%.zip
+set JVM_URL=https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk-%JBR_VERSION%-%JBR_ARCH%-%JBR_BUILD%.zip
 
 set POWERSHELL=%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe
 
@@ -59,7 +59,7 @@ cd /d "%JVM_TARGET_DIR%"
 if errorlevel 1 goto fail
 
 echo Extracting %TARGET_DIR%%JVM_TEMP_FILE% to %JVM_TARGET_DIR%
-"%POWERSHELL%" -nologo -noprofile -command "Set-StrictMode -Version 3.0; $ErrorActionPreference = \"Stop\"; (Get-Location) -split '\\' | ForEach { $dir='' } { $dir=Get-Item \"$dir$_\\\" -Force -ErrorAction SilentlyContinue; if ($dir.Attributes -band [System.IO.FileAttributes]::ReparsePoint) { $dir=\"$($dir.Target)\\\" } } { Set-Location $dir }; & tar -x -f \"..\%JVM_TEMP_FILE%\" -C .; if ($LastExitCode -ne 0) { throw \"Exec: tar exited with code $LastExitCode\" }"
+"%POWERSHELL%" -nologo -noprofile -command "Set-StrictMode -Version 3.0; $ErrorActionPreference = \"Stop\"; Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('..\\%JVM_TEMP_FILE%', '.');"
 if errorlevel 1 goto fail
 
 del /F "..\%JVM_TEMP_FILE%"
