6f7d6d322fa26d04fa799d2b2a9ffdf242f5b68d
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2025-03-24 13:14:52 +0100

    IDEA-365317 increment JavaFileElementType#STUB_VERSION
    GitOrigin-RevId: 92d5895ac5e56af8a3b99fef2e36fb6632d8dfb3

--- java/java-psi-impl/src/com/intellij/psi/impl/source/JavaFileElementType.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/JavaFileElementType.java
@@ -17,7 +17,7 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 public class JavaFileElementType extends IFileElementType {
-  public static final int STUB_VERSION = 62;
+  public static final int STUB_VERSION = 63;
 
   public JavaFileElementType() {
     super("java.FILE", JavaLanguage.INSTANCE);
