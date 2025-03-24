b749401f9198a47975debc56f67215bf932220ce
Ivan Morgillo <imorgillo@gmail.com>
2025-03-24 15:32:40 +0100

    [JEWEL-792] Add slot-based API for InlineBanner
    This adds a slot-based API to InlineBanner, in addition to
    the String-based API that already exists. It is a
    non-breaking change. The reason is to allow users to specify
    their own main content, e.g., to use Markdown. The title is
    unaffected as it remains plain text.
    
    Additionally, this fixes the KDoc for InlineBanner by making
    sure there is no content after the tags section. Before this
    all the content after the last tag would be considered to be
    part of the tag itself's documentation, which is incorrect.
    
    The KDoc have also been rewritten for correctness and to
    improve clarity, making them simpler and more readable.
    
    closes https://github.com/JetBrains/intellij-community/pull/3012
    
    Update platform/jewel/samples/showcase/src/main/kotlin/org/jetbrains/jewel/samples/showcase/components/Banners.kt
    
    Co-authored-by: Sebastiano Poggi <rock3r@users.noreply.github.com>
    
    closes https://github.com/JetBrains/intellij-community/pull/3012
    
    make linter happy
    
    closes https://github.com/JetBrains/intellij-community/pull/3012
    
    add optional title to InlineBanner
    
    closes https://github.com/JetBrains/intellij-community/pull/3012
    
    GitOrigin-RevId: 260c84b454819a47afe99f729ae375f57106d85d

--- platform/jewel/samples/ide-plugin/src/main/kotlin/org/jetbrains/jewel/samples/ideplugin/dialog/ComponentShowcaseDialog.kt
+++ platform/jewel/samples/ide-plugin/src/main/kotlin/org/jetbrains/jewel/samples/ideplugin/dialog/ComponentShowcaseDialog.kt
@@ -3,6 +3,7 @@ package org.jetbrains.jewel.samples.ideplugin.dialog
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
@@ -24,6 +25,7 @@ import org.jetbrains.jewel.bridge.theme.macOs
 import org.jetbrains.jewel.bridge.toDpSize
 import org.jetbrains.jewel.bridge.toPaddingValues
 import org.jetbrains.jewel.foundation.theme.JewelTheme
+import org.jetbrains.jewel.intui.markdown.bridge.ProvideMarkdownStyling
 import org.jetbrains.jewel.samples.showcase.views.ComponentsView
 import org.jetbrains.jewel.samples.showcase.views.ComponentsViewModel
 import org.jetbrains.jewel.ui.component.styling.IconButtonMetrics
@@ -43,7 +45,6 @@ internal class ComponentShowcaseDialog(project: Project) : DialogWrapper(project
 
     override fun createSouthPanel(): JComponent? = null
 
-    @OptIn(ExperimentalLayoutApi::class)
     override fun createCenterPanel(): JComponent {
         val viewModel =
             ComponentsViewModel(
@@ -54,31 +55,41 @@ internal class ComponentShowcaseDialog(project: Project) : DialogWrapper(project
         val dialogPanel = JewelComposePanel {
             val iconButtonMetrics = JewelTheme.iconButtonStyle.metrics
             val uiSettings = UISettings.Companion.getInstance()
-            ComponentsView(
-                viewModel = viewModel,
-                toolbarButtonMetrics =
-                    remember(uiSettings.compactMode, ResizeStripeManager.isShowNames()) {
-                        iconButtonMetrics.tweak(
-                            minSize = Toolbar.stripeToolbarButtonSize().toDpSize(),
-                            // See com.intellij.openapi.wm.impl.SquareStripeButtonLook.getButtonArc
-                            cornerSize =
-                                retrieveArcAsCornerSizeOrDefault(
-                                    "Button.ToolWindow.arc",
-                                    CornerSize(if (uiSettings.compactMode) 4.dp else 6.dp),
-                                ),
-                            padding =
-                                Toolbar.stripeToolbarButtonIconPadding(
-                                        uiSettings.compactMode, // compactMode
-                                        ResizeStripeManager.isShowNames(), // showNames
-                                    )
-                                    .toPaddingValues(),
-                            borderWidth = 0.dp,
-                        )
-                    },
-            )
+            ProvideMarkdownStyling { RootLayout(viewModel, uiSettings, iconButtonMetrics) }
         }
         dialogPanel.preferredSize = Dimension(800, 600)
         return dialogPanel
+    }
+
+    @OptIn(ExperimentalLayoutApi::class)
+    @Composable
+    private fun RootLayout(
+        viewModel: ComponentsViewModel,
+        uiSettings: UISettings,
+        iconButtonMetrics: IconButtonMetrics,
+    ) {
+        ComponentsView(
+            viewModel = viewModel,
+            toolbarButtonMetrics =
+                remember(uiSettings.compactMode, ResizeStripeManager.isShowNames()) {
+                    iconButtonMetrics.tweak(
+                        minSize = Toolbar.stripeToolbarButtonSize().toDpSize(),
+                        // See com.intellij.openapi.wm.impl.SquareStripeButtonLook.getButtonArc
+                        cornerSize =
+                            retrieveArcAsCornerSizeOrDefault(
+                                "Button.ToolWindow.arc",
+                                CornerSize(if (uiSettings.compactMode) 4.dp else 6.dp),
+                            ),
+                        padding =
+                            Toolbar.stripeToolbarButtonIconPadding(
+                                    uiSettings.compactMode, // compactMode
+                                    ResizeStripeManager.isShowNames(), // showNames
+                                )
+                                .toPaddingValues(),
+                        borderWidth = 0.dp,
+                    )
+                },
+        )
     }
 
     private fun IconButtonMetrics.tweak(
