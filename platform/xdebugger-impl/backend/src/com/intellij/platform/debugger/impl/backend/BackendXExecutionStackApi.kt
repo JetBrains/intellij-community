10a9d26a1f10a9e3d73faf4692d73ec4f6ccf2dc
Alexander Kuznetsov <aleksander.kuznetsov@jetbrains.com>
2025-03-24 13:00:40 +0100

    [rd][debugger] IJPL-177087 Trying to fix project leak 2
    Don't create XStackFrameEntity
    
    GitOrigin-RevId: a7372d5b169101f1567c887f62d95fcf31642f57

--- platform/xdebugger-impl/backend/src/com/intellij/platform/debugger/impl/backend/BackendXExecutionStackApi.kt
+++ platform/xdebugger-impl/backend/src/com/intellij/platform/debugger/impl/backend/BackendXExecutionStackApi.kt
@@ -5,7 +5,6 @@ import com.intellij.openapi.util.NlsContexts
 import com.intellij.xdebugger.frame.XExecutionStack
 import com.intellij.xdebugger.frame.XStackFrame
 import com.intellij.xdebugger.impl.rhizome.XDebuggerEntity.Companion.debuggerEntity
-import com.intellij.xdebugger.impl.rhizome.XDebuggerEntity.Companion.new
 import com.intellij.xdebugger.impl.rhizome.XExecutionStackEntity
 import com.intellij.xdebugger.impl.rhizome.XStackFrameEntity
 import com.intellij.xdebugger.impl.rpc.*
@@ -37,11 +36,13 @@ internal class BackendXExecutionStackApi : XExecutionStackApi {
           override fun addStackFrames(stackFrames: List<XStackFrame>, last: Boolean) {
             this@channelFlow.launch {
               withEntities(executionStackEntity) {
-                val frameEntities = stackFrames.map { frame ->
+                // TODO[IJPL-177087] suspect project leak
+                val frameEntities = /*stackFrames.map { frame ->
                   change {
                     XStackFrameEntity.new(this, frame)
                   }
-                }
+                }*/
+                  emptyList<XStackFrameEntity>()
                 change {
                   executionStackEntity.update {
                     it[XExecutionStackEntity.StackFrames] = executionStackEntity.frames + frameEntities
