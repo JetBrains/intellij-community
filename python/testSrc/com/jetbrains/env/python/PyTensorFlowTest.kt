f64eefd2933855159229c954e9e3e8a771dd24ee
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2025-03-24 19:17:05 +0200

    PY-53599 Enable the Typeshed stubs for tensorflow in its env test
    (cherry picked from commit 303317e205c0c784b3d40c129815cf5302becda2)
    
    IJ-MR-157295
    
    GitOrigin-RevId: 3371448e56240f07d210a54456b74636e3e1a037

--- python/testSrc/com/jetbrains/env/python/PyTensorFlowTest.kt
+++ python/testSrc/com/jetbrains/env/python/PyTensorFlowTest.kt
@@ -1,12 +1,18 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.env.python
 
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.projectRoots.SdkModificator
+import com.intellij.openapi.roots.OrderRootType
 import com.intellij.testFramework.UsefulTestCase
+import com.intellij.util.ThrowableRunnable
 import com.jetbrains.env.EnvTestTagsRequired
 import com.jetbrains.env.PyEnvTestCase
 import com.jetbrains.env.PyExecutionFixtureTestTask
 import com.jetbrains.python.PyPsiPackageUtil
+import com.jetbrains.python.codeInsight.typing.PyTypeShed.getStubRootForPackage
 import com.jetbrains.python.inspections.unresolvedReference.PyUnresolvedReferencesInspection
 import com.jetbrains.python.packaging.PyPackageManager
 import com.jetbrains.python.tools.sdkTools.SdkCreationType
@@ -26,6 +32,14 @@ class PyTensorFlowTest : PyEnvTestCase() {
       val sdk = createTempSdk(sdkHome, SdkCreationType.SDK_PACKAGES_AND_SKELETONS)
       val packages = PyPackageManager.getInstance(sdk).refreshAndGetPackages(false)
       assertNotNull(PyPsiPackageUtil.findPackage (packages, "tensorflow"))
+
+      ApplicationManager.getApplication().invokeAndWait(Runnable {
+        WriteAction.run<java.lang.RuntimeException?>(ThrowableRunnable {
+          val modificator: SdkModificator = sdk.getSdkModificator()
+          modificator.addRoot(getStubRootForPackage("tensorflow")!!, OrderRootType.CLASSES)
+          modificator.commitChanges()
+        })
+      })
 
       myFixture.enableInspections(PyUnresolvedReferencesInspection::class.java)
       myFixture.configureByFile("submodulesExportedAsAttributes.py")
