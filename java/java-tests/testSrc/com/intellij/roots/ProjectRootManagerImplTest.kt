a58e8a2a793c1096d6a8e83083380564d8aec8c9
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-03-25 14:27:48 +0100

    [threading] IJPL-182142: Migrate some tests to JUnit 5
    This is needed mostly because we need to release write-intent lock from the test
    
    GitOrigin-RevId: b85888ee0612a6ffd0b7842198648b2e5ca16e34

--- java/java-tests/testSrc/com/intellij/roots/ProjectRootManagerImplTest.kt
+++ java/java-tests/testSrc/com/intellij/roots/ProjectRootManagerImplTest.kt
@@ -1,30 +1,38 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.roots
 
 import com.intellij.idea.TestFor
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.roots.ex.ProjectRootManagerEx
 import com.intellij.openapi.roots.impl.ProjectRootManagerImpl.Companion.getInstanceImpl
 import com.intellij.openapi.util.JDOMUtil
-import com.intellij.testFramework.HeavyPlatformTestCase
+import com.intellij.testFramework.common.timeoutRunBlocking
 import com.intellij.testFramework.common.waitUntil
-import com.intellij.testFramework.executeSomeCoroutineTasksAndDispatchAllInvocationEvents
+import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.testFramework.junit5.fixture.projectFixture
 import com.intellij.util.concurrency.ThreadingAssertions
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.time.Duration.Companion.seconds
 
-class ProjectRootManagerImplTest : HeavyPlatformTestCase() {
+@TestApplication
+class ProjectRootManagerImplTest {
+
+  val project = projectFixture(openAfterCreation = true)
+
+  @Test
   @TestFor(issues = ["IDEA-232634"])
-  fun testLoadStateFiresJdkChange() {
+  fun testLoadStateFiresJdkChange(): Unit = timeoutRunBlocking {
+    val project = project.get()
     val count = AtomicInteger(0)
-    ProjectRootManagerEx.getInstanceEx(myProject).addProjectJdkListener {
+    ProjectRootManagerEx.getInstanceEx(project).addProjectJdkListener {
       ThreadingAssertions.assertWriteAccess()
       count.incrementAndGet()
     }
 
-    val impl = getInstanceImpl(myProject)
+    val impl = getInstanceImpl(project)
     val firstLoad = JDOMUtil.load("""
                                    <component name="ProjectRootManager" version="2" languageLevel="JDK_11" default="true" project-jdk-name="corretto-11" project-jdk-type="JavaSDK">
                                      <output url="file://${'$'}PROJECT_DIR${'$'}/out" />
@@ -36,22 +44,25 @@ class ProjectRootManagerImplTest : HeavyPlatformTestCase() {
                                    </component>
                                    """.trimIndent())
     impl.loadState(firstLoad)
-    executeSomeCoroutineTasksAndDispatchAllInvocationEvents(myProject)
+    awaitWriteActions()
+
     impl.loadState(secondLoad)
-    executeSomeCoroutineTasksAndDispatchAllInvocationEvents(myProject)
+    awaitWriteActions()
 
     Assertions.assertThat(count).hasValueGreaterThanOrEqualTo(2)
   }
 
+  @Test
   @TestFor(issues = ["IDEA-330499"])
-  fun testNoEventsIfNothingChanged() = runBlocking {
+  fun testNoEventsIfNothingChanged() = timeoutRunBlocking {
+    val project = project.get()
     val count = AtomicInteger(0)
-    ProjectRootManagerEx.getInstanceEx(myProject).addProjectJdkListener {
+    ProjectRootManagerEx.getInstanceEx(project).addProjectJdkListener {
       ThreadingAssertions.assertWriteAccess()
       count.incrementAndGet()
     }
 
-    val impl = getInstanceImpl(myProject)
+    val impl = getInstanceImpl(project)
     val firstLoad = JDOMUtil.load("""
                                         <component name="ProjectRootManager" version="2" languageLevel="JDK_11" default="true" project-jdk-name="corretto-11" project-jdk-type="JavaSDK">
                                           <output url="file://${'$'}PROJECT_DIR${'$'}/out" />
@@ -63,22 +74,27 @@ class ProjectRootManagerImplTest : HeavyPlatformTestCase() {
                                          </component>
                                          """.trimIndent())
     impl.loadState(firstLoad)
-    executeSomeCoroutineTasksAndDispatchAllInvocationEvents(myProject)
     waitUntil {
       1 == count.get()
     }
 
     impl.loadState(secondLoad)
-    executeSomeCoroutineTasksAndDispatchAllInvocationEvents(myProject)
+    awaitWriteActions()
     waitUntil {
       2 == count.get()
     }
 
     impl.loadState(secondLoad)
-    executeSomeCoroutineTasksAndDispatchAllInvocationEvents(myProject)
-    repeat(10) {
-      assertEquals(2, count.get())
+    awaitWriteActions()
+    repeat(5) {
+      org.junit.jupiter.api.Assertions.assertEquals(2, count.get())
       delay(1.seconds)
     }
   }
+
+  private suspend fun awaitWriteActions() {
+    delay(100)
+    readAction { } // custom read action in order to dispatch pending WAs; it will execute after all pending WAs
+  }
+
 }
