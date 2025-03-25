a6e9fd2476dfee17a585789a4db42fcdc727dd22
Boris Krylov <boris.krylov@jetbrains.com>
2025-03-25 22:12:36 +0200

    [syntax] moved LimitedPool class to multiplatform
    GitOrigin-RevId: eebf09e0cdf914cae790bb19e9cd5f4e9d24b46e

--- platform/syntax/syntax-api/src/com/intellij/platform/syntax/runtime/SyntaxGeneratedParserRuntime.kt
+++ platform/syntax/syntax-api/src/com/intellij/platform/syntax/runtime/SyntaxGeneratedParserRuntime.kt
@@ -11,7 +11,6 @@ import com.intellij.platform.syntax.parser.SyntaxTreeBuilder
 import com.intellij.platform.syntax.parser.WhitespacesAndCommentsBinder
 import com.intellij.platform.syntax.parser.WhitespacesBinders
 import com.intellij.platform.syntax.runtime.SyntaxGeneratedParserRuntime.Hook
-import com.intellij.platform.syntax.LimitedPool
 import com.intellij.platform.syntax.Logger
 import com.intellij.platform.syntax.NoopLogger
 import com.intellij.platform.syntax.SyntaxElementTypeSet
@@ -19,6 +18,7 @@ import org.jetbrains.annotations.Contract
 import org.jetbrains.annotations.NonNls
 import com.intellij.platform.syntax.i18n.ResourceBundle;
 import com.intellij.platform.syntax.syntaxElementTypeSetOf
+import com.intellij.util.containers.LimitedPool
 import org.jetbrains.annotations.ApiStatus
 import kotlin.math.min
 
