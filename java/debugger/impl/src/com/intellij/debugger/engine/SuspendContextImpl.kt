f45dbf74eee6d113d9b6c76c849b56f6f852d9c9
Egor Ushakov <egor.ushakov@jetbrains.com>
2025-03-25 18:50:38 +0100

    IDEA-323195 - enabled helper by default in user's evaluations
    GitOrigin-RevId: 4b50d8732836619ff6eb246907b5709bd9d8618e

--- java/debugger/impl/src/com/intellij/debugger/engine/SuspendContextImpl.kt
+++ java/debugger/impl/src/com/intellij/debugger/engine/SuspendContextImpl.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.debugger.engine
 
 import com.intellij.Patches
@@ -390,6 +390,15 @@ abstract class SuspendContextImpl @ApiStatus.Internal constructor(
       val added = myKeptReferences.add(reference)
       if (added) {
         DebuggerUtilsEx.disableCollection(reference)
+      }
+    }
+  }
+
+  fun keepAsync(reference: ObjectReference) {
+    if (!Patches.IBM_JDK_DISABLE_COLLECTION_BUG) {
+      val added = myKeptReferences.add(reference)
+      if (added) {
+        DebuggerUtilsAsync.disableCollection(reference)
       }
     }
   }
