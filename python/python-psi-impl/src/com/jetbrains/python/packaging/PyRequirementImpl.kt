6030f18c30e38a900ea43dd38efdcc35fb212ed6
Aleksandr Sorotskii <aleksandr.sorotskii@jetbrains.com>
2025-03-25 20:32:06 +0100

    cleanup package name normalization; PY-79870
    GitOrigin-RevId: cfc8e220df5d7a2a33b2c027bbf37a7d5055ffc5

--- python/python-psi-impl/src/com/jetbrains/python/packaging/PyRequirementImpl.kt
+++ python/python-psi-impl/src/com/jetbrains/python/packaging/PyRequirementImpl.kt
@@ -14,45 +14,41 @@ import com.jetbrains.python.packaging.requirement.PyRequirementVersionSpec
  * @see PyRequirementParser.fromLine
  * @see PyRequirementParser.fromFile
  */
-data class PyRequirementImpl(
-  private val name: String,
+class PyRequirementImpl(
+  private val presentableName: String,
   private val versionSpecs: List<PyRequirementVersionSpec>,
   private val installOptions: List<String>,
   private val extras: String,
 ) : PyRequirement {
 
-  override fun getName(): String = NormalizedPackageName.from(name).name
+  private val name: String = normalizePackageName(presentableName)
+
+  override fun getName(): String = name
   override fun getExtras(): String = extras
   override fun getVersionSpecs(): List<PyRequirementVersionSpec> = versionSpecs
   override fun getInstallOptions(): List<String> = installOptions
-  override fun getPresentableTextWithoutVersion(): @NlsSafe String = name
+  override fun getPresentableTextWithoutVersion(): @NlsSafe String = presentableName
 
   override fun match(packages: Collection<PyPackage>): PyPackage? {
-    return packages.firstOrNull { pkg ->
-      isPackageNameEqual(pkg.name) && versionSpecs.all { it.matches(pkg.version) }
-    }
+    return packages.firstOrNull { this.match(it) }
   }
 
-  private fun isPackageNameEqual(otherName: String): Boolean {
-    return normalizePackageName(name) == normalizePackageName(otherName)
+  override fun match(packageName: PyPackage): Boolean {
+    return name == normalizePackageName(packageName.name) && versionSpecs
+      .all { it.matches(packageName.version) }
   }
 
   override fun equals(other: Any?): Boolean {
-    if (this === other) return true
+    if (this === other) {
+      return true
+    }
+
     return when (other) {
-      is String -> getName() == NormalizedPackageName.from(other).name
-      is PyRequirementImpl -> getName().equals(other.getName(), ignoreCase = true)
+      is String -> name == normalizePackageName(other)
+      is PyRequirementImpl -> name == other.name // TODO: should we match specs & options ?
       else -> false
     }
   }
 
-  override fun hashCode(): Int = normalizePackageName(name).hashCode()
-
-  @JvmInline
-  value class NormalizedPackageName private constructor(val name: String) {
-    companion object {
-      fun from(name: String): NormalizedPackageName =
-        NormalizedPackageName(normalizePackageName(name))
-    }
-  }
+  override fun hashCode(): Int = name.hashCode()
 }
