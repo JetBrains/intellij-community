0b6eaba94280f20c92e775f7abf2d93a55796a12
Dmitriy.Panov <dmitriy.panov@jetbrains.com>
2025-03-26 16:08:31 +0100

    GitLab plugin: semantic versioning
    IJPL-178771 IJPL-180097
    
    GitOrigin-RevId: 1734dc16a231f8e0742f82cb0f27b59ea71a4e13

--- platform/build-scripts/src/org/jetbrains/intellij/build/CommunityRepositoryModules.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/CommunityRepositoryModules.kt
@@ -643,7 +643,7 @@ object CommunityRepositoryModules {
       spec.directoryName = "vcs-gitlab-$productCode"
       spec.mainJarName = "vcs-gitlab.jar"
       spec.withCustomVersion { _, version, _ ->
-        PluginVersionEvaluatorResult(pluginVersion = "$productCode-$version")
+        PluginVersionEvaluatorResult(pluginVersion = "$version-$productCode")
       }
     }
   }
