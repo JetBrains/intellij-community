d82c1ce288256f54e405b57dd9a0e335c70f25bf
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2025-03-26 21:39:57 +0100

    IJPL-181128 Provide import settings from Cursor and Windsurf
    (cherry picked from commit fb475bd796f7236791e03005f0e7dd02f153c508)
    
    IJ-CR-158529
    
    GitOrigin-RevId: 3a04ae05d7a60f6ace4fbcf672ba70cab0a01ea9

--- plugins/ide-startup/importSettings/src/com/intellij/ide/startup/importSettings/transfer/VsCodeSettingsTransfer.kt
+++ plugins/ide-startup/importSettings/src/com/intellij/ide/startup/importSettings/transfer/VsCodeSettingsTransfer.kt
@@ -2,6 +2,8 @@
 package com.intellij.ide.startup.importSettings.transfer
 
 import com.intellij.ide.startup.importSettings.providers.vscode.VSCodeTransferSettingsProvider
+import com.intellij.ide.startup.importSettings.transfer.backend.providers.cursor.CursorTransferSettingsProvider
+import com.intellij.ide.startup.importSettings.transfer.backend.providers.windsurf.WindsurfTransferSettingsProvider
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import kotlinx.coroutines.CoroutineScope
@@ -10,7 +12,7 @@ class VsCodeSettingsTransfer : ThirdPartyProductSettingsTransfer {
 
   override fun getProviders(): List<VSCodeTransferSettingsProvider> {
     val scope = service<ScopeHolder>().scope
-    return listOf(VSCodeTransferSettingsProvider(scope))
+    return listOf(VSCodeTransferSettingsProvider(scope), CursorTransferSettingsProvider(scope), WindsurfTransferSettingsProvider(scope))
   }
 }
 
