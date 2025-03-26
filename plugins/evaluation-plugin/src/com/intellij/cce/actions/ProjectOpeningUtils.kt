790e782c314a7ce009b13e22b5882291aaaec2e0
Edem Ibraimov <edem.ibraimov@jetbrains.com>
2025-03-26 16:06:55 +0500

    LLM-10738: add LLM_BASED SnippetApplicationBehaviour to the eval
    GitOrigin-RevId: 689d815d3f0f62c7388b92d1b446be857fbc0e7d

--- plugins/evaluation-plugin/src/com/intellij/cce/actions/ProjectOpeningUtils.kt
+++ plugins/evaluation-plugin/src/com/intellij/cce/actions/ProjectOpeningUtils.kt
@@ -1,24 +1,23 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.cce.actions
 
-import com.intellij.ide.highlighter.ProjectFileType
-import com.intellij.ide.impl.OpenProjectTask
+import com.intellij.ide.impl.runUnderModalProgressIfIsEdt
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ex.ProjectManagerEx
-import com.intellij.openapi.project.impl.ProjectImpl
 import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.warmup.util.OpenProjectArgs
+import com.intellij.warmup.util.importOrOpenProjectAsync
 import java.nio.file.Path
-import java.nio.file.Paths
 
 object ProjectOpeningUtils {
 
   fun trivialProject(): Project {
-    val tempDirectory = Paths.get(FileUtilRt.getTempDirectory()).resolve(ProjectImpl.LIGHT_PROJECT_NAME + ProjectFileType.DOT_DEFAULT_EXTENSION)
-    val task = OpenProjectTask()
-    return ProjectManagerEx.getInstanceEx().newProject(tempDirectory, task)!!
+    val emptyDir = FileUtilRt.createTempDirectory("trivial", null).toPath()
+    return runUnderModalProgressIfIsEdt {
+      importOrOpenProjectAsync(OpenProjectArgsData(emptyDir))
+    }
   }
 
   fun closeProject(project: Project) {
