d87967a74676bb0807b803182f7dcffd8de44c02
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-03-26 18:02:27 +0100

    [platform] Remove unused import
    GitOrigin-RevId: 50d5b2f86b3c5bf234b119e3f724bf301c9bbd5e

--- platform/kernel/backend/src/ids/BackendGlobalIds.kt
+++ platform/kernel/backend/src/ids/BackendGlobalIds.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.kernel.backend.ids
 
-import com.intellij.platform.kernel.backend.delete
 import com.intellij.platform.rpc.Id
 import kotlinx.coroutines.CoroutineScope
 import org.jetbrains.annotations.ApiStatus
