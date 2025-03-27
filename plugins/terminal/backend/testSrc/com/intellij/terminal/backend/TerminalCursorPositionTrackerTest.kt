e1d524dbca9c2b3c3d401e09820e53bdbc752b00
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-03-27 12:28:53 +0200

    [terminal] IJ-MR-158015 Implement BackendOutputTestFusActivity stub
    We don't test FUS in these tests, so an empty implementation will do.
    
    (cherry picked from commit fe87779a81b3a14a2b10418f3bae18f8b9d6b505)
    
    IJ-CR-160384
    
    GitOrigin-RevId: be156f60c206cb976b4e44ce53fc8b2f02405fc8

--- plugins/terminal/backend/testSrc/com/intellij/terminal/backend/TerminalCursorPositionTrackerTest.kt
+++ plugins/terminal/backend/testSrc/com/intellij/terminal/backend/TerminalCursorPositionTrackerTest.kt
@@ -1,9 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.terminal.backend
 
-import com.intellij.terminal.backend.util.backendOutputTestFusActivity
-import com.intellij.terminal.backend.util.startTestFusActivity
-import com.intellij.terminal.backend.util.stopTestFusActivity
+import com.intellij.terminal.backend.util.BackendOutputTestFusActivity
 import com.intellij.terminal.backend.util.write
 import com.intellij.terminal.session.TerminalContentUpdatedEvent
 import com.intellij.terminal.session.TerminalCursorPositionChangedEvent
@@ -11,27 +9,15 @@ import com.jediterm.terminal.model.StyleState
 import com.jediterm.terminal.model.TerminalTextBuffer
 import com.jediterm.terminal.ui.settings.DefaultSettingsProvider
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.After
-import org.junit.Before
 import org.junit.Test
 
 internal class TerminalCursorPositionTrackerTest {
-  @Before
-  fun setUp() {
-    startTestFusActivity()
-  }
-
-  @After
-  fun tearDown() {
-    stopTestFusActivity()
-  }
-
   @Test
   fun `cursor position should correspond to existing line in the TextBuffer`() {
     val textBuffer = TerminalTextBuffer(10, 5, StyleState(), 3)
     val discardedHistoryTracker = TerminalDiscardedHistoryTracker(textBuffer)
     val terminalDisplay = TerminalDisplayImpl(DefaultSettingsProvider())
-    val contentChangesTracker = TerminalContentChangesTracker(textBuffer, discardedHistoryTracker, backendOutputTestFusActivity!!)
+    val contentChangesTracker = TerminalContentChangesTracker(textBuffer, discardedHistoryTracker, BackendOutputTestFusActivity)
     val cursorPositionTracker = TerminalCursorPositionTracker(textBuffer, discardedHistoryTracker, terminalDisplay)
 
     // Prepare
@@ -41,17 +27,15 @@ internal class TerminalCursorPositionTrackerTest {
     cursorPositionTracker.getCursorPositionUpdate()
 
     // Test: for example, the user pressed enter, and we move the cursor to the next line.
-    backendOutputTestFusActivity!!.charProcessingStarted()
     terminalDisplay.setCursor(0, 2)
-    backendOutputTestFusActivity!!.charProcessingFinished()
 
     val contentUpdate = contentChangesTracker.getContentUpdate() ?: error("Content update is null")
     val cursorUpdate = cursorPositionTracker.getCursorPositionUpdate() ?: error("Cursor update is null")
 
     // We expect that moving cursor position to the next line creates this line in the TextBuffer,
     // and it is being caught by the TerminalContentChangesTracker.
-    // The strange 6/5 combination of character indices corresponds to the cursor position change without any characters read.
-    assertThat(contentUpdate).isEqualTo(TerminalContentUpdatedEvent("", emptyList(), 1, 6L, 5L))
+    // The character indices correspond to LongRange.EMPTY used in the test FUS activity stub.
+    assertThat(contentUpdate).isEqualTo(TerminalContentUpdatedEvent("", emptyList(), 1, 1L, 0L))
     assertThat(cursorUpdate).isEqualTo(TerminalCursorPositionChangedEvent(1, 0))
   }
 }
