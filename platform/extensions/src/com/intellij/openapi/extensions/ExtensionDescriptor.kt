a8f7a2dfe0fb962c40649577557a095409d2848d
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-03-27 18:55:54 +0100

    [plugins] IJPL-181065 extract ExtensionPointDescriptor.kt
    GitOrigin-RevId: c3c6880858bd3bfc650067ee6e727356db70d386

--- platform/extensions/src/com/intellij/openapi/extensions/ExtensionDescriptor.kt
+++ platform/extensions/src/com/intellij/openapi/extensions/ExtensionDescriptor.kt
@@ -31,31 +31,3 @@ class ExtensionDescriptor(
     }
   }
 }
-
-@ApiStatus.Internal
-class ExtensionPointDescriptor(@JvmField val name: String,
-                               @JvmField val isNameQualified: Boolean,
-                               @JvmField val className: String,
-                               @JvmField val isBean: Boolean,
-                               @JvmField val hasAttributes: Boolean,
-                               @JvmField val isDynamic: Boolean) {
-  fun getQualifiedName(pluginDescriptor: PluginDescriptor): String = if (isNameQualified) name else "${pluginDescriptor.pluginId}.$name"
-
-  // getQualifiedName() can be used instead, but this method allows avoiding temp string creation
-  fun nameEquals(qualifiedName: String, pluginDescriptor: PluginDescriptor): Boolean {
-    if (isNameQualified) {
-      return qualifiedName == name
-    }
-    else {
-      val pluginId = pluginDescriptor.pluginId.idString
-      return (qualifiedName.length == (pluginId.length + 1 + name.length)) &&
-             qualifiedName[pluginId.length] == '.' &&
-             qualifiedName.startsWith(pluginId) &&
-             qualifiedName.endsWith(name)
-    }
-  }
-
-  override fun toString(): String {
-    return "ExtensionPointDescriptor(name=$name, isNameQualified=$isNameQualified, className=$className, isBean=$isBean, isDynamic=$isDynamic)"
-  }
-}
