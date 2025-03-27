8179fd0793edbe3a8a335a7780c6e6b1eb26a1bb
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2025-03-27 13:19:28 +0100

    [git] Convert GitSpecialRefRemoteBranch to Kt
    GitOrigin-RevId: 61a5fe6d91e55c9c29554bf7b670172b36d89727

--- plugins/git4idea/src/git4idea/push/GitSpecialRefRemoteBranch.kt
+++ plugins/git4idea/src/git4idea/push/GitSpecialRefRemoteBranch.kt
@@ -1,33 +1,20 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package git4idea.push;
+package git4idea.push
 
-import git4idea.GitRemoteBranch;
-import git4idea.repo.GitRemote;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.util.NlsSafe
+import git4idea.GitRemoteBranch
+import git4idea.repo.GitRemote
 
 /**
  * Semi-fake remote branch if pushing to special push specs like "HEAD:refs/for/master".
  */
-public class GitSpecialRefRemoteBranch extends GitRemoteBranch {
-  private final String myRef;
+class GitSpecialRefRemoteBranch(private val ref: String, remote: GitRemote) : GitRemoteBranch(ref, remote) {
+  override val nameForRemoteOperations: @NlsSafe String
+    get() = ref
 
-  public GitSpecialRefRemoteBranch(@NotNull String ref, @NotNull GitRemote remote) {
-    super(ref, remote);
-    myRef = ref;
-  }
+  override val nameForLocalOperations: @NlsSafe String
+    get() = ref
 
-  @Override
-  public @NotNull String getNameForRemoteOperations() {
-    return myRef;
-  }
-
-  @Override
-  public @NotNull String getNameForLocalOperations() {
-    return myRef;
-  }
-
-  @Override
-  public @NotNull String getFullName() {
-    return myRef;
-  }
+  override val fullName: @NlsSafe String
+    get() = ref
 }
