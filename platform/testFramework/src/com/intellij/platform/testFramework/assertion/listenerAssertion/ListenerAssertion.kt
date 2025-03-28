90e6f37f08e7f7d1433b4211a3deed9b45cc0bcf
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2025-03-28 17:16:27 +0100

    [TestFramework] IJ-CR-159827 fix: move ListenerAssertion from the external system to the platform test framework
    (cherry picked from commit 79011bc79b63fef6f25bdb55948b0277493cdc17)
    
    GitOrigin-RevId: a3a6b1dd4a752c910e073a3689373257e238edba

--- platform/testFramework/src/com/intellij/platform/testFramework/assertion/listenerAssertion/ListenerAssertion.kt
+++ platform/testFramework/src/com/intellij/platform/testFramework/assertion/listenerAssertion/ListenerAssertion.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.externalSystem.util
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.testFramework.assertion.listenerAssertion
 
 import org.junit.jupiter.api.Assertions
 import org.opentest4j.AssertionFailedError
