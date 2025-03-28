73d8824983a091ea6a22e15510cdc21a590d24bb
Dmitry Kozhevnikov <dmitry.kozhevnikov@jetbrains.com>
2025-03-28 20:34:38 +0200

    [idea] Add Kotlin as primary language for IDEA
    This makes it appear next to Java in various settings like code style.
    
    Popped up when fixing CPP-39708
    
    GitOrigin-RevId: 985316fc18a9d44d03e237850527bec8f679e797

--- java/java-impl/src/com/intellij/ide/java/JavaIdeLanguageCustomization.java
+++ java/java-impl/src/com/intellij/ide/java/JavaIdeLanguageCustomization.java
@@ -4,14 +4,19 @@ package com.intellij.ide.java;
 import com.intellij.lang.IdeLanguageCustomization;
 import com.intellij.lang.Language;
 import com.intellij.lang.java.JavaLanguage;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public final class JavaIdeLanguageCustomization extends IdeLanguageCustomization {
   @Override
   public @NotNull List<Language> getPrimaryIdeLanguages() {
-    return Collections.singletonList(JavaLanguage.INSTANCE);
+    return ContainerUtil.addAllNotNull(
+      new ArrayList<>(),
+      JavaLanguage.INSTANCE,
+      Language.findLanguageByID("kotlin")
+    );
   }
 }
