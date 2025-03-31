2fdf97378bb0da3b0b5765b62d6f1ce57025ce20
sebastian.chiriac <sebastian.chiriac@jetbrains.com>
2025-04-01 01:48:18 +0200

    LLM-13849: added custom logic for selecting positions in text completion evaluation
    GitOrigin-RevId: 6696c86ec045675b182b0c07b56b65e272d23c35

--- plugins/evaluation-plugin/languages/java/src/com/intellij/cce/java/visitor/JavaLineCompletionEvaluationVisitor.kt
+++ plugins/evaluation-plugin/languages/java/src/com/intellij/cce/java/visitor/JavaLineCompletionEvaluationVisitor.kt
@@ -8,20 +8,14 @@ import com.intellij.cce.visitor.LineCompletionEvaluationVisitor
 import com.intellij.cce.visitor.LineCompletionVisitorFactory
 import com.intellij.cce.visitor.LineCompletionVisitorHelper
 import com.intellij.psi.*
-import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.impl.source.tree.java.PsiLiteralExpressionImpl
-import com.intellij.psi.javadoc.PsiDocComment
-import com.intellij.psi.util.parentOfType
 
 class JavaLineCompletionVisitorFactory : LineCompletionVisitorFactory {
   override val language: Language = Language.JAVA
   override fun createVisitor(featureName: String, mode: CompletionGolfMode): LineCompletionEvaluationVisitor =
-    when(featureName) {
-        "text-completion" -> CommentsTokensVisitor(featureName)
-        else -> when (mode) {
-          CompletionGolfMode.ALL -> throw UnsupportedOperationException("Completion Golf mode \"ALL\" is not supported for Java completion.")
-        CompletionGolfMode.TOKENS -> TokensVisitor(featureName)
-      }
+    when (mode) {
+      CompletionGolfMode.ALL -> throw UnsupportedOperationException("Completion Golf mode \"ALL\" is not supported for Java completion.")
+      CompletionGolfMode.TOKENS -> TokensVisitor(featureName)
     }
 
   class TokensVisitor(override val feature: String) : LineCompletionEvaluationVisitor, JavaRecursiveElementVisitor() {
@@ -76,34 +70,4 @@ class JavaLineCompletionVisitorFactory : LineCompletionVisitorFactory {
     override fun visitImportStaticStatement(statement: PsiImportStaticStatement) = Unit
     override fun visitComment(comment: PsiComment) = Unit
   }
-
-  class CommentsTokensVisitor(override val feature: String) : LineCompletionEvaluationVisitor, JavaRecursiveElementVisitor() {
-    private val visitorHelper = LineCompletionVisitorHelper()
-
-    override val language: Language = Language.JAVA
-
-    override fun getFile(): CodeFragment = visitorHelper.getFile()
-
-    override fun visitJavaFile(file: PsiJavaFile) {
-      visitorHelper.visitFile(file)
-      super.visitJavaFile(file)
-    }
-
-    private fun isCommentElement(element: PsiElement): Boolean {
-      return element is PsiComment;
-    }
-
-    private fun isDocComment(element: PsiElement): Boolean {
-      return element.node.elementType == JavaTokenType.C_STYLE_COMMENT ||
-             (element is LeafPsiElement && element.parentOfType<PsiDocComment>(withSelf = true) != null)
-    }
-
-    override fun visitElement(element: PsiElement) {
-      if (isCommentElement(element) || isDocComment(element)) {
-        visitorHelper.addElement(element.node)
-      }
-      super.visitElement(element)
-    }
-  }
 }
-
