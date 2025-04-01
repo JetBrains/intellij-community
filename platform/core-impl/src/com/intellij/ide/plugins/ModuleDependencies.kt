e229df242f77d70c254d0830561eee4aa269cff9
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-04-01 17:14:46 +0200

    [plugins] IJPL-181065 extract PluginContentDescriptor.kt
    GitOrigin-RevId: 55eb8979c19c5278e0dfc1bcf7f7fd3b0eeae600

--- platform/core-impl/src/com/intellij/ide/plugins/ModuleDependencies.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/ModuleDependencies.kt
@@ -2,9 +2,7 @@
 package com.intellij.ide.plugins
 
 import com.intellij.openapi.extensions.PluginId
-import com.intellij.util.Java11Shim
 import org.jetbrains.annotations.ApiStatus
-import org.jetbrains.annotations.TestOnly
 import java.util.Collections
 
 /**
@@ -25,31 +23,4 @@ class ModuleDependencies(@JvmField val modules: List<ModuleReference>, @JvmField
   }
 
   override fun toString(): String = "ModuleDependencies(modules=$modules, plugins=$plugins)"
-}
-
-@ApiStatus.Internal
-class PluginContentDescriptor(@JvmField val modules: List<ModuleItem>) {
-  companion object {
-    @JvmField val EMPTY: PluginContentDescriptor = PluginContentDescriptor(Java11Shim.INSTANCE.listOf())
-  }
-
-  @ApiStatus.Internal
-  class ModuleItem(
-    @JvmField val name: String,
-    @JvmField val configFile: String?,
-    @JvmField internal val descriptorContent: CharArray?,
-    @JvmField val loadingRule: ModuleLoadingRule,
-  ) {
-    @JvmField
-    internal var descriptor: IdeaPluginDescriptorImpl? = null
-
-    fun requireDescriptor(): IdeaPluginDescriptorImpl = descriptor ?: throw IllegalStateException("Descriptor is not set for $this")
-
-    @TestOnly
-    fun getDescriptorOrNull(): IdeaPluginDescriptorImpl? = descriptor
-
-    override fun toString(): String = "ModuleItem(name=$name, descriptor=$descriptor, configFile=$configFile)"
-  }
-
-  override fun toString(): String = "PluginContentDescriptor(modules=$modules)"
 }
