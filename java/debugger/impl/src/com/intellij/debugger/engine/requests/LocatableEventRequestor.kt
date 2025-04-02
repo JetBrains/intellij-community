fbeb351d2ea32be93305b82d8c8e95a65cee8201
Maria Sokolova <maria.sokolova@jetbrains.com>
2025-04-02 18:13:26 +0200

    [debugger] Translated LocatableEventRequestor.java to Kotlin
    GitOrigin-RevId: 613b9909c3c3c97ebc0fd5959c304f2eab5100d9

--- java/debugger/impl/src/com/intellij/debugger/engine/requests/LocatableEventRequestor.kt
+++ java/debugger/impl/src/com/intellij/debugger/engine/requests/LocatableEventRequestor.kt
@@ -1,31 +1,21 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.debugger.engine.requests;
+package com.intellij.debugger.engine.requests
 
-import com.intellij.debugger.engine.events.SuspendContextCommandImpl;
-import com.intellij.openapi.util.NlsContexts;
-import com.sun.jdi.event.LocatableEvent;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.debugger.engine.events.SuspendContextCommandImpl
+import com.intellij.openapi.util.NlsContexts
+import com.sun.jdi.event.LocatableEvent
 
-public interface LocatableEventRequestor extends SuspendingRequestor {
+interface LocatableEventRequestor : SuspendingRequestor {
   /**
-   * @return true if request was hit by the event, false otherwise
+   * @return true if the request was hit by the event, false otherwise
    */
-  boolean processLocatableEvent(@NotNull SuspendContextCommandImpl action, LocatableEvent event) throws EventProcessingException;
+  @Throws(EventProcessingException::class)
+  fun processLocatableEvent(action: SuspendContextCommandImpl, event: LocatableEvent?): Boolean
 
-  default boolean shouldIgnoreThreadFiltering() {
-    return false;
+  fun shouldIgnoreThreadFiltering(): Boolean {
+    return false
   }
 
-  class EventProcessingException extends Exception {
-    private final @NlsContexts.DialogTitle String myTitle;
-
-    public EventProcessingException(@NlsContexts.DialogTitle String title, String message, Throwable cause) {
-      super(message, cause);
-      myTitle = title;
-    }
-
-    public @NlsContexts.DialogTitle String getTitle() {
-      return myTitle;
-    }
-  }
+  class EventProcessingException(val title: @NlsContexts.DialogTitle String, message: String, cause: Throwable)
+    : Exception(message, cause)
 }
