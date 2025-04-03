ef8332f0fb08df457797654e21f7f3d4012f7254
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2025-04-03 10:51:56 +0200

    [java-inspections] IDEA-370073 incorrect field type
    GitOrigin-RevId: 0c256aef706e7674d123ee2fd00278a7e17a2ccb

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createFieldFromUsage/afterReferenceNull.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createFieldFromUsage/afterReferenceNull.java
@@ -1,10 +1,9 @@
 // "Create constant field 'MODULE' in 'Usage'" "true"
 class Usage {
-
     private static final Object MODULE = ;
 
     void usage() {
-    switch (type) {
+    switch (null) {
       case CLASS -> symbols.add(1);
       case INTERFACE -> symbols.add(2);
       case MODULE -> symbols.add(42);
--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createFieldFromUsage/beforeReferenceNull.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createFieldFromUsage/beforeReferenceNull.java
@@ -1,8 +1,7 @@
 // "Create constant field 'MODULE' in 'Usage'" "true"
 class Usage {
-
   void usage() {
-    switch (type) {
+    switch (null) {
       case CLASS -> symbols.add(1);
       case INTERFACE -> symbols.add(2);
       case <caret>MODULE -> symbols.add(42);
