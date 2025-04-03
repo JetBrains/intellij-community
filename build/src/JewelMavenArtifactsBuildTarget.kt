6fdb06a5cb61e105524e993d79e4ffbb21fa8736
Dmitriy.Panov <dmitriy.panov@jetbrains.com>
2025-04-03 16:48:14 +0200

    IJI-2358 Jewel Maven artifacts build target
    GitOrigin-RevId: d333b78045e444cff39e9b784141cb21f8c38900

---
+++ build/src/JewelMavenArtifactsBuildTarget.kt
@@ -0,0 +1,33 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.intellij.build.BuildPaths.Companion.COMMUNITY_ROOT
+import org.jetbrains.intellij.build.IdeaCommunityProperties
+import org.jetbrains.intellij.build.JewelMavenArtifacts
+import org.jetbrains.intellij.build.impl.BuildContextImpl
+import org.jetbrains.intellij.build.impl.maven.MavenArtifactsBuilder
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.deleteRecursively
+import kotlin.io.path.name
+
+@OptIn(ExperimentalPathApi::class)
+internal object JewelMavenArtifactsBuildTarget {
+  @JvmStatic
+  fun main(args: Array<String>) {
+    runBlocking(Dispatchers.Default) {
+      val context = BuildContextImpl.createContext(
+        projectHome = COMMUNITY_ROOT.communityRoot,
+        productProperties = IdeaCommunityProperties(COMMUNITY_ROOT.communityRoot),
+      )
+      val builder = MavenArtifactsBuilder(context)
+      val outputDir = context.paths.artifactDir.resolve("maven-artifacts")
+      outputDir.deleteRecursively()
+      builder.generateMavenArtifacts(
+        JewelMavenArtifacts.CORE.keys + JewelMavenArtifacts.STANDALONE.keys,
+        outputDir = outputDir.name,
+        validate = true,
+      )
+      context.notifyArtifactBuilt(outputDir)
+    }
+  }
+}
