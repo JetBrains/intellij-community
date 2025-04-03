1854b413e8b45382769aa9fe162c1c8e15dc4c40
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-04-03 17:40:38 +0200

    IJPL-162853 more docs for i18n in syntax lib
    GitOrigin-RevId: 81c494ad571d809875c6330676e59985ed6d4d2b

---
+++ platform/syntax/syntax-i18n/README.md
@@ -0,0 +1,23 @@
+# Syntax i18n Library
+
+Kotlin-multiplatform compatible ResourceBundle implementation for using in syntax implementations
+ 
+Example of usage:
+
+```kotlin
+object JavaSyntaxBundle {
+  const val BUNDLE: @NonNls String = "messages.JavaSyntaxBundle"
+
+  val resourceBundle: ResourceBundle = ResourceBundle("com.intellij.java.syntax.JavaSyntaxBundle", BUNDLE, this)
+
+  @JvmStatic
+  fun message(key: @PropertyKey(resourceBundle = BUNDLE) String, vararg params: Any): @Nls String {
+    return resourceBundle.message(key, *params)
+  }
+
+  @JvmStatic
+  fun messagePointer(key: @PropertyKey(resourceBundle = BUNDLE) String, vararg params: Any): () -> @Nls String {
+    return resourceBundle.messagePointer(key, *params)
+  }
+}
+```
--- platform/syntax/syntax-i18n/src/com/intellij/platform/syntax/i18n/ResourceBundle.kt
+++ platform/syntax/syntax-i18n/src/com/intellij/platform/syntax/i18n/ResourceBundle.kt
@@ -1,5 +1,6 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 @file: ApiStatus.Experimental
+
 package com.intellij.platform.syntax.i18n
 
 import fleet.util.multiplatform.linkToActual
@@ -9,11 +10,15 @@ import org.jetbrains.annotations.Nls
 @ApiStatus.Experimental
 interface ResourceBundle {
   fun message(key: String, vararg params: Any): @Nls String
+
   fun messagePointer(key: String, vararg params: Any): () -> @Nls String
 }
 
 /**
- * see [com.intellij.platform.syntax.i18n.JvmResourceBundle]
+ * see [com.intellij.platform.syntax.i18n.JvmResourceBundleKt.ResourceBundleJvm]
+ * @param bundleClass   the class of bundle, usually an object class where the bundle is declared
+ * @param pathToBundle  usually, something like `messages.MyBundle`
+ * @param self          pass `this` please
  */
 @Suppress("unused")
 fun ResourceBundle(
