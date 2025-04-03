52f3cd805c7f3b1d985507310a5eba4e6848c0a2
Andrei.Kuznetsov <andrei.kuznetsov@jetbrains.com>
2025-04-03 19:06:41 +0200

    IJPL-2920 Cancel indexing of excluded folder (under registry key indexing.double.check.files.still.in.project)
    GitOrigin-RevId: 767425c86a3d71e4742be50ef4c0b26000fdb643

--- platform/lang-impl/src/com/intellij/util/indexing/contentQueue/IndexUpdateRunner.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/contentQueue/IndexUpdateRunner.kt
@@ -4,6 +4,7 @@ package com.intellij.util.indexing.contentQueue
 import com.intellij.openapi.application.readAction
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.ThrottledLogger
+import com.intellij.openapi.diagnostic.debug
 import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.progress.Cancellation
 import com.intellij.openapi.progress.ProcessCanceledException
@@ -12,6 +13,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.io.FileUtilRt
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.InvalidVirtualFileAccessException
 import com.intellij.openapi.vfs.VfsUtil
@@ -38,6 +40,7 @@ import com.intellij.util.indexing.diagnostic.IndexStatisticGroup
 import com.intellij.util.indexing.diagnostic.IndexingFileSetStatistics
 import com.intellij.util.indexing.diagnostic.ProjectDumbIndexingHistoryImpl
 import com.intellij.util.indexing.events.FileIndexingRequest
+import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileIndex
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import org.jetbrains.annotations.ApiStatus
@@ -265,7 +268,7 @@ class IndexUpdateRunner(
     private val fileBasedIndex: FileBasedIndexImpl,
     private val indexingRequest: IndexingRequestToken,
   ) {
-
+    private val doubleCheckFilesAreStillInProject = Registry.`is`("indexing.double.check.files.still.in.project", false)
     private val indexingAttemptCount = AtomicInteger()
     private val indexingSuccessfulCount = AtomicInteger()
 
@@ -282,6 +285,18 @@ class IndexUpdateRunner(
         //  here with an invalid file, but in a (badly isolated) tests it could happen
         LOG.warn("Invalid (alien?) file: #${(file as VirtualFileWithId).id}")
         return
+      }
+
+      // This does not work for every language. JS, for example, expects that IDE is indexing files outside the project.
+      // See com.intellij.flex.completion.ActionScriptCompletionTest.testSOE
+      // which indexes file out/classes/production/intellij.flex/com/intellij/lang/javascript/flex/library/ECMAScript.js2
+      if (doubleCheckFilesAreStillInProject && !fileIndexingRequest.isDeleteRequest) {
+        // we check workspace, not content, because content does not contain libraries
+        val stillInWorkspace = readAction { WorkspaceFileIndex.getInstance(project).isInWorkspace(file) }
+        if (!stillInWorkspace) {
+          LOG.debug { "File is not in workspace anymore: #${(file as VirtualFileWithId).id}" }
+          return
+        }
       }
 
       // snapshot at the beginning: if file changes while being processed, we can detect this on the following scanning
