078fd78cc8b17a2f837065bd20dd0cd85ef45301
Tatiana Elfimova <tatiana.elfimova@jetbrains.com>
2025-04-03 17:06:54 +0200

    ABT-37 Define an ide version for `FuzzyFileSearchExperimentOption`
    GitOrigin-RevId: 4ce221fb20b805996c1a9e6f5fa10124b75fe29d

--- platform/lang-impl/src/com/intellij/ide/util/gotoByName/FuzzyFileSearchExperimentOption.kt
+++ platform/lang-impl/src/com/intellij/ide/util/gotoByName/FuzzyFileSearchExperimentOption.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.util.gotoByName
 
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.util.registry.Registry.Companion.`is`
 import com.intellij.platform.experiment.ab.impl.experiment.ABExperiment
 import com.intellij.platform.experiment.ab.impl.experiment.ABExperimentOption
@@ -16,7 +17,10 @@ internal class FuzzyFileSearchExperimentOption : ABExperimentOption {
 
   override fun checkIdeIsSuitable(): Boolean = true
 
-  override fun checkIdeVersionIsSuitable(): Boolean = true
+  override fun checkIdeVersionIsSuitable(): Boolean {
+    val appInfo = ApplicationInfo.getInstance()
+    return appInfo.isEAP && appInfo.majorVersion == "2025" && appInfo.minorVersion.let { it == "2" || it == "2.0" }
+  }
 
   companion object {
     @JvmStatic
