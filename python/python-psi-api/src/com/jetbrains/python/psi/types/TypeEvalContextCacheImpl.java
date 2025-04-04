b619e21989bfe469f759127fc144079c100628ae
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2025-04-04 17:28:58 +0300

    [python] Remove TypeEvalContextBasedCache
    GitOrigin-RevId: 74d87561c3cc857ede78e60f19a7edbad35f29ee

--- python/python-psi-api/src/com/jetbrains/python/psi/types/TypeEvalContextCacheImpl.java
+++ python/python-psi-api/src/com/jetbrains/python/psi/types/TypeEvalContextCacheImpl.java
@@ -2,9 +2,14 @@
 package com.jetbrains.python.psi.types;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.util.CachedValue;
+import com.intellij.psi.util.CachedValueProvider;
 import com.intellij.psi.util.CachedValuesManager;
-import com.intellij.util.Function;
+import com.intellij.psi.util.PsiModificationTracker;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Caches context by their constraints (to prevent context cache loss). Flushes cache every PSI change or low memory conditions.
@@ -14,23 +19,32 @@ import org.jetbrains.annotations.NotNull;
  * @author Ilya.Kazakevich
  */
 final class TypeEvalContextCacheImpl implements TypeEvalContextCache {
-  private static final @NotNull Function<TypeEvalContext, TypeEvalContext> VALUE_PROVIDER = new MyValueProvider();
-  private final @NotNull TypeEvalContextBasedCache<TypeEvalContext> myCache;
+  private final @NotNull CachedValue<ConcurrentMap<TypeEvalConstraints, TypeEvalContext>> myCachedMapStorage;
 
   TypeEvalContextCacheImpl(@NotNull Project project) {
-    myCache = new TypeEvalContextBasedCache<>(CachedValuesManager.getManager(project), VALUE_PROVIDER);
+    myCachedMapStorage = CachedValuesManager.getManager(project).createCachedValue(new CachedValueProvider<>() {
+      @Override
+      public @NotNull CachedValueProvider.Result<ConcurrentMap<TypeEvalConstraints, TypeEvalContext>> compute() {
+        // This method is called if cache is empty. Create new map for it.
+        // Concurrent map allows several threads to call get and put, so it is thread safe but not atomic
+        final ConcurrentMap<TypeEvalConstraints, TypeEvalContext> map = ContainerUtil.createConcurrentSoftValueMap();
+        return new CachedValueProvider.Result<>(map, PsiModificationTracker.MODIFICATION_COUNT);
+      }
+    });
   }
 
   @Override
-  public @NotNull TypeEvalContext getContext(final @NotNull TypeEvalContext standard) {
-    return myCache.getValue(standard);
-  }
-
-  private static class MyValueProvider implements Function<TypeEvalContext, TypeEvalContext> {
-    @Override
-    public TypeEvalContext fun(final TypeEvalContext param) {
-      // key and value are both context here. If no context stored, then key is stored. Old one is returned otherwise to cache.
-      return param;
+  public @NotNull TypeEvalContext getContext(@NotNull TypeEvalContext standard) {
+    // map is thread safe but not atomic nor getValue() is, so in worst case several threads may produce same result
+    // both explicit locking and computeIfAbsent leads to deadlock
+    final ConcurrentMap<TypeEvalConstraints, TypeEvalContext> map = myCachedMapStorage.getValue();
+    final TypeEvalConstraints key = standard.getConstraints();
+    final TypeEvalContext cachedContext = map.get(key);
+    if (cachedContext != null) {
+      return cachedContext;
     }
+    TypeEvalContext oldValue =
+      map.putIfAbsent(key, standard);// ConcurrentMap guarantees happens-before so from this moment get() should work in other threads
+    return oldValue == null ? standard : oldValue;
   }
 }
