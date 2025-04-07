7901b255c76a51661bb63a7d289ba30f45d6d537
Egor Ushakov <egor.ushakov@jetbrains.com>
2025-04-07 16:22:47 +0200

    IDEA-370410 Support thread dumps for openj9
    GitOrigin-RevId: d9c674ccf2e1d7f6e03679a0b1d3b372d2b88fae

--- java/execution/impl/src/com/intellij/execution/impl/ThreadDumpProvider.java
+++ java/execution/impl/src/com/intellij/execution/impl/ThreadDumpProvider.java
@@ -2,8 +2,13 @@
 package com.intellij.execution.impl;
 
 import com.intellij.debugger.impl.attach.JavaDebuggerAttachUtil;
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.process.ProcessOutput;
+import com.intellij.execution.util.ExecUtil;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.io.StreamUtil;
 import com.intellij.util.ArrayUtil;
 import com.sun.tools.attach.AttachNotSupportedException;
@@ -14,16 +19,53 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public abstract class ThreadDumpProvider {
   static final Logger LOG = Logger.getInstance(ThreadDumpProvider.class);
   private static final ExtensionPointName<ThreadDumpProvider> EP_NAME = new ExtensionPointName<>("com.intellij.threadDumpProvider");
 
   public static String dump(String pid) {
-    return EP_NAME.computeSafeIfAny(p -> p.dumpThreads(pid));
+    return EP_NAME.computeSafeIfAny(p -> p.isAvailable() ? p.dumpThreads(pid) : null);
   }
 
+  protected abstract boolean isAvailable();
   protected abstract String dumpThreads(String pid);
+}
+
+class JstackThreadDumpProvider extends ThreadDumpProvider {
+  @Override
+  protected String dumpThreads(String pid) {
+    ProcessHandle handle = ProcessHandle.of(Long.parseLong(pid)).orElse(null);
+    if (handle != null) {
+      String commandLine = handle.info().command().orElse(null);
+      if (commandLine != null) {
+        Path javaPath = Paths.get(commandLine);
+        String jstack = "jstack" + (SystemInfo.isWindows ? ".exe" : "");
+        Path jstackPath = javaPath.resolveSibling(jstack);
+        if (Files.isExecutable(jstackPath)) {
+          try {
+            GeneralCommandLine command = new GeneralCommandLine(jstackPath.toAbsolutePath().toString(), pid);
+            ProcessOutput output = ExecUtil.execAndGetOutput(command);
+            if (output.getExitCode() == 0) {
+              return output.getStdout();
+            }
+          }
+          catch (ExecutionException e) {
+            LOG.error(e);
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+  @Override
+  protected boolean isAvailable() {
+    return true;
+  }
 }
 
 class AttachAPIThreadDumpProvider extends ThreadDumpProvider {
@@ -54,5 +96,10 @@ class AttachAPIThreadDumpProvider extends ThreadDumpProvider {
       }
     }
     return null;
+  }
+
+  @Override
+  protected boolean isAvailable() {
+    return true;
   }
 }
