92f830363911a9791aba97db2974f40194810c77
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-04-07 16:43:35 +0300

    [terminal] IJPL-182032 Fix exception of trying to modify the RemDev protocol model in the wrong scheduler
    There is no easy way to allow modifying the map in the background in this case.
    
    (cherry picked from commit 932c223694d0c9a0bfc2d75008b390a1127497c0)
    
    IJ-CR-159801
    
    GitOrigin-RevId: 6f9903c43542e5aac7017a3b94c58d8c69f7ab82

--- plugins/terminal/backend/src/com/intellij/terminal/backend/TerminalPortForwardingManager.kt
+++ plugins/terminal/backend/src/com/intellij/terminal/backend/TerminalPortForwardingManager.kt
@@ -1,14 +1,14 @@
 package com.intellij.terminal.backend
 
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import kotlinx.coroutines.CoroutineScope
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.plugins.terminal.block.reworked.session.rpc.TerminalPortForwardingId
 
 @ApiStatus.Internal
 interface TerminalPortForwardingManager {
-  fun setupPortForwarding(ttyConnector: ObservableTtyConnector, disposable: Disposable): TerminalPortForwardingId?
+  suspend fun setupPortForwarding(ttyConnector: ObservableTtyConnector, coroutineScope: CoroutineScope): TerminalPortForwardingId?
 
   companion object {
     fun getInstance(project: Project): TerminalPortForwardingManager = project.service()
@@ -19,7 +19,7 @@ interface TerminalPortForwardingManager {
  * Default implementation of the [TerminalPortForwardingManager]: no port forwarding is provided.
  */
 internal class TerminalNoPortForwardingManager : TerminalPortForwardingManager {
-  override fun setupPortForwarding(ttyConnector: ObservableTtyConnector, disposable: Disposable): TerminalPortForwardingId? {
+  override suspend fun setupPortForwarding(ttyConnector: ObservableTtyConnector, coroutineScope: CoroutineScope): TerminalPortForwardingId? {
     return null
   }
 }
