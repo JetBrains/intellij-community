5a5540f2e8edb309d681e982d01a3d1b11721442
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-04-07 13:49:44 +0200

    [debugger] Remove unused constructors
    GitOrigin-RevId: 126c023fca564286e7d9db7c5d2cda0dc0b279f3

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/ViewBreakpointsAction.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/ViewBreakpointsAction.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 /*
  * Class ViewBreakpointsAction
@@ -6,7 +6,6 @@
  */
 package com.intellij.xdebugger.impl.actions;
 
-import com.intellij.idea.ActionsBundle;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -14,38 +13,28 @@ import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.NlsActions;
 import com.intellij.xdebugger.impl.breakpoints.XBreakpointUtil;
 import com.intellij.xdebugger.impl.breakpoints.ui.BreakpointsDialogFactory;
 import org.jetbrains.annotations.NotNull;
 
 public class ViewBreakpointsAction extends DumbAwareAction {
-  private Object myInitialBreakpoint;
-
-  public ViewBreakpointsAction(){
-    this(ActionsBundle.actionText(XDebuggerActions.VIEW_BREAKPOINTS), null);
-  }
-
-  public ViewBreakpointsAction(@NlsActions.ActionText String name, Object initialBreakpoint) {
-    super(name);
-    myInitialBreakpoint = initialBreakpoint;
-  }
-
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
     DataContext dataContext = e.getDataContext();
     Project project = CommonDataKeys.PROJECT.getData(dataContext);
     if (project == null) return;
 
-    if (myInitialBreakpoint == null) {
-      Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
-      if (editor != null) {
-        myInitialBreakpoint = XBreakpointUtil.findSelectedBreakpoint(project, editor).second;
-      }
+    Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
+
+    Object initialBreakpoint;
+    if (editor != null) {
+      initialBreakpoint = XBreakpointUtil.findSelectedBreakpoint(project, editor).second;
+    }
+    else {
+      initialBreakpoint = null;
     }
 
-    BreakpointsDialogFactory.getInstance(project).showDialog(myInitialBreakpoint);
-    myInitialBreakpoint = null;
+    BreakpointsDialogFactory.getInstance(project).showDialog(initialBreakpoint);
   }
 
   @Override
