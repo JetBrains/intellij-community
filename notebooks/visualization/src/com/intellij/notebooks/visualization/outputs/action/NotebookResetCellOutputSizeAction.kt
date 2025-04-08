d6e9dedb7cfe449f1c13fb696244a8b320234ed4
Nikita Pavlenko <nikita.pavlenko@jetbrains.com>
2025-04-08 17:35:11 +0200

    [PyCharm] Jupyter/RemoteDev (fix): Fixed the image-output context menu and all image-related actions. #PY-80186 Fixed
    (cherry picked from commit b252093cd991d1e56c27b9ac824b728c0847b0e1)
    
    GitOrigin-RevId: f4271011739e81150fa0e214cabd9d0c2fd326e4

--- notebooks/visualization/src/com/intellij/notebooks/visualization/outputs/action/NotebookResetCellOutputSizeAction.kt
+++ notebooks/visualization/src/com/intellij/notebooks/visualization/outputs/action/NotebookResetCellOutputSizeAction.kt
@@ -2,10 +2,11 @@
 package com.intellij.notebooks.visualization.outputs.action
 
 import com.intellij.notebooks.visualization.context.NotebookDataContext
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification.Frontend
+import com.intellij.openapi.project.DumbAwareAction
 
-class NotebookResetCellOutputSizeAction private constructor() : AnAction() {
+class NotebookResetCellOutputSizeAction private constructor() : DumbAwareAction(), Frontend {
   override fun actionPerformed(e: AnActionEvent) {
     val output = e.dataContext.getData(NotebookDataContext.NOTEBOOK_CELL_OUTPUT_DATA_KEY)
     output?.size?.set(output.size.get().copy(resized = false))
