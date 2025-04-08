66fff0fcaf1faa74924aa8c7187b8a37d567150e
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-04-08 13:32:11 +0300

    [terminal] IJPL-182276 Refactor: use better way to check that we are in IDE frontend
    (cherry picked from commit 1c6e6ec0bb1f5e034b4e9d6bd451c59bd0c57cc7)
    
    IJ-CR-157723
    
    GitOrigin-RevId: c25095075379d8f8f662f16efdabf8fccebd99e8

--- platform/platform-impl/src/com/intellij/configurationStore/storeUtil.kt
+++ platform/platform-impl/src/com/intellij/configurationStore/storeUtil.kt
@@ -15,8 +15,6 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.application.ex.ApplicationManagerEx
-import com.intellij.openapi.client.ClientKind
-import com.intellij.openapi.client.sessions
 import com.intellij.openapi.components.*
 import com.intellij.openapi.components.impl.stores.IComponentStore
 import com.intellij.openapi.components.impl.stores.stateStore
@@ -33,7 +31,7 @@ import com.intellij.platform.ide.progress.ModalTaskOwner
 import com.intellij.platform.ide.progress.runWithModalProgressBlocking
 import com.intellij.platform.ide.progress.withBackgroundProgress
 import com.intellij.util.ExceptionUtil
-import com.intellij.util.application
+import com.intellij.util.PlatformUtils
 import com.intellij.util.concurrency.annotations.RequiresEdt
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -290,7 +288,7 @@ fun forPoorJavaClientOnlySaveProjectIndEdtDoNotUseThisMethod(project: Project, f
  */
 @Internal
 fun saveSettingsForRemoteDevelopment(componentManager: ComponentManager) {
-  if (!AppMode.isRemoteDevHost() && application.sessions(ClientKind.ALL).size < 2)
+  if (!AppMode.isRemoteDevHost() && !PlatformUtils.isJetBrainsClient())
     return
 
   currentThreadCoroutineScope().launch {
