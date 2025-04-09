949b0caf21835aeafc15fdc2e885320ff7601f17
Roman Golyshev <roman.golyshev@jetbrains.com>
2025-04-09 14:18:11 +0200

    KTIJ-33378 [kotlin] Implement `InvokeFunctionImportQuickFixFactory` for `invoke` operators imports in K2 Mode
    Some tests are passing now, but we still
    have to handle UnresolvedReference diagnostic
    (see the following commits)
    
    GitOrigin-RevId: 9b988f66395b6fad30c8a42cc11902d11275e239

---
+++ plugins/kotlin/code-insight/fixes-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/fixes/imprt/factories/InvokeImportQuickFixFactory.kt
@@ -0,0 +1,41 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.k2.codeinsight.fixes.imprt.factories
+
+import org.jetbrains.kotlin.analysis.api.KaSession
+import org.jetbrains.kotlin.analysis.api.diagnostics.KaDiagnosticWithPsi
+import org.jetbrains.kotlin.analysis.api.fir.diagnostics.KaFirDiagnostic
+import org.jetbrains.kotlin.idea.base.analysis.api.utils.KtSymbolFromIndexProvider
+import org.jetbrains.kotlin.idea.k2.codeinsight.fixes.imprt.*
+import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.util.OperatorNameConventions
+
+internal object InvokeImportQuickFixFactory : AbstractImportQuickFixFactory() {
+    override fun KaSession.detectPositionContext(diagnostic: KaDiagnosticWithPsi<*>): ImportContext? =
+        when (diagnostic) {
+            is KaFirDiagnostic.FunctionExpected -> {
+                val expression = diagnostic.psi as? KtExpression ?: return null
+                val invokeReceiverType = diagnostic.type
+
+                ImportContextWithFixedReceiverType(
+                    expression,
+                    ImportPositionType.OperatorCall,
+                    explicitReceiverType = invokeReceiverType,
+                )
+            }
+
+            else -> null
+        }
+
+    override fun provideUnresolvedNames(diagnostic: KaDiagnosticWithPsi<*>, importContext: ImportContext): Set<Name> =
+        setOf(OperatorNameConventions.INVOKE)
+
+    override fun KaSession.provideImportCandidates(
+        unresolvedName: Name,
+        importContext: ImportContext,
+        indexProvider: KtSymbolFromIndexProvider
+    ): List<ImportCandidate> {
+        val provider = CallableImportCandidatesProvider(importContext)
+        return provider.collectCandidates(unresolvedName, indexProvider)
+    }
+}
