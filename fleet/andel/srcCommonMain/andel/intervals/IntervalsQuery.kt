1c677142f29531e6f7b8093b009b757d2a9fdb04
Dennis Ushakov <dennis.ushakov@jetbrains.com>
2025-04-09 17:33:45 +0200

    [fleet] honour greediness when adjusting interval for speculative edit
    GitOrigin-RevId: bd41f9115c35b08629650ae1e1b242cab58ad988

--- fleet/andel/srcCommonMain/andel/intervals/IntervalsQuery.kt
+++ fleet/andel/srcCommonMain/andel/intervals/IntervalsQuery.kt
@@ -59,8 +59,8 @@ fun <K, T> IntervalsQuery<K, T>.adjust(operation: Operation): IntervalsQuery<K, 
     val adjustedEnd = end.transformOnto(invertedOperation, Sticky.RIGHT)
 
     this.query(adjustedStart, adjustedEnd).map { interval ->
-      val newFrom = interval.from.transformOnto(operation, Sticky.LEFT)
-      val newTo = interval.to.transformOnto(operation, Sticky.LEFT)
+      val newFrom = interval.from.transformOnto(operation, if (interval.greedyLeft) Sticky.LEFT else Sticky.RIGHT)
+      val newTo = interval.to.transformOnto(operation, if (interval.greedyRight) Sticky.RIGHT else Sticky.LEFT)
       interval.copy(from = newFrom, to = newTo)
     }
   }
