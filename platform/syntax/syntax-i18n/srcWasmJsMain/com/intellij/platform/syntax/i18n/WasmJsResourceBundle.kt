04c59fb9f9875757e526ca5e92051675b85879fb
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-04-10 11:56:15 +0200

    IJPL-162853 implement BaseResourceBundle for KMP
    GitOrigin-RevId: 73663a4aee2cd30fa844e874b5f9133e1237e4e5

--- platform/syntax/syntax-i18n/srcWasmJsMain/com/intellij/platform/syntax/i18n/WasmJsResourceBundle.kt
+++ platform/syntax/syntax-i18n/srcWasmJsMain/com/intellij/platform/syntax/i18n/WasmJsResourceBundle.kt
@@ -6,10 +6,17 @@ import fleet.util.multiplatform.Actual
 import org.jetbrains.annotations.Nls
 import java.util.function.Supplier
 
+// TODO implement Gradle plugin which takes a .properties file and generates a corresponding kotlin class
+
 /**
  * see expect function [com.intellij.platform.syntax.i18n.ResourceBundle]
  */
 @Actual("ResourceBundle")
-internal fun ResourceBundleWasmJs(bundleClass: String, pathToBundle: String, self: Any): ResourceBundle {
-  TODO()
+internal fun ResourceBundleWasmJs(bundleClass: String, pathToBundle: String, self: Any): ResourceBundle =
+  DummyResourceBundle
+
+@Suppress("HardCodedStringLiteral")
+private object DummyResourceBundle : ResourceBundle {
+  override fun message(key: String, vararg params: Any): @Nls String = key
+  override fun messagePointer(key: String, vararg params: Any): () -> @Nls String = { key }
 }
