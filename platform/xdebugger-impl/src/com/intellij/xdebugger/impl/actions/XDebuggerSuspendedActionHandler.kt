98f26136c21a836393877d5d45706ba6eab750aa
Maksim Zuev <maksim.zuev@jetbrains.com>
2025-04-10 15:32:30 +0200

    [rd debugger] IJPL-182077 Convert XDebuggerSuspendedActionHandler to Kotlin
    GitOrigin-RevId: c96c77b253d51867c4a9c5cbdb35a9e15547995c

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/XDebuggerSuspendedActionHandler.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/XDebuggerSuspendedActionHandler.kt
@@ -1,31 +1,31 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.xdebugger.impl.actions;
+package com.intellij.xdebugger.impl.actions
 
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.xdebugger.XDebugSession;
-import com.intellij.xdebugger.impl.XDebugSessionImpl;
-import com.intellij.xdebugger.impl.actions.handlers.XDebuggerActionHandler;
-import com.intellij.xdebugger.impl.frame.XDebugSessionProxy;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.xdebugger.XDebugSession
+import com.intellij.xdebugger.impl.XDebugSessionImpl
+import com.intellij.xdebugger.impl.actions.handlers.XDebuggerActionHandler
+import com.intellij.xdebugger.impl.frame.XDebugSessionProxy
+import org.jetbrains.annotations.ApiStatus
 
-public abstract class XDebuggerSuspendedActionHandler extends XDebuggerActionHandler {
+abstract class XDebuggerSuspendedActionHandler : XDebuggerActionHandler() {
   @ApiStatus.Internal
-  @Override
-  protected boolean isEnabled(@NotNull XDebugSessionProxy session, @NotNull DataContext dataContext) {
-    return isEnabled(session);
+  override fun isEnabled(session: XDebugSessionProxy, dataContext: DataContext): Boolean {
+    return isEnabled(session)
   }
 
   @ApiStatus.Internal
-  public static boolean isEnabled(@NotNull XDebugSessionProxy session) {
-    return !session.isReadOnly() && session.isSuspended();
-  }
+  companion object {
+    @JvmStatic
+    @ApiStatus.Internal
+    fun isEnabled(session: XDebugSessionProxy): Boolean {
+      return !session.isReadOnly && session.isSuspended
+    }
 
-  /**
-   * @deprecated Use {@link XDebuggerSuspendedActionHandler#isEnabled(XDebugSessionProxy)} instead
-   */
-  @Deprecated
-  public static boolean isEnabled(@NotNull XDebugSession session) {
-    return !((XDebugSessionImpl)session).isReadOnly() && session.isSuspended();
+    @JvmStatic
+    @Deprecated("Use {@link XDebuggerSuspendedActionHandler#isEnabled(XDebugSessionProxy)} instead")
+    fun isEnabled(session: XDebugSession): Boolean {
+      return !(session as XDebugSessionImpl).isReadOnly && session.isSuspended
+    }
   }
 }
