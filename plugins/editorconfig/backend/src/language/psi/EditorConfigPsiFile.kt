3e20631c1dcd11561328ee75fb4d9241e12548a5
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2025-04-11 00:34:33 +0200

    IJPL-182231 remove unused `EditorConfigPsiFile.findRelevantNavigatable`
    GitOrigin-RevId: 7800e6c23efec592110bb37f2e951d978ffecbce

--- plugins/editorconfig/backend/src/language/psi/EditorConfigPsiFile.kt
+++ plugins/editorconfig/backend/src/language/psi/EditorConfigPsiFile.kt
@@ -3,12 +3,10 @@ package org.editorconfig.language.psi
 
 import com.intellij.extapi.psi.PsiFileBase
 import com.intellij.psi.FileViewProvider
-import com.intellij.psi.NavigatablePsiElement
 import com.intellij.psi.util.childrenOfType
 import org.editorconfig.language.EditorConfigLanguage
 import org.editorconfig.language.filetype.EditorConfigFileConstants
 import org.editorconfig.language.filetype.EditorConfigFileType
-import org.editorconfig.language.util.matches
 
 class EditorConfigPsiFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, EditorConfigLanguage) {
   override fun getFileType(): EditorConfigFileType = EditorConfigFileType
@@ -20,11 +18,4 @@ class EditorConfigPsiFile(viewProvider: FileViewProvider) : PsiFileBase(viewProv
 
   val sections: List<EditorConfigSection>
     get() = this.childrenOfType<EditorConfigSection>()
-
-  fun findRelevantNavigatable(): NavigatablePsiElement =
-    sections.lastOrNull { section ->
-      val header = section.header
-      if (header.isValidGlob) header matches virtualFile
-      else false
-    } ?: this
 }
