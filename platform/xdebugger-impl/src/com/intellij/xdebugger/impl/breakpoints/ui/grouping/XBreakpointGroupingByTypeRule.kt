1bbd9c16e2df16aec73984ffa9d54aaca375a3fa
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-04-10 19:23:48 +0200

    [debugger-rd] IJPL-160384: Convert java to Kotlin
    GitOrigin-RevId: 3469274e8f7ac520e452c3de9525abb4b026d4ac

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/breakpoints/ui/grouping/XBreakpointGroupingByTypeRule.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/breakpoints/ui/grouping/XBreakpointGroupingByTypeRule.kt
@@ -1,35 +1,25 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.xdebugger.impl.breakpoints.ui.grouping;
+package com.intellij.xdebugger.impl.breakpoints.ui.grouping
 
-import com.intellij.xdebugger.XDebuggerBundle;
-import com.intellij.xdebugger.breakpoints.XBreakpoint;
-import com.intellij.xdebugger.breakpoints.ui.XBreakpointGroupingRule;
-import com.intellij.xdebugger.breakpoints.ui.XBreakpointsGroupingPriorities;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.xdebugger.XDebuggerBundle
+import com.intellij.xdebugger.breakpoints.XBreakpoint
+import com.intellij.xdebugger.breakpoints.ui.XBreakpointGroupingRule
+import com.intellij.xdebugger.breakpoints.ui.XBreakpointsGroupingPriorities
 
-import java.util.Collection;
-
-public final class XBreakpointGroupingByTypeRule<B> extends XBreakpointGroupingRule<B, XBreakpointTypeGroup> {
-
-  public XBreakpointGroupingByTypeRule() {
-    super("XBreakpointGroupingByTypeRule", XDebuggerBundle.message("breakpoints.group.by.type.label"));
+private class XBreakpointGroupingByTypeRule<B : Any>
+  : XBreakpointGroupingRule<B, XBreakpointTypeGroup>("XBreakpointGroupingByTypeRule", XDebuggerBundle.message("breakpoints.group.by.type.label")) {
+  override fun isAlwaysEnabled(): Boolean {
+    return true
   }
 
-  @Override
-  public boolean isAlwaysEnabled() {
-    return true;
+  override fun getPriority(): Int {
+    return XBreakpointsGroupingPriorities.BY_TYPE
   }
 
-  @Override
-  public int getPriority() {
-    return XBreakpointsGroupingPriorities.BY_TYPE;
-  }
-
-  @Override
-  public XBreakpointTypeGroup getGroup(@NotNull B b) {
-    if (b instanceof XBreakpoint breakpoint) {
-      return new XBreakpointTypeGroup(breakpoint.getType());
+  override fun getGroup(b: B): XBreakpointTypeGroup? {
+    if (b is XBreakpoint<*>) {
+      return XBreakpointTypeGroup(b.getType())
     }
-    return null;
+    return null
   }
 }
