9987e6280d698d43d02ff962c909a5e757b4be57
Alexander Koshevoy <alexander.koshevoy@jetbrains.com>
2025-04-10 12:04:48 +0200

    [path-annotations] Introduce @LocalPath annotation
    GitOrigin-RevId: 67d2384a4d4623d41d460620ab3d561987e18ffc

--- platform/eel/src/com/intellij/platform/eel/annotations/Paths.kt
+++ platform/eel/src/com/intellij/platform/eel/annotations/Paths.kt
@@ -62,3 +62,15 @@ annotation class NativePath
   AnnotationTarget.TYPE,
 )
 annotation class Filename
+
+@Retention(AnnotationRetention.SOURCE)
+@Target(
+  AnnotationTarget.PROPERTY,
+  AnnotationTarget.FIELD,
+  AnnotationTarget.LOCAL_VARIABLE,
+  AnnotationTarget.VALUE_PARAMETER,
+  AnnotationTarget.PROPERTY_GETTER,
+  AnnotationTarget.PROPERTY_SETTER,
+  AnnotationTarget.TYPE,
+)
+annotation class LocalPath
