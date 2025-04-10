be67983841f451b41bf59d9ce763e7149287774b
Alexander Koshevoy <alexander.koshevoy@jetbrains.com>
2025-04-10 13:46:08 +0200

    [path-annotations] Add tests for PathAnnotationInspection quick fixes
    Introduce unit tests to verify quick fixes in PathAnnotationInspection. These include tests for adding annotations like `@MultiRoutingFileSystemPath` and `@NativePath` to resolve warnings and errors. Ensure proper functionality of related inspections and fixes.
    
    Generated by Junie.
    
    GitOrigin-RevId: 15e7033203a78e3a221d8825b896054bc0867570

---
+++ plugins/devkit/devkit-tests/testSrc/org/jetbrains/idea/devkit/inspections/path/PathAnnotationInspectionQuickFixTest.kt
@@ -0,0 +1,70 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.idea.devkit.inspections.path
+
+import org.jetbrains.idea.devkit.inspections.PathAnnotationInspectionTestBase
+
+/**
+ * Tests for the quick fixes in [PathAnnotationInspection].
+ */
+class PathAnnotationInspectionQuickFixTest : PathAnnotationInspectionTestBase() {
+  override fun getFileExtension(): String = "java"
+
+  /**
+   * Test for the inspection itself to verify it's working.
+   */
+  fun testNonAnnotatedStringInPathOf() {
+    doTest("""
+      import java.nio.file.Path;
+
+      public class NonAnnotatedStringInPathOf {
+          public void testMethod() {
+              String nonAnnotatedPath = "/usr/local/bin";
+              // This should be highlighted as a normal warning because non-annotated strings should be annotated with @MultiRoutingFileSystemPath
+              Path path = <warning descr="String without path annotation is used in Path constructor or factory method">Path.of(nonAnnotatedPath)</warning>;
+          }
+      }      
+      """.trimIndent())
+  }
+
+  /**
+   * Test for AddMultiRoutingAnnotationFix.
+   * 
+   * This test verifies that the quick fix correctly adds the @MultiRoutingFileSystemPath annotation
+   * to a string variable used in Path.of().
+   */
+  fun testAddMultiRoutingAnnotationFix() {
+    doTest("""
+      import java.nio.file.Path;
+
+      public class AddMultiRoutingAnnotationFix {
+          public void testMethod() {
+              String nonAnnotatedPath = "/usr/local/bin";
+              // This should be highlighted as a normal warning because non-annotated strings should be annotated with @MultiRoutingFileSystemPath
+              Path path = <warning descr="String without path annotation is used in Path constructor or factory method">Path.of(nonAnnotatedPath)</warning>;
+          }
+      }      
+      """.trimIndent())
+  }
+
+  /**
+   * Test for AddNativePathAnnotationFix.
+   * 
+   * This test verifies that the quick fix correctly adds the @NativePath annotation
+   * to a string variable used in FileSystem.getPath().
+   */
+  fun testAddNativePathAnnotationFix() {
+    doTest("""
+      import java.nio.file.FileSystem;
+      import java.nio.file.FileSystems;
+
+      public class AddNativePathAnnotationFix {
+          public void testMethod() {
+              FileSystem fs = FileSystems.getDefault();
+              String nonAnnotatedPath = "/usr/local/bin";
+              // This should be highlighted as an error because first argument of FileSystem.getPath() should be annotated with @NativePath
+              fs.getPath(<warning descr="First argument of FileSystem.getPath() should be annotated with @NativePath">nonAnnotatedPath</warning>, "file.txt");
+          }
+      }      
+      """.trimIndent())
+  }
+}
