d5c2f81b590cca20933929f6309bc36c46fbee99
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-04-10 19:46:11 +0200

    [debugger-rd] IJPL-160384: Convert XBreakpointFileGroupingRule to Kotlin
    GitOrigin-RevId: dc19b5027f6d7ce8e1b93e204422b04e8efb7de0

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/breakpoints/ui/grouping/XBreakpointFileGroupingRule.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/breakpoints/ui/grouping/XBreakpointFileGroupingRule.kt
@@ -1,45 +1,33 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.xdebugger.impl.breakpoints.ui.grouping;
+package com.intellij.xdebugger.impl.breakpoints.ui.grouping
 
-import com.intellij.icons.AllIcons;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.xdebugger.XDebuggerBundle;
-import com.intellij.xdebugger.XSourcePosition;
-import com.intellij.xdebugger.breakpoints.XLineBreakpoint;
-import com.intellij.xdebugger.breakpoints.ui.XBreakpointGroupingRule;
-import com.intellij.xdebugger.breakpoints.ui.XBreakpointsGroupingPriorities;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.icons.AllIcons
+import com.intellij.xdebugger.XDebuggerBundle
+import com.intellij.xdebugger.breakpoints.XLineBreakpoint
+import com.intellij.xdebugger.breakpoints.ui.XBreakpointGroupingRule
+import com.intellij.xdebugger.breakpoints.ui.XBreakpointsGroupingPriorities
+import javax.swing.Icon
 
-import javax.swing.*;
-import java.util.Collection;
-
-public final class XBreakpointFileGroupingRule<B> extends XBreakpointGroupingRule<B, XBreakpointFileGroup> {
-  public XBreakpointFileGroupingRule() {
-    super("by-file", XDebuggerBundle.message("rule.name.group.by.file"));
+private class XBreakpointFileGroupingRule<B : Any>
+  : XBreakpointGroupingRule<B, XBreakpointFileGroup>("by-file", XDebuggerBundle.message("rule.name.group.by.file")) {
+  override fun getPriority(): Int {
+    return XBreakpointsGroupingPriorities.BY_FILE
   }
 
-  @Override
-  public int getPriority() {
-    return XBreakpointsGroupingPriorities.BY_FILE;
-  }
-
-  @Override
-  public XBreakpointFileGroup getGroup(final @NotNull B breakpoint) {
-    if (!(breakpoint instanceof XLineBreakpoint)) {
-      return null;
+  override fun getGroup(breakpoint: B): XBreakpointFileGroup? {
+    if (breakpoint !is XLineBreakpoint<*>) {
+      return null
     }
-    XSourcePosition position = ((XLineBreakpoint<?>)breakpoint).getSourcePosition();
+    val position = (breakpoint as XLineBreakpoint<*>).getSourcePosition()
 
-    if (position == null) return null;
+    if (position == null) return null
 
-    VirtualFile file = position.getFile();
+    val file = position.getFile()
 
-    return new XBreakpointFileGroup(file);
+    return XBreakpointFileGroup(file)
   }
 
-  @Override
-  public @Nullable Icon getIcon() {
-    return AllIcons.Actions.GroupByFile;
+  override fun getIcon(): Icon? {
+    return AllIcons.Actions.GroupByFile
   }
 }
