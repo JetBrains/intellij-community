b6b84a74e770693829bffa67d0380163e351bd24
Petr Surkov <petr.surkov@jetbrains.com>
2025-04-11 12:03:47 +0200

    [ai-completion] LLM-16259 Bump `inline.completion.v2` group version
    GitOrigin-RevId: 85524de46f89659a64de3db50ab9906bfc948806

--- platform/platform-impl/codeinsight-inline/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionLogs.kt
+++ platform/platform-impl/codeinsight-inline/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionLogs.kt
@@ -14,7 +14,7 @@ import org.jetbrains.annotations.ApiStatus
 @ApiStatus.Internal
 object InlineCompletionLogs : CounterUsagesCollector() {
   // TODO use ML_RECORDER_ID
-  val GROUP: EventLogGroup = EventLogGroup("inline.completion.v2", 37, recorder = "ML")
+  val GROUP: EventLogGroup = EventLogGroup("inline.completion.v2", 38, recorder = "ML")
 
   val INSERTED_STATE_EVENT: VarargEventId = GROUP.registerVarargEvent(
     "inserted_state",
