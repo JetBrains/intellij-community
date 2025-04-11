b4d528a3b09232ec95d7cf76ec2d5bc3e081741e
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-04-11 13:47:25 +0300

    [terminal] IJPL-182482 Refactor: use TtyListener instead of additional tty connector wrapping
    GitOrigin-RevId: 90e3eed07269cb40ee99c2b1f704f16b44b4043a

--- plugins/terminal/backend/src/com/intellij/terminal/backend/TtyConnectorListener.kt
+++ plugins/terminal/backend/src/com/intellij/terminal/backend/TtyConnectorListener.kt
@@ -4,5 +4,7 @@ import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
 interface TtyConnectorListener {
-  fun charsRead(buf: CharArray, offset: Int, length: Int)
+  fun charsRead(buf: CharArray, offset: Int, length: Int) {}
+
+  fun bytesWritten(bytes: ByteArray) {}
 }
