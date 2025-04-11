c06a08aba347b53ed6137ebe41d358fd006135ef
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-04-11 14:44:19 +0200

    [plugins] IJPL-181065 mark IdeaPluginDescriptor as non-extendable too
    GitOrigin-RevId: 036619dd26a194b569e04b76de551114cc0c2c0e

--- platform/core-api/src/com/intellij/ide/plugins/IdeaPluginDescriptor.java
+++ platform/core-api/src/com/intellij/ide/plugins/IdeaPluginDescriptor.java
@@ -3,6 +3,7 @@ package com.intellij.ide.plugins;
 
 import com.intellij.openapi.extensions.PluginDescriptor;
 import com.intellij.openapi.extensions.PluginId;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -12,6 +13,7 @@ import java.util.List;
  * Describes a plugin which may be installed into IntelliJ-based IDE. Use {@link com.intellij.ide.plugins.PluginManagerCore#getPlugin(PluginId)}
  * to get a descriptor by a plugin ID and {@link com.intellij.ide.plugins.PluginManagerCore#getPlugins()} to get all plugins.
  */
+@ApiStatus.NonExtendable
 public interface IdeaPluginDescriptor extends PluginDescriptor {
   /**
    * aka {@code <depends>} elements from the plugin.xml
