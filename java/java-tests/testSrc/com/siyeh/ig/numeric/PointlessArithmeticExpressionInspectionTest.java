aca90dd190dd0f4f39a0c8224d2889521b69b9a2
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-04-11 13:39:20 +0000

    [java-inspections] get rid of OptionAccessor anti-pattern in PointlessArithmeticExpressionInspectionTest
    Merge-request: IJ-MR-159576
    Merged-by: Bartek Pacia <bartek.pacia@jetbrains.com>
    
    GitOrigin-RevId: e13a9a26ed89db8eb13957339ccbeb3e9f984a80

--- java/java-tests/testSrc/com/siyeh/ig/numeric/PointlessArithmeticExpressionInspectionTest.java
+++ java/java-tests/testSrc/com/siyeh/ig/numeric/PointlessArithmeticExpressionInspectionTest.java
@@ -1,23 +1,29 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+/**/// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig.numeric;
 
 import com.intellij.codeInspection.InspectionProfileEntry;
-import com.intellij.codeInspection.ui.OptionAccessor;
-import com.intellij.openapi.util.text.StringUtil;
 import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.LightJavaInspectionTestCase;
 import org.jetbrains.annotations.Nullable;
 
 public class PointlessArithmeticExpressionInspectionTest extends LightJavaInspectionTestCase {
 
+  private PointlessArithmeticExpressionInspection myInspectionProfileEntry;
+
   public void testPointlessArithmeticExpression() { doTest(); }
-  public void testPointlessArithmeticExpression_disable_m_ignoreExpressionsContainingConstants() { doTest(); }
+  public void testPointlessArithmeticExpression_disable_m_ignoreExpressionsContainingConstants() {
+    myInspectionProfileEntry.m_ignoreExpressionsContainingConstants = false;
+    doTest();
+  }
   public void testNestedVersusSuper() { doTest(); }
   public void testComments() { doQuickFixTest(); }
   public void testCast() { doQuickFixTest(); }
   public void testCommentInsideCall() { doQuickFixTest(); }
   public void testNamedConstants() { doTest(); }
-  public void testNamedConstants_disable_m_ignoreExpressionsContainingConstants() { doTest(); }
+  public void testNamedConstants_disable_m_ignoreExpressionsContainingConstants() {
+    myInspectionProfileEntry.m_ignoreExpressionsContainingConstants = false;
+    doTest();
+  }
 
   private void doQuickFixTest() {
     doTest();
@@ -27,16 +33,7 @@ public class PointlessArithmeticExpressionInspectionTest extends LightJavaInspec
   @Nullable
   @Override
   protected InspectionProfileEntry getInspection() {
-    PointlessArithmeticExpressionInspection inspection = new PointlessArithmeticExpressionInspection();
-    inspection.m_ignoreExpressionsContainingConstants = true;
-    String option = StringUtil.substringAfter(getName(), "_");
-    if (option != null) {
-      boolean enableOption = !option.startsWith("disable");
-      if (!enableOption) {
-        option = StringUtil.substringAfter(option, "disable_");
-      }
-      new OptionAccessor.Default(inspection).setOption(option, false);
-    }
-    return inspection;
+    myInspectionProfileEntry = new PointlessArithmeticExpressionInspection();
+    return myInspectionProfileEntry;
   }
 }
