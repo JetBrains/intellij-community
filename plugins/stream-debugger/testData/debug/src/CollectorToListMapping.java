2561796bf820514742f3b28c7216cdde9f6ee379
Egor Ushakov <egor.ushakov@jetbrains.com>
2025-04-11 16:14:36 +0200

    IDEA-370727 moved the tests to ResultMappingTest
    GitOrigin-RevId: 5081445224da9306a62174ed241e11cd7d4c1585

--- plugins/stream-debugger/testData/debug/src/CollectorToListMapping.java
+++ plugins/stream-debugger/testData/debug/src/CollectorToListMapping.java
@@ -2,9 +2,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class ToListMapping {
+public class CollectorToListMapping {
   public static void main(String[] args) {
     // Breakpoint!
-    final List<Object> objects = Stream.of(new Object(), new Object(), new Object()).toList();
+    final List<Object> objects = Stream.of(new Object(), new Object(), new Object()).collect(Collectors.toList());
   }
 }
