1c2a863ae80588766a74c94ec6debd78867a790a
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-04-14 16:16:28 +0300

    [terminal] IJPL-182482 Increase the terminal FUS group version
    (cherry picked from commit 3240887ac2a842d28f7baf93185c75a4eaa97df9)
    
    IJ-CR-160384
    
    GitOrigin-RevId: 0e85d2110c426df9d5d6d0a8baf6c8f3e329768b

--- plugins/terminal/src/org/jetbrains/plugins/terminal/fus/ReworkedTerminalUsageCollector.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/fus/ReworkedTerminalUsageCollector.kt
@@ -19,7 +19,7 @@ private const val GROUP_ID = "terminal"
 object ReworkedTerminalUsageCollector : CounterUsagesCollector() {
   override fun getGroup(): EventLogGroup = GROUP
 
-  private val GROUP = EventLogGroup(GROUP_ID, 1)
+  private val GROUP = EventLogGroup(GROUP_ID, 2)
 
   private val OS_VERSION_FIELD = EventFields.StringValidatedByRegexpReference("os-version", "version")
   private val SHELL_STR_FIELD = EventFields.String("shell", KNOWN_SHELLS.toList())
