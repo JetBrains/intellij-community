0320a179b20fc3354f33178dfe7f2ccb8b5ea352
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2025-04-14 11:38:12 +0300

    [qodana] QD-11247 Force gradle to set ProjectSDK on import until proper fix of IDEA-367680
    (cherry picked from commit 447f860cbd1aee4e037437002facf9fd08ca1523)
    
    IJ-MR-160269
    
    GitOrigin-RevId: 707496a601c02221f3ce22c19185462e6e4bc004

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/qodana/QodanaProductProperties.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/qodana/QodanaProductProperties.kt
@@ -19,6 +19,7 @@ private val COMMON_ADDITIONAL_VM_OPTIONS = listOf(
   "-Dide.activity.tracking.enable.debug=true",
   "-Deslint.service.expiration.timeout.ms=5000",
   "-Dphp.additional.library.manager.new.updater=true",
+  "-Dgradle.force.project.sdk.import=true" //workaround for https://youtrack.jetbrains.com/issue/IDEA-367680/The-module-SDK-isnt-always-specified
   )
 
 private const val IS_EAP = false
