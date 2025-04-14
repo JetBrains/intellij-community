7d930d013eebeb0d00df7185b75bf0d0f6b00be8
Suzy Kostumyan <suzy.kostumyan@jetbrains.com>
2025-04-14 13:19:01 +0400

    Group version incrementation for collectors
    GitOrigin-RevId: e87b54dd0ed60efae1e307ac2059b77ccb5372bd

--- plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/statistics/ProjectConfigurationCollector.kt
+++ plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/statistics/ProjectConfigurationCollector.kt
@@ -82,7 +82,7 @@ internal class ProjectConfigurationCollector : ProjectUsagesCollector() {
         }
     }
 
-    private val GROUP = EventLogGroup("kotlin.project.configuration", 25)
+    private val GROUP = EventLogGroup("kotlin.project.configuration", 26)
 
     private val systemField = EventFields.String("system", listOf("JPS", "Maven", "Gradle", "unknown"))
     private val platformField = EventFields.String("platform", composePlatformFields())
