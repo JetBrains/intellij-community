dc8d3b800c13bea4f24c67606a6078e4caf2d688
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-04-14 15:04:26 +0200

    [kotlin] generate light classes tests
    ^KT-75689
    
    GitOrigin-RevId: 7deb4891f89d7e94cf7ecc0767c0ee598ea7a906

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/asJava/classes/IdeLightClassesByFqNameTestGenerated.java
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/asJava/classes/IdeLightClassesByFqNameTestGenerated.java
@@ -880,6 +880,21 @@ public abstract class IdeLightClassesByFqNameTestGenerated extends AbstractIdeLi
             runTest(compilerTestData("compiler/testData/asJava/lightClasses/lightClassByFqName/SubstitutionOverride.kt"));
         }
 
+        @TestMetadata("SuperTypeWithBound.kt")
+        public void testSuperTypeWithBound() throws Exception {
+            runTest(compilerTestData("compiler/testData/asJava/lightClasses/lightClassByFqName/SuperTypeWithBound.kt"));
+        }
+
+        @TestMetadata("SuperTypeWithBound2.kt")
+        public void testSuperTypeWithBound2() throws Exception {
+            runTest(compilerTestData("compiler/testData/asJava/lightClasses/lightClassByFqName/SuperTypeWithBound2.kt"));
+        }
+
+        @TestMetadata("SuperTypeWithBoundKotlinCollection.kt")
+        public void testSuperTypeWithBoundKotlinCollection() throws Exception {
+            runTest(compilerTestData("compiler/testData/asJava/lightClasses/lightClassByFqName/SuperTypeWithBoundKotlinCollection.kt"));
+        }
+
         @TestMetadata("Throws.kt")
         public void testThrows() throws Exception {
             runTest(compilerTestData("compiler/testData/asJava/lightClasses/lightClassByFqName/Throws.kt"));
