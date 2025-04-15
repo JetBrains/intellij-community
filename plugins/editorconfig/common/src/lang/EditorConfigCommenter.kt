f58fe9fa43f11b25a51b76b2671aa0ac3c2ad8b1
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2025-04-15 15:00:47 +0200

    IJPL-182231 move commenter to common
    GitOrigin-RevId: 815396ff1d0afdd0eb1fd31ad1e8c38357112b72

--- plugins/editorconfig/common/src/lang/EditorConfigCommenter.kt
+++ plugins/editorconfig/common/src/lang/EditorConfigCommenter.kt
@@ -1,9 +1,9 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.editorconfig.language.codeinsight
+package com.intellij.editorconfig.common.lang
 
 import com.intellij.lang.Commenter
 
-class EditorConfigCommenter : Commenter {
+internal class EditorConfigCommenter : Commenter {
   override fun getLineCommentPrefix(): String = "# "
   override fun getBlockCommentPrefix(): String = ""
   override fun getBlockCommentSuffix(): String? = null
