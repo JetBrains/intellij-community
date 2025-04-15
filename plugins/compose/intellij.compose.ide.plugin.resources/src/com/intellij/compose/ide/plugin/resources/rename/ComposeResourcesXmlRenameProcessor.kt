364fb232cda4eeeda88027553f5fb966483bd642
Alexandru Resiga <alexandru.resiga@jetbrains.com>
2025-04-15 22:48:54 +0200

    KMT-500 [compose resources rename] filter out Android R.string values from rename
    - remove obsolete workaround for renaming string values; now psi changes are handled directly
    - removing this workaround also avoids displaying the 'Refactoring Preview' tool window since we don't use custom rename parameters
    
    (cherry picked from commit e3bd039083aa5bee3930da278be903ae06704f84)
    
    GitOrigin-RevId: e3826fc9df2d29c3b4f00dcb19431c166f1d0df9

--- plugins/compose/intellij.compose.ide.plugin.resources/src/com/intellij/compose/ide/plugin/resources/rename/ComposeResourcesXmlRenameProcessor.kt
+++ plugins/compose/intellij.compose.ide.plugin.resources/src/com/intellij/compose/ide/plugin/resources/rename/ComposeResourcesXmlRenameProcessor.kt
@@ -1,13 +1,16 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.compose.ide.plugin.resources.rename
 
+import com.intellij.compose.ide.plugin.resources.ANDROID_RESOURCE_REFERENCE
 import com.intellij.compose.ide.plugin.resources.STRINGS_XML_FILENAME
 import com.intellij.compose.ide.plugin.resources.VALUES_DIRNAME
 import com.intellij.compose.ide.plugin.resources.getAllComposeResourcesDirs
 import com.intellij.openapi.vfs.toNioPathOrNull
 import com.intellij.psi.PsiElement
 import com.intellij.psi.xml.XmlAttributeValue
+import com.intellij.refactoring.listeners.RefactoringElementListener
 import com.intellij.refactoring.rename.RenameXmlAttributeProcessor
+import com.intellij.usageView.UsageInfo
 
 /**
  * A [RenameXmlAttributeProcessor] based processor for renaming XML attributes in Compose resource files.
@@ -19,6 +22,12 @@ internal class ComposeResourcesXmlRenameProcessor : RenameXmlAttributeProcessor(
     if (element !is XmlAttributeValue || !element.isValueString) return false
     val resourceDirectoryPath = element.containingFile.parent?.parent?.virtualFile ?: return false
     return element.project.getAllComposeResourcesDirs().any { it.directoryPath == resourceDirectoryPath.toNioPathOrNull() }
+  }
+
+  override fun renameElement(element: PsiElement, newName: String, usages: Array<out UsageInfo?>, listener: RefactoringElementListener?) {
+    // filter out Android R.string values with matching name
+    val nonAndroidStringUsages = usages.filterNot { it?.reference?.javaClass?.name == ANDROID_RESOURCE_REFERENCE }.toTypedArray()
+    super.renameElement(element, newName, nonAndroidStringUsages, listener)
   }
 
   private val XmlAttributeValue.isValueString: Boolean
