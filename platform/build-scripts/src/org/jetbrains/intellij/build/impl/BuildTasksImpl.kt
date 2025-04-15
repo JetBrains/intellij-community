5aca1cc19f86c6d47d2a1f5002c4f8472b29969d
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2025-04-15 13:17:32 +0200

    [build scripts] cross-platform ZIP: giving special treatment to CLion tool directories (CPP-44159)
    ... which already have OS and arch parts in the path and don't need additional prefixes
    
    GitOrigin-RevId: 1d7389d92e254371445006aeb0182e7e41a3622e

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/BuildTasksImpl.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/BuildTasksImpl.kt
@@ -953,6 +953,15 @@ private fun crossPlatformZip(
       out.entryToDir(macArm64DistDir.resolve("bin/${executableName}.vmoptions"), "bin/mac")
       out.entryToDir(linuxX64DistDir.resolve("bin/${executableName}64.vmoptions"), "bin/linux")
 
+      val zipFileUniqueGuard = HashMap<String, DistFileContent>()
+
+      val nonConflictingBinDirs = when (context.applicationInfo.productCode) {
+        "CL" -> listOf("clang/", "cmake/", "gdb/", "lldb/", "mingw/", "ninja/", "profiler/")
+        else -> emptyList()
+      }
+      val binEntryCustomizer = { entry: ZipArchiveEntry, path: Path, relative: String ->
+        entryCustomizer.invoke(entry, path, "bin/${relative}")
+      }
       distResults.forEach {
         val prefix = "bin/${it.builder.targetOs.dirName}/${it.arch.dirName}/"
         out.dir(it.outDir.resolve("bin"), prefix, fileFilter = { _, relPath ->
@@ -964,13 +973,15 @@ private fun crossPlatformZip(
           relPath != "idea.properties" &&
           !relPath.endsWith(".vmoptions") &&
           !relPath.startsWith("repair") &&
-          !relPath.startsWith("restart")
-        }, entryCustomizer = { entry, path, relative ->
-          entryCustomizer.invoke(entry, path, "bin/$relative")
-        })
-      }
+          !relPath.startsWith("restart") &&
+          !nonConflictingBinDirs.any(relPath::startsWith)
+        }, binEntryCustomizer)
 
-      val zipFileUniqueGuard = HashMap<String, DistFileContent>()
+        out.dir(it.outDir.resolve("bin"), prefix = "bin/", fileFilter = { file, relPath ->
+          nonConflictingBinDirs.any(relPath::startsWith)&&
+          filterFileIfAlreadyInZip(relPath, file, zipFileUniqueGuard)
+        }, binEntryCustomizer)
+      }
 
       out.dir(context.paths.distAllDir, prefix = "", fileFilter = { _, relPath -> relPath != "bin/idea.properties" }, entryCustomizer)
 
