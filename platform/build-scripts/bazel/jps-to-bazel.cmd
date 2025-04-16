c69736cf59f54da1e5149b029294738589e5ee78
Leonid Shalupov <leonid@shalupov.com>
2025-04-16 22:07:33 +0200

    jps-to-bazel: fix bazel version and cmd scripts
    GitOrigin-RevId: 8acb9ffeb7d876cc09ad0510f995227ccc1a52e8

--- platform/build-scripts/bazel/jps-to-bazel.cmd
+++ platform/build-scripts/bazel/jps-to-bazel.cmd
@@ -4,9 +4,14 @@ GOTO :CMDSCRIPT
 ::CMDLITERAL
 
 set -eu
-root="$(cd "$(dirname "$0")/../../../.."; pwd)"
-exec /bin/bash "$root/bazel.cmd" run //:jps-to-bazel
+
+script_dir="$(cd "$(dirname "$0")"; pwd)"
+root="$script_dir/../../../.."
+
+cd "$script_dir"
+exec /bin/bash "$root/bazel.cmd" run "$@" //:jps-to-bazel
 :CMDSCRIPT
 
+cd /d "%~dp0"
 call "%~dp0\..\..\..\..\bazel.cmd" run //:jps-to-bazel
 EXIT /B %ERRORLEVEL%
