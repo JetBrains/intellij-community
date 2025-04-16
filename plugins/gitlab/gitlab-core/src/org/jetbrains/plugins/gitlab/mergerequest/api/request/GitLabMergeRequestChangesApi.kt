fcd57cc6e19b2f626b71b13e3f0e4ac1ab40be80
Chris Lemaire <chris.lemaire@jetbrains.com>
2025-04-16 16:28:45 +0200

    [gitlab] Fix missing items in changelist due to pagination links missing (IJPL-183235)
    #IJPL-183235 Fixed
    
    GitOrigin-RevId: db052c949fc3bd734000ec4b66dd352e61040deb

--- plugins/gitlab/gitlab-core/src/org/jetbrains/plugins/gitlab/mergerequest/api/request/GitLabMergeRequestChangesApi.kt
+++ plugins/gitlab/gitlab-core/src/org/jetbrains/plugins/gitlab/mergerequest/api/request/GitLabMergeRequestChangesApi.kt
@@ -4,6 +4,7 @@ package org.jetbrains.plugins.gitlab.mergerequest.api.request
 import com.intellij.collaboration.api.json.loadJsonList
 import com.intellij.collaboration.api.json.loadJsonValue
 import com.intellij.collaboration.util.resolveRelative
+import com.intellij.collaboration.util.withQuery
 import org.jetbrains.plugins.gitlab.api.*
 import org.jetbrains.plugins.gitlab.api.dto.GitLabCommitDetailedRestDTO
 import org.jetbrains.plugins.gitlab.api.dto.GitLabCommitRestDTO
@@ -29,9 +30,8 @@ suspend fun GitLabApi.Rest.loadMergeRequestCommits(uri: URI): HttpResponse<out L
 }
 
 
-
 data class GitLabChangesHolderDTO(
-  val changes: List<GitLabDiffDTO>
+  val changes: List<GitLabDiffDTO>,
 )
 
 @SinceGitLab("9.0", deprecatedIn = "15.7", note = "Deprecated in favour of /diffs")
@@ -62,14 +62,15 @@ suspend fun GitLabApi.Rest.loadMergeRequestDiffs(uri: URI): HttpResponse<out Lis
 }
 
 @SinceGitLab("15.7")
-suspend fun GitLabApi.getMergeRequestDiffsURI(project: GitLabProjectCoordinates, mrIid: String): URI {
+suspend fun GitLabApi.getMergeRequestDiffsURI(project: GitLabProjectCoordinates, mrIid: String, page: Int): URI {
   val metadata = getMetadataOrNull()
   requireNotNull(metadata)
 
   return project.restApiUri
-      .resolveRelative("merge_requests")
-      .resolveRelative(mrIid)
-      .resolveRelative("diffs")
+    .resolveRelative("merge_requests")
+    .resolveRelative(mrIid)
+    .resolveRelative("diffs")
+    .withQuery("page=$page")
 }
 
 @SinceGitLab("7.0")
@@ -89,8 +90,10 @@ fun getCommitDiffsURI(project: GitLabProjectCoordinates, commitSha: String): URI
     .resolveRelative("diff")
 
 @SinceGitLab("7.0")
-suspend fun GitLabApi.Rest.loadCommit(project: GitLabProjectCoordinates,
-                                      commitSha: String): HttpResponse<out GitLabCommitDetailedRestDTO> {
+suspend fun GitLabApi.Rest.loadCommit(
+  project: GitLabProjectCoordinates,
+  commitSha: String,
+): HttpResponse<out GitLabCommitDetailedRestDTO> {
   val uri = project.restApiUri
     .resolveRelative("repository")
     .resolveRelative("commits")
