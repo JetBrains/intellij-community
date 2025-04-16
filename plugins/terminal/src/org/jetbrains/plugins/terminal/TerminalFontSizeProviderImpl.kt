185c133e1843da68215c74d4ef06313c4b5ad65e
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-04-16 16:19:12 +0200

    IJPL-183523, IJPL-184075, IJPL-184079 Terminal font size changes
    Introduce ChangeEditorFontSizeStrategy that allows overriding
    the editor's behavior when it comes to mouse wheel zoom
    and font reset actions.
    
    Use this new API to override the behavior for the terminal editors
    and delegate to the new TerminalFontSizeProviderImpl service
    to apply the changes.
    
    Make the terminal editors listen to font changes and react.
    
    Note that the editor's change font size actions are not covered
    by this new API because the terminal has its own actions for that
    (to allow keymap customization) and those should be fixed instead.
    
    (cherry picked from commit 3c4173128df790708b74e8cde5516ce2d6aa3ae1)
    
    IJ-CR-160631
    
    GitOrigin-RevId: 7ef9180d4980c4c31aaf74d0b39b8a69ae03ebad

--- plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalFontSizeProviderImpl.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalFontSizeProviderImpl.kt
@@ -7,10 +7,12 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.terminal.TerminalFontSizeProvider
+import org.jetbrains.annotations.ApiStatus
 import java.util.concurrent.CopyOnWriteArrayList
 
 @Service(Service.Level.APP)
-internal class TerminalFontSizeProviderImpl : TerminalFontSizeProvider, Disposable {
+@ApiStatus.Internal
+class TerminalFontSizeProviderImpl : TerminalFontSizeProvider, Disposable {
   private val listeners = CopyOnWriteArrayList<TerminalFontSizeProvider.Listener>()
 
   private var fontSize: Float? = null
@@ -51,7 +53,7 @@ internal class TerminalFontSizeProviderImpl : TerminalFontSizeProvider, Disposab
     setFontSize(getDefaultScaledFontSize().floatValue)
   }
 
-  private fun getDefaultScaledFontSize(): TerminalFontSize {
+  internal fun getDefaultScaledFontSize(): TerminalFontSize {
     return TerminalFontOptions.getInstance().getSettings().fontSize.scale()
   }
 
