b7f67bc3b10be96dddf49b2bb17124fbbef3f6b2
ekaterina.itsenko <ekaterina.itsenko@jetbrains.com>
2025-04-16 03:10:58 +0200

    [pycharm] PY-80400 Debugger: add rotate action
    GitOrigin-RevId: c627857e001426e691946ed93b6f5f764249051f

--- images/src/org/intellij/images/scientific/utils/ScientificUtils.kt
+++ images/src/org/intellij/images/scientific/utils/ScientificUtils.kt
@@ -8,12 +8,36 @@ import com.intellij.openapi.util.Key
 import java.awt.image.BufferedImage
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.service
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.awt.geom.AffineTransform
 
 
 object ScientificUtils {
   val SCIENTIFIC_MODE_KEY: Key<Unit> = Key<Unit>("SCIENTIFIC_MODE")
   val ORIGINAL_IMAGE_KEY: Key<BufferedImage> = Key("ORIGINAL_IMAGE")
+  val ROTATION_ANGLE_KEY: Key<Int> = Key.create("IMAGE_ROTATION_ANGLE")
   const val DEFAULT_IMAGE_FORMAT: String = "png"
+
+  suspend fun rotateImage(image: BufferedImage, angle: Int): BufferedImage = withContext(Dispatchers.IO) {
+    val width = image.width
+    val height = image.height
+    val isRightAngle = angle % 180 == 90
+    val targetWidth = if (isRightAngle) height else width
+    val targetHeight = if (isRightAngle) width else height
+
+    val transform = AffineTransform()
+    transform.translate(targetWidth / 2.0, targetHeight / 2.0)
+    transform.rotate(Math.toRadians(angle.toDouble()))
+    transform.translate(-width / 2.0, -height / 2.0)
+
+    val rotatedImage = BufferedImage(targetWidth, targetHeight, image.type)
+    val graphics = rotatedImage.createGraphics()
+    graphics.transform = transform
+    graphics.drawImage(image, 0, 0, null)
+    graphics.dispose()
+    rotatedImage
+  }
 }
 
 @State(name = "BinarizationThresholdConfig", storages = [Storage("binarizationThresholdConfig.xml")])
