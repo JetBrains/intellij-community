b4a993ab03536c4a3965985fb4fcfa52b8b0a0ba
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2025-04-18 01:04:31 +0200

    PY-79013: Add `file_type` to `pycharm.misc.project` statistics group.
    This field might be either `script` or `notebook`.
    
    GitOrigin-RevId: fd766a7f709f40d7217860ac9b91483cb7dcc764

--- python/ide/impl/src/com/intellij/pycharm/community/ide/impl/miscProject/impl/MiscProjectUsageCollector.kt
+++ python/ide/impl/src/com/intellij/pycharm/community/ide/impl/miscProject/impl/MiscProjectUsageCollector.kt
@@ -1,23 +1,34 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.pycharm.community.ide.impl.miscProject.impl
 
 import com.intellij.internal.statistic.eventLog.EventLogGroup
+import com.intellij.internal.statistic.eventLog.events.EventFields
 import com.intellij.internal.statistic.eventLog.events.EventId
+import com.intellij.internal.statistic.eventLog.events.VarargEventId
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
+import com.intellij.pycharm.community.ide.impl.miscProject.MiscFileType
+import org.jetbrains.annotations.ApiStatus
 
 /**
  * Logs misc project creation and renaming to FUS
  */
-internal object MiscProjectUsageCollector : CounterUsagesCollector() {
-  private val GROUP: EventLogGroup = EventLogGroup("pycharm.misc.project", 3)
+@ApiStatus.Internal
+object MiscProjectUsageCollector : CounterUsagesCollector() {
+  private val GROUP: EventLogGroup = EventLogGroup("pycharm.misc.project", 4)
 
-  private val MISC_PROJECT_CREATED: EventId = GROUP.registerEvent("misc.project.created", "Misc project created (user clicked on the button on welcome screen)")
+  private val MISC_FILE_TYPE = EventFields.StringValidatedByCustomRule(
+    "file_type",
+    MiscProjectUsageCollectorValidationRule::class.java,
+    "Misc project created (user clicked on the button on welcome screen"
+  )
+
+  private val MISC_PROJECT_CREATED: VarargEventId = GROUP.registerVarargEvent("misc.project.created", MISC_FILE_TYPE)
   private val MISC_PROJECT_RENAMED: EventId = GROUP.registerEvent("misc.project.renamed", "Misc project renamed")
 
   override fun getGroup(): EventLogGroup = GROUP
 
-  fun projectCreated() {
-    MISC_PROJECT_CREATED.log()
+  fun projectCreated(fileType: MiscFileType) {
+    MISC_PROJECT_CREATED.log(MISC_FILE_TYPE.with(fileType.technicalNameForStatistics))
   }
 
   fun projectRenamed() {
