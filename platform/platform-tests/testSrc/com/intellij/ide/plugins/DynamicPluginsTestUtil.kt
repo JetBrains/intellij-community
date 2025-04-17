d5c34dad66b990cc57ccac23fcd668baddf7a584
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-04-17 18:11:27 +0200

    [plugins] IJPL-181065 migrate DynamicPluginsTestUtil to use plugin init context
    GitOrigin-RevId: 458f7269a7104307b1bce47799258c80671683e9

--- platform/platform-tests/testSrc/com/intellij/ide/plugins/DynamicPluginsTestUtil.kt
+++ platform/platform-tests/testSrc/com/intellij/ide/plugins/DynamicPluginsTestUtil.kt
@@ -23,16 +23,19 @@ internal fun loadAndInitDescriptorInTest(
   PluginManagerCore.getAndClearPluginLoadingErrors()
 
   val buildNumber = BuildNumber.fromString("2042.42")!!
-  val context = DescriptorListLoadingContext(
-    customDisabledPlugins = disabledPlugins.mapTo(LinkedHashSet(), PluginId::getId),
-    customExpiredPlugins = emptySet(),
-    customBrokenPluginVersions = emptyMap(),
+  val loadingContext = DescriptorListLoadingContext(
+    getProductBuildNumber = { buildNumber }
+  )
+  val initContext = PluginInitializationContext.build(
+    disabledPlugins = disabledPlugins.mapTo(LinkedHashSet(), PluginId::getId),
+    expiredPlugins = emptySet(),
+    brokenPluginVersions = emptyMap(),
     getProductBuildNumber = { buildNumber }
   )
   val result = runBlocking {
     loadDescriptorFromFileOrDirInTests(
       file = dir,
-      loadingContext = context,
+      loadingContext = loadingContext,
       isBundled = isBundled,
     )
   }
@@ -40,7 +43,7 @@ internal fun loadAndInitDescriptorInTest(
     assertThat(PluginManagerCore.getAndClearPluginLoadingErrors()).isNotEmpty()
     throw AssertionError("Cannot load plugin from $dir")
   }
-  result.initialize(context = context)
+  result.initialize(context = initContext)
   return result
 }
 
