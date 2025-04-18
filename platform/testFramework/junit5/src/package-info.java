2a6109afe2077741cbd0180743d3a63e92965ae8
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-04-18 13:52:35 +0000

    [test framework] add some documentation
    Also includes some Grazie-suggested typo fixes.
    
    Merge-request: IJ-MR-159365
    Merged-by: Bartek Pacia <bartek.pacia@jetbrains.com>
    
    GitOrigin-RevId: 60ac557d8a16d35dd2b61d748d805b9a2e1e8143

---
+++ platform/testFramework/junit5/src/package-info.java
@@ -0,0 +1,10 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+/**
+ * Provides a JUnit 5-based test framework for writing tests which use IDE projects, PSI and other services.
+ * <p>
+ * For examples, see {@link com.intellij.testFramework.junit5.showcase}.
+ * <p>
+ * This package is a successor to {@link com.intellij.testFramework}.
+ */
+package com.intellij.testFramework.junit5;
