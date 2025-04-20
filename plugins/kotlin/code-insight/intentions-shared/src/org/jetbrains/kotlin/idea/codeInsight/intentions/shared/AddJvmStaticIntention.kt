f9289fcd2c1678a38b0e51a76630dafbc7a07e4b
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2025-04-20 17:02:33 +0400

    [kotlin] Make `AddJvmStaticIntention` a shared intention
    #KTIJ-32004 Fixed
    
    GitOrigin-RevId: baf432e06091ce03fdf33db73c149e81e4a10503

--- plugins/kotlin/code-insight/intentions-shared/src/org/jetbrains/kotlin/idea/codeInsight/intentions/shared/AddJvmStaticIntention.kt
+++ plugins/kotlin/code-insight/intentions-shared/src/org/jetbrains/kotlin/idea/codeInsight/intentions/shared/AddJvmStaticIntention.kt
@@ -1,6 +1,5 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-
-package org.jetbrains.kotlin.idea.intentions
+package org.jetbrains.kotlin.idea.codeInsight.intentions.shared
 
 import com.intellij.codeInsight.intention.LowPriorityAction
 import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo
@@ -16,16 +15,16 @@ import com.intellij.psi.search.searches.ReferencesSearch
 import org.jetbrains.kotlin.asJava.classes.KtLightClass
 import org.jetbrains.kotlin.asJava.elements.KtLightElement
 import org.jetbrains.kotlin.asJava.elements.KtLightField
+import org.jetbrains.kotlin.idea.base.psi.KotlinPsiHeuristics
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.base.util.codeUsageScope
 import org.jetbrains.kotlin.idea.base.util.restrictByFileType
 import org.jetbrains.kotlin.idea.codeinsight.api.classic.intentions.SelfTargetingRangeIntention
 import org.jetbrains.kotlin.idea.core.util.runSynchronouslyWithProgress
 import org.jetbrains.kotlin.idea.util.addAnnotation
-import org.jetbrains.kotlin.idea.util.findAnnotation
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.load.java.JvmAbi
-import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.JvmStandardClassIds
 import org.jetbrains.kotlin.psi.KtNamedDeclaration
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtObjectDeclaration
@@ -33,11 +32,9 @@ import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
 
 class AddJvmStaticIntention : SelfTargetingRangeIntention<KtNamedDeclaration>(
-    KtNamedDeclaration::class.java,
-    KotlinBundle.lazyMessage("add.jvmstatic.annotation")
+  KtNamedDeclaration::class.java,
+  KotlinBundle.lazyMessage("add.jvmstatic.annotation")
 ), LowPriorityAction {
-    private val JvmStaticFqName = FqName("kotlin.jvm.JvmStatic")
-    private val JvmFieldFqName = FqName("kotlin.jvm.JvmField")
 
     override fun startInWriteAction() = false
 
@@ -52,15 +49,15 @@ class AddJvmStaticIntention : SelfTargetingRangeIntention<KtNamedDeclaration>(
         if (containingObject.isObjectLiteral()) return null
         if (element is KtProperty) {
             if (element.hasModifier(KtTokens.CONST_KEYWORD)) return null
-            if (element.findAnnotation(JvmFieldFqName) != null) return null
+            if (KotlinPsiHeuristics.hasJvmFieldAnnotation(element)) return null
         }
-        if (element.findAnnotation(JvmStaticFqName) != null) return null
+        if (KotlinPsiHeuristics.hasJvmStaticAnnotation(element)) return null
         return element.nameIdentifier?.textRange
     }
 
     override fun generatePreview(project: Project, editor: Editor, file: PsiFile): IntentionPreviewInfo {
         val target = getTarget(editor, file) ?: return IntentionPreviewInfo.EMPTY
-        target.addAnnotation(JvmStaticFqName)
+        target.addAnnotation(JvmStandardClassIds.Annotations.JvmStatic)
         return IntentionPreviewInfo.DIFF
     }
 
@@ -73,27 +70,27 @@ class AddJvmStaticIntention : SelfTargetingRangeIntention<KtNamedDeclaration>(
 
         val expressionsToReplaceWithQualifier =
             project.runSynchronouslyWithProgress(KotlinBundle.message("looking.for.usages.in.java.files"), true) {
-                runReadAction {
-                    val searchScope = element.codeUsageScope().restrictByFileType(JavaFileType.INSTANCE)
-                    ReferencesSearch
-                        .search(element, searchScope)
-                        .asIterable()
-                        .mapNotNull {
-                            val refExpr = it.element as? PsiReferenceExpression ?: return@mapNotNull null
-                            if ((refExpr.resolve() as? KtLightElement<*, *>)?.kotlinOrigin != element) return@mapNotNull null
-                            val qualifierExpr = refExpr.qualifierExpression as? PsiReferenceExpression ?: return@mapNotNull null
-                            if (qualifierExpr.qualifierExpression == null) return@mapNotNull null
-                            val instanceField = qualifierExpr.resolve() as? KtLightField ?: return@mapNotNull null
-                            if (instanceField.name != instanceFieldName) return@mapNotNull null
-                            if ((instanceField.containingClass as? KtLightClass)?.kotlinOrigin != instanceFieldContainingClass) return@mapNotNull null
-                            qualifierExpr
-                        }
-                }
+              runReadAction {
+                val searchScope = element.codeUsageScope().restrictByFileType(JavaFileType.INSTANCE)
+                ReferencesSearch
+                  .search(element, searchScope)
+                  .asIterable()
+                  .mapNotNull {
+                    val refExpr = it.element as? PsiReferenceExpression ?: return@mapNotNull null
+                    if ((refExpr.resolve() as? KtLightElement<*, *>)?.kotlinOrigin != element) return@mapNotNull null
+                    val qualifierExpr = refExpr.qualifierExpression as? PsiReferenceExpression ?: return@mapNotNull null
+                    if (qualifierExpr.qualifierExpression == null) return@mapNotNull null
+                    val instanceField = qualifierExpr.resolve() as? KtLightField ?: return@mapNotNull null
+                    if (instanceField.name != instanceFieldName) return@mapNotNull null
+                    if ((instanceField.containingClass as? KtLightClass)?.kotlinOrigin != instanceFieldContainingClass) return@mapNotNull null
+                    qualifierExpr
+                  }
+              }
             } ?: return
 
-        runWriteAction {
-            element.addAnnotation(JvmStaticFqName)
-            expressionsToReplaceWithQualifier.forEach { it.replace(it.qualifierExpression!!) }
-        }
+      runWriteAction {
+        element.addAnnotation(JvmStandardClassIds.Annotations.JvmStatic)
+        expressionsToReplaceWithQualifier.forEach { it.replace(it.qualifierExpression!!) }
+      }
     }
 }
