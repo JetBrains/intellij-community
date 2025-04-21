b7c0d033c7d995b639c43a595f5ff101ca075de3
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2025-04-21 11:29:05 +0200

    [kotlin] Fix dsl marker highlighting for functions and ext. functions
    #KTIJ-33856
    
    
    (cherry picked from commit b307dac32d5e3f9deed8155c91e76b7d157da56a)
    
    IJ-CR-160797
    
    GitOrigin-RevId: 26e852abad26ea351eb267e6b3f55faab77b7fe1

--- plugins/kotlin/idea/tests/testData/highlighterMetaInfo/dsl/RegularDsl.kt
+++ plugins/kotlin/idea/tests/testData/highlighterMetaInfo/dsl/RegularDsl.kt
@@ -6,10 +6,20 @@ annotation class Dsl1
 @Dsl1
 class ForDsl
 
+@Dsl1
+fun merge(f: () -> Unit) {
+}
+
 fun foo(f: ForDsl.() -> Unit) {}
+
+fun ForDsl.bar(f: () -> Unit) {
+}
+
 
 fun test() {
     foo {
 
     }
+    merge {}
+    ForDsl().bar {}
 }
