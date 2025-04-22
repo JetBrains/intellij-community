4544e6912445852faf5a4bb212939da9ef06538e
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2025-04-22 12:24:32 +0300

    Consent settings: cleanup unused import (IJPL-183359, LLM-6595)
    IJ-CR-157485
    
    GitOrigin-RevId: db78ed0c896f7d38228ab22efaf09284b38ce00e

--- platform/platform-impl/src/com/intellij/ide/gdpr/ui/consents/ConsentUi.kt
+++ platform/platform-impl/src/com/intellij/ide/gdpr/ui/consents/ConsentUi.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.gdpr.ui.consents
 
-import com.intellij.ide.gdpr.Consent
 import com.intellij.openapi.util.NlsSafe
 
 interface ConsentUi {
