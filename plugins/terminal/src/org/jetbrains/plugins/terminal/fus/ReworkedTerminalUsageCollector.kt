72206bbfdadc88fea366d47353e39e1b9669ab4d
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-04-22 18:20:05 +0300

    [terminal] IJPL-183992 Bump the terminal FUS group version
    (cherry picked from commit 7c8038196a1818dfd46b04e853606f6f456b93cb)
    
    IJ-CR-160917
    
    GitOrigin-RevId: b1ebd05326d32a7f6ce28550c3babdd97382f4fe

--- plugins/terminal/src/org/jetbrains/plugins/terminal/fus/ReworkedTerminalUsageCollector.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/fus/ReworkedTerminalUsageCollector.kt
@@ -20,7 +20,7 @@ private const val GROUP_ID = "terminal"
 object ReworkedTerminalUsageCollector : CounterUsagesCollector() {
   override fun getGroup(): EventLogGroup = GROUP
 
-  private val GROUP = EventLogGroup(GROUP_ID, 4)
+  private val GROUP = EventLogGroup(GROUP_ID, 5)
 
   private val OS_VERSION_FIELD = EventFields.StringValidatedByRegexpReference("os-version", "version")
   private val SHELL_STR_FIELD = EventFields.String("shell", KNOWN_SHELLS.toList())
