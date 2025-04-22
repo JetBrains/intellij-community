8a88929f90361a19d55a4e267a7f2004dafe066b
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-04-22 14:45:24 +0300

    IJPL-184686 Make isExpanded(int) safe for base class constructors
    (cherry picked from commit 637fcec0331d1bf54645ae7c2a206f6b695de6a1)
    
    IJ-CR-160923
    
    GitOrigin-RevId: f9151735d888e62b38f92d7ce567977f192193c4

--- platform/platform-tests/testSrc/com/intellij/ui/tree/TreeBaseConstructorSafetyTest.kt
+++ platform/platform-tests/testSrc/com/intellij/ui/tree/TreeBaseConstructorSafetyTest.kt
@@ -205,6 +205,32 @@ class TreeBaseConstructorSafetyTest {
     """.trimIndent())
   }
 
+  @Test
+  fun `calling isExpanded(int) from setModel is safe`() {
+    sut = object : Tree(createModel("""
+      root
+        node1
+          leaf11
+          leaf12
+    """.trimIndent())) {
+      init {
+        isRootVisible = true
+      }
+
+      override fun setModel(newModel: TreeModel?) {
+        super.setModel(newModel)
+        expandPath(path("root", "node1"))
+        assertThat(isExpanded(1)).isTrue()
+      }
+    }
+    assertTreeStructure("""
+      -root
+        -node1
+          leaf11
+          leaf12
+    """.trimIndent())
+  }
+
   private fun assertTreeStructure(structure: String) {
     val actualStructure = dumpTreeStructure()
     // these LFs make the output more readable
