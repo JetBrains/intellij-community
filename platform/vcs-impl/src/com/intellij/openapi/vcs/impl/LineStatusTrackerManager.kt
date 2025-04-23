ba84c4d92bd444957069d12c87c232a8c6beb1d6
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2025-04-23 18:28:27 +0200

    Better check for disposed project
    GitOrigin-RevId: 8d7fd591373728775cb3a266b983de6a96a7372b

--- platform/vcs-impl/src/com/intellij/openapi/vcs/impl/LineStatusTrackerManager.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/impl/LineStatusTrackerManager.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 @file:Suppress("ReplacePutWithAssignment")
 
 package com.intellij.openapi.vcs.impl
@@ -481,8 +481,8 @@ class LineStatusTrackerManager(
 
   @RequiresEdt
   private fun releaseTracker(document: Document, wasUnbound: Boolean = false) {
+    if (project.isDisposed) return
     val data = trackers.remove(document) ?: return
-
     BackgroundTaskUtil.syncPublisher(project, TOPIC).onTrackerRemoved(data.tracker)
     unregisterTrackerInCLM(data, wasUnbound)
     data.tracker.release()
