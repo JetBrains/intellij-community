110acaf0a4347f3b8bd2a0523826e57dc0394a01
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2025-04-23 20:12:09 +0200

    IJPL-185084 git: Add Commit action for staging area in the Quick Actions
    (cherry picked from commit 6966d22ae9dc1204d16d996c4f050f201de94815)
    
    IJ-MR-161047
    
    GitOrigin-RevId: 491cd34a061dd4707806f7b65e68d4bceeaa531d

--- plugins/git4idea/frontend/src/com/intellij/vcs/git/frontend/GitQuickActions.kt
+++ plugins/git4idea/frontend/src/com/intellij/vcs/git/frontend/GitQuickActions.kt
@@ -10,6 +10,6 @@ private val insertStrategy = GroupEnd("MainToolbarNewUI", "MainToolbarVCSGroup")
   .orElse(GroupEnd("MainToolbarNewUI", IdeActions.GROUP_MAIN_TOOLBAR_LEFT))
 
 class UpdatePushQuickAction: ToolbarAddQuickActionInfo(listOf("Vcs.UpdateProject", "Vcs.Push"), GitFrontendBundle.message("MainToolbarQuickActions.Git.UpdatePush.text"), AllIcons.Actions.CheckOut, insertStrategy)
-class CommitQuickAction: ToolbarAddQuickActionInfo(listOf("CheckinProject"), GitFrontendBundle.message("MainToolbarQuickActions.Git.Commit.text"), AllIcons.Actions.Commit, insertStrategy)
+class CommitQuickAction: ToolbarAddQuickActionInfo(listOf("CheckinProject", "Git.Commit.Stage"), GitFrontendBundle.message("MainToolbarQuickActions.Git.Commit.text"), AllIcons.Actions.Commit, insertStrategy)
 class HistoryQuickAction: ToolbarAddQuickActionInfo(listOf("Vcs.ShowTabbedFileHistory"), GitFrontendBundle.message("MainToolbarQuickActions.Git.History.text"), AllIcons.Vcs.History, insertStrategy)
 class RollbackQuickAction: ToolbarAddQuickActionInfo(listOf("ChangesView.Revert"), GitFrontendBundle.message("MainToolbarQuickActions.Git.Rollback.text"), AllIcons.Diff.Revert, insertStrategy)
