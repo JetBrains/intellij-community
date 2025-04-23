87a5090585cd144cc74ce5707647225470f2662d
Ilya Kazakevich <ilya.kazakevich@jetbrains.com>
2025-04-23 18:32:37 +0000

    Python: Do not access the Registry in `SystemPythonServiceImpl` ctor as it might not be available at that moment.
    If we access `SystemPythonServiceImpl` before the Application is fully loaded (i.e. `com.intellij.python.junit5Tests.env.systemPython.SystemPythonRootsFixer.execute`), we might encounter an error:
    ```
     Registry key python.system.refresh.minutes is not defined
     ```
    
     With this change we access Registry lazily and, unlike `Registry.get`, `RegistryManager` async service is fully safe: it just suspends until the Registry is available.
    
    
    Merge-request: IJ-MR-160727
    Merged-by: Ilya Kazakevich <ilya.kazakevich@jetbrains.com>
    
    GitOrigin-RevId: ef56cfee7b15907c7c99b859b5704b6fcef66b01

--- python/services/system-python/src/com/intellij/python/community/services/systemPython/impl/SystemPythonInitialLoader.kt
+++ python/services/system-python/src/com/intellij/python/community/services/systemPython/impl/SystemPythonInitialLoader.kt
@@ -6,14 +6,14 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.platform.eel.provider.getEelDescriptor
 import com.intellij.python.community.services.systemPython.SystemPythonService
-import com.intellij.python.community.services.systemPython.cacheTimeout
+import com.intellij.python.community.services.systemPython.getCacheTimeout
 
 private val logger = fileLogger()
 
 // Preload pythons as soon as a project gets loaded
 internal class SystemPythonInitialLoader : ProjectActivity {
   override suspend fun execute(project: Project) {
-    if (cacheTimeout == null) return // Cache is disabled, no need to preload it
+    if (getCacheTimeout() == null) return // Cache is disabled, no need to preload it
     logger.debug("Preloading pythons for $project")
     SystemPythonService().findSystemPythons(project.getEelDescriptor().upgrade())
   }
