d127c9a3ed3f0d1401d5ffeab66d22bbc9232941
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2025-04-24 01:38:05 +0200

    IJ-CR-160532 do not highlight parser errors twice for java in RD
    GitOrigin-RevId: be3ad94e93dd113c1aae1fb6c2b7f5ca69750ed6

--- java/java-frontback-impl/src/com/intellij/codeInsight/daemon/impl/analysis/JavaHighlightErrorFilter.java
+++ java/java-frontback-impl/src/com/intellij/codeInsight/daemon/impl/analysis/JavaHighlightErrorFilter.java
@@ -2,13 +2,19 @@
 package com.intellij.codeInsight.daemon.impl.analysis;
 
 import com.intellij.codeInsight.highlighting.HighlightErrorFilter;
+import com.intellij.idea.AppMode;
 import com.intellij.java.codeserver.highlighting.JavaSyntaxErrorChecker;
+import com.intellij.lang.java.JavaLanguage;
 import com.intellij.psi.PsiErrorElement;
 import org.jetbrains.annotations.NotNull;
 
 public final class JavaHighlightErrorFilter extends HighlightErrorFilter {
   @Override
   public boolean shouldHighlightErrorElement(@NotNull PsiErrorElement element) {
+    // todo we should handle it on the platform side
+    if (element.getLanguage() == JavaLanguage.INSTANCE && AppMode.isRemoteDevHost()) {
+      return false;
+    }
     return JavaSyntaxErrorChecker.shouldHighlightErrorElement(element);
   }
 }
