ef8a49bea1d095cc6ef344b228e78caa1eaa0fb9
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2025-04-23 12:13:31 +0300

    [Java. Tests. JUnit] Prefer junit-jupiter-vintage if it is present in classpath
    #IDEA-343605 fixed
    
    GitOrigin-RevId: 2c24e4b27ae327859ab1519945181e15e50b9ed1

---
+++ plugins/junit5_rt_tests/testData/integration/vintageEngine/test/v3/TestClass.java
@@ -0,0 +1,8 @@
+package v3;
+
+import junit.framework.TestCase;
+
+public class TestClass extends TestCase {
+  public void testSimple() {
+  }
+}
---
+++ plugins/junit5_rt_tests/testData/integration/vintageEngine/test/v4/TestClass.java
@@ -0,0 +1,9 @@
+package v4;
+
+import org.junit.Test;
+
+public class TestClass {
+  @Test
+  public void simple() {
+  }
+}
