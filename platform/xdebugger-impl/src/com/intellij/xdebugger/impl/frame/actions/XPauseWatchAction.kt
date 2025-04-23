b913b727e8671db6fb6a892342b2ab8beb9a57db
Maksim Zuev <maksim.zuev@jetbrains.com>
2025-04-23 16:02:07 +0200

    [debugger] IJPL-185111 Allow muting all selected watches
    GitOrigin-RevId: c8670bc457e30a5ed7f58d4de5897e1e9e2431d7

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/frame/actions/XPauseWatchAction.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/frame/actions/XPauseWatchAction.kt
@@ -6,9 +6,9 @@ import com.intellij.idea.ActionsBundle
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification
+import com.intellij.xdebugger.XDebuggerBundle
 import com.intellij.xdebugger.frame.XValuePlace
 import com.intellij.xdebugger.impl.frame.XWatchesView
-import com.intellij.xdebugger.impl.frame.actions.XWatchesTreeActionBase.getSelectedNodes
 import com.intellij.xdebugger.impl.ui.tree.XDebuggerTree
 import com.intellij.xdebugger.impl.ui.tree.nodes.WatchNodeImpl
 
@@ -16,8 +16,8 @@ private class XPauseWatchAction : XWatchesTreeActionBase(), ActionRemoteBehavior
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
 
   override fun isEnabled(e: AnActionEvent, tree: XDebuggerTree): Boolean {
-    val selectedNode = getSelectedWatchNode(tree) ?: return false
-    return selectedNode.xWatch.canBePaused
+    val selectedNodes = getSelectedNodes(tree, WatchNodeImpl::class.java)
+    return selectedNodes.any { it.xWatch.canBePaused }
   }
 
   override fun update(e: AnActionEvent) {
@@ -25,29 +25,36 @@ private class XPauseWatchAction : XWatchesTreeActionBase(), ActionRemoteBehavior
     e.presentation.isVisible = e.presentation.isEnabled
     if (e.presentation.isEnabled) {
       val tree = XDebuggerTree.getTree(e) ?: return
-      val node = getSelectedWatchNode(tree) ?: return
-      val paused = node.xWatch.isPaused
+      val selectedNodes = getSelectedNodes(tree, WatchNodeImpl::class.java)
+        .filter { it.xWatch.canBePaused }
+      // Resume action is shown only if all the breakpoints are paused
+      val paused = selectedNodes.all { it.xWatch.isPaused }
+      val watchText =
+        if (selectedNodes.size > 1) XDebuggerBundle.message("xdebugger.watches")
+        else XDebuggerBundle.message("xdebugger.watch")
       e.presentation.text =
-        if (paused) ActionsBundle.message("action.XDebugger.ResumeWatch.text")
-        else ActionsBundle.message("action.XDebugger.PauseWatch.text")
+        if (paused) XDebuggerBundle.message("action.XDebugger.ResumeWatch.template.text", watchText)
+        else XDebuggerBundle.message("action.XDebugger.PauseWatch.template.text", watchText)
       e.presentation.icon = if (paused) AllIcons.Toolwindows.ToolWindowRun else AllIcons.Actions.Pause
     }
   }
 
   override fun perform(e: AnActionEvent, tree: XDebuggerTree, watchesView: XWatchesView) {
-    val node = getSelectedWatchNode(tree) ?: return
-    val paused = node.xWatch.isPaused
-    node.xWatch.isPaused = !paused
-    if (!paused) {
-      // only update icon, keep calculated value
-      node.setPresentation(AllIcons.Actions.Pause, node.valuePresentation!!, !node.isLeaf)
-    }
-    else {
-      // resume watch, trigger value evaluation
-      node.valueContainer.computePresentation(node, XValuePlace.TREE)
+    val selectedNodes = getSelectedNodes(tree, WatchNodeImpl::class.java)
+      .filter { it.xWatch.canBePaused }
+    val paused = selectedNodes.all { it.xWatch.isPaused }
+    for (node in selectedNodes) {
+      // Skip nodes that are already in the correct state
+      if (node.xWatch.isPaused != paused) continue
+      node.xWatch.isPaused = !paused
+      if (!paused) {
+        // only update icon, keep calculated value
+        node.setPresentation(AllIcons.Actions.Pause, node.valuePresentation!!, !node.isLeaf)
+      }
+      else {
+        // resume watch, trigger value evaluation
+        node.valueContainer.computePresentation(node, XValuePlace.TREE)
+      }
     }
   }
 }
-
-private fun getSelectedWatchNode(tree: XDebuggerTree): WatchNodeImpl? =
-  getSelectedNodes(tree, WatchNodeImpl::class.java).singleOrNull()
