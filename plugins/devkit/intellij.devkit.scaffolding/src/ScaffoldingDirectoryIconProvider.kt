f0b6ee6a34d56de97d653f1dc93fdf54d8347487
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2025-04-24 22:20:52 +0200

    [devkit] show icons for modules and plugin directories with source roots
    GitOrigin-RevId: a1aaa1ca34bf6137157fadc53f3e229b91bd88c7

---
+++ plugins/devkit/intellij.devkit.scaffolding/src/ScaffoldingDirectoryIconProvider.kt
@@ -0,0 +1,45 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.devkit.scaffolding
+
+import com.intellij.icons.AllIcons
+import com.intellij.ide.IconProvider
+import com.intellij.openapi.project.IntelliJProjectUtil
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiElement
+import com.intellij.ui.LayeredIcon
+import com.intellij.util.IconUtil
+import javax.swing.Icon
+import javax.swing.SwingConstants
+
+internal class ScaffoldingDirectoryIconProvider : IconProvider() {
+
+  override fun getIcon(element: PsiElement, flags: Int): Icon? {
+    if (!Registry.`is`("devkit.plugin.directory.icons")) {
+      return null
+    }
+    if (element is PsiDirectory) {
+      val project = element.project
+      if (IntelliJProjectUtil.isIntelliJPlatformProject(project)) {
+        val kind = directoryKind(project, element.virtualFile)
+        return when (kind) {
+          DirectoryKind.PLUGIN -> AllIcons.Nodes.Plugin
+          DirectoryKind.LEGACY_PLUGIN_WITH_MAIN_MODULE -> legacyPluginWithModuleIcon
+          DirectoryKind.MODULE -> moduleDirectoryIcon
+          null -> null
+        }
+      }
+    }
+    return null
+  }
+}
+
+private val moduleDirectoryIcon = LayeredIcon(2).apply {
+  setIcon(AllIcons.Nodes.Plugin, 0)
+  setIcon(AllIcons.Nodes.Module8x8, 1, SwingConstants.SOUTH_EAST)
+}
+
+private val legacyPluginWithModuleIcon = LayeredIcon(2).apply {
+  setIcon(AllIcons.Nodes.Plugin, 0)
+  setIcon(IconUtil.scale(AllIcons.Actions.ModuleDirectory, ancestor = null, 0.7f), 1, SwingConstants.SOUTH_EAST)
+}
