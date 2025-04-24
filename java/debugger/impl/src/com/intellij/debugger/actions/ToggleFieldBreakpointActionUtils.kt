87fa6a884f335e83fc8504dd20a8d2c49066a1e9
Maksim Zuev <maksim.zuev@jetbrains.com>
2025-04-24 11:03:22 +0200

    [debugger] EA-1720304 Avoid calling runBlockingMaybeCancellable from EDT
    * replace with runWithModalProgressBlocking
    
    GitOrigin-RevId: 63aeb5b138142786e6429ce3f420b90273af9818

--- java/debugger/impl/src/com/intellij/debugger/actions/ToggleFieldBreakpointActionUtils.kt
+++ java/debugger/impl/src/com/intellij/debugger/actions/ToggleFieldBreakpointActionUtils.kt
@@ -1,17 +1,18 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.debugger.actions
 
+import com.intellij.debugger.JavaDebuggerBundle
 import com.intellij.debugger.SourcePosition
 import com.intellij.debugger.engine.SourcePositionProvider.Companion.getSourcePosition
 import com.intellij.debugger.engine.withDebugContext
 import com.intellij.debugger.impl.DebuggerContextImpl
 import com.intellij.debugger.ui.impl.watch.NodeDescriptorImpl
-import com.intellij.openapi.progress.runBlockingMaybeCancellable
+import com.intellij.platform.ide.progress.runWithModalProgressBlocking
 
 internal fun getSourcePositionNow(
   debuggerContext: DebuggerContextImpl,
   descriptor: NodeDescriptorImpl,
-): SourcePosition? = runBlockingMaybeCancellable {
+): SourcePosition? = runWithModalProgressBlocking(debuggerContext.project, JavaDebuggerBundle.message("compute.field.breakpoint")) {
   withDebugContext(debuggerContext) {
     getSourcePosition(descriptor, debuggerContext.project, debuggerContext)
   }
