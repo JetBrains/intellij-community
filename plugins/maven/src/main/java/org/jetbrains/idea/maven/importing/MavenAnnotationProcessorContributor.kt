152732e901fb4ae67b27f05985c3fc15d9951782
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2025-04-24 20:10:19 +0200

    [maven][IDEA-368907] do not import annotation processor for non-compile phases
    (cherry picked from commit 3fb229d91b45eb7bc161d4f24ed54744db383039)
    
    GitOrigin-RevId: 62c8fca3dfa2e254579cc8c62c92d7b1a2763fd9

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/MavenAnnotationProcessorContributor.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/MavenAnnotationProcessorContributor.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.util.ExceptionUtil
 import org.jetbrains.idea.maven.execution.SyncBundle
 import org.jetbrains.idea.maven.importing.MavenAnnotationProcessorConfiguratorUtil.getProcessorArtifactInfos
-import org.jetbrains.idea.maven.importing.MavenImportUtil.getAllCompilerConfigs
+import org.jetbrains.idea.maven.importing.MavenImportUtil.compilerConfigsForCompilePhase
 import org.jetbrains.idea.maven.model.MavenArtifactInfo
 import org.jetbrains.idea.maven.model.MavenId
 import org.jetbrains.idea.maven.project.MavenProject
@@ -17,7 +17,7 @@ import org.jetbrains.idea.maven.utils.MavenJDOMUtil
 
 internal class MavenAnnotationProcessorContributor : MavenProjectResolutionContributor {
   override suspend fun onMavenProjectResolved(project: Project, mavenProject: MavenProject, embedder: MavenEmbedderWrapper) {
-    val artifactsInfo = mavenProject.getAllCompilerConfigs()
+    val artifactsInfo = mavenProject.compilerConfigsForCompilePhase()
       .mapNotNull { MavenJDOMUtil.findChildByPath(it, "annotationProcessorPaths") }
       .flatMap { getProcessorArtifactInfos(it, mavenProject) }
 
