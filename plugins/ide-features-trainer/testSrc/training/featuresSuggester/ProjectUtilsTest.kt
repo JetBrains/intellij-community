d6879b09525964ade533b2a00e34b0170b97c209
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2025-04-26 18:08:16 +0200

    PY-78817: Dependency was set to the wrong module
    IJ-MR-160630
    
    
    (cherry picked from commit 795c0704e0206174ce69a914c0bb94190a2cb774)
    
    IJ-CR-161315
    
    GitOrigin-RevId: 72d3bc21147687b52b2f11be46d1eed7daeea74a

--- plugins/ide-features-trainer/testSrc/training/featuresSuggester/ProjectUtilsTest.kt
+++ plugins/ide-features-trainer/testSrc/training/featuresSuggester/ProjectUtilsTest.kt
@@ -1,19 +1,20 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package training.project
+package training.featuresSuggester
 
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.testFramework.common.timeoutRunBlocking
 import com.intellij.testFramework.junit5.TestApplication
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
-import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
 import training.lang.LangManager
 import training.lang.LangSupport
+import training.project.ProjectUtils
 import java.nio.file.Path
 import kotlin.io.path.*
 
@@ -29,7 +30,7 @@ class ProjectUtilsTest {
     withContext(Dispatchers.IO) {
       val protectedFile = root.resolve("excludedDir").resolve("excludedFile").createParentDirectories().createFile()
 
-      val lang = Mockito.mock<LangSupport>()
+      val lang = mock<LangSupport>()
       `when`(lang.getLearningProjectPath(any())).thenReturn(root)
       `when`(lang.getContentRootPath(any())).thenReturn(root)
       `when`(lang.getProtectedDirs(any())).thenReturn(setOf(protectedFile.parent))
@@ -42,11 +43,11 @@ class ProjectUtilsTest {
         root.resolve("venv").createDirectory(),
         root.resolve(".git").resolve("file").createParentDirectories().createFile())
 
-      LangManager.getInstance().setLearningProjectPath(lang, root.pathString)
+      LangManager.Companion.getInstance().setLearningProjectPath(lang, root.pathString)
       ProjectUtils.restoreProject(lang, ProjectManager.getInstance().defaultProject)
-      assertFalse(badFile.exists(), "$badFile should have been deleted")
+      Assertions.assertFalse(badFile.exists(), "$badFile should have been deleted")
       for (goodFile in goodFiles) {
-        assertTrue(goodFile.exists(), "$goodFile shouldn't have been deleted")
+        Assertions.assertTrue(goodFile.exists(), "$goodFile shouldn't have been deleted")
       }
     }
   }
