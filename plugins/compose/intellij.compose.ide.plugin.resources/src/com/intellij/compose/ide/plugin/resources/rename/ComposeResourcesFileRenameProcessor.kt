bbae34278407ab62c2b39a5ed5e524185fac2b02
Alexandru Resiga <alexandru.resiga@jetbrains.com>
2025-04-28 00:44:17 +0200

    KMT-917 [compose resources find usages] find usages from resource file/string values
    - extract common hierarchy through ComposeResourcesBase, ComposeResourcesFileBase, ComposeResourcesXmlBase
    
    GitOrigin-RevId: 8951fa54cf8ad4cfe3cd559917719d4ca22cf4ec

--- plugins/compose/intellij.compose.ide.plugin.resources/src/com/intellij/compose/ide/plugin/resources/rename/ComposeResourcesFileRenameProcessor.kt
+++ plugins/compose/intellij.compose.ide.plugin.resources/src/com/intellij/compose/ide/plugin/resources/rename/ComposeResourcesFileRenameProcessor.kt
@@ -1,17 +1,12 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.compose.ide.plugin.resources.rename
 
-import com.intellij.compose.ide.plugin.resources.getAllComposeResourcesDirs
-import com.intellij.compose.ide.plugin.resources.isValidInnerComposeResourcesDirName
-import com.intellij.psi.PsiBinaryFile
+import com.intellij.compose.ide.plugin.resources.ComposeResourcesFileBase
+import com.intellij.compose.ide.plugin.resources.withoutExtension
 import com.intellij.psi.PsiElement
-import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.refactoring.rename.RenamePsiFileProcessor
-import org.jetbrains.kotlin.asJava.namedUnwrappedElement
 import org.jetbrains.kotlin.idea.refactoring.rename.RenameKotlinPropertyProcessor
 import org.jetbrains.kotlin.idea.stubindex.KotlinPropertyShortNameIndex
-import org.jetbrains.kotlin.psi.KtProperty
-import org.jetbrains.kotlin.psi.psiUtil.isPublic
 
 /**
  * Rename Compose resource files and propagate to code usages
@@ -19,24 +14,12 @@ import org.jetbrains.kotlin.psi.psiUtil.isPublic
  * Extracts the specific Compose resource property from [KotlinPropertyShortNameIndex]
  * and delegates the rename to [RenameKotlinPropertyProcessor]
  * */
-internal class ComposeResourcesFileRenameProcessor : RenamePsiFileProcessor() {
-  override fun canProcessElement(element: PsiElement): Boolean {
-    val psiFile = element as? PsiBinaryFile ?: return false
-    val parentName = psiFile.parent?.name ?: return false
-    if (!parentName.isValidInnerComposeResourcesDirName) return false
-    val composeResourcesDir = psiFile.parent?.parent?.virtualFile ?: return false
-    return element.project.getAllComposeResourcesDirs().any { it.directoryPath == composeResourcesDir.toNioPath() }
-  }
+internal class ComposeResourcesFileRenameProcessor : RenamePsiFileProcessor(), ComposeResourcesFileBase {
+  override fun canProcessElement(element: PsiElement): Boolean = isComposeResourcesElement(element)
 
   override fun prepareRenaming(element: PsiElement, newName: String, allRenames: MutableMap<PsiElement, String>) {
-    val project = element.project
-    val projectScope = GlobalSearchScope.projectScope(project)
-    val nameWithoutExtension = element.namedUnwrappedElement?.name?.withoutExtension ?: return
-    val declaration = KotlinPropertyShortNameIndex[nameWithoutExtension, project, projectScope].firstOrNull { !it.isPublic } ?: return
-    val property = declaration as? KtProperty ?: return
+    val property = getKotlinPropertyFromComposeResource(element) ?: return
     val newNameWithoutExtension = newName.withoutExtension
     RenameKotlinPropertyProcessor().prepareRenaming(property, newNameWithoutExtension, allRenames)
   }
 }
-
-private val String.withoutExtension: String get() = substringBeforeLast(".")
