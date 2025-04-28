4b925afc48ceac7870bcd6532bb7216e18f17e9b
Timur Malanin <timur.malanin@jetbrains.com>
2025-04-28 15:18:38 +0200

    Refactor package managers to support dependency extraction.
    GitOrigin-RevId: 3b577f961f61562358fe7acf3c79fef4a249680a

--- python/src/com/jetbrains/python/packaging/conda/common.kt
+++ python/src/com/jetbrains/python/packaging/conda/common.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.packaging.conda
 
+import com.intellij.openapi.components.service
 import com.jetbrains.python.packaging.common.PythonPackage
 import com.jetbrains.python.packaging.common.PythonPackageDetails
 import com.jetbrains.python.packaging.common.PythonPackageSpecification
