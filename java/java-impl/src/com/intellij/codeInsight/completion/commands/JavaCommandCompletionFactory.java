e163e210af375c4eb25f02740cd95780e6c5ef65
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2025-04-28 12:09:55 +0200

    [command-completion] IDEA-371606 Disable command completion for JShell
    GitOrigin-RevId: c6e9f38cce30f84e18e41728cc9fe7a38655eb7d

--- java/java-impl/src/com/intellij/codeInsight/completion/commands/JavaCommandCompletionFactory.java
+++ java/java-impl/src/com/intellij/codeInsight/completion/commands/JavaCommandCompletionFactory.java
@@ -25,6 +25,8 @@ class JavaCommandCompletionFactory implements CommandCompletionFactory, DumbAwar
   @Override
   public boolean isApplicable(@NotNull PsiFile psiFile, int offset) {
     if (!(psiFile instanceof PsiJavaFile)) return false;
+    //Doesn't work well. Disable for now
+    if (psiFile instanceof PsiJShellFile) return false;
     PsiElement elementAt = psiFile.findElementAt(offset);
     if (elementAt == null) return true;
     if (!(elementAt.getParent() instanceof PsiParameterList)) return true;
