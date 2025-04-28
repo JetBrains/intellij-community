0ba0a20b4677d7e0c22b51403340850e768284ac
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-04-28 13:50:26 +0300

    IJPL-185469 Convert TerminalUsageLocalStorage state to immutable
    This is needed to make it thread-safe to safely modify
    it from background threads on the backend.
    
    (cherry picked from commit 69df4dc2de32629fb6c95fc6a975ff0c29e42c5b)
    
    IJ-CR-161379
    
    GitOrigin-RevId: f1d2ef6444362f37bad7fbba8309081884b9c418

--- plugins/terminal/src/org/jetbrains/plugins/terminal/block/reworked/TerminalUsageLocalStorage.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/block/reworked/TerminalUsageLocalStorage.kt
@@ -2,7 +2,10 @@
 package org.jetbrains.plugins.terminal.block.reworked
 
 import com.intellij.openapi.components.*
+import kotlinx.serialization.Serializable
 import org.jetbrains.annotations.ApiStatus
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Note, that this class is about reworked terminal usage.
@@ -11,18 +14,32 @@ import org.jetbrains.annotations.ApiStatus
 @Service
 @State(name = "ReworkedTerminalUsage", storages = [Storage(value = "terminal.xml", roamingType = RoamingType.DISABLED)])
 class TerminalUsageLocalStorage : PersistentStateComponent<TerminalUsageLocalStorage.State> {
-  private var state = State()
+  private val feedbackNotificationShown = AtomicBoolean()
+  private val enterKeyPressedTimes = AtomicInteger()
 
-  override fun getState(): State = state
+  override fun getState(): State = State(
+    feedbackNotificationShown.get(),
+    enterKeyPressedTimes.get(),
+  )
 
   override fun loadState(state: State) {
-    this.state = state
+    feedbackNotificationShown.set(state.feedbackNotificationShown)
+    enterKeyPressedTimes.set(state.enterKeyPressedTimes)
   }
 
-  class State {
-    var feedbackNotificationShown: Boolean = false
-    var enterKeyPressedTimes: Int = 0
+  fun recordFeedbackNotificationShown() {
+    feedbackNotificationShown.set(true)
   }
+
+  fun recordEnterKeyPressed() {
+    enterKeyPressedTimes.incrementAndGet()
+  }
+
+  @Serializable
+  data class State(
+    val feedbackNotificationShown: Boolean = false,
+    val enterKeyPressedTimes: Int = 0,
+  )
 
   companion object {
     @JvmStatic
