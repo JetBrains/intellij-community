a9f5d734f04d9be3041ac0d3c0764c26d36570e3
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-04-28 18:52:59 +0200

    [plugins] IJPL-181065 introduce ContentModule which always has a descriptor
    GitOrigin-RevId: 94fc3bd263ed365580d6a2d22d8511d4541a1c74

---
+++ platform/core-impl/src/com/intellij/ide/plugins/ContentModule.kt
@@ -0,0 +1,11 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide.plugins
+
+import org.jetbrains.annotations.ApiStatus
+
+@ApiStatus.Internal
+interface ContentModule {
+  val name: String
+  val loadingRule: ModuleLoadingRule
+  val descriptor: IdeaPluginDescriptorEx
+}
