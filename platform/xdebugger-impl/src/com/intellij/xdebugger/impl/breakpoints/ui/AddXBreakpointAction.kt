49ce4eaa057e6dba785b3ececff73c9a432d5338
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-04-28 12:02:34 +0200

    [debugger-rd] IJPL-184116: Extract AddXBreakpointAction to separate Kotlin class
    GitOrigin-RevId: ea7ca8839b8c23d083caa1285eb320964982c479

---
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/breakpoints/ui/AddXBreakpointAction.kt
@@ -0,0 +1,30 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.xdebugger.impl.breakpoints.ui
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.xdebugger.breakpoints.XBreakpoint
+import com.intellij.xdebugger.breakpoints.XBreakpointType
+import com.intellij.xdebugger.impl.breakpoints.XBreakpointBase
+import com.intellij.xdebugger.impl.rpc.XBreakpointId
+
+internal class AddXBreakpointAction internal constructor(
+  private val project: Project,
+  private val myType: XBreakpointType<*, *>,
+  private val saveCurrentItem: () -> Unit,
+  private val selectBreakpoint: (breakpointId: XBreakpointId) -> Unit,
+) : AnAction(), DumbAware {
+  init {
+    getTemplatePresentation().setIcon(myType.enabledIcon)
+    getTemplatePresentation().setText(myType.title)
+  }
+  override fun actionPerformed(e: AnActionEvent) {
+    saveCurrentItem()
+    val breakpoint: XBreakpoint<*>? = myType.addBreakpoint(project, null)
+    if (breakpoint is XBreakpointBase<*, *, *>) {
+      selectBreakpoint(breakpoint.breakpointId)
+    }
+  }
+}
