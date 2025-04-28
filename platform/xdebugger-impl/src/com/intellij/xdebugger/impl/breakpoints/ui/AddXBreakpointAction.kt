f0fe4445442622ff55f61cbefb585d5cb5b0992b
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-04-28 12:03:47 +0200

    [debugger-rd] IJPL-184116: Don't use template presentation
    GitOrigin-RevId: 05024663a3a265ff6211cffe11a9b65923291953

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/breakpoints/ui/AddXBreakpointAction.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/breakpoints/ui/AddXBreakpointAction.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.xdebugger.impl.breakpoints.ui
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAware
@@ -10,21 +11,28 @@ import com.intellij.xdebugger.breakpoints.XBreakpointType
 import com.intellij.xdebugger.impl.breakpoints.XBreakpointBase
 import com.intellij.xdebugger.impl.rpc.XBreakpointId
 
-internal class AddXBreakpointAction internal constructor(
+internal class AddXBreakpointAction(
   private val project: Project,
   private val myType: XBreakpointType<*, *>,
   private val saveCurrentItem: () -> Unit,
   private val selectBreakpoint: (breakpointId: XBreakpointId) -> Unit,
 ) : AnAction(), DumbAware {
-  init {
-    getTemplatePresentation().setIcon(myType.enabledIcon)
-    getTemplatePresentation().setText(myType.title)
+
+  override fun update(e: AnActionEvent) {
+    super.update(e)
+    e.presentation.icon = myType.enabledIcon
+    e.presentation.text = myType.title
   }
+
   override fun actionPerformed(e: AnActionEvent) {
     saveCurrentItem()
     val breakpoint: XBreakpoint<*>? = myType.addBreakpoint(project, null)
     if (breakpoint is XBreakpointBase<*, *, *>) {
       selectBreakpoint(breakpoint.breakpointId)
     }
+  }
+
+  override fun getActionUpdateThread(): ActionUpdateThread {
+    return ActionUpdateThread.EDT
   }
 }
