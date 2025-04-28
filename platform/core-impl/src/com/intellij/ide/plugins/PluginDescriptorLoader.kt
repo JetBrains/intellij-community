e528c57bf12f51588fc3ce3afb0eb8ad723a8973
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-04-28 14:12:15 +0200

    [plugins] IJPL-181065 PluginDescriptorLoader: drop dead code branch
    GitOrigin-RevId: 7f4aff1a73e62b36795014e784438fd5c77d4a83

--- platform/core-impl/src/com/intellij/ide/plugins/PluginDescriptorLoader.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/PluginDescriptorLoader.kt
@@ -541,26 +541,11 @@ private suspend fun loadAndInitDescriptors(
     )
     extraListDeferred = loadDescriptorsFromProperty(loadingContext, zipPool)
   }
-
   val loadingResult = PluginLoadingResult()
-
   val isMainProcess = isMainProcess()
   loadingResult.initAndAddAll(descriptors = toSequence(listDeferred, isMainProcess), overrideUseIfCompatible = false, initContext = initContext)
   // plugins added via property shouldn't be overridden to avoid plugin root detection issues when running external plugin tests
   loadingResult.initAndAddAll(descriptors = toSequence(extraListDeferred, isMainProcess), overrideUseIfCompatible = true, initContext = initContext)
-
-  if (isUnitTestMode && loadingResult.enabledPluginsById.size <= 1 &&
-      java.lang.Boolean.getBoolean("idea.load.bundled.plugins.in.test.revert")) { // TODO: drop this branch if nobody complains
-    // we're running in unit test mode, but the classpath doesn't contain any plugins; try to load bundled plugins anyway
-    loadingResult.initAndAddAll(
-      descriptors = toSequence(coroutineScope {
-        val dir = Paths.get(PathManager.getPreInstalledPluginsPath())
-        loadDescriptorsFromDir(dir = dir, loadingContext = loadingContext, isBundled = true, pool = zipPoolDeferred.await())
-      }, isMainProcess),
-      overrideUseIfCompatible = false,
-      initContext = initContext
-    )
-  }
   return loadingResult
 }
 
