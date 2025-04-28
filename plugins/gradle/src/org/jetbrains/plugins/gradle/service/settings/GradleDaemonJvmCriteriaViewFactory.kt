769e16eb585a2d97d00567bf575c77ad27a1e530
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2025-04-28 16:09:56 +0200

    [java] Use highest released language level in Gradle version suggester
    Don't use JPS language level. #IDEA-369013
    
    GitOrigin-RevId: d07eb5be6d83c100f232e1dc2d74468bc86789c0

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/settings/GradleDaemonJvmCriteriaViewFactory.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/settings/GradleDaemonJvmCriteriaViewFactory.kt
@@ -1,10 +1,11 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.service.settings
 
 import com.intellij.openapi.Disposable
+import com.intellij.pom.java.JavaRelease
+import com.intellij.pom.java.LanguageLevel
 import org.gradle.internal.jvm.inspection.JvmVendor
 import org.gradle.util.GradleVersion
-import org.jetbrains.jps.model.java.LanguageLevel
 import org.jetbrains.plugins.gradle.properties.GradleDaemonJvmPropertiesFile
 import org.jetbrains.plugins.gradle.service.execution.GradleDaemonJvmHelper
 import java.nio.file.Path
@@ -24,7 +25,7 @@ object GradleDaemonJvmCriteriaViewFactory {
   }
 
   private fun getSuggestedVersions() =
-    LanguageLevel.JDK_1_8.toJavaVersion().feature..LanguageLevel.HIGHEST.toJavaVersion().feature
+    LanguageLevel.JDK_1_8.toJavaVersion().feature..JavaRelease.getHighest().toJavaVersion().feature
 
   private fun getSuggestedVendors() =
     JvmVendor.KnownJvmVendor.entries.filter { it != JvmVendor.KnownJvmVendor.UNKNOWN }
