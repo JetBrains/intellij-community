0d3f25240e492c5a4cd5a68ceae019ba37442852
Maksim Zuev <maksim.zuev@jetbrains.com>
2025-04-29 14:32:05 +0200

    [debugger] EA-1720304 Fix threading for getSourcePositionNow
    GitOrigin-RevId: 52113f935dde74c8389ecfb96e6ea2cc09cc6664

--- java/debugger/impl/src/com/intellij/debugger/actions/ToggleFieldBreakpointAction.kt
+++ java/debugger/impl/src/com/intellij/debugger/actions/ToggleFieldBreakpointAction.kt
@@ -3,22 +3,43 @@ package com.intellij.debugger.actions
 
 import com.intellij.debugger.DebuggerManagerEx
 import com.intellij.debugger.SourcePosition
+import com.intellij.debugger.engine.SourcePositionProvider.Companion.getSourcePosition
+import com.intellij.debugger.engine.withDebugContext
+import com.intellij.debugger.impl.DebuggerContextImpl
 import com.intellij.debugger.ui.breakpoints.FieldBreakpoint
 import com.intellij.debugger.ui.impl.watch.FieldDescriptorImpl
+import com.intellij.debugger.ui.impl.watch.NodeDescriptorImpl
 import com.intellij.debugger.ui.impl.watch.NodeDescriptorProvider
 import com.intellij.ide.highlighter.JavaClassFileType
 import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.readAction
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.progress.runBlockingMaybeCancellable
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiField
 import com.intellij.xdebugger.impl.ui.tree.actions.XDebuggerTreeActionBase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ToggleFieldBreakpointAction : AnAction(), ActionRemoteBehaviorSpecification.Disabled {
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.getData(CommonDataKeys.PROJECT) ?: return
-    val place = getPlace(e) ?: return
+    project.service<ToggleFieldBreakpointActionService>().cs.launch {
+      val place = getPlace(e) ?: return@launch
+      withContext(Dispatchers.EDT) {
+        toggleFieldBreakpoint(project, place, e)
+      }
+    }
+  }
 
+  private fun toggleFieldBreakpoint(project: Project, place: SourcePosition, e: AnActionEvent) {
     val document = place.getFile().getViewProvider().getDocument() ?: return
     val debuggerManager = DebuggerManagerEx.getInstanceEx(project)
     val manager = debuggerManager.getBreakpointManager()
@@ -36,7 +57,9 @@ class ToggleFieldBreakpointAction : AnAction(), ActionRemoteBehaviorSpecificatio
   }
 
   override fun update(event: AnActionEvent) {
-    val place: SourcePosition? = getPlace(event)
+    val place: SourcePosition? = runBlockingMaybeCancellable {
+      getPlace(event)
+    }
     val toEnable = place != null
 
     val presentation = event.presentation
@@ -54,7 +77,7 @@ class ToggleFieldBreakpointAction : AnAction(), ActionRemoteBehaviorSpecificatio
   }
 }
 
-private fun getPlace(event: AnActionEvent): SourcePosition? {
+private suspend fun getPlace(event: AnActionEvent): SourcePosition? {
   val dataContext = event.dataContext
   val project = event.getData(CommonDataKeys.PROJECT) ?: return null
   if (ActionPlaces.PROJECT_VIEW_POPUP == event.place ||
@@ -62,7 +85,7 @@ private fun getPlace(event: AnActionEvent): SourcePosition? {
       ActionPlaces.BOOKMARKS_VIEW_POPUP == event.place
   ) {
     val psiElement = event.getData(CommonDataKeys.PSI_ELEMENT) as? PsiField ?: return null
-    return SourcePosition.createFromElement(psiElement)
+    return readAction { SourcePosition.createFromElement(psiElement) }
   }
 
   val value = XDebuggerTreeActionBase.getSelectedValue(dataContext)
@@ -72,7 +95,7 @@ private fun getPlace(event: AnActionEvent): SourcePosition? {
       val debuggerContext = DebuggerAction.getDebuggerContext(dataContext)
       val managerThread = debuggerContext.getManagerThread()
       if (managerThread != null) { // if there is an active debug session
-        val sourcePosition = getSourcePositionNow(debuggerContext, descriptor)
+        val sourcePosition = getSourcePositionAsync(debuggerContext, descriptor)
         if (sourcePosition != null) {
           return sourcePosition
         }
@@ -80,20 +103,32 @@ private fun getPlace(event: AnActionEvent): SourcePosition? {
     }
   }
 
-  val editor = ToggleMethodBreakpointAction.getEditor(event)
-  if (editor != null) {
-    val document = editor.getDocument()
-    val file = PsiDocumentManager.getInstance(project).getPsiFile(document)
-    if (file != null) {
-      val virtualFile = file.getVirtualFile()
-      val fileType = virtualFile?.fileType
-      if (JavaFileType.INSTANCE == fileType || JavaClassFileType.INSTANCE == fileType) {
-        val field = FieldBreakpoint.findField(project, document, editor.getCaretModel().offset)
-        if (field != null) {
-          return SourcePosition.createFromElement(field)
+  return readAction {
+    val editor = ToggleMethodBreakpointAction.getEditor(event)
+    if (editor != null) {
+      val document = editor.getDocument()
+      val file = PsiDocumentManager.getInstance(project).getPsiFile(document)
+      if (file != null) {
+        val virtualFile = file.getVirtualFile()
+        val fileType = virtualFile?.fileType
+        if (JavaFileType.INSTANCE == fileType || JavaClassFileType.INSTANCE == fileType) {
+          val field = FieldBreakpoint.findField(project, document, editor.getCaretModel().offset)
+          if (field != null) {
+            return@readAction SourcePosition.createFromElement(field)
+          }
         }
       }
     }
+    null
   }
-  return null
 }
+
+private suspend fun getSourcePositionAsync(
+  debuggerContext: DebuggerContextImpl,
+  descriptor: NodeDescriptorImpl,
+): SourcePosition? = withDebugContext(debuggerContext) {
+  getSourcePosition(descriptor, debuggerContext.project, debuggerContext)
+}
+
+@Service(Service.Level.PROJECT)
+private class ToggleFieldBreakpointActionService(val cs: CoroutineScope)
