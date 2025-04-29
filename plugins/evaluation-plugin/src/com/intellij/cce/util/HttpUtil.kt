e0584b2766127b3215a2aa73b35638c9f7fd20ca
anton.spilnyy <anton.spilnyy@jetbrains.com>
2025-04-29 10:23:23 +0000

    [aia-eval] LME-438 handle error during downloading datasets from Hugging Face
    GitOrigin-RevId: e000e8a6ad668598f3e4453227bcb83080b993ab

--- plugins/evaluation-plugin/src/com/intellij/cce/util/HttpUtil.kt
+++ plugins/evaluation-plugin/src/com/intellij/cce/util/HttpUtil.kt
@@ -5,6 +5,9 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.java.Java
 import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
 import io.ktor.client.request.headers
 import io.ktor.client.statement.bodyAsBytes
@@ -12,14 +15,23 @@ import io.ktor.http.HttpHeaders
 import io.ktor.http.isSuccess
 
 private val httpClient: HttpClient by lazy {
+  val log = com.intellij.openapi.diagnostic.Logger.getInstance("com.intellij.cce.util.httpClient")
   HttpClient(Java) {
     expectSuccess = true
     install(HttpRequestRetry) {
-      retryOnExceptionOrServerErrors(maxRetries = 3)
+      retryOnExceptionOrServerErrors(maxRetries = 10)
       exponentialDelay()
     }
     install(HttpTimeout) {
       connectTimeoutMillis = System.getProperty("idea.connection.timeout")?.toLongOrNull() ?: 10_000
+    }
+    install(Logging) {
+      logger = object: Logger {
+        override fun log(message: String) {
+          log.info(message)
+        }
+      }
+      level = LogLevel.INFO
     }
   }
 }
