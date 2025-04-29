ef356a67a2b90b8c4173284265f26c162b813cde
Ilia Kirianovskii <Ilia.Kirianovskii@jetbrains.com>
2025-04-29 11:56:20 +0200

    [bazel] Fix compilation in @community//platform/xdebugger-impl/frontend:debugger-impl-frontend
    GitOrigin-RevId: 8f95dc33360230837a6eeb2b4849057e06756258

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/breakpoints/CommonBreakpointGutterIconRenderer.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/breakpoints/CommonBreakpointGutterIconRenderer.java
@@ -4,9 +4,11 @@ package com.intellij.xdebugger.impl.breakpoints;
 import com.intellij.openapi.editor.ex.util.EditorUtil;
 import com.intellij.openapi.editor.markup.GutterIconRenderer;
 import com.intellij.ui.ExperimentalUI;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-abstract class CommonBreakpointGutterIconRenderer extends GutterIconRenderer {
+@ApiStatus.Internal
+public abstract class CommonBreakpointGutterIconRenderer extends GutterIconRenderer {
   @Override
   public @NotNull Alignment getAlignment() {
     return ExperimentalUI.isNewUI() && EditorUtil.isBreakPointsOnLineNumbers() ? Alignment.LINE_NUMBERS : Alignment.RIGHT;
