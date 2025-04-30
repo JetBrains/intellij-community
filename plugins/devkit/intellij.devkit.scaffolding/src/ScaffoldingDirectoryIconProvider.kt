c9cd26c5dae22a0a9137b34494ed71ae0cb92486
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2025-04-30 13:42:27 +0200

    [devkit] add tooltips for plugin and module directory icons
    GitOrigin-RevId: 47341c108679b62e5677c0e65e0be0b90f1ea89a

--- plugins/devkit/intellij.devkit.scaffolding/src/ScaffoldingDirectoryIconProvider.kt
+++ plugins/devkit/intellij.devkit.scaffolding/src/ScaffoldingDirectoryIconProvider.kt
@@ -8,7 +8,9 @@ import com.intellij.openapi.util.registry.Registry
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
 import com.intellij.ui.LayeredIcon
+import com.intellij.ui.icons.IconWrapperWithToolTip
 import com.intellij.util.IconUtil
+import java.util.function.Supplier
 import javax.swing.Icon
 import javax.swing.SwingConstants
 
@@ -23,7 +25,7 @@ internal class ScaffoldingDirectoryIconProvider : IconProvider() {
       if (IntelliJProjectUtil.isIntelliJPlatformProject(project)) {
         val kind = directoryKind(project, element.virtualFile)
         return when (kind) {
-          DirectoryKind.PLUGIN -> AllIcons.Nodes.Plugin
+          DirectoryKind.PLUGIN -> pluginDirectoryIcon
           DirectoryKind.LEGACY_PLUGIN_WITH_MAIN_MODULE -> legacyPluginWithModuleIcon
           DirectoryKind.MODULE -> moduleDirectoryIcon
           null -> null
@@ -34,12 +36,23 @@ internal class ScaffoldingDirectoryIconProvider : IconProvider() {
   }
 }
 
-private val moduleDirectoryIcon = LayeredIcon(2).apply {
-  setIcon(AllIcons.Nodes.Plugin, 0)
-  setIcon(AllIcons.Nodes.Module8x8, 1, SwingConstants.SOUTH_EAST)
-}
+private val pluginDirectoryIcon = AllIcons.Nodes.Plugin
+  .withTooltip(messagePointer("plugin.directory.tooltip"))
 
-private val legacyPluginWithModuleIcon = LayeredIcon(2).apply {
-  setIcon(AllIcons.Nodes.Plugin, 0)
-  setIcon(IconUtil.scale(AllIcons.Actions.ModuleDirectory, ancestor = null, 0.7f), 1, SwingConstants.SOUTH_EAST)
+private val moduleDirectoryIcon = LayeredIcon(2)
+  .apply {
+    setIcon(AllIcons.Nodes.Plugin, 0)
+    setIcon(AllIcons.Nodes.Module8x8, 1, SwingConstants.SOUTH_EAST)
+  }
+  .withTooltip(messagePointer("module.directory.tooltip"))
+
+private val legacyPluginWithModuleIcon = LayeredIcon(2)
+  .apply {
+    setIcon(AllIcons.Nodes.Plugin, 0)
+    setIcon(IconUtil.scale(AllIcons.Actions.ModuleDirectory, ancestor = null, 0.7f), 1, SwingConstants.SOUTH_EAST)
+  }
+  .withTooltip(messagePointer("plugin.and.module.directory.tooltip"))
+
+private fun Icon.withTooltip(message: Supplier<String>): Icon {
+  return IconWrapperWithToolTip(this, message)
 }
