84b4e2e323dfedad7f38767466ed935573f3ccc2
Alexander Kuznetsov <aleksander.kuznetsov@jetbrains.com>
2025-04-30 18:16:28 +0200

    IJPL-186217 [compose][markdown] Support autolinks
    (cherry picked from commit d4b2c4e680572f01683d40f1641fedb78117d3ea)
    
    
    (cherry picked from commit 187fcc7f8652abc4784d8d487f50a597f94eed2e)
    
    IJ-MR-169155
    
    GitOrigin-RevId: e405fcc8a02817f7022c65c8e9781e83df8caa14

--- plugins/markdown/compose/src/main/kotlin/com/intellij/markdown/compose/preview/MarkdownComposePanel.kt
+++ plugins/markdown/compose/src/main/kotlin/com/intellij/markdown/compose/preview/MarkdownComposePanel.kt
@@ -38,6 +38,7 @@ import org.jetbrains.jewel.intui.markdown.bridge.ProvideMarkdownStyling
 import org.jetbrains.jewel.intui.markdown.bridge.styling.extensions.github.tables.create
 import org.jetbrains.jewel.markdown.Markdown
 import org.jetbrains.jewel.markdown.MarkdownMode
+import org.jetbrains.jewel.markdown.extensions.autolink.AutolinkProcessorExtension
 import org.jetbrains.jewel.markdown.extensions.github.strikethrough.GitHubStrikethroughProcessorExtension
 import org.jetbrains.jewel.markdown.extensions.github.strikethrough.GitHubStrikethroughRendererExtension
 import org.jetbrains.jewel.markdown.extensions.github.tables.GfmTableStyling
@@ -84,6 +85,7 @@ internal class MarkdownComposePanel(
         listOf(
           GitHubTableProcessorExtension,
           GitHubStrikethroughProcessorExtension(),
+          AutolinkProcessorExtension,
         ),
         markdownMode,
       )
