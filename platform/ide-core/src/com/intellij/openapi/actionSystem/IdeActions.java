e9c71df28573595ba15b3f4af5290919bde7eaad
Anna Kozlova <anna.kozlova@jetbrains.com>
2025-04-30 10:26:12 +0200

    [kotlin] fix goto actuals keymap mapping
    ^KTIJ-33755 fixed
    
    GitOrigin-RevId: 171323e61b03a84a04b75e2d8fb44281140bff03

--- platform/ide-core/src/com/intellij/openapi/actionSystem/IdeActions.java
+++ platform/ide-core/src/com/intellij/openapi/actionSystem/IdeActions.java
@@ -1,6 +1,8 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.actionSystem;
 
+import org.jetbrains.annotations.Nullable;
+
 /**
  * Identifiers for standard actions and action groups.
  */
@@ -287,6 +289,7 @@ public interface IdeActions {
   String ACTION_GOTO_TYPE_DECLARATION = "GotoTypeDeclaration";
   String ACTION_GOTO_IMPLEMENTATION = "GotoImplementation";
   String ACTION_GOTO_SUPER = "GotoSuperMethod";
+  String ACTION_GOTO_RELATED = "GotoRelated";
 
   String MODULE_SETTINGS = "ModuleSettings";
 
