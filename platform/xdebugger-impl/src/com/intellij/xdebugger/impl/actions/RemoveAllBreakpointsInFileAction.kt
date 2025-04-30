d16baea0ea1f8b12a5358a5a8de65463b70576de
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-04-30 10:20:58 +0200

    [debugger] Convert RemoveAllBreakpointsInFileAction to Kotlin
    GitOrigin-RevId: cc23b73e71b54df47a6496725c8bbc336961c6c3

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/RemoveAllBreakpointsInFileAction.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/RemoveAllBreakpointsInFileAction.kt
@@ -1,27 +1,27 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.xdebugger.impl.actions;
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.xdebugger.impl.actions
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.project.DumbAwareAction;
-import com.intellij.openapi.project.Project;
-import com.intellij.xdebugger.XDebuggerManager;
-import com.intellij.xdebugger.impl.breakpoints.XBreakpointManagerImpl;
-import com.intellij.xdebugger.impl.breakpoints.XLineBreakpointManager;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.xdebugger.XDebuggerManager
+import com.intellij.xdebugger.impl.breakpoints.XBreakpointManagerImpl
+import com.intellij.xdebugger.impl.breakpoints.XLineBreakpointImpl
+import org.jetbrains.annotations.ApiStatus
+import java.util.function.Consumer
 
 @ApiStatus.Internal
-public class RemoveAllBreakpointsInFileAction extends DumbAwareAction {
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
-    Project project = e.getProject();
-    Editor editor = e.getData(CommonDataKeys.EDITOR);
+class RemoveAllBreakpointsInFileAction : DumbAwareAction() {
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project
+    val editor = e.getData(CommonDataKeys.EDITOR)
     if (project != null && editor != null) {
-      XBreakpointManagerImpl breakpointManager = (XBreakpointManagerImpl)XDebuggerManager.getInstance(project).getBreakpointManager();
-      XLineBreakpointManager lineBreakpointManager = breakpointManager.getLineBreakpointManager();
-      lineBreakpointManager.getDocumentBreakpoints(editor.getDocument()).forEach(breakpointManager::removeBreakpoint);
+      val breakpointManager = XDebuggerManager.getInstance(project).getBreakpointManager() as XBreakpointManagerImpl
+      val lineBreakpointManager = breakpointManager.lineBreakpointManager
+      lineBreakpointManager.getDocumentBreakpoints(editor.getDocument()).forEach {
+        breakpointManager.removeBreakpoint(it)
+      }
     }
   }
 }
