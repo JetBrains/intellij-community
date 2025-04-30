0196b4a68c5978c963886115b24b74140f62cab6
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-04-30 17:50:59 +0300

    IJPL-186161 Fix terminal pinch zoom
    It's a bit tricky because the magnificator API requires
    us to return the zoom point. So we have to encapsulate
    it into the strategy. Another tricky part is that the point
    we already have is relative to the viewport,
    but the one needed here is relative to the whole editor.
    So we need to convert.
    
    (cherry picked from commit 4a98544f73ab5722448d837c8f7ef63f2712e5d4)
    
    IJ-CR-161835
    
    GitOrigin-RevId: 810ae77ce87f255d5f475af6fa40c28af459f3a4

--- platform/platform-impl/src/com/intellij/openapi/editor/actions/ChangeEditorFontSizeStrategy.kt
+++ platform/platform-impl/src/com/intellij/openapi/editor/actions/ChangeEditorFontSizeStrategy.kt
@@ -1,9 +1,11 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.editor.actions
 
+import com.intellij.openapi.editor.impl.EditorImpl
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.NlsActions
 import org.jetbrains.annotations.ApiStatus
+import java.awt.Point
 
 @ApiStatus.Internal
 interface ChangeEditorFontSizeStrategy {
@@ -14,4 +16,5 @@ interface ChangeEditorFontSizeStrategy {
   val defaultFontSize: Float
   @get:NlsActions.ActionText val defaultFontSizeText: String
   val overridesChangeFontSizeActions: Boolean
+  fun preferredZoomPointRelative(editor: EditorImpl): Point
 }
