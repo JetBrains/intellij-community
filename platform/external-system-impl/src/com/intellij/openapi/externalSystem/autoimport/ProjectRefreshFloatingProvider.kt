de7d15b152907687910a0d627464b392c322c9d5
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2025-04-30 15:03:54 +0200

    Partial Revert "IJPL-185714 cleanup"
    (partially reverted commit 88f442c81395ff523ae02dc36de2bdb5491114b7)
    
    ### Important for commit 38388099dc49b8d7f6109c8e5f832138b9014a03
    **Make the ProjectRefreshFloatingProvider suppressable**
    
    Widen the visibility of the class itself so that application
    initialized listeners or other startup-related hooks can disable the
    registration of the ProjectRefreshFloatingProvider.
    
    GitOrigin-RevId: 30bba5eb4297615100109c428ee75431dc480f11

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/autoimport/ProjectRefreshFloatingProvider.kt
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/autoimport/ProjectRefreshFloatingProvider.kt
@@ -4,7 +4,6 @@ package com.intellij.openapi.externalSystem.autoimport
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.CommonDataKeys.PROJECT
 import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.components.service
 import com.intellij.openapi.components.serviceAsync
@@ -13,18 +12,23 @@ import com.intellij.openapi.editor.toolbar.floating.FloatingToolbarComponent
 import com.intellij.openapi.editor.toolbar.floating.isInsideMainEditor
 import com.intellij.openapi.project.Project
 import com.intellij.platform.ide.CoreUiCoroutineScopeHolder
+import com.intellij.util.application
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.intellij.lang.annotations.Language
+import org.jetbrains.annotations.ApiStatus
 
 @Language("devkit-action-id") private const val ACTION_GROUP = "ExternalSystem.ProjectRefreshActionGroup"
 
-private class ProjectRefreshFloatingProvider : AbstractFloatingToolbarProvider(ACTION_GROUP) {
-  override val autoHideable: Boolean
-    get() = false
+@ApiStatus.Internal
+class ProjectRefreshFloatingProvider : AbstractFloatingToolbarProvider(ACTION_GROUP) {
 
-  override fun isApplicable(dataContext: DataContext): Boolean = isInsideMainEditor(dataContext)
+  override val autoHideable: Boolean = false
+
+  override fun isApplicable(dataContext: DataContext): Boolean {
+    return isInsideMainEditor(dataContext)
+  }
 
   private fun updateToolbarComponent(project: Project, component: FloatingToolbarComponent) {
     project.service<CoreUiCoroutineScopeHolder>().coroutineScope.launch {
@@ -41,7 +45,7 @@ private class ProjectRefreshFloatingProvider : AbstractFloatingToolbarProvider(A
   override fun register(dataContext: DataContext, component: FloatingToolbarComponent, parentDisposable: Disposable) {
     val myProject = dataContext.getData(PROJECT) ?: return
 
-    ApplicationManager.getApplication().messageBus.connect(parentDisposable)
+    application.messageBus.connect(parentDisposable)
       .subscribe(ExternalSystemProjectNotificationAware.TOPIC, object : ExternalSystemProjectNotificationAware.Listener {
         override fun onNotificationChanged(project: Project) {
           if (project === myProject) {
