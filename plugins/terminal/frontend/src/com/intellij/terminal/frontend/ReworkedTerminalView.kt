b87dee6e0817600c4a09b852602b45ca31d0279c
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-04-30 17:50:59 +0300

    IJPL-186161 Fix terminal pinch zoom
    It's a bit tricky because the magnificator API requires
    us to return the zoom point. So we have to encapsulate
    it into the strategy. Another tricky part is that the point
    we already have is relative to the viewport,
    but the one needed here is relative to the whole editor.
    So we need to convert.
    
    (cherry picked from commit 4a98544f73ab5722448d837c8f7ef63f2712e5d4)
    
    IJ-CR-161835
    
    GitOrigin-RevId: 1af35cd6d958b487ba1910bee2074b9ef1a3feba

--- plugins/terminal/frontend/src/com/intellij/terminal/frontend/ReworkedTerminalView.kt
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/ReworkedTerminalView.kt
@@ -51,7 +51,6 @@ import org.jetbrains.plugins.terminal.fus.TerminalStartupFusInfo
 import org.jetbrains.plugins.terminal.util.terminalProjectScope
 import java.awt.Component
 import java.awt.Dimension
-import java.awt.Point
 import java.awt.event.*
 import java.util.concurrent.CompletableFuture
 import javax.swing.JComponent
@@ -393,21 +392,17 @@ internal class ReworkedTerminalView(
 
     TerminalFontSizeProviderImpl.getInstance().addListener(parentDisposable, object : TerminalFontSizeProvider.Listener {
       override fun fontChanged() {
-        result.setFontSize(TerminalFontSizeProviderImpl.getInstance().getFontSize(), result.bottomLeftCornerOrNull(), true)
+        result.setFontSize(
+          TerminalFontSizeProviderImpl.getInstance().getFontSize(),
+          ChangeTerminalFontSizeStrategy.preferredZoomPointRelative(result),
+          true
+        )
         result.resizeIfShowing()
       }
     })
 
     return result
   }
-
-  private fun EditorImpl.bottomLeftCornerOrNull(): Point? =
-    if (component.isShowing) {
-      Point(0, scrollingModel.visibleArea.height)
-    }
-    else {
-      null
-    }
 
   private fun EditorImpl.resizeIfShowing() {
     if (component.isShowing) { // to avoid sending the resize event twice, for the regular and alternate buffer editors
