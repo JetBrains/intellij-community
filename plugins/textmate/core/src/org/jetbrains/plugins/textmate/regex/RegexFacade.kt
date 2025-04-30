1ef0d54b39d3d1d59351660a09bc18a17969ba37
Alexander Zolotov <zolotov@jetbrains.com>
2025-04-30 07:33:13 +0200

    [textmate] introduce NotMatchingRegexFacade
    GitOrigin-RevId: 22b7dbb4b8375e0aa0f8c70b2eb7174948fbd5b9

--- plugins/textmate/core/src/org/jetbrains/plugins/textmate/regex/RegexFacade.kt
+++ plugins/textmate/core/src/org/jetbrains/plugins/textmate/regex/RegexFacade.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.plugins.textmate.regex
 
 import kotlinx.coroutines.Runnable
+import org.jetbrains.plugins.textmate.regex.MatchData.Companion.NOT_MATCHED
 
 interface RegexFacade {
   fun match(string: TextMateString, checkCancelledCallback: Runnable?): MatchData
@@ -16,4 +17,23 @@ interface RegexFactory {
   fun regex(pattern: CharSequence): RegexFacade
 
   fun string(string: CharSequence): TextMateString
+}
+
+object NotMatchingRegexFacade : RegexFacade {
+  override fun match(
+    string: TextMateString,
+    checkCancelledCallback: Runnable?
+  ): MatchData {
+    return NOT_MATCHED
+  }
+
+  override fun match(
+    string: TextMateString,
+    byteOffset: Int,
+    matchBeginPosition: Boolean,
+    matchBeginString: Boolean,
+    checkCancelledCallback: Runnable?
+  ): MatchData {
+    return NOT_MATCHED
+  }
 }
