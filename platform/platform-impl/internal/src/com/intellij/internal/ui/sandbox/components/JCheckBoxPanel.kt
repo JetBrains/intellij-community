e4409628c7a5b41119db2b6850d6bb5c2f10e06b
Nebojsa Vuksic <nebojsa.vuksic@jetbrains.com>
2025-05-01 00:53:51 +0200

    IJPL-185971 Add Checkbox validation examples to UISandbox
    GitOrigin-RevId: 09fdf49231f6d7ee40e074c6e18e19abaa275ae5

--- platform/platform-impl/internal/src/com/intellij/internal/ui/sandbox/components/JCheckBoxPanel.kt
+++ platform/platform-impl/internal/src/com/intellij/internal/ui/sandbox/components/JCheckBoxPanel.kt
@@ -4,6 +4,7 @@ package com.intellij.internal.ui.sandbox.components
 import com.intellij.internal.ui.sandbox.UISandboxPanel
 import com.intellij.internal.ui.sandbox.applyStateText
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.ui.ValidationInfo
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.builder.selected
 import javax.swing.JComponent
@@ -13,19 +14,50 @@ internal class JCheckBoxPanel : UISandboxPanel {
   override val title: String = "JCheckBox"
 
   override fun createContent(disposable: Disposable): JComponent {
-    return panel {
+    val panel = panel {
       group("States") {
-        for (isEnabled in listOf(true, false)) {
-          for (isSelected in listOf(false, true)) {
-            row {
-              checkBox("")
-                .selected(isSelected)
-                .enabled(isEnabled)
-                .applyStateText()
+        for (state in allCheckboxStates()) {
+          row {
+            val stateLabel = when {
+              state.isError -> "Error"
+              state.isWarning -> "Warning"
+              else -> null
             }
+            checkBox("")
+              .selected(state.isSelected)
+              .enabled(state.isEnabled)
+              .validationOnInput { validate(state) }
+              .validationOnApply { validate(state) }
+              .applyStateText(stateLabel)
           }
         }
       }
     }
+    panel.registerValidators(disposable)
+    panel.validateAll()
+    return panel
   }
+
+  private fun validate(checkBoxState: CheckBoxState): ValidationInfo? {
+    return when {
+      checkBoxState.isError -> ValidationInfo("Checkbox should be selected")
+      checkBoxState.isWarning -> ValidationInfo("Checkbox should be selected").asWarning()
+      else -> null
+    }
+  }
+
+  private fun allCheckboxStates(): List<CheckBoxState> {
+    return listOf(
+      CheckBoxState(isSelected = true, isEnabled = false, isError = false, isWarning = false),
+      CheckBoxState(isSelected = true, isEnabled = true, isError = false, isWarning = false),
+      CheckBoxState(isSelected = false, isEnabled = true, isError = false, isWarning = false),
+      CheckBoxState(isSelected = false, isEnabled = false, isError = false, isWarning = false),
+      CheckBoxState(isSelected = true, isEnabled = true, isError = true, isWarning = false),
+      CheckBoxState(isSelected = true, isEnabled = true, isError = false, isWarning = true),
+      CheckBoxState(isSelected = false, isEnabled = true, isError = true, isWarning = false),
+      CheckBoxState(isSelected = false, isEnabled = true, isError = false, isWarning = true),
+    )
+  }
+
+  private data class CheckBoxState(val isSelected: Boolean, val isEnabled: Boolean, val isError: Boolean, val isWarning: Boolean)
 }
