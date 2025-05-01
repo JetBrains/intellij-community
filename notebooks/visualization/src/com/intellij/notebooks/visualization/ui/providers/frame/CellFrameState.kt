1c260a114fe2f77cad2f1c093e5908f9055e5f59
Nikita.Ashihmin <nikita.ashihmin@jetbrains.com>
2025-05-02 00:52:21 +0400

    PY-80533 Jupyter: Split border to provider and controllers
    Signed-off-by: Nikita.Ashihmin <nikita.ashihmin@jetbrains.com>
    
    GitOrigin-RevId: 63c55d6de0ea0cc58d3430c9566bd3170665b0cb

--- notebooks/visualization/src/com/intellij/notebooks/visualization/ui/providers/frame/CellFrameState.kt
+++ notebooks/visualization/src/com/intellij/notebooks/visualization/ui/providers/frame/CellFrameState.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.notebooks.visualization.ui.cell.frame
+package com.intellij.notebooks.visualization.ui.providers.frame
 
 import com.intellij.ui.JBColor
 import java.awt.Color
