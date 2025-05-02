7e55f6e5c42d9fb8e911f717b9ea2d4376378855
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2025-05-03 00:24:24 +0200

    IJPL-185514 Many islands: what to reuse from Rider Next (separating from next part 1)
    GitOrigin-RevId: 50561d630e328485ebab7dca7a0a332a9dc6561f

---
+++ platform/platform-impl/src/com/intellij/openapi/application/impl/manyIslands/ManyIslandsRoundedBorder.kt
@@ -0,0 +1,40 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.application.impl.manyIslands
+
+import com.intellij.openapi.application.impl.InternalUICustomization
+import com.intellij.openapi.fileEditor.impl.EditorsSplitters
+import com.intellij.openapi.wm.impl.IdeBackgroundUtil
+import com.intellij.toolWindow.xNext.island.XNextRoundedBorder
+import com.intellij.ui.tabs.impl.JBEditorTabs
+import com.intellij.util.ui.JBInsets
+import com.intellij.util.ui.JBUI
+import com.intellij.util.ui.UIUtil
+import org.jetbrains.annotations.ApiStatus
+import java.awt.Graphics
+import java.awt.Paint
+import javax.swing.JComponent
+
+@ApiStatus.Internal
+internal class ManyIslandsRoundedBorder(fillColor: (JComponent) -> Paint?) :
+  XNextRoundedBorder(
+    fillColor,
+    fillColor,
+    { g: Graphics, c: JComponent -> IdeBackgroundUtil.withEditorBackground(g, c) },
+    { c: JComponent -> InternalUICustomization.getInstance()?.getCustomMainBackgroundColor() },
+    JBUI.getInt("Island.arc", 10),
+    JBUI.scale(2),
+    JBInsets.create("Island.border1.insets", JBInsets(4, 4, 4, 4)),
+    JBInsets.create("Island.border2.insets", JBInsets(2, 2, 2, 2))) {
+
+  companion object {
+    fun createToolWindowBorder(component: JComponent, child: JComponent) {
+      component.border = ManyIslandsRoundedBorder { child.background }
+    }
+
+    fun createEditorBorder(component: EditorsSplitters) {
+      component.border = ManyIslandsRoundedBorder {
+        UIUtil.findComponentOfType(it, JBEditorTabs::class.java)?.background ?: it.background
+      }
+    }
+  }
+}
---
+++ platform/platform-impl/src/com/intellij/openapi/application/impl/manyIslands/ManyIslandsUICustomization.kt
@@ -0,0 +1,62 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.application.impl.manyIslands
+
+import com.intellij.openapi.application.impl.InternalUICustomization
+import com.intellij.openapi.application.impl.ToolWindowUIDecorator
+import com.intellij.openapi.fileEditor.impl.EditorsSplitters
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.openapi.wm.impl.IdeBackgroundUtil
+import com.intellij.toolWindow.xNext.island.XNextIslandHolder
+import com.intellij.ui.ClientProperty
+import com.intellij.ui.JBColor
+import org.jetbrains.annotations.ApiStatus
+import java.awt.BorderLayout
+import java.awt.Color
+import javax.swing.JComponent
+
+@ApiStatus.Internal
+internal class ManyIslandsUICustomization : InternalUICustomization() {
+  private val isManyIslandsEnabled: Boolean = Registry.`is`("idea.many.islands.enabled", false)
+
+  override val isDefaultCustomization: Boolean = !isManyIslandsEnabled
+
+  private val toolWindowDecorator = object : ToolWindowUIDecorator() {
+    override fun decorateAndReturnHolder(divider: JComponent, child: JComponent): JComponent? {
+      return XNextIslandHolder().apply {
+        layout = BorderLayout()
+        add(divider, BorderLayout.NORTH)
+        add(child, BorderLayout.CENTER)
+
+        ManyIslandsRoundedBorder.createToolWindowBorder(this, child)
+        child.putClientProperty(IdeBackgroundUtil.NO_BACKGROUND, true)
+      }
+    }
+  }
+
+  override val toolWindowUIDecorator: ToolWindowUIDecorator
+    get() {
+      if (isManyIslandsEnabled) {
+        return toolWindowDecorator
+      }
+      return super.toolWindowUIDecorator
+    }
+
+  override fun installEditorBackground(component: JComponent) {
+    if (isManyIslandsEnabled) {
+      ClientProperty.putRecursive(component, IdeBackgroundUtil.NO_BACKGROUND, true)
+    }
+  }
+
+  override fun configureEditorsSplitters(component: EditorsSplitters) {
+    if (isManyIslandsEnabled) {
+      ManyIslandsRoundedBorder.createEditorBorder(component)
+    }
+  }
+
+  override fun getCustomMainBackgroundColor(): Color? {
+    if (isManyIslandsEnabled) {
+      return JBColor.namedColor("MainWindow.background", JBColor.PanelBackground)
+    }
+    return super.getCustomMainBackgroundColor()
+  }
+}
