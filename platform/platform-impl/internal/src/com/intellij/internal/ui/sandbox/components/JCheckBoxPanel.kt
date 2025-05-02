ef5512d116a4e10c01fd27d4291a5d99e9706596
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2025-05-02 14:58:31 +0200

    IJPL-185971 JBCheckBox validation error state is rendered a bit off
    - Fixed UI Sandbox
    
    GitOrigin-RevId: 96fc7ca4d20b9e15875ec41128639226f7563daa

--- platform/platform-impl/internal/src/com/intellij/internal/ui/sandbox/components/JCheckBoxPanel.kt
+++ platform/platform-impl/internal/src/com/intellij/internal/ui/sandbox/components/JCheckBoxPanel.kt
@@ -4,9 +4,9 @@ package com.intellij.internal.ui.sandbox.components
 import com.intellij.internal.ui.sandbox.UISandboxPanel
 import com.intellij.internal.ui.sandbox.applyStateText
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.ui.ValidationInfo
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.builder.selected
+import com.intellij.ui.dsl.validation.Level
 import javax.swing.JComponent
 
 internal class JCheckBoxPanel : UISandboxPanel {
@@ -16,48 +16,47 @@ internal class JCheckBoxPanel : UISandboxPanel {
   override fun createContent(disposable: Disposable): JComponent {
     val panel = panel {
       group("States") {
-        for (state in allCheckboxStates()) {
-          row {
-            val stateLabel = when {
-              state.isError -> "Error"
-              state.isWarning -> "Warning"
-              else -> null
+        for (isEnabled in listOf(true, false)) {
+          for (isSelected in listOf(false, true)) {
+            row {
+              checkBox("")
+                .selected(isSelected)
+                .enabled(isEnabled)
+                .applyStateText()
             }
-            checkBox("")
-              .selected(state.isSelected)
-              .enabled(state.isEnabled)
-              .validationOnInput { validate(state) }
-              .validationOnApply { validate(state) }
-              .applyStateText(stateLabel)
+          }
+        }
+      }
+
+      group("Validation") {
+        for (isSelected in listOf(false, true)) {
+          for (level in listOf(Level.ERROR, Level.WARNING)) {
+            row {
+              val stateLabel = when (level) {
+                Level.ERROR -> "Error"
+                Level.WARNING -> "Warning"
+              }
+
+              checkBox("")
+                .selected(isSelected)
+                .cellValidation {
+                  if (isSelected) {
+                    addInputRule("Checkbox should not be selected", level) { it.isSelected }
+                    addApplyRule("Checkbox should not be selected", level) { it.isSelected }
+                  }
+                  else {
+                    addInputRule("Checkbox should be selected", level) { !it.isSelected }
+                    addApplyRule("Checkbox should be selected", level) { !it.isSelected }
+                  }
+                }.applyStateText(stateLabel)
+            }
           }
         }
       }
     }
+
     panel.registerValidators(disposable)
     panel.validateAll()
     return panel
   }
-
-  private fun validate(checkBoxState: CheckBoxState): ValidationInfo? {
-    return when {
-      checkBoxState.isError -> ValidationInfo("Checkbox should be selected")
-      checkBoxState.isWarning -> ValidationInfo("Checkbox should be selected").asWarning()
-      else -> null
-    }
-  }
-
-  private fun allCheckboxStates(): List<CheckBoxState> {
-    return listOf(
-      CheckBoxState(isSelected = true, isEnabled = false, isError = false, isWarning = false),
-      CheckBoxState(isSelected = true, isEnabled = true, isError = false, isWarning = false),
-      CheckBoxState(isSelected = false, isEnabled = true, isError = false, isWarning = false),
-      CheckBoxState(isSelected = false, isEnabled = false, isError = false, isWarning = false),
-      CheckBoxState(isSelected = true, isEnabled = true, isError = true, isWarning = false),
-      CheckBoxState(isSelected = true, isEnabled = true, isError = false, isWarning = true),
-      CheckBoxState(isSelected = false, isEnabled = true, isError = true, isWarning = false),
-      CheckBoxState(isSelected = false, isEnabled = true, isError = false, isWarning = true),
-    )
-  }
-
-  private data class CheckBoxState(val isSelected: Boolean, val isEnabled: Boolean, val isError: Boolean, val isWarning: Boolean)
 }
