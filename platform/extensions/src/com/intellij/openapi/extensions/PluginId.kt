9242e138f1c70e413edc44ca947891ed854fc32d
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2025-05-02 10:31:37 +0200

    IJPL-177442 Migrate PluginId from Java to Kotlin. It will allow us to serialize PluginId in the future.
    (cherry picked from commit 6bf41c7930f0d7abec8738ce6dbed04a9865ec05)
    
    IJ-MR-163725
    
    GitOrigin-RevId: d1e866a3d7fdedd127214fa4ab0a68320d549c5b

--- platform/extensions/src/com/intellij/openapi/extensions/PluginId.kt
+++ platform/extensions/src/com/intellij/openapi/extensions/PluginId.kt
@@ -1,84 +1,70 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.extensions;
+package com.intellij.openapi.extensions
 
-import com.intellij.ReviseWhenPortedToJDK;
-import com.intellij.openapi.util.NlsSafe;
-import org.jetbrains.annotations.ApiStatus.Internal;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.Unmodifiable;
-
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import com.intellij.ReviseWhenPortedToJDK
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.Unmodifiable
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * Represents an ID of a plugin. A full descriptor of the plugin may be obtained
- * via {@link com.intellij.ide.plugins.PluginManagerCore#getPlugin(PluginId)} method.
+ * via [com.intellij.ide.plugins.PluginManagerCore.getPlugin] method.
  */
-public final class PluginId implements Comparable<PluginId> {
-  @Internal
-  public static final PluginId[] EMPTY_ARRAY = new PluginId[0];
+class PluginId private constructor(val idString: String) : Comparable<PluginId> {
 
-  private static final Map<String, PluginId> registeredIds = new ConcurrentHashMap<>();
+  override fun equals(o: Any?): Boolean {
+    if (this === o) return true
+    if (o !is PluginId) return false
 
-  public static @NotNull PluginId getId(@NotNull String idString) {
-    return registeredIds.computeIfAbsent(idString, PluginId::new);
+    val pluginId = o
+    return idString == pluginId.idString
   }
 
-  public static @Nullable PluginId findId(@NotNull String idString) {
-    return registeredIds.get(idString);
+  override fun hashCode(): Int {
+    return idString.hashCode()
   }
 
-  public static @Nullable PluginId findId(String @NotNull ... idStrings) {
-    for (String idString : idStrings) {
-      PluginId pluginId = registeredIds.get(idString);
-      if (pluginId != null) {
-        return pluginId;
-      }
+  override fun compareTo(o: PluginId): Int {
+    return idString.compareTo(o.idString)
+  }
+
+  override fun toString(): String {
+    return idString
+  }
+
+  companion object {
+    @ApiStatus.Internal
+    val EMPTY_ARRAY: Array<PluginId> = emptyArray()
+
+    private val registeredIds: MutableMap<String, PluginId> = ConcurrentHashMap<String, PluginId>()
+
+    @JvmStatic
+    fun getId(idString: String): PluginId {
+      return registeredIds.computeIfAbsent(idString) { idString: String -> PluginId(idString) }
     }
-    return null;
-  }
 
-  @Internal
-  @ReviseWhenPortedToJDK(value = "10", description = "Collectors.toUnmodifiableSet()")
-  public static @NotNull @Unmodifiable Set<PluginId> getRegisteredIds() {
-    return Collections.unmodifiableSet(new HashSet<>(registeredIds.values()));
-  }
+    @JvmStatic
+    fun findId(idString: String?): PluginId? {
+      return registeredIds[idString]
+    }
 
-  private final @NotNull String idString;
+    @JvmStatic
+    fun findId(vararg idStrings: String): PluginId? {
+      for (idString in idStrings) {
+        val pluginId: PluginId? = registeredIds[idString]
+        if (pluginId != null) {
+          return pluginId
+        }
+      }
+      return null
+    }
 
-  private PluginId(@NotNull String idString) {
-    this.idString = idString;
-  }
-
-  public @NotNull @NlsSafe String getIdString() {
-    return idString;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (!(o instanceof PluginId)) return false;
-
-    PluginId pluginId = (PluginId)o;
-    return idString.equals(pluginId.idString);
-  }
-
-  @Override
-  public int hashCode() {
-    return idString.hashCode();
-  }
-
-  @Override
-  public int compareTo(@NotNull PluginId o) {
-    return idString.compareTo(o.idString);
-  }
-
-  @Override
-  public String toString() {
-    return idString;
+    @JvmStatic
+    @ApiStatus.Internal
+    @ReviseWhenPortedToJDK(value = "10", description = "Collectors.toUnmodifiableSet()")
+    fun getRegisteredIds(): @Unmodifiable Set<PluginId> {
+      return registeredIds.values.toSet()
+    }
   }
 }
