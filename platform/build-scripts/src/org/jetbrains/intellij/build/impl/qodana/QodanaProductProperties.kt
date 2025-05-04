93366e5d775b09865b3e17b929871feb5c65b3c5
Mikhail Shagvaliev <mikhail.shagvaliev@jetbrains.com>
2025-05-04 10:42:27 +0200

    [qodana] QD-11102 Maven project import in qodana follows the UI mode
    (cherry picked from commit 9c55b3730d8690fc55bb65a7e9092336547ba620)
    
    IJ-CR-162002
    
    GitOrigin-RevId: ffb3f6cd368491e0a11c8ba878f5e6f1ec51658a

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/qodana/QodanaProductProperties.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/qodana/QodanaProductProperties.kt
@@ -22,6 +22,7 @@ private val COMMON_ADDITIONAL_VM_OPTIONS = listOf(
   "-Dgradle.force.project.sdk.import=true", //workaround for https://youtrack.jetbrains.com/issue/IDEA-367680/The-module-SDK-isnt-always-specified
   "-Dide.region.url.mapping.expiration.timeout=7200",
   "-Dexternal.system.auto.import.headless.async=true",
+  "-Dmaven.default.headless.import=true",
   )
 
 private const val IS_EAP = false
