3afc1374fd0e4913e551d8167fb6d0198ffd9f74
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2025-05-06 22:40:41 +0200

    [xml] IJI-1676 WEB-72767 Support running tests on compilation output jars: fix Html5SchemaProvider and RngHtml5SchemaProvider
    Code expects a path to the file on disk
    
    GitOrigin-RevId: 50585bdabb68fcc03eb5994f712a86f45d228f32

--- xml/relaxng/src/org/intellij/plugins/relaxNG/xml/RngHtml5SchemaProvider.java
+++ xml/relaxng/src/org/intellij/plugins/relaxNG/xml/RngHtml5SchemaProvider.java
@@ -1,24 +1,48 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.intellij.plugins.relaxNG.xml;
 
+import com.intellij.ide.plugins.PluginManagerCore;
+import com.intellij.openapi.application.PathManager;
+import com.intellij.util.PathUtil;
 import com.intellij.xml.Html5SchemaProvider;
+import kotlin.text.StringsKt;
 import org.jetbrains.annotations.NotNull;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Path;
 
 public class RngHtml5SchemaProvider extends Html5SchemaProvider {
   @Override
   public @NotNull URL getHtmlSchemaLocation() {
-    return RngHtml5SchemaProvider.class.getResource("/resources/html5-schema/html5.rnc");
+    return getResource("/resources/html5-schema/html5.rnc");
   }
 
   @Override
   public @NotNull URL getXhtmlSchemaLocation() {
-    return RngHtml5SchemaProvider.class.getResource("/resources/html5-schema/xhtml5.rnc");
+    return getResource("/resources/html5-schema/xhtml5.rnc");
   }
 
   @Override
   public @NotNull URL getCharsLocation() {
-    return RngHtml5SchemaProvider.class.getResource("/resources/html5-schema/html5chars.ent");
+    return getResource("/resources/html5-schema/html5chars.ent");
+  }
+
+  private static URL getResource(String name) {
+    if (PluginManagerCore.isRunningFromSources() && PathUtil.getJarPathForClass(RngHtml5SchemaProvider.class).endsWith(".jar")) {
+      Path community = Path.of(PathManager.getCommunityHomePath());
+      try {
+        name = StringsKt.removePrefix(name, "/");
+        return community.resolve("xml/relaxng/resources").resolve(name).toUri().toURL();
+      }
+      catch (MalformedURLException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    URL resource = RngHtml5SchemaProvider.class.getResource(name);
+    if (resource == null) {
+      throw new IllegalStateException("Resource '" + name + "' not found in classpath of " + RngHtml5SchemaProvider.class);
+    }
+    return resource;
   }
 }
