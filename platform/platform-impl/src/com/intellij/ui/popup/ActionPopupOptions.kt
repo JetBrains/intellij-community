c67e02131bdd4f9dddecc10f8cc11f499d3966a1
Aleksey Pivovarov <AMPivovarov@gmail.com>
2025-05-06 19:13:34 +0200

    convert ActionPopupOptions to Kotlin
    GitOrigin-RevId: 415b7c321cf285a7e51669bd1973356ef48799d5

--- platform/platform-impl/src/com/intellij/ui/popup/ActionPopupOptions.kt
+++ platform/platform-impl/src/com/intellij/ui/popup/ActionPopupOptions.kt
@@ -1,132 +1,199 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ui.popup;
+package com.intellij.ui.popup
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
-import com.intellij.openapi.util.Condition;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.ui.popup.JBPopupFactory.ActionSelectionAid
+import com.intellij.openapi.util.Condition
+import org.jetbrains.annotations.ApiStatus
 
-public final class ActionPopupOptions {
-  private final boolean showNumbers;
-  private final boolean useAlphaAsNumbers;
-  private final boolean showDisabledActions;
-  private final boolean honorActionMnemonics;
-  private final int maxRowCount;
-  private final boolean autoSelection;
-  private final Condition<? super AnAction> preselectCondition;
-  private final int defaultIndex;
-
-  public boolean showNumbers() {
-    return showNumbers;
-  }
-
-  public boolean useAlphaAsNumbers() {
-    return useAlphaAsNumbers;
-  }
-
-  public boolean showDisabledActions() {
-    return showDisabledActions;
-  }
-
-  public boolean honorActionMnemonics() {
-    return honorActionMnemonics;
-  }
-
-  public int getMaxRowCount() {
-    return maxRowCount;
-  }
-
-  public boolean autoSelectionEnabled() {
-    return autoSelection;
-  }
-
-  public Condition<? super AnAction> getPreselectCondition() {
-    return preselectCondition;
-  }
-
-  public int getDefaultIndex() {
-    return defaultIndex;
-  }
-
-  public static @NotNull ActionPopupOptions empty() {
-    return new ActionPopupOptions(false, false, false, false, -1, false, null, -1);
-  }
-
-  public static @NotNull ActionPopupOptions showDisabled() {
-    return new ActionPopupOptions(false, false, true, false, -1, false, null, -1);
-  }
-
-  public static @NotNull ActionPopupOptions honorMnemonics() {
-    return new ActionPopupOptions(false, false, false, true, -1, false, null, -1);
-  }
-
-  public static @NotNull ActionPopupOptions mnemonicsAndDisabled() {
-    return new ActionPopupOptions(false, false, true, true, -1, false, null, -1);
-  }
-
-  public static @NotNull ActionPopupOptions forStep(boolean showDisabledActions,
-                                                    boolean enableMnemonics,
-                                                    boolean autoSelection,
-                                                    @Nullable Condition<? super AnAction> preselectCondition) {
-    return new ActionPopupOptions(
-      false, false, showDisabledActions, enableMnemonics, -1,
-      autoSelection, preselectCondition, -1);
-  }
-
-  public static @NotNull ActionPopupOptions forStepAndItems(boolean showNumbers,
-                                                            boolean useAlphaAsNumbers,
-                                                            boolean showDisabledActions,
-                                                            boolean honorActionMnemonics,
-                                                            boolean autoSelectionEnabled,
-                                                            Condition<? super AnAction> preselectCondition,
-                                                            int defaultOptionIndex) {
-    return new ActionPopupOptions(
-      showNumbers, useAlphaAsNumbers, showDisabledActions, honorActionMnemonics, -1,
-      autoSelectionEnabled, preselectCondition, defaultOptionIndex);
+class ActionPopupOptions private constructor(
+  private val showNumbers: Boolean,
+  private val useAlphaAsNumbers: Boolean,
+  private val showDisabledActions: Boolean,
+  private val honorActionMnemonics: Boolean,
+  private val maxRowCount: Int,
+  private val autoSelection: Boolean,
+  private val preselectCondition: Condition<in AnAction>?,
+  private val defaultIndex: Int,
+) {
+  @ApiStatus.Internal
+  fun showNumbers(): Boolean {
+    return showNumbers
   }
 
   @ApiStatus.Internal
-  public static @NotNull ActionPopupOptions forAid(@Nullable JBPopupFactory.ActionSelectionAid aid,
-                                                   boolean showDisabledActions,
-                                                   int maxRowCount,
-                                                   @Nullable Condition<? super AnAction> preselectCondition) {
-    return new ActionPopupOptions(
-      aid == JBPopupFactory.ActionSelectionAid.ALPHA_NUMBERING || aid == JBPopupFactory.ActionSelectionAid.NUMBERING,
-      aid == JBPopupFactory.ActionSelectionAid.ALPHA_NUMBERING,
-      showDisabledActions,
-      aid == JBPopupFactory.ActionSelectionAid.MNEMONICS,
-      maxRowCount, false, preselectCondition, -1);
+  fun useAlphaAsNumbers(): Boolean {
+    return useAlphaAsNumbers
   }
 
-  public static @NotNull ActionPopupOptions create(boolean showNumbers,
-                                                   boolean useAlphaAsNumbers,
-                                                   boolean showDisabledActions,
-                                                   boolean honorActionMnemonics,
-                                                   int maxRowCount,
-                                                   boolean autoSelection,
-                                                   Condition<? super AnAction> preselectCondition) {
-    return new ActionPopupOptions(
-      showNumbers, useAlphaAsNumbers, showDisabledActions, honorActionMnemonics, maxRowCount,
-      autoSelection, preselectCondition, -1);
+  @ApiStatus.Internal
+  fun showDisabledActions(): Boolean {
+    return showDisabledActions
   }
 
-  private ActionPopupOptions(boolean showNumbers,
-                             boolean useAlphaAsNumbers,
-                             boolean showDisabledActions,
-                             boolean honorActionMnemonics,
-                             int maxRowCount,
-                             boolean autoSelection,
-                             @Nullable Condition<? super AnAction> preselectCondition,
-                             int defaultIndex) {
-    this.showNumbers = showNumbers;
-    this.useAlphaAsNumbers = useAlphaAsNumbers;
-    this.showDisabledActions = showDisabledActions;
-    this.honorActionMnemonics = honorActionMnemonics;
-    this.maxRowCount = maxRowCount;
-    this.autoSelection = autoSelection;
-    this.preselectCondition = preselectCondition;
-    this.defaultIndex = defaultIndex;
+  @ApiStatus.Internal
+  fun honorActionMnemonics(): Boolean {
+    return honorActionMnemonics
+  }
+
+  @ApiStatus.Internal
+  fun getMaxRowCount(): Int {
+    return maxRowCount
+  }
+
+  @ApiStatus.Internal
+  fun autoSelectionEnabled(): Boolean {
+    return autoSelection
+  }
+
+  @ApiStatus.Internal
+  fun getPreselectCondition(): Condition<in AnAction>? {
+    return preselectCondition
+  }
+
+  @ApiStatus.Internal
+  fun getDefaultIndex(): Int {
+    return defaultIndex
+  }
+
+  companion object {
+    @JvmStatic
+    fun empty(): ActionPopupOptions {
+      return ActionPopupOptions(
+        showNumbers = false,
+        useAlphaAsNumbers = false,
+        showDisabledActions = false,
+        honorActionMnemonics = false,
+        maxRowCount = -1,
+        autoSelection = false,
+        preselectCondition = null,
+        defaultIndex = -1
+      )
+    }
+
+    @JvmStatic
+    fun showDisabled(): ActionPopupOptions {
+      return ActionPopupOptions(
+        showNumbers = false,
+        useAlphaAsNumbers = false,
+        showDisabledActions = true,
+        honorActionMnemonics = false,
+        maxRowCount = -1,
+        autoSelection = false,
+        preselectCondition = null,
+        defaultIndex = -1
+      )
+    }
+
+    @JvmStatic
+    fun honorMnemonics(): ActionPopupOptions {
+      return ActionPopupOptions(
+        showNumbers = false,
+        useAlphaAsNumbers = false,
+        showDisabledActions = false,
+        honorActionMnemonics = true,
+        maxRowCount = -1,
+        autoSelection = false,
+        preselectCondition = null,
+        defaultIndex = -1
+      )
+    }
+
+    @JvmStatic
+    fun mnemonicsAndDisabled(): ActionPopupOptions {
+      return ActionPopupOptions(
+        showNumbers = false,
+        useAlphaAsNumbers = false,
+        showDisabledActions = true,
+        honorActionMnemonics = true,
+        maxRowCount = -1,
+        autoSelection = false,
+        preselectCondition = null,
+        defaultIndex = -1
+      )
+    }
+
+    @JvmStatic
+    fun forStep(
+      showDisabledActions: Boolean,
+      enableMnemonics: Boolean,
+      autoSelection: Boolean,
+      preselectCondition: Condition<in AnAction>?,
+    ): ActionPopupOptions {
+      return ActionPopupOptions(
+        showNumbers = false,
+        useAlphaAsNumbers = false,
+        showDisabledActions = showDisabledActions,
+        honorActionMnemonics = enableMnemonics,
+        maxRowCount = -1,
+        autoSelection = autoSelection,
+        preselectCondition = preselectCondition,
+        defaultIndex = -1
+      )
+    }
+
+    @JvmStatic
+    fun forStepAndItems(
+      showNumbers: Boolean,
+      useAlphaAsNumbers: Boolean,
+      showDisabledActions: Boolean,
+      honorActionMnemonics: Boolean,
+      autoSelectionEnabled: Boolean,
+      preselectCondition: Condition<in AnAction>?,
+      defaultOptionIndex: Int,
+    ): ActionPopupOptions {
+      return ActionPopupOptions(
+        showNumbers = showNumbers,
+        useAlphaAsNumbers = useAlphaAsNumbers,
+        showDisabledActions = showDisabledActions,
+        honorActionMnemonics = honorActionMnemonics,
+        maxRowCount = -1,
+        autoSelection = autoSelectionEnabled,
+        preselectCondition = preselectCondition,
+        defaultIndex = defaultOptionIndex
+      )
+    }
+
+    @ApiStatus.Internal
+    @JvmStatic
+    fun forAid(
+      aid: ActionSelectionAid?,
+      showDisabledActions: Boolean,
+      maxRowCount: Int,
+      preselectCondition: Condition<in AnAction>?,
+    ): ActionPopupOptions {
+      return ActionPopupOptions(
+        showNumbers = aid == ActionSelectionAid.ALPHA_NUMBERING || aid == ActionSelectionAid.NUMBERING,
+        useAlphaAsNumbers = aid == ActionSelectionAid.ALPHA_NUMBERING,
+        showDisabledActions = showDisabledActions,
+        honorActionMnemonics = aid == ActionSelectionAid.MNEMONICS,
+        maxRowCount = maxRowCount,
+        autoSelection = false,
+        preselectCondition = preselectCondition,
+        defaultIndex = -1
+      )
+    }
+
+    @JvmStatic
+    fun create(
+      showNumbers: Boolean,
+      useAlphaAsNumbers: Boolean,
+      showDisabledActions: Boolean,
+      honorActionMnemonics: Boolean,
+      maxRowCount: Int,
+      autoSelection: Boolean,
+      preselectCondition: Condition<in AnAction>?,
+    ): ActionPopupOptions {
+      return ActionPopupOptions(
+        showNumbers = showNumbers,
+        useAlphaAsNumbers = useAlphaAsNumbers,
+        showDisabledActions = showDisabledActions,
+        honorActionMnemonics = honorActionMnemonics,
+        maxRowCount = maxRowCount,
+        autoSelection = autoSelection,
+        preselectCondition = preselectCondition,
+        defaultIndex = -1
+      )
+    }
   }
 }
