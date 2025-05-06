a38edc55b6b116e5c1f574c0914ad953f8e22bda
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-05-06 10:05:36 +0200

    [ui] KTIJ-33122 Add documentation for ParameterInfoHandlerWithColoredSyntax
    GitOrigin-RevId: 4738866dc0a73434c5f58518c86fb9005ddc2673

--- platform/lang-api/src/com/intellij/lang/parameterInfo/ParameterInfoHandlerWithColoredSyntax.kt
+++ platform/lang-api/src/com/intellij/lang/parameterInfo/ParameterInfoHandlerWithColoredSyntax.kt
@@ -10,8 +10,130 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import org.jetbrains.annotations.ApiStatus.Experimental
 
+/**
+ * A specialized [com.intellij.lang.parameterInfo.ParameterInfoHandler], which supports colored syntax in parameter info.
+ *
+ * Subclasses should implement at least [findParameterOwner], [buildSignaturePresentations] and [parameterListSeparator]
+ * methods. To support highlighting of mismatched parameters, subclasses may also implement [getMismatchedParameters].
+ * To support highlighting of fully matched parameters, subclasses may also implement [isSignatureFullyMatched].
+ *
+ * Detection of the current parameter is performed by [getSignatureCurrentParameter] method, which by default uses
+ * [ParameterHtmlPresentation.sourceCodeRange]. If the code range is not provided, or if the default behavior is incorrect,
+ * subclasses should also override [getSignatureCurrentParameter].
+ *
+ * To support iteration through parameters using TAB, subclasses should implement [ParameterInfoHandlerWithTabActionSupport] interface.
+ *
+ * @param ParameterOwner Class of the PsiElement, which is the supported parameter owner at the current caret position.
+ * @param SigPresentation Subclasses may use a specialized class, which can hold additional information, especially through a customized
+ *   [ParameterHtmlPresentation] implementation.
+ *
+ * @see [ParameterInfoHandlerWithTabActionSupport]
+ */
 @Experimental
 abstract class ParameterInfoHandlerWithColoredSyntax<ParameterOwner : PsiElement, SigPresentation : SignatureHtmlPresentation> : ParameterInfoHandler<ParameterOwner, ParameterInfoHandlerWithColoredSyntaxData> {
+
+  /**
+   * Finds the PsiElement, which is the parameter owner at the current caret position.
+   *
+   * @return parameter owner, or null if no parameter owner was found at the current caret position.
+   */
+  protected abstract fun findParameterOwner(file: PsiFile, offset: Int): ParameterOwner?
+
+  /**
+   * The core method of the [ParameterInfoHandler]. The returned object should contain a list of parameters and information
+   * whether the signature is deprecated. Each of the parameters should have a name and a type with an optional default value.
+   * Both strings should be provided with HTML markup. To render colored syntax, methods of
+   * [com.intellij.lang.documentation.QuickDocHighlightingHelper] can be used. Each parameter may be matched to the provided
+   * argument in the parameter owner arguments list through the optional [ParameterHtmlPresentation.sourceCodeRange]. If the
+   * source code range is not provided, [getSignatureCurrentParameter] should be overridden to provide the current parameter index.
+   *
+   * If base [SignatureHtmlPresentation] and [ParameterHtmlPresentation] interfaces are enough, subclasses may use
+   * `SignatureHtmlPresentation()` and `ParameterHtmlPresentation()` methods to create objects of these classes.
+   */
+  protected abstract fun buildSignaturePresentations(parameterOwner: ParameterOwner): List<SigPresentation>
+
+  /**
+   * An HTML string, which will be used as a separator between elements in the parameter info list.
+   * For many languages this would be a `,` character. A space between the elements is automatically added.
+   */
+  protected abstract val parameterListSeparator: String
+
+  /**
+   * Implement to support the display of mismatched parameters. Such parameters would appear
+   * with a red highlight. The code should return the set of all mismatched parameters, which
+   * have a corresponding argument in the parameter owner arguments list, even these which
+   * are ahead of the current caret position.
+   */
+  protected open fun getMismatchedParameters(
+    signature: SigPresentation,
+    parameterOwner: ParameterOwner,
+    context: ParameterInfoContext,
+  ): Set<ParameterHtmlPresentation> = emptySet()
+
+  /**
+   * Implement to support the display of fully matched signatures. Such signatures would appear
+   * with a green background. The code should return true if the given signature is fully matched
+   * with all provided parameter owner arguments. It means that the signature will be used in the runtime.
+   * The green highlight will show up only if there is a single signature matched and only if there are multiple
+   * signatures/overloads provided.
+   */
+  protected open fun isSignatureFullyMatched(
+    signature: SigPresentation,
+    parameterOwner: ParameterOwner,
+  ): Boolean = false
+
+  /**
+   * If the handler is not able to provide the [ParameterHtmlPresentation.sourceCodeRange] when creating presentation,
+   * it should override this method to provide the current parameter index.
+   */
+  protected open fun getSignatureCurrentParameter(
+    signature: SigPresentation,
+    parameterOwner: ParameterOwner,
+    context: ParameterInfoContext,
+  ): Int {
+    val text = context.editor.document.text
+    val offset = StringUtil.skipWhitespaceForward(text, context.offset)
+    return signature.parameters
+      .indexOfLast { it.sourceCodeRange != null && offset in it.sourceCodeRange!!.startOffset..StringUtil.skipWhitespaceForward(text, it.sourceCodeRange!!.endOffset) }
+  }
+
+  /**
+   * A presentation of a call signature in the parameter info list.
+   *
+   * @property parameters A list of signature parameters with HTML presentation
+   * @property deprecated Whether the call signature is deprecated
+   */
+  interface SignatureHtmlPresentation {
+    val parameters: List<ParameterHtmlPresentation>
+    val deprecated: Boolean get() = false
+  }
+
+  /**
+   * A presentation of a call signature parameter in the parameter info list.
+   *
+   * @property nameAndType A string with HTML markup, which should contain the parameter name and the type.
+   * @property sourceCodeRange An optional range of the corresponding argument in the source code. To render colored syntax, methods of
+   *  [com.intellij.lang.documentation.QuickDocHighlightingHelper] can be used.
+   * @property defaultValue An optional string with HTML markup, which should contain the default value of the parameter if any.
+   */
+  interface ParameterHtmlPresentation {
+    val nameAndType: String
+    val sourceCodeRange: TextRange? get() = null
+    val defaultValue: String? get() = null
+  }
+
+  protected fun SignatureHtmlPresentation(parameters: List<ParameterHtmlPresentation>, deprecated: Boolean = false): SignatureHtmlPresentation =
+    object : SignatureHtmlPresentation {
+      override val parameters: List<ParameterHtmlPresentation> = parameters
+      override val deprecated: Boolean = deprecated
+    }
+
+  protected fun ParameterHtmlPresentation(nameAndType: String, sourceCodeRange: TextRange? = null, defaultValue: String? = null): ParameterHtmlPresentation =
+    object : ParameterHtmlPresentation {
+      override val nameAndType: String get() = nameAndType
+      override val sourceCodeRange: TextRange? get() = sourceCodeRange
+      override val defaultValue: String? get() = defaultValue
+    }
 
   final override fun findElementForParameterInfo(context: CreateParameterInfoContext): ParameterOwner? {
     val parameterOwner = findParameterOwner(context.file ?: return null, context.offset) ?: return null
@@ -68,34 +190,6 @@ abstract class ParameterInfoHandlerWithColoredSyntax<ParameterOwner : PsiElement
       null
   }
 
-  protected abstract fun findParameterOwner(file: PsiFile, offset: Int): ParameterOwner?
-
-  protected abstract fun buildSignaturePresentations(parameterOwner: ParameterOwner): List<SigPresentation>
-
-  protected abstract val parameterListSeparator: String
-
-  protected open fun getMismatchedParameters(
-    signature: SigPresentation,
-    parameterOwner: ParameterOwner,
-    context: ParameterInfoContext,
-  ): Set<ParameterHtmlPresentation> = emptySet()
-
-  protected open fun isSignatureFullyMatched(
-    signature: SigPresentation,
-    parameterOwner: ParameterOwner,
-  ): Boolean = false
-
-  protected open fun getSignatureCurrentParameter(
-    signature: SigPresentation,
-    parameterOwner: ParameterOwner,
-    context: ParameterInfoContext,
-  ): Int {
-    val text = context.editor.document.text
-    val offset = StringUtil.skipWhitespaceForward(text, context.offset)
-    return signature.parameters
-      .indexOfLast { it.sourceCodeRange != null && offset in it.sourceCodeRange!!.startOffset..StringUtil.skipWhitespaceForward(text, it.sourceCodeRange!!.endOffset) }
-  }
-
   final override fun updateUI(presentation: ParameterInfoHandlerWithColoredSyntaxData, context: ParameterInfoUIContext) {
     context.setupSignatureHtmlPresentation(
       presentation.signatureHtmlPresentation.parameters.map {
@@ -105,30 +199,6 @@ abstract class ParameterInfoHandlerWithColoredSyntax<ParameterOwner : PsiElement
       parameterListSeparator,
       presentation.signatureHtmlPresentation.deprecated
     )
-  }
-
-  protected fun SignatureHtmlPresentation(parameters: List<ParameterHtmlPresentation>, deprecated: Boolean = false): SignatureHtmlPresentation =
-    object : SignatureHtmlPresentation {
-      override val parameters: List<ParameterHtmlPresentation> = parameters
-      override val deprecated: Boolean = deprecated
-    }
-
-  interface SignatureHtmlPresentation {
-    val parameters: List<ParameterHtmlPresentation>
-    val deprecated: Boolean get() = false
-  }
-
-  protected fun ParameterHtmlPresentation(nameAndType: String, sourceCodeRange: TextRange? = null, defaultValue: String? = null): ParameterHtmlPresentation =
-    object : ParameterHtmlPresentation {
-      override val nameAndType: String get() = nameAndType
-      override val sourceCodeRange: TextRange? get() = sourceCodeRange
-      override val defaultValue: String? get() = defaultValue
-    }
-
-  interface ParameterHtmlPresentation {
-    val nameAndType: String
-    val sourceCodeRange: TextRange? get() = null
-    val defaultValue: String? get() = null
   }
 
   class ParameterInfoHandlerWithColoredSyntaxData internal constructor(
