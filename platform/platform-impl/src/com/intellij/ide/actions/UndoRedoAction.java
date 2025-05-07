fed86616dc7db766e12d5aa801888f961f5e1cf7
Alexander Trushev <aleksandr.trushev@jetbrains.com>
2025-05-07 14:34:44 +0200

    IJPL-185748 Refactor `UndoRedoAction`
    GitOrigin-RevId: a089e2ba212bf80a11623def6bea41cb6410fbeb

--- platform/platform-impl/src/com/intellij/ide/actions/UndoRedoAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/UndoRedoAction.java
@@ -2,13 +2,10 @@
 package com.intellij.ide.actions;
 
 import com.intellij.ide.lightEdit.LightEditCompatible;
-import com.intellij.idea.ActionsBundle;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehavior;
 import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification;
-import com.intellij.openapi.command.undo.DocumentReference;
 import com.intellij.openapi.command.undo.UndoManager;
-import com.intellij.openapi.command.undo.UndoableAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.TextEditor;
@@ -16,11 +13,11 @@ import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.util.Key;
-import com.intellij.openapi.util.NlsActions;
+import com.intellij.openapi.util.NlsActions.ActionDescription;
+import com.intellij.openapi.util.NlsActions.ActionText;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.ui.ClientProperty;
-import com.intellij.util.ui.SwingUndoUtil;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.ApiStatus.Internal;
 import org.jetbrains.annotations.NotNull;
@@ -30,9 +27,15 @@ import javax.swing.*;
 import javax.swing.text.JTextComponent;
 import java.awt.*;
 
+
 public abstract class UndoRedoAction extends DumbAwareAction implements ActionRemoteBehaviorSpecification, LightEditCompatible {
-  private static final Logger LOG = Logger.getInstance(UndoRedoAction.class);
+
+  /**
+   * Allow JB UndoManager for a JTextComponent (IJPL-8951)
+   */
   public static final Key<Boolean> IGNORE_SWING_UNDO_MANAGER = new Key<>("IGNORE_SWING_UNDO_MANAGER");
+
+  private static final Logger LOG = Logger.getInstance(UndoRedoAction.class);
 
   private boolean myActionInProgress;
 
@@ -40,26 +43,11 @@ public abstract class UndoRedoAction extends DumbAwareAction implements ActionRe
     setEnabledInModalContext(true);
   }
 
-  @Override
-  public @NotNull ActionUpdateThread getActionUpdateThread() {
-    return ActionUpdateThread.EDT;
-  }
+  protected abstract boolean isAvailable(FileEditor editor, UndoManager undoManager);
 
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
-    DataContext dataContext = e.getDataContext();
-    FileEditor editor = PlatformCoreDataKeys.FILE_EDITOR.getData(dataContext);
-    UndoManager undoManager = getUndoManager(editor, dataContext, true);
-    if (undoManager == null) return;
+  protected abstract void perform(FileEditor editor, UndoManager undoManager);
 
-    myActionInProgress = true;
-    try {
-      perform(editor, undoManager);
-    }
-    finally {
-      myActionInProgress = false;
-    }
-  }
+  protected abstract Pair<@ActionText String, @ActionDescription String> getActionNameAndDescription(FileEditor editor, UndoManager undoManager);
 
   @Override
   public void update(@NotNull AnActionEvent event) {
@@ -73,10 +61,31 @@ public abstract class UndoRedoAction extends DumbAwareAction implements ActionRe
     }
     presentation.setEnabled(isAvailable(editor, undoManager));
 
-    Pair<@NlsActions.ActionText String, @NlsActions.ActionDescription String> pair = getActionNameAndDescription(editor, undoManager);
-
+    var pair = getActionNameAndDescription(editor, undoManager);
     presentation.setText(pair.first);
     presentation.setDescription(pair.second);
+  }
+
+  @Override
+  public void actionPerformed(@NotNull AnActionEvent event) {
+    DataContext dataContext = event.getDataContext();
+    FileEditor editor = PlatformCoreDataKeys.FILE_EDITOR.getData(dataContext);
+    UndoManager undoManager = getUndoManager(editor, dataContext, true);
+    if (undoManager == null) {
+      return;
+    }
+    myActionInProgress = true;
+    try {
+      perform(editor, undoManager);
+    }
+    finally {
+      myActionInProgress = false;
+    }
+  }
+
+  @Override
+  public @NotNull ActionUpdateThread getActionUpdateThread() {
+    return ActionUpdateThread.EDT;
   }
 
   @Internal
@@ -89,7 +98,7 @@ public abstract class UndoRedoAction extends DumbAwareAction implements ActionRe
     return ActionRemoteBehavior.BackendOnly;
   }
 
-  private UndoManager getUndoManager(FileEditor editor, DataContext dataContext, boolean isActionPerformed) {
+  private @Nullable UndoManager getUndoManager(@Nullable FileEditor editor, DataContext dataContext, boolean isActionPerformed) {
     Component component = PlatformCoreDataKeys.CONTEXT_COMPONENT.getData(dataContext);
     if (component instanceof JTextComponent && !ClientProperty.isTrue(component, IGNORE_SWING_UNDO_MANAGER)) {
       return SwingUndoManagerWrapper.fromContext(dataContext);
@@ -106,126 +115,20 @@ public abstract class UndoRedoAction extends DumbAwareAction implements ActionRe
       }
     }
     if (myActionInProgress && isActionPerformed) {
-      LOG.error("Recursive undo invocation attempt, component: " + component + ", fileEditor: " + editor +
-                ", rootPane: " + rootPane + ", popup: " + popup);
+      LOG.error(
+        "Recursive undo invocation attempt, component: %s, fileEditor: %s, rootPane: %s, popup: %s"
+          .formatted(component, editor, rootPane, popup)
+      );
       return null;
     }
-
     Project project = getProject(editor, dataContext);
     return project != null && !project.isDefault() ? UndoManager.getInstance(project) : UndoManager.getGlobalInstance();
   }
 
-  private static Project getProject(FileEditor editor, DataContext dataContext) {
-    Project project;
-    if (editor instanceof TextEditor) {
-      project = ((TextEditor)editor).getEditor().getProject();
+  private static @Nullable Project getProject(FileEditor editor, DataContext dataContext) {
+    if (editor instanceof TextEditor textEditor) {
+      return textEditor.getEditor().getProject();
     }
-    else {
-      project = CommonDataKeys.PROJECT.getData(dataContext);
-    }
-    return project;
-  }
-
-  protected abstract void perform(FileEditor editor, UndoManager undoManager);
-
-  protected abstract boolean isAvailable(FileEditor editor, UndoManager undoManager);
-
-  protected abstract Pair<@NlsActions.ActionText String, @NlsActions.ActionDescription String> getActionNameAndDescription(FileEditor editor, UndoManager undoManager);
-
-  private static final class SwingUndoManagerWrapper extends UndoManager{
-    private final javax.swing.undo.UndoManager mySwingUndoManager;
-
-    static @Nullable UndoManager fromContext(DataContext dataContext) {
-      javax.swing.undo.UndoManager swingUndoManager =
-        SwingUndoUtil.getUndoManager(PlatformCoreDataKeys.CONTEXT_COMPONENT.getData(dataContext));
-      return swingUndoManager != null ? new SwingUndoManagerWrapper(swingUndoManager) : null;
-    }
-
-    SwingUndoManagerWrapper(javax.swing.undo.UndoManager swingUndoManager) {
-      mySwingUndoManager = swingUndoManager;
-    }
-
-    @Override
-    public void undoableActionPerformed(@NotNull UndoableAction action) {
-    }
-
-    @Override
-    public void nonundoableActionPerformed(@NotNull DocumentReference ref, boolean isGlobal) {
-    }
-
-    @Override
-    public boolean isUndoInProgress() {
-      return false;
-    }
-
-    @Override
-    public boolean isRedoInProgress() {
-      return false;
-    }
-
-    @Override
-    public void undo(@Nullable FileEditor editor) {
-       mySwingUndoManager.undo();
-    }
-
-    @Override
-    public void redo(@Nullable FileEditor editor) {
-      mySwingUndoManager.redo();
-    }
-
-    @Override
-    public boolean isUndoAvailable(@Nullable FileEditor editor) {
-      return mySwingUndoManager.canUndo();
-    }
-
-    @Override
-    public boolean isRedoAvailable(@Nullable FileEditor editor) {
-      return mySwingUndoManager.canRedo();
-    }
-
-    @Override
-    public @NotNull Pair<String, String> getUndoActionNameAndDescription(FileEditor editor) {
-      return getUndoOrRedoActionNameAndDescription( true);
-    }
-
-    @Override
-    public @NotNull Pair<String, String> getRedoActionNameAndDescription(FileEditor editor) {
-      return getUndoOrRedoActionNameAndDescription( false);
-    }
-
-    @Override
-    public long getNextUndoNanoTime(@NotNull FileEditor editor) {
-      return -1L;
-    }
-
-    @Override
-    public long getNextRedoNanoTime(@NotNull FileEditor editor) {
-      return -1L;
-    }
-
-    @Override
-    public boolean isNextUndoAskConfirmation(@NotNull FileEditor editor) {
-      return false;
-    }
-
-    @Override
-    public boolean isNextRedoAskConfirmation(@NotNull FileEditor editor) {
-      return false;
-    }
-
-    private static @NotNull Pair<String, String> getUndoOrRedoActionNameAndDescription(boolean undo) {
-      if (undo) {
-        return Pair.create(
-          ActionsBundle.message("action.undo.text", "").trim(),
-          ActionsBundle.message("action.undo.description",
-                                ActionsBundle.message("action.undo.description.empty")).trim());
-      }
-      else {
-        return Pair.create(
-          ActionsBundle.message("action.redo.text", "").trim(),
-          ActionsBundle.message("action.redo.description",
-                                ActionsBundle.message("action.redo.description.empty")).trim());
-      }
-    }
+    return CommonDataKeys.PROJECT.getData(dataContext);
   }
 }
