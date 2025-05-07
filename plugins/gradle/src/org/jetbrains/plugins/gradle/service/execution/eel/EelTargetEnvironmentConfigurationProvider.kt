9c201e2018edb7714d639b4ef2e82bda01f1776b
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2025-05-07 14:31:36 +0200

    [gradle][eel][docker][IDEA-370379] use EEL for tooling proxy port forwarding
    GitOrigin-RevId: 991cdb41fceb7dd5601a93c657f70e7b359add02

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/execution/eel/EelTargetEnvironmentConfigurationProvider.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/execution/eel/EelTargetEnvironmentConfigurationProvider.kt
@@ -1,21 +1,28 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.service.execution.eel
 
+import com.intellij.execution.target.HostPort
 import com.intellij.execution.target.TargetEnvironmentConfiguration
 import com.intellij.execution.target.eel.EelTargetEnvironmentRequest
-import com.intellij.openapi.externalSystem.service.execution.TargetEnvironmentConfigurationProvider
+import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.toCanonicalPath
 import com.intellij.platform.eel.EelApi
+import com.intellij.platform.eel.EelTunnelsApi.HostAddress
 import com.intellij.platform.eel.fs.getPath
 import com.intellij.platform.eel.provider.LocalEelDescriptor
 import com.intellij.platform.eel.provider.asEelPath
-import com.intellij.platform.eel.provider.asNioPath
 import com.intellij.platform.eel.provider.asNioPathOrNull
+import com.intellij.platform.eel.provider.getEelDescriptor
+import com.intellij.platform.eel.provider.utils.forwardLocalPort
 import com.intellij.util.PathMapper
+import com.intellij.util.net.NetUtils
+import org.jetbrains.plugins.gradle.GradleCoroutineScope.gradleCoroutineScope
+import org.jetbrains.plugins.gradle.service.execution.GradleServerConfigurationProvider
 import java.nio.file.Path
+import kotlin.time.Duration.Companion.seconds
 
-class EelTargetEnvironmentConfigurationProvider(val eel: EelApi, val project: Project) : TargetEnvironmentConfigurationProvider {
+class EelTargetEnvironmentConfigurationProvider(val eel: EelApi, val project: Project) : GradleServerConfigurationProvider {
 
   override val environmentConfiguration: TargetEnvironmentConfiguration by lazy { resolveEnvironmentConfiguration() }
   override val pathMapper: PathMapper by lazy { EelPathMapper(eel, project) }
@@ -53,5 +60,28 @@ class EelTargetEnvironmentConfigurationProvider(val eel: EelApi, val project: Pr
         convertToRemote(it)
       }
     }
+  }
+
+  override fun getClientCommunicationAddress(
+    targetEnvironmentConfiguration: TargetEnvironmentConfiguration,
+    gradleServerHostPort: HostPort,
+  ): HostPort {
+    return runBlockingCancellable {
+      val forwardedPort = forwardToolingProxyPortOntoLocalMachine(gradleServerHostPort.port)
+      return@runBlockingCancellable HostPort(NetUtils.getLocalHostString(), forwardedPort)
+    }
+  }
+
+  private suspend fun forwardToolingProxyPortOntoLocalMachine(port: Int): Int {
+    val eel = project.getEelDescriptor().upgrade()
+    // the address to which all the request will be forwarded to
+    // in this case port -- port of the tooling proxy
+    val address = HostAddress.Builder(port.toUShort())
+      .connectionTimeout(90.seconds)
+      .build()
+    // the local port which are bridges requests into [port]
+    val localPort = NetUtils.findAvailableSocketPort()
+    project.gradleCoroutineScope.forwardLocalPort(eel.tunnels, localPort, address)
+    return localPort
   }
 }
