54b7c7cf41aab0d291c0a784e1adc7252a2bfb82
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-05-07 11:10:01 +0200

    [debugger-rd] IJPL-186922: Don't initialize RemoteApiRegistry on EDT
    GitOrigin-RevId: 99e071fa2504fe59c50dfda82a52c4ba63b90b27

--- platform/kernel/rpc/src/RemoteApiProviderService.kt
+++ platform/kernel/rpc/src/RemoteApiProviderService.kt
@@ -1,9 +1,12 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.rpc
 
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.UI
 import fleet.rpc.RemoteApi
 import fleet.rpc.RemoteApiDescriptor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 interface RemoteApiProviderService {
 
@@ -11,7 +14,9 @@ interface RemoteApiProviderService {
 
   companion object {
     suspend fun <T : RemoteApi<Unit>> resolve(descriptor: RemoteApiDescriptor<T>): T {
-      return ApplicationManager.getApplication().getService(RemoteApiProviderService::class.java).resolve(descriptor)
+      return withContext(Dispatchers.IO) {
+        ApplicationManager.getApplication().getService(RemoteApiProviderService::class.java).resolve(descriptor)
+      }
     }
   }
 }
