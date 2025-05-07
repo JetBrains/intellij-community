98c0eed1eef9f8e7c0577500df9725072472c76a
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-05-07 17:17:50 +0300

    IJPL-184967 Add fullGC and delays to project leak testing
    This makes the test much less flaky. There are some
    false positives still left, though. Sometimes the test finds
    pending-finalization references.
    
    GitOrigin-RevId: 80e35ef4f01b87c9d60123398754e2ea2a71b1c4

--- plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/commands/GCCommand.kt
+++ plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/commands/GCCommand.kt
@@ -1,9 +1,11 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.performancePlugin.commands
 
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.ui.playback.PlaybackContext
 import com.intellij.openapi.ui.playback.commands.AbstractCommand
 import com.intellij.openapi.util.ActionCallback
+import com.jetbrains.JBR
 import com.jetbrains.performancePlugin.utils.ActionCallbackProfilerStopper
 import org.jetbrains.concurrency.Promise
 import org.jetbrains.concurrency.toPromise
@@ -21,7 +23,13 @@ internal class GCCommand(text: String, line: Int) : AbstractCommand(text, line) 
     val ref = WeakReference(IntArray(32 * 1024))
     try {
       Waiter.checkCondition {
-        System.gc()
+        if (JBR.isSystemUtilsSupported()) {
+          JBR.getSystemUtils().fullGC()
+        }
+        else {
+          LOG.warn("The JBR.getSystemUtils API isn't supported. Using regular GC, but this might increase the probability of false positives")
+          System.gc()
+        }
         (ref.get() == null).also { done ->
           if (done) {
             actionCallback.setDone()
@@ -35,3 +43,5 @@ internal class GCCommand(text: String, line: Int) : AbstractCommand(text, line) 
     return actionCallback.toPromise()
   }
 }
+
+private val LOG = logger<GCCommand>()
