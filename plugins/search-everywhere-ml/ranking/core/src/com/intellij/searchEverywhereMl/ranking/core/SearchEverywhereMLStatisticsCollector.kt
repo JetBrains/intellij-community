c7f163918f6040d3f743354ccb6f77e8b997fca5
Adam Malek <adam.malek@jetbrains.com>
2025-05-07 09:59:33 +0200

    ML in SE: Update group version number after model reupload (IJPL-186473)
    Though the behavior of each model
    has not changed, just in case,
    we are updating the version number.
    
    GitOrigin-RevId: f33b7ce728d22643f9bc27effc5866c2c8a1cdfd

--- plugins/search-everywhere-ml/ranking/core/src/com/intellij/searchEverywhereMl/ranking/core/SearchEverywhereMLStatisticsCollector.kt
+++ plugins/search-everywhere-ml/ranking/core/src/com/intellij/searchEverywhereMl/ranking/core/SearchEverywhereMLStatisticsCollector.kt
@@ -294,7 +294,7 @@ object SearchEverywhereMLStatisticsCollector : CounterUsagesCollector() {
     return true
   }
 
-  private val GROUP = EventLogGroup("mlse.log", 113, MLSE_RECORDER_ID)
+  private val GROUP = EventLogGroup("mlse.log", 114, MLSE_RECORDER_ID)
 
   private val IS_INTERNAL = EventFields.Boolean("isInternal")
   private val ORDER_BY_ML_GROUP = EventFields.Boolean("orderByMl")
