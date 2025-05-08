e9d01cc57d1be9f8300fc4d74e7e480eb0dde87e
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2025-05-08 22:52:25 +0400

    [kotlin] Fix K1 test by adding missing // WITH_STDLIB
    IJ-CR-160931
    #KTIJ-33891
    
    GitOrigin-RevId: 55574f078f7ce80e1dea16aa640776b272a6e724

--- plugins/kotlin/idea/tests/testData/quickfix/changeSignature/changeFunctionReturnTypeToNothing.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/changeSignature/changeFunctionReturnTypeToNothing.kt
@@ -1,8 +1,9 @@
 // "Change return type of enclosing function 'Test.foo' to 'Nothing'" "true"
+// WITH_STDLIB
 
 class Test {
-    fun fo<caret>o() = TODO()
+    fun fo<caret>o() = throw NotImplementedError()
 }
 
-// IGNORE_K1
+// FUS_QUICKFIX_NAME: org.jetbrains.kotlin.idea.quickfix.ChangeCallableReturnTypeFix$ForEnclosing
 // FUS_K2_QUICKFIX_NAME: org.jetbrains.kotlin.idea.k2.codeinsight.fixes.ChangeTypeQuickFixFactories$UpdateTypeQuickFix
