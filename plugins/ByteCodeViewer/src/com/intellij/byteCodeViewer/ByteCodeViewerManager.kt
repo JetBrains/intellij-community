75ecbf846d657017d900795078f49b82dd4458fe
joe <burtonjae@hotmail.co.uk>
2025-05-08 22:35:37 +0100

    [ByteCodeViewer] IDEA-372346 Fix local classes not working
    Allow local classes to work with bytecode viewer.
    
    Add a method in ClassUtil that takes into account anonymous and local classes
    and move duplicated logic into that method.
    
    Presumably, everything that used JavaAnonymousClassesHelper wanted local classes too.
    
    Also deprecate ClassUtil#getJVMClassName
    
    #IDEA-372346 fixed
    
    closes https://github.com/JetBrains/intellij-community/pull/3044
    
    GitOrigin-RevId: 7d300719c079d3de5a7cb589d50431326526faa0

--- plugins/ByteCodeViewer/src/com/intellij/byteCodeViewer/ByteCodeViewerManager.kt
+++ plugins/ByteCodeViewer/src/com/intellij/byteCodeViewer/ByteCodeViewerManager.kt
@@ -2,7 +2,6 @@
 package com.intellij.byteCodeViewer
 
 import com.intellij.ide.highlighter.JavaClassFileType
-import com.intellij.ide.util.JavaAnonymousClassesHelper
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.roots.CompilerModuleExtension
@@ -30,7 +29,7 @@ object ByteCodeViewerManager {
     val fileClass = aClass.containingClassFileClass()
     val file = fileClass.originalElement.containingFile.virtualFile ?: return null
     val fileIndex = ProjectFileIndex.getInstance(aClass.project)
-    val jvmClassName = getJVMClassName(aClass) ?: return null
+    val jvmClassName = ClassUtil.getBinaryClassName(aClass) ?: return null
     return if (FileTypeRegistry.getInstance().isFileOfType(file, JavaClassFileType.INSTANCE)) {
       // compiled class; looking for the right .class file (inner class 'A.B' is "contained" in 'A.class', but we need 'A$B.class')
       file.parent.findChild(StringUtil.getShortName(jvmClassName) + ".class")
@@ -50,13 +49,6 @@ object ByteCodeViewerManager {
   @JvmStatic
   fun loadClassFileBytes(aClass: PsiClass): ByteArray? {
     return findClassFile(aClass)?.contentsToByteArray(false)
-  }
-
-  private fun getJVMClassName(aClass: PsiClass): String? {
-    if (aClass !is PsiAnonymousClass) return ClassUtil.getJVMClassName(aClass)
-    val containingClass = PsiTreeUtil.getParentOfType<PsiClass?>(aClass, PsiClass::class.java)
-    if (containingClass != null) return getJVMClassName(containingClass) + JavaAnonymousClassesHelper.getName(aClass)
-    return null
   }
 
   @JvmStatic
