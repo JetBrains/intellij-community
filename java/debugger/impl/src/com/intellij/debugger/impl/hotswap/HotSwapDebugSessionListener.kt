b38340d92361222d81d1d1123a3d807ebad4f3e4
Maksim Zuev <maksim.zuev@jetbrains.com>
2025-05-09 12:27:35 +0200

    [debugger] IDEA-372303 HotSwap: switch to XDebuggerManagerListener as it seems more stable
    GitOrigin-RevId: 646c4770cc5867f3e2da6110bea27b38db91eff9

--- java/debugger/impl/src/com/intellij/debugger/impl/hotswap/HotSwapDebugSessionListener.kt
+++ java/debugger/impl/src/com/intellij/debugger/impl/hotswap/HotSwapDebugSessionListener.kt
@@ -1,72 +1,65 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.debugger.impl.hotswap
 
-import com.intellij.debugger.impl.DebuggerManagerListener
+import com.intellij.debugger.engine.JavaDebugProcess
 import com.intellij.debugger.impl.DebuggerSession
 import com.intellij.debugger.ui.HotSwapStatusListener
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.registry.Registry
+import com.intellij.util.asDisposable
+import com.intellij.xdebugger.XDebugProcess
 import com.intellij.xdebugger.XDebugSession
 import com.intellij.xdebugger.XDebuggerManager
 import com.intellij.xdebugger.XDebuggerManagerListener
+import com.intellij.xdebugger.impl.XDebugSessionImpl
 import com.intellij.xdebugger.impl.hotswap.HotSwapResultListener
 import com.intellij.xdebugger.impl.hotswap.HotSwapSession
 import com.intellij.xdebugger.impl.hotswap.HotSwapSessionManager
+import kotlinx.coroutines.CoroutineScope
 import java.util.concurrent.ConcurrentHashMap
 
-@Service(Service.Level.PROJECT)
-internal class HotSwapDebugSessionManager(project: Project) : Disposable.Default, XDebuggerManagerListener {
-  private val disposables = ConcurrentHashMap<DebuggerSession, Disposable>()
-  private val sessions = ConcurrentHashMap<DebuggerSession, HotSwapSession<*>>()
+private data class JavaHotSwapSessionEntry(val hotSwapSession: HotSwapSession<*>, val disposable: Disposable)
+
+internal class HotSwapDebugSessionManager(project: Project, cs: CoroutineScope) : XDebuggerManagerListener {
+  private val sessions = ConcurrentHashMap<DebuggerSession, JavaHotSwapSessionEntry>()
 
   init {
-    project.getMessageBus().connect(this).subscribe(XDebuggerManager.TOPIC, this)
+    project.getMessageBus().connect(cs).subscribe(XDebuggerManager.TOPIC, this)
   }
 
-  fun createSessionListenerOrNull(session: DebuggerSession): HotSwapStatusListener? = getSession(session)
-    ?.let { HotSwapStatusListenerAdapter(session, it.startHotSwapListening()) }
-
-  internal fun getSession(session: DebuggerSession): HotSwapSession<*>? = sessions[session]
-
-  companion object {
-    @JvmStatic
-    fun getInstance(project: Project): HotSwapDebugSessionManager = project.service()
+  fun createSessionListenerOrNull(session: DebuggerSession): HotSwapStatusListener? {
+    val hotSwapSession = findHotSwapSession(session) ?: return null
+    return HotSwapStatusListenerAdapter(session, hotSwapSession.startHotSwapListening())
   }
 
-  internal fun createSession(session: DebuggerSession) {
+  internal fun findHotSwapSession(session: DebuggerSession): HotSwapSession<*>? = sessions[session]?.hotSwapSession
+
+  override fun processStarted(debugProcess: XDebugProcess) {
+    val session = (debugProcess as? JavaDebugProcess)?.debuggerSession ?: return
     if (!Registry.`is`("debugger.hotswap.floating.toolbar")) return
-    val disposable = Disposer.newDisposable()
-    disposables[session] = disposable
+    val disposable = (debugProcess.session as XDebugSessionImpl).coroutineScope.asDisposable()
     val hotSwapSession = HotSwapSessionManager.getInstance(session.project).createSession(JvmHotSwapProvider(session), disposable)
-    sessions[session] = hotSwapSession
+    sessions[session] = JavaHotSwapSessionEntry(hotSwapSession, disposable)
   }
 
-  internal fun removeSession(session: DebuggerSession) {
-    disposables.remove(session)?.let { Disposer.dispose(it) }
-    sessions.remove(session)
+  override fun processStopped(debugProcess: XDebugProcess) {
+    val javaDebugSession = (debugProcess as? JavaDebugProcess)?.debuggerSession ?: return
+    val sessionEntry = sessions.remove(javaDebugSession) ?: return
+    Disposer.dispose(sessionEntry.disposable)
   }
 
   override fun currentSessionChanged(previousSession: XDebugSession?, currentSession: XDebugSession?) {
     if (currentSession == null) return
-    val (_, hotSwapSession) = sessions.entries.firstOrNull { (session, _) -> session.xDebugSession === currentSession } ?: return
-    HotSwapSessionManager.getInstance(currentSession.project).onSessionSelected(hotSwapSession)
-  }
-}
-
-
-internal class HotSwapDebugSessionListener : DebuggerManagerListener {
-  override fun sessionCreated(session: DebuggerSession?) {
-    if (session == null) return
-    HotSwapDebugSessionManager.getInstance(session.project).createSession(session)
+    val (_, entry) = sessions.entries.firstOrNull { (session, _) -> session.xDebugSession === currentSession } ?: return
+    HotSwapSessionManager.getInstance(currentSession.project).onSessionSelected(entry.hotSwapSession)
   }
 
-  override fun sessionRemoved(session: DebuggerSession?) {
-    if (session == null) return
-    HotSwapDebugSessionManager.getInstance(session.project).removeSession(session)
+  companion object {
+    @JvmStatic
+    fun getInstance(project: Project): HotSwapDebugSessionManager = project.service()
   }
 }
 
@@ -92,6 +85,6 @@ private class HotSwapStatusListenerAdapter(private val originalSession: Debugger
   }
 
   private fun List<DebuggerSession>.createListeners(except: DebuggerSession) = filter { it !== except }
-    .mapNotNull { HotSwapDebugSessionManager.getInstance(except.project).getSession(it) }
+    .mapNotNull { HotSwapDebugSessionManager.getInstance(except.project).findHotSwapSession(it) }
     .map { it.startHotSwapListening() }
 }
