6987fa35af0ce1597f468075ab2dbc284d4bb109
Alexandru Resiga <alexandru.resiga@jetbrains.com>
2025-05-09 17:04:56 +0200

    KMT-1078 [compose resources] extend search scope for custom compose resources directories
    - propagate isCustom property from Gradle build settings
    - when searching for the matching Kotlin property of a resource, extend the search scope for custom
    composeResources dirs which can be located e.g., outside the source set they are provided for
    
    (cherry picked from commit 17f18c64e19ed4f15b77e0432798eed356c94ef6)
    
    GitOrigin-RevId: 20e790cececeb90c2a32c301d5ac9c0edf14d6a5

--- plugins/compose/intellij.compose.ide.plugin.resources/src/com/intellij/compose/ide/plugin/resources/ComposeResourcesBase.kt
+++ plugins/compose/intellij.compose.ide.plugin.resources/src/com/intellij/compose/ide/plugin/resources/ComposeResourcesBase.kt
@@ -65,10 +65,13 @@ internal interface ComposeResourcesBase {
     val name = getName(element) ?: return null
     val module = element.module ?: return null
     val project = element.project
-    val projectScope = GlobalSearchScope.moduleScope(module)
-    return KotlinPropertyShortNameIndex[name, project, projectScope]
+    val composeResourcePath = element.containingFile.virtualFile.toNioPathOrNull() ?: return null
+    val composeResourcesDir = project.getAllComposeResourcesDirs().firstOrNull { composeResourcePath.startsWith(it.directoryPath) } ?: return null
+    // custom compose resources dirs can be anywhere, so we search in the whole project
+    val searchScope = if (composeResourcesDir.isCustom) GlobalSearchScope.projectScope(project) else GlobalSearchScope.moduleScope(module)
+    return KotlinPropertyShortNameIndex[name, project, searchScope]
       .filterIsInstance<KtProperty>() // even though it's called KotlinPropertyShortNameIndex it returns KtNamedDeclaration
-      .firstOrNull { it.isTopLevel } // todo[alexandru.resiga] improve search heuristic
+      .firstOrNull { it.isTopLevel } // todo[alexandru.resiga] from 1.8.1 there will be only one declaration with that name
   }
 }
 
