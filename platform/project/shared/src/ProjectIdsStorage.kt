a999645a0af85865ca041ac870f6a13bb499597e
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-05-10 18:38:19 +0200

    [platform] IJPL-175343: Use UserData for keeping project id, so it won't be lost when project is unregistered
    Some services like ProjectEntitiesStorage tries to access Project.projectId() during project disposing
    
    GitOrigin-RevId: e70d95981268ec67a63da4cf2ef6ba1559912518

--- platform/project/shared/src/ProjectIdsStorage.kt
+++ platform/project/shared/src/ProjectIdsStorage.kt
@@ -4,9 +4,11 @@ package com.intellij.platform.project
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
 import com.intellij.util.AwaitCancellationAndInvoke
 import com.intellij.util.awaitCancellationAndInvoke
 import kotlinx.coroutines.CoroutineScope
+import org.jetbrains.annotations.ApiStatus
 import java.util.concurrent.ConcurrentHashMap
 
 /**
@@ -18,33 +20,31 @@ import java.util.concurrent.ConcurrentHashMap
 @OptIn(AwaitCancellationAndInvoke::class)
 @Service(Service.Level.APP)
 internal class ProjectIdsStorage(cs: CoroutineScope) {
-  private val projectToIds = ConcurrentHashMap<Project, ProjectId>()
   private val idsToProject = ConcurrentHashMap<ProjectId, Project>()
 
   init {
     cs.awaitCancellationAndInvoke {
-      projectToIds.clear()
       idsToProject.clear()
     }
   }
 
   fun registerProject(project: Project): ProjectId {
     val projectId = ProjectId.create()
-    projectToIds[project] = projectId
+    project.putUserData(PROJECT_ID, projectId)
     idsToProject[projectId] = project
 
     return projectId
   }
 
   fun unregisterProject(project: Project) {
-    val currentId = projectToIds.remove(project)
+    val currentId = project.getUserData(PROJECT_ID)
     if (currentId != null) {
       idsToProject.remove(currentId)
     }
   }
 
   fun getProjectId(project: Project): ProjectId? {
-    return projectToIds[project]
+    return project.getUserData(PROJECT_ID)
   }
 
   fun findProject(projectId: ProjectId): Project? {
@@ -55,12 +55,17 @@ internal class ProjectIdsStorage(cs: CoroutineScope) {
    * This function is needed for remote dev, so client may set backend's project id to the local one.
    */
   fun setProjectId(project: Project, newProjectId: ProjectId) {
-    val oldId = projectToIds.put(project, newProjectId)
+    val oldId = project.getUserData(PROJECT_ID)
     idsToProject.remove(oldId)
+
+    project.putUserData(PROJECT_ID, newProjectId)
     idsToProject[newProjectId] = project
   }
 
   companion object {
+    @ApiStatus.Internal
+    private val PROJECT_ID: Key<ProjectId> = Key.create<ProjectId>("ProjectImpl.PROJECT_ID")
+
     @JvmStatic
     fun getInstance(): ProjectIdsStorage = service<ProjectIdsStorage>()
   }
