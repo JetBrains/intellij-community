3828d74623eee5efe6793c9fbc8286ccd2f2f9bf
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-05-12 19:47:51 +0200

    [plugins][rd] IJPL-184043 PoC RD plugin sync
    [plugins][rd] IJPL-184043 fix build config for split plugin modules
    
    [plugins][rd] IJPL-184043 BackendPluginModelApi: drop fixme about cancellation
    
    it is supported natively by rd protocol
    
    [plugins][rd] IJPL-184043 fix plugin sync progress reporting
    
    [plugins][rd] IJPL-184043 fix rd plugin modules project structure
    
    [plugins][rd] IJPL-184043 rd plugin sync PoC
    
    enabled by registry flag `plugins.rd.plugin.sync.check`
    
    [plugins][rd] IJPL-184043 move debug actions to ui package
    
    [plugins][rd] IJPL-184043 convert DetectedPluginsPanel to Kotlin
    
    Rename .java to .kt
    
    IJPL-184043
    
    [plugins][rd] IJPL-184043 mark DetectedPluginsPanel as internal
    
    [plugins][rd] IJPL-184043 show plugin set diff: mark extensions private
    
    [plugins][rd] IJPL-184043 add category to plugin set diff
    
    [plugins][rd] IJPL-184043 add a note about compatibility determination
    
    [plugins][rd] IJPL-184043 show plugin compatibility in plugin set diff view
    
    [plugins][rd] IJPL-184043 add PluginProductModeCompatibility
    
    [plugins][rd] IJPL-184043 diff builder: fix snapshot versions matching
    
    [plugins][rd] IJPL-184043 diff builder: fix ids in split plugin list
    
    [plugins][rd] IJPL-184043 fix package plugins.split -> plugins.shared.split
    
    [plugins][rd] IJPL-184043 rename module plugins.split -> plugins.shared.split
    
    [plugins][rd] IJPL-184043 drop unused plugins.common module
    
    [plugins][rd] IJPL-184043 diff builder: use buildnumber comparison for versions
    
    [plugins][rd] IJPL-184043 diff builder: adjust split plugins list
    
    [plugins][rd] IJPL-184043 diff builder: filter out implementation detail plugins
    
    [plugins][rd] IJPL-184043 backend rpc: pass isImplementationDetail flag
    
    [plugins][rd] IJPL-184043 plugin set diff: add custom handling of split plugins
    
    [plugins][rd] IJPL-184043 PluginSetDiff: show plugin names
    
    [plugins][rd] IJPL-184043 PluginSetDiff: add matches too
    
    [plugins][rd] IJPL-184043 PluginSetDiff + debug "Show Plugin Set Diff" action
    
    [plugins][rd] IJPL-184043 LoadedPluginWithModules: id -> pluginId
    
    [plugins][rd] IJPL-184043 introduce PluginSetSnapshot and impls for client and backend
    
    [plugins] rename intellij.platform.plugins module to intellij.platform.plugins.common
    
    [plugins][rd] IJPL-184043 introduce BackendPluginsQuery to efficiently fetch and process backend plugins data
    
    [plugins][rd] IJPL-184043 BackendPluginModelApi: introduce plugin descriptor references
    
    They are needed to disambiguate plugins with the same id
    
    [plugins][rd] IJPL-184043 set up base RPC for client to backend plugin communication
    
    with a sample "Dump Backend Plugins" action
    
    GitOrigin-RevId: 046e813668fc496ddffad0e8c57f05c5122830c1

---
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/DetectedPluginsPanel.kt
@@ -0,0 +1,122 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.updateSettings.impl
+
+import com.intellij.ide.plugins.PluginHeaderPanel
+import com.intellij.ide.plugins.PluginManagerCore.getPlugin
+import com.intellij.ide.plugins.newui.MyPluginModel
+import com.intellij.ide.plugins.newui.PluginDetailsPageComponent
+import com.intellij.openapi.extensions.PluginId
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.OnePixelDivider
+import com.intellij.openapi.ui.Splitter
+import com.intellij.openapi.util.Comparing
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.ui.*
+import com.intellij.ui.components.labels.LinkListener
+import com.intellij.util.ui.JBUI
+import com.intellij.util.ui.UIUtil
+import org.jetbrains.annotations.ApiStatus
+import java.awt.BorderLayout
+import javax.swing.JComponent
+import javax.swing.JTable
+
+@ApiStatus.Internal
+class DetectedPluginsPanel(project: Project?) : OrderPanel<PluginDownloader>(PluginDownloader::class.java) {
+  private val myDetailsComponent: PluginDetailsPageComponent
+  private val myHeader = PluginHeaderPanel()
+  private val mySkippedPlugins = HashSet<PluginId>()
+
+  init {
+    val pluginModel = MyPluginModel(project)
+    myDetailsComponent = PluginDetailsPageComponent(pluginModel, LinkListener { _, _ -> }, true)
+    val entryTable = getEntryTable()
+    entryTable.setTableHeader(null)
+    entryTable.setDefaultRenderer(PluginDownloader::class.java, object : ColoredTableCellRenderer() {
+      override fun customizeCellRenderer(
+        table: JTable,
+        value: Any?,
+        selected: Boolean,
+        hasFocus: Boolean,
+        row: Int,
+        column: Int
+      ) {
+        setBorder(null)
+        if (value !is PluginDownloader) {
+          return
+        }
+
+        val pluginName = value.pluginName
+        append(pluginName, SimpleTextAttributes.REGULAR_ATTRIBUTES)
+
+        val installedPlugin = getPlugin(value.id)
+
+        val oldPluginName = installedPlugin?.getName()
+        if (oldPluginName != null && !Comparing.strEqual(pluginName, oldPluginName)) {
+          append(" - $oldPluginName", SimpleTextAttributes.REGULAR_ATTRIBUTES)
+        }
+
+        val installedVersion = installedPlugin?.getVersion()
+        val availableVersion = value.pluginVersion
+        val version = if (installedVersion != null && availableVersion != null) {
+          StringUtil.join(arrayOf(installedVersion, UIUtil.rightArrow(), availableVersion), "")
+        }
+        else StringUtil.defaultIfEmpty(installedVersion, availableVersion)
+
+        if (StringUtil.isNotEmpty(version)) {
+          append(" $version", SimpleTextAttributes.GRAYED_SMALL_ATTRIBUTES)
+        }
+      }
+    })
+    entryTable.getSelectionModel().addListSelectionListener {
+      val selectedRow = entryTable.selectedRow
+      if (selectedRow != -1) {
+        val plugin = getValueAt(selectedRow)!!.descriptor
+        myHeader.setPlugin(plugin)
+        myDetailsComponent.setOnlyUpdateMode()
+        myDetailsComponent.showPluginImpl(plugin, null)
+      }
+    }
+    removeAll()
+
+    val splitter: Splitter = OnePixelSplitter(false)
+    splitter.setFirstComponent(wrapWithPane(entryTable, 1, 1, 0, true))
+    splitter.setSecondComponent(wrapWithPane(myDetailsComponent, 0, 0, 1, false))
+    splitter.setProportion(0.3f)
+
+    add(splitter, BorderLayout.CENTER)
+  }
+
+  override fun addAll(orderEntries: MutableCollection<out PluginDownloader?>) {
+    super.addAll(orderEntries)
+    TableUtil.ensureSelectionExists(entryTable)
+  }
+
+  override fun isChecked(downloader: PluginDownloader): Boolean {
+    return !mySkippedPlugins.contains(downloader.id)
+  }
+
+  override fun setChecked(downloader: PluginDownloader, checked: Boolean) {
+    val pluginId = downloader.id
+    if (checked) {
+      mySkippedPlugins.remove(pluginId)
+    }
+    else {
+      mySkippedPlugins.add(pluginId)
+    }
+  }
+
+  override fun requestFocus() {
+    entryTable.requestFocus()
+  }
+
+  companion object {
+    private fun wrapWithPane(c: JComponent, top: Int, left: Int, right: Int, scrollPane: Boolean): JComponent {
+      var c = c
+      if (scrollPane) {
+        c = ScrollPaneFactory.createScrollPane(c)
+      }
+      c.setBorder(JBUI.Borders.customLine(OnePixelDivider.BACKGROUND, top, left, 1, right))
+      return c
+    }
+  }
+}
