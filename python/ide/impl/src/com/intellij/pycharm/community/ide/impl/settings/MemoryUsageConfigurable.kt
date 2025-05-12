f25e6ca9811b5231edd0983a4046949dd5c4455c
Natalia.Murycheva <natalia.murycheva@jetbrains.com>
2025-05-12 13:59:41 +0200

    [PyCharm Cosmetics] PY-80790 Memory Settings Page: Fixed remarks from the tech writer
    GitOrigin-RevId: 21bddda75df4b0b1b642e9a163ec34f3bb04d656

--- python/ide/impl/src/com/intellij/pycharm/community/ide/impl/settings/MemoryUsageConfigurable.kt
+++ python/ide/impl/src/com/intellij/pycharm/community/ide/impl/settings/MemoryUsageConfigurable.kt
@@ -1,6 +1,5 @@
 package com.intellij.pycharm.community.ide.impl.settings
 
-import com.intellij.diagnostic.DiagnosticBundle
 import com.intellij.diagnostic.EditMemorySettingsService
 import com.intellij.diagnostic.VMOptions
 import com.intellij.openapi.components.service
@@ -9,7 +8,6 @@ import com.intellij.openapi.options.ConfigurationException
 import com.intellij.openapi.options.OptionsBundle
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.ui.DialogPanel
-import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.wm.StatusBarWidgetFactory
 import com.intellij.openapi.wm.impl.status.MemoryUsagePanel
 import com.intellij.openapi.wm.impl.status.widget.StatusBarWidgetSettings
@@ -29,7 +27,6 @@ class MemoryUsageConfigurable : BoundSearchableConfigurable(
   private val UNKNOWN_MEMORY_VALUE = -1
   private val MIN_VALUE: Int = 256
   private val MAX_VALUE: Int = 1_000_000
-  private val HEAP_INCREMENT: Int = 512
   private val option = VMOptions.MemoryKind.HEAP
 
   private val initialHeapSize: Int = getInitialHeapSize()
@@ -40,52 +37,46 @@ class MemoryUsageConfigurable : BoundSearchableConfigurable(
   private var initialMemoryWidgetValue: Boolean? = null
 
   override fun createPanel(): DialogPanel {
-    val formattedInitialHeapSize = if (initialHeapSize == UNKNOWN_MEMORY_VALUE) DiagnosticBundle.message("change.memory.unknown") else initialHeapSize
-    val suggestedHeapSize = getSuggestedValue()
-
     return panel {
-      group(PyBundle.message("settings.memory.heap.size.group")) {
-        row(PyBundle.message("settings.memory.heap.size.label")) {
-          newHeapSizeField = intTextField(IntRange(MIN_VALUE, MAX_VALUE))
-            .text(suggestedHeapSize.toString())
-            .columns(7)
-            .gap(RightGap.SMALL)
-            .focused()
-            .onChanged {
-              newHeapSize = it.text
-            }
-            .component
+      row(PyBundle.message("settings.memory.heap.size.label")) {
+        newHeapSizeField = intTextField(IntRange(MIN_VALUE, MAX_VALUE))
+          .text(initialHeapSize.toString())
+          .columns(7)
+          .gap(RightGap.SMALL)
+          .focused()
+          .onChanged {
+            newHeapSize = it.text
+          }
+          .component
 
-          text(PyBundle.message("settings.memory.heap.size.current.label", formattedInitialHeapSize))
+      }
+        .topGap(TopGap.MEDIUM)
+        .rowComment(PyBundle.message("settings.memory.heap.size.row.comment"))
 
-        }
-          .topGap(TopGap.SMALL)
-          .rowComment(PyBundle.message("settings.memory.heap.size.row.comment"))
-
-        row {
-          comment(PyBundle.message("settings.memory.heap.size.restarting.warn.label", "AllIcons.General.Warning"))
-        }
+      row {
+        comment(PyBundle.message("settings.memory.heap.size.restarting.warn.label", "AllIcons.General.Warning"))
       }
 
       val statusBarWidgetSettings = StatusBarWidgetSettings.getInstance()
       val memoryWidgetFactory: StatusBarWidgetFactory? = StatusBarWidgetFactory.EP_NAME.extensionList.find { it.id == MemoryUsagePanel.WIDGET_ID }
       if (memoryWidgetFactory != null) {
         initialMemoryWidgetValue = StatusBarWidgetSettings.getInstance().isEnabled(memoryWidgetFactory)
-        group(PyBundle.message("settings.memory.indicator.group")) {
-          row {
-            memoryWidgetCheckbox = checkBox(PyBundle.message("settings.memory.indicator.checkbox"))
-              .selected(initialMemoryWidgetValue ?: false)
-              .onChanged {
-                val newMemoryWidgetValue = it.isSelected
+        row {
+          @Suppress("DialogTitleCapitalization")
+          memoryWidgetCheckbox = checkBox(PyBundle.message("settings.memory.indicator.checkbox"))
+            .selected(initialMemoryWidgetValue ?: false)
+            .onChanged {
+              val newMemoryWidgetValue = it.isSelected
 
-                statusBarWidgetSettings.setEnabled(memoryWidgetFactory, newMemoryWidgetValue)
-                ProjectManager.getInstance().openProjects.forEach { project ->
-                  project.service<StatusBarWidgetsManager>().updateWidget(memoryWidgetFactory)
-                }
+              statusBarWidgetSettings.setEnabled(memoryWidgetFactory, newMemoryWidgetValue)
+              ProjectManager.getInstance().openProjects.forEach { project ->
+                project.service<StatusBarWidgetsManager>().updateWidget(memoryWidgetFactory)
               }
-              .component
-          }.rowComment(PyBundle.message("settings.memory.indicator.row.comment"))
+            }
+            .component
         }
+          .topGap(TopGap.MEDIUM)
+          .rowComment(PyBundle.message("settings.memory.indicator.row.comment"))
       }
     }
   }
@@ -131,17 +122,6 @@ class MemoryUsageConfigurable : BoundSearchableConfigurable(
     if (isInDebugMode()) return UNKNOWN_MEMORY_VALUE
 
     return VMOptions.readOption(option, true)
-  }
-
-  private fun getSuggestedValue(): Int {
-    val maxSuggestedHeapSizeFromRegistry = Registry.intValue("max.suggested.heap.size")
-
-    return if (initialHeapSize > 0) {
-      initialHeapSize + HEAP_INCREMENT
-    }
-    else {
-      maxSuggestedHeapSizeFromRegistry
-    }
   }
 
   private fun isInDebugMode(): Boolean {
