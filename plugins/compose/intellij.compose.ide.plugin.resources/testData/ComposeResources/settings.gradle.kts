42f2087aa44de6fa62a58150c22091652a98b45e
Alexandru Resiga <alexandru.resiga@jetbrains.com>
2025-05-12 22:09:16 +0200

    KMT-497 [compose resources, tests] add test project
    GitOrigin-RevId: ddc17bc9ffc418c2e582c81bbcdfb3dc35415e05

---
+++ plugins/compose/intellij.compose.ide.plugin.resources/testData/ComposeResources/settings.gradle.kts
@@ -0,0 +1,32 @@
+rootProject.name = "ComposeResources"
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+include(":composeApp")
