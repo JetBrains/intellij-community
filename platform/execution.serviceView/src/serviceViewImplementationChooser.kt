9d50c83e31ebcdb0885bd3c83652f5fc892282f4
Nikita Katkov <nikita.katkov@jetbrains.com>
2025-05-13 14:09:14 +0200

    [services view] IJPL-186932 Adjust services mode selector
    GitOrigin-RevId: f0c1ff463c66879cffeffa9ad93aaa9ccf4688ec

--- platform/execution.serviceView/src/serviceViewImplementationChooser.kt
+++ platform/execution.serviceView/src/serviceViewImplementationChooser.kt
@@ -1,6 +1,9 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.execution.serviceView
 
+import com.intellij.frontend.FrontendApplicationInfo
+import com.intellij.frontend.FrontendType
+import com.intellij.idea.AppMode
 import com.intellij.openapi.diagnostic.fileLogger
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.util.PlatformUtils
@@ -10,24 +13,34 @@ import org.jetbrains.annotations.ApiStatus
 // FIXME: When we consider client services stable enough, all usages of the method must be removed,
 //        and the corresponding classes must be moved to frontend modules of their plugins
 fun shouldEnableServicesViewInCurrentEnvironment(): Boolean {
-  return when {
-    isClient() && isFrontendServiceViewEnabled() -> true
-    !isClient() && isMonolithServiceViewEnabled() -> true
-    else -> {
-      fileLogger().debug("Services implementation is disabled in current environment. Is client: ${isClient()}")
-      false
-    }
+  val isServicesEnabled = when {
+    isFrontend() && isNewFrontendServiceViewEnabled() -> true
+    isBackend() && isOldMonolithServiceViewEnabled() -> true
+    isMonolith() -> true
+    else -> false
   }
+  fileLogger().debug("Services implementation is ${if (isServicesEnabled) "enabled" else "disabled"} in current environment. " +
+                     "Is frontend: ${isFrontend()}, is monolith: ${isMonolith()}, is backend: ${isBackend()}.")
+  return isServicesEnabled
 }
 
-private fun isClient(): Boolean {
+private fun isFrontend(): Boolean {
   return PlatformUtils.isJetBrainsClient()
 }
 
-private fun isFrontendServiceViewEnabled(): Boolean {
+private fun isMonolith(): Boolean {
+  // returns true in split mode backend 0_o
+  return FrontendApplicationInfo.getFrontendType() is FrontendType.Monolith && !isBackend() && !isFrontend()
+}
+
+private fun isBackend(): Boolean {
+  return AppMode.isRemoteDevHost()
+}
+
+private fun isNewFrontendServiceViewEnabled(): Boolean {
   return Registry.`is`("services.view.split.enabled")
 }
 
-private fun isMonolithServiceViewEnabled(): Boolean {
+private fun isOldMonolithServiceViewEnabled(): Boolean {
   return Registry.`is`("services.view.monolith.enabled")
 }
