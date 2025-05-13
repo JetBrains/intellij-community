f0baa09f3e12875e14eea853426e18842f286201
Ivan Semenov <ivan.semenov@jetbrains.com>
2025-05-13 17:42:36 +0200

    [vcs-log] convert file history interface to kotlin
    GitOrigin-RevId: 90efd249ed9d41a4c478efcf8424789ce59e53b7

--- platform/vcs-log/api/src/com/intellij/vcs/log/VcsLogFileHistoryProvider.kt
+++ platform/vcs-log/api/src/com/intellij/vcs/log/VcsLogFileHistoryProvider.kt
@@ -1,16 +1,12 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.vcs.log;
+package com.intellij.vcs.log
 
-import com.intellij.openapi.vcs.FilePath;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.Collection;
+import com.intellij.openapi.vcs.FilePath
+import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
-public interface VcsLogFileHistoryProvider {
-  boolean canShowFileHistory(@NotNull Collection<FilePath> path, @Nullable String revisionNumber);
+interface VcsLogFileHistoryProvider {
+  fun canShowFileHistory(paths: Collection<FilePath>, revisionNumber: String?): Boolean
 
-  void showFileHistory(@NotNull Collection<FilePath> path, @Nullable String revisionNumber);
+  fun showFileHistory(paths: Collection<FilePath>, revisionNumber: String?)
 }
