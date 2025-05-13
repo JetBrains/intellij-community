92845364b52e00b885553bbd7d6f6df6bbf34124
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-05-13 15:27:50 +0200

    [ui] WEB-72460 Correctly render chevron in summary view and properly support padding
    (cherry picked from commit 6c5e53ad99eace0c189bd00e8ed928982a0d05f5)
    
    IJ-CR-162640
    
    GitOrigin-RevId: 7a1287c20c2c6c152f8ecccc745f0ccc48ed029a

--- platform/util/ui/src/com/intellij/util/ui/html/DetailsView.kt
+++ platform/util/ui/src/com/intellij/util/ui/html/DetailsView.kt
@@ -1,57 +1,23 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.ui.html
 
-import com.intellij.icons.AllIcons
-import com.intellij.openapi.util.ScalableIcon
-import com.intellij.util.asSafely
-import com.intellij.util.ui.ExtendableHTMLViewFactory
-import org.jetbrains.annotations.ApiStatus
-import java.awt.*
-import javax.swing.Icon
+import java.awt.Graphics
+import java.awt.Rectangle
 import javax.swing.SizeRequirements
-import javax.swing.text.AttributeSet
 import javax.swing.text.Element
 import javax.swing.text.View
 import kotlin.math.max
-import kotlin.math.min
 
 internal class DetailsView(elem: Element, axis: Int) : BlockViewEx(elem, axis) {
 
-  companion object {
-    @JvmField
-    val EXPANDED: Any = "expanded"
-  }
-
-  private var expanded: Boolean = false
-
-  @ApiStatus.Internal
-  override fun setPropertiesFromAttributes() {
-    super.setPropertiesFromAttributes()
-    expanded = element.attributes.getAttribute(HTML_Tag_DETAILS)
-      ?.asSafely<AttributeSet>()?.getAttribute(EXPANDED) == true
-  }
-
-  override fun paintChild(g: Graphics, alloc: Rectangle, index: Int) {
-    val summaryView = summaryView
-    if (index > 0 && !expanded && summaryView != null) return
+  override fun paintChild(g: Graphics?, alloc: Rectangle?, index: Int) {
+    if (index > 0 && summaryView.let { it != null && !it.expanded }) return
     super.paintChild(g, alloc, index)
-    if (index == 0 && summaryView != null) {
-      val g2d = g as Graphics2D
-      val savedComposite = g2d.composite
-      g2d.composite = AlphaComposite.SrcOver // support transparency
-      val icon = chevronIcon
-      val cssMargin = summaryView.cssMargin
-      val cssPadding = summaryView.cssPadding
-      val topInset = cssMargin.top + cssPadding.top + max(g.fontMetrics.height / 2 - icon.iconHeight / 2, 0)
-      val leftInset = cssMargin.left + cssPadding.left
-      icon.paintIcon(null, g, alloc.bounds.x + alloc.bounds.width - icon.iconWidth - leftInset, alloc.bounds.y + topInset)
-      g2d.composite = savedComposite
-    }
   }
 
   override fun calculateMajorAxisRequirements(axis: Int, r: SizeRequirements?): SizeRequirements {
     val v = summaryView
-    return if (expanded || v == null)
+    return if (v == null || v.expanded)
       super.calculateMajorAxisRequirements(axis, r)
     else
       calculateSummaryRequirements(v, axis, r).apply { alignment = 0.5f }
@@ -59,7 +25,7 @@ internal class DetailsView(elem: Element, axis: Int) : BlockViewEx(elem, axis) {
 
   override fun calculateMinorAxisRequirements(axis: Int, r: SizeRequirements?): SizeRequirements {
     val v = summaryView
-    return if (expanded || v == null)
+    return if (v == null || v.expanded)
       super.calculateMinorAxisRequirements(axis, r)
     else
       calculateSummaryRequirements(v, axis, r)
@@ -67,7 +33,7 @@ internal class DetailsView(elem: Element, axis: Int) : BlockViewEx(elem, axis) {
 
   override fun layoutMajorAxis(targetSpan: Int, axis: Int, offsets: IntArray, spans: IntArray) {
     super.layoutMajorAxis(targetSpan, axis, offsets, spans)
-    if (axis == Y_AXIS && summaryView != null && spans.isNotEmpty()) {
+    if (axis == Y_AXIS && summaryView.let { it != null && !it.expanded } && spans.isNotEmpty()) {
       val lastOffset = spans[0] + offsets[0]
       for (counter in 1 until spans.size) {
         offsets[counter] = lastOffset
@@ -76,25 +42,13 @@ internal class DetailsView(elem: Element, axis: Int) : BlockViewEx(elem, axis) {
     }
   }
 
-  override fun layoutMinorAxis(targetSpan: Int, axis: Int, offsets: IntArray, spans: IntArray) {
-    super.layoutMinorAxis(targetSpan, axis, offsets, spans)
-    if (axis == X_AXIS && summaryView != null && spans.isNotEmpty()) {
-      val maxSpan = targetSpan - chevronIcon.iconWidth
-      spans[0] = min(spans[0], maxSpan)
-    }
-  }
-
   private fun calculateSummaryRequirements(v: View, axis: Int, r: SizeRequirements?): SizeRequirements {
-    val icon = chevronIcon
-    val baseline = if (axis == Y_AXIS) icon.iconHeight else 0
-    val min = max(v.getMinimumSpan(axis).toInt(), baseline)
-    val pref = max(v.getPreferredSpan(axis).toInt(), baseline)
-    val max = max(max(v.getMaximumSpan(axis).toInt(),  if (axis == Y_AXIS) 0 else Int.Companion.MAX_VALUE), baseline)
-
-    val delta = if (axis == X_AXIS) icon.iconWidth else 0
+    val min = v.getMinimumSpan(axis).toInt()
+    val pref = v.getPreferredSpan(axis).toInt()
+    val max = max(v.getMaximumSpan(axis).toInt(), if (axis == Y_AXIS) 0 else Int.Companion.MAX_VALUE)
     return (r ?: SizeRequirements().apply { alignment = 0.5f }).apply {
-      preferred = pref + delta
-      minimum = min + delta
+      preferred = pref
+      minimum = min
       maximum = max
     }
   }
@@ -112,24 +66,10 @@ internal class DetailsView(elem: Element, axis: Int) : BlockViewEx(elem, axis) {
     }
   }
 
-  private val summaryView: View?
+  private val summaryView: SummaryView?
     get() =
       if (viewCount > 0)
-        getView(0).takeIf { it.element.name == "summary" }
+        getView(0) as? SummaryView
       else null
-
-  private val chevronIcon: Icon
-    get() {
-      val icon = if (expanded)
-        AllIcons.General.ChevronUp
-      else
-        AllIcons.General.ChevronDown
-      val scaleFactor = container.asSafely<ExtendableHTMLViewFactory.ScaledHtmlJEditorPane>()?.contentsScaleFactor ?: 1f
-      return if (icon is ScalableIcon && scaleFactor != 1f)
-        icon.scale(scaleFactor)
-      else
-        icon
-    }
-
 
 }
