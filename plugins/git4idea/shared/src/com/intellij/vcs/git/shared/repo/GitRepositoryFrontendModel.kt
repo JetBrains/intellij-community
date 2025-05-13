a7c315ee103b6a8f7a4db254900d19e2fef5c13b
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2025-05-13 10:50:34 +0200

    [git] IJPL-173922 Make GitRepositoryFrontendModel comparable
    GitOrigin-RevId: 65ffc9700aa53283437c43dad8500422170745e7

--- plugins/git4idea/shared/src/com/intellij/vcs/git/shared/repo/GitRepositoryFrontendModel.kt
+++ plugins/git4idea/shared/src/com/intellij/vcs/git/shared/repo/GitRepositoryFrontendModel.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.git.shared.repo
 
+import com.intellij.openapi.util.text.NaturalComparator
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.vcs.impl.shared.rpc.RepositoryId
 import com.intellij.vcs.git.shared.ref.GitFavoriteRefs
@@ -8,10 +9,13 @@ import org.jetbrains.annotations.ApiStatus
 
 // This class is temporarily moved to the shared module until the branch widget can be fully moved to the frontend.
 @ApiStatus.Internal
-interface GitRepositoryFrontendModel {
+interface GitRepositoryFrontendModel: Comparable<GitRepositoryFrontendModel> {
   val repositoryId: RepositoryId
   val shortName: String
   val state: GitRepositoryState
   val favoriteRefs: GitFavoriteRefs
   val root: VirtualFile
+
+  override fun compareTo(other: GitRepositoryFrontendModel): Int =
+    NaturalComparator.INSTANCE.compare(shortName, other.shortName)
 }
