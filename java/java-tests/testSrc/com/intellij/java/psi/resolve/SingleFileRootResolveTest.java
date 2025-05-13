4ba9d7ecc8fa17c0aaab531b53454fdd82eda2e0
Tagir Valeev <tagir.valeev@jetbrains.com>
2025-05-13 13:39:14 +0200

    [java-psi] Fix package scope for single-file source roots
    Should fix IDEA-372529 Use scope is wrong for single-file sources
    
    GitOrigin-RevId: c6189758cd5039d09dd9a37bdac003f7b4633edb

--- java/java-tests/testSrc/com/intellij/java/psi/resolve/SingleFileRootResolveTest.java
+++ java/java-tests/testSrc/com/intellij/java/psi/resolve/SingleFileRootResolveTest.java
@@ -6,9 +6,10 @@ import com.intellij.openapi.roots.ContentEntry;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.PackageIndex;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiClass;
+import com.intellij.psi.*;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.SearchScope;
+import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.testFramework.LightProjectDescriptor;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 import org.jetbrains.annotations.NotNull;
@@ -16,6 +17,7 @@ import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
 
 public final class SingleFileRootResolveTest extends LightJavaCodeInsightFixtureTestCase {
   private final @NotNull LightProjectDescriptor MY_DESCRIPTOR = new LightProjectDescriptor() {
@@ -23,14 +25,22 @@ public final class SingleFileRootResolveTest extends LightJavaCodeInsightFixture
     protected void configureModule(@NotNull Module module, @NotNull ModifiableRootModel model, @NotNull ContentEntry contentEntry) {
       try {
         VirtualFile root = contentEntry.getFile().getParent();
+        VirtualFile oldRoot = root.findChild("singleFile");
+        if (oldRoot != null) {
+          oldRoot.delete(this);
+        }
         VirtualFile sfRoot = root.createChildDirectory(this, "singleFile");
         VirtualFile aFile = sfRoot.createChildData(this, "A.java");
-        aFile.setBinaryContent("package com.example;\npublic class A {}\n".getBytes(StandardCharsets.UTF_8));
+        aFile.setBinaryContent("package com.example;\npublic class A {C c;}\n".getBytes(StandardCharsets.UTF_8));
         // bFile is not registered as a root
         VirtualFile bFile = sfRoot.createChildData(this, "B.java");
         bFile.setBinaryContent("package com.example;\npublic class B {}\n".getBytes(StandardCharsets.UTF_8));
+        // cFile is a package-private class
+        VirtualFile cFile = sfRoot.createChildData(this, "C.java");
+        cFile.setBinaryContent("package com.example;\nclass C {}\n".getBytes(StandardCharsets.UTF_8));
         ContentEntry newEntry = model.addContentEntry(sfRoot);
         newEntry.addSourceFolder(aFile, false).setPackagePrefix("com.example");
+        newEntry.addSourceFolder(cFile, false).setPackagePrefix("com.example");
       }
       catch (IOException e) {
         throw new UncheckedIOException(e);
@@ -44,7 +54,7 @@ public final class SingleFileRootResolveTest extends LightJavaCodeInsightFixture
   }
 
   public void testSingleFileRootResolve() {
-    myFixture.configureByText("B.java", """
+    myFixture.configureByText("Use.java", """
       import com.example.*;
       
       class Use extends A {
@@ -60,5 +70,20 @@ public final class SingleFileRootResolveTest extends LightJavaCodeInsightFixture
     // Works for single-file roots as well
     String pkg = PackageIndex.getInstance(getProject()).getPackageNameByDirectory(aClass.getContainingFile().getVirtualFile());
     assertEquals("com.example", pkg);
+  }
+  
+  public void testPackagePrivateScope() {
+    PsiClass aClass = JavaPsiFacade.getInstance(getProject()).findClass("com.example.A", GlobalSearchScope.projectScope(getProject()));
+    assertNotNull(aClass);
+    PsiClass cClass = JavaPsiFacade.getInstance(getProject()).findClass("com.example.C", GlobalSearchScope.projectScope(getProject()));
+    assertNotNull(cClass);
+    SearchScope scope = cClass.getUseScope();
+    assertTrue(scope.contains(aClass.getContainingFile().getVirtualFile()));
+    Collection<PsiReference> uses = ReferencesSearch.search(cClass).findAll();
+    assertEquals(1, uses.size());
+    PsiElement element = uses.iterator().next().getElement();
+    PsiJavaCodeReferenceElement ref = assertInstanceOf(element, PsiJavaCodeReferenceElement.class);
+    PsiTypeElement typeElement = assertInstanceOf(ref.getParent(), PsiTypeElement.class);
+    assertInstanceOf(typeElement.getParent(), PsiField.class);
   }
 }
