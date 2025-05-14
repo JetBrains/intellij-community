70b1d57a418a5933eb0664eef52764164b3df8ad
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2025-05-14 11:36:04 +0200

    IJPL-158622 Search Everywhere for RD: add SeRecentFilesProvider
    GitOrigin-RevId: f59d3a380e6b6ecfde21220bd9f26e66ad8385f2

---
+++ platform/searchEverywhere/backend/src/providers/recentFiles/SeRecentFilesProvider.kt
@@ -0,0 +1,43 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.searchEverywhere.backend.providers.recentFiles
+
+import com.intellij.ide.util.gotoByName.FileTypeRef
+import com.intellij.openapi.util.Disposer
+import com.intellij.platform.searchEverywhere.*
+import com.intellij.platform.searchEverywhere.backend.providers.target.SeTargetsProviderDelegate
+import com.intellij.platform.searchEverywhere.providers.SeAsyncWeightedContributorWrapper
+import com.intellij.platform.searchEverywhere.providers.target.SeTypeVisibilityStatePresentation
+import org.jetbrains.annotations.ApiStatus.Internal
+import org.jetbrains.annotations.Nls
+
+@Internal
+class SeRecentFilesProvider(private val contributorWrapper: SeAsyncWeightedContributorWrapper<Any>) : SeItemsProvider,
+                                                                                                      SeSearchScopesProvider,
+                                                                                                      SeTypeVisibilityStateProvider {
+  override val id: String get() = ID
+  override val displayName: @Nls String
+    get() = contributorWrapper.contributor.fullGroupName
+
+  private val targetsProviderDelegate = SeTargetsProviderDelegate(contributorWrapper)
+
+  override suspend fun collectItems(params: SeParams, collector: SeItemsProvider.Collector) {
+    targetsProviderDelegate.collectItems<FileTypeRef>(params, collector)
+  }
+
+  override suspend fun itemSelected(item: SeItem, modifiers: Int, searchText: String): Boolean {
+    return targetsProviderDelegate.itemSelected(item, modifiers, searchText)
+  }
+
+  override fun dispose() {
+    Disposer.dispose(contributorWrapper)
+  }
+
+  override suspend fun getSearchScopesInfo(): SeSearchScopesInfo? = targetsProviderDelegate.getSearchScopesInfo()
+
+  override suspend fun getTypeVisibilityStates(): List<SeTypeVisibilityStatePresentation> =
+    targetsProviderDelegate.getTypeVisibilityStates<FileTypeRef>()
+
+  companion object {
+    const val ID: String = "RecentFilesSEContributor"
+  }
+}
---
+++ platform/searchEverywhere/backend/src/providers/recentFiles/SeRecentFilesProviderFactory.kt
@@ -0,0 +1,30 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.searchEverywhere.backend.providers.recentFiles
+
+import com.intellij.ide.actions.searcheverywhere.RecentFilesSEContributor
+import com.intellij.ide.actions.searcheverywhere.WeightedSearchEverywhereContributor
+import com.intellij.openapi.actionSystem.ActionUiKind
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.application.readAction
+import com.intellij.openapi.project.Project
+import com.intellij.platform.searchEverywhere.SeItemsProvider
+import com.intellij.platform.searchEverywhere.SeItemsProviderFactory
+import com.intellij.platform.searchEverywhere.providers.SeAsyncWeightedContributorWrapper
+import org.jetbrains.annotations.ApiStatus.Internal
+
+@Internal
+class SeRecentFilesProviderFactory : SeItemsProviderFactory {
+  override val id: String
+    get() = SeRecentFilesProvider.ID
+
+  override suspend fun getItemsProvider(project: Project?, dataContext: DataContext): SeItemsProvider {
+    val legacyContributor = readAction {
+      val actionEvent = AnActionEvent.createEvent(dataContext, null, "", ActionUiKind.NONE, null)
+      @Suppress("UNCHECKED_CAST")
+      RecentFilesSEContributor.Factory().createContributor(actionEvent) as WeightedSearchEverywhereContributor<Any>
+    }
+
+    return SeRecentFilesProvider(SeAsyncWeightedContributorWrapper(legacyContributor))
+  }
+}
