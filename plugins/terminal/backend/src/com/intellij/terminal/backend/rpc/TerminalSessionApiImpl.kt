33332c8a8e86da188fb8f6f928b7dd1a51faa960
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-05-14 18:39:32 +0300

    [terminal] IJPL-185844 Fix leak of the input channel after closing the terminal tab
    Overriding `SendChannel` was not a good idea, as it requires proper handling of close handlers. Implementing it on our own is very error-prone.
    So, use an alternative approach: use per-client service to store the instances of separate `SendChannel` values we send to the RPC.
    
    GitOrigin-RevId: 985636a0f894044b29ae8bb0b85bb8800637b81a

--- plugins/terminal/backend/src/com/intellij/terminal/backend/rpc/TerminalSessionApiImpl.kt
+++ plugins/terminal/backend/src/com/intellij/terminal/backend/rpc/TerminalSessionApiImpl.kt
@@ -11,11 +11,10 @@ import org.jetbrains.plugins.terminal.block.reworked.session.rpc.TerminalSession
 
 internal class TerminalSessionApiImpl : TerminalSessionApi {
   override suspend fun getInputChannel(sessionId: TerminalSessionId): SendChannel<TerminalInputEvent> {
-    // RPC logic closes the returned channel once the client disconnects,
-    // so it may interrupt the reading of the original channel.
-    // Wrap the channel to not allow the RPC logic to close the original channel.
-    val channel = getSession(sessionId).getInputChannel()
-    return NonClosableSendChannel(channel)
+    val channelsManager = TerminalInputChannelsManager.getInstanceOrNull()
+    // Get the channel from the manager if it is available, otherwise get it directly from the session.
+    return channelsManager?.getInputChannel(sessionId)
+           ?: getSession(sessionId).getInputChannel()
   }
 
   override suspend fun getOutputFlow(sessionId: TerminalSessionId): Flow<List<TerminalOutputEvent>> {
