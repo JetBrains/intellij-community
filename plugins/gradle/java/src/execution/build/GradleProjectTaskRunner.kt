2319e7ac3ddd127ea30570f67733ad290033b68a
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2025-05-15 18:35:27 +0200

    [Gradle|Exec] cleanup: linearize GradleProjectTaskRunner running function
    ### Issues
      * IDEA-359288 Cannot run Gradle compound run configurations because of fighting due to multiple daemons
    
    GitOrigin-RevId: 7a66fd2bd8ae14dc520868c8ceec9237217fd4ee

--- plugins/gradle/java/src/execution/build/GradleProjectTaskRunner.kt
+++ plugins/gradle/java/src/execution/build/GradleProjectTaskRunner.kt
@@ -10,140 +10,131 @@ import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.scratch.JavaScratchConfiguration
 import com.intellij.openapi.compiler.CompilerPaths
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.externalSystem.model.execution.ExternalSystemTaskExecutionSettings
 import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunConfiguration.PROGRESS_LISTENER_KEY
-import com.intellij.openapi.externalSystem.service.execution.ProgressExecutionMode.IN_BACKGROUND_ASYNC
-import com.intellij.openapi.externalSystem.task.TaskCallback
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.externalSystem.util.ExternalSystemUtil
+import com.intellij.openapi.externalSystem.util.task.TaskExecutionSpec
+import com.intellij.openapi.externalSystem.util.task.TaskExecutionUtil
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.UserDataHolder
 import com.intellij.openapi.util.UserDataHolderBase
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.task.*
-import com.intellij.task.TaskRunnerResults.FAILURE
 import com.intellij.task.TaskRunnerResults.SUCCESS
 import com.intellij.util.text.nullize
-import org.jetbrains.concurrency.AsyncPromise
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.future.asCompletableFuture
+import kotlinx.coroutines.launch
 import org.jetbrains.concurrency.Promise
+import org.jetbrains.concurrency.asPromise
+import org.jetbrains.plugins.gradle.GradleJavaCoroutineScope.gradleCoroutineScope
 import org.jetbrains.plugins.gradle.service.execution.loadHotswapDetectionInitScript
 import org.jetbrains.plugins.gradle.service.task.GradleTaskManager.*
 import org.jetbrains.plugins.gradle.settings.GradleProjectSettings
+import org.jetbrains.plugins.gradle.util.GradleBundle
 import org.jetbrains.plugins.gradle.util.GradleConstants.SYSTEM_ID
 import java.io.File
 import java.io.IOException
-import java.util.concurrent.atomic.AtomicInteger
+import kotlin.io.path.Path
 
-/**
- * @author Vladislav.Soroka
- */
 class GradleProjectTaskRunner : ProjectTaskRunner() {
 
   override fun isFileGeneratedEventsSupported(): Boolean = true
 
   override fun run(project: Project, context: ProjectTaskContext, vararg tasks: ProjectTask): Promise<Result> {
-    val resultPromise = AsyncPromise<Result>()
-    val executionSettingsBuilder = TasksExecutionSettingsBuilder(project, *tasks)
-    val rootPaths = executionSettingsBuilder.rootPaths
-    if (rootPaths.isEmpty()) {
-      LOG.warn("Nothing will be run for: " + tasks.contentToString())
-      resultPromise.setResult(SUCCESS)
-      return resultPromise
-    }
-
-    val successCounter = AtomicInteger()
-    val errorCounter = AtomicInteger()
-
-    val outputPathsFile = createTempOutputPathsFileIfNeeded(context)
-    val taskCallback = object : TaskCallback {
-
-      override fun onSuccess() = handle(true)
-      override fun onFailure() = handle(false)
-
-      fun handle(success: Boolean) {
-        val successes = if (success) successCounter.incrementAndGet() else successCounter.get()
-        val errors = if (success) errorCounter.get() else errorCounter.incrementAndGet()
-        if (successes + errors == rootPaths.size) {
-          if (!project.isDisposed()) {
-            try {
-              if (GradleImprovedHotswapDetection.isEnabled()) {
-                GradleImprovedHotswapDetection.processInitScriptOutput(context, outputPathsFile)
-              }
-              else {
-                val affectedRoots = getAffectedOutputRoots(outputPathsFile, context, executionSettingsBuilder)
-                if (!affectedRoots.isEmpty()) {
-                  if (context.isCollectionOfGeneratedFilesEnabled) {
-                    context.addDirtyOutputPathsProvider { affectedRoots }
-                  }
-                  // refresh on output roots is required in order for the order enumerator to see all roots via VFS
-                  // have to refresh in case of errors too, because run configuration may be set to ignore errors
-                  CompilerUtil.refreshOutputRoots(affectedRoots)
-                }
-              }
-            }
-            finally {
-              if (outputPathsFile != null) {
-                FileUtil.delete(outputPathsFile)
-              }
-            }
-          }
-          resultPromise.setResult(if (errors > 0) FAILURE else SUCCESS)
-        }
-        else {
-          if (successes + errors > rootPaths.size) {
-            LOG.error("Unexpected callback!")
-          }
-        }
-      }
-    }
-
-    for (rootProjectPath in rootPaths) {
-      if (!executionSettingsBuilder.containsTasksToExecuteFor(rootProjectPath)) {
-        LOG.warn("Nothing will be run for: " + tasks.contentToString() + " at '" + rootProjectPath + "'")
-        taskCallback.onSuccess()
-        continue
-      }
-
-      if (outputPathsFile != null && context.isCollectionOfGeneratedFilesEnabled) {
-        executionSettingsBuilder.addInitScripts(rootProjectPath, loadHotswapDetectionInitScript(
-          GradleImprovedHotswapDetection.isEnabled(),
-          FileUtil.toCanonicalPath(outputPathsFile.absolutePath)
-        ))
-      }
-
-      val settings = executionSettingsBuilder.build(rootProjectPath)
-
-      val userData = UserDataHolderBase().also {
-        it.putUserData(PROGRESS_LISTENER_KEY, BuildViewManager::class.java)
-        it.putUserData(VERSION_SPECIFIC_SCRIPTS_KEY, executionSettingsBuilder.getVersionedInitScripts(rootProjectPath))
-        it.putUserData(INIT_SCRIPT_KEY, executionSettingsBuilder.getInitScript(rootProjectPath))
-        it.putUserData(INIT_SCRIPT_PREFIX_KEY, BUILD_INIT_SCRIPT_NAME)
-      }
-
-      ExternalSystemUtil.runTask(settings, DefaultRunExecutor.EXECUTOR_ID, project, SYSTEM_ID,
-                                 taskCallback, IN_BACKGROUND_ASYNC, false, userData)
-    }
-    return resultPromise
+    return project.gradleCoroutineScope.async<Result> {
+      run(project, context, tasks.asList())
+      SUCCESS
+    }.asCompletableFuture().asPromise()
   }
 
-  private fun createTempOutputPathsFileIfNeeded(context: ProjectTaskContext): File? {
-    if (context.isCollectionOfGeneratedFilesEnabled) {
-      try {
-        return FileUtil.createTempFile("output", ".paths", true)
+  private suspend fun run(project: Project, context: ProjectTaskContext, tasks: List<ProjectTask>) {
+    val taskOutputFile = if (context.isCollectionOfGeneratedFilesEnabled) createTaskOutputFile() else null
+    try {
+      val hotswapDetectionInitScript = taskOutputFile?.let {
+        loadHotswapDetectionInitScript(
+          GradleImprovedHotswapDetection.isEnabled(),
+          FileUtil.toCanonicalPath(taskOutputFile.absolutePath)
+        )
       }
-      catch (e: IOException) {
-        LOG.warn("Can not create temp file to collect Gradle tasks output paths", e)
+
+      val settings = TasksExecutionSettingsBuilder(tasks)
+
+      if (settings.rootPaths.isEmpty()) {
+        LOG.warn("Nothing will be run for $tasks")
+        return
       }
+
+      coroutineScope {
+        for (rootProjectPath in settings.rootPaths) {
+          val tasksToExecute = settings.getTasksToExecute(rootProjectPath)
+
+          if (tasksToExecute.isEmpty()) {
+            LOG.warn("Nothing will be run for $tasks at '$rootProjectPath'")
+            continue
+          }
+
+          if (hotswapDetectionInitScript != null) {
+            settings.addInitScripts(rootProjectPath, hotswapDetectionInitScript)
+          }
+
+          launch {
+            TaskExecutionUtil.runTask(
+              TaskExecutionSpec.create()
+                .withProject(project)
+                .withSystemId(SYSTEM_ID)
+                .withSettings(ExternalSystemTaskExecutionSettings().also {
+                  it.executionName = GradleBundle.message("gradle.execution.name.build.project.", Path(rootProjectPath).fileName)
+                  it.externalSystemIdString = SYSTEM_ID.id
+                  it.externalProjectPath = rootProjectPath
+                  it.taskNames = tasksToExecute
+                })
+                .withUserData(UserDataHolderBase().also {
+                  it.putUserData(PROGRESS_LISTENER_KEY, BuildViewManager::class.java)
+                  it.putUserData(VERSION_SPECIFIC_SCRIPTS_KEY, settings.getVersionedInitScripts(rootProjectPath))
+                  it.putUserData(INIT_SCRIPT_KEY, settings.getInitScript(rootProjectPath))
+                  it.putUserData(INIT_SCRIPT_PREFIX_KEY, BUILD_INIT_SCRIPT_NAME)
+                })
+            )
+          }
+        }
+      }
+
+      if (GradleImprovedHotswapDetection.isEnabled()) {
+        GradleImprovedHotswapDetection.processInitScriptOutput(context, taskOutputFile)
+      }
+      else {
+        val affectedRoots = getAffectedOutputRoots(settings, taskOutputFile)
+        if (!affectedRoots.isEmpty()) {
+          context.addDirtyOutputPathsProvider { affectedRoots }
+        }
+        // refresh on output roots is required in order for the order enumerator to see all roots via VFS
+        // have to refresh in case of errors too, because run configuration may be set to ignore errors
+        CompilerUtil.refreshOutputRoots(affectedRoots)
+      }
+    }
+    finally {
+      if (taskOutputFile != null) {
+        FileUtil.delete(taskOutputFile)
+      }
+    }
+  }
+
+  private fun createTaskOutputFile(): File? {
+    try {
+      return FileUtil.createTempFile("output", ".paths", true)
+    }
+    catch (e: IOException) {
+      LOG.warn("Can not create temp file to collect Gradle tasks output paths", e)
     }
     return null
   }
 
-  private fun getAffectedOutputRoots(
-    outputPathsFile: File?,
-    context: ProjectTaskContext,
-    executionSettingsBuilder: TasksExecutionSettingsBuilder,
-  ): Set<String> {
-    if (outputPathsFile != null && context.isCollectionOfGeneratedFilesEnabled) {
+  private fun getAffectedOutputRoots(settings: TasksExecutionSettingsBuilder, outputPathsFile: File?): Set<String> {
+    if (outputPathsFile != null) {
       try {
         return FileUtil.loadLines(outputPathsFile)
           .mapNotNullTo(LinkedHashSet()) { it.trim().nullize() }
@@ -152,7 +143,7 @@ class GradleProjectTaskRunner : ProjectTaskRunner() {
         LOG.warn("Can not load temp file with collected Gradle tasks output paths", e)
       }
     }
-    return CompilerPaths.getOutputPaths(executionSettingsBuilder.affectedModules.toTypedArray()).toSet()
+    return CompilerPaths.getOutputPaths(settings.affectedModules.toTypedArray()).toSet()
   }
 
   override fun canRun(project: Project, projectTask: ProjectTask, context: ProjectTaskContext?): Boolean {
@@ -195,25 +186,45 @@ class GradleProjectTaskRunner : ProjectTaskRunner() {
   }
 
   override fun createExecutionEnvironment(project: Project, vararg tasks: ProjectTask): ExecutionEnvironment? {
-    var environment = super.createExecutionEnvironment(project, *tasks)
-    if (environment != null) {
-      return environment
-    }
-    val executionSettingsBuilder = TasksExecutionSettingsBuilder(project, *tasks)
-    val rootProjectPath = executionSettingsBuilder.rootPaths.firstOrNull() ?: return null
-    val settings = executionSettingsBuilder.build(rootProjectPath)
+    return super.createExecutionEnvironment(project, *tasks)
+           ?: createExecutionEnvironmentImpl(project, *tasks)
+  }
 
-    environment = ExternalSystemUtil.createExecutionEnvironment(project, SYSTEM_ID, settings, DefaultRunExecutor.EXECUTOR_ID)
-    if (environment == null) {
+  // Needed for Fleet FLEET-T-4855
+  private fun createExecutionEnvironmentImpl(project: Project, vararg tasks: ProjectTask): ExecutionEnvironment? {
+    val settingsBuilder = TasksExecutionSettingsBuilder(tasks.asList())
+
+    val rootProjectPath = settingsBuilder.rootPaths.firstOrNull() ?: run {
+      LOG.warn("Execution environment is null for $tasks")
+      return null
+    }
+
+    val tasksToExecute = settingsBuilder.getTasksToExecute(rootProjectPath)
+
+    if (tasksToExecute.isEmpty()) {
+      LOG.warn("Execution environment is null for $tasks at '$rootProjectPath'")
+      return null
+    }
+
+    val settings = ExternalSystemTaskExecutionSettings().also {
+      it.executionName = GradleBundle.message("gradle.execution.name.build.project.", Path(rootProjectPath).fileName)
+      it.externalSystemIdString = SYSTEM_ID.id
+      it.externalProjectPath = rootProjectPath
+      it.taskNames = tasksToExecute
+    }
+
+    val environment = ExternalSystemUtil.createExecutionEnvironment(project, SYSTEM_ID, settings, DefaultRunExecutor.EXECUTOR_ID) ?: run {
       LOG.warn("Execution environment for $SYSTEM_ID is null")
       return null
     }
 
     (environment.runnerAndConfigurationSettings!!.configuration as UserDataHolder).also {
-      it.putUserData(VERSION_SPECIFIC_SCRIPTS_KEY, executionSettingsBuilder.getVersionedInitScripts(rootProjectPath))
-      it.putUserData(INIT_SCRIPT_KEY, executionSettingsBuilder.getInitScript(rootProjectPath))
+      it.putUserData(PROGRESS_LISTENER_KEY, BuildViewManager::class.java)
+      it.putUserData(VERSION_SPECIFIC_SCRIPTS_KEY, settingsBuilder.getVersionedInitScripts(rootProjectPath))
+      it.putUserData(INIT_SCRIPT_KEY, settingsBuilder.getInitScript(rootProjectPath))
       it.putUserData(INIT_SCRIPT_PREFIX_KEY, BUILD_INIT_SCRIPT_NAME)
     }
+
     return environment
   }
 
