4db2ddeaf9d099a1d6b758325438443781b63ea4
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-05-15 14:49:50 +0300

    IJPL-188019 Document equals behavior for deferred icons
    This is something not obvious at all and can cause very subtle
    issues, like the one in the linked ticket.
    
    GitOrigin-RevId: 255fc18df762dfa443589c5d7727768fd49f7735

--- platform/core-ui/src/ui/IconDeferrer.kt
+++ platform/core-ui/src/ui/IconDeferrer.kt
@@ -14,6 +14,11 @@ abstract class IconDeferrer {
   /**
    * @param param Unique key that WILL BE USED to cache the icon instance.
    * Prefer passing unique objects over [String] or [Integer] to avoid accidental clashes with another module.
+   * Moreover, the returned icons will be considered equal if they have equal `param` values.
+   * So even if you clear the cache manually, you may still have some obsolete value stuck somewhere
+   * because some setter ignored the new value as equal to the old one.
+   * So make sure you use a new key after you clear the cache.
+   * For example, include a sequential counter and increment it every time you clear the cache.
    */
   abstract fun <T : Any> defer(base: Icon?, param: T, evaluator: (T) -> Icon?): Icon
 
