2b2f57d62c637ce8670a5a80a3221d20c51ae12d
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-05-15 15:55:25 +0300

    [terminal] IJPL-187899 Do not restore tabs on backend in the Reworked Terminal case
    It does not directly relate to the root cause of the mentioned issue, but let's add a protection for this case too.
    
    GitOrigin-RevId: ae31cf5e4d643823ae96f80d55991522918f5d34

--- plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalToolWindowFactory.java
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalToolWindowFactory.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.terminal;
 
+import com.intellij.idea.AppMode;
 import com.intellij.openapi.actionSystem.ActionGroup;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.application.ApplicationManager;
@@ -30,8 +31,11 @@ public final class TerminalToolWindowFactory implements ToolWindowFactory, DumbA
     ActionGroup toolWindowActions = (ActionGroup)ActionManager.getInstance().getAction("Terminal.ToolWindowActions");
     toolWindow.setAdditionalGearActions(toolWindowActions);
     if (ExperimentalUI.isNewUI() && TerminalOptionsProvider.getInstance().getTerminalEngine() == TerminalEngine.REWORKED) {
-      // Restore from backend if Reworked Terminal (Gen2) is enabled.
-      terminalToolWindowManager.restoreTabsFromBackend();
+      // Fetch the tabs from the backend and restore them if Reworked Terminal (Gen2) is enabled.
+      // If we are already on the backend, do nothing because tabs should be opened only on the frontend.
+      if (!AppMode.isRemoteDevHost()) {
+        terminalToolWindowManager.restoreTabsFromBackend();
+      }
     }
     else {
       // Do not restore tabs on the client side, they are restored on the backend and then synchronized
