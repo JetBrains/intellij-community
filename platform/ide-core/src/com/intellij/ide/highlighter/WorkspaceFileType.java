889bce9ab5cd765524ee9afdded518fa49c39a3a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-05-16 08:54:38 +0200

    don't use AllIcons
    GitOrigin-RevId: c38ad353c86fd8842c6add35a5a39b17c4bc7343

--- platform/ide-core/src/com/intellij/ide/highlighter/WorkspaceFileType.java
+++ platform/ide-core/src/com/intellij/ide/highlighter/WorkspaceFileType.java
@@ -1,16 +1,17 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.highlighter;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.ide.IdeCoreBundle;
 import com.intellij.openapi.fileTypes.InternalFileType;
-import com.intellij.openapi.vfs.CharsetToolkit;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.IconManager;
+import com.intellij.ui.PlatformIcons;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.nio.charset.StandardCharsets;
 
 public final class WorkspaceFileType implements InternalFileType {
   public static final WorkspaceFileType INSTANCE = new WorkspaceFileType();
@@ -41,7 +42,7 @@ public final class WorkspaceFileType implements InternalFileType {
 
   @Override
   public Icon getIcon() {
-    return AllIcons.Nodes.IdeaModule;
+    return IconManager.getInstance().getPlatformIcon(PlatformIcons.IdeaModule);
   }
 
   @Override
@@ -50,7 +51,7 @@ public final class WorkspaceFileType implements InternalFileType {
   }
 
   @Override
-  public String getCharset(@NotNull VirtualFile file, final byte @NotNull [] content) {
-    return CharsetToolkit.UTF8;
+  public String getCharset(@NotNull VirtualFile file, byte @NotNull [] content) {
+    return StandardCharsets.UTF_8.name();
   }
 }
