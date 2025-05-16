3038c5d36847872e3f42d5fe9acdc5d6fedcec95
ekaterina.itsenko <ekaterina.itsenko@jetbrains.com>
2025-05-17 00:48:09 +0200

    [pycharm] PY-23411 Debugger: add pillow support
    (cherry picked from commit 8b798c21866c7623ffd71f757ce833e3bb6bf763)
    
    GitOrigin-RevId: a0b9970678f21919ea6a70c2877cceb5ac650c03

--- python/helpers/pydev/_pydevd_bundle/tables/images/pydevd_matplotlib_image.py
+++ python/helpers/pydev/_pydevd_bundle/tables/images/pydevd_matplotlib_image.py
@@ -1,7 +1,25 @@
 #  Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-import matplotlib
 from _pydevd_bundle.tables.images.pydevd_image_loader import save_image_to_storage, DEFAULT_IMAGE_FORMAT
 
-def create_image(matplotlib_figure):
-    # type: (matplotlib.figure.Figure) -> str
-    return save_image_to_storage(matplotlib_figure, format=DEFAULT_IMAGE_FORMAT, save_func=lambda buffer, fmt: matplotlib_figure.savefig(buffer, format=fmt))
+
+def create_image(figure):
+    # type: (Union[matplotlib.figure.Figure | plotly.graph_objs._figure.Figure]) -> str
+    try:
+        try:
+            import matplotlib.figure
+        except ImportError:
+            matplotlib = None
+
+        try:
+            from plotly.graph_objects import Figure as PlotlyFigure
+        except ImportError:
+            PlotlyFigure = None
+
+        if matplotlib and isinstance(figure, matplotlib.figure.Figure):
+            return save_image_to_storage(figure, format=DEFAULT_IMAGE_FORMAT, save_func=lambda buffer, fmt: figure.savefig(buffer, format=fmt))
+        elif PlotlyFigure and isinstance(figure, PlotlyFigure):
+            return save_image_to_storage(figure, format=DEFAULT_IMAGE_FORMAT, save_func=lambda buffer, fmt: buffer.write(figure.to_image(format=fmt)))
+        else:
+            return "Error: Unsupported figure type."
+    except Exception as e:
+        return "Error: {}".format(e)
