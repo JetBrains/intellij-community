553138110e6571a4d5bdcde373c353f864c9aaea
Alexander Koshevoy <alexander.koshevoy@jetbrains.com>
2025-05-19 15:28:19 +0200

    [path-annotations] Kotlin tests for path annotations expected to pass both for K1 and K2
    GitOrigin-RevId: 5793d1ac214dcd0497a888d444f74f117b01edb3

--- plugins/devkit/devkit-kotlin-tests/testSrc/org/jetbrains/idea/devkit/kotlin/inspections/path/PathAnnotationInspectionKotlinTestBase.kt
+++ plugins/devkit/devkit-kotlin-tests/testSrc/org/jetbrains/idea/devkit/kotlin/inspections/path/PathAnnotationInspectionKotlinTestBase.kt
@@ -4,21 +4,16 @@ package org.jetbrains.idea.devkit.kotlin.inspections.path
 import com.intellij.testFramework.IndexingTestUtil
 import com.intellij.testFramework.LightProjectDescriptor
 import org.jetbrains.idea.devkit.inspections.PathAnnotationInspectionTestBase
-import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginMode
 import org.jetbrains.kotlin.idea.test.ConfigLibraryUtil
-import org.jetbrains.kotlin.idea.test.ExpectedPluginModeProvider
 import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
-import org.jetbrains.kotlin.idea.test.setUpWithKotlinPlugin
 
-abstract class PathAnnotationInspectionKotlinTestBase : PathAnnotationInspectionTestBase(), ExpectedPluginModeProvider {
+abstract class PathAnnotationInspectionKotlinTestBase : PathAnnotationInspectionTestBase() {
   override fun getFileExtension(): String = "kt"
 
   override fun getProjectDescriptor(): LightProjectDescriptor = KotlinWithJdkAndRuntimeLightProjectDescriptor.getInstanceFullJdk()
 
-  override val pluginMode = KotlinPluginMode.K2
-
   override fun setUp() {
-    setUpWithKotlinPlugin(testRootDisposable) { super.setUp() }
+    super.setUp()
     ConfigLibraryUtil.configureKotlinRuntime(myFixture.module)
     IndexingTestUtil.waitUntilIndexesAreReady(project)
   }
