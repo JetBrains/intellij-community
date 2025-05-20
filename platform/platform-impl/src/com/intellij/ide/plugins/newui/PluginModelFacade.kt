7be06b6f956b811d554ee13e17b7d713c2b74eee
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2025-05-20 17:54:25 +0200

    IJPL-177442 Got rid of PluginModelController.
    Since it's impossible to split plugin functionality without splitting the MyPluginModel class, we don't really need these controllers. We only need a facade to proxy MyPluginModel operations. All other cases with different implementations that don't use MyPluginModel will be moved out of this class.
    
    
    (cherry picked from commit 1b0b6cdc83be7f4c66f850095805352292c3d4aa)
    
    IJ-MR-163725
    
    GitOrigin-RevId: 31f37effaab50a9a004f5220734d1e5228f2367b

--- platform/platform-impl/src/com/intellij/ide/plugins/newui/PluginModelFacade.kt
+++ platform/platform-impl/src/com/intellij/ide/plugins/newui/PluginModelFacade.kt
@@ -1,85 +1,85 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.plugins.newui
 
-import com.intellij.ide.plugins.IdeaPluginDescriptor
+import com.intellij.ide.plugins.IdeaPluginDependency
 import com.intellij.ide.plugins.PluginEnableDisableAction
 import com.intellij.ide.plugins.PluginEnabledState
-import com.intellij.ide.plugins.PluginManagerCore.ULTIMATE_PLUGIN_ID
-import com.intellij.ide.plugins.PluginManagerCore.isDisabled
+import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.ide.plugins.PluginNode
+import com.intellij.ide.plugins.api.ReviewsPageContainer
 import com.intellij.ide.plugins.marketplace.IntellijPluginMetadata
+import com.intellij.ide.plugins.marketplace.MarketplaceRequests
 import com.intellij.ide.plugins.marketplace.PluginReviewComment
+import com.intellij.ide.plugins.marketplace.utils.MarketplaceUrls
+import com.intellij.ide.plugins.pluginRequiresUltimatePluginButItsDisabled
 import javax.swing.JComponent
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.util.text.HtmlChunk
 import com.intellij.openapi.wm.ex.ProgressIndicatorEx
 import org.jetbrains.annotations.ApiStatus
 
-/*
- * A service that executes operations on plugins, depending on the source of the plugin.
- * If it is local, the operation will be executed locally; otherwise, it will be executed on the server using an RPC call.
- */
 @ApiStatus.Internal
 class PluginModelFacade(private val pluginModel: MyPluginModel) {
-  private val localController = LocalPluginModelController(pluginModel)
+  private val marketplaceRequests = MarketplaceRequests.getInstance()
 
   fun isPluginInstallingOrUpdating(model: PluginUiModel): Boolean {
-    return getController(model).isPluginInstallingOrUpdating(model)
+    return MyPluginModel.isInstallingOrUpdate(model.pluginId)
   }
 
   fun getState(model: PluginUiModel): PluginEnabledState {
-    return getController(model).getState(model)
+    return pluginModel.getState(model.pluginId)
   }
 
   fun enable(model: PluginUiModel) {
-    getController(model).enable(listOf(model))
+    pluginModel.enable(listOf(model.getDescriptor()))
   }
 
   fun enable(models: Collection<PluginUiModel>) {
-    models.groupBy { it.source }.forEach {  getController(it.key).enable(it.value)}
+    pluginModel.enable(models.map { it.getDescriptor() })
   }
 
   fun disable(model: PluginUiModel) {
-    getController(model).disable(listOf(model))
+    pluginModel.disable(listOf(model.getDescriptor()))
   }
 
   fun disable(models: Collection<PluginUiModel>) {
-    models.groupBy { it.source }.forEach {  getController(it.key).disable(it.value)}
+    pluginModel.disable(models.map { it.getDescriptor() })
   }
 
   fun installOrUpdatePlugin(component: JComponent, model: PluginUiModel, updateDescriptor: PluginUiModel?, modalityState: ModalityState) {
-    getController(model).installOrUpdatePlugin(component, model, updateDescriptor, modalityState)
+    pluginModel.installOrUpdatePlugin(component, model, updateDescriptor, modalityState)
   }
 
   fun addUninstalled(model: PluginUiModel) {
-    getController(model).addUninstalled(model)
+    pluginModel.addUninstalled(model.getDescriptor())
   }
 
   fun getIcon(model: PluginUiModel, big: Boolean, error: Boolean, disabled: Boolean): javax.swing.Icon {
-    return getController(model).getIcon(model, big, error, disabled)
+    return pluginModel.getIcon(model.getDescriptor(), big, error, disabled)
   }
 
   fun getErrors(model: PluginUiModel): List<HtmlChunk> {
-    return getController(model).getErrors(model)
+    return pluginModel.getErrors(model.getDescriptor())
   }
 
   fun enableRequiredPlugins(model: PluginUiModel) {
-    getController(model).enableRequiredPlugins(model)
+    pluginModel.enableRequiredPlugins(model.getDescriptor())
   }
 
   fun isUninstalled(model: PluginUiModel): Boolean {
-    return getController(model).isUninstalled(model)
+    return pluginModel.isUninstalled(model.getDescriptor())
   }
 
   fun isEnabled(model: PluginUiModel): Boolean {
-    return getController(model).isEnabled(model)
+    return pluginModel.isEnabled(model.getDescriptor())
   }
 
   fun finishInstall(model: PluginUiModel, installedModel: PluginUiModel?, success: Boolean, showErrors: Boolean, restartRequired: Boolean) {
-    getController(model).finishInstall(model, installedModel, success, showErrors, restartRequired)
+    pluginModel.finishInstall(model, installedModel, success, showErrors, restartRequired)
   }
 
   fun isPluginRequiredForProject(model: PluginUiModel): Boolean {
-    return getController(model).isPluginRequiredForProject(model)
+    return pluginModel.isRequiredPluginForProject(model.pluginId)
   }
 
   fun addComponent(component: ListPluginComponent) {
@@ -90,96 +90,109 @@ class PluginModelFacade(private val pluginModel: MyPluginModel) {
     pluginModel.removeComponent(component)
   }
 
-  fun hasPluginRequiresUltimateButItsDisabled(models: Collection<PluginUiModel>): Boolean {
-    if (!isDisabled(ULTIMATE_PLUGIN_ID)) return false
-    return models.groupBy { it.source }.any { getController(it.key).hasPluginRequiresUltimateButItsDisabled(it.value) }
-  }
-
-  /** Returns true, if in descriptors list not only Ultimate plugins while we are on Core license.
-   * (Any plugin exists, for which we can change the enabled / disable state) */
   fun hasPluginForEnableDisable(models: Collection<PluginUiModel>): Boolean {
-    return models.groupBy { it.source }.any { getController(it.key).hasPluginForEnableDisable(it.value) }
+    val idMap = PluginManagerCore.buildPluginIdMap()
+    return models.any { !pluginRequiresUltimatePluginButItsDisabled(it.pluginId, idMap) }
   }
 
   fun setEnabledState(models: Collection<PluginUiModel>, action: PluginEnableDisableAction) {
-    models.groupBy { it.source }.forEach {  getController(it.key).setEnabledState(it.value, action)}
+    pluginModel.setEnabledState(models.map { it.getDescriptor() }, action)
   }
 
   fun uninstallAndUpdateUi(descriptor: PluginUiModel) {
-    return getController(descriptor).uninstallAndUpdateUi(descriptor)
+    pluginModel.uninstallAndUpdateUi(descriptor)
   }
 
   fun findInstalledPlugin(model: PluginUiModel): PluginUiModel? {
-    return getController(model).findInstalledPlugin(model)
+    return PluginManagerCore.getPlugin(model.pluginId)?.let { PluginUiModelAdapter(it) }
   }
 
   fun findPlugin(model: PluginUiModel): PluginUiModel? {
-    return getController(model).findPlugin(model)
+    return PluginManagerCore.buildPluginIdMap()[model.pluginId]?.let { PluginUiModelAdapter(it) }
   }
 
   fun getPluginManagerUrl(model: PluginUiModel): String {
-    return getController(model).getPluginManagerUrl(model)
+    return MarketplaceUrls.getPluginManagerUrl()
   }
 
   fun isDisabledInDiff(model: PluginUiModel): Boolean {
-    return getController(model).isDisabledInDiff(model)
+    return pluginModel.isDisabledInDiff(model.pluginId)
   }
 
   fun loadPluginDetails(model: PluginUiModel): PluginUiModel? {
-    return getController(model).loadPluginDetails(model)
+    return marketplaceRequests.loadPluginDetails(model)
   }
 
   fun loadAllPluginDetails(existingModel: PluginUiModel, targetModel: PluginUiModel): PluginUiModel? {
-    return getController(existingModel).loadAllPluginDetails(existingModel, targetModel)
+    if (!existingModel.suggestedFeatures.isEmpty()) {
+      targetModel.suggestedFeatures = existingModel.suggestedFeatures
+    }
+
+    val externalPluginId = existingModel.externalPluginId ?: return null
+    val metadata = marketplaceRequests.loadPluginMetadata(externalPluginId)
+    if (metadata != null) {
+      if (metadata.screenshots != null) {
+        targetModel.screenShots = metadata.screenshots
+        targetModel.externalPluginIdForScreenShots = externalPluginId
+      }
+      metadata.toPluginUiModel(targetModel)
+    }
+    loadReviews(targetModel)
+    loadDependencyNames(targetModel)
+    return targetModel
   }
 
   fun getLastCompatiblePluginUpdate(model: PluginUiModel): PluginUiModel? {
-    return getController(model).getLastCompatiblePluginUpdate(model)
+    return marketplaceRequests.getLastCompatiblePluginUpdateModel(model.pluginId)
   }
 
   fun loadPluginMetadata(source: PluginSource, pluginId: String): IntellijPluginMetadata? {
-    return getController(source).loadPluginMetadata(pluginId)
+    return marketplaceRequests.loadPluginMetadata(pluginId)
   }
 
   fun loadReviews(existingModel: PluginUiModel): PluginUiModel? {
-    return getController(existingModel).fetchReviews(existingModel)
+    val reviewComments = ReviewsPageContainer(20, 0)
+    reviewComments.addItems(loadPluginReviews(existingModel, reviewComments.getNextPage()))
+    existingModel.reviewComments = reviewComments
+    return existingModel
   }
 
   fun loadDependencyNames(targetModel: PluginUiModel): PluginUiModel? {
-    return getController(targetModel.source).fetchDependecyNames(targetModel)
+    val resultNode = targetModel.getDescriptor() as? PluginNode ?: return null
+    resultNode.dependencyNames = resultNode.dependencies.asSequence()
+      .filter { !it.isOptional }
+      .map(IdeaPluginDependency::pluginId)
+      .filter { isNotPlatformAlias(it) }
+      .map { pluginId ->
+        PluginManagerCore.findPlugin(pluginId)?.let {
+          return@map it.name
+        }
+        marketplaceRequests.getLastCompatiblePluginUpdate(pluginId)?.name ?: pluginId.idString
+      }
+      .toList()
+
+    return targetModel
   }
 
   fun loadPluginReviews(targetModel: PluginUiModel, page: Int): List<PluginReviewComment> {
-    return getController(targetModel.source).loadPluginReviews(targetModel, page)
+    return marketplaceRequests.loadPluginReviews(targetModel, page) ?: emptyList()
   }
 
   fun isLoaded(model: PluginUiModel): Boolean {
-    return getController(model).isLoaded(model)
+    return pluginModel.isLoaded(model.pluginId)
   }
 
-  private fun getController(model: PluginUiModel): PluginModelController {
-    return getController(model.source)
-  }
-
-  private fun getController(source: PluginSource): PluginModelController{
-    return when (source) {
-      PluginSource.LOCAL -> localController
-      PluginSource.REMOTE -> localController
-    }
-  }
-  //Temporary field that will allow as to change code in small parts
   fun getModel(): MyPluginModel = pluginModel
 
   companion object {
-
     @JvmStatic
     fun addProgress(model: PluginUiModel, indicator: ProgressIndicatorEx) {
       MyPluginModel.addProgress(model.getDescriptor(), indicator)
     }
+    
     @JvmStatic
     fun removeProgress(model: PluginUiModel, indicator: ProgressIndicatorEx) {
       MyPluginModel.removeProgress(model.getDescriptor(), indicator)
     }
   }
 }
-
