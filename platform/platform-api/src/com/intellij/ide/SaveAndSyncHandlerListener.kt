0f243138bc149c66ae9fea2f6b9d877b01637af2
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-05-20 23:50:40 +0200

    convert SaveAndSyncHandlerListener to modern language
    GitOrigin-RevId: 5edb8ab66640b2ed8547e9366fdb153a7a6c4fb0

--- platform/platform-api/src/com/intellij/ide/SaveAndSyncHandlerListener.kt
+++ platform/platform-api/src/com/intellij/ide/SaveAndSyncHandlerListener.kt
@@ -1,15 +1,21 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ide;
+package com.intellij.ide
 
-import com.intellij.util.messages.Topic;
-import org.jetbrains.annotations.ApiStatus;
+import com.intellij.util.messages.Topic
+import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Experimental
-public interface SaveAndSyncHandlerListener {
-  @Topic.AppLevel
-  Topic<SaveAndSyncHandlerListener> TOPIC = new Topic<>("SaveAndSyncHandler events", SaveAndSyncHandlerListener.class);
+@ApiStatus.Internal
+interface SaveAndSyncHandlerListener {
+  companion object {
+    @Topic.AppLevel
+    @JvmField
+    val TOPIC: Topic<SaveAndSyncHandlerListener> = Topic("SaveAndSyncHandler events",
+                                                         SaveAndSyncHandlerListener::class.java,
+                                                         Topic.BroadcastDirection.NONE)
+  }
 
-  default void beforeRefresh() {}
+  fun beforeRefresh() {}
 
-  default void beforeSave(SaveAndSyncHandler.SaveTask task, boolean forceExecuteImmediately) {}
+  fun beforeSave(task: SaveAndSyncHandler.SaveTask?, forceExecuteImmediately: Boolean) {}
 }
