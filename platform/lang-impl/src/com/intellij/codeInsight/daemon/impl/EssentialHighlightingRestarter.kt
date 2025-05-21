e79dae7542f1c94ec8a4b3ca5ec6abfdbb3df313
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-05-21 08:33:47 +0200

    convert EssentialHighlightingRestarter to modern language
    GitOrigin-RevId: 3b0dcaec45f311aaf12c855a2488636c4decf1bf

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/EssentialHighlightingRestarter.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/EssentialHighlightingRestarter.kt
@@ -1,61 +1,50 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.codeInsight.daemon.impl;
+package com.intellij.codeInsight.daemon.impl
 
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
+import com.intellij.codeInsight.daemon.EssentialHighlightingRestarterDisablement
+import com.intellij.codeInsight.daemon.impl.analysis.FileHighlightingSetting
+import com.intellij.codeInsight.daemon.impl.analysis.HighlightingSettingsPerFile
+import com.intellij.ide.SaveAndSyncHandler
+import com.intellij.ide.SaveAndSyncHandlerListener
+import com.intellij.openapi.application.readAction
+import com.intellij.openapi.extensions.ExtensionPointName.Companion.create
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.psi.impl.PsiManagerEx
 
-import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
-import com.intellij.codeInsight.daemon.EssentialHighlightingRestarterDisablement;
-import com.intellij.codeInsight.daemon.impl.analysis.FileHighlightingSetting;
-import com.intellij.codeInsight.daemon.impl.analysis.HighlightingSettingsPerFile;
-import com.intellij.ide.SaveAndSyncHandler;
-import com.intellij.ide.SaveAndSyncHandlerListener;
-import com.intellij.openapi.application.ReadAction;
-import com.intellij.openapi.extensions.ExtensionPointName;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.registry.Registry;
-import com.intellij.psi.impl.PsiManagerEx;
-import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
+private val EP_NAME = create<EssentialHighlightingRestarterDisablement>("com.intellij.daemon.essentialHighlightingRestarterDisablement")
 
-import java.util.Arrays;
-import java.util.Objects;
+private fun isEssentialHighlightingRestarterDisabledForProject(project: Project): Boolean {
+  return EP_NAME.extensionList.any { it.shouldBeDisabledForProject(project) }
+}
 
 /**
- * Tells {@link DaemonCodeAnalyzerImpl} to run full set of passes after "Save all" action was invoked, to show all diagnostics
+ * Tells [DaemonCodeAnalyzerImpl] to run full set of passes after "Save all" action was invoked, to show all diagnostics
  * if the current selected file configured as "Highlight: Essential only"
  */
-@ApiStatus.Internal
-public final class EssentialHighlightingRestarter implements SaveAndSyncHandlerListener {
-  private final Project myProject;
-
-  private final ExtensionPointName<EssentialHighlightingRestarterDisablement>
-    EP_NAME = ExtensionPointName.create("com.intellij.daemon.essentialHighlightingRestarterDisablement");
-
-  private boolean isEssentialHighlightingRestarterDisabledForProject(Project project) {
-    return ContainerUtil.exists(EP_NAME.getExtensionList(), extension -> extension.shouldBeDisabledForProject(project));
-  }
-
-  public EssentialHighlightingRestarter(Project project) {
-    myProject = project;
-  }
-
-  @Override
-  public void beforeSave(@NotNull SaveAndSyncHandler.SaveTask task, boolean forceExecuteImmediately) {
-    if (!myProject.isInitialized() || myProject.isDisposed()
-        || !Registry.is("highlighting.essential.should.restart.in.full.mode.on.save.all")
-        || isEssentialHighlightingRestarterDisabledForProject(myProject)) {
-      return;
+private class EssentialHighlightingRestarter(private val project: Project) : SaveAndSyncHandlerListener {
+  override suspend fun beforeSave(task: SaveAndSyncHandler.SaveTask, forceExecuteImmediately: Boolean) {
+    if (!project.isInitialized() || project.isDisposed()
+        || !Registry.`is`("highlighting.essential.should.restart.in.full.mode.on.save.all") || isEssentialHighlightingRestarterDisabledForProject(
+        project)
+    ) {
+      return
     }
-    boolean hasFilesWithEssentialHighlightingConfigured =
-      Arrays.stream(FileEditorManager.getInstance(myProject).getOpenFiles())
-        .map(vf -> ReadAction.nonBlocking(() -> PsiManagerEx.getInstanceEx(myProject).getFileManager().findFile(vf)).executeSynchronously())
-        .filter(Objects::nonNull)
-        .anyMatch(psiFile -> ReadAction.nonBlocking(() -> HighlightingSettingsPerFile.getInstance(myProject).getHighlightingSettingForRoot(psiFile) ==
-                                                      FileHighlightingSetting.ESSENTIAL).executeSynchronously());
+    val hasFilesWithEssentialHighlightingConfigured = FileEditorManager.getInstance(project).getOpenFiles()
+      .mapNotNull { vf ->
+        readAction { PsiManagerEx.getInstanceEx(project).getFileManager().findFile(vf!!) }
+      }
+      .any { psiFile ->
+        readAction {
+          HighlightingSettingsPerFile.getInstance(project).getHighlightingSettingForRoot(psiFile) ==
+            FileHighlightingSetting.ESSENTIAL
+        }
+      }
     if (hasFilesWithEssentialHighlightingConfigured) {
-      DaemonCodeAnalyzerImpl codeAnalyzer = (DaemonCodeAnalyzerImpl)DaemonCodeAnalyzer.getInstance(myProject);
-      codeAnalyzer.requestRestartToCompleteEssentialHighlighting();
+      val codeAnalyzer = DaemonCodeAnalyzer.getInstance(project) as DaemonCodeAnalyzerImpl
+      codeAnalyzer.requestRestartToCompleteEssentialHighlighting()
     }
   }
 }
