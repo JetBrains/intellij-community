ae9df6a19bf540d5bae0c91bfb2a58297f12aa62
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-05-21 12:46:46 +0200

    EssentialHighlightingRestarter - use modern non-blocking API
    GitOrigin-RevId: a55c5a014599d37f21b018c83eeebafe88fc9951

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/EssentialHighlightingRestarter.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/EssentialHighlightingRestarter.kt
@@ -4,15 +4,21 @@ package com.intellij.codeInsight.daemon.impl
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.codeInsight.daemon.EssentialHighlightingRestarterDisablement
 import com.intellij.codeInsight.daemon.impl.analysis.FileHighlightingSetting
+import com.intellij.codeInsight.daemon.impl.analysis.HighlightingLevelManager
 import com.intellij.codeInsight.daemon.impl.analysis.HighlightingSettingsPerFile
 import com.intellij.ide.SaveAndSyncHandler
 import com.intellij.ide.SaveAndSyncHandlerListener
 import com.intellij.openapi.application.readAction
+import com.intellij.openapi.components.serviceAsync
+import com.intellij.openapi.components.serviceIfCreated
 import com.intellij.openapi.extensions.ExtensionPointName.Companion.create
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.getOpenedProjects
 import com.intellij.openapi.util.registry.Registry
+import com.intellij.psi.PsiManager
 import com.intellij.psi.impl.PsiManagerEx
+import com.intellij.serviceContainer.AlreadyDisposedException
 
 private val EP_NAME = create<EssentialHighlightingRestarterDisablement>("com.intellij.daemon.essentialHighlightingRestarterDisablement")
 
@@ -21,30 +27,50 @@ private fun isEssentialHighlightingRestarterDisabledForProject(project: Project)
 }
 
 /**
- * Tells [DaemonCodeAnalyzerImpl] to run full set of passes after "Save all" action was invoked, to show all diagnostics
+ * Tells [DaemonCodeAnalyzerImpl] to run a full set of passes after "Save all" action was invoked, to show all diagnostics
  * if the current selected file configured as "Highlight: Essential only"
  */
-private class EssentialHighlightingRestarter(private val project: Project) : SaveAndSyncHandlerListener {
+private class EssentialHighlightingRestarter() : SaveAndSyncHandlerListener {
+  @Suppress("IncorrectCancellationExceptionHandling")
   override suspend fun beforeSave(task: SaveAndSyncHandler.SaveTask, forceExecuteImmediately: Boolean) {
-    if (!project.isInitialized() || project.isDisposed()
-        || !Registry.`is`("highlighting.essential.should.restart.in.full.mode.on.save.all") || isEssentialHighlightingRestarterDisabledForProject(
-        project)
-    ) {
-      return
-    }
-    val hasFilesWithEssentialHighlightingConfigured = FileEditorManager.getInstance(project).getOpenFiles()
-      .mapNotNull { vf ->
-        readAction { PsiManagerEx.getInstanceEx(project).getFileManager().findFile(vf!!) }
-      }
-      .any { psiFile ->
-        readAction {
-          HighlightingSettingsPerFile.getInstance(project).getHighlightingSettingForRoot(psiFile) ==
-            FileHighlightingSetting.ESSENTIAL
+    val requestedProject = task.project
+    if (requestedProject == null) {
+      for (project in getOpenedProjects()) {
+        try {
+          requestRestartToCompleteEssentialHighlighting(project)
+        }
+        catch (_: AlreadyDisposedException) {
         }
       }
-    if (hasFilesWithEssentialHighlightingConfigured) {
-      val codeAnalyzer = DaemonCodeAnalyzer.getInstance(project) as DaemonCodeAnalyzerImpl
-      codeAnalyzer.requestRestartToCompleteEssentialHighlighting()
     }
+    else {
+      requestRestartToCompleteEssentialHighlighting(requestedProject)
+    }
+  }
+}
+
+private suspend fun requestRestartToCompleteEssentialHighlighting(project: Project) {
+  if (!project.isInitialized() ||
+      !Registry.`is`("highlighting.essential.should.restart.in.full.mode.on.save.all", true) ||
+      isEssentialHighlightingRestarterDisabledForProject(project)) {
+    return
+  }
+
+  val openFiles = project.serviceIfCreated<FileEditorManager>()?.getOpenFiles()
+  if (openFiles.isNullOrEmpty()) {
+    return
+  }
+
+  val psiFileManager = (project.serviceAsync<PsiManager>() as PsiManagerEx).getFileManager()
+  val highlightingSettingsPerFile = project.serviceAsync<HighlightingLevelManager>() as HighlightingSettingsPerFile
+  val hasFilesWithEssentialHighlightingConfigured = openFiles
+    .any { file ->
+      readAction {
+        val psiFile = psiFileManager.findFile(file) ?: return@readAction false
+        highlightingSettingsPerFile.getHighlightingSettingForRoot(psiFile) == FileHighlightingSetting.ESSENTIAL
+      }
+    }
+  if (hasFilesWithEssentialHighlightingConfigured) {
+    (project.serviceAsync<DaemonCodeAnalyzer>() as DaemonCodeAnalyzerImpl).requestRestartToCompleteEssentialHighlighting()
   }
 }
