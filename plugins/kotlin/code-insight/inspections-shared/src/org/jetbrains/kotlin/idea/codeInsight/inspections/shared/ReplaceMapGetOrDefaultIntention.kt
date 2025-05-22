e763c51ed34c4ddd6149e792b9cd7807b197f323
Victoria.Petrakovich <victoria.petrakovich@jetbrains.com>
2025-05-22 17:46:08 +0200

    [kotlin] Move `ReplaceMapGetOrDefaultIntention` to shared inspections folder
    For the sake of preserving history
    
    KTIJ-32034
    
    GitOrigin-RevId: 5fdff71bacaa82dd1f2cd8790e2ab4e0bc06894e

--- plugins/kotlin/code-insight/inspections-shared/src/org/jetbrains/kotlin/idea/codeInsight/inspections/shared/ReplaceMapGetOrDefaultIntention.kt
+++ plugins/kotlin/code-insight/inspections-shared/src/org/jetbrains/kotlin/idea/codeInsight/inspections/shared/ReplaceMapGetOrDefaultIntention.kt
@@ -1,6 +1,6 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
-package org.jetbrains.kotlin.idea.intentions
+package org.jetbrains.kotlin.idea.codeInsight.inspections.shared
 
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.TextRange
@@ -17,6 +17,7 @@ import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
 import org.jetbrains.kotlin.resolve.calls.util.getType
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 import org.jetbrains.kotlin.types.isNullable
+import kotlin.collections.lastOrNull
 
 class ReplaceMapGetOrDefaultIntention : SelfTargetingRangeIntention<KtDotQualifiedExpression>(
     KtDotQualifiedExpression::class.java, KotlinBundle.lazyMessage("replace.with.indexing.and.elvis.operator")
