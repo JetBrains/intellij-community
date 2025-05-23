ab8b1813a2c8aa93936a5ac24c760387aae8c7b9
Chris Lemaire <chris.lemaire@jetbrains.com>
2025-05-23 17:26:14 +0200

    [github] Add way to forcefully guestimate the LEFT or RIGHT line number of a comment
    Includes declarativish testing of line mapping.
    
    GitOrigin-RevId: fc792139f4d40a2f5c88f09342030837be9839c8

--- plugins/git4idea/src/git4idea/changes/GitTextFilePatchWithHistory.kt
+++ plugins/git4idea/src/git4idea/changes/GitTextFilePatchWithHistory.kt
@@ -8,6 +8,7 @@ import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.diff.impl.patch.PatchHunkUtil
 import com.intellij.openapi.diff.impl.patch.TextFilePatch
 import org.jetbrains.annotations.ApiStatus
+import kotlin.math.floor
 
 /**
  * Holds the [patch] between two commits in [fileHistory]
@@ -25,6 +26,32 @@ class GitTextFilePatchWithHistory(val patch: TextFilePatch, val isCumulative: Bo
   fun contains(commitSha: String, filePath: String): Boolean = fileHistory.contains(commitSha, filePath)
 
   /**
+   * @return `null` only if the commit cannot be found.
+   * Otherwise, this function returns the most accurate line number on the given side of the patch history we could derive.
+   * If the line is removed during any patch, its location information can no longer be retrieved exactly, so instead, we
+   * continue with the topmost line of the changed hunk.
+   */
+  fun forcefullyMapLine(fromCommitSha: String, lineIndex: Int, side: Side): Int? {
+    // map to merge base, not left revision
+    val beforeSha = if (isCumulative) fileHistory.findStartCommit()!! else patch.beforeVersionId!!
+    val afterSha = patch.afterVersionId!!
+
+    if (fromCommitSha == beforeSha && side == Side.LEFT) return lineIndex
+    if (fromCommitSha == afterSha && side == Side.RIGHT) return lineIndex
+
+    return when (side) {
+      Side.LEFT -> {
+        val patches = fileHistory.getPatchesBetween(beforeSha, fromCommitSha)
+        transferLine(patches, lineIndex, rightToLeft = true).line
+      }
+      Side.RIGHT -> {
+        val patches = fileHistory.getPatchesBetween(fromCommitSha, afterSha)
+        transferLine(patches, lineIndex, rightToLeft = false).line
+      }
+    }
+  }
+
+  /**
    * Map the [lineIndex] in a file in a *commit* [fromCommitSha] to a location in the current [patch]
    *
    * @param fromCommitSha commit where the line index is known
@@ -39,38 +66,14 @@ class GitTextFilePatchWithHistory(val patch: TextFilePatch, val isCumulative: Bo
     if (fromCommitSha == beforeSha) return DiffLineLocation(Side.LEFT, lineIndex)
     if (fromCommitSha == afterSha) return DiffLineLocation(Side.RIGHT, lineIndex)
 
-    fun tryTransferToParent(): DiffLineLocation? {
-      return if (fileHistory.compare(afterSha, fromCommitSha) < 0) {
-        val patches = fileHistory.getPatchesBetween(afterSha, fromCommitSha)
-        transferLine(patches, lineIndex, true)
-      }
-      else if (fileHistory.compare(beforeSha, fromCommitSha) < 0) {
-        val patches = fileHistory.getPatchesBetween(beforeSha, fromCommitSha)
-        transferLine(patches, lineIndex, true)
-      }
-      else {
-        null
-      }
-    }
-
-    fun tryTransferToChild(): DiffLineLocation? {
-      return if (fileHistory.compare(fromCommitSha, beforeSha) < 0) {
-        val patches = fileHistory.getPatchesBetween(fromCommitSha, beforeSha)
-        transferLine(patches, lineIndex, false)
-      }
-      else if (fileHistory.compare(fromCommitSha, afterSha) < 0) {
-        val patches = fileHistory.getPatchesBetween(fromCommitSha, afterSha)
-        transferLine(patches, lineIndex, false)
-      }
-      else {
-        null
-      }
-    }
+    if (!fileHistory.contains(fromCommitSha, patch.filePath)) return null
 
     return try {
       when (bias) {
-        Side.LEFT -> tryTransferToParent() ?: tryTransferToChild()
-        Side.RIGHT -> tryTransferToChild() ?: tryTransferToParent()
+        Side.LEFT -> transferToParent(lineIndex, beforeSha, fromCommitSha, afterSha)
+                     ?: transferToChild(lineIndex, beforeSha, fromCommitSha, afterSha)
+        Side.RIGHT -> transferToChild(lineIndex, beforeSha, fromCommitSha, afterSha)
+                      ?: transferToParent(lineIndex, beforeSha, fromCommitSha, afterSha)
       }
     }
     catch (e: Exception) {
@@ -79,9 +82,43 @@ class GitTextFilePatchWithHistory(val patch: TextFilePatch, val isCumulative: Bo
     }
   }
 
-  private fun transferLine(patchChain: List<TextFilePatch>, lineIndex: Int, rightToLeft: Boolean): DiffLineLocation? {
-    val side: Side = if (rightToLeft) Side.RIGHT else Side.LEFT
-    var currentLine: Int = lineIndex
+  private fun transferToParent(lineIndex: Int, beforeSha: String, fromCommitSha: String, afterSha: String): DiffLineLocation? =
+    if (fileHistory.compare(afterSha, fromCommitSha) < 0) {
+      val patches = fileHistory.getPatchesBetween(afterSha, fromCommitSha)
+      transferLine(patches, lineIndex, true).exactLocation()?.let {
+        Side.RIGHT to it
+      }
+    }
+    else if (fileHistory.compare(beforeSha, fromCommitSha) < 0) {
+      val patches = fileHistory.getPatchesBetween(beforeSha, fromCommitSha)
+      transferLine(patches, lineIndex, true).exactLocation()?.let {
+        Side.LEFT to it
+      }
+    }
+    else {
+      error("Couldn't find commit ${fromCommitSha}")
+    }
+
+  private fun transferToChild(lineIndex: Int, beforeSha: String, fromCommitSha: String, afterSha: String): DiffLineLocation? =
+    if (fileHistory.compare(fromCommitSha, beforeSha) < 0) {
+      val patches = fileHistory.getPatchesBetween(fromCommitSha, beforeSha)
+      transferLine(patches, lineIndex, false).exactLocation()?.let {
+        Side.LEFT to it
+      }
+    }
+    else if (fileHistory.compare(fromCommitSha, afterSha) < 0) {
+      val patches = fileHistory.getPatchesBetween(fromCommitSha, afterSha)
+      transferLine(patches, lineIndex, false).exactLocation()?.let {
+        Side.RIGHT to it
+      }
+    }
+    else {
+      error("Couldn't find commit ${fromCommitSha}")
+    }
+
+  private fun transferLine(patchChain: List<TextFilePatch>, lineIndex: Int, rightToLeft: Boolean): TransferResult {
+    var currentLine = lineIndex.toDouble()
+    var isEstimate = false
 
     val patches = if (rightToLeft) patchChain.asReversed() else patchChain
 
@@ -91,23 +128,51 @@ class GitTextFilePatchWithHistory(val patch: TextFilePatch, val isCumulative: Bo
         if (rightToLeft) ranges.map { reverseRange(it) } else ranges
       }.flatten()
 
-      var offset = 0
+      var offset = 0.0
       loop@ for (range in changeOnlyRanges) {
         when {
           currentLine < range.start1 ->
             break@loop
-          currentLine in range.start1 until range.end1 ->
-            return null
-          currentLine >= range.end1 ->
+          range.start1 <= currentLine && currentLine < range.end1 -> {
+            isEstimate = true
+            // Careful when changing: we assume that range.end1 - range.start1 is non-zero due to the above check
+            // Estimated position algo: find the relative position of the line in the hunk, then map it to that relative position in the new hunk
+            // A choice can be made about how to map to 'relative position' though; we choose to find the relative position based on the 'center' of the line within a range:
+            //  - in a half-open range [1, 2), line 1 has relative position 0.5 (the 'center' of the line is used rather than the start)
+            //  - in a half-open range [1, 3), line 1 has relative position 0.333, line 2 has relative position 0.667
+            val relativePosition = ((currentLine - range.start1) + 1) / ((range.end1 - range.start1) + 1).toDouble()
+            offset -= currentLine - range.start1 // move line to the start of the range in the old hunk
+            // We map relative positions back to the output line in the following way:
+            //  - for any relPos and out-range [1, 1) - there no output line -, we map to line 1
+            //  - for any relPos and out-range [1, 2) - there is only 1 possible output line -, we map to line 1.5 (and round after all mapping is done)
+            //  - for relPos = 0.5 and out-range [1, 3) - there are 2 possible output lines -, we map to line 2
+            //  - for relPos = 0.5 and out-range [1, 4) - there are 3 possible output lines -, we map to line 2.5 (and round after all mapping is done)
+            offset += relativePosition * (range.end2 - range.start2) // then to the relative position in the new hunk
+          }
+          range.end1 <= currentLine ->
             offset += (range.end2 - range.start2) - (range.end1 - range.start1)
         }
       }
       currentLine += offset
     }
-    return DiffLineLocation(side, currentLine)
+
+    // Note that the only way for the line number to become floating point is when it IS an estimate
+    return when (isEstimate) {
+      false -> TransferResult.ExactTransfer(floor(currentLine).toInt())
+      true -> TransferResult.EstimatedTransfer(floor(currentLine).toInt())
+    }
   }
 
   private fun reverseRange(range: Range) = Range(range.start2, range.end2, range.start1, range.end1)
+
+  private sealed interface TransferResult {
+    data class ExactTransfer(override val line: Int) : TransferResult
+    data class EstimatedTransfer(override val line: Int) : TransferResult
+
+    val line: Int?
+
+    fun exactLocation(): Int? = (this as? ExactTransfer)?.line
+  }
 
   companion object {
     private val LOG = logger<GitTextFilePatchWithHistory>()
