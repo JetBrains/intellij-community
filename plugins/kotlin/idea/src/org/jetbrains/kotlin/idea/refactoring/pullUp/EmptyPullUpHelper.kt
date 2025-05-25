01190d6fe663d8396c0e6cbae0de1b8c0b2fc737
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2025-05-25 23:15:56 +0400

    [kotlin] Use expression body syntax in `EmptyPullUpHelper`
    #KTIJ-26170
    #KTIJ-32556
    
    GitOrigin-RevId: aca98063a4b7f787e4c71bbc28c6b8b8feef6742

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/refactoring/pullUp/EmptyPullUpHelper.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/refactoring/pullUp/EmptyPullUpHelper.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.refactoring.pullUp
 
@@ -10,28 +10,10 @@ import com.intellij.refactoring.classMembers.MemberInfoBase
 import com.intellij.refactoring.memberPullUp.PullUpHelper
 
 object EmptyPullUpHelper : PullUpHelper<MemberInfoBase<PsiMember>> {
-    override fun postProcessMember(member: PsiMember) {
-
-    }
-
-    override fun moveFieldInitializations(movedFields: LinkedHashSet<PsiField>) {
-
-    }
-
-    override fun encodeContextInfo(info: MemberInfoBase<PsiMember>) {
-
-    }
-
-    override fun setCorrectVisibility(info: MemberInfoBase<PsiMember>) {
-
-    }
-
-    override fun move(info: MemberInfoBase<PsiMember>, substitutor: PsiSubstitutor) {
-
-    }
-
-    override fun updateUsage(element: PsiElement) {
-
-    }
+    override fun postProcessMember(member: PsiMember): Unit = Unit
+    override fun moveFieldInitializations(movedFields: LinkedHashSet<PsiField>): Unit = Unit
+    override fun encodeContextInfo(info: MemberInfoBase<PsiMember>): Unit = Unit
+    override fun setCorrectVisibility(info: MemberInfoBase<PsiMember>): Unit = Unit
+    override fun move(info: MemberInfoBase<PsiMember>, substitutor: PsiSubstitutor): Unit = Unit
+    override fun updateUsage(element: PsiElement): Unit = Unit
 }
-
