931eb4c151a3567fd85d3fdd0bcb35b76ac44327
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-05-25 17:22:49 +0200

    add recovery policy for api dump parser
    GitOrigin-RevId: 12d92f74ec28d07e1f0c3c5392879528eda25edd

--- plugins/devkit/api-dump-lang/src/com/intellij/devkit/apiDump/lang/parser/ADParserUtil.kt
+++ plugins/devkit/api-dump-lang/src/com/intellij/devkit/apiDump/lang/parser/ADParserUtil.kt
@@ -1,5 +1,30 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:JvmName("ADParserUtil")
+
 package com.intellij.devkit.apiDump.lang.parser
 
-internal class ADParserUtil {
+import com.intellij.lang.PsiBuilder
+import com.intellij.psi.TokenType
+
+internal fun consumeUntilNextLineImpl(builder: PsiBuilder, level: Int) {
+  while (!builder.eof()) {
+    if (isLineFeed(builder, level)) break
+    builder.advanceLexer()
+  }
+}
+
+/** Check if there is a line feed before the current token  */
+private fun isLineFeed(builder: PsiBuilder, level: Int): Boolean {
+  if (builder.eof()) return true
+
+  var steps = 0
+  while (builder.rawLookup(steps - 1) === TokenType.WHITE_SPACE) {
+    steps--
+  }
+
+  if (builder.rawLookup(steps - 1) == null) return true
+  val start = builder.rawTokenTypeStart(steps)
+  val end = builder.getCurrentOffset()
+  val originalText = builder.getOriginalText()
+  return originalText.subSequence(start, end).contains('\n')
 }
