e4b5f396ddf4ab041d173593aebfbc319c3e2e8a
Leonid Shalupov <leonid@shalupov.com>
2025-05-26 09:33:04 +0200

    git: add worktree command
    GitOrigin-RevId: 3db697051d67ba75c838d898ff61daad682f632c

--- plugins/git4idea/src/git4idea/commands/GitCommand.java
+++ plugins/git4idea/src/git4idea/commands/GitCommand.java
@@ -66,6 +66,7 @@ public final class GitCommand {
   public static final GitCommand UPDATE_REF = write("update-ref");
   public static final GitCommand HASH_OBJECT = write("hash-object");
   public static final GitCommand VERSION = read("version");
+  public static final GitCommand WORKTREE = read("worktree");
 
   /**
    * Name of environment variable that specifies editor for the git
