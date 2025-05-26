85d6b6f37e049471f9fc218ad1e3264676889f15
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-05-26 11:47:54 +0200

    [threading] IJPL-182690: Allow disabling dispatch of events during freeze
    GitOrigin-RevId: 8285bbd2f2497938f82ef3d02e0e3135fb5b866b

--- platform/platform-impl/src/com/intellij/openapi/application/impl/SuvorovProgressInitializationActivity.kt
+++ platform/platform-impl/src/com/intellij/openapi/application/impl/SuvorovProgressInitializationActivity.kt
@@ -3,12 +3,16 @@ package com.intellij.openapi.application.impl
 
 import com.intellij.ide.AppLifecycleListener
 import com.intellij.openapi.progress.util.SuvorovProgress
+import com.intellij.openapi.util.registry.Registry
 import org.jetbrains.annotations.ApiStatus
 import javax.swing.SwingUtilities
 
 @ApiStatus.Internal
 class SuvorovProgressInitializationActivity : AppLifecycleListener {
   override fun appStarted() {
+    if (!Registry.`is`("ide.event.dispatch.during.lock.freezes")) {
+      return
+    }
     SwingUtilities.invokeLater {
       getGlobalThreadingSupport().setLockAcquisitionInterceptor(SuvorovProgress::dispatchEventsUntilComputationCompletes)
     }
