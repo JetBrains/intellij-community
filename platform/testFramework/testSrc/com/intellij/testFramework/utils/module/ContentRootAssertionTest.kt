78611e69dd74c4712132c0baec37f32d690c6f1d
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2025-05-28 15:32:26 +0200

    [TestFramework] cleanup: simplify test project preparation for ModuleAssertionTestCase
    GitOrigin-RevId: 3c742ff7d1e87e3210a0c4186c7a73106c59c957

--- platform/testFramework/testSrc/com/intellij/testFramework/utils/module/ContentRootAssertionTest.kt
+++ platform/testFramework/testSrc/com/intellij/testFramework/utils/module/ContentRootAssertionTest.kt
@@ -1,9 +1,8 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.testFramework.utils.module
 
+import com.intellij.platform.backend.workspace.workspaceModel
 import com.intellij.platform.testFramework.assertion.moduleAssertion.ContentRootAssertions.assertContentRoots
-import com.intellij.testFramework.useProjectAsync
-import com.intellij.testFramework.utils.io.createDirectory
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -13,25 +12,22 @@ class ContentRootAssertionTest : ModuleAssertionTestCase() {
   @Test
   fun `test ContentRootAssertions#assertContentRoots`() {
     runBlocking {
-      val projectRoot = testDirectory.createDirectory("project")
-      val project = generateProject(projectRoot) {
+      project.workspaceModel.update {
         addModuleEntity("project", ".")
         addModuleEntity("project.module1", "module1")
         addModuleEntity("project.module2", "module2")
       }
-      project.useProjectAsync {
-        assertContentRoots(project, "project", projectRoot)
-        assertContentRoots(project, "project.module1", projectRoot.resolve("module1"))
-        assertContentRoots(project, "project.module2", projectRoot.resolve("module2"))
-        Assertions.assertThrows(AssertionError::class.java) {
-          assertContentRoots(project, "module1", projectRoot.resolve("module1"))
-        }
-        Assertions.assertThrows(AssertionError::class.java) {
-          assertContentRoots(project, "project", projectRoot, projectRoot.resolve("other-root"))
-        }
-        Assertions.assertThrows(AssertionError::class.java) {
-          assertContentRoots(project, "other-module", projectRoot.resolve("other-module"))
-        }
+      assertContentRoots(project, "project", projectRoot)
+      assertContentRoots(project, "project.module1", projectRoot.resolve("module1"))
+      assertContentRoots(project, "project.module2", projectRoot.resolve("module2"))
+      Assertions.assertThrows(AssertionError::class.java) {
+        assertContentRoots(project, "module1", projectRoot.resolve("module1"))
+      }
+      Assertions.assertThrows(AssertionError::class.java) {
+        assertContentRoots(project, "project", projectRoot, projectRoot.resolve("other-root"))
+      }
+      Assertions.assertThrows(AssertionError::class.java) {
+        assertContentRoots(project, "other-module", projectRoot.resolve("other-module"))
       }
     }
   }
