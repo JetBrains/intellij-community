198b8a1f2bfac3670a88571432b4c0e666b542de
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-05-28 13:50:22 +0200

    syntax: JavaSyntaxDefinition implements LanguageSyntaxDefinition
    GitOrigin-RevId: a7559d9a261df58993d77d84d7d437419e744db7

--- java/java-syntax/src/com/intellij/java/syntax/JavaSyntaxDefinition.kt
+++ java/java-syntax/src/com/intellij/java/syntax/JavaSyntaxDefinition.kt
@@ -6,6 +6,7 @@ import com.intellij.java.syntax.element.JavaSyntaxTokenType
 import com.intellij.java.syntax.lexer.JavaDocLexer
 import com.intellij.java.syntax.lexer.JavaLexer
 import com.intellij.java.syntax.lexer.JavaTypeEscapeLexer
+import com.intellij.platform.syntax.LanguageSyntaxDefinition
 import com.intellij.platform.syntax.SyntaxElementTypeSet
 import com.intellij.platform.syntax.SyntaxLanguage
 import com.intellij.platform.syntax.element.SyntaxTokenTypes
@@ -14,7 +15,7 @@ import com.intellij.platform.syntax.syntaxElementTypeSetOf
 import com.intellij.pom.java.LanguageLevel
 import kotlin.jvm.JvmStatic
 
-object JavaSyntaxDefinition {
+object JavaSyntaxDefinition : LanguageSyntaxDefinition {
   val language: SyntaxLanguage = SyntaxLanguage("com.intellij.java")
 
   @JvmStatic
@@ -33,4 +34,10 @@ object JavaSyntaxDefinition {
   )
 
   val whitespaces: SyntaxElementTypeSet = syntaxElementTypeSetOf(SyntaxTokenTypes.WHITE_SPACE)
+
+  override fun getLexer(): Lexer = JavaLexer(LanguageLevel.HIGHEST)
+
+  override fun getCommentTokens(): SyntaxElementTypeSet = commentSet
+
+  override fun getWhitespaceTokens(): SyntaxElementTypeSet = whitespaces
 }
