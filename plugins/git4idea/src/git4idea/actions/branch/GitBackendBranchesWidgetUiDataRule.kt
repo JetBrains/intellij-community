7d1bc629d6d7d4201ffc04ac2b8c042390d8d8a7
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2025-05-29 18:09:56 +0200

    [git] Fix wrong data keys are used in GitBackendBranchesWidgetUiDataRule
    GitOrigin-RevId: 56fcf2c33afcb52ec5301f30d076244c2dcf03da

--- plugins/git4idea/src/git4idea/actions/branch/GitBackendBranchesWidgetUiDataRule.kt
+++ plugins/git4idea/src/git4idea/actions/branch/GitBackendBranchesWidgetUiDataRule.kt
@@ -5,12 +5,13 @@ import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataSink
 import com.intellij.openapi.actionSystem.DataSnapshot
 import com.intellij.openapi.actionSystem.UiDataRule
+import com.intellij.vcs.git.shared.widget.actions.GitBranchesWidgetKeys
 import git4idea.repo.GitRepositoryIdCache
 
-class GitBackendBranchesWidgetUiDataRule: UiDataRule {
+internal class GitBackendBranchesWidgetUiDataRule: UiDataRule {
   override fun uiDataSnapshot(sink: DataSink, snapshot: DataSnapshot) {
-    val affectedRepositories = snapshot[GitBranchActionsDataKeys.AFFECTED_REPOSITORIES]
-    val selectedRepository = snapshot[GitBranchActionsDataKeys.SELECTED_REPOSITORY]
+    val affectedRepositories = snapshot[GitBranchesWidgetKeys.AFFECTED_REPOSITORIES]
+    val selectedRepository = snapshot[GitBranchesWidgetKeys.SELECTED_REPOSITORY]
 
     if (affectedRepositories == null && selectedRepository == null) return
 
@@ -18,7 +19,7 @@ class GitBackendBranchesWidgetUiDataRule: UiDataRule {
 
     val repositoryIdCache = GitRepositoryIdCache.getInstance(project)
 
-    sink[GitBranchActionsDataKeys.SELECTED_REPOSITORY] = selectedRepository?.rpcId?.let { repositoryIdCache.get(it) }
-    sink[GitBranchActionsDataKeys.AFFECTED_REPOSITORIES] = affectedRepositories?.mapNotNull { repositoryIdCache.get(it.rpcId) }
+    sink[GitBranchActionsDataKeys.SELECTED_REPOSITORY] = selectedRepository?.repositoryId?.let { repositoryIdCache.get(it) }
+    sink[GitBranchActionsDataKeys.AFFECTED_REPOSITORIES] = affectedRepositories?.mapNotNull { repositoryIdCache.get(it.repositoryId) }
   }
 }
