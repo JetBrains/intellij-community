88963cf8c3d2bcfa3e5467011cb459b47147a379
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-05-29 14:02:55 +0200

    [polySymbols] WEB-72822 Refactor WebSymbolDeclarationProviderDelegate -> PolySymbolDeclarationProviderDelegate
    GitOrigin-RevId: b3af7663cc8776172246aa75803920eab43b4dd4

--- platform/webSymbols/src/com/intellij/webSymbols/declarations/impl/PolySymbolDeclarationProviderDelegate.kt
+++ platform/webSymbols/src/com/intellij/webSymbols/declarations/impl/PolySymbolDeclarationProviderDelegate.kt
@@ -5,7 +5,7 @@ import com.intellij.model.psi.PsiSymbolDeclarationProvider
 import com.intellij.psi.PsiElement
 import com.intellij.webSymbols.declarations.PolySymbolDeclarationProvider
 
-class WebSymbolDeclarationProviderDelegate : PsiSymbolDeclarationProvider {
+class PolySymbolDeclarationProviderDelegate : PsiSymbolDeclarationProvider {
   override fun getDeclarations(element: PsiElement, offsetInElement: Int): Collection<PsiSymbolDeclaration> =
     PolySymbolDeclarationProvider.getAllDeclarations(element, offsetInElement)
 }
