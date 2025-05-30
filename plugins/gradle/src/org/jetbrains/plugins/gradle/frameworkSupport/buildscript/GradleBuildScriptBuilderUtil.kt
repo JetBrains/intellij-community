42ed4bbd2cbb00dc3afb6d07e6fea28dc9d19d94
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2025-05-30 15:03:47 +0200

    [gradle][test][IDEA-373717] use K2 to run Kotlin-related tests with Gradle 8.11+
    Caused by https://github.com/gradle/gradle/issues/26720
    
    GitOrigin-RevId: 7cb812e14d2037e4e9077ae0413ed9f287eda736

--- plugins/gradle/src/org/jetbrains/plugins/gradle/frameworkSupport/buildscript/GradleBuildScriptBuilderUtil.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/frameworkSupport/buildscript/GradleBuildScriptBuilderUtil.kt
@@ -7,10 +7,45 @@ import com.intellij.gradle.toolingExtension.util.GradleVersionUtil
 import org.gradle.util.GradleVersion
 import org.gradle.util.GradleVersion.version
 
-
+/**
+ * Embedded Kotlin version     Minimum Gradle version      Kotlin Language version
+ *
+ * 1.3.10                      5.0                         1.3
+ * 1.3.11                      5.1                         1.3
+ * 1.3.20                      5.2                         1.3
+ * 1.3.21                      5.3                         1.3
+ * 1.3.31                      5.5                         1.3
+ * 1.3.41                      5.6                         1.3
+ * 1.3.50                      6.0                         1.3
+ * 1.3.61                      6.1                         1.3
+ * 1.3.70                      6.3                         1.3
+ * 1.3.71                      6.4                         1.3
+ * 1.3.72                      6.5                         1.3
+ * 1.4.20                      6.8                         1.3
+ * 1.4.31                      7.0                         1.4
+ * 1.5.21                      7.2                         1.4
+ * 1.5.31                      7.3                         1.4
+ * 1.6.21                      7.5                         1.4
+ * 1.7.10                      7.6                         1.4
+ * 1.8.10                      8.0                         1.8
+ * 1.8.20                      8.2                         1.8
+ * 1.9.0                       8.3                         1.8
+ * 1.9.10                      8.4                         1.8
+ * 1.9.20                      8.5                         1.8
+ * 1.9.22                      8.7                         1.8
+ * 1.9.23                      8.9                         1.8
+ * 1.9.24                      8.10                        1.8
+ * 2.0.20                      8.11                        1.8
+ * 2.0.21                      8.12                        1.8
+ *
+ * Source: https://docs.gradle.org/current/userguide/compatibility.html
+ */
 fun getKotlinVersion(gradleVersion: GradleVersion): String {
   val base = gradleVersion.baseVersion
   return when {
+    base >= version("8.12") -> "2.0.21"
+    base >= version("8.11") -> "2.0.20"
+    base >= version("8.10") -> "1.9.24"
     base >= version("8.7") -> "1.9.23"
     base >= version("6.7.1") -> "1.7.20"
     base >= version("5.6.2") -> "1.4.32"
