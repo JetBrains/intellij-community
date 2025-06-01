65fe061584a2a17e764b03956b330504fc5d45c4
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-06-01 20:06:26 +0200

    IJ-MR-162467 don't export intellij.platform.analysis
    GitOrigin-RevId: fcbcb68010b58cb507fee2c47473758b999e471b

--- platform/markdown-utils/src/com/intellij/markdown/utils/doc/DocMarkdownToHtmlConverter.kt
+++ platform/markdown-utils/src/com/intellij/markdown/utils/doc/DocMarkdownToHtmlConverter.kt
@@ -1,8 +1,7 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.markdown.utils.doc
 
 import com.intellij.lang.Language
-import com.intellij.lang.documentation.DocumentationSettings
 import com.intellij.lang.documentation.QuickDocHighlightingHelper
 import com.intellij.markdown.utils.doc.impl.DocFlavourDescriptor
 import com.intellij.markdown.utils.doc.impl.DocTagRenderer
