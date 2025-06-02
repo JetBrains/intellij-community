12f3ee65bd26fe6c88a8d49b7bd84ce0df218feb
Gregory.Shrago <gregory.shrago@jetbrains.com>
2025-06-02 23:37:17 +0400

    convert ActionProcessor to kotlin
    GitOrigin-RevId: de4fb67fe222fb73c50a156f4ee566d3a2b0d326

--- platform/platform-impl/src/com/intellij/openapi/keymap/impl/ActionProcessor.kt
+++ platform/platform-impl/src/com/intellij/openapi/keymap/impl/ActionProcessor.kt
@@ -1,36 +1,28 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.keymap.impl;
+package com.intellij.openapi.keymap.impl
 
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.actionSystem.ex.ActionUtil;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-
-import java.awt.event.InputEvent;
+import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.actionSystem.ex.ActionUtil.doPerformActionOrShowPopup
+import org.jetbrains.annotations.ApiStatus
+import java.awt.event.InputEvent
 
 @ApiStatus.Internal
-public abstract class ActionProcessor {
-  public @NotNull AnActionEvent createEvent(@NotNull InputEvent inputEvent,
-                                            @NotNull DataContext context,
-                                            @NotNull String place,
-                                            @NotNull Presentation presentation,
-                                            @NotNull ActionManager manager) {
-    //noinspection MagicConstant
-    return new AnActionEvent(context, presentation, place, ActionUiKind.NONE, inputEvent, inputEvent.getModifiersEx(), manager);
+abstract class ActionProcessor {
+  open fun createEvent(
+    inputEvent: InputEvent,
+    context: DataContext,
+    place: String,
+    presentation: Presentation,
+    manager: ActionManager
+  ): AnActionEvent {
+    return AnActionEvent(context, presentation, place, ActionUiKind.NONE, inputEvent, inputEvent.modifiersEx, manager)
   }
 
-  public void onUpdatePassed(@NotNull InputEvent inputEvent,
-                             @NotNull AnAction action,
-                             @NotNull AnActionEvent event) {
+  open fun onUpdatePassed(inputEvent: InputEvent, action: AnAction, event: AnActionEvent) {
   }
 
-  public void performAction(@NotNull InputEvent inputEvent,
-                            @NotNull AnAction action,
-                            @NotNull AnActionEvent event) {
-    inputEvent.consume();
-    ActionUtil.doPerformActionOrShowPopup(action, event, popup -> {
-      popup.show(JBPopupFactory.getInstance().guessBestPopupLocation(action, event));
-    });
+  open fun performAction(inputEvent: InputEvent, action: AnAction, event: AnActionEvent) {
+    inputEvent.consume()
+    doPerformActionOrShowPopup(action, event, null)
   }
 }
