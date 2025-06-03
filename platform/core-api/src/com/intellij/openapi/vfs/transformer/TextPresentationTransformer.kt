57971b417925c98b12e2ff67567cd3979d16d9a8
Andrey.Matveev <andrey.matveev@jetbrains.com>
2025-06-03 11:01:33 +0300

    PY-75877 Fix problem with jupyter notebook shelve
    (cherry picked from commit 9172e753ec6e1dffca73aa9bf4608400f1ea3c9a)
    
    GitOrigin-RevId: 6331ca59f530bb4b7ed177639ef2f1cafab3ff32

--- platform/core-api/src/com/intellij/openapi/vfs/transformer/TextPresentationTransformer.kt
+++ platform/core-api/src/com/intellij/openapi/vfs/transformer/TextPresentationTransformer.kt
@@ -6,7 +6,27 @@ import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
 interface TextPresentationTransformer {
-  fun fromPersistent(text: CharSequence, virtualFile: VirtualFile):CharSequence
+  /**
+   * Transforms the given text from its persistent representation to its in-memory representation
+   * based on the specified virtual file's context.
+   *
+   * Used in Jupyter Notebooks to switch from JSON representation to source code text with #%% separators between cells.
+   *
+   * @param text The text in its persistent representation to be transformed
+   * @param virtualFile The virtual file providing context for the transformation
+   * @return The text in its in-memory representation
+   */
+  fun fromPersistent(text: CharSequence, virtualFile: VirtualFile): CharSequence
 
-  fun toPersistent(text: CharSequence, virtualFile: VirtualFile):CharSequence
+  /**
+   * Transforms the given text from its in-memory representation to its persistent representation
+   * based on the context provided by the specified virtual file.
+   *
+   * Used in Jupyter Notebooks to switch from source code text with #%% separators between cells to JSON representation.
+   *
+   * @param text The text in its in-memory representation to be transformed
+   * @param virtualFile The virtual file providing context for the transformation
+   * @return The text in its persistent representation
+   */
+  fun toPersistent(text: CharSequence, virtualFile: VirtualFile): CharSequence
 }
