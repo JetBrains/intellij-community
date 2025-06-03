ef205a4a891b832cd75684645feb1837a9ab470f
Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
2025-06-03 16:35:54 +0000

    FL-33864 EDT freeze with Grazie spellchecker
    Merge-request: IJ-MR-164732
    Merged-by: Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
    
    GitOrigin-RevId: 7e8e0d0ca856021b865417ed6ce5209db155b622

--- python/src/com/jetbrains/python/packaging/PyPackagesDictionary.kt
+++ python/src/com/jetbrains/python/packaging/PyPackagesDictionary.kt
@@ -1,9 +1,11 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.packaging
 
+import com.intellij.openapi.components.service
 import com.intellij.spellchecker.dictionary.Dictionary
 import com.intellij.spellchecker.dictionary.RuntimeDictionaryProvider
 import com.jetbrains.python.PyBundle.message
+import com.jetbrains.python.packaging.pip.PypiPackageCache
 import org.jetbrains.annotations.ApiStatus
 
 /**
@@ -14,7 +16,7 @@ import org.jetbrains.annotations.ApiStatus
 class PyPackagesDictionary : Dictionary {
   override fun getName(): String = message("python.packages.dictionary.name")
 
-  override fun contains(word: String): Boolean = PyPIPackageCache.getInstance().containsPackage(word)
+  override fun contains(word: String): Boolean = service<PypiPackageCache>().contains(word)
 
   override fun getWords(): MutableSet<String> = throw UnsupportedOperationException()
 
