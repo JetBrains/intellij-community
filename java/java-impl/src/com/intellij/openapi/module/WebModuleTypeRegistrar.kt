ba9055dcfd1e7a273eaeb2c0b61abfae8644b69f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-06-03 14:22:52 +0200

    convert WebModuleTypeRegistrar to kotlin to use non-blocking API
    GitOrigin-RevId: 5d0d0bcd9c19d4ff437e18a684990a7292bc4bc6

--- java/java-impl/src/com/intellij/openapi/module/WebModuleTypeRegistrar.kt
+++ java/java-impl/src/com/intellij/openapi/module/WebModuleTypeRegistrar.kt
@@ -1,30 +1,29 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.openapi.module;
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.module
 
-import com.intellij.openapi.Disposable;
+import com.intellij.openapi.Disposable
 
 /**
- * Registers {@link WebModuleType} in IDEs which don't register neither {@link WebModuleType} nor {@link com.intellij.webcore.moduleType.PlatformWebModuleType}
- * but still need to use {@link WebModuleTypeBase} because they have Java plugin installed.
+ * Registers [WebModuleType] in IDEs which don't register neither [WebModuleType] nor [com.intellij.webcore.moduleType.PlatformWebModuleType]
+ * but still need to use [WebModuleTypeBase] because they have Java plugin installed.
  */
-final class WebModuleTypeRegistrar implements Disposable {
-  private final WebModuleType myWebModuleType;
+internal class WebModuleTypeRegistrar : Disposable {
+  private val webModuleType: WebModuleType?
 
-  WebModuleTypeRegistrar() {
-    ModuleTypeManager moduleTypeManager = ModuleTypeManager.getInstance();
-    if (moduleTypeManager.findByID(WebModuleTypeBase.WEB_MODULE) instanceof UnknownModuleType) {
-      myWebModuleType = new WebModuleType();
-      moduleTypeManager.registerModuleType(myWebModuleType);
+  init {
+    val moduleTypeManager = ModuleTypeManager.getInstance()
+    if (moduleTypeManager.findByID(WebModuleTypeBase.WEB_MODULE) is UnknownModuleType) {
+      webModuleType = WebModuleType()
+      moduleTypeManager.registerModuleType(webModuleType)
     }
     else {
-      myWebModuleType = null;
+      webModuleType = null
     }
   }
 
-  @Override
-  public void dispose() {
-    if (myWebModuleType != null) {
-      ModuleTypeManager.getInstance().unregisterModuleType(myWebModuleType);
+  override fun dispose() {
+    if (webModuleType != null) {
+      ModuleTypeManager.getInstance().unregisterModuleType(webModuleType)
     }
   }
 }
