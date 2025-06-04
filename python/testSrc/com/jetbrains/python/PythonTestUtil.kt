5cf715172679ac9975a8206d8e580fc4c30cb970
Morgan Bartholomew <morgan.bartholomew@jetbrains.com>
2025-06-04 13:12:54 +1000

    [python] convert `PythonTestUtil` to kotlin
    GitOrigin-RevId: 41a7d112dc0d39bb2aa67045bac08bac1ec05b35

--- python/testSrc/com/jetbrains/python/PythonTestUtil.kt
+++ python/testSrc/com/jetbrains/python/PythonTestUtil.kt
@@ -1,12 +1,8 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.jetbrains.python;
+package com.jetbrains.python
+
+import com.jetbrains.python.PythonHelpersLocator.Companion.getPythonCommunityPath
 
 
-public final class PythonTestUtil {
-  private PythonTestUtil() {
-  }
-
-  public static String getTestDataPath() {
-    return PythonHelpersLocator.getPythonCommunityPath() + "/testData";
-  }
-}
+val testDataPath: String
+  get() = "${getPythonCommunityPath()}/testData"
