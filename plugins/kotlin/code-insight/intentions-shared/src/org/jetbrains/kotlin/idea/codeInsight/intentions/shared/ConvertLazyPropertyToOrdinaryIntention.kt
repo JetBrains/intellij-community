433c35c9751f3321056381d3bbbbf68bd8710302
Victoria.Petrakovich <victoria.petrakovich@jetbrains.com>
2025-06-04 16:04:58 +0200

    [kotlin] Move `ConvertLazyPropertyToOrdinaryIntention` to shared
    KTIJ-32008
    
    GitOrigin-RevId: 02fae13cbb7434050844987e730d4815bfa05992

--- plugins/kotlin/code-insight/intentions-shared/src/org/jetbrains/kotlin/idea/codeInsight/intentions/shared/ConvertLazyPropertyToOrdinaryIntention.kt
+++ plugins/kotlin/code-insight/intentions-shared/src/org/jetbrains/kotlin/idea/codeInsight/intentions/shared/ConvertLazyPropertyToOrdinaryIntention.kt
@@ -1,18 +1,20 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
-package org.jetbrains.kotlin.idea.intentions
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.codeInsight.intentions.shared
 
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiComment
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.codeinsight.api.classic.intentions.SelfTargetingIntention
-import org.jetbrains.kotlin.idea.inspections.collections.isCalling
 import org.jetbrains.kotlin.name.FqName
-import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtFunctionLiteral
+import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.KtPsiFactory
 import org.jetbrains.kotlin.psi.psiUtil.allChildren
 
 class ConvertLazyPropertyToOrdinaryIntention : SelfTargetingIntention<KtProperty>(
-    KtProperty::class.java, KotlinBundle.lazyMessage("convert.to.ordinary.property")
+  KtProperty::class.java, KotlinBundle.lazyMessage("convert.to.ordinary.property")
 ) {
     override fun isApplicableTo(element: KtProperty, caretOffset: Int): Boolean {
         val delegateExpression = element.delegate?.expression as? KtCallExpression ?: return false
@@ -26,7 +28,7 @@ class ConvertLazyPropertyToOrdinaryIntention : SelfTargetingIntention<KtProperty
         val delegateExpression = delegate.expression as? KtCallExpression ?: return
         val functionLiteral = delegateExpression.functionLiteral() ?: return
         element.initializer = functionLiteral.singleStatement()
-            ?: KtPsiFactory(element.project).createExpression("run ${functionLiteral.text}")
+                              ?: KtPsiFactory(element.project).createExpression("run ${functionLiteral.text}")
         delegate.delete()
     }
 
