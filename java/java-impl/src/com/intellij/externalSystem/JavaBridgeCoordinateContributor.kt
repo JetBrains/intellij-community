99c62682dcd6fe16bcfa6430c7d67198db175cac
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2025-06-04 14:16:53 +0200

    [DependencySubstitution] new: add maven-like coordinates for dependency substitution based on workspace model entities
    These coordinates are expected to be used only for global library and module identification during the dependency substitution.
    
    ### Issues
      * IDEA-370483 Maven performance degradation related to library dependency substitution
    
    GitOrigin-RevId: 27df0b5ad9b51ce22bf4a88c850591ab600cdaaf

--- java/java-impl/src/com/intellij/externalSystem/JavaBridgeCoordinateContributor.kt
+++ java/java-impl/src/com/intellij/externalSystem/JavaBridgeCoordinateContributor.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.externalSystem
 
+import com.intellij.java.library.MavenCoordinates
 import com.intellij.openapi.externalSystem.model.project.ProjectCoordinate
 import com.intellij.openapi.externalSystem.service.project.ExternalSystemCoordinateContributor
 import com.intellij.platform.externalSystem.impl.dependencySubstitution.DependencySubstitutionCoordinateContributor
@@ -10,9 +11,16 @@ import com.intellij.platform.workspace.storage.impl.WorkspaceEntityBase
 import com.intellij.workspaceModel.ide.impl.legacyBridge.library.findLibraryBridge
 import com.intellij.workspaceModel.ide.legacyBridge.findModule
 
+private fun ProjectCoordinate.toMavenCoordinates(): MavenCoordinates? {
+  val groupId = groupId ?: return null
+  val artifactId = artifactId ?: return null
+  val version = version ?: return null
+  return MavenCoordinates(groupId, artifactId, version)
+}
+
 private class JavaBridgeCoordinateContributor : DependencySubstitutionCoordinateContributor {
 
-  override fun findModuleCoordinate(module: ModuleEntity): ProjectCoordinate? {
+  override fun findModuleCoordinate(module: ModuleEntity): MavenCoordinates? {
     if (module !is WorkspaceEntityBase) return null
     val moduleBridge = module.findModule(module.snapshot) ?: return null
 
@@ -21,10 +29,10 @@ private class JavaBridgeCoordinateContributor : DependencySubstitutionCoordinate
       result = result ?: contributor.findModuleCoordinate(moduleBridge)
     }
 
-    return result
+    return result?.toMavenCoordinates()
   }
 
-  override fun findLibraryCoordinate(library: LibraryEntity): ProjectCoordinate? {
+  override fun findLibraryCoordinate(library: LibraryEntity): MavenCoordinates? {
     if (library !is WorkspaceEntityBase) return null
     val libraryBridge = library.findLibraryBridge(library.snapshot) ?: return null
 
@@ -33,6 +41,6 @@ private class JavaBridgeCoordinateContributor : DependencySubstitutionCoordinate
       result = result ?: contributor.findLibraryCoordinate(libraryBridge)
     }
 
-    return result
+    return result?.toMavenCoordinates()
   }
 }
