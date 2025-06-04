58fb8ece5b3638c31e6699a5dd9ab5dfaf89ec0f
Sergey Pak <sergey.pak@jetbrains.com>
2025-06-04 21:07:09 +0200

    [settingsSync] support pending user action
    GitOrigin-RevId: cc5e70135f9c472ee98b45fd64638adc7e38976a

--- platform/settings-sync-core/src/com/intellij/settingsSync/core/config/SettingsSyncConfigurable.kt
+++ platform/settings-sync-core/src/com/intellij/settingsSync/core/config/SettingsSyncConfigurable.kt
@@ -43,7 +43,6 @@ import com.intellij.ui.components.DropDownLink
 import com.intellij.ui.dsl.builder.*
 import com.intellij.ui.dsl.builder.components.DslLabel
 import com.intellij.ui.dsl.builder.components.DslLabelType
-import com.intellij.ui.dsl.gridLayout.UnscaledGaps
 import com.intellij.ui.layout.ComponentPredicate
 import com.intellij.ui.layout.and
 import com.intellij.ui.layout.not
@@ -470,7 +469,7 @@ internal class SettingsSyncConfigurable(private val coroutineScope: CoroutineSco
       private var stepSelectedValue: UserProviderHolder? = null
       override fun onChosen(selectedValue: UserProviderHolder, finalChoice: Boolean): PopupStep<*>? {
         stepSelectedValue = selectedValue
-        return PopupStep.FINAL_CHOICE
+        return FINAL_CHOICE
       }
 
       override fun getTextFor(value: UserProviderHolder?): String {
@@ -561,6 +560,7 @@ internal class SettingsSyncConfigurable(private val coroutineScope: CoroutineSco
         }
       }
       userDropDownLink.selectedItem = selectedValue
+      refreshActionRequired()
       if (wasEnabled && !enableCheckbox.isSelected) {
         enableCheckbox.doClick()
       }
@@ -571,8 +571,8 @@ internal class SettingsSyncConfigurable(private val coroutineScope: CoroutineSco
 
   private fun updateUserAccountsList() {
     userAccountsList.clear()
-    val providersMap = RemoteCommunicatorHolder.getAvailableProviders().map { it.providerCode to it }.toMap()
-    providersMap.forEach { providerId, communicator ->
+    val providersMap = RemoteCommunicatorHolder.getAvailableProviders().associateBy { it.providerCode }
+    providersMap.forEach { (_, communicator) ->
       val authService = communicator.authService
       val providerName = authService.providerName
       authService.getAvailableUserAccounts().forEachIndexed { idx, account ->
@@ -665,12 +665,19 @@ internal class SettingsSyncConfigurable(private val coroutineScope: CoroutineSco
 
   private fun refreshActionRequired() {
     val currentStatus = SettingsSyncStatusTracker.getInstance().currentStatus
-    actionRequired.set(currentStatus is SettingsSyncStatusTracker.SyncStatus.ActionRequired)
-    if (currentStatus is SettingsSyncStatusTracker.SyncStatus.ActionRequired) {
-      actionRequiredAction = { currentStatus.execute(syncConfigPanel) }
-      actionRequiredLabel.text = currentStatus.message
-      actionRequiredButton.text = currentStatus.actionTitle
-      cellDropDownLink.comment?.text = message("sync.status.action.required.comment", currentStatus.actionTitle, currentStatus.message)
+    val userActionRequired: SettingsSyncStatusTracker.SyncStatus.ActionRequired? = userDropDownLink.selectedItem?.let {
+      RemoteCommunicatorHolder.getProvider(it.providerCode)?.authService?.getPendingUserAction(it.userId)
+    }
+    actionRequired.set(currentStatus is SettingsSyncStatusTracker.SyncStatus.ActionRequired || userActionRequired != null)
+    if (actionRequired.get()) {
+      val action = userActionRequired ?: currentStatus as? SettingsSyncStatusTracker.SyncStatus.ActionRequired ?: return
+      actionRequiredAction = {
+        action.execute(syncConfigPanel)
+        refreshActionRequired()
+      }
+      actionRequiredLabel.text = action.message
+      actionRequiredButton.text = action.actionTitle
+      cellDropDownLink.comment?.text = message("sync.status.action.required.comment", action.actionTitle, action.message)
     }
     else {
       cellDropDownLink.comment?.text = ""
@@ -722,7 +729,7 @@ internal class SettingsSyncConfigurable(private val coroutineScope: CoroutineSco
         return true
       }
       is Error -> {
-        if (updateResult != SettingsSyncEnabler.State.CANCELLED) {
+        if (updateResult != State.CANCELLED) {
           showErrorOnEDT(updateResult.message)
           return false
         }
