2fd5d32ae00830c83a97afd8e66c3fd7f87224a3
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2025-06-05 11:58:31 +0200

    [DependencySubstitution] fix: delete bridge coordinate contributor
    This coordinate contributor resolver library and module bridges. That gives negative performance impact on the dependency substitution.
    
    ### Issues
      * IDEA-370483 Maven performance degradation related to library dependency substitution
    
    GitOrigin-RevId: d6dc66dd184b995432c49c52d3e69c6eba7567b3

--- java/java-impl/src/com/intellij/externalSystem/JavaBridgeCoordinateContributor.kt
+++ java/java-impl/src/com/intellij/externalSystem/JavaBridgeCoordinateContributor.kt
@@ -8,19 +8,12 @@ import com.intellij.java.library.MavenCoordinates
 import com.intellij.openapi.externalSystem.model.project.LibraryData
 import com.intellij.openapi.externalSystem.model.project.ModuleData
 import com.intellij.openapi.externalSystem.model.project.ProjectCoordinate
-import com.intellij.openapi.externalSystem.service.project.ExternalSystemCoordinateContributor
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.roots.libraries.Library
-import com.intellij.platform.externalSystem.impl.dependencySubstitution.DependencySubstitutionCoordinateContributor
-import com.intellij.platform.workspace.jps.entities.LibraryEntity
-import com.intellij.platform.workspace.jps.entities.ModuleEntity
 import com.intellij.platform.workspace.jps.entities.modifyLibraryEntity
 import com.intellij.platform.workspace.jps.entities.modifyModuleEntity
-import com.intellij.platform.workspace.storage.impl.WorkspaceEntityBase
-import com.intellij.workspaceModel.ide.impl.legacyBridge.library.findLibraryBridge
 import com.intellij.workspaceModel.ide.legacyBridge.findLibraryEntity
-import com.intellij.workspaceModel.ide.legacyBridge.findModule
 import com.intellij.workspaceModel.ide.legacyBridge.findModuleEntity
 import org.jetbrains.annotations.ApiStatus
 
@@ -47,31 +40,4 @@ private fun ProjectCoordinate.toMavenCoordinates(): MavenCoordinates? {
   val artifactId = artifactId ?: return null
   val version = version ?: return null
   return MavenCoordinates(groupId, artifactId, version)
-}
-
-private class JavaBridgeCoordinateContributor : DependencySubstitutionCoordinateContributor {
-
-  override fun findModuleCoordinate(module: ModuleEntity): MavenCoordinates? {
-    if (module !is WorkspaceEntityBase) return null
-    val moduleBridge = module.findModule(module.snapshot) ?: return null
-
-    var result: ProjectCoordinate? = null
-    ExternalSystemCoordinateContributor.EP_NAME.forEachExtensionSafe { contributor ->
-      result = result ?: contributor.findModuleCoordinate(moduleBridge)
-    }
-
-    return result?.toMavenCoordinates()
-  }
-
-  override fun findLibraryCoordinate(library: LibraryEntity): MavenCoordinates? {
-    if (library !is WorkspaceEntityBase) return null
-    val libraryBridge = library.findLibraryBridge(library.snapshot) ?: return null
-
-    var result: ProjectCoordinate? = null
-    ExternalSystemCoordinateContributor.EP_NAME.forEachExtensionSafe { contributor ->
-      result = result ?: contributor.findLibraryCoordinate(libraryBridge)
-    }
-
-    return result?.toMavenCoordinates()
-  }
 }
