d1061e591db750ec3446553ef35806bf82f6a249
Eugene Zhuravlev <eugene.zhuravlev@jetbrains.com>
2025-06-05 16:19:34 +0000

    augment compiler's internal search sope with the data from the OutputFileSystem
    Merge-request: IJ-MR-164975
    Merged-by: Eugene Zhuravlev <eugene.zhuravlev@jetbrains.com>
    
    GitOrigin-RevId: e94040eb874b5512ccf96c7c997689c5196ac2ed

--- build/jvm-rules/jvm-inc-builder/src/com/intellij/tools/build/bazel/jvmIncBuilder/impl/KotlinCompilerRunner.java
+++ build/jvm-rules/jvm-inc-builder/src/com/intellij/tools/build/bazel/jvmIncBuilder/impl/KotlinCompilerRunner.java
@@ -2,6 +2,7 @@
 package com.intellij.tools.build.bazel.jvmIncBuilder.impl;
 
 import com.intellij.openapi.progress.ProcessCanceledException;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.tools.build.bazel.jvmIncBuilder.*;
 import com.intellij.tools.build.bazel.jvmIncBuilder.runner.CompilerDataSink;
 import com.intellij.tools.build.bazel.jvmIncBuilder.runner.CompilerRunner;
@@ -124,11 +125,12 @@ public class KotlinCompilerRunner implements CompilerRunner {
       }
       K2JVMCompilerArguments kotlinArgs = buildKotlinCompilerArguments(myContext, sources);
       KotlinIncrementalCacheImpl incCache = new KotlinIncrementalCacheImpl(myStorageManager, filter(flat(deletedSources, sources), KotlinCompilerRunner::isKotlinSource), myModuleEntryPath, myLastGoodModuleEntryContent);
-      Services services = buildServices(kotlinArgs.getModuleName(), incCache);
+      OutputFileSystem outputFileSystem = new OutputFileSystem(new KotlinVirtualFileProvider(out));
+      Services services = buildServices(kotlinArgs.getModuleName(), incCache, outputFileSystem.root);
       MessageCollector messageCollector = new KotlinMessageCollector(diagnostic, this);
       // todo: make sure if we really need to process generated outputs after the compilation and not "in place"
       List<GeneratedClass> generatedClasses = new ArrayList<>();
-      AbstractCliPipeline<K2JVMCompilerArguments> pipeline = createPipeline(out, generatedFile -> {
+      AbstractCliPipeline<K2JVMCompilerArguments> pipeline = createPipeline(out, outputFileSystem.root, generatedFile -> {
         String jvmClassName = null;
         if (generatedFile instanceof KotlinJvmGeneratedFile jvmClass) {
           jvmClassName = jvmClass.getOutputClass().getClassName().getInternalName();
@@ -259,7 +261,7 @@ public class KotlinCompilerRunner implements CompilerRunner {
     }
   }
 
-  private Services buildServices(String moduleName, IncrementalCache cacheImpl) {
+  private Services buildServices(String moduleName, IncrementalCache cacheImpl, VirtualFile outputRoot) {
     Services.Builder builder = new Services.Builder();
     lookupTracker = new LookupTrackerImpl(LookupTracker.DO_NOTHING.INSTANCE);
     inlineConstTracker = new InlineConstTrackerImpl();
@@ -272,7 +274,7 @@ public class KotlinCompilerRunner implements CompilerRunner {
     builder.register(ImportTracker.class, importTracker);
     builder.register(
       IncrementalCompilationComponents.class,
-      new KotlinIncrementalCompilationComponents(moduleName, cacheImpl)
+      new KotlinIncrementalCompilationComponents(moduleName, cacheImpl, outputRoot)
     );
 
     builder.register(CompilationCanceledStatus.class, new CompilationCanceledStatus() {
@@ -287,15 +289,14 @@ public class KotlinCompilerRunner implements CompilerRunner {
     return builder.build();
   }
 
-  private AbstractCliPipeline<K2JVMCompilerArguments> createPipeline(OutputSink out, Consumer<GeneratedFile> outputItemCollector) throws IOException {
-    return new BazelJvmCliPipeline(createCompilerConfigurationUpdater(out), createOutputConsumer(out, outputItemCollector));
+  private AbstractCliPipeline<K2JVMCompilerArguments> createPipeline(OutputSink out, VirtualFile outputRoot, Consumer<GeneratedFile> outputItemCollector) throws IOException {
+    return new BazelJvmCliPipeline(createCompilerConfigurationUpdater(outputRoot), createOutputConsumer(out, outputItemCollector));
   }
 
-  private @NotNull Function1<? super @NotNull CompilerConfiguration, @NotNull Unit> createCompilerConfigurationUpdater(OutputSink out) throws IOException {
+  private @NotNull Function1<? super @NotNull CompilerConfiguration, @NotNull Unit> createCompilerConfigurationUpdater(VirtualFile outputRoot) throws IOException {
     var abiConsumer = createAbiOutputConsumer(myStorageManager.getAbiOutputBuilder());
     return configuration -> {
-      OutputFileSystem outputFileSystem = new OutputFileSystem(new KotlinVirtualFileProvider(out));
-      configuration.add(CLIConfigurationKeys.CONTENT_ROOTS, new VirtualJvmClasspathRoot(outputFileSystem.root, false, true));
+      configuration.add(CLIConfigurationKeys.CONTENT_ROOTS, new VirtualJvmClasspathRoot(outputRoot, false, true));
       configurePlugins(myPluginIdToPluginClasspath, myContext.getBaseDir(), abiConsumer, registeredPluginInfo -> {
         CompilerPluginRegistrar registrar = Objects.requireNonNull(registeredPluginInfo.getCompilerPluginRegistrar());
         configuration.add(CompilerPluginRegistrar.Companion.getCOMPILER_PLUGIN_REGISTRARS(), registrar);
--- build/jvm-rules/jvm-inc-builder/src/com/intellij/tools/build/bazel/jvmIncBuilder/impl/KotlinIncrementalCompilationComponents.java
+++ build/jvm-rules/jvm-inc-builder/src/com/intellij/tools/build/bazel/jvmIncBuilder/impl/KotlinIncrementalCompilationComponents.java
@@ -1,19 +1,33 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.tools.build.bazel.jvmIncBuilder.impl;
 
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinToJVMBytecodeCompiler;
+import org.jetbrains.kotlin.cli.jvm.compiler.PsiBasedProjectFileSearchScope;
+import org.jetbrains.kotlin.cli.jvm.compiler.VfsBasedProjectEnvironment;
+import org.jetbrains.kotlin.cli.jvm.compiler.legacy.pipeline.ProjectFileSearchScopeProvider;
+import org.jetbrains.kotlin.fir.session.environment.AbstractProjectFileSearchScope;
 import org.jetbrains.kotlin.load.kotlin.incremental.components.IncrementalCache;
 import org.jetbrains.kotlin.load.kotlin.incremental.components.IncrementalCompilationComponents;
 import org.jetbrains.kotlin.modules.TargetId;
 
+import java.util.Set;
 
-public class KotlinIncrementalCompilationComponents implements IncrementalCompilationComponents {
+public class KotlinIncrementalCompilationComponents implements IncrementalCompilationComponents, ProjectFileSearchScopeProvider {
   private final String moduleName;
   private final IncrementalCache cache;
+  private final VirtualFile myOutputRoot;
 
-  public KotlinIncrementalCompilationComponents(String moduleName, IncrementalCache cache) {
+  public KotlinIncrementalCompilationComponents(String moduleName, IncrementalCache cache, @NotNull VirtualFile outputRoot) {
     this.moduleName = moduleName;
     this.cache = cache;
+    myOutputRoot = outputRoot;
+  }
+
+  @Override
+  public @NotNull AbstractProjectFileSearchScope createSearchScope(@NotNull VfsBasedProjectEnvironment pe) {
+    return new PsiBasedProjectFileSearchScope(new KotlinToJVMBytecodeCompiler.DirectoriesScope(pe.getProject(), Set.of(myOutputRoot)));
   }
 
   @Override
