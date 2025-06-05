0a6d7f069c93dd0361691693b757e6e391a8e371
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2025-06-05 12:47:41 +0200

    IJPL-158622 IJPL-190518 IJPL-190623 Search Everywhere for RD: disable for CWM client
    GitOrigin-RevId: bc9a0bc6c7373fdc0a265c577fc871f5a2a486e9

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereFeature.kt
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereFeature.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.actions.searcheverywhere
 
+import com.intellij.frontend.FrontendApplicationInfo
+import com.intellij.frontend.FrontendType
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.util.PlatformUtils
 import org.jetbrains.annotations.ApiStatus
@@ -9,7 +11,13 @@ import org.jetbrains.annotations.ApiStatus
 object SearchEverywhereFeature {
   val isSplit: Boolean get() = Registry.`is`(registryKey, false)
 
+  private val isCwmClient: Boolean get() {
+    val frontendType = FrontendApplicationInfo.getFrontendType()
+    return (frontendType is FrontendType.RemoteDev && !frontendType.isLuxSupported)
+  }
+
   val registryKey: String get() =
     if (PlatformUtils.isRider()) "search.everywhere.new.rider.enabled"
+    else if (isCwmClient) "search.everywhere.new.cwm.client.enabled"
     else "search.everywhere.new.enabled"
 }
