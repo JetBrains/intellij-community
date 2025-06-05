1221ca0db45bff33f7086ba0dec51e90e1a17cab
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2025-06-05 21:14:50 +0200

    [vfs][refactoring] move VfsImplUtil `platform.ide.core.impl`->`platform.analysis`
    + the only VFS class in `platform.ide.core.impl`, too lonely there
    
    GitOrigin-RevId: 887f4e01eaf1401cb3608d3b39353e40667eb8d0

--- platform/analysis-api/src/com/intellij/openapi/vfs/newvfs/VfsImplUtil.java
+++ platform/analysis-api/src/com/intellij/openapi/vfs/newvfs/VfsImplUtil.java
@@ -35,7 +35,7 @@ public final class VfsImplUtil {
   private static final String FILE_SEPARATORS = "/" + (File.separatorChar == '/' ? "" : File.separator);
 
   private VfsImplUtil() {
-    throw new AssertionError("Static utils class: not for instantiation");
+    throw new AssertionError("Not for instantiation");
   }
 
   public static @Nullable NewVirtualFile findFileByPath(@NotNull NewVirtualFileSystem vfs, @NotNull String path) {
@@ -45,20 +45,9 @@ public final class VfsImplUtil {
     NewVirtualFile file = rootAndPath.first;
     for (String pathElement : rootAndPath.second) {
       if (pathElement.isEmpty() || ".".equals(pathElement)) continue;
-      NewVirtualFile fileBefore = file;
-      //Here we do 'partial canonicalization' of the path: we resolve symlinks, but only before '..' segments.
-      // It makes the resolution a bit surprising: 'a/b/c' and 'a/../a/b/c' could be resolved to different files, if 'a'
-      // is a symlink. But this follows the POSIX rules for path resolution, there symlinks indeed resolved as soon, as
-      // they are met in the path.
-      //Still, the result differs from regular canonicalization, via VirtualFile.getCanonicalPath() or Path.toRealPath(),
-      // there _all_ symlinks are resolved -- and the result also differs from Path.toRealPath(NOFOLLOW_LINKS) where _none_
-      // of symlinks are resolved.
       if ("..".equals(pathElement)) {
         if (file.is(VFileProperty.SYMLINK)) {
           NewVirtualFile canonicalFile = file.getCanonicalFile();
-          if (LOG.isDebugEnabled()) {
-            LOG.debug("[" + file.getPath() + "]: symlink to [" + canonicalFile + "]");
-          }
           file = canonicalFile != null ? canonicalFile.getParent() : null;
         }
         else {
@@ -67,10 +56,6 @@ public final class VfsImplUtil {
       }
       else {
         file = file.findChild(pathElement);
-      }
-
-      if (LOG.isDebugEnabled()) {
-        LOG.debug("[" + fileBefore.getPath() + "]/[" + pathElement + "] resolved to [" + file + "]");
       }
 
       if (file == null) return null;
@@ -83,8 +68,7 @@ public final class VfsImplUtil {
     return findCachedFileByPath(vfs, path).first;
   }
 
-  public static @NotNull Pair<NewVirtualFile, NewVirtualFile> findCachedFileByPath(@NotNull NewVirtualFileSystem vfs,
-                                                                                   @NotNull String path) {
+  public static @NotNull Pair<NewVirtualFile, NewVirtualFile> findCachedFileByPath(@NotNull NewVirtualFileSystem vfs, @NotNull String path) {
     Pair<NewVirtualFile, Iterable<String>> rootAndPath = prepare(vfs, path);
     if (rootAndPath == null) return Pair.empty();
 
@@ -144,9 +128,7 @@ public final class VfsImplUtil {
 
   /** An experimental refresh-and-find routine that doesn't require a write-lock (and hence EDT). */
   @ApiStatus.Experimental
-  public static void refreshAndFindFileByPath(@NotNull NewVirtualFileSystem vfs,
-                                              @NotNull String path,
-                                              @NotNull Consumer<? super @Nullable NewVirtualFile> consumer) {
+  public static void refreshAndFindFileByPath(@NotNull NewVirtualFileSystem vfs, @NotNull String path, @NotNull Consumer<? super @Nullable NewVirtualFile> consumer) {
     ProcessIOExecutorService.INSTANCE.execute(() -> {
       Pair<NewVirtualFile, Iterable<String>> rootAndPath = prepare(vfs, path);
       if (rootAndPath == null) {
@@ -158,9 +140,7 @@ public final class VfsImplUtil {
     });
   }
 
-  private static void refreshAndFindFileByPath(@Nullable NewVirtualFile file,
-                                               @NotNull Iterator<String> path,
-                                               @NotNull Consumer<? super @Nullable NewVirtualFile> consumer) {
+  private static void refreshAndFindFileByPath(@Nullable NewVirtualFile file, @NotNull Iterator<String> path, @NotNull Consumer<? super @Nullable NewVirtualFile> consumer) {
     if (file == null || !path.hasNext()) {
       consumer.accept(file);
       return;
@@ -174,8 +154,7 @@ public final class VfsImplUtil {
       if (file.is(VFileProperty.SYMLINK)) {
         String canonicalPath = file.getCanonicalPath();
         if (canonicalPath != null) {
-          refreshAndFindFileByPath(file.getFileSystem(), canonicalPath,
-                                   canonicalFile -> refreshAndFindFileByPath(canonicalFile, path, consumer));
+          refreshAndFindFileByPath(file.getFileSystem(), canonicalPath, canonicalFile -> refreshAndFindFileByPath(canonicalFile, path, consumer));
         }
         else {
           consumer.accept(null);
@@ -192,25 +171,20 @@ public final class VfsImplUtil {
       }
       else {
         file.refresh(true, false, () -> ProcessIOExecutorService.INSTANCE.execute(() ->
-                                                                                    refreshAndFindFileByPath(file.findChild(pathElement),
-                                                                                                             path, consumer)
+          refreshAndFindFileByPath(file.findChild(pathElement), path, consumer)
         ));
       }
     }
   }
 
-  /** Splits the path into (root, collection of path segments from the root) */
-  private static @Nullable Pair<@NotNull NewVirtualFile, @NotNull Iterable<String>> prepare(@NotNull NewVirtualFileSystem vfs,
-                                                                                            @NotNull String path) {
+  private static @Nullable Pair<NewVirtualFile, Iterable<String>> prepare(@NotNull NewVirtualFileSystem vfs, @NotNull String path) {
     PathFromRoot pair = extractRootFromPath(vfs, path);
     if (pair == null) return null;
     Iterable<String> parts = StringUtil.tokenize(pair.pathFromRoot(), FILE_SEPARATORS);
     return Pair.create(pair.root(), parts);
   }
 
-  public record PathFromRoot(@NotNull NewVirtualFile root, @NotNull String pathFromRoot) {
-  }
-
+  public record PathFromRoot(@NotNull NewVirtualFile root, @NotNull String pathFromRoot) {}
   /**
    * Returns a (file system root, relative path inside that root) pair, or {@code null} when the path is invalid or the root is not found.
    * For example, {@code extractRootFromPath(LocalFileSystem.getInstance, "C:/temp")} -> ("C:", "/temp")
@@ -261,15 +235,13 @@ public final class VfsImplUtil {
    * </pre></code>
    */
   public static void forceSyncRefresh(@NotNull VirtualFile file) {
-    var event = new VFileContentChangeEvent(REFRESH_REQUESTOR, file, file.getModificationStamp(),
-                                            VFileContentChangeEvent.UNDEFINED_TIMESTAMP_OR_LENGTH);
+    var event = new VFileContentChangeEvent(REFRESH_REQUESTOR, file, file.getModificationStamp(), VFileContentChangeEvent.UNDEFINED_TIMESTAMP_OR_LENGTH);
     RefreshQueue.getInstance().processEvents(false, List.of(event));
   }
 
   private static final AtomicBoolean ourSubscribed = new AtomicBoolean(false);
   private static final Object ourLock = new Object();
-  private static final Map<String, Pair<ArchiveFileSystem, ArchiveHandler>> ourHandlerCache = CollectionFactory.createFilePathMap();
-  // guarded by ourLock
+  private static final Map<String, Pair<ArchiveFileSystem, ArchiveHandler>> ourHandlerCache = CollectionFactory.createFilePathMap(); // guarded by ourLock
   private static final Map<String, Set<String>> ourDominatorsMap = CollectionFactory.createFilePathMap(); // guarded by ourLock; values too
 
   public static @NotNull <T extends ArchiveHandler> T getHandler(@NotNull ArchiveFileSystem vfs,
@@ -390,8 +362,7 @@ public final class VfsImplUtil {
       if (myRootsToRefresh != null) {
         List<@NotNull NewVirtualFile> rootsToRefresh = ContainerUtil.mapNotNull(
           myRootsToRefresh,
-          pathAndFs -> ManagingFS.getInstance()
-            .findRoot(ArchiveFileSystem.composeRootPath(pathAndFs.second, pathAndFs.first), pathAndFs.second));
+          pathAndFs -> ManagingFS.getInstance().findRoot(ArchiveFileSystem.composeRootPath(pathAndFs.second, pathAndFs.first), pathAndFs.second));
         for (@NotNull NewVirtualFile root : rootsToRefresh) {
           root.markDirtyRecursively();
         }
@@ -421,12 +392,10 @@ public final class VfsImplUtil {
    * should generate {@link VFileDeleteEvent}('file://x.jar').</p>
    * (The latter might happen when someone explicitly called {@code fileInsideJar.refresh()} without refreshing jar file in local file system).
    */
-  public static @NotNull List<VFileEvent> getJarInvalidationEvents(@NotNull VFileEvent event,
-                                                                   @NotNull List<? super Runnable> outApplyActions) {
+  public static @NotNull List<VFileEvent> getJarInvalidationEvents(@NotNull VFileEvent event, @NotNull List<? super Runnable> outApplyActions) {
     if (!(event instanceof VFileDeleteEvent ||
           event instanceof VFileMoveEvent ||
-          event instanceof VFilePropertyChangeEvent propertyChangeEvent &&
-          VirtualFile.PROP_NAME.equals(propertyChangeEvent.getPropertyName()))) {
+          event instanceof VFilePropertyChangeEvent propertyChangeEvent && VirtualFile.PROP_NAME.equals(propertyChangeEvent.getPropertyName()))) {
       return Collections.emptyList();
     }
 
@@ -452,7 +421,7 @@ public final class VfsImplUtil {
     synchronized (ourLock) {
       Set<String> handlers = ourDominatorsMap.get(path);
       if (handlers == null) {
-        jarPaths = new String[]{path};
+        jarPaths = new String[] {path};
       }
       else {
         jarPaths = ArrayUtil.toStringArray(handlers);
@@ -491,9 +460,8 @@ public final class VfsImplUtil {
         // for "delete jar://x.jar!/" generate "delete file://x.jar", but
         // for "delete jar://x.jar!/web.xml" generate "changed file://x.jar"
         VFileEvent localJarDeleteEvent = file.getParent() == null ?
-                                         new VFileDeleteEvent(event.getRequestor(), local) :
-                                         new VFileContentChangeEvent(event.getRequestor(), local, local.getModificationStamp(),
-                                                                     local.getModificationStamp());
+           new VFileDeleteEvent(event.getRequestor(), local) :
+           new VFileContentChangeEvent(event.getRequestor(), local, local.getModificationStamp(), local.getModificationStamp());
         events.add(localJarDeleteEvent);
       }
     }
