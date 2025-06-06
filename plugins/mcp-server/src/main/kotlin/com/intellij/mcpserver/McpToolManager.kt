28e45a056de888a97a0a7448372b12d36aad1b95
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2025-06-06 03:25:58 +0200

    exclude several mcp tools from mcp server which were too experimental
    GitOrigin-RevId: 82081e83276fdbeb0553847f1050b09355434965

--- plugins/mcp-server/src/main/kotlin/com/intellij/mcpserver/McpToolManager.kt
+++ plugins/mcp-server/src/main/kotlin/com/intellij/mcpserver/McpToolManager.kt
@@ -1,36 +1,9 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.mcpserver
 
-import com.intellij.openapi.extensions.ExtensionPointName
-import com.intellij.mcpserver.general.CreateNewFileWithTextTool
-import com.intellij.mcpserver.general.ExecuteActionByIdTool
-import com.intellij.mcpserver.general.FindFilesByNameSubstring
-import com.intellij.mcpserver.general.GetAllOpenFilePathsTool
-import com.intellij.mcpserver.general.GetAllOpenFileTextsTool
-import com.intellij.mcpserver.general.GetCurrentFileErrorsTool
-import com.intellij.mcpserver.general.GetCurrentFilePathTool
-import com.intellij.mcpserver.general.GetCurrentFileTextTool
-import com.intellij.mcpserver.general.GetFileTextByPathTool
-import com.intellij.mcpserver.general.GetProblemsTools
-import com.intellij.mcpserver.general.GetProgressIndicatorsTool
-import com.intellij.mcpserver.general.GetProjectDependenciesTool
-import com.intellij.mcpserver.general.GetProjectModulesTool
-import com.intellij.mcpserver.general.GetRunConfigurationsTool
-import com.intellij.mcpserver.general.GetSelectedTextTool
-import com.intellij.mcpserver.general.ListAvailableActionsTool
-import com.intellij.mcpserver.general.ListDirectoryTreeInFolderTool
-import com.intellij.mcpserver.general.ListFilesInFolderTool
-import com.intellij.mcpserver.general.OpenFileInEditorTool
-import com.intellij.mcpserver.general.ReplaceCurrentFileTextTool
-import com.intellij.mcpserver.general.ReplaceSelectedTextTool
-import com.intellij.mcpserver.general.ReplaceSpecificTextTool
-import com.intellij.mcpserver.general.ReplaceTextByPathTool
-import com.intellij.mcpserver.general.RunConfigurationTool
-import com.intellij.mcpserver.general.SearchInFilesContentTool
-import com.intellij.mcpserver.general.WaitTool
+import com.intellij.mcpserver.general.*
 import com.intellij.mcpserver.git.GetVcsStatusTool
-import com.intellij.mcpserver.general.ReformatCurrentFileTool
-import com.intellij.mcpserver.general.ReformatFileTool
+import com.intellij.openapi.extensions.ExtensionPointName
 
 class McpToolManager {
     companion object {
@@ -69,10 +42,11 @@ class McpToolManager {
             GetAllOpenFileTextsTool(),
             GetAllOpenFilePathsTool(),
             OpenFileInEditorTool(),
-            ListAvailableActionsTool(),
-            ExecuteActionByIdTool(),
-            GetProgressIndicatorsTool(),
-            WaitTool(),
+            // todo delete tools impl
+            //ListAvailableActionsTool(),
+            //ExecuteActionByIdTool(),
+            //GetProgressIndicatorsTool(),
+            //WaitTool(),
             GetCurrentFileErrorsTool(),
             ReformatCurrentFileTool(),
             ReformatFileTool(),
