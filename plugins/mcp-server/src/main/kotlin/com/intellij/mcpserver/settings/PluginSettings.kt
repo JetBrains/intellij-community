6ba117c5814a6da886ecbaa3220dcfbf6424d901
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2025-06-06 12:34:30 +0200

    improve mcp plugin settings
    GitOrigin-RevId: 91d6878fc1d2a956203c3623e674378ee5416aff

--- plugins/mcp-server/src/main/kotlin/com/intellij/mcpserver/settings/PluginSettings.kt
+++ plugins/mcp-server/src/main/kotlin/com/intellij/mcpserver/settings/PluginSettings.kt
@@ -2,15 +2,16 @@ package com.intellij.mcpserver.settings
 
 import com.intellij.openapi.components.BaseState
 import com.intellij.openapi.components.SimplePersistentStateComponent
-import com.intellij.openapi.components.Storage
 import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
 
-@State(name = "MyPluginSettings", storages = [Storage("mcpServer.xml")])
-class PluginSettings : SimplePersistentStateComponent<MyState>(MyState()){}
+@State(name = "McpServerPluginSettings", storages = [Storage("mcpServer.xml")])
+internal class PluginSettings : SimplePersistentStateComponent<MyState>(MyState()){}
 
-class MyState : BaseState() {
+internal class MyState : BaseState() {
     var shouldShowNodeNotification: Boolean by property(true)
     var shouldShowClaudeNotification: Boolean by property(true)
     var shouldShowClaudeSettingsNotification: Boolean by property(true)
     var enableBraveMode: Boolean by property(false)
+    var enableMcpServer: Boolean by property(false)
 }
--- plugins/mcp-server/src/main/kotlin/com/intellij/mcpserver/settings/PluginSettingsConfigurable.kt
+++ plugins/mcp-server/src/main/kotlin/com/intellij/mcpserver/settings/PluginSettingsConfigurable.kt
@@ -1,48 +1,59 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.mcpserver.settings
 
+import com.intellij.icons.AllIcons
+import com.intellij.mcpserver.McpServerBundle
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.options.SearchableConfigurable
 import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.dsl.builder.Cell
 import com.intellij.ui.dsl.builder.bindSelected
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.builder.selected
 import org.jetbrains.annotations.NonNls
-import com.intellij.mcpserver.McpServerBundle
 import javax.swing.JComponent
 
 class PluginSettingsConfigurable : SearchableConfigurable {
     private var settingsPanel: DialogPanel? = null
 
-    override fun getDisplayName(): String = McpServerBundle.message("configurable.name.mcp.plugin")
+  override fun getDisplayName(): String = McpServerBundle.message("configurable.name.mcp.plugin")
 
-    override fun createComponent(): JComponent {
-        val settings = ApplicationManager.getApplication().getService(PluginSettings::class.java)
+  override fun createComponent(): JComponent {
+    val settings = ApplicationManager.getApplication().getService(PluginSettings::class.java)
+    lateinit var enabledCheckBox: Cell<JBCheckBox>
 
-        val panel = panel {
-            group(McpServerBundle.message("border.title.notifications")) {
-                row {
-                    checkBox(McpServerBundle.message("checkbox.show.node.js.notifications"))
-                        .bindSelected(settings.state::shouldShowNodeNotification)
-                }
-                row {
-                    checkBox(McpServerBundle.message("checkbox.show.claude.notifications"))
-                        .bindSelected(settings.state::shouldShowClaudeNotification)
-                }
-                row {
-                    checkBox(McpServerBundle.message("checkbox.show.claude.settings.notifications"))
-                        .bindSelected(settings.state::shouldShowClaudeSettingsNotification)
-                }
-            }
-            group(McpServerBundle.message("border.title.terminal.commands")) {
-                row {
-                    checkBox(McpServerBundle.message("checkbox.enable.brave.mode.skip.command.execution.confirmations"))
-                        .bindSelected(settings.state::enableBraveMode)
-                }
-                row {
-                    comment(McpServerBundle.message("text.warning.enabling.brave.mode.will.allow.terminal.commands.to.execute.without.confirmation.use.with.caution"))
-                }
-            }
+    val panel = panel {
+      row {
+        enabledCheckBox = checkBox(McpServerBundle.message("enable.mcp.server")).bindSelected(settings.state::enableMcpServer)
+      }
+      panel {
+        group(McpServerBundle.message("border.title.notifications")) {
+          row {
+            checkBox(McpServerBundle.message("checkbox.show.node.js.notifications"))
+              .bindSelected(settings.state::shouldShowNodeNotification)
+          }
+          row {
+            checkBox(McpServerBundle.message("checkbox.show.claude.notifications"))
+              .bindSelected(settings.state::shouldShowClaudeNotification)
+          }
+          row {
+            checkBox(McpServerBundle.message("checkbox.show.claude.settings.notifications"))
+              .bindSelected(settings.state::shouldShowClaudeSettingsNotification)
+          }
         }
+        group(McpServerBundle.message("border.title.terminal.commands")) {
+          row {
+            checkBox(McpServerBundle.message("checkbox.enable.brave.mode.skip.command.execution.confirmations"))
+              .bindSelected(settings.state::enableBraveMode)
+          }
+          row {
+            icon(AllIcons.General.Warning)
+            comment(McpServerBundle.message("text.warning.enabling.brave.mode.will.allow.terminal.commands.to.execute.without.confirmation.use.with.caution"))
+          }
+        }
+      }.enabledIf(enabledCheckBox.selected)
+    }
         
         settingsPanel = panel
         return panel
