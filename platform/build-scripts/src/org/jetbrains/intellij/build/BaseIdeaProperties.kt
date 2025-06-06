2785197f2aa5bccd96480d6e881cc461ebd8c618
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2025-06-06 10:57:30 +0200

    bundle mcp server plugin to ide-s
    GitOrigin-RevId: ca966482b4351da706c371a729272da4f2354d92

--- platform/build-scripts/src/org/jetbrains/intellij/build/BaseIdeaProperties.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/BaseIdeaProperties.kt
@@ -52,6 +52,7 @@ val IDEA_BUNDLED_PLUGINS: PersistentList<String> = DEFAULT_BUNDLED_PLUGINS + seq
   "intellij.statsCollector",
   "intellij.sh",
   "intellij.markdown",
+  "intellij.mcpserver",
   "intellij.webp",
   "intellij.grazie",
   "intellij.featuresTrainer",
