c633a8d11c3057bee348c082c2f583ab514384f0
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2025-06-08 15:23:22 +0200

    [terminal] IJPL-183169 fix `BasicShellTerminalIntegrationTest` on Windows/PowerShell
    GitOrigin-RevId: e95e2f80b89d71ec8f7325b6d28553f166b511fa

--- plugins/terminal/tests/org/jetbrains/plugins/terminal/classic/BasicShellTerminalIntegrationTest.kt
+++ plugins/terminal/tests/org/jetbrains/plugins/terminal/classic/BasicShellTerminalIntegrationTest.kt
@@ -9,29 +9,36 @@ import org.jetbrains.plugins.terminal.JBTerminalSystemSettingsProvider
 import org.jetbrains.plugins.terminal.ShellTerminalWidget
 import org.jetbrains.plugins.terminal.classic.fixture.TestShellSession
 import org.jetbrains.plugins.terminal.classic.fixture.TestTerminalBufferWatcher
-import org.junit.Assume
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 
 class BasicShellTerminalIntegrationTest : BasePlatformTestCase() {
   fun testEchoAndClear() {
-    Assume.assumeFalse(SystemInfo.isWindows)
-
     val session = TestShellSession(project, testRootDisposable)
-    session.executeCommand("_MY_FOO=test; echo -e \"1\\n2\\n\$_MY_FOO\"")
+    val command = if (SystemInfo.isWindows) {
+      $$"$env:_MY_FOO = 'test'; echo \"1`n2`n$env:_MY_FOO\""
+    }
+    else {
+      $$"_MY_FOO=test; echo -e \"1\\n2\\n$_MY_FOO\""
+    }
+    session.executeCommand(command)
     session.awaitScreenLinesEndWith(listOf("1", "2", "test"), 10000)
     session.executeCommand("clear")
     session.awaitScreenLinesAre(emptyList(), 10000)
   }
 
   fun testCommandsExecuteInOrder() {
-    Assume.assumeFalse(SystemInfo.isWindows)
-
     val outputFile = Files.createTempFile("output", ".txt")
     val widget = ShellTerminalWidget(project, JBTerminalSystemSettingsProvider(), testRootDisposable)
     val commandCount = 10
     for (i in 1..commandCount) {
-      widget.executeCommand("echo " + i + " >> " + CommandLineUtil.posixQuote(outputFile.toString()))
+      if (SystemInfo.isWindows) {
+        // PowerShell 5.1 has UTF-16LE output encoding by default
+        widget.executeCommand("Add-Content -Path '$outputFile' -Value $i -Encoding ASCII")
+      }
+      else {
+        widget.executeCommand("echo " + i + " >> " + CommandLineUtil.posixQuote(outputFile.toString()))
+      }
     }
     TestShellSession.start(widget)
     val finishMarker = "All commands have been executed"
@@ -40,7 +47,8 @@ class BasicShellTerminalIntegrationTest : BasePlatformTestCase() {
     }
     val watcher = TestTerminalBufferWatcher(widget.terminalTextBuffer, widget.terminal)
     watcher.awaitScreenLinesEndWith(listOf(finishMarker), 60_000)
-    val actualOutputLines = Files.readAllLines(outputFile, StandardCharsets.UTF_8)
+    val actualOutputLines = Files.readAllLines(outputFile, StandardCharsets.US_ASCII)
+
     outputFile.delete()
     val expectedOutputLines: List<String> = (1..commandCount).map { it.toString() }
     assertEquals(expectedOutputLines, actualOutputLines)
