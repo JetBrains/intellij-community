a0231c334a3551f879959c105dd367edc1922d90
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2025-06-08 17:33:51 +0200

    PY-77226: Introduce `InterpreterService`.
    This service is aimed to substitute various SDK-management tools in PyCharm.
    
    Begin with `InterpreterService()` function.
    
    GitOrigin-RevId: 368d56c4a78812fe81de941e5e5ce61a56d385e6

---
+++ python/interpreters/src/advancedApi/advancedApi.kt
@@ -0,0 +1,62 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.python.community.interpreters.advancedApi
+
+import com.intellij.python.community.execService.*
+import com.intellij.python.community.execService.python.HelperName
+import com.intellij.python.community.execService.python.advancedApi.executeHelperAdvanced
+import com.intellij.python.community.execService.python.advancedApi.executePythonAdvanced
+import com.intellij.python.community.execService.python.advancedApi.validatePythonAndGetVersion
+import com.intellij.python.community.interpreters.ValidInterpreter
+import com.jetbrains.python.errorProcessing.PyExecResult
+import com.jetbrains.python.errorProcessing.PyResult
+import com.jetbrains.python.psi.LanguageLevel
+import org.jetbrains.annotations.ApiStatus
+
+// This in advanced API, most probably you need "api.kt"
+
+/**
+ * Execute [python]
+ */
+suspend fun <T> ExecService.executePythonAdvanced(
+  python: ValidInterpreter,
+  argsBuilder: suspend ArgsBuilder.() -> Unit = {},
+  options: ExecOptions = ExecOptions(),
+  processInteractiveHandler: ProcessInteractiveHandler<T>,
+): PyResult<T> =
+  executePythonAdvanced(python.asExecutablePython, argsBuilder, options, processInteractiveHandler)
+
+
+/**
+ * Execute [helper] on [python]. For remote eels, [helper] is copied (but only one file!).
+ */
+suspend fun <T> ExecService.executeHelperAdvanced(
+  python: ValidInterpreter,
+  helper: HelperName,
+  args: List<String> = emptyList(),
+  options: ExecOptions = ExecOptions(),
+  procListener: PyProcessListener? = null,
+  processOutputTransformer: ProcessOutputTransformer<T>,
+): PyResult<T> = executeHelperAdvanced(python.asExecutablePython, helper, args, options, procListener, processOutputTransformer)
+
+/**
+ * Ensures that this python is executable and returns its version. Error if python is broken.
+ *
+ * Some pythons might be broken: they may be executable, even return a version, but still fail to execute it.
+ * As we need workable pythons, we validate it by executing
+ */
+@ApiStatus.Internal
+suspend fun ValidInterpreter.validatePythonAndGetVersion(): PyResult<LanguageLevel> =
+  ExecService().validatePythonAndGetVersion(asExecutablePython)
+
+/**
+ * Execute [helper] on [python]. For remote eels, [helper] is copied (but only one file!).
+ * Returns `stdout`
+ */
+suspend fun ExecService.executeHelper(
+  python: ValidInterpreter,
+  helper: HelperName,
+  args: List<String> = emptyList(),
+  options: ExecOptions = ExecOptions(),
+  procListener: PyProcessListener? = null,
+): PyResult<String> =
+  executeHelperAdvanced(python.asExecutablePython, helper, args, options, procListener, ZeroCodeStdoutTransformer)
---
+++ python/interpreters/src/impl/InterpreterImpl.kt
@@ -0,0 +1,60 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.python.community.interpreters.impl
+
+import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.python.community.execService.python.advancedApi.ExecutablePython
+import com.intellij.python.community.interpreters.Interpreter
+import com.intellij.python.community.interpreters.InvalidInterpreter
+import com.intellij.python.community.interpreters.ValidInterpreter
+import com.intellij.python.community.services.shared.PythonWithName
+import com.intellij.python.community.services.shared.UICustomization
+import com.jetbrains.python.psi.LanguageLevel
+import com.jetbrains.python.sdk.PythonSdkAdditionalData
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.Nls
+import java.util.*
+
+internal class ValidInterpreterImpl(
+  override val languageLevel: LanguageLevel,
+  override val asExecutablePython: ExecutablePython,
+  private val mixin: SdkMixin,
+  override val ui: UICustomization?,
+) : ValidInterpreter, InterpreterFields by mixin {
+  override fun toString(): String {
+    return "ValidInterpreterImpl(languageLevel=$languageLevel, asExecutablePython=$asExecutablePython)"
+  }
+}
+
+
+internal class InvalidInterpreterImpl(
+  private val mixin: SdkMixin,
+  override val invalidMessage: @Nls String,
+) : InvalidInterpreter, InterpreterFields by mixin {
+  override fun toString(): String {
+    return "InvalidInterpreterImpl(invalidMessage='$invalidMessage')"
+  }
+}
+
+@ApiStatus.NonExtendable
+interface InterpreterFields : PythonWithName {
+  /**
+   * Currently, interpreters are based on SDK. But this is an implementation detail to be changed soon.
+   * Do not use this field unless absolutely necessary.
+   */
+  val sdk: Sdk
+  val id: UUID
+}
+
+internal class SdkMixin(override val sdk: Sdk, data: PythonSdkAdditionalData) : InterpreterFields {
+  override val id: UUID = data.uuid
+
+  override suspend fun getReadableName(): @Nls String = sdk.name
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other !is Interpreter) return false
+
+    return id == other.id
+  }
+
+  override fun hashCode(): Int = id.hashCode()
+}
