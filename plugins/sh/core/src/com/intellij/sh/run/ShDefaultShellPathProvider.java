806acfc74d4776f377531dfaffde2be2bf6db8f8
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2025-06-11 00:35:41 +0200

    [sh] workaround `ShTerminalShellPathProvider.getDefaultShell` to make it callable on EDT
    Otherwise, `com.intellij.idea.ultimate.build.tests.buildScripts.IdeaUltimateBuildTest.ultimateBuild(search_index)` and other IDE build tests are failing.
    
    This stuff should be revisited later. Storing the default shell in the template Shell run configuration feels wrong.
    
    GitOrigin-RevId: f47bf4a1354a3c5d4ed59f2d14e7e038472170a9

--- plugins/sh/core/src/com/intellij/sh/run/ShDefaultShellPathProvider.java
+++ plugins/sh/core/src/com/intellij/sh/run/ShDefaultShellPathProvider.java
@@ -3,5 +3,9 @@ package com.intellij.sh.run;
 import org.jetbrains.annotations.NotNull;
 
 public interface ShDefaultShellPathProvider {
+  /**
+   * @deprecated should be converted to suspend API
+   */
+  @Deprecated
   @NotNull String getDefaultShell();
 }
