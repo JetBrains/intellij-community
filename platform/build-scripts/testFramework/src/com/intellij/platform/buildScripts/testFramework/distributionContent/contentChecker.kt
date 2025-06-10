c3e24a2ebd9a45a4ef50e6e421fcda93639b8c9a
Vladislav Yaroshchuk <vladislav.yaroshchuk@jetbrains.com>
2025-06-10 07:44:33 +0000

    [build-scripts] IJI-2659 checkThatContentIsNotChanged: enhance error message
    Add a note about PatronusConfigYamlConsistencyTest to the test error mess
    
    GitOrigin-RevId: 5822a697445f343bd9bbe55daf2c9bd93e498f81

--- platform/build-scripts/testFramework/src/com/intellij/platform/buildScripts/testFramework/distributionContent/contentChecker.kt
+++ platform/build-scripts/testFramework/src/com/intellij/platform/buildScripts/testFramework/distributionContent/contentChecker.kt
@@ -62,6 +62,7 @@ fun checkThatContentIsNotChanged(
     "or someone else from the Core team to approve changes.\n\n" +
     "Please do not push changes without approval.\n" +
     "For more details, please visit https://youtrack.jetbrains.com/articles/IDEA-A-80/Distribution-Content-Approving.\n\n" +
+    "When the patch is applied (after approval), please also run PatronusConfigYamlConsistencyTest to ensure the Patronus configuration consistency.\n\n" +
     "Patch:\n${DiffUtils.generateUnifiedDiff(fileName, fileName, expectedLines, patch, 3).joinToString(separator = "\n")}"
   }
   else {
@@ -69,6 +70,7 @@ fun checkThatContentIsNotChanged(
     "If you are sure that the difference is as expected, please apply and commit a new snapshot.\n" +
     "Approval is not required. For more details, please visit https://youtrack.jetbrains.com/articles/IDEA-A-80/Distribution-Content-Approving.\n\n" +
     "Please copy the patch below and apply it, or open the Diff Viewer to accept the proposed changes.\n\n" +
+    "When the patch is applied, please also run PatronusConfigYamlConsistencyTest to ensure the Patronus configuration consistency.\n\n" +
     "Patch:\n${DiffUtils.generateUnifiedDiff(fileName, fileName, expectedLines, patch, 3).joinToString(separator = "\n")}"
   }
 
