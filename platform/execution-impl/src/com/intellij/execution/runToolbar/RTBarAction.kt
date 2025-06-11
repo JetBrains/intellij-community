bc4bcb67bd15a31692e32bf31526d142ac9c19e5
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-06-11 09:29:00 +0200

    IJPL-191239 reduce visibility
    GitOrigin-RevId: 0ecc507b6bd80390684f4cea51513bd3f5850c32

--- platform/execution-impl/src/com/intellij/execution/runToolbar/RTBarAction.kt
+++ platform/execution-impl/src/com/intellij/execution/runToolbar/RTBarAction.kt
@@ -1,9 +1,8 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.runToolbar
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.diagnostic.Logger
-import org.jetbrains.annotations.ApiStatus
 
 interface RTBarAction {
   enum class Type {
@@ -13,11 +12,11 @@ interface RTBarAction {
     FLEXIBLE
   }
 
-  fun traceLog(logger: Logger, e: AnActionEvent, additionalInfo: String?=null) {
-/*    if (RunToolbarProcess.logNeeded) {
-      logger.info(
-        "RunToolbar UPDATE slot: ${e.id()} ${"visible: ${e.presentation.isVisible}, enable: ${e.presentation.isEnabled}"}; ${additionalInfo ?: ""} ")
-    }*/
+  fun traceLog(logger: Logger, e: AnActionEvent, additionalInfo: String? = null) {
+    /*    if (RunToolbarProcess.logNeeded) {
+          logger.info(
+            "RunToolbar UPDATE slot: ${e.id()} ${"visible: ${e.presentation.isVisible}, enable: ${e.presentation.isEnabled}"}; ${additionalInfo ?: ""} ")
+        }*/
   }
 
   fun getRightSideType(): Type = Type.STABLE
@@ -25,8 +24,7 @@ interface RTBarAction {
   fun checkMainSlotVisibility(state: RunToolbarMainSlotState): Boolean
 }
 
-@ApiStatus.Internal
-interface RTRunConfiguration : RTBarAction {
+internal interface RTRunConfiguration : RTBarAction {
   override fun getRightSideType(): RTBarAction.Type = RTBarAction.Type.STABLE
   fun isStable(): Boolean {
     return getRightSideType() == RTBarAction.Type.STABLE
