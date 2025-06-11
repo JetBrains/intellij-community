67460c03357b6251de6cfaf3ef3e9933c5ed443c
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2025-06-11 13:39:44 +0200

    [sh] IJ-CR-165354 link the issue
    GitOrigin-RevId: f5de48419ed8da5b97d7331730293cad7f71ce1b

--- plugins/sh/core/src/com/intellij/sh/run/ShDefaultShellPathProvider.java
+++ plugins/sh/core/src/com/intellij/sh/run/ShDefaultShellPathProvider.java
@@ -4,8 +4,9 @@ import org.jetbrains.annotations.NotNull;
 
 public interface ShDefaultShellPathProvider {
   /**
-   * @deprecated should be converted to suspend API
+   * @deprecated should be converted to suspend API (IJPL-191352)
    */
+  @SuppressWarnings("DeprecatedIsStillUsed")
   @Deprecated
   @NotNull String getDefaultShell();
 }
