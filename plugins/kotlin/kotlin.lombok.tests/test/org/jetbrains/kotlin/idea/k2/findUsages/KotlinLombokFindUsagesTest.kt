41c2e6350a31816eeccac7f5d9dd070e73e5a186
Anna Kozlova <anna.kozlova@jetbrains.com>
2025-06-11 18:04:24 +0200

    [kotlin] lombok: fix test name to be searchable by default
    GitOrigin-RevId: 7e680015321973d631c53187cf7f084be558743f

--- plugins/kotlin/kotlin.lombok.tests/test/org/jetbrains/kotlin/idea/k2/findUsages/KotlinLombokFindUsagesTest.kt
+++ plugins/kotlin/kotlin.lombok.tests/test/org/jetbrains/kotlin/idea/k2/findUsages/KotlinLombokFindUsagesTest.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.findUsages
+package org.jetbrains.kotlin.idea.k2.findUsages
 
 import com.intellij.testFramework.LightProjectDescriptor
 import com.intellij.testFramework.fixtures.impl.CodeInsightTestFixtureImpl
@@ -7,7 +7,7 @@ import de.plushnikov.intellij.plugin.LombokTestUtil
 import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginMode
 import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
 
-class KotlinLombokFindUsages: KotlinLightCodeInsightFixtureTestCase() {
+class KotlinLombokFindUsagesTest: KotlinLightCodeInsightFixtureTestCase() {
     override fun getProjectDescriptor(): LightProjectDescriptor {
         return LombokTestUtil.LOMBOK_NEW_DESCRIPTOR
     }
