1eae945baeabaa99c6c136110f3bd3e4b2d2d459
Marius Erath <marius.erath@jetbrains.com>
2025-06-16 23:29:13 +0200

    KMT-1009 Remove `debug`, `coverage`, `profiling` for Web run configurations
    Simplify check
    
    GitOrigin-RevId: 093fbbe98010c0973697fab4197de6ea36a3c089

--- plugins/gradle/java/src/execution/test/runner/GradleCoverageExtension.kt
+++ plugins/gradle/java/src/execution/test/runner/GradleCoverageExtension.kt
@@ -3,9 +3,7 @@ package org.jetbrains.plugins.gradle.execution.test.runner
 
 import com.intellij.coverage.JavaCoverageEngineExtension
 import com.intellij.execution.configurations.RunConfigurationBase
-import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunConfiguration
 import org.jetbrains.plugins.gradle.service.execution.GradleRunConfiguration
-import org.jetbrains.plugins.gradle.util.GradleConstants
 
 /**
  * Created by Nikita.Skvortsov
@@ -13,8 +11,7 @@ import org.jetbrains.plugins.gradle.util.GradleConstants
  */
 
 class GradleCoverageExtension: JavaCoverageEngineExtension() {
-  override fun isApplicableTo(conf: RunConfigurationBase<*>?) =
-    conf is ExternalSystemRunConfiguration &&
-    GradleConstants.SYSTEM_ID == conf.settings.externalSystemId &&
-    (conf as? GradleRunConfiguration)?.isCoverageDisabled == false
+  override fun isApplicableTo(conf: RunConfigurationBase<*>?) = conf is GradleRunConfiguration &&
+                                                                !conf.isCoverageDisabled
+
 }
