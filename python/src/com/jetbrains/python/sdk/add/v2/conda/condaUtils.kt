5dcdfc85ce259a25c3cee82c726eb6c633bd1b16
Nikita.Ashihmin <nikita.ashihmin@jetbrains.com>
2025-06-17 07:14:25 +0400

    PY-19974 Packages:  Fix that conda do not set associated module by default
    Signed-off-by: Nikita.Ashihmin <nikita.ashihmin@jetbrains.com>
    
    
    (cherry picked from commit c863f53ccba93e177c5092194c9c7aa608e2b932)
    
    IJ-CR-167014
    
    GitOrigin-RevId: 6144bec9a086b1639f2475e853f0e2034632cda5

--- python/src/com/jetbrains/python/sdk/add/v2/conda/condaUtils.kt
+++ python/src/com/jetbrains/python/sdk/add/v2/conda/condaUtils.kt
@@ -4,7 +4,6 @@ package com.jetbrains.python.sdk.add.v2.conda
 import com.intellij.execution.target.TargetEnvironmentConfiguration
 import com.intellij.execution.target.local.LocalTargetEnvironmentRequest
 import com.intellij.openapi.application.EDT
-import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.platform.ide.progress.ModalTaskOwner
 import com.intellij.platform.ide.progress.TaskCancellation
@@ -13,7 +12,9 @@ import com.intellij.util.concurrency.annotations.RequiresEdt
 import com.jetbrains.python.PyBundle
 import com.jetbrains.python.errorProcessing.PyResult
 import com.jetbrains.python.onSuccess
+import com.jetbrains.python.sdk.ModuleOrProject
 import com.jetbrains.python.sdk.PythonSdkType
+import com.jetbrains.python.sdk.add.v2.PyProjectCreateHelpers
 import com.jetbrains.python.sdk.add.v2.PythonAddInterpreterModel
 import com.jetbrains.python.sdk.add.v2.convertToPathOnTarget
 import com.jetbrains.python.sdk.add.v2.existingSdks
@@ -23,6 +24,7 @@ import com.jetbrains.python.sdk.conda.createCondaSdkAlongWithNewEnv
 import com.jetbrains.python.sdk.flavors.conda.NewCondaEnvRequest
 import com.jetbrains.python.sdk.flavors.conda.PyCondaCommand
 import com.jetbrains.python.sdk.persist
+import com.jetbrains.python.sdk.setAssociationToModule
 import kotlinx.coroutines.Dispatchers
 
 @RequiresEdt
@@ -30,8 +32,7 @@ internal fun PythonAddInterpreterModel.createCondaCommand(): PyCondaCommand =
   PyCondaCommand(state.condaExecutable.get().convertToPathOnTarget(targetEnvironmentConfiguration),
                  targetConfig = targetEnvironmentConfiguration)
 
-internal suspend fun PythonAddInterpreterModel.createCondaEnvironment(request: NewCondaEnvRequest): PyResult<Sdk> {
-  val project = ProjectManager.getInstance().defaultProject
+internal suspend fun PythonAddInterpreterModel.createCondaEnvironment(moduleOrProject: ModuleOrProject, request: NewCondaEnvRequest): PyResult<Sdk> {
 
   val result = withModalProgress(ModalTaskOwner.guess(),
                                  PyBundle.message("sdk.create.custom.conda.create.progress"),
@@ -40,10 +41,15 @@ internal suspend fun PythonAddInterpreterModel.createCondaEnvironment(request: N
       newCondaEnvInfo = request,
       uiContext = Dispatchers.EDT,
       existingSdks = existingSdks,
-      project = project
+      project = moduleOrProject.project
     )
   }.onSuccess { sdk ->
     (sdk.sdkType as PythonSdkType).setupSdkPaths(sdk)
+
+    val module = PyProjectCreateHelpers.getModule(moduleOrProject, null)
+    if (module != null) {
+      sdk.setAssociationToModule(module)
+    }
     sdk.persist()
   }
 
