693f04cd278ec38edc22bf26fc0447730fbe553b
Vlad Koshkin <vladislav.koshkin@jetbrains.com>
2025-06-18 00:08:40 +0000

    [kotlin] K2: add test for navigation to .settings gradle convention plugin
    #KTIJ-34533 Fixed
    
    
    Merge-request: IJ-MR-165495
    Merged-by: Vlad Koshkin <Vladislav.Koshkin@jetbrains.com>
    
    GitOrigin-RevId: f5ca4c087f3491901a97aa273f37b090d6ba7023

--- plugins/kotlin/gradle/gradle-java/k2/test/org/jetbrains/kotlin/idea/k2/codeInsight/gradle/highlighting/K2GradleHighlightingTest.kt
+++ plugins/kotlin/gradle/gradle-java/k2/test/org/jetbrains/kotlin/idea/k2/codeInsight/gradle/highlighting/K2GradleHighlightingTest.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.k2.codeInsight.gradle
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.k2.codeInsight.gradle.highlighting
 
 import org.jetbrains.kotlin.gradle.AbstractKotlinGradleHighlightingTest
 import org.jetbrains.kotlin.idea.test.AssertKotlinPluginMode
