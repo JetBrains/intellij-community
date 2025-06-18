e81d3866fa98f5b40fb899b856224430f8e64b90
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-06-18 22:31:37 +0200

    [polySymbols] WEB-73291 Improve API of PolySymbolQueryScopeContributor
    (cherry picked from commit 0a64beffb907a5425939f1f2943c9778a8e12088)
    
    IJ-CR-166348
    
    GitOrigin-RevId: c97bacd5aba0fcbdf2ac5214acba90935b9f8b17

--- platform/polySymbols/src/com/intellij/polySymbols/query/PolySymbolQueryScopeContributor.kt
+++ platform/polySymbols/src/com/intellij/polySymbols/query/PolySymbolQueryScopeContributor.kt
@@ -4,8 +4,10 @@ package com.intellij.polySymbols.query
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
 import com.intellij.patterns.PsiElementPattern
+import com.intellij.patterns.PsiFilePattern
 import com.intellij.polySymbols.context.PolyContext
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 import org.jetbrains.annotations.TestOnly
 import java.util.function.Predicate
 
@@ -24,32 +26,84 @@ interface PolySymbolQueryScopeContributor {
 
 }
 
-fun interface PolySymbolQueryScopeProvider<T: PsiElement?> {
+fun interface PolySymbolLocationQueryScopeProvider<T : PsiElement?> {
 
   fun getScopes(location: T): List<PolySymbolScope>
 
 }
+
 fun interface PolySymbolProjectQueryScopeProvider {
 
   fun getScopes(project: Project): List<PolySymbolScope>
 
 }
 
-interface PolySymbolQueryScopeProviderRegistrar {
+interface PolySymbolQueryScopeProviderRegistrarInFileContext {
 
-  fun <T: PsiElement> forPsiLocation(psiLocationPattern: PsiElementPattern.Capture<T>): Builder<PolySymbolQueryScopeProvider<T>>
+  fun inContext(contextFilter: Predicate<PolyContext>): PolySymbolQueryScopeProviderRegistrarInFileContext
 
-  fun forAnywhere(): Builder<PolySymbolProjectQueryScopeProvider>
+  fun withoutResolve(): PolySymbolQueryScopeProviderRegistrarInFileContext
 
-  interface Builder <P> {
+  fun forAnyPsiLocation(): PolySymbolLocationQueryScopeProviderRegistrar<PsiElement>
 
-    fun inContext(contextFilter: Predicate<PolyContext>): Builder<P>
+  fun <T : PsiElement> forPsiLocation(psiLocationClass: Class<T>): PolySymbolLocationQueryScopeProviderRegistrar<T>
 
-    fun withoutResolve(): Builder<P>
+  fun <T : PsiElement> forPsiLocation(psiLocationPattern: PsiElementPattern<T, *>): PolySymbolLocationQueryScopeProviderRegistrar<T>
 
-    fun contributeScopeProvider(provider: P)
+  fun <T : PsiElement> forPsiLocations(vararg psiLocationPatterns: PsiElementPattern<out T, *>): PolySymbolLocationQueryScopeProviderRegistrar<T>
 
-  }
+  fun <T : PsiElement> forPsiLocations(vararg psiLocationClasses: Class<out T>): PolySymbolLocationQueryScopeProviderRegistrar<T>
+
+  fun <T : PsiElement> forPsiLocations(psiLocationPatterns: Collection<PsiElementPattern<out T, *>>): PolySymbolLocationQueryScopeProviderRegistrar<T>
+}
+
+interface PolySymbolQueryScopeProviderRegistrar: PolySymbolQueryScopeProviderRegistrarInFileContext {
+
+  fun inFile(filePattern: PsiFilePattern<out PsiFile, *>): PolySymbolQueryScopeProviderRegistrarInFileContext
+
+  fun inFile(fileClass: Class<out PsiFile>): PolySymbolQueryScopeProviderRegistrarInFileContext
+
+  fun inFiles(vararg filePatterns: PsiFilePattern<out PsiFile, *>): PolySymbolQueryScopeProviderRegistrarInFileContext
+
+  fun inFiles(vararg fileClasses: Class<out PsiFile>): PolySymbolQueryScopeProviderRegistrarInFileContext
+
+  fun inFiles(filePatterns: Collection<PsiFilePattern<out PsiFile, *>>): PolySymbolQueryScopeProviderRegistrarInFileContext
+
+  override fun inContext(contextFilter: Predicate<PolyContext>): PolySymbolQueryScopeProviderRegistrar
+
+  override fun withoutResolve(): PolySymbolQueryScopeProviderRegistrar
+
+  fun forProject(): PolySymbolProjectQueryScopeProviderRegistrar
+
+}
+
+interface PolySymbolProjectQueryScopeProviderRegistrar {
+
+  fun inContext(contextFilter: Predicate<PolyContext>): PolySymbolProjectQueryScopeProviderRegistrar
+
+  fun withoutResolve(): PolySymbolProjectQueryScopeProviderRegistrar
+
+  fun contributeScopeProvider(provider: PolySymbolProjectQueryScopeProvider)
+
+}
+
+interface PolySymbolLocationQueryScopeProviderRegistrar<T: PsiElement> {
+
+  fun inContext(contextFilter: Predicate<PolyContext>): PolySymbolLocationQueryScopeProviderRegistrar<T>
+
+  fun withoutResolve(): PolySymbolLocationQueryScopeProviderRegistrar<T>
+
+  fun inFile(filePattern: PsiFilePattern<out PsiFile, *>): PolySymbolLocationQueryScopeProviderRegistrar<T>
+
+  fun inFile(fileClass: Class<out PsiFile>): PolySymbolLocationQueryScopeProviderRegistrar<T>
+
+  fun inFiles(vararg filePatterns: PsiFilePattern<out PsiFile, *>): PolySymbolLocationQueryScopeProviderRegistrar<T>
+
+  fun inFiles(vararg fileClasses: Class<out PsiFile>): PolySymbolLocationQueryScopeProviderRegistrar<T>
+
+  fun inFiles(filePatterns: Collection<PsiFilePattern<out PsiFile, *>>): PolySymbolLocationQueryScopeProviderRegistrar<T>
+
+  fun contributeScopeProvider(provider: PolySymbolLocationQueryScopeProvider<T>)
 
 }
 
--- platform/polySymbols/src/com/intellij/polySymbols/query/impl/PolySymbolQueryScopeService.kt
+++ platform/polySymbols/src/com/intellij/polySymbols/query/impl/PolySymbolQueryScopeService.kt
@@ -5,10 +5,14 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
+import com.intellij.patterns.PlatformPatterns
+import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.patterns.PsiElementPattern
+import com.intellij.patterns.PsiFilePattern
 import com.intellij.polySymbols.context.PolyContext
 import com.intellij.polySymbols.query.*
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 import com.intellij.util.SmartList
 import java.util.concurrent.ConcurrentHashMap
 import java.util.function.Predicate
@@ -40,54 +44,131 @@ class PolySymbolQueryScopeService {
       }
 
   private fun buildWrappersForElementClass(elementClass: Class<*>): List<ScopeProviderWrapper> =
-    providerWrappers.filter {
-      it.elementClass.let { cls -> cls == null || cls.isAssignableFrom(elementClass) }
-    }
+    if (elementClass == Unit::class.java)
+      providerWrappers
+    else
+      providerWrappers.filter {
+        it.elementClasses.any { cls -> cls.isAssignableFrom(elementClass) }
+      }
 
   private fun buildWrappers(contributor: PolySymbolQueryScopeContributor): List<ScopeProviderWrapper> =
     PolySymbolQueryScopeProviderRegistrarImpl()
       .also { contributor.registerProviders(it) }
       .scopeProviderWrappers
 
-  private class PolySymbolQueryScopeProviderRegistrarImpl() : PolySymbolQueryScopeProviderRegistrar {
-    val scopeProviderWrappers: MutableList<ScopeProviderWrapper> = SmartList()
+  private data class PolySymbolQueryScopeProviderRegistrarImpl(
+    private val filePatterns: Collection<PsiFilePattern<out PsiFile, *>> = emptyList(),
+    private val contextFilter: Predicate<PolyContext>? = null,
+    private val withResolve: Boolean = true,
+    val scopeProviderWrappers: MutableList<ScopeProviderWrapper> = SmartList(),
+  ) : PolySymbolQueryScopeProviderRegistrar {
 
-    override fun <T : PsiElement> forPsiLocation(psiLocationPattern: PsiElementPattern.Capture<T>): PolySymbolQueryScopeProviderRegistrar.Builder<PolySymbolQueryScopeProvider<T>> =
-      object : PolySymbolQueryScopeProviderRegistrarBuilderBase<PolySymbolQueryScopeProvider<T>>() {
-        override fun contributeScopeProvider(provider: PolySymbolQueryScopeProvider<T>) {
-          scopeProviderWrappers.add(LocationScopeProviderWrapper(psiLocationPattern, provider, filters))
-        }
-      }
+    override fun inFile(fileClass: Class<out PsiFile>): PolySymbolQueryScopeProviderRegistrarInFileContext =
+      copy(filePatterns = listOf(PlatformPatterns.psiFile(fileClass)))
 
-    override fun forAnywhere(): PolySymbolQueryScopeProviderRegistrar.Builder<PolySymbolProjectQueryScopeProvider> {
-      return object : PolySymbolQueryScopeProviderRegistrarBuilderBase<PolySymbolProjectQueryScopeProvider>() {
-        override fun contributeScopeProvider(provider: PolySymbolProjectQueryScopeProvider) {
-          scopeProviderWrappers.add(ProjectScopeProviderWrapper(provider, filters))
-        }
-      }
-    }
+    override fun inFile(filePattern: PsiFilePattern<out PsiFile, *>): PolySymbolQueryScopeProviderRegistrarInFileContext =
+      copy(filePatterns = listOf(filePattern))
+
+    override fun inFiles(vararg fileClasses: Class<out PsiFile>): PolySymbolQueryScopeProviderRegistrarInFileContext =
+      copy(filePatterns = fileClasses.map { PlatformPatterns.psiFile(it) })
+
+    override fun inFiles(filePatterns: Collection<PsiFilePattern<out PsiFile, *>>): PolySymbolQueryScopeProviderRegistrarInFileContext =
+      copy(filePatterns = filePatterns)
+
+    override fun inFiles(vararg filePatterns: PsiFilePattern<out PsiFile, *>): PolySymbolQueryScopeProviderRegistrarInFileContext =
+      copy(filePatterns = filePatterns.toList())
+
+    override fun inContext(contextFilter: Predicate<PolyContext>): PolySymbolQueryScopeProviderRegistrar =
+      copy(contextFilter = contextFilter)
+
+    override fun withoutResolve(): PolySymbolQueryScopeProviderRegistrar =
+      copy(withResolve = false)
+
+    override fun forAnyPsiLocation(): PolySymbolLocationQueryScopeProviderRegistrar<PsiElement> =
+      forPsiLocations(listOf(psiElement()))
+
+    override fun <T : PsiElement> forPsiLocation(psiLocationClass: Class<T>): PolySymbolLocationQueryScopeProviderRegistrar<T> =
+      forPsiLocations(listOf(psiElement(psiLocationClass)))
+
+    override fun <T : PsiElement> forPsiLocation(psiLocationPattern: PsiElementPattern<T, *>): PolySymbolLocationQueryScopeProviderRegistrar<T> =
+      forPsiLocations(listOf(psiLocationPattern))
+
+    override fun <T : PsiElement> forPsiLocations(vararg psiLocationClasses: Class<out T>): PolySymbolLocationQueryScopeProviderRegistrar<T> =
+      forPsiLocations(psiLocationClasses.map { psiElement(it) })
+
+    override fun <T : PsiElement> forPsiLocations(vararg psiLocationPatterns: PsiElementPattern<out T, *>): PolySymbolLocationQueryScopeProviderRegistrar<T> =
+      forPsiLocations(psiLocationPatterns.toList())
+
+    override fun <T : PsiElement> forPsiLocations(psiLocationPatterns: Collection<PsiElementPattern<out T, *>>): PolySymbolLocationQueryScopeProviderRegistrar<T> =
+      PolySymbolQueryScopeProviderBuilder(scopeProviderWrappers, psiLocationPatterns, filePatterns, contextFilter, withResolve)
+
+    override fun forProject(): PolySymbolProjectQueryScopeProviderRegistrar =
+      PolySymbolProjectQueryScopeProviderRegistrarImpl(scopeProviderWrappers, contextFilter, withResolve)
   }
 
   private interface ScopeProviderWrapper {
-    val elementClass: Class<*>?
+    val elementClasses: List<Class<*>>
     val filters: ScopeProviderWrapperFilters
-    fun getScopes(project: Project, location: PsiElement?): List<PolySymbolScope>
+    fun getScopes(project: Project, location: PsiElement?): Collection<PolySymbolScope>
   }
 
-  private abstract class PolySymbolQueryScopeProviderRegistrarBuilderBase<T> : PolySymbolQueryScopeProviderRegistrar.Builder<T> {
+  private class PolySymbolQueryScopeProviderBuilder<T : PsiElement>(
+    private val scopeProviderWrappers: MutableList<ScopeProviderWrapper>,
+    private val psiLocationPatterns: Collection<PsiElementPattern<out T, *>>,
+    private var filePatterns: Collection<PsiFilePattern<out PsiFile, *>>,
+    private var contextFilter: Predicate<PolyContext>?,
+    private var withResolve: Boolean,
+  ) : PolySymbolLocationQueryScopeProviderRegistrar<T> {
 
-    private var contextFilter: Predicate<PolyContext>? = null
-    private var withResolve: Boolean = true
-
-    protected val filters: ScopeProviderWrapperFilters
-      get() = ScopeProviderWrapperFilters(contextFilter, withResolve)
-
-    override fun inContext(contextFilter: Predicate<PolyContext>): PolySymbolQueryScopeProviderRegistrar.Builder<T> = apply {
+    override fun inContext(contextFilter: Predicate<PolyContext>): PolySymbolLocationQueryScopeProviderRegistrar<T> = apply {
       this.contextFilter = contextFilter
     }
 
-    override fun withoutResolve(): PolySymbolQueryScopeProviderRegistrar.Builder<T> = apply {
+    override fun withoutResolve(): PolySymbolLocationQueryScopeProviderRegistrar<T> = apply {
       this.withResolve = false
+    }
+
+    override fun inFile(filePattern: PsiFilePattern<out PsiFile, *>): PolySymbolLocationQueryScopeProviderRegistrar<T> = apply {
+      this.filePatterns = listOf(filePattern)
+    }
+
+    override fun inFile(fileClass: Class<out PsiFile>): PolySymbolLocationQueryScopeProviderRegistrar<T> = apply {
+      this.filePatterns = listOf(PlatformPatterns.psiFile(fileClass))
+    }
+
+    override fun inFiles(vararg filePatterns: PsiFilePattern<out PsiFile, *>): PolySymbolLocationQueryScopeProviderRegistrar<T> = apply {
+      this.filePatterns = filePatterns.toList()
+    }
+
+    override fun inFiles(vararg fileClasses: Class<out PsiFile>): PolySymbolLocationQueryScopeProviderRegistrar<T> = apply {
+      this.filePatterns = fileClasses.map { PlatformPatterns.psiFile(it) }
+    }
+
+    override fun inFiles(filePatterns: Collection<PsiFilePattern<out PsiFile, *>>): PolySymbolLocationQueryScopeProviderRegistrar<T> = apply {
+      this.filePatterns = filePatterns
+    }
+
+    override fun contributeScopeProvider(provider: PolySymbolLocationQueryScopeProvider<T>) {
+      scopeProviderWrappers.add(LocationScopeProviderWrapper(psiLocationPatterns, filePatterns, provider, ScopeProviderWrapperFilters(contextFilter, withResolve)))
+    }
+  }
+
+  private class PolySymbolProjectQueryScopeProviderRegistrarImpl(
+    private val scopeProviderWrappers: MutableList<ScopeProviderWrapper>,
+    private var contextFilter: Predicate<PolyContext>?,
+    private var withResolve: Boolean,
+  ) : PolySymbolProjectQueryScopeProviderRegistrar {
+
+    override fun inContext(contextFilter: Predicate<PolyContext>): PolySymbolProjectQueryScopeProviderRegistrar = apply {
+      this.contextFilter = contextFilter
+    }
+
+    override fun withoutResolve(): PolySymbolProjectQueryScopeProviderRegistrar = apply {
+      this.withResolve = false
+    }
+
+    override fun contributeScopeProvider(provider: PolySymbolProjectQueryScopeProvider) {
+      scopeProviderWrappers.add(ProjectScopeProviderWrapper(provider, ScopeProviderWrapperFilters(contextFilter, withResolve)))
     }
   }
 
@@ -101,15 +182,19 @@ class PolySymbolQueryScopeService {
   }
 
   private class LocationScopeProviderWrapper<T : PsiElement?>(
-    val pattern: PsiElementPattern.Capture<T>,
-    val provider: PolySymbolQueryScopeProvider<T>,
+    val locationPatterns: Collection<PsiElementPattern<out T, *>>,
+    val filePatterns: Collection<PsiFilePattern<out PsiFile, *>>,
+    val provider: PolySymbolLocationQueryScopeProvider<T>,
     override val filters: ScopeProviderWrapperFilters,
   ) : ScopeProviderWrapper {
 
-    override val elementClass: Class<T> = pattern.condition.initialCondition.acceptedClass
+    override val elementClasses: List<Class<*>> = locationPatterns.map { it.condition.initialCondition.acceptedClass }.distinct()
 
-    override fun getScopes(project: Project, location: PsiElement?): List<PolySymbolScope> {
-      if (location == null || !pattern.accepts(location)) return emptyList()
+    override fun getScopes(project: Project, location: PsiElement?): Collection<PolySymbolScope> {
+      if (location == null) return emptyList()
+      val containingFile = location.containingFile
+      if (!filePatterns.any { it.accepts(containingFile) }) return emptyList()
+      if (!locationPatterns.any { it.accepts(location) }) return emptyList()
 
       @Suppress("UNCHECKED_CAST")
       location as T
@@ -137,7 +222,7 @@ class PolySymbolQueryScopeService {
     override val filters: ScopeProviderWrapperFilters,
   ) : ScopeProviderWrapper {
 
-    override val elementClass: Class<*>? = null
+    override val elementClasses: List<Class<*>> = emptyList()
 
     override fun getScopes(project: Project, location: PsiElement?): List<PolySymbolScope> =
       provider.getScopes(project)
