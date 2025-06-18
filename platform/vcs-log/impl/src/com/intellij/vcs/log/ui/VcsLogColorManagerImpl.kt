8f30ea0d3579a8c358c678786a525845d5376f17
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2025-06-18 15:03:52 +0200

    IJPL-191999: vcs: Migrate VcsLogColorManagerImpl to Kotlin
    IJ-MR-166259
    
    (cherry picked from commit b2f3b30d4e59d1792e84db7687b12377cac4863c)
    
    GitOrigin-RevId: be7e6a3c1043f04c45a4de0ed22ab7255d00d0da

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/VcsLogColorManagerImpl.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/VcsLogColorManagerImpl.kt
@@ -1,117 +1,105 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.vcs.log.ui;
+package com.intellij.vcs.log.ui
 
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.vcs.FilePath;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.ui.ColorUtil;
-import com.intellij.ui.JBColor;
-import com.intellij.util.ui.UIUtil;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-
-import java.awt.*;
-import java.util.List;
-import java.util.*;
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.vcs.FilePath
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.ColorUtil
+import com.intellij.ui.JBColor
+import com.intellij.util.ui.UIUtil
+import org.jetbrains.annotations.ApiStatus
+import java.awt.Color
+import java.util.function.Supplier
+import kotlin.math.abs
 
 /**
  * @author Kirill Likhodedov
  */
 @ApiStatus.Internal
-public class VcsLogColorManagerImpl implements VcsLogColorManager {
-  private static final Logger LOG = Logger.getInstance(VcsLogColorManagerImpl.class);
+class VcsLogColorManagerImpl internal constructor(
+  paths: Collection<FilePath>,
+  mainPalette: List<Color>,
+  vararg additionalColorSpaces: AdditionalColorSpace
+) : VcsLogColorManager {
+  private val myPath2Palette: Map<String, Map<String, Color>>
+  private val myPaths: List<FilePath>
 
-  private final @NotNull Map<String, Map<String, Color>> myPath2Palette;
-  private final @NotNull List<FilePath> myPaths;
+  init {
+    var defaultPalette = mainPalette
+    myPaths = paths.toList()
+    myPath2Palette = mutableMapOf()
 
-  VcsLogColorManagerImpl(
-    @NotNull Collection<? extends FilePath> paths,
-    @NotNull List<Color> defaultPalette,
-    AdditionalColorSpace... additionalColorSpaces
-  ) {
-    myPaths = new ArrayList<>(paths);
-    myPath2Palette = new HashMap<>();
+    defaultPalette = if (defaultPalette.isEmpty()) mutableListOf<Color>(defaultRootColor) else ArrayList<Color>(defaultPalette)
 
-    defaultPalette = defaultPalette.isEmpty() ? List.of(getDefaultRootColor()) : new ArrayList<>(defaultPalette);
-    myPath2Palette.put(DEFAULT_COLOR_MODE, generateFromPalette(defaultPalette));
+    myPath2Palette[VcsLogColorManager.DEFAULT_COLOR_MODE] = generateFromPalette(defaultPalette)
 
-    for (AdditionalColorSpace colorSpace : additionalColorSpaces) {
+    for (colorSpace in additionalColorSpaces) {
       // do not allow to override default palette
-      if (colorSpace.colorMode.equals(DEFAULT_COLOR_MODE)) continue;
+      if (colorSpace.colorMode == VcsLogColorManager.DEFAULT_COLOR_MODE) continue
 
       // allow additional palettes only the same size as the default
-      if (colorSpace.palette.size() != defaultPalette.size()) continue;
+      if (colorSpace.palette.size != defaultPalette.size) continue
 
-      Map<String, Color> colors = generateFromPalette(new ArrayList<>(colorSpace.palette));
-      myPath2Palette.put(colorSpace.colorMode, colors);
+      val colors = generateFromPalette(ArrayList<Color>(colorSpace.palette))
+      myPath2Palette[colorSpace.colorMode] = colors
     }
   }
 
-  private Map<String, Color> generateFromPalette(@NotNull List<Color> defaultPalette) {
-    Map<String, Color> path2Palette = new HashMap<>();
+  private fun generateFromPalette(defaultPalette: List<Color>): Map<String, Color> {
+    val path2Palette: MutableMap<String, Color> = mutableMapOf()
 
-    for (int i = 0; i < myPaths.size(); i++) {
-      path2Palette.put(myPaths.get(i).getPath(), getColor(i, myPaths.size(), defaultPalette));
+    for (i in myPaths.indices) {
+      path2Palette[myPaths[i].getPath()] = getColor(i, myPaths.size, defaultPalette)
     }
-    return path2Palette;
+    return path2Palette
   }
 
-  private static @NotNull Color getColor(int rootNumber, int rootsCount, @NotNull List<Color> palette) {
-    Color color;
-    int size = palette.size();
-    if (rootNumber >= size) {
-      double balance = ((double)(rootNumber / size)) / (rootsCount / size);
-      Color mix = ColorUtil.mix(palette.get(rootNumber % size), palette.get((rootNumber + 1) % size), balance);
-      int tones = (int)(Math.abs(balance - 0.5) * 2 * (rootsCount / size) + 1);
-      if (mix instanceof JBColor) {
-        color = JBColor.lazy(() -> new JBColor(ColorUtil.darker(mix, tones), ColorUtil.brighter(mix, 2 * tones)));
+  override fun getPathColor(path: FilePath, colorMode: String): Color {
+    return getColor(path.getPath(), colorMode)
+  }
+
+  override fun getRootColor(root: VirtualFile, colorMode: String): Color {
+    return getColor(root.getPath(), colorMode)
+  }
+
+  private fun getColor(path: String, colorMode: String): Color {
+    val paletteToColor = myPath2Palette.getOrDefault(colorMode, myPath2Palette[VcsLogColorManager.DEFAULT_COLOR_MODE])!!
+    var color = paletteToColor[path]
+    if (color == null) {
+      LOG.error("No color record for path $path. All paths: $paletteToColor")
+      color = defaultRootColor
+    }
+    return color
+  }
+
+  override fun getPaths(): Collection<FilePath> = myPaths
+
+  class AdditionalColorSpace(val colorMode: String, val palette: List<Color>)
+
+  companion object {
+    private val LOG = Logger.getInstance(VcsLogColorManagerImpl::class.java)
+
+    private fun getColor(rootNumber: Int, rootsCount: Int, palette: List<Color>): Color {
+      val color: Color
+      val size = palette.size
+      if (rootNumber >= size) {
+        val balance = ((rootNumber / size).toDouble()) / (rootsCount / size)
+        val mix = ColorUtil.mix(palette[rootNumber % size], palette[(rootNumber + 1) % size], balance)
+        val tones = (abs(balance - 0.5) * 2 * (rootsCount / size) + 1).toInt()
+        if (mix is JBColor) {
+          color = JBColor.lazy(Supplier { JBColor(ColorUtil.darker(mix, tones), ColorUtil.brighter(mix, 2 * tones)) })
+        }
+        else {
+          color = JBColor(ColorUtil.darker(mix, tones), ColorUtil.brighter(mix, 2 * tones))
+        }
       }
       else {
-        color = new JBColor(ColorUtil.darker(mix, tones), ColorUtil.brighter(mix, 2 * tones));
+        color = palette[rootNumber]
       }
+      return color
     }
-    else {
-      color = palette.get(rootNumber);
-    }
-    return color;
-  }
 
-  @Override
-  public @NotNull Color getPathColor(@NotNull FilePath path, @NotNull String colorMode) {
-    return getColor(path.getPath(), colorMode);
-  }
-
-  @Override
-  public @NotNull Color getRootColor(@NotNull VirtualFile root, @NotNull String colorMode) {
-    return getColor(root.getPath(), colorMode);
-  }
-
-  private @NotNull Color getColor(@NotNull String path, @NotNull String colorMode) {
-    Map<String, Color> paletteToColor = myPath2Palette.getOrDefault(colorMode, myPath2Palette.get(DEFAULT_COLOR_MODE));
-    Color color = paletteToColor.get(path);
-    if (color == null) {
-      LOG.error("No color record for path " + path + ". All paths: " + paletteToColor);
-      color = getDefaultRootColor();
-    }
-    return color;
-  }
-
-  private static @NotNull Color getDefaultRootColor() {
-    return UIUtil.getTableBackground();
-  }
-
-  @Override
-  public @NotNull Collection<FilePath> getPaths() {
-    return myPaths;
-  }
-
-  public static class AdditionalColorSpace {
-    private final String colorMode;
-    private final List<Color> palette;
-
-    AdditionalColorSpace(@NotNull String colorMode, @NotNull List<Color> palette) {
-      this.colorMode = colorMode;
-      this.palette = palette;
-    }
+    private val defaultRootColor: Color
+      get() = UIUtil.getTableBackground()
   }
 }
