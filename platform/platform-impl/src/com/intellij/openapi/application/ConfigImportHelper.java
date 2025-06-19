9105bd038a70cdbbe6131028da5be994573f80d9
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2025-06-19 12:37:09 +0200

    [remote dev] store the list of plugins disabled in the frontend process in a separate file (IJPL-192491)
    The frontend process now stores the list of disabled plugins in a disabled_plugins_frontend.txt file instead of disabled_plugins.txt used by a regular IDE. When settings are imported from a previous version, the frontend imports data from disabled_plugins_frontend.txt if it is present, and otherwise imports disabled_plugins.txt to ensure that plugins which were disabled in a regular IDE will be disabled in the frontend process by default.
    
    'installPerProcessInstanceSupportImplementation' calls are moved before 'PerProcessPathCustomization.prepareConfig' to ensure that P3Support is already initialized inside the latter.
    
    
    (cherry picked from commit c60a152321d0d725d6c6f4ac249a00b4cfe9fb09)
    
    IJ-CR-166284
    
    GitOrigin-RevId: c0c9ab1a5603d28bfa13f1d4b58d011564ab26aa

--- platform/platform-impl/src/com/intellij/openapi/application/ConfigImportHelper.java
+++ platform/platform-impl/src/com/intellij/openapi/application/ConfigImportHelper.java
@@ -29,6 +29,7 @@ import com.intellij.openapi.progress.EmptyProgressIndicator;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.project.ex.ProjectManagerEx;
+import com.intellij.openapi.project.impl.P3SupportKt;
 import com.intellij.openapi.project.impl.shared.P3DynamicPluginSynchronizerKt;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.updateSettings.impl.PluginDownloader;
@@ -176,9 +177,14 @@ public final class ConfigImportHelper {
                 vmOptionsLines = Files.readAllLines(newConfigDir.resolve(VMOptions.getFileName()), VMOptions.getFileCharset());
                 vmOptionFileChanged = false;
               }
-             if (Files.isRegularFile(newConfigDir.resolve(DisabledPluginsState.DISABLED_PLUGINS_FILENAME))){
-               currentlyDisabledPlugins = Files.readAllLines(newConfigDir.resolve(DisabledPluginsState.DISABLED_PLUGINS_FILENAME));
-             }
+              String disabledPluginsFileName = P3SupportKt.processPerProjectSupport().getDisabledPluginsFileName();
+              if (Files.isRegularFile(newConfigDir.resolve(disabledPluginsFileName))) {
+                currentlyDisabledPlugins = Files.readAllLines(newConfigDir.resolve(disabledPluginsFileName));
+              }
+              else if (!DisabledPluginsState.DISABLED_PLUGINS_FILENAME.equals(disabledPluginsFileName) &&
+                       Files.isRegularFile(newConfigDir.resolve(DisabledPluginsState.DISABLED_PLUGINS_FILENAME))){
+                currentlyDisabledPlugins = Files.readAllLines(newConfigDir.resolve(DisabledPluginsState.DISABLED_PLUGINS_FILENAME));
+              }
             }
             tempBackup = backupCurrentConfigToTempAndDelete(newConfigDir, log, true, settings);
             importScenarioStatistics = IMPORT_SETTINGS_ACTION;
@@ -255,7 +261,7 @@ public final class ConfigImportHelper {
         setConfigImportedInThisSession();
         if (currentlyDisabledPlugins != null) {
           try {
-            Path newDisablePluginsFile = newConfigDir.resolve(DisabledPluginsState.DISABLED_PLUGINS_FILENAME);
+            Path newDisablePluginsFile = newConfigDir.resolve(P3SupportKt.processPerProjectSupport().getDisabledPluginsFileName());
             Set<String> newDisabledPlugins = new LinkedHashSet<>();
             if (Files.isRegularFile(newDisablePluginsFile)) {
               newDisabledPlugins.addAll(Files.readAllLines(newDisablePluginsFile, CharsetToolkit.getPlatformCharset()));
@@ -1037,6 +1043,11 @@ public final class ConfigImportHelper {
         return FileVisitResult.CONTINUE;
       }
     });
+    String disabledPluginsFileName = P3SupportKt.processPerProjectSupport().getDisabledPluginsFileName();
+    if (!disabledPluginsFileName.equals(DisabledPluginsState.DISABLED_PLUGINS_FILENAME) && 
+        Files.exists(oldConfigDir.resolve(DisabledPluginsState.DISABLED_PLUGINS_FILENAME)) && !Files.exists(newConfigDir.resolve(disabledPluginsFileName))) {
+      Files.copy(oldConfigDir.resolve(DisabledPluginsState.DISABLED_PLUGINS_FILENAME), newConfigDir.resolve(disabledPluginsFileName));
+    }
 
     List<ActionCommand> actionCommands = loadStartupActionScript(oldConfigDir, oldIdeHome, oldPluginsDir);
 
