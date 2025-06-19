7f5edd86d8c8f548f87abee419f17f9cbd91798e
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2025-06-19 09:06:04 +0200

    [maven] IDEA-374666 split modules if compiler arguments differ: test
    Avoid splitting when differences exist only in other execution configuration fields (e.g., generatedSourcesDirectory).
    
    GitOrigin-RevId: 2018edef61601a555084619452783c18552fea85

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenCompilerImportingTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenCompilerImportingTest.kt
@@ -1304,4 +1304,32 @@ class MavenCompilerImportingTest : MavenMultiVersionImportingTestCase() {
 
   }
 
+  @Test
+  @TestFor(issues = ["IDEA-374666"])
+  fun testAvoidUnnecessaryModuleSplitting() = runBlocking {
+    importProjectAsync("""
+      <groupId>test</groupId>
+      <artifactId>project</artifactId>
+      <version>1</version>
+      <build>  
+        <plugins>
+          <plugin> 
+            <groupId>org.apache.maven.plugins</groupId>
+            <artifactId>maven-compiler-plugin</artifactId>
+            <version>3.12.1</version>
+            <executions>
+              <execution>
+                <id>default-testCompile</id>
+                <configuration>
+                  <generatedSourcesDirectory>./java-test</generatedSourcesDirectory>
+                </configuration>
+              </execution>
+            </executions>
+          </plugin>
+        </plugins>
+      </build> """.trimIndent())
+
+    assertModules("project")
+  }
+
 }
