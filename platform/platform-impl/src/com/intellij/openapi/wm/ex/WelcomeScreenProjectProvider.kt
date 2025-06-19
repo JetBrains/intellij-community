e4a155547f3276432519cf81323360df37cb2c10
Nikita Mironov <nikita.mironov@jetbrains.com>
2025-06-19 17:13:09 +0200

    GO-18841 GO-18842 Ensure presence of the one and only extension at the API level
    (cherry picked from commit ef7da01af201c1338b992425eb75e658dcdb7baa)
    
    IJ-CR-167212
    
    GitOrigin-RevId: 89f03ab7bd77e33313a4e61087cdfaeebcedda2a

--- platform/platform-impl/src/com/intellij/openapi/wm/ex/WelcomeScreenProjectProvider.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/ex/WelcomeScreenProjectProvider.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.wm.ex
 
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
 import org.jetbrains.annotations.ApiStatus
@@ -11,12 +12,25 @@ import org.jetbrains.annotations.ApiStatus
  *
  * E.g., if a project is created/opened/cloned from a welcome screen project,
  * we should close the welcome screen project to preserve the welcome screen experience.
+ *
+ * This customization is intended to be used per-IDE, not per language.
  */
 @ApiStatus.Internal
-interface WelcomeScreenProjectProvider {
+abstract class WelcomeScreenProjectProvider {
   companion object {
-    val EP_NAME: ExtensionPointName<WelcomeScreenProjectProvider> = ExtensionPointName("com.intellij.welcomeScreenProjectProvider")
+    private val EP_NAME: ExtensionPointName<WelcomeScreenProjectProvider> = ExtensionPointName("com.intellij.welcomeScreenProjectProvider")
+
+    @JvmStatic
+    fun isSingleWelcomeScreenProject(project: Project): Boolean {
+      val providers = EP_NAME.extensionList
+      if (providers.isEmpty()) return false
+      if (providers.size > 1) {
+        thisLogger().warn("Multiple WelcomeScreenProjectProvider extensions")
+        return false
+      }
+      return providers.first().isWelcomeScreenProject(project)
+    }
   }
 
-  fun isWelcomeScreenProject(project: Project): Boolean
+  protected abstract fun isWelcomeScreenProject(project: Project): Boolean
 }
