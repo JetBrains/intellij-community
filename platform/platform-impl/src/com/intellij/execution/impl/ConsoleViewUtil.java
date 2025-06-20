3524898484f1320b8a039362d9a8794ec5755155
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-06-20 09:50:05 +0300

    IJPL-192451 Wrap ConsoleFilterProvider exceptions
    This will ensure that the exceptions are attributed to the plugin
    that caused them.
    
    GitOrigin-RevId: bd594ae602d3bff5a7966979ff407a9aa5031090

--- platform/platform-impl/src/com/intellij/execution/impl/ConsoleViewUtil.java
+++ platform/platform-impl/src/com/intellij/execution/impl/ConsoleViewUtil.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.impl;
 
+import com.intellij.diagnostic.PluginException;
 import com.intellij.execution.filters.*;
 import com.intellij.execution.ui.ConsoleView;
 import com.intellij.execution.ui.ConsoleViewContentType;
@@ -300,7 +301,11 @@ public final class ConsoleViewUtil {
         throw e;
       }
       catch (Exception e) {
-        LOG.error("An exception occurred when trying to get console filters, this provider will be unavailable: " + eachProvider, e);
+        LOG.error(PluginException.createByClass(
+          "An exception occurred when trying to get console filters, this provider will be unavailable: " + eachProvider,
+          e,
+          eachProvider.getClass()
+        ));
       }
     }
     return result;
