ff1c04dcde172f02747d99d0a0e546a0c72d1eaf
Artem.Grigorash <artem.grigorash@jetbrains.com>
2025-06-20 11:02:34 +0300

    [driver][ui-tests] Added getLineNumber function for editor
    (cherry picked from commit 5f8cab22f665bcfc703d052ef892cab839ec7481)
    (cherry picked from commit 17830666cb7e8d8f4a0768d1f0bb07e07697ddca)
    
    GitOrigin-RevId: ddc9843924a78371f9870c9be61d74b3c6489ba3

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/JEditorUi.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/JEditorUi.kt
@@ -59,6 +59,8 @@ open class JEditorUiComponent(data: ComponentData) : UiComponent(data) {
       }
     }
 
+  fun getLineNumber(text: String): Int = document.getLineNumber(this.text.indexOf(text)) + 1
+
   fun expandAllFoldings() {
     driver.invokeAction("ExpandAllRegions", component = component)
   }
