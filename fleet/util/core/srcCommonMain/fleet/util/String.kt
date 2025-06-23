70035c983504e7cc4712b504e6ec76120be51516
Bernhard Geisberger <bernhard.geisberger@jetbrains.com>
2025-06-23 17:44:29 +0200

    [fleet] Implement multiplatform ProtocolPath <-> URI translation FL-33928
    This is intended to replace Path.toUri and similar functions
    
    GitOrigin-RevId: 4ca518f7ffb5873649cfe2f2ccd50f4ad486476a

--- fleet/util/core/srcCommonMain/fleet/util/String.kt
+++ fleet/util/core/srcCommonMain/fleet/util/String.kt
@@ -7,6 +7,9 @@ import fleet.util.text.codepoints
 fun String.capitalizeWithCurrentLocale(): String = linkToActual()
 fun String.lowercaseWithCurrentLocale(): String = linkToActual()
 fun String.uppercaseWithCurrentLocale(): String = linkToActual()
+fun String.encodeUriComponent(): String = linkToActual()
+fun String.decodeUriComponent(): String = linkToActual()
+fun String.isValidUriString(): Boolean = linkToActual()
 
 fun String.capitalizeLocaleAgnostic(): String = replaceFirstChar {
   if (it.isLowerCase()) it.titlecase() else it.toString()
