388d9f54a3bfcd961b79c0adb2a5530e0ee1e527
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-06-23 11:15:10 +0200

    warn on legacy EDT dispatcher usage before project open
    GitOrigin-RevId: 24923913463ff159525ef18a43eba2d540f18c37

--- platform/core-impl/src/com/intellij/openapi/application/impl/PlatformCoroutineSupport.kt
+++ platform/core-impl/src/com/intellij/openapi/application/impl/PlatformCoroutineSupport.kt
@@ -4,22 +4,29 @@ package com.intellij.openapi.application.impl
 import com.intellij.openapi.application.CoroutineSupport
 import com.intellij.openapi.application.UiDispatcherKind
 import com.intellij.openapi.application.UiDispatcherKind.*
-import org.jetbrains.annotations.ApiStatus.Internal
+import org.jetbrains.annotations.ApiStatus
 import kotlin.coroutines.CoroutineContext
 
-@Internal
-internal class PlatformCoroutineSupport : CoroutineSupport {
-
-  override fun uiDispatcher(kind: UiDispatcherKind, immediate: Boolean): CoroutineContext {
+@ApiStatus.Internal
+open class PlatformCoroutineSupport() : CoroutineSupport {
+  final override fun uiDispatcher(kind: UiDispatcherKind, immediate: Boolean): CoroutineContext {
     val mainDispatcher = when (kind) {
       STRICT -> EdtCoroutineDispatcher.LockForbidden
       RELAX -> EdtCoroutineDispatcher.NonLocking
-      LEGACY -> EdtCoroutineDispatcher.LockWrapping
+      LEGACY -> {
+        warnAboutUsingLegacyEdt()
+        EdtCoroutineDispatcher.LockWrapping
+      }
     }
     return if (immediate) {
       mainDispatcher.immediate
-    } else {
+    }
+    else {
       mainDispatcher
     }
+  }
+
+  // we cannot use modern StackWalker API - limited to Java 8
+  protected open fun warnAboutUsingLegacyEdt() {
   }
 }
