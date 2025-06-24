37a4e639a8ad753bd031f7c14e9d7aafb48c7e31
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2025-06-24 13:07:39 +0200

    PY-74332 Patch typeshed: Restore SQLAlchemy and Flask-SQLAlchemy
    GitOrigin-RevId: ab77ea6b5faeb37bd8b687ab3e859818f5e6eb6a

---
+++ python/helpers/typeshed/stubs/SQLAlchemy/sqlalchemy/dialects/oracle/__init__.pyi
@@ -0,0 +1,54 @@
+#  Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+from typing import Any
+
+from .base import (
+    BFILE as BFILE,
+    BINARY_DOUBLE as BINARY_DOUBLE,
+    BINARY_FLOAT as BINARY_FLOAT,
+    BLOB as BLOB,
+    CHAR as CHAR,
+    CLOB as CLOB,
+    DATE as DATE,
+    DOUBLE_PRECISION as DOUBLE_PRECISION,
+    FLOAT as FLOAT,
+    INTERVAL as INTERVAL,
+    LONG as LONG,
+    NCHAR as NCHAR,
+    NCLOB as NCLOB,
+    NUMBER as NUMBER,
+    NVARCHAR as NVARCHAR,
+    NVARCHAR2 as NVARCHAR2,
+    RAW as RAW,
+    ROWID as ROWID,
+    TIMESTAMP as TIMESTAMP,
+    VARCHAR as VARCHAR,
+    VARCHAR2 as VARCHAR2,
+)
+
+__all__ = (
+    "VARCHAR",
+    "NVARCHAR",
+    "CHAR",
+    "NCHAR",
+    "DATE",
+    "NUMBER",
+    "BLOB",
+    "BFILE",
+    "CLOB",
+    "NCLOB",
+    "TIMESTAMP",
+    "RAW",
+    "FLOAT",
+    "DOUBLE_PRECISION",
+    "BINARY_DOUBLE",
+    "BINARY_FLOAT",
+    "LONG",
+    "dialect",
+    "INTERVAL",
+    "VARCHAR2",
+    "NVARCHAR2",
+    "ROWID",
+)
+
+dialect: Any
---
+++ python/helpers/typeshed/stubs/SQLAlchemy/sqlalchemy/ext/mypy/apply.pyi
@@ -0,0 +1,29 @@
+#  Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+from typing import Any
+from typing_extensions import TypeAlias
+
+from . import util
+
+_AssignmentStmt: TypeAlias = Any  # mypy.nodes.AssignmentStmt
+_NameExpr: TypeAlias = Any  # mypy.nodes.NameExpr
+_StrExpr: TypeAlias = Any  # mypy.nodes.StrExpr
+_SemanticAnalyzerPluginInterface: TypeAlias = Any  # mypy.plugin.SemanticAnalyzerPluginInterface
+_ProperType: TypeAlias = Any  # mypy.types.ProperType
+
+def apply_mypy_mapped_attr(
+    cls, api: _SemanticAnalyzerPluginInterface, item: _NameExpr | _StrExpr, attributes: list[util.SQLAlchemyAttribute]
+) -> None: ...
+def re_apply_declarative_assignments(
+    cls, api: _SemanticAnalyzerPluginInterface, attributes: list[util.SQLAlchemyAttribute]
+) -> None: ...
+def apply_type_to_mapped_statement(
+    api: _SemanticAnalyzerPluginInterface,
+    stmt: _AssignmentStmt,
+    lvalue: _NameExpr,
+    left_hand_explicit_type: _ProperType | None,
+    python_type_for_type: _ProperType | None,
+) -> None: ...
+def add_additional_orm_attributes(
+    cls, api: _SemanticAnalyzerPluginInterface, attributes: list[util.SQLAlchemyAttribute]
+) -> None: ...
