365a6a23255b032d78321eaaca95cedb972d10de
Maria Sokolova <maria.sokolova@jetbrains.com>
2025-06-25 11:03:20 +0200

    [debugger] Fixed flacky test for testCoroutinesHierarchy3
    GitOrigin-RevId: c3ae8452f1585378b3d337dfe90d9cb34196fce0

--- plugins/kotlin/jvm-debugger/test/testData/coroutinesView/coroutinesHierarchy3.kt
+++ plugins/kotlin/jvm-debugger/test/testData/coroutinesView/coroutinesHierarchy3.kt
@@ -6,52 +6,18 @@ import kotlinx.coroutines.*
 
 fun main() {
     runBlocking(CoroutineName("root")) {
-        test()
-    }
-}
-
-suspend fun test() {
-    coroutineScope {
-        launch(CoroutineName("launch1")) {
-            delay(1)
-            println()
-            delay(1)
-        }
-        launch(CoroutineName("launch2")) {
-            coroutineScope {
-                val a = async(CoroutineName("childAsync1")) {
-                    val coroutineVar = 1
-                    delay(100)
-                    println("coroutineVar=$coroutineVar")
-                    6
-                }
-
-                val b = async(CoroutineName("childAsync2")) {
-                    val coroutineVar = 2
-                    delay(100)
-                    println("coroutineVar=$coroutineVar")
-                    7
-                }
+        val a = async(CoroutineName("childAsync1")) {
+            delay(10)
+            val b = async(CoroutineName("childAsync2")) {
+                val coroutineVar = 2
+                delay(20)
+                "b"
             }
+            delay(5)
+            //Breakpoint!
+            "a" + b.await()
         }
-        withContext(Dispatchers.Default) {
-            coroutineScope {
-                val a = async(CoroutineName("async1")) {
-                    val coroutineVar = 1
-                    delay(100)
-                    println("coroutineVar=$coroutineVar") // ← breakpoint
-                    6
-                }
-
-                val b = async(CoroutineName("async2")) {
-                    val coroutineVar = 2
-                    delay(100)
-                    println("coroutineVar=$coroutineVar")
-                    7
-                }
-                //Breakpoint!
-                println("result: ${a.await() * b.await()}")
-            }
-        }
+        delay(100)
+        println()
     }
 }
