7a65757b7d54ca65b9f7860eee9805f2140eed8f
Aleksandr Sorotskii <aleksandr.sorotskii@jetbrains.com>
2025-06-25 22:26:19 +0200

    cleanup; unbundle vagrant plugin #PY-80258 Ready for Merge
    (cherry picked from commit 43f8dda8114f1d78e43f54d11d43b072fd5e7bda)
    
    GitOrigin-RevId: 16af4dc9778db240fb078cd8bdda77360b246c05

--- python/src/com/jetbrains/python/sdk/PySdkExt.kt
+++ python/src/com/jetbrains/python/sdk/PySdkExt.kt
@@ -94,6 +94,7 @@ fun configurePythonSdk(project: Project, module: Module, sdk: Sdk) {
   module.pythonSdk = sdk
   module.excludeInnerVirtualEnv(sdk)
 }
+
 // TODO: PythonInterpreterService: get system pythons
 /**
  * @param context used to get [BASE_DIR] in [com.jetbrains.python.sdk.flavors.VirtualEnvSdkFlavor.suggestLocalHomePaths]
@@ -141,24 +142,20 @@ fun resetSystemWideSdksDetectors() {
 }
 
 @Internal
-
 fun detectVirtualEnvs(module: Module?, existingSdks: List<Sdk>, context: UserDataHolder): List<PyDetectedSdk> =
   filterSuggestedPaths(VirtualEnvSdkFlavor.getInstance(), existingSdks, module, context)
 
 @Internal
-
 fun filterSharedCondaEnvs(module: Module?, existingSdks: List<Sdk>): List<Sdk> {
-  return existingSdks.filter { it.sdkType is PythonSdkType && PythonSdkUtil.isConda(it) && !it.isAssociatedWithAnotherModule(module) }
+  return existingSdks.filter { PythonSdkUtil.isPythonSdk(it) && PythonSdkUtil.isConda(it) && !it.isAssociatedWithAnotherModule(module) }
 }
 
 @Internal
-
 fun filterAssociatedSdks(module: Module, existingSdks: List<Sdk>): List<Sdk> {
-  return existingSdks.filter { it.sdkType is PythonSdkType && it.isAssociatedWithModule(module) }
+  return existingSdks.filter { PythonSdkUtil.isPythonSdk(it) && it.isAssociatedWithModule(module) }
 }
 
 @Internal
-
 fun detectAssociatedEnvironments(module: Module, existingSdks: List<Sdk>, context: UserDataHolder): List<PyDetectedSdk> =
   detectVirtualEnvs(module, existingSdks, context).filter { it.isAssociatedWithModule(module) }
 
@@ -172,7 +169,6 @@ fun createSdkByGenerateTask(
 ): Sdk = createSdkByGenerateTask(generateSdkHomePath, existingSdks, baseSdk, associatedProjectPath, suggestedSdkName, null)
 
 @Internal
-
 fun createSdkByGenerateTask(
   generateSdkHomePath: Task.WithResult<String, ExecutionException>,
   existingSdks: List<Sdk>,
@@ -365,7 +361,6 @@ var Project.pythonSdk: Sdk?
   }
 
 @Internal
-
 fun Module.excludeInnerVirtualEnv(sdk: Sdk) {
   val root = getInnerVirtualEnvRoot(sdk) ?: return
 
@@ -443,7 +438,6 @@ private val Sdk.sitePackagesDirectory: VirtualFile?
 val Sdk.sdkFlavor: PythonSdkFlavor<*> get() = getOrCreateAdditionalData().flavor
 
 @Internal
-
 fun Sdk.isLocatedInsideModule(module: Module?): Boolean {
   val moduleDir = module?.baseDir
   val sdkDir = homeDirectory
@@ -533,7 +527,6 @@ val Sdk.remoteSourcesLocalPath: Path
  * Configures [targetCommandLineBuilder] (sets a binary path and other stuff) so it could run python on this target
  */
 @Internal
-
 fun Sdk.configureBuilderToRunPythonOnTarget(targetCommandLineBuilder: TargetedCommandLineBuilder) {
   getOrCreateAdditionalData().flavorAndData.data.prepareTargetCommandLine(this, targetCommandLineBuilder)
 }
