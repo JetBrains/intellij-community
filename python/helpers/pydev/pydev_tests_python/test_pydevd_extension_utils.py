0478a12f4cd5c883edaf2915df068c00754ea971
Pavel Karateev <pavel.karateev@jetbrains.com>
2025-06-25 21:41:54 +0200

    PY-77993 run plugin import verification in a subprocess
    otherwise Python import caching may result
    in a test failure when executed together
    with other tests
    
    
    (cherry picked from commit 51da58244f782fe41adc06c2ff5b378cee767ab4)
    
    IJ-MR-166981
    
    GitOrigin-RevId: 4a814ceffecd0ed74f213b84d108583a1fce4d5c

--- python/helpers/pydev/pydev_tests_python/test_pydevd_extension_utils.py
+++ python/helpers/pydev/pydev_tests_python/test_pydevd_extension_utils.py
@@ -1,24 +1,38 @@
+import json
 import os
+import subprocess
+import sys
+from textwrap import dedent
 
-import pytest
 
-
-@pytest.fixture
-def with_my_extensions(monkeypatch):
+def test_could_load_extensions():
     my_extensions_path = os.path.join(os.path.dirname(__file__), "my_extensions")
-    monkeypatch.syspath_prepend(my_extensions_path)
 
+    code = dedent("""
+        import json
+        from _pydevd_bundle.pydevd_extension_utils import EXTENSION_MANAGER_INSTANCE as em
+        em._load_modules()
+        print(json.dumps([m.__name__ for m in em.loaded_extensions]))
+    """).strip()
 
-def test_could_load_extensions(with_my_extensions):
-    from _pydevd_bundle.pydevd_extension_utils import ExtensionManager
-
-    em = ExtensionManager()
-    em._load_modules()
-
-    expected_modules = [
+    expected_modules = {
         'pydevd_plugins.extensions.pydevd_plugin_test_events',
         'pydevd_plugins.extensions.pydevd_plugin_test_exttype',
         'pydevd_plugins.extensions.types.pydevd_plugin_numpy_types',
         'pydevd_plugins.extensions.types.pydevd_plugins_django_form_str',
-    ]
-    assert [m.__name__ for m in em.loaded_extensions] == expected_modules
+    }
+
+    result = subprocess.run(
+        [sys.executable, "-c", code],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        env={"PYTHONPATH": my_extensions_path}
+    )
+
+    loaded_modules = json.loads(result.stdout)
+
+    assert len(loaded_modules) == len(expected_modules)
+    assert result.returncode == 0
+    assert result.stderr == ""
+    assert set(loaded_modules) == expected_modules
