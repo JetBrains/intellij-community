0cebb3ffaf9dda17b852ce9971d9619ff6e5c355
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-06-25 18:05:09 +0300

    Revert "IJPL-193277 The Structure View: retrieve the editor from the data context"
    This reverts commit 6582879a7bb7ffb9d819b29dad804740e103fe80.
    
    GitOrigin-RevId: d930747ad79b24337acb427feb66e2d17ca69d5e

--- platform/lang-impl/src/com/intellij/ide/impl/StructureViewWrapperImpl.kt
+++ platform/lang-impl/src/com/intellij/ide/impl/StructureViewWrapperImpl.kt
@@ -54,6 +54,7 @@ import com.intellij.ui.content.ContentManagerEvent.ContentOperation
 import com.intellij.ui.content.ContentManagerListener
 import com.intellij.ui.switcher.QuickActionProvider
 import com.intellij.util.PlatformUtils
+import com.intellij.util.cancelOnDispose
 import com.intellij.util.concurrency.AppExecutorUtil
 import com.intellij.util.messages.Topic
 import com.intellij.util.ui.JBUI
@@ -644,8 +645,8 @@ class StructureViewWrapperImpl(
         AppMode.isRemoteDevHost() && project != null -> {
           // In RD, when focus is set to a frontend-component (e.g., tabs, editors, notification tool window),
           // on the backend it can be set to anything, unfortunately.
-          // So we fall back to the active editor (or the last active one), or else the structure view may stop updating completely.
-          PlatformDataKeys.LAST_ACTIVE_FILE_EDITOR.getData(asyncDataContext)?.file
+          // So we fall back to the active editor, or else the structure view may stop updating completely.
+          FileEditorManager.getInstance(project).selectedFiles.firstOrNull()
         }
         else -> null
       }
