fcc5e59573647f3d72c1af34bf9d8a636488b2fe
Bas Leijdekkers <bas.leijdekkers@jetbrains.com>
2025-06-26 17:52:30 +0200

    Java: fix incorrect "Static member qualifying type may not be annotated" (IDEA-210876)
    (cherry picked from commit 48d08a4d0d3db72fa458d7d67f6ccd72a435b4c8)
    
    IJ-CR-168601
    
    GitOrigin-RevId: ca9dfd435af2f6c580af42d6eb2061d01cad98cf

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/annotations/falseTypeUse.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/annotations/falseTypeUse.java
@@ -1,3 +1,4 @@
+import java.lang.annotation.*;
 
 @interface Ann { }
 
@@ -6,3 +7,18 @@ class Foo {
     return null;
   }
 }
+class Example {
+
+  public static class Foo {
+    public interface Bar<T> {}
+  }
+
+  public static <T> @One Foo.Bar<T> test1(T t) { return null; }
+  public static <T> <error descr="Static member qualifying type may not be annotated">@Two</error> Foo.Bar<T> test2(T t) { return null; }
+
+}
+@Target(ElementType.METHOD)
+@interface One {}
+
+@Target(ElementType.TYPE_USE)
+@interface Two {}
