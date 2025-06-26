4648976c82221a13e925aa0e5d3c22ec0bee0ec2
Vera Petrenkova <vera.petrenkova@jetbrains.com>
2025-06-26 11:54:53 +0200

    [find in files] IJ-CR-168030 IJPL-186012 Introduce `FrontendScopeChooserCombo` for remote development
    - Added APIs and services to allow the backend to expose available search scopes to the frontend (ScopeModelService)
    - created ScopesStateService that keeps map scopeId to ScopeDescriptor
    - added scopeId to FindModel for getting scope on the backend using ScopesStateService
    - refactored FindPopupScopeUIImpl for using FrontendScopeChooserCombo instead of ScopeChooserCombo in case when FindKey is enabled
    
    (cherry picked from commit 4d44d7aaadff23a0a3bb4262ea4d6f5a7dfe1f85)
    
    GitOrigin-RevId: 7a1174256fc723c3373c7924a4d028fd6e3d1285

---
+++ platform/scopes/backend/src/ScopeModelApiImpl.kt
@@ -0,0 +1,95 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.scopes.backend
+
+import com.intellij.ide.util.scopeChooser.*
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
+import com.intellij.platform.project.ProjectId
+import com.intellij.platform.project.findProjectOrNull
+import com.intellij.platform.rpc.backend.RemoteApiProvider
+import com.intellij.platform.scopes.ScopeModelApi
+import com.intellij.platform.scopes.SearchScopeData
+import com.intellij.platform.scopes.SearchScopesInfo
+import fleet.rpc.remoteApiDescriptor
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.launch
+import java.util.*
+
+private val LOG = logger<ScopesModelApiImpl>()
+
+internal class ScopesModelApiImpl : ScopeModelApi {
+  private val modelIdToModel = mutableMapOf<String, AbstractScopeModel>()
+  private val modelIdToScopes = mutableMapOf<String, ScopesState>()
+
+  override suspend fun createModelAndSubscribe(projectId: ProjectId, modelId: String): Flow<SearchScopesInfo>? {
+    val project = projectId.findProjectOrNull()
+    if (project == null) {
+      LOG.warn("Project not found for projectId: $projectId")
+      return null
+    }
+    val model = project.getService(ScopeService::class.java)
+      .createModel(EnumSet.of(
+        ScopeOption.FROM_SELECTION,
+        ScopeOption.USAGE_VIEW,
+        ScopeOption.LIBRARIES,
+        ScopeOption.SEARCH_RESULTS
+      ))
+    modelIdToModel[modelId] = model
+    val flow = subscribeToModelUpdates(model, modelId, project)
+    model.refreshScopes(null)
+    return flow
+  }
+
+
+  private fun subscribeToModelUpdates(model: AbstractScopeModel, modelId: String, project: Project): Flow<SearchScopesInfo> {
+    val flow = channelFlow {
+      model.addScopeModelListener(object : ScopeModelListener {
+        override fun scopesUpdated(scopes: ScopesSnapshot) {
+          val scopesStateMap = mutableMapOf<String, ScopeDescriptor>()
+          val scopesData = scopes.scopeDescriptors.mapNotNull { descriptor ->
+            val scopeId = UUID.randomUUID().toString()
+            val scopeData = SearchScopeData.from(descriptor, scopeId) ?: return@mapNotNull null
+            scopesStateMap[scopeData.scopeId] = descriptor
+            scopeData
+          }
+          var scopeState = modelIdToScopes[modelId]
+          if (scopeState == null) {
+            scopeState = ScopesStateService.getInstance(project).getOrCreateScopesState(project)
+            modelIdToScopes[modelId] = scopeState
+          }
+          scopeState.updateScopes(scopesStateMap)
+
+          val searchScopesInfo = SearchScopesInfo(scopesData, null, null, null)
+          launch {
+            send(searchScopesInfo)
+          }
+        }
+      })
+
+      awaitClose {}
+    }
+    return flow
+  }
+
+  override suspend fun updateModel(modelId: String, scopesInfo: SearchScopesInfo): Flow<SearchScopesInfo> {
+    TODO("Not yet implemented")
+  }
+
+  override suspend fun dispose(modelId: String) {
+    modelIdToScopes.remove(modelId)
+    val model = modelIdToModel[modelId] ?: return
+    Disposer.dispose(model)
+    modelIdToModel.remove(modelId)
+  }
+}
+
+private class ScopesStateApiProvider : RemoteApiProvider {
+  override fun RemoteApiProvider.Sink.remoteApis() {
+    remoteApi(remoteApiDescriptor<ScopeModelApi>()) {
+      ScopesModelApiImpl()
+    }
+  }
+}
---
+++ platform/scopes/src/com/intellij/platform/scopes/service/ScopeModelServiceImpl.kt
@@ -0,0 +1,60 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.scopes.service
+
+import com.intellij.ide.util.scopeChooser.ScopeDescriptor
+import com.intellij.ide.util.scopeChooser.ScopeModelService
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.project.Project
+import com.intellij.platform.project.projectId
+import com.intellij.platform.scopes.ScopeModelApi
+import com.intellij.platform.util.coroutines.childScope
+import fleet.rpc.client.RpcTimeoutException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import org.jetbrains.annotations.ApiStatus
+
+
+private val LOG = logger<ScopeModelServiceImpl>()
+
+@ApiStatus.Internal
+private class ScopeModelServiceImpl(private val project: Project, private val coroutineScope: CoroutineScope) : ScopeModelService {
+  private var scopeIdToDescriptor = mapOf<String, ScopeDescriptor>()
+
+  override fun loadItemsAsync(modelId: String, onFinished: suspend (Map<String, ScopeDescriptor>?) -> Unit) {
+    coroutineScope.childScope("ScopesStateService.subscribeToScopeStates").launch {
+      try {
+        val scopesFlow = ScopeModelApi.getInstance().createModelAndSubscribe(project.projectId(), modelId)
+        if (scopesFlow == null) {
+          LOG.warn("Failed to subscribe to model updates for modelId: $modelId")
+          onFinished(null)
+          return@launch
+        }
+        scopesFlow.collect { scopesInfo ->
+          val fetchedScopes = scopesInfo.getScopeDescriptors()
+          onFinished(fetchedScopes)
+          scopeIdToDescriptor = fetchedScopes
+        }
+      }
+      catch (e: RpcTimeoutException) {
+        LOG.warn("Failed to subscribe to model updates for modelId: $modelId", e)
+        onFinished(null)
+      }
+    }
+  }
+
+  override fun disposeModel(modelId: String) {
+    coroutineScope.launch {
+      try {
+        ScopeModelApi.getInstance().dispose(modelId)
+      }
+      catch (e: RpcTimeoutException) {
+        LOG.warn("Failed to dispose model for modelId: $modelId", e)
+      }
+    }
+  }
+
+  override fun getScopeById(scopeId: String): ScopeDescriptor? {
+    scopeIdToDescriptor.get(scopeId)?.let { return it }
+    return null
+  }
+}
