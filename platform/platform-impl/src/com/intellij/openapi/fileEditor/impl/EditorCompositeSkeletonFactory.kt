aff0e08091e6fb426996cea3e8ad2289bcc722e8
Sergei Kharitontcev-Beglov <sergei.kharitontcev-beglov@jetbrains.com>
2025-06-26 17:21:12 +0200

    [rdct] IJPL-193192: Do not use listener, just Atomics
    GitOrigin-RevId: 82976fc3e3ec36a22e2104a257ea0e0a06db8c4d

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorCompositeSkeletonFactory.kt
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorCompositeSkeletonFactory.kt
@@ -3,15 +3,12 @@ package com.intellij.openapi.fileEditor.impl
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
-import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.registry.Registry
-import com.intellij.ui.tabs.TabInfo
-import com.intellij.ui.tabs.TabsListener
+import com.intellij.util.AwaitCancellationAndInvoke
 import com.intellij.util.awaitCancellationAndInvoke
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.atomic.AtomicLong
@@ -21,56 +18,38 @@ import java.util.concurrent.atomic.AtomicLong
  *
  * Skeleton could be created in two cases:
  * 1. If the editor is not created in [SKELETON_DELAY] ms
- * 2. If the selected tab is changed from the tab with skeleton to the tab without a skeleton
- *
- * The first case is covered with [createSkeleton] method.
- * The second case is covered with [TabsListener] that listens for tab selection changes.
+ * 2. If the new tab (without an editor) is shown after a recently shown skeleton
  */
 @Service(Service.Level.PROJECT)
-internal class EditorCompositeSkeletonFactory(private val project: Project, private val coroutineScope: CoroutineScope) {
-  init {
-    val fileEditorManager = FileEditorManager.getInstance(project)
-    coroutineScope.launch {
-      (fileEditorManager as? FileEditorManagerImpl)?.splitterFlow?.collectLatest {
-        for (window in it.windows()) {
-          window.tabbedPane.tabs.addListener(object : TabsListener {
-            override fun selectionChanged(oldSelection: TabInfo?, newSelection: TabInfo?) {
-              val newComposite = newSelection?.composite ?: return
-              if (newComposite.isAvailable()) return // Skeleton is already removed
-
-              val newCompositePanel = newComposite.component as EditorCompositePanel
-              val oldComposite = oldSelection?.composite?.component as? EditorCompositePanel ?: return
-
-              if (oldComposite.skeleton != null && newCompositePanel.skeleton == null) {
-                newCompositePanel.setNewSkeleton(doCreateSkeleton(newCompositePanel.skeletonScope))
-              }
-            }
-          }, project)
-        }
-      }
-    }
-  }
-
-
+internal class EditorCompositeSkeletonFactory(project: Project, private val scope: CoroutineScope) {
   private val currentlyShownSkeleton = AtomicInteger(0)
   private val initialTime: AtomicLong = AtomicLong(System.currentTimeMillis())
 
   companion object {
     fun getInstance(project: Project): EditorCompositeSkeletonFactory = project.service()
   }
-  suspend fun createSkeleton(skeletonScope: CoroutineScope): EditorSkeleton {
-    delay(SKELETON_DELAY)
+  suspend fun createSkeleton(skeletonScope: CoroutineScope): EditorSkeleton? {
+    if (!Registry.`is`("editor.skeleton.animation.enabled")) return null
+    // if [currentlyShownSkeleton] equals 0, then there's no skeleton shown at the moment. We need to delay the skeleton creation to avoid flickering
+    if (currentlyShownSkeleton.get() == 0) {
+      delay(SKELETON_DELAY)
+    }
+
     currentlyShownSkeleton.incrementAndGet()
     initialTime.compareAndSet(-1, System.currentTimeMillis())
-
     return doCreateSkeleton(skeletonScope)
   }
 
+  @OptIn(AwaitCancellationAndInvoke::class)
   private fun doCreateSkeleton(skeletonScope: CoroutineScope): EditorSkeleton {
     skeletonScope.awaitCancellationAndInvoke {
-      // If the last skeleton is removed, the process of tab jumping is completed and the phase could be moved to show an initial animation
-      if (currentlyShownSkeleton.decrementAndGet() == 0) {
-        initialTime.set(-1)
+      scope.launch {
+        // delay actual deletion for skeleton to avoid flickering
+        delay(SKELETON_DELAY)
+        // If the last skeleton is removed, the process of tab jumping is completed and the phase could be moved to show an initial animation
+        if (currentlyShownSkeleton.decrementAndGet() == 0) {
+          initialTime.set(-1)
+        }
       }
     }
     return EditorSkeleton(skeletonScope, initialTime)
