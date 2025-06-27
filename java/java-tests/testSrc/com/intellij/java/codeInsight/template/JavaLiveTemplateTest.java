ecfb4551aabb01f51697c47e3231f1c55461ee07
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-06-27 14:46:29 +0200

    [java] IDEA-372997 Add tests for availability of Structured Concurrency templates
    (cherry picked from commit c60edd41d08a99bdb6bf79baaacbd9bca2bdc28b)
    
    
    (cherry picked from commit 5f2baebff482d5fe659dc608047fbbbe9a39de7e)
    
    IJ-MR-169535
    
    GitOrigin-RevId: 57676d655d39fec188c1524cd5c521835b849070

--- java/java-tests/testSrc/com/intellij/java/codeInsight/template/JavaLiveTemplateTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/template/JavaLiveTemplateTest.java
@@ -5,7 +5,6 @@ import com.intellij.JavaTestUtil;
 import com.intellij.codeInsight.CodeInsightSettings;
 import com.intellij.codeInsight.daemon.impl.quickfix.EmptyExpression;
 import com.intellij.codeInsight.lookup.Lookup;
-import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.template.*;
 import com.intellij.codeInsight.template.actions.SaveAsTemplateAction;
 import com.intellij.codeInsight.template.impl.*;
@@ -857,6 +856,50 @@ public class JavaLiveTemplateTest extends LiveTemplateTestCase {
             """);
       }
     );
+  }
+
+  public void testSTSSAvailable() {
+    final TemplateImpl template = TemplateSettings.getInstance().getTemplate("stss", "Java");
+    IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_20, () -> {
+      assertFalse(isApplicable("class Foo {void x(){ <caret>JUNK }", template));
+    });
+    IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_21, () -> {
+      assertFalse(isApplicable("class Foo {void x(){ <caret>JUNK }", template));
+    });
+    IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_21_PREVIEW, () -> {
+      assertTrue(isApplicable("class Foo {void x(){ <caret>JUNK }", template));
+    });
+    IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_24_PREVIEW, () -> {
+      assertTrue(isApplicable("class Foo {void x(){ <caret>JUNK }", template));
+    });
+    IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_25, () -> {
+      assertFalse(isApplicable("class Foo {void x(){ <caret>JUNK }", template));
+    });
+    IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_25_PREVIEW, () -> {
+      assertFalse(isApplicable("class Foo {void x(){ <caret>JUNK }", template));
+    });
+  }
+
+  public void testSTSAvailable() {
+    final TemplateImpl template = TemplateSettings.getInstance().getTemplate("sts", "Java");
+    IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_20, () -> {
+      assertFalse(isApplicable("class Foo {void x(){ <caret>JUNK }", template));
+    });
+    IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_21, () -> {
+      assertFalse(isApplicable("class Foo {void x(){ <caret>JUNK }", template));
+    });
+    IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_21_PREVIEW, () -> {
+      assertFalse(isApplicable("class Foo {void x(){ <caret>JUNK }", template));
+    });
+    IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_24_PREVIEW, () -> {
+      assertFalse(isApplicable("class Foo {void x(){ <caret>JUNK }", template));
+    });
+    IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_25, () -> {
+      assertFalse(isApplicable("class Foo {void x(){ <caret>JUNK }", template));
+    });
+    IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_25_PREVIEW, () -> {
+      assertTrue(isApplicable("class Foo {void x(){ <caret>JUNK }", template));
+    });
   }
 
   @Override
