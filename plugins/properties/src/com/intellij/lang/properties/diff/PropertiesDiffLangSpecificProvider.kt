41002f46158e81eb1750aac3445b133c5a8ab98d
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2025-06-30 09:43:01 +0000

    [Version Control. Diff and Merge] IJPL-192179 semantic difference for properties files algorithm redesign
    GitOrigin-RevId: 7b0b4a0289c47838afadb602fc4ad497bd499379

--- plugins/properties/src/com/intellij/lang/properties/diff/PropertiesDiffLangSpecificProvider.kt
+++ plugins/properties/src/com/intellij/lang/properties/diff/PropertiesDiffLangSpecificProvider.kt
@@ -1,4 +1,3 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.lang.properties.diff
 
 import com.intellij.diff.comparison.ComparisonManagerImpl
@@ -11,162 +10,176 @@ import com.intellij.diff.util.Side
 import com.intellij.lang.Language
 import com.intellij.lang.properties.PropertiesBundle
 import com.intellij.lang.properties.PropertiesLanguage
-import com.intellij.lang.properties.diff.data.PropertiesDiffContext
+import com.intellij.lang.properties.diff.data.FileInfoHolder
+import com.intellij.lang.properties.diff.data.PropertyInfo
 import com.intellij.lang.properties.diff.data.SemiOpenLineRange
-import com.intellij.lang.properties.diff.data.UnchangedRangeInfo
-import com.intellij.lang.properties.diff.util.toLastLineRange
-import com.intellij.lang.properties.diff.util.toLineRange
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.Project
 import org.jetbrains.annotations.Nls
 
+/**
+ * Extension for implementing the semantic-based algorithm for properties difference.
+ *
+ * Each property might be in 1 of 4 states:
+ * 1. Property (value) is unchanged. Algorithm ignores such property even if it was moved
+ * 2. Property (value) is changed. In this case algorithm provides the *modification* highlighting based on the property ranges in both versions on the file.
+ * 3. Property is added (there was no such key in the previous version of the file). In this case algorithm provides the *addition* highlighting.
+ * 4. Property is removed (there was no such key in the new version of the file). In this case algorithm provides the *deletion* highlighting.
+ *
+ * For cases *3* and *4* algorithm uses the other properties to find the anchor ranges in the opposite side.
+ *
+ * For example, text before:
+ * ```properties
+ * key1=value1
+ *
+ * # some comment
+ * ```
+ * text after:
+ * ```properties
+ * key1=value1
+ *
+ * # some comment
+ * key2=value2
+ * ```
+ * In this scenario algorithm will consider that property was added directly below the `key1=value1` property ignoring comments and blank lines.
+ */
 class PropertiesDiffLangSpecificProvider : DiffLangSpecificProvider {
-  override val shouldPrecalculateLineFragments: Boolean
-    get() = true
+  override val shouldPrecalculateLineFragments: Boolean = false
 
   override val description: @Nls(capitalization = Nls.Capitalization.Sentence) String
     get() = PropertiesBundle.message("ignore.unchanged.properties.text")
 
   override fun isApplicable(language: Language): Boolean = language == PropertiesLanguage.INSTANCE
 
-  override fun getPatchedLineFragments(project: Project?, fragmentList: List<List<LineFragment>>, textLeft: CharSequence, textRight: CharSequence, ignorePolicy: IgnorePolicy, highlightPolicy: HighlightPolicy, indicator: ProgressIndicator): List<List<LineFragment>> {
+  override fun getLineFragments(
+    project: Project?,
+    textLeft: CharSequence,
+    textRight: CharSequence,
+    ignorePolicy: IgnorePolicy,
+    highlightPolicy: HighlightPolicy,
+    indicator: ProgressIndicator,
+  ): List<List<LineFragment>> {
     indicator.checkCanceled()
     if (project == null) return emptyList()
-    val innerFragmentList = fragmentList.flatten()
-    val contextResult = ReadAction.compute<Result<PropertiesDiffContext>, Exception> {
-      try {
-        val fileInfoHolder = FileInfoHolder.create(project, textLeft, textRight, innerFragmentList)
-        val matchProcessor = MatchProcessor(fileInfoHolder, innerFragmentList)
-
-        Result.success(matchProcessor.collect())
-      }
-      catch (exception: Exception) {
-        Result.failure(exception)
-      }
-    }
-    val lineFragmentList = createLineFragmentList(contextResult.getOrNull(), ignorePolicy, highlightPolicy, indicator)
-
-    return if (lineFragmentList != null && lineFragmentList.isNotEmpty()) listOf(lineFragmentList) else emptyList()
+    val fileInfoHolder = ReadAction.compute<FileInfoHolder?, Exception> { FileInfoHolder.create(project, textLeft, textRight) }
+    if (fileInfoHolder == null) return emptyList()
+    val ranges = computePropertiesRanges(fileInfoHolder)
+    val lineFragmentList = ranges.sortedWith(compareBy({ it.start1 }, { it.start2 }))
+      .flatMap { range -> createLineFragments(range, fileInfoHolder, ignorePolicy, highlightPolicy, indicator) }
+    return if (lineFragmentList.isEmpty()) emptyList() else listOf(lineFragmentList)
   }
 
-  private fun createLineFragmentList(context: PropertiesDiffContext?, ignorePolicy: IgnorePolicy, highlightPolicy: HighlightPolicy, indicator: ProgressIndicator): List<LineFragment>? {
-    if (context == null) return null
-    val rangeList = createPatchedDiffRangeList(context)
-    val comparisonManager = ComparisonManagerImpl.getInstanceImpl()
-    val fileInfoHolder = context.fileInfoHolder
-    return rangeList.flatMap { range ->
-        comparisonManager.compareLinesInner(
-          range,
-          fileInfoHolder[Side.LEFT].fileText,
-          fileInfoHolder[Side.RIGHT].fileText,
-          fileInfoHolder[Side.LEFT].lineOffsets,
-          fileInfoHolder[Side.RIGHT].lineOffsets,
-          ignorePolicy.comparisonPolicy,
-          highlightPolicy.fragmentsPolicy,
-          indicator
-        )
-      }
+  private fun computePropertiesRanges(fileInfoHolder: FileInfoHolder): List<Range> {
+    val leftPropertiesMap = fileInfoHolder[Side.LEFT].propertyInfoMap
+    val rightPropertiesMap = fileInfoHolder[Side.RIGHT].propertyInfoMap
+    val commonPropertiesKeys = leftPropertiesMap.keys.intersect(rightPropertiesMap.keys)
+
+    val modifiedPropertiesRanges = computeModifiedPropertiesRanges(commonPropertiesKeys, leftPropertiesMap, rightPropertiesMap)
+    val deletedPropertiesRanges = computeAddedOrDeletedPropertiesRanges(commonPropertiesKeys, fileInfoHolder, Side.LEFT)
+    val addedPropertiesRanges = computeAddedOrDeletedPropertiesRanges(commonPropertiesKeys, fileInfoHolder, Side.RIGHT)
+
+    return modifiedPropertiesRanges + deletedPropertiesRanges + addedPropertiesRanges
   }
 
-  private fun createPatchedDiffRangeList(
-    propertiesDiffContext: PropertiesDiffContext,
+  private fun computeCommonProperties(commonPropertiesKeys: Set<String>, fileInfoHolder: FileInfoHolder, side: Side): List<PropertyInfo> {
+    return fileInfoHolder[side].propertyInfoMap.mapNotNull { if (it.key !in commonPropertiesKeys) null else it.value }
+  }
+
+  private fun computeModifiedPropertiesRanges(
+    commonPropertiesKeys: Set<String>,
+    leftProperties: Map<String, PropertyInfo>,
+    rightProperties: Map<String, PropertyInfo>,
   ): List<Range> {
-    val patchedLineFragmentList = propertiesDiffContext.disabledRangeMap.flatMap { (lineFragment, unchangedRangeList) ->
-      if (unchangedRangeList.isEmpty()) {
-        return@flatMap listOf(
-          Range(lineFragment.startLine1, lineFragment.endLine1, lineFragment.startLine2, lineFragment.endLine2)
-        )
-      }
-      val mergedDisabledRangeMap = unchangedRangeList.groupBy { it.side }.mapValues { (_, unchangedRangeList) -> mergeDisabledRanges(unchangedRangeList) }
-
-      val sideToRangeListMap = Side.entries.associateWith { side ->
-        excludeRanges(lineFragment.toLineRange(side), mergedDisabledRangeMap[side])
-      }
-
-      val zippedRangeList = sideToRangeListMap.getOrDefault(Side.LEFT, emptyList()) zip
-        sideToRangeListMap.getOrDefault(Side.RIGHT, emptyList())
-      val baseFragmentList = zippedRangeList.map { (leftRange, rightRange) ->
-        Range(leftRange.startLine, leftRange.endLine, rightRange.startLine, rightRange.endLine)
-      }
-      val suffixFragmentList = createSuffixFragments(
-        sideToRangeListMap,
-        lineFragment,
-        zippedRangeList.size
-      )
-      baseFragmentList + suffixFragmentList
+    val modifiedPropertiesRangeList = commonPropertiesKeys.mapNotNull { key ->
+      val propertyBefore = leftProperties.getValue(key)
+      val propertyAfter = rightProperties.getValue(key)
+      if (propertyBefore.value == propertyAfter.value) return@mapNotNull null
+      Range(propertyBefore.range.startLine, propertyBefore.range.endLine,
+            propertyAfter.range.startLine, propertyAfter.range.endLine)
     }
-
-    val modifiedLineFragmentList = propertiesDiffContext.modifiedPropertyList.map { propertyRange ->
-      Range(propertyRange.left.startLine, propertyRange.left.endLine, propertyRange.right.startLine, propertyRange.right.endLine)
-    }
-
-    return (patchedLineFragmentList + modifiedLineFragmentList).filter { !it.isEmpty }
+    return squashRanges(modifiedPropertiesRangeList)
   }
 
-  private fun createSuffixFragments(
-    sideToRangeListMap: Map<Side, List<SemiOpenLineRange>>,
-    lineFragment: LineFragment,
-    startIndex: Int,
-  ): List<Range> {
-    return Side.entries.flatMap { side ->
-      val changedRangeList = sideToRangeListMap[side] ?: return@flatMap emptyList()
-      if (startIndex >= changedRangeList.size) return@flatMap emptyList()
-      val lastLineRange = lineFragment.toLastLineRange(side)
-      changedRangeList.subList(startIndex, changedRangeList.size).map { changedRange ->
-        when (side) {
-          Side.LEFT ->
-            Range(changedRange.startLine, changedRange.endLine, lastLineRange.startLine, lastLineRange.endLine)
-          Side.RIGHT ->
-            Range(lastLineRange.startLine, lastLineRange.endLine, changedRange.startLine, changedRange.endLine)
+  private fun computeAddedOrDeletedPropertiesRanges(commonPropertiesKeys: Set<String>, fileInfoHolder: FileInfoHolder, currentSide: Side): List<Range> {
+    val commonPropertiesList = computeCommonProperties(commonPropertiesKeys, fileInfoHolder, currentSide)
+    val oppositeSide = currentSide.other()
+    val currentPropertiesMap = fileInfoHolder[currentSide].propertyInfoMap
+    val oppositePropertiesMap = fileInfoHolder[oppositeSide].propertyInfoMap
+
+    var anchorIndex = 0
+    val addedOrDeletedPropertiesRangeList = currentPropertiesMap.mapNotNull { (_, propertyInfo) ->
+      if (propertyInfo.key in commonPropertiesKeys) return@mapNotNull null
+      val propertyRange = propertyInfo.range
+
+      val anchorRange = if (commonPropertiesList.isEmpty()) {
+        SemiOpenLineRange(0, 0)
+      } else if (commonPropertiesList.first().range.startLine > propertyRange.startLine) {
+        extractAnchorRange(commonPropertiesList.first(), oppositePropertiesMap, false)
+      } else {
+        while (anchorIndex < commonPropertiesList.size) {
+          val nextAnchorIndex = anchorIndex + 1
+          val nextAnchorInfo = commonPropertiesList.getOrNull(nextAnchorIndex)
+          if (nextAnchorInfo == null || nextAnchorInfo.range.startLine > propertyRange.startLine) break
+          anchorIndex = nextAnchorIndex
         }
+        extractAnchorRange(commonPropertiesList[anchorIndex], oppositePropertiesMap, true)
       }
+      currentSide.createRange(propertyRange, anchorRange)
     }
+    return squashRanges(addedOrDeletedPropertiesRangeList)
   }
 
-  private fun excludeRanges(lineRange: SemiOpenLineRange, lineRangeList: List<SemiOpenLineRange>?): List<SemiOpenLineRange> {
-    fun MutableList<SemiOpenLineRange>.tryAddHighlightingRange(lineRange: SemiOpenLineRange, startLine: Int, endLine: Int) {
-      if (startLine >= endLine) return
-      val newRange = SemiOpenLineRange(startLine, endLine)
-      if (newRange !in lineRange) return
-      this.add(newRange)
-    }
-
-    if (lineRangeList == null || lineRangeList.isEmpty()) return listOf(lineRange)
-    val result = mutableListOf<SemiOpenLineRange>()
-    var lastEndLine = lineRange.startLine
-
-    for (range in lineRangeList) {
-      result.tryAddHighlightingRange(lineRange, lastEndLine, range.startLine)
-      lastEndLine = range.endLine
-    }
-
-    result.tryAddHighlightingRange(lineRange, lastEndLine, lineRange.endLine)
-
-    return result
+  private fun extractAnchorRange(propertyInfo: PropertyInfo, oppositePropertiesMap: Map<String, PropertyInfo>, isEndBound: Boolean): SemiOpenLineRange {
+    val range = oppositePropertiesMap.getValue(propertyInfo.key).range
+    val line = if (isEndBound) range.endLine else range.startLine
+    return SemiOpenLineRange(line, line)
   }
 
-  private fun mergeDisabledRanges(unchangedRangeInfoList: List<UnchangedRangeInfo>): List<SemiOpenLineRange> {
-    fun List<UnchangedRangeInfo>.getRange(leftIndex: Int, rightIndex: Int): SemiOpenLineRange {
-      val startLine = this[leftIndex].range.startLine
-      val endLine = this[rightIndex - 1].range.endLine
-      return SemiOpenLineRange(startLine, endLine)
-    }
-
-    if (unchangedRangeInfoList.isEmpty()) return emptyList()
-    val result = mutableListOf<SemiOpenLineRange>()
+  private fun squashRanges(rangeList: List<Range>): List<Range> {
     var leftIndex = 0
-    var rightIndex = 0
-    while (rightIndex < unchangedRangeInfoList.size) {
-      if (rightIndex > 0 && unchangedRangeInfoList[rightIndex].range.startLine != unchangedRangeInfoList[rightIndex - 1].range.endLine) {
-
-        result.add(unchangedRangeInfoList.getRange(leftIndex, rightIndex))
-        leftIndex = rightIndex
+    val result = mutableListOf<Range>()
+    while (leftIndex < rangeList.size) {
+      var rightIndex = leftIndex + 1
+      while (rightIndex < rangeList.size &&
+             rangeList[rightIndex - 1].end1 == rangeList[rightIndex].start1 &&
+             rangeList[rightIndex - 1].end2 == rangeList[rightIndex].start2) {
+        rightIndex++
       }
-      rightIndex++
+      val newRange = if (leftIndex == rightIndex - 1) {
+        rangeList[leftIndex]
+      }
+      else {
+        Range(rangeList[leftIndex].start1, rangeList[rightIndex - 1].end1, rangeList[leftIndex].start2, rangeList[rightIndex - 1].end2)
+      }
+      result.add(newRange)
+      leftIndex = rightIndex
     }
-
-    result.add(unchangedRangeInfoList.getRange(leftIndex, rightIndex))
     return result
+  }
+
+  private fun Side.createRange(first: SemiOpenLineRange, second: SemiOpenLineRange): Range = when (this) {
+    Side.LEFT -> Range(first.startLine, first.endLine, second.startLine, second.endLine)
+    Side.RIGHT -> Range(second.startLine, second.endLine, first.startLine, first.endLine)
+  }
+
+  private fun createLineFragments(
+    range: Range,
+    fileInfoHolder: FileInfoHolder,
+    ignorePolicy: IgnorePolicy,
+    highlightPolicy: HighlightPolicy,
+    indicator: ProgressIndicator,
+  ): List<LineFragment> {
+    val manager = ComparisonManagerImpl.getInstanceImpl()
+    return manager.compareLinesInner(
+      range,
+      fileInfoHolder[Side.LEFT].fileText,
+      fileInfoHolder[Side.RIGHT].fileText,
+      fileInfoHolder[Side.LEFT].lineOffsets,
+      fileInfoHolder[Side.RIGHT].lineOffsets,
+      ignorePolicy.comparisonPolicy,
+      highlightPolicy.fragmentsPolicy,
+      indicator
+    )
   }
 }
