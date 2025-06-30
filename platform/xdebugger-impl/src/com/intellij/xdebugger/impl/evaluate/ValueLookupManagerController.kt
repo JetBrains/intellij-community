f836013c99b480feb473144c0d1ac233d89803fe
Maksim Zuev <maksim.zuev@jetbrains.com>
2025-06-30 10:40:12 +0200

    [rd debugger] Rewrite ValueLookupManager from rhizome to RPC
    GitOrigin-RevId: 579fdc4bdd37a14dcbf18b205f0d05bb3cf84e6b

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/evaluate/ValueLookupManagerController.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/evaluate/ValueLookupManagerController.kt
@@ -8,98 +8,46 @@ import com.intellij.openapi.editor.EditorMouseHoverPopupManager
 import com.intellij.openapi.editor.event.EditorMouseEvent
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
-import com.intellij.platform.kernel.EntityTypeProvider
-import com.intellij.platform.project.ProjectEntity
-import com.intellij.platform.project.asEntity
-import com.jetbrains.rhizomedb.EID
-import com.jetbrains.rhizomedb.Entity
-import com.jetbrains.rhizomedb.EntityType
-import fleet.kernel.DurableEntityType
-import fleet.kernel.change
-import fleet.kernel.shared
+import com.intellij.platform.debugger.impl.rpc.ValueHintEvent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import org.jetbrains.annotations.ApiStatus
-import java.util.concurrent.atomic.AtomicBoolean
 
-private class XDebuggerValueLookupEntityTypesProvider : EntityTypeProvider {
-  override fun entityTypes(): List<EntityType<*>> {
-    return listOf(
-      XDebuggerValueLookupListeningStartedEntity,
-      XDebuggerValueLookupHideHintsRequestEntity,
-    )
-  }
-}
-
-@ApiStatus.Internal
-data class XDebuggerValueLookupListeningStartedEntity(override val eid: EID) : Entity {
-  val projectEntity by Project
-
-  companion object : DurableEntityType<XDebuggerValueLookupListeningStartedEntity>(
-    XDebuggerValueLookupListeningStartedEntity::class.java.name,
-    "com.intellij",
-    ::XDebuggerValueLookupListeningStartedEntity
-  ) {
-    val Project = requiredRef<ProjectEntity>("project")
-  }
-}
-
-@ApiStatus.Internal
-data class XDebuggerValueLookupHideHintsRequestEntity(override val eid: EID) : Entity {
-  val projectEntity by Project
-
-  companion object : DurableEntityType<XDebuggerValueLookupHideHintsRequestEntity>(
-    XDebuggerValueLookupHideHintsRequestEntity::class.java.name,
-    "com.intellij",
-    ::XDebuggerValueLookupHideHintsRequestEntity
-  ) {
-    val Project = requiredRef<ProjectEntity>("project")
-  }
-}
 
 @ApiStatus.Internal
 @Service(Service.Level.PROJECT)
-class ValueLookupManagerController(private val project: Project, private val cs: CoroutineScope) {
-  private val listeningStarted = AtomicBoolean(false)
+class ValueLookupManagerController(private val cs: CoroutineScope) {
+  private val events = MutableSharedFlow<ValueHintEvent>()
+  private val channel = Channel<ValueHintEvent>(Channel.UNLIMITED)
+
+  init {
+    cs.launch {
+      channel.consumeEach {
+        events.emit(it)
+      }
+    }
+  }
+
+  fun getEventsFlow(): Flow<ValueHintEvent> = events.asSharedFlow()
 
   /**
    * Starts [ValueLookupManager] listening for events (e.g. mouse movement) to trigger evaluation popups
    */
   fun startListening() {
-    if (!listeningStarted.compareAndSet(false, true)) {
-      return
-    }
-    cs.launch(Dispatchers.IO) {
-      val projectEntity = project.asEntity()
-      change {
-        shared {
-          val alreadyExists = XDebuggerValueLookupListeningStartedEntity.all().any { it.projectEntity == projectEntity }
-          if (!alreadyExists) {
-            XDebuggerValueLookupListeningStartedEntity.new {
-              it[XDebuggerValueLookupListeningStartedEntity.Project] = projectEntity
-            }
-          }
-        }
-      }
-    }
+    channel.trySend(ValueHintEvent.StartListening)
   }
 
   /**
    * Requests [ValueLookupManager] to hide current evaluation hints
    */
   fun hideHint() {
-    cs.launch(Dispatchers.IO) {
-      val projectEntity = project.asEntity()
-      change {
-        shared {
-          val projectEntity = projectEntity
-          XDebuggerValueLookupHideHintsRequestEntity.new {
-            it[XDebuggerValueLookupHideHintsRequestEntity.Project] = projectEntity
-          }
-        }
-      }
-    }
+    channel.trySend(ValueHintEvent.HideHint)
   }
 
   fun showEditorInfoTooltip(event: EditorMouseEvent?) {
