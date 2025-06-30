55adc62299f1a3bd23c59cb3c1f8437429800d68
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-06-30 17:16:40 +0200

    [i18n] Fix hardcoded string literals
    GitOrigin-RevId: de932bb8509ba9531fae90f8d7753303e5f8aef0

--- platform/platform-impl/src/com/intellij/configurationStore/SavingTracker.kt
+++ platform/platform-impl/src/com/intellij/configurationStore/SavingTracker.kt
@@ -1,12 +1,13 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.configurationStore
 
+import com.intellij.ide.IdeBundle
 import com.intellij.openapi.project.Project
 import com.intellij.platform.backend.observation.ActivityTracker
 import org.jetbrains.annotations.Nls
 
 private class SavingTracker : ActivityTracker {
-  override val presentableName: @Nls String = "saving"
+  override val presentableName: @Nls String = IdeBundle.message("saving.activity.tracker.name")
 
   override suspend fun isInProgress(project: Project): Boolean {
     return false
