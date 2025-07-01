a57f67604acaf02283e5b1f1d2ee85f2a629a3c3
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-07-01 16:55:19 +0200

    [threading] KMT-1329: Do not spin modal progress dialog under write-intent read action
    (cherry picked from commit ef0405066de3ad830bee5d70b055ceea95badf9e)
    
    IJ-CR-168169
    
    GitOrigin-RevId: f6fefa082ce0a198210b2c5f5efe422839274c0d

--- platform/platform-impl/src/com/intellij/openapi/progress/util/ProgressWindow.java
+++ platform/platform-impl/src/com/intellij/openapi/progress/util/ProgressWindow.java
@@ -5,6 +5,7 @@ import com.intellij.ide.IdeEventQueue;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.InstantShutdown;
+import com.intellij.openapi.application.UiDispatcherKind;
 import com.intellij.openapi.application.ex.ApplicationEx;
 import com.intellij.openapi.application.ex.ApplicationManagerEx;
 import com.intellij.openapi.application.impl.LaterInvocator;
@@ -179,7 +180,9 @@ public class ProgressWindow extends ProgressIndicatorBase implements BlockingPro
     // executed in a small amount of time. Problem: UI blinks and looks ugly if we show progress dialog that disappears shortly
     // for each of them. The solution is to postpone the tasks of showing progress dialog. Hence, it will not be shown at all
     // if the task is already finished when the time comes.
-    EdtScheduler.getInstance().schedule(delayInMillis, getModalityState(), () -> {
+
+    // Modal progresses must run on EDT without write-intent lock, hence we pass RELAX dispatcher
+    EdtScheduler.getInstance().schedule(delayInMillis, getModalityState(), UiDispatcherKind.RELAX, () -> {
       if (isRunning()) {
         showDialog();
       }
