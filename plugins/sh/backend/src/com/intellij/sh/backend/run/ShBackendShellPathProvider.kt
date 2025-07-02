2d5e2597622b2aaa35a17766a46d3560d0abe5f7
Milica Sladakovic <milica.sladakovic@jetbrains.com>
2025-07-02 12:43:52 +0200

    IJPL-184315 Remove unnecessary `ShBackendShellPathProvider`
    No usages after refactoring.
    
    GitOrigin-RevId: 2fbe6926c0f4de5d3890b8cc9af67deef21a5c59

--- plugins/sh/backend/src/com/intellij/sh/backend/run/ShBackendShellPathProvider.kt
+++
@@ -1,7 +0,0 @@
-package com.intellij.sh.backend.run
-
-import com.intellij.sh.run.ShDefaultShellPathProvider
-
-interface ShBackendShellPathProvider : ShDefaultShellPathProvider {
-  override fun getDefaultShell(): String
-}
