2fd0b2d8dba3b01d731a28a54bc00df838b82a4b
Kate Botsman <kate.botsman@jetbrains.com>
2025-07-02 16:53:19 +0200

    IJPL-194248 Fix race condition causing freeze on startup
    Editors are opened in parallel during startup.
    It can happen that `startupFileEntry` is not set before the next file is created.
    
    In such cases, the file goes through the cache to look for an already created `VirtualFile` instance.
    We compare files using the `path`, which we obtain from either `fileProperties` or `startupFileEntry`.
    If neither is set, we wait for `fileProperties` to be received from the backend (with 3s timeout), which can cause a freeze.
    
    (cherry picked from commit 8f266311492057c4061281cc31e8752850d615b8)
    
    GitOrigin-RevId: 37e94c0abbcd9fc869c38ed0a10b0531f449c34e

--- platform/platform-impl/src/com/intellij/openapi/vfs/impl/FileIdAdapterImpl.kt
+++ platform/platform-impl/src/com/intellij/openapi/vfs/impl/FileIdAdapterImpl.kt
@@ -8,10 +8,11 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.openapi.vfs.VirtualFileWithId
 import com.intellij.openapi.vfs.newvfs.ManagingFS
+import com.intellij.platform.fileEditor.FileEntry
 
 private class FileIdAdapterImpl : FileIdAdapter {
 
-  override fun getFile(id: Int): VirtualFile? = VirtualFileManager.getInstance().findFileById(id)
+  override fun getFile(id: Int, fileEntry: FileEntry?): VirtualFile? = VirtualFileManager.getInstance().findFileById(id)
 
   override fun getId(file: VirtualFile): Int? = (file as? VirtualFileWithId)?.id
   override fun getManagingFsCreationTimestamp(file: VirtualFile): Long {
