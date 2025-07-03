ed3d62cd9e0f2cfaf468ed44ce1ddeb0e58e7607
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2025-07-03 16:43:35 +0200

    IJPL-175939 Consider deprecating the Thumbnails tool window
    - Remove content from intellij.devkit.images module and related stuff
    
    GitOrigin-RevId: 84d37140f2e01abadd8dadef8d202fd962b132e7

--- plugins/devkit/intellij.devkit.images/src/AbstractThemeFilter.java
+++ plugins/devkit/intellij.devkit.images/src/AbstractThemeFilter.java
@@ -1,24 +1,9 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.images;
 
-import com.intellij.openapi.vfs.VirtualFile;
-import org.intellij.images.thumbnail.actions.ThemeFilter;
-
-public abstract class AbstractThemeFilter implements ThemeFilter {
-  private final Theme myTheme;
-
-  protected AbstractThemeFilter(Theme theme) {
-    myTheme = theme;
-  }
-
-  @Override
-  public String getDisplayName() {
-    return myTheme.getDisplayName();
-  }
-
-  @Override
-  public boolean accepts(VirtualFile file) {
-    return myTheme.accepts(file);
-  }
-
+/**
+ * @deprecated Should be removed together with the module
+ */
+@Deprecated(forRemoval = true)
+abstract class AbstractThemeFilter {
 }
--- plugins/devkit/intellij.devkit.images/src/DefaultThemeFilter.java
+++
@@ -1,9 +0,0 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.idea.devkit.images;
-
-final class DefaultThemeFilter extends AbstractThemeFilter {
-
-  DefaultThemeFilter() {
-    super(Theme.WHITE);
-  }
-}
