65e2e151a81356884f292942fb678a3af09bdaec
Daniel Bertoldi <dsdanielbertoldi@gmail.com>
2025-07-04 16:07:46 +0200

    [JEWEL-838] Remove API Dumps from Gradle Builds
    Validate api dumps against data classes
    
    removing some leftover code
    
    oops left some commented out code
    
    undo unrelated code change
    
    remove check api from ui-test:
    
    more code fixes
    
    more code fixes
    
    more code fixes
    
    applying change suggestions
    
    fixing more tiny stuff
    
    remove developer_dir from pre_push run config (AGAIN)
    
    use sourcetask and fix unused code
    
    updated ValidatePublicApiTask output to give more context about the problem with data classes in public api
    
    closes https://github.com/JetBrains/intellij-community/pull/3119
    
    (cherry picked from commit 3e474f3b7cff6119b99d5e3f13709527a65f8f2c)
    
    
    (cherry picked from commit 0448389aab948229e76eba26085d9ca91dc2653e)
    
    IJ-MR-173299
    
    GitOrigin-RevId: f476253ae215e5e0e6c8e341d15a4c0e39c4ac1a

--- platform/jewel/buildSrc/src/main/kotlin/jewel-check-public-api.gradle.kts
+++ platform/jewel/buildSrc/src/main/kotlin/jewel-check-public-api.gradle.kts
@@ -1,21 +1,8 @@
-@file:Suppress("UnstableApiUsage")
-
 import org.jetbrains.jewel.buildlogic.apivalidation.ApiValidationExtension
 
 plugins {
-    id("org.jetbrains.kotlinx.binary-compatibility-validator")
     kotlin("jvm")
 }
-
-apiValidation {
-    /**
-     * Set of annotations that exclude API from being public. Typically, it is all kinds of `@InternalApi` annotations
-     * that mark effectively private API that cannot be actually private for technical reasons.
-     */
-    nonPublicMarkers.add("org.jetbrains.jewel.InternalJewelApi")
-}
-
-kotlin { compilerOptions { freeCompilerArgs.add("-Xexplicit-api=strict") } }
 
 val extension = project.extensions.create("publicApiValidation", ApiValidationExtension::class.java)
 
@@ -24,9 +11,12 @@ with(extension) { excludedClassRegexes.convention(emptySet()) }
 tasks {
     val validatePublicApi =
         register<ValidatePublicApiTask>("validatePublicApi") {
-            include { it.file.extension == "api" }
-            source(project.fileTree("api"))
-            dependsOn(named("apiCheck"))
+            source(
+                project.fileTree(".") {
+                    include("api-dump*.txt")
+                }
+            )
+
             excludedClassRegexes = project.the<ApiValidationExtension>().excludedClassRegexes.get()
         }
 
