42d35e5ded44825f20a942b4a05b0b4cff51fb38
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2025-07-04 19:48:33 +0200

    [maven] IDEA-375474 fix SslDelegateHandlerStateMachineTest
    GitOrigin-RevId: f1525cead20d7361fef16b7a5b36b3f31244d8fd

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/server/ssl/SslDelegateHandlerStateMachineTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/server/ssl/SslDelegateHandlerStateMachineTest.kt
@@ -6,11 +6,9 @@ import com.intellij.util.ArrayUtilRt
 import com.intellij.util.ResourceUtil
 import junit.framework.TestCase
 import java.io.ByteArrayOutputStream
-import java.io.IOException
 import java.util.*
 
 class SslDelegateHandlerStateMachineTest : UsefulTestCase() {
-
 
   fun testResultOk() {
     val data = fromFile("ssl_remote_query.txt");
@@ -18,7 +16,9 @@ class SslDelegateHandlerStateMachineTest : UsefulTestCase() {
     val out = ByteArrayOutputStream()
     machine.output = out
     data.forEach { machine.addLine(it) }
-    TestCase.assertEquals(fromFile("ssl_remote_query_response_ok.txt").joinToString(""), out.toString())
+    val expected = fromFile("ssl_remote_query_response_ok.txt").joinToString("")
+    val actual = out.toString()
+    assertNormalizedEquals(expected, actual)
   }
 
   fun testResultFail() {
@@ -27,12 +27,18 @@ class SslDelegateHandlerStateMachineTest : UsefulTestCase() {
     val out = ByteArrayOutputStream()
     machine.output = out
     data.forEach { machine.addLine(it) }
-    TestCase.assertEquals(fromFile("ssl_remote_query_response_error.txt").joinToString(""), out.toString())
+    val expected = fromFile("ssl_remote_query_response_error.txt").joinToString("")
+    val actual = out.toString()
+    assertNormalizedEquals(expected, actual)
   }
 
+  private fun assertNormalizedEquals(expected: String, actual: String) {
+    TestCase.assertEquals(expected.normalizeLineEndings(), actual.normalizeLineEndings())
+  }
 
-  @Throws(IOException::class)
-  protected fun fromFile(file: String): Array<String> {
+  private fun String.normalizeLineEndings(): String = this.replace("\r\n", "\n").replace("\r", "\n")
+
+  private fun fromFile(file: String): Array<String> {
     ResourceUtil.getResourceAsStream(SslDelegateHandlerStateMachineTest::class.java.classLoader, "org/jetbrains/maven/server/ssl", file).use { stream ->
       Scanner(stream).use { scanner ->
         val result: MutableList<String> = ArrayList()
