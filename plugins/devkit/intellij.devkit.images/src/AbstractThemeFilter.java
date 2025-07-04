651177ae9dcd72f877db92035af5338431c7d3e7
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2025-07-04 13:49:02 +0200

    IJPL-175939 Consider deprecating the Thumbnails tool window
    - Remove intellij.devkit.images module
    
    # Conflicts:
    #	build/bazel-targets.json
    
    GitOrigin-RevId: bcec601f46e0e7cbbca23716f94c7ae3cfbdb435

--- plugins/devkit/intellij.devkit.images/src/AbstractThemeFilter.java
+++
@@ -1,9 +0,0 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.idea.devkit.images;
-
-/**
- * @deprecated Should be removed together with the module
- */
-@Deprecated(forRemoval = true)
-abstract class AbstractThemeFilter {
-}
