b2aa5b7bac97ba9f9ba1ad5e12dba691dc435074
Vitaly Legchilkin <vitaly.legchilkin@jetbrains.com>
2025-07-07 19:14:57 +0200

    [python] support of two ways of possible responses from Hatch (make it internal) (PY-60410)
    (cherry picked from commit 79fd57310a7861e6ef4923296749d264b71df43c)
    (cherry picked from commit 61fdf630bd5ca133a59a6358112c0f6eecd0bafb)
    
    GitOrigin-RevId: 8ab1ad1e04d38c275bd28c480f5eafec5e4deb12

--- python/python-hatch/src/com/intellij/python/hatch/cli/utils.kt
+++ python/python-hatch/src/com/intellij/python/hatch/cli/utils.kt
@@ -7,7 +7,7 @@ import com.intellij.platform.eel.provider.utils.stderrString
  * Hatch has two versions of messages in the stop function: "Expected prefix ..." (old_message) and "Finished expected prefix ..." (final_text)
  * https://github.com/pypa/hatch/blob/4ebce0e1fe8bf0fcdef587a704c207a063d72575/src/hatch/cli/terminal.py#L65
  */
-fun EelProcessExecutionResult.isSuccessStop(expectedPrefix: String): Boolean {
+internal fun EelProcessExecutionResult.isSuccessStop(expectedPrefix: String): Boolean {
   require(expectedPrefix.isNotEmpty()) { "Expected prefix can't be empty" }
   if (exitCode != 0) return false
 
