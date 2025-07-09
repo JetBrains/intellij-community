5b5082fa987384370c9613a69dc7ceb3eb6b4218
Sebastiano Poggi <sebp@google.com>
2025-07-09 10:17:11 +0200

    [JEWEL-874] Fix `reverseLayout` on non-lazy scrolling containers
    This fixes the behaviour of `reverseLayout` on non-lazy scrolling
    containers. We had forgotten to pass the flag value to the
    `reverseScrolling` parameter of the `vertical`/`horizontalScroll`
    modifiers. Lazy overloads do not provide their own scrolling modifier,
    and thus worked fine.
    
    I also took the opportunity to add a bunch of KDoc clarifying how the
    scrollbars & styling works, and fixed the scrollable containers' KDoc,
    which were wrong or imprecise in several points.

--- platform/jewel/ui/src/main/kotlin/org/jetbrains/jewel/ui/component/styling/ScrollbarStyling.kt
+++ platform/jewel/ui/src/main/kotlin/org/jetbrains/jewel/ui/component/styling/ScrollbarStyling.kt
@@ -11,7 +11,18 @@ import androidx.compose.ui.unit.Dp
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 import org.jetbrains.jewel.foundation.GenerateDataFunctions
+import org.jetbrains.jewel.ui.theme.scrollbarStyle
 
+/**
+ * Defines the visual style for a scrollbar. This includes colors, metrics, and behaviors.
+ *
+ * The style can be accessed through [`JewelTheme.scrollbarStyle`][scrollbarStyle].
+ *
+ * @param colors The set of colors used to paint the scrollbar.
+ * @param metrics The sizing and shape properties of the scrollbar.
+ * @param trackClickBehavior The behavior of the scrollbar track when clicked.
+ * @param scrollbarVisibility The visibility behavior of the scrollbar.
+ */
 @Stable
 @GenerateDataFunctions
 public class ScrollbarStyle(
@@ -54,6 +65,22 @@ public class ScrollbarStyle(
     public companion object
 }
 
+/**
+ * Contains the color properties for a [ScrollbarStyle].
+ *
+ * @param thumbBackground The background color of the scrollbar thumb.
+ * @param thumbBackgroundActive The background color of the scrollbar thumb when active (e.g., being dragged).
+ * @param thumbOpaqueBackground The background color of the scrollbar thumb when the scrollbar is in opaque mode.
+ * @param thumbOpaqueBackgroundHovered The background color of the scrollbar thumb when hovered and in opaque mode.
+ * @param thumbBorder The border color of the scrollbar thumb.
+ * @param thumbBorderActive The border color of the scrollbar thumb when active.
+ * @param thumbOpaqueBorder The border color of the scrollbar thumb when in opaque mode.
+ * @param thumbOpaqueBorderHovered The border color of the scrollbar thumb when hovered and in opaque mode.
+ * @param trackBackground The background color of the scrollbar track.
+ * @param trackBackgroundExpanded The background color of the scrollbar track when expanded.
+ * @param trackOpaqueBackground The background color of the scrollbar track when in opaque mode.
+ * @param trackOpaqueBackgroundHovered The background color of the scrollbar track when hovered and in opaque mode.
+ */
 @Immutable
 @GenerateDataFunctions
 public class ScrollbarColors(
@@ -108,8 +135,8 @@ public class ScrollbarColors(
         return result
     }
 
-    override fun toString(): String {
-        return "ScrollbarColors(" +
+    override fun toString(): String =
+        "ScrollbarColors(" +
             "thumbBackground=$thumbBackground, " +
             "thumbBackgroundActive=$thumbBackgroundActive, " +
             "thumbOpaqueBackground=$thumbOpaqueBackground, " +
@@ -123,11 +150,16 @@ public class ScrollbarColors(
             "trackOpaqueBackground=$trackOpaqueBackground, " +
             "trackOpaqueBackgroundHovered=$trackOpaqueBackgroundHovered" +
             ")"
-    }
 
     public companion object
 }
 
+/**
+ * Defines the metrics for a [ScrollbarStyle].
+ *
+ * @param thumbCornerSize The corner size of the scrollbar thumb.
+ * @param minThumbLength The minimum length of the scrollbar thumb.
+ */
 @Stable
 @GenerateDataFunctions
 public class ScrollbarMetrics(public val thumbCornerSize: CornerSize, public val minThumbLength: Dp) {
@@ -155,16 +187,56 @@ public class ScrollbarMetrics(public val thumbCornerSize: CornerSize, public val
     public companion object
 }
 
+/** Defines the visibility behavior of a scrollbar. */
 public sealed interface ScrollbarVisibility {
+    /** The thickness of the scrollbar track when not expanded. */
     public val trackThickness: Dp
+
+    /** The thickness of the scrollbar track when expanded (e.g., on hover). */
     public val trackThicknessExpanded: Dp
+
+    /** The padding around the scrollbar track. */
     public val trackPadding: PaddingValues
+
+    /** The padding around the scrollbar track when a border is visible. */
     public val trackPaddingWithBorder: PaddingValues
+
+    /** The duration for the track color animation. */
     public val trackColorAnimationDuration: Duration
+
+    /** The duration for the track expansion animation. */
     public val expandAnimationDuration: Duration
+
+    /** The duration for the thumb color animation. */
     public val thumbColorAnimationDuration: Duration
+
+    /**
+     * The duration to wait before hiding the scrollbar after scrolling has stopped. When using a
+     * [org.jetbrains.jewel.ui.component.VerticallyScrollableContainer] or a
+     * [org.jetbrains.jewel.ui.component.HorizontallyScrollableContainer], the linger starts after the scroll ends and
+     * the user stops moving the mouse over the content.
+     */
     public val lingerDuration: Duration
 
+    /**
+     * A [ScrollbarVisibility] that keeps the scrollbar always visible.
+     *
+     * This is equivalent to the _Always_ setting in macOS' _System Settings > Appearance > Show scroll bars_ settings.
+     * It is also used when the _Automatically based on mouse or trackpad_ option is selected, and there is a mouse
+     * connected to the computer.
+     *
+     * Note that this behavior is usually only used on macOS, and it causes the scrollbars to be laid out to the side of
+     * a [org.jetbrains.jewel.ui.component.VerticallyScrollableContainer]'s main content (or below, for a
+     * [org.jetbrains.jewel.ui.component.HorizontallyScrollableContainer]), instead of overlaid.
+     *
+     * @param trackThickness The thickness of the scrollbar track.
+     * @param trackPadding The padding around the scrollbar track.
+     * @param trackPaddingWithBorder The padding when a border is visible.
+     * @param thumbColorAnimationDuration The duration for the thumb color animation.
+     * @param trackColorAnimationDuration The duration for the track color animation.
+     * @param scrollbarBackgroundColorLight The background color in light theme.
+     * @param scrollbarBackgroundColorDark The background color in dark theme.
+     */
     @GenerateDataFunctions
     public class AlwaysVisible(
         public override val trackThickness: Dp,
@@ -231,6 +303,22 @@ public sealed interface ScrollbarVisibility {
         public companion object
     }
 
+    /**
+     * A [ScrollbarVisibility] that shows the scrollbar only when scrolling, overlaid on top of the content.
+     *
+     * This is the only available behavior on Windows and Linux. On macOS, it is equivalent to the _When scrolling_
+     * setting in macOS' _System Settings > Appearance > Show scroll bars_ settings. It is also used when the
+     * _Automatically based on mouse or trackpad_ option is selected, and there is no mouse connected to the computer.
+     *
+     * @param trackThickness The thickness of the scrollbar track.
+     * @param trackThicknessExpanded The thickness when expanded.
+     * @param trackPadding The padding around the scrollbar track.
+     * @param trackPaddingWithBorder The padding when a border is visible.
+     * @param trackColorAnimationDuration The duration for the track color animation.
+     * @param expandAnimationDuration The duration for the track expansion animation.
+     * @param thumbColorAnimationDuration The duration for the thumb color animation.
+     * @param lingerDuration The duration to wait before hiding the scrollbar.
+     */
     @GenerateDataFunctions
     public class WhenScrolling(
         public override val trackThickness: Dp,
@@ -289,11 +377,30 @@ public sealed interface ScrollbarVisibility {
     }
 }
 
+/** Defines the behavior when the scrollbar track is clicked. */
 public enum class TrackClickBehavior {
+    /**
+     * Scrolls to the next page in the direction of the click.
+     *
+     * Equivalent to the _Jump to the next page_ setting in macOS' _System Settings > Appearance > Click in the scroll
+     * bar to_ settings.
+     *
+     * This is normally not used on Windows and Linux.
+     */
     NextPage,
+
+    /**
+     * Jumps directly to the clicked spot on the track.
+     *
+     * Equivalent to the _Jump to the spot that's clicked_ setting in macOS' _System Settings > Appearance > Click in
+     * the scroll bar to_ settings.
+     *
+     * This is the only available behavior on Windows and Linux.
+     */
     JumpToSpot,
 }
 
+/** The CompositionLocal that provides the default [ScrollbarStyle]. */
 public val LocalScrollbarStyle: ProvidableCompositionLocal<ScrollbarStyle> = staticCompositionLocalOf {
     error("No ScrollbarStyle provided. Have you forgotten the theme?")
 }
