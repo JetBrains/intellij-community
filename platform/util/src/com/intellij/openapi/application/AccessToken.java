b310a00ab97f7c5421e777894ebb47d6356cb82b
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2025-07-10 15:44:41 +0200

    [util] add `AccessToken.compound`
    GitOrigin-RevId: f69082be4d31fa7b9d94b487b36f6b302defa789

--- platform/util/src/com/intellij/openapi/application/AccessToken.java
+++ platform/util/src/com/intellij/openapi/application/AccessToken.java
@@ -1,7 +1,9 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.application;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public abstract class AccessToken implements AutoCloseable {
   @Override
@@ -13,6 +15,16 @@ public abstract class AccessToken implements AutoCloseable {
 
   public static final @NotNull AccessToken EMPTY_ACCESS_TOKEN = new AccessToken() {
     @Override
-    public void finish() {}
+    public void finish() { }
   };
+
+  public static @NotNull AccessToken compound(@NotNull List<@NotNull AccessToken> tokens) {
+    if (tokens.isEmpty()) {
+      return EMPTY_ACCESS_TOKEN;
+    }
+    if (tokens.size() == 1) {
+      return tokens.get(0);
+    }
+    return new CompoundAccessToken(tokens);
+  }
 }
