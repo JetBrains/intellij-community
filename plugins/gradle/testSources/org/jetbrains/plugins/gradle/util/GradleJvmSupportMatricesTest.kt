cd772b4550a6d0a9e70e5edfa71798d9ae5349b5
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2025-07-10 13:17:49 +0200

    [gradle][IDEA-372139] Do not suggest Gradle 9.0 for the end user
    (cherry picked from commit bb4cb74127d5fae128e69bf58afd00ed93456f4f)
    
    IJ-CR-170856
    
    GitOrigin-RevId: c12e3fcd4f90c8887397ebf544c828150efc21c4

--- plugins/gradle/testSources/org/jetbrains/plugins/gradle/util/GradleJvmSupportMatricesTest.kt
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/util/GradleJvmSupportMatricesTest.kt
@@ -11,7 +11,7 @@ class GradleJvmSupportMatricesTest : GradleJvmSupportMatricesTestCase() {
     private const val LATEST_SUPPORTED_JAVA_VERSION = 24
     private const val FUTURE_JAVA_VERSION = 100
     private const val BUNDLED_GRADLE_VERSION = "8.14"
-    private const val LATEST_GRADLE_VERSION = "9.0"
+    private const val LATEST_GRADLE_VERSION = "8.14"
   }
 
   fun `test bundled gradle version`() {
