52f2471a706f38fed6fce82b97686ecacdc32961
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-07-10 13:53:23 +0200

    [rd-topics] IJPL-193540: Introduce kdoc for RemoteTopicListener
    GitOrigin-RevId: e53134c044ee6c418287f30a6111ad589aa0cfc9

--- platform/remote-topics/shared/src/com/intellij/platform/rpc/topics/RemoteTopicListener.kt
+++ platform/remote-topics/shared/src/com/intellij/platform/rpc/topics/RemoteTopicListener.kt
@@ -3,9 +3,38 @@ package com.intellij.platform.rpc.topics
 
 import com.intellij.openapi.extensions.ExtensionPointName
 
+/**
+ * Handles events from [RemoteTopic] on the frontend side.
+ *
+ * Register listeners via extension points in plugin.xml:
+ * ```xml
+ * <extensions defaultExtensionNs="com.intellij">
+ *   <platform.rpc.remoteTopicListener implementation="com.example.MyListener"/>
+ * </extensions>
+ * ```
+ *
+ * Example implementation:
+ * ```kotlin
+ * class MyListener : RemoteTopicListener<MyEvent> {
+ *   override val topic = MY_TOPIC
+ *
+ *   override fun handleEvent(event: MyEvent) {
+ *     // Process the event
+ *   }
+ * }
+ * ```
+ *
+ * @see RemoteTopic
+ */
 interface RemoteTopicListener<E : Any> {
+  /**
+   *  The [RemoteTopic] this listener subscribes to.
+   */
   val topic: RemoteTopic<E>
 
+  /**
+   * Called when the [event] is received from the topic.
+   */
   fun handleEvent(event: E)
 
   companion object {
