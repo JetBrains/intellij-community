84b13cadf408bb8d490c230ff4bbae40d3732cc9
Tagir Valeev <tagir.valeev@jetbrains.com>
2025-07-10 15:31:15 +0200

    [java-analysis] AndroidAnnotationSupport: add android.annotation.*
    GitOrigin-RevId: b7ee276e743231a832918c4e18a993233baf7ad9

--- java/java-impl/src/com/intellij/codeInsight/annoPackages/AndroidAnnotationSupport.java
+++ java/java-impl/src/com/intellij/codeInsight/annoPackages/AndroidAnnotationSupport.java
@@ -13,10 +13,12 @@ final class AndroidAnnotationSupport implements AnnotationPackageSupport {
   public @NotNull List<String> getNullabilityAnnotations(@NotNull Nullability nullability) {
     return switch (nullability) {
       case NOT_NULL -> Arrays.asList("android.support.annotation.NonNull",
+                                     "android.annotation.NonNull",
                                      "androidx.annotation.NonNull",
                                      "androidx.annotation.RecentlyNonNull",
                                      "com.android.annotations.NonNull");
       case NULLABLE -> Arrays.asList("android.support.annotation.Nullable",
+                                     "android.annotation.Nullable",
                                      "androidx.annotation.Nullable",
                                      "androidx.annotation.RecentlyNullable",
                                      "com.android.annotations.Nullable");
