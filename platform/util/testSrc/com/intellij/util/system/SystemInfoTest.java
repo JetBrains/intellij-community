9ddec0c817753d44e3a4a1adafd5f431e0371559
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2025-07-10 22:23:45 +0200

    [tests] adding more system info test cases
    GitOrigin-RevId: 55787de466f632276810ce6cb5cf9237bfaf71ec

--- platform/util/testSrc/com/intellij/util/system/SystemInfoTest.java
+++ platform/util/testSrc/com/intellij/util/system/SystemInfoTest.java
@@ -1,15 +1,45 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.system;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
-public class SystemInfoTest {
-  @Test
-  public void jvmCpuArchitectureDetection() {
-    CpuArch arch = CpuArch.CURRENT;
+class SystemInfoTest {
+  @Test void jvmCpuArchitectureDetection() {
+    var arch = CpuArch.CURRENT;
     assertThat(arch).isNotIn(CpuArch.OTHER, CpuArch.UNKNOWN);
     assertThat(arch.width).isGreaterThan(0);
+  }
+
+  @Test void osDetection() {
+    assertThat(OS.CURRENT).isNotEqualTo(OS.Other);
+  }
+
+  @Test void windowsBuildNumberReading() {
+    assumeTrue(OS.CURRENT == OS.Windows);
+
+    var osInfo = (OS.WindowsInfo)OS.CURRENT.getOsInfo();
+    assertThat(osInfo.getBuildNumber()).isGreaterThan(10240);
+  }
+
+  @Test void unixReleaseDataReading() {
+    assumeTrue(OS.isGenericUnix());
+    assumeTrue(Files.exists(Path.of("/etc/os-release")));
+
+    var osInfo = (OS.UnixInfo)OS.CURRENT.getOsInfo();
+    assertThat(osInfo.getDistro()).isNotBlank();
+    assertThat(osInfo.getRelease()).isNotBlank();
+  }
+
+  @Test void linuxFeatures() {
+    assumeTrue(OS.CURRENT == OS.Linux);
+
+    var osInfo = (OS.LinuxInfo)OS.CURRENT.getOsInfo();
+    assertThat(osInfo.getGlibcVersion()).isNotBlank();
   }
 }
