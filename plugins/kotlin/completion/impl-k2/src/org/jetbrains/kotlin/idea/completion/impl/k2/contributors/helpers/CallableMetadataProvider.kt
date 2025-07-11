0495c0fc71cdbc1185c77f92131ee1977a7ab967
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2025-07-11 13:19:27 +0200

    [kotlin] KTIJ-33966 multiple inner classes case supported
    GitOrigin-RevId: 604e8d6330da7de1639e34ffcdbd82adfd4d0190

--- plugins/kotlin/completion/impl-k2/src/org/jetbrains/kotlin/idea/completion/impl/k2/contributors/helpers/CallableMetadataProvider.kt
+++ plugins/kotlin/completion/impl-k2/src/org/jetbrains/kotlin/idea/completion/impl/k2/contributors/helpers/CallableMetadataProvider.kt
@@ -261,13 +261,11 @@ internal object CallableMetadataProvider {
     context(KaSession)
     @OptIn(KaExperimentalApi::class)
     private fun buildClassType(symbol: KaClassLikeSymbol): KaType = buildClassType(symbol) {
-        val containingSymbol = if (symbol is KaNamedClassSymbol && symbol.isInner)
-            symbol.containingDeclaration as? KaClassSymbol
-        else
-            null
+        val times = when (val defaultType = symbol.defaultType) {
+            is KaClassType -> defaultType.qualifiers.sumOf { it.typeArguments.size }
+            else -> 0
+        }
 
-        val times = (containingSymbol?.typeParameters?.size ?: 0) +
-                symbol.typeParameters.size
         @OptIn(KaExperimentalApi::class)
         repeat(times) {
             argument(buildStarTypeProjection())
