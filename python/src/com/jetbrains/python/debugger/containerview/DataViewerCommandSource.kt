bd821107d59da2aa9ee9ad1e019aa31c4bb677eb
Natalia.Murycheva <natalia.murycheva@jetbrains.com>
2025-07-11 16:53:35 +0200

    [PyCharm Tables] PY-76066 Add the Reload button to the Data View tool window #PY-76066 Ready for Merge
    * Added a new action to reload data in case it's opened in the Data View but already changed
    * Updated FUS logging: added a new command source type
    * Minor rename refactoring
    
    (cherry picked from commit edff1a40bc9c12a57677345462274f8c8da84c3d)
    
    GitOrigin-RevId: 27fceef4e55e7c46c9a48ccd62d1741194d5e004

--- python/src/com/jetbrains/python/debugger/containerview/DataViewerCommandSource.kt
+++ python/src/com/jetbrains/python/debugger/containerview/DataViewerCommandSource.kt
@@ -1,7 +1,8 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.debugger.containerview
 
-enum class TextFieldCommandSource {
+enum class DataViewerCommandSource {
   SLICING,
-  FORMATTING
+  FORMATTING,
+  RELOAD
 }
