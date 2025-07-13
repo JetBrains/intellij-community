d3efa004f11c5b55d6ecebeaad8a0757ba585a3c
Nikita Biriukov <nikita.biriukov@jetbrains.com>
2025-07-13 16:33:10 +0200

    [gradle] release tests back from quarantine
    These tests have a 100% success rate for the last ~100 runs in Gradle Tests Quarantine:
    
    - GradleTestAssertionTest: IDEA-350149, ASI-17
    - GradleRerunFailedTestsTest: IDEA-351008, ASI-17
    - GradleOpenProjectTest: IDEA-351014
    
    GitOrigin-RevId: e0fcce9f14310c420a4dc4993a150cb0ae1feedb

--- plugins/gradle/java/testSources/dsl/GradleJavaExtensionsTest.kt
+++ plugins/gradle/java/testSources/dsl/GradleJavaExtensionsTest.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.gradle.quarantine.dsl
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.gradle.dsl
 
 import com.intellij.psi.PsiMethod
 import org.gradle.util.GradleVersion
