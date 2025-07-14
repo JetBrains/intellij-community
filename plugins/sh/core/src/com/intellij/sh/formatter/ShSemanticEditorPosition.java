92e9187869fd095825431585e13f671a0a423bdb
Milica Sladakovic <milica.sladakovic@jetbrains.com>
2025-07-14 17:01:15 +0200

    IJPL-184315 Move `ShLineIndentProvider` to core
    Solved multiple indentation problems, so now backend and frontend are synced on indentation.
    
    GitOrigin-RevId: 1a697fb9ccfd4b27f305cb61a71b3580b5c0cea3

--- plugins/sh/core/src/com/intellij/sh/formatter/ShSemanticEditorPosition.java
+++ plugins/sh/core/src/com/intellij/sh/formatter/ShSemanticEditorPosition.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.sh.backend.formatter;
+package com.intellij.sh.formatter;
 
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.highlighter.HighlighterIterator;
