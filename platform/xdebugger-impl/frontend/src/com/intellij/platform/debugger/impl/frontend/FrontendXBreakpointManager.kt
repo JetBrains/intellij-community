ac13c5ce13f6d7a55328ca1bc297538562f08774
Maksim Zuev <maksim.zuev@jetbrains.com>
2025-07-14 18:24:39 +0200

    [rd debugger] IJPL-196916 Log exceptions during breakpoint install on frontend
    GitOrigin-RevId: ec737013b1991241e748e94ca3300062819787a3

--- platform/xdebugger-impl/frontend/src/com/intellij/platform/debugger/impl/frontend/FrontendXBreakpointManager.kt
+++ platform/xdebugger-impl/frontend/src/com/intellij/platform/debugger/impl/frontend/FrontendXBreakpointManager.kt
@@ -73,22 +73,38 @@ class FrontendXBreakpointManager(private val project: Project, private val cs: C
       FrontendXBreakpointTypesManager.getInstance(project).typesInitialized().await()
       val (initialBreakpoints, breakpointEvents) = XDebuggerManagerApi.getInstance().getBreakpoints(project.projectId())
       for (breakpointDto in initialBreakpoints) {
-        addBreakpoint(breakpointDto, updateUI = false)
+        try {
+          addBreakpoint(breakpointDto, updateUI = false)
+        }
+        catch (e: CancellationException) {
+          throw e
+        }
+        catch (e: Throwable) {
+          log.error("Error during initial breakpoints creation from backend $breakpointDto", e)
+        }
       }
       lineBreakpointManager.queueAllBreakpointsUpdate()
 
       breakpointEvents.toFlow().collect { event ->
-        when (event) {
-          is XBreakpointEvent.BreakpointAdded -> {
-            log.info("Breakpoint add request from backend: ${event.breakpointDto.id}")
-            addBreakpoint(event.breakpointDto, updateUI = true)
+        try {
+          when (event) {
+            is XBreakpointEvent.BreakpointAdded -> {
+              log.info("Breakpoint add request from backend: ${event.breakpointDto.id}")
+              addBreakpoint(event.breakpointDto, updateUI = true)
+            }
+            is XBreakpointEvent.BreakpointRemoved -> {
+              log.info("Breakpoint removal request from backend: ${event.breakpointId}")
+              removeBreakpointLocally(event.breakpointId)
+              // breakpointRemoved event happened on the server, so we can remove id from the frontend
+              breakpointIdsRemovedLocally.remove(event.breakpointId)
+            }
           }
-          is XBreakpointEvent.BreakpointRemoved -> {
-            log.info("Breakpoint removal request from backend: ${event.breakpointId}")
-            removeBreakpointLocally(event.breakpointId)
-            // breakpointRemoved event happened on the server, so we can remove id from the frontend
-            breakpointIdsRemovedLocally.remove(event.breakpointId)
-          }
+        }
+        catch (e: CancellationException) {
+          throw e
+        }
+        catch (e: Throwable) {
+          log.error("Error during breakpoint event processing from backend: $event", e)
         }
       }
     }
