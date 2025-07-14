a652636d4b47f0a42f59fbfb27b4649c55b87890
Victoria.Petrakovich <victoria.petrakovich@jetbrains.com>
2025-07-14 12:41:09 +0200

    [kotlin] Calculate type references in `prepareContext()`
    KTIJ-32017
    
    GitOrigin-RevId: 5d07429b635fcee613bd793a0f9eef1822598236

--- plugins/kotlin/code-insight/intentions-shared/src/org/jetbrains/kotlin/idea/codeInsight/intentions/shared/SpecifyTypeExplicitlyInDestructuringAssignmentIntention.kt
+++ plugins/kotlin/code-insight/intentions-shared/src/org/jetbrains/kotlin/idea/codeInsight/intentions/shared/SpecifyTypeExplicitlyInDestructuringAssignmentIntention.kt
@@ -6,19 +6,13 @@ import com.intellij.codeInspection.util.IntentionFamilyName
 import com.intellij.modcommand.ActionContext
 import com.intellij.modcommand.ModPsiUpdater
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.SmartPsiElementPointer
-import com.intellij.psi.createSmartPointer
 import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
 import org.jetbrains.kotlin.analysis.api.KaSession
-import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.renderer.types.impl.KaTypeRendererForSource
 import org.jetbrains.kotlin.analysis.api.types.KaErrorType
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.KotlinApplicableModCommandAction
-import org.jetbrains.kotlin.psi.KtCodeFragment
-import org.jetbrains.kotlin.psi.KtDestructuringDeclaration
-import org.jetbrains.kotlin.psi.KtDestructuringDeclarationEntry
-import org.jetbrains.kotlin.psi.KtPsiFactory
+import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.endOffset
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
 import org.jetbrains.kotlin.types.Variance
@@ -28,7 +22,7 @@ internal class SpecifyTypeExplicitlyInDestructuringAssignmentIntention :
         KtDestructuringDeclaration::class
     ) {
 
-    internal class Context(val entries: List<SmartPsiElementPointer<KtDestructuringDeclarationEntry>>)
+    internal class Context(val entriesAndTypeReferences: Map<KtDestructuringDeclarationEntry, KtTypeReference>)
 
     override fun getFamilyName(): @IntentionFamilyName String =
         KotlinBundle.message("specify.all.types.explicitly.in.destructuring.declaration")
@@ -47,11 +41,17 @@ internal class SpecifyTypeExplicitlyInDestructuringAssignmentIntention :
     @OptIn(KaExperimentalApi::class)
     override fun KaSession.prepareContext(element: KtDestructuringDeclaration): Context? {
         val entries = element.entriesWithoutExplicitTypes()
-        if (entries.any { entry ->
-                val symbol = entry.symbol
-                symbol.returnType is KaErrorType
-            }) return null
-        return Context(entries.map { it.createSmartPointer() }.toList())
+        val factory = KtPsiFactory(element.project)
+        val entriesAndTypeReferences = mutableMapOf<KtDestructuringDeclarationEntry, KtTypeReference>()
+
+        for (entry in entries) {
+            val returnType = entry.symbol.returnType
+            if (returnType is KaErrorType) return null
+            val type = returnType.render(renderer = KaTypeRendererForSource.WITH_SHORT_NAMES, position = Variance.OUT_VARIANCE)
+            val typeReference = factory.createType(type)
+            entriesAndTypeReferences[entry] = typeReference
+        }
+        return Context(entriesAndTypeReferences)
     }
 
     override fun invoke(
@@ -60,31 +60,16 @@ internal class SpecifyTypeExplicitlyInDestructuringAssignmentIntention :
         elementContext: Context,
         updater: ModPsiUpdater,
     ) {
-        val entries = elementContext.entries
-        for (entry in entries) {
-            val element = entry.element ?: continue
-            setTypeForDeclaration(element)
-            if (entries.last() == entry) {
-                updater.moveCaretTo(element.endOffset)
-            }
-        }
-    }
+        val entriesAndTypeReferences = elementContext.entriesAndTypeReferences
+        var lastEntry: KtDestructuringDeclarationEntry? = null
 
-    private fun setTypeForDeclaration(entry: KtDestructuringDeclarationEntry) {
-        val factory = KtPsiFactory(entry.project)
-        val typeReference = factory.createType(entry.getKotlinType())
-        entry.typeReference = typeReference
+        for (entryAndTypeReference in entriesAndTypeReferences) {
+            entryAndTypeReference.key.typeReference = entryAndTypeReference.value
+            lastEntry = entryAndTypeReference.key
+        }
+        lastEntry?.let { updater.moveCaretTo(lastEntry.endOffset) }
     }
 }
 
 private fun KtDestructuringDeclaration.entriesWithoutExplicitTypes(): List<KtDestructuringDeclarationEntry> =
     entries.filter { it.typeReference == null }
-
-@OptIn(KaExperimentalApi::class)
-private fun KtDestructuringDeclarationEntry.getKotlinType(): String {
-    val entry = this
-    return analyze(entry) {
-        val symbol = entry.symbol
-        symbol.returnType.render(renderer = KaTypeRendererForSource.WITH_SHORT_NAMES, position = Variance.OUT_VARIANCE)
-    }
-}
