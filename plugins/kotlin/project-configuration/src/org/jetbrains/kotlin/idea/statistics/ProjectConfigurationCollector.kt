04cc54ba48286249c3de0600c32ea85f6f410acc
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2025-07-14 19:26:37 +0200

    [kotlin] Update project configuration collector
    GitOrigin-RevId: 32bd4e8d6beaf1e93aabcf9cc0f332d954404a11

--- plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/statistics/ProjectConfigurationCollector.kt
+++ plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/statistics/ProjectConfigurationCollector.kt
@@ -82,7 +82,7 @@ internal class ProjectConfigurationCollector : ProjectUsagesCollector() {
         }
     }
 
-    private val GROUP = EventLogGroup("kotlin.project.configuration", 37)
+    private val GROUP = EventLogGroup("kotlin.project.configuration", 38)
 
     private val systemField = EventFields.String("system", listOf("JPS", "Maven", "Gradle", "unknown"))
     private val platformField = EventFields.String("platform", composePlatformFields())
