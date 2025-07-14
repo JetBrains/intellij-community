eec4f89f4bd44c400b1b15f8432710a8443258d1
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2025-07-14 10:18:22 +0200

    [kotlin] Fix test data to reflect changes in kotlin
    #KT-58988
    
    GitOrigin-RevId: 47ca5bcb1ad6de83f0f343fb1874e0691af5300f

--- plugins/kotlin/idea/tests/testData/inspections/dfa/AnnotationInJava.kt
+++ plugins/kotlin/idea/tests/testData/inspections/dfa/AnnotationInJava.kt
@@ -1,5 +1,5 @@
 // WITH_STDLIB
-internal fun bug(storage: Storage) {
+internal fun bug(<warning descr="[EXPOSED_PACKAGE_PRIVATE_TYPE_FROM_INTERNAL_WARNING] 'internal' declaration exposes 'public/*package*/' type 'Storage'. This will become an error in language version 2.4. See https://youtrack.jetbrains.com/issue/KTLC-271.">storage: Storage</warning>) {
     if (storage is FileStorageAnnotation) {
         println(true)
     }
