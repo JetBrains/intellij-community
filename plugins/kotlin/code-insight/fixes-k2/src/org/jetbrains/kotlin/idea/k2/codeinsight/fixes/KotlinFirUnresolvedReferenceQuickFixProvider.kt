173de373588ad79fafc5aea81a43d56a6798324f
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2025-07-15 18:08:36 +0200

    [kotlin] KTIJ-31125 Move multiplatform module check directly into AddDependencyQuickFixHelper
    Otherwise, it produces quick fixes on import that might work incorrectly in the multiplatform modules (while it's not supported).
    
    GitOrigin-RevId: b4e6d95eafbd85df12e00933c32e5de03d94b0ae

--- plugins/kotlin/code-insight/fixes-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/fixes/KotlinFirUnresolvedReferenceQuickFixProvider.kt
+++ plugins/kotlin/code-insight/fixes-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/fixes/KotlinFirUnresolvedReferenceQuickFixProvider.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.k2.codeinsight.fixes
 
 import com.intellij.codeInsight.daemon.QuickFixActionRegistrar
@@ -8,8 +8,6 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.debug
 import com.intellij.psi.PsiReference
 import org.jetbrains.kotlin.analysis.api.analyze
-import org.jetbrains.kotlin.idea.base.facet.isMultiPlatformModule
-import org.jetbrains.kotlin.idea.base.util.module
 import org.jetbrains.kotlin.idea.codeinsights.impl.base.quickFix.AddDependencyQuickFixHelper
 import org.jetbrains.kotlin.idea.highlighter.binaryExpressionForOperationReference
 import org.jetbrains.kotlin.idea.highlighter.restoreKaDiagnosticsForUnresolvedReference
@@ -36,10 +34,8 @@ class KotlinFirUnresolvedReferenceQuickFixProvider : UnresolvedReferenceQuickFix
     override fun registerFixes(reference: PsiReference, registrar: QuickFixActionRegistrar) {
         val ktElement = reference.element as? KtElement ?: return
 
-        if (ktElement.module?.isMultiPlatformModule != true) {
-            for (action in AddDependencyQuickFixHelper.createQuickFix(ktElement)) {
-                registrar.register(action)
-            }
+        for (action in AddDependencyQuickFixHelper.createQuickFix(ktElement)) {
+            registrar.register(action)
         }
 
         analyze(ktElement) {
