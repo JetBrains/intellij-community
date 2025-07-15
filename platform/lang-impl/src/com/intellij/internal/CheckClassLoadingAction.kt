f9b2337e3d82b5b729424be0b586248fe63539fe
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-07-15 13:28:01 +0200

    [diagnostic] IJPL-185789 add Check Class Loading internal action
    GitOrigin-RevId: 7217ce353c0c72937139f2a232ce6f3c2938bd98

---
+++ platform/lang-impl/src/com/intellij/internal/CheckClassLoadingAction.kt
@@ -0,0 +1,109 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.internal
+
+import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.ide.plugins.PluginModuleDescriptor
+import com.intellij.ide.plugins.PluginSetBuilder
+import com.intellij.ide.plugins.contentModuleName
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.ui.DialogBuilder
+import com.intellij.openapi.ui.Messages
+import com.intellij.ui.TableSpeedSearch
+import com.intellij.ui.components.JBScrollPane
+import com.intellij.ui.dsl.builder.Align
+import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.table.JBTable
+import java.awt.Dimension
+import javax.swing.table.DefaultTableModel
+
+internal class CheckClassLoadingAction : DumbAwareAction("Check Class Loading"), ActionRemoteBehaviorSpecification.Duplicated {
+  override fun actionPerformed(e: AnActionEvent) {
+    val className = Messages.showInputDialog(e.project, "Enter class name:", "Check Class Loading", Messages.getQuestionIcon())
+    if (className.isNullOrBlank()) return
+    
+    val classLoadingMap = buildClassLoadingMap(className)
+
+    fun PluginModuleDescriptor.id() = buildString {
+      append(pluginId)
+      if (contentModuleName != null) append(":$contentModuleName")
+    }
+
+    val columns = arrayOf("Module", "Class")
+    val data = classLoadingMap.map { (module, clazz) -> // keep topological order
+      arrayOf(module.id(), if (clazz != null) "Loaded @${System.identityHashCode(clazz).toString(16)}" else "null")
+    }.toTypedArray()
+
+    val model = DefaultTableModel(columns, 0)
+    for (row in data) {
+      model.addRow(row)
+    }
+    val table = object : JBTable(model) {
+      override fun isCellEditable(row: Int, column: Int): Boolean = false
+    }
+    TableSpeedSearch.installOn(table)
+    table.columnModel.getColumn(0).preferredWidth = 400
+    table.columnModel.getColumn(1).preferredWidth = 200
+
+    val distinct = classLoadingMap.entries.groupBy { it.value }
+      .mapValues { it.value.map { it.key } }
+      .filter { it.key != null }
+    val distinctCount = distinct.size
+
+    val panel = panel {
+      row("Class: $className") {}
+      row {
+        label(
+          "$distinctCount instance${if (distinctCount == 1) "" else "s"} found. " +
+          if (distinct.isNotEmpty()) "Representative module for each distinct instance:" else ""
+        ).applyToComponent {
+          icon = if (distinctCount != 1) Messages.getWarningIcon() else null
+        }
+      }
+      row { text(distinct.values.joinToString("<br>") { it[0].id() }) }
+      row { label("Modules are in topological order.") }
+      row { cell(table).align(Align.FILL) }
+    }
+    table.tableHeader.isVisible = true
+
+    DialogBuilder(e.project).apply {
+      setTitle("Class Loading Report")
+      val scrollPane = JBScrollPane(panel)
+      scrollPane.preferredSize = Dimension(650, 600)
+      setCenterPanel(scrollPane)
+      addOkAction()
+      show()
+    }
+  }
+
+  private fun PluginModuleDescriptor.tryLoadClass(className: String): Class<*>? {
+    if (pluginClassLoader == null) return null
+    return try {
+      Class.forName(className, false, pluginClassLoader)
+    }
+    catch (e: ClassNotFoundException) {
+      null
+    }
+  }
+
+  private fun buildClassLoadingMap(className: String): Map<PluginModuleDescriptor, Class<*>?> {
+    val pluginSet = PluginManagerCore.getPluginSet()
+    val loadingResults = mutableMapOf<PluginModuleDescriptor, Class<*>?>()
+    val topologicalComparator = PluginSetBuilder(pluginSet.enabledPlugins.toSet()).topologicalComparator
+    for (plugin in pluginSet.enabledPlugins) {
+      loadingResults[plugin] = plugin.tryLoadClass(className)
+      for (module in plugin.contentModules) {
+        loadingResults[module] = module.tryLoadClass(className)
+      }
+    }
+    return loadingResults.entries.sortedWith { (k1, v1), (k2, v2) ->
+      when {
+        // nulls to the end
+        v1 != null && v2 == null -> -1
+        v1 == null && v2 != null -> 1
+        else -> topologicalComparator.compare(k1, k2)
+      }
+    }.associateTo(LinkedHashMap()) { it.key to it.value }
+  }
+}
