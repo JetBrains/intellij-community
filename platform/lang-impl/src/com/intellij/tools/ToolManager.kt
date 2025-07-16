a1adaa20808e115968e8e40cae2d56b1714f2688
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-07-16 10:49:09 +0200

    convert ToolManager to kotlin to use coroutine api
    GitOrigin-RevId: 2ad8d3f0097f754fe0810a0fd46f577acf13f2e8

--- platform/lang-impl/src/com/intellij/tools/ToolManager.kt
+++ platform/lang-impl/src/com/intellij/tools/ToolManager.kt
@@ -1,60 +1,37 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.tools;
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.tools
 
-import com.intellij.openapi.actionSystem.ex.ActionRuntimeRegistrar;
-import com.intellij.openapi.actionSystem.impl.ActionConfigurationCustomizer;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.Service;
-import com.intellij.openapi.options.SchemeManagerFactory;
-import com.intellij.openapi.options.SchemeProcessor;
-import com.intellij.util.JavaCoroutines;
-import kotlin.Unit;
-import kotlin.coroutines.Continuation;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.actionSystem.ex.ActionRuntimeRegistrar
+import com.intellij.openapi.actionSystem.impl.ActionConfigurationCustomizer
+import com.intellij.openapi.actionSystem.impl.ActionConfigurationCustomizer.LightCustomizeStrategy
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.options.SchemeManagerFactory
+import com.intellij.openapi.options.SchemeProcessor
+import com.intellij.tools.ToolManager.Companion.getInstance
 
 @Service
-public final class ToolManager extends BaseToolManager<Tool> {
-  public ToolManager() {
-    super(SchemeManagerFactory.getInstance(), "tools", ToolsBundle.message("tools.settings"));
+class ToolManager : BaseToolManager<Tool>(SchemeManagerFactory.getInstance(), "tools", ToolsBundle.message("tools.settings")) {
+  companion object {
+    @JvmStatic
+    fun getInstance(): ToolManager = service<ToolManager>()
   }
 
-  static final class MyActionTuner implements ActionConfigurationCustomizer, ActionConfigurationCustomizer.LightCustomizeStrategy {
-    @Override
-    public @Nullable Object customize(@NotNull ActionRuntimeRegistrar actionRegistrar, @NotNull Continuation<? super Unit> $completion) {
-      return JavaCoroutines.suspendJava(jc -> {
-        getInstance().registerActions(actionRegistrar);
-        jc.resume(Unit.INSTANCE);
-      }, $completion);
+  override fun createProcessor(): SchemeProcessor<ToolsGroup<Tool?>, ToolsGroup<Tool?>> {
+    return object : ToolsProcessor<Tool?>() {
+      override fun createToolsGroup(groupName: String?) = ToolsGroup<Tool?>(groupName)
+
+      override fun createTool(): Tool = Tool()
     }
   }
 
-  public static ToolManager getInstance() {
-    return ApplicationManager.getApplication().getService(ToolManager.class);
-  }
+  override fun getActionIdPrefix(): String = Tool.ACTION_ID_PREFIX
 
-  @Override
-  protected SchemeProcessor<ToolsGroup<Tool>, ToolsGroup<Tool>> createProcessor() {
-    return new ToolsProcessor<>() {
-      @Override
-      protected ToolsGroup<Tool> createToolsGroup(String groupName) {
-        return new ToolsGroup<>(groupName);
-      }
+  public override fun getGroupIdPrefix(): String = "Tools_"
+}
 
-      @Override
-      protected Tool createTool() {
-        return new Tool();
-      }
-    };
-  }
-
-  @Override
-  protected @NotNull String getActionIdPrefix() {
-    return Tool.ACTION_ID_PREFIX;
-  }
-
-  @Override
-  protected @NotNull String getGroupIdPrefix() {
-    return "Tools_";
+private class ToolManagerActionTuner : ActionConfigurationCustomizer, LightCustomizeStrategy {
+  override suspend fun customize(actionRegistrar: ActionRuntimeRegistrar) {
+    getInstance().registerActions(actionRegistrar)
   }
 }
