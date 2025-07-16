5b612a708594d5cc755a560313f1de574f797a6a
Andrei.Kuznetsov <andrei.kuznetsov@jetbrains.com>
2025-07-16 11:27:11 +0200

    IJPL-196089 Cleanup: delete unused ModuleRootsIteratorBuilder in indexableIteratorBuilders
    GitOrigin-RevId: cd28ae6fc0864902a6bd9849763136ed0ea771ed

--- platform/lang-impl/src/com/intellij/util/indexing/roots/builders/indexableIteratorBuilders.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/roots/builders/indexableIteratorBuilders.kt
@@ -12,7 +12,6 @@ import com.intellij.platform.workspace.jps.entities.SdkId
 import com.intellij.platform.workspace.storage.EntityPointer
 import com.intellij.platform.workspace.storage.EntityStorage
 import com.intellij.platform.workspace.storage.WorkspaceEntity
-import com.intellij.platform.workspace.storage.url.VirtualFileUrl
 import com.intellij.util.indexing.IndexableSetContributor
 import com.intellij.util.indexing.roots.IndexableEntityProvider.IndexableIteratorBuilder
 import com.intellij.util.indexing.roots.IndexableFilesIterator
@@ -102,8 +101,6 @@ internal data class SdkIteratorBuilder(val sdkName: String,
 internal object InheritedSdkIteratorBuilder : IndexableIteratorBuilder
 
 internal data class FullModuleContentIteratorBuilder(val moduleId: ModuleId) : IndexableIteratorBuilder
-
-internal class ModuleRootsIteratorBuilder(val moduleId: ModuleId, val urls: Collection<VirtualFileUrl>) : IndexableIteratorBuilder
 
 internal class ModuleRootsFileBasedIteratorBuilder(val moduleId: ModuleId, val files: IndexingUrlRootHolder) : IndexableIteratorBuilder
 
