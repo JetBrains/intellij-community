fc4dbb9beffc32f033f660600637ef1671389a0b
Aleksey Pivovarov <aleksey.pivovarov@jetbrains.com>
2025-07-16 19:07:52 +0200

    IJPL-179136 avoid spamming coroutine thread with CodeStyleCachedValueProviderService jobs
    GitOrigin-RevId: 4efa90e8b5ccf5a73598a0967cb528b3928cda45

--- platform/code-style-impl/src/com/intellij/application/options/codeStyle/cache/CodeStyleCachedValueProvider.kt
+++ platform/code-style-impl/src/com/intellij/application/options/codeStyle/cache/CodeStyleCachedValueProvider.kt
@@ -130,6 +130,7 @@ internal class CodeStyleCachedValueProvider(val fileSupplier: Supplier<VirtualFi
       val app = ApplicationManager.getApplication()
       if (app.isDispatchThread && !app.isUnitTestMode && !app.isHeadlessEnvironment) {
         LOG.debug { "async for ${file.name}" }
+        job?.cancel()
         job = project.service<CodeStyleCachedValueProviderService>().coroutineScope.launch {
           readAction {
             computeSettings()
