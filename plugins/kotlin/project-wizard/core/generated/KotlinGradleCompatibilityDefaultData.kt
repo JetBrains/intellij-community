2b46c7e45ec68dfa264b9b701560199f7a14794a
Victoria.Petrakovich <victoria.petrakovich@jetbrains.com>
2025-07-16 14:02:59 +0200

    [kotlin] Update Kotlin New Project wizard version to Kotlin 2.2.10
    KTIJ-34891
    
    
    (cherry picked from commit 01364894a5e319805514bbb60188fb06c362da5e)
    
    IJ-CR-173244
    
    GitOrigin-RevId: 96c0e17a8243e818b4a28f6acd89ccd6ab23c97b

--- plugins/kotlin/project-wizard/core/generated/KotlinGradleCompatibilityDefaultData.kt
+++ plugins/kotlin/project-wizard/core/generated/KotlinGradleCompatibilityDefaultData.kt
@@ -10,6 +10,7 @@ import org.jetbrains.kotlin.tools.projectWizard.compatibility.KotlinGradleCompat
  */
 internal val DEFAULT_KOTLIN_GRADLE_COMPATIBILITY_DATA = KotlinGradleCompatibilityState(
     kotlinVersions = listOf(
+        "2.2.10",
         "2.2.0",
         "2.1.21",
         "2.1.20",
