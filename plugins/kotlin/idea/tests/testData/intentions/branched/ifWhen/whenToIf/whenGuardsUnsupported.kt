cbf299b367ee2af7119351654fa9e3c1e1ead3c0
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2025-07-16 11:17:38 +0200

    [kotlin] KTIJ-34661 - Fixed whenGuardsUnsupported test for Kotlin 2.2.0
    When guards are now available, but are disabled in K1.
    
    GitOrigin-RevId: 4d96fad132549eb4f0cbb2ac32f0bc59cc1a1674

--- plugins/kotlin/idea/tests/testData/intentions/branched/ifWhen/whenToIf/whenGuardsUnsupported.kt
+++ plugins/kotlin/idea/tests/testData/intentions/branched/ifWhen/whenToIf/whenGuardsUnsupported.kt
@@ -1,6 +1,6 @@
 // COMPILER_ARGUMENTS: -XXLanguage:-WhenGuards
 // IS_APPLICABLE: false
-// ERROR: The feature "when guards" is only available since language version 2.2
+// ERROR: The feature "when guards" is disabled
 // K2_ERROR: The feature "when guards" is only available since language version 2.2
 
 private fun test(s: Any) {
