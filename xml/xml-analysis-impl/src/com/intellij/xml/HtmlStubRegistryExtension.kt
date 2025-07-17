aa73212b06d7ba6d3420e915cb054c94af963efe
Alexandr Evstigneev <alexandr.evstigneev@jetbrains.com>
2025-07-17 08:09:48 +0400

    RUBY-33279 introduced DefaultFileStubSerializer
    Seen this code few times already
    
    GitOrigin-RevId: e678777fd0ca1e7f4550cb283f522572efc25c1d

--- xml/xml-analysis-impl/src/com/intellij/xml/HtmlStubRegistryExtension.kt
+++ xml/xml-analysis-impl/src/com/intellij/xml/HtmlStubRegistryExtension.kt
@@ -1,36 +1,13 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.xml
 
-import com.intellij.psi.stubs.IndexSink
-import com.intellij.psi.stubs.PsiFileStub
-import com.intellij.psi.stubs.PsiFileStubImpl
-import com.intellij.psi.stubs.StubElement
-import com.intellij.psi.stubs.StubInputStream
-import com.intellij.psi.stubs.StubOutputStream
+import com.intellij.psi.stubs.DefaultFileStubSerializer
 import com.intellij.psi.stubs.StubRegistry
 import com.intellij.psi.stubs.StubRegistryExtension
-import com.intellij.psi.stubs.StubSerializer
-import com.intellij.psi.stubs.StubSerializerId
 import com.intellij.psi.xml.XmlElementType
 
 class HtmlStubRegistryExtension : StubRegistryExtension {
   override fun register(registry: StubRegistry) {
-    registry.registerStubSerializer(XmlElementType.HTML_FILE, HtmlFileStubSerializer())
-  }
-
-  private class HtmlFileStubSerializer : StubSerializer<PsiFileStub<*>> {
-    override fun getExternalId(): String {
-      return StubSerializerId.DEFAULT_EXTERNAL_ID
-    }
-
-    override fun serialize(stub: PsiFileStub<*>, dataStream: StubOutputStream) {
-    }
-
-    override fun deserialize(dataStream: StubInputStream, parentStub: StubElement<*>?): PsiFileStub<*> {
-      return PsiFileStubImpl(null)
-    }
-
-    override fun indexStub(stub: PsiFileStub<*>, sink: IndexSink) {
-    }
+    registry.registerStubSerializer(XmlElementType.HTML_FILE, DefaultFileStubSerializer())
   }
 }
