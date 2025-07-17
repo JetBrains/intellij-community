4e79983b4ccdf504576c7a96edfaf952b69be328
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2025-07-17 09:30:15 +0200

    [git] IJPL-197368 Add missing CWM check
    GitOrigin-RevId: 03b11f0b739a0d03949bacb7e89a19711e674dba

--- plugins/git4idea/frontend/src/com/intellij/vcs/git/frontend/GitFrontendStartupActivity.kt
+++ plugins/git4idea/frontend/src/com/intellij/vcs/git/frontend/GitFrontendStartupActivity.kt
@@ -1,12 +1,16 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.git.frontend
 
+import com.intellij.frontend.FrontendApplicationInfo
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.vcs.git.frontend.widget.GitWidgetStateHolder
+import com.intellij.vcs.git.isCodeWithMe
 
 internal class GitFrontendStartupActivity: ProjectActivity {
   override suspend fun execute(project: Project) {
-    GitWidgetStateHolder.getInstance(project)
+    if (!FrontendApplicationInfo.isCodeWithMe()) {
+      GitWidgetStateHolder.getInstance(project)
+    }
   }
 }
