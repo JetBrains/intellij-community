e4b9c31304345d82b09681405864d70216edb283
Maksim Zuev <maksim.zuev@jetbrains.com>
2025-07-17 11:45:56 +0200

    [rd debugger] Move ShowInstancesByClassAction to the shared module
    GitOrigin-RevId: a11ce0fa0b59d7747bb34e075ba1ff2a50dbd9e6

--- java/debugger/shared/src/com/intellij/debugger/memory/action/ShowInstancesByClassAction.kt
+++ java/debugger/shared/src/com/intellij/debugger/memory/action/ShowInstancesByClassAction.kt
@@ -1,38 +1,22 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.java.debugger.impl.frontend.actions
+package com.intellij.debugger.memory.action
 
-import com.intellij.java.debugger.impl.frontend.messages.JavaDebuggerImplFrontendBundle
+import com.intellij.java.debugger.impl.shared.JavaDebuggerSharedBundle
 import com.intellij.java.debugger.impl.shared.engine.JavaValueDescriptor
 import com.intellij.java.debugger.impl.shared.rpc.JavaDebuggerLuxActionsApi
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification
-import com.intellij.openapi.components.Service
-import com.intellij.openapi.components.service
 import com.intellij.openapi.util.text.StringUtil
-import com.intellij.xdebugger.impl.actions.areFrontendDebuggerActionsEnabled
 import com.intellij.xdebugger.impl.frame.XDebugManagerProxy
 import com.intellij.xdebugger.impl.ui.DebuggerUIUtil
 import com.intellij.xdebugger.impl.ui.tree.actions.XDebuggerTreeActionBase
 import com.intellij.xdebugger.impl.ui.tree.nodes.XValueNodeImpl
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-/**
- * Frontend version of [com.intellij.debugger.memory.action.ShowInstancesByClassAction]
- */
-private class FrontendShowInstancesByClassAction : XDebuggerTreeActionBase(), ActionRemoteBehaviorSpecification.Frontend {
+class ShowInstancesByClassAction : XDebuggerTreeActionBase(), ActionRemoteBehaviorSpecification.FrontendOtherwiseBackend {
   override fun getActionUpdateThread(): ActionUpdateThread {
     return ActionUpdateThread.BGT
-  }
-
-  override fun update(e: AnActionEvent) {
-    if (!areFrontendDebuggerActionsEnabled()) {
-      e.presentation.isEnabledAndVisible = false
-      return
-    }
-
-    super.update(e)
   }
 
   override fun isEnabled(node: XValueNodeImpl, e: AnActionEvent): Boolean {
@@ -42,22 +26,18 @@ private class FrontendShowInstancesByClassAction : XDebuggerTreeActionBase(), Ac
     val objectReferenceInfo = descriptor.objectReferenceInfo ?: return false
 
     val visibleTypeName = StringUtil.getShortName(objectReferenceInfo.typeName)
-    e.presentation.setText(JavaDebuggerImplFrontendBundle.message("action.show.objects.text", visibleTypeName))
+    e.presentation.setText(JavaDebuggerSharedBundle.message("action.show.objects.text", visibleTypeName))
 
     return objectReferenceInfo.canGetInstanceInfo
   }
 
-
   override fun perform(node: XValueNodeImpl, nodeName: String, e: AnActionEvent) {
     val xValue = node.valueContainer
     val session = DebuggerUIUtil.getSessionProxy(e) ?: return
-    service<FrontendShowInstancesByClassActionCoroutineScope>().cs.launch {
+    session.coroutineScope.launch {
       XDebugManagerProxy.getInstance().withId(xValue, session) { xValueId ->
         JavaDebuggerLuxActionsApi.getInstance().showInstancesDialog(xValueId)
       }
     }
   }
 }
-
-@Service(Service.Level.APP)
-private class FrontendShowInstancesByClassActionCoroutineScope(val cs: CoroutineScope)
