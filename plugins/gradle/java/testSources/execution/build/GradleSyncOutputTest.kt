48de846b89a7b64b15a77bb3fbc1d550e2f5e7df
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2025-07-18 13:02:25 +0200

    [Gradle|Test] cleanup: remove useless utilities from GradleSyncOutputTestCase
    ### Issues
      * IDEA-376112 Junit5: Simplify Gradle test fixture configuration
    
    GitOrigin-RevId: 321346a4f3b9f114e479d98abaa884a9bafa045d

--- plugins/gradle/java/testSources/execution/build/GradleSyncOutputTest.kt
+++ plugins/gradle/java/testSources/execution/build/GradleSyncOutputTest.kt
@@ -1,15 +1,46 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.execution.build
 
+import com.intellij.openapi.externalSystem.autoimport.AutoImportProjectTracker
+import com.intellij.platform.externalSystem.testFramework.ExternalSystemImportingTestCase
+import com.intellij.testFramework.junit5.fixture.disposableFixture
+import com.intellij.testFramework.junit5.fixture.projectFixture
+import com.intellij.testFramework.junit5.fixture.tempPathFixture
 import com.intellij.util.asDisposable
 import kotlinx.coroutines.runBlocking
 import org.gradle.util.GradleVersion
 import org.jetbrains.plugins.gradle.testFramework.annotations.AllGradleVersionsSource
+import org.jetbrains.plugins.gradle.testFramework.fixtures.application.GradleTestApplication
+import org.jetbrains.plugins.gradle.testFramework.fixtures.buildViewFixture
+import org.jetbrains.plugins.gradle.testFramework.fixtures.gradleFixture
 import org.jetbrains.plugins.gradle.testFramework.fixtures.gradleJvmFixture
+import org.jetbrains.plugins.gradle.testFramework.util.createBuildFile
+import org.jetbrains.plugins.gradle.testFramework.util.createSettingsFile
 import org.jetbrains.plugins.gradle.tooling.JavaVersionRestriction
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
 
-class GradleSyncOutputTest : GradleSyncOutputTestCase() {
+@GradleTestApplication
+class GradleSyncOutputTest {
+
+  private val testDisposable by disposableFixture()
+
+  private val projectRootFixture = tempPathFixture()
+  private val projectRoot by projectRootFixture
+
+  private val projectFixture = projectFixture(projectRootFixture, openAfterCreation = true)
+  private val project by projectFixture
+
+  private val gradleFixture by gradleFixture()
+
+  private val buildViewFixture by buildViewFixture(projectFixture)
+
+  @BeforeEach
+  fun setUpGradleReloadProjectBaseTestCase() {
+    AutoImportProjectTracker.enableAutoReloadInTests(testDisposable)
+    AutoImportProjectTracker.enableAsyncAutoReloadInTests(testDisposable)
+    ExternalSystemImportingTestCase.installExecutionOutputPrinter(testDisposable)
+  }
 
   @ParameterizedTest
   @AllGradleVersionsSource
@@ -17,15 +48,15 @@ class GradleSyncOutputTest : GradleSyncOutputTestCase() {
     gradleJvmFixture(gradleVersion, JavaVersionRestriction.NO, asDisposable())
       .installProjectSettingsConfigurator(asDisposable())
 
-    createSettingsFile(gradleVersion) {
+    projectRoot.createSettingsFile(gradleVersion) {
       setProjectName(project.name)
     }
-    reloadProject()
-    assertSyncViewTree {
+    gradleFixture.reloadProject(project, projectRoot)
+    buildViewFixture.assertSyncViewTree {
       assertNode("finished")
     }
 
-    createBuildFile(gradleVersion) {
+    projectRoot.createBuildFile(gradleVersion) {
       withJavaPlugin()
       withPostfix {
         call("tasks.register<Jar>", string("my-jar-task")) {
@@ -33,12 +64,12 @@ class GradleSyncOutputTest : GradleSyncOutputTestCase() {
         }
       }
     }
-    reloadProject()
-    assertSyncViewTree {
+    gradleFixture.reloadProject(project, projectRoot)
+    buildViewFixture.assertSyncViewTree {
       assertNode("finished")
     }
 
-    createBuildFile(gradleVersion) {
+    projectRoot.createBuildFile(gradleVersion) {
       withJavaPlugin()
       withPostfix {
         call("tasks.register", string("my-task")) {
@@ -49,8 +80,8 @@ class GradleSyncOutputTest : GradleSyncOutputTestCase() {
         }
       }
     }
-    reloadProject()
-    assertSyncViewTree {
+    gradleFixture.reloadProject(project, projectRoot)
+    buildViewFixture.assertSyncViewTree {
       assertNode("finished")
     }
   }
