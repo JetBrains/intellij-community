230662b47eb5f4dbab7874b124bd5e30557d3b5f
Alexandr Evstigneev <alexandr.evstigneev@jetbrains.com>
2025-07-18 10:01:12 +0400

    IJPL-184662 moved LanguageCommentFoldingBuilder to the comment part
    Moved UAST registration to the java part, because UAST is java-specific language
    
    GitOrigin-RevId: 22950c125b8eeebbca6a56151a08ae42df9180a8

--- plugins/IntelliLang/backend/src/org/intellij/plugins/intelliLang/references/LanguageReferenceContributor.kt
+++ plugins/IntelliLang/backend/src/org/intellij/plugins/intelliLang/references/LanguageReferenceContributor.kt
@@ -4,33 +4,26 @@ package org.intellij.plugins.intelliLang.references
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.lang.ASTNode
-import com.intellij.lang.LanguageCommenters
-import com.intellij.lang.folding.FoldingBuilderEx
-import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.model.Pointer
 import com.intellij.model.Symbol
 import com.intellij.model.psi.*
 import com.intellij.model.search.SearchRequest
-import com.intellij.openapi.editor.Document
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiRecursiveElementWalkingVisitor
 import com.intellij.util.ProcessingContext
-import com.intellij.util.SmartList
 import com.intellij.util.asSafely
-import com.intellij.util.text.findTextRange
 import org.intellij.plugins.intelliLang.inject.InjectLanguageAction
-import org.intellij.plugins.intelliLang.inject.InjectorUtils
+import org.intellij.plugins.intelliLang.references.LanguageInjectionUtil.commentBody
+import org.intellij.plugins.intelliLang.references.LanguageInjectionUtil.languageRange
 
 internal class LanguageReferenceContributor : PsiSymbolReferenceProvider {
   override fun getReferences(element: PsiExternalReferenceHost, hints: PsiSymbolReferenceHints): Collection<PsiSymbolReference> {
     val psiComment = element.asSafely<PsiComment>() ?: return emptyList()
-    val languageRange = getLanguageRange(psiComment) ?: return emptyList()
+    val languageRange = languageRange(psiComment) ?: return emptyList()
     return listOf(object : PsiCompletableReference {
 
       override fun getCompletionVariants(): Collection<LookupElement> =
@@ -51,15 +44,6 @@ internal class LanguageReferenceContributor : PsiSymbolReferenceProvider {
 
 }
 
-private fun getLanguageRange(psiComment: PsiComment): TextRange? {
-  val commentBody = psiComment.commentBody.trim()
-  if (!commentBody.startsWith(LANGUAGE_PREFIX)) return null
-
-  val startOffset = psiComment.text.findTextRange(LANGUAGE_PREFIX)!!.endOffset
-  val endOffset = psiComment.text.findTextRange(commentBody)!!.endOffset
-  return TextRange(startOffset, endOffset)
-}
-
 private data class LanguageSymbol(val name: String) : Symbol, Pointer<LanguageSymbol> {
   override fun createPointer(): Pointer<out Symbol> = this
   override fun dereference(): LanguageSymbol = this
@@ -72,7 +56,7 @@ internal class LanguageWordInCommentCompletionContributor : CompletionContributo
         if (parameters.invocationCount < 2) return
         if (!Registry.`is`("org.intellij.intelliLang.comment.completion")) return
         val psiComment = parameters.originalPosition?.asSafely<PsiComment>() ?: return
-        val trimmedBody = psiComment.commentBody.trim()
+        val trimmedBody = commentBody(psiComment).trim()
         if (trimmedBody.isBlank() ||
             trimmedBody.length != LANGUAGE_PREFIX.length && LANGUAGE_PREFIX.startsWith(trimmedBody, true)) {
           result.addElement(LookupElementBuilder.create(LANGUAGE_PREFIX))
@@ -81,42 +65,3 @@ internal class LanguageWordInCommentCompletionContributor : CompletionContributo
     })
   }
 }
-
-internal class LanguageCommentFolding : FoldingBuilderEx() {
-  override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
-    return SmartList<FoldingDescriptor>().also { result ->
-      root.accept(object : PsiRecursiveElementWalkingVisitor() {
-        override fun visitComment(comment: PsiComment) {
-          super.visitComment(comment)
-          if (getLanguageRange(comment) != null) {
-            result.add(FoldingDescriptor(comment, comment.textRange))
-          }
-        }
-      })
-    }.toArray(FoldingDescriptor.EMPTY_ARRAY)
-  }
-
-  override fun getPlaceholderText(node: ASTNode): String? {
-    val psiComment = node.psi.asSafely<PsiComment>() ?: return null
-    val languageRange = getLanguageRange(psiComment) ?: return null
-    val writtenText = languageRange.substring(psiComment.text)
-    return InjectorUtils.getLanguageByString(writtenText)?.id ?: writtenText
-  }
-
-  override fun isCollapsedByDefault(node: ASTNode): Boolean = true
-
-}
-
-private const val LANGUAGE_PREFIX = "language="
-
-@Suppress("NAME_SHADOWING")
-private val PsiComment.commentBody: String
-  get() {
-    val text = text
-    val commenter = LanguageCommenters.INSTANCE.forLanguage(language) ?: return text
-    for (lineCommentPrefix in commenter.lineCommentPrefixes) {
-      if (text.startsWith(lineCommentPrefix)) return text.substring(lineCommentPrefix.length)
-    }
-    return text.let { text -> commenter.blockCommentPrefix?.let { text.removePrefix(it) } ?: text }
-      .let { text -> commenter.blockCommentSuffix?.let { text.removeSuffix(it) } ?: text }
-  }
