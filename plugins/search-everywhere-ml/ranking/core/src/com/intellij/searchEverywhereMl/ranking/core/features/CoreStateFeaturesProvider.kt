ab95fe84bae037bd42a0363d3e5e05c74f7834e4
Adam Malek <adam.malek@jetbrains.com>
2025-07-18 10:53:13 +0200

    ML in SE: Use buildList instead of mutable list (IJPL-202712)
    GitOrigin-RevId: d3b49ac1e90ac2b6ea5ca0c542891cfbb15473ff

--- plugins/search-everywhere-ml/ranking/core/src/com/intellij/searchEverywhereMl/ranking/core/features/CoreStateFeaturesProvider.kt
+++ plugins/search-everywhere-ml/ranking/core/src/com/intellij/searchEverywhereMl/ranking/core/features/CoreStateFeaturesProvider.kt
@@ -64,38 +64,36 @@ internal class CoreStateFeaturesProvider : SearchEverywhereStateFeaturesProvider
 
   fun getFeatures(project: Project?, tab: SearchEverywhereTab, query: String,
                   searchScope: ScopeDescriptor?, isSearchEverywhere: Boolean): List<EventPair<*>> {
-    val features = arrayListOf<EventPair<*>>(
-      QUERY_LENGTH_DATA_KEY.with(query.length),
-      IS_EMPTY_QUERY_DATA_KEY.with(query.isEmpty()),
-      QUERY_CONTAINS_SPACES_DATA_KEY.with(query.contains(" ")),
-      QUERY_IS_CAMEL_CASE_DATA_KEY.with(query.isCamelCase()),
-      QUERY_CONTAINS_ABBREVIATIONS_DATA_KEY.with(query.containsAbbreviations()),
-      QUERY_IS_ALL_UPPERCASE_DATA_KEY.with(query.all { it.isUpperCase() }),
-      IS_SEARCH_EVERYWHERE_DATA_KEY.with(isSearchEverywhere)
-    )
+    return buildList {
+      add(QUERY_LENGTH_DATA_KEY.with(query.length))
+      add(IS_EMPTY_QUERY_DATA_KEY.with(query.isEmpty()))
+      add(QUERY_CONTAINS_SPACES_DATA_KEY.with(query.contains(" ")))
+      add(QUERY_IS_CAMEL_CASE_DATA_KEY.with(query.isCamelCase()), )
+      add(QUERY_CONTAINS_ABBREVIATIONS_DATA_KEY.with(query.containsAbbreviations()))
+      add(QUERY_IS_ALL_UPPERCASE_DATA_KEY.with(query.all { it.isUpperCase() }))
+      add(IS_SEARCH_EVERYWHERE_DATA_KEY.with(isSearchEverywhere))
 
-    project?.let {
-      val isDumb = DumbService.isDumb(project)
-      features.add(IS_DUMB_MODE.with(isDumb))
-    }
+      if (project != null) {
+        val isDumb = DumbService.isDumb(project)
+        add(IS_DUMB_MODE.with(isDumb))
 
-    if (hasSuitableContributor(tab, SearchEverywhereTab.Files)) {
-      features.addAll(getFileQueryFeatures(query))
-    }
-    if (hasSuitableContributor(tab, SearchEverywhereTab.All)) {
-      features.addAll(getAllTabQueryFeatures(query))
-    }
+        if (isTabWithTextContributor(tab)) {
+          addAll(getTextContributorFeatures(project))
+        }
+      }
 
-    searchScope?.displayName?.let { searchScopeDisplayName ->
-      val scopeId = ScopeIdMapper.instance.getScopeSerializationId(searchScopeDisplayName)
-      features.add(SEARCH_SCOPE_DATA_KEY.with(scopeId))
-    }
+      if (hasSuitableContributor(tab, SearchEverywhereTab.Files)) {
+        addAll(getFileQueryFeatures(query))
+      }
+      if (hasSuitableContributor(tab, SearchEverywhereTab.All)) {
+        addAll(getAllTabQueryFeatures(query))
+      }
 
-    if (project != null && isTabWithTextContributor(tab)) {
-      features.addAll(getTextContributorFeatures(project))
+      searchScope?.displayName?.let { searchScopeDisplayName ->
+        val scopeId = ScopeIdMapper.instance.getScopeSerializationId(searchScopeDisplayName)
+        add(SEARCH_SCOPE_DATA_KEY.with(scopeId))
+      }
     }
-
-    return features
   }
 
   private fun getFileQueryFeatures(query: String) = listOf(
