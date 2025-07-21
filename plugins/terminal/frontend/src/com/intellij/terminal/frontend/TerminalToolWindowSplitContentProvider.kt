cbaf12fb1ff46fb7eb30ae6c235d479c908f3e7d
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-07-21 17:32:45 +0300

    [terminal] IJPL-194083 Make split terminal tab open in the same directory as the original one
    GitOrigin-RevId: 688d6a5fdf4a649abc173e6134c571e2bdc17e41

--- plugins/terminal/frontend/src/com/intellij/terminal/frontend/TerminalToolWindowSplitContentProvider.kt
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/TerminalToolWindowSplitContentProvider.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.toolWindow.ToolWindowSplitContentProvider
 import com.intellij.ui.content.Content
 import org.jetbrains.plugins.terminal.TerminalOptionsProvider
+import org.jetbrains.plugins.terminal.TerminalTabState
 import org.jetbrains.plugins.terminal.TerminalToolWindowManager
 import org.jetbrains.plugins.terminal.fus.TerminalOpeningWay
 import org.jetbrains.plugins.terminal.fus.TerminalStartupFusInfo
@@ -12,12 +13,18 @@ internal class TerminalToolWindowSplitContentProvider : ToolWindowSplitContentPr
   override fun createContentCopy(project: Project, content: Content): Content {
     val startupFusInfo = TerminalStartupFusInfo(TerminalOpeningWay.SPLIT_TOOLWINDOW)
     val manager = TerminalToolWindowManager.getInstance(project)
-    // TODO: start the new session in the same working directory as the provided content's session
+
+    val widget = TerminalToolWindowManager.findWidgetByContent(content)
+    val currentDirectory = widget?.getCurrentDirectory()
+    val tabState = TerminalTabState().also {
+      it.myWorkingDirectory = currentDirectory
+    }
+
     return manager.createTerminalContent(
       manager.terminalRunner,
       TerminalOptionsProvider.instance.terminalEngine,
       null,
-      null,
+      tabState,
       null,
       startupFusInfo,
       true,
