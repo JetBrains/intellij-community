e2a3abffbf63216a6806442f985d273c51982e81
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2025-07-21 11:19:38 +0200

    [vcs] IJPL-173924 Convert VcsKey to Kt and make @Serializable
    GitOrigin-RevId: e446269410c91f942fb9b465f6bcfda6a75d7e38

--- platform/vcs-api/vcs-api-core/src/com/intellij/openapi/vcs/VcsKey.kt
+++ platform/vcs-api/vcs-api-core/src/com/intellij/openapi/vcs/VcsKey.kt
@@ -1,44 +1,12 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.vcs;
+package com.intellij.openapi.vcs
 
-import com.intellij.openapi.util.IntellijInternalApi;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.util.IntellijInternalApi
+import kotlinx.serialization.Serializable
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.NonNls
 
-public final class VcsKey {
-  private final @NotNull String myName;
-
-  // to forbid creation outside AbstractVcs
-  @ApiStatus.Internal
-  @IntellijInternalApi
-  public VcsKey(@NotNull @NonNls String name) {
-    myName = name;
-  }
-
-  public @NonNls @NotNull String getName() {
-    return myName;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    VcsKey vcsKey = (VcsKey)o;
-
-    if (!myName.equals(vcsKey.myName)) return false;
-
-    return true;
-  }
-
-  @Override
-  public int hashCode() {
-    return myName.hashCode();
-  }
-
-  @Override
-  public String toString() {
-    return myName;
-  }
+@Serializable
+data class VcsKey @ApiStatus.Internal @IntellijInternalApi constructor(val name: @NonNls String) {
+  override fun toString(): String = name
 }
