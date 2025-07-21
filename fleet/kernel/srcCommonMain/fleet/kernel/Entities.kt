ee249ca59bd472338e5b1fa456955982f478072f
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2025-07-21 15:55:31 +0200

    [kernel] `useEntity`: fix `UnsatisfiedMatchException` during entity deletion
    GitOrigin-RevId: 8ab50a97d96235c0495f993ace409d67c08f2d74

--- fleet/kernel/srcCommonMain/fleet/kernel/Entities.kt
+++ fleet/kernel/srcCommonMain/fleet/kernel/Entities.kt
@@ -2,10 +2,10 @@
 package fleet.kernel
 
 import com.jetbrains.rhizomedb.Entity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.NonCancellable
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.withContext
+import fleet.kernel.rete.ContextMatches
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.*
+import kotlin.coroutines.EmptyCoroutineContext
 
 /**
  * Binds entity lifecycle to a coroutine.
@@ -31,7 +31,32 @@ suspend fun <E : Entity, R> E.useEntity(
     }
   }
   finally {
-    withContext(NonCancellable) {
+    /**
+     * Consider the following situation:
+     * ```
+     * E1.each().launchOnEach {
+     *   change {
+     *     E2.new(...)
+     *   }.useEntity {
+     *     awaitCancellation()
+     *   }
+     * }
+     * ```
+     * Deletion of `E1` triggers the cancellation of `launchOnEach` block,
+     * which in turn cancels `awaitCancellation`,
+     * and we get into the current `finally` block trying to delete `E2`.
+     * At this point we'll get the exception `UnsatisfiedMatchException: match invalidated by rete`
+     * where the match is the `E1` entity, and the match was indeed invalidated because `E1` was just deleted.
+     *
+     * But now we are in a situation where we can't clean up `E2`.
+     * To avoid `UnsatisfiedMatchException` we remove `ContextMatches` from the current coroutine context.
+     * This is not safe in general, but here we control the code,
+     * and we know that `E2` is being deleted and that's it.
+     */
+    val contextMatches = currentCoroutineContext()[ContextMatches]
+    val withoutContextMatches = contextMatches?.copy(matches = persistentListOf())
+                                ?: EmptyCoroutineContext
+    withContext(NonCancellable + withoutContextMatches) {
       change {
         e.delete()
       }
