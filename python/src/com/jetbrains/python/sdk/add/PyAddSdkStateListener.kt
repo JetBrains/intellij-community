3f6e828379956db38ed6687437e5966d1a389662
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2025-07-22 18:44:37 +0200

    Python: drop unused symbol
    GitOrigin-RevId: f732aae1b363d269a28e7afbdb27bf6db00d9529

--- python/src/com/jetbrains/python/sdk/add/PyAddSdkStateListener.kt
+++ python/src/com/jetbrains/python/sdk/add/PyAddSdkStateListener.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.sdk.add
 
-interface PyAddSdkStateListener {
+import kotlin.DeprecationLevel.ERROR
 
-}
+@Deprecated("This interface isn't used", level = ERROR)
+interface PyAddSdkStateListener
