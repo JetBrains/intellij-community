fa2febfde0db9aca92357806c99dd3d141dd49be
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2025-07-22 14:28:47 +0200

    Fix Py3TypeCheckerInspectionTest#testWithItemNonContextManager
    _GeneratorContextManager is now a generic over 2 parameters, not 1
    
    GitOrigin-RevId: 123d31b3b4b52bb672aaa05ed99a3f3131c98e27

--- python/testData/inspections/PyTypeCheckerInspection/WithItemNonContextManager.py
+++ python/testData/inspections/PyTypeCheckerInspection/WithItemNonContextManager.py
@@ -41,7 +41,7 @@ with (
     <warning descr="Expected type 'contextlib.AbstractContextManager', got 'CustomAsyncContextManager' instead">CustomAsyncContextManager()</warning>,
     <warning descr="Expected type 'contextlib.AbstractContextManager', got 'NonContextManager' instead">NonContextManager()</warning>,
     generator_cm(),
-    <warning descr="Expected type 'contextlib.AbstractContextManager', got '_AsyncGeneratorContextManager[None]' instead">generator_acm()</warning>,
+    <warning descr="Expected type 'contextlib.AbstractContextManager', got '_AsyncGeneratorContextManager[None, None]' instead">generator_acm()</warning>,
     <warning descr="Expected type 'contextlib.AbstractContextManager', got 'object' instead">object()</warning>
 ):
     pass
@@ -53,7 +53,7 @@ async def f():
         <warning descr="Expected type 'contextlib.AbstractAsyncContextManager', got 'CustomContextManager2' instead">CustomContextManager2()</warning>,
         CustomAsyncContextManager(),
         <warning descr="Expected type 'contextlib.AbstractAsyncContextManager', got 'NonContextManager' instead">NonContextManager()</warning>,
-        <warning descr="Expected type 'contextlib.AbstractAsyncContextManager', got '_GeneratorContextManager[None]' instead">generator_cm()</warning>,
+        <warning descr="Expected type 'contextlib.AbstractAsyncContextManager', got '_GeneratorContextManager[None, None, None]' instead" >generator_cm()</warning>,
         generator_acm(),
         <warning descr="Expected type 'contextlib.AbstractAsyncContextManager', got 'object' instead">object()</warning>
     ):
