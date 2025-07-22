45d3c7a6919a7d1a881924b4e61be612e01a318d
Ilya Kazakevich <ilya.kazakevich@jetbrains.com>
2025-07-22 12:49:44 +0000

    PY-76167: Do not access FS from EDT + test.
    PY-76167: Convert class to a light service to satisfy Qodana
    
    PY-76167: Do not access FS from EDT + test.
    
    `PyConfigurableInterpreterList` checks sdk type to sort it.
    
    To get SDK type it used to access FS. It does no more: it now uses SDK additional data.
    
    To test it, we've introduced `FORBID_SLOW_OPS_PROPERTY`
    
    
    Merge-request: IJ-MR-169961
    Merged-by: Ilya Kazakevich <ilya.kazakevich@jetbrains.com>
    
    GitOrigin-RevId: 80631e31a021e5ea7007c5873596446ef6d8665e

--- python/python-psi-impl/src/com/jetbrains/python/sdk/PythonSdkUtil.java
+++ python/python-psi-impl/src/com/jetbrains/python/sdk/PythonSdkUtil.java
@@ -24,6 +24,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.util.ObjectUtils;
 import com.intellij.util.PlatformUtils;
+import com.intellij.util.SlowOperations;
 import com.intellij.util.SystemProperties;
 import com.intellij.util.concurrency.annotations.RequiresBackgroundThread;
 import com.intellij.util.containers.ContainerUtil;
@@ -34,10 +35,7 @@ import com.jetbrains.python.module.PyModuleService;
 import com.jetbrains.python.psi.search.PySearchUtilBase;
 import com.jetbrains.python.sdk.skeleton.PySkeletonHeader;
 import com.jetbrains.python.venvReader.VirtualEnvReader;
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.Unmodifiable;
+import org.jetbrains.annotations.*;
 
 import java.io.File;
 import java.nio.file.Files;
@@ -364,7 +362,9 @@ public final class PythonSdkUtil {
    * @param binaryPath must point to a Python interpreter
    * @return if the surroundings look like a virtualenv installation, its root is returned (normally the grandparent of binaryPath).
    */
+  @RequiresBackgroundThread(generateAssertion = false)
   public static @Nullable File getVirtualEnvRoot(final @NotNull String binaryPath) {
+    SlowOperations.assertSlowOperationsAreAllowed();
     final File bin = new File(binaryPath).getParentFile();
     if (bin != null) {
       final String rootPath = bin.getParent();
@@ -464,6 +464,7 @@ public final class PythonSdkUtil {
     return libDir != null ? libDir.findChild(PyNames.SITE_PACKAGES) : null;
   }
 
+  @RequiresBackgroundThread(generateAssertion = false)
   public static boolean isVirtualEnv(@NotNull Sdk sdk) {
     final String path = sdk.getHomePath();
     return isVirtualEnv(path);
@@ -474,6 +475,7 @@ public final class PythonSdkUtil {
     return path != null && getVirtualEnvRoot(path) != null;
   }
 
+  @RequiresBackgroundThread(generateAssertion = false)
   public static @Nullable VirtualFile getCondaDirectory(@NotNull Sdk sdk) {
     final VirtualFile homeDirectory = sdk.getHomeDirectory();
     if (homeDirectory == null) return null;
@@ -481,11 +483,14 @@ public final class PythonSdkUtil {
     return homeDirectory.getParent().getParent();
   }
 
+  @RequiresBackgroundThread(generateAssertion = false)
   public static boolean isCondaVirtualEnv(@NotNull Sdk sdk) {
     return isCondaVirtualEnv(sdk.getHomePath());
   }
 
+  @RequiresBackgroundThread(generateAssertion = false)
   public static boolean isCondaVirtualEnv(@Nullable String sdkPath) {
+    SlowOperations.assertSlowOperationsAreAllowed();
     final VirtualFile condaMeta = findCondaMeta(sdkPath);
     if (condaMeta == null) {
       return false;
@@ -531,7 +536,10 @@ public final class PythonSdkUtil {
     return parent.findChild("envs") != null;
   }
 
+  @RequiresBackgroundThread(generateAssertion = false)
+  @ApiStatus.Internal
   public static @Nullable VirtualFile findCondaMeta(@Nullable String sdkPath) {
+    SlowOperations.assertSlowOperationsAreAllowed();
     if (sdkPath == null || CustomSdkHomePattern.isCustomPythonSdkHomePath(sdkPath)) {
       return null;
     }
