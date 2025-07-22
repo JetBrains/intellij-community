f1f5123f3516ef02be0d8a3dcab07e0bbe8dca7d
Roman Golyshev <roman.golyshev@jetbrains.com>
2025-07-22 13:45:33 +0200

    KT-76782 [kotlin] Fix testdata after changes in resolve
    Now the missing parameter is reported before
    reporting the problem with inaccessibility
    
    GitOrigin-RevId: ff3b5f6499350d2a98cc4b9d97d85816e9a7f2db

--- plugins/kotlin/idea/tests/testData/quickfix/supertypeInitialization/noAccessibleConstructors.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/supertypeInitialization/noAccessibleConstructors.kt
@@ -1,6 +1,6 @@
 // "class org.jetbrains.kotlin.idea.quickfix.SuperClassNotInitialized$AddParenthesisFix" "false"
 // ERROR: This type has a constructor, and thus must be initialized here
-// K2_AFTER_ERROR: Cannot access 'constructor(p: Int): Base': it is private in 'Base'.
+// K2_AFTER_ERROR: No value passed for parameter 'p'.
 // K2_AFTER_ERROR: This type has a constructor, so it must be initialized here.
 open class Base private constructor(p: Int)
 
