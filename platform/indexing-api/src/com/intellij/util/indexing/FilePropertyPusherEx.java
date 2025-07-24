b9fc147641a615a037b84dc50d147e43e8ea0ec5
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2025-07-24 18:38:37 +0200

    Javadoc for FilePropertyPusherEx
    GitOrigin-RevId: bf1d91a5deac6b0f3ca7f9d395c04cdbcdc448c1

--- platform/indexing-api/src/com/intellij/util/indexing/FilePropertyPusherEx.java
+++ platform/indexing-api/src/com/intellij/util/indexing/FilePropertyPusherEx.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.indexing;
 
 import com.intellij.openapi.project.Project;
@@ -8,6 +8,16 @@ import org.jetbrains.annotations.ApiStatus.Internal;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * As of 25.2 {@link FilePropertyPusher} allows pushing properties outside module content only
+ * through {@link FilePropertyPusher#initExtra(Project)} and {@link FilePropertyPusher#afterRootsChanged(Project)}.
+ * Such API enforced pushing values to all related files in project, ignoring information about the scope of the current scanning.
+ * It's inefficient, and may result in a performance issue (see IJPL-2963).
+ * <p>
+ * {@link FilePropertyPusherEx} allows selecting applicable {@link IndexableSetOrigin}s of current scanning, and push new values only there,
+ * making pusher's work incremental instead of a push to all related files
+ * on every {@link com.intellij.openapi.roots.ModuleRootEvent} (which includes every scanning) and on opening a project.
+ */
 @Internal
 public interface FilePropertyPusherEx<T> extends FilePropertyPusher<T> {
 
