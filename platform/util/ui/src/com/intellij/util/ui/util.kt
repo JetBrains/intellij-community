df98d6fc3f8ddb0195225bb59ebffefa9609d05c
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-07-24 17:09:13 +0200

    [threading] IJPL-196952: Handle jobs more carefully
    In case of frequent synchronous event dispatch, the created `Job`s retain their stacktrace which may lead to OOMs in tests. Here we limit the processing to a single coroutine
    
    GitOrigin-RevId: 1dba938ef53eb15636c57e8eeb8c9cd41383eefa

--- platform/util/ui/src/com/intellij/util/ui/util.kt
+++ platform/util/ui/src/com/intellij/util/ui/util.kt
@@ -5,22 +5,40 @@ package com.intellij.util.ui
 
 import com.intellij.openapi.application.AccessToken
 import com.intellij.openapi.diagnostic.logger
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.jetbrains.annotations.ApiStatus
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.time.Duration.Companion.minutes
 
-internal fun reportTooLongDispatch(): AccessToken {
-  @Suppress("OPT_IN_USAGE") val job = GlobalScope.launch {
+private val snapshot = AtomicInteger(0)
+private val reporting = AtomicBoolean(false)
+
+@OptIn(DelicateCoroutinesApi::class)
+private val reportingJob = GlobalScope.launch {
+  while (true) {
+    val oldCounter = snapshot.get()
+    val oldReporting = reporting.get()
     delay(2.minutes)
-    logger<UIUtil>().warn("Synchronous AWT Event dispatch takes too long. This may be a deadlock in tests caused by a pending background write action.\n" +
-                          "Consider using `com.intellij.testFramework.PlatformTestUtil.dispatchAllInvocationEventsInIdeEventQueue`, instead of your way of `UIUtil.dispatchAllInvocationEvents`" +
-                          "which permits execution of background write actions")
+    val newCounter = snapshot.get()
+    val newReporting = reporting.get()
+    if (newCounter == oldCounter && oldReporting && newReporting) {
+      logger<UIUtil>().warn("Synchronous AWT Event dispatch takes too long. This may be a deadlock in tests caused by a pending background write action.\n" +
+                            "Consider using `com.intellij.testFramework.PlatformTestUtil.dispatchAllInvocationEventsInIdeEventQueue`, instead of your way of `UIUtil.dispatchAllInvocationEvents`" +
+                            "which permits execution of background write actions")
+    }
   }
+}
+
+internal fun reportTooLongDispatch(): AccessToken {
+  snapshot.incrementAndGet()
+  reporting.set(true)
   return object : AccessToken() {
     override fun finish() {
-      job.cancel()
+      reporting.set(false)
     }
   }
 }
