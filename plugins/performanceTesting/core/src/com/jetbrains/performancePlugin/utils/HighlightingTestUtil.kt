678b7fe146f5a41ec5c3cb44a29772fbfddd6c3f
Alexey Kryuchkov <alexey.kryuchkov@jetbrains.com>
2025-07-24 11:14:09 +0400

    Added reopenFileAfterIdeRestart metric for monolith
    GitOrigin-RevId: 101f99bc157d547d1b0c6f0b6ee70d391a6f5f07

--- plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/utils/HighlightingTestUtil.kt
+++ plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/utils/HighlightingTestUtil.kt
@@ -1,10 +1,18 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.performancePlugin.utils
 
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
 import com.intellij.platform.diagnostic.telemetry.Scope
 import com.intellij.platform.diagnostic.telemetry.TelemetryManager
 import java.util.concurrent.TimeUnit
 import com.intellij.openapi.util.Pair
+import com.intellij.openapi.util.Ref
+import com.jetbrains.performancePlugin.commands.CodeAnalysisStateListener
+import io.opentelemetry.api.trace.Span
+import java.util.concurrent.TimeoutException
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 
 class HighlightingTestUtil {
 
@@ -21,5 +29,25 @@ class HighlightingTestUtil {
       span.end(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
     }
 
+
+    @JvmStatic
+    suspend fun waitForAnalysisWithNewApproach(project: Project, spanRef: Ref<Span>, timeout: Long, suppressErrors: Boolean) {
+      waitForAnalysisWithNewApproach(project, spanRef.get(), timeout, suppressErrors)
+    }
+
+    @JvmStatic
+    suspend fun waitForAnalysisWithNewApproach(project: Project, span: Span?, timeout: Long, suppressErrors: Boolean) {
+      val timeoutDuration = if (timeout == 0L) 5.minutes else timeout.seconds
+      try {
+        project.service<CodeAnalysisStateListener>().waitAnalysisToFinish(timeoutDuration, !suppressErrors)
+      }
+      catch (e: TimeoutException) {
+        span?.setAttribute("timeout", "true")
+      }
+      finally {
+        span?.end()
+      }
+    }
   }
+
 }
