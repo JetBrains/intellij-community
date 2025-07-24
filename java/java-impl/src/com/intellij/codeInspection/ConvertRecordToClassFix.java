b6ba7e49842a54009713b3cf2dde08e25bfb1d03
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-07-24 14:03:10 +0200

    [java-inspections] IDEA-333755 Fix incorrect annotations not being removed from the "main" generated constructor
    This bug occurs when at least one record component refers to a nested class.
    
    It is a weird bug, and one that I don't know the root cause of. But this workaround fixes the visible cause.
    
    GitOrigin-RevId: 18b779ae79ef82e14764254ac2c181c8460b7089

--- java/java-impl/src/com/intellij/codeInspection/ConvertRecordToClassFix.java
+++ java/java-impl/src/com/intellij/codeInspection/ConvertRecordToClassFix.java
@@ -172,7 +172,14 @@ public class ConvertRecordToClassFix extends PsiUpdateModCommandAction<PsiElemen
         postProcessFieldAnnotations(componentMap, field);
       }
     }
+    boolean canonicalConstructorSeen = false;
     for (PsiMethod method : converted.getMethods()) {
+      if (method.isConstructor() && !canonicalConstructorSeen) {
+        // The canonical constructor we generate always comes before other constructors (if any).
+        canonicalConstructorSeen = true;
+        postProcessConstructorAnnotations(method);
+      }
+
       postProcessMethodAnnotations(recordClass, componentMap, method);
     }
   }
@@ -192,23 +199,21 @@ public class ConvertRecordToClassFix extends PsiUpdateModCommandAction<PsiElemen
     copyAnnotations(field, component, PsiAnnotation.TargetType.FIELD);
   }
 
-  private static void postProcessMethodAnnotations(PsiClass recordClass, Map<String, PsiRecordComponent> componentMap, PsiMethod method) {
-    if (method.hasModifierProperty(PsiModifier.STATIC)) return;
-    if (method.isConstructor()) {
-      PsiMethod recordCtor = recordClass.findMethodBySignature(method, false);
-      // Record constructor is generated with all component annotations, so we need to delete inappropriate ones
-      if (recordCtor instanceof SyntheticElement) {
-        for (PsiParameter parameter : method.getParameterList().getParameters()) {
-          for (PsiAnnotation annotation : parameter.getAnnotations()) {
-            if (AnnotationTargetUtil
-                  .findAnnotationTarget(annotation, PsiAnnotation.TargetType.PARAMETER, PsiAnnotation.TargetType.TYPE_USE) == null) {
-              annotation.delete();
-            }
-          }
+  private static void postProcessConstructorAnnotations(PsiMethod method) {
+    for (PsiParameter parameter : method.getParameterList().getParameters()) {
+      for (PsiAnnotation annotation : parameter.getAnnotations()) {
+        if (AnnotationTargetUtil
+              .findAnnotationTarget(annotation, PsiAnnotation.TargetType.PARAMETER, PsiAnnotation.TargetType.TYPE_USE) == null) {
+          annotation.delete();
         }
       }
     }
-    else if (method.getParameterList().isEmpty()) {
+  }
+
+  private static void postProcessMethodAnnotations(PsiClass recordClass, Map<String, PsiRecordComponent> componentMap, PsiMethod method) {
+    if (method.hasModifierProperty(PsiModifier.STATIC)) return;
+    if (method.isConstructor()) return;
+    if (method.getParameterList().isEmpty()) {
       PsiRecordComponent component = componentMap.get(method.getName());
       if (component != null) {
         PsiMethod recordMethod = recordClass.findMethodBySignature(method, false);
---
+++ java/java-tests/testData/inspection/recordCanBeClass/afterAnnotationsAndNestedClass_3.java
@@ -0,0 +1,68 @@
+// "Convert record to class" "true-preview"
+import java.lang.annotation.*;
+import java.util.Objects;
+
+
+@Target(ElementType.METHOD)
+@interface MethodAnno {
+  int value();
+}
+
+final class Person {
+    @Deprecated
+    private final String name;
+    private final int age;
+    private final Address address;
+
+    Person(
+            @Deprecated String name,
+            int age,
+            Address address
+    ) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    @MethodAnno
+    @Deprecated
+    public String name() {
+        return name;
+    }
+
+    @MethodAnno
+    public int age() {
+        return age;
+    }
+
+    @MethodAnno
+    public Address address() {
+        return address;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Person) obj;
+        return Objects.equals(this.name, that.name) &&
+                this.age == that.age &&
+                Objects.equals(this.address, that.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, address);
+    }
+
+    @Override
+    public String toString() {
+        return "Person[" +
+                "name=" + name + ", " +
+                "age=" + age + ", " +
+                "address=" + address + ']';
+    }
+
+}
+
+record Address(String street, int number) {}
