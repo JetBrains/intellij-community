1a4c48114e6dcbdcc1be14388fdc657db1785103
Andrei.Kuznetsov <andrei.kuznetsov@jetbrains.com>
2025-07-25 15:10:59 +0200

    Revert "IJPL-198137 debugging: log before/after yield"
    This reverts commit 6ab12cd39444a27f74b4ddc14ce0cfc9dfaa150f.
    
    We've confirmed that the problem is indeed in the `yield()` call. Debug code is not needed anymore.
    
    GitOrigin-RevId: 98203112e8134e31b2a1412bcad1e297384ba282

--- platform/lang-impl/src/com/intellij/util/indexing/contentQueue/IndexUpdateRunner.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/contentQueue/IndexUpdateRunner.kt
@@ -201,14 +201,7 @@ class IndexUpdateRunner(
 
                 i++
                 if (i % 10 == 0) {
-                  try {
-                    if (IJPL198137){
-                      LOG.info("IJPL198137: Before yield")
-                    }
-                    yield()
-                  } finally {
-                    LOG.info("IJPL198137: After yield")
-                  }
+                  yield()
                 }
               }
             }
@@ -447,8 +440,6 @@ class IndexUpdateRunner(
 
     /** Number indexing tasks to run in parallel */
     val INDEXING_PARALLELIZATION: Int = UnindexedFilesUpdater.getMaxNumberOfIndexingThreads()
-
-    private val IJPL198137 = getBooleanProperty("IJPL198137", false)
 
     /**
      * Soft cap of memory we are using for loading files content during indexing process.
