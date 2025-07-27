af9f5c9be6a57d8bdee0a514d811476eaf89f566
Ilia Kirianovskii <Ilia.Kirianovskii@jetbrains.com>
2025-07-27 22:07:36 +0200

    [rules_jvm] Don't add __index__ file in JvmWorkerKt#createZip (IJI-2835)
    GitOrigin-RevId: cd8a0be97dfbda21514369fa05f048c7d7bfa8b8

--- build/jvm-rules/src/misc/JvmWorker.kt
+++ build/jvm-rules/src/misc/JvmWorker.kt
@@ -4,8 +4,6 @@ package org.jetbrains.bazel.jvm
 import io.opentelemetry.api.trace.Tracer
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.jetbrains.intellij.build.io.AddDirEntriesMode
-import org.jetbrains.intellij.build.io.PackageIndexBuilder
 import org.jetbrains.intellij.build.io.zipWriter
 import java.io.File
 import java.io.Writer
@@ -91,11 +89,9 @@ private suspend fun createZip(outJar: Path, inputs: Array<Input>, baseDir: Path,
   //Files.writeString(Path.of("/tmp/f2.txt"), stripPrefixWithSlash + "\n" + files.joinToString("\n") { it.toString() })
 
   withContext(Dispatchers.IO) {
-    val packageIndexBuilder = PackageIndexBuilder(AddDirEntriesMode.RESOURCE_ONLY)
-    zipWriter(targetFile = outJar, packageIndexBuilder = packageIndexBuilder, overwrite = true).use { stream ->
+    zipWriter(targetFile = outJar, packageIndexBuilder = null, overwrite = true).use { stream ->
       for (path in files) {
         val name = addPrefixWithSlash + path
-        packageIndexBuilder.addFile(name = name)
         stream.fileWithoutCrc(path = name.toByteArray(), file = root.resolve(path))
       }
     }
