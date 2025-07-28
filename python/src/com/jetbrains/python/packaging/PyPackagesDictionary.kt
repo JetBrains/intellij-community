8917e8fbea790f2e4144cf8ce01e3c2fea08b9b9
Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
2025-07-28 15:48:53 +0000

    IJPL-198335 Only checking supported languages' typo and ignoring other unsupported languages like Bulgarian
    Co-authored-by: Peter Gromov <peter.gromov@jetbrains.com>
    
    Merge-request: IJ-MR-170350
    Merged-by: Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
    
    (cherry picked from commit dc696f6772e22b88d0c9edff535aff4da15c24bf)
    
    IJ-MR-170350
    
    GitOrigin-RevId: 115225541d617cec514e7d04219429b3173173cf

--- python/src/com/jetbrains/python/packaging/PyPackagesDictionary.kt
+++ python/src/com/jetbrains/python/packaging/PyPackagesDictionary.kt
@@ -16,7 +16,9 @@ import org.jetbrains.annotations.ApiStatus
 class PyPackagesDictionary : Dictionary {
   override fun getName(): String = message("python.packages.dictionary.name")
 
-  override fun contains(word: String): Boolean? = service<PypiPackageCache>().contains(word)
+  override fun contains(word: String): Boolean? = throw UnsupportedOperationException()
+
+  override fun lookup(word: String): Dictionary.LookupStatus = service<PypiPackageCache>().lookup(word)
 
   override fun getWords(): MutableSet<String> = throw UnsupportedOperationException()
 
