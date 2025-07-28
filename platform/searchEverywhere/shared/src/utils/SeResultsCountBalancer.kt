1284de0cfd3c6b1bf51bdeba238baaadbb8bffb8
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2025-07-28 13:57:00 +0200

    IJPL-198927 Search Everywhere: move SeResultsCountBalancer to shared
    GitOrigin-RevId: 9b0ada294baa66571d03895a61e9f05a966486d2

--- platform/searchEverywhere/shared/src/utils/SeResultsCountBalancer.kt
+++ platform/searchEverywhere/shared/src/utils/SeResultsCountBalancer.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.searchEverywhere.backend.impl
+package com.intellij.platform.searchEverywhere.utils
 
 import com.intellij.platform.searchEverywhere.SeItemData
 import com.intellij.platform.searchEverywhere.SeProviderId
