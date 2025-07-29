74ac002f346a65be5ba66a64fcb521abf7380395
Dmitry Drobotov <dmitry.drobotov@jetbrains.com>
2025-07-29 19:13:04 +0200

    IJPL-187935 Implement accessibility support for ToolbarComboButton
    GitOrigin-RevId: 4dd41227ed1b18d31e87f43849e31c4859b069bf

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/ToolbarComboButton.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/ToolbarComboButton.kt
@@ -7,15 +7,25 @@ import com.intellij.openapi.actionSystem.ActionToolbar
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
+import com.intellij.ui.UIBundle
 import com.intellij.util.ui.JBInsets
 import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.Nls
 import java.awt.Insets
+import java.awt.event.ActionEvent
+import javax.accessibility.Accessible
+import javax.accessibility.AccessibleAction
+import javax.accessibility.AccessibleContext
+import javax.accessibility.AccessibleRole
+import javax.swing.UIManager
 import kotlin.properties.Delegates
 
-open class ToolbarComboButton(val model: ToolbarComboButtonModel) : AbstractToolbarCombo() {
+open class ToolbarComboButton(val model: ToolbarComboButtonModel) : AbstractToolbarCombo(), Accessible {
   var margin: Insets by Delegates.observable(JBInsets.emptyInsets(), this::fireUpdateEvents)
 
   internal var preferredHeightSupplier: (() -> Int)? = null
+
+  var accessibleNamePrefix: @Nls String? = null
 
   override fun getUIClassID(): String = "ToolbarComboButtonUI"
 
@@ -28,6 +38,43 @@ open class ToolbarComboButton(val model: ToolbarComboButtonModel) : AbstractTool
   }
 
   override fun getLeftGap(): Int = insets.left + margin.left
+
+  override fun getAccessibleContext(): AccessibleContext? {
+    if (accessibleContext == null) {
+      accessibleContext = object : AccessibleJComponent(), AccessibleAction {
+        override fun getAccessibleName(): String? =
+          when {
+            accessibleName != null -> accessibleName
+            accessibleNamePrefix != null && !text.isNullOrEmpty() -> UIBundle.message("toolbar.combo.button.accessible.name.with.prefix",
+                                                                                      accessibleNamePrefix, text)
+            !text.isNullOrEmpty() -> UIBundle.message("toolbar.combo.button.accessible.name", text)
+            else -> null
+          }
+
+        override fun getAccessibleRole(): AccessibleRole = AccessibleRole.PUSH_BUTTON
+
+        override fun getAccessibleAction(): AccessibleAction = this
+
+        override fun getAccessibleActionCount(): Int = 1
+
+        override fun getAccessibleActionDescription(i: Int): String? =
+          when {
+            i == 0 -> UIManager.getString("AbstractButton.clickText")
+            else -> null
+          }
+
+        override fun doAccessibleAction(i: Int): Boolean {
+          if (i == 0) {
+            val ae = ActionEvent(this@ToolbarComboButton, 0, null, System.currentTimeMillis(), 0)
+            model.getActionListeners().forEach { l -> l.actionPerformed(ae) }
+            return true
+          }
+          return false
+        }
+      }
+    }
+    return accessibleContext
+  }
 }
 
 @ApiStatus.Internal
