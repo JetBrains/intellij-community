3767ec32bb504a057c68f837115dd27a7a5f3cf6
Vladimir.Koshelev <vladimir.koshelev@jetbrains.com>
2025-07-29 17:47:14 +0200

    [vision] adjust PyCharmWhatsNewInVisionContentProvider to a new refactoring
    GitOrigin-RevId: 3d1548b2f528fd1ada3bdcf355ec596bd9ded22f

--- python/ide/impl/src/com/intellij/pycharm/community/ide/impl/whatsnew/PyCharmWhatsNewInVisionContentProvider.kt
+++ python/ide/impl/src/com/intellij/pycharm/community/ide/impl/whatsnew/PyCharmWhatsNewInVisionContentProvider.kt
@@ -8,9 +8,17 @@ import com.intellij.util.PlatformUtils.isCommunityEdition
 
 class PyCharmWhatsNewInVisionContentProvider : WhatsNewInVisionContentProvider() {
 
+  override fun getResourceNameByPath(path: String): String {
+    return "whatsNew/$path"
+  }
+
+  override fun getResource(resourceName: String): ContentSource {
+    return ResourceContentSource(PyCharmWhatsNewInVisionContentProvider::class.java.classLoader, resourceName)
+  }
+
   override fun getResource(): ContentSource {
     // return a vision file for the current version
-    val resourceName = if (isCommunityEdition()) "whatsNew/pycharmCE2024.2.json" else "whatsNew/pycharm2024.2.json"
+    val resourceName = if (isCommunityEdition()) "whatsNew/pycharmCE2025.2.json" else "whatsNew/pycharm2025.2.json"
     val resourceContentSource = ResourceContentSource(PyCharmWhatsNewInVisionContentProvider::class.java.classLoader, resourceName)
     return resourceContentSource
   }
