d623c92075cf5a13dd484740ffe729bb5970da88
Kate Botsman <kate.botsman@jetbrains.com>
2025-07-30 18:01:03 +0200

    IJPL-197162 Fix Embedded frontend doesn't restart after plugin installation
    Apply changes from IJPL-181051 to RemoteDevHost and monolith only.
    JBC cannot be restarted this way as we don't provide `jetbrains_client.sh` script on mac os.
    
    It's better to use `open -n`
    
    
    (cherry picked from commit 35a527612fb6bf2b98604d6a0f0f6f19422df663)
    
    IJ-CR-170877
    
    GitOrigin-RevId: 2fd1616b355c6969de883b5e3e7c58b344fc94f4

--- platform/platform-impl/src/com/intellij/util/Restarter.java
+++ platform/platform-impl/src/com/intellij/util/Restarter.java
@@ -44,9 +44,16 @@ public final class Restarter {
       }
     }
     else if (SystemInfo.isMac) {
-      var starter = Path.of(PathManager.getHomePath(), "MacOS", ApplicationNamesInfo.getInstance().getScriptName());
-      if (Files.exists(starter)) {
-        return starter;
+      if (PlatformUtils.isJetBrainsClient()) {
+        var appDir = Path.of(PathManager.getHomePath()).getParent();
+        if (appDir != null && appDir.getFileName().toString().endsWith(".app") && Files.isDirectory(appDir)) {
+          return appDir;
+        }
+      } else {
+        var starter = Path.of(PathManager.getHomePath(), "MacOS", ApplicationNamesInfo.getInstance().getScriptName());
+        if (Files.exists(starter)) {
+          return starter;
+        }
       }
     }
     else if (SystemInfo.isLinux) {
@@ -151,7 +158,11 @@ public final class Restarter {
       restartOnWindows(elevate, List.of(beforeRestart), mainAppArgs);
     }
     else if (SystemInfo.isMac) {
-      restartOnMac(List.of(beforeRestart), mainAppArgs);
+      if (PlatformUtils.isJetBrainsClient()) {
+        restartJBCOnMac(List.of(beforeRestart), mainAppArgs);
+      } else {
+        restartOnMac(List.of(beforeRestart), mainAppArgs);
+      }
     }
     else if (SystemInfo.isLinux) {
       restartOnLinux(List.of(beforeRestart), mainAppArgs);
@@ -176,6 +187,29 @@ public final class Restarter {
     }
     command.add(starter.toString());
     command.addAll(args);
+    runRestarter(command);
+  }
+
+  private static void restartJBCOnMac(List<String> beforeRestart, List<String> args) throws IOException {
+    var appDir = ourStarter.getValue();
+    if (appDir == null) throw new IOException("Application bundle not found: " + PathManager.getHomePath());
+    var command = prepareCommand(beforeRestart);
+
+    var runProcessCommand = new ArrayList<String>();
+    runProcessCommand.add("/usr/bin/open");
+
+    /* JetBrains Client process may be started from the same bundle as the full IDE, so we need to force 'open' command to run a new
+       process from that bundle instead of focusing on the existing application if it's running */
+    runProcessCommand.add("-n");
+
+    runProcessCommand.add(appDir.toString());
+    if (!args.isEmpty()) {
+      runProcessCommand.add("--args");
+      runProcessCommand.addAll(args);
+    }
+
+    command.add(String.valueOf(runProcessCommand.size()));
+    command.addAll(runProcessCommand);
     runRestarter(command);
   }
 
