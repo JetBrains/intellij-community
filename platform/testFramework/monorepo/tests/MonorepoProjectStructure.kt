725d4bdbefa1b549d7677b4bf816d223c7b961b6
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2025-07-30 18:17:05 +0200

    [project structure tests] make IntelliJProjectPackageNamesTest work when module output is packed in a JAR file (IJI-1676)
    This restores checks in the IntelliJProjectPackageNamesTest on TeamCity. Before they were skipped because 'findPackages' returns empty list of the output directory doesn't exist.
    
    GitOrigin-RevId: fc7bb2763d3b988da9d10e75754563b825c33e5d

--- platform/testFramework/monorepo/tests/MonorepoProjectStructure.kt
+++ platform/testFramework/monorepo/tests/MonorepoProjectStructure.kt
@@ -2,16 +2,19 @@
 
 package com.intellij.platform.testFramework.monorepo
 
+import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.project.IntelliJProjectConfiguration
 import com.intellij.testFramework.PlatformTestUtil
 import org.jetbrains.jps.model.JpsProject
 import org.jetbrains.jps.model.java.JavaSourceRootType
+import org.jetbrains.jps.model.java.JpsJavaExtensionService
 import org.jetbrains.jps.model.library.JpsLibrary
 import org.jetbrains.jps.model.library.JpsOrderRootType
 import org.jetbrains.jps.model.module.JpsModule
 import org.jetbrains.jps.model.serialization.JpsModelSerializationDataService
 import java.io.File
+import java.nio.file.FileSystems
 import java.nio.file.Path
 import kotlin.io.path.Path
 
@@ -29,3 +32,22 @@ object MonorepoProjectStructure {
 }
 
 fun JpsModule.hasProductionSources(): Boolean = getSourceRoots(JavaSourceRootType.SOURCE).iterator().hasNext()
+
+/**
+ * Calls [processor] for the path containing the production output of [module].
+ * Works both when module output is located in a directory and when it's packed in a JAR.
+ */
+fun <T> processModuleProductionOutput(module: JpsModule, processor: (Path) -> T): T {
+  val archivedCompiledClassesMapping = PathManager.getArchivedCompiledClassesMapping()
+  val outputJarPath = archivedCompiledClassesMapping?.get("production/${module.name}")
+  if (outputJarPath == null) {
+    val outputDirectoryPath = JpsJavaExtensionService.getInstance().getOutputDirectoryPath(module, false)
+                              ?: error("Output directory is not specified for '${module.name}'")
+    return processor(outputDirectoryPath)
+  }
+  else {
+    return FileSystems.newFileSystem(Path(outputJarPath)).use { 
+      processor(it.rootDirectories.single())
+    }
+  }
+}
