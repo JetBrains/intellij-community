314bc94f0fc146ca3889317b915001a65e92bedd
Maria Sokolova <maria.sokolova@jetbrains.com>
2025-07-31 18:36:31 +0200

    [debugger] Translated the old InterruptThreadAction to Kotlin
    This action uses the old ThreadsPanel based on the Java DebuggerTree. It doesn't work in split mode.
    It will be used when useFeProxy == false
    
    GitOrigin-RevId: 6416c58bb693d6e086e6970c8f75c1366aa082e1

--- java/debugger/impl/src/com/intellij/debugger/actions/InterruptThreadAction.kt
+++ java/debugger/impl/src/com/intellij/debugger/actions/InterruptThreadAction.kt
@@ -1,104 +1,90 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.debugger.actions;
+package com.intellij.debugger.actions
 
-import com.intellij.debugger.JavaDebuggerBundle;
-import com.intellij.debugger.engine.DebugProcessImpl;
-import com.intellij.debugger.engine.events.DebuggerCommandImpl;
-import com.intellij.debugger.impl.DebuggerContextImpl;
-import com.intellij.debugger.jdi.ThreadReferenceProxyImpl;
-import com.intellij.debugger.ui.impl.watch.DebuggerTreeNodeImpl;
-import com.intellij.debugger.ui.impl.watch.NodeDescriptorImpl;
-import com.intellij.debugger.ui.impl.watch.ThreadDescriptorImpl;
-import com.intellij.openapi.actionSystem.ActionUpdateThread;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.Presentation;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.MessageType;
-import com.intellij.xdebugger.impl.XDebuggerManagerImpl;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.debugger.JavaDebuggerBundle
+import com.intellij.debugger.engine.DebugProcessImpl
+import com.intellij.debugger.engine.DebuggerManagerThreadImpl
+import com.intellij.debugger.engine.events.DebuggerCommandImpl
+import com.intellij.debugger.engine.executeOnDMT
+import com.intellij.debugger.jdi.ThreadReferenceProxyImpl
+import com.intellij.debugger.ui.impl.watch.ThreadDescriptorImpl
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ui.MessageType
+import com.intellij.xdebugger.impl.XDebuggerManagerImpl
+import java.util.*
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-final class InterruptThreadAction extends DebuggerAction {
-  @Override
-  public void actionPerformed(final @NotNull AnActionEvent e) {
-    final DebuggerTreeNodeImpl[] nodes = getSelectedNodes(e.getDataContext());
+internal class InterruptThreadAction : DebuggerAction() {
+  override fun actionPerformed(e: AnActionEvent) {
+    val nodes = getSelectedNodes(e.dataContext)
     if (nodes == null) {
-      return;
+      return
     }
 
-    final List<ThreadReferenceProxyImpl> threadsToInterrupt = new ArrayList<>();
-    for (final DebuggerTreeNodeImpl debuggerTreeNode : nodes) {
-      final NodeDescriptorImpl descriptor = debuggerTreeNode.getDescriptor();
-      if (descriptor instanceof ThreadDescriptorImpl) {
-        threadsToInterrupt.add(((ThreadDescriptorImpl)descriptor).getThreadReference());
-      }
+    val threadsToInterrupt = nodes.mapNotNull {
+      val descriptor = it.descriptor
+      if (descriptor is ThreadDescriptorImpl) descriptor.threadReference else null
     }
 
-    if (!threadsToInterrupt.isEmpty()) {
-      final DebuggerContextImpl debuggerContext = getDebuggerContext(e.getDataContext());
-      final DebugProcessImpl debugProcess = debuggerContext.getDebugProcess();
-      if (debugProcess != null) {
-        Objects.requireNonNull(debuggerContext.getManagerThread()).schedule(new DebuggerCommandImpl() {
-          @Override
-          protected void action() {
-            boolean unsupported = false;
-            for (ThreadReferenceProxyImpl thread : threadsToInterrupt) {
-              try {
-                thread.getThreadReference().interrupt();
-              }
-              catch (UnsupportedOperationException ignored) {
-                unsupported = true;
-              }
-            }
-            if (unsupported) {
-              final Project project = debugProcess.getProject();
-              XDebuggerManagerImpl.getNotificationGroup()
-                .createNotification(JavaDebuggerBundle.message("thread.operation.interrupt.is.not.supported.by.vm"), MessageType.INFO).notify(project);
-            }
-          }
-        });
-      }
+    if (threadsToInterrupt.isEmpty()) return
+    val debuggerContext = getDebuggerContext(e.dataContext)
+    val debugProcess = debuggerContext.debugProcess ?: return
+    val managerThread = debuggerContext.getManagerThread() ?: return
+
+    for (thread in threadsToInterrupt) {
+      interruptThread(thread, debugProcess, managerThread)
     }
   }
 
-  @Override
-  public void update(@NotNull AnActionEvent e) {
-    final DebuggerTreeNodeImpl[] selectedNodes = getSelectedNodes(e.getDataContext());
+  override fun update(e: AnActionEvent) {
+    val selectedNodes = getSelectedNodes(e.dataContext)
 
-    boolean visible = false;
-    boolean enabled = false;
+    var visible = false
+    var enabled = false
 
-    if (selectedNodes != null && selectedNodes.length > 0) {
-      visible = true;
-      enabled = true;
-      for (DebuggerTreeNodeImpl selectedNode : selectedNodes) {
-        final NodeDescriptorImpl threadDescriptor = selectedNode.getDescriptor();
-        if (!(threadDescriptor instanceof ThreadDescriptorImpl)) {
-          visible = false;
-          break;
+    if (selectedNodes != null && selectedNodes.size > 0) {
+      visible = true
+      enabled = true
+      for (selectedNode in selectedNodes) {
+        val threadDescriptor = selectedNode.descriptor
+        if (threadDescriptor !is ThreadDescriptorImpl) {
+          visible = false
+          break
         }
       }
 
       if (visible) {
-        for (DebuggerTreeNodeImpl selectedNode : selectedNodes) {
-          final ThreadDescriptorImpl threadDescriptor = (ThreadDescriptorImpl)selectedNode.getDescriptor();
-          if (threadDescriptor.isFrozen() || threadDescriptor.isSuspended()) {
-            enabled = false;
-            break;
+        for (selectedNode in selectedNodes) {
+          val threadDescriptor = selectedNode.descriptor as ThreadDescriptorImpl
+          if (threadDescriptor.isFrozen || threadDescriptor.isSuspended) {
+            enabled = false
+            break
           }
         }
       }
     }
-    final Presentation presentation = e.getPresentation();
-    presentation.setText(JavaDebuggerBundle.messagePointer("action.interrupt.thread.text"));
-    presentation.setEnabledAndVisible(visible && enabled);
+    val presentation = e.presentation
+    presentation.setText(JavaDebuggerBundle.messagePointer("action.interrupt.thread.text"))
+    presentation.setEnabledAndVisible(visible && enabled)
   }
 
-  @Override
-  public @NotNull ActionUpdateThread getActionUpdateThread() {
-    return ActionUpdateThread.EDT;
+  override fun getActionUpdateThread(): ActionUpdateThread {
+    return ActionUpdateThread.EDT
+  }
+
+  companion object {
+    fun interruptThread(thread: ThreadReferenceProxyImpl, debugProcess: DebugProcessImpl, managerThread: DebuggerManagerThreadImpl) {
+      executeOnDMT(managerThread) {
+        try {
+          thread.getThreadReference().interrupt()
+        }
+        catch (_: UnsupportedOperationException) {
+          val project = debugProcess.project
+          XDebuggerManagerImpl.getNotificationGroup()
+            .createNotification(JavaDebuggerBundle.message("thread.operation.interrupt.is.not.supported.by.vm"),
+                                MessageType.INFO).notify(project)
+        }
+      }
+    }
   }
 }
