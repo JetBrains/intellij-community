589b225420fc2389f315e987969797c15ee482b5
Mikhail Mazurkevich <Mikhail.Mazurkevich@jetbrains.com>
2025-07-31 15:48:42 +0400

    [ai assistant] LLM-18802 Fix for "Plugin com.intellij.ml.llm is not allowed to provide OpenTelemetryExporterProvider"
    GitOrigin-RevId: 3fc378a857916e98afc1007eb58907fd2caab0c0

--- platform/diagnostic/telemetry-impl/src/pluginDescriptor-ext.kt
+++ platform/diagnostic/telemetry-impl/src/pluginDescriptor-ext.kt
@@ -4,7 +4,8 @@ package com.intellij.platform.diagnostic.telemetry.impl
 import com.intellij.openapi.extensions.PluginDescriptor
 
 internal val pluginsExplicitlyAllowedToExportOT = setOf(
-  "org.jetbrains.toolbox-enterprise-client"
+  "org.jetbrains.toolbox-enterprise-client",
+  "com.intellij.ml.llm"
 )
 
 internal fun PluginDescriptor.isAllowedToExportOT(): Boolean {
