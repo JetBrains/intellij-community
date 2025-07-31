05c317089e1b6d4b8e892bdbe2a9e89f39dee70d
Roman Golyshev <roman.golyshev@jetbrains.com>
2025-07-31 12:53:07 +0200

    KTIJ-31949 [kotlin] Migrate `DeferredResultUnusedInspection` to Analysis API
    GitOrigin-RevId: 0def28f8205c54c29cd4a3fdfec05856cb3ce9f3

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/coroutines/DeferredResultUnusedInspection.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/coroutines/DeferredResultUnusedInspection.kt
@@ -2,40 +2,35 @@
 
 package org.jetbrains.kotlin.idea.inspections.coroutines
 
+import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.codeInspection.options.OptPane
 import com.intellij.codeInspection.options.OptPane.checkbox
 import com.intellij.codeInspection.options.OptPane.pane
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiElementVisitor
+import org.jetbrains.kotlin.analysis.api.KaSession
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.resolution.KaFunctionCall
+import org.jetbrains.kotlin.analysis.api.resolution.singleFunctionCallOrNull
+import org.jetbrains.kotlin.analysis.api.resolution.symbol
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.caches.resolve.safeAnalyzeNonSourceRootCode
 import org.jetbrains.kotlin.idea.codeinsight.api.classic.inspections.AbstractKotlinInspection
-import org.jetbrains.kotlin.idea.imports.importableFqName
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.bindingContextUtil.isUsedAsExpression
-import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall
-import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
-import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
-import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 
 class DeferredResultUnusedInspection(@JvmField var standardOnly: Boolean = false) : AbstractKotlinInspection() {
     private fun isExpressionApplicable(expression: KtExpression): Boolean =
         expression is KtCallExpression && (!standardOnly || expression.calleeExpression?.text in shortNames)
 
-    private fun shouldReportCall(resolvedCall: ResolvedCall<*>): Boolean {
-        val resultingDescriptor = resolvedCall.resultingDescriptor
-        val fqName = resultingDescriptor.fqNameOrNull()
-        if (fqName in fqNamesThatShouldNotBeReported) return false
+    private fun KaSession.shouldReportCall(call: KaFunctionCall<*>): Boolean {
+        val callableId = call.partiallyAppliedSymbol.symbol.callableId?.asSingleFqName()
+        if (callableId in fqNamesThatShouldNotBeReported) return false
 
         return if (standardOnly) {
-            fqName in fqNamesAll
+            callableId in fqNamesAll
         } else {
-            val returnTypeClassifier = resultingDescriptor.returnType?.constructor?.declarationDescriptor
-            val importableFqName = returnTypeClassifier?.importableFqName
-            importableFqName == deferred || importableFqName == deferredExperimental
+            val returnTypeClassId = call.partiallyAppliedSymbol.signature.returnType.expandedSymbol?.classId?.asSingleFqName()
+            returnTypeClassId == deferred || returnTypeClassId == deferredExperimental
         }
     }
 
@@ -52,18 +47,22 @@ class DeferredResultUnusedInspection(@JvmField var standardOnly: Boolean = false
             current = parent
             parent = parent.parent
         }
-        // Then check by call
-        val context = expression.safeAnalyzeNonSourceRootCode(BodyResolveMode.PARTIAL_WITH_CFA)
-        if (context == BindingContext.EMPTY || expression.isUsedAsExpression(context)) return false
-        val resolvedCall = expression.getResolvedCall(context) ?: return false
-        return shouldReportCall(resolvedCall)
+        // Then check by call using Analysis API
+        analyze(expression) {
+            if (expression.isUsedAsExpression) return false
+            val call = expression.resolveToCall()?.singleFunctionCallOrNull() ?: return false
+            return shouldReportCall(call)
+        }
     }
 
-    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor =
-        callExpressionVisitor(fun(expression) {
-            if (!check(expression)) return
-            holder.registerProblem(expression.calleeExpression ?: expression, KotlinBundle.message("deferred.result.is.never.used"))
-        })
+    override fun buildVisitor(
+        holder: ProblemsHolder,
+        isOnTheFly: Boolean,
+        session: LocalInspectionToolSession
+    ): KtVisitorVoid = callExpressionVisitor(fun(expression) {
+        if (!check(expression)) return
+        holder.registerProblem(expression.calleeExpression ?: expression, KotlinBundle.message("deferred.result.is.never.used"))
+    })
 
   override fun getOptionsPane(): OptPane = pane(
     checkbox("standardOnly", KotlinBundle.message("reports.only.function.calls.from.kotlinx.coroutines")))
@@ -81,9 +80,9 @@ private val fqNamesExperimental: Set<FqName> = shortNames.mapTo(mutableSetOf()) 
 
 private val fqNamesAll = fqNames + fqNamesExperimental
 
-private val deferred = FqName("$COROUTINE_PACKAGE.Deferred")
+private val deferred: FqName = FqName("$COROUTINE_PACKAGE.Deferred")
 
-private val deferredExperimental = FqName("$COROUTINE_EXPERIMENTAL_PACKAGE.Deferred")
+private val deferredExperimental: FqName = FqName("$COROUTINE_EXPERIMENTAL_PACKAGE.Deferred")
 
 private val fqNamesThatShouldNotBeReported =
     listOf("kotlin.test.assertNotNull", "kotlin.requireNotNull", "kotlin.checkNotNull").map { FqName(it) }
