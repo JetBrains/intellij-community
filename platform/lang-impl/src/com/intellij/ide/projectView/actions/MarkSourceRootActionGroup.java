e0e46b52f63d3ab14a9d5cc3a7b528d40deaee64
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2025-08-03 09:14:16 +0200

    [api] IJPL-797 Tighten the access level for MarkSourceRootActionGroup
    GitOrigin-RevId: 346cf21ca62160ad6cf349acc1352c4ecea02c71

--- platform/lang-impl/src/com/intellij/ide/projectView/actions/MarkSourceRootActionGroup.java
+++ platform/lang-impl/src/com/intellij/ide/projectView/actions/MarkSourceRootActionGroup.java
@@ -5,7 +5,6 @@ import com.intellij.openapi.actionSystem.ActionGroup;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.jps.model.java.JavaResourceRootType;
@@ -16,8 +15,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@ApiStatus.Internal
-public final class MarkSourceRootActionGroup extends ActionGroup {
+final class MarkSourceRootActionGroup extends ActionGroup {
   @Override
   public AnAction @NotNull [] getChildren(@Nullable AnActionEvent e) {
     //todo obtain compatible root types by module
