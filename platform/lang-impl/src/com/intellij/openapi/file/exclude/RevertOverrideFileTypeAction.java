71218a4fd8c05b2be38341329ddeb821fd77a9e1
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2025-08-03 10:00:08 +0200

    [spelling] Fix spelling issues in ActionsBundle.properties
    GitOrigin-RevId: f8507c9bc4dd48ad36e983eae5374ac3fe605c6c

--- platform/lang-impl/src/com/intellij/openapi/file/exclude/RevertOverrideFileTypeAction.java
+++ platform/lang-impl/src/com/intellij/openapi/file/exclude/RevertOverrideFileTypeAction.java
@@ -10,15 +10,15 @@ import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
 /** removes destruction caused by {@link OverrideFileTypeAction} and restores the original file type */
-final class ReverteOverrideFileTypeAction extends DumbAwareAction {
+final class RevertOverrideFileTypeAction extends DumbAwareAction {
   @Override
   public void update(@NotNull AnActionEvent e) {
     VirtualFile[] files = OverrideFileTypeAction.getContextFiles(e, file -> OverrideFileTypeManager.getInstance().getFileValue(file) != null);
     Presentation presentation = e.getPresentation();
     boolean enabled = files.length != 0;
     presentation.setDescription(enabled
-                                ? ActionsBundle.message("action.ReverteOverrideFileTypeAction.verbose.description", files[0].getName(), files.length - 1)
-                                : ActionsBundle.message("action.ReverteOverrideFileTypeAction.description"));
+                                ? ActionsBundle.message("action.RevertOverrideFileTypeAction.verbose.description", files[0].getName(), files.length - 1)
+                                : ActionsBundle.message("action.RevertOverrideFileTypeAction.description"));
     presentation.setEnabledAndVisible(enabled);
   }
 
