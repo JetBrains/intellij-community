fbae2b5fed19337e084d68a3aa59a55357aaba9e
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-08-04 17:18:30 +0200

    [threading, vfs] IJPL-188470: Convert RefreshQueue to Kotlin
    GitOrigin-RevId: df27955147caede0f026f6ae00aef1080aab4c8c

--- platform/analysis-api/src/com/intellij/openapi/vfs/newvfs/RefreshQueue.kt
+++ platform/analysis-api/src/com/intellij/openapi/vfs/newvfs/RefreshQueue.kt
@@ -1,56 +1,44 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.vfs.newvfs;
+package com.intellij.openapi.vfs.newvfs
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ModalityState;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent
+import org.jetbrains.annotations.ApiStatus
 
-import java.util.Collection;
-import java.util.List;
-
-public abstract class RefreshQueue {
-  public static RefreshQueue getInstance() {
-    return ApplicationManager.getApplication().getService(RefreshQueue.class);
+abstract class RefreshQueue {
+  fun createSession(async: Boolean, recursive: Boolean, finishRunnable: Runnable?): RefreshSession {
+    return createSession(async, recursive, finishRunnable, ModalityState.defaultModalityState())
   }
 
-  public final @NotNull RefreshSession createSession(boolean async, boolean recursive, @Nullable Runnable finishRunnable) {
-    return createSession(async, recursive, finishRunnable, ModalityState.defaultModalityState());
+  abstract fun createSession(async: Boolean, recursive: Boolean, finishRunnable: Runnable?, state: ModalityState): RefreshSession
+
+  fun refresh(async: Boolean, recursive: Boolean, finishRunnable: Runnable?, vararg files: VirtualFile) {
+    refresh(async, recursive, finishRunnable, ModalityState.defaultModalityState(), *files)
   }
 
-  public abstract @NotNull RefreshSession createSession(boolean async, boolean recursive, @Nullable Runnable finishRunnable, @NotNull ModalityState state);
-
-  public final void refresh(boolean async, boolean recursive, @Nullable Runnable finishRunnable, @NotNull VirtualFile @NotNull ... files) {
-    refresh(async, recursive, finishRunnable, ModalityState.defaultModalityState(), files);
+  fun refresh(async: Boolean, recursive: Boolean, finishRunnable: Runnable?, files: Collection<out VirtualFile>) {
+    refresh(async, recursive, finishRunnable, ModalityState.defaultModalityState(), files)
   }
 
-  public final void refresh(boolean async, boolean recursive, @Nullable Runnable finishRunnable, @NotNull Collection<? extends @NotNull VirtualFile> files) {
-    refresh(async, recursive, finishRunnable, ModalityState.defaultModalityState(), files);
+  fun refresh(async: Boolean, recursive: Boolean, finishRunnable: Runnable?, state: ModalityState, vararg files: VirtualFile) {
+    val session = createSession(async, recursive, finishRunnable, state)
+    session.addAllFiles(*files)
+    session.launch()
   }
 
-  public final void refresh(boolean async,
-                            boolean recursive,
-                            @Nullable Runnable finishRunnable,
-                            @NotNull ModalityState state,
-                            @NotNull VirtualFile @NotNull ... files) {
-    RefreshSession session = createSession(async, recursive, finishRunnable, state);
-    session.addAllFiles(files);
-    session.launch();
-  }
-
-  public final void refresh(boolean async,
-                            boolean recursive,
-                            @Nullable Runnable finishRunnable,
-                            @NotNull ModalityState state,
-                            @NotNull Collection<? extends @NotNull VirtualFile> files) {
-    RefreshSession session = createSession(async, recursive, finishRunnable, state);
-    session.addAllFiles(files);
-    session.launch();
+  fun refresh(async: Boolean, recursive: Boolean, finishRunnable: Runnable?, state: ModalityState, files: Collection<out VirtualFile>) {
+    val session = createSession(async, recursive, finishRunnable, state)
+    session.addAllFiles(files)
+    session.launch()
   }
 
   @ApiStatus.Internal
-  public abstract void processEvents(boolean async, @NotNull List<? extends @NotNull VFileEvent> events);
+  abstract fun processEvents(async: Boolean, events: List<VFileEvent>)
+
+  companion object {
+    @JvmStatic
+    fun getInstance(): RefreshQueue = ApplicationManager.getApplication().getService(RefreshQueue::class.java)
+  }
 }
