c30401813cf58f6475a834f7f13d78b86e2eed58
Anna Kozlova <anna.kozlova@jetbrains.com>
2025-08-04 15:17:05 +0200

    [kotlin] k2: enable tests on psi based test frameworks
    GitOrigin-RevId: 61bc2e4acfe50be1f9ae16e91564b3471ba6a325

--- plugins/kotlin/util/test-generator-fir/test/org/jetbrains/kotlin/fir/testGenerator/codeinsight/GenerateK2LineMarkerTests.kt
+++ plugins/kotlin/util/test-generator-fir/test/org/jetbrains/kotlin/fir/testGenerator/codeinsight/GenerateK2LineMarkerTests.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.fir.testGenerator.codeinsight
 
+import org.jetbrains.kotlin.idea.k2.codeInsight.lineMarkers.test.AbstractKotlinPsiBasedTestFrameworkK2Test
 import org.jetbrains.kotlin.idea.k2.codeInsight.lineMarkers.test.AbstractLineMarkersK2Test
 import org.jetbrains.kotlin.testGenerator.model.*
 import org.jetbrains.kotlin.testGenerator.model.GroupCategory.*
@@ -16,6 +17,27 @@ internal fun MutableTWorkspace.generateK2LineMarkerTests() {
             model("../../../idea/tests/testData/codeInsight/lineMarker/runMarkers", pattern = Patterns.KT_WITHOUT_DOTS)
             model("../../../idea/tests/testData/codeInsight/lineMarker/methodSeparators", pattern = Patterns.KT_WITHOUT_DOTS)
 
+        }
+
+        testClass<AbstractKotlinPsiBasedTestFrameworkK2Test> {
+            model(
+                "../../../idea/tests/testData/codeInsight/lineMarker/runMarkers",
+                pattern = Patterns.forRegex("^((jUnit|test)\\w*)\\.kt$"),
+                testMethodName = "doPsiBasedTest",
+                testClassName = "WithLightTestFramework"
+            )
+            model(
+                "../../../idea/tests/testData/codeInsight/lineMarker/runMarkers",
+                pattern = Patterns.forRegex("^((jUnit|test)\\w*)\\.kt$"),
+                testMethodName = "doPureTest",
+                testClassName = "WithoutLightTestFramework"
+            )
+            model(
+                "../../../idea/tests/testData/codeInsight/lineMarker/runMarkers",
+                pattern = Patterns.forRegex("^((jUnit|test)\\w*)\\.kt$"),
+                testMethodName = "doTestWithGradleConfiguration",
+                testClassName = "WithGradleConfiguration"
+            )
         }
     }
 }
