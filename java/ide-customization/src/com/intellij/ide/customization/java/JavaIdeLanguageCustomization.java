e1fa31e074481d60bf615aa113abd200fe2fe7e2
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2025-08-04 14:47:50 +0200

    [java] IJPL-35276 Move JavaIdeLanguageCustomization to intellij.java.ide.customization
    GitOrigin-RevId: df7050cee69caf31a74454d974740e7d8dd293e2

--- java/ide-customization/src/com/intellij/ide/customization/java/JavaIdeLanguageCustomization.java
+++ java/ide-customization/src/com/intellij/ide/customization/java/JavaIdeLanguageCustomization.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ide.java;
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide.customization.java;
 
 import com.intellij.lang.IdeLanguageCustomization;
 import com.intellij.lang.Language;
@@ -10,7 +10,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-public final class JavaIdeLanguageCustomization extends IdeLanguageCustomization {
+final class JavaIdeLanguageCustomization extends IdeLanguageCustomization {
   @Override
   public @NotNull List<Language> getPrimaryIdeLanguages() {
     return ContainerUtil.addAllNotNull(
--- java/ide-customization/src/com/intellij/ide/customization/java/training/JavaOnboardingTourPromoter.kt
+++ java/ide-customization/src/com/intellij/ide/customization/java/training/JavaOnboardingTourPromoter.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ide.java.training
+package com.intellij.ide.customization.java.training
 
 import com.intellij.java.ift.JavaLessonsBundle
 import com.intellij.java.ift.javaLanguageId
