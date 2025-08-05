539d6b79d53f2054f691e335a1d1a9f2991233af
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2025-08-05 12:02:13 +0200

    [junit k2] support k2 tests
    GitOrigin-RevId: 5ee9a1c292c138046d465cd9b61dfa32ab69add8

--- plugins/junit/kotlin-tests-k2/test/com/intellij/execution/junit/kotlin/codeInspection/deadCode/K2JUnit5ImplicitUsageProviderTest.kt
+++ plugins/junit/kotlin-tests-k2/test/com/intellij/execution/junit/kotlin/codeInspection/deadCode/K2JUnit5ImplicitUsageProviderTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.junit.kotlin.codeInspection.deadCode
 
 import com.intellij.codeInspection.InspectionProfileEntry
