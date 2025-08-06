6a01073573e734f6442753b057fff8e5317f7a1b
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2025-08-06 11:23:55 +0300

    [Groovy] IDEA-376878 Error messages for usage of implication operator in old versions of Groovy
    GitOrigin-RevId: 80a2e34b981a1fc7a8213ffb5382bcb603a67d19

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/annotator/GroovyAnnotatorPre50.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/annotator/GroovyAnnotatorPre50.kt
@@ -3,15 +3,37 @@ package org.jetbrains.plugins.groovy.annotator
 
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.psi.PsiElement
+import org.jetbrains.annotations.PropertyKey
 import org.jetbrains.plugins.groovy.GroovyBundle
+import org.jetbrains.plugins.groovy.lang.lexer.GroovyTokenTypes.mIMPL
 import org.jetbrains.plugins.groovy.lang.psi.GroovyElementVisitor
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrPatternVariable
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrBinaryExpression
 
 class GroovyAnnotatorPre50(val holder : AnnotationHolder) : GroovyElementVisitor() {
   override fun visitPatternVariable(variable: GrPatternVariable) {
+    error(variable, Groovy5Features.PATTERN_MATCHING)
+  }
+
+  override fun visitBinaryExpression(expression: GrBinaryExpression) {
+    if (expression.operator == mIMPL) {
+      error(expression.operationToken, Groovy5Features.LOGICAL_IMPLICATION)
+    }
+  }
+
+  private fun error(element: PsiElement, feature: Groovy5Features) {
+    val message = GroovyBundle.message(feature.messageKey)
     holder.newAnnotation(
       HighlightSeverity.ERROR,
-      GroovyBundle.message("inspection.message.instanceof.pattern.variable.with.groovy.5.or.later")
-    ).range(variable).create()
+      GroovyBundle.message("inspection.available.with.groovy.5.or.later", message)
+    )
+      .range(element)
+      .create()
+  }
+
+  private enum class Groovy5Features(val messageKey: @PropertyKey(resourceBundle = GroovyBundle.BUNDLE) String) {
+    PATTERN_MATCHING("instanceof.pattern.variable.feature"),
+    LOGICAL_IMPLICATION("logical.implication.feature"),
   }
 }
