1744a07f4174c2bb160ed911facfef516242315d
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2025-08-06 11:16:05 +0200

    [build scripts] modernizing `PatchBuilderBuildTarget` (IJPL-188456 prerequisite)
    GitOrigin-RevId: 3841512ce6a897ed02ff1770dd7c7ef962137efe

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/productInfo/ProductInfoGenerator.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/productInfo/ProductInfoGenerator.kt
@@ -1,4 +1,5 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:JvmName("ProductInfoGenerator")
 package org.jetbrains.intellij.build.impl.productInfo
 
 import com.intellij.platform.buildData.productInfo.CustomCommandLaunchData
