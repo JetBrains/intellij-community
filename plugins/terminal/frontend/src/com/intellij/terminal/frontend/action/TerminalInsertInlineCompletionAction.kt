2cfd5a40ba956020a511d670247d60deaf9f0af5
Sergei Tachenov <Sergei.Tachenov@jetbrains.com>
2025-08-07 17:24:59 +0300

    [terminal] IJPL-200524 Terminal editor key: migrate TerminalInsertInlineCompletionAction
    GitOrigin-RevId: ab76c4089a157efcc8e6d03ed107ae5730ba5207

--- plugins/terminal/frontend/src/com/intellij/terminal/frontend/action/TerminalInsertInlineCompletionAction.kt
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/action/TerminalInsertInlineCompletionAction.kt
@@ -2,30 +2,28 @@ package com.intellij.terminal.frontend.action
 
 import com.intellij.codeInsight.inline.completion.InlineCompletion
 import com.intellij.codeInsight.inline.completion.session.InlineCompletionSession
-import com.intellij.openapi.actionSystem.ActionPromoter
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.WriteAction
-import com.intellij.openapi.editor.Caret
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.actionSystem.EditorAction
-import com.intellij.openapi.editor.actionSystem.EditorActionHandler
+import org.jetbrains.plugins.terminal.block.TerminalPromotedDumbAwareAction
 import org.jetbrains.plugins.terminal.block.util.TerminalDataContextUtils.editor
 import org.jetbrains.plugins.terminal.block.util.TerminalDataContextUtils.isReworkedTerminalEditor
 
-internal class TerminalInsertInlineCompletionAction : EditorAction(InsertInlineCompletionHandler()), ActionPromoter {
-  override fun promote(actions: List<AnAction>, context: DataContext): List<AnAction> =
-    if (context.editor?.isReworkedTerminalEditor == true) listOf(this) else emptyList()
+internal class TerminalInsertInlineCompletionAction : TerminalPromotedDumbAwareAction() {
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 
-  class InsertInlineCompletionHandler : EditorActionHandler() {
-    override fun doExecute(editor: Editor, caret: Caret?, dataContext: DataContext) {
-      WriteAction.run<Throwable> {
-        InlineCompletion.getHandlerOrNull(editor)?.insert()
-      }
-    }
+  override fun update(e: AnActionEvent) {
+    val editor = e.editor
+    e.presentation.isEnabled =
+      editor != null &&
+      editor.isReworkedTerminalEditor &&
+      !InlineCompletionSession.getOrNull(editor)?.context?.textToInsert().isNullOrEmpty()
+  }
 
-    override fun isEnabledForCaret(editor: Editor, caret: Caret, dataContext: DataContext): Boolean {
-      return editor.isReworkedTerminalEditor && !InlineCompletionSession.getOrNull(editor)?.context?.textToInsert().isNullOrEmpty()
+  override fun actionPerformed(e: AnActionEvent) {
+    val editor = e.editor ?: return
+    WriteAction.run<Throwable> {
+      InlineCompletion.getHandlerOrNull(editor)?.insert()
     }
   }
 
