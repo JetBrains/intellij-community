11c0efa4f9bb6ae7214c676f81174eab9e98dc30
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-08-07 16:18:03 +0200

    apicheck: reuse existing graph implementation
    GitOrigin-RevId: f0f1fbd739ed88dd4f527c79689b855cc07b7d4f

--- platform/testFramework/monorepo/tests/api/ModuleGraph.kt
+++ platform/testFramework/monorepo/tests/api/ModuleGraph.kt
@@ -1,19 +1,29 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.testFramework.monorepo.api
 
+import com.intellij.util.graph.DFSTBuilder
+import com.intellij.util.graph.OutboundSemiGraph
 import org.jetbrains.jps.model.module.JpsModule
 
-internal fun Iterable<JpsModule>.prepareModuleList(): List<JpsModule> {
-  val moduleSet = toSet()
+internal fun List<JpsModule>.prepareModuleList(): List<JpsModule> {
+  val graph = DepGraph(this)
 
-  val graph = Graph(this) { module ->
-    module.moduleDependencies { it in moduleSet }
+  val builder = DFSTBuilder(graph)
+  builder.circularDependency?.let { circularDependency ->
+    throw AssertionError("Cycle detected: ${circularDependency}")
   }
 
-  val cycle = graph.findCycle()
-  if (cycle != null) {
-    throw AssertionError("Cycle detected: ${cycle.joinToString(separator = " -> ")}")
-  }
+  return builder.sortedNodes.reversed()
+}
 
-  return graph.sortedTopologically()
+private class DepGraph(
+  val modules: List<JpsModule>,
+) : OutboundSemiGraph<JpsModule> {
+  private val moduleSet = modules.toSet()
+
+  override fun getNodes(): Collection<JpsModule> =
+    modules
+
+  override fun getOut(node: JpsModule): Iterator<JpsModule?> =
+    node.moduleDependencies { it in moduleSet }.iterator()
 }
