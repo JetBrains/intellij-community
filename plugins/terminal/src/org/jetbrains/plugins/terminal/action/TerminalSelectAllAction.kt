fd4148fe8f9c9048058da42e962fb0d6c9f243cc
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-08-07 16:53:09 +0300

    [terminal] IJPL-200524 Terminal editor key: migrate TerminalSelectAllAction
    GitOrigin-RevId: 6948cc4654c6cabf346f1574c22b87bae56c5e7c

--- plugins/terminal/src/org/jetbrains/plugins/terminal/action/TerminalSelectAllAction.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/action/TerminalSelectAllAction.kt
@@ -1,18 +1,26 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.terminal.action
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Caret
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler
 import org.jetbrains.plugins.terminal.block.TerminalFrontendEditorAction
+import org.jetbrains.plugins.terminal.block.TerminalPromotedDumbAwareAction
+import org.jetbrains.plugins.terminal.block.util.TerminalDataContextUtils.editor
 import org.jetbrains.plugins.terminal.block.util.TerminalDataContextUtils.isReworkedTerminalEditor
 
-internal class TerminalSelectAllAction : TerminalFrontendEditorAction(SelectAllHandler())
+internal class TerminalSelectAllAction : TerminalPromotedDumbAwareAction() {
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 
-private class SelectAllHandler : EditorActionHandler() {
-  override fun isEnabledForCaret(editor: Editor, caret: Caret, dataContext: DataContext?): Boolean = editor.isReworkedTerminalEditor
-  override fun doExecute(editor: Editor, caret: Caret?, dataContext: DataContext?) {
+  override fun update(e: AnActionEvent) {
+    e.presentation.isEnabled = e.editor?.isReworkedTerminalEditor == true
+  }
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val editor = e.editor ?: return
     editor.selectionModel.setSelection(0, editor.document.textLength)
   }
 }
