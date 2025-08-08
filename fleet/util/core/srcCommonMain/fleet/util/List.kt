8c520c6d107fde52fab096c32837bc6bbf1b3b58
Alexander Zolotov <zolotov@jetbrains.com>
2025-08-08 11:17:50 +0200

    [fleet, util] drop ListWithContext
    GitOrigin-RevId: d89c14404d75478522eee86c39af8af9afa0ac7e

--- fleet/util/core/srcCommonMain/fleet/util/List.kt
+++ fleet/util/core/srcCommonMain/fleet/util/List.kt
@@ -1,29 +1,6 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package fleet.util
 
-class ListForEach<T>(val items: List<T>) {
-  class Context {
-    internal var _index: Int = 0
-    internal var _isLast: Boolean = false
-    internal var _isFirst: Boolean = false
-    val index get() = _index
-    val isLast get() = _isLast
-    val isFirst get() = _isFirst
-  }
-  fun forEach(body: ListForEach.Context.(T) -> Unit) {
-    val ctx = Context()
-    val lastIndex = items.size - 1
-    items.forEachIndexed { index, t ->
-      ctx.apply {
-        _index = index
-        _isFirst = index == 0
-        _isLast = index == lastIndex
-        body(t)
-      }
-    }
-  }
-}
-
 private val associateNotNullToken = object {}
 
 @Suppress("UNCHECKED_CAST")
@@ -39,9 +16,6 @@ fun <K, V> Iterable<K>.associateWithNotNull(valueSelector: (K) -> V?): Map<K, V>
     .filterKeys { it != associateWithNotNullToken } as Map<K, V>
 
 fun <T> Iterable<Set<T>>.union(): Set<T> = flatMapTo(mutableSetOf()) { it }
-
-fun <T> List<T>.forEachWithContext(body: ListForEach.Context.(T) -> Unit) =
-  ListForEach(this).forEach(body)
 
 //@fleet.kernel.plugins.InternalInPluginModules(where = ["fleet.util.test"])
 fun <T> MutableList<T>.updateGrouping(
