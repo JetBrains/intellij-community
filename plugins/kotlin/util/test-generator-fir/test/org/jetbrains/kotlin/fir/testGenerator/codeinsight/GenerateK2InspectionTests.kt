529ebe3cf99e252d7b3f577a54cc9bb45f8f2c37
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2025-08-09 20:21:42 +0400

    [kotlin] Port `SetterBackingFieldAssignmentInspection` to K2
    #KTIJ-31938 Fixed
    
    GitOrigin-RevId: 0e188e7be3d2809bfc388bd016686ab3bb3f9307

--- plugins/kotlin/util/test-generator-fir/test/org/jetbrains/kotlin/fir/testGenerator/codeinsight/GenerateK2InspectionTests.kt
+++ plugins/kotlin/util/test-generator-fir/test/org/jetbrains/kotlin/fir/testGenerator/codeinsight/GenerateK2InspectionTests.kt
@@ -134,6 +134,7 @@ internal fun MutableTWorkspace.generateK2InspectionTests() {
             model("${idea}/inspectionsLocal/javaMapForEach")
             model("${idea}/inspectionsLocal/functionWithLambdaExpressionBody")
             model("${idea}/inspectionsLocal/replaceUntilWithRangeUntil")
+            model("${idea}/inspectionsLocal/setterBackingFieldAssignment")
 
             // There is no `RemoveExplicitTypeArgumentsIntention` in K2 because `RemoveExplicitTypeArgumentsInspection` is available
             // and the inspection can have the "No highlighting (fix available)" severity.
