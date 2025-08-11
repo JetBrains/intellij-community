a98d438649fb6ff5427f44b5969e6748d4b46b1f
Bas Leijdekkers <bas.leijdekkers@jetbrains.com>
2025-08-11 17:46:57 +0200

    Java: fix PsiClass.getScope() method which was broken by the migration to the new stubs api (IDEA-376896)
    (cherry picked from commit e204142d2cd57a6cdb83285064ec21fe15b8270b)
    
    IJ-CR-172079
    
    GitOrigin-RevId: 21e206ae79b065f4eacdabfdebd37b1faa01305d

--- java/java-tests/testSrc/com/siyeh/ig/performance/ClassInitializerMayBeStaticInspectionTest.java
+++ java/java-tests/testSrc/com/siyeh/ig/performance/ClassInitializerMayBeStaticInspectionTest.java
@@ -1,7 +1,9 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.siyeh.ig.performance;
 
 import com.intellij.codeInspection.InspectionProfileEntry;
+import com.intellij.pom.java.LanguageLevel;
+import com.intellij.testFramework.IdeaTestUtil;
 import com.siyeh.ig.LightJavaInspectionTestCase;
 import org.jetbrains.annotations.Nullable;
 
@@ -13,6 +15,10 @@ public class ClassInitializerMayBeStaticInspectionTest extends LightJavaInspecti
 
   public void testAnonymousClass() {
     doTest();
+  }
+  
+  public void testNoStaticInInnerClass() {
+    IdeaTestUtil.withLevel(getModule(), LanguageLevel.JDK_15, this::doTest);
   }
 
   @Nullable
