a031d7b6f6fe220d7f1e3d947144c22c8216d6da
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2025-08-11 15:22:26 +0200

    IJPL-199990 Many islands: empty editor text is visible through transparent tool window borders
    (cherry picked from commit 296cbf56b1199c82c32f6063f4555a11de93d32e)
    
    IJ-CR-175619
    
    GitOrigin-RevId: 5c320c4b1664789dd76fe644bf3ca4b3dbeb7331

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorEmptyTextPainter.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorEmptyTextPainter.java
@@ -21,16 +21,20 @@ import com.intellij.ui.Gray;
 import com.intellij.ui.JBColor;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.*;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class EditorEmptyTextPainter {
   public void paintEmptyText(@NotNull JComponent splitters, @NotNull Graphics g) {
+    if (JBUI.getInt("Islands", 0) != 1) {
+      doPaintEmptyText(splitters, g);
+    }
+  }
+
+  @ApiStatus.Internal
+  public void doPaintEmptyText(@NotNull JComponent splitters, @NotNull Graphics g) {
     if (!isEnabled()) {
       return;
     }
