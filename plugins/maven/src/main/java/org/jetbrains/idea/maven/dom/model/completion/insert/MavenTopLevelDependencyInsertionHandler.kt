7443aa5cbfec70ede05f5f1891273023359b931c
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2025-08-11 15:20:48 +0200

    [maven] convert MavenTopLevelDependencyInsertionHandler to kotlin
    GitOrigin-RevId: 6fe53141abb10fc5169657530a40d48d6700edde

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/dom/model/completion/insert/MavenTopLevelDependencyInsertionHandler.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/dom/model/completion/insert/MavenTopLevelDependencyInsertionHandler.kt
@@ -1,70 +1,59 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.idea.maven.dom.model.completion.insert;
+package org.jetbrains.idea.maven.dom.model.completion.insert
 
-import com.intellij.codeInsight.completion.InsertHandler;
-import com.intellij.codeInsight.completion.InsertionContext;
-import com.intellij.codeInsight.lookup.LookupElement;
-import com.intellij.codeInsight.template.TemplateManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.psi.xml.XmlFile;
-import com.intellij.psi.xml.XmlText;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.idea.maven.dom.DependencyConflictId;
-import org.jetbrains.idea.maven.dom.MavenDomUtil;
-import org.jetbrains.idea.maven.dom.generate.GenerateDependencyAction;
-import org.jetbrains.idea.maven.dom.generate.GenerateManagedDependencyAction;
-import org.jetbrains.idea.maven.dom.model.MavenDomDependency;
-import org.jetbrains.idea.maven.dom.model.MavenDomProjectModel;
-import org.jetbrains.idea.maven.onlinecompletion.model.MavenRepositoryArtifactInfo;
+import com.intellij.codeInsight.completion.InsertHandler
+import com.intellij.codeInsight.completion.InsertionContext
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.template.TemplateManager
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.xml.XmlFile
+import com.intellij.psi.xml.XmlText
+import org.jetbrains.idea.maven.dom.MavenDomUtil
+import org.jetbrains.idea.maven.dom.generate.GenerateDependencyAction
+import org.jetbrains.idea.maven.dom.generate.GenerateManagedDependencyAction
+import org.jetbrains.idea.maven.dom.model.MavenDomProjectModel
+import org.jetbrains.idea.maven.onlinecompletion.model.MavenRepositoryArtifactInfo
 
-import java.util.Collections;
-import java.util.Map;
-
-import static org.jetbrains.idea.maven.dom.model.completion.insert.MavenDependencyInsertionTrackerKt.logMavenDependencyInsertion;
-
-public class MavenTopLevelDependencyInsertionHandler implements InsertHandler<LookupElement> {
-
-  public static final InsertHandler<LookupElement> INSTANCE = new MavenTopLevelDependencyInsertionHandler();
-
-  @Override
-  public void handleInsert(final @NotNull InsertionContext context, @NotNull LookupElement item) {
-    if (TemplateManager.getInstance(context.getProject()).getActiveTemplate(context.getEditor()) != null) {
-      return; // Don't brake the template.
+internal class MavenTopLevelDependencyInsertionHandler : InsertHandler<LookupElement> {
+  override fun handleInsert(context: InsertionContext, item: LookupElement) {
+    if (TemplateManager.getInstance(context.project).getActiveTemplate(context.editor) != null) {
+      return  // Don't brake the template.
     }
-    Object object = item.getObject();
-    if (!(object instanceof MavenRepositoryArtifactInfo completionItem)) {
-      return;
+    val obj = item.getObject()
+    if (obj !is MavenRepositoryArtifactInfo) {
+      return
     }
-    PsiFile contextFile = context.getFile();
-    if (!(contextFile instanceof XmlFile)) return;
-    Project project = context.getProject();
-    MavenDomProjectModel model = MavenDomUtil.getMavenDomModel(contextFile, MavenDomProjectModel.class);
+    val contextFile = context.file
+    if (contextFile !is XmlFile) return
+    val project = context.project
+    val model = MavenDomUtil.getMavenDomModel(contextFile, MavenDomProjectModel::class.java)
     if (model == null) {
-      return;
+      return
     }
-    final Map<DependencyConflictId, MavenDomDependency>
-      managedDependencies = GenerateManagedDependencyAction.collectManagingDependencies(model);
-    PsiElement element = contextFile.findElementAt(context.getStartOffset());
-    if (!(element instanceof XmlText)) {
-      element = PsiTreeUtil.getParentOfType(element, XmlText.class);
+    val managedDependencies = GenerateManagedDependencyAction.collectManagingDependencies(model)
+    var element = contextFile.findElementAt(context.startOffset)
+    if (element !is XmlText) {
+      element = PsiTreeUtil.getParentOfType(element, XmlText::class.java)
       if (element == null) {
-        return;
+        return
       }
     }
-    PsiDocumentManager.getInstance(project).commitAllDocuments();
-    MavenDomDependency dependency =
-      GenerateDependencyAction.createDependency(model, context.getEditor(), managedDependencies, Collections.singletonList(completionItem));
+    PsiDocumentManager.getInstance(project).commitAllDocuments()
+    val dependency =
+      GenerateDependencyAction.createDependency(model, context.editor, managedDependencies,
+                                                mutableListOf<MavenRepositoryArtifactInfo?>(obj))
 
-    element.delete();
+    element.delete()
     if (dependency != null && dependency.getXmlTag() != null) {
-      context.getEditor().getCaretModel().moveToOffset(dependency.getXmlTag().getTextOffset());
+      context.editor.getCaretModel().moveToOffset(dependency.getXmlTag()!!.getTextOffset())
     }
-    context.commitDocument();
+    context.commitDocument()
 
-    logMavenDependencyInsertion(context, item, completionItem);
+    logMavenDependencyInsertion(context, item, obj)
+  }
+
+  companion object {
+    val INSTANCE: InsertHandler<LookupElement> = MavenTopLevelDependencyInsertionHandler()
   }
 }
