48c8b71ea7ae3988e11d69011ea2b66fd691bd7f
Nikita.Ashihmin <nikita.ashihmin@jetbrains.com>
2025-08-13 01:25:34 +0400

    PY-83323 Jupyter: Create JupyterKernelTaskExecutor and refactor there all standard methods for JupyterTaskExecutor
    GitOrigin-RevId: 2a01013cd6e41e62855dc9ebd65afe412545da37

--- notebooks/visualization/src/com/intellij/notebooks/visualization/NotebookCellLinesUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.intellij.notebooks.visualization
-
-import com.intellij.openapi.editor.Document
-
-fun Document.getContentText(cell: NotebookCellLines.Interval): CharSequence {
-  return cell.getContentText(this)
-}
