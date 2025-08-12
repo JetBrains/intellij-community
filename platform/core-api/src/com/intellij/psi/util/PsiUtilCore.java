69654a4bfcf682aa9fc5077bbed16630d088dd97
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-08-12 11:58:33 +0200

    PsiUtilCore#ensureValid: created PIEAE exception on a separate line
    to simplify stacktrace investigation
    
    GitOrigin-RevId: 3c53ea62ca7f8a693653bcd522bc2dc05c20dd1b

--- platform/core-api/src/com/intellij/psi/util/PsiUtilCore.java
+++ platform/core-api/src/com/intellij/psi/util/PsiUtilCore.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.util;
 
 import com.intellij.diagnostic.PluginException;
@@ -505,7 +505,8 @@ public class PsiUtilCore {
           return;
         }
       }
-      throw PluginException.createByClass(new PsiInvalidElementAccessException(element), element.getClass());
+      PsiInvalidElementAccessException exception = new PsiInvalidElementAccessException(element);
+      throw PluginException.createByClass(exception, element.getClass());
     }
   }
 
