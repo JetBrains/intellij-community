7ffe0c10b557107e65ce67314bc1d9260ae94a35
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2025-08-12 09:37:23 +0200

    [platform] convert intellij.platform.inspect to content module descriptor (IJPL-200936)
    GitOrigin-RevId: 356481e75a9f92d9e658747779d5ff0109868e0a

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/PlatformModules.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/PlatformModules.kt
@@ -58,7 +58,6 @@ private val PLATFORM_IMPLEMENTATION_MODULES = java.util.List.of(
   "intellij.platform.diff.impl",
   "intellij.platform.editor.ex",
   "intellij.platform.externalProcessAuthHelper",
-  "intellij.platform.inspect",
   "intellij.platform.lvcs",
   "intellij.platform.macro",
   "intellij.platform.scriptDebugger.protocolReaderRuntime",
