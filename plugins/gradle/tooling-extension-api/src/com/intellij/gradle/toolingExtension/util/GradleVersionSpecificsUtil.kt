65f88d295d4a5404f1bd0e726e55ea357c5c0abb
Nikita Biriukov <nikita.biriukov@jetbrains.com>
2025-08-12 16:06:59 +0200

    [gradle] move GradleVersionSpecificsUtil to the same package as GradleVersionUtil
    GitOrigin-RevId: 1b39e8d89aaa046947484f5cad01ea66e41d51c3

--- plugins/gradle/tooling-extension-api/src/com/intellij/gradle/toolingExtension/util/GradleVersionSpecificsUtil.kt
+++ plugins/gradle/tooling-extension-api/src/com/intellij/gradle/toolingExtension/util/GradleVersionSpecificsUtil.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.gradle.testFramework.util
+package com.intellij.gradle.toolingExtension.util
 
-import com.intellij.gradle.toolingExtension.util.GradleVersionUtil
 import org.gradle.util.GradleVersion
 
 object GradleVersionSpecificsUtil {
