7dd65ec6b70d8ef3ca8ad1acaef9f15ed472e50a
Sergei Tachenov <Sergei.Tachenov@jetbrains.com>
2025-08-13 10:26:10 +0300

    IJPL-54302 Refactor: extract createDeferredIcon
    Get rid of code duplication before changing it,
    so we don't have to change it twice.
    
    GitOrigin-RevId: 53f729d3649037308e1f63e2a221b3d5ed75ffcc

--- platform/platform-impl/src/com/intellij/ide/RecentProjectIconHelper.kt
+++ platform/platform-impl/src/com/intellij/ide/RecentProjectIconHelper.kt
@@ -153,13 +153,7 @@ class RecentProjectIconHelper {
     if (!RecentProjectsManagerBase.isFileSystemPath(path)) {
       return EmptyIcon.create(iconSize)
     }
-
-    return IconDeferrer.getInstance().defer(
-      EmptyIcon.create(iconSize),
-      LocalProjectIconKey(cacheEpoch.get(), path, isProjectValid, iconSize, name),
-    ) {
-      it.loadIcon()
-    }
+    return createDeferredIcon(LocalProjectIconKey(cacheEpoch.get(), path, isProjectValid, iconSize, name))
   }
 
   fun getNonLocalProjectIcon(
@@ -168,12 +162,7 @@ class RecentProjectIconHelper {
     iconSize: Int = unscaledProjectIconSize(),
     name: String? = null,
   ): Icon {
-    return IconDeferrer.getInstance().defer(
-      EmptyIcon.create(iconSize),
-      NonLocalProjectIconKey(cacheEpoch.get(), id, isProjectValid, iconSize, name),
-    ) {
-      it.loadIcon()
-    }
+    return createDeferredIcon(NonLocalProjectIconKey(cacheEpoch.get(), id, isProjectValid, iconSize, name))
   }
 
 
@@ -181,7 +170,17 @@ class RecentProjectIconHelper {
     ProjectWindowCustomizerService.projectPath(project)?.let { getCustomIconFileInfo(it) } != null
 }
 
+private fun createDeferredIcon(key: DeferredIconKey): Icon {
+  return IconDeferrer.getInstance().defer(
+    EmptyIcon.create(key.iconSize),
+    key,
+  ) {
+    it.loadIcon()
+  }
+}
+
 private sealed class DeferredIconKey {
+  abstract val iconSize: Int
   abstract fun loadIcon(): Icon
 }
 
@@ -189,7 +188,7 @@ private data class LocalProjectIconKey(
   val cacheEpoch: Int,
   val path: @SystemIndependent String,
   val isProjectValid: Boolean,
-  val iconSize: Int,
+  override val iconSize: Int,
   val name: String?,
 ) : DeferredIconKey() {
   override fun loadIcon(): Icon =
@@ -201,7 +200,7 @@ private data class NonLocalProjectIconKey(
   val cacheEpoch: Int,
   val id: String,
   val isProjectValid: Boolean,
-  val iconSize: Int,
+  override val iconSize: Int,
   val name: String?,
 ) : DeferredIconKey() {
   override fun loadIcon(): Icon =
