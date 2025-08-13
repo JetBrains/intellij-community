4de15f251ef5d3e9dd40c3dd8a290add08778b5e
Sebastiano Poggi <sebp@google.com>
2025-08-13 19:18:49 +0200

    [JEWEL-940] Add Jewel build information
    This adds the Jewel API version to the build. The version is updated
    via a new script, and has been included in the release process docs.
    
    closes https://github.com/JetBrains/intellij-community/pull/3179
    
    (cherry picked from commit 4bf7de427012daf3271fc39d9cc7555aede43e97)
    
    
    (cherry picked from commit 7c1dbf002e07e6289d10d65881405f2ed52dd2ce)
    
    IJ-MR-173299
    
    GitOrigin-RevId: 56f3b137f561cd690273132cce8c4a271dd4553d

---
+++ platform/jewel/foundation/src/main/kotlin/org/jetbrains/jewel/foundation/JewelBuild.kt
@@ -0,0 +1,5 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.jewel.foundation
+
+/** Contains information about the Jewel build in use. */
+public object JewelBuild
