76096c5dca8020f990d1183a31ac24115cee4872
Boris Krylov <boris.krylov@jetbrains.com>
2025-08-13 23:48:55 +0300

    [syntax][json] regenerate bazel files and update API usage
    GitOrigin-RevId: bee5af4ebe72d7e40e0dce00fbd7933e7a46e1cf

--- json/src/com/intellij/json/JsonLanguageDefinition.kt
+++ json/src/com/intellij/json/JsonLanguageDefinition.kt
@@ -4,26 +4,31 @@ package com.intellij.json
 import com.intellij.json.syntax.JsonSyntaxElementTypes
 import com.intellij.json.syntax.JsonSyntaxLexer
 import com.intellij.platform.syntax.LanguageSyntaxDefinition
-import com.intellij.platform.syntax.lexer.Lexer
-import com.intellij.platform.syntax.util.runtime.SyntaxGeneratedParserRuntime
 import com.intellij.platform.syntax.SyntaxElementTypeSet
+import com.intellij.platform.syntax.lexer.Lexer
+import com.intellij.platform.syntax.parser.SyntaxTreeBuilder
 import com.intellij.platform.syntax.syntaxElementTypeSetOf
 import com.intellij.platform.syntax.util.runtime.GrammarKitLanguageDefinition
+import com.intellij.platform.syntax.util.runtime.SyntaxGeneratedParserRuntime
 
 class JsonLanguageDefinition : LanguageSyntaxDefinition, GrammarKitLanguageDefinition {
-  override fun getLexer(): Lexer {
-    return JsonSyntaxLexer()
-  }
-
   private val jsonComments = syntaxElementTypeSetOf(JsonSyntaxElementTypes.LINE_COMMENT,
                                                     JsonSyntaxElementTypes.BLOCK_COMMENT)
-
-  override fun getCommentTokens(): SyntaxElementTypeSet = jsonComments
-
   private val jsonPairedBraces = listOf(
     SyntaxGeneratedParserRuntime.BracePair(JsonSyntaxElementTypes.L_CURLY, JsonSyntaxElementTypes.R_CURLY, true),
     SyntaxGeneratedParserRuntime.BracePair(JsonSyntaxElementTypes.L_BRACKET, JsonSyntaxElementTypes.R_BRACKET, true),
   )
 
   override fun getPairedBraces(): Collection<SyntaxGeneratedParserRuntime.BracePair> = jsonPairedBraces
+  
+  override fun parse(builder: SyntaxTreeBuilder) {
+    throw UnsupportedOperationException("Unsupported operation - generated Parser.")
+  }
+
+  override fun createLexer(): Lexer {
+    return JsonSyntaxLexer()
+  }
+
+  override val comments: SyntaxElementTypeSet
+    get() = jsonComments
 }
