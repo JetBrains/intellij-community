f46b6435275205125ee14d7585e445ffff047831
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2025-08-15 00:18:09 +0200

    [terminal] IJPL-201764 disable `terminal.use.EelApi` in 2025.2.1
    Also, provide support for Unix shell paths stored when `terminal.use.EelApi` was enabled.
    
    (cherry picked from commit 6063ac7a335d879020b10bf5480d24ae91155558)
    
    IJ-CR-172541
    
    
    (cherry picked from commit 6884a32b16de1d51dd9f1708323784c9e72541ce)
    
    GitOrigin-RevId: 635a131dadac5590249f473e6eb03d3c14a56e54

--- plugins/terminal/src/org/jetbrains/plugins/terminal/runner/LocalOptionsConfigurer.java
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/runner/LocalOptionsConfigurer.java
@@ -21,12 +21,15 @@ import com.intellij.terminal.ui.TerminalWidget;
 import com.intellij.util.EnvironmentRestorer;
 import com.intellij.util.SystemProperties;
 import com.intellij.util.containers.CollectionFactory;
+import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.system.OS;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.VisibleForTesting;
 import org.jetbrains.plugins.terminal.ShellStartupOptions;
 import org.jetbrains.plugins.terminal.TerminalProjectOptionsProvider;
+import org.jetbrains.plugins.terminal.TerminalStartupKt;
 import org.jetbrains.plugins.terminal.util.TerminalEnvironment;
 
 import java.nio.file.Files;
@@ -46,8 +49,8 @@ public final class LocalOptionsConfigurer {
   private static final Logger LOG = Logger.getInstance(LocalOptionsConfigurer.class);
 
   public static @NotNull ShellStartupOptions configureStartupOptions(@NotNull ShellStartupOptions baseOptions, @NotNull Project project) {
-    List<String> initialCommand = getInitialCommand(baseOptions, project);
     String workingDir = getWorkingDirectory(baseOptions.getWorkingDirectory(), project);
+    List<String> initialCommand = getInitialCommand(baseOptions, project, workingDir);
     var eelDescriptor = findEelDescriptor(workingDir, initialCommand);
     Map<String, String> envs = getTerminalEnvironment(baseOptions.getEnvVariables(), workingDir, project, eelDescriptor);
 
@@ -156,9 +159,39 @@ public final class LocalOptionsConfigurer {
     return envs;
   }
 
-  private static @NotNull List<String> getInitialCommand(@NotNull ShellStartupOptions options, @NotNull Project project) {
-    List<String> shellCommand = options.getShellCommand();
-    return shellCommand != null ? shellCommand : LocalTerminalStartCommandBuilder.convertShellPathToCommand(getShellPath(project));
+  private static @NotNull List<String> getInitialCommand(
+    @NotNull ShellStartupOptions options,
+    @NotNull Project project,
+    @NotNull String workingDir
+  ) {
+    List<String> shellCommand = fixShellCommand(options.getShellCommand());
+    if (shellCommand != null) {
+      return shellCommand;
+    }
+    String shellPath = fixShellPath(getShellPath(project), workingDir);
+    return LocalTerminalStartCommandBuilder.convertShellPathToCommand(shellPath);
+  }
+
+  private static @Nullable List<String> fixShellCommand(@Nullable List<String> shellCommand) {
+    if (OS.CURRENT == OS.Windows && !TerminalStartupKt.shouldUseEelApi() &&
+        isUnixPath(ContainerUtil.getFirstItem(shellCommand))) {
+      return null; // use the default shell path
+    }
+    return shellCommand;
+  }
+
+  private static @NotNull String fixShellPath(@NotNull String shellPath, @NotNull String workingDirectory) {
+    if (OS.CURRENT == OS.Windows && !TerminalStartupKt.shouldUseEelApi() && isUnixPath(shellPath)) {
+      WslPath wslPath = WslPath.parseWindowsUncPath(workingDirectory);
+      if (wslPath != null) {
+        return "wsl.exe --distribution " + wslPath.getDistributionId();
+      }
+    }
+    return shellPath;
+  }
+
+  private static boolean isUnixPath(@Nullable String path) {
+    return path != null && path.startsWith("/") && !path.startsWith("//") /* UNC path */;
   }
 
   private static @NotNull String getShellPath(@NotNull Project project) {
