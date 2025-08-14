4c0f8db317296581c671e9bf529cffae14e8e015
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2025-08-14 15:18:12 +0200

    [test] add comments about isInStressTests in Benchmark
    GitOrigin-RevId: f56d7f812622c4474197c176e099a9962cd1d0ae

--- tools/intellij.tools.ide.metrics.benchmark/src/com/intellij/tools/ide/metrics/benchmark/Benchmark.java
+++ tools/intellij.tools.ide.metrics.benchmark/src/com/intellij/tools/ide/metrics/benchmark/Benchmark.java
@@ -12,6 +12,12 @@ public final class Benchmark {
    * Init a performance test.<br/>
    * E.g: {@code newBenchmark("calculating pi", () -> { CODE_TO_BE_MEASURED_IS_HERE }).start();}
    *
+   * BEWARE: if you start a benchmark in unit-test, ensure {@code ApplicationManagerEx.setInStressTest(true)} is called.
+   * In junit5 tests just use {@link com.intellij.testFramework.junit5.StressTestApplication} instead of plain {@link com.intellij.testFramework.junit5.TestApplication},
+   * in earlier junit3-4 use setUp/tearDown methods.
+   * Without inStressTest=true a lot of debug-checks are executed, most important of them is logLevel=DEBUG -- that makes
+   * benchmark results inadequate.
+   *
    * @see BenchmarkTestInfo#start()
    */
   // to warn about not calling .assertTiming() in the end
@@ -28,6 +34,12 @@ public final class Benchmark {
    * Init a performance test which input may change.<br/>
    * E.g: it depends on the number of files in the project.
    * <p>
+   *
+   * BEWARE: if you start a benchmark in unit-test, ensure {@code ApplicationManagerEx.setInStressTest(true)} is called.
+   * In junit5 tests just use {@link com.intellij.testFramework.junit5.StressTestApplication} instead of plain {@link com.intellij.testFramework.junit5.TestApplication},
+   * in earlier junit3-4 use setUp/tearDown methods.
+   * Without inStressTest=true a lot of debug-checks are executed, most important of them is logLevel=DEBUG -- that makes
+   * benchmark results inadequate.
    *
    * @param expectedInputSize specifies size of the input,
    * @param test              returns actual size of the input. It is supposed that the execution time is lineally proportionally dependent on the input size.
