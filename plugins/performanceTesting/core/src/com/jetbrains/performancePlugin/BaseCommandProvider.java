af5c832df0e01dfc20e114828db2678464232e6d
Alexey Kryuchkov <alexey.kryuchkov@jetbrains.com>
2025-08-14 07:37:34 +0400

    Added reopenFileAfterIdeRestart metric for monolith
    GitOrigin-RevId: eb0ba66d6d470f022fae18327f1243546d1e2321

--- plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/BaseCommandProvider.java
+++ plugins/performanceTesting/core/src/com/jetbrains/performancePlugin/BaseCommandProvider.java
@@ -134,7 +134,8 @@ public final class BaseCommandProvider implements CommandProvider {
       Map.entry(CloseLookupCommand.PREFIX, CloseLookupCommand::new),
       Map.entry(OpenProblemViewPanelCommand.PREFIX, OpenProblemViewPanelCommand::new),
       Map.entry(AssertProblemsViewCountCommand.PREFIX, AssertProblemsViewCountCommand::new),
-      Map.entry(DetectProjectLeaksCommand.PREFIX, DetectProjectLeaksCommand::new)
+      Map.entry(DetectProjectLeaksCommand.PREFIX, DetectProjectLeaksCommand::new),
+      Map.entry(WaitForReOpenedFileCommand.PREFIX, WaitForReOpenedFileCommand::new)
     );
   }
 }
