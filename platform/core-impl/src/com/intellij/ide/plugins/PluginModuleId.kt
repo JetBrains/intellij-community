4b6643e46f0bf8fed56b6e6326c8843c3d021e43
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-08-15 17:59:08 +0200

    [plugins] IJPL-202170 add PluginModuleId getId for java, deprecated conversion methods to and from PluginId
    GitOrigin-RevId: 7f6dcbeffa2cba3354990f615ec464bec1901163

--- platform/core-impl/src/com/intellij/ide/plugins/PluginModuleId.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/PluginModuleId.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.plugins
 
+import com.intellij.ide.plugins.PluginModuleId.Companion.getId
+import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.util.IntellijInternalApi
 import com.intellij.util.containers.Interner
 import org.jetbrains.annotations.ApiStatus
@@ -20,6 +22,15 @@ value class PluginModuleId private constructor(val id: String){
     // PluginModuleId can be either boxed or unboxed, so only interning of value matters
     private val interner = Interner.createWeakInterner<String>()
 
-    operator fun invoke(id: String): PluginModuleId = PluginModuleId(interner.intern(id))
+    fun getId(id: String): PluginModuleId = PluginModuleId(interner.intern(id))
+
+    /** shorthand for [getId] in kotlin */
+    operator fun invoke(id: String): PluginModuleId = getId(id)
+
+    @Deprecated("plugin and module id namespaces are separate")
+    fun PluginId.asPluginModuleId(): PluginModuleId = getId(idString)
+
+    @Deprecated("plugin and module id namespaces are separate")
+    fun PluginModuleId.asPluginId(): PluginId = PluginId.getId(id)
   }
 }
