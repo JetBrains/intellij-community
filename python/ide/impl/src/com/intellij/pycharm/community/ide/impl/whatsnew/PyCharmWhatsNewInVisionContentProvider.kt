846fd422ccd62e009f228ee0b84f44d6436e629b
Ivan Migalev <ivan.migalev@jetbrains.com>
2025-08-15 19:56:44 +0200

    (IJPL-197329) What's New: introduce more, new sane API
    #IJPL-197329 Fixed
    
    GitOrigin-RevId: 6cbcc4baa8c2a440b838d0505dd4b2c5efdaceb5

--- python/ide/impl/src/com/intellij/pycharm/community/ide/impl/whatsnew/PyCharmWhatsNewInVisionContentProvider.kt
+++ python/ide/impl/src/com/intellij/pycharm/community/ide/impl/whatsnew/PyCharmWhatsNewInVisionContentProvider.kt
@@ -1,25 +1,10 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.pycharm.community.ide.impl.whatsnew
 
-import com.intellij.platform.whatsNew.ContentSource
-import com.intellij.platform.whatsNew.ResourceContentSource
 import com.intellij.platform.whatsNew.WhatsNewInVisionContentProvider
 import com.intellij.util.PlatformUtils.isCommunityEdition
 
 class PyCharmWhatsNewInVisionContentProvider : WhatsNewInVisionContentProvider() {
-
-  override fun getResourceNameByPath(path: String): String {
-    return "whatsNew/$path"
-  }
-
-  override fun getResource(resourceName: String): ContentSource {
-    return ResourceContentSource(PyCharmWhatsNewInVisionContentProvider::class.java.classLoader, resourceName)
-  }
-
-  override fun getResource(): ContentSource {
-    // return a vision file for the current version
-    val resourceName = if (isCommunityEdition()) "whatsNew/pycharmCE2025.2.json" else "whatsNew/pycharm2025.2.json"
-    val resourceContentSource = ResourceContentSource(PyCharmWhatsNewInVisionContentProvider::class.java.classLoader, resourceName)
-    return resourceContentSource
-  }
+  override val baseResourcePathInClassLoader: String = "whatsNew"
+  override val visionJsonFileNames: List<String> = listOf(if (isCommunityEdition()) "pycharmCE2025.2.json" else "pycharm2025.2.json")
 }
