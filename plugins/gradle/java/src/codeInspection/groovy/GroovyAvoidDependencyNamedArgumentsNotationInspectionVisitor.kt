d163fb0650eea8eacf9031289a3826abaac22fa1
Kajus Kuncaitis <kajus.kuncaitis@jetbrains.com>
2025-08-18 10:57:18 +0200

    [gradle][IDEA-376861] Correct the problem highlight type
    GitOrigin-RevId: e9ca56ba9b2e76cb6a408bc43cf5c62d5e4eecf7

--- plugins/gradle/java/src/codeInspection/groovy/GroovyAvoidDependencyNamedArgumentsNotationInspectionVisitor.kt
+++ plugins/gradle/java/src/codeInspection/groovy/GroovyAvoidDependencyNamedArgumentsNotationInspectionVisitor.kt
@@ -39,7 +39,7 @@ class GroovyAvoidDependencyNamedArgumentsNotationInspectionVisitor(val holder: P
     holder.registerProblem(
       call.argumentList,
       GradleInspectionBundle.message("inspection.message.avoid.dependency.named.arguments.notation.descriptor"),
-      ProblemHighlightType.WEAK_WARNING,
+      ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
       GradleDependencyNamedArgumentsFix(call)
     )
   }
