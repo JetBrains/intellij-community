777cdb216f82d26c9a4a76d71dcf9502eeb2ff8f
Leonid Shalupov <leonid.shalupov@jetbrains.com>
2025-08-18 19:51:30 +0200

    native certs: disable loading of OS-specific trusted certificates with -Dide.load.os.certificates=false
    a workaround for IJPL-191263 System certificate validation can take a lot of time and freeze IDE on network operations
    
    GitOrigin-RevId: 8dbda1cf0c54cb169a58eb2edf415cde08766b16

--- platform/platform-impl/src/com/intellij/util/net/ssl/OsCertificatesServiceImpl.java
+++ platform/platform-impl/src/com/intellij/util/net/ssl/OsCertificatesServiceImpl.java
@@ -2,15 +2,25 @@
 package com.intellij.util.net.ssl;
 
 import com.intellij.diagnostic.logs.LogLevelConfigurationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.nativecerts.NativeTrustedCertificates;
 
 import java.security.cert.X509Certificate;
 import java.util.Collection;
+import java.util.Collections;
 
 final class OsCertificatesServiceImpl implements OsCertificatesService {
   @Override
   public @NotNull Collection<X509Certificate> getCustomOsSpecificTrustedCertificates() {
+    String property = System.getProperty("ide.load.os.certificates", "true");
+    if (!Boolean.parseBoolean(property)) {
+      LOG.info("System property 'idea.os.certificates' is set to false value ('" +
+               property +
+               "'), skipping loading custom certificates from operating system.");
+      return Collections.emptyList();
+    }
+
     // Ensure debug log categories are available before loading custom certificates.
     // Otherwise, CertificateManager could be requested earlier than DebugLogManager,
     // and we lose debugging info
@@ -19,4 +29,6 @@ final class OsCertificatesServiceImpl implements OsCertificatesService {
     // see https://github.com/JetBrains/jvm-native-trusted-roots
     return NativeTrustedCertificates.getCustomOsSpecificTrustedCertificates();
   }
+
+  private static final Logger LOG = Logger.getInstance(OsCertificatesServiceImpl.class);
 }
