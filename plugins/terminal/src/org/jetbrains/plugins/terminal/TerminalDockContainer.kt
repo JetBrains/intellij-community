67832364ace09c4c746af07abef99cac3488d7f9
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-08-18 14:15:26 +0300

    [terminal] IJPL-201735 Refactor: translate TerminalDockContainer to Kotlin
    GitOrigin-RevId: cafbe22cca2d40fbf284a919eaa1380ba923bd9c

--- plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalDockContainer.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalDockContainer.kt
@@ -1,79 +1,60 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.terminal;
+package org.jetbrains.plugins.terminal
 
-import com.intellij.openapi.wm.ex.ToolWindowEx;
-import com.intellij.ui.awt.RelativePoint;
-import com.intellij.ui.awt.RelativeRectangle;
-import com.intellij.ui.content.Content;
-import com.intellij.ui.content.ContentManager;
-import com.intellij.ui.docking.DockContainer;
-import com.intellij.ui.docking.DockableContent;
-import com.intellij.util.ui.UIUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.terminal.vfs.TerminalSessionVirtualFileImpl;
-
-import javax.swing.*;
-import java.awt.*;
-
-import static org.jetbrains.plugins.terminal.TerminalToolWindowManager.findNearestContentManager;
+import com.intellij.openapi.wm.ex.ToolWindowEx
+import com.intellij.ui.awt.RelativePoint
+import com.intellij.ui.awt.RelativeRectangle
+import com.intellij.ui.docking.DockContainer
+import com.intellij.ui.docking.DockableContent
+import com.intellij.util.ui.UIUtil
+import org.jetbrains.plugins.terminal.vfs.TerminalSessionVirtualFileImpl
+import javax.swing.JComponent
 
 /**
- * Terminal tab can be moved to the editor using {@link org.jetbrains.plugins.terminal.action.MoveTerminalSessionToEditorAction}.
+ * Terminal tab can be moved to the editor using [org.jetbrains.plugins.terminal.action.MoveTerminalSessionToEditorAction].
  * This dock container is responsible for an ability to drop the editor tab with the terminal
  * back to the terminal tool window.
  */
-final class TerminalDockContainer implements DockContainer {
-  private final @NotNull ToolWindowEx myToolWindow;
-
-  TerminalDockContainer(@NotNull ToolWindowEx toolWindow) {
-    myToolWindow = toolWindow;
+internal class TerminalDockContainer(private val toolWindow: ToolWindowEx) : DockContainer {
+  override fun getAcceptArea(): RelativeRectangle {
+    return RelativeRectangle(toolWindow.getDecorator())
   }
 
-  @Override
-  public @NotNull RelativeRectangle getAcceptArea() {
-    return new RelativeRectangle(myToolWindow.getDecorator());
+  override fun getContentResponse(content: DockableContent<*>, point: RelativePoint?): DockContainer.ContentResponse {
+    return if (isTerminalSessionContent(content)) DockContainer.ContentResponse.ACCEPT_MOVE else DockContainer.ContentResponse.DENY
   }
 
-  @Override
-  public @NotNull ContentResponse getContentResponse(@NotNull DockableContent content, RelativePoint point) {
-    return isTerminalSessionContent(content) ? ContentResponse.ACCEPT_MOVE : ContentResponse.DENY;
+  override fun getContainerComponent(): JComponent {
+    return toolWindow.getDecorator()
   }
 
-  @Override
-  public @NotNull JComponent getContainerComponent() {
-    return myToolWindow.getDecorator();
-  }
-
-  @Override
-  public void add(@NotNull DockableContent content, RelativePoint dropTarget) {
-    if (dropTarget == null) return;
+  override fun add(content: DockableContent<*>, dropTarget: RelativePoint?) {
+    if (dropTarget == null) return
     if (isTerminalSessionContent(content)) {
       // Find the right split to create the new tab in
-      Component component = dropTarget.getOriginalComponent();
-      Point point = dropTarget.getOriginalPoint();
-      Component deepestComponent = UIUtil.getDeepestComponentAt(component, point.x, point.y);
-      ContentManager nearestManager = findNearestContentManager(deepestComponent);
+      val component = dropTarget.originalComponent
+      val point = dropTarget.originalPoint
+      val deepestComponent = UIUtil.getDeepestComponentAt(component, point.x, point.y)
+      val nearestManager = TerminalToolWindowManager.findNearestContentManager(deepestComponent)
 
-      TerminalSessionVirtualFileImpl terminalFile = (TerminalSessionVirtualFileImpl)content.getKey();
-      var engine = TerminalEngine.CLASSIC; // Engine doesn't matter here because we will reuse the existing terminal widget.
-      var manager = TerminalToolWindowManager.getInstance(myToolWindow.getProject());
-      Content newContent = manager.createNewTab(nearestManager, terminalFile.getTerminalWidget(), manager.getTerminalRunner(), engine,
-                                                null, null, null, true, true);
-      newContent.setDisplayName(terminalFile.getName());
+      val terminalFile = content.getKey() as TerminalSessionVirtualFileImpl
+      val engine = TerminalEngine.CLASSIC // Engine doesn't matter here because we will reuse the existing terminal widget.
+      val manager = TerminalToolWindowManager.getInstance(toolWindow.getProject())
+      val newContent = manager.createNewTab(nearestManager, terminalFile.terminalWidget, manager.terminalRunner, engine,
+                                            null, null, null, true, true)
+      newContent.setDisplayName(terminalFile.name)
     }
   }
 
-  private static boolean isTerminalSessionContent(@NotNull DockableContent<?> content) {
-    return content.getKey() instanceof TerminalSessionVirtualFileImpl;
+  override fun isEmpty(): Boolean {
+    return false
   }
 
-  @Override
-  public boolean isEmpty() {
-    return false;
+  override fun isDisposeWhenEmpty(): Boolean {
+    return false
   }
 
-  @Override
-  public boolean isDisposeWhenEmpty() {
-    return false;
+  private fun isTerminalSessionContent(content: DockableContent<*>): Boolean {
+    return content.getKey() is TerminalSessionVirtualFileImpl
   }
 }
