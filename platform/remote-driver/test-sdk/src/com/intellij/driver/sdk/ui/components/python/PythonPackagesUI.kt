dfd32d1e598d96218ac714104ec645d9d9a80616
Denis Mashutin <Denis.Mashutin@jetbrains.com>
2025-08-18 20:52:39 +0200

    PCQA-1195 Write tests for remote interpreters
    GitOrigin-RevId: 75fc2477fe8a07a3d9a89e86bc83f6b4d7cff90f

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/python/PythonPackagesUI.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/python/PythonPackagesUI.kt
@@ -2,8 +2,8 @@ package com.intellij.driver.sdk.ui.components.python
 
 import com.intellij.driver.sdk.ui.Finder
 import com.intellij.driver.sdk.ui.components.ComponentData
-import com.intellij.driver.sdk.ui.components.elements.JTableUiComponent
 import com.intellij.driver.sdk.ui.components.UiComponent
+import com.intellij.driver.sdk.ui.components.elements.JTableUiComponent
 import org.intellij.lang.annotations.Language
 
 fun Finder.pythonPackagesToolWindow(@Language("xpath") xpath: String? = null) =
@@ -13,4 +13,13 @@ fun Finder.pythonPackagesToolWindow(@Language("xpath") xpath: String? = null) =
 class PythonPackagesToolWindowUiComponent(data: ComponentData) : UiComponent(data) {
   val packagesTable
     get() = x("//div[@class='PyPackagesTable']", JTableUiComponent::class.java)
+
+  val searchField
+    get() = x("//div[@class='TextFieldWithProcessing']", UiComponent::class.java)
+
+  val installButton
+    get() = x("//div[@class='JBOptionButton']", UiComponent::class.java)
+
+  val uninstallButton
+    get() = x("//div[@accessiblename='Uninstall']", UiComponent::class.java)
 }
