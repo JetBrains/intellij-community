d82d12e3e27512868d062d86a0ac688e0599eb34
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-08-19 14:25:26 +0200

    [threading] IJPL-201090: Add test for running PSI listeners on background
    GitOrigin-RevId: 14a741a4146aff9690607423240b85a77bf94208

---
+++ platform/platform-tests/testSrc/com/intellij/psi/impl/DocumentCommitOnBackgroundTest.kt
@@ -0,0 +1,107 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.psi.impl
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.invokeAndWaitIfNeeded
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.project.DumbService
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiTreeChangeAdapter
+import com.intellij.psi.PsiTreeChangeEvent
+import com.intellij.testFramework.PlatformTestUtil
+import com.intellij.testFramework.junit5.RegistryKey
+import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.testFramework.junit5.TestDisposable
+import com.intellij.testFramework.junit5.fixture.moduleFixture
+import com.intellij.testFramework.junit5.fixture.projectFixture
+import com.intellij.testFramework.junit5.fixture.psiFileFixture
+import com.intellij.testFramework.junit5.fixture.sourceRootFixture
+import com.intellij.util.ui.EDT
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
+
+@TestApplication
+class DocumentCommitOnBackgroundTest {
+  private companion object {
+    val project = projectFixture(openAfterCreation = true)
+    val module = project.moduleFixture()
+    val sourceRoot = module.sourceRootFixture()
+    val file = sourceRoot.psiFileFixture("A.java", "class A {}")
+  }
+
+  class Recorder {
+    val invoked: AtomicInteger = AtomicInteger()
+    val onEdt: AtomicInteger = AtomicInteger()
+
+    fun recordUsage() {
+      invoked.incrementAndGet()
+      if (EDT.isCurrentThreadEdt()) {
+        onEdt.incrementAndGet()
+      }
+    }
+  }
+
+  class RecordingTreeChangeListener(): PsiTreeChangeAdapter() {
+    val recorder = Recorder()
+
+    override fun beforeChildrenChange(event: PsiTreeChangeEvent) {
+      recorder.recordUsage()
+    }
+
+    override fun childrenChanged(event: PsiTreeChangeEvent) {
+      recorder.recordUsage()
+    }
+  }
+
+  class RecordingTreeChangePreprocessor() : PsiTreeChangePreprocessor {
+    val recorder = Recorder()
+
+    override fun treeChanged(event: PsiTreeChangeEventImpl) {
+      recorder.recordUsage()
+    }
+  }
+
+  @Test
+  @RegistryKey("document.async.commit.with.coroutines", "true")
+  fun `psiTreeChangeListener and psiTreeChangePreprocessor can be invoked on background`(@TestDisposable testDisposable: Disposable) {
+    val file = file.get()
+    val psiDocumentManager = PsiDocumentManager.getInstance(project.get())
+    val psiManager = PsiManagerEx.getInstanceEx(project.get())
+    val plainListener = RecordingTreeChangeListener()
+    val backgroundableListener = RecordingTreeChangeListener()
+    psiManager.addPsiTreeChangeListener(plainListener, testDisposable)
+    psiManager.addPsiTreeChangeListenerBackgroundable(backgroundableListener, testDisposable)
+
+    val plainPreprocessor = RecordingTreeChangePreprocessor()
+    val backgroundablePreprocessor = RecordingTreeChangePreprocessor()
+
+    psiManager.addTreeChangePreprocessor(plainPreprocessor, testDisposable)
+    psiManager.addTreeChangePreprocessorBackgroundable(backgroundablePreprocessor, testDisposable)
+
+    val document = runReadAction {
+      psiDocumentManager.getDocument(file)!!
+    }
+    invokeAndWaitIfNeeded {
+      WriteCommandAction.runWriteCommandAction(project.get(), { document.insertString(0, " ") })
+      PlatformTestUtil.waitForAllDocumentsCommitted(10, TimeUnit.SECONDS)
+    }
+
+    DumbService.getInstance(project.get()).waitForSmartMode()
+
+    assertTrue(plainListener.recorder.invoked.get() > 0)
+    assertTrue(plainListener.recorder.onEdt.get() > 0)
+    assertTrue(backgroundableListener.recorder.invoked.get() > 0)
+    assertTrue(backgroundableListener.recorder.onEdt.get() < backgroundableListener.recorder.invoked.get())
+
+
+    assertTrue(plainPreprocessor.recorder.invoked.get() > 0)
+    assertTrue(plainPreprocessor.recorder.onEdt.get() > 0)
+    assertTrue(backgroundablePreprocessor.recorder.invoked.get() > 0)
+    // sometimes psi events get fired when dumb mode is ending, in this case we have usages of this listener on EDT
+    assertTrue(backgroundablePreprocessor.recorder.onEdt.get() < backgroundablePreprocessor.recorder.invoked.get())
+  }
+
+}
