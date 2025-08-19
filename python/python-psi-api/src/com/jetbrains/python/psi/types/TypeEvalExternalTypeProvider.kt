e5aec74d8ca479be954252a909d3b3f16e764a63
Morgan Bartholomew <morgan.bartholomew@jetbrains.com>
2025-08-19 18:54:48 +1000

    [python] PY-83505 `ExternalTypeProvider` extension point
    GitOrigin-RevId: 4c576007a0f84542f5b91f5e502d0e280c3d5b7c

---
+++ python/python-psi-api/src/com/jetbrains/python/psi/types/TypeEvalExternalTypeProvider.kt
@@ -0,0 +1,21 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.jetbrains.python.psi.types
+
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.util.Ref
+import com.intellij.psi.PsiElement
+import com.jetbrains.python.psi.PyTypedElement
+
+/**
+ * An external type provider invoked by [TypeEvalContext] to obtain a type from a separate engine.
+ * Implementations may return `null` if they cannot provide a type for the given element.
+ */
+interface TypeEvalExternalTypeProvider {
+  fun provideType(element: PyTypedElement, context: TypeEvalContext): Ref<PyType?>?
+
+  companion object {
+    @JvmField
+    val EP_NAME: ExtensionPointName<TypeEvalExternalTypeProvider> =
+      ExtensionPointName.create("Pythonid.typeEvalExternalTypeProvider")
+  }
+}
