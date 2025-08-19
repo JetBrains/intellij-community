a3ee2d7ec0e562e52684b5487ad3672f24c141ac
Alexander Zolotov <zolotov@jetbrains.com>
2025-08-19 11:43:09 +0200

    [syntax] add a helper method for parsing based on syntax definition
    GitOrigin-RevId: 1a99d0cf73153164a03e3be27a5581bf28fb46ca

--- platform/syntax/syntax-tree/src/com/intellij/platform/syntax/tree/Parse.kt
+++ platform/syntax/syntax-tree/src/com/intellij/platform/syntax/tree/Parse.kt
@@ -4,6 +4,7 @@
 package com.intellij.platform.syntax.tree
 
 import com.intellij.platform.syntax.CancellationProvider
+import com.intellij.platform.syntax.LanguageSyntaxDefinition
 import com.intellij.platform.syntax.Logger
 import com.intellij.platform.syntax.SyntaxElementTypeSet
 import com.intellij.platform.syntax.element.SyntaxTokenTypes
@@ -18,6 +19,26 @@ import com.intellij.platform.syntax.parser.SyntaxTreeBuilderFactory
 import com.intellij.platform.syntax.parser.WhitespaceOrCommentBindingPolicy
 import com.intellij.platform.syntax.util.language.SyntaxElementLanguageProvider
 import org.jetbrains.annotations.ApiStatus
+
+fun parse(
+  text: CharSequence,
+  syntaxDefinition: LanguageSyntaxDefinition,
+  languageMapper: SyntaxElementLanguageProvider,
+  cancellationProvider: CancellationProvider? = null,
+  logger: Logger? = null,
+): KmpSyntaxNode {
+  return parse(
+    text = text,
+    lexerFactory = syntaxDefinition::createLexer,
+    parser = syntaxDefinition::parse,
+    whitespaces = syntaxDefinition.whitespaces,
+    comments = syntaxDefinition.comments,
+    languageMapper = languageMapper,
+    cancellationProvider = cancellationProvider,
+    logger = logger,
+    whitespaceOrCommentBindingPolicy = syntaxDefinition.whitespaceOrCommentBindingPolicy,
+  )
+}
 
 fun parse(
   text: CharSequence,
