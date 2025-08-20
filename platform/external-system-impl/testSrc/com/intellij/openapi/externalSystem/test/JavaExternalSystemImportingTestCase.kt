ff0517645803696fac7ff04f0deee3a1aa462e90
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2025-08-20 16:22:03 +0200

    [cleanup][tests][IDEA-375395] converted JavaExternalSystemImportingTestCase to Kotlin
    GitOrigin-RevId: af7bde8bbaca6e9e181578bc1be8cc092b5f0390

--- platform/external-system-impl/testSrc/com/intellij/openapi/externalSystem/test/JavaExternalSystemImportingTestCase.kt
+++ platform/external-system-impl/testSrc/com/intellij/openapi/externalSystem/test/JavaExternalSystemImportingTestCase.kt
@@ -1,91 +1,93 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.externalSystem.test;
+package com.intellij.openapi.externalSystem.test
 
-import com.intellij.compiler.artifacts.ArtifactsTestUtil;
-import com.intellij.openapi.application.ReadAction;
-import com.intellij.openapi.compiler.CompilerPaths;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.CompilerModuleExtension;
-import com.intellij.openapi.roots.OrderEnumerator;
-import com.intellij.openapi.vfs.VfsUtilCore;
-import com.intellij.packaging.artifacts.Artifact;
-import com.intellij.packaging.artifacts.ArtifactManager;
-import com.intellij.platform.externalSystem.testFramework.ExternalSystemImportingTestCase;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.io.TestFileSystemItem;
+import com.intellij.compiler.artifacts.ArtifactsTestUtil
+import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.compiler.CompilerPaths
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.CompilerModuleExtension
+import com.intellij.openapi.roots.OrderEnumerator
+import com.intellij.openapi.util.ThrowableComputable
+import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.packaging.artifacts.Artifact
+import com.intellij.packaging.artifacts.ArtifactManager
+import com.intellij.platform.externalSystem.testFramework.ExternalSystemImportingTestCase
+import com.intellij.testFramework.assertions.Assertions
+import com.intellij.util.PathUtil
+import com.intellij.util.io.TestFileSystemItem
+import junit.framework.TestCase
+import java.io.File
 
-import java.io.File;
-import java.util.List;
+abstract class JavaExternalSystemImportingTestCase : ExternalSystemImportingTestCase() {
 
-import static com.intellij.testFramework.assertions.Assertions.assertThat;
-import static com.intellij.util.PathUtil.toSystemIndependentName;
+  @JvmField
+  protected var useProjectTaskManager: Boolean = false
 
-public abstract class JavaExternalSystemImportingTestCase extends ExternalSystemImportingTestCase {
-
-  protected boolean useProjectTaskManager;
-
-  protected void compileModules(final String... moduleNames) {
-    JavaCompileTestUtil.compileModules(getMyProject(), useProjectTaskManager, moduleNames);
+  protected fun compileModules(vararg moduleNames: String) {
+    compileModules(myProject, useProjectTaskManager, *moduleNames)
   }
 
-  protected void buildArtifacts(String... artifactNames) {
-    JavaCompileTestUtil.buildArtifacts(getMyProject(), useProjectTaskManager, artifactNames);
+  protected fun buildArtifacts(vararg artifactNames: String) {
+    buildArtifacts(myProject, useProjectTaskManager, *artifactNames)
   }
 
-  protected Artifact findArtifact(Project project, String artifactName) {
-    return ReadAction.compute(() -> ArtifactsTestUtil.findArtifact(project, artifactName));
+  protected fun findArtifact(project: Project, artifactName: String): Artifact {
+    return ReadAction.compute<Artifact, RuntimeException?>(ThrowableComputable { ArtifactsTestUtil.findArtifact(project, artifactName) })
   }
 
-  protected void assertArtifactOutputPath(final String artifactName, final String expected) {
-    Artifact artifact = findArtifact(getMyProject(), artifactName);
-    assertThat(toSystemIndependentName(artifact.getOutputPath())).isEqualTo(expected);
+  protected open fun assertArtifactOutputPath(artifactName: String, expected: String) {
+    val artifact = findArtifact(myProject, artifactName)
+    Assertions.assertThat(PathUtil.toSystemIndependentName(artifact.getOutputPath())).isEqualTo(expected)
   }
 
-  protected void assertArtifactOutput(String artifactName, TestFileSystemItem fs) {
-    final Artifact artifact = findArtifact(getMyProject(), artifactName);
-    final String outputFile = artifact.getOutputFilePath();
-    assert outputFile != null;
-    final File file = new File(outputFile);
-    assert file.exists();
-    fs.assertFileEqual(file);
+  protected fun assertArtifactOutput(artifactName: String, fs: TestFileSystemItem) {
+    val artifact = findArtifact(myProject, artifactName)
+    val outputFile: String = checkNotNull(artifact.getOutputFilePath())
+    val file = File(outputFile)
+    assert(file.exists())
+    fs.assertFileEqual(file)
   }
 
-  protected void assertModuleOutputs(String moduleName, String... outputs) {
-    Module module = getModule(moduleName);
-    String[] outputPaths = ContainerUtil.map2Array(CompilerPaths.getOutputPaths(new Module[]{module}), String.class,
-                                                   s -> getAbsolutePath(s));
-    assertUnorderedElementsAreEqual(outputPaths, outputs);
-    String[] outputPathsFromEnumerator = ContainerUtil.map2Array(
-      OrderEnumerator.orderEntries(module).withoutSdk().withoutLibraries().withoutDepModules().classes().getUrls(),
-      String.class,
-      VfsUtilCore::urlToPath
-    );
-    assertUnorderedElementsAreEqual(outputPathsFromEnumerator, outputs);
+  protected fun assertModuleOutputs(moduleName: String, vararg outputs: String) {
+    val module = getModule(moduleName)
+    val outputPaths = CompilerPaths.getOutputPaths(arrayOf(module))
+      .map { getAbsolutePath(it) }
+      .toTypedArray()
+    assertUnorderedElementsAreEqual(outputPaths, *outputs)
+    val outputPathsFromEnumerator = OrderEnumerator.orderEntries(module)
+      .withoutSdk()
+      .withoutLibraries()
+      .withoutDepModules()
+      .classes()
+      .getUrls()
+      .map { VfsUtilCore.urlToPath(it) }.toTypedArray()
+    assertUnorderedElementsAreEqual(outputPathsFromEnumerator, *outputs)
   }
 
-  protected void assertModuleOutput(String moduleName, String output, String testOutput) {
-    CompilerModuleExtension e = getCompilerExtension(moduleName);
+  protected fun assertModuleOutput(moduleName: String, output: String, testOutput: String) {
+    val e = getCompilerExtension(moduleName)
 
-    assertFalse(e.isCompilerOutputPathInherited());
-    assertEquals(output, getAbsolutePath(e.getCompilerOutputUrl()));
-    assertEquals(testOutput, getAbsolutePath(e.getCompilerOutputUrlForTests()));
+    assertFalse(e.isCompilerOutputPathInherited())
+    TestCase.assertEquals(output, getAbsolutePath(e.getCompilerOutputUrl()))
+    TestCase.assertEquals(testOutput, getAbsolutePath(e.getCompilerOutputUrlForTests()))
   }
 
-  protected void assertModuleInheritedOutput(String moduleName) {
-    CompilerModuleExtension e = getCompilerExtension(moduleName);
-    assertTrue(e.isCompilerOutputPathInherited());
+  protected fun assertModuleInheritedOutput(moduleName: String) {
+    val e = getCompilerExtension(moduleName)
+    assertTrue(e.isCompilerOutputPathInherited())
   }
 
-  protected CompilerModuleExtension getCompilerExtension(String module) {
-    return CompilerModuleExtension.getInstance(getModule(module));
+  protected fun getCompilerExtension(module: String): CompilerModuleExtension {
+    return CompilerModuleExtension.getInstance(getModule(module))!!
   }
 
-  protected void assertArtifacts(String... expectedNames) {
-    final List<String> actualNames = ContainerUtil.map(
-      ReadAction.compute(() -> ArtifactManager.getInstance(getMyProject()).getAllArtifactsIncludingInvalid()),
-      artifact -> artifact.getName());
-
-    assertUnorderedElementsAreEqual(actualNames, expectedNames);
+  protected fun assertArtifacts(vararg expectedNames: String) {
+    val artifacts = ReadAction.compute(
+      ThrowableComputable {
+        ArtifactManager.getInstance(myProject).getAllArtifactsIncludingInvalid()
+      }
+    )
+    val actualNames = artifacts.map { it.name }
+    assertUnorderedElementsAreEqual(actualNames, *expectedNames)
   }
 }
