05277848041b70900b2129384781b996085d5e0a
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-08-20 15:17:58 +0200

    nullability for skippers
    GitOrigin-RevId: f05553a0ed956be14cb4e410a4ceb9f996b851d8

--- platform/analysis-api/src/com/intellij/codeInsight/completion/CompletionPreselectSkipper.java
+++ platform/analysis-api/src/com/intellij/codeInsight/completion/CompletionPreselectSkipper.java
@@ -1,27 +1,18 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package com.intellij.codeInsight.completion;
 
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.openapi.extensions.ExtensionPointName;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * Extension point to control which {@link LookupElement}s should be skipped during code completion.
+ * <p/>
+ * Register via extension point "com.intellij.completion.skip".
+ */
 public abstract class CompletionPreselectSkipper {
   public static final ExtensionPointName<CompletionPreselectSkipper> EP_NAME = ExtensionPointName.create("com.intellij.completion.skip");
 
-  public abstract boolean skipElement(LookupElement element, CompletionLocation location);
-
+  public abstract boolean skipElement(@NotNull LookupElement element, @NotNull CompletionLocation location);
 }
