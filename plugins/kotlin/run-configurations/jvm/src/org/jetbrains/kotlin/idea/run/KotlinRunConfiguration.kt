bfb668b494696a867bae3e93eb6e0d4a8f1f9b1a
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-08-20 10:48:02 +0200

    [kotlin] KotlinRunConfiguration: replace `StringUtil#isEmptyOrSpaces()` with `CharSequence#isNullOrBlank()`
    GitOrigin-RevId: 1a0d7732c3a2aac2a90546f3d2e8cf2b2c118425

--- plugins/kotlin/run-configurations/jvm/src/org/jetbrains/kotlin/idea/run/KotlinRunConfiguration.kt
+++ plugins/kotlin/run-configurations/jvm/src/org/jetbrains/kotlin/idea/run/KotlinRunConfiguration.kt
@@ -102,7 +102,7 @@ open class KotlinRunConfiguration(name: String?, runConfigurationModule: JavaRun
     }
 
     override fun setWorkingDirectory(value: String?) {
-        val normalizedValue = if (StringUtil.isEmptyOrSpaces(value)) null else value!!.trim { it <= ' ' }
+        val normalizedValue = if (value.isNullOrBlank()) null else value.trim { it <= ' ' }
         val independentValue = PathUtil.toSystemIndependentName(normalizedValue)
         options.workingDirectory = independentValue?.takeIf { it != defaultWorkingDirectory() }
     }
