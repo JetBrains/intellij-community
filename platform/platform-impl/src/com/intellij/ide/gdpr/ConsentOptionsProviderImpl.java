c6e94278498c6d7bf0d2cd3e156e9978b3dfba23
Dmitry Pogrebnoy <dmitry.pogrebnoy@jetbrains.com>
2025-08-20 13:31:01 +0200

    RUBY-34049 Enable non-com licenses in RubyMine
    (cherry picked from commit 78bed59be107dd62a44ff1fb4bd3dc34bdc7911d)
    
    IJ-CR-173056
    
    GitOrigin-RevId: 880b73f7bca6d9a8bd538845489cd558044b03a5

--- platform/platform-impl/src/com/intellij/ide/gdpr/ConsentOptionsProviderImpl.java
+++ platform/platform-impl/src/com/intellij/ide/gdpr/ConsentOptionsProviderImpl.java
@@ -7,7 +7,7 @@ import com.intellij.ui.LicensingFacade;
 import java.util.Set;
 
 final class ConsentOptionsProviderImpl implements ConsentOptionsProvider {
-  private static final Set<String> productsSupportingForcedConsent = Set.of("QA", "RR", "WS", "RD", "CL");
+  private static final Set<String> productsSupportingForcedConsent = Set.of("QA", "RR", "WS", "RD", "CL", "RM");
 
   private volatile long myLastModificationCount = -1;
   private volatile boolean mySendingAllowed = false;
