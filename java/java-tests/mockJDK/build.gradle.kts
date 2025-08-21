46e3e76039538430017c80b28de264377facef99
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-08-21 16:14:12 +0200

    [java-tests] IDEA-377610 fix incorrect usage of new Gradle APIs
    GitOrigin-RevId: a87373c3a89a59cfeb70f9bcd9ffbcc67b94305b

--- java/java-tests/mockJDK/build.gradle.kts
+++ java/java-tests/mockJDK/build.gradle.kts
@@ -4,10 +4,10 @@
  * To publish a new mock to repository, create gradle.properties file and define "spaceUsername" and "spacePassword" properties.
  * The created mockJDK version is the same as Gradle bootstrap JDK, so carefully set it.
  * For password, you might need to create an Application Password on Space.
- * 
+ *
  * Use `gradle clean build` to ensure that proper lib is created under build/libs
  * Use `gradle clean publish` to publish
- * 
+ *
  * You can find the uploaded artifact at https://packages.jetbrains.team/maven/p/ij/intellij-dependencies/org/jetbrains/mockjdk/.
  *
  * Update (21/08/2025):
@@ -24,7 +24,8 @@ plugins {
 
 val javaVersion: String = Runtime.version().feature().toString()
 
-val jmodDir = project.file("${layout.buildDirectory}/jmod")
+val buildDir = layout.buildDirectory.get().asFile
+val jmodDir = project.file("$buildDir/jmod")
 
 tasks.register("ensureDirectory") {
   doLast {
@@ -49,14 +50,13 @@ tasks.register<Copy>("jmodCopy") {
   into(layout.buildDirectory.dir("resources"))
 }
 
-tasks {
-  withType<JavaCompile>() {
-    dependsOn("jmodCopy")
-  }
-  withType<Jar>() {
-    dependsOn("processTestResources")
-    from("${layout.buildDirectory}/resources")
-  }
+tasks.withType<JavaCompile>() {
+  dependsOn("jmodCopy")
+}
+
+tasks.withType<Jar>() {
+  dependsOn("processTestResources")
+  from("$buildDir/resources")
 }
 
 publishing {
@@ -68,7 +68,7 @@ publishing {
       from(components["java"])
       pom {
         licenses {
-          license { 
+          license {
             name = "GNU General Public License, version 2, with the Classpath Exception"
             url = "https://openjdk.org/legal/gplv2+ce.html"
           }
