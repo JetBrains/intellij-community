6e9126c3db19723d092999df9caa435a8ca61d11
Sebastiano Poggi <sebp@google.com>
2025-08-21 15:05:02 +0200

    [JEWEL-986] Bump Jewel API version to 0.30.0
    This change bumps the Jewel API version to 0.30.0, while fixing a bug
    in the version updater script. It also adds the missing Compose
    Resources dependencies from the UI module's POM, and Coil from the
    Markdown Images extension's (also fixed in the CMP 1.9.0-beta03
    branch).
    
    closes https://github.com/JetBrains/intellij-community/pull/3195
    
    (cherry picked from commit 72b870278cb733155bca9bdaae11088640749aea)
    
    
    (cherry picked from commit ff79dbee1feaf8729aaf248182ce4d4ac86a0de2)
    
    IJ-MR-173046
    
    GitOrigin-RevId: d2c82fca2c64adebdf8273d40aabbe862fbf0bcb

--- platform/jewel/scripts/jewel-version-updater.main.kts
+++ platform/jewel/scripts/jewel-version-updater.main.kts
@@ -49,7 +49,7 @@ private val jewelApiVersionTemplate =
     |
     |/** The Jewel API version for this build, expressed as a string. E.g.: "0.30.0" */
     |public val JewelBuild.apiVersionString: String
-    |    get() = "0.29.0"
+    |    get() = "%%JEWEL_VERSION%%"
     |
     """
         .trimMargin()
