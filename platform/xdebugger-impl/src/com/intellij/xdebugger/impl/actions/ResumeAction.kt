a5fc77f581243cfc60c1b8329ca88e0736b4bacd
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2025-08-22 12:50:11 +0200

    [execution] IJPL-200094 Implement usage collector for ResumeAction outcomes
    GitOrigin-RevId: bfedc6b7e7097fe8904e4cb45246013c0125f2a0

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/ResumeAction.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/ResumeAction.kt
@@ -2,11 +2,13 @@
 package com.intellij.xdebugger.impl.actions
 
 import com.intellij.execution.actions.ChooseDebugConfigurationPopupAction
+import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.platform.debugger.impl.rpc.XDebugSessionApi
+import com.intellij.xdebugger.impl.XDebuggerActionsCollector
 import com.intellij.xdebugger.impl.performDebuggerActionAsync
 import com.intellij.xdebugger.impl.ui.DebuggerUIUtil
 import java.awt.event.KeyEvent
@@ -18,20 +20,28 @@ open class ResumeAction : DumbAwareAction(), ActionRemoteBehaviorSpecification.F
       e.presentation.isEnabled = false
       return
     }
+
     val session = DebuggerUIUtil.getSessionProxy(e)
     // Do nothing when there is no session, so that LUX-ed action on frontend will delegate to the backend action
     if (session == null) {
-      // we may trigger the Choose Run Configuration dialog via the action shortcut, the action must be enabled
-      e.presentation.isEnabled = true
+      // we may trigger the Choose Run Configuration dialog via the action shortcut, the action must be enabled,
+      // but in the Run / Debug toolbar it should be disabled when no session. There the action is attached to the concrete process.
+      e.presentation.isEnabled = isFromShortcutOrSearch(e)
       return
     }
+
     if (!session.isStopped) {
       e.presentation.isEnabled = session.isPaused && !session.isReadOnly
     }
     else {
       // disable visual representation but leave the shortcut action enabled
-      e.presentation.isEnabled = e.inputEvent is KeyEvent
+      e.presentation.isEnabled = isFromShortcutOrSearch(e)
     }
+  }
+
+  private fun isFromShortcutOrSearch(e: AnActionEvent): Boolean {
+    return e.inputEvent is KeyEvent
+           || e.place == ActionPlaces.ACTION_SEARCH
   }
 
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
@@ -40,11 +50,15 @@ open class ResumeAction : DumbAwareAction(), ActionRemoteBehaviorSpecification.F
     val session = DebuggerUIUtil.getSessionProxy(e)
     if (session == null) {
       getEventProject(e).let {
+        XDebuggerActionsCollector.chooseDebugConfigurationOnResume(e)
+
         ChooseDebugConfigurationPopupAction().actionPerformed(e)
       }
     }
     else {
       performDebuggerActionAsync(e) {
+        XDebuggerActionsCollector.sessionResumedOnResume(e)
+
         XDebugSessionApi.getInstance().resume(session.id)
       }
     }
