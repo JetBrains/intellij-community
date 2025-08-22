e4093e8a09d7f63ba37e3bb94e40d2a2356b8559
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-08-22 13:27:05 +0200

    Revert "[threading] IJPL-188470: Do not fork `BulkFileListener`"
    Because of declarative registration, two topics with different semantics cannot share the same interface
    
    This reverts commit 768e7fa97c4b3d6ddcaf9f62262247640d770905.
    
    GitOrigin-RevId: 12cf52a3b594114601e302de65ddd40c2ee3e346

--- platform/platform-tests/testSrc/com/intellij/openapi/vfs/newvfs/persistent/VfsRefreshTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/vfs/newvfs/persistent/VfsRefreshTest.kt
@@ -10,6 +10,7 @@ import com.intellij.openapi.vfs.AsyncFileListener
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.openapi.vfs.newvfs.BulkFileListener
+import com.intellij.openapi.vfs.newvfs.BulkFileListenerBackgroundable
 import com.intellij.openapi.vfs.newvfs.RefreshQueue
 import com.intellij.openapi.vfs.newvfs.RefreshQueueImpl
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
@@ -162,7 +163,7 @@ class VfsRefreshTest {
           counter.incrementAndGet()
         }
       })
-      application.messageBus.connect(disposable).subscribe(VirtualFileManager.VFS_CHANGES_BG, object : BulkFileListener {
+      application.messageBus.connect(disposable).subscribe(VirtualFileManager.VFS_CHANGES_BG, object : BulkFileListenerBackgroundable {
         override fun before(events: List<VFileEvent>) {
           assertThat(EDT.isCurrentThreadEdt()).isEqualTo(bgListenersShouldRunOnEdt)
           counter.incrementAndGet()
