b27347266b860ff88b18609748c32cffb6ec5b5a
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-08-22 18:57:10 +0300

    [terminal] IJPL-176779 Refactor command execution
    GitOrigin-RevId: 41c60398799a03e1d2fa0a7d0451352bf843fb45

--- plugins/terminal/src/org/jetbrains/plugins/terminal/block/completion/spec/impl/reworked/ShellCommandExecutorReworked.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/block/completion/spec/impl/reworked/ShellCommandExecutorReworked.kt
@@ -3,6 +3,7 @@ package org.jetbrains.plugins.terminal.block.completion.spec.impl.reworked
 
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.platform.eel.EelDescriptor
+import com.intellij.platform.eel.ExecuteProcessException
 import com.intellij.platform.eel.path.EelPath
 import com.intellij.platform.eel.provider.utils.awaitProcessResult
 import com.intellij.platform.eel.provider.utils.stdoutString
@@ -15,27 +16,44 @@ import kotlin.coroutines.cancellation.CancellationException
 internal class ShellCommandExecutorReworked(private val eelDescriptor: EelDescriptor) : ShellCommandExecutor {
   override suspend fun runShellCommand(directory: String, command: String): ShellCommandResult {
     val commandList = ParametersListUtil.parse(command)
-    val commandName = commandList.firstOrNull() ?: return ShellCommandResult.create("{}", 0)
+    val commandName = commandList.firstOrNull() ?: return emptyResult()
     val arguments = commandList.drop(1)
     return executeCommandViaEel(directory, commandName, arguments)
   }
 
-  private suspend fun executeCommandViaEel(directory: String, commandName: String, arguments: List<String>): ShellCommandResult {
-    try {
+  private suspend fun executeCommandViaEel(
+    directory: String,
+    commandName: String,
+    arguments: List<String>,
+  ): ShellCommandResult {
+    val eelDirectory = try {
+      EelPath.parse(directory, eelDescriptor)
+    }
+    catch (e: Exception) {
+      LOG.error("Failed to parse directory as EelPath: '$directory'", e)
+      return emptyResult()
+    }
+
+    return try {
       val eel = eelDescriptor.toEelApi()
-      val eelDirectory = EelPath.parse(directory, eelDescriptor)
-      val processResult = eel.exec.spawnProcess(commandName).args(arguments).workingDirectory(eelDirectory).eelIt()
-      val result = processResult.awaitProcessResult()
-      return ShellCommandResult.create(result.stdoutString, result.exitCode)
+      val process = eel.exec
+        .spawnProcess(commandName)
+        .args(arguments)
+        .workingDirectory(eelDirectory)
+        .eelIt()
+      val result = process.awaitProcessResult()
+      ShellCommandResult.create(result.stdoutString, result.exitCode)
     }
     catch (ce: CancellationException) {
       throw ce
     }
-    catch (e: Exception) {
-      LOG.error("Failed to access Eel API: ${e.message}", e)
-      return ShellCommandResult.create("{}", 0)
+    catch (e: ExecuteProcessException) {
+      LOG.error("Failed to execute command using Eel API. Command: '$commandName', arguments: $arguments, directory: '$directory'", e)
+      emptyResult()
     }
   }
+
+  private fun emptyResult(): ShellCommandResult = ShellCommandResult.create("{}", 0)
 
   companion object {
     private val LOG = logger<ShellCommandExecutorReworked>()
