45a60ec188cb352f43ca6c7f4b7764cdcf59b57e
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-08-26 13:25:14 +0200

    [threading] IJPL-199522: Generalize VFS threading service
    GitOrigin-RevId: 8487ecf2f83ba3bc99e733f5dc927ec0d5a1b940

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/impl/VfsThreadingUtil.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/impl/VfsThreadingUtil.java
@@ -1,12 +1,9 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vfs.newvfs.impl;
 
-import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.impl.ApplicationImpl;
-import com.intellij.openapi.application.impl.InternalThreading;
+import com.intellij.openapi.application.impl.TransferredWriteActionService;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.vfs.newvfs.persistent.PersistentFSImpl;
 import com.intellij.util.ui.EDT;
 import org.jetbrains.annotations.ApiStatus;
 
@@ -21,17 +18,7 @@ public final class VfsThreadingUtil {
       action.run();
     }
     else {
-      Application application = ApplicationManager.getApplication();
-      if (application instanceof ApplicationImpl) {
-        try {
-          InternalThreading.invokeAndWaitWithTransferredWriteAction(action);
-        }
-        catch (Throwable t) {
-          throw new RuntimeException(t);
-        }
-      } else {
-        LOG.warn("Cannot invoke EDT VFS listeners on background");
-      }
+      ApplicationManager.getApplication().getService(TransferredWriteActionService.class).runOnEdtWithTransferredWriteActionAndWait(action);
     }
   }
 }
