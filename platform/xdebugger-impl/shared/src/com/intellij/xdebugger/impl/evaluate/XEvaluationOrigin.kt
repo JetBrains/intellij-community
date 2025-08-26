e98d6fd3593c34427bb5233f0aef64ca80270f59
Alexey.Merkulov <alexey.merkulov@jetbrains.com>
2025-08-26 16:40:45 +0200

    [rd debugger] Refactoring to use XEvaluationOrigin in remote API
    GitOrigin-RevId: d5cdbc95379546696ad80f233bbdbb68fccfb21e

--- platform/xdebugger-impl/shared/src/com/intellij/xdebugger/impl/evaluate/XEvaluationOrigin.kt
+++ platform/xdebugger-impl/shared/src/com/intellij/xdebugger/impl/evaluate/XEvaluationOrigin.kt
@@ -1,12 +1,15 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.xdebugger.impl.ui.tree.nodes
+package com.intellij.xdebugger.impl.evaluate
 
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.ThrowableComputable
 import com.intellij.openapi.util.UserDataHolder
 import com.intellij.openapi.util.UserDataHolderBase
+import kotlinx.serialization.Serializable
 import org.jetbrains.annotations.ApiStatus
 
+@ApiStatus.Internal
+@Serializable
 enum class XEvaluationOrigin {
   INLINE,
   DIALOG,
