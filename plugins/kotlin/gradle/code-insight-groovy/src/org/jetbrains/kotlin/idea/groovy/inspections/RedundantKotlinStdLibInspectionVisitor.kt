d42ae327a654458578bb2f00d572502f603c1dfd
Kajus Kuncaitis <kajus.kuncaitis@jetbrains.com>
2025-08-27 14:26:16 +0200

    [gradle][IDEA-376859] Detect Kotlin JVM plugin when it's used from a version catalog
    GitOrigin-RevId: eddb2abd38a6dd6b6cf3021504c8e15cd8ce880a

--- plugins/kotlin/gradle/code-insight-groovy/src/org/jetbrains/kotlin/idea/groovy/inspections/RedundantKotlinStdLibInspectionVisitor.kt
+++ plugins/kotlin/gradle/code-insight-groovy/src/org/jetbrains/kotlin/idea/groovy/inspections/RedundantKotlinStdLibInspectionVisitor.kt
@@ -20,6 +20,9 @@ import org.jetbrains.kotlin.utils.PathUtil.KOTLIN_JAVA_STDLIB_NAME
 import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames
 import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.GRADLE_API_DEPENDENCY_HANDLER
 import org.jetbrains.plugins.gradle.service.resolve.GradleDependencyHandlerContributor.Companion.dependencyMethodKind
+import org.jetbrains.plugins.gradle.toml.findOriginInTomlFile
+import org.jetbrains.plugins.gradle.util.isInVersionCatalogAccessor
+import org.jetbrains.plugins.groovy.lang.psi.GrReferenceElement
 import org.jetbrains.plugins.groovy.lang.psi.GroovyFileBase
 import org.jetbrains.plugins.groovy.lang.psi.GroovyRecursiveElementVisitor
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrMethodCall
@@ -30,19 +33,23 @@ import org.jetbrains.plugins.groovy.lang.psi.impl.stringValue
 import org.jetbrains.plugins.groovy.lang.psi.patterns.GroovyMethodCallPattern
 import org.jetbrains.plugins.groovy.lang.psi.patterns.psiMethod
 import org.jetbrains.plugins.groovy.lang.psi.patterns.withKind
+import org.toml.lang.psi.TomlInlineTable
+import org.toml.lang.psi.TomlKeyValue
+import org.toml.lang.psi.TomlLiteral
 
 private val LOG = logger<RedundantKotlinStdLibInspectionVisitor>()
 
 class RedundantKotlinStdLibInspectionVisitor(val holder: ProblemsHolder) : KotlinGradleInspectionVisitor() {
+
+    private val KOTLIN_JVM_PLUGIN_ID = "$KOTLIN_GROUP_ID.jvm"
+
     override fun visitCallExpression(callExpression: GrCallExpression) {
         // first check if org.jetbrains.kotlin.jvm plugin is applied before searching for kotlin-stdlib dependencies
-        val pluginIdPattern = GroovyMethodCallPattern.resolvesTo(
-            psiMethod(GradleCommonClassNames.GRADLE_PLUGIN_USE_PLUGIN_DEPENDENCIES_SPEC, "id")
-        )
-        if (!pluginIdPattern.accepts(callExpression)) return
-        val pluginId = callExpression.expressionArguments.firstOrNull()
-        // TODO only works if plugin id argument is a literal
-        if (pluginId !is GrLiteral || pluginId.value != "$KOTLIN_GROUP_ID.jvm") return
+        if (!isPluginIdOfKotlinJvm(callExpression) && !isPluginAliasOfKotlinJvm(callExpression)) return
+        DependenciesVisitor(holder).visitFile(callExpression.containingFile as GroovyFileBase)
+    }
+
+    private fun isPluginCallApplied(callExpression: GrCallExpression): Boolean {
         val applyPattern = GroovyMethodCallPattern.resolvesTo(
             psiMethod(GradleCommonClassNames.GRADLE_PLUGIN_USE_PLUGIN_DEPENDENCY_SPEC, "apply")
         )
@@ -51,10 +58,56 @@ class RedundantKotlinStdLibInspectionVisitor(val holder: ProblemsHolder) : Kotli
         }
 
         // no implicit kotlin-stdlib dependency if the kotlin plugin is not applied
-        if (applyCall != null && applyCall.expressionArguments.firstOrNull().booleanValue() == false) return
+        return !(applyCall != null && applyCall.expressionArguments.firstOrNull().booleanValue() == false)
+    }
 
-        LOG.debug("Kotlin JVM plugin is applied in file ${callExpression.containingFile.virtualFile.path}, searching for kotlin-stdlib dependencies")
-        DependenciesVisitor(holder).visitFile(callExpression.containingFile as GroovyFileBase)
+    private fun isPluginIdOfKotlinJvm(callExpression: GrCallExpression): Boolean {
+        val pluginIdPattern = GroovyMethodCallPattern.resolvesTo(
+            psiMethod(GradleCommonClassNames.GRADLE_PLUGIN_USE_PLUGIN_DEPENDENCIES_SPEC, "id")
+        )
+        if (!pluginIdPattern.accepts(callExpression) || !isPluginCallApplied(callExpression)) return false
+
+        val pluginId = callExpression.expressionArguments.firstOrNull()
+        // TODO only works if plugin id argument is a literal
+        if (pluginId !is GrLiteral || pluginId.value != KOTLIN_JVM_PLUGIN_ID) return false
+
+        LOG.debug("Kotlin JVM plugin is applied in file ${callExpression.containingFile.virtualFile.path}")
+        return true
+    }
+
+    private fun isPluginAliasOfKotlinJvm(callExpression: GrCallExpression): Boolean {
+        val pluginAliasPattern = GroovyMethodCallPattern.resolvesTo(
+            psiMethod(GradleCommonClassNames.GRADLE_PLUGIN_USE_PLUGIN_DEPENDENCIES_SPEC, "alias")
+        )
+        if (!pluginAliasPattern.accepts(callExpression) || !isPluginCallApplied(callExpression)) return false
+
+        val catalogReference = callExpression.expressionArguments.firstOrNull() as? GrReferenceElement<*> ?: return false
+        val resolved = catalogReference.resolve()
+        if (resolved !is PsiMethod || !isInVersionCatalogAccessor(resolved)) return false
+        val origin = findOriginInTomlFile(resolved, catalogReference) as? TomlKeyValue ?: return false
+
+        val pluginIdLiteral = when (val originValue = origin.value) {
+            is TomlLiteral -> originValue
+            is TomlInlineTable -> {
+                val pluginId = originValue.entries.find { it.key.segments.size == 1 && it.key.segments.firstOrNull()?.name == "id" }?.value
+                    ?: return false
+                if (pluginId !is TomlLiteral) return false
+                pluginId
+            }
+
+            else -> return false
+        }
+
+        val idRaw = pluginIdLiteral.text.removeSurrounding("\"").removeSurrounding("'")
+            .split(":").firstOrNull() ?: return false
+        if (idRaw != KOTLIN_JVM_PLUGIN_ID) return false
+        LOG.debug(
+            """
+            Kotlin JVM plugin is applied in file ${callExpression.containingFile.virtualFile.path} 
+            from version catalog ${origin.containingFile.virtualFile.path}
+            """
+        )
+        return true
     }
 }
 
