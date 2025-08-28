ce1b59f221a9379cacc5b24d00a753c4248929fc
Anastasia.Katsman <anastasia.katsman@jetbrains.com>
2025-08-28 12:38:22 +0200

    Add an option to disable notification about trace data sharing
    (cherry picked from commit 8fd9ccfb8cf73474e25ef1225caf5719fd7ac840)
    
    IJ-CR-177425
    
    GitOrigin-RevId: 326073aa42ce617a2f51e390c3ecd707eee0c2cf

--- platform/platform-impl/src/com/intellij/ide/gdpr/trace/TraceDataSharingActivity.kt
+++ platform/platform-impl/src/com/intellij/ide/gdpr/trace/TraceDataSharingActivity.kt
@@ -14,6 +14,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.ui.MessageDialogBuilder
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.ui.LicensingFacade
 import com.intellij.util.concurrency.annotations.RequiresEdt
 import kotlinx.coroutines.CoroutineScope
@@ -104,7 +105,7 @@ private fun isInternalUser(): Boolean {
 }
 
 private fun canAskForTraceDataCollection(): Boolean =
-  !isChinaRegion() && DataCollectionAgreement.getInstance() in listOf(DataCollectionAgreement.NOT_SET, null)
+  !isChinaRegion() && DataCollectionAgreement.getInstance() in listOf(DataCollectionAgreement.NOT_SET, null) && isEnabled
 
 private fun isChinaRegion(): Boolean {
   return when (RegionSettings.getRegion()) {
@@ -113,3 +114,5 @@ private fun isChinaRegion(): Boolean {
     else -> false
   }
 }
+
+private val isEnabled by lazy { Registry.`is`("ide.enable.notification.trace.data.sharing") }
