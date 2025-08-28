9a2b20bc7d41234d9b68a141b59880b5e3644138
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-08-28 11:44:27 +0200

    formatting
    GitOrigin-RevId: 1a95e6ed53a75b80f4d344ff0989693804bc2022

--- platform/analysis-impl/src/com/intellij/codeInsight/AutoPopupController.java
+++ platform/analysis-impl/src/com/intellij/codeInsight/AutoPopupController.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package com.intellij.codeInsight;
 
@@ -56,7 +56,9 @@ public abstract class AutoPopupController {
 
   public abstract void autoPopupMemberLookup(@NotNull Editor editor, @Nullable Condition<? super PsiFile> condition);
 
-  public abstract void autoPopupMemberLookup(@NotNull Editor editor, @NotNull CompletionType completionType, @Nullable Condition<? super PsiFile> condition);
+  public abstract void autoPopupMemberLookup(@NotNull Editor editor,
+                                             @NotNull CompletionType completionType,
+                                             @Nullable Condition<? super PsiFile> condition);
 
   public abstract void scheduleAutoPopup(@NotNull Editor editor,
                                          @NotNull CompletionType completionType,
