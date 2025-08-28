53212ab08513bcff40fc6b5b34cc853f69d74deb
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-08-28 10:24:20 +0200

    [java-inspections] ClassCanBeRecord: simplify some test fixtures
    The nested classes were not testing anything.
    Unnecessarily complicated.
    
    GitOrigin-RevId: fd12c7bbf15b4b1ff78e350cd656976da10d0196

--- java/java-tests/testData/inspection/classCanBeRecord/afterCtorParamsDifferFromFields_4_large.java
+++ java/java-tests/testData/inspection/classCanBeRecord/afterCtorParamsDifferFromFields_4_large.java
@@ -1,7 +1,6 @@
 // "Convert to record class" "true-preview"
+
 import org.jetbrains.annotations.NotNull;
 
-class SomeClass {
-    public record Problem(int myLine, int myColumn, @NotNull String code) {
-    }
+record Problem(int myLine, int myColumn, @NotNull String code) {
 }
--- java/java-tests/testData/inspection/classCanBeRecord/beforeCtorParamsDifferFromFields_6_large_veryBadOrder_badTypes.java
+++ java/java-tests/testData/inspection/classCanBeRecord/beforeCtorParamsDifferFromFields_6_large_veryBadOrder_badTypes.java
@@ -1,24 +1,23 @@
 // "Convert to record class" "false"
+
 import org.jetbrains.annotations.NotNull;
 
-class SomeClass {
-  static class Prob<caret>lem {
-    private final int myLine;
-    private final @NotNull String code;
-    private final int myColumn;
+class Problem<caret> {
+  private final int myLine;
+  private final @NotNull String code;
+  private final int myColumn;
 
-    // Notice 1: order of parameters differs from order of instance fields
-    // Notice 2: second parameter 'line' has incompatible type
-    Problem(int column, Object line, @NotNull String code) {
-      myLine = line;
-      this.code = code;
-      myColumn = column;
-    }
+  // Notice 1: order of parameters differs from order of instance fields
+  // Notice 2: second parameter 'line' has incompatible type
+  Problem(int column, Object line, @NotNull String code) {
+    myLine = line;
+    this.code = code;
+    myColumn = column;
+  }
 
-    static Problem make() {
-      int columnArg = 42;
-      int lineArg = 0;
-      return new Problem(columnArg, lineArg, "");
-    }
+  static Problem make() {
+    int columnArg = 42;
+    int lineArg = 0;
+    return new Problem(columnArg, lineArg, "");
   }
 }
