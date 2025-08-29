29f330dab6f3d4dca151fc9f707bbc2be9b9affd
Morgan Bartholomew <morgan.bartholomew@jetbrains.com>
2025-08-29 15:51:19 +1000

    [python] PY-65674 pyright support
    (cherry picked from commit ca3727d7eb017ec61e93d1336d9ca97dfa39c325)
    
    GitOrigin-RevId: 02c10705a3dbe186d2db70f7a249854494c50e3d

--- python/ide/impl/src/com/intellij/pycharm/community/ide/impl/PyCharmProjectConfigurableStartupActivity.kt
+++ python/ide/impl/src/com/intellij/pycharm/community/ide/impl/PyCharmProjectConfigurableStartupActivity.kt
@@ -47,6 +47,10 @@ class PyCharmProjectConfigurableStartupActivity : ProjectActivity {
           ep.groupId = PythonToolsConfigurable.ID
           ep.groupWeight = 40
         }
+        "com.intellij.python.pyright.PyrightConfigurable" -> {
+          ep.groupId = PythonToolsConfigurable.ID
+          ep.groupWeight = 40
+        }
         "com.jetbrains.python.black.configuration.BlackFormatterConfigurable" -> {
           ep.groupId = PythonToolsConfigurable.ID
           ep.groupWeight = 30
