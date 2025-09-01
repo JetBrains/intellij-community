9748ceb3c28d845bdc5dc5ecfc5b5ba73fdcf801
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2025-09-01 09:15:39 +0200

    [vcs] IJPL-173924 Revert non-modal commit changes in ChangesViewPanel
    GitOrigin-RevId: 8e325a6b254b16e2f72cc8f9eea5ebaf8fc23e7b

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ChangesViewPanel.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ChangesViewPanel.kt
@@ -7,11 +7,7 @@ import com.intellij.openapi.actionSystem.ActionPlaces.CHANGES_VIEW_TOOLBAR
 import com.intellij.openapi.actionSystem.ActionToolbar
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.actionSystem.toolbarLayout.ToolbarLayoutStrategy
-import com.intellij.openapi.vcs.VcsBundle
 import com.intellij.openapi.vcs.changes.ui.ChangesListView
-import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManagerListener
-import com.intellij.openapi.vcs.merge.ChangesViewConflictsBanner
-import com.intellij.openapi.vcs.merge.MergeConflictManager
 import com.intellij.ui.ScrollPaneFactory.createScrollPane
 import com.intellij.ui.ScrollableContentBorder
 import com.intellij.ui.Side
@@ -40,15 +36,8 @@ class ChangesViewPanel(val changesView: ChangesListView, parentDisposable: Dispo
   private val changesScrollPane = createScrollPane(changesView, true)
   private val scrollableBordersPanel = simplePanel(changesScrollPane).andTransparent()
   private val centerPanel = simplePanel(scrollableBordersPanel).andTransparent()
-  private val conflictsBanner =
-    ChangesViewConflictsBanner(
-      VcsBundle.message("changes.view.conflicts.banner.title"),
-      changesView
-    ).showCloseButton(false)
 
   init {
-    ChangeListManager.getInstance(changesView.project)
-      .addChangeListListener(MyMergeConflictBannerListener(), parentDisposable)
     addToCenter(centerPanel)
     addToolbar(isToolbarHorizontal)
   }
@@ -69,21 +58,6 @@ class ChangesViewPanel(val changesView: ChangesListView, parentDisposable: Dispo
       toolbar.setOrientation(SwingConstants.VERTICAL)
       ScrollableContentBorder.setup(changesScrollPane, setOf(Side.LEFT), scrollableBordersPanel)
       addToLeft(toolbar.component)
-    }
-  }
-
-  private inner class MyMergeConflictBannerListener: ChangeListListener {
-    override fun changeListUpdateDone() {
-      val project = changesView.project
-      if (MergeConflictManager.getInstance(project).isMergeConflict() && MergeConflictManager.isNonModalMergeEnabled(project)) {
-        centerPanel.addToTop(conflictsBanner)
-      }
-      else {
-        conflictsBanner.close()
-      }
-      ChangeListManager.getInstance(project).invokeAfterUpdate(true) {
-        project.getMessageBus().syncPublisher(ChangesViewContentManagerListener.TOPIC).toolWindowMappingChanged()
-      }
     }
   }
 }
