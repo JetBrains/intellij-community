0da95d59746da313bb109de6092c30b06a4bf452
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2025-09-01 15:18:44 +0200

    [devkit] IJPL-194034: Temporarily disable ModuleNotRegisteredAsPluginContentInspection before fixing false-positives
    GitOrigin-RevId: 2fe1a273e67fd4331bdae81e7b147e5c5c27787f

--- plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/inspections/DevkitInspectionsRegistrationCheckTest.java
+++ plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/inspections/DevkitInspectionsRegistrationCheckTest.java
@@ -19,6 +19,7 @@ public class DevkitInspectionsRegistrationCheckTest extends BasePlatformTestCase
    */
   private static final List<String> DISABLED_INSPECTIONS =
     Stream.of(
+      "ModuleNotRegisteredAsPluginContent",
       "SerializableCtor",
       "StatisticsCollectorNotRegistered"
     ).sorted().toList();
