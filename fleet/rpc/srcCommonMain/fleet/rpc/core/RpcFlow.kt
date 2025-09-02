29ceefaa0dab3917b5e5304e74428b86375e69c5
Alexander Shparun <alexander.shparun@jetbrains.com>
2025-09-02 19:15:15 +0200

    [fleet] minor
    GitOrigin-RevId: 97a9a941080ba4dfc3fdf2b95226d66dd86bf7b9

--- fleet/rpc/srcCommonMain/fleet/rpc/core/RpcFlow.kt
+++ fleet/rpc/srcCommonMain/fleet/rpc/core/RpcFlow.kt
@@ -4,6 +4,7 @@ package fleet.rpc.core
 import fleet.util.serialization.DataSerializer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flowOf
@@ -11,7 +12,6 @@ import kotlinx.coroutines.flow.flowOn
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.coroutineContext
 
 @Serializable(with=RpcFlow.Serializer::class)
 data class RpcFlow<T> internal constructor(internal val flow: Flow<T>) {
@@ -35,6 +35,6 @@ class FlowScope(val scope: CoroutineScope): CoroutineContext.Element {
   override val key: CoroutineContext.Key<*> get() = FlowScope
 }
 
-suspend fun<T> Flow<T>.toRpc(): RpcFlow<T> = toRpc(coroutineContext)
+suspend fun<T> Flow<T>.toRpc(): RpcFlow<T> = toRpc(currentCoroutineContext())
 
 fun<T> Flow<T>.toRpc(context: CoroutineContext): RpcFlow<T> = RpcFlow(flowOn(context.minusKey(Job)))
