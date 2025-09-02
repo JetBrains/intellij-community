d5a024cf30ca4e1508d6f9a582141cc4d8b7927b
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-09-02 17:16:03 +0300

    [terminal] IJPL-176779 Disable command completion in RemDev and Windows for now
    Windows case just need to be reviewed and presumably won't require a lot of additional work.
    RemDev - requires much more work, such as proper split and typeahead support.
    
    GitOrigin-RevId: fe533cb0b8993f089e064b92af47512457e5681c

--- plugins/terminal/src/org/jetbrains/plugins/terminal/block/reworked/TerminalCommandCompletion.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/block/reworked/TerminalCommandCompletion.kt
@@ -1,8 +1,10 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.terminal.block.reworked
 
+import com.intellij.idea.AppModeAssertions
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.util.registry.Registry
+import com.intellij.util.system.OS
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.TestOnly
 
@@ -12,6 +14,8 @@ object TerminalCommandCompletion {
 
   fun isEnabled(): Boolean {
     return Registry.`is`(REGISTRY_KEY, false)
+           && AppModeAssertions.isMonolith()  // Disable in RemDev at the moment because it is not supported yet
+           && OS.CURRENT != OS.Windows        // Disable on Windows for now as it requires additional support
   }
 
   @TestOnly
