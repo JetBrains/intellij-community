c71e02a4cee9e8737f0048baaf9e569b1796da42
Titouan Bion <titouan.bion@jetbrains.com>
2025-09-03 11:31:14 +0200

    [fleet] Adding require transitive `hamcrest` (2) library after its removal from `JUnit4` by fef9b98f962c06ae5269415d9235875320a034ed
    GitOrigin-RevId: 73dbbdfaaba679c4b76f25098d2af081bf23528a

---
+++ fleet/junit4/gradlebuild/build.gradle.kts
@@ -0,0 +1,49 @@
+// IMPORT__MARKER_START
+import fleet.buildtool.jps.module.plugin.configureAtMostOneJvmTargetOrThrow
+import fleet.buildtool.jps.module.plugin.withJavaSourceSet
+// IMPORT__MARKER_END
+
+plugins {
+  alias(libs.plugins.kotlin.multiplatform)
+  id("fleet.project-module-conventions")
+  id("fleet.toolchain-conventions")
+  id("fleet-build-jps-module-plugin")
+  alias(libs.plugins.dokka)
+  // GRADLE_PLUGINS__MARKER_START
+  // GRADLE_PLUGINS__MARKER_END
+}
+
+val jpsModuleName = "fleet.junit4"
+
+jpsModule {
+  location {
+    moduleName = jpsModuleName
+  }
+}
+
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
+kotlin {
+  // KOTLIN__MARKER_START
+  jvm {}
+  pluginManager.withPlugin("fleet-build-jps-module-plugin") {
+    sourceSets.commonMain.configure { kotlin.srcDir(layout.projectDirectory.dir("../srcCommonMain")) }
+    sourceSets.commonMain.configure { resources.srcDir(layout.projectDirectory.dir("../resourcesCommonMain")) }
+    sourceSets.commonTest.configure { kotlin.srcDir(layout.projectDirectory.dir("../testCommonTest")) }
+    sourceSets.commonTest.configure { resources.srcDir(layout.projectDirectory.dir("../testResourcesCommonTest")) }
+    sourceSets.jvmMain.configure { kotlin.srcDir(layout.projectDirectory.dir("../srcJvmMain")) }
+    configureAtMostOneJvmTargetOrThrow { compilations.named("main") { withJavaSourceSet { javaSourceSet -> javaSourceSet.java.srcDir(layout.projectDirectory.dir("../srcJvmMain")) } } }
+    sourceSets.jvmMain.configure { resources.srcDir(layout.projectDirectory.dir("../resourcesJvmMain")) }
+    sourceSets.jvmTest.configure { kotlin.srcDir(layout.projectDirectory.dir("../testJvmTest")) }
+    configureAtMostOneJvmTargetOrThrow { compilations.named("test") { withJavaSourceSet { javaSourceSet -> javaSourceSet.java.srcDir(layout.projectDirectory.dir("../testJvmTest")) } } }
+    sourceSets.jvmTest.configure { resources.srcDir(layout.projectDirectory.dir("../testResourcesJvmTest")) }
+  }
+  sourceSets.commonMain.dependencies {
+    api(jps.junit.junit399551665.get().let { "${it.group}:${it.name}:${it.version}" }) {
+      exclude(group = "org.hamcrest", module = "hamcrest-core")
+    }
+    api(jps.org.hamcrest.hamcrest1545074716.get().let { "${it.group}:${it.name}:${it.version}" }) {
+      isTransitive = false
+    }
+  }
+  // KOTLIN__MARKER_END
+}
