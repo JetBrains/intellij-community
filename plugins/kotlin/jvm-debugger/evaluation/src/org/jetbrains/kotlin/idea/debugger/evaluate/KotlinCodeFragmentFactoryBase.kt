80a39bd4fc3e02beadd7d819e689940c2ae88c83
Maria Sokolova <maria.sokolova@jetbrains.com>
2025-09-04 17:01:32 +0200

    [debugger evaluation] Use NewJavaToKotlinConverter to convert evaluatable javaExpression
    GitOrigin-RevId: b728e70f87f8b75e9aa7347574d0396056c8b5db

--- plugins/kotlin/jvm-debugger/evaluation/src/org/jetbrains/kotlin/idea/debugger/evaluate/KotlinCodeFragmentFactoryBase.kt
+++ plugins/kotlin/jvm-debugger/evaluation/src/org/jetbrains/kotlin/idea/debugger/evaluate/KotlinCodeFragmentFactoryBase.kt
@@ -5,14 +5,16 @@ import com.intellij.debugger.engine.JavaDebuggerCodeFragmentFactory
 import com.intellij.debugger.engine.evaluation.TextWithImports
 import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.project.Project
 import com.intellij.psi.*
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.PsiTypesUtil
 import com.intellij.util.IncorrectOperationException
 import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.base.util.module
 import org.jetbrains.kotlin.j2k.ConverterSettings
-import org.jetbrains.kotlin.j2k.OldJavaToKotlinConverter
+import org.jetbrains.kotlin.nj2k.NewJavaToKotlinConverter
 import org.jetbrains.kotlin.psi.KtBlockCodeFragment
 import org.jetbrains.kotlin.psi.KtCodeFragment
 import org.jetbrains.kotlin.psi.KtExpressionCodeFragment
@@ -38,26 +40,27 @@ abstract class KotlinCodeFragmentFactoryBase : JavaDebuggerCodeFragmentFactory()
         } catch (_: IncorrectOperationException) {
             null
         }
-        val convertedFragment: KtExpressionCodeFragment? = runReadAction {
-            try {
-                val converter = OldJavaToKotlinConverter(project, ConverterSettings.defaultSettings)
-                val res = converter.elementsToKotlin(listOfNotNull(javaExpression))
-                val newText = res.results.singleOrNull()?.text
-                val newImports = importList?.text
-                newText?.let {
-                    KtExpressionCodeFragment(
-                        project,
-                        kotlinCodeFragment.name,
-                        it,
-                        newImports,
-                        kotlinCodeFragment.context
-                    )
-                }
-            } catch (e: Exception) {
-                // ignored because text can be invalid
-                LOG.error("Couldn't convert expression:\n`${javaExpression?.text}`", e)
-                null
+        val convertedFragment = try {
+            val converter = NewJavaToKotlinConverter(project, javaExpression?.module, ConverterSettings.defaultSettings)
+            val convertedExpression = runReadAction {
+                converter.elementsToKotlin(listOfNotNull(javaExpression))
             }
+            val newText = convertedExpression.results.singleOrNull()?.text
+            val newImports = importList?.text
+            newText?.let {
+                KtExpressionCodeFragment(
+                    project,
+                    kotlinCodeFragment.name,
+                    it,
+                    newImports,
+                    kotlinCodeFragment.context
+                )
+            }
+        } catch (e: ProcessCanceledException) {
+            throw e
+        } catch(e: Throwable) {
+            LOG.error("Couldn't convert expression:\n`${javaExpression?.text}`", e)
+            null
         }
         return convertedFragment ?: kotlinCodeFragment
     }
