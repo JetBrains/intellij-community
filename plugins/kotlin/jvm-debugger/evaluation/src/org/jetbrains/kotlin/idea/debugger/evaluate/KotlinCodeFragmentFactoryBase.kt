0749b29ec9d207865040d6a07c713dd667221e65
Maria Sokolova <maria.sokolova@jetbrains.com>
2025-09-04 18:17:33 +0200

    [debugger evaluation] Refactoring: shared K1/K2 logic of KotlinCodeFragmentFactory
    GitOrigin-RevId: 645c397b59fffb3cd58a6d23a491a96c141c215e

---
+++ plugins/kotlin/jvm-debugger/evaluation/src/org/jetbrains/kotlin/idea/debugger/evaluate/KotlinCodeFragmentFactoryBase.kt
@@ -0,0 +1,94 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.debugger.evaluate
+
+import com.intellij.debugger.engine.JavaDebuggerCodeFragmentFactory
+import com.intellij.debugger.engine.evaluation.TextWithImports
+import com.intellij.ide.highlighter.JavaFileType
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.project.Project
+import com.intellij.psi.*
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.PsiTypesUtil
+import com.intellij.util.IncorrectOperationException
+import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.j2k.ConverterSettings
+import org.jetbrains.kotlin.j2k.OldJavaToKotlinConverter
+import org.jetbrains.kotlin.psi.KtBlockCodeFragment
+import org.jetbrains.kotlin.psi.KtCodeFragment
+import org.jetbrains.kotlin.psi.KtExpressionCodeFragment
+
+abstract class KotlinCodeFragmentFactoryBase : JavaDebuggerCodeFragmentFactory() {
+
+    abstract fun KtBlockCodeFragment.registerCodeFragmentExtensions(contextElement: PsiElement?)
+
+    override fun createPresentationPsiCodeFragmentImpl(item: TextWithImports, context: PsiElement?, project: Project): JavaCodeFragment? {
+        val kotlinCodeFragment = createPsiCodeFragment(item, context, project) ?: return null
+        if (!PsiTreeUtil.hasErrorElements(kotlinCodeFragment) || kotlinCodeFragment !is KtCodeFragment) return kotlinCodeFragment
+        val javaExpression = try {
+            PsiElementFactory.getInstance(project).createExpressionFromText(item.text, context)
+        } catch (e: IncorrectOperationException) {
+            null
+        }
+        val importList = try {
+            kotlinCodeFragment.importsAsImportList()?.let {
+                (PsiFileFactory.getInstance(project).createFileFromText(
+                    "dummy.java", JavaFileType.INSTANCE, it.text
+                ) as? PsiJavaFile)?.importList
+            }
+        } catch (e: IncorrectOperationException) {
+            null
+        }
+        val convertedFragment: KtExpressionCodeFragment? = runReadAction {
+            try {
+                val converter = OldJavaToKotlinConverter(project, ConverterSettings.defaultSettings)
+                val res = converter.elementsToKotlin(listOfNotNull(javaExpression))
+                val newText = res.results.singleOrNull()?.text
+                val newImports = importList?.text
+                newText?.let {
+                    KtExpressionCodeFragment(
+                        project,
+                        kotlinCodeFragment.name,
+                        it,
+                        newImports,
+                        kotlinCodeFragment.context
+                    )
+                }
+            } catch (e: Exception) {
+                // ignored because text can be invalid
+                LOG.error("Couldn't convert expression:\n`${javaExpression?.text}`", e)
+                null
+            }
+        }
+        return convertedFragment ?: kotlinCodeFragment
+    }
+
+    override fun getFileType(): KotlinFileType {
+        return KotlinFileType.INSTANCE
+    }
+
+    override fun getEvaluatorBuilder(): KotlinEvaluatorBuilder {
+        return KotlinEvaluatorBuilder
+    }
+
+    protected fun initImports(imports: String?): String? {
+        if (!imports.isNullOrEmpty()) {
+            return imports.split(KtCodeFragment.IMPORT_SEPARATOR)
+                .mapNotNull { fixImportIfNeeded(it) }
+                .joinToString(KtCodeFragment.IMPORT_SEPARATOR)
+        }
+        return null
+    }
+
+    private fun fixImportIfNeeded(import: String): String? {
+        // skip arrays
+        if (import.endsWith("[]")) {
+            return fixImportIfNeeded(import.removeSuffix("[]").trim())
+        }
+
+        // skip primitive types
+        if (PsiTypesUtil.boxIfPossible(import) != import) {
+            return null
+        }
+        return import
+    }
+}
--- plugins/kotlin/jvm-debugger/evaluation/src/org/jetbrains/kotlin/idea/debugger/evaluate/KotlinK2CodeFragmentFactory.kt
+++ plugins/kotlin/jvm-debugger/evaluation/src/org/jetbrains/kotlin/idea/debugger/evaluate/KotlinK2CodeFragmentFactory.kt
@@ -1,28 +1,43 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.debugger.evaluate
 
-import com.intellij.debugger.engine.JavaDebuggerCodeFragmentFactory
+import com.intellij.debugger.DebuggerManagerEx
 import com.intellij.debugger.engine.evaluation.TextWithImports
+import com.intellij.openapi.progress.EmptyProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.psi.JavaCodeFragment
 import com.intellij.psi.PsiElement
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.withTimeout
+import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
 import org.jetbrains.kotlin.analysis.api.KaImplementationDetail
 import org.jetbrains.kotlin.analysis.api.projectStructure.analysisContextModule
+import org.jetbrains.kotlin.analysis.api.types.KaType
+import org.jetbrains.kotlin.analysis.api.types.KaTypePointer
 import org.jetbrains.kotlin.idea.KotlinFileType
 import org.jetbrains.kotlin.idea.base.facet.implementingModules
 import org.jetbrains.kotlin.idea.base.facet.platform.platform
 import org.jetbrains.kotlin.idea.base.projectStructure.toKaSourceModuleWithElementSourceModuleKindOrProduction
 import org.jetbrains.kotlin.idea.base.util.module
 import org.jetbrains.kotlin.idea.debugger.core.CodeFragmentContextTuner
+import org.jetbrains.kotlin.idea.debugger.evaluate.util.KotlinK2CodeFragmentUtils
 import org.jetbrains.kotlin.platform.jvm.isJvm
 import org.jetbrains.kotlin.psi.KtBlockCodeFragment
+import org.jetbrains.kotlin.psi.KtExpression
+import kotlin.collections.filter
+import kotlin.collections.firstOrNull
+import kotlin.collections.orEmpty
+import kotlin.time.Duration.Companion.milliseconds
 
-class KotlinK2CodeFragmentFactory : JavaDebuggerCodeFragmentFactory() {
+class KotlinK2CodeFragmentFactory : KotlinCodeFragmentFactoryBase() {
     @OptIn(KaImplementationDetail::class)
     override fun createPsiCodeFragmentImpl(item: TextWithImports, context: PsiElement?, project: Project): JavaCodeFragment {
         val contextElement = CodeFragmentContextTuner.getInstance().tuneContextElement(context)
-
-        return KtBlockCodeFragment(project, "fragment.kt", item.text, item.imports, contextElement).apply {
+        val codeFragment = KtBlockCodeFragment(project, "fragment.kt", item.text, initImports(item.imports), contextElement).apply {
             /*
             Handle support for KMP:
             If the given module has refining (modules that have a 'refines' edge to this module) modules,
@@ -37,21 +52,17 @@ class KotlinK2CodeFragmentFactory : JavaDebuggerCodeFragmentFactory() {
                 virtualFile.analysisContextModule = jvmLeafModule?.toKaSourceModuleWithElementSourceModuleKindOrProduction(contextElement)
             }
         }
+        codeFragment.registerCodeFragmentExtensions(contextElement)
+        return codeFragment
     }
 
     override fun createPresentationPsiCodeFragmentImpl(item: TextWithImports, context: PsiElement?, project: Project): JavaCodeFragment? {
         return createPsiCodeFragment(item, context, project)
     }
 
+    override fun KtBlockCodeFragment.registerCodeFragmentExtensions(contextElement: PsiElement?) {}
+
     override fun isContextAccepted(contextElement: PsiElement?): Boolean {
         return contextElement?.language == KotlinFileType.INSTANCE.language
-    }
-
-    override fun getFileType(): KotlinFileType {
-        return KotlinFileType.INSTANCE
-    }
-
-    override fun getEvaluatorBuilder(): KotlinEvaluatorBuilder {
-        return KotlinEvaluatorBuilder
     }
 }
