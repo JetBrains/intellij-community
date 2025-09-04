a6463608fe342f39184e3b3f34f5e7350ff54caf
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-09-04 16:40:36 +0300

    [terminal] IJPL-176779 Refactor TerminalCompletionTest
    - Do some renaming and rearranging.
    - Edit order of parameters in assertions: put expected and actual arguments to the correct places.
    
    GitOrigin-RevId: c4a223a582bdcf14c2651c210d3fd49612827043

--- plugins/terminal/tests/src/com/intellij/terminal/tests/reworked/frontend/TerminalCompletionTest.kt
+++ plugins/terminal/tests/src/com/intellij/terminal/tests/reworked/frontend/TerminalCompletionTest.kt
@@ -30,114 +30,112 @@ class TerminalCompletionTest : BasePlatformTestCase() {
 
   @Test
   fun `test completions list filtered on typing`() {
-    val terminalCompletionFixture = TerminalCompletionFixture(project, testRootDisposable)
-    terminalCompletionFixture.mockTestShellCommand(testCommandSpec)
-    terminalCompletionFixture.type("test_cmd ")
-    terminalCompletionFixture.callCompletionPopup()
-    assertEquals(true, terminalCompletionFixture.isLookupActive())
-    val beforeResult = terminalCompletionFixture.getLookupElements()
-    val beforeResultStrings = beforeResult.map { it.lookupString }
-    assertSameElements(listOf("bind", "branch", "build", "set", "show", "start", "status", "stop", "sync"),
-                 beforeResultStrings)
+    val fixture = createFixture()
 
-    terminalCompletionFixture.type("s")
-    val result = terminalCompletionFixture.getLookupElements()
-    val resultStrings = result.map { it.lookupString }
-    assertSameElements(listOf("set", "show", "start", "status", "stop", "sync"), resultStrings)
-    terminalCompletionFixture.type("t")
-    assertSameElements(listOf("start", "status", "stop"), terminalCompletionFixture.getLookupElements().map { it.lookupString })
-
+    fixture.type("test_cmd ")
+    fixture.callCompletionPopup()
+    assertEquals(true, fixture.isLookupActive())
+    assertSameElements(fixture.getLookupElements().map { it.lookupString },
+                       listOf("bind", "branch", "build", "set", "show", "start", "status", "stop", "sync"))
+    fixture.type("s")
+    assertSameElements(fixture.getLookupElements().map { it.lookupString },
+                       listOf("set", "show", "start", "status", "stop", "sync"))
+    fixture.type("t")
+    assertSameElements(fixture.getLookupElements().map { it.lookupString },
+                       listOf("start", "status", "stop"))
   }
 
   @Test
   fun `test selection returns to original item after down and up actions`() {
-    val terminalCompletionFixture = TerminalCompletionFixture(project, testRootDisposable)
-    terminalCompletionFixture.mockTestShellCommand(testCommandSpec)
-    terminalCompletionFixture.type("test_cmd ")
-    terminalCompletionFixture.callCompletionPopup()
-    assertSameElements(listOf("bind", "branch", "build", "set", "show", "start", "status", "stop", "sync"),
-                 terminalCompletionFixture.getLookupElements().map { it.lookupString })
+    val fixture = createFixture()
 
-    val firstElement = terminalCompletionFixture.getCurrentItem()
-    terminalCompletionFixture.downCompletionPopup()
-    val secondElement = terminalCompletionFixture.getCurrentItem()
-    terminalCompletionFixture.upCompletionPopup()
-    val fistAfterElement = terminalCompletionFixture.getCurrentItem()
+    fixture.type("test_cmd ")
+    fixture.callCompletionPopup()
+    assertSameElements(fixture.getLookupElements().map { it.lookupString },
+                       listOf("bind", "branch", "build", "set", "show", "start", "status", "stop", "sync"))
 
-    assertEquals(firstElement, fistAfterElement)
-    assertNotEquals(secondElement, fistAfterElement)
+    val firstElement = fixture.getCurrentItem()
+    fixture.downCompletionPopup()
+    val secondElement = fixture.getCurrentItem()
+    fixture.upCompletionPopup()
+    val firstAfterElement = fixture.getCurrentItem()
+
+    assertEquals(firstElement, firstAfterElement)
+    assertNotEquals(secondElement, firstAfterElement)
   }
 
   @Test
   fun `test completion list is re-filtered when caret moves over prefix to the left, right`() {
-    val terminalCompletionFixture = TerminalCompletionFixture(project, testRootDisposable)
-    terminalCompletionFixture.mockTestShellCommand(testCommandSpec)
-    terminalCompletionFixture.type("test_cmd ")
-    terminalCompletionFixture.callCompletionPopup()
+    val fixture = createFixture()
 
-    terminalCompletionFixture.type("st")
-    val startResult = terminalCompletionFixture.getLookupElements()
-    assertSameElements(listOf("start", "status", "stop"), startResult.map { it.lookupString })
+    fixture.type("test_cmd ")
+    fixture.callCompletionPopup()
 
-    terminalCompletionFixture.pressKey(VK_LEFT)
-    val afterFirstLeftResult = terminalCompletionFixture.getLookupElements()
-    assertSameElements(listOf("set", "show", "start", "status", "stop", "sync"),
-                 afterFirstLeftResult.map { it.lookupString })
-    terminalCompletionFixture.pressKey(VK_LEFT)
-    val afterSecondLeftResult = terminalCompletionFixture.getLookupElements()
-    assertSameElements(listOf("bind", "branch", "build", "set", "show", "start", "status", "stop", "sync"),
-                 afterSecondLeftResult.map { it.lookupString })
+    fixture.type("st")
+    val startResult = fixture.getLookupElements()
+    assertSameElements(startResult.map { it.lookupString }, listOf("start", "status", "stop"))
 
+    fixture.pressKey(VK_LEFT)
+    val afterFirstLeftResult = fixture.getLookupElements()
+    assertSameElements(afterFirstLeftResult.map { it.lookupString },
+                       listOf("set", "show", "start", "status", "stop", "sync"))
 
-    terminalCompletionFixture.pressKey(VK_RIGHT)
-    val afterRightResult = terminalCompletionFixture.getLookupElements()
+    fixture.pressKey(VK_LEFT)
+    val afterSecondLeftResult = fixture.getLookupElements()
+    assertSameElements(afterSecondLeftResult.map { it.lookupString },
+                       listOf("bind", "branch", "build", "set", "show", "start", "status", "stop", "sync"))
+
+    fixture.pressKey(VK_RIGHT)
+    val afterRightResult = fixture.getLookupElements()
     assertEquals(afterFirstLeftResult, afterRightResult)
 
-    terminalCompletionFixture.pressKey(VK_RIGHT)
-    val afterSecondRightResult = terminalCompletionFixture.getLookupElements()
+    fixture.pressKey(VK_RIGHT)
+    val afterSecondRightResult = fixture.getLookupElements()
     assertEquals(startResult, afterSecondRightResult)
   }
 
   @Test
   fun `test lookup remains active when caret moves into text typed before call popup`() {
-    val terminalCompletionFixture = TerminalCompletionFixture(project, testRootDisposable)
-    terminalCompletionFixture.mockTestShellCommand(testCommandSpec)
-    terminalCompletionFixture.type("test_cmd st")
-    terminalCompletionFixture.callCompletionPopup()
+    val fixture = createFixture()
 
-    val startResult = terminalCompletionFixture.getLookupElements()
-    assertSameElements(listOf("start", "status", "stop"),
-                 startResult.map { it.lookupString })
-    terminalCompletionFixture.pressKey(VK_LEFT)
-    assertSameElements(listOf("set", "show", "start", "status", "stop", "sync"),
-                 terminalCompletionFixture.getLookupElements().map { it.lookupString })
-    terminalCompletionFixture.pressKey(VK_RIGHT)
-    assertSameElements(startResult.map { it.lookupString },
-                 terminalCompletionFixture.getLookupElements().map { it.lookupString })
+    fixture.type("test_cmd st")
+    fixture.callCompletionPopup()
+
+    val startResult = fixture.getLookupElements().map { it.lookupString }
+    assertSameElements(startResult, listOf("start", "status", "stop"))
+    fixture.pressKey(VK_LEFT)
+    assertSameElements(fixture.getLookupElements().map { it.lookupString },
+                       listOf("set", "show", "start", "status", "stop", "sync"))
+    fixture.pressKey(VK_RIGHT)
+    assertSameElements(fixture.getLookupElements().map { it.lookupString }, startResult)
   }
 
   @Test
   fun `test completion list is correctly re-filtered after pressing backspace`() {
-    val terminalCompletionFixture = TerminalCompletionFixture(project, testRootDisposable)
-    terminalCompletionFixture.mockTestShellCommand(testCommandSpec)
-    terminalCompletionFixture.type("test_cmd st")
-    terminalCompletionFixture.callCompletionPopup()
+    val fixture = createFixture()
 
-    val startResult = terminalCompletionFixture.getLookupElements()
-    assertSameElements(listOf("start", "status", "stop"),
-                 startResult.map { it.lookupString })
+    fixture.type("test_cmd st")
+    fixture.callCompletionPopup()
 
-    terminalCompletionFixture.type("a")
-    assertSameElements(listOf("start", "status"),
-                 terminalCompletionFixture.getLookupElements().map { it.lookupString })
+    assertSameElements(fixture.getLookupElements().map { it.lookupString },
+                       listOf("start", "status", "stop"))
 
-    terminalCompletionFixture.pressKey(VK_BACK_SPACE)
-    assertSameElements(listOf("start", "status", "stop"),
-                 terminalCompletionFixture.getLookupElements().map { it.lookupString })
+    fixture.type("a")
+    assertSameElements(fixture.getLookupElements().map { it.lookupString },
+                       listOf("start", "status"))
 
-    terminalCompletionFixture.pressKey(VK_BACK_SPACE)
-    assertSameElements(listOf("set", "show", "start", "status", "stop", "sync"),
-                 terminalCompletionFixture.getLookupElements().map { it.lookupString })
+    fixture.pressKey(VK_BACK_SPACE)
+    assertSameElements(fixture.getLookupElements().map { it.lookupString },
+                       listOf("start", "status", "stop"))
+
+    fixture.pressKey(VK_BACK_SPACE)
+    assertSameElements(fixture.getLookupElements().map { it.lookupString },
+                       listOf("set", "show", "start", "status", "stop", "sync"))
   }
 
+  private fun createFixture(): TerminalCompletionFixture {
+    val fixture = TerminalCompletionFixture(project, testRootDisposable)
+    fixture.mockTestShellCommand(testCommandSpec)
+    return fixture
+  }
 }
