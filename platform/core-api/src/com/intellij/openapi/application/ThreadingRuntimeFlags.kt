0ed87ba1d2169e913faf88bc99413b53dc431853
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-09-04 12:52:01 +0200

    [threading] IJPL-205724: Enable quicker processing of events in SuvorovProgress
    GitOrigin-RevId: 0e417f3e06cef2c8eac6d58bff46b47e5d103975

--- platform/core-api/src/com/intellij/openapi/application/ThreadingRuntimeFlags.kt
+++ platform/core-api/src/com/intellij/openapi/application/ThreadingRuntimeFlags.kt
@@ -39,7 +39,7 @@ val reportInvalidActionChains: Boolean = System.getProperty("ijpl.report.invalid
 val installSuvorovProgress: Boolean = System.getProperty("ide.install.suvorov.progress", "true").toBoolean()
 
 @get:ApiStatus.Internal
-val useDebouncedDrawingInSuvorovProgress: Boolean = System.getProperty("ide.suvorov.progress.debounced.drawing", "false").toBoolean()
+val useDebouncedDrawingInSuvorovProgress: Boolean = System.getProperty("ide.suvorov.progress.debounced.drawing", "true").toBoolean()
 
 /**
  * Represents the deadline before blocking read lock acquisition starts compensating parallelism for coroutine worker threads
