85f0de2e6ecc165f5dd858d0c57bf1ca752a5d87
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-09-04 12:49:34 +0200

    [threading] IJPL-205937: Fix incorrect transition from UI to UiWithModelAccessImmediate
    GitOrigin-RevId: e55c9eaeebf6ce0ff34bbfb7d3a3ea5a5913b51d

--- platform/core-impl/src/com/intellij/openapi/application/impl/EdtCoroutineDispatcher.kt
+++ platform/core-impl/src/com/intellij/openapi/application/impl/EdtCoroutineDispatcher.kt
@@ -112,7 +112,10 @@ private class ImmediateEdtCoroutineDispatcher(type: EdtDispatcherKind) : EdtCoro
       // `Dispatchers.Main.immediate` must look only at the thread where it is executing.
       // This is needed to support 3rd party libraries which use this dispatcher for getting the UI thread
       // Relaxed UI dispatcher is indifferent to locks, so it can run in-place.
-      EdtDispatcherKind.MAIN, EdtDispatcherKind.LAX_UI -> false
+      EdtDispatcherKind.MAIN -> false
+      // Immediate relaxed dispatcher should do redispatch if it runs under Dispatchers.UI
+      // that's because the context of Dispatchers.UI forbids taking locks, so we need to get into an appropriate context
+      EdtDispatcherKind.LAX_UI -> ApplicationManager.getApplication().isLockingProhibited != null
       // `Dispatchers.EdtImmediate` must perform dispatch when invoked on a thread without locks, because it needs to get into correct context.
       EdtDispatcherKind.EDT -> !ApplicationManager.getApplication().isWriteIntentLockAcquired
       // `Dispatchers.UIImmediate` must perform dispatch when invoked on a thread with locks, because it needs to escape locking and forbid using them inside.
--- platform/platform-tests/testSrc/com/intellij/openapi/application/impl/EdtCoroutineDispatcherTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/application/impl/EdtCoroutineDispatcherTest.kt
@@ -28,7 +28,6 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.concurrent.atomic.AtomicInteger
-import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
 import kotlin.math.min
 import kotlin.test.assertTrue
@@ -332,6 +331,26 @@ class EdtCoroutineDispatcherTest {
       val currentTrace = getCurrentTrace()
       launch(Dispatchers.UiImmediate) {
         assertTraceContinuation(currentTrace)
+      }
+    }
+  }
+
+  @Test
+  fun `immediate relaxed ui dispatcher performs dispatch when locks are forbidden`(): Unit = timeoutRunBlocking {
+    withContext(Dispatchers.UI) {
+      assertThat(application.isReadAccessAllowed).isFalse
+      val currentTrace = getCurrentTrace()
+      launch(Dispatchers.UiWithModelAccessImmediate) {
+        assertNotTraceContinuation(currentTrace)
+        runReadAction { }
+      }
+    }
+    withContext(Dispatchers.UiImmediate) {
+      assertThat(application.isReadAccessAllowed).isFalse
+      val currentTrace = getCurrentTrace()
+      launch(Dispatchers.UiWithModelAccessImmediate) {
+        assertNotTraceContinuation(currentTrace)
+        runReadAction { }
       }
     }
   }
