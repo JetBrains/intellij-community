ccc6c0ee82ce2054503e66c710606c8fa0b48bdb
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2025-09-04 11:36:14 +0200

    [java] IDEA-378236 Double-clicking the closing bracket line of a switch block doesn’t select the entire block inside the brackets
    - exclude braces from selection
    
    GitOrigin-RevId: 1a0d1d6a237e9069b60321668c2ef9f570a82464

---
+++ java/java-tests/testData/codeInsight/selectWordAction/switchEnd2/after1.java
@@ -0,0 +1,15 @@
+public class TestSwitchSelect
+{
+    public static void main(String[] args)
+    {
+        switch (type) <selection>{
+         case 1:
+            break;
+
+         case 2:
+            break;
+         default:
+            break;
+      }<caret></selection>
+    }
+}
