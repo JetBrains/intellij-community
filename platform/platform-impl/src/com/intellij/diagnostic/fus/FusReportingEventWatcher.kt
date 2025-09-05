2002e12a388f008f93db846c84e34edaf4a139ee
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-09-05 11:25:40 +0200

    [threading] IJPL-206130: Temporarily disable recording of freeze chains
    GitOrigin-RevId: 30ce320c13a8fbc5307a2c1e4d0cea71933b9551

--- platform/platform-impl/src/com/intellij/diagnostic/fus/FusReportingEventWatcher.kt
+++ platform/platform-impl/src/com/intellij/diagnostic/fus/FusReportingEventWatcher.kt
@@ -33,7 +33,7 @@ class FusReportingEventWatcher : PreciseEventWatcher() {
   init {
     // preload service so that it starts monitoring locks
     service<EdtLockAggregatingMonitor>()
-    if (SystemProperties.getBooleanProperty("ui.freeze.chain.reporting.enabled", true)) {
+    if (SystemProperties.getBooleanProperty("ui.freeze.chain.reporting.enabled", false)) {
       // start freeze chain monitor to report sequences of micro-freezes
       service<EdtFreezeChainMonitor>()
     }
