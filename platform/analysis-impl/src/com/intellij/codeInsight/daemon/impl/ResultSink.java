3ffec6e86b617808f4782b7fcde947d59754efd5
Alexey Kudravtsev <Alexey.Kudravtsev@jetbrains.com>
2025-09-05 16:40:33 +0200

    more notnulls to completely rule out IJPL-150067 NullPointerException on HighlightInfo.getSeverity
    GitOrigin-RevId: 0aba97184566cd0e65590a50a674d91fd06acd72

--- platform/analysis-impl/src/com/intellij/codeInsight/daemon/impl/ResultSink.java
+++ platform/analysis-impl/src/com/intellij/codeInsight/daemon/impl/ResultSink.java
@@ -12,5 +12,5 @@ import java.util.List;
 interface ResultSink {
   void accept(@NotNull Object toolId,
               @NotNull PsiElement element,
-              @NotNull List<? extends HighlightInfo> info);
+              @NotNull List<? extends @NotNull HighlightInfo> info);
 }
