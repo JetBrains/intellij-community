9b38c2131db4b322c495798f71084d89252f731b
Aleksandra Olemskaia <aleksandra.olemskaia@jetbrains.com>
2025-09-05 14:54:34 +0200

    [settings-sync] Improve "Choose Provider" dialog UX
    - change OK button text to "Log in..."
    - remove "Please" from the dialog text
    - disable the login button if no provider is selected
    
    
    (cherry picked from commit 86e9c2e3751af5411f9e8d086cc780abe1c09e38)
    
    IJ-CR-174183
    
    GitOrigin-RevId: 52217d3034e992916a3bd3101169681558bf6fc0

--- platform/settings-sync-core/src/com/intellij/settingsSync/core/config/SettingsSyncConfigurable.kt
+++ platform/settings-sync-core/src/com/intellij/settingsSync/core/config/SettingsSyncConfigurable.kt
@@ -60,6 +60,7 @@ import com.intellij.util.ui.JBFont
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.NamedColorUtil
 import kotlinx.coroutines.*
+import java.awt.event.ActionEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.util.concurrent.CancellationException
@@ -951,6 +952,16 @@ internal class SettingsSyncConfigurable(private val coroutineScope: CoroutineSco
   private class AddAccountDialog(parent: JComponent) : DialogWrapper(parent, false) {
 
     var providerCode: String = ""
+    private val loginAction = object : DialogWrapperAction(message("enable.sync.choose.data.provider.login.button")) {
+      init {
+        putValue(DEFAULT_ACTION, true)
+        isEnabled = false
+      }
+
+      override fun doAction(e: ActionEvent?) {
+        close(OK_EXIT_CODE)
+      }
+    }
 
     init {
       title = message("title.settings.sync")
@@ -1005,12 +1016,17 @@ internal class SettingsSyncConfigurable(private val coroutineScope: CoroutineSco
       }
     }
 
+    override fun createActions(): Array<Action> =
+      arrayOf(cancelAction, loginAction)
+
+
     private fun createRadioButtonPanelForProvider(provider: SettingsSyncCommunicatorProvider, buttonGroup: ButtonGroup): JPanel {
       val radioButton = JBRadioButton().apply {
         actionCommand = provider.providerCode
         addActionListener {
           if (isSelected) {
             providerCode = provider.providerCode
+            loginAction.isEnabled = true
           }
         }
       }
