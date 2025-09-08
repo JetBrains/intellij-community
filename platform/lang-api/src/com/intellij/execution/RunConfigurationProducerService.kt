f61725f22d88503bb80c8b5ddea2f762886579b8
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-09-08 17:42:01 +0300

    BAZEL-2453 introduce RunConfigurationProducerSuppressor
    GitOrigin-RevId: f117d59fab00d6624a856c99b4ba73c1da0f4f99

--- platform/lang-api/src/com/intellij/execution/RunConfigurationProducerService.kt
+++ platform/lang-api/src/com/intellij/execution/RunConfigurationProducerService.kt
@@ -3,8 +3,18 @@ package com.intellij.execution
 
 import com.intellij.execution.actions.RunConfigurationProducer
 import com.intellij.openapi.components.*
+import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
 import org.jetbrains.annotations.ApiStatus
+
+@ApiStatus.Experimental
+interface RunConfigurationProducerSuppressor {
+  companion object {
+    internal val EP_NAME = ExtensionPointName<RunConfigurationProducerSuppressor>("com.intellij.runConfigurationProducerSuppressor")
+  }
+
+  fun shouldSuppress(producer: RunConfigurationProducer<*>): Boolean
+}
 
 /**
  * Project component that keeps track of [RunConfigurationProducer] implementations that should be ignored for a given project. All
@@ -32,9 +42,19 @@ class RunConfigurationProducerService : PersistentStateComponent<RunConfiguratio
     val ignoredProducers: MutableSet<String?> = HashSet<String?>()
   }
 
+  @Deprecated("Use RunConfigurationProducerSuppressor")
   fun addIgnoredProducer(ignoredProducer: Class<out RunConfigurationProducer<*>?>) {
     myState.ignoredProducers.add(ignoredProducer.getName())
   }
 
-  fun isIgnored(producer: RunConfigurationProducer<*>): Boolean = myState.ignoredProducers.contains(producer.javaClass.getName())
+  fun isIgnored(producer: RunConfigurationProducer<*>): Boolean {
+    for (suppressor in RunConfigurationProducerSuppressor.EP_NAME.extensionList) {
+      if (suppressor.shouldSuppress(producer)) {
+        return true
+      }
+    }
+
+    val ignoredProducers = myState.ignoredProducers
+    return !ignoredProducers.isEmpty() && ignoredProducers.contains(producer.javaClass.getName())
+  }
 }
