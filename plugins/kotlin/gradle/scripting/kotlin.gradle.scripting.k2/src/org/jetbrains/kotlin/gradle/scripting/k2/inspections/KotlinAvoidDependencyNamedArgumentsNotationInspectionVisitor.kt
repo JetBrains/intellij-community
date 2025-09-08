dcad25912d9162467c25aeba848360ca0979477d
Kajus Kuncaitis <kajus.kuncaitis@jetbrains.com>
2025-09-08 17:13:23 +0200

    [gradle][IDEA-376861] (kotlin) Extract common dependency related logic to a Util file
    GitOrigin-RevId: 32449f7a34dcb3696a2d272fbb06036395a612dd

--- plugins/kotlin/gradle/scripting/kotlin.gradle.scripting.k2/src/org/jetbrains/kotlin/gradle/scripting/k2/inspections/KotlinAvoidDependencyNamedArgumentsNotationInspectionVisitor.kt
+++ plugins/kotlin/gradle/scripting/kotlin.gradle.scripting.k2/src/org/jetbrains/kotlin/gradle/scripting/k2/inspections/KotlinAvoidDependencyNamedArgumentsNotationInspectionVisitor.kt
@@ -8,21 +8,14 @@ import com.intellij.codeInspection.util.IntentionFamilyName
 import com.intellij.codeInspection.util.IntentionName
 import com.intellij.modcommand.ModPsiUpdater
 import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.analysis.api.analyze
-import org.jetbrains.kotlin.analysis.api.resolution.singleFunctionCallOrNull
-import org.jetbrains.kotlin.analysis.api.resolution.symbol
-import org.jetbrains.kotlin.analysis.api.types.symbol
 import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinModCommandQuickFix
-import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.plugins.gradle.codeInspection.GradleInspectionBundle
-import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.GRADLE_API_ARTIFACTS_EXTERNAL_MODULE_DEPENDENCY
 
 class KotlinAvoidDependencyNamedArgumentsNotationInspectionVisitor(val holder: ProblemsHolder) : KtVisitorVoid() {
-    private val GRADLE_DSL_PACKAGE = FqName("org.gradle.kotlin.dsl")
-
     override fun visitCallExpression(expression: KtCallExpression) {
-        if (!isExternalDependencyDeclaration(expression)) return
+        val dependencyType = findDependencyType(expression) ?: return
+        if (dependencyType != DependencyType.NAMED_ARGUMENTS) return
         val argList = expression.valueArgumentList ?: return
         if (!argList.isPhysical) return
         val args = argList.arguments
@@ -37,22 +30,9 @@ class KotlinAvoidDependencyNamedArgumentsNotationInspectionVisitor(val holder: P
         holder.registerProblem(
             argList,
             GradleInspectionBundle.message("inspection.message.avoid.dependency.named.arguments.notation.descriptor"),
-            ProblemHighlightType.WEAK_WARNING,
+            ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
             GradleDependencyNamedArgumentsFix()
         )
-    }
-
-    private fun isExternalDependencyDeclaration(expression: KtCallExpression): Boolean {
-        return analyze(expression) {
-            val singleFunctionCallOrNull = expression.resolveToCall()?.singleFunctionCallOrNull()
-            val symbol = singleFunctionCallOrNull?.symbol ?: return@analyze false
-            symbol.callableId?.packageName == GRADLE_DSL_PACKAGE
-                    && symbol.returnType.symbol?.classId?.asSingleFqName() == FqName(GRADLE_API_ARTIFACTS_EXTERNAL_MODULE_DEPENDENCY)
-                    && symbol.valueParameters.size >= 3
-                    && symbol.valueParameters[0].name.identifier == "group"
-                    && symbol.valueParameters[1].name.identifier == "name"
-                    && symbol.valueParameters[2].name.identifier == "version"
-        }
     }
 }
 
@@ -66,18 +46,11 @@ private class GradleDependencyNamedArgumentsFix() : KotlinModCommandQuickFix<KtV
     }
 
     override fun applyFix(project: Project, element: KtValueArgumentList, updater: ModPsiUpdater) {
-        val group = findArgumentInDependency(element, "group", 0) ?: return
-        val name = findArgumentInDependency(element, "name", 1) ?: return
-        val version = findArgumentInDependency(element, "version", 2)
+        val group = findArgumentInDependency(element, "group", 0)?.text?.removeSurrounding("\"") ?: return
+        val name = findArgumentInDependency(element, "name", 1)?.text?.removeSurrounding("\"") ?: return
+        val version = findArgumentInDependency(element, "version", 2)?.text?.removeSurrounding("\"")
         element.arguments.forEach { element.removeArgument(it) }
         val stringNotationArgument = if (version != null) "\"$group:$name:$version\"" else "\"$group:$name\""
         element.addArgument(KtPsiFactory(project, true).createArgument(stringNotationArgument))
-    }
-
-    private fun findArgumentInDependency(element: KtValueArgumentList, parameterName: String, expectedIndex: Int): String? {
-        val argument = element.arguments.find {
-            it.getArgumentName()?.asName?.identifier == parameterName
-        } ?: element.arguments.getOrNull(expectedIndex)
-        return argument?.getArgumentExpression()?.text?.removeSurrounding("\"")
     }
 }
---
+++ plugins/kotlin/gradle/scripting/kotlin.gradle.scripting.k2/src/org/jetbrains/kotlin/gradle/scripting/k2/inspections/KotlinGradleDependencyUtil.kt
@@ -0,0 +1,51 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.gradle.scripting.k2.inspections
+
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.resolution.singleFunctionCallOrNull
+import org.jetbrains.kotlin.analysis.api.resolution.symbol
+import org.jetbrains.kotlin.analysis.api.types.symbol
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtValueArgumentList
+import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.GRADLE_API_ARTIFACTS_DEPENDENCY
+import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.GRADLE_API_ARTIFACTS_EXTERNAL_MODULE_DEPENDENCY
+
+internal val GRADLE_DSL_PACKAGE = FqName("org.gradle.kotlin.dsl")
+internal const val KOTLIN_GROUP_ID: String = "org.jetbrains.kotlin"
+
+internal enum class DependencyType {
+    SINGLE_ITEM, NAMED_ARGUMENTS, OTHER
+}
+
+/**
+ * @return dependency argument type or null if the expression is not a dependency call
+ */
+internal fun findDependencyType(expression: KtCallExpression): DependencyType? {
+    analyze(expression) {
+        val singleFunctionCallOrNull = expression.resolveToCall()?.singleFunctionCallOrNull()
+        val symbol = singleFunctionCallOrNull?.symbol ?: return DependencyType.OTHER
+        if (symbol.callableId?.packageName != GRADLE_DSL_PACKAGE
+            || (symbol.returnType.symbol?.classId?.asSingleFqName() != FqName(GRADLE_API_ARTIFACTS_EXTERNAL_MODULE_DEPENDENCY)
+                    && symbol.returnType.symbol?.classId?.asSingleFqName() != FqName(GRADLE_API_ARTIFACTS_DEPENDENCY))
+        ) {
+            return null
+        }
+        val parameters = symbol.valueParameters
+        if (parameters.isEmpty()) return DependencyType.OTHER
+        val firstParameter = parameters.first()
+        when (firstParameter.name.identifier) {
+            "group" -> return DependencyType.NAMED_ARGUMENTS
+            "dependencyNotation" -> return DependencyType.SINGLE_ITEM
+            else -> return DependencyType.OTHER
+        }
+    }
+}
+
+internal fun findArgumentInDependency(element: KtValueArgumentList, parameterName: String, expectedIndex: Int): KtExpression? {
+    val argument = element.arguments.find {
+        it.getArgumentName()?.asName?.identifier == parameterName
+    } ?: element.arguments.getOrNull(expectedIndex)
+    return argument?.getArgumentExpression()
+}
