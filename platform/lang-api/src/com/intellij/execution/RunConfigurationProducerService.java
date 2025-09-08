830db14da1502370d15788db7d6a288dda8209e4
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-09-08 15:05:10 +0300

    BAZEL-2453 mark RunConfigurationProducersDisabler as internal API
    GitOrigin-RevId: 0a7ef3d21ed92a22ca50421c8d80152ba949a03b

--- platform/lang-api/src/com/intellij/execution/RunConfigurationProducerService.java
+++ platform/lang-api/src/com/intellij/execution/RunConfigurationProducerService.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2015 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution;
 
 import com.intellij.execution.actions.RunConfigurationProducer;
@@ -21,6 +7,7 @@ import com.intellij.openapi.components.Service;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashSet;
@@ -32,6 +19,7 @@ import java.util.Set;
  */
 @Service(Service.Level.PROJECT)
 @State(name = "RunConfigurationProducerService", storages = @Storage("runConfigurations.xml"))
+@ApiStatus.Internal
 public final class RunConfigurationProducerService implements PersistentStateComponent<RunConfigurationProducerService.State> {
   private State myState = new State();
 
@@ -46,7 +34,7 @@ public final class RunConfigurationProducerService implements PersistentStateCom
   }
 
   public static final class State {
-    public Set<String> ignoredProducers = new HashSet<>();
+    public final Set<String> ignoredProducers = new HashSet<>();
   }
 
   public static @NotNull RunConfigurationProducerService getInstance(@NotNull Project project) {
@@ -57,7 +45,7 @@ public final class RunConfigurationProducerService implements PersistentStateCom
     myState.ignoredProducers.add(ignoredProducer.getName());
   }
 
-  public boolean isIgnored(RunConfigurationProducer<?> producer) {
+  public boolean isIgnored(@NotNull RunConfigurationProducer<?> producer) {
     return myState.ignoredProducers.contains(producer.getClass().getName());
   }
 }
--- platform/lang-impl/src/com/intellij/execution/RunConfigurationConfigurableAdapter.java
+++ platform/lang-impl/src/com/intellij/execution/RunConfigurationConfigurableAdapter.java
@@ -1,11 +1,13 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution;
 
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.options.SettingsEditorConfigurable;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
+@ApiStatus.Internal
 public final class RunConfigurationConfigurableAdapter<T extends RunConfiguration> extends SettingsEditorConfigurable<T>{
   public RunConfigurationConfigurableAdapter(@NotNull SettingsEditor<T> settingsEditor, @NotNull T configuration) {
     super(settingsEditor, configuration);
