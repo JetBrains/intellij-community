c7104b975b20b86aa92143b988ff390c6461a26a
Roman Chertishchev <roman.chertishchev@jetbrains.com>
2025-09-10 17:31:51 +0200

    LLM-18251: add per provider storage for finished completions
    (cherry picked from commit 0ae62408b6c90f17dd1923dc03a51aa59ae7e554)
    
    
    (cherry picked from commit 1484f1f8513ef43b4b302877396ee0293924f566)
    
    IJ-MR-175246
    
    GitOrigin-RevId: e873b289aa0d943ba0eb50c2204bf9a81a3bd614

--- platform/platform-impl/codeinsight-inline/src/com/intellij/codeInsight/inline/completion/debounce/InlineCompletionAdaptiveDebounceListener.kt
+++ platform/platform-impl/codeinsight-inline/src/com/intellij/codeInsight/inline/completion/debounce/InlineCompletionAdaptiveDebounceListener.kt
@@ -4,19 +4,16 @@ package com.intellij.codeInsight.inline.completion.debounce
 import com.intellij.codeInsight.inline.completion.InlineCompletionEventAdapter
 import com.intellij.codeInsight.inline.completion.InlineCompletionEventType
 import com.intellij.codeInsight.inline.completion.logs.InlineCompletionUsageTracker.ShownEvents.FinishType
+import com.intellij.codeInsight.inline.completion.session.InlineCompletionSession
 import com.intellij.openapi.editor.Editor
 
 /**
  * Listener that observes inline completion lifecycle and records
- * accept/reject finish type into [InlineCompletionFinishedCompletionsStorage].
+ * accept/reject finish type into [InlineCompletionFinishedCompletionsStorage] per provider.
  */
 internal class InlineCompletionAdaptiveDebounceListener(private val editor: Editor) : InlineCompletionEventAdapter {
 
   private fun serviceOrNull(): InlineCompletionFinishedCompletionsStorage? = editor.project?.let { InlineCompletionFinishedCompletionsStorage.getInstance(it) }
-
-  override fun onAfterInsert(event: InlineCompletionEventType.AfterInsert) {
-    serviceOrNull()?.record(InlineCompletionFinishedCompletionsStorage.Result.ACCEPTED)
-  }
 
   override fun onHide(event: InlineCompletionEventType.Hide) {
     val result = when (event.finishType) {
@@ -30,12 +27,13 @@ internal class InlineCompletionAdaptiveDebounceListener(private val editor: Edit
       FinishType.BACKSPACE_PRESSED,
       FinishType.KEY_PRESSED,
       FinishType.INVALIDATED,
-      FinishType.SELECTED,
       FinishType.MOUSE_PRESSED,
       FinishType.DOCUMENT_CHANGED,
         -> InlineCompletionFinishedCompletionsStorage.Result.OTHER
       FinishType.ESCAPE_PRESSED -> InlineCompletionFinishedCompletionsStorage.Result.REJECTED
+      FinishType.SELECTED -> InlineCompletionFinishedCompletionsStorage.Result.ACCEPTED
     }
-    serviceOrNull()?.record(result)
+    val provider = InlineCompletionSession.getOrNull(editor)?.provider?.id
+    provider?.let { serviceOrNull()?.record(it, result) }
   }
 }
