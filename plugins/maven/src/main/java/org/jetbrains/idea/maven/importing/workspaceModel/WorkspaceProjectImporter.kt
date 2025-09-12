a4fdab1d89a4d5ad54d1d7e8657be48d5b8255bb
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-09-12 14:03:36 +0300

    BAZEL-2160 WorkspaceProjectImporter - do not try to migrate external storage if it is not supported at all
    GitOrigin-RevId: a5ab684222fe2e70d32c9000c820f458a504d5a8

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/workspaceModel/WorkspaceProjectImporter.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/workspaceModel/WorkspaceProjectImporter.kt
@@ -39,6 +39,7 @@ import com.intellij.platform.workspace.storage.EntitySource
 import com.intellij.platform.workspace.storage.EntityStorage
 import com.intellij.platform.workspace.storage.MutableEntityStorage
 import com.intellij.platform.workspace.storage.WorkspaceEntity
+import com.intellij.project.stateStore
 import com.intellij.util.ExceptionUtil
 import com.intellij.util.ui.EDT
 import com.intellij.workspaceModel.ide.impl.legacyBridge.module.findModule
@@ -148,6 +149,10 @@ internal open class WorkspaceProjectImporter(
   }
 
   private suspend fun migrateToExternalStorageIfNeeded(): Boolean {
+    if (!project.stateStore.isExternalStorageSupported) {
+      return false
+    }
+
     val externalStorageManager = project.serviceAsync<ExternalStorageConfigurationManager>()
     if (externalStorageManager.isEnabled) {
       return false
