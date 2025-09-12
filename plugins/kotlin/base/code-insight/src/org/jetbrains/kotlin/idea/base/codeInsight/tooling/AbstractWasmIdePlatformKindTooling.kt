0dab7529c5adb6b9fc8d5ec113ccbd1f32e24923
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2025-09-12 15:04:41 +0200

    [Kotlin, Wasm] Add test icons for Kotlin Wasm tests
    ^KTIJ-34663 fixed
    
    GitOrigin-RevId: c163c30075d30372a7ed8768912b763f0119b63f

--- plugins/kotlin/base/code-insight/src/org/jetbrains/kotlin/idea/base/codeInsight/tooling/AbstractWasmIdePlatformKindTooling.kt
+++ plugins/kotlin/base/code-insight/src/org/jetbrains/kotlin/idea/base/codeInsight/tooling/AbstractWasmIdePlatformKindTooling.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.base.codeInsight.tooling
 
+import com.intellij.execution.actions.RunConfigurationProducer
+import com.intellij.util.SmartList
 import org.jetbrains.kotlin.idea.base.platforms.KotlinWasmJsLibraryKind
 import org.jetbrains.kotlin.idea.base.platforms.KotlinWasmWasiLibraryKind
 import org.jetbrains.kotlin.idea.projectModel.KotlinPlatform
@@ -10,6 +12,8 @@ import org.jetbrains.kotlin.psi.KtFunction
 import org.jetbrains.kotlin.psi.KtNamedDeclaration
 import javax.swing.Icon
 
+interface KotlinWasmRunConfigurationDataProvider
+
 abstract class AbstractWasmIdePlatformKindTooling : IdePlatformKindTooling() {
 
     override val mavenLibraryIds: List<String> get() = emptyList<String>()
@@ -18,7 +22,28 @@ abstract class AbstractWasmIdePlatformKindTooling : IdePlatformKindTooling() {
 
     override fun acceptsAsEntryPoint(function: KtFunction): Boolean = false
 
-    override fun getTestIcon(declaration: KtNamedDeclaration, allowSlowOperations: Boolean): Icon? = null
+    override fun getTestIcon(declaration: KtNamedDeclaration, allowSlowOperations: Boolean): Icon? {
+        if (!allowSlowOperations) {
+            return null
+        }
+
+        val initialLocations = computeInitialIconLocations(declaration) ?: return null
+        return testIconProvider.getGenericTestIcon(declaration, initialLocations)
+    }
+
+    private fun computeInitialIconLocations(declaration: KtNamedDeclaration): List<String>? {
+        val hasWasmConfigurationProviders = RunConfigurationProducer
+            .getProducers(declaration.project)
+            .asSequence()
+            .filterIsInstance<KotlinWasmRunConfigurationDataProvider>()
+            .any()
+
+        if (!hasWasmConfigurationProviders) return null
+
+        val location = declaration.containingKtFile.packageFqName.asString()
+
+        return SmartList(location)
+    }
 }
 
 abstract class AbstractWasmJsIdePlatformKindTooling : AbstractWasmIdePlatformKindTooling() {
