0d62b0ee1804321de2065db24e6a126b57343644
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2025-09-12 15:38:01 +0200

    kotlin tests: do not leak paths to download cache location
    GitOrigin-RevId: d4a97b51a13e0cd7f5ab0caac87d315dfcd63260

--- plugins/kotlin/base/plugin/test/org/jetbrains/kotlin/idea/artifacts/KotlinNativePrebuiltDownloader.kt
+++ plugins/kotlin/base/plugin/test/org/jetbrains/kotlin/idea/artifacts/KotlinNativePrebuiltDownloader.kt
@@ -4,14 +4,15 @@
  */
 package org.jetbrains.kotlin.idea.artifacts
 
-import kotlinx.coroutines.Dispatchers
+import com.intellij.openapi.application.PathManager
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.intellij.build.dependencies.BuildDependenciesCommunityRoot
-import org.jetbrains.intellij.build.dependencies.extractFileToCacheLocation
+import org.jetbrains.intellij.build.dependencies.BuildDependenciesDownloader.extractFile
 import org.jetbrains.intellij.build.downloadFileToCacheLocationSync
 import org.jetbrains.kotlin.konan.target.HostManager
 import org.jetbrains.kotlin.konan.target.TargetSupportException
 import java.nio.file.Path
+import kotlin.io.path.exists
 
 const val NATIVE_PREBUILT_DEV_CDN_URL = "https://download-cdn.jetbrains.com/kotlin/native/builds/dev"
 const val NATIVE_PREBUILT_RELEASE_CDN_URL = "https://download-cdn.jetbrains.com/kotlin/native/builds/releases"
@@ -25,9 +26,16 @@ fun getNativePrebuilt(version: String, platform: String, communityRoot: BuildDep
     val archiveName = if (HostManager.hostIsMingw) "$prebuilt.zip" else "$prebuilt.tar.gz"
     val cdnUrl = if ("dev" in version) NATIVE_PREBUILT_DEV_CDN_URL else NATIVE_PREBUILT_RELEASE_CDN_URL
     val downloadUrl = "$cdnUrl/$version/$platform/$archiveName"
+    val outDir = Path.of(PathManager.getCommunityHomePath()).resolve("out")
+    val expandedDir = outDir.resolve(archiveName)
+    val target = expandedDir.resolve(prebuilt)
 
-    val archiveFilePath = downloadFileToCacheLocationSync(downloadUrl, communityRoot)
-    return runBlocking(Dispatchers.IO) {
-        return@runBlocking extractFileToCacheLocation(archiveFilePath, communityRoot, stripRoot = true)
+    if (!target.exists()) {
+        val archiveFilePath = downloadFileToCacheLocationSync(downloadUrl, communityRoot)
+        runBlocking {
+            extractFile(archiveFilePath, expandedDir, communityRoot)
+        }
     }
+
+    return target
 }
