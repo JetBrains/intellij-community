4e08a6ffc3b360b525bef43168d6ea0c53f896d7
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2025-09-15 20:02:25 +0200

    [diagnostic] IJPL-189458 Try to suppress low-memory notifications during indexing as they often are false-positives
    GitOrigin-RevId: 9dbafdac853df387c3581c9a88fca47e405b1f39

--- platform/platform-impl/src/com/intellij/diagnostic/LowMemoryNotifier.java
+++ platform/platform-impl/src/com/intellij/diagnostic/LowMemoryNotifier.java
@@ -2,6 +2,7 @@
 package com.intellij.diagnostic;
 
 import com.intellij.concurrency.ConcurrentCollectionFactory;
+import com.intellij.diagnostic.VMOptions.MemoryKind;
 import com.intellij.diagnostic.hprof.action.HeapDumpSnapshotRunnable;
 import com.intellij.diagnostic.report.MemoryReportReason;
 import com.intellij.ide.IdeBundle;
@@ -10,10 +11,11 @@ import com.intellij.notification.NotificationAction;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbService;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.util.LowMemoryWatcher;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -22,10 +24,10 @@ import java.util.Set;
 import static com.intellij.openapi.util.LowMemoryWatcher.LowMemoryWatcherType.ONLY_AFTER_GC;
 
 final class LowMemoryNotifier implements Disposable {
-  private static final Set<VMOptions.MemoryKind> ourNotifications = ConcurrentCollectionFactory.createConcurrentSet();
+  private static final Set<MemoryKind> ourNotifications = ConcurrentCollectionFactory.createConcurrentSet();
 
   private final LowMemoryWatcher myWatcher =
-    LowMemoryWatcher.register(() -> showNotification(VMOptions.MemoryKind.HEAP, false), ONLY_AFTER_GC);
+    LowMemoryWatcher.register(() -> showNotification(MemoryKind.HEAP, false), ONLY_AFTER_GC);
 
   @Override
   public void dispose() {
@@ -33,21 +35,30 @@ final class LowMemoryNotifier implements Disposable {
   }
 
   static void showNotificationFromCrashAnalysis() {
-    showNotification(VMOptions.MemoryKind.HEAP, true, true);
+    showNotification(MemoryKind.HEAP, true, true);
   }
 
-  static void showNotification(@NotNull VMOptions.MemoryKind kind, boolean oomError) {
+  static void showNotification(@NotNull MemoryKind kind, boolean oomError) {
     showNotification(kind, oomError, false);
   }
 
-  private static void showNotification(@NotNull VMOptions.MemoryKind kind, boolean oomError, boolean fromCrashReport) {
-    if (!ourNotifications.add(kind)) return;
-
-    int currentXmx = VMOptions.readOption(VMOptions.MemoryKind.HEAP, true);
-    Project[] projects = ProjectManager.getInstance().getOpenProjects();
+  private static void showNotification(@NotNull MemoryKind kind, boolean oomError, boolean fromCrashReport) {
+    int currentXmx = VMOptions.readOption(MemoryKind.HEAP, true);
+    var projects = ProjectManager.getInstance().getOpenProjects();
     int projectCount = projects.length;
     boolean isDumb = ContainerUtil.exists(projects, p -> DumbService.isDumb(p));
     UILatencyLogger.lowMemory(kind, currentXmx, projectCount, oomError, fromCrashReport, isDumb);
+
+    if (isDumb
+        && kind == MemoryKind.HEAP
+        && !oomError
+        && !fromCrashReport
+        && Registry.is("ide.suppress.low.memory.notifications.when.dumb")) {
+      Logger.getInstance(LowMemoryNotifier.class).info("Skipped Low Memory notifications because indexing is in progress");
+      return;
+    }
+
+    if (!ourNotifications.add(kind)) return;
 
     var message = oomError ? IdeBundle.message("low.memory.notification.error", kind.label()) : IdeBundle.message("low.memory.notification.warning");
     var type = oomError ? NotificationType.ERROR : NotificationType.WARNING;
