4c239d0fe1b3dc4cf50b2cece6c63105b5ba530d
Nikita Katkov <nikita.katkov@jetbrains.com>
2025-09-16 14:06:50 +0200

    [execution & split mode] IJPL-184406 Do not use content descriptor in monolith since it is NOT ALLOWED by the assertion inside its getter
    Not sure what's the reason behind it, this assert will likely make it impossible for the debugger to work in split services view in monolith - TBD
    
    GitOrigin-RevId: d5f91fab3b98297d50220dc1ee1dd36faa7877bb

--- platform/xdebugger-impl/backend/src/com/intellij/platform/debugger/impl/backend/BackendXDebuggerManagerApi.kt
+++ platform/xdebugger-impl/backend/src/com/intellij/platform/debugger/impl/backend/BackendXDebuggerManagerApi.kt
@@ -70,7 +70,7 @@ internal class BackendXDebuggerManagerApi : XDebuggerManagerApi {
     val cs = currentSession.coroutineScope
     return XDebugSessionDto(
       currentSession.id,
-      currentSession.runContentDescriptor.id as RunContentDescriptorIdImpl,
+      currentSession.runContentDescriptor.id as RunContentDescriptorIdImpl?,
       debugProcess.editorsProvider.toRpc(),
       initialSessionState,
       currentSession.suspendData(),
