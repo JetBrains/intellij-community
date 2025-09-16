33ce7aff07b2d6a1bb4d027d9de3851849591d5e
Denis Mashutin <Denis.Mashutin@jetbrains.com>
2025-09-16 13:10:14 +0200

    PCQA-1255 Write 'Test floating toolbar in requirements.txt / requirements-dev.txt
    GitOrigin-RevId: fcbad3aa75d78fab81a43fb3e9a467f96866a3ee

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/python/PythonEditorFloatingToolbarUi.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/python/PythonEditorFloatingToolbarUi.kt
@@ -11,4 +11,8 @@ fun Finder.pythonEditorFloatingToolbar(@Language("xpath") xpath: String? = null)
 class PythonEditorFloatingToolbarUi(data: ComponentData) : UiComponent(data) {
   val poetryUpdateButton = x { byAccessibleName("Poetry Update") }
   val poetryLockButton = x { byAccessibleName("Poetry Lock") }
+
+  // Requirements floating toolbar actions
+  val setAsEnvDependenciesButton = x { byAccessibleName("Set as the environment dependencies list") }
+  val installAllButton = x { byAccessibleName("Install all") }
 }
