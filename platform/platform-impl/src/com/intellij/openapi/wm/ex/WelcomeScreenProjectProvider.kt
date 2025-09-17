f82b34f30226c423a63eb808ef30a4206317a607
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-09-17 11:09:55 +0200

    IJPL-207755 get rid of WelcomeProjectScopeHolder - we MUST NOT have any new API that do not uses coroutines
    (cherry picked from commit 6f43a1e7120c6b26b05db2fe2318b103bd7cd1c1)
    
    IJ-CR-175907
    
    GitOrigin-RevId: 149bb88bbf614cf859753b3a150389d9c5e55885

--- platform/platform-impl/src/com/intellij/openapi/wm/ex/WelcomeScreenProjectProvider.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/ex/WelcomeScreenProjectProvider.kt
@@ -2,11 +2,12 @@
 package com.intellij.openapi.wm.ex
 
 import com.intellij.diagnostic.WindowsDefenderChecker
+import com.intellij.ide.RecentProjectsManager
 import com.intellij.ide.RecentProjectsManagerBase
 import com.intellij.ide.impl.ProjectUtil
 import com.intellij.ide.trustedProjects.TrustedProjects
 import com.intellij.ide.util.TipAndTrickManager
-import com.intellij.openapi.components.service
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
@@ -73,13 +74,13 @@ abstract class WelcomeScreenProjectProvider {
         projectPath.createDirectories()
       }
       TrustedProjects.setProjectTrusted(projectPath, true)
-      service<WindowsDefenderChecker>().markProjectPath(projectPath, /*skip =*/ true)
+      serviceAsync<WindowsDefenderChecker>().markProjectPath(projectPath, /*skip =*/ true)
 
       val project = extension.doCreateOrOpenWelcomeScreenProject(projectPath) ?: return null
       LOG.info("Opened the welcome screen project at $projectPath")
       LOG.debug("Project: ", project)
 
-      val recentProjectsManager = RecentProjectsManagerBase.getInstanceEx()
+      val recentProjectsManager = serviceAsync<RecentProjectsManager>() as RecentProjectsManagerBase
       recentProjectsManager.setProjectHidden(project, true)
       TipAndTrickManager.DISABLE_TIPS_FOR_PROJECT.set(project, true)
 
