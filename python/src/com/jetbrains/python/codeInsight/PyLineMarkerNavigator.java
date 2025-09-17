6f5afdb9f3f47e916ad9279a320b32f090876263
Andrey Vokin <andrey.vokin@jetbrains.com>
2025-09-17 21:18:55 +0200

    PY-84187 Exception when clicking the subclass gutter icon
    GitOrigin-RevId: 531047f58de947459e724e50db1abb2abaf0013e

--- python/src/com/jetbrains/python/codeInsight/PyLineMarkerNavigator.java
+++ python/src/com/jetbrains/python/codeInsight/PyLineMarkerNavigator.java
@@ -2,14 +2,16 @@
 package com.jetbrains.python.codeInsight;
 
 import com.intellij.codeInsight.daemon.GutterIconNavigationHandler;
-import com.intellij.codeInsight.daemon.impl.PsiElementListNavigator;
-import com.intellij.ide.util.DefaultPsiElementCellRenderer;
+import com.intellij.codeInsight.navigation.NavigationUtil;
+import com.intellij.codeInsight.navigation.PsiTargetNavigator;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.NlsContexts.PopupTitle;
 import com.intellij.openapi.util.UserDataHolder;
 import com.intellij.psi.NavigatablePsiElement;
 import com.intellij.psi.PsiElement;
+import com.intellij.ui.awt.RelativePoint;
 import com.intellij.util.Query;
 import com.jetbrains.python.psi.types.TypeEvalContext;
 import org.jetbrains.annotations.ApiStatus;
@@ -49,7 +51,10 @@ public abstract class PyLineMarkerNavigator<T extends PsiElement> implements Gut
       elt.putUserData(MARKERS, methods);
     }
     else {
-      PsiElementListNavigator.openTargets(e, methods, getTitle(elt), null, new DefaultPsiElementCellRenderer());
+      var project = elt.getProject();
+      JBPopup popup = new PsiTargetNavigator<>(methods).createPopup(project, getTitle(elt));
+      NavigationUtil.hidePopupIfDumbModeStarts(popup, project);
+      popup.show(new RelativePoint(e));
     }
   }
 
