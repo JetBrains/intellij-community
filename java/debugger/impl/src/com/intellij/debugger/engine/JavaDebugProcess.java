3b3368abaad18cef096ce88ff9386207580a01c8
Egor Ushakov <egor.ushakov@jetbrains.com>
2025-09-18 11:53:02 +0200

    [debugger] reverted to invoke method
    GitOrigin-RevId: ce0a89b2c2ab95e9d8e30c2218ddc3d0bb435cbd

--- java/debugger/impl/src/com/intellij/debugger/engine/JavaDebugProcess.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/JavaDebugProcess.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.debugger.engine;
 
 import com.intellij.debugger.JavaDebuggerBundle;
@@ -244,7 +244,8 @@ public class JavaDebugProcess extends XDebugProcess {
   }
 
   private void saveNodeHistory(final StackFrameProxyImpl frameProxy) {
-    myJavaSession.getProcess().getManagerThread().schedule(PrioritizedTask.Priority.NORMAL,
+    // TODO: reject calls from non-DMT, for now must use invoke to avoid IncompatibleThreadStateException
+    myJavaSession.getProcess().getManagerThread().invoke(PrioritizedTask.Priority.NORMAL,
                                                            () -> myNodeManager.setHistoryByContext(frameProxy));
   }
 
