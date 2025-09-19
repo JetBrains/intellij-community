736018ec126769655e634ef9f270aa892de9d100
Kirill Rakhman <kirill.rakhman@gmail.com>
2025-09-19 14:06:16 +0200

    [kotlin] Fix testdata after #KT-9111
    (cherry picked from commit 230e4b8a6b387aff5141473b5432e209a04b4b6c)
    
    GitOrigin-RevId: 101e4d70340d990bec42ca795d4c2272707ba947

--- plugins/kotlin/idea/tests/testData/quickfix/createFromUsage/createVariable/localVariable/inAnnotation.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/createFromUsage/createVariable/localVariable/inAnnotation.kt
@@ -1,7 +1,8 @@
 // "Create local variable 'defaultPreferencesName'" "false"
 // ERROR: Unresolved reference: defaultPreferencesName
 // ERROR: Default value of annotation parameter must be a compile-time constant
-// K2_AFTER_ERROR: Unresolved reference 'defaultPreferencesName'.
+// K2_AFTER_ERROR: Default value of annotation parameter must be a compile-time constant.
+// K2_AFTER_ERROR: Outer class 'class AppModule : Any' of non-inner class cannot be used as receiver.
 
 class AppModule {
     val defaultPreferencesName  = "defaultPreferences"
