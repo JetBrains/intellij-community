b8bf29c8de107911e8c3b60a3504f2fbdc3b195b
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-09-19 14:10:22 +0200

    [ab] IJPL-186222: Introduce a simpler API for running AB experiments
    GitOrigin-RevId: d37a06ebb60306d38f9e5a04d98df6669f07946a

---
+++ platform/experiment/src/com/intellij/platform/experiment/ab/impl/experiments.kt
@@ -0,0 +1,102 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.experiment.ab.impl
+
+import com.intellij.internal.statistic.eventLog.fus.MachineIdManager
+import com.intellij.openapi.application.ApplicationInfo.getInstance
+import com.intellij.openapi.diagnostic.debug
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.diagnostic.runAndLogException
+import com.intellij.platform.experiment.ab.impl.ABExperimentOption.*
+import com.intellij.platform.experiment.ab.impl.statistic.ABExperimentCountCollector
+import org.jetbrains.annotations.VisibleForTesting
+import kotlin.math.absoluteValue
+
+/**
+ * Complete list of all available AB experiments.
+ *
+ * The plugins are welcome to use [ABExperimentOption.isEnabled] to check whether the experiment is enabled on the user's machine
+ */
+enum class ABExperimentOption {
+  EXPERIMENT_1,
+  EXPERIMENT_2,
+  EXPERIMENT_3,
+
+  /**
+   * A group for users which are not assigned to any experiment.
+   */
+  UNASSIGNED;
+
+  fun isEnabled(): Boolean {
+    require(this != UNASSIGNED) {
+      "UNASSIGNED experiment option is not supposed to be used in the isEnabled() method"
+    }
+    val decision = thisUserDecision
+    ABExperimentCountCollector.logABExperimentOptionUsed(decision)
+    return isAllowed(decision.option) && decision.option == this && !decision.isControlGroup
+  }
+}
+
+/**
+ * Total number of "containers" where users are distributed. Each user belongs exactly to one bucket.
+ * Each bucket has at most one experimental option enabled, which are controlled by [experimentsPartition].
+ */
+internal const val NUMBER_OF_BUCKETS: Int = 1024
+
+/**
+ * Mapping of buckets to experiments.
+ * Each experiment is assigned a non-overlapping range of buckets in [0..[NUMBER_OF_BUCKETS])
+ * The buckets that are not assigned to any experiment are considered to be in the UNASSIGNED experiment (i.e., no experiments are enabled for them).
+ */
+@VisibleForTesting
+internal val experimentsPartition: List<ExperimentAssignment> = listOf(
+  ExperimentAssignment(experiment = EXPERIMENT_1, experimentBuckets = (0 until 180).toSet(), controlBuckets = (180 until 256).toSet()),
+  ExperimentAssignment(experiment = EXPERIMENT_2, experimentBuckets = (256 until 384).toSet(), controlBuckets = (384 until 512).toSet()),
+  ExperimentAssignment(experiment = EXPERIMENT_3, experimentBuckets = (512 until 640).toSet(), controlBuckets = (640 until 768).toSet()),
+  // the rest belongs to the unassigned experiment
+)
+
+/**
+ * This method can be configured to allow options only in particular IDEs.
+ */
+fun isAllowed(option: ABExperimentOption): Boolean {
+  return true
+}
+
+// ================= IMPLEMENTATION ====================
+
+internal data class ABExperimentDecision(val option: ABExperimentOption, val isControlGroup: Boolean, val bucketNumber: Int)
+
+internal fun ABExperimentOption.reportableName(): String {
+  return toString().lowercase().replace('_', '.')
+}
+
+private val thisUserDecision: ABExperimentDecision by lazy {
+  val currentBucket = getUserBucketNumber()
+  val option = experimentsPartition.find {
+    it.experimentBuckets.contains(currentBucket) || it.controlBuckets.contains(currentBucket)
+  } ?: return@lazy ABExperimentDecision(option = UNASSIGNED, isControlGroup = true, bucketNumber = currentBucket)
+  ABExperimentDecision(option = option.experiment, isControlGroup = option.controlBuckets.contains(currentBucket), bucketNumber = currentBucket)
+}
+
+data class ExperimentAssignment(val experiment: ABExperimentOption, val experimentBuckets: Set<Int>, val controlBuckets: Set<Int>)
+
+internal fun getUserBucketNumber(): Int {
+  val overridingBucket = Integer.getInteger("ide.ab.test.overriding.bucket")
+  if (overridingBucket != null) {
+    LOG.info("Overriding bucket number: $overridingBucket")
+    return overridingBucket
+  }
+  val deviceId = LOG.runAndLogException {
+    MachineIdManager.getAnonymizedMachineId(getDeviceIdPurpose())
+  }
+
+  val bucketNumber = deviceId.hashCode().absoluteValue % NUMBER_OF_BUCKETS
+  LOG.debug { "User bucket number is: $bucketNumber." }
+  return bucketNumber
+}
+
+private fun getDeviceIdPurpose(): String {
+  return "A/B Experiment" + getInstance().shortVersion
+}
+
+private val LOG = logger<ABExperimentOption>()
