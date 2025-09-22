ea5fa1b63fb77d66ac7fc494ec03d79b3e365530
Tagir Valeev <tagir.valeev@jetbrains.com>
2025-09-22 09:27:23 +0200

    [util] IJPL-208889 MarkupText experimental API
    A simple model of a text line with markup which doesn't depend on Swing to be used in  completion, etc.
    
    GitOrigin-RevId: 3aef202aa8f00edf149f11f6d3b859456ba44fe0

---
+++ platform/analysis-api/src/com/intellij/modcompletion/package-info.java
@@ -0,0 +1,5 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@Internal
+package com.intellij.modcompletion;
+
+import org.jetbrains.annotations.ApiStatus.Internal;
