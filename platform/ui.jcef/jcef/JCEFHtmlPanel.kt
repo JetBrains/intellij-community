b78d02d49a9b319cdc64757b5bf264209c8be23c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-09-23 07:26:48 +0200

    IJPL-156676 convert JCEFHtmlPanel to kotlin
    GitOrigin-RevId: 4e7a0f227e12de5169b8db99e17c6dae8126dd05

--- platform/ui.jcef/jcef/JCEFHtmlPanel.kt
+++ platform/ui.jcef/jcef/JCEFHtmlPanel.kt
@@ -1,61 +1,52 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ui.jcef;
+package com.intellij.ui.jcef
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.util.Disposer;
-import org.cef.browser.CefBrowser;
-import org.cef.browser.CefFrame;
-import org.cef.callback.CefContextMenuParams;
-import org.cef.callback.CefMenuModel;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.util.Disposer
+import org.cef.browser.CefBrowser
+import org.cef.browser.CefFrame
+import org.cef.callback.CefContextMenuParams
+import org.cef.callback.CefMenuModel
 
 /**
  * @author tav
  */
-public class JCEFHtmlPanel extends JBCefBrowser {
-  private static final JBCefClient ourCefClient = JBCefApp.getInstance().createClient();
+open class JCEFHtmlPanel(isOffScreenRendering: Boolean, client: JBCefClient?, url: String?) : JBCefBrowser(
+  createBuilder().setOffScreenRendering(isOffScreenRendering).setClient(client).setUrl(url)) {
+  private val myUrl: String
 
-  static {
-    Disposer.register(ApplicationManager.getApplication(), ourCefClient);
+  constructor(url: String?) : this(ourCefClient, url)
+
+  constructor(isOffScreenRendering: Boolean, url: String?) : this(isOffScreenRendering, ourCefClient, url)
+
+  constructor(client: JBCefClient?, url: String?) : this(true, client, url) // should no pass url to ctor
+
+
+  init {
+    myUrl = getCefBrowser().getURL()
   }
 
-  private final @NotNull String myUrl;
+  companion object {
+    private val ourCefClient = JBCefApp.getInstance().createClient()
 
-  public JCEFHtmlPanel(@Nullable String url) {
-    this(ourCefClient, url);
+    init {
+      Disposer.register(ApplicationManager.getApplication(), ourCefClient)
+    }
   }
 
-  public JCEFHtmlPanel(boolean isOffScreenRendering, @Nullable String url) {
-    this(isOffScreenRendering, ourCefClient, url);
-  }
-
-  public JCEFHtmlPanel(@Nullable JBCefClient client, @Nullable String url) {
-    this(true, client, url); // should no pass url to ctor
-  }
-
-  public JCEFHtmlPanel(boolean isOffScreenRendering, @Nullable JBCefClient client, @Nullable String url) {
-    super(JBCefBrowser.createBuilder().setOffScreenRendering(isOffScreenRendering).setClient(client).setUrl(url));
-    myUrl = getCefBrowser().getURL();
-  }
-
-  @Override
-  protected DefaultCefContextMenuHandler createDefaultContextMenuHandler() {
-    return new DefaultCefContextMenuHandler() {
-      @Override
-      public void onBeforeContextMenu(CefBrowser browser, CefFrame frame, CefContextMenuParams params, CefMenuModel model) {
-        model.clear();
-        super.onBeforeContextMenu(browser, frame, params, model);
+  override fun createDefaultContextMenuHandler(): DefaultCefContextMenuHandler {
+    return object : DefaultCefContextMenuHandler() {
+      override fun onBeforeContextMenu(browser: CefBrowser?, frame: CefFrame?, params: CefContextMenuParams?, model: CefMenuModel) {
+        model.clear()
+        super.onBeforeContextMenu(browser, frame, params, model)
       }
-    };
+    }
   }
 
-  public void setHtml(@NotNull String html) {
-    String htmlToRender = prepareHtml(html);
-    loadHTML(htmlToRender, myUrl);
+  open fun setHtml(html: String) {
+    val htmlToRender = prepareHtml(html)
+    loadHTML(htmlToRender, myUrl)
   }
 
-  protected @NotNull String prepareHtml(@NotNull String html) {
-    return html;
-  }
+  protected open fun prepareHtml(html: String): String = html
 }
