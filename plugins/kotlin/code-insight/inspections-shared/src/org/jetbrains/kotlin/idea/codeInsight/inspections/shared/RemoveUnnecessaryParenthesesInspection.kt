917ca3498530c14df540f4d415407374e6ecc42e
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2025-09-23 22:36:25 +0200

    [kotlin] KTIJ-17913 Add a highlighting test
    GitOrigin-RevId: f8b38372fc46ad010a95f05ad5dedfaa72e0e4f9

--- plugins/kotlin/code-insight/inspections-shared/src/org/jetbrains/kotlin/idea/codeInsight/inspections/shared/RemoveUnnecessaryParenthesesInspection.kt
+++ plugins/kotlin/code-insight/inspections-shared/src/org/jetbrains/kotlin/idea/codeInsight/inspections/shared/RemoveUnnecessaryParenthesesInspection.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.codeInsight.inspections.shared
 
+import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.codeInspection.util.InspectionMessage
 import com.intellij.modcommand.ModPsiUpdater
@@ -21,6 +22,9 @@ class RemoveUnnecessaryParenthesesInspection : KotlinApplicableInspectionBase.Si
     override fun getProblemDescription(
         element: KtParenthesizedExpression, context: Unit
     ): @InspectionMessage String = KotlinBundle.message("inspection.remove.unnecessary.parentheses.display.name")
+
+    override fun getProblemHighlightType(element: KtParenthesizedExpression, context: Unit): ProblemHighlightType =
+        ProblemHighlightType.LIKE_UNUSED_SYMBOL
 
     override fun createQuickFix(
         element: KtParenthesizedExpression, context: Unit
@@ -54,5 +58,5 @@ class RemoveUnnecessaryParenthesesInspection : KotlinApplicableInspectionBase.Si
         }
     }
 
-    override fun KaSession.prepareContext(element: KtParenthesizedExpression): Unit = Unit
+    override fun KaSession.prepareContext(element: KtParenthesizedExpression) {}
 }
