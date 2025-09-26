b8c99666774c7b1234516f67c9d637766ce64560
Sergei Kharitontcev-Beglov <sergei.kharitontcev-beglov@jetbrains.com>
2025-09-26 11:23:51 +0200

    [ij-light, eel] IJPL-209607: initial TCP support for local mode
    This commit introduces the first step to introduce proper tcp connection for IJent GRPC connection. That includes:
    1. Implementation of a plain-text transport server on the IJent's side
    2. Initial connection flow for the TCP connection that is based on the already decided port
    3. Local TCP Test fixture
    
    GitOrigin-RevId: ace82de5a3ab22263103d952bce7d64fa7ff7d74

--- platform/ijent/src/com/intellij/platform/ijent/IjentProcessUtil.kt
+++ platform/ijent/src/com/intellij/platform/ijent/IjentProcessUtil.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 @file:JvmName("IjentProcessUtil")
 
 package com.intellij.platform.ijent
@@ -14,13 +14,16 @@ fun getIjentGrpcArgv(
   additionalEnv: Map<String, String> = mapOf(),
   selfDeleteOnExit: Boolean = false,
   usrBinEnv: String = "/usr/bin/env",
+  tcpConfig: TcpConnectionInfo? = null,
 ): List<String> {
+  val command = if (tcpConfig != null) arrayOf("grpc-socket-server", "--port=${tcpConfig.port}") else arrayOf("grpc-stdio-server")
+  LOG.info("Ijent gRPC command: ${command.joinToString(" ")}")
   return listOfNotNull(
     usrBinEnv,
     *additionalEnv.entries.map2Array { (k, v) -> "$k=$v" },
     // "gdbserver", "0.0.0.0:12345",  // https://sourceware.org/gdb/onlinedocs/gdb/Connecting.html
     remotePathToIjent,
-    "grpc-stdio-server",
+    *command,
     if (selfDeleteOnExit) "--self-delete-on-exit" else null,
   )
 }
