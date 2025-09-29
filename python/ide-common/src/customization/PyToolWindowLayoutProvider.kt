fca40ea6583b53f61532426ce951c7398bb0a76b
Nikita Zaytsev <nikita.zaytsev@jetbrains.com>
2025-09-29 18:57:00 +0300

    [rdct] IJPL-169754: Move default tool window layout customization to common modules
    Some products customize the default tool window layout by adding additional tool windows to the stripes. This commit moves the customization to common modules, so it's available in the JetBrains Client for the products supported in remote development, and the default layout is correct when restoring it.
    
    GitOrigin-RevId: 8e2d378ff77b54ede76193b3867a57b42ef9bca5

--- python/ide-common/src/customization/PyToolWindowLayoutProvider.kt
+++ python/ide-common/src/customization/PyToolWindowLayoutProvider.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.pycharm.community.ide.impl
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.pycharm.community.customization
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.toolWindow.DefaultToolWindowLayoutBuilder
