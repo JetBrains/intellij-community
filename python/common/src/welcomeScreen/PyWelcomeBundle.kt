71250e309539c7b7ecf51a9e08335db30e8e507b
Vyacheslav Moklev <vyacheslav.moklev@jetbrains.com>
2025-09-30 09:38:35 +0300

    [RDCT] PY-84272: Split PyCharm's non-modal welcome screen right tab to frontend and backend
    This commit:
    Moves PyWelcomeBundle to the common module, so it is accessible both on frontend and backend
    Moves icons to the frontend module, where they are used now
    Moves PyCharmWelcomeRightTabContentProvider to the frontend module, where it should create UI
    Extracts backend-related parts of right tab provider to separate implementations of WelcomeScreenFeatureBackend
    
    GitOrigin-RevId: b9de1cd8afc2691d256f44824c36803d269388c3

--- python/common/src/welcomeScreen/PyWelcomeBundle.kt
+++ python/common/src/welcomeScreen/PyWelcomeBundle.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.pycharm.community.ide.impl.newProjectWizard.welcome
+package com.intellij.python.common.welcomeScreen
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.ApiStatus
