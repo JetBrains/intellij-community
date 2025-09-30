1b75140da11c8ac846cfa382cde36bb99314f5c9
Edem Ibraimov <edem.ibraimov@jetbrains.com>
2025-09-30 22:18:01 +0300

    IDEA-209728: convert ProjectTaskContext to kotlin
    GitOrigin-RevId: 6593bbf5444cc7ef61bf8e14e39339ba5948c79c

--- platform/lang-api/src/com/intellij/task/ProjectTaskContext.kt
+++ platform/lang-api/src/com/intellij/task/ProjectTaskContext.kt
@@ -1,149 +1,117 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.task;
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.task
 
-import com.intellij.execution.configurations.RunConfiguration;
-import com.intellij.openapi.util.Key;
-import com.intellij.openapi.util.UserDataHolderBase;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.containers.MultiMap;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.Unmodifiable;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
+import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.UserDataHolderBase
+import com.intellij.util.containers.ContainerUtil
+import com.intellij.util.containers.MultiMap
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.Unmodifiable
+import java.util.*
+import java.util.function.Supplier
+import java.util.stream.Stream
+import kotlin.concurrent.Volatile
 
 /**
  * @author Vladislav.Soroka
  */
-public class ProjectTaskContext extends UserDataHolderBase {
-  private final @Nullable Object mySessionId;
-  private final @Nullable RunConfiguration myRunConfiguration;
-  private final boolean myAutoRun;
-  private final MultiMap<String, String> myGeneratedFiles;
-  private final List<Supplier<? extends Collection<String>>> myDirtyOutputPaths;
-  private volatile boolean myCollectGeneratedFiles;
-
-  public ProjectTaskContext() {
-    this(null, null, false);
-  }
-
-  public ProjectTaskContext(boolean autoRun) {
-    this(null, null, autoRun);
-  }
-
-  public ProjectTaskContext(@Nullable Object sessionId) {
-    this(sessionId, null, false);
-  }
-
-  public ProjectTaskContext(@Nullable Object sessionId, @Nullable RunConfiguration runConfiguration) {
-    this(sessionId, runConfiguration, false);
-  }
-
-  public ProjectTaskContext(@Nullable Object sessionId, @Nullable RunConfiguration runConfiguration, boolean autoRun) {
-    mySessionId = sessionId;
-    myRunConfiguration = runConfiguration;
-    myAutoRun = autoRun;
-    myGeneratedFiles = MultiMap.createConcurrentSet();
-    myDirtyOutputPaths = ContainerUtil.createConcurrentList();
-  }
-
-  public @Nullable Object getSessionId() {
-    return mySessionId;
-  }
-
-  public @Nullable RunConfiguration getRunConfiguration() {
-    return myRunConfiguration;
-  }
-
+open class ProjectTaskContext @JvmOverloads constructor(
+  val sessionId: Any? = null,
+  val runConfiguration: RunConfiguration? = null,
   /**
-   * @return true indicates that the task was started automatically, e.g. resources compilation on frame deactivation
+   * true indicates that the task was started automatically, e.g. resources compilation on frame deactivation
    */
-  public boolean isAutoRun() {
-    return myAutoRun;
-  }
+  val isAutoRun: Boolean = false
+) : UserDataHolderBase() {
+  private val myGeneratedFiles: MultiMap<String?, String?> = MultiMap.createConcurrentSet<String?, String?>()
+  private val myDirtyOutputPaths: MutableList<Supplier<out Collection<String?>?>?> = ContainerUtil.createConcurrentList<Supplier<out Collection<String?>?>?>()
+
+  constructor(autoRun: Boolean) : this(null, null, autoRun)
+
+  @get:ApiStatus.Experimental
+  @Volatile
+  var isCollectionOfGeneratedFilesEnabled: Boolean = false
+    private set
 
   @ApiStatus.Experimental
-  public void enableCollectionOfGeneratedFiles() {
-    myCollectGeneratedFiles = true;
+  fun enableCollectionOfGeneratedFiles() {
+    this.isCollectionOfGeneratedFilesEnabled = true
   }
 
-  @ApiStatus.Experimental
-  public boolean isCollectionOfGeneratedFilesEnabled() {
-    return myCollectGeneratedFiles;
-  }
-
-  /**
-   * Returns roots of the files generated during the task session.
-   * Note, generated files collecting is disabled by default.
-   * It can be requested using the {@link #enableCollectionOfGeneratedFiles()} method by the task initiator, see {@link ProjectTaskManager#run(ProjectTaskContext, ProjectTask)}.
-   * Or using the {@link ProjectTaskListener#started(ProjectTaskContext)} event.
-   */
-  @ApiStatus.Experimental
-  public @NotNull @Unmodifiable Collection<String> getGeneratedFilesRoots() {
-    return myGeneratedFiles.keySet();
-  }
+  @get:ApiStatus.Experimental
+  val generatedFilesRoots: @Unmodifiable MutableCollection<String?>
+    /**
+     * Returns roots of the files generated during the task session.
+     * Note, generated files collecting is disabled by default.
+     * It can be requested using the [.enableCollectionOfGeneratedFiles] method by the task initiator, see [ProjectTaskManager.run].
+     * Or using the [ProjectTaskListener.started] event.
+     */
+    get() = myGeneratedFiles.keySet()
 
   /**
    * Returns files generated during the task session in the specified root.
    * Note, generated files collecting is disabled by default.
-   * It can be requested using the {@link #enableCollectionOfGeneratedFiles()} method by the task initiator, see {@link ProjectTaskManager#run(ProjectTaskContext, ProjectTask)}.
-   * Or using the {@link ProjectTaskListener#started(ProjectTaskContext)} event.
+   * It can be requested using the [.enableCollectionOfGeneratedFiles] method by the task initiator, see [ProjectTaskManager.run].
+   * Or using the [ProjectTaskListener.started] event.
    */
   @ApiStatus.Experimental
-  public @NotNull @Unmodifiable Collection<String> getGeneratedFilesRelativePaths(@NotNull String root) {
-    return myGeneratedFiles.get(root);
+  fun getGeneratedFilesRelativePaths(root: String): @Unmodifiable MutableCollection<String?> {
+    return myGeneratedFiles.get(root)
   }
 
   /**
    * This method isn't supposed to be used directly.
-   * {@link ProjectTaskRunner}s can use it to report information about generated files during some task execution.
-   *
+   * [ProjectTaskRunner]s can use it to report information about generated files during some task execution.
+   * 
    * @param root the root directory of the generated file
    * @param relativePath the generated file relative path with regard to the root
    */
   @ApiStatus.Experimental
-  public void fileGenerated(@NotNull String root, @NotNull String relativePath) {
-    if (myCollectGeneratedFiles) {
-      myGeneratedFiles.putValue(root, relativePath);
+  fun fileGenerated(root: String, relativePath: String) {
+    if (this.isCollectionOfGeneratedFilesEnabled) {
+      myGeneratedFiles.putValue(root, relativePath)
     }
   }
 
   /**
    * This method isn't supposed to be used directly.
-   * {@link ProjectTaskRunner}s can use it to report output paths of generated files produced during some task execution.
-   * <p>
+   * [ProjectTaskRunner]s can use it to report output paths of generated files produced during some task execution.
+   * 
+   * 
    * Note, generated files collecting is disabled by default.
-   * It can be requested using the {@link #enableCollectionOfGeneratedFiles()} method by the task initiator, see {@link ProjectTaskManager#run(ProjectTaskContext, ProjectTask)}.
-   * Or using the {@link ProjectTaskListener#started(ProjectTaskContext)} event.
-   * <p>
-   * The method should be used ONLY if the {@link ProjectTaskRunner} doesn't support {@link #fileGenerated} events.
+   * It can be requested using the [.enableCollectionOfGeneratedFiles] method by the task initiator, see [ProjectTaskManager.run].
+   * Or using the [ProjectTaskListener.started] event.
+   * 
+   * 
+   * The method should be used ONLY if the [ProjectTaskRunner] doesn't support [.fileGenerated] events.
    */
   @ApiStatus.Experimental
-  public void addDirtyOutputPathsProvider(@NotNull Supplier<? extends Collection<String>> outputPathsProvider) {
-    if (myCollectGeneratedFiles) {
-      myDirtyOutputPaths.add(outputPathsProvider);
+  fun addDirtyOutputPathsProvider(outputPathsProvider: Supplier<out Collection<String?>?>) {
+    if (this.isCollectionOfGeneratedFilesEnabled) {
+      myDirtyOutputPaths.add(outputPathsProvider)
     }
   }
 
-  /**
-   * Provides output paths that can be used for generated files by some tasks.
-   * The intended usage is to scan those directories for modified files.
-   * <p>
-   * Can be useful for {@link ProjectTaskRunner}s which doesn't support {@link #fileGenerated} events.
-   */
-  @ApiStatus.OverrideOnly
-  public Optional<Stream<String>> getDirtyOutputPaths() {
-    return myDirtyOutputPaths.isEmpty() ? Optional.empty() :
-           Optional.of(myDirtyOutputPaths.stream().map(supplier -> supplier.get()).flatMap(Collection::stream).distinct());
-  }
+  @get:ApiStatus.OverrideOnly
+  open val dirtyOutputPaths: Optional<Stream<String?>>
+    /**
+     * Provides output paths that can be used for generated files by some tasks.
+     * The intended usage is to scan those directories for modified files.
+     * 
+     * 
+     * Can be useful for [ProjectTaskRunner]s which doesn't support [.fileGenerated] events.
+     */
+    get() = if (myDirtyOutputPaths.isEmpty()) Optional.empty<Stream<String?>>()
+    else Optional.of(
+      myDirtyOutputPaths.stream()
+        .map { supplier -> supplier?.get() }
+        .flatMap<String?> { obj -> obj?.stream() }
+        .distinct()
+    )
 
-  public <T> ProjectTaskContext withUserData(@NotNull Key<T> key, @Nullable T value) {
-    putUserData(key, value);
-    return this;
+  fun <T> withUserData(key: Key<T>, value: T?): ProjectTaskContext {
+    putUserData(key, value)
+    return this
   }
 }
