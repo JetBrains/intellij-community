04dcfb458651c995d26317abe2f2694e2729c3d4
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-09-30 09:25:53 +0300

    [terminal] IJPL-211122 Refactor: move MoveTerminalSessionToEditorAction to frontend module
    GitOrigin-RevId: 00b9d0b76771cee7f4832b5941ab5fbfdfcb7527

--- plugins/terminal/frontend/src/com/intellij/terminal/frontend/action/MoveTerminalSessionToEditorAction.kt
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/action/MoveTerminalSessionToEditorAction.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.terminal.action
+package com.intellij.terminal.frontend.action
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.fileEditor.FileEditorManager
@@ -11,10 +11,13 @@ import com.intellij.terminal.JBTerminalWidget
 import com.intellij.terminal.ui.TerminalWidget
 import com.intellij.ui.content.Content
 import org.jetbrains.plugins.terminal.TerminalToolWindowManager
+import org.jetbrains.plugins.terminal.action.TerminalSessionContextMenuActionBase
 import org.jetbrains.plugins.terminal.vfs.TerminalEditorWidgetListener
 import org.jetbrains.plugins.terminal.vfs.TerminalSessionVirtualFileImpl
+import java.lang.Boolean
+import kotlin.let
 
-private class MoveTerminalSessionToEditorAction : TerminalSessionContextMenuActionBase(), DumbAware {
+internal class MoveTerminalSessionToEditorAction : TerminalSessionContextMenuActionBase(), DumbAware {
   override fun updateInTerminalToolWindow(e: AnActionEvent, project: Project, content: Content, terminalWidget: TerminalWidget) {
     e.presentation.isEnabledAndVisible = !Registry.`is`("toolwindow.open.tab.in.editor")
   }
@@ -22,7 +25,7 @@ private class MoveTerminalSessionToEditorAction : TerminalSessionContextMenuActi
   override fun actionPerformedInTerminalToolWindow(e: AnActionEvent, project: Project, content: Content, terminalWidget: TerminalWidget) {
     val terminalToolWindowManager = TerminalToolWindowManager.getInstance(project)
     val file = TerminalSessionVirtualFileImpl(terminalWidget.terminalTitle.buildTitle(), terminalWidget, terminalToolWindowManager.terminalRunner.settingsProvider)
-    file.putUserData(FileEditorManagerImpl.CLOSING_TO_REOPEN, java.lang.Boolean.TRUE)
+    file.putUserData(FileEditorManagerImpl.CLOSING_TO_REOPEN, Boolean.TRUE)
     FileEditorManager.getInstance(project).openFile(file, true).first()
     JBTerminalWidget.asJediTermWidget(terminalWidget)?.let {
       it.listener = TerminalEditorWidgetListener(project, file)
