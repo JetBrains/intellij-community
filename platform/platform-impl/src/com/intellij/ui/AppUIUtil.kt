de719921d192933c0fd4faa657e93435158205cc
Ilia Smirnov <ilia.smirnov@jetbrains.com>
2025-09-30 10:26:07 +0200

    IJPL-210395 Get rid of the LLMC consent in Settings
    (cherry picked from commit 90696c7f7a1d37318e17e9dc8fba6433b26ee8d3)
    
    IJ-CR-177425
    
    GitOrigin-RevId: 9cd8f3e6767ecb8bd35045a79251dbbd29dbf779

--- platform/platform-impl/src/com/intellij/ui/AppUIUtil.kt
+++ platform/platform-impl/src/com/intellij/ui/AppUIUtil.kt
@@ -356,12 +356,8 @@ object AppUIUtil {
       }
     }
     result.removeIf(ConsentOptions.condTraceDataCollectionConsent()) // IJPL-208500
-    val traceConsentManager = TraceConsentManager.getInstance()
-    if (traceConsentManager != null && traceConsentManager.isTraceConsentEnabled()) {
-      val isLegacyAiDataCollectionConsent = ConsentOptions.condAiDataCollectionConsent()
-      result.removeIf(isLegacyAiDataCollectionConsent) // IJPL-195651; AI data collection (LLMC) consent should not be present on UI while it's staying a default consent as a part of migration from LLMC to TRACE consent
-    }
-    if (traceConsentManager?.canDisplayTraceConsent() != true) {
+    result.removeIf(ConsentOptions.condAiDataCollectionConsent()) // IJPL-195651; AI data collection (LLMC) consent should not be present on UI while it's staying a default consent as a part of migration from LLMC to TRACE consent
+    if (TraceConsentManager.getInstance()?.canDisplayTraceConsent() != true) {
       removeTraceConsents(result)
     } else {
       val licenseTypeFlag = LicensingFacade.getInstance()?.metadata?.getOrNull(10)
