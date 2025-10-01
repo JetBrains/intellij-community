45da3ac48d561cd88884f36b219fce6245ed08d9
Daniil Kalinin <Daniil.Kalinin@jetbrains.com>
2025-10-01 09:14:52 +0200

    PY-82344 Add resources and icons for PyCharm unified promotion
    GitOrigin-RevId: 2aec51539234a3255795fc94f33d2bf711f89794

---
+++ python/ide/impl/intellij.pycharm.community.ide.impl.promotion/src/com/intellij/pycharm/community/ide/impl/promotion/icons/PycharmCommunityIdeImplPromotionIcons.java
@@ -0,0 +1,26 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.pycharm.community.ide.impl.promotion.icons;
+
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+@org.jetbrains.annotations.ApiStatus.Internal
+public final class PycharmCommunityIdeImplPromotionIcons {
+  private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
+    return IconManager.getInstance().loadRasterizedIcon(path, PycharmCommunityIdeImplPromotionIcons.class.getClassLoader(), cacheKey, flags);
+  }
+
+  public static final class Backgrounds {
+    /** 360x128 */ public static final @NotNull Icon Promotion_bg = load("promo/backgrounds/promotion_bg.svg", -882473096, 0);
+  }
+
+  public static final class Images {
+    /** 384x242 */ public static final @NotNull Icon Popup_img = load("promo/images/popup_img.svg", -1065917548, 0);
+  }
+}
