bdb3dba8465d0910e0694c63c94bd5269f27112a
Egor Malyshev <egor.malyshev@jetbrains.com>
2025-10-01 14:58:05 +0200

    Use current base path for en-us and all other fallback content
    (cherry picked from commit d2a7b811615335aea7aa15631309477706b0f0c1)
    
    IJ-MR-176799
    
    GitOrigin-RevId: 8b24727e5e7667cc0288ecfaf7f4ff626c20f299

--- plugins/built-in-help/src/com/jetbrains/builtInHelp/HelpSearchRequestHandler.kt
+++ plugins/built-in-help/src/com/jetbrains/builtInHelp/HelpSearchRequestHandler.kt
@@ -12,17 +12,23 @@ class HelpSearchRequestHandler : HelpRequestHandlerBase() {
   @NonNls
   override val prefix: String = "/search/"
 
-  override fun process(urlDecoder: QueryStringDecoder, request: FullHttpRequest, context: ChannelHandlerContext): Boolean {
+  override fun process(
+    urlDecoder: QueryStringDecoder,
+    request: FullHttpRequest,
+    context: ChannelHandlerContext,
+  ): Boolean {
 
     val query = urlDecoder.parameters()["query"]?.get(0)
     val maxHitsParam = urlDecoder.parameters()["maxHits"]?.get(0)
 
     sendData(
-      HelpSearch.search(query, if (maxHitsParam != null) Integer.parseInt(maxHitsParam) else 100).encodeToByteArray(),
-             "data.json",
-             request,
-             context.channel(),
-             request.headers())
+      HelpSearch.search(query,
+                        if (maxHitsParam != null) Integer.parseInt(maxHitsParam) else 100,
+                        getRequestLocalePath(request)).encodeToByteArray(),
+      "data.json",
+      request,
+      context.channel(),
+      request.headers())
 
     return true
   }
