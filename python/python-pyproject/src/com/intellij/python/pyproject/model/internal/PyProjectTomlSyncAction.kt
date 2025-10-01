d03c264a1bcf4909f0ff77a79d7115c2f3d8b997
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2025-10-01 21:21:09 +0200

    PY-82580: Move action i18n to xml
    GitOrigin-RevId: 280e878cd2335039fc2bac2e7cfa5c1ee91e7217

--- python/python-pyproject/src/com/intellij/python/pyproject/model/internal/PyProjectTomlSyncAction.kt
+++ python/python-pyproject/src/com/intellij/python/pyproject/model/internal/PyProjectTomlSyncAction.kt
@@ -4,7 +4,7 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
-internal class PyProjectTomlSyncAction : AnAction(PyProjectTomlBundle.message("action.PyProjectTomlSyncAction.text")) {
+internal class PyProjectTomlSyncAction : AnAction() {
 
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.project ?: return
