7af128036e710b5b818a3f5568b08314f9c88614
Anna Antonova <anna.antonova@jetbrains.com>
2025-10-01 10:17:07 +0200

    [kotlin] KTIJ-22909 rewrite using analysis API
    GitOrigin-RevId: f79a379d9dd9d0795cc1abc45ed64cf3dbe25966

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/ImplicitNullableNothingTypeInspection.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/ImplicitNullableNothingTypeInspection.kt
@@ -1,34 +1,82 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.inspections
 
-import org.jetbrains.kotlin.descriptors.CallableMemberDescriptor
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.modcommand.ModPsiUpdater
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
+import org.jetbrains.kotlin.analysis.api.KaSession
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.symbols.KaCallableSymbol
+import org.jetbrains.kotlin.idea.base.psi.textRangeIn
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.caches.resolve.resolveToDescriptorIfAny
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.inspections.IntentionBasedInspection
-import org.jetbrains.kotlin.idea.intentions.SpecifyTypeExplicitlyIntention
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinApplicableInspectionBase
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinModCommandQuickFix
+import org.jetbrains.kotlin.idea.codeinsights.impl.base.CallableReturnTypeUpdaterUtils.TypeInfo
+import org.jetbrains.kotlin.idea.codeinsights.impl.base.CallableReturnTypeUpdaterUtils.getTypeInfo
+import org.jetbrains.kotlin.idea.codeinsights.impl.base.CallableReturnTypeUpdaterUtils.updateType
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtCallableDeclaration
+import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtProperty
-import org.jetbrains.kotlin.types.typeUtil.isNullableNothing
+import org.jetbrains.kotlin.psi.KtVisitorVoid
 
-class ImplicitNullableNothingTypeInspection : IntentionBasedInspection<KtCallableDeclaration>(
-    intention = SpecifyTypeExplicitlyIntention::class,
-    additionalChecker = { declaration -> declaration.check() },
-    problemText = KotlinBundle.message("inspection.implicit.nullable.nothing.type.display.name")
-) {
-    override fun inspectionTarget(element: KtCallableDeclaration) = element.nameIdentifier
-}
 
-private fun KtCallableDeclaration.check(): Boolean {
-    if (!SpecifyTypeExplicitlyIntention.getTypeForDeclaration(this).isNullableNothing()) return false
-    return isVarOrOpen() && !isOverridingNullableNothing()
-}
+class ImplicitNullableNothingTypeInspection : KotlinApplicableInspectionBase.Simple<KtCallableDeclaration, Unit>() {
 
-private fun KtCallableDeclaration.isVarOrOpen() = this is KtProperty && this.isVar || hasModifier(KtTokens.OPEN_KEYWORD)
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): KtVisitorVoid = object : KtVisitorVoid() {
+        override fun visitNamedFunction(function: KtNamedFunction) {
+            visitTargetElement(function, holder, isOnTheFly)
+        }
 
-private fun KtCallableDeclaration.isOverridingNullableNothing(): Boolean {
-    if (!hasModifier(KtTokens.OVERRIDE_KEYWORD)) return false
-    val descriptor = resolveToDescriptorIfAny() as? CallableMemberDescriptor ?: return false
-    return descriptor.overriddenDescriptors.any { it.returnType?.isNullableNothing() == true }
+        override fun visitProperty(property: KtProperty) {
+            visitTargetElement(property, holder, isOnTheFly)
+        }
+    }
+
+    override fun isApplicableByPsi(element: KtCallableDeclaration): Boolean {
+        if (element.typeReference != null) return false
+
+        val isVarProperty = (element as? KtProperty)?.isVar == true
+        val isOpen = element.hasModifier(KtTokens.OPEN_KEYWORD)
+        if (!isVarProperty && !isOpen) return false
+
+        return element.nameIdentifier != null
+    }
+
+    override fun getApplicableRanges(element: KtCallableDeclaration): List<TextRange> {
+        val name = element.nameIdentifier ?: return emptyList()
+        return listOf(name.textRangeIn(element))
+    }
+
+    override fun KaSession.prepareContext(element: KtCallableDeclaration): Unit? {
+        val symbol = element.symbol as? KaCallableSymbol ?: return null
+
+        val returnType = symbol.returnType
+        if (!(returnType.isNothingType && returnType.isMarkedNullable)) return null
+
+        if (element.hasModifier(KtTokens.OVERRIDE_KEYWORD)) {
+            if (symbol.allOverriddenSymbols.any { it.returnType.isNothingType && it.returnType.isMarkedNullable }) return null
+        }
+        return Unit
+    }
+
+    override fun getProblemDescription(element: KtCallableDeclaration, context: Unit): String =
+        KotlinBundle.message("inspection.implicit.nullable.nothing.type.display.name")
+
+    override fun createQuickFix(
+        element: KtCallableDeclaration,
+        context: Unit,
+    ): KotlinModCommandQuickFix<KtCallableDeclaration> = object : KotlinModCommandQuickFix<KtCallableDeclaration>() {
+        override fun getFamilyName(): String =
+            if (element is KtNamedFunction) KotlinBundle.message("specify.return.type.explicitly")
+            else KotlinBundle.message("specify.type.explicitly")
+
+        override fun applyFix(project: Project, element: KtCallableDeclaration, updater: ModPsiUpdater) {
+            val info: TypeInfo = analyze(element) {
+                getTypeInfo(element, useSmartCastType = true)
+            }
+            updateType(element, info, project, updater)
+        }
+    }
 }
