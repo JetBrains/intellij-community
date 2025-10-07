e66cf33b42c5ae870006676a127e1d1a8ed26373
Egor Ushakov <egor.ushakov@jetbrains.com>
2025-10-07 17:24:39 +0200

    convert to kotlin
    GitOrigin-RevId: 705c96b85529fa7f5cd2481d081a2745cc231115

--- java/debugger/impl/src/com/intellij/debugger/actions/JvmSmartStepIntoActionHandler.kt
+++ java/debugger/impl/src/com/intellij/debugger/actions/JvmSmartStepIntoActionHandler.kt
@@ -1,104 +1,67 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.debugger.actions;
+package com.intellij.debugger.actions
 
-import com.intellij.debugger.JavaDebuggerBundle;
-import com.intellij.debugger.SourcePosition;
-import com.intellij.debugger.engine.MethodFilter;
-import com.intellij.debugger.impl.DebuggerSession;
-import com.intellij.debugger.impl.DebuggerUtilsEx;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.PsiElement;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.xdebugger.XDebugSession;
-import com.intellij.xdebugger.XSourcePosition;
-import com.intellij.xdebugger.frame.XSuspendContext;
-import com.intellij.xdebugger.stepping.ForceSmartStepIntoSource;
-import com.intellij.xdebugger.stepping.XSmartStepIntoHandler;
-import com.intellij.xdebugger.stepping.XSmartStepIntoVariant;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.concurrency.Promise;
-import org.jetbrains.concurrency.Promises;
+import com.intellij.debugger.JavaDebuggerBundle
+import com.intellij.debugger.actions.JvmSmartStepIntoActionHandler.JvmSmartStepIntoVariant
+import com.intellij.debugger.engine.MethodFilter
+import com.intellij.debugger.impl.DebuggerSession
+import com.intellij.debugger.impl.DebuggerUtilsEx
+import com.intellij.openapi.util.TextRange
+import com.intellij.xdebugger.XDebugSession
+import com.intellij.xdebugger.XSourcePosition
+import com.intellij.xdebugger.frame.XSuspendContext
+import com.intellij.xdebugger.stepping.ForceSmartStepIntoSource
+import com.intellij.xdebugger.stepping.XSmartStepIntoHandler
+import com.intellij.xdebugger.stepping.XSmartStepIntoVariant
+import org.jetbrains.concurrency.Promise
+import org.jetbrains.concurrency.rejectedPromise
+import javax.swing.Icon
 
-import javax.swing.*;
-import java.util.List;
-
-public class JvmSmartStepIntoActionHandler extends XSmartStepIntoHandler<JvmSmartStepIntoActionHandler.JvmSmartStepIntoVariant> {
-  private final DebuggerSession mySession;
-
-  public JvmSmartStepIntoActionHandler(@NotNull DebuggerSession session) {
-    mySession = session;
+internal class JvmSmartStepIntoActionHandler(private val session: DebuggerSession) : XSmartStepIntoHandler<JvmSmartStepIntoVariant>() {
+  override fun computeSmartStepVariantsAsync(position: XSourcePosition): Promise<List<JvmSmartStepIntoVariant>> {
+    return findVariants(position, true)
   }
 
-  @Override
-  public @NotNull Promise<List<JvmSmartStepIntoVariant>> computeSmartStepVariantsAsync(@NotNull XSourcePosition position) {
-    return findVariants(position, true);
+  override fun computeStepIntoVariants(position: XSourcePosition): Promise<List<JvmSmartStepIntoVariant>> {
+    return findVariants(position, false)
   }
 
-  @Override
-  public @NotNull Promise<List<JvmSmartStepIntoVariant>> computeStepIntoVariants(@NotNull XSourcePosition position) {
-    return findVariants(position, false);
-  }
-
-  private Promise<List<JvmSmartStepIntoVariant>> findVariants(@NotNull XSourcePosition xPosition, boolean smart) {
-    SourcePosition position = DebuggerUtilsEx.toSourcePosition(xPosition, mySession.getProject());
-    JvmSmartStepIntoHandler handler = JvmSmartStepIntoHandler.EP_NAME.findFirstSafe(h -> h.isAvailable(position));
-    if (handler != null) {
-      Promise<List<SmartStepTarget>> targets =
-        smart ? handler.findSmartStepTargetsAsync(position, mySession) : handler.findStepIntoTargets(position, mySession);
-      return targets.then(results -> ContainerUtil.map(results, target -> new JvmSmartStepIntoVariant(target, handler)));
+  private fun findVariants(xPosition: XSourcePosition, smart: Boolean): Promise<List<JvmSmartStepIntoVariant>> {
+    val position = DebuggerUtilsEx.toSourcePosition(xPosition, session.project)
+    val handler = JvmSmartStepIntoHandler.EP_NAME.findFirstSafe { it.isAvailable(position) } 
+                  ?: return rejectedPromise()
+    val targets = if (smart) handler.findSmartStepTargetsAsync(position, session) else handler.findStepIntoTargets(position, session)
+    return targets.then { results ->
+      results.map { JvmSmartStepIntoVariant(it, handler) }
     }
-    return Promises.rejectedPromise();
   }
 
-  @Override
-  public @NotNull List<JvmSmartStepIntoVariant> computeSmartStepVariants(@NotNull XSourcePosition position) {
-    throw new IllegalStateException("Should not be called");
+  override fun computeSmartStepVariants(position: XSourcePosition): List<JvmSmartStepIntoVariant> {
+    throw IllegalStateException("Should not be called")
   }
 
-  @Override
-  public String getPopupTitle() {
-    return JavaDebuggerBundle.message("title.smart.step.popup");
+  override fun getPopupTitle(): String = JavaDebuggerBundle.message("title.smart.step.popup")
+
+  override fun stepIntoEmpty(session: XDebugSession) {
+    session.forceStepInto()
   }
 
-  @Override
-  public void stepIntoEmpty(XDebugSession session) {
-    session.forceStepInto();
+  override fun startStepInto(variant: JvmSmartStepIntoVariant, context: XSuspendContext?) {
+    session.stepInto(true, variant.methodFilter)
   }
 
-  @Override
-  public void startStepInto(@NotNull JvmSmartStepIntoVariant variant, @Nullable XSuspendContext context) {
-    mySession.stepInto(true, variant.myMethodFilter);
-  }
+  internal class JvmSmartStepIntoVariant(
+    private val target: SmartStepTarget,
+    handler: JvmSmartStepIntoHandler,
+  ) : XSmartStepIntoVariant(), ForceSmartStepIntoSource {
+    val methodFilter: MethodFilter? = handler.createMethodFilter(target)
 
-  static class JvmSmartStepIntoVariant extends XSmartStepIntoVariant implements ForceSmartStepIntoSource {
-    private final SmartStepTarget myTarget;
-    private final MethodFilter myMethodFilter;
+    override fun getText(): String = target.presentation
 
-    JvmSmartStepIntoVariant(SmartStepTarget target, JvmSmartStepIntoHandler handler) {
-      myTarget = target;
-      myMethodFilter = handler.createMethodFilter(target);
-    }
+    override fun getIcon(): Icon? = target.icon
 
-    @Override
-    public String getText() {
-      return myTarget.getPresentation();
-    }
+    override fun getHighlightRange(): TextRange? = target.highlightElement?.getTextRange()
 
-    @Override
-    public @Nullable Icon getIcon() {
-      return myTarget.getIcon();
-    }
-
-    @Override
-    public @Nullable TextRange getHighlightRange() {
-      PsiElement element = myTarget.getHighlightElement();
-      return element != null ? element.getTextRange() : null;
-    }
-
-    @Override
-    public boolean needForceSmartStepInto() {
-      return myTarget instanceof ForceSmartStepIntoSource forceSmartStepIntoSource && forceSmartStepIntoSource.needForceSmartStepInto();
-    }
+    override fun needForceSmartStepInto(): Boolean = target is ForceSmartStepIntoSource && target.needForceSmartStepInto()
   }
 }
