041d18afffacb0df9ec3314567eb0ca5f2056bfe
Dmitriy.Panov <dmitriy.panov@jetbrains.com>
2025-10-07 13:51:05 +0200

    build scripts: no need to generate a Maven artifact for a library module
    IJ-CR-167471
    
    GitOrigin-RevId: 8bdb511651b51fdb65d783237d00c4ebf7bd94b6

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/libraries/libraries.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/libraries/libraries.kt
@@ -2,6 +2,7 @@
 package org.jetbrains.intellij.build.impl.libraries
 
 import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.jps.model.java.JavaSourceRootType
 import org.jetbrains.jps.model.module.JpsModule
 
 /**
@@ -11,57 +12,6 @@ import org.jetbrains.jps.model.module.JpsModule
  */
 @ApiStatus.Internal
 fun JpsModule.isLibraryModule(): Boolean {
-  return name in LIBRARY_MODULE_NAMES
+  return name.startsWith("intellij.libraries.") &&
+         sourceRoots.none { it.rootType == JavaSourceRootType.SOURCE }
 }
-
-/**
- * Please mind that intellij.libraries.microba and intellij.libraries.cglib are exceptions and should not be included in this list
- */
-private val LIBRARY_MODULE_NAMES: Set<String> = setOf(
-  "intellij.libraries.aalto.xml",
-  "intellij.libraries.asm",
-  "intellij.libraries.assertj.core",
-  "intellij.libraries.caffeine",
-  "intellij.libraries.compose.foundation.desktop",
-  "intellij.libraries.compose.foundation.desktop.junit",
-  "intellij.libraries.compose.runtime.desktop",
-  "intellij.libraries.fastutil",
-  "intellij.libraries.grpc",
-  "intellij.libraries.grpc.netty.shaded",
-  "intellij.libraries.gson",
-  "intellij.libraries.hamcrest",
-  "intellij.libraries.icu4j",
-  "intellij.libraries.ion",
-  "intellij.libraries.jackson",
-  "intellij.libraries.jackson.databind",
-  "intellij.libraries.jackson.jr.objects",
-  "intellij.libraries.jackson.module.kotlin",
-  "intellij.libraries.junit4",
-  "intellij.libraries.junit5",
-  "intellij.libraries.kotlinTest",
-  "intellij.libraries.kotlinTestAssertionsCoreJvm",
-  "intellij.libraries.kotlin.reflect",
-  "intellij.libraries.kotlinx.coroutines.slf4j",
-  "intellij.libraries.lz4",
-  "intellij.libraries.ktor.client",
-  "intellij.libraries.ktor.client.cio",
-  "intellij.libraries.ktor.io",
-  "intellij.libraries.ktor.network.tls",
-  "intellij.libraries.ktor.utils",
-  "intellij.libraries.kotlinx.io",
-  "intellij.libraries.skiko",
-  "intellij.libraries.coil",
-  "intellij.libraries.miglayout.swing",
-  "intellij.libraries.proxy.vole",
-  "intellij.libraries.mvstore",
-  "intellij.libraries.rhino",
-  "intellij.libraries.testng",
-  "intellij.libraries.winp",
-  "intellij.libraries.xml.rpc",
-  "intellij.libraries.xstream",
-  "intellij.libraries.xz",
-  "intellij.libraries.dokka",
-  "intellij.libraries.guava",
-  "intellij.libraries.hash4j",
-  "intellij.libraries.xerces",
-)
