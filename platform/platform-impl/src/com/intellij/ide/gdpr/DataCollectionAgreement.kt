6fdf91ac422546d35bf5283011cb8a68186cfc3a
Ilia Smirnov <ilia.smirnov@jetbrains.com>
2025-10-08 19:52:57 +0200

    Revert "IJPL-198533 IJPL-193751 Create a non-community module for managing TRACE consent activities and move TraceDataSharingActivity there"
    This reverts commit d60c576375554f92ec1bf6824900716b816db55a.
    
    IJ-CR-177425
    
    GitOrigin-RevId: 7901c62998ac590bb3d50cca98de97853a72ff59

--- platform/platform-impl/src/com/intellij/ide/gdpr/DataCollectionAgreement.kt
+++ platform/platform-impl/src/com/intellij/ide/gdpr/DataCollectionAgreement.kt
@@ -1,13 +1,9 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.gdpr
 
-import com.intellij.openapi.util.IntellijInternalApi
 import com.intellij.ui.LicensingFacade
-import org.jetbrains.annotations.ApiStatus
 
-@ApiStatus.Internal
-@IntellijInternalApi
-enum class DataCollectionAgreement(private val code: Char) {
+internal enum class DataCollectionAgreement(private val code: Char) {
   YES('Y'),
   NOT_SET('X'),
   NO('N');
