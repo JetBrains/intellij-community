65652884d1dbf7c0403254e2d9aa1798177f9b7b
Ilia Smirnov <ilia.smirnov@jetbrains.com>
2025-10-08 19:44:57 +0200

    Revert "IJPL-193751 Allow loading TraceConsentManager from JetBrains plugins only"
    This reverts commit 59e7b7dac8897998d0edca98885955be6cae1fee.
    
    IJ-CR-177425
    
    GitOrigin-RevId: e5077d7f4df1cd35c2fc9615b268db0e4481de22

--- platform/platform-impl/src/com/intellij/ide/gdpr/trace/TraceConsentManager.kt
+++ platform/platform-impl/src/com/intellij/ide/gdpr/trace/TraceConsentManager.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.gdpr.trace
 
-import com.intellij.internal.statistic.utils.getPluginInfo
 import com.intellij.openapi.extensions.ExtensionPointName
 import org.jetbrains.annotations.ApiStatus
 
@@ -22,10 +21,6 @@ interface TraceConsentManager {
     val EP_NAME: ExtensionPointName<TraceConsentManager> = ExtensionPointName("com.intellij.ide.gdpr.traceConsentManager")
 
     @JvmStatic
-    fun getInstance(): TraceConsentManager? {
-      return EP_NAME.findFirstSafe {
-        getPluginInfo(it.javaClass).isDevelopedByJetBrains()
-      }
-    }
+    fun getInstance(): TraceConsentManager? = EP_NAME.extensionList.firstOrNull()
   }
 }
