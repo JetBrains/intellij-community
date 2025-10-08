d9f3ae24ede5c045f8e8ebe718f2cfd9c30f6e17
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2025-10-08 22:29:58 +0200

    [vfs] add jmh benchmark
    GitOrigin-RevId: 623a4491d859b99777c7ac1356988c9f09ec1f39

---
+++ platform/platform-tests/benchmarks/src/com/intellij/openapi/util/CryptoHashBenchmark.java
@@ -0,0 +1,62 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.util;
+
+import com.intellij.openapi.vfs.newvfs.persistent.PersistentFSContentAccessor;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+/**
+ * Benchmark for MessageDigest used in VFS content storage
+ */
+@BenchmarkMode({Mode.AverageTime})
+@OutputTimeUnit(NANOSECONDS)
+@Warmup(iterations = 3, time = 2, timeUnit = SECONDS)
+@Measurement(iterations = 5, time = 5, timeUnit = SECONDS)
+@Fork(1)
+@Threads(1)
+public class CryptoHashBenchmark {
+
+  @State(Scope.Benchmark)
+  public static class DataContext {
+
+    @Param({"64", "1024", "16384"})
+    private int contentSize;
+
+    public byte[] content;
+
+    @Setup
+    public void setup() throws Exception {
+      ThreadLocalRandom rnd = ThreadLocalRandom.current();
+      content = new byte[contentSize];
+      rnd.nextBytes(content);
+    }
+
+    @TearDown
+    public void tearDown() throws Exception {
+    }
+  }
+
+  @Benchmark
+  public byte[] calculateHash(DataContext context) {
+    byte[] content = context.content;
+    return PersistentFSContentAccessor.calculateHash(content, 0, content.length);
+  }
+
+  public static void main(String[] args) throws RunnerException {
+    final Options opt = new OptionsBuilder()
+      //.mode(Mode.SampleTime)
+      .include(CryptoHashBenchmark.class.getSimpleName() + ".*")
+      .threads(1)
+      .build();
+
+    new Runner(opt).run();
+  }
+}
