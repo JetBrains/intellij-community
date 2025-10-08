fff0a5f8f6526d6022ffcce684ab537c4888e050
Anna Kozlova <anna.kozlova@jetbrains.com>
2025-10-08 16:36:15 +0200

    [kotlin] KQA-2451 rename sanity test to follow the common naming schema
    GitOrigin-RevId: 7d4f8c66f6c38f6eedf656f56fd9bd5db3f6e1e0

--- plugins/kotlin/fir/tests/test/org/jetbrains/kotlin/idea/fir/propertyBased/FirKotlinCodeInsightSanityTest.kt
+++ plugins/kotlin/fir/tests/test/org/jetbrains/kotlin/idea/fir/propertyBased/FirKotlinCodeInsightSanityTest.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginMode
 import org.jetbrains.kotlin.idea.propertyBased.KotlinCodeInsightSanityTest
 import org.jetbrains.kotlin.idea.test.setUpWithKotlinPlugin
 
-class FirKotlinCodeInsightSanity: KotlinCodeInsightSanityTest() {
+class FirKotlinCodeInsightSanityTest: KotlinCodeInsightSanityTest() {
     override val pluginMode: KotlinPluginMode = KotlinPluginMode.K2
     override fun setUp() {
         setUpWithKotlinPlugin { super.setUp() }
