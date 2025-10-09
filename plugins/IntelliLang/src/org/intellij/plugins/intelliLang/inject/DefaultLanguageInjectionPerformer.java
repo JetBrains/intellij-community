471daaa3a20f88e15f3d8bc8d9c937c99dc448e9
Alexandr Evstigneev <alexandr.evstigneev@jetbrains.com>
2025-10-09 14:24:35 +0400

    IJPL-211956 Introduced DefaultInjectorSuppressor
    Allows to suppress the injection for particular psi element on the frontend by default injector.
    
    GitOrigin-RevId: ebde4c966afd7c13c8e6c56824b6ebb3c1be9746

--- plugins/IntelliLang/src/org/intellij/plugins/intelliLang/inject/DefaultLanguageInjectionPerformer.java
+++ plugins/IntelliLang/src/org/intellij/plugins/intelliLang/inject/DefaultLanguageInjectionPerformer.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.intellij.plugins.intelliLang.inject;
 
 import com.intellij.lang.Language;
@@ -21,7 +21,7 @@ final class DefaultLanguageInjectionPerformer implements FallbackInjectionPerfor
   public boolean performInjection(@NotNull MultiHostRegistrar registrar,
                                   @NotNull Injection injection,
                                   @NotNull PsiElement context) {
-    if (!(context instanceof PsiLanguageInjectionHost host) || !host.isValidHost()) {
+    if (!(context instanceof PsiLanguageInjectionHost host) || !host.isValidHost() || DefaultInjectorSuppressorKt.isSuppressedFor(host)) {
       return false;
     }
 
