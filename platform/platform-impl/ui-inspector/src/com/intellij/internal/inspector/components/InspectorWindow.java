15aab21a85817fddd0632bcb882d25af22f2e9c9
Nadia Tarashkevich <nadia.tarashkevich@jetbrains.com>
2025-10-09 16:21:57 +0300

    IJPL-211726 UI inspector: improve component size display
    Show hint only once
    
    GitOrigin-RevId: d49a5a195bf5cd6bfcb647971a4bc0bf3ba1e808

--- platform/platform-impl/ui-inspector/src/com/intellij/internal/inspector/components/InspectorWindow.java
+++ platform/platform-impl/ui-inspector/src/com/intellij/internal/inspector/components/InspectorWindow.java
@@ -205,8 +205,15 @@ public final class InspectorWindow extends JDialog implements Disposable {
       topPanel.add(banner);
       // Add a subtle, visible hint about navigation to help discovery without being intrusive
       String altHoverHint = InternalActionsBundle.message("ui.inspector.hint.alt.hover.next");
-      InlineBanner hintBanner = new InlineBanner(altHoverHint, EditorNotificationPanel.Status.Info);
-      topPanel.add(hintBanner);
+      // Show once by default; hide permanently after user closes it
+      String altHoverHintKey = "ui.inspector.hint.alt.hover.next.dismissed";
+      PropertiesComponent props = PropertiesComponent.getInstance();
+      if (!props.getBoolean(altHoverHintKey, false)) {
+        InlineBanner hintBanner = new InlineBanner(altHoverHint, EditorNotificationPanel.Status.Info)
+          .showCloseButton(true)
+          .setCloseAction(() -> props.setValue(altHoverHintKey, true));
+        topPanel.add(hintBanner);
+      }
     }
 
     topPanel.add(navBarScroll);
