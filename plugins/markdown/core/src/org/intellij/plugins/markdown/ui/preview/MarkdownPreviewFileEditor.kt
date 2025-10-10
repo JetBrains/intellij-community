617597eb41aee97f17b3536e753e20174e17e3c6
Vladimir Kharitonov <Vladimir.Kharitonov@jetbrains.com>
2025-10-10 23:18:41 +0200

    IJPL-190637 MarkdownPreviewFileEditor: trace invalid state
    GitOrigin-RevId: d9b12e98c5c16947b37f6460c85369f40fd1f682

--- plugins/markdown/core/src/org/intellij/plugins/markdown/ui/preview/MarkdownPreviewFileEditor.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/ui/preview/MarkdownPreviewFileEditor.kt
@@ -214,8 +214,18 @@ class MarkdownPreviewFileEditor(
 
   @RequiresEdt
   private suspend fun updateHtml() {
-    val panel = this.panel ?: return
-    if (!file.isValid || isDisposed) {
+    val panel = this.panel ?: run {
+      logger.warn("MarkdownPreviewFileEditor: panel is null, cannot update preview")
+      return
+    }
+
+    if (isDisposed) {
+      logger.warn("MarkdownPreviewFileEditor: cannot update preview for disposed file ${file.path}")
+      return
+    }
+
+    if (!file.isValid) {
+      logger.warn("MarkdownPreviewFileEditor: Cannot update preview for invalid file ${file.path}")
       return
     }
 
