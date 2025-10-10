659247781ed111b132c7254660c72aea19c7563a
Jinseong Jeon <jsjeon@google.com>
2025-10-09 17:28:54 -0700

    K2 UAST: FirKotlinUastLibraryPsiProviderService#provide with context
    ...so that the lib PSI provider can distinguish ambiguous candidates
    
    ^KTIJ-35935 fixed
    
    closes https://github.com/JetBrains/intellij-community/pull/3269
    
    GitOrigin-RevId: 185f7eda6e12680809125061c0c2aacbb6c930a1

--- plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/internal/FirKotlinUastLibraryPsiProviderService.kt
+++ plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/internal/FirKotlinUastLibraryPsiProviderService.kt
@@ -6,15 +6,26 @@ import com.intellij.openapi.components.service
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.analysis.api.symbols.KaSymbol
+import org.jetbrains.kotlin.psi.KtElement
 
 /**
  * This service is required to add an ability to provide correct [PsiElement] for libraries in UAST/Lint CLI mode
  */
 interface FirKotlinUastLibraryPsiProviderService {
-    fun KaSession.provide(symbol: KaSymbol): PsiElement?
+    /**
+     * Provide [PsiElement] from libraries that corresponds to the given [symbol].
+     *
+     * If multiple candidates are available, an extra [context] will be used.
+     * For example, for the property symbol and its access, with a context like
+     * LHS or RHS of an assignment, either accessor would be appropriate, not both.
+     */
+    fun KaSession.provide(
+        symbol: KaSymbol,
+        context: KtElement? = null,
+    ): PsiElement?
 
     class Default : FirKotlinUastLibraryPsiProviderService {
-        override fun KaSession.provide(symbol: KaSymbol): PsiElement? = symbol.psi
+        override fun KaSession.provide(symbol: KaSymbol, context: KtElement?): PsiElement? = symbol.psi
     }
 
     companion object {
