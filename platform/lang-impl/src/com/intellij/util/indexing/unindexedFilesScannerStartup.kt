278fba7b9b0fd2bd3dfaad5adf2d75ea8651444f
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2025-10-12 14:13:01 +0200

    Revert "[indexing] IJPL-207117 Schedule scanning synchronously from ProjectFileBasedIndexStartupActivity to make sure that smartReadAction waits for the first scanning"
    This reverts commit 5174b49bdf7ec50d8a815dc028f568a9436970c3.
    
    GitOrigin-RevId: 15f57fda445c93779042053628ee65e55afa9bfc

--- platform/lang-impl/src/com/intellij/util/indexing/unindexedFilesScannerStartup.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/unindexedFilesScannerStartup.kt
@@ -6,7 +6,6 @@ import com.intellij.openapi.application.readAction
 import com.intellij.openapi.application.readActionBlocking
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
-import com.intellij.openapi.util.ThrowableComputable
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileWithId
@@ -146,15 +145,11 @@ private fun scheduleFullScanning(
   }
   else CompletableDeferred(Unit)
   val parameters = CompletableDeferred(ScanningIterators(indexingReason, null, null, fullScanningType))
-  ApplicationManager.getApplication().invokeAndWait {
-    ApplicationManager.getApplication().runWriteIntentReadAction(ThrowableComputable { // makes scanning to be scheduled immediately
-      UnindexedFilesScanner(project, true, isFilterUpToDate,
-                            someDirtyFilesScheduledForIndexing.asCompletableFuture(),
-                            forceCheckingForOutdatedIndexesUsingFileModCount = notSeenIds !is AllNotSeenDirtyFileIds,
-                            scanningParameters = parameters)
-        .queue()
-    })
-  }
+  UnindexedFilesScanner(project, true, isFilterUpToDate,
+                        someDirtyFilesScheduledForIndexing.asCompletableFuture(),
+                        forceCheckingForOutdatedIndexesUsingFileModCount = notSeenIds !is AllNotSeenDirtyFileIds,
+                        scanningParameters = parameters)
+    .queue()
   return someDirtyFilesScheduledForIndexing
 }
 
@@ -178,14 +173,10 @@ private fun scheduleDirtyFilesScanning(
                          DirtyFilesIndexableFilesIterator(projectDirtyFilesFromOrphanQueue, true))
 
   val scanningIterators = CompletableDeferred(ScanningIterators(indexingReason, iterators, null, partialScanningType))
-  ApplicationManager.getApplication().invokeAndWait {
-    ApplicationManager.getApplication().runWriteIntentReadAction(ThrowableComputable { // makes scanning to be scheduled immediately
-      UnindexedFilesScanner(project, true, true,
-                            projectDirtyFiles.asCompletableFuture(),
-                            scanningParameters = scanningIterators)
-        .queue()
-    })
-  }
+  UnindexedFilesScanner(project, true, true,
+                        projectDirtyFiles.asCompletableFuture(),
+                        scanningParameters = scanningIterators)
+    .queue()
   return projectDirtyFiles
 }
 
