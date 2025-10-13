011c111b865ba87cbfbcdfb5b3c7a3c272951c1d
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2025-10-13 23:50:48 +0200

    [python] PY-84910 PY-79486: (WIP) Replace old SDK configuration process with new one if enabled.
    When registry key is enabled, we replace all other SDK configurations (done by inspection and open processors) with a new one.
    
    We also have an action to start this process
    
    GitOrigin-RevId: 425ae78b769c5b5d40974cee9f19227410f63d45

--- python/python-sdk-configurator/backend/src/impl/rpcBridge/SdkConfiguratorApiImpl.kt
+++ python/python-sdk-configurator/backend/src/impl/rpcBridge/SdkConfiguratorApiImpl.kt
@@ -3,10 +3,10 @@ package com.intellij.python.sdkConfigurator.backend.impl.rpcBridge
 import com.intellij.platform.project.ProjectId
 import com.intellij.platform.project.findProject
 import com.intellij.python.sdkConfigurator.backend.impl.configureSdkAutomatically
-import com.intellij.python.sdkConfigurator.common.ModuleName
-import com.intellij.python.sdkConfigurator.common.SdkConfiguratorApi
+import com.intellij.python.sdkConfigurator.common.impl.ModuleName
+import com.intellij.python.sdkConfigurator.common.impl.SdkConfiguratorBackEndApi
 
-internal object SdkConfiguratorApiImpl : SdkConfiguratorApi {
+internal object SdkConfiguratorApiImpl : SdkConfiguratorBackEndApi {
   override suspend fun configureSdkAutomatically(projectId: ProjectId, onlyModules: Set<ModuleName>) {
     configureSdkAutomatically(projectId.findProject(), onlyModules)
   }
