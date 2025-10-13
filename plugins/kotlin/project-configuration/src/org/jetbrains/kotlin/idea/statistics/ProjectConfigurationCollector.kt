8b353c02b7bede7f41a59e044a4df76fcba96252
Vlad Koshkin <vladislav.koshkin@jetbrains.com>
2025-10-13 23:56:41 +0200

    [kotlin] Fix tests after kt-master merge
    GitOrigin-RevId: 1467e006a1864e40310b280498f3425bfd41fbc8

--- plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/statistics/ProjectConfigurationCollector.kt
+++ plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/statistics/ProjectConfigurationCollector.kt
@@ -82,7 +82,7 @@ internal class ProjectConfigurationCollector : ProjectUsagesCollector() {
         }
     }
 
-    private val GROUP = EventLogGroup("kotlin.project.configuration", 46)
+    private val GROUP = EventLogGroup("kotlin.project.configuration", 47)
 
     private val systemField = EventFields.String("system", listOf("JPS", "Maven", "Gradle", "unknown"))
     private val platformField = EventFields.String("platform", composePlatformFields())
