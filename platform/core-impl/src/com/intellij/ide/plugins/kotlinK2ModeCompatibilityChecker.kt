931efb88b51208a59cf8fff6185703851a9f7086
Boris Krylov <boris.krylov@jetbrains.com>
2025-10-14 03:02:23 +0300

    [fleet][swift] remove fleet.backend.mercury reference
    GitOrigin-RevId: 6a63316aa354d2f9c4b1182dd98babd7225af98b

--- platform/core-impl/src/com/intellij/ide/plugins/kotlinK2ModeCompatibilityChecker.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/kotlinK2ModeCompatibilityChecker.kt
@@ -9,7 +9,7 @@ import java.io.IOException
 
 private val pluginIdsToIgnoreK2KotlinCompatibility: Set<String> = buildSet {
   System.getProperty("idea.kotlin.plugin.plugin.ids.to.ignore.k2.compatibility")?.split(',')?.mapTo(this) { it.trim() }
-  addAll(listOf("fleet.backend.kotlin", "fleet.backend.mercury"))
+  addAll(listOf("fleet.backend.kotlin"))
 
   try {
     // KTIJ-30545
