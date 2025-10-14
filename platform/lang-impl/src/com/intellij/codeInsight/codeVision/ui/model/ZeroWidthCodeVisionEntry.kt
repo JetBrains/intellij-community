100f6b3b3fe89e6bc88b1dc3de7591b6802193b7
Egor <egor.skrypnikov@jetbrains.com>
2025-10-14 14:35:51 +0200

    [Rider] RIDER-131203 move zero width lens to the platform to pass tests
    GitOrigin-RevId: a20dead0721f0e994a60d3ffcf94e1d0118762be

---
+++ platform/lang-impl/src/com/intellij/codeInsight/codeVision/ui/model/ZeroWidthCodeVisionEntry.kt
@@ -0,0 +1,14 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.codeInsight.codeVision.ui.model
+
+import com.intellij.codeInsight.codeVision.CodeVisionEntry
+
+class ZeroWidthCodeVisionEntry(
+  providerId: String
+) : CodeVisionEntry(
+  providerId,
+  icon = null,
+  longPresentation = "",
+  tooltip = "",
+  extraActions = emptyList()
+)
