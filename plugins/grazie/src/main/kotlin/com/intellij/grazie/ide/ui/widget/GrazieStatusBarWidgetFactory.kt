e4d042cafb48c96b46105ae2aa99b39e314cca0d
Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
2025-10-15 18:49:31 +0000

    [grazie] IJPL-213087 Don't show Grazie widget until explicitly enabled
    Merge-request: IJ-MR-178626
    Merged-by: Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
    
    GitOrigin-RevId: e549bc7034e0cb7d565317470ee04da3da70959a

--- plugins/grazie/src/main/kotlin/com/intellij/grazie/ide/ui/widget/GrazieStatusBarWidgetFactory.kt
+++ plugins/grazie/src/main/kotlin/com/intellij/grazie/ide/ui/widget/GrazieStatusBarWidgetFactory.kt
@@ -1,9 +1,14 @@
 package com.intellij.grazie.ide.ui.widget
 
 import com.intellij.grazie.GrazieBundle
+import com.intellij.grazie.GrazieConfig
+import com.intellij.grazie.GrazieConfig.State.Processing.*
 import com.intellij.grazie.cloud.GrazieCloudConnector
+import com.intellij.grazie.ide.msg.GrazieInitializerManager
+import com.intellij.grazie.ide.msg.GrazieStateLifecycle
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.wm.StatusBar
@@ -11,7 +16,10 @@ import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.StatusBarWidgetFactory
 import com.intellij.openapi.wm.impl.status.widget.StatusBarWidgetsManager
 
-internal class GrazieStatusBarWidgetFactory : StatusBarWidgetFactory {
+internal class GrazieStatusBarWidgetFactory : StatusBarWidgetFactory, GrazieStateLifecycle {
+
+  init { service<GrazieInitializerManager>().register(this) }
+
   override fun getId(): String {
     return widgetId
   }
@@ -21,7 +29,9 @@ internal class GrazieStatusBarWidgetFactory : StatusBarWidgetFactory {
   }
 
   override fun isAvailable(project: Project): Boolean {
-    return Registry.`is`("grazie.enable.status.bar.widget", false) && !GrazieCloudConnector.hasAdditionalConnectors()
+    return Registry.`is`("grazie.enable.status.bar.widget", false)
+           && !GrazieCloudConnector.hasAdditionalConnectors()
+           && GrazieConfig.get().explicitlyChosenProcessing == Cloud
   }
 
   override fun createWidget(project: Project): StatusBarWidget {
@@ -36,13 +46,15 @@ internal class GrazieStatusBarWidgetFactory : StatusBarWidgetFactory {
     return true
   }
 
+  override fun update(prevState: GrazieConfig.State, newState: GrazieConfig.State) {
+    if (newState.explicitlyChosenProcessing != prevState.explicitlyChosenProcessing) {
+      ProjectManager.getInstance().openProjects.forEach { project ->
+        project.service<StatusBarWidgetsManager>().updateWidget(this)
+      }
+    }
+  }
+
   companion object {
     const val widgetId = "GrazieStatusBarWidget"
-
-    internal fun findFactory(project: Project): GrazieStatusBarWidgetFactory? {
-      @Suppress("IncorrectServiceRetrieving")
-      val manager = project.service<StatusBarWidgetsManager>()
-      return manager.findWidgetFactory(widgetId) as? GrazieStatusBarWidgetFactory
-    }
   }
 }
