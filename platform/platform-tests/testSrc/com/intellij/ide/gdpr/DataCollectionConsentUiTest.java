777b80e91ea2c97d65b0c0a6d183db76b7d492cf
Ilia Smirnov <ilia.smirnov@jetbrains.com>
2025-10-16 18:05:52 +0200

    IJPL-213049 Separate consents and local settings on the Data Sharing page
    - Get rid of consent groups
    - Create 2 DSL UI groups for consents and local settings
    - Add a title for the Data Sharing page
    
    (cherry picked from commit 943d5c11fe486b5772475d1ba7ce33723e4154f1)
    
    
    (cherry picked from commit 3a4fc8c74acb61874979985639cbe294bbd5a418)
    
    IJ-CR-183709
    
    GitOrigin-RevId: e4851cd8525d2ef2737d61adb735b99be5a63920

--- platform/platform-tests/testSrc/com/intellij/ide/gdpr/DataCollectionConsentUiTest.java
+++ platform/platform-tests/testSrc/com/intellij/ide/gdpr/DataCollectionConsentUiTest.java
@@ -1,20 +1,14 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.gdpr;
 
-import com.intellij.ide.IdeBundle;
 import com.intellij.ide.gdpr.ui.consents.ConsentForcedState;
-import com.intellij.ide.gdpr.ui.consents.ConsentGroup;
-import com.intellij.ide.gdpr.ui.consents.ConsentGroupUi;
 import com.intellij.ide.gdpr.ui.consents.ConsentUi;
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 import com.intellij.ui.LicensingFacade;
 
-import java.util.List;
-
 public class DataCollectionConsentUiTest extends BasePlatformTestCase {
   private static final String CONSENT_ID_USAGE_STATS = "rsch.send.usage.stat";
   private static final String CONSENT_ID_TRACE_DATA_COM_COLLECTION = "ai.trace.data.collection.and.use.com.policy";
-  private static final String GROUP_CONSENT_ID_DATA_COLLECTION = "data.collection";
 
   private static void setupLicensingFacade(char customerAgreementOnDetailedDataSharing) {
     LicensingFacade licensingFacade = new LicensingFacade();
@@ -50,27 +44,6 @@ public class DataCollectionConsentUiTest extends BasePlatformTestCase {
     assertNull(DataCollectionAgreement.getInstance());
   }
 
-  public void testGroupForcedStateDescription() {
-    Consent fus = new Consent(CONSENT_ID_USAGE_STATS, Version.fromString("1.1"), "Send anonymous usage statistics", "text", false, false, "en");
-    Consent trace = new Consent(CONSENT_ID_TRACE_DATA_COM_COLLECTION, Version.fromString("1.0"), "Send detailed code-related data", "text", false, false, "en");
-    ConsentGroup group = new ConsentGroup(GROUP_CONSENT_ID_DATA_COLLECTION, List.of(fus, trace));
-
-    setupLicensingFacade('X');
-    ConsentGroupUi groupUi = ConsentSettingsUi.getConsentGroupUi(group);
-    assertNotNull(groupUi);
-    assertNull(groupUi.getForcedStateDescription());
-
-    setupLicensingFacade('N');
-    groupUi = ConsentSettingsUi.getConsentGroupUi(group);
-    assertNotNull(groupUi);
-    assertEquals(IdeBundle.message("gdpr.data.collection.consent.group.setting.disabled.warning.text"), groupUi.getForcedStateDescription());
-
-    setupLicensingFacade('Y');
-    groupUi = ConsentSettingsUi.getConsentGroupUi(group);
-    assertNotNull(groupUi);
-    assertEquals(IdeBundle.message("gdpr.data.collection.consent.group.setting.enabled.warning.text"), groupUi.getForcedStateDescription());
-  }
-
   public void testUsageStatisticsConsentForcedStateDependsOnAgreement() {
     Consent fus = new Consent(CONSENT_ID_USAGE_STATS, Version.fromString("1.1"), "Send anonymous usage statistics", "text", false, false, "en");
     ConsentUi ui = ConsentSettingsUi.getConsentUi(fus);
@@ -85,7 +58,7 @@ public class DataCollectionConsentUiTest extends BasePlatformTestCase {
     var state = ui.getForcedState();
     assertNotNull(state);
     assertInstanceOf(state, ConsentForcedState.AlwaysEnabled.class);
-    assertNull(state.getDescription());
+    assertEquals("Data collection has been enabled by your organization administrator.", state.getDescription());
   }
 
   public void testTraceDataCollectionConsentForcedStateDependsOnAgreement() {
@@ -100,12 +73,12 @@ public class DataCollectionConsentUiTest extends BasePlatformTestCase {
     var state = ui.getForcedState();
     assertNotNull(state);
     assertInstanceOf(state, ConsentForcedState.ExternallyDisabled.class);
-    assertNull(state.getDescription());
+    assertEquals("Data collection has been disabled by your organization administrator.", state.getDescription());
 
     setupLicensingFacade('Y');
     state = ui.getForcedState();
     assertNotNull(state);
     assertInstanceOf(state, ConsentForcedState.AlwaysEnabled.class);
-    assertNull(state.getDescription());
+    assertEquals("Data collection has been enabled by your organization administrator.", state.getDescription());
   }
 }
