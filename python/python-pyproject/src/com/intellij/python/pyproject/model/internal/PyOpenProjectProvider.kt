0471ccb0d8c7b4c01969dc87880937293618163c
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2025-10-16 21:24:40 +0200

    [python] PY-79486, PY-84609, PY-84572: Fix `pyproject.toml`-based opener.
    1. Obey registry key.
    2. Use `readAction` to prevent race when file chaned from another thread.
    3. Extract action title to the i18n
    
    GitOrigin-RevId: eb9ca0841c13de06fb636d1e14f34d4a1b72b598

--- python/python-pyproject/src/com/intellij/python/pyproject/model/internal/PyOpenProjectProvider.kt
+++ python/python-pyproject/src/com/intellij/python/pyproject/model/internal/PyOpenProjectProvider.kt
@@ -1,6 +1,7 @@
 package com.intellij.python.pyproject.model.internal
 
 import com.intellij.ide.impl.runUnderModalProgressIfIsEdt
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.externalSystem.importing.AbstractOpenProjectProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
@@ -12,10 +13,14 @@ import kotlinx.coroutines.withContext
 internal object PyOpenProjectProvider : AbstractOpenProjectProvider() {
   override val systemId = SYSTEM_ID
 
-  override fun isProjectFile(file: VirtualFile): Boolean = @Suppress("DEPRECATION") // We have no choice: API is broken
-  (runUnderModalProgressIfIsEdt {
-    file.isFile && file.name == PY_PROJECT_TOML || file.isDirectory && file.findChild(PY_PROJECT_TOML) != null
-  })
+  override fun isProjectFile(file: VirtualFile): Boolean =
+    projectModelEnabled &&
+    @Suppress("DEPRECATION") // We have no choice: API is broken
+    (runUnderModalProgressIfIsEdt {
+      readAction { // To prevent concurrent access (file might be changed from another thread)
+        file.isFile && file.name == PY_PROJECT_TOML || file.isDirectory && file.findChild(PY_PROJECT_TOML) != null
+      }
+    })
 
   override suspend fun linkProject(projectFile: VirtualFile, project: Project) {
     val projectDirectory = withContext(Dispatchers.IO) {
