7cb0be8f1508b44f5b10cdc23bfe35e74fda226b
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2025-10-17 11:56:45 +0200

    [vcs] IJPL-213061 Log: Fix branch submenu presence
    IJ-MR-179021
    
    GitOrigin-RevId: 84dfa2b4357cbd366c8b602bd4215a51582021b5

--- plugins/git4idea/src/git4idea/ui/branch/GitBranchActionWrapper.kt
+++ plugins/git4idea/src/git4idea/ui/branch/GitBranchActionWrapper.kt
@@ -1,10 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.ui.branch
 
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionWrapper
-import com.intellij.openapi.actionSystem.DataSink
-import com.intellij.openapi.actionSystem.DataSnapshotProvider
+import com.intellij.openapi.actionSystem.*
 import com.intellij.vcs.git.actions.GitSingleRefActions
 import git4idea.GitBranch
 import git4idea.actions.branch.GitBranchActionsDataKeys
@@ -25,7 +22,37 @@ internal class GitBranchActionWrapper(
 
   companion object {
     @JvmStatic
-    fun tryWrap(action: AnAction, branch: GitBranch, selectedRepository: GitRepository, repositories: List<GitRepository>) =
-      if (action is GitSingleRefAction<*>) GitBranchActionWrapper(action, branch, selectedRepository, repositories) else action
+    fun wrapActions(
+      actions: Array<AnAction>,
+      e: AnActionEvent?,
+      branch: GitBranch,
+      selectedRepository: GitRepository,
+      repositories: List<GitRepository>,
+    ): Array<AnAction> {
+      val wrappedActions = mutableListOf<AnAction>()
+      for (action in actions) {
+        doWrapActions(wrappedActions, e, action, branch, selectedRepository, repositories)
+      }
+      return wrappedActions.toTypedArray()
+    }
+
+    private fun doWrapActions(
+      result: MutableList<AnAction>,
+      e: AnActionEvent?,
+      action: AnAction,
+      branch: GitBranch,
+      selectedRepository: GitRepository,
+      repositories: List<GitRepository>,
+    ) {
+      when (action) {
+        is GitSingleRefAction<*> -> result.add(GitBranchActionWrapper(action, branch, selectedRepository, repositories))
+        is ActionGroup -> {
+          action.getChildren(e).forEach {
+            doWrapActions(result, e, it, branch, selectedRepository, repositories)
+          }
+        }
+        else -> result.add(action)
+      }
+    }
   }
 }
