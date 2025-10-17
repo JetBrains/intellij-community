e76d8f730a0388f93dc5bb28581371c24957cf8d
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-10-17 10:42:10 +0300

    [terminal] IJPL-211122 Refactor: move blocks model modification to TerminalShellIntegration
    There will be other additional logic there.
    
    GitOrigin-RevId: 618414b9940fe07bfbed593746d7e6437d8c7449

--- plugins/terminal/frontend/src/com/intellij/terminal/frontend/view/impl/TerminalShellIntegrationImpl.kt
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/view/impl/TerminalShellIntegrationImpl.kt
@@ -4,6 +4,7 @@ import com.intellij.util.asDisposable
 import kotlinx.coroutines.CoroutineScope
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.plugins.terminal.block.reworked.TerminalBlocksModelImpl
+import org.jetbrains.plugins.terminal.block.reworked.TerminalOffset
 import org.jetbrains.plugins.terminal.block.reworked.TerminalOutputModel
 import org.jetbrains.plugins.terminal.view.TerminalShellIntegration
 
@@ -13,4 +14,26 @@ class TerminalShellIntegrationImpl(
   coroutineScope: CoroutineScope,
 ) : TerminalShellIntegration {
   override val blocksModel: TerminalBlocksModelImpl = TerminalBlocksModelImpl(outputModel, coroutineScope.asDisposable())
+
+  fun onPromptStarted(offset: TerminalOffset) {
+    blocksModel.startNewBlock(offset)
+  }
+
+  fun onPromptFinished(offset: TerminalOffset) {
+    blocksModel.updateActiveCommandBlock { block ->
+      block.copy(commandStartOffset = offset)
+    }
+  }
+
+  fun onCommandStarted(offset: TerminalOffset) {
+    blocksModel.updateActiveCommandBlock { block ->
+      block.copy(outputStartOffset = offset)
+    }
+  }
+
+  fun onCommandFinished(exitCode: Int) {
+    blocksModel.updateActiveCommandBlock { block ->
+      block.copy(exitCode = exitCode)
+    }
+  }
 }
