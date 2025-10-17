3bd10b142c9735a6247379a2141b7ef495a130a7
Roman Golyshev <roman.golyshev@jetbrains.com>
2025-10-17 16:05:37 +0200

    [kotlin] KTIJ-35994 Move `UnusedFlowInspection` to more appropriate `group.names.coroutine` inspections group
    ^KTIJ-35994 Fixed
    
    GitOrigin-RevId: db2b96715006c0ca44c9bee7fd6dd4dfd27542a7

--- plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/coroutines/UnusedFlowInspection.kt
+++ plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/coroutines/UnusedFlowInspection.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.k2.codeinsight.inspections
+package org.jetbrains.kotlin.idea.k2.codeinsight.inspections.coroutines
 
 import com.intellij.codeInspection.InspectionManager
 import com.intellij.codeInspection.ProblemDescriptor
