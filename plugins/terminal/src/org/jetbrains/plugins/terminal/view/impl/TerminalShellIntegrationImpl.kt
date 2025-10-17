af0ab248168f795a908d54d3183aefd5f497b582
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-10-17 10:49:48 +0300

    [terminal] IJPL-211122 Refactor: move TerminalShellIntegrationImpl to the shared module
    GitOrigin-RevId: 66a6ea85e1d6a3480be52b30f786729222e7576b

--- plugins/terminal/src/org/jetbrains/plugins/terminal/view/impl/TerminalShellIntegrationImpl.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/view/impl/TerminalShellIntegrationImpl.kt
@@ -1,4 +1,5 @@
-package com.intellij.terminal.frontend.view.impl
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.terminal.view.impl
 
 import com.intellij.util.asDisposable
 import kotlinx.coroutines.CoroutineScope
