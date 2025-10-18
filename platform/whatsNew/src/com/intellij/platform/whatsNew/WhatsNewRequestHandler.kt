bc0f66728a486d161c0a2b38b412761691f3bbe3
Andrey Vokin <andrey.vokin@jetbrains.com>
2025-10-18 15:42:46 +0200

    IJPL-204155 Make IDEA open a vision page after patch upgrade from CE to SID
    GitOrigin-RevId: 7260d7f252d2ae04f55ee1354cea567bac800877

--- platform/whatsNew/src/com/intellij/platform/whatsNew/WhatsNewRequestHandler.kt
+++ platform/whatsNew/src/com/intellij/platform/whatsNew/WhatsNewRequestHandler.kt
@@ -1,12 +1,12 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.whatsNew
 
-import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.fileEditor.impl.HTMLEditorProvider.ResourceHandler
 import com.intellij.openapi.fileEditor.impl.HTMLEditorProvider.ResourceHandler.ResourceResponse.NotFound
 import com.intellij.openapi.util.io.toNioPathOrNull
 import com.intellij.platform.whatsNew.WhatsNewVisionContent.Companion.LOCALHOST
 import com.intellij.platform.whatsNew.WhatsNewVisionContent.Companion.WHATS_NEW_VISION_SCHEME
+import com.intellij.util.HtmlVisionHelper.Companion.getMIMEType
 import java.io.InputStream
 import kotlin.io.path.extension
 
@@ -32,20 +32,5 @@ class WhatsNewRequestHandler(val contentProvider: WhatsNewInVisionContentProvide
     }
 
     return ResourceHandler.ResourceResponse.HandleResource(resource)
-  }
-}
-
-private val logger = logger<WhatsNewRequestHandler>()
-
-private fun getMIMEType(extension: String): String {
-  return when (extension) {
-    "gif" -> "image/gif"
-    "png" -> "image/png"
-    "jpg", "jpeg" -> "image/jpeg"
-    "svg" -> "image/svg+xml"
-    "mp4" -> "video/mp4"
-    "webm" -> "video/webm"
-    "webp" -> "image/webp"
-    else -> "".also { logger.warn("Unknown file extension: '$extension' for MIME type. Falling back to '$it'.") }
   }
 }
