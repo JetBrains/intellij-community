33493d04a6f1ff358838cea2a43622d2b6328e36
Artem.Bukhonov <artem.bukhonov@jetbrains.com>
2025-10-20 23:22:50 +0200

    [MCP Server] IJPL-213931 Add logging to Stdio proxy process
    GitOrigin-RevId: 71e8476f41cf6f782e0a65a9430c25c924ba05e1

--- plugins/mcp-server/mcpserver.stdio/src/McpStdioRunner.kt
+++ plugins/mcp-server/mcpserver.stdio/src/McpStdioRunner.kt
@@ -15,7 +15,7 @@ private const val IJ_MCP_PREFIX = "IJ_MCP_"
 const val IJ_MCP_HEADER_PREFIX: String = "${IJ_MCP_PREFIX}HEADER_"
 val IJ_MCP_SERVER_PORT: String = ::IJ_MCP_SERVER_PORT.name
 val IJ_MCP_SERVER_PROJECT_PATH: String = ::IJ_MCP_SERVER_PROJECT_PATH.name
-
+private val IJ_MCP_DEBUG: String = ::IJ_MCP_DEBUG.name
 
 suspend fun main() {
   val inputStream = System.`in`
@@ -26,46 +26,77 @@ suspend fun main() {
     return
   }
 
+  if (isDebugEnabled) {
+    info("Debug mode enabled")
+  }
+  else {
+    info("Debug mode can be enabled by setting the $IJ_MCP_DEBUG environment variable to any value (empty string or TRUE). Debug messages will be printed to stderr.")
+  }
+
   val stdioServerTransport = StdioServerTransport(inputStream.asSource().buffered(), outputStream.asSink().buffered())
 
-  val httpClient = HttpClient() {
+  val httpClient = HttpClient {
     install(SSE)
   }
 
-  val envsToPass = System.getenv().filter { (key, _) -> key.startsWith(IJ_MCP_PREFIX) }
+  val envsToPass = System.getenv().filter { (key, _) -> key != IJ_MCP_DEBUG && key.startsWith(IJ_MCP_PREFIX) }
+  debug("Passing the following headers to the server: ${envsToPass.entries.joinToString { (key, value) -> "$key=$value"}}")
+
   val sseClientTransport = SseClientTransport(httpClient, "http://localhost:$port/sse") {
     envsToPass.forEach { (key, value) -> header(key.removePrefix(IJ_MCP_HEADER_PREFIX), value)}
   }
 
   stdioServerTransport.onMessage {
+    debug("STDIO->SSE: $it")
     sseClientTransport.send(it)
   }
 
   sseClientTransport.onMessage {
+    debug("SSE->STDIO: $it")
     stdioServerTransport.send(it)
   }
 
   stdioServerTransport.onClose {
     @Suppress("RAW_RUN_BLOCKING")
     runBlocking {
+      info("STDIO closed -> closing SSE")
       sseClientTransport.close()
     }
   }
 
   stdioServerTransport.onError {
-    System.err.println("Error in StdIO: $it")
+    error("Error in STDIO: $it")
   }
   sseClientTransport.onError {
-    System.err.println("Error in Socket: $it")
+    error("Error in SSE: $it")
   }
   sseClientTransport.start()
   stdioServerTransport.start()
 
+  info("Proxy transports started")
   val finished = CompletableDeferred<Unit>()
 
   stdioServerTransport.onClose { finished.complete(Unit) }
 
+  info("Waiting for the transports to finish")
   finished.await()
+  info("Transports finished")
 }
 
 object ClassPathMarker
+
+private val isDebugEnabled: Boolean = System.getenv(IJ_MCP_DEBUG)?.toBoolean() == true
+
+private fun error(message: String) {
+  System.err.println("[ERROR] $message")
+}
+
+private fun info(message: String) {
+  System.err.println("[INFO] $message")
+}
+
+private fun debug(message: String) {
+  if (isDebugEnabled) {
+    System.err.println("[DEBUG] $message")
+  }
+}
