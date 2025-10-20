2b142a7b860ccca22abcfbf8a24655f099452fed
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-10-20 14:53:58 +0300

    [terminal] IJPL-211122 Add a utility to get the shell name from TerminalStartupOptions
    Introduce the new interface instead of using the one from the `intellij.terminal.completion` module. It sounds better to have this utility defined in the main module. And to replace usages in completion later.
    
    GitOrigin-RevId: 8a2e6386fca44773d88fd3b90853f029b3c42787

---
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/session/ShellName.kt
@@ -0,0 +1,20 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.terminal.session
+
+import org.jetbrains.annotations.ApiStatus
+
+@ApiStatus.Experimental
+sealed interface ShellName {
+  val value: String
+
+  companion object {
+    fun of(value: String): ShellName = ShellNameImpl(value.lowercase())
+
+    val BASH: ShellName = of("bash")
+    val ZSH: ShellName = of("zsh")
+    val FISH: ShellName = of("fish")
+    val POWERSHELL: ShellName = of("powershell")
+  }
+}
+
+private class ShellNameImpl(override val value: String) : ShellName
