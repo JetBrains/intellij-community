c6650705f48c9c14a4facde714f4f93e23ab147e
Alexander Koshevoy <alexander.koshevoy@jetbrains.com>
2025-10-20 02:06:17 +0200

    [wsm] IJPL-178632 Separate JpsGlobalModelLoadedListener by environment
    Pass `InternalEnvironmentName` through the JPS global model loaded flow and gate the Ruby `SdkRefresher` listener to `InternalEnvironmentName.Local`, preventing the "This method may be invoked only once per session." exception. The listener now ignores non‑local environments. Applying the logic there is postponed until it's adapted to use the eel context.
    
    
    (cherry picked from commit 9ee29949ed6322d6f08484d9411f70bc5595c90b)
    
    IJ-CR-179816
    
    GitOrigin-RevId: 0e0a93fd83f2354c2e67c811f6c7d2993d1a6b49

--- platform/projectModel-api/src/com/intellij/workspaceModel/ide/JpsGlobalModelLoadedListener.kt
+++ platform/projectModel-api/src/com/intellij/workspaceModel/ide/JpsGlobalModelLoadedListener.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.workspaceModel.ide
 
+import com.intellij.platform.workspace.storage.InternalEnvironmentName
 import com.intellij.util.messages.Topic
 import org.jetbrains.annotations.ApiStatus
 import java.util.*
@@ -8,7 +9,7 @@ import java.util.*
 @ApiStatus.Internal
 interface JpsGlobalModelLoadedListener : EventListener {
   /** This method will be executed right after JPS global model loading e.g., SDK or global libraries */
-  fun loaded()
+  fun loaded(environmentName: InternalEnvironmentName)
 
   companion object {
     @Topic.AppLevel
