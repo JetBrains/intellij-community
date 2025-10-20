f56abee683e52ab103039db5ad71fefefd506e3a
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-10-20 16:56:49 +0300

    [terminal] IJPL-211122 Introduce TerminalTextSelectionModel
    Make it as a wrapper around regular and alternate buffer editors.
    
    GitOrigin-RevId: d156cb6138a8269cba784d7f9d0e1a49cb0f4679

---
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/view/TerminalTextSelectionModel.kt
@@ -0,0 +1,51 @@
+package com.intellij.terminal.frontend.view
+
+import com.intellij.openapi.Disposable
+import com.intellij.util.concurrency.annotations.RequiresEdt
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.plugins.terminal.block.reworked.TerminalOffset
+import org.jetbrains.plugins.terminal.block.reworked.TerminalOutputModel
+import java.util.*
+
+@ApiStatus.Experimental
+@ApiStatus.NonExtendable
+interface TerminalTextSelectionModel {
+  @get:RequiresEdt
+  val selection: TerminalTextSelection?
+
+  @RequiresEdt
+  fun updateSelection(newSelection: TerminalTextSelection?)
+
+  fun addListener(parentDisposable: Disposable, listener: TerminalTextSelectionListener)
+}
+
+@ApiStatus.Experimental
+interface TerminalTextSelectionListener : EventListener {
+  fun selectionChanged(event: TerminalTextSelectionChangeEvent)
+}
+
+@ApiStatus.Experimental
+@ApiStatus.NonExtendable
+interface TerminalTextSelectionChangeEvent {
+  val outputModel: TerminalOutputModel
+  val oldSelection: TerminalTextSelection?
+  val newSelection: TerminalTextSelection?
+}
+
+@ApiStatus.Experimental
+sealed interface TerminalTextSelection {
+  val startOffset: TerminalOffset
+  val endOffset: TerminalOffset
+
+  companion object {
+    fun of(startOffset: TerminalOffset, endOffset: TerminalOffset): TerminalTextSelection {
+      require(startOffset < endOffset) { "startOffset must be less than endOffset and selection should not be empty" }
+      return TerminalTextSelectionImpl(startOffset, endOffset)
+    }
+  }
+}
+
+private data class TerminalTextSelectionImpl(
+  override val startOffset: TerminalOffset,
+  override val endOffset: TerminalOffset,
+) : TerminalTextSelection
