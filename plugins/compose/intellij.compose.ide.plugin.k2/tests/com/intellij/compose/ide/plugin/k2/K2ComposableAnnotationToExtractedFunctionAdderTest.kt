2f2d8a134cc8307a84e61a6c4794d5b2190e5b0a
Roman Golyshev <roman.golyshev@jetbrains.com>
2025-10-21 10:38:53 +0200

    [kotlin] KTIJ-36061 Use `waitForAsyncTaskCompletion` instead of semaphores in `K2ComposableAnnotationToExtractedFunctionAdderTest`
    The same change was done to the code from the AOSP for the same reasons (to avoid infinite waiting
    on semaphores in case of exceptions), see 33663cb6
    
    ^KTIJ-36061 Fixed
    
    GitOrigin-RevId: 9ae770f98072d929208fdf259544250ec76fa4c5

--- plugins/compose/intellij.compose.ide.plugin.k2/tests/com/intellij/compose/ide/plugin/k2/K2ComposableAnnotationToExtractedFunctionAdderTest.kt
+++ plugins/compose/intellij.compose.ide.plugin.k2/tests/com/intellij/compose/ide/plugin/k2/K2ComposableAnnotationToExtractedFunctionAdderTest.kt
@@ -19,24 +19,20 @@ package com.intellij.compose.ide.plugin.k2
 
 import com.intellij.compose.ide.plugin.shared.ComposableAnnotationToExtractedFunctionAdderTest
 import com.intellij.openapi.application.Application
+import com.intellij.openapi.application.impl.NonBlockingReadActionImpl
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture
 import org.jetbrains.kotlin.analysis.api.permissions.KaAllowAnalysisOnEdt
 import org.jetbrains.kotlin.analysis.api.permissions.allowAnalysisOnEdt
 import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginMode
-import org.jetbrains.kotlin.idea.k2.refactoring.extractFunction.ExtractableCodeDescriptor
-import org.jetbrains.kotlin.idea.k2.refactoring.extractFunction.ExtractableCodeDescriptorWithConflicts
-import org.jetbrains.kotlin.idea.k2.refactoring.extractFunction.ExtractionGeneratorConfiguration
-import org.jetbrains.kotlin.idea.k2.refactoring.extractFunction.ExtractionResult
-import org.jetbrains.kotlin.idea.k2.refactoring.extractFunction.KotlinFirExtractFunctionHandler
+import org.jetbrains.kotlin.idea.k2.refactoring.extractFunction.*
 import org.jetbrains.kotlin.idea.k2.refactoring.introduce.extractionEngine.ExtractionEngineHelper
 import org.jetbrains.kotlin.idea.k2.refactoring.introduceConstant.INTRODUCE_CONSTANT
 import org.jetbrains.kotlin.idea.k2.refactoring.introduceConstant.KotlinIntroduceConstantHandler
 import org.jetbrains.kotlin.idea.refactoring.introduce.extractFunction.EXTRACT_FUNCTION
 import org.jetbrains.kotlin.idea.refactoring.introduce.extractionEngine.ExtractionGeneratorOptions
 import org.jetbrains.kotlin.name.FqName
-import java.util.concurrent.Semaphore
 
 class K2ComposableAnnotationToExtractedFunctionAdderTest : ComposableAnnotationToExtractedFunctionAdderTest() {
   override val pluginMode: KotlinPluginMode get() = KotlinPluginMode.K2
@@ -46,8 +42,8 @@ class K2ComposableAnnotationToExtractedFunctionAdderTest : ComposableAnnotationT
     application.invokeAndWait {
       KotlinFirExtractFunctionHandler(helper = helper)
         .invoke(this.project, this.editor, this.file!!, null)
+      NonBlockingReadActionImpl.waitForAsyncTaskCompletion()
     }
-    helper.waitUntilFinished()
   }
 
   override fun JavaCodeInsightTestFixture.invokeExtractConstantIn(application: Application) {
@@ -55,20 +51,14 @@ class K2ComposableAnnotationToExtractedFunctionAdderTest : ComposableAnnotationT
     application.invokeAndWait {
       KotlinIntroduceConstantHandler(helper = helper)
         .invoke(this.project, this.editor, this.file!!, null)
+      NonBlockingReadActionImpl.waitForAsyncTaskCompletion()
     }
-    helper.waitUntilFinished()
   }
 
 }
 
 // Following 2 are heavily inspired/copied from AOSP
 private class ExtractionHelper(private val existingAnnotationFqNames: List<FqName>) : ExtractionEngineHelper(EXTRACT_FUNCTION) {
-  private val finishedSemaphore = Semaphore(0)
-
-  fun waitUntilFinished() {
-    finishedSemaphore.acquire()
-  }
-
   @OptIn(KaAllowAnalysisOnEdt::class)
   override fun configureAndRun(
     project: Project,
@@ -92,19 +82,12 @@ private class ExtractionHelper(private val existingAnnotationFqNames: List<FqNam
         ExtractionGeneratorConfiguration(newDescriptor, ExtractionGeneratorOptions.DEFAULT)
       ) { er: ExtractionResult ->
         onFinish(er)
-        finishedSemaphore.release()
       }
     }
   }
 }
 
 private class InteractiveExtractionHelper : ExtractionEngineHelper(INTRODUCE_CONSTANT) {
-  private val finishedSemaphore = Semaphore(0)
-
-  fun waitUntilFinished() {
-    finishedSemaphore.acquire()
-  }
-
   override fun validate(
     descriptor: ExtractableCodeDescriptor
   ): ExtractableCodeDescriptorWithConflicts =
@@ -122,7 +105,6 @@ private class InteractiveExtractionHelper : ExtractionEngineHelper(INTRODUCE_CON
       descriptorWithConflicts,
     ) { er: ExtractionResult ->
       onFinish(er)
-      finishedSemaphore.release()
     }
   }
 }
