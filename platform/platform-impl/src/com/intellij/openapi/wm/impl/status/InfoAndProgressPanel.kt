d754010951c3a273222a5b18b3aa66ae10a440d1
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2025-10-21 17:28:28 +0200

    [progresses] [status bar] IJPL-212629 Status bar: progress counter is collapsed to an ellipsis if IDE is zoomed in
    GitOrigin-RevId: fbb833b0a1c1da9e6530f761262a481dd605dbfb

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/status/InfoAndProgressPanel.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/status/InfoAndProgressPanel.kt
@@ -1243,7 +1243,7 @@ class InfoAndProgressPanel internal constructor(
   }
 }
 
-private class CounterLabel : JPanel() {
+private class CounterLabel : JPanel(), UISettingsListener {
   private val label: JBLabel
   private var numberOfProgresses: Int = 0
   private var isProgressVisible: Boolean = false
@@ -1328,5 +1328,9 @@ private class CounterLabel : JPanel() {
 
   override fun getMinimumSize(): Dimension {
     return preferredSize
+  }
+
+  override fun uiSettingsChanged(uiSettings: UISettings) {
+    minimumSize = null
   }
 }
