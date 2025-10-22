2f8dab96ca60a05691859432741f8851ca91a61e
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2025-10-22 13:53:36 +0200

    [ui] IJPL-204342 Islands: wrong notification pending icon
    (cherry picked from commit b8d8adde736139a9c3588b479d8251e16d363cc3)
    
    IJ-CR-180253
    
    GitOrigin-RevId: a9e05f0c1f98134b803f2a1c516b66f9cc51d36b

--- platform/platform-impl/src/com/intellij/openapi/application/impl/islands/IslandsGradientPainter.kt
+++ platform/platform-impl/src/com/intellij/openapi/application/impl/islands/IslandsGradientPainter.kt
@@ -14,6 +14,7 @@ import com.intellij.openapi.wm.impl.IdeGlassPaneEx
 import com.intellij.openapi.wm.impl.customFrameDecorations.header.CustomWindowHeaderUtil
 import com.intellij.ui.ColorUtil
 import com.intellij.ui.Gray
+import com.intellij.ui.JBColor
 import com.intellij.ui.paint.PaintUtil
 import com.intellij.ui.paint.PaintUtil.alignIntToInt
 import com.intellij.ui.paint.PaintUtil.alignTxToInt
@@ -23,6 +24,14 @@ import java.awt.*
 import javax.swing.JComponent
 
 internal class IslandsGradientPainter(private val frame: IdeFrame, private val mainColor: Color, private val enabled: () -> Boolean) : AbstractPainter() {
+
+  private val islandsGradientColors = setOf(
+    "MainWindow.background",
+    "MainToolbar.background",
+    "MainToolbar.inactiveBackground",
+    "ToolWindow.Stripe.background",
+    "StatusBar.background")
+
   private val projectWindowCustomizer = ProjectWindowCustomizerService.getInstance()
 
   private var doPaint = true
@@ -33,7 +42,11 @@ internal class IslandsGradientPainter(private val frame: IdeFrame, private val m
     if (doPaint) {
       try {
         doPaint = false
-        islandsGradientPaint(frame, mainColor, projectWindowCustomizer, component, g)
+
+        val colorName = (g.paint as? JBColor)?.name
+        if (colorName in islandsGradientColors) {
+          islandsGradientPaint(frame, mainColor, projectWindowCustomizer, component, g)
+        }
       }
       finally {
         doPaint = true
@@ -47,11 +60,6 @@ internal fun islandsGradientPaint(frame: IdeFrame, mainColor: Color, projectWind
     return
   }
   if (component is IdeGlassPaneEx && !component.isColorfulToolbar) {
-    return
-  }
-
-  val paintRgb = (g.paint as? Color)?.rgb
-  if (paintRgb != mainColor.rgb) {
     return
   }
 
