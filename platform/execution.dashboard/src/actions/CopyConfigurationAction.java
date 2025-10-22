d2a017d6bac3cc994891491cdc2b47b8816e4041
Nikita Katkov <nikita.katkov@jetbrains.com>
2025-10-22 12:20:05 +0200

    [run dashboard] IJPL-208071 Enable back the copy configuration action
    GitOrigin-RevId: 2d9aea804fb67f72c5034234031b14c493c8745c

--- platform/execution.dashboard/src/actions/CopyConfigurationAction.java
+++ platform/execution.dashboard/src/actions/CopyConfigurationAction.java
@@ -1,28 +1,17 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.execution.dashboard.actions;
 
-import com.intellij.execution.ExecutionBundle;
-import com.intellij.execution.RunManager;
-import com.intellij.execution.RunnerAndConfigurationSettings;
-import com.intellij.execution.configuration.ConfigurationFactoryEx;
-import com.intellij.execution.configurations.ConfigurationFactory;
-import com.intellij.execution.configurations.RunConfiguration;
-import com.intellij.execution.configurations.RunConfigurationBase;
-import com.intellij.execution.dashboard.RunDashboardManager;
-import com.intellij.execution.dashboard.RunDashboardRunConfigurationNode;
-import com.intellij.execution.dashboard.actions.RunDashboardActionUtils;
-import com.intellij.execution.impl.RunDialog;
-import com.intellij.execution.impl.RunnerAndConfigurationSettingsImpl;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
-import com.intellij.platform.execution.dashboard.RunDashboardManagerImpl;
+import com.intellij.platform.execution.dashboard.splitApi.frontend.FrontendRunDashboardService;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
+import static com.intellij.platform.execution.dashboard.actions.RunDashboardActionUtilsKt.getSelectedNode;
+import static com.intellij.platform.execution.dashboard.actions.RunDashboardActionUtilsKt.scheduleCopyConfiguration;
 
 /**
  * @author konstantin.aleev
@@ -37,8 +26,8 @@ final class CopyConfigurationAction extends DumbAwareAction implements ActionRem
   @Override
   public void update(@NotNull AnActionEvent e) {
     Project project = e.getProject();
-    RunDashboardRunConfigurationNode node = project == null ? null : RunDashboardActionUtils.getTarget(e);
-    boolean enabled = node != null && RunManager.getInstance(project).hasSettings(node.getConfigurationSettings());
+    FrontendRunDashboardService node = project == null ? null : getSelectedNode(e);
+    boolean enabled = node != null;
     Presentation presentation = e.getPresentation();
     presentation.setEnabled(enabled);
     boolean popupPlace = e.isFromContextMenu();
@@ -51,38 +40,9 @@ final class CopyConfigurationAction extends DumbAwareAction implements ActionRem
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
     Project project = e.getProject();
-    RunDashboardRunConfigurationNode node = project == null ? null : RunDashboardActionUtils.getTarget(e);
+    FrontendRunDashboardService node = project == null ? null : getSelectedNode(e);
     if (node == null) return;
 
-    RunManager runManager = RunManager.getInstance(project);
-    RunnerAndConfigurationSettings settings = node.getConfigurationSettings();
-
-    RunnerAndConfigurationSettings copiedSettings = ((RunnerAndConfigurationSettingsImpl)settings).clone();
-    runManager.setUniqueNameIfNeeded(copiedSettings);
-    copiedSettings.setFolderName(settings.getFolderName());
-    copiedSettings.getConfiguration().setBeforeRunTasks(settings.getConfiguration().getBeforeRunTasks());
-
-    final ConfigurationFactory factory = settings.getFactory();
-    RunConfiguration configuration = settings.getConfiguration();
-    //noinspection deprecation
-    if (factory instanceof ConfigurationFactoryEx) {
-      //noinspection deprecation,unchecked
-      ((ConfigurationFactoryEx)factory).onConfigurationCopied(configuration);
-    }
-    if (configuration instanceof RunConfigurationBase) {
-      ((RunConfigurationBase<?>)configuration).onConfigurationCopied();
-    }
-
-    if (RunDialog.editConfiguration(project, copiedSettings,
-                                    ExecutionBundle.message("run.dashboard.edit.configuration.dialog.title"))) {
-
-      runManager.addConfiguration(copiedSettings);
-    }
-
-    RunConfiguration copiedConfiguration = copiedSettings.getConfiguration();
-    RunDashboardManager runDashboardManager = RunDashboardManager.getInstance(project);
-    if (!runDashboardManager.isShowInDashboard(copiedConfiguration)) {
-      runDashboardManager.restoreConfigurations(Collections.singletonList(copiedConfiguration));
-    }
+    scheduleCopyConfiguration(node.getRunDashboardServiceDto().getUuid(), project);
   }
 }
--- platform/execution.dashboard/src/splitApi/RunDashboardServiceRpc.kt
+++ platform/execution.dashboard/src/splitApi/RunDashboardServiceRpc.kt
@@ -37,4 +37,5 @@ interface RunDashboardServiceRpc : RemoteApi<Unit> {
   suspend fun hideConfigurations(projectId: ProjectId, configurations: List<RunDashboardConfigurationId>)
   suspend fun getRunManagerUpdates(projectId: ProjectId): Flow<Unit>
   suspend fun editConfiguration(projectId: ProjectId, serviceId: RunDashboardServiceId)
+  suspend fun copyConfiguration(projectId: ProjectId, serviceId: RunDashboardServiceId)
 }
