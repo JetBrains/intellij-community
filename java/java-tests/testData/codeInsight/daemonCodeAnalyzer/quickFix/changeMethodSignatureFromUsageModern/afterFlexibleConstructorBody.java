0574aa7344a167647a5b2adfdaeef9df7ea21833
Bas Leijdekkers <bas.leijdekkers@jetbrains.com>
2025-10-22 15:38:13 +0200

    Java: fix Find Usages for Flexible Constructor Bodies super/this calls (IDEA-381039)
    GitOrigin-RevId: e5710feac14035bd2a0193fa84a00f11c8bf8987

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/changeMethodSignatureFromUsageModern/afterFlexibleConstructorBody.java
@@ -0,0 +1,14 @@
+// "Add 'int' as 1st parameter to constructor 'Friend()'" "true"
+class Friend {
+  Friend(int i) {
+    System.out.println(0);
+  }
+}
+class Flexible extends Friend {
+
+  Flexible() {
+    System.out.println("before");
+    super(<caret>1);
+    System.out.println("after");
+  }
+}
