b36ce84c8e517d40f6824dc4487fdf7674251151
Ilia Malakhov <ilia.malakhov@jetbrains.com>
2025-10-23 16:30:01 +0200

    IJPL-198986 Show "all tasks finished" in processes popup
    GitOrigin-RevId: 6922936925ebec0a94336cf63513668b22042d21

---
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/status/TasksFinishedDecorator.kt
@@ -0,0 +1,52 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.wm.impl.status
+
+import com.intellij.icons.AllIcons
+import com.intellij.ide.IdeBundle
+import com.intellij.openapi.wm.impl.status.ProcessPopup.hideSeparator
+import com.intellij.ui.ExperimentalUI
+import com.intellij.util.ui.JBUI
+import com.intellij.util.ui.UIUtil
+import javax.swing.JComponent
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.SwingConstants
+
+
+/**
+ * Adds an "all tasks finished" label when there are no background tasks running
+ */
+internal class TasksFinishedDecorator(private val panel: JPanel) {
+  private val finishedTasksLabel = createTasksFinishedLabel()
+  private var indicators: Int = 0
+
+
+  fun addIndicator(component: JComponent) {
+    if (indicators == 0) {
+      panel.remove(finishedTasksLabel)
+      hideSeparator(component)
+    }
+    indicators++
+  }
+
+  fun removeIndicator() {
+    indicators--
+    if (indicators == 0) {
+      panel.add(finishedTasksLabel, 0, 0)
+    }
+  }
+
+
+  private fun createTasksFinishedLabel(): JLabel = JLabel().apply {
+    text = IdeBundle.message("all.background.tasks.completed")
+    icon = AllIcons.Status.Success
+    horizontalAlignment = SwingConstants.LEFT
+
+    setBorder(JBUI.Borders.empty(8, 12))
+    foreground = UIUtil.getLabelInfoForeground()
+    hideSeparator(this)
+    if (ExperimentalUI.isNewUI()) {
+      setOpaque(false)
+    }
+  }
+}
