37263e3fa44221ab3ba333c37dd634a50e789224
Ilya Korennoy <ilya.korennoy@jetbrains.com>
2025-10-23 17:33:47 +0200

    [kotlin] KTIJ-35916 Do not use LibraryBridge
    GitOrigin-RevId: 4cb2f812622638e157d31bf6de3e0e9a0490ddfd

--- plugins/kotlin/base/project-structure/src/org/jetbrains/kotlin/idea/base/projectStructure/forwardDeclarations/KotlinForwardDeclarationsModelChangeService.kt
+++ plugins/kotlin/base/project-structure/src/org/jetbrains/kotlin/idea/base/projectStructure/forwardDeclarations/KotlinForwardDeclarationsModelChangeService.kt
@@ -2,19 +2,17 @@
 package org.jetbrains.kotlin.idea.base.projectStructure.forwardDeclarations
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.OrderRootType
-import com.intellij.openapi.roots.impl.libraries.LibraryEx
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.backend.workspace.WorkspaceModel
 import com.intellij.platform.backend.workspace.WorkspaceModelChangeListener
+import com.intellij.platform.backend.workspace.virtualFile
 import com.intellij.platform.workspace.jps.entities.LibraryEntity
+import com.intellij.platform.workspace.jps.entities.LibraryRootTypeId
 import com.intellij.platform.workspace.jps.entities.modifyLibraryEntity
 import com.intellij.platform.workspace.storage.EntityChange
-import com.intellij.platform.workspace.storage.ImmutableEntityStorage
 import com.intellij.platform.workspace.storage.VersionedStorageChange
 import com.intellij.platform.workspace.storage.impl.url.toVirtualFileUrl
 import com.intellij.util.PathUtil
-import com.intellij.workspaceModel.ide.impl.legacyBridge.library.findLibraryBridge
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.jetbrains.kotlin.idea.base.platforms.detectLibraryKind
@@ -45,7 +43,7 @@ internal class KotlinForwardDeclarationsModelChangeService(
             val libraryChanges = event.getChanges<LibraryEntity>().ifEmpty { return@launch }
 
             val nativeKlibs: Map<LibraryEntity, KLibRoot> =
-                libraryChanges.toNativeKLibs(event.storageAfter).ifEmpty { return@launch }
+                libraryChanges.toNativeKLibs().ifEmpty { return@launch }
             val workspaceModel = WorkspaceModel.getInstance(project)
             val createEntityStorageChanges = createEntityStorageChanges(workspaceModel, nativeKlibs)
 
@@ -91,29 +89,28 @@ internal class KotlinForwardDeclarationsModelChangeService(
         }
     }
 
-    private fun List<EntityChange<LibraryEntity>>.toNativeKLibs(
-        storageAfter: ImmutableEntityStorage
-    ): Map<LibraryEntity, KLibRoot> {
+    private fun List<EntityChange<LibraryEntity>>.toNativeKLibs(): Map<LibraryEntity, KLibRoot> {
         val libraryEntityChanges = this
         return buildMap {
             for (entityChange in libraryEntityChanges) {
                 val newLibraryEntity: LibraryEntity = entityChange.newEntity ?: continue
-                val library = newLibraryEntity.findLibraryBridge(storageAfter) as? LibraryEx ?: continue
-                val nativeRootsAfterChange = library.getClassRootsIfNative()
+                val nativeRootsAfterChange = newLibraryEntity.getClassRootsIfNative()
 
                 for (classRoot in nativeRootsAfterChange) {
                     val path = PathUtil.getLocalPath(classRoot) ?: continue
-                    put(newLibraryEntity, KLibRoot(library, path))
+                    put(newLibraryEntity, KLibRoot(path))
                 }
             }
         }
     }
 
-    private fun LibraryEx.getClassRootsIfNative(): List<VirtualFile> {
+    private fun LibraryEntity.getClassRootsIfNative(): List<VirtualFile> {
         if (detectLibraryKind(this, project)?.platform?.idePlatformKind != NativeIdePlatformKind) return emptyList()
 
-        return getFiles(OrderRootType.CLASSES).filter {
-            it.isKlibLibraryRootForPlatform(NativeIdePlatformKind.defaultPlatform)
-        }
+        return roots.asSequence()
+            .filter { it.type == LibraryRootTypeId.COMPILED }
+            .mapNotNull { it.url.virtualFile }
+            .filter { it.isKlibLibraryRootForPlatform(NativeIdePlatformKind.defaultPlatform) }
+            .toList()
     }
 }
