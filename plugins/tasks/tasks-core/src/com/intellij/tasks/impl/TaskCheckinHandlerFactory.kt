7a1d0ac40be0c8be050905d10f426dcd8c8cf134
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-10-23 11:29:10 +0200

    [tasks] IJPL-214046 Use coroutines to save task context asynchronously
    (cherry picked from commit 29b13650f3bb940bc9b9dddbaf7ef19854cc40e9)
    
    IJ-CR-179796
    
    GitOrigin-RevId: eba8aa03d06556711556e06fe3cec0b0a9e7cce8

--- plugins/tasks/tasks-core/src/com/intellij/tasks/impl/TaskCheckinHandlerFactory.kt
+++ plugins/tasks/tasks-core/src/com/intellij/tasks/impl/TaskCheckinHandlerFactory.kt
@@ -1,15 +1,23 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.tasks.impl
 
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.vcs.CheckinProjectPanel
 import com.intellij.openapi.vcs.changes.CommitContext
 import com.intellij.openapi.vcs.checkin.CheckinHandler
 import com.intellij.openapi.vcs.checkin.CheckinHandlerFactory
+import com.intellij.platform.ide.progress.withBackgroundProgress
 import com.intellij.tasks.Task
+import com.intellij.tasks.TaskBundle
 import com.intellij.tasks.TaskManager
 import com.intellij.tasks.context.WorkingContextManager
+import com.intellij.util.concurrency.annotations.RequiresEdt
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.*
 
 class TaskCheckinHandlerFactory : CheckinHandlerFactory() {
@@ -17,22 +25,31 @@ class TaskCheckinHandlerFactory : CheckinHandlerFactory() {
     TaskCheckinHandler(panel)
 
   private class TaskCheckinHandler(val panel: CheckinProjectPanel) : CheckinHandler(), DumbAware {
+
+    @RequiresEdt
     override fun checkinSuccessful() {
       val message = panel.getCommitMessage()
       val project = panel.getProject()
       val manager = TaskManager.getManager(project) as TaskManagerImpl
       if (manager.getState().saveContextOnCommit) {
-        val task = findTaskInRepositories(message, manager)
-                   ?: manager.createLocalTask(message)
-                     .also { (it as LocalTaskImpl).isClosed = true }
-        val localTask = manager.addTask(task)
-        localTask.setUpdated(Date())
-
-        ApplicationManager.getApplication().invokeLater(
-          { WorkingContextManager.getInstance(project).saveContext(localTask) }, project.getDisposed())
+        project.service<CoroutineScopeHolder>().launch {
+          withBackgroundProgress(project, TaskBundle.message("progress.title.saving.task.context"), cancellable = true) {
+            val task = findTaskInRepositories(message, manager)
+                       ?: manager.createLocalTask(message)
+                         .also { (it as LocalTaskImpl).isClosed = true }
+            val localTask = manager.addTask(task)
+            localTask.setUpdated(Date())
+            withContext(Dispatchers.Main) {
+              WorkingContextManager.getInstance(project).saveContext(localTask)
+            }
+          }
+        }
       }
     }
   }
+
+  @Service(Service.Level.PROJECT)
+  private class CoroutineScopeHolder(private val cs: CoroutineScope) : CoroutineScope by cs
 }
 
 private fun findTaskInRepositories(message: String, manager: TaskManager): Task? {
