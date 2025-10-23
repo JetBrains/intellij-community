231869b6a06c7a2d232cab6ac86057f611f15255
Jakub Senohrabek <jakub.senohrabek@jetbrains.com>
2025-10-23 15:06:28 +0200

    [JEWEL-1066] Bump Jewel version to 0.32.0
    GitOrigin-RevId: bf9677e032be79b3896f6e5652ebe16ae4eaf82d

--- platform/jewel/samples/showcase/src/main/kotlin/org/jetbrains/jewel/samples/showcase/components/Typography.kt
+++ platform/jewel/samples/showcase/src/main/kotlin/org/jetbrains/jewel/samples/showcase/components/Typography.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UnusedImports") // Detekt false positive - org.jetbrains.jewel.ui.component.InfoText
+
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.jewel.samples.showcase.components
 
