bedbb443c19c5144229dc5041ece6f6eb5c565d0
Alexander Kuznetsov <aleksander.kuznetsov@jetbrains.com>
2025-10-24 21:10:41 +0200

    [rd debugger] IJPL-214299 Navigate to an execution point position when the execution point is currently selected
    (cherry picked from commit c2d1ea761e40e631a43f51b525ff0538737ee20d)
    
    IJ-CR-180120
    
    GitOrigin-RevId: 8070265143e77a9f1053b8cd49dd5f3d9820000e

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/ShowExecutionPointAction.kt
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/ShowExecutionPointAction.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecificat
 import com.intellij.xdebugger.impl.DebuggerSupport
 import com.intellij.xdebugger.impl.frame.XDebugSessionProxy
 import com.intellij.xdebugger.impl.performDebuggerActionAsync
+import com.intellij.xdebugger.impl.updateExecutionPosition
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
@@ -25,6 +26,14 @@ private val ourHandler = object : XDebuggerProxySuspendedActionHandler() {
   override fun perform(session: XDebugSessionProxy, dataContext: DataContext) {
     performDebuggerActionAsync(session.project, dataContext) {
       session.switchToTopFrame()
+      // TODO that's a temporary solution to make the action working in 253.
+      //  This method shouldn't be called anywhere but `ExecutionPointManagerChangeListener`.
+      //  Instead, here we should make sure `switchToTopFrame` actually makes the backend
+      //  send a `FRAME_CHANGED` event back to the frontend,
+      //  and the frontend should make an exception for this particular case
+      //  and run the `stackFrameChanged` even if the stack frame wasn't changed.
+      //  See IJPL-214299 for details.
+      updateExecutionPosition(session.project)
     }
   }
 }
