526ad7da937bb3241590157e64e16e32f2bd2355
Valeria Golovina <valeria.golovina@jetbrains.com>
2025-10-26 17:46:06 +0200

    [gitlab] IJPL-82619: customized image parsing to hide an image settings section while rendering review notes
    * The text after the section should still be visible: `![image](url){settings} some text here`
    * Customise GitLabImageParser to include the `{settings}` inside the Markdown Image
    * Customise ImageGeneratingProvider to hide {settings} section but renders further text
    
    GitOrigin-RevId: f780d753d84567a5921b29bb8595762153d89236

--- plugins/gitlab/gitlab-core/src/org/jetbrains/plugins/gitlab/ui/GitLabUIUtil.kt
+++ plugins/gitlab/gitlab-core/src/org/jetbrains/plugins/gitlab/ui/GitLabUIUtil.kt
@@ -11,6 +11,7 @@ import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.util.text.HtmlChunk
 import git4idea.repo.GitRepository
 import org.intellij.markdown.IElementType
+import org.intellij.markdown.MarkdownElementType
 import org.intellij.markdown.MarkdownElementTypes
 import org.intellij.markdown.MarkdownTokenTypes
 import org.intellij.markdown.ast.ASTNode
@@ -18,14 +19,33 @@ import org.intellij.markdown.ast.findChildOfType
 import org.intellij.markdown.ast.getTextInNode
 import org.intellij.markdown.flavours.gfm.GFMElementTypes
 import org.intellij.markdown.flavours.gfm.GFMFlavourDescriptor
+import org.intellij.markdown.flavours.gfm.GFMTokenTypes
+import org.intellij.markdown.flavours.gfm.StrikeThroughDelimiterParser
 import org.intellij.markdown.html.GeneratingProvider
 import org.intellij.markdown.html.HtmlGenerator
+import org.intellij.markdown.html.ImageGeneratingProvider
 import org.intellij.markdown.html.SimpleInlineTagProvider
+import org.intellij.markdown.html.makeXssSafe
 import org.intellij.markdown.html.makeXssSafeDestination
 import org.intellij.markdown.parser.LinkMap
+import org.intellij.markdown.parser.sequentialparsers.EmphasisLikeParser
+import org.intellij.markdown.parser.sequentialparsers.LocalParsingResult
+import org.intellij.markdown.parser.sequentialparsers.RangesListBuilder
+import org.intellij.markdown.parser.sequentialparsers.SequentialParser
+import org.intellij.markdown.parser.sequentialparsers.SequentialParserManager
+import org.intellij.markdown.parser.sequentialparsers.TokensCache
+import org.intellij.markdown.parser.sequentialparsers.impl.AutolinkParser
+import org.intellij.markdown.parser.sequentialparsers.impl.BacktickParser
+import org.intellij.markdown.parser.sequentialparsers.impl.EmphStrongDelimiterParser
+import org.intellij.markdown.parser.sequentialparsers.impl.InlineLinkParser
+import org.intellij.markdown.parser.sequentialparsers.impl.MathParser
+import org.intellij.markdown.parser.sequentialparsers.impl.ReferenceLinkParser
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.plugins.gitlab.util.GitLabProjectPath
 import java.net.URI
+
+
+private val MARKDOWN_IMAGE_SETTINGS: IElementType = MarkdownElementType("MARKDOWN_IMAGE_SETTINGS")
 
 object GitLabUIUtil {
   const val OPEN_FILE_LINK_PREFIX = "glfilelink:"
@@ -62,13 +82,116 @@ object GitLabUIUtil {
     private val projectPath: GitLabProjectPath,
     private val htmlSyntaxHighlighter: HtmlSyntaxHighlighter
   ) : GFMFlavourDescriptor() {
+
+    /**
+     * The same as for GFMFlavourDescriptor, but with custom GitLabImageParser
+     */
+    override val sequentialParserManager = object : SequentialParserManager() {
+      override fun getParserSequence(): List<SequentialParser> {
+        return listOf(AutolinkParser(listOf(MarkdownTokenTypes.AUTOLINK, GFMTokenTypes.GFM_AUTOLINK)),
+                      BacktickParser(),
+                      MathParser(),
+                      GitLabImageParser(),
+                      InlineLinkParser(),
+                      ReferenceLinkParser(),
+                      EmphasisLikeParser(EmphStrongDelimiterParser(), StrikeThroughDelimiterParser()))
+      }
+    }
+
     override fun createHtmlGeneratingProviders(linkMap: LinkMap, baseURI: URI?): Map<IElementType, GeneratingProvider> {
       val map = super.createHtmlGeneratingProviders(linkMap, baseURI)
       return map + hashMapOf(
+        MarkdownElementTypes.IMAGE to GitLabImageWithSettingsGeneratingProvider(linkMap, baseURI).makeXssSafe(useSafeLinks),
         GFMElementTypes.STRIKETHROUGH to SimpleInlineTagProvider("strike", 2, -2),
         MarkdownElementTypes.CODE_FENCE to CodeFenceSyntaxHighlighterGeneratingProvider(htmlSyntaxHighlighter),
         MarkdownElementTypes.INLINE_LINK to GitLabLinkGeneratingProvider(gitRepository, projectPath, map[MarkdownElementTypes.INLINE_LINK]),
       )
+    }
+  }
+
+  /**
+   * Copied ImageParser with the only difference: image can contain settings block in curly braces in the end. Examples:
+   * ```
+   * ![image](url){width=50% height=50%}
+   * ```
+   * ```
+   * [link]: url "label"
+   * The picture ![image][link]{width=10 height=10}
+   * ```
+   * @see org.intellij.markdown.parser.sequentialparsers.impl.ImageParser
+   */
+  private class GitLabImageParser : SequentialParser {
+    override fun parse(tokens: TokensCache, rangesToGlue: List<IntRange>): SequentialParser.ParsingResult {
+      val result = SequentialParser.ParsingResultBuilder()
+      val delegateIndices = RangesListBuilder()
+      var iterator: TokensCache.Iterator = tokens.RangesListIterator(rangesToGlue)
+
+      while (iterator.type != null) {
+        if (iterator.type == MarkdownTokenTypes.EXCLAMATION_MARK
+            && iterator.rawLookup(1) == MarkdownTokenTypes.LBRACKET) {
+          val link = InlineLinkParser.parseInlineLink(iterator.advance())
+                     ?: ReferenceLinkParser.parseReferenceLink(iterator.advance())
+
+          if (link != null) {
+            val index = iterator.index
+            val parsedSettings = parseImageSettings(link.iteratorPosition.advance(), tokens)
+            val lastIteratorPosition = parsedSettings?.iteratorPosition ?: link.iteratorPosition
+
+            result
+              .withNode(SequentialParser.Node(index..lastIteratorPosition.index + 1, MarkdownElementTypes.IMAGE))
+              .withOtherParsingResult(link)
+
+            parsedSettings?.let { result.withOtherParsingResult(it) }
+
+            iterator = lastIteratorPosition.advance()
+            continue
+          }
+        }
+        delegateIndices.put(iterator.index)
+        iterator = iterator.advance()
+      }
+
+      return result.withFurtherProcessing(delegateIndices.get())
+    }
+
+    /**
+     * Parses image settings in curly braces at the end of the image link.
+     * If any symbols follow the "}" without a space, they also will be a part of the parsed result
+     * `![image](url){settings}other text` -> `{settings}other`
+     */
+    private fun parseImageSettings(iterator: TokensCache.Iterator, tokens: TokensCache): LocalParsingResult? {
+      if (iterator.type == MarkdownTokenTypes.TEXT) {
+        if (iterator.firstChar == '{') {
+          var it = iterator
+          while (it.type != MarkdownTokenTypes.EOL && it.type != null) {
+            if (tokens.getRawCharAt(it.end - 1) == '}' ||
+                (it.start until it.end - 1).any { tokens.getRawCharAt(it) == '}' }) {
+              val range = iterator.index..it.index + 1
+              return LocalParsingResult(it, listOf(SequentialParser.Node(range, MARKDOWN_IMAGE_SETTINGS)))
+            }
+            it = it.advance()
+          }
+        }
+      }
+      return null
+    }
+  }
+
+  /**
+   * Copied ImageGeneratingProvider with the only difference: image can contain a settings block in curly braces in the end,
+   * and we want to render the part which goes after '}', but avoid showing the settings block.
+   * `![image](http://url){width=50% height=10}other text` -> `<img src="http://url" alt="image" />other text`
+   */
+  private class GitLabImageWithSettingsGeneratingProvider(linkMap: LinkMap, baseURI: URI?) : ImageGeneratingProvider(linkMap, baseURI) {
+    override fun renderLink(visitor: HtmlGenerator.HtmlGeneratingVisitor, text: String, node: ASTNode, info: RenderInfo) {
+      super.renderLink(visitor, text, node, info)
+      node.findChildOfType(MARKDOWN_IMAGE_SETTINGS)?.let { linkNode ->
+        val textInNode = linkNode.getTextInNode(text)
+        val closingIndex = textInNode.indexOf('}')
+        if (closingIndex > 0 && closingIndex < textInNode.length - 1) {
+          visitor.consumeHtml(textInNode.substring(closingIndex + 1))
+        }
+      }
     }
   }
 
--- plugins/gitlab/gitlab-core/test/org/jetbrains/plugins/gitlab/ui/GitLabUIUtilTest.kt
+++ plugins/gitlab/gitlab-core/test/org/jetbrains/plugins/gitlab/ui/GitLabUIUtilTest.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.gitlab.ui
 
+import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.LightPlatformTestCase
 import git4idea.repo.GitRepository
@@ -7,6 +8,7 @@ import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.plugins.gitlab.util.GitLabProjectPath
+import java.io.File
 import java.nio.file.Path
 
 class GitLabUIUtilTest : LightPlatformTestCase() {
@@ -26,6 +28,8 @@ class GitLabUIUtilTest : LightPlatformTestCase() {
     }
   }
 
+  private fun @NlsSafe String.substituteSeparators(): String = replace(File.separator, "/")
+
   // https://youtrack.jetbrains.com/issue/IJPL-148576
   fun `test link with query does not throw an exception`() {
     assertNoThrowable {
@@ -37,8 +41,7 @@ class GitLabUIUtilTest : LightPlatformTestCase() {
   }
 
   fun `test link with starting with project path redirects to browser (for now)`() {
-    val parsed = GitLabUIUtil.convertToHtml(
-      project, gitRepository, GitLabProjectPath("test-account", "mr-test"), """
+    val parsed = convertToHtml("""
         [link](/test-account/mr-test/-/merge_requests/1)
       """.trimIndent())
 
@@ -46,38 +49,198 @@ class GitLabUIUtilTest : LightPlatformTestCase() {
   }
 
   fun `test simple file link gets file link prefix`() {
-    val parsed = GitLabUIUtil.convertToHtml(
-      project, gitRepository, GitLabProjectPath("test-account", "mr-test"), """
+    val parsed = convertToHtml("""
         [link](bla.md)
       """.trimIndent())
 
-    assertThat(parsed).contains("${GitLabUIUtil.OPEN_FILE_LINK_PREFIX}${gitRoot}/bla.md")
+    assertThat(parsed.substituteSeparators()).contains("${GitLabUIUtil.OPEN_FILE_LINK_PREFIX}${gitRoot}/bla.md")
   }
 
   fun `test nested file link gets file link prefix`() {
-    val parsed = GitLabUIUtil.convertToHtml(
-      project, gitRepository, GitLabProjectPath("test-account", "mr-test"), """
+    val parsed = convertToHtml("""
         [link](directory/a/b/bla.md)
       """.trimIndent())
 
-    assertThat(parsed).contains("${GitLabUIUtil.OPEN_FILE_LINK_PREFIX}${gitRoot}/directory/a/b/bla.md")
+    assertThat(parsed.substituteSeparators()).contains("${GitLabUIUtil.OPEN_FILE_LINK_PREFIX}${gitRoot}/directory/a/b/bla.md")
   }
 
   fun `test nested file link with backslashes gets file link prefix`() {
-    val parsed = GitLabUIUtil.convertToHtml(
-      project, gitRepository, GitLabProjectPath("test-account", "mr-test"), """
+    val parsed = convertToHtml("""
         [link](directory\a\b\bla.md)
       """.trimIndent())
 
-    assertThat(parsed).contains("${GitLabUIUtil.OPEN_FILE_LINK_PREFIX}${gitRoot}/directory/a/b/bla.md")
+    assertThat(parsed.substituteSeparators()).contains("${GitLabUIUtil.OPEN_FILE_LINK_PREFIX}${gitRoot}/directory/a/b/bla.md")
   }
 
   fun `test simple MR link gets MR link prefix`() {
-    val parsed = GitLabUIUtil.convertToHtml(
-      project, gitRepository, GitLabProjectPath("test-account", "mr-test"), """
+    val parsed = convertToHtml("""
         !53
       """.trimIndent())
 
     assertThat(parsed).contains("${GitLabUIUtil.OPEN_MR_LINK_PREFIX}53")
+  }
+
+  fun `test images rendering`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg)
+      """.trimIndent())
+
+    assertThat(parsed.substituteSeparators()).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" /></p></body>""")
+  }
+
+  fun `test images rendering with one setting`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg){width=10}
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" /></p></body>""")
+  }
+
+  fun `test images rendering with one percent setting`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg){width=10%}
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" /></p></body>""")
+  }
+
+
+  fun `test images rendering with two settings`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg){width=10 height=10}
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" /></p></body>""")
+  }
+
+  fun `test images rendering with two precent settings`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg){width=10% height=10%}
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" /></p></body>""")
+  }
+
+  fun `test images rendering with many settings`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg){width=10 height=10 other=123 other2=123}
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" /></p></body>""")
+  }
+
+  fun `test images rendering with many percent settings`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg){width=10% height=10% other=123% other2=123%}
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" /></p></body>""")
+  }
+
+  fun `test images rendering with different settings`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg){width=10 height=10% other=123 other2=123%}
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" /></p></body>""")
+  }
+
+  fun `test images rendering with two double quote settings`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg){width="10" height="10"}
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" /></p></body>""")
+  }
+
+  fun `test images rendering with two double quote settings and some text`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg){width="10" height="10"} some text
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" /> some text</p></body>""")
+  }
+
+  fun `test images rendering with different settings and some text`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg){width=10 height=10% other=123 other2=123%} some text
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" /> some text</p></body>""")
+  }
+
+  fun `test images rendering with settings and additional curly braces section`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg){width=10 height=10} {here is the additional section}
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" /> {here is the additional section}</p></body>""")
+  }
+
+  fun `test images rendering with settings and immediate symbols after curly braces`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg){width=10 height=10}immediate text
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" />immediate text</p></body>""")
+  }
+
+  fun `test images rendering with settings and immediate curly braces after curly braces section`() {
+    val parsed = convertToHtml("""
+        ![link](/uploads/a/b/c.jpg){width=10 height=10}{here is the additional section}
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p><img src="/uploads/a/b/c.jpg" alt="link" />{here is the additional section}</p></body>""")
+  }
+
+  fun `test images rendering with settings reference link`() {
+    val parsed = convertToHtml("""
+        [link]: http://url "label"
+        The picture ![image][link]
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p>The picture <img src="http://url" alt="image" title="label" /></p></body>""")
+  }
+
+  fun `test images rendering with settings reference link with settings`() {
+    val parsed = convertToHtml("""
+        [link]: http://url "label"
+        The picture ![image][link]{width=10 height=10}
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p>The picture <img src="http://url" alt="image" title="label" /></p></body>""")
+  }
+
+  fun `test images rendering with settings reference link with settings and immediate text after`() {
+    val parsed = convertToHtml("""
+        [link]: http://url "label"
+        The picture ![image][link]{width=10 height=10}immediate text
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p>The picture <img src="http://url" alt="image" title="label" />immediate text</p></body>""")
+  }
+
+  fun `test images rendering with settings reference link and text`() {
+    val parsed = convertToHtml("""
+        [link]: http://url "label"
+        The picture ![image][link] some text
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p>The picture <img src="http://url" alt="image" title="label" /> some text</p></body>""")
+  }
+
+  fun `test images rendering with settings reference link with settings and text`() {
+    val parsed = convertToHtml("""
+        [link]: http://url "label"
+        The picture ![image][link]{width=10 height=10} some text
+      """.trimIndent())
+
+    assertThat(parsed).isEqualTo("""<body><p>The picture <img src="http://url" alt="image" title="label" /> some text</p></body>""")
+  }
+
+  private fun convertToHtml(markdownSource: String): @NlsSafe String {
+    val parsed = GitLabUIUtil.convertToHtml(
+      project, gitRepository, GitLabProjectPath("test-account", "mr-test"), markdownSource)
+    return parsed
   }
 }
