0557cce4301015ac26ba5526d0658fda346ba806
Bas Leijdekkers <bas.leijdekkers@jetbrains.com>
2025-10-27 17:52:49 +0100

    Java: better way to recognize "NullAway.Init" suppression (IJ-CR-179854)
    (cherry picked from commit b0e9def53ddd557344125fc10f9e1b2d7bef344d)
    
    IJ-CR-179854
    
    GitOrigin-RevId: 322448ed55f985425bf7f9db54008f07c3c2c844

--- java/java-tests/testData/inspection/notNullField/SuppressNullAwayInit.java
+++ java/java-tests/testData/inspection/notNullField/SuppressNullAwayInit.java
@@ -4,9 +4,12 @@ import javax.annotation.*;
 class Test {
   @SuppressWarnings("NullAway.Init")
   Object member;
+  
+  Object <warning descr="@NonnullByDefault fields must be initialized">otherMember</warning>;
 
   private void accessMember() {
     member = new Object();
+    otherMember = new String();
   }
 }
 
