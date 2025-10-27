4034fadb00ac0a285d64c9ca381797ab254b946a
Kajus Kuncaitis <kajus.kuncaitis@jetbrains.com>
2025-10-27 12:16:14 +0100

    [gradle][IDEA-380006] (kotlin) Add a quickfix that adds a missing group and description
    Space-RevId: e8f3eb417f68d155a0216de0c0692427afc20e50
    (cherry picked from commit 12d078caa2a5a7c921787d9b25c2e25e2d6c91ad)
    
    GitOrigin-RevId: cd325fa43b426be3bb44f7b9117921da4a5c7859

--- plugins/kotlin/gradle/scripting/kotlin.gradle.scripting.k2/src/org/jetbrains/kotlin/gradle/scripting/k2/inspections/KotlinTaskMissingGroupAndDescriptionInspectionVisitor.kt
+++ plugins/kotlin/gradle/scripting/kotlin.gradle.scripting.k2/src/org/jetbrains/kotlin/gradle/scripting/k2/inspections/KotlinTaskMissingGroupAndDescriptionInspectionVisitor.kt
@@ -2,19 +2,21 @@
 package org.jetbrains.kotlin.gradle.scripting.k2.inspections
 
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.codeInspection.util.IntentionFamilyName
+import com.intellij.modcommand.ModPsiUpdater
+import com.intellij.openapi.project.Project
 import com.intellij.psi.util.descendantsOfType
+import com.intellij.util.asSafely
 import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.resolution.singleFunctionCallOrNull
 import org.jetbrains.kotlin.analysis.api.resolution.symbol
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinModCommandQuickFix
 import org.jetbrains.kotlin.idea.codeinsight.utils.getFqNameIfPackageOrNonLocal
 import org.jetbrains.kotlin.idea.codeinsight.utils.resolveExpression
 import org.jetbrains.kotlin.lang.BinaryOperationPrecedence
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.parentOrNull
-import org.jetbrains.kotlin.psi.KtBinaryExpression
-import org.jetbrains.kotlin.psi.KtBlockExpression
-import org.jetbrains.kotlin.psi.KtCallExpression
-import org.jetbrains.kotlin.psi.KtVisitorVoid
+import org.jetbrains.kotlin.psi.*
 import org.jetbrains.plugins.gradle.codeInspection.GradleInspectionBundle
 
 class KotlinTaskMissingGroupAndDescriptionInspectionVisitor(private val holder: ProblemsHolder) : KtVisitorVoid() {
@@ -28,25 +30,31 @@ class KotlinTaskMissingGroupAndDescriptionInspectionVisitor(private val holder: 
         val calleeExpression = expression.getCalleeExpression() ?: return
         val taskBlock = expression.getBlock() ?: return
 
-        // find if group or description are already set
+        // find if group or description properties are already set
         val alreadySetProperties = taskBlock.findPropertyAssignments() + taskBlock.findSetterCalls()
         if (alreadySetProperties.size >= 2) return
 
-        val message = when (alreadySetProperties) {
+        val (message, fix) = when (alreadySetProperties) {
             setOf(Property.GROUP) ->
-                GradleInspectionBundle.message("inspection.message.task.missing.group.or.description.descriptor", "description")
+                GradleInspectionBundle.message(
+                    "inspection.message.task.missing.group.or.description.descriptor",
+                    "description"
+                ) to
+                        AddGroupDescriptionFix(addGroup = false, addDescription = true)
 
             setOf(Property.DESCRIPTION) ->
-                GradleInspectionBundle.message("inspection.message.task.missing.group.or.description.descriptor", "group")
+                GradleInspectionBundle.message("inspection.message.task.missing.group.or.description.descriptor", "group") to
+                        AddGroupDescriptionFix(addGroup = true, addDescription = false)
 
             emptySet<Property>() ->
-                GradleInspectionBundle.message("inspection.message.task.missing.group.and.description.descriptor")
+                GradleInspectionBundle.message("inspection.message.task.missing.group.and.description.descriptor") to
+                        AddGroupDescriptionFix(addGroup = true, addDescription = true)
 
             else -> return
         }
 
-        holder.problem(calleeExpression, message).register()
-
+        holder.problem(expression, message)
+            .range(calleeExpression.textRangeInParent).fix(fix).register()
     }
 
     private fun KtBlockExpression.findPropertyAssignments(): Set<Property> = this.descendantsOfType<KtBinaryExpression>()
@@ -86,6 +94,49 @@ class KotlinTaskMissingGroupAndDescriptionInspectionVisitor(private val holder: 
 
         private enum class Property {
             GROUP, DESCRIPTION
+        }
+    }
+}
+
+private class AddGroupDescriptionFix(
+    private val addGroup: Boolean,
+    private val addDescription: Boolean
+) : KotlinModCommandQuickFix<KtCallExpression>() {
+    override fun getName(): String =
+        if (addGroup && addDescription) GradleInspectionBundle.message("intention.name.task.missing.group.and.description")
+        else if (addGroup) GradleInspectionBundle.message("intention.name.task.missing.group.or.description", "group")
+        else GradleInspectionBundle.message("intention.name.task.missing.group.or.description", "description")
+
+    override fun getFamilyName(): @IntentionFamilyName String =
+        GradleInspectionBundle.message("intention.family.name.task.missing.group.and.description")
+
+    override fun applyFix(
+        project: Project,
+        element: KtCallExpression,
+        updater: ModPsiUpdater
+    ) {
+        val block = element.getBlock() ?: return
+        val psiFactory = KtPsiFactory(project, true)
+        val templateBuilder = updater.templateBuilder()
+
+        if (addGroup) {
+            val assignment = psiFactory.createExpression("group = \"example group\"")
+            val templateElement = block.addAfter(assignment, null)
+                .apply { block.addAfter(psiFactory.createNewLine(), this) }
+                .asSafely<KtBinaryExpression>()!!.right!!
+                .asSafely<KtStringTemplateExpression>()!!.entries[0]
+            templateBuilder.field(templateElement, "example group")
+        }
+        if (addDescription) {
+            val assignment = psiFactory.createExpression("description = \"example description\"")
+            val anchor = if (addGroup) block.firstChild else null
+            val templateElement = block.addAfter(assignment, anchor)
+                .apply {
+                    if (addGroup) block.addBefore(psiFactory.createNewLine(), this)
+                    else block.addAfter(psiFactory.createNewLine(), this)
+                }.asSafely<KtBinaryExpression>()!!.right!!
+                .asSafely<KtStringTemplateExpression>()!!.entries[0]
+            templateBuilder.field(templateElement, "example description")
         }
     }
 }
