f987593b195ad5f253524395a25c5f6f8ed645c9
Alexander Trushev <aleksandr.trushev@jetbrains.com>
2025-10-27 22:40:22 +0100

    Revert "Undo: take time into account when merging undo commands (IJPL-53146)"
    This reverts commit 4ccbab17c1b247de008419b1c4e3cb94a0a6a971.
    
    GitOrigin-RevId: e322d51e559715b7f50181b64feb7c65332752ba

--- platform/platform-impl/src/com/intellij/openapi/command/impl/PerformedCommand.java
+++ platform/platform-impl/src/com/intellij/openapi/command/impl/PerformedCommand.java
@@ -7,7 +7,7 @@ import com.intellij.openapi.util.NlsContexts.Command;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
+import java.util.Collection;
 
 
 record PerformedCommand(
@@ -17,7 +17,7 @@ record PerformedCommand(
   @NotNull UndoConfirmationPolicy confirmationPolicy,
   @Nullable EditorAndState editorStateBefore,
   @Nullable EditorAndState editorStateAfter,
-  @NotNull List<UndoableAction> undoableActions,
+  @NotNull Collection<UndoableAction> undoableActions,
   @NotNull UndoAffectedDocuments affectedDocuments,
   @NotNull UndoAffectedDocuments additionalAffectedDocuments,
   boolean isTransparent,
