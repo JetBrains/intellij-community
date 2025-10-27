953951c9bd42e96c3dea02fa287b8242ee2b37c2
Tatiana Elfimova <tatiana.elfimova@jetbrains.com>
2025-10-27 14:33:39 +0100

    [platform UI] IJPL-214562 Search Everywhere: file type filter resets when switching to another tab
    - We temporarily set `findModel.fileFilter = null` when searching in the All tab, but when we restore the previous value, the selectedItem is not updated.
    
    GitOrigin-RevId: 0305cbdd2af2f15b09a85e42b35ec32894190153

--- platform/lang-impl/src/com/intellij/find/impl/JComboboxAction.kt
+++ platform/lang-impl/src/com/intellij/find/impl/JComboboxAction.kt
@@ -65,7 +65,7 @@ class JComboboxAction(val project: Project, private val disposable: Disposable, 
     private val itemListener = ItemListener { rebuild() }
     private val findModelObserver = FindModel.FindModelObserver { findModel ->
       runInEdt {
-        if (findModel.fileFilter == null) selectedItem = emptyText
+        selectedItem = if (findModel.fileFilter == null) emptyText else findModel.fileFilter
       }
     }
     private val documentListener = object : DocumentAdapter() {
