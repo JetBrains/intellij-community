04f7d7dddea0c246af3aa7c42784bb6e648f9879
Egor <egor.skrypnikov@jetbrains.com>
2025-10-28 12:22:20 +0100

    [Rider] RIDER-131362 retvrn the PsiCoreUtil.java to its pristine state
    GitOrigin-RevId: 06c06737ccd87c122ead6f4ab98488e29bdc8bd4

--- platform/core-api/src/com/intellij/psi/util/PsiUtilCore.java
+++ platform/core-api/src/com/intellij/psi/util/PsiUtilCore.java
@@ -342,9 +342,6 @@ public class PsiUtilCore {
     if (element instanceof PsiFileSystemItem) {
       return element.isValid() ? ((PsiFileSystemItem)element).getVirtualFile() : null;
     }
-    if (element instanceof PsiElementWithVirtualFile) {
-      return ((PsiElementWithVirtualFile)element).getVirtualFile();
-    }
     PsiFile containingFile = element.getContainingFile();
     if (containingFile == null || !containingFile.isValid()) {
       return null;
