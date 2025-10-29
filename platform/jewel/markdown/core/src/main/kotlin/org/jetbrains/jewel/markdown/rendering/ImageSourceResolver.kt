1edb2f41154f0324c9adb9316e5f50f2ba6ebd96
Alexander Kuznetsov <aleksander.kuznetsov@jetbrains.com>
2025-10-29 11:52:10 +0100

    [JEWEL-1058] Improve DefaultImageSourceResolver
    - Support absolute and relative paths
    - Abstract different approaches
     to resolve a given string
    - Provide an option to not log resolve failures --
     we don't want to pollute logs in editor+preview mode
     when resolve works on each source file edit
    - Make `ImageSourceResolver.resolve` return null
    if a given string cannot be resolved.
    Making Coil try to render something by an obscure string
    can lead to unexpected behavior (exceptions at least)
    
    closes https://github.com/JetBrains/intellij-community/pull/3292
    
    (cherry picked from commit c67399952bc03a63312bbe59cf610f7c491a045b)
    
    
    (cherry picked from commit ea9c59b388589b925932414147f7ac446211f1c8)
    
    IJ-MR-185912
    
    GitOrigin-RevId: 1b920864172b38c0880b7af8ce0ed1635613f60a

--- platform/jewel/markdown/core/src/main/kotlin/org/jetbrains/jewel/markdown/rendering/ImageSourceResolver.kt
+++ platform/jewel/markdown/core/src/main/kotlin/org/jetbrains/jewel/markdown/rendering/ImageSourceResolver.kt
@@ -4,7 +4,9 @@ package org.jetbrains.jewel.markdown.rendering
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.staticCompositionLocalOf
 import java.net.URI
+import java.nio.file.Path
 import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.VisibleForTesting
 import org.jetbrains.jewel.foundation.ExperimentalJewelApi
 import org.jetbrains.jewel.foundation.util.myLogger
 
@@ -27,35 +29,134 @@ public interface ImageSourceResolver {
      *
      * @param rawDestination The raw destination string from the Markdown, e.g., "my-image.png" or
      *   "https://example.com/image.png".
-     * @return A fully-qualified, loadable path to the image, which can be consumed by an image loader.
+     * @return A fully-qualified, loadable path to the image, which can be consumed by an image loader, or `null` if the
+     *   image could not be resolved.
      */
-    public fun resolve(rawDestination: String): String
+    public fun resolve(rawDestination: String): String?
+
+    public companion object {
+        @VisibleForTesting
+        internal val defaultCapabilities =
+            setOf(
+                ResolveCapability.PlainUri,
+                ResolveCapability.RelativePathInResources(),
+                ResolveCapability.AbsolutePath,
+            )
+
+        /**
+         * Creates [ImageSourceResolver] that can resolve image links in Markdown files if they are either:
+         * - plain URIs, e.g., `https://example.com/image.png` or `file:///image.png`
+         * - absolute paths, e.g., `/image.png`
+         * - relative paths in the current classloader's resources, e.g., `/images/my-image.png`
+         * - relative paths relative to a given root directory [rootDir], e.g., `../images/my-image.png`
+         *
+         * If [logResolveFailure] is true, logs any failures to resolve image sources.
+         */
+        public fun create(rootDir: Path, logResolveFailure: Boolean): ImageSourceResolver =
+            create(
+                buildSet {
+                    addAll(defaultCapabilities)
+                    add(ResolveCapability.RelativePath(rootDir))
+                },
+                logResolveFailure,
+            )
+
+        /**
+         * Creates [ImageSourceResolver] that can resolve image links in Markdown files according to provided
+         * [resolveCapabilities].
+         *
+         * If [logResolveFailure] is true, logs any failures to resolve image sources.
+         */
+        public fun create(
+            resolveCapabilities: Set<ResolveCapability> = defaultCapabilities,
+            logResolveFailure: Boolean = true,
+        ): ImageSourceResolver = DefaultImageSourceResolver(resolveCapabilities, logResolveFailure)
+    }
+
+    /** Provides a list of capabilities that the default [ImageSourceResolver] implementation supports. */
+    @ApiStatus.Experimental
+    @ExperimentalJewelApi
+    public sealed interface ResolveCapability {
+        /** Resolves a raw image destination string from a Markdown file into a fully-qualified, loadable path. */
+        public fun resolve(rawDestination: String): String?
+
+        /** Represents the ability to resolve plain URIs as-is. */
+        @ApiStatus.Experimental
+        @ExperimentalJewelApi
+        public object PlainUri : ResolveCapability {
+            override fun toString(): String = "PlainUri"
+
+            override fun resolve(rawDestination: String): String? {
+                val uri = runCatching { URI.create(rawDestination) }.getOrNull() ?: return null
+                return if (uri.isAbsolute) rawDestination else null
+            }
+        }
+
+        /**
+         * Represents the ability to resolve relative paths in the [resourceClass] classloader's resources, or in the
+         * current classloader's resources if [resourceClass] is `null`.
+         */
+        @ApiStatus.Experimental
+        @ExperimentalJewelApi
+        public class RelativePathInResources(private val resourceClass: Class<*>? = null) : ResolveCapability {
+            override fun toString(): String = "RelativePathInResources"
+
+            override fun resolve(rawDestination: String): String? =
+                (resourceClass ?: javaClass).classLoader.getResource(rawDestination.removePrefix("/"))?.toExternalForm()
+        }
+
+        /** Represents the ability to resolve absolute paths as-is. */
+        public object AbsolutePath : ResolveCapability {
+            override fun resolve(rawDestination: String): String? {
+                val rawPath = Path.of(rawDestination)
+                if (rawPath.isAbsolute) return rawDestination
+                return null
+            }
+        }
+
+        /** Represents the ability to resolve relative paths relative to a given root directory [rootDir]. */
+        @ApiStatus.Experimental
+        @ExperimentalJewelApi
+        public class RelativePath(private val rootDir: Path) : ResolveCapability {
+            override fun resolve(rawDestination: String): String? {
+                val rawPath = Path.of(rawDestination)
+                // don't resolve absolute paths, it's not this resolver's capability
+                if (rawPath.isAbsolute) return null
+
+                val normalizedRoot = runCatching { rootDir.toAbsolutePath().normalize() }.getOrNull() ?: return null
+
+                val resolved = runCatching { normalizedRoot.resolve(rawPath).normalize() }.getOrNull() ?: return null
+
+                return resolved.toString()
+            }
+
+            override fun toString(): String = "RelativePath(rootDir=$rootDir)"
+        }
+    }
 }
 
 /**
- * The default implementation of [ImageSourceResolver].
+ * The default implementation of [ImageSourceResolver] that can resolve image links in Markdown files according to
+ * provided [resolveCapabilities].
  *
- * Resolves full URIs as-is and attempts to find relative paths in the current classloader's resources.
- *
+ * @param resolveCapabilities A list of [ImageSourceResolver.ResolveCapability]s that this resolver can support.
+ * @param logResolveFailure Whether to log any failures to resolve image sources.
  * @see ImageSourceResolver
  */
-internal object DefaultImageSourceResolver : ImageSourceResolver {
-    override fun resolve(rawDestination: String): String {
-        val uri = URI.create(rawDestination)
-        if (uri.scheme != null) return rawDestination
-
-        val resourceUrl = javaClass.classLoader.getResource(rawDestination.removePrefix("/"))
-
-        if (resourceUrl == null) {
+internal class DefaultImageSourceResolver(
+    private val resolveCapabilities: Set<ImageSourceResolver.ResolveCapability> =
+        ImageSourceResolver.defaultCapabilities,
+    private val logResolveFailure: Boolean = true,
+) : ImageSourceResolver {
+    override fun resolve(rawDestination: String): String? {
+        val result = resolveCapabilities.firstNotNullOfOrNull { it.resolve(rawDestination) }
+        if (result == null && logResolveFailure) {
             myLogger()
                 .warn(
-                    "Markdown image '$rawDestination' expected at classpath '$rawDestination' but not found. " +
-                        "Please ensure it's in your 'src/main/resources/' folder."
+                    "Failed to resolve image source: $rawDestination. Supported capabilities: ${resolveCapabilities.joinToString()}"
                 )
-            return rawDestination // This will cause Coil to fail and not render anything.
         }
-
-        return resourceUrl.toExternalForm()
+        return result
     }
 }
 
@@ -84,5 +185,5 @@ internal object DefaultImageSourceResolver : ImageSourceResolver {
 @ExperimentalJewelApi
 public val LocalMarkdownImageSourceResolver: ProvidableCompositionLocal<ImageSourceResolver> =
     staticCompositionLocalOf {
-        DefaultImageSourceResolver
+        ImageSourceResolver.create()
     }
