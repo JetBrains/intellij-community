5dc7067bd24bc8f8036511c7e1126190cd68df73
Andrey Zaytsev <andrey.zaytsev@jetbrains.com>
2025-10-29 10:08:01 +0100

    [analyzer] refactor: extract progress to a file
    GitOrigin-RevId: 78d779182d536b6f632ce4763ebff631ee2096d5

--- fleet/lsp.protocol/srcCommonMain/com/jetbrains/lsp/implementation/LspHandlers.kt
+++ fleet/lsp.protocol/srcCommonMain/com/jetbrains/lsp/implementation/LspHandlers.kt
@@ -53,33 +53,7 @@ class LspHandlerContext(
 context(context: LspHandlerContext)
 val lspClient: LspClient get() = context.lspClient
 
-fun interface ProgressReporter {
-  companion object {
-    val NOOP: ProgressReporter = ProgressReporter {}
-  }
-  fun report(progress: WorkDoneProgress)
-}
 
-suspend fun<T> LspClient.withProgress(
-  request: WorkDoneProgressParams,
-  body: suspend CoroutineScope.(ProgressReporter) -> T
-): T =
-  coroutineScope {
-    when (val token = request.workDoneToken) {
-      null -> body(ProgressReporter.NOOP)
-      else -> {
-        val state = MutableStateFlow<WorkDoneProgress?>(null)
-        launch {
-          state.filterNotNull().collect { p ->
-            notify(ProgressNotificationType, ProgressParams(token, LSP.json.encodeToJsonElement(WorkDoneProgress.serializer(), p)))
-            delay(100.milliseconds)
-          }
-        }.use {
-          body(ProgressReporter { p -> state.value = p })
-        }
-      }
-    }
-  }
 
 class LspRequestHandler<Params, Result, Error>(
     val requestType: RequestType<Params, Result, Error>,
