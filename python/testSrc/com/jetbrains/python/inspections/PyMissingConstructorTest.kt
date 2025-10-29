23a25581f0a025f7553707c58e0f6a457a959101
Daniil Kalinin <Daniil.Kalinin@jetbrains.com>
2025-10-29 15:34:55 +0100

    PY-85639 Migrate `PyMissingConstructorTest` to JUnit 5 framework (experiment) and restructure test data files
    Space-RevId: a1e4fc85f2f26b8a94779be8ae05b394f5d5ebed
    
    GitOrigin-RevId: 189b327b77b8fd2aa067fef708881fec4da337b9

--- python/testSrc/com/jetbrains/python/inspections/PyMissingConstructorTest.kt
+++ python/testSrc/com/jetbrains/python/inspections/PyMissingConstructorTest.kt
@@ -1,69 +1,22 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.inspections
 
-import com.jetbrains.python.fixtures.PyTestCase
+import com.intellij.psi.PsiFile
+import com.intellij.python.junit5Tests.framework.FolderTest
+import com.intellij.python.junit5Tests.framework.metaInfo.Repository
+import com.intellij.python.junit5Tests.framework.metaInfo.TestClassInfo
+import com.intellij.testFramework.TestDataPath
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import com.jetbrains.python.inspections.JUnit5.util.InspectionTest
+import com.jetbrains.python.inspections.JUnit5.util.doTestByFile
 
-class PyMissingConstructorTest : PyTestCase() {
-  fun testBasic() {
-    doTest()
-  }
+@InspectionTest(PyMissingConstructorInspection::class)
+@TestClassInfo(Repository.PY_COMMUNITY)
+@TestDataPath("\$CONTENT_ROOT/../testData/inspections/PyMissingConstructorInspection")
+class PyMissingConstructorTest {
 
-  // PY-3278
-  fun testQualifiedName() {
-    doTest()
-  }
-
-  // PY-3238
-  fun testNoConstructor() {
-    doTest()
-  }
-
-  // PY-3313
-  fun testDeepInheritance() {
-    doTest()
-  }
-
-  // PY-3395
-  fun testInheritFromSelf() {
-    doTest()
-  }
-
-  // PY-4038
-  fun testExplicitDunderClass() {
-    doTest()
-  }
-
-  // PY-20038
-  fun testImplicitDunderClass() {
-    doTest()
-  }
-
-  // PY-7176
-  fun testException() {
-    doTest()
-  }
-
-  // PY-7699
-  fun testInnerClass() {
-    doTest()
-  }
-
-  fun testPy3k() {
-    doTest()
-  }
-
-  // PY-33265
-  fun testAbstractConstructor() {
-    doTest()
-  }
-
-  private fun doTest() {
-    myFixture.configureByFile(TEST_DIRECTORY + getTestName(true) + ".py")
-    myFixture.enableInspections(PyMissingConstructorInspection::class.java)
-    myFixture.checkHighlighting(true, false, false)
-  }
-
-  companion object {
-    private const val TEST_DIRECTORY = "inspections/PyMissingConstructorInspection/"
+  @FolderTest
+  fun allTests(file: PsiFile, fixture: CodeInsightTestFixture) {
+    fixture.doTestByFile(file)
   }
 }
