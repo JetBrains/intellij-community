57f5eb0619d0b00ed9f34bbcb77debd8e8cadf6a
Egor Eliseev <egor.eliseev@jetbrains.com>
2025-10-30 21:14:56 +0000

    [python] PY-83362 Cannot stop debugger when debugging FastAPI
    Add a temporary solution.
    Will be fixed with debugpy
    
    
    Merge-request: IJ-MR-180688
    Merged-by: Egor Eliseev <Egor.Eliseev@jetbrains.com>
    
    GitOrigin-RevId: 4e536078c38eaa8013dc04c73c2c017def014919

--- python/src/com/jetbrains/python/run/PyDebugProcessHandler.kt
+++ python/src/com/jetbrains/python/run/PyDebugProcessHandler.kt
@@ -3,6 +3,10 @@ package com.jetbrains.python.run
 
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.util.io.BaseOutputReader
+import com.intellij.util.io.awaitExit
+import com.jetbrains.python.debugger.PythonDebuggerScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withTimeoutOrNull
 import java.nio.charset.Charset
 
 class PyDebugProcessHandler : PythonProcessHandler {
@@ -14,6 +18,21 @@ class PyDebugProcessHandler : PythonProcessHandler {
       BaseOutputReader.Options.forTerminalPtyProcess()
     } else {
       BaseOutputReader.Options.forMostlySilentProcess()
+    }
+  }
+
+  protected override fun doDestroyProcess() {
+    super.doDestroyProcess()
+
+    PythonDebuggerScope.launchOn(Dispatchers.IO) {
+      withTimeoutOrNull(1000) {
+        process.awaitExit()
+      } ?: if (shouldDestroyProcessRecursively() && processCanBeKilledByOS(process)) {
+        killProcessTree(process)
+      }
+      else {
+        process.destroy()
+      }
     }
   }
 }
