7b3fa32c6f8eee6a2778d89fe6175d28193a4a02
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2025-10-31 14:02:37 +0100

    Revert "[Kotlin] KTIJ-35916 Register KotlinForwardDeclarationsModelChangeService as WorkspaceModelChangeListener directly"
    This reverts commit 8ec8b5b491dbc0bcd239cd36159ea5e0d8c2e4ee.
    
    GitOrigin-RevId: 87b8e8a79ed75a7a35cde18e4595b4549a9d04de

--- plugins/kotlin/base/project-structure/src/org/jetbrains/kotlin/idea/base/projectStructure/forwardDeclarations/KotlinForwardDeclarationsModelChangeService.kt
+++ plugins/kotlin/base/project-structure/src/org/jetbrains/kotlin/idea/base/projectStructure/forwardDeclarations/KotlinForwardDeclarationsModelChangeService.kt
@@ -1,12 +1,16 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.base.projectStructure.forwardDeclarations
 
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.OrderRootType
 import com.intellij.openapi.roots.impl.libraries.LibraryEx
+import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.backend.workspace.WorkspaceModel
 import com.intellij.platform.backend.workspace.WorkspaceModelChangeListener
+import com.intellij.platform.backend.workspace.WorkspaceModelTopics
 import com.intellij.platform.workspace.jps.entities.LibraryEntity
 import com.intellij.platform.workspace.jps.entities.modifyLibraryEntity
 import com.intellij.platform.workspace.storage.EntityChange
@@ -26,44 +30,44 @@ import org.jetbrains.kotlin.platform.impl.NativeIdePlatformKind
 import java.io.File
 
 /**
- * Listener for background generation of K/N forward declaration files on workspace model updates.
+ * Service for background generation of K/N forward declaration files on workspace model updates.
  * Detected updates in relevant KLIB libraries trigger regeneration of synthetic Kotlin files.
  * Generated roots are stored inside a child [KotlinForwardDeclarationsWorkspaceEntity] of the affected library.
  *
  * @see [KotlinForwardDeclarationsFileGenerator]
  */
-internal class KotlinForwardDeclarationsModelChangeService(
-    private val project: Project,
-    private val cs: CoroutineScope,
-) : WorkspaceModelChangeListener {
+@Service(Service.Level.PROJECT)
+internal class KotlinForwardDeclarationsModelChangeService(private val project: Project, cs: CoroutineScope) {
+    init {
+        val busConnection = project.messageBus.connect()
+        busConnection.subscribe(WorkspaceModelTopics.CHANGED, object : WorkspaceModelChangeListener {
+            override fun changed(event: VersionedStorageChange) {
+                val fwdDeclarationChanges = event.getChanges<KotlinForwardDeclarationsWorkspaceEntity>()
+                cleanUp(fwdDeclarationChanges)
 
-    override fun changed(event: VersionedStorageChange) {
-        cs.launch {
-            val fwdDeclarationChanges = event.getChanges<KotlinForwardDeclarationsWorkspaceEntity>()
-            cleanUp(fwdDeclarationChanges)
+                val libraryChanges = event.getChanges<LibraryEntity>().ifEmpty { return }
 
-            val libraryChanges = event.getChanges<LibraryEntity>().ifEmpty { return@launch }
+                val nativeKlibs: Map<LibraryEntity, KLibRoot> =
+                    libraryChanges.toNativeKLibs(event.storageAfter).ifEmpty { return }
+                val workspaceModel = WorkspaceModel.getInstance(project)
+                val createEntityStorageChanges = createEntityStorageChanges(workspaceModel, nativeKlibs)
 
-            val nativeKlibs: Map<LibraryEntity, KLibRoot> =
-                libraryChanges.toNativeKLibs(event.storageAfter).ifEmpty { return@launch }
-            val workspaceModel = WorkspaceModel.getInstance(project)
-            val createEntityStorageChanges = createEntityStorageChanges(workspaceModel, nativeKlibs)
+                cs.launch {
+                    workspaceModel.update("Kotlin Forward Declarations workspace model update") { storage ->
+                        createEntityStorageChanges.forEach { (libraryEntity, builder) ->
 
-            cs.launch {
-                workspaceModel.update("Kotlin Forward Declarations workspace model update") { storage ->
-                    createEntityStorageChanges.forEach { (libraryEntity, builder) ->
-
-                        // a hack to bypass workspace model issues; without the extra check entity updates lead to recursion
-                        if (libraryEntity.kotlinForwardDeclarationsWorkspaceEntity == null) {
-                            storage.modifyLibraryEntity(libraryEntity) {
-                                this.kotlinForwardDeclarationsWorkspaceEntity = builder
+                            // a hack to bypass workspace model issues; without the extra check entity updates lead to recursion
+                            if (libraryEntity.kotlinForwardDeclarationsWorkspaceEntity == null) {
+                                storage.modifyLibraryEntity(libraryEntity) {
+                                    this.kotlinForwardDeclarationsWorkspaceEntity = builder
+                                }
+                                storage.addEntity(builder)
                             }
-                            storage.addEntity(builder)
                         }
                     }
                 }
             }
-        }
+        })
     }
 
     private fun cleanUp(fwdDeclarationChanges: List<EntityChange<KotlinForwardDeclarationsWorkspaceEntity>>) {
@@ -115,5 +119,14 @@ internal class KotlinForwardDeclarationsModelChangeService(
         return getFiles(OrderRootType.CLASSES).filter {
             it.isKlibLibraryRootForPlatform(NativeIdePlatformKind.defaultPlatform)
         }
+    }
+}
+
+/**
+ * Request [KotlinForwardDeclarationsModelChangeService] on startup to start receiving workspace model update events.
+ */
+internal class KotlinForwardDeclarationsStartupActivity : ProjectActivity {
+    override suspend fun execute(project: Project) {
+        project.service<KotlinForwardDeclarationsModelChangeService>()
     }
 }
