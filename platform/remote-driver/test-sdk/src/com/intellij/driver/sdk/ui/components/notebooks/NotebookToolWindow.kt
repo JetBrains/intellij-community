a353dc54228065a55411cfbe88d9d5b8a553aacd
Ilya.Komolkin <ilya.komolkin@jetbrains.com>
2025-10-31 12:31:40 +0000

    ik/test-speedup
    more precise work the kernel restart notification
    
    minor
    
    review fixes
    
    extract RestartNotificationUtil.kt
    
    speed up + waitFor text
    
    
    Merge-request: IJ-MR-180606
    Merged-by: Ilya Komolkin <Ilya.Komolkin@jetbrains.com>
    
    GitOrigin-RevId: bcdbb660666941741c31572e94320dd4f94dc2eb

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/notebooks/NotebookToolWindow.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/notebooks/NotebookToolWindow.kt
@@ -4,6 +4,8 @@ import com.intellij.driver.sdk.ui.Finder
 import com.intellij.driver.sdk.ui.components.ComponentData
 import com.intellij.driver.sdk.ui.components.UIComponentsList
 import com.intellij.driver.sdk.ui.components.UiComponent
+import com.intellij.driver.sdk.waitFor
+import kotlin.time.Duration.Companion.seconds
 
 internal val Finder.kotlinNotebookSessionToolWindow: KotlinNotebookToolWindowUiComponent
   get() = x("""
@@ -62,4 +64,6 @@ class KotlinNotebookToolWindowUiComponent(data: ComponentData) : UiComponent(dat
     get() = if (variablesButton == null) "" else {
       variablesToolWindowUiComponent.getAllTexts().joinToString("") { it.text }
     }
+
+  fun waitForVariablesViewText(text: String) = waitFor("expect $text in $variablesViewText", timeout = 25.seconds) { variablesViewText.contains(text) }
 }
