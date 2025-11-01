aa86c24099ef1d9ed6397aef4725a751f5affae7
Nikita Biriukov <nikita.biriukov@jetbrains.com>
2025-11-01 14:18:11 +0100

    [gradle][kotlin][tests] IDEA-302835 refactor KotlinGradleTomlVersionCatalogGotoDeclarationHandler
    * simplify code
    * move a method from the class to util.kt, nearby the related function
    
    Tests:
    * KotlinGradleFindUsagesTest
    * KotlinGradleVersionCatalogNavigationTest
    
    GitOrigin-RevId: 46b11b081cd5ae084a06f8753636b3e87b6ce7de

--- plugins/kotlin/gradle/code-insight-toml/src/org/jetbrains/kotlin/idea/gradle/versionCatalog/toml/KotlinGradleTomlVersionCatalogGotoDeclarationHandler.kt
+++ plugins/kotlin/gradle/code-insight-toml/src/org/jetbrains/kotlin/idea/gradle/versionCatalog/toml/KotlinGradleTomlVersionCatalogGotoDeclarationHandler.kt
@@ -8,51 +8,33 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
-import com.intellij.psi.impl.source.tree.LeafPsiElement
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
 import org.jetbrains.kotlin.utils.addToStdlib.UnsafeCastFunction
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
-import org.jetbrains.plugins.gradle.service.project.CommonGradleProjectResolverExtension
+import org.jetbrains.plugins.gradle.service.project.CommonGradleProjectResolverExtension.GRADLE_VERSION_CATALOGS_DYNAMIC_SUPPORT
 import org.jetbrains.plugins.gradle.toml.findOriginInTomlFile
 import org.jetbrains.plugins.gradle.util.isInVersionCatalogAccessor
 import org.jetbrains.uast.toUElement
 import org.jetbrains.uast.tryResolve
 
 /**
- * Enables navigation to a library in a Version catalog from its reference in `build.gradle.kts`.
- * Relies on `SyntheticVersionCatalogAccessor`, which generates classes providing getters for properties of Gradle Version catalogs.
- * UAST element for a dot-qualified expression could be resolved to such a getter.
- *
- * @see [org.jetbrains.plugins.gradle.service.resolve.static.SyntheticVersionCatalogAccessor]
+ * Enables navigation: `build.gradle.kts` -> TOML Version catalog
  */
 class KotlinGradleTomlVersionCatalogGotoDeclarationHandler : GotoDeclarationHandler {
 
     override fun getGotoDeclarationTargets(sourceElement: PsiElement?, offset: Int, editor: Editor?): Array<PsiElement>? {
-        if (sourceElement !is LeafPsiElement) return null
-        if (!Registry.`is`(CommonGradleProjectResolverExtension.GRADLE_VERSION_CATALOGS_DYNAMIC_SUPPORT, false)) {
-            return null
-        }
-        val grandParent = sourceElement.parent?.parent ?: return null
-        if (grandParent !is KtDotQualifiedExpression) return null
-        val fullExpression = findFullVersionCatalogExpression(grandParent)
-        val propertyGetter = fullExpression.toUElement()?.tryResolve()
+        if (!Registry.`is`(GRADLE_VERSION_CATALOGS_DYNAMIC_SUPPORT, false)) return null
+
+        val topmostDotExpression = findTopmostVersionCatalogExpression(sourceElement) ?: return null
+        return resolveViaCatalogAccessor(topmostDotExpression)
+    }
+
+    private fun resolveViaCatalogAccessor(dotExpression: KtDotQualifiedExpression): Array<PsiElement>? {
+        val propertyGetter = dotExpression.toUElement()?.tryResolve()
             ?.safeAs<PsiMethod>()
             ?.takeIf(::isInVersionCatalogAccessor)
             ?: return null
-        return findOriginInTomlFile(propertyGetter, fullExpression)
+        return findOriginInTomlFile(propertyGetter, dotExpression)
             ?.let { arrayOf(it) }
-    }
-
-    /**
-     * Finds the full dot-qualified expression referring to a version catalog variable.
-     * E.g., if the element is `libs.aaa` in `libs.aaa.b.c` (or in `libs.aaa.b.c.get()`), it will return the element corresponding to
-     * `libs.aaa.b.c` (but before `.get()`).
-     */
-    private fun findFullVersionCatalogExpression(element: KtDotQualifiedExpression): PsiElement {
-        var fullExpression = element
-        while (fullExpression.hasWrappingVersionCatalogExpression()) {
-            fullExpression = fullExpression.parent as KtDotQualifiedExpression
-        }
-        return fullExpression
     }
 }
--- plugins/kotlin/gradle/code-insight-toml/src/org/jetbrains/kotlin/idea/gradle/versionCatalog/toml/util.kt
+++ plugins/kotlin/gradle/code-insight-toml/src/org/jetbrains/kotlin/idea/gradle/versionCatalog/toml/util.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.gradle.versionCatalog.toml
 
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.parentOfType
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
 import org.jetbrains.kotlin.psi.KtNameReferenceExpression
 
@@ -9,4 +11,18 @@ import org.jetbrains.kotlin.psi.KtNameReferenceExpression
  */
 internal fun KtDotQualifiedExpression.hasWrappingVersionCatalogExpression() =
     parent is KtDotQualifiedExpression
-        && parent.lastChild is KtNameReferenceExpression // this is false if parent expression ends with `.get()`
+            && parent.lastChild is KtNameReferenceExpression // this is false if parent expression ends with `.get()`
+
+/**
+ * Finds the topmost wrapping KtDotQualifiedExpression that could be a version catalog reference.
+ * Example:
+ *  - Given: `aaa` element from `libs.aaa.b.c`. Returns: `libs.aaa.b.c` KtDotQualifiedExpression
+ *  - Given: `aaa` element from `libs.aaa.b.c.get()`. Returns: `libs.aaa.b.c` (before `.get()`).
+ */
+internal fun findTopmostVersionCatalogExpression(element: PsiElement?): KtDotQualifiedExpression? {
+    var fullExpression = element?.parentOfType<KtDotQualifiedExpression>() ?: return null
+    while (fullExpression.hasWrappingVersionCatalogExpression()) {
+        fullExpression = fullExpression.parent as KtDotQualifiedExpression
+    }
+    return fullExpression
+}
