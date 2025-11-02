6be296bc1c046bf888ed49f9b1d143845d7ae586
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2025-11-02 22:10:44 +0100

    [terminal] IJPL-214284 the requested working directory path should be absolute
    This protects from accepting Unix paths on Windows, because `Files.isDirectory(Path.of("/"))` returns true on Windows.
    This is so, because `Path.of("/").toAbsolutePath().toString() == "C:\"`.
    
    
    (cherry picked from commit 41a021fd6c604b2dc54543c887df9196e20e6873)
    
    IJ-CR-180815
    
    GitOrigin-RevId: eddc660f0f3f10e41996011f3ebc63941522e8cf

--- plugins/terminal/tests/src/com/intellij/terminal/tests/runner/LocalOptionsConfigurerTest.kt
+++ plugins/terminal/tests/src/com/intellij/terminal/tests/runner/LocalOptionsConfigurerTest.kt
@@ -1,13 +1,16 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.terminal.tests.runner
 
+import com.intellij.execution.Platform
 import com.intellij.execution.configuration.EnvironmentVariablesData
 import com.intellij.idea.TestFor
+import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.impl.wsl.WslConstants
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.testFramework.utils.io.deleteRecursively
 import com.intellij.util.containers.CollectionFactory
+import com.intellij.util.system.OS
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.plugins.terminal.ShellStartupOptions
 import org.jetbrains.plugins.terminal.TerminalProjectOptionsProvider
@@ -16,9 +19,11 @@ import org.jetbrains.plugins.terminal.runner.LocalTerminalStartCommandBuilder.co
 import org.jetbrains.plugins.terminal.util.TerminalEnvironment
 import org.jetbrains.plugins.terminal.util.TerminalEnvironment.TERMINAL_EMULATOR
 import org.jetbrains.plugins.terminal.util.TerminalEnvironment.TERM_SESSION_ID
+import java.nio.file.FileSystems
 import java.nio.file.Path
 import kotlin.io.path.createTempDirectory
 import kotlin.io.path.pathString
+import kotlin.reflect.KMutableProperty0
 
 @TestFor(classes = [LocalOptionsConfigurer::class])
 internal class LocalOptionsConfigurerTest : BasePlatformTestCase() {
@@ -44,11 +49,11 @@ internal class LocalOptionsConfigurerTest : BasePlatformTestCase() {
   }
 
   fun testZsh() {
-    TerminalProjectOptionsProvider.getInstance(project).startingDirectory = tempDirectory.pathString
+    setDefaultStartingDirectory(tempDirectory.pathString)
 
     val actual = LocalOptionsConfigurer.configureStartupOptions(
       ShellStartupOptions.Builder()
-        .shellCommand(mutableListOf<String>("/bin/zsh"))
+        .shellCommand(mutableListOf("/bin/zsh"))
         .envVariables(buildMap {
           put("MY_CUSTOM_ENV1", "MY_CUSTOM_ENV_VALUE1")
         })
@@ -64,11 +69,11 @@ internal class LocalOptionsConfigurerTest : BasePlatformTestCase() {
   }
 
   fun testBash() {
-    TerminalProjectOptionsProvider.getInstance(project).startingDirectory = tempDirectory.pathString
+    setDefaultStartingDirectory(tempDirectory.pathString)
 
     val actual = LocalOptionsConfigurer.configureStartupOptions(
       ShellStartupOptions.Builder()
-        .shellCommand(mutableListOf<String>("/bin/bash"))
+        .shellCommand(mutableListOf("/bin/bash"))
         .envVariables(buildMap {
           put("MY_CUSTOM_ENV1", "MY_CUSTOM_ENV_VALUE1")
         })
@@ -153,4 +158,109 @@ internal class LocalOptionsConfigurerTest : BasePlatformTestCase() {
     assertThat(resultEnvs).isEqualTo(mapOf(WslConstants.WSLENV to expectedResultWslEnvValue))
   }
 
+  fun testAcceptWorkingDirectoryIfAbsoluteAndExits() {
+    val defaultStartingDirectory = tempDirectory.pathString
+    setDefaultStartingDirectory(defaultStartingDirectory)
+    val defaultShellCommand = listOf("/bin/default-shell")
+    setDefaultShellPath(defaultShellCommand.joinToString(separator = " "))
+    val customShellCommand = listOf("/bin/custom-shell")
+    val tmpDir2 = createTmpDirectory("tmp-dir-2")
+
+    doTestShellPathAndWorkingDirectory(
+      null,
+      null,
+      defaultShellCommand,
+      defaultStartingDirectory
+    )
+    doTestShellPathAndWorkingDirectory(
+      null,
+      "",
+      defaultShellCommand,
+      defaultStartingDirectory
+    )
+    doTestShellPathAndWorkingDirectory(
+      customShellCommand,
+      ".",
+      defaultShellCommand,
+      defaultStartingDirectory
+    )
+    doTestShellPathAndWorkingDirectory(
+      customShellCommand,
+      "",
+      defaultShellCommand,
+      defaultStartingDirectory
+    )
+    doTestShellPathAndWorkingDirectory(
+      customShellCommand,
+      null,
+      customShellCommand,
+      defaultStartingDirectory
+    )
+    doTestShellPathAndWorkingDirectory(
+      customShellCommand,
+      tmpDir2.pathString,
+      customShellCommand,
+      tmpDir2.pathString
+    )
+    doTestShellPathAndWorkingDirectory(
+      customShellCommand,
+      if (OS.CURRENT.platform == Platform.WINDOWS) "/" else "C:/" /* flipped roots */,
+      defaultShellCommand,
+      defaultStartingDirectory
+    )
+    val root = FileSystems.getDefault().rootDirectories.first()
+    doTestShellPathAndWorkingDirectory(
+      customShellCommand,
+      root.pathString,
+      customShellCommand,
+      root.pathString
+    )
+  }
+
+  private fun doTestShellPathAndWorkingDirectory(
+    requestedShellCommand: List<String>?,
+    requestedWorkingDirectory: String?,
+    expectedShellCommand: List<String>,
+    expectedWorkingDirectory: String,
+  ) {
+    val actual = LocalOptionsConfigurer.configureStartupOptions(
+      ShellStartupOptions.Builder()
+        .shellCommand(requestedShellCommand)
+        .workingDirectory(requestedWorkingDirectory)
+        .build(),
+      project
+    )
+    assertEquals(expectedShellCommand, actual.shellCommand)
+    assertEquals(expectedWorkingDirectory, actual.workingDirectory)
+  }
+
+  @Suppress("SameParameterValue")
+  private fun createTmpDirectory(prefix: String? = null): Path {
+    val dir = createTempDirectory(prefix)
+    Disposer.register(testRootDisposable) {
+      try {
+        tempDirectory.deleteRecursively()
+      }
+      catch (e: Throwable) {
+        LOG.warn(e)
+      }
+    }
+    return dir
+  }
+
+  private fun setDefaultStartingDirectory(startingDirectory: String) {
+    setValueForTest(TerminalProjectOptionsProvider.getInstance(project)::startingDirectory, startingDirectory)
+  }
+
+  private fun setDefaultShellPath(shellPath: String) {
+    setValueForTest(TerminalProjectOptionsProvider.getInstance(project)::shellPath, shellPath)
+  }
+
+  private fun <V> setValueForTest(prop: KMutableProperty0<V>, newValue: V) {
+    val prevValue = prop.get()
+    prop.set(newValue)
+    Disposer.register(testRootDisposable) {
+      prop.set(prevValue)
+    }
+  }
 }
