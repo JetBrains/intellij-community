37d614dbad50fec970203df0672cadf4ec2c512e
Piotr Kasprowicz <piotr.kasprowicz@jetbrains.com>
2025-11-03 21:06:53 +0100

    [kotlin] KTIJ-35934 Migrate SortModifiersInspection to ModCommand
    GitOrigin-RevId: b396c6d57018cc6481e1068f8e2e7f3009d05540

--- plugins/kotlin/code-insight/inspections-shared/src/org/jetbrains/kotlin/idea/codeInsight/inspections/shared/SortModifiersInspection.kt
+++ plugins/kotlin/code-insight/inspections-shared/src/org/jetbrains/kotlin/idea/codeInsight/inspections/shared/SortModifiersInspection.kt
@@ -2,59 +2,79 @@
 package org.jetbrains.kotlin.idea.codeInsight.inspections.shared
 
 import com.intellij.codeInspection.CleanupLocalInspectionTool
-import com.intellij.openapi.editor.Editor
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.modcommand.ModPsiUpdater
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.IntellijInternalApi
 import com.intellij.openapi.util.TextRange
 import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.inspections.AbstractApplicabilityBasedInspection
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.asUnit
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinApplicableInspectionBase
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinModCommandQuickFix
 import org.jetbrains.kotlin.lexer.KtModifierKeywordToken
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtAnnotation
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.psi.KtModifierList
 import org.jetbrains.kotlin.psi.KtModifierListOwner
+import org.jetbrains.kotlin.psi.KtVisitor
+import org.jetbrains.kotlin.psi.KtVisitorVoid
 import org.jetbrains.kotlin.psi.addRemoveModifier.sortModifiers
+import org.jetbrains.kotlin.psi.modifierListVisitor
 import org.jetbrains.kotlin.psi.psiUtil.allChildren
 import org.jetbrains.kotlin.psi.psiUtil.endOffset
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
 
 @ApiStatus.Internal
 @IntellijInternalApi
-class SortModifiersInspection : AbstractApplicabilityBasedInspection<KtModifierList>(
-    KtModifierList::class.java
-), CleanupLocalInspectionTool {
-    override fun isApplicable(element: KtModifierList): Boolean {
+class SortModifiersInspection : KotlinApplicableInspectionBase.Simple<KtModifierList, Unit>(), CleanupLocalInspectionTool {
+
+    override fun KaSession.prepareContext(element: KtModifierList) {}
+
+    override fun isApplicableByPsi(element: KtModifierList): Boolean {
         val modifiers = element.modifierKeywordTokens()
         if (modifiers.isEmpty()) return false
         val sortedModifiers = sortModifiers(modifiers)
         return modifiers != sortedModifiers || element.modifiersBeforeAnnotations()
     }
 
-    override fun inspectionHighlightRangeInElement(element: KtModifierList): TextRange? {
-        val modifierElements = element.allChildren.toList()
-        val startElement = modifierElements.firstOrNull { it.node.elementType is KtModifierKeywordToken } ?: return null
-        val endElement = modifierElements.lastOrNull { it.node.elementType is KtModifierKeywordToken } ?: return null
-        return TextRange(startElement.startOffset, endElement.endOffset).shiftLeft(element.startOffset)
+    override fun buildVisitor(
+        holder: ProblemsHolder,
+        isOnTheFly: Boolean,
+    ): KtVisitor<*, *> = modifierListVisitor {
+        visitTargetElement(it, holder, isOnTheFly)
     }
 
-    override fun inspectionText(element: KtModifierList) =
+    override fun getApplicableRanges(element: KtModifierList): List<TextRange> {
+        val modifierElements = element.allChildren.toList()
+        val startElement = modifierElements.firstOrNull { it.node.elementType is KtModifierKeywordToken } ?: return emptyList()
+        val endElement = modifierElements.lastOrNull { it.node.elementType is KtModifierKeywordToken } ?: return emptyList()
+        return listOf(TextRange(startElement.startOffset, endElement.endOffset).shiftLeft(element.startOffset))
+    }
+
+    override fun getProblemDescription(element: KtModifierList, context: Unit): String =
         if (element.modifiersBeforeAnnotations()) KotlinBundle.message("modifiers.should.follow.annotations") else KotlinBundle.message("non.canonical.modifiers.order")
 
-    override val defaultFixText get() = KotlinBundle.message("sort.modifiers")
+    override fun createQuickFix(
+        element: KtModifierList,
+        context: Unit,
+    ): KotlinModCommandQuickFix<KtModifierList> = object : KotlinModCommandQuickFix<KtModifierList>() {
+        override fun getFamilyName(): String = KotlinBundle.message("sort.modifiers")
 
-    override fun applyTo(element: KtModifierList, project: Project, editor: Editor?) {
-        val owner = element.parent as? KtModifierListOwner ?: return
-        val sortedModifiers = sortModifiers(element.modifierKeywordTokens())
-        val existingModifiers = sortedModifiers.filter { owner.hasModifier(it) }
-        existingModifiers.forEach { owner.removeModifier(it) }
-        // We add visibility / modality modifiers after all others,
-        // because they can be redundant or not depending on others (e.g. override)
-        existingModifiers
-            .partition { it in KtTokens.VISIBILITY_MODIFIERS || it in KtTokens.MODALITY_MODIFIERS }
-            .let { it.second + it.first }
-            .forEach { owner.addModifier(it) }
+        override fun applyFix(project: Project, element: KtModifierList, updater: ModPsiUpdater) {
+            val owner = element.parent as? KtModifierListOwner ?: return
+            val sortedModifiers = sortModifiers(element.modifierKeywordTokens())
+            val existingModifiers = sortedModifiers.filter { owner.hasModifier(it) }
+            existingModifiers.forEach { owner.removeModifier(it) }
+            // We add visibility / modality modifiers after all others,
+            // because they can be redundant or not depending on others (e.g. override)
+            existingModifiers
+                .partition { it in KtTokens.VISIBILITY_MODIFIERS || it in KtTokens.MODALITY_MODIFIERS }
+                .let { it.second + it.first }
+                .forEach { owner.addModifier(it) }
+        }
     }
 
     private fun KtModifierList.modifierKeywordTokens(): List<KtModifierKeywordToken> {
