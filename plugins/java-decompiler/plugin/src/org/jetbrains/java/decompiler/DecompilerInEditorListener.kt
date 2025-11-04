df3b0fb096dc01eda47ac2dcdfc5895a9bb2a800
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-11-04 11:39:05 +0100

    [java-decompiler] format DecompilerInEditorListener: fix minor grammar in comment
    GitOrigin-RevId: f566f3513a24059a5450b721eef858de132bb76e

--- plugins/java-decompiler/plugin/src/org/jetbrains/java/decompiler/DecompilerInEditorListener.kt
+++ plugins/java-decompiler/plugin/src/org/jetbrains/java/decompiler/DecompilerInEditorListener.kt
@@ -38,7 +38,7 @@ private class DecompilerInEditorListener : EditorFactoryListener {
    *
    * We want to add an icon to the small toolbar in the upper-right corner of the editor (aka "the inspection widget").
    *
-   * Normal, well-behaved plugins (for example GitHub plugin, for code review), contribute their own actions to the
+   * Normal, well-behaved plugins (for example, the GitHub plugin, for code review), contribute their own actions to the
    * inspection widget by calling [EditorMarkupModel.addInspectionWidgetAction].
    *
    * Unfortunately, we cannot act like a well-behaved plugin because the inspection widget is not shown for
