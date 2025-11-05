1e0516bbf567190f0f0ba6c3163fca5d00f5d388
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2025-11-05 13:22:26 +0100

    [gradle][docker][wsl][IDEA-285542] gradle scripting debug support over Eel
    (cherry picked from commit 72123e6197cec203ae1bb3b3986e8fb78792dbc7)
    
    IJ-CR-181569
    
    GitOrigin-RevId: 2cfdaab497fb4b1e0b093eed80a117aa82e3199c

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/task/debugger/GradleDebuggerSupport.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/task/debugger/GradleDebuggerSupport.kt
@@ -1,9 +1,11 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.service.task.debugger
 
-import com.intellij.openapi.externalSystem.rt.execution.ForkedDebuggerHelper
-import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunnableState
+import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
+import com.intellij.openapi.externalSystem.rt.execution.ForkedDebuggerHelper.DEBUGGER_AGENT_SINK_PORT_SYS_PROP
+import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunnableState.*
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
 import com.intellij.platform.eel.provider.LocalEelDescriptor
 import com.intellij.platform.eel.provider.getEelDescriptor
 import org.jetbrains.annotations.ApiStatus
@@ -14,7 +16,7 @@ import org.jetbrains.plugins.gradle.settings.GradleExecutionSettings
 object GradleDebuggerSupport {
 
   private const val LOCALHOST = "127.0.0.1"
-  private const val DEBUGGER_AGENT_JVM_ARG_FORMAT = "-D${ForkedDebuggerHelper.DEBUGGER_AGENT_SINK_PORT_SYS_PROP}=%d"
+  private const val DEBUGGER_AGENT_JVM_ARG_FORMAT = "-D${DEBUGGER_AGENT_SINK_PORT_SYS_PROP}=%d"
 
   /**
    * See the documentation entry for [GradleDebuggerProxyManager.getReverseProxyLocalSink].
@@ -38,18 +40,46 @@ object GradleDebuggerSupport {
    */
   @JvmStatic
   fun setupDebuggerProxy(context: GradleExecutionContext, settings: GradleExecutionSettings) {
-    val dispatchPort = settings.getUserData(ExternalSystemRunnableState.DEBUGGER_DISPATCH_PORT_KEY) ?: return
-    val project = context.project
-    if (project.getEelDescriptor() == LocalEelDescriptor) {
+    if (context.project.getEelDescriptor() == LocalEelDescriptor) {
       return
     }
-    val proxyManager = project.getService(GradleDebuggerProxyManager::class.java)
 
-    val remoteSink: Int = proxyManager.launchProxy(context.taskId, dispatchPort)
-    settings.putUserData(ExternalSystemRunnableState.DEBUGGER_DISPATCH_ADDR_KEY, LOCALHOST)
-    settings.putUserData(ExternalSystemRunnableState.DEBUGGER_DISPATCH_PORT_KEY, remoteSink)
+    if (settings.isExecutionDebugRequired()) {
+      val proxyManager = context.project.getService(GradleDebuggerProxyManager::class.java)
+      settings.replacePortWithProxy(proxyManager, context.taskId, DEBUGGER_DISPATCH_PORT_KEY)
 
-    val debuggerAgentSink = proxyManager.setupReverseProxy(context.taskId)
-    settings.withArguments(DEBUGGER_AGENT_JVM_ARG_FORMAT.format(debuggerAgentSink));
+      // this port will be used by the debugger agent ON THE REMOTE SIDE.
+      val debuggerAgentSink = proxyManager.setupReverseProxy(context.taskId)
+      val debugArgument = DEBUGGER_AGENT_JVM_ARG_FORMAT.format(debuggerAgentSink)
+      settings.withArguments(debugArgument)
+    }
+
+    if (settings.isScriptDebugRequired()) {
+      val proxyManager = context.project.getService(GradleDebuggerProxyManager::class.java)
+      settings.replacePortWithProxy(proxyManager, context.taskId, BUILD_PROCESS_DEBUGGER_PORT_KEY)
+    }
+  }
+
+  private fun GradleExecutionSettings.replacePortWithProxy(
+    proxyManager: GradleDebuggerProxyManager,
+    taskId: ExternalSystemTaskId,
+    portKey: Key<Int>,
+  ) {
+    val sourcePort = getUserData(portKey)
+                     ?: throw IllegalStateException("The source port from key $portKey should not be null")
+    val proxyPort = proxyManager.launchProxy(taskId, sourcePort)
+
+    putUserData(portKey, proxyPort)
+    putUserData(DEBUGGER_DISPATCH_ADDR_KEY, LOCALHOST)
+  }
+
+  private fun GradleExecutionSettings.isExecutionDebugRequired(): Boolean {
+    val port = getUserData(DEBUGGER_DISPATCH_PORT_KEY) ?: -1
+    return port > 0
+  }
+
+  private fun GradleExecutionSettings.isScriptDebugRequired(): Boolean {
+    val port = getUserData(BUILD_PROCESS_DEBUGGER_PORT_KEY) ?: -1
+    return isDebugServerProcess && port > 0
   }
 }
