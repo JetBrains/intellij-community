51b7288b04fac1993d4b55204d7e85cf399ecb16
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2025-11-05 14:48:12 +0100

    [maven] do not hardcore expected thread id in tests
    GitOrigin-RevId: b0a5e6319fd64e7ff6563b46e9c8be6d6badefe8

--- plugins/maven/maven-event-listener/src/test/java/org/jetbrains/maven/server/EventInfoPrinterTest.java
+++ plugins/maven/maven-event-listener/src/test/java/org/jetbrains/maven/server/EventInfoPrinterTest.java
@@ -8,23 +8,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class EventInfoPrinterTest {
   @Test
   public void testBasicPrinting() {
-    assertPrintResult("[IJ]-1-null", null);
-    assertPrintResult("[IJ]-1-type", "type");
-    assertPrintResult("[IJ]-1-type-[IJ]-name=value", "type", "name", "value");
-    assertPrintResult("[IJ]-1-type-[IJ]-name=null", "type", "name", null);
-    assertPrintResult("[IJ]-1-type-[IJ]-name1=value1-[IJ]-name2=value2",
+    String threadId = String.valueOf(Thread.currentThread().getId());
+    assertPrintResult("[IJ]-" + threadId + "-null", null);
+    assertPrintResult("[IJ]-" + threadId + "-type", "type");
+    assertPrintResult("[IJ]-" + threadId + "-type-[IJ]-name=value", "type", "name", "value");
+    assertPrintResult("[IJ]-" + threadId + "-type-[IJ]-name=null", "type", "name", null);
+    assertPrintResult("[IJ]-" + threadId + "-type-[IJ]-name1=value1-[IJ]-name2=value2",
                       "type", "name1", "value1", "name2", "value2");
   }
 
   @Test
   public void testPrintingWithNewLines() {
-    assertPrintResult("[IJ]-1-type-[IJ]-name=-[N]-", "type", "name", "\n");
-    assertPrintResult("[IJ]-1-type-[IJ]-name=-[N]--[N]-", "type", "name", "\n\n");
-    assertPrintResult("[IJ]-1-type-[IJ]-name=-[N]-abc", "type", "name", "\nabc");
-    assertPrintResult("[IJ]-1-type-[IJ]-name=abc-[N]-", "type", "name", "abc\n");
-    assertPrintResult("[IJ]-1-type-[IJ]-name=abc-[N]-def", "type", "name", "abc\ndef");
-    assertPrintResult("[IJ]-1-type-[IJ]-name=-[N]-abc-[N]-", "type", "name", "\nabc\n");
-    assertPrintResult("[IJ]-1-type-[IJ]-name=1-[N]-2-[N]-3-[N]--[N]-4-[N]--[N]-5-[N]--[N]-6",
+    String threadId = String.valueOf(Thread.currentThread().getId());
+    assertPrintResult("[IJ]-" + threadId + "-type-[IJ]-name=-[N]-", "type", "name", "\n");
+    assertPrintResult("[IJ]-" + threadId + "-type-[IJ]-name=-[N]--[N]-", "type", "name", "\n\n");
+    assertPrintResult("[IJ]-" + threadId + "-type-[IJ]-name=-[N]-abc", "type", "name", "\nabc");
+    assertPrintResult("[IJ]-" + threadId + "-type-[IJ]-name=abc-[N]-", "type", "name", "abc\n");
+    assertPrintResult("[IJ]-" + threadId + "-type-[IJ]-name=abc-[N]-def", "type", "name", "abc\ndef");
+    assertPrintResult("[IJ]-" + threadId + "-type-[IJ]-name=-[N]-abc-[N]-", "type", "name", "\nabc\n");
+    assertPrintResult("[IJ]-" + threadId + "-type-[IJ]-name=1-[N]-2-[N]-3-[N]--[N]-4-[N]--[N]-5-[N]--[N]-6",
                       "type", "name", "1\n2\n\r3\n\n4\n\r\n5\n\n\r6");
   }
 
@@ -32,6 +34,5 @@ class EventInfoPrinterTest {
                                         Object type,
                                         CharSequence... objects) {
     assertEquals(expected, EventInfoPrinter.printToBuffer(type, objects).toString());
-
   }
 }
