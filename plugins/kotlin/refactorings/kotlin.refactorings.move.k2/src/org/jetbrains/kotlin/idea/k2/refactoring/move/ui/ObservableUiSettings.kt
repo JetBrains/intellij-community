dd9616d40ae7ad39059aa751d4d1c7a8a513eab0
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2025-11-05 16:44:29 +0200

    [kotlin] KTIJ-35016 Add doc comments for observable UI settings
    GitOrigin-RevId: e0ae915ae7f586681d865f969188a8519b7e52d4

--- plugins/kotlin/refactorings/kotlin.refactorings.move.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/move/ui/ObservableUiSettings.kt
+++ plugins/kotlin/refactorings/kotlin.refactorings.move.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/move/ui/ObservableUiSettings.kt
@@ -3,25 +3,77 @@
 package org.jetbrains.kotlin.idea.k2.refactoring.move.ui
 
 import com.intellij.openapi.observable.properties.ObservableProperty
+import org.jetbrains.annotations.ApiStatus
 
+/**
+ * Top-level interface for combined UI settings of the move refactoring.
+ *
+ * The purpose of the interface is to share state between different parts of the UI model via observable properties.
+ * This is necessary for tracking component changes between different parts of the UI model when presentation should be conditional.
+ *
+ * All sub-interface implementors must be registered before access to their properties.
+ * Normally a shared settings instance is passed to the model part constructor and used for registration.
+ */
+@ApiStatus.Internal
 interface ObservableUiSettings : K2MoveModelObservableSettings, K2SourceModelObservableSettings, K2TargetModelObservableSettings {
     fun registerK2MoveModelSettings(settings: K2MoveModelObservableSettings)
     fun registerK2SourceModelSettings(settings: K2SourceModelObservableSettings)
     fun registerK2TargetModelSettings(settings: K2TargetModelObservableSettings)
 }
 
+/**
+ * Move refactoring settings providing properties of the [K2MoveModel] useful for other parts of the UI model.
+ */
+@ApiStatus.Internal
 interface K2MoveModelObservableSettings {
+    /**
+     * State of the 'Search references' setting.
+     */
     val searchReferencesSettingObservable: ObservableProperty<Boolean>
+
+    /**
+     * State of the 'Search for text occurrences' setting.
+     */
     val searchForTextSettingObservable: ObservableProperty<Boolean>
+
+    /**
+     * State of the 'Search in comments and strings' setting.
+     */
     val searchInCommentsSettingObservable: ObservableProperty<Boolean>
+
+    /**
+     * State of the 'Move expect/actual counterparts' setting.
+     */
     val mppDeclarationsSettingObservable: ObservableProperty<Boolean>
 }
 
+/**
+ * Move refactoring settings providing properties of the [K2MoveSourceModel] useful for other parts of the UI model.
+ */
+@ApiStatus.Internal
 interface K2SourceModelObservableSettings {
+    /**
+     * Shows whether among the selected elements about to be moved there are 'expect' or 'actual' declarations.
+     */
     val mppDeclarationsSelectedObservable: ObservableProperty<Boolean>
 }
 
+/**
+ * Move refactoring settings providing properties of the [K2MoveTargetModel] useful for other parts of the UI model.
+ */
+@ApiStatus.Internal
 interface K2TargetModelObservableSettings {
+    /**
+     * Provides information about detected platform suffix in the currently selected target file.
+     * Platform suffixes are part of the naming convention in multiplatform projects.
+     * [kotlinlang documentation](https://kotlinlang.org/docs/coding-conventions.html#multiplatform-projects)
+     *
+     * The value is `null` for non-multiplatform projects
+     *
+     * The value is `null` if the selected file does not have a suffix matching the platform part of the source set name
+     *
+     * Example: the suffix of a file with the name `myFile.jvm.kt` is `jvm` in `jvm`/`jvmMain`/`jvmTest` source sets; `null` otherwise.
+     */
     val sourceSetSuffix: ObservableProperty<String?>
 }
 
