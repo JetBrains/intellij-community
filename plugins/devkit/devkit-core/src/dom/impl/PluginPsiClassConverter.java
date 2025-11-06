1696927b5917e2fe8ab8f6fc413ff2d198b1a4dc
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2025-11-06 17:40:35 +0100

    [devkit] IJPL-212813 Prioritize searching in the project scope over the all scope
    The change adjusts the super class' method behavior, preserving the default.
    
    GitOrigin-RevId: b6c4325a762c9ad53c6ece25157c5cf6c15a9990

--- plugins/devkit/devkit-core/src/dom/impl/PluginPsiClassConverter.java
+++ plugins/devkit/devkit-core/src/dom/impl/PluginPsiClassConverter.java
@@ -1,18 +1,45 @@
-// Copyright 2000-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.dom.impl;
 
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.IntelliJProjectUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.JavaClassReferenceProvider;
 import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.util.xml.ConvertContext;
-import com.intellij.util.xml.ExtendClass;
-import com.intellij.util.xml.GenericDomValue;
-import com.intellij.util.xml.PsiClassConverter;
+import com.intellij.psi.xml.XmlFile;
+import com.intellij.util.xml.*;
 import org.jetbrains.annotations.NotNull;
 
 public class PluginPsiClassConverter extends PsiClassConverter {
+
+  @Override
+  public PsiClass fromString(final String s, final @NotNull ConvertContext context) {
+    if (s == null) return null;
+    final String fqn = s.trim();
+    if (fqn.isEmpty()) return null;
+
+    final DomElement element = context.getInvocationElement();
+    final XmlFile file = context.getFile();
+    final Module module = context.getModule();
+    if (element instanceof GenericDomValue) {
+      final Project project = context.getProject();
+      // IJPL-212813
+      // try to find in project sources:
+      PsiClass cls = DomJavaUtil.findClass(fqn, file, module, GlobalSearchScope.projectScope(project));
+      if (cls != null) {
+        return cls;
+      }
+      // fallback to all, if not found:
+      cls = DomJavaUtil.findClass(fqn, file, module, GlobalSearchScope.allScope(project));
+      if (cls != null) {
+        return cls;
+      }
+    }
+    // default behavior from super class:
+    final GlobalSearchScope scope = element instanceof GenericDomValue ? getScope(context) : null;
+    return DomJavaUtil.findClass(fqn, file, module, scope);
+  }
 
   @Override
   protected GlobalSearchScope getScope(@NotNull ConvertContext context) {
