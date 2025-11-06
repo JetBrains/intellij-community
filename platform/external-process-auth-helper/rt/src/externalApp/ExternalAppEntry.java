c43ff906a2c44490f3157e9c1d3f5e0ce923606d
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2025-11-06 15:02:09 +0200

    [eel-vcs] IJPL-204860 cleanup after code review: make ExternalCli experimental
    GitOrigin-RevId: d2638bdc2364fae85caf274c2e1dc93c93b27367

--- platform/external-process-auth-helper/rt/src/externalApp/ExternalAppEntry.java
+++ platform/external-process-auth-helper/rt/src/externalApp/ExternalAppEntry.java
@@ -1,11 +1,14 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package externalApp;
 
+import org.jetbrains.annotations.ApiStatus;
+
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.nio.file.Path;
 import java.util.Map;
 
+@ApiStatus.Experimental
 public interface ExternalAppEntry {
   String[] getArgs();
   Map<String, String> getEnvironment();
