0393090db4ec62cedabc8a0dd6185db040014a27
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2025-11-06 17:11:55 +0100

    AT-3038 allow running `serialized test` with JpsEmptyProject
    GitOrigin-RevId: 85a74c4d8f19f04a473168f89c1d6e5f013450bf

--- tools/intellij.lambda.testFramework/src/com/intellij/lambda/testFramework/junit/UltimateTestCases.kt
+++ tools/intellij.lambda.testFramework/src/com/intellij/lambda/testFramework/junit/UltimateTestCases.kt
@@ -14,9 +14,4 @@ object UltimateTestCases : TestCaseTemplate(IdeProductProvider.IU) {
       projectDir = Paths.get(PathManager.getHomePath(), "out/ide-tests/cache/empty-project").createDirectories()
     )
   )
-  val JavaTestProject: TestCase<LocalProjectInfo> = withProject(
-    projectInfo = LocalProjectInfo(
-      projectDir = Paths.get(PathManager.getHomePath(), "out/ide-tests/cache/projects/unpacked/java-ui-automation-data").createDirectories()
-    )
-  )
 }
