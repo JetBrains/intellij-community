60958389ca87b8b52e298cea6ba0aface9dda006
Morgan Bartholomew <morgan.bartholomew@jetbrains.com>
2025-11-07 00:52:44 +1000

    PY-85418 Cannot select interpreter to use with `black`
    reverted some changes from lsp feature
    
    closes PY-85418
    
    GitOrigin-RevId: 55aafe58040f90ae4d9931f50732277d6891ffea

--- python/src/com/jetbrains/python/externaltools/configuration/PySdkChooser.kt
+++ python/src/com/jetbrains/python/externaltools/configuration/PySdkChooser.kt
@@ -2,13 +2,7 @@
 package com.jetbrains.python.externaltools.configuration
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.modules
 import com.intellij.openapi.projectRoots.Sdk
-import com.intellij.openapi.ui.ComboBox
-import com.intellij.openapi.util.io.FileUtil
-import com.intellij.ui.CollectionComboBoxModel
-import com.intellij.ui.ComboboxSpeedSearch
-import com.jetbrains.python.sdk.PySdkListCellRenderer
 import com.jetbrains.python.sdk.PythonSdkUtil
 import com.jetbrains.python.sdk.pythonSdk
 
@@ -16,27 +10,3 @@ const val DEFAULT_ENVIRONMENT: String = "Project Default"
 
 fun Project.sdkByNameDefaultAware(name: String): Sdk? =
   if (name == DEFAULT_ENVIRONMENT) pythonSdk else PythonSdkUtil.findSdkByKey(name)
-
-fun createPythonSdkComboBox(project: Project, initialSelection: Any?): ComboBox<Any> {
-  val sdks = buildList {
-    project.pythonSdk?.let { add(it) }
-    project.modules.mapNotNullTo(this) { it.pythonSdk }
-  }.distinct()
-  return ComboBox<Any>().apply {
-    model = CollectionComboBoxModel(
-      // we use a simple string here, ideally we should introduce some special class for this
-      (listOf(DEFAULT_ENVIRONMENT) + sdks).toMutableList(),
-      initialSelection,
-    )
-    renderer = PySdkListCellRenderer()
-    addActionListener { updateTooltip() }
-    ComboboxSpeedSearch.installOn(this)
-    updateTooltip()
-  }
-}
-
-private fun ComboBox<*>.updateTooltip() {
-  val item = selectedItem
-  val sdkHomePath = if (item is Sdk) item.homePath else null
-  setToolTipText(if (sdkHomePath != null) FileUtil.toSystemDependentName(sdkHomePath) else null)
-}
