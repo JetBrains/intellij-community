76bde6846f5e0d9bbce9ac0e7a6bfb3e4054d35b
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2025-11-07 16:41:03 +0100

    [bazel] BAZEL-2585 the actual file should be marked as trusted
    otherwise the root directory is used in the later stages when the actual project is instantiated
    
    IJ-CR-181912
    
    (cherry picked from commit e887d855b26d368fa460033377b1219791c263b8)
    
    GitOrigin-RevId: 8b45aff88b4b5c031f7bd365351e7f57b74d9809

--- platform/platform-impl/src/com/intellij/ide/trustedProjects/impl/DefaultTrustedProjectsLocator.kt
+++ platform/platform-impl/src/com/intellij/ide/trustedProjects/impl/DefaultTrustedProjectsLocator.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.trustedProjects.impl
 
 import com.intellij.ide.trustedProjects.TrustedProjectsLocator
@@ -11,7 +11,7 @@ private class DefaultTrustedProjectsLocator : TrustedProjectsLocator {
     if (project !is ProjectStoreOwner) {
       return emptyList()
     }
-    return listOf(project.componentStore.storeDescriptor.historicalProjectBasePath)
+    return listOf(project.componentStore.storeDescriptor.projectIdentityFile)
   }
 
   override fun getProjectRoots(projectRoot: Path, project: Project?): List<Path> = listOf(projectRoot)
