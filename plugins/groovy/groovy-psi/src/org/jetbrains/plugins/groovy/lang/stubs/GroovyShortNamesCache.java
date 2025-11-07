ed01243c557133e35e1bd1cfbc91004b9110a631
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2025-11-07 22:33:55 +0100

    refactor [groovy]: Remove unnecessary qualifier
    GitOrigin-RevId: 32f35464fceb6a011a734a62b6bce96df827ab2e

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/stubs/GroovyShortNamesCache.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/stubs/GroovyShortNamesCache.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.groovy.lang.stubs;
 
 import com.intellij.openapi.application.ReadAction;
@@ -50,7 +50,7 @@ public final class GroovyShortNamesCache extends PsiShortNamesCache {
 
   public static GroovyShortNamesCache getGroovyShortNamesCache(Project project) {
     return Objects
-      .requireNonNull(ContainerUtil.findInstance(PsiShortNamesCache.EP_NAME.getExtensionList(project), GroovyShortNamesCache.class));
+      .requireNonNull(ContainerUtil.findInstance(EP_NAME.getExtensionList(project), GroovyShortNamesCache.class));
   }
 
   private @Nullable TopLevelFQNames getTopLevelNames() {
