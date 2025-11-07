ac948d7fc1922b8f9aebc2cc4362254082497207
Valeria Golovina <valeria.golovina@jetbrains.com>
2025-11-07 16:12:04 +0200

    [gitlab] IJPL-82619: added "upload file" context action to a text field of a review comment
    * registry key defaulted to true
    
    GitOrigin-RevId: 1c4ae8b724999ea4403407e195943d2bda872bbe

--- plugins/gitlab/gitlab-core/src/org/jetbrains/plugins/gitlab/ui/comment/GitLabCodeReviewCommentTextFieldFactory.kt
+++ plugins/gitlab/gitlab-core/src/org/jetbrains/plugins/gitlab/ui/comment/GitLabCodeReviewCommentTextFieldFactory.kt
@@ -8,19 +8,51 @@ import com.intellij.collaboration.ui.codereview.comment.CodeReviewTextEditingVie
 import com.intellij.collaboration.ui.codereview.comment.CommentInputActionsComponentFactory
 import com.intellij.collaboration.ui.codereview.comment.createEditActionsConfig
 import com.intellij.collaboration.ui.codereview.timeline.comment.CommentTextFieldFactory
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.event.EditorMouseEvent
+import com.intellij.openapi.editor.ex.EditorEx
+import com.intellij.openapi.editor.impl.ContextMenuPopupHandler
+import com.intellij.openapi.util.registry.Registry
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import org.jetbrains.plugins.gitlab.util.GitLabBundle
 import javax.swing.JComponent
 
 
 object GitLabCodeReviewCommentTextFieldFactory {
+
   fun createIn(
     cs: CoroutineScope,
     vm: CodeReviewSubmittableTextViewModel,
     actions: CommentInputActionsComponentFactory.Config,
     icon: CommentTextFieldFactory.IconConfig? = null,
   ): JComponent {
-    return CodeReviewCommentTextFieldFactory.createIn(cs, vm, actions, icon)
+
+    val uploadFileAction = object : AnAction(GitLabBundle.message("action.GitLab.Review.Upload.File.text"),
+                                             GitLabBundle.message("action.GitLab.Review.Upload.File.description"),
+                                             AllIcons.Actions.Upload) {
+      override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+      }
+
+      override fun update(e: AnActionEvent) {
+        e.presentation.isEnabledAndVisible = Registry.`is`("gitlab.merge.requests.file.upload.enabled")
+      }
+
+      override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+    }
+
+    return CodeReviewCommentTextFieldFactory.createIn(cs, vm, actions, icon) { editor: Editor ->
+
+      (editor as EditorEx).installPopupHandler(object : ContextMenuPopupHandler() {
+        override fun getActionGroup(event: EditorMouseEvent): ActionGroup {
+          return DefaultActionGroup(ActionManager.getInstance().getAction(IdeActions.GROUP_BASIC_EDITOR_POPUP),
+                                    uploadFileAction)
+        }
+      })
+    }
   }
 }
 
