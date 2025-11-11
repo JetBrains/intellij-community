c7e8aeac4f7f562754c3307b22d16608c46a1e65
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-11-11 15:41:56 +0100

    IJPL-217839 convert ide-concurrency.xml to product module
    GitOrigin-RevId: 67bca1707505994650fc9ad63ba845c47a048122

---
+++ platform/build-scripts/src/org/jetbrains/intellij/build/productLayout/CommunityProductFragments.kt
@@ -0,0 +1,24 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.intellij.build.productLayout
+
+/**
+ * Registry of reusable product content fragments for community products.
+ * These fragments bundle commonly repeated patterns into reusable units.
+ */
+object CommunityProductFragments {
+  /**
+   * Platform Lang base fragment: includes PlatformLangPlugin.xml and its transitively required modules.
+   * 
+   * PlatformLangPlugin.xml provides core platform language support including:
+   * - Platform language components and extensions
+   * - Core actions, refactoring, and editor support
+   * - Concurrency infrastructure (intellij.platform.ide.concurrency)
+   * 
+   * Use this fragment instead of manually calling deprecatedInclude("intellij.platform.resources", "META-INF/PlatformLangPlugin.xml")
+   * to ensure all required modules are properly included.
+   */
+  fun platformLangBaseFragment(): ProductModulesContentSpec = productModules {
+    deprecatedInclude("intellij.platform.resources", "META-INF/PlatformLangPlugin.xml")
+    embeddedModule("intellij.platform.ide.concurrency")
+  }
+}
