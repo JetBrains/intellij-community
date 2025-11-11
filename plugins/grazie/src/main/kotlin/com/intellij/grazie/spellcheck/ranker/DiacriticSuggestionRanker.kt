3650ee92ea06065427710c59bd8a09f11c4dfb60
Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
2025-11-11 13:10:36 +0200

    cleanup [grazie]: Use platform API and remove duplicated code
    GitOrigin-RevId: 09695557b808c651f56bb9115e76f049ed110890

--- plugins/grazie/src/main/kotlin/com/intellij/grazie/spellcheck/ranker/DiacriticSuggestionRanker.kt
+++ plugins/grazie/src/main/kotlin/com/intellij/grazie/spellcheck/ranker/DiacriticSuggestionRanker.kt
@@ -1,22 +1,18 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.grazie.spellcheck.ranker
 
+import ai.grazie.spell.suggestion.ranker.AsciiRanker
 import ai.grazie.spell.suggestion.ranker.SuggestionRanker
 import ai.grazie.utils.LinkedSet
-import com.intellij.grazie.spellcheck.diacritic.Diacritics
 
 class DiacriticSuggestionRanker(
   private val fallbackSuggestionRanker: SuggestionRanker,
 ) : SuggestionRanker {
   override fun score(word: String, suggestions: LinkedSet<String>): Map<String, Double> {
-    val weights = suggestions.associateWith { score(word, it) }
+    val weights = AsciiRanker().score(word, suggestions)
     if (weights.filter { it.value > 0 }.isEmpty()) {
       return fallbackSuggestionRanker.score(word, suggestions)
     }
     return weights
-  }
-  
-  private fun score(word: String, suggestion: String): Double {
-    return if (Diacritics.equalsIgnoringDiacritics(word, suggestion)) 1.0 else 0.0
   }
 }
