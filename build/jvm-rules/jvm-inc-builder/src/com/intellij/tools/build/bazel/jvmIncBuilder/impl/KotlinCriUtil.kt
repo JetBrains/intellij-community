e8678632d2aeed32698bc4cb3177743dc3f6bc7e
Aleksei.Cherepanov <aleksei.cherepanov@jetbrains.com>
2025-11-11 17:04:24 +0100

    KTIJ-36250 [Bazel] Distribute Kotlin CRI data as a single file instead of a folder
    Since Kotlin CRI data is decided to be as default output, it is more convenient to provide it as a single serialized file instead of folder with three artifacts
    
    GitOrigin-RevId: 1b5468c4070b017b75dd5a1adfd53e648b7b7e86

--- build/jvm-rules/jvm-inc-builder/src/com/intellij/tools/build/bazel/jvmIncBuilder/impl/KotlinCriUtil.kt
+++ build/jvm-rules/jvm-inc-builder/src/com/intellij/tools/build/bazel/jvmIncBuilder/impl/KotlinCriUtil.kt
@@ -11,66 +11,46 @@ import org.jetbrains.jps.dependency.java.JvmNodeReferenceID
 import org.jetbrains.jps.dependency.java.SubclassesIndex
 import org.jetbrains.jps.dependency.kotlin.LookupsIndex
 
-const val VERSION = 1
+const val VERSION = 2
 
-internal fun prepareSerializedData(graph: DependencyGraph): Map<String, ByteArray> {
-  val serializedDataResult = mutableMapOf<String, ByteArray>()
+internal fun prepareSerializedData(graph: DependencyGraph): ByteArray {
+  val serializedDataResult = ByteArrayOutputStream().use { byteStream ->
+    DataOutputStream(byteStream).use { dataOut ->
+      dataOut.writeVersion()
 
-  // process subtypes
-  val subclassesIndex: BackDependencyIndex? = graph.getIndex(SubclassesIndex.NAME)
-  val subclassesCount = subclassesIndex?.keys?.count() ?: 0
-  if (subclassesCount > 0) {
-    ByteArrayOutputStream().use { byteStream ->
-      DataOutputStream(byteStream).use { dataOut ->
-        dataOut.writeVersion()
-        dataOut.writeInt(subclassesCount)
-        subclassesIndex?.keys?.forEach { key ->
-          val dependencies = subclassesIndex.getDependencies(key).map { it.toFqName() }
-          dataOut.writeUTF(key.toFqName())
-          dataOut.writeInt(dependencies.size)
-          dependencies.forEach { dataOut.writeUTF(it) }
-        }
+      // process subtypes
+      val subclassesIndex: BackDependencyIndex? = graph.getIndex(SubclassesIndex.NAME)
+      val subclassesCount = subclassesIndex?.keys?.count() ?: 0
+      dataOut.writeInt(subclassesCount)
+      subclassesIndex?.keys?.forEach { key ->
+        val dependencies = subclassesIndex.getDependencies(key).map { it.toFqName() }
+        dataOut.writeUTF(key.toFqName())
+        dataOut.writeInt(dependencies.size)
+        dependencies.forEach { dataOut.writeUTF(it) }
       }
-      byteStream.toByteArray()
-    }.also { serializedDataResult["subtypes"] = it }
-  }
 
-  // process lookups
-  val lookupsIndex: BackDependencyIndex? = graph.getIndex(LookupsIndex.NAME)
-  val lookupsCount = lookupsIndex?.keys?.count() ?: 0
-  val lookupsAccumulator = mutableMapOf<String, Int>()
-  if (lookupsCount > 0) {
-    ByteArrayOutputStream().use { byteStream ->
-      DataOutputStream(byteStream).use { dataOut ->
-        dataOut.writeVersion()
-        dataOut.writeInt(lookupsCount)
-        lookupsIndex?.keys?.forEach { key ->
-          val dependencies = lookupsIndex.getDependencies(key)
-            .mapNotNull { (it as? JvmNodeReferenceID)?.nodeName }
-          dataOut.writeUTF(key.toFqName())
-          dataOut.writeInt(dependencies.count())
-          dependencies.forEach {
-            dataOut.writeInt(it.addFilePathIfNeeded(lookupsAccumulator)) }
-        }
+      // process lookups
+      val lookupsIndex: BackDependencyIndex? = graph.getIndex(LookupsIndex.NAME)
+      val lookupsCount = lookupsIndex?.keys?.count() ?: 0
+      val lookupsAccumulator = mutableMapOf<String, Int>()
+      dataOut.writeInt(lookupsCount)
+      lookupsIndex?.keys?.forEach { key ->
+        val dependencies = lookupsIndex.getDependencies(key)
+          .mapNotNull { (it as? JvmNodeReferenceID)?.nodeName }
+        dataOut.writeUTF(key.toFqName())
+        dataOut.writeInt(dependencies.count())
+        dependencies.forEach {
+          dataOut.writeInt(it.addFilePathIfNeeded(lookupsAccumulator)) }
       }
-      byteStream.toByteArray()
-    }.also { serializedDataResult["lookups"] = it }
 
-    if (lookupsAccumulator.isNotEmpty()) {
-      ByteArrayOutputStream().use { byteStream ->
-        DataOutputStream(byteStream).use { dataOut ->
-          dataOut.writeVersion()
-          dataOut.writeInt(lookupsAccumulator.size)
-          lookupsAccumulator.forEach { (path, fileId) ->
-            dataOut.writeInt(fileId)
-            dataOut.writeUTF(path)
-          }
-        }
-        byteStream.toByteArray()
-      }.also { serializedDataResult["fileIdToPath"] = it }
+      dataOut.writeInt(lookupsAccumulator.size)
+      lookupsAccumulator.forEach { (path, fileId) ->
+        dataOut.writeInt(fileId)
+        dataOut.writeUTF(path)
+      }
     }
+    byteStream.toByteArray()
   }
-
   return serializedDataResult
 }
 
