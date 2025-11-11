3fca07247e38bf25e337a1299e1afc801398eccc
Aleksey Pivovarov <aleksey.pivovarov@jetbrains.com>
2025-11-11 12:42:11 +0100

    tests [rdct]: introduce tests on ClientSessionImpl context
    (cherry picked from commit eb5e17c1687811f53cff6646bc03133338661b1c)
    
    Space-RevId: 8fe332bedfa9c7c1640409d8fef0b8156fefc3c4
    
    (cherry picked from commit 680f28f34cda1901a037e2a30c1a09a1100e08c6)
    
    IJ-MR-188045
    
    GitOrigin-RevId: 97ada7bd08596730313301fc02057ad51f2cbfa9

--- platform/platform-tests/testSrc/com/intellij/serviceContainer/ServiceContainerTest.kt
+++ platform/platform-tests/testSrc/com/intellij/serviceContainer/ServiceContainerTest.kt
@@ -1,4 +1,6 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:Suppress("SSBasedInspection")
+
 package com.intellij.serviceContainer
 
 import com.intellij.concurrency.currentThreadContext
@@ -7,8 +9,11 @@ import com.intellij.openapi.components.ComponentManager
 import com.intellij.openapi.extensions.DefaultPluginDescriptor
 import com.intellij.openapi.util.Disposer
 import com.intellij.testFramework.LoggedErrorProcessor
+import io.kotest.common.runBlocking
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.withContext
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Ignore
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertInstanceOf
 import org.junit.jupiter.api.assertNotNull
@@ -18,6 +23,7 @@ import kotlin.coroutines.AbstractCoroutineContextElement
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.test.assertContains
+import kotlin.test.assertTrue
 
 private val testPluginDescriptor: DefaultPluginDescriptor = DefaultPluginDescriptor("test")
 
@@ -65,24 +71,204 @@ class ServiceContainerTest {
   }
 
   @Test
-  fun `initializer overwrites caller context`() {
-    @Suppress("SSBasedInspection") val componentManager = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_1)))
-    @Suppress("SSBasedInspection") val componentManager2 = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_2)))
+  fun `initializer overwrites caller context - load 1`() {
+    val componentManager = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_1)))
+    val componentManager2 = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_2)))
     componentManager.registerService(T1::class.java, T1::class.java, testPluginDescriptor, false)
     componentManager2.registerService(T2::class.java, T2::class.java, testPluginDescriptor, false)
-    publishComponentManager(componentManager2) {
+    publishComponentManager2(componentManager2) {
       componentManager.getService(T1::class.java)
     }
   }
 
   @Test
+  fun `initializer overwrites caller context - load 2`() {
+    val componentManager = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_1)))
+    val componentManager2 = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_3)),
+                                                 additionalContext = ContextElement(MARKER_2))
+    componentManager.registerService(T1::class.java, T1::class.java, testPluginDescriptor, false)
+    componentManager2.registerService(T2::class.java, T2::class.java, testPluginDescriptor, false)
+    publishComponentManager1(componentManager) {
+      publishComponentManager2(componentManager2) {
+        componentManager.getService(T1::class.java)
+      }
+    }
+  }
+
+  @Test
+  fun `initializer overwrites caller context - load 3`() {
+    val componentManager = TestComponentManager(additionalContext = ContextElement(MARKER_1))
+    val componentManager2 = TestComponentManager(parentScope = CoroutineScope(EmptyCoroutineContext),
+                                                 additionalContext = ContextElement(MARKER_2))
+    componentManager.registerService(T1::class.java, T1::class.java, testPluginDescriptor, false)
+    componentManager2.registerService(T2::class.java, T2::class.java, testPluginDescriptor, false)
+    publishComponentManager1(componentManager) {
+      publishComponentManager2(componentManager2) {
+        runBlocking {
+          withContext(ContextElement(MARKER_3)) {
+            componentManager.getService(T1::class.java)
+          }
+        }
+      }
+    }
+  }
+
+  @Test
+  fun `initializer overwrites caller context - load 4`() {
+    val componentManager = TestComponentManager(additionalContext = ContextElement(MARKER_1))
+    val componentManager2 = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_3)),
+                                                 additionalContext = ContextElement(MARKER_2))
+    componentManager.registerService(T1::class.java, T1::class.java, testPluginDescriptor, false)
+    componentManager2.registerService(T2::class.java, T2::class.java, testPluginDescriptor, false)
+    publishComponentManager1(componentManager) {
+      publishComponentManager2(componentManager2) {
+        runBlocking {
+          withContext(ContextElement(MARKER_3)) {
+            componentManager.getService(T1::class.java)
+          }
+        }
+      }
+    }
+  }
+
+  @Test
+  fun `initializer overwrites caller context - load 5`() {
+    val componentManager = TestComponentManager(additionalContext = ContextElement(MARKER_1))
+    val componentManager2 = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_3)),
+                                                 additionalContext = ContextElement(MARKER_2))
+    componentManager2.registerService(T0::class.java, T0::class.java, testPluginDescriptor, false)
+    componentManager.registerService(T1::class.java, T1::class.java, testPluginDescriptor, false)
+    componentManager2.registerService(T2::class.java, T2::class.java, testPluginDescriptor, false)
+    publishComponentManager1(componentManager) {
+      publishComponentManager2(componentManager2) {
+        runBlocking {
+          withContext(ContextElement(MARKER_3)) {
+            componentManager.getService(T0::class.java)
+          }
+        }
+      }
+    }
+  }
+
+  @Test
+  fun `initializer overwrites caller context - preload 1`() {
+    val componentManager = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_1)))
+    val componentManager2 = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_2)))
+    componentManager.registerService(T1::class.java, T1::class.java, testPluginDescriptor, false)
+    componentManager2.registerService(T2::class.java, T2::class.java, testPluginDescriptor, false)
+    publishComponentManager2(componentManager2) {
+      runBlocking {
+        componentManager.preload(T1::class.java)
+      }
+    }
+  }
+
+  @Test
+  fun `initializer overwrites caller context - preload 2`() {
+    val componentManager = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_1)))
+    val componentManager2 = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_3)),
+                                                 additionalContext = ContextElement(MARKER_2))
+    componentManager.registerService(T1::class.java, T1::class.java, testPluginDescriptor, false)
+    componentManager2.registerService(T2::class.java, T2::class.java, testPluginDescriptor, false)
+    publishComponentManager1(componentManager) {
+      publishComponentManager2(componentManager2) {
+        runBlocking {
+          componentManager.preload(T1::class.java)
+        }
+      }
+    }
+  }
+
+  @Test
+  @Ignore("Broken: demonstration of an issue")
+  fun `initializer overwrites caller context - preload 3`() {
+    val componentManager = TestComponentManager(additionalContext = ContextElement(MARKER_1))
+    val componentManager2 = TestComponentManager(parentScope = CoroutineScope(EmptyCoroutineContext),
+                                                 additionalContext = ContextElement(MARKER_2))
+    componentManager.registerService(T1::class.java, T1::class.java, testPluginDescriptor, false)
+    componentManager2.registerService(T2::class.java, T2::class.java, testPluginDescriptor, false)
+    publishComponentManager1(componentManager) {
+      publishComponentManager2(componentManager2) {
+        runBlocking {
+          withContext(ContextElement(MARKER_3)) {
+            componentManager.preload(T1::class.java)
+          }
+        }
+      }
+    }
+  }
+
+  @Test
+  @Ignore("Broken: demonstration of an issue")
+  fun `initializer overwrites caller context - preload 4`() {
+    val componentManager = TestComponentManager(additionalContext = ContextElement(MARKER_1))
+    val componentManager2 = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_3)),
+                                                 additionalContext = ContextElement(MARKER_2))
+    componentManager.registerService(T1::class.java, T1::class.java, testPluginDescriptor, false)
+    componentManager2.registerService(T2::class.java, T2::class.java, testPluginDescriptor, false)
+    publishComponentManager1(componentManager) {
+      publishComponentManager2(componentManager2) {
+        runBlocking {
+          withContext(ContextElement(MARKER_3)) {
+            componentManager.preload(T1::class.java)
+          }
+        }
+      }
+    }
+  }
+
+  @Test
+  @Ignore("Broken: demonstration of an issue")
+  fun `initializer overwrites caller context - preload 5`() {
+    val componentManager = TestComponentManager(additionalContext = ContextElement(MARKER_1))
+    val componentManager2 = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_3)),
+                                                 additionalContext = ContextElement(MARKER_2))
+    componentManager2.registerService(T0::class.java, T0::class.java, testPluginDescriptor, false)
+    componentManager.registerService(T1::class.java, T1::class.java, testPluginDescriptor, false)
+    componentManager2.registerService(T2::class.java, T2::class.java, testPluginDescriptor, false)
+    publishComponentManager1(componentManager) {
+      publishComponentManager2(componentManager2) {
+        runBlocking {
+          withContext(ContextElement(MARKER_3)) {
+            componentManager2.preload(T0::class.java)
+          }
+        }
+      }
+    }
+  }
+
+  @Test
   fun `initializer is isolated from the caller`() {
-    @Suppress("SSBasedInspection") val componentManager = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_1)))
-    @Suppress("SSBasedInspection") val componentManager2 = TestComponentManager(parentScope = CoroutineScope(EmptyCoroutineContext))
+    val componentManager = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_1)))
+    val componentManager2 = TestComponentManager(parentScope = CoroutineScope(EmptyCoroutineContext))
     componentManager.registerService(R1::class.java, R1::class.java, testPluginDescriptor, false)
     componentManager2.registerService(R2::class.java, R2::class.java, testPluginDescriptor, false)
-    publishComponentManager(componentManager2) {
+    publishComponentManager2(componentManager2) {
       componentManager.getService(R1::class.java)
+    }
+  }
+
+  @Test
+  fun `additional context has priority - load`() {
+    val componentManager = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_0)), additionalContext = ContextElement(MARKER_1))
+    val componentManager2 = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_0)), additionalContext = ContextElement(MARKER_2))
+    componentManager.registerService(T1::class.java, T1::class.java, testPluginDescriptor, false)
+    componentManager2.registerService(T2::class.java, T2::class.java, testPluginDescriptor, false)
+    publishComponentManager2(componentManager2) {
+      componentManager.getService(T1::class.java)
+    }
+  }
+
+  @Test
+  fun `additional context has priority - preload`() {
+    val componentManager = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_0)), additionalContext = ContextElement(MARKER_1))
+    val componentManager2 = TestComponentManager(parentScope = CoroutineScope(ContextElement(MARKER_0)), additionalContext = ContextElement(MARKER_2))
+    componentManager.registerService(T1::class.java, T1::class.java, testPluginDescriptor, false)
+    componentManager2.registerService(T2::class.java, T2::class.java, testPluginDescriptor, false)
+    publishComponentManager2(componentManager2) {
+      runBlocking {
+        componentManager.preload(T1::class.java)
+      }
     }
   }
 
@@ -141,15 +327,26 @@ class ServiceContainerTest {
     assertNotNull(service)
   }
 
-  private fun publishComponentManager(componentManager: ComponentManager, action: () -> Unit): Unit {
-    componentManagerHolder.set(componentManager)
+  private fun publishComponentManager1(componentManager: ComponentManager, action: () -> Unit): Unit {
+    componentManagerHolder1.set(componentManager)
     try {
       action()
     }
     finally {
-      componentManagerHolder.set(null)
+      componentManagerHolder1.set(null)
     }
   }
+
+  private fun publishComponentManager2(componentManager: ComponentManager, action: () -> Unit): Unit {
+    componentManagerHolder2.set(componentManager)
+    try {
+      action()
+    }
+    finally {
+      componentManagerHolder2.set(null)
+    }
+  }
+
 }
 
 private class SimpleService
@@ -168,14 +365,22 @@ private class C2(componentManager: ComponentManager) {
 
 private data class ServiceAsDataClass(var a: String? = null, var b: String? = null)
 
-private val componentManagerHolder: AtomicReference<ComponentManager> = AtomicReference(null)
+private val componentManagerHolder1: AtomicReference<ComponentManager> = AtomicReference(null)
+private val componentManagerHolder2: AtomicReference<ComponentManager> = AtomicReference(null)
 
+
+private class T0 {
+  init {
+    assertThat(currentThreadContext()[ContextElement.Key]).isNotNull().extracting("marker").isEqualTo(MARKER_2)
+    componentManagerHolder1.get()!!.getService(T1::class.java)!!
+  }
+}
 
 private class T1 {
   init {
     assertThat(currentThreadContext()[ContextElement.Key]).isNotNull().extracting("marker").isEqualTo(MARKER_1)
     // imitation of a globally-available `application`
-    componentManagerHolder.get()!!.getService(T2::class.java)
+    componentManagerHolder2.get()!!.getService(T2::class.java)!!
   }
 }
 
@@ -189,7 +394,7 @@ private class R1 {
   init {
     assertThat(currentThreadContext()[ContextElement.Key]).isNotNull().extracting("marker").isEqualTo(MARKER_1)
     // imitation of a globally-available `application`
-    componentManagerHolder.get()!!.getService(R2::class.java)
+    componentManagerHolder2.get()!!.getService(R2::class.java)!!
   }
 }
 
@@ -200,8 +405,21 @@ private class R2 {
 }
 
 private class ContextElement(val marker: String) : AbstractCoroutineContextElement(Key) {
-  object Key : CoroutineContext.Key<ContextElement>
+  override fun toString(): String {
+    return "ServiceContainerTest: ContextElement(marker='$marker')"
+  }
+
+  override val key: CoroutineContext.Key<*> get() = Key
+
+  object Key : CoroutineContext.Key<ContextElement> {
+    override fun toString(): String {
+      return "ServiceContainerTest: " + super.toString()
+    }
+  }
+
 }
 
+private const val MARKER_0 = "marker 0"
 private const val MARKER_1 = "marker 1"
 private const val MARKER_2 = "marker 2"
+private const val MARKER_3 = "marker 3"
--- platform/service-container/testSrc/com/intellij/serviceContainer/TestComponentManager.kt
+++ platform/service-container/testSrc/com/intellij/serviceContainer/TestComponentManager.kt
@@ -4,11 +4,13 @@ package com.intellij.serviceContainer
 import com.intellij.configurationStore.NonPersistentStore
 import com.intellij.ide.plugins.IdeaPluginDescriptorImpl
 import com.intellij.openapi.application.Application
+import com.intellij.openapi.components.ServiceDescriptor
 import com.intellij.openapi.components.impl.stores.IComponentStore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import org.jetbrains.annotations.TestOnly
+import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
 @OptIn(DelicateCoroutinesApi::class)
@@ -16,11 +18,24 @@ import kotlin.coroutines.EmptyCoroutineContext
 class TestComponentManager(
   override var isGetComponentAdapterOfTypeCheckEnabled: Boolean = true,
   @JvmField val parentScope: CoroutineScope = GlobalScope,
-) : ComponentManagerImpl(parent = null, parentScope = parentScope, additionalContext = EmptyCoroutineContext) {
+  additionalContext: CoroutineContext = EmptyCoroutineContext,
+) : ComponentManagerImpl(parent = null, parentScope = parentScope, additionalContext = additionalContext) {
   override val componentStore: IComponentStore
     get() = NonPersistentStore
 
   override fun getContainerDescriptor(pluginDescriptor: IdeaPluginDescriptorImpl) = pluginDescriptor.appContainerDescriptor
 
   override fun getApplication(): Application? = null
+
+  suspend fun preload(clazz: Class<*>) {
+    val originalService = getServiceIfCreated(clazz)
+    assert(originalService === null) { "Service should not be created before preload: $originalService" }
+
+    // empty descriptor, it is not used
+    val mockDescriptor = ServiceDescriptor(null, null, null, null, false, null, ServiceDescriptor.PreloadMode.TRUE, null, null)
+    preloadService(mockDescriptor, clazz.name)
+
+    val preloadedService = getServiceIfCreated(clazz)
+    assert(preloadedService !== null) { "Service was not preloaded: $clazz" }
+  }
 }
