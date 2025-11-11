d18c7b9992852b39be9bf17c791bbc435d3f7ea4
Liubov Afanaseva <Liubov.Afanaseva@jetbrains.com>
2025-11-11 11:14:53 +0100

    DO-1529 fixed race condition in buildEventScheme generation
    Descriptions in lifecycle group aren't generated because this group is initialized before resetDescriptions call.
    
    GitOrigin-RevId: c276320f4eb2f7f5e578e081020aba2a76bdf022

--- platform/statistics/src/com/intellij/internal/statistic/eventLog/events/scheme/RegisteredLogDescriptionsProcessor.kt
+++ platform/statistics/src/com/intellij/internal/statistic/eventLog/events/scheme/RegisteredLogDescriptionsProcessor.kt
@@ -3,12 +3,14 @@ package com.intellij.internal.statistic.eventLog.events.scheme
 
 import org.jetbrains.annotations.ApiStatus
 
+const val FUS_DESCRIPTION_REGISTRATION_ENABLED: String = "FUS_DESCRIPTION_REGISTRATION_ENABLED"
+
 /**
  * Internal processor for managing to store descriptions of event log groups and their events.
  * Provides functionality to store, retrieve descriptions for groups and their corresponding events.
  *
- * Descriptions are registered and stored in the memory if [isRegistered] is true.
- * Please use it just for [com.intellij.internal.statistic.eventLog.events.scheme.EventsSchemeBuilderAppStarter] and tests.
+ * Descriptions are registered and stored in the memory if the environment variable [FUS_DESCRIPTION_REGISTRATION_ENABLED] is true.
+ * Please use it just for [EventsSchemeBuilderAppStarter] and tests.
  *
  * It ensures that event and group descriptions are immutable once registered.
  * If an attempt is made to override an already registered description with a differing value,
@@ -23,25 +25,31 @@ import org.jetbrains.annotations.ApiStatus
 object RegisteredLogDescriptionsProcessor {
   private val groupDescriptionsMap = HashMap<String, String>()
   private val eventDescriptionsMap = HashMap<String, HashMap<String, String>>()
-  private var isRegistered = false
+  private var descriptionRegistrationEnabled = System.getenv(FUS_DESCRIPTION_REGISTRATION_ENABLED)?.toBoolean() ?: false
 
   /**
-   * Resets the state of the current instance by clearing all relevant data and updating registration status.
-   *
-   * @param isRegistered A boolean indicating the new registration status to set.
+   * Resets the descriptions of the current instance by clearing all relevant data.
    */
-  fun reset(isRegistered: Boolean) {
-    this.isRegistered = isRegistered
+  fun resetDescriptions() {
     groupDescriptionsMap.clear()
     eventDescriptionsMap.clear()
   }
 
   /**
-   * Register a description for a specified group if [isRegistered] is true, the description is not empty and not null.
+   * Enables/disables description registration.
+   *
+   * @param descriptionRegistrationEnabled A boolean indicating the new registration status to set.
+   */
+  fun configureDescriptionRegistration(isEnabled: Boolean) {
+    this.descriptionRegistrationEnabled = isEnabled
+  }
+
+  /**
+   * Register a description for a specified group if [descriptionRegistrationEnabled] is true, the description is not empty and not null.
    * Throws an exception if the description for the group already exists and differs from the new one.
    */
   fun registerGroupDescription(groupId: String, description: String?) {
-    if (!isRegistered || description == null || description.isEmpty()) return
+    if (!descriptionRegistrationEnabled || description == null || description.isEmpty()) return
 
     if (groupDescriptionsMap.containsKey(groupId) && groupDescriptionsMap[groupId] != description) {
       throw IllegalStateException("Trying to override registered event log group description in group '$groupId'. " +
@@ -51,11 +59,11 @@ object RegisteredLogDescriptionsProcessor {
   }
 
   /**
-   * Register a description for an event within a specific group if [isRegistered] is true, the description is not empty and not null.
+   * Register a description for an event within a specific group if [descriptionRegistrationEnabled] is true, the description is not empty and not null.
    * Throws an exception if the description for the event already exists and differs from the new one.
    */
   fun registerEventDescription(groupId: String, eventId: String, description: String?) {
-    if (!isRegistered || description == null || description.isEmpty()) return
+    if (!descriptionRegistrationEnabled || description == null || description.isEmpty()) return
 
     val groupMap = eventDescriptionsMap[groupId]
     if (groupMap == null) {
@@ -75,18 +83,18 @@ object RegisteredLogDescriptionsProcessor {
   }
 
   /**
-   * Retrieves the description for a specified group or returns null if [isRegistered] is false or the group description is not registered.
+   * Retrieves the description for a specified group or returns null if [descriptionRegistrationEnabled] is false or the group description is not registered.
    */
   fun calculateGroupDescription(groupId: String): String? {
-    if (!isRegistered) return null
+    if (!descriptionRegistrationEnabled) return null
     return groupDescriptionsMap[groupId]
   }
 
   /**
-   * Retrieves the description for a specific event in the group or returns null if [isRegistered] is false or the events description is not registered.
+   * Retrieves the description for a specific event in the group or returns null if [descriptionRegistrationEnabled] is false or the events description is not registered.
    */
   fun calculateEventDescription(groupId: String, eventId: String): String? {
-    if (!isRegistered) return null
+    if (!descriptionRegistrationEnabled) return null
     val groupMap = eventDescriptionsMap[groupId]
     return groupMap?.get(eventId)
   }
