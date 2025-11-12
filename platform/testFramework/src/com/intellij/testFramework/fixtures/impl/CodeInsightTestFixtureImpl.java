70f8d732c3c4d21d32eff1c27c8216b75908be65
Henry Wylde <henry.wylde@jetbrains.com>
2025-11-13 10:56:22 +1300

    Revert "RUBY-34869 wrap `CodeInsightTestFixtureImpl` call to `createUsageView` with `invokeAndWait`"
    This reverts commit 29bef00dd220e26c41fd143e378445c64979abbe.
    
    GitOrigin-RevId: db540361c9d0cc5d9a8218ced7518a0265d4d2d0

--- platform/testFramework/src/com/intellij/testFramework/fixtures/impl/CodeInsightTestFixtureImpl.java
+++ platform/testFramework/src/com/intellij/testFramework/fixtures/impl/CodeInsightTestFixtureImpl.java
@@ -1089,16 +1089,10 @@ public class CodeInsightTestFixtureImpl extends BaseFixture implements CodeInsig
   @Override
   public @NotNull String getUsageViewTreeTextRepresentation(@NotNull @Unmodifiable List<? extends UsageTarget> usageTargets,
                                                             @NotNull Collection<? extends Usage> usages) {
-    final Ref<UsageViewImpl> usageViewRef = new Ref<>();
-    ApplicationManager.getApplication().invokeAndWait(() -> {
-      UsageView usageView = UsageViewManager.getInstance(getProject())
-        .createUsageView(usageTargets.toArray(UsageTarget.EMPTY_ARRAY), usages.toArray(Usage.EMPTY_ARRAY), new UsageViewPresentation(),
-                         null);
+    UsageViewImpl usageView = (UsageViewImpl)UsageViewManager.getInstance(getProject())
+      .createUsageView(usageTargets.toArray(UsageTarget.EMPTY_ARRAY), usages.toArray(Usage.EMPTY_ARRAY), new UsageViewPresentation(), null);
 
-      usageViewRef.set((UsageViewImpl)usageView);
-    });
-
-    return getUsageViewTreeTextRepresentation(usageViewRef.get());
+    return getUsageViewTreeTextRepresentation(usageView);
   }
 
   @Override
