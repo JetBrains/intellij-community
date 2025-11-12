799a08dcbf0c872d76a1ff21a6919ac123f16926
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2025-11-12 14:32:52 +0100

    LLM-21314 [inline-completion] show inline completion tooltip in small editors only on a right click
    (cherry picked from commit 521b34cc41c011098e2d10af04192f4968c5c602)
    
    IJ-CR-182588
    
    GitOrigin-RevId: 7fcd38b7707071ffba585b1b41afc05aa27718b3

--- platform/platform-impl/codeinsight-inline/src/com/intellij/codeInsight/inline/completion/tooltip/InlineCompletionTooltipProvokerMouseMotionListener.kt
+++ platform/platform-impl/codeinsight-inline/src/com/intellij/codeInsight/inline/completion/tooltip/InlineCompletionTooltipProvokerMouseMotionListener.kt
@@ -1,25 +1,29 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.inline.completion.tooltip
 
-import com.intellij.codeInsight.inline.completion.session.InlineCompletionContext
+import com.intellij.codeInsight.inline.completion.editor.InlineCompletionEditorType
 import com.intellij.codeInsight.inline.completion.session.InlineCompletionSession
+import com.intellij.codeInsight.inline.completion.session.containsPoint
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.event.EditorMouseEvent
 import com.intellij.openapi.editor.event.EditorMouseEventArea
 import com.intellij.openapi.editor.event.EditorMouseMotionListener
 import com.intellij.openapi.util.Disposer
-import java.awt.Point
 
-internal class InlineCompletionMouseMotionListener : EditorMouseMotionListener {
+internal class InlineCompletionTooltipProvokerMouseMotionListener : EditorMouseMotionListener {
   private var hoveredSession: InlineCompletionSession? = null
 
   override fun mouseMoved(e: EditorMouseEvent) {
-    if (e.isConsumed) return
+    if (e.isConsumed || !isEditorTypeSupported(e.editor)) {
+      return
+    }
+
     if (e.area != EditorMouseEventArea.EDITING_AREA) {
       exitHover()
       return
     }
     val newHoveredSession = e.editor.let { InlineCompletionSession.getOrNull(it) }
-    if (newHoveredSession == null || !newHoveredSession.context.containsPoint(e.mouseEvent.point)) {
+    if (newHoveredSession == null || !newHoveredSession.context.state.containsPoint(e.mouseEvent.point)) {
       exitHover()
       return
     }
@@ -41,9 +45,16 @@ internal class InlineCompletionMouseMotionListener : EditorMouseMotionListener {
     hoveredSession = null
   }
 
-  private fun InlineCompletionContext.containsPoint(mousePoint: Point): Boolean {
-    return state.elements.mapNotNull { it.getBounds() }.any {
-      it.contains(mousePoint)
+  private fun isEditorTypeSupported(editor: Editor): Boolean {
+    return when (InlineCompletionEditorType.get(editor)) {
+      InlineCompletionEditorType.TERMINAL,
+      InlineCompletionEditorType.MAIN_EDITOR,
+        -> true
+      InlineCompletionEditorType.XDEBUGGER,
+      InlineCompletionEditorType.COMMIT_MESSAGES,
+      InlineCompletionEditorType.AI_ASSISTANT_CHAT_INPUT,
+      InlineCompletionEditorType.UNKNOWN,
+        -> false
     }
   }
 }
