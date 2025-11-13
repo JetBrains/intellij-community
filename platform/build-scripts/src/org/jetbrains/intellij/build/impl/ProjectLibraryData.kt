030a14314fd5017d4b0bb5a3f4b937ee4fb762bd
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-11-13 15:22:17 +0100

    IJPL-217708 Add kdoc for ProjectLibraryData.owner
    GitOrigin-RevId: 7567a60c0932022049e53baeb185f132ecefe24b

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/ProjectLibraryData.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/ProjectLibraryData.kt
@@ -14,6 +14,12 @@ enum class LibraryPackMode {
   STANDALONE_SEPARATE_WITHOUT_VERSION_NAME,
 }
 
+/**
+ * @param owner content module ([ModuleItem]) which brings the project library to distribution
+ * (commonly when "intellij.libraries.*" content module brings the project library).
+ * Or `null` if content modules that hold the project library are not used.
+ * So, this library is brought as an `iml` dependency or through custom layout.
+ */
 class ProjectLibraryData(
   @JvmField val libraryName: String,
   @JvmField val packMode: LibraryPackMode = LibraryPackMode.STANDALONE_MERGED,
