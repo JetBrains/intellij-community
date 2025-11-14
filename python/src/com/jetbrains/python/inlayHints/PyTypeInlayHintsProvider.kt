b51231f76a98837cb9837bc7dcc04a31d4d60de7
Aleksandr.Govenko <aleksandr.govenko@jetbrains.com>
2025-11-14 15:13:01 +0100

    PY-86123 Move inlay provider to a separate file, add tests and preview
    GitOrigin-RevId: 8b5e353dd533824f532d608547eb64034fe82bf3

--- python/src/com/jetbrains/python/inlayHints/PyTypeInlayHintsProvider.kt
+++ python/src/com/jetbrains/python/inlayHints/PyTypeInlayHintsProvider.kt
@@ -1,42 +1,23 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.inlayHints
 
-import com.intellij.codeInsight.CodeInsightBundle
-import com.intellij.codeInsight.hint.HintManager
 import com.intellij.codeInsight.hints.declarative.*
 import com.intellij.codeInsight.hints.declarative.impl.PresentationTreeBuilderImpl.Companion.MAX_SEGMENT_TEXT_LENGTH
-import com.intellij.openapi.application.EDT
-import com.intellij.openapi.application.readAction
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.event.EditorMouseEvent
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.psi.SmartPointerManager
-import com.intellij.psi.SmartPsiElementPointer
-import com.intellij.util.PsiNavigateUtil
-import com.intellij.util.concurrency.annotations.RequiresEdt
 import com.jetbrains.python.codeInsight.typing.PyTypingTypeProvider.REVEAL_TYPE
 import com.jetbrains.python.codeInsight.typing.PyTypingTypeProvider.REVEAL_TYPE_EXT
 import com.jetbrains.python.documentation.PythonDocumentationProvider
 import com.jetbrains.python.psi.PyCallExpression
-import com.jetbrains.python.psi.PyClassPattern
 import com.jetbrains.python.psi.PyFunction
-import com.jetbrains.python.psi.PyKeywordPattern
-import com.jetbrains.python.psi.impl.PyClassPatternImpl
-import com.jetbrains.python.psi.impl.references.PyKeywordPatternReference
 import com.jetbrains.python.psi.resolve.PyResolveContext
-import com.jetbrains.python.psi.types.PyClassType
 import com.jetbrains.python.psi.types.TypeEvalContext
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class PyTypeInlayHintsProvider : InlayHintsProvider {
   companion object {
     const val REVEAL_TYPE_OPTION_ID: String = "python.type.inlays.reveal_type"
     const val FUNCTION_RETURN_TYPE_OPTION_ID: String = "python.type.inlays.function.return"
-    const val PSEUDO_KEYWORD_PATTERN_OPTION_ID: String = "python.type.inlays.pseudo.keyword.pattern"
   }
 
   override fun createCollector(file: PsiFile, editor: Editor): InlayHintsCollector? = Collector()
@@ -56,10 +37,6 @@ class PyTypeInlayHintsProvider : InlayHintsProvider {
 
       sink.whenOptionEnabled(FUNCTION_RETURN_TYPE_OPTION_ID) {
         getInlaysForReturnType(element, sink, resolveContext)
-      }
-      
-      sink.whenOptionEnabled(PSEUDO_KEYWORD_PATTERN_OPTION_ID) {
-        getPositionalPatternNames(element, sink, resolveContext)
       }
     }
 
@@ -101,59 +78,6 @@ class PyTypeInlayHintsProvider : InlayHintsProvider {
             text(typeHint)
           }
         }
-      }
-    }
-    
-    private fun getPositionalPatternNames(element: PsiElement, sink: InlayTreeSink, resolveContext: PyResolveContext) {
-      val typeEvalContext = resolveContext.typeEvalContext
-      val classPattern = element as? PyClassPattern ?: return
-      val classType = typeEvalContext.getType(classPattern) as? PyClassType ?: return
-      val matchArgs = PyClassPatternImpl.getMatchArgs(classType, typeEvalContext) ?: return
-      classPattern.argumentList.patterns.mapIndexed { idx, pattern ->
-        if (pattern is PyKeywordPattern) return // positional patterns can only be before keyword patterns
-        val name = matchArgs.getOrNull(idx) ?: return@mapIndexed
-        sink.addPresentation(position = InlineInlayPosition(pattern.textRange.startOffset, false), hintFormat = hintFormat) {
-          val navigationData = InlayActionData(
-            PyPseudoKeywordPatternInlayActionHandler.Payload(SmartPointerManager.createPointer(classPattern), name), 
-            PyPseudoKeywordPatternInlayActionHandler.HANDLER_NAME
-          )
-          text(name, actionData = navigationData)
-          text("=")
-        }
-      }
-    }
-  }
-}
-
-
-
-class PyPseudoKeywordPatternInlayActionHandler(private val cs: CoroutineScope) : InlayActionHandler {
-  companion object {
-    const val HANDLER_NAME: String = "py.pseudo.keyword.pattern"
-  }
-
-  data class Payload(val classPattern: SmartPsiElementPointer<PyClassPattern>, val keyword: String) : InlayActionPayload
-
-  @RequiresEdt
-  override fun handleClick(e: EditorMouseEvent, payload: InlayActionPayload) {
-    payload as Payload
-    
-    cs.launch {
-      val resolveResults = readAction {
-        val classPattern = payload.classPattern.element ?: return@readAction null
-        val keyword = payload.keyword
-        val typeEvalContext = TypeEvalContext.userInitiated(classPattern.project, classPattern.containingFile)
-        PyKeywordPatternReference.resolveKeyword(classPattern, keyword, PyResolveContext.defaultContext(typeEvalContext))
-      }
-      val element = resolveResults?.firstOrNull()?.element ?: run {
-        withContext(Dispatchers.EDT) {
-          HintManager.getInstance().showInformationHint(e.editor, CodeInsightBundle.message("declaration.navigation.nowhere.to.go"))
-        }
-        return@launch
-      }
-      
-      withContext(Dispatchers.EDT) {
-        PsiNavigateUtil.navigate(element)
       }
     }
   }
