f05d88a6a3c462900d7dbc03f926bb9aba9dffc4
Daniil Kalinin <Daniil.Kalinin@jetbrains.com>
2025-11-17 11:27:40 +0100

    test framework [Code Insight]: PY-85639 Introduce `PlatformWayJUnit5CodeInsightTest` for showcasing explicit JUnit 5 fixtures usage in Python inspections test
    Space-RevId: 95e321d8d5680f3b2f491a1b8e76b83f60cb4a20
    
    GitOrigin-RevId: 4fc6654ebbe0443f79920c70555a53b6ce5ccce2

---
+++ python/testSrc/com/jetbrains/python/inspections/JUnit5/showcase/PlatformWayJUnit5CodeInsightTest.kt
@@ -0,0 +1,55 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.jetbrains.python.inspections.JUnit5.showcase
+
+import com.intellij.platform.testFramework.junit5.codeInsight.fixture.codeInsightFixture
+import com.intellij.testFramework.TestDataPath
+import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.testFramework.junit5.fixture.moduleFixture
+import com.intellij.testFramework.junit5.fixture.projectFixture
+import com.intellij.testFramework.junit5.fixture.tempPathFixture
+import com.jetbrains.python.PythonMockSdk
+import com.jetbrains.python.inspections.JUnit5.pyMockSdkFixture
+import com.jetbrains.python.inspections.PyTypeCheckerInspection
+import com.jetbrains.python.psi.LanguageLevel
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+/**
+ * This test represents the platform way of writing JUnit5 tests.
+ *
+ * Unlike [com.jetbrains.python.inspections.JUnit5.showcase.PyJUnit5CodeInsightExampleTest],
+ * all fixtures here are defined explicitly.
+ */
+@TestApplication
+@TestDataPath($$"$PROJECT_ROOT/community/python/testData/junit5/showcase/PyTypeCheckerInspection")
+class PlatformWayJUnit5CodeInsightTest {
+  companion object {
+    private val tempDir = tempPathFixture()
+    private val project = projectFixture(tempDir, openAfterCreation = true)
+    @Suppress("unused")
+    private val module = project.moduleFixture(tempDir, addPathToSourceRoot = true)
+    @Suppress("unused")
+    private val mockSdk = project.pyMockSdkFixture(module) {
+      PythonMockSdk.create(LanguageLevel.getLatest())
+    }
+  }
+  private val codeInsightFixture by codeInsightFixture(project, tempDir)
+
+  @BeforeEach
+  fun beforeEach() {
+    codeInsightFixture.enableInspections(PyTypeCheckerInspection::class.java)
+  }
+
+  @Test
+  fun testAssignIntToStr() {
+    codeInsightFixture.configureByText("test.py",
+                       "x: str = <warning descr=\"Expected type 'str', got 'int' instead\">3</warning>")
+    codeInsightFixture.checkHighlighting()
+  }
+
+  @Test
+  fun testSingle() {
+    codeInsightFixture.configureByFile("single.py")
+    codeInsightFixture.checkHighlighting()
+  }
+}
