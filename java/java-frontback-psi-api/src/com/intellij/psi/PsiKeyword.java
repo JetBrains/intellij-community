718cd73e6720f6672e894d48de567a16894959b3
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2025-11-17 12:53:11 +0100

    [java] IDEA-365317 deprecate for removal constants in PsiKeyword
    GitOrigin-RevId: 086047ab42eba3dc019c8013545a89300145d1e0

--- java/java-frontback-psi-api/src/com/intellij/psi/PsiKeyword.java
+++ java/java-frontback-psi-api/src/com/intellij/psi/PsiKeyword.java
@@ -3,6 +3,7 @@ package com.intellij.psi;
 
 import com.intellij.java.syntax.parser.JavaKeywords;
 import com.intellij.openapi.util.NlsSafe;
+import org.jetbrains.annotations.ApiStatus;
 
 /**
  * Represents a Java keyword.
@@ -13,359 +14,359 @@ public interface PsiKeyword extends PsiJavaToken {
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String ABSTRACT = JavaKeywords.ABSTRACT;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String ABSTRACT = JavaKeywords.ABSTRACT;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String ASSERT = JavaKeywords.ASSERT;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String ASSERT = JavaKeywords.ASSERT;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String BOOLEAN = JavaKeywords.BOOLEAN;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String BOOLEAN = JavaKeywords.BOOLEAN;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String BREAK = JavaKeywords.BREAK;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String BREAK = JavaKeywords.BREAK;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String BYTE = JavaKeywords.BYTE;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String BYTE = JavaKeywords.BYTE;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String CASE = JavaKeywords.CASE;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String CASE = JavaKeywords.CASE;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String CATCH = JavaKeywords.CATCH;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String CATCH = JavaKeywords.CATCH;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String CHAR = JavaKeywords.CHAR;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String CHAR = JavaKeywords.CHAR;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String CLASS = JavaKeywords.CLASS;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String CLASS = JavaKeywords.CLASS;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String CONST = JavaKeywords.CONST;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String CONST = JavaKeywords.CONST;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String CONTINUE = JavaKeywords.CONTINUE;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String CONTINUE = JavaKeywords.CONTINUE;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String DEFAULT = JavaKeywords.DEFAULT;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String DEFAULT = JavaKeywords.DEFAULT;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String DO = JavaKeywords.DO;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String DO = JavaKeywords.DO;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String DOUBLE = JavaKeywords.DOUBLE;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String DOUBLE = JavaKeywords.DOUBLE;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String ELSE = JavaKeywords.ELSE;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String ELSE = JavaKeywords.ELSE;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String ENUM = JavaKeywords.ENUM;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String ENUM = JavaKeywords.ENUM;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String EXTENDS = JavaKeywords.EXTENDS;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String EXTENDS = JavaKeywords.EXTENDS;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String FINAL = JavaKeywords.FINAL;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String FINAL = JavaKeywords.FINAL;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String FINALLY = JavaKeywords.FINALLY;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String FINALLY = JavaKeywords.FINALLY;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String FLOAT = JavaKeywords.FLOAT;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String FLOAT = JavaKeywords.FLOAT;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String FOR = JavaKeywords.FOR;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String FOR = JavaKeywords.FOR;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String GOTO = JavaKeywords.GOTO;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String GOTO = JavaKeywords.GOTO;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String IF = JavaKeywords.IF;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String IF = JavaKeywords.IF;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String IMPLEMENTS = JavaKeywords.IMPLEMENTS;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String IMPLEMENTS = JavaKeywords.IMPLEMENTS;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String IMPORT = JavaKeywords.IMPORT;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String IMPORT = JavaKeywords.IMPORT;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String INSTANCEOF = JavaKeywords.INSTANCEOF;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String INSTANCEOF = JavaKeywords.INSTANCEOF;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String INT = JavaKeywords.INT;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String INT = JavaKeywords.INT;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String INTERFACE = JavaKeywords.INTERFACE;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String INTERFACE = JavaKeywords.INTERFACE;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String LONG = JavaKeywords.LONG;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String LONG = JavaKeywords.LONG;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String NATIVE = JavaKeywords.NATIVE;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String NATIVE = JavaKeywords.NATIVE;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String NEW = JavaKeywords.NEW;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String NEW = JavaKeywords.NEW;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String PACKAGE = JavaKeywords.PACKAGE;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String PACKAGE = JavaKeywords.PACKAGE;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String PRIVATE = JavaKeywords.PRIVATE;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String PRIVATE = JavaKeywords.PRIVATE;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String PROTECTED = JavaKeywords.PROTECTED;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String PROTECTED = JavaKeywords.PROTECTED;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String PUBLIC = JavaKeywords.PUBLIC;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String PUBLIC = JavaKeywords.PUBLIC;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String RETURN = JavaKeywords.RETURN;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String RETURN = JavaKeywords.RETURN;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String SHORT = JavaKeywords.SHORT;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String SHORT = JavaKeywords.SHORT;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String STATIC = JavaKeywords.STATIC;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String STATIC = JavaKeywords.STATIC;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String STRICTFP = JavaKeywords.STRICTFP;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String STRICTFP = JavaKeywords.STRICTFP;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String SUPER = JavaKeywords.SUPER;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String SUPER = JavaKeywords.SUPER;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String SWITCH = JavaKeywords.SWITCH;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String SWITCH = JavaKeywords.SWITCH;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String SYNCHRONIZED = JavaKeywords.SYNCHRONIZED;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String SYNCHRONIZED = JavaKeywords.SYNCHRONIZED;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String THIS = JavaKeywords.THIS;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String THIS = JavaKeywords.THIS;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String THROW = JavaKeywords.THROW;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String THROW = JavaKeywords.THROW;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String THROWS = JavaKeywords.THROWS;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String THROWS = JavaKeywords.THROWS;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String TRANSIENT = JavaKeywords.TRANSIENT;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String TRANSIENT = JavaKeywords.TRANSIENT;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String TRY = JavaKeywords.TRY;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String TRY = JavaKeywords.TRY;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String VOID = JavaKeywords.VOID;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String VOID = JavaKeywords.VOID;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String VOLATILE = JavaKeywords.VOLATILE;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String VOLATILE = JavaKeywords.VOLATILE;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String WHILE = JavaKeywords.WHILE;
-
-
-  /**
-   * @deprecated Use {@link JavaKeywords} instead
-   */
-  @Deprecated @NlsSafe String TRUE = JavaKeywords.TRUE;
-
-  /**
-   * @deprecated Use {@link JavaKeywords} instead
-   */
-  @Deprecated @NlsSafe String FALSE = JavaKeywords.FALSE;
-
-  /**
-   * @deprecated Use {@link JavaKeywords} instead
-   */
-  @Deprecated @NlsSafe String NULL = JavaKeywords.NULL;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String WHILE = JavaKeywords.WHILE;
 
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String NON_SEALED = JavaKeywords.NON_SEALED;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String TRUE = JavaKeywords.TRUE;
+
+  /**
+   * @deprecated Use {@link JavaKeywords} instead
+   */
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String FALSE = JavaKeywords.FALSE;
+
+  /**
+   * @deprecated Use {@link JavaKeywords} instead
+   */
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String NULL = JavaKeywords.NULL;
+
+
+  /**
+   * @deprecated Use {@link JavaKeywords} instead
+   */
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String NON_SEALED = JavaKeywords.NON_SEALED;
 
   // soft keywords:
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String EXPORTS = JavaKeywords.EXPORTS;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String EXPORTS = JavaKeywords.EXPORTS;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String MODULE = JavaKeywords.MODULE;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String MODULE = JavaKeywords.MODULE;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String OPEN = JavaKeywords.OPEN;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String OPEN = JavaKeywords.OPEN;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String OPENS = JavaKeywords.OPENS;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String OPENS = JavaKeywords.OPENS;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String PERMITS = JavaKeywords.PERMITS;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String PERMITS = JavaKeywords.PERMITS;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String PROVIDES = JavaKeywords.PROVIDES;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String PROVIDES = JavaKeywords.PROVIDES;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String RECORD = JavaKeywords.RECORD;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String RECORD = JavaKeywords.RECORD;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String REQUIRES = JavaKeywords.REQUIRES;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String REQUIRES = JavaKeywords.REQUIRES;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String SEALED = JavaKeywords.SEALED;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String SEALED = JavaKeywords.SEALED;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String TO = JavaKeywords.TO;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String TO = JavaKeywords.TO;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String TRANSITIVE = JavaKeywords.TRANSITIVE;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String TRANSITIVE = JavaKeywords.TRANSITIVE;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String USES = JavaKeywords.USES;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String USES = JavaKeywords.USES;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String VALUE = JavaKeywords.VALUE;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String VALUE = JavaKeywords.VALUE;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String VAR = JavaKeywords.VAR;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String VAR = JavaKeywords.VAR;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String WHEN = JavaKeywords.WHEN;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String WHEN = JavaKeywords.WHEN;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String WITH = JavaKeywords.WITH;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String WITH = JavaKeywords.WITH;
 
   /**
    * @deprecated Use {@link JavaKeywords} instead
    */
-  @Deprecated @NlsSafe String YIELD = JavaKeywords.YIELD;
+  @Deprecated @ApiStatus.ScheduledForRemoval @NlsSafe String YIELD = JavaKeywords.YIELD;
 }
