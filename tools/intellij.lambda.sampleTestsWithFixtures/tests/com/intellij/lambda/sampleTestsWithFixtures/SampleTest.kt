e835485f6247838291fec8a2e94ce4b0e7fe3f2f
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2025-11-17 22:17:54 +0100

    AT-3511: initial commit for sample test with fixtures
    not yet working
    
    GitOrigin-RevId: 449ae4455b2412b838a5575c842e72c9251d4026

---
+++ tools/intellij.lambda.sampleTestsWithFixtures/tests/com/intellij/lambda/sampleTestsWithFixtures/SampleTest.kt
@@ -0,0 +1,39 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.lambda.sampleTestsWithFixtures
+
+import com.intellij.lambda.sampleTestsWithFixtures.util.openNewEditor
+import com.intellij.lambda.testFramework.junit.ExecuteInMonolithAndSplitMode
+import com.intellij.lambda.testFramework.junit.IdeRunMode
+import com.intellij.lambda.testFramework.testApi.editor.openFile
+import com.intellij.lambda.testFramework.testApi.getProject
+import com.intellij.lambda.testFramework.testApi.getProjects
+import com.intellij.lambda.testFramework.testApi.waitForProject
+import com.intellij.lambda.testFramework.utils.BackgroundRunWithLambda
+import com.intellij.openapi.diagnostic.Logger
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.TestTemplate
+import kotlin.time.Duration.Companion.seconds
+
+@ExecuteInMonolithAndSplitMode
+class SampleTest {
+  @TestTemplate
+  fun `serialized test`(ide: BackgroundRunWithLambda) = runBlocking {
+    ide.apply {
+      runInBackend {
+        //waitForProject(20.seconds)
+      }
+
+      run {
+        openNewEditor("/src/com/example/Foo.java")
+        Logger.getInstance("test").warn("Projects: " + getProjects().joinToString { it.name })
+      }
+
+      runInBackend {
+        Logger.getInstance("test").warn("backend Projects: " + getProject())
+        openFile("src/com/example/Foo.java", waitForReadyState = false, requireFocus = false)
+      }
+    }
+    Unit
+  }
+}
+
