f3c841a8a876ae577df949300137b79ecdf9ed9e
Sergei Tachenov <Sergei.Tachenov@jetbrains.com>
2025-11-18 10:33:28 +0200

    [UI, PV] IJPL-197345 Refactor ProjectViewUpdateCause: bring back the enum
    We'll need an enum anyway for FUS. So let's extract all
    internal causes plus the PLUGIN_3RD_PARTY value.
    
    GitOrigin-RevId: fa91aca95de0a2dfc4e5ba35ba6d110a0c64f80f

--- platform/platform-api/src/com/intellij/ui/treeStructure/ProjectViewUpdateCause.kt
+++ platform/platform-api/src/com/intellij/ui/treeStructure/ProjectViewUpdateCause.kt
@@ -8,62 +8,62 @@ sealed class ProjectViewUpdateCause : Comparable<ProjectViewUpdateCause> {
   companion object {
     fun plugin(pluginId: String): ProjectViewUpdateCause = ProjectView3rdPartyPluginUpdateCause(pluginId)
 
-    @JvmField val UNKNOWN: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("UNKNOWN")
-    @JvmField val LEGACY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("LEGACY")
-    @JvmField val SETTINGS: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("SETTINGS")
-    @JvmField val ACTION: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("ACTION")
-    @JvmField val BOOKMARKS: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("BOOKMARKS")
-    @JvmField val CLIPBOARD: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("CLIPBOARD")
-    @JvmField val EXTENSIONS_CHANGED: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("EXTENSIONS_CHANGED")
-    @JvmField val PSI_FLATTEN_PACKAGES: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PSI_FLATTEN_PACKAGES")
-    @JvmField val PSI_SCRATCH: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PSI_SCRATCH")
-    @JvmField val PSI_PROPERTY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PSI_PROPERTY")
-    @JvmField val PSI_CHILDREN: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PSI_CHILDREN")
-    @JvmField val PSI_MOVE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PSI_MOVE")
-    @JvmField val ROOTS_LIBRARY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("ROOTS_LIBRARY")
-    @JvmField val ROOTS_MODULE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("ROOTS_MODULE")
-    @JvmField val ROOTS_EP: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("ROOTS_EP")
-    @JvmField val FILE_OPENED: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("FILE_OPENED")
-    @JvmField val FILE_CLOSED: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("FILE_CLOSED")
-    @JvmField val FILE_STATUS: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("FILE_STATUS")
-    @JvmField val FILE_STATUSES: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("FILE_STATUSES")
-    @JvmField val FILE_APPEARANCE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("FILE_APPEARANCE")
-    @JvmField val VFS: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("VFS")
-    @JvmField val VFS_CREATE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("VFS_CREATE")
-    @JvmField val VFS_COPY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("VFS_COPY")
-    @JvmField val VFS_MOVE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("VFS_MOVE")
-    @JvmField val VFS_DELETE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("VFS_DELETE")
-    @JvmField val PROBLEMS_APPEARED: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PROBLEMS_APPEARED")
-    @JvmField val PROBLEMS_DISAPPEARED: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PROBLEMS_DISAPPEARED")
-    @JvmField val SCOPE_CHOOSER: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("SCOPE_CHOOSER")
-    @JvmField val SCRATCHES: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("SCRATCHES")
-    @JvmField val REFACTORING: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("REFACTORING")
-    @JvmField val ANDROID: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("ANDROID")
-    @JvmField val PLUGIN_BAZEL: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_BAZEL")
-    @JvmField val PLUGIN_COVERAGE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_COVERAGE")
-    @JvmField val PLUGIN_DART: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_DART")
-    @JvmField val PLUGIN_DBE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_DBE")
-    @JvmField val PLUGIN_DTS: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_DTS")
-    @JvmField val PLUGIN_JAVAEE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_JAVAEE")
-    @JvmField val PLUGIN_JUPYTER: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_JUPYTER")
-    @JvmField val PLUGIN_PROJECT_FRAGMENTS: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_PROJECT_FRAGMENTS")
-    @JvmField val PLUGIN_PHP: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_PHP")
-    @JvmField val PLUGIN_PROPERTIES: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_PROPERTIES")
-    @JvmField val PLUGIN_PUPPET: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_PUPPET")
-    @JvmField val PLUGIN_PYTHON: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_PYTHON")
-    @JvmField val PLUGIN_REACT_BUDDY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_REACT_BUDDY")
-    @JvmField val PLUGIN_RUBY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_RUBY")
-    @JvmField val PLUGIN_SPRING: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_SPRING")
-    @JvmField val PLUGIN_WORKSPACE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_WORKSPACE")
-    @JvmField val PLUGIN_XPATH: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("PLUGIN_XPATH")
-    @JvmField val DEBUG_VFS_INFO: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("DEBUG_VFS_INFO")
-    @JvmField val DEBUG_INDEXABILITY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause("DEBUG_INDEXABILITY")
+    @JvmField val UNKNOWN: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.UNKNOWN)
+    @JvmField val LEGACY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.LEGACY)
+    @JvmField val SETTINGS: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.SETTINGS)
+    @JvmField val ACTION: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.ACTION)
+    @JvmField val BOOKMARKS: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.BOOKMARKS)
+    @JvmField val CLIPBOARD: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.CLIPBOARD)
+    @JvmField val EXTENSIONS_CHANGED: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.EXTENSIONS_CHANGED)
+    @JvmField val PSI_FLATTEN_PACKAGES: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PSI_FLATTEN_PACKAGES)
+    @JvmField val PSI_SCRATCH: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PSI_SCRATCH)
+    @JvmField val PSI_PROPERTY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PSI_PROPERTY)
+    @JvmField val PSI_CHILDREN: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PSI_CHILDREN)
+    @JvmField val PSI_MOVE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PSI_MOVE)
+    @JvmField val ROOTS_LIBRARY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.ROOTS_LIBRARY)
+    @JvmField val ROOTS_MODULE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.ROOTS_MODULE)
+    @JvmField val ROOTS_EP: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.ROOTS_EP)
+    @JvmField val FILE_OPENED: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.FILE_OPENED)
+    @JvmField val FILE_CLOSED: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.FILE_CLOSED)
+    @JvmField val FILE_STATUS: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.FILE_STATUS)
+    @JvmField val FILE_STATUSES: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.FILE_STATUSES)
+    @JvmField val FILE_APPEARANCE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.FILE_APPEARANCE)
+    @JvmField val VFS: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.VFS)
+    @JvmField val VFS_CREATE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.VFS_CREATE)
+    @JvmField val VFS_COPY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.VFS_COPY)
+    @JvmField val VFS_MOVE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.VFS_MOVE)
+    @JvmField val VFS_DELETE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.VFS_DELETE)
+    @JvmField val PROBLEMS_APPEARED: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PROBLEMS_APPEARED)
+    @JvmField val PROBLEMS_DISAPPEARED: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PROBLEMS_DISAPPEARED)
+    @JvmField val SCOPE_CHOOSER: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.SCOPE_CHOOSER)
+    @JvmField val SCRATCHES: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.SCRATCHES)
+    @JvmField val REFACTORING: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.REFACTORING)
+    @JvmField val ANDROID: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.ANDROID)
+    @JvmField val PLUGIN_BAZEL: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_BAZEL)
+    @JvmField val PLUGIN_COVERAGE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_COVERAGE)
+    @JvmField val PLUGIN_DART: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_DART)
+    @JvmField val PLUGIN_DBE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_DBE)
+    @JvmField val PLUGIN_DTS: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_DTS)
+    @JvmField val PLUGIN_JAVAEE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_JAVAEE)
+    @JvmField val PLUGIN_JUPYTER: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_JUPYTER)
+    @JvmField val PLUGIN_PROJECT_FRAGMENTS: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_PROJECT_FRAGMENTS)
+    @JvmField val PLUGIN_PHP: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_PHP)
+    @JvmField val PLUGIN_PROPERTIES: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_PROPERTIES)
+    @JvmField val PLUGIN_PUPPET: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_PUPPET)
+    @JvmField val PLUGIN_PYTHON: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_PYTHON)
+    @JvmField val PLUGIN_REACT_BUDDY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_REACT_BUDDY)
+    @JvmField val PLUGIN_RUBY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_RUBY)
+    @JvmField val PLUGIN_SPRING: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_SPRING)
+    @JvmField val PLUGIN_WORKSPACE: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_WORKSPACE)
+    @JvmField val PLUGIN_XPATH: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.PLUGIN_XPATH)
+    @JvmField val DEBUG_VFS_INFO: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.DEBUG_VFS_INFO)
+    @JvmField val DEBUG_INDEXABILITY: ProjectViewUpdateCause = ProjectViewStandardUpdateCause(ProjectViewUpdateCauseId.DEBUG_INDEXABILITY)
   }
 
   override fun compareTo(other: ProjectViewUpdateCause): Int {
     return when (this) {
       is ProjectViewStandardUpdateCause -> when (other) {
-        is ProjectViewStandardUpdateCause -> id.compareTo(other.id)
+        is ProjectViewStandardUpdateCause -> causeId.compareTo(other.causeId)
         is ProjectView3rdPartyPluginUpdateCause -> 1
       }
       is ProjectView3rdPartyPluginUpdateCause -> when (other) {
@@ -74,13 +74,74 @@ sealed class ProjectViewUpdateCause : Comparable<ProjectViewUpdateCause> {
   }
 }
 
-private data class ProjectViewStandardUpdateCause(
-  val id: String
-): ProjectViewUpdateCause() {
-  override fun toString(): String = id
+@ApiStatus.Internal
+enum class ProjectViewUpdateCauseId {
+  PLUGIN_3RD_PARTY,
+
+  UNKNOWN,
+  LEGACY,
+  SETTINGS,
+  ACTION,
+  BOOKMARKS,
+  CLIPBOARD,
+  EXTENSIONS_CHANGED,
+  PSI_FLATTEN_PACKAGES,
+  PSI_SCRATCH,
+  PSI_PROPERTY,
+  PSI_CHILDREN,
+  PSI_MOVE,
+  ROOTS_LIBRARY,
+  ROOTS_MODULE,
+  ROOTS_EP,
+  FILE_OPENED,
+  FILE_CLOSED,
+  FILE_STATUS,
+  FILE_STATUSES,
+  FILE_APPEARANCE,
+  VFS,
+  VFS_CREATE,
+  VFS_COPY,
+  VFS_MOVE,
+  VFS_DELETE,
+  PROBLEMS_APPEARED,
+  PROBLEMS_DISAPPEARED,
+  SCOPE_CHOOSER,
+  SCRATCHES,
+  REFACTORING,
+  ANDROID,
+  PLUGIN_BAZEL,
+  PLUGIN_COVERAGE,
+  PLUGIN_DART,
+  PLUGIN_DBE,
+  PLUGIN_DTS,
+  PLUGIN_JAVAEE,
+  PLUGIN_JUPYTER,
+  PLUGIN_PROJECT_FRAGMENTS,
+  PLUGIN_PHP,
+  PLUGIN_PROPERTIES,
+  PLUGIN_PUPPET,
+  PLUGIN_PYTHON,
+  PLUGIN_REACT_BUDDY,
+  PLUGIN_RUBY,
+  PLUGIN_SPRING,
+  PLUGIN_WORKSPACE,
+  PLUGIN_XPATH,
+  DEBUG_VFS_INFO,
+  DEBUG_INDEXABILITY,
 }
 
-private data class ProjectView3rdPartyPluginUpdateCause(
+@ApiStatus.Internal
+data class ProjectViewStandardUpdateCause(
+  val causeId: ProjectViewUpdateCauseId
+): ProjectViewUpdateCause() {
+  init {
+    require(causeId != ProjectViewUpdateCauseId.PLUGIN_3RD_PARTY)
+  }
+  override fun toString(): String = causeId.toString()
+}
+
+@ApiStatus.Internal
+data class ProjectView3rdPartyPluginUpdateCause(
   val pluginId: String
 ): ProjectViewUpdateCause() {
   override fun toString(): String = "PLUGIN=$pluginId"
