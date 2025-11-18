51a2d46d28a75c5039d8c18cdff9047c6165dd23
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2025-11-18 15:54:32 +0100

    [vcs-log]: make highlight non-cherry-picked commits work for arbitrary target branch (IJPL-219013)
    GitOrigin-RevId: a476ea0aca8e099b13aef26a20504582cf06b21b

--- plugins/git4idea/src/git4idea/branch/GitCompareBranchesUi.kt
+++ plugins/git4idea/src/git4idea/branch/GitCompareBranchesUi.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.branch
 
 import com.intellij.openapi.Disposable
@@ -100,7 +100,7 @@ class GitCompareBranchesUi(
     }
   }
 
-  private class MyVcsLogUi(id: String, logData: VcsLogData, colorManager: VcsLogColorManager,
+  internal class MyVcsLogUi(id: String, logData: VcsLogData, colorManager: VcsLogColorManager,
                            uiProperties: MainVcsLogUiProperties, refresher: VisiblePackRefresher,
                            rangeFilter: VcsLogRangeFilter, rootFilter: VcsLogRootFilter?,
                            isEditorDiffPreview: Boolean) :
