975a8b776e6811e28a8cf4847134b74570f404a5
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2025-11-19 14:38:03 +0200

    [lambda tests] AT-3575 Register CI server DI instance
    GitOrigin-RevId: 2392c8596a6c3b722c6013d2a6b1a02d31974108

--- tools/intellij.tools.ide.starter.build.server/src/com/intellij/tools/ide/starter/build/server/DevBuildServerListener.kt
+++ tools/intellij.tools.ide.starter.build.server/src/com/intellij/tools/ide/starter/build/server/DevBuildServerListener.kt
@@ -1,11 +1,14 @@
 package com.intellij.tools.ide.starter.build.server
 
+import com.intellij.ide.starter.ci.CIServer
+import com.intellij.ide.starter.ci.teamcity.TeamCityCIServer
 import com.intellij.ide.starter.di.DISnapshot
 import com.intellij.ide.starter.di.di
 import com.intellij.ide.starter.runner.DevBuildServerRunner
 import org.junit.platform.launcher.TestExecutionListener
 import org.kodein.di.DI
 import org.kodein.di.bindSingleton
+import java.net.URI
 
 private val lock = Any()
 
@@ -14,6 +17,10 @@ fun initStarterDevBuildServerDI() {
     di = DI {
       extend(di)
       bindSingleton<DevBuildServerRunner>(overrides = true) { DevBuildServerRunnerImpl }
+
+      // TODO: remove this temporary workaround in case if we will announce lambda tests for external contributors/plugin developers
+      bindSingleton(tag = "teamcity.uri", overrides = true) { URI("https://buildserver.labs.intellij.net").normalize() }
+      bindSingleton<CIServer>(overrides = true) { TeamCityCIServer() }
     }
     DISnapshot.initSnapshot(di, overwrite = true)
   }
