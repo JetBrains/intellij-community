b1cbb27146c73a802361aa18c61d8ca6d09236e3
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-11-20 09:57:34 +0200

    [terminal] IJPL-192671 Fix shell path reset to default after first restart
    Was caused by the migration logic. Now this logic is not more needed, so let's just remove it.
    
    
    (cherry picked from commit 9e9f960a4b9a84b3e93a048cc44f14e364c6458f)
    
    IJ-CR-183174
    
    GitOrigin-RevId: 02afdcb40c4ecaaba71852a7d31c90c5f478f1c9

--- plugins/terminal/src/org/jetbrains/plugins/terminal/settings/impl/TerminalLocalOptionsImpl.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/settings/impl/TerminalLocalOptionsImpl.kt
@@ -1,9 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.terminal.settings.impl
 
-import com.intellij.ide.util.RunOnceUtil
 import com.intellij.openapi.components.*
-import org.jetbrains.plugins.terminal.TerminalOptionsProvider
 import org.jetbrains.plugins.terminal.settings.TerminalLocalOptions
 
 @State(name = "TerminalLocalOptions",
@@ -19,12 +17,6 @@ internal class TerminalLocalOptionsImpl : TerminalLocalOptions, PersistentStateC
 
   override fun loadState(state: State) {
     this.state = state
-
-    RunOnceUtil.runOnceForApp("TerminalLocalOptions.migration") {
-      @Suppress("DEPRECATION")
-      val previousShellPath = TerminalOptionsProvider.Companion.instance.state.myShellPath
-      shellPath = previousShellPath
-    }
   }
 
   class State {
