1ec8bb7406ee15d2bf88a233bc993dd32f19d7c6
David Lysenko <david.lysenko@jetbrains.com>
2025-11-21 17:27:32 +0100

    [pycharm] PY-85729 Fix smoke tests
    GitOrigin-RevId: 43ef5cff8a1bb2a555ceb5a6a50a776726a0bbde

--- python/python-process-output/impl/src/com/intellij/python/processOutput/impl/icons/PythonProcessOutputImplIcons.java
+++ python/python-process-output/impl/src/com/intellij/python/processOutput/impl/icons/PythonProcessOutputImplIcons.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.python.processOutput.impl;
+package com.intellij.python.processOutput.impl.icons;
 
 import com.intellij.ui.IconManager;
 import org.jetbrains.annotations.NotNull;
@@ -10,9 +10,10 @@ import javax.swing.*;
  * NOTE THIS FILE IS AUTO-GENERATED
  * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
  */
-public final class PythonProcessOutputIcons {
+@org.jetbrains.annotations.ApiStatus.Internal
+public final class PythonProcessOutputImplIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
-    return IconManager.getInstance().loadRasterizedIcon(path, PythonProcessOutputIcons.class.getClassLoader(), cacheKey, flags);
+    return IconManager.getInstance().loadRasterizedIcon(path, PythonProcessOutputImplIcons.class.getClassLoader(), cacheKey, flags);
   }
   /** 16x16 */ public static final @NotNull Icon CommandQueue = load("icons/commandQueue.svg", 1434828820, 2);
   /** 16x16 */ public static final @NotNull Icon Process = load("icons/process.svg", -2015894419, 2);
