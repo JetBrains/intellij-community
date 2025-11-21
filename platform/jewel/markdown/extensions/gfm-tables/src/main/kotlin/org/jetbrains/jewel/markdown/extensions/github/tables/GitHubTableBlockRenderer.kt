10911f5139364182bd081262d60f2ab1f051e73f
Alexander Kuznetsov <aleksander.kuznetsov@jetbrains.com>
2025-11-21 16:00:42 +0100

    [JEWEL-1155] Clip table cell content
    So it doesn't go beyond the composable
    when there's not enough horizontal space.
    
    closes https://github.com/JetBrains/intellij-community/pull/3317
    
    GitOrigin-RevId: fb6c0002c37c70229caa6b86a73abec72a0df54c

--- platform/jewel/markdown/extensions/gfm-tables/src/main/kotlin/org/jetbrains/jewel/markdown/extensions/github/tables/GitHubTableBlockRenderer.kt
+++ platform/jewel/markdown/extensions/gfm-tables/src/main/kotlin/org/jetbrains/jewel/markdown/extensions/github/tables/GitHubTableBlockRenderer.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import org.jetbrains.annotations.ApiStatus
@@ -159,7 +160,7 @@ internal class GitHubTableBlockRenderer(
         onUrlClick: (String) -> Unit,
     ) {
         Box(
-            modifier = Modifier.background(backgroundColor).padding(padding),
+            modifier = Modifier.background(backgroundColor).padding(padding).clipToBounds(),
             contentAlignment = (cell.alignment ?: defaultAlignment).asContentAlignment(),
         ) {
             blockRenderer.RenderParagraph(
