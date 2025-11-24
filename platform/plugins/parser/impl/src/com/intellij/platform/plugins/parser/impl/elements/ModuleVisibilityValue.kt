5f577a40c54e29fe395ecbf1beef1badd431b268
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-11-24 14:52:15 +0100

    IJPL-219674 add xmlValue to ModuleVisibilityValue enum
    GitOrigin-RevId: 686da4ae3e9103981539abcbed21db60124ab044

--- platform/plugins/parser/impl/src/com/intellij/platform/plugins/parser/impl/elements/ModuleVisibilityValue.kt
+++ platform/plugins/parser/impl/src/com/intellij/platform/plugins/parser/impl/elements/ModuleVisibilityValue.kt
@@ -1,8 +1,10 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.plugins.parser.impl.elements
 
-enum class ModuleVisibilityValue {
-  PRIVATE,
-  INTERNAL,
-  PUBLIC,
+import com.intellij.platform.plugins.parser.impl.PluginXmlConst
+
+enum class ModuleVisibilityValue(val xmlValue: String) {
+  PRIVATE(PluginXmlConst.CONTENT_MODULE_VISIBILITY_PRIVATE_VALUE),
+  INTERNAL(PluginXmlConst.CONTENT_MODULE_VISIBILITY_INTERNAL_VALUE),
+  PUBLIC(PluginXmlConst.CONTENT_MODULE_VISIBILITY_PUBLIC_VALUE),
 }
