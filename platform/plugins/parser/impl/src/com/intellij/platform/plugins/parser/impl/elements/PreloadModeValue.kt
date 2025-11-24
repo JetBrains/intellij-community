0f5a4d1255acb3466755c5ae2dcfae447a97bd7e
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-11-24 15:09:28 +0100

    IJPL-219674 add xmlValue to PreloadModeValue enum
    GitOrigin-RevId: 858ace88fcff0dc5eb089ae24bd4094de492b3a2

--- platform/plugins/parser/impl/src/com/intellij/platform/plugins/parser/impl/elements/PreloadModeValue.kt
+++ platform/plugins/parser/impl/src/com/intellij/platform/plugins/parser/impl/elements/PreloadModeValue.kt
@@ -1,6 +1,12 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.plugins.parser.impl.elements
 
-enum class PreloadModeValue {
-  TRUE, FALSE, AWAIT, NOT_HEADLESS, NOT_LIGHT_EDIT;
+import com.intellij.platform.plugins.parser.impl.PluginXmlConst
+
+enum class PreloadModeValue(val xmlValue: String) {
+  TRUE(PluginXmlConst.SERVICE_EP_PRELOAD_TRUE_VALUE),
+  FALSE(PluginXmlConst.SERVICE_EP_PRELOAD_FALSE_VALUE),
+  AWAIT(PluginXmlConst.SERVICE_EP_PRELOAD_AWAIT_VALUE),
+  NOT_HEADLESS(PluginXmlConst.SERVICE_EP_PRELOAD_NOT_HEADLESS_VALUE),
+  NOT_LIGHT_EDIT(PluginXmlConst.SERVICE_EP_PRELOAD_NOT_LIGHT_EDIT_VALUE);
 }
