50279c259cce45328f7cf07055b2d6a7f28d4b1a
Alexander Zolotov <zolotov@jetbrains.com>
2025-11-24 11:42:28 +0100

    FL-33296 [text-matching]: make NameUtilCore multiplatform
    hide operations over codepoint behind the abstraction, so KMP client could implement it for particular platform by themselves
    
    GitOrigin-RevId: 61d203f51cd8ffd3c809b3092ebc6e9737c6e819

--- platform/util/base/src/com/intellij/util/text/NameUtilCore.kt
+++ platform/util/base/src/com/intellij/util/text/NameUtilCore.kt
@@ -1,7 +1,7 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.text
 
-import com.intellij.util.ArrayUtilRt
+import kotlin.jvm.JvmStatic
 
 object NameUtilCore {
   private const val KANA_START = 0x3040
@@ -12,7 +12,7 @@ object NameUtilCore {
   /**
    * Splits an identifier into words, separated with underscores or upper-case characters
    * (camel-case).
-   * 
+   *
    * @param name the identifier to split.
    * @return the list of strings into which the identifier has been split.
    */
@@ -44,22 +44,22 @@ object NameUtilCore {
    
     """)
   fun splitNameIntoWords(name: String): Array<String> {
-    return ArrayUtilRt.toStringArray(splitNameIntoWordList(name))
+    return splitNameIntoWordList(name).toTypedArray()
   }
 
   @JvmStatic
   fun nextWord(text: String, start: Int): Int {
-    val ch = text.codePointAt(start)
-    val chLen = Character.charCount(ch)
-    if (!Character.isLetterOrDigit(ch)) {
+    val ch = codePointAt(text, start)
+    val chLen = ch.charCount
+    if (!ch.isLetterOrDigit()) {
       return start + chLen
     }
 
     var i = start
     while (i < text.length) {
-      val codePoint = text.codePointAt(i)
-      if (!Character.isDigit(codePoint)) break
-      i += Character.charCount(codePoint)
+      val codePoint = codePointAt(text, i)
+      if (!codePoint.isDigit()) break
+      i += codePoint.charCount
     }
     if (i > start) {
       // digits form a separate hump
@@ -67,73 +67,73 @@ object NameUtilCore {
     }
 
     while (i < text.length) {
-      val codePoint = text.codePointAt(i)
-      if (!Character.isUpperCase(codePoint)) break
-      i += Character.charCount(codePoint)
+      val codePoint = codePointAt(text, i)
+      if (!codePoint.isUpperCase()) break
+      i += codePoint.charCount
     }
 
     if (i > start + chLen) {
       // several consecutive uppercase letters form a hump
-      if (i == text.length || !Character.isLetter(text.codePointAt(i))) {
+      if (i == text.length || !codePointAt(text, i).isLetter()) {
         return i
       }
-      return i - Character.charCount(text.codePointBefore(i))
+      return i - codePointBefore(text, i).charCount
     }
 
     if (i == start) i += chLen
     while (i < text.length) {
-      val codePoint = text.codePointAt(i)
-      if (!Character.isLetter(codePoint) || isWordStart(text, i)) break
-      i += Character.charCount(codePoint)
+      val codePoint = codePointAt(text, i)
+      if (!codePoint.isLetter() || isWordStart(text, i)) break
+      i += codePoint.charCount
     }
     return i
   }
 
   @JvmStatic
   fun isWordStart(text: String, i: Int): Boolean {
-    val cur = text.codePointAt(i)
-    val prev = if (i > 0) text.codePointBefore(i) else -1
-    if (Character.isUpperCase(cur)) {
-      if (Character.isUpperCase(prev)) {
+    val cur = codePointAt(text, i)
+    val prev = if (i > 0) codePointBefore(text, i) else null
+    if (cur.isUpperCase()) {
+      if (prev != null && prev.isUpperCase()) {
         // check that we're not in the middle of an all-caps word
-        val nextPos = i + Character.charCount(cur)
-        return nextPos < text.length && Character.isLowerCase(text.codePointAt(nextPos))
+        val nextPos = i + cur.charCount
+        return nextPos < text.length && codePointAt(text, nextPos).isLowerCase()
       }
       return true
     }
-    if (Character.isDigit(cur)) {
+    if (cur.isDigit()) {
       return true
     }
-    if (!Character.isLetter(cur)) {
+    if (!cur.isLetter()) {
       return false
     }
-    if (Character.isIdeographic(cur)) {
+    if (cur.isIdeographic()) {
       // Consider every ideograph as a separate word
       return true
     }
-    return i == 0 || !text[i - 1].isLetterOrDigit() || isKanaBreak(cur, prev)
+    return i == 0 || !text[i - 1].isLetterOrDigit() || (prev != null && isKanaBreak(cur, prev))
   }
 
-  private fun maybeKana(codePoint: Int): Boolean {
-    return codePoint in KANA_START..KANA_END ||
-           codePoint in KANA2_START..KANA2_END
+  private fun maybeKana(codePoint: CodePoint): Boolean {
+    return codePoint.codePoint in KANA_START..KANA_END ||
+           codePoint.codePoint in KANA2_START..KANA2_END
   }
 
-  private fun isKanaBreak(cur: Int, prev: Int): Boolean {
+  private fun isKanaBreak(cur: CodePoint, prev: CodePoint): Boolean {
     if (!maybeKana(cur) && !maybeKana(prev)) return false
-    val curScript = Character.UnicodeScript.of(cur)
-    val prevScript = Character.UnicodeScript.of(prev)
+    val curScript = cur.unicodeScript
+    val prevScript = prev.unicodeScript
     if (prevScript == curScript) return false
-    return (curScript == Character.UnicodeScript.KATAKANA ||
-            curScript == Character.UnicodeScript.HIRAGANA ||
-            prevScript == Character.UnicodeScript.KATAKANA ||
-            prevScript == Character.UnicodeScript.HIRAGANA) && prevScript != Character.UnicodeScript.COMMON && curScript != Character.UnicodeScript.COMMON
+    return (curScript == UnicodeScript.KATAKANA ||
+            curScript == UnicodeScript.HIRAGANA ||
+            prevScript == UnicodeScript.KATAKANA ||
+            prevScript == UnicodeScript.HIRAGANA) && prevScript != UnicodeScript.COMMON && curScript != UnicodeScript.COMMON
   }
 
   @JvmStatic
   @Deprecated("use {@link #nameToWordList(String)} to avoid redundant allocations")
   fun nameToWords(name: String): Array<String> {
-    return ArrayUtilRt.toStringArray(nameToWordList(name))
+    return nameToWordList(name).toTypedArray()
   }
 
   @JvmStatic
