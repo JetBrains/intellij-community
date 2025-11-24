810a4aa0bdcf5f07fbe40c652ec2c465d3d89f70
Victoria.Petrakovich <victoria.petrakovich@jetbrains.com>
2025-11-24 18:35:33 +0100

    [kotlin] KTIJ-36269 Rename `kotlin.project.configuration` collector to `kotlin.project.setup`
    GitOrigin-RevId: aeee83fb1dae73258c7c7e02e3b429a6065ac866

--- plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/statistics/ProjectSetupCollector.kt
+++ plugins/kotlin/project-configuration/src/org/jetbrains/kotlin/idea/statistics/ProjectSetupCollector.kt
@@ -25,7 +25,7 @@ import org.jetbrains.kotlin.idea.facet.KotlinFacetType
 import org.jetbrains.kotlin.konan.target.KonanTarget
 import java.util.*
 
-internal class ProjectConfigurationCollector : ProjectUsagesCollector() {
+internal class ProjectSetupCollector : ProjectUsagesCollector() {
     override fun getGroup(): EventLogGroup = GROUP
 
     override fun getMetrics(project: Project): Set<MetricEvent> {
@@ -82,7 +82,7 @@ internal class ProjectConfigurationCollector : ProjectUsagesCollector() {
         }
     }
 
-    private val GROUP = EventLogGroup("kotlin.project.configuration", 54)
+    private val GROUP = EventLogGroup("kotlin.project.setup", 1)
 
     private val systemField = EventFields.String("system", listOf("JPS", "Maven", "Gradle", "unknown"))
     private val platformField = EventFields.String("platform", composePlatformFields())
