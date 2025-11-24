36bb4565ca7b34e68fdf0235b67a690f820c7ae0
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2025-11-24 18:05:40 +0100

    refactor [java]: Don't use StdFileTypes
    GitOrigin-RevId: 4e026b9898db8ddb8b407235f154121f472832f8

--- java/idea-ui/testSrc/com/intellij/facet/mock/MockFacetDetector.java
+++ java/idea-ui/testSrc/com/intellij/facet/mock/MockFacetDetector.java
@@ -3,9 +3,9 @@ package com.intellij.facet.mock;
 
 import com.intellij.framework.detection.FacetBasedFrameworkDetector;
 import com.intellij.framework.detection.FileContentPattern;
+import com.intellij.ide.highlighter.XmlFileType;
 import com.intellij.openapi.extensions.InternalIgnoreDependencyViolation;
 import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -68,7 +68,7 @@ public final class MockFacetDetector extends FacetBasedFrameworkDetector<MockFac
   @NotNull
   @Override
   public FileType getFileType() {
-    return StdFileTypes.XML;
+    return XmlFileType.INSTANCE;
   }
 
   @NotNull
