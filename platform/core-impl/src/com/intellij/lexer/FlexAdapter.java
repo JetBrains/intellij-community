13afc47a1ae9eed220846115e9b6260d5a4b2a11
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-11-24 15:18:50 +0100

    Revert "IJPL-212853 FlexAdapter: store buffer end position in the LexerPosition"
    This reverts commit 3c7f192c2649b868581b9628ad822a48ccbf887c.
    
    GitOrigin-RevId: 9a372eaa219e5719e28ae492814e908d974f51f2

--- platform/core-impl/src/com/intellij/lexer/FlexAdapter.java
+++ platform/core-impl/src/com/intellij/lexer/FlexAdapter.java
@@ -84,13 +84,13 @@ public class FlexAdapter extends LexerBase {
   @Override
   public @NotNull LexerPosition getCurrentPosition() {
     locateToken();
-    return new FlexAdapterPosition(myTokenType, myTokenStart, myTokenEnd, myState, myFlex.yystate(), myBufferEnd);
+    return new FlexAdapterPosition(myTokenType, myTokenStart, myTokenEnd, myState, myFlex.yystate());
   }
 
   @Override
   public void restore(@NotNull LexerPosition position) {
     FlexAdapterPosition flexAdapterPosition = (FlexAdapterPosition)position;
-    myFlex.reset(myText, flexAdapterPosition.tokenEnd, flexAdapterPosition.bufferEnd, flexAdapterPosition.flexState);
+    myFlex.reset(myText, flexAdapterPosition.tokenEnd, getBufferEnd(), flexAdapterPosition.flexState);
     myTokenStart = flexAdapterPosition.tokenStart;
     myTokenEnd = flexAdapterPosition.tokenEnd;
     myTokenType = flexAdapterPosition.currentToken;
@@ -130,15 +130,13 @@ public class FlexAdapter extends LexerBase {
     private final int tokenEnd;
     private final int state;
     private final int flexState;
-    private final int bufferEnd;
 
-    private FlexAdapterPosition(IElementType currentToken, int tokenStart, int tokenEnd, int state, int flexState, int bufferEnd) {
+    private FlexAdapterPosition(IElementType currentToken, int tokenStart, int tokenEnd, int state, int flexState) {
       this.currentToken = currentToken;
       this.tokenStart = tokenStart;
       this.tokenEnd = tokenEnd;
       this.state = state;
       this.flexState = flexState;
-      this.bufferEnd = bufferEnd;
     }
 
     @Override
