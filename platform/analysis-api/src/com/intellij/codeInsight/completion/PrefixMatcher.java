3c24c8e3986c079a87fbf5eb42e59d3451b73559
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-11-24 11:07:04 +0100

    doc [completion]: add documentation for PrefixMatcher
    GitOrigin-RevId: 79900d83f9c406050f32b2724ff0ecbb48ab5f95

--- platform/analysis-api/src/com/intellij/codeInsight/completion/PrefixMatcher.java
+++ platform/analysis-api/src/com/intellij/codeInsight/completion/PrefixMatcher.java
@@ -32,6 +32,9 @@ public abstract class PrefixMatcher {
     myPrefix = prefix;
   }
 
+  /**
+   * @return true if {@code element} matches this prefix matcher, false otherwise.
+   */
   public boolean prefixMatches(@NotNull LookupElement element) {
     for (String s : element.getAllLookupStrings()) {
       if (prefixMatches(s)) {
@@ -41,6 +44,9 @@ public abstract class PrefixMatcher {
     return false;
   }
 
+  /**
+   * @return true if {@code element} matches this prefix matcher and the matched substring is at the start of the lookup string.
+   */
   public boolean isStartMatch(@NotNull LookupElement element) {
     for (String s : element.getAllLookupStrings()) {
       if (isStartMatch(s)) {
@@ -50,35 +56,56 @@ public abstract class PrefixMatcher {
     return false;
   }
 
+  /**
+   * @return true if {@code name} matches this prefix matcher and the matched substring is at the start of the lookup string.
+   */
   public boolean isStartMatch(@NotNull String name) {
     return prefixMatches(name);
   }
 
+  /**
+   * @return true if {@code name} matches this prefix matcher.
+   */
   public abstract boolean prefixMatches(@NotNull String name);
 
+  /**
+   * @return the prefix used for matching.
+   */
   public final @NotNull String getPrefix() {
     return myPrefix;
   }
 
+  /**
+   * @return the same-logic prefix matcher but using {@code prefix} for matching.
+   */
   public abstract @NotNull PrefixMatcher cloneWithPrefix(@NotNull String prefix);
 
+  /**
+   * @return matching degree of the given name. The bigger result, the better match.
+   * <p>
+   * Used by sorting algorithms to show better matches higher in the completion list.
+   */
   public int matchingDegree(@NotNull String name) {
     return 0;
   }
 
   /**
-   * Filters _names for strings that match given matcher and sorts them.
+   * Filters {@code names} for strings that match given matcher and sorts them.
    * "Start matching" items go first, then others.
    * Within both groups, names are sorted lexicographically in a case-insensitive way.
+   * <p>
+   * IntelliJ completion machinery ignores new results after a certain limit is reached, see {@code ide.completion.variant.limit} registry key.
+   * Thus, if your {@link CompletionContributor} wants to provide a potentially large number of items, it should pass more relevant ones first.
+   * Use {@code sortMatching} to sort them in the desired order.
    */
-  public @NotNull LinkedHashSet<String> sortMatching(Collection<String> _names) {
+  public @NotNull LinkedHashSet<String> sortMatching(@NotNull Collection<@NotNull String> names) {
     ProgressManager.checkCanceled();
     if (getPrefix().isEmpty()) {
-      return new LinkedHashSet<>(_names);
+      return new LinkedHashSet<>(names);
     }
 
     List<String> sorted = new ArrayList<>();
-    for (String name : _names) {
+    for (String name : names) {
       if (prefixMatches(name)) {
         sorted.add(name);
       }
@@ -101,6 +128,9 @@ public abstract class PrefixMatcher {
     return result;
   }
 
+  /**
+   * @return a list of text ranges in the given name that match the prefix.
+   */
   public @Nullable List<@NotNull TextRange> getMatchingFragments(@NotNull String name) {
     return null;
   }
