4f4d8658f5f37e61743f0f652331021a72dcd721
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-11-24 14:39:25 +0100

    IJPL-219674 add xmlValue to ModuleLoadingRuleValue enum
    GitOrigin-RevId: 1e84f6c22e166e9efe559ec2e1353ce3084a6456

--- platform/plugins/parser/impl/src/com/intellij/platform/plugins/parser/impl/elements/ModuleLoadingRuleValue.kt
+++ platform/plugins/parser/impl/src/com/intellij/platform/plugins/parser/impl/elements/ModuleLoadingRuleValue.kt
@@ -1,6 +1,11 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.plugins.parser.impl.elements
 
-enum class ModuleLoadingRuleValue {
-  REQUIRED, EMBEDDED, OPTIONAL, ON_DEMAND
+import com.intellij.platform.plugins.parser.impl.PluginXmlConst
+
+enum class ModuleLoadingRuleValue(val xmlValue: String) {
+  REQUIRED(PluginXmlConst.CONTENT_MODULE_LOADING_REQUIRED_VALUE),
+  EMBEDDED(PluginXmlConst.CONTENT_MODULE_LOADING_EMBEDDED_VALUE),
+  OPTIONAL(PluginXmlConst.CONTENT_MODULE_LOADING_OPTIONAL_VALUE),
+  ON_DEMAND(PluginXmlConst.CONTENT_MODULE_LOADING_ON_DEMAND_VALUE),
 }
