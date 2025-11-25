ab5587e6034eaebc8071b2d749c78bd2c30e12c2
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-11-25 14:20:38 +0100

    [actions] IJPL-219724: Hide `RunAnythingPopupUI` under `@Internal`
    (cherry picked from commit fc9721a636c211987128385bb22e70979934e998)
    
    IJ-CR-183625
    
    GitOrigin-RevId: 0416af4d441a5eb021d9ef0c863d49f0e1765f24

--- platform/lang-impl/src/com/intellij/ide/actions/runAnything/RunAnythingPopupUI.java
+++ platform/lang-impl/src/com/intellij/ide/actions/runAnything/RunAnythingPopupUI.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.actions.runAnything;
 
 import com.intellij.execution.Executor;
@@ -57,10 +57,7 @@ import com.intellij.util.concurrency.SequentialTaskExecutor;
 import com.intellij.util.concurrency.ThreadingAssertions;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.*;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.Unmodifiable;
+import org.jetbrains.annotations.*;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -79,6 +76,7 @@ import static com.intellij.ide.actions.runAnything.RunAnythingIconHandler.MATCHE
 import static com.intellij.ide.actions.runAnything.RunAnythingSearchListModel.RunAnythingMainListModel;
 import static com.intellij.openapi.wm.IdeFocusManager.getGlobalInstance;
 
+@ApiStatus.Internal
 public final class RunAnythingPopupUI extends BigPopupUI {
   public static final int SEARCH_FIELD_COLUMNS = 25;
   public static final Icon UNKNOWN_CONFIGURATION_ICON = AllIcons.Actions.RunAnything;
