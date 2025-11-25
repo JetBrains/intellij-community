2458ede42b8baf973ec9c6750ea3aa293ce28c00
Aleksander Movsesov <aleksander.movsesov@jetbrains.com>
2025-11-25 12:40:35 +0100

    QD-12413: bundle code provenance to IntelliJ IDEA
    (cherry picked from commit 669ed3e8e999d84d02928d670534f727fffcc775)
    
    IJ-MR-183517
    
    (cherry picked from commit 2f3b8638d42851e2edb2dbf870b5327ccca77012)
    
    GitOrigin-RevId: 3946ff03906f9abda3ce19165aef7b9ce30f5773

--- platform/build-scripts/src/org/jetbrains/intellij/build/BaseIdeaProperties.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/BaseIdeaProperties.kt
@@ -59,6 +59,7 @@ val IDEA_BUNDLED_PLUGINS: PersistentList<String> = DEFAULT_BUNDLED_PLUGINS + seq
   "intellij.keymap.netbeans",
   "intellij.performanceTesting",
   "intellij.compose.ide.plugin",
+  "intellij.code.provenance"
 )
 
 val CE_CLASS_VERSIONS: Map<String, String> = mapOf(
