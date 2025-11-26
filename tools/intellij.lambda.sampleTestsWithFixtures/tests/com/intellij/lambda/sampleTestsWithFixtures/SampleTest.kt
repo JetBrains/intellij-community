910bd0ed2db50a526d6136dc530bfb9ac7df19c7
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2025-11-26 16:42:26 +0100

    AT-3038: add names for steps
    # Conflicts:
    #	community/tools/intellij.lambda.sampleTestsWithFixtures/tests/com/intellij/lambda/sampleTestsWithFixtures/SampleTest.kt
    
    GitOrigin-RevId: 7937924b7db53e2532f5bbcc939ae6a50d4321fc

--- tools/intellij.lambda.sampleTestsWithFixtures/tests/com/intellij/lambda/sampleTestsWithFixtures/SampleTest.kt
+++ tools/intellij.lambda.sampleTestsWithFixtures/tests/com/intellij/lambda/sampleTestsWithFixtures/SampleTest.kt
@@ -1,31 +1,32 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.lambda.sampleTestsWithFixtures
 
-import com.intellij.lambda.sampleTestsWithFixtures.util.openNewEditor
+import com.intellij.lambda.sampleTestsWithFixtures.util.openNewProjectAndEditor
 import com.intellij.lambda.testFramework.junit.RunInMonolithAndSplitMode
 import com.intellij.lambda.testFramework.testApi.editor.openFile
-import com.intellij.lambda.testFramework.testApi.getProject
 import com.intellij.lambda.testFramework.testApi.getProjects
+import com.intellij.lambda.testFramework.testApi.waitForProject
 import com.intellij.lambda.testFramework.utils.BackgroundRunWithLambda
 import com.intellij.openapi.diagnostic.Logger
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.TestTemplate
+import kotlin.time.Duration.Companion.seconds
 
 @RunInMonolithAndSplitMode
 class SampleTest {
   @TestTemplate
   fun `serialized test`(ide: BackgroundRunWithLambda) = runBlocking {
     ide.apply {
-      runInBackend {
+      runInBackend("Wait for project") {
         waitForProject(20.seconds)
       }
 
-      run {
-        openNewEditor("/src/com/example/Foo.java")
+      runInBackend("Open project via fixture") {
+        openNewProjectAndEditor("/src/com/example/Foo.java")
         Logger.getInstance("test").warn("Projects: " + getProjects().joinToString { it.name })
       }
 
-      runInBackend {
+      run("Open File in Project") {
         Logger.getInstance("test").warn("Projects: " + getProjects().joinToString { it.name })
         openFile("src/com/example/Foo.java", project = getProjects().single { it.name == "Test" }, waitForReadyState = false, requireFocus = false)
       }
