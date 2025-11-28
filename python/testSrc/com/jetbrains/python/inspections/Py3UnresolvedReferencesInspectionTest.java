390852d346ebb09f217d3d4aa24bd18b71b718e3
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2025-11-28 14:26:31 +0100

    PY-85880 Properly handle right-hand side calls in PyUnresolvedReferencesVisitor for strict unions
    (cherry picked from commit a2d03ad6c6f9fb78c78eef3a3ada5509dd7dc4aa)
    
    GitOrigin-RevId: 116bd552c2d864b8a098d4118b268170d4dcc2e4

--- python/testSrc/com/jetbrains/python/inspections/Py3UnresolvedReferencesInspectionTest.java
+++ python/testSrc/com/jetbrains/python/inspections/Py3UnresolvedReferencesInspectionTest.java
@@ -27,6 +27,7 @@ import com.jetbrains.python.psi.PyClass;
 import com.jetbrains.python.psi.PyFile;
 import com.jetbrains.python.psi.stubs.PyClassNameIndex;
 import org.jetbrains.annotations.NotNull;
+import org.junit.ComparisonFailure;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -485,6 +486,31 @@ public class Py3UnresolvedReferencesInspectionTest extends PyInspectionTestCase 
         configureInspection();
         assertSdkRootsNotParsed(currentFile);
       });
+    });
+  }
+
+  // PY-85880
+  public void testLiteralUnionInTuple() {
+    doTestByText("""
+                   from typing import Literal
+                   
+                   
+                   def f(e: Literal[1, 2]):
+                       _ = e in ()
+                   """);
+  }
+
+  // PY-85880
+  public void testLiteralInUnionTupleNone() {
+    fixme("PY-85880", ComparisonFailure.class, () -> {
+      doTestByText("""
+                     from typing import Literal
+                     
+                     
+                     def f(e: Literal[1, 2]):
+                         a: tuple | None = None
+                         _ = e <weak_warning descr="Member 'None' of 'tuple | None' does not have attribute '__contains__'">in</weak_warning> a
+                     """);
     });
   }
 }
