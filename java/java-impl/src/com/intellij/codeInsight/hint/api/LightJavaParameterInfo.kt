cf9d0543e17deac804f3386ff5637449659f7c0d
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2025-11-28 16:52:37 +0100

    [Java] LSP-287 Refactoring LightJavaParameterInfo: better description and name for signature list
    GitOrigin-RevId: cabb9b3e07c107f02f01a96cacf52ce3ffeb3129

--- java/java-impl/src/com/intellij/codeInsight/hint/api/LightJavaParameterInfo.kt
+++ java/java-impl/src/com/intellij/codeInsight/hint/api/LightJavaParameterInfo.kt
@@ -4,12 +4,12 @@ package com.intellij.codeInsight.hint.api
 import com.intellij.util.Range
 
 /**
- * Stores information about available signatures for the current context. The context means here the place where `Signature Help`
- * action is invoked.
+ * Stores information about available signatures for the current context. The context means here the place where `Parameter Info`
+ * action or similar is invoked.
  * @see LightJavaParameterInfoHandler
  */
 public class LightJavaParameterInfo(
-  public val methodPresentationList: List<LightJavaSignaturePresentation>,
+  public val signaturePresentationList: List<LightJavaSignaturePresentation>,
   public val currentSignatureIndex: Int?,
   public val currentParameterIndex: Int?,
 )
