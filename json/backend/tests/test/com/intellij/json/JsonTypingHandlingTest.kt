d805fe53dbbd0ea97d11b506653605410a90f2c8
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-11-30 12:57:10 +0100

    refactor [json]: convert JsonTypingHandlingTest to Kotlin
    GitOrigin-RevId: 75061aed2b7e0d5f776179586efe0bc2be634c20

--- json/backend/tests/test/com/intellij/json/JsonTypingHandlingTest.kt
+++ json/backend/tests/test/com/intellij/json/JsonTypingHandlingTest.kt
@@ -1,291 +1,330 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.json;
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.json
 
-import com.intellij.codeInsight.CodeInsightSettings;
-import com.intellij.json.editor.JsonEditorOptions;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.codeInsight.CodeInsightSettings
+import com.intellij.json.editor.JsonEditorOptions
 
-public class JsonTypingHandlingTest extends JsonTestCase {
-  private void doTestEnter(@NotNull final String before, @NotNull final String expected) {
-    doTypingTest('\n', before, expected, "json");
-  }
-  private void doTestLBrace(@NotNull final String before, @NotNull final String expected) {
-    doTypingTest('{', before, expected, "json");
-  }
-  private void doTestLBracket(@NotNull final String before, @NotNull final String expected) {
-    doTypingTest('[', before, expected, "json");
-  }
-  private void doTestQuote(@NotNull final String before, @NotNull final String expected) {
-    doTypingTest('"', before, expected, "json");
-  }
-  private void doTestSingleQuote(@NotNull final String before, @NotNull final String expected) {
-    doTypingTest('\'', before, expected, "json");
-  }
-  private void doTestColon(@NotNull final String before, @NotNull final String expected) {
-    doTypingTest(':', before, expected, "json");
-  }
-  private void doTestComma(@NotNull final String before, @NotNull final String expected) {
-    doTypingTest(',', before, expected, "json");
+@Suppress("SameParameterValue")
+class JsonTypingHandlingTest : JsonTestCase() {
+  private fun doTestEnter(before: String, expected: String) {
+    doTypingTest('\n', before, expected, "json")
   }
 
-  @SuppressWarnings("SameParameterValue")
-  private void doTypingTest(char c,
-                            @NotNull String before,
-                            @NotNull String expected,
-                            @NotNull String extension) {
-    myFixture.configureByText("test." + extension, before);
-    myFixture.type(c);
-    myFixture.checkResult(expected);
+  private fun doTestLBrace(before: String, expected: String) {
+    doTypingTest('{', before, expected, "json")
   }
 
-  @SuppressWarnings("SameParameterValue")
-  private void doTypingTest(String s,
-                            @NotNull String before,
-                            @NotNull String expected,
-                            @NotNull String extension) {
-    myFixture.configureByText("test." + extension, before);
-    myFixture.type(s);
-    myFixture.checkResult(expected);
+  private fun doTestLBracket(before: String, expected: String) {
+    doTypingTest('[', before, expected, "json")
+  }
+
+  private fun doTestQuote(before: String, expected: String) {
+    doTypingTest('"', before, expected, "json")
+  }
+
+  private fun doTestSingleQuote(before: String, expected: String) {
+    doTypingTest('\'', before, expected, "json")
+  }
+
+  private fun doTestColon(before: String, expected: String) {
+    doTypingTest(':', before, expected, "json")
+  }
+
+  private fun doTestComma(before: String, expected: String) {
+    doTypingTest(',', before, expected, "json")
+  }
+
+  private fun doTypingTest(
+    c: Char,
+    before: String,
+    expected: String,
+    extension: String
+  ) {
+    myFixture.configureByText("test.$extension", before)
+    myFixture.type(c)
+    myFixture.checkResult(expected)
+  }
+
+  private fun doTypingTest(
+    s: String,
+    before: String,
+    expected: String,
+    extension: String
+  ) {
+    myFixture.configureByText("test.$extension", before)
+    myFixture.type(s)
+    myFixture.checkResult(expected)
   }
 
   // JsonEnterHandler
-  public void testEnterAfterProperty() {
-    doTestEnter("{\"a\": true<caret>\n}", "{\"a\": true,\n  <caret>\n}");
+  fun testEnterAfterProperty() {
+    doTestEnter("{\"a\": true<caret>\n}", "{\"a\": true,\n  <caret>\n}")
   }
-  public void testEnterMidProperty() {
-    doTestEnter("{\"a\": tr<caret>ue}", "{\"a\": true,\n}");
+
+  fun testEnterMidProperty() {
+    doTestEnter("{\"a\": tr<caret>ue}", "{\"a\": true,\n}")
   }
-  public void testEnterMidObjectNoFollowing() {
-    doTestEnter("{\"a\": {<caret>}}", "{\"a\": {\n  \n}}");
+
+  fun testEnterMidObjectNoFollowing() {
+    doTestEnter("{\"a\": {<caret>}}", "{\"a\": {\n  \n}}")
   }
-  public void testEnterMidObjectWithFollowing() {
-    doTestEnter("{\"a\": {<caret>} \"b\": 5}", "{\"a\": {\n  \n}, \"b\": 5}");
+
+  fun testEnterMidObjectWithFollowing() {
+    doTestEnter("{\"a\": {<caret>} \"b\": 5}", "{\"a\": {\n  \n}, \"b\": 5}")
   }
-  public void testEnterAfterObject() {
-    doTestEnter("{\"a\": {}<caret>\n}", "{\"a\": {},\n  <caret>\n}");
+
+  fun testEnterAfterObject() {
+    doTestEnter("{\"a\": {}<caret>\n}", "{\"a\": {},\n  <caret>\n}")
   }
 
   // JsonTypedHandler
-  public void testAutoCommaAfterLBraceInArray() {
-    doTestLBrace("[ <caret> {\"a\": 5} ]", "[ {}, {\"a\": 5} ]");
+  fun testAutoCommaAfterLBraceInArray() {
+    doTestLBrace("[ <caret> {\"a\": 5} ]", "[ {}, {\"a\": 5} ]")
   }
-  public void testAutoCommaAfterLBracketInArray() {
-    doTestLBracket("[ <caret> {\"a\": 5} ]", "[ [], {\"a\": 5} ]");
+
+  fun testAutoCommaAfterLBracketInArray() {
+    doTestLBracket("[ <caret> {\"a\": 5} ]", "[ [], {\"a\": 5} ]")
   }
-  public void testAutoCommaAfterQuoteInArray() {
-    doTestQuote("[ <caret> {\"a\": 5} ]", "[ \"\", {\"a\": 5} ]");
+
+  fun testAutoCommaAfterQuoteInArray() {
+    doTestQuote("[ <caret> {\"a\": 5} ]", "[ \"\", {\"a\": 5} ]")
   }
-  public void testAutoCommaAfterLBraceInObject() {
-    doTestLBrace("{ \"x\": <caret> \"y\": {\"a\": 5} }", "{ \"x\": {}, \"y\": {\"a\": 5} }");
+
+  fun testAutoCommaAfterLBraceInObject() {
+    doTestLBrace("{ \"x\": <caret> \"y\": {\"a\": 5} }", "{ \"x\": {}, \"y\": {\"a\": 5} }")
   }
-  public void testAutoCommaAfterLBracketInObject() {
-    doTestLBracket("{ \"x\": <caret> \"y\": {\"a\": 5} }", "{ \"x\": [], \"y\": {\"a\": 5} }");
+
+  fun testAutoCommaAfterLBracketInObject() {
+    doTestLBracket("{ \"x\": <caret> \"y\": {\"a\": 5} }", "{ \"x\": [], \"y\": {\"a\": 5} }")
   }
-  public void testAutoCommaAfterQuoteInObject() {
-    doTestQuote("{ \"x\": <caret> \"y\": {\"a\": 5} }", "{ \"x\": \"\", \"y\": {\"a\": 5} }");
+
+  fun testAutoCommaAfterQuoteInObject() {
+    doTestQuote("{ \"x\": <caret> \"y\": {\"a\": 5} }", "{ \"x\": \"\", \"y\": {\"a\": 5} }")
   }
-  public void testAutoQuotesForPropName() {
+
+  fun testAutoQuotesForPropName() {
     doTestColon("{ x<caret>}", """
       {
         "x": <caret>
-      }""");
+      }
+      """.trimIndent())
   }
-  public void testAutoQuotesForPropNameFalse1() {
-    doTestColon( "{ \"x\"<caret>}", "{ \"x\": <caret>}");
+
+  fun testAutoQuotesForPropNameFalse1() {
+    doTestColon("{ \"x\"<caret>}", "{ \"x\": <caret>}")
   }
-  public void testAutoQuotesForPropNameFalse2() {
-    doTestColon( "{ \"x<caret>\"}", "{ \"x:<caret>\"}");
+
+  fun testAutoQuotesForPropNameFalse2() {
+    doTestColon("{ \"x<caret>\"}", "{ \"x:<caret>\"}")
   }
-  public void testAutoQuotesAndWhitespaceFollowingNewline() {
+
+  fun testAutoQuotesAndWhitespaceFollowingNewline() {
     doTestColon("""
                   {
                    "a": 5,
                    x<caret>
                    "q": 8
-                  }""",
+                  }
+                  """.trimIndent(),
                 """
                   {
                    "a": 5,
                     "x": <caret>
                    "q": 8
-                  }""");
+                  }
+                  """.trimIndent())
   }
 
-  public void testAutoWhitespaceErasure() {
-    myFixture.configureByText("test.json", "{a<caret>}");
-    myFixture.type(":");
-    myFixture.type(" ");
+  fun testAutoWhitespaceErasure() {
+    myFixture.configureByText("test.json", "{a<caret>}")
+    myFixture.type(":")
+    myFixture.type(" ")
     myFixture.checkResult("""
                             {
                               "a": <caret>
-                            }""");
+                            }
+                            """.trimIndent())
   }
 
-  public void testPairedSingleQuote() {
-    doTypingTest('\'', "{<caret>}", "{'<caret>'}", "json");
+  fun testPairedSingleQuote() {
+    doTypingTest('\'', "{<caret>}", "{'<caret>'}", "json")
   }
-  public void testPairedSingleQuote2() {
+
+  fun testPairedSingleQuote2() {
     doTypingTest('\'', """
       {
         "rules": {
           "at-rule-no-vendor-prefix": null,
           <caret>
         }
-      }""", """
+      }
+      """.trimIndent(), """
                    {
                      "rules": {
                        "at-rule-no-vendor-prefix": null,
                        '<caret>'
                      }
-                   }""", "json");
+                   }
+                   """.trimIndent(), "json")
   }
 
-  private static void testWithPairQuotes(boolean on, Runnable test) {
-    CodeInsightSettings settings = CodeInsightSettings.getInstance();
-    boolean oldQuote = settings.AUTOINSERT_PAIR_QUOTE;
-    try {
-      settings.AUTOINSERT_PAIR_QUOTE = on;
-      test.run();
-    }
-    finally {
-      settings.AUTOINSERT_PAIR_QUOTE = oldQuote;
-    }
-  }
-
-  public void testNoCommaInNextQuotes() {
+  fun testNoCommaInNextQuotes() {
     testWithPairQuotes(false,
-                       () -> doTypingTest("\"ccc\": \"", "{<caret>\"aaa\": \"bbb\"}", "{\"ccc\": \"<caret>\"aaa\": \"bbb\"}", "json"));
+                       Runnable {
+                         doTypingTest("\"ccc\": \"", "{<caret>\"aaa\": \"bbb\"}", "{\"ccc\": \"<caret>\"aaa\": \"bbb\"}", "json")
+                       })
   }
 
-  public void testNoCommaAfterArray() {
-    testWithPairQuotes(false, () ->
+  fun testNoCommaAfterArray() {
+    testWithPairQuotes(false, Runnable {
       doTypingTest('"', """
         [
           {"aaa": [<caret>]},
           {}
-        ]""", """
+        ]
+        """.trimIndent(), """
                      [
                        {"aaa": ["<caret>]},
                        {}
-                     ]""", "json"));
+                     ]
+                     """.trimIndent(), "json")
+    })
   }
 
-  public void testAddCommaWithPairedQuotes() {
-    testWithPairQuotes(true, () -> doTypingTest("\"ccc\": \"", "{<caret>\"aaa\": \"bbb\"}", "{\"ccc\": \"<caret>\",\"aaa\": \"bbb\"}", "json"));
+  fun testAddCommaWithPairedQuotes() {
+    testWithPairQuotes(true, Runnable {
+      doTypingTest("\"ccc\": \"", "{<caret>\"aaa\": \"bbb\"}", "{\"ccc\": \"<caret>\",\"aaa\": \"bbb\"}", "json")
+    })
   }
 
-  public void testNoCommaIfRBraceAndNoNewline() {
+  fun testNoCommaIfRBraceAndNoNewline() {
     doTestEnter("""
                   {
                     "x": 5<caret>}
-                  """, """
+                  
+                  """.trimIndent(), """
                   {
                     "x": 5
                   }
-                  """);
+                  
+                  """.trimIndent())
   }
 
-  public void testMoveColon() {
-    JsonEditorOptions editorOptions = JsonEditorOptions.getInstance();
-    boolean oldQuote = editorOptions.COLON_MOVE_OUTSIDE_QUOTES;
+  fun testMoveColon() {
+    val editorOptions = JsonEditorOptions.getInstance()
+    val oldQuote = editorOptions.COLON_MOVE_OUTSIDE_QUOTES
     try {
-      editorOptions.COLON_MOVE_OUTSIDE_QUOTES = true;
-      doTestColon("{\"x<caret>\"}", "{\"x\": <caret>}");
+      editorOptions.COLON_MOVE_OUTSIDE_QUOTES = true
+      doTestColon("{\"x<caret>\"}", "{\"x\": <caret>}")
     }
     finally {
-      editorOptions.COLON_MOVE_OUTSIDE_QUOTES = oldQuote;
+      editorOptions.COLON_MOVE_OUTSIDE_QUOTES = oldQuote
     }
   }
 
-  public void testMoveComma() {
-    JsonEditorOptions editorOptions = JsonEditorOptions.getInstance();
-    boolean oldQuote = editorOptions.COMMA_MOVE_OUTSIDE_QUOTES;
+  fun testMoveComma() {
+    val editorOptions = JsonEditorOptions.getInstance()
+    val oldQuote = editorOptions.COMMA_MOVE_OUTSIDE_QUOTES
     try {
-      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = true;
-      doTestComma("{\"x\": \"value<caret>\"}", "{\"x\": \"value\",<caret>}");
+      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = true
+      doTestComma("{\"x\": \"value<caret>\"}", "{\"x\": \"value\",<caret>}")
     }
     finally {
-      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = oldQuote;
+      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = oldQuote
     }
   }
 
-  public void testMoveCommaForArray() {
-    JsonEditorOptions editorOptions = JsonEditorOptions.getInstance();
-    boolean oldQuote = editorOptions.COMMA_MOVE_OUTSIDE_QUOTES;
+  fun testMoveCommaForArray() {
+    val editorOptions = JsonEditorOptions.getInstance()
+    val oldQuote = editorOptions.COMMA_MOVE_OUTSIDE_QUOTES
     try {
-      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = true;
-      doTestComma("{\"x\": [\"value<caret>\"]}", "{\"x\": [\"value\",<caret>]}");
+      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = true
+      doTestComma("{\"x\": [\"value<caret>\"]}", "{\"x\": [\"value\",<caret>]}")
     }
     finally {
-      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = oldQuote;
+      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = oldQuote
     }
   }
 
-  public void testDoNotMoveColonIfColon() {
-    JsonEditorOptions editorOptions = JsonEditorOptions.getInstance();
-    boolean oldQuote = editorOptions.COLON_MOVE_OUTSIDE_QUOTES;
+  fun testDoNotMoveColonIfColon() {
+    val editorOptions = JsonEditorOptions.getInstance()
+    val oldQuote = editorOptions.COLON_MOVE_OUTSIDE_QUOTES
     try {
-      editorOptions.COLON_MOVE_OUTSIDE_QUOTES = true;
-      doTestColon("{\"x<caret>\":}", "{\"x:<caret>\":}");
+      editorOptions.COLON_MOVE_OUTSIDE_QUOTES = true
+      doTestColon("{\"x<caret>\":}", "{\"x:<caret>\":}")
     }
     finally {
-      editorOptions.COLON_MOVE_OUTSIDE_QUOTES = oldQuote;
+      editorOptions.COLON_MOVE_OUTSIDE_QUOTES = oldQuote
     }
   }
 
-  public void testDoNotMoveColonIfDisabled() {
-    JsonEditorOptions editorOptions = JsonEditorOptions.getInstance();
-    boolean oldQuote = editorOptions.COLON_MOVE_OUTSIDE_QUOTES;
+  fun testDoNotMoveColonIfDisabled() {
+    val editorOptions = JsonEditorOptions.getInstance()
+    val oldQuote = editorOptions.COLON_MOVE_OUTSIDE_QUOTES
     try {
-      editorOptions.COLON_MOVE_OUTSIDE_QUOTES = false;
-      doTestColon("{\"x<caret>\"}", "{\"x:<caret>\"}");
+      editorOptions.COLON_MOVE_OUTSIDE_QUOTES = false
+      doTestColon("{\"x<caret>\"}", "{\"x:<caret>\"}")
     }
     finally {
-      editorOptions.COLON_MOVE_OUTSIDE_QUOTES = oldQuote;
+      editorOptions.COLON_MOVE_OUTSIDE_QUOTES = oldQuote
     }
   }
 
-  public void testDoNotMoveCommaIfComma() {
-    JsonEditorOptions editorOptions = JsonEditorOptions.getInstance();
-    boolean oldQuote = editorOptions.COMMA_MOVE_OUTSIDE_QUOTES;
+  fun testDoNotMoveCommaIfComma() {
+    val editorOptions = JsonEditorOptions.getInstance()
+    val oldQuote = editorOptions.COMMA_MOVE_OUTSIDE_QUOTES
     try {
-      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = true;
-      doTestComma("{\"x\": \"value<caret>\",}", "{\"x\": \"value,<caret>\",}");
+      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = true
+      doTestComma("{\"x\": \"value<caret>\",}", "{\"x\": \"value,<caret>\",}")
     }
     finally {
-      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = oldQuote;
+      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = oldQuote
     }
   }
 
-  public void testDoNotMoveCommaIfDisabled() {
-    JsonEditorOptions editorOptions = JsonEditorOptions.getInstance();
-    boolean oldQuote = editorOptions.COMMA_MOVE_OUTSIDE_QUOTES;
+  fun testDoNotMoveCommaIfDisabled() {
+    val editorOptions = JsonEditorOptions.getInstance()
+    val oldQuote = editorOptions.COMMA_MOVE_OUTSIDE_QUOTES
     try {
-      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = false;
-      doTestComma("{\"x\": \"value<caret>\"}", "{\"x\": \"value,<caret>\"}");
+      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = false
+      doTestComma("{\"x\": \"value<caret>\"}", "{\"x\": \"value,<caret>\"}")
     }
     finally {
-      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = oldQuote;
+      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = oldQuote
     }
   }
 
-  public void testDoNotMoveCommaForArrayIfComma() {
-    JsonEditorOptions editorOptions = JsonEditorOptions.getInstance();
-    boolean oldQuote = editorOptions.COMMA_MOVE_OUTSIDE_QUOTES;
+  fun testDoNotMoveCommaForArrayIfComma() {
+    val editorOptions = JsonEditorOptions.getInstance()
+    val oldQuote = editorOptions.COMMA_MOVE_OUTSIDE_QUOTES
     try {
-      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = true;
-      doTestComma("{\"x\": [\"value<caret>\",]}", "{\"x\": [\"value,<caret>\",]}");
+      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = true
+      doTestComma("{\"x\": [\"value<caret>\",]}", "{\"x\": [\"value,<caret>\",]}")
     }
     finally {
-      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = oldQuote;
+      editorOptions.COMMA_MOVE_OUTSIDE_QUOTES = oldQuote
     }
   }
 
-  public void testQuotePairingNotInsideString01() {
-    doTestQuote("{\"MyKey\": \"This \\<caret>\"}", "{\"MyKey\": \"This \\\"<caret>\"}");
+  fun testQuotePairingNotInsideString01() {
+    doTestQuote("{\"MyKey\": \"This \\<caret>\"}", "{\"MyKey\": \"This \\\"<caret>\"}")
   }
 
-  public void testQuotePairingNotInsideString02() {
-    doTestSingleQuote("{\"MyKey\": \"This <caret> \\\"is\\\" my value\"}", "{\"MyKey\": \"This ' \\\"is\\\" my value\"}");
+  fun testQuotePairingNotInsideString02() {
+    doTestSingleQuote("{\"MyKey\": \"This <caret> \\\"is\\\" my value\"}", "{\"MyKey\": \"This ' \\\"is\\\" my value\"}")
+  }
+
+  companion object {
+    private fun testWithPairQuotes(on: Boolean, test: Runnable) {
+      val settings = CodeInsightSettings.getInstance()
+      val oldQuote = settings.AUTOINSERT_PAIR_QUOTE
+      try {
+        settings.AUTOINSERT_PAIR_QUOTE = on
+        test.run()
+      }
+      finally {
+        settings.AUTOINSERT_PAIR_QUOTE = oldQuote
+      }
+    }
   }
 }
