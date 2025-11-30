c7c8066f28b6ced420107e402e95fd0f8a19c6b2
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2025-11-30 18:34:41 +0100

    [diagnostic] IJPL-217892 Fix TriggerLowMemoryNotificationAction to emulate low memory notification
    GitOrigin-RevId: aa4d208a3193e9b8c842758a1e66891d503d3d1b

--- platform/platform-impl/src/com/intellij/diagnostic/TriggerLowMemoryNotificationAction.kt
+++ platform/platform-impl/src/com/intellij/diagnostic/TriggerLowMemoryNotificationAction.kt
@@ -4,14 +4,11 @@ package com.intellij.diagnostic
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
-import com.intellij.openapi.util.LowMemoryWatcher
-import org.jetbrains.annotations.ApiStatus
 
-@ApiStatus.Internal
-class TriggerLowMemoryNotificationAction : DumbAwareAction() {
+internal class TriggerLowMemoryNotificationAction : DumbAwareAction() {
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 
   override fun actionPerformed(e: AnActionEvent) {
-    LowMemoryWatcher.onLowMemorySignalReceived(true)
+    LowMemoryNotifier.showNotification(VMOptions.MemoryKind.HEAP, true)
   }
 }
