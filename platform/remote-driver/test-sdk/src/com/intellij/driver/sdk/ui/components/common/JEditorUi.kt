36dee9bc3f2f83907625ba149af0e7fd0c7dc574
vladislav.shishov <vladislav.shishov@jetbrains.com>
2025-12-01 15:20:32 +0200

    test framework [remote-driver]: wait for scroll to finish in JEditorUi
    GitOrigin-RevId: 6a4b739b75d8dbd6edf90beb0668c113125a3851

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/JEditorUi.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/JEditorUi.kt
@@ -25,6 +25,7 @@ import java.awt.Point
 import java.awt.Rectangle
 import kotlin.time.Duration
 import com.intellij.openapi.editor.markup.EffectType
+import kotlin.time.Duration.Companion.milliseconds
 
 fun Finder.editor(@Language("xpath") xpath: String? = null): JEditorUiComponent {
   return x(xpath ?: "//div[@class='EditorComponentImpl']",
@@ -265,15 +266,16 @@ open class JEditorUiComponent(data: ComponentData) : UiComponent(data) {
   }
 
   fun scrollToPosition(line: Int, column: Int) {
-    interact {
-      editor.getScrollingModel().scrollTo(driver.logicalPosition(line, column, component.rdTarget), scrollType())
-    }
+    val position = driver.logicalPosition(line, column, component.rdTarget)
+    val scrollType = scrollType()
+    interact { editor.getScrollingModel().scrollTo(position, scrollType) }
+    wait(200.milliseconds) // wait for scroll to finish
   }
 
   fun scrollToCaret() {
-    interact {
-      editor.getScrollingModel().scrollToCaret(scrollType())
-    }
+    val scrollType = scrollType()
+    interact { editor.getScrollingModel().scrollToCaret(scrollType) }
+    wait(200.milliseconds) // wait for scroll to finish
   }
 
   private fun scrollType(name: String = "CENTER") = driver.utility(ScrollType::class).valueOf(name)
