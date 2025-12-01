db2880eb53855cdff268a8d69af3f147e9c958dd
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-12-01 16:53:59 +0100

    WEB-75484 DefaultStubBuilder: cache StubElementRegistryService.getInstance() in StubBuildingWalkingVisitor to avoid costly service acquisition for every stub.
    GitOrigin-RevId: b89c036a604d9a8bff8897dcc7b497af40e7c6c6

--- platform/core-impl/src/com/intellij/psi/stubs/DefaultStubBuilder.java
+++ platform/core-impl/src/com/intellij/psi/stubs/DefaultStubBuilder.java
@@ -46,6 +46,7 @@ public class DefaultStubBuilder implements StubBuilder {
     private final Stack<StubElement> parentStubs = new Stack<>();
     private final Stack<ASTNode> parentNodes = new Stack<>();
     private final BooleanStack parentNodesStubbed = new BooleanStack();
+    private final StubElementRegistryService stubElementRegistryService = StubElementRegistryService.getInstance();
 
     protected StubBuildingWalkingVisitor(ASTNode root, StubElement parentStub) {
       parentNodes.push(root);
@@ -60,7 +61,7 @@ public class DefaultStubBuilder implements StubBuilder {
     }
 
     protected void visitNode(StubElement parentStub, ASTNode node, boolean immediateParentStubbed) {
-      StubElement stub = createStub(parentStub, node);
+      StubElement stub = createStub(parentStub, node, stubElementRegistryService);
       if (stub != null && !immediateParentStubbed) {
         ((ObjectStubBase<?>)stub).markDangling();
       }
@@ -72,10 +73,10 @@ public class DefaultStubBuilder implements StubBuilder {
       return parentNodes.isEmpty() ? null : parentNodes.peek();
     }
 
-    protected @Nullable StubElement createStub(StubElement parentStub, ASTNode node) {
+    protected @Nullable StubElement createStub(StubElement parentStub, ASTNode node, StubElementRegistryService stubElementRegistryService) {
       IElementType nodeType = node.getElementType();
 
-      StubElementFactory factory = StubElementRegistryService.getInstance().getStubFactory(nodeType);
+      StubElementFactory factory = stubElementRegistryService.getStubFactory(nodeType);
       if (factory == null) {
         return null;
       }
