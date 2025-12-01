63ed2bb30a72effb1f1e9ed7b3c65916c17d0a4d
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2025-12-01 18:15:13 +0100

    [kotlin] Limit tree change processing to avoid excessive event handling
    - To avoid performance problems associated with excessive handling of tree change events (KT-82171), we can limit tree change event processing to a single file and also avoid publishing multiple module OOBMs for the same file. This commit implements both limits.
    - The new implementation is essentially a state machine: As the write action progresses and more events are processed, the tree change preprocessor moves through multiple states. The first state represents the preprocessor in its unconstrained form. The intermediate states memorize the single file that is being processed. And the last state, `GlobalEventPublished`, allows us to ignore all further tree change events.
    - I tried using `ReadActionCachedValue` for the context, but it's not persistent enough in write actions (only persisting inside the *same* `allowInWriteAction` block).
    
    ^KT-82731 fixed
    ^KT-82171
    
    Space-RevId: d9aba9852db6377678a59eb3f9ce689e4e78bd4a
    
    GitOrigin-RevId: a00f8432cf493f80e117299fcc41fb20aa3eb365

--- plugins/kotlin/base/fir/analysis-api-platform/src/org/jetbrains/kotlin/idea/base/fir/analysisApiPlatform/FirIdeOutOfBlockPsiTreeChangePreprocessor.kt
+++ plugins/kotlin/base/fir/analysis-api-platform/src/org/jetbrains/kotlin/idea/base/fir/analysisApiPlatform/FirIdeOutOfBlockPsiTreeChangePreprocessor.kt
@@ -3,10 +3,14 @@
 package org.jetbrains.kotlin.idea.base.fir.analysisApiPlatform
 
 import com.intellij.injected.editor.DocumentWindow
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.WriteActionListener
+import com.intellij.openapi.application.ex.ApplicationManagerEx
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiLanguageInjectionHost
 import com.intellij.psi.PsiTreeChangeEvent
 import com.intellij.psi.impl.PsiModificationTrackerImpl
@@ -17,80 +21,170 @@ import com.intellij.psi.impl.source.tree.injected.InjectedLanguageUtilBase
 import com.intellij.psi.util.parentOfType
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.kotlin.analysis.api.platform.modification.KaElementModificationType
+import org.jetbrains.kotlin.analysis.api.platform.modification.KaSourceModificationLocality
 import org.jetbrains.kotlin.analysis.api.platform.modification.KaSourceModificationService
 import org.jetbrains.kotlin.analysis.api.platform.modification.publishGlobalSourceOutOfBlockModificationEvent
 import org.jetbrains.kotlin.psi.KtCodeFragment
 import org.jetbrains.kotlin.utils.exceptions.errorWithAttachment
 import org.jetbrains.kotlin.utils.exceptions.withPsiEntry
 
+/**
+ * Processes PSI tree change events to invalidate caches and publish modification events.
+ *
+ * To avoid performance problems associated with excessive handling of tree change events, the tree change preprocessor memorizes a
+ * [TreeChangeHandlingState] that allows avoiding the processing of further events for the duration of the write action.
+ */
 @ApiStatus.Internal
-class FirIdeOutOfBlockPsiTreeChangePreprocessor(private val project: Project) : PsiTreeChangePreprocessor {
+class FirIdeOutOfBlockPsiTreeChangePreprocessor(private val project: Project) : PsiTreeChangePreprocessor, Disposable {
+    private val threadLocalContext = ThreadLocal.withInitial { TreeChangeHandlingContext() }
+
+    init {
+        ApplicationManagerEx.getApplicationEx().addWriteActionListener(ContextRemovalWriteActionListener(), this)
+    }
+
     override fun treeChanged(event: PsiTreeChangeEventImpl) {
         if (project.isDefault) {
             return
         }
 
+        val context = threadLocalContext.get()
+        if (context.state == TreeChangeHandlingState.GlobalEventPublished) {
+            return
+        }
+
+        val hasEventBeenHandled = preprocessEvent(event, context)
+        if (hasEventBeenHandled) {
+            return
+        }
+
+        val rootElement = event.parent ?: return
+
+        val hasEventBeenHandledByRootElement = preprocessRootElement(event, rootElement, context)
+        if (hasEventBeenHandledByRootElement) {
+            return
+        }
+
+        val containingFile = rootElement.containingFile
+
+        if (containingFile is KtCodeFragment && containingFile.context?.isValid == false) {
+            // There is no need to invalidate caches for already invalid code fragments.
+            return
+        }
+
+        // We have to handle the rest of the states here because preceding lines may want to publish a global modification event even for
+        // the same file.
+        when (val currentState = context.state) {
+            TreeChangeHandlingState.Accepting -> {
+                // We've encountered the first legitimate event. The tree change preprocessor will be limited to its file for the remainder
+                // of the write action.
+                context.state = TreeChangeHandlingState.ProcessingFile(containingFile)
+
+                processElementModification(event, rootElement, containingFile, context)
+            }
+
+            is TreeChangeHandlingState.ProcessingFile -> {
+                if (currentState.file == containingFile) {
+                    processElementModification(event, rootElement, containingFile, context)
+                } else {
+                    publishGlobalModificationEvent(context)
+                }
+            }
+
+            is TreeChangeHandlingState.ModuleEventPublished -> {
+                // If the processed file and containing file match, we do nothing: There has already been an OOBM so we can skip the event.
+                if (currentState.processedFile != containingFile) {
+                    publishGlobalModificationEvent(context)
+                }
+            }
+
+            TreeChangeHandlingState.GlobalEventPublished -> {}
+        }
+    }
+
+    /**
+     * @return Whether the given event has been handled.
+     */
+    private fun preprocessEvent(event: PsiTreeChangeEventImpl, context: TreeChangeHandlingContext): Boolean {
         val eventCode = event.code
         if (!PsiModificationTrackerImpl.canAffectPsi(event) ||
             event.isGenericChange ||
             eventCode == PsiEventType.BEFORE_CHILD_ADDITION
         ) {
-            return
+            return true
         }
 
         if (event.isGlobalChange()) {
-            // We should not invalidate binary module content here, because global PSI tree changes have no effect on binary modules.
-            project.publishGlobalSourceOutOfBlockModificationEvent()
-            return
+            publishGlobalModificationEvent(context)
+            return true
         }
 
-        val rootElement = event.parent
+        return false
+    }
+
+    /**
+     * @return Whether the given event has been handled by processing the root element.
+     */
+    private fun preprocessRootElement(
+        event: PsiTreeChangeEventImpl,
+        rootElement: PsiElement,
+        context: TreeChangeHandlingContext
+    ): Boolean {
+        val eventCode = event.code
 
         // No reasons to invalidate injected documents before an actual replacement.
-        if (rootElement != null && eventCode != PsiEventType.BEFORE_CHILD_REPLACEMENT) {
+        if (eventCode != PsiEventType.BEFORE_CHILD_REPLACEMENT) {
             if (isInjectionChange(rootElement)) {
                 // Finding the exact `KtFile` for the injection is expensive and can lead to freezes (see KTIJ-36275). A global modification
                 // event can be published without a `KtFile`/`KaModule`. Since changes in injected files should be rare, such an event
                 // should be fine performance-wise.
-                project.publishGlobalSourceOutOfBlockModificationEvent()
-
-                return
+                publishGlobalModificationEvent(context)
+                return true
             }
         }
 
-        val child = when (eventCode) {
-            PsiEventType.CHILD_REMOVED -> rootElement
-            PsiEventType.BEFORE_CHILD_REPLACEMENT -> event.oldChild
-            else -> event.child
+        if (!rootElement.isPhysical) {
+            // Elements which do not belong to the project should not cause an OOBM.
+            return true
         }
 
-        if (rootElement == null || !rootElement.isPhysical) {
-            /**
-             * Element which do not belong to a project should not cause OOBM
-             */
-            return
-        }
+        return false
+    }
 
-        val containingFile = rootElement.containingFile
-        if (containingFile is KtCodeFragment && containingFile.context?.isValid == false) {
-            // no need in cache invalidation for already invalid code fragments
-            return
-        }
-
-        val modificationType = when (eventCode) {
+    private fun processElementModification(
+        event: PsiTreeChangeEventImpl,
+        rootElement: PsiElement,
+        containingFile: PsiFile,
+        context: TreeChangeHandlingContext,
+    ) {
+        val modificationType = when (event.code) {
             PsiEventType.CHILD_ADDED -> KaElementModificationType.ElementAdded
             PsiEventType.CHILD_REMOVED -> {
-                val removedElement = event.child ?:
-                    errorWithAttachment("A ${PsiEventType.CHILD_REMOVED} PSI tree change event should have a child element") {
+                val removedElement = event.child
+                    ?: errorWithAttachment("A ${PsiEventType.CHILD_REMOVED} PSI tree change event should have a child element") {
                         withEntry("psiTreeChangeEvent", event.toString())
                         withPsiEntry("rootElement", rootElement)
                     }
                 KaElementModificationType.ElementRemoved(removedElement)
             }
+
             else -> KaElementModificationType.Unknown
         }
 
-        KaSourceModificationService.getInstance(project).handleElementModification(child ?: rootElement, modificationType)
+        val child = when (event.code) {
+            PsiEventType.CHILD_REMOVED -> rootElement
+            PsiEventType.BEFORE_CHILD_REPLACEMENT -> event.oldChild
+            else -> event.child
+        }
+        val targetElement = child ?: rootElement
+
+        val sourceModificationService = KaSourceModificationService.getInstance(project)
+        val locality = sourceModificationService.detectLocality(targetElement, modificationType)
+
+        sourceModificationService.handleInvalidation(targetElement, locality)
+
+        if (locality is KaSourceModificationLocality.OutOfBlock) {
+            context.state = TreeChangeHandlingState.ModuleEventPublished(containingFile)
+        }
     }
 
     private fun isInjectionChange(rootElement: PsiElement): Boolean {
@@ -113,6 +207,81 @@ class FirIdeOutOfBlockPsiTreeChangePreprocessor(private val project: Project) : 
 
     private fun DocumentWindow.containsInjectionAt(textRange: TextRange): Boolean =
         this.hostRanges.any { textRange.intersects(it) }
+
+    private fun publishGlobalModificationEvent(context: TreeChangeHandlingContext) {
+        // We should only invalidate source module content here because global PSI tree changes have no effect on binary modules.
+        project.publishGlobalSourceOutOfBlockModificationEvent()
+
+        context.state = TreeChangeHandlingState.GlobalEventPublished
+    }
+
+    override fun dispose() {
+    }
+
+    private inner class ContextRemovalWriteActionListener : WriteActionListener {
+        override fun writeActionStarted(action: Class<*>) {
+            // Cleaning up on write action start is not strictly necessary because the context should have been cleaned up at the end of
+            // the previous write action. But it's good to ensure that we start a write action with a clean context in case something went
+            // wrong.
+            threadLocalContext.remove()
+        }
+
+        override fun writeActionFinished(action: Class<*>) {
+            threadLocalContext.remove()
+        }
+    }
+}
+
+/**
+ * [TreeChangeHandlingContext] memorizes information between tree change events. It lives for the duration of the write action.
+ */
+private class TreeChangeHandlingContext(
+    var state: TreeChangeHandlingState = TreeChangeHandlingState.Accepting,
+)
+
+/**
+ * The tree change preprocessor is a state machine that can be in four different states: [Accepting], [ProcessingFile],
+ * [ModuleEventPublished], and [GlobalEventPublished].
+ *
+ * @see FirIdeOutOfBlockPsiTreeChangePreprocessor
+ */
+private sealed class TreeChangeHandlingState {
+    /**
+     * The tree change preprocessor has not yet published modification events and is accepting tree change events from any files.
+     */
+    data object Accepting : TreeChangeHandlingState()
+
+    /**
+     * The tree change preprocessor is processing tree change events only for [file].
+     *
+     * If we encounter a different file, we're likely dealing with a complex write action. To avoid excessive processing, we proceed to
+     * publish a global modification event (and jump straight to [GlobalEventPublished]).
+     *
+     * It's important here to draw the boundary at different files instead of different modules. One of the most expensive operations during
+     * tree change processing is getting the module for a specific file. If we instead proceeded to global modification when encountering a
+     * different module, we might still process hundreds or thousands of files.
+     *
+     * Note that this state is necessary to catch multi-file processing when no out-of-block modification has been detected yet. For
+     * example, we could have in-block modification in multiple files. Because in-block modification processing can also cause performance
+     * problems, we should still proceed with a global modification event.
+     */
+    data class ProcessingFile(val file: PsiFile) : TreeChangeHandlingState()
+
+    /**
+     * The tree change preprocessor has published an out-of-block modification event for the module of [processedFile].
+     *
+     * If we encounter the same file again, we can skip publishing another module modification event because we've already published such an
+     * event. If it's a different file, we publish a global modification event like in [ProcessingFile].
+     */
+    data class ModuleEventPublished(val processedFile: PsiFile) : TreeChangeHandlingState()
+
+    /**
+     * The tree change preprocessor has published a global modification event.
+     *
+     * In this context, there is nothing in session invalidation that can be done on top of that, so this state is terminal and effectively
+     * turns off the tree change preprocessor for the remainder of the write action.
+     */
+    data object GlobalEventPublished : TreeChangeHandlingState()
 }
 
 /**
