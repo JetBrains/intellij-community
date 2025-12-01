3ee7fa647768ac18cd96b3d45d54f77322f24e9b
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2025-12-01 18:18:39 +0100

    [kotlin] Add tests for tree change processing limits
    - The tree change preprocessor is limited in the number of tree change events it should process and the number of modification events it should publish. The added tests verify these limitations.
    
    ^KT-82731
    ^KT-82171
    
    Space-RevId: ec78b22bec25a3c06b6067c30adc9373089db108
    
    GitOrigin-RevId: 86b03ec1b332eaad68e9f632e6aeb659d63c68dc

---
+++ plugins/kotlin/base/fir/analysis-api-platform/test/org/jetbrains/kotlin/idea/base/fir/analysisApiPlatform/modificationEvents/KotlinEventLimitOutOfBlockModificationTest.kt
@@ -0,0 +1,169 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.base.fir.analysisApiPlatform.modificationEvents
+
+import com.intellij.openapi.application.runUndoTransparentWriteAction
+import org.jetbrains.kotlin.analysis.api.platform.modification.KotlinModificationEventKind
+import org.jetbrains.kotlin.idea.base.projectStructure.toKaSourceModuleForProduction
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtNamedFunction
+
+/**
+ * The [tree change preprocessor][org.jetbrains.kotlin.idea.base.fir.analysisApiPlatform.FirIdeOutOfBlockPsiTreeChangePreprocessor] is
+ * limited in the number of tree change events it should process and the number of modification events it should publish. This class
+ * contains tests which verify these limitations.
+ *
+ * See [FirIdeOutOfBlockPsiTreeChangePreprocessor][org.jetbrains.kotlin.idea.base.fir.analysisApiPlatform.FirIdeOutOfBlockPsiTreeChangePreprocessor]
+ * for more information.
+ */
+class KotlinEventLimitOutOfBlockModificationTest : AbstractKotlinModificationEventTest() {
+    fun `test that modification events are limited when modifying multiple files`() {
+        val moduleA = createModuleInTmpDir("a") {
+            listOf(
+                FileWithText("a.kt", """
+                    fun foo(): Int { 
+                        println("Hello!")
+                        return 10
+                    }
+                """.trimIndent()),
+                FileWithText("b.kt", "fun bar(): Int = 20"),
+                FileWithText("c.kt", "fun baz(): Int = 30"),
+            )
+        }
+
+        val globalTracker = createGlobalTracker(
+            "the project after modifying multiple files",
+            expectedEventKind = KotlinModificationEventKind.GLOBAL_SOURCE_OUT_OF_BLOCK_MODIFICATION,
+            additionalAllowedEventKinds = setOf(
+                KotlinModificationEventKind.MODULE_OUT_OF_BLOCK_MODIFICATION
+            )
+        )
+
+        val moduleTracker = createModuleTracker(
+            moduleA.toKaSourceModuleForProduction()!!,
+            "module A after modifying multiple files",
+            expectedEventKind = KotlinModificationEventKind.MODULE_OUT_OF_BLOCK_MODIFICATION,
+            additionalAllowedEventKinds = setOf(KotlinModificationEventKind.GLOBAL_SOURCE_OUT_OF_BLOCK_MODIFICATION),
+        )
+
+        val fileA = moduleA.findSourceKtFile("a.kt")
+        val fileB = moduleA.findSourceKtFile("b.kt")
+        val fileC = moduleA.findSourceKtFile("c.kt")
+
+        fun deleteTypeReference(file: KtFile) {
+            file.firstTopLevelFunction.typeReference!!.delete()
+        }
+
+        runUndoTransparentWriteAction {
+            // First, no OOBM event should be published on in-block modification. While we test in-block modification separately, the intent
+            // here is to check that OOBM publishing limits still apply even after processing an in-block event.
+            fileA.firstTopLevelFunction.bodyBlockExpression!!.firstStatement!!.delete()
+
+            moduleTracker.assertNotModified()
+            globalTracker.assertNotModified()
+
+            // Then, a single module OOBM event should be published after editing `fileA`.
+            deleteTypeReference(fileA)
+
+            moduleTracker.assertModifiedOnce()
+            globalTracker.assertNotModified()
+
+            // A second out-of-block modification in `fileA` should not lead to another modification event, since we've already published
+            // an event for the file's module.
+            fileA.firstTopLevelFunction.delete()
+
+            moduleTracker.assertModifiedOnce()
+            globalTracker.assertNotModified()
+
+            // When changes in `fileB` are encountered, a single global OOBM event should be published, as we're now dealing with multiple
+            // files.
+            deleteTypeReference(fileB)
+
+            moduleTracker.assertModifiedOnce()
+            globalTracker.assertModifiedOnce()
+
+            // Finally, changes in `fileC` should not lead to any further modification events.
+            deleteTypeReference(fileC)
+
+            moduleTracker.assertModifiedOnce()
+            globalTracker.assertModifiedOnce()
+        }
+    }
+
+    fun `test that a global modification event is published after in-block modifications in multiple files`() {
+        val moduleA = createModuleInTmpDir("a") {
+            listOf(
+                FileWithText("a.kt", """
+                    fun foo(): Int { 
+                        println("foo")
+                        println("foo2")
+                        return 10
+                    }
+                """.trimIndent()),
+                FileWithText("b.kt", """
+                    fun bar(): Int { 
+                        println("bar")
+                        return 20
+                    }
+                """.trimIndent()),
+                FileWithText("c.kt", """
+                    fun baz(): Int { 
+                        println("baz")
+                        return 30
+                    }
+                """.trimIndent()),
+            )
+        }
+
+        val globalTracker = createGlobalTracker(
+            "the project after multi-file in-block modification",
+            expectedEventKind = KotlinModificationEventKind.GLOBAL_SOURCE_OUT_OF_BLOCK_MODIFICATION,
+            additionalAllowedEventKinds = setOf(
+                KotlinModificationEventKind.MODULE_OUT_OF_BLOCK_MODIFICATION
+            )
+        )
+
+        val moduleTracker = createModuleTracker(
+            moduleA.toKaSourceModuleForProduction()!!,
+            "module A after multi-file in-block modification",
+            expectedEventKind = KotlinModificationEventKind.MODULE_OUT_OF_BLOCK_MODIFICATION,
+            additionalAllowedEventKinds = setOf(KotlinModificationEventKind.GLOBAL_SOURCE_OUT_OF_BLOCK_MODIFICATION),
+        )
+
+        val fileA = moduleA.findSourceKtFile("a.kt")
+        val fileB = moduleA.findSourceKtFile("b.kt")
+        val fileC = moduleA.findSourceKtFile("c.kt")
+
+        fun deleteFirstLineInFunction(file: KtFile) {
+            file.firstTopLevelFunction.bodyBlockExpression!!.firstStatement!!.delete()
+        }
+
+        runUndoTransparentWriteAction {
+            // First in-block modification: There should be no modification events.
+            deleteFirstLineInFunction(fileA)
+
+            moduleTracker.assertNotModified()
+            globalTracker.assertNotModified()
+
+            // Another in-block modification in the same file: No modification events either.
+            deleteFirstLineInFunction(fileA)
+
+            moduleTracker.assertNotModified()
+            globalTracker.assertNotModified()
+
+            // An in-block modification in another file: Since we're processing another file, a global event should be published.
+            deleteFirstLineInFunction(fileB)
+
+            moduleTracker.assertNotModified()
+            globalTracker.assertModifiedOnce()
+
+            // An in-block modification in a third file: As we've already published a global event, no further events should be published.
+            deleteFirstLineInFunction(fileC)
+
+            moduleTracker.assertNotModified()
+            globalTracker.assertModifiedOnce()
+        }
+    }
+
+    private val KtFile.firstTopLevelFunction: KtNamedFunction
+        get() = declarations.first() as KtNamedFunction
+}
