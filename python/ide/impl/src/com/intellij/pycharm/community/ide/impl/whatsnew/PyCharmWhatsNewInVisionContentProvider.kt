95af317029d5495e1ce54a87804b2bbe73f5a292
Vladimir.Koshelev <vladimir.koshelev@jetbrains.com>
2025-12-01 14:47:10 +0100

    docs [pycharm]: add what's new for 2025.3
    GitOrigin-RevId: 755d5e2674e5dbad4c3cbb1be0f758e3ad63e489

--- python/ide/impl/src/com/intellij/pycharm/community/ide/impl/whatsnew/PyCharmWhatsNewInVisionContentProvider.kt
+++ python/ide/impl/src/com/intellij/pycharm/community/ide/impl/whatsnew/PyCharmWhatsNewInVisionContentProvider.kt
@@ -3,12 +3,11 @@ package com.intellij.pycharm.community.ide.impl.whatsnew
 
 import com.intellij.openapi.application.InitialConfigImportState
 import com.intellij.platform.whatsNew.WhatsNewInVisionContentProvider
-import com.intellij.util.PlatformUtils.isCommunityEdition
 
 // The files are missing in master. Both files were added, and version was updated only in stable brunches.
 class PyCharmWhatsNewInVisionContentProvider : WhatsNewInVisionContentProvider() {
   override val baseResourcePathInClassLoader: String = "whatsNew"
-  override val visionJsonFileNames: List<String> = listOf(if (isCommunityEdition()) "pycharmCE2025.2.json" else "pycharm2025.2.json")
+  override val visionJsonFileNames: List<String> = listOf("pycharm2025.3.json")
 
   override suspend fun isAvailable(): Boolean {
     // Will skip WhatsNew if the first launch / new user, not to overload user with info.
