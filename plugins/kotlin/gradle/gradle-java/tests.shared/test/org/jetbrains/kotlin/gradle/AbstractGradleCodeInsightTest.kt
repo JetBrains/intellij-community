74c4ec83ba3c80ca988d1f2e0be7d7f15cb903d7
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2025-12-01 22:11:06 +0100

    [gradle] IDEA-381944 Improve `dataFile` handling for tests without `@TestMetadata` annotations
    GitOrigin-RevId: b6b35fe4cf5d4ee8d522f3996642d58318434fa8

--- plugins/kotlin/gradle/gradle-java/tests.shared/test/org/jetbrains/kotlin/gradle/AbstractGradleCodeInsightTest.kt
+++ plugins/kotlin/gradle/gradle-java/tests.shared/test/org/jetbrains/kotlin/gradle/AbstractGradleCodeInsightTest.kt
@@ -13,10 +13,7 @@ import com.intellij.testFramework.junit5.TestDisposable
 import com.intellij.testFramework.utils.vfs.getPsiFile
 import org.jetbrains.kotlin.idea.framework.KotlinSdkType
 import org.jetbrains.kotlin.idea.test.Directives
-import org.jetbrains.kotlin.idea.test.KotlinTestUtils.getMethodMetadata
-import org.jetbrains.kotlin.idea.test.KotlinTestUtils.getTestDataFileName
-import org.jetbrains.kotlin.idea.test.KotlinTestUtils.getTestsRoot
-import org.jetbrains.kotlin.idea.test.KotlinTestUtils.toSlashEndingDirPath
+import org.jetbrains.kotlin.idea.test.KotlinTestUtils.*
 import org.jetbrains.kotlin.idea.test.TestFiles
 import org.jetbrains.plugins.gradle.frameworkSupport.GradleDsl
 import org.jetbrains.plugins.gradle.testFramework.GradleTestFixtureBuilder
@@ -93,9 +90,12 @@ abstract class AbstractGradleCodeInsightTest : AbstractKotlinGradleCodeInsightBa
 
     protected fun dataFile(fileName: String): File = File(getTestDataPath(), fileName)
 
-    protected fun dataFile(): File = dataFile(fileName())
+    protected fun dataFile(): File? {
+        val testMetadataFileName = fileName() ?: return null
+        return dataFile(testMetadataFileName)
+    }
 
-    protected open fun fileName(): String = getMethodMetadata(testInfo.testMethod.get()) ?: error("no @TestMetadata")
+    protected open fun fileName(): String? = getMethodMetadata(testInfo.testMethod.get())
 
     protected val document: Document
         get() {
@@ -103,23 +103,30 @@ abstract class AbstractGradleCodeInsightTest : AbstractKotlinGradleCodeInsightBa
             return fileEditorManager.selectedTextEditor?.document ?: error("no document found")
         }
 
+    protected open val testFileFactory = object : TestFiles.TestFileFactoryNoModules<TestFile>() {
+        override fun create(fileName: String, text: String, directives: Directives): TestFile {
+            val linesWithoutDirectives = text.lines().filter { !it.startsWith("// FILE") }
+            return TestFile(fileName, linesWithoutDirectives.joinToString(separator = "\n"), directives)
+        }
+    }
+
     private fun loadTestDataFiles() {
         val mainFile = dataFile()
+        if (mainFile == null) {
+            // when a test method doesn't have @TestMetadata
+            _testDataFiles = emptyList()
+            return
+        }
         val multiFileText = FileUtil.loadFile(mainFile, true)
 
         _testDataFiles = TestFiles.createTestFiles(
             mainFile.name,
             multiFileText,
-            object : TestFiles.TestFileFactoryNoModules<TestFile>() {
-                override fun create(fileName: String, text: String, directives: Directives): TestFile {
-                    val linesWithoutDirectives = text.lines().filter { !it.startsWith("// FILE") }
-                    return TestFile(fileName, linesWithoutDirectives.joinToString(separator = "\n"))
-                }
-            }
+            testFileFactory
         )
     }
 
-    class TestFile internal constructor(val path: String, val content: String)
+    class TestFile internal constructor(val path: String, val content: String, val directives: Directives)
 
     companion object {
         @JvmStatic
