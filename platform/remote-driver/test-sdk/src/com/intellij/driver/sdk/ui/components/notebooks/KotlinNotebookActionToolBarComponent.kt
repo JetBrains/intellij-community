24d186ca1659c18a5fa57745e8f389c720f9366b
Ilya.Komolkin <ilya.komolkin@jetbrains.com>
2025-12-03 13:43:24 +0000

    [tests] KTNB-1276 Refactor Kotlin Notebook Settings workflow: replace settings panel with dialog implementation, update related tests and utilities
    Merge-request: IJ-MR-184360
    Merged-by: Ilya Komolkin <Ilya.Komolkin@jetbrains.com>
    
    GitOrigin-RevId: 036fd2e3990c44dc51f1f5efd8db67488a45444a

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/notebooks/KotlinNotebookActionToolBarComponent.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/notebooks/KotlinNotebookActionToolBarComponent.kt
@@ -1,24 +1,7 @@
 package com.intellij.driver.sdk.ui.components.notebooks
 
-import com.intellij.driver.sdk.ui.Finder
 import com.intellij.driver.sdk.ui.components.ComponentData
 import com.intellij.driver.sdk.ui.components.UiComponent
-
-fun Finder.openKotlinNotebookSettingsPanel() {
-  notebookEditor {
-    kotlinNotebookToolbar.openSettingsButton.click()
-  }
-}
-
-/**
- * Opens a Kotlin Notebook Settings panel and applies [action] to it with autoclose
- */
-fun Finder.kotlinNotebookSettingsFromToolbar(action: KotlinNotebookUiSettingsComponent.() -> Unit) {
-  openKotlinNotebookSettingsPanel()
-  withKotlinNotebookSettingsPanel {
-    action()
-  }
-}
 
 /**
  * Represents UI-component for Kotlin-specific toolbar actions
