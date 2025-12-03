3b102ef499272e609df2c73bb5822dc8c11ec498
Nikita Zaytsev <nikita.zaytsev@jetbrains.com>
2025-12-03 19:28:34 +0200

    IJPL-216122 rdct: Propagate account changes from repository to manager
    Before these changes, the set of accounts in the AccountManagerBase wasn't updated when the component of the corresponding AccountsRepository was reloaded. This caused, for example, the list of GitHub accounts not being updated after the corresponding settings file was changed during sync.
    
    (cherry picked from commit 742262142496000e3e60b95ddc809f77d5252205)
    
    GitOrigin-RevId: 96fd4e22b5f1f8365c6884214de4a6e33dabf46d

--- plugins/gitlab/gitlab-core/src/org/jetbrains/plugins/gitlab/authentication/accounts/GitLabPersistentAccounts.kt
+++ plugins/gitlab/gitlab-core/src/org/jetbrains/plugins/gitlab/authentication/accounts/GitLabPersistentAccounts.kt
@@ -1,15 +1,16 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gitlab.authentication.accounts
 
-import com.intellij.collaboration.auth.AccountsRepository
+import com.intellij.collaboration.auth.ObservableAccountsRepository
 import com.intellij.openapi.components.*
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.serialization.Serializable
 
 @State(name = "GitLabAccounts",
        category = SettingsCategory.TOOLS,
        exportable = true,
        storages = [Storage(value = "gitlab.xml", roamingType = RoamingType.DISABLED)], reportStatistic = false)
-internal class GitLabPersistentAccounts : AccountsRepository<GitLabAccount>,
+internal class GitLabPersistentAccounts : ObservableAccountsRepository<GitLabAccount>,
                                           SerializablePersistentStateComponent<GitLabPersistentAccounts.GitLabAccountsState>(
                                             GitLabAccountsState()) {
 
@@ -19,8 +20,25 @@ internal class GitLabPersistentAccounts : AccountsRepository<GitLabAccount>,
   override var accounts: Set<GitLabAccount>
     get() = state.accounts.toSet()
     set(value) {
-      updateState {
-        GitLabAccountsState(value.toSet())
+      synchronized(this) {
+        updateState {
+          GitLabAccountsState(value.toSet())
+        }
+        accountsFlow.value = value
       }
     }
+
+  override val accountsFlow = MutableStateFlow(emptySet<GitLabAccount>())
+
+  override fun loadState(state: GitLabAccountsState) {
+    synchronized(this) {
+      super.loadState(state)
+
+      accountsFlow.value = state.accounts
+    }
+  }
+
+  override fun noStateLoaded() {
+    loadState(GitLabAccountsState())
+  }
 }
