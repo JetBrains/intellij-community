9aea2d445a27c80f442e9a988f7b74ffe4e4ab62
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2025-12-03 18:20:08 +0200

    fixup! PY-86002 Disable "python.typing.strict.unions" and revert the tests updated for PY-24834 in 253
    (cherry picked from commit 980a5ad341400a271acadf07d40f346f6919d421)
    
    IJ-MR-184663
    
    GitOrigin-RevId: fe632291357677359b5186160ce2e14cb7fdde60

--- python/testData/inspections/PyUnresolvedReferencesInspection/AsyncAwaitWarningOnImportedFun/a.py
+++ python/testData/inspections/PyUnresolvedReferencesInspection/AsyncAwaitWarningOnImportedFun/a.py
@@ -6,7 +6,7 @@ async def expect_no_warning():
 
 
 async def expect_new_warning():
-    await <warning descr="Function 'fun_non_async()' neither declared as 'async' nor with 'Awaitable' as return type">fun_non_async()</warning>
+    await <weak_warning descr="Function 'fun_non_async()' neither declared as 'async' nor with 'Awaitable' as return type">fun_non_async()</weak_warning>
 
 
 def local_fun_non_async():
