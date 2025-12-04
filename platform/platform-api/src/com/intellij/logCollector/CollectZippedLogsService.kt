a47e3730915572f55ecc55822f7b45b131ad4a10
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2025-12-04 11:27:59 +0100

    refactor [platform]: more direct way of starting log collection
    Space-RevId: 3d2df9274b473b68111dac634a3268f9d1ca2e41
    
    GitOrigin-RevId: 2155600d00212800a426d64572167a95bc2d76e8

--- platform/platform-api/src/com/intellij/logCollector/CollectZippedLogsService.kt
+++ platform/platform-api/src/com/intellij/logCollector/CollectZippedLogsService.kt
@@ -1,18 +1,14 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.logCollector
 
+import com.intellij.ide.ui.IdeUiService
+import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 import org.jetbrains.annotations.ApiStatus
 
-/**
- * Service for collecting and packaging IDE logs and diagnostic data.
- */
+@Deprecated("Use 'IdeUiService.invokeLogCollectionAction' instead")
 @ApiStatus.Internal
-interface CollectZippedLogsService {
-  /**
-   * Collects logs and diagnostic data, packages them into a zip file, and reveals the file to the user.
-   *
-   * @param project The project context, or null for application-level logs
-   */
-  fun collectZippedLogs(project: Project?)
+@Service(Service.Level.APP)
+class CollectZippedLogsService {
+  fun collectZippedLogs(project: Project?): Unit = IdeUiService.getInstance().invokeLogCollectionAction(project)
 }
