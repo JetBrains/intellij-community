8201c3a0209c71e31764c343753179f50f12aea6
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-12-05 13:58:06 +0100

    WEB-75484 StubBuildCachedValuesManager: document and move the class to core-api module to allow for assertion in CachedValuesManager.
    GitOrigin-RevId: aca94d09e934f2487b4189736b57859a0385189a

--- platform/core-api/src/com/intellij/psi/stubs/StubBuildCachedValuesManager.kt
+++ platform/core-api/src/com/intellij/psi/stubs/StubBuildCachedValuesManager.kt
@@ -1,9 +1,10 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.stubs
 
+import com.intellij.lang.ASTNode
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
 import com.intellij.psi.PsiElement
-import com.intellij.psi.impl.source.tree.CompositeElement
 import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
 import com.intellij.psi.util.ParameterizedCachedValue
@@ -12,7 +13,25 @@ import org.jetbrains.annotations.ApiStatus
 import java.util.concurrent.atomic.AtomicLong
 import java.util.function.Function
 
-@ApiStatus.Internal
+/**
+ * This class serves as an entry point for optimizing usage of cached values during stub building, i.e., during indexing.
+ *
+ * Each [com.intellij.psi.StubBuilder] should ensure that [startBuildingStubs] is called before starting the stub building process
+ * and [finishBuildingStubs] is called after the stub building process is finished.
+ *
+ * The main scenario is to replace [com.intellij.psi.util.CachedValuesManager.getCachedValue] or
+ * [com.intellij.psi.util.CachedValuesManager.getParameterizedCachedValue] calls, which are used during stub building,
+ * with calls to [getCachedValueStubBuildOptimized]. The [com.intellij.psi.util.CachedValuesManager.getCachedValue] call performs quite
+ * costly checks to ensure that a cached value is up to date, which is an unnecessary work during stub building,
+ * as none of the other AST elements of the file can change in the meanwhile. You should use [getCachedValueStubBuildOptimized]
+ * overload with [com.intellij.psi.util.ParameterizedCachedValueProvider], because it avoids instantiation of a lambda object,
+ * saving some tiny, but precious, amount of time during indexing.
+ *
+ * The other scenario is to use [getCachedValueIfBuildingStubs] to cache some results only during stub building,
+ * as using a regular [com.intellij.psi.util.CachedValue] would be overkill; i.e., it would be more expensive to check if a CachedValue
+ * is up to date than to compute the value.
+ */
+@ApiStatus.Experimental
 object StubBuildCachedValuesManager {
 
   private val myStubBuildId = ThreadLocal<Long?>()
@@ -39,10 +58,10 @@ object StubBuildCachedValuesManager {
 
   @JvmStatic
   fun <T, P> getCachedValueIfBuildingStubs(
-    dataHolder: PsiElement,
-    stubBuildingKey: Key<StubBuildCachedValue<T>>,
-    parameter: P,
-    provider: Function<P, T>
+      dataHolder: PsiElement,
+      stubBuildingKey: Key<StubBuildCachedValue<T>>,
+      parameter: P,
+      provider: Function<P, T>
   ): T {
     val stubBuildId = stubBuildId
     if (stubBuildId != null) {
@@ -59,7 +78,8 @@ object StubBuildCachedValuesManager {
 
   @JvmStatic
   fun <T, P> getCachedValueStubBuildOptimized(
-    node: CompositeElement,
+    node: ASTNode,
+    project: Project,
     key: Key<ParameterizedCachedValue<T, P>>,
     stubBuildingKey: Key<StubBuildCachedValue<T>>,
     provider: ParameterizedCachedValueProvider<T, P>,
@@ -75,7 +95,7 @@ object StubBuildCachedValuesManager {
       }
       return current.value
     }
-    return CachedValuesManager.getManager(node.getManager().getProject()).getParameterizedCachedValue(
+    return CachedValuesManager.getManager(project).getParameterizedCachedValue(
       node, key, provider, false, parameter
     )
   }
@@ -83,11 +103,11 @@ object StubBuildCachedValuesManager {
   // Avoid using recursion manager and other complex logic when building stubs - improves speed by 10%.
   @JvmStatic
   fun <T, P> getCachedValueStubBuildOptimized(
-    dataHolder: PsiElement,
-    key: Key<ParameterizedCachedValue<T, P>>,
-    stubBuildingKey: Key<StubBuildCachedValue<T>>,
-    provider: ParameterizedCachedValueProvider<T, P>,
-    parameter: P
+      dataHolder: PsiElement,
+      key: Key<ParameterizedCachedValue<T, P>>,
+      stubBuildingKey: Key<StubBuildCachedValue<T>>,
+      provider: ParameterizedCachedValueProvider<T, P>,
+      parameter: P
   ): T {
     val stubBuildId = stubBuildId
     if (stubBuildId != null) {
@@ -112,9 +132,9 @@ object StubBuildCachedValuesManager {
    */
   @JvmStatic
   fun <T> getCachedValueStubBuildOptimized(
-    dataHolder: PsiElement,
-    stubBuildingKey: Key<StubBuildCachedValue<T?>>,
-    provider: CachedValueProvider<T>,
+      dataHolder: PsiElement,
+      stubBuildingKey: Key<StubBuildCachedValue<T?>>,
+      provider: CachedValueProvider<T>,
   ): T? {
     val stubBuildId = stubBuildId
     if (stubBuildId != null) {
@@ -130,5 +150,5 @@ object StubBuildCachedValuesManager {
     return CachedValuesManager.getCachedValue(dataHolder, provider)
   }
 
-  class StubBuildCachedValue<T>(val buildId: Long, val value: T)
+  class StubBuildCachedValue<T> internal constructor(internal val buildId: Long, internal val value: T)
 }
