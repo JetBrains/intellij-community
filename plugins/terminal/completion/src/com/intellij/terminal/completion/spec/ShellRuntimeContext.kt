7c410cf828c20c0484da8548fd9e717c8edab084
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2025-12-05 17:58:03 +0200

    [terminal] IJPL-220311 Make `runShellCommand` obsolete
    GitOrigin-RevId: 44f9417164ee0ea863dffc43f040f6f2670745d8

--- plugins/terminal/completion/src/com/intellij/terminal/completion/spec/ShellRuntimeContext.kt
+++ plugins/terminal/completion/src/com/intellij/terminal/completion/spec/ShellRuntimeContext.kt
@@ -23,13 +23,26 @@ interface ShellRuntimeContext {
   val shellName: ShellName
 
   /**
-   * Runs the internal user invisible [command] in the current terminal session and returns the [ShellCommandResult].
+   * This method was initially designed for the Experimental 2024 Terminal, which is now deprecated.
+   * It was running the internal user invisible [command] in the current terminal session.
+   *
+   * Current status of this method:
+   * 1. Experimental 2024 Terminal - should work as designed.
+   * 2. Reworked Terminal - will work only if [command] can be interpreted as OS process command line (bash syntax won't work),
+   * because Reworked Terminal can only execute OS processes in the same environment where shell is running.
+   *
+   * Prefer using [createProcessBuilder] instead if you write the new code, that is OK to work only in the Reworked Terminal.
+   * This method can be used only in the existing command specifications that should work in the Experimental 2024 Terminal as well.
    */
+  @ApiStatus.Obsolete
   suspend fun runShellCommand(@Language("ShellScript") command: String): ShellCommandResult
 
   /**
    * Creates the builder for process execution in the environment where the current terminal session is running.
    * Modify the builder parameters and call [ShellDataGeneratorProcessBuilder.execute] to execute the process.
+   *
+   * **Works only in the Reworked Terminal.**
+   * In Experimental 2024 Terminal it will create a no-op builder that always returns empty output with exit code 1.
    *
    * @param executable the absolute path the executable or the executable name to be found in the PATH environment variable.
    */
