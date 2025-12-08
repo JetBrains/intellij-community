6aaffe6d31369453d661e2a46322f223bfd0fe85
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-12-08 15:47:52 +0100

    [threading] IJPL-222247: Fix read access in PsiCrumb
    GitOrigin-RevId: 25c478b2adf496540b6d3c3ef2352ef1c162e586

--- platform/platform-impl/src/com/intellij/xml/breadcrumbs/PsiCrumb.java
+++ platform/platform-impl/src/com/intellij/xml/breadcrumbs/PsiCrumb.java
@@ -1,7 +1,8 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.xml.breadcrumbs;
 
 import com.intellij.internal.statistic.service.fus.collectors.UIEventLogger;
+import com.intellij.openapi.application.WriteIntentReadAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.util.NlsContexts;
 import com.intellij.openapi.util.TextRange;
@@ -29,13 +30,15 @@ final class PsiCrumb extends Crumb.Impl implements NavigatableCrumb, LazyTooltip
   @Override
   public String getTooltip() {
     if (needCalculateTooltip()) {
-      PsiElement element = getElement(this);
-      tooltip = element == null ? null
-                                : provider.getElementTooltip(element);
-      provider = null; // do not try recalculate tooltip
-      if (element != null) {
-        UIEventLogger.BreadcrumbShowTooltip.log(element.getProject(), element.getLanguage());
-      }
+      WriteIntentReadAction.run(() -> {
+        PsiElement element = getElement(this);
+        tooltip = element == null ? null
+                                  : provider.getElementTooltip(element);
+        provider = null; // do not try recalculate tooltip
+        if (element != null) {
+          UIEventLogger.BreadcrumbShowTooltip.log(element.getProject(), element.getLanguage());
+        }
+      });
     }
     return tooltip;
   }
