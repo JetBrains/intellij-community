9d6223a5eacbeefd096fc9c2e48e88c56ddd33e8
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-12-08 15:48:18 +0100

    [threading] IJPL-222248: Fix read access in SearchForUsagesRunnable
    GitOrigin-RevId: efd70c895bc4913fc3d1a0638bc4d500002ad4bd

--- platform/usageView-impl/src/com/intellij/usages/impl/SearchForUsagesRunnable.java
+++ platform/usageView-impl/src/com/intellij/usages/impl/SearchForUsagesRunnable.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.usages.impl;
 
 import com.intellij.diagnostic.PerformanceWatcher;
@@ -9,6 +9,7 @@ import com.intellij.openapi.actionSystem.KeyboardShortcut;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.application.WriteIntentReadAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.colors.CodeInsightColors;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
@@ -237,7 +238,9 @@ final class SearchForUsagesRunnable implements Runnable {
       @Override
       protected void hyperlinkActivated(@NotNull HyperlinkEvent e) {
         if (e.getDescription().equals(UsageViewUtil.FIND_OPTIONS_HREF_TARGET)) {
-          FindManager.getInstance(myProject).showSettingsAndFindUsages(targets);
+          WriteIntentReadAction.run(() -> {
+            FindManager.getInstance(myProject).showSettingsAndFindUsages(targets);
+          });
         }
       }
     };
