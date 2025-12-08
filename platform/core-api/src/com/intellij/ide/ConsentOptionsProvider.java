a843a9478c32f932db156d749c78bce37a413431
Ilia Smirnov <ilia.smirnov@jetbrains.com>
2025-12-08 16:31:48 +0100

    Revert "IJPL-213050 Revert TRACE from 2025.3"
    This reverts commit 7e3a91cb and effectively enables the first part of TRACE functionality in 2025.3.1
    
    
    (cherry picked from commit 79975aded0643edd51afa392450842c1ec5c98b9)
    
    IJ-CR-183709
    
    GitOrigin-RevId: 830a90fb78f36241c9e0a4d482e32f11c61f200a

--- platform/core-api/src/com/intellij/ide/ConsentOptionsProvider.java
+++ platform/core-api/src/com/intellij/ide/ConsentOptionsProvider.java
@@ -1,6 +1,8 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide;
 
+import org.jetbrains.annotations.ApiStatus;
+
 public interface ConsentOptionsProvider {
 
   boolean isEAP();
@@ -10,4 +12,7 @@ public interface ConsentOptionsProvider {
   void setSendingUsageStatsAllowed(boolean allowed);
 
   boolean isSendingUsageStatsAllowed();
+
+  @ApiStatus.Internal
+  boolean isTraceDataCollectionAllowed();
 }
