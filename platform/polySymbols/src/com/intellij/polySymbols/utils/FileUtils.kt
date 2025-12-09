b49de9051db0d80246aadc0c8270c8a12c6a974a
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-12-09 10:38:30 +0100

    WEB-75484 CssTagClassesScope: improve check if file is indexed; fix issues with code completion
    GitOrigin-RevId: d8ef83ddcca09c35eeadb43ee54d00ca23c837dc

--- platform/polySymbols/src/com/intellij/polySymbols/utils/FileUtils.kt
+++ platform/polySymbols/src/com/intellij/polySymbols/utils/FileUtils.kt
@@ -4,8 +4,15 @@
 package com.intellij.polySymbols.utils
 
 import com.intellij.injected.editor.VirtualFileWindow
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileWithId
+import com.intellij.psi.stubs.StubUpdatingIndex
 import com.intellij.testFramework.LightVirtualFileBase
+import com.intellij.util.indexing.FileBasedIndex
+import com.intellij.util.indexing.FileBasedIndexEx
+import com.intellij.util.indexing.ID
+import com.intellij.util.indexing.IndexedFileImpl
 
 fun findOriginalFile(file: VirtualFile?): VirtualFile? =
   file?.let {
@@ -15,3 +22,13 @@ fun findOriginalFile(file: VirtualFile?): VirtualFile? =
     }
     f
   }
+
+@JvmOverloads
+fun isFileIndexed(project: Project, file: VirtualFile, indexId: ID<*, *> = StubUpdatingIndex.INDEX_ID): Boolean {
+  if (file !is VirtualFileWithId) return false
+
+  return (FileBasedIndex.getInstance() as FileBasedIndexEx)
+    .getIndex(indexId)
+    .getIndexingStateForFile(file.id, IndexedFileImpl(file, project))
+    .isNotIndexed.not()
+}
