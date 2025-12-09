6e7d61d64cb4ce3837acb5289bfbc5393faaca6c
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2025-12-09 12:30:36 +0200

    [lambda tests] AT-3038 Remove unnecessary annotation. Comments
    GitOrigin-RevId: 3b36418586a3ce45898f1cf8af5778becbfe0cf6

--- tools/intellij.lambda.sampleTestsWithPlugin/tests/com/intellij/lambda/sampleTestsWithPlugin/SampleTest.kt
+++ tools/intellij.lambda.sampleTestsWithPlugin/tests/com/intellij/lambda/sampleTestsWithPlugin/SampleTest.kt
@@ -31,6 +31,7 @@ class SampleTest {
     Assumptions.assumeThat(ide.rdSession.rdIdeType)
       .describedAs("works in both modes if headless is turned off for monolith in com.intellij.lambda.testFramework.starter.NewContextWithLambdaKt.newContextWithLambda" +
                    "as ProjectManager returns empty projects list in headless IJPL-221229")
+      // TODO: https://youtrack.jetbrains.com/issue/AT-3645/Lambda-tests-possibility-to-use-RunInMonolithAndSplitMode-annotation-on-test-methods
       .isNotIn(LambdaRdIdeType.MONOLITH)
     JpsEmptyProject.projectInfo.projectDir.resolve("src").resolve("FormattingExamplesExpected.java").let {
       if (!it.exists()) {
