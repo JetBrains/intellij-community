6235de87bf4cb8f69965ff5e17263c2e5254d834
Bas Leijdekkers <bas.leijdekkers@jetbrains.com>
2025-12-09 16:16:55 +0100

    IDEA-379518 [inlays] refresh saved settings after creating component
    otherwise settings modified check can be incorrect
    
    GitOrigin-RevId: c5d78b4357fd639864fbc1e3387c7083f6d59fcc

--- platform/lang-impl/src/com/intellij/codeInsight/hints/declarative/impl/DeclarativeHintsProviderSettingsModel.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/hints/declarative/impl/DeclarativeHintsProviderSettingsModel.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.hints.declarative.impl
 
 import com.intellij.codeInsight.hints.ImmediateConfigurable
@@ -85,6 +85,7 @@ class DeclarativeHintsProviderSettingsModel(
       row {
         cell(customSettingsProvider.createComponent(project, language))
       }
+      savedSettings = customSettingsProvider.getSettingsCopy()
     }
 
   override val description: String?
