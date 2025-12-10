87d0f572db0a37730249f921e6969d4bc7080e37
Jan Papesch <jan.papesch@jetbrains.com>
2025-12-10 10:38:33 +0100

    [serial monitor] CPP-47737, CPP-47047: Update to jSerialComm 2.11.4
    GitOrigin-RevId: 41c6db6e3c26167c5642d6e818f8cc3084ca60e5

--- platform/build-scripts/src/org/jetbrains/intellij/build/CommunityRepositoryModules.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/CommunityRepositoryModules.kt
@@ -238,7 +238,7 @@ object CommunityRepositoryModules {
 
       // jSerialComm native library
       spec.withGeneratedResources { targetDir, context ->
-        val uri = URI.create("https://packages.jetbrains.team/files/p/ij/intellij-build-dependencies/jSerialComm/25666bc98300c6fd674d3a03afd700714c6fe571/jSerialComm.zip")
+        val uri = URI.create("https://packages.jetbrains.team/files/p/ij/intellij-build-dependencies/jSerialComm/9a7813435b79aa2e23c7f2a78f1b66b48c0504c4/jSerialComm.zip")
         val downloaded = BuildDependenciesDownloader.downloadFileToCacheLocation(context.paths.communityHomeDirRoot, uri)
         BuildDependenciesDownloader.extractFile(downloaded, targetDir.resolve("bin"), context.paths.communityHomeDirRoot)
       }
