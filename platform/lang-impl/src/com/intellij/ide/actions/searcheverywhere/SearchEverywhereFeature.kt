8254538bd5b8d1a5274923dfb5f3f85eac8b3775
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2025-12-10 19:35:27 +0100

    [platform UI][se] IJPL-220342 Search Everywhere: remove internal key and turn on by the main key
    GitOrigin-RevId: bf3587d70ff8cce421193cc7c3237a6a9bac63fc

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereFeature.kt
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereFeature.kt
@@ -3,7 +3,6 @@ package com.intellij.ide.actions.searcheverywhere
 
 import com.intellij.frontend.FrontendApplicationInfo
 import com.intellij.frontend.FrontendType
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.util.PlatformUtils
 import org.jetbrains.annotations.ApiStatus
@@ -12,34 +11,20 @@ import org.jetbrains.annotations.TestOnly
 @ApiStatus.Internal
 object SearchEverywhereFeature {
   private const val PLATFORM_KEY = "search.everywhere.new.enabled"
-  private const val PLATFORM_INTERNAL_KEY = "search.everywhere.new.internal.enabled"
-
   private const val RIDER_KEY = "search.everywhere.new.rider.enabled"
   private const val CLION_KEY = "search.everywhere.new.clion.enabled"
   private const val CWM_CLIENT_KEY = "search.everywhere.new.cwm.client.enabled"
 
-  private val platformKey: String get() =
-    if (ApplicationManager.getApplication().isInternal) PLATFORM_INTERNAL_KEY
-    else PLATFORM_KEY
-
-  private val platformBasedProductKey: String? get() =
+  private val registryKey: String get() =
     if (isGuest) CWM_CLIENT_KEY
     else if (PlatformUtils.isRider()) RIDER_KEY
     else if (PlatformUtils.isCLion()) CLION_KEY
-    else null
+    else PLATFORM_KEY
 
   var isSplit: Boolean
-    get() =
-      Registry.`is`(platformKey, false) &&
-      (platformBasedProductKey?.let {
-        Registry.`is`(it, false)
-      } ?: true)
-
+    get() = Registry.`is`(registryKey, true)
     set(value) {
-      Registry.get(platformKey).setValue(value)
-      platformBasedProductKey?.let {
-        Registry.get(it).setValue(value)
-      }
+      Registry.get(registryKey).setValue(value)
     }
 
   private val isGuest: Boolean get() {
@@ -49,7 +34,6 @@ object SearchEverywhereFeature {
 
   val allRegistryKeys: List<String>
   @TestOnly get() = listOf(PLATFORM_KEY,
-                           PLATFORM_INTERNAL_KEY,
                            RIDER_KEY,
                            CLION_KEY,
                            CWM_CLIENT_KEY)
