16333977f125b3b51ee44977f22506c8364eaf09
Kajus Kuncaitis <kajus.kuncaitis@jetbrains.com>
2025-12-11 16:01:40 +0100

    [gradle][IDEA-381944] Move `KotlinGradleDependencyCompletionContributor` to the Gradle plugin
    GitOrigin-RevId: f6133a59b74df1972f18613cfb0da304acc7efd7

--- plugins/gradle/src/org/jetbrains/plugins/gradle/KotlinGradleDependencyCompletionContributor.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/KotlinGradleDependencyCompletionContributor.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.gradle.scripting.shared.completion
+package org.jetbrains.plugins.gradle
 
 import org.jetbrains.idea.completion.api.DependencyCompletionContext
 import org.jetbrains.idea.completion.api.*
