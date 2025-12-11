c4e0723683215c036b56e4bc5689c4c530b72f57
Bas Leijdekkers <bas.leijdekkers@jetbrains.com>
2025-12-11 09:41:12 +0100

    IDEA-234253 [java]: allow Rename on java file and only offer Rename File when Rename would rename the class
    GitOrigin-RevId: 3be347900e77e33d85ce6365dbbcdb3ccd4e7e9c

--- java/java-impl-refactorings/src/com/intellij/refactoring/rename/JavaVetoRenameCondition.java
+++ java/java-impl-refactorings/src/com/intellij/refactoring/rename/JavaVetoRenameCondition.java
@@ -1,30 +1,19 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.refactoring.rename;
 
-import com.intellij.openapi.roots.JavaProjectRootsUtil;
 import com.intellij.openapi.util.Condition;
-import com.intellij.psi.*;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiImplicitClass;
+import com.intellij.psi.PsiReceiverParameter;
 import com.intellij.psi.impl.light.LightMethod;
-import com.intellij.psi.util.FileTypeUtils;
 
 public final class JavaVetoRenameCondition implements Condition<PsiElement> {
   @Override
   public boolean value(PsiElement element) {
-    if (element instanceof LightMethod) {
-      PsiClass containingClass = ((LightMethod)element).getContainingClass();
-      if (containingClass.isEnum()) return true;
-    }
-    if (element instanceof PsiImplicitClass) {
-      return true;
-    }
+    if (element instanceof LightMethod method && method.getContainingClass().isEnum()) return true;
+    if (element instanceof PsiImplicitClass) return true;
+    if (element instanceof PsiReceiverParameter) return true;
 
-    if (element instanceof PsiReceiverParameter) {
-      return true;
-    }
-
-    return element instanceof PsiJavaFile &&
-           !FileTypeUtils.isInServerPageFile(element) &&
-           !JavaProjectRootsUtil.isOutsideJavaSourceRoot((PsiFile)element) &&
-           ((PsiJavaFile) element).getClasses().length > 0;
+    return false;
   }
 }
