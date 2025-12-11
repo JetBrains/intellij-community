497e2b0698321dc416b5ed3421e4b89040a0c2b8
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-12-11 21:24:09 +0300

    IJPL-207762 convert BetterPrefixMatcher to kotlin
    GitOrigin-RevId: 991cd8e2cd0163aedd577f88b77094221aeff2d9

--- platform/lang-impl/src/com/intellij/codeInsight/completion/impl/BetterPrefixMatcher.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/completion/impl/BetterPrefixMatcher.kt
@@ -1,105 +1,79 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.codeInsight.completion.impl;
+package com.intellij.codeInsight.completion.impl
 
-import com.intellij.codeInsight.completion.CompletionResult;
-import com.intellij.codeInsight.completion.CompletionResultSet;
-import com.intellij.codeInsight.completion.PrefixMatcher;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.codeStyle.MinusculeMatcher;
-import com.intellij.util.containers.FList;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.codeInsight.completion.CompletionResult
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.completion.PrefixMatcher
+import com.intellij.psi.codeStyle.MinusculeMatcher
 
-public class BetterPrefixMatcher extends PrefixMatcher {
-  private final PrefixMatcher myOriginal;
-  private final @Nullable CamelHumpMatcher myHumpMatcher;
-  private final int myMinMatchingDegree;
+open class BetterPrefixMatcher(
+  private val original: PrefixMatcher,
+  private val minMatchingDegree: Int,
+) : PrefixMatcher(original.prefix) {
+  private val humpMatcher: CamelHumpMatcher? = original as? CamelHumpMatcher
 
-  public BetterPrefixMatcher(PrefixMatcher original, int minMatchingDegree) {
-    super(original.getPrefix());
-    myOriginal = original;
-    myHumpMatcher = original instanceof CamelHumpMatcher ? (CamelHumpMatcher)original : null;
-    myMinMatchingDegree = minMatchingDegree;
+  open fun improve(result: CompletionResult): BetterPrefixMatcher {
+    val degree = RealPrefixMatchingWeigher.getBestMatchingDegree(result.lookupElement, result.prefixMatcher)
+    if (degree <= minMatchingDegree) return this
+
+    return createCopy(original, degree)
   }
 
-  public @NotNull BetterPrefixMatcher improve(CompletionResult result) {
-    int degree = RealPrefixMatchingWeigher.getBestMatchingDegree(result.getLookupElement(), result.getPrefixMatcher());
-    if (degree <= myMinMatchingDegree) return this;
+  protected open fun createCopy(original: PrefixMatcher, degree: Int): BetterPrefixMatcher =
+    BetterPrefixMatcher(original, degree)
 
-    return createCopy(myOriginal, degree);
+  override fun prefixMatches(name: String): Boolean =
+    prefixMatchesEx(name) == MatchingOutcome.BETTER_MATCH
+
+  protected open fun prefixMatchesEx(name: String): MatchingOutcome =
+    if (humpMatcher != null) matchOptimized(name, humpMatcher) else matchGeneric(name)
+
+  private fun matchGeneric(name: String): MatchingOutcome {
+    return when {
+      !original.prefixMatches(name) -> MatchingOutcome.NON_MATCH
+      !original.isStartMatch(name) -> MatchingOutcome.WORSE_MATCH
+      else -> if (original.matchingDegree(name) >= minMatchingDegree) MatchingOutcome.BETTER_MATCH else MatchingOutcome.WORSE_MATCH
+    }
   }
 
-  protected @NotNull BetterPrefixMatcher createCopy(PrefixMatcher original, int degree) {
-    return new BetterPrefixMatcher(original, degree);
+  private fun matchOptimized(name: String?, matcher: CamelHumpMatcher): MatchingOutcome {
+    val fragments = matcher.matchingFragments(name) ?: return MatchingOutcome.NON_MATCH
+    return when {
+      !MinusculeMatcher.isStartMatch(fragments) -> MatchingOutcome.WORSE_MATCH
+      matcher.matchingDegree(name, fragments) >= minMatchingDegree -> MatchingOutcome.BETTER_MATCH
+      else -> MatchingOutcome.WORSE_MATCH
+    }
   }
 
-  @Override
-  public boolean prefixMatches(@NotNull String name) {
-    return prefixMatchesEx(name) == MatchingOutcome.BETTER_MATCH;
-  }
-
-  protected @NotNull MatchingOutcome prefixMatchesEx(String name) {
-    return myHumpMatcher != null ? matchOptimized(name, myHumpMatcher) : matchGeneric(name);
-  }
-
-  private @NotNull MatchingOutcome matchGeneric(String name) {
-    if (!myOriginal.prefixMatches(name)) return MatchingOutcome.NON_MATCH;
-    if (!myOriginal.isStartMatch(name)) return MatchingOutcome.WORSE_MATCH;
-    return myOriginal.matchingDegree(name) >= myMinMatchingDegree ? MatchingOutcome.BETTER_MATCH : MatchingOutcome.WORSE_MATCH;
-  }
-
-  private @NotNull MatchingOutcome matchOptimized(String name, CamelHumpMatcher matcher) {
-    FList<TextRange> fragments = matcher.matchingFragments(name);
-    if (fragments == null) return MatchingOutcome.NON_MATCH;
-    if (!MinusculeMatcher.isStartMatch(fragments)) return MatchingOutcome.WORSE_MATCH;
-    return matcher.matchingDegree(name, fragments) >= myMinMatchingDegree ? MatchingOutcome.BETTER_MATCH : MatchingOutcome.WORSE_MATCH;
-  }
-
-  protected enum MatchingOutcome {
+  protected enum class MatchingOutcome {
     NON_MATCH, WORSE_MATCH, BETTER_MATCH
   }
 
-  @Override
-  public boolean isStartMatch(@NotNull String name) {
-    return myOriginal.isStartMatch(name);
-  }
+  override fun isStartMatch(name: String): Boolean =
+    original.isStartMatch(name)
 
-  @Override
-  public int matchingDegree(@NotNull String name) {
-    return myOriginal.matchingDegree(name);
-  }
+  override fun matchingDegree(name: String): Int =
+    original.matchingDegree(name)
 
-  @Override
-  public @NotNull PrefixMatcher cloneWithPrefix(@NotNull String prefix) {
-    return createCopy(myOriginal.cloneWithPrefix(prefix), myMinMatchingDegree);
-  }
+  override fun cloneWithPrefix(prefix: String): PrefixMatcher =
+    createCopy(original.cloneWithPrefix(prefix), minMatchingDegree)
 
-  public static final class AutoRestarting extends BetterPrefixMatcher {
-    private final CompletionResultSet myResult;
+  class AutoRestarting private constructor(
+    private val result: CompletionResultSet,
+    original: PrefixMatcher,
+    minMatchingDegree: Int,
+  ) : BetterPrefixMatcher(original, minMatchingDegree) {
+    constructor(result: CompletionResultSet) : this(result, result.prefixMatcher, Int.MIN_VALUE)
 
-    public AutoRestarting(@NotNull CompletionResultSet result) {
-      this(result, result.getPrefixMatcher(), Integer.MIN_VALUE);
-    }
+    override fun createCopy(original: PrefixMatcher, degree: Int): BetterPrefixMatcher =
+      AutoRestarting(result, original, degree)
 
-    private AutoRestarting(@NotNull CompletionResultSet result,
-                           @NotNull PrefixMatcher original,
-                           int minMatchingDegree) {
-      super(original, minMatchingDegree);
-      myResult = result;
-    }
-
-    @Override
-    protected @NotNull BetterPrefixMatcher createCopy(PrefixMatcher original, int degree) {
-      return new AutoRestarting(myResult, original, degree);
-    }
-
-    @Override
-    protected @NotNull MatchingOutcome prefixMatchesEx(String name) {
-      MatchingOutcome outcome = super.prefixMatchesEx(name);
+    override fun prefixMatchesEx(name: String): MatchingOutcome {
+      val outcome = super.prefixMatchesEx(name)
       if (outcome == MatchingOutcome.WORSE_MATCH) {
-        myResult.restartCompletionOnAnyPrefixChange();
+        result.restartCompletionOnAnyPrefixChange()
       }
-      return outcome;
+      return outcome
     }
   }
 }
