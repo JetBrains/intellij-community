adec5729c409c1ffc0cc2923c0f62270c3d5d800
Roman Golyshev <roman.golyshev@jetbrains.com>
2025-12-12 19:47:09 +0100

    [kotlin] KTIJ-36850 Move `CodeFragmentTestUtils.kt` to `intellij.kotlin.base.test`
    GitOrigin-RevId: 42b79fa490bf2f772d564a294008f8825d2c6985

--- plugins/kotlin/intellij.kotlin.base.test/test/org/jetbrains/kotlin/idea/debugger/evaluate/CodeFragmentTestUtils.kt
+++ plugins/kotlin/intellij.kotlin.base.test/test/org/jetbrains/kotlin/idea/debugger/evaluate/CodeFragmentTestUtils.kt
@@ -1,15 +1,14 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.debugger.evaluate
 
 import com.intellij.openapi.util.text.StringUtil
 import org.jetbrains.kotlin.idea.test.KotlinTestUtils
 import org.jetbrains.kotlin.psi.KtCodeFragment
 import org.jetbrains.kotlin.test.utils.withExtension
+import org.junit.Assert
 import java.io.File
-import kotlin.test.assertTrue
 
-internal fun KtCodeFragment.checkImports(testFile: File) {
+fun KtCodeFragment.checkImports(testFile: File) {
     val importList = importsAsImportList()
     val importsText = StringUtil.convertLineSeparators(importList?.text ?: "")
 
@@ -17,6 +16,6 @@ internal fun KtCodeFragment.checkImports(testFile: File) {
     if (importsAfterFile.exists()) {
         KotlinTestUtils.assertEqualsToFile(importsAfterFile, importsText)
     } else {
-        assertTrue(importsText.isEmpty(), "Unexpected imports found: $importsText")
+        Assert.assertTrue("Unexpected imports found: $importsText", importsText.isEmpty())
     }
 }
