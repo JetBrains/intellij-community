8bd6004aea8f637899c998b1353591c3ab2ac761
Roman Golyshev <roman.golyshev@jetbrains.com>
2025-12-12 21:32:41 +0100

    [kotlin] KTIJ-36850 Move `AbstractCodeFragmentHighlightingTest.kt` to `intellij.kotlin.base.test`
    GitOrigin-RevId: 5974a6f8b05cb82ccb2699c4db87adfe998896f2

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/debugger/evaluate/AbstractK1CodeFragmentHighlightingTest.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/debugger/evaluate/AbstractK1CodeFragmentHighlightingTest.kt
@@ -3,45 +3,12 @@
 package org.jetbrains.kotlin.idea.debugger.evaluate
 
 import com.intellij.codeInspection.InspectionProfileEntry
-import com.intellij.openapi.util.io.FileUtil
-import org.jetbrains.kotlin.checkers.AbstractKotlinHighlightVisitorTest
 import org.jetbrains.kotlin.idea.base.test.InTextDirectivesUtils
 import org.jetbrains.kotlin.idea.caches.resolve.resolveImportReference
-import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
 import org.jetbrains.kotlin.idea.util.ImportInsertHelper
-import org.jetbrains.kotlin.idea.util.application.executeWriteCommand
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtFile
 import java.io.File
-
-abstract class AbstractCodeFragmentHighlightingTest : KotlinLightCodeInsightFixtureTestCase() {
-    open fun doTest(filePath: String) {
-        configureByCodeFragment(filePath)
-        checkHighlighting(filePath)
-    }
-
-    protected open fun doTestWithImport(filePath: String) {
-        configureByCodeFragment(filePath)
-
-        project.executeWriteCommand("Imports insertion") {
-            val fileText = FileUtil.loadFile(File(filePath), true)
-            val file = myFixture.file as KtFile
-            InTextDirectivesUtils.findListWithPrefixes(fileText, "// IMPORT: ").forEach {
-                doImport(file, it)
-            }
-        }
-
-        checkHighlighting(filePath)
-    }
-
-    protected open fun doImport(file: KtFile, importName: String) {}
-
-    protected open fun checkHighlighting(filePath: String) {
-        myFixture.checkHighlighting(true, false, false)
-    }
-
-    protected abstract fun configureByCodeFragment(filePath: String)
-}
 
 abstract class AbstractK1CodeFragmentHighlightingTest : AbstractCodeFragmentHighlightingTest() {
     override fun doImport(file: KtFile, importName: String) {
