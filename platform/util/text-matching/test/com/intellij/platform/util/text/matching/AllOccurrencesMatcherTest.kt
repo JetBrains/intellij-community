318a04e9ed872f83319b6bdb7cd93f6bf887bad8
Alexander Zolotov <zolotov@jetbrains.com>
2025-12-12 11:02:04 +0100

    test [text-matching]: add more tests for handling matching mode in typo-tolerant and all-occurrences matchers
    GitOrigin-RevId: bc1754fe3f742c6d526ec593876291e78970d33b

--- platform/util/text-matching/test/com/intellij/platform/util/text/matching/AllOccurrencesMatcherTest.kt
+++ platform/util/text-matching/test/com/intellij/platform/util/text/matching/AllOccurrencesMatcherTest.kt
@@ -6,16 +6,54 @@ import com.intellij.psi.codeStyle.AllOccurrencesMatcher
 import com.intellij.util.text.matching.KeyboardLayoutConverter
 import com.intellij.util.text.matching.MatchingMode
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertNull
 import kotlin.test.assertEquals
 
 class AllOccurrencesMatcherTest {
   @Test
   fun simpleCase() {
     val matcher = AllOccurrencesMatcher.create("*fooBar", MatchingMode.IGNORE_CASE, "", KeyboardLayoutConverter.noop)
-    assertEquals(matcher.matchingFragments("fooBarFooBar")?.toList(), listOf(TextRange(0, 6), TextRange(6, 12)));
-    assertEquals(matcher.matchingFragments("fooBarFooBuzzBar")?.toList(), listOf(TextRange(0, 6), TextRange(6, 9), TextRange(13, 16)));
-    assertEquals(matcher.matchingFragments("fooBarBuzzFoo")?.toList(), listOf(TextRange(0, 6)));
-    assertEquals(matcher.matchingFragments("fooBarBuzzFooBuzzBar")?.toList(), listOf(TextRange(0, 6), TextRange(10, 13), TextRange(17, 20)));
-    assertEquals(matcher.matchingFragments("fooBuzzFooBuzzBar")?.toList(), listOf(TextRange(0, 3), TextRange(14, 17)));
+    assertEquals(listOf(TextRange(0, 6), TextRange(6, 12)), matcher.matchingFragments("fooBarFooBar")?.toList())
+    assertEquals(listOf(TextRange(0, 6), TextRange(6, 9), TextRange(13, 16)), matcher.matchingFragments("fooBarFooBuzzBar")?.toList())
+    assertEquals(listOf(TextRange(0, 6)), matcher.matchingFragments("fooBarBuzzFoo")?.toList())
+    assertEquals(listOf(TextRange(0, 6), TextRange(10, 13), TextRange(17, 20)), matcher.matchingFragments("fooBarBuzzFooBuzzBar")?.toList())
+    assertEquals(listOf(TextRange(0, 3), TextRange(14, 17)), matcher.matchingFragments("fooBuzzFooBuzzBar")?.toList())
+  }
+
+  @Test
+  fun testCaseInsensitive() {
+    val matcher = AllOccurrencesMatcher.create("*foo", MatchingMode.IGNORE_CASE, "", KeyboardLayoutConverter.noop)
+    assertEquals(listOf(TextRange(0, 3), TextRange(3, 6), TextRange(6, 9)), matcher.matchingFragments("fooFooFOO")?.toList())
+    assertEquals(listOf(TextRange(0, 3), TextRange(6, 9)), matcher.matchingFragments("FooBarfooBar")?.toList())
+  }
+
+  @Test
+  fun testCaseSensitive() {
+    val matcher = AllOccurrencesMatcher.create("*Foo", MatchingMode.MATCH_CASE, "", KeyboardLayoutConverter.noop)
+    assertEquals(listOf(TextRange(0, 3), TextRange(6, 9)), matcher.matchingFragments("FooBarFooBar")?.toList())
+    assertEquals(listOf(TextRange(0, 3)), matcher.matchingFragments("FooBarfooBar")?.toList())
+    assertNull(matcher.matchingFragments("fooBarFOOBar"))
+  }
+
+  @Test
+  fun testFirstLetterSensitive() {
+    val lowerMatcher = AllOccurrencesMatcher.create("*foo", MatchingMode.FIRST_LETTER, "", KeyboardLayoutConverter.noop)
+    assertEquals(listOf(TextRange(0, 3)), lowerMatcher.matchingFragments("fooBarfooBar")?.toList())
+    assertNull(lowerMatcher.matchingFragments("FooBarFooBar"))
+
+    val upperMatcher = AllOccurrencesMatcher.create("*Foo", MatchingMode.FIRST_LETTER, "", KeyboardLayoutConverter.noop)
+    assertEquals(listOf(TextRange(0, 3), TextRange(6, 9)), upperMatcher.matchingFragments("FooBarFooBar")?.toList())
+    assertNull(upperMatcher.matchingFragments("fooBarfooBar"))
+  }
+
+  @Test
+  fun testFirstLetterSensitiveWithWildcardLeadingSpace() {
+    val lowerMatcher = AllOccurrencesMatcher.create(" foo", MatchingMode.FIRST_LETTER, "", KeyboardLayoutConverter.noop)
+    assertEquals(listOf(TextRange(0, 3)), lowerMatcher.matchingFragments("fooBarfoo")?.toList())
+    assertNull(lowerMatcher.matchingFragments("FooBarFoo"))
+
+    val upperMatcher = AllOccurrencesMatcher.create(" Foo", MatchingMode.FIRST_LETTER, "", KeyboardLayoutConverter.noop)
+    assertEquals(listOf(TextRange(0, 3), TextRange(6, 9)), upperMatcher.matchingFragments("FooBarFoo")?.toList())
+    assertNull(upperMatcher.matchingFragments("fooBarfoo"))
   }
 }
--- platform/util/text-matching/test/com/intellij/platform/util/text/matching/TypoTolerantMatcherTest.kt
+++ platform/util/text-matching/test/com/intellij/platform/util/text/matching/TypoTolerantMatcherTest.kt
@@ -11,7 +11,6 @@ import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class TypoTolerantMatcherTest {
-
   @Test
   fun testStability() {
     val data = randomStrings(Random(1)).take(20000)
@@ -26,22 +25,6 @@ class TypoTolerantMatcherTest {
                           "agupnvSxdgnukGlpwxhyLxvp", "avuvzzgdvtSdqEgFqz", "asfKryfdl", "hasfiwopqfaoghaDcGokaoq", "ssbzBsmkDv",
                           "aqSSykiocfzWhajmtYHkshnm", "aiyADtwtlnkdYKqhdlj", "asfcqZgtHAqeOpcpFxp", "ssmw\$dsFzenxpyx1Ff")
     assertEquals(expected, matched, matched.joinToString(", ") { "\"$it\"" })
-  }
-
-  @Test
-  fun testEmptyPattern() {
-    val matcher = TypoTolerantMatcher("*", MatchingMode.IGNORE_CASE, "")
-    val data = listOf("foo", "bar", "buzz")
-    val matched = data.filter(matcher::matches)
-    val expected = listOf("foo", "bar", "buzz")
-    assertEquals(expected, matched)
-  }
-
-  @Test
-  fun testLongPattern() {
-    val matcher = NameUtil.buildMatcher("MyLongTestClassName").typoTolerant().build()
-    assertFalse(matcher is TypoTolerantMatcher)
-    assertTrue(matcher.matches("MyLongTestClassName"))
   }
 
   private fun randomStrings(random: Random): Sequence<String> = sequence {
@@ -60,4 +43,101 @@ class TypoTolerantMatcherTest {
       })
     }
   }
+
+  @Test
+  fun testEmptyPattern() {
+    val matcher = TypoTolerantMatcher("*", MatchingMode.IGNORE_CASE, "")
+    val data = listOf("foo", "bar", "buzz")
+    val matched = data.filter(matcher::matches)
+    val expected = listOf("foo", "bar", "buzz")
+    assertEquals(expected, matched)
+  }
+
+  @Test
+  fun testLongPattern() {
+    val matcher = NameUtil.buildMatcher("MyLongTestClassName").typoTolerant().build()
+    assertFalse(matcher is TypoTolerantMatcher)
+    assertTrue(matcher.matches("MyLongTestClassName"))
+  }
+
+  @Test
+  fun testCaseInsensitive() {
+    val matcher = TypoTolerantMatcher("foo", MatchingMode.IGNORE_CASE, "")
+    assertTrue(matcher.matches("foo"))
+    assertTrue(matcher.matches("Foo"))
+    assertTrue(matcher.matches("FOO"))
+    assertTrue(matcher.matches("fOo"))
+    assertTrue(matcher.matches("FooBar"))
+    assertTrue(matcher.matches("foobar"))
+  }
+
+  @Test
+  fun testCaseSensitive() {
+    val matcher = TypoTolerantMatcher("Foo", MatchingMode.MATCH_CASE, "")
+    assertTrue(matcher.matches("FooBar"))
+    assertTrue(matcher.matches("Foobar"))
+    assertFalse(matcher.matches("foo"))
+    assertFalse(matcher.matches("FOO"))
+    assertFalse(matcher.matches("fooBar"))
+  }
+
+  @Test
+  fun testFirstLetterSensitive() {
+    val lowerMatcher = TypoTolerantMatcher("foo", MatchingMode.FIRST_LETTER, "")
+    assertTrue(lowerMatcher.matches("fooBar"))
+    assertTrue(lowerMatcher.matches("foobar"))
+    assertFalse(lowerMatcher.matches("FooBar"), "Should not match: first letter case mismatch")
+    assertFalse(lowerMatcher.matches("Foobar"), "Should not match: first letter case mismatch")
+
+    val upperMatcher = TypoTolerantMatcher("Foo", MatchingMode.FIRST_LETTER, "")
+    assertTrue(upperMatcher.matches("FooBar"))
+    assertTrue(upperMatcher.matches("Foobar"))
+    assertFalse(upperMatcher.matches("fooBar"), "Should not match: first letter case mismatch")
+    assertFalse(upperMatcher.matches("foobar"), "Should not match: first letter case mismatch")
+  }
+
+  @Test
+  fun testFirstLetterSensitiveLeadingSpace() {
+    val lowerMatcher = TypoTolerantMatcher(" foo", MatchingMode.FIRST_LETTER, "")
+    assertTrue(lowerMatcher.matches("fooBar"), "Should match: both start with lowercase 'f'")
+    assertTrue(lowerMatcher.matches("foo bar"), "Should match: both start with lowercase 'f'")
+    assertFalse(lowerMatcher.matches("FooBar"), "Should NOT match: name starts with 'F', pattern starts with 'f'")
+    assertFalse(lowerMatcher.matches("Foo bar"), "Should NOT match: name starts with 'F', pattern starts with 'f'")
+
+    val upperMatcher = TypoTolerantMatcher(" Foo", MatchingMode.FIRST_LETTER, "")
+    assertTrue(upperMatcher.matches("FooBar"), "Should match: both start with uppercase 'F'")
+    assertTrue(upperMatcher.matches("Foo bar"), "Should match: both start with uppercase 'F'")
+    assertFalse(upperMatcher.matches("fooBar"), "Should NOT match: name starts with 'f', pattern starts with 'F'")
+    assertFalse(upperMatcher.matches("foo Bar"), "Should NOT match: name starts with 'f', pattern starts with 'F'")
+  }
+
+  @Test
+  fun testFirstLetterSensitiveLeadingWildcard() {
+    val lowerMatcher = TypoTolerantMatcher("*foo", MatchingMode.FIRST_LETTER, "")
+    assertTrue(lowerMatcher.matches("fooBar"), "Should match: both start with lowercase 'f'")
+    assertTrue(lowerMatcher.matches("foo"), "Should match: both start with lowercase 'f'")
+    assertFalse(lowerMatcher.matches("FooBar"), "Should NOT match: name starts with 'F', pattern starts with 'f'")
+
+    val upperMatcher = TypoTolerantMatcher("*Foo", MatchingMode.FIRST_LETTER, "")
+    assertTrue(upperMatcher.matches("FooBar"), "Should match: both start with uppercase 'F'")
+    assertTrue(upperMatcher.matches("Foo"), "Should match: both start with uppercase 'F'")
+    assertFalse(upperMatcher.matches("fooBar"), "Should NOT match: name starts with 'f', pattern starts with 'F'")
+  }
+
+  @Test
+  fun testCaseSensitivityWithTypos() {
+    val insensitiveMatcher = TypoTolerantMatcher("fooBar", MatchingMode.IGNORE_CASE, "")
+    assertTrue(insensitiveMatcher.matches("FooBar"))
+    assertTrue(insensitiveMatcher.matches("fooBra"))
+
+    val sensitiveMatcher = TypoTolerantMatcher("FooBar", MatchingMode.MATCH_CASE, "")
+    assertTrue(sensitiveMatcher.matches("FooBar"))
+    assertFalse(sensitiveMatcher.matches("fooBar"), "Case mismatch should prevent match")
+
+    val firstLetterMatcher = TypoTolerantMatcher("FooBar", MatchingMode.FIRST_LETTER, "")
+    assertTrue(firstLetterMatcher.matches("FooBar"))
+    assertTrue(firstLetterMatcher.matches("Foobar"))
+    assertFalse(firstLetterMatcher.matches("fooBar"), "First letter case mismatch should prevent match")
+  }
+
 }
