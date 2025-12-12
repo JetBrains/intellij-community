50fef82bed994903530965113de4aa4232b8f414
Roman Golyshev <roman.golyshev@jetbrains.com>
2025-12-12 17:51:51 +0100

    [kotlin] KTIJ-36850 Move `AbstractCopyPasteTest.kt` to `intellij.kotlin.base.test`
    GitOrigin-RevId: 0e4863c42ebb51b38a18965a297b19d220855148

--- plugins/kotlin/intellij.kotlin.base.test/test/org/jetbrains/kotlin/idea/AbstractCopyPasteTest.kt
+++ plugins/kotlin/intellij.kotlin.base.test/test/org/jetbrains/kotlin/idea/AbstractCopyPasteTest.kt
@@ -1,5 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea
 
 import com.intellij.codeInsight.CodeInsightSettings
@@ -46,4 +45,3 @@ abstract class AbstractCopyPasteTest : KotlinLightCodeInsightFixtureTestCase() {
         }
     }
 }
-
