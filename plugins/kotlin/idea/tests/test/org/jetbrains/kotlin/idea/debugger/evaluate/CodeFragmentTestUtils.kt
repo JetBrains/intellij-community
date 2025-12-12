dc19b917da20c9ac69afce60cf77276ad972f14c
Roman Golyshev <roman.golyshev@jetbrains.com>
2025-12-12 19:36:22 +0100

    [kotlin] KTIJ-36850 Split `CodeFragmentTestUtils.kt` into K1 and K2 files
    GitOrigin-RevId: 155a06c891c8d4c374ea7e448db62e5fabd2aa8a

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/debugger/evaluate/CodeFragmentTestUtils.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/debugger/evaluate/CodeFragmentTestUtils.kt
@@ -2,31 +2,9 @@
 
 package org.jetbrains.kotlin.idea.debugger.evaluate
 
-import com.intellij.openapi.application.runReadAction
-import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.text.StringUtil
-import com.intellij.psi.JavaPsiFacade
-import com.intellij.psi.PsiElement
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture
-import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
-import org.jetbrains.kotlin.analysis.api.analyze
-import org.jetbrains.kotlin.idea.base.test.InTextDirectivesUtils
-import org.jetbrains.kotlin.idea.caches.resolve.analyzeWithContent
-import org.jetbrains.kotlin.idea.completion.test.ExpectedCompletionUtils
-import org.jetbrains.kotlin.idea.completion.test.ExpectedCompletionUtils.BLOCK_CODE_FRAGMENT
-import org.jetbrains.kotlin.idea.core.util.CodeFragmentUtils
-import org.jetbrains.kotlin.idea.debugger.core.CodeFragmentContextTuner
-import org.jetbrains.kotlin.idea.debugger.evaluate.util.KotlinK2CodeFragmentUtils
 import org.jetbrains.kotlin.idea.test.KotlinTestUtils
-import org.jetbrains.kotlin.name.ClassId
-import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtCodeFragment
-import org.jetbrains.kotlin.psi.KtElement
-import org.jetbrains.kotlin.psi.KtPsiFactory
-import org.jetbrains.kotlin.psi.KtTypeReference
-import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.test.utils.withExtension
 import java.io.File
 import kotlin.test.assertTrue
@@ -40,91 +18,5 @@ internal fun KtCodeFragment.checkImports(testFile: File) {
         KotlinTestUtils.assertEqualsToFile(importsAfterFile, importsText)
     } else {
         assertTrue(importsText.isEmpty(), "Unexpected imports found: $importsText")
-    }
-}
-
-@OptIn(KaExperimentalApi::class)
-fun JavaCodeInsightTestFixture.configureByK2ModeCodeFragment(filePath: String) {
-    configureByFile(File(filePath).name)
-
-    val elementAt = file?.findElementAt(caretOffset)
-
-    val isBlock = InTextDirectivesUtils.isDirectiveDefined(file.text, BLOCK_CODE_FRAGMENT)
-    val file = createK2ModeCodeFragment(filePath, elementAt!!, isBlock)
-
-    val typeStr = InTextDirectivesUtils.findStringWithPrefixes(getFile().text, "// ${ExpectedCompletionUtils.RUNTIME_TYPE} ")
-
-    file.putCopyableUserData(KotlinK2CodeFragmentUtils.RUNTIME_TYPE_EVALUATOR_K2) { expression ->
-        if (typeStr != null) {
-            analyze(expression) {
-                val kaType = buildClassType(ClassId.topLevel(FqName(typeStr)))
-                kaType.createPointer()
-            }
-        } else {
-            null
-        }
-    }
-    configureFromExistingVirtualFile(file.virtualFile!!)
-}
-
-@OptIn(KaExperimentalApi::class)
-internal fun JavaCodeInsightTestFixture.configureByK1ModeCodeFragment(filePath: String) {
-    configureByFile(File(filePath).name)
-
-    val elementAt = file?.findElementAt(caretOffset)
-
-    val isBlock = InTextDirectivesUtils.isDirectiveDefined(file.text, BLOCK_CODE_FRAGMENT)
-    val file = createK1ModeCodeFragment(filePath, elementAt!!, isBlock)
-
-    val typeStr = InTextDirectivesUtils.findStringWithPrefixes(getFile().text, "// ${ExpectedCompletionUtils.RUNTIME_TYPE} ")
-
-    file.putCopyableUserData(CodeFragmentUtils.RUNTIME_TYPE_EVALUATOR) {
-        if (typeStr != null) {
-            val codeFragment = KtPsiFactory(project).createBlockCodeFragment(
-                "val xxx: $typeStr",
-                PsiTreeUtil.getParentOfType(elementAt, KtElement::class.java)
-            )
-            val context = codeFragment.analyzeWithContent()
-            val typeReference: KtTypeReference =
-                PsiTreeUtil.getChildOfType(codeFragment.getContentElement().firstChild, KtTypeReference::class.java)!!
-            context[BindingContext.TYPE, typeReference]
-        } else {
-            null
-        }
-    }
-    configureFromExistingVirtualFile(file.virtualFile!!)
-}
-
-private fun createK2ModeCodeFragment(filePath: String, contextElement: PsiElement, isBlock: Boolean): KtCodeFragment {
-    val contextTuner = CodeFragmentContextTuner.getInstance()
-    val effectiveContextElement = contextTuner.tuneContextElement(contextElement)
-
-    val fileNameForFragment = "$filePath.fragment.k2"
-
-    val fileForFragment = File(fileNameForFragment)
-    val codeFragmentText = FileUtil.loadFile(fileForFragment, true).trim()
-    val psiFactory = KtPsiFactory(contextElement.project)
-
-    return if (isBlock) {
-        psiFactory.createBlockCodeFragment(codeFragmentText, effectiveContextElement)
-    } else {
-        psiFactory.createExpressionCodeFragment(codeFragmentText, effectiveContextElement)
-    }
-}
-
-private fun createK1ModeCodeFragment(filePath: String, contextElement: PsiElement, isBlock: Boolean): KtCodeFragment {
-    val contextTuner = CodeFragmentContextTuner.getInstance()
-    val effectiveContextElement = contextTuner.tuneContextElement(contextElement)
-
-    val fileNameForFragment = "$filePath.fragment"
-
-    val fileForFragment = File(fileNameForFragment)
-    val codeFragmentText = FileUtil.loadFile(fileForFragment, true).trim()
-    val psiFactory = KtPsiFactory(contextElement.project)
-
-    return if (isBlock) {
-        psiFactory.createBlockCodeFragment(codeFragmentText, effectiveContextElement)
-    } else {
-        psiFactory.createExpressionCodeFragment(codeFragmentText, effectiveContextElement)
     }
 }
