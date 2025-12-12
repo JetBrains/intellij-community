269038557eeec5563311637fcfd1e1b0c75861b4
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-12-12 10:49:26 +0100

    IJPL-220159 move validatePluginIsCompatible to PluginInitializationContext
    GitOrigin-RevId: 55029c539d7fcdab475b8451ff31c45ff714f1d1

--- platform/core-impl/src/com/intellij/ide/plugins/PluginInitializationContext.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/PluginInitializationContext.kt
@@ -1,7 +1,9 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.plugins
 
+import com.intellij.core.CoreBundle
 import com.intellij.ide.plugins.ProductPluginInitContext.Companion.configureProductModeModules
+import com.intellij.idea.AppMode
 import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.util.BuildNumber
 import org.jetbrains.annotations.ApiStatus
@@ -89,6 +91,29 @@ interface PluginInitializationContext {
         }
       }
   }
+}
+
+@ApiStatus.Internal
+fun PluginInitializationContext.validatePluginIsCompatible(plugin: PluginMainDescriptor): PluginNonLoadReason? {
+  if (isPluginWhichDependsOnKotlinPluginAndItsIncompatibleWithIt(plugin)) {
+    // disable plugins which are incompatible with the Kotlin Plugin K1/K2 Modes KTIJ-24797, KTIJ-30474
+    val mode = if (isKotlinPluginK1Mode()) CoreBundle.message("plugin.loading.error.k1.mode") else CoreBundle.message("plugin.loading.error.k2.mode")
+    return PluginIsIncompatibleWithKotlinMode(plugin, mode)
+  }
+  if (plugin.isBundled) {
+    return null
+  }
+  if (AppMode.isDisableNonBundledPlugins()) {
+    return NonBundledPluginsAreExplicitlyDisabled(plugin)
+  }
+  PluginManagerCore.checkBuildNumberCompatibility(plugin, productBuildNumber)?.let {
+    return it
+  }
+  // "Show broken plugins in Settings | Plugins so that users can uninstall them and resolve 'Plugin Error' (IDEA-232675)"
+  if (isPluginBroken(plugin.pluginId, plugin.version)) {
+    return PluginIsMarkedBroken(plugin)
+  }
+  return null
 }
 
 @ApiStatus.Internal
