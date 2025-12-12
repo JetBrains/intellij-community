3087d06b57af04a6c2be7113944c28e23eab69fc
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-12-12 16:46:03 +0100

    IJPL-220159 add AmbiguousPluginSet and tests
    Space-RevId: 156fd6ac6f0737e6e7d16bd6f760c5284afbc659
    
    GitOrigin-RevId: 4772d75286ab08d2041fe948485954b1de047b79

--- platform/core-impl/src/com/intellij/ide/plugins/UnambiguousPluginSet.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/UnambiguousPluginSet.kt
@@ -7,6 +7,8 @@ import org.jetbrains.annotations.ApiStatus
 /**
  * An instance of [UnambiguousPluginSet] represents a set of plugins that do not conflict
  * with each other by declared plugin and content module ids (including plugin aliases).
+ *
+ * @see AmbiguousPluginSet
  */
 @ApiStatus.Internal
 interface UnambiguousPluginSet {
@@ -27,6 +29,33 @@ interface UnambiguousPluginSet {
   fun getFullPluginIdMapping(): Map<PluginId, PluginModuleDescriptor>
 
   fun getFullContentModuleIdMapping(): Map<PluginModuleId, ContentModuleDescriptor>
+
+  companion object
+}
+
+/**
+ * Counterpart to [UnambiguousPluginSet], plugin set may contain multiple versions of the same plugin,
+ * or there can be id conflicts between plugins/modules.
+ * Resolve methods return all applicable entries.
+ */
+@ApiStatus.Internal
+interface AmbiguousPluginSet {
+  val plugins: List<PluginMainDescriptor>
+  /**
+   * Plugin id can resolve either as a regular plugin id, or as a plugin alias,
+   * in which case it may refer either to the plugin descriptor module or to the plugin content module.
+   */
+  fun resolvePluginId(id: PluginId): Sequence<PluginModuleDescriptor>
+
+  fun resolveContentModuleId(id: PluginModuleId): Sequence<ContentModuleDescriptor>
+
+  /**
+   * Plugin id can resolve either as a regular plugin id, or as a plugin alias,
+   * in which case it may refer either to the plugin descriptor module or to the plugin content module.
+   */
+  fun getFullPluginIdMapping(): Map<PluginId, List<PluginModuleDescriptor>>
+
+  fun getFullContentModuleIdMapping(): Map<PluginModuleId, List<ContentModuleDescriptor>>
 
   companion object
 }
