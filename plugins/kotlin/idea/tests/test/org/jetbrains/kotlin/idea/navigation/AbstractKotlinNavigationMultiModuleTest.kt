2aeff9f8508770c50c83792d24cf8ac5c50919c6
Roman Golyshev <roman.golyshev@jetbrains.com>
2025-12-13 01:14:44 +0100

    [kotlin] KTIJ-36850 Move `AbstractKotlinGotoRelatedSymbolMultiModuleTest.kt` to `intellij.kotlin.base.test`
    GitOrigin-RevId: afce87e5b4a8703e4c086e1d689e96e5097473a5

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/navigation/AbstractKotlinNavigationMultiModuleTest.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/navigation/AbstractKotlinNavigationMultiModuleTest.kt
@@ -3,12 +3,7 @@
 package org.jetbrains.kotlin.idea.navigation
 
 import com.intellij.codeInsight.navigation.GotoTargetHandler
-import com.intellij.codeInsight.navigation.collectRelatedItems
-import com.intellij.openapi.actionSystem.impl.SimpleDataContext
-import com.intellij.openapi.application.readAction
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.progress.runBlockingMaybeCancellable
-import com.intellij.platform.ide.progress.withBackgroundProgress
 import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.idea.codeInsight.GotoSuperActionHandler
 import org.jetbrains.kotlin.idea.test.IDEA_TEST_DATA_DIR
@@ -30,19 +25,3 @@ abstract class AbstractKotlinGotoSuperMultiModuleTest : AbstractKotlinNavigation
     }
 }
 
-abstract class AbstractKotlinGotoRelatedSymbolMultiModuleTest : AbstractKotlinNavigationMultiModuleTest() {
-    override fun getTestDataDirectory(): File = IDEA_TEST_DATA_DIR.resolve("navigation/relatedSymbols/multiModule")
-
-    override fun doNavigate(editor: Editor, file: PsiFile): GotoTargetHandler.GotoData {
-        val source = file.findElementAt(editor.caretModel.offset)!!
-        val relatedItems =
-            runBlockingMaybeCancellable {
-                withBackgroundProgress(project, "Test Android Gradle Sync") {
-                    readAction {
-                        collectRelatedItems(contextElement = source, dataContext = SimpleDataContext.EMPTY_CONTEXT)
-                    }
-                }
-            }
-        return GotoTargetHandler.GotoData(source, relatedItems.map { it.element }.toTypedArray(), emptyList())
-    }
-}
