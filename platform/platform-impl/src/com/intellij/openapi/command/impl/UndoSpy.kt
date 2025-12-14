1bbc934d2ae30480d487f50c40a0de25d2eff229
Alexander Trushev <aleksandr.trushev@jetbrains.com>
2025-12-14 22:38:46 +0100

    [undo] IJPL-201535 Create topic to listen to change accumulation sessions
    GitOrigin-RevId: c208c435465b9b4ddee38dfebf7e1ae9fa5601e0

--- platform/platform-impl/src/com/intellij/openapi/command/impl/UndoSpy.kt
+++ platform/platform-impl/src/com/intellij/openapi/command/impl/UndoSpy.kt
@@ -3,7 +3,6 @@ package com.intellij.openapi.command.impl
 
 import com.intellij.ide.impl.UndoRemoteBehaviorService
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.command.undo.DocumentReference
 import com.intellij.openapi.command.undo.UndoableAction
 import com.intellij.openapi.components.service
 import com.intellij.openapi.fileEditor.FileEditor
