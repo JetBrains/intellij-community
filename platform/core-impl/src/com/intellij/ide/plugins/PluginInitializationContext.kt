d3f4d1018ff3330b02b4ab3b3322c488f246098a
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-12-15 19:06:40 +0100

    IJPL-220159 extract selectPluginsToLoad
    Space-RevId: 7a310e92e1e951919de308e6ac555b5dfad59809
    
    GitOrigin-RevId: c169c11f9ad6c84ecda4f7357b1921cef769f18f

--- platform/core-impl/src/com/intellij/ide/plugins/PluginInitializationContext.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/PluginInitializationContext.kt
@@ -6,7 +6,6 @@ import com.intellij.ide.plugins.ProductPluginInitContext.Companion.configureProd
 import com.intellij.idea.AppMode
 import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.util.BuildNumber
-import com.intellij.util.text.VersionComparatorUtil
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.TestOnly
 
@@ -122,70 +121,3 @@ fun PluginInitializationContext.validatePluginIsCompatible(plugin: PluginMainDes
 
 @ApiStatus.Internal
 data class PluginsPerProjectConfig(val isMainProcess: Boolean)
-
-/**
- * Selects plugins to load by applying all business logic for plugin selection.
- * This is a single comprehensive phase that:
- * - Filters out disabled plugins
- * - Validates plugin compatibility
- * - Selects the most recent version per plugin ID
- *
- * @param onPluginExcluded Callback invoked for each excluded plugin
- * @return Filtered list containing only plugins selected for loading.
- *         Returns the original list if no exclusions occurred, otherwise returns filtered lists.
- */
-@ApiStatus.Internal
-fun PluginInitializationContext.selectPluginsToLoad(
-  discoveredPlugins: List<DiscoveredPluginsList>,
-  onPluginExcluded: (PluginMainDescriptor, PluginNonLoadReason) -> Unit,
-): List<DiscoveredPluginsList> {
-  if (discoveredPlugins.isEmpty()) {
-    return emptyList()
-  }
-
-  val selectedPluginsByPluginId = LinkedHashMap<PluginId, PluginMainDescriptor>()
-  var hasExclusions = false
-  // name shadowing is intended
-  val onPluginExcluded: (PluginMainDescriptor, PluginNonLoadReason) -> Unit = { plugin, reason ->
-    hasExclusions = true
-    onPluginExcluded(plugin, reason)
-  }
-
-  for (pluginList in discoveredPlugins) {
-    for (plugin in pluginList.plugins) {
-      if (isPluginDisabled(plugin.pluginId)) {
-        onPluginExcluded(plugin, PluginIsMarkedDisabled(plugin))
-        continue
-      }
-      validatePluginIsCompatible(plugin)?.let { reason ->
-        onPluginExcluded(plugin, reason)
-        continue
-      }
-      
-      val pluginId = plugin.pluginId
-      val existingPlugin = selectedPluginsByPluginId[pluginId]
-      if (existingPlugin == null) {
-        selectedPluginsByPluginId[pluginId] = plugin
-        continue
-      }
-
-      // plugins added via property shouldn't be overridden to avoid plugin root detection issues when running external plugin tests
-      if (VersionComparatorUtil.compare(plugin.version, existingPlugin.version) > 0 ||
-          pluginList.source is PluginsSourceContext.SystemPropertyProvided) {
-        onPluginExcluded(existingPlugin, PluginVersionIsSuperseded(existingPlugin, plugin))
-        selectedPluginsByPluginId[pluginId] = plugin
-      }
-      else {
-        onPluginExcluded(plugin, PluginVersionIsSuperseded(plugin, existingPlugin))
-      }
-    }
-  }
-
-  if (!hasExclusions) {
-    return discoveredPlugins
-  }
-  return discoveredPlugins.map { pluginList ->
-    val filteredPlugins = pluginList.plugins.filter { selectedPluginsByPluginId[it.pluginId] === it }
-    DiscoveredPluginsList(filteredPlugins, pluginList.source)
-  }
-}
