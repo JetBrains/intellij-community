025c277e9a8d959b784c850af3f8699039105d72
Alexander Trushev <aleksandr.trushev@jetbrains.com>
2025-12-15 21:22:42 +0100

    [undo] IJPL-201535 Move undo spy base class to common rd module
    GitOrigin-RevId: 8aefba8339e8c88d90bbacb1615d5909bec71864

--- platform/platform-impl/src/com/intellij/openapi/command/impl/UndoSpy.kt
+++ platform/platform-impl/src/com/intellij/openapi/command/impl/UndoSpy.kt
@@ -4,7 +4,7 @@ package com.intellij.openapi.command.impl
 import com.intellij.ide.impl.UndoRemoteBehaviorService
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.undo.UndoableAction
-import com.intellij.openapi.components.service
+import com.intellij.openapi.components.serviceOrNull
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
 import org.jetbrains.annotations.ApiStatus
@@ -30,7 +30,7 @@ interface UndoSpy {
       return ProgressManager.getInstance().computeInNonCancelableSection<UndoSpy?, Exception> {
         if (UndoRemoteBehaviorService.isSpeculativeUndoEnabled()) {
           val application = ApplicationManager.getApplication()
-          application?.service<UndoSpy>()
+          application?.serviceOrNull<UndoSpy>()
         } else {
           null
         }
--- platform/platform-impl/src/com/intellij/openapi/command/impl/UndoableActionType.java
+++ platform/platform-impl/src/com/intellij/openapi/command/impl/UndoableActionType.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.command.impl;
 
-
 import com.intellij.openapi.command.undo.*;
 import org.jetbrains.annotations.ApiStatus.Experimental;
 import org.jetbrains.annotations.ApiStatus.Internal;
