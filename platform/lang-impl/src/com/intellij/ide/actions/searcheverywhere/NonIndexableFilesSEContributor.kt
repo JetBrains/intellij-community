657b4520d3331650617162f0702bbe66a5ae4037
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2025-12-15 14:06:00 +0100

    [pltform ui][se] IJPL-223249 Make NonIndexableFilesSEContributor providing extended info
    GitOrigin-RevId: 67c1accbdb363dda19961eaf7936f4d01ad1a943

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/NonIndexableFilesSEContributor.kt
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/NonIndexableFilesSEContributor.kt
@@ -4,6 +4,7 @@ package com.intellij.ide.actions.searcheverywhere
 import com.intellij.ide.IdeBundle
 import com.intellij.ide.actions.GotoFileItemProvider
 import com.intellij.ide.actions.SearchEverywherePsiRenderer
+import com.intellij.ide.actions.searcheverywhere.footer.createPsiExtendedInfo
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.application.runReadAction
@@ -48,7 +49,10 @@ interface FilesTabSEContributor {
 private val LOG = Logger.getInstance(NonIndexableFilesSEContributor::class.java)
 
 @ApiStatus.Internal
-class NonIndexableFilesSEContributor(event: AnActionEvent) : WeightedSearchEverywhereContributor<Any>, DumbAware, FilesTabSEContributor {
+class NonIndexableFilesSEContributor(event: AnActionEvent) : WeightedSearchEverywhereContributor<Any>,
+                                                             DumbAware,
+                                                             FilesTabSEContributor,
+                                                             SearchEverywhereExtendedInfoProvider {
   private val project: Project = event.project!!
   private val navigationHandler: SearchEverywhereNavigationHandler = FileSearchEverywhereNavigationContributionHandler(project)
 
@@ -166,6 +170,7 @@ class NonIndexableFilesSEContributor(event: AnActionEvent) : WeightedSearchEvery
     }
   }
 
+  override fun createExtendedInfo(): @Nls ExtendedInfo = createPsiExtendedInfo()
 
   @ApiStatus.Internal
   class Factory : SearchEverywhereContributorFactory<Any?> {
