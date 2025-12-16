64dd3e99b60e339af43bef8d4fe6a0fc092259c4
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2025-12-16 17:58:43 +0100

    [command-completion] IJPL-223668 IOOBE: CharSequenceSubSequence via CommandCompletionProviderKt.findCommandCompletionType
    GitOrigin-RevId: d2dfd172bd1e81ba9d6cde3fa161bf4d7f470b25

--- platform/lang-impl/src/com/intellij/codeInsight/completion/command/CommandCompletionProvider.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/completion/command/CommandCompletionProvider.kt
@@ -624,11 +624,15 @@ internal fun findCommandCompletionType(
                                                text.substring(offset - indexOf, offset - indexOf + 1))
   }
   //two points
-  else if (offset - indexOf + 2 <= text.length && text.substring(offset - indexOf, offset - indexOf + 2) == suffix) {
+  else if (offset - indexOf + 2 <= text.length &&
+           offset >= offset - indexOf + 2 &&
+           text.substring(offset - indexOf, offset - indexOf + 2) == suffix) {
     return InvocationCommandType.FullSuffix(text.substring(offset - indexOf + 2, offset),
                                             text.substring(offset - indexOf, offset - indexOf + 2))
   }
-  if (indexOf > 0 && text.substring(offset - indexOf).startsWith(factory.suffix())) {
+  if (indexOf > 0 &&
+      offset >= offset - indexOf + 1 &&
+      text.substring(offset - indexOf).startsWith(factory.suffix())) {
     //force call with one point
     return InvocationCommandType.PartialSuffix(text.substring(offset - indexOf + 1, offset),
                                                text.substring(offset - indexOf, offset - indexOf + 1))
