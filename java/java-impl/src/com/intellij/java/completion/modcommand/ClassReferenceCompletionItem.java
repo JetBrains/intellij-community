2c84b84cfae90109a1e348512e0871e84b20856d
Tagir Valeev <tagir.valeev@jetbrains.com>
2025-12-16 15:16:50 +0100

    IJPL-214795 [java-completion] Move to another package
    GitOrigin-RevId: a44a67352728fdcb4a121c1b5fe88a5bdd52501a

--- java/java-impl/src/com/intellij/java/completion/modcommand/ClassReferenceCompletionItem.java
+++ java/java-impl/src/com/intellij/java/completion/modcommand/ClassReferenceCompletionItem.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.codeInsight.completion.modcompletion;
+package com.intellij.java.completion.modcommand;
 
 import com.intellij.application.options.CodeStyle;
 import com.intellij.codeInsight.completion.AllClassesGetter;
