3d9d854e6d66d80a31f25b587cad8251cb74bddd
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2025-12-16 11:38:27 +0100

    IJPL-220159 select plugins to load: handle explicitPluginSubsetToLoad and disablePluginLoadingCompletely options
    follows the logic from PluginManagerCore.selectPluginsForLoading
    
    Space-RevId: e653e4de498efa43b30acc42ff4635508f072cd2
    
    GitOrigin-RevId: 827997b5a7ee079758563b2c4b5b760a078f973c

--- platform/core-impl/src/com/intellij/ide/plugins/pluginInitContextSelectPluginsToLoad.kt
+++ platform/core-impl/src/com/intellij/ide/plugins/pluginInitContextSelectPluginsToLoad.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.plugins
 
+import com.intellij.ide.plugins.PluginManagerCore.CORE_ID
 import com.intellij.openapi.extensions.PluginId
 import com.intellij.util.text.VersionComparatorUtil
 import org.jetbrains.annotations.ApiStatus
@@ -24,7 +25,19 @@ fun PluginInitializationContext.selectPluginsToLoad(
   if (discoveredPlugins.isEmpty()) {
     return emptyList()
   }
+  if (explicitPluginSubsetToLoad != null) {
+    return selectFromExplicitSubset(discoveredPlugins, onPluginExcluded)
+  }
+  if (disablePluginLoadingCompletely) {
+    return selectOnlyCorePlugin(discoveredPlugins, onPluginExcluded)
+  }
+  return regularSelect(onPluginExcluded, discoveredPlugins)
+}
 
+private fun PluginInitializationContext.regularSelect(
+  onPluginExcluded: (PluginMainDescriptor, PluginNonLoadReason) -> Unit,
+  discoveredPlugins: List<DiscoveredPluginsList>,
+): List<DiscoveredPluginsList> {
   val selectedPluginsByPluginId = LinkedHashMap<PluginId, PluginMainDescriptor>()
   var hasExclusions = false
   // name shadowing is intended
@@ -69,5 +82,50 @@ fun PluginInitializationContext.selectPluginsToLoad(
   return discoveredPlugins.map { pluginList ->
     val filteredPlugins = pluginList.plugins.filter { selectedPluginsByPluginId[it.pluginId] === it }
     DiscoveredPluginsList(filteredPlugins, pluginList.source)
+  }
+}
+
+
+private fun PluginInitializationContext.selectFromExplicitSubset(
+  discoveredPlugins: List<DiscoveredPluginsList>,
+  onPluginExcluded: (PluginMainDescriptor, PluginNonLoadReason) -> Unit,
+): List<DiscoveredPluginsList> {
+  val regularSelectResult = regularSelect(onPluginExcluded, discoveredPlugins)
+  val regularSelectionPlugins = regularSelectResult.flatMap { it.plugins }
+  val pluginIdsSubset = essentialPlugins + explicitPluginSubsetToLoad!! + setOf(CORE_ID)
+  val pluginSubset = regularSelectionPlugins.filter { it.pluginId in pluginIdsSubset }
+  val ambiguousPluginSet = AmbiguousPluginSet.build(regularSelectionPlugins)
+  val requiredModules = PluginDependencyAnalysis.getRequiredTransitiveModules(
+    this,
+    pluginSubset,
+    ambiguousPluginSet,
+  )
+  return regularSelectResult.map { pluginsList ->
+    DiscoveredPluginsList(pluginsList.plugins.mapNotNull { plugin ->
+      if (plugin in requiredModules) {
+        plugin
+      } else {
+        onPluginExcluded(plugin, PluginIsNotRequiredForLoadingTheExplicitlyConfiguredSubsetOfPlugins(plugin))
+        null
+      }
+    }, pluginsList.source)
+  }
+}
+
+
+private fun selectOnlyCorePlugin(
+  discoveredPlugins: List<DiscoveredPluginsList>,
+  onPluginExcluded: (PluginMainDescriptor, PluginNonLoadReason) -> Unit,
+): List<DiscoveredPluginsList> {
+  return discoveredPlugins.map { pluginsList ->
+    DiscoveredPluginsList(pluginsList.plugins.mapNotNull { plugin ->
+      if (plugin.pluginId == CORE_ID) {
+        plugin
+      }
+      else {
+        onPluginExcluded(plugin, PluginLoadingIsDisabledCompletely(plugin))
+        null
+      }
+    }, pluginsList.source)
   }
 }
