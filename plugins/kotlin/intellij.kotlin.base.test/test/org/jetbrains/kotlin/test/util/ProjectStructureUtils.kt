0e304502ca96de48a07ec2db2abd856667e0f9a2
Anna Kozlova <anna.kozlova@jetbrains.com>
2025-12-16 10:06:07 +0100

    [kotlin] remove kotlin.idea.test from intellij.kotlin.base.analysis.tests
    ^KTIJ-36869 fixed
    
    GitOrigin-RevId: 6fd2f8eb77b430d69ad6c2c9036c2dfda77f9524

--- plugins/kotlin/intellij.kotlin.base.test/test/org/jetbrains/kotlin/test/util/ProjectStructureUtils.kt
+++ plugins/kotlin/intellij.kotlin.base.test/test/org/jetbrains/kotlin/test/util/ProjectStructureUtils.kt
@@ -1,5 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.test.util
 
 import com.intellij.openapi.application.runWriteAction
@@ -49,7 +48,7 @@ fun HeavyPlatformTestCase.multiRootProjectLibrary(
         }
         library
     }.also {
-        IndexingTestUtil.waitUntilIndexesAreReady(module.project)
+        IndexingTestUtil.Companion.waitUntilIndexesAreReady(module.project)
     }
 }
 
@@ -76,7 +75,7 @@ fun moduleLibrary(
 
         library
     }.also {
-        IndexingTestUtil.waitUntilIndexesAreReady(module.project)
+        IndexingTestUtil.Companion.waitUntilIndexesAreReady(module.project)
     }
 }
 
