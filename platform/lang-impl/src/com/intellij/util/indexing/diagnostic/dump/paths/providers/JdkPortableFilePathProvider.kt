900415c6516ac29a5709455f85466541c3e33a48
Ilya Korennoy <ilya.korennoy@jetbrains.com>
2025-12-17 14:20:32 +0100

    IJPL-930 Remove getOrderEntriesForFile from JdkPortableFilePathProvider
    GitOrigin-RevId: 5d7f17715e109ba0505546fe654029dd63a4fd9a

--- platform/lang-impl/src/com/intellij/util/indexing/diagnostic/dump/paths/providers/JdkPortableFilePathProvider.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/diagnostic/dump/paths/providers/JdkPortableFilePathProvider.kt
@@ -1,32 +1,28 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.indexing.diagnostic.dump.paths.providers
 
-import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.JdkOrderEntry
 import com.intellij.openapi.roots.OrderRootType
-import com.intellij.openapi.roots.libraries.LibraryUtil
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.backend.workspace.virtualFile
 import com.intellij.util.indexing.diagnostic.dump.paths.PortableFilePath
+import com.intellij.workspaceModel.ide.impl.legacyBridge.sdk.customName
 
 object JdkPortableFilePathProvider : PortableFilePathProvider {
   override fun getRelativePortableFilePath(project: Project, virtualFile: VirtualFile): PortableFilePath.RelativePath? {
-    val orderEntry = runReadAction { LibraryUtil.findLibraryEntry(virtualFile, project) }
-    if (orderEntry !is JdkOrderEntry) return null
-    val jdkName = orderEntry.jdkName
-    if (jdkName == null) return null
+    val sdkEntity = ProjectFileIndex.getInstance(project).findContainingSdks(virtualFile).firstOrNull()
+    if (sdkEntity == null) { return null }
+    val jdkName = sdkEntity.name
 
-    for (rootType in listOf(OrderRootType.CLASSES, OrderRootType.SOURCES)) {
-      val inClassFiles = rootType == OrderRootType.CLASSES
-      val jdkRoots = orderEntry.getRootFiles(rootType)
-      for ((rootIndex, rootFile) in jdkRoots.withIndex()) {
-        val relativePath = VfsUtilCore.getRelativePath(virtualFile, rootFile)
-        if (relativePath == null) continue
-        return PortableFilePath.RelativePath(PortableFilePath.JdkRoot(jdkName, rootIndex, inClassFiles), relativePath)
-      }
+    for ((rootIndex, sdkRoot) in sdkEntity.roots.withIndex()) {
+      val inClassFiles = sdkRoot.type.name == OrderRootType.CLASSES.customName
+      val rootFile = sdkRoot.url.virtualFile ?: continue
+      val relativePath = VfsUtilCore.getRelativePath(virtualFile, rootFile)
+      if (relativePath == null) continue
+      return PortableFilePath.RelativePath(PortableFilePath.JdkRoot(jdkName, rootIndex, inClassFiles), relativePath)
     }
-
     return null
   }
 }
