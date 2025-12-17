f7aa8c0ab4a8837302d2cf91b46ea8750e597e98
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2025-12-17 16:17:09 +0100

    test [workspace] PY-82580: Migrate test to `sdkFixture`
    To reduce copy/paste, use `sdkFixture`
    
    GitOrigin-RevId: 68120820f2a02459eda2f702db0f2d1640931a46

--- platform/workspace/jps/tests/testSrc/com/intellij/workspaceModel/ide/ModuleEntityTest.kt
+++ platform/workspace/jps/tests/testSrc/com/intellij/workspaceModel/ide/ModuleEntityTest.kt
@@ -1,14 +1,10 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.workspaceModel.ide
 
-import com.intellij.openapi.application.writeAction
-import com.intellij.openapi.projectRoots.ProjectJdkTable
-import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.projectRoots.SdkType
-import com.intellij.openapi.projectRoots.impl.SdkConfigurationUtil
 import com.intellij.openapi.roots.ModuleRootManager
-import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.platform.backend.workspace.workspaceModel
+import com.intellij.platform.testFramework.junit5.projectStructure.fixture.sdkFixture
 import com.intellij.platform.workspace.jps.entities.SdkId
 import com.intellij.platform.workspace.jps.entities.modifyModuleEntity
 import com.intellij.platform.workspace.jps.entities.sdkId
@@ -16,42 +12,25 @@ import com.intellij.testFramework.common.timeoutRunBlocking
 import com.intellij.testFramework.junit5.TestApplication
 import com.intellij.testFramework.junit5.fixture.moduleFixture
 import com.intellij.testFramework.junit5.fixture.projectFixture
+import com.intellij.testFramework.junit5.fixture.tempPathFixture
 import com.intellij.workspaceModel.ide.legacyBridge.findModuleEntity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.io.TempDir
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import java.nio.file.Path
 
 @TestApplication
 class ModuleEntityTest {
-  private lateinit var sdk: Sdk
-  private val moduleFixture = projectFixture().moduleFixture()
+  private val projectFixture = projectFixture()
+  private val moduleFixture = projectFixture.moduleFixture()
+  private val dirFixture = tempPathFixture()
+  private val sdkFixture = projectFixture.sdkFixture("mySDKName", SdkType.EP_NAME.extensionList.first(), dirFixture)
 
-  @BeforeEach
-  fun registerSdk(@TempDir tempDir: Path): Unit = timeoutRunBlocking {
-    val tempDirVfs = withContext(Dispatchers.IO) { VfsUtil.findFile(tempDir, true)!! }
-    sdk = SdkConfigurationUtil.setupSdk(emptyArray<Sdk>(), tempDirVfs, SdkType.EP_NAME.extensionList.first(), true, null, "mySDKname")!!
-    writeAction {
-      ProjectJdkTable.getInstance().addJdk(sdk)
-    }
-  }
-
-  @AfterEach
-  fun dropSdk(): Unit = timeoutRunBlocking {
-    writeAction {
-      ProjectJdkTable.getInstance().removeJdk(sdk)
-    }
-  }
 
   @ParameterizedTest
   @ValueSource(booleans = [true, false])
   fun testModuleSdk(removeExistingSdk: Boolean): Unit = timeoutRunBlocking {
     val module = moduleFixture.get()
+    val sdk = sdkFixture.get()
     val (sdkIdToSet, expectedSdk) = if (removeExistingSdk) Pair(null, null) else Pair(SdkId(sdk.name, sdk.sdkType.name), sdk)
 
     module.project.workspaceModel.update("...") { storage ->
