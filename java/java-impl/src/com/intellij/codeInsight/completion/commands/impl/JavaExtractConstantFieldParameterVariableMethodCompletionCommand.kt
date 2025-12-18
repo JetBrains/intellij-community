9ece7cef24d7ffae83cb7dc42422632a6d456783
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2025-12-18 13:52:41 +0100

    [command-completion] IDEA-383573 Support introduce constant in command completions for literals
    (cherry picked from commit 49b388d9cfe35075d99a8b19e88098d36e420701)
    
    IJ-CR-186454
    
    GitOrigin-RevId: c5b2b537c8533dd9a301f70eb62a7e76aa189a4d

--- java/java-impl/src/com/intellij/codeInsight/completion/commands/impl/JavaExtractConstantFieldParameterVariableMethodCompletionCommand.kt
+++ java/java-impl/src/com/intellij/codeInsight/completion/commands/impl/JavaExtractConstantFieldParameterVariableMethodCompletionCommand.kt
@@ -12,19 +12,19 @@ import com.intellij.psi.util.findParentOfType
 
 internal class JavaExtractConstantCompletionCommandProvider : AbstractExtractConstantCompletionCommandProvider() {
   override fun findOffsetToCall(offset: Int, psiFile: PsiFile): Int? {
-    return findOffsetForLocalVariable(offset, psiFile)
+    return findOffsetToExtract(offset, psiFile)
   }
 }
 
 internal class JavaExtractFieldCompletionCommandProvider : AbstractExtractFieldCompletionCommandProvider() {
   override fun findOffsetToCall(offset: Int, psiFile: PsiFile): Int? {
-    return findOffsetForLocalVariable(offset, psiFile)
+    return findOffsetToExtract(offset, psiFile)
   }
 }
 
 internal class JavaExtractParameterCompletionCommandProvider : AbstractExtractParameterCompletionCommandProvider() {
   override fun findOffsetToCall(offset: Int, psiFile: PsiFile): Int? {
-    return findOffsetForLocalVariable(offset, psiFile)
+    return findOffsetToExtract(offset, psiFile)
   }
 }
 
@@ -99,7 +99,7 @@ private fun isApplicableCallExpression(expression: PsiExpression?, offset: Int):
          || expression is PsiNewExpression && (expression.textRange.endOffset != offset || PsiTreeUtil.skipWhitespacesForward(expression) !is PsiErrorElement)
 }
 
-private fun findOffsetForLocalVariable(offset: Int, psiFile: PsiFile): Int? {
+private fun findOffsetToExtract(offset: Int, psiFile: PsiFile): Int? {
   var currentOffset = offset
   if (currentOffset == 0) return null
   var element = getCommandContext(offset, psiFile) ?: return null
@@ -107,6 +107,10 @@ private fun findOffsetForLocalVariable(offset: Int, psiFile: PsiFile): Int? {
     element = PsiTreeUtil.skipWhitespacesBackward(element) ?: return null
   }
   currentOffset = element.textRange?.endOffset ?: currentOffset
+  val literal = element.findParentOfType<PsiLiteral>(strict = false)
+  if (literal != null && literal.textRange.endOffset == currentOffset) {
+    return currentOffset
+  }
   val localVariable = element.findParentOfType<PsiLocalVariable>() ?: return null
   if (localVariable.textRange.endOffset == currentOffset ||
       localVariable.textRange.endOffset - 1 == currentOffset ||
