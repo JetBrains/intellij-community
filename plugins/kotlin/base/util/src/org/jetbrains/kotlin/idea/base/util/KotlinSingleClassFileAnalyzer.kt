a2bc2fa83fea6e1cc5c1e710169798345e871782
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2025-12-18 17:02:18 +0100

    [kotlin] KTIJ-34674 Clean up
    Green stub optimization is redundant after KT-73687
    
    GitOrigin-RevId: a7b1f89b5f5a3357474a465cebcc48da1ae43d62

--- plugins/kotlin/base/util/src/org/jetbrains/kotlin/idea/base/util/KotlinSingleClassFileAnalyzer.kt
+++ plugins/kotlin/base/util/src/org/jetbrains/kotlin/idea/base/util/KotlinSingleClassFileAnalyzer.kt
@@ -2,17 +2,13 @@
 package org.jetbrains.kotlin.idea.base.util
 
 import com.intellij.openapi.util.text.StringUtil
-import com.intellij.psi.PsiElement
 import org.jetbrains.annotations.ApiStatus
-import org.jetbrains.kotlin.KtNodeTypes
 import org.jetbrains.kotlin.idea.util.isFileInRoots
 import org.jetbrains.kotlin.psi.KtClassOrObject
 import org.jetbrains.kotlin.psi.KtDeclaration
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtTypeAlias
-import org.jetbrains.kotlin.psi.psiUtil.children
 import org.jetbrains.kotlin.psi.psiUtil.isPrivate
-import org.jetbrains.kotlin.psi.stubs.elements.KtTokenSets
 
 @ApiStatus.Internal
 object KotlinSingleClassFileAnalyzer {
@@ -28,39 +24,12 @@ object KotlinSingleClassFileAnalyzer {
         }
 
         var targetDeclaration: KtDeclaration? = null
-
-        /**
-         * Returns true if more iterations are needed.
-         *
-         * [targetDeclaration] points to the only one non-private declaration, otherwise it is null.
-         */
-        fun handleDeclaration(psiElement: PsiElement?): Boolean {
-            val declaration = psiElement as? KtDeclaration ?: return true
+        for (declaration: KtDeclaration in file.declarations) {
             if (!declaration.isPrivate() && declaration !is KtTypeAlias) {
-                if (targetDeclaration != null) {
-                    targetDeclaration = null
-                    return false
-                }
+                if (targetDeclaration != null) return null
                 targetDeclaration = declaration
             }
-            return true
         }
-
-        // do not build AST for stubs when it is unnecessary.
-        file.withGreenStubOrAst(
-            { fileStub ->
-                for (stubElement in fileStub.childrenStubs) {
-                    val elementType = stubElement.elementType
-                    if (elementType != KtNodeTypes.TYPEALIAS && elementType in KtTokenSets.DECLARATION_TYPES) {
-                        if (!handleDeclaration(stubElement.psi)) return@withGreenStubOrAst
-                    }
-                }
-            }, { fileElement ->
-                for (node in fileElement.children()) {
-                    if (!handleDeclaration(node.psi)) return@withGreenStubOrAst
-                }
-            }
-        )
         return targetDeclaration?.takeIf { it is KtClassOrObject && StringUtil.getPackageName(file.name) == it.name } as? KtClassOrObject
     }
 }
