7038d276498b0b18d43870150a770dfe424cd916
Ruslan Cheremin <ruslan.cheremin@jetbrains.com>
2025-12-19 00:28:04 +0100

    cleanup [test]: do not access invalid files
    GitOrigin-RevId: 465ca4132b35d688e14a146da17aeb5e179227a9

--- platform/testFramework/src/com/intellij/testFramework/LightPlatformTestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/LightPlatformTestCase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.testFramework;
 
 import com.intellij.application.options.CodeStyle;
@@ -453,14 +453,16 @@ public abstract class LightPlatformTestCase extends UsefulTestCase implements Da
         return;
       }
 
-      for (VirtualFile child : sourceRoot.getChildren()) {
-        try {
-          child.delete(LightPlatformTestCase.class);
-        }
-        catch (IOException | IllegalStateException e) {
-          // TempFileSystem.deleteFile can throw not IOException but IllegalStateException
-          //noinspection CallToPrintStackTrace
-          e.printStackTrace();
+      if (sourceRoot.isValid()) {
+        for (VirtualFile child : sourceRoot.getChildren()) {
+          try {
+            child.delete(LightPlatformTestCase.class);
+          }
+          catch (IOException | IllegalStateException e) {
+            // TempFileSystem.deleteFile can throw not IOException but IllegalStateException
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+          }
         }
       }
     });
