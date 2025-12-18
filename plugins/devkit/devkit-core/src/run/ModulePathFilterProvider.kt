0eafc61472e2a0e2e268b7a904663453e6c32092
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-12-18 12:44:36 +0100

    IJPL-224531 devkit: navigate to module descriptor XML by default
    When clicking on a module name in console output, navigate to:
    1. META-INF/plugin.xml (if exists in resource roots)
    2. {moduleName}.xml descriptor (if exists in resource roots)
    3. .iml file (fallback)
    
    Registry key `devkit.module.hyperlink.show.chooser` enables a file
    chooser popup when multiple files exist.
    
    GitOrigin-RevId: ba67dde2ef4dcb4028e2fe35cc9bb452904f2cc8

--- plugins/devkit/devkit-core/src/run/ModulePathFilterProvider.kt
+++ plugins/devkit/devkit-core/src/run/ModulePathFilterProvider.kt
@@ -1,14 +1,18 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.run
 
-import com.intellij.execution.filters.ConsoleFilterProvider
-import com.intellij.execution.filters.Filter
-import com.intellij.execution.filters.OpenFileHyperlinkInfo
+import com.intellij.execution.filters.*
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.extensions.ExtensionNotApplicableException
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.IntelliJProjectUtil
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.awt.RelativePoint
+import org.jetbrains.jps.model.java.JavaResourceRootType
 
 private val ModulePattern = """(intellij|kotlin|fleet|android)(\.[-\w]+)+""".toRegex()
 
@@ -35,24 +39,61 @@ private class ModulePathFilter(private val project: Project) : Filter {
       .mapNotNull { resultItem ->
         val moduleName = resultItem.value
         val module = moduleManager.findModuleByName(moduleName) ?: return@mapNotNull null
-        val moduleFile = module.moduleFile ?: return@mapNotNull null
 
         val textRange = resultItem.range
         Filter.ResultItem(
           /* highlightStartOffset = */ textStartOffset + textRange.first,
           /* highlightEndOffset = */ textStartOffset + textRange.last + 1,
-          /* hyperlinkInfo = */
-          OpenFileHyperlinkInfo(
-            /* project = */ project,
-            /* file = */ moduleFile,
-            /* documentLine = */ 0,
-            /* documentColumn = */ 0,
-            /* isUseBrowser = */ false,
-          ),
+          /* hyperlinkInfo = */ ModuleFilesHyperlinkInfo(module),
         )
       }
       .toList()
 
     return if (items.isEmpty()) null else Filter.Result(items)
+  }
+}
+
+/**
+ * Lazy hyperlink info that navigates to module files when clicked.
+ * By default, navigates to module descriptor XML (e.g., intellij.foo.bar.xml) if exists, otherwise .iml.
+ * When registry key `devkit.module.hyperlink.show.chooser` is enabled,
+ * shows a chooser if multiple files exist (iml + descriptor XML).
+ */
+private class ModuleFilesHyperlinkInfo(private val module: Module) : HyperlinkInfoBase() {
+  override fun navigate(project: Project, hyperlinkLocationPoint: RelativePoint?) {
+    if (Registry.`is`("devkit.module.hyperlink.show.chooser")) {
+      val descriptorFiles = findDescriptorFiles(module).toList()
+      val files = listOfNotNull(module.moduleFile) + descriptorFiles
+      if (files.size > 1) {
+        (HyperlinkInfoFactory.getInstance().createMultipleFilesHyperlinkInfo(files, 0, project) as HyperlinkInfoBase)
+          .navigate(project, hyperlinkLocationPoint)
+        return
+      }
+      // files.size <= 1, use the collected result
+      navigateToFile(project, descriptorFiles.firstOrNull() ?: module.moduleFile ?: return)
+    }
+    else {
+      navigateToFile(project, findDescriptorFiles(module).firstOrNull() ?: module.moduleFile ?: return)
+    }
+  }
+}
+
+private fun navigateToFile(project: Project, file: VirtualFile) {
+  OpenFileHyperlinkInfo(
+    /* project = */ project,
+    /* file = */ file,
+    /* documentLine = */ 0,
+    /* documentColumn = */ 0,
+    /* isUseBrowser = */ false,
+  ).navigate(project)
+}
+
+private fun findDescriptorFiles(module: Module): Sequence<VirtualFile> {
+  return sequence {
+    val rootManager = ModuleRootManager.getInstance(module)
+    for (resourceRoot in rootManager.getSourceRoots(JavaResourceRootType.RESOURCE)) {
+      resourceRoot.findFileByRelativePath("META-INF/plugin.xml")?.let { yield(it) }
+      resourceRoot.findChild("${module.name}.xml")?.let { yield(it) }
+    }
   }
 }
