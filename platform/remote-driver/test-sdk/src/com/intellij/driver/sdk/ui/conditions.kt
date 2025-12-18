2ed13c8242d60546a2f15c769ec50635f5aaad92
vladislav.shishov <vladislav.shishov@jetbrains.com>
2025-12-18 20:50:08 +0200

    AT-3720 [remote-driver]: update MainToolbarUiTest on windows
    GitOrigin-RevId: 71a4ccf42eb030fb71690db9b5852010f7b468d4

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/conditions.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/conditions.kt
@@ -6,6 +6,7 @@ import com.intellij.driver.sdk.ui.components.common.JEditorUiComponent
 import com.intellij.driver.sdk.ui.components.elements.JComboBoxUiComponent
 import com.intellij.driver.sdk.ui.components.elements.JListUiComponent
 import com.intellij.driver.sdk.ui.components.elements.JTextFieldUI
+import com.intellij.driver.sdk.ui.components.elements.PopupMenuUiComponent
 import com.intellij.driver.sdk.waitFor
 import kotlin.time.Duration
 
@@ -114,6 +115,14 @@ fun JEditorUiComponent.shouldContainText(expectedText: String, message: String? 
 fun JEditorUiComponent.shouldNotContainText(notExpectedText: String, message: String? = null, timeout: Duration = DEFAULT_FIND_TIMEOUT): JEditorUiComponent {
   return should(message ?: "Editor should not contain text: $notExpectedText", timeout) {
     !text.contains(notExpectedText)
+  }
+}
+
+fun PopupMenuUiComponent.selectedPathShouldBe(path: List<String>, message: String? = null, timeout: Duration = DEFAULT_FIND_TIMEOUT): PopupMenuUiComponent {
+  var lastSelectedPath: List<String>? = null
+  return should(message ?: "Selected menu path should be $path", timeout, { "expected: $path, but found: $lastSelectedPath" }) {
+    lastSelectedPath = getSelectedPath()
+    lastSelectedPath == path
   }
 }
 
