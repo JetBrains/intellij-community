0c46fc3ca1b6b21360c139a5b7b1774a96118813
Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
2025-12-19 11:10:40 +0000

    [grazie] IJPL-221635: Retain only one TreeRuleChecker
    Merge-request: IJ-MR-186462
    Merged-by: Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
    
    GitOrigin-RevId: 68f5f47f5adc3c900f3d2ff21c2a79236c98bd0d

--- plugins/grazie/src/main/kotlin/com/intellij/grazie/text/AsyncTreeRuleChecker.kt
+++ plugins/grazie/src/main/kotlin/com/intellij/grazie/text/AsyncTreeRuleChecker.kt
@@ -1,52 +1,20 @@
 package com.intellij.grazie.text
 
-import ai.grazie.gec.model.problem.concedeToOtherGrammarCheckers
 import ai.grazie.nlp.langs.Language
 import com.intellij.grazie.rule.ParsedSentence
 import com.intellij.grazie.text.TreeRuleChecker.TreeProblem
 import java.util.*
 
-sealed class AsyncTreeRuleChecker : ExternalTextChecker() {
+class AsyncTreeRuleChecker : ExternalTextChecker() {
+  override fun getRules(locale: Locale): Collection<Rule> {
+    val language = Language.parse(locale.language) ?: return emptyList()
+    return TreeRuleChecker.getRules(language)
+  }
+
   override suspend fun checkExternally(context: ProofreadingContext): Collection<TreeProblem> {
     val sentences = ParsedSentence.getSentencesAsync(context)
     if (sentences.isEmpty()) return emptyList()
 
     return TreeRuleChecker.check(context.text, sentences)
-  }
-
-  class Grammar : AsyncTreeRuleChecker() {
-
-    override fun getRules(locale: Locale): Collection<Rule> {
-      val language = Language.parse(locale.language) ?: return emptyList()
-      return TreeRuleChecker.getRules(language)
-    }
-
-    override suspend fun checkExternally(context: ProofreadingContext): Collection<TreeProblem> {
-      return super.checkExternally(context).filter { !it.isStyleLike && !it.isSpellingProblem && !concedeToOtherGrammarCheckers(it.source) }
-    }
-  }
-
-  class GrammarLowPriority : AsyncTreeRuleChecker() {
-    override fun getRules(locale: Locale): Collection<Rule> = emptyList()
-
-    override suspend fun checkExternally(context: ProofreadingContext): Collection<TreeProblem> {
-      return super.checkExternally(context).filter { !it.isStyleLike && !it.isSpellingProblem && !concedeToOtherGrammarCheckers(it.source) }
-    }
-  }
-
-  class Style : AsyncTreeRuleChecker() {
-    override fun getRules(locale: Locale): Collection<Rule> = emptyList()
-
-    override suspend fun checkExternally(context: ProofreadingContext): Collection<TreeProblem> {
-      return super.checkExternally(context).filter { it.isStyleLike }
-    }
-  }
-
-  class Spelling : AsyncTreeRuleChecker() {
-    override fun getRules(locale: Locale): Collection<Rule> = emptyList()
-
-    override suspend fun checkExternally(context: ProofreadingContext): Collection<TreeProblem> {
-      return super.checkExternally(context).filter { it.isSpellingProblem }
-    }
   }
 }
