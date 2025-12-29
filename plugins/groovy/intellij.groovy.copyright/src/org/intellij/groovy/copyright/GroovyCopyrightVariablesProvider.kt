403b7a886f49e24fde2adeacb2f8a3e5817f1ea7
Mikhail Pyltsin <Mikhail.Pyltsin@jetbrains.com>
2025-12-29 11:57:33 +0100

    [java-frontend] IDEA-366600 Get rid of additional "Java for JetBrains Client" plugin
    - extract copyright
    
    GitOrigin-RevId: 19b066c5a28c526411898b1aff56d957c98b6965

---
+++ plugins/groovy/intellij.groovy.copyright/src/org/intellij/groovy/copyright/GroovyCopyrightVariablesProvider.kt
@@ -0,0 +1,29 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.intellij.groovy.copyright
+
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import com.maddyhome.idea.copyright.pattern.CopyrightVariablesProvider
+import com.maddyhome.idea.copyright.pattern.FileInfo
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFile
+
+class GroovyCopyrightVariablesProvider : CopyrightVariablesProvider() {
+
+  override fun collectVariables(
+    context: MutableMap<String?, Any?>,
+    project: Project?,
+    module: com.intellij.openapi.module.Module?,
+    file: PsiFile,
+  ) {
+    if (file !is GroovyFile) return
+
+    context["file"] = object : FileInfo(file) {
+
+      override fun getQualifiedClassName() = getClazz()?.qualifiedName ?: super.getQualifiedClassName()
+
+      override fun getClassName(): String = getClazz()?.name ?: super.getClassName()
+
+      private fun getClazz() = if (file.isScript) file.scriptClass else file.classes.firstOrNull()
+    }
+  }
+}
--- plugins/groovy/intellij.groovy.copyright/src/org/intellij/groovy/copyright/UpdateGroovyCopyrightsProvider.java
+++ plugins/groovy/intellij.groovy.copyright/src/org/intellij/groovy/copyright/UpdateGroovyCopyrightsProvider.java
@@ -1,6 +1,6 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
-package org.jetbrains.plugins.groovy.copyright;
+package org.intellij.groovy.copyright;
 
 import com.intellij.copyright.UpdateJavaFileCopyright;
 import com.intellij.openapi.fileTypes.FileType;
