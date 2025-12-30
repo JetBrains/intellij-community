b34cf9b3ca95a1505cd0f53d7cdf8c858141cef2
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2025-12-30 15:18:48 +0100

    [kotlin] KTIJ-36993 Highlight smart casts with info severity
    GitOrigin-RevId: abbf02f448c1b2589e1566a68532ddfde8deadb9

--- plugins/kotlin/idea/tests/testData/checker/infos/SmartCastOnIf.kt
+++ plugins/kotlin/idea/tests/testData/checker/infos/SmartCastOnIf.kt
@@ -7,6 +7,6 @@ fun <symbolName descr="null" tooltip="null" textAttributesKey="KOTLIN_FUNCTION_D
     else {
         val <symbolName descr="null" tooltip="null" textAttributesKey="KOTLIN_LOCAL_VARIABLE">u</symbolName>: <symbolName descr="null" tooltip="null" textAttributesKey="KOTLIN_CLASS">String</symbolName>? = null
         if (<symbolName descr="null" tooltip="null" textAttributesKey="KOTLIN_LOCAL_VARIABLE">u</symbolName> == null) return 0
-        <symbolName descr="null" tooltip="null" textAttributesKey="KOTLIN_LOCAL_VARIABLE"><symbolName descr="Smart cast to kotlin.String" tooltip="Smart cast to kotlin.String" textAttributesKey="KOTLIN_SMART_CAST_VALUE">u</symbolName></symbolName>
+        <info descr="Smart cast to kotlin.String" tooltip="Smart cast to kotlin.String" textAttributesKey="KOTLIN_SMART_CAST_VALUE"><symbolName descr="null" tooltip="null" textAttributesKey="KOTLIN_LOCAL_VARIABLE">u</symbolName></info>
     }.<symbolName descr="null" tooltip="null" textAttributesKey="KOTLIN_INSTANCE_PROPERTY">length</symbolName>
 }
