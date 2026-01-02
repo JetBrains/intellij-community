ac7e1e756ec7f3e74e57c0d2d77ca5315d07569c
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2026-01-02 13:40:28 +0100

    [threading] IJPL-226478: Add read access to TypeFilter
    GitOrigin-RevId: 41970d568e0ca937cda90b63cd1e64a18a3147ed

--- platform/structuralsearch/source/com/intellij/structuralsearch/plugin/ui/filters/TypeFilter.java
+++ platform/structuralsearch/source/com/intellij/structuralsearch/plugin/ui/filters/TypeFilter.java
@@ -1,6 +1,7 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.structuralsearch.plugin.ui.filters;
 
+import com.intellij.openapi.application.WriteIntentReadAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.PlainTextFileType;
@@ -95,9 +96,11 @@ public final class TypeFilter extends FilterAction {
 
       {
         myRegexCheckBox.addActionListener(e -> {
-          final FileType fileType = myRegexCheckBox.isSelected() ? RegExpFileType.INSTANCE : PlainTextFileType.INSTANCE;
-          final Document document = UIUtil.createDocument(fileType, myTextField.getText(), myTable.getProject());
-          myTextField.setDocument(document);
+          WriteIntentReadAction.run(() -> {
+            final FileType fileType = myRegexCheckBox.isSelected() ? RegExpFileType.INSTANCE : PlainTextFileType.INSTANCE;
+            final Document document = UIUtil.createDocument(fileType, myTextField.getText(), myTable.getProject());
+            myTextField.setDocument(document);
+          });
         });
       }
 
