45193b5f38c06c21c5743b79377d8813e020fd59
Max Medvedev <maxim.medvedev@jetbrains.com>
2026-01-05 12:01:38 +0100

    cleanup [logging]: suppress incorrect warning in ThrottledLogger
    GitOrigin-RevId: 3beb3d820b2c35a22f74378303714a354e1b471d

--- platform/util/src/com/intellij/openapi/diagnostic/ThrottledLogger.java
+++ platform/util/src/com/intellij/openapi/diagnostic/ThrottledLogger.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.diagnostic;
 
 import org.jetbrains.annotations.ApiStatus;
@@ -16,7 +16,9 @@ import static java.util.Objects.requireNonNull;
  */
 @ApiStatus.Internal
 public final class ThrottledLogger {
+  @SuppressWarnings("NonConstantLogger")
   private final @NotNull Logger logger;
+
   /**
    * Ignore (i.e. skip logging) subsequent messages with same key if they come during that period after the first message.
    */
