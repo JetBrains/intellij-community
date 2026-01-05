10d00f19423ecd92c435fa04cb26e7d96d6e1f29
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2026-01-05 17:38:48 +0100

    [threading] IJPL-226875: Wrap cell renderer in `CallerChooserBase` in write-intent read action
    GitOrigin-RevId: cc3c73ddea3c1b6031b8931ef11d0a9120d61328

--- platform/lang-impl/src/com/intellij/refactoring/changeSignature/CallerChooserBase.java
+++ platform/lang-impl/src/com/intellij/refactoring/changeSignature/CallerChooserBase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.refactoring.changeSignature;
 
 import com.intellij.codeInsight.highlighting.HighlightManager;
@@ -6,6 +6,7 @@ import com.intellij.ide.highlighter.HighlighterFactory;
 import com.intellij.lang.injection.InjectedLanguageManager;
 import com.intellij.openapi.application.AccessToken;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.WriteIntentReadAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
@@ -261,7 +262,9 @@ public abstract class CallerChooserBase<M extends PsiElement> extends DialogWrap
                                     int row,
                                     boolean hasFocus) {
         if (value instanceof MemberNodeBase) {
-          ((MemberNodeBase<?>)value).customizeRenderer(getTextRenderer());
+          WriteIntentReadAction.run(() -> {
+            ((MemberNodeBase<?>)value).customizeRenderer(getTextRenderer());
+          });
         }
       }
     };
