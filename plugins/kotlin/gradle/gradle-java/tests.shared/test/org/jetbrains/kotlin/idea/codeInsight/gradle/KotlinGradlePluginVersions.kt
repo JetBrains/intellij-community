f17903579a645d503c337ebdaca027e3a66c80c3
Vlad Koshkin <vladislav.koshkin@jetbrains.com>
2026-01-06 00:33:37 +0100

    [kotlin] advance kotlinc version for analyzer to 2.3.20-dev-7874
    Includes: [84c73d65...5432712f](https://github.com/JetBrains/kotlin/compare/84c73d654447385c3ed0aa9c6e4e204779f431c0...5432712fbf58479468dab4dd32cbf5df872086df)
    
    GitOrigin-RevId: 7ac1519c3e3308340b5832a68481be368a7a976b

--- plugins/kotlin/gradle/gradle-java/tests.shared/test/org/jetbrains/kotlin/idea/codeInsight/gradle/KotlinGradlePluginVersions.kt
+++ plugins/kotlin/gradle/gradle-java/tests.shared/test/org/jetbrains/kotlin/idea/codeInsight/gradle/KotlinGradlePluginVersions.kt
@@ -9,7 +9,7 @@ object KotlinGradlePluginVersions {
     val V_1_9_25 = KotlinToolingVersion(1, 9, 25, null)
     val V_2_1_0 = KotlinToolingVersion(2, 1, 0, null)
     val V_2_2_0 = KotlinToolingVersion(2, 2, 0, null)
-    val latest = KotlinToolingVersion(2, 2, 21, null)
+    val latest = KotlinToolingVersion("2.2.21")
     val latestBootstrap = KotlinToolingVersion("1.9.20-dev-6845")
     val latestStable = V_1_8_22
 }
