3afe9d52ace9be78a691bf9badab96c088fb1568
Max Medvedev <maxim.medvedev@jetbrains.com>
2026-01-05 18:23:05 +0100

    IJPL-226893 Drop `ide.completion.delay.autopopup.until.completed` registry flag
    GitOrigin-RevId: a9d5607bd1f3ddd217091baf677c60f5d2ca6037

--- platform/lang-impl/src/com/intellij/codeInsight/completion/CompletionProgressIndicator.java
+++ platform/lang-impl/src/com/intellij/codeInsight/completion/CompletionProgressIndicator.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package com.intellij.codeInsight.completion;
 
@@ -500,9 +500,6 @@ public final class CompletionProgressIndicator extends ProgressIndicatorBase imp
     }
     if (isAutopopupCompletion()) {
       if (count == 0) {
-        return false;
-      }
-      if (lookup.isCalculating() && Registry.is("ide.completion.delay.autopopup.until.completed")) {
         return false;
       }
     }
