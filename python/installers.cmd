da2b4c412dc74d9101a7a0a9eb43ce74157b9a3b
Leonid Shalupov <leonid.shalupov@jetbrains.com>
2026-01-05 21:16:20 +0100

    IJI-3417 build scripts: make community/python/installers.cmd use Bazel
    GitOrigin-RevId: 0a47e64d6a4676ad8f41cf884f73636c145818d8

--- python/installers.cmd
+++ python/installers.cmd
@@ -3,10 +3,25 @@
 GOTO :CMDSCRIPT
 ::CMDLITERAL
 
-set -eux
+# installer.cmd builds PyCharm Community installers
+# THIS SCRIPTS WORKS FOR ALL SYSTEMS Linux/Windows/macOS
+# See README.md for usage scenarios
+
+# Arguments are passed as JVM options
+# and used in org.jetbrains.intellij.build.BuildOptions
+
+# Pass --debug to suspend and wait for debugger at port 5005
+
+set -eu
 root="$(cd "$(dirname "$0")" && cd .. && pwd)"
-exec "$root/platform/jps-bootstrap/jps-bootstrap.sh" "$@" "$root" intellij.pycharm.community.build PyCharmCommunityInstallersBuildTarget
+
+exec "$root/build/run_build_target.sh" "$root" @community//python/build:i_build_target "$@"
+
 :CMDSCRIPT
 
-call "%~dp0\..\platform\jps-bootstrap\jps-bootstrap.cmd" %* "%~dp0\.." intellij.pycharm.community.build PyCharmCommunityInstallersBuildTarget
-EXIT /B %ERRORLEVEL%
+"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" ^
+  -NoLogo -NoProfile -NonInteractive -ExecutionPolicy Bypass ^
+  -File "%~dp0..\build\run_build_target.ps1" ^
+  "%~dp0.." ^
+  "@community//python/build:i_build_target" ^
+  %*
