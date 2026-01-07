ec291716adb9240aa72ff35707bf08e4b259188f
Sergei Kharitontcev-Beglov <sergei.kharitontcev-beglov@jetbrains.com>
2026-01-07 16:06:34 +0100

    IJPL-233109 rdct: Propagate `without-module` to the deeper levels
    This commit allows propagating `<without-module>` tag in `product-modules.xml` to the deeper levels of `from-module`. This is done to mitigate the complicated chain of customization `ide/product-modules.xml -> frontend.split.plugin/product-modules.xml -> platform.frontend.split/product-modules.xml`. Before this commit, the modules from `intellij.platform.frontend.split`'s `product-modules.xml` couldn't be excluded from the IDE's `product-modules.xml`.
    
    Space-RevId: ab7063d15585bc3c439cd3b54dd06bd7076027e8
    
    GitOrigin-RevId: d18f6a3efee3194125f3e07f837f107e41874c81

--- platform/runtime/product/src/serialization/ProductModulesSerialization.java
+++ platform/runtime/product/src/serialization/ProductModulesSerialization.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.runtime.product.serialization;
 
 import com.intellij.platform.runtime.product.PluginModuleGroup;
@@ -21,6 +21,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -63,7 +64,7 @@ public final class ProductModulesSerialization {
       
       Set<RawIncludedRuntimeModule> allMainGroupModules = new LinkedHashSet<>(rawProductModules.getMainGroupModules());
       Set<RuntimeModuleId> allBundledPluginMainModules = new LinkedHashSet<>(rawProductModules.getBundledPluginMainModules());
-      mergeIncludedFiles(rawProductModules, filePath, resolver, allMainGroupModules, allBundledPluginMainModules);
+      mergeIncludedFiles(rawProductModules, filePath, resolver, allMainGroupModules, allBundledPluginMainModules, Collections.emptySet());
       return new RawProductModules(new ArrayList<>(allMainGroupModules), new ArrayList<>(allBundledPluginMainModules), 
                                    Collections.emptyList());
     }
@@ -96,7 +97,8 @@ public final class ProductModulesSerialization {
                                          @NotNull String debugName,
                                          @NotNull ResourceFileResolver resolver,
                                          @NotNull Set<RawIncludedRuntimeModule> mainGroupModules,
-                                         @NotNull Set<RuntimeModuleId> bundledPluginMainModules) throws IOException, XMLStreamException {
+                                         @NotNull Set<RuntimeModuleId> bundledPluginMainModules,
+                                         @NotNull Set<RuntimeModuleId> withoutModules) throws IOException, XMLStreamException {
     for (RawIncludedFromData includedFromData : rawProductModules.getIncludedFrom()) {
       RuntimeModuleId includedId = includedFromData.getFromModule();
       InputStream inputStream = resolver.readResourceFile(includedId, "META-INF/" + includedId.getStringId() + "/product-modules.xml");
@@ -105,14 +107,16 @@ public final class ProductModulesSerialization {
                                                debugName + " doesn't contain product-modules.xml");
       }
       RawProductModules includedModules = ProductModulesXmlSerializer.parseModuleXml(inputStream);
-      mergeIncludedFiles(includedModules, debugName, resolver, mainGroupModules, bundledPluginMainModules);
+      var withoutIncluding = new HashSet<>(withoutModules);
+      withoutIncluding.addAll(includedFromData.getWithoutModules());
+      mergeIncludedFiles(includedModules, debugName, resolver, mainGroupModules, bundledPluginMainModules, withoutIncluding);
       for (RawIncludedRuntimeModule mainGroupModule : includedModules.getMainGroupModules()) {
-        if (!includedFromData.getWithoutModules().contains(mainGroupModule.getModuleId())) {
+        if (!withoutIncluding.contains(mainGroupModule.getModuleId())) {
           mainGroupModules.add(mainGroupModule);
         }
       }
       for (RuntimeModuleId pluginMainModule : includedModules.getBundledPluginMainModules()) {
-        if (!includedFromData.getWithoutModules().contains(pluginMainModule)) {
+        if (!withoutIncluding.contains(pluginMainModule)) {
           bundledPluginMainModules.add(pluginMainModule);
         }
       }
