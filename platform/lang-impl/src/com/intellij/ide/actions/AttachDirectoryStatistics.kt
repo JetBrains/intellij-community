a81fab2f261bc99aec9643571dba673230133137
Dmitrii Suvorov <dmitrii.suvorov@jetbrains.com>
2026-01-09 15:05:22 +0100

    DBE-25128 Log special directories
    Space-RevId: fdfcad80f8777c9842637babcc3cb90e8001cdef
    
    GitOrigin-RevId: 15251f5b288701af124bc5f7cc510b0b6d344df7

--- platform/lang-impl/src/com/intellij/ide/actions/AttachDirectoryStatistics.kt
+++ platform/lang-impl/src/com/intellij/ide/actions/AttachDirectoryStatistics.kt
@@ -1,6 +1,9 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.actions
 
+import com.intellij.ide.actions.AttachDirectoryUsagesCollector.SpecialDirectory
+import com.intellij.ide.actions.AttachDirectoryUsagesCollector.logAttachedDirectoryFilesCount
+import com.intellij.ide.actions.AttachDirectoryUsagesCollector.logAttachedSpecialDirectory
 import com.intellij.internal.statistic.eventLog.EventLogGroup
 import com.intellij.internal.statistic.eventLog.events.EventFields
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
@@ -9,10 +12,12 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.SystemProperties
 import com.intellij.openapi.vfs.newvfs.NewVirtualFile
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.nio.file.Paths
 
 internal object AttachDirectoryUsagesCollector : CounterUsagesCollector() {
   private val GROUP = EventLogGroup(
@@ -21,10 +26,46 @@ internal object AttachDirectoryUsagesCollector : CounterUsagesCollector() {
     description = "Reports file count in attached directories"
   )
 
+  enum class SpecialDirectory {
+    HOME {
+      override fun matches(root: VirtualFile): Boolean {
+        val userHome = SystemProperties.getUserHome()
+        if (userHome.isEmpty()) return false
+        return try {
+          val home = Paths.get(userHome).toRealPath()
+          val rootPath = Paths.get(root.path).toRealPath()
+          home == rootPath
+        }
+        catch (_: Exception) {
+          false
+        }
+      }
+    },
+    ROOT {
+      override fun matches(root: VirtualFile): Boolean {
+        return try {
+          val p = Paths.get(root.path)
+          p.isAbsolute && p.nameCount == 0
+        }
+        catch (_: Exception) {
+          false
+        }
+      }
+    };
+
+    abstract fun matches(root: VirtualFile): Boolean
+  }
+
   private val ATTACHED_DIRECTORY_EVENT = GROUP.registerEvent(
     eventId = "attached.directory",
     eventField1 = EventFields.LogarithmicInt("files_count"),
-    description = "Reports the count of files located under an attached root directory"
+    description = "Reports the file count under an attached directory"
+  )
+
+  private val ATTACHED_SPECIAL_DIRECTORY_EVENT = GROUP.registerEvent(
+    eventId = "attached.special.directory",
+    eventField1 = EventFields.Enum<SpecialDirectory>("type"),
+    description = "Reports an attached special directory type"
   )
 
   override fun getGroup() = GROUP
@@ -33,14 +74,26 @@ internal object AttachDirectoryUsagesCollector : CounterUsagesCollector() {
   fun logAttachedDirectoryFilesCount(filesCount: Int) {
     ATTACHED_DIRECTORY_EVENT.log(filesCount)
   }
+
+  @JvmStatic
+  fun logAttachedSpecialDirectory(type: SpecialDirectory) {
+    ATTACHED_SPECIAL_DIRECTORY_EVENT.log(type)
+  }
 }
 
 @Service(Service.Level.PROJECT)
 private class FileCountLogger(private val cs: CoroutineScope) {
   fun logFilesOnDiskCount(root: VirtualFile) {
-    cs.launch(Dispatchers.IO) {
-      val filesCount = countFilesOnDisk(root)
-      AttachDirectoryUsagesCollector.logAttachedDirectoryFilesCount(filesCount)
+    when (val specialKind = SpecialDirectory.entries.firstOrNull { it.matches(root) }) {
+      null -> {
+        cs.launch(Dispatchers.IO) {
+          val filesCount = countFilesOnDisk(root)
+          logAttachedDirectoryFilesCount(filesCount)
+        }
+      }
+      else -> {
+        logAttachedSpecialDirectory(specialKind)
+      }
     }
   }
 
