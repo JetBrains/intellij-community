37e74e665bf92bda64e0f818301ee321ea1ea984
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2026-01-09 20:56:24 +0100

    IJPL-220985 services: run dashboard: fix edit configuration action enablement
    (cherry picked from commit 73a4df47454c62b11dab624b0d688e86b544199d)
    
    IJ-CR-187553
    
    Space-RevId: aed0da8e54a68b444db06f217a1f007b676bec6a
    
    GitOrigin-RevId: b0c05d637ef69dd75fbcf4496f8e1c4a69e47ded

--- platform/execution.dashboard/backend/src/BackendRunDashboardSelectedItemProvider.kt
+++ platform/execution.dashboard/backend/src/BackendRunDashboardSelectedItemProvider.kt
@@ -6,11 +6,14 @@ import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.platform.execution.dashboard.RunDashboardManagerImpl
 import com.intellij.platform.execution.serviceView.BackendServiceViewSelectedItemProvider
+import com.intellij.platform.ide.productMode.IdeProductMode.Companion.isMonolith
 
 internal class BackendRunDashboardSelectedItemProvider : BackendServiceViewSelectedItemProvider {
   override fun getId(): String = "Run Dashboard"
 
   override fun getSelectedItem(project: Project, descriptorId: String): RunDashboardService? {
+    if (isMonolith) return null
+
     val serviceId = deserializeDescriptorId(descriptorId) ?: return null
     return RunDashboardManagerImpl.getInstance(project).findServiceById(serviceId)
   }
