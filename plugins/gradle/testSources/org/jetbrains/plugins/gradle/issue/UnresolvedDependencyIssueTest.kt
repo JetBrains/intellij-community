4ec2efe7d98e64d3be3e13b6fce0cb4c5d5ba80a
Kajus Kuncaitis <kajus.kuncaitis@jetbrains.com>
2026-01-09 15:22:52 +0100

    [gradle][IDEA-384297] Add unit tests to check for offline mode quick-fix id and description hyperlink validity
    Space-RevId: 55313d85ec61ceda47f8c01989918ccc95fe39f4
    
    GitOrigin-RevId: 0bdcc15b20a4bc48faf8c7f5c0091e6a957b08ba

---
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/issue/UnresolvedDependencyIssueTest.kt
@@ -0,0 +1,52 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.gradle.issue
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class UnresolvedDependencyIssueTest {
+  @Test
+  fun `test unresolved dependency during sync in offline mode has valid quick-fix`() {
+    val syncIssue = UnresolvedDependencySyncIssue(
+      dependencyName = "my.not.existing.dependency:gradle:1.2.3-dev",
+      failureMessage = "No cached version of my.not.existing.dependency:gradle:1.2.3-dev available for offline mode.",
+      projectPath = "irrelevant",
+      isOfflineMode = true
+    )
+    val actualQuickFixes = syncIssue.quickFixes
+    assertEquals(1, actualQuickFixes.size) { "Should contain just the offline mode quick-fix" }
+    val offlineQuickFixActualId = actualQuickFixes[0].id
+    assertEquals(
+      """
+        No cached version of my.not.existing.dependency:gradle:1.2.3-dev available for offline mode.
+  
+        Possible solution:
+         - <a href="$offlineQuickFixActualId">Disable offline mode and reload the project</a>
+        
+      """.trimIndent(),
+      syncIssue.description
+    )
+  }
+
+  @Test
+  fun `test unresolved dependency during build in offline mode has valid quick-fix`() {
+    val syncIssue = UnresolvedDependencyBuildIssue(
+      dependencyName = "my.not.existing.dependency:gradle:1.2.3-dev",
+      failureMessage = "No cached version of my.not.existing.dependency:gradle:1.2.3-dev available for offline mode.",
+      isOfflineMode = true
+    )
+    val actualQuickFixes = syncIssue.quickFixes
+    assertEquals(1, actualQuickFixes.size) { "Should contain just the offline mode quick-fix" }
+    val offlineQuickFixActualId = actualQuickFixes[0].id
+    assertEquals(
+      """
+        No cached version of my.not.existing.dependency:gradle:1.2.3-dev available for offline mode.
+  
+        Possible solution:
+         - <a href="$offlineQuickFixActualId">Disable offline mode and rerun the build</a>
+        
+      """.trimIndent(),
+      syncIssue.description
+    )
+  }
+}
