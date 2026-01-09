a4c5290a90957c46c21c0cc6f0cb740ae9d6f5c7
Daniil Maslov <daniil.maslov@jetbrains.com>
2026-01-09 11:58:00 +0100

    GOQA-163: update downloadGoSdk() to use eel in tests
    GitOrigin-RevId: be074b3780c892a55ffe65693632a8586d3d7759

--- tools/intellij.tools.ide.starter/src/com/intellij/ide/starter/golang.kt
+++ tools/intellij.tools.ide.starter/src/com/intellij/ide/starter/golang.kt
@@ -1,21 +1,26 @@
 package com.intellij.ide.starter
 
 import com.intellij.ide.starter.path.GlobalPaths
+import com.intellij.ide.starter.runner.targets.TargetIdentifier
 import com.intellij.ide.starter.utils.FileSystem
 import com.intellij.ide.starter.utils.HttpClient
-import com.intellij.openapi.util.SystemInfo
+import com.intellij.platform.eel.isArm64
+import com.intellij.platform.eel.isLinux
+import com.intellij.platform.eel.isMac
+import com.intellij.platform.eel.isWindows
 import java.nio.file.Path
 import kotlin.io.path.exists
 
 fun downloadGoSdk(version: String): Path {
+  val platform = TargetIdentifier.current.eelApi.platform
   val os = when {
-    SystemInfo.isWindows -> "windows"
-    SystemInfo.isLinux -> "linux"
-    SystemInfo.isMac -> "darwin"
+    platform.isWindows -> "windows"
+    platform.isLinux -> "linux"
+    platform.isMac -> "darwin"
     else -> error("Unknown OS")
   }
-  val arch = if (SystemInfo.isAarch64) "arm64" else "amd64"
-  val extension = if (SystemInfo.isWindows) ".zip" else ".tar.gz"
+  val arch = if (platform.isArm64) "arm64" else "amd64"
+  val extension = if (platform.isWindows) ".zip" else ".tar.gz"
   val sdkFileName = "go$version.$os-$arch$extension"
   val url = "https://cache-redirector.jetbrains.com/dl.google.com/go/$sdkFileName"
   val dirToDownload = GlobalPaths.instance.getCacheDirectoryFor("go-sdk/$version")
