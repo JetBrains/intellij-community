184190c6351758ca0cf92f9eb28214e743ddda1f
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2026-01-09 20:56:24 +0100

    IJPL-220985 services: run dashboard: fix edit configuration action enablement
    (cherry picked from commit 73a4df47454c62b11dab624b0d688e86b544199d)
    
    IJ-CR-187553
    
    Space-RevId: c28b0cb3da396e0fd23bf9eb76a3d0bff296394d
    
    GitOrigin-RevId: b176895fe5db6e34b996b64faf5f0100cec59755

--- platform/execution.dashboard/src/actions/RestoreConfigurationAction.java
+++ platform/execution.dashboard/src/actions/RestoreConfigurationAction.java
@@ -27,7 +27,8 @@ final class RestoreConfigurationAction
   public void update(@NotNull AnActionEvent e) {
     Project project = e.getProject();
     RunDashboardRunConfigurationNode node = project == null ? null : getTarget(e);
-    boolean enabled = node != null && !RunManager.getInstance(project).hasSettings(node.getConfigurationSettings());
+    // TODO [nikita.katkov] re-enable action
+    boolean enabled = node != null && node.getConfigurationSettings() != null && !RunManager.getInstance(project).hasSettings(node.getConfigurationSettings());
     Presentation presentation = e.getPresentation();
     presentation.setEnabled(enabled);
     boolean popupPlace = e.isFromContextMenu();
