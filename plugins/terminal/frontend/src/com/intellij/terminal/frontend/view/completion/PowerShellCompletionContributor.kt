f0f2c950a95206a2de924a21051e966877c88159
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-01-09 17:38:00 +0200

    [terminal] IJPL-207768 Parse PowerShell completions and merge with command spec ones
    Space-RevId: 87d57fce032d1f8292a629b9e294fd8b2b393477
    
    GitOrigin-RevId: db4a4388868ba8c9607c541b19a99ea205aae876

--- plugins/terminal/frontend/src/com/intellij/terminal/frontend/view/completion/PowerShellCompletionContributor.kt
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/view/completion/PowerShellCompletionContributor.kt
@@ -1,19 +1,60 @@
 package com.intellij.terminal.frontend.view.completion
 
+import com.intellij.openapi.diagnostic.fileLogger
+import com.intellij.openapi.diagnostic.trace
 import com.intellij.openapi.util.Disposer
 import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.serialization.json.Json
+import org.jetbrains.plugins.terminal.block.completion.powershell.PowerShellCompletionResult
+import org.jetbrains.plugins.terminal.block.completion.spec.ShellCompletionSuggestion
+import org.jetbrains.plugins.terminal.view.shellIntegration.TerminalCommandBlock
 import org.jetbrains.plugins.terminal.view.shellIntegration.TerminalShellBasedCompletionListener
 import org.jetbrains.plugins.terminal.view.shellIntegration.TerminalShellIntegration
+import org.jetbrains.plugins.terminal.view.shellIntegration.getTypedCommandText
 import kotlin.coroutines.resume
 
-internal suspend fun getPowerShellCompletionSuggestions(context: TerminalCommandCompletionContext): TerminalCompletionResult {
+internal suspend fun getPowerShellCompletionSuggestions(context: TerminalCommandCompletionContext): TerminalCompletionResult? {
   context.terminalView.sendText(CALL_COMPLETION_SEQUENCE)
 
   val result: String = awaitCompletionResult(context.shellIntegration)
-  // TODO: parse
-  println(result)
+  LOG.trace { "PowerShell completion result: $result" }
 
-  return TerminalCompletionResult(emptyList(), "")
+  val json = Json { ignoreUnknownKeys = true }
+  val completionResult: PowerShellCompletionResult = try {
+    json.decodeFromString(result)
+  }
+  catch (ex: Exception) {
+    LOG.error("Failed to parse PowerShell completion result: $result", ex)
+    return null
+  }
+
+  if (completionResult.matches.isEmpty()) {
+    return null
+  }
+
+  val activeBlock = context.shellIntegration.blocksModel.activeBlock as TerminalCommandBlock
+  val commandText = activeBlock.getTypedCommandText(context.outputModel) ?: return null
+  val absCursorOffset = context.outputModel.cursorOffset
+  val localCursorOffset = (absCursorOffset - activeBlock.commandStartOffset!!).toInt()
+
+  val replacementIndex = completionResult.replacementIndex
+  val replacementLength = completionResult.replacementLength
+  if (replacementIndex < 0 || replacementLength < 0 || replacementIndex + replacementLength > commandText.length) {
+    LOG.error("""Incorrect completion replacement indexes.
+        |Command: '$commandText'
+        |CursorOffset: $localCursorOffset
+        |Completion Result: $completionResult""".trimMargin())
+    return null
+  }
+
+  val prefix = commandText.substring(replacementIndex, localCursorOffset)
+  val suggestions = completionResult.matches.map { item ->
+    ShellCompletionSuggestion(item.value) {
+      displayName(item.presentableText ?: item.value)
+    }
+  }
+
+  return TerminalCompletionResult(suggestions, prefix)
 }
 
 private suspend fun awaitCompletionResult(shellIntegration: TerminalShellIntegration): String {
@@ -34,3 +75,5 @@ private suspend fun awaitCompletionResult(shellIntegration: TerminalShellIntegra
  * Our PowerShell integration script binds completion to this sequence.
  */
 private const val CALL_COMPLETION_SEQUENCE = "\u001b[24~e"
+
+private val LOG = fileLogger()
