21ae4db9bfb66200a37f32e9b8c8771d79eb8625
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2026-01-12 15:15:52 +0100

    IJPL-225441 Stop loading VFS files when reaching excluded dir registered via an entity
    Space-RevId: a110235ffb12d685a505cd828d189c409556ceb4
    
    GitOrigin-RevId: 851edf979ece0484f9b2a133cf8e7fe0deacf26d

--- platform/platform-impl/src/com/intellij/openapi/project/impl/ExcludeRootsCache.java
+++ platform/platform-impl/src/com/intellij/openapi/project/impl/ExcludeRootsCache.java
@@ -2,18 +2,30 @@
 package com.intellij.openapi.project.impl;
 
 import com.intellij.openapi.application.ReadAction;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.ProjectManagerListener;
-import com.intellij.openapi.roots.ModuleRootManager;
-import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.platform.backend.workspace.WorkspaceModel;
+import com.intellij.platform.backend.workspace.impl.WorkspaceModelInternal;
+import com.intellij.platform.workspace.storage.EntityStorage;
+import com.intellij.platform.workspace.storage.WorkspaceEntity;
+import com.intellij.platform.workspace.storage.url.VirtualFileUrl;
 import com.intellij.util.ArrayUtilRt;
 import com.intellij.util.messages.SimpleMessageBusConnection;
+import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileIndexContributor;
+import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileKind;
+import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileSetData;
+import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileSetRegistrar;
+import kotlin.jvm.functions.Function1;
+import kotlin.sequences.SequencesKt;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 // provides list of all excluded folders across all opened projects, fast.
 final class ExcludeRootsCache {
@@ -47,19 +59,28 @@ final class ExcludeRootsCache {
     return ReadAction.compute(() -> {
       CachedUrls cache = myCache;
       long actualModCount = Arrays.stream(ProjectManager.getInstance().getOpenProjects())
-        .map(ProjectRootManager::getInstance)
-        .mapToLong(ProjectRootManager::getModificationCount)
+        .mapToLong(project -> {
+          WorkspaceModelInternal workspaceModel = (WorkspaceModelInternal)WorkspaceModel.getInstance(project);
+          return workspaceModel.getEntityStorage().getVersion();
+        })
         .sum();
       String[] urls;
       if (cache != null && actualModCount == cache.myModificationCount) {
         urls = cache.myUrls;
       }
       else {
-        Collection<String> excludedUrls = new HashSet<>();
+        Set<String> excludedUrls = new HashSet<>();
         for (Project project : ProjectManager.getInstance().getOpenProjects()) {
-          for (Module module : ModuleManager.getInstance(project).getModules()) {
-            Collections.addAll(excludedUrls, ModuleRootManager.getInstance(module).getExcludeRootUrls());
+          WorkspaceModel workspaceModel = WorkspaceModel.getInstance(project);
+          EntityStorage storage = workspaceModel.getCurrentSnapshot();
+
+          // Collect excluded URLs from all contributors using the same pattern as ProjectRootManagerComponent
+          ExtensionPointName<WorkspaceFileIndexContributor<?>> EP_NAME = ExtensionPointName.create("com.intellij.workspaceModel.fileIndexContributor");
+          ExcludedRootsCollector collector = new ExcludedRootsCollector();
+          for (WorkspaceFileIndexContributor<?> contributor : EP_NAME.getExtensionList()) {
+            collectExcludedRootsFromContributor(contributor, storage, collector);
           }
+          excludedUrls.addAll(collector.getExcludedUrls());
         }
         urls = ArrayUtilRt.toStringArray(excludedUrls);
         Arrays.sort(urls);
@@ -67,5 +88,75 @@ final class ExcludeRootsCache {
       }
       return Arrays.asList(urls);
     });
+  }
+
+  private static <E extends WorkspaceEntity> void collectExcludedRootsFromContributor(
+    WorkspaceFileIndexContributor<E> contributor,
+    EntityStorage storage,
+    ExcludedRootsCollector collector) {
+    // Convert Kotlin Sequence to Iterable for Java iteration
+    Iterable<E> entities = SequencesKt.asIterable(storage.entities(contributor.getEntityClass()));
+    for (E entity : entities) {
+      contributor.registerFileSets(entity, collector, storage);
+    }
+  }
+
+  private static class ExcludedRootsCollector implements WorkspaceFileSetRegistrar {
+    private final Set<String> excludedUrls = new HashSet<>();
+
+    @Override
+    public void registerFileSet(@NotNull VirtualFileUrl root,
+                                @NotNull WorkspaceFileKind kind,
+                                @NotNull WorkspaceEntity entity,
+                                WorkspaceFileSetData customData) {
+      // We only care about excluded roots
+    }
+
+    @Override
+    public void registerFileSet(@NotNull VirtualFile root,
+                                @NotNull WorkspaceFileKind kind,
+                                @NotNull WorkspaceEntity entity,
+                                WorkspaceFileSetData customData) {
+      // We only care about excluded roots
+    }
+
+    @Override
+    public void registerExcludedRoot(@NotNull VirtualFileUrl excludedRoot, @NotNull WorkspaceEntity entity) {
+      excludedUrls.add(excludedRoot.getUrl());
+    }
+
+    @Override
+    public void registerExcludedRoot(@NotNull VirtualFileUrl excludedRoot,
+                                     @NotNull WorkspaceFileKind excludedFrom,
+                                     @NotNull WorkspaceEntity entity) {
+      excludedUrls.add(excludedRoot.getUrl());
+    }
+
+    @Override
+    public void registerExclusionPatterns(@NotNull VirtualFileUrl root,
+                                         @NotNull List<String> patterns,
+                                         @NotNull WorkspaceEntity entity) {
+      // Exclusion patterns are not URLs themselves
+    }
+
+    @Override
+    public void registerExclusionCondition(@NotNull VirtualFileUrl root,
+                                          @NotNull Function1<? super VirtualFile, Boolean> condition,
+                                          @NotNull WorkspaceEntity entity) {
+      // Exclusion conditions are not URLs themselves
+    }
+
+    @Override
+    public void registerNonRecursiveFileSet(@NotNull VirtualFileUrl file,
+                                           @NotNull WorkspaceFileKind kind,
+                                           @NotNull WorkspaceEntity entity,
+                                           WorkspaceFileSetData customData) {
+      // We only care about excluded roots
+    }
+
+    @NotNull
+    Set<String> getExcludedUrls() {
+      return excludedUrls;
+    }
   }
 }
--- platform/platform-tests/testSrc/com/intellij/openapi/roots/impl/OnlyIndexableFilesAreLoadedIntoVfsOnDirectoryCreationTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/roots/impl/OnlyIndexableFilesAreLoadedIntoVfsOnDirectoryCreationTest.kt
@@ -10,12 +10,15 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.newvfs.RefreshQueue
 import com.intellij.platform.backend.workspace.toVirtualFileUrl
 import com.intellij.platform.backend.workspace.workspaceModel
+import com.intellij.platform.workspace.storage.impl.url.toVirtualFileUrl
 import com.intellij.testFramework.TestObservation
 import com.intellij.testFramework.VfsTestUtil
 import com.intellij.testFramework.junit5.TestApplication
 import com.intellij.testFramework.junit5.TestDisposable
 import com.intellij.testFramework.rules.TempDirectoryExtension
 import com.intellij.testFramework.useProjectAsync
+import com.intellij.util.indexing.testEntities.ExcludedKindFileSetTestContributor
+import com.intellij.util.indexing.testEntities.ExcludedTestEntity
 import com.intellij.util.indexing.testEntities.NonRecursiveFileSetContributor
 import com.intellij.util.indexing.testEntities.NonRecursiveTestEntity
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileIndex
@@ -30,6 +33,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
 import java.nio.file.Path
 import kotlin.io.path.pathString
+import kotlin.io.path.writeText
 import kotlin.time.Duration.Companion.seconds
 
 @TestApplication
@@ -44,6 +48,7 @@ class OnlyIndexableFilesAreLoadedIntoVfsOnDirectoryCreationTest {
   @BeforeEach
   fun setUp() {
     WorkspaceFileIndexImpl.EP_NAME.point.registerExtension(NonRecursiveFileSetContributor(), disposable)
+    WorkspaceFileIndexImpl.EP_NAME.point.registerExtension(ExcludedKindFileSetTestContributor(), disposable)
   }
 
   private fun findVirtualFile(path: Path): VirtualFile {
@@ -93,4 +98,38 @@ class OnlyIndexableFilesAreLoadedIntoVfsOnDirectoryCreationTest {
       assertThat(filesInVfs).hasSize(3)
     }
   }
+
+  @Test
+  fun `test that VFS loading stops at excluded dir registered via an entity`(): Unit = runBlocking {
+    rootDir.newFileNio("pom.xml").writeText(pom)
+
+    ProjectUtil.openOrImportAsync(rootDir.rootPath)!!.useProjectAsync { project ->
+      TestObservation.awaitConfiguration(project)
+      val rootVirtualFile = findVirtualFile(rootDir.rootPath)
+      rootVirtualFile.children // load all children to trigger full sync later
+      Assertions.assertTrue(readAction { WorkspaceFileIndex.getInstance(project).isIndexable(rootVirtualFile) })
+
+      val excludedUrl = rootDir.rootPath.resolve("d1/excluded").toVirtualFileUrl(project.workspaceModel.getVirtualFileUrlManager())
+      project.workspaceModel.update("Add excluded dir $excludedUrl") {
+        it.addEntity(ExcludedTestEntity(excludedUrl, NonPersistentEntitySource))
+      }
+
+      rootDir.newDirectoryPath("d1/excluded/d3")
+      delay(1.seconds) // wait for fs events to arrive
+      RefreshQueue.getInstance().refresh(true, listOf(rootVirtualFile))
+      val filesInVfs = visitChildrenInVfsRecursively(rootVirtualFile).toList()
+      assertThat(filesInVfs).noneMatch { it.name == "d3" }
+    }
+  }
+
+  private val pom = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                    "<project xmlns=\"http://maven.apache.org/POM/4.0.0\"\n" +
+                    "         xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+                    "         xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n" +
+                    "    <modelVersion>4.0.0</modelVersion>\n" +
+                    "\n" +
+                    "    <groupId>org.example</groupId>\n" +
+                    "    <artifactId>main</artifactId>\n" +
+                    "    <version>1.0-SNAPSHOT</version>\n" +
+                    "</project>"
 }
