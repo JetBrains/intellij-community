aef22e67bf3b10dcdff6305bfc9b908e68395900
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2026-01-13 23:03:21 +0100

    [log] Add tracing for VcsLogGraphDataFactory (IJPL-221571)
    GitOrigin-RevId: 2edac9f3fa1f5097852a18bfcf79b2bb5e4fcec4

--- platform/vcs-log/impl/src/com/intellij/vcs/log/data/VcsLogGraphDataFactory.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/data/VcsLogGraphDataFactory.kt
@@ -1,24 +1,26 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.log.data
 
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.diagnostic.trace
 import com.intellij.openapi.vcs.telemetry.VcsBackendTelemetrySpan
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.diagnostic.telemetry.TelemetryManager.Companion.getInstance
 import com.intellij.platform.diagnostic.telemetry.helpers.use
 import com.intellij.platform.vcs.impl.shared.telemetry.VcsScope
 import com.intellij.vcs.log.*
-import com.intellij.vcs.log.graph.GraphColorManagerImpl
-import com.intellij.vcs.log.graph.GraphCommit
-import com.intellij.vcs.log.graph.HeadCommitsComparator
-import com.intellij.vcs.log.graph.PermanentGraph
+import com.intellij.vcs.log.graph.*
 import com.intellij.vcs.log.graph.impl.facade.PermanentGraphImpl
 import com.intellij.vcs.log.graph.impl.print.GraphColorGetterByHeadFactory
 import it.unimi.dsi.fastutil.ints.IntOpenHashSet
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.NonNls
+import kotlin.time.measureTimedValue
 
 @ApiStatus.Internal
 object VcsLogGraphDataFactory {
+  private val LOG = logger<VcsLogGraphDataFactory>()
+
   @JvmStatic
   fun buildData(
     commits: List<GraphCommit<VcsLogCommitStorageIndex>>,
@@ -27,8 +29,12 @@ object VcsLogGraphDataFactory {
     storage: VcsLogStorage,
     full: Boolean,
   ): VcsLogGraphData {
-    val refsModel = RefsModel.create(refs, getHeads(commits), storage, providers)
-    val permanentGraph = buildPermanentGraph(commits, refsModel, providers, storage)
+    val (heads, headsCalculationTime) = measureTimedValue { getHeads(commits) }
+    LOG.trace { "Heads calculated in ${headsCalculationTime.inWholeMilliseconds} ms" }
+    val (refsModel, refsModelCreationTime) = measureTimedValue { RefsModel.create(refs, heads, storage, providers) }
+    LOG.trace { "Refs model created in ${refsModelCreationTime.inWholeMilliseconds} ms" }
+    val (permanentGraph, permanentGraphBuildTime) = measureTimedValue { buildPermanentGraph(commits, refsModel, providers, storage) }
+    LOG.trace { "Permanent graph created in ${permanentGraphBuildTime.inWholeMilliseconds} ms" }
 
     return buildData(refsModel, permanentGraph, providers, full)
   }
