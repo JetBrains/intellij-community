5fd6ed44045e3be7f6a03dc28e077b4c50c4a6fb
Ivan Semenov <ivan.semenov@jetbrains.com>
2026-01-13 15:40:06 +0100

    refactor [vcs]: move project changes refresh to a single place
    Space-RevId: 3174f3da9aa63a3770c3d65d151a948943ef6b60
    
    GitOrigin-RevId: 5084b77e4302aa7539acb62f223c5878ea0ced7a

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/RefreshAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/RefreshAction.kt
@@ -1,26 +1,14 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vcs.changes.actions
 
-import com.intellij.configurationStore.saveSettings
-import com.intellij.ide.IdeBundle
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.application.EDT
-import com.intellij.openapi.diagnostic.logger
-import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.ProjectLevelVcsManager
-import com.intellij.openapi.vcs.changes.ChangeListManager
-import com.intellij.openapi.vcs.changes.ChangeListManagerEx
-import com.intellij.openapi.vcs.changes.ChangesViewRefresher
-import com.intellij.openapi.vcs.changes.VcsDirtyScopeManager
-import com.intellij.platform.ide.progress.runWithModalProgressBlocking
+import com.intellij.openapi.vcs.changes.ChangeListManagerRefreshHelper
 import com.intellij.util.concurrency.annotations.RequiresEdt
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import org.jetbrains.annotations.CalledInAny
 
 class RefreshAction : AnAction(), DumbAware {
   override fun update(e: AnActionEvent) {
@@ -37,72 +25,15 @@ class RefreshAction : AnAction(), DumbAware {
     doRefresh(project)
   }
 
+  @Suppress("CompanionObjectInExtension")
   companion object {
-    private val LOG = logger<RefreshAction>()
-
+    @Deprecated("Use ChangeListManagerRefreshHelper instead",
+                replaceWith = ReplaceWith("ChangeListManagerRefreshHelper.refreshSync(project)",
+                                          "com.intellij.openapi.vcs.changes.ChangeListManagerRefreshHelper"))
     @JvmStatic
     @RequiresEdt
     fun doRefresh(project: Project) {
-      if (project.isDisposed()) return
-      if (ChangeListManager.getInstance(project).isFreezedWithNotification(null)) return
-
-      doRefreshAndReportMetrics(project) {
-        saveAllAndInvokeCustomRefreshersOnEdt(project)
-      }
-    }
-
-    @RequiresEdt
-    internal fun saveAllAndInvokeCustomRefreshersOnEdt(project: Project) {
-      LOG.info("Saving all documents and project settings")
-
-      FileDocumentManager.getInstance().saveAllDocuments()
-      runWithModalProgressBlocking(project, IdeBundle.message("progress.saving.project", project.name)) {
-        saveSettings(project)
-      }
-      invokeCustomRefreshes(project)
-    }
-
-    internal suspend fun doRefreshSuspending(project: Project) {
-      if (project.isDisposed()) return
-      if (ChangeListManager.getInstance(project).isFreezed != null) return
-
-      withContext(Dispatchers.Default) {
-        doRefreshAndReportMetrics(project) {
-          withContext(Dispatchers.EDT) {
-            FileDocumentManager.getInstance().saveAllDocuments()
-          }
-          saveSettings(project)
-          invokeCustomRefreshes(project)
-        }
-      }
-    }
-
-    private fun invokeCustomRefreshes(project: Project) {
-      for (refresher in ChangesViewRefresher.EP_NAME.getExtensionList(project)) {
-        refresher.refresh(project)
-      }
-    }
-
-    @CalledInAny
-    private inline fun doRefreshAndReportMetrics(project: Project, refreshAction: () -> Unit) {
-      LOG.debug("Performing changes refresh")
-
-      val changeListManager = ChangeListManagerEx.getInstanceEx(project)
-      val changesBeforeUpdate = changeListManager.getAllChanges()
-      val unversionedBefore = changeListManager.getUnversionedFilesPaths()
-      val wasUpdatingBefore = changeListManager.isInUpdate()
-
-      refreshAction()
-
-      VcsDirtyScopeManager.getInstance(project).markEverythingDirty()
-
-      changeListManager.invokeAfterUpdate(false) {
-        val changesAfterUpdate = changeListManager.getAllChanges()
-        val unversionedAfter = changeListManager.getUnversionedFilesPaths()
-        VcsStatisticsCollector
-          .logRefreshActionPerformed(project, changesBeforeUpdate, changesAfterUpdate, unversionedBefore, unversionedAfter,
-                                     wasUpdatingBefore)
-      }
+      ChangeListManagerRefreshHelper.refreshSync(project)
     }
   }
 }
--- platform/vcs-impl/src/com/intellij/vcs/commit/ChangesViewToolWindowRefresher.kt
+++ platform/vcs-impl/src/com/intellij/vcs/commit/ChangesViewToolWindowRefresher.kt
@@ -2,34 +2,16 @@
 package com.intellij.vcs.commit
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vcs.changes.actions.RefreshAction
+import com.intellij.openapi.vcs.changes.ChangeListManagerRefreshHelper
 import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager
 import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager.Companion.LOCAL_CHANGES
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ex.ToolWindowManagerListener
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.channels.BufferOverflow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.launch
-import kotlin.time.Duration.Companion.milliseconds
 
-private class ChangesViewToolWindowRefresher(private val project: Project, coroutineScope: CoroutineScope) : ToolWindowManagerListener {
-  private val refreshFlow = MutableSharedFlow<Unit>(replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
-
-  init {
-    coroutineScope.launch {
-      @OptIn(FlowPreview::class)
-      refreshFlow.debounce(300.milliseconds).collect {
-        RefreshAction.doRefreshSuspending(project)
-      }
-    }
-  }
-
+private class ChangesViewToolWindowRefresher(private val project: Project) : ToolWindowManagerListener {
   override fun toolWindowShown(toolWindow: ToolWindow) {
     if (ChangesViewContentManager.getToolWindowIdFor(project, LOCAL_CHANGES) == toolWindow.id) {
-      refreshFlow.tryEmit(Unit)
+      ChangeListManagerRefreshHelper.requestRefresh(project)
     }
   }
 }
