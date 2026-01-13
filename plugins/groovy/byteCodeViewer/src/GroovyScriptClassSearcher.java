3db52be701b3e61340ab9922a599ed1fa0e6480c
Nikolay Chashnikov <nikolay.chashnikov@jetbrains.com>
2026-01-13 12:01:12 +0100

    IDEA-384598 Groovy: use the standard package name for classes in intellij.groovy.byteCodeViewer module
    GitOrigin-RevId: 4fabe0b91ea1db436b96664d12f5bd71a327431a

--- plugins/groovy/byteCodeViewer/src/GroovyScriptClassSearcher.java
+++ plugins/groovy/byteCodeViewer/src/GroovyScriptClassSearcher.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.groovy.byteCodeViewer;
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.groovy.byteCodeViewer;
 
 import com.intellij.byteCodeViewer.ClassSearcher;
 import com.intellij.psi.PsiClass;
