3a09a44b2d5c244da4dcbab9dff1213acf99087d
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2026-01-14 15:20:08 +0100

    IJPL-228968 Replace hardcoded url with Help Id in IgnoredToExcludedSynchronizer
    New id is `help.content.roots.folder.categories`
    
    
    IJ-MR-187914
    
    GitOrigin-RevId: a3475d0f815cd39747a7c24f6472d7d182abe274

--- platform/dvcs-impl/src/com/intellij/dvcs/ignore/IgnoredToExcludedSynchronizer.kt
+++ platform/dvcs-impl/src/com/intellij/dvcs/ignore/IgnoredToExcludedSynchronizer.kt
@@ -1,9 +1,8 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.dvcs.ignore
 
 import com.intellij.CommonBundle
 import com.intellij.icons.AllIcons
-import com.intellij.ide.BrowserUtil
 import com.intellij.ide.projectView.actions.MarkRootsManager
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.idea.ActionsBundle
@@ -17,6 +16,7 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.help.HelpManager
 import com.intellij.openapi.module.ModuleUtil
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.DumbAware
@@ -314,7 +314,7 @@ class IgnoredToExcludeNotificationProvider : EditorNotificationProvider, DumbAwa
       panel.createActionLabel(message("ignore.to.exclude.notification.action.view")) { showIgnoredAction(project) }
       panel.createActionLabel(message("ignore.to.exclude.notification.action.mute"), muteAction(project))
       panel.createActionLabel(message("ignore.to.exclude.notification.action.details")) {
-        BrowserUtil.browse("https://www.jetbrains.com/help/idea/content-roots.html#folder-categories")
+        HelpManager.getInstance().invokeHelp("help.content.roots.folder.categories")
       }
       panel
     }
