c8f045061ec4cd8eac813d6bb862cdab8091dd14
Sergei Tachenov <Sergei.Tachenov@jetbrains.com>
2026-01-14 10:38:55 +0200

    [UI, Wayland] IJPL-222913 Fix the default popup background
    The popup window and its root pane by default have
    white background. That's what we see as flickering.
    Replace it with the content's background
    (normally it's just Panel.background).
    
    GitOrigin-RevId: 08f23a5d99c51c359d8b2b405f0a0665d3de980f

--- platform/platform-impl/src/com/intellij/ui/popup/AbstractPopup.java
+++ platform/platform-impl/src/com/intellij/ui/popup/AbstractPopup.java
@@ -1330,8 +1330,9 @@ public class AbstractPopup implements JBPopup, ScreenAreaConsumer, AlignedPopup 
       setSize(targetBounds.getSize()); // Might need to make it smaller than its preferred size.
     }
 
+    final JRootPane root = myContent.getRootPane();
+
     if (myResizable) {
-      final JRootPane root = myContent.getRootPane();
       final IdeGlassPaneImpl glass = new IdeGlassPaneImpl(root);
       root.setGlassPane(glass);
 
@@ -1356,6 +1357,11 @@ public class AbstractPopup implements JBPopup, ScreenAreaConsumer, AlignedPopup 
     if (window instanceof IdeFrame) {
       LOG.warn("Lightweight popup is shown using AbstractPopup class. But this class is not supposed to work with lightweight popups.");
     }
+
+    // In some environments, e.g. native Wayland, the default root and/or window background (white)
+    // may be displayed briefly, causing very noticeable flickering in dark themes (IJPL-222913).
+    window.setBackground(myContent.getBackground());
+    root.setBackground(myContent.getBackground());
 
     window.setFocusableWindowState(myFocusable);
     window.setFocusable(myRequestFocus);
