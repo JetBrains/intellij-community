672c18f814c75faa0dc52331a323d5422c18319e
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2026-01-14 10:34:59 +0100

    [log] Lazy calculations in RefsModel (IJPL-221571)
    Previously, values for `bestRefForHead` and `rootForHead` were pre-populated.
    However, it required all commits traversal to get graph heads (from commits model) and was used only in this place.
    However, similar heads traversal was performed with the graph model later.
    
    Space-RevId: 04d1affd03eba59d7641e18c1f8d5afe4bfe8ac1
    
    GitOrigin-RevId: 038ff9a7f2b94a8b364171e504922e54711a03e1

--- platform/vcs-log/impl/src/com/intellij/vcs/log/data/RefsModel.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/data/RefsModel.kt
@@ -1,13 +1,10 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.log.data
 
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.diagnostic.debug
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.vcs.log.*
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap
-import it.unimi.dsi.fastutil.ints.IntOpenHashSet
 import org.jetbrains.annotations.ApiStatus
 
 internal class RefsModel private constructor(
@@ -15,63 +12,49 @@ internal class RefsModel private constructor(
   private val storage: VcsLogStorage,
   private val providers: Map<VirtualFile, VcsLogProvider>,
 ) : VcsLogAggregatedStoredRefs {
+  private val singleRoot = refsByRoot.keys.singleOrNull()
+
   private val bestRefForHead: Int2ObjectMap<VcsRef> = Int2ObjectOpenHashMap()
   private val rootForHead: Int2ObjectMap<VirtualFile> = Int2ObjectOpenHashMap()
 
-  override fun getRefForHeadCommit(headIndex: VcsLogCommitStorageIndex): VcsRef? = bestRefForHead[headIndex]
-
-  override fun getRootForHeadCommit(headIndex: VcsLogCommitStorageIndex): VirtualFile? = rootForHead[headIndex]
-
-  override fun refsToCommit(index: VcsLogCommitStorageIndex): List<VcsRef> {
-    if (refsByRoot.size <= 10) {
-      val refs = refsByRoot.values.firstOrNull { it.contains(index) }
-      return refs?.refsToCommit(index) ?: emptyList()
-    }
-    val id = storage.getCommitId(index) ?: return emptyList()
-    return refsToCommit(id.root, index)
+  override fun getRefForHeadCommit(headIndex: VcsLogCommitStorageIndex): VcsRef? = bestRefForHead.computeIfAbsent(headIndex) {
+    val id = storage.getCommitId(headIndex) ?: return@computeIfAbsent null
+    refsToCommit(headIndex).minWithOrNull(providers[id.root]!!.referenceManager.branchLayoutComparator)
   }
 
-  private fun updateCacheForHead(head: VcsLogCommitStorageIndex, root: VirtualFile) {
-    rootForHead.put(head, root)
-
-    val bestRef = refsToCommit(root, head).minWithOrNull(providers[root]!!.referenceManager.branchLayoutComparator)
-    if (bestRef != null) {
-      bestRefForHead.put(head, bestRef)
+  override fun getRootForHeadCommit(headIndex: VcsLogCommitStorageIndex): VirtualFile? {
+    return singleRoot ?: rootForHead.computeIfAbsent(headIndex) {
+      storage.getCommitId(headIndex)?.root
     }
-    else {
-      LOG.debug { "No references at head ${storage.getCommitId(head)}" }
+  }
+
+  override fun refsToCommit(index: VcsLogCommitStorageIndex): List<VcsRef> {
+    return when {
+      singleRoot != null -> refsByRoot.getValue(singleRoot).refsToCommit(index)
+      refsByRoot.size <= 10 -> {
+        val refs = refsByRoot.values.firstOrNull { it.contains(index) }
+        refs?.refsToCommit(index) ?: emptyList()
+      }
+      else -> {
+        val id = storage.getCommitId(index)
+        if (id != null) refsToCommit(id.root, index) else emptyList()
+      }
     }
   }
 
   companion object {
-    private val LOG = Logger.getInstance(RefsModel::class.java)
-
     @ApiStatus.Internal
     @JvmStatic
     fun createEmptyInstance(storage: VcsLogStorage): RefsModel {
-      return create(emptyMap(), emptySet(), storage, emptyMap())
+      return create(emptyMap(), storage, emptyMap())
     }
 
     @ApiStatus.Internal
     @JvmStatic
     fun create(
       refs: Map<VirtualFile, VcsLogRootStoredRefs>,
-      heads: Set<VcsLogCommitStorageIndex>,
       storage: VcsLogStorage,
       providers: Map<VirtualFile, VcsLogProvider>,
-    ): RefsModel {
-      val refsModel = RefsModel(refs, storage, providers)
-
-      val remainingHeads = IntOpenHashSet(heads)
-      refs.forEach { (root, refsForRoot) ->
-        refsForRoot.forEachBranchIndex { commit ->
-          refsModel.updateCacheForHead(commit, root)
-          remainingHeads.remove(commit)
-        }
-      }
-      storage.getCommitIds(remainingHeads).forEach { (head, commitId) -> refsModel.updateCacheForHead(head, commitId.root) }
-
-      return refsModel
-    }
+    ): RefsModel = RefsModel(refs, storage, providers)
   }
 }
