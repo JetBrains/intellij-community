6ae527caa2ba77ec87fbb8726cb8586bb6107035
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2026-01-14 13:06:24 +0100

    [threading] IJPL-219518: Verify that text processing is cancellable by context-based cancellation
    Space-RevId: 3d845e31833f6362262ddb4ec89c80cbbc9f3564
    
    GitOrigin-RevId: dd680b05c1f4c3e369f75868defc517f51dd3c46

--- platform/indexing-impl/src/com/intellij/psi/impl/search/PsiSearchHelperImpl.java
+++ platform/indexing-impl/src/com/intellij/psi/impl/search/PsiSearchHelperImpl.java
@@ -230,13 +230,15 @@ public class PsiSearchHelperImpl implements PsiSearchHelper {
       throw new IllegalArgumentException("Cannot search for elements with empty text");
     }
     if (searchScope instanceof GlobalSearchScope) {
-      ProgressIndicator progress = getOrCreateIndicator();
-      StringSearcher searcher = new StringSearcher(text, options.contains(Options.CASE_SENSITIVE_SEARCH), true,
-                                                   searchContext == UsageSearchContext.IN_STRINGS,
-                                                   options.contains(Options.PROCESS_ONLY_JAVA_IDENTIFIERS_IF_POSSIBLE));
+      return ConcurrencyUtils.runWithIndicatorOrContextCancellation((__) -> {
+        ProgressIndicator progress = getOrCreateIndicator();
+        StringSearcher searcher = new StringSearcher(text, options.contains(Options.CASE_SENSITIVE_SEARCH), true,
+                                                     searchContext == UsageSearchContext.IN_STRINGS,
+                                                     options.contains(Options.PROCESS_ONLY_JAVA_IDENTIFIERS_IF_POSSIBLE));
 
-      return processElementsWithTextInGlobalScope((GlobalSearchScope)searchScope, searcher, searchContext,
-                                                  options.contains(Options.CASE_SENSITIVE_SEARCH), containerName, session, progress, processor);
+        return processElementsWithTextInGlobalScope((GlobalSearchScope)searchScope, searcher, searchContext,
+                                                    options.contains(Options.CASE_SENSITIVE_SEARCH), containerName, session, progress, processor);
+      });
     }
     LocalSearchScope scope = (LocalSearchScope)searchScope;
     PsiElement[] scopeElements = scope.getScope();
--- platform/platform-tests/testSrc/com/intellij/openapi/application/impl/HeavyPlatformUtilitiesTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/application/impl/HeavyPlatformUtilitiesTest.kt
@@ -3,23 +3,43 @@ package com.intellij.openapi.application.impl
 
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.application.backgroundWriteAction
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.DumbModeTask
 import com.intellij.openapi.project.DumbService
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.LocalSearchScope
+import com.intellij.psi.search.PsiSearchHelper
+import com.intellij.psi.search.SearchScope
+import com.intellij.psi.search.UsageSearchContext
 import com.intellij.testFramework.IndexingTestUtil
 import com.intellij.testFramework.common.timeoutRunBlocking
 import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.testFramework.junit5.fixture.moduleFixture
 import com.intellij.testFramework.junit5.fixture.projectFixture
+import com.intellij.testFramework.junit5.fixture.psiFileFixture
+import com.intellij.testFramework.junit5.fixture.sourceRootFixture
 import com.intellij.util.application
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.future.asCompletableFuture
+import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.test.assertEquals
 
 @TestApplication
 class HeavyPlatformUtilitiesTest {
   val project = projectFixture(openAfterCreation = true)
+  val module = project.moduleFixture()
+  val sourceRoot = module.sourceRootFixture()
+  val file = sourceRoot.psiFileFixture("A.java", """class A {
+    |  String s = "abcde";
+    |  String f = "___abcde___";
+    |}""".trimMargin())
 
   @Test
   fun `waitUntilIndexesReady works with background WA`(): Unit = timeoutRunBlocking(context = Dispatchers.EDT) {
@@ -42,4 +62,36 @@ class HeavyPlatformUtilitiesTest {
     Assertions.assertTrue(dumbActionCompleted.get())
   }
 
+  @Test
+  fun `PsiSearchHelper#processElementsWithWord can be canceled on project scope`() = `PsiSearchHelperImpl cancellation test`(GlobalSearchScope.projectScope(project.get()))
+
+  @Test
+  fun `PsiSearchHelper#processElementsWithWord can be canceled on file scope`() = `PsiSearchHelperImpl cancellation test`(runReadAction { LocalSearchScope(file.get()) })
+
+  fun `PsiSearchHelperImpl cancellation test`(scope: SearchScope): Unit = timeoutRunBlocking {
+    val prj = project.get()
+    val j1 = Job(coroutineContext.job)
+    val j2 = Job(coroutineContext.job)
+    val counter = AtomicInteger(0)
+    val textProcessingJob = launch(Dispatchers.Default) {
+      PsiSearchHelper.getInstance(prj).processElementsWithWord(
+        { _, _ ->
+          val counterValue = counter.getAndIncrement()
+          if (counterValue == 0) {
+            j1.complete()
+            j2.asCompletableFuture().join()
+          }
+          if (counterValue == 1) {
+            error("Should not be reached")
+          }
+          true
+        }, scope, "abcde",
+        UsageSearchContext.ANY, false)
+    }
+    j1.join()
+    textProcessingJob.cancel()
+    j2.complete()
+    textProcessingJob.join()
+    assertEquals(1, counter.get())
+  }
 }
