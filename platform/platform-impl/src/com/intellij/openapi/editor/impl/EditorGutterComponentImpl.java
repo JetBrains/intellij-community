03712784ed07b463535e5e356a413dd22af08841
Ekaterina Berezhko <ekaterina.berezhko@jetbrains.com>
2026-01-14 15:25:25 +0200

    [UI][IJPL-202981] Fix breakpoints being covered after zooming
    Adjust initial line number width scaling
    
    GitOrigin-RevId: 266ee320e545793a3b01d7c1ef1067cbb254187d

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorGutterComponentImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorGutterComponentImpl.java
@@ -187,7 +187,7 @@ final class EditorGutterComponentImpl extends EditorGutterComponentEx
   private boolean myHasInlaysWithGutterIcons;
   private int myStartIconAreaWidth = ExperimentalUI.isNewUI() ? 0 : START_ICON_AREA_WIDTH.get();
   private int myIconsAreaWidth;
-  int myLineNumberAreaWidth = getInitialLineNumberWidth();
+  int myLineNumberAreaWidth = getInitialLineNumberWidth(false);
   int myAdditionalLineNumberAreaWidth;
   private @NotNull List<FoldRegion> myActiveFoldRegions = Collections.emptyList();
   int myTextAnnotationGuttersSize;
@@ -1853,7 +1853,7 @@ final class EditorGutterComponentImpl extends EditorGutterComponentEx
     }
     if (ExperimentalUI.isNewUI() && isRealEditor()) {
       //todo[kb] recalculate gutters renderers and return 0 if there are none in EditorMouseEventArea.LINE_NUMBERS_AREA
-      return JBUI.scale(14);
+      return EditorUIUtil.scaleWidth(JBUI.scale(14), myEditor);
     }
     return 0;
   }
@@ -2883,10 +2883,19 @@ final class EditorGutterComponentImpl extends EditorGutterComponentEx
     }
   }
 
-  private static int getInitialLineNumberWidth() {
+  private int getInitialLineNumberWidth() {
+    return getInitialLineNumberWidth(true);
+  }
+
+  private int getInitialLineNumberWidth(boolean applyEditorScale) {
     if (ExperimentalUI.isNewUI()) {
       //have a placeholder for breakpoints
-      return JBUI.scale(12);
+      int baseWidth = JBUI.scale(12);
+      if (applyEditorScale) {
+        return EditorUIUtil.scaleWidth(baseWidth, myEditor);
+      } else {
+        return baseWidth;
+      }
     }
     return 0;
   }
