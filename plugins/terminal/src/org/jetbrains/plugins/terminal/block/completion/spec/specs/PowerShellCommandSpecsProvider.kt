cf84738ea3498bde189f3082de4052ab8196432c
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-01-16 16:00:23 +0200

    [terminal] IJPL-207768 Show completion popup automatically for base PowerShell file manipulation cmdlets
    Completion popup is showing automatically now only for commands defined as a command spec. We can't use PowerShell-based completion here for performance reasons (it blocks PTY when computing suggestions and sending results).
    So, we have to define command specs for PowerShell cmdlets that should support showing completion popup automatically.
    
    GitOrigin-RevId: 08002d87701875747fdec8d7328b1afc155bdeb5

---
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/block/completion/spec/specs/PowerShellCommandSpecsProvider.kt
@@ -0,0 +1,64 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.terminal.block.completion.spec.specs
+
+import org.jetbrains.plugins.terminal.block.completion.spec.*
+
+internal class PowerShellCommandSpecsProvider : ShellCommandSpecsProvider {
+  override fun getCommandSpecs(): List<ShellCommandSpecInfo> {
+    return listOf(
+      pathSuggestingCommandSpec("Get-Content"),
+      pathSuggestingCommandSpec("Add-Content"),
+      pathSuggestingCommandSpec("Set-Content"),
+      pathSuggestingCommandSpec("Clear-Content"),
+      pathSuggestingCommandSpec("Set-Location", onlyDirectories = true),
+      pathSuggestingCommandSpec("Get-ChildItem"),
+      pathSuggestingCommandSpec("Remove-Item"),
+      pathSuggestingCommandSpec("Rename-Item"),
+      pathAndDestinationSuggestingCommandSpec("Copy-Item"),
+      pathAndDestinationSuggestingCommandSpec("Move-Item"),
+    )
+  }
+}
+
+private fun pathSuggestingCommandSpec(commandName: String, onlyDirectories: Boolean = false): ShellCommandSpecInfo {
+  val spec = ShellCommandSpec(commandName) {
+    option("-Path") {
+      argument {
+        suggestions(ShellDataGenerators.fileSuggestionsGenerator(onlyDirectories))
+      }
+    }
+
+    argument {
+      optional()
+      suggestions(ShellDataGenerators.fileSuggestionsGenerator(onlyDirectories))
+    }
+  }
+
+  return ShellCommandSpecInfo.create(spec, ShellCommandSpecConflictStrategy.DEFAULT)
+}
+
+private fun pathAndDestinationSuggestingCommandSpec(commandName: String): ShellCommandSpecInfo {
+  val spec = ShellCommandSpec(commandName) {
+    option("-Path") {
+      argument {
+        suggestions(ShellDataGenerators.fileSuggestionsGenerator())
+      }
+    }
+    option("-Destination") {
+      argument {
+        suggestions(ShellDataGenerators.fileSuggestionsGenerator())
+      }
+    }
+
+    argument {
+      optional()
+      suggestions(ShellDataGenerators.fileSuggestionsGenerator())
+    }
+    argument {
+      optional()
+      suggestions(ShellDataGenerators.fileSuggestionsGenerator())
+    }
+  }
+
+  return ShellCommandSpecInfo.create(spec, ShellCommandSpecConflictStrategy.DEFAULT)
+}
