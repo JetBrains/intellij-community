0096714303fb6e23100e337d2ae9b58332873ca8
Alexander Kuznetsov <aleksander.kuznetsov@jetbrains.com>
2026-01-16 13:51:15 +0100

    [JEWEL-1138] Scale images to fit available width
    Update placeholders during measurement phase based on available width.
    Fast path is available for text without images
    or with only images with zero width,
    so no extra computations and compositions for a regular text.
    
    This solution preserves `maxIntrinsicWidth`.
    It's done to keep the right proportions
    during resize in some container composables, i.e., tables.
    
    Notable change: FlowRow isn't used anymore
    if a paragraph or a heading contains only of images.
    This was done for several reasons, the main being
    to not reserve vertical space if images fail to load.
    However, the better alternative I think would be
    to render this image as a hyperlink (like GH does).
    
    Possible alternatives:
    - Split a text with images into different composables
      and arrange them in a `FlowRow`. While it does the job,
      it breaks behavior based on text continuity.
      For example, the text is copied with line breaks.
      Visibly, if an image fits the available width
      and there's place for a portion of text on the right,
      this text will nevertheless appear below the text
      (as it's a different composable).
    - Wrap an image in a `BoxWithConstraints`.
      It would limit Markdown extensions rendering a lot,
      as it prohibits possible containers to measure themselves
      by using children's intrinsics.
      Note that `FlowRow` doesn't cause such a problem
      because it defines custom intrinsic measurement
      (see `FlowMeasurePolicy`).
    - Custom `SubcomposeLayout` with a custom intrinsic measurement
      instead of a `FlowRow`. `FlowRow` downscales an image properly,
      but it keeps the reserved vertical space,
      so other content is rendered far enough
      as if the image were unscaled.
      While theoretically possible, that would be too challenging
      to implement, and maybe not immensely required for our case --
      images right in-between text shouldn't be a common practice.
      Plus, it's inferior to the current solution in a sense that
      we only have one composable, in fact; breaking it into parts
      causes potential visibility problems and accessibility issues,
      as described above.

--- platform/jewel/markdown/extensions/images/src/test/kotlin/org/jetbrains/jewel/markdown/extensions/images/Coil3ImageRendererExtensionImplTest.kt
+++ platform/jewel/markdown/extensions/images/src/test/kotlin/org/jetbrains/jewel/markdown/extensions/images/Coil3ImageRendererExtensionImplTest.kt
@@ -1,8 +1,11 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.jewel.markdown.extensions.images
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.text.appendInlineContent
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.test.assertHeightIsAtLeast
@@ -25,16 +28,24 @@ import coil3.test.FakeImageLoaderEngine
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.StandardTestDispatcher
+import org.jetbrains.jewel.foundation.ExperimentalJewelApi
+import org.jetbrains.jewel.foundation.theme.JewelTheme
 import org.jetbrains.jewel.markdown.InlineMarkdown
+import org.jetbrains.jewel.markdown.MarkdownBlock.Paragraph
+import org.jetbrains.jewel.markdown.createMarkdownStyling
+import org.jetbrains.jewel.markdown.createThemeDefinition
+import org.jetbrains.jewel.markdown.rendering.DefaultInlineMarkdownRenderer
+import org.jetbrains.jewel.markdown.rendering.DefaultMarkdownBlockRenderer
 import org.junit.Rule
 import org.junit.Test
 
-@OptIn(ExperimentalCoilApi::class)
+@OptIn(ExperimentalCoilApi::class, ExperimentalJewelApi::class)
 public class Coil3ImageRendererExtensionImplTest {
     @get:Rule public val composeTestRule: ComposeContentTestRule = createComposeRule()
 
     private val platformContext: PlatformContext = PlatformContext.INSTANCE
     private val imageUrl = "https://example.com/image.png"
+    private val imageUrl2 = "https://example.com/image2.png"
 
     @Test
     public fun `image renders with correct size on success`() {
@@ -123,6 +134,175 @@ public class Coil3ImageRendererExtensionImplTest {
             .assertHeightIsAtLeast(fakeImageHeight.dp)
     }
 
+    @Test
+    public fun `single image wider than container is downscaled to fit available width`() {
+        val fakeImageWidth = 300
+        val fakeImageHeight = 200
+        val containerWidth = 100
+        val fakeImage = ColorImage(Color.Blue.toArgb(), width = fakeImageWidth, height = fakeImageHeight)
+
+        val engine = FakeImageLoaderEngine.Builder().intercept({ it == imageUrl }, fakeImage).build()
+        val imageLoader = ImageLoader.Builder(platformContext).components { add(engine) }.build()
+
+        val paragraph = Paragraph(InlineMarkdown.Image(source = imageUrl, alt = "Alt text", title = "HUGE image"))
+
+        setConstrainedContentWithParagraph(imageLoader, paragraph, containerWidth)
+
+        // The image should be scaled down to fit the container width
+        // Scale factor = 100 / 300 = 0.333...
+        // Expected height = 200 * 0.333... = 66.67
+        val expectedHeight = (fakeImageHeight * containerWidth / fakeImageWidth)
+        composeTestRule
+            .onNodeWithContentDescription("HUGE image")
+            .assertExists()
+            .assertWidthIsEqualTo(containerWidth.dp)
+            .assertHeightIsEqualTo(expectedHeight.dp)
+    }
+
+    @Test
+    public fun `single image narrower than container is not upscaled`() {
+        val fakeImageWidth = 50
+        val fakeImageHeight = 30
+        val containerWidth = 200
+        val fakeImage = ColorImage(Color.Green.toArgb(), width = fakeImageWidth, height = fakeImageHeight)
+
+        val engine = FakeImageLoaderEngine.Builder().intercept({ it == imageUrl }, fakeImage).build()
+        val imageLoader = ImageLoader.Builder(platformContext).components { add(engine) }.build()
+
+        val paragraph = Paragraph(InlineMarkdown.Image(source = imageUrl, alt = "Alt text", title = "smol image"))
+
+        setConstrainedContentWithParagraph(imageLoader, paragraph, containerWidth)
+
+        // The image should remain at its original size, not upscaled
+        composeTestRule
+            .onNodeWithContentDescription("smol image")
+            .assertExists()
+            .assertWidthIsEqualTo(fakeImageWidth.dp)
+            .assertHeightIsEqualTo(fakeImageHeight.dp)
+    }
+
+    @Test
+    public fun `text with leading image is downscaled when container is narrow`() {
+        val fakeImageWidth = 200
+        val fakeImageHeight = 100
+        val containerWidth = 100
+        val fakeImage = ColorImage(Color.Red.toArgb(), width = fakeImageWidth, height = fakeImageHeight)
+
+        val engine = FakeImageLoaderEngine.Builder().intercept({ it == imageUrl }, fakeImage).build()
+        val imageLoader = ImageLoader.Builder(platformContext).components { add(engine) }.build()
+
+        val paragraph =
+            Paragraph(
+                InlineMarkdown.Image(source = imageUrl, alt = "Alt text", title = "HUGE image"),
+                InlineMarkdown.Text(" and then some!"),
+            )
+
+        setConstrainedContentWithParagraph(imageLoader, paragraph, containerWidth)
+
+        val expectedHeight = (fakeImageHeight * containerWidth / fakeImageWidth)
+        composeTestRule
+            .onNodeWithContentDescription("HUGE image")
+            .assertExists()
+            .assertWidthIsEqualTo(containerWidth.dp)
+            .assertHeightIsEqualTo(expectedHeight.dp)
+    }
+
+    @Test
+    public fun `text with trailing image is downscaled when container is narrow`() {
+        val fakeImageWidth = 200
+        val fakeImageHeight = 100
+        val containerWidth = 100
+        val fakeImage = ColorImage(Color.Blue.toArgb(), width = fakeImageWidth, height = fakeImageHeight)
+
+        val engine = FakeImageLoaderEngine.Builder().intercept({ it == imageUrl }, fakeImage).build()
+        val imageLoader = ImageLoader.Builder(platformContext).components { add(engine) }.build()
+
+        val paragraph =
+            Paragraph(
+                InlineMarkdown.Text("Behold the "),
+                InlineMarkdown.Image(source = imageUrl, alt = "Alt text", title = "HUGE image"),
+            )
+
+        setConstrainedContentWithParagraph(imageLoader, paragraph, containerWidth)
+
+        val expectedHeight = (fakeImageHeight * containerWidth / fakeImageWidth)
+        composeTestRule
+            .onNodeWithContentDescription("HUGE image")
+            .assertExists()
+            .assertWidthIsEqualTo(containerWidth.dp)
+            .assertHeightIsEqualTo(expectedHeight.dp)
+    }
+
+    @Test
+    public fun `image between text is downscaled when container is narrow`() {
+        val fakeImageWidth = 200
+        val fakeImageHeight = 100
+        val containerWidth = 100
+        val fakeImage = ColorImage(Color.Green.toArgb(), width = fakeImageWidth, height = fakeImageHeight)
+
+        val engine = FakeImageLoaderEngine.Builder().intercept({ it == imageUrl }, fakeImage).build()
+        val imageLoader = ImageLoader.Builder(platformContext).components { add(engine) }.build()
+
+        val paragraph =
+            Paragraph(
+                InlineMarkdown.Text("Behold, the "),
+                InlineMarkdown.Image(source = imageUrl, alt = "Alt text", title = "HUGE image"),
+                InlineMarkdown.Text("!!!"),
+            )
+
+        setConstrainedContentWithParagraph(imageLoader, paragraph, containerWidth)
+
+        val expectedHeight = (fakeImageHeight * containerWidth / fakeImageWidth)
+        composeTestRule
+            .onNodeWithContentDescription("HUGE image")
+            .assertExists()
+            .assertWidthIsEqualTo(containerWidth.dp)
+            .assertHeightIsEqualTo(expectedHeight.dp)
+    }
+
+    @Test
+    public fun `two images in paragraph are both downscaled when container is narrow`() {
+        val fakeImage1Width = 200
+        val fakeImage1Height = 100
+        val fakeImage2Width = 300
+        val fakeImage2Height = 150
+        val containerWidth = 100
+
+        val fakeImage1 = ColorImage(Color.Red.toArgb(), width = fakeImage1Width, height = fakeImage1Height)
+        val fakeImage2 = ColorImage(Color.Blue.toArgb(), width = fakeImage2Width, height = fakeImage2Height)
+
+        val engine =
+            FakeImageLoaderEngine.Builder()
+                .intercept({ it == imageUrl }, fakeImage1)
+                .intercept({ it == imageUrl2 }, fakeImage2)
+                .build()
+        val imageLoader = ImageLoader.Builder(platformContext).components { add(engine) }.build()
+
+        val paragraph =
+            Paragraph(
+                InlineMarkdown.Image(source = imageUrl, alt = "Alt text 1", title = "First image"),
+                InlineMarkdown.Text(" text between "),
+                InlineMarkdown.Image(source = imageUrl2, alt = "Alt text 2", title = "Second image"),
+            )
+
+        setConstrainedContentWithParagraph(imageLoader, paragraph, containerWidth)
+
+        val expectedHeight1 = (fakeImage1Height * containerWidth / fakeImage1Width)
+        val expectedHeight2 = (fakeImage2Height * containerWidth / fakeImage2Width)
+
+        composeTestRule
+            .onNodeWithContentDescription("First image")
+            .assertExists()
+            .assertWidthIsEqualTo(containerWidth.dp)
+            .assertHeightIsEqualTo(expectedHeight1.dp)
+
+        composeTestRule
+            .onNodeWithContentDescription("Second image")
+            .assertExists()
+            .assertWidthIsEqualTo(containerWidth.dp)
+            .assertHeightIsEqualTo(expectedHeight2.dp)
+    }
+
     private fun setContent(extension: Coil3ImageRendererExtensionImpl, image: InlineMarkdown.Image) {
         composeTestRule.setContent {
             val inlineContent = buildMap { extension.renderImageContent(image)?.let { put("inlineTextContent", it) } }
@@ -131,6 +311,34 @@ public class Coil3ImageRendererExtensionImplTest {
                 appendInlineContent("inlineTextContent", "[rendered image]")
             }
             BasicText(text = annotatedString, inlineContent = inlineContent)
+        }
+    }
+
+    private fun setConstrainedContentWithParagraph(
+        imageLoader: ImageLoader,
+        paragraph: Paragraph,
+        containerWidthDp: Int,
+    ) {
+        composeTestRule.setContent {
+            JewelTheme(createThemeDefinition()) {
+                val imageExtension = Coil3ImageRendererExtension(imageLoader)
+                val markdownStyling = createMarkdownStyling()
+                val blockRenderer =
+                    DefaultMarkdownBlockRenderer(
+                        rootStyling = markdownStyling,
+                        rendererExtensions = listOf(imageExtension),
+                        inlineRenderer = DefaultInlineMarkdownRenderer(listOf(imageExtension)),
+                    )
+                Box(modifier = Modifier.width(containerWidthDp.dp)) {
+                    blockRenderer.RenderParagraph(
+                        block = paragraph,
+                        styling = markdownStyling.paragraph,
+                        enabled = true,
+                        onUrlClick = {},
+                        modifier = Modifier,
+                    )
+                }
+            }
         }
     }
 }
