0ff6562a7f4294ee95f254655da63c68ad5a558c
Aleksey Pivovarov <aleksey.pivovarov@jetbrains.com>
2026-01-16 14:23:37 +0100

    [rdct] IJPL-229363 IJPL-192442 do not throw exception on invalid client session during Undo
    Space-RevId: 3e804a73b87d3039be4b4e481104bad117f3d38d
    
    (cherry picked from commit 90a2cc69a693877d5f13d8522ff972b6be2034cb)
    
    
    (cherry picked from commit e375cd043b838743b01ddf08968a0ae4f8f8056d)
    
    IJ-MR-189486
    
    GitOrigin-RevId: 381d9fa871d8568762a80a9513e5af95235447d5

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/ClientCurrentEditorProvider.kt
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/ClientCurrentEditorProvider.kt
@@ -1,7 +1,10 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.fileEditor.impl
 
-import com.intellij.openapi.client.currentSession
+import com.intellij.codeWithMe.ClientId
+import com.intellij.openapi.client.ClientAppSession
+import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.editor.ClientEditorManager
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorManager
@@ -9,20 +12,24 @@ import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
 import com.intellij.openapi.project.Project
 import com.intellij.util.ui.UIUtil
 
-private class ClientCurrentEditorProvider : CurrentEditorProvider {
+internal class ClientCurrentEditorProvider(val session: ClientAppSession) : CurrentEditorProvider {
   override fun getCurrentEditor(project: Project?): FileEditor? {
+    if (session.isDisposed) {
+      logger<ClientCurrentEditorProvider>().warn("The client (${ClientId.current}) session has expired.", Throwable())
+      return null
+    }
+
     if (project == null) {
       // fallback to search by focus
-      return ClientEditorManager.getCurrentInstance().editorsSequence()
+      return session.service<ClientEditorManager>().editorsSequence()
         .firstOrNull { UIUtil.hasFocus(it.contentComponent) }
         ?.let { TextEditorProvider.getInstance().getTextEditor(it) }
     }
     else {
-      if (project.currentSession.isController) {
-        // doesn't work in case of other remote clients.
+      if (session.isController) {
+        // GTW-6595 doesn't work in case of other remote clients.
         // check com.jetbrains.rdct.cwm.distributed.undo.EditorComplicatedUndoTest.testRedoBasicCommandAfterForeignTyping
         FocusBasedCurrentEditorProvider.getCurrentEditorEx()?.let { editor ->
-          // GTW-6595
           return editor
         }
       }
