afdf83f3a179c89ee58d458fa0739e29f4a1bf47
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2026-01-16 16:49:17 +0100

    [devkit] IJPL-229756 Properly enlarge use scope for @Remote usages of Kotlin classes
    GitOrigin-RevId: 81c9fc6a21f5ad9365e3386042076b18981ce043

--- plugins/devkit/intellij.kotlin.devkit/src/driver/RemoteUseScopeEnlarger.kt
+++ plugins/devkit/intellij.kotlin.devkit/src/driver/RemoteUseScopeEnlarger.kt
@@ -4,30 +4,33 @@ package org.jetbrains.idea.devkit.kotlin.driver
 import com.intellij.java.library.JavaLibraryUtil
 import com.intellij.lang.jvm.JvmModifier
 import com.intellij.psi.JavaPsiFacade
-import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiMethod
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.GlobalSearchScope.allScope
 import com.intellij.psi.search.SearchScope
 import com.intellij.psi.search.UseScopeEnlarger
+import org.jetbrains.uast.UClass
+import org.jetbrains.uast.UMethod
+import org.jetbrains.uast.toUElementOfType
 
 internal const val REMOTE_ANNOTATION_FQN: String = "com.intellij.driver.client.Remote"
 
 internal class RemoteUseScopeEnlarger : UseScopeEnlarger() {
   override fun getAdditionalUseScope(element: PsiElement): SearchScope? {
-    if (element !is PsiClass && element !is PsiMethod) return null
+    val project = element.project
+    if (!JavaLibraryUtil.hasLibraryClass(project, REMOTE_ANNOTATION_FQN)) return null
 
-    if (element is PsiMethod
-        && (element.hasModifier(JvmModifier.PRIVATE)
-            || element.hasModifier(JvmModifier.PACKAGE_LOCAL)
-            || element.hasModifier(JvmModifier.PROTECTED))
+    val uMethod = element.toUElementOfType<UMethod>()
+    if (uMethod != null
+        && (uMethod.javaPsi.hasModifier(JvmModifier.PRIVATE)
+            || uMethod.javaPsi.hasModifier(JvmModifier.PACKAGE_LOCAL)
+            || uMethod.javaPsi.hasModifier(JvmModifier.PROTECTED))
     ) {
       return null
     }
 
-    val project = element.project
-    if (!JavaLibraryUtil.hasLibraryClass(project, REMOTE_ANNOTATION_FQN)) return null
+    val uClass = element.toUElementOfType<UClass>()
+    if (uClass == null && uMethod == null) return null
 
     val remoteClass = JavaPsiFacade.getInstance(project)
                         .findClass(REMOTE_ANNOTATION_FQN, allScope(project)) ?: return null
