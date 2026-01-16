f8a12c66cc97d715cc07cf8a88d8df64013c9403
Kajus Kuncaitis <kajus.kuncaitis@jetbrains.com>
2026-01-16 14:39:02 +0100

    [gradle][IDEA-384297] Add unit tests to check for unresolved dependency due to JVM version incompatibility issues
    Space-RevId: 1f5f29f6f48822292652cbddb4b681565c8bcc62
    (cherry picked from commit 52abd11128b30595662abe2fa6b75c070d69912a)
    
    
    (cherry picked from commit 7bb5791ec984608fd868fbbe1f6123f6a360c122)
    
    IJ-MR-192511
    
    GitOrigin-RevId: 68eca41ba4750c8b726b6017599f897f373c929e

--- plugins/gradle/testSources/org/jetbrains/plugins/gradle/issue/UnresolvedDependencyIssueTest.kt
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/issue/UnresolvedDependencyIssueTest.kt
@@ -3,6 +3,8 @@ package org.jetbrains.plugins.gradle.issue
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class UnresolvedDependencyIssueTest {
   @Test
@@ -107,6 +109,70 @@ internal class UnresolvedDependencyIssueTest {
         
         Possible solution:
          - Declare repository providing the artifact, see the documentation at https://docs.gradle.org/current/userguide/declaring_repositories.html
+        
+      """.trimIndent(),
+      syncIssue.description
+    )
+  }
+
+  @ParameterizedTest
+  @CsvSource(
+    "8, 17",
+    "11, 21",
+    "17, 21"
+  )
+  fun `test unresolved dependency due to JVM version incompatibility during sync has valid quick-fix`(
+    currentJvmVersion: Int,
+    expectedJvmVersion: Int,
+  ) {
+    val syncIssue = UnresolvedDependencySyncIssue(
+      dependencyName = "org.junit.jupiter:junit-jupiter:6.0.0",
+      failureMessage = "Dependency resolution is looking for a library compatible with JVM runtime version $currentJvmVersion, but " +
+                       "'org.junit.jupiter:junit-jupiter:6.0.0' is only compatible with JVM runtime version $expectedJvmVersion or newer.",
+      projectPath = "irrelevant",
+      isOfflineMode = false
+    )
+    val actualQuickFixes = syncIssue.quickFixes
+    assertEquals(1, actualQuickFixes.size) { "Should contain just the upgrade Gradle JVM version quick-fix" }
+    val upgradeQuickFixActualId = actualQuickFixes[0].id
+    assertEquals(
+      """
+        Dependency resolution is looking for a library compatible with JVM runtime version $currentJvmVersion, but 'org.junit.jupiter:junit-jupiter:6.0.0' is only compatible with JVM runtime version $expectedJvmVersion or newer.
+  
+        Possible solution:
+         - Use Java $expectedJvmVersion or newer as Gradle JVM: <a href="$upgradeQuickFixActualId">Open Gradle settings</a>
+        
+      """.trimIndent(),
+      syncIssue.description
+    )
+  }
+
+  @ParameterizedTest
+  @CsvSource(
+    "8, 17",
+    "11, 21",
+    "17, 21"
+  )
+  fun `test unresolved dependency due to JVM version incompatibility during sync in offline mode has valid quick-fix`(
+    currentJvmVersion: Int,
+    expectedJvmVersion: Int,
+  ) {
+    val syncIssue = UnresolvedDependencySyncIssue(
+      dependencyName = "org.junit.jupiter:junit-jupiter:6.0.0",
+      failureMessage = "Dependency resolution is looking for a library compatible with JVM runtime version $currentJvmVersion, but " +
+                       "'org.junit.jupiter:junit-jupiter:6.0.0' is only compatible with JVM runtime version $expectedJvmVersion or newer.",
+      projectPath = "irrelevant",
+      isOfflineMode = true
+    )
+    val actualQuickFixes = syncIssue.quickFixes
+    assertEquals(1, actualQuickFixes.size) { "Should contain just the upgrade Gradle JVM version quick-fix" }
+    val upgradeQuickFixActualId = actualQuickFixes[0].id
+    assertEquals(
+      """
+        Dependency resolution is looking for a library compatible with JVM runtime version $currentJvmVersion, but 'org.junit.jupiter:junit-jupiter:6.0.0' is only compatible with JVM runtime version $expectedJvmVersion or newer.
+  
+        Possible solution:
+         - Use Java $expectedJvmVersion or newer as Gradle JVM: <a href="$upgradeQuickFixActualId">Open Gradle settings</a>
         
       """.trimIndent(),
       syncIssue.description
