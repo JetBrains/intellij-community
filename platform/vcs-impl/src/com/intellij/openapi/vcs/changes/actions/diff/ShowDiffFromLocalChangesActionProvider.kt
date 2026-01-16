a84a84e98fc982ad9168fdd988874ecc87fa85ce
Ivan Semenov <ivan.semenov@jetbrains.com>
2026-01-16 13:33:09 +0100

    refactor [vcs]: convert diff action provider to kotlin
    Space-RevId: d925e0955f3cd87b6bbc8d9ccdc9f574ae6d4f40
    
    GitOrigin-RevId: 20569ea10b5e097c1eb38041f43a08c7f944bbf9

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/diff/ShowDiffFromLocalChangesActionProvider.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/diff/ShowDiffFromLocalChangesActionProvider.kt
@@ -1,203 +1,202 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.vcs.changes.actions.diff;
+package com.intellij.openapi.vcs.changes.actions.diff
 
-import com.intellij.diff.DiffDialogHints;
-import com.intellij.diff.DiffManager;
-import com.intellij.diff.chains.DiffRequestChain;
-import com.intellij.diff.chains.DiffRequestProducerException;
-import com.intellij.diff.util.DiffUserDataKeysEx;
-import com.intellij.openapi.ListSelection;
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.FilePath;
-import com.intellij.openapi.vcs.VcsDataKeys;
-import com.intellij.openapi.vcs.changes.*;
-import com.intellij.openapi.vcs.changes.ui.ChangeDiffRequestChain;
-import com.intellij.openapi.vcs.changes.ui.ChangeDiffRequestChain.Producer;
-import com.intellij.openapi.vcs.changes.ui.ChangesListView;
-import com.intellij.ui.ExperimentalUI;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.containers.JBIterable;
-import com.intellij.vcsUtil.VcsImplUtil;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
-import static com.intellij.openapi.vcs.changes.actions.diff.lst.LocalChangeListDiffTool.ALLOW_EXCLUDE_FROM_COMMIT;
+import com.intellij.diff.DiffDialogHints
+import com.intellij.diff.DiffManager
+import com.intellij.diff.chains.DiffRequestChain
+import com.intellij.diff.chains.DiffRequestProducerException
+import com.intellij.diff.util.DiffUserDataKeysEx
+import com.intellij.openapi.ListSelection
+import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.FilePath
+import com.intellij.openapi.vcs.VcsDataKeys
+import com.intellij.openapi.vcs.changes.*
+import com.intellij.openapi.vcs.changes.ChangesViewWorkflowManager.Companion.getInstance
+import com.intellij.openapi.vcs.changes.actions.diff.lst.LocalChangeListDiffTool
+import com.intellij.openapi.vcs.changes.ui.ChangeDiffRequestChain
+import com.intellij.openapi.vcs.changes.ui.ChangesListView
+import com.intellij.ui.ExperimentalUI.Companion.isNewUI
+import com.intellij.util.containers.ContainerUtil
+import com.intellij.util.containers.JBIterable
+import com.intellij.vcsUtil.VcsImplUtil
+import org.jetbrains.annotations.ApiStatus
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ExecutionException
 
 @ApiStatus.Internal
-public class ShowDiffFromLocalChangesActionProvider implements AnActionExtensionProvider {
-  @Override
-  public @NotNull ActionUpdateThread getActionUpdateThread() {
-    return ActionUpdateThread.BGT;
+class ShowDiffFromLocalChangesActionProvider : AnActionExtensionProvider {
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+  override fun isActive(e: AnActionEvent): Boolean {
+    return e.getData(ChangesListView.DATA_KEY) != null
   }
 
-  @Override
-  public boolean isActive(@NotNull AnActionEvent e) {
-    return e.getData(ChangesListView.DATA_KEY) != null;
+  override fun update(e: AnActionEvent) {
+    updateAvailability(e)
   }
 
-  @Override
-  public void update(@NotNull AnActionEvent e) {
-    updateAvailability(e);
-  }
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.getData(CommonDataKeys.PROJECT) ?: return
+    val view = e.getData(ChangesListView.DATA_KEY) ?: return
+    if (ChangeListManager.getInstance(project).isFreezedWithNotification(null)) return
 
-  public static void updateAvailability(@NotNull AnActionEvent e) {
-    Project project = e.getData(CommonDataKeys.PROJECT);
-    Presentation presentation = e.getPresentation();
-    String place = e.getPlace();
+    val changes = view.selectedChanges.toList()
+    val unversioned = view.selectedUnversionedFiles.toList()
 
-    if (e.getData(ChangesListView.DATA_KEY) == null) {
-      presentation.setEnabled(false);
-      return;
-    }
+    val needsConversion = checkIfThereAreFakeRevisions(project, changes)
 
-    JBIterable<Change> changes = JBIterable.of(e.getData(VcsDataKeys.CHANGES));
-    JBIterable<FilePath> unversionedFiles = JBIterable.from(e.getData(ChangesListView.UNVERSIONED_FILE_PATHS_DATA_KEY));
-
-    if (ActionPlaces.MAIN_MENU.equals(place)) {
-      presentation.setEnabled(project != null && (changes.isNotEmpty() || unversionedFiles.isNotEmpty()));
-    }
-    else {
-      presentation.setEnabled(project != null && canShowDiff(project, changes, unversionedFiles));
-    }
-
-    if (ActionPlaces.CHANGES_VIEW_TOOLBAR.equals(place)) {
-      presentation.setVisible(!ExperimentalUI.isNewUI());
-    }
-  }
-
-  private static boolean canShowDiff(@Nullable Project project,
-                                     @NotNull JBIterable<? extends Change> changes,
-                                     @NotNull JBIterable<? extends FilePath> paths) {
-    return paths.isNotEmpty() ||
-           changes.filter(it -> ChangeDiffRequestProducer.canCreate(project, it)).isNotEmpty();
-  }
-
-  @Override
-  public void actionPerformed(final @NotNull AnActionEvent e) {
-    Project project = e.getData(CommonDataKeys.PROJECT);
-    if (project == null) return;
-    ChangesListView view = e.getData(ChangesListView.DATA_KEY);
-    if (view == null) return;
-    if (ChangeListManager.getInstance(project).isFreezedWithNotification(null)) return;
-
-    List<Change> changes = view.getSelectedChanges().toList();
-    List<FilePath> unversioned = view.getSelectedUnversionedFiles().toList();
-
-    final boolean needsConversion = checkIfThereAreFakeRevisions(project, changes);
-
-    DiffRequestChain chain;
-    if (needsConversion) {
-      CompletableFuture<ListSelection<Producer>> resultRef = new CompletableFuture<>();
+    val chain = if (needsConversion) {
+      val resultRef = CompletableFuture<ListSelection<ChangeDiffRequestChain.Producer?>>()
       // this trick is essential since we are under some conditions to refresh changes;
       // but we can only rely on callback after refresh
-      ChangeListManager.getInstance(project).invokeAfterUpdate(true, () -> {
-        ChangesViewManager.getInstanceEx(project).scheduleRefresh(() -> {
+      ChangeListManager.getInstance(project).invokeAfterUpdate(true, Runnable {
+        ChangesViewManager.getInstanceEx(project).scheduleRefresh(Runnable {
           try {
-            List<? extends Change> actualChanges = loadFakeRevisions(project, changes);
-            resultRef.complete(collectRequestProducers(project, actualChanges, unversioned, view));
+            val actualChanges = loadFakeRevisions(project, changes)
+            resultRef.complete(collectRequestProducers(project, actualChanges, unversioned, view))
           }
-          catch (Throwable err) {
-            resultRef.completeExceptionally(err);
+          catch (err: Throwable) {
+            resultRef.completeExceptionally(err)
           }
-        });
-      });
+        })
+      })
 
-      chain = new ChangeDiffRequestChain.Async() {
-        @Override
-        protected @NotNull ListSelection<? extends Producer> loadRequestProducers() throws DiffRequestProducerException {
+      object : ChangeDiffRequestChain.Async() {
+        @Throws(DiffRequestProducerException::class)
+        override fun loadRequestProducers(): ListSelection<out ChangeDiffRequestChain.Producer?> {
           try {
-            return resultRef.get();
+            return resultRef.get()
           }
-          catch (InterruptedException | ExecutionException ex) {
-            throw new DiffRequestProducerException(ex);
+          catch (ex: InterruptedException) {
+            throw DiffRequestProducerException(ex)
+          }
+          catch (ex: ExecutionException) {
+            throw DiffRequestProducerException(ex)
           }
         }
-      };
+      }
     }
     else {
-      ListSelection<Producer> producers = collectRequestProducers(project, changes, unversioned, view);
-      if (producers.isEmpty()) return;
-      chain = new ChangeDiffRequestChain(producers);
+      val producers = collectRequestProducers(project, changes, unversioned, view)
+      if (producers.isEmpty) return
+      ChangeDiffRequestChain(producers)
     }
 
-    chain.putUserData(DiffUserDataKeysEx.LAST_REVISION_WITH_LOCAL, true);
-    setAllowExcludeFromCommit(project, chain);
-    DiffManager.getInstance().showDiff(project, chain, DiffDialogHints.DEFAULT);
+    chain.putUserData(DiffUserDataKeysEx.LAST_REVISION_WITH_LOCAL, true)
+    setAllowExcludeFromCommit(project, chain)
+    DiffManager.getInstance().showDiff(project, chain, DiffDialogHints.DEFAULT)
   }
 
-  private static boolean checkIfThereAreFakeRevisions(@NotNull Project project, @NotNull List<? extends Change> changes) {
-    boolean needsConversion = false;
-    for (Change change : changes) {
-      final ContentRevision beforeRevision = change.getBeforeRevision();
-      final ContentRevision afterRevision = change.getAfterRevision();
-      if (beforeRevision instanceof FakeRevision) {
-        VcsDirtyScopeManager.getInstance(project).fileDirty(beforeRevision.getFile());
-        needsConversion = true;
-      }
-      if (afterRevision instanceof FakeRevision) {
-        VcsDirtyScopeManager.getInstance(project).fileDirty(afterRevision.getFile());
-        needsConversion = true;
-      }
-    }
-    return needsConversion;
-  }
+  companion object {
+    fun updateAvailability(e: AnActionEvent) {
+      val project = e.getData(CommonDataKeys.PROJECT)
+      val presentation = e.presentation
+      val place = e.place
 
-  private static @NotNull List<? extends Change> loadFakeRevisions(@NotNull Project project, @NotNull List<? extends Change> changes) {
-    Collection<Change> allChanges = ChangeListManager.getInstance(project).getAllChanges();
-    return VcsImplUtil.filterChangesUnder(allChanges, ChangesUtil.getPaths(changes)).toList();
-  }
-
-  public static @NotNull ListSelection<Producer> collectRequestProducers(@NotNull Project project,
-                                                                         @NotNull List<? extends Change> changes,
-                                                                         @NotNull List<? extends FilePath> unversioned,
-                                                                         @NotNull ChangesListView changesView) {
-    if (changes.size() == 1 && unversioned.isEmpty()) { // show all changes from this changelist
-      Change selectedChange = changes.get(0);
-      List<Change> selectedChanges = changesView.getAllChangesFromSameChangelist(selectedChange);
-      if (selectedChanges == null) {
-        selectedChanges = changesView.getAllChangesFromSameAmendNode(selectedChange);
+      if (e.getData(ChangesListView.DATA_KEY) == null) {
+        presentation.setEnabled(false)
+        return
       }
-      if (selectedChanges != null) {
-        int selectedIndex = ContainerUtil.indexOf(selectedChanges, it -> ChangeListChange.HASHING_STRATEGY.equals(selectedChange, it));
-        if (selectedIndex == -1) selectedIndex = selectedChanges.indexOf(selectedChange);
-        return createChangeProducers(project, selectedChanges, selectedIndex);
+
+      val changes = JBIterable.of(*e.getData(VcsDataKeys.CHANGES))
+      val unversionedFiles = JBIterable.from(e.getData(ChangesListView.UNVERSIONED_FILE_PATHS_DATA_KEY))
+
+      if (ActionPlaces.MAIN_MENU == place) {
+        presentation.setEnabled(project != null && (changes.isNotEmpty || unversionedFiles.isNotEmpty))
+      }
+      else {
+        presentation.setEnabled(project != null && canShowDiff(project, changes, unversionedFiles))
+      }
+
+      if (ActionPlaces.CHANGES_VIEW_TOOLBAR == place) {
+        presentation.setVisible(!isNewUI())
       }
     }
 
-    if (unversioned.size() == 1 && changes.isEmpty()) { // show all unversioned changes
-      FilePath selectedFile = unversioned.get(0);
-      List<FilePath> allUnversioned = changesView.getUnversionedFiles().toList();
-      int selectedIndex = allUnversioned.indexOf(selectedFile);
-      return createUnversionedProducers(project, allUnversioned, selectedIndex);
+    private fun canShowDiff(project: Project?, changes: JBIterable<Change>, paths: JBIterable<FilePath>): Boolean {
+      return paths.isNotEmpty || changes.filter({ ChangeDiffRequestProducer.canCreate(project, it) }).isNotEmpty
     }
 
-    ListSelection<Producer> changeProducers = createChangeProducers(project, changes, 0);
-    ListSelection<Producer> unversionedProducers = createUnversionedProducers(project, unversioned, 0);
-    return ListSelection.createAt(ContainerUtil.concat(changeProducers.getList(), unversionedProducers.getList()), 0)
-      .asExplicitSelection();
-  }
+    private fun checkIfThereAreFakeRevisions(project: Project, changes: List<Change>): Boolean {
+      var needsConversion = false
+      for (change in changes) {
+        val beforeRevision = change.beforeRevision
+        val afterRevision = change.afterRevision
+        if (beforeRevision is FakeRevision) {
+          VcsDirtyScopeManager.getInstance(project).fileDirty(beforeRevision.file)
+          needsConversion = true
+        }
+        if (afterRevision is FakeRevision) {
+          VcsDirtyScopeManager.getInstance(project).fileDirty(afterRevision.file)
+          needsConversion = true
+        }
+      }
+      return needsConversion
+    }
 
-  private static ListSelection<Producer> createChangeProducers(@NotNull Project project,
-                                                               @NotNull List<? extends Change> changes,
-                                                               int selected) {
-    return ListSelection.createAt(changes, selected).map(change -> ChangeDiffRequestProducer.create(project, change));
-  }
+    private fun loadFakeRevisions(project: Project, changes: List<Change>): List<Change> {
+      val allChanges = ChangeListManager.getInstance(project).getAllChanges()
+      return VcsImplUtil.filterChangesUnder(allChanges, ChangesUtil.getPaths(changes)).toList()
+    }
 
-  private static ListSelection<Producer> createUnversionedProducers(@NotNull Project project,
-                                                                    @NotNull List<? extends FilePath> unversioned,
-                                                                    int selected) {
-    return ListSelection.createAt(unversioned, selected).map(path -> UnversionedDiffRequestProducer.create(project, path));
-  }
+    private fun collectRequestProducers(
+      project: Project,
+      changes: List<Change>,
+      unversioned: List<FilePath>,
+      changesView: ChangesListView,
+    ): ListSelection<ChangeDiffRequestChain.Producer?> {
+      if (changes.size == 1 && unversioned.isEmpty()) { // show all changes from this changelist
+        val selectedChange: Change = changes.first()
+        var selectedChanges = changesView.getAllChangesFromSameChangelist(selectedChange)
+        if (selectedChanges == null) {
+          selectedChanges = changesView.getAllChangesFromSameAmendNode(selectedChange)
+        }
+        if (selectedChanges != null) {
+          var selectedIndex = selectedChanges.indexOfFirst { ChangeListChange.HASHING_STRATEGY.equals(selectedChange, it) }
+          if (selectedIndex == -1) selectedIndex = selectedChanges.indexOf(selectedChange)
+          return createChangeProducers(project, selectedChanges, selectedIndex)
+        }
+      }
 
-  private static void setAllowExcludeFromCommit(@NotNull Project project, @NotNull DiffRequestChain chain) {
-    boolean allowExcludeFromCommit = ChangesViewWorkflowManager.getInstance(project).getAllowExcludeFromCommit().getValue();
-    chain.putUserData(ALLOW_EXCLUDE_FROM_COMMIT, allowExcludeFromCommit);
+      if (unversioned.size == 1 && changes.isEmpty()) { // show all unversioned changes
+        val selectedFile = unversioned.first()
+        val allUnversioned = changesView.unversionedFiles.toList()
+        val selectedIndex = allUnversioned.indexOf(selectedFile)
+        return createUnversionedProducers(project, allUnversioned, selectedIndex)
+      }
+
+      val changeProducers = createChangeProducers(project, changes, 0)
+      val unversionedProducers = createUnversionedProducers(project, unversioned, 0)
+      return ListSelection.createAt(
+        ContainerUtil.concat(changeProducers.getList(), unversionedProducers.getList()),
+        0
+      ).asExplicitSelection()
+    }
+
+    private fun createChangeProducers(
+      project: Project,
+      changes: List<Change>,
+      selected: Int,
+    ): ListSelection<ChangeDiffRequestChain.Producer?> {
+      return ListSelection.createAt(changes, selected).map {
+        ChangeDiffRequestProducer.create(project, it)
+      }
+    }
+
+    private fun createUnversionedProducers(
+      project: Project,
+      unversioned: List<FilePath>,
+      selected: Int,
+    ): ListSelection<ChangeDiffRequestChain.Producer?> {
+      return ListSelection.createAt(unversioned, selected).map {
+        UnversionedDiffRequestProducer.create(project, it)
+      }
+    }
+
+    private fun setAllowExcludeFromCommit(project: Project, chain: DiffRequestChain) {
+      val allowExcludeFromCommit = getInstance(project).allowExcludeFromCommit.value
+      chain.putUserData(LocalChangeListDiffTool.ALLOW_EXCLUDE_FROM_COMMIT, allowExcludeFromCommit)
+    }
   }
 }
