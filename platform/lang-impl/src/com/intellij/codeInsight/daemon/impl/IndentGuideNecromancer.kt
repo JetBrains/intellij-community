4a0bd3085be207919feb6c52736f2a5cb8de06c6
Alexander Trushev <aleksandr.trushev@jetbrains.com>
2026-01-17 01:16:00 +0100

    [editor] IJPL-229958 Implement necromancer for indent guides
    GitOrigin-RevId: f461509dcb7534b3118b88d1a18ffa6f38b14644

---
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/IndentGuideNecromancer.kt
@@ -0,0 +1,106 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.codeInsight.daemon.impl
+
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.editor.IndentGuideDescriptor
+import com.intellij.openapi.editor.ex.EditorSettingsExternalizable
+import com.intellij.openapi.editor.impl.IndentsModelImpl
+import com.intellij.openapi.editor.impl.zombie.*
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.util.io.DataInputOutputUtil.readINT
+import com.intellij.util.io.DataInputOutputUtil.writeINT
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.DataInput
+import java.io.DataOutput
+
+
+typealias IndentGuideLimb = IndentGuideDescriptor
+
+internal class IndentGuideNecromancerAwaker : NecromancerAwaker<IndentGuideZombie> {
+  override fun awake(project: Project, coroutineScope: CoroutineScope): Necromancer<IndentGuideZombie> {
+    return IndentGuideNecromancer(project, coroutineScope)
+  }
+}
+
+private class IndentGuideNecromancer(
+  project: Project,
+  coroutineScope: CoroutineScope,
+) : GravingNecromancer<IndentGuideZombie>(
+  project,
+  coroutineScope,
+  "graved-indent-guides",
+  IndentGuideNecromancy,
+) {
+
+  override fun turnIntoZombie(recipe: TurningRecipe): IndentGuideZombie? {
+    if (isEnabled()) {
+      val indents = (recipe.editor.indentsModel as IndentsModelImpl).indents
+      if (indents.isNotEmpty()) {
+        return IndentGuideZombie(indents)
+      }
+    }
+    return null
+  }
+
+  override suspend fun shouldSpawnZombie(recipe: SpawnRecipe): Boolean {
+    return isEnabled()
+  }
+
+  override suspend fun spawnZombie(recipe: SpawnRecipe, zombie: IndentGuideZombie?) {
+    if (zombie != null && zombie.limbs().isNotEmpty()) {
+      val ranges = IndentsPass.buildRanges(recipe.document, zombie.limbs())
+      val editor = recipe.editorSupplier()
+      withContext(Dispatchers.EDT) {
+        if (recipe.isValid(editor)) {
+          IndentsPass.applyIndents(editor, zombie.limbs(), ranges, true)
+        }
+      }
+    }
+  }
+
+  private fun isEnabled(): Boolean {
+    return isNecromancerEnabled() && isIndentGuideEnabled()
+  }
+
+  private fun isIndentGuideEnabled(): Boolean {
+    return EditorSettingsExternalizable.getInstance().isIndentGuidesShown
+  }
+
+  private fun isNecromancerEnabled(): Boolean {
+    return Registry.`is`("cache.indent.guide.model.on.disk", true)
+  }
+}
+
+internal class IndentGuideZombie(
+  limbs: List<IndentGuideLimb>,
+) : LimbedZombie<IndentGuideLimb>(limbs)
+
+private object IndentGuideNecromancy : LimbedNecromancy<IndentGuideZombie, IndentGuideLimb>(spellLevel=0) {
+
+  override fun buryLimb(grave: DataOutput, limb: IndentGuideLimb) {
+    writeINT(grave, limb.indentLevel)
+    writeINT(grave, limb.codeConstructStartLine)
+    writeINT(grave, limb.startLine)
+    writeINT(grave, limb.endLine)
+  }
+
+  override fun exhumeLimb(grave: DataInput): IndentGuideLimb {
+    val indentLevel           : Int = readINT(grave)
+    val codeConstructStartLine: Int = readINT(grave)
+    val startLine             : Int = readINT(grave)
+    val endLine               : Int = readINT(grave)
+    return IndentGuideDescriptor(
+      indentLevel,
+      codeConstructStartLine,
+      startLine,
+      endLine,
+    )
+  }
+
+  override fun formZombie(limbs: List<IndentGuideLimb>): IndentGuideZombie {
+    return IndentGuideZombie(limbs)
+  }
+}
