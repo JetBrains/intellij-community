af5580882a2d545c7a978b32c0d12a81774d25a0
Alexander Trushev <aleksandr.trushev@jetbrains.com>
2026-01-17 22:29:09 +0100

    cleanup [editor]: `CodeFoldingZombieUtils`
    Space-RevId: a4adf81211a0edb4704d7d7738379de9aa52ef50
    
    GitOrigin-RevId: aa679a24113927faa802576be0c1d0d74221df69

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/zombie/CodeFoldingZombieUtils.kt
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/zombie/CodeFoldingZombieUtils.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.editor.impl.zombie
 
 import com.intellij.openapi.application.EDT
@@ -11,19 +11,16 @@ import com.intellij.openapi.editor.impl.FoldingKeys
 import com.intellij.openapi.util.Key
 import com.intellij.platform.util.coroutines.childScope
 import com.intellij.psi.impl.source.tree.injected.InjectedLanguageEditorUtil
-import fleet.util.logging.logger
 import kotlinx.coroutines.*
-import org.jetbrains.annotations.ApiStatus
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 import kotlin.time.Duration.Companion.seconds
 
+
 object CodeFoldingZombieUtils {
   private val ZOMBIE_RAISED_KEY: Key<Boolean> = Key.create("zombie.raised.in.editor")
   private val ZOMBIE_CLEANUP_CONTEXT_KEY: Key<CoroutineScope> = Key.create("zombie.cleanup.context.in.editor")
-  private val LOG = logger<CodeFoldingZombieUtils>()
-  
-  @ApiStatus.Internal
+
   fun getZombieRegions(editor: Editor, removeFiltered: Boolean, zombieFilter: (FoldRegion) -> Boolean): List<FoldRegion> {
     val foldingModel = editor.foldingModel
     val filteredZombie = foldingModel
@@ -38,10 +35,8 @@ object CodeFoldingZombieUtils {
     return filteredZombie 
   }
 
-  @ApiStatus.Internal
   fun setZombieRaised(editor: Editor, raised: Boolean): Unit =  ZOMBIE_RAISED_KEY.set(editor, raised)
 
-  @ApiStatus.Internal
   fun isZombieRaised(editor: Editor): Boolean = ZOMBIE_RAISED_KEY.get(editor) ?: false
 
   private val waitingForNovaAndRiderBackendInitializationCompleteIfAny = 5.seconds
@@ -59,7 +54,6 @@ object CodeFoldingZombieUtils {
     }
   }
 
-  @ApiStatus.Internal
   fun postponeAndScheduleCleanupZombieRegions(editor: Editor) {
     // There may be a huge amount injected editors, but only the top editor is zombies' owner.
     // Covered by the test: InjectedLanguageEditingTest.testTypingNearBigHeapOfInjectedFragmentsDoesNotCauseTooManyRangeMarkersAllocated 
@@ -91,7 +85,7 @@ object CodeFoldingZombieUtils {
 
   private fun createZombieCleanerScope(editor: Editor): CoroutineScope? {
     ZOMBIE_CLEANUP_CONTEXT_KEY.get(editor)?.cancel()
-    
+
     val project = editor.project
     if (project == null || project.isDisposed) return null
     val editorCoroutineScope = project.service<ZombieCleanupService>().scope.childScope("editorZombieCleaner")
@@ -100,7 +94,7 @@ object CodeFoldingZombieUtils {
     ZOMBIE_CLEANUP_CONTEXT_KEY.set(editor, editorCoroutineScope)
     return editorCoroutineScope
   }
-  
+
   @Service(Service.Level.PROJECT)
   private class ZombieCleanupService(val scope: CoroutineScope) 
 }
