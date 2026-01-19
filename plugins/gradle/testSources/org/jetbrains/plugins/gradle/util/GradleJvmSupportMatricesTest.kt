c2f6e23436ef8a483a61f95b5dad6c0d013571f7
Alexander.Glukhov <alexander.glukhov@jetbrains.com>
2026-01-19 12:35:27 +0100

    [gradle][IDEA-384807] update the Gradle Compatibility Matrix for Gradle 9.3.0
    GitOrigin-RevId: cb9f9ba4fed9ee1c7af7648b623435aad40822ad

--- plugins/gradle/testSources/org/jetbrains/plugins/gradle/util/GradleJvmSupportMatricesTest.kt
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/util/GradleJvmSupportMatricesTest.kt
@@ -10,8 +10,8 @@ class GradleJvmSupportMatricesTest : GradleJvmSupportMatricesTestCase() {
     private const val FUTURE_GRADLE_VERSION = "100.0.0"
     private const val LATEST_SUPPORTED_JAVA_VERSION = 25
     private const val FUTURE_JAVA_VERSION = 100
-    private const val BUNDLED_GRADLE_VERSION = "9.2.0"
-    private const val LATEST_GRADLE_VERSION = "9.2.0"
+    private const val BUNDLED_GRADLE_VERSION = "9.3.0"
+    private const val LATEST_GRADLE_VERSION = "9.3.0"
   }
 
   fun `test bundled gradle version`() {
@@ -213,7 +213,7 @@ class GradleJvmSupportMatricesTest : GradleJvmSupportMatricesTestCase() {
     assertSupportedGradleVersion("8.0") { minOrNull() }
     assertSupportedGradleVersion("8.14") { maxOrNull() }
     assertSupportedGradleVersion("9.0.0") { minOrNull() }
-    assertSupportedGradleVersion("9.2.0") { maxOrNull() }
+    assertSupportedGradleVersion("9.3.0") { maxOrNull() }
 
     assertSupportedGradleVersion(LATEST_GRADLE_VERSION) { maxOrNull() }
   }
