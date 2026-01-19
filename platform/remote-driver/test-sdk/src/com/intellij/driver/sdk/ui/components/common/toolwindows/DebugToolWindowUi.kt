3df2e76654c2628ae10eccfeed21cc4fe4dff038
vfilin <vladyslav.filin.external@jetbrains.com>
2026-01-19 20:14:16 +0200

    Cherry-picked functionality from [AT-3388]
    Space-RevId: 4113321d89ece538f0e625899f0ba1e0b2a3d9a7
    
    GitOrigin-RevId: fd4cedc6563528635b12a479485e3bb71bf58ae3

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/toolwindows/DebugToolWindowUi.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/toolwindows/DebugToolWindowUi.kt
@@ -1,23 +1,69 @@
 package com.intellij.driver.sdk.ui.components.common.toolwindows
 
 import com.intellij.driver.sdk.ui.components.ComponentData
+import com.intellij.driver.sdk.ui.components.UiComponent
 import com.intellij.driver.sdk.ui.components.common.IdeaFrameUI
+import com.intellij.driver.sdk.ui.components.elements.JButtonUiComponent
+import com.intellij.driver.sdk.ui.components.elements.accessibleList
 import com.intellij.driver.sdk.ui.components.elements.button
+import com.intellij.driver.sdk.ui.shouldBe
+import com.intellij.driver.sdk.ui.ui
 
-fun IdeaFrameUI.debugToolWindow(func: DebugToolWindowUi.() -> Unit = {}) =
+fun IdeaFrameUI.debugToolWindow(func: DebugToolWindowUi.() -> Unit = {}): DebugToolWindowUi =
   x(DebugToolWindowUi::class.java) { componentWithChild(byClass("InternalDecoratorImpl"), byAccessibleName("Threads & Variables")) }.apply(func)
 
-class DebugToolWindowUi(data: ComponentData) : ToolWindowUiComponent(data) {
-  val consoleTab get() = x { and(byClass("SimpleColoredComponent"), byAccessibleName("Console")) }
-  val threadsAndVariablesTab get() = x { and(byClass("SimpleColoredComponent"), byAccessibleName("Threads & Variables")) }
-  val debuggerConsoleTab get() = x { and(byClass("SimpleColoredComponent"), byAccessibleName("Debugger Console")) }
-  val memoryView get() = x { and(byClass("SimpleColoredComponent"), byAccessibleName("Memory View")) }
-  val consoleView get() = x { byType("com.intellij.execution.impl.ConsoleViewImpl") }
-  val resumeButton get() = button { byAccessibleName("Resume Program") }
-  val stopButton get() = x("//div[@myicon='stop.svg']")
-  val stepOverButton get() = button { byAccessibleName("Step Over") }
-  val stepOutButton get() = button { byAccessibleName("Step Out") }
-  val stepIntoButton get() = button { byAccessibleName("Step Into") }
-  val jbRunnerTabs get() = x { byType("com.intellij.execution.ui.layout.impl.JBRunnerTabs") }
+fun DebugToolWindowUi.languageChooser(func: LanguageChooser.() -> Unit = {}): LanguageChooser = x(LanguageChooser::class.java) { byClass("LanguageChooser") }.apply(func)
+
+
+open class DebugToolWindowUi(data: ComponentData) : ToolWindowUiComponent(data) {
+
+  protected var defaultTabs: MutableMap<String, UiComponent> = mapOf("Threads & Variables" to threadsAndVariablesTab, "Console" to consoleTab).toMutableMap()
+
+  protected fun defineDebuggerTabByAccessibleName(name: String): UiComponent = x { and(byClass("SimpleColoredComponent"), byAccessibleName(name)) }
+  val consoleTab: UiComponent get() = defineDebuggerTabByAccessibleName("Console")
+  val threadsAndVariablesTab: UiComponent get() = defineDebuggerTabByAccessibleName("Threads & Variables")
+  val debuggerConsoleTab: UiComponent get() = defineDebuggerTabByAccessibleName("Debugger Console")
+  val memoryView: UiComponent get() = defineDebuggerTabByAccessibleName("Memory View")
+  val consoleView: UiComponent get() = x { byType("com.intellij.execution.impl.ConsoleViewImpl") }
+  val resumeButton: JButtonUiComponent get() = button { byAccessibleName("Resume Program") }
+  val stopButton: UiComponent get() = x("//div[@myicon='stop.svg']")
+  val stepOverButton: JButtonUiComponent get() = button { byAccessibleName("Step Over") }
+  val stepOutButton: JButtonUiComponent get() = button { byAccessibleName("Step Out") }
+  val stepIntoButton: JButtonUiComponent get() = button { byAccessibleName("Step Into") }
+  val jbRunnerTabs: UiComponent get() = x { byType("com.intellij.execution.ui.layout.impl.JBRunnerTabs") }
+  val debugTabWindow: UiComponent get() = x { byType("com.intellij.openapi.wm.impl.ToolWindowImpl") }
+
+  fun selectOptionFromEvaluationToolTip(optionToSelect: String, fullMatch: Boolean = false) {
+    driver.ui.accessibleList { byClass("LookupList") }
+      .shouldBe("Evaluation tooltip with available items should be present") { present() }
+      .clickItem(optionToSelect, fullMatch)
+    keyboard {
+      enter()
+      enter()
+    }
+  }
 }
 
+class LanguageChooser(data: ComponentData) : DebugToolWindowUi(data) {
+
+  fun getCurrentValue(): String {
+    return getAllTexts().map { it.text }.first()
+  }
+
+  fun verifyCurrentValue(expectedValue: String): LanguageChooser {
+    shouldBe("Verify current value equals to $expectedValue", { getCurrentValue() == expectedValue })
+    return this
+  }
+
+  fun openLanguagePicker(): LanguageChooser {
+    this.click()
+    return this
+  }
+
+  fun selectLanguage(languageToSelect: String): LanguageChooser {
+    driver.ui.accessibleList { and(byAccessibleName("Choose Language"), byClass("MyList")) }
+      .clickItem(languageToSelect)
+    return this
+  }
+
+}
