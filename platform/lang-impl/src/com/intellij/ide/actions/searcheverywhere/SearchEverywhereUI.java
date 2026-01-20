788f671be0e3bf4beed95adf0ac588414c069c41
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2026-01-20 18:26:34 +0100

    [threading] IJPL-230978: Add write-intent-read action to JsImportPopup
    Space-RevId: a47c45266177e332cc46bc8d2cc27a431aa86fe4
    
    GitOrigin-RevId: 376046ed8b19055a35ccb76c5aa473acc43260d1

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereUI.java
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereUI.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.actions.searcheverywhere;
 
 import com.intellij.accessibility.TextFieldWithListAccessibleContext;
@@ -31,6 +31,7 @@ import com.intellij.openapi.actionSystem.impl.ActionMenu;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.application.WriteIntentReadAction;
 import com.intellij.openapi.application.ex.ApplicationManagerEx;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
@@ -930,7 +931,9 @@ public final class SearchEverywhereUI extends BigPopupUI implements UiDataProvid
 
       @Override
       public void mouseClicked(MouseEvent e) {
-        onMouseClicked(e);
+        WriteIntentReadAction.run(() -> {
+          onMouseClicked(e);
+        });
       }
 
       @Override
