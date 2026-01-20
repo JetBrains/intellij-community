2e3dcd9954e65caac85e7f2fc6e60096d145663b
Tagir Valeev <tagir.valeev@jetbrains.com>
2026-01-20 15:23:38 +0100

    IJPL-214795 [mod-completion] Internal -> Experimental
    Space-RevId: 1428f6720c87f90d10feb6809d0fa5f0affbc6db
    
    GitOrigin-RevId: b58127e0df64f5def60a0e706f901666a80c3850

--- platform/analysis-api/src/com/intellij/modcompletion/package-info.java
+++ platform/analysis-api/src/com/intellij/modcompletion/package-info.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-@Internal
+@ApiStatus.Experimental
 package com.intellij.modcompletion;
 
-import org.jetbrains.annotations.ApiStatus.Internal;
+import org.jetbrains.annotations.ApiStatus;
