051574b9d345559b282ae59e4e5eb5f984592219
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2026-01-20 16:05:33 +0100

    [git] Refactor: Generify GitMergeAction (JPL-224804)
    IJ-MR-188593
    
    Space-RevId: b9dbdd59884a7ee94b21d7d914fb9a85aeec07f4
    
    GitOrigin-RevId: f78ed40a6d12d5e12c1de8f76e49ac11d199257b

--- plugins/git4idea/src/git4idea/actions/GitMerge.java
+++ plugins/git4idea/src/git4idea/actions/GitMerge.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.actions;
 
 import com.intellij.dvcs.repo.Repository;
@@ -6,41 +6,43 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.containers.ContainerUtil;
 import git4idea.GitBranch;
+import git4idea.GitOperationsCollector;
 import git4idea.GitUtil;
 import git4idea.commands.GitCommand;
 import git4idea.commands.GitLineHandler;
 import git4idea.i18n.GitBundle;
 import git4idea.merge.GitMergeDialog;
 import git4idea.merge.GitMergeOption;
+import git4idea.repo.GitRepository;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Supplier;
 
 import static git4idea.GitNotificationIdsHolder.MERGE_FAILED;
 
-final class GitMerge extends GitMergeAction {
+final class GitMerge extends GitMergeAction<GitMergeOption> {
   @Override
   protected @NotNull String getActionName() {
     return GitBundle.message("merge.action.name");
   }
 
   @Override
-  protected @Nullable DialogState displayDialog(@NotNull Project project, @NotNull List<VirtualFile> gitRoots, @NotNull VirtualFile defaultRoot) {
+  protected @Nullable DialogState<GitMergeOption> displayDialog(@NotNull Project project, @NotNull List<VirtualFile> gitRoots, @NotNull VirtualFile defaultRoot) {
     final GitMergeDialog dialog = new GitMergeDialog(project, defaultRoot, gitRoots);
     if (!dialog.showAndGet()) {
       return null;
     }
-    return new DialogState(dialog.getSelectedRoot(),
-                           GitBundle.message("merging.title", dialog.getSelectedRoot().getPath()),
-                           getHandlerProvider(project, dialog),
-                           dialog.getSelectedBranch(),
-                           dialog.shouldCommitAfterMerge(),
-                           ContainerUtil.map(dialog.getSelectedOptions(), option -> option.getOption()));
+    return new DialogState<>(dialog.getSelectedRoot(),
+                             GitBundle.message("merging.title", dialog.getSelectedRoot().getPath()),
+                             getHandlerProvider(project, dialog),
+                             dialog.getSelectedBranch(),
+                             dialog.shouldCommitAfterMerge(),
+                             dialog.getSelectedOptions());
   }
 
   @Override
