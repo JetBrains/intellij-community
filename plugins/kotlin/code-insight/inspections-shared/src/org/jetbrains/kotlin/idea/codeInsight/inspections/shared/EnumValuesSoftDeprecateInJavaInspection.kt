1aa5e9c068219cc343613b2db82d3bd8a53f521e
Anna Kozlova <anna.kozlova@jetbrains.com>
2026-01-21 14:27:28 +0100

    [kotlin] move EnumValuesSoftDeprecateInJavaInspection registered in the shared module to the shared module
    KTIJ-28142
    
    Space-RevId: 3ae1658014c646b0f9cb1cc9d6fbd24ffb27b5db
    
    GitOrigin-RevId: 6bc321e5c0dc25568aa8b0194ff9db3e6b7c6122

--- plugins/kotlin/code-insight/inspections-shared/src/org/jetbrains/kotlin/idea/codeInsight/inspections/shared/EnumValuesSoftDeprecateInJavaInspection.kt
+++ plugins/kotlin/code-insight/inspections-shared/src/org/jetbrains/kotlin/idea/codeInsight/inspections/shared/EnumValuesSoftDeprecateInJavaInspection.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.inspections
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.codeInsight.inspections.shared
 
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
@@ -10,9 +10,8 @@ import com.intellij.psi.JavaElementVisitor
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.search.FileTypeIndex
-import com.intellij.psi.search.GlobalSearchScope.moduleWithDependenciesAndLibrariesScope
+import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.util.CachedValueProvider
-import com.intellij.psi.util.CachedValueProvider.Result
 import com.intellij.psi.util.CachedValuesManager
 import org.jetbrains.kotlin.asJava.elements.KtLightMethod
 import org.jetbrains.kotlin.asJava.isGetEntriesMethod
@@ -59,8 +58,8 @@ internal fun hasKotlinFiles(module: Module): Boolean {
     return CachedValuesManager.getManager(module.project).getCachedValue(module, CachedValueProvider {
         val hasKotlinFiles = FileTypeIndex.containsFileOfType(
             KotlinFileType.INSTANCE,
-            moduleWithDependenciesAndLibrariesScope(module)
+            GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module)
         )
-        Result.create(hasKotlinFiles, OuterModelsModificationTrackerManager.getTracker(module.project))
+        CachedValueProvider.Result.create(hasKotlinFiles, OuterModelsModificationTrackerManager.getTracker(module.project))
     })
 }
