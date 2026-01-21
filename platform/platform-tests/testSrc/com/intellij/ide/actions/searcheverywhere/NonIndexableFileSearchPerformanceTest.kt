8c040d801d1bc49dcf7e0605bc1c285aed41a4ed
Ilia Malakhov <ilia.malakhov@jetbrains.com>
2026-01-21 10:57:45 +0100

    IJPL-230876 [tests] refactor perf test to appear on different charts
    GitOrigin-RevId: d40d1efb901b2904c6868722a897d39cf1aae31b

--- platform/platform-tests/testSrc/com/intellij/ide/actions/searcheverywhere/NonIndexableFileSearchPerformanceTest.kt
+++ platform/platform-tests/testSrc/com/intellij/ide/actions/searcheverywhere/NonIndexableFileSearchPerformanceTest.kt
@@ -23,6 +23,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
 import kotlin.io.path.Path
+import kotlin.reflect.jvm.javaMethod
 
 /**
  * Performance test for [NonIndexableFilesSEContributor].
@@ -54,136 +55,152 @@ open class NonIndexableFileSearchPerformanceTest {
 
   @BeforeEach
   fun createNonIndexableFileset(): Unit = runBlocking {
-      Assumptions.assumeTrue(project.isOpen)
+    Assumptions.assumeTrue(project.isOpen)
 
-      runInEdtAndWait { registerProjectRootBlocking(project, communityPath) }
+    runInEdtAndWait { registerProjectRootBlocking(project, communityPath) }
 
-      Assumptions.assumeTrue(readAction { workspaceFileIndex.isInContent(communityVirtualFile) }) {
-          "project root must be in content"
-      }
-      Assumptions.assumeFalse(readAction { workspaceFileIndex.isIndexable(communityVirtualFile) }) {
-          "project root must be non-indexable"
-      }
+    Assumptions.assumeTrue(readAction { workspaceFileIndex.isInContent(communityVirtualFile) }) {
+      "project root must be in content"
+    }
+    Assumptions.assumeFalse(readAction { workspaceFileIndex.isIndexable(communityVirtualFile) }) {
+      "project root must be non-indexable"
+    }
+  }
+
+  fun `iterate over all files - base`(subtestName: String) {
+    val searchPattern = "ProjectRootEntity"
+    val contributor = createContributor()
+    Benchmark.newBenchmarkWithVariableInputSize("search \"$searchPattern\"", nonIndexableFilesCount) {
+      contributor.search(searchPattern, MockProgressIndicator())
+      nonIndexableFilesCount
+    }.start(NonIndexableFileSearchPerformanceTest::`iterate over all files`.javaMethod!!, subtestName)
   }
 
   @Test
-  fun `iterate over all files`(): Unit = runBlocking {
-      val searchPattern = "ProjectRootEntity"
-      val contributor = createContributor()
-      Benchmark.newBenchmarkWithVariableInputSize("search \"$searchPattern\"", nonIndexableFilesCount) {
-          contributor.search(searchPattern, MockProgressIndicator())
-          nonIndexableFilesCount
-      }.start()
+  fun `iterate over all files`() {
+    `iterate over all files - base`("dfs - blocking read actions")
   }
 
   @Test
   @RegistryKey("intellij.platform.iterate.non.indexable.files.use.cancellable.read.actions", "true")
-  fun `iterate over all files - cancellable read actions`() = `iterate over all files`()
+  fun `iterate over all files - cancellable read actions`() = `iterate over all files - base`("dfs - cancellable read actions")
 
   @Test
   @RegistryKey("se.enable.non.indexable.files.use.bfs", "true")
   @RegistryKey("se.enable.non.indexable.files.use.bfs.blocking.read.actions", "true")
-  fun `iterate over all files - bfs many read actions`() = `iterate over all files`()
+  fun `iterate over all files - bfs many read actions`() = `iterate over all files - base`("bfs - blocking read actions")
 
   @Test
   @RegistryKey("se.enable.non.indexable.files.use.bfs", "true")
   @RegistryKey("se.enable.non.indexable.files.use.bfs.blocking.read.actions", "true")
   @RegistryKey("intellij.platform.iterate.non.indexable.files.use.cancellable.read.actions", "true")
-  fun `iterate over all files - bfs many read actions - cancellable read actions`() = `iterate over all files`()
+  fun `iterate over all files - bfs many read actions - cancellable read actions`() = `iterate over all files - base`("bfs - cancellable read actions")
 
   @Test
   @RegistryKey("se.enable.non.indexable.files.use.bfs", "true")
   @RegistryKey("se.enable.non.indexable.files.use.bfs.blocking.read.actions", "false")
-  fun `iterate over all files - bfs one read action`() = `iterate over all files`()
+  fun `iterate over all files - bfs one read action`() = `iterate over all files - base`("bfs - one read action")
+
+  fun `search for one file deep inside - base`(subtestName: String) {
+    val searchPattern = "ProjectRootEntity"
+    val contributor = createContributor()
+    Benchmark.newBenchmarkWithVariableInputSize("search \"$searchPattern\"", nonIndexableFilesCount) {
+      contributor.search(searchPattern, MockProgressIndicator(), 1)
+      nonIndexableFilesCount
+    }.start(NonIndexableFileSearchPerformanceTest::`search for one file deep inside`.javaMethod!!, subtestName)
+  }
 
   @Test
-  fun `search for one file deep inside`(): Unit = runBlocking {
-      val searchPattern = "ProjectRootEntity"
-      val contributor = createContributor()
-      Benchmark.newBenchmarkWithVariableInputSize("search \"$searchPattern\"", nonIndexableFilesCount) {
-          contributor.search(searchPattern, MockProgressIndicator(), 1)
-          nonIndexableFilesCount
-      }.start()
+  fun `search for one file deep inside`() {
+    `search for one file deep inside - base`("dfs - blocking read actions")
   }
 
   @Test
   @RegistryKey("intellij.platform.iterate.non.indexable.files.use.cancellable.read.actions", "true")
-  fun `search for one file deep inside - cancellable read actions`() = `search for one file deep inside`()
+  fun `search for one file deep inside - cancellable read actions`() = `search for one file deep inside - base`("dfs - cancellable read actions")
 
   @Test
   @RegistryKey("se.enable.non.indexable.files.use.bfs", "true")
   @RegistryKey("se.enable.non.indexable.files.use.bfs.blocking.read.actions", "true")
-  fun `search for one file deep inside - bfs many read actions`() = `search for one file deep inside`()
+  fun `search for one file deep inside - bfs many read actions`() = `search for one file deep inside - base`("bfs - blocking read actions")
 
   @Test
   @RegistryKey("se.enable.non.indexable.files.use.bfs", "true")
   @RegistryKey("se.enable.non.indexable.files.use.bfs.blocking.read.actions", "true")
   @RegistryKey("intellij.platform.iterate.non.indexable.files.use.cancellable.read.actions", "true")
-  fun `search for one file deep inside - bfs many read actions - cancellable read actions`() = `search for one file deep inside`()
+  fun `search for one file deep inside - bfs many read actions - cancellable read actions`() = `search for one file deep inside - base`("bfs - cancellable read actions")
 
   @Test
   @RegistryKey("se.enable.non.indexable.files.use.bfs", "true")
   @RegistryKey("se.enable.non.indexable.files.use.bfs.blocking.read.actions", "false")
-  fun `search for one file deep inside - bfs one read action`() = `search for one file deep inside`()
+  fun `search for one file deep inside - bfs one read action`() = `search for one file deep inside - base`("bfs - one read action")
+
+  fun `search for one last root child - base`(subtestName: String) {
+    val filename = communityVirtualFile.getChildren(true).last().name
+    val contributor = createContributor()
+    Benchmark.newBenchmarkWithVariableInputSize("search \"$filename\"", nonIndexableFilesCount) {
+      contributor.search(filename, MockProgressIndicator(), 1)
+      nonIndexableFilesCount
+    }.start(NonIndexableFileSearchPerformanceTest::`search for one last root child`.javaMethod!!, subtestName)
+  }
 
   @Test
-  fun `search for one last root child`(): Unit = runBlocking {
-      val filename = communityVirtualFile.getChildren(true).last().name
-      val contributor = createContributor()
-      Benchmark.newBenchmarkWithVariableInputSize("search \"$filename\"", nonIndexableFilesCount) {
-          contributor.search(filename, MockProgressIndicator(), 1)
-          nonIndexableFilesCount
-      }.start()
+  fun `search for one last root child`() {
+    `search for one last root child - base`("dfs - blocking read actions")
   }
 
   @Test
   @RegistryKey("intellij.platform.iterate.non.indexable.files.use.cancellable.read.actions", "true")
-  fun `search for one last root child - cancellable read actions`() = `search for one last root child`()
+  fun `search for one last root child - cancellable read actions`() = `search for one last root child - base`("dfs - cancellable read actions")
 
   @Test
   @RegistryKey("se.enable.non.indexable.files.use.bfs", "true")
   @RegistryKey("se.enable.non.indexable.files.use.bfs.blocking.read.actions", "true")
-  fun `search for one last root child - bfs many read actions`() = `search for one last root child`()
+  fun `search for one last root child - bfs many read actions`() = `search for one last root child - base`("bfs - blocking read actions")
 
   @Test
   @RegistryKey("se.enable.non.indexable.files.use.bfs", "true")
   @RegistryKey("se.enable.non.indexable.files.use.bfs.blocking.read.actions", "true")
   @RegistryKey("intellij.platform.iterate.non.indexable.files.use.cancellable.read.actions", "true")
-  fun `search for one last root child - bfs many read actions - cancellable read actions`() = `search for one last root child`()
+  fun `search for one last root child - bfs many read actions - cancellable read actions`() = `search for one last root child - base`("bfs - cancellable read actions")
 
   @Test
   @RegistryKey("se.enable.non.indexable.files.use.bfs", "true")
   @RegistryKey("se.enable.non.indexable.files.use.bfs.blocking.read.actions", "false")
-  fun `search for one last root child - bfs one read action`() = `search for one last root child`()
+  fun `search for one last root child - bfs one read action`() = `search for one last root child - base`("bfs - one read action")
+
+  fun `search for the first root child - base`(subtestName: String) {
+    val filename = communityVirtualFile.getChildren(true).first().name
+    val contributor = createContributor()
+    Benchmark.newBenchmark("search \"$filename\"") {
+      contributor.search(filename, MockProgressIndicator(), 1)
+    }.start(NonIndexableFileSearchPerformanceTest::`search for the first root child`.javaMethod!!, subtestName)
+  }
 
   @Test
-  fun `search for the first root child`(): Unit = runBlocking {
-      val filename = communityVirtualFile.getChildren(true).first().name
-      val contributor = createContributor()
-      Benchmark.newBenchmark("search \"$filename\"") {
-          contributor.search(filename, MockProgressIndicator(), 1)
-      }.start()
+  fun `search for the first root child`() {
+    `search for the first root child - base`("dfs - blocking read actions")
   }
 
   @Test
   @RegistryKey("intellij.platform.iterate.non.indexable.files.use.cancellable.read.actions", "true")
-  fun `search for the first root child - cancellable read actions`() = `search for the first root child`()
+  fun `search for the first root child - cancellable read actions`() = `search for the first root child - base`("dfs - cancellable read actions")
 
   @Test
   @RegistryKey("se.enable.non.indexable.files.use.bfs", "true")
   @RegistryKey("se.enable.non.indexable.files.use.bfs.blocking.read.actions", "true")
-  fun `search for the first root child - bfs many read actions`() = `search for the first root child`()
+  fun `search for the first root child - bfs many read actions`() = `search for the first root child - base`("bfs - blocking read actions")
 
   @Test
   @RegistryKey("se.enable.non.indexable.files.use.bfs", "true")
   @RegistryKey("se.enable.non.indexable.files.use.bfs.blocking.read.actions", "true")
   @RegistryKey("intellij.platform.iterate.non.indexable.files.use.cancellable.read.actions", "true")
-  fun `search for the first root child - bfs many read actions - cancellable read actions`() = `search for the first root child`()
+  fun `search for the first root child - bfs many read actions - cancellable read actions`() = `search for the first root child - base`("bfs - cancellable read actions")
 
   @Test
   @RegistryKey("se.enable.non.indexable.files.use.bfs", "true")
   @RegistryKey("se.enable.non.indexable.files.use.bfs.blocking.read.actions", "false")
-  fun `search for the first root child - bfs one read action`() = `search for the first root child`()
+  fun `search for the first root child - bfs one read action`() = `search for the first root child - base`("bfs - one read action")
 
   private fun createContributor(): NonIndexableFilesSEContributor {
     val event = TestActionEvent.createTestEvent(SimpleDataContext.getProjectContext(project))
