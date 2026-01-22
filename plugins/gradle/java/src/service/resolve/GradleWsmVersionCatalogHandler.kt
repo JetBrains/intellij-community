45d9696b89523c5c88dd077e5cb3aca57b420737
Kajus Kuncaitis <kajus.kuncaitis@jetbrains.com>
2026-01-22 17:08:17 +0100

    [gradle][IDEA-379633] Extract common logic for finding `GradleBuildEntity` to `getGradleBuild`
    Space-RevId: 8233ecef71d65a8d95f9760ea336cdea05337467
    
    GitOrigin-RevId: 98814f7f352f20e6bdd1b59ae7370a5fd5cf41bb

--- plugins/gradle/java/src/service/resolve/GradleWsmVersionCatalogHandler.kt
+++ plugins/gradle/java/src/service/resolve/GradleWsmVersionCatalogHandler.kt
@@ -1,14 +1,12 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.service.resolve
 
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.backend.workspace.virtualFile
-import com.intellij.platform.backend.workspace.workspaceModel
-import com.intellij.workspaceModel.ide.legacyBridge.findModuleEntity
-import org.jetbrains.plugins.gradle.model.projectModel.gradleModuleEntity
 import org.jetbrains.plugins.gradle.model.versionCatalogs.GradleVersionCatalogEntity
 import org.jetbrains.plugins.gradle.model.versionCatalogs.versionCatalogs
+import org.jetbrains.plugins.gradle.util.getGradleBuild
 
 /**
  * Provides version catalog data collected at Gradle sync and stored in the Workspace Model.
@@ -23,12 +21,6 @@ internal class GradleWsmVersionCatalogHandler : GradleVersionCatalogHandler {
     return result
   }
 
-  private fun getVersionCatalogEntities(module: Module): List<GradleVersionCatalogEntity> {
-    val moduleEntity = module.findModuleEntity() ?: return emptyList()
-    val gradleModuleEntity = moduleEntity.gradleModuleEntity ?: return emptyList()
-
-    val storage = module.project.workspaceModel.currentSnapshot
-    val build = storage.resolve(gradleModuleEntity.gradleProjectId.buildId) ?: return emptyList()
-    return build.versionCatalogs
-  }
+  private fun getVersionCatalogEntities(module: Module): List<GradleVersionCatalogEntity> =
+    module.getGradleBuild()?.versionCatalogs ?: emptyList()
 }
