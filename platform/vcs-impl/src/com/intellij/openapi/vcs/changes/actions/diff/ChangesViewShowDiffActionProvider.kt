6ea8f7ee9adf2444269c532a936ca711aaff15ef
Ivan Semenov <ivan.semenov@jetbrains.com>
2026-01-22 19:01:10 +0100

    refactor [vcs]: extract changes view diff selection to a shared utility object
    Space-RevId: 73d51ba4e908db452d31d3f032b9a87b1ae81ba2
    
    GitOrigin-RevId: 7bb540b4c3b1e94ea111d96a96376d6095fd7fbd

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/diff/ChangesViewShowDiffActionProvider.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/diff/ChangesViewShowDiffActionProvider.kt
@@ -10,14 +10,14 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.FilePath
 import com.intellij.openapi.vcs.VcsDataKeys
 import com.intellij.openapi.vcs.changes.Change
-import com.intellij.openapi.vcs.changes.ChangeListChange
 import com.intellij.openapi.vcs.changes.ChangeListManager
 import com.intellij.openapi.vcs.changes.ChangesViewWorkflowManager
 import com.intellij.openapi.vcs.changes.actions.diff.lst.LocalChangeListDiffTool
 import com.intellij.openapi.vcs.changes.ui.ChangeDiffRequestChain
 import com.intellij.openapi.vcs.changes.ui.ChangesListView
+import com.intellij.openapi.vcs.changes.ui.ChangesListViewDiffableSelectionUtil
+import com.intellij.openapi.vcs.changes.ui.DiffSource
 import com.intellij.ui.ExperimentalUI.Companion.isNewUI
-import com.intellij.util.containers.ContainerUtil
 import com.intellij.util.containers.JBIterable
 import org.jetbrains.annotations.ApiStatus
 
@@ -38,10 +38,8 @@ class ChangesViewShowDiffActionProvider : AnActionExtensionProvider {
     val view = e.getData(ChangesListView.DATA_KEY) ?: return
     if (ChangeListManager.getInstance(project).isFreezedWithNotification(null)) return
 
-    val changes = view.selectedChanges.toList()
-    val unversioned = view.selectedUnversionedFiles.toList()
-
-    val producers = collectRequestProducers(project, changes, unversioned, view)
+    val selection = ChangesListViewDiffableSelectionUtil.computeSelectionForDiff(view)
+    val producers = selection.mapToProducers(project)
     if (producers.isEmpty) return
     val chain = ChangeDiffRequestChain(producers).apply {
       putUserData(DiffUserDataKeysEx.LAST_REVISION_WITH_LOCAL, true)
@@ -81,59 +79,14 @@ class ChangesViewShowDiffActionProvider : AnActionExtensionProvider {
     private fun canShowDiff(project: Project?, changes: JBIterable<Change>, paths: JBIterable<FilePath>): Boolean {
       return paths.isNotEmpty || changes.filter({ ChangeDiffRequestProducer.canCreate(project, it) }).isNotEmpty
     }
+  }
+}
 
-    private fun collectRequestProducers(
-      project: Project,
-      changes: List<Change>,
-      unversioned: List<FilePath>,
-      changesView: ChangesListView,
-    ): ListSelection<ChangeDiffRequestChain.Producer?> {
-      if (changes.size == 1 && unversioned.isEmpty()) { // show all changes from this changelist
-        val selectedChange: Change = changes.first()
-        var selectedChanges = changesView.getAllChangesFromSameChangelist(selectedChange)
-        if (selectedChanges == null) {
-          selectedChanges = changesView.getAllChangesFromSameAmendNode(selectedChange)
-        }
-        if (selectedChanges != null) {
-          var selectedIndex = selectedChanges.indexOfFirst { ChangeListChange.HASHING_STRATEGY.equals(selectedChange, it) }
-          if (selectedIndex == -1) selectedIndex = selectedChanges.indexOf(selectedChange)
-          return createChangeProducers(project, selectedChanges, selectedIndex)
-        }
-      }
-
-      if (unversioned.size == 1 && changes.isEmpty()) { // show all unversioned changes
-        val selectedFile = unversioned.first()
-        val allUnversioned = changesView.unversionedFiles.toList()
-        val selectedIndex = allUnversioned.indexOf(selectedFile)
-        return createUnversionedProducers(project, allUnversioned, selectedIndex)
-      }
-
-      val changeProducers = createChangeProducers(project, changes, 0)
-      val unversionedProducers = createUnversionedProducers(project, unversioned, 0)
-      return ListSelection.createAt(
-        ContainerUtil.concat(changeProducers.getList(), unversionedProducers.getList()),
-        0
-      ).asExplicitSelection()
-    }
-
-    private fun createChangeProducers(
-      project: Project,
-      changes: List<Change>,
-      selected: Int,
-    ): ListSelection<ChangeDiffRequestChain.Producer?> {
-      return ListSelection.createAt(changes, selected).map {
-        ChangeDiffRequestProducer.create(project, it)
-      }
-    }
-
-    private fun createUnversionedProducers(
-      project: Project,
-      unversioned: List<FilePath>,
-      selected: Int,
-    ): ListSelection<ChangeDiffRequestChain.Producer?> {
-      return ListSelection.createAt(unversioned, selected).map {
-        UnversionedDiffRequestProducer.create(project, it)
-      }
+private fun ListSelection<DiffSource>.mapToProducers(project: Project): ListSelection<out ChangeDiffRequestChain.Producer?> {
+  return map {
+    when (it) {
+      is DiffSource.Change -> ChangeDiffRequestProducer.create(project, it.change)
+      is DiffSource.UnversionedFile -> UnversionedDiffRequestProducer.create(project, it.filePath)
     }
   }
 }
