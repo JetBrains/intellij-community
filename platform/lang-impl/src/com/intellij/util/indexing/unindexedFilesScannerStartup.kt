786faf543693e60ba6c3f16687595bdf3e723bc2
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2026-01-22 16:58:24 +0100

    IJPL-230553 Use NBRA to fix freeze
    Space-RevId: ecebb9c355db570954c3883c1b9231c931b2bc11
    
    GitOrigin-RevId: 4781a118710d4d0322114fcc28265856ae24b04c

--- platform/lang-impl/src/com/intellij/util/indexing/unindexedFilesScannerStartup.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/unindexedFilesScannerStartup.kt
@@ -2,8 +2,8 @@
 package com.intellij.util.indexing
 
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.application.readActionBlocking
-import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.progress.Cancellation
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
@@ -252,32 +252,33 @@ private class AllNotSeenDirtyFileIds(val result: Collection<Int>) : GetNotSeenDi
   override fun getFullScanningDecision(): NotSeenIdsBasedFullScanningDecision = DirtyFileIdsCompatibleWithFullScanningSkip
 }
 
-private fun findProjectFiles(project: Project, dirtyFilesIds: Collection<Int>, limit: Int = -1): List<VirtualFile> {
+private suspend fun findProjectFiles(project: Project, dirtyFilesIds: Collection<Int>, limit: Int = -1): List<VirtualFile> {
   val fs = ManagingFS.getInstance()
   val fileBasedIndex = FileBasedIndex.getInstance() as FileBasedIndexImpl
   var exceptionLogged = false
-  return dirtyFilesIds.asSequence()
-    .mapNotNull { fileId ->
-      try {
-        val file = fs.findFileById(fileId)
-        // Blocking read action because the lambda is fast and the number of files can be large.
-        // And the previous solution was de-facto blocking because somebody (me) forgot checkCancelled.
-        val inProject = file != null && runReadAction { fileBasedIndex.belongsToProjectIndexableFiles(file, project) }
-        if (inProject) file else null
+  val projectFiles = mutableListOf<VirtualFile>()
+  val dirtyFilesIds = dirtyFilesIds.run {
+    if (limit <= 0) this
+    else this.take(limit)
+  }
+  for (fileId in dirtyFilesIds) {
+    try {
+      val file = fs.findFileById(fileId)
+      // Blocking read action because the lambda is fast and the number of files can be large.
+      // And the previous solution was de-facto blocking because somebody (me) forgot checkCancelled.
+      val inProject = file != null && readAction { fileBasedIndex.belongsToProjectIndexableFiles(file, project) }
+      if (inProject) projectFiles.add(file)
+    }
+    catch (e: VfsRootAccessNotAllowedError) {
+      if (!exceptionLogged) {
+        LOG.debug("VfsRootAccessNotAllowedError occurred. " +
+                  "Probably previous test with different rules for project roots saved these files to dirty files queue. " +
+                  "Example of error:", e)
+        exceptionLogged = true
       }
-      catch (e: VfsRootAccessNotAllowedError) {
-        if (!exceptionLogged) {
-          LOG.debug("VfsRootAccessNotAllowedError occurred. " +
-                    "Probably previous test with different rules for project roots saved these files to dirty files queue. " +
-                    "Example of error:", e)
-          exceptionLogged = true
-        }
-        null
-      }
-    }.run {
-      if (limit <= 0) this
-      else this.take(limit)
-    }.toList()
+    }
+  }
+  return projectFiles
 }
 
 private suspend fun scheduleForIndexing(someProjectDirtyFilesFiles: List<VirtualFile>, project: Project, fileBasedIndex: FileBasedIndexImpl, limit: Int) {
