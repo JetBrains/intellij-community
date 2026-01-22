31c52ba7626bcb6260b253b36a8baff67e818f8f
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-01-22 14:00:29 +0200

    [terminal] IJPL-207768 Edit PowerShell specs
    - Specify `flagsArePosixNonCompliant` to not split `-path` like options into 1 char options.
    - Add argument display names.
    
    Space-RevId: 04077208447448b88fe576df008fb27ce68934f8
    
    GitOrigin-RevId: 4b3757c8b8bc209621cd0b5c0a4dd73e97a31edd

--- plugins/terminal/src/org/jetbrains/plugins/terminal/block/completion/spec/specs/PowerShellCommandSpecsProvider.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/block/completion/spec/specs/PowerShellCommandSpecsProvider.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.terminal.block.completion.spec.specs
 
+import com.intellij.terminal.completion.spec.ShellCommandParserOptions
+import org.jetbrains.plugins.terminal.TerminalBundle
 import org.jetbrains.plugins.terminal.block.completion.spec.*
 
 internal class PowerShellCommandSpecsProvider : ShellCommandSpecsProvider {
@@ -22,13 +24,21 @@ internal class PowerShellCommandSpecsProvider : ShellCommandSpecsProvider {
 
 private fun pathSuggestingCommandSpec(commandName: String, onlyDirectories: Boolean = false): ShellCommandSpecInfo {
   val spec = ShellCommandSpec(commandName) {
+    parserOptions(
+      ShellCommandParserOptions.builder()
+        .flagsArePosixNonCompliant(true)
+        .build()
+    )
+
     option("-Path") {
       argument {
+        displayName(TerminalBundle.message("powershell.specs.argument.path"))
         suggestions(ShellDataGenerators.fileSuggestionsGenerator(onlyDirectories))
       }
     }
 
     argument {
+      displayName(TerminalBundle.message("powershell.specs.argument.path"))
       optional()
       suggestions(ShellDataGenerators.fileSuggestionsGenerator(onlyDirectories))
     }
@@ -39,22 +49,32 @@ private fun pathSuggestingCommandSpec(commandName: String, onlyDirectories: Bool
 
 private fun pathAndDestinationSuggestingCommandSpec(commandName: String): ShellCommandSpecInfo {
   val spec = ShellCommandSpec(commandName) {
+    parserOptions(
+      ShellCommandParserOptions.builder()
+        .flagsArePosixNonCompliant(true)
+        .build()
+    )
+
     option("-Path") {
       argument {
+        displayName(TerminalBundle.message("powershell.specs.argument.path"))
         suggestions(ShellDataGenerators.fileSuggestionsGenerator())
       }
     }
     option("-Destination") {
       argument {
+        displayName(TerminalBundle.message("powershell.specs.argument.path"))
         suggestions(ShellDataGenerators.fileSuggestionsGenerator())
       }
     }
 
     argument {
+      displayName(TerminalBundle.message("powershell.specs.argument.srcPath"))
       optional()
       suggestions(ShellDataGenerators.fileSuggestionsGenerator())
     }
     argument {
+      displayName(TerminalBundle.message("powershell.specs.argument.dstPath"))
       optional()
       suggestions(ShellDataGenerators.fileSuggestionsGenerator())
     }
