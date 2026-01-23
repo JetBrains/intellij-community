657024fa03737ebdc7a7a7b08c7347ec20e521d1
Alexandr Evstigneev <alexandr.evstigneev@jetbrains.com>
2026-01-23 14:51:40 +0400

    IJPL-225354 More known desktop variants
    Space-RevId: 8ca9883b1d4641e98706ab5ebfbf2576fa1ba621
    
    GitOrigin-RevId: 8ee8aed98392841f5ab2d4a690b3306f46c6e1f9

--- platform/util/ui/src/com/intellij/util/ui/UnixDesktopEnv.java
+++ platform/util/ui/src/com/intellij/util/ui/UnixDesktopEnv.java
@@ -1,22 +1,66 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.ui;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public enum UnixDesktopEnv {
-  GNOME, KDE;
+  Budgie("Budgie:GNOME", "budgie-desktop"),
+  Cinnamon("X-Cinnamon", "cinnamon"),
+  Deepin("Deepin", "dde-desktop"),
+  GNOME("GNOME", "gnome-shell"),
+  Hyprland("Hyprland", "hyprctl", List.of("version")),
+  KDE("KDE", "plasmashell"),
+  LXDE("LXDE", "lxsession"),
+  LXQT("LXQt", "lxqt-session"),
+  MATE("MATE", "mate-session"),
+  Pantheon("Pantheon", "gala"),
+  Unity("Unity", "unity"),
+  XFCE("XFCE", "xfce4-session"),
+  i3("i3", "i3"),
+  sway("sway", "sway");
 
   public static final @Nullable UnixDesktopEnv CURRENT = getDesktop();
+
+  private final @NotNull String myXdgDesktopSubstring;
+  private final @NotNull String myVersionTool;
+  private final @NotNull List<String> myVersionToolArguments;
+
+  UnixDesktopEnv(@NotNull String xdgDesktopSubstring, @NotNull String versionTool, @NotNull List<String> versionToolArguments) {
+    myXdgDesktopSubstring = xdgDesktopSubstring;
+    myVersionTool = versionTool;
+    myVersionToolArguments = versionToolArguments;
+  }
+
+  UnixDesktopEnv(@NotNull String xdgDesktopSubstring, @NotNull String versionTool) {
+    this(xdgDesktopSubstring, versionTool, List.of("--version"));
+  }
+
+  public @NotNull String getVersionTool() {
+    return myVersionTool;
+  }
+
+  public @NotNull List<String> getVersionToolArguments() {
+    return myVersionToolArguments;
+  }
 
   private static @Nullable UnixDesktopEnv getDesktop() {
     @SuppressWarnings({"SpellCheckingInspection", "RedundantSuppression"})
     String desktop = System.getenv("XDG_CURRENT_DESKTOP"), gdmSession = System.getenv("GDMSESSION");
 
+    var knownEnvironments = new ArrayList<>(List.of(values()));
+    knownEnvironments.sort(Comparator.comparing(it -> -it.myXdgDesktopSubstring.length()));
+    for (UnixDesktopEnv env : knownEnvironments) {
+      if (desktop != null && desktop.contains(env.myXdgDesktopSubstring)) return env;
+    }
+
     // http://askubuntu.com/questions/72549/how-to-determine-which-window-manager-is-running/227669#227669
     // https://userbase.kde.org/KDE_System_Administration/Environment_Variables#KDE_FULL_SESSION
-    if (desktop != null && desktop.contains("GNOME")) return GNOME;
     if (gdmSession != null && gdmSession.contains("gnome")) return GNOME;
-    if (desktop != null && desktop.contains("KDE")) return KDE;
     if (System.getenv("KDE_FULL_SESSION") != null) return KDE;
     return null;
   }
