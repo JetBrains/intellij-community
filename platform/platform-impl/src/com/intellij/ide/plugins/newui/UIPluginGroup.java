74efe9124c62266bcd6d61379b75587af53d4ae4
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2026-01-23 16:30:20 +0100

    IJPL-203048 cleanup: drop unused methods in UIPluginGroup
    Space-RevId: 5dc23ad258adca706b7a6f4c13e20184494dad8c
    
    GitOrigin-RevId: c1b3ce8fc561cda4bd634c19799eeffc235a7b95

--- platform/platform-impl/src/com/intellij/ide/plugins/newui/UIPluginGroup.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/newui/UIPluginGroup.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.plugins.newui;
 
-import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.openapi.extensions.PluginId;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
@@ -16,11 +15,6 @@ public final class UIPluginGroup {
   public Component panel;
   public List<ListPluginComponent> plugins = new ArrayList<>();
   public boolean excluded;
-
-  @Deprecated
-  public @Nullable ListPluginComponent findComponent(@NotNull IdeaPluginDescriptor descriptor) {
-    return findComponent(descriptor.getPluginId());
-  }
 
   public @Nullable ListPluginComponent findComponent(@NotNull PluginId pluginId) {
     for (ListPluginComponent component : plugins) {
