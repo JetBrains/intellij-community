10f94f58683ba72dd381b3403f87068ada2004a9
Ilia Malakhov <ilia.malakhov@jetbrains.com>
2026-01-23 16:53:05 +0100

    IJPL-5613 Allow renaming top-level class nodes by renaming the containing file
    GitOrigin-RevId: 3c724b32de029a898f5039bd0de4d295f72f6768

--- platform/refactoring/src/com/intellij/refactoring/rename/FileDumbRenameHandler.kt
+++ platform/refactoring/src/com/intellij/refactoring/rename/FileDumbRenameHandler.kt
@@ -11,12 +11,32 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiFileSystemItem
+import com.intellij.psi.PsiNamedElement
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
 class FileDumbRenameHandler : RenameHandler, DumbAware {
   private fun getElement(dataContext: DataContext): PsiElement? {
-    return PsiElementRenameHandler.getElement(dataContext) as? PsiFileSystemItem
+    if (dataContext.getData(CommonDataKeys.EDITOR) != null) return null
+
+    val element = PsiElementRenameHandler.getElement(dataContext)
+    return when {
+      element == null -> null
+      element is PsiFileSystemItem -> element
+      element !is PsiNamedElement -> null
+
+      /**
+      Check if the element replaces a file in the project view (i.e., public java class or single kotlin class)
+      Some nodes in the project view are not files and not directories.
+      For example, a Java file with a single class (e.g. `Foo` in `Foo.java`)
+      When selecting such a node in the project view, we want to rename the file it belongs to.
+       */
+      with(element.parent) {
+        this is PsiFile && this == this.containingFile && this.name.substringBeforeLast(".") == element.name
+      } -> element.parent
+
+      else -> null
+    }
   }
 
   override fun isAvailableOnDataContext(dataContext: DataContext): Boolean {
@@ -26,12 +46,12 @@ class FileDumbRenameHandler : RenameHandler, DumbAware {
   }
 
   override fun invoke(project: Project, editor: Editor, file: PsiFile, dataContext: DataContext) {
-    val element: PsiElement? = getElement(dataContext)
+    val element = getElement(dataContext)
     editor.getScrollingModel().scrollToCaret(ScrollType.MAKE_VISIBLE)
     val nameSuggestionContext = file.findElementAt(editor.getCaretModel().offset)
 
     // this call should use com.intellij.refactoring.rename.RenamePsiFileDumbProcessor
-    PsiElementRenameHandler.rename(element!!, project, nameSuggestionContext, editor )
+    PsiElementRenameHandler.rename(element!!, project, nameSuggestionContext, editor)
   }
 
   override fun invoke(project: Project, elements: Array<PsiElement?>, dataContext: DataContext) {
