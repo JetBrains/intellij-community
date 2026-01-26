72855a7dd0a9d32fb85a92edc821c376db3f9ea4
Sergei Kharitontcev-Beglov <sergei.kharitontcev-beglov@jetbrains.com>
2026-01-26 17:38:39 +0000

    IJPL-222430 rdct: Provide an Apache 2.0 license for `intellij.remoteDev.util` files
    GitOrigin-RevId: 0e1f6a78ccc5c774ba4225839fd5d0402a2dbb8d

--- platform/remoteDev-util/src/com/intellij/remoteDev/pluginSync/PluginManagerConstants.kt
+++ platform/remoteDev-util/src/com/intellij/remoteDev/pluginSync/PluginManagerConstants.kt
@@ -1,3 +1,4 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.remoteDev.pluginSync
 
 import org.jetbrains.annotations.ApiStatus
