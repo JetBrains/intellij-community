d5e54fbbfd00056db4436b613ab5eb0adc3e8862
Alexander Trushev <aleksandr.trushev@jetbrains.com>
2026-01-27 21:17:34 +0100

    [undo] IJPL-201535 Refactor speculative undo classes
    GitOrigin-RevId: b3c0a72975da62d60b1f83d4b2c98d2164a9bfaf

--- platform/platform-impl/src/com/intellij/ide/impl/UndoRemoteBehaviorService.java
+++ platform/platform-impl/src/com/intellij/ide/impl/UndoRemoteBehaviorService.java
@@ -1,28 +1,17 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.impl;
-
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.util.PlatformUtils;
-import com.intellij.util.messages.Topic;
-import org.jetbrains.annotations.ApiStatus.Experimental;
 import org.jetbrains.annotations.ApiStatus.Internal;
 
-import java.util.EventListener;
 
-
-@Experimental
 @Internal
 public class UndoRemoteBehaviorService {
-  @Topic.AppLevel
-  public static final Topic<UndoRemoteBehaviorListener> TOPIC = new Topic<>(UndoRemoteBehaviorListener.class, Topic.BroadcastDirection.TO_PARENT);
 
   public static boolean isSpeculativeUndoEnabled() {
-    if (!Registry.is("ide.undo.frontend.if.possible", true) ||
-        PlatformUtils.isRider() ||
-        PlatformUtils.isCLion() ||
-        PlatformUtils.isAppCode()) {
+    if (PlatformUtils.isRider() || PlatformUtils.isCLion() || PlatformUtils.isAppCode()) {
       return false;
     }
     var service = ApplicationManager.getApplication().getService(UndoRemoteBehaviorService.class);
@@ -38,9 +27,5 @@ public class UndoRemoteBehaviorService {
 
   protected boolean isSpeculativeUndoAvailable() {
     return false;
-  }
-
-  public interface UndoRemoteBehaviorListener extends EventListener {
-    void onAvailabilityChanged();
   }
 }
