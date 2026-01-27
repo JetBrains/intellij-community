ea99de0880a17b6e3bb86de35cd3df02deebec9c
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2026-01-27 17:41:05 +0100

    [devkit] IJPL-231307: Add the new ApplicationStarter's location for determining the required JDK version
    GitOrigin-RevId: 14bc1b10064ec90e81365f572d45949f97ff12a4

--- plugins/devkit/devkit-core/src/projectRoots/IdeaJdk.java
+++ plugins/devkit/devkit-core/src/projectRoots/IdeaJdk.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.projectRoots;
 
 import com.intellij.icons.AllIcons;
@@ -106,7 +106,7 @@ public final class IdeaJdk extends JavaDependentSdkType implements JavaSdkType {
     // in 173.* and earlier builds all IDEs included platform modules into openapi.jar
     // (see org.jetbrains.intellij.build.ProductModulesLayout.platformApiModules)
     // DO NOT change order of this list, first match must return JAR containing marker class for getRequiredJdkVersion()
-    for (String name : List.of("app-client.jar", "app.jar", "platform-api.jar", "openapi.jar")) {
+    for (String name : List.of("intellij.platform.ide.core.jar", "app-client.jar", "app.jar", "platform-api.jar", "openapi.jar")) {
       Path result = libDir.resolve(name);
       if (Files.exists(result)) {
         return result;
