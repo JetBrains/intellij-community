8328b73e5817e3075d868aa87825cf1c5c6274e7
Nikita.Ashihmin <nikita.ashihmin@jetbrains.com>
2026-01-27 13:23:50 +0400

    [jupyter] PY-87098 Jupyter: Remove deprecated package= from xml
    GitOrigin-RevId: 2610520ea38febee6208b4a6ca1795a81aaec1cb

--- notebooks/visualization/gen/com/intellij/notebooks/visualization/icons/NotebooksVisualizationIcons.java
+++ notebooks/visualization/gen/com/intellij/notebooks/visualization/icons/NotebooksVisualizationIcons.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.notebooks.visualization;
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.notebooks.visualization.icons;
 
 import com.intellij.ui.IconManager;
 import org.jetbrains.annotations.NotNull;
