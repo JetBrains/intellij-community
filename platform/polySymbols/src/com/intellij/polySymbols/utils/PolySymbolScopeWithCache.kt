8dae702df963ac7bbba37e35bd29ad4a98bc5bdd
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-01-30 08:53:54 +0100

    WEB-76560 Vue: enforce symbol contract for dereferencing pointer; ensure that JS_PROPERTIES symbols are exclusive for JS properties
    GitOrigin-RevId: f97b309967fd5399e3d022450f4c9cb6fb5b1638

--- platform/polySymbols/src/com/intellij/polySymbols/utils/PolySymbolScopeWithCache.kt
+++ platform/polySymbols/src/com/intellij/polySymbols/utils/PolySymbolScopeWithCache.kt
@@ -2,6 +2,7 @@
 package com.intellij.polySymbols.utils
 
 import com.intellij.model.Pointer
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.ModificationTracker
 import com.intellij.openapi.util.UserDataHolder
@@ -112,10 +113,20 @@ abstract class PolySymbolScopeWithCache<T : UserDataHolder, K>(
     CachedValuesManager.getManager(project).createCachedValue {
       val dependencies = mutableSetOf<Any>()
       val map = PolySymbolSearchMap(namesProvider)
+      val unitTestMode = ApplicationManager.getApplication().isUnitTestMode
       initialize(
         {
           if (!provides(it.kind))
-            throw IllegalArgumentException("Poly Symbol with unsupported kind: ${it.kind} added. $it")
+            throw IllegalArgumentException("Poly Symbol with unsupported kind: ${it.kind} added. $it (${it.javaClass}")
+          if (unitTestMode) {
+            val dereferenced = it.createPointer().dereference()
+            when {
+                dereferenced == null ->
+                  throw IllegalArgumentException("Poly Symbol dereferenced from pointer is null. $it (${it.javaClass})")
+                !dereferenced.isEquivalentTo(it) ->
+                  throw IllegalArgumentException("Poly Symbol dereferenced from pointer is not equivalent to the original. $dereferenced (${dereferenced.javaClass}) !isEquivalentTo $it (${it.javaClass})")
+            }
+          }
           map.add(it)
         }, dependencies)
       if (dependencies.isEmpty()) {
