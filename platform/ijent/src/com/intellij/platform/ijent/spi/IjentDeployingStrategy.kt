fdef7bb3b790136c8a5e02c724499733c5960243
Andrii Zinchenko <andrii.zinchenko@jetbrains.com>
2026-01-30 23:08:43 +0100

    [IJPL-209592] IJent: simplify IjentDeployingStrategy interface
    Remove getTargetPlatform(), getConnectionStrategy() and
    Posix/Windows sub-interfaces from the base interface.
    
    These are now implementation details returned via
    IjentConnectionContext from concrete strategies.
    
    GitOrigin-RevId: f2b299b03ddc98599c314091f26b9673a69fee77

--- platform/ijent/src/com/intellij/platform/ijent/spi/IjentDeployingStrategy.kt
+++ platform/ijent/src/com/intellij/platform/ijent/spi/IjentDeployingStrategy.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.ijent.spi
 
-import com.intellij.platform.eel.EelPlatform
 import com.intellij.platform.ijent.IjentApi
 import com.intellij.platform.ijent.IjentSession
 import org.jetbrains.annotations.ApiStatus
@@ -19,37 +18,8 @@ import org.jetbrains.annotations.ApiStatus
 @ApiStatus.OverrideOnly
 interface IjentDeployingStrategy {
   /**
-   * Architecture of the ijent binary that will be copied to the target machine.
-   * Typically, the ijent architecture should match the target environment architecture.
-   *
-   * The implementation doesn't need to cache the result, because the function is called exactly once.
-   *
-   * @see com.intellij.platform.ijent.IjentExecFileProvider.getIjentBinary
-   */
-  suspend fun getTargetPlatform(): EelPlatform
-
-  /**
-   * Defines a set of options for connecting to a running IJent
-   * This step is logically different from deployment,
-   * and here we allow to configure the actual process of initial message exchange.
-   *
-   * @see IjentConnectionStrategy
-   */
-  suspend fun getConnectionStrategy(): IjentConnectionStrategy
-
-  /**
    * Creates a new IJent session in the target environment.
    * This method should be called exactly once.
    */
   suspend fun <T : IjentApi> createIjentSession(): IjentSession<T>
-
-  interface Posix : IjentDeployingStrategy {
-    /** @see [IjentDeployingStrategy.getTargetPlatform] */
-    override suspend fun getTargetPlatform(): EelPlatform.Posix
-  }
-
-  interface Windows : IjentDeployingStrategy {
-    /** @see [IjentDeployingStrategy.getTargetPlatform] */
-    override suspend fun getTargetPlatform(): EelPlatform.Windows
-  }
 }
