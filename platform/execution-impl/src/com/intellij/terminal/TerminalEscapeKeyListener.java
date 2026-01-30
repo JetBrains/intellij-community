2eac55307540fa33da5f77996c63725917c7209f
Konstantin.Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-01-30 10:03:22 +0200

    [terminal] IJPL-184110 Make Classic Terminal respect TerminalMoveFocusToEditor action shortcuts
    The logic is quite complicated here.
    If terminal plugin is available, and we are in the terminal tool window - we match against the action shortcuts.
    But if there is no terminal plugin - we should still allow switching focus on Escape.
    But also, we should allow switching focus on Escape in terminal panels located outside of Terminal Tool window (for example, execution consoles) if `TerminalMoveFocusToEditor` has no shortcuts. It is required to preserve the old behavior.
    So, consoles will respect `TerminalMoveFocusToEditor` only if it has its own defined shortcuts.
    
    GitOrigin-RevId: 90c01f775a90eaa163b4757926b75875efaabbd3

--- platform/execution-impl/src/com/intellij/terminal/TerminalEscapeKeyListener.java
+++ platform/execution-impl/src/com/intellij/terminal/TerminalEscapeKeyListener.java
@@ -4,11 +4,11 @@ package com.intellij.terminal;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.keymap.KeymapUtil;
-import com.intellij.openapi.options.advanced.AdvancedSettings;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.toolWindow.InternalDecoratorImpl;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
@@ -16,14 +16,15 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.event.KeyEvent;
+import java.util.Collection;
 
 /**
  * Moves focus to editor on Escape key pressed, similarly to {@link InternalDecoratorImpl#processKeyBinding}.
  */
 @ApiStatus.Internal
 public final class TerminalEscapeKeyListener {
-  private final JBTerminalPanel myTerminalPanel;
-  private final AnAction myTerminalSwitchFocusToEditorAction;
+  private final @NotNull JBTerminalPanel myTerminalPanel;
+  private final @Nullable AnAction myTerminalSwitchFocusToEditorAction;
 
   public TerminalEscapeKeyListener(@NotNull JBTerminalPanel terminalPanel) {
     myTerminalPanel = terminalPanel;
@@ -31,47 +32,50 @@ public final class TerminalEscapeKeyListener {
   }
 
   public void handleKeyEvent(@NotNull KeyEvent e) {
-    if (e.getID() == KeyEvent.KEY_PRESSED && !e.isConsumed() && isMatched(e) && switchFocusToEditorIfSuitable()) {
+    Project project = myTerminalPanel.getContextProject();
+    if (e.getID() == KeyEvent.KEY_PRESSED && !e.isConsumed()
+        && project != null && !project.isDisposed()
+        && shouldSwitchFocusToEditor(e)) {
+      ToolWindowManager.getInstance(project).activateEditorComponent();
       e.consume();
     }
   }
 
-  private boolean isMatched(@NotNull KeyEvent e) {
-    KeyStroke stroke = getKeyStroke();
-    return stroke != null && stroke.getKeyCode() == e.getKeyCode() &&
-           stroke.getModifiers() == UIUtil.getAllModifiers(e);
-  }
-
-  private @Nullable KeyStroke getKeyStroke() {
-    if (myTerminalSwitchFocusToEditorAction == null) {
-      return null;
-    }
-    return KeymapUtil.getKeyStroke(myTerminalSwitchFocusToEditorAction.getShortcutSet());
-  }
-
-  private boolean switchFocusToEditorIfSuitable() {
-    if (shouldSwitchFocusToEditor()) {
-      Project project = myTerminalPanel.getContextProject();
-      if (project != null && !project.isDisposed()) {
-        // Repeat logic from com.intellij.openapi.wm.impl.InternalDecorator#processKeyBinding
-        ToolWindowManager.getInstance(project).activateEditorComponent();
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private boolean shouldSwitchFocusToEditor() {
-    if (myTerminalPanel.getTerminalTextBuffer().isUsingAlternateBuffer()) {
-      return false;
-    }
+  private boolean shouldSwitchFocusToEditor(@NotNull KeyEvent e) {
     ToolWindow toolWindow = myTerminalPanel.getContextToolWindow();
     if (toolWindow == null) {
+      // We are not in the tool window, so where we are? Maybe in the editor already.
       return false;
     }
-    if (JBTerminalWidget.isTerminalToolWindow(toolWindow) && !AdvancedSettings.getBoolean("terminal.escape.moves.focus.to.editor")) {
-      return false; // For example, vi key bindings configured in terminal
+    else if (myTerminalSwitchFocusToEditorAction != null) {
+      Collection<KeyStroke> strokes = KeymapUtil.getKeyStrokes(myTerminalSwitchFocusToEditorAction.getShortcutSet());
+      if (JBTerminalWidget.isTerminalToolWindow(toolWindow)) {
+        // If we are in the terminal tool window, allow moving focus only if it matches the defined shortcut.
+        return isMatched(e, strokes);
+      }
+      else {
+        // This terminal panel is located out of the terminal tool window.
+        // For example, it can be an execution console.
+        // Let's follow the terminal shortcut if it is defined, but if it is not, then allow moving focus by Escape.
+        return strokes.isEmpty() ? isEscape(e) : isMatched(e, strokes);
+      }
     }
-    return true;
+    else {
+      // If there is no TerminalSwitchFocusToEditorAction, then it means that the Terminal plugin is unloaded.
+      // And this terminal panel is located outside the terminal tool window.
+      // So, let's allow moving focus to the editor if it is an escape key.
+      return isEscape(e);
+    }
+  }
+
+  private static boolean isMatched(@NotNull KeyEvent e, @NotNull Collection<KeyStroke> strokes) {
+    return ContainerUtil.exists(strokes, stroke -> {
+      //noinspection MagicConstant
+      return stroke.getKeyCode() == e.getKeyCode() && stroke.getModifiers() == UIUtil.getAllModifiers(e);
+    });
+  }
+
+  private static boolean isEscape(@NotNull KeyEvent e) {
+    return e.getKeyCode() == KeyEvent.VK_ESCAPE && e.getModifiersEx() == 0;
   }
 }
