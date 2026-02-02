f3d5c82c7088735cdcab3c500dde01d2de1d0b62
Nikolay Chashnikov <nikolay.chashnikov@jetbrains.com>
2026-02-02 12:09:51 +0100

    refactoring [product modules]: convert ProductModules and ProductModulesImpl to Kotlin (IJPL-232775)
    GitOrigin-RevId: ad9a2f06eab538e8d43c37ff88d95af6b1afeaa0

--- platform/runtime/product/src/ProductModules.kt
+++ platform/runtime/product/src/ProductModules.kt
@@ -1,23 +1,19 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.runtime.product;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
+package com.intellij.platform.runtime.product
 
 /**
- * Describes modules included in the product. The modules are specified in product-modules.xml file, which is loaded by 
- * {@link com.intellij.platform.runtime.product.serialization.ProductModulesSerialization#loadProductModules} method.
+ * Describes modules included in the product. The modules are specified in a `product-modules.xml` file, which is loaded by
+ * [com.intellij.platform.runtime.product.serialization.ProductModulesSerialization.loadProductModules] method.
  */
-public interface ProductModules {
+interface ProductModules {
   /**
-   * Returns description of the main module group. Modules from this group are always enabled.
+   * Returns a description of the main module group. Modules from this group are always enabled.
    */
-  @NotNull RuntimeModuleGroup getMainModuleGroup();
+  val mainModuleGroup: RuntimeModuleGroup
 
   /**
    * Returns description of module groups corresponding to the bundled plugins. Modules from these groups may be disabled if the corresponding
    * plugin is disabled by the user.
    */
-  @NotNull List<@NotNull PluginModuleGroup> getBundledPluginModuleGroups();
+  val bundledPluginModuleGroups: List<PluginModuleGroup>
 }
--- platform/runtime/product/src/impl/ProductModulesImpl.kt
+++ platform/runtime/product/src/impl/ProductModulesImpl.kt
@@ -1,38 +1,16 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.runtime.product.impl;
+package com.intellij.platform.runtime.product.impl
 
-import com.intellij.platform.runtime.product.PluginModuleGroup;
-import com.intellij.platform.runtime.product.ProductModules;
-import com.intellij.platform.runtime.product.RuntimeModuleGroup;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.platform.runtime.product.PluginModuleGroup
+import com.intellij.platform.runtime.product.ProductModules
+import org.jetbrains.annotations.NonNls
 
-import java.util.List;
-
-public final class ProductModulesImpl implements ProductModules {
-  private final String myDebugName;
-  private final MainRuntimeModuleGroup myMainGroup;
-  private final List<PluginModuleGroup> myBundledPluginGroups;
-
-  public ProductModulesImpl(@NotNull @NonNls String debugName, MainRuntimeModuleGroup mainGroup,
-                            @NotNull List<PluginModuleGroup> bundledPluginGroups) {
-    myDebugName = debugName;
-    myMainGroup = mainGroup;
-    myBundledPluginGroups = bundledPluginGroups;
-  }
-
-  @Override
-  public @NotNull RuntimeModuleGroup getMainModuleGroup() {
-    return myMainGroup;
-  }
-
-  @Override
-  public @NotNull List<@NotNull PluginModuleGroup> getBundledPluginModuleGroups() {
-    return myBundledPluginGroups;
-  }
-
-  @Override
-  public String toString() {
-    return "ProductModules{debugName=" + myDebugName + '}';
+class ProductModulesImpl(
+  private val debugName: @NonNls String,
+  override val mainModuleGroup: MainRuntimeModuleGroup,
+  override val bundledPluginModuleGroups: List<PluginModuleGroup>,
+) : ProductModules {
+  override fun toString(): String {
+    return "ProductModules{debugName=$debugName}"
   }
 }
