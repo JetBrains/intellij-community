15bde2c45d07e0e0ecc89e92e281d91b3c5463de
Victor Turansky <victor.turansky@jetbrains.com>
2026-02-03 00:27:30 +0100

    IJPL-223873 Extract `TestVisibilityServiceImpl` to separate file
    (cherry picked from commit 2e1018cb1c9bcbade6606b1a15f7db4e6271126c)
    
    GitOrigin-RevId: 1d1f6d844e4d67d7770b3ba18c58cbd968276b65

--- platform/service-container/testSrc/com/intellij/serviceContainer/TestVisibilityService.java
+++ platform/service-container/testSrc/com/intellij/serviceContainer/TestVisibilityService.java
@@ -14,16 +14,3 @@ public class TestVisibilityService {
 
   String callPrivate() { return privateMethod(); }
 }
-
-class TestVisibilityServiceImpl extends TestVisibilityService {
-  private String privateMethod() { return "private_impl"; }
-
-  @Override
-  public String publicMethod() { return "public_impl"; }
-
-  @Override
-  protected String protectedMethod() { return "protected_impl"; }
-
-  @Override
-  String packagePrivateMethod() { return "package_private_impl"; }
-}
