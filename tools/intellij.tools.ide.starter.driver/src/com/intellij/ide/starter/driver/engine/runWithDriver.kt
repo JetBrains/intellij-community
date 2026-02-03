42ff80853c8021857de85743b5c2625b615c9c10
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2026-02-03 19:45:59 +0100

    testFramework [driver]: use rem dev aware DI by default in runIdeWithDriver
    GitOrigin-RevId: c1eb6b7f57d024dd100402ff876a46b8200631aa

--- tools/intellij.tools.ide.starter.driver/src/com/intellij/ide/starter/driver/engine/runWithDriver.kt
+++ tools/intellij.tools.ide.starter.driver/src/com/intellij/ide/starter/driver/engine/runWithDriver.kt
@@ -1,15 +1,27 @@
 package com.intellij.ide.starter.driver.engine
 
+import com.intellij.ide.starter.config.ConfigurationStorage
+import com.intellij.ide.starter.config.splitMode
 import com.intellij.ide.starter.di.di
+import com.intellij.ide.starter.driver.driver.remoteDev.RemDevDriverRunner
 import com.intellij.ide.starter.ide.IDETestContext
 import com.intellij.ide.starter.runner.IDECommandLine
 import com.intellij.ide.starter.runner.IDERunContext
 import com.intellij.tools.ide.performanceTesting.commands.CommandChain
 import com.intellij.tools.ide.performanceTesting.commands.MarshallableCommand
+import org.kodein.di.DI
+import org.kodein.di.bindProvider
 import org.kodein.di.direct
-import org.kodein.di.instanceOrNull
+import org.kodein.di.instance
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
+
+private val remDevAwareDi
+  get() = DI {
+    extend(di)
+    bindProvider<DriverRunner> { if (ConfigurationStorage.splitMode()) RemDevDriverRunner() else LocalDriverRunner() }
+  }
+
 
 fun IDETestContext.runIdeWithDriver(commandLine: (IDERunContext) -> IDECommandLine = determineDefaultCommandLineArguments(),
                                     commands: Iterable<MarshallableCommand> = CommandChain(),
@@ -20,7 +32,7 @@ fun IDETestContext.runIdeWithDriver(commandLine: (IDERunContext) -> IDECommandLi
                                     expectedExitCode: Int = 0,
                                     collectNativeThreads: Boolean = false,
                                     configure: IDERunContext.() -> Unit = {}): BackgroundRun {
-  val driverRunner = di.direct.instanceOrNull<DriverRunner>() ?: LocalDriverRunner()
+  val driverRunner = remDevAwareDi.direct.instance<DriverRunner>()
   return driverRunner.runIdeWithDriver(this, commandLine, commands, runTimeout, useStartupScript, launchName, expectedKill, expectedExitCode, collectNativeThreads) {
     if (System.getenv("SCREEN_RECORDING_ENABLED").toBoolean()) {
       withScreenRecording()
