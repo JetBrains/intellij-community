aebb485425c407ff28813c3ffe61c6204f58df66
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-02-04 13:24:47 +0200

    [tabs] IJPL-59194 Add documentation to ContentManagerListener
    Explore contracts and provide recommendations.
    
    GitOrigin-RevId: 94e58df42f105945aa4b6f397ac807dd713b00fb

--- platform/ide-core/src/com/intellij/ui/content/ContentManagerListener.java
+++ platform/ide-core/src/com/intellij/ui/content/ContentManagerListener.java
@@ -9,9 +9,25 @@ public interface ContentManagerListener extends EventListener {
   default void contentAdded(@NotNull ContentManagerEvent event) {
   }
 
+  /**
+   * Called after content was removed from the manager.
+   * <p>
+   * But it can be removed temporarily, for example, to reorder the contents (like using drag and drop).
+   * In this case, content has {@link Content#TEMPORARY_REMOVED_KEY} user data set to true.
+   * <b>Avoid disposing the content resources</b> in the body of this method, or do that only
+   * if the content is not removed temporarily.
+   */
   default void contentRemoved(@NotNull ContentManagerEvent event) {
   }
 
+  /**
+   * Called before content is removed from the manager.
+   * <p>
+   * Can be used to perform some checks, for example, show a confirmation dialog.
+   * And prohibit content removal if necessary by calling {@link ContentManagerEvent#consume()}.
+   * <p>
+   * This method isn't called when content is being removed temporarily ({@link Content#TEMPORARY_REMOVED_KEY} is set to true).
+   */
   default void contentRemoveQuery(@NotNull ContentManagerEvent event) {
   }
 
