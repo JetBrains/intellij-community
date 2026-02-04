0e5ffd65a310bd64cf2febd736716e900296925b
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-02-04 14:06:07 +0200

    [terminal] IJPL-233276 Update action shortcut on EDT in RemDev
    GitOrigin-RevId: 9c85e7ce0be6562756e316ad46e5d1a150b3a3a6

--- plugins/terminal/frontend/src/com/intellij/terminal/frontend/settings/TerminalActionShortcutChangedListener.kt
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/settings/TerminalActionShortcutChangedListener.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.terminal.frontend.settings
 
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.runInEdt
 import com.intellij.platform.rpc.topics.ApplicationRemoteTopic
 import com.intellij.platform.rpc.topics.ApplicationRemoteTopicListener
 import org.jetbrains.annotations.ApiStatus
@@ -13,6 +15,8 @@ class TerminalActionShortcutChangedListener : ApplicationRemoteTopicListener<Ter
   override val topic: ApplicationRemoteTopic<TerminalActionShortcutChangedEvent> = TERMINAL_ACTION_SHORTCUT_CHANGED_TOPIC
 
   override fun handleEvent(event: TerminalActionShortcutChangedEvent) {
-    updateActionShortcut(event.actionId, event.shortcut)
+    runInEdt(ModalityState.any()) {
+      updateActionShortcut(event.actionId, event.shortcut)
+    }
   }
 }
