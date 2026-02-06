223afcae1e3cbf8db7f12ce628fa7f99f9886ba9
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2026-02-06 20:14:26 +0000

    IJPL-231661 Gateway connection window doesn't hide to tray
    disable Wayland auto-detection for Gateway, as WLToolkit doesn't support system tray currently
    
    
    Merge-request: IJ-MR-190639
    Merged-by: Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
    
    GitOrigin-RevId: 79163c94431e563f5e94b083edaa7100684b08a7

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/LinuxDistributionBuilder.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/LinuxDistributionBuilder.kt
@@ -3,7 +3,6 @@ package org.jetbrains.intellij.build.impl
 
 import com.intellij.openapi.util.io.NioFiles
 import com.intellij.platform.buildData.productInfo.ProductInfoLaunchData
-import com.intellij.platform.runtime.product.ProductMode
 import io.opentelemetry.api.trace.Span
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.Dispatchers
@@ -476,10 +475,11 @@ class LinuxDistributionBuilder(
 
   private fun writeLinuxVmOptions(distBinDir: Path, context: BuildContext): Path {
     val vmOptionsPath = distBinDir.resolve("${context.productProperties.baseFileName}64.vmoptions")
-    val waylandOptions = if (context.productProperties.productMode == ProductMode.FRONTEND)
-      emptySequence() // Wayland auto-detection is disabled for JetBrains Client until rem-dev specific compatibility issues are resolved (IJPL-231136)
-    else
-      sequenceOf("-Dawt.toolkit.name=auto")
+    val waylandOptions = when (context.productProperties.platformPrefix) {
+      "JetBrainsClient" -> emptySequence() // Wayland auto-detection is disabled for JetBrains Client until rem-dev specific compatibility issues are resolved (IJPL-231136)
+      "Gateway" -> emptySequence() // and for Gateway until system tray will be supported in Wayland toolkit in JBR (IJPL-231661/JBR-9966)
+      else -> sequenceOf("-Dawt.toolkit.name=auto")
+    }
     val vmOptions = VmOptionsGenerator.generate(context).asSequence() + sequenceOf("-Dsun.tools.attach.tmp.only=true", "-Dawt.lock.fair=true") + waylandOptions
     VmOptionsGenerator.writeVmOptions(vmOptionsPath, vmOptions, separator = "\n")
     return vmOptionsPath
