528fae0c81809625687d8571c60c7a2ebad4fe9a
Alexander Trushev <aleksandr.trushev@jetbrains.com>
2026-02-06 14:47:57 +0100

    [editor] IJPL-233407 Add smooth caret feedback survey
    GitOrigin-RevId: 13376576d0dae8e280dafeebd9cb70bddabbb829

---
+++ platform/feedback/src/com/intellij/platform/feedback/editor/smoothcaret/SmoothCaretFeedbackDialog.kt
@@ -0,0 +1,79 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.feedback.editor.smoothcaret
+
+import com.intellij.openapi.editor.ex.EditorSettingsExternalizable
+import com.intellij.openapi.project.Project
+import com.intellij.platform.feedback.dialog.BlockBasedFeedbackDialog
+import com.intellij.platform.feedback.dialog.CommonFeedbackSystemData
+import com.intellij.platform.feedback.dialog.showFeedbackSystemInfoDialog
+import com.intellij.platform.feedback.dialog.uiBlocks.DescriptionBlock
+import com.intellij.platform.feedback.dialog.uiBlocks.FeedbackBlock
+import com.intellij.platform.feedback.dialog.uiBlocks.RadioButtonGroupBlock
+import com.intellij.platform.feedback.dialog.uiBlocks.RadioButtonItemData
+import com.intellij.platform.feedback.dialog.uiBlocks.RatingBlock
+import com.intellij.platform.feedback.dialog.uiBlocks.TextAreaBlock
+import com.intellij.platform.feedback.dialog.uiBlocks.TopLabelBlock
+
+
+class SmoothCaretFeedbackDialog(
+  project: Project,
+  forTest: Boolean,
+) : BlockBasedFeedbackDialog<SmoothCaretUsageData>(project, forTest) {
+
+  override val myFeedbackReportId: String = "smooth_caret"
+
+  override val myTitle: String = SmoothCaretFeedbackBundle.message("feedback.smooth.caret.dialog.title")
+
+  override suspend fun computeSystemInfoData(): SmoothCaretUsageData {
+    val editorSettings = EditorSettingsExternalizable.getInstance()
+    return SmoothCaretUsageData(
+      isAnimatedCaret = editorSettings.isAnimatedCaret,
+      caretEasing = editorSettings.caretEasing.name,
+      smoothCaretBlinking = editorSettings.isSmoothBlinkCaret,
+      systemInfo = CommonFeedbackSystemData.getCurrentData(),
+    )
+  }
+
+  override fun showFeedbackSystemInfoDialog(systemInfoData: SmoothCaretUsageData) {
+    showFeedbackSystemInfoDialog(myProject, systemInfoData.systemInfo) {
+      row(SmoothCaretFeedbackBundle.message("feedback.smooth.caret.system.info.is.animated")) {
+        label(systemInfoData.isAnimatedCaret.toString())
+      }
+      row(SmoothCaretFeedbackBundle.message("feedback.smooth.caret.system.info.easing")) {
+        label(systemInfoData.caretEasing)
+      }
+      row(SmoothCaretFeedbackBundle.message("feedback.smooth.caret.system.info.blinking")) {
+        label(systemInfoData.smoothCaretBlinking.toString())
+      }
+    }
+  }
+
+  override val myBlocks: List<FeedbackBlock> = listOf(
+    TopLabelBlock(SmoothCaretFeedbackBundle.message("feedback.smooth.caret.dialog.header")),
+    DescriptionBlock(SmoothCaretFeedbackBundle.message("feedback.smooth.caret.dialog.description")),
+    
+    RadioButtonGroupBlock(
+      SmoothCaretFeedbackBundle.message("feedback.smooth.caret.dialog.noticed.question"),
+      listOf(
+        RadioButtonItemData(SmoothCaretFeedbackBundle.message("feedback.smooth.caret.dialog.noticed.yes"), "yes"),
+        RadioButtonItemData(SmoothCaretFeedbackBundle.message("feedback.smooth.caret.dialog.noticed.no"), "no"),
+      ),
+      "noticed_animated_caret",
+    ).requireAnswer(),
+    
+    RatingBlock(
+      SmoothCaretFeedbackBundle.message("feedback.smooth.caret.dialog.rating.title"),
+      "smooth_caret_rating",
+    ),
+    
+    TextAreaBlock(
+      SmoothCaretFeedbackBundle.message("feedback.smooth.caret.dialog.additional"),
+      "additional_feedback",
+    )
+      .setPlaceholder(SmoothCaretFeedbackBundle.message("feedback.smooth.caret.dialog.additional.placeholder")),
+  )
+
+  init {
+    init()
+  }
+}
---
+++ platform/feedback/src/com/intellij/platform/feedback/editor/smoothcaret/SmoothCaretUsageData.kt
@@ -0,0 +1,30 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.feedback.editor.smoothcaret
+
+import com.intellij.openapi.util.NlsSafe
+import com.intellij.platform.feedback.dialog.CommonFeedbackSystemData
+import com.intellij.platform.feedback.dialog.SystemDataJsonSerializable
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.encodeToJsonElement
+
+@Serializable
+data class SmoothCaretUsageData(
+  val isAnimatedCaret: Boolean,
+  @get:NlsSafe val caretEasing: String,
+  val smoothCaretBlinking: Boolean,
+  val systemInfo: CommonFeedbackSystemData,
+) : SystemDataJsonSerializable {
+  
+  override fun serializeToJson(json: Json): JsonElement {
+    return json.encodeToJsonElement(this)
+  }
+
+  override fun toString(): String = buildString {
+    appendLine("Animated Caret Enabled: $isAnimatedCaret")
+    appendLine("Caret Easing: $caretEasing")
+    appendLine("Smooth Blinking: $smoothCaretBlinking")
+    append(systemInfo.toString())
+  }
+}
