fcea9e4776bfb2aabeddf0829dc0005e41102279
Shumaf Lovpache <shumaf.lovpache@jetbrains.com>
2026-02-16 13:08:19 +0200

    refactor stream debugger: convert PrimitiveValueDescriptor to kotlin
    GitOrigin-RevId: 9cbedcd70c9851673106353c21e2e3b911f6f7e1

--- plugins/stream-debugger/java/src/com/intellij/debugger/streams/ui/impl/PrimitiveValueDescriptor.kt
+++ plugins/stream-debugger/java/src/com/intellij/debugger/streams/ui/impl/PrimitiveValueDescriptor.kt
@@ -1,45 +1,37 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.debugger.streams.ui.impl;
+package com.intellij.debugger.streams.ui.impl
 
-import com.intellij.debugger.DebuggerContext;
-import com.intellij.debugger.engine.ContextUtil;
-import com.intellij.debugger.engine.evaluation.EvaluateException;
-import com.intellij.debugger.memory.utils.InstanceValueDescriptor;
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiElementFactory;
-import com.intellij.psi.PsiExpression;
-import com.sun.jdi.ObjectReference;
-import com.sun.jdi.Value;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.debugger.DebuggerContext
+import com.intellij.debugger.engine.ContextUtil
+import com.intellij.debugger.engine.evaluation.EvaluateException
+import com.intellij.debugger.memory.utils.InstanceValueDescriptor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.PsiExpression
+import com.sun.jdi.ObjectReference
+import com.sun.jdi.Value
 
-public final class PrimitiveValueDescriptor extends InstanceValueDescriptor {
-  PrimitiveValueDescriptor(@NotNull Project project, @Nullable Value value) {
-    super(project, value);
-  }
-
-  @Override
-  public String calcValueName() {
-    final Value value = getValue();
+internal class PrimitiveValueDescriptor internal constructor(project: Project, value: Value?) : InstanceValueDescriptor(project, value) {
+  override fun calcValueName(): String? {
+    val value = getValue()
     if (value == null) {
-      return "value";
+      return "value"
     }
-    if (value instanceof ObjectReference) {
-      return super.calcValueName();
+    if (value is ObjectReference) {
+      return super.calcValueName()
     }
 
-    return value.type().name();
+    return value.type().name()
   }
 
-  @Override
-  public PsiExpression getDescriptorEvaluation(DebuggerContext debuggerContext) throws EvaluateException {
-    final Value value = getValue();
-    if (value instanceof ObjectReference) {
-      return super.getDescriptorEvaluation(debuggerContext);
+  @Throws(EvaluateException::class)
+  override fun getDescriptorEvaluation(debuggerContext: DebuggerContext?): PsiExpression? {
+    val value = getValue()
+    if (value is ObjectReference) {
+      return super.getDescriptorEvaluation(debuggerContext)
     }
 
-    final PsiElementFactory elementFactory = JavaPsiFacade.getInstance(myProject).getElementFactory();
-    return elementFactory.createExpressionFromText(value.toString(), ContextUtil.getContextElement(debuggerContext));
+    val elementFactory = JavaPsiFacade.getInstance(myProject).getElementFactory()
+    return elementFactory.createExpressionFromText(value.toString(), ContextUtil.getContextElement(debuggerContext))
   }
 }
