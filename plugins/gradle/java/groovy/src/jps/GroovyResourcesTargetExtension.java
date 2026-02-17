df2459aaf0c7b076e390e532de1306381d85e368
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-02-17 09:10:53 +0100

    [gradle] IDEA-385912 remove Gradle for Java dependency on Groovy plugin
    (cherry picked from commit 2b3b8bea1c80d794179ca215ad2fd92e26542791)
    
    IJ-CR-192487
    
    GitOrigin-RevId: 2ff2afce8d8e56e11ff3e997bf2b793789aa619a

--- plugins/gradle/java/groovy/src/jps/GroovyResourcesTargetExtension.java
+++ plugins/gradle/java/groovy/src/jps/GroovyResourcesTargetExtension.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.jps.gradle.model.impl;
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.gradle.java.groovy.jps;
 
 import com.intellij.openapi.util.io.FileUtil;
 import org.jetbrains.annotations.NotNull;
