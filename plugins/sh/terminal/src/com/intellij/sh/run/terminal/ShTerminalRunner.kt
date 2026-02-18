a509e823205f11658124fd69cf39a5fb259c3a94
Konstantin.Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-02-18 17:46:15 +0200

    [terminal] IJPL-235417 Do not run hasRunningCommands on EDT in ShTerminalRunner
    Actually, `ShRunner#run` doesn't return a result. And current usages don't rely on it to create the terminal tab immediately. So, let's do it asynchronously, using coroutines, and using a proper dispatcher for `hasRunningCommands` check.
    
    (cherry picked from commit 34012369a219d5571c7542d790cd93cd1caf7dd2)
    
    IJ-CR-193029
    
    GitOrigin-RevId: ab97e6a630465a0732dd4c4bf8214edfbe0cf9e9

--- plugins/sh/terminal/src/com/intellij/sh/run/terminal/ShTerminalRunner.kt
+++ plugins/sh/terminal/src/com/intellij/sh/run/terminal/ShTerminalRunner.kt
@@ -1,7 +1,11 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.sh.run.terminal;
 
+import com.intellij.openapi.application.UiWithModelAccess
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.NlsContexts
 import com.intellij.openapi.util.Pair
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.wm.ToolWindowManager
@@ -9,13 +13,20 @@ import com.intellij.sh.run.ShRunner
 import com.intellij.terminal.JBTerminalWidget
 import com.intellij.terminal.ui.TerminalWidget
 import com.intellij.ui.content.Content
-import com.intellij.ui.content.ContentManager
+import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
+import com.intellij.util.concurrency.annotations.RequiresReadLockAbsence
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.plugins.terminal.ShellTerminalWidget
 import org.jetbrains.plugins.terminal.TerminalToolWindowFactory
 import org.jetbrains.plugins.terminal.TerminalToolWindowManager
 import org.jetbrains.plugins.terminal.TerminalUtil
 import org.jetbrains.plugins.terminal.arrangement.TerminalWorkingDirectoryManager
 
+@ApiStatus.Internal
 open class ShTerminalRunner : ShRunner {
   override fun run(
     project: Project,
@@ -24,21 +35,43 @@ open class ShTerminalRunner : ShRunner {
     title: String,
     activateToolWindow: Boolean,
   ) {
+    ShTerminalScopeProvider.getInstance(project).coroutineScope.launch(Dispatchers.UiWithModelAccess) {
+      doRun(project, command, workingDirectory, title, activateToolWindow)
+    }
+  }
+
+  private suspend fun doRun(
+    project: Project,
+    command: String,
+    workingDirectory: String,
+    @NlsContexts.TabTitle title: String,
+    activateToolWindow: Boolean,
+  ) {
     val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(TerminalToolWindowFactory.TOOL_WINDOW_ID) ?: return
     val contentManager = toolWindow.getContentManager()
-    val pair = getSuitableProcess(project, contentManager, workingDirectory)
+    val contents = contentManager.contents
+    val selectedContent = contentManager.selectedContent
+
+    val pair = withContext(Dispatchers.IO) {
+      getSuitableProcess(project, contents, selectedContent, workingDirectory)
+    }
+
     if (pair == null) {
       TerminalToolWindowManager.getInstance(project)
         .createShellWidget(workingDirectory, title, activateToolWindow, activateToolWindow)
         .sendCommandToExecute(command)
     }
-    else {
+    else if (contentManager.contents.contains(pair.first) && pair.first.isValid) {  // Check that chosen content is still valid
       if (activateToolWindow) {
         toolWindow.activate(null)
       }
       pair.first.setDisplayName(title)
       contentManager.setSelectedContent(pair.first)
       pair.second.sendCommandToExecute(command)
+    }
+    else {
+      // The chosen content became invalid during background checks, let's retry.
+      doRun(project, command, workingDirectory, title, false)
     }
   }
 
@@ -49,18 +82,21 @@ open class ShTerminalRunner : ShRunner {
 
   private fun getSuitableProcess(
     project: Project,
-    contentManager: ContentManager,
+    contents: Array<Content>,
+    selectedContent: Content?,
     workingDirectory: String,
   ): Pair<Content, TerminalWidget>? {
-    val selectedContent = contentManager.getSelectedContent()
     if (selectedContent != null) {
       val pair = getSuitableProcess(project, selectedContent, workingDirectory)
       if (pair != null) return pair
     }
 
-    return contentManager.contents.firstNotNullOfOrNull { getSuitableProcess(project, it, workingDirectory) }
+    val otherContents = contents.filter { it != selectedContent }
+    return otherContents.firstNotNullOfOrNull { getSuitableProcess(project, it, workingDirectory) }
   }
 
+  @RequiresReadLockAbsence
+  @RequiresBackgroundThread
   protected open fun getSuitableProcess(
     project: Project,
     content: Content,
@@ -86,5 +122,12 @@ open class ShTerminalRunner : ShRunner {
     }
 
     return Pair(content, widget)
+  }
+}
+
+@Service(Service.Level.PROJECT)
+private class ShTerminalScopeProvider(val coroutineScope: CoroutineScope) {
+  companion object {
+    fun getInstance(project: Project): ShTerminalScopeProvider = project.service()
   }
 }
