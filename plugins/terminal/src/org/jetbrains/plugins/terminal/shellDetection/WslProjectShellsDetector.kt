b9d5f0fa313053bf7b9ae39a8738e4a7aa9ba6d1
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-02-19 17:39:45 +0200

    [terminal] IJPL-202636 Implement WslProjectShellsDetector
    (cherry picked from commit b6234032653e3f68c41dfa608e099078d4929e8d)
    
    IJ-CR-193031
    
    GitOrigin-RevId: b669e1c10140964ddad7e3dfdd7a509be7936694

---
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/shellDetection/WslProjectShellsDetector.kt
@@ -0,0 +1,31 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.terminal.shellDetection
+
+import com.intellij.openapi.project.Project
+import com.intellij.platform.eel.provider.LocalEelDescriptor
+import com.intellij.platform.eel.provider.getEelDescriptor
+import com.intellij.platform.ide.impl.wsl.WslEelDescriptor
+import org.jetbrains.plugins.terminal.shellDetection.TerminalShellsDetectionUtil.detectUnixShells
+import org.jetbrains.plugins.terminal.shellDetection.TerminalShellsDetectionUtil.detectWindowsShells
+import org.jetbrains.plugins.terminal.shellDetection.TerminalShellsDetectionUtil.detectWslDistributions
+
+/**
+ * When the project is opened in the WSL file system, it is worth allowing starting shells from both WSL and the host OS.
+ * So, it detects Unix shells in the WSL and Windows shells in the host OS.
+ */
+internal class WslProjectShellsDetector : TerminalShellsDetector {
+  override suspend fun detectShells(project: Project): List<DetectedShellInfo> {
+    check(isApplicable(project)) { "Should only be called if isApplicable() == true" }
+
+    val wslEelDescriptor = project.getEelDescriptor()
+    val hostEelDescriptor = LocalEelDescriptor
+
+    val wslShells = detectUnixShells(wslEelDescriptor)
+    val hostShells = detectWindowsShells(hostEelDescriptor) + detectWslDistributions()
+    return wslShells + hostShells
+  }
+
+  override fun isApplicable(project: Project): Boolean {
+    return project.getEelDescriptor() is WslEelDescriptor
+  }
+}
