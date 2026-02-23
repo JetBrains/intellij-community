81e9f419db33811604ee13181f32eb79616187cc
Adam Malek <adam.malek@jetbrains.com>
2026-02-23 13:57:55 +0100

    doc ML in SE: Fix copyright year
    (cherry picked from commit 1b9086d80aa05723160d3d86546a2e983b76176b)
    
    IJ-CR-192893
    
    GitOrigin-RevId: 761283503c0b6b6285a7437af1d3000ff3e3922e

--- plugins/search-everywhere-ml/ranking/core/src/com/intellij/searchEverywhereMl/ranking/core/model/SearchEverywhereFeatureNameConverter.kt
+++ plugins/search-everywhere-ml/ranking/core/src/com/intellij/searchEverywhereMl/ranking/core/model/SearchEverywhereFeatureNameConverter.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.searchEverywhereMl.ranking.core.model
 
 // This conversion is required while bundled models still expect legacy camelCase names.
