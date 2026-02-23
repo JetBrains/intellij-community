72f06bb380e55e869556ec348c19f9be3045c8d1
Morgan Bartholomew <morgan.bartholomew@jetbrains.com>
2026-02-23 10:58:05 +1000

    PY-87998 PyStringConversionWithoutDunderMethodInspection: initial
    GitOrigin-RevId: 359b9a41515da9a841aad787695dee4ed14a12f5

---
+++ python/testSrc/com/jetbrains/python/inspections/PyStringConversionWithoutDunderMethodInspectionTest.kt
@@ -0,0 +1,239 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.jetbrains.python.inspections
+
+import com.jetbrains.python.fixtures.PyInspectionTestCase
+
+class PyStringConversionWithoutDunderMethodInspectionTest : PyInspectionTestCase() {
+
+  override fun getInspectionClass() = PyStringConversionWithoutDunderMethodInspection::class.java
+
+  fun `test generator`() = doTestByText("""
+    f"{<weak_warning descr="Type 'Generator' doesn't define '__str__', '__repr__', or '__format__', so the result won't be useful">(_ for _ in range(10))</weak_warning>}"
+  """.trimIndent())
+
+  fun `test function`() = doTestByText("""
+    def f(): ...
+    f"{<weak_warning descr="Type 'FunctionType' string value is not helpful">f</weak_warning>}"
+  """.trimIndent())
+
+  fun `test zip`() = doTestByText("""
+    def f(): ...
+    f"{<weak_warning descr="Type 'zip' doesn't define '__str__', '__repr__', or '__format__', so the result won't be useful">zip()</weak_warning>}"
+  """.trimIndent())
+
+  fun `test super`() = doTestByText("""
+    def f(): ...
+    f"{<weak_warning descr="Type 'super' doesn't define '__str__', '__repr__', or '__format__', so the result won't be useful">super()</weak_warning>}"
+  """.trimIndent())
+
+  fun `test str call without dunder methods`() = doTestByText("""
+    class A:
+        pass
+
+    str(<weak_warning descr="Type 'A' doesn't define '__str__' or '__repr__', so the result won't be useful">A()</weak_warning>)
+    """.trimIndent())
+
+  fun `test str call with dunder str`() = doTestByText("""
+    class A:
+        def __str__(self):
+            return "A instance"
+
+    str(A())
+    """.trimIndent())
+
+  fun `test str call with dunder repr`() = doTestByText("""
+    class A:
+        def __repr__(self):
+            return "A()"
+
+    str(A())
+    """.trimIndent())
+
+  fun `test str call with dunder format`() = doTestByText("""
+    class A:
+        def __format__(self, format_spec):
+            return f"A formatted with {format_spec}"
+
+    str(<weak_warning descr="Type 'A' doesn't define '__str__' or '__repr__', so the result won't be useful">A()</weak_warning>)
+    """.trimIndent())
+
+  fun `test format call without dunder methods`() = doTestByText("""
+    class A:
+        pass
+
+    format(<weak_warning descr="Type 'A' doesn't define '__str__', '__repr__', or '__format__', so the result won't be useful">A()</weak_warning>)
+    """.trimIndent())
+
+  fun `test format call with repr method`() = doTestByText("""
+    class A:
+        def __repr__(self): ...
+
+    format(A())
+    """.trimIndent())
+
+  fun `test format call with str method`() = doTestByText("""
+    class A:
+        def __str__(self): ...
+
+    format(A())
+    """.trimIndent())
+
+  fun `test inherited dunder methods`() = doTestByText("""
+    class Base:
+        def __str__(self):
+            return "Base"
+
+    class Derived(Base):
+        pass
+
+    str(Derived())
+    """.trimIndent())
+
+  fun `test should not warn for builtin types`() = doTestByText("""
+    # Should not warn for builtin types
+    str(42)
+    str([1, 2, 3])
+    str({"key": "value"})
+    str(None)
+    str(True)
+
+    format(42)
+    format("hello")
+    """.trimIndent())
+
+  fun `test should warn for type`() = doTestByText("""
+    str(<weak_warning descr="Type 'type' string value is not helpful">int</weak_warning>)
+    """.trimIndent())
+
+  fun `test repr call without dunder methods`() = doTestByText("""
+    class A:
+        pass
+
+    repr(<weak_warning descr="Type 'A' doesn't define '__repr__', so the result won't be useful">A()</weak_warning>)
+    """.trimIndent())
+
+  fun `test repr call with dunder repr`() = doTestByText("""
+    class A:
+        def __repr__(self):
+            return "A()"
+
+    repr(A())
+    """.trimIndent())
+
+  fun `test repr call with dunder str`() = doTestByText("""
+    class A:
+        def __str__(self):
+            return "A instance"
+
+    repr(<weak_warning descr="Type 'A' doesn't define '__repr__', so the result won't be useful">A()</weak_warning>)
+    """.trimIndent())
+
+  fun `test f-string without dunder methods`() = doTestByText("""
+    class A:
+        pass
+
+    f"{<weak_warning descr="Type 'A' doesn't define '__str__', '__repr__', or '__format__', so the result won't be useful">A()</weak_warning>}"
+    """.trimIndent())
+
+  fun `test f-string string`() = doTestByText("""
+    class A:
+        def __format__(self, format_spec): ...
+
+    f"{<weak_warning descr="Type 'A' doesn't define '__str__' or '__repr__', so the result won't be useful">A()</weak_warning>!s}"
+    """.trimIndent())
+
+  fun `test f-string repr`() = doTestByText("""
+    class A:
+        def __str__(self): ...
+        
+        def __format__(self, format_spec): ...
+
+    f"{<weak_warning descr="Type 'A' doesn't define '__repr__', so the result won't be useful">A()</weak_warning>!r}"
+    """.trimIndent())
+
+  fun `test f-string debug`() = doTestByText("""
+    class A:
+        def __str__(self): ...
+        
+        def __format__(self, format_spec): ...
+
+    f"{<weak_warning descr="Type 'A' doesn't define '__repr__', so the result won't be useful">A()</weak_warning>=}"
+    """.trimIndent())
+
+  fun `test f-string with dunder str`() = doTestByText("""
+    class A:
+        def __str__(self):
+            return "A instance"
+
+    f"{A()}"
+    """.trimIndent())
+
+  fun `test f-string with dunder repr`() = doTestByText("""
+    class A:
+        def __repr__(self):
+            return "A()"
+
+    f"{A()}"
+    """.trimIndent())
+
+  fun `test f-string with dunder format`() = doTestByText("""
+    class A:
+        def __format__(self):
+            return "A()"
+
+    f"{A()}"
+    """.trimIndent())
+
+  fun `test print call without dunder methods`() = doTestByText("""
+    class A:
+        pass
+
+    print(<weak_warning descr="Type 'A' doesn't define '__str__' or '__repr__', so the result won't be useful">A()</weak_warning>, <weak_warning descr="Type 'A' doesn't define '__str__' or '__repr__', so the result won't be useful">A()</weak_warning>)
+    """.trimIndent())
+
+  fun `test print call with dunder str`() = doTestByText("""
+    class A:
+        def __str__(self):
+            return "A instance"
+
+    print(A())
+    """.trimIndent())
+
+  fun `test print with keyword arguments`() = doTestByText("""
+    class A:
+        pass
+
+    print(
+        <weak_warning descr="Type 'A' doesn't define '__str__' or '__repr__', so the result won't be useful">A()</weak_warning>,
+        file=A(),
+    )
+    """.trimIndent())
+
+  fun `test union reports`() = doTestByText("""
+    class A:
+        def __str__(self): pass
+    
+    class B:
+        pass
+    
+    def f(ab: A | B):
+        str(<weak_warning descr="Type 'B' doesn't define '__str__' or '__repr__', so the result won't be useful">ab</weak_warning>)
+    """.trimIndent())
+
+  fun `test union doesn't report`() = doTestByText("""
+    class A:
+        def __str__(self): pass
+    
+    class B:
+        def __str__(self): pass
+    
+    def f(ab: A | B):
+        str(ab)
+    """.trimIndent())
+
+  fun `test derived from ignored`() = doTestByText("""
+    class A(int): ...
+    
+    str(A())    
+    """.trimIndent())
+}
