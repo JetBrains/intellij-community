02eb21d92b6785636d1cf78446b5ab36121d00d6
Evgeny.Terekhin <Evgeny.Terekhin@jetbrains.com>
2026-02-23 19:34:04 +0100

    [debugger-split] RIDER-134022. Fixed jump to statement feature for mixed mode in the split debugger mode.
    (cherry picked from commit d8f179a74512c645a63751739526e4b78b3a9bdd)
    
    GitOrigin-RevId: 11aa67b4c3bd70d2f9e4bc0fc8e79697142a9468

--- platform/xdebugger-impl/shared/src/com/intellij/xdebugger/impl/mixedmode/XMixedModeExecutionStackDescriptor.kt
+++ platform/xdebugger-impl/shared/src/com/intellij/xdebugger/impl/mixedmode/XMixedModeExecutionStackDescriptor.kt
@@ -1,8 +1,10 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.xdebugger.impl.mixedmode
 
+import com.intellij.platform.debugger.impl.rpc.XSourcePositionDto
 import com.intellij.xdebugger.frame.CustomXDescriptorSerializerProvider
 import com.intellij.xdebugger.frame.XDescriptor
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import org.jetbrains.annotations.ApiStatus
@@ -11,7 +13,11 @@ const val MIXED_MODE_EXECUTION_STACK_DESCRIPTOR_ID: String = "XMixedModeExecutio
 
 @ApiStatus.Internal
 @Serializable
-class XMixedModeExecutionStackDescriptor(val highStackDescriptor : XDescriptor?, val lowStackDescriptor: XDescriptor?) : XDescriptor {
+class XMixedModeExecutionStackDescriptor(
+  val highStackDescriptor: XDescriptor?,
+  val lowStackDescriptor: XDescriptor?,
+  val topFramePosition: CompletableDeferred<XSourcePositionDto?>,
+) : XDescriptor {
 
   override val kind: String = MIXED_MODE_EXECUTION_STACK_DESCRIPTOR_ID
 
