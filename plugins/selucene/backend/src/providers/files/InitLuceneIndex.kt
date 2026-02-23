db492a5ab70908b6c8375b690fc135bb31b8a1ff
Jakob Gerhardt <jakob.gerhardt@jetbrains.com>
2026-02-23 12:09:51 +0100

    IJPL-232764 remove unused imports
    GitOrigin-RevId: f858dfd6728c2eb7a62df48f8a037f6868c180f3

--- plugins/selucene/backend/src/providers/files/InitLuceneIndex.kt
+++ plugins/selucene/backend/src/providers/files/InitLuceneIndex.kt
@@ -1,7 +1,5 @@
 package com.intellij.selucene.backend.providers.files
 
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.platform.backend.observation.Observation
