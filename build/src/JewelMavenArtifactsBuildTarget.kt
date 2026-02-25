e785a1851256d12809e19b12130be1e30e8c37c9
Nebojsa.Vuksic <nebojsa.vuksic@jetbrains.com>
2026-02-25 15:19:10 +0100

    [JEWEL-1274] Add script to publish Jewel Standalone to MavenLocal
    - Add publishJewelStandaloneToMavenLocal.cmd to build and install Jewel Standalone artifacts to ~/.m2/repository using the same Bazel target as TeamCity
    - Update JewelMavenArtifactsBuildTarget to support both Ultimate and Community repository layouts
    - Add Bazel target that invokes JewelMavenArtifactsBuildTarget
    
    Signed-off-by: Nebojsa.Vuksic <nebojsa.vuksic@jetbrains.com>
    
    closes https://github.com/JetBrains/intellij-community/pull/3440
    
    (cherry picked from commit c5c6386339706c11f7893dee0b830122fda92a82)
    
    # Conflicts:
    #	community/build/BUILD.bazel
    #	community/build/src/JewelMavenArtifactsBuildTarget.kt
    
    
    (cherry picked from commit 72511d48e0c543f73a47111399b60db5ae5f729a)
    
    IJ-MR-193916
    
    GitOrigin-RevId: 9d83a753c89d5b2576ae5f25497348a8a5efad36

--- build/src/JewelMavenArtifactsBuildTarget.kt
+++ build/src/JewelMavenArtifactsBuildTarget.kt
@@ -3,7 +3,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.intellij.build.BuildPaths.Companion.COMMUNITY_ROOT
 import org.jetbrains.intellij.build.BuildPaths.Companion.ULTIMATE_HOME
-import org.jetbrains.intellij.build.CompilationTasks
 import org.jetbrains.intellij.build.IdeaCommunityProperties
 import org.jetbrains.intellij.build.JewelMavenArtifacts
 import org.jetbrains.intellij.build.impl.BuildContextImpl
@@ -15,22 +14,16 @@ import kotlin.io.path.name
 @OptIn(ExperimentalPathApi::class)
 internal object JewelMavenArtifactsBuildTarget {
   @JvmStatic
-  fun main(args: Array<String>) {
-    runBlocking(Dispatchers.Default) {
-      val context = BuildContextImpl.createContext(
-        projectHome = ULTIMATE_HOME,
-        productProperties = IdeaCommunityProperties(COMMUNITY_ROOT.communityRoot),
-      )
-      CompilationTasks.create(context).compileModules(JewelMavenArtifacts.ALL_MODULES)
-      val builder = MavenArtifactsBuilder(context)
-      val outputDir = context.paths.artifactDir.resolve("maven-artifacts")
-      outputDir.deleteRecursively()
-      builder.generateMavenArtifacts(
-        JewelMavenArtifacts.ALL_MODULES,
-        outputDir = outputDir.name,
-        validate = true,
-      )
-      context.notifyArtifactBuilt(outputDir)
-    }
+  fun main(args: Array<String>) = runBlocking(Dispatchers.Default) {
+    val context = BuildContextImpl.createContext(
+      projectHome = ULTIMATE_HOME,
+      productProperties = IdeaCommunityProperties(COMMUNITY_ROOT.communityRoot),
+    )
+    context.compileModules(JewelMavenArtifacts.ALL_MODULES)
+    val builder = MavenArtifactsBuilder(context)
+    val outputDir = context.paths.artifactDir.resolve("maven-artifacts")
+    outputDir.deleteRecursively()
+    builder.generateMavenArtifacts(moduleNamesToPublish = JewelMavenArtifacts.ALL_MODULES, outputDir = outputDir.name, validate = true)
+    context.notifyArtifactBuilt(outputDir)
   }
 }
