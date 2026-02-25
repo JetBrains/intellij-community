1ffa80857e214e5eaeb1ba5e106a021218af8c31
Kate Botsman <kate.botsman@jetbrains.com>
2026-02-25 17:39:39 +0100

    IJPL-236470 include Daemon logs directory into IDE logs archive
    (cherry-picked from commit e968429728e9184c188d007d77c97fc76ede1de2)
    
    IJ-CR-194269 IJ-CR-193859
    
    GitOrigin-RevId: 63ee8b2e1562c50de6c67c593fe83e15dbd6bb84

--- platform/platform-impl/src/com/intellij/ide/logsUploader/DefaultLogsProcessor.kt
+++ platform/platform-impl/src/com/intellij/ide/logsUploader/DefaultLogsProcessor.kt
@@ -3,8 +3,22 @@ package com.intellij.ide.logsUploader
 
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.project.Project
+import java.nio.file.Files
+import java.nio.file.Path
 
-class DefaultLogsProcessor: LogProvider {
-  override fun getAdditionalLogFiles(project: Project?): List<LogProvider.LogsEntry> =
-    listOf(LogProvider.LogsEntry("", listOf(PathManager.getLogDir())))
+class DefaultLogsProcessor : LogProvider {
+  override fun getAdditionalLogFiles(project: Project?): List<LogProvider.LogsEntry> {
+    val ideLogDir = PathManager.getLogDir()
+    val daemonLogDir = findDaemonLogDir()
+
+    return listOfNotNull(
+      LogProvider.LogsEntry("", listOf(ideLogDir)),
+      daemonLogDir?.let { LogProvider.LogsEntry("Daemon", listOf(it)) },
+    )
+  }
+
+  private fun findDaemonLogDir(): Path? {
+    val daemonLogDir = Path.of(PathManager.getDefaultLogPathFor("Daemon"))
+    return daemonLogDir.takeIf(Files::isDirectory)
+  }
 }
