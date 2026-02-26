9328216ced6bac5d2cc46cdff4f2e5a57589ef84
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2026-02-26 19:29:23 +0100

    IJPL-236789 threading: fix freeze in collectIntentionsFromDoNotShowLeveledInspections
    (cherry picked from commit dfb2c9015d0e9a9c786000405c92e790653e25c8)
    
    IJ-MR-193606
    
    GitOrigin-RevId: 5f9d6fa85d09b4b1763db9a86e0053e7c2a1dfad

--- platform/core-impl/src/com/intellij/concurrency/ConcurrencyUtils.java
+++ platform/core-impl/src/com/intellij/concurrency/ConcurrencyUtils.java
@@ -10,23 +10,31 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @ApiStatus.Internal
 public final class ConcurrencyUtils {
   private ConcurrencyUtils() { }
 
+  public static <T> T runWithIndicatorOrContextCancellation(@NotNull Function<? super @NotNull ProgressIndicator, ? extends T> action) {
+    return runWithIndicatorOrContextCancellation(EmptyProgressIndicator::new, action);
+  }
+
   /**
    * Executes {@code action} under {@link ProgressIndicator} that corresponds to the context {@link Cancellation#currentJob()}.
    * Otherwise, runs {@code action} under the installed {@link ProgressIndicatorProvider#getGlobalProgressIndicator()}.
    */
-  public static <T> T runWithIndicatorOrContextCancellation(@NotNull Function<? super ProgressIndicator, ? extends T> action) {
+  public static <T> T runWithIndicatorOrContextCancellation(
+    @NotNull Supplier<? extends @NotNull ProgressIndicator> defaultIndicatorSupplier,
+    @NotNull Function<? super @NotNull ProgressIndicator, ? extends T> action
+  ) {
     ProgressIndicator progressIndicator = ProgressIndicatorProvider.getGlobalProgressIndicator();
     //noinspection TestOnlyProblems
     if (progressIndicator == null && Cancellation.currentJob() != null) {
       return CoroutinesKt.blockingContextToIndicator(() -> action.apply(ProgressIndicatorProvider.getGlobalProgressIndicator()));
     }
     else {
-      ProgressIndicator actualIndicator = progressIndicator == null ? new EmptyProgressIndicator() : progressIndicator;
+      ProgressIndicator actualIndicator = progressIndicator == null ? defaultIndicatorSupplier.get() : progressIndicator;
       return action.apply(actualIndicator);
     }
   }
