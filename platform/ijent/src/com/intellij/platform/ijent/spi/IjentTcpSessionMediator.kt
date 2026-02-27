919cbd0d0415fe24cb86f436dbc6359cb5e7d5b4
Vyacheslav Karpukhin <Vyacheslav.Karpukhin@jetbrains.com>
2026-02-27 17:49:14 +0100

    IJPL-239842 Reconnection in TCP Ijent: support SSH reconnect with Gateway
    GitOrigin-RevId: ce259262c1a95492c11fb7fa20b2777e8580c1f2

--- platform/ijent/src/com/intellij/platform/ijent/spi/IjentTcpSessionMediator.kt
+++ platform/ijent/src/com/intellij/platform/ijent/spi/IjentTcpSessionMediator.kt
@@ -1,10 +1,12 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.ijent.spi
 
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 
 data class IjentTcpSessionMediator(
   override val ijentProcessScope: CoroutineScope,
   override val processExit: Deferred<Unit>,
+  val remotePid: CompletableDeferred<Long> = CompletableDeferred(),
 ) : IjentSessionMediator
