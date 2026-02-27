84ef8f108fc908d37668f54a50337867044dd601
Kajus Kuncaitis <kajus.kuncaitis@jetbrains.com>
2026-02-27 17:10:05 +0100

    [gradle][IDEA-385370] Don't trigger auto completion popups on closing quotes in `.gradle.kts`
    GitOrigin-RevId: ede2a4e3b1ed22eee03d73922eb1d77bf3bcb299

--- plugins/gradle/completion/kotlin/src/KotlinGradleDependenciesAutoPopupHandler.kt
+++ plugins/gradle/completion/kotlin/src/KotlinGradleDependenciesAutoPopupHandler.kt
@@ -5,19 +5,23 @@ import com.intellij.codeInsight.AutoPopupController
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.elementType
+import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.plugins.gradle.util.useDependencyCompletionService
 
 internal class KotlinGradleDependenciesAutoPopupHandler : TypedHandlerDelegate() {
   override fun checkAutoPopup(charTyped: Char, project: Project, editor: Editor, file: PsiFile): Result {
     if (!useDependencyCompletionService()) return Result.CONTINUE
-    if (!FileUtilRt.extensionEquals(file.name, "gradle.kts") || charTyped != '"') return Result.CONTINUE
+    if (!file.name.endsWith(".gradle.kts") || charTyped != '"') return Result.CONTINUE
 
     val offset = editor.caretModel.offset
+    val element = file.findElementAt(offset) ?: return Result.CONTINUE
+    if (!insideScriptBlockPattern(DEPENDENCIES).accepts(element)) return Result.CONTINUE
+    if (element.elementType == KtTokens.CLOSING_QUOTE || element.elementType == KtTokens.REGULAR_STRING_PART) return Result.CONTINUE
+
     AutoPopupController.getInstance(project).scheduleAutoPopup(editor) { psiFile ->
       val element = psiFile.findElementAt(offset) ?: return@scheduleAutoPopup false
-      if (!insideScriptBlockPattern(DEPENDENCIES).accepts(element)) return@scheduleAutoPopup false
       element.isSingleDependencyArgument()
       || element.isDependencyArgument(exclude)
       || element.isPositionalOrNamedDependencyArgument()
