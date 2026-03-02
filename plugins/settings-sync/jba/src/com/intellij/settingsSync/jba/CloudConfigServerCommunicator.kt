f5fadf63f15934a2d5773b6d8078ac8168d9dcd9
Ilia Epelbaum <ilia.epelbaum@jetbrains.com>
2026-03-02 09:55:42 +0200

    DBE-25499: Handle UnknownHostException in CloudConfigServerCommunicator and add details action to the error balloon
    Merge-request: IJ-CR-196311
    (cherry picked from commit 1a310ab177f6d49c9044011ba0a5fb8fa2ed55ba)
    
    GitOrigin-RevId: f6291c0e6cbfa3b6ddc5f07d31ab3aedf129fb0f

--- plugins/settings-sync/jba/src/com/intellij/settingsSync/jba/CloudConfigServerCommunicator.kt
+++ plugins/settings-sync/jba/src/com/intellij/settingsSync/jba/CloudConfigServerCommunicator.kt
@@ -24,6 +24,7 @@ import java.io.FileNotFoundException
 import java.io.IOException
 import java.io.InputStream
 import java.net.URI
+import java.net.UnknownHostException
 import java.net.http.HttpResponse
 import java.util.concurrent.atomic.AtomicReference
 
@@ -103,7 +104,20 @@ open class CloudConfigServerCommunicator(private val serverUrl: String?, private
     else {
       LOG.error(e)
     }
-    return e.message ?: defaultMessage
+    return customizeErrorMessage(e) ?: defaultMessage
+  }
+
+  private fun customizeErrorMessage(e: Throwable): String? {
+    if (e is UnknownHostException) {
+      val message = e.message
+      if (message != null) {
+        return SettingsSyncJbaBundle.message("unknown.host.error.message.with.additional.message", message)
+      }
+
+      return SettingsSyncJbaBundle.message("unknown.host.error.message")
+    }
+
+    return e.message
   }
 
   fun downloadSnapshot(filePath: String, version: FileVersionInfo): InputStream? {
