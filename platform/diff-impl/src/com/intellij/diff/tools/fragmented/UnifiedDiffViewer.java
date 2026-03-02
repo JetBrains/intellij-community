941ec2daded7ecd53c186e076a8b8e6d9f62349f
Aleksey Pivovarov <aleksey.pivovarov@jetbrains.com>
2026-03-02 16:34:28 +0100

    [diff] IJPL-235464 remove DocumentListener after DiffViewer disposal
    Fix memory leak via UndoManager.
    
    (cherry picked from commit 3ae943b22ea41a407af662a2a59d12994e0425e7)
    
    GitOrigin-RevId: 2636a8be9aa87e2dba1ae9fb86cc6c462fd016a9

--- platform/diff-impl/src/com/intellij/diff/tools/fragmented/UnifiedDiffViewer.java
+++ platform/diff-impl/src/com/intellij/diff/tools/fragmented/UnifiedDiffViewer.java
@@ -285,7 +285,7 @@ public class UnifiedDiffViewer extends ListenerDiffViewerBase implements EditorD
     EditorActionManager.getInstance().setReadonlyFragmentModificationHandler(myDocument, new MyReadonlyFragmentModificationHandler());
     myDocument.putUserData(UndoManager.ORIGINAL_DOCUMENT, getDocument(myMasterSide)); // use undo of master document
 
-    myDocument.addDocumentListener(new MyOnesideDocumentListener());
+    myDocument.addDocumentListener(new MyOnesideDocumentListener(), this);
   }
 
   @Override
