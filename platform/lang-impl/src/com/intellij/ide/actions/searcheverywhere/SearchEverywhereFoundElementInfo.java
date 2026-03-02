7f8755c10cfdb781304b08e7be7468089f0a3f2e
Adam Malek <adam.malek@jetbrains.com>
2026-03-02 11:58:41 +0100

    cleanup SE: Remove unused import
    GitOrigin-RevId: 5380867a36cd9a607140e732ba58378ba1de7255

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereFoundElementInfo.java
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereFoundElementInfo.java
@@ -2,7 +2,6 @@
 package com.intellij.ide.actions.searcheverywhere;
 
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Comparator;
