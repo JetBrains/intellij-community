d72a0b0e4e46d4c23ab2be2874a15d4a4e62c725
Morgan Bartholomew <morgan.bartholomew@jetbrains.com>
2026-03-03 13:45:15 +1000

    refactor python: j2k `PyTupleExpressionImpl`
    GitOrigin-RevId: c39511add7e7172eb21cba4820b0c7b01dffd3bb

--- python/python-psi-impl/src/com/jetbrains/python/psi/impl/PyTupleExpressionImpl.kt
+++ python/python-psi-impl/src/com/jetbrains/python/psi/impl/PyTupleExpressionImpl.kt
@@ -1,45 +1,37 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.jetbrains.python.psi.impl;
+package com.jetbrains.python.psi.impl
 
-import com.intellij.lang.ASTNode;
-import com.intellij.util.containers.ContainerUtil;
-import com.jetbrains.python.psi.LanguageLevel;
-import com.jetbrains.python.psi.PyElementGenerator;
-import com.jetbrains.python.psi.PyElementVisitor;
-import com.jetbrains.python.psi.PyExpression;
-import com.jetbrains.python.psi.PyParenthesizedExpression;
-import com.jetbrains.python.psi.PyTupleExpression;
-import com.jetbrains.python.psi.types.PyTupleType;
-import com.jetbrains.python.psi.types.PyType;
-import com.jetbrains.python.psi.types.TypeEvalContext;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.lang.ASTNode
+import com.jetbrains.python.psi.LanguageLevel
+import com.jetbrains.python.psi.PyElementGenerator
+import com.jetbrains.python.psi.PyElementVisitor
+import com.jetbrains.python.psi.PyParenthesizedExpression
+import com.jetbrains.python.psi.PyTupleExpression
+import com.jetbrains.python.psi.types.PyTupleType
+import com.jetbrains.python.psi.types.PyType
+import com.jetbrains.python.psi.types.TypeEvalContext
 
-
-public class PyTupleExpressionImpl extends PySequenceExpressionImpl implements PyTupleExpression {
-  public PyTupleExpressionImpl(ASTNode astNode) {
-    super(astNode);
+class PyTupleExpressionImpl(astNode: ASTNode) : PySequenceExpressionImpl(astNode), PyTupleExpression {
+  override fun acceptPyVisitor(pyVisitor: PyElementVisitor) {
+    pyVisitor.visitPyTupleExpression(this)
   }
 
-  @Override
-  protected void acceptPyVisitor(PyElementVisitor pyVisitor) {
-    pyVisitor.visitPyTupleExpression(this);
+  override fun getType(context: TypeEvalContext, key: TypeEvalContext.Key): PyType? {
+    return PyTupleType.create(
+      this,
+      elements.map { context.getType(it) }
+    )
   }
 
-  @Override
-  public PyType getType(@NotNull TypeEvalContext context, @NotNull TypeEvalContext.Key key) {
-    return PyTupleType.create(this, ContainerUtil.map(getElements(), context::getType));
-  }
-
-  @Override
-  public void deleteChildInternal(@NotNull ASTNode child) {
-    super.deleteChildInternal(child);
-    final PyExpression[] children = getElements();
-    final PyElementGenerator generator = PyElementGenerator.getInstance(getProject());
-    if (children.length == 1 && PyPsiUtils.getNextComma(children[0]) == null) {
-      addAfter(generator.createComma().getPsi(), children[0]);
+  override fun deleteChildInternal(child: ASTNode) {
+    super.deleteChildInternal(child)
+    val children = elements
+    val generator = PyElementGenerator.getInstance(project)
+    if (children.size == 1 && PyPsiUtils.getNextComma(children.single()) == null) {
+      addAfter(generator.createComma().psi, children.single())
     }
-    else if (children.length == 0 && !(getParent() instanceof PyParenthesizedExpression)) {
-      replace(generator.createExpressionFromText(LanguageLevel.forElement(this), "()"));
+    else if (children.isEmpty() && parent !is PyParenthesizedExpression) {
+      replace(generator.createExpressionFromText(LanguageLevel.forElement(this), "()"))
     }
   }
 }
