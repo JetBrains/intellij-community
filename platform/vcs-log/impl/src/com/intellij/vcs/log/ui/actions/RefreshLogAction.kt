5e231c90717aeab3d29dded53cbddb81311d29a1
Ilia.Shulgin <ilia.shulgin@jetbrains.com>
2026-03-03 09:32:59 +0100

    refactoring [log]: Convert RefreshLogAction to Kotlin
    GitOrigin-RevId: c20f3770c7d71977b055d392ba7db7ecfcb63b3c

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/RefreshLogAction.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/RefreshLogAction.kt
@@ -1,87 +1,73 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.vcs.log.ui.actions;
+package com.intellij.vcs.log.ui.actions
 
-import com.intellij.ide.actions.RefreshAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.diagnostic.Attachment;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager;
-import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.ui.content.TabbedContent;
-import com.intellij.vcs.log.VcsLogDataKeys;
-import com.intellij.vcs.log.VcsLogUi;
-import com.intellij.vcs.log.impl.VcsLogManager;
-import com.intellij.vcs.log.statistics.VcsLogUsageTriggerCollector;
-import com.intellij.vcs.log.ui.VcsLogInternalDataKeys;
-import com.intellij.vcs.log.ui.VcsLogUiEx;
-import com.intellij.vcs.log.util.VcsLogUtil;
-import com.intellij.vcs.log.visible.VisiblePackRefresher;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.ide.actions.RefreshAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.diagnostic.Attachment
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.ui.content.TabbedContent
+import com.intellij.vcs.log.VcsLogDataKeys
+import com.intellij.vcs.log.impl.VcsLogManager
+import com.intellij.vcs.log.statistics.VcsLogUsageTriggerCollector
+import com.intellij.vcs.log.ui.VcsLogInternalDataKeys
+import com.intellij.vcs.log.ui.VcsLogUiEx
+import com.intellij.vcs.log.util.VcsLogUtil
 
-import java.util.ArrayList;
-import java.util.List;
+private val LOG = logger<RefreshLogAction>()
 
-@ApiStatus.Internal
-public final class RefreshLogAction extends RefreshAction {
-  private static final Logger LOG = Logger.getInstance(RefreshLogAction.class);
+internal class RefreshLogAction : RefreshAction() {
+  override fun actionPerformed(e: AnActionEvent) {
+    VcsLogUsageTriggerCollector.triggerUsage(e, this)
 
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
-    VcsLogUsageTriggerCollector.triggerUsage(e, this);
-
-    VcsLogManager logManager = e.getData(VcsLogInternalDataKeys.LOG_MANAGER);
-    if (logManager == null) return;
-    VcsLogUi ui = e.getData(VcsLogDataKeys.VCS_LOG_UI);
-    if (ui == null) return;
+    val logManager = e.getData(VcsLogInternalDataKeys.LOG_MANAGER) ?: return
+    val ui = e.getData(VcsLogDataKeys.VCS_LOG_UI) ?: return
 
     // diagnostic for possible refresh problems
-    if (ui instanceof VcsLogUiEx) {
-      VisiblePackRefresher refresher = ((VcsLogUiEx)ui).getRefresher();
-      if (!refresher.isValid()) {
-        String message = "Trying to refresh invalid log tab '" + ui.getId() + "'.";
-        if (!logManager.getDataManager().getProgress().isRunning()) {
-          LOG.error(message, collectDiagnosticInformation(e.getProject(), logManager));
+    if (ui is VcsLogUiEx) {
+      val refresher = ui.refresher
+      if (!refresher.isValid) {
+        val message = "Trying to refresh invalid log tab '${ui.id}'."
+        if (!logManager.dataManager.progress.isRunning) {
+          LOG.error(message, *collectDiagnosticInformation(e.project, logManager))
         }
         else {
-          LOG.warn(message);
+          LOG.warn(message)
         }
-        refresher.setValid(true, false);
+        refresher.setValid(true, false)
       }
     }
 
-    logManager.getDataManager().refresh(VcsLogUtil.getVisibleRoots(ui));
+    logManager.dataManager.refresh(VcsLogUtil.getVisibleRoots(ui))
   }
 
-  @Override
-  public void update(@NotNull AnActionEvent e) {
-    VcsLogManager logManager = e.getData(VcsLogInternalDataKeys.LOG_MANAGER);
-    e.getPresentation().setEnabledAndVisible(logManager != null && e.getData(VcsLogDataKeys.VCS_LOG_UI) != null);
+  override fun update(e: AnActionEvent) {
+    val logManager = e.getData(VcsLogInternalDataKeys.LOG_MANAGER)
+    e.presentation.isEnabledAndVisible = logManager != null && e.getData(VcsLogDataKeys.VCS_LOG_UI) != null
   }
+}
 
-  private static Attachment @NotNull [] collectDiagnosticInformation(@Nullable Project project, @NotNull VcsLogManager logManager) {
-    List<Attachment> result = new ArrayList<>();
-    result.add(new Attachment("log-windows.txt", "Log Windows:\n" + logManager.getLogUiInformation()));
+private fun collectDiagnosticInformation(project: Project?, logManager: VcsLogManager): Array<Attachment> {
+  val result = mutableListOf<Attachment>()
+  result.add(Attachment("log-windows.txt", "Log Windows:\n${logManager.getLogUiInformation()}"))
 
-    if (project != null) {
-      ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID);
-      if (toolWindow != null) {
-        String contentDump = StringUtil.join(toolWindow.getContentManager().getContents(), content -> {
-          if (content instanceof TabbedContent) {
-            return content + ", tabs=[" +
-                   StringUtil.join(((TabbedContent)content).getTabs(), pair -> pair.first, ", ") + "]";
-          }
-          return content.toString();
-        }, "\n");
-        result.add(new Attachment("vcs-tool-window-content.txt",
-                                  "Tool Window " + toolWindow.getTitle() + " (" + toolWindow.getType() + "):\n" + contentDump));
+  if (project != null) {
+    val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID)
+    if (toolWindow != null) {
+      val contentDump = toolWindow.contentManager.contents.joinToString("\n") { content ->
+        if (content is TabbedContent) {
+          "$content, tabs=[${content.tabs.joinToString(", ") { it.first }}]"
+        }
+        else {
+          content.toString()
+        }
       }
+      result.add(Attachment("vcs-tool-window-content.txt",
+                            "Tool Window ${toolWindow.title} (${toolWindow.type}):\n$contentDump"))
     }
-
-    return result.toArray(Attachment.EMPTY_ARRAY);
   }
+
+  return result.toTypedArray()
 }
