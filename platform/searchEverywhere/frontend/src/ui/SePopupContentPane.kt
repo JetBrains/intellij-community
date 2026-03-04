10ffbf0ca9cb1a12be157b09c8d257dcec199041
Sergei Kharitontcev-Beglov <sergei.kharitontcev-beglov@jetbrains.com>
2026-03-04 12:01:29 +0100

    IJPL-237895 search-everywhere: Use KeepAlive in the document right before the item is selected
    This commit fixes a bug when a rapid editor close on the frontend and navigation on the backend could conflict and skip editor to be opened.
    
    This commit introduced the Rider's keep alive workaround for same problem
    
    (cherry-picked from f4144e1819baa6f30d25a8d38cfdebe3d8df06e0)
    
    
    (cherry picked from commit 5ed488db9b341dd48a8f6421e54e931a214ba548)
    
    IJ-MR-195270
    
    GitOrigin-RevId: 7ae16f504516391e08c4ed06b4acf8501ad00ae9

--- platform/searchEverywhere/frontend/src/ui/SePopupContentPane.kt
+++ platform/searchEverywhere/frontend/src/ui/SePopupContentPane.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.searchEverywhere.frontend.ui
 
 import com.intellij.icons.AllIcons
@@ -1073,6 +1073,7 @@ class SePopupContentPane(
       }
 
       override fun onEditorCreated(editor: Editor) {
+        SePreviewPanelListener.EP.forEachExtensionSafe { it.onNewPreviewEditor(editor) }
         if (editor is EditorEx) {
           editor.setRendererMode(true)
         }
