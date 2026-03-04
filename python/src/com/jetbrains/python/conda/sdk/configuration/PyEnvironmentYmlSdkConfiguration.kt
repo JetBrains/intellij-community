7ae0adaeb1d29004798ec20abd4d33e38961613b
Vitaly Legchilkin <vitaly.legchilkin@jetbrains.com>
2026-03-04 17:16:12 +0100

    PY-87695 Fix AssertionError when using existing conda venv as WSL interpreter
    Change createCondaSdkFromExistingEnv to return PyResult<Sdk> instead of
    throwing AssertionError on failure. The error was already shown to the user
    via ShowingMessageErrorSync but then re-thrown, causing an unhandled crash.
    Now errors propagate via PyResult through all callers.
    
    (cherry picked from commit 9db24bf72d06d06508435af0dbcbde659c6c94f9)
    
    
    (cherry picked from commit 30fa2d561ba2f27a6f59066c60f7e9e7d481fbf9)
    
    IJ-MR-194701
    
    GitOrigin-RevId: fb612d2c0035fa3b40a1bd0cdbb4fa19d7c0736d

--- python/src/com/jetbrains/python/conda/sdk/configuration/PyEnvironmentYmlSdkConfiguration.kt
+++ python/src/com/jetbrains/python/conda/sdk/configuration/PyEnvironmentYmlSdkConfiguration.kt
@@ -32,7 +32,7 @@ import com.jetbrains.python.sdk.PythonSdkUpdater
 import com.jetbrains.python.sdk.baseDir
 import com.jetbrains.python.sdk.conda.PyCondaSdkCustomizer
 import com.jetbrains.python.sdk.conda.createCondaSdkAlongWithNewEnv
-import com.jetbrains.python.sdk.conda.createCondaSdkFromExistingEnv
+import com.jetbrains.python.sdk.conda.createCondaSdkFromExistingEnvironment
 import com.jetbrains.python.sdk.conda.execution.CondaExecutor
 import com.jetbrains.python.sdk.conda.suggestCondaPath
 import com.jetbrains.python.sdk.configuration.CONDA_TOOL_ID
@@ -149,12 +149,12 @@ internal class PyEnvironmentYmlSdkConfiguration : PyProjectSdkConfigurationExten
 
   private suspend fun useExistingCondaEnv(module: Module, condaExecutable: String): PyResult<Sdk> {
     val project = module.project
-    return PyResult.success(PyCondaCommand(condaExecutable, null).createCondaSdkFromExistingEnv(
+    return PyCondaCommand(condaExecutable, null).createCondaSdkFromExistingEnvironment(
       getCondaEnvIdentity(module, condaExecutable)
       ?: return PyResult.localizedError(PyBundle.message("sdk.cannot.use.existing.conda.environment")),
       PyConfigurableInterpreterList.getInstance(project).model.sdks.toList(),
       project
-    ))
+    )
   }
 
   private suspend fun getCondaEnvIdentity(module: Module, condaExecutable: String): PyCondaEnvIdentity? {
