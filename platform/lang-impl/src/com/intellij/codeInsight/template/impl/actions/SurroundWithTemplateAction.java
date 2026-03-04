925e5100aa6f2cfd2a859874bf5c46524284a236
Alexandr Evstigneev <alexandr.evstigneev@jetbrains.com>
2026-03-04 10:14:03 +0400

    IJPL-237486 Moved surround with tag action to the common part
    There is no dedicated module besides UI, so I did move it to the emmet module which is shared and related to the live templates
    
    GitOrigin-RevId: 420083eeaf8e07395c30f73451fd4b22cf7a21cd

--- platform/lang-impl/src/com/intellij/codeInsight/template/impl/actions/SurroundWithTemplateAction.java
+++ platform/lang-impl/src/com/intellij/codeInsight/template/impl/actions/SurroundWithTemplateAction.java
@@ -1,13 +1,14 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package com.intellij.codeInsight.template.impl.actions;
 
 import com.intellij.codeInsight.CodeInsightActionHandler;
 import com.intellij.codeInsight.actions.BaseCodeInsightAction;
 import com.intellij.codeInsight.template.impl.SurroundWithTemplateHandler;
+import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification;
 import org.jetbrains.annotations.NotNull;
 
-public class SurroundWithTemplateAction extends BaseCodeInsightAction {
+public class SurroundWithTemplateAction extends BaseCodeInsightAction implements ActionRemoteBehaviorSpecification.Frontend {
   @Override
   protected @NotNull CodeInsightActionHandler getHandler() {
     return new SurroundWithTemplateHandler();
