a5972dedc6035a5a163bf0f5e530bcbde6acd2bf
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-03-04 10:46:33 +0100

    [maven] IDEA-386652 Migrate Maven Plugin descriptors to V2: proofreading module
    GitOrigin-RevId: 04c1769c2e1963c7d7569ca9e5b77e403ef2d725

--- plugins/maven/proofreading/src/MavenProblemFilter.java
+++ plugins/maven/proofreading/src/MavenProblemFilter.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.idea.maven.proofreading;
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.maven.proofreading;
 
 import com.intellij.grazie.text.ProblemFilter;
 import com.intellij.grazie.text.TextContent;
