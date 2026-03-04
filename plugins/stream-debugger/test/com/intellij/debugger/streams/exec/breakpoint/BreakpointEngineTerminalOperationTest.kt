67839391ce6b2c0638d1922368e7bfde86eee045
Shumaf Lovpache <shumaf.lovpache@jetbrains.com>
2026-03-04 16:27:47 +0200

    tests stream debugger: add terminal operation tests for new engine
    GitOrigin-RevId: ed26d2a541b459318ce95af84d848290c516a2b2

--- plugins/stream-debugger/test/com/intellij/debugger/streams/exec/breakpoint/BreakpointEngineTerminalOperationTest.kt
+++ plugins/stream-debugger/test/com/intellij/debugger/streams/exec/breakpoint/BreakpointEngineTerminalOperationTest.kt
@@ -2,9 +2,13 @@
 package com.intellij.debugger.streams.exec.breakpoint
 
 import com.intellij.debugger.streams.core.testFramework.TraceExecutionTestHelper
-import com.intellij.debugger.streams.exec.TerminalOperationTest
+import com.intellij.debugger.streams.test.TraceExecutionTestCase
 import com.intellij.xdebugger.XDebugSession
 
-class BreakpointEngineTerminalOperationTest : TerminalOperationTest() {
+class BreakpointEngineTerminalOperationTest : TraceExecutionTestCase() {
   override fun getHelper(session: XDebugSession): TraceExecutionTestHelper = breakpointEngineHelper(session)
+
+  fun testForEachNoExtraCalls(): Unit = doTest(true)
+
+  fun testForEachOrderedNoExtraCalls(): Unit = doTest(true)
 }
