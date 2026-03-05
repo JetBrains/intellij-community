2a48dbac14c8fad73afc6279cc62b41868b94785
Matthew Gharrity <gharrma@google.com>
2026-03-04 16:54:45 -0800

    Run AndroidStudioBuildTarget in Bazel
    Now we build the distro in Bazel instead of JPS!
    
    Also, remove the --incremental flag from build_studio.sh since Bazel
    is already incremental by default.
    
    See also Change Ic4595eccb.
    
    Bug: 490117560
    Change-Id: I07df760cbf6a4eb309d423c5e960059131938ed7

--- build_studio.sh
+++ build_studio.sh
@@ -6,8 +6,7 @@ set -e
 PROG_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 function die() {
-  echo "$*" > /dev/stderr
-  echo "Usage: $0 [--incremental]" > /dev/stderr
+  echo "ERROR: $*" > /dev/stderr
   exit 1
 }
 
@@ -17,15 +16,13 @@ DIST="${DIST_DIR:-"${OUT}/dist"}"
 mkdir -p "$OUT"
 mkdir -p "$DIST"
 
-INCREMENTAL=false
-while [[ $# -gt 0 ]]; do
+if [[ $# -gt 0 ]]; then
   if [[ $1 = "--incremental" ]]; then
-    INCREMENTAL=true
+    die "Passing --incremental is redundant because the platform is now built with Bazel"
   else
-    die "[$0] Unknown parameter: $1"
+    die "Unknown parameter: $1"
   fi
-  shift
-done
+fi
 
 # Build the Kotlin compiler, which is a dependency of the Kotlin IDE plugin.
 "${PROG_DIR}/build_kotlinc.py"
@@ -37,12 +34,9 @@ declare -ar BUILD_PROPERTIES=(
   "-Dbuild.number=${AS_BUILD_NUMBER}"
   "-Dkotlin.plugin.kind=AS"
   "-Dintellij.build.dev.mode=false"
-  "-Dcompile.parallel=true"
   "-Dintellij.build.dmg.with.bundled.jre=false"
   "-Dintellij.build.dmg.without.bundled.jre=true"
   "-Dintellij.build.skip.build.steps=repair_utility_bundle_step,mac_dmg,mac_sign,mac_sit,windows_exe_installer,linux aarch64,windows aarch64"
-  "-Dintellij.build.incremental.compilation=${INCREMENTAL}"
-  "-Dintellij.build.incremental.compilation.fallback.rebuild=false"
   "-Dintellij.build.store.git.revision=false"
   # Set the "major-version release date" to nil. This field is unused in
   # Android Studio, but the platform build scripts complain if it is missing.
@@ -50,12 +44,13 @@ declare -ar BUILD_PROPERTIES=(
   "-Dintellij.build.override.application.version.majorReleaseDate=20000101"
 )
 
-"${PROG_DIR}/platform/jps-bootstrap/jps-bootstrap.sh" "${BUILD_PROPERTIES[@]}" "${PROG_DIR}" intellij.idea.community.build AndroidStudioBuildTarget
+"$PROG_DIR/build/run_build_target.sh" "$PROG_DIR" //build:i_build_target_studio "${BUILD_PROPERTIES[@]}"
 
 mkdir -p "$DIST"
 cp -Rfv "$OUT"/artifacts/android-studio* "$DIST"
 
 # Build the updater-full.jar
+# Eventually this should be built in Bazel, but the Bazel target is not ready yet as of IntelliJ 2026.1.
 (
     # Set JAVA_HOME to the one in prebuilts, which is needed to invoke the updater jar.
   JDK_DIR=$PROG_DIR/../../prebuilts/studio/jdk/jdk17
