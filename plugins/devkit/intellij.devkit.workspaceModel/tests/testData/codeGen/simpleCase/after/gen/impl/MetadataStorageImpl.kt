d99060e8203f36208c84b5632385f2bc29e350b4
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2026-03-06 12:04:34 +0100

    IJPL-14741 Add tests and update generator version
    GitOrigin-RevId: e7fb77afd18bc9737fd704f1d16de23c78ee7d9c

--- plugins/devkit/intellij.devkit.workspaceModel/tests/testData/codeGen/simpleCase/after/gen/impl/MetadataStorageImpl.kt
+++ plugins/devkit/intellij.devkit.workspaceModel/tests/testData/codeGen/simpleCase/after/gen/impl/MetadataStorageImpl.kt
@@ -14,15 +14,21 @@ override fun initializeMetadata(){
 val primitiveTypeIntNotNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = false, type = "Int")
 val primitiveTypeStringNotNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = false, type = "String")
 val primitiveTypeBooleanNotNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = false, type = "Boolean")
+val primitiveTypeCharNotNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = false, type = "Char")
+val primitiveTypeLongNotNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = false, type = "Long")
+val primitiveTypeFloatNotNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = false, type = "Float")
+val primitiveTypeDoubleNotNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = false, type = "Double")
+val primitiveTypeShortNotNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = false, type = "Short")
+val primitiveTypeByteNotNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = false, type = "Byte")
 
 var typeMetadata: StorageTypeMetadata
 
-typeMetadata = EntityMetadata(fqName = "com.intellij.workspaceModel.test.api.SimpleEntity", entityDataFqName = "com.intellij.workspaceModel.test.api.impl.SimpleEntityData", supertypes = listOf("com.intellij.platform.workspace.storage.WorkspaceEntity"), properties = listOf(OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "entitySource", valueType = ValueTypeMetadata.SimpleType.CustomType(isNullable = false, typeMetadata = FinalClassMetadata.KnownClass(fqName = "com.intellij.platform.workspace.storage.EntitySource")), withDefault = false),OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "version", valueType = primitiveTypeIntNotNullable, withDefault = false),OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "name", valueType = primitiveTypeStringNotNullable, withDefault = false),OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "isSimple", valueType = primitiveTypeBooleanNotNullable, withDefault = false)), extProperties = listOf(), isAbstract = false)
+typeMetadata = EntityMetadata(fqName = "com.intellij.workspaceModel.test.api.SimpleEntity", entityDataFqName = "com.intellij.workspaceModel.test.api.impl.SimpleEntityData", supertypes = listOf("com.intellij.platform.workspace.storage.WorkspaceEntity"), properties = listOf(OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "entitySource", valueType = ValueTypeMetadata.SimpleType.CustomType(isNullable = false, typeMetadata = FinalClassMetadata.KnownClass(fqName = "com.intellij.platform.workspace.storage.EntitySource")), withDefault = false),OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "version", valueType = primitiveTypeIntNotNullable, withDefault = false),OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "name", valueType = primitiveTypeStringNotNullable, withDefault = false),OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "isSimple", valueType = primitiveTypeBooleanNotNullable, withDefault = false),OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "char", valueType = primitiveTypeCharNotNullable, withDefault = false),OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "long", valueType = primitiveTypeLongNotNullable, withDefault = false),OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "float", valueType = primitiveTypeFloatNotNullable, withDefault = false),OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "double", valueType = primitiveTypeDoubleNotNullable, withDefault = false),OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "short", valueType = primitiveTypeShortNotNullable, withDefault = false),OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "byte", valueType = primitiveTypeByteNotNullable, withDefault = false)), extProperties = listOf(), isAbstract = false)
 
 addMetadata(typeMetadata)
 }
 
 override fun initializeMetadataHash(){
-addMetadataHash(typeFqn = "com.intellij.workspaceModel.test.api.SimpleEntity", metadataHash = 284369588)
+addMetadataHash(typeFqn = "com.intellij.workspaceModel.test.api.SimpleEntity", metadataHash = 1469705114)
 }
 }
