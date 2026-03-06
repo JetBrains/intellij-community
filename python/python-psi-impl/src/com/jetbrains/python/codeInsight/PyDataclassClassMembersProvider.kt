66447e514207cb34de6daa1c041f59a0d82aa766
Nikita Paniukhin <nikita.paniukhin@jetbrains.com>
2026-03-06 20:00:57 +0100

    [python] PY-85419 Convert `PyDataclassClassMembersProvider` to Kotlin
    GitOrigin-RevId: 2ae4d96600d6a0c16d9b9bb3514bb89884b2054b

--- python/python-psi-impl/src/com/jetbrains/python/codeInsight/PyDataclassClassMembersProvider.kt
+++ python/python-psi-impl/src/com/jetbrains/python/codeInsight/PyDataclassClassMembersProvider.kt
@@ -1,64 +1,55 @@
-// Copyright 2000-2025 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.jetbrains.python.codeInsight;
+// Copyright 2000-2026 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.jetbrains.python.codeInsight
 
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.util.CachedValueProvider;
-import com.intellij.psi.util.CachedValuesManager;
-import com.intellij.psi.util.PsiModificationTracker;
-import com.jetbrains.python.PyNames;
-import com.jetbrains.python.codeInsight.PyDataclassNames.Dataclasses;
-import com.jetbrains.python.psi.PyClass;
-import com.jetbrains.python.psi.impl.PyBuiltinCache;
-import com.jetbrains.python.psi.types.PyClassMembersProviderBase;
-import com.jetbrains.python.psi.types.PyClassType;
-import com.jetbrains.python.psi.types.PyTupleType;
-import com.jetbrains.python.psi.types.PyType;
-import com.jetbrains.python.psi.types.TypeEvalContext;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.Collection;
-
-import static com.jetbrains.python.codeInsight.PyDataclassesKt.parseDataclassParameters;
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
+import com.intellij.psi.util.PsiModificationTracker
+import com.jetbrains.python.PyNames
+import com.jetbrains.python.codeInsight.PyDataclassNames.Dataclasses
+import com.jetbrains.python.psi.impl.PyBuiltinCache
+import com.jetbrains.python.psi.types.PyClassMembersProviderBase
+import com.jetbrains.python.psi.types.PyClassType
+import com.jetbrains.python.psi.types.PyTupleType
+import com.jetbrains.python.psi.types.TypeEvalContext
 
 /**
  * Adds members for dataclass-like classes.
  */
-public final class PyDataclassClassMembersProvider extends PyClassMembersProviderBase {
+class PyDataclassClassMembersProvider : PyClassMembersProviderBase() {
 
-  @Override
-  public @NotNull Collection<PyCustomMember> getMembers(PyClassType clazz, PsiElement location, @NotNull TypeEvalContext context) {
-    PyClass pyClass = clazz.getPyClass();
-    PyDataclassParameters dataclassParameters = parseDataclassParameters(pyClass, context);
+  override fun getMembers(
+    clazz: PyClassType,
+    location: PsiElement?,
+    context: TypeEvalContext,
+  ): Collection<PyCustomMember> {
+    val pyClass = clazz.pyClass
+    val dataclassParameters = parseDataclassParameters(pyClass, context)
 
-    return CachedValuesManager.getCachedValue(pyClass, () -> {
-      Collection<PyCustomMember> result = new ArrayList<>();
+    return CachedValuesManager.getCachedValue(pyClass) {
+      val result = mutableListOf<PyCustomMember>()
 
       // Adds member __slots__ caused by dataclass.slots
-      if (dataclassParameters != null && dataclassParameters.getSlots()) {
-        PyBuiltinCache builtinCache = PyBuiltinCache.getInstance(pyClass);
-        PyType strType = builtinCache.getStrType();
-
+      if (dataclassParameters?.slots == true) {
+        val strType = PyBuiltinCache.getInstance(pyClass).strType
         if (strType != null) {
           // creating the following PyTupleType requires caching the result to avoid Idempotency Errors at runtime
-          PyTupleType tupleOfStrings = PyTupleType.createHomogeneous(pyClass, strType);
+          val tupleOfStrings = PyTupleType.createHomogeneous(pyClass, strType)
           if (tupleOfStrings != null) {
-            String qNameTuple = tupleOfStrings.getPyClass().getQualifiedName();
-            PyCustomMember slotsMember = new PyCustomMember(Dataclasses.DUNDER_SLOTS, qNameTuple, ignored -> tupleOfStrings);
-            result.add(slotsMember);
+            val qNameTuple = tupleOfStrings.pyClass.qualifiedName
+            result.add(PyCustomMember(Dataclasses.DUNDER_SLOTS, qNameTuple) { tupleOfStrings })
           }
         }
       }
 
       // Adds member __attrs_attrs__ caused by decorator @attrs.define
-      boolean hasAttrs = dataclassParameters != null && dataclassParameters.getType() == PyDataclassParameters.PredefinedType.ATTRS;
+      val hasAttrs = dataclassParameters?.type == PyDataclassParameters.PredefinedType.ATTRS
       if (hasAttrs) {
-        PyBuiltinCache builtinCache = PyBuiltinCache.getInstance(pyClass);
-        PyClass objectClass = builtinCache.getClass(PyNames.OBJECT);
-        result.add(new PyCustomMember("__attrs_attrs__", objectClass).asClassVar());
+        val objectClass = PyBuiltinCache.getInstance(pyClass).getClass(PyNames.OBJECT)
+        result.add(PyCustomMember("__attrs_attrs__", objectClass).asClassVar())
       }
 
-      return CachedValueProvider.Result.create(result, PsiModificationTracker.MODIFICATION_COUNT);
-    });
+      CachedValueProvider.Result.create(result, PsiModificationTracker.MODIFICATION_COUNT)
+    }
   }
 }
