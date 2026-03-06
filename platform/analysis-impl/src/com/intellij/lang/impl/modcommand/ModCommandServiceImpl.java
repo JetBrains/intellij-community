c9982eaeb66a92eab60128e722491995f005f81d
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2026-03-06 10:55:44 +0100

    [threading] IJPL-236240 Disable ReadActionCache in ModCommandService computations
    GitOrigin-RevId: 57f85b0c39a839d9d66c1f95f967dc482d564008

--- platform/analysis-impl/src/com/intellij/lang/impl/modcommand/ModCommandServiceImpl.java
+++ platform/analysis-impl/src/com/intellij/lang/impl/modcommand/ModCommandServiceImpl.java
@@ -32,6 +32,7 @@ import com.intellij.profile.codeInspection.InspectionProfileManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.impl.source.tree.injected.InjectedLanguageUtilBase;
+import com.intellij.psi.util.ReadActionCache;
 import com.intellij.util.concurrency.annotations.RequiresBackgroundThread;
 import org.jdom.Element;
 import org.jetbrains.annotations.ApiStatus;
@@ -136,6 +137,8 @@ public final class ModCommandServiceImpl implements ModCommandService {
     Project project = hostFile.getProject();
     ThrowableComputable<ModCommandWithContext, RuntimeException> computable =
       () -> ReadAction.nonBlocking(() -> {
+          ReadActionCache.getInstance().disable(); // this read action in fact modifies Document, do not try to cache its results
+
           ActionContext context = chooseContextForAction(hostFile, hostEditor, commandAction, fixOffset);
           if (context == null) {
             return new ModCommandWithContext(ActionContext.from(null, hostFile), ModCommand.nop());
