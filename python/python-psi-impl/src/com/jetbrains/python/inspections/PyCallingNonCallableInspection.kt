2f7804c37f82810b316a73a6c764469c9bf5c25d
Nikita Paniukhin <nikita.paniukhin@jetbrains.com>
2026-03-09 15:21:05 +0100

    [python] PY-85470 Convert `PyCallingNonCallableInspection` to Kotlin
    GitOrigin-RevId: 7811c2ee5b977bc81abf109a57b4ba601c871d10

--- python/python-psi-impl/src/com/jetbrains/python/inspections/PyCallingNonCallableInspection.kt
+++ python/python-psi-impl/src/com/jetbrains/python/inspections/PyCallingNonCallableInspection.kt
@@ -13,83 +13,66 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jetbrains.python.inspections;
+package com.jetbrains.python.inspections
 
-import com.intellij.codeInspection.LocalInspectionToolSession;
-import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.codeInspection.util.InspectionMessage;
-import com.intellij.psi.PsiElementVisitor;
-import com.jetbrains.python.PyNames;
-import com.jetbrains.python.PyPsiBundle;
-import com.jetbrains.python.inspections.quickfix.PyRemoveCallQuickFix;
-import com.jetbrains.python.psi.PyCallExpression;
-import com.jetbrains.python.psi.PyDecorator;
-import com.jetbrains.python.psi.PyElement;
-import com.jetbrains.python.psi.PyExpression;
-import com.jetbrains.python.psi.PyQualifiedExpression;
-import com.jetbrains.python.psi.types.PyClassType;
-import com.jetbrains.python.psi.types.PyType;
-import com.jetbrains.python.psi.types.PyTypeChecker;
-import com.jetbrains.python.psi.types.TypeEvalContext;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.codeInspection.LocalInspectionToolSession
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import com.jetbrains.python.PyNames
+import com.jetbrains.python.PyPsiBundle
+import com.jetbrains.python.inspections.quickfix.PyRemoveCallQuickFix
+import com.jetbrains.python.psi.PyCallExpression
+import com.jetbrains.python.psi.PyDecorator
+import com.jetbrains.python.psi.PyElement
+import com.jetbrains.python.psi.PyExpression
+import com.jetbrains.python.psi.PyQualifiedExpression
+import com.jetbrains.python.psi.types.PyClassType
+import com.jetbrains.python.psi.types.PyTypeChecker
+import com.jetbrains.python.psi.types.TypeEvalContext
 
+class PyCallingNonCallableInspection : PyInspection() {
 
-public final class PyCallingNonCallableInspection extends PyInspection {
-
-  @Override
-  public @NotNull PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder,
-                                                 boolean isOnTheFly,
-                                                 @NotNull LocalInspectionToolSession session) {
-    return new Visitor(holder, PyInspectionVisitor.getContext(session));
+  override fun buildVisitor(
+    holder: ProblemsHolder,
+    isOnTheFly: Boolean,
+    session: LocalInspectionToolSession,
+  ): PsiElementVisitor {
+    return Visitor(holder, PyInspectionVisitor.getContext(session))
   }
 
-  public static class Visitor extends PyInspectionVisitor {
-    public Visitor(@Nullable ProblemsHolder holder,
-                   @NotNull TypeEvalContext context) {
-      super(holder, context);
+  class Visitor(holder: ProblemsHolder?, context: TypeEvalContext) : PyInspectionVisitor(holder, context) {
+
+    override fun visitPyCallExpression(node: PyCallExpression) {
+      super.visitPyCallExpression(node)
+      checkCallable(node, node.callee)
     }
 
-    @Override
-    public void visitPyCallExpression(@NotNull PyCallExpression node) {
-      super.visitPyCallExpression(node);
-      checkCallable(node, node.getCallee());
-    }
-
-    @Override
-    public void visitPyDecorator(@NotNull PyDecorator decorator) {
-      super.visitPyDecorator(decorator);
-      final PyExpression callee = decorator.getCallee();
-      checkCallable(decorator, callee);
+    override fun visitPyDecorator(decorator: PyDecorator) {
+      super.visitPyDecorator(decorator)
+      checkCallable(decorator, decorator.callee)
       if (decorator.hasArgumentList()) {
-        checkCallable(decorator, decorator);
+        checkCallable(decorator, decorator)
       }
     }
 
-    private void checkCallable(@NotNull PyElement node, @Nullable PyExpression callee) {
-      if (node.getParent() instanceof PyDecorator) return; //we've already been here
+    private fun checkCallable(node: PyElement, callee: PyExpression?) {
+      if (node.parent is PyDecorator) return // we've already been here
 
-      if (callee != null && isCallable(callee, myTypeEvalContext) == Boolean.FALSE) {
-        final PyType calleeType = myTypeEvalContext.getType(callee);
-        @InspectionMessage String message = PyPsiBundle.message("INSP.expression.is.not.callable");
-        if (calleeType instanceof PyClassType) {
-          message = PyPsiBundle.message("INSP.class.object.is.not.callable", calleeType.getName());
+      if (callee != null && isCallable(callee, myTypeEvalContext) == false) {
+        val calleeType = myTypeEvalContext.getType(callee)
+        val message = when {
+          calleeType is PyClassType -> PyPsiBundle.message("INSP.class.object.is.not.callable", calleeType.name)
+          callee.name != null -> PyPsiBundle.message("INSP.symbol.is.not.callable", callee.name)
+          else -> PyPsiBundle.message("INSP.expression.is.not.callable")
         }
-        else {
-          final String name = callee.getName();
-          if (name != null) {
-            message = PyPsiBundle.message("INSP.symbol.is.not.callable", name);
-          }
-        }
-        registerProblem(node, message, new PyRemoveCallQuickFix());
+        registerProblem(node, message, PyRemoveCallQuickFix())
       }
     }
   }
+}
 
-  private static @Nullable Boolean isCallable(@NotNull PyExpression element, @NotNull TypeEvalContext context) {
-    if (element instanceof PyQualifiedExpression && PyNames.__CLASS__.equals(element.getName())) {
-      return true;
-    }
-    return PyTypeChecker.isCallable(context.getType(element));
-  }
+private fun isCallable(element: PyExpression, context: TypeEvalContext): Boolean? {
+  if (element is PyQualifiedExpression && PyNames.__CLASS__ == element.name) return true
+
+  return PyTypeChecker.isCallable(context.getType(element))
 }
