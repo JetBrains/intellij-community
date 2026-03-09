f00811bc02ca8a2818bc618f700b04910c18c640
Eugene Zhuravlev <eugene.zhuravlev@jetbrains.com>
2026-03-09 15:47:01 +0100

    tests rules_jvm: regression tests for iteration primitives, some consistency checks
    -- correct "filterWithOrder" implementation + javadoc, used for annotation processor filtering
    
    GitOrigin-RevId: ecafd30e745cd797e6e3917b3962905134a0a0e6

--- build/jvm-rules/jvm-inc-builder-tests/src/com/intellij/tools/build/bazel/AllTests.java
+++ build/jvm-rules/jvm-inc-builder-tests/src/com/intellij/tools/build/bazel/AllTests.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.fail;
   KotlinTests.class,
   JavaTests.class,
   ZipBuilderTest.class,
+  IteratorsTest.class,
   DependencyGraphTest.class,
   KotlinCriTest.class
 })
