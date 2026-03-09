43b5315d222d7612e8a615fb636ea13bece7c4cc
Dmitriy.Panov <dmitriy.panov@jetbrains.com>
2026-03-09 18:43:11 +0100

    IDEA-384855 experiment #3

--- build/run_build_target.sh
+++ build/run_build_target.sh
@@ -18,6 +18,11 @@ for arg in "$@"; do
 done
 
 cd "$root"
+"$root/bazel.cmd" build //... --disk_cache=
+#"$root/bazel.cmd" build //... # populates the disk cache and incremental compilation caches
+#"$root/bazel.cmd" clean # cleaning current exec-root to wipe incremental compilation caches out
+#"$root/bazel.cmd" build //... # reuses previously built artifacts from the disk cache
+#rm -rf "$HOME/.cache/JetBrains/monorepo-bazel-cache"
 if [ ${#args[@]} -eq 0 ]; then
   exec /bin/bash "$root/bazel.cmd" run "$build_target_name" --
 else
