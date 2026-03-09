6590080575d5d7951fa9e28692e86e109ff3888b
Anna Kozlova <anna.kozlova@jetbrains.com>
2026-03-09 15:15:32 +0100

    [kotlin] kotlin serial uid version: fix hint wording
    ^KTIJ-37816 fixed
    
    GitOrigin-RevId: ce8390ea660695dc86ee15cef6cf03a51d65f713

--- jvm/jvm-analysis-kotlin-tests-k2/testSrc/com/intellij/codeInspection/tests/kotlin/K2SerializableHasSerialVersionUidFieldInspectionTest.kt
+++ jvm/jvm-analysis-kotlin-tests-k2/testSrc/com/intellij/codeInspection/tests/kotlin/K2SerializableHasSerialVersionUidFieldInspectionTest.kt
@@ -3,8 +3,7 @@ package com.intellij.codeInspection.tests.kotlin
 import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginMode
 import org.junit.Ignore
 
-@Ignore("AT-3959")
 class K2SerializableHasSerialVersionUidFieldInspectionTest : KotlinSerializableHasSerialVersionUidFieldInspectionTest() {
   override val pluginMode: KotlinPluginMode get() = KotlinPluginMode.K2
-  override fun getHint(): String = "Add 'private const val' property 'serialVersionUID' to 'Foo'"
+  override fun getHint(): String = "Add 'const val' property 'serialVersionUID' to 'Foo'"
 }
