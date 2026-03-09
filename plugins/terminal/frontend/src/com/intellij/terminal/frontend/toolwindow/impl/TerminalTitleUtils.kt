a4059610de01fa6a28d6532e021a250016585097
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-03-09 17:14:52 +0200

    [terminal] IJPL-237675 Refactor: use channelFlow for listening title updates
    This approach is more flexible than the previous one.
    
    GitOrigin-RevId: be19247be1212d83c85f0dd5e739ffe87c8153e8

--- plugins/terminal/frontend/src/com/intellij/terminal/frontend/toolwindow/impl/TerminalTitleUtils.kt
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/toolwindow/impl/TerminalTitleUtils.kt
@@ -3,20 +3,22 @@ package com.intellij.terminal.frontend.toolwindow.impl
 import com.intellij.openapi.application.UI
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Condition
+import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.platform.project.projectId
 import com.intellij.terminal.TerminalTitle
 import com.intellij.terminal.TerminalTitleListener
 import com.intellij.ui.content.Content
-import com.intellij.util.asDisposable
 import com.intellij.util.text.UniqueNameGenerator
 import fleet.rpc.client.durable
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.jetbrains.plugins.terminal.TerminalOptionsProvider
 import org.jetbrains.plugins.terminal.block.reworked.session.rpc.TerminalTabsManagerApi
 import org.jetbrains.plugins.terminal.buildSettingsAwareTitle
@@ -35,8 +37,8 @@ internal fun createDefaultTabName(toolWindow: ToolWindow): String {
 }
 
 internal fun updateTabNameOnTitleChange(title: TerminalTitle, content: Content, scope: CoroutineScope) {
-  title.addListener(scope) {
-    withContext(Dispatchers.UI) {
+  scope.launch(Dispatchers.UI) {
+    title.stateFlow().collect {
       content.displayName = it.text
     }
   }
@@ -48,33 +50,35 @@ internal fun updateBackendTabNameOnTitleChange(
   project: Project,
   scope: CoroutineScope,
 ) {
-  title.addListener(scope) {
-    durable {
-      TerminalTabsManagerApi.getInstance().renameTerminalTab(
-        project.projectId(),
-        backendTabId,
-        it.text,
-        it.isUserDefined
-      )
+  scope.launch {
+    title.stateFlow().collect {
+      durable {
+        TerminalTabsManagerApi.getInstance().renameTerminalTab(
+          project.projectId(),
+          backendTabId,
+          it.text,
+          it.isUserDefined
+        )
+      }
     }
   }
 }
 
-private fun TerminalTitle.addListener(coroutineScope: CoroutineScope, onChange: suspend (TitleData) -> Unit) {
-  val initialValue = TitleData(buildSettingsAwareTitle(), userDefinedTitle != null)
-  val flow = MutableStateFlow(initialValue)
+private fun TerminalTitle.stateFlow(): Flow<TitleData> {
+  val flow = channelFlow {
+    val disposable = Disposer.newDisposable()
+    addTitleListener(object : TerminalTitleListener {
+      override fun onTitleChanged(terminalTitle: TerminalTitle) {
+        trySend(TitleData(terminalTitle.buildSettingsAwareTitle(), terminalTitle.userDefinedTitle != null))
+      }
+    }, disposable)
 
-  addTitleListener(object : TerminalTitleListener {
-    override fun onTitleChanged(terminalTitle: TerminalTitle) {
-      flow.value = TitleData(terminalTitle.buildSettingsAwareTitle(), terminalTitle.userDefinedTitle != null)
-    }
-  }, parentDisposable = coroutineScope.asDisposable())
+    send(TitleData(buildSettingsAwareTitle(), userDefinedTitle != null))
 
-  coroutineScope.launch {
-    flow.collect {
-      onChange(it)
-    }
+    awaitClose { Disposer.dispose(disposable) }
   }
+
+  return flow.distinctUntilChanged()
 }
 
 internal data class TitleData(@param:NlsSafe val text: String, val isUserDefined: Boolean)
