76f270c858d91bd316cdd07bb918da624af3f31e
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2026-03-09 17:46:54 +0100

    IJPL-895 Fix imports in intellij.platform.externalSystem
    GitOrigin-RevId: db4d1ecba28f5a50d65757d7df99442a543244b7

--- platform/external-system-api/gen/com/intellij/openapi/externalSystem/settings/workspaceModel/impl/ExternalProjectsBuildClasspathEntityImpl.kt
+++ platform/external-system-api/gen/com/intellij/openapi/externalSystem/settings/workspaceModel/impl/ExternalProjectsBuildClasspathEntityImpl.kt
@@ -3,7 +3,14 @@ package com.intellij.openapi.externalSystem.settings.workspaceModel.impl
 
 import com.intellij.openapi.externalSystem.settings.workspaceModel.ExternalProjectBuildClasspathEntity
 import com.intellij.openapi.externalSystem.settings.workspaceModel.ExternalProjectsBuildClasspathEntity
-import com.intellij.platform.workspace.storage.*
+import com.intellij.platform.workspace.storage.ConnectionId
+import com.intellij.platform.workspace.storage.EntitySource
+import com.intellij.platform.workspace.storage.GeneratedCodeApiVersion
+import com.intellij.platform.workspace.storage.GeneratedCodeImplVersion
+import com.intellij.platform.workspace.storage.MutableEntityStorage
+import com.intellij.platform.workspace.storage.WorkspaceEntity
+import com.intellij.platform.workspace.storage.WorkspaceEntityBuilder
+import com.intellij.platform.workspace.storage.WorkspaceEntityInternalApi
 import com.intellij.platform.workspace.storage.impl.ModifiableWorkspaceEntityBase
 import com.intellij.platform.workspace.storage.impl.WorkspaceEntityBase
 import com.intellij.platform.workspace.storage.impl.WorkspaceEntityData
