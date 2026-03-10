0e93d743a31328fae51ef74eb50f5f66b9090f64
Jakob Gerhardt <jakob.gerhardt@jetbrains.com>
2026-03-10 12:54:06 +0100

    IJPL-232764 refactor frontend file structure
    GitOrigin-RevId: c3a91c26cfa07bce06b6e0eaba6648af68ca7426

--- plugins/search-everywhere-lucene/frontend/src/SearchEverywhereLuceneFrontendBundle.kt
+++ plugins/search-everywhere-lucene/frontend/src/SearchEverywhereLuceneFrontendBundle.kt
@@ -5,7 +5,6 @@ import com.intellij.DynamicBundle
 import org.jetbrains.annotations.Nls
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
-import java.util.function.Supplier
 
 internal object SearchEverywhereLuceneFrontendBundle {
   private const val BUNDLE: @NonNls String = "messages.searchEverywhereLuceneFrontendBundle"
@@ -17,7 +16,4 @@ internal object SearchEverywhereLuceneFrontendBundle {
     return INSTANCE.getMessage(key, *params)
   }
 
-  fun messagePointer(key: @PropertyKey(resourceBundle = BUNDLE) String, vararg params: Any): Supplier<@Nls String> {
-    return INSTANCE.getLazyMessage(key, *params)
-  }
 }
--- plugins/search-everywhere-lucene/frontend/src/tabs/SearchEverywhereLuceneFilesTab.kt
+++ plugins/search-everywhere-lucene/frontend/src/tabs/SearchEverywhereLuceneFilesTab.kt
@@ -1,18 +1,12 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.searchEverywhereLucene.frontend.tabs
 
-import com.intellij.openapi.util.Disposer
 import com.intellij.platform.searchEverywhere.SeItemData
-import com.intellij.platform.searchEverywhere.SeParams
-import com.intellij.platform.searchEverywhere.SePreviewInfo
-import com.intellij.platform.searchEverywhere.SeResultEvent
 import com.intellij.platform.searchEverywhere.frontend.SeFilterEditor
-import com.intellij.platform.searchEverywhere.frontend.SeTab
 import com.intellij.platform.searchEverywhere.frontend.resultsProcessing.SeTabDelegate
 import com.intellij.platform.searchEverywhere.frontend.tabs.SeDefaultTabBase
 import com.intellij.searchEverywhereLucene.common.SearchEverywhereLuceneProviderIdUtils
 import com.intellij.searchEverywhereLucene.frontend.SearchEverywhereLuceneFrontendBundle
-import kotlinx.coroutines.flow.Flow
 import org.jetbrains.annotations.Nls
 
 class SearchEverywhereLuceneFilesTab(delegate: SeTabDelegate) : SeDefaultTabBase(delegate) {
