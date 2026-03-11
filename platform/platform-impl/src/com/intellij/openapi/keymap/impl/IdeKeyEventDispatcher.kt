2af1f1835ff0da44df5d3e72d9e357d686240fde
Marcus Mews <marcus.mews@jetbrains.com>
2026-03-11 14:27:05 +0000

    IDEA-385970 Had to press Down key twice in 'Select Methods to Propagate Parameter Deletion'
    GitOrigin-RevId: 9512be9a3440f6b84b564ae2f0e8b5e097b023f3

--- platform/platform-impl/src/com/intellij/openapi/keymap/impl/IdeKeyEventDispatcher.kt
+++ platform/platform-impl/src/com/intellij/openapi/keymap/impl/IdeKeyEventDispatcher.kt
@@ -408,7 +408,8 @@ class IdeKeyEventDispatcher(private val queue: IdeEventQueue?) {
       KeyEvent.KEY_TYPED == e.id && isPressedWasProcessed -> true
       //see IDEADEV-8615
       KeyEvent.KEY_RELEASED == e.id && KeyEvent.VK_ALT == e.keyCode && isPressedWasProcessed -> true
-      KeyEvent.KEY_PRESSED == e.id -> true
+      //see PY-86725 (macOS generates three VK_MINUS events instead of one on non-English keyboards)
+      KeyEvent.KEY_PRESSED == e.id && KeyEvent.VK_MINUS == e.keyCode && isPressedWasProcessed && SystemInfoRt.isMac -> true
       else -> {
         state = KeyState.STATE_INIT
         isPressedWasProcessed = false
