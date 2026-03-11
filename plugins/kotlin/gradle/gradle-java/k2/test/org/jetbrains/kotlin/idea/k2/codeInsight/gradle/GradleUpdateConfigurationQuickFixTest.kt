6d9846fea96489cb9ab7f6411fe150dcecaabc11
Victoria Petrakovich <victoria.petrakovich@jetbrains.com>
2026-03-11 18:42:14 +0100

    [kotlin] KTIJ-37342 Move `GradleUpdateConfigurationQuickFixTest` to k2 module
    GitOrigin-RevId: 91dcc0c3cf5baa00515acbc65ac32cc8cdc41431

--- plugins/kotlin/gradle/gradle-java/k2/test/org/jetbrains/kotlin/idea/k2/codeInsight/gradle/GradleUpdateConfigurationQuickFixTest.kt
+++ plugins/kotlin/gradle/gradle-java/k2/test/org/jetbrains/kotlin/idea/k2/codeInsight/gradle/GradleUpdateConfigurationQuickFixTest.kt
@@ -1,6 +1,5 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
-package org.jetbrains.kotlin.idea.codeInsight.gradle
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.k2.codeInsight.gradle
 
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.impl.LoadTextUtil
