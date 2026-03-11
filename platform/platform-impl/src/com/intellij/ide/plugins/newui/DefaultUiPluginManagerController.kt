1d53dde9cf0362572d03f5917a75361e79128a1c
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2026-03-11 13:55:13 +0100

    IJPL-238175 check for cancellation before processing the exception in DefaultUiPluginManagerController
    GitOrigin-RevId: 72c5bd6db872db6b2508c725ba202d2957da1dfd

--- platform/platform-impl/src/com/intellij/ide/plugins/newui/DefaultUiPluginManagerController.kt
+++ platform/platform-impl/src/com/intellij/ide/plugins/newui/DefaultUiPluginManagerController.kt
@@ -76,6 +76,8 @@ import com.intellij.openapi.util.io.FileUtil
 import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
 import com.intellij.util.concurrency.annotations.RequiresReadLockAbsence
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.withContext
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.Nls
@@ -780,6 +782,7 @@ object DefaultUiPluginManagerController : UiPluginManagerController {
       return PluginSearchResult(plugins)
     }
     catch (e: IOException) {
+      currentCoroutineContext().ensureActive()
       LOG.warn(e)
       return PluginSearchResult(emptyList(), e.message)
     }
