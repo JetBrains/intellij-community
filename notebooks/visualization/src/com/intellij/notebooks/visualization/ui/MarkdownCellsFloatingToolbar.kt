e4660ba06f3a4f6c6492bc6cba28e838e7ba42cb
Nikita Pavlenko <nikita.pavlenko@jetbrains.com>
2026-03-11 15:21:15 +0100

    [PyCharm] Jupyter/RemoteDev (fix): Markdown toolbar will appear in RemoteDev. #PY-85735 Ready For Merge
    GitOrigin-RevId: c858c09d52fc5c1f1653ece47a78525fbe042d82

--- notebooks/visualization/src/com/intellij/notebooks/visualization/ui/MarkdownCellsFloatingToolbar.kt
+++ notebooks/visualization/src/com/intellij/notebooks/visualization/ui/MarkdownCellsFloatingToolbar.kt
@@ -1,12 +1,12 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.notebooks.visualization.ui
 
-import com.intellij.ide.ui.customization.CustomActionsSchema
 import com.intellij.notebooks.jupyter.core.jupyter.CellType
 import com.intellij.notebooks.visualization.NotebookCellLines
 import com.intellij.notebooks.visualization.getCells
 import com.intellij.notebooks.visualization.getSelectionLines
 import com.intellij.openapi.actionSystem.ActionGroup
+import com.intellij.openapi.actionSystem.ex.ActionUtil
 import com.intellij.openapi.actionSystem.impl.FloatingToolbar
 import com.intellij.openapi.editor.Editor
 import kotlinx.coroutines.CoroutineScope
@@ -14,7 +14,7 @@ import kotlinx.coroutines.CoroutineScope
 class MarkdownCellsFloatingToolbar(
   editor: Editor,
   coroutineScope: CoroutineScope,
-): FloatingToolbar(editor, coroutineScope) {
+) : FloatingToolbar(editor, coroutineScope) {
   override fun isEnabled(): Boolean {
     val selectedCells = getSelectedCells()
     if (selectedCells.isEmpty() || selectedCells.size > 1) return false
@@ -28,7 +28,5 @@ class MarkdownCellsFloatingToolbar(
     }.distinct()
   }
 
-  override fun createActionGroup(): ActionGroup? {
-    return CustomActionsSchema.getInstance().getCorrectedAction("Markdown.Toolbar.Floating") as? ActionGroup
-  }
+  override fun createActionGroup(): ActionGroup? = ActionUtil.getActionGroup("Markdown.Toolbar.Floating")
 }
