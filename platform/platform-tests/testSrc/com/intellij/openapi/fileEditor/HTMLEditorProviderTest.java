4d5598c2b6569b2d65a9adff40aaca54ccd46dd0
Alexander Trushev <aleksandr.trushev@jetbrains.com>
2026-03-11 14:19:27 +0100

    [editor] IJPL-187076 Fix for issue with reopening html editor tab
    - `Reopen Closed Tab` action now restores closed tabs from the active editor window even when invoked without an editor context component, instead of immediately falling back to editor history.
    
    - `EditorWindow` stores the VirtualFile reference in RemovedTabInfo instead of just the URL string, so synthetic HTMLVirtualFile(LightVirtualFile) tabs can be restored
    
    
    (cherry picked from commit f53c84efba7657f8a35a4c5be261bf7e1bd26534)
    
    IJ-CR-195747
    
    GitOrigin-RevId: e807d80803dc609b874d8d1d8801c2076f27cc87

--- platform/platform-tests/testSrc/com/intellij/openapi/fileEditor/HTMLEditorProviderTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/fileEditor/HTMLEditorProviderTest.java
@@ -1,12 +1,15 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.fileEditor;
 
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext;
 import com.intellij.openapi.fileEditor.impl.HTMLEditorProvider;
 import com.intellij.openapi.fileEditor.impl.HTMLFileEditorMock;
 import com.intellij.openapi.fileTypes.PlainTextFileType;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.testFramework.FileEditorManagerTestCase;
+import com.intellij.testFramework.TestActionEvent;
 import com.intellij.ui.jcef.JBCefApp;
 import com.intellij.util.containers.ContainerUtil;
 
@@ -62,6 +65,35 @@ public final class HTMLEditorProviderTest extends FileEditorManagerTestCase {
 
     FileEditor[] reopenedEditors = manager.openFile(file, true);
     FileEditor reopened = ContainerUtil.find(reopenedEditors, it -> it instanceof HTMLEditorProvider.HTMLFileEditor);
+    assertNotNull(reopened);
+    assertEquals(HTMLFileEditorMock.UNIT_TEST_EDITOR_NAME, reopened.getName());
+  }
+
+  public void testReopenClosedTabActionUsesCurrentWindowClosedTabsWithoutContextComponent() {
+    Registry.get("ide.browser.jcef.enabled").setValue(false, getTestRootDisposable());
+    Registry.get("ide.browser.jcef.testMode.enabled").setValue(true, getTestRootDisposable());
+
+    HTMLEditorProvider.Request request = HTMLEditorProvider.Request.Companion.html("<html><body>test</body></html>");
+    FileEditor htmlEditor = HTMLEditorProvider.openEditorWithoutJcef(getProject(), "html-preview", request, PlainTextFileType.INSTANCE);
+    assertNotNull(htmlEditor);
+    assertEquals(HTMLFileEditorMock.UNIT_TEST_EDITOR_NAME, htmlEditor.getName());
+
+    VirtualFile htmlFile = htmlEditor.getFile();
+    VirtualFile regularFile = myFixture.addFileToProject("foo.txt", "foo").getVirtualFile();
+    manager.openFile(regularFile, true);
+    manager.closeFile(htmlFile);
+    executeSomeCoroutineTasksAndDispatchAllInvocationEvents(getProject());
+
+    assertFalse(manager.isFileOpen(htmlFile));
+    assertTrue(manager.isFileOpen(regularFile));
+
+    ActionManager.getInstance().getAction("ReopenClosedTab").actionPerformed(
+      TestActionEvent.createTestEvent(SimpleDataContext.getProjectContext(getProject()))
+    );
+    executeSomeCoroutineTasksAndDispatchAllInvocationEvents(getProject());
+
+    assertTrue(manager.isFileOpen(htmlFile));
+    FileEditor reopened = ContainerUtil.find(manager.getEditors(htmlFile), it -> it instanceof HTMLEditorProvider.HTMLFileEditor);
     assertNotNull(reopened);
     assertEquals(HTMLFileEditorMock.UNIT_TEST_EDITOR_NAME, reopened.getName());
   }
