c2e52f50392b303537f958cbfa2cf046ddc58fe3
Daniil Kalinin <Daniil.Kalinin@jetbrains.com>
2026-03-12 09:44:28 +0100

    test framework [Code Insight]: PY-85639 Use custom pyCodeInsightFixture to avoid $CONTENT_ROOT/$PROJECT_ROOT incompatibility
    The platform's codeInsightFixture resolves @TestDataPath with $PROJECT_ROOT, which is incompatible with Python tests using $CONTENT_ROOT.
    Also pre-register the temp directory in VFS to fix a race condition where concurrent fixture initialization could cause setUp() to fail.
    
    GitOrigin-RevId: 47b8122495e58b4b268ff102504f9a7b1fad0840

--- python/testSrc/com/jetbrains/python/junit5/framework/impl/PyCodeInsightFixturesExtension.kt
+++ python/testSrc/com/jetbrains/python/junit5/framework/impl/PyCodeInsightFixturesExtension.kt
@@ -3,12 +3,25 @@ package com.jetbrains.python.junit5.framework.impl
 
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
-import com.intellij.platform.testFramework.junit5.codeInsight.fixture.codeInsightFixture
+import com.intellij.openapi.project.modules
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.testFramework.IndexingTestUtil
-import com.intellij.testFramework.junit5.fixture.*
+import com.intellij.testFramework.TestApplicationManager
+import com.intellij.testFramework.TestDataProvider
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import com.intellij.testFramework.fixtures.IdeaProjectTestFixture
+import com.intellij.testFramework.fixtures.impl.CodeInsightTestFixtureImpl
+import com.intellij.testFramework.fixtures.impl.TempDirTestFixtureImpl
+import com.intellij.testFramework.junit5.fixture.LookupFixture
 import com.intellij.testFramework.junit5.fixture.LookupFixtureExtension.Companion.getClassLevelLookupFixtureManager
 import com.intellij.testFramework.junit5.fixture.LookupFixtureExtension.Companion.getLookupFixtureManager
 import com.intellij.testFramework.junit5.fixture.LookupFixtureExtension.Companion.registerImplicitFixtures
+import com.intellij.testFramework.junit5.fixture.TestFixture
+import com.intellij.testFramework.junit5.fixture.moduleFixture
+import com.intellij.testFramework.junit5.fixture.projectFixture
+import com.intellij.testFramework.junit5.fixture.sourceRootFixture
+import com.intellij.testFramework.junit5.fixture.tempPathFixture
+import com.intellij.testFramework.junit5.fixture.testFixture
 import com.jetbrains.python.PyNames
 import com.jetbrains.python.PythonMockSdk
 import com.jetbrains.python.junit5.framework.pyMockSdkFixture
@@ -83,7 +96,7 @@ internal class PyCodeInsightFixturesExtension : BeforeAllCallback, BeforeEachCal
     }
 
     manager.getOrDefault {
-      codeInsightFixture(project, tempDirFixture).also {
+      pyCodeInsightFixture(project, tempDirFixture).also {
         implicitFixtures += LookupFixture(DEFAULT_CODE_INSIGHT, it, true)
       }
     }
@@ -91,5 +104,56 @@ internal class PyCodeInsightFixturesExtension : BeforeAllCallback, BeforeEachCal
     runBlocking {
       context.registerImplicitFixtures(implicitFixtures, static = false)
     }
+  }
+}
+
+/**
+ * Creates a [CodeInsightTestFixture] without the platform's `@TestDataPath` resolution.
+ *
+ * The platform's [com.intellij.platform.testFramework.junit5.codeInsight.fixture.codeInsightFixture] resolves
+ * `@TestDataPath` using `$PROJECT_ROOT` prefix, which is incompatible with Python tests that use `$CONTENT_ROOT`.
+ * The test data path is set later by [PyTestDataExtension].
+ */
+private fun pyCodeInsightFixture(
+  projectFixture: TestFixture<Project>,
+  tempDirFixture: TestFixture<Path>,
+): TestFixture<CodeInsightTestFixture> = testFixture {
+  val project = projectFixture.init()
+  val tempDir = tempDirFixture.init()
+
+  val ideaProjectTestFixture = object : IdeaProjectTestFixture {
+    override fun getProject(): Project = project
+
+    override fun getModule(): Module {
+      check(project.modules.isNotEmpty()) {
+        "At least one module is required for the project. Use TestFixture<Project>.moduleFixture() to register one in your test class."
+      }
+      return project.modules[0]
+    }
+
+    override fun setUp() {
+      TestApplicationManager.getInstance().setDataProvider(TestDataProvider(project))
+    }
+
+    override fun tearDown() {
+      TestApplicationManager.getInstance().setDataProvider(null)
+    }
+  }
+
+  val tempDirTestFixture = object : TempDirTestFixtureImpl() {
+    override fun doCreateTempDirectory(): Path = tempDir
+    override fun deleteOnTearDown(): Boolean = false
+  }
+
+  val codeInsightFixture = CodeInsightTestFixtureImpl(ideaProjectTestFixture, tempDirTestFixture)
+  // Ensure the temp directory is registered in VFS before setUp().
+  // Implicit fixtures are initialized concurrently (see registerImplicitFixtures),
+  // so the sourceRootFixture (which calls VfsUtil.createDirectories) may not have run yet.
+  // Without this, CodeInsightTestFixtureImpl.setUp() fails at getFile("") because VFS
+  // doesn't know about the temp directory path.
+  VfsUtil.createDirectories(tempDir.toString())
+  codeInsightFixture.setUp()
+  initialized(codeInsightFixture) {
+    codeInsightFixture.tearDown()
   }
 }
