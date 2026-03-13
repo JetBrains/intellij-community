9bfa274a63cf9d0bb6db71d2066a05e4dd2621af
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2026-03-14 02:33:52 +0400

    [kotlin] KTIJ-32836 Unmute 'Change visibility' intention tests for context parameter declarations
    The 'Change visibility' intention was previously broken for declarations with a
    context parameter list — the line break after the context parameter list was
    dropped and the caret was placed incorrectly after applying the intention.
    
    The underlying issue is now obsolete. Unmute the affected tests:
    - changeVisibility/private/hasModifier5.kt
    - changeVisibility/private/noModifierListContext.kt
    
    GitOrigin-RevId: 0daf21ab9da1bcaeced9fadfc8e14eff52d57ec3

--- plugins/kotlin/idea/tests/testData/intentions/changeVisibility/private/noModifierListContext.kt
+++ plugins/kotlin/idea/tests/testData/intentions/changeVisibility/private/noModifierListContext.kt
@@ -1,10 +1,6 @@
-// ERROR: The feature "context receivers" is experimental and should be enabled explicitly. This can be done by supplying the compiler argument '-Xcontext-receivers', but note that no stability guarantees are provided.
-// K2_ERROR: The feature "context parameters" is experimental and should be enabled explicitly. This can be done by supplying the compiler argument '-Xcontext-parameters', but note that no stability guarantees are provided.
-// K2_AFTER_ERROR: The feature "context parameters" is experimental and should be enabled explicitly. This can be done by supplying the compiler argument '-Xcontext-parameters', but note that no stability guarantees are provided.
+// COMPILER_ARGUMENTS: -Xcontext-parameters
 // IGNORE_K1
-// IGNORE_K2
-// KTIJ-32836
 class A
 
-context(A)
+context(_: A)
 <caret>fun foo() {}
