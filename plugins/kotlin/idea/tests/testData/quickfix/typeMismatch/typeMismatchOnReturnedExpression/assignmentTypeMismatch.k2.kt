8fd82d0ab6eb871e94e296374ab2c139a25b1008
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2026-03-13 14:33:54 +0100

    KTIJ-36918 [kotlin] Add ignore directive until it is fixed
    GitOrigin-RevId: 5ec00da66eb8893e8058b0f93dbc9dce0e060516

--- plugins/kotlin/idea/tests/testData/quickfix/typeMismatch/typeMismatchOnReturnedExpression/assignmentTypeMismatch.k2.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/typeMismatch/typeMismatchOnReturnedExpression/assignmentTypeMismatch.k2.kt
@@ -6,5 +6,9 @@ fun foo() {
         x += 21
     }
 }
+
+// IGNORE_K2
+// TODO: Drop IGNORE_K2 when KTIJ-36918 is fixed
+
 // FUS_QUICKFIX_NAME: org.jetbrains.kotlin.idea.quickfix.ChangeFunctionLiteralReturnTypeFix
 // FUS_K2_QUICKFIX_NAME: org.jetbrains.kotlin.idea.k2.codeinsight.fixes.ChangeTypeQuickFixFactories$UpdateTypeQuickFix
--- plugins/kotlin/idea/tests/testData/quickfix/typeMismatch/typeMismatchOnReturnedExpression/literalPropertyWithGetter.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/typeMismatch/typeMismatchOnReturnedExpression/literalPropertyWithGetter.kt
@@ -4,5 +4,8 @@
 val complex: (Int) -> String
     get() = { it.toLong()<caret> }
 
+// IGNORE_K2
+// TODO: Drop IGNORE_K2 when KTIJ-36918 is fixed
+
 // FUS_QUICKFIX_NAME: org.jetbrains.kotlin.idea.quickfix.ChangeCallableReturnTypeFix$ForEnclosing
 // FUS_K2_QUICKFIX_NAME: org.jetbrains.kotlin.idea.k2.codeinsight.fixes.ChangeTypeQuickFixFactories$UpdateTypeQuickFix
