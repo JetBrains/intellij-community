bd36932ac891afbcd612c3e54c183222cd8281e0
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2026-03-15 00:26:32 +0100

    [agent-workbench][vcs-ai]: override default Self-Review actions when AWB plugin active (IJPL-233558)
    GitOrigin-RevId: 3624822e472d6ca086f3bbbf4460f138e72f840a

--- plugins/agent-workbench/prompt/core/src/AgentPromptInvocationDataAttributes.kt
+++ plugins/agent-workbench/prompt/core/src/AgentPromptInvocationDataAttributes.kt
@@ -1,5 +1,9 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.agent.workbench.prompt.core
 
+import com.intellij.openapi.actionSystem.DataKey
+
 const val AGENT_PROMPT_INVOCATION_DATA_CONTEXT_KEY: String = "dataContext"
 const val AGENT_PROMPT_INVOCATION_PREFER_EXTENSIONS_KEY: String = "preferExtensionTabs"
+
+val AGENT_PROMPT_INITIAL_TEXT_DATA_KEY: DataKey<String> = DataKey.create("AgentPrompt.initialText")
