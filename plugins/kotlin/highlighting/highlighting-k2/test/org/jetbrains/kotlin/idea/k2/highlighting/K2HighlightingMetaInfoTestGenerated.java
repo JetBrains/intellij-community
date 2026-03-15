4052bf6141ecff14bfca2e251e24c947caad8e8c
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-03-15 22:34:21 +0200

    [kotlin] Account for ^KT-73945
    `REDUNDANT_MODALITY_MODIFIER` is unnecessary because there's
    `REDUNDANT_OPEN_IN_INTERFACE` already. But we want to keep the "unused"
    attribute for better UX.
    
    GitOrigin-RevId: 2f854fd488d1622eaa9895dbd5525d7641ddb34c

--- plugins/kotlin/highlighting/highlighting-k2/test/org/jetbrains/kotlin/idea/k2/highlighting/K2HighlightingMetaInfoTestGenerated.java
+++ plugins/kotlin/highlighting/highlighting-k2/test/org/jetbrains/kotlin/idea/k2/highlighting/K2HighlightingMetaInfoTestGenerated.java
@@ -53,6 +53,11 @@ public abstract class K2HighlightingMetaInfoTestGenerated extends AbstractK2High
             runTest("../../idea/tests/testData/highlighterMetaInfo/diagnostics/mppIsNotEnabled.kt");
         }
 
+        @TestMetadata("redundantOpenFunctionInInterfaceDiagnostic.kt")
+        public void testRedundantOpenFunctionInInterfaceDiagnostic() throws Exception {
+            runTest("../../idea/tests/testData/highlighterMetaInfo/diagnostics/redundantOpenFunctionInInterfaceDiagnostic.kt");
+        }
+
         @TestMetadata("uselessCast.kt")
         public void testUselessCast() throws Exception {
             runTest("../../idea/tests/testData/highlighterMetaInfo/diagnostics/uselessCast.kt");
---
+++ plugins/kotlin/idea/tests/testData/highlighterMetaInfo/diagnostics/redundantOpenFunctionInInterfaceDiagnostic.kt
@@ -0,0 +1,7 @@
+// ALLOW_ERRORS
+// HIGHLIGHT_WARNINGS
+// HIGHLIGHTER_ATTRIBUTES_KEY
+
+interface Interface {
+    open fun foo()
+}
