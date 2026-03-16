168ab9855b39fcbbd5956abd8289bdfbc8d925c1
Anna Kozlova <anna.kozlova@jetbrains.com>
2026-03-16 18:13:54 +0100

    [kotlin]: setup explicit k1 mode to avoid failures via bazel without explicit -Didea.kotlin.plugin.use.k1=true property
    - caused by re-bucketing
    
    KTIJ-38008
    
    GitOrigin-RevId: 9bd7d7eb26c5ca8c232077606d64defb767504ea

--- plugins/kotlin/jvm-debugger/test/kotlin.jvm-debugger.test.k1/test/org/jetbrains/kotlin/idea/debugger/test/sequence/KotlinPsiChainBuilderTestCase.kt
+++ plugins/kotlin/jvm-debugger/test/kotlin.jvm-debugger.test.k1/test/org/jetbrains/kotlin/idea/debugger/test/sequence/KotlinPsiChainBuilderTestCase.kt
@@ -7,10 +7,18 @@ import com.intellij.debugger.streams.test.StreamChainBuilderTestCase
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.testFramework.IdeaTestUtil
 import junit.framework.TestCase
+import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginMode
 import org.jetbrains.kotlin.idea.debugger.test.DEBUGGER_TESTDATA_PATH_BASE
+import org.jetbrains.kotlin.idea.test.ExpectedPluginModeProvider
 import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
+import org.jetbrains.kotlin.idea.test.setUpWithKotlinPlugin
 
-abstract class KotlinPsiChainBuilderTestCase(private val relativePath: String) : StreamChainBuilderTestCase() {
+abstract class KotlinPsiChainBuilderTestCase(private val relativePath: String) : StreamChainBuilderTestCase(), ExpectedPluginModeProvider {
+    override val pluginMode = KotlinPluginMode.K1
+    override fun setUp() {
+        setUpWithKotlinPlugin { super.setUp() }
+    }
+
     override fun getTestDataPath(): String = "$DEBUGGER_TESTDATA_PATH_BASE/sequence/psi/$relativeTestPath"
     override fun getProjectDescriptor() = KotlinWithJdkAndRuntimeLightProjectDescriptor.getInstance()
 
