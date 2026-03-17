f35fd3560939784dd6228085ae79a96f1f9a042b
Ilya.Komolkin <ilya.komolkin@jetbrains.com>
2026-03-17 14:24:06 +0000

    [test] KTNB-1427 fix deadlock by using RequiresLockSemantics
    Merge-request: IJ-MR-196453
    Merged-by: Ilya Komolkin <Ilya.Komolkin@jetbrains.com>
    
    GitOrigin-RevId: 2184f3c583934fd45660aa90fa601c17e4f45069

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/remote/SwingHierarchyService.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/remote/SwingHierarchyService.kt
@@ -1,11 +1,14 @@
 package com.intellij.driver.sdk.ui.remote
 
 import com.intellij.driver.client.Remote
+import com.intellij.driver.client.RequiresLockSemantics
+import com.intellij.driver.model.LockSemantics
 import com.intellij.driver.model.TextDataList
 
 @Remote("com.jetbrains.performancePlugin.remotedriver.SwingHierarchyService",
         plugin = REMOTE_ROBOT_MODULE_ID)
 interface SwingHierarchyService {
+  @RequiresLockSemantics(LockSemantics.NO_LOCK)
   fun getSwingHierarchyAsDOM(component: Component?, onlyFrontend: Boolean): String
   fun findAllText(component: Component): TextDataList
 }
