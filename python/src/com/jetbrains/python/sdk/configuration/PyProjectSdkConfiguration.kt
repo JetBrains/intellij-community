fe8bea3fcb18c6f191819baeb3523b6a8a4c51d1
Vitaly Legchilkin <vitaly.legchilkin@jetbrains.com>
2026-03-17 14:28:57 +0100

    PY-88280 Extract autoConfigureSdkIfNeeded and simplify SDK auto-configuration
    - Add autoConfigureSdkIfNeeded to sdkSuggestionTools that waits for
      project model and uses the global mutex
    - Add early return in getModuleInfo when SDK is already configured
    - Simplify ModulesSdkConfigurator to use autoConfigureSdkIfNeeded
    - Simplify InterpreterSettingsQuickFix to use autoConfigureSdkIfNeeded
    - Remove SDK-configured notification from PyProjectSdkConfiguration
    
    (cherry picked from commit f1423427eeb433d379519e3b9d1af00b46fc869d)
    (cherry picked from commit b81353d3af62f2d1f694d8c0d470d4495a01c989)
    
    GitOrigin-RevId: 1b075eba8569d3eeb10e5ad6eed1730d4147e32e

--- python/src/com/jetbrains/python/sdk/configuration/PyProjectSdkConfiguration.kt
+++ python/src/com/jetbrains/python/sdk/configuration/PyProjectSdkConfiguration.kt
@@ -1,36 +1,25 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.sdk.configuration
 
-import com.intellij.notification.NotificationAction
-import com.intellij.notification.NotificationGroupManager
-import com.intellij.notification.NotificationType
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.application.EDT
-
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.isNotificationSilentMode
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.util.Disposer
-
-import com.intellij.openapi.wm.ex.WelcomeScreenProjectProvider
-
 import com.intellij.python.community.services.systemPython.SystemPythonService
 import com.jetbrains.python.PyBundle
 import com.jetbrains.python.PythonPluginDisposable
 import com.jetbrains.python.errorProcessing.PyResult
 import com.jetbrains.python.errorProcessing.emit
-import com.jetbrains.python.sdk.PySdkPopupFactory
 import com.jetbrains.python.sdk.configuration.suppressors.PyPackageRequirementsInspectionSuppressor
 import com.jetbrains.python.sdk.configuration.suppressors.TipOfTheDaySuppressor
 import com.jetbrains.python.sdk.configurePythonSdk
-import com.jetbrains.python.sdk.impl.PySdkBundle
 import com.jetbrains.python.sdk.installExecutableViaPythonScript
-import com.jetbrains.python.statistics.ConfiguredPythonInterpreterIdsHolder.Companion.SDK_HAS_BEEN_CONFIGURED_AS_THE_PROJECT_INTERPRETER
 import com.jetbrains.python.util.ShowingMessageErrorSync
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.jetbrains.annotations.ApiStatus
 import java.nio.file.Path
 
 object PyProjectSdkConfiguration {
@@ -56,20 +45,18 @@ object PyProjectSdkConfiguration {
       return@withContext true
     }
 
-    setReadyToUseSdk(module.project, module, sdk)
+    configurePythonSdk(module.project, module, sdk)
     thisLogger().debug("Successfully configured sdk using ${createSdkInfoWithTool.toolId}")
     true
   }
 
-  suspend fun setReadyToUseSdk(project: Project, module: Module, sdk: Sdk) {
+  @ApiStatus.Obsolete
+  fun setReadyToUseSdk(project: Project, module: Module, sdk: Sdk) {
     if (module.isDisposed) {
       return
     }
 
     configurePythonSdk(project, module, sdk)
-    withContext(Dispatchers.EDT) {
-      notifyAboutConfiguredSdk(project, module, sdk)
-    }
   }
 
   fun suppressTipAndInspectionsFor(module: Module, toolName: String): Disposable {
@@ -85,23 +72,5 @@ object PyProjectSdkConfiguration {
 
     PythonSdkCreationWaiter.register(module, lifetime)
     return lifetime
-  }
-
-  private fun notifyAboutConfiguredSdk(project: Project, module: Module, sdk: Sdk) {
-    if (isNotificationSilentMode(project) || WelcomeScreenProjectProvider.isWelcomeScreenProject(project)) return
-    NotificationGroupManager.getInstance().getNotificationGroup("ConfiguredPythonInterpreter")
-      .createNotification(
-        content = PyBundle.message("sdk.has.been.configured.as.the.project.interpreter", sdk.name),
-        type = NotificationType.INFORMATION
-      )
-      .setDisplayId(SDK_HAS_BEEN_CONFIGURED_AS_THE_PROJECT_INTERPRETER)
-      .apply {
-        val configureSdkAction = NotificationAction.createSimpleExpiring(PySdkBundle.message("python.configure.interpreter.action")) {
-          PySdkPopupFactory.createAndShow(module)
-        }
-
-        addAction(configureSdkAction)
-        notify(project)
-      }
   }
 }
