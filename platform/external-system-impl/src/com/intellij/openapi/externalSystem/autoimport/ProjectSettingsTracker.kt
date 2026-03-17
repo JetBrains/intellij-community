f0d75243d321bed97b11dd2520e83fa7cdb553a8
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2026-03-17 12:22:02 +0100

    IJPL-239527 [AutoSync] cleanup ProjectSettingsTracker: add explicit return types to public functions
    GitOrigin-RevId: 72da54a4128d6cd970751805794e29ecaf9170b7

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/autoimport/ProjectSettingsTracker.kt
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/autoimport/ProjectSettingsTracker.kt
@@ -72,9 +72,13 @@ class ProjectSettingsTracker(
     return file.calculateCrc(project, projectAware.projectId.systemId)
   }
 
-  fun isUpToDate() = projectStatus.isUpToDate()
+  fun isUpToDate(): Boolean {
+    return projectStatus.isUpToDate()
+  }
 
-  fun getModificationType() = projectStatus.getModificationType()
+  fun getModificationType(): ExternalSystemModificationType {
+    return projectStatus.getModificationType()
+  }
 
   fun getSettingsContext(): ExternalSystemSettingsFilesReloadContext {
     val status = settingsFilesStatus.get()
@@ -141,7 +145,9 @@ class ProjectSettingsTracker(
     return status
   }
 
-  fun getState() = State(projectStatus.isDirty(), settingsFilesStatus.get().oldCRC.toMap())
+  fun getState(): State {
+    return State(projectStatus.isDirty(), settingsFilesStatus.get().oldCRC.toMap())
+  }
 
   fun loadState(state: State) {
     val operationStamp = Stamp.nextStamp()
@@ -226,15 +232,13 @@ class ProjectSettingsTracker(
     }
   }
 
-  fun beforeApplyChanges(
-    parentDisposable: Disposable,
-    listener: () -> Unit
-  ) = applyChangesOperation.whenOperationStarted(parentDisposable, listener)
+  fun beforeApplyChanges(parentDisposable: Disposable, listener: () -> Unit) {
+    applyChangesOperation.whenOperationStarted(parentDisposable, listener)
+  }
 
-  fun afterApplyChanges(
-    parentDisposable: Disposable,
-    listener: () -> Unit
-  ) = applyChangesOperation.whenOperationFinished(parentDisposable, listener)
+  fun afterApplyChanges(parentDisposable: Disposable, listener: () -> Unit) {
+    applyChangesOperation.whenOperationFinished(parentDisposable, listener)
+  }
 
   init {
     projectAware.subscribe(ProjectListener(), parentDisposable)
