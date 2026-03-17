a1d0eded304d0ac089875ca050ab7fc197be0131
Aleksandra Olemskaia <aleksandra.olemskaia@jetbrains.com>
2026-03-17 21:09:17 +0100

    IJPL-581: replace MUQ with DebouncedUpdates in UnknownSdkCollectorQueue
    - Use UpdateQueue with runLatest for simple debounce (original muq has fixed identity)
    - usePassThroughInUnitTestMode() is not needed: updates bypass the queue in UnitTestMode
    
    GitOrigin-RevId: 7b17d28ecd238d9f7e04d4a7a00ad8d8a40150fe

--- platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkTrackerQueue.kt
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkTrackerQueue.kt
@@ -7,14 +7,14 @@ import com.intellij.openapi.application.readAction
 import com.intellij.openapi.application.runReadActionBlocking
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
-import com.intellij.openapi.progress.runBlockingMaybeCancellable
 import com.intellij.openapi.project.Project
-import com.intellij.util.ui.update.MergingUpdateQueue
-import com.intellij.util.ui.update.Update
+import com.intellij.util.ui.update.DebouncedUpdates
+import com.intellij.util.ui.update.UpdateQueue
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jetbrains.annotations.ApiStatus
+import kotlin.time.Duration.Companion.milliseconds
 
 @Service(Service.Level.PROJECT)
 internal class UnknownSdkTrackerQueue(coroutineScope: CoroutineScope) : UnknownSdkCollectorQueue(mergingTimeSpaceMillis = 700, coroutineScope = coroutineScope) {
@@ -24,12 +24,11 @@ internal class UnknownSdkTrackerQueue(coroutineScope: CoroutineScope) : UnknownS
 }
 
 internal abstract class UnknownSdkCollectorQueue(mergingTimeSpaceMillis : Int, coroutineScope: CoroutineScope) {
-  @Suppress("DEPRECATION")
-  private val updateQueue = MergingUpdateQueue.mergingUpdateQueue(
-    name = javaClass.simpleName,
-    mergingTimeSpan = mergingTimeSpaceMillis,
-    coroutineScope = coroutineScope,
-  ).usePassThroughInUnitTestMode()
+  private val updateQueue: UpdateQueue<UnknownSdkTrackerTask> = DebouncedUpdates.forScope<UnknownSdkTrackerTask>(
+    coroutineScope,
+    javaClass.simpleName,
+    mergingTimeSpaceMillis.milliseconds
+  ).runLatest { task -> executeTask(task) }
 
   fun queue(task: UnknownSdkTrackerTask) {
     if (ApplicationManager.getApplication().isUnitTestMode) {
@@ -39,25 +38,18 @@ internal abstract class UnknownSdkCollectorQueue(mergingTimeSpaceMillis : Int, c
       return
     }
 
-    updateQueue.queue(object : Update(this) {
-      override suspend fun execute() {
-        val collector = task.createCollector() ?: return
-        val result = readAction {
-          collector.collectSdksUnderReadAction()
-        }
+    updateQueue.queue(task)
+  }
 
-        withContext(Dispatchers.EDT) {
-          task.onLookupCompleted(result)
-        }
-      }
+  private suspend fun executeTask(task: UnknownSdkTrackerTask) {
+    val collector = task.createCollector() ?: return
+    val result = readAction {
+      collector.collectSdksUnderReadAction()
+    }
 
-      // not expected to be called in production
-      override fun run() {
-        runBlockingMaybeCancellable {
-         execute()
-        }
-      }
-    })
+    withContext(Dispatchers.EDT) {
+      task.onLookupCompleted(result)
+    }
   }
 }
 
