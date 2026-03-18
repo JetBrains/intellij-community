32b5e3288e6045188ddf5d243aa4e67006d6f79e
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2026-03-18 17:06:23 +0100

    IJPL-895 Regenerate entities in intellij.platform.projectModel (test entity sources removed from metadata)
    (cherry picked from commit b488c1a72c217efcfed475ddcd32e9d541db2a2f)
    
    IJ-MR-196806
    
    GitOrigin-RevId: c1fd2ec496be518a6d02e75330367c595622c1bc

--- platform/projectModel-api/gen/com/intellij/workspaceModel/ide/ProjectRootEntityModifications.kt
+++ platform/projectModel-api/gen/com/intellij/workspaceModel/ide/ProjectRootEntityModifications.kt
@@ -3,7 +3,11 @@
 
 package com.intellij.workspaceModel.ide
 
-import com.intellij.platform.workspace.storage.*
+import com.intellij.platform.workspace.storage.EntitySource
+import com.intellij.platform.workspace.storage.EntityType
+import com.intellij.platform.workspace.storage.GeneratedCodeApiVersion
+import com.intellij.platform.workspace.storage.MutableEntityStorage
+import com.intellij.platform.workspace.storage.WorkspaceEntityBuilder
 import com.intellij.platform.workspace.storage.url.VirtualFileUrl
 import org.jetbrains.annotations.ApiStatus.Internal
 
