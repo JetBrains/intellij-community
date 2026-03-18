92639f41d5d910943a5479d7eaaa4f99a7dd0a29
Mathias <mathias.boulay@jetbrains.com>
2026-03-18 16:57:42 +0100

    IDEA-387100 Javadoc: inspection is triggered when it shouldn't.
    GitOrigin-RevId: 27d8d5194dc2c1ef1734ff374762a53fb1b2f51d

--- java/java-tests/testData/inspection/javadocLinkAsPlainText/MarkdownElements.java
+++ java/java-tests/testData/inspection/javadocLinkAsPlainText/MarkdownElements.java
@@ -1,6 +1,11 @@
 
 /// [Beep Boop](https://www.wikipedia.org/)
-class MarkdownLinks {}
+/// [https://jetbrains.com][String]
+/// `https://jetbrains.com`
+/// ```
+/// Hello https://jetbrains.com
+/// ```
+class MarkdownElements {}
 
 /// <warning descr="Link specified as plain text">https://www.wikipedia.org/</warning>
 class NoMarkdownLinks {}
