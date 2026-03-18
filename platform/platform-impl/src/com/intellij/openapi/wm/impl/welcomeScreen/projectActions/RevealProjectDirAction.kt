791e5eeb14900e90b2d0cc28fcbb2043ac08d3ec
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2026-03-18 13:32:22 +0100

    IJPL-158198 [platform]: adjusting the "reveal project directory" action to host's capabilities
    GitOrigin-RevId: bb0dd95710f9199b7205f51bc5f90a5ca4492942

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/projectActions/RevealProjectDirAction.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/projectActions/RevealProjectDirAction.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.wm.impl.welcomeScreen.projectActions
 
 import com.intellij.ide.actions.RevealFileAction
@@ -10,18 +10,26 @@ import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.wm.impl.welcomeScreen.projectActions.RecentProjectsWelcomeScreenActionBase.Companion.getSelectedItems
 import com.intellij.openapi.wm.impl.welcomeScreen.recentProjects.RecentProjectItem
 import java.nio.file.Path
+import kotlin.io.path.isDirectory
 
+@Suppress("ActionPresentationInstantiatedInCtor")
 class RevealProjectDirAction : DumbAwareAction(RevealFileAction.getActionName()), LightEditCompatible, ActionRemoteBehaviorSpecification.Disabled {
   override fun update(e: AnActionEvent) {
-    val items = getSelectedItems(e)
-    e.presentation.isEnabledAndVisible = items?.any { it is RecentProjectItem } == true
+    val hasItemsToOpen = getSelectedItems(e)?.any { it is RecentProjectItem } == true
+    e.presentation.isEnabled = RevealFileAction.isDirectoryOpenSupported() && hasItemsToOpen
+    e.presentation.isVisible = hasItemsToOpen
   }
 
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
 
   override fun actionPerformed(e: AnActionEvent) {
     getSelectedItems(e)?.filterIsInstance<RecentProjectItem>()?.forEach { item ->
-      RevealFileAction.openFile(Path.of(item.projectPath))
+      val projectPath = Path.of(item.projectPath)
+      when {
+        RevealFileAction.isSupported() -> RevealFileAction.openFile(projectPath)
+        projectPath.isDirectory() -> RevealFileAction.openDirectory(projectPath)
+        else -> RevealFileAction.openDirectory(projectPath.parent!!)
+      }
     }
   }
 }
