cbb308ad3198401ff2a9a2274847324f52859a6f
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2026-03-18 10:23:20 +0100

    [terminal] IJPL-231853 use default `AlphaComposite.SrcOver` in `JBTerminalPanel`
    Previously, we had the following problem with `AlphaComposite.Src` on macOS:
    (Theme: Monokai Pro)
    Filling a rect with color `rgba(252 252 250 31)` (Editor | Selection Background) over `rgb(34 31 34)` (Console | System output) resulted in the actual color `rgb(30 30 30)` instead of expected `rgb(60 57 60)`.
    
    GitOrigin-RevId: 6e9ab0245a889c8911d220d6e231eccb0bcf33dc

--- platform/execution-impl/src/com/intellij/terminal/JBTerminalPanel.java
+++ platform/execution-impl/src/com/intellij/terminal/JBTerminalPanel.java
@@ -231,7 +231,7 @@ public class JBTerminalPanel extends TerminalPanel implements FocusListener, Ter
 
   @Override
   protected void setupAntialiasing(Graphics graphics) {
-    UIUtil.setupComposite((Graphics2D)graphics);
+    //UIUtil.setupComposite((Graphics2D)graphics);
     UISettings.setupAntialiasing(graphics);
   }
 
