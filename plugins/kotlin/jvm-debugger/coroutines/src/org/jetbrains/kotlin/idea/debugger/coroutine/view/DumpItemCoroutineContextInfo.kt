dd1ff4b44328fe6ba580441740ebde6d18dea6c2
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2026-03-18 16:21:58 +0100

    [threadDump] IDEA-369748: take only dispatcher into account for merging dumps
    (cherry picked from commit 0b53191cff1f83cf5a5e2df2ec540d74196ac915)
    
    
    (cherry picked from commit f535f9d3e42ed2ca576a2369c0eb55c05f07ef1f)
    
    IJ-CR-198171
    
    GitOrigin-RevId: 6c1fcd43e518570a639bf000fdab3b82d7668b58

---
+++ plugins/kotlin/jvm-debugger/coroutines/src/org/jetbrains/kotlin/idea/debugger/coroutine/view/DumpItemCoroutineContextInfo.kt
@@ -0,0 +1,51 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.debugger.coroutine.view
+
+import org.jetbrains.kotlin.idea.debugger.coroutine.data.CoroutineInfoData
+
+/**
+ * Additional info about [CoroutineDumpItem] holding raw [kotlinx.coroutines.CoroutineDispatcher] and [kotlinx.coroutines.Job]
+ */
+internal data class DumpItemCoroutineContextInfo(
+    val dispatcher: String?,
+    val job: String?,
+) {
+    companion object {
+        /**
+         * Builds dump-item context info from live debugger data.
+         */
+        fun from(info: CoroutineInfoData): DumpItemCoroutineContextInfo = DumpItemCoroutineContextInfo(info.dispatcher, info.job)
+
+        /**
+         * Parses the serialized dump-item fragment, for example
+         * `"[dispatcher=Dispatchers.Default, job=StandaloneCoroutine{Active}]"`.
+         */
+        fun parse(serialized: String?): DumpItemCoroutineContextInfo {
+            val rawInfo = serialized?.trim() ?: return DumpItemCoroutineContextInfo(null, null)
+            if (rawInfo.firstOrNull() != '[' || rawInfo.lastOrNull() != ']') {
+                return DumpItemCoroutineContextInfo(null, null)
+            }
+
+            var dispatcher: String? = null
+            var job: String? = null
+            for (entry in rawInfo.removePrefix("[").removeSuffix("]").split(", ")) {
+                when {
+                    entry.startsWith("dispatcher=") -> dispatcher = entry.removePrefix("dispatcher=")
+                    entry.startsWith("job=") -> job = entry.removePrefix("job=")
+                }
+            }
+            return DumpItemCoroutineContextInfo(dispatcher, job)
+        }
+    }
+}
+
+/**
+ * Serializes this context info back into the dump-item header format.
+ * Example: "[dispatcher=Dispatchers.Default, job=StandaloneCoroutine{Active}]"
+ */
+internal fun DumpItemCoroutineContextInfo.serialize(): String {
+    return buildList {
+        dispatcher?.let { add("dispatcher=$it") }
+        job?.let { add("job=$it") }
+    }.joinToString(prefix = "[", separator = ", ", postfix = "]")
+}
