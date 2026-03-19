afef5877240059726476a8755f5f110a8be66b3f
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2026-03-19 09:57:24 +0100

    [threading] IDEA-387207: Fix 'extract field' refactorings
    GitOrigin-RevId: e376b30423f41482b10d26e924061e257e1ceba9

--- java/java-impl-refactorings/src/com/intellij/refactoring/introduceField/InplaceIntroduceFieldPopup.java
+++ java/java-impl-refactorings/src/com/intellij/refactoring/introduceField/InplaceIntroduceFieldPopup.java
@@ -1,7 +1,9 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.refactoring.introduceField;
 
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.application.WriteIntentReadAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
@@ -168,7 +170,9 @@ public class InplaceIntroduceFieldPopup extends AbstractInplaceIntroduceFieldPop
       myIntroduceFieldPanel.addOccurrenceListener(new ItemListener() {
         @Override
         public void itemStateChanged(ItemEvent e) {
-          restartInplaceIntroduceTemplate();
+          WriteIntentReadAction.run(() -> {
+            restartInplaceIntroduceTemplate();
+          });
         }
       });
 
