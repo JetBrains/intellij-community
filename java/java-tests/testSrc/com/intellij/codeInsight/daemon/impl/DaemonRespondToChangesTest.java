167d1c48bafc61b8168f5eba6b4891c7792cfa24
Alexey Kudravtsev <Alexey.Kudravtsev@jetbrains.com>
2026-03-19 17:20:00 +0100

    cancel daemon right after evicted infos were detected, restart it more granularly, more cancellations (IJPL-235455)
    (cherry picked from commit 4c5c0d5dbb653f4597d02ae2773c4c29d7bdc35f)
    
    
    (cherry picked from commit a360d672c256bc4c3bb4d0ffbffb985a34694685)
    
    IJ-MR-203095
    
    GitOrigin-RevId: 8b0b5e4fdf0a846ab36c1f4c80739cc06c779063

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/impl/DaemonRespondToChangesTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/impl/DaemonRespondToChangesTest.java
@@ -116,9 +116,9 @@ import com.intellij.testFramework.LightPlatformCodeInsightTestCase;
 import com.intellij.testFramework.PlatformTestUtil;
 import com.intellij.testFramework.SkipSlowTestLocally;
 import com.intellij.testFramework.fixtures.impl.CodeInsightTestFixtureImpl;
-import com.intellij.util.ArrayUtilRt;
 import com.intellij.ui.EditorNotifications;
 import com.intellij.ui.EditorNotificationsImpl;
+import com.intellij.util.ArrayUtilRt;
 import com.intellij.util.DocumentUtil;
 import com.intellij.util.ExceptionUtil;
 import com.intellij.util.TestTimeOut;
@@ -141,6 +141,7 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.io.File;
 import java.io.IOException;
+import java.lang.ref.Reference;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -1963,9 +1964,6 @@ public class DaemonRespondToChangesTest extends DaemonAnalyzerTestCase {
     configureByFiles(null, BASE_PATH+getTestName(true)+"/p2/A2222.java", BASE_PATH+getTestName(true)+"/p1/A1111.java");
     assertEquals("A2222.java", getFile().getName());
 
-    HighlightInfo info = assertOneElement(waitHighlighting(getProject(), getEditor().getDocument(), HighlightSeverity.WARNING));
-    assertEquals("Class 'A2222' is never used", info.getDescription());
-
     Document document1111 = getFile().getParent().findFile("A1111.java").getFileDocument();
     PsiFile psiFile1111 = PsiDocumentManager.getInstance(getProject()).getPsiFile(document1111);// avoid "cached psiFile is null, cancel all passes"
     Editor editor1111 = ((TextEditor)FileEditorManager.getInstance(getProject())
@@ -1973,14 +1971,14 @@ public class DaemonRespondToChangesTest extends DaemonAnalyzerTestCase {
     Editor editor2222 = getEditor();
     setActiveEditors(editor2222, editor1111);
 
-    HighlightInfo info = assertOneElement(myTestDaemonCodeAnalyzer.waitHighlighting(editor2222.getDocument(), HighlightSeverity.WARNING));
+    HighlightInfo info = assertOneElement(waitHighlighting(getProject(), editor2222.getDocument(), HighlightSeverity.WARNING));
     assertEquals("Class 'A2222' is never used", info.getDescription());
 
     // uncomment (inside code block) the reference to A2222
     WriteCommandAction.writeCommandAction(myProject).run(()->document1111.deleteString(document1111.getText().indexOf("//"), document1111.getText().indexOf("//")+2));
 
     // now A2222 is no longer unused
-    assertEmpty(myTestDaemonCodeAnalyzer.waitHighlighting(editor2222.getDocument(), HighlightSeverity.WARNING));
+    assertEmpty(waitHighlighting(getProject(), editor2222.getDocument(), HighlightSeverity.WARNING));
     Reference.reachabilityFence(psiFile1111);
   }
 
