536819092399bdaa244c0d6df1a49ce19b3c380a
Nebojsa Vuksic <nebojsa.vuksic@jetbrains.com>
2026-03-19 14:58:47 +0100

    [JEWEL-1297] Update Jewel release flow and Metalava API compatibility checks
    Signed-off-by: Nebojsa.Vuksic <nebojsa.vuksic@jetbrains.com>
    
    closes https://github.com/JetBrains/intellij-community/pull/3465
    
    (cherry picked from commit a1fe9caac69247e9278cc2b8ed62df98f28d1247)
    
    GitOrigin-RevId: 6e67d48d54b580939939c9f040d2cb4611bd84b1

---
+++ platform/jewel/buildSrc/src/main/kotlin/org/jetbrains/jewel/buildlogic/metalava/patcher/PatchWriter.kt
@@ -0,0 +1,42 @@
+package org.jetbrains.jewel.buildlogic.metalava.patcher
+
+import java.io.File
+
+/**
+ * Writes generated patch text to a file.
+ *
+ * @param patchFile Destination file for the patch output.
+ */
+internal class PatchWriter(private val patchFile: File) {
+    /** Outcome of a patch file write attempt. */
+    sealed interface PatchWriteResult {
+        /** Write succeeded; [patchFile] is the written file. */
+        data class Success(val patchFile: File) : PatchWriteResult
+
+        /** Write failed; [message] describes the error. */
+        data class Failure(val message: String) : PatchWriteResult
+        
+        /** Returns `true` if this result represents a successful write. */
+        fun isSuccess() = this is Success
+    }
+
+    /**
+     * Writes [patchText] to the configured file and returns the outcome.
+     *
+     * @param patchText Unified diff content to persist.
+     */
+    fun write(patchText: String): PatchWriteResult {
+        patchFile.parentFile.mkdirs()
+        
+        return runCatching {
+            patchFile.writeText(patchText, Charsets.UTF_8)
+        }.fold(
+            onSuccess = { PatchWriteResult.Success(patchFile) },
+            onFailure = { t ->
+                PatchWriteResult.Failure(
+                    "Failed to write patch file '${patchFile.absolutePath}': ${t.message ?: t::class.java.name}"
+                )
+            },
+        )
+    }
+}
