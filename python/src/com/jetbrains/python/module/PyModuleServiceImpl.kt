85f2b3a5eddaa12b82c9ee61a01723c0ba448311
Vitaly Legchilkin <vitaly.legchilkin@jetbrains.com>
2026-03-20 12:05:27 +0100

    PY-88280 Add timeout workaround for isJpsProjectLoaded in PyModuleServiceImpl
    If the jpsProjectLoaded callback is never invoked (e.g. due to a platform
    bug), findPythonSdkWaitingForProjectModel hangs forever. Add a 10-second
    safety-net coroutine that auto-completes the deferred and logs an error.
    The timeout accounts for the 5-second delay in JpsGlobalModelSynchronizerImpl
    and the 1-second delay in ModuleManagerComponentBridgeInitializer.
    
    GitOrigin-RevId: e4ba206653a682dfa293bea72011b78c3547fc5b

--- python/src/com/jetbrains/python/module/PyModuleServiceImpl.kt
+++ python/src/com/jetbrains/python/module/PyModuleServiceImpl.kt
@@ -1,7 +1,8 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.module
 
 import com.intellij.facet.FacetManager
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleType
 import com.intellij.openapi.project.Project
@@ -12,17 +13,30 @@ import com.intellij.workspaceModel.ide.JpsProjectLoadingManager
 import com.jetbrains.python.PyNames
 import com.jetbrains.python.PythonModuleTypeBase
 import com.jetbrains.python.facet.PythonFacetSettings
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
 
-internal class PyModuleServiceImpl(val project: Project) : PyModuleService {
+internal class PyModuleServiceImpl(val project: Project, private val coroutineScope: CoroutineScope) : PyModuleService {
   // Lazy to avoid calling JpsProjectLoadingManager.jpsProjectLoaded during service construction.
   // jpsProjectLoaded acquires a @Synchronized lock on JpsProjectLoadingManagerImpl, which may
   // already be held by a worker thread running JpsProjectLoadedListenerImpl.loaded() (triggered
   // by ModuleBridgeLoaderService after modules finish loading). If the service is instantiated
   // on EDT, EDT blocks on that lock.
   private val isJpsProjectLoaded by lazy {
-    CompletableDeferred<Unit>().also {
-      JpsProjectLoadingManager.getInstance(project).jpsProjectLoaded { it.complete(Unit) }
+    CompletableDeferred<Unit>().also { deferred ->
+      JpsProjectLoadingManager.getInstance(project).jpsProjectLoaded { deferred.complete(Unit) }
+
+      // Safety net: if the callback is never invoked (e.g. due to a platform bug), unblock waiters after a timeout.
+      coroutineScope.launch {
+        // JpsGlobalModelSynchronizerImpl has a 5-second delay and ModuleManagerComponentBridgeInitializer has a 1-second delay;
+        delay(10.seconds)
+        if (deferred.complete(Unit)) {
+          thisLogger().error("JPS project loaded callback was not called within 10 seconds, proceeding anyway")
+        }
+      }
     }
   }
 
