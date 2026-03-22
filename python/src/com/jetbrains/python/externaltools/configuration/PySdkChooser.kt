36c06f459cec30c35c1d5ae658d87d3192700434
Morgan Bartholomew <morgan.bartholomew@jetbrains.com>
2026-03-22 15:53:34 +1000

    PY-88583 lsp-tool: fix install tool button
    GitOrigin-RevId: 366f4ea73bf77f036a6887c437e0e93065b025f3

--- python/src/com/jetbrains/python/externaltools/configuration/PySdkChooser.kt
+++ python/src/com/jetbrains/python/externaltools/configuration/PySdkChooser.kt
@@ -1,7 +1,8 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.externaltools.configuration
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.modules
 import com.intellij.openapi.projectRoots.Sdk
 import com.jetbrains.python.sdk.PythonSdkUtil
 import com.jetbrains.python.sdk.pythonSdk
@@ -9,4 +10,4 @@ import com.jetbrains.python.sdk.pythonSdk
 const val DEFAULT_ENVIRONMENT: String = "Project Default"
 
 fun Project.sdkByNameDefaultAware(name: String): Sdk? =
-  if (name == DEFAULT_ENVIRONMENT) pythonSdk else PythonSdkUtil.findSdkByKey(name)
+  if (name == DEFAULT_ENVIRONMENT) pythonSdk ?: modules.firstOrNull()?.pythonSdk else PythonSdkUtil.findSdkByKey(name)
