2dd00a459080eb229eae424ad723e675eb91b085
Morgan Bartholomew <morgan.bartholomew@jetbrains.com>
2026-03-23 14:18:24 +1000

    PY-86281/PY-86020 lsp: support multi-module projects
    GitOrigin-RevId: 16e423f55148145d35462948aeaea1a4f5c9b6c9

--- python/src/com/jetbrains/python/externaltools/configuration/PyLspToolConfiguration.kt
+++ python/src/com/jetbrains/python/externaltools/configuration/PyLspToolConfiguration.kt
@@ -1,10 +1,15 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.externaltools.configuration
 
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.util.xmlb.XmlSerializerUtil
 import java.nio.file.Path
 import kotlin.io.path.Path
+
+/**
+ * Storage file for LSP tool settings. This file is saved in .idea/ and can be committed to VCS.
+ */
+const val LSP_TOOLS_STORAGE_FILE: String = "pyLspTools.xml"
 
 interface PyLspExecutionConfiguration {
   var executableDiscoveryMode: ExecutableDiscoveryMode
@@ -13,21 +18,33 @@ interface PyLspExecutionConfiguration {
   var sdkName: String
 }
 
+/**
+ * Common interface for LSP tool settings that can be used for both project-level
+ * and module-level settings. This allows UI bindings to work with either type.
+ */
+interface PyLspToolSettings : PyLspExecutionConfiguration {
+  var enabled: Boolean
+  var inspections: Boolean
+  var completions: Boolean?
+  var inlayHints: Boolean?
+  var documentation: Boolean?
+}
+
 abstract class PyLspToolConfiguration<State : PyLspToolConfiguration<State>> : PersistentStateComponent<State>,
-                                                                               PyLspExecutionConfiguration {
-  open var enabled: Boolean = false
-  var inspections: Boolean = true
-  open var completions: Boolean? = null
+                                                                               PyLspToolSettings {
+  override var enabled: Boolean = false
+  override var inspections: Boolean = true
+  override var completions: Boolean? = null
 
   /**
    * `null` means: not supported
    */
-  open var inlayHints: Boolean? = null
+  override var inlayHints: Boolean? = null
 
   /**
    * `null` means: not supported
    */
-  open var documentation: Boolean? = null
+  override var documentation: Boolean? = null
   override var executableDiscoveryMode: ExecutableDiscoveryMode = ExecutableDiscoveryMode.INTERPRETER
   override var pathToExecutable: String = ""
   override var sdkName: String = DEFAULT_ENVIRONMENT
