e630019ffa8b82d315db2a4303a2cd04641da9d2
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2026-03-23 14:20:49 +0100

    IJPL-895 Drop outdated PyProjectTomlWorkspaceEntityImpl.kt
    GitOrigin-RevId: e9257271754e0c684de9f5215452a2c6342a8f08

--- python/python-pyproject/gen/com/intellij/python/pyproject/model/internal/impl/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Workspace internal generated tools, do not touch
- */
-@ApiStatus.Internal
-package com.intellij.python.pyproject.model.internal.impl;
-
-import org.jetbrains.annotations.ApiStatus;
