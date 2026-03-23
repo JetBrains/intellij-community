c3e107863327a59c33eb51b065ab9febb5415e12
Ekaterina.Itsenko <ekaterina.itsenko@jetbrains.com>
2026-03-23 12:30:21 +0000

    [pycharm] PY-84800 IDE: fix validation errors and delete odd field
    Merge-request: IJ-MR-197033
    Merged-by: Ekaterina Itsenko <ekaterina.itsenko@jetbrains.com>
    
    GitOrigin-RevId: d8bad36c97003f31fd7df656d921087bbb045272

--- platform/platform-impl/src/com/intellij/help/impl/HelpUsageCollector.kt
+++ platform/platform-impl/src/com/intellij/help/impl/HelpUsageCollector.kt
@@ -4,21 +4,20 @@ package com.intellij.help.impl
 import com.intellij.internal.statistic.eventLog.EventLogGroup
 import com.intellij.internal.statistic.eventLog.events.EventFields
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
-import com.intellij.openapi.project.Project
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
 object HelpUsageCollector : CounterUsagesCollector() {
-  private val GROUP = EventLogGroup("help.usage", 1)
-  private val HELP_ID_FIELD = EventFields.StringValidatedByRegexpReference("help_id", "version")
+  private val GROUP = EventLogGroup("help.usage", 2)
+  private val HELP_ID_FIELD = EventFields.StringValidatedByInlineRegexp("help_id", "[a-zA-Z0-9_.\\-]+")
   private val HELP_OPENED_EVENT = GROUP.registerEvent("opened", HELP_ID_FIELD)
 
   override fun getGroup(): EventLogGroup = GROUP
 
   @JvmStatic
-  fun logOpened(project: Project?, helpId: String?) {
+  fun logOpened(helpId: String?) {
     if (!helpId.isNullOrBlank()) {
-      HELP_OPENED_EVENT.log(project, helpId)
+      HELP_OPENED_EVENT.log(helpId)
     }
   }
 }
