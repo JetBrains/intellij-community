352e162756bab2d13ad8800bd8dc0f5d8c3fb8b0
Nikolay Chashnikov <nikolay.chashnikov@jetbrains.com>
2026-03-24 17:34:58 +0100

    IJPL-238954 Maven: remove dependency from intellij.maven to intellij.maven.server.telemetry module
    `intellij.maven.server.telemetry` module isn't added to the IDE's classpath. The dependency `intellij.maven` on it worked because it was used only when running from source code with a single classloader. However, such a configuration is fragile, one may easily start using classes from other code and get `NoClassDefFoundError` at runtime. Now MavenServerTelemetryClasspathUtil class is moved to `intellij.maven` module and the dependency is removed.
    
    This change is necessary to enable checks for dependencies in *.iml file pointing to modules not present in the distribution (IJPL-238954).
    
    GitOrigin-RevId: 62e0533beb5401413d3287bc8b17945d122c53b5

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/telemetry/MavenServerTelemetryClasspathUtil.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/telemetry/MavenServerTelemetryClasspathUtil.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.maven.server.telemetry;
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.idea.maven.telemetry;
 
 import com.intellij.platform.diagnostic.telemetry.rt.PlatformTelemetryRtClass;
 import io.opentelemetry.api.OpenTelemetry;
