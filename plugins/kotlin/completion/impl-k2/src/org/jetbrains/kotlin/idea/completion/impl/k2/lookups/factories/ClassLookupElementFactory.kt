086af74b52084a68dc874ecbc233e0f29b06b146
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2026-03-24 11:24:49 +0100

    [kotlin] KTIJ-37938 Added K2 completion statistician
    This is required to distinguish between different `object` completion items. Without the custom statistician, all `object` items would share the same statistics key and once choosing any, all of them would be lifted up by the statistician weigher. The new statistician adds the FqName of the class being extended to distinguish between the items.
    
    GitOrigin-RevId: 30c565fa10b885bc47f97ae0d0b95b6a9a7ae35d

--- plugins/kotlin/completion/impl-k2/src/org/jetbrains/kotlin/idea/completion/impl/k2/lookups/factories/ClassLookupElementFactory.kt
+++ plugins/kotlin/completion/impl-k2/src/org/jetbrains/kotlin/idea/completion/impl/k2/lookups/factories/ClassLookupElementFactory.kt
@@ -21,6 +21,7 @@ import org.jetbrains.kotlin.analysis.api.types.KaStarTypeProjection
 import org.jetbrains.kotlin.analysis.api.types.KaTypeArgumentWithVariance
 import org.jetbrains.kotlin.analysis.api.types.KaTypeProjection
 import org.jetbrains.kotlin.idea.base.analysis.withRootPrefixIfNeeded
+import org.jetbrains.kotlin.idea.base.serialization.names.KotlinFqNameSerializer
 import org.jetbrains.kotlin.idea.base.serialization.names.KotlinNameSerializer
 import org.jetbrains.kotlin.idea.completion.impl.k2.handlers.AnonymousObjectInsertHandler
 import org.jetbrains.kotlin.idea.completion.impl.k2.lookups.CallableInsertionOptions
@@ -33,6 +34,7 @@ import org.jetbrains.kotlin.idea.completion.impl.k2.lookups.TailTextProvider
 import org.jetbrains.kotlin.idea.completion.impl.k2.lookups.addImportIfRequired
 import org.jetbrains.kotlin.idea.completion.impl.k2.lookups.withClassifierSymbolInfo
 import org.jetbrains.kotlin.idea.completion.impl.k2.weighers.KindWeigher.isConstructorCall
+import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.renderer.render
@@ -92,6 +94,7 @@ internal object ClassLookupElementFactory {
             append("{...}")
         }
 
+        val fqName = symbol.importableFqName ?: FqName.topLevel(name)
         val insertHandler = AnonymousObjectInsertHandler(
             constructorParenthesis = constructorParenthesis,
             renderedClassifier = symbol.importableFqName?.withRootPrefixIfNeeded()?.asString() ?: name.asString(),
@@ -99,7 +102,7 @@ internal object ClassLookupElementFactory {
             hasTypeArguments = hasTypeArguments,
         )
 
-        return LookupElementBuilder.create(AnonymousObjectLookupObject(name, importingStrategy), "object")
+        return LookupElementBuilder.create(AnonymousObjectLookupObject(name, importingStrategy, fqName), "object")
             .withPresentableText(itemText)
             .withLookupString(name.asString())
             .withInsertHandler(insertHandler)
@@ -154,7 +157,9 @@ internal data class ClassifierLookupObject(
 @Serializable
 internal data class AnonymousObjectLookupObject(
     @Serializable(with = KotlinNameSerializer::class) override val shortName: Name,
-    val importingStrategy: ImportStrategy
+    val importingStrategy: ImportStrategy,
+    @Serializable(with = KotlinFqNameSerializer::class)
+    val fqName: FqName,
 ) : KotlinLookupObject
 
 /**
