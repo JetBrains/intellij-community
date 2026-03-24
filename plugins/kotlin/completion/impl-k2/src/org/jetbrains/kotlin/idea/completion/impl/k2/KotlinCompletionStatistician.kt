d41a9f61aa26cd9e74606935f23c1a5270640f77
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2026-03-24 16:02:45 +0100

    [kotlin] KTIJ-37938 added KDocs for KotlinCompletionStatistician
    GitOrigin-RevId: b71dddb135781566acb9e07873ebd56f3409d2f1

--- plugins/kotlin/completion/impl-k2/src/org/jetbrains/kotlin/idea/completion/impl/k2/KotlinCompletionStatistician.kt
+++ plugins/kotlin/completion/impl-k2/src/org/jetbrains/kotlin/idea/completion/impl/k2/KotlinCompletionStatistician.kt
@@ -8,17 +8,25 @@ import com.intellij.psi.statistics.StatisticsInfo
 import org.jetbrains.kotlin.idea.completion.impl.k2.lookups.KotlinLookupObject
 import org.jetbrains.kotlin.idea.completion.impl.k2.lookups.factories.AnonymousObjectLookupObject
 
+/**
+ * This statistician is responsible for providing [StatisticsInfo] instances for certain Kotlin completion
+ * lookup items.
+ * This is required in case the itemText of the lookup item is not enough to distinguish it from other
+ * similar lookup items so they would all share the same statistics.
+ * For example, all anonymous object lookup items have the itemText `object`, so their statistics would collide
+ * without this statistician.
+ */
 internal class KotlinCompletionStatistician : CompletionStatistician() {
     override fun serialize(
         element: LookupElement,
         location: CompletionLocation
     ): StatisticsInfo? {
-        val o = element.`object` as? KotlinLookupObject ?: return null
+        val lookupObject = element.`object` as? KotlinLookupObject ?: return null
 
-        return if (o is AnonymousObjectLookupObject) {
+        return if (lookupObject is AnonymousObjectLookupObject) {
             // Anonymous objects all have the name `object` which would lead to collisions unless
             // we explicitly add the FqName here
-            StatisticsInfo("completion#Kotlin#AnonymousObject", o.fqName.asString())
+            StatisticsInfo("completion#Kotlin#AnonymousObject", lookupObject.fqName.asString())
         } else {
             null
         }
