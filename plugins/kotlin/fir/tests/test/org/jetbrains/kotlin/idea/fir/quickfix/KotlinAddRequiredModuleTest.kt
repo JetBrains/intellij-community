09ccaecfa8c38075231b3041dee0b6ca3babc402
Anna Kozlova <anna.kozlova@jetbrains.com>
2026-03-24 19:05:14 +0100

    [kotlin] enable even more tests for k2
    KTIJ-37643
    ignored under KTIJ-38114
    
    GitOrigin-RevId: 047e8358406a9988e9927f4a99361aadaa5ebdfa

---
+++ plugins/kotlin/fir/tests/test/org/jetbrains/kotlin/idea/fir/quickfix/KotlinAddRequiredModuleTest.kt
@@ -0,0 +1,104 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+package org.jetbrains.kotlin.idea.fir.quickfix
+
+import com.intellij.codeInsight.daemon.QuickFixBundle
+import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginMode
+import org.jetbrains.kotlin.idea.test.KotlinLightJava9ModulesCodeInsightFixtureTestCase
+import org.jetbrains.kotlin.idea.test.KotlinMultiModuleJava9ProjectDescriptor.ModuleDescriptor.M2
+import org.jetbrains.kotlin.idea.test.KotlinMultiModuleJava9ProjectDescriptor.ModuleDescriptor.M3
+import org.jetbrains.kotlin.idea.test.KotlinMultiModuleJava9ProjectDescriptor.ModuleDescriptor.MAIN
+import org.junit.internal.runners.JUnit38ClassRunner
+import org.junit.runner.RunWith
+
+@RunWith(JUnit38ClassRunner::class)
+class KotlinAddRequiredModuleTest : KotlinLightJava9ModulesCodeInsightFixtureTestCase() {
+    override val pluginMode: KotlinPluginMode = KotlinPluginMode.K2
+
+    private val messageM2 = QuickFixBundle.message("module.info.add.requires.name", "M_TWO")
+
+    override fun setUp() {
+        super.setUp()
+        moduleInfo("module M_TWO { exports pkgA; }", M2)
+        addJavaFile("pkgA/A.java", "package pkgA; public class A {}", M2)
+    }
+
+    // IGNORE_K2 KTIJ-38114
+    fun _testAddRequiresToModuleInfo() {
+        moduleInfo("module MAIN {}", MAIN)
+        val editedFile = addKotlinFile(
+            "pkgB/B.kt",
+            """
+                package pkgB
+                import pkgA.A
+                class B(a: /*|*/A)
+                """,
+            MAIN
+        )
+        myFixture.configureFromExistingVirtualFile(editedFile)
+
+        findActionAndExecute(messageM2)
+        assertNoErrors()
+
+        checkModuleInfo(
+            """
+                module MAIN {
+                    requires M_TWO;
+                }
+                """
+        )
+    }
+
+    fun testNoIdeaModuleDependency() {
+        moduleInfo("module M_THREE {}", M3)
+        val editedFile = addKotlinFile(
+            "pkgB/B.kt",
+            """
+                package pkgB
+                import pkgA.A
+                class B(a: /*|*/A)
+                """,
+            M3
+        )
+        myFixture.configureFromExistingVirtualFile(editedFile)
+
+        val actions = myFixture.filterAvailableIntentions(messageM2)
+        assertEmpty(actions)
+    }
+
+    // IGNORE_K2 KTIJ-38114
+    fun _testAddRequiresToInfoForJavaModule() {
+        moduleInfo("module MAIN {}", MAIN)
+        val editedFile = addKotlinFile(
+            "test.kt",
+            """
+                fun test() {
+                    java.util.logging./*|*/FileHandler()   // <-- error; "add 'requires java.logging'" quick fix expected
+                }
+                """,
+            MAIN
+        )
+        myFixture.configureFromExistingVirtualFile(editedFile)
+
+        findActionAndExecute(QuickFixBundle.message("module.info.add.requires.name", "java.logging"))
+
+        assertNoErrors()
+        checkModuleInfo(
+            """
+                module MAIN {
+                    requires java.logging;
+                }
+                """
+        )
+    }
+
+    private fun assertNoErrors() {
+        myFixture.checkHighlighting(false, false, false)
+    }
+
+    private fun findActionAndExecute(message: String) {
+        val action = myFixture.findSingleIntention(message)
+        assertNotNull(action)
+        myFixture.launchAction(action)
+    }
+}
