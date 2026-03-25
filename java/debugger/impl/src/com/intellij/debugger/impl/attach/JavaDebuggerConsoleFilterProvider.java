e5bcf3528f2b8436547d1fa1c021b3ceaeed014a
Egor Ushakov <egor.ushakov@jetbrains.com>
2026-03-25 11:45:23 +0100

    IDEA-384577 Can't Attach debugger on wsl2 via link - disable the link if a debug session is already attached
    GitOrigin-RevId: 966741922d55656d7772e08cfe2b19616159bb00

--- java/debugger/impl/src/com/intellij/debugger/impl/attach/JavaDebuggerConsoleFilterProvider.java
+++ java/debugger/impl/src/com/intellij/debugger/impl/attach/JavaDebuggerConsoleFilterProvider.java
@@ -1,18 +1,23 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.debugger.impl.attach;
 
+import com.intellij.codeInsight.hints.InlayContentListener;
+import com.intellij.codeInsight.hints.presentation.DynamicDelegatePresentation;
 import com.intellij.codeInsight.hints.presentation.InlayPresentation;
 import com.intellij.codeInsight.hints.presentation.PresentationFactory;
 import com.intellij.codeInsight.hints.presentation.PresentationRenderer;
 import com.intellij.debugger.DebuggerManagerEx;
+import com.intellij.debugger.JavaDebuggerBundle;
 import com.intellij.debugger.actions.JavaDebuggerActionsCollector;
+import com.intellij.debugger.impl.DebuggerManagerListener;
+import com.intellij.debugger.impl.DebuggerSession;
 import com.intellij.execution.filters.ConsoleFilterProvider;
 import com.intellij.execution.filters.Filter;
 import com.intellij.execution.impl.InlayProvider;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.EditorCustomElementRenderer;
+import com.intellij.openapi.editor.Inlay;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.registry.Registry;
 import org.jetbrains.annotations.NotNull;
@@ -65,7 +70,7 @@ public final class JavaDebuggerConsoleFilterProvider implements ConsoleFilterPro
 
       // to trick the code unwrapping single results in com.intellij.execution.filters.CompositeFilter#createFinalResult
       return new Result(Arrays.asList(
-        new AttachInlayResult(start + matcher.start(), start + matcher.end(), transport, address),
+        new AttachInlayResult(start + matcher.start(), start + matcher.end(), transport, address, myProject),
         new ResultItem(0, 0, null)));
     }
   }
@@ -80,24 +85,117 @@ public final class JavaDebuggerConsoleFilterProvider implements ConsoleFilterPro
   private static class AttachInlayResult extends Filter.ResultItem implements InlayProvider {
     private final String myTransport;
     private final String myAddress;
+    private final @NotNull Project myProject;
 
-    AttachInlayResult(int highlightStartOffset, int highlightEndOffset, String transport, String address) {
+    AttachInlayResult(int highlightStartOffset,
+                      int highlightEndOffset,
+                      String transport,
+                      String address,
+                      @NotNull Project project) {
       super(highlightStartOffset, highlightEndOffset, null);
       myTransport = transport;
       myAddress = address;
+      myProject = project;
     }
 
     @Override
-    public EditorCustomElementRenderer createInlayRenderer(Editor editor) {
-      JavaDebuggerActionsCollector.attachFromConsoleInlayShown.log();
+    public @Nullable Inlay<?> createInlay(@NotNull Editor editor, int offset) {
+      AttachDebuggerInlayPresentation presentation = new AttachDebuggerInlayPresentation(editor, myTransport, myAddress, myProject);
+      if (!presentation.isAttached()) {
+        JavaDebuggerActionsCollector.attachFromConsoleInlayShown.log();
+      }
+      Inlay<?> inlay = editor.getInlayModel().addInlineElement(offset, new PresentationRenderer(presentation));
+      if (inlay == null) {
+        return null;
+      }
+      presentation.installListeners(inlay);
+      return inlay;
+    }
+  }
+
+  private static final class AttachDebuggerInlayPresentation extends DynamicDelegatePresentation {
+    private final @NotNull InlayPresentation myAttachPresentation;
+    private final @NotNull InlayPresentation myAttachedPresentation;
+    private final @NotNull String myTransport;
+    private final @NotNull String myAddress;
+    private final @NotNull Project myProject;
+
+    private AttachDebuggerInlayPresentation(@NotNull Editor editor,
+                                            @NotNull String transport,
+                                            @NotNull String address,
+                                            @NotNull Project project) {
+      this(createAttachPresentation(editor, transport, address, project),
+           createAttachedPresentation(editor),
+           transport,
+           address,
+           project);
+    }
+
+    private AttachDebuggerInlayPresentation(@NotNull InlayPresentation attachPresentation,
+                                            @NotNull InlayPresentation attachedPresentation,
+                                            @NotNull String transport,
+                                            @NotNull String address,
+                                            @NotNull Project project) {
+      super(isDebuggerAttached(transport, address, project) ? attachedPresentation : attachPresentation);
+      myAttachPresentation = attachPresentation;
+      myAttachedPresentation = attachedPresentation;
+      myTransport = transport;
+      myAddress = address;
+      myProject = project;
+    }
+
+    private boolean isAttached() {
+      return getDelegate() == myAttachedPresentation;
+    }
+
+    private void installListeners(@NotNull Inlay<?> inlay) {
+      addListener(new InlayContentListener(inlay));
+      myProject.getMessageBus().connect(inlay).subscribe(DebuggerManagerListener.TOPIC, new DebuggerManagerListener() {
+        @Override
+        public void sessionCreated(DebuggerSession session) {
+          updateLater(inlay);
+        }
+
+        @Override
+        public void sessionRemoved(DebuggerSession session) {
+          updateLater(inlay);
+        }
+      });
+      update();
+    }
+
+    private void updateLater(@NotNull Inlay<?> inlay) {
+      ApplicationManager.getApplication().invokeLater(() -> {
+        if (!inlay.isValid()) {
+          return;
+        }
+        update();
+      }, ModalityState.any());
+    }
+
+    private void update() {
+      InlayPresentation delegate = isDebuggerAttached(myTransport, myAddress, myProject) ? myAttachedPresentation : myAttachPresentation;
+      if (getDelegate() != delegate) {
+        setDelegate(delegate);
+      }
+    }
+
+    private static @NotNull InlayPresentation createAttachPresentation(@NotNull Editor editor,
+                                                                       @NotNull String transport,
+                                                                       @NotNull String address,
+                                                                       @NotNull Project project) {
       PresentationFactory factory = new PresentationFactory(editor);
-      InlayPresentation presentation = factory.referenceOnHover(
-        factory.roundWithBackground(factory.smallText("Attach debugger")),
+      return factory.referenceOnHover(
+        factory.roundWithBackground(factory.smallText(JavaDebuggerBundle.message("debugger.console.inlay.attach"))),
         (event, point) -> {
           JavaDebuggerActionsCollector.attachFromConsoleInlay.log();
-          JavaAttachDebuggerProvider.attach(myTransport, myAddress, null, editor.getProject());
+          JavaAttachDebuggerProvider.attach(transport, address, null, project);
         });
-      return new PresentationRenderer(presentation);
+    }
+
+    private static @NotNull InlayPresentation createAttachedPresentation(@NotNull Editor editor) {
+      PresentationFactory factory = new PresentationFactory(editor);
+      return factory.roundWithBackground(factory.smallText(JavaDebuggerBundle.message("debugger.console.inlay.attached")));
     }
   }
 }
