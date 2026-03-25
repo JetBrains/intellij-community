d0347f067f437cee2560dfd6e2c8ad39a793ac9a
Vitaly Legchilkin <vitaly.legchilkin@jetbrains.com>
2026-03-25 17:48:31 +0100

    PY-88244 Pass project to UrlFilter so file:// URIs open in file manager
    UrlFilter requires a non-null Project to resolve file:// URLs as
    FileUrlHyperlinkInfo. Without it, all file:// links fall through to
    OpenUrlHyperlinkInfo and open in the browser instead.
    
    GitOrigin-RevId: 2283497565f2d1e3243fff9894d1c0ad64d97153

--- python/src/com/jetbrains/python/run/PythonCommandLineState.java
+++ python/src/com/jetbrains/python/run/PythonCommandLineState.java
@@ -1142,7 +1142,7 @@ public abstract class PythonCommandLineState extends CommandLineState {
   }
 
   protected @NotNull UrlFilter createUrlFilter(ProcessHandler handler) {
-    return new UrlFilter();
+    return new UrlFilter(myConfig.getProject());
   }
 
   protected @NotNull Function<TargetEnvironment, String> getTargetPath(@NotNull TargetEnvironmentRequest targetEnvironmentRequest,
