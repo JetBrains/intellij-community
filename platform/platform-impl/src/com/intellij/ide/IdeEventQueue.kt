70e2f4a2a913c3446c6972651901abc8aac985fe
Dmitry Drobotov <dmitry.drobotov@jetbrains.com>
2026-03-26 11:06:11 +0100

    IJPL-238972 Don't trigger Alt press in WindowsAltSuppressor when another key was pressed when Alt was down
    This is needed to prevent Alt presses from closing popups when they are opened with a shortcut containing Alt, e.g., Alt+Insert.
    
    GitOrigin-RevId: 21173e775c44c4892bee6a40ff2e940fcbd7eaba

--- platform/platform-impl/src/com/intellij/ide/IdeEventQueue.kt
+++ platform/platform-impl/src/com/intellij/ide/IdeEventQueue.kt
@@ -1276,6 +1276,7 @@ private fun cancelCellEditing(): Boolean {
 
 private class WindowsAltSuppressor : IdeEventQueue.NonLockedEventDispatcher {
   private var waitingForAltRelease = false
+  private var altPressedOnly = false
   private var robot: Robot? = null
 
   override fun dispatch(e: AWTEvent): Boolean = e is KeyEvent && dispatchKeyEvent(e)
@@ -1285,6 +1286,9 @@ private class WindowsAltSuppressor : IdeEventQueue.NonLockedEventDispatcher {
     val pureAlt = ke.keyCode == KeyEvent.VK_ALT && ke.modifiers or InputEvent.ALT_MASK == InputEvent.ALT_MASK
     if (!pureAlt) {
       waitingForAltRelease = false
+      if (altPressedOnly && ke.id == KeyEvent.KEY_PRESSED) {
+        altPressedOnly = false
+      }
       return false
     }
 
@@ -1302,13 +1306,14 @@ private class WindowsAltSuppressor : IdeEventQueue.NonLockedEventDispatcher {
     var dispatch = true
     if (ke.id == KeyEvent.KEY_PRESSED) {
       dispatch = !waitingForAltRelease
+      altPressedOnly = true
     }
     else if (ke.id == KeyEvent.KEY_RELEASED) {
       if (waitingForAltRelease) {
         waitingForAltRelease = false
         dispatch = false
       }
-      else if (component != null) {
+      else if (component != null && altPressedOnly) {
         EventQueue.invokeLater {
           try {
             val window = ComponentUtil.getWindow(component)
@@ -1327,6 +1332,8 @@ private class WindowsAltSuppressor : IdeEventQueue.NonLockedEventDispatcher {
           }
         }
       }
+
+      altPressedOnly = false
     }
     return !dispatch
   }
