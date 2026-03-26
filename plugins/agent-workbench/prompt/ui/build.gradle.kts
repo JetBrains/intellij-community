52c3cffa0cf15bd495594b4b65cb847ed3893704
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2026-03-26 15:45:29 +0100

    [agent-workbench]: add Gradle build (IJPL-233558)
    GitOrigin-RevId: c5ccc4310d17429842ebc5b67a8060bad4df2e8d

---
+++ plugins/agent-workbench/prompt/ui/build.gradle.kts
@@ -0,0 +1,70 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
+plugins {
+  id("java")
+  id("org.jetbrains.kotlin.jvm")
+  id("org.jetbrains.intellij.platform.module")
+  kotlin("plugin.serialization")
+}
+
+repositories {
+  mavenCentral()
+  maven("https://oss.sonatype.org/content/repositories/snapshots/")
+  intellijPlatform {
+    defaultRepositories()
+    snapshots()
+  }
+}
+
+intellijPlatform {
+  instrumentCode = false
+}
+
+sourceSets {
+  main {
+    java { setSrcDirs(listOf("src")) }
+    resources { setSrcDirs(listOf("resources")) }
+  }
+}
+
+val platformLocalPath = rootProject.extra["platformLocalPath"] as? String
+val platformVersion = rootProject.extra["platformVersion"] as String
+
+dependencies {
+  intellijPlatform {
+    if (platformLocalPath != null) {
+      local(platformLocalPath)
+    } else {
+      intellijIdeaUltimate(platformVersion) { useInstaller = false }
+    }
+    jetbrainsRuntime()
+    if (platformLocalPath == null) {
+      bundledPlugins("org.intellij.plugins.markdown")
+    }
+  }
+
+  if (platformLocalPath != null) {
+    val ideDir = rootProject.file(platformLocalPath)
+    compileOnly(fileTree(ideDir.resolve("plugins/markdown/lib")) { include("**/*.jar") })
+  }
+
+  implementation(project(":common"))
+  implementation(project(":prompt-core"))
+  implementation(project(":sessions-core"))
+}
+
+kotlin {
+  compilerOptions {
+    jvmTarget.set(JvmTarget.JVM_21)
+    freeCompilerArgs.addAll("-Xjvm-default=all")
+    apiVersion.set(KotlinVersion.KOTLIN_2_3)
+    languageVersion.set(KotlinVersion.KOTLIN_2_3)
+  }
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_21
+  targetCompatibility = JavaVersion.VERSION_21
+}
