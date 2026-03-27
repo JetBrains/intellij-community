cea714ae739c3c9bcd9104ef53f369bc0254dec8
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2026-03-27 07:35:26 +0100

    IJPL-239466 convert ProxyShortcutSet to a better suited language
    GitOrigin-RevId: dab379d0277db672ae5c6ba1631ae58dbdc318f4

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ProxyShortcutSet.kt
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ProxyShortcutSet.kt
@@ -1,27 +1,14 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.openapi.actionSystem.impl;
+package com.intellij.openapi.actionSystem.impl
 
-import com.intellij.openapi.actionSystem.Shortcut;
-import com.intellij.openapi.actionSystem.ShortcutSet;
-import com.intellij.openapi.keymap.KeymapManager;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.Shortcut
+import com.intellij.openapi.actionSystem.ShortcutSet
+import com.intellij.openapi.keymap.KeymapManager
+import org.jetbrains.annotations.ApiStatus.Internal
 
-@ApiStatus.Internal
-public final class ProxyShortcutSet implements ShortcutSet {
-  private final String myActionId;
-
-  ProxyShortcutSet(@NotNull String actionId) {
-    myActionId = actionId;
-  }
-
-  @Override
-  public Shortcut @NotNull [] getShortcuts() {
-    KeymapManager manager = KeymapManager.getInstance();
-    return manager != null ? manager.getActiveKeymap().getShortcuts(myActionId) : Shortcut.EMPTY_ARRAY;
-  }
-
-  public @NotNull String getActionId() {
-    return myActionId;
+@Internal
+class ProxyShortcutSet internal constructor(val actionId: String) : ShortcutSet {
+  override fun getShortcuts(): Array<Shortcut?> {
+    return KeymapManager.getInstance()?.getActiveKeymap()?.getShortcuts(actionId) ?: Shortcut.EMPTY_ARRAY
   }
 }
