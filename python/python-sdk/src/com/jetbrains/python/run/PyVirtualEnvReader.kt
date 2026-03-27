13acadb05b503ad1283a3951d5c1237f75357f99
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2026-03-27 17:56:29 +0100

    cleanup [python]: deprecated API; suppressions; formatting
    GitOrigin-RevId: b8ad9940d791aef36d24ad367287e155a9a6c0b6

--- python/python-sdk/src/com/jetbrains/python/run/PyVirtualEnvReader.kt
+++ python/python-sdk/src/com/jetbrains/python/run/PyVirtualEnvReader.kt
@@ -1,27 +1,33 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:OptIn(LowLevelLocalMachineAccess::class)
 package com.jetbrains.python.run
 
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.util.SystemInfo
 import com.intellij.util.EnvironmentUtil
 import com.intellij.util.ShellEnvironmentReader
+import com.intellij.util.system.LowLevelLocalMachineAccess
+import com.intellij.util.system.OS
 import com.jetbrains.python.packaging.PyCondaPackageService
 import com.jetbrains.python.sdk.legacy.PythonSdkUtil
-import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.io.path.exists
+import kotlin.io.path.name
 
 internal class PyVirtualEnvReader(private val virtualEnvSdkPath: String) {
   companion object {
     private val LOG = Logger.getInstance(PyVirtualEnvReader::class.java)
 
-    private val virtualEnvVars = listOf("PATH", "PS1", "VIRTUAL_ENV", "PYTHONHOME", "PROMPT", "_OLD_VIRTUAL_PROMPT",
-                                        "_OLD_VIRTUAL_PYTHONHOME", "_OLD_VIRTUAL_PATH", "CONDA_SHLVL", "CONDA_PROMPT_MODIFIER",
-                                        "CONDA_PREFIX", "CONDA_DEFAULT_ENV",
-                                        "GDAL_DATA", "PROJ_LIB", "JAVA_HOME", "JAVA_LD_LIBRARY_PATH")
+    @Suppress("SpellCheckingInspection")
+    private val virtualEnvVars = listOf(
+      "PATH", "PS1", "VIRTUAL_ENV", "PYTHONHOME", "PROMPT", "_OLD_VIRTUAL_PROMPT",
+      "_OLD_VIRTUAL_PYTHONHOME", "_OLD_VIRTUAL_PATH", "CONDA_SHLVL", "CONDA_PROMPT_MODIFIER",
+      "CONDA_PREFIX", "CONDA_DEFAULT_ENV",
+      "GDAL_DATA", "PROJ_LIB", "JAVA_HOME", "JAVA_LD_LIBRARY_PATH"
+    )
 
     /**
-     * Filter envs that are setup by the activate script, adding other variables from the different shell can break the actual shell.
+     * Filter envs that are set up by the activate script, adding other variables from the different shell can break the actual shell.
      */
     fun filterVirtualEnvVars(env: Map<String, String>): Map<String, String> {
       return env.filterKeys { k -> virtualEnvVars.any { it.equals(k, true) } }
@@ -39,9 +45,10 @@ internal class PyVirtualEnvReader(private val virtualEnvSdkPath: String) {
   // in case of Conda we need to pass an argument to the activation script telling which environment to activate
   val activate: Pair<String, String?>? = findActivateScript(virtualEnvSdkPath, shell)
 
+  @Suppress("PyExceptionTooBroad")
   fun readPythonEnv(): MutableMap<String, String> {
     try {
-      if (SystemInfo.isUnix) {
+      if (OS.CURRENT != OS.Windows) {
         val command = ShellEnvironmentReader.shellCommand(shell, activate?.first?.let { Path.of(it) }, activate?.second?.let { listOf(it) })
         // pass shell environment for correct virtualenv environment setup (virtualenv expects to be executed from the terminal)
         command.environment().putAll(EnvironmentUtil.getEnvironmentMap())
@@ -63,22 +70,19 @@ internal class PyVirtualEnvReader(private val virtualEnvSdkPath: String) {
 
 fun findActivateScript(sdkPath: String?, shellPath: String?): Pair<String, String?>? {
   if (PythonSdkUtil.isVirtualEnv(sdkPath)) {
-    val shellName = if (shellPath != null) File(shellPath).name else null
-    val activate = findActivateInPath(sdkPath!!, shellName)
-
+    val shellName = if (shellPath != null) Path.of(shellPath).name else null
+    val activate = findActivateInPath(Path.of(sdkPath!!), shellName)
     return if (activate != null && activate.exists()) {
-      Pair(activate.absolutePath, null)
+      Pair(activate.toAbsolutePath().toString(), null)
     }
     else null
   }
-  else if (PythonSdkUtil.isConda(sdkPath)) {
+  else if (@Suppress("DEPRECATION") PythonSdkUtil.isConda(sdkPath)) {
     val condaExecutable = PyCondaPackageService.getCondaExecutable(sdkPath!!)
-
     if (condaExecutable != null) {
-      val activate = findActivateInPath(File(condaExecutable).path, null)
-
+      val activate = findActivateInPath(Path.of(condaExecutable), shellName = null)
       if (activate != null && activate.exists()) {
-        return Pair(activate.path, condaEnvFolder(sdkPath))
+        return Pair(activate.toAbsolutePath().toString(), condaEnvFolder(sdkPath))
       }
     }
   }
@@ -86,23 +90,21 @@ fun findActivateScript(sdkPath: String?, shellPath: String?): Pair<String, Strin
   return null
 }
 
-private fun findActivateInPath(path: String, shellName: String?): File? {
-  return if (SystemInfo.isWindows) findActivateOnWindows(path)
-  else if (shellName == "fish" || shellName == "csh") File(File(path).parentFile, "activate.$shellName")
-  else File(File(path).parentFile, "activate")
+private fun findActivateInPath(path: Path, shellName: String?): Path? = when {
+  OS.CURRENT == OS.Windows -> findActivateOnWindows(path)
+  shellName == "fish" || shellName == "csh" -> path.resolveSibling("activate.${shellName}")
+  else -> path.resolveSibling("activate")
 }
 
-private fun condaEnvFolder(path: String): String? {
-  return if (SystemInfo.isWindows) File(path).parent else File(path).parentFile.parent
-}
+private fun condaEnvFolder(path: String): String? =
+  Path.of(path).let { if (OS.CURRENT == OS.Windows) it.parent else it.parent?.parent }?.toString()
 
-private fun findActivateOnWindows(path: String): File? {
+private fun findActivateOnWindows(path: Path): Path? {
   for (location in arrayListOf("activate.bat", "Scripts/activate.bat")) {
-    val file = File(File(path).parentFile, location)
+    val file = path.resolveSibling(location)
     if (file.exists()) {
       return file
     }
   }
-
   return null
 }
