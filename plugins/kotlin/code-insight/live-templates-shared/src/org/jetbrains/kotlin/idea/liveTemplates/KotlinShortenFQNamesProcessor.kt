eeb97920f81c0bb41afc9d228c07f14a986b9204
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2026-03-27 14:36:36 +0000

    KTNB-1308 kotlin-notebooks: fix injected file copying
    🍒 Cherry-picked from review IJ-CR-196745
    
    Co-authored-by: Ilya Komolkin <Ilya.Komolkin@jetbrains.com>
    
    Merge-request: IJ-MR-196746
    Merged-by: Ilya Komolkin <Ilya.Komolkin@jetbrains.com>
    
    GitOrigin-RevId: 125ecd37b9b350ee27247a39fd3027dc61ba483e

--- plugins/kotlin/code-insight/live-templates-shared/src/org/jetbrains/kotlin/idea/liveTemplates/KotlinShortenFQNamesProcessor.kt
+++ plugins/kotlin/code-insight/live-templates-shared/src/org/jetbrains/kotlin/idea/liveTemplates/KotlinShortenFQNamesProcessor.kt
@@ -6,11 +6,15 @@ import com.intellij.codeInsight.CodeInsightBundle
 import com.intellij.codeInsight.template.Template
 import com.intellij.codeInsight.template.impl.TemplateContext
 import com.intellij.codeInsight.template.impl.TemplateOptionalProcessor
+import com.intellij.injected.editor.DocumentWindow
+import com.intellij.lang.injection.InjectedLanguageManager
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.RangeMarker
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiUtilBase
 import org.jetbrains.kotlin.idea.base.codeInsight.ShortenReferencesFacility
 import org.jetbrains.kotlin.psi.KtFile
@@ -21,8 +25,9 @@ class KotlinShortenFQNamesProcessor : TemplateOptionalProcessor {
 
         PsiDocumentManager.getInstance(project).commitDocument(document)
 
-        val file = PsiUtilBase.getPsiFileInEditor(editor, project) as? KtFile ?: return
-        ShortenReferencesFacility.getInstance().shorten(file, templateRange.textRange)
+        val file = PsiUtilBase.getPsiFileInEditor(editor, project)
+        val (ktFile, range) = getMaybeInjectedRange(file, templateRange.textRange) ?: return
+        ShortenReferencesFacility.getInstance().shorten(ktFile, range)
 
         PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(document)
     }
@@ -33,5 +38,35 @@ class KotlinShortenFQNamesProcessor : TemplateOptionalProcessor {
 
     override fun isEnabled(template: Template): Boolean = template.isToShortenLongNames
     override fun setEnabled(template: Template, value: Boolean) {}
-    override fun isVisible(template: Template, context: TemplateContext) = false
+    override fun isVisible(template: Template, context: TemplateContext): Boolean = false
+
+    private fun getMaybeInjectedRange(file: PsiFile?, textRange: TextRange): FileWithRange? {
+        var ktFile = file as? KtFile
+        var range = textRange
+        if (file != null && ktFile == null) {
+            val project = file.project
+            val manager = InjectedLanguageManager.getInstance(project)
+            val psiElement = manager.findInjectedElementAt(file, range.startOffset)
+
+            if (psiElement != null) {
+                val documentWindow = PsiDocumentManager.getInstance(project)
+                    .getCachedDocument(psiElement.containingFile) as? DocumentWindow
+                if (documentWindow != null) {
+                    val rangeStart = documentWindow.hostToInjected(range.startOffset)
+                    val rangeEnd = documentWindow.hostToInjected(range.endOffset)
+                    if (rangeStart != -1 && rangeEnd != -1) {
+                        ktFile = psiElement.containingFile as? KtFile
+                        range = TextRange(rangeStart, rangeEnd)
+                    }
+                }
+            }
+        }
+        if (ktFile == null) return null
+        return FileWithRange(ktFile, range)
+    }
+
+    private data class FileWithRange(
+        val ktFile: KtFile,
+        val textRange: TextRange,
+    )
 }
