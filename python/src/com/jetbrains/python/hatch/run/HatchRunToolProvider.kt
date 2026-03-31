188d038fccd69ecc86b59c4c19d8e33bcb676843
TimurMalanin <timur.malanin@jetbrains.com>
2026-03-31 12:52:11 +0000

    PY-79879 Add `hatch` run configuration
    (cherry picked from commit 9a5d13840c4af0496be568288321a6d03d6ea9f6)
    
    IJ-MR-197783
    
    GitOrigin-RevId: 88d1af4de181d1e2f0824ec345bbd03d3c52cc74

---
+++ python/src/com/jetbrains/python/hatch/run/HatchRunToolProvider.kt
@@ -0,0 +1,31 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.jetbrains.python.hatch.run
+
+import com.intellij.python.hatch.HatchConfiguration
+import com.intellij.python.hatch.runtime.HatchConstants
+import com.jetbrains.python.PyBundle
+import com.jetbrains.python.hatch.sdk.HatchSdkAdditionalData
+import com.jetbrains.python.run.features.PyRunToolData
+import com.jetbrains.python.run.features.PyRunToolId
+import com.jetbrains.python.run.features.PyRunToolParameters
+import com.jetbrains.python.run.features.PySdkRunToolProvider
+
+internal class HatchRunToolProvider : PySdkRunToolProvider<HatchSdkAdditionalData>(HatchSdkAdditionalData::class.java) {
+
+  override suspend fun getRunToolParameters(data: HatchSdkAdditionalData): PyRunToolParameters {
+    val hatchPath = HatchConfiguration.getOrDetectHatchExecutablePath().getOr { error("Unable to find hatch executable.") }
+    val env = mutableMapOf<String, String>()
+    data.hatchEnvironmentName?.let {
+      env += HatchConstants.AppEnvVars.ENV to it
+    }
+    return PyRunToolParameters(hatchPath.toString(), listOf("run"), env)
+  }
+
+  override val runToolData: PyRunToolData = PyRunToolData(
+    PyRunToolId("hatch.run"),
+    PyBundle.message("hatch.run.configuration.type.display.name"),
+    PyBundle.message("python.run.configuration.fragments.python.group"),
+  )
+
+  override val initialToolState: Boolean = true
+}
