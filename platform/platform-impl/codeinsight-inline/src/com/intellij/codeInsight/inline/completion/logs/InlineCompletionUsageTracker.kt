d2f96733de4ed9b0315c2506653edb9524be4cfa
Gleb.Marin <gleb.marin@jetbrains.com>
2026-04-08 12:24:38 +0200

    [ai-completion] LLM-25004 Introduce customizable control model thresholds
    Also fix the misuse of the chat filter model
    
    (cherry picked from commit ffd79eccdd78241dcaf7ec89bbbaf5266282099e)
    
    
    (cherry picked from commit 7bdf1681652354d17a1513a2b658e10bb3585e78)
    
    IJ-MR-202752
    
    GitOrigin-RevId: 4286462c60ba1d75d0cc0c9fdc90b402804d5812

--- platform/platform-impl/codeinsight-inline/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionUsageTracker.kt
+++ platform/platform-impl/codeinsight-inline/src/com/intellij/codeInsight/inline/completion/logs/InlineCompletionUsageTracker.kt
@@ -24,7 +24,7 @@ import kotlin.coroutines.cancellation.CancellationException
 @ApiStatus.Internal
 @Deprecated("will be moved to inline.completion.v2")
 object InlineCompletionUsageTracker : CounterUsagesCollector() {
-  private val GROUP = EventLogGroup("inline.completion", 40)
+  private val GROUP = EventLogGroup("inline.completion", 41)
 
   const val INVOKED_EVENT_ID: String = "invoked"
   const val SHOWN_EVENT_ID: String = "shown"
