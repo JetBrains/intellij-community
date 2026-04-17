2784889b112ebe42b82e5e7e0752217890eb18de
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2026-04-17 19:12:59 +0200

    test [gradle|test-framework] cleanup: extract mock for ProjectResolverContext
    GitOrigin-RevId: 79e3578b64c963ef247d8199564d343155a918a0

--- plugins/gradle/java/testSources/importing/GradleJavaProjectResolverPerformanceTest.kt
+++ plugins/gradle/java/testSources/importing/GradleJavaProjectResolverPerformanceTest.kt
@@ -4,22 +4,15 @@ package org.jetbrains.plugins.gradle.importing
 import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.model.project.ModuleData
 import com.intellij.openapi.externalSystem.model.project.ProjectData
-import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
-import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskType
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Key
-import com.intellij.openapi.util.UserDataHolderBase
 import com.intellij.testFramework.PerformanceUnitTest
 import com.intellij.testFramework.common.mock.notImplemented
 import com.intellij.testFramework.junit5.TestApplication
 import com.intellij.testFramework.junit5.fixture.projectFixture
 import com.intellij.testFramework.junit5.fixture.testFixture
 import com.intellij.tools.ide.metrics.benchmark.Benchmark
-import org.gradle.tooling.model.ProjectModel
 import org.gradle.tooling.model.idea.IdeaModule
 import org.gradle.tooling.model.idea.IdeaProject
-import org.jetbrains.plugins.gradle.model.ExternalProject
-import org.jetbrains.plugins.gradle.model.GradleSourceSetModel
 import org.jetbrains.plugins.gradle.service.project.GradleProjectResolverExtension
 import org.jetbrains.plugins.gradle.service.project.JavaGradleProjectResolver
 import org.jetbrains.plugins.gradle.service.project.ProjectResolverContext
@@ -28,10 +21,10 @@ import org.jetbrains.plugins.gradle.testFramework.fixtures.gradleJvmFixture
 import org.jetbrains.plugins.gradle.testFramework.projectModel.mock.GradleTestExternalProject.Companion.externalProjects
 import org.jetbrains.plugins.gradle.testFramework.projectModel.mock.GradleTestIdeaProject.Companion.ideaProject
 import org.jetbrains.plugins.gradle.testFramework.projectModel.mock.GradleTestProjectNode.Companion.projectNode
+import org.jetbrains.plugins.gradle.testFramework.projectModel.mock.GradleTestProjectResolverContext.Companion.projectResolverContext
 import org.jetbrains.plugins.gradle.testFramework.projectModel.moduleNodes
 import org.jetbrains.plugins.gradle.testFramework.projectModel.moduleSdkNode
 import org.jetbrains.plugins.gradle.testFramework.projectModel.projectSdkNode
-import org.jetbrains.plugins.gradle.util.GradleConstants
 import org.jetbrains.plugins.gradle.util.gradleSettings
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Nested
@@ -96,11 +89,8 @@ class GradleJavaProjectResolverPerformanceTest {
         it.numHolderModules = numHolderModules
         it.numSourceSetModules = numSourceSetModules
       }
-      val syncContext = TestProjectResolverContext(project, ideaProject, externalProjects)
-      val projectResolver = JavaGradleProjectResolver().also {
-        it.setProjectResolverContext(syncContext)
-        it.setNext(NoOpNextResolver())
-      }
+      val projectResolverContext = projectResolverContext(project, ideaProject, externalProjects)
+      val projectResolver = javaProjectResolver(projectResolverContext)
 
       val setup = {
         // Removes the project resolver's caches
@@ -124,42 +114,19 @@ class GradleJavaProjectResolverPerformanceTest {
   }
 
   @Suppress("JavaDefaultMethodsNotOverriddenByDelegation")
-  private class TestProjectResolverContext(
-    override val project: Project,
-    ideaProject: IdeaProject,
-    externalProjects: List<ExternalProject>,
-  ) : ProjectResolverContext by notImplemented<ProjectResolverContext>() {
-
-    private val externalProjectModels = ideaProject.modules.zip(externalProjects).toMap()
-
-    override val projectPath: String = project.basePath!!
-    override val taskId = ExternalSystemTaskId.create(GradleConstants.SYSTEM_ID, ExternalSystemTaskType.RESOLVE_PROJECT, project)
-    override fun getBuildSrcGroup(): String? = null
-    override fun isResolveModulePerSourceSet(): Boolean = true
-    override fun getExternalSystemTaskId(): ExternalSystemTaskId = taskId
-
-    @Suppress("UNCHECKED_CAST")
-    override fun <T> getProjectModel(projectModel: ProjectModel, modelClass: Class<T>): T? =
-      when (modelClass) {
-        ExternalProject::class.java -> externalProjectModels[projectModel] as T
-        GradleSourceSetModel::class.java -> externalProjectModels[projectModel]?.sourceSetModel as T
-        else -> throw AssertionError("Unexpected model class requested in performance test: $modelClass")
-      }
-
-    private val userDataHolder = UserDataHolderBase()
-    override fun <T> getUserData(key: Key<T>): T? = userDataHolder.getUserData(key)
-    override fun <T> putUserData(key: Key<T>, value: T?) = userDataHolder.putUserData(key, value)
-    override fun <T : Any> putUserDataIfAbsent(key: Key<T>, value: T): T = userDataHolder.putUserDataIfAbsent(key, value)
-    override fun <T> replace(key: Key<T>, oldValue: T?, newValue: T?): Boolean = userDataHolder.replace(key, oldValue, newValue)
-  }
-
-  @Suppress("JavaDefaultMethodsNotOverriddenByDelegation")
-  private class NoOpNextResolver : GradleProjectResolverExtension by notImplemented<GradleProjectResolverExtension>() {
+  private class GradleTestNoOpProjectResolverExtension :
+    GradleProjectResolverExtension by notImplemented<GradleProjectResolverExtension>() {
     override fun populateModuleExtraModels(gradleModule: IdeaModule, ideModule: DataNode<ModuleData>) {}
     override fun populateProjectExtraModels(gradleProject: IdeaProject, ideProject: DataNode<ProjectData>) {}
   }
 
   companion object {
+
+    fun javaProjectResolver(context: ProjectResolverContext): JavaGradleProjectResolver = JavaGradleProjectResolver().apply {
+      setProjectResolverContext(context)
+      setNext(GradleTestNoOpProjectResolverExtension())
+    }
+
     fun setGradleJvm(project: Project, gradleJvm: String) {
       project.gradleSettings.linkedProjectsSettings = listOf(
         GradleProjectSettings(project.basePath!!).also { it.gradleJvm = gradleJvm }
