e4533d315d6314b7df6a66d4bd2b645aaf6c84e1
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2026-04-24 19:57:05 +0200

    IJPL-209388: check if license is available in license vault before loading paid features
    GitOrigin-RevId: b6b947e2afca0e085c8ae0185a49ec6e5f3078fb

--- platform/platform-impl/src/com/intellij/ide/impl/FLSConfigurationInfoService.java
+++ platform/platform-impl/src/com/intellij/ide/impl/FLSConfigurationInfoService.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
 @ApiStatus.Internal
@@ -13,10 +14,25 @@ public interface FLSConfigurationInfoService {
 
   record FLSInfo(@NotNull String url) {}
 
-  record AuthData(@NotNull FLSConfigurationInfoService.FLSInfo config, @NotNull Map<String, String> authHeaders) {}
+  record AuthData(@NotNull FLSInfo config, @NotNull Map<String, String> authHeaders) {}
+
+  sealed interface TicketAvailabilityResult permits TicketAvailabilityResult.TicketAvailable,
+                                                    TicketAvailabilityResult.TicketUnavailable,
+                                                    TicketAvailabilityResult.RequestFailed,
+                                                    TicketAvailabilityResult.AuthRequired {
+    record TicketAvailable(@Nullable String pluginsAllowed) implements TicketAvailabilityResult {}
+
+    record TicketUnavailable(@NotNull String message) implements TicketAvailabilityResult {}
+
+    record RequestFailed(@NotNull String errorMessage) implements TicketAvailabilityResult {}
+
+    record AuthRequired(@Nullable String message, @Nullable String authorizationUrl, @Nullable String authorizationClientId,
+                        @Nullable String codeExchangeUrl) implements TicketAvailabilityResult {}
+  }
 
   /**
-   * Provides an FLS descriptors used currently by the IDE to obtain license tickets
+   * Provides FLS descriptors used currently by the IDE to obtain license tickets
+   *
    * @return a list of descriptor structures for every FLS resource currently used
    */
   @NotNull
@@ -27,7 +43,14 @@ public interface FLSConfigurationInfoService {
    * @return a future, providing authentication data for the given FLS resource. If no authentication data is available, null is returned by the Future
    */
   @NotNull
-  Future<@Nullable AuthData> lookupAuthData(@NotNull FLSConfigurationInfoService.FLSInfo config);
+  Future<@Nullable AuthData> lookupAuthData(@NotNull FLSInfo config);
+
+  /**
+   * @param config the FLS descriptor obtained from {@link #getFLSConfiguration()} or {@link FLSConfigurationProvider#getUrl()}
+   * @return a future, providing ticket availability information for the given FLS resource
+   */
+  @NotNull
+  CompletableFuture<@Nullable TicketAvailabilityResult> checkTicketAvailability(@NotNull FLSInfo config);
 
   static FLSConfigurationInfoService getInstance() {
     return FLSConfigurationInfoServiceHolder.INSTANCE;
