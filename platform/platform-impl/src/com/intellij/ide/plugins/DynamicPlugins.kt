b7a2908061f21b53ee41daae0f2a9e78943cf6b2
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2026-04-27 17:37:57 +0200

    IJPL-221926 DynamicPlugins: drop unused compatibility overload
    GitOrigin-RevId: 8d3cfa498a709231f9f68f1242362571b720d271

--- platform/platform-impl/src/com/intellij/ide/plugins/DynamicPlugins.kt
+++ platform/platform-impl/src/com/intellij/ide/plugins/DynamicPlugins.kt
@@ -169,23 +169,6 @@ object DynamicPlugins {
     }
   }
 
-  /** Left for compatibility, drop later */
-  @Deprecated("use overload with Collection<PluginMainDescriptor> parameter")
-  fun unloadPlugins(
-    plugins: Collection<IdeaPluginDescriptorImpl>,
-    project: Project? = null,
-    parentComponent: JComponent? = null,
-    options: UnloadPluginOptions = UnloadPluginOptions(disable = true),
-  ): Boolean {
-    val unexpectedDescriptor = plugins.find { it !is PluginMainDescriptor }
-    if (unexpectedDescriptor != null) {
-      LOG.warn("Unexpected descriptor type: $unexpectedDescriptor")
-      return false
-    }
-    @Suppress("UNCHECKED_CAST")
-    return unloadPlugins(plugins.toList() as List<PluginMainDescriptor>, project, parentComponent, options)
-  }
-
   /**
    * @return true if the requested enabled state was applied without restart, false if restart is required
    */
