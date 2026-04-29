bd5d796d32371e5d097340618692264317897832
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2026-04-29 22:47:36 +0200

    IDEA-389353 [gradle|test-framework] cleanup GradleTestCase: inline GradleBaseTestCase
    GitOrigin-RevId: 8d48e562ce26dfad16a7a2738bb2a2a13efb7d09

--- plugins/gradle/testSources/org/jetbrains/plugins/gradle/testFramework/GradleTestCase.kt
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/testFramework/GradleTestCase.kt
@@ -1,8 +1,14 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.gradle.testFramework
 
+import com.intellij.openapi.externalSystem.importing.ImportSpecBuilder
 import com.intellij.openapi.project.Project
+import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.testFramework.junit5.fixture.tempPathFixture
+import org.gradle.util.GradleVersion
+import org.jetbrains.jps.model.java.JdkVersionDetector.JdkVersionInfo
 import org.jetbrains.plugins.gradle.frameworkSupport.GradleDsl
+import org.jetbrains.plugins.gradle.testFramework.fixtures.gradleFixture
 import org.jetbrains.plugins.gradle.testFramework.projectInfo.GradleProjectInfo
 import org.jetbrains.plugins.gradle.testFramework.projectInfo.GradleProjectInfoAssertions.assertProjectState
 import org.jetbrains.plugins.gradle.testFramework.projectInfo.GradleProjectInfoBuilder
@@ -13,7 +19,32 @@ import org.jetbrains.plugins.gradle.testFramework.projectInfo.initProject
 import org.jetbrains.plugins.gradle.testFramework.projectInfo.simpleJavaProjectInfo
 import java.nio.file.Path
 
-abstract class GradleTestCase : GradleBaseTestCase() {
+@TestApplication
+abstract class GradleTestCase {
+
+  val gradleVersion: GradleVersion = GradleVersion.current()
+
+  val testPath: Path by tempPathFixture()
+
+  private val gradleFixture by gradleFixture(gradleVersion)
+  val gradleJvm: String get() = gradleFixture.gradleJvm
+  val gradleJvmInfo: JdkVersionInfo get() = gradleFixture.gradleJvmInfo
+
+  suspend fun openProject(relativePath: String, numProjectSyncs: Int = 1): Project {
+    return gradleFixture.openProject(testPath.resolve(relativePath), numProjectSyncs)
+  }
+
+  suspend fun linkProject(project: Project, relativePath: String) {
+    gradleFixture.linkProject(project, testPath.resolve(relativePath))
+  }
+
+  suspend fun syncProject(project: Project, relativePath: String, configure: ImportSpecBuilder.() -> Unit = {}) {
+    gradleFixture.syncProject(project, testPath.resolve(relativePath), configure)
+  }
+
+  suspend fun <R> withAllowedProjectSyncs(numProjectSyncs: Int = 1, action: suspend () -> R): R {
+    return gradleFixture.withAllowedProjectSyncs(numProjectSyncs, action)
+  }
 
   suspend fun initProject(projectInfo: GradleProjectInfo): Path =
     initProject(testPath, projectInfo)
