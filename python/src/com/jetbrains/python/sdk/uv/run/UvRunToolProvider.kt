bd2373c544edde3957fdc1c02a92654adecebea4
Alexey Katsman <alexey.katsman@jetbrains.com>
2026-05-01 20:19:43 +0200

    PY-89538 Fix uv run for remotes
    (cherry picked from commit a7d4312e517e561ebfdbdd26230558096710ef5a)
    
    IJ-MR-202819
    
    GitOrigin-RevId: ee18b7255ba8feeb0f038dc4aeca6db442731a67

--- python/src/com/jetbrains/python/sdk/uv/run/UvRunToolProvider.kt
+++ python/src/com/jetbrains/python/sdk/uv/run/UvRunToolProvider.kt
@@ -1,27 +1,37 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.sdk.uv.run
 
+import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.platform.eel.provider.localEel
 import com.jetbrains.python.PyBundle
 import com.jetbrains.python.run.features.PyRunToolData
 import com.jetbrains.python.run.features.PyRunToolId
 import com.jetbrains.python.run.features.PyRunToolParameters
-import com.jetbrains.python.run.features.PySdkRunToolProvider
+import com.jetbrains.python.run.features.PyRunToolProvider
 import com.jetbrains.python.sdk.add.v2.FileSystem
 import com.jetbrains.python.sdk.uv.UvSdkAdditionalData
+import com.jetbrains.python.sdk.uv.getUvExecutionContext
 import com.jetbrains.python.sdk.uv.impl.getUvExecutable
+import com.jetbrains.python.sdk.uv.isUv
+import com.jetbrains.python.target.PyTargetAwareAdditionalData
 
 /**
  * PyRunToolProvider implementation that runs scripts/modules using `uv run`.
+ *
+ * Matches both local UV SDKs ([UvSdkAdditionalData]) and remote ones
+ * ([PyTargetAwareAdditionalData] wrapping `UvSdkFlavorData`).
  */
-internal class UvRunToolProvider : PySdkRunToolProvider<UvSdkAdditionalData>(UvSdkAdditionalData::class.java) {
+internal class UvRunToolProvider : PyRunToolProvider {
 
-  override suspend fun getRunToolParameters(data: UvSdkAdditionalData): PyRunToolParameters {
+  override fun isAvailable(sdk: Sdk): Boolean = sdk.isUv
+
+  override suspend fun getRunToolParameters(sdk: Sdk): PyRunToolParameters {
     val env = mutableMapOf<String, String>()
-    val flavorData = data.flavorData
-    val uvExecutable = getUvExecutable(FileSystem.Eel(localEel), flavorData.uvPath)?.toString()
+    val uvExecutionContext = sdk.getUvExecutionContext()
+    val fileSystem = uvExecutionContext?.fileSystem ?: FileSystem.Eel(localEel)
+    val uvExecutable = getUvExecutable(fileSystem, uvExecutionContext?.uvPath?.toString())?.toString()
     // TODO PY-87712 Duplicated code for setting up uv envs
-    flavorData.venvPath?.let {
+    uvExecutionContext?.venvPath?.toString()?.let {
       env += "VIRTUAL_ENV" to it
       env += "UV_PROJECT_ENVIRONMENT" to it
     }
