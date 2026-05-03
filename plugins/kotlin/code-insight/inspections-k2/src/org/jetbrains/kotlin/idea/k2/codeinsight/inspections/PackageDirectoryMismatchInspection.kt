d2515e048e1f7414d299f637876e3a9e35d457f2
Roman Golyshev <roman.golyshev@jetbrains.com>
2026-05-03 21:32:26 +0200

    [lsp] LSP-954 Highlight only the first declaration in Kotlin LSP for the `PackageDirectoryMismatchInspection`
    Introduce a `PackageDirectoryMismatchInspection.EmptyPackageHighlightingFallback` service.
    
    The Kotlin plugin registers the `WholeFile` implementation
    that preserves IDEA's file-level banner, while the Kotlin LSP `codeActions.xml`
    overrides it with `FirstDeclaration` so VS Code highlights only the first
    declaration of the file instead of the entire document.
    
    GitOrigin-RevId: 1b80fa210d3a246555ffbe169f1fc7a90b1aeb8f

--- plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/PackageDirectoryMismatchInspection.kt
+++ plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/k2/codeinsight/inspections/PackageDirectoryMismatchInspection.kt
@@ -8,10 +8,13 @@ import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.lang.injection.InjectedLanguageManager
 import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.SingleFileSourcesTracker
 import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
+import com.intellij.psi.PsiNameIdentifierOwner
 import com.intellij.refactoring.PackageWrapper
 import com.intellij.refactoring.move.moveClassesOrPackages.AutocreatingSingleSourceRootMoveDestination
 import com.intellij.refactoring.move.moveFilesOrDirectories.MoveFilesOrDirectoriesUtil
@@ -69,7 +72,12 @@ class PackageDirectoryMismatchInspection : AbstractKotlinInspection() {
                 fixes += ChangePackageFix("'${fqNameWithImplicitPrefix.asString()}'", fqNameWithImplicitPrefix)
             }
 
-            val element = if (directive.textLength != 0) directive else file
+            val element = if (directive.textLength != 0) {
+                directive
+            } else {
+                EmptyPackageHighlightingFallback.getInstance().getElementToHighlight(directive)
+            }
+
             holder.registerProblem(
                 element,
               KotlinBundle.message("text.package.directive.dont.match.file.location"),
@@ -136,6 +144,34 @@ class PackageDirectoryMismatchInspection : AbstractKotlinInspection() {
             val packageDirective = file.packageDirective ?: return IntentionPreviewInfo.EMPTY
             packageDirective.fqName = packageFqName
             return IntentionPreviewInfo.DIFF
+        }
+    }
+
+    /**
+     * Picks the PSI element to anchor the "package directive doesn't match directory" problem on
+     * when the package directive itself is empty (missing or commented out).
+     */
+    internal interface EmptyPackageHighlightingFallback {
+        fun getElementToHighlight(directive: KtPackageDirective): PsiElement
+
+        companion object {
+            @JvmStatic
+            fun getInstance(): EmptyPackageHighlightingFallback = service()
+        }
+
+        class WholeFile : EmptyPackageHighlightingFallback {
+            override fun getElementToHighlight(directive: KtPackageDirective): PsiElement =
+                directive.containingKtFile
+        }
+
+        class FirstDeclarationName : EmptyPackageHighlightingFallback {
+            override fun getElementToHighlight(directive: KtPackageDirective): PsiElement {
+                val file = directive.containingKtFile
+
+                val firstNamedDeclaration = file.declarations.filterIsInstance<PsiNameIdentifierOwner>().firstOrNull()
+
+                return firstNamedDeclaration?.nameIdentifier ?: file
+            }
         }
     }
 }
