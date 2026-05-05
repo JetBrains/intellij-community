e7e00e32a8de960ca97ce22a3af5453d95b273c5
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2026-05-05 14:12:00 +0200

    [debugger] IDEA-388175: Use non-blocking read action for JavaSourcePosition.mapDelegate
    This read action should be cancellable because decompile in the path: JavaSourcePosition.mapDelegate -> ClsSourcePosition.mapDelegate -> `file.getViewProvider().getDocument()` -> ... -> IdeaDecompiler.decompile (that takes plenty of time) may be slow.
    
    GitOrigin-RevId: 8d6f21b1f4115d62e6223f310bb536cb5dbd24e4

--- java/debugger/impl/src/com/intellij/debugger/engine/PositionManagerImpl.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/PositionManagerImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.debugger.engine;
 
 import com.intellij.debugger.MultiRequestPositionManager;
@@ -282,7 +282,7 @@ public class PositionManagerImpl implements PositionManager, MultiRequestPositio
 
     @Override
     public SourcePosition mapDelegate(final SourcePosition original) {
-      return ReadAction.compute(() -> {
+      return ReadAction.nonBlocking(() -> {
         PsiFile file = original.getFile();
         int line = original.getLine();
 
@@ -319,7 +319,7 @@ public class PositionManagerImpl implements PositionManager, MultiRequestPositio
           }
         }
         return original;
-      });
+      }).executeSynchronously();
     }
   }
 
