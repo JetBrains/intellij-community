9d195f4981a5c70a8c6991f79e5700fb93640eb7
Andrey Sokolov <andrey.sokolov@jetbrains.com>
2026-05-06 00:02:47 +0200

    IJPL-240700 add in-process JB protocol test action
    Add an internal action that prompts for a jetbrains:// URL and executes the resolved JBProtocolCommand in the running IDE process.
    
    This makes protocol-command testing independent of OS protocol-handler registration and Launch Services behavior while still exercising the command dispatch path.
    
    GitOrigin-RevId: 6e31863ed7c0255df157bcb64bc432bf4e460b3c

---
+++ platform/built-in-server/src/org/jetbrains/ide/TestJbProtocolCommandAction.kt
@@ -0,0 +1,36 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.ide
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.JBProtocolCommand
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.ui.Messages
+import com.intellij.platform.ide.progress.ModalTaskOwner
+import com.intellij.platform.ide.progress.TaskCancellation
+import com.intellij.platform.ide.progress.runWithModalProgressBlocking
+
+internal class TestJbProtocolCommandAction : DumbAwareAction() {
+  @Suppress("HardCodedStringLiteral", "DialogTitleCapitalization")
+  override fun actionPerformed(e: AnActionEvent) {
+    val input = Messages.showInputDialog(
+      e.project,
+      "jetbrains:// URL (or query starting with command name):",
+      "Test JB Protocol Command",
+      null,
+      "jetbrains://idea/installPlugin?id=IdeaVIM",
+      null,
+    ) ?: return
+
+    val query = input.trim()
+      .removePrefix("jetbrains://")
+      .removePrefix("jetbrains:/")
+
+    val owner = e.project?.let(ModalTaskOwner::project) ?: ModalTaskOwner.guess()
+    val result = runWithModalProgressBlocking(owner, "Running JB Protocol Command…", TaskCancellation.cancellable()) {
+      JBProtocolCommand.execute(query)
+    }
+    result.message?.let {
+      Messages.showInfoMessage(e.project, it, "JB Protocol Result")
+    }
+  }
+}
