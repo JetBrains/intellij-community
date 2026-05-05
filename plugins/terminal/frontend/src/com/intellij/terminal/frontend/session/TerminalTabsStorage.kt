690ef17c928ab2a3fca0501004c95898cb078e3f
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-05-05 10:42:43 +0300

    [terminal] IJPL-244421 TerminalTabsStorage: remove obsolete migration
    GitOrigin-RevId: d7ce31dba2d9a80f4cbdbd7faf23cd2e2dcff35d

--- plugins/terminal/frontend/src/com/intellij/terminal/frontend/session/TerminalTabsStorage.kt
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/session/TerminalTabsStorage.kt
@@ -1,7 +1,5 @@
 package com.intellij.terminal.frontend.session
 
-import com.intellij.ide.util.RunOnceUtil
-import com.intellij.idea.AppMode
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.State
@@ -10,13 +8,10 @@ import com.intellij.openapi.components.StoragePathMacros
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.util.xmlb.annotations.XCollection
-import org.jetbrains.plugins.terminal.TerminalTabState
-import org.jetbrains.plugins.terminal.arrangement.TerminalArrangementManager
-import org.jetbrains.plugins.terminal.startup.TerminalProcessType
 
 @Service(Service.Level.PROJECT)
 @State(name = "TerminalTabsStorage", storages = [Storage(StoragePathMacros.PRODUCT_WORKSPACE_FILE)])
-internal class TerminalTabsStorage(private val project: Project) : PersistentStateComponent<TerminalTabsStorage.State> {
+internal class TerminalTabsStorage : PersistentStateComponent<TerminalTabsStorage.State> {
   class State {
     @XCollection
     var tabs: List<TerminalSessionPersistedTab> = emptyList()
@@ -25,17 +20,6 @@ internal class TerminalTabsStorage(private val project: Project) : PersistentSta
   private var state = State()
 
   fun getStoredTabs(): List<TerminalSessionPersistedTab> {
-    // Fill the initial state of the reworked terminal tabs storage from the TerminalArrangementManager (classic terminal tabs storage).
-    RunOnceUtil.runOnceForProject(project, "TerminalTabsStorage.copyFrom.TerminalArrangementManager.252") {
-      // Do not perform the migration in RemDev, since the Reworked terminal is already enabled there by default.
-      if (AppMode.isRemoteDevHost()) return@runOnceForProject
-
-      // Do not perform the migration if the user already has any stored Reworked Terminal tabs.
-      if (state.tabs.isEmpty()) {
-        restoreStateFromTerminalArrangementManager()
-      }
-    }
-
     return state.tabs
   }
 
@@ -49,25 +33,6 @@ internal class TerminalTabsStorage(private val project: Project) : PersistentSta
 
   override fun loadState(state: State) {
     this.state = state
-  }
-
-  private fun restoreStateFromTerminalArrangementManager() {
-    val tabsToCopy = TerminalArrangementManager.getInstance(project).arrangementState?.myTabStates ?: return
-
-    val newTabs = mutableListOf<TerminalSessionPersistedTab>()
-    for (tabToCopy: TerminalTabState in tabsToCopy) {
-      val tab = TerminalSessionPersistedTab(
-        name = tabToCopy.myTabName,
-        isUserDefinedName = tabToCopy.myIsUserDefinedTabTitle,
-        shellCommand = tabToCopy.myShellCommand,
-        workingDirectory = tabToCopy.myWorkingDirectory,
-        envVariables = null,
-        processType = TerminalProcessType.SHELL,
-      )
-      newTabs.add(tab)
-    }
-
-    state.tabs = newTabs
   }
 
   companion object {
