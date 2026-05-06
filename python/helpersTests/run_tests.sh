6a636011d7cf541133c455916d0dcd8d7bf142df
Pavel Karateev <pavel.karateev@jetbrains.com>
2026-05-06 17:08:46 +0200

    [python] PCQA-1660 bump PyCharm helpers tests Python version from 3.14.4 to 3.14.5rc1
    (cherry picked from commit 46a107fc0e49445dfc24c23f0b3e242e2bb40ee7)
    
    IJ-MR-203205
    
    GitOrigin-RevId: 413b49ef29c4e4dc89729017904a12c35a7db0ac

--- python/helpersTests/run_tests.sh
+++ python/helpersTests/run_tests.sh
@@ -2,6 +2,6 @@
 
 set -euxo pipefail
 
-for version in 3.9.25 3.10.20 3.11.15 3.12.13 3.13.13 3.14.4 3.15.0a8; do
+for version in 3.9.25 3.10.20 3.11.15 3.12.13 3.13.13 3.14.5rc1 3.15.0a8; do
   uv run --python "$version" __main__.py
 done
