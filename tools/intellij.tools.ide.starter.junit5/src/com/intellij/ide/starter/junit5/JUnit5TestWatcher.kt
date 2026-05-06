041e17e3e8d71dd3dd336977bb754faafa70be8c
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2026-05-06 13:41:42 +0000

    [AT-4052] do not fail test suite if WorkspaceModelIntegrationTest forget to set context
    Merge-request: IJ-MR-202973
    Merged-by: Aleksandr Bubenchikov <alexander.bubenchikov@jetbrains.com>
    
    GitOrigin-RevId: 889ccd3e6c361fc7ed46e2ce5752b19305b484c8

--- tools/intellij.tools.ide.starter.junit5/src/com/intellij/ide/starter/junit5/JUnit5TestWatcher.kt
+++ tools/intellij.tools.ide.starter.junit5/src/com/intellij/ide/starter/junit5/JUnit5TestWatcher.kt
@@ -2,22 +2,35 @@ package com.intellij.ide.starter.junit5
 
 import com.intellij.ide.starter.ide.IDETestContext
 import com.intellij.ide.starter.runner.TestWatcherActions
+import com.intellij.tools.ide.util.common.logError
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.api.extension.TestWatcher
 
-class JUnit5TestWatcher(private val testContextGetter: () -> IDETestContext) : TestWatcher {
+class JUnit5TestWatcher(private val testContextGetter: () -> IDETestContext?) : TestWatcher {
   val watcherActions = TestWatcherActions()
 
-  private fun callOnFinishedActions() = watcherActions.onFinishedActions.forEach { action -> action(testContextGetter()) }
+  private fun callOnFinishedActions(ideTestContext: IDETestContext) =
+    watcherActions.onFinishedActions.forEach { action -> action(ideTestContext) }
+
+  private fun callOrLog(ideTestContext: IDETestContext?, method: (IDETestContext) -> Unit) {
+    ideTestContext
+      ?.let { method(it) }
+    ?: logError(MISSING_CONTEXT_LOG_MESSAGE)
+  }
 
   override fun testSuccessful(context: ExtensionContext?) {
-    callOnFinishedActions()
+    callOrLog(testContextGetter(), ::callOnFinishedActions)
     super.testSuccessful(context)
   }
 
   override fun testFailed(context: ExtensionContext?, cause: Throwable?) {
-    watcherActions.onFailureActions.forEach { action -> action(testContextGetter()) }
-    callOnFinishedActions()
+    callOrLog(testContextGetter()) {
+      watcherActions.onFailureActions.forEach { action -> action(it) }
+      callOnFinishedActions(it)
+    }
     super.testFailed(context, cause)
+  }
+  companion object {
+    internal const val MISSING_CONTEXT_LOG_MESSAGE: String = "JUnit5TestWatcher can't run as there is no context"
   }
 }
