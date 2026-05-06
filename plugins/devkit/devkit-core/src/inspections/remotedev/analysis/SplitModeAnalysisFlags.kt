5675f2a283d4f0f9d93f84adab67b65d5be0a0df
Nikita Katkov <nikita.katkov@jetbrains.com>
2026-05-06 14:57:47 +0200

    [remote development tooling] IJPL-231068 Inline registry keys for references search
    GitOrigin-RevId: 16f1554e32a99ea55984b1e674387275968fd9ee

--- plugins/devkit/devkit-core/src/inspections/remotedev/analysis/SplitModeAnalysisFlags.kt
+++ plugins/devkit/devkit-core/src/inspections/remotedev/analysis/SplitModeAnalysisFlags.kt
@@ -4,29 +4,21 @@ package org.jetbrains.idea.devkit.inspections.remotedev.analysis
 import com.intellij.openapi.util.registry.RegistryManager
 
 internal object SplitModeAnalysisFlags {
-  const val TRANSITIVE_DEPENDENCIES_ANALYSIS_KEY: String =
-    "devkit.remote.dev.split.mode.analysis.transitive.dependencies"
-  const val CONTAINING_PLUGINS_ANALYSIS_KEY: String =
-    "devkit.remote.dev.split.mode.analysis.containing.plugins"
-  const val SKIP_INSPECTIONS_FOR_PREDEFINED_MODULE_KINDS_KEY: String =
-    "devkit.remote.dev.split.mode.inspections.skip.predefined"
-  const val ADDITIONAL_PREDEFINED_MODULE_KINDS_FILE_KEY: String =
-    "devkit.remote.dev.split.mode.analysis.predefined.module.kinds.additional.file"
 
   fun isTransitiveDependenciesAnalysisEnabled(): Boolean {
-    return RegistryManager.getInstance().`is`(TRANSITIVE_DEPENDENCIES_ANALYSIS_KEY)
+    return RegistryManager.getInstance().`is`("devkit.remote.dev.split.mode.analysis.transitive.dependencies")
   }
 
   fun isContainingPluginsAnalysisEnabled(): Boolean {
-    return RegistryManager.getInstance().`is`(CONTAINING_PLUGINS_ANALYSIS_KEY)
+    return RegistryManager.getInstance().`is`("devkit.remote.dev.split.mode.analysis.containing.plugins")
   }
 
   fun isSkippingInspectionsForPredefinedModuleKindsEnabled(): Boolean {
-    return RegistryManager.getInstance().`is`(SKIP_INSPECTIONS_FOR_PREDEFINED_MODULE_KINDS_KEY)
+    return RegistryManager.getInstance().`is`("devkit.remote.dev.split.mode.inspections.skip.predefined")
   }
 
   fun getAdditionalPredefinedModuleKindsFilePath(): String? {
-    val value = RegistryManager.getInstance().get(ADDITIONAL_PREDEFINED_MODULE_KINDS_FILE_KEY).asString()
+    val value = RegistryManager.getInstance().get("devkit.remote.dev.split.mode.analysis.predefined.module.kinds.additional.file").asString()
     if (value.isBlank()) {
       return null
     }
