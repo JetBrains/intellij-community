5aadc45fefed1b233f4dcdbcc94336709bbd7b0b
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-05-06 14:20:15 +0300

    [terminal] IJPL-244419 Make 'Open in Terminal' action start remote terminal process
    Virtual file implementations we receive in Remote Dev mode are `ThinClientVirtualFile` and `ThinClientNodeVirtualFile` - they represent the path native to the remote environment.
    But to start the terminal process in the correct environment - we need a remote NIO path.
    So, until these files properly implement `toNioPath`, we have to handle this case manually.
    
    Also, ensure that we pass the directory, not the file path to `createTerminalTab`.
    
    GitOrigin-RevId: 2ba26badaf9be03222c3b46f600f42a26dbb3192

--- plugins/terminal/frontend/src/com/intellij/terminal/frontend/action/RevealFileInReworkedTerminalAction.kt
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/action/RevealFileInReworkedTerminalAction.kt
@@ -5,11 +5,21 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.toNioPathOrNull
+import com.intellij.platform.eel.annotations.NativePath
+import com.intellij.platform.eel.path.EelPath
+import com.intellij.platform.eel.path.EelPathException
+import com.intellij.platform.eel.provider.asNioPath
+import com.intellij.platform.eel.provider.getEelDescriptor
 import com.intellij.terminal.frontend.toolwindow.impl.createTerminalTab
 import org.jetbrains.plugins.terminal.TerminalEngine
 import org.jetbrains.plugins.terminal.TerminalOptionsProvider
+import java.nio.file.Path
+import kotlin.io.path.isDirectory
 
 /**
  * Copy of [org.jetbrains.plugins.terminal.action.RevealFileInTerminalAction], but frontend-only.
@@ -25,8 +35,9 @@ import org.jetbrains.plugins.terminal.TerminalOptionsProvider
 internal class RevealFileInReworkedTerminalAction : DumbAwareAction(), ActionRemoteBehaviorSpecification.Frontend {
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.project ?: return
-    val file = getSelectedFile(e) ?: return
-    createTerminalTab(project, workingDirectory = file.path)
+    val file = getContextFile(e) ?: return
+    val path = getNearestDirectoryPath(file, project) ?: return
+    createTerminalTab(project, workingDirectory = path.toString())
   }
 
   override fun update(e: AnActionEvent) {
@@ -40,12 +51,48 @@ internal class RevealFileInReworkedTerminalAction : DumbAwareAction(), ActionRem
 
     val project = e.project
     val editor = e.getData(CommonDataKeys.EDITOR)
-    return project != null && !LightEdit.owns(project) && getSelectedFile(e) != null &&
+    return project != null && !LightEdit.owns(project) && getContextFile(e) != null &&
            (!e.isFromContextMenu || editor == null || !editor.getSelectionModel().hasSelection())
   }
 
-  private fun getSelectedFile(e: AnActionEvent): VirtualFile? {
+  private fun getContextFile(e: AnActionEvent): VirtualFile? {
     return e.getData(CommonDataKeys.VIRTUAL_FILE)
+  }
+
+  private fun getNearestDirectoryPath(file: VirtualFile, project: Project): Path? {
+    val nioPath = file.toNioPathOrNull()
+    if (nioPath != null) {
+      return getNearestDirectory(nioPath)
+    }
+
+    // In the case of Remote Dev, `file.toNioPath` is not implemented.
+    // And `file.path` is a local path in the remote environment.
+    // So, let's try to get the remote NIO path to start a Terminal process in the correct environment.
+    val remotePath = getRemotePath(file.path, project)
+    return if (remotePath != null) getNearestDirectory(remotePath) else null
+  }
+
+  private fun getRemotePath(path: @NativePath String, project: Project): Path? {
+    val eelDescriptor = project.getEelDescriptor()
+    val eelPath = try {
+      EelPath.parse(path, eelDescriptor)
+    }
+    catch (e: EelPathException) {
+      thisLogger().warn("Failed to parse EelPath for path: $path", e)
+      return null
+    }
+
+    return try {
+      eelPath.asNioPath()
+    }
+    catch (e: IllegalArgumentException) {
+      thisLogger().warn("Failed to convert EelPath to Nio Path for path: $path", e)
+      return null
+    }
+  }
+
+  private fun getNearestDirectory(path: Path): Path {
+    return if (path.isDirectory()) path else path.parent
   }
 
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
