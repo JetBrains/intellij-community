7aac0d6e9fc8a4b6d8275700dc54d23d2bc455a7
Max Medvedev <maxim.medvedev@jetbrains.com>
2026-05-07 18:23:24 +0200

    IJPL-244297 fix retrieval of existing contexts for files
    don't count external source sets. they don't provide any meaningful information,
    but still can hide existing proper source sets that are sitting on deeper lever
    
    GitOrigin-RevId: 22329395d49f5e381ea12d7e912a807ff334ad68

--- platform/projectModel-impl/src/com/intellij/multiverse/ProjectModelEntityContextProvider.kt
+++ platform/projectModel-impl/src/com/intellij/multiverse/ProjectModelEntityContextProvider.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.multiverse
 
 import com.intellij.codeInsight.multiverse.CodeInsightContext
@@ -42,9 +42,9 @@ internal class ProjectModelEntityContextProvider : CodeInsightContextProvider {
       includeContentSets = true,
       includeContentNonIndexableSets = true,
       includeExternalSets = true,
-      includeExternalSourceSets = true,
-      includeExternalNonIndexableSets = true,
-      includeCustomKindSets = true
+      includeExternalSourceSets = false,
+      includeExternalNonIndexableSets = false,
+      includeCustomKindSets = false
     )
     if (fileSets.isEmpty()) return emptyList()
 
