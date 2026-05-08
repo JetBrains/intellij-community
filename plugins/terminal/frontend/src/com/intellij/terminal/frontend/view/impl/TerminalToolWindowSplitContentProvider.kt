719c0d97f00a767b2710407c0c2dcdca061a5db1
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-05-08 15:35:40 +0300

    [terminal] IJPL-244419 Use remote working directory for starting a process during tab split
    GitOrigin-RevId: 690fd9fc7d55349a7148ce6cc24f40dc859288ee

--- plugins/terminal/frontend/src/com/intellij/terminal/frontend/view/impl/TerminalToolWindowSplitContentProvider.kt
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/view/impl/TerminalToolWindowSplitContentProvider.kt
@@ -1,9 +1,12 @@
 package com.intellij.terminal.frontend.view.impl
 
 import com.intellij.openapi.project.Project
+import com.intellij.platform.eel.path.EelPath
+import com.intellij.platform.eel.provider.asNioPath
 import com.intellij.terminal.frontend.toolwindow.TerminalToolWindowTabsManager
 import com.intellij.terminal.frontend.toolwindow.findTabByContent
 import com.intellij.terminal.frontend.toolwindow.impl.shouldUseReworkedTerminal
+import com.intellij.terminal.frontend.view.TerminalView
 import com.intellij.toolWindow.ToolWindowSplitContentProvider
 import com.intellij.ui.content.Content
 import org.jetbrains.plugins.terminal.TerminalOptionsProvider
@@ -11,6 +14,8 @@ import org.jetbrains.plugins.terminal.TerminalTabState
 import org.jetbrains.plugins.terminal.TerminalToolWindowManager
 import org.jetbrains.plugins.terminal.fus.TerminalOpeningWay
 import org.jetbrains.plugins.terminal.fus.TerminalStartupFusInfo
+import org.jetbrains.plugins.terminal.util.getNow
+import java.nio.file.Path
 
 internal class TerminalToolWindowSplitContentProvider : ToolWindowSplitContentProvider {
   override fun createContentCopy(project: Project, content: Content): Content {
@@ -27,14 +32,22 @@ internal class TerminalToolWindowSplitContentProvider : ToolWindowSplitContentPr
   private fun createReworkedTerminalContent(project: Project, content: Content, fusInfo: TerminalStartupFusInfo): Content {
     val manager = TerminalToolWindowTabsManager.getInstance(project)
     val originalView = manager.findTabByContent(content)?.view
-    val currentDirectory = originalView?.getCurrentDirectory()
+    val currentDirectory = originalView?.getCurrentDirectoryPath()
 
     return manager.createTabBuilder()
-      .workingDirectory(currentDirectory)
+      .workingDirectory(currentDirectory?.toString())
       .shouldAddToToolWindow(false)
       .startupFusInfo(fusInfo)
       .createTab()
       .content
+  }
+
+  private fun TerminalView.getCurrentDirectoryPath(): Path? {
+    val currentDirectory = getCurrentDirectory() ?: return null
+    val session = sessionDeferred.getNow() ?: return null
+    return runCatching {
+      EelPath.parse(currentDirectory, session.eelDescriptor).asNioPath()
+    }.getOrNull()
   }
 
   private fun createClassicTerminalContent(
