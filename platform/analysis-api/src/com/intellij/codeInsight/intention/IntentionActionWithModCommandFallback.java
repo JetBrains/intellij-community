877ec0c2bafdf2c50dfd92782f37c291f7d1990f
Tagir Valeev <tagir.valeev@jetbrains.com>
2026-05-08 11:22:35 +0200

    LSP-448 [intention-actions] generatePreview
    GitOrigin-RevId: e977ac45fa406e2676559bf2e8c0c9a5f389b2f9

--- platform/analysis-api/src/com/intellij/codeInsight/intention/IntentionActionWithModCommandFallback.java
+++ platform/analysis-api/src/com/intellij/codeInsight/intention/IntentionActionWithModCommandFallback.java
@@ -1,8 +1,13 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.intention;
 
+import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo;
+import com.intellij.modcommand.ActionContext;
 import com.intellij.modcommand.ModCommand;
 import com.intellij.modcommand.ModCommandAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,6 +19,15 @@ import org.jetbrains.annotations.Nullable;
  */
 @ApiStatus.Experimental
 public interface IntentionActionWithModCommandFallback extends IntentionAction {
+  @Override
+  default @NotNull IntentionPreviewInfo generatePreview(@NotNull Project project, @NotNull Editor editor, @NotNull PsiFile psiFile) {
+    ModCommandAction fallback = getFallbackModCommandAction();
+    if (fallback != null) {
+      return fallback.generatePreview(ActionContext.from(editor, psiFile));
+    }
+    return IntentionAction.super.generatePreview(project, editor, psiFile);
+  }
+
   /**
    * @return a ModCommandAction that can be used instead of this intention action.
    * The command should provide a similar behavior, but probably miss some advanced features like complex UI,
