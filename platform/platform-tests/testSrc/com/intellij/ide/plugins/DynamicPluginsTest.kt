bfcaa457f3686f69a70b1b01ddb66209951bb369
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2026-05-08 14:24:40 +0200

    [tests] IJPL-244459 DynamicPluginsTest: remove unnecessary suppression
    GitOrigin-RevId: 7fd8a6da675e7334de36efbbb1b4b6473073ca64

--- platform/platform-tests/testSrc/com/intellij/ide/plugins/DynamicPluginsTest.kt
+++ platform/platform-tests/testSrc/com/intellij/ide/plugins/DynamicPluginsTest.kt
@@ -1,6 +1,4 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-@file:Suppress("UsePropertyAccessSyntax")
-
 package com.intellij.ide.plugins
 
 import com.intellij.codeInsight.intention.IntentionAction
