5127f72925f552bd38971527e30917af8380f84d
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2026-05-08 18:53:42 +0200

    cleanup [platform]: formatting
    GitOrigin-RevId: f9bdbb82faeb0c35c63bab9cdcbd6be7d322c44f

--- platform/platform-impl/src/com/intellij/util/io/PlatformNioHelper.java
+++ platform/platform-impl/src/com/intellij/util/io/PlatformNioHelper.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.io;
 
 import com.intellij.openapi.util.io.NioFiles;
@@ -33,32 +33,26 @@ public final class PlatformNioHelper {
     }
   }
 
-  /**
-   * A specialized alternative to {@link Files#newDirectoryStream} and {@link Files#walkFileTree}.
-   * Only children that names are in the filter set are passed to consumer.
-   * (filter == null) means 'no filter', i.e. all children must be passed to consumer, but filter={} (empty set) means 'do nothing'.
-   *
-   * @see NioFiles#readAttributes
-   */
+  /// A specialized alternative to [Files#newDirectoryStream] and [Files#walkFileTree].
+  /// Only children whose names are in the filter set are passed to consumer.
+  /// `(filter == null)` means 'no filter', i.e., all children must be passed to consumer; `filter={}` (an empty set) means 'do nothing'.
+  ///
+  /// @see NioFiles#readAttributes
   @SuppressWarnings("UnnecessaryFullyQualifiedName")
   public static void visitDirectory(
     @NotNull Path directory,
     @Nullable Set<String> filter,
     @NotNull BiPredicate<Path, Result<BasicFileAttributes>> consumer
   ) throws IOException, SecurityException {
-    if (filter != null && filter.isEmpty()) {
-      return;//nothing to read
-    }
+    if (filter != null && filter.isEmpty()) return;  // nothing to read
+
     try (var dirStream = directory.getFileSystem().provider().newDirectoryStream(directory, FetchAttributesFilter.ACCEPT_ALL)) {
       for (var path : dirStream) {
         if (filter != null && !filter.contains(path.getFileName().toString())) {
           continue;
         }
 
-        BasicFileAttributes attrs = null;
-        if (path instanceof sun.nio.fs.BasicFileAttributesHolder) {
-          attrs = ((sun.nio.fs.BasicFileAttributesHolder)path).get();
-        }
+        var attrs = path instanceof sun.nio.fs.BasicFileAttributesHolder holder ? holder.get() : null;
 
         Result<BasicFileAttributes> result;
         if (attrs != null) {
