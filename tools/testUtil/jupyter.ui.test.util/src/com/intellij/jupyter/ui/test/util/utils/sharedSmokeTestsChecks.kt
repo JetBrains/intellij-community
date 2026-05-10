9d749b158b8ae92aae21b30fdbc6eb195e32efba
Daniil Stankevych <daniil.stankevych@jetbrains.com>
2026-05-11 02:00:51 +0300

    AT-4570 [JUIT]: extract from the common part `change color theme`
    GitOrigin-RevId: a293fd246f0fc6c2853d7a5af00726df1b89ffa0

--- tools/testUtil/jupyter.ui.test.util/src/com/intellij/jupyter/ui/test/util/utils/sharedSmokeTestsChecks.kt
+++ tools/testUtil/jupyter.ui.test.util/src/com/intellij/jupyter/ui/test/util/utils/sharedSmokeTestsChecks.kt
@@ -1,10 +1,7 @@
 package com.intellij.jupyter.ui.test.util.utils
 
 import com.intellij.driver.client.Driver
-import com.intellij.driver.sdk.IdeTheme
-import com.intellij.driver.sdk.changeTheme
 import com.intellij.driver.sdk.step
-import com.intellij.driver.sdk.ui.components.common.ideFrame
 import com.intellij.driver.sdk.ui.components.notebooks.FirstCell
 import com.intellij.driver.sdk.ui.components.notebooks.NotebookEditorUiComponent
 import com.intellij.driver.sdk.ui.components.notebooks.NotebookType
@@ -14,115 +11,13 @@ import com.intellij.driver.sdk.ui.components.notebooks.withNotebookEditor
 import com.intellij.driver.sdk.ui.should
 import com.intellij.driver.sdk.waitFor
 import com.intellij.driver.sdk.waitForOne
-import com.intellij.jupyter.ui.test.util.codeinsight.checkLookAndFeel
 import com.intellij.jupyter.ui.test.util.kernel.runAllCellsAndWaitExecuted
-import com.intellij.jupyter.ui.test.util.kernel.softRunAllCellsAndWaitExecuted
-import java.awt.Color
-import kotlin.math.roundToInt
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 // Shared smoke test utilities used across Kotlin and Python notebooks to avoid duplication
-fun Driver.checkColorThemeChange() = ideFrame {
-  step("Change color theme to light") {
-    changeTheme(IdeTheme.LIGHT)
-    notebookEditor { checkLookAndFeel(IdeTheme.LIGHT) }
-  }
 
-  step("Change color theme to dark") {
-    changeTheme(IdeTheme.DARK)
-    notebookEditor { checkLookAndFeel(IdeTheme.DARK) }
-  }
-}
-
-fun Driver.checkColorThemeChangeForAllNotebookElements() {
-  fun Color.toHex(): String = "#%02X%02X%02X".format(red, green, blue)
-  fun Color.luma(): Int = (0.2126 * red + 0.7152 * green + 0.0722 * blue).roundToInt()
-  fun assertLight(color: Color, what: String) = waitFor(message = "$what should be light, actual=${color.toHex()} luma=${color.luma()}", timeout = 15.seconds) { color.luma() > 180 }
-  fun assertDark(color: Color, what: String) = waitFor(message = "$what should be dark, actual=${color.toHex()} luma=${color.luma()}", timeout = 15.seconds) { color.luma() < 100 }
-
-  ideFrame {
-    notebookEditor {
-      // Prepare cells: code, markdown, table, static matplotlib plot
-      step("Prepare notebook content: code + markdown + table + plot") {
-        pasteToCell(FirstCell, "print(1)")
-        addMarkdownCell("# header")
-        addCodeCell(
-          """
-            import pandas as pd
-            df = pd.DataFrame([[1,2],[3,4]], columns=["A","B"])
-            df
-          """.trimIndent()
-        )
-        addCodeCell(
-          """
-            %matplotlib inline
-            import matplotlib.pyplot as plt
-            import math
-            x = [i * (6.28 / 99) for i in range(100)]
-            y = [math.sin(val) for val in x]
-
-            plt.figure(figsize=(3,2))
-            plt.plot(x, y)
-            plt.show()
-          """.trimIndent()
-        )
-        softRunAllCellsAndWaitExecuted(5.minutes)
-        waitFor("Markdown rendered", 20.seconds) { jcefOffScreens.isNotEmpty() }
-        waitFor("Table rendered", 20.seconds) { notebookTables.isNotEmpty() }
-        waitFor("Plot rendered", 30.seconds) { imagePanel.isNotEmpty() || jcefOffScreens.size >= 2 }
-      }
-
-      step("Validate jcef height") {
-        val regex = Regex("""(\d+)x(\d+)""")
-        val matchResult = regex.find(jcefOffScreens.last().component.toString())
-        val height = matchResult?.groupValues[2]?.toInt()
-
-        should("Jcef panel should have non-zero height") {
-          height!! > 0
-        }
-      }
-
-      step("Validate static plot height") {
-        val regex = Regex("""(\d+)x(\d+)""")
-        val matchResult = regex.find(imagePanel.last().component.toString())
-        val height = matchResult?.groupValues[2]?.toInt()
-
-        should("Image panel should have non-zero height") {
-          height!! > 0
-        }
-      }
-
-      step("Validate colors in LIGHT theme") {
-        changeTheme(IdeTheme.LIGHT)
-        checkLookAndFeel(IdeTheme.LIGHT)
-
-        val md = jcefOffScreens.firstOrNull() ?: imagePanel.first() // markdown JCEF or fallback image
-        val mdColor = md.getColor(null)
-        assertLight(mdColor, "Markdown background")
-
-        val table = notebookTables.first()
-        val tableColor = table.getColor(null)
-        assertLight(tableColor, "Table background")
-      }
-
-      step("Validate colors in DARK theme") {
-        changeTheme(IdeTheme.DARK)
-        checkLookAndFeel(IdeTheme.DARK)
-
-        val md = jcefOffScreens.firstOrNull() ?: imagePanel.first()
-        val mdColor = md.getColor(null)
-        assertDark(mdColor, "Markdown background")
-
-        val table = notebookTables.first()
-        val tableColor = table.getColor(null)
-        assertDark(tableColor, "Table background")
-      }
-    }
-  }
-}
-
-fun NotebookEditorUiComponent.checkCreateNewNotebook(type: NotebookType, name: String = "test") {
+fun NotebookEditorUiComponent.checkCreateNewNotebook(name: String = "test") {
   step("Create a new notebook") {
     driver.createNewNotebook(name, type)
   }
