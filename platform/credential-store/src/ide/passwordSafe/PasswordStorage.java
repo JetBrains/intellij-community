14872a1d45b2c2d6c529335cb69785170cccda43
Nikolay Chashnikov <nikolay.chashnikov@jetbrains.com>
2026-05-11 15:09:09 +0200

    IJPL-234888 credential store: remove unused deprecated API
    GitOrigin-RevId: 3bc4efa8426a7da21306ed3f550d7579ea8e83c3

--- platform/credential-store/src/ide/passwordSafe/PasswordStorage.java
+++ platform/credential-store/src/ide/passwordSafe/PasswordStorage.java
@@ -3,7 +3,6 @@ package com.intellij.ide.passwordSafe;
 
 import com.intellij.credentialStore.CredentialAttributes;
 import com.intellij.credentialStore.Credentials;
-import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -11,19 +10,6 @@ import org.jetbrains.annotations.Nullable;
 @Deprecated(forRemoval = true)
 @SuppressWarnings("DeprecatedIsStillUsed")
 public interface PasswordStorage {
-  /** @deprecated Please use {@link #set(CredentialAttributes, Credentials)} */
-  @Deprecated(forRemoval = true)
-  default void storePassword(@SuppressWarnings("unused") @Nullable Project project, @NotNull Class<?> requestor, @NotNull String key, @Nullable String value) {
-    set(new CredentialAttributes(requestor.getName(), key, requestor), value == null ? null : new Credentials(key, value));
-  }
-
-  /** @deprecated use {@link #get(CredentialAttributes)} + {@link Credentials#getPasswordAsString()} */
-  @Deprecated(forRemoval = true)
-  default @Nullable String getPassword(@SuppressWarnings("unused") @Nullable Project project, @NotNull Class<?> requestor, @NotNull String key) {
-    var credentials = get(new CredentialAttributes(requestor.getName(), key, requestor));
-    return credentials == null ? null : credentials.getPasswordAsString();
-  }
-
   @Nullable Credentials get(@NotNull CredentialAttributes attributes);
 
   void set(@NotNull CredentialAttributes attributes, @Nullable Credentials credentials);
