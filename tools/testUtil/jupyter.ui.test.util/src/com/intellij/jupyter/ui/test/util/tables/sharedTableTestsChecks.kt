76332d14dbc0847d8d2a9e7f278769146c3b5580
Daniil Stankevych <daniil.stankevych@jetbrains.com>
2026-05-11 11:58:57 +0300

    AT-4570 [KTNB]: migrate `checkMarkdownCellRendering` from common module to `KotlinNotebooksSmokeTest`
    GitOrigin-RevId: f2f292e9226d3c42402d68a5b6592f0b006129ad

--- tools/testUtil/jupyter.ui.test.util/src/com/intellij/jupyter/ui/test/util/tables/sharedTableTestsChecks.kt
+++ tools/testUtil/jupyter.ui.test.util/src/com/intellij/jupyter/ui/test/util/tables/sharedTableTestsChecks.kt
@@ -1,16 +1,12 @@
 package com.intellij.jupyter.ui.test.util.tables
 
-import com.intellij.driver.sdk.step
 import com.intellij.driver.sdk.ui.components.common.ideFrame
 import com.intellij.driver.sdk.ui.components.elements.popup
-import com.intellij.driver.sdk.ui.components.notebooks.LastCell
 import com.intellij.driver.sdk.ui.components.notebooks.NotebookEditorUiComponent
 import com.intellij.driver.sdk.ui.getClipboardText
 import com.intellij.driver.sdk.ui.should
 import com.intellij.driver.sdk.waitFor
-import com.intellij.jupyter.ui.test.util.kernel.runAllCellsAndWaitExecuted
 import io.kotest.matchers.shouldBe
-import kotlin.time.Duration.Companion.minutes
 
 // Shared tables test utilities used across Kotlin and Python notebooks to avoid duplication
 fun NotebookEditorUiComponent.checkTableSize() {
--- tools/testUtil/jupyter.ui.test.util/src/com/intellij/jupyter/ui/test/util/utils/sharedSmokeTestsChecks.kt
+++ tools/testUtil/jupyter.ui.test.util/src/com/intellij/jupyter/ui/test/util/utils/sharedSmokeTestsChecks.kt
@@ -12,38 +12,6 @@ import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 // Shared smoke test utilities used across Kotlin and Python notebooks to avoid duplication
-
-//issue = "PY-75616"
-fun NotebookEditorUiComponent.checkMarkdownCellRendering() {
-  step("Create a markdown cell") {
-    addMarkdownCell("# header")
-    runCell()
-  }
-  step("Check the cell is rendered") {
-    waitFor("The MD cell should be rendered", 15.seconds) {
-      jcefOffScreens.isNotEmpty()
-    }
-  }
-  step("Check markdown cell is not rendered after double click") {
-    jcefOffScreens.first().doubleClick()
-    waitFor("The MD cell should be rendered", 15.seconds) {
-      jcefOffScreens.isEmpty()
-    }
-  }
-  step("Check markdown cell is rendered after switching focus") {
-    clickOnCell(FirstCell)
-    waitFor("The MD cell should be rendered", 15.seconds) {
-      jcefOffScreens.isNotEmpty()
-    }
-  }
-  val expectedMdCellHtmlContent = "<h1 data-jupyter-id=\"header\">header"
-  step("Check the cell content") {
-    jcefOffScreens.first().should {
-      htmlSource.contains(expectedMdCellHtmlContent)
-    }
-  }
-}
-
 fun NotebookEditorUiComponent.checkRunCellsAndCleanUpOutputs(
   runAllCellsAndAwait: NotebookEditorUiComponent.(kotlin.time.Duration) -> Unit = { timeout -> runAllCellsAndWaitExecuted(timeout) },
 ) {
