8e19515557e047d4ef156d088ac245311691950b
Andrey Zaytsev <andrey.zaytsev@jetbrains.com>
2026-05-12 16:05:12 +0200

    refactor [lsp]: support cancellation of server-initiated progresses
    GitOrigin-RevId: 61a2fe682f06686511dadfeaac3fcff7b0cf21ff

--- fleet/lsp.protocol/srcCommonMain/com/jetbrains/lsp/implementation/progress.kt
+++ fleet/lsp.protocol/srcCommonMain/com/jetbrains/lsp/implementation/progress.kt
@@ -8,13 +8,20 @@ import com.jetbrains.lsp.protocol.StringOrInt
 import com.jetbrains.lsp.protocol.Window
 import com.jetbrains.lsp.protocol.WorkDoneProgress
 import com.jetbrains.lsp.protocol.WorkDoneProgressCreateParams
+import fleet.multiplatform.shims.MultiplatformConcurrentHashMap
 import fleet.util.UID
 import fleet.util.async.catching
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -26,28 +33,7 @@ fun interface ProgressReporter {
     fun report(progress: WorkDoneProgress)
 }
 
-suspend fun<T> LspClient.withServerInitiatedProgress(
-    capabilities: ClientCapabilities,
-    beginTitle: String,
-    body: suspend CoroutineScope.(ProgressReporter) -> T,
-): T {
-    val token = when {
-        capabilities.window?.workDoneProgress == true -> {
-            val token = ProgressToken(StringOrInt.string(UID.random().id))
-            catching {
-                request(Window.CreateProgress, WorkDoneProgressCreateParams(token))
-            }
-            token
-        }
-
-        else -> null
-    }
-    return withProgress(token, beginTitle) { progress ->
-        body(progress)
-    }
-}
-
-suspend fun<T> LspClient.withProgress(
+suspend fun <T> LspClient.withProgress(
     token: ProgressToken?,
     beginTitle: String,
     body: suspend CoroutineScope.(ProgressReporter) -> T,
@@ -78,9 +64,76 @@ suspend fun<T> LspClient.withProgress(
                 } finally {
                     notify(
                         LSP.ProgressNotificationType,
-                        ProgressParams(token, LSP.json.encodeToJsonElement(WorkDoneProgress.serializer(), WorkDoneProgress.End()))
+                        ProgressParams(
+                            token,
+                            LSP.json.encodeToJsonElement(WorkDoneProgress.serializer(), WorkDoneProgress.End())
+                        )
                     )
                 }
+            }
+        }
+    }
+
+
+interface ServerInitiatedProgresses {
+    fun <T> asyncWithProgress(
+        scope: CoroutineScope,
+        title: String,
+        cancellable: Boolean,
+        body: suspend CoroutineScope.(ProgressReporter) -> T
+    ): Deferred<T>
+}
+
+fun LspHandlersBuilder.serverInitiatedProgresses(
+    client: LspClient,
+    capabilities: ClientCapabilities
+): ServerInitiatedProgresses =
+    when {
+        capabilities.window?.workDoneProgress == true -> {
+            val cancellableJobs = MultiplatformConcurrentHashMap<ProgressToken, Job>()
+            notification(Window.CancelProgress) { n ->
+                cancellableJobs[n.token]?.let { job ->
+                    job.cancel(CancellationException("Canceled by lsp client"))
+                    job.join()
+                }
+            }
+            object : ServerInitiatedProgresses {
+                override fun <T> asyncWithProgress(
+                    scope: CoroutineScope,
+                    title: String,
+                    cancellable: Boolean,
+                    body: suspend CoroutineScope.(ProgressReporter) -> T
+                ): Deferred<T> =
+                    scope.async {
+                        val token = ProgressToken(StringOrInt.string(UID.random().id))
+                        catching {
+                            client.request(Window.CreateProgress, WorkDoneProgressCreateParams(token))
+                        }
+                        if (cancellable) {
+                            cancellableJobs[token] = coroutineContext.job
+                        }
+                        try {
+                            client.withProgress(token, title) { reporter ->
+                                body(reporter)
+                            }
+                        } finally {
+                            cancellableJobs.remove(token)
+                        }
+                    }
+            }
+        }
+
+        else -> {
+            object : ServerInitiatedProgresses {
+                override fun <T> asyncWithProgress(
+                    scope: CoroutineScope,
+                    title: String,
+                    cancellable: Boolean,
+                    body: suspend CoroutineScope.(ProgressReporter) -> T
+                ): Deferred<T> =
+                    scope.async {
+                        body(ProgressReporter.NOOP)
+                    }
             }
         }
     }
