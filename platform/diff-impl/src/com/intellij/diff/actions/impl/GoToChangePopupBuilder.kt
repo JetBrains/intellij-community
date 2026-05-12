2a9fe5cd8eb976f35a5efcfada71a76fc08d6573
Ivan Semenov <ivan.semenov@jetbrains.com>
2026-05-12 15:41:11 +0200

    refactor [diff]: convert to Kotlin
    GitOrigin-RevId: 99c31b41ba635226154f057bc02429ff136e9569

--- platform/diff-impl/src/com/intellij/diff/actions/impl/GoToChangePopupBuilder.kt
+++ platform/diff-impl/src/com/intellij/diff/actions/impl/GoToChangePopupBuilder.kt
@@ -1,124 +1,96 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.diff.actions.impl;
+package com.intellij.diff.actions.impl
 
-import com.intellij.diff.chains.DiffRequestChain;
-import com.intellij.diff.chains.DiffRequestProducer;
-import com.intellij.diff.tools.util.DiffDataKeys;
-import com.intellij.openapi.actionSystem.ActionGroup;
-import com.intellij.openapi.actionSystem.ActionUpdateThread;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.ex.ActionUtil;
-import com.intellij.openapi.diff.DiffBundle;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.ui.popup.JBPopup;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
-import com.intellij.openapi.ui.popup.PopupStep;
-import com.intellij.openapi.ui.popup.util.BaseListPopupStep;
-import com.intellij.util.Consumer;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.diff.chains.DiffRequestChain
+import com.intellij.diff.chains.DiffRequestProducer
+import com.intellij.diff.tools.util.DiffDataKeys
+import com.intellij.openapi.actionSystem.ActionGroup
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.ex.ActionUtil.copyFrom
+import com.intellij.openapi.diff.DiffBundle
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.ui.popup.JBPopup
+import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.openapi.ui.popup.PopupStep
+import com.intellij.openapi.ui.popup.util.BaseListPopupStep
+import com.intellij.util.Consumer
+import java.awt.event.MouseEvent
 
-import java.awt.event.InputEvent;
-import java.awt.event.MouseEvent;
-
-public final class GoToChangePopupBuilder {
-  public interface Chain extends DiffRequestChain {
-    @Nullable
-    AnAction createGoToChangeAction(@NotNull Consumer<? super Integer> onSelected, int defaultSelection);
+object GoToChangePopupBuilder {
+  @JvmStatic
+  fun create(chain: DiffRequestChain, onSelected: Consumer<in Int>, defaultSelection: Int): AnAction {
+    if (chain is Chain) {
+      val action = chain.createGoToChangeAction(onSelected, defaultSelection)
+      if (action != null) return action
+    }
+    return SimpleGoToChangePopupAction(chain, onSelected, defaultSelection)
   }
 
-  public static @NotNull AnAction create(@NotNull DiffRequestChain chain, @NotNull Consumer<? super Integer> onSelected, int defaultSelection) {
-    if (chain instanceof Chain) {
-      AnAction action = ((Chain)chain).createGoToChangeAction(onSelected, defaultSelection);
-      if (action != null) return action;
-    }
-    return new SimpleGoToChangePopupAction(chain, onSelected, defaultSelection);
+  interface Chain : DiffRequestChain {
+    fun createGoToChangeAction(onSelected: Consumer<in Int>, defaultSelection: Int): AnAction?
   }
 
-  public abstract static class BaseGoToChangePopupAction extends ActionGroup implements DumbAware {
-    public BaseGoToChangePopupAction() {
-      ActionUtil.copyFrom(this, "GotoChangedFile");
-      setPopup(true);
-      getTemplatePresentation().setPerformGroup(true);
+  abstract class BaseGoToChangePopupAction : ActionGroup(), DumbAware {
+    init {
+      copyFrom(this, "GotoChangedFile")
+      setPopup(true)
+      getTemplatePresentation().setPerformGroup(true)
     }
 
-    @Override
-    public AnAction @NotNull [] getChildren(@Nullable AnActionEvent e) {
-      return EMPTY_ARRAY;
+    final override fun getChildren(e: AnActionEvent?): Array<AnAction> = EMPTY_ARRAY
+
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+
+    override fun update(e: AnActionEvent) {
+      e.presentation.setEnabledAndVisible(canNavigate() && e.getData(DiffDataKeys.DIFF_CONTEXT) != null)
     }
 
-    @Override
-    public @NotNull ActionUpdateThread getActionUpdateThread() {
-      return ActionUpdateThread.EDT;
-    }
+    protected abstract fun canNavigate(): Boolean
 
-    @Override
-    public void update(@NotNull AnActionEvent e) {
-      e.getPresentation().setEnabledAndVisible(canNavigate() && e.getData(DiffDataKeys.DIFF_CONTEXT) != null);
-    }
+    final override fun actionPerformed(e: AnActionEvent) {
+      val popup = createPopup(e)
 
-    protected abstract boolean canNavigate();
-
-    @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
-      final JBPopup popup = createPopup(e);
-
-      InputEvent event = e.getInputEvent();
-      if (event instanceof MouseEvent) {
-        popup.showUnderneathOf(event.getComponent());
+      val event = e.inputEvent
+      if (event is MouseEvent) {
+        popup.showUnderneathOf(event.component)
       }
       else {
-        popup.showInBestPositionFor(e.getDataContext());
+        popup.showInBestPositionFor(e.dataContext)
       }
     }
 
-    protected abstract @NotNull JBPopup createPopup(@NotNull AnActionEvent e);
+    protected abstract fun createPopup(e: AnActionEvent): JBPopup
   }
 
-  private static class SimpleGoToChangePopupAction extends BaseGoToChangePopupAction {
-    private final DiffRequestChain myChain;
-    private final @NotNull Consumer<? super Integer> myOnSelected;
-    private final int myDefaultSelection;
+  private class SimpleGoToChangePopupAction(
+    private val chain: DiffRequestChain,
+    private val onSelected: Consumer<in Int>,
+    private val defaultSelection: Int,
+  ) : BaseGoToChangePopupAction() {
+    override fun canNavigate(): Boolean = chain.getRequests().size > 1
 
-    SimpleGoToChangePopupAction(@NotNull DiffRequestChain chain, @NotNull Consumer<? super Integer> onSelected, int defaultSelection) {
-      myChain = chain;
-      myOnSelected = onSelected;
-      myDefaultSelection = defaultSelection;
-    }
+    override fun createPopup(e: AnActionEvent): JBPopup = JBPopupFactory.getInstance().createListPopup(MyListPopupStep())
 
-    @Override
-    protected boolean canNavigate() {
-      return myChain.getRequests().size() > 1;
-    }
-
-    @Override
-    protected @NotNull JBPopup createPopup(@NotNull AnActionEvent e) {
-      return JBPopupFactory.getInstance().createListPopup(new MyListPopupStep());
-    }
-
-    private class MyListPopupStep extends BaseListPopupStep<DiffRequestProducer> {
-      MyListPopupStep() {
-        super(DiffBundle.message("action.presentation.go.to.change.text"), myChain.getRequests());
-        setDefaultOptionIndex(myDefaultSelection);
+    private inner class MyListPopupStep :
+      BaseListPopupStep<DiffRequestProducer>(
+        DiffBundle.message("action.presentation.go.to.change.text"),
+        chain.getRequests()
+      ) {
+      init {
+        defaultOptionIndex = defaultSelection
       }
 
-      @Override
-      public @NotNull String getTextFor(DiffRequestProducer value) {
-        return value.getName();
-      }
+      override fun getTextFor(value: DiffRequestProducer): String = value.getName()
 
-      @Override
-      public boolean isSpeedSearchEnabled() {
-        return true;
-      }
+      override fun isSpeedSearchEnabled(): Boolean = true
 
-      @Override
-      public PopupStep<?> onChosen(final DiffRequestProducer selectedValue, boolean finalChoice) {
-        return doFinalStep(() -> {
-          int index = myChain.getRequests().indexOf(selectedValue);
-          myOnSelected.consume(index);
-        });
+      override fun onChosen(selectedValue: DiffRequestProducer?, finalChoice: Boolean): PopupStep<*>? {
+        return doFinalStep(Runnable {
+          val index = chain.getRequests().indexOf(selectedValue)
+          onSelected.consume(index)
+        })
       }
     }
   }
