91bb56bf184e90179fdad31f49e955ec6bb90ed0
Anna Kozlova <anna.kozlova@jetbrains.com>
2026-05-12 16:44:08 +0200

    [kotlin] ignore actions check in android tests
    - Fix with AI appears when authorization is done and is missed otherwise. To avoid flikering, let's disable the check
    
    ^KTIJ-38668 fixed
    
    GitOrigin-RevId: c8410818ddbc4005dd6f54dd68f3ea7474fc4049

--- plugins/kotlin/idea/tests/testData/gradle/fixesWithAndroid/createActualForAndroidWithExistentAndroidMainKotlinPath/before/src/commonMain/kotlin/my/pack/PlatformUtils.kt
+++ plugins/kotlin/idea/tests/testData/gradle/fixesWithAndroid/createActualForAndroidWithExistentAndroidMainKotlinPath/before/src/commonMain/kotlin/my/pack/PlatformUtils.kt
@@ -1,6 +1,5 @@
 // "Add missing actual declarations" "true"
-// ACTION: Convert function to property
-// ACTION: Create test
+// IGNORE_IRRELEVANT_ACTIONS
 // K2_ACTION: "Create actual in 'testProjectName.main'" "true"
 // K2_TOOL: org.jetbrains.kotlin.idea.k2.codeinsight.inspections.KotlinNoActualForExpectInspection
 
