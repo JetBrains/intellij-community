5820e361c3619505eb487b137930c40fcbca218e
Fabio Gaiba <fabio.gaiba@jetbrains.com>
2026-05-13 10:14:05 +0200

    [clion][ui-test]: CPP-47923 fix toolset of CustomMingw toolchain
    GitOrigin-RevId: 1476821d5c0fdc2ae9d3fe39b434eb9e34462692

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/clion/Toolchain.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/clion/Toolchain.kt
@@ -68,7 +68,8 @@ sealed class Toolchain(
     debugger: Debugger = Debugger.MINGW_CUSTOM_GDB,
     buildTool: BuildTool = BuildTool.DEFAULT,
     name: ToolchainNames = ToolchainNames.MINGW_GDB,
-  ) : Toolchain(name, compiler, debugger, buildTool)
+    toolset: Toolset = Toolset(kind = "MINGW", path = "\"C:\\Tools\\msys2\\mingw64\"")
+  ) : Toolchain(name, compiler, debugger, buildTool, toolset)
 
   class MingwCustomGDB(
     compiler: Compiler = Compiler.DEFAULT,
