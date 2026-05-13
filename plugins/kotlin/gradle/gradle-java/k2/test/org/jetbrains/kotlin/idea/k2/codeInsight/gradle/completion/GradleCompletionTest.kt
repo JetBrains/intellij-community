dec441993b4a9558f26d3f93fdebd18be6c31168
Victoria Petrakovich <victoria.petrakovich@jetbrains.com>
2026-05-13 15:26:42 +0200

    [kotlin] KTIJ-38145 Add pluginMode to `GradleCompletionTest`
    GitOrigin-RevId: 0fe049efcdb7df3de2e0c1f6da33e75675a6bdd7

--- plugins/kotlin/gradle/gradle-java/k2/test/org/jetbrains/kotlin/idea/k2/codeInsight/gradle/completion/GradleCompletionTest.kt
+++ plugins/kotlin/gradle/gradle-java/k2/test/org/jetbrains/kotlin/idea/k2/codeInsight/gradle/completion/GradleCompletionTest.kt
@@ -3,11 +3,16 @@ package org.jetbrains.kotlin.idea.k2.codeInsight.gradle.completion
 
 import com.intellij.openapi.actionSystem.IdeActions
 import com.intellij.testFramework.runInEdtAndWait
+import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginMode
 import org.jetbrains.kotlin.idea.codeInsight.gradle.MultiplePluginVersionGradleImportingCodeInsightTestCase
 import org.jetbrains.plugins.gradle.tooling.annotation.PluginTargetVersions
 import org.junit.Test
 
 class GradleCompletionTest : MultiplePluginVersionGradleImportingCodeInsightTestCase() {
+
+    override val pluginMode: KotlinPluginMode
+        get() = KotlinPluginMode.K2
+
     override fun testDataDirName() = "completion"
 
     private fun doCompletionTest(expected: List<String>) {
