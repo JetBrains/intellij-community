39760253ea810808890275ecc0fe7541b0926034
Andrii Zinchenko <andrii.zinchenko@jetbrains.com>
2026-05-14 11:39:46 +0200

    refactor [ijent] drop defaultCoroutineDispatchingContext from ijent core
    Remove the helper from intellij.platform.ijent and inline its two-line body
    (ModalityState.any().asContextElement() + Dispatchers.IO) at the sole caller
    in DockerBinaryPathsProvider. The helper pulled openapi.application into the
    small ijent core for no real benefit beyond a shared expression.
    
    GitOrigin-RevId: d36c5e5111fffb2b03fbc3ed3e91129896f0544c

--- platform/ijent/src/com/intellij/platform/ijent/util.kt
+++ platform/ijent/src/com/intellij/platform/ijent/util.kt
@@ -1,16 +1,10 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.ijent
 
-import com.intellij.openapi.application.ModalityState
-import com.intellij.openapi.application.asContextElement
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.currentCoroutineContext
-import kotlin.coroutines.CoroutineContext
-
-fun defaultCoroutineDispatchingContext(): CoroutineContext =
-  ModalityState.any().asContextElement() + Dispatchers.IO
 
 @OptIn(ExperimentalStdlibApi::class)
 suspend fun currentCoroutineDispatcher(): CoroutineDispatcher {
