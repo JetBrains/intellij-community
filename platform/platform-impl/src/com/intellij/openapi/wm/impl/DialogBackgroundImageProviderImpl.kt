c8d9dfae785780307ff5e6dfd1a3949186a7b063
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2026-05-14 11:44:42 +0200

    IJPL-244687 No background image in update dialog in Remote development
    GitOrigin-RevId: 5bd6ab53594b9888af6c1b3e9ddfbe283bfcb3c3

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/DialogBackgroundImageProviderImpl.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/DialogBackgroundImageProviderImpl.kt
@@ -1,17 +1,10 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.wm.impl
 
-import com.intellij.util.PlatformUtils
 import org.jetbrains.annotations.ApiStatus.Internal
 import java.net.URL
 
 @Internal
 class DialogBackgroundImageProviderImpl : DialogBackgroundImageProviderBase() {
-  override fun getImageUrl(isDark: Boolean, isIslands: Boolean): URL? =
-    if (PlatformUtils.isIntelliJ()) javaClass.getResource(if (isDark) {
-      if (isIslands) "/images/gradientBackground-islands-dark.svg" else "/images/gradientBackground-dark.svg"
-    } else {
-      "/images/gradientBackground-light.svg"
-    })
-    else null
+  override fun getImageUrl(isDark: Boolean, isIslands: Boolean): URL? = null
 }
