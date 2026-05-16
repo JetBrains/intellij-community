624e92accd63f403581fa66789864019f8e60dd1
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2026-05-16 12:31:51 +0200

    IJPL-241535 fix smoke tests
    Signed-off-by: Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
    
    GitOrigin-RevId: 327b7f183e78c4b41ad32ee3dfbc9c4de6a26251

--- platform/platform-impl/src/com/intellij/platform/ide/impl/common/file/icons/CommonFileIconsProvider.kt
+++ platform/platform-impl/src/com/intellij/platform/ide/impl/common/file/icons/CommonFileIconsProvider.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.common.file.icons
+package com.intellij.platform.ide.impl.common.file.icons
 
 import com.intellij.ide.FileIconProvider
 import com.intellij.openapi.fileTypes.PlainTextLikeFileType
