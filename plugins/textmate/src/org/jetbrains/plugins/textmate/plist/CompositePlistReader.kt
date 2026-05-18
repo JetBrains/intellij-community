9db4727f131caa3072687f525eef8e8c55cc517a
Alexander Zolotov <zolotov@jetbrains.com>
2026-05-18 12:22:55 +0200

    cleanup [textmate]: fix references to the new plist reader
    GitOrigin-RevId: ef45c7e61d2bfbbf23fa66de3cfeddca6911f460

--- plugins/textmate/src/org/jetbrains/plugins/textmate/plist/CompositePlistReader.kt
+++ plugins/textmate/src/org/jetbrains/plugins/textmate/plist/CompositePlistReader.kt
@@ -2,10 +2,10 @@ package org.jetbrains.plugins.textmate.plist
 
 import java.io.InputStream
 
-@Deprecated("Use JsonOrXmlPlistReader instead")
+@Deprecated("Use JsonOrXmlOrYamlPlistReader instead")
 class CompositePlistReader : PlistReader {
-  @Deprecated("Use JsonOrXmlPlistReader instead",
-              ReplaceWith("org.jetbrains.plugins.textmate.plist.JsonOrXmlPlistReader(jsonReader = org.jetbrains.plugins.textmate.plist.JsonPlistReader(), xmlReader = org.jetbrains.plugins.textmate.plist.XmlPlistReader(), yamlReader = org.jetbrains.plugins.textmate.plist.YamlPlistReader())"))
+  @Deprecated("Use JsonOrXmlOrYamlPlistReader instead",
+              ReplaceWith("org.jetbrains.plugins.textmate.plist.JsonOrXmlOrYamlPlistReader(jsonReader = org.jetbrains.plugins.textmate.plist.JsonPlistReader(), xmlReader = org.jetbrains.plugins.textmate.plist.XmlPlistReader(), yamlReader = org.jetbrains.plugins.textmate.plist.YamlPlistReader())"))
   constructor()
   private val delegate = JsonOrXmlOrYamlPlistReader(xmlReader = XmlPlistReader(), yamlReader = YamlPlistReader())
 
