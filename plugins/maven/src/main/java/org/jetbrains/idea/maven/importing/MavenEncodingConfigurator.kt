c823d0863022c463d4f003045d080aa6c88cd23e
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2026-05-18 14:37:24 +0200

    [IDEA-386681] workaround
    GitOrigin-RevId: 7498ab3b81ea3c292f3cf44e1d59a5b3162b9d77

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/MavenEncodingConfigurator.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/MavenEncodingConfigurator.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.maven.importing
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
@@ -30,7 +31,14 @@ internal class MavenEncodingConfigurator : MavenWorkspaceConfigurator {
   }
 
   override fun afterModelApplied(context: MavenWorkspaceConfigurator.AppliedModelContext) {
-    PREPARED_MAPPER.get(context)?.applyCollectedInfo()
+    val mapper = PREPARED_MAPPER.get(context) ?: return
+    // EncodingProjectManagerImpl.setPointerMapping runs a synchronous modal progress that
+    // walks the VFS; nesting it in the importer's EDT write action freezes EDT. Defer to
+    // after the WA returns.
+    ApplicationManager.getApplication().invokeLater(
+      { mapper.applyCollectedInfo() },
+      context.project.disposed
+    )
   }
 
   private fun mapEncodings(mavenProjects: Sequence<MavenProject>, project: Project): EncodingMapper {
