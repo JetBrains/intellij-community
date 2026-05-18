79b59d6e863a16021453934d3bc9d705df4f1978
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2026-05-18 13:17:58 +0200

    misc [platform]: skipping shell env loading in the dev mode
    ... (generally not needed; avoiding "missing helper" exceptions on macOS) + cleanup (formatting)
    
    GitOrigin-RevId: 88d2f844fc819e8f40185fe067c101a1ee63055b

--- platform/platform-impl/bootstrap/src/com/intellij/platform/ide/bootstrap/startup.kt
+++ platform/platform-impl/bootstrap/src/com/intellij/platform/ide/bootstrap/startup.kt
@@ -153,9 +153,9 @@ fun startApplication(
     }
   }
 
-  val initAwtToolkitJob = scheduleInitAwtToolkit(scope = scope, lockSystemDirsJob = lockSystemDirsJob, busyThread = busyThread)
+  val initAwtToolkitJob = scheduleInitAwtToolkit(scope, lockSystemDirsJob, busyThread)
   val initBaseLafJob = scope.launch {
-    initUi(initAwtToolkitJob = initAwtToolkitJob, isHeadless = isHeadless, asyncScope = scope)
+    initUi(initAwtToolkitJob, isHeadless, scope)
   }
 
   var initUiScale: Job? = null
@@ -173,9 +173,9 @@ fun startApplication(
       }
     }
 
-    scheduleUpdateFrameClassAndWindowIconAndPreloadSystemFonts(scope = scope, initAwtToolkitJob = initAwtToolkitJob, initUiScale = initUiScale, appInfoDeferred = appInfoDeferred)
+    scheduleUpdateFrameClassAndWindowIconAndPreloadSystemFonts(scope, initAwtToolkitJob, initUiScale, appInfoDeferred)
 
-    scheduleShowSplashIfNeeded(scope = scope, lockSystemDirsJob = lockSystemDirsJob, initUiScale = initUiScale, appInfoDeferred = appInfoDeferred, args = args)
+    scheduleShowSplashIfNeeded(scope, lockSystemDirsJob, initUiScale, appInfoDeferred, args)
   }
 
   val initLafJob = scope.launch {
@@ -231,29 +231,14 @@ fun startApplication(
     }
   }
 
-  scheduleLoadSystemLibsAndLogInfoAndInitMacApp(
-    scope = scope,
-    logDeferred = logDeferred,
-    appInfoDeferred = appInfoDeferred,
-    initUiDeferred = initLafJob,
-    args = args,
-    mainScope = mainScope,
-  )
+  scheduleLoadSystemLibsAndLogInfoAndInitMacApp(scope, logDeferred, appInfoDeferred, initLafJob, args, mainScope)
 
   val euaDocumentDeferred = scope.async { loadEuaDocument(appInfoDeferred) }
 
   val configImportDeferred = scope.async {
     importConfigIfNeeded(
-      scope = scope,
-      isHeadless = isHeadless,
-      configImportNeededDeferred = configImportNeededDeferred,
-      lockSystemDirsJob = lockSystemDirsJob,
-      logDeferred = logDeferred,
-      args = args,
-      customTargetDirectoryToImportConfig = customTargetDirectoryToImportConfig,
-      appStarterDeferred = appStarterDeferred,
-      euaDocumentDeferred = euaDocumentDeferred,
-      initLafJob = initLafJob,
+      scope, isHeadless, configImportNeededDeferred, lockSystemDirsJob, logDeferred, args, customTargetDirectoryToImportConfig,
+      appStarterDeferred, euaDocumentDeferred, initLafJob
     )
   }
 
@@ -290,12 +275,7 @@ fun startApplication(
       }
     }
 
-    PluginManagerCore.scheduleDescriptorLoading(
-      coroutineScope = this,
-      zipPoolDeferred = zipPoolDeferred,
-      mainClassLoaderDeferred = mainClassLoaderDeferred,
-      logDeferred = logDeferred,
-    )
+    PluginManagerCore.scheduleDescriptorLoading(coroutineScope = this, zipPoolDeferred, mainClassLoaderDeferred, logDeferred)
   }
 
   val isInternal = System.getProperty(ApplicationManagerEx.IS_INTERNAL_PROPERTY).toBoolean()
@@ -341,18 +321,7 @@ fun startApplication(
     }
 
     val args = ApplicationStartArguments.stripKnownArguments(args)
-    loadApp(
-      app = app,
-      pluginSetDeferred = pluginSetDeferred,
-      appInfoDeferred = appInfoDeferred,
-      euaDocumentDeferred = euaDocumentDeferred,
-      asyncScope = scope,
-      initLafJob = initLafJob,
-      logDeferred = logDeferred,
-      appRegisteredJob = appRegisteredJob,
-      args = args,
-      initAwtToolkitAndEventQueueJob = initEventQueueJob,
-    )
+    loadApp(app, pluginSetDeferred, appInfoDeferred, euaDocumentDeferred, scope, initLafJob, logDeferred, appRegisteredJob, args, initEventQueueJob)
   }
 
   scope.launch {
@@ -697,8 +666,13 @@ private fun shouldLoadShellEnv(log: Logger): Boolean {
   }
 
   val shLvl = System.getenv("SHLVL")
-  if (shLvl != null && @Suppress("RemoveUnnecessaryParentheses") (shLvl.toIntOrNull() ?: 1) > 0) {
+  if (shLvl != null && (shLvl.toIntOrNull() ?: 1) > 0) {
     log.info("skipping shell environment: the IDE is likely launched from a terminal (SHLVL=${shLvl})")
+    return false
+  }
+
+  if (AppMode.isRunningFromDevBuild()) {
+    log.info("skipping shell environment: dev mode")
     return false
   }
 
