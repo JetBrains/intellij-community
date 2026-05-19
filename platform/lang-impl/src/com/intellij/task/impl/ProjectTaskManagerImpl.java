0b42494aadb09b1381f0dbfe7748e4afef0f3163
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2026-05-19 10:30:20 +0200

    IJPL-245237 [platform]: deprecating and stubbing obsolete `ProjectTaskRunner#canRun` overloads
    ... + cleanup (typos; formatting)
    
    GitOrigin-RevId: f55f1f214655f5b4ca985781edb3de536b76b737

--- platform/lang-impl/src/com/intellij/task/impl/ProjectTaskManagerImpl.java
+++ platform/lang-impl/src/com/intellij/task/impl/ProjectTaskManagerImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.task.impl;
 
 import com.intellij.execution.ExecutionException;
@@ -52,7 +52,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -433,7 +432,7 @@ public final class ProjectTaskManagerImpl extends ProjectTaskManager {
     }
 
     @Override
-    public boolean canRun(@NotNull ProjectTask projectTask) {
+    public boolean canRun(@NotNull Project project, @NotNull ProjectTask projectTask, @Nullable ProjectTaskContext context) {
       return true;
     }
   }
