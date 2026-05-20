27674c020f34cde7cab13ef0b68ea96903b2a3c4
Ivan Migalev <ivan.migalev@jetbrains.com>
2026-05-20 19:29:39 +0200

    RIDER-127935 warmup: dump backend threads
    GitOrigin-RevId: c92ecfe15f23e3c0a0fa48a6e50c6d8878ead85e

--- platform/warmup/src/com/intellij/warmup/util/utils-time-logger.kt
+++ platform/warmup/src/com/intellij/warmup/util/utils-time-logger.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.util.text.Formats
 import com.intellij.util.application
+import com.intellij.util.text.nullize
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -86,6 +87,10 @@ suspend fun <Y> runTaskAndLogTime(
         val threadDump = ThreadDumper.dumpForDebug()
         ConsoleLog.info("Printing a threadDump for diagnostic in case of process' hanging")
         ConsoleLog.info("... $prefix\n$threadDump")
+
+        AdditionalThreadDumpSupport.dumpThreads().nullize(nullizeSpaces = true)?.let {
+          ConsoleLog.info("Additional thread dump information:\n$it")
+        }
 
         try {
           val logFile = File(PathManager.getLogPath(), "too-long-wait-thread-dump-${System.currentTimeMillis()}.txt")
