b93582bccecef92ab2bd9c79e8b9371ca41b30fe
Victoria Petrakovich <victoria.petrakovich@jetbrains.com>
2026-05-20 17:12:27 +0200

    [kotlin] KTIJ-38145 Move `KotlinGradlePluginVersionImportTest` to K2
    GitOrigin-RevId: 12e8370c7f28f95c170ad3a049f6beac161f555e

--- plugins/kotlin/gradle/gradle-java/k2/test/org/jetbrains/kotlin/idea/k2/codeInsight/gradle/KotlinGradlePluginVersionImportTest.kt
+++ plugins/kotlin/gradle/gradle-java/k2/test/org/jetbrains/kotlin/idea/k2/codeInsight/gradle/KotlinGradlePluginVersionImportTest.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.gradle
+package org.jetbrains.kotlin.idea.k2.codeInsight.gradle
 
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.vfs.writeText
