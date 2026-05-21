f6dc03c9c710562cb153586f9578c92e61dccf28
Bas Leijdekkers <bas.leijdekkers@jetbrains.com>
2026-05-21 14:10:52 +0200

    IJPL-245459 [platform] recategorize intention, improve its text
    GitOrigin-RevId: 6640201c2964345aa2e3e5fc21f60bbec013cd55

--- platform/lang-impl/src/com/intellij/psi/templateLanguages/ChooseTemplateDataLanguageIntention.kt
+++ platform/lang-impl/src/com/intellij/psi/templateLanguages/ChooseTemplateDataLanguageIntention.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.psi.templateLanguages
 
 import com.intellij.codeInsight.intention.IntentionAction
@@ -21,7 +21,7 @@ internal class ChooseTemplateDataLanguageIntention : IntentionAction {
   }
 
   override fun getText(): String {
-    return LangBundle.message("template.data.language.chooser.intention.title")
+    return familyName
   }
 
   override fun isAvailable(project: Project, editor: Editor?, psiFile: PsiFile?): Boolean {
