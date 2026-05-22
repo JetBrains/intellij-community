e0db81f49388925e3d39d3b493e3a0f7ee67ac66
Sergei Sysoev <Sergey.Sysoev@jetbrains.com>
2026-05-22 13:29:58 +0200

    AIR-6481 add ios as a separate OS
    GitOrigin-RevId: d9f86dbf088f938bc76af15ea3df8672f55349e8

--- fleet/util/core/srcIosMain/fleet/util/Os.ios.kt
+++ fleet/util/core/srcIosMain/fleet/util/Os.ios.kt
@@ -2,13 +2,18 @@
 package fleet.util
 
 import fleet.util.multiplatform.Actual
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSProcessInfo
+import kotlin.experimental.ExperimentalNativeApi
 
+@OptIn(ExperimentalNativeApi::class)
 @Actual
-internal fun getNameNative(): String = "ios"
+internal fun getNameNative(): String = Platform.osFamily.name.lowercase()
 
+@OptIn(ExperimentalForeignApi::class)
 @Actual
 internal fun getVersionNative(): String = NSProcessInfo.processInfo.operatingSystemVersionString
 
+@OptIn(ExperimentalNativeApi::class)
 @Actual
-internal fun getArchNative(): String = "aarch64"
+internal fun getArchNative(): String = Platform.cpuArchitecture.name.lowercase()
