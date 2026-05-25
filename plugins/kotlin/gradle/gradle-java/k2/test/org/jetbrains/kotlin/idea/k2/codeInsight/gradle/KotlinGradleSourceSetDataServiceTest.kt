745bf636af2b6e4dd6c22fe451ff65f926771d5c
Victoria Petrakovich <victoria.petrakovich@jetbrains.com>
2026-05-25 19:47:18 +0200

    [kotlin] KTIJ-38145 Move `KotlinGradleSourceSetDataServiceTest` to K2
    GitOrigin-RevId: 2bb1ff11d5b9bf99dc0742f0ab48dc0f0ef21e8f

--- plugins/kotlin/gradle/gradle-java/k2/test/org/jetbrains/kotlin/idea/k2/codeInsight/gradle/KotlinGradleSourceSetDataServiceTest.kt
+++ plugins/kotlin/gradle/gradle-java/k2/test/org/jetbrains/kotlin/idea/k2/codeInsight/gradle/KotlinGradleSourceSetDataServiceTest.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.gradle
+package org.jetbrains.kotlin.idea.k2.codeInsight.gradle
 
 import com.intellij.openapi.application.runWriteActionAndWait
 import com.intellij.openapi.externalSystem.model.ProjectKeys
