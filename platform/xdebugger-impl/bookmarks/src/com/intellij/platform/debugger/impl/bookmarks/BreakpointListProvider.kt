ef17f89f1ad6eeec858d7bce210efb55928c8466
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2026-05-25 09:38:16 +0200

    IJPL-245430 fix pluginized module dependencies
    Move bookmarks-related platform modules behind an explicit bundled plugin wrapper and split debugger and JetBrains Client bookmark pieces into content modules. This lets products include bookmarks through normal plugin wiring instead of keeping the modules embedded in broad module sets.
    
    Update generated product layouts, module descriptors, and packaging snapshots so the new wrapper is present where products and remote development clients need it.
    
    IJ-MR-184958 IJ-MR-184993 IJ-MR-179029 IJ-MR-184126 IJ-MR-181153 IJ-MR-146078 IJ-MR-175479 IJ-MR-186058 IJ-MR-193195 IJ-MR-196957 IJ-MR-199124 IJ-MR-197441 IJ-MR-204135 IJ-MR-204674
    
    GitOrigin-RevId: e51549c55232531961957702572ce184d03ec277

--- platform/xdebugger-impl/bookmarks/src/com/intellij/platform/debugger/impl/bookmarks/BreakpointListProvider.kt
+++ platform/xdebugger-impl/bookmarks/src/com/intellij/platform/debugger/impl/bookmarks/BreakpointListProvider.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.xdebugger.impl.breakpoints
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.debugger.impl.bookmarks
 
 import com.intellij.icons.AllIcons
 import com.intellij.ide.bookmark.BookmarksListProvider
