2a8b18bd8a8532089cd3088c0df1b8b5e83592ec
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-05-25 12:25:01 +0300

    [terminal] IJPL-244832 Refactor: translate LocalTerminalTtyConnector to Kotlin
    GitOrigin-RevId: 6e5778e4e658e3d5ce9b9dc281e39d4f359f9df0

--- plugins/terminal/src/org/jetbrains/plugins/terminal/LocalTerminalTtyConnector.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/LocalTerminalTtyConnector.kt
@@ -1,100 +1,90 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.plugins.terminal;
+package org.jetbrains.plugins.terminal
 
-import com.google.common.base.Ascii;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.platform.eel.EelDescriptor;
-import com.intellij.platform.ijent.IjentChildPtyProcessAdapter;
-import com.intellij.terminal.pty.PtyProcessTtyConnector;
-import com.intellij.util.concurrency.AppExecutorUtil;
-import com.jediterm.core.util.TermSize;
-import com.pty4j.PtyProcess;
-import com.pty4j.unix.UnixPtyProcess;
-import com.pty4j.windows.conpty.WinConPtyProcess;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.terminal.util.ShellEelProcess;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.charset.Charset;
-import java.util.concurrent.TimeUnit;
+import com.google.common.base.Ascii
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.debug
+import com.intellij.platform.eel.EelDescriptor
+import com.intellij.platform.ijent.IjentChildPtyProcessAdapter
+import com.intellij.terminal.pty.PtyProcessTtyConnector
+import com.intellij.util.concurrency.AppExecutorUtil
+import com.jediterm.core.util.TermSize
+import com.pty4j.PtyProcess
+import com.pty4j.unix.UnixPtyProcess
+import com.pty4j.windows.conpty.WinConPtyProcess
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.plugins.terminal.util.ShellEelProcess
+import java.io.IOException
+import java.nio.charset.Charset
+import java.util.concurrent.TimeUnit
 
 @ApiStatus.Internal
-public class LocalTerminalTtyConnector extends PtyProcessTtyConnector {
-  private static final Logger LOG = Logger.getInstance(LocalTerminalTtyConnector.class);
-  private final @NotNull PtyProcess myProcess;
-  private final @NotNull ShellProcessHolder myShellProcessHolder;
+class LocalTerminalTtyConnector internal constructor(
+  private val shellProcessHolder: ShellProcessHolder,
+  charset: Charset,
+) : PtyProcessTtyConnector(shellProcessHolder.ptyProcess, charset) {
+  private val ptyProcess: PtyProcess = shellProcessHolder.ptyProcess
 
-  public LocalTerminalTtyConnector(@NotNull ShellProcessHolder shellProcessHolder, @NotNull Charset charset) {
-    super(shellProcessHolder.getPtyProcess(), charset);
-    myProcess = shellProcessHolder.getPtyProcess();
-    myShellProcessHolder = shellProcessHolder;
-  }
+  val eelDescriptor: EelDescriptor
+    get() = shellProcessHolder.descriptor
 
-  @ApiStatus.Internal
-  public @NotNull EelDescriptor getEelDescriptor() {
-    return myShellProcessHolder.getDescriptor();
-  }
+  val shellEelProcess: ShellEelProcess
+    get() = ShellEelProcess(
+      eelProcess = shellProcessHolder.eelProcess,
+      eelApi = shellProcessHolder.eelApi,
+      process = ptyProcess
+    )
 
-  @ApiStatus.Internal
-  public @NotNull ShellEelProcess getShellEelProcess() {
-    return new ShellEelProcess(
-      myShellProcessHolder.getEelProcess(),
-      myShellProcessHolder.getEelApi(),
-      myProcess
-    );
-  }
-
-  @Override
-  public void close() {
-    if (myProcess instanceof UnixPtyProcess) {
-      ((UnixPtyProcess)myProcess).hangup();
-      AppExecutorUtil.getAppScheduledExecutorService().schedule(() -> {
-        if (myProcess.isAlive()) {
-          LOG.info("Terminal hasn't been terminated by SIGHUP, performing default termination");
-          myProcess.destroy();
+  override fun close() {
+    if (ptyProcess is UnixPtyProcess) {
+      ptyProcess.hangup()
+      AppExecutorUtil.getAppScheduledExecutorService().schedule(Runnable {
+        if (ptyProcess.isAlive) {
+          LOG.info("Terminal hasn't been terminated by SIGHUP, performing default termination")
+          ptyProcess.destroy()
         }
-      }, 1000, TimeUnit.MILLISECONDS);
+      }, 1000, TimeUnit.MILLISECONDS)
     }
-    else if (myProcess instanceof IjentChildPtyProcessAdapter && myShellProcessHolder.isPosix()) {
-      myShellProcessHolder.terminatePosixShellBlocking();
+    else if (ptyProcess is IjentChildPtyProcessAdapter && shellProcessHolder.isPosix) {
+      shellProcessHolder.terminatePosixShellBlocking()
     }
     else {
-      if (myProcess instanceof WinConPtyProcess winConPtyProcess && !winConPtyProcess.isBundledConPtyLibrary()) {
-        sendInterruptToWinConPtyProcess();
+      if (ptyProcess is WinConPtyProcess && !ptyProcess.isBundledConPtyLibrary) {
+        sendInterruptToWinConPtyProcess()
       }
-      myProcess.destroy();
+      ptyProcess.destroy()
     }
   }
 
   /**
-   * Workaround for the ConPTY issue <a href="https://github.com/microsoft/terminal/issues/15373">#15373</a>
+   * Workaround for the ConPTY issue [#15373](https://github.com/microsoft/terminal/issues/15373)
    * which was fixed in Windows Terminal 1.22.
    * Even though this version is bundled with IDE, ConPTY can be loaded from the OS.
-   * @see <a href="https://github.com/microsoft/terminal/discussions/19030">workaround discussion</a>
+   *
+   * See also [workaround discussion](https://github.com/microsoft/terminal/discussions/19030).
    */
-  private void sendInterruptToWinConPtyProcess() {
-    OutputStream outputStream = myProcess.getOutputStream();
-    if (outputStream != null && myProcess.isAlive()) {
+  private fun sendInterruptToWinConPtyProcess() {
+    val outputStream = ptyProcess.outputStream
+    if (outputStream != null && ptyProcess.isAlive) {
       try {
         // ConPTY will process `0x03` and raise `CTRL_C_EVENT`.
         // This will hopefully terminate the whole process hierarchy of running user command,
         // which is a must for commands like `npm run dev`.
-        outputStream.write(Ascii.ETX);
-        outputStream.flush();
+        outputStream.write(Ascii.ETX.toInt())
+        outputStream.flush()
       }
-      catch (IOException e) {
-        LOG.info("Failed to send Ctrl+C to " + myProcess.getClass().getSimpleName() + ", alive:" + myProcess.isAlive(), e);
+      catch (e: IOException) {
+        LOG.info("Failed to send Ctrl+C to ${ptyProcess.javaClass.getSimpleName()}, alive:${ptyProcess.isAlive}", e)
       }
     }
   }
 
-  @Override
-  public void resize(@NotNull TermSize termSize) {
-    if (LOG.isDebugEnabled()) {
-      LOG.debug("resize to " + termSize);
-    }
-    super.resize(termSize);
+  override fun resize(termSize: TermSize) {
+    LOG.debug { "resize to $termSize" }
+    super.resize(termSize)
+  }
+
+  companion object {
+    private val LOG = Logger.getInstance(LocalTerminalTtyConnector::class.java)
   }
 }
