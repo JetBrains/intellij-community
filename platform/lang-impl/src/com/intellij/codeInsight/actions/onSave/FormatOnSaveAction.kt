a76961081ab88ab6895aa66915ecd8579c4c7dfb
Vojtech Balik <vojtech.balik@jetbrains.com>
2026-05-26 20:21:01 +0200

    IJPL-29913 [actions-on-save]: modernize FormatOnSaveAction
    Converted FormatOnSaveAction from modal progress API to the modern suspending API.
    
    Now there is a separate background RA followed by WA sequence for each code processor for each modified document, instead of a single long modal progress blocking user interaction.
    
    GitOrigin-RevId: 00599bf6230a67b2daf25db8708caa8fb6de1c58

--- platform/lang-impl/src/com/intellij/codeInsight/actions/onSave/FormatOnSaveAction.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/actions/onSave/FormatOnSaveAction.kt
@@ -7,17 +7,17 @@ import com.intellij.codeInsight.actions.OptimizeImportsProcessor
 import com.intellij.codeInsight.actions.RearrangeCodeProcessor
 import com.intellij.codeInsight.actions.ReformatCodeProcessor
 import com.intellij.codeInsight.actions.VcsFacade
-import com.intellij.ide.actionsOnSave.impl.ActionsOnSaveFileDocumentManagerListener.ActionOnSave
+import com.intellij.ide.actionsOnSave.impl.ActionsOnSaveFileDocumentManagerListener
 import com.intellij.lang.LanguageFormatting
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.editor.Document
-import com.intellij.openapi.project.DumbService.Companion.isDumb
+import com.intellij.openapi.progress.coroutineToIndicator
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDocumentManager
-import com.intellij.psi.PsiFile
-import com.intellij.util.SlowOperations
-import java.util.concurrent.FutureTask
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-internal class FormatOnSaveAction : ActionOnSave() {
+internal class FormatOnSaveAction : ActionsOnSaveFileDocumentManagerListener.DocumentUpdatingActionOnSave() {
   override fun isEnabledForProject(project: Project): Boolean {
     return FormatOnSaveOptions.getInstance(project).isRunOnSaveEnabled ||
            OptimizeImportsOnSaveOptions.getInstance(project).isRunOnSaveEnabled ||
@@ -25,79 +25,66 @@ internal class FormatOnSaveAction : ActionOnSave() {
            CodeCleanupOnSaveActionInfo.isCodeCleanupOnSaveEnabled(project)
   }
 
-  override fun processDocuments(project: Project, documents: Array<Document>) {
-    val allFiles = mutableListOf<PsiFile>()
-    val filesToFormat = mutableListOf<PsiFile>()
-    val filesToOptimizeImports = mutableListOf<PsiFile>()
+  override val presentableName: String
+    get() = ReformatCodeProcessor.getCommandName()
 
-    val manager = PsiDocumentManager.getInstance(project)
-    val formatOptions = FormatOnSaveOptions.getInstance(project)
-    val optimizeImportsOptions = OptimizeImportsOnSaveOptions.getInstance(project)
+  override suspend fun updateDocument(project: Project, document: Document) {
+    val psiFile = readAction {
+      PsiDocumentManager.getInstance(project)
+        .getPsiFile(document)
+        ?.takeIf { LanguageFormatting.INSTANCE.isAutoFormatAllowed(it) }
+    } ?: return
 
-    for (document in documents) {
-      val psiFile = SlowOperations.knownIssue("IJPL-162973").use {
-        manager.getPsiFile(document)?.takeIf {
-          LanguageFormatting.INSTANCE.isAutoFormatAllowed(it)
-        }
-      }
-      if (psiFile == null) continue
-
-      allFiles.add(psiFile)
-
-      if (formatOptions.isRunOnSaveEnabled && formatOptions.isFileTypeSelected(psiFile.fileType)) {
-        filesToFormat.add(psiFile)
-      }
-
-      if (optimizeImportsOptions.isRunOnSaveEnabled && optimizeImportsOptions.isFileTypeSelected(psiFile.fileType)) {
-        filesToOptimizeImports.add(psiFile)
-      }
+    val isFormat = run {
+      val formatOptions = FormatOnSaveOptions.getInstance(project)
+      formatOptions.isRunOnSaveEnabled && formatOptions.isFileTypeSelected(psiFile.fileType)
     }
-
-    if (filesToFormat.isEmpty() &&
-        filesToOptimizeImports.isEmpty() &&
-        !RearrangeCodeOnSaveActionInfo.isRearrangeCodeOnSaveEnabled(project) &&
-        !CodeCleanupOnSaveActionInfo.isCodeCleanupOnSaveEnabled(project)
-    ) {
-      // nothing to do
-      return
+    val isOptimizeImports = run {
+      val optimizeImportsOptions = OptimizeImportsOnSaveOptions.getInstance(project)
+      optimizeImportsOptions.isRunOnSaveEnabled && optimizeImportsOptions.isFileTypeSelected(psiFile.fileType)
     }
-
-    processFiles(project, allFiles, filesToFormat, filesToOptimizeImports)
-  }
-
-  private fun processFiles(project: Project,
-                           allFiles: List<PsiFile>,
-                           filesToFormat: List<PsiFile>,
-                           filesToOptimizeImports: List<PsiFile>) {
-    val onlyChangedLines = VcsFacade.getInstance().hasActiveVcss(project) &&
+    val changedLinesOnly = VcsFacade.getInstance().hasActiveVcss(project) &&
                            FormatOnSaveOptions.getInstance(project).isFormatOnlyChangedLines
 
-    var processor: AbstractLayoutCodeProcessor =
-      object : ReformatCodeProcessor(project, allFiles.toTypedArray(), null, onlyChangedLines) {
-        override fun prepareTask(psiFile: PsiFile, processChangedTextOnly: Boolean): FutureTask<Boolean> {
-          return if (filesToFormat.contains(psiFile)) super.prepareTask(psiFile, processChangedTextOnly) else emptyTask()
-        }
+    var processor: AbstractLayoutCodeProcessor? = null
+    val arrayOfPsiFile = arrayOf(psiFile)
+    if (isFormat) {
+      processor = ReformatCodeProcessor(project, arrayOfPsiFile, null, changedLinesOnly)
+    }
+    if (isOptimizeImports) {
+      processor = if (processor != null) {
+        OptimizeImportsProcessor(processor)
       }
-
-    if (OptimizeImportsOnSaveOptions.getInstance(project).isRunOnSaveEnabled) {
-      processor = object : OptimizeImportsProcessor(processor) {
-        override fun prepareTask(psiFile: PsiFile, processChangedTextOnly: Boolean): FutureTask<Boolean> {
-          return if (filesToOptimizeImports.contains(psiFile)) super.prepareTask(psiFile, processChangedTextOnly) else emptyTask()
-        }
+      else {
+        OptimizeImportsProcessor(project, arrayOf(psiFile), null)
       }
     }
-
     if (RearrangeCodeOnSaveActionInfo.isRearrangeCodeOnSaveEnabled(project)) {
-      processor = RearrangeCodeProcessor(processor)
+      processor = if (processor != null) {
+        RearrangeCodeProcessor(processor)
+      }
+      else {
+        RearrangeCodeProcessor(project, arrayOfPsiFile, null, changedLinesOnly)
+      }
     }
-
-    if (CodeCleanupOnSaveActionInfo.isCodeCleanupOnSaveEnabled(project) && !isDumb(project)) {
-      processor = CodeCleanupCodeProcessor(processor)
+    if (CodeCleanupOnSaveActionInfo.isCodeCleanupOnSaveEnabled(project)) {
+      processor = if (processor != null) {
+        CodeCleanupCodeProcessor(processor)
+      }
+      else {
+        CodeCleanupCodeProcessor(project, arrayOfPsiFile, null, changedLinesOnly)
+      }
       processor.setProfile(CodeCleanupOnSaveOptions.getInstance(project).getInspectionProfile())
     }
-
-    // This guarantees that per-file undo chain won't break and there won't be the "Following files affected by this action have been already changed" modal error dialog.
+    if (processor == null) {
+      return
+    }
     processor.setProcessAllFilesAsSingleUndoStep(false)
-    processor.run()
+
+    withContext(Dispatchers.Default) {
+      coroutineToIndicator {
+        processor.processFilesUnderProgress(it)
+      }
+    }
   }
 }
