c45e93b7c22b919274e934aeeb7e244ca9516a89
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2026-05-28 23:11:27 +0200

    PY-89964: Do not execute anything in untrusted projects.
    There are 0 or more opened projects in the Platform. Each of which might be trusrted or untrusted. For untrusted project, its basePath (whatever it means) and roots of all its modules are called "prohibited".
    We shouldn't start anything if it sits in on the aforementioned "prohibited" paths.
    
    GitOrigin-RevId: 9a7c2da916bd95eaf07a72a6cfa83593cf1ec205

--- python/python-exec-service/src/com/intellij/python/community/execService/impl/processLaunchers/eel.kt
+++ python/python-exec-service/src/com/intellij/python/community/execService/impl/processLaunchers/eel.kt
@@ -1,23 +1,36 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.python.community.execService.impl.processLaunchers
 
+import com.intellij.ide.trustedProjects.TrustedProjects
+import com.intellij.openapi.diagnostic.fileLogger
+import com.intellij.openapi.diagnostic.trace
+import com.intellij.openapi.project.ProjectManager
 import com.intellij.platform.eel.EelExecApi
 import com.intellij.platform.eel.EelProcess
 import com.intellij.platform.eel.ExecuteProcessException
 import com.intellij.platform.eel.impl.base.ProcessFunctions
 import com.intellij.platform.eel.path.EelPath
 import com.intellij.platform.eel.provider.asEelPath
+import com.intellij.platform.eel.provider.asNioPath
 import com.intellij.platform.eel.provider.getEelDescriptor
 import com.intellij.platform.eel.provider.toEelApi
 import com.intellij.platform.eel.provider.utils.EelPathUtils
 import com.intellij.platform.eel.spawnProcess
+import com.intellij.project.stateStore
 import com.intellij.python.community.execService.BinOnEel
 import com.intellij.python.community.execService.TtySize
+import com.intellij.python.community.execService.impl.PyExecBundle
 import com.jetbrains.python.Result
 import com.jetbrains.python.errorProcessing.Exe
 import com.jetbrains.python.errorProcessing.ExecErrorReason
+import com.jetbrains.python.sdk.getModuleRoots
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.nio.file.Path
 import kotlin.io.path.pathString
+
+private val log = fileLogger()
 
 internal suspend fun createProcessLauncherOnEel(binOnEel: BinOnEel, launchRequest: LaunchRequest): ProcessLauncher {
   val exePath: EelPath = with(binOnEel) {
@@ -61,8 +74,23 @@ private class EelProcessCommands(
 
   override suspend fun start(): Result<Process, ExecErrorReason.CantStart> {
     var workDir = binOnEel.workDir
-    workDir = if (workDir != null && !workDir.isAbsolute) workDir.toRealPath() else workDir
+    workDir = withContext(Dispatchers.IO) { if (workDir != null && !workDir.isAbsolute) workDir.toRealPath() else workDir }
+
+    // If project is untrusted we should not execute anything there
+    val nioPathToExec = withContext(Dispatchers.IO) {
+      path.asNioPath().toAbsolutePath()
+    }
+    val pathIsProhibited = getProhibitedPaths().any { prohibitedParent ->
+      nioPathToExec.startsWith(prohibitedParent) ||
+      (workDir != null && workDir.startsWith(prohibitedParent))
+    }
+    if (pathIsProhibited) {
+      log.trace { "Prohibited exec $nioPathToExec" }
+      return Result.failure(ExecErrorReason.CantStart(null, PyExecBundle.message("py.exec.error.not.trusted", nioPathToExec)))
+    }
+
     try {
+      log.trace { "Spawning $nioPathToExec" }
       val eelProcess = path.descriptor.toEelApi().exec.spawnProcess(path)
         .scope(scopeToBind)
         .args(args)
@@ -77,4 +105,14 @@ private class EelProcessCommands(
       return Result.failure(ExecErrorReason.CantStart(e.errno, e.message))
     }
   }
+}
+
+/**
+ * List of roots of all untrusted projects
+ */
+private suspend fun getProhibitedPaths(): List<Path> = withContext(Dispatchers.Default) {
+  val untrustedProjects = ProjectManager.getInstance().openProjects.filter { !TrustedProjects.isProjectTrusted(it) }
+  return@withContext untrustedProjects.flatMap { project ->
+    setOf(project.stateStore.projectBasePath) + project.getModuleRoots().map { it.toNioPath() }
+  }.map { it.toAbsolutePath() }
 }
