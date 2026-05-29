6af24be076afb45d8907c321a7b5d38025f58494
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2026-05-29 11:31:03 +0200

    IJPL-245969 remove xstream dependency from lang-impl
    Offline inspection result parsing no longer needs XStream just to walk the inspection XML dump. Replace the XStream reader with a small JDK StAX parser that preserves the existing public API and handles the fields used by the offline inspection view.
    
    This allows intellij.platform.lang.impl to drop the temporary bundled xstream content-module dependency. The Java offline inspection test also gets the libraries.misc wrapper as a narrow runtime dependency so plugin loading continues to resolve Lucene after the libraries.misc wrapper migration.
    
    IJ-MR-184958 IJ-MR-184993 IJ-MR-179029 IJ-MR-184126 IJ-MR-181153 IJ-MR-146078 IJ-MR-175479 IJ-MR-186058 IJ-MR-193195 IJ-MR-196957 IJ-MR-199124 IJ-MR-197441 IJ-MR-204135 IJ-MR-204674
    
    GitOrigin-RevId: a0ae782f43fb871fe153736f560bdf45be20af69

--- platform/lang-impl/src/com/intellij/codeInspection/offlineViewer/OfflineViewParseUtil.java
+++ platform/lang-impl/src/com/intellij/codeInspection/offlineViewer/OfflineViewParseUtil.java
@@ -6,14 +6,16 @@ import com.intellij.codeInspection.offline.OfflineProblemDescriptor;
 import com.intellij.codeInspection.reference.SmartRefElementPointerImpl;
 import com.intellij.openapi.util.NlsSafe;
 import com.intellij.util.containers.Interner;
-import com.thoughtworks.xstream.io.xml.XppReader;
-import io.github.xstream.mxparser.MXParser;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -37,95 +39,192 @@ public final class OfflineViewParseUtil {
   }
 
   public static Map<String, Set<OfflineProblemDescriptor>> parse(Path problemFile) throws IOException {
-    return parse(Files.newBufferedReader(problemFile));
+    try (Reader reader = Files.newBufferedReader(problemFile)) {
+      return parse(reader);
+    }
   }
 
   public static Map<String, Set<OfflineProblemDescriptor>> parse(Reader problemReader) {
     Object2IntMap<String> fqName2IdxMap = new Object2IntOpenHashMap<>();
     Interner<@NlsSafe String> stringInterner = Interner.createStringInterner();
     Map<String, Set<OfflineProblemDescriptor>> package2Result = new HashMap<>();
-    XppReader reader = new XppReader(problemReader, new MXParser());
+    XMLStreamReader reader = null;
     try {
-      while(reader.hasMoreChildren()) {
-        reader.moveDown(); //problem
-        final OfflineProblemDescriptor descriptor = new OfflineProblemDescriptor();
-        boolean added = false;
-        while(reader.hasMoreChildren()) {
-          reader.moveDown();
-          if (SmartRefElementPointerImpl.ENTRY_POINT.equals(reader.getNodeName())) {
-            descriptor.setType(reader.getAttribute(SmartRefElementPointerImpl.TYPE_ATTR));
-            final String fqName = reader.getAttribute(SmartRefElementPointerImpl.FQNAME_ATTR);
-            descriptor.setFQName(fqName);
-
-            if (!fqName2IdxMap.containsKey(fqName)) {
-              fqName2IdxMap.put(fqName, 0);
-            }
-            int idx = fqName2IdxMap.getInt(fqName);
-            descriptor.setProblemIndex(idx);
-            fqName2IdxMap.put(fqName, idx + 1);
-          }
-          if (DESCRIPTION.equals(reader.getNodeName())) {
-            descriptor.setDescription(stringInterner.intern(reader.getValue()));
-          }
-          if (LINE.equals(reader.getNodeName())) {
-            descriptor.setLine(Integer.parseInt(reader.getValue()));
-          }
-          if(OFFSET.equals(reader.getNodeName())) {
-            descriptor.setOffset(Integer.parseInt(reader.getValue()));
-          }
-          if (MODULE.equals(reader.getNodeName())) {
-            descriptor.setModule(stringInterner.intern(reader.getValue()));
-          }
-          if (HINTS.equals(reader.getNodeName())) {
-            while(reader.hasMoreChildren()) {
-              reader.moveDown();
-              List<String> hints = descriptor.getHints();
-              if (hints == null) {
-                hints = new ArrayList<>();
-                descriptor.setHints(hints);
-              }
-              hints.add(stringInterner.intern(reader.getAttribute("value")));
-              reader.moveUp();
-            }
-          }
-          if (PACKAGE.equals(reader.getNodeName())) {
-            appendDescriptor(package2Result, reader.getValue(), descriptor);
-            added = true;
-          }
-          while(reader.hasMoreChildren()) {
-            reader.moveDown();
-            if (PACKAGE.equals(reader.getNodeName())) {
-              appendDescriptor(package2Result, reader.getValue(), descriptor);
-              added = true;
-            }
-            reader.moveUp();
-          }
-          reader.moveUp();
+      reader = createXmlStreamReader(problemReader);
+      while (reader.hasNext()) {
+        if (reader.next() == XMLStreamConstants.START_ELEMENT && "problem".equals(reader.getLocalName())) {
+          parseProblem(reader, fqName2IdxMap, stringInterner, package2Result);
         }
-        if (!added) appendDescriptor(package2Result, null, descriptor);
-        reader.moveUp();
       }
     }
+    catch (XMLStreamException e) {
+      throw new RuntimeException(e);
+    }
     finally {
-      reader.close();
+      closeReader(reader);
     }
     return package2Result;
   }
 
+  private static void parseProblem(@NotNull XMLStreamReader reader,
+                                   @NotNull Object2IntMap<String> fqName2IdxMap,
+                                   @NotNull Interner<@NlsSafe String> stringInterner,
+                                   @NotNull Map<String, Set<OfflineProblemDescriptor>> package2Result) throws XMLStreamException {
+    final OfflineProblemDescriptor descriptor = new OfflineProblemDescriptor();
+    final List<String> packageNames = new ArrayList<>();
+    int depth = 0;
+    int hintsDepth = -1;
+    int textDepth = -1;
+    String textElement = null;
+    StringBuilder text = null;
+
+    while (reader.hasNext()) {
+      int event = reader.next();
+      if (event == XMLStreamConstants.START_ELEMENT) {
+        depth++;
+        String nodeName = reader.getLocalName();
+        if (depth == 1 && SmartRefElementPointerImpl.ENTRY_POINT.equals(nodeName)) {
+          parseEntryPoint(reader, descriptor, fqName2IdxMap);
+        }
+        if (depth == 1 && HINTS.equals(nodeName)) {
+          hintsDepth = depth;
+        }
+        else if (hintsDepth >= 0 && depth == hintsDepth + 1) {
+          String hintValue = reader.getAttributeValue(null, "value");
+          if (hintValue == null) {
+            throw new XMLStreamException("Missing 'value' attribute in inspection hint", reader.getLocation());
+          }
+          addHint(descriptor, stringInterner, hintValue);
+        }
+        if (isTextElement(nodeName, depth)) {
+          textDepth = depth;
+          textElement = nodeName;
+          text = new StringBuilder();
+        }
+      }
+      else if (event == XMLStreamConstants.CHARACTERS || event == XMLStreamConstants.CDATA) {
+        if (text != null) {
+          text.append(reader.getText());
+        }
+      }
+      else if (event == XMLStreamConstants.END_ELEMENT) {
+        if (depth == 0 && "problem".equals(reader.getLocalName())) {
+          break;
+        }
+        if (depth == textDepth && reader.getLocalName().equals(textElement)) {
+          applyText(descriptor, stringInterner, packageNames, textElement, text.toString());
+          textDepth = -1;
+          textElement = null;
+          text = null;
+        }
+        if (depth == hintsDepth && HINTS.equals(reader.getLocalName())) {
+          hintsDepth = -1;
+        }
+        depth--;
+      }
+    }
+
+    if (packageNames.isEmpty()) {
+      appendDescriptor(package2Result, null, descriptor);
+    }
+    else {
+      for (String packageName : packageNames) {
+        appendDescriptor(package2Result, packageName, descriptor);
+      }
+    }
+  }
+
+  private static void parseEntryPoint(@NotNull XMLStreamReader reader,
+                                      @NotNull OfflineProblemDescriptor descriptor,
+                                      @NotNull Object2IntMap<String> fqName2IdxMap) {
+    descriptor.setType(reader.getAttributeValue(null, SmartRefElementPointerImpl.TYPE_ATTR));
+    final String fqName = reader.getAttributeValue(null, SmartRefElementPointerImpl.FQNAME_ATTR);
+    descriptor.setFQName(fqName);
+
+    if (!fqName2IdxMap.containsKey(fqName)) {
+      fqName2IdxMap.put(fqName, 0);
+    }
+    int idx = fqName2IdxMap.getInt(fqName);
+    descriptor.setProblemIndex(idx);
+    fqName2IdxMap.put(fqName, idx + 1);
+  }
+
+  private static boolean isTextElement(@NotNull String nodeName, int depth) {
+    return PACKAGE.equals(nodeName) ||
+           depth == 1 && (DESCRIPTION.equals(nodeName) || LINE.equals(nodeName) || OFFSET.equals(nodeName) || MODULE.equals(nodeName));
+  }
+
+  private static void applyText(@NotNull OfflineProblemDescriptor descriptor,
+                                @NotNull Interner<@NlsSafe String> stringInterner,
+                                @NotNull List<String> packageNames,
+                                @NotNull String nodeName,
+                                @NotNull String value) {
+    if (DESCRIPTION.equals(nodeName)) {
+      descriptor.setDescription(stringInterner.intern(value));
+    }
+    else if (LINE.equals(nodeName)) {
+      descriptor.setLine(Integer.parseInt(value));
+    }
+    else if (OFFSET.equals(nodeName)) {
+      descriptor.setOffset(Integer.parseInt(value));
+    }
+    else if (MODULE.equals(nodeName)) {
+      descriptor.setModule(stringInterner.intern(value));
+    }
+    else if (PACKAGE.equals(nodeName)) {
+      packageNames.add(value);
+    }
+  }
+
+  private static void addHint(@NotNull OfflineProblemDescriptor descriptor,
+                              @NotNull Interner<@NlsSafe String> stringInterner,
+                              @NotNull String value) {
+    List<String> hints = descriptor.getHints();
+    if (hints == null) {
+      hints = new ArrayList<>();
+      descriptor.setHints(hints);
+    }
+    hints.add(stringInterner.intern(value));
+  }
+
   public static @Nullable String parseProfileName(@NotNull Path descriptorFile) throws IOException {
-    return parseProfileName(Files.newBufferedReader(descriptorFile));
+    try (Reader reader = Files.newBufferedReader(descriptorFile)) {
+      return parseProfileName(reader);
+    }
   }
 
   public static @Nullable String parseProfileName(Reader descriptorReader) {
-    final XppReader reader = new XppReader(descriptorReader, new MXParser());
+    XMLStreamReader reader = null;
     try {
-      return reader.getAttribute(InspectionsResultUtil.PROFILE);
+      reader = createXmlStreamReader(descriptorReader);
+      while (reader.hasNext()) {
+        if (reader.next() == XMLStreamConstants.START_ELEMENT) {
+          return reader.getAttributeValue(null, InspectionsResultUtil.PROFILE);
+        }
+      }
+      return null;
     }
     catch (Exception e) {
       return null;
     }
     finally {
+      closeReader(reader);
+    }
+  }
+
+  private static @NotNull XMLStreamReader createXmlStreamReader(@NotNull Reader reader) throws XMLStreamException {
+    XMLInputFactory factory = XMLInputFactory.newDefaultFactory();
+    factory.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+    factory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+    return factory.createXMLStreamReader(reader);
+  }
+
+  private static void closeReader(@Nullable XMLStreamReader reader) {
+    if (reader == null) return;
+    try {
       reader.close();
+    }
+    catch (XMLStreamException ignored) {
     }
   }
 
