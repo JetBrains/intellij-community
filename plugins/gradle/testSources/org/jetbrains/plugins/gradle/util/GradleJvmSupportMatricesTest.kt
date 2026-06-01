4d382c994c6b720e9879ebf8c0832945d3466d92
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2026-06-01 12:51:45 +0200

    [gradle] IDEA-390010 Update Gradle version
    GitOrigin-RevId: 7cd8ae96c2b414c4e55c156216e4647b85a7e533

--- plugins/gradle/testSources/org/jetbrains/plugins/gradle/util/GradleJvmSupportMatricesTest.kt
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/util/GradleJvmSupportMatricesTest.kt
@@ -11,7 +11,7 @@ class GradleJvmSupportMatricesTest : GradleJvmSupportMatricesTestCase() {
     private const val LATEST_SUPPORTED_JAVA_VERSION = 26
     private const val FUTURE_JAVA_VERSION = 100
     private const val BUNDLED_GRADLE_VERSION = "9.5.0"
-    private const val LATEST_GRADLE_VERSION = "9.5.1"
+    private const val LATEST_GRADLE_VERSION = "9.6.0-rc-1"
   }
 
   fun `test bundled gradle version`() {
@@ -228,7 +228,7 @@ class GradleJvmSupportMatricesTest : GradleJvmSupportMatricesTestCase() {
     assertSupportedGradleVersion("8.0.2") { minOrNull() }
     assertSupportedGradleVersion("8.14.5") { maxOrNull() }
     assertSupportedGradleVersion("9.0.0") { minOrNull() }
-    assertSupportedGradleVersion("9.5.1") { maxOrNull() }
+    assertSupportedGradleVersion("9.6.0-rc-1") { maxOrNull() }
 
     assertSupportedGradleVersion(LATEST_GRADLE_VERSION) { maxOrNull() }
   }
