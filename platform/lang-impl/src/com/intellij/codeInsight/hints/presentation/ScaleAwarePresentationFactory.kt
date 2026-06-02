5b12f382076c826f9ce151aa63ae938137a23005
Sergei Tachenov <Sergei.Tachenov@jetbrains.com>
2026-06-02 15:26:24 +0300

    [UI, Editor Inlays] IJPL-246239 Fix icon shift scaling in ScaleAwareIconPresentation
    Because the font size is computed scaled here
    (it's unscaled later, two lines above this),
    we must also scale the font shift.
    Important for large scaling factors, as the difference
    can become quite noticeable.
    
    GitOrigin-RevId: 99e225c1f1af501a76e0ae4fe54b6d10e2a9c601

--- platform/lang-impl/src/com/intellij/codeInsight/hints/presentation/ScaleAwarePresentationFactory.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/hints/presentation/ScaleAwarePresentationFactory.kt
@@ -169,7 +169,7 @@ class ScaleAwareIconPresentation(val icon: Icon,
   private val scaledIcon by valueOf<Icon, IconParameters> { (fontSize, scale) ->
     IconUtil.scaleByFont(icon = icon, ancestor = editor.component, fontSize = fontSize / scale)
   }.withState {
-    IconParameters(editor.colorsScheme.editorFontSize2D - fontShift)
+    IconParameters(editor.colorsScheme.editorFontSize2D - JBUIScale.scale(fontShift))
   }
 
   private data class IconParameters(val font: Float, val scale: Float = JBUIScale.scale(1.0f))
