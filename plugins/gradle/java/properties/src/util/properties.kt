3419440caa1cbb94d3463934b0139bd3bd112cf1
Kajus Kuncaitis <kajus.kuncaitis@jetbrains.com>
2026-06-02 14:45:54 +0200

    [gradle][IDEA-388541] Don't refresh VFS in gradlePropertiesStream
    GitOrigin-RevId: c98ceb96cc74d4b40f990e7f555c865b24985851

--- plugins/gradle/java/properties/src/util/properties.kt
+++ plugins/gradle/java/properties/src/util/properties.kt
@@ -3,28 +3,20 @@ package com.intellij.gradle.java.properties.util
 
 import com.intellij.lang.properties.psi.PropertiesFile
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.findPsiFile
+import com.intellij.openapi.vfs.findVirtualFileOrDirectory
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.PsiUtilCore
-import com.intellij.util.asSafely
+import com.intellij.util.concurrency.annotations.RequiresReadLock
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.plugins.gradle.properties.GradlePropertiesFile
-import org.jetbrains.plugins.gradle.util.GradleConstants.GRADLE_PROPERTIES_FILE_NAME
 import java.nio.file.Path
 
 @ApiStatus.Internal
+@RequiresReadLock
 fun gradlePropertiesStream(place: PsiElement): Sequence<PropertiesFile> {
-  val projectPath = place.getRootGradleProjectPath()?.let { Path.of(it) } ?: return emptySequence()
-  return GradlePropertiesFile.getPropertyPaths(place.project, projectPath).asSequence()
-    .mapNotNull { it.parent.getGradlePropertiesFile(place.project) }
-}
-
-private fun Path.getGradlePropertiesFile(project: Project): PropertiesFile? {
-  val file = VfsUtil.findFile(this, false)?.findChild(GRADLE_PROPERTIES_FILE_NAME)
-  return file?.let { PsiUtilCore.getPsiFile(project, it) }.asSafely<PropertiesFile>()
-}
-
-internal fun PsiElement.getRootGradleProjectPath() : String? {
-  return ExternalSystemApiUtil.getExternalRootProjectPath(module)
+  val projectPath = ExternalSystemApiUtil.getExternalRootProjectPath(place.module) ?: return emptySequence()
+  return GradlePropertiesFile.getPropertyPaths(place.project, Path.of(projectPath)).asSequence()
+    .mapNotNull { it.findVirtualFileOrDirectory() }
+    .mapNotNull { it.findPsiFile(place.project) }
+    .filterIsInstance<PropertiesFile>()
 }
