49b36c93d3c915e3ce021eb269dff0c14b17abc5
Maria Sokolova <maria.sokolova@jetbrains.com>
2026-06-02 11:26:01 +0200

    [debugger] IDEA-388068 Add more Kotlin tests for instrumented conditional breakpoints for Kotlin
    # Conflicts:
    #	BUILD.bazel
    #	plugins/debugger/jvm-advanced/jvm.advanced.kotlin/BUILD.bazel
    
    GitOrigin-RevId: bea34cecdf12916e7b4262c6127ce2c84d330faf

--- plugins/kotlin/jvm-debugger/test/k2/testFramework/testSrc/org/jetbrains/kotlin/idea/k2/debugger/test/cases/AbstractK2IrKotlinSteppingTest.kt
+++ plugins/kotlin/jvm-debugger/test/k2/testFramework/testSrc/org/jetbrains/kotlin/idea/k2/debugger/test/cases/AbstractK2IrKotlinSteppingTest.kt
@@ -2,9 +2,6 @@
 
 package org.jetbrains.kotlin.idea.k2.debugger.test.cases
 
-import com.intellij.debugger.engine.INSTRUMENTATION_CONDITION_HIT_CALLBACK
-import com.intellij.execution.process.ProcessOutputTypes
-import com.intellij.openapi.util.registry.Registry
 import org.jetbrains.kotlin.config.JvmClosureGenerationScheme
 import org.jetbrains.kotlin.idea.debugger.test.AbstractIrKotlinSteppingTest
 
@@ -14,22 +11,4 @@ abstract class AbstractK2IdeK2CodeKotlinSteppingTest : AbstractK2IdeK1CodeKotlin
     override val compileWithK2 = true
 
     override fun lambdasGenerationScheme() = JvmClosureGenerationScheme.INDY
-}
-
-abstract class AbstractK2IdeK2CodeKotlinInstrumentationConditionsTest : AbstractK2IdeK2CodeKotlinSteppingTest() {
-    override fun setUp() {
-        super.setUp()
-        val enabledByDefault = Registry.`is`("debugger.breakpoint.instrumentation")
-        Registry.get("debugger.breakpoint.instrumentation").setValue(true)
-        atDebuggerTearDown {
-            Registry.get("debugger.breakpoint.instrumentation").setValue(enabledByDefault)
-        }
-    }
-
-    override fun createLocalProcess(className: String?) {
-        super.createLocalProcess(className)
-        debugProcess.putUserData(INSTRUMENTATION_CONDITION_HIT_CALLBACK) {
-            println("Instrumentation hit", ProcessOutputTypes.SYSTEM)
-        }
-    }
 }
