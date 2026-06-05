f6c1a75aa14b0d0a5f6918549c85b1742953fcd7
Max Medvedev <maxim.medvedev@jetbrains.com>
2026-06-05 12:18:22 +0200

    IJPL-236953 RpcCompletionRequestId should not hold project id
    GitOrigin-RevId: e0872e77c8a15745d461a6bbe7f496af748d314a

--- platform/completion/common/src/protocol/RpcCompletionRequest.kt
+++ platform/completion/common/src/protocol/RpcCompletionRequest.kt
@@ -1,8 +1,9 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.completion.common.protocol
 
 import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.openapi.editor.impl.EditorId
+import com.intellij.platform.project.ProjectId
 import kotlinx.serialization.Serializable
 
 /**
@@ -12,6 +13,7 @@ import kotlinx.serialization.Serializable
 data class RpcCompletionRequest(
   val id: RpcCompletionRequestId,
   val editorId: EditorId,
+  val projectId: ProjectId,
   val startingEditorVersion: Int,
   val completionType: CompletionType = CompletionType.BASIC,
   val invocationCount: Int = 0,
@@ -19,6 +21,7 @@ data class RpcCompletionRequest(
   override fun toString(): String = buildToString("RpcCompletionRequest") {
     field("id", id)
     field("editorId", editorId)
+    field("projectId", projectId)
     field("startingEditorVersion", startingEditorVersion)
     fieldWithDefault("completionType", completionType, CompletionType.BASIC)
     fieldWithDefault("invocationCount", invocationCount, 0)
