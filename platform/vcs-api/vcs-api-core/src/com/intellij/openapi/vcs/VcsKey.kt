92bc397f9c9d34ed04b14daa199619fe456016ec
Nikolay Chashnikov <nikolay.chashnikov@jetbrains.com>
2026-06-05 12:54:23 +0200

    IJPL-246466 VCS: replace usages of @IntellijInternalApi with @ApiStatus.Internal
    GitOrigin-RevId: 379b344852578ebac80e3111fd0499f33738cde2

--- platform/vcs-api/vcs-api-core/src/com/intellij/openapi/vcs/VcsKey.kt
+++ platform/vcs-api/vcs-api-core/src/com/intellij/openapi/vcs/VcsKey.kt
@@ -1,12 +1,11 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vcs
 
-import com.intellij.openapi.util.IntellijInternalApi
 import kotlinx.serialization.Serializable
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.NonNls
 
 @Serializable
-data class VcsKey @ApiStatus.Internal @IntellijInternalApi constructor(val name: @NonNls String) {
+data class VcsKey @ApiStatus.Internal constructor(val name: @NonNls String) {
   override fun toString(): String = name
 }
