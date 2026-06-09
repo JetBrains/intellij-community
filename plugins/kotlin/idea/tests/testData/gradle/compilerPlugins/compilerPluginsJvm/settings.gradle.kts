a5cc42e263230d3f75e1f4291ae8cfdaed659adb
Vladimir Iakunin <vladimir.iakunin@jetbrains.com>
2026-06-09 16:53:58 +0200

    KTIJ-38420 [Kotlin] migrate compiler-plugin suite
    GitOrigin-RevId: f4a10b39da7cf1bea5eee67cfb599979b68f5138

--- plugins/kotlin/idea/tests/testData/gradle/compilerPlugins/compilerPluginsJvm/settings.gradle.kts
+++ plugins/kotlin/idea/tests/testData/gradle/compilerPlugins/compilerPluginsJvm/settings.gradle.kts
@@ -7,6 +7,7 @@ pluginManagement {
         kotlin("jvm") version "{{kgp_version}}"
         kotlin("plugin.serialization") version "{{kgp_version}}"
         kotlin("plugin.noarg") version "{{kgp_version}}"
+        kotlin("plugin.allopen") version "{{kgp_version}}"
         kotlin("plugin.lombok") version "{{kgp_version}}"
         kotlin("plugin.assignment") version "{{kgp_version}}"
         kotlin("plugin.power-assert") version "{{kgp_version}}"
