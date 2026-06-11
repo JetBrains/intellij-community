e6d0661469fe99942932830fc8dded7f5f60ebbf
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2026-06-11 15:51:07 +0200

    IJPL-246148 LSP: change return type of LspClientDescriptor.startServerProcess to less specific BaseProcessHandler
    Intorduction of new classes with migration strategy allows us to clean up long-standing annoyances in the API
    
    GitOrigin-RevId: 99807acd1c4ae16af32accab873203e7d7c68698

--- platform/lsp/src/api/LspServerDescriptor.kt
+++ platform/lsp/src/api/LspServerDescriptor.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.lsp.api
 
+import com.intellij.execution.process.OSProcessHandler
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsSafe
@@ -15,6 +16,10 @@ abstract class LspServerDescriptor protected constructor(
   @NlsSafe presentableName: String,
   vararg roots: VirtualFile,
 ) : LspClientDescriptor(project, presentableName, *roots) {
+
+  override fun startServerProcess(): OSProcessHandler {
+    return super.startServerProcess() as OSProcessHandler
+  }
 
   companion object {
     @Deprecated("Use LspClientDescriptor.LOG", ReplaceWith("LspClientDescriptor.LOG", "com.intellij.platform.lsp.api.LspClientDescriptor"))
