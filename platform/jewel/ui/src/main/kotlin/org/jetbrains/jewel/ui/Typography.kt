0d19e5f633e312f0a115e9a752e40ef3f7d722d4
Sebastiano Poggi <sebp@google.com>
2026-06-11 15:36:27 +0200

    [JEWEL-1317] Add portable typography text style APIs
    Adds portable Jewel typography helpers for default, editor, and console text styles so UI code can use JewelTheme.typography instead of platform-specific bridge or standalone APIs.
    
    Keeps bridge font-size requests aligned with Swing/editor font metrics for sp sizes while preserving proportional em sizing, and updates Jewel metalava API dumps.

--- platform/jewel/ui/src/main/kotlin/org/jetbrains/jewel/ui/Typography.kt
+++ platform/jewel/ui/src/main/kotlin/org/jetbrains/jewel/ui/Typography.kt
@@ -1,11 +1,19 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the
+// Apache 2.0 license.
 package org.jetbrains.jewel.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ProvidableCompositionLocal
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.isUnspecified
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.takeOrElse
 import org.jetbrains.jewel.foundation.theme.JewelTheme
 
 /**
@@ -15,6 +23,63 @@ import org.jetbrains.jewel.foundation.theme.JewelTheme
  * These match the functionality provided by `JBFont` in the IntelliJ Platform.
  */
 public interface Typography {
+    /**
+     * Remembers the default text style for this typography implementation.
+     *
+     * @param fontSize The font size to use. If unspecified, the implementation default is used.
+     * @param fontWeight The typeface thickness to use. If null, the implementation default is used.
+     * @param fontStyle The typeface variant to use. If null, the implementation default is used.
+     */
+    @Composable
+    public fun rememberDefaultTextStyle(
+        fontSize: TextUnit = TextUnit.Unspecified,
+        fontWeight: FontWeight? = null,
+        fontStyle: FontStyle? = null,
+    ): TextStyle {
+        val baseStyle = labelTextStyle
+        return remember(baseStyle, fontSize, fontWeight, fontStyle) {
+            baseStyle.withTypographyRequest(fontSize, fontWeight, fontStyle)
+        }
+    }
+
+    /**
+     * Remembers the editor text style for this typography implementation.
+     *
+     * @param fontSize The font size to use. If unspecified, the implementation default is used.
+     * @param fontWeight The typeface thickness to use. If null, the implementation default is used.
+     * @param fontStyle The typeface variant to use. If null, the implementation default is used.
+     */
+    @Composable
+    public fun rememberEditorTextStyle(
+        fontSize: TextUnit = TextUnit.Unspecified,
+        fontWeight: FontWeight? = null,
+        fontStyle: FontStyle? = null,
+    ): TextStyle {
+        val baseStyle = editorTextStyle
+        return remember(baseStyle, fontSize, fontWeight, fontStyle) {
+            baseStyle.withTypographyRequest(fontSize, fontWeight, fontStyle)
+        }
+    }
+
+    /**
+     * Remembers the console text style for this typography implementation.
+     *
+     * @param fontSize The font size to use. If unspecified, the implementation default is used.
+     * @param fontWeight The typeface thickness to use. If null, the implementation default is used.
+     * @param fontStyle The typeface variant to use. If null, the implementation default is used.
+     */
+    @Composable
+    public fun rememberConsoleTextStyle(
+        fontSize: TextUnit = TextUnit.Unspecified,
+        fontWeight: FontWeight? = null,
+        fontStyle: FontStyle? = null,
+    ): TextStyle {
+        val baseStyle = consoleTextStyle
+        return remember(baseStyle, fontSize, fontWeight, fontStyle) {
+            baseStyle.withTypographyRequest(fontSize, fontWeight, fontStyle)
+        }
+    }
+
     /**
      * The text style to use for labels. Identical to
      * [`JewelTheme.defaultTextStyle`][org.jetbrains.jewel.foundation.theme.JewelTheme.defaultTextStyle].
@@ -57,6 +122,32 @@ public interface Typography {
     /** The text style used for code consoles. Usually is a monospaced font. Can be the same as [editorTextStyle]. */
     @get:Composable public val consoleTextStyle: TextStyle
 }
+
+private fun TextStyle.withTypographyRequest(
+    fontSize: TextUnit,
+    fontWeight: FontWeight?,
+    fontStyle: FontStyle?,
+): TextStyle =
+    copy(
+        fontSize = fontSize.takeOrElse { this.fontSize },
+        fontWeight = fontWeight ?: this.fontWeight,
+        fontStyle = fontStyle ?: this.fontStyle,
+        lineHeight = lineHeight.scaledFrom(this.fontSize, fontSize),
+    )
+
+private fun TextUnit.scaledFrom(baseFontSize: TextUnit, requestedFontSize: TextUnit): TextUnit =
+    when {
+        requestedFontSize.isUnspecified -> this
+        isSp && baseFontSize.isSp && requestedFontSize.isSp -> {
+            val scale = requestedFontSize.value / baseFontSize.value
+            (value * scale).sp
+        }
+        isSp && baseFontSize.isSp && requestedFontSize.isEm -> {
+            val lineHeightToFontSizeRatio = value / baseFontSize.value
+            lineHeightToFontSizeRatio.em
+        }
+        else -> this
+    }
 
 public val LocalTypography: ProvidableCompositionLocal<Typography> = staticCompositionLocalOf {
     error("No Typography provided. Have you forgotten the theme?")
