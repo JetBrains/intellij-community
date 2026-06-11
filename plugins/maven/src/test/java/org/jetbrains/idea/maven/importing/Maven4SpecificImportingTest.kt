53aae2f5041187505b839d3e65db05f0a44f8c03
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-06-11 15:48:46 +0200

    [maven] IDEA-389543 convert Maven4SpecificImportingTest to junit 5
    GitOrigin-RevId: 0583bafb2b0a3ab7a4809a7e9242637677d9aabb

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/Maven4SpecificImportingTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/Maven4SpecificImportingTest.kt
@@ -1,85 +1,108 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.maven.importing
 
-import com.intellij.maven.testFramework.MavenMultiVersionImportingTestCase
+import com.intellij.testFramework.UsefulTestCase.assertSameElements
+import com.intellij.testFramework.junit5.TestApplication
 import kotlinx.coroutines.runBlocking
-import org.junit.Assume
-import org.junit.Test
+import org.jetbrains.idea.maven.fixtures.MavenVersionArguments
+import org.jetbrains.idea.maven.fixtures.assertModules
+import org.jetbrains.idea.maven.fixtures.assumeModel_4_1_0
+import org.jetbrains.idea.maven.fixtures.createModulePom
+import org.jetbrains.idea.maven.fixtures.getActualMavenVersion
+import org.jetbrains.idea.maven.fixtures.importProjectAsync
+import org.jetbrains.idea.maven.fixtures.mavenImportingFixture
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertSame
+import org.junit.jupiter.api.Assumptions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedClass
+import org.junit.jupiter.params.provider.ArgumentsSource
 
-class Maven4SpecificImportingTest : MavenMultiVersionImportingTestCase() {
-  override fun setUp() {
-    super.setUp()
-    assumeModel_4_1_0("Tests are specific for maven 4.1.0 only")
+@TestApplication
+@ParameterizedClass
+@ArgumentsSource(MavenVersionArguments::class)
+class Maven4SpecificImportingTest(mavenVersion: String, modelVersion: String) {
+
+  private val maven by mavenImportingFixture(
+    mavenVersion = mavenVersion,
+    modelVersion = modelVersion
+  )
+  
+  @BeforeEach
+  fun setUp() {
+    maven.assumeModel_4_1_0("Tests are specific for maven 4.1.0 only")
   }
 
   @Test
   fun testChildAutoscanningWorksForMaven4() = runBlocking {
-    val modulePom = createModulePom("m1", """
+    val modulePom = maven.createModulePom("m1", """
       <groupId>test</groupId>
       <artifactId>m1</artifactId>
       <version>1</version>
     """.trimIndent())
-    importProjectAsync("""
+    maven.importProjectAsync("""
       <groupId>test</groupId>
       <artifactId>project</artifactId>
       <version>1</version>
       <packaging>pom</packaging>
       """)
-    assertModules("project", "m1")
-    val moduleProject = projectsManager.findProject(modulePom)
-    val rootProject = projectsManager.findProject(projectPom)
-    assertNotNull("m1 project should not be null", moduleProject)
-    assertNotNull("root project should not be null", rootProject)
-    assertSameElements("Project root should have one child", projectsManager.projectsTree.getModules(rootProject!!), listOf(moduleProject))
-    assertSame("Root projecy should be same", projectsManager.projectsTree.findRootProject(moduleProject!!), rootProject)
+    maven.assertModules("project", "m1")
+    val moduleProject = maven.projectsManager.findProject(modulePom)
+    val rootProject = maven.projectsManager.findProject(maven.projectPom)
+    assertNotNull(moduleProject, "m1 project should not be null")
+    assertNotNull(rootProject, "root project should not be null")
+    assertSameElements("Project root should have one child", maven.projectsManager.projectsTree.getModules(rootProject!!), listOf(moduleProject))
+    assertSame(maven.projectsManager.projectsTree.findRootProject(moduleProject!!), rootProject, "Root projecy should be same")
   }
 
   @Test
   fun testChildAutoscanningCouldBeDisabledForMaven4() = runBlocking {
-    val version = getActualMavenVersion()
-    Assume.assumeTrue("For maven newer that 4.0.0-rc4 - see IDEA-379195 and https://github.com/apache/maven/issues/11114", false)
-    createModulePom("m1", """
+    val version = maven.getActualMavenVersion()
+    Assumptions.assumeTrue(false, "For maven newer that 4.0.0-rc4 - see IDEA-379195 and https://github.com/apache/maven/issues/11114")
+    maven.createModulePom("m1", """
       <groupId>test</groupId>
       <artifactId>m1</artifactId>
       <version>1</version>
     """.trimIndent())
-    importProjectAsync("""
+    maven.importProjectAsync("""
       <groupId>test</groupId>
       <artifactId>project</artifactId>
       <version>1</version>
       <packaging>pom</packaging>
       <subprojects/>
       """)
-    assertModules("project")
+    maven.assertModules("project")
   }
 
   @Test
   fun testGrandChildAutoscanningDoesNotWorksForMaven4() = runBlocking {
-     createModulePom("m1", """
+     maven.createModulePom("m1", """
       <groupId>test</groupId>
       <artifactId>m1</artifactId>
       <version>1</version>
     """.trimIndent())
 
-    createModulePom("sub/m2", """
+    maven.createModulePom("sub/m2", """
       <groupId>test</groupId>
       <artifactId>m2</artifactId>
       <version>1</version>
     """.trimIndent())
 
-    importProjectAsync("""
+    maven.importProjectAsync("""
       <groupId>test</groupId>
       <artifactId>project</artifactId>
       <version>1</version>
       <packaging>pom</packaging>
       """)
-    assertModules("project", "m1")
+    maven.assertModules("project", "m1")
 
   }
 
   @Test
   fun testAutomaticParentVersioning() = runBlocking {
-    val modulePom = createModulePom("m1", """
+    val modulePom = maven.createModulePom("m1", """
       <parent>
           <groupId>test</groupId>
           <artifactId>project</artifactId>
@@ -88,20 +111,20 @@ class Maven4SpecificImportingTest : MavenMultiVersionImportingTestCase() {
      
     """.trimIndent())
 
-    importProjectAsync("""
+    maven.importProjectAsync("""
       <groupId>test</groupId>
       <artifactId>project</artifactId>
       <version>42</version>
       <packaging>pom</packaging>
       """)
-    assertModules("project", "m1")
-    val moduleProject = projectsManager.findProject(modulePom)
+    maven.assertModules("project", "m1")
+    val moduleProject = maven.projectsManager.findProject(modulePom)
     assertNotNull(moduleProject)
-    assertEquals("Should have parent", "42", moduleProject!!.mavenId.version);
-    val rootProject = projectsManager.findProject(projectPom)
-    assertNotNull("m1 project should not be null", moduleProject)
-    assertNotNull("root project should not be null", rootProject)
-    assertSameElements("Project root should have one child", projectsManager.projectsTree.getModules(rootProject!!), listOf(moduleProject))
-    assertSame("Root projecy should be same", projectsManager.projectsTree.findRootProject(moduleProject!!), rootProject)
+    assertEquals("42", moduleProject!!.mavenId.version, "Should have parent");
+    val rootProject = maven.projectsManager.findProject(maven.projectPom)
+    assertNotNull(moduleProject, "m1 project should not be null")
+    assertNotNull(rootProject, "root project should not be null")
+    assertSameElements("Project root should have one child", maven.projectsManager.projectsTree.getModules(rootProject!!), listOf(moduleProject))
+    assertSame(maven.projectsManager.projectsTree.findRootProject(moduleProject!!), rootProject, "Root projecy should be same")
   }
 }
