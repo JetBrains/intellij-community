4c08fe7e29a2e442efc43de895c5b830d50041d7
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-06-12 17:39:31 +0200

    [maven] IDEA-389543 convert MavenProjectReaderConnectorsTest to junit 5
    GitOrigin-RevId: e48f7e5c9a1ba9e16e2df10aefc61ba1c043f012

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/project/MavenProjectReaderConnectorsTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/project/MavenProjectReaderConnectorsTest.kt
@@ -2,37 +2,59 @@
 package org.jetbrains.idea.maven.project
 
 import kotlinx.coroutines.runBlocking
+import org.jetbrains.idea.maven.fixtures.MavenVersionArguments
+import org.jetbrains.idea.maven.fixtures.createProjectPom
+import org.jetbrains.idea.maven.fixtures.evaluateEffectivePom
+import org.jetbrains.idea.maven.fixtures.mavenImportingFixture
+import org.jetbrains.idea.maven.fixtures.readProject
 import org.jetbrains.idea.maven.server.MavenServerConnector
 import org.jetbrains.idea.maven.server.MavenServerManager
 import org.jetbrains.idea.maven.server.withCompatibleConnector
 import org.jetbrains.idea.maven.server.withStoppedConnector
 import org.jetbrains.idea.maven.server.withStoppedConnectorOnce
-import org.junit.Test
 import java.rmi.ConnectException
+import com.intellij.testFramework.junit5.TestApplication
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedClass
+import org.junit.jupiter.params.provider.ArgumentsSource
 
-class MavenProjectReaderConnectorsTest : MavenProjectReaderTestCase() {
+@TestApplication
+@ParameterizedClass
+@ArgumentsSource(MavenVersionArguments::class)
+class MavenProjectReaderConnectorsTest(mavenVersion: String, modelVersion: String) {
+
+  private val maven by mavenImportingFixture(
+    mavenVersion = mavenVersion,
+    modelVersion = modelVersion
+  )
+  
   @Test
   fun `test when using stopped connector always then get exception`() = runBlocking {
-    createProjectPom("""
+    maven.createProjectPom("""
                        <groupId>test</groupId>
                        <artifactId>project</artifactId>
                        <version>1</version>
                        """.trimIndent())
     assertThrows(ConnectException::class.java) {
       runBlocking {
-        withStoppedConnector { evaluateEffectivePom(projectPom) }
+        withStoppedConnector { maven.evaluateEffectivePom(maven.projectPom) }
       }
     }
   }
 
   @Test
   fun `test when using stopped connector once then recover`() = runBlocking {
-    createProjectPom("""
+    maven.createProjectPom("""
                        <groupId>test</groupId>
                        <artifactId>project</artifactId>
                        <version>1</version>
                        """.trimIndent())
-    val p = withStoppedConnectorOnce { readProject(projectPom).mavenId }
+    val p = withStoppedConnectorOnce { maven.readProject(maven.projectPom).mavenId }
     assertEquals("test", p.groupId)
     assertEquals("project", p.artifactId)
     assertEquals("1", p.version)
@@ -42,8 +64,8 @@ class MavenProjectReaderConnectorsTest : MavenProjectReaderTestCase() {
   @Test
   fun `test when connector is shut down then it is removed from manager`() = runBlocking {
     val mavenServerManager = MavenServerManager.getInstance()
-    val connector1 = withCompatibleConnector { mavenServerManager.getConnector(project, project.basePath + "/1") }
-    val connector2 = mavenServerManager.getConnector(project, project.basePath + "/2")
+    val connector1 = withCompatibleConnector { mavenServerManager.getConnector(maven.project, maven.project.basePath + "/1") }
+    val connector2 = mavenServerManager.getConnector(maven.project, maven.project.basePath + "/2")
     assertTrue(connector1 === connector2)
     assertEquals(setOf(connector1), mavenServerManager.getAllConnectors().toSet())
     mavenServerManager.shutdownConnector(connector1, false)
