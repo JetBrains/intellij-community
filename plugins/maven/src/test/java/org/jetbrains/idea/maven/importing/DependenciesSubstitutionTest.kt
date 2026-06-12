fbe930671a4469bb439f1cb5b732a8d87b2ab58d
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-06-12 17:31:32 +0200

    [maven] IDEA-389543 convert DependenciesSubstitutionTest to junit 5
    GitOrigin-RevId: 999e318ebcc2ca4811034d38491802cac8e2f6b1

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/DependenciesSubstitutionTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/DependenciesSubstitutionTest.kt
@@ -4,21 +4,45 @@ package org.jetbrains.idea.maven.importing
 import com.intellij.maven.testFramework.MavenMultiVersionImportingTestCase
 import com.intellij.openapi.util.registry.Registry
 import kotlinx.coroutines.runBlocking
-import org.junit.Test
+import com.intellij.testFramework.junit5.TestApplication
+import org.jetbrains.idea.maven.fixtures.MavenVersionArguments
+import org.jetbrains.idea.maven.fixtures.assertModuleModuleDeps
+import org.jetbrains.idea.maven.fixtures.assertModules
+import org.jetbrains.idea.maven.fixtures.createProjectSubFile
+import org.jetbrains.idea.maven.fixtures.importProjectAsync
+import org.jetbrains.idea.maven.fixtures.mavenImportingFixture
+import org.jetbrains.idea.maven.fixtures.refreshFiles
+import org.jetbrains.idea.maven.fixtures.setPomContent
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedClass
+import org.junit.jupiter.params.provider.ArgumentsSource
 
-open class DependenciesSubstitutionTest : MavenMultiVersionImportingTestCase() {
+open @TestApplication
+@ParameterizedClass
+@ArgumentsSource(MavenVersionArguments::class)
+class DependenciesSubstitutionTest(mavenVersion: String, modelVersion: String) {
+
+  private val maven by mavenImportingFixture(
+    mavenVersion = mavenVersion,
+    modelVersion = modelVersion
+  )
+  
   @Test
   fun `simple library substitution`() = runBlocking {
     val value = Registry.get("external.system.substitute.library.dependencies")
     try {
       value.setValue(true)
-      val p1Pom = createProjectSubFile("p1/pom.xml")
-      setPomContent(p1Pom, "<groupId>test</groupId>" +
+      val p1Pom = maven.createProjectSubFile("p1/pom.xml")
+      maven.setPomContent(p1Pom, "<groupId>test</groupId>" +
                            "<artifactId>p1</artifactId>" +
                            "<packaging>jar</packaging>" +
                            "<version>1.0</version>")
-      val p2Pom = createProjectSubFile("p2/pom.xml")
-      setPomContent(p2Pom, "<groupId>test</groupId>" +
+      val p2Pom = maven.createProjectSubFile("p2/pom.xml")
+      maven.setPomContent(p2Pom, "<groupId>test</groupId>" +
                            "<artifactId>p2</artifactId>" +
                            "<packaging>jar</packaging>" +
                            "<version>1</version>" +
@@ -29,11 +53,11 @@ open class DependenciesSubstitutionTest : MavenMultiVersionImportingTestCase() {
                            "    <version>1.0</version>" +
                            "  </dependency>" +
                            "</dependencies>")
-      refreshFiles(listOf(p1Pom, p2Pom))
-      importProjectAsync(p1Pom)
-      importProjectAsync(p2Pom)
-      assertModules("p1", "p2")
-      assertModuleModuleDeps("p2", "p1")
+      maven.refreshFiles(listOf(p1Pom, p2Pom))
+      maven.importProjectAsync(p1Pom)
+      maven.importProjectAsync(p2Pom)
+      maven.assertModules("p1", "p2")
+      maven.assertModuleModuleDeps("p2", "p1")
     }
     finally {
       value.resetToDefault()
