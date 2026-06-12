0876bcd0c6ad8b9dee312100d3aae0548e2415e8
Stanislau Palyn <stanislau.palyn@jetbrains.com>
2026-06-12 15:36:15 +0200

    [git] Add rewritten list support for in-memory reword and extract operations (IJPL-246423)
    GitOrigin-RevId: eb67dd2d59fdad59e854b0808ad2b82fbc7975e1

--- plugins/git4idea/tests/git4idea/inMemory/rebase/GitInMemoryInteractiveRebaseProcessTest.kt
+++ plugins/git4idea/tests/git4idea/inMemory/rebase/GitInMemoryInteractiveRebaseProcessTest.kt
@@ -21,7 +21,6 @@ import git4idea.test.last
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.assertThrows
-import java.util.Collections.synchronizedList
 import kotlin.io.path.exists
 import kotlin.io.path.readText
 
@@ -849,22 +848,6 @@ internal class GitInMemoryInteractiveRebaseProcessTest : GitInMemoryOperationTes
       captureFile.readText()
     )
   }
-
-  private fun capturePostRewrites(): List<PostRewriteInvocation> {
-    val captures = synchronizedList(mutableListOf<PostRewriteInvocation>())
-    git.runHookListener = { _, hookName, _, stdinLines ->
-      if (hookName == "post-rewrite") {
-        captures += PostRewriteInvocation(stdinLines.map {
-          val parts = it.split(' ', limit = 2)
-          RewrittenCommit(oldHash = parts[0], newHash = parts[1])
-        })
-      }
-    }
-    return captures
-  }
-
-  private data class RewrittenCommit(val oldHash: String, val newHash: String)
-  private data class PostRewriteInvocation(val mappings: List<RewrittenCommit>)
 
   private fun getRebaseEntries(firstCommit: VcsFullCommitDetails): List<GitRebaseEntryGeneratedUsingLog> = runBlocking {
     val entries = project.service<GitInteractiveRebaseEntriesProvider>().tryGetEntriesForDialog(repo, firstCommit, logData)
