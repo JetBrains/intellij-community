959f6d5eda2449aae7b3e60d0f3f58ae019aaa6a
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-06-12 08:55:59 +0200

    [maven] IDEA-389543 convert AddFileAsMavenProjectActionTest to junit 5
    GitOrigin-RevId: 03eab7126c67e2acc6c195ac0e209d33b0752003

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/project/actions/AddFileAsMavenProjectActionTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/project/actions/AddFileAsMavenProjectActionTest.kt
@@ -13,37 +13,58 @@ import com.intellij.testFramework.TestActionEvent
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
-import org.junit.Test
+import com.intellij.testFramework.junit5.TestApplication
+import org.jetbrains.idea.maven.fixtures.MavenVersionArguments
+import org.jetbrains.idea.maven.fixtures.assertModules
+import org.jetbrains.idea.maven.fixtures.createPomXml
+import org.jetbrains.idea.maven.fixtures.createProjectPom
+import org.jetbrains.idea.maven.fixtures.mavenImportingFixture
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedClass
+import org.junit.jupiter.params.provider.ArgumentsSource
 
-class AddFileAsMavenProjectActionTest : MavenMultiVersionImportingTestCase() {
+@TestApplication
+@ParameterizedClass
+@ArgumentsSource(MavenVersionArguments::class)
+class AddFileAsMavenProjectActionTest(mavenVersion: String, modelVersion: String) {
+
+  private val maven by mavenImportingFixture(
+    mavenVersion = mavenVersion,
+    modelVersion = modelVersion
+  )
+  
 
   @Test
   fun testFilesSavedOnAction() = runBlocking {
-    val projectPom = createProjectPom("<groupId>test</groupId>" +
+    val projectPom = maven.createProjectPom("<groupId>test</groupId>" +
                                       "<artifactId>project</artifactId>" +
                                       "<version>1</version>")
 
     val document = readAction {
-      val psiFile = PsiManager.getInstance(project).findFile(projectPom)!!
-      PsiDocumentManager.getInstance(project).getDocument(psiFile)!!
+      val psiFile = PsiManager.getInstance(maven.project).findFile(projectPom)!!
+      PsiDocumentManager.getInstance(maven.project).getDocument(psiFile)!!
     }
 
     // make a change but do not save
     edtWriteAction {
-      document.setText(createPomXml("<groupId>test</groupId>" +
+      document.setText(maven.createPomXml("<groupId>test</groupId>" +
                                     "<artifactId>project-new</artifactId>" +
                                     "<version>1</version>"))
     }
 
     val context = MapDataContext()
-    context.put(CommonDataKeys.PROJECT, project)
+    context.put(CommonDataKeys.PROJECT, maven.project)
     context.put(CommonDataKeys.VIRTUAL_FILE, projectPom)
     val event = TestActionEvent.createTestEvent(context)
     withContext(Dispatchers.EDT) {
       AddFileAsMavenProjectAction().actionPerformedAsync(event)
     }
 
-    assertModules("project-new")
+    maven.assertModules("project-new")
   }
 
 }
