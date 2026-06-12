c66e1fbcbdfa46ebe7d084d287238c89ba5daf2a
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-06-12 17:29:22 +0200

    [maven] IDEA-389543 convert MavenSetupProjectTest to junit 5
    GitOrigin-RevId: b28fc630ff40b019e906699150d71262a627f0a1

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenSetupProjectTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenSetupProjectTest.kt
@@ -5,111 +5,140 @@ import com.intellij.testFramework.openProjectAsync
 import com.intellij.testFramework.useProjectAsync
 import com.intellij.workspaceModel.ide.impl.WorkspaceModelCacheImpl
 import kotlinx.coroutines.runBlocking
-import org.junit.Test
+import com.intellij.testFramework.junit5.TestApplication
+import org.jetbrains.idea.maven.fixtures.MavenVersionArguments
+import org.jetbrains.idea.maven.fixtures.ProjectInfo
+import org.jetbrains.idea.maven.fixtures.assertProjectState
+import org.jetbrains.idea.maven.fixtures.attachProjectAsync
+import org.jetbrains.idea.maven.fixtures.attachProjectFromScriptAsync
+import org.jetbrains.idea.maven.fixtures.createModulePom
+import org.jetbrains.idea.maven.fixtures.generateProject
+import org.jetbrains.idea.maven.fixtures.getGeneralSettings
+import org.jetbrains.idea.maven.fixtures.importProjectActionAsync
+import org.jetbrains.idea.maven.fixtures.mavenImportingFixture
+import org.jetbrains.idea.maven.fixtures.openPlatformProjectAsync
+import org.jetbrains.idea.maven.fixtures.testRootDisposable
+import org.jetbrains.idea.maven.fixtures.waitForImport
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedClass
+import org.junit.jupiter.params.provider.ArgumentsSource
 
 
-class MavenSetupProjectTest : MavenSetupProjectTestCase() {
+@TestApplication
+@ParameterizedClass
+@ArgumentsSource(MavenVersionArguments::class)
+class MavenSetupProjectTest(mavenVersion: String, modelVersion: String) {
+
+  private val maven by mavenImportingFixture(
+    mavenVersion = mavenVersion,
+    modelVersion = modelVersion
+  )
+  
 
   @Test
   fun `test settings are not reset`() = runBlocking {
-    val projectInfo = generateProject("A")
-    val linkedProjectInfo = generateProject("L")
-    waitForImport {
+    val projectInfo = maven.generateProject("A")
+    val linkedProjectInfo = maven.generateProject("L")
+    maven.waitForImport {
       openProjectAsync(projectInfo.projectFile)
     }.useProjectAsync {
-      assertProjectState(it, projectInfo)
-      getGeneralSettings(it).isWorkOffline = true
-      waitForImport {
-        attachProjectAsync(it, linkedProjectInfo.projectFile)
+      maven.assertProjectState(it, projectInfo)
+      maven.getGeneralSettings(it).isWorkOffline = true
+      maven.waitForImport {
+        maven.attachProjectAsync(it, linkedProjectInfo.projectFile)
       }
-      assertProjectState(it, projectInfo, linkedProjectInfo)
-      assertTrue(getGeneralSettings(it).isWorkOffline)
+      maven.assertProjectState(it, projectInfo, linkedProjectInfo)
+      assertTrue(maven.getGeneralSettings(it).isWorkOffline)
     }
   }
 
   @Test
   fun `test project open`() = runBlocking {
-    val projectInfo = generateProject("A")
-    waitForImport {
+    val projectInfo = maven.generateProject("A")
+    maven.waitForImport {
       openProjectAsync(projectInfo.projectFile)
     }.useProjectAsync {
-      assertProjectState(it, projectInfo)
+      maven.assertProjectState(it, projectInfo)
     }
   }
 
   @Test
   fun `test project import`() = runBlocking {
-    val projectInfo = generateProject("A")
-    waitForImport {
-      importProjectActionAsync(projectInfo.projectFile)
+    val projectInfo = maven.generateProject("A")
+    maven.waitForImport {
+      maven.importProjectActionAsync(projectInfo.projectFile)
     }.useProjectAsync {
-      assertProjectState(it, projectInfo)
+      maven.assertProjectState(it, projectInfo)
     }
   }
 
   @Test
   fun `test project attach`() = runBlocking {
-    val projectInfo = generateProject("A")
-    openPlatformProjectAsync(projectInfo.projectFile.parent)
+    val projectInfo = maven.generateProject("A")
+    maven.openPlatformProjectAsync(projectInfo.projectFile.parent)
       .useProjectAsync {
-        waitForImport {
-          attachProjectAsync(it, projectInfo.projectFile)
+        maven.waitForImport {
+          maven.attachProjectAsync(it, projectInfo.projectFile)
         }
-        assertProjectState(it, projectInfo)
+        maven.assertProjectState(it, projectInfo)
       }
   }
 
   @Test
   fun `test project import from script`() = runBlocking {
-    val projectInfo = generateProject("A")
-    openPlatformProjectAsync(projectInfo.projectFile.parent)
+    val projectInfo = maven.generateProject("A")
+    maven.openPlatformProjectAsync(projectInfo.projectFile.parent)
       .useProjectAsync {
-        waitForImport {
-          attachProjectFromScriptAsync(it, projectInfo.projectFile)
+        maven.waitForImport {
+          maven.attachProjectFromScriptAsync(it, projectInfo.projectFile)
         }
-        assertProjectState(it, projectInfo)
+        maven.assertProjectState(it, projectInfo)
       }
   }
 
   @Test
   fun `test module attach`() = runBlocking {
-    val projectInfo = generateProject("A")
-    val linkedProjectInfo = generateProject("L")
-    waitForImport {
+    val projectInfo = maven.generateProject("A")
+    val linkedProjectInfo = maven.generateProject("L")
+    maven.waitForImport {
       openProjectAsync(projectInfo.projectFile)
     }.useProjectAsync {
-      assertProjectState(it, projectInfo)
-      waitForImport {
-        attachProjectAsync(it, linkedProjectInfo.projectFile)
+      maven.assertProjectState(it, projectInfo)
+      maven.waitForImport {
+        maven.attachProjectAsync(it, linkedProjectInfo.projectFile)
       }
-      assertProjectState(it, projectInfo, linkedProjectInfo)
+      maven.assertProjectState(it, projectInfo, linkedProjectInfo)
     }
   }
 
   @Test
   fun `test project re-open`() = runBlocking {
-    val projectInfo = generateProject("A")
-    val linkedProjectInfo = generateProject("L")
+    val projectInfo = maven.generateProject("A")
+    val linkedProjectInfo = maven.generateProject("L")
 
-    WorkspaceModelCacheImpl.forceEnableCaching(testRootDisposable)
-    waitForImport {
+    WorkspaceModelCacheImpl.forceEnableCaching(maven.testRootDisposable)
+    maven.waitForImport {
       openProjectAsync(projectInfo.projectFile)
     }.useProjectAsync(save = true) {
-      assertProjectState(it, projectInfo)
-      waitForImport {
-        attachProjectAsync(it, linkedProjectInfo.projectFile)
+      maven.assertProjectState(it, projectInfo)
+      maven.waitForImport {
+        maven.attachProjectAsync(it, linkedProjectInfo.projectFile)
       }
-      assertProjectState(it, projectInfo, linkedProjectInfo)
+      maven.assertProjectState(it, projectInfo, linkedProjectInfo)
     }
     openProjectAsync(projectInfo.projectFile)
       .useProjectAsync {
-        assertProjectState(it, projectInfo, linkedProjectInfo)
+        maven.assertProjectState(it, projectInfo, linkedProjectInfo)
       }
   }
 
   @Test
   fun `test project re-open with same module name in different cases`() = runBlocking {
-    val projectPom = createModulePom("project-name", """
+    val projectPom = maven.createModulePom("project-name", """
                      <groupId>test</groupId>
                      <artifactId>project</artifactId>
                      <packaging>pom</packaging>
@@ -119,12 +148,12 @@ class MavenSetupProjectTest : MavenSetupProjectTestCase() {
                        <module>dir2/M</module>
                      </modules>
                      """.trimIndent())
-    createModulePom("project-name/dir1/m", """
+    maven.createModulePom("project-name/dir1/m", """
     <groupId>test</groupId>
     <artifactId>m</artifactId>
     <version>1</version>
     """.trimIndent())
-    createModulePom("project-name/dir2/M", """
+    maven.createModulePom("project-name/dir2/M", """
     <groupId>test</groupId>
     <artifactId>M</artifactId>
     <version>1</version>
@@ -133,37 +162,37 @@ class MavenSetupProjectTest : MavenSetupProjectTestCase() {
     runBlocking {
       val projectInfo = ProjectInfo(projectPom, "project", "m (1)", "M (2)")
 
-      WorkspaceModelCacheImpl.forceEnableCaching(testRootDisposable)
-      waitForImport {
+      WorkspaceModelCacheImpl.forceEnableCaching(maven.testRootDisposable)
+      maven.waitForImport {
         openProjectAsync(projectInfo.projectFile)
       }.useProjectAsync(save = true) {
-        assertProjectState(it, projectInfo)
+        maven.assertProjectState(it, projectInfo)
       }
       openProjectAsync(projectInfo.projectFile)
         .useProjectAsync {
-          assertProjectState(it, projectInfo)
+          maven.assertProjectState(it, projectInfo)
         }
     }
   }
 
   @Test
   fun `test project re-import deprecation`() = runBlocking {
-    val projectInfo = generateProject("A")
-    val linkedProjectInfo = generateProject("L")
+    val projectInfo = maven.generateProject("A")
+    val linkedProjectInfo = maven.generateProject("L")
 
-    WorkspaceModelCacheImpl.forceEnableCaching(testRootDisposable)
-    waitForImport {
+    WorkspaceModelCacheImpl.forceEnableCaching(maven.testRootDisposable)
+    maven.waitForImport {
       openProjectAsync(projectInfo.projectFile)
     }.useProjectAsync(save = true) {
-      assertProjectState(it, projectInfo)
-      waitForImport {
-        attachProjectAsync(it, linkedProjectInfo.projectFile)
+      maven.assertProjectState(it, projectInfo)
+      maven.waitForImport {
+        maven.attachProjectAsync(it, linkedProjectInfo.projectFile)
       }
-      assertProjectState(it, projectInfo, linkedProjectInfo)
+      maven.assertProjectState(it, projectInfo, linkedProjectInfo)
     }
-    importProjectActionAsync(projectInfo.projectFile)
+    maven.importProjectActionAsync(projectInfo.projectFile)
       .useProjectAsync {
-        assertProjectState(it, projectInfo, linkedProjectInfo)
+        maven.assertProjectState(it, projectInfo, linkedProjectInfo)
       }
   }
 }
