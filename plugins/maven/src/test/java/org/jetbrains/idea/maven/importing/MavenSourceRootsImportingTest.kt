cddbbd6fde653c348f695d807223fc7c60da1e89
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-06-12 17:31:51 +0200

    [maven] IDEA-389543 convert MavenSourceRootsImportingTest to junit 5
    GitOrigin-RevId: 58b15fd5c2175e190cb46d863d4cba66b56ba7dc

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenSourceRootsImportingTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/MavenSourceRootsImportingTest.kt
@@ -3,15 +3,38 @@ package org.jetbrains.idea.maven.importing
 
 import com.intellij.maven.testFramework.MavenMultiVersionImportingTestCase
 import kotlinx.coroutines.runBlocking
-import org.junit.Test
+import com.intellij.testFramework.junit5.TestApplication
+import org.jetbrains.idea.maven.fixtures.MavenVersionArguments
+import org.jetbrains.idea.maven.fixtures.assertModules
+import org.jetbrains.idea.maven.fixtures.assertSources
+import org.jetbrains.idea.maven.fixtures.assertTestSources
+import org.jetbrains.idea.maven.fixtures.createProjectSubDirs
+import org.jetbrains.idea.maven.fixtures.importProjectAsync
+import org.jetbrains.idea.maven.fixtures.mavenImportingFixture
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedClass
+import org.junit.jupiter.params.provider.ArgumentsSource
 
-internal class MavenSourceRootsImportingTest : MavenMultiVersionImportingTestCase() {
+internal @TestApplication
+@ParameterizedClass
+@ArgumentsSource(MavenVersionArguments::class)
+class MavenSourceRootsImportingTest(mavenVersion: String, modelVersion: String) {
+
+  private val maven by mavenImportingFixture(
+    mavenVersion = mavenVersion,
+    modelVersion = modelVersion
+  )
+  
 
   @Test
   fun testKotlinSourceRootsAreImportedWithSmartDefaults() = runBlocking {
-    createProjectSubDirs("src/main/kotlin", "src/test/kotlin")
+    maven.createProjectSubDirs("src/main/kotlin", "src/test/kotlin")
 
-    importProjectAsync("""
+    maven.importProjectAsync("""
       <groupId>test</groupId>
       <artifactId>project</artifactId>
       <version>1</version>
@@ -27,8 +50,8 @@ internal class MavenSourceRootsImportingTest : MavenMultiVersionImportingTestCas
       </build>
     """.trimIndent())
 
-    assertModules("project")
-    assertSources("project", "src/main/java", "src/main/kotlin")
-    assertTestSources("project", "src/test/java", "src/test/kotlin")
+    maven.assertModules("project")
+    maven.assertSources("project", "src/main/java", "src/main/kotlin")
+    maven.assertTestSources("project", "src/test/java", "src/test/kotlin")
   }
 }
