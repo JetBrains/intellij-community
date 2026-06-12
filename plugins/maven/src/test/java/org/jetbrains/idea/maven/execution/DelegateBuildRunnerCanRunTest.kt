4af1729998409148a4f7af9dc14f9ae246cae20c
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-06-12 18:17:02 +0200

    [maven] IDEA-389543 convert DelegateBuildRunnerCanRunTest to junit 5
    GitOrigin-RevId: e1995fc13e0e733454b1d4bbf18b46226be1abcc

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/execution/DelegateBuildRunnerCanRunTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/execution/DelegateBuildRunnerCanRunTest.kt
@@ -6,31 +6,65 @@ import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.runners.ProgramRunner
 import com.intellij.maven.testFramework.MavenExecutionTestCase
 import org.jetbrains.idea.maven.execution.build.DELEGATE_RUNNER_ID
-import org.junit.Test
+import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.maven.testFramework.utils.MavenProjectJDKTestFixture
+import com.intellij.openapi.application.WriteAction
+import com.intellij.testFramework.EdtTestUtil
+import com.intellij.util.ThrowableRunnable
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.jetbrains.idea.maven.fixtures.MavenVersionArguments
+import org.jetbrains.idea.maven.fixtures.mavenImportingFixture
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedClass
+import org.junit.jupiter.params.provider.ArgumentsSource
 
-class DelegateBuildRunnerCanRunTest : MavenExecutionTestCase() {
+@TestApplication
+@ParameterizedClass
+@ArgumentsSource(MavenVersionArguments::class)
+class DelegateBuildRunnerCanRunTest(mavenVersion: String, modelVersion: String) {
+
+  private val maven by mavenImportingFixture(
+    mavenVersion = mavenVersion,
+    modelVersion = modelVersion
+  )
+
+  private lateinit var jdkFixture: MavenProjectJDKTestFixture
+
+  @BeforeEach
+  fun setUp() {
+    jdkFixture = MavenProjectJDKTestFixture(maven.project, "MavenExecutionTestJDK")
+    EdtTestUtil.runInEdtAndWait<RuntimeException?>(ThrowableRunnable {
+      WriteAction.runAndWait<RuntimeException?>(ThrowableRunnable { jdkFixture.setUp() })
+    })
+  }
+
+  @AfterEach
+  fun tearDownJdk() {
+    EdtTestUtil.runInEdtAndWait<RuntimeException?>(ThrowableRunnable {
+      WriteAction.runAndWait<RuntimeException?>(ThrowableRunnable { jdkFixture.tearDown() })
+    })
+  }
 
   @Test
   fun testDoesNotMavenRunConfiguration() {
     val runner = ProgramRunner.findRunnerById(DELEGATE_RUNNER_ID)?: throw AssertionError("[$DELEGATE_RUNNER_ID] build runner should be registered")
     val mavenFactory = MavenRunConfigurationType.getInstance().configurationFactories[0]
-    val mavenConfiguration = mavenFactory.createTemplateConfiguration(project)
+    val mavenConfiguration = mavenFactory.createTemplateConfiguration(maven.project)
 
-    assertFalse(
-      "Delegate build runner must accept Maven run configuration with DefaultRunExecutor",
-      runner.canRun(DefaultRunExecutor.EXECUTOR_ID, mavenConfiguration)
-    )
+    assertFalse(runner.canRun(DefaultRunExecutor.EXECUTOR_ID, mavenConfiguration), "Delegate build runner must accept Maven run configuration with DefaultRunExecutor")
   }
 
   @Test
   fun testDoesNotAcceptGenericApplicationRunConfiguration() {
     val runner = ProgramRunner.findRunnerById(DELEGATE_RUNNER_ID) ?: throw AssertionError("[$DELEGATE_RUNNER_ID] build runner should be registered")
     val appFactory = ApplicationConfigurationType.getInstance().configurationFactories[0]
-    val appConfiguration = appFactory.createTemplateConfiguration(project)
+    val appConfiguration = appFactory.createTemplateConfiguration(maven.project)
 
-    assertFalse(
-      "Delegate build runner must not accept generic Application run configuration",
-      runner.canRun(DefaultRunExecutor.EXECUTOR_ID, appConfiguration)
-    )
+    assertFalse(runner.canRun(DefaultRunExecutor.EXECUTOR_ID, appConfiguration), "Delegate build runner must not accept generic Application run configuration")
   }
 }
