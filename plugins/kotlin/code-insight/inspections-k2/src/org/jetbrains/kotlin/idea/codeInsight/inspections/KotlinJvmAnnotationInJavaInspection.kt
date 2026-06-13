5c90a47d3b5e7adfde177f05265c679793dd3f8b
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2026-06-13 17:10:28 +0400

    [kotlin] KTIJ-35361 convert `RemoveAnnotationFix` to `ModCommandAction`
    GitOrigin-RevId: 70ee7e03ac2cb44acfca6422c0512c4c78304d37

--- plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/codeInsight/inspections/KotlinJvmAnnotationInJavaInspection.kt
+++ plugins/kotlin/code-insight/inspections-k2/src/org/jetbrains/kotlin/idea/codeInsight/inspections/KotlinJvmAnnotationInJavaInspection.kt
@@ -3,12 +3,14 @@ package org.jetbrains.kotlin.idea.codeInsight.inspections
 
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.LocalQuickFix
-import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.openapi.project.Project
+import com.intellij.codeInspection.util.IntentionFamilyName
+import com.intellij.modcommand.ActionContext
+import com.intellij.modcommand.ModPsiUpdater
 import com.intellij.psi.JavaElementVisitor
 import com.intellij.psi.PsiAnnotation
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.intentions.KotlinPsiUpdateModCommandAction
 
 const val KOTLIN_JVM_PACKAGE: String = "kotlin.jvm."
 
@@ -20,19 +22,27 @@ internal class KotlinJvmAnnotationInJavaInspection : LocalInspectionTool() {
                 if (!qualifiedName.startsWith(KOTLIN_JVM_PACKAGE)) return
 
                 val annotationName = qualifiedName.removePrefix(KOTLIN_JVM_PACKAGE)
+                val action = RemoveAnnotationFix(annotation)
+                val fix = LocalQuickFix.from(action) ?: error("Broken contract: unexpected null quick fix for non-null action")
                 holder.registerProblem(
                     annotation,
                     KotlinBundle.message("inspection.kotlin.jvm.annotation.in.java.description", "@$annotationName"),
-                    RemoveAnnotationFix()
+                    fix,
                 )
             }
         }
 
-    private class RemoveAnnotationFix : LocalQuickFix {
-        override fun getFamilyName() =
+    private class RemoveAnnotationFix(
+        element: PsiAnnotation
+    ) : KotlinPsiUpdateModCommandAction.ElementContextless<PsiAnnotation>(element) {
+
+        override fun getFamilyName(): @IntentionFamilyName String =
             KotlinBundle.message("fix.remove.annotation.text")
 
-        override fun applyFix(project: Project, descriptor: ProblemDescriptor) =
-            descriptor.psiElement.delete()
+        override fun invoke(
+            context: ActionContext,
+            element: PsiAnnotation,
+            updater: ModPsiUpdater
+        ): Unit = element.delete()
     }
 }
