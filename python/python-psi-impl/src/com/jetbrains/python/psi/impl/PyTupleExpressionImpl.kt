3e8df897b99bd9c45e3b89a19551043caf443f45
Morgan Bartholomew <morgan.bartholomew@jetbrains.com>
2026-06-14 04:34:21 +1000

    PY-27205 typing: expand unpacked heterogeneous tuples in tuple literals
     Conflicts:
    	community/python/python-psi-impl/src/com/jetbrains/python/psi/impl/PyTupleExpressionImpl.kt
    	community/python/testSrc/com/jetbrains/python/Py3TypeTest.java
    	community/python/testSrc/com/jetbrains/python/PyTypeTest.java
    
    GitOrigin-RevId: b883e8c6bbb6938eed7d221fbc4dcb6ef842de49

--- python/python-psi-impl/src/com/jetbrains/python/psi/impl/PyTupleExpressionImpl.kt
+++ python/python-psi-impl/src/com/jetbrains/python/psi/impl/PyTupleExpressionImpl.kt
@@ -6,6 +6,7 @@ import com.jetbrains.python.psi.LanguageLevel
 import com.jetbrains.python.psi.PyElementGenerator
 import com.jetbrains.python.psi.PyElementVisitor
 import com.jetbrains.python.psi.PyParenthesizedExpression
+import com.jetbrains.python.psi.PyStarExpression
 import com.jetbrains.python.psi.PyTupleExpression
 import com.jetbrains.python.psi.types.getLiteralType
 import com.jetbrains.python.psi.types.PyLiteralType
@@ -20,10 +21,22 @@ class PyTupleExpressionImpl(astNode: ASTNode) : PySequenceExpressionImpl(astNode
 
   override fun getType(context: TypeEvalContext, key: TypeEvalContext.Key): PyType? {
     val inferLiteralTypes = PyLiteralType.inferLiteralTypeForLiteralExpressions()
-    return PyTupleType.create(
-      this,
-      elements.map { (if (inferLiteralTypes) it.getLiteralType(context) else null) ?: context.getType(it) }
-    )
+    val elementTypes = buildList {
+      for (element in elements) {
+        val starOperand = (element as? PyStarExpression)?.expression
+        if (starOperand != null) {
+          // Splice a statically known (heterogeneous) tuple operand into the surrounding tuple:
+          // `(1, *a)` with `a: tuple[int, int]` has type `tuple[int, int, int]`.
+          val operandType = context.getType(starOperand)
+          if (operandType is PyTupleType && !operandType.isHomogeneous) {
+            addAll(operandType.elementTypes)
+            continue
+          }
+        }
+        add((if (inferLiteralTypes) element.getLiteralType(context) else null) ?: context.getType(element))
+      }
+    }
+    return PyTupleType.create(this, elementTypes)
   }
 
   override fun deleteChildInternal(child: ASTNode) {
