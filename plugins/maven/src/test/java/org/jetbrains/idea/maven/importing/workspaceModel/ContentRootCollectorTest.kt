f26ea0663c29f2ee45c5c516bb937b85d9730a50
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-06-14 10:29:31 +0200

    [maven] IDEA-389543 convert ContentRootCollectorTest to junit 5
    GitOrigin-RevId: 65d66925e88a8ea9b2cf1352a3b120a37a8fa62a

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/workspaceModel/ContentRootCollectorTest.kt
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/workspaceModel/ContentRootCollectorTest.kt
@@ -1,17 +1,16 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.maven.importing.workspaceModel
 
-import com.intellij.maven.testFramework.MavenTestCase
-import junit.framework.TestCase
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.idea.maven.importing.StandardMavenModuleType
 import org.jetbrains.idea.maven.importing.workspaceModel.ContentRootCollector.ProjectRootFolder
 import org.jetbrains.jps.model.java.JavaResourceRootType
 import org.jetbrains.jps.model.java.JavaSourceRootType
 import org.jetbrains.jps.model.module.JpsModuleSourceRootType
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
-class ContentRootCollectorTest : MavenTestCase() {
+class ContentRootCollectorTest {
   @Test
   fun `test simple content root`() = runBlocking {
     val baseContentRoot = "/home/a/b/c/maven"
@@ -781,7 +780,7 @@ class ContentRootCollectorTest : MavenTestCase() {
       )
     }.sortedBy { it.expectedPath }
 
-    TestCase.assertEquals(expectedSorted, actualSorted)
+    assertEquals(expectedSorted, actualSorted)
   }
 
   private data class ContentRootTestData(val expectedPath: String,
