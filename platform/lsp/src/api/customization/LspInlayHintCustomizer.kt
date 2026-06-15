030d0802cf8bd7627c39ec955956c478222a2120
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2026-06-15 23:14:11 +0200

    cleanup LSP: remove redundancy from docs
    (cherry picked from commit 2c2570ceff269b6cfd70e7907c1f90fbfb3fcc73)
    
    GitOrigin-RevId: 09516ff711765c0c46d29fedbc3477126efc6b50

--- platform/lsp/src/api/customization/LspInlayHintCustomizer.kt
+++ platform/lsp/src/api/customization/LspInlayHintCustomizer.kt
@@ -13,10 +13,6 @@ sealed class LspInlayHintCustomizer
  * This is the primary integration point for language plugins that want to provide
  * settings and customization for LSP inlay hints. Language plugins should extend
  * this class and override the relevant methods to implement their own settings logic.
- *
- * This approach allows each language to have independent settings without affecting
- * other languages, solving the multi-language settings problem inherent in LSP providers.
- *
  */
 open class LspInlayHintSupport : LspInlayHintCustomizer() {
   /**
