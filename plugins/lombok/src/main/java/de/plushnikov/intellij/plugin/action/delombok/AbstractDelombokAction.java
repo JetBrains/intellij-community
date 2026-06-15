6f53b031952d204d8ff7a37f45c7b8cdb5486b10
Marcin Mikosik <marcin.mikosik@jetbrains.com>
2026-06-15 16:51:20 +0000

    IDEA-389775 [lombok]: Improve AbstractDelombokAction to use SequentialModalProgressTask to avoid freezes
    Merge-request: IJ-MR-207602
    Merged-by: Marcin Mikosik <marcin.mikosik@jetbrains.com>
    
    GitOrigin-RevId: 7733873cf9194ab320186d8df2847a2c0c596c65

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/action/delombok/AbstractDelombokAction.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/action/delombok/AbstractDelombokAction.java
@@ -8,12 +8,18 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.NlsContexts;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileSet;
 import com.intellij.openapi.vfs.VirtualFileVisitor;
 import com.intellij.psi.PsiAnnotation;
 import com.intellij.psi.PsiClass;
@@ -26,19 +32,24 @@ import com.intellij.psi.PsiManager;
 import com.intellij.psi.SyntheticElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.PsiUtilBase;
+import com.intellij.util.SequentialModalProgressTask;
+import com.intellij.util.SequentialTask;
 import com.intellij.util.containers.ContainerUtil;
+import de.plushnikov.intellij.plugin.LombokBundle;
 import de.plushnikov.intellij.plugin.util.LombokLibraryUtil;
 import de.plushnikov.intellij.plugin.util.PsiClassUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.NotNullByDefault;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 public abstract class AbstractDelombokAction extends AnAction {
   private DelombokHandler myHandler;
 
   protected AbstractDelombokAction() {
-    //default constructor
   }
 
   @Override
@@ -76,47 +87,99 @@ public abstract class AbstractDelombokAction extends AnAction {
           process(project, psiFile, targetClass);
         }
       }
-    } else {
+    }
+    else {
       final VirtualFile[] files = CommonDataKeys.VIRTUAL_FILE_ARRAY.getData(dataContext);
-      if (null != files) {
-        for (VirtualFile file : files) {
-          if (file.isDirectory()) {
-            processDirectory(project, file);
-          } else {
-            processFile(project, file);
-          }
-        }
+      if (null != files && files.length > 0) {
+        collectJavaFilesAndProcessThem(project, files);
       }
     }
   }
 
-  private void processDirectory(final @NotNull Project project, @NotNull VirtualFile vFile) {
-    VfsUtilCore.visitChildrenRecursively(vFile, new VirtualFileVisitor<Void>() {
+  private void collectJavaFilesAndProcessThem(Project project, VirtualFile @NotNull [] selectedFilesAndDirs) {
+    var task = new Task.Backgroundable(project, LombokBundle.message("group.DelombokActionGroup.text"), true) {
       @Override
-      public boolean visitFile(@NotNull VirtualFile file) {
-        if (!file.isDirectory()) {
-          processFile(project, file);
-        }
-        return true;
+      public void run(@NotNull ProgressIndicator indicator) {
+        List<PsiFile> files = ReadAction.computeBlocking(() -> {
+          indicator.setIndeterminate(true);
+          return collectJavaFilesToProcess(project, selectedFilesAndDirs);
+        });
+        processJavaFilesWithModalProgress(files, project);
+      }
+    };
+    task.queue();
+  }
+
+  private static @NotNull List<PsiFile> collectJavaFilesToProcess(@NotNull Project project,
+                                                                 VirtualFile @NotNull [] selectedFilesAndDirs) {
+    VirtualFileSet virtualFiles = VfsUtilCore.createCompactVirtualFileSet();
+    for (VirtualFile file : selectedFilesAndDirs) {
+      ProgressManager.checkCanceled();
+      if (file.isDirectory()) {
+        VfsUtilCore.visitChildrenRecursively(file, new VirtualFileVisitor<Void>() {
+          @Override
+          public boolean visitFile(@NotNull VirtualFile file) {
+            ProgressManager.checkCanceled();
+            if (!file.isDirectory()) {
+              addFileIfJava(file, virtualFiles);
+            }
+            return true;
+          }
+        });
+      }
+      else {
+        addFileIfJava(file, virtualFiles);
+      }
+    }
+    return toPsiFiles(project, virtualFiles);
+  }
+
+  private static @NotNull List<PsiFile> toPsiFiles(@NotNull Project project, @NotNull VirtualFileSet virtualFiles) {
+    PsiManager psiManager = PsiManager.getInstance(project);
+    return ContainerUtil.mapNotNull(virtualFiles, virtualFile -> {
+      ProgressManager.checkCanceled();
+      return psiManager.findFile(virtualFile);
+    });
+  }
+
+  private void processJavaFilesWithModalProgress(@NotNull List<PsiFile> psiFiles, Project project) {
+    if (ContainerUtil.isEmpty(psiFiles)) return;
+    String progressTitle = LombokBundle.message("group.DelombokActionGroup.text");
+    SequentialModalProgressTask progressTask = new SequentialModalProgressTask(project, progressTitle, true);
+    progressTask.setTask(new ProcessingSequentialTask(psiFiles, project));
+    progressTask.queue();
+  }
+
+  private static void addFileIfJava(@NotNull VirtualFile virtualFile, @NotNull VirtualFileSet result) {
+    if (!JavaFileType.INSTANCE.equals(virtualFile.getFileType())) {
+      return;
+    }
+    result.add(virtualFile);
+  }
+
+  private void process(@NotNull Project project, @NotNull PsiFile psiFile) {
+    executeCommand(project, () -> {
+      if (!psiFile.isValid()) {
+        return;
+      }
+      if (commitDocument(project, psiFile)) return;
+      if (psiFile instanceof PsiJavaFile psiJavaFile) {
+        getHandler().invoke(project, psiJavaFile);
       }
     });
   }
 
-  private void processFile(Project project, VirtualFile file) {
-    if (JavaFileType.INSTANCE.equals(file.getFileType())) {
-      final PsiManager psiManager = PsiManager.getInstance(project);
-      PsiJavaFile psiFile = (PsiJavaFile) psiManager.findFile(file);
-      if (psiFile != null) {
-        process(project, psiFile);
-      }
+  private static boolean commitDocument(@NotNull Project project, @NotNull PsiFile psiFile) {
+    PsiDocumentManager psiDocumentManager = PsiDocumentManager.getInstance(project);
+    Document document = psiDocumentManager.getDocument(psiFile);
+    if (document == null) {
+      return true;
     }
+    psiDocumentManager.commitDocument(document);
+    return false;
   }
 
-  protected void process(final @NotNull Project project, final @NotNull PsiJavaFile psiJavaFile) {
-    executeCommand(project, () -> getHandler().invoke(project, psiJavaFile));
-  }
-
-  protected void process(final @NotNull Project project, final @NotNull PsiFile psiFile, final @NotNull PsiClass psiClass) {
+  private void process(final @NotNull Project project, final @NotNull PsiFile psiFile, final @NotNull PsiClass psiClass) {
     executeCommand(project, () -> getHandler().invoke(project, psiFile, psiClass));
   }
 
@@ -207,5 +270,51 @@ public abstract class AbstractDelombokAction extends AnAction {
   private @NlsContexts.Command String getCommandName() {
     String text = getTemplatePresentation().getText();
     return text == null ? "" : text;
+  }
+
+  @NotNullByDefault
+  private class ProcessingSequentialTask implements SequentialTask {
+    private final Project project;
+    private final Iterator<PsiFile> iterator;
+    private final int fileCount;
+    private int processedFiles;
+
+    private ProcessingSequentialTask(List<PsiFile> files, Project project) {
+      this.project = project;
+      this.iterator = files.iterator();
+      this.fileCount = files.size();
+    }
+
+    @Override
+    public boolean iteration() {
+      return iteration(null);
+    }
+
+    @Override
+    public boolean iteration(@Nullable ProgressIndicator indicator) {
+      if (!iterator.hasNext()) {
+        return true;
+      }
+      PsiFile javaFile = iterator.next();
+      if (javaFile.isValid()) {
+        process(project, javaFile);
+      }
+      processedFiles++;
+      if (indicator != null) {
+        indicator.setText(javaFile.getName());
+        indicator.setFraction((double)processedFiles / fileCount);
+      }
+      return isDone();
+    }
+
+    @Override
+    public void stop() {
+      processedFiles = fileCount;
+    }
+
+    @Override
+    public boolean isDone() {
+      return processedFiles >= fileCount;
+    }
   }
 }
