dee0f2d0e542ab9c4cb4516429864740fcaecd33
Vlad Koshkin <vladislav.koshkin@jetbrains.com>
2026-06-15 18:52:18 +0200

    [kotlin] KTIJ-39172 switch on .main.kts
    GitOrigin-RevId: 817370da91eb55df1f6ae63b47e1aea5d347e987

--- plugins/kotlin/base/scripting/scripting.k2/src/org/jetbrains/kotlin/idea/core/script/k2/MainKtsAfterRunCallbackProvider.kt
+++ plugins/kotlin/base/scripting/scripting.k2/src/org/jetbrains/kotlin/idea/core/script/k2/MainKtsAfterRunCallbackProvider.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.core.script.k2.mainkts
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.core.script.k2
 
 import com.intellij.execution.runners.ProgramRunner
 import com.intellij.openapi.actionSystem.ActionPlaces
--- plugins/kotlin/base/scripting/scripting.k2/src/org/jetbrains/kotlin/idea/core/script/k2/definitions/MainKtsScriptDefinitionSource.kt
+++ plugins/kotlin/base/scripting/scripting.k2/src/org/jetbrains/kotlin/idea/core/script/k2/definitions/MainKtsScriptDefinitionSource.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.core.script.k2.mainkts.definitions
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.core.script.k2.definitions
 
 import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.idea.base.plugin.artifacts.KotlinArtifacts
