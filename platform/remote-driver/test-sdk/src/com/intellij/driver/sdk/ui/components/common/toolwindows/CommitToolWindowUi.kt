2ecc923a5bfe018e2c756671e44b26c637105be2
Radu Nedelcu <radu.nedelcu.external@jetbrains.com>
2026-06-15 11:23:27 +0300

    tests [VCS]: Add Rollback tests IJPL-221201
    GitOrigin-RevId: cc7cd029981e43025f25e46c36334d9d2818da65

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/toolwindows/CommitToolWindowUi.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/toolwindows/CommitToolWindowUi.kt
@@ -4,21 +4,28 @@ import com.intellij.driver.sdk.ui.components.ComponentData
 import com.intellij.driver.sdk.ui.components.UiComponent
 import com.intellij.driver.sdk.ui.components.common.JEditorUiComponent
 import com.intellij.driver.sdk.ui.components.vcs.JChangesListViewUi
-import com.intellij.driver.sdk.ui.xQuery
 
 class CommitToolWindowUi(data: ComponentData) : ToolWindowUiComponent(data) {
 
   val changeListView: JChangesListViewUi =
-    x(xQuery { byJavaClass("com.intellij.openapi.vcs.changes.LocalChangesListView") }, JChangesListViewUi::class.java)
-  val commitButton: UiComponent = x("//div[@class='CommitActionsPanel']//div[@class='MainButton' and @visible_text='Commit']")
-  val commitAndPushButton: UiComponent = x("//div[@class='CommitActionsPanel']//div[@visible_text='Commit and Push…']")
+    x(JChangesListViewUi::class.java) { byJavaClass("com.intellij.openapi.vcs.changes.LocalChangesListView") }
+  val commitButton: UiComponent =
+    x { componentWithChild(byClass("CommitActionsPanel"), byClass("MainButton") and byVisibleText("Commit")) }
+  val commitAndPushButton: UiComponent =
+    x { componentWithChild(byClass("CommitActionsPanel"), byVisibleText("Commit and Push…")) }
+
+  val rollbackToolbarButton: UiComponent =
+    x {
+      byClass("ActionButton") and (contains(byAccessibleName("Revert")) or contains(byAccessibleName("Rollback")) or byAttribute("myaction.key",
+                                                                                                                                 "action.ChangesView.Revert.text"))
+    }
 
   fun rightClickFile(fileName: String) {
     changeListView.rightClickPath("Changes", fileName, fullMatch = false)
   }
 
   fun commitEditor(block: JEditorUiComponent.() -> Unit = {}): JEditorUiComponent =
-    x(xQuery { byAccessibleName("Commit Message") }, JEditorUiComponent::class.java).apply { block() }
+    x(JEditorUiComponent::class.java) { byAccessibleName("Commit Message") }.apply { block() }
 
   fun selectFile(fileName: String) {
     changeListView.addFileName(fileName)
@@ -32,5 +39,7 @@ class CommitToolWindowUi(data: ComponentData) : ToolWindowUiComponent(data) {
     changeListView.removeFileName(fileName)
   }
 
-
+  fun clickRollbackToolbarButton() {
+    rollbackToolbarButton.click()
+  }
 }
