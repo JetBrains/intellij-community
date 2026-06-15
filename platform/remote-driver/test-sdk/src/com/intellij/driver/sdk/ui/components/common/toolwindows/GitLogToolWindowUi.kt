01d9087fce6cdf09b385c5703c825ff3c4b64b8a
Radu Nedelcu <radu.nedelcu.external@jetbrains.com>
2026-06-15 19:27:48 +0300

    refactor [VCS]: Streamline commit context opening in VCS log
    GitOrigin-RevId: 2ed10dc0eba48363e5ac4120c351f4184e587f58

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/toolwindows/GitLogToolWindowUi.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/toolwindows/GitLogToolWindowUi.kt
@@ -2,32 +2,19 @@ package com.intellij.driver.sdk.ui.components.common.toolwindows
 
 import com.intellij.driver.sdk.ui.components.ComponentData
 import com.intellij.driver.sdk.ui.components.elements.PopupMenuUiComponent
-import com.intellij.driver.sdk.ui.components.elements.contentTabLabel
 import com.intellij.driver.sdk.ui.components.elements.popupMenu
 import com.intellij.driver.sdk.ui.ui
-import com.intellij.driver.sdk.waitFor
 import java.awt.event.KeyEvent
 
 class GitLogToolWindowUi(data: ComponentData) : ToolWindowUiComponent(data) {
-
-  private fun openTab(tab: GitLogTab) {
-    contentTabLabel(tab.value).apply {
-      click()
-      waitFor("Wait until tab ${tab.value} will be selected") { isSelected }
-    }
-  }
-
-  fun openConsole() = openTab(GitLogTab.CONSOLE)
-  fun openLog() = openTab(GitLogTab.LOG)
-
   fun selectCommitAndGetInfo(commit: String): List<CommitId> {
     vcsLogGraphTable().clickCell { it.contains(commit) }
     return vcsLogGraphTable().logTable.getSelection().commits
   }
 
-  fun openContextMenuForCommitAndSelectOption(partialCommitText: String, optionToSelect: String) {
+  fun openContextMenuForCommit(partialCommitText: String): PopupMenuUiComponent {
     vcsLogGraphTable().apply { findRowColumn { it.contains(partialCommitText) }.apply { rightClickCell(first, second) } }
-    driver.ui.popupMenu().select(optionToSelect)
+    return driver.ui.popupMenu()
   }
 
   /**
@@ -48,14 +35,5 @@ class GitLogToolWindowUi(data: ComponentData) : ToolWindowUiComponent(data) {
     // Right-click on the originally selected commit row (still part of the multi-selection)
     table.rightClickCell(row, column)
     return driver.ui.popupMenu()
-  }
-
-
-  fun pressUndoCommit(commitMessage: String): Unit = openContextMenuForCommitAndSelectOption(commitMessage, "Undo Commit…")
-  fun pressRevertCommit(commitMessage: String): Unit = openContextMenuForCommitAndSelectOption(commitMessage, "Revert Commit")
-
-  private enum class GitLogTab(val value: String) {
-    CONSOLE("Console"),
-    LOG("Log");
   }
 }
