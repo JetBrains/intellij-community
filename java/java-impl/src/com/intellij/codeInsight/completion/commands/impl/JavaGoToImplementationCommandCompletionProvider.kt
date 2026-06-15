f4fe899a96eca8a72a36de4d25559bebb854169d
Mikhail Pyltsin <mikhail.pyltsin@jetbrains.com>
2026-06-15 12:07:33 +0200

    [command-completion] IDEA-390469 Command completion. Support go to implementation after `;`
    GitOrigin-RevId: 0ceb849f9f2878bca7d3a1cc52c9d95778661941

--- java/java-impl/src/com/intellij/codeInsight/completion/commands/impl/JavaGoToImplementationCommandCompletionProvider.kt
+++ java/java-impl/src/com/intellij/codeInsight/completion/commands/impl/JavaGoToImplementationCommandCompletionProvider.kt
@@ -1,11 +1,16 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.codeInsight.completion.commands.impl
 
+import com.intellij.codeInsight.completion.command.CommandCompletionProviderContext
 import com.intellij.codeInsight.completion.command.commands.AbstractGoToImplementationCompletionCommandProvider
+import com.intellij.codeInsight.completion.command.commands.ActionCompletionCommand
+import com.intellij.codeInsight.completion.command.getCommandContext
 import com.intellij.openapi.util.TextRange
+import com.intellij.psi.JavaTokenType
 import com.intellij.psi.LambdaUtil
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiJavaToken
 import com.intellij.psi.PsiMember
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiModifier
@@ -25,8 +30,14 @@ internal class JavaGoToImplementationCommandCompletionProvider : AbstractGoToImp
       }
     }
     else if (member is PsiMethod) {
+      val currentOffset = if (element is PsiJavaToken && element.tokenType == JavaTokenType.SEMICOLON) {
+        offset - 1
+      }
+      else {
+        offset
+      }
       if (!TextRange(member.textRange.startOffset, member.body?.textRange?.startOffset ?: member.textRange.endOffset)
-          .contains(offset)
+          .contains(currentOffset)
       ) {
         return false
       }
@@ -45,5 +56,18 @@ internal class JavaGoToImplementationCommandCompletionProvider : AbstractGoToImp
     }
 
     return true
+  }
+
+  override fun createCommand(context: CommandCompletionProviderContext): ActionCompletionCommand? {
+    val currentElement = getCommandContext(context.offset, context.psiFile)
+    var targetContext = context
+    if (currentElement is PsiJavaToken && currentElement.tokenType == JavaTokenType.SEMICOLON) {
+      var currentOffset = context.offset - 1
+      if (getCommandContext(context.offset - 1, context.psiFile)?.textMatches(")") == true) {
+        currentOffset = context.offset - 3
+      }
+      targetContext = context.copy(offset = currentOffset)
+    }
+    return super.createCommandWithNameIdentifierAndLastAdjusted(targetContext)
   }
 }
