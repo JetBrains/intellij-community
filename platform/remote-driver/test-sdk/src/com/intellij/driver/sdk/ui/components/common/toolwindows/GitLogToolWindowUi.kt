ca65efaf734dfce518e9b5539f2e75b65171901b
Radu Nedelcu <radu.nedelcu.external@jetbrains.com>
2026-06-15 16:10:03 +0300

    tests [VCS]: Add Revert Commits tests IJPL-221201
    GitOrigin-RevId: f406749c5b45980e5163b4ed325d57652e9b968f

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/toolwindows/GitLogToolWindowUi.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/toolwindows/GitLogToolWindowUi.kt
@@ -1,10 +1,12 @@
 package com.intellij.driver.sdk.ui.components.common.toolwindows
 
 import com.intellij.driver.sdk.ui.components.ComponentData
+import com.intellij.driver.sdk.ui.components.elements.PopupMenuUiComponent
 import com.intellij.driver.sdk.ui.components.elements.contentTabLabel
 import com.intellij.driver.sdk.ui.components.elements.popupMenu
 import com.intellij.driver.sdk.ui.ui
 import com.intellij.driver.sdk.waitFor
+import java.awt.event.KeyEvent
 
 class GitLogToolWindowUi(data: ComponentData) : ToolWindowUiComponent(data) {
 
@@ -28,7 +30,29 @@ class GitLogToolWindowUi(data: ComponentData) : ToolWindowUiComponent(data) {
     driver.ui.popupMenu().select(optionToSelect)
   }
 
+  /**
+   * Selects a commit, then extends the selection downward by [extraRows] rows using Shift+Down.
+   * Right-clicks on the last selected row and returns the context menu.
+   */
+  fun selectMultipleCommitsAndOpenContextMenu(firstCommit: String, extraRows: Int = 1): PopupMenuUiComponent {
+    val table = vcsLogGraphTable {
+      waitFound()
+    }
+    val (row, column) = table.findRowColumn { it.contains(firstCommit) }
+    table.clickCell(row, column)
+    driver.ui.keyboard {
+      pressing(KeyEvent.VK_SHIFT) {
+        repeat(extraRows) { key(KeyEvent.VK_DOWN) }
+      }
+    }
+    // Right-click on the originally selected commit row (still part of the multi-selection)
+    table.rightClickCell(row, column)
+    return driver.ui.popupMenu()
+  }
+
+
   fun pressUndoCommit(commitMessage: String): Unit = openContextMenuForCommitAndSelectOption(commitMessage, "Undo Commit…")
+  fun pressRevertCommit(commitMessage: String): Unit = openContextMenuForCommitAndSelectOption(commitMessage, "Revert Commit")
 
   private enum class GitLogTab(val value: String) {
     CONSOLE("Console"),
