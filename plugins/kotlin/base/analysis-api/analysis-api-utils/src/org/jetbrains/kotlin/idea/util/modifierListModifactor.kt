390b4d1a79e51a22c33bfd4bb8abb30d25f67a85
Anna Kozlova <anna.kozlova@jetbrains.com>
2026-06-16 22:25:37 +0200

    [kotlin] restore some API used by third-party plugins
    - add formal @ScheduledForRemoval for deprecated since 2020 api
    
    KTIJ-36178
    
    GitOrigin-RevId: dff81bac4ea6e2309acbaeb4eafc760fce3d5dda

--- plugins/kotlin/base/analysis-api/analysis-api-utils/src/org/jetbrains/kotlin/idea/util/modifierListModifactor.kt
+++ plugins/kotlin/base/analysis-api/analysis-api-utils/src/org/jetbrains/kotlin/idea/util/modifierListModifactor.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.base.util
+package org.jetbrains.kotlin.idea.util
 
 import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.types.KaClassType
