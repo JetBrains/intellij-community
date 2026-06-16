cdfdd304f27b0059aded5a61abe743059dae2b45
Max Medvedev <maxim.medvedev@jetbrains.com>
2026-06-16 20:10:53 +0200

    testFramework [TeamCity]: generify fleet.util.UID in error messages
    GitOrigin-RevId: 17c77b49e49a9f98edf2779619b8da458632c4bc

--- platform/testFramework/teamCity/src/generify.kt
+++ platform/testFramework/teamCity/src/generify.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.testFramework.teamCity
 
 private val OFFSET: Int = "AAAAAAA".hashCode()
@@ -22,18 +22,20 @@ fun convertToHashCodeWithOnlyLetters(hash: Int): String {
 }
 
 /**
- * Simplifies test grouping by replacing numbers, hashes, hex numbers with predefined constant values <ID>, <HASH>, <HEX>
+ * Simplifies test grouping by replacing numbers, hashes, hex numbers with predefined constant values <ID>, <HASH>, <HEX>, <UID>
  *  Eg:
  *  /mnt/build/temp35321324432 => <FILE>
  *  text@3ba5aac, text => text<ID>, text
  *  some-text.db451f59 => some-text.<HASH>
  *  0x01 => <HEX>
  *  text1234text => text<NUM>text
+ *  id=l8nnaskjn7k16m0qqqpt => id=<UID>
  **/
 fun generifyErrorMessage(originalMessage: String): String {
   return originalMessage
     .generifyFileNames()
     .generifyID()
+    .generifyUID()
     .generifyHash()
     .generifyHexadecimal()
     .generifyHexCode()
@@ -52,6 +54,24 @@ fun String.generifyID(omitDollarSign: Boolean = false): String {
   else "[\$@#][A-Za-z\\d-_]+"
 
   return this.replace(regexpStr.toRegex(), "<ID>")
+}
+
+/**
+ * Replaces a fleet.util.UID (random base-32, fixed length) with the placeholder <UID>.
+ * Without this, [generifyNumber] only swaps the digit runs and leaves the random letters,
+ * so each UID still generifies to a unique string. E.g. id=l8nnaskjn7k16m0qqqpt => id=<UID>
+ *
+ * Anchored to what UID.random() emits: alphabet [0-9a-v] (0123456789abcdefghijklmnopqrstuv),
+ * length 20 (see UID.BASE32 / UID.LENGTH in fleet/util/UID.kt).
+ */
+fun String.generifyUID(): String {
+  // boundary excludes the UID charset [A-Za-z0-9_-] so we don't match inside a longer token
+  val regex = Regex("(?<![A-Za-z0-9_-])[0-9a-v]{20}(?![A-Za-z0-9_-])")
+  return this.replace(regex) { match ->
+    val token = match.value
+    // require a mix of digits and letters, so pure numbers / pure words are left to other steps
+    if (token.any { it.isDigit() } && token.any { it in 'a'..'v' }) "<UID>" else token
+  }
 }
 
 /** some-text.db451f59 => some-text.<HASH> */
