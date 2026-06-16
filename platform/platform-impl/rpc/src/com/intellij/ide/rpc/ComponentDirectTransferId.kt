ae7622ed1824009113646b3e9a22f2bc485b4db4
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2026-06-16 17:10:32 +0300

    IJPL-246949 UiInfrastructureTest.context menu in a component passed using 'setupTransfer' is flaky
    Fixing the problem, when ComponentDirectTransferId.getComponent() returned null for the value obtained via RPC, due to a race with RD protocol dispatching (via RdDispatcher). Now an empty wrapper panel is
    returned if Lux content isn't available at the moment, and it will be populated when it becomes available.
    This also re-fixes the problem addressed by commit 91357e28 in another way - a wrapper panel is also used in monolith mode.
    
    GitOrigin-RevId: 888324620fe8e5e082c548b9a10df5130e7af798

--- platform/platform-impl/rpc/src/com/intellij/ide/rpc/ComponentDirectTransferId.kt
+++ platform/platform-impl/rpc/src/com/intellij/ide/rpc/ComponentDirectTransferId.kt
@@ -2,20 +2,24 @@
 package com.intellij.ide.rpc
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.util.Disposer
 import com.intellij.util.concurrency.annotations.RequiresEdt
 import fleet.openmap.SerializedValue
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 import org.jetbrains.annotations.ApiStatus
+import java.awt.BorderLayout
+import java.awt.Component
 import javax.swing.JComponent
+import javax.swing.JPanel
 
 /**
  * Prepares the transmission of the graphical representation of the component over the network to the frontend with input events
- * transferred in the opposite direction. The generated id object can be passed over RPC and used with
- * [ComponentDirectTransferId.getComponent] (or [ComponentDirectTransferId.getComponentWithDisposable]) method on the frontend to realize the
- * 'mirroring' component. The binding is only valid during the lifetime of the passed `disposable` parameter. During this time the source
- * component is extracted from the original component hierarchy, and it shouldn't be put to any other hierarchy, or the binding will be
- * broken. In particular, calling this method while the previous binding is still in effect isn't allowed.
+ * transferred in the opposite direction. The generated id object can be passed over RPC, and used with
+ * [ComponentDirectTransferId.getComponent] method on the frontend to realize the 'mirroring' component. The binding is only valid during
+ * the lifetime of the passed `disposable` parameter. During this time the source component is extracted from the original component
+ * hierarchy, and it shouldn't be put to any other hierarchy, or the binding will be broken. In particular, calling this method while the
+ * previous binding is still in effect isn't allowed.
  *
  * When used in monolith mode, the component returned by [ComponentDirectTransferId.getComponent] is just the original source component,
  * so no bridging of painting and input events is performed.
@@ -35,25 +39,17 @@ fun JComponent.setupTransfer(disposable: Disposable): ComponentDirectTransferId 
  * In monolith mode this just returns the original source component.
  *
  * @see JComponent.setupTransfer
- * @see ComponentDirectTransferId.getComponentWithDisposable
  */
 @ApiStatus.Internal
 @RequiresEdt
 fun ComponentDirectTransferId.getComponent(): JComponent? {
-  return getComponentWithDisposable()?.component
-}
-
-/**
- * Same as [ComponentDirectTransferId.getComponent], but also returns the disposable object, conveying the lifespan of the 'mirroring'.
- * It corresponds to the parameter passed to [JComponent.setupTransfer] (and is the same object in monolith mode).
- *
- * @see JComponent.setupTransfer
- * @see ComponentDirectTransferId.getComponent
- */
-@ApiStatus.Internal
-@RequiresEdt
-fun ComponentDirectTransferId.getComponentWithDisposable(): ComponentWithDisposable? {
-  return localValue ?: deserializeFromRpc(serializedValue)
+  return localValue?.let {
+    Wrapper().apply {
+      if (Disposer.tryRegister(it.disposable) { setContent(null) }) {
+        setContent(it.component)
+      }
+    }
+  } ?: deserializeFromRpc(serializedValue)
 }
 
 /**
@@ -68,3 +64,18 @@ data class ComponentDirectTransferId(val serializedValue: SerializedValue?, @Tra
 
 @ApiStatus.Internal
 class ComponentWithDisposable(val component: JComponent, val disposable: Disposable)
+
+private class Wrapper : JPanel(BorderLayout()) {
+  init {
+    isOpaque = false
+  }
+
+  fun setContent(content: Component?) {
+    removeAll()
+    if (content != null) {
+      add(content, BorderLayout.CENTER)
+    }
+    revalidate()
+    repaint()
+  }
+}
