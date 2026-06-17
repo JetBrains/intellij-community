e0f3b42164e3512278a25d797726ff7ff5f945f1
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2026-06-17 23:18:56 +0200

    tests [platform]: making `LogFilesCollectorTest` Windows-compatible
    GitOrigin-RevId: 5f62174f47692707989f0133e2c700135aeea2e6

--- platform/execution-impl/testSources/com/intellij/diagnostic/logging/LogFilesCollectorTest.kt
+++ platform/execution-impl/testSources/com/intellij/diagnostic/logging/LogFilesCollectorTest.kt
@@ -1,35 +1,31 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.diagnostic.logging
 
-import com.intellij.openapi.util.io.FileUtil
 import com.intellij.util.io.directoryContent
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import kotlin.io.path.pathString
+import kotlin.io.path.invariantSeparatorsPathString
 
 class LogFilesCollectorTest {
   @Test
   fun `collect files in root`() {
-    val logDir = directoryContent { 
+    val logDir = directoryContent {
       file("main.log")
       file("main2.log")
     }.generateInTempDir()
-    val root = logDir.pathString
+    val root = logDir.invariantSeparatorsPathString
 
     assertLogs("*")
     assertLogs("**/**")
-    assertLogs("$root/x.log")
-    
-    assertLogs("$root/main.log",
-               "$root/main.log")
-    assertLogs("$root/*.log",
-               "$root/main.log",
-               "$root/main2.log")
+    assertLogs("${root}/x.log")
+
+    assertLogs("${root}/main.log", "${root}/main.log")
+    assertLogs("${root}/*.log", "${root}/main.log", "${root}/main2.log")
   }
-  
+
   @Test
   fun `collect files in subdirectories`() {
-    val logDir = directoryContent { 
+    val logDir = directoryContent {
       dir("subDir1") {
         dir("deepSubDir") {
           file("file1.log")
@@ -42,23 +38,14 @@ class LogFilesCollectorTest {
         file("file2.log")
       }
     }.generateInTempDir()
-    val root = logDir.pathString
+    val root = logDir.invariantSeparatorsPathString
 
-    assertLogs("$root/*/*.log",
-               "$root/subDir1/file1.log",
-               "$root/subDir2/file1.log",
-               "$root/subDir2/file2.log")
-    assertLogs("$root/*/file1.log",
-               "$root/subDir1/file1.log",
-               "$root/subDir2/file1.log")
-    assertLogs("$root/**/file1.log",
-               "$root/subDir1/deepSubDir/file1.log",
-               "$root/subDir1/file1.log",
-               "$root/subDir2/file1.log")
+    assertLogs("${root}/*/*.log", "${root}/subDir1/file1.log", "${root}/subDir2/file1.log", "${root}/subDir2/file2.log")
+    assertLogs("${root}/*/file1.log", "${root}/subDir1/file1.log", "${root}/subDir2/file1.log")
+    assertLogs("${root}/**/file1.log", "${root}/subDir1/deepSubDir/file1.log", "${root}/subDir1/file1.log", "${root}/subDir2/file1.log")
   }
 
   private fun assertLogs(pathPattern: String, vararg expected: String) {
-    val actual = collectLogPaths(FileUtil.toSystemDependentName(pathPattern), true).toList().sorted()
-    Assertions.assertThatList(actual).isEqualTo(expected.toList())
+    assertThat(collectLogPaths(pathPattern, includeAll = true)).containsExactlyInAnyOrder(*expected)
   }
 }
