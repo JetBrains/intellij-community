a30d242e8f12fd0206ef3f7564acc5a401d2382e
Ilia Epelbaum <ilia.epelbaum@jetbrains.com>
2026-06-18 13:39:43 +0000

    DBE-26207: Specify the correct primary classloader
    Merge-request: IJ-MR-209080
    Merged-by: Ilia Epelbaum <ilia.epelbaum@jetbrains.com>
    
    (cherry picked from commit dfac26cc7d56f8e5ab56d9c6e0c9f2c687f3c109)
    
    IJ-CR-209514
    
    GitOrigin-RevId: 82a4f5d7c5e1d6264e1ac89e9809b4622fd828a1

--- grid/core-impl/src/extensions/ExtensionScriptsUtil.java
+++ grid/core-impl/src/extensions/ExtensionScriptsUtil.java
@@ -45,21 +45,6 @@ public final class ExtensionScriptsUtil {
   private ExtensionScriptsUtil() {
   }
 
-  //public static @Nullable IdeScriptEngine getEngineFor(@Nullable Project project,
-  //                                                     @Nullable PluginId pluginId,
-  //                                                     @NotNull Path file,
-  //                                                     @Nullable BiConsumer<String, Project> installPlugin) {
-  //  return getEngineFor(project, pluginId, file, installPlugin, true);
-  //}
-
-  //public static @Nullable IdeScriptEngine getEngineFor(@Nullable Project project,
-  //                                                     @Nullable PluginId pluginId,
-  //                                                     @NotNull Path file,
-  //                                                     @Nullable BiConsumer<String, Project> installPlugin,
-  //                                                     boolean showBalloon) {
-  //  return getEngineFor(project, getDefaultClassLoader(pluginId), file, installPlugin, showBalloon);
-  //}
-
   public static @Nullable ClassLoader getDefaultClassLoader(@Nullable PluginId pluginId) {
     IdeaPluginDescriptor plugin = PluginManagerCore.getPlugin(pluginId);
     plugin = plugin != null ? plugin : PluginManagerCore.getPlugin(PluginManagerCore.CORE_ID);
