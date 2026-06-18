f1744aef317ef8f4a1439a9e0e8a0aaa768bfe69
Frederik Haselmeier <frederik.haselmeier@jetbrains.com>
2026-06-18 16:13:25 +0200

    [kotlin] KTIJ-38050 Do not use light classes in KotlinGoToImplementationCommandCompletionProvider
    Use `DirectKotlinClassInheritorsSearch` directly, which also works for non-JVM classes.
    Also do not show the respective command completion for SAM interface instantiations because they do not show up by Kotlin's GotoImplementation implementation.
    
    ^KTIJ-38050 fixed
    
    GitOrigin-RevId: e1f263c713b21691087aa35d8e70f4f2de5c8a40

--- plugins/kotlin/completion/impl-k2/src/org/jetbrains/kotlin/idea/completion/impl/k2/contributors/commands/KotlinGoToImplementationCommandCompletionProvider.kt
+++ plugins/kotlin/completion/impl-k2/src/org/jetbrains/kotlin/idea/completion/impl/k2/contributors/commands/KotlinGoToImplementationCommandCompletionProvider.kt
@@ -2,14 +2,10 @@
 package org.jetbrains.kotlin.idea.completion.impl.k2.contributors.commands
 
 import com.intellij.codeInsight.completion.command.commands.AbstractGoToImplementationCompletionCommandProvider
-import com.intellij.psi.LambdaUtil
 import com.intellij.psi.PsiElement
-import com.intellij.psi.search.searches.ClassInheritorsSearch
-import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.parentOfType
-import org.jetbrains.kotlin.asJava.toFakeLightClass
-import org.jetbrains.kotlin.asJava.toLightClass
+import org.jetbrains.kotlin.idea.searching.inheritors.DirectKotlinClassInheritorsSearch
 import org.jetbrains.kotlin.idea.util.CommentSaver.Companion.tokenType
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtClass
@@ -17,12 +13,12 @@ import org.jetbrains.kotlin.psi.KtNamedDeclaration
 import org.jetbrains.kotlin.psi.psiUtil.containingClass
 
 internal class KotlinGoToImplementationCommandCompletionProvider : AbstractGoToImplementationCompletionCommandProvider() {
+
     override fun canGoToImplementation(element: PsiElement, offset: Int): Boolean {
         if (element.tokenType != KtTokens.IDENTIFIER) return false
         val member = element.parentOfType<KtNamedDeclaration>()
         val name = member?.nameIdentifier ?: return false
         val containingClass = member as? KtClass ?: member.containingClass() ?: return false
-        // TODO(KTIJ-38050): drop light classes
         val file = containingClass.containingKtFile
         val originalFile = file.originalFile
         val originalClass = if (originalFile == file) {
@@ -31,13 +27,11 @@ internal class KotlinGoToImplementationCommandCompletionProvider : AbstractGoToI
             PsiTreeUtil.findSameElementInCopy(containingClass, originalFile) ?: return false
         }
 
-        val lightClass = originalClass.toLightClass() ?: originalClass.toFakeLightClass()
-        if (ClassInheritorsSearch.search(lightClass, false)
-                .findFirst() == null && !(LambdaUtil.isFunctionalClass(lightClass) && ReferencesSearch.search(lightClass)
-                .findFirst() != null)
-        ) {
+        if (DirectKotlinClassInheritorsSearch.search(originalClass, originalClass.useScope).findFirst() == null) {
+            // If there are no direct inheritors, there are none at all
             return false
         }
+
         val fileDocument = element.containingFile.fileDocument
         return fileDocument.getLineNumber(name.textRange.startOffset) == fileDocument.getLineNumber(offset)
     }
