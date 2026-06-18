ed7d0b64fb9793ee1b97728ecad02b6c987629c7
Ilia Malakhov <ilia.malakhov@jetbrains.com>
2026-06-18 17:25:34 +0200

    IJPL-240707 [refactoring] create modules in tests using workspace model instead of writing modules.xml and iml files
    (cherry picked from commit b70f76e07dce6511a785710092d15af65c29fbf8)
    
    GitOrigin-RevId: e38c2639478a4ae37f1d098b1ff16b64d8bf05ac

--- platform/platform-tests/testSrc/com/intellij/openapi/roots/impl/VfsRefreshLoadingFilesToVfsTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/roots/impl/VfsRefreshLoadingFilesToVfsTest.kt
@@ -6,14 +6,24 @@ import com.intellij.ide.impl.ProjectUtil
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.impl.OnlyIndexableFilesAreLoadedIntoVfsOnDirectoryCreationTest.Companion.collectFilesLoadedIntoVfsBeforeListenersRuns
+import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.newvfs.NewVirtualFile
+import com.intellij.platform.backend.workspace.workspaceModel
+import com.intellij.platform.workspace.jps.entities.ContentRootEntity
+import com.intellij.platform.workspace.jps.entities.ExcludeUrlEntity
+import com.intellij.platform.workspace.jps.entities.InheritedSdkDependency
+import com.intellij.platform.workspace.jps.entities.ModuleEntity
+import com.intellij.platform.workspace.jps.entities.ModuleSourceDependency
+import com.intellij.platform.workspace.storage.url.VirtualFileUrlManager
 import com.intellij.testFramework.TestObservation
 import com.intellij.testFramework.VfsTestUtil
+import com.intellij.testFramework.junit5.RegistryKey
 import com.intellij.testFramework.junit5.TestApplication
 import com.intellij.testFramework.junit5.TestDisposable
 import com.intellij.testFramework.rules.TempDirectoryExtension
 import com.intellij.testFramework.useProjectAsync
+import com.intellij.workspaceModel.ide.NonPersistentEntitySource
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
@@ -22,12 +32,15 @@ import org.junit.jupiter.api.extension.RegisterExtension
 import java.nio.file.Path
 import kotlin.io.path.createDirectories
 import kotlin.io.path.invariantSeparatorsPathString
+import kotlin.io.path.name
 import kotlin.io.path.pathString
+import kotlin.io.path.relativeTo
 import kotlin.io.path.writeText
 import kotlin.time.Duration.Companion.seconds
 
+@RegistryKey("vfs.refresh.iterate.included.files.under.exclude", "true")
 @TestApplication
-class LocalHistoryChildrenCacheRegressionTest {
+class VfsRefreshLoadingFilesToVfsTest {
   @JvmField
   @RegisterExtension
   val rootDir: TempDirectoryExtension = TempDirectoryExtension()
@@ -39,8 +52,13 @@ class LocalHistoryChildrenCacheRegressionTest {
   fun `new content dir under excluded parent is loaded into vfs after refresh`(): Unit = runBlocking {
     stageNestedExcludedLayout()
 
-    withOpenedProject { _, rootVirtualFile ->
-      val generatedRoot = rootDir.rootPath.resolve("outer/build/generated")
+    withOpenedProject { project, rootVirtualFile ->
+      val outerRoot = rootDir.rootPath.resolve("outer")
+      val excludedRoot = outerRoot.resolve("build")
+      val generatedRoot = excludedRoot.resolve("generated")
+
+      addModuleWithContentRootUnderExplicitExclude(project, outerRoot, excludedRoot, generatedRoot)
+
       generatedRoot.createDirectories()
       generateFiles(generatedRoot, packagePrefix = "com/example")
 
@@ -51,10 +69,40 @@ class LocalHistoryChildrenCacheRegressionTest {
   }
 
   @Test
+  fun `new content dir under excluded parent is loaded into vfs after refresh when appear simultaneously`(): Unit = runBlocking {
+    stageNestedExcludedLayout()
+
+    withOpenedProject { project, rootVirtualFile ->
+      val root = rootDir.rootPath
+      val excludedDir = rootDir.rootPath.resolve("other/build")
+      val generatedRoot = excludedDir.resolve("inner/generated")
+
+      addModuleWithContentRootUnderExcludedPattern(project, root, excludedDir.name, generatedRoot)
+
+      generatedRoot.createDirectories()
+      generateFiles(generatedRoot, packagePrefix = "com/example")
+
+      delay(1.seconds)
+
+      val vfuManager = project.workspaceModel.getVirtualFileUrlManager()
+      val excludedDirVfu = vfuManager.findByUrl(excludedDir.toIdeUrl())
+      assertThat(excludedDirVfu).isNull()
+
+      val filesLoadedIntoVfs = collectFilesLoadedIntoVfsBeforeListenersRuns(rootVirtualFile, disposable)
+      assertSubtreeLoadedIntoVfs(filesLoadedIntoVfs,
+                                 rootVirtualFile,
+                                 relativeRoot = generatedRoot.relativeTo(root).pathString,
+                                 packagePrefix = "com/example")
+    }
+  }
+
+  @Test
   fun `new dir under content root is loaded into vfs after refresh`(): Unit = runBlocking {
     stageFlatContentLayout()
 
-    withOpenedProject { _, rootVirtualFile ->
+    withOpenedProject { project, rootVirtualFile ->
+      addModuleWithContentRoot(project, rootDir.rootPath.resolve("single"))
+
       val newPackageRoot = rootDir.rootPath.resolve("single/newpkg")
       newPackageRoot.createDirectories()
       generateFiles(newPackageRoot, packagePrefix = "")
@@ -66,6 +114,7 @@ class LocalHistoryChildrenCacheRegressionTest {
   }
 
   private suspend fun withOpenedProject(action: suspend (Project, VirtualFile) -> Unit) {
+    rootDir.newDirectoryPath(".idea") // forces project.basePath to match project root which may affect how VFS files are refreshed
     val options = OpenProjectTask { createModule = false }
     ProjectUtil.openOrImportAsync(rootDir.rootPath, options)!!.useProjectAsync { project ->
       TestObservation.awaitConfiguration(project)
@@ -142,22 +191,58 @@ class LocalHistoryChildrenCacheRegressionTest {
     }
   }
 
-  private fun stageNestedExcludedLayout() {
-    rootDir.newDirectoryPath(".idea")
-    rootDir.rootPath.resolve(".idea/modules.xml").writeText(NESTED_MODULES_XML)
-    rootDir.rootPath.resolve("outer.iml").writeText(OUTER_IML)
-    rootDir.rootPath.resolve("inner.iml").writeText(INNER_IML)
+  private suspend fun addModuleWithContentRoot(project: Project, contentRoot: Path) {
+    val urlManager = project.workspaceModel.getVirtualFileUrlManager()
+    project.workspaceModel.update("add single content root") { storage ->
+      storage.addEntity(ModuleEntity("single", moduleDependencies(), NonPersistentEntitySource) {
+        contentRoots = listOf(ContentRootEntity(contentRoot.toVirtualFileUrl(urlManager), emptyList(), NonPersistentEntitySource))
+      })
+    }
+  }
 
+  private suspend fun addModuleWithContentRootUnderExplicitExclude(
+    project: Project,
+    contentRoot: Path,
+    excludedRoot: Path,
+    nestedContentRoot: Path,
+  ) {
+    val urlManager = project.workspaceModel.getVirtualFileUrlManager()
+    project.workspaceModel.update("add content root under excluded dir") { storage ->
+      storage.addEntity(ModuleEntity("module", moduleDependencies(), NonPersistentEntitySource) {
+        contentRoots = listOf(
+          ContentRootEntity(contentRoot.toVirtualFileUrl(urlManager), emptyList(), NonPersistentEntitySource) {
+            excludedUrls = listOf(ExcludeUrlEntity(excludedRoot.toVirtualFileUrl(urlManager), NonPersistentEntitySource))
+          },
+          ContentRootEntity(nestedContentRoot.toVirtualFileUrl(urlManager), emptyList(), NonPersistentEntitySource),
+        )
+      })
+    }
+  }
+
+  private suspend fun addModuleWithContentRootUnderExcludedPattern(
+    project: Project,
+    contentRoot: Path,
+    excludedPattern: String,
+    nestedContentRoot: Path,
+  ) {
+    val urlManager = project.workspaceModel.getVirtualFileUrlManager()
+    project.workspaceModel.update("add content root under excluded pattern") { storage ->
+      storage.addEntity(ModuleEntity("module", moduleDependencies(), NonPersistentEntitySource) {
+        contentRoots = listOf(
+          ContentRootEntity(contentRoot.toVirtualFileUrl(urlManager), listOf(excludedPattern), NonPersistentEntitySource),
+          ContentRootEntity(nestedContentRoot.toVirtualFileUrl(urlManager), emptyList(), NonPersistentEntitySource),
+        )
+      })
+    }
+  }
+
+  private fun stageNestedExcludedLayout() {
     val buildDir = rootDir.rootPath.resolve("outer/build")
     buildDir.createDirectories()
     buildDir.resolve("placeholder.txt").writeText("seed\n")
   }
 
   private fun stageFlatContentLayout() {
-    rootDir.newDirectoryPath(".idea")
-    rootDir.rootPath.resolve(".idea/modules.xml").writeText(FLAT_MODULES_XML)
-    rootDir.rootPath.resolve("single.iml").writeText(SINGLE_IML)
-
     val contentDir = rootDir.rootPath.resolve("single")
     contentDir.createDirectories()
     contentDir.resolve("placeholder.txt").writeText("seed\n")
@@ -171,70 +256,11 @@ class LocalHistoryChildrenCacheRegressionTest {
   companion object {
     private const val SUBDIR_COUNT = 10
     private const val FILES_PER_SUBDIR = 20
-
-    private val NESTED_MODULES_XML = $$"""
-      <?xml version="1.0" encoding="UTF-8"?>
-      <project version="4">
-        <component name="ProjectModuleManager">
-          <modules>
-            <module fileurl="file://$PROJECT_DIR$/outer.iml" filepath="$PROJECT_DIR$/outer.iml" />
-            <module fileurl="file://$PROJECT_DIR$/inner.iml" filepath="$PROJECT_DIR$/inner.iml" />
-          </modules>
-        </component>
-      </project>
-    """.trimIndent()
-
-    private val FLAT_MODULES_XML = $$"""
-      <?xml version="1.0" encoding="UTF-8"?>
-      <project version="4">
-        <component name="ProjectModuleManager">
-          <modules>
-            <module fileurl="file://$PROJECT_DIR$/single.iml" filepath="$PROJECT_DIR$/single.iml" />
-          </modules>
-        </component>
-      </project>
-    """.trimIndent()
-
-    private val OUTER_IML = $$"""
-      <?xml version="1.0" encoding="UTF-8"?>
-      <module type="JAVA_MODULE" version="4">
-        <component name="NewModuleRootManager" inherit-compiler-output="true">
-          <exclude-output />
-          <content url="file://$MODULE_DIR$/outer">
-            <excludeFolder url="file://$MODULE_DIR$/outer/build" />
-          </content>
-          <orderEntry type="inheritedJdk" />
-          <orderEntry type="sourceFolder" forTests="false" />
-        </component>
-      </module>
-    """.trimIndent()
-
-    private val INNER_IML = $$"""
-      <?xml version="1.0" encoding="UTF-8"?>
-      <module type="JAVA_MODULE" version="4">
-        <component name="NewModuleRootManager" inherit-compiler-output="true">
-          <exclude-output />
-          <content url="file://$MODULE_DIR$/outer/build/generated">
-            <sourceFolder url="file://$MODULE_DIR$/outer/build/generated" isTestSource="false" />
-          </content>
-          <orderEntry type="inheritedJdk" />
-          <orderEntry type="sourceFolder" forTests="false" />
-        </component>
-      </module>
-    """.trimIndent()
-
-    private val SINGLE_IML = $$"""
-      <?xml version="1.0" encoding="UTF-8"?>
-      <module type="JAVA_MODULE" version="4">
-        <component name="NewModuleRootManager" inherit-compiler-output="true">
-          <exclude-output />
-          <content url="file://$MODULE_DIR$/single">
-            <sourceFolder url="file://$MODULE_DIR$/single" isTestSource="false" />
-          </content>
-          <orderEntry type="inheritedJdk" />
-          <orderEntry type="sourceFolder" forTests="false" />
-        </component>
-      </module>
-    """.trimIndent()
   }
 }
+
+private fun moduleDependencies() = listOf(InheritedSdkDependency, ModuleSourceDependency)
+
+private fun Path.toIdeUrl(): String = VfsUtilCore.pathToUrl(pathString)
+
+private fun Path.toVirtualFileUrl(urlManager: VirtualFileUrlManager) = urlManager.getOrCreateFromUrl(toIdeUrl())
