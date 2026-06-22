0e6f123b1124f475ddbfbadbd280bed48bdce4bc
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2026-06-22 18:04:17 +0400

    [kotlin] KTIJ-38334 Extract `findContextToAnalyze` to a new utility file in refactoring package
    (cherry picked from commit c130d4f74fbfc25d973087ef1faced3dabbf14e6)
    
    IJ-CR-212042
    
    GitOrigin-RevId: bc4c82ec26f550da73347c13a5113a910e6713f7

---
+++ plugins/kotlin/refactorings/kotlin.refactorings.k2/src/org/jetbrains/kotlin/idea/k2/refactoring/util/AnalyzeContextUtils.kt
@@ -0,0 +1,34 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+package org.jetbrains.kotlin.idea.k2.refactoring.util
+
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.KtClassBody
+import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.psi.KtFunctionLiteral
+import org.jetbrains.kotlin.psi.KtParameter
+import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.KtPropertyAccessor
+import org.jetbrains.kotlin.psi.psiUtil.parentsWithSelf
+
+/**
+ * Finds the nearest enclosing declaration that can be used as a context for a code fragment built from [this] expression.
+ */
+@ApiStatus.Internal
+fun KtExpression.findContextToAnalyze(): KtExpression? {
+    for (element in parentsWithSelf) {
+        when (element) {
+            is KtFunctionLiteral -> continue
+            is KtParameter -> continue
+            is KtPropertyAccessor -> continue
+            is KtProperty -> if (element.parent is KtClassBody) continue else return element
+            is KtFunction -> if (element.hasModifier(KtTokens.OVERRIDE_KEYWORD)) continue else return element
+            is KtDeclaration -> return element
+            else -> continue
+        }
+    }
+    return null
+}
