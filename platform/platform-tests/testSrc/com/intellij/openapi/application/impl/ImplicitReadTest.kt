ede1b96bc692c2da5b39b44b7a17a3a9bb717c1d
Alexey Kudravtsev <Alexey.Kudravtsev@jetbrains.com>
2026-06-22 16:14:48 +0200

    cleanup ImplicitReadTest: enable more tests
    GitOrigin-RevId: 3a7404dae74dc43f2c908c30670aef7c2038ebd1

--- platform/platform-tests/testSrc/com/intellij/openapi/application/impl/ImplicitReadTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/application/impl/ImplicitReadTest.kt
@@ -1,24 +1,25 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.application.impl
 
+import com.intellij.ide.IdeEventQueue
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.testFramework.junit5.TestApplication
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.condition.EnabledIfSystemProperty
+import org.junit.jupiter.api.assertNotNull
 import java.util.concurrent.CompletableFuture
 import javax.swing.SwingUtilities
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
-
 @TestApplication
 class ImplicitReadTest {
-  companion object {
-    const val DISABLE_IR_PROPERTY = "idea.disable.implicit.read.on.edt"
+  @BeforeEach
+  fun setup() {
+    assertNotNull(IdeEventQueue.getInstance()) // ensure invokeLaters would go through our IdeEventQueue
   }
 
   @Test
-  @EnabledIfSystemProperty(named = DISABLE_IR_PROPERTY, matches = "^true$", disabledReason = "Need this property set to enable functionality under test")
   fun implicitReadOnEDTIsDisabled() {
       val app = ApplicationManager.getApplication()
       val f = CompletableFuture<Boolean>()
@@ -29,7 +30,6 @@ class ImplicitReadTest {
   }
 
   @Test
-  @EnabledIfSystemProperty(named = DISABLE_IR_PROPERTY, matches = "^true$", disabledReason = "Need this property set to enable functionality under test")
   fun explicitReadWorkInReadActionOnEDT() {
     val app = ApplicationManager.getApplication()
     val f = CompletableFuture<Boolean>()
@@ -42,7 +42,6 @@ class ImplicitReadTest {
   }
 
   @Test
-  @EnabledIfSystemProperty(named = DISABLE_IR_PROPERTY, matches = "^true$", disabledReason = "Need this property set to enable functionality under test")
   fun explicitReadWorkInWriteActionOnEDT() {
     val app = ApplicationManager.getApplication()
     val f = CompletableFuture<Boolean>()
@@ -56,7 +55,6 @@ class ImplicitReadTest {
   }
 
   @Test
-  @EnabledIfSystemProperty(named = DISABLE_IR_PROPERTY, matches = "^true$", disabledReason = "Need this property set to enable functionality under test")
   fun implicitReadWorksInApplicationActionAlways() {
     val app = ApplicationManager.getApplication()
     val f = CompletableFuture<Boolean>()
