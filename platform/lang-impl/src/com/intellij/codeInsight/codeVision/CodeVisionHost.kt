59b580d8e6c84b13e37ba46f5448f76473ab8b33
Vojtech Balik <vojtech.balik@jetbrains.com>
2026-06-22 16:19:28 +0200

    tests [code-vision] do not attempt to enqueue code vision refresh on editorContextsChanged if the Editor is already disposed
    The action prevented by this change already has no real effect other than a warning that spams tests where editor is created and quickly disposed of. The warning comes from DebouncedUpdates queue in CodeVisionHost, specifically `com.intellij.util.ui.update.BaseUpdateQueue.queue`, which warns about and refuses tasks if the queue is canceled. The lifetime of this queue is the very same `editorLifetime` as in this change.
    
    
    (cherry picked from commit b858b09c57dcf382e69280d014c2d6721eeeb8d4)
    
    IJ-CR-215356
    
    GitOrigin-RevId: 302166cb9ba8d0fda96939bdd5ec9cc529425792

--- platform/lang-impl/src/com/intellij/codeInsight/codeVision/CodeVisionHost.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/codeVision/CodeVisionHost.kt
@@ -273,7 +273,9 @@ open class CodeVisionHost(val project: Project, protected val coroutineScope: Co
       override fun editorContextsChanged(event: EditorContextManager.ChangeEvent) {
         if (editor == event.editor) {
           application.invokeLater {
-            onContextChanged()
+            editorLifetime.executeIfAlive {
+              onContextChanged()
+            }
           }
         }
       }
