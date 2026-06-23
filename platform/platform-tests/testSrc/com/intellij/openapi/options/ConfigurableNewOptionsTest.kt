1af4c3c65abf676d3c085582a9f9dfebcee23179
Stanislav Alekseev <stanislav.alekseev@jetbrains.com>
2026-06-23 10:31:46 +0300

    [settingsDialog] IJPL-247005 replace plugin update handler with a noop for configurable testing
    GitOrigin-RevId: b28e3dea6de96e90cbe4b8dfa5266cd053974b08

--- platform/platform-tests/testSrc/com/intellij/openapi/options/ConfigurableNewOptionsTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/options/ConfigurableNewOptionsTest.kt
@@ -1,22 +1,50 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.options
 
+import com.intellij.ide.plugins.newui.PluginUiModel
+import com.intellij.ide.plugins.newui.PluginUpdatesService
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.UiWithModelAccess
 import com.intellij.openapi.application.WriteIntentReadAction
 import com.intellij.openapi.options.ex.ConfigurableExtensionPointUtil
 import com.intellij.openapi.options.ex.ConfigurableWrapper
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.updateSettings.impl.PluginUpdateHandler
+import com.intellij.openapi.updateSettings.impl.PluginUpdateHandlerProvider
+import com.intellij.openapi.updateSettings.impl.PluginUpdatesModel
 import com.intellij.testFramework.common.timeoutRunBlocking
 import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.testFramework.junit5.TestDisposable
+import com.intellij.testFramework.registerOrReplaceServiceInstance
 import com.intellij.ui.components.Badge
 import com.intellij.util.ui.UIUtil
 import kotlinx.coroutines.Dispatchers
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.awt.Component
+import java.util.function.Consumer
+import javax.swing.JComponent
 import javax.swing.JLabel
 import kotlin.test.assertTrue
 
 @TestApplication
 internal class ConfigurableNewOptionsTest {
+  @BeforeEach
+  fun registerNoopPluginUpdateHandler(@TestDisposable disposable: Disposable) {
+    ApplicationManager.getApplication().registerOrReplaceServiceInstance(
+      PluginUpdateHandlerProvider::class.java,
+      NoopPluginUpdateHandlerProvider,
+      disposable,
+    )
+  }
+
+  @AfterEach
+  fun drainPluginUpdates(): Unit = timeoutRunBlocking {
+    PluginUpdatesService.getInstance().awaitUpdates()
+  }
+
   @Test
   fun `leaf configurable NewOptions marker matches rendered new badges`() = timeoutRunBlocking(context = Dispatchers.UiWithModelAccess) {
     val leaves = mutableListOf<LeafConfigurable>()
@@ -127,4 +155,29 @@ internal class ConfigurableNewOptionsTest {
   }
 
   private data class LeafConfigurable(val configurable: UnnamedConfigurable, val path: List<String>)
+}
+
+private object NoopPluginUpdateHandlerProvider : PluginUpdateHandlerProvider {
+  override fun getPluginUpdateHandler(): PluginUpdateHandler = NoopPluginUpdateHandler
+}
+
+private object NoopPluginUpdateHandler : PluginUpdateHandler {
+  override suspend fun loadAndStorePluginUpdates(buildNumber: String?, indicator: ProgressIndicator?): PluginUpdatesModel =
+    PluginUpdatesModel(
+      pluginUpdates = emptyList(),
+      updatesFromCustomRepositories = emptyList(),
+      incompatiblePluginNames = emptyList(),
+      internalErrors = emptyMap(),
+    )
+
+  override suspend fun installUpdates(
+    updates: Collection<PluginUiModel>,
+    component: JComponent?,
+    finishCallback: Runnable?,
+    customRestarter: Consumer<Boolean>?,
+  ) {
+  }
+
+  override suspend fun ignorePluginUpdates() {
+  }
 }
