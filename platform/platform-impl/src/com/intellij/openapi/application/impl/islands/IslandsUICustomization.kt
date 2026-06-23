0ba770a3266cb2e0b2281b424726d6bfaadebf8c
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2026-06-23 22:16:22 +0200

    IJPL-221382 High Contrast theme doesn't support islands
    (cherry picked from commit 47cc14de0d33ed96d48e75d20af95d06032296be)
    
    IJ-CR-210704
    
    GitOrigin-RevId: a050dcc8f273f747e6a3adf9e716b0f153ff18e0

--- platform/platform-impl/src/com/intellij/openapi/application/impl/islands/IslandsUICustomization.kt
+++ platform/platform-impl/src/com/intellij/openapi/application/impl/islands/IslandsUICustomization.kt
@@ -932,6 +932,10 @@ internal class IslandsUICustomization : InternalUICustomization() {
   }
 
   private fun getLastOffset(component: JComponent): Point {
+    if (uiSettings.hideToolStripes) {
+      return Point()
+    }
+
     val rootPane = component.rootPane
 
     val componentStart = Point()
