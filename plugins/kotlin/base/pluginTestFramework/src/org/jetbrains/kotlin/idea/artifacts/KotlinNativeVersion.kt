5b4ac3914208d3d1aa5018b7e1cf4d9df4264a80
Aleksei.Cherepanov <aleksei.cherepanov@jetbrains.com>
2026-06-23 17:44:46 +0200

    KTIJ-39166 [kotlin] bump KGP version in KMP tests
    GitOrigin-RevId: 8c9d98b827600013f8ecf5704a4ef6d7c50b146a

--- plugins/kotlin/base/pluginTestFramework/src/org/jetbrains/kotlin/idea/artifacts/KotlinNativeVersion.kt
+++ plugins/kotlin/base/pluginTestFramework/src/org/jetbrains/kotlin/idea/artifacts/KotlinNativeVersion.kt
@@ -18,7 +18,7 @@ object KotlinNativeVersion {
     /** This field is automatically setup from project-module-updater.
      *  See [org.jetbrains.tools.model.updater.updateKGPVersionForKotlinNativeTests]
      */
-    private const val kotlinGradlePluginVersion: String = "2.4.0-dev-8449"
+    private const val kotlinGradlePluginVersion: String = "2.4.20-dev-7885"
 
     /** Return bootstrap version or version from properties file of specified Kotlin Gradle Plugin.
      *  Make sure localMaven has kotlin-gradle-plugin with required version for cooperative development environment.
