9d8eff3cabc5112e9747fd2f58706b1e95e1086f
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2026-06-23 22:29:52 +0200

    IJPL-245975 [platform]: adjusting the "do-not-add-to-Defender-exclusions" check
    GitOrigin-RevId: 52b9813eda9865649815d2e98e52122e53118e09

--- platform/platform-tests/testSrc/com/intellij/diagnostic/WindowsDefenderCheckerTest.java
+++ platform/platform-tests/testSrc/com/intellij/diagnostic/WindowsDefenderCheckerTest.java
@@ -1,7 +1,8 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.diagnostic;
 
 import com.intellij.testFramework.junit5.TestApplication;
+import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -9,23 +10,46 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static com.intellij.openapi.util.io.IoTestUtil.assumeWindows;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 @TestApplication
 public class WindowsDefenderCheckerTest {
+  private static final Condition<Path> UNTRUSTWORTHY = new Condition<>(
+    path -> WindowsDefenderChecker.getInstance().isUntrustworthyLocation(path),
+    "untrustworthy"
+  );
+
   @BeforeAll static void beforeAll() {
     assumeWindows();
   }
 
   @Test void defenderStatusDetection() {
-    assertNotNull(WindowsDefenderChecker.getInstance().isRealTimeProtectionEnabled());
+    assertThat(WindowsDefenderChecker.getInstance().isRealTimeProtectionEnabled()).isNotNull();
   }
 
   @Test void downloadDirDetection() {
     var downloadDir = Path.of(System.getProperty("user.home"), "Downloads");
     assumeTrue(Files.isDirectory(downloadDir));
-    assertTrue(WindowsDefenderChecker.getInstance().isUntrustworthyLocation(downloadDir.resolve("project")));
+    assertThat(downloadDir).is(UNTRUSTWORTHY);
+    assertThat(downloadDir.resolve("project")).is(UNTRUSTWORTHY);
+  }
+
+  @Test void otherUntrustworthyDirs() {
+    assertThat(Path.of("M:\\")).is(UNTRUSTWORTHY);
+    assertThat(Path.of("M:\\dir")).isNot(UNTRUSTWORTHY);
+
+    var tempVar = System.getenv("TEMP");
+    if (tempVar != null) {
+      var tempDir = Path.of(tempVar);
+      assertThat(tempDir).is(UNTRUSTWORTHY);
+      assertThat(tempDir.resolve("project")).is(UNTRUSTWORTHY);
+      assertThat(tempDir.getParent()).is(UNTRUSTWORTHY);
+    }
+
+    var userDir = Path.of(System.getProperty("user.home"));
+    assertThat(userDir).is(UNTRUSTWORTHY);
+    assertThat(userDir.getParent()).is(UNTRUSTWORTHY);
+    assertThat(userDir.resolve("project")).isNot(UNTRUSTWORTHY);
   }
 }
