9adcc7f8d3097dae8653ece971586d519bc73b09
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2026-06-24 20:13:50 +0200

    IJPL-248294 [diagnostics]: correcting broken main menu latency calculation
    (cherry picked from commit f40427e7110fb656d1fb93a1601bd69e643d68a6)
    
    IJ-CR-210172
    
    GitOrigin-RevId: f01dd9c12ad16be7b6e50ca41a2938313b386777

--- platform/platform-impl/src/com/intellij/platform/ide/menu/MacNativeActionMenu.kt
+++ platform/platform-impl/src/com/intellij/platform/ide/menu/MacNativeActionMenu.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.ide.menu
 
 import com.intellij.diagnostic.UILatencyLogger
@@ -56,7 +56,7 @@ internal fun createMacNativeActionMenu(context: DataContext?,
       logger<Menu>().error(e)
     }
     finally {
-      UILatencyLogger.logMainMenuLatency(System.currentTimeMillis() - menuPeer.openTimeMs);
+      UILatencyLogger.logMainMenuLatency(menuPeer.openTimeNs);
     }
   }
   menuPeer.listenPresentationChanges(presentation)
