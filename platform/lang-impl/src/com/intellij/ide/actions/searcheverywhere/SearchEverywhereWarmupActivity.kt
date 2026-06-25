1f5e6b4b7aa260a5de99f759b71d0bfecb5516a0
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2026-06-25 10:33:11 +0200

    [platform ui][se] IJPL-248298 Precompute the file-type and language filter icons off the EDT
    GitOrigin-RevId: 9dcfafa005a4b2b1843dda25e3f306e53f845134

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereWarmupActivity.kt
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereWarmupActivity.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.actions.searcheverywhere
 
+import com.intellij.ide.util.gotoByName.FileTypeRef
+import com.intellij.ide.util.gotoByName.LanguageRef
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
@@ -23,5 +25,11 @@ internal class SearchEverywhereWarmupActivity : ProjectActivity {
       .onFailure { LOG.debug("SE warmup: failed to preload contributor factories", it) }
     runCatching { SearchEverywhereMlContributorReplacement.getFirstExtension() }
       .onFailure { LOG.debug("SE warmup: failed to preload ML contributor replacement", it) }
+
+    // Eagerly compute the file-type and language filter icons off the EDT.
+    runCatching { FileTypeRef.forAllFileTypes().forEach { it.icon } }
+      .onFailure { LOG.debug("SE warmup: failed to preload file type icons", it) }
+    runCatching { LanguageRef.forAllLanguages().forEach { it.icon } }
+      .onFailure { LOG.debug("SE warmup: failed to preload language icons", it) }
   }
 }
