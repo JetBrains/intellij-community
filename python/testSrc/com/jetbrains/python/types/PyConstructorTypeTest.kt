8d614188045c2af156d9c2829acca569b72624fb
Petr <petr.golubev@jetbrains.com>
2026-06-25 13:52:18 +0200

    tests [python]: Move `PyConstructorTypeTest` to `PyPureTypingTestSuite`
    GitOrigin-RevId: 1f214594643533251da4b08460f9d3c71333804a

--- python/testSrc/com/jetbrains/python/types/PyConstructorTypeTest.kt
+++ python/testSrc/com/jetbrains/python/types/PyConstructorTypeTest.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.jetbrains.python
+package com.jetbrains.python.types
 
 import com.jetbrains.python.allure.Layers
 import com.jetbrains.python.allure.Subsystems
@@ -14,7 +14,7 @@ import com.jetbrains.python.inspections.unresolvedReference.PyUnresolvedReferenc
 
 @Subsystems.CodeInsight
 @Layers.Functional
-internal class PyConstructorTypeTest : PyInspectionTestCase() {
+class PyConstructorTypeTest : PyInspectionTestCase() {
   override fun getInspectionClass(): Class<PyAssertTypeInspection> = PyAssertTypeInspection::class.java
 
   override fun getAdditionalInspectionClasses(): List<Class<out LocalInspectionTool>> = listOf(
