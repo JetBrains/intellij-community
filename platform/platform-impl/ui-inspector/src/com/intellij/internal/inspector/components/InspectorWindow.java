9908e77728dce841392b7b86dedf49d516a5aeb7
Aleksey Pivovarov <aleksey.pivovarov@jetbrains.com>
2026-06-26 15:13:46 +0200

    IJPL-248551 platform: fix memory leak via UIInspector
    GitOrigin-RevId: 6f153618f6b29116dde5cc5b3ea3508770ce8830

--- platform/platform-impl/ui-inspector/src/com/intellij/internal/inspector/components/InspectorWindow.java
+++ platform/platform-impl/ui-inspector/src/com/intellij/internal/inspector/components/InspectorWindow.java
@@ -456,6 +456,7 @@ public final class InspectorWindow extends JDialog implements Disposable {
     uninstallAltKeyListener();
     DimensionService.getInstance().setSize(getDimensionServiceKey(), getSize(), null);
     DimensionService.getInstance().setLocation(getDimensionServiceKey(), getLocation(), null);
+    Disposer.dispose(myInspector);
     Disposer.dispose(myInspectorTable);
     super.dispose();
     // remove this object from the Disposer hierarchy manually here because this method could be called from Swing when it e.g., hides the popup and calls Window.dispose()
