08a367c51fd012f0b86630daa719cbd8d37d2955
Anna Kozlova <anna.kozlova@jetbrains.com>
2026-06-26 12:28:43 +0200

    [kotlin] provide api to stop searching for inheritors by the processor
    - api with the returned sequence produces all direct inheritors of a class and then can stop. It might mean that additional index access would be done e.g. for java when we actually don't need more results anyway.
    
    KTIJ-39103
    
    GitOrigin-RevId: 72efbc47f4deeccd8911b12319d206deff3cf249

--- plugins/kotlin/kotlin.searching/src/org/jetbrains/kotlin/idea/searching/inheritors/kotlinSearchUtil.kt
+++ plugins/kotlin/kotlin.searching/src/org/jetbrains/kotlin/idea/searching/inheritors/kotlinSearchUtil.kt
@@ -2,6 +2,7 @@
 package org.jetbrains.kotlin.idea.searching.inheritors
 
 import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.application.runReadActionBlocking
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
@@ -10,6 +11,8 @@ import com.intellij.psi.search.searches.ClassInheritorsSearch
 import com.intellij.psi.search.searches.OverridingMethodsSearch
 import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
+import com.intellij.util.CommonProcessors
+import com.intellij.util.Processor
 import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
 import com.intellij.util.mappingNotNull
 import org.jetbrains.kotlin.analysis.api.analyze
@@ -18,13 +21,12 @@ import org.jetbrains.kotlin.analysis.api.symbols.KaSymbolModality
 import org.jetbrains.kotlin.analysis.api.symbols.KaValueParameterSymbol
 import org.jetbrains.kotlin.asJava.unwrapped
 import org.jetbrains.kotlin.idea.base.analysis.KotlinUastOutOfCodeBlockModificationTracker
-import org.jetbrains.kotlin.idea.search.ExpectActualUtils
-import org.jetbrains.kotlin.idea.search.ExpectActualUtils.actualsForExpect
 import org.jetbrains.kotlin.idea.searching.kmp.findAllActualForExpect
 import org.jetbrains.kotlin.psi.KtCallableDeclaration
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
 import java.util.ArrayDeque
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Returns a set of PsiMethods/KtFunctions/KtProperties which "deep" override current [this] declaration.
@@ -43,14 +45,22 @@ fun KtCallableDeclaration.findAllOverridings(searchScope: SearchScope = runReadA
 }
 
 @RequiresBackgroundThread(generateAssertion = false)
-fun KtCallableDeclaration.hasAnyOverridings(): Boolean =
-    CachedValuesManager.getCachedValue(this) {
-        val hasAnyInheritors = findAllOverridings().firstOrNull() != null
+fun KtCallableDeclaration.hasAnyOverridings(): Boolean {
+    fun hasFound(): Boolean {
+        val findProcessor = CommonProcessors.FindFirstProcessor<PsiElement>()
+        processAllOverridings(findProcessor)
+        val value = findProcessor.isFound
+        return value
+    }
+
+    if (!isPhysical) return hasFound()
+    return CachedValuesManager.getCachedValue(this) {
         CachedValueProvider.Result.create(
-            hasAnyInheritors,
+            hasFound(),
             KotlinUastOutOfCodeBlockModificationTracker.getInstance(project)
         )
     }
+}
 
 @RequiresBackgroundThread(generateAssertion = false)
 fun KtCallableDeclaration.hasAnyActuals(): Boolean =
@@ -90,7 +100,7 @@ fun KtCallableDeclaration.findHierarchyWithSiblings(searchScope: SearchScope = r
 } 
 
 private fun KtCallableDeclaration.findAllOverridings(withFullHierarchy: Boolean, searchScope: SearchScope): Sequence<PsiElement> {
-    if (runReadAction { containingClassOrObject !is KtClass }) return emptySequence()
+    if (runReadActionBlocking { containingClassOrObject !is KtClass }) return emptySequence()
 
     val queue = ArrayDeque<PsiElement>()
     val visited = HashSet<PsiElement>()
@@ -105,14 +115,14 @@ private fun KtCallableDeclaration.findAllOverridings(withFullHierarchy: Boolean,
             }
             when (currentMethod) {
                 is KtCallableDeclaration -> {
-                    val isFinal = runReadAction {
+                    val isFinal = runReadActionBlocking {
                         analyze(currentMethod) {
                             val symbol = currentMethod.symbol
                             ((symbol as? KaValueParameterSymbol)?.generatedPrimaryConstructorProperty ?: symbol).modality == KaSymbolModality.FINAL
                         }
                     }
                     if (isFinal) continue
-                    DirectKotlinOverridingCallableSearch.search(currentMethod, searchScope).asIterable().forEach {
+                    DirectKotlinOverridingCallableSearch.search(currentMethod, searchScope).forEach {
                         queue.offer(it)
                     }
                     if (withFullHierarchy) {
@@ -128,7 +138,6 @@ private fun KtCallableDeclaration.findAllOverridings(withFullHierarchy: Boolean,
                 is PsiMethod -> {
                     OverridingMethodsSearch.search(currentMethod, searchScope,true)
                         .mappingNotNull { it.unwrapped }
-                        .asIterable()
                         .forEach { queue.offer(it) }
                     if (withFullHierarchy) {
                         currentMethod.findSuperMethods(true)
@@ -139,6 +148,102 @@ private fun KtCallableDeclaration.findAllOverridings(withFullHierarchy: Boolean,
             }
         }
     }
+}
+
+/**
+ * If processor returns `false`, the search is stopped and the result is `false`.
+ */
+@RequiresBackgroundThread(generateAssertion = false)
+fun KtCallableDeclaration.processAllOverridings(
+    processor: Processor<PsiElement>,
+    searchScope: SearchScope = runReadActionBlocking { useScope },
+): Boolean {
+    return processAllOverridings(withFullHierarchy = false, processor, searchScope)
+}
+
+/**
+ * Feed a set of PsiMethods/KtFunctions/KtProperties which belong to the hierarchy of a function and all its siblings to the processor.
+ *
+ * Example:
+ * ```
+ * interface A { fun f() }
+ * interface B { fun f() }
+ * interface C : A, B { override fun f() }
+ * ```
+ * Hierarchy for B.f contains both A.f and C.f
+ */
+@RequiresBackgroundThread(generateAssertion = false)
+fun KtCallableDeclaration.processHierarchyWithSiblings(
+    processor: Processor<PsiElement>,
+    searchScope: SearchScope = runReadActionBlocking { useScope }
+): Boolean {
+    return processAllOverridings(withFullHierarchy = true, processor, searchScope)
+}
+
+
+private fun KtCallableDeclaration.processAllOverridings(
+    withFullHierarchy: Boolean,
+    processor: Processor<PsiElement>,
+    searchScope: SearchScope,
+): Boolean {
+    if (runReadActionBlocking { containingClassOrObject !is KtClass }) return true
+
+    val queue = ArrayDeque<PsiElement>()
+    val visited = HashSet<PsiElement>()
+
+    queue.add(this)
+    while (!queue.isEmpty()) {
+        val currentMethod = queue.poll()
+        when (currentMethod) {
+            is KtCallableDeclaration -> {
+                val isFinal = runReadActionBlocking {
+                    analyze(currentMethod) {
+                        val symbol = currentMethod.symbol
+                        ((symbol as? KaValueParameterSymbol)?.generatedPrimaryConstructorProperty
+                            ?: symbol).modality == KaSymbolModality.FINAL
+                    }
+                }
+                if (isFinal) continue
+                if (!DirectKotlinOverridingCallableSearch.search(currentMethod, searchScope).forEach(Processor<PsiElement> {
+                        if (!visited.add(it)) return@Processor true
+                        queue.offer(it)
+                        processor.process(it)
+                    })) return false
+                if (withFullHierarchy) {
+                    val overridden = analyze(currentMethod) {
+                        val ktCallableSymbol = currentMethod.symbol as? KaCallableSymbol ?: return@analyze emptyList()
+                        ktCallableSymbol.directlyOverriddenSymbols.mapNotNull { it.psi }.toList()
+                    }
+                    for (psi in overridden) {
+                        if (!visited.add(psi)) continue
+                        queue.offer(psi)
+                        if (!processor.process(psi)) return false
+                    }
+                }
+            }
+
+            is PsiMethod -> {
+                if (!OverridingMethodsSearch.search(currentMethod, searchScope, true)
+                        .mappingNotNull { it.unwrapped }
+                        .forEach(Processor<PsiElement> {
+                            if (!visited.add(it)) return@Processor true
+                            queue.offer(it)
+                            processor.process(it)
+                        })
+                ) return false
+                if (withFullHierarchy) {
+                    for (superMethod in currentMethod.findSuperMethods(true)) {
+                        val unwrapped = superMethod.unwrapped ?: continue
+                        if (!visited.add(unwrapped)) continue
+                        queue.offer(unwrapped)
+                        if (!processor.process(unwrapped)) return false
+                    }
+                }
+            }
+        }
+
+    }
+    return true
 }
 
 /**
@@ -171,13 +276,13 @@ fun PsiElement.findAllInheritors(searchScope: SearchScope = useScope): Sequence<
             }
             when (currentClass) {
                 is KtClass -> {
-                    val isFinal = runReadAction {
+                    val isFinal = runReadActionBlocking {
                         !currentClass.isEnum() && analyze(currentClass) { // allow searching for enum constants in enum but don't bother about other final classes
                             currentClass.symbol.modality == KaSymbolModality.FINAL
                         }
                     }
                     if (isFinal) continue
-                    DirectKotlinClassInheritorsSearch.search(currentClass, searchScope).asIterable().forEach {
+                    DirectKotlinClassInheritorsSearch.search(currentClass, searchScope).forEach {
                         queue.offer(it)
                     }
                 }
@@ -185,7 +290,6 @@ fun PsiElement.findAllInheritors(searchScope: SearchScope = useScope): Sequence<
                 is PsiClass -> {
                     ClassInheritorsSearch.search(currentClass, searchScope, /* checkDeep = */ false)
                         .mappingNotNull { it.unwrapped }
-                        .asIterable()
                         .forEach {
                             queue.offer(it)
                         }
@@ -202,12 +306,79 @@ fun PsiElement.findAllInheritors(searchScope: SearchScope = useScope): Sequence<
 fun KtClass.findAllInheritors(searchScope: SearchScope = useScope): Sequence<PsiElement> =
     (this as PsiElement).findAllInheritors(searchScope)
 
+/**
+ * If processor returns `false`, the search is stopped and the result is `false`.
+ */
 @RequiresBackgroundThread(generateAssertion = false)
-fun KtClass.hasAnyInheritors(): Boolean =
-    CachedValuesManager.getCachedValue(this) {
-        val hasAnyInheritors = findAllInheritors().firstOrNull() != null
+fun PsiElement.processAllInheritors(processor: Processor<PsiElement>, searchScope: SearchScope = useScope): Boolean {
+    val queue = ArrayDeque<PsiElement>()
+    val visited = HashSet<PsiElement>()
+
+    queue.add(this)
+
+    while (!queue.isEmpty()) {
+        val currentClass = queue.poll()
+
+        when (currentClass) {
+            is KtClass -> {
+                val isFinal = runReadActionBlocking {
+                    !currentClass.isEnum() && analyze(currentClass) { // allow searching for enum constants in enum but don't bother about other final classes
+                        currentClass.symbol.modality == KaSymbolModality.FINAL
+                    }
+                }
+                if (isFinal) continue
+                if (!DirectKotlinClassInheritorsSearch.search(currentClass, searchScope).forEach(Processor<PsiElement> {
+                        if (!visited.add(it)) return@Processor true
+                        queue.offer(it)
+                        processor.process(it)
+                    })) {
+                    return false
+                }
+            }
+
+            is PsiClass -> {
+                if (!ClassInheritorsSearch.search(currentClass, searchScope, /* checkDeep = */ false)
+                        .mappingNotNull { it.unwrapped }
+                        .forEach(Processor<PsiElement> {
+                            if (!visited.add(it)) return@Processor true
+                            queue.offer(it)
+                            processor.process(it)
+                        })
+                ) {
+                    return false
+                }
+            }
+        }
+    }
+    return true
+}
+
+@RequiresBackgroundThread(generateAssertion = false)
+fun KtClass.hasAnyInheritors(): Boolean {
+    fun hasFound(): Boolean {
+        val findProcessor = CommonProcessors.FindFirstProcessor<PsiElement>()
+        processAllInheritors(findProcessor)
+        val found = findProcessor.isFound
+        return found
+    }
+
+    if (!isPhysical) return hasFound()
+    return CachedValuesManager.getCachedValue(this) {
         CachedValueProvider.Result.create(
-            hasAnyInheritors,
+            hasFound(),
             KotlinUastOutOfCodeBlockModificationTracker.getInstance(project)
         )
     }
+}
+
+/**
+ * Counts processed elements and stops once [maxCount] of them have been seen.
+ * Only the count is retained; elements are not collected.
+ */
+class LimitedCountingProcessor(private val maxCount: Int) : Processor<PsiElement> {
+    private val count = AtomicInteger(0)
+
+    override fun process(t: PsiElement?): Boolean = count.incrementAndGet() < maxCount
+
+    fun getCount(): Int = count.get()
+}
