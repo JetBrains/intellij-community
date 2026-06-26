f5b7dc12bb0edbe536730db013a27a42b81c57bc
Mihail Buryakov <mihail.buryakov@jetbrains.com>
2026-06-26 14:53:18 +0400

    IJPL-172902 fix spi depdendency injection problems
    GitOrigin-RevId: f6e61b96c735c0e3f8fb51d6f3cb16be90a27728

---
+++ platform/ijent/impl/src/com/intellij/platform/ijent/community/impl/LoginShellEnvVarModeProviderSpiForwarder.kt
@@ -0,0 +1,28 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.platform.ijent.community.impl
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.platform.eel.EelMachine
+import com.intellij.platform.ijent.LoginShellEnvVarMode
+import com.intellij.platform.ijent.LoginShellEnvVarModeProvider
+
+/**
+ * `ServiceLoader`-discovered SPI forwarder for [LoginShellEnvVarModeProvider].
+ *
+ * The real implementation (e.g. `LoginShellEnvVarModeProviderImpl` in
+ * `intellij.platform.ijent.community.ui`) is registered as an `applicationService` and looked up
+ * through IntelliJ's service container, which crosses plugin-classloader boundaries unlike
+ * `ServiceLoader`.
+ *
+ * Registered in `META-INF/services/com.intellij.platform.ijent.LoginShellEnvVarModeProvider`.
+ */
+internal class LoginShellEnvVarModeProviderSpiForwarder : LoginShellEnvVarModeProvider {
+  override fun get(eelMachine: EelMachine): LoginShellEnvVarMode =
+    delegate()?.get(eelMachine) ?: LoginShellEnvVarMode.LOGIN_INTERACTIVE
+
+  private fun delegate(): LoginShellEnvVarModeProvider? {
+    val app = ApplicationManager.getApplication() ?: return null
+    val service = app.getService(LoginShellEnvVarModeProvider::class.java) ?: return null
+    return service.takeUnless { it === this }
+  }
+}
