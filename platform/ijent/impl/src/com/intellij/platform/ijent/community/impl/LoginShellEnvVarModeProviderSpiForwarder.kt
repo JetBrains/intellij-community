a9bc3e238ce4253e568db0de56a5904ff57fae9f
Mihail Buryakov <mihail.buryakov@jetbrains.com>
2026-06-28 02:35:15 +0400

    cleanup minor improvements after code review
    GitOrigin-RevId: bd50cea8f6c7cd7fda1b211502f20eb754b5c27e

--- platform/ijent/impl/src/com/intellij/platform/ijent/community/impl/LoginShellEnvVarModeProviderSpiForwarder.kt
+++ platform/ijent/impl/src/com/intellij/platform/ijent/community/impl/LoginShellEnvVarModeProviderSpiForwarder.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.ijent.community.impl
 
-import com.intellij.openapi.components.service
 import com.intellij.openapi.components.serviceOrNull
 import com.intellij.platform.eel.EelMachine
 import com.intellij.platform.ijent.LoginShellEnvVarMode
