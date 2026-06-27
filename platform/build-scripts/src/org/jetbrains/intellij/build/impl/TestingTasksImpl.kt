cbc6649aa52d384d4385f8eccf8496e3b5205c89
Evgenii Ilichev <evgenii.ilichev@jetbrains.com>
2026-06-27 22:40:54 +0200

    [MRI-4591] switch intellij.kotlin.onboarding.tests
    GitOrigin-RevId: 714364849f43e0d31c88779796a3e35cf7c7a42e

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
@@ -1522,4 +1522,5 @@ private val COMMUNITY_AGGREGATOR_BAZEL_MIGRATED_MODULES = listOf(
   "intellij.java.jshell.protocol.tests",
   "intellij.javaFX.tests",
   "intellij.kotlin.gradle.tooling.impl.tests",
+  "intellij.kotlin.onboarding.tests",
 )
