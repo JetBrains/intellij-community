9ed032417871ae858cc5eb4d44b4249f0c074a46
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2026-06-27 16:24:32 +0200

    IJPL-248623 drop awb exp site
    GitOrigin-RevId: 6e927d70993910671a90df88cfd10496e4587e2b

--- plugins/agent-workbench/site/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Static landing site. Build output goes to `dist/`.
-export default defineConfig({
-  plugins: [react()],
-})
