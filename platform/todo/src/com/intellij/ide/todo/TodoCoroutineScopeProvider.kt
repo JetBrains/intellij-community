45ae6f5d1819f1e4bffa28fcfb5eff9e1076f248
Milica Sladakovic <milica.sladakovic@jetbrains.com>
2026-06-29 19:21:17 +0200

    [todo] IJPL-246417 Make `TodoCoroutineScopeProvider` a light service
    Per Quodana: Zero Tolerance test failure.
    
    GitOrigin-RevId: bc36c8d29ad7ee16d3ca90033c1e910b3d587683

--- platform/todo/src/com/intellij/ide/todo/TodoCoroutineScopeProvider.kt
+++ platform/todo/src/com/intellij/ide/todo/TodoCoroutineScopeProvider.kt
@@ -1,10 +1,12 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.todo
 
+import com.intellij.openapi.components.Service
 import kotlinx.coroutines.CoroutineScope
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
+@Service(Service.Level.PROJECT)
 internal class TodoCoroutineScopeProvider(
   val coroutineScope: CoroutineScope,
 )
