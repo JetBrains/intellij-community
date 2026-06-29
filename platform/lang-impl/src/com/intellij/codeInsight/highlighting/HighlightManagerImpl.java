9819d991a19818f20071c23ed78d7fb2dce9e2e0
Alexey Kudravtsev <Alexey.Kudravtsev@jetbrains.com>
2026-06-29 15:26:50 +0200

    cleanup highlight manager: inline unnecessary methods
    GitOrigin-RevId: ee14709789709cb69dc64eeb5889d95ca5efc2bc

--- platform/lang-impl/src/com/intellij/codeInsight/highlighting/HighlightManagerImpl.java
+++ platform/lang-impl/src/com/intellij/codeInsight/highlighting/HighlightManagerImpl.java
@@ -79,8 +79,7 @@ public final class HighlightManagerImpl extends HighlightManager {
 
   public @NotNull RangeHighlighter @NotNull [] getHighlighters(@NotNull Editor editor) {
     Set<RangeHighlighter> highlighters = getEditorHighlighters(editor, false);
-    if (highlighters == null) return RangeHighlighter.EMPTY_ARRAY;
-    return highlighters.toArray(RangeHighlighter.EMPTY_ARRAY);
+    return highlighters == null ? RangeHighlighter.EMPTY_ARRAY : highlighters.toArray(RangeHighlighter.EMPTY_ARRAY);
   }
 
   @Override
@@ -164,7 +163,7 @@ public final class HighlightManagerImpl extends HighlightManager {
                                              int end,
                                              @Nullable TextAttributes forcedAttributes,
                                              @Nullable TextAttributesKey attributesKey,
-                                             int flags,
+                                             @HideFlags int flags,
                                              @Nullable Collection<? super RangeHighlighter> outHighlighters,
                                              @Nullable Color scrollMarkColor) {
     MarkupModelEx markupModel = (MarkupModelEx)editor.getMarkupModel();
@@ -266,22 +265,12 @@ public final class HighlightManagerImpl extends HighlightManager {
                                       @NotNull TextAttributesKey attributesKey,
                                       boolean hideByTextChange,
                                       @Nullable Collection<? super RangeHighlighter> outHighlighters) {
-    addOccurrenceHighlights(editor, elements, null, attributesKey, hideByTextChange, outHighlighters);
-  }
-
-  private void addOccurrenceHighlights(@NotNull Editor editor,
-                                      PsiElement @NotNull [] elements,
-                                      @Nullable TextAttributes attributes,
-                                      @Nullable TextAttributesKey attributesKey,
-                                      boolean hideByTextChange,
-                                      @Nullable Collection<? super RangeHighlighter> outHighlighters) {
     if (elements.length == 0 || editor instanceof ImaginaryEditor) return;
     int flags = HIDE_BY_ESCAPE;
     if (hideByTextChange) {
       flags |= HIDE_BY_TEXT_CHANGE;
     }
 
-    Color scrollMarkColor = getScrollMarkColor(attributes, editor.getColorsScheme());
     editor = InjectedLanguageEditorUtil.getTopLevelEditor(editor);
 
     for (PsiElement element : elements) {
@@ -290,7 +279,7 @@ public final class HighlightManagerImpl extends HighlightManager {
       addOccurrenceHighlight(editor,
                              trimOffsetToDocumentSize(editor, range.getStartOffset()),
                              trimOffsetToDocumentSize(editor, range.getEndOffset()),
-                             attributes, attributesKey, flags, outHighlighters, scrollMarkColor);
+                             null, attributesKey, flags, outHighlighters, null);
     }
   }
 
@@ -370,9 +359,10 @@ public final class HighlightManagerImpl extends HighlightManager {
     }
 
     private void requestHideHighlights(@NotNull DataContext dataContext) {
-      final Editor editor = ReadAction.compute(() -> CommonDataKeys.EDITOR.getData(dataContext));
-      if (editor == null) return;
-      hideHighlights(editor, HIDE_BY_ANY_KEY);
+      Editor editor = ReadAction.computeBlocking(() -> CommonDataKeys.EDITOR.getData(dataContext));
+      if (editor != null) {
+        hideHighlights(editor, HIDE_BY_ANY_KEY);
+      }
     }
   }
 }
