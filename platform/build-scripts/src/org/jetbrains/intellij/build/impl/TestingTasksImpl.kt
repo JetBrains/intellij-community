a6e41b3df181c2b9b58069b5101380914e91e371
Evgenii Ilichev <evgenii.ilichev@jetbrains.com>
2026-06-30 20:13:13 +0200

    [MRI-4591] switch intellij.platform.settings.local.tests
    Run the local settings tests through their protected Bazel target and exclude the module from the community aggregator JPS run once Bazel owns it.
    
    GitOrigin-RevId: b3878fe4b4bccb7560d90b63f83d5f0ff23ecf62

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
@@ -1599,4 +1599,5 @@ private val COMMUNITY_AGGREGATOR_BAZEL_MIGRATED_MODULES = listOf(
   "intellij.platform.searchEverywhere.backend.tests",
   "intellij.platform.searchEverywhere.frontend.tests",
   "intellij.platform.serviceContainer.tests",
+  "intellij.platform.settings.local.tests",
 )
