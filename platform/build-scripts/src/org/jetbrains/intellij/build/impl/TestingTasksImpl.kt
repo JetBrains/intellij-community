6c389714be54366b1bc8e8e1d8457fa916793a4f
Evgenii Ilichev <evgenii.ilichev@jetbrains.com>
2026-06-30 20:10:25 +0200

    [MRI-4591] switch intellij.platform.runtime.repository.tests
    Run the runtime repository tests through their protected Bazel target and exclude the module from the community aggregator JPS run once Bazel owns it.
    
    GitOrigin-RevId: 3e1484be4592b18cd1416644c11aec36cdd5d25e

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
@@ -1595,4 +1595,5 @@ private val COMMUNITY_AGGREGATOR_BAZEL_MIGRATED_MODULES = listOf(
   "intellij.platform.problemView.backend.tests",
   "intellij.platform.problemView.ui.tests",
   "intellij.platform.runtime.product.tests",
+  "intellij.platform.runtime.repository.tests",
 )
