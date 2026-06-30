9ae42118994ed7fd3db67e3ff338fea652c24e89
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2026-06-30 18:04:53 +0200

    [java] Fix false positive in marked for removal inspection
    #IDEA-391095 Fixed
    
    GitOrigin-RevId: a67c8c4ba621973ee7aa39da37a8397947625667

--- jvm/jvm-analysis-kotlin-tests-k2/testSrc/com/intellij/codeInspection/tests/kotlin/K2MarkedForRemovalInspectionTest.kt
+++ jvm/jvm-analysis-kotlin-tests-k2/testSrc/com/intellij/codeInspection/tests/kotlin/K2MarkedForRemovalInspectionTest.kt
@@ -17,4 +17,32 @@ class K2MarkedForRemovalInspectionTest : KotlinMarkedForRemovalInspectionTest() 
       }
     """.trimIndent())
   }
+
+  fun `test call of override of non-deprecated interface method is not reported`() {
+    myFixture.addClass("""
+      package test;
+      public interface A {
+        void foo();
+      }
+    """.trimIndent())
+    myFixture.addClass("""
+      package test;
+      @Deprecated(forRemoval = true)
+      public class B implements A {
+        @Override public void foo() { }
+        public void bar() { }
+      }
+    """.trimIndent())
+    myFixture.addClass("""
+      package test;
+      public class C extends B { }
+    """.trimIndent())
+    myFixture.testHighlighting(JvmLanguage.KOTLIN, """
+      package test
+      fun test(c: C) {
+        c.foo()
+        c.<error descr="'test.B' is deprecated and marked for removal">bar</error>()
+      }
+    """.trimIndent())
+  }
 }
