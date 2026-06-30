b0bd800a6834f5f75260c90ebe1ef31b433e2820
Evgenii Ilichev <evgenii.ilichev@jetbrains.com>
2026-06-30 20:11:51 +0200

    [MRI-4591] switch intellij.platform.searchEverywhere.frontend.tests
    Run the Search Everywhere frontend tests through their protected Bazel target and exclude the module from the community aggregator JPS run once Bazel owns it.
    
    GitOrigin-RevId: 701abe5732bc92035d505091fac7d6360fe58f1d

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
@@ -1597,4 +1597,5 @@ private val COMMUNITY_AGGREGATOR_BAZEL_MIGRATED_MODULES = listOf(
   "intellij.platform.runtime.product.tests",
   "intellij.platform.runtime.repository.tests",
   "intellij.platform.searchEverywhere.backend.tests",
+  "intellij.platform.searchEverywhere.frontend.tests",
 )
