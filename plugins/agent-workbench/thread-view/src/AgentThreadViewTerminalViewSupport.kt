41d0f86f534f16a41e94145fb836b8ef53e6fc8a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2026-06-30 20:34:05 +0200

    IJPL-248623 Move Agent Workbench sessions
    IJ-MR-184958 IJ-MR-184993 IJ-MR-179029 IJ-MR-184126 IJ-MR-181153 IJ-MR-146078 IJ-MR-175479 IJ-MR-186058 IJ-MR-193195 IJ-MR-196957 IJ-MR-199124 IJ-MR-197441 IJ-MR-204135 IJ-MR-204674 IJ-MR-205883 IJ-MR-208539
    
    GitOrigin-RevId: 6b9571aff16f673a11c75b4825546554a2f5c5a2

--- plugins/agent-workbench/thread-view/src/AgentThreadViewTerminalViewSupport.kt
+++
@@ -1,9 +0,0 @@
-// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.agent.workbench.thread.view
-
-import org.jetbrains.annotations.ApiStatus
-
-@ApiStatus.Internal
-interface AgentThreadViewDisposableController {
-  fun dispose()
-}
