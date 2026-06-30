3d6f4808746b86e6693c89cdfb8286becc82798a
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2026-06-30 16:06:00 +0200

    [devkit] IJPL-246844 Fix several structure/conventions/Patronus tests
    GitOrigin-RevId: b2a6d347e062b8eb590e7eddb60704ff78ccf862

--- plugins/devkit/intellij.devkit.gradle/src/DevKitGradleProjectResolverExtension.kt
+++ plugins/devkit/intellij.devkit.gradle/src/DevKitGradleProjectResolverExtension.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.devkit.gradle
 
-import org.jetbrains.idea.devkit.gradle.tooling.IntelliJPlatformAuxiliaryArtifactProvider
+import com.intellij.devkit.gradle.tooling.IntelliJPlatformAuxiliaryArtifactProvider
 import org.jetbrains.plugins.gradle.service.project.AbstractProjectResolverExtension
 
 internal class DevKitGradleProjectResolverExtension : AbstractProjectResolverExtension() {
