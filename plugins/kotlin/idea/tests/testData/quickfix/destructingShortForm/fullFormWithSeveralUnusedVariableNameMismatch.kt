88e6873b27d423bfd3118abd7816ed92b7ac9980
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2026-06-30 21:44:53 +0200

    KTIJ-38748 IJ-CR-210500 [kotlin] Fix omitting unused variables
    (cherry picked from commit 96984aa5230d850a001e938497632979b99d9b23)
    
    
    (cherry picked from commit 9881a869736dae1c819e1578dd47b0d7e14a9e8e)
    
    IJ-MR-211612
    
    GitOrigin-RevId: 87017d71afe73777302579d30371ac691302a770

--- plugins/kotlin/idea/tests/testData/quickfix/destructingShortForm/fullFormWithSeveralUnusedVariableNameMismatch.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/destructingShortForm/fullFormWithSeveralUnusedVariableNameMismatch.kt
@@ -1,10 +1,9 @@
 // "Convert to a full name-based destructuring form" "true"
 // COMPILER_ARGUMENTS: -Xname-based-destructuring=name-mismatch
 
-data class Product(val id: String, val productName: String)
+data class Product(val id: String, val productName: String, val price: Int, val category: String)
 
 fun test(product: Product) {
-    (val <caret>myId = id) = product
+    val (_, <caret>myName, _, myCategory) = product
 }
-
 // FUS_K2_QUICKFIX_NAME: org.jetbrains.kotlin.idea.k2.codeinsight.fixes.DestructuringFormFactory$ConvertNameBasedDestructuringToFullFormFix
