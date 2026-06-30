3c8bba3d01b4f0ecab0dff633de647f872e17d92
Nikita Biriukov <nikita.biriukov@jetbrains.com>
2026-06-30 12:21:02 +0200

    [gradle] IDEA-389885 rename Gradle Kotlin package: integrationTests -> tests.integration
    GitOrigin-RevId: bde020184344fa3e3b18a411f20574bb1f1a8bb3

--- plugins/gradle/completion/kotlin/tests/test/KotlinGradleDependencyCompletionFusPositionTest.kt
+++ plugins/gradle/completion/kotlin/tests/test/KotlinGradleDependencyCompletionFusPositionTest.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.gradle.completion.kotlin.integrationTests
+package com.intellij.gradle.completion.kotlin.tests.integration
 
 import com.intellij.codeInsight.CodeInsightSettings
 import com.intellij.gradle.completion.GRADLE_SCRIPT_DEPENDENCY_COMPLETION_POSITION_KEY
