117d50217a123548239f19e3b5155db44d52a061
Evgenii Ilichev <evgenii.ilichev@jetbrains.com>
2026-06-30 20:37:57 +0200

    [MRI-4591] switch intellij.platform.util.rt.tests
    Run this test module through its protected Bazel target and exclude it from the community aggregator JPS run once Bazel owns it.
    
    GitOrigin-RevId: 6a5678bd7317a84bb20059cbd0290d7135e7966b

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
@@ -1616,4 +1616,5 @@ private val COMMUNITY_AGGREGATOR_BAZEL_MIGRATED_MODULES = listOf(
   "intellij.platform.uast.tests",
   "intellij.platform.util.coroutines.tests",
   "intellij.platform.util.progress.tests",
+  "intellij.platform.util.rt.tests",
 )
