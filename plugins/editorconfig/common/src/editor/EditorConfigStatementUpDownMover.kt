cd2402244383013ee31faaeafbfba88214578660
Vojtech Balik <vojtech.balik@jetbrains.com>
2026-06-30 12:45:16 +0200

    IJPL-196854 [editorconfig, remdev] move typing assist to common module
    Since FrontendOnly is now not used for these actions, the backend must agree to confirm frontend results.
    
    
    (cherry picked from commit 849056a994ec945a4d934daf44da52442104ac71)
    
    IJ-CR-218117
    
    GitOrigin-RevId: fc525c8ae990678939226dc82aab2b444a90850d

--- plugins/editorconfig/common/src/editor/EditorConfigStatementUpDownMover.kt
+++ plugins/editorconfig/common/src/editor/EditorConfigStatementUpDownMover.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.editorconfig.frontend.editor
+package com.intellij.editorconfig.common.editor
 
 import com.intellij.codeInsight.editorActions.moveUpDown.LineMover
 import com.intellij.codeInsight.editorActions.moveUpDown.LineRange
