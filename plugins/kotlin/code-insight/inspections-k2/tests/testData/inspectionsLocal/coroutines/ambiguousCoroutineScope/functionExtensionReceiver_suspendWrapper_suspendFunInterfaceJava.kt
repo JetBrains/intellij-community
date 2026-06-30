bcfb7f23abfe34788d1f4ff5891a930c6ec284c6
Anna Kozlova <anna.kozlova@jetbrains.com>
2026-06-30 09:06:04 +0200

    [kotlin] SuspiciousImplicitCoroutineScopeReceiverAccess: avoid endless loop
    - when parameter type is a flexible type
    
    KTIJ-39388
    
    GitOrigin-RevId: 915620ede97aa559cc284b4210492ded9a07e408

---
+++ plugins/kotlin/code-insight/inspections-k2/tests/testData/inspectionsLocal/coroutines/ambiguousCoroutineScope/functionExtensionReceiver_suspendWrapper_suspendFunInterfaceJava.kt
@@ -0,0 +1,14 @@
+// WITH_COROUTINES
+// PROBLEM: none
+package test
+
+import kotlinx.coroutines.CoroutineScope
+
+
+fun CoroutineScope.doStuff() {}
+
+suspend fun CoroutineScope.test() {
+    Main.suspendWrapper {
+        <caret>doStuff()
+    }
+}
