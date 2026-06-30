a7bdf2c5cdfa43140ab1a0661f96c0e0656156cd
Evgenii Ilichev <evgenii.ilichev@jetbrains.com>
2026-06-30 20:28:51 +0200

    [MRI-4591] switch intellij.platform.testFramework.junit5.jimfs.tests
    Run this test module through its protected Bazel target and exclude it from the community aggregator JPS run once Bazel owns it.
    
    GitOrigin-RevId: 47292fec5ea88a36aa6ab2167314e6eddfd99515

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
@@ -1608,4 +1608,5 @@ private val COMMUNITY_AGGREGATOR_BAZEL_MIGRATED_MODULES = listOf(
   "intellij.platform.syntax.i18n.tests",
   "intellij.platform.syntax.psi.tests",
   "intellij.platform.syntax.tests",
+  "intellij.platform.testFramework.junit5.jimfs.tests",
 )
