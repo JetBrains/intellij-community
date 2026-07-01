aae9e4f8d5229b279235675125d25227169764c7
Max Medvedev <maxim.medvedev@jetbrains.com>
2026-07-01 18:47:48 +0200

    refactoring [psi]: drop LightPsiFileBase
    GitOrigin-RevId: aff0767f7023c08e9b5ac888263fb35261934e53

--- platform/lang-impl/src/com/intellij/psi/impl/source/LightPsiFileImpl.java
+++ platform/lang-impl/src/com/intellij/psi/impl/source/LightPsiFileImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package com.intellij.psi.impl.source;
 
@@ -52,7 +52,17 @@ public abstract class LightPsiFileImpl extends PsiElementBase implements PsiFile
 
   @Override
   public VirtualFile getVirtualFile() {
-    return getViewProvider().isEventSystemEnabled() ? getViewProvider().getVirtualFile() : null;
+    return getViewProvider().getVirtualFile();
+  }
+
+  @Override
+  public boolean isDirectory() {
+    return false;
+  }
+
+  @Override
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    visitor.visitFile(this);
   }
 
   @Override
