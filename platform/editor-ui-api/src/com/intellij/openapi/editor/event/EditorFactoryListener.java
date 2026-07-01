2af0aa781cac81edef58611a0bfdbcc30e60537b
Max Medvedev <maxim.medvedev@jetbrains.com>
2026-07-01 10:16:48 +0200

    IJPL-246571 add docs for EditorFactoryListener
    GitOrigin-RevId: 818bece7256f4d31a804826f0596b44d1b363b03

--- platform/editor-ui-api/src/com/intellij/openapi/editor/event/EditorFactoryListener.java
+++ platform/editor-ui-api/src/com/intellij/openapi/editor/event/EditorFactoryListener.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.editor.event;
 
 import org.jetbrains.annotations.NotNull;
@@ -9,16 +9,18 @@ import java.util.EventListener;
  * Allows receiving notifications when editors are created and released.
  * To subscribe, use the {@code com.intellij.editorFactoryListener} extension point or
  * {@link com.intellij.openapi.editor.EditorFactory#addEditorFactoryListener(EditorFactoryListener, com.intellij.openapi.Disposable)}.
+ * <p>
+ * This listener is application level and is triggered for editors of ALL OPEN PROJECTS.
  */
 public interface EditorFactoryListener extends EventListener {
   /**
-   * Called after {@link com.intellij.openapi.editor.Editor} instance has been created.
+   * Called after {@link com.intellij.openapi.editor.Editor} instance has been created for any open project.
    */
   default void editorCreated(@NotNull EditorFactoryEvent event) {
   }
 
   /**
-   * Called before {@link com.intellij.openapi.editor.Editor} instance will be released.
+   * Called before {@link com.intellij.openapi.editor.Editor} instance will be released for any open project.
    */
   default void editorReleased(@NotNull EditorFactoryEvent event) {
   }
