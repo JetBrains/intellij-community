6ed08624c94a0d7ca904168105cef5979d2864f0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2026-07-01 12:24:21 +0200

    IJPL-248623 move agent providers
    IJ-MR-184958 IJ-MR-184993 IJ-MR-179029 IJ-MR-184126 IJ-MR-181153 IJ-MR-146078 IJ-MR-175479 IJ-MR-186058 IJ-MR-193195 IJ-MR-196957 IJ-MR-199124 IJ-MR-197441 IJ-MR-204135 IJ-MR-204674 IJ-MR-205883 IJ-MR-208539
    
    GitOrigin-RevId: f85fe7af527038075bcdaa511a766a88588182a8

--- plugins/agent-workbench/lib-agent/providers/codex/common/src/CodexAppServerExceptions.kt
+++
@@ -1,6 +0,0 @@
-// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.ai.agent.codex.common
-
-open class CodexAppServerException(message: String, cause: Throwable? = null) : RuntimeException(message, cause)
-
-class CodexCliNotFoundException : CodexAppServerException("Codex CLI not found")
--- plugins/agent-workbench/lib-agent/providers/codex/sessions/src/CodexAgentSessionProviderId.kt
+++
@@ -1,6 +0,0 @@
-// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.ai.agent.codex.sessions
-
-import com.intellij.platform.ai.agent.core.session.AgentSessionProvider
-
-internal val CODEX_AGENT_SESSION_PROVIDER: AgentSessionProvider = AgentSessionProvider.from("codex")
