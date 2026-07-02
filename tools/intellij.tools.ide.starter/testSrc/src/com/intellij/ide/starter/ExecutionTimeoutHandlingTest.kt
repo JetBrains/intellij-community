5ab0ad2c7257422b76ac305dc7e92cd8b1eb9d3b
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2026-07-02 19:39:24 +0200

    tests [starter]: move starter and starter-driver tests to corresponding modules
    GitOrigin-RevId: d856bb845aaf3465484ba20a274257c794b76b68

---
+++ tools/intellij.tools.ide.starter/testSrc/src/com/intellij/ide/starter/ExecutionTimeoutHandlingTest.kt
@@ -0,0 +1,49 @@
+package com.intellij.ide.starter
+
+import com.intellij.ide.starter.data.IdeaUltimateCases
+import com.intellij.ide.starter.process.exec.ExecTimeoutException
+import com.intellij.ide.starter.project.NoProject
+import com.intellij.ide.starter.report.DetailsOnCI
+import com.intellij.ide.starter.runner.IDERunContext
+import com.intellij.ide.starter.runner.Starter
+import com.intellij.ide.starter.runner.events.IdeBeforeLaunchEvent
+import com.intellij.ide.starter.utils.hyphenateTestName
+import com.intellij.tools.ide.performanceTesting.commands.CommandChain
+import com.intellij.tools.ide.performanceTesting.commands.delay
+import com.intellij.tools.ide.performanceTesting.commands.exitApp
+import com.intellij.tools.ide.starter.bus.EventsBus
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInfo
+import kotlin.time.Duration.Companion.seconds
+
+class ExecutionTimeoutHandlingTest {
+
+  @Test
+  fun `runIDE timeout should not throw raw exception without details`(testInfo: TestInfo) {
+    val testName = "${testInfo.testClass.get().simpleName}/${testInfo.testMethod.get().name}".hyphenateTestName()
+    val context = Starter.newContext(testName, IdeaUltimateCases.withProject(NoProject))
+    val timeout = 5.seconds
+
+    var runContext: IDERunContext? = null
+
+    EventsBus.subscribeOnce(this) { event: IdeBeforeLaunchEvent ->
+      runContext = event.runContext
+    }
+
+    try {
+      context.runIDE(
+        commands = CommandChain().delay(10_000).exitApp(),
+        runTimeout = timeout
+      )
+    }
+    catch (e: ExecTimeoutException) {
+      val ciFailureDetails = DetailsOnCI.instance.getLinkToCIArtifacts(runContext!!)?.let { "Link on CI artifacts ${it}" }
+      e.message.shouldBe(
+        "Timeout of IDE run '$testName' for $timeout" + System.lineSeparator() + (ciFailureDetails ?: "")
+      )
+      e.stackTrace.shouldBeEmpty()
+    }
+  }
+}
---
+++ tools/intellij.tools.ide.starter/testSrc/src/com/intellij/ide/starter/timeoutAnalyzer/TimeoutAnalyzerTest.kt
@@ -0,0 +1,112 @@
+package com.intellij.ide.starter.timeoutAnalyzer
+
+import com.intellij.ide.starter.report.TimeoutAnalyzer
+import com.intellij.ide.starter.runner.IDEReportingData
+import com.intellij.ide.starter.runner.IDERunContext
+import com.intellij.ide.starter.utils.JarUtils
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.string.shouldContain
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import org.mockito.Answers
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import java.nio.file.Path
+
+class TimeoutAnalyzerTest {
+
+  private lateinit var logsDir: Path
+
+  @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+  private lateinit var runContextMock: IDERunContext
+
+  @BeforeEach
+  fun setUp() {
+    MockitoAnnotations.openMocks(this)
+    Mockito.lenient().doReturn("timeout-analyzer-test").`when`(runContextMock).contextName
+  }
+
+  @TempDir
+  lateinit var tempDir: Path
+
+  private fun setUpLogsDir(resourcePath: String) {
+    logsDir = JarUtils.extractResource(resourcePath, tempDir)
+    val reportingData = Mockito.mock(IDEReportingData::class.java)
+    Mockito.lenient().doReturn(logsDir).`when`(reportingData).logsDir
+    Mockito.lenient().doReturn(reportingData).`when`(runContextMock).lastIdeReportingData
+    Mockito.lenient().doReturn(listOf(reportingData)).`when`(runContextMock).registeredIdeReportingData()
+  }
+
+  @Test
+  fun testDialogInKillThreadDump() {
+    setUpLogsDir("all-data")
+    val error = TimeoutAnalyzer.analyzeTimeout(runContextMock)
+    error.shouldNotBeNull().messageText.shouldContain("due to a dialog being shown")
+  }
+
+  @Test
+  fun testDialogInMonitoringThreadDump() {
+    setUpLogsDir("no-kill-dump")
+    val error = TimeoutAnalyzer.analyzeTimeout(runContextMock)
+    error.shouldNotBeNull().messageText.shouldContain("due to a dialog being shown")
+  }
+
+  @Test
+  fun testNoDialog() {
+    setUpLogsDir("no-dialog")
+    val error = TimeoutAnalyzer.analyzeTimeout(runContextMock)
+    error.shouldBeNull()
+  }
+
+  @Test
+  fun testDetectCommandFromLog() {
+    setUpLogsDir("all-data")
+    val error = TimeoutAnalyzer.analyzeTimeout(runContextMock)
+    error.shouldNotBeNull().messageText.shouldContain("openSettingsDialog")
+  }
+
+  @Test
+  fun testNoCommandInLog() {
+    setUpLogsDir("no-command-in-log")
+    val error = TimeoutAnalyzer.analyzeTimeout(runContextMock)
+    error.shouldNotBeNull().messageText.shouldContain("due to a dialog being shown")
+  }
+
+  @Test
+  fun testNoIdeaLog() {
+    setUpLogsDir("timeouts/no-idea-log")
+    val error = TimeoutAnalyzer.analyzeTimeout(runContextMock)
+    error.shouldNotBeNull().messageText.shouldContain("No idea.log file present in log directory")
+  }
+
+  @Test
+  fun testJavaDialog() {
+    setUpLogsDir("java-dialog")
+    val error = TimeoutAnalyzer.analyzeTimeout(runContextMock)
+    error.shouldNotBeNull().messageText.shouldContain("due to a dialog being shown")
+  }
+
+  @Test
+  fun testHungIndicators() {
+    setUpLogsDir("hung-indicators")
+    val error = TimeoutAnalyzer.analyzeTimeout(runContextMock).shouldNotBeNull()
+    error.messageText.shouldContain("during `%exitApp true` execution because some indicators haven't finished")
+    error.stackTraceContent.shouldContain("Closing attached shared indexes")
+    error.stackTraceContent.shouldContain("Flushing indexes for project files")
+  }
+
+  @Test
+  fun testNoHungIndicators() {
+    setUpLogsDir("no-hung-indicators")
+    TimeoutAnalyzer.analyzeTimeout(runContextMock).shouldBeNull()
+  }
+
+  @Test
+  fun testNoEdtInThreadDump() {
+    setUpLogsDir("no-edt-in-dump")
+    TimeoutAnalyzer.analyzeTimeout(runContextMock).shouldBeNull()
+  }
+}
