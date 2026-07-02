523792fb7d3d4b5a1a9e43855652654633401460
Max Medvedev <maxim.medvedev@jetbrains.com>
2026-07-02 18:55:00 +0200

    IJPL-238885 do checkCancelled much more often in ChunkExtractor
    GitOrigin-RevId: 6f0592f99e531e048b4b126c19f6a11a1667f809

--- platform/usageView/src/com/intellij/usages/ChunkExtractor.java
+++ platform/usageView/src/com/intellij/usages/ChunkExtractor.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.usages;
 
 import com.intellij.injected.editor.DocumentWindow;
@@ -195,7 +195,7 @@ public final class ChunkExtractor {
     boolean isBeginning = true;
 
     for (int j = 0; lexer.getTokenType() != null; lexer.advance(), j++) {
-      if (j % 1000 == 0) {
+      if (j % 50 == 0) {
         ProgressManager.checkCanceled();
       }
       int hiStart = lexer.getTokenStart();
