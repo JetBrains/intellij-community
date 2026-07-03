44fe477e417ce920cfebea92b8b85b2c91995e32
Alexandru Resiga <alexandru.resiga@jetbrains.com>
2026-07-03 14:15:24 +0200

    KMT-2953 [compose ide plugin] convert CreateStringResourceQuickFixTest.kt
    GitOrigin-RevId: 669ea69cf74f6e46c8b5537afd008e74ee9f69cb

--- plugins/compose/intellij.compose.ide.plugin.resources/tests/com/intellij/compose/ide/plugin/resources/intentions/quickfix/CreateStringResourceQuickFixTest.kt
+++ plugins/compose/intellij.compose.ide.plugin.resources/tests/com/intellij/compose/ide/plugin/resources/intentions/quickfix/CreateStringResourceQuickFixTest.kt
@@ -2,95 +2,89 @@
 package com.intellij.compose.ide.plugin.resources.intentions.quickfix
 
 import com.intellij.codeInsight.intention.IntentionAction
-import com.intellij.compose.ide.plugin.resources.ComposeResourcesTestCase
+import com.intellij.compose.ide.plugin.resources.ComposeResourcesAllSourceSets
+import com.intellij.compose.ide.plugin.resources.ComposeResourcesCodeInsightTestCase
 import com.intellij.compose.ide.plugin.resources.ResourceType
 import com.intellij.compose.ide.plugin.resources.STRINGS_XML_FILENAME
-import com.intellij.compose.ide.plugin.resources.TARGET_GRADLE_VERSION
 import com.intellij.compose.ide.plugin.resources.getComposeResourcesDir
 import com.intellij.compose.ide.plugin.resources.intentions.hasSubTagWithName
-import com.intellij.openapi.application.ModalityState
-import com.intellij.openapi.application.invokeAndWaitIfNeeded
+import com.intellij.openapi.application.EDT
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiManager
 import com.intellij.psi.xml.XmlFile
-import org.jetbrains.kotlin.test.TestMetadata
-import org.jetbrains.plugins.gradle.tooling.annotation.TargetVersions
-import org.junit.Test
-import kotlin.test.DefaultAsserter.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertNotNull
-import kotlin.test.assertTrue
+import com.intellij.testFramework.common.timeoutRunBlocking
+import kotlinx.coroutines.Dispatchers
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import kotlin.io.path.invariantSeparatorsPathString
+import kotlin.test.assertNotNull as kAssertNotNull
 
-internal class CreateStringResourceQuickFixTest : ComposeResourcesTestCase() {
+@ComposeResourcesAllSourceSets
+internal class CreateStringResourceQuickFixTest : ComposeResourcesCodeInsightTestCase() {
 
-  @TargetVersions(TARGET_GRADLE_VERSION)
   @Test
-  @TestMetadata("ComposeResources")
   fun `test quickfix adds string entry`() = doQuickFix(codeLine = "Res.string.new_string_resource")
 
-  @TargetVersions(TARGET_GRADLE_VERSION)
   @Test
-  @TestMetadata("ComposeResources")
   fun `test quickfix adds string-array entry`() = doQuickFix(codeLine = "Res.array.new_array_resource")
 
-  @TargetVersions(TARGET_GRADLE_VERSION)
   @Test
-  @TestMetadata("ComposeResources")
   fun `test quickfix adds plurals entry`() = doQuickFix(codeLine = "Res.plurals.new_plurals_resource")
 
-  @TargetVersions(TARGET_GRADLE_VERSION)
   @Test
-  @TestMetadata("ComposeResources")
   fun `test quickfix does not add duplicate string entry`() = doQuickFix(expectAdded = false, codeLine = "Res.string.test")
 
-  @TargetVersions(TARGET_GRADLE_VERSION)
   @Test
-  @TestMetadata("ComposeResources")
   fun `test quickfix does not add duplicate string-array entry`() = doQuickFix(expectAdded = false, codeLine = "Res.array.test")
 
-  @TargetVersions(TARGET_GRADLE_VERSION)
   @Test
-  @TestMetadata("ComposeResources")
   fun `test quickfix does not add duplicate plurals entry`() = doQuickFix(expectAdded = false, codeLine = "Res.plurals.test")
 
-  @TargetVersions(TARGET_GRADLE_VERSION)
   @Test
-  @TestMetadata("ComposeResources")
   fun `test quickfix does nothing on drawable`() = doQuickFix(expectAdded = false, expectAvailable = false, "Res.drawable.test")
 
   private fun doQuickFix(
     expectAdded: Boolean = true,
     expectAvailable: Boolean = true,
     codeLine: String,
-  ) {
-    invokeAndWaitIfNeeded(ModalityState.nonModal()) {
-      val files = importProjectFromTestData()
+  ) = testComposeResourcesProject {
+    timeoutRunBlocking(context = Dispatchers.EDT) {
+      try {
+        val sourceKtFilePath = "composeApp/src/$sourceSetName/kotlin/org/example/project/test.$sourceSetName.kt"
+        val sourceKtFile = getFile(sourceKtFilePath)
+        val stringsXmlFile = findStringsXmlFile(project, sourceSetName)
+        snapshotProjectFile(projectRelativePath(stringsXmlFile))
+        val (resourceType, resourceName) = parseResReference(codeLine)
 
-      val (sourceKtFile, stringsXmlFile) = files.findTestFiles(myProject, sourceSetName)
-      val (resourceType, resourceName) = parseResReference(codeLine)
+        val quickFix = CreateStringResourceQuickFix(
+          resourceName = resourceName,
+          resourceType = resourceType,
+          sourceKtFileUrl = sourceKtFile.url,
+          stringsXmlUrl = stringsXmlFile.url,
+          composeResourcesDirUrl = stringsXmlFile.parent.parent.url,
+        )
 
-      val quickFix = CreateStringResourceQuickFix(
-        resourceName = resourceName,
-        resourceType = resourceType,
-        sourceKtFileUrl = sourceKtFile.url,
-        stringsXmlUrl = stringsXmlFile.url,
-        composeResourcesDirUrl = stringsXmlFile.parent.parent.url,
-      )
+        if (!expectAvailable) {
+          assertFalse(quickFix.isAvailable(project, null, null), "QuickFix should not be available")
+          return@timeoutRunBlocking
+        }
 
-      if (!expectAvailable) {
-        assertFalse(quickFix.isAvailable(myProject, null, null), "QuickFix should not be available")
-        return@invokeAndWaitIfNeeded
+        invokeAndAssertQuickFixResult(
+          quickFix = quickFix,
+          composeResourcesDirVirtualFile = stringsXmlFile.parent.parent,
+          project = project,
+          codeLine = codeLine,
+          expectAdded = expectAdded,
+        )
       }
-
-      invokeAndAssertQuickFixResult(
-        quickFix = quickFix,
-        composeResourcesDirVirtualFile = stringsXmlFile.parent.parent,
-        project = myProject,
-        codeLine = codeLine,
-        expectAdded = expectAdded,
-      )
+      finally {
+        revertUnsavedDocuments()
+      }
     }
   }
 }
@@ -100,6 +94,10 @@ internal fun List<VirtualFile>.findTestFiles(project: Project, sourceSetName: St
     it.path.endsWith("composeApp/src/$sourceSetName/kotlin/org/example/project/test.$sourceSetName.kt")
   }
 
+  return sourceKtFile to findStringsXmlFile(project, sourceSetName)
+}
+
+private fun findStringsXmlFile(project: Project, sourceSetName: String): VirtualFile {
   val module = ModuleManager.getInstance(project).modules.first {
     it.name.endsWith(".$sourceSetName") || (sourceSetName == "androidMain" && it.name.endsWith(".main"))
   }
@@ -107,8 +105,9 @@ internal fun List<VirtualFile>.findTestFiles(project: Project, sourceSetName: St
   val valuesDir = composeResourcesDir.findChild(ResourceType.STRING.dirName)!!
   val stringsXmlFile = valuesDir.findChild(STRINGS_XML_FILENAME)!!
 
-  return sourceKtFile to stringsXmlFile
+  return stringsXmlFile
 }
+
 internal fun invokeAndAssertQuickFixResult(
   quickFix: IntentionAction,
   composeResourcesDirVirtualFile: VirtualFile,
@@ -129,19 +128,20 @@ internal fun invokeAndAssertQuickFixResult(
                                 ?.hasSubTagWithName(resourceType.typeName, resourceName)
                               ?: false
 
-  assertTrue("QuickFix should be available") { quickFix.isAvailable(project, null, null) }
+  assertTrue(quickFix.isAvailable(project, null, null), "QuickFix should be available")
   quickFix.invoke(project, null, null)
+  PsiDocumentManager.getInstance(project).commitAllDocuments()
 
-  val stringsXmlFileVirtualFile = composeResourcesDirVirtualFile
-    .findChild(ResourceType.STRING.dirName)
-    ?.findChild(STRINGS_XML_FILENAME)
-  assertNotNull(stringsXmlFileVirtualFile, "Strings.xml file should not be null")
+  val stringsXmlFileVirtualFile = kAssertNotNull(
+    composeResourcesDirVirtualFile
+      .findChild(ResourceType.STRING.dirName)
+      ?.findChild(STRINGS_XML_FILENAME),
+    "Strings.xml file should not be null"
+  )
 
-  val stringsXmlFile = psiManager.findFile(stringsXmlFileVirtualFile) as? XmlFile
-  assertNotNull(stringsXmlFile, "Strings.xml file should not be null")
+  val stringsXmlFile = kAssertNotNull(psiManager.findFile(stringsXmlFileVirtualFile) as? XmlFile, "Strings.xml file should not be null")
 
-  val rootTagAfter = stringsXmlFile.rootTag
-  assertNotNull(rootTagAfter, "Root tag should not be null")
+  val rootTagAfter = kAssertNotNull(stringsXmlFile.rootTag, "Root tag should not be null")
   val resourceExistsNow = rootTagAfter.hasSubTagWithName(resourceType.typeName, resourceName)
 
   if (expectAdded) {
@@ -151,7 +151,7 @@ internal fun invokeAndAssertQuickFixResult(
   else {
     assertTrue(resourceExistedBefore, "Resource '$resourceName' should already exist before the quickfix")
     val count = rootTagAfter.findSubTags(resourceType.typeName).count { it.getAttributeValue("name") == resourceName }
-    assertEquals("Expected exactly one <${resourceType.typeName}> tag with name='$resourceName'", 1, count)
+    assertEquals(1, count, "Expected exactly one <${resourceType.typeName}> tag with name='$resourceName'")
   }
 }
 
