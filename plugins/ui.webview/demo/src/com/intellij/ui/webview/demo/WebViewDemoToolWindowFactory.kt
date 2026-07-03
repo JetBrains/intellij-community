91ba5af8cc8289dec792a9636cbd52ca265ffce1
Artem.Bukhonov <artem.bukhonov@jetbrains.com>
2026-07-03 21:55:41 +0200

    IJPL-247160 [WebView] Demo: do not activate Demo toolwindow by default
    GitOrigin-RevId: bbcf502707669741332e66621ce1098380f677ba

--- plugins/ui.webview/demo/src/com/intellij/ui/webview/demo/WebViewDemoToolWindowFactory.kt
+++ plugins/ui.webview/demo/src/com/intellij/ui/webview/demo/WebViewDemoToolWindowFactory.kt
@@ -4,24 +4,17 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.ui.content.ContentFactory
 
 internal class WebViewDemoToolWindowFactory : ToolWindowFactory, DumbAware {
-  override suspend fun manage(toolWindow: ToolWindow, toolWindowManager: ToolWindowManager) {
-    toolWindowManager.invokeLater {
-      if (!toolWindow.project.isDisposed) {
-        toolWindow.activate(null, true)
-      }
-    }
-  }
+  override fun shouldBeAvailable(project: Project): Boolean = false
 
   override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
     val service = WebViewDemoProjectService.getInstance(project)
-    toolWindow.addContent(service.createSamplePanelContent(), WebViewDemoBundle.message("toolwindow.tab.sample.panel"))
-    toolWindow.addContent(service.createControlsShowcaseContent(), WebViewDemoBundle.message("toolwindow.tab.controls.showcase"))
-    toolWindow.addContent(service.createReactControlsShowcaseContent(), WebViewDemoBundle.message("toolwindow.tab.react.controls.showcase"))
     toolWindow.addContent(service.createUiDslShowcaseContent(), WebViewDemoBundle.message("toolwindow.tab.ui.dsl.showcase"))
+    toolWindow.addContent(service.createControlsShowcaseContent(), WebViewDemoBundle.message("toolwindow.tab.controls.showcase"))
+    toolWindow.addContent(service.createSamplePanelContent(), WebViewDemoBundle.message("toolwindow.tab.sample.panel"))
+    toolWindow.addContent(service.createReactControlsShowcaseContent(), WebViewDemoBundle.message("toolwindow.tab.react.controls.showcase"))
     toolWindow.addContent(service.createMarkdownLinkGraphContent(), WebViewDemoBundle.message("toolwindow.tab.markdown.link.graph"))
   }
 
