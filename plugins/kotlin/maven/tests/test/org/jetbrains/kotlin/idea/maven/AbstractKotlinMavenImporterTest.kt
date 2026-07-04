ee0e207fe49c81253e8d6c4f772c03e81737c5d3
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-07-04 14:01:53 +0200

    [maven] IDEA-391004 Convert CommonDetectionByGoalWithJvmStdlib1164 to JUnit 5
    GitOrigin-RevId: 472e0a53a452b1e7ee69cae2d799995c727ec552

--- plugins/kotlin/maven/tests/test/org/jetbrains/kotlin/idea/maven/AbstractKotlinMavenImporterTest.kt
+++ plugins/kotlin/maven/tests/test/org/jetbrains/kotlin/idea/maven/AbstractKotlinMavenImporterTest.kt
@@ -144,60 +144,6 @@ abstract class AbstractKotlinMavenImporterTest(private val createStdProjectFolde
         get() = facetSettings("project")
 
     @MppGoal
-    class CommonDetectionByGoalWithJvmStdlib1164 : AbstractKotlinMavenImporterTest() {
-        @Test
-        fun testCommonDetectionByGoalWithJvmStdlib() = runBlocking {
-            createProjectSubDirs("src/main/kotlin", "src/main/kotlin.jvm", "src/test/kotlin", "src/test/kotlin.jvm")
-
-            importProjectAsync(
-                """
-            <groupId>test</groupId>
-            <artifactId>project</artifactId>
-            <version>1.0.0</version>
-
-            <dependencies>
-                <dependency>
-                    <groupId>org.jetbrains.kotlin</groupId>
-                    <artifactId>kotlin-stdlib</artifactId>
-                    <version>$kotlinVersion</version>
-                </dependency>
-            </dependencies>
-
-            <build>
-                <sourceDirectory>src/main/kotlin</sourceDirectory>
-
-                <plugins>
-                    <plugin>
-                        <groupId>org.jetbrains.kotlin</groupId>
-                        <artifactId>kotlin-maven-plugin</artifactId>
-                        <executions>
-                            <execution>
-                                <id>compile</id>
-                                <goals>
-                                    <goal>metadata</goal>
-                                </goals>
-                            </execution>
-                        </executions>
-                    </plugin>
-                </plugins>
-            </build>
-            """
-            )
-
-            assertModules("project")
-
-            Assert.assertTrue(facetSettings.targetPlatform.isCommon())
-
-            Assert.assertTrue(ModuleRootManager.getInstance(getModule("project")).sdk!!.sdkType is KotlinSdkType)
-
-            assertKotlinSources("project", "src/main/kotlin")
-            assertKotlinTestSources("project", "src/test/java")
-            assertDefaultKotlinResources("project")
-            assertDefaultKotlinTestResources("project")
-        }
-    }
-
-    @MppGoal
     class CommonDetectionByGoalWithCommonStdlib : AbstractKotlinMavenImporterTest() {
         @Test
         fun testCommonDetectionByGoalWithCommonStdlib() = runBlocking {
