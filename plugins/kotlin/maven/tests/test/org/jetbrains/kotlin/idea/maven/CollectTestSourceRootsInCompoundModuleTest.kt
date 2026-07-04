c3f798c7e5b4711fbf195653bca67c9c8d948f18
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-07-04 14:50:01 +0200

    [maven] IDEA-391004 Convert CollectTestSourceRootsInCompoundModule to JUnit 5
    GitOrigin-RevId: 4ee56860fb4035fd32617702b3eab45c5a14cd90

---
+++ plugins/kotlin/maven/tests/test/org/jetbrains/kotlin/idea/maven/CollectTestSourceRootsInCompoundModuleTest.kt
@@ -0,0 +1,102 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.maven
+
+import com.intellij.maven.testFramework.fixtures.MavenVersionArguments
+import com.intellij.maven.testFramework.fixtures.assertModules
+import com.intellij.maven.testFramework.fixtures.createProjectSubDirs
+import com.intellij.maven.testFramework.fixtures.getContentRoots
+import com.intellij.maven.testFramework.fixtures.importProjectAsync
+import com.intellij.testFramework.UsefulTestCase.assertEmpty
+import com.intellij.testFramework.junit5.TestApplication
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.jps.model.java.JavaSourceRootType
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedClass
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+@TestApplication
+@ParameterizedClass
+@ArgumentsSource(MavenVersionArguments::class)
+class CollectTestSourceRootsInCompoundModuleTest(mavenVersion: String, modelVersion: String) :
+    KotlinMavenImportingTestBase(mavenVersion, modelVersion) {
+    @Test
+    fun testCollectSourceRootsInCompoundModule() = runBlocking {
+        maven.createProjectSubDirs("src/main/kotlin", "src/main/kotlin.jvm", "src/test/kotlin", "src/test/kotlin.jvm")
+
+        maven.importProjectAsync(
+            """
+                    <groupId>test</groupId>
+                    <artifactId>project</artifactId>
+                    <version>1</version>
+                    <properties>
+                        <maven.compiler.release>8</maven.compiler.release>
+                        <maven.compiler.testRelease>11</maven.compiler.testRelease>
+                    </properties>
+                    <build>
+                        <plugins>
+                            <plugin>
+                                <artifactId>maven-compiler-plugin</artifactId>
+                                <version>3.11.0</version>
+                            </plugin>
+                            <plugin>
+                                <groupId>org.jetbrains.kotlin</groupId>
+                                <artifactId>kotlin-maven-plugin</artifactId>
+                                <version>1.8.10</version>
+                                <executions>
+                                    <execution>
+                                        <id>compile</id>
+                                        <goals>
+                                            <goal>compile</goal>
+                                        </goals>
+                                        <configuration>
+                                            <sourceDirs>
+                                                <sourceDir>${"$"}{project.basedir}/src/main/kotlin</sourceDir>
+                                                <sourceDir>${"$"}{project.basedir}/src/main/java</sourceDir>
+                                            </sourceDirs>
+                                        </configuration>
+                                    </execution>
+                                    <execution>
+                                        <id>test-compile</id>
+                                        <goals>
+                                            <goal>test-compile</goal>
+                                        </goals>
+                                        <configuration>
+                                            <sourceDirs>
+                                                <sourceDir>${"$"}{project.basedir}/src/test/kotlin</sourceDir>
+                                                <sourceDir>${"$"}{project.basedir}/src/test/java</sourceDir>
+                                            </sourceDirs>
+                                        </configuration>
+                                    </execution>
+                                </executions>
+                            </plugin>
+                        </plugins>
+                        <resources>
+                            <resource>
+                              <directory>${"$"}{project.basedir}</directory>
+                              <targetPath>META-INF</targetPath>
+                              <includes>
+                                  <include>LICENSE</include>
+                              </includes>
+                            </resource>
+                        </resources>
+                    </build>
+            """
+        )
+
+        val mainModule = "project.main"
+        val testModule = "project.test"
+        val compoundModule = "project"
+
+        maven.assertModules(compoundModule, mainModule, testModule)
+
+        val mainModuleTestSources = maven.getContentRoots(mainModule)
+            .flatMap { it.getSourceFolders(JavaSourceRootType.TEST_SOURCE) }
+            .map { it.url }
+        assertEmpty(mainModuleTestSources)
+
+        val testModuleSources = maven.getContentRoots(testModule)
+            .flatMap { it.getSourceFolders(JavaSourceRootType.SOURCE) }
+            .map { it.url }
+        assertEmpty(testModuleSources)
+    }
+}
