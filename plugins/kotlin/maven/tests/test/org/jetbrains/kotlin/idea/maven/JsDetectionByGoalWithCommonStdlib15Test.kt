c4771e89d60aa70392f13108354e843ac54f9a29
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-07-04 14:01:49 +0200

    [maven] IDEA-391004 Convert JsDetectionByGoalWithCommonStdlib15 to JUnit 5
    GitOrigin-RevId: cdf468e6b97ace166067a3013d78e908345eb166

---
+++ plugins/kotlin/maven/tests/test/org/jetbrains/kotlin/idea/maven/JsDetectionByGoalWithCommonStdlib15Test.kt
@@ -0,0 +1,81 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.maven
+
+import com.intellij.maven.testFramework.fixtures.MavenVersionArguments
+import com.intellij.maven.testFramework.fixtures.assertModules
+import com.intellij.maven.testFramework.fixtures.createProjectSubDirs
+import com.intellij.maven.testFramework.fixtures.getModule
+import com.intellij.maven.testFramework.fixtures.importProjectAsync
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.testFramework.junit5.TestApplication
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.kotlin.idea.framework.KotlinSdkType
+import org.jetbrains.kotlin.platform.isJs
+import org.junit.Assert
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedClass
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+@TestApplication
+@ParameterizedClass
+@ArgumentsSource(MavenVersionArguments::class)
+@KotlinMavenImportingTestBase.MppGoal
+class JsDetectionByGoalWithCommonStdlib15Test(mavenVersion: String, modelVersion: String) :
+    KotlinMavenImportingTestBase(mavenVersion, modelVersion) {
+    @Test
+    fun testJsDetectionByGoalWithCommonStdlib() = runBlocking {
+        maven.createProjectSubDirs("src/main/kotlin", "src/main/kotlin.jvm", "src/test/kotlin", "src/test/kotlin.jvm")
+
+        maven.importProjectAsync(
+            """
+            <groupId>test</groupId>
+            <artifactId>project</artifactId>
+            <version>1.0.0</version>
+
+            <dependencies>
+                <dependency>
+                    <groupId>org.jetbrains.kotlin</groupId>
+                    <artifactId>kotlin-stdlib-common</artifactId>
+                    <version>$kotlinVersion</version>
+                </dependency>
+            </dependencies>
+
+            <build>
+                <sourceDirectory>src/main/kotlin</sourceDirectory>
+
+                <plugins>
+                    <plugin>
+                        <groupId>org.jetbrains.kotlin</groupId>
+                        <artifactId>kotlin-maven-plugin</artifactId>
+                        <executions>
+                            <execution>
+                                <id>compile</id>
+                                <goals>
+                                    <goal>js</goal>
+                                </goals>
+                            </execution>
+                            <execution>
+                                <id>test-compile</id>
+                                <goals>
+                                    <goal>test-js</goal>
+                                </goals>
+                            </execution>
+                        </executions>
+                    </plugin>
+                </plugins>
+            </build>
+            """
+        )
+
+        maven.assertModules("project")
+
+        Assert.assertTrue(facetSettings.targetPlatform.isJs())
+
+        Assert.assertTrue(ModuleRootManager.getInstance(maven.getModule("project")).sdk!!.sdkType is KotlinSdkType)
+
+        assertKotlinSources("project", "src/main/kotlin")
+        assertKotlinTestSources("project", "src/test/java")
+        assertDefaultKotlinResources("project")
+        assertDefaultKotlinTestResources("project")
+    }
+}
