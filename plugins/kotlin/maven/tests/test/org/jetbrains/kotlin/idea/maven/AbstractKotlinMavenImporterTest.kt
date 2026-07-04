d124481d72341d33136efce4fbf6168288f62cd1
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-07-04 14:49:50 +0200

    [maven] IDEA-391004 Convert StableModuleNameWhileUsngMavenJS to JUnit 5
    GitOrigin-RevId: 1f612a4771ffe1b71869d9d783f7ac5ac9c9a9fe

--- plugins/kotlin/maven/tests/test/org/jetbrains/kotlin/idea/maven/AbstractKotlinMavenImporterTest.kt
+++ plugins/kotlin/maven/tests/test/org/jetbrains/kotlin/idea/maven/AbstractKotlinMavenImporterTest.kt
@@ -143,69 +143,6 @@ abstract class AbstractKotlinMavenImporterTest(private val createStdProjectFolde
     protected val facetSettings: IKotlinFacetSettings
         get() = facetSettings("project")
 
-    @MppGoal
-    class StableModuleNameWhileUsngMavenJS : AbstractKotlinMavenImporterTest() {
-        @Test
-        fun testStableModuleNameWhileUsngMaven_JS() = runBlocking {
-            createProjectSubDirs("src/main/kotlin", "src/main/kotlin.jvm", "src/test/kotlin", "src/test/kotlin.jvm")
-
-            importProjectAsync(
-                """
-            <groupId>test</groupId>
-            <artifactId>project</artifactId>
-            <version>1.0.0</version>
-
-            <dependencies>
-                <dependency>
-                    <groupId>org.jetbrains.kotlin</groupId>
-                    <artifactId>kotlin-stdlib-js</artifactId>
-                    <version>$kotlinVersion</version>
-                </dependency>
-            </dependencies>
-
-            <build>
-                <sourceDirectory>src/main/kotlin</sourceDirectory>
-
-                <plugins>
-                    <plugin>
-                        <groupId>org.jetbrains.kotlin</groupId>
-                        <artifactId>kotlin-maven-plugin</artifactId>
-
-                        <executions>
-                            <execution>
-                                <id>compile</id>
-                                <phase>compile</phase>
-                                <goals>
-                                    <goal>js</goal>
-                                </goals>
-                            </execution>
-                        </executions>
-                        <configuration>
-                            <languageVersion>1.1</languageVersion>
-                            <apiVersion>1.0</apiVersion>
-                            <multiPlatform>true</multiPlatform>
-                            <nowarn>true</nowarn>
-                            <args>
-                                <arg>-Xcoroutines=enable</arg>
-                            </args>
-                            <sourceMap>true</sourceMap>
-                            <outputFile>test.js</outputFile>
-                            <metaInfo>true</metaInfo>
-                            <moduleKind>commonjs</moduleKind>
-                        </configuration>
-                    </plugin>
-                </plugins>
-            </build>
-            """
-            )
-
-            // Note that we check name induced by '-output-file' -- may be it's not the best
-            // decision, but we don't have a better one
-            checkStableModuleName("project", "test", JsPlatforms.defaultJsPlatform, isProduction = true)
-            checkStableModuleName("project", "test", JsPlatforms.defaultJsPlatform, isProduction = false)
-        }
-    }
-
     class JvmTarget6IsImportedAsIs : AbstractKotlinMavenImporterTest() {
         @Test
         fun testJvmTargetIsImportedAsIs() = runBlocking {
