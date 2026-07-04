5317324be7a52d1fe0a35e76f8bd56088bbe5432
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-07-04 16:13:33 +0200

    [maven] IDEA-391004 Convert AbstractMavenUpdateConfigurationQuickFixTest to JUnit 5
    GitOrigin-RevId: 5386fb33e848b9d5b08283853be46d9cefbac45e

--- plugins/kotlin/maven/tests/test/org/jetbrains/kotlin/idea/maven/AbstractMavenUpdateConfigurationQuickFixTest.kt
+++ plugins/kotlin/maven/tests/test/org/jetbrains/kotlin/idea/maven/AbstractMavenUpdateConfigurationQuickFixTest.kt
@@ -1,10 +1,13 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.maven
 
+import com.intellij.maven.testFramework.fixtures.createProjectSubFile
+import com.intellij.maven.testFramework.fixtures.importProjectAsync
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.application.writeIntentReadAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.impl.LoadTextUtil
+import com.intellij.openapi.project.modules
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.LocalFileSystem
@@ -15,9 +18,13 @@ import kotlinx.coroutines.withContext
 import org.jetbrains.kotlin.idea.test.DirectiveBasedActionUtils.ACTION_DIRECTIVE
 import org.jetbrains.kotlin.idea.test.KotlinTestUtils.TestFile
 import org.jetbrains.kotlin.test.InTextDirectivesUtils
+import org.junit.Assert.assertTrue
 import java.io.File
 
-abstract class AbstractMavenUpdateConfigurationQuickFixTest : AbstractMavenImportingTest() {
+abstract class AbstractMavenUpdateConfigurationQuickFixTest(
+  mavenVersion: String,
+  modelVersion: String,
+) : AbstractMavenImportingTest(mavenVersion, modelVersion) {
 
     override fun doTestAction(mainFile: TestFile) {
         val action = InTextDirectivesUtils.findStringWithPrefixes(mainFile.content, ACTION_DIRECTIVE)
@@ -30,15 +37,14 @@ abstract class AbstractMavenUpdateConfigurationQuickFixTest : AbstractMavenImpor
     }
 
     protected suspend fun doTest(intentionName: String) {
-        val pomVFile = createProjectSubFile("pom.xml", File(getTestDataPath(), "pom.xml").readText())
-        val sourceVFile = createProjectSubFile("src/main/kotlin/src.kt", File(getTestDataPath(), "src.kt").readText())
+        val pomVFile = maven.createProjectSubFile("pom.xml", File(getTestDataPath(), "pom.xml").readText())
+        val sourceVFile = maven.createProjectSubFile("src/main/kotlin/src.kt", File(getTestDataPath(), "src.kt").readText())
         LocalFileSystem.getInstance().refreshFiles(listOf(pomVFile, sourceVFile))
-        projectPom = pomVFile
-        addPom(projectPom)
-        importProjectAsync()
+        maven.projectPom = pomVFile
+        maven.importProjectAsync(pomVFile)
         withContext(Dispatchers.EDT) {
             writeIntentReadAction {
-                assertTrue(ModuleRootManager.getInstance(testFixture.module).fileIndex.isInSourceContent(sourceVFile))
+                assertTrue(ModuleRootManager.getInstance(project.modules.single()).fileIndex.isInSourceContent(sourceVFile))
                 with(codeInsightTestFixture) {
                     configureFromExistingVirtualFile(sourceVFile)
                     canChangeDocumentDuringHighlighting(true)
