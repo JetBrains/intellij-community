34589aaaa6649da31ad7b5f7cf1d7c3fddd4780d
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-07-04 16:25:47 +0200

    [maven] IDEA-391004 Inline shared doJvmTarget6Test helper into JvmTarget6 tests
    GitOrigin-RevId: acfe4caf78d8b1c79f805258e63e2b366e95123b

--- plugins/kotlin/maven/tests/test/org/jetbrains/kotlin/idea/maven/JvmTarget6IsImported8Test.kt
+++ plugins/kotlin/maven/tests/test/org/jetbrains/kotlin/idea/maven/JvmTarget6IsImported8Test.kt
@@ -2,10 +2,17 @@
 package org.jetbrains.kotlin.idea.maven
 
 import com.intellij.maven.testFramework.fixtures.MavenVersionArguments
+import com.intellij.maven.testFramework.fixtures.assertModules
+import com.intellij.maven.testFramework.fixtures.createProjectSubDirs
+import com.intellij.maven.testFramework.fixtures.importProjectAsync
+import com.intellij.maven.testFramework.fixtures.testRootDisposable
+import com.intellij.notification.Notification
 import com.intellij.testFramework.junit5.TestApplication
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.kotlin.cli.common.arguments.K2JVMCompilerArguments
+import org.jetbrains.kotlin.config.IKotlinFacetSettings
 import org.jetbrains.kotlin.idea.notification.asText
+import org.jetbrains.kotlin.idea.notification.catchNotificationsAsync
 import org.jetbrains.kotlin.platform.oldFashionedDescription
 import org.junit.Assert
 import org.junit.jupiter.api.Test
@@ -34,5 +41,56 @@ class JvmTarget6IsImported8Test(mavenVersion: String, modelVersion: String) :
                 """.trimIndent(),
             notifications.asText(),
         )
+    }
+
+    private suspend fun doJvmTarget6Test(version: String?): Pair<IKotlinFacetSettings, List<Notification>> {
+        maven.createProjectSubDirs("src/main/kotlin", "src/main/kotlin.jvm", "src/test/kotlin", "src/test/kotlin.jvm")
+
+        val notifications = catchNotificationsAsync(project, "Kotlin Maven project import", maven.testRootDisposable) {
+            maven.importProjectAsync(
+                """
+                    <groupId>test</groupId>
+                    <artifactId>project</artifactId>
+                    <version>1.0.0</version>
+
+                    <dependencies>
+                        <dependency>
+                            <groupId>org.jetbrains.kotlin</groupId>
+                            <artifactId>kotlin-stdlib</artifactId>
+                            <version>$kotlinVersion</version>
+                        </dependency>
+                    </dependencies>
+
+                    <build>
+                        <sourceDirectory>src/main/kotlin</sourceDirectory>
+
+                        <plugins>
+                            <plugin>
+                                <groupId>org.jetbrains.kotlin</groupId>
+                                <artifactId>kotlin-maven-plugin</artifactId>
+                                ${version?.let { "<version>$it</version>" } ?: ""}
+
+                                <executions>
+                                    <execution>
+                                        <id>compile</id>
+                                        <phase>compile</phase>
+                                        <goals>
+                                            <goal>compile</goal>
+                                        </goals>
+                                    </execution>
+                                </executions>
+                                <configuration>
+                                    <jvmTarget>1.6</jvmTarget>
+                                </configuration>
+                            </plugin>
+                        </plugins>
+                    </build>
+                """
+            )
+        }
+
+        maven.assertModules("project")
+
+        return facetSettings to notifications
     }
 }
