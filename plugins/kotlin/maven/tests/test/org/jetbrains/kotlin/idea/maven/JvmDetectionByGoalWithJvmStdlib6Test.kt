6b27aa4e95b70f5a35237399eee70861ccf399ca
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2026-07-04 14:01:43 +0200

    [maven] IDEA-391004 Convert JvmDetectionByGoalWithJvmStdlib6 to JUnit 5
    GitOrigin-RevId: c53c3e818cdc6d08aba7976e24c58e82f37d3031

---
+++ plugins/kotlin/maven/tests/test/org/jetbrains/kotlin/idea/maven/JvmDetectionByGoalWithJvmStdlib6Test.kt
@@ -0,0 +1,79 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.maven
+
+import com.intellij.maven.testFramework.fixtures.MavenVersionArguments
+import com.intellij.maven.testFramework.fixtures.assertDefaultResources
+import com.intellij.maven.testFramework.fixtures.assertDefaultTestResources
+import com.intellij.maven.testFramework.fixtures.assertModules
+import com.intellij.maven.testFramework.fixtures.assertSources
+import com.intellij.maven.testFramework.fixtures.assertTestSources
+import com.intellij.maven.testFramework.fixtures.createProjectSubDirs
+import com.intellij.maven.testFramework.fixtures.importProjectAsync
+import com.intellij.testFramework.junit5.TestApplication
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
+import org.junit.Assert
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedClass
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+@TestApplication
+@ParameterizedClass
+@ArgumentsSource(MavenVersionArguments::class)
+class JvmDetectionByGoalWithJvmStdlib6Test(mavenVersion: String, modelVersion: String) :
+    KotlinMavenImportingTestBase(mavenVersion, modelVersion) {
+    @Test
+    fun testJvmDetectionByGoalWithJvmStdlib() = runBlocking {
+        maven.createProjectSubDirs("src/main/kotlin", "src/main/kotlin.jvm", "src/test/kotlin", "src/test/kotlin.jvm")
+
+        maven.importProjectAsync(
+            """
+            <groupId>test</groupId>
+            <artifactId>project</artifactId>
+            <version>1.0.0</version>
+
+            <dependencies>
+                <dependency>
+                    <groupId>org.jetbrains.kotlin</groupId>
+                    <artifactId>kotlin-stdlib</artifactId>
+                    <version>$kotlinVersion</version>
+                </dependency>
+            </dependencies>
+
+            <build>
+                <sourceDirectory>src/main/kotlin</sourceDirectory>
+
+                <plugins>
+                    <plugin>
+                        <groupId>org.jetbrains.kotlin</groupId>
+                        <artifactId>kotlin-maven-plugin</artifactId>
+                        <executions>
+                            <execution>
+                                <id>compile</id>
+                                <goals>
+                                    <goal>compile</goal>
+                                </goals>
+                            </execution>
+                            <execution>
+                                <id>test-compile</id>
+                                <goals>
+                                    <goal>test-compile</goal>
+                                </goals>
+                            </execution>
+                        </executions>
+                    </plugin>
+                </plugins>
+            </build>
+            """
+        )
+
+        maven.assertModules("project")
+
+        Assert.assertEquals(JvmPlatforms.jvm6, facetSettings.targetPlatform)
+
+        maven.assertSources("project", "src/main/kotlin")
+        maven.assertTestSources("project", "src/test/java")
+        maven.assertDefaultResources("project")
+        maven.assertDefaultTestResources("project")
+    }
+}
