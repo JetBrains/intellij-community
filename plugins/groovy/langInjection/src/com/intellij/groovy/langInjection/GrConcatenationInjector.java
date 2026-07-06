b5a15e217d82c8a491f3f4a6a822886cc618b333
Nikolay Chashnikov <nikolay.chashnikov@jetbrains.com>
2026-07-06 12:09:41 +0200

    IDEA-391261 Groovy: fix the root package for the extracted intellij.groovy.langInjection module
    GitOrigin-RevId: 262d441935770efde12e60b9df98783a399e1154

--- plugins/groovy/langInjection/src/com/intellij/groovy/langInjection/GrConcatenationInjector.java
+++ plugins/groovy/langInjection/src/com/intellij/groovy/langInjection/GrConcatenationInjector.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.intellij.plugins.intelliLang.inject.groovy;
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.groovy.langInjection;
 
 import com.intellij.lang.injection.MultiHostInjector;
 import com.intellij.lang.injection.MultiHostRegistrar;
