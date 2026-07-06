e61f76e63b05a5f2f99bc8189f2ad381e7af519c
Evgenii Ilichev <evgenii.ilichev@jetbrains.com>
2026-07-06 18:15:21 +0200

    [MRI-4591] switch intellij.vcs.git.featuresTrainer.tests
    GitOrigin-RevId: cb8f8c91c1a42ef47f76c82540be47f4bf3644c2

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
@@ -1632,4 +1632,5 @@ private val COMMUNITY_AGGREGATOR_BAZEL_MIGRATED_MODULES = listOf(
   "intellij.tools.ide.metrics.collector.tests",
   "intellij.tools.ide.starter.bus.tests",
   "intellij.turboComplete.tests",
+  "intellij.vcs.git.featuresTrainer.tests",
 )
