b7b6a1fef48c9504a7fa19886952cc7d9a1e681c
Ilya Korennoy <ilya.korennoy@jetbrains.com>
2026-07-07 20:24:00 +0400

    IJPL-249653 Remove EntityStorage from WorkspaceFileIndexChangedEvent
    GitOrigin-RevId: 53b217b14e33416e5885ba78a76f3437232948cf

--- platform/projectModel-api/src/com/intellij/workspaceModel/ide/LibraryEntities.kt
+++ platform/projectModel-api/src/com/intellij/workspaceModel/ide/LibraryEntities.kt
@@ -3,10 +3,19 @@
 package com.intellij.workspaceModel.ide
 
 import com.intellij.platform.workspace.jps.entities.LibraryEntity
+import com.intellij.platform.workspace.jps.entities.LibraryId
 import com.intellij.platform.workspace.jps.entities.LibraryRootTypeId
 import com.intellij.platform.workspace.jps.serialization.impl.LibraryNameGenerator
 import com.intellij.projectModel.ProjectModelBundle
 import com.intellij.util.PathUtil
+
+fun getPresentableLibraryName(libraryId: LibraryId?, libraryRootUrl: String?): String {
+  return if (libraryId != null) {
+    LibraryNameGenerator.getLegacyLibraryName(libraryId) ?: getPresentableNameForLibraryRoot(libraryRootUrl)
+  } else {
+    getPresentableNameForLibraryRoot(libraryRootUrl)
+  }
+}
 
 /**
  * Returns the user-visible name of this [LibraryEntity]
@@ -20,5 +29,9 @@ val LibraryEntity.presentableName: String
 
 private fun LibraryEntity.getPresentableNameForUnnamedLibrary(): String {
   val url = roots.firstOrNull { it.type == LibraryRootTypeId.COMPILED }?.url?.url
-  return if (url != null) PathUtil.toPresentableUrl(url) else ProjectModelBundle.message("empty.library.title")
+  return getPresentableNameForLibraryRoot(url)
+}
+
+private fun getPresentableNameForLibraryRoot(libraryRootUrl: String?): String {
+  return if (libraryRootUrl != null) PathUtil.toPresentableUrl(libraryRootUrl) else ProjectModelBundle.message("empty.library.title")
 }
