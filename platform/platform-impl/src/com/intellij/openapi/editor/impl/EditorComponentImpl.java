f63f82765963a1fc0c13171741c27399f139936a
Alexander Trushev <aleksandr.trushev@jetbrains.com>
2026-07-07 13:27:49 +0200

    [editor] IJPL-236335 Wrap painter into IW to prevent editor disposal during painting
    GitOrigin-RevId: 265c4402f2573d9eede9a44ea54b4e5d66f8b266

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorComponentImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorComponentImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.editor.impl;
 
 import com.intellij.ide.CutProvider;
@@ -341,7 +341,19 @@ public final class EditorComponentImpl extends JTextComponent implements Scrolla
     }
     gg.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, UISettings.getEditorFractionalMetricsHint());
     AffineTransform origTx = PaintUtil.alignTxToInt(gg, PaintUtil.insets2offset(getInsets()), true, false, RoundingMode.FLOOR);
-    editor.paint(gg);
+
+    // Wrap painting into RA to prevent the following error (IJPL-236335):
+    // Painting calls plugin ->
+    // plugin calls read action ->
+    // suvorov interception ->
+    // read action calls write action ->
+    // write action deletes virtual file ->
+    // editor tab is disposed ->
+    // editor is disposed ->
+    // plugin or editor gets disposed exception during painting
+    EditorThreading.read(() -> {
+      editor.paint(gg);
+    });
     if (origTx != null) {
       gg.setTransform(origTx);
     }
