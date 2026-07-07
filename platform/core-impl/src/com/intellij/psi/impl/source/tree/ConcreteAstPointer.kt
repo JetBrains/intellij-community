39ddcf007ae47732d61562eacb09d787cee65cc2
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2026-07-07 18:28:00 +0200

    [threading, psi] IJPL-244698: Convert file to kotlin
    GitOrigin-RevId: 6f8a31061de89b1bc0406c1c700860594ebde027

--- platform/core-impl/src/com/intellij/psi/impl/source/tree/ConcreteAstPointer.kt
+++ platform/core-impl/src/com/intellij/psi/impl/source/tree/ConcreteAstPointer.kt
@@ -1,87 +1,74 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.psi.impl.source.tree;
+package com.intellij.psi.impl.source.tree
 
-import com.intellij.psi.impl.source.tree.mvcc.VersionedPsiReference;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.function.Supplier;
+import com.intellij.psi.impl.source.tree.mvcc.VersionedPsiReference
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.Contract
+import java.util.function.Supplier
 
 /**
- * A reference to {@link FileElement}. Can be either versioned or non-versioned (with the semantics of versioned PSI),
- * depending on the properties of {@link com.intellij.psi.FileViewProvider} for which this pointer was created.
- * <p>
- * While mutation operations are copy-on-write, the produced elements can retain memory about past values of {@link FileElement} in order to support versioning.
- * <p>
+ * A reference to [FileElement]. Can be either versioned or non-versioned (with the semantics of versioned PSI),
+ * depending on the properties of [com.intellij.psi.FileViewProvider] for which this pointer was created.
+ *
+ * While mutation operations are copy-on-write, the produced elements can retain memory about past values of [FileElement] in order to support versioning.
+ *
  * In the majority of cases, the underlying references are either weak or soft, so the dereferencing function is not pure.
  */
 @ApiStatus.Internal
-public interface ConcreteAstPointer {
+sealed interface ConcreteAstPointer {
+    fun dereference(): FileElement?
 
-  @Nullable FileElement dereference();
+    fun updateWith(supplier: Supplier<out FileElement?>?): ConcreteAstPointer
 
-  @NotNull ConcreteAstPointer updateWith(@Nullable Supplier<? extends FileElement> supplier);
-
-  @Contract("_, _ -> new")
-  static @NotNull ConcreteAstPointer createPointer(boolean isVersioned, @Nullable Supplier<? extends FileElement> initialValue) {
-    return isVersioned ? new VersionedAstPointer(initialValue) : new NonVersionedAstPointer(initialValue);
-  }
-}
-
-class VersionedAstPointer implements ConcreteAstPointer {
-  @NotNull final VersionedPsiReference<Supplier<? extends FileElement>> ref;
-
-  VersionedAstPointer(@Nullable Supplier<? extends FileElement> initialValue) {
-    this.ref = new VersionedPsiReference<>();
-    if (initialValue != null) {
-      ref.set(initialValue);
+    companion object {
+        @Contract("_, _ -> new")
+        @JvmStatic
+        fun createPointer(isVersioned: Boolean, initialValue: Supplier<out FileElement?>?): ConcreteAstPointer {
+            return if (isVersioned) VersionedAstPointer(initialValue) else NonVersionedAstPointer(initialValue)
+        }
     }
+
+  class VersionedAstPointer : ConcreteAstPointer {
+      val ref: VersionedPsiReference<Supplier<out FileElement?>>
+
+      constructor(initialValue: Supplier<out FileElement?>?) {
+          this.ref = VersionedPsiReference()
+          if (initialValue != null) {
+              ref.set(initialValue)
+          }
+      }
+
+      private constructor(ref: VersionedPsiReference<Supplier<out FileElement?>>) {
+          this.ref = ref
+      }
+
+      override fun dereference(): FileElement? {
+          val supplier = ref.get()
+          return supplier?.get()
+      }
+
+      override fun updateWith(supplier: Supplier<out FileElement?>?): ConcreteAstPointer {
+          val forkedRef = ref.fork()
+          forkedRef.set(supplier)
+          return VersionedAstPointer(forkedRef)
+      }
+
+      override fun toString(): String {
+          return ref.toString()
+      }
   }
 
-  private VersionedAstPointer(@NotNull VersionedPsiReference<Supplier<? extends FileElement>> ref) {
-    this.ref = ref;
-  }
+  class NonVersionedAstPointer(val ref: Supplier<out FileElement?>?) : ConcreteAstPointer {
+      override fun dereference(): FileElement? {
+          return ref?.get()
+      }
 
-  @Override
-  public @Nullable FileElement dereference() {
-    Supplier<? extends FileElement> supplier = ref.get();
-    return supplier == null ? null : supplier.get();
-  }
+      override fun updateWith(supplier: Supplier<out FileElement?>?): ConcreteAstPointer {
+          return NonVersionedAstPointer(supplier)
+      }
 
-  @Override
-  public @NotNull ConcreteAstPointer updateWith(@Nullable Supplier<? extends FileElement> supplier) {
-    VersionedPsiReference<Supplier<? extends FileElement>> forkedRef = ref.fork();
-    forkedRef.set(supplier);
-    return new VersionedAstPointer(forkedRef);
-  }
-
-  @Override
-  public String toString() {
-    return ref.toString();
-  }
-}
-
-class NonVersionedAstPointer implements ConcreteAstPointer {
-  final @Nullable Supplier<? extends FileElement> ref;
-
-  NonVersionedAstPointer(@Nullable Supplier<? extends FileElement> provider) {
-    ref = provider;
-  }
-
-  @Override
-  public @Nullable FileElement dereference() {
-    return ref == null ? null : ref.get();
-  }
-
-  @Override
-  public @NotNull ConcreteAstPointer updateWith(@Nullable Supplier<? extends FileElement> supplier) {
-    return new NonVersionedAstPointer(supplier);
-  }
-
-  @Override
-  public String toString() {
-    return ref == null ? "no ref" : "reference installed: " + ref.get();
+      override fun toString(): String {
+          return if (ref == null) "no ref" else "reference installed: " + ref.get()
+      }
   }
 }
