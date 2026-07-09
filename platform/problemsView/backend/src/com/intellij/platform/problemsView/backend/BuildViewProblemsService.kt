f336f51707b2d1cb87c2240996ec14e92d99cbb8
Marina Mădăraș <marina.madaras@jetbrains.com>
2026-07-09 18:04:43 +0200

    [problems-view] IJPL-249015 move BuildViewProblemsService to BE and remove its empty module
    GitOrigin-RevId: 60f62f8e8f416252da284dd01cd559487306775e

--- platform/problemsView/backend/src/com/intellij/platform/problemsView/backend/BuildViewProblemsService.kt
+++ platform/problemsView/backend/src/com/intellij/platform/problemsView/backend/BuildViewProblemsService.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.platform.problemsView.buildView
+package com.intellij.platform.problemsView.backend
 
 import com.intellij.analysis.problemsView.FileProblem
 import com.intellij.analysis.problemsView.HighlightingDuplicateProblem
