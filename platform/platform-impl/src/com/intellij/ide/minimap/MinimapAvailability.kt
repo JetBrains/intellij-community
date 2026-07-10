23d5b181404bd67143e5bfca645187807539cc1c
Bogdan Kirilenko <bogdan.kirilenko@jetbrains.com>
2026-07-10 21:27:35 +0200

    [minimap] PY-88182 added registry key ide.minimap.available to disable minimap
    GitOrigin-RevId: e322f528e6b3d64a487155c5fe0c5af3c8612c1c

--- platform/platform-impl/src/com/intellij/ide/minimap/MinimapAvailability.kt
+++ platform/platform-impl/src/com/intellij/ide/minimap/MinimapAvailability.kt
@@ -2,14 +2,18 @@
 package com.intellij.ide.minimap
 
 import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.testFramework.TestModeFlags
+import com.intellij.util.PlatformUtils
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
 object MinimapAvailability {
   val FORCE_AVAILABLE: Key<Boolean> = Key.create("minimap.force.available.in.tests")
+  private const val REGISTRY_KEY: String = "ide.minimap.available"
 
   fun isAvailable(): Boolean {
-    return TestModeFlags.`is`(FORCE_AVAILABLE)
+    return TestModeFlags.`is`(FORCE_AVAILABLE) ||
+           (PlatformUtils.isPyCharm() && Registry.`is`(REGISTRY_KEY))
   }
 }
