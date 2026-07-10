4359c8d8136dad32e7a8e202d16beb7a3bb0f1cb
Mikhail Pyltsin <mikhail.pyltsin@jetbrains.com>
2026-07-10 17:27:18 +0200

    [java-inspection] IDEA-377691 Nullable incompatibilities. Return types over type parameters
    - fix for inner classes
    
    GitOrigin-RevId: 0b5acf34ef6e540a061a97fcd32c8901729b4d76

--- java/java-tests/testData/inspection/dataFlow/fixture/JSpecifyPlainTypeVariableReturnOptionOn.java
+++ java/java-tests/testData/inspection/dataFlow/fixture/JSpecifyPlainTypeVariableReturnOptionOn.java
@@ -1,7 +1,8 @@
-class JSpecifyPlainTypeVariableReturnOptionOn<T> {
-
-  // With REPORT_UNSPECIFIED_PARAMETRIC_NULLNESS ON this is reported.
-  T returnsNull() {
-    return <warning descr="'null' is returned from a method whose type-variable return type may be instantiated as non-null">null</warning>;
+class JSpecifyPlainTypeVariableReturnOptionOn {
+  static class NestedClass<T>
+  {
+    // With REPORT_UNSPECIFIED_PARAMETRIC_NULLNESS ON this is reported.
+    T returnsNull() {
+    return <warning descr="'null' is returned from a method whose type-variable return type may be instantiated as non-null">null</warning>;  }
   }
 }
