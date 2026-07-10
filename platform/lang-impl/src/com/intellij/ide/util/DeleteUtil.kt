7e5449d3c76437a0fd9cf870f4dcc4e928a360c0
Sergei Tachenov <Sergei.Tachenov@jetbrains.com>
2026-07-10 13:38:46 +0300

    [UI] IJPL-239326 Convert DeleteUtil to Kotlin
    GitOrigin-RevId: f9ea6ecdd66c80c9c58cb1965384aff1dc4d222f

--- platform/lang-impl/src/com/intellij/ide/util/DeleteUtil.kt
+++ platform/lang-impl/src/com/intellij/ide/util/DeleteUtil.kt
@@ -1,68 +1,67 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ide.util;
+package com.intellij.ide.util
 
-import com.intellij.ide.IdeBundle;
-import com.intellij.openapi.util.NlsContexts;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.psi.ElementDescriptionUtil;
-import com.intellij.psi.PsiDirectoryContainer;
-import com.intellij.psi.PsiElement;
-import com.intellij.util.containers.FactoryMap;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.PropertyKey;
-
-import java.util.HashMap;
+import com.intellij.ide.IdeBundle
+import com.intellij.openapi.util.NlsContexts
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.ElementDescriptionUtil
+import com.intellij.psi.PsiDirectoryContainer
+import com.intellij.psi.PsiElement
+import com.intellij.util.Function
+import com.intellij.util.containers.FactoryMap
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.PropertyKey
 
 @ApiStatus.Internal
-public final class DeleteUtil {
-  private DeleteUtil() { }
-
-  public static @NlsContexts.DialogMessage String generateWarningMessage(
-    @PropertyKey(resourceBundle = IdeBundle.BUNDLE) String key,
-    @NotNull PsiElement @NotNull [] elements
-  ) {
-    if (elements.length == 1) {
-      var name = ElementDescriptionUtil.getElementDescription(elements[0], DeleteNameDescriptionLocation.INSTANCE);
-      var type = ElementDescriptionUtil.getElementDescription(elements[0], DeleteTypeDescriptionLocation.SINGULAR);
-      return IdeBundle.message(key, type + (StringUtil.isEmptyOrSpaces(name) ? "" : " \"" + name + '"'));
+object DeleteUtil {
+  @NlsContexts.DialogMessage
+  fun generateWarningMessage(
+    @PropertyKey(resourceBundle = IdeBundle.BUNDLE) key: @PropertyKey(resourceBundle = IdeBundle.BUNDLE) String,
+    elements: Array<PsiElement>
+  ): @NlsContexts.DialogMessage String {
+    if (elements.size == 1) {
+      val name = ElementDescriptionUtil.getElementDescription(elements[0], DeleteNameDescriptionLocation.INSTANCE)
+      val type = ElementDescriptionUtil.getElementDescription(elements[0], DeleteTypeDescriptionLocation.SINGULAR)
+      return IdeBundle.message(key, type + (if (StringUtil.isEmptyOrSpaces(name)) "" else " \"" + name + '"'))
     }
 
-    var countMap = FactoryMap.<String, Integer>create(k -> 0);
-    var pluralToSingular = new HashMap<String, String>();
-    var directoryCount = 0;
-    var containerType = (String)null;
-    for (var elementToDelete : elements) {
-      var type = ElementDescriptionUtil.getElementDescription(elementToDelete, DeleteTypeDescriptionLocation.PLURAL);
-      pluralToSingular.put(type, ElementDescriptionUtil.getElementDescription(elementToDelete, DeleteTypeDescriptionLocation.SINGULAR));
-      var oldCount = countMap.get(type).intValue();
-      countMap.put(type, oldCount+1);
-      if (elementToDelete instanceof PsiDirectoryContainer) {
-        containerType = type;
-        directoryCount += ((PsiDirectoryContainer) elementToDelete).getDirectories().length;
+    val countMap = FactoryMap.create<String?, Int?>(Function { k: String? -> 0 })
+    val pluralToSingular = HashMap<String?, String?>()
+    var directoryCount = 0
+    var containerType = null as String?
+    for (elementToDelete in elements) {
+      val type = ElementDescriptionUtil.getElementDescription(elementToDelete, DeleteTypeDescriptionLocation.PLURAL)
+      pluralToSingular.put(
+        type,
+        ElementDescriptionUtil.getElementDescription(elementToDelete, DeleteTypeDescriptionLocation.SINGULAR)
+      )
+      val oldCount: Int = countMap.get(type)!!
+      countMap.put(type, oldCount + 1)
+      if (elementToDelete is PsiDirectoryContainer) {
+        containerType = type
+        directoryCount += elementToDelete.getDirectories().size
       }
     }
 
-    var buffer = new StringBuilder();
-    for (var entry : countMap.entrySet()) {
+    val buffer = StringBuilder()
+    for (entry in countMap.entries) {
       if (!buffer.isEmpty()) {
-        buffer.append(" ").append(IdeBundle.message("prompt.delete.and")).append(" ");
+        buffer.append(" ").append(IdeBundle.message("prompt.delete.and")).append(" ")
       }
 
-      var count = entry.getValue().intValue();
-      buffer.append(count).append(" ");
+      val count: Int = entry.value!!
+      buffer.append(count).append(" ")
       if (count == 1) {
-        buffer.append(pluralToSingular.get(entry.getKey()));
-      }
-      else {
-        buffer.append(entry.getKey());
+        buffer.append(pluralToSingular.get(entry.key))
+      } else {
+        buffer.append(entry.key)
       }
 
-      if (entry.getKey().equals(containerType)) {
-        buffer.append(" ").append(IdeBundle.message("prompt.delete.directory.paren", directoryCount));
+      if (entry.key == containerType) {
+        buffer.append(" ").append(IdeBundle.message("prompt.delete.directory.paren", directoryCount))
       }
     }
 
-    return IdeBundle.message(key, buffer.toString());
+    return IdeBundle.message(key, buffer.toString())
   }
 }
