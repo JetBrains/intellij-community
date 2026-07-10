45706f71fa562310562a794e6c815cf64ca645a8
Evgenii Ilichev <evgenii.ilichev@jetbrains.com>
2026-07-10 11:07:31 +0200

    [MRI-4591] switch intellij.groovy.structuralSearch.tests
    GitOrigin-RevId: fdf3f5486d46900d3e02c48237a57e246d13025a

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
@@ -1514,6 +1514,7 @@ private val COMMUNITY_AGGREGATOR_BAZEL_MIGRATED_MODULES = listOf(
   "intellij.gradle.completion.tests",
   "intellij.gradle.java.maven.tests",
   "intellij.gradle.toolingExtension.tests",
+  "intellij.groovy.structuralSearch.tests",
   "intellij.html.tools.tests",
   "intellij.ide.startup.importSettings.tests",
   "intellij.idea.community.build.tasks.tests",
