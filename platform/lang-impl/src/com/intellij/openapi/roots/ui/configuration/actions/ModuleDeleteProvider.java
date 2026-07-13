e4fef9599f0a7666d3deab5137d0bd0114bfc5e8
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2026-07-13 23:10:12 +0200

    IJPL-249990: Prevent module deletion only if module is the last module in a project or `.iml` file is outside `.idea`.
    Currently, module can't be deleted if its `.iml` sits in `.idea` (see an issue).
    This is quite strange, and doesn't work at least for PyCharm (it stores all modules in `.idea`).
    
    The right logic (as discussed, see an issue again) is to delete any module but the last one (except for modules outside `.idea`: these modules can always be deleted, see the review).
    
    This fix stops a user from deleting all modules.
    
    GitOrigin-RevId: 836f4b43a4b1b3c6adf55a9aba312f4ff5e6c59e

--- platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/actions/ModuleDeleteProvider.java
+++ platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/actions/ModuleDeleteProvider.java
@@ -18,6 +18,7 @@ import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.module.UnloadedModuleDescription;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectBundle;
+import com.intellij.openapi.project.ProjectUtil;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ModuleOrderEntry;
 import com.intellij.openapi.roots.ModuleRootManager;
@@ -25,18 +26,14 @@ import com.intellij.openapi.roots.OrderEntry;
 import com.intellij.openapi.roots.impl.ModifiableModelCommitter;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.NlsContexts;
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.project.ProjectKt;
 import com.intellij.projectImport.ProjectAttachProcessor;
-import com.intellij.util.PathUtilRt;
-import com.intellij.util.PlatformUtils;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.VisibleForTesting;
 
-import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -59,27 +56,38 @@ public class ModuleDeleteProvider implements DeleteProvider, TitledHandler {
   public final boolean canDeleteElement(@NotNull DataContext dataContext) {
     final Module[] modules = LangDataKeys.MODULE_CONTEXT_ARRAY.getData(dataContext);
     List<UnloadedModuleDescription> unloadedModules = ProjectView.UNLOADED_MODULES_CONTEXT_KEY.getData(dataContext);
-    return modules != null && !containsPrimaryModule(modules) || unloadedModules != null && !unloadedModules.isEmpty();
+    return modules != null && modulesCanBeDeleted(modules) || unloadedModules != null && !unloadedModules.isEmpty();
   }
 
-  private static boolean containsPrimaryModule(Module[] modules) {
-    if (!ProjectAttachProcessor.canAttachToProject()) {
-      return !PlatformUtils.isIntelliJ();
+  /**
+   * Is it allowed to delete these modules?
+   * <p>
+   * Modules whose {@code .iml} lives outside {@code .idea} are always deletable.
+   * Deletion is only blocked when the whole set of loaded project modules is selected and at least one of them
+   * is stored in {@code .idea}: this stops a user from deleting all modules of a directory-based project at once.
+   */
+  private static boolean modulesCanBeDeleted(Module @NotNull [] modulesToDelete) {
+    if (modulesToDelete.length == 0) {
+      return false; // No need to delete 0 modules
     }
+    // All modules belong to the same project
+    var project = modulesToDelete[0].getProject();
+    var allProjectModules = ProjectUtil.getModules(project);
 
-    for (Module module : modules) {
-      String moduleFile = module.getModuleFilePath();
-      Project project = module.getProject();
-      if (!ProjectKt.isDirectoryBased(project)) {
-        continue;
-      }
-
-      Path ideaDir = ProjectKt.getStateStore(project).getDirectoryStorePath();
-      if (ideaDir != null && PathUtilRt.getParentPath(moduleFile).equals(FileUtil.toSystemIndependentName(ideaDir.toString()))) {
-        return true;
+    var allModulesWillBeDeleted = modulesToDelete.length == allProjectModules.length;
+    if (allModulesWillBeDeleted) {
+      // All modules can only be deleted if they are NOT in .idea dir
+      @Nullable
+      var projectIdeaDir = ProjectKt.getStateStore(project).getDirectoryStorePath();
+      if (projectIdeaDir != null) {
+        for (Module module : modulesToDelete) {
+          if (module.getModuleNioFile().startsWith(projectIdeaDir)) {
+            return false; // At least one module is in .idea, can't delete
+          }
+        }
       }
     }
-    return false;
+    return true; //Either not all modules are selected, or all of them are outside .idea dir -> deletable
   }
 
   @Override
