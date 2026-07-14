5a3ad49c366fe248831f096f7855dd5d09d28bdc
Evgenii Ilichev <evgenii.ilichev@jetbrains.com>
2026-07-14 17:06:37 +0200

    [MRI-4591] switch intellij.platform.lsp.impl.tests
    GitOrigin-RevId: 42ba11af4fb4ac4416c55b3fd90402ab88be4813

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
@@ -1599,6 +1599,7 @@ private val COMMUNITY_AGGREGATOR_BAZEL_MIGRATED_MODULES = listOf(
   "intellij.statsCollector.tests",
   "intellij.platform.jewel.ui.tests",
   "intellij.platform.jps.model.tests",
+  "intellij.platform.lsp.impl.tests",
   "intellij.platform.markdown.utils.tests",
   "intellij.platform.ml.impl.tests",
   "intellij.platform.pluginGraph.tests",
