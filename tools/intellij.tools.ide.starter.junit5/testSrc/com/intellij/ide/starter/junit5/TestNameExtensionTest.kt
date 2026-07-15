1c6a21daf1e48db35cd9361c483c167a6bf7cad7
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2026-07-15 10:02:55 +0200

    testFramework [starter]: fix incorrect caps lock strings hyphenation
    GitOrigin-RevId: 8728bdab4f6cb582d3fc5e7c10b49d2f1e55011e

---
+++ tools/intellij.tools.ide.starter.junit5/testSrc/com/intellij/ide/starter/junit5/TestNameExtensionTest.kt
@@ -0,0 +1,38 @@
+package com.intellij.ide.starter.junit5
+
+import com.intellij.ide.starter.utils.hyphenateTestName
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class TestNameExtensionTest {
+  @Test
+  fun `hyphenate test name handles camel case`() {
+    "testMethodName".hyphenateTestName() shouldBe "test-method-name"
+    "SlowFileOpeningTest".hyphenateTestName() shouldBe "slow-file-opening-test"
+  }
+
+  @Test
+  fun `hyphenate test name keeps acronym prefixes as words`() {
+    "SPLIT-SlowFileOpeningTest".hyphenateTestName() shouldBe "split-slow-file-opening-test"
+    "SPLITSlowFileOpeningTest".hyphenateTestName() shouldBe "split-slow-file-opening-test"
+    "HTTPServerTest".hyphenateTestName() shouldBe "http-server-test"
+  }
+
+  @Test
+  fun `hyphenate test name replaces spaces with hyphens`() {
+    "slow file opening test".hyphenateTestName() shouldBe "slow-file-opening-test"
+    "slow  file   opening test".hyphenateTestName() shouldBe "slow-file-opening-test"
+    "  slow file opening test  ".hyphenateTestName() shouldBe "slow-file-opening-test"
+  }
+
+  @Test
+  fun `hyphenate test name replaces punctuation with hyphens`() {
+    "slow:file(opening), test!".hyphenateTestName() shouldBe "slow-file-opening-test"
+    "slow_file[opening] test".hyphenateTestName() shouldBe "slow-file-opening-test"
+  }
+
+  @Test
+  fun `hyphenate test name preserves path segments`() {
+    "SlowFileOpeningTest/fileOpeningWithDelay".hyphenateTestName() shouldBe "slow-file-opening-test/file-opening-with-delay"
+  }
+}
