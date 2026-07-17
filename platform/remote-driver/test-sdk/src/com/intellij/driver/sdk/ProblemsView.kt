8abb441759d5483070ddbaeb9612cd74c52c9b64
Marina Mădăraș <marina.madaras@jetbrains.com>
2026-07-17 11:25:04 +0200

    [problems-view] IJPL-245334 adapt UI test to work in split mode
    GitOrigin-RevId: 647f15add66501fd4234c9b413987dfc4fe6db5a

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ProblemsView.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ProblemsView.kt
@@ -6,6 +6,7 @@ import com.intellij.driver.client.Remote
 import com.intellij.driver.client.service
 import com.intellij.driver.client.utility
 import com.intellij.driver.model.OnDispatcher
+import com.intellij.driver.model.RdTarget
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
@@ -13,43 +14,43 @@ private const val PROBLEMS_VIEW_UI_MODULE: String = "intellij.problemView.plugin
 private const val PROBLEMS_VIEW_FRONTEND_MODULE: String = "intellij.problemView.plugin/intellij.platform.problemView.frontend"
 private const val PROBLEMS_VIEW_BACKEND_MODULE: String = "intellij.problemView.plugin/intellij.platform.problemView.backend"
 
-@Remote("com.intellij.analysis.problemsView.toolWindow.ProblemsViewToolWindowUtils", plugin = PROBLEMS_VIEW_UI_MODULE)
+@Remote("com.intellij.analysis.problemsView.toolWindow.ProblemsViewToolWindowUtils", plugin = PROBLEMS_VIEW_UI_MODULE, rdTarget = RdTarget.FRONTEND)
 interface ProblemsViewToolWindowUtils {
   fun getToolWindow(project: Project): ToolWindowRef?
   fun getTabById(project: Project, id: String): ProblemsViewTab?
   fun selectTab(project: Project, id: String)
 }
 
-@Remote("com.intellij.openapi.wm.ToolWindow")
+@Remote("com.intellij.openapi.wm.ToolWindow", rdTarget = RdTarget.FRONTEND)
 interface ToolWindowRef {
   fun isVisible(): Boolean
   fun isActive(): Boolean
 }
 
-@Remote("com.intellij.analysis.problemsView.toolWindow.ProblemsViewTab", plugin = PROBLEMS_VIEW_UI_MODULE)
+@Remote("com.intellij.analysis.problemsView.toolWindow.ProblemsViewTab", plugin = PROBLEMS_VIEW_UI_MODULE, rdTarget = RdTarget.FRONTEND)
 interface ProblemsViewTab {
   fun getTabId(): String
 }
 
-@Remote("com.intellij.analysis.problemsView.toolWindow.HighlightingPanel", plugin = PROBLEMS_VIEW_UI_MODULE)
+@Remote("com.intellij.analysis.problemsView.toolWindow.HighlightingPanel", plugin = PROBLEMS_VIEW_UI_MODULE, rdTarget = RdTarget.FRONTEND)
 interface HighlightingPanel : ProblemsViewTab {
   fun getCurrentRoot(): HighlightingFileRoot?
   fun getCurrentFile(): VirtualFile?
 }
 
-@Remote("com.intellij.analysis.problemsView.toolWindow.splitApi.HighlightingFileRoot", plugin = PROBLEMS_VIEW_UI_MODULE)
+@Remote("com.intellij.analysis.problemsView.toolWindow.splitApi.HighlightingFileRoot", plugin = PROBLEMS_VIEW_UI_MODULE, rdTarget = RdTarget.FRONTEND)
 interface HighlightingFileRoot {
   fun getFile(): VirtualFile
 }
 
-@Remote("com.intellij.platform.problemsView.frontend.FrontendProblemsViewHighlightingFileRoot", plugin = PROBLEMS_VIEW_FRONTEND_MODULE)
+@Remote("com.intellij.platform.problemsView.frontend.FrontendProblemsViewHighlightingFileRoot", plugin = PROBLEMS_VIEW_FRONTEND_MODULE, rdTarget = RdTarget.FRONTEND)
 interface ProblemsViewHighlightingFileRoot {
   fun getFile(): VirtualFile
   fun getProblemCount(): Int
   fun getFileProblems(file: VirtualFile): List<HighlightingProblem>
 }
 
-@Remote("com.intellij.platform.problemsView.frontend.FrontendHighlightingProblem", plugin = PROBLEMS_VIEW_FRONTEND_MODULE)
+@Remote("com.intellij.platform.problemsView.frontend.FrontendHighlightingProblem", plugin = PROBLEMS_VIEW_FRONTEND_MODULE, rdTarget = RdTarget.FRONTEND)
 interface HighlightingProblem {
   fun getId(): String
   fun getText(): String
@@ -57,22 +58,26 @@ interface HighlightingProblem {
   fun getColumn(): Int
 }
 
-
-@Remote("com.intellij.platform.problemsView.backend.ProblemLifetimeManager", plugin = PROBLEMS_VIEW_BACKEND_MODULE)
+@Remote("com.intellij.platform.problemsView.backend.ProblemLifetimeManager", plugin = PROBLEMS_VIEW_BACKEND_MODULE, rdTarget = RdTarget.BACKEND)
 interface ProblemLifetimeManager {
   fun hasProblemId(id: String): Boolean
   fun getProblemIdsSize(): Int
 }
 
+fun Driver.openProblemsViewToolWindow() {
+  invokeAction("ActivateProblemsViewToolWindow")
+}
+
 fun Driver.selectProblemsViewTab(id: String, project: Project? = null) {
   val forProject = project ?: singleProject()
-  utility<ProblemsViewToolWindowUtils>().selectTab(forProject, id)
+  utility<ProblemsViewToolWindowUtils>(RdTarget.FRONTEND).selectTab(forProject, id)
 }
 
 fun Driver.getHighlightingPanel(project: Project? = null): HighlightingPanel? {
   val forProject = project ?: singleProject()
   return withContext(OnDispatcher.EDT) {
-    val tab = utility<ProblemsViewToolWindowUtils>().getTabById(forProject, HIGHLIGHTING_PANEL_ID) ?: return@withContext null
+    val tab = utility<ProblemsViewToolWindowUtils>(RdTarget.FRONTEND).getTabById(forProject, HIGHLIGHTING_PANEL_ID)
+              ?: return@withContext null
     cast(tab, HighlightingPanel::class)
   }
 }
@@ -102,16 +107,16 @@ const val HIGHLIGHTING_PANEL_ID: String = "CurrentFile"
 /** Shown-problem ids that do NOT resolve in the backend id store (empty == every shown problem has an id). */
 fun Driver.unresolvedProblemIds(file: VirtualFile, project: Project? = null): List<String> {
   val forProject = project ?: singleProject()
-  val manager = service<ProblemLifetimeManager>(forProject)
+  val manager = service<ProblemLifetimeManager>(forProject, RdTarget.BACKEND)
   return getProblemsViewProblems(file, forProject)
     .map { it.getId() }
     .filter { !manager.hasProblemId(it) }
 }
 
 /** Total number of problem ids in the backend id store. */
-fun Driver.backendProblemIdsSize(project: Project? = null): Int {
+fun Driver.problemIdsSize(project: Project? = null): Int {
   val forProject = project ?: singleProject()
-  return service<ProblemLifetimeManager>(forProject).getProblemIdsSize()
+  return service<ProblemLifetimeManager>(forProject, RdTarget.BACKEND).getProblemIdsSize()
 }
 
 /** Closes the editor for [file] (triggers backend root disposal, which reaps the file's problem ids). */
@@ -121,3 +126,7 @@ fun Driver.closeProblemsViewFile(file: VirtualFile, project: Project? = null) {
     service<FileEditorManager>(forProject).closeFile(file)
   }
 }
+
+fun Driver.findTestFile(relativePath: String): VirtualFile? =
+  if (!isRemDevMode) { findFile(relativePath = relativePath) }
+  else { service<FileEditorManager>(singleProject()).getSelectedTextEditor()?.getVirtualFile() }
