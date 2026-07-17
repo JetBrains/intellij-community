a2238b5bf1355114904e86ba63c87ecf281f394f
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2026-07-17 16:09:02 +0200

    KTIJ-39576 [kotlin] Fix configuring lombok.config from subfolders
    GitOrigin-RevId: 29e7cab3ec6c247f33738cea6b5e569206fb698c

---
+++ plugins/kotlin/lombok/tests/testData/gradle/fixes/addKotlinLombokCompilerPluginWithConfigInSubfolder/after/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+// "Add Kotlin Lombok compiler plugin" "true"
+// TOOL: org.jetbrains.kotlin.idea.lombok.inspections.LombokKotlinCompilerPluginInspection
+
+fun main() {
+    val p = Person()
+    p.name = "Test"
+}
---
+++ plugins/kotlin/lombok/tests/testData/gradle/fixes/addKotlinLombokCompilerPluginWithConfigInSubfolder/before/src/main/java/Person.java
@@ -0,0 +1,7 @@
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+    private int age;
+}
