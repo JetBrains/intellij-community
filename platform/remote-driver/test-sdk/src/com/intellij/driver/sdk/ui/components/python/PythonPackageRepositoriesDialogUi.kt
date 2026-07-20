a711fbceef80d9a0ae1c578c07b9f69c405c94cf
Denis Mashutin <Denis.Mashutin@jetbrains.com>
2026-07-20 16:21:17 +0200

    PCQA-1757 Point repositories UI at the Settings "Package Repositories" page
    Package repository management moved from the standalone "Python Package
    Repositories" dialog into the Settings dialog "Python | Package Repositories"
    page (PY-89838). Target the Settings dialog, switch the executable-list add/
    remove buttons to the toolbar ActionButtons (Add/Delete) and the Name field to
    JBTextField.
    
    GitOrigin-RevId: e18c0d63dc50c6c6cef7273f65526960d17f3e7e

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/python/PythonPackageRepositoriesDialogUi.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/python/PythonPackageRepositoriesDialogUi.kt
@@ -3,17 +3,26 @@ package com.intellij.driver.sdk.ui.components.python
 import com.intellij.driver.sdk.ui.components.ComponentData
 import com.intellij.driver.sdk.ui.components.UiComponent
 import com.intellij.driver.sdk.ui.components.common.IdeaFrameUI
+import javax.swing.JDialog
+import javax.swing.JFrame
 
+/**
+ * Package repository management moved from the standalone "Python Package Repositories" dialog into
+ * the Settings dialog "Python | Package Repositories" page (PY-89838). This component targets that
+ * Settings page; open it via the Python Packages tool window Options menu -> "Repositories...".
+ */
 fun IdeaFrameUI.pythonPackageRepositoriesDialog(func: PythonPackageRepositoriesDialogUi.() -> Unit = {}) =
-  x(PythonPackageRepositoriesDialogUi::class.java) { byTitle("Python Package Repositories") }.apply(func)
+  x(PythonPackageRepositoriesDialogUi::class.java) {
+    and(or(byType(JDialog::class.java), byType(JFrame::class.java)), byAccessibleName("Settings"))
+  }.apply(func)
 
 class PythonPackageRepositoriesDialogUi(data: ComponentData) : UiComponent(data) {
-  val addButton = x { byAccessibleName("Add") }
-  val removeButton = x { byAccessibleName("Remove") }
+  val addButton = x { and(byClass("ActionButton"), byAccessibleName("Add")) }
+  val removeButton = x { and(byClass("ActionButton"), byAccessibleName("Delete")) }
   val okButton = x { byAccessibleName("OK") }
   val applyButton = x { byAccessibleName("Apply") }
   val cancelButton = x { byAccessibleName("Cancel") }
 
-  val nameField = x("//div[@accessiblename='Name:' and @class='JTextField']")
+  val nameField = x("//div[@accessiblename='Name:' and @class='JBTextField']")
   val urlField = x("//div[@accessiblename='Repository URL:' and @class='JBTextField']")
 }
