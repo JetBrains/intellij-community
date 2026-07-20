15e411ac19a4c8cd663ad37f261bd7ed5357d07f
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2026-07-20 16:27:15 +0200

    IDEA-391810 java: Replace static import: preserve trailing comment in package-info.java
    GitOrigin-RevId: 8740468ccd34517061add64ab52d9dd0bbb8f508

--- java/java-tests/testSrc/com/intellij/java/codeInsight/intention/ExpandStaticImportActionHeavyTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/intention/ExpandStaticImportActionHeavyTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package com.intellij.java.codeInsight.intention;
 
@@ -23,6 +23,19 @@ public class ExpandStaticImportActionHeavyTest extends JavaCodeInsightFixtureTes
                              }
                          }""");
     final String name = "package-info";
+    CodeInsightTestUtil.doIntentionTest(myFixture, JavaBundle.message("intention.text.replace.static.import.with.qualified.access.to.0", "Interfaces"), name + ".java", name + "_after.java");
+  }
+
+  public void testPackageInfoTrailingComment() {
+    myFixture.addClass("""
+                         package a;
+
+                         public interface Interfaces {
+                             @interface SomeInterface {
+
+                             }
+                         }""");
+    final String name = "packageInfoTrailingComment";
     CodeInsightTestUtil.doIntentionTest(myFixture, JavaBundle.message("intention.text.replace.static.import.with.qualified.access.to.0", "Interfaces"), name + ".java", name + "_after.java");
   }
 }
