ec2c18285e59079dc97ccdfa47d516c2317ef42a
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2026-07-23 13:42:01 +0900

    misc [kotlin]: Unify module settings between JPS and Gradle for 'kotlin.util.project-model-updater'
    The module compiles with JPS/Bazel locally, yet on CI it builds with Gradle, so its compilation settings must be synchronized.
    
    GitOrigin-RevId: cced77d22944eb408d053ed2b692ca063acb18d2

--- plugins/kotlin/util/project-model-updater/build.gradle.kts
+++ plugins/kotlin/util/project-model-updater/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-	kotlin("jvm") version "2.2.20"
-    kotlin("plugin.serialization") version "2.2.20"
+	kotlin("jvm") version "2.4.10"
+    kotlin("plugin.serialization") version "2.4.10"
     id("application")
 }
 
