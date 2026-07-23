582285df00e05f643df602d6fc538c93ced5402c
Bas Leijdekkers <bas.leijdekkers@jetbrains.com>
2026-07-23 16:52:57 +0200

    IDEA-390511 [groovy] report unnecessary 'final' modifier on 'val'
    GitOrigin-RevId: 94d26bb9e693d213faae72b1198183d6f96dab4e

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/codeInspection/style/GrUnnecessaryFinalModifierInspection.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/codeInspection/style/GrUnnecessaryFinalModifierInspection.kt
@@ -1,11 +1,13 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.groovy.codeInspection.style
 
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiModifier
 import com.intellij.psi.PsiModifierListOwner
 import com.intellij.psi.util.parentOfType
+import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.GrModifier
 import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.GrModifierList
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariableDeclaration
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrRecordDefinition
 
 class GrUnnecessaryFinalModifierInspection : GrUnnecessaryModifierInspection(PsiModifier.FINAL) {
@@ -13,6 +15,8 @@ class GrUnnecessaryFinalModifierInspection : GrUnnecessaryModifierInspection(Psi
   override fun isRedundant(element: PsiElement): Boolean {
     val modifierList = element.parentOfType<GrModifierList>() ?: return false
     val owner = modifierList.parentOfType<PsiModifierListOwner>() ?: return false
-    return owner is GrRecordDefinition && owner.modifierList === modifierList
+    if (owner.modifierList !== modifierList) return false
+    if (owner is GrVariableDeclaration && modifierList.hasModifierProperty(GrModifier.VAL)) return true
+    return owner is GrRecordDefinition
   }
 }
