2f76a1989dcddce789f9cf858b12072d589585a7
Vadim Salavatov <Vadim.Salavatov@jetbrains.com>
2026-07-23 12:12:08 +0000

    Revert "[mermaid] IJPL-250887 declare incompatible-with with third-party plugin to avoid startup failure"
    This reverts commit ae3e0a5586161dcfb9c695934cd2e927b9380240.
    
    GitOrigin-RevId: e271f89970c36c1fb94afcdbd53891b3e05d4d1e

--- platform/platform-tests/testSrc/com/intellij/ide/plugins/CommunityPluginModelTest.kt
+++ platform/platform-tests/testSrc/com/intellij/ide/plugins/CommunityPluginModelTest.kt
@@ -49,7 +49,6 @@ val communityPluginValidationOptions: PluginValidationOptions = PluginValidation
     "com.intellij.jetbrains.client",
     "com.intellij.modules.appcode.ide",
     "com.intellij.modules.rustrover",
-    "dev.tachilabs.mermaidstudio.core", // IJPL-250887
   ),
   componentImplementationClassesToIgnore = setOf(
     "com.intellij.designer.DesignerToolWindowManager",
