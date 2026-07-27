ae96c77b55ab3a0de2aa6b605c360c3512603073
Alexey.Merkulov <alexey.merkulov@jetbrains.com>
2026-07-27 14:57:21 +0200

    [debugger] IJPL-251143 Do not instrument breakpoints with changing local variables
    IJ-CR-215549
    
    GitOrigin-RevId: 18f61afb2c6ad3dae45ac5d1c012ac26e5fdec1c

--- java/java-impl/src/com/intellij/refactoring/extractMethodObject/LightMethodObjectExtractedData.java
+++ java/java-impl/src/com/intellij/refactoring/extractMethodObject/LightMethodObjectExtractedData.java
@@ -6,6 +6,7 @@ import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiType;
+import org.jetbrains.annotations.ApiStatus;
 
 public class LightMethodObjectExtractedData {
   public static final Key<PsiMethod> REFERENCE_METHOD = Key.create("CompilingEvaluatorReferenceMethod");
@@ -14,12 +15,19 @@ public class LightMethodObjectExtractedData {
   private final PsiClass myGeneratedInnerClass;
   private final PsiElement myAnchor;
   private final boolean myUseMagicAccessor;
+  private final boolean myHasOutputVariables;
 
-  public LightMethodObjectExtractedData(String generatedCallText, PsiClass generatedInnerClass, PsiElement anchor, boolean useMagicAccessor) {
+  @ApiStatus.Internal
+  public LightMethodObjectExtractedData(String generatedCallText,
+                                        PsiClass generatedInnerClass,
+                                        PsiElement anchor,
+                                        boolean useMagicAccessor,
+                                        boolean hasOutputVariables) {
     myGeneratedCallText = generatedCallText;
     myGeneratedInnerClass = generatedInnerClass;
     myAnchor = anchor;
     myUseMagicAccessor = useMagicAccessor;
+    myHasOutputVariables = hasOutputVariables;
   }
 
   public PsiElement getAnchor() {
@@ -36,5 +44,10 @@ public class LightMethodObjectExtractedData {
 
   public boolean useMagicAccessor() {
     return myUseMagicAccessor;
+  }
+
+  @ApiStatus.Internal
+  public boolean hasOutputVariables() {
+    return myHasOutputVariables;
   }
 }
