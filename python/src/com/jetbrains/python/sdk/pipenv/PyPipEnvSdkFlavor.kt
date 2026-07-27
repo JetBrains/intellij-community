684f62dde0ab0abc5eab0d0d609aa2ea66b6bf66
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2026-07-27 20:18:11 +0200

    cleanup [python] [mcp]: Use interpreter API instead of bare SDK, fix version obtaining logic.
    New code should use Interpreter API both for environment parsing and version parsing.
    
    GitOrigin-RevId: 4a7230ca85fa0d401208476836e5795d065caf0b

--- python/src/com/jetbrains/python/sdk/pipenv/PyPipEnvSdkFlavor.kt
+++ python/src/com/jetbrains/python/sdk/pipenv/PyPipEnvSdkFlavor.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.python.sdk.pipenv
 
 import com.intellij.python.community.impl.pipenv.PipEnvPyTool
@@ -6,14 +6,12 @@ import com.jetbrains.python.PyInternalExecApi
 import com.jetbrains.python.sdk.flavors.CPythonSdkFlavor
 import com.jetbrains.python.sdk.flavors.PyFlavorData
 import com.jetbrains.python.sdk.flavors.PythonFlavorProvider
-import org.jetbrains.annotations.ApiStatus
 import java.nio.file.Path
 import javax.swing.Icon
 
 
-@ApiStatus.Internal
 @PyInternalExecApi
-object PyPipEnvSdkFlavor : CPythonSdkFlavor<PyFlavorData.Empty>() {
+internal object PyPipEnvSdkFlavor : CPythonSdkFlavor<PyFlavorData.Empty>() {
   override fun getIcon(): Icon = PipEnvPyTool.getInstance().icon
   override fun getFlavorDataClass(): Class<PyFlavorData.Empty> = PyFlavorData.Empty::class.java
 
