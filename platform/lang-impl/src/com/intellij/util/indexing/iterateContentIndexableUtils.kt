c15d623b35d1dbfe80ec27b014f662da352bdd7a
Ilia Malakhov <ilia.malakhov@jetbrains.com>
2026-07-27 18:19:37 +0200

    IJPL-251342 use iterateIndexableContentUnderDirectory in indexable files iterators
    to avoid additional WorkspaceFileIndexEx.getFileInfo calls
    
    GitOrigin-RevId: 8b9793e113042ea5d3a234bf4ebf630de5fbeb55

--- platform/lang-impl/src/com/intellij/util/indexing/iterateContentIndexableUtils.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/iterateContentIndexableUtils.kt
@@ -3,10 +3,8 @@ package com.intellij.util.indexing
 
 import com.intellij.openapi.roots.ContentIterator
 import com.intellij.openapi.roots.ContentIteratorEx
-import com.intellij.openapi.vfs.VirtualFileFilter
 import com.intellij.openapi.vfs.newvfs.CacheAvoidingVirtualFile
 import com.intellij.util.containers.TreeNodeProcessingResult
-import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileIndex
 
 internal fun ContentIterator.unwrapCacheAvoiding(): ContentIterator = ContentIterator { file ->
   when {
@@ -29,10 +27,5 @@ internal fun ContentIteratorEx.unwrapCacheAvoiding(): ContentIteratorEx = Conten
     }
   }
 }
-
-internal fun VirtualFileFilter.andIndexable(workspaceFileIndex: WorkspaceFileIndex): VirtualFileFilter =
-  // run `this` filter second, because it can be deduplicating, shared by multiple iterators
-  // running it first may mark it, so other iterators couldn't process it anymore
-  VirtualFileFilter { file -> workspaceFileIndex.isIndexable(file) }.and(this)
 
 
