04cd6ea8211322b7dc7eaa259afa7c1d15e48229
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2026-07-28 14:16:40 +0200

    KTIJ-37628 [kotlin] Add editor notification for generated kotlin sources
    GitOrigin-RevId: 02bcaeeb2900a893cd56f09d55a0074182ccd98a

---
+++ plugins/kotlin/code-insight/impl-base/src/org/jetbrains/kotlin/idea/codeInsight/KotlinGeneratedSourcesFilter.kt
@@ -0,0 +1,28 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.codeInsight
+
+import com.intellij.ide.projectView.impl.ProjectRootsUtil
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.GeneratedSourcesFilter
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileIndex
+import org.jetbrains.jps.model.java.JavaSourceRootProperties
+import org.jetbrains.kotlin.idea.util.isKotlinFileType
+
+internal class KotlinGeneratedSourcesFilter : GeneratedSourcesFilter() {
+    override fun isGeneratedSource(
+        file: VirtualFile,
+        project: Project
+    ): Boolean {
+        if (!file.isKotlinFileType()) return false
+
+        val workspaceFileIndex = WorkspaceFileIndex.getInstance(project)
+        val contentFileSetRoot =
+            workspaceFileIndex.getContentFileSetRoot(file, true) ?: return false
+
+        val moduleSourceRoot = ProjectRootsUtil.getModuleSourceRoot(contentFileSetRoot, project)
+        val properties =
+            moduleSourceRoot?.getJpsElement()?.getProperties() as? JavaSourceRootProperties ?: return false
+        return properties.isForGeneratedSources
+    }
+}
