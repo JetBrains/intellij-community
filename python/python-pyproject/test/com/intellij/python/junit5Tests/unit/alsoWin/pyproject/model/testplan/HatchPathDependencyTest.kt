ed10ec1ff7ed1026de22fe8c515e10265987b244
Alexey Katsman <alexey.katsman@jetbrains.com>
2026-07-28 14:32:58 +0200

    PY-90798 Support Hatch context-formatted path dependencies
    Resolve {root[:parent...]:uri} references relative to the owning
    pyproject.toml to properly resolve dependencies.
    
    ALso, support Hatch-specific grouped dependency arrays
    (tool.hatch.envs.*.dependencies)
    
    GitOrigin-RevId: 0175ee07b03e41fc5e6902596b3de5421a7d05cb

--- python/python-pyproject/test/com/intellij/python/junit5Tests/unit/alsoWin/pyproject/model/testplan/HatchPathDependencyTest.kt
+++ python/python-pyproject/test/com/intellij/python/junit5Tests/unit/alsoWin/pyproject/model/testplan/HatchPathDependencyTest.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.python.junit5Tests.unit.alsoWin.pyproject.model.testplan
 
+import com.intellij.idea.TestFor
 import com.intellij.python.junit5Tests.framework.PyDefaultTestApplication
 import com.intellij.python.junit5Tests.framework.metaInfo.TestClassInfo
 import com.intellij.python.junit5Tests.unit.alsoWin.pyproject.div
@@ -11,7 +12,6 @@ import com.intellij.testFramework.common.timeoutRunBlocking
 import com.intellij.testFramework.junit5.fixture.projectFixture
 import com.intellij.testFramework.junit5.fixture.tempPathFixture
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 @PyDefaultTestApplication
 @TestClassInfo(contentRootPath ="python-pyproject/test")
@@ -24,15 +24,13 @@ internal class HatchPathDependencyTest {
   private val f by pyProjectTomlSyncFixture(projectFixture, tempDirFixture)
 
   @Test
+  @TestFor(issues = ["PY-90798"])
   fun sanity(): Unit = timeoutRunBlocking {
     f.reloadProject()
-    // PY-90798 Hatch path dependencies with relative paths (context formatters (for example {root:uri} and {root:parent:uri})) are not supported
-    assertThrows<AssertionError> {
-      f.assertProjectStructure(
-        ExpectedModule("my-hatch-monorepo", contentRoot = ".", deps = listOf("package_a", "package_b")),
-        ExpectedModule("package_a", contentRoot = "packages" / "package_a"),
-        ExpectedModule("package_b", contentRoot = "packages" / "package_b", deps = listOf("package_a")),
-      )
-    }
+    f.assertProjectStructure(
+      ExpectedModule("my-hatch-monorepo", contentRoot = ".", deps = listOf("package_a", "package_b"), sourceRoots = listOf(".")),
+      ExpectedModule("package_a", contentRoot = "packages" / "package_a"),
+      ExpectedModule("package_b", contentRoot = "packages" / "package_b", deps = listOf("package_a")),
+    )
   }
 }
