b20e55a9e0c20084d3b426a1029de1399f84a8cc
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2026-07-29 17:51:54 +0200

    cleanup [threading]: Remove unsued field
    GitOrigin-RevId: 93b0f77eeed96a531f136b171e08bc7614effb70

--- platform/util/src/com/intellij/concurrency/threadContext.kt
+++ platform/util/src/com/intellij/concurrency/threadContext.kt
@@ -24,7 +24,6 @@ import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.ApiStatus.Experimental
 import org.jetbrains.annotations.ApiStatus.Internal
 import java.util.concurrent.Callable
-import java.util.concurrent.atomic.AtomicInteger
 import java.util.function.Consumer
 import java.util.function.Function
 import java.util.function.Supplier
@@ -266,8 +265,6 @@ If this behavior is unexpected, please consult the documentation for com.intelli
              Throwable("Querying stacktrace"))
   }
 }
-
-private val counter = AtomicInteger()
 
 /**
  * Do not use this function -- it is invisible in stacktraces, and it complicates the debugging of erroneously dropped thread context.
