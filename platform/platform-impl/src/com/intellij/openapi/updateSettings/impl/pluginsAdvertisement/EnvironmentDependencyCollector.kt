c92ee9ab0e21f5aa7ffc6a136d162ca6efd03171
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2026-07-29 11:50:27 +0200

    [fus] IJPL-251495 New CLI install usage collector
    (cherry picked from commit c13e3c73397748f1449a3e7a0774f7d2c470ab5b)
    IJ-CR-218650
    (cherry picked from commit e1fda984eebc3e73f9e09187fd59316b47e356d5)
    
    GitOrigin-RevId: 48c19ea6cd123134b4744b35a8097650ef75b42d

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/pluginsAdvertisement/EnvironmentDependencyCollector.kt
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/pluginsAdvertisement/EnvironmentDependencyCollector.kt
@@ -5,15 +5,7 @@ import com.intellij.ide.IdeBundle
 import com.intellij.ide.plugins.DependencyCollector
 import com.intellij.ide.plugins.DependencyInformation
 import com.intellij.openapi.project.Project
-import com.intellij.util.EnvironmentUtil
-import com.intellij.util.system.OS
-import org.jetbrains.annotations.ApiStatus
-import java.nio.file.FileSystems
-import java.nio.file.Files
-import java.nio.file.InvalidPathException
-import java.nio.file.Path
-import kotlin.io.path.isExecutable
-import kotlin.io.path.isRegularFile
+import com.intellij.openapi.util.EnvironmentScanner
 
 internal class EnvironmentDependencyCollector : DependencyCollector {
   private val ALLOWED_EXECUTABLES: List<String> = listOf(
@@ -34,44 +26,5 @@ internal class EnvironmentDependencyCollector : DependencyCollector {
     return ALLOWED_EXECUTABLES
       .filter { EnvironmentScanner.hasToolInLocalPath(pathNames, it) }
       .map { DependencyInformation(it, IdeBundle.message("plugins.configurable.suggested.features.executable", it)) }
-  }
-}
-
-@ApiStatus.Internal
-object EnvironmentScanner {
-  fun getPathNames(): List<Path> {
-    val fs = FileSystems.getDefault()
-
-    @Suppress("IO_FILE_USAGE")
-    val pathDelimiter = java.io.File.pathSeparatorChar
-
-    val pathNames = EnvironmentUtil.getEnvironmentMap()["PATH"]?.split(pathDelimiter)
-      ?.mapNotNull {
-        try {
-          fs.getPath(it)
-        }
-        catch (_: InvalidPathException) {
-          null
-        }
-      }
-      ?.filter(Files::exists)
-    return pathNames ?: emptyList()
-  }
-
-  fun hasToolInLocalPath(pathNames: List<Path>, executableWithoutExt: String): Boolean {
-    val baseNames = if (OS.CURRENT == OS.Windows) {
-      sequenceOf(".bat", ".cmd", ".com", ".exe")
-        .map { exeSuffix -> executableWithoutExt + exeSuffix }
-    }
-    else {
-      sequenceOf(executableWithoutExt)
-    }
-
-    return pathNames.asSequence()
-      .flatMap { pathEntry ->
-        baseNames.map { basename -> pathEntry.resolve(basename) }
-      }
-      .filter(Path::isRegularFile)
-      .any(Path::isExecutable)
   }
 }
