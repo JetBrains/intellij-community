61904c9bed8f9a4be0ef2cff8985eed14458e08a
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2026-08-03 18:16:10 +0200

    refactoring [platform]: protecting proxy settings modification API
    GitOrigin-RevId: 38cb1fab6a56aee99001712ce196afdd36e934e2

--- platform/platform-api/src/com/intellij/util/net/ProxySettings.kt
+++ platform/platform-api/src/com/intellij/util/net/ProxySettings.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.net
 
 import com.intellij.openapi.application.ApplicationManager
@@ -22,7 +22,7 @@ interface ProxySettings {
 
   fun getProxyConfiguration(): ProxyConfiguration
 
-  @ApiStatus.Experimental // maybe make it internal if we decide that plugins must not edit user settings
+  @ApiStatus.Internal
   fun setProxyConfiguration(proxyConfiguration: ProxyConfiguration)
 }
 
