f0ccf7c5747b25e50ded591bbb763d67ed8e8944
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2026-08-04 13:16:34 +0200

    refactoring [UI]: Move ComponentPanelTestAction to a dedicated package
    GitOrigin-RevId: e9639ff8fb69f649361f26e5f3b0091f15a4ae12

--- platform/platform-impl/internal/src/com/intellij/internal/ui/componentPanelTestAction/ComponentPanelTestAction.java
+++ platform/platform-impl/internal/src/com/intellij/internal/ui/componentPanelTestAction/ComponentPanelTestAction.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.internal.ui;
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.internal.ui.componentPanelTestAction;
 
 import com.google.common.collect.ImmutableList;
 import com.intellij.icons.AllIcons;
