4ab116be6c0354eb5ae3c32602e50d36477f8b10
Ekaterina Berezhko <ekaterina.berezhko@jetbrains.com>
2026-08-04 13:45:35 +0300

    [Markdown][IJPL-249154] Move "Find in Preview" action to frontend
    GitOrigin-RevId: 296a475a2fa3ce1d76f28b0593985e10f6663d23

--- plugins/markdown/core/frontend/src/com/intellij/markdown/frontend/actions/FindInPreviewAction.kt
+++ plugins/markdown/core/frontend/src/com/intellij/markdown/frontend/actions/FindInPreviewAction.kt
@@ -1,9 +1,10 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.intellij.plugins.markdown.ui.actions
+package com.intellij.markdown.frontend.actions
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
+import org.intellij.plugins.markdown.ui.actions.MarkdownActionUtil
 
 internal class FindInPreviewAction : DumbAwareAction() {
   override fun actionPerformed(event: AnActionEvent) {
