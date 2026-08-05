3fb83d83be5f184d4c93a8dfff4b51a94492cc98
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2026-08-05 09:54:54 +0200

    [kotlin] Upgrade project-model-updater Gradle script to JDK 21 (KTIJ-39831)
    GitOrigin-RevId: 0fc6dfa91b457072a07e0bf806b01844a83d7492

--- plugins/kotlin/util/project-model-updater/build.gradle.kts
+++ plugins/kotlin/util/project-model-updater/build.gradle.kts
@@ -1,3 +1,4 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 plugins {
 	kotlin("jvm") version "2.4.10"
     kotlin("plugin.serialization") version "2.4.10"
@@ -17,7 +18,7 @@ dependencies {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 sourceSets.main.configure {
