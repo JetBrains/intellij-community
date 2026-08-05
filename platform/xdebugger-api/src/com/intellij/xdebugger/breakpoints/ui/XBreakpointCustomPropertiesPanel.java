a563125a04af906a86e0276c61c33b7e6d140254
Mikhail.Ustinov <mikhail.ustinov@jetbrains.com>
2026-08-05 17:14:29 +0200

    [Native Debugger] RIDER-89680 Mark breakpoint name hiding API internal
    Keep hidesBreakpointNameLabel() available to the breakpoint properties editor while preventing it from becoming a supported plugin API.
    
    GitOrigin-RevId: 68278f24cb00688fd61b6c1b1a79663558da70cf

--- platform/xdebugger-api/src/com/intellij/xdebugger/breakpoints/ui/XBreakpointCustomPropertiesPanel.java
+++ platform/xdebugger-api/src/com/intellij/xdebugger/breakpoints/ui/XBreakpointCustomPropertiesPanel.java
@@ -3,6 +3,7 @@ package com.intellij.xdebugger.breakpoints.ui;
 
 import com.intellij.openapi.Disposable;
 import com.intellij.xdebugger.breakpoints.XBreakpoint;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.JComponent;
@@ -27,6 +28,7 @@ public abstract class XBreakpointCustomPropertiesPanel<B extends XBreakpoint<?>>
    * Whether this panel already identifies the breakpoint and the generic breakpoint name/description label should be
    * hidden while it is shown. Opt-in: returns {@code false} by default, so existing panels keep the standard name label.
    */
+  @ApiStatus.Internal
   public boolean hidesBreakpointNameLabel() {
     return false;
   }
