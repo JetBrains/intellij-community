5d2a1f76cf06a565a3bee57e24bbee00b838fb86
Wellington Pereira <wellington.costa128@gmail.com>
2026-08-06 18:14:27 -0300

    [JEWEL-1383] Migrate Jewel UI tests to Compose test v2 API
    closes https://github.com/JetBrains/intellij-community/pull/3603
    
    GitOrigin-RevId: 0986e4461d524a74b458de0f9761c1c5720dc1a0

--- platform/jewel/ui-tests/src/test/kotlin/org/jetbrains/jewel/ui/component/SplitButtonTest.kt
+++ platform/jewel/ui-tests/src/test/kotlin/org/jetbrains/jewel/ui/component/SplitButtonTest.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.test.assertHeightIsEqualTo
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertWidthIsAtLeast
 import androidx.compose.ui.test.assertWidthIsEqualTo
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.unit.Dp
