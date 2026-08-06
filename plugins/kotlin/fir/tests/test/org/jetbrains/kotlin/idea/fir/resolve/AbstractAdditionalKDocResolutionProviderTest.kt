761a960e1060a75f05c54c98a97d36c2d5300b02
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-08-06 11:47:37 +0200

    KTIJ-39782 [kotlin] migrate AdditionalKDocResolutionProvider -> KaAdditionalKDocResolutionProvider
    GitOrigin-RevId: 2501fb727f3e3b5af543976ae5036f2c05fbf387

--- plugins/kotlin/fir/tests/test/org/jetbrains/kotlin/idea/fir/resolve/AbstractAdditionalKDocResolutionProviderTest.kt
+++ plugins/kotlin/fir/tests/test/org/jetbrains/kotlin/idea/fir/resolve/AbstractAdditionalKDocResolutionProviderTest.kt
@@ -1,21 +1,23 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.fir.resolve
 
 import com.intellij.openapi.application.ApplicationManager
 import org.jetbrains.kotlin.analysis.api.KaSession
-import org.jetbrains.kotlin.analysis.api.symbols.AdditionalKDocResolutionProvider
+import org.jetbrains.kotlin.analysis.api.KaSpiExtensionPoint
+import org.jetbrains.kotlin.analysis.api.symbols.KaAdditionalKDocResolutionProvider
 import org.jetbrains.kotlin.analysis.api.symbols.KaSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.symbol
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtElement
 
 abstract class AbstractAdditionalKDocResolutionProviderTest : AbstractFirReferenceResolveTest() {
-    private object AdditionalKDocResolutionProviderForTest: AdditionalKDocResolutionProvider {
+    private object AdditionalKDocResolutionProviderForTest: KaAdditionalKDocResolutionProvider {
         /**
          * This function returns symbols whose name is the same as `fqName.shortName().asString()` among declarations
          * in the file where [contextElement] is located. Note that the symbols does not have FqNames that matches
          * FqName of [contextElement]. Therefore, this can show AdditionalKDocResolutionProvider works as we intended.
          */
+        @KaSpiExtensionPoint
         override fun resolveKdocFqName(
             analysisSession: KaSession,
             fqName: FqName,
@@ -31,7 +33,7 @@ abstract class AbstractAdditionalKDocResolutionProviderTest : AbstractFirReferen
 
     override fun setUp() {
         super.setUp()
-        ApplicationManager.getApplication().extensionArea.getExtensionPoint(AdditionalKDocResolutionProvider.EP_NAME)
+        ApplicationManager.getApplication().extensionArea.getExtensionPoint<KaAdditionalKDocResolutionProvider>("org.jetbrains.kotlin.kaAdditionalKDocResolutionProvider")
             .registerExtension(AdditionalKDocResolutionProviderForTest, testRootDisposable)
     }
 }
