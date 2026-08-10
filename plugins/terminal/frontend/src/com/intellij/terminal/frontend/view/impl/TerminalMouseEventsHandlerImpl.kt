5f204dedd849d2581900490f57063982dbd14e7c
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-08-10 09:48:53 +0300

    [terminal] IJPL-251692 Refactor: make mouse reporting handling more verbose
    GitOrigin-RevId: 97ecdf20c498b6e30c6d568998dd2d2f091c044a

--- plugins/terminal/frontend/src/com/intellij/terminal/frontend/view/impl/TerminalMouseEventsHandlerImpl.kt
+++ plugins/terminal/frontend/src/com/intellij/terminal/frontend/view/impl/TerminalMouseEventsHandlerImpl.kt
@@ -13,16 +13,25 @@ internal class TerminalMouseEventsHandlerImpl(
   private val session: Deferred<TerminalSession>,
 ) : TerminalMouseEventsHandler {
   override fun onMouseEvent(x: Int, y: Int, event: MouseEvent) {
-    // Some other handler may already consume this event, for example, hyperlinks logic.
-    // Do not send a mouse report to the process in this case.
-    if (event.isConsumed) return
+    if (event.isConsumed) {
+      // Some other handler already consumed this event, for example, hyperlinks logic.
+      // Do not send a mouse report to the process in this case.
+      return
+    }
 
-    val terminalSession = session.getNow() ?: return
-    val encodedEvent = terminalSession.processMouseEvent(
-      event,
-      x,
-      y,
-    ) ?: return
+    val terminalSession = session.getNow()
+    if (terminalSession == null) {
+      // Session is not yet available, so we can just ignore the event because there is no terminal process to send it to.
+      return
+    }
+
+    val encodedEvent = terminalSession.processMouseEvent(event, x, y)
+    if (encodedEvent == null) {
+      // Null means that mouse reporting is not enabled now or Shift modifier is used,
+      // so we leave the event to the editor logic (for example, context menu or text selection).
+      return
+    }
+
     terminalInput.sendBytes(encodedEvent)
     if (event is MouseWheelEvent) {
       editor.selectionModel.removeSelection()
