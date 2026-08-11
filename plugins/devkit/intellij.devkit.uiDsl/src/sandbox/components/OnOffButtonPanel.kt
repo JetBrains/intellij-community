25f2c8879b76c7591d9e0da74f46fdea5acf6e59
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2026-08-11 10:53:10 +0200

    Style IntelliJ Platform: new design for OnOffButtonPanel.kt for Islands themes
    - Fixed merge problems
    
    GitOrigin-RevId: fc86701dd6bcc60459cc924da1829b19d4f02fc3

--- plugins/devkit/intellij.devkit.uiDsl/src/sandbox/components/OnOffButtonPanel.kt
+++ plugins/devkit/intellij.devkit.uiDsl/src/sandbox/components/OnOffButtonPanel.kt
@@ -1,8 +1,9 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.internal.ui.sandbox.components
 
-import com.intellij.internal.ui.sandbox.UISandboxPanel
-import com.intellij.internal.ui.sandbox.getStateText
+import com.intellij.devkit.uiDsl.DevkitUiDslBundle
+import com.intellij.devkit.uiDsl.sandbox.UISandboxPanel
+import com.intellij.devkit.uiDsl.sandbox.getStateText
 import com.intellij.openapi.Disposable
 import com.intellij.ui.components.OnOffButton
 import com.intellij.ui.dsl.builder.panel
@@ -14,7 +15,7 @@ internal class OnOffButtonPanel : UISandboxPanel {
 
   override fun createContent(disposable: Disposable): JComponent {
     return panel {
-      group("States") {
+      group(DevkitUiDslBundle.message("sandbox.states")) {
         for (isEnabled in listOf(true, false)) {
           for (isSelected in listOf(false, true)) {
             row {
