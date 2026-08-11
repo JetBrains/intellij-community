49c6af86d0419dd52290ce0e9ca3eb5e08ad8373
Bas Leijdekkers <bas.leijdekkers@jetbrains.com>
2026-08-11 14:51:53 +0200

    IDEA-390511 [groovy] prevent duplicate 'final' or no keyword at all when introducing variable
    GitOrigin-RevId: df0db31271711e79c21a033af1c0ea955368040f

--- plugins/groovy/src/org/jetbrains/plugins/groovy/refactoring/introduce/GrFinalListener.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/refactoring/introduce/GrFinalListener.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.groovy.refactoring.introduce;
 
 import com.intellij.codeInsight.lookup.LookupManager;
@@ -22,6 +8,7 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiModifier;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.GrModifierList;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariable;
 import org.jetbrains.plugins.groovy.lang.psi.api.types.GrTypeElement;
@@ -30,33 +17,32 @@ import org.jetbrains.plugins.groovy.lang.psi.api.types.GrTypeElement;
  * @author Max Medvedev
  */
 public class GrFinalListener {
-  private final Editor myEditor;
   private static final Logger LOG = Logger.getInstance(GrFinalListener.class);
+  private final Editor myEditor;
 
   public GrFinalListener(Editor editor) {
     myEditor = editor;
   }
 
-  public void perform(final boolean generateFinal, GrVariable variable) {
-    perform(generateFinal, PsiModifier.FINAL, variable);
-  }
-
-  public void perform(final boolean generateFinal, final String modifier, final GrVariable variable) {
-    final Document document = myEditor.getDocument();
-    LOG.assertTrue(variable != null);
+  public void perform(boolean generateFinal, @NotNull GrVariable variable) {
     final GrModifierList modifierList = variable.getModifierList();
     LOG.assertTrue(modifierList != null);
+    if (modifierList.hasModifierProperty(PsiModifier.FINAL) == generateFinal) return;
+    if (modifierList.getModifiers().length == 1) return;
+
+    final Document document = myEditor.getDocument();
     final int textOffset = modifierList.getTextOffset();
 
     final Runnable runnable = () -> {
       if (generateFinal) {
         final GrTypeElement typeElement = variable.getTypeElementGroovy();
         final int typeOffset = typeElement != null ? typeElement.getTextOffset() : textOffset;
-        document.insertString(typeOffset, modifier + " ");
+        document.insertString(typeOffset, PsiModifier.FINAL + " ");
       }
       else {
-        final int idx = modifierList.getText().indexOf(modifier);
-        document.deleteString(textOffset + idx, textOffset + idx + modifier.length() + 1);
+        final int idx = modifierList.getText().indexOf(PsiModifier.FINAL);
+        if (idx < 0) return;
+        document.deleteString(textOffset + idx, textOffset + idx + PsiModifier.FINAL.length() + 1);
       }
     };
     final LookupImpl lookup = (LookupImpl)LookupManager.getActiveLookup(myEditor);
