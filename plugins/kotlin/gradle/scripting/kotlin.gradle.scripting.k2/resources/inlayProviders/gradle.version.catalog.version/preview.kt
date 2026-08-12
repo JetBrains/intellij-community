abcb646930d146182ff072cab6e2d7c21be41ae5
Vlad Koshkin <vladislav.koshkin@jetbrains.com>
2026-08-12 20:28:26 +0200

    [kotlin] KTIJ-39944 change GradleVersionCatalogVersionInlayHintsProvider group
    GitOrigin-RevId: 1be3ff64afdcbda9be4ebea0bb56ba8856f6594e

---
+++ plugins/kotlin/gradle/scripting/kotlin.gradle.scripting.k2/resources/inlayProviders/gradle.version.catalog.version/preview.kt
@@ -0,0 +1,4 @@
+dependencies {
+    implementation(libs.hyperscript.org)/*<# org.hyperscript:hyperscript:0.9.12 #>*/
+    testImplementation(libs.junit.jupiter)/*<# org.junit.jupiter:junit-jupiter:5.10.2 #>*/
+}
