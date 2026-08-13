1b8403e5b98f6d06d68e9ca29320e10cbcddea76
Anna Solomennik <anna.solomennik@jetbrains.com>
2026-08-13 07:10:31 +0000

    IJPL-93440 Markdown: Create link from selection: do not create link in code snippets etc
    GitOrigin-RevId: 4cf115e6079fdd241167ebbd1677cd8c56d5bdc9

--- plugins/markdown/core/src/org/intellij/plugins/markdown/lang/MarkdownLanguageUtils.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/lang/MarkdownLanguageUtils.kt
@@ -4,7 +4,10 @@ import com.intellij.lang.Language
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.fileTypes.FileTypeRegistry
+import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 
 fun Language.isMarkdownLanguage(): Boolean {
   return this == MarkdownLanguage.INSTANCE
@@ -12,6 +15,14 @@ fun Language.isMarkdownLanguage(): Boolean {
 
 fun Language.supportsMarkdown(dataContext: DataContext? = null): Boolean {
   return MarkdownCompatibilityChecker.EP_NAME.extensionList.any { it.isSupportedContext(this, dataContext) }
+}
+
+fun PsiElement.supportsMarkdown(): Boolean {
+  return MarkdownCompatibilityChecker.EP_NAME.extensionList.any { it.isSupportedElement(this) }
+}
+
+fun PsiFile.supportsMarkdown(range: TextRange): Boolean {
+  return MarkdownCompatibilityChecker.EP_NAME.extensionList.any { it.isSupportedRange(this, range) }
 }
 
 fun FileType.isMarkdownType(): Boolean {
