ecd958f56e9b2e987e5bd43f5cdd566b7030a301
Nikita Paniukhin <nikita.paniukhin@jetbrains.com>
2026-08-14 18:35:12 +0200

    PY-90014 pydantic: resolve fields in decorators with field-name argument
    Add a Psi Reference for string literal field-name arguments in
    `@field_validator`, `@field_serializer`, and `@validator` (v1) decorators
    inside Pydantic models.
    
    - Resolve field names to field declarations
    - Completion for field names
    
    GitOrigin-RevId: 0f1c6b9dabc048d3709ac3ef531dee4533c1e281

---
+++ python/testData/stubs/pydantic/class_validators.py
@@ -0,0 +1,4 @@
+#  Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+# Pydantic v1 location of `validator`
+def validator(*fields: str, pre: bool = False, each_item: bool = False, always: bool = False, check_fields: bool = True): ...
