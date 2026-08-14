3ae383ad9bfba6cc4552b5e6fc25c1a3f9d6a2c2
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2026-08-14 19:08:32 +0200

    [plugins] IJPL-249174 PluginSetBuilderDsl: drop unused buildPluginManagerState method
    GitOrigin-RevId: 873512907f5aec40e6b2b6db6601d424a5a6eec1

--- platform/platform-tests/testSrc/com/intellij/platform/pluginSystem/testFramework/PluginSetBuilderDsl.kt
+++ platform/platform-tests/testSrc/com/intellij/platform/pluginSystem/testFramework/PluginSetBuilderDsl.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.pluginSystem.testFramework
 
-import com.intellij.ide.plugins.PluginManagerState
 import com.intellij.ide.plugins.PluginSet
 import com.intellij.platform.testFramework.plugins.PluginBuilderDsl
 import com.intellij.platform.testFramework.plugins.PluginSpecBuilder
@@ -14,15 +13,9 @@ import com.intellij.platform.testFramework.plugins.plugin as buildPlugin
  * @param pluginsDirPath the directory where the plugins will be stored; maybe in an in-memory filesystem obtained via
  *                       [com.intellij.testFramework.rules.InMemoryFsExtension]
  */
-//todo: move this to some testFramework module
 fun buildPluginSet(pluginsDirPath: Path, configureClassLoaders: Boolean = true, builder: PluginSetSpecBuilder.() -> Unit): PluginSet {
   builder(PluginSetSpecBuilder(pluginsDirPath))
   return PluginSetTestBuilder.fromPath(pluginsDirPath).build(configureClassLoaders)
-}
-
-fun buildPluginManagerState(pluginsDirPath: Path, configureClassLoaders: Boolean = true, builder: PluginSetSpecBuilder.() -> Unit): PluginManagerState {
-  builder(PluginSetSpecBuilder(pluginsDirPath))
-  return PluginSetTestBuilder.fromPath(pluginsDirPath).buildManagerState(configureClassLoaders)
 }
 
 @PluginBuilderDsl
