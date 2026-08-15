5087c6d60e534a2eef2a5ecce850a6493b35c51d
Brice Dutheil <brice.dutheil@gmail.com>
2026-08-16 01:45:49 +0200

    chore: Additional JUnit report tests on AntImportTest

--- java/java-tests/testSrc/com/intellij/java/execution/AntImportTest.java
+++ java/java-tests/testSrc/com/intellij/java/execution/AntImportTest.java
@@ -191,6 +191,65 @@ public class AntImportTest extends BaseSMTRunnerTestCase {
     assertContains(failed.getStacktrace(), "java.lang.IllegalStateException: rerun errored");
   }
 
+  public void testNestedSuitesPreserveHierarchyAndResults() throws Exception {
+    SMTestProxy.SMRootTestProxy rootNode = parseTestResultFixture("nested-junit-report.xml");
+
+    assertEquals(1, rootNode.getChildren().size());
+    SMTestProxy allTests = rootNode.getChildren().getFirst();
+    assertEquals("idea.import.AllChecks", allTests.getName());
+    assertEquals(2, allTests.getChildren().size());
+
+    SMTestProxy fastChecks = allTests.getChildren().getFirst();
+    assertEquals("idea.import.FastChecks", fastChecks.getName());
+    assertEquals(1, fastChecks.getChildren().size());
+    SMTestProxy arithmeticChecks = fastChecks.getChildren().getFirst();
+    assertEquals("idea.import.ArithmeticChecks", arithmeticChecks.getName());
+    assertEquals(4, arithmeticChecks.getChildren().size());
+    assertTrue(findTest(rootNode, "reportsUnexpectedProduct").isDefect());
+    assertTrue(findTest(rootNode, "skipsDivisionByZero").isIgnored());
+    assertTrue(findTest(rootNode, "reportsMissingOperand").isDefect());
+    MockPrinter fastChecksPrinter = MockPrinter.fillPrinter(fastChecks);
+    assertContains(fastChecksPrinter.getStdOut(), "Arithmetic checks produced diagnostic output");
+    assertContains(fastChecksPrinter.getStdOut(), "Fast checks finished");
+    assertContains(fastChecksPrinter.getStdErr(), "Arithmetic checks produced diagnostic warnings");
+
+    SMTestProxy serviceChecks = allTests.getChildren().get(1);
+    assertEquals("idea.import.ServiceChecks", serviceChecks.getName());
+    assertEquals(3, serviceChecks.getChildren().size());
+  }
+
+  @SuppressWarnings("UnnecessaryUnicodeEscape")
+  public void testProblemStringsArePreserved() throws Exception {
+    SMTestProxy.SMRootTestProxy rootNode = parseTestResultFixture("unusual-strings-junit-report.xml");
+
+    SMTestProxy failure = findTest(rootNode, "rendersCafeComparison");
+    assertEquals("\uFEFFExpected \u2018caf\u00E9\u2019 but was \u2018cafe\u2019", failure.getErrorMessage());
+    assertContains(failure.getStacktrace(), "Terminal-style markers: [31mDIFFERENT[0m");
+    MockPrinter failurePrinter = MockPrinter.fillPrinter(failure);
+    assertContains(failurePrinter.getStdOut(), "[32mencoded stdout[0m");
+    assertContains(failurePrinter.getStdErr(), "decoder warning [33mCHECK INPUT[0m");
+
+    SMTestProxy error = findTest(rootNode, "readsSymbol\uD834\uDD1EWithoutLoss");
+    assertEquals("java.nio.charset.MalformedInputException: Input length = 1", error.getErrorMessage());
+
+    SMTestProxy flaky = findTest(rootNode, "recoversAfterRetry   ");
+    assertEquals("recoversAfterRetry   ", flaky.getName());
+    MockPrinter flakyPrinter = MockPrinter.fillPrinter(flaky);
+    assertContains(flakyPrinter.getStdOut(), "retry output [32mRECOVERED[0m");
+    assertContains(flakyPrinter.getStdErr(), "first attempt compared decomposed text");
+    assertContains(flakyPrinter.getStdErr(), "second attempt rejected the byte sequence");
+
+    SMTestProxy retryChecks = rootNode.getChildren().get(1);
+    assertEquals("dev.acme.RetryTextChecks", retryChecks.getName());
+    assertEquals(2, retryChecks.getChildren().size());
+    SMTestProxy retried = findTest(rootNode, "continuesAfterReplacement\uFFFD");
+    assertTrue(retried.isPassed());
+    MockPrinter retriedPrinter = MockPrinter.fillPrinter(retried);
+    assertContains(retriedPrinter.getStdErr(), "first run saw a replacement character");
+    assertContains(retriedPrinter.getStdErr(), "second run reset the decoder");
+    assertTrue(findTest(rootNode, "stillRunsAfterOddName").isPassed());
+  }
+
   private static @NotNull SMTestProxy findTest(@NotNull SMTestProxy.SMRootTestProxy rootNode, @NotNull String testName) {
     SMTestProxy test = (SMTestProxy)findTestByName(testName, rootNode);
     assertNotNull(test);
