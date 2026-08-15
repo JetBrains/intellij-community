49b50ad01c784af47d4ea5bfa1d61dbc9c07731c
Evgenii Ilichev <evgenii.ilichev@jetbrains.com>
2026-08-15 18:07:26 +0200

    [MRI-4591] switch intellij.python.pyproject.tests
    GitOrigin-RevId: 9cba2b80207a31f6d9eae6eaccd000c0830c3304

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
@@ -1593,6 +1593,7 @@ private val COMMUNITY_AGGREGATOR_BAZEL_MIGRATED_MODULES = listOf(
   "intellij.python.community.services.internal.impl.tests",
   "intellij.python.community.services.shared.tests",
   "intellij.python.processOutput.frontend.tests",
+  "intellij.python.pyproject.tests",
   "intellij.python.sdk.tests",
   "intellij.python.test.env.junit5",
   "intellij.python.pytools.tests",
