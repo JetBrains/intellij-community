cfea13ac6dfb37f7c505d95300f5ac1b68a426c0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2026-08-16 23:38:14 +0200

    cleanup remote driver: remove unused catch binding
    Use an unnamed catch parameter where reconnect failure is intentionally reduced to a null remote result.
    
    IJ-MR-184958 IJ-MR-184993 IJ-MR-179029 IJ-MR-184126 IJ-MR-181153 IJ-MR-146078 IJ-MR-175479 IJ-MR-186058 IJ-MR-193195 IJ-MR-196957 IJ-MR-199124 IJ-MR-197441 IJ-MR-204135 IJ-MR-204674 IJ-MR-205883 IJ-MR-208539 IJ-MR-217652
    
    GitOrigin-RevId: f9279db708ada7678767c52e038a10fe73c63e75

--- platform/remote-driver/client/src/com/intellij/driver/client/impl/DriverImpl.kt
+++ platform/remote-driver/client/src/com/intellij/driver/client/impl/DriverImpl.kt
@@ -426,7 +426,7 @@ open class DriverImpl(host: JmxHost, override val isRemDevMode: Boolean, overrid
         try {
           invoker.cleanup(runAsSession.id)
         }
-        catch (e: JmxCallException) {
+        catch (_: JmxCallException) {
           System.err.println("Unable to cleanup remote Driver session")
         }
 
