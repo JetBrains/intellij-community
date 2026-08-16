7f6380d835c65d8567ba76170c7b87069e348547
Valeria Golovina <valeria.golovina@jetbrains.com>
2026-08-16 21:51:21 +0300

    tests [git] fixed muted test
    GitOrigin-RevId: 486e46a332a194225a4e805cd098c911b904d83b

--- plugins/git4idea/tests/git4idea/log/GitLogIndexTest.kt
+++ plugins/git4idea/tests/git4idea/log/GitLogIndexTest.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.log
 
-import com.intellij.idea.IgnoreJUnit3
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vcs.Executor.append
@@ -98,7 +97,6 @@ abstract class GitLogIndexTest(val useSqlite: Boolean) : GitSingleRepoTest() {
     TestCase.assertEquals(expectedMetadata.presentation(), actualMetadata.presentation())
   }
 
-  @IgnoreJUnit3
   fun `test forward index with batch api`() {
     val file = "file.txt"
     tac(file)
@@ -110,7 +108,11 @@ abstract class GitLogIndexTest(val useSqlite: Boolean) : GitSingleRepoTest() {
 
     val collector = CollectConsumer<VcsCommitMetadata>()
     logProvider.readMetadata(repo.root, commits.map { getCommitHash(it) }, collector)
-    val expectedPresentation = collector.result.joinToString("\n\n") { it.presentation() }
+    // skips initial commit for sqlite since commits with no parents are not indexed
+    // because it's not possible to distinguish between commits without parents and commits that were not indexed yet
+    val expectedPresentation = collector.result.joinToString("\n\n") { metadata ->
+      metadata.presentation().takeUnless { useSqlite && it.contains("initial") } ?: "NOT LOADED"
+    }
 
     val actualMetadata = IndexedDetails.createMetadata(commits.toSet(), dataGetter, storage,
                                                        project.getService(VcsLogObjectsFactory::class.java))
