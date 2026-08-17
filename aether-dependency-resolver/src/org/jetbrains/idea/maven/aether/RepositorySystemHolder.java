1ddbbfd6a77d2f5d627a22b622ead3e5a6542363
Ilia Kirianovskii <Ilia.Kirianovskii@jetbrains.com>
2026-08-17 11:52:27 +0000

    fixup IJPL-251892 prevent Aether lock starvation (MRI-4624)
    GitOrigin-RevId: 56a07c57018c0c13bf2b98cbdd323befc6f1568f

--- aether-dependency-resolver/src/org/jetbrains/idea/maven/aether/RepositorySystemHolder.java
+++ aether-dependency-resolver/src/org/jetbrains/idea/maven/aether/RepositorySystemHolder.java
@@ -18,6 +18,7 @@ import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
 import org.eclipse.aether.impl.DefaultServiceLocator;
 import org.eclipse.aether.internal.impl.TrackingFileManager;
 import org.eclipse.aether.named.NamedLockFactory;
+import org.eclipse.aether.named.providers.LocalReadWriteLockNamedLockFactory;
 import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
 import org.eclipse.aether.spi.connector.transport.TransporterFactory;
 import org.eclipse.aether.transport.file.FileTransporterFactory;
@@ -38,7 +39,7 @@ final class RepositorySystemHolder {
     locator.addService(RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class);
     locator.addService(TransporterFactory.class, FileTransporterFactory.class);
     locator.addService(TransporterFactory.class, HttpTransporterFactory.class);
-    locator.setServices(NamedLockFactory.class, new FairLocalReadWriteLockNamedLockFactory());
+    locator.addService(NamedLockFactory.class, LocalReadWriteLockNamedLockFactory.class);
     locator.setServices(ModelBuilder.class, new DefaultModelBuilderFactory() {
       @Override
       public ProfileActivator[] newProfileActivators() {
