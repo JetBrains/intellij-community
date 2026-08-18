424c809edf5277d0904f3202c39f131a18148ea8
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2026-08-18 17:03:05 +0400

    IJPL-247398 Update status of JBCefHealthMonitor for in-process JCEF starting
    GitOrigin-RevId: c7a438291be411bf20bd1c921ba61e778641e3b7

--- platform/ui.jcef/jcef/JBCefApp.java
+++ platform/ui.jcef/jcef/JBCefApp.java
@@ -478,8 +478,7 @@ public final class JBCefApp {
       LOG.info(logTxt);
 
       // Start internal jcef test
-      if (myIsRemoteEnabled)
-        JBCefHealthMonitor.getInstance().performStartupTestAsync();
+      JBCefHealthMonitor.getInstance().performStartupTestAsync(myIsRemoteEnabled);
 
       return true;
     }
