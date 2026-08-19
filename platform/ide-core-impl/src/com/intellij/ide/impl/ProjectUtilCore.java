c8fe40f472d21c3ca64c5cb23f87dad7bdd69b43
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2026-08-19 10:07:41 +0200

    cleanup [platform]: minor optimization; typos; formatting
    GitOrigin-RevId: e515511ed485b57c599be3cb34a8a4fe09381230

--- platform/ide-core-impl/src/com/intellij/ide/impl/ProjectUtilCore.java
+++ platform/ide-core-impl/src/com/intellij/ide/impl/ProjectUtilCore.java
@@ -1,11 +1,10 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.impl;
 
 import com.intellij.configurationStore.ProjectStorePathManager;
 import com.intellij.ide.highlighter.ProjectFileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.Strings;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -17,28 +16,30 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public final class ProjectUtilCore {
+  private ProjectUtilCore() { }
+
+  @SuppressWarnings("SSBasedInspection")
   public static @NotNull Project @NotNull [] getOpenProjects() {
-    ProjectManager projectManager = ProjectManager.getInstanceIfCreated();
+    var projectManager = ProjectManager.getInstanceIfCreated();
     return projectManager == null ? new Project[0] : projectManager.getOpenProjects();
   }
 
-  /**
-   * @deprecated Use {@code com.intellij.ide.impl.ProjectUtil#isValidProjectPath}
-   */
+  /// @deprecated use [com.intellij.ide.impl.ProjectUtil#isValidProjectPathAsync]
   @Deprecated
   public static boolean isValidProjectPath(@NotNull Path file) {
-    ProjectStorePathManager storePathManager = ProjectStorePathManager.Companion.getInstance();
-    return storePathManager.testStoreDirectoryExistsForProjectRoot(file) ||
-           (Strings.endsWith(file.toString(), ProjectFileType.DOT_DEFAULT_EXTENSION) && Files.isRegularFile(file));
+    var storePathManager = ProjectStorePathManager.Companion.getInstance();
+    return (
+      storePathManager.testStoreDirectoryExistsForProjectRoot(file) ||
+      Strings.endsWith(file.toString(), ProjectFileType.DOT_DEFAULT_EXTENSION) && Files.isRegularFile(file)
+    );
   }
 
   @ApiStatus.Internal
   public static @Nullable VirtualFile getFileAndRefresh(@NotNull Path file) {
-    VirtualFile virtualFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(FileUtil.toSystemIndependentName(file.toString()));
+    var virtualFile = LocalFileSystem.getInstance().refreshAndFindFileByNioFile(file);
     if (virtualFile == null || !virtualFile.isValid()) {
       return null;
     }
-
     virtualFile.refresh(false, false);
     return virtualFile;
   }
