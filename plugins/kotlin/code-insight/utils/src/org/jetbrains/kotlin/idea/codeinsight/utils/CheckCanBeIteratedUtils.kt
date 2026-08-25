ac4880850be0a0fd77b5e6f8b055838413c1d7d7
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2026-08-25 12:21:10 +0200

    KTIJ-40033 [kotlin] Improve responsibility and calculation complexity of isInheritorOf
    - Add checkCanceled to responsiveness
    - Check directSupertypes only, other are calculated recursively
    
    GitOrigin-RevId: de581a292f0ae82e45fdf27f7526c16d02b25892

--- plugins/kotlin/code-insight/utils/src/org/jetbrains/kotlin/idea/codeinsight/utils/CheckCanBeIteratedUtils.kt
+++ plugins/kotlin/code-insight/utils/src/org/jetbrains/kotlin/idea/codeinsight/utils/CheckCanBeIteratedUtils.kt
@@ -1,41 +1,56 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.codeinsight.utils
 
+import com.intellij.openapi.progress.ProgressManager
 import org.jetbrains.annotations.ApiStatus
-import org.jetbrains.kotlin.analysis.api.KaContextParameterApi
 import org.jetbrains.kotlin.analysis.api.KaSession
-import org.jetbrains.kotlin.analysis.api.types.KaStandardTypeClassIds
-import org.jetbrains.kotlin.analysis.api.types.allSupertypes
-import org.jetbrains.kotlin.analysis.api.types.isMarkedNullable
-import org.jetbrains.kotlin.analysis.api.types.lowerBoundIfFlexible
 import org.jetbrains.kotlin.analysis.api.types.KaClassType
 import org.jetbrains.kotlin.analysis.api.types.KaDefinitelyNotNullType
 import org.jetbrains.kotlin.analysis.api.types.KaFlexibleType
 import org.jetbrains.kotlin.analysis.api.types.KaIntersectionType
+import org.jetbrains.kotlin.analysis.api.types.KaStandardTypeClassIds
 import org.jetbrains.kotlin.analysis.api.types.KaType
 import org.jetbrains.kotlin.analysis.api.types.KaTypeParameterType
+import org.jetbrains.kotlin.analysis.api.types.allSupertypes
+import org.jetbrains.kotlin.analysis.api.types.directSupertypes
+import org.jetbrains.kotlin.analysis.api.types.isArrayOrPrimitiveArray
+import org.jetbrains.kotlin.analysis.api.types.isMarkedNullable
+import org.jetbrains.kotlin.analysis.api.types.lowerBoundIfFlexible
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.StandardClassIds
 
 private val ITERABLE_CLASS_IDS: Set<ClassId> = buildSet {
-    this += StandardClassIds.Array
-    this += StandardClassIds.primitiveArrayTypeByElementType.values // What about elementTypeByUnsignedArrayType?
     this += StandardClassIds.Iterable
     this += StandardClassIds.Map
     this += StandardClassIds.Sequence
     this += ClassId.fromString("java/util/stream/Stream")
     this += KaStandardTypeClassIds.CHAR_SEQUENCE
 }
+private val ITERABLE_AND_ITERATOR_CLASS_IDS: Set<ClassId> = ITERABLE_CLASS_IDS + StandardClassIds.Iterator
+private val ITERABLE_AND_ARRAYS_CLASS_IDS: Set<ClassId> = buildSet {
+    this += StandardClassIds.Array
+    this += StandardClassIds.primitiveArrayTypeByElementType.values // What about elementTypeByUnsignedArrayType?
+    this += ITERABLE_CLASS_IDS
+}
 
 @ApiStatus.Internal
 context(_: KaSession)
-fun canBeIterated(type: KaType, checkNullability: Boolean = true): Boolean =
-    type.isInheritorOf(ITERABLE_CLASS_IDS, checkNullability)
+fun canBeIterated(type: KaType, checkNullability: Boolean = true): Boolean {
+    return type.isArrayType(checkNullability) || type.isInheritorOf(
+        ITERABLE_CLASS_IDS,
+        hashMapOf(StandardClassIds.Any to false),
+        checkNullability
+    )
+}
+
+context(_: KaSession)
+private fun KaType.isArrayType(checkNullability: Boolean): Boolean =
+    (!checkNullability || !isMarkedNullable) && isArrayOrPrimitiveArray
 
 @ApiStatus.Internal
 context(_: KaSession)
 fun iterationElementType(classType: KaClassType): KaType? =
-    typeArgumentFrom(classType, ITERABLE_CLASS_IDS, index = 0)
+    typeArgumentFrom(classType, ITERABLE_AND_ARRAYS_CLASS_IDS, index = 0)
 
 context(_: KaSession)
 private fun typeArgumentFrom(classType: KaClassType, classIds: Set<ClassId>, index: Int): KaType? =
@@ -43,28 +58,48 @@ private fun typeArgumentFrom(classType: KaClassType, classIds: Set<ClassId>, ind
         if (type.classId in classIds) type.typeArguments.getOrNull(index)?.type else null
     }
 
-@OptIn(KaContextParameterApi::class)
 context(_: KaSession)
 private fun selfAndSupertypes(classType: KaClassType): Sequence<KaClassType> =
     sequenceOf(classType) + classType.allSupertypes(shouldApproximate = true).filterIsInstance<KaClassType>()
 
 @ApiStatus.Internal
 context(_: KaSession)
-fun canBeIteratedOrIterator(type: KaType, checkNullability: Boolean = true): Boolean =
-    type.isInheritorOf(ITERABLE_CLASS_IDS + StandardClassIds.Iterator, checkNullability)
+fun canBeIteratedOrIterator(type: KaType, checkNullability: Boolean = true): Boolean {
+    return type.isArrayType(checkNullability) || type.isInheritorOf(
+        ITERABLE_AND_ITERATOR_CLASS_IDS,
+        hashMapOf(StandardClassIds.Any to false),
+        checkNullability
+    )
+}
 
-@OptIn(KaContextParameterApi::class)
 @ApiStatus.Internal
 context(_: KaSession)
-private fun KaType.isInheritorOf(classIds: Set<ClassId>, checkNullability: Boolean = true): Boolean {
+private fun KaType.isInheritorOf(
+    classIds: Set<ClassId>,
+    visitedTypes: MutableMap<ClassId, Boolean>,
+    checkNullability: Boolean = true
+): Boolean {
+    ProgressManager.checkCanceled()
     return when (this) {
-        is KaFlexibleType -> this.lowerBoundIfFlexible().isInheritorOf(classIds)
-        is KaIntersectionType -> this.conjuncts.any { it.isInheritorOf(classIds) }
-        is KaDefinitelyNotNullType -> this.original.isInheritorOf(classIds, checkNullability = false)
-        is KaTypeParameterType -> symbol.upperBounds.any { it.isInheritorOf(classIds) }
+        is KaFlexibleType -> this.lowerBoundIfFlexible().isInheritorOf(classIds, visitedTypes)
+        is KaIntersectionType -> this.conjuncts.any { it.isInheritorOf(classIds, visitedTypes) }
+        is KaDefinitelyNotNullType -> this.original.isInheritorOf(
+            classIds,
+            if (checkNullability) hashMapOf() else visitedTypes,
+            checkNullability = false
+        )
+        is KaTypeParameterType -> symbol.upperBounds.any { it.isInheritorOf(classIds, visitedTypes) }
         is KaClassType -> {
-            (!checkNullability || !isMarkedNullable)
-                    && (classId in classIds || allSupertypes(shouldApproximate = true).any { it.isInheritorOf(classIds) })
+            val classId = classId
+            visitedTypes[classId]?.let { return it }
+
+            val inheritor = (!checkNullability || !isMarkedNullable)
+                    && (classId in classIds || directSupertypes(shouldApproximate = true).any {
+                        it.isInheritorOf(classIds, visitedTypes)
+                    })
+
+            visitedTypes[classId] = inheritor
+            inheritor
         }
         else -> false
     }
