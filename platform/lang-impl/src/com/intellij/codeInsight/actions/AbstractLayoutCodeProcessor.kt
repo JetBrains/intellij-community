2969c5a8b25b23026395e492cc6e2657d44630ee
konrad.sztenderski <konrad.sztenderski@jetbrains.com>
2026-08-26 20:13:14 +0000

    RUST-20732 Pass specific processor name as a command name
    This makes it possible to decide with `CommandProcessor.getInstance().currentCommandName` which exact command is processed currently when multiple commands are used e.g. on file save
    
    (cherry picked from commit 024385ef8637576d6141442a50fa30eec075926f)
    
    GitOrigin-RevId: 8f233520c301efe610b5604a6b9716d166e9c00d

--- platform/lang-impl/src/com/intellij/codeInsight/actions/AbstractLayoutCodeProcessor.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/actions/AbstractLayoutCodeProcessor.kt
@@ -470,9 +470,10 @@ abstract class AbstractLayoutCodeProcessor private constructor(
 
         ProgressIndicatorProvider.checkCanceled()
 
+        val stageCommandName = processor.commandName
         withStructuredAsyncDocumentFormattingIfNotEdt {
-          processor.runTask(file, commandName, groupId, processAllFilesAsSingleUndoStep) {
-            AbstractLayoutCodeProcessorWriteInterceptor.getInstance().runFileWrite(writeTask, myProject, commandName);
+          processor.runTask(file, stageCommandName, groupId, processAllFilesAsSingleUndoStep) {
+            AbstractLayoutCodeProcessorWriteInterceptor.getInstance().runFileWrite(writeTask, myProject, stageCommandName)
           }
         }
 
