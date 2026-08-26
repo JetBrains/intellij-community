f649bc586c7d1078fa3409a29c45637ad9137b02
Petr <petr.golubev@jetbrains.com>
2026-08-26 22:48:48 +0200

    PY-91669 Enable `completeFunctionParens` for Pyrefly
    GitOrigin-RevId: 867b183ea0e8c8cf3501d4560eb239130a2e40c2

--- python/python-pyrefly/src/com/intellij/python/pyrefly/lsp/PyreflyLspClientDescriptor.kt
+++ python/python-pyrefly/src/com/intellij/python/pyrefly/lsp/PyreflyLspClientDescriptor.kt
@@ -76,6 +76,7 @@ class PyreflyLspClientDescriptor(module: Module) : PyLspToolDescriptor(module, P
     return buildMap {
       put("pythonPath", homePath)
       put("pyrefly", buildPyreflyClientSettings())
+      put("analysis", buildPyreflyAnalysisSettings())
     }
   }
 
@@ -110,8 +111,13 @@ class PyreflyLspClientDescriptor(module: Module) : PyLspToolDescriptor(module, P
     put("disableBundledThirdPartyStubs", true)
   }
 
+  private fun buildPyreflyAnalysisSettings(): Map<String, Any> = mapOf("completeFunctionParens" to true)
+
   override fun getWorkspaceConfiguration(item: ConfigurationItem): Map<String, Map<String, Any>> =
-    mapOf("pyrefly" to buildPyreflyClientSettings())
+    mapOf(
+      "pyrefly" to buildPyreflyClientSettings(),
+      "analysis" to buildPyreflyAnalysisSettings(),
+    )
 
   override fun startServerProcess(): BaseProcessHandler<*> {
     // Pyrefly always runs against the module interpreter (discovery mode is no longer selectable).
