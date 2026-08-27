246e19c337d9eefb39e94c43e174af0984fbd1f5
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2026-08-27 10:30:52 +0200

    tests [threading]: Benchmark the EDT dispatcher
    Measure the strict EDT dispatcher with the existing UI dispatcher benchmark.
    
    GitOrigin-RevId: f08984e3f2e0f4625a2a769438e713dd91df9e3f

--- platform/platform-tests/testSrc/com/intellij/performance/CoroutinesIntegrationBenchmarkTest.kt
+++ platform/platform-tests/testSrc/com/intellij/performance/CoroutinesIntegrationBenchmarkTest.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.performance
 
+import com.intellij.openapi.application.EDT
 import com.intellij.openapi.application.UI
 import com.intellij.openapi.progress.runBlockingMaybeCancellable
 import com.intellij.testFramework.PerformanceUnitTest
@@ -11,6 +12,7 @@ import kotlinx.coroutines.withContext
 import kotlinx.coroutines.yield
 import org.junit.jupiter.api.Assumptions
 import org.junit.jupiter.api.Test
+import kotlin.coroutines.CoroutineContext
 
 @StressTestApplication
 @PerformanceUnitTest
@@ -18,10 +20,20 @@ class CoroutinesIntegrationBenchmarkTest {
 
   @Test
   fun `performance of strict UI dispatcher`() {
-    val debugProperty = System.getProperty("kotlinx.coroutines.debug")
-    Assumptions.assumeTrue({ debugProperty == "off" }, "Existing system properties: ${System.getProperties()}")
+    requireDisabledCoroutineDebug()
     Benchmark.newBenchmark("Frequent dispatch on Dispatchers.UI") {
-      runUiDispatcherBenchmark()
+      runUiDispatcherBenchmark(Dispatchers.UI)
+    }
+      .attempts(3)
+      .warmupIterations(2)
+      .start()
+  }
+
+  @Test
+  fun `performance of strict EDT dispatcher`() {
+    requireDisabledCoroutineDebug()
+    Benchmark.newBenchmark("Frequent dispatch on Dispatchers.EDT") {
+      runUiDispatcherBenchmark(Dispatchers.EDT)
     }
       .attempts(3)
       .warmupIterations(2)
@@ -29,13 +41,18 @@ class CoroutinesIntegrationBenchmarkTest {
   }
 
 
-  fun runUiDispatcherBenchmark() {
+  fun runUiDispatcherBenchmark(dispatcher: CoroutineContext) {
     runBlockingMaybeCancellable {
-      withContext(Dispatchers.UI) {
+      withContext(dispatcher) {
         repeat(5_000_000) {
           yield()
         }
       }
     }
+  }
+
+  private fun requireDisabledCoroutineDebug() {
+    val debugProperty = System.getProperty("kotlinx.coroutines.debug")
+    Assumptions.assumeTrue({ debugProperty == "off" }, "Existing system properties: ${System.getProperties()}")
   }
 }
