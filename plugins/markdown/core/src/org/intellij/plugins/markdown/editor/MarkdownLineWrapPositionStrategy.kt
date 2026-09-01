8b59af613ced318e8543f1daf2b91d4fd5e8d765
Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
2026-09-01 14:25:24 +0300

    [markdown] IJPL-249417 Move `MarkdownLineWrapPositionStrategy` to shared module
    GitOrigin-RevId: 4a3abe76e758dca71dd7c32090dc44085e0f4373

--- plugins/markdown/core/src/org/intellij/plugins/markdown/editor/MarkdownLineWrapPositionStrategy.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/editor/MarkdownLineWrapPositionStrategy.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.markdown.frontend.editor
+package org.intellij.plugins.markdown.editor
 
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
--- plugins/markdown/test/src/org/intellij/plugins/markdown/wrap/MarkdownSoftWrapTest.kt
+++ plugins/markdown/test/src/org/intellij/plugins/markdown/wrap/MarkdownSoftWrapTest.kt
@@ -29,7 +29,7 @@ class MarkdownSoftWrapTest : BasePlatformTestCase() {
   /**
    * The wrap candidate falls inside the link, where wrapping is forbidden, so the strategy has to move
    * the soft wrap before the link start. The link is placed after several blank-line-separated paragraphs to check
-   * that [com.intellij.markdown.frontend.editor.MarkdownLineWrapPositionStrategy] resolves offsets correctly when
+   * that [org.intellij.plugins.markdown.editor.MarkdownLineWrapPositionStrategy] resolves offsets correctly when
    * the line under test does not start at the beginning of the document.
    */
   fun `test soft wrap does not break a link located after the first block`() {
