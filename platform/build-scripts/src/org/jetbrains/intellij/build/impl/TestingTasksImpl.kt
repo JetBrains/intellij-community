26c1431c2f28c12877f739b414af1e3e3541aad2
Evgenii Ilichev <evgenii.ilichev@jetbrains.com>
2026-09-01 14:25:18 +0200

    [MRI-4814] switch intellij.platform.core.nio.fs.tests
    fix dec4cbfb7f7b513fc1cfac746972f24156cff830
    
    GitOrigin-RevId: f201b24468c255b5808a71f05605db148ff6044b

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/TestingTasksImpl.kt
@@ -1609,7 +1609,7 @@ private val COMMUNITY_AGGREGATOR_BAZEL_MIGRATED_MODULES = listOf(
   "intellij.searchEverywhereMl.typos.tests",
   "intellij.statsCollector.tests",
   "intellij.platform.builtInServer.tests",
-  "intellij.platform.core.nio.fs",
+  "intellij.platform.core.nio.fs.tests",
   "intellij.platform.jewel.ui.tests",
   "intellij.platform.jps.model.tests",
   "intellij.platform.lang.tests",
