2c21e64d541b3c513d46140f3cba2641192d3964
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2026-09-01 17:40:29 +0000

    [Analyzer] LSP-1301 Stable virtual files new implementation
    GitOrigin-RevId: 8b98bdd398b6e906e3043e083309138d0c412c05

--- platform/core-impl/src/com/intellij/openapi/vfs/CompactVirtualFileSet.java
+++ platform/core-impl/src/com/intellij/openapi/vfs/CompactVirtualFileSet.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vfs;
 
 import com.intellij.openapi.progress.ProgressManager;
@@ -85,7 +85,7 @@ public final class CompactVirtualFileSet extends AbstractSet<VirtualFile> implem
   public CompactVirtualFileSet() {
   }
 
-  CompactVirtualFileSet(@NotNull Collection<? extends VirtualFile> files) {
+  public CompactVirtualFileSet(@NotNull Collection<? extends VirtualFile> files) {
     addAll(files);
   }
 
