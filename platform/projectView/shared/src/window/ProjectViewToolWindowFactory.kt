dd8f0ba2eab4c507a847c73ca78d1be2cf5d9a4a
Sergei Tachenov <Sergei.Tachenov@jetbrains.com>
2026-09-01 16:29:02 +0300

    [UI, PV] IJPL-158511 Migrate isApplicableAsync to the new factory
    GitOrigin-RevId: c4da2173e30435911c001d74aa19edbcaad0efce

--- platform/projectView/shared/src/window/ProjectViewToolWindowFactory.kt
+++ platform/projectView/shared/src/window/ProjectViewToolWindowFactory.kt
@@ -5,17 +5,24 @@ import com.intellij.icons.AllIcons
 import com.intellij.ide.projectView.impl.ProjectViewImpl
 import com.intellij.ide.projectView.impl.isProjectViewSplit
 import com.intellij.idea.AppMode
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.ex.ProjectFrameCapabilitiesService
+import com.intellij.openapi.wm.ex.ProjectFrameCapability
 import com.intellij.util.PlatformUtils.isJetBrainsClient
 import javax.swing.Icon
 
 internal class ProjectViewToolWindowFactory : ToolWindowFactory, DumbAware {
   override val icon: Icon
     get() = AllIcons.Toolwindows.ToolWindowProject
+
+  override suspend fun isApplicableAsync(project: Project): Boolean {
+    return !serviceAsync<ProjectFrameCapabilitiesService>().has(project, ProjectFrameCapability.SUPPRESS_PROJECT_VIEW)
+  }
 
   override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
     if (isProjectViewSplit()) {
