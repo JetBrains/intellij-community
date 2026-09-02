10ca8b87fa917dbb7fcca1b12dc9540b486f3621
Valeria Golovina <valeria.golovina@jetbrains.com>
2026-09-02 18:51:44 +0300

    tests [git] converted git shelf tests to junit5
    GitOrigin-RevId: ed5001a38a0f5a4dc624878315aa2eea27582868

--- plugins/git4idea/tests/git4idea/shelf/GitRevisionContentPreLoaderTest.kt
+++ plugins/git4idea/tests/git4idea/shelf/GitRevisionContentPreLoaderTest.kt
@@ -3,21 +3,34 @@ package git4idea.shelf
 
 import com.intellij.openapi.vcs.ProjectLevelVcsManager
 import com.intellij.openapi.vcs.impl.ContentRevisionCache
+import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.vcs.test.refresh
+import com.intellij.vcs.test.updateChangeListManager
 import com.intellij.vcsUtil.VcsUtil
 import git4idea.GitRevisionNumber
 import git4idea.GitVcs
 import git4idea.stash.GitRevisionContentPreLoader
-import git4idea.test.GitSingleRepoTest
+import git4idea.test.GitSingleRepoContext
 import git4idea.test.TestFile
+import git4idea.test.file
+import git4idea.test.git
+import git4idea.test.gitSingleRepoContextFixture
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
-class GitRevisionContentPreLoaderTest : GitSingleRepoTest() {
+@TestApplication
+class GitRevisionContentPreLoaderTest {
+  private val fixture = gitSingleRepoContextFixture()
+  private val context: GitSingleRepoContext get() = fixture.get()
 
-  override fun setUp() {
-    super.setUp()
+  @BeforeEach
+  fun setUp(): Unit = with(context) {
     git("config core.autocrlf false")
   }
 
-  fun `test two files modification`() {
+  @Test
+  fun `test two files modification`(): Unit = with(context) {
     val afile = file("a.txt")
     val initialA = "initialA\n"
     afile.create(initialA).addCommit("initial")
@@ -45,11 +58,11 @@ class GitRevisionContentPreLoaderTest : GitSingleRepoTest() {
 
   private fun assertBaseContents(contents: Map<TestFile, String>, revisionNumber: GitRevisionNumber) {
     for ((file, content) in contents) {
-      val cache = ProjectLevelVcsManager.getInstance(project).contentRevisionCache
-      val bytes = cache.getFromConstantCache(VcsUtil.getFilePath(file.file), revisionNumber, GitVcs.getKey(),
+      val cache = ProjectLevelVcsManager.getInstance(context.project).contentRevisionCache
+      val bytes = cache.getFromConstantCache(VcsUtil.getFilePath(file.file.path, false), revisionNumber, GitVcs.getKey(),
                                              ContentRevisionCache.UniqueType.REPOSITORY_CONTENT)
-      assertNotNull("No content recorded for $file", bytes)
-      assertEquals("Incorrect content for $file", content, String(bytes))
+      assertThat(bytes).describedAs("No content recorded for $file").isNotNull()
+      assertThat(String(bytes!!)).describedAs("Incorrect content for $file").isEqualTo(content)
     }
   }
 }
--- plugins/git4idea/tests/git4idea/shelf/GitShelveTest.kt
+++ plugins/git4idea/tests/git4idea/shelf/GitShelveTest.kt
@@ -1,7 +1,8 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.shelf
 
-import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.runReadActionBlocking
 import com.intellij.openapi.command.WriteCommandAction.runWriteCommandAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.impl.LoadTextUtil
@@ -13,51 +14,56 @@ import com.intellij.openapi.vcs.changes.shelf.ShelvedChangeList
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.runInEdtAndWait
+import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.testFramework.junit5.TestDisposable
 import com.intellij.util.LineSeparator
+import com.intellij.vcs.test.refresh
+import com.intellij.vcs.test.updateChangeListManager
 import git4idea.stash.GitShelveChangesSaver
 import git4idea.test.ChangesBuilder
-import git4idea.test.GitSingleRepoTest
+import git4idea.test.GitSingleRepoContext
+import git4idea.test.assertChanges
 import git4idea.test.assertNoChanges
 import git4idea.test.assertStatus
+import git4idea.test.createDir
 import git4idea.test.createRepository
 import git4idea.test.file
+import git4idea.test.git
+import git4idea.test.gitSingleRepoContextFixture
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedClass
+import org.junit.jupiter.params.provider.ValueSource
 
-class GitStandardShelveTest : GitShelveTest() {
-  override fun setUp() {
-    super.setUp()
-    setBatchShelveOptimization(false)
-  }
-}
+@TestApplication
+@ParameterizedClass(name = "batch optimization = {0}")
+@ValueSource(booleans = [false, true])
+class GitShelveTest(private val batchOptimization: Boolean) {
+  private val fixture = gitSingleRepoContextFixture()
+  private val context: GitSingleRepoContext get() = fixture.get()
 
-class GitBatchShelveTest: GitShelveTest() {
-  override fun setUp() {
-    super.setUp()
-    setBatchShelveOptimization(true)
-  }
-}
+  @TestDisposable
+  lateinit var disposable: Disposable
 
-abstract class GitShelveTest : GitSingleRepoTest() {
-  private lateinit var shelveChangesManager : ShelveChangesManager
+  private lateinit var shelveChangesManager: ShelveChangesManager
   private lateinit var saver: GitShelveChangesSaver
 
   companion object {
     private val CRLF = LineSeparator.CRLF.separatorString
   }
 
-  override fun setUp() {
-    super.setUp()
+  @BeforeEach
+  fun setUp(): Unit = with(context) {
     shelveChangesManager = ShelveChangesManager.getInstance(project)
     saver = GitShelveChangesSaver(project, git, EmptyProgressIndicator(), "test")
     git("config core.autocrlf false")
+    val batchSize = if (batchOptimization) 100 else -1
+    Registry.get("git.shelve.load.base.in.batches").setValue(batchSize, disposable)
   }
 
-  protected fun setBatchShelveOptimization(value: Boolean) {
-    val batchSize = if (value) 100 else -1
-    Registry.get("git.shelve.load.base.in.batches").setValue(batchSize, testRootDisposable)
-  }
-
-  fun `test modification`() {
+  @Test
+  fun `test modification`(): Unit = with(context) {
     val file = file("a.txt")
     val initialContent = "initial\n"
     file.create(initialContent).addCommit("initial")
@@ -72,7 +78,7 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     updateChangeListManager()
 
     changeListManager.assertNoChanges()
-    assertEquals("Current file content is incorrect", initialContent, file.read())
+    assertThat(file.read()).describedAs("Current file content is incorrect").isEqualTo(initialContent)
 
     val list = `assert single shelvelist`()
     assertChanges(list) {
@@ -80,7 +86,8 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     }
   }
 
-  fun `test two files modification`() {
+  @Test
+  fun `test two files modification`(): Unit = with(context) {
     val aFile = file("a.txt")
     val initialContent = "initial\n"
     aFile.create(initialContent).addCommit("initial")
@@ -100,8 +107,8 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     updateChangeListManager()
 
     changeListManager.assertNoChanges()
-    assertEquals("Current file content is incorrect", initialContent, aFile.read())
-    assertEquals("Current file content is incorrect", initialContent, bfile.read())
+    assertThat(aFile.read()).describedAs("Current file content is incorrect").isEqualTo(initialContent)
+    assertThat(bfile.read()).describedAs("Current file content is incorrect").isEqualTo(initialContent)
 
     val list = `assert single shelvelist`()
     assertChanges(list) {
@@ -110,7 +117,8 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     }
   }
 
-  fun `test addition`() {
+  @Test
+  fun `test addition`(): Unit = with(context) {
     val file = file("a.txt")
     val initialContent = "initial\n"
     file.create(initialContent).add()
@@ -123,7 +131,7 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     updateChangeListManager()
 
     changeListManager.assertNoChanges()
-    assertFalse("There should be no file a.txt on disk", file.file.exists())
+    assertThat(file.file).doesNotExist()
 
     val list = `assert single shelvelist`()
     assertChanges(list) {
@@ -131,7 +139,8 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     }
   }
 
-  fun `test hidden stage-only changes 1`() {
+  @Test
+  fun `test hidden stage-only changes 1`(): Unit = with(context) {
     val initialContent = "initial\n"
 
     val aFile = file("a.txt")
@@ -154,8 +163,8 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     updateChangeListManager()
 
     changeListManager.assertNoChanges()
-    assertEquals("Current file content is incorrect", initialContent, aFile.read())
-    assertEquals("Current file content is incorrect", initialContent, bfile.read())
+    assertThat(aFile.read()).describedAs("Current file content is incorrect").isEqualTo(initialContent)
+    assertThat(bfile.read()).describedAs("Current file content is incorrect").isEqualTo(initialContent)
 
     val list = `assert single shelvelist`()
     assertChanges(list) {
@@ -171,7 +180,8 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     }
   }
 
-  fun `test hidden stage-only changes 2`() {
+  @Test
+  fun `test hidden stage-only changes 2`(): Unit = with(context) {
     val aFile = file("a.txt")
     val initialContent = "initial\n"
     aFile.create(initialContent).addCommit("initial")
@@ -186,21 +196,22 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     refresh()
     updateChangeListManager()
 
-    assertEmpty(repo.stagingAreaHolder.allRecords)
-    assertEmpty(shelveChangesManager.shelvedChangeLists)
+    assertThat(repo.stagingAreaHolder.allRecords).isEmpty()
+    assertThat(shelveChangesManager.shelvedChangeLists).isEmpty()
 
     saver.load()
     refresh()
     updateChangeListManager()
 
-    assertEmpty(repo.stagingAreaHolder.allRecords)
-    assertEmpty(shelveChangesManager.shelvedChangeLists)
+    assertThat(repo.stagingAreaHolder.allRecords).isEmpty()
+    assertThat(shelveChangesManager.shelvedChangeLists).isEmpty()
   }
 
-  fun `test hidden stage-only changes 3`() {
-    val afile = file("b.txt")
-    afile.create("initial content").add()
-    afile.delete()
+  @Test
+  fun `test hidden stage-only changes 3`(): Unit = with(context) {
+    val bFile = file("b.txt")
+    bFile.create("initial content").add()
+    bFile.delete()
 
     refresh()
     updateChangeListManager()
@@ -209,23 +220,24 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     refresh()
     updateChangeListManager()
 
-    assertEmpty(repo.stagingAreaHolder.allRecords)
-    assertEmpty(shelveChangesManager.shelvedChangeLists)
+    assertThat(repo.stagingAreaHolder.allRecords).isEmpty()
+    assertThat(shelveChangesManager.shelvedChangeLists).isEmpty()
 
     saver.load()
     refresh()
     updateChangeListManager()
 
-    assertEmpty(repo.stagingAreaHolder.allRecords)
-    assertEmpty(shelveChangesManager.shelvedChangeLists)
+    assertThat(repo.stagingAreaHolder.allRecords).isEmpty()
+    assertThat(shelveChangesManager.shelvedChangeLists).isEmpty()
   }
 
-  fun `test shelf and load files added in multiple roots`() {
+  @Test
+  fun `test shelf and load files added in multiple roots`(): Unit = with(context) {
     val file = file("a.txt")
     val initialContent = "initial\n"
     file.create(initialContent).add()
 
-    val secondRoot = createRepository(project, projectRoot.createDir("secondRoot").toNioPath(), false)
+    val secondRoot = createRepository(project, createDir(projectRoot, "secondRoot").toNioPath(), false)
     val file2 = secondRoot.file("b.txt")
     file2.create(initialContent).add()
 
@@ -250,29 +262,26 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     refresh()
     updateChangeListManager()
 
-    assertTrue("There should be the file a.txt on the disk", file.file.exists())
-    assertTrue("There should be the file b.txt on the disk", file2.file.exists())
+    assertThat(file.file).exists()
+    assertThat(file2.file).exists()
     repo.assertStatus(file.file, 'A')
     secondRoot.assertStatus(file2.file, 'A')
   }
 
-  fun `test crlf line separators are preserved after shelve and unshelve`() {
+  @Test
+  fun `test crlf line separators are preserved after shelve and unshelve`(): Unit = with(context) {
     val file = file("a.txt")
     val initialText = "first line${CRLF}second line${CRLF}"
     file.create(initialText)
 
     val vFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(file.file)
-    if (vFile == null) {
-      assertNotNull("VirtualFile not found for file ${file.file}", vFile)
-      return
-    }
+    assertThat(vFile).describedAs("VirtualFile not found for file ${file.file}").isNotNull()
+    val nonNullVFile = vFile!!
 
-    val document = runReadAction { FileDocumentManager.getInstance().getDocument(vFile) }
-    if (document == null) {
-      assertNotNull("Document not found for file ${file.file}", document)
-      return
-    }
-    assertHasCrlfSeparators(vFile)
+    val nullableDocument = runReadActionBlocking { FileDocumentManager.getInstance().getDocument(nonNullVFile) }
+    assertThat(nullableDocument).describedAs("Document not found for file ${file.file}").isNotNull()
+    val document = nullableDocument!!
+    assertHasCrlfSeparators(nonNullVFile)
 
     file.addCommit("initial commit file with CRLF line separators")
 
@@ -292,7 +301,7 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     updateChangeListManager()
 
     assertThat(ChangeListManager.getInstance(project).allChanges).isEmpty()
-    assertHasCrlfSeparators(vFile)
+    assertHasCrlfSeparators(nonNullVFile)
 
     val shelvedLists = ShelveChangesManager.getInstance(project).shelvedChangeLists
     assertThat(shelvedLists).isNotEmpty
@@ -303,7 +312,7 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     updateChangeListManager()
 
     assertThat(ChangeListManager.getInstance(project).allChanges).isNotEmpty
-    assertHasCrlfSeparators(vFile)
+    assertHasCrlfSeparators(nonNullVFile)
   }
 
   private fun assertHasCrlfSeparators(vFile: VirtualFile) {
@@ -318,8 +327,8 @@ abstract class GitShelveTest : GitSingleRepoTest() {
 
   private fun `assert single shelvelist`(): ShelvedChangeList {
     val lists = shelveChangesManager.shelvedChangeLists
-    assertEquals("Incorrect shelve lists amount", 1, lists.size)
-    return lists[0]
+    assertThat(lists).describedAs("Incorrect shelve lists amount").hasSize(1)
+    return lists.single()
   }
 
   private fun assertChanges(list: ShelvedChangeList, changes: ChangesBuilder.() -> Unit) {
@@ -331,9 +340,9 @@ abstract class GitShelveTest : GitSingleRepoTest() {
     val actualChanges = HashSet(changesInShelveList)
     for (change in cb.changes) {
       val found = actualChanges.find(change.changeMatcher)
-      assertNotNull("The change [$change] not found\n$changesInShelveList", found)
+      assertThat(found).describedAs("The change [$change] not found\n$changesInShelveList").isNotNull()
       actualChanges.remove(found)
     }
-    assertEmpty("There are unexpected changes in the shelvelist", actualChanges)
+    assertThat(actualChanges).describedAs("There are unexpected changes in the shelvelist").isEmpty()
   }
 }
