f3c93bc0bf83dc018608b9e43bc0ec39b237fc3e
Denis Mashutin <Denis.Mashutin@jetbrains.com>
2026-09-03 10:15:23 +0000

    PCQA-1810 tests [pycharm]: migrate PythonConsoleTest to PyCharmBaseUiTest
    🍒 Cherry-picked from review IJ-CR-221163
    
    Merge-request: IJ-MR-221164
    Merged-by: Denis Mashutin <Denis.Mashutin@jetbrains.com>
    
    GitOrigin-RevId: 05bfd94554ea2ae62c59a37ef347f91df5c96608

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/python/PythonConsoleUI.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/python/PythonConsoleUI.kt
@@ -4,6 +4,7 @@ import com.intellij.driver.sdk.ui.components.ComponentData
 import com.intellij.driver.sdk.ui.components.UiComponent
 import com.intellij.driver.sdk.ui.components.common.IdeaFrameUI
 import com.intellij.driver.sdk.ui.components.common.JEditorUiComponent
+import com.intellij.driver.sdk.ui.components.elements.actionButton
 
 
 fun IdeaFrameUI.pythonConsole(func: PythonConsoleUi.() -> Unit = {}) =
@@ -14,6 +15,15 @@ fun IdeaFrameUI.pythonConsole(func: PythonConsoleUi.() -> Unit = {}) =
 class PythonConsoleUi(data: ComponentData) : UiComponent(data) {
   val header = x { byAccessibleName("Tool Window Header") }
   val content = x { byClass("PythonConsoleView") }
+
+  /**
+   * The `Console.Execute` button of the console toolbar.
+   *
+   * `performAction` clicks the button on the EDT. So it needs no key event and no focus owner.
+   */
+  val executeButton = actionButton {
+    and(byClass("ActionButton"), byAccessibleName("Execute Current Statement in Console"))
+  }
   val consoleEditor: UiComponent = x("//div[@class='PythonConsoleView']//div[@accessiblename='Editor' and @class='EditorComponentImpl']", "Python console editor")
   val prompt = xx(JEditorUiComponent::class.java) { byClass("EditorComponentImpl") }.list().last()
   fun promptIsReady(): Boolean {
