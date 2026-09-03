046d8e005013151c10b4a757782b82618ea4c1fc
Sergei Tachenov <Sergei.Tachenov@jetbrains.com>
2026-09-03 11:54:33 +0300

    [UI, PV] IJPL-158511 Update PV UI test accessors to work with both PVs
    The only thing that changes from this point of view
    is the tree class, and since both classes are unique to PV
    implementations, we can continue relying on them.
    
    GitOrigin-RevId: b4615c7f82713bdae0351d158480b0be4557fd7a

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/toolwindows/ProjectViewToolWindowUi.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/toolwindows/ProjectViewToolWindowUi.kt
@@ -8,14 +8,24 @@ import com.intellij.driver.sdk.ui.xQuery
 
 fun IdeaFrameUI.projectView(
   locator: QueryBuilder.() -> String = {
-    componentWithChild(byType("com.intellij.toolWindow.InternalDecoratorImpl"),
-                       byType("com.intellij.ide.projectView.impl.ProjectViewTree"))
+    componentWithChild(
+      byType("com.intellij.toolWindow.InternalDecoratorImpl"),
+      or(
+        byType("com.intellij.ide.projectView.impl.ProjectViewTree"),
+        byType("com.intellij.platform.projectView.frontend.impl.FrontendProjectViewTree"),
+      )
+    )
   },
   action: ProjectViewToolWindowUi.() -> Unit = {},
 ): ProjectViewToolWindowUi = x(ProjectViewToolWindowUi::class.java, locator).apply(action)
 
 class ProjectViewToolWindowUi(data: ComponentData) : ToolWindowUiComponent(data) {
-  val projectViewTree = tree(xQuery { byType("com.intellij.ide.projectView.impl.ProjectViewTree") })
+  val projectViewTree = tree(xQuery {
+    or(
+      byType("com.intellij.ide.projectView.impl.ProjectViewTree"),
+      byType("com.intellij.platform.projectView.frontend.impl.FrontendProjectViewTree"),
+    )
+  })
 
   val expandAllIcon = x("//div[@myicon='expandAll.svg']")
   fun expandAll() = x("//div[@myicon='expandAll.svg']").click()
