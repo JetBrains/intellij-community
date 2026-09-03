9567964a5bc2f9eed0100d498cae952765aca8b6
Mikhail Pyltsin <mikhail.pyltsin@jetbrains.com>
2026-09-03 10:01:11 +0200

    [java] IDEA-393077 add javadoc to JavaSyntheticLibrary
    GitOrigin-RevId: 97bda25f3bde5ff8d3e97c5291c2822a58152944

--- platform/projectModel-impl/src/com/intellij/openapi/roots/JavaSyntheticLibrary.java
+++ platform/projectModel-impl/src/com/intellij/openapi/roots/JavaSyntheticLibrary.java
@@ -7,6 +7,20 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * A {@link SyntheticLibrary} that holds JVM class files or JVM sources.
+ * <p>
+ * Use this class, and not {@link SyntheticLibrary} itself, when a JVM language must resolve against the library.
+ * Only the roots of a {@code JavaSyntheticLibrary} get a JVM package prefix in the workspace file index.
+ * As a result, {@code PackageIndex} reports a package name only for these roots.
+ * {@code JavaPsiFacade.findPackage} also finds a package only for these roots.
+ * <p>
+ * {@code PackageIndexTest} holds both halves of this contract.
+ * The test {@code synthetic java library} shows the packages of this class.
+ * The test {@code synthetic non-java library} shows no package for a plain {@link SyntheticLibrary}.
+ *
+ * @see AdditionalLibraryRootsProvider
+ */
 public class JavaSyntheticLibrary extends ImmutableSyntheticLibrary {
 
   public JavaSyntheticLibrary(@NotNull String comparisonId,
