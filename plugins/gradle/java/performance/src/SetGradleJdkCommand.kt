bc40681cbf6f0708c416db89d606071e53967f44
Kajus Kuncaitis <kajus.kuncaitis@jetbrains.com>
2026-09-04 11:51:32 +0200

    [testFramework][gradle][IDEA-393508] Convert ImportGradleProjectCommand to Kotlin
    GitOrigin-RevId: eab78f71bec950cd979b5c6da61c80182ba4c68d

--- plugins/gradle/java/performance/src/SetGradleJdkCommand.kt
+++ plugins/gradle/java/performance/src/SetGradleJdkCommand.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.gradle.java.performance
 
-import com.intellij.gradle.java.performance.ImportGradleProjectCommand.linkGradleProjectIfNeeded
+import com.intellij.gradle.java.performance.ImportGradleProjectCommand.Companion.linkGradleProjectIfNeeded
 import com.intellij.openapi.ui.playback.PlaybackContext
 import com.jetbrains.performancePlugin.commands.PerformanceCommandCoroutineAdapter
 import com.jetbrains.performancePlugin.commands.SetupProjectSdkUtil
