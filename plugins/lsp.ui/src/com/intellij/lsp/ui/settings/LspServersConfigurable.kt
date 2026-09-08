fd1709af073d3de0c22a8e9d609bf967560e4b0b
daria.suvorova <daria.suvorova@jetbrains.com>
2026-09-08 17:00:42 +0200

    IJPL-249564 LSP UI: Search by tag in Marketplace for supported LSP server plugins, from settings page
    GitOrigin-RevId: 24af97cebd6ff044a80ba7ff5d094ad8c50d5918

--- plugins/lsp.ui/src/com/intellij/lsp/ui/settings/LspServersConfigurable.kt
+++ plugins/lsp.ui/src/com/intellij/lsp/ui/settings/LspServersConfigurable.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.lsp.ui.settings
 
+import com.intellij.ide.DataManager
+import com.intellij.ide.plugins.PluginManagerConfigurable
 import com.intellij.lsp.ui.ConfigurableLspIntegrationProvider
 import com.intellij.lsp.ui.LspUiBundle
 import com.intellij.openapi.Disposable
@@ -10,6 +12,8 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.extensions.PluginDescriptor
 import com.intellij.openapi.options.ConfigurationException
 import com.intellij.openapi.options.SearchableConfigurable
+import com.intellij.openapi.options.ShowSettingsUtil
+import com.intellij.openapi.options.ex.Settings
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.MasterDetailsComponent
@@ -21,11 +25,22 @@ import com.intellij.platform.lsp.api.LspIntegrationProvider
 import com.intellij.platform.lsp.impl.LspPluginServerConfiguration
 import com.intellij.platform.lsp.impl.LspServerSettingsProvider
 import com.intellij.ui.EditorNotifications
+import com.intellij.ui.components.ActionLink
 import com.intellij.ui.components.JBScrollPane
+import com.intellij.ui.components.panels.NonOpaquePanel
 import com.intellij.util.IconUtil
+import com.intellij.util.ui.JBUI
+import java.awt.BorderLayout
 import javax.swing.tree.TreeNode
+
+private const val LSP_PLUGIN_SEARCH_TAG = "/tag:\"Language Server\""
+
 internal class LspServersConfigurable(private val project: Project) : MasterDetailsComponent(), SearchableConfigurable, Disposable {
   private val settings = LspServerSettings.getInstance(project)
+  private val morePluginsLinkPanel = NonOpaquePanel(BorderLayout()).apply {
+    border = JBUI.Borders.empty(8, 20, 8, 0)
+    add(ActionLink(LspUiBundle.message("lsp.settings.more.plugins")) { showMorePlugins() }, BorderLayout.WEST)
+  }
 
   init {
     initTree()
@@ -36,6 +51,27 @@ internal class LspServersConfigurable(private val project: Project) : MasterDeta
   override fun getDisplayName(): String = LspUiBundle.message("lsp.settings.name")
 
   override fun getEmptySelectionString(): String = LspUiBundle.message("lsp.settings.empty.selection")
+
+  override fun reInitWholePanelIfNeeded() {
+    super.reInitWholePanelIfNeeded()
+
+    val leftPanel = splitter.firstComponent
+    if (morePluginsLinkPanel.parent !== leftPanel) {
+      leftPanel.add(morePluginsLinkPanel, BorderLayout.SOUTH)
+    }
+  }
+
+  private fun showMorePlugins() {
+    val allSettings = Settings.KEY.getData(DataManager.getInstance().getDataContext(myTree))
+    if (allSettings == null) {
+      ShowSettingsUtil.getInstance().showSettingsDialog(project, PluginManagerConfigurable::class.java) {
+        it.enableSearch(LSP_PLUGIN_SEARCH_TAG)
+      }
+    }
+    else {
+      allSettings.select(allSettings.find(PluginManagerConfigurable.ID), LSP_PLUGIN_SEARCH_TAG)
+    }
+  }
 
   override fun createActions(fromPopup: Boolean): List<AnAction> = listOf(AddAction(), MyDeleteAction())
 
