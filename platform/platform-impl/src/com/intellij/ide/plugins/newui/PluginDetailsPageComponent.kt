d0c04c1597ace99a43d23301213639871d76af86
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2026-09-09 16:03:39 +0200

    [plugin-manager] IJPL-248768 reuse plugin restart state
    The details page must interpret restart states in the same way as the shared plugin status model. Use the common predicate for this decision.
    
    GitOrigin-RevId: 7d53bc3bbd9935bfba4a5dc2db19470af352cfa5

--- platform/platform-impl/src/com/intellij/ide/plugins/newui/PluginDetailsPageComponent.kt
+++ platform/platform-impl/src/com/intellij/ide/plugins/newui/PluginDetailsPageComponent.kt
@@ -2130,8 +2130,7 @@ private suspend fun getDeletedState(pluginUiModel: PluginUiModel): BooleanArray 
   val state = UiPluginManager.getInstance().getPluginInstallationState(pluginId)
   val uninstalledWithoutRestart = state.status == PluginStatus.UNINSTALLED_WITHOUT_RESTART
   if (!uninstalled) {
-    uninstalled =
-      state.status in listOf(PluginStatus.INSTALLED_AND_REQUIRED_RESTART, PluginStatus.UPDATED, PluginStatus.UPDATED_WITH_RESTART)
+    uninstalled = state.status.isRestartRequired()
   }
 
   return booleanArrayOf(uninstalled, uninstalledWithoutRestart)
