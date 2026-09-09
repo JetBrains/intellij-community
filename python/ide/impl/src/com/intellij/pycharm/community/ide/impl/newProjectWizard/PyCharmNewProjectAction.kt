7510cea9cda1f20bddde1f7556e190f79e7989a1
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2026-09-09 13:21:04 +0200

    IJPL-251610 Projectless window behavior
    (cherry picked from commit b30f43e2b83ec8aa7b810416e71df70cb9491071)
    
    IJ-MR-222584
    
    GitOrigin-RevId: f4a5f8c489bcb6d3c4821099a24512a9e67a499a

--- python/ide/impl/src/com/intellij/pycharm/community/ide/impl/newProjectWizard/PyCharmNewProjectAction.kt
+++ python/ide/impl/src/com/intellij/pycharm/community/ide/impl/newProjectWizard/PyCharmNewProjectAction.kt
@@ -1,11 +1,15 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.pycharm.community.ide.impl.newProjectWizard;
 
+import com.intellij.icons.AllIcons
+import com.intellij.idea.ActionsBundle
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.wm.impl.welcomeScreen.WelcomeScreenActionsUtil
 import com.intellij.platform.ProjectGeneratorManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,6 +19,15 @@ import kotlinx.coroutines.withContext
  * New project wizard entry point
  */
 internal class PyCharmNewProjectAction : DumbAwareAction() {
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+  override fun update(e: AnActionEvent) {
+    if (e.getData(WelcomeScreenActionsUtil.NON_MODAL_WELCOME_SCREEN) == true) {
+      e.presentation.icon = AllIcons.Nodes.Folder
+      e.presentation.text = ActionsBundle.message("action.NewDirectoryProject.welcome.text")
+    }
+  }
+
   override fun actionPerformed(e: AnActionEvent) {
     e.coroutineScope.launch {
       runCatching {
