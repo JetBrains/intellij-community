330768aac72ae0240327cf7b4714ca8d5d59e433
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2026-09-09 16:51:58 +0200

    [platform ui][se] IJPL-255165 Disable possibility to switch to the old SE for external users
    GitOrigin-RevId: 84fb1a1f80a9ac21db3ccca592a3af5ce647d632

--- platform/experiment/src/com/intellij/platform/experiment/ab/impl/experiments.kt
+++ platform/experiment/src/com/intellij/platform/experiment/ab/impl/experiments.kt
@@ -24,7 +24,6 @@ enum class ABExperimentOption {
   FUZZY_FILE_SEARCH,
   SHOW_TRIAL_SURVEY,
   NEW_USERS_ONBOARDING,
-  //SPLIT_SEARCH_EVERYWHERE,
   CLION_WIZARD_REMOVAL,
   RIDER_REPO_VIEW,
 
@@ -66,15 +65,6 @@ val experimentsPartition: List<ExperimentAssignment> = listOf(
   //  controlBuckets = (128 until 256).toSet(),
   //  majorVersion = "2025.2"
   //),
-  //ExperimentAssignment(
-  //  experiment = ABExperimentOption.SPLIT_SEARCH_EVERYWHERE,
-  //  experimentBuckets = (0 until 256).toSet(),
-  //  controlBuckets = (256 until 512).toSet(),
-  //  majorVersion = "2026.1 EAP",
-  //  products = EnumSet.of(IntelliJPlatformProduct.IDEA,
-  //                        IntelliJPlatformProduct.PYCHARM,
-  //                        IntelliJPlatformProduct.RIDER),
-  //),
   ExperimentAssignment(
     experiment = ABExperimentOption.CLION_WIZARD_REMOVAL,
     experimentBuckets = (256 until 768).toSet(),
@@ -96,10 +86,7 @@ val experimentsPartition: List<ExperimentAssignment> = listOf(
 /**
  * This method can be configured to allow options only in particular IDEs.
  */
-fun isAllowed(option: ABExperimentOption): Boolean = when (option) {
-  //ABExperimentOption.SPLIT_SEARCH_EVERYWHERE -> IdeProductMode.isMonolith
-  else -> true
-}
+fun isAllowed(option: ABExperimentOption): Boolean = true
 
 // ================= IMPLEMENTATION ====================
 
--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereFeature.kt
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereFeature.kt
@@ -1,43 +1,22 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.actions.searcheverywhere
 
-import com.intellij.frontend.FrontendApplicationInfo
-import com.intellij.frontend.FrontendType
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.util.registry.Registry
-import com.intellij.platform.experiment.ab.impl.ABExperimentOption
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.TestOnly
 
 @ApiStatus.Internal
 object SearchEverywhereFeature {
   private const val PLATFORM_KEY = "search.everywhere.new.enabled"
-  private const val CWM_CLIENT_KEY = "search.everywhere.new.cwm.client.enabled"
-
-  private const val ALLOW_AB_KEY = "search.everywhere.new.allow.ab"
-
-  private val registryKey: String get() =
-    if (isGuest) CWM_CLIENT_KEY
-    else PLATFORM_KEY
 
   var isSplit: Boolean
     get() =
-      Registry.`is`(registryKey, false) //||
-      //Registry.`is`(ALLOW_AB_KEY, false) && ABExperimentOption.SPLIT_SEARCH_EVERYWHERE.isEnabled()
+      !ApplicationManager.getApplication().isInternal || Registry.`is`(PLATFORM_KEY, false)
 
     set(value) {
-      Registry.get(registryKey).setValue(value)
-
-      // Ignore AB test since the Split SE was turned on/off explicitly
-      Registry.get(ALLOW_AB_KEY).setValue(false)
+      Registry.get(PLATFORM_KEY).setValue(value)
     }
 
-  private val isGuest: Boolean get() {
-    val frontendType = FrontendApplicationInfo.getFrontendType()
-    return (frontendType is FrontendType.Remote && frontendType.isGuest())
-  }
-
-  val allRegistryKeys: List<String>
-  @TestOnly get() = listOf(PLATFORM_KEY,
-                           CWM_CLIENT_KEY,
-                           ALLOW_AB_KEY)
+  val allRegistryKeys: List<String> @TestOnly get() = listOf(PLATFORM_KEY)
 }
