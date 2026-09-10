97319f3b83c57bcb540c648ace4d308dda23f24f
Anna Kozlova <anna.kozlova@jetbrains.com>
2026-09-10 17:34:43 +0200

    cleanup [kotlin] remove // IGNORE_K1 directive
    GitOrigin-RevId: f1dc84032c73407ab4b38e76d282fb27d033c737

--- plugins/kotlin/idea/tests/testData/quickfix/contextParameter/existingContext/alreadyInContext.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/contextParameter/existingContext/alreadyInContext.kt
@@ -1,6 +1,5 @@
 // "Add 'extraLogger' as 'MyLogger' to existing context" "false"
 // COMPILER_ARGUMENTS: -Xcontext-parameters
-// IGNORE_K1
 // DISABLE_K2_ERRORS
 interface MyLogger { fun log(msg: String) }
 class ConsoleLogger : MyLogger { override fun log(msg: String) {} }
