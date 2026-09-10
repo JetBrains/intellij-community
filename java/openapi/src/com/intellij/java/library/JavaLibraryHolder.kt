6749640529a810e45c997e41e37ef37ad4c49bc7
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2026-09-10 17:01:31 +0200

    [psi] IJPL-255299: Make it possible to check the presence of java libraries without read action
    GitOrigin-RevId: 8e86d400ffab0f72bf840adb044e2b2cd94e1a45

--- java/openapi/src/com/intellij/java/library/JavaLibraryHolder.kt
+++ java/openapi/src/com/intellij/java/library/JavaLibraryHolder.kt
@@ -1,47 +1,101 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.java.library;
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.java.library
 
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.components.Service;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ModuleRootEvent;
-import com.intellij.openapi.roots.ModuleRootListener;
-import com.intellij.openapi.roots.OrderEnumerator;
-import org.jetbrains.annotations.NotNull;
-
-import static com.intellij.java.library.JavaLibraryUtil.fillLibraries;
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.readAction
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootEvent
+import com.intellij.openapi.roots.ModuleRootListener
+import com.intellij.openapi.roots.OrderEnumerator
+import com.intellij.util.concurrency.ThreadingAssertions
+import com.intellij.util.concurrency.annotations.RequiresReadLock
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.async
 
 @Service(Service.Level.PROJECT)
-public final class JavaLibraryHolder implements Disposable {
+class JavaLibraryHolder(
+  private val project: Project,
+  private val coroutineScope: CoroutineScope,
+) : Disposable.Default {
 
-  private final Project myProject;
-  private volatile Libraries myValue;
+  @Volatile
+  private var libraries: Deferred<Libraries> = produceRecalculation()
 
-  @Override
-  public void dispose() {
-    // do nothing
-  }
-
-  public static @NotNull JavaLibraryHolder getInstance(@NotNull Project project) {
-    return project.getService(JavaLibraryHolder.class);
-  }
-
-  public JavaLibraryHolder(@NotNull Project project) {
-    myProject = project;
-
-    project.getMessageBus().connect(this)
-      .subscribe(ModuleRootListener.TOPIC, new ModuleRootListener() {
-        @Override
-        public void rootsChanged(@NotNull ModuleRootEvent event) {
-          myValue = null;
+  init {
+    project.messageBus.connect(this)
+      .subscribe(ModuleRootListener.TOPIC, object : ModuleRootListener {
+        override fun rootsChanged(event: ModuleRootEvent) {
+          val current = libraries
+          libraries = produceRecalculation()
+          current.cancel(NewRootsArrived())
         }
-      });
+      })
   }
 
-  Libraries getLibraries() {
-    if (myValue == null) {
-      myValue = fillLibraries(OrderEnumerator.orderEntries(myProject), true);
+  private class NewRootsArrived : CancellationException()
+
+  @RequiresReadLock
+  @JvmName("getLibraries")
+  internal fun getLibraries(): Libraries {
+    ThreadingAssertions.assertReadAccess()
+    val captured = libraries
+    captured.start()
+    if (captured.isCompleted) {
+      try {
+        @OptIn(ExperimentalCoroutinesApi::class)
+        return captured.getCompleted()
+      } catch (_: CancellationException) {
+        // no-op, will proceed with the following lines
+      }
     }
-    return myValue;
+    val newlyComputedLibraries = doComputeLibraries()
+    libraries = CompletableDeferred(newlyComputedLibraries)
+    captured.cancel()
+    return newlyComputedLibraries
+  }
+
+  /**
+   * Returns `null` if the libraries are not ready. This function can be used in read-lock-free environments such as UI thread
+   */
+  @JvmName("getLibrariesOrNull")
+  internal fun getLibrariesOrNull(): Libraries? {
+    val captured = libraries
+    captured.start()
+    if (!captured.isCompleted) {
+      return null
+    }
+    return try {
+      @OptIn(ExperimentalCoroutinesApi::class)
+      captured.getCompleted()
+    }
+    catch (_: CancellationException) {
+      return null
+    }
+  }
+
+  private fun produceRecalculation(): Deferred<Libraries> {
+    return coroutineScope.async(Dispatchers.Default, start = CoroutineStart.LAZY) {
+      readAction {
+        doComputeLibraries()
+      }
+    }
+  }
+
+  @RequiresReadLock
+  private fun doComputeLibraries(): Libraries {
+    return JavaLibraryUtil.fillLibraries(OrderEnumerator.orderEntries(project), true)
+  }
+
+  companion object {
+    @JvmStatic
+    fun getInstance(project: Project): JavaLibraryHolder = project.service()
   }
 }
