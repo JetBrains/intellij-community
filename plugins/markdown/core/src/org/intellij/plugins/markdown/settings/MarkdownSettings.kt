bd6bbc98850017d64b81ad32396489c768987a1d
Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
2026-09-14 14:10:02 +0000

    [markdown] IJPL-254864 Markdown: don't suggest adding .idea/markdown.xml with user preferences to Git
    Merge-request: IJ-MR-223127
    Merged-by: Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
    
    GitOrigin-RevId: 8514686aba29eb4503e83c7045af7c32bc49b729

--- plugins/markdown/core/src/org/intellij/plugins/markdown/settings/MarkdownSettings.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/settings/MarkdownSettings.kt
@@ -13,8 +13,10 @@ import com.intellij.openapi.project.Project
 import com.intellij.util.messages.Topic
 import org.intellij.plugins.markdown.ui.preview.MarkdownHtmlPanelProvider
 
+const val MARKDOWN_SETTINGS_FILE_NAME: String = "markdown.xml"
+
 @Service(Service.Level.PROJECT)
-@State(name = "MarkdownSettings", storages = [(Storage("markdown.xml"))])
+@State(name = "MarkdownSettings", storages = [(Storage(MARKDOWN_SETTINGS_FILE_NAME))])
 class MarkdownSettings(internal val project: Project): SimplePersistentStateComponent<MarkdownSettingsState>(MarkdownSettingsState()) {
   var enableLivePreview: Boolean
     get() = state.enableLivePreview
