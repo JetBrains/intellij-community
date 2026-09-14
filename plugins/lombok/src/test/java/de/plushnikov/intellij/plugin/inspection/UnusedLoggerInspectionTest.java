04d7057e61d0a8478d7901e4b9be8818aaa948f9
mibac138 <5672750+mibac138@users.noreply.github.com>
2026-09-14 11:03:53 +0200

    IDEA-332430 [lombok] Lombok plug-in: warn when there are unused logger annotations
    GitOrigin-RevId: 84b2415ee2e918337ea2d2ace7c6afe221ed22fe

---
+++ plugins/lombok/src/test/java/de/plushnikov/intellij/plugin/inspection/UnusedLoggerInspectionTest.java
@@ -0,0 +1,58 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package de.plushnikov.intellij.plugin.inspection;
+
+import com.intellij.codeInspection.InspectionProfileEntry;
+
+public class UnusedLoggerInspectionTest extends LombokInspectionTest {
+  @Override
+  protected String getBasePath() {
+    return super.getBasePath() + "/" + TEST_DATA_INSPECTION_DIRECTORY + "/unusedLogger";
+  }
+
+  @Override
+  protected InspectionProfileEntry getInspection() {
+    return new UnusedLoggerInspection();
+  }
+
+  public void testUnusedLogger() {
+    doTest();
+    checkQuickFixAll();
+  }
+
+  public void testRemoveUnusedLoggerImport() {
+    doTest();
+    checkQuickFixAll();
+  }
+
+  public void testInvalidAnnotationTargetsWithoutClassLogger() {
+    checkInvalidAnnotationTargets("", "");
+  }
+
+  public void testInvalidAnnotationTargetsWithClassLogger() {
+    checkInvalidAnnotationTargets("@Log", "Object logger = log;");
+  }
+
+  private void checkInvalidAnnotationTargets(String classAnnotation, String loggerUsage) {
+    myFixture.configureByText("InvalidTargets.java", """
+      import lombok.extern.java.Log;
+
+      %s
+      class InvalidTargets {
+        <error>@Log</error>
+        private String field;
+
+        <error>@Log</error>
+        InvalidTargets() {}
+
+        <error>@Log</error>
+        void method(<error>@Log</error> String parameter) {
+          <error>@Log</error> String local = parameter;
+          %s
+        }
+
+        Object value = new <error>@Log</error> Object();
+      }
+      """.formatted(classAnnotation, loggerUsage));
+    myFixture.checkHighlighting();
+  }
+}
