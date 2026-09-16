90a971031b4791bc895b8694d4ae4f41e32e0d93
Alexey Kudravtsev <Alexey.Kudravtsev@jetbrains.com>
2026-09-16 15:21:31 +0200

    revert annotation since they don't work on junit3 (part of IJPL-234343)
    GitOrigin-RevId: 77a42fd87b0c165d8d181ffef4421b5b949614d7

--- platform/platform-tests/testSrc/com/intellij/openapi/editor/impl/marker/PRangeMarkerTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/editor/impl/marker/PRangeMarkerTest.java
@@ -1,8 +1,14 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.editor.impl.marker;
 
+import com.intellij.openapi.editor.impl.RangeMarkersImpl;
 import com.intellij.openapi.editor.impl.RangeMarkerTest;
+import com.intellij.util.ThrowableRunnable;
+import org.jetbrains.annotations.NotNull;
 
-@UsePMarkerImplementation
 public final class PRangeMarkerTest extends RangeMarkerTest {
+  @Override
+  protected void runTestRunnable(@NotNull ThrowableRunnable<Throwable> testRunnable) throws Throwable {
+    RangeMarkersImpl.usePMarkerImplementationIn(()->super.runTestRunnable(testRunnable));
+  }
 }
