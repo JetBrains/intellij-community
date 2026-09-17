4710fe480e27c4a162f65d8bf24f36d27d6091db
Alexander Trushev <aleksandr.trushev@jetbrains.com>
2026-09-17 20:46:07 +0200

    [editor] IJPL-254585 transfer focus owner subscription to new manager
    GitOrigin-RevId: 3256f4ef73e07fdaa4f8ae5012cb0a76c98c371e

--- platform/util/ui/src/com/intellij/util/ui/FocusUtil.java
+++ platform/util/ui/src/com/intellij/util/ui/FocusUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.util.ui;
 
 import com.intellij.openapi.Disposable;
@@ -14,6 +14,7 @@ import java.awt.Component;
 import java.awt.FocusTraversalPolicy;
 import java.awt.KeyboardFocusManager;
 import java.awt.Window;
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
@@ -22,6 +23,7 @@ import java.util.List;
 
 public final class FocusUtil {
   private static final @NonNls String SWING_FOCUS_OWNER_PROPERTY = "focusOwner";
+  private static final @NonNls String MANAGING_FOCUS_PROPERTY = "managingFocus";
 
   public static Component findFocusableComponentIn(Component searchIn, Component toSkip) {
     List<Component> components = UIUtil.uiTraverser(searchIn).toList();
@@ -76,12 +78,74 @@ public final class FocusUtil {
   }
 
   /**
-   * Add {@link PropertyChangeListener} listener to the current {@link KeyboardFocusManager} until the {@code parentDisposable} is disposed
+   * Adds {@code listener} to the {@code focusOwner} property of the current {@link KeyboardFocusManager}, until the
+   * {@code parentDisposable} is disposed. The subscription follows the manager when the manager is replaced.
    */
   public static void addFocusOwnerListener(@NotNull Disposable parentDisposable, @NotNull PropertyChangeListener listener) {
-    KeyboardFocusManager.getCurrentKeyboardFocusManager().addPropertyChangeListener(SWING_FOCUS_OWNER_PROPERTY, listener);
-    Disposer.register(parentDisposable, () -> {
-      KeyboardFocusManager.getCurrentKeyboardFocusManager().removePropertyChangeListener(SWING_FOCUS_OWNER_PROPERTY, listener);
-    });
+    FocusOwnerSubscription subscription = new FocusOwnerSubscription(listener);
+    Disposer.register(parentDisposable, subscription);
+    subscription.moveTo(KeyboardFocusManager.getCurrentKeyboardFocusManager());
+  }
+
+  /**
+   * A manager fires {@code focusOwner} only while it is the current manager, so a subscription that stays on a
+   * replaced manager hears nothing more. A manager announces its own replacement with {@code managingFocus}. This
+   * class then moves the subscription over and reports the focus owner of the new manager.
+   */
+  private static final class FocusOwnerSubscription implements PropertyChangeListener, Disposable {
+    private final PropertyChangeListener myListener;
+    private KeyboardFocusManager myManager;
+    private boolean myDisposed;
+
+    FocusOwnerSubscription(@NotNull PropertyChangeListener listener) {
+      myListener = listener;
+    }
+
+    @Override
+    public void propertyChange(@NotNull PropertyChangeEvent event) {
+      String propertyName = event.getPropertyName();
+      if (SWING_FOCUS_OWNER_PROPERTY.equals(propertyName)) {
+        myListener.propertyChange(event);
+      } else if (MANAGING_FOCUS_PROPERTY.equals(propertyName) && Boolean.FALSE.equals(event.getNewValue())) {
+        KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+        if (moveTo(manager)) {
+          PropertyChangeEvent changeEvent = new PropertyChangeEvent(
+            manager,
+            SWING_FOCUS_OWNER_PROPERTY,
+            null,
+            manager.getFocusOwner()
+          );
+          myListener.propertyChange(changeEvent);
+        }
+      }
+    }
+
+    @Override
+    public synchronized void dispose() {
+      myDisposed = true;
+      unsubscribe();
+    }
+
+    /**
+     * @return whether the subscription moved, which is {@code false} for the manager it already listens to
+     */
+    private synchronized boolean moveTo(@NotNull KeyboardFocusManager manager) {
+      if (myDisposed || manager == myManager) {
+        return false;
+      }
+      unsubscribe();
+      myManager = manager;
+      manager.addPropertyChangeListener(SWING_FOCUS_OWNER_PROPERTY, this);
+      manager.addPropertyChangeListener(MANAGING_FOCUS_PROPERTY, this);
+      return true;
+    }
+
+    private void unsubscribe() {
+      if (myManager != null) {
+        myManager.removePropertyChangeListener(SWING_FOCUS_OWNER_PROPERTY, this);
+        myManager.removePropertyChangeListener(MANAGING_FOCUS_PROPERTY, this);
+        myManager = null;
+      }
+    }
   }
 }
