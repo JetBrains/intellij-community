3014dd7a1a97776b1c856768d092e74347730590
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2026-09-18 16:37:44 +0200

    misc [build scripts]: correcting variable expansion on Windows
    GitOrigin-RevId: a07b44446b8995c8a807094186f69f1754378748

--- platform/build-scripts/bazel/build/bazel-wrapper/bazel.bat
+++ platform/build-scripts/bazel/build/bazel-wrapper/bazel.bat
@@ -4,7 +4,7 @@ setlocal
 
 if "%RUN_WITHOUT_ULTIMATE_ROOT%"=="true" goto skip_auth
 REM Provide packages.jetbrains.team credentials for Bazel in .netrc
-set ULTIMATE_ROOT=%~dp0%..\..\..\..\..\..
+set ULTIMATE_ROOT=%~dp0..\..\..\..\..\..
 set AUTHORIZER=%ULTIMATE_ROOT%\build\private-packages-auth\authorizer.cmd
 REM The script will be missing if run from community repository
 if exist "%AUTHORIZER%" call "%AUTHORIZER%"
