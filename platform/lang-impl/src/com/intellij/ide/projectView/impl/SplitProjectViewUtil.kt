5c1792e5275f4073f4a488ed9f0aa12b0ce49145
Sergei Tachenov <Sergei.Tachenov@jetbrains.com>
2026-09-18 17:16:56 +0300

    [UI, PV] IJPL-158511 Add split PV registry keys for specific modes
    To be able to turn it on for specific modes only,
    add two more keys.
    
    The Registry call is replaced with RegistryManager
    to reduce duplication, but it should be OK,
    as we don't need to rely on the hardcoded default value.
    Better wait for the registry to load.
    
    GitOrigin-RevId: 9a3bea4522100ab85733cccd00b0e9cb15b51c25

--- platform/lang-impl/src/com/intellij/ide/projectView/impl/SplitProjectViewUtil.kt
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/SplitProjectViewUtil.kt
@@ -2,12 +2,23 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.projectView.impl
 
-import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.util.registry.RegistryManager
+import com.intellij.platform.ide.productMode.IdeProductMode
 import org.jetbrains.annotations.ApiStatus
 
-fun isProjectViewSplit(): Boolean = Registry.`is`("project.view.toolwindow.split", defaultValue = false)
+fun isProjectViewSplit(): Boolean {
+  return RegistryManager.getInstance().isProjectViewSplit()
+}
 
 suspend fun isProjectViewSplitAsync(): Boolean {
-  return RegistryManager.getInstanceAsync().`is`("project.view.toolwindow.split")
+  return RegistryManager.getInstanceAsync().isProjectViewSplit()
+}
+
+private fun RegistryManager.isProjectViewSplit(): Boolean {
+  if (IdeProductMode.isMonolith) {
+    return `is`("project.view.toolwindow.split") || `is`("project.view.toolwindow.split.monolith")
+  }
+  else {
+    return `is`("project.view.toolwindow.split") || `is`("project.view.toolwindow.split.remdev")
+  }
 }
