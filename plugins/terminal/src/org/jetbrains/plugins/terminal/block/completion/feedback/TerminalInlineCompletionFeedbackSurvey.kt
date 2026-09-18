c0cfdbd11503d910b94c23fa296a42dc6bf1ca08
Uladzislau Trybukhouski <uladzislau.trybukhouski@jetbrains.com>
2026-09-18 09:04:28 +0200

    [terminal] IJPL-255887 Create feedback survay and dialog for terminal inline completion
    GitOrigin-RevId: 1604e7a23baf27daf0825b5df721472359c93627

---
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/block/completion/feedback/TerminalInlineCompletionFeedbackSurvey.kt
@@ -0,0 +1,59 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.terminal.block.completion.feedback
+
+import com.intellij.openapi.project.Project
+import com.intellij.platform.feedback.FeedbackSurvey
+import com.intellij.platform.feedback.FeedbackSurveyType
+import com.intellij.platform.feedback.InIdeFeedbackSurveyConfig
+import com.intellij.platform.feedback.InIdeFeedbackSurveyType
+import com.intellij.platform.feedback.dialog.BlockBasedFeedbackDialog
+import com.intellij.platform.feedback.dialog.SystemDataJsonSerializable
+import com.intellij.platform.feedback.impl.notification.RequestFeedbackNotification
+import com.intellij.util.PlatformUtils
+import kotlinx.datetime.LocalDate
+import org.jetbrains.plugins.terminal.TerminalBundle
+import org.jetbrains.plugins.terminal.block.reworked.TerminalUsageLocalStorage
+
+internal class TerminalInlineCompletionFeedbackSurvey : FeedbackSurvey() {
+  override val feedbackSurveyType: FeedbackSurveyType<*> = InIdeFeedbackSurveyType(TerminalInlineCompletionFeedbackSurveyConfig())
+}
+
+private class TerminalInlineCompletionFeedbackSurveyConfig : InIdeFeedbackSurveyConfig{
+  override val surveyId: String = "terminal_inline_completion"
+
+  override val lastDayOfFeedbackCollection: LocalDate = LocalDate(2026, 10, 20) // Estimated Code Freeze
+
+  override val requireIdeEAP: Boolean = true
+
+  override fun checkIdeIsSuitable(): Boolean = PlatformUtils.isJetBrainsProduct()
+
+  override fun checkExtraConditionSatisfied(project: Project): Boolean {
+    return true
+  }
+
+  override fun createFeedbackDialog(project: Project, forTest: Boolean): BlockBasedFeedbackDialog<out SystemDataJsonSerializable> {
+    return TerminalInlineCompletionFeedbackDialog(project, forTest)
+  }
+
+  override fun updateStateAfterDialogClosedOk(project: Project) {  }
+
+  override fun createNotification(project: Project, forTest: Boolean): RequestFeedbackNotification {
+    return RequestFeedbackNotification(
+      "Feedback in IDE",
+      TerminalBundle.message("inline.completion.feedback.notification.title"),
+      TerminalBundle.message("inline.completion.feedback.notification.text")
+    )
+  }
+
+  override fun getRespondNotificationActionLabel(): String {
+    return TerminalBundle.message("inline.completion.feedback.notification.btn")
+  }
+
+  override fun getCancelNotificationActionLabel(): String {
+    return TerminalBundle.message("inline.completion.feedback.notification.link")
+  }
+
+  override fun updateStateAfterNotificationShowed(project: Project) {
+    TerminalUsageLocalStorage.getInstance().recordInlineCompletionFeedbackNotificationShown()
+  }
+}
