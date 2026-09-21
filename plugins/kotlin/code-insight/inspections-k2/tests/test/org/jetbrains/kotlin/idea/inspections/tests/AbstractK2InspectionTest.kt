7fa592f255104d1a0d42d9e78acf2fd03b64eb66
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2026-09-21 16:54:04 +0200

    KTIJ-39601 [kotlin] Rename K2_INSPECTION_CLASS directive to INSPECTION_CLASS
    GitOrigin-RevId: aa1d235092cf0ce7a05533736d4ca44a8564496e

--- plugins/kotlin/code-insight/inspections-k2/tests/test/org/jetbrains/kotlin/idea/inspections/tests/AbstractK2InspectionTest.kt
+++ plugins/kotlin/code-insight/inspections-k2/tests/test/org/jetbrains/kotlin/idea/inspections/tests/AbstractK2InspectionTest.kt
@@ -5,8 +5,8 @@ import com.intellij.platform.ide.progress.runWithModalProgressBlocking
 import com.intellij.psi.PsiFile
 import com.intellij.testFramework.common.runAll
 import org.jetbrains.kotlin.idea.base.test.IgnoreTests
-import org.jetbrains.kotlin.idea.core.script.configurations.KotlinScriptService
 import org.jetbrains.kotlin.idea.core.script.alwaysVirtualFile
+import org.jetbrains.kotlin.idea.core.script.configurations.KotlinScriptService
 import org.jetbrains.kotlin.idea.inspections.AbstractInspectionTest
 import org.jetbrains.kotlin.idea.test.KotlinLightProjectDescriptor
 import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
@@ -16,7 +16,6 @@ import kotlin.io.path.Path
 
 abstract class AbstractK2InspectionTest : AbstractInspectionTest() {
 
-    override fun inspectionClassDirective() = "// K2_INSPECTION_CLASS:"
     override fun registerGradlePlugin() {}
 
     override fun getDefaultProjectDescriptor(): KotlinLightProjectDescriptor {
