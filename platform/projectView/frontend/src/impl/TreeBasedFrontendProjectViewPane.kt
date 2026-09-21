a6d7dadeefcb3059d52cd82ff1026bb117932c4c
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2026-09-21 09:25:10 +0300

    [UI, PV] IJPL-255001 Fix Expand Selected in the new PV
    Turns out consumeAsFlow() closes the channel,
    so it can't be reused. Replace with a flow.
    The usual emit-before-collect issue is not applicable here
    because expand requests should only be processed while
    the pane is actually showing, meaning the collector must be running.
    If it's not running, requests can, and should be ignored.
    
    GitOrigin-RevId: a518c3c0803df9a95422b91f75030290d214215e

--- platform/projectView/frontend/src/impl/TreeBasedFrontendProjectViewPane.kt
+++ platform/projectView/frontend/src/impl/TreeBasedFrontendProjectViewPane.kt
@@ -52,10 +52,9 @@ import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.awaitCancellation
 import kotlinx.coroutines.channels.BufferOverflow
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
@@ -92,7 +91,7 @@ internal class TreeBasedFrontendProjectViewPane(
   private val paneTreeModel = FrontendProjectViewPaneTreeModel(project, descriptor)
   private val tree = FrontendProjectViewTree(paneTreeModel.treeModel)
   private val scrollPane = ScrollPaneFactory.createScrollPane(tree, true)
-  private val expandRequests = Channel<ExpandRequest>(capacity = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+  private val expandRequests = MutableSharedFlow<ExpandRequest>(extraBufferCapacity = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
   private val treeExpander = ProjectViewTreeExpander(tree, expandRequests)
   private val autoscrollToSourceHandler = MyAutoscrollToSourceHandler(project)
   private val cutCopyPasteDeleteProvider = FrontendProjectViewCutCopyPasteDeleteProvider(paneTreeModel)
@@ -154,7 +153,7 @@ internal class TreeBasedFrontendProjectViewPane(
     EditSourceOnEnterKeyHandler.install(tree)
     autoscrollToSourceHandler.install(tree)
     tree.launchOnShow("expand requests") {
-      expandRequests.consumeAsFlow().collectLatest { expandRequest ->
+      expandRequests.collectLatest { expandRequest ->
         expand(expandRequest)
       }
     }
@@ -434,7 +433,7 @@ internal class TreeBasedFrontendProjectViewPane(
   }
 }
 
-private class ProjectViewTreeExpander(tree: Tree, private val expandRequests: SendChannel<ExpandRequest>) : DefaultTreeExpander(tree) {
+private class ProjectViewTreeExpander(tree: Tree, private val expandRequests: MutableSharedFlow<ExpandRequest>) : DefaultTreeExpander(tree) {
   override fun isExpandAllVisible(): Boolean {
     return Registry.`is`("ide.project.view.expand.all.action.visible") && !Registry.`is`("ide.project.view.replace.expand.all.with.expand.recursively")
   }
@@ -445,14 +444,14 @@ private class ProjectViewTreeExpander(tree: Tree, private val expandRequests: Se
 
   override fun expandSelected(tree: JTree) {
     val selection = tree.selectionPaths?.toList() ?: return
-    val result = expandRequests.trySend(ExpandRequest(selection))
-    check(!result.isFailure)
+    val result = expandRequests.tryEmit(ExpandRequest(selection))
+    check(result)
   }
 
   override fun expandAll(tree: JTree) {
     val root = tree.model?.root ?: return
-    val result = expandRequests.trySend(ExpandRequest(listOf(CachingTreePath(root))))
-    check(!result.isFailure)
+    val result = expandRequests.tryEmit(ExpandRequest(listOf(CachingTreePath(root))))
+    check(result)
   }
 
   override fun collapseAll(tree: JTree, strict: Boolean, keepSelectionLevel: Int) {
