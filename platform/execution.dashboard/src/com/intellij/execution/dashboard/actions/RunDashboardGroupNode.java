e4e73ed91e6041cf560b22b0243c86c4aae9a1b3
Nikolay Chashnikov <nikolay.chashnikov@jetbrains.com>
2026-09-24 09:36:26 +0200

    IJPL-256569 execution: move classes related to Execution Dashboard from intellij.platform.lang.impl to the corresponding module
    This is needed for consistency and to remove dependency on `intellij.platform.execution` from `intellij.platform.lang.impl`.
    
    GitOrigin-RevId: fccc705f39239e0e96f3b65028dcef601f54abc0

--- platform/execution.dashboard/src/com/intellij/execution/dashboard/actions/RunDashboardGroupNode.java
+++ platform/execution.dashboard/src/com/intellij/execution/dashboard/actions/RunDashboardGroupNode.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.execution.dashboard.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
@@ -13,7 +13,9 @@ import java.util.List;
  * It depends on RunDashboardActionUtils.
  * RunDashboardActionUtils depend on RunDashboardServiceViewContributor and GroupingNode.
  * This interface abstracts RunDashboardServiceViewContributor and GroupingNode and breaks the last dependency.
+ * @deprecated this interface isn't needed anymore
  */
+@Deprecated
 @Internal
 public interface RunDashboardGroupNode {
 
