16812a69de81db9c5b195bbb4162b04ad6243227
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2026-09-24 11:53:13 +0300

    [UI, Tool Windows] IJPL-248129 Fix Problems View stripe title in UI tests
    The test was "fixed" (actually broken) on purpose to make it pass
    when the title (Problems) was accidentally changed to ID
    (Problems View). Now that we've fixed it, we have to fix the test.
    
    GitOrigin-RevId: 6789a8d5a8c917e1daa704dcbec7f9c8f6388bfb

--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/IdeaFrameUiExt.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/IdeaFrameUiExt.kt
@@ -44,7 +44,7 @@ fun IdeaFrameUI.terminalToolWindow(action: TerminalToolWindowUi.() -> Unit = {})
   x(TerminalToolWindowUi::class.java) { componentWithChild(byType(TOOL_WINDOW_ROOT_COMPONENT_CLASS), byType("org.jetbrains.plugins.terminal.TerminalToolWindowPanel")) }.apply(action)
 
 fun IdeaFrameUI.problemsToolWindow(action: ToolWindowUiComponent.() -> Unit = {}): ToolWindowUiComponent =
-  x(ToolWindowUiComponent::class.java) { componentWithChild(byType(TOOL_WINDOW_ROOT_COMPONENT_CLASS), byAccessibleName("Problems View")) }.apply(action)
+  x(ToolWindowUiComponent::class.java) { componentWithChild(byType(TOOL_WINDOW_ROOT_COMPONENT_CLASS), byAccessibleName("Problems")) }.apply(action)
 
 fun IdeaFrameUI.usagesToolWindow(action: ToolWindowUiComponent.() -> Unit = {}): ToolWindowUiComponent =
   x(ToolWindowUiComponent::class.java) { componentWithChild(byType(TOOL_WINDOW_ROOT_COMPONENT_CLASS), byAccessibleName("Find")) }.apply(action)
--- platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/toolwindows/ToolWindowsToolbars.kt
+++ platform/remote-driver/test-sdk/src/com/intellij/driver/sdk/ui/components/common/toolwindows/ToolWindowsToolbars.kt
@@ -26,7 +26,7 @@ class ToolWindowLeftToolbarUi(data: ComponentData) : ToolWindowToolbarUi(data) {
   val structureButton: StripeButtonUi = stripeButton("Structure")
   val servicesButton: StripeButtonUi = stripeButton("Services")
   val terminalButton: StripeButtonUi = stripeButton("Terminal")
-  val problemsButton: StripeButtonUi = stripeButton("Problems View")
+  val problemsButton: StripeButtonUi = stripeButton("Problems")
   val moreButton: StripeButtonUi = stripeButton("More")
   val debugButton: StripeButtonUi = stripeButton("Debug")
   val findButton: StripeButtonUi = stripeButton("Find")
